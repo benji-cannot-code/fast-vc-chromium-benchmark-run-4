bd301f47ae1a43f412c88c5c682dc41465507f2b
cfredric <cfredric@chromium.org>
2022-03-21 16:13:47 +0000

    Split non-disjointness validation errors into kNonDisjointSets, kRepeatedDomain.
    Previously, a set like {owner: a.com, members: [a.com]} would fail
    validation with the kNonDisjoint error code, even if there *are* no
    other sets, because of the way we check for disjointness. Since the
    error codes will be exposed to enterprise admins, it would be better to
    be more accurate.
    
    This CL also contains a slight performance tweak so that we don't incur
    the linear cost of inserting into `elements` unless validation was
    successful. (There's still a linear cost to checking containment in
    `members`, but we assume that an individual set will have a small number
    of members (by policy), so this is ok.)
    
    Change-Id: If6045f02829b344485b906e24fb14e84eb224206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3534465
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983340}

--- services/network/first_party_sets/first_party_set_parser.cc
+++ services/network/first_party_sets/first_party_set_parser.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/files/file_util.h"
@@ -99,8 +100,6 @@ absl::optional<FirstPartySetParser::ParseError> ParseSet(
   if (elements.contains(*canonical_owner))
     return FirstPartySetParser::ParseError::kNonDisjointSets;
 
-  elements.insert(*canonical_owner);
-
   // Confirm that the members field is present, and is an array of strings.
   const base::Value* maybe_members_list =
       value.FindListKey(kFirstPartySetMembersField);
@@ -122,15 +121,21 @@ absl::optional<FirstPartySetParser::ParseError> ParseSet(
     if (!member.has_value())
       return FirstPartySetParser::ParseError::kInvalidOrigin;
 
+    if (*member == *canonical_owner || base::Contains(members, member))
+      return FirstPartySetParser::ParseError::kRepeatedDomain;
+
     if (elements.contains(*member))
       return FirstPartySetParser::ParseError::kNonDisjointSets;
 
     members.push_back(*member);
-    elements.insert(std::move(*member));
   }
 
-  out_set = std::make_pair(*canonical_owner,
-                           base::flat_set<net::SchemefulSite>(members));
+  elements.insert(*canonical_owner);
+  for (const auto& member : members) {
+    elements.insert(member);
+  }
+
+  out_set = std::make_pair(*canonical_owner, members);
   return absl::nullopt;
 }
 
