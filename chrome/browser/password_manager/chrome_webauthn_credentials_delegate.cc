3c901aed79c52052f43969804fb7af1dbb833577
Ken Buchanan <kenrb@chromium.org>
2022-03-21 17:24:58 +0000

    [WebAuthn] Add Android Conditional UI delegate class
    Adds a new class that caches callbacks between Password Manager autofill
    and the Android Web Authentication implementation. This is necessary
    because autofill invocation and a Web Authentication getAssertion
    request are received independently from the renderer but there has to
    be communication between them.
    
    A subsequent CL will plumb a JNI call on RenderFrameHostAndroid to
    the delegate that is added here, along with the delegate's actual
    interface in content/public.
    
    Bug: 1171985
    Change-Id: I0da3fc3382d6414713a964b2231d6087a62c6699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526902
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983367}

--- chrome/browser/password_manager/chrome_webauthn_credentials_delegate.cc
+++ chrome/browser/password_manager/chrome_webauthn_credentials_delegate.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/webauthn/chrome_conditional_ui_delegate_android.h"
+#endif
+
 ChromeWebAuthnCredentialsDelegate::ChromeWebAuthnCredentialsDelegate(
     ChromePasswordManagerClient* client)
     : client_(client) {}
@@ -29,7 +33,17 @@ bool ChromeWebAuthnCredentialsDelegate::IsWebAuthnAutofillEnabled() const {
 
 void ChromeWebAuthnCredentialsDelegate::SelectWebAuthnCredential(
     std::string backend_id) {
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
+  auto* credentials_delegate =
+      ChromeConditionalUiDelegateAndroid::GetConditionalUiDelegate(
+          client_->web_contents());
+  if (!credentials_delegate) {
+    std::move(retrieve_suggestions_callback_).Run();
+    return;
+  }
+  credentials_delegate->OnWebAuthnAccountSelected(
+      std::vector<uint8_t>(backend_id.begin(), backend_id.end()));
+#else
   ChromeAuthenticatorRequestDelegate* authenticator_delegate =
       AuthenticatorRequestScheduler::GetRequestDelegate(
           client_->web_contents());
@@ -38,7 +52,7 @@ void ChromeWebAuthnCredentialsDelegate::SelectWebAuthnCredential(
   }
   authenticator_delegate->dialog_model()->OnAccountPreselected(
       std::vector<uint8_t>(backend_id.begin(), backend_id.end()));
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 const std::vector<autofill::Suggestion>&
@@ -51,7 +65,16 @@ void ChromeWebAuthnCredentialsDelegate::RetrieveWebAuthnSuggestions(
   retrieve_suggestions_callback_ = std::move(callback);
 
 #if BUILDFLAG(IS_ANDROID)
-  std::move(retrieve_suggestions_callback_).Run();
+  auto* credentials_delegate =
+      ChromeConditionalUiDelegateAndroid::GetConditionalUiDelegate(
+          client_->web_contents());
+  if (!credentials_delegate) {
+    std::move(retrieve_suggestions_callback_).Run();
+    return;
+  }
+  credentials_delegate->RetrieveWebAuthnCredentials(
+      base::BindOnce(&ChromeWebAuthnCredentialsDelegate::OnCredentialsReceived,
+                     weak_ptr_factory_.GetWeakPtr()));
 #else
   ChromeAuthenticatorRequestDelegate* authenticator_delegate =
       AuthenticatorRequestScheduler::GetRequestDelegate(
