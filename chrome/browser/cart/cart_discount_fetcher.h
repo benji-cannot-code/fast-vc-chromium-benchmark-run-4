835ef5a22ea74e936931b1cce9585d2627a6dbba
Victor Vianna <victorvianna@google.com>
2022-03-21 20:10:29 +0000

    Fix IWYU in chrome/browser/cart/cart_discount_fetcher.h
    Caused a build failure in multiple bots for PS1 in
    crrev.com/c/3540326. Example:
    https://ci.chromium.org/ui/p/chromium/builders/try/chromeos-amd64-generic-rel/1105054/overview
    
    Change-Id: I433b53586ce03ac7f35d5c4c0dc9c371545bbf91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540940
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Owners-Override: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#983463}

--- chrome/browser/cart/cart_discount_fetcher.h
+++ chrome/browser/cart/cart_discount_fetcher.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <unordered_map>
 
+#include "base/containers/flat_map.h"
 #include "base/memory/scoped_refptr.h"
 #include "chrome/browser/cart/cart_db.h"
 #include "chrome/browser/cart/cart_db_content.pb.h"
