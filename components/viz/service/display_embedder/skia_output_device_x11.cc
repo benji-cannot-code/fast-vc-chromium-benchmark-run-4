dc111b83d16a8734a39dd74ed2bcc19efd4017d8
Peng Huang <penghuang@chromium.org>
2022-03-21 18:08:40 +0000

    RawDraw: use MSAA for output surface
    Without MSAA, skia may use slow path for AA. It may regress performance,
    so use MSAA for the output surface with RawDraw. To reduce the scope,
    this CL only uses MSAA on Linux with SkiaOutputOutputDeviceGL and
    SkiaOutputDeviceVulkan.
    
    This CL change SkiaOutputSurfaceImplOnGpu::Reshape() to take
    SkSurfaceCharacterization param. It contains the all related surface
    infos (size, colorType, colorSpace, sampleCount, etc). And all
    SkiaOutputDevice implementations can create SkSurface based on the
    given SkSurfaceCharacterization.
    
    Bug: 1301316
    Change-Id: I56f4f490e82538303b3c35583b53a11a66c8cbce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488719
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983396}

--- components/viz/service/display_embedder/skia_output_device_x11.cc
+++ components/viz/service/display_embedder/skia_output_device_x11.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display_embedder/skia_output_device_x11.h"
 
 #include <utility>
+#include <vector>
 
 #include "base/logging.h"
 #include "base/memory/ref_counted_memory.h"
@@ -70,17 +71,17 @@ SkiaOutputDeviceX11::~SkiaOutputDeviceX11() {
   connection_->FreeGC({gc_});
 }
 
-bool SkiaOutputDeviceX11::Reshape(const gfx::Size& size,
-                                  float device_scale_factor,
-                                  const gfx::ColorSpace& color_space,
-                                  gfx::BufferFormat format,
-                                  gfx::OverlayTransform transform) {
-  if (!SkiaOutputDeviceOffscreen::Reshape(size, device_scale_factor,
-                                          color_space, format, transform)) {
+bool SkiaOutputDeviceX11::Reshape(
+    const SkSurfaceCharacterization& characterization,
+    const gfx::ColorSpace& color_space,
+    float device_scale_factor,
+    gfx::OverlayTransform transform) {
+  if (!SkiaOutputDeviceOffscreen::Reshape(characterization, color_space,
+                                          device_scale_factor, transform)) {
     return false;
   }
-  auto ii =
-      SkImageInfo::MakeN32(size.width(), size.height(), kOpaque_SkAlphaType);
+  auto ii = SkImageInfo::MakeN32(
+      characterization.width(), characterization.height(), kOpaque_SkAlphaType);
   std::vector<uint8_t> mem(ii.computeMinByteSize());
   pixels_ = base::RefCountedBytes::TakeVector(&mem);
   return true;
