45506e441707b612b2bb07c569e6c082b03b3c89
David Sanders <dsanders11@ucsbalum.com>
2022-03-21 12:22:25 +0000

    Remove unused include of //media/base/audio_bus.h
    Bug: 242216
    Change-Id: If506d49beeebb3a6587fa3a75a1db965d55aa8eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537760
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#983268}

--- media/base/audio_parameters.h
+++ media/base/audio_parameters.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "media/base/audio_bus.h"
 #include "media/base/audio_latency.h"
 #include "media/base/audio_point.h"
 #include "media/base/channel_layout.h"
