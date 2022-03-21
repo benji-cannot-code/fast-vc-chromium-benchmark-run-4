ff76c3f4501701b294665ede0b33b138cf7f8cf7
Irina Fedorova <irfedorova@google.com>
2022-03-21 19:46:18 +0000

    Add session-type crash keys to Crash Reports
    If we set a crash key in Ash, but Lacros crashes, the crash key will not
    be shown in a crash report. Because of it we need to set session-type
    crash key in both Ash and Lacros.
    
    Bug: b/207562010
    Change-Id: I6c8cf34407dfc92109807a0fad774252495ec422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529211
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#983454}

--- components/user_manager/user_manager_base.cc
+++ components/user_manager/user_manager_base.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/values.h"
+#include "components/crash/core/common/crash_key.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -85,6 +86,30 @@ UserType GetStoredUserType(const base::Value* prefs_user_types,
     return USER_TYPE_REGULAR;
   }
   return static_cast<UserType>(int_user_type);
+}
+
+std::string UserTypeToString(UserType user_type) {
+  switch (user_type) {
+    case USER_TYPE_REGULAR:
+      return "regular";
+    case USER_TYPE_CHILD:
+      return "child";
+    case USER_TYPE_GUEST:
+      return "guest";
+    case USER_TYPE_PUBLIC_ACCOUNT:
+      return "managed-guest-session";
+    case USER_TYPE_KIOSK_APP:
+      return "chrome-app-kiosk";
+    case USER_TYPE_ARC_KIOSK_APP:
+      return "arc-kiosk";
+    case USER_TYPE_WEB_KIOSK_APP:
+      return "web-kiosk";
+    case USER_TYPE_ACTIVE_DIRECTORY:
+      return "active-directory";
+    case NUM_USER_TYPES:
+      NOTREACHED();
+      return "";
+  }
 }
 
 }  // namespace
@@ -235,6 +260,9 @@ void UserManagerBase::UserLoggedIn(const AccountId& account_id,
 
   UMA_HISTOGRAM_ENUMERATION(
       "UserManager.LoginUserType", active_user_->GetType(), NUM_USER_TYPES);
+
+  static crash_reporter::CrashKeyString<32> session_type("session-type");
+  session_type.Set(UserTypeToString(active_user_->GetType()));
 
   GetLocalState()->SetString(
       kLastLoggedInGaiaUser,
