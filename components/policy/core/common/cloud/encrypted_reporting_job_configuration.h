2154f2ca665890d12bbe181d520e6e18f4e0b821
Hong Xu <xuhong@chromium.org>
2022-03-21 23:33:46 +0000

    Mandate requestId field
    Bug: b:225412986
    Fixed: b:225412986
    Change-Id: I676301729f4cf7df1fc2ee3c4b81996dba6a217e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540907
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#983553}

--- components/policy/core/common/cloud/encrypted_reporting_job_configuration.h
+++ components/policy/core/common/cloud/encrypted_reporting_job_configuration.h
@@ -50,6 +50,8 @@ namespace policy {
 //       }
 //     }
 //   ],
+//   "attachEncryptionSettings": true,  // optional field
+//   "requestId": "SomeString",
 //   "device": {
 //     "client_id": "abcdef1234",
 //     "dmToken": "abcdef1234",
