9e8e63eb196484bd9be4e691123328f68a6bdd8e
Addison Luh <aluh@chromium.org>
2022-03-21 21:00:36 +0000

    Clean up some drag drop and gesture code.
    Change-Id: I7886965c7d67f7fcdfc650c13e537fa8ec951218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537575
    Auto-Submit: Addison Luh <aluh@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983481}

--- ash/drag_drop/drag_drop_tracker.cc
+++ ash/drag_drop/drag_drop_tracker.cc
@@ -84,11 +84,9 @@ class CaptureWindowActivationDelegate : public ::wm::ActivationDelegate {
 std::unique_ptr<aura::Window> CreateCaptureWindow(
     aura::Window* context_root,
     aura::WindowDelegate* delegate) {
-  static CaptureWindowActivationDelegate* activation_delegate_instance = NULL;
-  if (!activation_delegate_instance)
-    activation_delegate_instance = new CaptureWindowActivationDelegate;
-  std::unique_ptr<aura::Window> window =
-      std::make_unique<aura::Window>(delegate);
+  static CaptureWindowActivationDelegate* activation_delegate_instance =
+      new CaptureWindowActivationDelegate();
+  auto window = std::make_unique<aura::Window>(delegate);
   // Set type of window as popup to prevent different window manager codes
   // trying to manage this window.
   window->SetType(aura::client::WINDOW_TYPE_POPUP);
@@ -105,10 +103,9 @@ std::unique_ptr<aura::Window> CreateCaptureWindow(
 
 DragDropTracker::DragDropTracker(aura::Window* context_root,
                                  CancelDragDropCallback callback)
-    : tracker_window_delegate_(new DragDropTrackerDelegate(callback)) {
-  capture_window_ =
-      CreateCaptureWindow(context_root, tracker_window_delegate_.get());
-}
+    : tracker_window_delegate_(new DragDropTrackerDelegate(callback)),
+      capture_window_(
+          CreateCaptureWindow(context_root, tracker_window_delegate_.get())) {}
 
 DragDropTracker::~DragDropTracker() {
   capture_window_->ReleaseCapture();
