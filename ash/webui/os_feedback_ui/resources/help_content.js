ca0fe634de46ac08f18dc3f30dd7eb3685e6421e
xiangdong kong <xiangdongkong@google.com>
2022-03-21 17:33:24 +0000

    feedback: add icons for help content types
    - Add an icon for Forum content type.
    - Add an icon for both Article and Unknown content types.
    - Use rgb() over #hex when not a shade of gray (like #333).
        fill: #db4437 (replace with rgb(219, 68, 55))
        fill: #0f9d58 (replace with rgb(15, 157, 88))
        fill: #ffcd40 (replace with rgb(255, 205, 64))
    
    See [1] for a screenshot showing the icons.
    
    [1] https://screenshot.googleplex.com/BuEp9RogcePHffD.
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I230fd9b3111f93af75890e30fd863a2dc9d6d232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529231
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#983376}

--- ash/webui/os_feedback_ui/resources/help_content.js
+++ ash/webui/os_feedback_ui/resources/help_content.js
@@ -3,10 +3,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './help_resources_icons.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import {mojoString16ToString} from '//resources/ash/common/mojo_utils.js';
 import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {HelpContent, HelpContentList, HelpContentType} from './feedback_types.js';
 
-import {HelpContent, HelpContentList} from './feedback_types.js';
+/**
+ * @const {string}
+ */
+const ICON_NAME_FOR_ARTICLE = 'content-type:article';
+
+/**
+ * @const {string}
+ */
+const ICON_NAME_FOR_FORUM = 'content-type:forum';
 
 /**
  * @fileoverview
@@ -29,6 +40,25 @@ export class HelpContentElement extends PolymerElement {
        */
       helpContentList: {type: HelpContentList, value: () => []}
     };
+  }
+
+  /**
+   * Find the icon name to be used for a help content type.
+   * @param {!HelpContentType} contentType
+   * @return {string}
+   * @protected
+   */
+  getIcon_(contentType) {
+    switch (contentType) {
+      case HelpContentType.kForum:
+        return ICON_NAME_FOR_FORUM;
+      case HelpContentType.kArticle:
+        return ICON_NAME_FOR_ARTICLE;
+      case HelpContentType.kUnknown:
+        return ICON_NAME_FOR_ARTICLE;
+      default:
+        return ICON_NAME_FOR_ARTICLE;
+    }
   }
 
   /**
