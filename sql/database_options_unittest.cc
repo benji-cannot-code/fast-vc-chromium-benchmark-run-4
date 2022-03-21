06562cf8f5228d262d95c373bd9b956210c1dde6
Victor Costan <pwnall@chromium.org>
2022-03-21 23:06:08 +0000

    sql: Introduce strongly typed enums for SQLite result codes.
    This CL introduces the following strongly typed enums, which are meant
    to replace raw integers in Chrome's layer on top of SQLite.
    
    * SqliteResultCode represents all result codes.
    * SqliteErrorCode represent a result code that's known to be an error.
    
    This CL integrates each new type in one place, to give an idea of what
    our code will look like once we adopt these new types.
    
    * SqliteResultCode is integrated in
      Database::ExecuteAndReturnResultCode().
    * SqliteErrorCode is integrated in Database::OnSqliteError().
    
    The new enums are declared in //sql/sqlite_result_code.h, together with
    helper functions for converting raw integers returned by the SQLite API.
    The values for the new enums are defined in a separate header,
    //sql/sqlite_result_code_values.h. This is intended to help readability,
    as sqlite_result_code.h has everything developers need to start using
    the types, while the boilerplate is relegated to
    sqlite_result_code_values.h.
    
    Last, this CL moves the SqliteLoggedResultCode logic from
    error_metrics.{h,cc} into the newly introduced files. This is done in
    part because this type is also a strongly typed replacement for SQLite's
    result codes, and in part because we want all the SQLite result code
    conversion logic to share one mapping table.
    
    Bug: 1308290
    Change-Id: I580ae4649cabccaab74c1d4749b5f349cf42f047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538180
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983543}

--- sql/database_options_unittest.cc
+++ sql/database_options_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
-#include "sql/error_metrics.h"
+#include "sql/sqlite_result_code.h"
 
 #include "sql/database.h"
 #include "sql/statement.h"
--- sql/statement.cc
+++ sql/statement.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "sql/sqlite_result_code.h"
 #include "third_party/sqlite/sqlite3.h"
 
 namespace sql {
@@ -588,9 +589,11 @@ int Statement::CheckSqliteResultCode(int sqlite_result_code) {
       (sqlite_result_code == SQLITE_OK || sqlite_result_code == SQLITE_ROW ||
        sqlite_result_code == SQLITE_DONE);
 
-  // Database::OnSqliteError() DCHECKs for
-  if (!succeeded_ && ref_.get() && ref_->database())
-    ref_->database()->OnSqliteError(sqlite_result_code, this, nullptr);
+  if (!succeeded_ && ref_.get() && ref_->database()) {
+    SqliteErrorCode sqlite_error_code =
+        ToSqliteErrorCode(ToSqliteResultCode(sqlite_result_code));
+    ref_->database()->OnSqliteError(sqlite_error_code, this, nullptr);
+  }
   return sqlite_result_code;
 }
 
