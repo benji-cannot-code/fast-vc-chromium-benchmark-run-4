73c70716b837d1f6b5ab5c2fc6338f7dea8cf89e
S. Ganesh <ganesh@chromium.org>
2022-03-22 04:19:21 +0000

    InstallerData needs to be written to the application installer dir
    I was writing the InstallerData to the current module directory in
    crrev/982553, which would be the directory for the Updater itself, not
    the application installer. This CL corrects that bug by writing to the
    application installer directory.
    
    Bug: 1282020
    Change-Id: Id1e157aa0ab57f2896c25407a91bf0e8aad8ba70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537103
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983659}

--- chrome/updater/util_unittest.cc
+++ chrome/updater/util_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/path_service.h"
 #include "base/strings/strcat.h"
 #include "base/test/scoped_command_line.h"
 #include "chrome/updater/constants.h"
@@ -43,12 +44,19 @@ TEST(Util, AppArgsAndAP) {
 }
 
 TEST(Util, WriteInstallerDataToTempFile) {
-  EXPECT_FALSE(WriteInstallerDataToTempFile(""));
+  base::FilePath directory;
+  ASSERT_TRUE(base::PathService::Get(base::DIR_MODULE, &directory));
+
+  EXPECT_FALSE(WriteInstallerDataToTempFile(directory, ""));
 
   const std::string kInstallerData =
       R"({"distribution":{"msi":true,"allow_downgrade":false}})";
+  EXPECT_FALSE(WriteInstallerDataToTempFile(
+      directory.Append(FILE_PATH_LITERAL("NonExistentDirectory")),
+      kInstallerData));
+
   const absl::optional<base::FilePath> installer_data_file =
-      WriteInstallerDataToTempFile(kInstallerData);
+      WriteInstallerDataToTempFile(directory, kInstallerData);
   ASSERT_TRUE(installer_data_file);
 
   std::string contents;
