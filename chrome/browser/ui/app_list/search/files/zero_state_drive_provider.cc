9d4c09bb99859136cea9400217f8033f5dca5017
tby <tby@chromium.org>
2022-03-22 01:58:50 +0000

    Call ClearResultsSilently on Start() for zero-state providers
    This ensures no zero-state results appear in the search results list on
    the first character of a search query.
    
    Bug: 1298854
    Change-Id: I1a69fe253be6aae5a20c968f030f8545d47e09a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539600
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983608}

--- chrome/browser/ui/app_list/search/files/zero_state_drive_provider.cc
+++ chrome/browser/ui/app_list/search/files/zero_state_drive_provider.cc
@@ -251,6 +251,10 @@ bool ZeroStateDriveProvider::ShouldBlockZeroState() const {
   return true;
 }
 
+void ZeroStateDriveProvider::Start(const std::u16string& query) {
+  ClearResultsSilently();
+}
+
 void ZeroStateDriveProvider::StartZeroState() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   ClearResultsSilently();
