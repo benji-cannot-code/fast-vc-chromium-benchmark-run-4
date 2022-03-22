4e1e10015f665ac7f553e87c1c3221e360cecc76
Grayson LaFleur <graysonlafleur@google.com>
2022-03-22 16:20:34 +0000

    Update proto file with follow changes:
    - change variable name from templates to collectionItems inside the Collection Message
    - change variable name from templateid to noteTemplate inside the CollectionItem Message
    
    Bug: 1309057
    Change-Id: Ibed42465dc6375f9c5801e9485afb204932862d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542625
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Grayson LaFleur <graysonlafleur@google.com>
    Cr-Commit-Position: refs/heads/main@{#983873}

--- components/content_creation/notes/core/templates/template_store.cc
+++ components/content_creation/notes/core/templates/template_store.cc
@@ -63,7 +63,6 @@ void TemplateStore::FetchTemplates(GetTemplatesCallback callback) {
 
 void TemplateStore::GetTemplates(GetTemplatesCallback callback) {
   if (IsDynamicTemplatesEnabled()) {
-    LOG(ERROR) << "enabled";
     FetchTemplates(std::move(callback));
   } else {
     base::PostTaskAndReplyWithResult(
@@ -91,20 +90,19 @@ std::vector<NoteTemplate> TemplateStore::BuildDefaultTemplates() {
   return templates;
 }
 
-bool TemplateStore::TemplateDateAvailable(
-    proto::CollectionItem current_template,
-    base::Time today) {
+bool TemplateStore::TemplateDateAvailable(proto::CollectionItem current_item,
+                                          base::Time today) {
   base::Time activation;
   base::Time expiration;
 
-  if (current_template.has_activation() &&
-      (!ConvertProtoDateToTime(current_template.activation(), activation) ||
+  if (current_item.has_activation() &&
+      (!ConvertProtoDateToTime(current_item.activation(), activation) ||
        today < activation)) {
     return false;
   }
 
-  if (current_template.has_expiration() &&
-      (!ConvertProtoDateToTime(current_template.expiration(), expiration) ||
+  if (current_item.has_expiration() &&
+      (!ConvertProtoDateToTime(current_item.expiration(), expiration) ||
        today >= expiration)) {
     return false;
   }
@@ -113,10 +111,10 @@ bool TemplateStore::TemplateDateAvailable(
 }
 
 bool TemplateStore::TemplateLocationAvailable(
-    proto::CollectionItem current_template) {
+    proto::CollectionItem current_item) {
   // If there are no locations set, the template is considered available for all
   // locations.
-  if (current_template.geo_size() == 0) {
+  if (current_item.geo_size() == 0) {
     return true;
   }
 
@@ -126,7 +124,7 @@ bool TemplateStore::TemplateLocationAvailable(
     return false;
   }
 
-  for (std::string template_location : current_template.geo()) {
+  for (std::string template_location : current_item.geo()) {
     if (country_code_ == template_location) {
       return true;
     }
@@ -149,20 +147,20 @@ std::vector<NoteTemplate> TemplateStore::ParseTemplatesFromString(
 
   int numTemplates = 0;
 
-  for (int i = 0; i < collection.templates_size() &&
+  for (int i = 0; i < collection.collectionitems_size() &&
                   numTemplates < collection.max_template_number();
        i++) {
-    proto::CollectionItem current_template = collection.templates(i);
+    proto::CollectionItem current_item = collection.collectionitems(i);
 
-    if (!TemplateDateAvailable(current_template, today)) {
+    if (!TemplateDateAvailable(current_item, today)) {
       continue;
     }
 
-    if (!TemplateLocationAvailable(current_template)) {
+    if (!TemplateLocationAvailable(current_item)) {
       continue;
     }
 
-    templates.push_back(NoteTemplate(current_template.templateid()));
+    templates.push_back(NoteTemplate(current_item.notetemplate()));
     numTemplates++;
   }
 
