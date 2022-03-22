23bb3841ed8180428afa8bd47eb4b2350ace2cfd
Andrea Orru <andreaorru@chromium.org>
2022-03-22 07:04:02 +0000

    Update Ash extension keeplist in Move Migrator
    This commit updates the list of ids for extensions
    that have to stay in Ash only after the migration
    to Lacros.
    
    Source of truth: go/1st-party-keep-list
    
    Bug: 1302613
    Change-Id: I9b6e965beeb585af7b33dd3bfbf57486e270d89a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527788
    Reviewed-by: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983696}

--- chrome/browser/ash/crosapi/browser_data_migrator_util_unittest.cc
+++ chrome/browser/ash/crosapi/browser_data_migrator_util_unittest.cc
@@ -72,7 +72,7 @@ void SetUpLocalStorage(const base::FilePath& path,
   batch.Put("VERSION", "1");
 
   std::string keep_extension_id =
-      browser_data_migrator_util::kExtensionKeepList[0];
+      browser_data_migrator_util::kExtensionsAshOnly[0];
   batch.Put("META:chrome-extension://" + keep_extension_id, "meta");
   batch.Put("_chrome-extension://" + keep_extension_id + "\x00key1"s, "value1");
 
@@ -96,7 +96,7 @@ void SetUpStateStore(const base::FilePath& path,
 
   leveldb::WriteBatch batch;
   std::string keep_extension_id =
-      browser_data_migrator_util::kExtensionKeepList[0];
+      browser_data_migrator_util::kExtensionsAshOnly[0];
   batch.Put(keep_extension_id + ".key1", "value1");
   batch.Put(keep_extension_id + ".key2", "value2");
   batch.Put(std::string(kMoveExtensionId) + ".key1", "value1");
@@ -212,7 +212,7 @@ TEST(BrowserDataMigratorUtilTest, GetExtensionKeys) {
   db.reset();
 
   std::string keep_extension_id =
-      browser_data_migrator_util::kExtensionKeepList[0];
+      browser_data_migrator_util::kExtensionsAshOnly[0];
   ExtensionKeys expected_keys = {
       {keep_extension_id,
        {
@@ -280,7 +280,7 @@ TEST(BrowserDataMigratorUtilTest, MigrateLevelDB) {
   db.reset();
 
   std::string keep_extension_id =
-      browser_data_migrator_util::kExtensionKeepList[0];
+      browser_data_migrator_util::kExtensionsAshOnly[0];
   ExtensionKeys expected_keys = {
       {keep_extension_id,
        {
