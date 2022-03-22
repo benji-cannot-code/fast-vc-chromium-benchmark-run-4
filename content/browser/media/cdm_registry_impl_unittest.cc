1f71ef4c8b66f0a36c3c3a9a3575508e0d6c728f
Xiaohan Wang <xhwang@chromium.org>
2022-03-22 01:10:17 +0000

    media: Add CdmInfo::Status
    This CL adds CdmInfo::Status enum class and cdm_info.status member to
    keep track the status of cdm_info.capability. "kUninitialized" provides
    a clear way to signal "need for lazy initialization". New *Disabled
    statuses can be used to indicate why it's disabled.
    
    Now after lazy initialization if there's no capability supported, either
    because some feature is disabled or the device doesn't support the key
    system and robustness combination, the CdmInfo will NOT be removed from
    CdmRegistry. Instead, we'll just update the status. This status can be
    seen in the "CDMs" tab in about://media-internals, providing an easy
    way to understand why it's not supported.
    
    Bug: 1296219
    Test: Updated tests
    Change-Id: I3d48a0a30f5097bec2be7147c9085e2343e83dc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536895
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983586}

--- content/browser/media/cdm_registry_impl_unittest.cc
+++ content/browser/media/cdm_registry_impl_unittest.cc
@@ -416,7 +416,8 @@ TEST_F(CdmRegistryImplTest, KeySystemCapabilities_LazyInitialize_NotSupported) {
 
   auto cdm_info = cdm_registry_.GetCdmInfo(
       kTestKeySystem, CdmInfo::Robustness::kHardwareSecure);
-  ASSERT_FALSE(cdm_info);
+  ASSERT_EQ(cdm_info->status, CdmInfo::Status::kEnabled);
+  ASSERT_FALSE(cdm_info->capability);
 }
 
 TEST_F(CdmRegistryImplTest, KeySystemCapabilities_HardwareSecureDisabled) {
@@ -431,7 +432,9 @@ TEST_F(CdmRegistryImplTest, KeySystemCapabilities_HardwareSecureDisabled) {
 
   auto cdm_info = cdm_registry_.GetCdmInfo(
       kTestKeySystem, CdmInfo::Robustness::kHardwareSecure);
-  ASSERT_FALSE(cdm_info);
+  ASSERT_EQ(cdm_info->status,
+            CdmInfo::Status::kHardwareSecureDecryptionDisabled);
+  ASSERT_FALSE(cdm_info->capability);
 }
 
 TEST_F(CdmRegistryImplTest, KeySystemCapabilities_SoftwareAndHardwareSecure) {
