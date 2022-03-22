583b399dd5e41dca49d95465a681a4d9023a263e
Boris Sazonov <bsazonov@chromium.org>
2022-03-22 17:44:43 +0000

    Actually run OSSettingsSearchEngineV3Test
    It looks like https://crrev.com/c/3370006 didn't actually have
    mocha.run() call, meaning that OSSettingsSearchEngineV3Test tests
    aren't executed. This CL fixes that.
    
    Bug: 1309147
    Change-Id: I9efbbec6ee888ce53607a0ef4ee6f30a1b8b4c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540724
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983907}

--- chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
@@ -281,6 +281,10 @@ var OSSettingsSearchEngineV3Test = class extends OSSettingsV3BrowserTest {
   }
 };
 
+TEST_F('OSSettingsSearchEngineV3Test', 'AllJsTests', () => {
+  mocha.run();
+});
+
 var OSSettingsAppManagementAppDetailsV3Test =
     class extends OSSettingsV3BrowserTest {
   /** @override */
