d5ad0d913ce20aa674d0e04b5a28e81f50304e60
David Sanders <dsanders11@ucsbalum.com>
2022-03-22 19:04:01 +0000

    Remove unused include of browser_save_password_progress_logger.h
    Also adds an include to another file which was getting it
    transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I0bd26a8e0311033e1e99bc3aa40ceb803e503d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539004
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#983953}

--- components/password_manager/core/browser/http_auth_manager.h
+++ components/password_manager/core/browser/http_auth_manager.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_HTTP_AUTH_MANAGER_H_
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_HTTP_AUTH_MANAGER_H_
 
-#include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
-
 namespace password_manager {
 
 class PasswordManagerClient;
