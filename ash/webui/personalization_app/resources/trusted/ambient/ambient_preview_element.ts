42f3da97bc4eca2bb394842f24f950026b7998f8
Jason Thai <jasontt@chromium.org>
2022-03-22 21:24:36 +0000

    personalization: zero state for ambient mode on main page
    Shows a semi transparent preview and a message to let users know they
    can turn on the screensaver feature.
    
    Screenshot: https://screenshot.googleplex.com/9nwZuTzQVboF8JA
    
    BUG=b/225198640
    TEST=New browser tests
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ic3c2651cab16a6a6ff08a7340fa5381f2549e107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540917
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984024}

--- ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
+++ ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
@@ -11,14 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import '../../common/styles.js';
+import '../cros_button_style.js';
 
 import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {isNonEmptyArray} from '../../common/utils.js';
 import {AmbientModeAlbum, TopicSource} from '../personalization_app.mojom-webui.js';
+import {Paths, PersonalizationRouter} from '../personalization_router_element.js';
 import {WithPersonalizationStore} from '../personalization_store.js';
 import {getPhotoCount, getTopicSourceName, replaceResolutionSuffix} from '../utils.js';
 
+import {setAmbientModeEnabled} from './ambient_controller.js';
+import {getAmbientProvider} from './ambient_interface_provider.js';
 import {AmbientObserver} from './ambient_observer.js';
 
 export class AmbientPreview extends WithPersonalizationStore {
@@ -32,6 +36,7 @@ export class AmbientPreview extends WithPersonalizationStore {
 
   static get properties() {
     return {
+      ambientModeEnabled_: Boolean,
       albums_: {
         type: Array,
         value: null,
@@ -48,13 +53,19 @@ export class AmbientPreview extends WithPersonalizationStore {
         type: AmbientModeAlbum,
         computed: 'computeFirstPreviewAlbum_(previewAlbums_)',
       },
+      loading_: {
+        type: Boolean,
+        computed: 'computeLoading_(ambientModeEnabled_, albums_, topicSource_)',
+      }
     };
   }
 
+  private ambientModeEnabled_: boolean|null;
   private albums_: AmbientModeAlbum[]|null;
   private topicSource_: TopicSource|null;
   private previewAlbums_: AmbientModeAlbum[]|null;
   private firstPreviewAlbum_: AmbientModeAlbum|null;
+  private loading_: boolean;
 
   override ready() {
     super.ready();
@@ -63,9 +74,24 @@ export class AmbientPreview extends WithPersonalizationStore {
 
   override connectedCallback() {
     super.connectedCallback();
+    this.watch(
+        'ambientModeEnabled_', state => state.ambient.ambientModeEnabled);
     this.watch('albums_', state => state.ambient.albums);
     this.watch('topicSource_', state => state.ambient.topicSource);
     this.updateFromStore();
+  }
+
+  private computeLoading_(): boolean {
+    return this.ambientModeEnabled_ === null || this.albums_ === null ||
+        this.topicSource_ === null;
+  }
+
+  /** Enable ambient mode and navigates to the ambient subpage. */
+  private async onClickAmbientModeButton_(event: Event) {
+    event.stopPropagation();
+    await setAmbientModeEnabled(
+        /*ambientModeEnabled=*/ true, getAmbientProvider(), this.getStore());
+    PersonalizationRouter.instance().goToRoute(Paths.Ambient);
   }
 
   private computePreviewAlbums_(): AmbientModeAlbum[]|null {
--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://personalization/strings.m.js';
 import 'chrome://webui-test/mojo_webui_test_support.js';
 
+import {AmbientPreviewTest} from './ambient_preview_element_test.js';
 import {AmbientSubpageTest} from './ambient_subpage_element_test.js';
 import {AvatarCameraTest} from './avatar_camera_element_test.js';
 import {AvatarListTest} from './avatar_list_element_test.js';
@@ -34,6 +35,7 @@ import {WallpaperSelectedTest} from './wallpaper_selected_element_test.js';
 window.console.warn = () => {};
 
 const testCases = [
+  AmbientPreviewTest,
   AmbientSubpageTest,
   AvatarCameraTest,
   AvatarListTest,
