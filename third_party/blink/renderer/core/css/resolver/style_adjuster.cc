987ac6b00a58c1b98153606e54e508046bc40a1e
Frédéric Wang <fwang@igalia.com>
2022-03-22 08:01:11 +0000

    Guard more MathML-specific code with the MathMLCoreEnabled flag
    After [1], MathMLElement instances are created even when MathMLCore is
    disabled. Particular attention was paid to the (previously incorrect)
    HasTagName(mathml_names::...) as well as to places where elements were
    cast to MathMLElement types. This CL performs the remaining work for
    callers of isMathMLElement(), checking possible behavior changes
    when MathMLCore is disabled for now. In details, these places are:
    
    1. Adjustment of display: math on MathML elements.
    2. Adjustment of display: contents on MathML elements.
    3. Application of custom style to MathML elements.
    4. Sanitization of MathML elements via the sanitizer API.
    
    For (1), there is already a guard in the CSS parser so there was no
    behavior change (cf isDisplayInside). An ASSERT is added to guarantee
    that.
    
    For (2), we don't resolve display: contents to none on MathML elements
    when MathMLCore is disabled, so they continue to behave the same as
    HTML elements.
    
    The code path for (3) performs dynamic casts to classes
    derived from MathMLElement, relying on generated implementations of
    AllowFrom(const Element&) that only check tag names. As a consequence
    this leads to incorrect casts when MathMLCore is disabled, since all
    MathML elements use the base MathMLElement class in that case.
    
    Finally for (4), we keep the changed implied by [1] to the FromAPI()
    function of sanitizer.cc i.e. the "math:" namespace designator is
    always appended for nodes in the MathML namespace, whether or not
    MathMLCore is enabled. Note that the Sanitizer API is not shipped
    yet so it's fine to do this web-exposed change without API owner
    approval.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3506644
    [2] https://source.chromium.org/search?q=mathml_element_type_helpers.h
    
    Bug: 6606, 1272556, 1307667
    Change-Id: Ib069944e55e102381192d90c13c676fc2cb0b51d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536908
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#983716}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/layout/list_marker.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text_combine.h"
+#include "third_party/blink/renderer/core/mathml/mathml_element.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/style/style_intrinsic_length.h"
@@ -784,6 +785,10 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
   }
 
   auto* svg_element = DynamicTo<SVGElement>(element);
+
+  bool is_mathml_element = RuntimeEnabledFeatures::MathMLCoreEnabled() &&
+                           IsA<MathMLElement>(element);
+
   if (style.Display() != EDisplay::kNone) {
     if (svg_element)
       AdjustStyleForSvgElement(*svg_element, style);
@@ -820,8 +825,9 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
 
     // math display values on non-MathML elements compute to flow display
     // values.
-    if ((!element || !element->IsMathMLElement()) &&
+    if ((!element || !is_mathml_element) &&
         style.IsDisplayMathBox(style.Display())) {
+      DCHECK(RuntimeEnabledFeatures::MathMLCoreEnabled());
       style.SetDisplay(style.Display() == EDisplay::kBlockMath
                            ? EDisplay::kBlock
                            : EDisplay::kInline);
@@ -944,7 +950,7 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
     }
     style.SetCssDominantBaseline(baseline);
 
-  } else if (element && element->IsMathMLElement()) {
+  } else if (is_mathml_element) {
     if (style.Display() == EDisplay::kContents) {
       // https://drafts.csswg.org/css-display/#unbox-mathml
       style.SetDisplay(EDisplay::kNone);
