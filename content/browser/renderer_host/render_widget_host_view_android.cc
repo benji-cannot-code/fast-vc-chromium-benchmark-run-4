6c14c0159cb2cb7136e562cc94eae97bb4db28a0
Bo Liu <boliu@chromium.org>
2022-03-22 18:41:29 +0000

    android: Null check in WakeUpGpu
    Add back the null check that was accidentally removed in r973834
    
    Bug: 1308776
    Change-Id: I9b47faf76aad5a89b7efe2c0bebd744be621ff2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3543706
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983938}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -166,7 +166,8 @@ void RecordToolTypeForActionDown(const ui::MotionEventAndroid& event) {
 }
 
 void WakeUpGpu(GpuProcessHost* host) {
-  host->gpu_service()->WakeUpGpu();
+  if (host)
+    host->gpu_service()->WakeUpGpu();
 }
 
 std::string CompressAndSaveBitmap(const std::string& dir,
