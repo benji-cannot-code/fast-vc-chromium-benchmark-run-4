d04239f04bd88226d5454ca839b4c21e2be17c1b
Bao-Duy Tran <tranbaoduy@chromium.org>
2022-03-22 14:21:43 +0000

    Remove unused platform_ field in {Decoder|System}Engine.
    __SIMPLE_REVIEW__
    __driveby_cleanup__
    
    Bug: None
    Change-Id: Iafc5e5dd32f89bd70dc3ea143a809a276032237b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536696
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983798}

--- ash/services/ime/decoder/decoder_engine.h
+++ ash/services/ime/decoder/decoder_engine.h
@@ -46,7 +46,6 @@ class DecoderEngine : public mojom::InputChannel {
                       ProcessMessageCallback callback) override;
 
  private:
-  ImeCrosPlatform* platform_ = nullptr;
   absl::optional<ImeDecoder::EntryPoints> decoder_entry_points_;
   mojo::ReceiverSet<mojom::InputChannel> decoder_channel_receivers_;
 };
