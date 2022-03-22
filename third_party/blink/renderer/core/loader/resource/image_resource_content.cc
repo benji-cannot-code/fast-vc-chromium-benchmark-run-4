236c4d1bd656f52c28b3b71713c09ea8de142061
Ian Clelland <iclelland@chromium.org>
2022-03-22 14:27:36 +0000

    Add MediaTiming class to track image/video load timing.
    This adds a new MediaTiming class, which is then extended by the
    existing ImageResourceContent, and which will be used by a new
    VideoTiming class in subsequent CLs. Code which previously required an
    ImageResourceContent is updated to use the more generic MediaTiming
    where applicable.
    
    This is in preparation for allowing LCP to take the first rendered
    video frame into account.
    
    Design: https://docs.google.com/document/d/1llkjQ4V7sWgfnavKW2mq67f3F--78JQImKBBqLI9j08/edit?usp=sharing
    
    Bug: 1289664
    Change-Id: I44dfe6916b2114134c78819cac7d5cb4da33fe34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417257
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983801}

--- third_party/blink/renderer/core/loader/resource/image_resource_content.cc
+++ third_party/blink/renderer/core/loader/resource/image_resource_content.cc
@@ -116,6 +116,7 @@ void ImageResourceContent::Trace(Visitor* visitor) const {
   visitor->Trace(observers_);
   visitor->Trace(finished_observers_);
   ImageObserver::Trace(visitor);
+  MediaTiming::Trace(visitor);
 }
 
 void ImageResourceContent::HandleObserverFinished(
@@ -151,7 +152,7 @@ void ImageResourceContent::AddObserver(ImageResourceObserver* observer) {
     observer->ImageChanged(this, CanDeferInvalidation::kNo);
   }
 
-  if (IsLoaded() && observers_.Contains(observer))
+  if (IsSufficientContentLoadedForPaint() && observers_.Contains(observer))
     HandleObserverFinished(observer);
 }
 
@@ -615,6 +616,10 @@ void ImageResourceContent::EmulateLoadStartedForInspector(
   info_->EmulateLoadStartedForInspector(fetcher, url, initiator_name);
 }
 
+bool ImageResourceContent::IsSufficientContentLoadedForPaint() const {
+  return IsLoaded();
+}
+
 bool ImageResourceContent::IsLoaded() const {
   return GetContentStatus() > ResourceStatus::kPending;
 }
@@ -636,9 +641,16 @@ ResourceStatus ImageResourceContent::GetContentStatus() const {
   return content_status_;
 }
 
-bool ImageResourceContent::IsAnimatedImageWithPaintedFirstFrame() const {
-  return (image_ && !image_->IsNull() && image_->MaybeAnimated() &&
-          image_->CurrentFrameIsComplete());
+bool ImageResourceContent::IsAnimatedImage() const {
+  return image_ && !image_->IsNull() && image_->MaybeAnimated();
+}
+
+bool ImageResourceContent::IsPaintedFirstFrame() const {
+  return IsAnimatedImage() && image_->CurrentFrameIsComplete();
+}
+
+bool ImageResourceContent::TimingAllowPassed() const {
+  return GetResponse().TimingAllowPassed();
 }
 
 // TODO(hiroshige): Consider removing the following methods, or stopping
