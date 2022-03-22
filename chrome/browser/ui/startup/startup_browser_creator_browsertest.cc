057768dc2e5c782b6c74887f39a38afd8e42a0cc
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-03-22 09:02:42 +0000

    Implements `AddFirstRunTabs` directly in `StartupBrowserCreator`
    Tiny refactor that removes the single-url
    `StartupBrowserCreator::AddFirstRunTab` in favour of a version that
    takes a vector, as this matches the common use cases better and is
    required in a follow-up CL.
    
    Bug: 1300109
    Change-Id: I820f8f0739b7f8e8e2aa87bcc732734ee2ddb139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540720
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983729}

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -3000,13 +3000,12 @@ void StartupBrowserCreatorFirstRunTest::SetUpInProcessBrowserTestFixture() {
   policy::BrowserPolicyConnector::SetPolicyProviderForTesting(&provider_);
 }
 
-IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest, AddFirstRunTab) {
+IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest, AddFirstRunTabs) {
   ASSERT_TRUE(embedded_test_server()->Start());
   StartupBrowserCreator browser_creator;
-  browser_creator.AddFirstRunTab(
-      embedded_test_server()->GetURL("/title1.html"));
-  browser_creator.AddFirstRunTab(
-      embedded_test_server()->GetURL("/title2.html"));
+  browser_creator.AddFirstRunTabs(
+      {embedded_test_server()->GetURL("/title1.html"),
+       embedded_test_server()->GetURL("/title2.html")});
 
   // Do a simple non-process-startup browser launch.
   base::CommandLine dummy(base::CommandLine::NO_PROGRAM);
@@ -3108,8 +3107,8 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserCreatorFirstRunTest,
   // }
   ASSERT_TRUE(embedded_test_server()->Start());
   StartupBrowserCreator browser_creator;
-  browser_creator.AddFirstRunTab(
-      embedded_test_server()->GetURL("/title1.html"));
+  browser_creator.AddFirstRunTabs(
+      {embedded_test_server()->GetURL("/title1.html")});
   browser()->profile()->GetPrefs()->SetInteger(
       prefs::kRestoreOnStartup, 1);
 
