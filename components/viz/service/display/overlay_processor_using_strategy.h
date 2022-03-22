baf33fbffc3ecdde094bf66b60019dbbde02ef93
Kevin Haslett <khaslett@chromium.org>
2022-03-22 23:40:51 +0000

    Keep track of attempted combinations of overlays, and if they worked.
    Create the OverlayCombinationCache to encapsulate all logic regarding
    choosing a combination of OverlayProposedCandidates to test. We will
    also use OverlayCombinationResult::previously_succeeded in a future CL
    to save some testing time on the DRM Thread.
    
    Change-Id: Ie0f2a4a0889e2c854c2baf2ed5d6b59350755d62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510654
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984084}

--- components/viz/service/display/overlay_processor_using_strategy.h
+++ components/viz/service/display/overlay_processor_using_strategy.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display/overlay_candidate.h"
 #include "components/viz/service/display/overlay_candidate_temporal_tracker.h"
+#include "components/viz/service/display/overlay_combination_cache.h"
 #include "components/viz/service/display/overlay_processor_interface.h"
 #include "components/viz/service/display/overlay_processor_strategy.h"
 #include "components/viz/service/display/overlay_proposed_candidate.h"
@@ -30,6 +31,7 @@ class DisplayResourceProvider;
 }
 
 namespace viz {
+
 // OverlayProcessor subclass that goes through a list of strategies to determine
 // overlay candidates. THis is used by Android and Ozone platforms.
 class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
@@ -88,6 +90,9 @@ class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
   void CheckOverlaySupport(
       const OverlayProcessorInterface::OutputSurfaceOverlayPlane* primary_plane,
       OverlayCandidateList* candidate_list);
+
+  // Clears the cache of attempted overlay combinations and their results.
+  void ClearOverlayCombinationCache();
 
   // This should be called during overlay processing to register whether or not
   // there is a candidate that requires an overlay so that the manager can allow
@@ -284,6 +289,8 @@ class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
   // used by UpdateDamageRect() to update damage properly.
   OverlayStatusMap prev_overlays_;
   OverlayStatusMap curr_overlays_;
+
+  OverlayCombinationCache overlay_combination_cache_;
 };
 
 }  // namespace viz
