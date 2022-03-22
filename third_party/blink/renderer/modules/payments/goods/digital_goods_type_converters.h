6964b691a4e6a7f4b9a4ee9732ac4020547118c9
Glen Robertson <glenrob@chromium.org>
2022-03-22 02:13:20 +0000

    DGAPI: Blink changes for DGAPI v2.1
    - Behind DigitalGoodsV2_1 runtime feature flag
    - add ListPurchaseHistory method
    - add to ItemDetails:
      - ItemType type
      - sequence<DOMString> iconUrls
      - unsigned short introductoryPriceCycles
    - Corresponding unit tests and WPT tests
    - IWYU in digital_goods_service & digital_goods_type_converters
    
    I2P for blink changes:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/JaNDPCUr1ws/m/JFt-E7ePAQAJ
    
    Bug: 1250604
    Change-Id: Ia621ec6fc004ca251135fb053715db13253bd42e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288243
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983617}

--- third_party/blink/renderer/modules/payments/goods/digital_goods_type_converters.h
+++ third_party/blink/renderer/modules/payments/goods/digital_goods_type_converters.h
@@ -6,13 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PAYMENTS_GOODS_DIGITAL_GOODS_TYPE_CONVERTERS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PAYMENTS_GOODS_DIGITAL_GOODS_TYPE_CONVERTERS_H_
 
-#include "mojo/public/cpp/bindings/type_converter.h"
 #include "third_party/blink/public/mojom/digital_goods/digital_goods.mojom-blink-forward.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_item_details.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_purchase_details.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace mojo {
+
+template <typename T, typename U>
+struct TypeConverter;
 
 template <>
 struct MODULES_EXPORT
