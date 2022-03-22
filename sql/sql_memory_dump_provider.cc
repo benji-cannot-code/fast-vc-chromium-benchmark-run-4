8836ed2c5352fc97a1a2b4f97cf86a0a801d6310
Victor Costan <pwnall@chromium.org>
2022-03-22 05:33:33 +0000

    sql: Integrate memory dump providers with SqliteResultCode.
    This CL also simplifies SqlMemoryDumpProvider::OnMemoryDump() to reflect
    the fact that sqlite3_status64() does not fail when given a valid code.
    
    Bug: 1308290
    Change-Id: I28fab96ad3b7e17e865d22a9991707387d27a462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541600
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983683}

--- sql/sql_memory_dump_provider.cc
+++ sql/sql_memory_dump_provider.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
+#include "sql/sqlite_result_code.h"
+#include "sql/sqlite_result_code_values.h"
 #include "third_party/sqlite/sqlite3.h"
 
 namespace sql {
@@ -27,10 +29,11 @@ bool SqlMemoryDumpProvider::OnMemoryDump(
     base::trace_event::ProcessMemoryDump* pmd) {
   sqlite3_int64 memory_used = 0;
   sqlite3_int64 memory_high_water = 0;
-  int status = sqlite3_status64(SQLITE_STATUS_MEMORY_USED, &memory_used,
-                                &memory_high_water, /* resetFlag= */ 1);
-  if (status != SQLITE_OK)
-    return false;
+  auto sqlite_result_code = ToSqliteResultCode(sqlite3_status64(
+      SQLITE_STATUS_MEMORY_USED, &memory_used, &memory_high_water,
+      /*resetFlag=*/1));
+  DCHECK_EQ(sqlite_result_code, SqliteResultCode::kOk)
+      << "sqlite3_status64(SQLITE_STATUS_MEMORY_USED) failed";
 
   base::trace_event::MemoryAllocatorDump* dump =
       pmd->CreateAllocatorDump("sqlite");
@@ -43,13 +46,14 @@ bool SqlMemoryDumpProvider::OnMemoryDump(
 
   sqlite3_int64 dummy_high_water = -1;
   sqlite3_int64 malloc_count = -1;
-  status = sqlite3_status64(SQLITE_STATUS_MALLOC_COUNT, &malloc_count,
-                            &dummy_high_water, /* resetFlag= */ 0);
-  if (status == SQLITE_OK) {
-    dump->AddScalar("malloc_count",
-                    base::trace_event::MemoryAllocatorDump::kUnitsObjects,
-                    malloc_count);
-  }
+  sqlite_result_code = ToSqliteResultCode(sqlite3_status64(
+      SQLITE_STATUS_MALLOC_COUNT, &malloc_count, &dummy_high_water,
+      /*resetFlag=*/0));
+  DCHECK_EQ(sqlite_result_code, SqliteResultCode::kOk)
+      << "sqlite3_status64(SQLITE_STATUS_MALLOC_COUNT) failed";
+  dump->AddScalar("malloc_count",
+                  base::trace_event::MemoryAllocatorDump::kUnitsObjects,
+                  malloc_count);
 
   const char* system_allocator_name =
       base::trace_event::MemoryDumpManager::GetInstance()
