bc8d6ee9b248429bd4ff182ff2649628e739e77b
David Sanders <dsanders11@ucsbalum.com>
2022-03-22 07:30:11 +0000

    Remove unused include of //base/threading/thread.h
    Also adds some includes to other files which were getting
    them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I13a35234e3f7180c76c01d5a4c7e5ac8562756a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3535380
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#983708}

--- content/browser/utility_process_host.cc
+++ content/browser/utility_process_host.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/base_i18n_switches.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/threading/thread.h"
 #include "build/build_config.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "content/browser/browser_child_process_host_impl.h"
--- storage/browser/file_system/copy_or_move_operation_delegate_unittest.cc
+++ storage/browser/file_system/copy_or_move_operation_delegate_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/task_environment.h"
+#include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/services/filesystem/public/mojom/types.mojom.h"
 #include "storage/browser/file_system/copy_or_move_file_validator.h"
