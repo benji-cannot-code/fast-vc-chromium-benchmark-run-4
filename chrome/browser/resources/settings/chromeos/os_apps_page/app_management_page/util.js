9e7953e00ab9b4f048424e2d5aebaba50a8de5cd
Wes Okuhara <wesokuhara@google.com>
2022-03-22 16:19:28 +0000

    [CrOS Settings] Remove .m extension from os_route.m.js
    All Chrome OS Settings pages are using JS imports and not HTML
    imports for os_route.m.js. The '.m' part should only be used for
    auto-generated JS modules, to differentiate from the pre-JS module
    file. Once a JS module is checked in the repository and no longer
    auto-generated the '.m' part should be removed.
    
    Similar to https://chromium-review.googlesource.com/c/chromium/src/+/3489749
    
    Bug: 1180979
    Change-Id: I21d744a703f2bcf4497fd8b5161b2ec282b306ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541421
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#983871}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/util.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/util.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {Route, Router} from '../../../router.js';
-import {routes} from '../../os_route.m.js';
+import {routes} from '../../os_route.js';
 
 /**
  * Navigates to the App Detail page.
