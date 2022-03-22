06d243423d4b1cef94e9625b1523d451f1faf0e8
David Sanders <dsanders11@ucsbalum.com>
2022-03-22 08:34:48 +0000

    Remove unused include of page_scheduler.h
    Bug: 242216
    Change-Id: I07237b30eda4e958ac5c6d553d25f90b8442da02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541642
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#983722}

--- third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h
+++ third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/input/web_input_event_attribution.h"
 #include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
-#include "third_party/blink/renderer/platform/scheduler/public/page_scheduler.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
