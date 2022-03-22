f2c18e5eaecdac52ce4c6eab9c205dc6bd64c12b
Nohemi Fernandez <fernandex@chromium.org>
2022-03-22 11:08:36 +0000

    Support capability 'canStopParentalSupervision' in account APIs.
    Adds support for a capability that distinguishes whether or not an
    account is able to stop parental supervision.
    
    Bug: 1295720
    Change-Id: I86f6e026c79cd7206094706f4b809927a4f07220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528481
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983751}

--- components/signin/public/android/junit/src/org/chromium/components/signin/base/AccountCapabilitiesTest.java
+++ components/signin/public/android/junit/src/org/chromium/components/signin/base/AccountCapabilitiesTest.java
@@ -51,6 +51,8 @@ public final class AccountCapabilitiesTest {
                 return capabilities.canRunChromePrivacySandboxTrials();
             case AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME:
                 return capabilities.isSubjectToParentalControls();
+            case AccountCapabilitiesConstants.CAN_STOP_PARENTAL_SUPERVISION_CAPABILITY_NAME:
+                return capabilities.canStopParentalSupervision();
         }
         assert false : "Capability name is not known.";
         return -1;
@@ -80,6 +82,10 @@ public final class AccountCapabilitiesTest {
                         .name("IsSubjectToParentalControls")
                         .value(AccountCapabilitiesConstants
                                         .IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME),
+                new ParameterSet()
+                        .name("CanStopParentalSupervision")
+                        .value(AccountCapabilitiesConstants
+                                        .CAN_STOP_PARENTAL_SUPERVISION_CAPABILITY_NAME),
                 new ParameterSet()
                         .name("CanOfferExtendedChromeSyncPromos")
                         .value(AccountCapabilitiesConstants
