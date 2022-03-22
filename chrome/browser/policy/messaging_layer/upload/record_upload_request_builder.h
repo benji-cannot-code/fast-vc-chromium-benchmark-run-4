cad14162fe7cca490f8ac430e4a86bd5c96ae19f
Hong Xu <xuhong@chromium.org>
2022-03-22 16:15:24 +0000

    DCHECK the type of requestId in Build
    Ensure the type of "requestId" is appropriate in
    `UploadEncryptedReportingRequestBuilder::Build`.
    
    Bug: b:225412986
    Change-Id: I4256a5644cbcf561b0c9b2cb9f1a9e50f7fcebca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540766
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983866}

--- chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h
+++ chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h
@@ -82,6 +82,8 @@ class UploadEncryptedReportingRequestBuilder {
   UploadEncryptedReportingRequestBuilder& SetRequestId(
       base::StringPiece request_id);
 
+  // Return the built dictionary. Also set requestId to a random string if it
+  // hasn't been set yet.
   absl::optional<base::Value::Dict> Build();
 
   static base::StringPiece GetEncryptedRecordListPath();
