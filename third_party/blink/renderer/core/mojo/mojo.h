74d29f68b217437bd48254e6dc2e11599795f8c3
Jiewei Qian <qjw@chromium.org>
2022-03-22 01:45:52 +0000

    blink/mojo: make Mojo a supplementable and CORE_EXPORT it
    This CL changes Mojo interface (exposed when MojoJS is enabled) to a
    supplementable, so blink/modules can extend it with partial interface.
    
    Subsequent CLs will add partial interface methods to make Mojo work with
    blink/module components.
    
    Bug: 1288174
    Change-Id: I1062d54bb7d7371392e94e12254feb620ab93725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3524541
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983602}

--- third_party/blink/renderer/core/mojo/mojo.h
+++ third_party/blink/renderer/core/mojo/mojo.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_MOJO_MOJO_H_
 
 #include "mojo/public/cpp/system/core.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -20,10 +22,15 @@ class MojoCreateSharedBufferResult;
 class MojoHandle;
 class ScriptState;
 
-class Mojo final : public ScriptWrappable {
+class CORE_EXPORT Mojo final : public ScriptWrappable,
+                               public Supplementable<Mojo> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  Mojo() = default;
+  Mojo(const Mojo&) = delete;
+  Mojo& operator=(const Mojo&) = delete;
+
   // MojoResult
   static const MojoResult kResultOk = MOJO_RESULT_OK;
   static const MojoResult kResultCancelled = MOJO_RESULT_CANCELLED;
@@ -58,6 +65,11 @@ class Mojo final : public ScriptWrappable {
                             MojoHandle*,
                             const String& scope,
                             ExceptionState& exception_state);
+
+  void Trace(Visitor* visitor) const override {
+    ScriptWrappable::Trace(visitor);
+    Supplementable<Mojo>::Trace(visitor);
+  }
 };
 
 }  // namespace blink
