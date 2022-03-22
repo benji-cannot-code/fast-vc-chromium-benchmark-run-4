44fa6ec52910135eebd40acd7e1a8d56b76731c4
Nan Lin <linnan@chromium.org>
2022-03-22 21:39:04 +0000

    [attribution_reporting] Show aggregatable source in internals UI
    Screenshot can be found at crbug.com/1308824#c1.
    
    Bug: 1308824
    Change-Id: I6374c91f5475b029b04af6b7d52fc1b2ad9c59ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537314
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984038}

--- content/browser/resources/attribution_reporting/attribution_internals.js
+++ content/browser/resources/attribution_reporting/attribution_internals.js
@@ -27,6 +27,15 @@ function compareDefault(a, b) {
 }
 
 /**
+ * @param {string} key
+ * @param {*} value
+ * @return {*}
+ */
+function bigint_replacer(key, value) {
+  return typeof value === 'bigint' ? value.toString() : value;
+}
+
+/**
  * @template T
  * @abstract
  */
@@ -428,6 +437,7 @@ class Source {
     this.sourceType = SourceTypeToText(mojo.sourceType);
     this.priority = mojo.priority;
     this.filterData = JSON.stringify(mojo.filterData, null, ' ');
+    this.aggregatableSource = JSON.stringify(mojo.aggregatableSource, bigint_replacer, ' ');
     this.debugKey = mojo.debugKey ? mojo.debugKey.value : '';
     this.dedupKeys = mojo.dedupKeys.join(', ');
     this.status = AttributabilityToText(mojo.attributability);
@@ -449,6 +459,7 @@ class SourceTableModel extends TableModel {
       new ValueColumn('Source Type', (e) => e.sourceType),
       new ValueColumn('Priority', (e) => e.priority),
       new CodeColumn('Filter Data', (e) => e.filterData),
+      new CodeColumn('Aggregatable Source', (e) => e.aggregatableSource),
       new ValueColumn('Debug Key', (e) => e.debugKey),
       new ValueColumn('Dedup Keys', (e) => e.dedupKeys, /*compare=*/ null),
       new ValueColumn('Status', (e) => e.status),
@@ -590,9 +601,7 @@ class AggregatableAttributionReport extends Report {
     super(mojo, mojo.data.aggregatableAttributionData.id);
 
     this.contributions = JSON.stringify(
-        mojo.data.aggregatableAttributionData.contributions,
-        (key, value) =>
-            typeof value === 'bigint' ? value.toString() : value, ' ');
+        mojo.data.aggregatableAttributionData.contributions, bigint_replacer, ' ');
   }
 }
 
