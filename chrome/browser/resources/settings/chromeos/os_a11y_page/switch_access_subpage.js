ae950263659c9e671e39def78521a1a77dbc26ab
dpapad <dpapad@chromium.org>
2022-03-23 07:14:33 +0000

    WebUI: Migrate cr-slider shared element to TypeScript.
    Adding minimal Closure externs file to satisfy a few CrOS cases that
    still refer to cr-slider from JS code.
    
    Bug: 1189595
    Change-Id: I31cb1d19fc33fe02d97e6635bdcf5d15b3c85fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536427
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984209}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_subpage.js
@@ -16,7 +16,6 @@ import './switch_access_action_assignment_dialog.js';
 import './switch_access_setup_guide_dialog.js';
 import './switch_access_setup_guide_warning_dialog.js';
 
-import {SliderTick} from '//resources/cr_elements/cr_slider/cr_slider.js';
 import {I18nBehavior} from '//resources/js/i18n_behavior.m.js';
 import {loadTimeData} from '//resources/js/load_time_data.m.js';
 import {WebUIListenerBehavior} from '//resources/js/web_ui_listener_behavior.m.js';
