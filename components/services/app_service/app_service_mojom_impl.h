fbc152eb26cadd81c6280fa1d60ec1d592fc72ad
Tim Sergeant <tsergeant@chromium.org>
2022-03-23 23:49:28 +0000

    App Service: Queue preferred apps changes that are requested before init
    During App Service startup, we read the preferred apps .json file from
    disk asynchronously, and use this to initialize the PreferredAppsList.
    Any changes to preferred apps which are made before this initialization
    completes are currently dropped.
    
    Generally, this is not an issue for real-world use, as it's not feasible
    to interact with browser UI before this disk read is done. However, some
    tests try to make modifications before a disk read can complete.
    
    This CL changes the implementation to queue up any changes which are
    made before initialization. These will all be executed immediately after
    the PreferredAppsList has finished initializing.
    
    Bug: 1293173
    Change-Id: I6752098a1ee95e1b2758de367005fd5542802b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542830
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984596}

--- components/services/app_service/app_service_mojom_impl.h
+++ components/services/app_service/app_service_mojom_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/callback.h"
+#include "base/containers/queue.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -148,6 +149,28 @@ class AppServiceMojomImpl : public apps::mojom::AppService {
 
   void ReadCompleted(std::string preferred_apps_string);
 
+  // Runs |task| after the PreferredAppsList is fully initialized. |task| will
+  // be run immediately if preferred apps are already initialized.
+  void RunAfterPreferredAppsReady(base::OnceClosure task);
+
+  void AddPreferredAppImpl(apps::mojom::AppType app_type,
+                           const std::string& app_id,
+                           apps::mojom::IntentFilterPtr intent_filter,
+                           apps::mojom::IntentPtr intent,
+                           bool from_publisher);
+  void RemovePreferredAppImpl(apps::mojom::AppType app_type,
+                              const std::string& app_id);
+  void RemovePreferredAppForFilterImpl(
+      apps::mojom::AppType app_type,
+      const std::string& app_id,
+      apps::mojom::IntentFilterPtr intent_filter);
+  void SetSupportedLinksPreferenceImpl(
+      apps::mojom::AppType app_type,
+      const std::string& app_id,
+      std::vector<apps::mojom::IntentFilterPtr> all_link_filters);
+  void RemoveSupportedLinksPreferenceImpl(apps::mojom::AppType app_type,
+                                          const std::string& app_id);
+
   // publishers_ is a std::map, not a mojo::RemoteSet, since we want to
   // be able to find *the* publisher for a given apps::mojom::AppType.
   std::map<apps::mojom::AppType, mojo::Remote<apps::mojom::Publisher>>
@@ -176,6 +199,8 @@ class AppServiceMojomImpl : public apps::mojom::AppService {
   base::OnceClosure read_completed_for_testing_;
 
   base::OnceClosure write_completed_for_testing_;
+
+  base::queue<base::OnceClosure> pending_preferred_apps_tasks_;
 
   base::WeakPtrFactory<AppServiceMojomImpl> weak_ptr_factory_{this};
 };
