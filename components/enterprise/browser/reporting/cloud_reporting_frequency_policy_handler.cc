1c5163fe4107d4c9e9bb8e185cbdad58fd70992f
Owen Min <zmin@chromium.org>
2022-03-23 22:05:00 +0000

    Reduce maximum value of CloudReportingUploadFrequency
    Reduce the number from 168 hours (7 days) to 24 hours (1 day)
    
    Change-Id: I9e0095b0c6a7254f8f4afc7ebb1f5de0f4585822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518047
    Auto-Submit: Owen Min <zmin@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984548}

--- components/enterprise/browser/reporting/cloud_reporting_frequency_policy_handler.cc
+++ components/enterprise/browser/reporting/cloud_reporting_frequency_policy_handler.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_reporting {
 namespace {
 constexpr int kMinimumReportFrequencyInHour = 3;
-constexpr int kMaximumReportFrequencyInour = 24 * 7;
+constexpr int kMaximumReportFrequencyInour = 24;
 
 }  // namespace
 
