f080f3f4a6934118cd2846f6f6f23530ed6d92b3
Cici Ruan <cuicuiruan@google.com>
2022-03-23 17:56:30 +0000

    arc/input_overlay: Add ErrorView to show error message
    When the same key is pressed for customizing the key mapping, it will
    popup an error message. When clicking outside of the error view, the
    error message will disappear.
    
    Bug: b:188452744
    Test: manual test and unittest
    Change-Id: If4bb83526df7a96322ab53523e0c266ded4704b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3534461
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#984428}

--- chrome/browser/ash/arc/input_overlay/ui/action_tag.h
+++ chrome/browser/ash/arc/input_overlay/ui/action_tag.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_ACTION_TAG_H_
 #define CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_ACTION_TAG_H_
 
+#include "base/strings/string_piece_forward.h"
 #include "chrome/browser/ash/arc/input_overlay/constants.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/action_label.h"
 #include "ui/views/view.h"
@@ -18,6 +19,8 @@ namespace input_overlay {
 class ActionTag : public views::View {
  public:
   ActionTag();
+  ActionTag(const ActionTag&) = delete;
+  ActionTag& operator=(const ActionTag&) = delete;
   ~ActionTag() override;
 
   static std::unique_ptr<ActionTag> CreateTextActionTag(std::string text);
@@ -25,7 +28,7 @@ class ActionTag : public views::View {
       std::string mouse_action);
 
   void SetDisplayMode(DisplayMode mode);
-  void OnActionLabelFocused();
+  void ShowErrorMsg(base::StringPiece error_msg);
 
   // views::View:
   gfx::Size CalculatePreferredSize() const override;
