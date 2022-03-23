cb1b64a5056f3e73f305e3ab5fb1a5c6861b396f
Rakina Zata Amni <rakina@chromium.org>
2022-03-23 12:57:35 +0000

    Get unload timing info from old LocalFrame's document on LocalFrame swap
    Currently unload timing info passing assumes that the old document uses
    the same FrameLoader as the new committing document. This might not be
    the case if the commit uses LocalFrame swap.
    
    This CL makes it so that we correctly use the unload timing from the
    old LocalFrame's document, by making the old LocalFrame save its unload
    timing in the new document's FrameLoader during swap & detach.
    
    Bug: 1153288
    Change-Id: I39b93e909c946af0cc1396e7999b10dd6084985c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291108
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984290}

--- third_party/blink/renderer/core/frame/local_frame_mojo_handler.cc
+++ third_party/blink/renderer/core/frame/local_frame_mojo_handler.cc
@@ -1187,7 +1187,8 @@ void LocalFrameMojoHandler::ClosePage(
   // when unloading itself.
   IgnoreOpensDuringUnloadCountIncrementer ignore_opens_during_unload(
       frame_->GetDocument());
-  frame_->Loader().DispatchUnloadEvent(/*unload_timing_info=*/nullptr);
+  frame_->Loader().DispatchUnloadEvent(
+      false /* need_unload_info_for_new_document */);
 
   std::move(completion_callback).Run();
 }
