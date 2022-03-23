c80d774e31cd0c37bd497e74e820cb9912d0c107
Allen Bauer <kylixrd@chromium.org>
2022-03-23 18:30:14 +0000

    Define and use debug rect colors in the color pipeline instead of hard-coding them.
    Bug: 1003612
    Change-Id: I7b0a929be7e954415c5847ff42eca9af38f7f7be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3543261
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984444}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -59,6 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorButtonForegroundDisabled) \
   E_CPONLY(kColorButtonForegroundProminent) \
   E_CPONLY(kColorButtonForegroundUnchecked) \
+  E_CPONLY(kColorDebugBoundsOutline) \
+  E_CPONLY(kColorDebugContentOutline) \
   E_CPONLY(kColorDialogBackground) \
   E_CPONLY(kColorDialogForeground) \
   E_CPONLY(kColorDropdownBackground) \
