b6013463d5d2e0ea1288e6525306a2cbec395f71
Lingqi Chi <lingqi@chromium.org>
2022-03-23 10:11:02 +0000

    Prerender: Add timer logic for DSE Prerender
    To ensure prerendered results are fresh, this CL deletes the unused
    prerendered search results after a fixed duration(following search
    prefetch setting).
    
    It also adds a metric tracking the duration between:
    1. chrome receives the first correct prerender hints
    2. user navigates to the search result page.
    
    Besides that, this CL is the first step to make prerender reuse the
    prefetched response. See ryansturm@'s comments on the second patchset in
    this CL https://crrev.com/c/3459941. So the newly added metric would
    also be used to find the best threshold.
    
    This change should not affect the DSE's PrerenderPredictionStatus.
    The newly added SearchPrerenderTask plays the role that
    search_prerender_handle_ played before this CL. It is responsible for
    tracking a started search prerender, and it keeps alive even if the
    handle_ is destroyed by the timer. With its help, PrerenderManager can
    record the prediction regardless whether a prerender is expired or not.
    
    
    Bug: 1295170, 1278634
    Change-Id: Ie52c245b752d64e1139627579dcd62700aedebed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495266
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984254}

--- chrome/browser/prerender/prerender_manager_unittest.cc
+++ chrome/browser/prerender/prerender_manager_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/prerender/prerender_utils.h"
@@ -120,10 +122,9 @@ TEST_F(PrerenderManagerTest, StartNewSuggestionPrerender) {
   prerender_manager()->StartPrerenderSearchSuggestion(match);
   host_observer.WaitForDestroyed();
   registry_observer.WaitForTrigger(prerendering_url2);
-  EXPECT_TRUE(prerender_manager()->search_prerender_handle());
-  EXPECT_EQ(prerendering_url2, prerender_manager()
-                                   ->search_prerender_handle()
-                                   ->GetInitialPrerenderingUrl());
+  EXPECT_TRUE(prerender_manager()->HasSearchResultPagePrerendered());
+  EXPECT_EQ(std::u16string(u"prerender"),
+            prerender_manager()->GetPrerenderSearchTermForTesting());
 }
 
 // Tests that the old prerender is not destroyed when starting prerendering the
@@ -141,7 +142,7 @@ TEST_F(PrerenderManagerTest, StartSameSuggestionPrerender) {
   EXPECT_NE(prerender_host_id, content::RenderFrameHost::kNoFrameTreeNodeId);
   match = CreateSearchSuggestionMatch("/title1.html", "prer", "prerender");
   prerender_manager()->StartPrerenderSearchSuggestion(match);
-  EXPECT_TRUE(prerender_manager()->search_prerender_handle());
+  EXPECT_TRUE(prerender_manager()->HasSearchResultPagePrerendered());
 
   // The created prerender for `prerendering_url` still exists, so the
   // prerender_host_id should be the same.
@@ -166,7 +167,7 @@ TEST_F(PrerenderManagerTest, DestroyedOnNavigateAway) {
                                                      prerender_host_id);
   web_contents_tester()->NavigateAndCommit(GetUrl("/empty.html"));
   host_observer.WaitForDestroyed();
-  EXPECT_FALSE(prerender_manager()->search_prerender_handle());
+  EXPECT_FALSE(prerender_manager()->HasSearchResultPagePrerendered());
 }
 
 }  // namespace
