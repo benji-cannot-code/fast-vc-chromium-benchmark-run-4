38589708dc88b48a4d489cbece975984cff7c118
Joel Hockey <joelhockey@chromium.org>
2022-03-23 00:00:39 +0000

    Remove Terminal System App Menu
    Menu replaced by pinned home tab.
    
    Bug: b/223076712
    Change-Id: Id67f60f72c96f35d0ee830830c010a0bc7d2eeee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536691
    Reviewed-by: Jason Lin <lxj@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984101}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -82,11 +82,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_WEB_APP_MENU_APP_INFO    34063
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-// Terminal system app commands
-#define IDC_TERMINAL_LINUX   34070
-#define IDC_TERMINAL_SSH     34071
-#define IDC_TERMINAL_HOME    34072
-
 // Move window to other user commands
 #define IDC_VISIT_DESKTOP_OF_LRU_USER_2 34080
 #define IDC_VISIT_DESKTOP_OF_LRU_USER_3 34081
--- chrome/browser/ash/web_applications/terminal_system_web_app_info.cc
+++ chrome/browser/ash/web_applications/terminal_system_web_app_info.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/constants/ash_features.h"
-#include "base/feature_list.h"
 #include "base/strings/strcat.h"
 #include "chrome/browser/ash/crostini/crostini_terminal.h"
 #include "chrome/browser/ash/web_applications/system_web_app_install_utils.h"
@@ -81,10 +79,6 @@ bool TerminalSystemAppDelegate::ShouldShowNewWindowMenuOption() const {
 
 bool TerminalSystemAppDelegate::ShouldHaveTabStrip() const {
   return true;
-}
-
-bool TerminalSystemAppDelegate::HasTitlebarTerminalSelectNewTabButton() const {
-  return base::FeatureList::IsEnabled(chromeos::features::kTerminalSSH);
 }
 
 gfx::Rect TerminalSystemAppDelegate::GetDefaultBounds(Browser* browser) const {
