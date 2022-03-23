265773eeb46d09fbfc9cb5bf17eed835236e55a9
Yann Dago <ydago@chromium.org>
2022-03-23 18:47:58 +0000

    Make EnterpriseMDMManagementStatusProvider use ManagementService cache
    Use the cache system so that all calls to
    EnterpriseMDMManagementStatusProvider::FetchAuthority are made on the
    background thread.
    
    Bug: 1308857
    Change-Id: I0959c9fa2af0fac70df4534d2ea7fa726b72d79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546456
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984454}

--- components/policy/core/common/management/platform_management_status_provider_win.cc
+++ components/policy/core/common/management/platform_management_status_provider_win.cc
@@ -22,8 +22,8 @@ bool DomainEnrollmentStatusProvider::IsEnrolledToDomain() {
   return base::win::IsEnrolledToDomain();
 }
 
-EnterpriseMDMManagementStatusProvider::EnterpriseMDMManagementStatusProvider() =
-    default;
+EnterpriseMDMManagementStatusProvider::EnterpriseMDMManagementStatusProvider()
+    : ManagementStatusProvider(policy_prefs::kEnterpriseMDMManagementWindows) {}
 
 EnterpriseManagementAuthority
 EnterpriseMDMManagementStatusProvider::FetchAuthority() {
