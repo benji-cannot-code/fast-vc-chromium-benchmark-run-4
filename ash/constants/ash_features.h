4a60483a0e5fef832c7ffdd3e1c7d02a84aeab32
Ben Becker <benbecker@chromium.org>
2022-03-23 18:02:00 +0000

    CloseAll: Create flag and add basic UI components
    Thic CL creates a flag to add an option to close a desk along with all
    of its windows, and adds the basic UI components behind the flag.
    
    Bug: 1308408
    Change-Id: I87815089de614b20482892d0e5f19a8ec59392e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540701
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984429}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -129,6 +129,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kClipboardHistoryNudgeSessionReset;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kClipboardHistoryScreenshotNudge;
+COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kDesksCloseAll;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kContextualNudges;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kCroshSWA;
 COMPONENT_EXPORT(ASH_CONSTANTS)
@@ -594,6 +595,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsClipboardHistoryEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsClipboardHistoryNudgeSessionResetEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsClipboardHistoryScreenshotNudgeEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsDesksCloseAllEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCryptohomeRecoveryFlowEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsDarkLightModeEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsDeepLinkingEnabled();
--- ash/wm/desks/desk_preview_view.cc
+++ ash/wm/desks/desk_preview_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wallpaper/wallpaper_base_view.h"
 #include "ash/wm/desks/desk_mini_view.h"
+#include "ash/wm/desks/desk_name_view.h"
 #include "ash/wm/desks/desks_bar_view.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_util.h"
@@ -391,7 +392,15 @@ void DeskPreviewView::Layout() {
 }
 
 bool DeskPreviewView::OnMousePressed(const ui::MouseEvent& event) {
-  mini_view_->owner_bar()->HandlePressEvent(mini_view_, event);
+  // If we have a right click and the `kDesksCloseAll` feature is enabled, we
+  // should open the context menu.
+  if (features::IsDesksCloseAllEnabled() && event.IsRightMouseButton()) {
+    DeskNameView::CommitChanges(GetWidget());
+    mini_view_->OpenContextMenu();
+  } else {
+    mini_view_->owner_bar()->HandlePressEvent(mini_view_, event);
+  }
+
   return Button::OnMousePressed(event);
 }
 
@@ -411,6 +420,8 @@ void DeskPreviewView::OnGestureEvent(ui::GestureEvent* event) {
   switch (event->type()) {
     // Only long press can trigger drag & drop.
     case ui::ET_GESTURE_LONG_PRESS:
+      // TODO(crbug.com/1308780): Need to figure out how we can still maintain
+      // drag functionality while allowing long press to open the context menu.
       owner_bar->HandleLongPressEvent(mini_view_, *event);
       event->SetHandled();
       break;
