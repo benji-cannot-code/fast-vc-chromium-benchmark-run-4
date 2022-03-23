2befb05775a2169a6ef5e7970e27a57f74a2a475
Ian Barkley-Yeung <iby@chromium.org>
2022-03-23 20:25:52 +0000

    Allow preprocess_if_expr to add comments
    Add a command line argument '--enable_removal_comments' which instructs
    preprocess_if_expr to add 'grit-removed-lines:<numberOfLinesRemoved>'
    comments whenever it removes lines.
    
    BUG=b:186885186,chromium:1303956
    
    Change-Id: I9df99480b06c55b2436ded4f03f77ddce3bd87b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504445
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984505}

---
+++ tools/grit/preprocess_tests/test_with_ifexpr.html.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export function getTemplate() {
+  return `<!--_html_template_start_--><html>
+  <head>
+  <meta charset="utf-8">
+  </head>
+  <body>
+  Always present text
+  <if expr="foo">
+  Text for foo only
+  </if>
+  <if expr="bar">
+  Text for bar only
+  </if>
+  More text for everyone
+  </body>
+  </html><!--_html_template_end_-->`;
+}
---
+++ tools/grit/preprocess_tests/test_with_ifexpr_expected.html.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function getTemplate() {
+  return `<!--_html_template_start_--><html>
+  <head>
+  <meta charset="utf-8">
+  </head>
+  <body>
+  Always present text
+  
+  Text for foo only
+  
+  <!--grit-removed-lines:2-->
+  More text for everyone
+  </body>
+  </html><!--_html_template_end_-->`;
+}
