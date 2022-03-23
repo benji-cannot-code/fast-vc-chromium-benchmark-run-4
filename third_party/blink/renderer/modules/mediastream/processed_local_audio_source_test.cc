60fa56ec3a6147c9f84e57f3bd72544fec286209
Olga Sharonova <olka@chromium.org>
2022-03-23 15:11:34 +0000

    AudioServiceAudioProcessorProxy to allow audio processing in the audio service.
    GetStats() polling is done once per second to preserve
    the existing behavior.
    
    Bug: 1215049
    Change-Id: I3c747fee8d0d575a02adb0ea1e56e8ccbc4aff5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431020
    Reviewed-by: Sam Zackrisson <saza@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984339}

--- third_party/blink/renderer/modules/mediastream/processed_local_audio_source_test.cc
+++ third_party/blink/renderer/modules/mediastream/processed_local_audio_source_test.cc
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "media/base/audio_bus.h"
 #include "media/base/audio_parameters.h"
+#include "media/base/media_switches.h"
+#include "media/media_buildflags.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/modules/mediastream/web_media_stream_audio_sink.h"
@@ -78,7 +81,8 @@ class FormatCheckingMockAudioSink : public WebMediaStreamAudioSink {
 
 }  // namespace
 
-class ProcessedLocalAudioSourceTest : public SimTest {
+class ProcessedLocalAudioSourceTest : public SimTest,
+                                      public testing::WithParamInterface<bool> {
  protected:
   ProcessedLocalAudioSourceTest() = default;
 
@@ -126,7 +130,10 @@ class ProcessedLocalAudioSourceTest : public SimTest {
   void CheckOutputFormatMatches(const media::AudioParameters& params) {
     EXPECT_EQ(kSampleRate, params.sample_rate());
     EXPECT_EQ(kChannelLayout, params.channel_layout());
-    EXPECT_EQ(kExpectedOutputBufferSize, params.frames_per_buffer());
+    if (media::IsChromeWideEchoCancellationEnabled())
+      EXPECT_EQ(kExpectedSourceBufferSize, params.frames_per_buffer());
+    else
+      EXPECT_EQ(kExpectedOutputBufferSize, params.frames_per_buffer());
   }
 
   media::AudioCapturerSource::CaptureCallback* capture_source_callback() const {
@@ -155,7 +162,18 @@ class ProcessedLocalAudioSourceTest : public SimTest {
 // shut-down. The unit tests in media_stream_audio_unittest.cc provide more
 // comprehensive testing of the object graph connections and multi-threading
 // concerns.
-TEST_F(ProcessedLocalAudioSourceTest, VerifyAudioFlowWithoutAudioProcessing) {
+TEST_P(ProcessedLocalAudioSourceTest, VerifyAudioFlowWithoutAudioProcessing) {
+  base::test::ScopedFeatureList scoped_feature_list;
+#if BUILDFLAG(CHROME_WIDE_ECHO_CANCELLATION)
+  if (GetParam()) {
+    scoped_feature_list.InitAndEnableFeature(
+        media::kChromeWideEchoCancellation);
+  } else {
+    scoped_feature_list.InitAndEnableFeature(
+        media::kChromeWideEchoCancellation);
+  }
+#endif
+
   using ThisTest =
       ProcessedLocalAudioSourceTest_VerifyAudioFlowWithoutAudioProcessing_Test;
 
@@ -203,5 +221,13 @@ TEST_F(ProcessedLocalAudioSourceTest, VerifyAudioFlowWithoutAudioProcessing) {
   EXPECT_CALL(*mock_audio_capturer_source(), Stop());
   MediaStreamAudioTrack::From(audio_track())->Stop();
 }
+
+#if BUILDFLAG(CHROME_WIDE_ECHO_CANCELLATION)
+INSTANTIATE_TEST_SUITE_P(All, ProcessedLocalAudioSourceTest, testing::Bool());
+#else
+INSTANTIATE_TEST_SUITE_P(All,
+                         ProcessedLocalAudioSourceTest,
+                         testing::Values(false));
+#endif
 
 }  // namespace blink
