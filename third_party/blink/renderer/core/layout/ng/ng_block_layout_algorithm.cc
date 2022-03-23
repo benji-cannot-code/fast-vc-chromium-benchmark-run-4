2929049530c2ac1f284933d85ab7319b7d596e12
Kent Tamura <tkent@chromium.org>
2022-03-23 23:16:04 +0000

    Deferred Shaping: Do not reuse NGFragmentItem objects on reshaping
    Bug: 1259085
    Change-Id: Ie5b0cd92e9fbb5ca3901db4a7e0bc3c556c31cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542735
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984578}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1017,6 +1017,9 @@ bool NGBlockLayoutAlgorithm::TryReuseFragmentsFromCache(
     const NGInlineBreakToken** inline_break_token_out) {
   DCHECK(previous_result_);
 
+  if (inline_node.ShouldBeReshaped())
+    return false;
+
   const auto& previous_fragment =
       To<NGPhysicalBoxFragment>(previous_result_->PhysicalFragment());
   const NGFragmentItems* previous_items = previous_fragment.Items();
