f2c68a2d9679e4984657c971b14288f610c876f3
Ethan Jimenez <ethavar@microsoft.com>
2022-03-23 17:16:47 +0000

    [GridNG] Clamp grid item placement to subgrid spanned area
    Refactoring the auto-placement algorithm to clamp the resolved positions
    of a subgrid's items to be contained within the explicit grid.
    
    Bug: 618969
    Change-Id: Ia28e334167a9ebc1c11ff6db08e690a713ac4bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541702
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#984406}

--- third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.cc
+++ third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.cc
@@ -16,7 +16,6 @@ void LayoutNGGrid::UpdateBlockLayout(bool relayout_children) {
     UpdateOutOfFlowBlockLayout();
     return;
   }
-
   UpdateInFlowBlockLayout();
 }
 
@@ -25,7 +24,7 @@ void LayoutNGGrid::AddChild(LayoutObject* new_child,
   NOT_DESTROYED();
   LayoutBlock::AddChild(new_child, before_child);
 
-  // Out-of-flow grid items do not impact grid placement.
+  // Out-of-flow grid items don't impact placement.
   if (!new_child->IsOutOfFlowPositioned())
     SetGridPlacementDirty(true);
 }
@@ -34,7 +33,7 @@ void LayoutNGGrid::RemoveChild(LayoutObject* child) {
   NOT_DESTROYED();
   LayoutBlock::RemoveChild(child);
 
-  // Out-of-flow grid items do not impact grid placement.
+  // Out-of-flow grid items don't impact placement.
   if (!child->IsOutOfFlowPositioned())
     SetGridPlacementDirty(true);
 }
@@ -113,13 +112,9 @@ const LayoutNGGridInterface* LayoutNGGrid::ToLayoutNGGridInterface() const {
   return this;
 }
 
-bool LayoutNGGrid::HasCachedPlacementData() const {
-  return cached_placement_data_ && !IsGridPlacementDirty();
-}
-
 const NGGridPlacementData& LayoutNGGrid::CachedPlacementData() const {
-  DCHECK(HasCachedPlacementData());
-  return *cached_placement_data_;
+  DCHECK(!IsGridPlacementDirty());
+  return cached_placement_data_;
 }
 
 void LayoutNGGrid::SetCachedPlacementData(
@@ -140,7 +135,7 @@ const NGGridLayoutData* LayoutNGGrid::GridLayoutData() const {
 wtf_size_t LayoutNGGrid::AutoRepeatCountForDirection(
     const GridTrackSizingDirection track_direction) const {
   NOT_DESTROYED();
-  if (!HasCachedPlacementData())
+  if (IsGridPlacementDirty())
     return 0;
 
   const bool is_for_columns = track_direction == kForColumns;
@@ -150,18 +145,18 @@ wtf_size_t LayoutNGGrid::AutoRepeatCountForDirection(
           : StyleRef().GridTemplateRows().TrackList().AutoRepeatTrackCount();
 
   return auto_repeat_size *
-         (is_for_columns ? cached_placement_data_->column_auto_repetitions
-                         : cached_placement_data_->row_auto_repetitions);
+         (is_for_columns ? cached_placement_data_.column_auto_repetitions
+                         : cached_placement_data_.row_auto_repetitions);
 }
 
 wtf_size_t LayoutNGGrid::ExplicitGridStartForDirection(
     const GridTrackSizingDirection track_direction) const {
   NOT_DESTROYED();
-  if (!HasCachedPlacementData())
+  if (IsGridPlacementDirty())
     return 0;
   return (track_direction == kForColumns)
-             ? cached_placement_data_->column_start_offset
-             : cached_placement_data_->row_start_offset;
+             ? cached_placement_data_.column_start_offset
+             : cached_placement_data_.row_start_offset;
 }
 
 wtf_size_t LayoutNGGrid::ExplicitGridEndForDirection(
--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -84,6 +84,7 @@ class CORE_EXPORT NGGridLayoutAlgorithm
       const GridItemContributionType contribution_type,
       GridItemData* grid_item) const;
 
+  NGGridPlacementData PlacementData() const;
   wtf_size_t ComputeAutomaticRepetitions(
       const GridTrackSizingDirection track_direction) const;
 
