ec2f5cc905e99c85ef1a8109546c3ecc42a48753
Og Astorga <ogastorga@chromium.org>
2022-03-23 16:58:55 +0000

    [Clank CBCM] Hide MetricsReportingEnabled method behind it's feature flag
    This CL verifies that the MetricsReportingEnabled policy is enabled on Android. Then, it checks the set policy value to answer the intended query.
    
    Bug: 1267980
    Change-Id: I6114c8c39894092ede9c559f98a825d52b210b66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545849
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Og Astorga <ogastorga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984396}

--- chrome/browser/android/preferences/privacy_preferences_manager_impl.cc
+++ chrome/browser/android/preferences/privacy_preferences_manager_impl.cc
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include "base/feature_list.h"
 #include "chrome/android/chrome_jni_headers/PrivacyPreferencesManagerImpl_jni.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefetch/prefetch_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/pref_names.h"
 #include "components/metrics/metrics_pref_names.h"
+#include "components/policy/core/common/features.h"
 #include "components/prefs/pref_service.h"
 
 static jboolean JNI_PrivacyPreferencesManagerImpl_IsMetricsReportingEnabled(
@@ -29,6 +31,12 @@ static void JNI_PrivacyPreferencesManagerImpl_SetMetricsReportingEnabled(
 static jboolean
 JNI_PrivacyPreferencesManagerImpl_IsMetricsReportingDisabledByPolicy(
     JNIEnv* env) {
+  // Metrics reporting can only be disabled by policy if the policy is active.
+  if (!base::FeatureList::IsEnabled(
+          policy::features::kActivateMetricsReportingEnabledPolicyAndroid)) {
+    return false;
+  }
+
   const PrefService* local_state = g_browser_process->local_state();
   return local_state->IsManagedPreference(
              metrics::prefs::kMetricsReportingEnabled) &&
