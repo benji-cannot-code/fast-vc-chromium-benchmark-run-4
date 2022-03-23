dff87ac9b228ac24a32fced58e23c53c3b04c128
Xiaohan Wang <xhwang@chromium.org>
2022-03-23 16:27:43 +0000

    media: Add MediaFoundationServiceMonitor
    This is the class to monitor MediaFoundationService processes. If the
    process crashes twice or more, hardware secure decryption will be
    disabled to prevent more crashes.
    
    This file lives in chrome/ instead of content/ because later we want
    to store local state to disable hardware secure decryption even after
    browser restart, similar to GpuModeManager.
    
    In a later CL, this class will also handle playback and CDM failures.
    
    Manually tested by modifying code to trigger a MediaFoundationService
    process crash. When playing on a page that uses hardware secure
    decryption, the MediaFoundationService crashes. Refreshing the page
    caused another crash. Refreshing again, observed hardware secure
    decryption is disabled and the page starts to use software secure
    decryption mode.
    
    Bug: 1296219,1297907
    Test: See above
    Change-Id: Iefbdb6493d3a4fbb43dcfb991c9d6e1b5a0d3761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541700
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984382}

---
+++ chrome/browser/media/media_foundation_service_monitor.h
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_MEDIA_FOUNDATION_SERVICE_MONITOR_H_
+#define CHROME_BROWSER_MEDIA_MEDIA_FOUNDATION_SERVICE_MONITOR_H_
+
+#include "content/public/browser/service_process_host.h"
+
+class MediaFoundationServiceMonitor
+    : public content::ServiceProcessHost::Observer {
+ public:
+  // Returns the MediaFoundationServiceMonitor singleton.
+  static MediaFoundationServiceMonitor* GetInstance();
+
+  MediaFoundationServiceMonitor(const MediaFoundationServiceMonitor&) = delete;
+  MediaFoundationServiceMonitor& operator=(
+      const MediaFoundationServiceMonitor&) = delete;
+
+  // ServiceProcessHost::Observer implementation.
+  void OnServiceProcessCrashed(
+      const content::ServiceProcessInfo& info) override;
+
+ private:
+  // Make constructor/destructor private since this is a singleton.
+  MediaFoundationServiceMonitor();
+  ~MediaFoundationServiceMonitor() override;
+
+  int num_crashes_ = 0;
+};
+
+#endif  // CHROME_BROWSER_MEDIA_MEDIA_FOUNDATION_SERVICE_MONITOR_H_
