1e631632d32b81bc542510f5f83a23e9ca027ad7
Honglin Yu <honglinyu@chromium.org>
2022-03-23 07:18:41 +0000

    webml: Add IDL definition of web ml model loader API.
    This is the first CL to implement this API. After this CL, there will
    be,
    
    - A CL to add the mojom definition.
    - A CL to prepare the code in the browser process.
    - A CL to add the implementation of the IDL definition here.
    - A CL to implement the WPT tests.
    
    Intent to prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/w7Oq2_x2D8U/m/LOAfC3SdBwAJ
    Explainer:
    https://github.com/webmachinelearning/model-loader/blob/main/explainer.md
    Chrome Status:
    https://chromestatus.com/feature/6505078216196096
    
    Bug: 1298707
    Test: combined with the other CLs, the API works.
    
    Change-Id: I5dbd3bdc58402ee9428c5b937ac1f0e39b79aabe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3520923
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Honglin Yu <honglinyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984212}

---
+++ third_party/blink/renderer/modules/ml/navigator_ml.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/ml/navigator_ml.h"
+
+namespace blink {
+
+const char NavigatorML::kSupplementName[] = "NavigatorML";
+
+NavigatorML::NavigatorML(Navigator& navigator)
+    : Supplement<Navigator>(navigator),
+      ml_(MakeGarbageCollected<ML>(navigator.GetExecutionContext())) {}
+
+ML* NavigatorML::ml(Navigator& navigator) {
+  NavigatorML* supplement = Supplement<Navigator>::From<NavigatorML>(navigator);
+  if (!supplement) {
+    supplement = MakeGarbageCollected<NavigatorML>(navigator);
+    ProvideTo(navigator, supplement);
+  }
+  return supplement->ml_;
+}
+
+void NavigatorML::Trace(Visitor* visitor) const {
+  visitor->Trace(ml_);
+  Supplement<Navigator>::Trace(visitor);
+}
+
+}  // namespace blink
