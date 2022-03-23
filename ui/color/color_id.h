d1efbce1d0f0119dff469f66787a1cd57a8f4bbf
Allen Bauer <kylixrd@chromium.org>
2022-03-23 23:40:30 +0000

    Remove hard-coded color from CustomFrameView.
    Bug: 1003612
    Change-Id: I6023885714c5c8fc12ce87ab9b8cca73bbff5dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3543658
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984592}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorButtonForegroundDisabled) \
   E_CPONLY(kColorButtonForegroundProminent) \
   E_CPONLY(kColorButtonForegroundUnchecked) \
+  E_CPONLY(kColorCustomFrameCaptionForeground) \
   E_CPONLY(kColorDebugBoundsOutline) \
   E_CPONLY(kColorDebugContentOutline) \
   E_CPONLY(kColorDialogBackground) \
