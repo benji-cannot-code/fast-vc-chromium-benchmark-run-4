61d70617d484378790ac9e3c1c6f287677b4de9f
Hayato Ito <hayato@chromium.org>
2022-03-23 03:21:30 +0000

    Remove an abstraction layer for Web Bundles WPT tests
    A follow-up CL for https://crrev.com/c/3539725.
    
    Remove the most of wpt/web-bundle/subresource-loading/resources files,
    which were previously used from both <link>-based API and <script>-based
    API tests. They are now directly embedded in <script>-based API tests.
    
    This CL also removes window.TEST_WEB_BUNDLE_ELEMENT_TYPE hack from
    test-helper.js, which is no longer necessary.
    
    Bug: 1306725
    Change-Id: Id47fd8691a87207a2fa4303285fa388d885f2d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541723
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984168}

--- third_party/blink/web_tests/external/wpt/web-bundle/subresource-loading/resources/accept-header-test.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-promise_test(async () => {
-  await addWebBundleElementAndWaitForLoad(
-    '../resources/check-accept-header-and-return-bundle.py',
-    /*resources=*/[]);
-},
-'"Accept:" header in a request for a bundle should contain ' +
-'application/webbundle MIME type.');
