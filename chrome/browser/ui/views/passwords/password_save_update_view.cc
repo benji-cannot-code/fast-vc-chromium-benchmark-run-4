9af2b34090868047e8dfd3e2a16bbc0cb902e5fb
Fabio Tirelo <ftirelo@chromium.org>
2022-03-23 17:31:47 +0000

    Revert "[Passwords] Add UPM Icon in Password Save/Update Bubbles"
    This reverts commit d842afb53216c7615fe804e42e2fb159df534221.
    
    Reason for revert: It looks like the icon file is missing (crbug.com/1309548)
    
    Original change's description:
    > [Passwords] Add UPM Icon in Password Save/Update Bubbles
    >
    > This CL add the UPM icon to password save/update bubbles.
    > The code is guarded behind a feature toggle.
    > For non-branded Chrome, we fall back to a generic key icon.
    >
    > Screenshots:
    > Branded Build:
    > Light Mode: http://screen/RjynAsfK6tdyzmP
    > Dark Mode: http://screen/8UaMfyEKPmk2LNr
    >
    > Non-Branded Build:
    > Light Mode: http://screen/7kCxxGrWz7yqhPJ
    > Dark Mode: http://screen/8RvL7rqTYT2DRqm
    >
    > Bug: 1309480
    > Change-Id: Ica298e80fa29b2a779f8641e40c83bc106cb2fda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545153
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#984394}
    
    Bug: 1309480
    Change-Id: I4ea5aa61e8524ea4e373d534c7bd0f9add2768e7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546796
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Owners-Override: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984415}

--- chrome/browser/ui/views/passwords/password_save_update_view.cc
+++ chrome/browser/ui/views/passwords/password_save_update_view.cc
@@ -70,8 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr int kIconHeight = 20;
-
 int ComboboxIconSize() {
   // Use the line height of the body small text. This allows the icons to adapt
   // if the user changes the font size.
@@ -444,9 +442,7 @@ PasswordSaveUpdateView::PasswordSaveUpdateView(
         is_update_bubble_ ? &Controller::OnNopeUpdateClicked
                           : &Controller::OnNeverForThisSiteClicked));
   }
-
-  SetShowIcon(base::FeatureList::IsEnabled(
-      password_manager::features::kUnifiedPasswordManagerDesktop));
+  SetShowIcon(false);
 
   SetFootnoteView(CreateFooterView());
   UpdateBubbleUIElements();
@@ -520,16 +516,7 @@ bool PasswordSaveUpdateView::IsDialogButtonEnabled(
 }
 
 ui::ImageModel PasswordSaveUpdateView::GetWindowIcon() {
-  if (!base::FeatureList::IsEnabled(
-          password_manager::features::kUnifiedPasswordManagerDesktop)) {
-    return ui::ImageModel();
-  }
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return ui::ImageModel::FromVectorIcon(kGooglePasswordManagerIcon,
-                                        ui::kColorIcon, kIconHeight);
-#else
-  return ui::ImageModel::FromVectorIcon(kKeyIcon, ui::kColorIcon, kIconHeight);
-#endif
+  return ui::ImageModel();
 }
 
 void PasswordSaveUpdateView::AddedToWidget() {
