89e549221846ae87ba49768c584d9b8558d46249
Morten Stenshorne <mstensho@chromium.org>
2022-03-23 16:23:05 +0000

    Avoid vtable in NGBreakToken.
    Do manual dispatch for ToString() and Trace(). Note that the base
    implementation of NGBreakToken::String() was never used.
    
    Change-Id: I066ed7cb45db0f5419416d48b4626d473548ed06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545209
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984377}

--- third_party/blink/renderer/core/layout/ng/ng_block_break_token.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_break_token.h
@@ -146,7 +146,7 @@ class CORE_EXPORT NGBlockBreakToken final : public NGBreakToken {
   const NGInlineBreakToken* InlineBreakTokenFor(const LayoutBox&) const;
 
 #if DCHECK_IS_ON()
-  String ToString() const override;
+  String ToString() const;
 #endif
 
   using PassKey = base::PassKey<NGBlockBreakToken>;
@@ -182,7 +182,7 @@ class CORE_EXPORT NGBlockBreakToken final : public NGBreakToken {
     return MutableForOutOfFlow(this);
   }
 
-  void Trace(Visitor*) const override;
+  void TraceAfterDispatch(Visitor*) const;
 
  private:
   Member<NGBlockBreakTokenData> data_;
--- third_party/blink/renderer/core/layout/ng/ng_break_token.cc
+++ third_party/blink/renderer/core/layout/ng/ng_break_token.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/ng_break_token.h"
 
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/platform/wtf/size_assertions.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
@@ -14,7 +15,6 @@ namespace blink {
 namespace {
 
 struct SameSizeAsNGBreakToken : GarbageCollected<NGBreakToken> {
-  virtual ~SameSizeAsNGBreakToken() = default;
   Member<void*> member;
   unsigned flags;
 };
@@ -48,11 +48,13 @@ void AppendBreakTokenToString(const NGBreakToken* token,
 }  // namespace
 
 String NGBreakToken::ToString() const {
-  StringBuilder string_builder;
-  string_builder.Append("(");
-  string_builder.Append(InputNode().ToString());
-  string_builder.Append(")");
-  return string_builder.ToString();
+  switch (Type()) {
+    case kBlockBreakToken:
+      return To<NGBlockBreakToken>(this)->ToString();
+    case kInlineBreakToken:
+      return To<NGInlineBreakToken>(this)->ToString();
+  }
+  NOTREACHED();
 }
 
 void NGBreakToken::ShowBreakTokenTree() const {
@@ -64,6 +66,18 @@ void NGBreakToken::ShowBreakTokenTree() const {
 #endif  // DCHECK_IS_ON()
 
 void NGBreakToken::Trace(Visitor* visitor) const {
+  switch (Type()) {
+    case kBlockBreakToken:
+      To<NGBlockBreakToken>(this)->TraceAfterDispatch(visitor);
+      return;
+    case kInlineBreakToken:
+      To<NGInlineBreakToken>(this)->TraceAfterDispatch(visitor);
+      return;
+  }
+  NOTREACHED();
+}
+
+void NGBreakToken::TraceAfterDispatch(Visitor* visitor) const {
   visitor->Trace(box_);
 }
 
