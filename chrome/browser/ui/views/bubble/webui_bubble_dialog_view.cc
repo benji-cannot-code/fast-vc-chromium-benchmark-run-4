044a1a8968648307f5f31d6288312e246c10af61
Fred Shih <ffred@google.com>
2022-03-23 23:27:18 +0000

    Anchor the tab search bubble with a rect anchor when in fullscreen
    The tab search bubble is misplaced because it is anchored on the tab search button. This button does not have a proper position when the browser is in fullscreen mode. To fix this issue, we will create a rect anchor when we are in fullscreen and plumb it down to the bubble manager.
    
    Tested in RTL mode as well.
    
    Bug: 1175130
    Change-Id: I3a3fc80881cf8ae50b2e6807a118313fa157c621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438697
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Fred Shih <ffred@google.com>
    Cr-Commit-Position: refs/heads/main@{#984585}

--- chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
+++ chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/visibility.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/webview/webview.h"
@@ -124,6 +125,12 @@ bool WebUIBubbleDialogView::HandleKeyboardEvent(
     const content::NativeWebKeyboardEvent& event) {
   return unhandled_keyboard_event_handler_.HandleKeyboardEvent(
       event, GetFocusManager());
+}
+
+gfx::Rect WebUIBubbleDialogView::GetAnchorRect() const {
+  if (anchor_rect())
+    return anchor_rect().value();
+  return BubbleDialogDelegateView::GetAnchorRect();
 }
 
 BEGIN_METADATA(WebUIBubbleDialogView, views::BubbleDialogDelegateView)
