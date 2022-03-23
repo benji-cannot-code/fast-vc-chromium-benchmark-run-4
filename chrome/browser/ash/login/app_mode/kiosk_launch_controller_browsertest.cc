4af2077827e613a2c9b97a8a818065f563ffcd88
Yi Xie <yixie@chromium.org>
2022-03-23 10:16:41 +0000

    Report total duration UMA for launching Kiosk
    Kiosk.LaunchDuration records the duration between the time Kiosk
    launcher starts and the time splash screen disappears.
    
    Bug: b:214364780
    Change-Id: I287fde4af5d4158fad44ae0b31727483d158550c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530329
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Yi Xie <yixie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984255}

--- chrome/browser/ash/login/app_mode/kiosk_launch_controller_browsertest.cc
+++ chrome/browser/ash/login/app_mode/kiosk_launch_controller_browsertest.cc
@@ -127,6 +127,8 @@ class KioskLaunchControllerTest
 
   void FireSplashScreenTimer() { controller_->OnTimerFire(); }
 
+  void DeleteSplashScreen() { controller_->OnDeletingSplashScreenView(); }
+
   void SetOnline(bool online) {
     view_->SetNetworkReady(online);
     static_cast<AppLaunchSplashScreenView::Delegate*>(controller_.get())
@@ -150,6 +152,7 @@ class KioskLaunchControllerTest
 };
 
 IN_PROC_BROWSER_TEST_P(KioskLaunchControllerTest, RegularFlow) {
+  base::HistogramTester histogram;
   controller()->Start(kiosk_app_id(), false);
   ExpectState(AppState::kCreatingProfile, NetworkUIState::kNotShowing);
 
@@ -177,6 +180,21 @@ IN_PROC_BROWSER_TEST_P(KioskLaunchControllerTest, RegularFlow) {
   ExpectState(AppState::kLaunched, NetworkUIState::kNotShowing);
   ExpectViewState(AppLaunchSplashScreenView::AppLaunchState::kWaitingAppWindow);
   EXPECT_TRUE(session_manager::SessionManager::Get()->IsSessionStarted());
+
+  DeleteSplashScreen();
+  content::FetchHistogramsFromChildProcesses();
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+  switch (GetParam()) {
+    case KioskAppType::kArcApp:
+      histogram.ExpectTotalCount("Kiosk.LaunchDuration.Arc", 1);
+      break;
+    case KioskAppType::kChromeApp:
+      histogram.ExpectTotalCount("Kiosk.LaunchDuration.ChromeApp", 1);
+      break;
+    case KioskAppType::kWebApp:
+      histogram.ExpectTotalCount("Kiosk.LaunchDuration.Web", 1);
+      break;
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(KioskLaunchControllerTest, AlreadyInstalled) {
