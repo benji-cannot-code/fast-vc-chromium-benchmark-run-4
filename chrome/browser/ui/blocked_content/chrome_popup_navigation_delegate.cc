01bd867868df69c38a8dc6b10579ca79f397fb82
Colin Blundell <blundell@chromium.org>
2022-03-23 15:48:45 +0000

    [Messages] Guard against popup blocked message being shown before init
    //chrome-level initialization of the messages infrastructure happens in
    RootUiCoordinator#onFinishNativeInitialization(). The execution of this
    method is not ordered with respect to the initial navigation, and if
    that initial navigation occurs quickly enough, it can result in messages
    being shown before the initialization has occurred. One such case is for
    a blocked popup. When the blocked popup message is asked to show itself
    in this case, a crash results as it calls into
    MessageDispatcherBridge::MapToJavaDrawableId() before the
    //chrome-level call to MessageDispatcherBridge::Initialize() has set
    the ResourceMapper callback.
    
    This CL fixes the crash by having ChromePopupNavigationDelegate check
    for this case and short-circuit out of showing the message as
    necessary.
    
    Other potential ways to fix this issue that I considered:
    - Make PopupNavigationMessageDelegate resilient to being asked to show
    itself in this case. This is doable, but seems odd (from the
    component's POV, why would an embedder ask the message to show itself if
    messages aren't supported in the embedder?)
    - Make MessageDispatcherBridge resilient to this case. AFAICT this is
    not possible, as there is no safe sentinel value to return from
    MapToJavaDrawableId().
    - Fix the startup ordering issue. This would be ideal, but is out of the
    scope of this fix and seems tricky given
    https://chromium-review.googlesource.com/c/chromium/src/+/2476616.
    
    Finally, I noted that there are two other production callers of
    MessageDispatcherBridge::MapToJavaDrawableId:
    - AdsBlockedMessageDelegate is already only shown in the case where
    MessageDispatcherBridge::IsMessagesEnabledForEmbedder() is true.
    - In a separate CL, ArCoreInstallHelper could have a similar change to
    the one in this CL made if that is deemed necessary (I don't know
    exactly what flows cause the message there to be shown).
    
    Bug: 1308214
    Change-Id: I3aad67a38962cebf1d14305b63e93423b102c3da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541981
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984352}

--- chrome/browser/ui/blocked_content/chrome_popup_navigation_delegate.cc
+++ chrome/browser/ui/blocked_content/chrome_popup_navigation_delegate.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "components/blocked_content/android/popup_blocked_infobar_delegate.h"
 #include "components/blocked_content/android/popup_blocked_message_delegate.h"
+#include "components/messages/android/message_dispatcher_bridge.h"
 #include "components/messages/android/messages_feature.h"
 #endif
 
@@ -72,19 +73,29 @@ void ChromePopupNavigationDelegate::OnPopupBlocked(
 #if BUILDFLAG(IS_ANDROID)
   bool is_created = false;
   if (messages::IsPopupBlockedMessagesUiEnabled()) {
-    blocked_content::PopupBlockedMessageDelegate::CreateForWebContents(
-        web_contents);
-    blocked_content::PopupBlockedMessageDelegate*
-        popup_blocked_message_delegate =
-            blocked_content::PopupBlockedMessageDelegate::FromWebContents(
-                web_contents);
-    is_created = popup_blocked_message_delegate->ShowMessage(
-        total_popups_blocked_on_page,
-        HostContentSettingsMapFactory::GetForProfile(
-            web_contents->GetBrowserContext()),
-        base::BindOnce(
-            &content_settings::RecordPopupsAction,
-            content_settings::POPUPS_ACTION_CLICKED_ALWAYS_SHOW_ON_MOBILE));
+    messages::MessageDispatcherBridge* message_dispatcher_bridge =
+        messages::MessageDispatcherBridge::Get();
+
+    // It is possible that an initial navigation results in a blocked popup
+    // before the //chrome-level initialization of the messages infrastructure
+    // has run. Short-circuit out in that case to prevent a crash when
+    // PopupBlockedMessageDelegate tries to map the resource ID via
+    // MessageDispatcherBridge. crbug.com/1308214
+    if (message_dispatcher_bridge->IsMessagesEnabledForEmbedder()) {
+      blocked_content::PopupBlockedMessageDelegate::CreateForWebContents(
+          web_contents);
+      blocked_content::PopupBlockedMessageDelegate*
+          popup_blocked_message_delegate =
+              blocked_content::PopupBlockedMessageDelegate::FromWebContents(
+                  web_contents);
+      is_created = popup_blocked_message_delegate->ShowMessage(
+          total_popups_blocked_on_page,
+          HostContentSettingsMapFactory::GetForProfile(
+              web_contents->GetBrowserContext()),
+          base::BindOnce(
+              &content_settings::RecordPopupsAction,
+              content_settings::POPUPS_ACTION_CLICKED_ALWAYS_SHOW_ON_MOBILE));
+    }
   } else {
     // Should replace existing popup infobars, with an updated count of how many
     // popups have been blocked.
