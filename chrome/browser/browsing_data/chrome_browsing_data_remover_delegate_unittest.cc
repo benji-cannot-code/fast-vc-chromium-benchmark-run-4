140963b952bb8cdf5d616a60d599a863072d1f4a
Glen Robertson <glenrob@chromium.org>
2022-03-24 00:26:45 +0000

    IWYU: include web_app_sync_bridge.h in chrome_browsing_data_remover_delegate_unittest
    It is used on 1419-1423.
    
    Change-Id: I0b9434e9ff89a21fd7d602bdef643952cba09484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539722
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984625}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/test/web_app_test_utils.h"
 #include "chrome/browser/web_applications/web_app.h"
+#include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
