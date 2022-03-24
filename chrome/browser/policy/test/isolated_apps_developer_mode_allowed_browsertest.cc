25f53409a9944fcb3b9476b4b75ac3b599002ca8
Glen Robertson <glenrob@chromium.org>
2022-03-24 02:20:18 +0000

    CodeHealth: Fix deps between test support in web_apps and //chrome/test
    web_app_test.h and system_web_app_browsertest_base.{cc,h} introduce deps
    on //chrome/test:test_support_{,ui}, which itself needs to dep on
    :web_applications_test_support for test/fake web app subsystems
    (directly referenced via FakeWebAppProvider in TestingProfile).
    
    Solved by moving those two classes out of :w_a_test_support.
    Also required moving isolated_app_test_utils into /ui/ where it belongs.
    
    For additional clarity:
    Deps Before:
    :web_applications_unit_tests and ..._browser_tests
    -> :web_applications_test_support
       -> //chrome/test:test_support
         -> :web_applications_test_support_minimal
    -> //chrome/test:test_support (directly too)
    Deps After:
    :web_applications_unit_tests and ..._browser_tests
    -> //chrome/test:test_support
       -> :web_applications_test_support (merged with _minimal)
    
    Change-Id: Ic7b7a6dda328427000d4e413f824e61ccf1a4319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539769
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984663}

--- chrome/browser/policy/test/isolated_apps_developer_mode_allowed_browsertest.cc
+++ chrome/browser/policy/test/isolated_apps_developer_mode_allowed_browsertest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/web_applications/test/isolated_app_test_utils.h"
+#include "chrome/browser/ui/web_applications/test/isolated_app_test_utils.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
