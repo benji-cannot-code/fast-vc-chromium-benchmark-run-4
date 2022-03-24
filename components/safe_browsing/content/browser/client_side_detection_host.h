4194bede66d00a7e6ff31142593b71163698f707
Kevin McNee <mcnee@chromium.org>
2022-03-24 19:59:54 +0000

    Make RFH handling more robust in ClientSideDetectionHost
    ClientSideDetectionHost was previously storing raw RenderFrameHost
    pointers as keys for a map. Fortunately, these were not being
    dereferenced, but we still change the key to be a RenderFrameHost id
    out of caution.
    
    We also no longer create phishing detectors for inner WebContents when
    initializing this class. See https://crbug.com/1308829 for details of
    why this is a problem. Fortunately, in this case, it doesn't look like
    there would be an appreciable difference due to the disconnect handlers
    and the lack of dereferencing mentioned above.
    
    Ideally, this class would not manage lifetimes and instead use a
    DocumentUserData, but that might not have the desired semantics based on
    my skimming of https://crbug.com/1217128.
    
    Bug: 1308829
    Change-Id: I6d78518972d0fdd272a4152a51e86426b5074827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547399
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984980}

--- components/safe_browsing/content/browser/client_side_detection_host.h
+++ components/safe_browsing/content/browser/client_side_detection_host.h
@@ -31,10 +31,6 @@ namespace base {
 class TickClock;
 }
 
-namespace content {
-struct GlobalRenderFrameHostId;
-}
-
 namespace safe_browsing {
 class ClientPhishingRequest;
 class ClientSideDetectionService;
@@ -191,6 +187,8 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   // Setup a PhishingDetector Mojo connection for the given render frame.
   void InitializePhishingDetector(content::RenderFrameHost* render_frame_host);
 
+  void ClearPhishingDetector(content::GlobalRenderFrameHostId rfh_id);
+
   // This pointer may be nullptr if client-side phishing detection is
   // disabled.
   raw_ptr<ClientSideDetectionService> csd_service_;
@@ -209,7 +207,7 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   // A map from the live RenderFrameHosts to their PhishingDetector. These
   // correspond to the `phishing_detector_receiver_` in the
   // PhishingClassifierDelegate.
-  base::flat_map<content::RenderFrameHost*,
+  base::flat_map<content::GlobalRenderFrameHostId,
                  mojo::Remote<mojom::PhishingDetector>>
       phishing_detectors_;
 
