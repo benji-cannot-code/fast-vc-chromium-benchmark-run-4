c46113effa651c50243be50cd3a209a70721c49d
Nathan Eliason <eliason@chromium.org>
2022-03-24 12:43:19 +0000

    Propagate FS BucketLocator in FileSystemAccessHandleBase::GetParentURL.
    Fixed: 1296127
    Change-Id: I2f95239c04273997758e2b7818c02a5baf2e2e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3478736
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Nathan Eliason <eliason@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984786}

--- content/browser/file_system_access/file_system_access_handle_base.h
+++ content/browser/file_system_access/file_system_access_handle_base.h
@@ -58,6 +58,7 @@ class CONTENT_EXPORT FileSystemAccessHandleBase {
 
   PermissionStatus GetReadPermissionStatus();
   PermissionStatus GetWritePermissionStatus();
+  storage::FileSystemURL GetParentURLForTesting() { return GetParentURL(); }
 
   // Implementation for the GetPermissionStatus method in the
   // blink::mojom::FileSystemAccessFileHandle and DirectoryHandle interfaces.
@@ -117,7 +118,6 @@ class CONTENT_EXPORT FileSystemAccessHandleBase {
 
  private:
   storage::FileSystemURL GetParentURL();
-
   void DidRequestPermission(
       bool writable,
       base::OnceCallback<void(blink::mojom::FileSystemAccessErrorPtr,
