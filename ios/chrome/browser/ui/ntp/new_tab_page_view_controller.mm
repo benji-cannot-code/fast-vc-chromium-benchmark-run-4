66e32f4f6a2220e8a7337d0dd30d6607431736bc
adamta <adamta@google.com>
2022-03-24 19:28:25 +0000

    [iOS] [WebChannels] Remove spacing when sticking WC header
    Bug: 1277974
    Change-Id: Ide26ed40892ff6ec430a9453a2bfc5f47ecdbdf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546083
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#984966}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "ios/chrome/browser/ui/bubble/bubble_presenter.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizing.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.h"
@@ -729,7 +730,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.feedHeaderConstraints = @[
     [self.feedHeaderViewController.view.topAnchor
-        constraintEqualToAnchor:self.headerController.view.bottomAnchor],
+        constraintEqualToAnchor:self.headerController.view.bottomAnchor
+                       constant:-content_suggestions::headerBottomPadding()],
     [self.collectionView.topAnchor
         constraintEqualToAnchor:[self contentSuggestionsViewController]
                                     .view.bottomAnchor],
@@ -963,7 +965,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the Feed.
 - (CGFloat)offsetWhenScrolledIntoFeed {
   return -(self.headerController.view.frame.size.height -
-           [self stickyOmniboxHeight]);
+           [self stickyOmniboxHeight] -
+           content_suggestions::headerBottomPadding());
 }
 
 // The y-position content offset for when the fake omnibox
