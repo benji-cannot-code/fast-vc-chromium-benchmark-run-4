5fbfb0fe533962e1bcb232773129139e3dae9e10
hmare <hmare@google.com>
2022-03-24 14:59:49 +0000

    Fix persistence file unlocking after file write operation
    This change fixes the bug of the persistence file unlocking after the
    store key pair operation. With this change there is no longer a need to
    relock since the file remains locked during the entire rotation.
    
    Bug: b/225163677
    Change-Id: Id506b34edfc76d8ed322d0ab9c1701a4ef012796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541561
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#984826}

--- chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/linux_key_persistence_delegate.cc
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/linux_key_persistence_delegate.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/linux_key_persistence_delegate.h"
 
+#include <fcntl.h>
 #include <grp.h>
+#include <sys/file.h>
 #include <sys/stat.h>
 
 #include <string>
@@ -18,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/notreached.h"
+#include "base/posix/eintr_wrapper.h"
 #include "base/syslog_logging.h"
 #include "base/values.h"
 #include "build/branding_buildflags.h"
@@ -74,13 +77,13 @@ bool LinuxKeyPersistenceDelegate::CheckRotationPermissions() {
   locked_file_ = base::File(signing_key_path, base::File::FLAG_OPEN |
                                                   base::File::FLAG_READ |
                                                   base::File::FLAG_WRITE);
-
-  if (locked_file_ && !locked_file_.value().IsValid() &&
-      (locked_file_.value().Lock(base::File::LockMode::kExclusive) !=
-       base::File::FILE_OK))
+  if (!locked_file_ || !locked_file_->IsValid() ||
+      HANDLE_EINTR(flock(locked_file_->GetPlatformFile(), LOCK_EX | LOCK_NB)) ==
+          -1) {
     return LogFailure(
         "Device trust key rotation failed. Could not acquire lock on the "
         "signing key storage.");
+  }
 
   int mode;
   if (!base::GetPosixFilePermissions(signing_key_path, &mode))
@@ -135,14 +138,6 @@ bool LinuxKeyPersistenceDelegate::StoreKeyPair(
           : LogFailure(
                 "Device trust key rotation failed. Could not write to the "
                 "signing key storage.");
-
-  // TODO(b/225163677): File gets unlocked after being written to.
-  if (locked_file_ && !locked_file_.value().IsValid() &&
-      (locked_file_.value().Lock(base::File::LockMode::kExclusive) !=
-       base::File::FILE_OK))
-    return LogFailure(
-        "Device trust key rotation failed. Could not reacquire lock on the "
-        "signing key storage.");
 
   return write_result;
 }
