378cc7869b9e94e508c2df1d7e0726d93fe809e0
Anastasiia N <anastasiian@google.com>
2022-03-24 13:58:53 +0000

    Move ArcAccountPickerAppElement to resources/chromeos/arc_account_picker/
    Move ArcAccountPickerAppElement to a separate folder so that is can be
    used from resources/inline_login/ (shown for regular users) and from
    resources/chromeos/edu_coexistence/ (shown for child users) UIs.
    
    Test: browser_tests --gtest_filter="*InlineLogin*BrowserTest*"
    Test: browser_tests --gtest_filter="*ArcAccountPickerTest*"
    Bug: 1291993
    Change-Id: Ieb53de260a56ff35e9daaa45b7b35a8949cb9d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3477176
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984802}

--- chrome/browser/resources/inline_login/inline_login_app.js
+++ chrome/browser/resources/inline_login/inline_login_app.js
@@ -26,7 +26,7 @@ import './strings.m.js';
 import {getAccountAdditionOptionsFromJSON} from './inline_login_util.js';
 // </if>
 
-import {AuthCompletedCredentials, Authenticator, AuthParams} from '../gaia_auth_host/authenticator.m.js';
+import {AuthCompletedCredentials, Authenticator, AuthParams} from './gaia_auth_host/authenticator.m.js';
 import {InlineLoginBrowserProxy, InlineLoginBrowserProxyImpl} from './inline_login_browser_proxy.js';
 
 /**
@@ -175,10 +175,11 @@ Polymer({
     this.addWebUIListener(
         'send-lst-fetch-results', arg => this.sendLSTFetchResults_(arg));
     this.addWebUIListener('close-dialog', () => this.closeDialog_());
-
+    // <if expr="chromeos_ash">
     this.addWebUIListener(
         'show-signin-blocked-by-policy-page',
         data => this.signinBlockedByPolicyShowView_(data));
+    // </if>
   },
 
   /** @private */
@@ -314,6 +315,7 @@ Polymer({
     this.browserProxy_.dialogClose();
   },
 
+  // <if expr="chromeos_ash">
   /**
    * Navigates to the welcome screen.
    * @private
@@ -382,6 +384,7 @@ Polymer({
     return this.enableGaiaActionButtons_ &&
         this.currentView_ === View.addAccount;
   },
+  // </if>
 
   /**
    * Navigates to the default view.
@@ -417,10 +420,10 @@ Polymer({
   getDefaultView_() {
     // TODO(https://crbug.com/1155041): simplify this when the file will be
     // split into CrOS and non-CrOS parts.
-    if (!isChromeOS) {
-      // On non-ChromeOS always show 'Add account'.
-      return View.addAccount;
-    }
+    // <if expr="not chromeos_ash">
+    // On non-ChromeOS always show 'Add account'.
+    return View.addAccount;
+    // </if>
 
     // <if expr="chromeos_ash">
     if (this.isReauthentication_) {
@@ -453,10 +456,12 @@ Polymer({
    * @private
    */
   isWelcomePageEnabled_() {
-    if (!isChromeOS) {
-      return false;
-    }
+    // <if expr="not chromeos_ash">
+    return false;
+    // </if>
+    // <if expr="chromeos_ash">
     return !this.shouldSkipWelcomePage_ && !this.isReauthentication_;
+    // </if>
   },
 
   // <if expr="chromeos">
