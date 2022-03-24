8bbc8f791ff239909e60412f69b67d42d9e1fae2
Irem Uguz <iremuguz@google.com>
2022-03-24 17:04:09 +0000

    support-tool: Get SupportToolHandler to collect data for support case
    Add GetSupportToolHandler function to support_tool_util.* which will
    create a SupportToolHandler and fill it with the issue details like case
    ID, issue description, user email address and the selected
    DataCollectors to include. Call this SupportToolHandler from
    SupportToolUIHandler to start data collection and delete it to cancel.
    
    Add WebUIListeners for data collection completed and cancelled signals
    to Support Tool UI elements. Add browser tests for new functions.
    
    Bug:b:200511640
    Test:Run browser tests with "autoninja -C out/Default/ browser_tests &&
    out/Default/browser_tests --gtest_filter=*SupportToolTest*" command
    
    Change-Id: Ie5012d6933021243dfb8b5321f489f33c32ed1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545354
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#984894}

--- chrome/browser/ui/webui/support_tool/support_tool_ui.cc
+++ chrome/browser/ui/webui/support_tool/support_tool_ui.cc
@@ -5,15 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/support_tool/support_tool_ui.h"
 
+#include <set>
 #include <string>
 
 #include "base/base64url.h"
+#include "base/bind.h"
 #include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_ui_util.h"
 #include "chrome/browser/support_tool/data_collection_module.pb.h"
+#include "chrome/browser/support_tool/data_collector.h"
+#include "chrome/browser/support_tool/support_tool_handler.h"
 #include "chrome/browser/support_tool/support_tool_util.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
@@ -26,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
 #include "net/base/url_util.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
 const char kSupportCaseIDQuery[] = "case_id";
@@ -141,6 +146,22 @@ base::Value::List GetDataCollectorItemsInQuery(std::string module_query) {
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   return data_collector_list;
 }
+
+std::set<support_tool::DataCollectorType> GetIncludedDataCollectorTypes(
+    const base::Value::List* data_collector_items) {
+  std::set<support_tool::DataCollectorType> included_data_collectors;
+  for (const auto& item : *data_collector_items) {
+    const base::Value::Dict* item_as_dict = item.GetIfDict();
+    DCHECK(item_as_dict);
+    absl::optional<bool> isIncluded = item_as_dict->FindBool("isIncluded");
+    if (isIncluded && isIncluded.value()) {
+      included_data_collectors.insert(
+          static_cast<support_tool::DataCollectorType>(
+              item_as_dict->FindInt("protoEnum").value()));
+    }
+  }
+  return included_data_collectors;
+}
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -174,6 +195,10 @@ class SupportToolMessageHandler : public content::WebUIMessageHandler {
  private:
   base::Value::List GetAccountsList();
 
+  void OnDataCollectionDone(const PIIMap& detected_pii,
+                            std::set<SupportToolError> errors);
+
+  std::unique_ptr<SupportToolHandler> handler_;
   base::WeakPtrFactory<SupportToolMessageHandler> weak_ptr_factory_{this};
 };
 
@@ -243,13 +268,31 @@ void SupportToolMessageHandler::HandleStartDataCollection(
   DCHECK(issue_details);
   const base::Value::List* data_collectors = args[1].GetIfList();
   DCHECK(data_collectors);
-  // TODO(b/219730597): Create SupportToolHandler from `issue_details` and
-  // `data_collectors`. Will be added in follow-up CL.
+  this->handler_ =
+      GetSupportToolHandler(*issue_details->FindString("caseId"),
+                            *issue_details->FindString("emailAddress"),
+                            *issue_details->FindString("issueDescription"),
+                            GetIncludedDataCollectorTypes(data_collectors));
+  this->handler_->CollectSupportData(
+      base::BindOnce(&SupportToolMessageHandler::OnDataCollectionDone,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void SupportToolMessageHandler::OnDataCollectionDone(
+    const PIIMap& detected_pii,
+    std::set<SupportToolError> errors) {
+  // TODO(b/200511640): Process `detected_pii` and send the detected PII in
+  // FireWebUIListener.
+  AllowJavascript();
+  FireWebUIListener("data-collection-completed");
 }
 
 void SupportToolMessageHandler::HandleCancelDataCollection(
     const base::Value::List& args) {
-  // TODO(b/200511640): Cancel data collection.
+  AllowJavascript();
+  // Deleting the SupportToolHandler object will stop data collection.
+  this->handler_.reset();
+  FireWebUIListener("data-collection-cancelled");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- chrome/test/data/webui/support_tool/support_tool_test.ts
+++ chrome/test/data/webui/support_tool/support_tool_test.ts
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://support-tool/support_tool.js';
 
+import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {BrowserProxy, BrowserProxyImpl, DataCollectorItem, IssueDetails} from 'chrome://support-tool/browser_proxy.js';
 import {SupportToolElement, SupportToolPageIndex} from 'chrome://support-tool/support_tool.js';
 import {assertEquals, assertFalse} from 'chrome://webui-test/chai_assert.js';
@@ -129,5 +131,20 @@ suite('SupportToolTest', function() {
       assertEquals(listItem.isIncluded, DATA_COLLECTORS[i]!.isIncluded);
       assertEquals(listItem.protoEnum, DATA_COLLECTORS[i]!.protoEnum);
     }
+  });
+
+  test('spinner page', () => {
+    // Check the contents of spinner page.
+    const spinner = supportTool.$.spinnerPage;
+    spinner.shadowRoot!.getElementById('cancelButton')!.click();
+    browserProxy.whenCalled('cancelDataCollection').then(function() {
+      webUIListenerCallback('data-collection-cancelled');
+      flush();
+      // Make sure the issue details page is displayed after cancelling data
+      // collection.
+      assertEquals(
+          supportTool.shadowRoot!.querySelector('iron-pages')!.selected,
+          SupportToolPageIndex.ISSUE_DETAILS);
+    });
   });
 });
