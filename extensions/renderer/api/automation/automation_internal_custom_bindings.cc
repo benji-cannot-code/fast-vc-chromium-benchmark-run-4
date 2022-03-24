79f8f9d90a6eae10308f1d0eea7bea55ca1590d3
Toni Barzic <tbarzic@chromium.org>
2022-03-24 00:04:28 +0000

    Fix UAF in DestroyAccessibilityTree
    AutomationInternalCustomBindings::DestroyAccessibilityTree iterates over
    a global map returned by GetChildTreeIDReverseMap(), which maps tree IDs
    to AutomationAxTreeWrappers (owned by AutomationInternalCustomBindings).
    This was crashing in some tast tests (which disable automation between
    test runs), due to UAF to a tree wrapper object.
    Old tree wrappers were left in the map after calls to Invalidate(),
    which was deleting all tree wrappers owned by the custom bindings
    instance, without updating child tree ID reverse map. This CL adds a
    step to invalidation to remove soon to be deleted tree wrappers from
    child - tree ID reverse map.
    
    BUG=1308288
    
    AX-Relnotes: N/A
    
    Change-Id: Ibe7595499d43e1887089b51f63fcafe72ecf158e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541465
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984612}

--- extensions/renderer/api/automation/automation_internal_custom_bindings.cc
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.cc
@@ -1747,6 +1747,14 @@ void AutomationInternalCustomBindings::Invalidate() {
   if (message_filter_)
     message_filter_->Detach();
 
+  auto& child_tree_id_reverse_map =
+      AutomationAXTreeWrapper::GetChildTreeIDReverseMap();
+  base::EraseIf(
+      child_tree_id_reverse_map,
+      [this](const std::pair<ui::AXTreeID, AutomationAXTreeWrapper*>& pair) {
+        return pair.second->owner() == this;
+      });
+
   tree_id_to_tree_wrapper_map_.clear();
 }
 
