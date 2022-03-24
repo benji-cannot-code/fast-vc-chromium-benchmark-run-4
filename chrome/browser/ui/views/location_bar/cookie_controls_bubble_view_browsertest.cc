1b42501444690c4b36b94064c056dcc837146661
Fergal Daly <fergal@chromium.org>
2022-03-24 03:24:07 +0000

    Disable flaky test: CookieControlsBubbleViewTest.NotWorkingClicked
    This is flaky on win and mac
    
    
    Bug: 1309497
    Change-Id: Ibac76b9ec4eec5e6def75fc7ab41187b4bb16537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545493
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Yamada <yukiy@chromium.org>
    Owners-Override: Fergal Daly <fergal@google.com>
    Cr-Commit-Position: refs/heads/main@{#984675}

--- chrome/browser/ui/views/location_bar/cookie_controls_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls_bubble_view_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "build/build_config.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -106,7 +107,13 @@ IN_PROC_BROWSER_TEST_F(CookieControlsBubbleViewTest, NoCookiesBlocked) {
 
 // Test opening cookie controls bubble and clicking on "not working" link.
 // Check that accepting the bubble unblocks 3p cookies for this origin.
-IN_PROC_BROWSER_TEST_F(CookieControlsBubbleViewTest, NotWorkingClicked) {
+// TODO(https://crbug.com/1309497): Flaky on win and mac.
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+#define MAYBE_NotWorkingClicked DISABLED_NotWorkingClicked
+#else
+#define MAYBE_NotWorkingClicked NotWorkingClicked
+#endif
+IN_PROC_BROWSER_TEST_F(CookieControlsBubbleViewTest, MAYBE_NotWorkingClicked) {
   // Block 3p cookies.
   SetThirdPartyCookieBlocking(true);
   GURL origin = embedded_test_server()->GetURL("a.com", "/");
