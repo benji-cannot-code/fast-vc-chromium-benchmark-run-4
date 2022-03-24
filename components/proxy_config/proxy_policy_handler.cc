98d9fadfab210d3eecb8644096bc97795765151a
Igor Ruvinov <igorruvinov@chromium.org>
2022-03-24 15:02:14 +0000

    Migrate policy value accessors to typesafe versions.
    This covers various files missed in previous CLs.
    
    Reasons for using "unsafe" over type-checking versions:
    - Handling multiple policies and types
    - Need to differentiate between nullptr (unset) and type mismatch
    
    Bug: 1294817, b/221918704
    Change-Id: I064ae9c05899dad91f4c068407292d4ac0f33a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546396
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984827}

--- components/proxy_config/proxy_policy_handler.cc
+++ components/proxy_config/proxy_policy_handler.cc
@@ -77,8 +77,8 @@ base::Value RemapProxyPolicies(const PolicyMap& policies) {
   policy::PolicySource inherited_source =
       policy::POLICY_SOURCE_ENTERPRISE_DEFAULT;
   base::Value proxy_settings(base::Value::Type::DICTIONARY);
-  for (size_t i = 0; i < std::size(kDeprecatedProxyPolicies); ++i) {
-    const PolicyMap::Entry* entry = policies.Get(kDeprecatedProxyPolicies[i]);
+  for (auto* policy : kDeprecatedProxyPolicies) {
+    const PolicyMap::Entry* entry = policies.Get(policy);
     if (!entry)
       continue;
     if (policies.EntryHasHigherPriority(*entry, current_priority)) {
@@ -90,8 +90,8 @@ base::Value RemapProxyPolicies(const PolicyMap& policies) {
     // If two priorities are the same.
     if (!policies.EntryHasHigherPriority(*entry, current_priority) &&
         !policies.EntryHasHigherPriority(current_priority, *entry)) {
-      proxy_settings.SetKey(kDeprecatedProxyPolicies[i],
-                            entry->value()->Clone());
+      // |value_unsafe| is used due to multiple policy types being handled.
+      proxy_settings.SetKey(policy, entry->value_unsafe()->Clone());
     }
   }
   // Sets the new |proxy_settings| if kProxySettings isn't set yet, or if the
@@ -101,8 +101,8 @@ base::Value RemapProxyPolicies(const PolicyMap& policies) {
       (!existing ||
        policies.EntryHasHigherPriority(current_priority, *existing))) {
     return proxy_settings;
-  } else if (existing && existing->value()) {
-    return existing->value()->Clone();
+  } else if (existing && existing->value(base::Value::Type::DICT)) {
+    return existing->value(base::Value::Type::DICT)->Clone();
   }
   return base::Value();
 }
--- ios/chrome/browser/policy/restrict_accounts_policy_handler.cc
+++ ios/chrome/browser/policy/restrict_accounts_policy_handler.cc
@@ -31,7 +31,9 @@ RestrictAccountsPolicyHandler::~RestrictAccountsPolicyHandler() {}
 bool RestrictAccountsPolicyHandler::CheckPolicySettings(
     const policy::PolicyMap& policies,
     policy::PolicyErrorMap* errors) {
-  const base::Value* value = policies.GetValue(policy_name());
+  // |GetValueUnsafe| is used to differentiate between the policy value being
+  // unset vs being set with an incorrect type.
+  const base::Value* value = policies.GetValueUnsafe(policy_name());
   if (!value)
     return true;
   if (!ArePatternsValid(value)) {
@@ -46,8 +48,9 @@ bool RestrictAccountsPolicyHandler::CheckPolicySettings(
 void RestrictAccountsPolicyHandler::ApplyPolicySettings(
     const PolicyMap& policies,
     PrefValueMap* prefs) {
-  const base::Value* value = policies.GetValue(policy_name());
-  if (value && value->is_list())
+  const base::Value* value =
+      policies.GetValue(policy_name(), base::Value::Type::LIST);
+  if (value)
     prefs->SetValue(prefs::kRestrictAccountsToPatterns, value->Clone());
 }
 
