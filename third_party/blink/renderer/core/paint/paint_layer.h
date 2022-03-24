c3a8bb1da9d4fbbb29694b35a2291a7bdbba9dc3
Philip Rogers <pdr@chromium.org>
2022-03-24 13:36:10 +0000

    Do not eagerly mark ancestors for repaint
    CompositeAfterPaint removed the compositing logic from PaintLayer so
    we no longer need to eagerly mark ancestors as needing a repaint.
    
    Change-Id: Ic5dba300ba234c9df9be534d636e7b20b52adfc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546178
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984798}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -793,7 +793,6 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
   void UpdatePaginationRecursive(bool needs_pagination_update = false);
   void ClearPaginationRecursive();
 
-  void SetSelfNeedsRepaint();
   void MarkCompositingContainerChainForNeedsRepaint();
 
   PaintLayerRareData& EnsureRareData() {
