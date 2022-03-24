a8ea22dc0435f076037c07ba9ba0ff52c27db96c
Glen Robertson <glenrob@chromium.org>
2022-03-24 03:37:46 +0000

    CodeHealth: Add method to set fake subsystems in FakeWebAppProvider
    Using an actual provider with fakes set on it means less boilerplate
    setting up various managers in many unit tests, and less need to
    manually set subsystems or make sure the right manager is used in code
    under test.
    Ideally fakes would be set by default in
    FakeWebAppProvider::BuildDefault but that breaks some tests so it can be
    done in a separate CL.
    
    This makes FakeWebAppRegistryController redundant. We should remove it
    in a follow-up CL so there's one correct way to do things.
    
    As a trial/example, removed WebAppRegistryController from
    web_app_install_finalizer_unittest.cc and
    web_app_translation_manager_unittest.cc
    
    Also fixed to IWYU in these classes.
    
    Bug: 973324
    Change-Id: Iaafef580f434d6839712a788d1ec74e07734b7f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526299
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984678}

--- chrome/browser/web_applications/extensions/externally_managed_app_install_task_unittest.cc
+++ chrome/browser/web_applications/extensions/externally_managed_app_install_task_unittest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_manager.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
+#include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
