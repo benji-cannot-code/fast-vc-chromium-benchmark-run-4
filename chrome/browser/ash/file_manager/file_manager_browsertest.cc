1719e242b914115e2dcb0f394353fd7e946d1e08
Alex Danilo <adanilo@chromium.org>
2022-03-24 05:58:36 +0000

    Extract: Test ZIP extraction into duplicated folders
    Bug: 953256
    Tests: browser_tests --gtest_filter="*zipExtractCheckDuplicates*"
    Change-Id: I1ce4b6e3e9737bacf8db438fe94f16c0e97af22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547978
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984697}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -485,6 +485,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("zipCreateFileUsb"),
         TestCase("zipCreateFileUsb").FilesSwa(),
         TestCase("zipExtractCheckContent").ExtractArchive().FilesSwa(),
+        TestCase("zipExtractCheckDuplicates").ExtractArchive().FilesSwa(),
         TestCase("zipExtractShowPanel").ExtractArchive().FilesSwa(),
         TestCase("zipExtractSelectionMenus").ExtractArchive().FilesSwa()));
 
