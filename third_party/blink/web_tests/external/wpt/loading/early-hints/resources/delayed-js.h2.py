8b85435b77e3d30e9f8d602f2b7e3a54c1da2390
Kenichi Ishibashi <bashi@chromium.org>
2022-03-24 12:10:03 +0000

    Add WPT for Early Hints preload is in-flight when consumed
    The test page sends an Early Hints with a resource preload. The response
    of the resource is delayed until the final response consumes the
    resource.
    
    The purpose of this test is to make sure the resource is added to the
    document's map of preloads [1].
    
    [1] https://github.com/whatwg/html/pull/7675
    
    Bug: 1305896
    Change-Id: I39371b41860190936991799fb6457f142e7ffef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545997
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984778}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/resources/delayed-js.h2.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+
+
+def main(request, response):
+    id = request.GET.first(b"id")
+    url_dir = u'/'.join(request.url_parts.path.split(u'/')[:-1]) + u'/'
+    # Wait until the id is set via resume-delayed-js.h2.py.
+    while True:
+        if request.server.stash.take(id, url_dir):
+            break
+        time.sleep(0.1)
+
+    headers = [
+        ("Content-Type", "text/javascript"),
+        ("Cache-Control", "max-age=600"),
+    ]
+    body = "/*empty script*/"
+    return (200, "OK"), headers, body
---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/resources/resume-delayed-js.h2.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    id = request.GET.first(b"id")
+    url_dir = u'/'.join(request.url_parts.path.split(u'/')[:-1]) + u'/'
+    request.server.stash.put(id, True, url_dir)
+    headers = [
+        ("Content-Type", "text/plain"),
+    ]
+    body = "OK"
+    return (200, "OK"), headers, body
