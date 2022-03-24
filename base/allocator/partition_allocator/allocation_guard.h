74bcab1661a023a4dc486018f7ee574aea664ba6
Takashi Sakamoto <tasak@google.com>
2022-03-24 06:07:38 +0000

    [PA] Add base/allocator/partition_allocator/BUILD.gn
    Bug: 1151236
    
    Change-Id: I214e6abd7eb2cc8ec4538f31065591de18b023e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538208
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#984700}

--- base/allocator/partition_allocator/allocation_guard.h
+++ base/allocator/partition_allocator/allocation_guard.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_ALLOCATION_GUARD_H_
 
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
+#include "base/base_export.h"
 #include "build/build_config.h"
 
 namespace partition_alloc {
@@ -14,14 +15,14 @@ namespace partition_alloc {
 #if defined(PA_HAS_ALLOCATION_GUARD)
 
 // Disallow allocations in the scope. Does not nest.
-class ScopedDisallowAllocations {
+class BASE_EXPORT ScopedDisallowAllocations {
  public:
   ScopedDisallowAllocations();
   ~ScopedDisallowAllocations();
 };
 
 // Disallow allocations in the scope. Does not nest.
-class ScopedAllowAllocations {
+class BASE_EXPORT ScopedAllowAllocations {
  public:
   ScopedAllowAllocations();
   ~ScopedAllowAllocations();
