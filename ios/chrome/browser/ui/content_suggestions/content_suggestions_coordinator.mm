e41a49994ab4fe3575c2c9ff71b984b43aa84596
Chris Lu <thegreenfrog@chromium.org>
2022-03-24 18:11:08 +0000

    [ios] Assign the correct ContentSuggestionsViewController for sharing
    Bug: 1309184
    Change-Id: Iab585d698f4f528e0452b58f9b675df3efdc983a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549681
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#984928}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -543,11 +543,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[ActivityParams alloc] initWithURL:URL
                                     title:title
                                  scenario:ActivityScenario::MostVisitedEntry];
-  self.sharingCoordinator =
-      [[SharingCoordinator alloc] initWithBaseViewController:self.viewController
-                                                     browser:self.browser
-                                                      params:params
-                                                  originView:view];
+  UIViewController* contentSuggestionsVC =
+      IsContentSuggestionsUIViewControllerMigrationEnabled()
+          ? self.contentSuggestionsViewController
+          : self.collectionViewController;
+  self.sharingCoordinator = [[SharingCoordinator alloc]
+      initWithBaseViewController:contentSuggestionsVC
+                         browser:self.browser
+                          params:params
+                      originView:view];
   [self.sharingCoordinator start];
 }
 
