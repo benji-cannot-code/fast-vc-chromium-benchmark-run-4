69045e08dada463f855fc82746e6a3d8dc609b6a
Kenichi Ishibashi <bashi@chromium.org>
2022-03-24 08:48:50 +0000

    Add WPTs for early hints between redirect
    Test scenario is that sending 103 -> 302 -> 103 -> 200 responses. The
    redirect is either the same origin or cross origin. Each 101 response
    contains a preload link header.
    
    For the same origin redirect the second 101 should be ignored.
    For the cross origin redirect the first 101 should be discarded and the
    second 101 should be handled, as it is the first 103 response for the
    redirected origin.
    
    Currently the cross origin redirect test is failing since at the time
    the second 101 arrives an appropriate RenderProcessHost isn't ready
    yet. NavigationEarlyHintsManager needs a RenderProcessHost to handle
    preload requests.
    
    Bug: 1305896
    Change-Id: Ifc0163bd14aeba9dbcf672159659ae6cca53c2d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539768
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984736}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/redirect-same-origin-between-early-hints.h2.window.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+test(() => {
+    const params = new URLSearchParams();
+    params.set("preload-before-redirect", SAME_ORIGIN_RESOURCES_URL + "/empty.js?" + token());
+    params.set("preload-after-redirect", SAME_ORIGIN_RESOURCES_URL + "/empty.js?" + token());
+    params.set("redirect-url", SAME_ORIGIN_RESOURCES_URL + "/redirect-between-early-hints.h2.py");
+    params.set("final-test-page", "redirect-same-origin-between-early-hints.html");
+
+    params.set("test-step", "redirect");
+    const test_url = "resources/redirect-between-early-hints.h2.py?" + params.toString();
+    window.location.replace(new URL(test_url, window.location));
+});
