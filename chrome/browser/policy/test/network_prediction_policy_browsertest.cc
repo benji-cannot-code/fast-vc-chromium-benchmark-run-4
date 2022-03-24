260a67abf47033c9e38b3e4d1402ff6fbfb531b4
Fabio Tirelo <ftirelo@chromium.org>
2022-03-24 17:08:59 +0000

    [Clank CBCM] Enable network_prediction_policy_browsertest on Android
    Bug: 1309928
    Change-Id: I33f99a4a0febf6a1c0fc23e2cd3d5d5435b275d5
    Fixed: 1309928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549520
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984898}

--- chrome/browser/policy/test/network_prediction_policy_browsertest.cc
+++ chrome/browser/policy/test/network_prediction_policy_browsertest.cc
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/prefetch/prefetch_prefs.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/chrome_test_utils.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
 #include "components/policy/policy_constants.h"
@@ -17,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 IN_PROC_BROWSER_TEST_F(PolicyTest, NetworkPrediction) {
-  PrefService* prefs = browser()->profile()->GetPrefs();
+  PrefService* prefs = chrome_test_utils::GetProfile(this)->GetPrefs();
 
   // Enabled by default.
   EXPECT_TRUE(prefetch::IsSomePreloadingEnabled(*prefs));
