3d9cddca5787d2e3cc975fe035b89794d0ec6df0
Anastasia Helfinstein <anastasi@google.com>
2022-03-24 01:19:19 +0000

    [ChromeVox] Modularize AutoScrollHandler
    Following the pattern established by dtseng[1], convert a class from
    goog.provide to an ES6 Module. This is part of item 3 in the
    go/unified-accessibility-component-extensions effort, as well as a
    prerequisite to go/a11y-extension-v3.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3484259
    
    AX-Relnotes: n/a.
    Bug: 1301002
    Test: browser_tests --gtest_filter=*ChromeVox*AutoScroll*
    Change-Id: Iff269e37c91de22552b7712a9297e1572adbe9fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526875
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984642}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import {TypingEcho} from '../common/editable_text_base.js';
 
+import {AutoScrollHandler} from './auto_scroll_handler.js';
 import {BrailleBackground} from './braille_background.js';
 import {ChromeVoxBackground} from './classic_background.js';
 import {Color} from './color.js';
--- chrome/browser/resources/chromeos/accessibility/chromevox/background/loader.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/loader.js
@@ -11,7 +11,6 @@ goog.require('AbstractEarcons');
 goog.require('AutomationPredicate');
 goog.require('AutomationTreeWalker');
 goog.require('AutomationUtil');
-goog.require('AutoScrollHandler');
 goog.require('AutomationObjectConstructorInstaller');
 goog.require('BrailleCaptionsBackground');
 goog.require('BrailleCommandData');
