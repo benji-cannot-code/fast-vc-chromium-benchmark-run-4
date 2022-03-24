513fa5dbf69530d1b45d15663a7df3eb34e76b34
Peng Huang <penghuang@chromium.org>
2022-03-24 22:40:27 +0000

    viz: Enable SkiaRenderer unconditionally on CrOS
    SkiaRenderer is currently disabled on a small set of CrOS boards
    (MT8173, RK3399) that use the legacy video decoder. The legacy video
    decoder wraps video buffers in legacy mailboxes instead of
    SharedImages which SkiaRenderer cannot handle when processing for
    overlays.
    
    Now that SkiaRenderer is fully launched on the vast majority of CrOS
    devices, it's dangerous to leave these old devices on GLRenderer as
    GLRenderer no longer receives updates and lacks any automated test
    coverage (not to mention the performance benefits of SkiaRenderer).
    Furthermore, MT8173 and RK3399 have limited support for overlays in
    the first place.
    
    Thus, this CL makes the tradeoff between SkiaRenderer and overlays by
    disabling overlays on legacy video decoder boards and enabling
    SkiaRenderer unconditionally. It also moves the Disallow Direct
    Video Decoder flag to ui/ozone/. The flag was moved to viz purely for
    visibility [1], but now it is no longer needed there.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2518460
    
    Bug: b:192563524,b:159825227,1247756
    Change-Id: Ic599aa5acc2d2f5760f91f5fe6ea9c5e6a711649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514562
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985048}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -304,7 +304,6 @@ static const char* const kSwitchNames[] = {
     switches::kReachedCodeSamplingIntervalUs,
 #endif
 #if BUILDFLAG(IS_CHROMEOS)
-    switches::kPlatformDisallowsChromeOSDirectVideoDecoder,
     switches::kSchedulerBoostUrgent,
 #endif
 #if BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
--- content/public/browser/gpu_utils.cc
+++ content/public/browser/gpu_utils.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "cc/base/switches.h"
 #include "components/viz/common/features.h"
-#include "components/viz/common/switches.h"
 #include "components/viz/common/viz_utils.h"
 #include "content/browser/browser_main_loop.h"
 #include "content/browser/gpu/gpu_process_host.h"
@@ -28,6 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/media_buildflags.h"
 #include "ui/gfx/switches.h"
+
+// TODO(b/192563524): remove it when the legacy video decoder is replaced for
+// all devices.
+#if BUILDFLAG(IS_CHROMEOS)
+#include "ui/ozone/public/ozone_switches.h"  // nogncheck
+#endif                                       // BUILDFLAG(IS_CHROMEOS)
 
 namespace {
 
