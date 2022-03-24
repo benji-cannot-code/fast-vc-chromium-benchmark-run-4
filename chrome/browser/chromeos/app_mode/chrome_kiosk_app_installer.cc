788909968fbd5913a48ce450818324ef92189cdf
Ben Franz <bfranz@chromium.org>
2022-03-24 17:11:01 +0000

    Add more unit tests for retry behaviour in StartupAppLauncher
    One unit test unearthed a bug where we never try to initialize the network again if we have secondary apps to install.
    
    Bug: b/226560767
    Test: StartupAppLauncherTest
    Change-Id: Id5404cb58c8f32666b5ed21244b7d9a1ffc14046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549036
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984899}

--- chrome/browser/chromeos/app_mode/chrome_kiosk_app_installer.cc
+++ chrome/browser/chromeos/app_mode/chrome_kiosk_app_installer.cc
@@ -96,9 +96,7 @@ void ChromeKioskAppInstaller::MaybeInstallSecondaryApps() {
     return;
 
   if (!AreSecondaryAppsInstalled() && !delegate_->IsNetworkReady()) {
-    RetryWhenNetworkIsAvailable(
-        base::BindOnce(&ChromeKioskAppInstaller::MaybeInstallSecondaryApps,
-                       weak_ptr_factory_.GetWeakPtr()));
+    ReportInstallFailure(InstallResult::kNetworkMissing);
     return;
   }
 
@@ -287,16 +285,10 @@ void ChromeKioskAppInstaller::ReportInstallSuccess() {
 
 void ChromeKioskAppInstaller::ReportInstallFailure(
     ChromeKioskAppInstaller::InstallResult error) {
-  SYSLOG(ERROR) << "App launch failed, error: " << static_cast<int>(error);
+  SYSLOG(ERROR) << "App install failed, error: " << static_cast<int>(error);
   DCHECK_NE(ChromeKioskAppInstaller::InstallResult::kSuccess, error);
 
   std::move(on_ready_callback_).Run(error);
-}
-
-void ChromeKioskAppInstaller::RetryWhenNetworkIsAvailable(
-    base::OnceClosure callback) {
-  DelayNetworkCall(base::Milliseconds(kDefaultNetworkRetryDelayMS),
-                   std::move(callback));
 }
 
 void ChromeKioskAppInstaller::ObserveActiveInstallations() {
--- chrome/browser/chromeos/app_mode/chrome_kiosk_app_installer.h
+++ chrome/browser/chromeos/app_mode/chrome_kiosk_app_installer.h
@@ -64,7 +64,6 @@ class ChromeKioskAppInstaller : private extensions::InstallObserver {
 
   void ReportInstallSuccess();
   void ReportInstallFailure(InstallResult result);
-  void RetryWhenNetworkIsAvailable(base::OnceClosure callback);
   void ObserveActiveInstallations();
 
   const extensions::Extension* GetPrimaryAppExtension() const;
