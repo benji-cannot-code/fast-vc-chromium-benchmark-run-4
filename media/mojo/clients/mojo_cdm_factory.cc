8912e6572d4b6db6a0325f0eaee24d72dcdd99bc
Xiaohan Wang <xhwang@chromium.org>
2022-03-24 20:02:35 +0000

    media: Use WrapCallbackWithDefaultInvokeIfNotRun() in MojoCdmFactory
    The CDM is often created in a different (new or existing) process. If
    the new process crashes during CDM loading or initialization, we want
    to signal CDM creation failure.
    
    This CL uses mojo::WrapCallbackWithDefaultInvokeIfNotRun() to achieve
    that.
    
    Bug: 1296219
    Change-Id: Id891773cfc07412fcc26118c6aee05dc2c2a7245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547391
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984981}

--- media/mojo/clients/mojo_cdm_factory.cc
+++ media/mojo/clients/mojo_cdm_factory.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/clients/mojo_cdm.h"
 #include "media/mojo/mojom/content_decryption_module.mojom.h"
 #include "media/mojo/mojom/interface_factory.mojom.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
@@ -83,11 +84,15 @@ void MojoCdmFactory::Create(
     return;
   }
 
+  // Use `mojo::WrapCallbackWithDefaultInvokeIfNotRun()` in case the CDM process
+  // crashes.
   interface_factory_->CreateCdm(
       cdm_config,
-      base::BindOnce(&OnCdmCreated, session_message_cb, session_closed_cb,
-                     session_keys_change_cb, session_expiration_update_cb,
-                     std::move(cdm_created_cb)));
+      mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+          base::BindOnce(&OnCdmCreated, session_message_cb, session_closed_cb,
+                         session_keys_change_cb, session_expiration_update_cb,
+                         std::move(cdm_created_cb)),
+          mojo::NullRemote(), nullptr, "disconnection error"));
 }
 
 }  // namespace media
