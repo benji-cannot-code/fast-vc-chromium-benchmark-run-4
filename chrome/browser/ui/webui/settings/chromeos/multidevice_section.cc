8dcd7e07fbecad56355bb059caf919c6efb85a90
Guanru Lee <guanrulee@chromium.org>
2022-03-24 22:41:49 +0000

    Add a pref observer of screen lock to update the latest status
    Bug: b/224442009
    Test: unit_tests --gtest_filter=MultiDeviceSectionTest.*
    Change-Id: I423a28bf84783741b9b0889ea497d586a8882fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518710
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985050}

--- chrome/browser/ui/webui/settings/chromeos/multidevice_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/multidevice_section.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/phonehub/screen_lock_manager.h"
 #include "ash/components/phonehub/url_constants.h"
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/services/multidevice_setup/public/cpp/prefs.h"
 #include "ash/services/multidevice_setup/public/cpp/url_provider.h"
 #include "ash/services/multidevice_setup/public/mojom/multidevice_setup.mojom.h"
@@ -322,7 +323,8 @@ MultiDeviceSection::MultiDeviceSection(
       phone_hub_manager_(phone_hub_manager),
       android_sms_service_(android_sms_service),
       pref_service_(pref_service),
-      eche_app_manager_(eche_app_manager) {
+      eche_app_manager_(eche_app_manager),
+      html_source_(nullptr) {
   if (NearbySharingServiceFactory::IsNearbyShareSupportedForBrowserContext(
           profile)) {
     NearbySharingService* nearby_sharing_service =
@@ -334,6 +336,13 @@ MultiDeviceSection::MultiDeviceSection(
         nearby_sharing_service->GetSettings();
     OnEnabledChanged(nearby_share_settings->GetEnabled());
     RefreshNearbyBackgroundScanningShareSearchConcepts();
+  }
+  if (features::IsEcheSWAEnabled()) {
+    pref_change_registrar_.Init(pref_service_);
+    pref_change_registrar_.Add(
+        ash::prefs::kEnableAutoScreenLock,
+        base::BindRepeating(&MultiDeviceSection::OnEnableScreenLockChanged,
+                            base::Unretained(this)));
   }
 
   // Note: |multidevice_setup_client_| is null when multi-device features are
@@ -347,12 +356,14 @@ MultiDeviceSection::MultiDeviceSection(
 }
 
 MultiDeviceSection::~MultiDeviceSection() {
+  pref_change_registrar_.RemoveAll();
   if (multidevice_setup_client_)
     multidevice_setup_client_->RemoveObserver(this);
 }
 
 void MultiDeviceSection::AddLoadTimeData(
     content::WebUIDataSource* html_source) {
+  html_source_ = html_source;
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
       {"multidevicePageTitle", IDS_SETTINGS_MULTIDEVICE},
       {"multideviceSetupButton", IDS_SETTINGS_MULTIDEVICE_SETUP_BUTTON},
@@ -603,11 +614,7 @@ void MultiDeviceSection::AddLoadTimeData(
       phonehub::ScreenLockManager::LockStatus::kLockedOn;
   html_source->AddBoolean("isPhoneScreenLockEnabled",
                           is_phone_screen_lock_enabled);
-  const bool is_screen_lock_enabled =
-      SessionControllerClientImpl::CanLockScreen() &&
-      SessionControllerClientImpl::ShouldLockScreenAutomatically();
-  html_source->AddBoolean("isChromeosScreenLockEnabled",
-                          is_screen_lock_enabled);
+  OnEnableScreenLockChanged();
   html_source->AddBoolean("isOnePageOnboardingEnabled",
                           base::FeatureList::IsEnabled(
                               ::features::kNearbySharingOnePageOnboarding));
@@ -815,6 +822,16 @@ void MultiDeviceSection::OnFastInitiationNotificationStateChanged(
 void MultiDeviceSection::OnIsFastInitiationHardwareSupportedChanged(
     bool is_supported) {
   RefreshNearbyBackgroundScanningShareSearchConcepts();
+}
+
+void MultiDeviceSection::OnEnableScreenLockChanged() {
+  const bool is_screen_lock_enabled =
+      SessionControllerClientImpl::CanLockScreen() &&
+      SessionControllerClientImpl::ShouldLockScreenAutomatically();
+  if (html_source_) {
+    html_source_->AddBoolean("isChromeosScreenLockEnabled",
+                             is_screen_lock_enabled);
+  }
 }
 
 }  // namespace settings
--- chrome/browser/ui/webui/settings/chromeos/multidevice_section.h
+++ chrome/browser/ui/webui/settings/chromeos/multidevice_section.h
@@ -47,6 +47,7 @@ class MultiDeviceSection
   ~MultiDeviceSection() override;
 
  private:
+  friend class MultiDeviceSectionTest;
   // OsSettingsSection:
   void AddLoadTimeData(content::WebUIDataSource* html_source) override;
   void AddHandlers(content::WebUI* web_ui) override;
@@ -65,8 +66,8 @@ class MultiDeviceSection
       const multidevice_setup::MultiDeviceSetupClient::FeatureStatesMap&
           feature_states_map) override;
 
-  // Nearby Share enabled pref change observer.
-  void OnNearbySharingEnabledChanged();
+  // Screen lock enabled pref change observer.
+  void OnEnableScreenLockChanged();
 
   bool IsFeatureSupported(ash::multidevice_setup::mojom::Feature feature);
   void RefreshNearbyBackgroundScanningShareSearchConcepts();
@@ -93,6 +94,7 @@ class MultiDeviceSection
   PrefService* pref_service_;
   PrefChangeRegistrar pref_change_registrar_;
   ash::eche_app::EcheAppManager* eche_app_manager_;
+  content::WebUIDataSource* html_source_;
 };
 
 }  // namespace settings
