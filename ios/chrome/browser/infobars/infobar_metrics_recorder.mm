0120e2e3f06ce46a0b99c5a80789748d5984e726
ginnnnnnny <ginnyhuang@chromium.org>
2022-03-24 16:45:17 +0000

    [iOS][media-permissions] Fix infobar metrics typo
    Change-Id: Ie92dc4b4aff1e21afc22a8817e1b4f3bce225517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546357
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984883}

--- ios/chrome/browser/infobars/infobar_metrics_recorder.mm
+++ ios/chrome/browser/infobars/infobar_metrics_recorder.mm
@@ -117,7 +117,7 @@ const char kInfobarPermissionsModalEventHistogram[] =
     "Mobile.Messages.Modal.Event.InfobarTypePermissions";
 // Badge.
 const char kInfobarPermissionsBadgeTappedHistogram[] =
-    "Mobile.Messages.Badge.Tapped.InfobarTypeReadingList";
+    "Mobile.Messages.Badge.Tapped.InfobarTypePermissions";
 
 }  // namespace
 
