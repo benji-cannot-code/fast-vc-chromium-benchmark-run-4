93a019b09d054b5937bb151677cb83db7bffdb46
Joanmarie Diggs <jdiggs@igalia.com>
2022-03-25 19:01:18 +0000

    Trace AXObjectCacheImpl::ChildrenChanged for LayoutObject
    This change adds a new metric where we have some performance issues,
    and uses that metric in relevant tests.
    
    Change-Id: I0add431c1233cb50b72cec4c449f1edf37bc4f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540181
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#985407}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -2167,6 +2167,8 @@ void AXObjectCacheImpl::ChildrenChanged(Node* node) {
 }
 
 void AXObjectCacheImpl::ChildrenChanged(const LayoutObject* layout_object) {
+  TRACE_EVENT0("accessibility",
+               "AXObjectCacheImpl::ChildrenChanged(LayoutObject)");
   if (!layout_object)
     return;
 
