31e465440dea0f77be552272138127893ea403d9
George Benz <gbj@google.com>
2022-03-25 00:31:45 +0000

    Extract EndpointFetcher from Handler.
    Refactor the AccessCodeCastHandler to only depend on the
    AccessCodeSinkService. Additionally remove dependencies of the discovery
    server interface from the generic MDNS discovery of the Media Router.
    
    I was not able to simply solve the circular dependency from the
    //chrome//browser (see b/220386256) -- however discovery no longer needs
    this circular dependency (so the Media Router team doesn't have to deal
    with it).
    
    Bug: b/218327526
    Change-Id: If939fe5bf1ccf8beb01b9222df09b2b9f9e3e983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475163
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: George Benz <gbj@google.com>
    Cr-Commit-Position: refs/heads/main@{#985104}

--- chrome/browser/media/router/discovery/access_code/access_code_cast_discovery_interface.cc
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_discovery_interface.cc
@@ -162,6 +162,7 @@ AccessCodeCastDiscoveryInterface::AccessCodeCastDiscoveryInterface(
       logger_(logger),
       endpoint_fetcher_(CreateEndpointFetcher(access_code)) {
   DCHECK(profile_);
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 }
 
 AccessCodeCastDiscoveryInterface::AccessCodeCastDiscoveryInterface(
@@ -308,6 +309,8 @@ void AccessCodeCastDiscoveryInterface::SetNetworkInfoField(
 std::unique_ptr<EndpointFetcher>
 AccessCodeCastDiscoveryInterface::CreateEndpointFetcher(
     const std::string& access_code) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
   std::vector<std::string> discovery_scopes;
   discovery_scopes.push_back(kDiscoveryOAuth2Scope);
 
@@ -321,6 +324,7 @@ AccessCodeCastDiscoveryInterface::CreateEndpointFetcher(
 void AccessCodeCastDiscoveryInterface::ValidateDiscoveryAccessCode(
     DiscoveryDeviceCallback callback) {
   DCHECK(!callback_);
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   callback_ = std::move(callback);
 
   auto* const fetcher_ptr = endpoint_fetcher_.get();
