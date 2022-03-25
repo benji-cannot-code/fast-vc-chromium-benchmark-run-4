31a0b7e7dc0688f125c38805677e83fa98bf04a0
Christoph Schwering <schwering@google.com>
2022-03-25 23:05:34 +0000

    [Autofill] Add parameter for relaxed version of shared-autofill.
    This CL adds a parameter relax_shared_autofill to the
    AutofillSharedAutofill feature that relaxes the policy for filling
    across iframes:
    
    Before this CL, or if relax_shared_autofill is false,
    (1) certain field types may be filled if
        (a) the target field has the main origin and
        (b) the target field's frame has shared-autofill enabled;
    (2) *all* field types may be filled if
        (a) the source field has the main origin and
        (b) the target field's frame has shared-autofill enabled.
    
    Setting relax_shared_autofill to true lifts condition (2.a).
    As a consequence, more fields can be filled, and reasoning about
    which fields can be filled becomes easier.
    
    Consider the below example and suppose Autofill is triggered on
    the cc-number field.
    
      <form> <!-- origin https://a.com -->
        <input autocomplete=cc-name>
        <iframe allow=shared-autofill src=https://b.com>
          <input autocomplete=cc-number>
        </iframe>
        <input autocomplete=cc-exp>
        <iframe allow=shared-autofill src=https://c.com>
          <input autocomplete=cc-cvc>
        </iframe>
      </form>
    
    With relax_shared_autofill = false, Autofill only fills cc-name.
    It does not fill cc-exp (because it's not among the field types
    to which (1) applies) and cc-cvc (because of (2.a)).
    
    With relax_shared_autofill = true, Autofill fills all fields.
    
    Bug: 1187842, 1201849
    Change-Id: I22710e5be74e6e1bfb48759943cce864ab1bef23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500623
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#985546}

--- components/autofill/content/browser/form_forest_unittest.cc
+++ components/autofill/content/browser/form_forest_unittest.cc
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
+#include "base/test/scoped_feature_list.h"
 #include "components/autofill/content/browser/form_forest.h"
 #include "components/autofill/content/browser/form_forest_test_api.h"
 #include "components/autofill/content/browser/form_forest_util_inl.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -313,6 +315,13 @@ class FormForestTest : public content::RenderViewHostTestHarness {
   // FormForest::GetBrowserFormOfRendererForm() for details).
   enum class Policy { kDefault, kSharedAutofill, kNoSharedAutofill };
 
+  explicit FormForestTest(bool relax_shared_autofill = false) {
+    feature_list_.InitAndEnableFeatureWithParameters(
+        features::kAutofillSharedAutofill,
+        {{features::kAutofillSharedAutofillRelaxedParam.name,
+          relax_shared_autofill ? "true" : "false"}});
+  }
+
   void SetUp() override {
     RenderViewHostTestHarness::SetUp();
     CHECK(kOpaqueOrigin.opaque());
@@ -401,6 +410,7 @@ class FormForestTest : public content::RenderViewHostTestHarness {
     return it->second.get();
   }
 
+  base::test::ScopedFeatureList feature_list_;
   std::map<content::RenderFrameHost*,
            std::unique_ptr<MockContentAutofillDriver>>
       autofill_drivers_;
@@ -437,6 +447,10 @@ class FormForestTestWithMockedTree : public FormForestTest {
     size_t begin = 0;
     size_t count = base::dynamic_extent;
   };
+
+  explicit FormForestTestWithMockedTree(bool relax_shared_autofill = false)
+      : FormForestTest(
+            /*relax_shared_autofill=*/relax_shared_autofill) {}
 
   void TearDown() override {
     mocked_forms_.Reset();
@@ -1400,6 +1414,11 @@ INSTANTIATE_TEST_SUITE_P(FormForestTest,
 // Tests of FormForest::GetRendererFormsOfBrowserForm().
 
 class FormForestTestUnflatten : public FormForestTestWithMockedTree {
+ public:
+  explicit FormForestTestUnflatten(bool relax_shared_autofill = false)
+      : FormForestTestWithMockedTree(
+            /*relax_shared_autofill=*/relax_shared_autofill) {}
+
  protected:
   // The subject of this test fixture.
   std::vector<FormData> GetRendererFormsOfBrowserForm(
@@ -1571,9 +1590,17 @@ TEST_F(FormForestTestUnflatten, MainOriginPolicyWithoutSharedAutofill) {
 }
 
 // Fixture for the shared-autofill policy tests.
+// The parameter controls the value of relax_shared_autofill.
 class FormForestTestUnflattenSharedAutofillPolicy
-    : public FormForestTestUnflatten {
+    : public FormForestTestUnflatten,
+      public ::testing::WithParamInterface<bool> {
  public:
+  FormForestTestUnflattenSharedAutofillPolicy()
+      : FormForestTestUnflatten(
+            /*relax_shared_autofill=*/relax_shared_autofill()) {}
+
+  bool relax_shared_autofill() const { return GetParam(); }
+
   void SetUp() override {
     FormForestTestUnflatten::SetUp();
     MockFormForest(
@@ -1590,7 +1617,7 @@ class FormForestTestUnflattenSharedAutofillPolicy
 };
 
 // Tests filling into frames with shared-autofill policy from the main origin.
-TEST_F(FormForestTestUnflattenSharedAutofillPolicy, FromMainOrigin) {
+TEST_P(FormForestTestUnflattenSharedAutofillPolicy, FromMainOrigin) {
   MockFlattening({{"main"}, {"disallowed"}, {"allowed"}});
   std::vector<FormData> expectation = {
       WithValues(GetMockedForm("main"), Profile(0)),
@@ -1601,12 +1628,18 @@ TEST_F(FormForestTestUnflattenSharedAutofillPolicy, FromMainOrigin) {
 }
 
 // Tests filling into frames with shared-autofill policy from the main origin.
-TEST_F(FormForestTestUnflattenSharedAutofillPolicy, FromOtherOrigin) {
+TEST_P(FormForestTestUnflattenSharedAutofillPolicy, FromOtherOrigin) {
   MockFlattening({{"main"}, {"disallowed"}, {"allowed"}});
-  std::vector<FormData> expectation = {
-      WithoutValues(GetMockedForm("main")),
-      WithValues(GetMockedForm("disallowed"), Profile(1)),
-      WithoutValues(GetMockedForm("allowed"))};
+  std::vector<FormData> expectation;
+  if (!relax_shared_autofill()) {
+    expectation = {WithoutValues(GetMockedForm("main")),
+                   WithValues(GetMockedForm("disallowed"), Profile(1)),
+                   WithoutValues(GetMockedForm("allowed"))};
+  } else {
+    expectation = {WithValues(GetMockedForm("main"), Profile(0)),
+                   WithValues(GetMockedForm("disallowed"), Profile(1)),
+                   WithValues(GetMockedForm("allowed"), Profile(2))};
+  }
   EXPECT_THAT(GetRendererFormsOfBrowserForm("main", Origin(kOtherUrl), {}),
               UnorderedArrayEquals(expectation));
 }
@@ -1680,6 +1713,10 @@ TEST_P(ForEachInSetDifferenceTest, Test) {
   EXPECT_THAT(diff, ElementsAreArray(GetParam().diff));
   EXPECT_EQ(num_equals_calls_, GetParam().expected_comparisons);
 }
+
+INSTANTIATE_TEST_SUITE_P(FormForestTest,
+                         FormForestTestUnflattenSharedAutofillPolicy,
+                         testing::Bool());
 
 INSTANTIATE_TEST_SUITE_P(
     FormForestTest,
