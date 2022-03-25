6bde5dcb5216ce93a0e6463536f793e7ef9ef61b
Anastasia Helfinstein <anastasi@google.com>
2022-03-25 23:05:10 +0000

    [ChromeVox] Modularize CommandStore
    Following the pattern established by dtseng[1], convert a class from
    goog.provide to an ES6 Module. This is part of item 3 in the
    go/unified-accessibility-component-extensions effort, as well as a
    prerequisite to go/a11y-extension-v3.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3484259
    
    AX-Relnotes: n/a.
    Bug: 1301002
    Test: browser_tests --gtest_filter=*ChromeVox*Command*
    Change-Id: I5b1c91674055d2e85cc7a3974d2509d531cac178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3544008
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985545}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
@@ -8,14 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 import {GestureCommandData} from '../background/gesture_command_data.js';
+import {CommandStore} from '../common/command_store.js';
 import {ChromeVoxKbHandler} from '../common/keyboard_handler.js';
 
 /**
  * Class to manage the keyboard explorer.
  */
 export class LearnMode {
-  constructor() {}
-
   /**
    * Initialize keyboard explorer.
    */
--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel_loader.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel_loader.js
@@ -12,7 +12,6 @@ goog.require('AutomationTreeWalker');
 goog.require('AutomationUtil');
 goog.require('BrailleCommandData');
 goog.require('ChromeVoxState');
-goog.require('CommandStore');
 goog.require('EventGenerator');
 goog.require('EventSourceType');
 goog.require('KeyCode');
