cb3537898893365971a2979b6dde2b77c80e1aea
Yao Qijing <yaoqq@google.com>
2022-03-25 03:43:21 +0000

    Close compat menu if entry button is clicked again
    Currently if compat mode button is clicked when the compat menu is
    shown, the menu is closed and then gets shown again. It's an unnatural
    UX since the user would expect a toggle effect for the button.
    This CL fixes the issue by adding a check before (re-)showing the menu,
    which is based on a weak pointer of the menu bubble.
    Also need to note that in this implementation, the close action (when
    the button is clicked when bubble is shown) relies on the delayed
    destruction of the bubble object which is triggered by activation change
    of the menu bubble.
    
    R=toshikikikuchi
    
          --gtest_filter="*ResizeToggleMenuTest.TestIsBubbleShown"
    
    Test: Manual testing
    Test: ash_components_unittests \
    Bug: b/192514067
    Change-Id: I41dbabf985aee9c205810a771cf10a22663e1d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539609
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Qijing Yao <yaoqq@google.com>
    Cr-Commit-Position: refs/heads/main@{#985166}

--- ash/components/arc/compat_mode/resize_toggle_menu.h
+++ ash/components/arc/compat_mode/resize_toggle_menu.h
@@ -80,6 +80,8 @@ class ResizeToggleMenu : public views::WidgetObserver,
                                intptr_t old) override;
   void OnWindowDestroying(aura::Window* window) override;
 
+  bool IsBubbleShown() const;
+
  private:
   friend class ResizeToggleMenuTest;
 
@@ -88,6 +90,8 @@ class ResizeToggleMenu : public views::WidgetObserver,
   void ApplyResizeCompatMode(ResizeCompatMode mode);
 
   gfx::Rect GetAnchorRect() const;
+
+  base::WeakPtr<views::BubbleDialogDelegateView> bubble_view_;
 
   std::unique_ptr<views::BubbleDialogDelegateView> MakeBubbleDelegateView(
       views::Widget* parent,
@@ -107,8 +111,9 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
   base::CancelableOnceClosure auto_close_closure_;
 
-  // Store only for testing.
   views::Widget* bubble_widget_{nullptr};
+
+  // Store only for testing.
   MenuButtonView* phone_button_{nullptr};
   MenuButtonView* tablet_button_{nullptr};
   MenuButtonView* resizable_button_{nullptr};
