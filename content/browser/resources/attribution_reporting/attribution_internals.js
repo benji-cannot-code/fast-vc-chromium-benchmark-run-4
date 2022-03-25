b07aaf700a5a04036b7195d1414eff57bd3dd2d1
Andrew Paseltiner <apaseltiner@chromium.org>
2022-03-25 17:46:29 +0000

    Highlight debug attribution report URLs in internals UI
    To make debug more easily distinguished at a glance.
    
    Screenshot of changes in crbug.com/1310266#c1.
    
    Bug: 1310266
    Change-Id: I82d4e4254494e8ab7d6ae00f9b072644d359e697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550770
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985365}

--- content/browser/resources/attribution_reporting/attribution_internals.js
+++ content/browser/resources/attribution_reporting/attribution_internals.js
@@ -138,6 +138,36 @@ class CodeColumn extends ValueColumn {
   }
 }
 
+const debugPathPattern =
+    /(?<=\/\.well-known\/attribution-reporting\/)debug(?=\/)/;
+
+/**
+ * @extends {ValueColumn<Report, string>}
+ */
+class ReportUrlColumn extends ValueColumn {
+  constructor() {
+    super('Report URL', (e) => e.reportUrl);
+  }
+
+  /** @override */
+  render(td, row) {
+    if (!row.isDebug) {
+      td.innerText = row.reportUrl;
+      return;
+    }
+
+    const [pre, post] = row.reportUrl.split(debugPathPattern, 2);
+    td.appendChild(new Text(pre));
+
+    const span = td.ownerDocument.createElement('span');
+    span.classList.add('debug-url');
+    span.innerText = 'debug';
+    td.appendChild(span);
+
+    td.appendChild(new Text(post));
+  }
+}
+
 /**
  * @template T
  * @abstract
@@ -725,7 +755,7 @@ class EventLevelReportTableModel extends ReportTableModel {
       new CodeColumn('Report Body', (e) => e.reportBody),
       new ValueColumn('Status', (e) => e.status),
       new ValueColumn('Destination', (e) => e.attributionDestination),
-      new ValueColumn('Report URL', (e) => e.reportUrl),
+      new ReportUrlColumn(),
       new DateColumn('Trigger Time', (e) => e.triggerTime),
       new DateColumn('Report Time', (e) => e.reportTime),
       new ValueColumn('Report Priority', (e) => e.reportPriority),
@@ -751,7 +781,7 @@ class AggregatableAttributionReportTableModel extends ReportTableModel {
       new CodeColumn('Report Body', (e) => e.reportBody),
       new ValueColumn('Status', (e) => e.status),
       new ValueColumn('Destination', (e) => e.attributionDestination),
-      new ValueColumn('Report URL', (e) => e.reportUrl),
+      new ReportUrlColumn(),
       new DateColumn('Trigger Time', (e) => e.triggerTime),
       new DateColumn('Report Time', (e) => e.reportTime),
       new CodeColumn('Histograms', (e) => e.contributions),
