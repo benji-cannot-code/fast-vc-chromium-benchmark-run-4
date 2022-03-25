f4c7cece374501c8e39ca79842fc666c49939641
Alexander Tekle <alexandertekle@google.com>
2022-03-25 03:10:32 +0000

    [M2] Log and check strikes for VCN Enrollment
    This CL is a follow up for crrev.com/c/3414638 to begin logging and checking for strikes in the VCN Enrollment flow. We will check strikes before offering enrollment and log a strike if the user does not accept enrollment.
    
    Bug: 1281695, 1310134
    Change-Id: I7cdf2b3ea28fe06d9317ddb0c214ea721bcdfc8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453029
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#985159}

--- components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.cc
+++ components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.cc
@@ -101,6 +101,15 @@ void LogVirtualCardEnrollmentLinkClickedMetric(
       true);
 }
 
+void LogVirtualCardEnrollmentStrikeDatabaseEvent(
+    VirtualCardEnrollmentSource source,
+    VirtualCardEnrollmentStrikeDatabaseEvent strike_event) {
+  base::UmaHistogramEnumeration(
+      "Autofill.VirtualCardEnrollmentStrikeDatabase." +
+          VirtualCardEnrollmentSourceToMetricSuffix(source),
+      strike_event);
+}
+
 void LogVirtualCardEnrollBubbleLatencySinceUpstream(
     const base::TimeDelta& latency) {
   base::UmaHistogramTimes(
@@ -125,6 +134,19 @@ std::string VirtualCardEnrollmentBubbleSourceToMetricSuffix(
   }
 }
 
+const std::string VirtualCardEnrollmentLinkTypeToMetricSuffix(
+    VirtualCardEnrollmentLinkType link_type) {
+  switch (link_type) {
+    case VirtualCardEnrollmentLinkType::
+        VIRTUAL_CARD_ENROLLMENT_GOOGLE_PAYMENTS_TOS_LINK:
+      return "GoogleLegalMessageLink";
+    case VirtualCardEnrollmentLinkType::VIRTUAL_CARD_ENROLLMENT_ISSUER_TOS_LINK:
+      return "IssuerLegalMessageLink";
+    case VirtualCardEnrollmentLinkType::VIRTUAL_CARD_ENROLLMENT_LEARN_MORE_LINK:
+      return "LearnMoreLink";
+  }
+}
+
 const std::string VirtualCardEnrollmentSourceToMetricSuffix(
     VirtualCardEnrollmentSource source) {
   switch (source) {
@@ -136,19 +158,6 @@ const std::string VirtualCardEnrollmentSourceToMetricSuffix(
       return "SettingsPage";
     case VirtualCardEnrollmentSource::kNone:
       return "Unknown";
-  }
-}
-
-const std::string VirtualCardEnrollmentLinkTypeToMetricSuffix(
-    VirtualCardEnrollmentLinkType link_type) {
-  switch (link_type) {
-    case VirtualCardEnrollmentLinkType::
-        VIRTUAL_CARD_ENROLLMENT_GOOGLE_PAYMENTS_TOS_LINK:
-      return "GoogleLegalMessageLink";
-    case VirtualCardEnrollmentLinkType::VIRTUAL_CARD_ENROLLMENT_ISSUER_TOS_LINK:
-      return "IssuerLegalMessageLink";
-    case VirtualCardEnrollmentLinkType::VIRTUAL_CARD_ENROLLMENT_LEARN_MORE_LINK:
-      return "LearnMoreLink";
   }
 }
 
