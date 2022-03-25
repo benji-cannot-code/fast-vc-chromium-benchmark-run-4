e4137f2bdda9dfd6316de0eb0076ec9d5c3823bc
Roger Tinkoff <rtinkoff@google.com>
2022-03-25 16:53:37 +0000

    ash: Calendar event fetch changes
    Resurrect the concept of non-prunable months, make todays's date and
    +/- 2 surrounding months non-prunable.
    
    Add method ClearAllPrunableEvents(), invoked from
    CalendarViewController's destructor.
    
    We now support re-fetching of events for a month. In
    CalendarModel::MaybeFetchMonth() no fetch is performed on a prunable
    month that's already in the cache, while non-prunable months are
    always refreshed.
    
    Bug: 1304001, 1302285
    Change-Id: Iab67f1400d0c46f1f28fbbeeac235ac3d8407a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3519013
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#985339}

--- ash/system/time/calendar_month_view_unittest.cc
+++ ash/system/time/calendar_month_view_unittest.cc
@@ -80,7 +80,7 @@ class CalendarMonthViewTest : public AshTestBase {
   }
 
   void UploadEvents() {
-    Shell::Get()->system_tray_model()->calendar_model()->InsertEvents(
+    Shell::Get()->system_tray_model()->calendar_model()->InsertEventsForTesting(
         CreateMockEventList().get());
   }
 
