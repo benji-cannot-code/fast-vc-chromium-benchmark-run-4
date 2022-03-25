0126b0b7e6433893a7aaffb7b68b475307fc2ae7
Abhijith Nair <abhijithnair@chromium.org>
2022-03-25 16:31:46 +0000

    Move JsSandboxContext to using threadpool instead of individual threads
    We were using individual threads per JsSandboxContext object and killing
    the thread as and when we close() the context. We were destructing the
    thread from the same thread that was used for creation.
    This is an incorrect approach since Thread APIs specify that all
    Thread APIs should be run on the same thread that was used to call
    Thread.Start() which in this case would be the Binder thread.
    This was leading to flaky tests when using .close(). We could potentially
    work around this by continuing to use a thread and making some
    modifications on where we do the destruction, but as an initial step,
    I am moving to using a thread pool so as to not bother about the
    lifetime of threads. We can revisit this if needed when we implement
    more complex logic as part of the .close() API.
    
    Bug: 1309447
    Change-Id: I95534f6f1cf0b06bf2a91b98834cc8757126529d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545308
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985328}

--- android_webview/renderer/js_sandbox/js_sandbox_context.h
+++ android_webview/renderer/js_sandbox/js_sandbox_context.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/callback_forward.h"
 #include "base/compiler_specific.h"
-#include "base/threading/thread.h"
 #include "gin/public/isolate_holder.h"
 #include "gin/shell_runner.h"
 
@@ -46,7 +45,6 @@ class JsSandboxContext {
                                   FinishedCallback success_callback,
                                   FinishedCallback failure_callback);
 
-  std::unique_ptr<base::Thread> thread_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   std::unique_ptr<gin::IsolateHolder> isolate_holder_;
   std::unique_ptr<SandboxRunnerDelegate> delegate_;
