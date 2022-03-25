1b02640d582815a4c62cbe858c61c5f94636af77
Josiah K <josiahk@google.com>
2022-03-25 17:22:07 +0000

    [Magnifier] Store preference for Docked Magnifier screen height divisor
    Use pref to maintain Docked Magnifier viewport height across sessions.
    The Docked Magnifier is resizable by mouse dragging on the separator
    dividing the magnified viewport from the rest of the screen.
    
    This should be a clean refactor, replacing an internal variable with a
    pref.
    
    AX-Relnotes: N/A.
    Bug: 1271733
    Test: Enable Docked Magnifier, resize, restart Chromebook, login, verify new size maintained.
    Change-Id: I52614f73e6c7f2b762e3c0b54bd77f763662e90d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501232
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#985352}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -734,6 +734,8 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_allowlist)[ash::prefs::kDockedMagnifierScale] =
       settings_api::PrefType::PREF_TYPE_NUMBER;
+  (*s_allowlist)[ash::prefs::kDockedMagnifierScreenHeightDivisor] =
+      settings_api::PrefType::PREF_TYPE_NUMBER;
 
   // Input method settings.
   (*s_allowlist)[::prefs::kLanguagePreloadEngines] =
--- chrome/browser/prefs/pref_service_incognito_allowlist.cc
+++ chrome/browser/prefs/pref_service_incognito_allowlist.cc
@@ -68,6 +68,7 @@ const char* const kPersistentPrefNames[] = {
     ash::prefs::kAccessibilityDictationLocale,
     ash::prefs::kDockedMagnifierEnabled,
     ash::prefs::kDockedMagnifierScale,
+    ash::prefs::kDockedMagnifierScreenHeightDivisor,
     ash::prefs::kDockedMagnifierAcceleratorDialogHasBeenAccepted,
     ash::prefs::kHighContrastAcceleratorDialogHasBeenAccepted,
     ash::prefs::kScreenMagnifierAcceleratorDialogHasBeenAccepted,
