34c1f70c3b91d36d1d545a778dd617563dc61288
Yutaka Hirano <yhirano@chromium.org>
2022-03-25 00:15:52 +0000

    Use URLRequestContextBuilder in ios/web_view
    Configuring URLRequestContext manually is error prone, so use
    URLRequestContextBuilder instead.
    
    Bug: 1282359, 1282361
    Change-Id: I7b56e53da0d7d396b134d41559aca40050ee8336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549697
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985098}

--- ios/web_view/internal/web_view_url_request_context_getter.h
+++ ios/web_view/internal/web_view_url_request_context_getter.h
@@ -17,11 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 class NetLog;
-class NetworkDelegate;
 class ProxyConfigService;
-class TransportSecurityPersister;
 class URLRequestContext;
-class URLRequestContextStorage;
 class SystemCookieStore;
 }  // namespace net
 
@@ -63,11 +60,7 @@ class WebViewURLRequestContextGetter : public net::URLRequestContextGetter {
   net::NetLog* net_log_;
   scoped_refptr<base::SingleThreadTaskRunner> network_task_runner_;
   std::unique_ptr<net::ProxyConfigService> proxy_config_service_;
-  std::unique_ptr<net::NetworkDelegate> network_delegate_;
   std::unique_ptr<net::URLRequestContext> url_request_context_;
-  std::unique_ptr<net::URLRequestContextStorage> storage_;
-  std::unique_ptr<net::TransportSecurityPersister>
-      transport_security_persister_;
   // SystemCookieStore must be created on UI thread in
   // WebViewURLRequestContextGetter's constructor. Later the ownership is passed
   // to net::URLRequestContextStorage on IO thread. |system_cookie_store_| is
