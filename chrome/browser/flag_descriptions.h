9a9527de2875810f973c7bbd22f45745f5d8c1dc
Yuichiro Hanada <yhanada@google.com>
2022-03-25 01:35:49 +0000

    Remove 'arc-mouse-wheel-smooth-scroll' feature flag.
    The feature was enabled by default in M-98.
    
    Bug: 1270626
    Change-Id: I8c8e3d243b515f79b0c8eb9f25a746505d15e917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549338
    Auto-Submit: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985118}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2378,9 +2378,6 @@ extern const char kArcFilePickerExperimentDescription[];
 extern const char kArcKeyboardShortcutHelperIntegrationName[];
 extern const char kArcKeyboardShortcutHelperIntegrationDescription[];
 
-extern const char kArcMouseWheelSmoothScrollName[];
-extern const char kArcMouseWheelSmoothScrollDescription[];
-
 extern const char kArcNativeBridgeToggleName[];
 extern const char kArcNativeBridgeToggleDescription[];
 
