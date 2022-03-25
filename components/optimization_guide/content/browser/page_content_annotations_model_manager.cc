8349ae01782aa71e2db8a9e992ba4d6e5181fe7c
Tarun Bansal <tbansal@chromium.org>
2022-03-25 22:16:11 +0000

    Use continue_on_shutdown for opt. guide task runners
    Bug: 1310377
    Change-Id: Ibfc1b07c0e47dbedead6da537ecfdbbab82c9946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3543659
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985517}

--- components/optimization_guide/content/browser/page_content_annotations_model_manager.cc
+++ components/optimization_guide/content/browser/page_content_annotations_model_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/content/browser/page_content_annotations_model_manager.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros_local.h"
 #include "base/strings/string_number_conversions.h"
@@ -124,9 +125,22 @@ void PageContentAnnotationsModelManager::SetUpPageEntitiesModel(
       "PageEntitiesModelRequested",
       true);
 #if BUILDFLAG(BUILD_WITH_INTERNAL_OPTIMIZATION_GUIDE)
+
+  base::TaskTraits task_traits = {base::MayBlock(),
+                                  base::TaskPriority::BEST_EFFORT};
+  if (base::FeatureList::IsEnabled(
+          features::
+              kOptimizationGuideUseContinueOnShutdownForPageContentAnnotations)) {
+    task_traits = {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+                   base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN};
+  }
+
+  scoped_refptr<base::SequencedTaskRunner> background_task_runner =
+      base::ThreadPool::CreateSequencedTaskRunner(task_traits);
+
   page_entities_model_executor_ =
       std::make_unique<PageEntitiesModelExecutorImpl>(
-          optimization_guide_model_provider);
+          optimization_guide_model_provider, background_task_runner);
 #endif
 }
 
@@ -213,10 +227,18 @@ void PageContentAnnotationsModelManager::SetUpPageTopicsModel(
       proto::PAGE_TOPICS_SUPPORTED_OUTPUT_CATEGORIES);
   page_topics_model_metadata.SerializeToString(model_metadata.mutable_value());
 
+  base::TaskTraits task_traits = {base::MayBlock(),
+                                  base::TaskPriority::BEST_EFFORT};
+  if (base::FeatureList::IsEnabled(
+          features::
+              kOptimizationGuideUseContinueOnShutdownForPageContentAnnotations)) {
+    task_traits = {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+                   base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN};
+  }
+
   page_topics_model_handler_ = std::make_unique<BertModelHandler>(
       optimization_guide_model_provider,
-      base::ThreadPool::CreateSequencedTaskRunner(
-          {base::MayBlock(), base::TaskPriority::BEST_EFFORT}),
+      base::ThreadPool::CreateSequencedTaskRunner(task_traits),
       proto::OPTIMIZATION_TARGET_PAGE_TOPICS, model_metadata);
 }
 
