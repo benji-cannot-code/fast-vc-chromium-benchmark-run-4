5beb6a8408b2a0b8f8df1d909e27a2757bc43122
Christopher Cameron <ccameron@chromium.org>
2022-03-26 06:35:40 +0000

    Add ScreenCaptureKitDeviceMac
    This class is similar to DesktopCaptureDeviceMac. Create a common base
    class, IOSurfaceCaptureDeviceBase, with the shared functionality.
    
    Add ScreenCaptureKitDeviceMac, and a feature kScreenCaptureKitMac
    that it's gated on.
    
    Bug: 1309653
    Change-Id: Ic888bcb0c745a50574e82e645dc50fd41836542d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3544199
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985638}

--- content/browser/media/capture/screen_capture_kit_device_mac.h
+++ content/browser/media/capture/screen_capture_kit_device_mac.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_MEDIA_CAPTURE_DESKTOP_CAPTURE_DEVICE_MAC_H_
-#define CONTENT_BROWSER_MEDIA_CAPTURE_DESKTOP_CAPTURE_DEVICE_MAC_H_
+#ifndef CONTENT_BROWSER_MEDIA_CAPTURE_SCREEN_CAPTURE_KIT_DEVICE_MAC_H_
+#define CONTENT_BROWSER_MEDIA_CAPTURE_SCREEN_CAPTURE_KIT_DEVICE_MAC_H_
 
 #include <stdint.h>
 
@@ -21,11 +21,8 @@ class VideoCaptureDevice;
 namespace content {
 
 std::unique_ptr<media::VideoCaptureDevice> CONTENT_EXPORT
-CreateDesktopCaptureDeviceMac(const DesktopMediaID& source);
-
-std::unique_ptr<media::VideoCaptureDevice> CONTENT_EXPORT
 CreateScreenCaptureKitDeviceMac(const DesktopMediaID& source);
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_MEDIA_CAPTURE_DESKTOP_CAPTURE_DEVICE_MAC_H_
+#endif  // CONTENT_BROWSER_MEDIA_CAPTURE_SCREEN_CAPTURE_KIT_DEVICE_MAC_H_
--- content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
+++ content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_ANDROID)
 #if BUILDFLAG(IS_MAC)
 #include "content/browser/media/capture/desktop_capture_device_mac.h"
+#include "content/browser/media/capture/screen_capture_kit_device_mac.h"
 #include "content/browser/media/capture/views_widget_video_capture_device_mac.h"
 #endif
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -88,6 +89,8 @@ const int kMaxNumberOfBuffers = media::kVideoCaptureDefaultMaxBufferPoolSize;
 const base::Feature kDesktopCaptureMacV2{"DesktopCaptureMacV2",
                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
+const base::Feature kScreenCaptureKitMac{"ScreenCaptureKitMac",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
 
 #if BUILDFLAG(ENABLE_SCREEN_CAPTURE)
@@ -456,8 +459,16 @@ void InProcessVideoCaptureDeviceLauncher::DoStartDesktopCaptureOnDeviceThread(
   video_capture_device = std::make_unique<ScreenCaptureDeviceAndroid>();
 #else
 #if BUILDFLAG(IS_MAC)
-  if (base::FeatureList::IsEnabled(kDesktopCaptureMacV2))
+  // Prefer using ScreenCaptureKit. After that try DesktopCaptureDeviceMac, and
+  // if both fail, use the generic DesktopCaptureDevice.
+  if (!video_capture_device &&
+      base::FeatureList::IsEnabled(kScreenCaptureKitMac)) {
+    video_capture_device = CreateScreenCaptureKitDeviceMac(desktop_id);
+  }
+  if (!video_capture_device &&
+      base::FeatureList::IsEnabled(kDesktopCaptureMacV2)) {
     video_capture_device = CreateDesktopCaptureDeviceMac(desktop_id);
+  }
 #endif
   if (!video_capture_device)
     video_capture_device = DesktopCaptureDevice::Create(desktop_id);
