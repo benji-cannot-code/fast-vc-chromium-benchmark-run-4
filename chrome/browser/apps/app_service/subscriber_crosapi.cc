b6d2c14c8ce0a31ce2c2ce06a4cc47a039b806a2
Eric Willigers <ericwilligers@chromium.org>
2022-03-26 04:46:33 +0000

    Lacros PWAs: Launch Ash Management page in Ash
    As lacros_url_handling::NavigateInAsh() blocks query strings like in
    chrome://os-settings/app-management/detail?id=mmigdlpiebcbgjebhnmlhckojlmncaem
    we add a method ShowAppManagementPage() to AppServiceProxy crosapi.
    
    Bug: 1297868
    Change-Id: I9a42dfe6f616a89223c010a885ca642bd5befa60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536695
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985630}

--- chrome/browser/apps/app_service/subscriber_crosapi.cc
+++ chrome/browser/apps/app_service/subscriber_crosapi.cc
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/apps/app_service/metrics/app_service_metrics.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/chrome_pages.h"
+#include "chrome/browser/ui/webui/settings/ash/app_management/app_management_uma.h"
 #include "chrome/common/chrome_features.h"
+#include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 
@@ -41,6 +44,8 @@ bool Accepts(const std::vector<apps::mojom::AppPtr>& deltas) {
 }  // namespace
 
 namespace apps {
+
+class AppUpdate;
 
 SubscriberCrosapi::SubscriberCrosapi(Profile* profile) : profile_(profile) {}
 
@@ -163,6 +168,17 @@ void SubscriberCrosapi::AddPreferredApp(const std::string& app_id,
   auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
   proxy->AddPreferredApp(
       app_id, apps_util::ConvertCrosapiToAppServiceIntent(intent, profile_));
+}
+
+void SubscriberCrosapi::ShowAppManagementPage(const std::string& app_id) {
+  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
+  if (!proxy->AppRegistryCache().ForOneApp(app_id,
+                                           [](const apps::AppUpdate&) {})) {
+    LOG(WARNING) << "Unknown app: " << app_id;
+    return;
+  }
+  chrome::ShowAppManagementPage(
+      profile_, app_id, ash::settings::AppManagementEntryPoint::kPageInfoView);
 }
 
 void SubscriberCrosapi::OnSubscriberDisconnected() {
--- chrome/browser/ui/web_applications/web_app_ui_utils.cc
+++ chrome/browser/ui/web_applications/web_app_ui_utils.cc
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/features.h"
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/crosapi/mojom/app_service.mojom.h"
+#include "chromeos/lacros/lacros_service.h"
+#endif
+
 namespace web_app {
 
 namespace {
@@ -29,9 +34,6 @@ absl::optional<AppId> GetAppIdForManagementLinkInWebContents(
     content::WebContents* web_contents) {
 #if BUILDFLAG(IS_CHROMEOS)
   bool show_app_link_in_app_window = true;
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  // TODO(crbug.com/1297868): implement `ShowAppManagementPage()` for lacros.
-  bool show_app_link_in_app_window = false;
 #else
   bool show_app_link_in_app_window =
       base::FeatureList::IsEnabled(features::kDesktopPWAsWebAppSettingsPage);
@@ -65,6 +67,29 @@ absl::optional<AppId> GetAppIdForManagementLinkInWebContents(
   return helper->GetAppId();
 }
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+void ShowAppManagementPage(const AppId& app_id) {
+  auto* service = chromeos::LacrosService::Get();
+  if (!service || !service->IsAvailable<crosapi::mojom::AppServiceProxy>()) {
+    LOG(ERROR) << "AppServiceProxy not available.";
+    return;
+  }
+
+  auto remote_version =
+      service->GetInterfaceVersion(crosapi::mojom::AppServiceProxy::Uuid_);
+
+  if (remote_version < int{crosapi::mojom::AppServiceProxy::MethodMinVersions::
+                               kShowAppManagementPageMinVersion}) {
+    LOG(WARNING) << "Ash AppServiceProxy version " << remote_version
+                 << " does not support ShowAppManagementPage().";
+    return;
+  }
+
+  service->GetRemote<crosapi::mojom::AppServiceProxy>()->ShowAppManagementPage(
+      app_id);
+}
+#endif
+
 }  // namespace
 
 bool GetLabelIdsForAppManagementLinkInPageInfo(
@@ -94,8 +119,8 @@ bool HandleAppManagementLinkClickedInPageInfo(
       ash::settings::AppManagementEntryPoint::kPageInfoView);
   return true;
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  // TODO(crbug.com/1297868): implement for lacros.
-  return false;
+  ShowAppManagementPage(*app_id);
+  return true;
 #else
   chrome::ShowWebAppSettings(chrome::FindBrowserWithWebContents(web_contents),
                              *app_id, AppSettingsPageEntryPoint::kPageInfoView);
