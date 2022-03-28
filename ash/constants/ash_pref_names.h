24242a6a8e707c8816e92708c793639dab2080ba
Michael Martis <martis@chromium.org>
2022-03-28 01:15:52 +0000

    [Adaptive charging] Add inert preference.
    This CL adds an initial unused preference for adaptive charging. This
    unblocks work on enterprise management for the feature.
    
    Feature details: go/cros-charge
    
    Bug: b:216035280
    Change-Id: Ia9a743e6d0f2c9393454e8879e704428557703d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551936
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985802}

--- ash/constants/ash_pref_names.h
+++ ash/constants/ash_pref_names.h
@@ -261,6 +261,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kPowerAcScreenLockDelayMs[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kPowerAcIdleWarningDelayMs[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kPowerAcIdleDelayMs[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const char kPowerAdaptiveChargingEnabled[];
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kPowerBatteryScreenBrightnessPercent[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kPowerBatteryScreenDimDelayMs[];
