edef6a435656d0139e54b068098adf872a1ec4b1
Friedrich Horschig <fhorschig@chromium.org>
2022-03-28 13:46:11 +0000

    [UPM] Define upstream PasswordManagerResourceProvider
    Defined provider will be used by the PasswordGenerationDialogMediator
    to get icon identifier for the password generation dialog title.
    
    Upstream implementation uses non-colored icon if the
    UnifiedPasswordManagerAndroid feature is enabled.
    This behaviour could be overridden by the downstream implementation.
    
    This is part 1/3 of the password generation dialog icon update and does
    not change actual behaviour.
    
    Bug: 1271552
    Change-Id: I55cb79ce7727887afdc8fb5709b7ea573c40447b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412442
    Auto-Submit: Maxim Anufriev <maxan@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985943}

---
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerResourceProvider.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+import androidx.annotation.DrawableRes;
+
+/**
+ * Provides functions that choose the correct resource IDs for the password manager UI.
+ * Upstream resources may be overridden by a downstream implementation.
+ */
+interface PasswordManagerResourceProvider {
+    /**
+     * Returns the drawable id to be displayed as a password manager key icon.
+     *
+     * @return A resource file for the 24dp logo.
+     */
+    @DrawableRes
+    int getPasswordManagerIcon();
+}
---
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerResourceProviderImpl.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+import androidx.annotation.DrawableRes;
+
+/**
+ * Public version of {@link PasswordManagerResourceProviderImpl}.
+ * Downstream may provide a different implementation.
+ */
+class PasswordManagerResourceProviderImpl implements PasswordManagerResourceProvider {
+    @Override
+    public @DrawableRes int getPasswordManagerIcon() {
+        return R.drawable.ic_vpn_key_blue;
+    }
+}
