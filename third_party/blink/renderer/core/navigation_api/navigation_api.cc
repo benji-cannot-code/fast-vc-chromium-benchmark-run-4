43f3149923be055e6d3bb035148d1a1aee54626f
Nate Chapin <japhet@chromium.org>
2022-03-28 18:32:59 +0000

    NavigationApi: Fire the navigate event for <a download>
    Expose the download attribute on the NavigateEvent as the
    downloadRequest attribute.
    
    Bug: 1183545
    Change-Id: I0300b9ea8f9599f2148d8317fa1b14eec5c71e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547556
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986065}

--- third_party/blink/renderer/core/navigation_api/navigation_api.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api.cc
@@ -676,7 +676,8 @@ NavigationApi::DispatchResult NavigationApi::DispatchNavigateEvent(
     SerializedScriptValue* state_object,
     HistoryItem* destination_item,
     bool is_browser_initiated,
-    bool is_synchronously_committed) {
+    bool is_synchronously_committed,
+    const String& download_filename) {
   // TODO(japhet): The draft spec says to cancel any ongoing navigate event
   // before invoking DispatchNavigateEvent(), because not all navigations will
   // fire a navigate event, but all should abort an ongoing navigate event.
@@ -751,6 +752,7 @@ NavigationApi::DispatchResult NavigationApi::DispatchNavigateEvent(
   if (ongoing_navigation_)
     init->setInfo(ongoing_navigation_->GetInfo());
   init->setSignal(MakeGarbageCollected<AbortSignal>(GetSupplementable()));
+  init->setDownloadRequest(download_filename);
   auto* navigate_event = NavigateEvent::Create(
       GetSupplementable(), event_type_names::kNavigate, init);
   navigate_event->SetUrl(url);
