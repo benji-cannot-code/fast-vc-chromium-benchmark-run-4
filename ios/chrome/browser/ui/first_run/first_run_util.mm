3e62f7c27e899a5d0193aa4b08a7efa2a6cfd837
jlebel <jlebel@chromium.org>
2022-03-28 08:47:05 +0000

    [iOS] Clean up for UMA reporting methods
    This patch removes duplicated code by creating the following methods:
    void RecordMetricsReportingDefaultState();
    bool DefaultMetricsReportingCheckboxValue();
    
    Those methods are used by the old FRE and the new FRE. It will be used
    soon by the new new FRE.
    
    Change-Id: I41c4c1ca4b3ecbb6bc9087f6dc4ac576de5035f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545156
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985881}

--- ios/chrome/browser/ui/first_run/first_run_util.mm
+++ ios/chrome/browser/ui/first_run/first_run_util.mm
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#import "components/metrics/metrics_reporting_default_state.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #import "ios/chrome/app/tests_hook.h"
+#import "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/crash_report/crash_helper.h"
 #include "ios/chrome/browser/first_run/first_run.h"
@@ -37,6 +39,8 @@ NSString* const kChromeFirstRunUIWillFinishNotification =
 NSString* const kChromeFirstRunUIDidFinishNotification =
     @"kChromeFirstRunUIDidFinishNotification";
 
+constexpr BOOL kDefaultMetricsReportingCheckboxValue = YES;
+
 namespace {
 
 // Trampoline method for Bind to create the sentinel file.
@@ -46,9 +50,11 @@ void CreateSentinel() {
       FirstRun::CreateSentinel(&file_error);
   base::UmaHistogramEnumeration("FirstRun.Sentinel.Created", sentinel_created,
                                 FirstRun::SentinelResult::SENTINEL_RESULT_MAX);
-  if (sentinel_created == FirstRun::SentinelResult::SENTINEL_RESULT_FILE_ERROR)
+  if (sentinel_created ==
+      FirstRun::SentinelResult::SENTINEL_RESULT_FILE_ERROR) {
     base::UmaHistogramExactLinear("FirstRun.Sentinel.CreatedFileError",
                                   -file_error, -base::File::FILE_ERROR_MAX);
+  }
 }
 
 bool kFirstRunSentinelCreated = false;
@@ -151,4 +157,24 @@ bool ShouldPresentFirstRunExperience() {
     return false;
 
   return FirstRun::IsChromeFirstRun();
+}
+
+void RecordMetricsReportingDefaultState() {
+  // Record metrics reporting as opt-in/opt-out only once.
+  static dispatch_once_t once;
+  dispatch_once(&once, ^{
+    // Don't call RecordMetricsReportingDefaultState twice. This can happen if
+    // the app is quit before accepting the TOS, or via experiment settings.
+    if (metrics::GetMetricsReportingDefaultState(
+            GetApplicationContext()->GetLocalState()) !=
+        metrics::EnableMetricsDefault::DEFAULT_UNKNOWN) {
+      return;
+    }
+
+    metrics::RecordMetricsReportingDefaultState(
+        GetApplicationContext()->GetLocalState(),
+        kDefaultMetricsReportingCheckboxValue
+            ? metrics::EnableMetricsDefault::OPT_OUT
+            : metrics::EnableMetricsDefault::OPT_IN);
+  });
 }
--- ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.mm
+++ ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/metrics/metrics_pref_names.h"
-#include "components/metrics/metrics_reporting_default_state.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/web_resource/web_resource_pref_names.h"
@@ -51,13 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-
-// Default value for metrics reporting state. "YES" corresponding to "opt-out"
-// state.
-const BOOL kDefaultStatsCheckboxValue = YES;
-}
 
 @interface WelcomeToChromeViewController () <WelcomeToChromeViewDelegate>
 
@@ -100,23 +92,7 @@ const BOOL kDefaultStatsCheckboxValue = YES;
 @synthesize dispatcher = _dispatcher;
 
 + (BOOL)defaultStatsCheckboxValue {
-  // Record metrics reporting as opt-in/opt-out only once.
-  static dispatch_once_t once;
-  dispatch_once(&once, ^{
-    // Don't call RecordMetricsReportingDefaultState twice.  This can happen
-    // if the app is quit before accepting the TOS, or via experiment settings.
-    if (metrics::GetMetricsReportingDefaultState(
-            GetApplicationContext()->GetLocalState()) !=
-        metrics::EnableMetricsDefault::DEFAULT_UNKNOWN) {
-      return;
-    }
-
-    metrics::RecordMetricsReportingDefaultState(
-        GetApplicationContext()->GetLocalState(),
-        kDefaultStatsCheckboxValue ? metrics::EnableMetricsDefault::OPT_OUT
-                                   : metrics::EnableMetricsDefault::OPT_IN);
-  });
-  return kDefaultStatsCheckboxValue;
+  return kChromeFirstRunUIDidFinishNotification;
 }
 
 - (instancetype)initWithBrowser:(Browser*)browser
@@ -151,6 +127,7 @@ const BOOL kDefaultStatsCheckboxValue = YES;
 }
 
 - (void)loadView {
+  RecordMetricsReportingDefaultState();
   WelcomeToChromeView* welcomeToChromeView =
       [[WelcomeToChromeView alloc] initWithFrame:CGRectZero];
   [welcomeToChromeView setDelegate:self];
