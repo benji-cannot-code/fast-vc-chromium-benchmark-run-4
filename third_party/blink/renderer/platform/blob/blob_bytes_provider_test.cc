53d2008d13cdf0ab84f961fba7144e97c39b0167
Marijn Kruisselbrink <mek@chromium.org>
2022-03-28 20:12:14 +0000

    Change ownership of BlobBytesProvider.
    Rather than immediately passing ownership to a cross-thread
    SelfOwnedReceiver while retaining a raw pointer, instead maintain
    ownership in a unique_ptr as long as it is needed, only transferring
    ownership to a SelfOwnedReceiver when BlobData is done with the
    BlobBytesProvider.
    
    Also clean-up/tighten down sequence checks for BlobBytesProvider a bit.
    
    Bug: 1285234
    Change-Id: I7273e886a0bab2ae489b680d786991c9e4ff1dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553304
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986111}

--- third_party/blink/renderer/platform/blob/blob_bytes_provider_test.cc
+++ third_party/blink/renderer/platform/blob/blob_bytes_provider_test.cc
@@ -53,8 +53,7 @@ class BlobBytesProviderTest : public testing::Test {
 
   std::unique_ptr<BlobBytesProvider> CreateProvider(
       scoped_refptr<RawData> data = nullptr) {
-    auto result = BlobBytesProvider::CreateForTesting(
-        blink::scheduler::GetSequencedTaskRunnerForTesting());
+    auto result = std::make_unique<BlobBytesProvider>();
     if (data)
       result->AppendData(std::move(data));
     return result;
@@ -74,6 +73,8 @@ class BlobBytesProviderTest : public testing::Test {
 
 TEST_F(BlobBytesProviderTest, Consolidation) {
   auto data = CreateProvider();
+  DCHECK_CALLED_ON_VALID_SEQUENCE(data->sequence_checker_);
+
   data->AppendData(base::make_span("abc", 3));
   data->AppendData(base::make_span("def", 3));
   data->AppendData(base::make_span("ps1", 3));
