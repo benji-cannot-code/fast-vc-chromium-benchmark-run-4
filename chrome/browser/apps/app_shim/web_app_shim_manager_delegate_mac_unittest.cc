c49f63683b9fa0483ceeafa226cb6e9670cc8376
Evan Stade <estade@chromium.org>
2022-03-28 11:15:13 +0000

    Reland "File Handling multi launch"
    This reverts commit 4e3e4ea4a59bc247d4f4c2ad48b4fc70995d752f.
    
    No changes to original CL. The build failure was due to an earlier
    CL in the chain.
    
    Original change's description:
    > Revert "File Handling multi launch"
    >
    > This reverts commit bc3ef1ba9275bf83a408b2fe8b9169827625278e.
    >
    > Reason for revert: Looks like the culprit for new failures of many
    > TwoClientWebAppsIntegrationTestMacWinLinux tests.
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.14%20Tests/23414/overview
    >
    > Original change's description:
    > > File Handling multi launch
    > >
    > > Translate an app launch with multiple files handled by separate
    > > file handlers into multiple launches (i.e. multiple windows),
    > > rather than failing the launch if no single file handler accepts
    > > all file types present.
    > >
    > > The behavior on each OS between when a user selects multiple files
    > > and a command line for the PWA is crafted is slightly different on
    > > each platform --- see doc for details. As such, this primarily
    > > affects Linux for now, but will be more impactful when the new
    > > manifest member is added that controls whether an app wants to
    > > handle many files or just one in each launched context.*
    > >
    > > *See document linked from bug for more details.
    > >
    > > Bug: 1304003
    > > Change-Id: Iff88f8dd9be11b31967f9037543fd57ba007ef3f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3503850
    > > Commit-Queue: Evan Stade <estade@chromium.org>
    > > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > > Reviewed-by: Greg Thompson <grt@chromium.org>
    > > Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    > > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#984626}
    >
    > Bug: 1304003
    > Change-Id: I22e7c3fcd28e00045bf3f25ac7f0eda241d972cd
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545998
    > Auto-Submit: Fergal Daly <fergal@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Fergal Daly <fergal@google.com>
    > Commit-Queue: Fergal Daly <fergal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#984661}
    
    Bug: 1304003
    Change-Id: I1c24d583574b5e94cc6ba83faab6ad42e4e4a1b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549438
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985917}

--- chrome/browser/apps/app_shim/web_app_shim_manager_delegate_mac_unittest.cc
+++ chrome/browser/apps/app_shim/web_app_shim_manager_delegate_mac_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/test/fake_os_integration_manager.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
 
 namespace web_app {
@@ -106,8 +108,21 @@ class WebAppShimManagerDelegateTest : public WebAppTest {
     web_app::test::AwaitStartWebAppProviderAndSubsystems(profile());
 
     // Install a dummy app
-    app_id_ = web_app::test::InstallDummyWebApp(profile(), "WebAppTest",
-                                                GURL("https://testpwa.com/"));
+    auto web_app_info = std::make_unique<WebAppInstallInfo>();
+    web_app_info->title = u"WebAppTest";
+    web_app_info->start_url = GURL("https://testpwa.com/");
+    web_app_info->scope = GURL("https://testpwa.com/");
+    web_app_info->display_mode = blink::mojom::DisplayMode::kStandalone;
+
+    // Basic plain text format.
+    apps::FileHandler entry1;
+    entry1.action = GURL("https://testpwa.com/files");
+    entry1.accept.emplace_back();
+    entry1.accept[0].mime_type = "text/*";
+    entry1.accept[0].file_extensions.insert(".txt");
+    web_app_info->file_handlers.push_back(std::move(entry1));
+
+    app_id_ = test::InstallWebApp(profile(), std::move(web_app_info));
   }
 
  protected:
@@ -160,6 +175,8 @@ class WebAppShimManagerDelegateTest : public WebAppTest {
 
  private:
   web_app::AppId app_id_;
+  base::test::ScopedFeatureList scoped_feature_list_{
+      blink::features::kFileHandlingAPI};
 };
 
 TEST_F(WebAppShimManagerDelegateTest, LaunchApp) {
@@ -229,9 +246,9 @@ TEST_F(WebAppShimManagerDelegateTest, LaunchApp_ProtocolMailTo) {
 TEST_F(WebAppShimManagerDelegateTest, LaunchApp_ProtocolFile) {
   GURL protocol_handler_launch_url("file:///test_app_path/test_app_file.txt");
 
-  apps::AppLaunchParams expected_results =
-      CreateLaunchParams({base::FilePath("/test_app_path/test_app_file.txt")},
-                         absl::nullopt, absl::nullopt, GURL());
+  apps::AppLaunchParams expected_results = CreateLaunchParams(
+      {base::FilePath("/test_app_path/test_app_file.txt")}, absl::nullopt,
+      absl::nullopt, GURL("https://testpwa.com/files"));
 
   std::unique_ptr<MockDelegate> delegate = std::make_unique<MockDelegate>();
   WebAppShimManagerDelegate shim_manager(std::move(delegate));
@@ -273,7 +290,8 @@ TEST_F(WebAppShimManagerDelegateTest, LaunchApp_FileFullPath) {
   base::FilePath test_path(kTestPath);
 
   apps::AppLaunchParams expected_results =
-      CreateLaunchParams({test_path}, absl::nullopt, absl::nullopt, GURL());
+      CreateLaunchParams({test_path}, absl::nullopt, absl::nullopt,
+                         GURL("https://testpwa.com/files"));
 
   std::unique_ptr<MockDelegate> delegate = std::make_unique<MockDelegate>();
   WebAppShimManagerDelegate shim_manager(std::move(delegate));
@@ -295,7 +313,8 @@ TEST_F(WebAppShimManagerDelegateTest, LaunchApp_FileRelativePath) {
   base::FilePath test_path(kTestPath);
 
   apps::AppLaunchParams expected_results =
-      CreateLaunchParams({test_path}, absl::nullopt, absl::nullopt, GURL());
+      CreateLaunchParams({test_path}, absl::nullopt, absl::nullopt,
+                         GURL("https://testpwa.com/files"));
 
   std::unique_ptr<MockDelegate> delegate = std::make_unique<MockDelegate>();
   WebAppShimManagerDelegate shim_manager(std::move(delegate));
@@ -318,7 +337,7 @@ TEST_F(WebAppShimManagerDelegateTest, LaunchApp_ProtocolAndFileHandlerMixed) {
   base::FilePath test_path(kTestPath);
 
   apps::AppLaunchParams expected_results = CreateLaunchParams(
-      {test_path}, absl::nullopt, protocol_handler_launch_url, GURL());
+      {}, absl::nullopt, protocol_handler_launch_url, GURL());
   expected_results.launch_source =
       apps::mojom::LaunchSource::kFromProtocolHandler;
 
@@ -345,8 +364,7 @@ TEST_F(WebAppShimManagerDelegateTest,
   base::FilePath test_path(kTestPath);
 
   apps::AppLaunchParams expected_results = CreateLaunchParams(
-      {test_path, base::FilePath("/test_app_path/test_app_file.txt")},
-      absl::nullopt, protocol_handler_launch_url, GURL());
+      {}, absl::nullopt, protocol_handler_launch_url, GURL());
   expected_results.launch_source =
       apps::mojom::LaunchSource::kFromProtocolHandler;
 
