c294e1d77b47e2953b1561ba5a5f5d157ff29278
kylechar <kylechar@chromium.org>
2022-03-28 15:54:09 +0000

    Remove GLRenderer from viz_unittests
    viz_unittests will no longer initialize/use GLRenderer in any tests if
    |enable_gl_renderer_tests| is false. Also set that GN arg to false on
    all platforms since we are starting to turn down GLRenderer.
    
    Note that other tests, at least cc_unittests, still end up using
    GLRenderer with enable_gl_renderer_tests=false and need to be fixed
    still.
    
    Bug: 1247756
    Change-Id: I911da8bd8ff9dea0706d1417d8a80c9e18a6369f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550761
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985993}

--- components/viz/test/test_output_surface_provider.cc
+++ components/viz/test/test_output_surface_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/display_compositor_memory_and_task_controller.h"
 #include "components/viz/service/display/software_output_device.h"
 #include "components/viz/test/fake_output_surface.h"
+#include "components/viz/test/fake_skia_output_surface.h"
 
 namespace viz {
 
@@ -33,7 +34,7 @@ std::unique_ptr<OutputSurface> TestOutputSurfaceProvider::CreateOutputSurface(
     const RendererSettings& renderer_settings,
     const DebugRendererSettings* debug_settings) {
   if (gpu_compositing) {
-    return FakeOutputSurface::Create3d();
+    return FakeSkiaOutputSurface::Create3d();
   } else {
     return FakeOutputSurface::CreateSoftware(
         std::make_unique<SoftwareOutputDevice>());
