c499e07e49fba228ba307a1bba125b6b63d5dfce
Andrew Paseltiner <apaseltiner@chromium.org>
2022-03-28 22:45:58 +0000

    Buffer attribution triggers for 5 seconds during source registration
    Otherwise, in the scenario in which a navigation source is registered
    and a trigger is registered immediately after, the browser may process
    the registrations in an arbitrary order, making it possible for
    attribution to fail due to no matching sources. The 5-second delay
    on processing triggers makes this less likely, but doesn't completely
    eliminate the problem.
    
    Triggers only need to be buffered if at least one data host is connected
    that may yet register a source. That includes undetermined receivers
    (ones that haven't registered anything yet) and navigation-source
    receivers.
    
    We buffer at most 30 triggers to avoid unbounded memory usage, though
    inherently this may cause data loss.
    
    We also remove the explicit waits in AttributionsBrowserTest
    for source storage because the delay in this CL makes it unnecessary.
    
    Bug: 1309176, 1309173, 1307082
    Change-Id: I3b2a0c1fbfcc962c3eec87172960825be5f0d9bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547458
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986184}

--- content/browser/attribution_reporting/attributions_browsertest.cc
+++ content/browser/attribution_reporting/attributions_browsertest.cc
@@ -7,21 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/run_loop.h"
-#include "base/scoped_observation.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/values_test_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/values.h"
-#include "content/browser/attribution_reporting/attribution_manager.h"
 #include "content/browser/attribution_reporting/attribution_manager_impl.h"
 #include "content/browser/attribution_reporting/attribution_test_utils.h"
-#include "content/browser/attribution_reporting/storable_source.h"
-#include "content/browser/storage_partition_impl.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/network_service_instance.h"
-#include "content/public/browser/storage_partition.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -49,7 +44,6 @@ namespace content {
 
 namespace {
 
-using ::testing::_;
 using ::testing::Return;
 
 constexpr char kBaseDataDir[] = "content/test/data/";
@@ -184,35 +178,12 @@ class AttributionsBrowserTest : public ContentBrowserTest {
 
   net::EmbeddedTestServer* https_server() { return https_server_.get(); }
 
-  AttributionManager* attribution_manager() {
-    return static_cast<StoragePartitionImpl*>(
-               web_contents()
-                   ->GetBrowserContext()
-                   ->GetDefaultStoragePartition())
-        ->GetAttributionManager();
-  }
-
-  void RegisterSourceAndWaitForStorage(const GURL& attribution_src_url,
-                                       bool use_js = false) {
-    MockAttributionObserver observer;
-    base::ScopedObservation<AttributionManager, AttributionObserver>
-        observation(&observer);
-    observation.Observe(attribution_manager());
-
-    base::RunLoop loop;
-    EXPECT_CALL(observer, OnSourceHandled(_, StorableSource::Result::kSuccess))
-        .WillOnce([&]() { loop.Quit(); });
-
+  void RegisterSource(const GURL& attribution_src_url, bool use_js = false) {
     base::StringPiece register_js_template =
         use_js ? "window.attributionReporting.registerSource($1);"
                : "createAttributionSrcImg($1);";
     EXPECT_TRUE(ExecJs(web_contents(),
                        JsReplace(register_js_template, attribution_src_url)));
-
-    // Wait until the source has been stored before registering the trigger;
-    // otherwise the trigger could be processed before the source, in which case
-    // there would be no matching source: crbug.com/1307082.
-    loop.Run();
   }
 
  private:
@@ -709,7 +680,7 @@ IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
       "a.test", "/attribution_reporting/page_with_impression_creator.html");
   EXPECT_TRUE(NavigateToURL(web_contents(), impression_url));
 
-  RegisterSourceAndWaitForStorage(https_server()->GetURL(
+  RegisterSource(https_server()->GetURL(
       "a.test",
       "/attribution_reporting/register_source_headers_debug_key.html"));
 
@@ -742,7 +713,7 @@ IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
       "a.test", "/attribution_reporting/page_with_impression_creator.html");
   EXPECT_TRUE(NavigateToURL(web_contents(), impression_url));
 
-  RegisterSourceAndWaitForStorage(
+  RegisterSource(
       https_server()->GetURL("a.test",
                              "/attribution_reporting/"
                              "register_source_headers_debug_key_cookie.html"));
@@ -777,7 +748,7 @@ IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
       "a.test", "/attribution_reporting/page_with_impression_creator.html");
   EXPECT_TRUE(NavigateToURL(web_contents(), impression_url));
 
-  RegisterSourceAndWaitForStorage(
+  RegisterSource(
       https_server()->GetURL(
           "a.test", "/attribution_reporting/register_source_headers.html"),
       /*use_js=*/true);
@@ -818,7 +789,7 @@ IN_PROC_BROWSER_TEST_F(AttributionsBrowserTest,
           "b.test",
           "/attribution_reporting/page_with_impression_creator.html")));
 
-  RegisterSourceAndWaitForStorage(https_server()->GetURL(
+  RegisterSource(https_server()->GetURL(
       "a.test", "/attribution_reporting/register_source_headers.html"));
 
   EXPECT_TRUE(NavigateToURL(
@@ -906,7 +877,7 @@ IN_PROC_BROWSER_TEST_F(
           "b.test",
           "/attribution_reporting/page_with_impression_creator.html")));
 
-  RegisterSourceAndWaitForStorage(https_server()->GetURL(
+  RegisterSource(https_server()->GetURL(
       "a.test", "/attribution_reporting/register_source_headers.html"));
 
   EXPECT_TRUE(NavigateToURL(
