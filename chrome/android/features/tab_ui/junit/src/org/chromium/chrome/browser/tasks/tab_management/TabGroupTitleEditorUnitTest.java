76135d51b1ff7ed7658f4753fcab5a1146ad59e8
Ben Joyce <bjoyce@google.com>
2022-03-28 20:52:49 +0000

    Update tab junit tests to use baserobolectrictestrunner.
    Bug: 1273881
    Change-Id: I07b516283e0871cefd709d3d7356c34bb72f0d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553681
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986134}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupTitleEditorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupTitleEditorUnitTest.java
@@ -24,6 +24,7 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.UserDataHost;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabImpl;
 import org.chromium.chrome.browser.tab.state.CriticalPersistedTabData;
@@ -33,7 +34,6 @@ import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
 import org.chromium.chrome.test.util.browser.Features;
-import org.chromium.testing.local.LocalRobolectricTestRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,7 +45,7 @@ import java.util.Map;
  * Tests for {@link TabGroupTitleEditor}.
  */
 @SuppressWarnings({"ArraysAsListWithZeroOrOneArgument", "ResultOfMethodCallIgnored"})
-@RunWith(LocalRobolectricTestRunner.class)
+@RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class TabGroupTitleEditorUnitTest {
     @Rule
@@ -275,5 +275,4 @@ public class TabGroupTitleEditorUnitTest {
             doReturn(rootId).when(criticalPersistedTabData).getRootId();
         }
     }
-
 }
