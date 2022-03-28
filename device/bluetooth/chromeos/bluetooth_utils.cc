07cd262f72356305db93f0063f70664e8efe609a
Eric Willigers <ericwilligers@chromium.org>
2022-03-28 22:55:28 +0000

    device/bluetooth: Include chromeos_buildflags
    Source files using OS_CHROME_... now include
    build/chromeos_buildflags.h
    
    Bug: 1246565
    Change-Id: Id718771db9a93a87bc69ef3c79e60d89931868b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554366
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986189}

--- device/bluetooth/chromeos/bluetooth_utils.cc
+++ device/bluetooth/chromeos/bluetooth_utils.cc
@@ -15,11 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
+#include "build/chromeos_buildflags.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "device/base/features.h"
 #include "device/bluetooth/floss/floss_features.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-
-#include "device/base/features.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_switches.h"
