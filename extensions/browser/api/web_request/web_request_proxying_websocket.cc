f77261c2519c9e840ef241543a6b93891a157884
Alex Moshchuk <alexmos@chromium.org>
2022-03-28 19:37:14 +0000

    Support site isolation for <webview> tags in WebViewRendererState.
    WebViewRendererState is a cross-thread singleton object that tracks
    various information about all <webview> guest instances.  In
    particular, this is used for tracking content scripts injected into
    each guest.  It also allows looking up guest properties like partition
    ID, embedder process ID, or embedder's host.
    
    Currently, the code that manages this object assumes that each
    <webview> will always be associated with just one {process_id,
    render_view_routing_id} tuple.  With <webview> site isolation, this
    assumption no longer holds, as a single <webview> can navigate across
    multiple processes, and further may embed OOPIFs which would also be
    considered guest instances for the purposes of tracking content
    scripts.
    
    This CL adds support to track cross-process instances of <webview> in
    WebViewRendererState.  To do this, it (1) switches maps to work with
    RenderFrame routing IDs instead of RenderView routing IDs, and (2)
    listens to RenderFrame(Created,Deleted) to update the maps when
    individual guest frames are created or destroyed.  This allows content
    script and other WebViewRendererState information to be queried
    properly following cross-process navigations in any <webview> frame.
    
    This refactor only matters when features::kSiteIsolationForGuests is
    on; it should introduce no changes in behavior when it's off (which is
    currently the default), as multiple attempts to add the same guest
    instance are ignored.  (Without kSiteIsolationForGuests, all guest
    navigations used to stay in the same RenderFrameHost, except when a
    <webview> crashes and reloads, in which case we swap to a new
    RenderFrameHost but keep the old RenderViewHost.)
    
    Updating from view to frame routing IDs involved a few straightforward
    plumbing updates, many in WebRequest API.  Note that the frame routing
    ID was already plumbed in most affected places and was (likely
    erroneously) named as frame_id in some places.  Those places were
    renamed to frame_routing_id to avoid confusion with FrameId (where
    main frames have value 0 and subframes IDs correspond to FrameTreeNode
    IDs) that's actually exposed through extension APIs.
    
    Bug: 1267977
    Change-Id: Ib56e85a597b8d67b003b86cac7d155053c3d39cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537735
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986092}

--- extensions/browser/api/web_request/web_request_proxying_websocket.cc
+++ extensions/browser/api/web_request/web_request_proxying_websocket.cc
@@ -71,7 +71,6 @@ WebRequestProxyingWebSocket::WebRequestProxyingWebSocket(
           process_id,
           render_frame_id,
           nullptr,
-          MSG_ROUTING_NONE,
           request,
           /*is_download=*/false,
           /*is_async=*/true,
