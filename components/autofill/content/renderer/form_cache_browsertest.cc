e110fad468e8269426b48a795e8ca780570f3e6b
Christoph Schwering <schwering@google.com>
2022-03-28 10:40:14 +0000

    [Autofill] Extract also invisible child frames.
    This CL relaxes the requirement for FormData::child_frames to be
    populated: previously, no invisible iframes were extracted; now
    only invisible ad iframes are not extracted.
    
    The rationale is that invisible may become visible only after the
    FormData has been extracted from the page. Since Autofill does not
    necessarily re-extract the FormData between the frame becoming
    visible and the user focusing a field, Autofill may be working with
    an outdated flattened FormData at the time the user focuses the field.
    
    Bug: 1117028, 1187842
    Change-Id: Iabeb5f4cda5c11875010e638fde831b08499457c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525237
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#985907}

--- components/autofill/content/renderer/form_cache_browsertest.cc
+++ components/autofill/content/renderer/form_cache_browsertest.cc
@@ -327,6 +327,7 @@ TEST_P(FormCacheIframeBrowserTest, ExtractFramesTwice) {
   EXPECT_TRUE(forms.removed_forms.empty());
 }
 
+// TODO(crbug.com/1117028) Adjust expectations when we omit invisible iframes.
 TEST_P(FormCacheIframeBrowserTest, ExtractFramesAfterVisibilityChange) {
   LoadHTML(R"(
     <form id="form1">
@@ -352,7 +353,8 @@ TEST_P(FormCacheIframeBrowserTest, ExtractFramesAfterVisibilityChange) {
   FormCache form_cache(GetMainFrame());
   FormCache::UpdateFormCacheResult forms =
       form_cache.ExtractNewForms(/*field_data_manager=*/nullptr);
-  EXPECT_TRUE(forms.updated_forms.empty());
+  EXPECT_THAT(forms.updated_forms,
+              UnorderedElementsAre(HasId(FormRendererId()), HasName("form1")));
   EXPECT_TRUE(forms.removed_forms.empty());
 
   iframe1.SetAttribute("style", "display: block;");
@@ -364,8 +366,7 @@ TEST_P(FormCacheIframeBrowserTest, ExtractFramesAfterVisibilityChange) {
   ASSERT_GT(GetSize(iframe3), 0);
 
   forms = form_cache.ExtractNewForms(nullptr);
-  EXPECT_THAT(forms.updated_forms,
-              UnorderedElementsAre(HasId(FormRendererId()), HasName("form1")));
+  EXPECT_TRUE(forms.updated_forms.empty());
   EXPECT_TRUE(forms.removed_forms.empty());
 
   iframe2.SetAttribute("style", "display: none;");
@@ -376,8 +377,8 @@ TEST_P(FormCacheIframeBrowserTest, ExtractFramesAfterVisibilityChange) {
   ASSERT_LE(GetSize(iframe3), 0);
 
   forms = form_cache.ExtractNewForms(nullptr);
-  EXPECT_THAT(forms.updated_forms, ElementsAre(HasName("form1")));
-  EXPECT_THAT(forms.removed_forms, ElementsAre(FormRendererId()));
+  EXPECT_TRUE(forms.updated_forms.empty());
+  EXPECT_TRUE(forms.removed_forms.empty());
 }
 
 TEST_P(ParameterizedFormCacheBrowserTest, ExtractFormsAfterModification) {
