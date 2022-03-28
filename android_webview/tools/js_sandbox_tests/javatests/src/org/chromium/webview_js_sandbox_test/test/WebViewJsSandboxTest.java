7902021d74fc9a381ac3d98aac5e6c42de8e2649
Abhijith Nair <abhijithnair@chromium.org>
2022-03-28 20:34:38 +0000

    Write instrumentation tests for Webview JS Sandbox
    These tests are similar to UI tests such that they compile and install
    a local version of WebView and set it as provider before beginning the
    tests. The test uses actual IPC calls to the webview provider package
    and is a more complete integration test as compared to
    webview_instrumentation_test_apk test.
    
    Bug: 1309447
    Change-Id: I0b6fb009e48b07ca6615052ee20fa69f9971e1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550196
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986120}

--- android_webview/tools/js_sandbox_tests/javatests/src/org/chromium/webview_js_sandbox_test/test/WebViewJsSandboxTest.java
+++ android_webview/tools/js_sandbox_tests/javatests/src/org/chromium/webview_js_sandbox_test/test/WebViewJsSandboxTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.android_webview.test.services;
+package org.chromium.webview_js_sandbox_test.test;
 
 import androidx.test.filters.MediumTest;
 
@@ -13,12 +13,12 @@ import org.junit.runner.RunWith;
 
 import org.chromium.android_webview.js.browser.AwJsContext;
 import org.chromium.android_webview.js.browser.AwJsSandbox;
-import org.chromium.android_webview.test.AwJUnit4ClassRunner;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
 
 /** Instrumentation test for JsSandboxService. */
-@RunWith(AwJUnit4ClassRunner.class)
-public class JsSandboxServiceTest {
+@RunWith(BaseJUnit4ClassRunner.class)
+public class WebViewJsSandboxTest {
     private class TestExecutionCallback implements AwJsContext.ExecutionCallback {
         public CallbackHelper helper = new CallbackHelper();
         public String result;
@@ -44,7 +44,7 @@ public class JsSandboxServiceTest {
         final String expected = "PASS";
         TestExecutionCallback callback = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting((AwJsSandbox jsSandbox) -> {
+        AwJsSandbox.newConnectedInstance((AwJsSandbox jsSandbox) -> {
             AwJsContext jsContext = jsSandbox.createContext();
             jsContext.evaluateJavascript(code, callback);
         });
@@ -60,7 +60,7 @@ public class JsSandboxServiceTest {
         final String expected = "PASS";
         TestExecutionCallback callback = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting((AwJsSandbox jsSandbox) -> {
+        AwJsSandbox.newConnectedInstance((AwJsSandbox jsSandbox) -> {
             AwJsContext jsContext1 = jsSandbox.createContext();
             AwJsContext jsContext2 = jsSandbox.createContext();
             jsContext1.close();
@@ -82,7 +82,7 @@ public class JsSandboxServiceTest {
         TestExecutionCallback callback1 = new TestExecutionCallback();
         TestExecutionCallback callback2 = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting((AwJsSandbox jsSandbox) -> {
+        AwJsSandbox.newConnectedInstance((AwJsSandbox jsSandbox) -> {
             AwJsContext jsContext1 = jsSandbox.createContext();
             jsContext1.evaluateJavascript(code1, callback1);
             AwJsContext jsContext2 = jsSandbox.createContext();
@@ -107,7 +107,7 @@ public class JsSandboxServiceTest {
         TestExecutionCallback callback1 = new TestExecutionCallback();
         TestExecutionCallback callback2 = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting((AwJsSandbox jsSandbox) -> {
+        AwJsSandbox.newConnectedInstance((AwJsSandbox jsSandbox) -> {
             AwJsContext jsContext1 = jsSandbox.createContext();
             jsContext1.evaluateJavascript(code1, callback1);
             AwJsContext jsContext2 = jsSandbox.createContext();
@@ -132,7 +132,7 @@ public class JsSandboxServiceTest {
         TestExecutionCallback callback1 = new TestExecutionCallback();
         TestExecutionCallback callback2 = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting((AwJsSandbox jsSandbox) -> {
+        AwJsSandbox.newConnectedInstance((AwJsSandbox jsSandbox) -> {
             AwJsContext jsContext1 = jsSandbox.createContext();
             jsContext1.evaluateJavascript(code1, callback1);
             jsContext1.evaluateJavascript(code2, callback2);
@@ -153,7 +153,7 @@ public class JsSandboxServiceTest {
         final String contains = "SyntaxError";
         TestExecutionCallback callback = new TestExecutionCallback();
 
-        AwJsSandbox.newConnectedInstanceForTesting(jsSandbox -> {
+        AwJsSandbox.newConnectedInstance(jsSandbox -> {
             AwJsContext jsContext = jsSandbox.createContext();
             jsContext.evaluateJavascript(code, callback);
         });
