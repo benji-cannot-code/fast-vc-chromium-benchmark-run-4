696d7acedd36a33e221515be49b9dd3e7a9f68ff
Jason Thai <jasontt@chromium.org>
2022-03-28 22:37:09 +0000

    personalization: style fixes
    * Home and chevron buttons should be round
    * chevron buttons should be grey
    * Add padding to wallpaper and screensaver label
    * Reorder theme button style so active state is displayed after pressed
    
    BUG=b/226522774
    BUG=b/226529127
    BUG=b/225197750
    Bug=b/226528121
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I3db5f678277a8231b20aa94092901584ce50959b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553309
    Reviewed-by: Yuki Awano <yawano@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986179}

--- ash/webui/personalization_app/resources/trusted/personalization_main_element.ts
+++ ash/webui/personalization_app/resources/trusted/personalization_main_element.ts
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the personalization hub.
  */
 
+import './cros_button_style.js';
+
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {getTemplate} from './personalization_main_element.html.js';
 import {isAmbientModeAllowed, Paths, PersonalizationRouter} from './personalization_router_element.js';
