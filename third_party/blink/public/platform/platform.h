39eaaa9ca83ce25e27f87d5a15b6538c6bbb2257
Stephan Hartmann <stha09@googlemail.com>
2022-03-28 23:36:18 +0000

    libstdc++: move definition of CreateDedicatedWorkerHostFactoryClient
    Follow up of https://crrev.com/c/3550586 to avoid large header
    inclusion.
    
    Bug: 957519
    Change-Id: Ic43cab209043a986c98262850c33c129ddbc2b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554970
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986221}

--- third_party/blink/public/platform/platform.h
+++ third_party/blink/public/platform/platform.h
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_code_cache_loader.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_data.h"
-#include "third_party/blink/public/platform/web_dedicated_worker_host_factory_client.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_v8_value_converter.h"
 #include "third_party/blink/public/platform/websocket_handshake_throttle_provider.h"
@@ -135,6 +134,7 @@ class WebAudioBus;
 class WebAudioLatencyHint;
 class WebCrypto;
 class WebDedicatedWorker;
+class WebDedicatedWorkerHostFactoryClient;
 class WebGraphicsContext3DProvider;
 class WebLocalFrame;
 class WebMediaCapabilitiesClient;
@@ -711,9 +711,7 @@ class BLINK_PLATFORM_EXPORT Platform {
 
   virtual std::unique_ptr<WebDedicatedWorkerHostFactoryClient>
   CreateDedicatedWorkerHostFactoryClient(WebDedicatedWorker*,
-                                         const BrowserInterfaceBrokerProxy&) {
-    return nullptr;
-  }
+                                         const BrowserInterfaceBrokerProxy&);
   virtual void DidStartWorkerThread() {}
   virtual void WillStopWorkerThread() {}
   virtual void WorkerContextCreated(const v8::Local<v8::Context>& worker) {}
