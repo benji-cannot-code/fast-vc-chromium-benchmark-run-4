b2271548fb27cb0dff107af5ae0c4904f52edcbc
David Dorwin <ddorwin@chromium.org>
2022-03-28 23:02:17 +0000

    [fuchsia] enable_pdf = true
    This change builds PDF-specific code on Fuchsia, but Chrome does not yet
    support rendering PDF files because that depends on code behind the
    `BUILDFLAG(ENABLE_PLUGINS)`. This is tracked in crbug.com/1306610.
    
    Since `enable_basic_printing` is now enabled on Fuchsia, this change
    also causes `enable_print_preview` to be true on Fuchsia, though this
    feature may not work either.
    
    Bug: 1273277, 1306610, 1272829
    Change-Id: I7843a7c92536822e131603d7565b177372a03c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518625
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986196}

--- chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "components/prefs/pref_service.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
-#include "content/public/browser/plugin_service.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
