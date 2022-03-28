3d6a7451291a7d2c40589c40648d6efd8a0cee68
Sinan Sahin <sinansahin@google.com>
2022-03-28 18:50:30 +0000

    Revert "Disabled failing tests in TabbedNavigationBarColorControllerTest"
    This reverts commit 01a89c684fb2dd13bc6bad02a1809fc4f546da26.
    
    Reason for revert: The tests have been fixed in https://chromium-review.googlesource.com/c/chromium/src/+/3537808
    
    Original change's description:
    > Disabled failing tests in TabbedNavigationBarColorControllerTest
    >
    > Bug: 1308777
    > Change-Id: I864d067f3522c1bf2374b1b68f289fac765f1daa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538825
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#983484}
    
    Bug: 1308777
    Change-Id: Ieb678ca5ab09ec10085d06793767c7463d3cb781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547296
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#986070}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
@@ -25,7 +25,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -70,7 +69,6 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleOverview() {
         assertEquals("Navigation bar should be white before entering overview mode.",
                 mLightNavigationColor, mWindow.getNavigationBarColor());
@@ -90,7 +88,6 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleIncognito() {
         assertEquals("Navigation bar should be white on normal tabs.", mLightNavigationColor,
                 mWindow.getNavigationBarColor());
@@ -110,7 +107,6 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleFullscreen() throws TimeoutException {
         assertEquals("Navigation bar should be white before entering fullscreen mode.",
                 mLightNavigationColor, mWindow.getNavigationBarColor());
