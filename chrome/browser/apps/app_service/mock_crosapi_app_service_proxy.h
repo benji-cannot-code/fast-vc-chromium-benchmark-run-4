04e3070953fe4bac8c2c5b0dff61e49440510817
Leonard Grey <lgrey@chromium.org>
2022-03-28 19:27:01 +0000

    Revert "Intents: Lacros support for Supported Links Infobar"
    This reverts commit 8c48faa026e0b62e0597a96fa18125bb418f8557.
    
    Reason for revert: Linux Chromium OS ASan LSan Tests
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/42358/test-results
    
    Original change's description:
    > Intents: Lacros support for Supported Links Infobar
    >
    > This allows the supported links infobar to be launched in Lacros. The
    > infobar calls the SetSupportedLinksPreference App Service method, and
    > so this method needs to be implemented in crosapi. If ash-chrome is
    > older than ash-chrome and doesn't support the crosapi method, the
    > infobar will not appear.
    >
    > Bug: 1293173
    > Change-Id: I50fde627383d12731f8fb6f6fe34ce4936532f10
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546060
    > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#986012}
    
    Bug: 1293173
    Change-Id: I75ea8283e89d02fcbaf1095f7ea5e478816ceecf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553576
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Owners-Override: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986085}

--- chrome/browser/apps/app_service/mock_crosapi_app_service_proxy.h
+++ chrome/browser/apps/app_service/mock_crosapi_app_service_proxy.h
@@ -23,9 +23,6 @@ class MockCrosapiAppServiceProxy : public crosapi::mojom::AppServiceProxy {
   const std::vector<crosapi::mojom::LaunchParamsPtr>& launched_apps() const {
     return launched_apps_;
   }
-  const std::vector<std::string>& supported_link_apps() const {
-    return supported_link_apps_;
-  }
 
  private:
   // crosapi::mojom::AppServiceProxy:
@@ -41,10 +38,8 @@ class MockCrosapiAppServiceProxy : public crosapi::mojom::AppServiceProxy {
   void AddPreferredApp(const std::string& app_id,
                        crosapi::mojom::IntentPtr intent) override;
   void ShowAppManagementPage(const std::string& app_id) override;
-  void SetSupportedLinksPreference(const std::string& app_id) override;
 
   std::vector<crosapi::mojom::LaunchParamsPtr> launched_apps_;
-  std::vector<std::string> supported_link_apps_;
   std::unique_ptr<base::RunLoop> run_loop_;
 };
 }  // namespace apps
--- chrome/browser/apps/app_service/subscriber_crosapi.h
+++ chrome/browser/apps/app_service/subscriber_crosapi.h
@@ -68,7 +68,6 @@ class SubscriberCrosapi : public KeyedService,
   void AddPreferredApp(const std::string& app_id,
                        crosapi::mojom::IntentPtr intent) override;
   void ShowAppManagementPage(const std::string& app_id) override;
-  void SetSupportedLinksPreference(const std::string& app_id) override;
 
   void OnSubscriberDisconnected();
 
