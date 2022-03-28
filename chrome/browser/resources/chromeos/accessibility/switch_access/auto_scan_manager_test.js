40249f8617b472ca4ecb11ba0b972fa5cfd3aaa9
Tim Sergeant <tsergeant@chromium.org>
2022-03-28 01:46:33 +0000

    Revert "[a11y-extensions] Refactor runWithLoadedTree to be async"
    This reverts commit 83cfaf020896c2ed27cb11d36dbfb6e0cd94351a.
    
    Reason for revert: ChromeVoxTutorialTest.OnlyLessonTest is failing on linux-chromeos-rel: 
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/55795/overview
    
    Original change's description:
    > [a11y-extensions] Refactor runWithLoadedTree to be async
    >
    > This change converts the test function runWithLoadedTree from a
    > callback-based method to the ES6 async/await paradigm. This greatly
    > improves the readability of tests, and the readability of
    > runWithLoadedTree itself.
    >
    > The majority of this change is straightforward migration -- e.g.
    > making the top-level function async, removing the callback braces, etc.
    >
    > This change is a pure refactor.
    >
    > AX-Relnotes: n/a.
    > Bug: none
    > Test: automated tests for chromevox, select to speak, switch access...
    > Change-Id: If02ca57552f8cff9c1acad0e7d4d0587be08e2f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3511256
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Reviewed-by: Josiah Krutz <josiahk@google.com>
    > Cr-Commit-Position: refs/heads/main@{#985573}
    
    Bug: none
    Change-Id: I7f9ed5d93ed0d3966bec375f85984200d2426b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552063
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985808}

--- chrome/browser/resources/chromeos/accessibility/switch_access/auto_scan_manager_test.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/auto_scan_manager_test.js
@@ -55,7 +55,7 @@ SwitchAccessAutoScanManagerTest = class extends SwitchAccessE2ETest {
 };
 
 TEST_F('SwitchAccessAutoScanManagerTest', 'SetEnabled', function() {
-  this.runWithLoadedDesktop(() => {
+  this.runWithLoadedTree('', () => {
     assertFalse(
         AutoScanManager.instance.isRunning_(),
         'Auto scan manager is running prematurely');
