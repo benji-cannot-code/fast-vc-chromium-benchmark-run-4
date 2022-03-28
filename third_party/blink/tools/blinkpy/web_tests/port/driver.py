f6794b1c5bd797a049fefe0d198a936ee4cca3c9
Patrick Meiring <meiring@google.com>
2022-03-28 07:46:55 +0000

    [blinkpy] Report failure reasons for browser crashes.
    If FATAL log messages are visible in the test stderr log, report
    these as the test failure reason. This is usually occurs because
    a (D)Check fails while running the test.
    
    Failure reasons will be used in Weetbix to perform failure
    clustering and to assist users link bugs to known failures using
    failure association rules. They will also be surfaced in MILO.
    
    BUG=1243174
    TEST=third_party/blink/tools/run_blinkpy_tests.py
    
    Change-Id: I2f140fb975b1b00e48d434e05fdb8be554f0c918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551478
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Commit-Queue: Patrick Meiring <meiring@google.com>
    Cr-Commit-Position: refs/heads/main@{#985863}

--- third_party/blink/tools/blinkpy/web_tests/port/driver.py
+++ third_party/blink/tools/blinkpy/web_tests/port/driver.py
@@ -121,7 +121,7 @@ class DriverOutput(object):
                  test_time=0,
                  measurements=None,
                  timeout=False,
-                 error='',
+                 error=b'',
                  crashed_process_name='??',
                  crashed_pid=None,
                  crash_log=None,
