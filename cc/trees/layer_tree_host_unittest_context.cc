7f39b594febe44b81eda6ce405ae3225f6df6e66
kylechar <kylechar@chromium.org>
2022-03-28 23:46:21 +0000

    Stop using GLRenderer in cc_unittestss
    Ensure that cc_unittests doesn't use GLRenderer when
    |enable_gl_renderer_tests| is false. Biggest change is making default
    renderer type for LayerTreeTest by SkiaRenderer/GL but also fix a few
    other tests.
    
    Bug: 1247756
    Change-Id: I7f2dc460a4d1f92a2487da80e1d52a9110e14c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555887
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986226}

--- cc/trees/layer_tree_host_unittest_context.cc
+++ cc/trees/layer_tree_host_unittest_context.cc
@@ -1603,6 +1603,9 @@ SINGLE_AND_MULTI_THREAD_TEST_F(TileResourceFreedIfLostWhileExported);
 
 class SoftwareTileResourceFreedIfLostWhileExported : public LayerTreeTest {
  protected:
+  SoftwareTileResourceFreedIfLostWhileExported()
+      : LayerTreeTest(viz::RendererType::kSoftware) {}
+
   std::unique_ptr<TestLayerTreeFrameSink> CreateLayerTreeFrameSink(
       const viz::RendererSettings& renderer_settings,
       double refresh_rate,
@@ -1612,14 +1615,6 @@ class SoftwareTileResourceFreedIfLostWhileExported : public LayerTreeTest {
     // Induce software compositing in cc.
     return LayerTreeTest::CreateLayerTreeFrameSink(
         renderer_settings, refresh_rate, nullptr, nullptr);
-  }
-
-  std::unique_ptr<viz::OutputSurface> CreateDisplayOutputSurfaceOnThread(
-      scoped_refptr<viz::ContextProvider> compositor_context_provider)
-      override {
-    // Induce software compositing in the display compositor.
-    return viz::FakeOutputSurface::CreateSoftware(
-        std::make_unique<viz::SoftwareOutputDevice>());
   }
 
   void SetupTree() override {
