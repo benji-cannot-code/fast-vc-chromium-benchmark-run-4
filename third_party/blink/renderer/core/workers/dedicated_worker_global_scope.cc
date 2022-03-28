d49c46bbcfaacbbe7b3711474904ae7e54a2f5ce
Hiroki Nakagawa <nhiroki@chromium.org>
2022-03-28 17:17:52 +0000

    Revert "Enable to iterate DedicatedWorkers from their creators: LocalDOMWindow or DedicatedWorkerGlobalScope"
    This reverts commit 99a84d0bbad5568ba176a669ae4cbca7a52bcd4c.
    
    Reason for revert:
    - This code hasn't been used.
    - The code has caused the memory leak: https://crbug.com/1310170
    
    Original change's description:
    > Enable to iterate DedicatedWorkers from their creators: LocalDOMWindow or DedicatedWorkerGlobalScope
    >
    > Now DedicatedWorkers is created at LocalDOMWindow or other
    > DedicatedWorkers. This CL enables to iterate DedicatedWorkers from their
    > creators.
    >
    > The purpose is to enable to notify a page lifecycle change to all the
    > workers so that all the workers can be frozen or resumed when the page
    > goes to or restores from back/forward cache.
    >
    > This is a preparation to make pages with dedicated workers eligible for
    > back/forward cache. The main purpose is freezing the schedulers.
    > Creating dedicated workers on shared/service workers are not implemented
    > in Chromium yet (though it is allowed in the spec).
    >
    > Design Doc: https://docs.google.com/document/d/1CJfDNqA2tEp_dmxK5kaBHHqGjx7cK2_jhTpsCwSCa3A/edit?usp=sharing
    >
    > Bug: 1146955
    > Change-Id: I8ba7dc2ba40c023048ce7b89a0466d9ea8886b40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3281682
    > Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#942040}
    
    Bug: 1146955, 1310170
    Change-Id: I4c8132eb6372bce26fd533d5d3ddb9c52d5da74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555498
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986027}

--- third_party/blink/renderer/core/workers/dedicated_worker_global_scope.cc
+++ third_party/blink/renderer/core/workers/dedicated_worker_global_scope.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/messaging/blink_transferable_message.h"
 #include "third_party/blink/renderer/core/origin_trials/origin_trial_context.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
-#include "third_party/blink/renderer/core/workers/dedicated_worker.h"
 #include "third_party/blink/renderer/core/workers/dedicated_worker_object_proxy.h"
 #include "third_party/blink/renderer/core/workers/dedicated_worker_thread.h"
 #include "third_party/blink/renderer/core/workers/global_scope_creation_params.h"
@@ -512,7 +511,6 @@ void DedicatedWorkerGlobalScope::Trace(Visitor* visitor) const {
   visitor->Trace(dedicated_worker_host_);
   visitor->Trace(back_forward_cache_controller_host_);
   visitor->Trace(animation_frame_provider_);
-  visitor->Trace(dedicated_workers_);
   WorkerGlobalScope::Trace(visitor);
 }
 
@@ -551,16 +549,6 @@ void DedicatedWorkerGlobalScope::SetIsInBackForwardCache(
             total_bytes_buffered_while_in_back_forward_cache_);
     total_bytes_buffered_while_in_back_forward_cache_ = 0;
   }
-}
-
-void DedicatedWorkerGlobalScope::AddDedicatedWorker(
-    DedicatedWorker* dedicated_worker) {
-  dedicated_workers_.insert(dedicated_worker);
-}
-
-void DedicatedWorkerGlobalScope::RemoveDedicatedWorker(
-    DedicatedWorker* dedicated_worker) {
-  dedicated_workers_.erase(dedicated_worker);
 }
 
 }  // namespace blink
