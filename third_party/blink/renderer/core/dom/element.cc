1acd62da69615f957fe6a1adc053f7e14e17c241
Steinar H. Gunderson <sesse@chromium.org>
2022-03-28 13:42:52 +0000

    Disable the independent inheritance optimization for objects with custom style callbacks.
    We don't call these callbacks when doing independent inheritance,
    and all bets are off as of what they may contain. In particular,
    this caused a bug where the white-space property would get wrong
    on an internal <div> used to implement the <input> editor.
    
    Change-Id: I98ab2988ed948408861959586aaa00a2a0058f32
    Fixed: 1309014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554653
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985937}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3163,6 +3163,8 @@ scoped_refptr<ComputedStyle> Element::PropagateInheritedProperties() {
     return nullptr;
   if (HasAnimations())
     return nullptr;
+  if (HasCustomStyleCallbacks())
+    return nullptr;
   const ComputedStyle* parent_style = ParentComputedStyle();
   DCHECK(parent_style);
   const ComputedStyle* style = GetComputedStyle();
