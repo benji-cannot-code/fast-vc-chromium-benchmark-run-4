7518e0983bcba37def4390a5f8f26cdd2109ddbc
Kenichi Ishibashi <bashi@chromium.org>
2022-03-28 04:41:43 +0000

    Add WPTs for Early Hints preload is disallowed by document's CSP
    The test scenario is:
    * The test page sends an Early Hints containing cross origin preload
      with/without CSP. The preload request is delayed until the document
      is loaded.
    * The document later sets CSP to disallow the preload.
    
    The expectation is that the preload should be denied by CSP.
    
    Bug: 1305896
    Change-Id: Ia23ac612a485793568a4f78d740f832a424f6564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551777
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985826}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/preload-without-csp-document-disallow.h2.window.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+test(() => {
+    const early_hints_policy = "absent";
+    navigateToContentSecurityPolicyDocumentDisallowTest(early_hints_policy);
+});
