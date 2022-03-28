363dc9abbe6b1988e7ad654f167c56b256a7f49f
Yoshifumi Inoue <yosin@chromium.org>
2022-03-28 05:02:44 +0000

    Get rid of unused functions FontPlatformData::operator=(const FontPlatformData&)
    This patch gets rid gets rid of |FontPlatformData::operator=()| to
    simplify for improving code health.
    
    Note: This CL is a preparation of the CL[1].
    
    [1] http://crrev.com/c/3211416: Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: I473ba9f27575b70fe0bf4cc083b3f0a99e749dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551790
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#985829}

--- third_party/blink/renderer/platform/fonts/font_platform_data.h
+++ third_party/blink/renderer/platform/fonts/font_platform_data.h
@@ -123,7 +123,7 @@ class PLATFORM_EXPORT FontPlatformData {
     avoid_embedded_bitmaps_ = embedded_bitmaps;
   }
   bool operator==(const FontPlatformData&) const;
-  const FontPlatformData& operator=(const FontPlatformData&);
+  FontPlatformData& operator=(const FontPlatformData&) = delete;
 
   bool IsHashTableDeletedValue() const { return is_hash_table_deleted_value_; }
   bool FontContainsCharacter(UChar32 character);
@@ -161,7 +161,7 @@ class PLATFORM_EXPORT FontPlatformData {
   WebFontRenderStyle QuerySystemForRenderStyle();
 #endif
 
-  sk_sp<SkTypeface> typeface_;
+  const sk_sp<SkTypeface> typeface_;
 #if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_MAC)
   std::string family_;
 #endif
