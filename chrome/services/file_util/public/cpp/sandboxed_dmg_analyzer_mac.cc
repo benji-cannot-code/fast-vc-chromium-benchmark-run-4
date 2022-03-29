de6ed33485e2c63aa071bf700d9fa1b662e932ef
Daniel Rubery <drubery@chromium.org>
2022-03-29 01:02:10 +0000

    Add safety checks to SandboxedDmgAnalyzer
    This has the same flaws as the other sandboxed file analyzers, but was
    missed in the first round of fixes. We now check that the remotes are
    valid before sending messages, and that callbacks are not null before
    running them.
    
    Change-Id: I44e8bf88475d9533b86644c69ec7d11856ebf0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3556487
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986258}

--- chrome/services/file_util/public/cpp/sandboxed_dmg_analyzer_mac.cc
+++ chrome/services/file_util/public/cpp/sandboxed_dmg_analyzer_mac.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/thread_pool.h"
 #include "chrome/common/safe_browsing/archive_analyzer_results.h"
@@ -71,16 +72,24 @@ void SandboxedDMGAnalyzer::PrepareFileToAnalyze() {
 void SandboxedDMGAnalyzer::ReportFileFailure() {
   DCHECK(!content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
-  content::GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(std::move(callback_),
-                                safe_browsing::ArchiveAnalyzerResults()));
+  if (callback_) {
+    content::GetUIThreadTaskRunner({})->PostTask(
+        FROM_HERE, base::BindOnce(std::move(callback_),
+                                  safe_browsing::ArchiveAnalyzerResults()));
+  }
 }
 
 void SandboxedDMGAnalyzer::AnalyzeFile(base::File file) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  remote_analyzer_->AnalyzeDmgFile(
-      std::move(file),
-      base::BindOnce(&SandboxedDMGAnalyzer::AnalyzeFileDone, this));
+  base::UmaHistogramBoolean("SBClientDownload.DmgAnalysisRemoteValid",
+                            remote_analyzer_.is_bound());
+  if (remote_analyzer_) {
+    remote_analyzer_->AnalyzeDmgFile(
+        std::move(file),
+        base::BindOnce(&SandboxedDMGAnalyzer::AnalyzeFileDone, this));
+  } else {
+    AnalyzeFileDone(safe_browsing::ArchiveAnalyzerResults());
+  }
 }
 
 void SandboxedDMGAnalyzer::AnalyzeFileDone(
