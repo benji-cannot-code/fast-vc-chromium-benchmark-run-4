c2ed15581bc7f965690bac72a7fb1d5470dbf2d7
Arthur Eubanks <aeubanks@google.com>
2022-03-29 20:05:45 +0000

    Remove set but otherwise unused variables
    Recent Clang versions have enhanced -Wunused-but-set-variable which now
    warns about these.
    
    Bug: 1309955
    Change-Id: I823e68888c98b95abc5ac5257b57741e0a8fdab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3556838
    Auto-Submit: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986657}

--- services/network/sct_auditing/sct_auditing_handler.cc
+++ services/network/sct_auditing/sct_auditing_handler.cc
@@ -214,7 +214,6 @@ void SCTAuditingHandler::DeserializeData(const std::string& serialized) {
     return;
   }
 
-  size_t num_reporters_deserialized = 0u;
   for (base::Value& sct_entry : value->GetListDeprecated()) {
     if (!sct_entry.is_dict()) {
       continue;
@@ -276,7 +275,6 @@ void SCTAuditingHandler::DeserializeData(const std::string& serialized) {
 
     AddReporter(cache_key, std::move(audit_report), std::move(sct_metadata),
                 std::move(backoff_entry));
-    ++num_reporters_deserialized;
   }
   // TODO(crbug.com/1144205): Add metrics for number of reporters deserialized.
 }
