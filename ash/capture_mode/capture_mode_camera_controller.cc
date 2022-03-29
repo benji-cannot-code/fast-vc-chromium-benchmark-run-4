92c7054bc1cd0ab1ccf2bc00b68d6c2adc3bb160
Ahmed Fakhry <afakhry@chromium.org>
2022-03-29 17:27:39 +0000

    capture_selfie_cam: Cleanup some leftover code
    Bug: None
    Test: None
    Change-Id: I9b54541aaec6112059a841ff1e23b835cab2d3ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558134
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986578}

--- ash/capture_mode/capture_mode_camera_controller.cc
+++ ash/capture_mode/capture_mode_camera_controller.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_conversions.h"
-#include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/transform.h"
 #include "ui/views/animation/animation_builder.h"
