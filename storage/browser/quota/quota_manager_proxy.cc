cfd50255d14e5652bbea53f33acb4a4363eac8ef
Austin Sullivan <asully@chromium.org>
2022-03-29 14:45:39 +0000

    Nest media license data in own separate directory
    Old path:
      $PROFILE/WebStorage/<bucketID>/Media Licenses.db
    
    New path:
      $PROFILE/WebStorage/<bucketID>/Media Licenses/Media Licenses.db
    
    Creating a separate directory encapsulates the database and all of its
    associated log files.
    
    Bug: 1231162
    Change-Id: I2a5a37dc281525e7e979d988009f1976bf76dcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552380
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986489}

--- storage/browser/quota/quota_manager_proxy.cc
+++ storage/browser/quota/quota_manager_proxy.cc
@@ -101,6 +101,8 @@ base::FilePath QuotaManagerProxy::GetClientBucketPath(
       return bucket_directory.Append(kCacheStorageDirectory);
     case QuotaClientType::kServiceWorker:
       return bucket_directory.Append(kScriptCacheDirectory);
+    case QuotaClientType::kMediaLicense:
+      return bucket_directory.Append(kMediaLicenseDirectory);
     default:
       NOTREACHED() << "Unsupported QuotaClientType";
       return base::FilePath();
