a968f1bc2d9f40688521a555453d71e1c5139492
Adrian Taylor <adetaylor@chromium.org>
2022-03-29 21:09:23 +0000

    [rust] autocxx binding generation
    This CL uses autocxx to generate Rust bindings to
    pre-existing Chromium C++ headers.
    
    autocxx is one of several possible interop approaches with which
    the Chromium Rust team plans to experiment.
    
    Specifically, it switches from using cxx's 'cxxbridge' command
    to autocxx's 'autocxx_gen' command. autocxx_gen is a superset of
    cxxbridge, so handles all the existing #[cxx::bridge] bindings
    we have, but is also able to handle:
    
      include_cpp!(
        "foo.h"
      )
    
    macros which refer to existing C++ header files.
    
    Like cxx, autocxx requires generation of C++-side bindings and uses a
    procedural macro to expand bindings in the Rust side.
    
    There are differences which dictate extra complexity in this CL:
    
    * The Rust side bindings include! a file generated by an external
      code generation utility. This has little effect except that we need
      to add an extra dependency; the Rust compile step can't proceed
      until that code generator has run.
      The location of the included file needs to be passed to
      rustc via an environment variable. At the moment that limits us to
      a single set of autocxx bindings in each target.
    * The code generator (which generates both Rust and C++ side bindings)
      obviously needs to parse C++ headers - that's the whole point.
      To do that, it needs the include path and any #defines which are
      applicable to code. This is achieved by using extra gn variables
      in the associated 'action' target.
    
    Bug: 1272780, 1296155
    Change-Id: Ifa3ccfbe6b1b29781726a700505c2ad800721b44
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-rel,android-rust-arm-dbg,linux-rust-x64-rel,linux-rust-x64-dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321897
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986694}

---
+++ build/rust/tests/autocxx_test/main.rs
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+fn main() {
+    println!(
+        "Hello, world! Origin is: {}",
+        autocxx_test_lib::serialize_url("https", "foo.com", 443)
+    );
+    println!("CPU vendor is: {}", autocxx_test_lib::get_cpu_vendor());
+}
