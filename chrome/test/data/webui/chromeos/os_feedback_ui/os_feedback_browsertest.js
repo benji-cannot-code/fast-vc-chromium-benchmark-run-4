b297643080fb99c69e4e315d72dee1a7ac51a61b
xiangdong kong <xiangdongkong@google.com>
2022-03-29 02:40:57 +0000

    feedback: Add initial navigation using iron pages
    Submitting a feedback report involves the following three steps [1]:
    1) First step is the search page where the users can enter text and get
       suggested help contents.
    2) Second step is the share data page where the users can choose what
       data to send with the report, e.g., email address, screenshot, etc.
    3) Last step is the confirmation page.
    
    This CL adds the steps inside an iron-pages element which can manage the
    navigation among the steps.
    
    No business logic implemented yet.
    
    [1] https://screenshot.googleplex.com/3a8stYEg2DYkZGA.
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I975bd7f1e6423946856862bb8acb47687c5529e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553422
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#986292}

--- chrome/test/data/webui/chromeos/os_feedback_ui/os_feedback_browsertest.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/os_feedback_browsertest.js
@@ -44,6 +44,7 @@ const debug_suites_list = [
   'confirmationPageTest',
   'fakeHelpContentProviderTest',
   'fakeMojoProviderTest',
+  'feedbackFlowTest',
   'helpContentTest',
   'searchPageTest',
   'shareDataPageTest',
