6212f364406f5678cba4a88cdcb9694f06734ebc
Wan-Teh Chang <wtc@google.com>
2022-03-29 03:27:38 +0000

    Update package names for current version of Debian
    The package "cmake3" doesn't exist in the current version of Debian.
    Replace it with "cmake".
    
    Similarly, replace lib32gcc-7-dev with lib32gcc-11-dev and replace
    lib32stdc++-7-dev with lib32stdc++-11-dev.
    
    Change-Id: I80ad6115175ed348d8680e3fc22f0dfc020e3656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3556691
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/main@{#986307}

--- third_party/libaom/cmake_update.sh
+++ third_party/libaom/cmake_update.sh
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # $ ./cmake_update.sh
 # Requirements:
 # Install the following Debian packages.
-# - cmake3
+# - cmake
 # - yasm or nasm
 # Toolchain for armv7:
 # - gcc-arm-linux-gnueabihf
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # - gcc-aarch64-linux-gnu
 # - g++-aarch64-linux-gnu
 # 32bit build environment for cmake. Including but potentially not limited to:
-# - lib32gcc-7-dev
-# - lib32stdc++-7-dev
+# - lib32gcc-11-dev
+# - lib32stdc++-11-dev
 # Alternatively: treat 32bit builds like Windows and manually tweak aom_config.h
 
 set -eE
