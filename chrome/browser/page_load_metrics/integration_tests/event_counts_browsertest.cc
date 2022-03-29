ef71240fcb61b1a0c008c09991a34a9c6897e784
Lan Wei <lanwei@chromium.org>
2022-03-29 17:04:29 +0000

    Waiting to receive the events back in event handler to finish the test
    This test is to verify if we receive the correct events in performance
    EventTiming API, but we do not know when the events are received. If we
    add an event handler in JS, and wait until we receive the events, we can
    guarantee that the events generated by the Testdriver are received in
    the renderer.
    
    Bug: 1207854
    Change-Id: Icdaf450f691d66587914f81709289718b99dbc27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552037
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986566}

--- chrome/browser/page_load_metrics/integration_tests/event_counts_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/event_counts_browsertest.cc
@@ -9,9 +9,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 
 #if defined(USE_AURA)
-IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, DISABLED_EventCounts) {
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, EventCounts) {
   LoadHTML(R"HTML(
     <p>Sample website</p>
+    <script type="text/javascript">
+    window.eventCounts =
+        {mousedown: 0, touchstart: 0, pointerdown: 0, click: 0};
+    function recordEvent(e) {
+        eventCounts[e.type]++;
+    }
+    for (var evt in eventCounts) {
+        document.addEventListener(evt, recordEvent);
+    }
+    </script>
   )HTML");
 
   // Simulate tap on screen.
@@ -28,7 +38,13 @@ IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, DISABLED_EventCounts) {
   content::SimulateMouseClick(web_contents(), 0,
                               blink::WebMouseEvent::Button::kLeft);
 
-  base::PlatformThread::Sleep(base::Milliseconds(3000));
+  while (EvalJs(web_contents(), "window.eventCounts.mousedown").ExtractInt() <
+         3) {
+    base::RunLoop run_loop;
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, run_loop.QuitClosure(), base::Milliseconds(100));
+    run_loop.Run();
+  }
 
   // Check event counts.
   int expected_pointerdown =
