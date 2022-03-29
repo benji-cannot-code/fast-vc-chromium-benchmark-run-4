6d16a3c2cde434941ee5cc0bd8132ed466fbfa5a
Kent Tamura <tkent@chromium.org>
2022-03-29 18:01:17 +0000

    Deferred Shaping: Do not reuse NGFragmentItem objects on reshaping, take 2
    crrev.com/984578 was incomplete. If a deferred IFC was unlocked and its
    ComputeMinMaxSizes() was called, ComputeMinMaxSizes() reshped and
    cleared IsShapingDeferred flag. Then TryReuseFragmentsFromCache()
    incorrectly applied cached NGFragmentItem because IsShapingDeferred flag
    was already cleared.
    
    This CL clears cached layout result on unlocking.
    This fixes a rendering error on chrome://flags.
    
    Bug: 1259085
    Change-Id: I947dacf12757215664505cf5020eda2da2d9e6c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554240
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986592}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1017,9 +1017,6 @@ bool NGBlockLayoutAlgorithm::TryReuseFragmentsFromCache(
     const NGInlineBreakToken** inline_break_token_out) {
   DCHECK(previous_result_);
 
-  if (inline_node.ShouldBeReshaped())
-    return false;
-
   const auto& previous_fragment =
       To<NGPhysicalBoxFragment>(previous_result_->PhysicalFragment());
   const NGFragmentItems* previous_items = previous_fragment.Items();
