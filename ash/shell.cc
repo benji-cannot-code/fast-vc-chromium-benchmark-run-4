1ad12219182be5711ec866936657bdf93176a484
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2022-03-29 00:17:17 +0000

    dark_mode: Add a controller for scheduling feature
    - Add DarkModeController which auto scheduling dark mode on/off
    and provide API to set and get AUTO scheduling
    `Set/GetAutoScheduleEnabled()`.
    - Register profile pref of the controller.
    - Init `dark_mode_controller_` in `ash::Shell`.
    
    Bug: 1290919
    Test: Manual
    Change-Id: Ie5d18c0dccc1bec0442a89f44c0e1b7d6f56893a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465682
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986239}

--- ash/shell.cc
+++ ash/shell.cc
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shutdown_controller_impl.h"
 #include "ash/style/ash_color_mixer.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/style/dark_mode_controller.h"
 #include "ash/system/audio/display_speaker_controller.h"
 #include "ash/system/bluetooth/bluetooth_device_status_ui_handler.h"
 #include "ash/system/bluetooth/bluetooth_notification_controller.h"
@@ -840,6 +841,10 @@ Shell::~Shell() {
   // Similarly for PrivacyScreenController.
   privacy_screen_controller_ = nullptr;
 
+  // Depends on `geolocation_controller_`, so it must be destructed before the
+  // geolocation controller.
+  dark_mode_controller_.reset();
+
   geolocation_controller_.reset();
 
   // NearbyShareDelegateImpl must be destroyed before SessionController and
@@ -1069,6 +1074,10 @@ void Shell::Init(
   ui::ColorProviderManager::Get().AppendColorProviderInitializer(
       base::BindRepeating(AddAshColorMixer));
 
+  // Geolocation controller needs to be created before any `ScheduledFeature`
+  // subclasses such as night light and dark mode controllers because
+  // `ScheduledFeature` ctor will access `geolocation_controller_` from
+  // `Shell`.
   geolocation_controller_ = std::make_unique<GeolocationController>(
       shell_delegate_->GetGeolocationUrlLoaderFactory());
 
@@ -1076,6 +1085,8 @@ void Shell::Init(
   // aura::Env, and geolocation controller, so initialize it after all have
   // been initialized.
   night_light_controller_ = std::make_unique<NightLightControllerImpl>();
+
+  dark_mode_controller_ = std::make_unique<DarkModeController>();
 
   // Privacy Screen depends on the display manager, so initialize it after
   // display manager was properly initialized.
