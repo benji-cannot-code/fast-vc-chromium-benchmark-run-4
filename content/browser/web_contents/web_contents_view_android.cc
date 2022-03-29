260f8ae1d73423fe1fd55b54e997afb57698c229
Mustaq Ahmed <mustaq@google.com>
2022-03-29 16:35:59 +0000

    Drag events: avoid converting coordinates to integers at the JNI layer.
    The conversion is unnecessary because both sides of the JNI layer use
    floating point coordinates.
    
    Bug: 1294838
    Change-Id: I7f1416d2f182a7e7356994313b4c41946fb6106b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550457
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986548}

--- content/browser/web_contents/web_contents_view_android.cc
+++ content/browser/web_contents/web_contents_view_android.cc
@@ -389,11 +389,11 @@ bool WebContentsViewAndroid::OnDragEvent(const ui::DragEventAndroid& event) {
         metadata.push_back(DropData::Metadata::CreateForMimeType(
             DropData::Kind::STRING, mime_type));
       }
-      OnDragEntered(metadata, event.location_f(), event.screen_location_f());
+      OnDragEntered(metadata, event.location(), event.screen_location());
       break;
     }
     case JNI_DragEvent::ACTION_DRAG_LOCATION:
-      OnDragUpdated(event.location_f(), event.screen_location_f());
+      OnDragUpdated(event.location(), event.screen_location());
       break;
     case JNI_DragEvent::ACTION_DROP: {
       DropData drop_data;
@@ -411,7 +411,7 @@ bool WebContentsViewAndroid::OnDragEvent(const ui::DragEventAndroid& event) {
         }
       }
 
-      OnPerformDrop(&drop_data, event.location_f(), event.screen_location_f());
+      OnPerformDrop(&drop_data, event.location(), event.screen_location());
       break;
     }
     case JNI_DragEvent::ACTION_DRAG_EXITED:
