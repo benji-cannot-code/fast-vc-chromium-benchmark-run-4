90fc4b62502f5b4884c366da7011c2d35c532898
Danila Kuzmin <dkuzmin@google.com>
2022-03-29 13:48:06 +0000

    [OOBE]: Check captive_protal_delegate_ ptr before calling
    Bug: 1289892
    Change-Id: Id38ca7c5de5467acae7d86e14a77c6feb5e71ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557370
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#986462}

--- chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.cc
+++ chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.cc
@@ -433,7 +433,8 @@ void OobeUIDialogDelegate::OnKeyboardVisibilityChanged(bool visible) {
 void OobeUIDialogDelegate::OnBeforeCaptivePortalShown() {
   should_display_captive_portal_ = false;
 
-  captive_portal_delegate_->Show();
+  if (captive_portal_delegate_)
+    captive_portal_delegate_->Show();
 }
 
 void OobeUIDialogDelegate::OnAfterCaptivePortalHidden() {
@@ -442,7 +443,8 @@ void OobeUIDialogDelegate::OnAfterCaptivePortalHidden() {
   // captive portal next time the OOBE dialog pops up.
   should_display_captive_portal_ = false;
 
-  captive_portal_delegate_->Hide();
+  if (captive_portal_delegate_)
+    captive_portal_delegate_->Hide();
 }
 
 void OobeUIDialogDelegate::OnCurrentScreenChanged(OobeScreenId current_screen,
