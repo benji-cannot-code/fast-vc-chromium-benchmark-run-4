b12a34f7d6574d31977c399286da93f2aa35df03
Fabrice de Gans <fdegans@chromium.org>
2022-03-29 18:26:23 +0000

    [code-health] Fix a typo in third_party/PRESUBMIT.py
    It should be USE_PYTHON3, not USE_PYTHON.
    
    Bug: 1212061
    Change-Id: I4543df9267df02851f8af3ec5caf7d4cc495a21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558192
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986612}

--- third_party/PRESUBMIT.py
+++ third_party/PRESUBMIT.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
-USE_PYTHON = True
+USE_PYTHON3 = True
 
 ANDROID_ALLOWED_LICENSES = [
   'A(pple )?PSL 2(\.0)?',
