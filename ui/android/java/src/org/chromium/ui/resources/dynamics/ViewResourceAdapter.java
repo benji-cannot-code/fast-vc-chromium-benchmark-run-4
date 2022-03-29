5b6d944a8a9dbd30ee19640d306fe5bdba5e8acf
Omar Elmekkawy <mekk@google.com>
2022-03-29 11:47:42 +0000

    Avoiding ImageReader creation for invalid dimensions
    The following crrev.com/c/3452087 caused crashes when the an omnibar
    dimension is less than 1, this is an attempt to fix those crashes.
    
    Bug: 1308335
    Change-Id: I06a01ff0ddcf7c6e522ac62df10f4169c9e0664a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3548598
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986437}

--- ui/android/java/src/org/chromium/ui/resources/dynamics/ViewResourceAdapter.java
+++ ui/android/java/src/org/chromium/ui/resources/dynamics/ViewResourceAdapter.java
@@ -124,6 +124,11 @@ public class ViewResourceAdapter extends DynamicResource implements OnLayoutChan
                     // and contexts, prevents unneeded callbacks as well.
                     mReaderDelegate.close();
                 }
+                if (width == 0 || height == 0) {
+                    // If we get less than 1 width or height, there is nothing to draw.
+                    // Keep this in sync with other restrictions in captureWithHardwareDraw().
+                    return;
+                }
                 mReaderDelegate = ImageReader.newInstance(
                         width, height, PixelFormat.RGBA_8888, maxBitmapsToAcquire);
                 mReaderDelegate.setOnImageAvailableListener(this, sHandler);
@@ -308,6 +313,7 @@ public class ViewResourceAdapter extends DynamicResource implements OnLayoutChan
         try (TraceEvent e = TraceEvent.scoped("ViewResourceAdapter:captureWithHardwareDraw")) {
             if (mView.getWidth() == 0 || mView.getHeight() == 0) {
                 // We haven't actually laid out this view yet no point in requesting a screenshot.
+                // Keep this in sync with other restrictions in init().
                 return;
             }
 
