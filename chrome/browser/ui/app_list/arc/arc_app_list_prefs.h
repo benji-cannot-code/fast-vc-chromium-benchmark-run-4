48f73fc9b6e5ceb9792a15d8ccc7daba472d5633
Yusuke Sato <yusukes@chromium.org>
2022-03-29 16:20:27 +0000

    arc: Add Arc.AppCount.* UMA metrics
    This allows us to know how many users have manually installed one
    or more ARC apps.
    
    BUG=b:219115916
    TEST=sign in, then check chrome://histograms
    
    Change-Id: I16e248fb41755723b958c13562d2812ac8080160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551878
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986538}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.h
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.h
@@ -616,6 +616,9 @@ class ArcAppListPrefs : public KeyedService,
   // Callback called once default apps are ready.
   void OnDefaultAppsReady();
 
+  // Records UMA metrics on app counts on ARC start.
+  void RecordAppIdsUma();
+
   Profile* const profile_;
 
   // Owned by the BrowserContext.
