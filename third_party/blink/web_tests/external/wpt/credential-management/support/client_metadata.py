8937c2b3f6469ee43cb0a7e7c947e5fc58b9fb43
Christian Biesinger <cbiesinger@chromium.org>
2022-03-29 00:07:26 +0000

    [FedCM] More checks that we send cookies for the right endpoints
    R=yigu@chromium.org
    
    Bug: 1309251
    Change-Id: Ic7f4377a946ff46c3661257133c44227f04309bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554744
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986231}

---
+++ third_party/blink/web_tests/external/wpt/credential-management/support/client_metadata.py
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+  if b"cookie" in request.cookies:
+    return (500, [], "Cookie should not be sent to this endpoint")
+  return """
+{
+  "privacy_policy_url": "https://privacypolicy.com"
+}
+"""
