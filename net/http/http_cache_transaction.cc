e28114e8210d0df6474d33ae949c33c9face581e
Arthur Hemery <ahemery@chromium.org>
2022-03-29 10:43:22 +0000

    Reuse CORP headers from cached resources on 304.
    Resources that set Cross-Origin-Resource-Policy (CORP) headers and get
    cached might be blocked when then getting revalidated via a 304. We add
    the stored CORP header again when going through the cache to avoid the
    issue.
    
    Also drive-by nit with init lists to please the linter.
    
    Bug: 1241264
    Change-Id: I8a1aa0be96d1fb98ca3f37293f2e6d38308c2231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550262
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986418}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -159,8 +159,7 @@ static bool HeaderMatches(const HttpRequestHeaders& headers,
 //-----------------------------------------------------------------------------
 
 HttpCache::Transaction::Transaction(RequestPriority priority, HttpCache* cache)
-    : next_state_(STATE_NONE),
-      initial_request_(nullptr),
+    : initial_request_(nullptr),
       request_(nullptr),
       priority_(priority),
       cache_(cache->GetWeakPtr()),
@@ -1938,6 +1937,7 @@ int HttpCache::Transaction::DoUpdateCachedResponseComplete(int result) {
     //
     // By stopping to write to the cache now, we make sure that the 304 rather
     // than the cached 200 response, is what will be returned to the user.
+    UpdateSecurityHeadersBeforeForwarding();
     DoneWithEntry(true);
   } else if (entry_ && !handling_206_) {
     DCHECK_EQ(READ_WRITE, mode_);
@@ -3619,6 +3619,19 @@ bool HttpCache::Transaction::ShouldDisableCaching(
     }
   }
   return disable_caching;
+}
+
+void HttpCache::Transaction::UpdateSecurityHeadersBeforeForwarding() {
+  // Because of COEP, we need to add CORP to the 304 of resources that set it
+  // previously. It will be blocked in the network service otherwise.
+  std::string stored_corp_header;
+  response_.headers->GetNormalizedHeader("Cross-Origin-Resource-Policy",
+                                         &stored_corp_header);
+  if (!stored_corp_header.empty()) {
+    new_response_->headers->SetHeader("Cross-Origin-Resource-Policy",
+                                      stored_corp_header);
+  }
+  return;
 }
 
 }  // namespace net
