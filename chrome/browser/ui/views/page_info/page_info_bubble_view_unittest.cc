d796b69f73c3d16e37b5993f8a12dabd320d741c
Olesia Marukhno <olesiamarukhno@google.com>
2022-03-29 08:37:44 +0000

    [page info] Clean up page info unit tests
    Remove setting source of the permissions as it doesn't do anything.
    Add a comment explaining why it works like that.
    
    Bug: 1250045
    Change-Id: Ieb8a6d2a4bf529a874b8be08f1166f6e7cc12e63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554968
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#986384}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
@@ -234,6 +234,9 @@ class PageInfoBubbleViewTestApi {
   }
 
   // Simulates recreating the dialog with a new PermissionInfoList.
+  // It ignores `source` field and assumes that user is the source. It's because
+  // in the actual UI, permission's state can be changed only if the source is
+  // user.
   void SetPermissionInfo(const PermissionInfoList& list) {
     for (const PageInfo::PermissionInfo& info : list) {
       presenter_->OnSitePermissionChanged(info.type, info.setting,
@@ -431,7 +434,6 @@ TEST_F(PageInfoBubbleViewTest, NotificationPermissionRevokeUkm) {
 
   PermissionInfoList list(1);
   list.back().type = ContentSettingsType::NOTIFICATIONS;
-  list.back().source = content_settings::SETTING_SOURCE_USER;
 
   list.back().setting = CONTENT_SETTING_ALLOW;
   api_->SetPermissionInfo(list);
@@ -461,7 +463,6 @@ TEST_F(PageInfoBubbleViewTest, NotificationPermissionRevokeUkm) {
 TEST_F(PageInfoBubbleViewTest, SetPermissionInfo) {
   PermissionInfoList list(1);
   list.back().type = ContentSettingsType::GEOLOCATION;
-  list.back().source = content_settings::SETTING_SOURCE_USER;
   list.back().setting = CONTENT_SETTING_BLOCK;
 
   // Initially, no permissions are shown because they are all set to default.
@@ -530,7 +531,6 @@ TEST_F(PageInfoBubbleViewOffTheRecordTest, ResetBlockedInIncognitoPermission) {
 
   PermissionInfoList list(1);
   list.back().type = ContentSettingsType::NOTIFICATIONS;
-  list.back().source = content_settings::SETTING_SOURCE_POLICY;
   list.back().setting = CONTENT_SETTING_BLOCK;
 
   // Initially, no permissions are shown because they are all set to default.
@@ -557,7 +557,6 @@ TEST_F(PageInfoBubbleViewOffTheRecordTest, ResetBlockedInIncognitoPermission) {
   window_placement_permission.type = ContentSettingsType::WINDOW_PLACEMENT;
   window_placement_permission.setting = CONTENT_SETTING_ALLOW;
   window_placement_permission.default_setting = CONTENT_SETTING_ASK;
-  window_placement_permission.source = content_settings::SETTING_SOURCE_USER;
   list.push_back(window_placement_permission);
 
   num_expected_children = list.size();
@@ -816,7 +815,6 @@ TEST_F(PageInfoBubbleViewTest, SetPermissionInfoWithUserAndPolicyUsbDevices) {
 TEST_F(PageInfoBubbleViewTest, SetPermissionInfoForUsbGuard) {
   PermissionInfoList list(1);
   list.back().type = ContentSettingsType::USB_GUARD;
-  list.back().source = content_settings::SETTING_SOURCE_USER;
   list.back().setting = CONTENT_SETTING_ASK;
 
   // Initially, no permissions are shown because they are all set to default.
