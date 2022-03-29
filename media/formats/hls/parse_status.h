ab451a0a5add1809a61077946cf0535974f3e66a
Will Cassella <cassew@chromium.org>
2022-03-29 21:58:29 +0000

    HLS: Implement VariableDictionary
    This CL implements the VariableDictionary structure which tracks the
    set of variables defined by a playlist and implements the variable
    substitution procedure as defined by the HLS spec. Variable definition
    via the EXT-X-DEFINE tag and the importing of definitions across
    playlists is implemented in a separate CL.
    
    Bug: 1266991
    Change-Id: Ia3a9942efc786ee0128836c5b153cb981c1e45e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553074
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986718}

--- media/formats/hls/parse_status.h
+++ media/formats/hls/parse_status.h
@@ -29,6 +29,7 @@ enum class ParseStatusCode : StatusCodeType {
   kPlaylistHasDuplicateTags,
   kPlaylistHasUnsupportedVersion,
   kMediaPlaylistHasMultivariantPlaylistTag,
+  kVariableUndefined,
 };
 
 struct ParseStatusTraits {
--- media/formats/hls/tags.cc
+++ media/formats/hls/tags.cc
@@ -230,7 +230,7 @@ ParseStatus::Or<XDefineTag> XDefineTag::Parse(TagItem tag) {
   if (map.HasValue(XDefineTagAttribute::kName)) {
     auto var_name =
         ParseXDefineTagQuotedString(map.GetValue(XDefineTagAttribute::kName))
-            .MapValue(types::ParseVariableName);
+            .MapValue(types::VariableName::Parse);
     if (var_name.has_error()) {
       return ParseStatus(ParseStatusCode::kMalformedTag)
           .AddCause(std::move(var_name).error());
@@ -254,7 +254,7 @@ ParseStatus::Or<XDefineTag> XDefineTag::Parse(TagItem tag) {
   if (map.HasValue(XDefineTagAttribute::kImport)) {
     auto var_name =
         ParseXDefineTagQuotedString(map.GetValue(XDefineTagAttribute::kImport))
-            .MapValue(types::ParseVariableName);
+            .MapValue(types::VariableName::Parse);
     if (var_name.has_error()) {
       return ParseStatus(ParseStatusCode::kMalformedTag)
           .AddCause(std::move(var_name).error());
