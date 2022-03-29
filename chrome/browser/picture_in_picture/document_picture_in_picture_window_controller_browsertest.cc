3b0d8538277910e2c0e464f23a9b706032d8befd
liberato@chromium.org <liberato@chromium.org>
2022-03-29 23:44:38 +0000

    Restrict Picture in Picture 2.0 to secure contexts
    Only secure contexts should be allowed to create Picture in Picture
    2.0 windows.  Other contexts should fail.
    
    Bug: 1311088
    Change-Id: I8749e9e769ab90635c2bc88b71af46c0d5bbe19d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549757
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986773}

--- chrome/browser/picture_in_picture/document_picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/document_picture_in_picture_window_controller_browsertest.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/media_session/public/cpp/features.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "skia/ext/image_operations.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
@@ -85,7 +86,7 @@ class DocumentPictureInPictureWindowControllerBrowserTest
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
-    embedded_test_server()->ServeFilesFromSourceDirectory("content/test/data");
+    embedded_test_server()->ServeFilesFromSourceDirectory("chrome/test/data");
     ASSERT_TRUE(embedded_test_server()->Start());
   }
 
@@ -114,10 +115,14 @@ class DocumentPictureInPictureWindowControllerBrowserTest
         window_controller()->GetWindowForTesting());
   }
 
-  void LoadTabAndEnterPictureInPicture(Browser* browser,
-                                       const base::FilePath& file_path) {
+  void LoadTabAndEnterPictureInPicture(
+      Browser* browser,
+      const base::FilePath::CharType* file_path) {
+    // Make a file:// URL, which is secure.
     GURL test_page_url = ui_test_utils::GetTestUrl(
-        base::FilePath(base::FilePath::kCurrentDirectory), file_path);
+        base::FilePath(base::FilePath::kCurrentDirectory),
+        base::FilePath(file_path));
+
     ASSERT_TRUE(ui_test_utils::NavigateToURL(browser, test_page_url));
 
     content::WebContents* active_web_contents =
@@ -182,9 +187,10 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 // new one.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
                        CreateTwice) {
-  LoadTabAndEnterPictureInPicture(
-      browser(), base::FilePath(kPictureInPictureWindowSizePage));
+  LoadTabAndEnterPictureInPicture(browser(), kPictureInPictureWindowSizePage);
 
+  ASSERT_TRUE(window_controller());
+  ASSERT_TRUE(window_controller()->GetWindowForTesting());
   EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
 
   gfx::NativeWindow native_window_1 = GetOverlayWindow()->GetNativeWindow();
@@ -199,6 +205,8 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
                          "  {width: 200, height: 200}"
                          ").then(w => true)"));
 
+  ASSERT_TRUE(window_controller());
+  ASSERT_TRUE(window_controller()->GetWindowForTesting());
   EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
 
   gfx::NativeWindow native_window_2 = GetOverlayWindow()->GetNativeWindow();
@@ -211,10 +219,10 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 // Tests closing the document picture-in-picture window.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
                        CloseWindow) {
-  LoadTabAndEnterPictureInPicture(
-      browser(), base::FilePath(kPictureInPictureWindowSizePage));
+  LoadTabAndEnterPictureInPicture(browser(), kPictureInPictureWindowSizePage);
 
   ASSERT_TRUE(window_controller());
+  ASSERT_TRUE(window_controller()->GetWindowForTesting());
   EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
   window_controller()->Close(/*should_pause_video=*/true);
 
@@ -224,8 +232,7 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 // Tests navigating the opener closes the picture in picture window.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
                        ClosePictureInPictureWhenOpenerNavigates) {
-  LoadTabAndEnterPictureInPicture(
-      browser(), base::FilePath(kPictureInPictureWindowSizePage));
+  LoadTabAndEnterPictureInPicture(browser(), kPictureInPictureWindowSizePage);
   ASSERT_TRUE(window_controller());
   EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
   GURL test_page_url = ui_test_utils::GetTestUrl(
@@ -234,4 +241,19 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), test_page_url));
 
   EXPECT_FALSE(window_controller()->GetWindowForTesting());
+}
+
+IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
+                       RequiresSecureContext) {
+  GURL test_page_url("http://media/picture-in-picture/blank.html");
+  ASSERT_FALSE(network::IsUrlPotentiallyTrustworthy(test_page_url));
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), test_page_url));
+
+  content::WebContents* active_web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  ASSERT_NE(nullptr, active_web_contents);
+
+  // In an insecure context, there should not be a method.
+  EXPECT_EQ(false, EvalJs(active_web_contents,
+                          "'requestPictureInPictureWindow' in window"));
 }
