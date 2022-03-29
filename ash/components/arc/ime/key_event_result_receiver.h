6bd5985bf5dad6ba2a40c8e4049da7a94b7220f9
Yuichiro Hanada <yhanada@google.com>
2022-03-29 10:06:32 +0000

    arc: Stop recording IME latency on ARC++ apps.
    We got enough data to determine the appropriate timeout.
    
    Bug: b:226572698
    Bug: 1299515
    Test: ash_components_unittests
    Change-Id: Idb495a0fc8e7b588c173b2cff5394d660bea0c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3548939
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986408}

--- ash/components/arc/ime/key_event_result_receiver.h
+++ ash/components/arc/ime/key_event_result_receiver.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
-#include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/ime/input_method_delegate.h"
 #include "ui/events/event.h"
@@ -38,11 +37,8 @@ class KeyEventResultReceiver {
 
   void RunCallbackIfNeeded(bool result);
 
-  void RecordImeLatency();
-
   KeyEventDoneCallback callback_{};
   absl::optional<ui::KeyEvent> expected_key_event_{};
-  absl::optional<base::TimeTicks> callback_set_time_{};
   base::WeakPtrFactory<KeyEventResultReceiver> weak_ptr_factory_{this};
 };
 
