e0e819b892c2bbc47ed5ef59af47dfa690d0053d
Noel Gordon <noel@chromium.org>
2022-03-29 05:02:07 +0000

    fusebox: detect if SelectFileAsh is opening the file app dialog
    Per crbug.com/1292825#c20, files app needs to know if SelectFileAsh is
    opening the files app file picker dialog.
    
    Add a new SelectFileDialogExtension::Owner field that is set true only
    if SelectFileAsh is opening the dialog.
    
    Use that field to populate the file app launch parameter volumeFilter,
    newly added in the CL, with filter name 'fusebox-only' [1].
    
    Plumb the volumeFilter field through the SelectFileDialogExtension C++
    into the files app URL launch parameters.
    
    [1] The filter will make files app use fusebox volumes in the UI so it
    can return real file descriptors to SelectFileAsh.
    
    Bug: 1292825
    Change-Id: Ia5aaf0d5ff0fc053c250a2536ced6ad6642dd4fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555209
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986331}

--- chrome/browser/ash/file_manager/url_util.h
+++ chrome/browser/ash/file_manager/url_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_FILE_MANAGER_URL_UTIL_H_
 
 #include <string>
+#include <vector>
 
 #include "ui/shell_dialogs/select_file_dialog.h"
 #include "url/gurl.h"
@@ -30,7 +31,8 @@ GURL GetFileManagerMainPageUrlWithParams(
     const ui::SelectFileDialog::FileTypeInfo* file_types,
     int file_type_index,
     const std::string& search_query,
-    bool show_android_picker_apps);
+    bool show_android_picker_apps,
+    std::vector<std::string> volume_filter);
 
 }  // namespace util
 }  // namespace file_manager
