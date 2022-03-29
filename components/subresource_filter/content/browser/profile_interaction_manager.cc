8b8425b32f133dbad7abbf5cd68b7f1f6cfe4922
Adithya Srinivasan <adithyas@chromium.org>
2022-03-29 21:57:28 +0000

    MPArch: Update DCHECK in profile_interaction_manager.cc
    Small follow up to https://crrev.com/c/3488833, the DCHECK for can be
    more strict now.
    
    Bug: 1263541
    Change-Id: Ifcf0f6d0660d4a17938081c498e5043a8bac86c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558829
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986717}

--- components/subresource_filter/content/browser/profile_interaction_manager.cc
+++ components/subresource_filter/content/browser/profile_interaction_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/subresource_filter/content/browser/ads_intervention_manager.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.h"
+#include "components/subresource_filter/content/browser/content_subresource_filter_web_contents_helper.h"
 #include "components/subresource_filter/content/browser/subresource_filter_content_settings_manager.h"
 #include "components/subresource_filter/content/browser/subresource_filter_profile_context.h"
 #include "content/public/browser/navigation_controller.h"
@@ -101,7 +102,7 @@ mojom::ActivationLevel ProfileInteractionManager::OnPageActivationComputed(
     content::NavigationHandle* navigation_handle,
     mojom::ActivationLevel initial_activation_level,
     ActivationDecision* decision) {
-  DCHECK(navigation_handle->IsInMainFrame());
+  DCHECK(IsInSubresourceFilterRoot(navigation_handle));
 
   mojom::ActivationLevel effective_activation_level = initial_activation_level;
 
