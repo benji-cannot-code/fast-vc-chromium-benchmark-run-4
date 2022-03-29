42ecbb712847d4ec6d53b74b2e30864cb81bd37e
Monica Basta <msalama@chromium.org>
2022-03-29 14:22:45 +0000

    [Profile] Enable turn sync off button on Lacros
    This CL stops hidding the turn sync off button in the settings page if
    |kLacrosNonSyncingProfiles| is enabled.
    
    'HandleSignout' will be implemented on Lacros in the next CL. Note: The
    implementation is currently empty on Lacros to avoid crashes.
    
    Bug: 1307249
    Change-Id: If1feb71ae7d55d6a1171a311338bcb286c08446d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555671
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986475}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -236,6 +237,9 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
   AddSettingsPageUIHandler(std::make_unique<SystemHandler>());
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   html_source->AddBoolean("isSecondaryUser", !profile->IsMainProfile());
+  html_source->AddBoolean(
+      "nonSyncingProfilesEnabled",
+      base::FeatureList::IsEnabled(switches::kLacrosNonSyncingProfiles));
 #endif
 
 #endif
