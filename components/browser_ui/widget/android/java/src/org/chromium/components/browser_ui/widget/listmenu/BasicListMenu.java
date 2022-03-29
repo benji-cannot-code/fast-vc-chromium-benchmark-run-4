ea306d62978f02919e7951a0733717c5d761323a
Rohit Agarwal <roagarwal@chromium.org>
2022-03-29 11:46:33 +0000

    [Android] [IncognitoReauth] Add three dots menu in incognito reauth.
    This CL adds the implementation of the three dots menu present in the
    incognito reauth full screen view.
    
    UX: https://screenshot.googleplex.com/3CQuZ7pPo8puzDp (normal)
    UX: https://screenshot.googleplex.com/6AncGSeQ5FY3d9a (ellipsis)
    
    Bug: 1227656
    Change-Id: I7f37a2618a7a3a3bd95e31d931028de0c0a3d7a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512617
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986436}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/listmenu/BasicListMenu.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/listmenu/BasicListMenu.java
@@ -6,17 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.widget.listmenu;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+import androidx.annotation.ColorRes;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.IdRes;
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
 
 import org.chromium.components.browser_ui.widget.R;
 import org.chromium.ui.UiUtils;
@@ -113,6 +117,7 @@ public class BasicListMenu implements ListMenu, OnItemClickListener {
      * @param context The {@link Context} to inflate the layout.
      * @param data Data representing the list items.
      *             All items in data are assumed to be enabled.
+     * @param delegate The {@link Delegate} that would be called when the menu is clicked.
      */
     public BasicListMenu(@NonNull Context context, ModelList data, Delegate delegate) {
         mContext = context;
@@ -125,6 +130,20 @@ public class BasicListMenu implements ListMenu, OnItemClickListener {
         mDelegate = delegate;
         mClickRunnables = new LinkedList<>();
         registerListItemTypes();
+    }
+
+    /**
+     * @param context The {@link Context} to inflate the layout.
+     * @param data Data representing the list items.
+     *             All items in data are assumed to be enabled.
+     * @param delegate The {@link Delegate} that would be called when the menu is clicked.
+     * @param backgroundTintColor The background tint color of the menu.
+     */
+    public BasicListMenu(@NonNull Context context, ModelList data, Delegate delegate,
+            @ColorRes int backgroundTintColor) {
+        this(context, data, delegate);
+        ViewCompat.setBackgroundTintList(mContentView,
+                ColorStateList.valueOf(ContextCompat.getColor(mContext, backgroundTintColor)));
     }
 
     @Override
