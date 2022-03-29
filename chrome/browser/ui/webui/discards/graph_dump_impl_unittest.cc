7d11490e96e782464b26ca8d0f1c9c5232af3739
David Bienvenu <davidbienvenu@chromium.org>
2022-03-29 13:58:20 +0000

    codehealth: Modernize base::Value usage in cbui/webui/
    Also fix cpp lint warnings.
    
    Bug: 1187025, 1187026
    Change-Id: Ife61c5d470c15d9b41156018566b90ebda6e0993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551904
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986466}

--- chrome/browser/ui/webui/discards/graph_dump_impl_unittest.cc
+++ chrome/browser/ui/webui/discards/graph_dump_impl_unittest.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <set>
+#include <string>
 #include <utility>
+#include <vector>
 
 #include "base/containers/contains.h"
 #include "base/json/json_reader.h"
@@ -314,7 +316,7 @@ TEST_F(DiscardsGraphDumpImplTest, ChangeStream) {
                 absl::optional<base::Value> v =
                     base::JSONReader::Read(kv.second);
                 EXPECT_TRUE(v->is_dict());
-                std::string* str = v->FindStringKey("test");
+                std::string* str = v->GetDict().FindString("test");
                 EXPECT_TRUE(str);
                 if (str) {
                   EXPECT_TRUE(*str == "frame" || *str == "page" ||
