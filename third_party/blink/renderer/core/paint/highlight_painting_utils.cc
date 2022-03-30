3618fe858a10e6de16ba2e80437295a5df8e7dd3
Luis Juan Sanchez Padilla <lusanpad@microsoft.com>
2022-03-30 17:56:23 +0000

    Custom Highlight uses new Highlight Inheritance model by default
    This change enables Custom Highlights to use the new Highlight
    Inheritance model without the need of enabling the new model's feature
    flag, and without affecting the behaviour of other types of highlights.
    
    Bug: 1024156,1164461
    Change-Id: Ic2231b5700e7bc4fbcbea9f4aa67366e3253637e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498908
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Delan Azabani <dazabani@igalia.com>
    Commit-Queue: Luis Sanchez Padilla <lusanpad@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#987061}

--- third_party/blink/renderer/core/paint/highlight_painting_utils.cc
+++ third_party/blink/renderer/core/paint/highlight_painting_utils.cc
@@ -210,8 +210,10 @@ Color HighlightColor(const Document& document,
                                                    pseudo_argument);
 
   mojom::blink::ColorScheme color_scheme = style.UsedColorScheme();
-  if (pseudo_style && (!RuntimeEnabledFeatures::HighlightInheritanceEnabled() ||
-                       !UseUaHighlightColors(pseudo, *pseudo_style))) {
+  if (pseudo_style &&
+      ((!RuntimeEnabledFeatures::HighlightInheritanceEnabled() &&
+        pseudo != PseudoId::kPseudoIdHighlight) ||
+       !UseUaHighlightColors(pseudo, *pseudo_style))) {
     if (!document.InForcedColorsMode() ||
         pseudo_style->ForcedColorAdjust() != EForcedColorAdjust::kAuto) {
       if (pseudo_style->ColorIsCurrentColor()) {
@@ -239,7 +241,8 @@ scoped_refptr<const ComputedStyle> HighlightPaintingUtils::HighlightPseudoStyle(
     const ComputedStyle& style,
     PseudoId pseudo,
     const AtomicString& pseudo_argument) {
-  if (!RuntimeEnabledFeatures::HighlightInheritanceEnabled()) {
+  if (!RuntimeEnabledFeatures::HighlightInheritanceEnabled() &&
+      pseudo != PseudoId::kPseudoIdHighlight) {
     return HighlightPseudoStyleWithOriginatingInheritance(node, pseudo,
                                                           pseudo_argument);
   }
@@ -279,8 +282,10 @@ Color HighlightPaintingUtils::HighlightBackgroundColor(
       HighlightPseudoStyle(node, style, pseudo, pseudo_argument);
 
   mojom::blink::ColorScheme color_scheme = style.UsedColorScheme();
-  if (pseudo_style && (!RuntimeEnabledFeatures::HighlightInheritanceEnabled() ||
-                       !UseUaHighlightColors(pseudo, *pseudo_style))) {
+  if (pseudo_style &&
+      ((!RuntimeEnabledFeatures::HighlightInheritanceEnabled() &&
+        pseudo != PseudoId::kPseudoIdHighlight) ||
+       !UseUaHighlightColors(pseudo, *pseudo_style))) {
     if (!document.InForcedColorsMode() ||
         pseudo_style->ForcedColorAdjust() != EForcedColorAdjust::kAuto) {
       Color highlight_color =
