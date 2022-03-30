5f421ce79fc1e190f5b87497bfc26b1561aa1f7c
Quentin Pubert <qpubert@google.com>
2022-03-30 10:36:24 +0000

    [iOS] Add "Set Chrome as Default Browser" pedal
    This CL adds the "Set Chrome as Default Browser" pedal to the iOS
    omnibox. It does not add any icons or the subtitle as those aren't
    available yet. Tests will also be added along with those.
    
    Bug: 1308814
    Change-Id: I0e3c2a312bd72e7375e616e657e9d63fb4bd96c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557273
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#986919}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_pedal_annotator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_pedal_annotator.mm
@@ -49,6 +49,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                   [pedalsEndpoint showClearBrowsingDataSettings];
                 }];
     }
+    case (int)OmniboxPedalId::SET_CHROME_AS_DEFAULT_BROWSER: {
+      return [[OmniboxPedalData alloc]
+          initWithHint:hint
+                action:^{
+                  [omniboxCommandHandler cancelOmniboxEdit];
+                  [pedalsEndpoint
+                      showDefaultBrowserSettingsFromViewController:nil];
+                }];
+    }
     default:
       return nil;
   }
