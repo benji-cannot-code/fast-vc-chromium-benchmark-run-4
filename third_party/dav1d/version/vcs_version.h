0d4749ed57383ebeb0bbc2a9a6efccc887f19621
Wan-Teh Chang <wtc@google.com>
2022-03-30 17:18:19 +0000

    Roll src/third_party/dav1d/libdav1d/ 56e7ffc0d..87f9a81cd (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/56e7ffc0dbe4..87f9a81cd770
    
    $ git log 56e7ffc0d..87f9a81cd --date=short --no-merges --format='%ad %ae %s'
    2022-03-19 gramner checkasm: Fix alignment of stack buffers in the film grain tests
    2022-03-18 jb 1.0.0
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,sandersd@chromium.org
    
    Change-Id: I7b6eba980daa6d1cc6b8293ef18ecb5cb0966d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3559549
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987044}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "0.9.2-174-g56e7ffc"
+#define DAV1D_VERSION "1.0.0-1-g87f9a81"
