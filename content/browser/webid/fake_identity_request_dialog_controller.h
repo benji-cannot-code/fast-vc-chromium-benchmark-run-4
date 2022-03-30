207c5864b849aa011ae853aff45164f1797d04e2
Christian Biesinger <cbiesinger@chromium.org>
2022-03-30 22:37:25 +0000

    [FedCM] Implement a command line option to use a fake UI
    This is useful for WPT tests and Webdriver, along the lines of the
    existing --use-fake-ui-for-media-stream.
    
    This also merges the two existing fake dialogs into a single impl
    in content/public/browser.
    
    Bug: 1302097
    Change-Id: I7bc173c432a17755c2c3ea7ecc9c4c64fd3166d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546261
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987225}

--- content/browser/webid/fake_identity_request_dialog_controller.h
+++ content/browser/webid/fake_identity_request_dialog_controller.h
@@ -1,39 +1,29 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_WEBID_TEST_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
-#define CONTENT_BROWSER_WEBID_TEST_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
+#ifndef CONTENT_BROWSER_WEBID_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
+#define CONTENT_BROWSER_WEBID_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
 
 #include <string>
 
 #include "content/public/browser/identity_request_dialog_controller.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "url/gurl.h"
 
 namespace content {
 
-class WebContents;
-
 // This fakes the request dialogs to always provide user consent.
-// Tests that need to vary the responses or set test expectations should use
-// MockIdentityRequestDialogController.
-// This also fakes an IdP sign-in page until tests can be set up to
-// verify the FederatedAuthResponse mechanics.
-class FakeIdentityRequestDialogController
+// Used by tests and if the --use-fake-ui-for-fedcm command-line
+// flag is provided.
+class CONTENT_EXPORT FakeIdentityRequestDialogController
     : public IdentityRequestDialogController {
  public:
   explicit FakeIdentityRequestDialogController(
-      absl::optional<std::string> dialog_selected_account);
+      absl::optional<std::string> selected_account = absl::nullopt);
   ~FakeIdentityRequestDialogController() override;
 
-  FakeIdentityRequestDialogController(
-      const FakeIdentityRequestDialogController&) = delete;
-  FakeIdentityRequestDialogController& operator=(
-      const FakeIdentityRequestDialogController&) = delete;
-
-  void ShowAccountsDialog(WebContents* rp_web_contents,
+  void ShowAccountsDialog(content::WebContents* rp_web_contents,
                           const GURL& idp_signin_url,
                           base::span<const IdentityRequestAccount> accounts,
                           const IdentityProviderMetadata& idp_metadata,
@@ -42,9 +32,9 @@ class FakeIdentityRequestDialogController
                           AccountSelectionCallback on_selected) override;
 
  private:
-  absl::optional<std::string> dialog_selected_account_;
+  absl::optional<std::string> selected_account_;
 };
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_WEBID_TEST_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
+#endif  // CONTENT_BROWSER_WEBID_FAKE_IDENTITY_REQUEST_DIALOG_CONTROLLER_H_
