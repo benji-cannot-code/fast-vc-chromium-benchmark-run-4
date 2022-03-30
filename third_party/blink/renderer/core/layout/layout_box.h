7130692e704401bb76ca9349ee1b2fd1e7cd0900
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-03-30 00:44:02 +0000

    [cleanup] Remove LayoutBox::ScrollByRecursively...
    and...
     - LayoutBox::CanAutoscroll
     - AutoscrollController::UpdateAutoscrollLayoutObject
    
    There should be no behaviour change.
    
    Change-Id: Icfdb167747618e7c3ec778b808db7525ae6a0e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558757
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986792}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -888,7 +888,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   int PixelSnappedScrollWidth() const;
   int PixelSnappedScrollHeight() const;
 
-  void ScrollByRecursively(const ScrollOffset& delta);
   // If makeVisibleInVisualViewport is set, the visual viewport will be scrolled
   // if required to make the rect visible.
   PhysicalRect ScrollRectToVisibleRecursive(
@@ -1511,7 +1510,6 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   bool CanBeScrolledAndHasScrollableArea() const;
   virtual bool CanBeProgramaticallyScrolled() const;
   virtual void Autoscroll(const PhysicalOffset&);
-  bool CanAutoscroll() const;
   PhysicalOffset CalculateAutoscrollDirection(
       const gfx::PointF& point_in_root_frame) const;
   static LayoutBox* FindAutoscrollable(LayoutObject*,
