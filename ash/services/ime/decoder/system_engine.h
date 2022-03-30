2d8f252966d08c332f5de6c50683bef2bd08e751
Bao-Duy Tran <tranbaoduy@chromium.org>
2022-03-30 12:54:47 +0000

    Dismantle unnecessary InputEngine interface.
    At this point, InputEngine interface has one and only one impl
    SystemEngine, and serves no purposes. It'd been confusing, thus
    incrementally dismantled for clarity. This CL is the finale.
    
    While bearing a sophisticated sounding name, it defines nothing but
    generic IsConnected(), and used to facilitate polymorphism among
    unrelated stuff connected and managed in totally different ways:
    
    - RuleBasedEngine (IMEs embedded directly in IME service container).
    - SystemEngine (IMEs in 1P shared lib, connected via its MojoMode).
    
    At the same time, counterpart DecoderEngine (IMEs in 1P shared lib,
    connected via its ProtoMode) was left out of this polymorphism.
    
    NOTE: Naming of these three "engines" is vague and confusing, barely
    reflecting what they truly are, but that's out of scope here.
    
    __SIMPLE_REVIEW__
    
    Bug: 1287417
    Change-Id: Id81524abf7f94b31a6aed8885dbca791a51d3e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554229
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986951}

--- ash/services/ime/decoder/system_engine.h
+++ ash/services/ime/decoder/system_engine.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SERVICES_IME_DECODER_SYSTEM_ENGINE_H_
 
 #include "ash/services/ime/ime_decoder.h"
-#include "ash/services/ime/input_engine.h"
 #include "ash/services/ime/public/cpp/shared_lib/interfaces.h"
 #include "ash/services/ime/public/mojom/connection_factory.mojom.h"
 #include "ash/services/ime/public/mojom/input_engine.mojom.h"
@@ -25,14 +24,14 @@ namespace ime {
 
 // An enhanced implementation of the basic InputEngine that uses a built-in
 // shared library for handling key events.
-class SystemEngine : public InputEngine {
+class SystemEngine {
  public:
   explicit SystemEngine(ImeCrosPlatform* platform,
                         absl::optional<ImeDecoder::EntryPoints> entry_points);
 
   SystemEngine(const SystemEngine&) = delete;
   SystemEngine& operator=(const SystemEngine&) = delete;
-  ~SystemEngine() override;
+  ~SystemEngine();
 
   // Binds the mojom::InputMethod interface to this object and returns true if
   // the given ime_spec is supported by the engine.
@@ -44,8 +43,7 @@ class SystemEngine : public InputEngine {
   bool BindConnectionFactory(
       mojo::PendingReceiver<mojom::ConnectionFactory> receiver);
 
-  // InputEngine:
-  bool IsConnected() override;
+  bool IsConnected();
 
  private:
   absl::optional<ImeDecoder::EntryPoints> decoder_entry_points_;
