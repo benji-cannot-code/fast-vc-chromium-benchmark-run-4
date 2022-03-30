915ecc521a5b4b151f49452d03e2c3d16a381569
Brandon Wylie <wylieb@chromium.org>
2022-03-30 22:29:03 +0000

    [Sheriff] Extend video callback time for PhotoPickerDialogTest
    Bug: 1311783
    Change-Id: I23213087a11e9ccc157ed1a58f85afcdad1bc47b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3560913
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987222}

--- components/browser_ui/photo_picker/android/java/src/org/chromium/components/browser_ui/photo_picker/PhotoPickerDialogTest.java
+++ components/browser_ui/photo_picker/android/java/src/org/chromium/components/browser_ui/photo_picker/PhotoPickerDialogTest.java
@@ -68,6 +68,7 @@ public class PhotoPickerDialogTest extends BlankUiTestActivityTestCase
 
     // The timeout (in seconds) to wait for the decoder service to be ready.
     private static final long WAIT_TIMEOUT_SECONDS = 30L;
+    private static final long VIDEO_TIMEOUT_SECONDS = 10L;
 
     @Rule
     public RenderTestRule mRenderTestRule =
@@ -349,7 +350,8 @@ public class PhotoPickerDialogTest extends BlankUiTestActivityTestCase
         int callCount = mOnVideoPlayingCallback.getCallCount();
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> { mDialog.getCategoryViewForTesting().startVideoPlaybackAsync(uri); });
-        mOnVideoPlayingCallback.waitForCallback(callCount, 1);
+        mOnVideoPlayingCallback.waitForCallback(
+                callCount, 1, VIDEO_TIMEOUT_SECONDS, TimeUnit.SECONDS);
     }
 
     private void dismissDialog() {
