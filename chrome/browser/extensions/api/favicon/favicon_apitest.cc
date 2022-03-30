a0954b595b70e3e2c7f1b5c2b770eaedf41030b7
Solomon Kinard <solomonkinard@chromium.org>
2022-03-30 20:19:27 +0000

    Extensions: Favicon: Add stub for _favicon endpoint
    This CL hooks up the endpoint in extension_protocols.cc with a
    handler that returns a constant value. Future CLs will return the
    real favicon value.
    
    Bug: 104102
    Change-Id: I613d37b9b41456bbef12fb372d27e20c80bd673a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508336
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987141}

--- chrome/browser/extensions/api/favicon/favicon_apitest.cc
+++ chrome/browser/extensions/api/favicon/favicon_apitest.cc
@@ -3,14 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // // Use of this source code is governed by a BSD-style license that can be
 // // found in the LICENSE file.
 
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "components/version_info/channel.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/common/extension_features.h"
 
 namespace extensions {
 
 class FaviconApiTest : public ExtensionApiTest {
+ public:
+  FaviconApiTest() {
+    feature_list_.InitAndEnableFeature(
+        extensions_features::kNewExtensionFaviconHandling);
+  }
+
  private:
+  base::test::ScopedFeatureList feature_list_;
   ScopedCurrentChannel current_cnannel_{version_info::Channel::CANARY};
 };
 
