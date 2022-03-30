679952fb3d87c7a40a169f07efcaa6501e5dd173
Ovidio Ruiz-Henríquez <odejesush@chromium.org>
2022-03-30 16:24:42 +0000

    Enforce header policies for isolated apps
    This changes injects the proper COOP, COEP, CORP, and X-Frame-Options
    values if a navigation request is being made to an isolated app.
    
    BUG=1282072
    
    Change-Id: Ib9eaed41c8c6cd4bfd22a6007bc9b3f9e38a950b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497999
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987024}

--- content/browser/renderer_host/isolated_app_throttle_unittest.cc
+++ content/browser/renderer_host/isolated_app_throttle_unittest.cc
@@ -269,24 +269,6 @@ TEST_F(IsolatedAppThrottleTest, CancelCrossOriginNavigation) {
       ui::PageTransition::PAGE_TRANSITION_LINK));
 }
 
-TEST_F(IsolatedAppThrottleTest, BlockNavigationWithinIsolatedAppWithBadCoop) {
-  CommitBrowserInitiatedNavigation(kAppUrl, coop_coep_headers());
-  EXPECT_EQ(kMaybeIsolatedApplication,
-            GetWebExposedIsolationLevel(main_frame_id()));
-
-  auto simulator = StartRendererInitiatedNavigation(main_frame_id(), kAppUrl2);
-
-  auto start_result = simulator->GetLastThrottleCheckResult();
-  EXPECT_EQ(NavigationThrottle::PROCEED, start_result.action());
-
-  // Don't set COEP/COOP headers, which will prevent the navigation from having
-  // application isolation.
-  simulator->ReadyToCommit();
-
-  auto response_result = simulator->GetLastThrottleCheckResult();
-  EXPECT_EQ(NavigationThrottle::BLOCK_RESPONSE, response_result.action());
-}
-
 TEST_F(IsolatedAppThrottleTest, BlockRedirectOutOfIsolatedApp) {
   CommitBrowserInitiatedNavigation(kAppUrl, coop_coep_headers());
   EXPECT_EQ(kMaybeIsolatedApplication,
@@ -309,13 +291,13 @@ TEST_F(IsolatedAppThrottleTest, BlockRedirectOutOfIsolatedApp) {
 }
 
 TEST_F(IsolatedAppThrottleTest, AllowIframeNavigationOutOfApp) {
-  CommitBrowserInitiatedNavigation(kAppUrl, coop_coep_headers());
+  CommitBrowserInitiatedNavigation(kAppUrl);
   EXPECT_EQ(kMaybeIsolatedApplication,
             GetWebExposedIsolationLevel(main_frame_id()));
   int iframe_id = CreateIframe(main_frame_id(), "test_frame");
 
   // Navigate the iframe to an app page.
-  CommitRendererInitiatedNavigation(iframe_id, kAppUrl, corp_coep_headers());
+  CommitRendererInitiatedNavigation(iframe_id, kAppUrl);
 
   // Navigate the iframe to a non-app page.
   CommitRendererInitiatedNavigation(iframe_id, kNonAppUrl, corp_coep_headers());
@@ -340,13 +322,13 @@ TEST_F(IsolatedAppThrottleTest,
 
 TEST_F(IsolatedAppThrottleTest,
        AllowIframeBrowserInitiatedNavigationIntoIsolatedApp) {
-  CommitBrowserInitiatedNavigation(kAppUrl, coop_coep_headers());
+  CommitBrowserInitiatedNavigation(kAppUrl);
   EXPECT_EQ(kMaybeIsolatedApplication,
             GetWebExposedIsolationLevel(main_frame_id()));
   int iframe_id = CreateIframe(main_frame_id(), "test_frame");
 
   // Navigate the iframe to an app page.
-  CommitRendererInitiatedNavigation(iframe_id, kAppUrl, corp_coep_headers());
+  CommitRendererInitiatedNavigation(iframe_id, kAppUrl);
 
   // Navigate the iframe to a non-app page.
   CommitRendererInitiatedNavigation(iframe_id, kNonAppUrl, corp_coep_headers());
@@ -362,7 +344,6 @@ TEST_F(IsolatedAppThrottleTest,
   // incorrectly choose the main frame as the one being navigated.
   simulator = NavigationSimulatorImpl::CreateFromPendingInFrame(
       FrameTreeNode::GloballyFindByID(iframe_id));
-  simulator->SetResponseHeaders(corp_coep_headers());
   simulator->Commit();
 
   auto commit_result = simulator->GetLastThrottleCheckResult();
@@ -381,7 +362,6 @@ TEST_F(IsolatedAppThrottleTest, BlockIframeRedirectOutThenIntoIsolatedApp) {
   EXPECT_EQ(NavigationThrottle::PROCEED, start_result.action());
 
   // Redirect to a non-app page.
-  simulator->SetRedirectHeaders(corp_coep_headers());
   simulator->Redirect(GURL(kNonAppUrl));
 
   auto redirect_result1 = simulator->GetLastThrottleCheckResult();
