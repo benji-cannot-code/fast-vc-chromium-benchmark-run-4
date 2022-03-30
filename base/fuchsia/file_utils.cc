c3d87708bb45c7ee268f3cdd74e55afca01773f3
Kevin Marshall <kmarshall@chromium.org>
2022-03-30 20:30:04 +0000

    [network] Introduce TransferableDirectory for sandbox directory sharing.
    Adds a Mojo type and a native class called "TransferableDirectory"
    for taking paths and marshaling them as handles across IPC boundaries.
    
    Platform support is added for Fuchsia. All other platforms which don't
    require handle sharing can also use TransferableDirectory (skipping the
    marshaling step).
    
    This allows for a high degree of consistency across platforms and
    minimal semantic changes to existing code.
    
    Bug: 1290481
    Change-Id: I7c4eb95325b8bb867e7067b4b164adf8846356fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533056
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987149}

--- base/fuchsia/file_utils.cc
+++ base/fuchsia/file_utils.cc
@@ -18,14 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/fuchsia_logging.h"
 namespace base {
 
-const char kPersistedDataDirectoryPath[] = "/data";
-const char kPersistedCacheDirectoryPath[] = "/cache";
-const char kServiceDirectoryPath[] = "/svc";
-const char kPackageRootDirectoryPath[] = "/pkg";
+namespace {
 
-fidl::InterfaceHandle<::fuchsia::io::Directory> OpenDirectoryHandle(
-    const base::FilePath& path) {
-  ScopedFD fd(open(path.value().c_str(), O_DIRECTORY | O_RDONLY));
+fidl::InterfaceHandle<::fuchsia::io::Directory> OpenDirectoryHandleInternal(
+    const base::FilePath& path,
+    bool read_only) {
+  ScopedFD fd(open(path.value().c_str(),
+                   O_DIRECTORY | (read_only ? O_RDONLY : O_RDWR)));
   if (!fd.is_valid()) {
     DPLOG(ERROR) << "Failed to open " << path;
     return fidl::InterfaceHandle<::fuchsia::io::Directory>();
@@ -42,6 +41,23 @@ fidl::InterfaceHandle<::fuchsia::io::Directory> OpenDirectoryHandle(
   }
 
   return fidl::InterfaceHandle<::fuchsia::io::Directory>(std::move(channel));
+}
+
+}  // namespace
+
+const char kPersistedDataDirectoryPath[] = "/data";
+const char kPersistedCacheDirectoryPath[] = "/cache";
+const char kServiceDirectoryPath[] = "/svc";
+const char kPackageRootDirectoryPath[] = "/pkg";
+
+fidl::InterfaceHandle<::fuchsia::io::Directory> OpenDirectoryHandle(
+    const base::FilePath& path) {
+  return OpenDirectoryHandleInternal(path, true);
+}
+
+fidl::InterfaceHandle<::fuchsia::io::Directory> OpenWritableDirectoryHandle(
+    const base::FilePath& path) {
+  return OpenDirectoryHandleInternal(path, false);
 }
 
 }  // namespace base
