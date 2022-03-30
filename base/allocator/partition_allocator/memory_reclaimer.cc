d6bf8da5563138daa6f1cf98e118bed502429656
Takashi Sakamoto <tasak@google.com>
2022-03-30 02:35:33 +0000

    Revert "[PA] Copy //base/debug/alias.h to //base/allocator/partition_allocator/partition_alloc/base/debug/."
    This reverts commit 80cc651e88644dd54bdc5b0bf89dc6820331d1a4.
    
    Reason for revert: we will use mitigation_adapter.h instead of manual replacement.
    
    Original change's description:
    > [PA] Copy //base/debug/alias.h to //base/allocator/partition_allocator/partition_alloc/base/debug/.
    >
    > Make partition_alloc to use ::base instead of base, because
    > partition_alloc::internal::base is introduced.
    > Remove DEBUG_ALIAS_FOR_CSTR from the copied alias.h, because
    > partition_allocator doesn't depend on DEBUG_ALIAS_FOR_CSTR.
    >
    > Bug: 1151236
    > Change-Id: I127364cefe88b9b368b169afb25fa36144f7f582
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554735
    > Reviewed-by: Benoit Lize <lizeb@chromium.org>
    > Commit-Queue: Takashi Sakamoto <tasak@google.com>
    > Cr-Commit-Position: refs/heads/main@{#986295}
    
    Bug: 1151236
    Change-Id: Ie3d30a90d6d518e5522b82ea7ad97977290d800b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3556273
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#986804}

--- base/allocator/partition_allocator/memory_reclaimer.cc
+++ base/allocator/partition_allocator/memory_reclaimer.cc
@@ -19,7 +19,7 @@ namespace partition_alloc {
 
 // static
 MemoryReclaimer* MemoryReclaimer::Instance() {
-  static ::base::NoDestructor<MemoryReclaimer> instance;
+  static base::NoDestructor<MemoryReclaimer> instance;
   return instance.get();
 }
 
--- base/allocator/partition_allocator/partition_address_space.cc
+++ base/allocator/partition_allocator/partition_address_space.cc
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/address_pool_manager.h"
 #include "base/allocator/partition_allocator/page_allocator.h"
-#include "base/allocator/partition_allocator/partition_alloc/base/debug/alias.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/allocator/partition_allocator/tagging.h"
 #include "base/bits.h"
 #include "base/compiler_specific.h"
+#include "base/debug/alias.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -32,19 +32,19 @@ namespace {
 
 #if BUILDFLAG(IS_WIN)
 NOINLINE void HandleGigaCageAllocFailureOutOfVASpace() {
-  PA_NO_CODE_FOLDING();
+  NO_CODE_FOLDING();
   PA_CHECK(false);
 }
 
 NOINLINE void HandleGigaCageAllocFailureOutOfCommitCharge() {
-  PA_NO_CODE_FOLDING();
+  NO_CODE_FOLDING();
   PA_CHECK(false);
 }
 #endif  // BUILDFLAG(IS_WIN)
 
 NOINLINE void HandleGigaCageAllocFailure() {
-  PA_NO_CODE_FOLDING();
-  uint32_t alloc_page_error_code = ::base::GetAllocPageErrorCode();
+  NO_CODE_FOLDING();
+  uint32_t alloc_page_error_code = base::GetAllocPageErrorCode();
   PA_DEBUG_DATA_ON_STACK("error", static_cast<size_t>(alloc_page_error_code));
   // It's important to easily differentiate these two failures on Windows, so
   // crash with different stacks.
@@ -148,7 +148,7 @@ void PartitionAddressSpace::InitConfigurablePool(uintptr_t pool_base,
   PA_CHECK(pool_base);
   PA_CHECK(size <= kConfigurablePoolMaxSize);
   PA_CHECK(size >= kConfigurablePoolMinSize);
-  PA_CHECK(::base::bits::IsPowerOfTwo(size));
+  PA_CHECK(base::bits::IsPowerOfTwo(size));
   PA_CHECK(pool_base % size == 0);
 
   setup_.configurable_pool_base_address_ = pool_base;
