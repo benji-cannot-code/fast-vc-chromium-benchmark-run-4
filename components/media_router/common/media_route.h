35a16de3982397e13d2d37e82c428ae4fbada954
ahmedmoussa <ahmedmoussa@google.com>
2022-03-30 20:35:46 +0000

    Eliminate MediaSource default constructor and  use absl::optional<MediaSource> instead
    Having instances of MediaSource that don't actually represent a media
     source is sloppy. Eliminate MediaSource::MediaSource() and use
     absl::optional<MediaSource> where the media source is truly optional.
    
    Bug: 1291731
    Change-Id: Ic373dde6453dd82a91f62a22612be08f66d543ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550020
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#987155}

--- components/media_router/common/media_route.h
+++ components/media_router/common/media_route.h
@@ -50,6 +50,9 @@ class MediaRoute {
              const std::string& description,
              bool is_local);
   MediaRoute(const MediaRoute& other);
+
+  // TODO(crbug.com/1311341): Delete the default constructor and
+  // disallow passing in an empty string into the MediaSource ctor.
   MediaRoute();
 
   ~MediaRoute();
