68456a22ca5f89d06ffdc6621fe56eadcb4adfbb
Wes Okuhara <wesokuhara@google.com>
2022-03-30 16:13:34 +0000

    [CrOS Settings] Check-in Polymer3 code for nearby share browser tests
    We no longer need to generate modulized versions of the JS test files
    (`.m.js`) at build time. Instead, we can check-in the modulized version
    and then use the test source files directly in
    `os_settings_v3_browsertest.js`.
    
    Bug: 1180979
    Test: out/Default/browser_tests --gtest_filter='*OSSettingsNearby*'
    Change-Id: Ie1baeb3003a20f3f31495a3c5db8e5170b1954db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558898
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#987015}

--- chrome/test/data/webui/settings/chromeos/nearby_share_receive_dialog_tests.js
+++ chrome/test/data/webui/settings/chromeos/nearby_share_receive_dialog_tests.js
@@ -3,24 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// clang-format off
-// #import {assertEquals} from '../../chai_assert.js';
-// #import {isChildVisible, waitAfterNextRender} from '../../test_util.js';
-// #import {setNearbyShareSettingsForTesting, setReceiveManagerForTesting, setContactManagerForTesting} from 'chrome://os-settings/chromeos/os_settings.js';
-// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-// #import {FakeContactManager} from '../../nearby_share/shared/fake_nearby_contact_manager.m.js';
-// #import {FakeNearbyShareSettings} from '../../nearby_share/shared/fake_nearby_share_settings.m.js';
-// #import {FakeReceiveManager} from './fake_receive_manager.m.js'
-// clang-format on
+import {setContactManagerForTesting, setNearbyShareSettingsForTesting, setReceiveManagerForTesting} from 'chrome://os-settings/chromeos/os_settings.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {assertEquals} from '../../chai_assert.js';
+import {FakeContactManager} from '../../nearby_share/shared/fake_nearby_contact_manager.m.js';
+import {FakeNearbyShareSettings} from '../../nearby_share/shared/fake_nearby_share_settings.m.js';
+import {isChildVisible, waitAfterNextRender} from '../../test_util.js';
+
+import {FakeReceiveManager} from './fake_receive_manager.m.js';
 
 suite('NearbyShare', function() {
   /** @type {!NearbyShareReceiveDialogElement} */
   let dialog;
   /** @type {!FakeReceiveManager} */
   let fakeReceiveManager;
-  /** @type {!nearby_share.FakeContactManager} */
+  /** @type {!FakeContactManager} */
   let fakeContactManager;
-  /** @type {!nearby_share.FakeNearbyShareSettings} */
+  /** @type {!FakeNearbyShareSettings} */
   let fakeSettings;
 
   /**
@@ -31,13 +31,13 @@ suite('NearbyShare', function() {
    *     isOnboardingComplete setting.
    */
   function sharedSetup(enabled, isOnboardingComplete) {
-    fakeReceiveManager = new nearby_share.FakeReceiveManager();
-    fakeContactManager = new nearby_share.FakeContactManager();
-    fakeSettings = new nearby_share.FakeNearbyShareSettings();
+    fakeReceiveManager = new FakeReceiveManager();
+    fakeContactManager = new FakeContactManager();
+    fakeSettings = new FakeNearbyShareSettings();
 
-    nearby_share.setReceiveManagerForTesting(fakeReceiveManager);
-    nearby_share.setContactManagerForTesting(fakeContactManager);
-    nearby_share.setNearbyShareSettingsForTesting(fakeSettings);
+    setReceiveManagerForTesting(fakeReceiveManager);
+    setContactManagerForTesting(fakeContactManager);
+    setNearbyShareSettingsForTesting(fakeSettings);
 
     PolymerTest.clearBody();
     fakeSettings.setEnabled(enabled);
@@ -51,7 +51,7 @@ suite('NearbyShare', function() {
     };
     dialog.isSettingsRetreived = true;
     document.body.appendChild(dialog);
-    Polymer.dom.flush();
+    flush();
   }
 
   /**
@@ -59,7 +59,7 @@ suite('NearbyShare', function() {
    * @return {boolean} Returns true if the element is visible in the shadow dom.
    */
   function isVisible(selector) {
-    return test_util.isChildVisible(dialog, selector, false);
+    return isChildVisible(dialog, selector, false);
   }
 
   /**
@@ -82,7 +82,7 @@ suite('NearbyShare', function() {
     setup(function() {
       sharedSetup(/*enabled=*/ true, /*isOnboardingComplete=*/ true);
       dialog.showHighVisibilityPage(/*shutoffTimeoutInSeconds=*/ 5 * 60);
-      Polymer.dom.flush();
+      flush();
     });
 
     teardown(function() {
@@ -90,7 +90,7 @@ suite('NearbyShare', function() {
     });
 
     test('show high visibility page, get a target, accept', async function() {
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
       // When attached we enter high visibility mode by default
       assertTrue(isVisible('nearby-share-high-visibility-page'));
       assertFalse(isVisible('nearby-share-confirm-page'));
@@ -98,14 +98,14 @@ suite('NearbyShare', function() {
       const target =
           fakeReceiveManager.simulateShareTargetArrival('testName', '1234');
       const confirmPage = dialog.$$('nearby-share-confirm-page');
-      Polymer.dom.flush();
+      flush();
 
       const progressIcon = confirmPage.$$('#progressIcon');
       assertTrue(!!progressIcon.shareTarget);
       assertEquals(target.name, progressIcon.shareTarget.name);
       assertTrue(
           confirmPage.$$('#connectionToken').textContent.includes('1234'));
-      assertTrue(test_util.isChildVisible(confirmPage, 'nearby-preview'));
+      assertTrue(isChildVisible(confirmPage, 'nearby-preview'));
 
       confirmPage.$$('nearby-page-template').$$('#actionButton').click();
       const shareTargetId = await fakeReceiveManager.whenCalled('accept');
@@ -113,7 +113,7 @@ suite('NearbyShare', function() {
     });
 
     test('show high visibility page, get a target, reject', async function() {
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
       // When attached we enter high visibility mode by default
       assertTrue(isVisible('nearby-share-high-visibility-page'));
       assertFalse(isVisible('nearby-share-confirm-page'));
@@ -121,14 +121,14 @@ suite('NearbyShare', function() {
       const target =
           fakeReceiveManager.simulateShareTargetArrival('testName', '1234');
       const confirmPage = dialog.$$('nearby-share-confirm-page');
-      Polymer.dom.flush();
+      flush();
 
       const progressIcon = confirmPage.$$('#progressIcon');
       assertTrue(!!progressIcon.shareTarget);
       assertEquals(target.name, progressIcon.shareTarget.name);
       assertTrue(
           confirmPage.$$('#connectionToken').textContent.includes('1234'));
-      assertTrue(test_util.isChildVisible(confirmPage, 'nearby-preview'));
+      assertTrue(isChildVisible(confirmPage, 'nearby-preview'));
 
       confirmPage.$$('nearby-page-template').$$('#cancelButton').click();
       const shareTargetId = await fakeReceiveManager.whenCalled('reject');
@@ -138,13 +138,13 @@ suite('NearbyShare', function() {
     test(
         'show high visibility page, unregister surface, closes dialog',
         async function() {
-          await test_util.waitAfterNextRender(dialog);
+          await waitAfterNextRender(dialog);
           // When attached we enter high visibility mode by default
           assertTrue(isVisible('nearby-share-high-visibility-page'));
           assertFalse(isVisible('nearby-share-confirm-page'));
           // If a share target comes in, we show it.
           await fakeReceiveManager.unregisterForegroundReceiveSurface();
-          Polymer.dom.flush();
+          flush();
           assertTrue(dialog.closing_);
           assertFalse(isVisible('cr-dialog'));
         });
@@ -152,28 +152,28 @@ suite('NearbyShare', function() {
     test(
         'OnTransferUpdate, unregister surface, does not close dialog',
         async function() {
-          await test_util.waitAfterNextRender(dialog);
+          await waitAfterNextRender(dialog);
           // When attached we enter high visibility mode by default
           assertTrue(isVisible('nearby-share-high-visibility-page'));
           assertFalse(isVisible('nearby-share-confirm-page'));
           // If a share target comes in, we show it.
           const target =
               fakeReceiveManager.simulateShareTargetArrival('testName', '1234');
-          Polymer.dom.flush();
+          flush();
           assertFalse(dialog.closing_);
           await fakeReceiveManager.unregisterForegroundReceiveSurface();
-          Polymer.dom.flush();
+          flush();
           assertFalse(dialog.closing_);
         });
 
     test('onStartAdvertisingFailure shows an error', async function() {
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
       assertTrue(isVisible('nearby-share-high-visibility-page'));
       const highVisibilityPage = dialog.$$('nearby-share-high-visibility-page');
       assertFalse(!!highVisibilityPage.$$('#errorTitle'));
 
       dialog.onStartAdvertisingFailure();
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       const errorTitle = highVisibilityPage.$$('#errorTitle');
       assertTrue(!!errorTitle && errorTitle.textContent.length > 0);
@@ -191,13 +191,13 @@ suite('NearbyShare', function() {
 
     test('when disabled, onboarding is shown first', async function() {
       dialog.showHighVisibilityPage();
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-onboarding-page'));
       // Advance to the next page.
       getButton('nearby-onboarding-page', '#actionButton').click();
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-visibility-page'));
       // Advance to the next page.
@@ -205,7 +205,7 @@ suite('NearbyShare', function() {
       selectAllContacts();
       getButton('nearby-visibility-page', '#actionButton').click();
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(dialog.settings.enabled);
       assertTrue(isVisible('nearby-share-high-visibility-page'));
@@ -213,13 +213,13 @@ suite('NearbyShare', function() {
 
     test('when showing onboarding, close when complete.', async function() {
       dialog.showOnboarding();
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-onboarding-page'));
       // Advance to the next page.
       getButton('nearby-onboarding-page', '#actionButton').click();
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-visibility-page'));
       // This should close the dialog.
@@ -229,7 +229,7 @@ suite('NearbyShare', function() {
 
       assertTrue(dialog.closing_);
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertFalse(dialog.$$('#dialog').open);
     });
@@ -239,13 +239,13 @@ suite('NearbyShare', function() {
         'isOnePageOnboardingEnabled': true,
       });
       dialog.showHighVisibilityPage();
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-onboarding-one-page'));
       // Finish onboarding
       getButton('nearby-onboarding-one-page', '#actionButton').click();
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(dialog.settings.enabled);
       assertEquals(
@@ -259,13 +259,13 @@ suite('NearbyShare', function() {
         'isOnePageOnboardingEnabled': true,
       });
       dialog.showOnboarding();
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-onboarding-one-page'));
       // Select visibility button and advance to the next page.
       dialog.$$('nearby-onboarding-one-page').$$('#visibilityButton').click();
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertTrue(isVisible('nearby-visibility-page'));
       // All contacts should be selected and confirm should close the dialog.
@@ -278,7 +278,7 @@ suite('NearbyShare', function() {
 
       assertTrue(dialog.closing_);
 
-      await test_util.waitAfterNextRender(dialog);
+      await waitAfterNextRender(dialog);
 
       assertFalse(dialog.$$('#dialog').open);
     });
