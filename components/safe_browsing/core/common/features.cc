92954338184b843bca96a456c18b1d70360b7e9a
Daniel Rubery <drubery@chromium.org>
2022-03-30 21:45:01 +0000

    Cleanup long-launched base::Features
    This CL cleans up some features that have been launched for several
    milestones:
    - ClientSideDetectionDocumentScanning
    - ClientSideDetectionModelOnAndroid
    - ClientSideDetectionModel
    - SafeBrowsingDisableConsumerCsdForEnterprise
    - VisualFeaturesInPasswordProtectionAndroid
    
    Change-Id: Ia410b4f9b2322a3e7745956b8991a5de899fab2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3556417
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987198}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -32,12 +32,6 @@ const base::Feature kBetterTelemetryAcrossReports{
     "SafeBrowsingBetterTelemetryAcrossReports",
     base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kClientSideDetectionDocumentScanning{
-    "ClientSideDetectionDocumentScanning", base::FEATURE_ENABLED_BY_DEFAULT};
-
-const base::Feature kClientSideDetectionForAndroid{
-    "ClientSideDetectionModelOnAndroid", base::FEATURE_ENABLED_BY_DEFAULT};
-
 // Enable only for Android
 #if BUILDFLAG(IS_ANDROID)
 const base::Feature kClientSideDetectionModelIsFlatBuffer{
@@ -46,9 +40,6 @@ const base::Feature kClientSideDetectionModelIsFlatBuffer{
 const base::Feature kClientSideDetectionModelIsFlatBuffer{
     "ClientSideDetectionModelIsFlatBuffer", base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
-
-extern const base::Feature kClientSideDetectionModelVersion{
-    "ClientSideDetectionModel", base::FEATURE_ENABLED_BY_DEFAULT};
 
 extern const base::Feature kClientSideDetectionModelTag{
     "ClientSideDetectionTag", base::FEATURE_DISABLED_BY_DEFAULT};
@@ -117,9 +108,6 @@ const base::Feature kOmitNonUserGesturesFromReferrerChain{
     "SafeBrowsingOmitNonUserGesturesFromReferrerChain",
     base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kPromptEsbForDeepScanning{
-    "SafeBrowsingPromptEsbForDeepScanning", base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kSafeBrowsingCsbrrWithToken{
     "SafeBrowsingCsbrrWithToken", base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -153,12 +141,9 @@ const base::Feature kThreatDomDetailsTagAndAttributeFeature{
 const base::Feature kTriggerThrottlerDailyQuotaFeature{
     "SafeBrowsingTriggerThrottlerDailyQuota",
     base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kUseNewDownloadWarnings{"UseNewDownloadWarnings",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kVisualFeaturesInPasswordProtectionAndroid{
-    "VisualFeaturesInPasswordProtectionAndroid",
-    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kVisualFeaturesSizes{"VisualFeaturesSizes",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
@@ -175,9 +160,7 @@ constexpr struct {
     {&kAccuracyTipsFeature, true},
     {&kAdSamplerTriggerFeature, false},
     {&kBetterTelemetryAcrossReports, true},
-    {&kClientSideDetectionForAndroid, true},
     {&kClientSideDetectionModelIsFlatBuffer, true},
-    {&kClientSideDetectionModelVersion, true},
     {&kClientSideDetectionReferrerChain, true},
     {&kConnectorsScanningReportOnlyUI, true},
     {&kDelayedWarnings, true},
