cb00a2004fe17deea68fddf19f9d230a941a33cd
Nina Satragno <nsatragno@chromium.org>
2022-03-30 23:45:15 +0000

    [CT] Fix PKI Metadata config lifetime
    Apply the PKI Metadata configuration to new network services created
    after the component is updated, e.g. if the network service crashes. For
    the Certificate Transparency enforcement setting, apply it to any
    new NetworkContexts created after the setting is changed.
    
    This CL also adds some missing tests. Particularly, a browser test for
    enabling and disabling CT enforcement, and a unit test for disabling CT
    enforcement through finch.
    
    Fixed: 1306559
    Change-Id: Ie7903f19a13cb3a2aee7ed71f9e566d62a8c5136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539127
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987258}

--- services/network/network_service.h
+++ services/network/network_service.h
@@ -277,6 +277,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
   base::Time ct_log_list_update_time() const {
     return ct_log_list_update_time_;
   }
+
+  bool is_ct_enforcement_enabled_for_testing() const {
+    return ct_enforcement_enabled_;
+  }
 #endif
 
   bool pins_list_updated() const { return pins_list_updated_; }
@@ -412,6 +416,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
   std::unique_ptr<CtLogListDistributor> ct_log_list_distributor_;
 
   base::Time ct_log_list_update_time_;
+
+  bool ct_enforcement_enabled_ = true;
 #endif
 
   bool pins_list_updated_ = false;
