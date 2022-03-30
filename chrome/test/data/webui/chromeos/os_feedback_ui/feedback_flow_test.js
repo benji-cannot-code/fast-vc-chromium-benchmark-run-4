b8f938153f25bdc66b834cd826e85fd2d8d3eed5
xiangdong kong <xiangdongkong@google.com>
2022-03-30 07:29:55 +0000

    feedback: Handle click event of continue button with empty description
    Expected behaviors of clicking the continue button:
    - If the description entered in the textarea is blank [1],
      - should stay on the same step.
      - should show error message below the textarea.
      - should focus on the textarea to accept input.
      - should hide the error message once some text has been entered.
    
    - If the description entered in the textarea is not blank,
      - should move to next step.
      - will be implemented in a followup CL.
    
    [1] https://screenshot.googleplex.com/6PP6pxtmRR4QDmw.
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: Id96d626d8c9c4052c41cbaa3a10ed61a9ac811f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555737
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#986886}

--- chrome/test/data/webui/chromeos/os_feedback_ui/feedback_flow_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/feedback_flow_test.js
@@ -54,13 +54,13 @@ export function FeedbackFlowTestSuite() {
     // Verify the title is in the page.
     const title = activePage.shadowRoot.querySelector('#title');
     assertTrue(!!title);
-    assertEquals('Send feedback', title.textContent);
+    assertEquals('Send feedback', title.textContent.trim());
 
     // Verify the continue button is in the page.
     const buttonContinue =
         activePage.shadowRoot.querySelector('#buttonContinue');
     assertTrue(!!buttonContinue);
-    assertEquals('Continue', buttonContinue.textContent);
+    assertEquals('Continue', buttonContinue.textContent.trim());
   });
 
 
@@ -76,17 +76,17 @@ export function FeedbackFlowTestSuite() {
     // Verify the title is in the page.
     const title = activePage.shadowRoot.querySelector('#title');
     assertTrue(!!title);
-    assertEquals('Send feedback', title.textContent);
+    assertEquals('Send feedback', title.textContent.trim());
 
     // Verify the back button is in the page.
     const buttonBack = activePage.shadowRoot.querySelector('#buttonBack');
     assertTrue(!!buttonBack);
-    assertEquals('Back', buttonBack.textContent);
+    assertEquals('Back', buttonBack.textContent.trim());
 
     // Verify the send button is in the page.
     const buttonSend = activePage.shadowRoot.querySelector('#buttonSend');
     assertTrue(!!buttonSend);
-    assertEquals('Send', buttonSend.textContent);
+    assertEquals('Send', buttonSend.textContent.trim());
   });
 
 
@@ -102,17 +102,17 @@ export function FeedbackFlowTestSuite() {
     // Verify the title is in the page.
     const title = activePage.shadowRoot.querySelector('#title');
     assertTrue(!!title);
-    assertEquals('Thanks for your feedback', title.textContent);
+    assertEquals('Thanks for your feedback', title.textContent.trim());
 
     // Verify the done button is in the page.
     const buttonDone = activePage.shadowRoot.querySelector('#buttonDone');
     assertTrue(!!buttonDone);
-    assertEquals('Done', buttonDone.textContent);
+    assertEquals('Done', buttonDone.textContent.trim());
 
     // Verify the startNewReport button is in the page.
     const buttonNewReport =
         activePage.shadowRoot.querySelector('#buttonNewReport');
     assertTrue(!!buttonNewReport);
-    assertEquals('Send new report', buttonNewReport.textContent);
+    assertEquals('Send new report', buttonNewReport.textContent.trim());
   });
 }
