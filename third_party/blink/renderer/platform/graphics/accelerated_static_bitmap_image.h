e5a2478d2dad6fc92205d7b8d09ede740e1b67ac
Christopher Cameron <ccameron@chromium.org>
2022-03-30 15:26:26 +0000

    StaticBitmapImage: Use SkImageInfo
    Add a StaticBitmapImage::GetSkColorInfoInternal method. Use this method
    in place of the existing GetSkColorType, IsPremultiplied, and
    GetSizeInternal methods.
    
    The UnacceleratedBitmapImage implementation makes use of cc::PaintImage.
    
    cc::PaintImage has several methods that can potentially return
    inconsistent results. They include GetSkImageInfo, width, height,
    GetSkColorType, GetAlphaType, color_space, and IsOpaque. Most of the
    inconsistency comes from the behavior when backed by a
    PaintWorkletInput, which did not populate an SkImageInfo. Change this
    to populate an SkImageInfo that is consistent with the other accessors.
    
    Bug: 1304524
    Change-Id: I1a7352220785fbbfeaa2622dff437f6a7e81ccab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558612
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986995}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
@@ -122,10 +122,6 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
     return supports_display_compositing_;
   }
 
-  SkColorType GetSkColorType() const override {
-    return sk_image_info_.colorType();
-  }
-
   PaintImage PaintImageForCurrentFrame() override;
 
  private:
@@ -154,7 +150,7 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
   void CreateImageFromMailboxIfNeeded();
   void InitializeTextureBacking(GLuint shared_image_texture_id);
 
-  gfx::Size SizeInternal() const override;
+  SkImageInfo GetSkImageInfoInternal() const override;
 
   const gpu::Mailbox mailbox_;
   const SkImageInfo sk_image_info_;
