52ca9e85e40ed5a0d580ae7eed5a4554e7b4fe3c
Lei Zhang <thestig@chromium.org>
2022-03-30 21:58:00 +0000

    Mark the Privet PrinterType as deprecated.
    Since it is never used in Print Preview Typescript code, C++ code that
    handles this printer type is useless. Delete those checks or mark them
    as unreachable when appropriate.
    
    Also update several comments that cross-references other printing files,
    as the file names have changed over time.
    
    Bug: 1223593
    Change-Id: I6868b6b5c61c7e9dfee90abf4cfe17669638687e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549504
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987202}

--- chrome/browser/printing/print_test_utils.cc
+++ chrome/browser/printing/print_test_utils.cc
@@ -19,6 +19,8 @@ namespace printing {
 const char kDummyPrinterName[] = "DefaultPrinter";
 
 base::Value GetPrintTicket(mojom::PrinterType type) {
+  DCHECK_NE(type, mojom::PrinterType::kPrivetDeprecated);
+
   base::Value ticket(base::Value::Type::DICTIONARY);
 
   // Letter
@@ -54,8 +56,7 @@ base::Value GetPrintTicket(mojom::PrinterType type) {
 
   if (type == mojom::PrinterType::kCloud) {
     ticket.SetStringKey(kSettingCloudPrintId, kDummyPrinterName);
-  } else if (type == mojom::PrinterType::kPrivet ||
-             type == mojom::PrinterType::kExtension) {
+  } else if (type == mojom::PrinterType::kExtension) {
     base::Value capabilities(base::Value::Type::DICTIONARY);
     capabilities.SetBoolKey("duplex", true);  // non-empty
     std::string caps_string;
