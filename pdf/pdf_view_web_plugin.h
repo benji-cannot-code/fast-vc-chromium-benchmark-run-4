1ec771035b40889f2141cef7e4e33b2bbdc52aa0
Chris Harrelson <chrishtr@chromium.org>
2022-03-30 20:34:17 +0000

    Fix bug in units for PDF print previews
    A previous CL (*) neglected to remove the base class implementation
    of the flag.
    
    (*): https://chromium-review.googlesource.com/c/chromium/src/+/3491625
    
    Fixed: 1311169
    Change-Id: I51214f5e67fc32ef3807ead28f4d4bd81b9cc913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558955
    Reviewed-by: Nigi <nigi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987153}

--- pdf/pdf_view_web_plugin.h
+++ pdf/pdf_view_web_plugin.h
@@ -163,9 +163,6 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
     virtual std::unique_ptr<PdfAccessibilityDataHandler>
     CreateAccessibilityDataHandler(
         PdfAccessibilityActionHandler* action_handler);
-
-    // Indicates whether to use zoom for DSF (device scale factor).
-    virtual bool IsUseZoomForDSFEnabled() const;
   };
 
   PdfViewWebPlugin(
