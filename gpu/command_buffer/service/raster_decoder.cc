a137db1143032573f6085b48cecd6b6233675453
Justin Novosad <junov@chromium.org>
2022-03-31 15:43:38 +0000

    Allow OOPR-Canvas to use hardware overlay
    This change updates how the RasterDecoder initializes its GPU
    capabilities in order to set the texture_storage_image bit according
    to whether the image factory supports anonymous images.  This is
    equivalent to how that bit gets set in GLES2 decoders.  We are using
    this capability as a proxy for determining whether SharedImage
    supports scanout, which is conservative.  In the long term we should
    probably add a dedicated capability bit that is specific to
    SharedImage functionality. This is a rapid fix for unblocking the
    launch of OOPR-Canvas in M101.
    
    Tests landing separately. See: crbug.com/1309206
    
    Bug: 1305679
    Change-Id: I854f37a0ebb76026a4f278d4ca72572372ca08cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557831
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987470}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/gles2_cmd_copy_tex_image.h"
 #include "gpu/command_buffer/service/gles2_cmd_copy_texture_chromium.h"
 #include "gpu/command_buffer/service/gpu_tracer.h"
+#include "gpu/command_buffer/service/image_factory.h"
 #include "gpu/command_buffer/service/logger.h"
 #include "gpu/command_buffer/service/mailbox_manager.h"
 #include "gpu/command_buffer/service/query_manager.h"
@@ -436,6 +437,7 @@ class RasterDecoderImpl final : public RasterDecoder,
                     const GpuPreferences& gpu_preferences,
                     MemoryTracker* memory_tracker,
                     SharedImageManager* shared_image_manager,
+                    ImageFactory* image_factory,
                     scoped_refptr<SharedContextState> shared_context_state,
                     bool is_privileged);
 
@@ -921,6 +923,7 @@ class RasterDecoderImpl final : public RasterDecoder,
   bool gpu_raster_enabled_ = false;
   bool use_gpu_raster_ = false;
   bool supports_oop_canvas_ = false;
+  bool texture_storage_image_enabled_ = false;
   bool use_passthrough_ = false;
 
   // The current decoder error communicates the decoder error through command
@@ -1017,12 +1020,13 @@ RasterDecoder* RasterDecoder::Create(
     const GpuPreferences& gpu_preferences,
     MemoryTracker* memory_tracker,
     SharedImageManager* shared_image_manager,
+    ImageFactory* image_factory,
     scoped_refptr<SharedContextState> shared_context_state,
     bool is_privileged) {
-  return new RasterDecoderImpl(client, command_buffer_service, outputter,
-                               gpu_feature_info, gpu_preferences,
-                               memory_tracker, shared_image_manager,
-                               std::move(shared_context_state), is_privileged);
+  return new RasterDecoderImpl(
+      client, command_buffer_service, outputter, gpu_feature_info,
+      gpu_preferences, memory_tracker, shared_image_manager, image_factory,
+      std::move(shared_context_state), is_privileged);
 }
 
 RasterDecoder::RasterDecoder(DecoderClient* client,
@@ -1074,6 +1078,7 @@ RasterDecoderImpl::RasterDecoderImpl(
     const GpuPreferences& gpu_preferences,
     MemoryTracker* memory_tracker,
     SharedImageManager* shared_image_manager,
+    ImageFactory* image_factory,
     scoped_refptr<SharedContextState> shared_context_state,
     bool is_privileged)
     : RasterDecoder(client, command_buffer_service, outputter),
@@ -1088,6 +1093,8 @@ RasterDecoderImpl::RasterDecoderImpl(
           gpu_feature_info
               .status_values[GPU_FEATURE_TYPE_CANVAS_OOP_RASTERIZATION] ==
           kGpuFeatureStatusEnabled),
+      texture_storage_image_enabled_(
+          image_factory && image_factory->SupportsCreateAnonymousImage()),
       use_passthrough_(gles2::PassthroughCommandDecoderSupported() &&
                        gpu_preferences.use_passthrough_cmd_decoder),
       gpu_preferences_(gpu_preferences),
@@ -1280,8 +1287,7 @@ Capabilities RasterDecoderImpl::GetCapabilities() {
       gpu_preferences_.texture_target_exception_list;
   caps.texture_format_bgra8888 =
       feature_info()->feature_flags().ext_texture_format_bgra8888;
-  caps.texture_storage_image =
-      feature_info()->feature_flags().chromium_texture_storage_image;
+  caps.texture_storage_image = texture_storage_image_enabled_;
   caps.texture_storage = feature_info()->feature_flags().ext_texture_storage;
   // TODO(piman): have a consistent limit in shared image backings.
   // https://crbug.com/960588
--- gpu/command_buffer/service/raster_decoder.h
+++ gpu/command_buffer/service/raster_decoder.h
@@ -16,6 +16,7 @@ namespace gpu {
 class DecoderClient;
 struct GpuFeatureInfo;
 struct GpuPreferences;
+class ImageFactory;
 class MemoryTracker;
 class ServiceTransferCache;
 class SharedContextState;
@@ -44,6 +45,7 @@ class GPU_GLES2_EXPORT RasterDecoder : public DecoderContext,
       const GpuPreferences& gpu_preferences,
       MemoryTracker* memory_tracker,
       SharedImageManager* shared_image_manager,
+      ImageFactory* image_factory,
       scoped_refptr<SharedContextState> shared_context_state,
       bool is_priviliged);
 
