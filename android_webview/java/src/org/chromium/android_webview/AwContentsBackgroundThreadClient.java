08e29997f74f05f72298f7b3be5942da72019faa
Nate Fischer <ntfschr@chromium.org>
2022-03-31 23:48:34 +0000

    AW UMA: fix InterceptionType histogram
    This fixes a logic bug in the InterceptionType histogram. This histogram
    was previously checking the null-ness of
    shouldInterceptRequestFromNative()'s return value. This should
    always be non-null because the Java method returns a non-null value in
    both branches. What we actually needed to check was
    AwWebResourceInterceptResponse::HasResponse(), as this goes over JNI to
    check the actual return value.
    
    This renames the histogram to avoid confusion with the invalid data.
    This also adds integration tests to verify this is implemented
    correctly.
    
    Fixed: 1310825
    Test: run_webview_instrumentation_test_apk -f *InterceptRequestTest.*
    Change-Id: I54d80d4c5e7fc34cc3f0c4f647646ecd6a472e94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558031
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987702}

--- android_webview/java/src/org/chromium/android_webview/AwContentsBackgroundThreadClient.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentsBackgroundThreadClient.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview;
 
+import androidx.annotation.NonNull;
+
 import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -23,6 +25,7 @@ public abstract class AwContentsBackgroundThreadClient {
 
     // Protected methods ---------------------------------------------------------------------------
 
+    @NonNull
     @CalledByNative
     private AwWebResourceInterceptResponse shouldInterceptRequestFromNative(String url,
             boolean isMainFrame, boolean hasUserGesture, String method, String[] requestHeaderNames,
