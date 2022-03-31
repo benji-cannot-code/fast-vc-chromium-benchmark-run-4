a0b68f019f0c1eb429c643a342c11fe2326ed931
Ashley Prasad <ashleydp@google.com>
2022-03-31 21:04:41 +0000

    diagnostics: add skeleton files for log controller
    - Add skeleton class for log controller and unit test.
    - Add OWNER and DIR_METADATA files to describe owning component.
    - Update required BUILD.gn.
    
    Bug: b:225191282
    Test: out/Default/ash_unittests --gtest_filter=*Diagnostics*
    Change-Id: I5f9f04bd89167059513bca7ec55ff53b1474daf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540241
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#987636}

---
+++ ash/system/diagnostics/diagnostics_log_controller.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/diagnostics/diagnostics_log_controller.h"
+
+#include "base/check_op.h"
+
+namespace ash {
+namespace diagnostics {
+
+namespace {
+
+DiagnosticsLogController* g_instance = nullptr;
+
+}  // namespace
+
+DiagnosticsLogController::DiagnosticsLogController() {
+  DCHECK_EQ(nullptr, g_instance);
+  g_instance = this;
+}
+
+DiagnosticsLogController::~DiagnosticsLogController() {
+  DCHECK_EQ(this, g_instance);
+  g_instance = nullptr;
+}
+
+// static
+DiagnosticsLogController* DiagnosticsLogController::Get() {
+  return g_instance;
+}
+
+}  // namespace diagnostics
+}  // namespace ash
---
+++ ash/system/diagnostics/diagnostics_log_controller_unittest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/diagnostics/diagnostics_log_controller.h"
+
+#include <memory>
+
+#include "ash/test/ash_test_base.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash {
+namespace diagnostics {
+
+class DiagnosticsLogControllerTest : public NoSessionAshTestBase {
+ public:
+  DiagnosticsLogControllerTest() = default;
+  DiagnosticsLogControllerTest(DiagnosticsLogControllerTest&) = delete;
+  DiagnosticsLogControllerTest& operator=(DiagnosticsLogControllerTest&) =
+      delete;
+  ~DiagnosticsLogControllerTest() override = default;
+};
+
+}  // namespace diagnostics
+}  // namespace ash
