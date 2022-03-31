a6d9b562915ee2bb215fc14fade82c043fc6e876
Jason Zhang <jiajunz@google.com>
2022-03-31 19:00:37 +0000

    [CrOS Cellular] Add connectivity check before managed eSIM installation
    This CL add a non-cellular type connectivity check each time before
    attempt managed eSIM profile installation. If there's no non-cellular
    internet available, it will retry exponential with maximum delay of one
    hour.
    
    Fixed: b/226678951
    Change-Id: I41d4958a8a26c25988a58f6edfe016365325f0ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555148
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#987570}

--- chromeos/network/cellular_policy_handler.cc
+++ chromeos/network/cellular_policy_handler.cc
@@ -18,12 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_profile_handler.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/policy_util.h"
+#include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
 
 namespace chromeos {
 
 namespace {
 
-const int kInstallRetryLimit = 3;
+const int kInstallRetryLimit = 10;
 
 constexpr net::BackoffEntry::Policy kRetryBackoffPolicy = {
     0,               // Number of initial errors to ignore.
@@ -213,6 +214,15 @@ void CellularPolicyHandler::SetupESim(const dbus::ObjectPath& euicc_path) {
     return;
   }
 
+  if (!HasNonCellularInternetConnectivity()) {
+    NET_LOG(ERROR) << "No non-cellular type Internet connectivity.";
+    auto current_request = std::move(remaining_install_requests_.front());
+    PopRequest();
+    ScheduleRetry(std::move(current_request));
+    ProcessRequests();
+    return;
+  }
+
   NET_LOG(EVENT) << "Install policy eSIM profile with properties: "
                  << new_shill_properties;
   // Remote provisioning of eSIM profiles via SMDP address in policy does not
@@ -360,6 +370,13 @@ void CellularPolicyHandler::OnWaitTimeout() {
   PopRequest();
   ScheduleRetry(std::move(current_request));
   ProcessRequests();
+}
+
+bool CellularPolicyHandler::HasNonCellularInternetConnectivity() {
+  const NetworkState* default_network =
+      network_state_handler_->DefaultNetwork();
+  return default_network && default_network->type() != shill::kTypeCellular &&
+         default_network->IsOnline();
 }
 
 }  // namespace chromeos
