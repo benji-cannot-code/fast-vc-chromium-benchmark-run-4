52d9ed0fdebd7282ffbc92222cecd41f6e5c02e8
Philipp Hancke <philipp.hancke@googlemail.com>
2022-03-31 08:06:19 +0000

    webrtc: measure opus stereo sdp munging
    BUG=webrtc:12368
    
    Change-Id: Icb2471a7c7fc2382841727fc33859ced3ba9aa27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3560009
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#987366}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
@@ -541,7 +541,11 @@ bool ContainsLegacyRtpDataChannel(String sdp) {
 }
 
 bool ContainsCandidate(String sdp) {
-  return sdp.Find("a=candidate") != kNotFound;
+  return sdp.Find("\na=candidate") != kNotFound;
+}
+
+bool ContainsOpusStereo(String sdp) {
+  return sdp.Find("stereo=1") != kNotFound;
 }
 
 enum class SdpFormat {
@@ -1128,6 +1132,11 @@ DOMException* RTCPeerConnection::checkSdpForStateErrors(
           UseCounter::Count(context,
                             WebFeature::kRTCLocalSdpModificationIceUfragPwd);
         }
+        if (ContainsOpusStereo(parsed_sdp.sdp()) &&
+            !ContainsOpusStereo(last_offer_)) {
+          UseCounter::Count(context,
+                            WebFeature::kRTCLocalSdpModificationOpusStereo);
+        }
         return nullptr;
         // TODO(https://crbug.com/823036): Return failure for all modification.
       }
@@ -1146,6 +1155,11 @@ DOMException* RTCPeerConnection::checkSdpForStateErrors(
         if (IceUfragPwdMismatch(last_answer_, parsed_sdp.sdp())) {
           UseCounter::Count(context,
                             WebFeature::kRTCLocalSdpModificationIceUfragPwd);
+        }
+        if (ContainsOpusStereo(parsed_sdp.sdp()) &&
+            !ContainsOpusStereo(last_offer_)) {
+          UseCounter::Count(context,
+                            WebFeature::kRTCLocalSdpModificationOpusStereo);
         }
         return nullptr;
         // TODO(https://crbug.com/823036): Return failure for all modification.
