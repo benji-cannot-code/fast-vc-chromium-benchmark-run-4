d6d0215f59df301271522f2eae5351387bc5d735
Owen Min <zmin@chromium.org>
2022-03-31 21:49:16 +0000

    Revert "Reland "Anchor the tab search bubble with a rect anchor when in fullscreen""
    This reverts commit 2f14b448d53b356fbace652af6632ef1f66a291b.
    
    Reason for revert:
    TabSearchBubbleHostBrowserTest.BubbleShowCorrectlyInFullscreen is
    flaky a lot on mac11-arm64-rel.
    Sample build:
    https://ci.chromium.org/ui/p/chromium/builders/try/mac11-arm64-rel/105518/overview
    
    
    Original change's description:
    > Reland "Anchor the tab search bubble with a rect anchor when in fullscreen"
    >
    > This is a reland of commit 568fd3e8df1379f530bab50833d9ac734c2251d4
    >
    > Change from using display to compute positioning to using the work area bound. This should resolve the failing test in mac where the top ~25px is not usable.
    >
    > Original change's description:
    > > Anchor the tab search bubble with a rect anchor when in fullscreen
    > >
    > > The tab search bubble is misplaced because it is anchored on the tab search button. This button does not have a proper position when the browser is in fullscreen mode. To fix this issue, we will create a rect anchor when we are in fullscreen and plumb it down to the bubble manager.
    > >
    > > Tested in RTL mode as well.
    > >
    > > Bug: 1175130
    > > Change-Id: I3a3fc80881cf8ae50b2e6807a118313fa157c621
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438697
    > > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > > Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    > > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > > Commit-Queue: Fred Shih <ffred@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#984585}
    >
    > Bug: 1175130
    > Change-Id: I57887bd1bf29a177c93afee33ecb690482085053
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551637
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Commit-Queue: Fred Shih <ffred@google.com>
    > Cr-Commit-Position: refs/heads/main@{#986171}
    
    Bug: 1175130, 1311699
    Change-Id: I0021a960fd1fce3fa32437e87417a28a543b05c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564622
    Owners-Override: Owen Min <zmin@chromium.org>
    Auto-Submit: Owen Min <zmin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#987652}

--- chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
+++ chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/visibility.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/webview/webview.h"
@@ -46,13 +45,11 @@ class WebUIBubbleView : public views::WebView {
 
 WebUIBubbleDialogView::WebUIBubbleDialogView(
     views::View* anchor_view,
-    BubbleContentsWrapper* contents_wrapper,
-    const absl::optional<gfx::Rect>& anchor_rect)
+    BubbleContentsWrapper* contents_wrapper)
     : BubbleDialogDelegateView(anchor_view, views::BubbleBorder::TOP_RIGHT),
       contents_wrapper_(contents_wrapper),
       web_view_(AddChildView(std::make_unique<WebUIBubbleView>(
-          contents_wrapper_->web_contents()))),
-      bubble_anchor_(anchor_rect) {
+          contents_wrapper_->web_contents()))) {
   DCHECK(!contents_wrapper_->GetHost());
   contents_wrapper_->SetHost(weak_factory_.GetWeakPtr());
 
@@ -127,12 +124,6 @@ bool WebUIBubbleDialogView::HandleKeyboardEvent(
     const content::NativeWebKeyboardEvent& event) {
   return unhandled_keyboard_event_handler_.HandleKeyboardEvent(
       event, GetFocusManager());
-}
-
-gfx::Rect WebUIBubbleDialogView::GetAnchorRect() const {
-  if (bubble_anchor_)
-    return bubble_anchor_.value();
-  return BubbleDialogDelegateView::GetAnchorRect();
 }
 
 BEGIN_METADATA(WebUIBubbleDialogView, views::BubbleDialogDelegateView)
