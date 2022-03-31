1e67dd4348c093b5c85370d48641b09e9c2a5edd
Luca Hunkeler <hluca@google.com>
2022-03-31 08:14:05 +0000

    Make Starter a WebContentUserData
    Before this change, the starter was owned by the Android platform
    delegate. In order to support other platforms, the starter is
    now a WebContentUserData and will be created on tab creation.
    This CL adds the creation for android only, a follow-up will
    add the same for other platforms.
    
    Bug: b:201964911
    
    
    Change-Id: I7fea6d3d47ea09502530f25daca891e0c09ab86c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528555
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#987368}

--- components/autofill_assistant/browser/fake_starter_platform_delegate.cc
+++ components/autofill_assistant/browser/fake_starter_platform_delegate.cc
@@ -120,4 +120,8 @@ FakeStarterPlatformDelegate::CreateFieldTrialUtil() {
   return std::make_unique<MockAssistantFieldTrialUtil>();
 }
 
+bool FakeStarterPlatformDelegate::IsAttached() {
+  return is_attached_;
+}
+
 }  // namespace autofill_assistant
