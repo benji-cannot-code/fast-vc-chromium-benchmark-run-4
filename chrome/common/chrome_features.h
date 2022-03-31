54b7e6c658965c4fae6b6dfa647b98debb8a2e91
Mike Jackson <mjackson@microsoft.com>
2022-03-31 00:39:35 +0000

    dpwas: Add feature flag to gate parsing WebAppSettings policy
    Adding an enabled-by-default feature flag to determine if the
    WebAppPolicyManager will subscribe to pref change notifications,
    and perform initial parsing of the WebAppSettings policy.
    
    The check was not put into WebAppPolicyManager::RefreshPolicySettings,
    so that tests can continue to call this function without needing to
    enable the flag.
    
    There are a few tests that rely on this feature being enabled, and
    these tests have been updated to ensure the flag is set to enabled.
    
    Bug: 1280773
    Change-Id: Ia98d48dee51c565099ffc10fca9c6474c4174c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558075
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Auto-Submit: Mike Jackson <mjackson@microsoft.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987282}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -202,6 +202,9 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsElidedExtensionsMenu;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kDesktopPWAsEnforceWebAppSettingsPolicy;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsFlashAppNameInsteadOfOrigin;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
