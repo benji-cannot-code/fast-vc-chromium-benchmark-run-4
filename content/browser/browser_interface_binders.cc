343447cef19e926098e9d045a865638c7e6eac90
Yao Xiao <yaoxia@chromium.org>
2022-03-31 21:20:04 +0000

    [Topics] Fix BrowsingTopics interface binding in browser_interface_binders.cc
    The map->Add<blink::mojom::BrowsingTopicsDocumentService> was
    incorrectly added under #if !BUILDFLAG(IS_ANDROID). We should need it
    for both desktop and android.
    
    Bug: 1312152
    Change-Id: I46ccfe0bdf29b8bf195c8f902e6c4552989c05ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564620
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987644}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -1079,6 +1079,10 @@ void PopulateBinderMapWithContext(
       &EmptyBinderForFrame<blink::mojom::AnchorElementMetricsHost>));
   map->Add<blink::mojom::CredentialManager>(base::BindRepeating(
       &EmptyBinderForFrame<blink::mojom::CredentialManager>));
+  if (base::FeatureList::IsEnabled(blink::features::kBrowsingTopics)) {
+    map->Add<blink::mojom::BrowsingTopicsDocumentService>(
+        base::BindRepeating(&BrowsingTopicsDocumentHost::CreateMojoService));
+  }
 #if !BUILDFLAG(IS_ANDROID)
   if (SiteIsolationPolicy::IsApplicationIsolationLevelEnabled()) {
     map->Add<blink::mojom::DirectSocketsService>(
@@ -1092,10 +1096,6 @@ void PopulateBinderMapWithContext(
               media::mojom::SpeechRecognitionClientBrowserInterface>));
   map->Add<media::mojom::MediaFoundationRendererNotifier>(base::BindRepeating(
       &EmptyBinderForFrame<media::mojom::MediaFoundationRendererNotifier>));
-  if (base::FeatureList::IsEnabled(blink::features::kBrowsingTopics)) {
-    map->Add<blink::mojom::BrowsingTopicsDocumentService>(
-        base::BindRepeating(&BrowsingTopicsDocumentHost::CreateMojoService));
-  }
   map->Add<media::mojom::MediaPlayerObserverClient>(base::BindRepeating(
       &EmptyBinderForFrame<media::mojom::MediaPlayerObserverClient>));
 #endif
