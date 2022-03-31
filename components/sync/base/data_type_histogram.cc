72656153c3c3b139ecccf679da370dfe0131c258
Bruce Dawson <brucedawson@chromium.org>
2022-03-31 09:18:53 +0000

    Fixes header issues found by cpp.py
    While testing "git cl presubmit --all" I found three latent include
    ordering issues in components/sync. All other .cc files in the
    components/sync directory pass the presubmits.
    
    Bug: 1309977
    Change-Id: I579786e6bb798f0e5a57c73409f5abf0d9708d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561095
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#987384}

--- components/sync/base/data_type_histogram.cc
+++ components/sync/base/data_type_histogram.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/base/data_type_histogram.h"
 
+#include <string>
+
 #include "base/metrics/histogram_functions.h"
 
 namespace syncer {
