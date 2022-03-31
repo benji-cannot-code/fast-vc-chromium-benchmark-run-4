a9d71f7774f0ec3bd7040be7e4393577de4d0cfb
Sneha Nagpaul <snehanagpaul@google.com>
2022-03-31 15:36:27 +0000

    Move kBypassJustificationEnabled to base for reuse.
    Required during download bypass justification implementation
    
    Bug: b/225351697
    Change-Id: I92440c4e286ddde41d9baee31b14a6c17077dff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3560746
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#987468}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.h
@@ -34,8 +34,6 @@ class Profile;
 
 namespace enterprise_connectors {
 
-extern const base::Feature kBypassJustificationEnabled;
-
 class ContentAnalysisDialog;
 
 // A class that performs deep scans of data (for example malicious or sensitive
