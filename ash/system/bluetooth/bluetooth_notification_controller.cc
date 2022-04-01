24b8f13d880b4846ea16e8e4ec87271b1b3730ad
Kevin Radtke <kradtke@chromium.org>
2022-04-01 21:25:00 +0000

    Do not show adapter discoverable toast when in kiosk app mode
    Return early if we are running in AppMode when trying to show the
    `kBluetoothAdapterDiscoverable` toast.
    
    Buganizer issue: b/219804107
    
    Change-Id: Ia3104209bdeddce2075749f5f715626207882fdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565138
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988109}

--- ash/system/bluetooth/bluetooth_notification_controller.cc
+++ ash/system/bluetooth/bluetooth_notification_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/system_tray_client.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/services/nearby/public/cpp/nearby_client_uuids.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
@@ -309,6 +310,10 @@ void BluetoothNotificationController::OnGetAdapter(
 }
 
 void BluetoothNotificationController::NotifyAdapterDiscoverable() {
+  // Do not show toast in kiosk app mode.
+  if (Shell::Get()->session_controller()->IsRunningInAppMode())
+    return;
+
   // If Nearby Share has made the local device discoverable, do not
   // unnecessarily display this toast.
   // TODO(crbug.com/1155669): Generalize this logic to prevent leaking Nearby
