593778a1fffc6fd0c0a08d8a4eac8885a329b5ae
Yunchao He <yunchao.he@intel.com>
2022-04-01 18:09:09 +0000

    Add accelerated_webgpu to the feature list of Chromium's blocklist mechanism
    With this CL, accelerated webgpu can be blocklisted for broken
    hardwares, drivers, and/or OSes via adding appropriate entry/entries
    into software_rendering_list.json. In this situation, WebGPU will be
    rendered via software (SwiftShader) instead.
    
    Bug: 1289487
    Change-Id: Ic43b0168d744e98493a9adf2840b0840586d9c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486856
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Yunchao He <yunchao.he@intel.com>
    Cr-Commit-Position: refs/heads/main@{#988002}

--- gpu/command_buffer/service/webgpu_decoder.h
+++ gpu/command_buffer/service/webgpu_decoder.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 class DecoderClient;
+struct GpuFeatureInfo;
 struct GpuPreferences;
 class MemoryTracker;
 class SharedContextState;
@@ -43,7 +44,7 @@ class GPU_GLES2_EXPORT WebGPUDecoder : public DecoderContext,
 
   // WebGPU-specific initialization that's different than DecoderContext's
   // Initialize that is tied to GLES2 concepts and a noop for WebGPU decoders.
-  virtual ContextResult Initialize() = 0;
+  virtual ContextResult Initialize(const GpuFeatureInfo& gpu_feature_info) = 0;
 
   ContextResult Initialize(const scoped_refptr<gl::GLSurface>& surface,
                            const scoped_refptr<gl::GLContext>& context,
--- gpu/config/gpu_util.cc
+++ gpu/config/gpu_util.cc
@@ -218,6 +218,16 @@ GpuFeatureStatus GetWebGL2FeatureStatus(
   return kGpuFeatureStatusEnabled;
 }
 
+GpuFeatureStatus GetWebGPUFeatureStatus(
+    const std::set<int>& blocklisted_features,
+    bool use_swift_shader) {
+  if (use_swift_shader)
+    return kGpuFeatureStatusSoftware;
+  if (blocklisted_features.count(GPU_FEATURE_TYPE_ACCELERATED_WEBGPU))
+    return kGpuFeatureStatusSoftware;
+  return kGpuFeatureStatusEnabled;
+}
+
 GpuFeatureStatus Get2DCanvasFeatureStatus(
     const std::set<int>& blocklisted_features,
     bool use_swift_shader) {
@@ -430,6 +440,8 @@ GpuFeatureInfo ComputeGpuFeatureInfoWithHardwareAccelerationDisabled() {
       kGpuFeatureStatusDisabled;
   gpu_feature_info.status_values[GPU_FEATURE_TYPE_CANVAS_OOP_RASTERIZATION] =
       kGpuFeatureStatusDisabled;
+  gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGPU] =
+      kGpuFeatureStatusSoftware;
 #if DCHECK_IS_ON()
   for (int ii = 0; ii < NUMBER_OF_GPU_FEATURE_TYPES; ++ii) {
     DCHECK_NE(kGpuFeatureStatusUndefined, gpu_feature_info.status_values[ii]);
@@ -462,6 +474,8 @@ GpuFeatureInfo ComputeGpuFeatureInfoWithNoGpu() {
       kGpuFeatureStatusDisabled;
   gpu_feature_info.status_values[GPU_FEATURE_TYPE_CANVAS_OOP_RASTERIZATION] =
       kGpuFeatureStatusDisabled;
+  gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGPU] =
+      kGpuFeatureStatusSoftware;
 #if DCHECK_IS_ON()
   for (int ii = 0; ii < NUMBER_OF_GPU_FEATURE_TYPES; ++ii) {
     DCHECK_NE(kGpuFeatureStatusUndefined, gpu_feature_info.status_values[ii]);
@@ -494,6 +508,8 @@ GpuFeatureInfo ComputeGpuFeatureInfoForSwiftShader() {
       kGpuFeatureStatusDisabled;
   gpu_feature_info.status_values[GPU_FEATURE_TYPE_CANVAS_OOP_RASTERIZATION] =
       kGpuFeatureStatusDisabled;
+  gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGPU] =
+      kGpuFeatureStatusSoftware;
 #if DCHECK_IS_ON()
   for (int ii = 0; ii < NUMBER_OF_GPU_FEATURE_TYPES; ++ii) {
     DCHECK_NE(kGpuFeatureStatusUndefined, gpu_feature_info.status_values[ii]);
@@ -571,6 +587,8 @@ GpuFeatureInfo ComputeGpuFeatureInfo(const GPUInfo& gpu_info,
       GetWebGLFeatureStatus(blocklisted_features, use_swift_shader);
   gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGL2] =
       GetWebGL2FeatureStatus(blocklisted_features, use_swift_shader);
+  gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_WEBGPU] =
+      GetWebGPUFeatureStatus(blocklisted_features, use_swift_shader);
   gpu_feature_info.status_values[GPU_FEATURE_TYPE_ACCELERATED_2D_CANVAS] =
       Get2DCanvasFeatureStatus(blocklisted_features, use_swift_shader);
 #if !BUILDFLAG(IS_CHROMEOS)
