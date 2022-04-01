0dd35b0c1c2cdc435db357f77e02d2dec90901e7
Adithya Srinivasan <adithyas@chromium.org>
2022-04-01 18:26:06 +0000

    Sheriff: Disable InitFailure_DestroyAndRecreateDatabase
    Bug: 1312273
    Change-Id: I591fcbc9d807872c45dd9c90eaf67de07045afa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563628
    Owners-Override: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988011}

--- components/services/storage/shared_storage/shared_storage_manager_unittest.cc
+++ components/services/storage/shared_storage/shared_storage_manager_unittest.cc
@@ -1007,8 +1007,9 @@ TEST_P(SharedStorageManagerErrorParamTest,
       "Storage.SharedStorage.OnShutdown.RecoveryOnDiskAttempted", false, 1);
 }
 
+// TODO(crbug.com/1312273): Test is flaky.
 TEST_P(SharedStorageManagerErrorParamTest,
-       InitFailure_DestroyAndRecreateDatabase) {
+       DISABLED_InitFailure_DestroyAndRecreateDatabase) {
   ASSERT_TRUE(GetManager());
   ASSERT_TRUE(GetManager()->database());
   SetDestroyCallback();
