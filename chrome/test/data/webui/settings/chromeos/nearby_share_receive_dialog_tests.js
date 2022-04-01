0856d2c858e3d31ce1172a2e7ebc0c4865b8b9e0
Pu Shi <pushi@google.com>
2022-04-01 22:56:45 +0000

    [Nearby] Add field trial testing config for onboarding
    This adds Field Trial Testing config for onboarding so that we can run
    the experiment in Beta.
    
    Bug: 1265562
    Change-Id: Ic2aa0667e264146c9181112168bfa450330fa1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565642
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#988162}

--- chrome/test/data/webui/settings/chromeos/nearby_share_receive_dialog_tests.js
+++ chrome/test/data/webui/settings/chromeos/nearby_share_receive_dialog_tests.js
@@ -193,9 +193,15 @@ suite('NearbyShare', function() {
       dialog.showHighVisibilityPage();
       await waitAfterNextRender(dialog);
 
-      assertTrue(isVisible('nearby-onboarding-page'));
-      // Advance to the next page.
-      getButton('nearby-onboarding-page', '#actionButton').click();
+      if (loadTimeData.getValue('isOnePageOnboardingEnabled')) {
+        assertTrue(isVisible('nearby-onboarding-one-page'));
+        // Select visibility button and advance to the next page.
+        dialog.$$('nearby-onboarding-one-page').$$('#visibilityButton').click();
+      } else {
+        assertTrue(isVisible('nearby-onboarding-page'));
+        // Advance to the next page.
+        getButton('nearby-onboarding-page', '#actionButton').click();
+      }
 
       await waitAfterNextRender(dialog);
 
@@ -215,9 +221,15 @@ suite('NearbyShare', function() {
       dialog.showOnboarding();
       await waitAfterNextRender(dialog);
 
-      assertTrue(isVisible('nearby-onboarding-page'));
-      // Advance to the next page.
-      getButton('nearby-onboarding-page', '#actionButton').click();
+      if (loadTimeData.getValue('isOnePageOnboardingEnabled')) {
+        assertTrue(isVisible('nearby-onboarding-one-page'));
+        // Select visibility button and advance to the next page.
+        dialog.$$('nearby-onboarding-one-page').$$('#visibilityButton').click();
+      } else {
+        assertTrue(isVisible('nearby-onboarding-page'));
+        // Advance to the next page.
+        getButton('nearby-onboarding-page', '#actionButton').click();
+      }
 
       await waitAfterNextRender(dialog);
 
