61a598d0c4f6e54bdd450e9b46cf8f45c51cfbd0
Nick Burris <nburris@chromium.org>
2022-04-01 17:11:54 +0000

    [SPC] Correct sizing for default instrument icon
    The vector icon used for the default payment instrument icon is intended
    to be drawn as a 20x20 square (vertical whitespace is included in the
    icon). Update the size constant accordingly.
    
    Bug: 1298505
    Change-Id: Ia63feba98779c7cf95dc5eec412c8582a1de86a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565246
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987969}

--- components/payments/core/sizes.h
+++ components/payments/core/sizes.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
-// The default width of the instrument icon for SPC. Based on the aspect ratio
-// of the default instrument icon.
-constexpr int kSecurePaymentConfirmationInstrumentIconDefaultWidthPx = 26;
+// The default width of the instrument icon for SPC. Based on the size of the
+// default instrument icon.
+constexpr int kSecurePaymentConfirmationInstrumentIconDefaultWidthPx = 20;
 
 // The maximum width of the instrument icon for SPC. Used as the preferred size
 // of downloaded instrument icons.
