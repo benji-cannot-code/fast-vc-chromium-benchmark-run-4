f2770fdfdaab53f3a4ee205808c7a232f7690962
Adam Langley <agl@chromium.org>
2022-04-01 18:34:05 +0000

    webauthn: enable some experimental tweaks to the UI behind a flag.
    BUG=1312411
    
    Change-Id: I9633d24e388142ef3b2376902a9edddcd2b040ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565959
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988024}

--- chrome/browser/ui/views/webauthn/authenticator_qr_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_qr_sheet_view.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/feature_list.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/services/qrcode_generator/public/cpp/qrcode_generator_service.h"
 #include "chrome/services/qrcode_generator/public/mojom/qrcode_generator.mojom.h"
+#include "device/fido/features.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_provider.h"
@@ -50,7 +52,8 @@ class AuthenticatorQRViewCentered : public views::View {
         qrcode_generator::mojom::GenerateQRCodeRequest::New();
     request->data = qr_string;
     request->should_render = true;
-    request->render_dino = true;
+    request->render_dino =
+        !base::FeatureList::IsEnabled(device::kWebAuthPasskeysUIExperiment);
     request->render_module_style =
         qrcode_generator::mojom::ModuleStyle::CIRCLES;
     request->render_locator_style =
