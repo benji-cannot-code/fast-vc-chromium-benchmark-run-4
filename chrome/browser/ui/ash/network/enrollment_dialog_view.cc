1d839a580e8f94a1a647648618bde2f63ac1a4a6
Peter Kasting <pkasting@chromium.org>
2022-04-01 20:43:59 +0000

    Eliminate some more GridLayout usage.
    Bug: 1261301
    Change-Id: Idbd23fa5f42c7d4480c8a00cc67a3b66dabe1676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564962
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988093}

--- chrome/browser/ui/ash/network/enrollment_dialog_view.cc
+++ chrome/browser/ui/ash/network/enrollment_dialog_view.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
 
@@ -71,7 +70,6 @@ class EnrollmentDialogView : public views::DialogDelegateView {
   std::string network_name_;
   Profile* profile_;
   GURL target_uri_;
-  bool added_cert_;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -83,14 +81,22 @@ EnrollmentDialogView::EnrollmentDialogView(const std::string& network_name,
     : accepted_(false),
       network_name_(network_name),
       profile_(profile),
-      target_uri_(target_uri),
-      added_cert_(false) {
+      target_uri_(target_uri) {
   SetTitle(l10n_util::GetStringUTF16(IDS_NETWORK_ENROLLMENT_HANDLER_TITLE));
   DialogDelegate::SetButtonLabel(
       ui::DIALOG_BUTTON_OK,
       l10n_util::GetStringUTF16(IDS_NETWORK_ENROLLMENT_HANDLER_BUTTON));
   set_margins(ChromeLayoutProvider::Get()->GetDialogInsetsForContentType(
       views::DialogContentType::kText, views::DialogContentType::kText));
+
+  SetUseDefaultFillLayout(true);
+  auto* label = AddChildView(std::make_unique<views::Label>(
+      l10n_util::GetStringFUTF16(IDS_NETWORK_ENROLLMENT_HANDLER_INSTRUCTIONS,
+                                 base::UTF8ToUTF16(network_name_))));
+  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  label->SetMultiLine(true);
+  label->SetAllowCharacterBreak(true);
+
   chrome::RecordDialogCreation(chrome::DialogIdentifier::ENROLLMENT);
 }
 
@@ -111,7 +117,6 @@ void EnrollmentDialogView::ShowDialog(const std::string& network_name,
       &params, ash_util::GetSystemModalDialogContainerId());
   views::Widget* widget = new views::Widget;  // Owned by native widget.
   widget->Init(std::move(params));
-  dialog_view->InitDialog();
   widget->Show();
 }
 
@@ -135,50 +140,6 @@ void EnrollmentDialogView::WindowClosing() {
 
 gfx::Size EnrollmentDialogView::CalculatePreferredSize() const {
   return gfx::Size(kDefaultWidth, kDefaultHeight);
-}
-
-void EnrollmentDialogView::InitDialog() {
-  added_cert_ = false;
-  // Create the views and layout manager and set them up.
-  auto label = std::make_unique<views::Label>(
-      l10n_util::GetStringFUTF16(IDS_NETWORK_ENROLLMENT_HANDLER_INSTRUCTIONS,
-                                 base::UTF8ToUTF16(network_name_)));
-  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  label->SetMultiLine(true);
-  label->SetAllowCharacterBreak(true);
-
-  views::GridLayout* grid_layout =
-      SetLayoutManager(std::make_unique<views::GridLayout>());
-
-  views::ColumnSet* columns = grid_layout->AddColumnSet(0);
-  columns->AddColumn(
-      views::GridLayout::FILL,                       // Horizontal resize.
-      views::GridLayout::FILL,                       // Vertical resize.
-      1,                                             // Resize weight.
-      views::GridLayout::ColumnSize::kUsePreferred,  // Size type.
-      0,                                             // Ignored for USE_PREF.
-      0);                                            // Minimum size.
-  columns = grid_layout->AddColumnSet(1);
-
-  ChromeLayoutProvider* provider = ChromeLayoutProvider::Get();
-
-  columns->AddPaddingColumn(
-      views::GridLayout::kFixedSize,
-      provider->GetDistanceMetric(DISTANCE_UNRELATED_CONTROL_HORIZONTAL));
-  columns->AddColumn(
-      views::GridLayout::LEADING,                    // Horizontal leading.
-      views::GridLayout::FILL,                       // Vertical resize.
-      1,                                             // Resize weight.
-      views::GridLayout::ColumnSize::kUsePreferred,  // Size type.
-      0,                                             // Ignored for USE_PREF.
-      0);                                            // Minimum size.
-
-  grid_layout->StartRow(views::GridLayout::kFixedSize, 0);
-  grid_layout->AddView(std::move(label));
-  grid_layout->AddPaddingRow(
-      views::GridLayout::kFixedSize,
-      provider->GetDistanceMetric(views::DISTANCE_UNRELATED_CONTROL_VERTICAL));
-  grid_layout->Layout(this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
