6363de24cbe1670c6a943f64e9127b8369446b57
Stepan Khapugin <stkhapugin@chromium.org>
2022-04-01 01:15:35 +0000

    [iOS] Fix swiftui previews project.
    Fixes the demo swiftui previews project. It now compiles.
    
    Bug: none
    Change-Id: Id11087753a9f9b73d8412ad49aedd87062399932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563322
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#987732}

---
+++ ios/chrome/browser/ui/omnibox/popup/swiftui_previews/omnibox_popup/external_defines.swift
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+//  external_defines.swift
+//  omnibox_popup
+//
+//  Created by Stepan Khapugin on 31/03/2022.
+//
+
+import Foundation
+
+// Defines for chrome constants etc that aren't built as part of this demo project.
+
+public let kOmniboxPopupRowSwitchTabAccessibilityIdentifier =
+  "kOmniboxPopupRowSwitchTabAccessibilityIdentifier"
+
+public let kOmniboxPopupRowAppendAccessibilityIdentifier =
+  "kOmniboxPopupRowAppendAccessibilityIdentifier"
+
+public let kOmniboxPopupTableViewAccessibilityIdentifier =
+  "OmniboxPopupTableViewAccessibilityIdentifier"
+
+public class OmniboxPopupAccessibilityIdentifierHelper {
+  static func accessibilityIdentifierForRow(at indexPath: IndexPath) -> String {
+    return "omnibox suggestion \(indexPath.section) \(indexPath.row)"
+  }
+}
