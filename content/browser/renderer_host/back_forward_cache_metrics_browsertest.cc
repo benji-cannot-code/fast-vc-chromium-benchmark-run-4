1b6dd3740a508634a0af8b72ee9ba53fc3073ea1
Fergal Daly <fergal@chromium.org>
2022-04-01 07:04:03 +0000

    Correct feature name to BackForwardCacheDedicatedWorker.
    Also tweak a test to be skipped when this is enabled, the test assumes
    workers cannot be cached.
    
    Bug: 1146955
    Change-Id: I1742361daf2db1bff47e5f3e061bf808f9950fba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3534401
    Reviewed-by: Darren Willis <djw@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987809}

--- content/browser/renderer_host/back_forward_cache_metrics_browsertest.cc
+++ content/browser/renderer_host/back_forward_cache_metrics_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/public/cpp/test/scoped_geolocation_overrider.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 
 using base::Bucket;
@@ -636,8 +637,12 @@ IN_PROC_BROWSER_TEST_P(BackForwardCacheMetricsBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_P(BackForwardCacheMetricsBrowserTest, DedicatedWorker) {
-  ukm::TestAutoSetUkmRecorder recorder;
+  // This test should only run if the feature is disabled.
+  if (base::FeatureList::IsEnabled(
+          blink::features::kBackForwardCacheDedicatedWorker))
+    return;
 
+  ukm::TestAutoSetUkmRecorder recorder;
   const GURL url(embedded_test_server()->GetURL(
       "/back_forward_cache/page_with_dedicated_worker.html"));
 
