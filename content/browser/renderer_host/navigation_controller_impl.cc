41aaa71d4f34fa2d14fa3d6433bb2d3dcf199a18
Nate Chapin <japhet@chromium.org>
2022-04-01 00:16:25 +0000

    Navigation API: fix traversal + sandboxing interactions
    Previous behavior: navigation API's traversal methods can ignore
    sandboxing flags in some situations, because we didn't pass them
    through in NavigateToNavigationApiKey.
    
    Current behavior with this CL: navigation API's traversal methods
    now return never-settled promises when sandboxing prevents the
    navigation.
    
    Desired behavior: navigation API's traversal methods return promises
    rejected with a "SecurityError" DOMException when sandboxing prevents
    the navigation.
    
    Fixed: 1234117
    Bug: 1311786
    Change-Id: Ib9a10113a11a419569534f07a1a56434d528da54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561323
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987711}

--- content/browser/renderer_host/navigation_controller_impl.cc
+++ content/browser/renderer_host/navigation_controller_impl.cc
@@ -4468,6 +4468,7 @@ NavigationControllerImpl::ShouldNavigateToEntryForNavigationApiKey(
 
 void NavigationControllerImpl::NavigateToNavigationApiKey(
     FrameTreeNode* node,
+    int sandboxed_source_frame_tree_node_id,
     const std::string& key) {
   FrameNavigationEntry* current_entry =
       GetLastCommittedEntry()->GetFrameEntry(node);
@@ -4483,7 +4484,7 @@ void NavigationControllerImpl::NavigateToNavigationApiKey(
     if (result == HistoryNavigationAction::kStopLooking)
       break;
     if (result != HistoryNavigationAction::kKeepLooking) {
-      GoToIndex(i, FrameTreeNode::kFrameTreeNodeInvalidId,
+      GoToIndex(i, sandboxed_source_frame_tree_node_id,
                 false /* is_browser_initiated*/);
       return;
     }
@@ -4494,7 +4495,7 @@ void NavigationControllerImpl::NavigateToNavigationApiKey(
     if (result == HistoryNavigationAction::kStopLooking)
       break;
     if (result != HistoryNavigationAction::kKeepLooking) {
-      GoToIndex(i, FrameTreeNode::kFrameTreeNodeInvalidId,
+      GoToIndex(i, sandboxed_source_frame_tree_node_id,
                 false /* is_browser_initiated*/);
       return;
     }
