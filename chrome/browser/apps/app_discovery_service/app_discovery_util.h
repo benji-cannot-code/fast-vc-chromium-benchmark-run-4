dc955452f052dd971a169b0d5119b99afad6824e
tby <tby@chromium.org>
2022-04-01 04:26:57 +0000

    [game search] Tweak GameExtras and add kGames enum values
    Three of the members in GameExtras are already present in the top-level
    apps::Result class, so can be removed. While here I've added a kGames
    enum value to AppSource and ResultType. This is a NOTREACHED in the
    fetcher, to be expanded on later.
    
    Bug: 1305880
    Change-Id: I8108975446edb5f869104d29682542afaa26ef2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558692
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987784}

--- chrome/browser/apps/app_discovery_service/app_discovery_util.h
+++ chrome/browser/apps/app_discovery_service/app_discovery_util.h
@@ -16,11 +16,13 @@ namespace apps {
 enum class ResultType {
   kTestType,
   kRecommendedArcApps,
+  kGameSearchCatalog,
 };
 
 enum class AppSource {
   kTestSource,
   kPlay,
+  kGames,
 };
 
 enum class DiscoveryError {
--- chrome/browser/apps/app_discovery_service/game_extras.cc
+++ chrome/browser/apps/app_discovery_service/game_extras.cc
@@ -6,41 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_discovery_service/game_extras.h"
 
 namespace apps {
-namespace {
-
-using Source = GameExtras::Source;
-
-}  // namespace
 
 GameExtras::GameExtras(
-    const std::string& id,
-    const std::u16string& title,
-    Source source,
     const absl::optional<std::vector<std::u16string>>& platforms,
+    Source source,
     const GURL& icon_url)
-    : id_(id),
-      title_(title),
-      source_(source),
-      platforms_(platforms),
-      icon_url_(icon_url) {}
+    : platforms_(platforms), source_(source), icon_url_(icon_url) {}
 
 GameExtras::~GameExtras() = default;
-
-const std::string& GameExtras::GetId() const {
-  return id_;
-}
-
-const std::u16string& GameExtras::GetTitle() const {
-  return title_;
-}
-
-Source GameExtras::GetSource() const {
-  return source_;
-}
 
 const absl::optional<std::vector<std::u16string>>& GameExtras::GetPlatforms()
     const {
   return platforms_;
+}
+
+GameExtras::Source GameExtras::GetSource() const {
+  return source_;
 }
 
 const GURL& GameExtras::GetIconUrl() const {
