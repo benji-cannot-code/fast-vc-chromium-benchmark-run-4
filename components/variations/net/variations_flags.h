584100f267479119982b7d0519ef6e39a643a6a3
Olivier Robin <olivierrobin@google.com>
2022-04-01 10:36:17 +0000

    Add flag to append variation headers to localhost requests.
    On iOS, EmbeddedTestServer is only accessible test servers
    using 127.0.0.1.
    In the current configuration, this means that variations headers
    are not appended to the requests.
    On other platforms, a local DNS mapping is done to map every URL
    to 127.0.0.1 [1] which allows to still use Google URLs to test
    that variation headers are sent correctly.
    This is not available on iOS due to platform restrictions.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/optimization_guide/prediction/prediction_manager_browsertest.cc;l=177
    
    
    Change-Id: Ib20481fe41280eab826b0abcac7a70d8c091a631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507791
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987854}

---
+++ components/variations/net/variations_flags.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VARIATIONS_NET_VARIATIONS_FLAGS_H_
+#define COMPONENTS_VARIATIONS_NET_VARIATIONS_FLAGS_H_
+
+namespace variations {
+
+// A command line switch to append variation headers to localhost requests.
+// This can be used in unittests.
+extern const char kAppendVariationsHeadersToLocalhostForTesting[];
+
+}  // namespace variations
+
+#endif  // COMPONENTS_VARIATIONS_NET_VARIATIONS_FLAGS_H_
