c11dd573350079559511158a583bfabc6120f419
Matt Menke <mmenke@chromium.org>
2022-04-01 13:57:48 +0000

    [FLEDGE] Add componentAuctions to config passed to seller worklets.
    Previously, the auctionConfig seller worklets received omitted this
    field, in violation of the spec. This requires moving nested
    component auction configurations from mojom::blink::AuctionAdConfig to
    mojom::blink::AuctionAdConfigNonSharedParams, so they're passed to
    seller worklets.
    
    Bug: 1311052
    Change-Id: I47f70823e19888697ec6ed71028b73245fcd7b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557492
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987890}

--- content/browser/interest_group/ad_auction_service_impl_unittest.cc
+++ content/browser/interest_group/ad_auction_service_impl_unittest.cc
@@ -3770,7 +3770,8 @@ function scoreAd(
       blink::mojom::AuctionAdConfigNonSharedParams::New();
   component_auction->auction_ad_config_non_shared_params
       ->interest_group_buyers = {kOriginC};
-  auction_config->component_auctions.emplace_back(std::move(component_auction));
+  auction_config->auction_ad_config_non_shared_params->component_auctions
+      .emplace_back(std::move(component_auction));
   absl::optional<GURL> auction_result =
       RunAdAuctionAndFlush(std::move(auction_config));
   ASSERT_NE(auction_result, absl::nullopt);
@@ -3879,7 +3880,8 @@ function scoreAd(
       blink::mojom::AuctionAdConfigNonSharedParams::New();
   component_auction->auction_ad_config_non_shared_params
       ->interest_group_buyers = {kOriginC};
-  auction_config->component_auctions.emplace_back(std::move(component_auction));
+  auction_config->auction_ad_config_non_shared_params->component_auctions
+      .emplace_back(std::move(component_auction));
   absl::optional<GURL> auction_result =
       RunAdAuctionAndFlush(std::move(auction_config));
   ASSERT_NE(auction_result, absl::nullopt);
@@ -3990,7 +3992,8 @@ function scoreAd(
       blink::mojom::AuctionAdConfigNonSharedParams::New();
   component_auction->auction_ad_config_non_shared_params
       ->interest_group_buyers = {kOriginC};
-  auction_config->component_auctions.emplace_back(std::move(component_auction));
+  auction_config->auction_ad_config_non_shared_params->component_auctions
+      .emplace_back(std::move(component_auction));
   absl::optional<GURL> auction_result =
       RunAdAuctionAndFlush(std::move(auction_config));
   EXPECT_EQ(auction_result, absl::nullopt);
