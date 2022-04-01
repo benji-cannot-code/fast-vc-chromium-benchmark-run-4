27231f4fd66a79263f2aedd575f53f375d76bd1f
Hong Xu <xuhong@chromium.org>
2022-04-01 01:52:09 +0000

    Use the new test library for RecordHandler tests
    Use the new test support library in `test_response_payload.h` for tests
    in `record_handler_impl_unittest.cc`.
    
    Bug: b:214040998
    Change-Id: Icacbebbccc1f21f9e56f3ec497d748baa24a575a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564275
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#987748}

--- chrome/browser/policy/messaging_layer/util/test_response_payload.h
+++ chrome/browser/policy/messaging_layer/util/test_response_payload.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_POLICY_MESSAGING_LAYER_UTIL_TEST_RESPONSE_PAYLOAD_H_
 #define CHROME_BROWSER_POLICY_MESSAGING_LAYER_UTIL_TEST_RESPONSE_PAYLOAD_H_
 
+#include <cstddef>
+
 #include "base/values.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
