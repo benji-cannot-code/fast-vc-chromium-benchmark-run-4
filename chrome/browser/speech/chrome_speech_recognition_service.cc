b8c9da345e7289ec55c6e522406852e662567c58
evliu <evliu@google.com>
2022-04-01 18:32:27 +0000

    Clean up //chrome/browser/speech
    This CL cleans up parts of //chrome/browser/speech by adding/removing required/unused dependencies.
    
    Change-Id: Ib7f9d3632908bcc10a407a98aac82d8a0568e827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546847
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#988021}

--- chrome/browser/speech/chrome_speech_recognition_service.cc
+++ chrome/browser/speech/chrome_speech_recognition_service.cc
@@ -15,11 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/service_process_host.h"
-#include "content/public/browser/storage_partition.h"
 #include "media/base/media_switches.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
-#include "services/network/network_context.h"
-#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 
 namespace speech {
 
--- chrome/services/speech/speech_recognition_service_impl.h
+++ chrome/services/speech/speech_recognition_service_impl.h
@@ -51,8 +51,6 @@ class SpeechRecognitionServiceImpl
       BindRecognizerCallback callback) override;
 
  protected:
-  void DisconnectHandler();
-
   mojo::Receiver<media::mojom::SpeechRecognitionService> receiver_;
 
   // The set of receivers used to receive messages from the renderer clients.
