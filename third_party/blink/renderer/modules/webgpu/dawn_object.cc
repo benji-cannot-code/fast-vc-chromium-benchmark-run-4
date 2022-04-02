c2d88a3de06d1f6bfc90dd3e8a7573a93580dd66
Brandon Jones <bajones@chromium.org>
2022-04-02 00:08:34 +0000

    Communicate WebGPU label attribute changes to Dawn
    This change forwards label changes to Dawn for every object that has
    *SetLabel() implemented.
    
    Does not include Device or Queue, since those will require
    https://dawn-review.googlesource.com/c/dawn/+/85540 to land first
    but doing so requires the command_buffer change from this patch.
    
    Bug: dawn:840
    Change-Id: I1ab173e4d036fea49f7d7343a29ffefdc4b301bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565481
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988194}

--- third_party/blink/renderer/modules/webgpu/dawn_object.cc
+++ third_party/blink/renderer/modules/webgpu/dawn_object.cc
@@ -37,6 +37,7 @@ void DawnObjectBase::setLabel(ScriptState* script_state,
 void DawnObjectBase::setLabel(const String& value) {
   // TODO: Relay label changes to Dawn
   label_ = value;
+  setLabelImpl(value);
 }
 
 void DawnObjectBase::EnsureFlush() {
--- third_party/blink/renderer/modules/webgpu/gpu_texture.h
+++ third_party/blink/renderer/modules/webgpu/gpu_texture.h
@@ -54,6 +54,11 @@ class GPUTexture : public DawnObject<WGPUTexture> {
   WGPUTextureUsage Usage() { return usage_; }
 
  private:
+  void setLabelImpl(const String& value) override {
+    std::string utf8_label = value.Utf8();
+    GetProcs().textureSetLabel(GetHandle(), utf8_label.c_str());
+  }
+
   WGPUTextureDimension dimension_;
   WGPUTextureFormat format_;
   WGPUTextureUsage usage_;
