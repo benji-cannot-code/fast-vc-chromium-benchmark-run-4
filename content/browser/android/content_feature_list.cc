d2d481990a2096e6763629cc7939e69d38d123c2
Clark DuVall <cduvall@chromium.org>
2022-04-04 18:16:57 +0000

    Put Android font lookup cache behind a feature
    A cache was introduced in https://crrev.com/c/3496065 to reduce crashes
    in the PrefetchAndroidFonts feature. This crash fix unintentionally
    affected the metrics in the control arm. This change puts the cache
    behind a feature to get accurate measurements of the changes from this
    experiment.
    
    Bug: 1270949
    Change-Id: I9db35065f3a2b2d5d3478bac91e2d1e0276c2eb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569844
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988581}

--- content/browser/android/content_feature_list.cc
+++ content/browser/android/content_feature_list.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "content/public/android/content_jni_headers/ContentFeatureListImpl_jni.h"
 #include "content/public/common/content_features.h"
+#include "third_party/blink/public/common/features.h"
 #include "ui/accessibility/accessibility_features.h"
 
 using base::android::ConvertJavaStringToUTF8;
@@ -22,6 +23,7 @@ namespace {
 // this array may either refer to features defined in the header of this file or
 // in other locations in the code base (e.g. content_features.h).
 const base::Feature* const kFeaturesExposedToJava[] = {
+    &blink::features::kPrefetchAndroidFonts,
     &features::kAutoDisableAccessibility,
     &features::kAccessibilityPageZoom,
     &features::kBackgroundMediaRendererHasModerateBinding,
