7d5c286d80c3ee8fe624f81ccc260184f24d0b71
Abbas Nayebi <nayebi@google.com>
2022-04-04 20:57:15 +0000

    [eche] Moving the bubble along with the status area widget when 1) Tablet mode status changes 2) Shelf is changed 3) Screen layout changes
    Bug: b/227140198 b/219067116
    
    Change-Id: Id250b81fcb11dadc550527f25c6d090f06e1a6c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569982
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#988661}

--- ash/system/eche/eche_tray.h
+++ ash/system/eche/eche_tray.h
@@ -10,13 +10,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/session/session_observer.h"
+#include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/session/session_controller_impl.h"
+#include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_observer.h"
+#include "ash/shell.h"
+#include "ash/shell_observer.h"
 #include "ash/system/eche/eche_icon_loading_indicator_view.h"
+#include "ash/system/screen_layout_observer.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "ash/webui/eche_app_ui/mojom/eche_app.mojom.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/gtest_prod_util.h"
 #include "base/timer/timer.h"
 #include "components/session_manager/session_manager_types.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/views/controls/button/button.h"
 #include "url/gurl.h"
 
@@ -48,7 +56,12 @@ class PhoneHubTray;
 
 // This class represents the Eche tray button in the status area and
 // controls the bubble that is shown when the tray button is clicked.
-class ASH_EXPORT EcheTray : public TrayBackgroundView, public SessionObserver {
+class ASH_EXPORT EcheTray : public TrayBackgroundView,
+                            public SessionObserver,
+                            public ScreenLayoutObserver,
+                            public ShelfObserver,
+                            public TabletModeObserver,
+                            ShellObserver {
  public:
   METADATA_HEADER(EcheTray);
 
@@ -156,6 +169,27 @@ class ASH_EXPORT EcheTray : public TrayBackgroundView, public SessionObserver {
   PhoneHubTray* GetPhoneHubTray();
   EcheIconLoadingIndicatorView* GetLoadingIndicator();
 
+  // Updates the bubble's position based on the movements of the shelf.
+  void UpdateBubbleBounds();
+
+  // ScreenLayoutObserver:
+  void OnDisplayConfigurationChanged() override;
+
+  // ShelfObserver:
+  void OnAutoHideStateChanged(ShelfAutoHideState new_state) override;
+  void OnShelfIconPositionsChanged() override;
+
+  // TabletModeObserver:
+  void OnTabletModeStarted() override;
+  void OnTabletModeEnded() override;
+
+  // ShellObserver:
+  void OnShelfAlignmentChanged(aura::Window* root_window,
+                               ShelfAlignment old_alignment) override;
+
+  // returns the position of the anchor that bubble needs to be anchored to.
+  gfx::Rect GetAnchor();
+
   // The url that is transferred to the web view.
   // In the current implementation, this is supposed to be
   // Eche window URL. However, the bubble does not interpret,
@@ -172,9 +206,6 @@ class ASH_EXPORT EcheTray : public TrayBackgroundView, public SessionObserver {
   // owned by `bubble_`
   AshWebView* web_view_ = nullptr;
 
-  base::ScopedObservation<SessionControllerImpl, SessionObserver>
-      observed_session_{this};
-
   GracefulCloseCallback graceful_close_callback_;
 
   // The unload timer to force close EcheTray in case unload error.
@@ -182,6 +213,18 @@ class ASH_EXPORT EcheTray : public TrayBackgroundView, public SessionObserver {
 
   views::Button* close_button_ = nullptr;
   views::Button* minimize_button_ = nullptr;
+
+  // Observers
+  base::ScopedObservation<SessionControllerImpl, SessionObserver>
+      observed_session_{this};
+  base::ScopedObservation<Shelf, ShelfObserver> shelf_observation_{this};
+  base::ScopedObservation<TabletModeController, TabletModeObserver>
+      tablet_mode_observation_{this};
+  base::ScopedObservation<Shell,
+                          ShellObserver,
+                          &Shell::AddShellObserver,
+                          &Shell::RemoveShellObserver>
+      shell_observer_{this};
 
   base::WeakPtrFactory<EcheTray> weak_factory_{this};
 };
