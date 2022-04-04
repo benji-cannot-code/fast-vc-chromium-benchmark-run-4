7c25aed7c84dc0b638f251a38366e61d9185c997
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-04-04 16:37:40 +0000

    Import wpt@bc351be47b742e2d306907de797f6dd56093f092
    Using wpt-import in Chromium 64854898b22a6d7ce7956dccea6399ae51a70c6e.
    With Chromium commits locally applied on WPT:
    9df922f8ad "[renderblocking] Register finish callbacks for link header preloads"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dgrogan@chromium.org:
      external/wpt/css/css-tables
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I745be8dacdfa25d933b7a6b01282f66eaea7b98b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566095
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#988525}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/get_current_url/file.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from tests.support import platform_name
+from tests.support.asserts import assert_success
+
+
+def get_current_url(session):
+    return session.transport.send(
+        "GET", "session/{session_id}/url".format(**vars(session)))
+
+
+def test_get_current_url_file_protocol(session, server_config):
+    # tests that the browsing context remains the same
+    # when navigated privileged documents
+    path = server_config["doc_root"]
+    if platform_name == "windows":
+        # Convert the path into the format eg. /c:/foo/bar
+        path = "/{}".format(path.replace("\\", "/"))
+    url = u"file://{}".format(path)
+    session.url = url
+
+    response = get_current_url(session)
+    if response.status == 200 and response.body['value'].endswith('/'):
+        url += '/'
+    assert_success(response, url)
