ab6e9fbf6c181123360d50ad2b3b6ce6957e139c
Geng Li <ligeng@google.com>
2022-04-04 18:34:31 +0000

    remove demo mode language and keyboard selection
    Bug: b/227652797
    Change-Id: I284320524accae88bbb417a650afc73a3e09989f
    screenshot: https://screenshot.googleplex.com/8EBznnC5hWddHF3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564188
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Geng Li <ligeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#988592}

--- chrome/browser/ash/login/screens/demo_preferences_screen.h
+++ chrome/browser/ash/login/screens/demo_preferences_screen.h
@@ -36,8 +36,6 @@ class DemoPreferencesScreen
 
   ~DemoPreferencesScreen() override;
 
-  void SetLocale(const std::string& locale);
-  void SetInputMethod(const std::string& input_method);
   void SetDemoModeCountry(const std::string& country_id);
 
   // Called when view is being destroyed. If Screen is destroyed earlier
@@ -60,14 +58,6 @@ class DemoPreferencesScreen
 
   // Passes current input method to the context, so it can be shown in the UI.
   void UpdateInputMethod(input_method::InputMethodManager* input_manager);
-
-  // Initial locale that was set when the screen was shown. It will be restored
-  // if user presses back button.
-  std::string initial_locale_;
-
-  // Initial input method that was set when the screen was shown. It will be
-  // restored if user presses back button.
-  std::string initial_input_method_;
 
   base::ScopedObservation<input_method::InputMethodManager,
                           input_method::InputMethodManager::Observer>
