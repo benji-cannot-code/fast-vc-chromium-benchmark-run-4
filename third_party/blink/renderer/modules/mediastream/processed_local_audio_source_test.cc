96315982ce91fb6bed55b3a39c3a2a7f520fec03
Tony Herre <toprice@chromium.org>
2022-04-04 12:41:14 +0000

    Remove SetPlatformSource calls from mediastream unittests
    Part of an effort to remove SetPlatformSource from MediaStreamSource,
    instead using a new constructor, to better align their lifecycles.
    
    #removeplatformsetters
    
    Bug: 1302689
    Change-Id: I6fb421bc3b3ebfd26b78e853df64570626055f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568543
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988462}

--- third_party/blink/renderer/modules/mediastream/processed_local_audio_source_test.cc
+++ third_party/blink/renderer/modules/mediastream/processed_local_audio_source_test.cc
@@ -88,15 +88,6 @@ class ProcessedLocalAudioSourceTest : public SimTest,
 
   ~ProcessedLocalAudioSourceTest() override = default;
 
-  void SetUp() override {
-    SimTest::SetUp();
-    audio_source_ = MakeGarbageCollected<MediaStreamSource>(
-        String::FromUTF8("audio_label"), MediaStreamSource::kTypeAudio,
-        String::FromUTF8("audio_track"), false /* remote */);
-    audio_component_ = MakeGarbageCollected<MediaStreamComponent>(
-        audio_source_->Id(), audio_source_);
-  }
-
   void TearDown() override {
     SimTest::TearDown();
     audio_source_ = nullptr;
@@ -118,7 +109,11 @@ class ProcessedLocalAudioSourceTest : public SimTest,
             base::DoNothing(),
             scheduler::GetSingleThreadTaskRunnerForTesting());
     source->SetAllowInvalidRenderFrameIdForTesting(true);
-    audio_source_->SetPlatformSource(std::move(source));
+    audio_source_ = MakeGarbageCollected<MediaStreamSource>(
+        String::FromUTF8("audio_label"), MediaStreamSource::kTypeAudio,
+        String::FromUTF8("audio_track"), false /* remote */, std::move(source));
+    audio_component_ = MakeGarbageCollected<MediaStreamComponent>(
+        audio_source_->Id(), audio_source_);
   }
 
   void CheckSourceFormatMatches(const media::AudioParameters& params) {
