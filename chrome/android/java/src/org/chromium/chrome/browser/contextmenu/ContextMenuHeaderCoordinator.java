00914266e64819f24111ade62f9f28b7891428d0
Wenyu Fu <wenyufu@google.com>
2022-04-04 20:03:28 +0000

    Make TouchDragAndContextMenu equivalent of ContextMenuPopup
    Add several helper functions in ContextMenuUtils, and make every non
    testing code check both ContextMenuPopupStyle and
    TouchDragAndContextMenu so enabling latter along will also enable
    context menu popup.
    
    Bug: 1294312
    Change-Id: I07d4bce55dd9880820f83b8d99e21f2e8d87991c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566224
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988634}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderCoordinator.java
@@ -13,7 +13,6 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.ChromeAutocompleteSchemeClassifier;
 import org.chromium.chrome.browser.performance_hints.PerformanceHintsObserver.PerformanceClass;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -37,11 +36,8 @@ class ContextMenuHeaderCoordinator {
 
     @VisibleForTesting
     static PropertyModel buildModel(Context context, String title, CharSequence url) {
-        boolean usePopupContextMenu =
-                ChromeFeatureList.isEnabled(ChromeFeatureList.CONTEXT_MENU_POPUP_STYLE);
-        boolean hideHeaderImage = ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
-                ChromeFeatureList.CONTEXT_MENU_POPUP_STYLE,
-                ContextMenuCoordinator.HIDE_HEADER_IMAGE_PARAM, false);
+        boolean usePopupContextMenu = ContextMenuUtils.forcePopupStyleEnabled();
+        boolean hideHeaderImage = ContextMenuUtils.hideContextMenuHeaderImage();
 
         int monogramSizeDimen = usePopupContextMenu
                 ? R.dimen.context_menu_popup_header_monogram_size
