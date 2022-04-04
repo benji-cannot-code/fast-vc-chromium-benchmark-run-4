3bad5fb11732aec1180a863825433d14b2bad7b1
Colin Kincaid <ckincaid@chromium.org>
2022-04-04 20:14:48 +0000

    Fix fingerprint enrollment animations for dark mode (Part 4 of N)
    This CL renames the "enrollment done" icon as the "fingerprint scanned"
    icon for clarity of meaning and allows the icon to change its colors
    according to dark/light mode.
    
    Dark before: https://screenshot.googleplex.com/96EJfcaPBRDoo8V
    Dark after: https://screenshot.googleplex.com/7DuNsVW5ThtwNWZ
    Light before: https://screenshot.googleplex.com/5XtzhUJsbzYDbKN
    Light after: https://screenshot.googleplex.com/54qoh6E2hfkpsGD
    
    Forthcoming CLs will fix the animating fingerprint within the progress
    circle, the enrollment-done tick animation, and the fingerprint
    enrollment onboarding animations. See attached bug for full plan.
    
    Bug: 1295944
    Change-Id: I2498c6ea4ec0d0090177c32fc13060d99da10f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499160
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988644}

--- ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.m.d.ts
+++ ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.m.d.ts
@@ -4,8 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {CrLottieElement} from 'chrome://resources/cr_elements/cr_lottie/cr_lottie.m.js';
+import {IronIconElement} from 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
 
+export const FINGERPRINT_SCANNED_ICON_DARK: string;
+export const FINGERPRINT_SCANNED_ICON_LIGHT: string;
 export const FINGERPRINT_TICK_DARK_URL: string;
 export const FINGERPRINT_TICK_LIGHT_URL: string;
 export const PROGRESS_CIRCLE_BACKGROUND_COLOR_DARK: string;
@@ -25,6 +28,7 @@ interface CrFingerprintProgressArcElement extends LegacyElementMixin,
   isComplete(): boolean;
   $: {
     canvas: HTMLCanvasElement,
+    fingerprintScanned: IronIconElement,
     scanningAnimation: CrLottieElement,
   };
 }
