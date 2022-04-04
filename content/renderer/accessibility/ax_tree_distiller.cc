24c9102ae66ebaba224c3a4741ffa68694c8988b
Abigail Klein <abigailbklein@google.com>
2022-04-04 21:39:11 +0000

    [Read Anything] Introduce AXTreeDistillerTest.
    Bug: 1266555
    Change-Id: I6cdcf44c5cc4c11b9f0a5b7964a5f64564c5070c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570028
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#988692}

--- content/renderer/accessibility/ax_tree_distiller.cc
+++ content/renderer/accessibility/ax_tree_distiller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/accessibility/ax_tree_distiller.h"
 
+#include <memory>
 #include <queue>
 #include <vector>
 
--- content/renderer/accessibility/ax_tree_distiller.h
+++ content/renderer/accessibility/ax_tree_distiller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "content/common/content_export.h"
 #include "ui/accessibility/ax_node_id_forward.h"
 #include "ui/accessibility/ax_tree_update_forward.h"
 
@@ -22,7 +23,7 @@ class RenderFrameImpl;
 //  A class that creates and stores a distilled AXTree for a particular render
 //  frame.
 //
-class AXTreeDistiller {
+class CONTENT_EXPORT AXTreeDistiller {
  public:
   explicit AXTreeDistiller(RenderFrameImpl* render_frame);
   ~AXTreeDistiller();
