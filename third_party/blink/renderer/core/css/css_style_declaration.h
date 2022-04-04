c8f5317b19c4695b370830b86bcc5c2db8f5b71c
Yuki Shiino <yukishiino@chromium.org>
2022-04-04 10:17:43 +0000

    bindings: Do not make side effect for non-existing CSS properties
    Avoids performing type conversion of a property value if the
    property name is not a valid CSS property name.
    
    Bug: 1310062
    Change-Id: Iedbeec7fafa6937715401f9f350bbf666ef9559b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565510
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988435}

--- third_party/blink/renderer/core/css/css_style_declaration.h
+++ third_party/blink/renderer/core/css/css_style_declaration.h
@@ -102,7 +102,7 @@ class CORE_EXPORT CSSStyleDeclaration : public ScriptWrappable,
   // an argument (see bug 829408).
   NamedPropertySetterResult AnonymousNamedSetter(ScriptState*,
                                                  const AtomicString& name,
-                                                 const String& value);
+                                                 const ScriptValue& value);
   NamedPropertyDeleterResult AnonymousNamedDeleter(const AtomicString& name);
   void NamedPropertyEnumerator(Vector<String>& names, ExceptionState&);
   bool NamedPropertyQuery(const AtomicString&, ExceptionState&);
