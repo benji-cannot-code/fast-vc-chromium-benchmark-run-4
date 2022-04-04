85625ab3c405264bc7a21787a3b44036a979a436
Bo Liu <boliu@chromium.org>
2022-04-04 18:21:53 +0000

    viz: Add AChoreographer vsync id plumbing
    Get deadline information and store it in optional BeginFrameArgs field.
    
    Bug: 1308459
    Change-Id: Ia7f31d189895ad921a6b824053f3e63cdba9254c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566369
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988584}

--- components/viz/service/frame_sinks/external_begin_frame_source_android.h
+++ components/viz/service/frame_sinks/external_begin_frame_source_android.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_weak_ref.h"
 #include "base/time/time.h"
+#include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
 #include "components/viz/service/viz_service_export.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace viz {
 
@@ -51,7 +53,10 @@ class VIZ_SERVICE_EXPORT ExternalBeginFrameSourceAndroid
   void OnNeedsBeginFrames(bool needs_begin_frames) override;
 
   void SetEnabled(bool enabled);
-  void OnVSyncImpl(int64_t time_nanos, base::TimeDelta vsync_period);
+  void OnVSyncImpl(int64_t time_nanos,
+                   int64_t deadline_nanos,
+                   base::TimeDelta vsync_period,
+                   absl::optional<PossibleDeadlines> possible_deadlines);
 
   std::unique_ptr<AChoreographerImpl> achoreographer_;
   base::android::ScopedJavaGlobalRef<jobject> j_object_;
