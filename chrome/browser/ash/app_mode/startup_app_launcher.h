5be19c25f02acf99eee216466d6b15b3cfae2cb3
Ben Franz <bfranz@chromium.org>
2022-04-04 09:00:02 +0000

    Create a ChromeKioskAppLauncher that takes care of the launch part of app kiosk setup
    This further splits the StartupAppLauncher into a third component for the launch part. This code will run in Lacros after the split.
    
    This change introduces some changes in the order of operations: enabling secondary apps now happens in the launch part rather than the install part. This change is reflected in changes to the unit tests.
    
    Bug: 226560767
    Test: StartupAppLauncherUnittest
    Change-Id: Ibc214a95f711ce359b111fbfaf2fc0c8a7d80877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3548857
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988419}

--- chrome/browser/ash/app_mode/startup_app_launcher.h
+++ chrome/browser/ash/app_mode/startup_app_launcher.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_manager_observer.h"
 #include "chrome/browser/chromeos/app_mode/chrome_kiosk_app_installer.h"
+#include "chrome/browser/chromeos/app_mode/chrome_kiosk_app_launcher.h"
 #include "chrome/browser/extensions/install_observer.h"
 #include "chrome/browser/extensions/install_tracker.h"
 #include "extensions/browser/app_window/app_window_registry.h"
@@ -61,15 +62,17 @@ class StartupAppLauncher : public KioskAppLauncher,
   // KioskAppLauncher:
   void Initialize() override;
   void ContinueWithNetworkReady() override;
-  void LaunchApp() override;
   void RestartLauncher() override;
+  void LaunchApp() override;
+
+  void BeginInstall();
+  void OnInstallComplete(ChromeKioskAppInstaller::InstallResult result);
+  void OnInstallSuccess();
+
+  void OnLaunchComplete(ChromeKioskAppLauncher::LaunchResult result);
 
   void OnLaunchSuccess();
   void OnLaunchFailure(KioskAppLaunchError::Error error);
-
-  void FinalizeAppInstall();
-  void BeginInstall(bool finalize_only = false);
-  void OnInstallComplete(ChromeKioskAppInstaller::InstallResult result);
 
   void MaybeInitializeNetwork();
   bool RetryWhenNetworkIsAvailable();
@@ -90,6 +93,7 @@ class StartupAppLauncher : public KioskAppLauncher,
   LaunchState state_ = LaunchState::kNotStarted;
 
   std::unique_ptr<ChromeKioskAppInstaller> installer_;
+  std::unique_ptr<ChromeKioskAppLauncher> launcher_;
 
   extensions::AppWindowRegistry* window_registry_;
 
