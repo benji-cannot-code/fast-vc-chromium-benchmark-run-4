0d51af162709f698a0ff8cb84eff73c967ad13b9
Nan Lin <linnan@chromium.org>
2022-04-05 17:29:13 +0000

    [attribution_reporting] Use single sendReports interface for event-level and aggregatable reports
    Change-Id: Ic5bffa0864a39f1a8565bf7a947eadfc5c52f2e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546259
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989036}

--- content/browser/attribution_reporting/attribution_internals_mojom_traits.cc
+++ content/browser/attribution_reporting/attribution_internals_mojom_traits.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/attribution_reporting/attribution_internals_mojom_traits.h"
 
+#include "base/check.h"
+
+#include "third_party/abseil-cpp/absl/types/variant.h"
+
 namespace mojo {
 
 // static
@@ -26,6 +30,62 @@ bool StructTraits<
   *out =
       content::AttributionReport::AggregatableAttributionData::Id(data.value());
   return true;
+}
+
+// static
+content::AttributionReport::EventLevelData::Id
+UnionTraits<attribution_internals::mojom::ReportIDDataView,
+            content::AttributionReport::Id>::
+    event_level_id(const content::AttributionReport::Id& id) {
+  return absl::get<content::AttributionReport::EventLevelData::Id>(id);
+}
+
+// static
+content::AttributionReport::AggregatableAttributionData::Id
+UnionTraits<attribution_internals::mojom::ReportIDDataView,
+            content::AttributionReport::Id>::
+    aggregatable_attribution_id(const content::AttributionReport::Id& id) {
+  return absl::get<content::AttributionReport::AggregatableAttributionData::Id>(
+      id);
+}
+
+// static
+bool UnionTraits<attribution_internals::mojom::ReportIDDataView,
+                 content::AttributionReport::Id>::
+    Read(attribution_internals::mojom::ReportIDDataView data,
+         content::AttributionReport::Id* out) {
+  if (data.is_event_level_id()) {
+    content::AttributionReport::EventLevelData::Id event_level_id;
+    if (!data.ReadEventLevelId(&event_level_id))
+      return false;
+    *out = event_level_id;
+    return true;
+  } else if (data.is_aggregatable_attribution_id()) {
+    content::AttributionReport::AggregatableAttributionData::Id
+        aggregatable_attribution_id;
+    if (!data.ReadAggregatableAttributionId(&aggregatable_attribution_id))
+      return false;
+    *out = aggregatable_attribution_id;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// static
+attribution_internals::mojom::ReportIDDataView::Tag
+UnionTraits<attribution_internals::mojom::ReportIDDataView,
+            content::AttributionReport::Id>::
+    GetTag(const content::AttributionReport::Id& id) {
+  if (absl::holds_alternative<content::AttributionReport::EventLevelData::Id>(
+          id)) {
+    return attribution_internals::mojom::ReportIDDataView::Tag::EVENT_LEVEL_ID;
+  } else {
+    DCHECK(absl::holds_alternative<
+           content::AttributionReport::AggregatableAttributionData::Id>(id));
+    return attribution_internals::mojom::ReportIDDataView::Tag::
+        AGGREGATABLE_ATTRIBUTION_ID;
+  }
 }
 
 }  // namespace mojo
--- content/browser/attribution_reporting/attribution_internals_mojom_traits.h
+++ content/browser/attribution_reporting/attribution_internals_mojom_traits.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/attribution_source_type.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
+#include "mojo/public/cpp/bindings/union_traits.h"
 
 namespace mojo {
 
@@ -103,6 +104,23 @@ class StructTraits<
       attribution_internals::mojom::AggregatableAttributionReportIDDataView
           data,
       content::AttributionReport::AggregatableAttributionData::Id* out);
+};
+
+template <>
+class UnionTraits<attribution_internals::mojom::ReportIDDataView,
+                  content::AttributionReport::Id> {
+ public:
+  static content::AttributionReport::EventLevelData::Id event_level_id(
+      const content::AttributionReport::Id& id);
+
+  static content::AttributionReport::AggregatableAttributionData::Id
+  aggregatable_attribution_id(const content::AttributionReport::Id& id);
+
+  static bool Read(attribution_internals::mojom::ReportIDDataView data,
+                   content::AttributionReport::Id* out);
+
+  static attribution_internals::mojom::ReportIDDataView::Tag GetTag(
+      const content::AttributionReport::Id& id);
 };
 
 }  // namespace mojo
