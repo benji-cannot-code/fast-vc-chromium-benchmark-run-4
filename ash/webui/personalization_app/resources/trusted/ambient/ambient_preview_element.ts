0d066ecad58c229d42ceb8456d94bc91b1b1a6ab
Jason Thai <jasontt@chromium.org>
2022-04-05 18:05:44 +0000

    personalization: clickable ambient preview image
    Navigate to ambient subpage if users click on main page ambient
    preview image.
    Update cursor style for both wallpaper and ambient previews.
    
    BUG=b/225198614
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I425172d3f0e57f6a58ba86fc577d43ad3381fabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566439
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989063}

--- ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
+++ ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
@@ -35,6 +35,10 @@ export class AmbientPreview extends WithPersonalizationStore {
 
   static get properties() {
     return {
+      clickable: {
+        type: Boolean,
+        value: false,
+      },
       ambientModeEnabled_: Boolean,
       albums_: {
         type: Array,
@@ -58,6 +62,8 @@ export class AmbientPreview extends WithPersonalizationStore {
       }
     };
   }
+
+  clickable: boolean;
 
   private ambientModeEnabled_: boolean|null;
   private albums_: AmbientModeAlbum[]|null;
