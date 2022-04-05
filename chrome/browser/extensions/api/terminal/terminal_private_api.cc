b6da0778e2c10b3bd1c4c9994bebb4ba6053df7c
Joel Hockey <joelhockey@chromium.org>
2022-04-05 21:08:17 +0000

    Revert "Allow Terminal to read crostini.terminal_ssh_allowed_by_policy"
    This reverts commit cced8e6bea4f57fee4715cce303762fbfaca6d6d.
    
    Reason for revert: I'm going to hold off landing this until
    internal policy is updated.
    
    Original change's description:
    > Allow Terminal to read crostini.terminal_ssh_allowed_by_policy
    >
    > Bug: b/227418623
    > Change-Id: I4b893dba87e2bca61b4164eb74cc9319b05e151e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564462
    > Reviewed-by: Jason Lin <lxj@google.com>
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#989143}
    
    Bug: b/227418623
    Change-Id: Id46cecbf058e6debc04acd92561517912d187d15
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573421
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989158}

--- chrome/browser/extensions/api/terminal/terminal_private_api.cc
+++ chrome/browser/extensions/api/terminal/terminal_private_api.cc
@@ -705,7 +705,6 @@ ExtensionFunction::ResponseAction TerminalPrivateGetPrefsFunction::Run() {
       crostini::prefs::kCrostiniContainers,
       crostini::prefs::kCrostiniEnabled,
       crostini::prefs::kCrostiniTerminalSettings,
-      crostini::prefs::kTerminalSshAllowedByPolicy,
   }};
 
   for (const auto& path : params->paths) {
