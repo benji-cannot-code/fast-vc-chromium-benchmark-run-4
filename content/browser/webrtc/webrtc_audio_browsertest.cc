8ab168c80097dd640a266cb645034d6dc5eeb1d2
Olga Sharonova <olka@chromium.org>
2022-04-05 09:56:27 +0000

    Reenable WebRtcAudioBrowserTest* on Mac
    The tests were disabled in 2019; the current state is unknown.
    Reenabling: if they are still flaky, we can gather diagnostics.
    
    We need to restore integration test coverage on Mac.
    
    Bug: 982421, 1215049
    Change-Id: Id0b5fddb1424bda328ca960adbbfb6013b40172b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569743
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988891}

--- content/browser/webrtc/webrtc_audio_browsertest.cc
+++ content/browser/webrtc/webrtc_audio_browsertest.cc
@@ -78,49 +78,8 @@ class WebRtcAudioBrowserTest : public WebRtcContentBrowserTestBase {
   base::test::ScopedFeatureList audio_service_features_;
 };
 
-#if BUILDFLAG(IS_MAC)
-
-// Flaky on MacOS: http://crbug.com/982421
-#define MAYBE_CanMakeVideoCallAndThenRenegotiateToAudio \
-  DISABLED_CanMakeVideoCallAndThenRenegotiateToAudio
-#define MAYBE_EstablishAudioVideoCallAndEnsureAudioIsPlaying \
-  DISABLED_EstablishAudioVideoCallAndEnsureAudioIsPlaying
-#define MAYBE_EstablishAudioOnlyCallAndEnsureAudioIsPlaying \
-  DISABLED_EstablishAudioOnlyCallAndEnsureAudioIsPlaying
-#define MAYBE_EstablishIsac16KCallAndEnsureAudioIsPlaying \
-  DISABLED_EstablishIsac16KCallAndEnsureAudioIsPlaying
-#define MAYBE_EnsureLocalVideoMuteDoesntMuteAudio \
-  DISABLED_EnsureLocalVideoMuteDoesntMuteAudio
-#define MAYBE_EnsureRemoteVideoMuteDoesntMuteAudio \
-  DISABLED_EnsureRemoteVideoMuteDoesntMuteAudio
-#define MAYBE_EstablishAudioVideoCallAndVerifyUnmutingWorks \
-  DISABLED_EstablishAudioVideoCallAndVerifyUnmutingWorks
-#define MAYBE_EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks \
-  DISABLED_EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks
-
-#else
-
-#define MAYBE_CanMakeVideoCallAndThenRenegotiateToAudio \
-  CanMakeVideoCallAndThenRenegotiateToAudio
-#define MAYBE_EstablishAudioVideoCallAndEnsureAudioIsPlaying \
-  EstablishAudioVideoCallAndEnsureAudioIsPlaying
-#define MAYBE_EstablishAudioOnlyCallAndEnsureAudioIsPlaying \
-  EstablishAudioOnlyCallAndEnsureAudioIsPlaying
-#define MAYBE_EstablishIsac16KCallAndEnsureAudioIsPlaying \
-  EstablishIsac16KCallAndEnsureAudioIsPlaying
-#define MAYBE_EnsureLocalVideoMuteDoesntMuteAudio \
-  EnsureLocalVideoMuteDoesntMuteAudio
-#define MAYBE_EnsureRemoteVideoMuteDoesntMuteAudio \
-  EnsureRemoteVideoMuteDoesntMuteAudio
-#define MAYBE_EstablishAudioVideoCallAndVerifyUnmutingWorks \
-  EstablishAudioVideoCallAndVerifyUnmutingWorks
-#define MAYBE_EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks \
-  EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks
-
-#endif  // BUILDFLAG(IS_MAC)
-
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_CanMakeVideoCallAndThenRenegotiateToAudio) {
+                       CanMakeVideoCallAndThenRenegotiateToAudio) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   std::string audio_only_constraints = BuildConstraints(kAudioConstraints, "");
@@ -130,7 +89,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EstablishAudioVideoCallAndEnsureAudioIsPlaying) {
+                       EstablishAudioVideoCallAndEnsureAudioIsPlaying) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall("callAndEnsureAudioIsPlaying(" +
@@ -138,7 +97,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EstablishAudioOnlyCallAndEnsureAudioIsPlaying) {
+                       EstablishAudioOnlyCallAndEnsureAudioIsPlaying) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall("callAndEnsureAudioIsPlaying(" +
@@ -146,7 +105,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EstablishIsac16KCallAndEnsureAudioIsPlaying) {
+                       EstablishIsac16KCallAndEnsureAudioIsPlaying) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall(
@@ -170,7 +129,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EnsureLocalVideoMuteDoesntMuteAudio) {
+                       EnsureLocalVideoMuteDoesntMuteAudio) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall(
@@ -178,7 +137,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EnsureRemoteVideoMuteDoesntMuteAudio) {
+                       EnsureRemoteVideoMuteDoesntMuteAudio) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall(
@@ -186,7 +145,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
-                       MAYBE_EstablishAudioVideoCallAndVerifyUnmutingWorks) {
+                       EstablishAudioVideoCallAndVerifyUnmutingWorks) {
   std::string constraints =
       BuildConstraints(kAudioConstraints, kVideoConstraints);
   MakeAudioDetectingPeerConnectionCall("callAndEnsureAudioTrackUnmutingWorks(" +
@@ -197,7 +156,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioBrowserTest,
 // external/wpt/webrtc/RTCRtpReceiver-getSynchronizationSources.https.html
 IN_PROC_BROWSER_TEST_F(
     WebRtcAudioBrowserTest,
-    MAYBE_EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks) {
+    EstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks) {
   MakeAudioDetectingPeerConnectionCall(
       "testEstablishAudioOnlyCallAndVerifyGetSynchronizationSourcesWorks();");
 }
