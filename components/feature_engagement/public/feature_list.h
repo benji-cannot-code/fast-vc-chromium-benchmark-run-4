b3a28fadd6525303083a133cab7ead4d0583fbc2
Louis Romero <lpromero@google.com>
2022-04-05 10:05:55 +0000

    [iOS] Add PasswordSuggestions IPH feature
    Bug: 1310735
    Change-Id: I53b6dc2145c7ef7dc1d982a306a7801a5a996a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553009
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Auto-Submit: Louis Romero <lpromero@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988894}

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -220,6 +220,8 @@ DEFINE_VARIATION_PARAM(kIPHBadgedTranslateManualTriggerFeature,
 DEFINE_VARIATION_PARAM(kIPHDiscoverFeedHeaderFeature,
                        "IPH_DiscoverFeedHeaderMenu");
 DEFINE_VARIATION_PARAM(kIPHDefaultSiteViewFeature, "IPH_DefaultSiteView");
+DEFINE_VARIATION_PARAM(kIPHPasswordSuggestionsFeature,
+                       "IPH_PasswordSuggestions");
 #endif  // BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) || \
@@ -356,6 +358,7 @@ constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHBadgedTranslateManualTriggerFeature),
         VARIATION_ENTRY(kIPHDiscoverFeedHeaderFeature),
         VARIATION_ENTRY(kIPHDefaultSiteViewFeature),
+        VARIATION_ENTRY(kIPHPasswordSuggestionsFeature),
 #elif BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_FUCHSIA)
         VARIATION_ENTRY(kIPHDesktopTabGroupsNewGroupFeature),
