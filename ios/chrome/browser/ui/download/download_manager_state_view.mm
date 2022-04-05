df379d9434aad7247ac2e7506f29f48e93bbc6bc
Joemer Ramos <joemerramos@chromium.org>
2022-04-05 15:26:46 +0000

    [iOS] Download API NOTREACHED correction
    It turns out that the switch case statement in this CL was reached in
    the kFailedNotResumable state so I'm moving the edge case to follow
    the same logic as kFailed to avoid crashing dev build with NOTREACHED.
    
    Bug: 1252380
    Change-Id: If492840e7bbecf90389404ab98130e1cd5e8a590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536330
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988979}

--- ios/chrome/browser/ui/download/download_manager_state_view.mm
+++ ios/chrome/browser/ui/download/download_manager_state_view.mm
@@ -115,7 +115,8 @@ const CGFloat kInProgressScale = 0.65f;
           CATransform3DScale(CATransform3DIdentity, 0, 0, 1);
       break;
     case kDownloadManagerStateSucceeded:
-    case kDownloadManagerStateFailed: {
+    case kDownloadManagerStateFailed:
+    case kDownloadManagerStateFailedNotResumable: {
       self.badgeLayer.contents = (__bridge id)[self completionBadgeImage];
       self.iconLayer.path = self.documentPath.CGPath;
       self.iconLayer.fillColor = self.documentColor.CGColor;
@@ -134,9 +135,6 @@ const CGFloat kInProgressScale = 0.65f;
       }
       break;
     }
-    case kDownloadManagerStateFailedNotResumable:
-      NOTREACHED();
-      break;
     case kDownloadManagerStateInProgress:
       if (CGPathEqualToPath(self.iconLayer.path, self.downloadPath.CGPath)) {
         // There should be no animation when changing from downloadPath to
