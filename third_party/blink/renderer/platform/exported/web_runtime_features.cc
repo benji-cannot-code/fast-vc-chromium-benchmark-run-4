5a675e9b287209e7e2baaa1688a8611e6fee4806
Glen Robertson <glenrob@chromium.org>
2022-04-05 06:50:45 +0000

    DGAPI: Re-enable DGAPI on Android excluding WebView.
    Was originally enabled in crrev.com/c/3439111
    Was disabled in crrev.com/c/3566398 due to a crash in WebView.
    
    Bug: 1312509,1312827
    Change-Id: I41a0ab864fc6e3fb9a7dc016e0f1774aa55685ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565637
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988858}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -546,6 +546,10 @@ void WebRuntimeFeatures::EnableAutomationControlled(bool enable) {
   RuntimeEnabledFeatures::SetAutomationControlledEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableDigitalGoodsAPI(bool enable) {
+  RuntimeEnabledFeatures::SetDigitalGoodsEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnableDisplayCutoutAPI(bool enable) {
   RuntimeEnabledFeatures::SetDisplayCutoutAPIEnabled(enable);
 }
