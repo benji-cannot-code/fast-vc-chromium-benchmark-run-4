d873bd00dd4dd04d8654c9cc4e4626119916bd4d
Sirisha Kavuluru <skavuluru@google.com>
2022-04-05 18:04:40 +0000

    [Tablet GTS] Fixing issue with thumbnail image not stretching to boundaries
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/3553397. Forcing MATCH_HEIGHT for the bitmap ImageView to match the height of the grid tab view.
    
    Screenshots with fix:
    * http://shortn/_mh2888ai4g
    * http://shortn/_d93HSZsyG9
    * http://shortn/_rhGvCqWSbH
    
    Bug: 1312397
    Change-Id: I0079a861faebdcd70b3991685a1aa3c1c1f88e01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569133
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#989061}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
@@ -15,6 +15,7 @@ import android.graphics.drawable.InsetDrawable;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
@@ -145,6 +146,9 @@ class TabGridViewBinder {
             view.setMinimumHeight(model.get(TabProperties.GRID_CARD_HEIGHT));
             view.getLayoutParams().height = model.get(TabProperties.GRID_CARD_HEIGHT);
             view.setLayoutParams(view.getLayoutParams());
+            TabGridThumbnailView thumbnail =
+                    (TabGridThumbnailView) view.fastFindViewById(R.id.tab_thumbnail);
+            thumbnail.getLayoutParams().height = LayoutParams.MATCH_PARENT;
         } else if (TabProperties.GRID_CARD_WIDTH == propertyKey) {
             view.setMinimumWidth(model.get(TabProperties.GRID_CARD_WIDTH));
             view.getLayoutParams().width = model.get(TabProperties.GRID_CARD_WIDTH);
@@ -344,6 +348,7 @@ class TabGridViewBinder {
             thumbnail.setScaleType(ScaleType.FIT_CENTER);
             thumbnail.setAdjustViewBounds(true);
         }
+
         if (fetcher == null) {
             thumbnail.setImageDrawable(null);
             return;
