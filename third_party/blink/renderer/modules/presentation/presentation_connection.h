984d9af37b7b8f059adb00ffcf6205e5bf75c1a3
David Sanders <dsanders11@ucsbalum.com>
2022-04-05 12:58:27 +0000

    Reland "Remove unused include of script_promise_resolver.h"
    This is a reland of d737e749b34abb22cdc3f61da2bb1501d0eac880
    
    The original CL collided with another CL and the build was
    broken since a transitive include was removed. That issue
    has since been fixed, and missing includes were added to
    files in others CLs, so this can land again.
    
    Original change's description:
    > Remove unused include of script_promise_resolver.h
    >
    > Also adds includes and forward declarations to other files which
    > were getting them transitively, to fix the build.
    >
    > Bug: 242216
    > Change-Id: Ib32bb862746aa1c22808af1b633af69c58bcc1e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487386
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#974563}
    
    Bug: 242216
    Change-Id: I93ff493fc43368bb3d9eb9ad51a6133076d91711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487337
    Auto-Submit: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988936}

--- third_party/blink/renderer/modules/presentation/presentation_connection.h
+++ third_party/blink/renderer/modules/presentation/presentation_connection.h
@@ -31,6 +31,7 @@ enum class FileErrorCode;
 class PresentationController;
 class PresentationReceiver;
 class PresentationRequest;
+class ScriptPromiseResolver;
 class WebString;
 
 class MODULES_EXPORT PresentationConnection
--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_graphics_context_3d_provider.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_webgl_context_attributes.h"
 #include "third_party/blink/renderer/core/core_export.h"
