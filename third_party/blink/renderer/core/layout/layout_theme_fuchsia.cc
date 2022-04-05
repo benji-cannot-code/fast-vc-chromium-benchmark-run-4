0c0fe49d0d256683c34d022fe690a7560ceab6e9
David Sanders <dsanders11@ucsbalum.com>
2022-04-05 15:35:16 +0000

    Remove unused include in layout_theme.h
    Bug: 242216
    Change-Id: I8eb31c6fb514f31545395ed9c067ef2ac49a8db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571165
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#988982}

--- third_party/blink/renderer/core/layout/layout_theme_fuchsia.cc
+++ third_party/blink/renderer/core/layout/layout_theme_fuchsia.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/layout_theme_default.h"
 
+#include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
+
 namespace blink {
 namespace {
 
