e3c2d208d83f44617afecc67f38c3acbdc1ddfb9
Rachel Wong <wrong@chromium.org>
2022-04-05 02:37:40 +0000

    [launcher] Special-case the screenshot shortcut result.
    UX decision is to override the provided shortcut to add Ctrl + Shift +
    Overview as an alternative option. See crbug for more details.
    
    Bug: 1292703
    Change-Id: I111094904213d70c110718675f9728a188ce9e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568928
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988815}

--- chrome/browser/ui/app_list/search/keyboard_shortcut_result.cc
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_result.cc
@@ -146,7 +146,6 @@ TextVector KeyboardShortcutResult::CreateTextVectorFromTemplateString(
   return text_vector;
 }
 
-// TODO(crbug.com/1290682): Complete implementation.
 KeyboardShortcutResult::KeyboardShortcutResult(Profile* profile,
                                                const KeyboardShortcutData& data,
                                                double relevance)
@@ -292,8 +291,6 @@ double KeyboardShortcutResult::CalculateRelevance(
 }
 
 void KeyboardShortcutResult::UpdateIcon() {
-  // TODO(crbug.com/1290682): Set the icon to a larger size when a
-  // KeyboardShortcutResult is promoted to an answer card.
   gfx::ImageSkia icon =
       gfx::CreateVectorIcon(chromeos::kKeyboardShortcutsIcon,
                             GetAppIconDimension(), SK_ColorTRANSPARENT);
--- chrome/browser/ui/app_list/search/keyboard_shortcut_result.h
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_result.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace app_list {
 
-// TODO(crbug.com/1290682): Finish implementation.
 class KeyboardShortcutResult : public ChromeSearchResult {
  public:
   explicit KeyboardShortcutResult(Profile* profile,
