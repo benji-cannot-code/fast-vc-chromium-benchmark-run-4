b4d036f186e3c6cf9f716c011a2802d33e952812
David Sanders <dsanders11@ucsbalum.com>
2022-04-05 18:27:55 +0000

    Do IWYU for //net/log/net_log_entry
    Bug: 242216
    Change-Id: I1f6f7a8120d24b5b1fbd8e2a3d47bdcbfd8b1b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564764
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#989077}

--- net/log/net_log_entry.h
+++ net/log/net_log_entry.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_event_type.h"
 #include "net/log/net_log_source.h"
 
-namespace base {
-class Value;
-}
-
 namespace net {
 
 // Represents an event that was sent to a NetLog observer, including the
