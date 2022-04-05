b26eac1b365b0a9c99b265d962c4ed480e46bebe
David Sanders <dsanders11@ucsbalum.com>
2022-04-05 01:26:25 +0000

    Remove unused include in inspector_trace_events.h
    Bug: 242216
    Change-Id: Ib6e3e1b6906942de0b5a3b9bf11bda2f9457909c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566600
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#988793}

--- third_party/blink/renderer/core/inspector/inspector_trace_events.h
+++ third_party/blink/renderer/core/inspector/inspector_trace_events.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/trace_event/trace_event.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_streamer.h"
 #include "third_party/blink/renderer/core/animation/compositor_animations.h"
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_selector.h"
 #include "third_party/blink/renderer/core/loader/frame_loader_types.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_load_priority.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
