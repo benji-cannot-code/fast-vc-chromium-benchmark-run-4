c58786ecb38d686137242a1be85b5681ca234a1e
Hidehiko Abe <hidehiko@chromium.org>
2022-04-05 16:24:06 +0000

    LacrosDataMigrator: Update the delay to show skip button.
    Currently, Skip button is shown if it takes more than 20 seconds since
    ShowImpl().
    Update the timeout to 10 seconds per design.
    Also, update the timer to start on the UI is displayed to the user.
    Note that ShowImpl() and actual UI visibility looks like to have some
    gap.
    
    BUG=1296174
    TEST=Ran on DUT.
    
    Change-Id: I368a35b057b2ebb82ae48def9354ccc964d86a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3567852
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988994}

--- chrome/browser/ui/webui/chromeos/login/lacros_data_migration_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/lacros_data_migration_screen_handler.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_LACROS_DATA_MIGRATION_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_LACROS_DATA_MIGRATION_SCREEN_HANDLER_H_
 
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
+#include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash {
@@ -50,7 +52,8 @@ class LacrosDataMigrationScreenView {
 };
 
 class LacrosDataMigrationScreenHandler : public BaseScreenHandler,
-                                         public LacrosDataMigrationScreenView {
+                                         public LacrosDataMigrationScreenView,
+                                         public OobeUI::Observer {
  public:
   using TView = LacrosDataMigrationScreenView;
 
@@ -75,6 +78,11 @@ class LacrosDataMigrationScreenHandler : public BaseScreenHandler,
   void SetFailureStatus(const absl::optional<uint64_t>& required_size,
                         bool show_goto_files) override;
 
+  // OobeUI::Observer:
+  void OnCurrentScreenChanged(OobeScreenId current_screen,
+                              OobeScreenId new_screen) override;
+  void OnDestroyingOobeUI() override;
+
  private:
   // BaseScreenHandler:
   void InitializeDeprecated() override;
@@ -83,6 +91,8 @@ class LacrosDataMigrationScreenHandler : public BaseScreenHandler,
 
   // Whether the screen should be shown right after initialization.
   bool show_on_init_ = false;
+
+  base::ScopedObservation<OobeUI, OobeUI::Observer> observation_{this};
 };
 
 }  // namespace chromeos
