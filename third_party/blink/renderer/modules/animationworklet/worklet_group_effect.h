6fd818e61a9d6db6077dbcc613123a7110501358
David Sanders <dsanders11@ucsbalum.com>
2022-04-05 16:47:16 +0000

    Remove unused include in timing.h
    Also adds some includes to other files which were
    getting them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I8a192d4c70a8dd4616b0acda43913cb093080fc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563848
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#989014}

--- third_party/blink/renderer/modules/animationworklet/worklet_group_effect.h
+++ third_party/blink/renderer/modules/animationworklet/worklet_group_effect.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/animationworklet/worklet_animation_effect_timings.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 
 namespace blink {
 
