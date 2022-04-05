2e6a826a646d8dc149ae6998c7bb6619e5553a35
Marijn Kruisselbrink <mek@chromium.org>
2022-04-05 07:00:37 +0000

    Close a MessagePort if it is created in a destroyed context.
    MessagePort assumes it is only destroyed either after ContextDestroyed,
    or after the port has been closed explicitly. As it turns out ports that
    were created in an already detached iframe would violate this invariant,
    causing issues.
    
    Bug: 1228661
    Change-Id: Ib1abce15f1d1d15f044de19fe0534767db488af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561845
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988859}

--- third_party/blink/renderer/core/messaging/message_port.cc
+++ third_party/blink/renderer/core/messaging/message_port.cc
@@ -57,7 +57,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 MessagePort::MessagePort(ExecutionContext& execution_context)
-    : ExecutionContextLifecycleObserver(&execution_context),
+    : ExecutionContextLifecycleObserver(execution_context.IsContextDestroyed()
+                                            ? nullptr
+                                            : &execution_context),
+      // Ports in a destroyed context start out in a closed state.
+      closed_(execution_context.IsContextDestroyed()),
       task_runner_(execution_context.GetTaskRunner(TaskType::kPostedMessage)) {}
 
 MessagePort::~MessagePort() {
@@ -169,10 +173,21 @@ void MessagePort::Entangle(MessagePortDescriptor port) {
   DCHECK(port.IsValid());
   DCHECK(!connector_);
 
+  // If the context was already destroyed, there is no reason to actually
+  // entangle the port and create a Connector. No messages will ever be able to
+  // be sent or received anyway, as StartReceiving will never be called.
+  if (!GetExecutionContext())
+    return;
+
   port_ = std::move(port);
   connector_ = std::make_unique<mojo::Connector>(
       port_.TakeHandleToEntangle(GetExecutionContext()),
       mojo::Connector::SINGLE_THREADED_SEND);
+  // The raw `this` is safe despite `this` being a garbage collected object
+  // because we make sure that:
+  // 1. This object will not be garbage collected while it is connected and
+  //    the execution context is not destroyed, and
+  // 2. when the execution context is destroyed, the connector_ is reset.
   connector_->set_incoming_receiver(this);
   connector_->set_connection_error_handler(
       WTF::Bind(&MessagePort::close, WrapWeakPersistent(this)));
