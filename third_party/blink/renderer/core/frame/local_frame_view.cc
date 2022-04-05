b3f626ee9f493751153c2b5743cfed34706395c0
Koji Ishii <kojii@chromium.org>
2022-04-05 23:45:05 +0000

    [fencedframes] Move coordinate system conversions to |LayoutEmbeddedContent|
    This patch moves |LayoutEmbeddedContent|-specific coordinate
    system conversions in |LocalFrameView| to
    |LayoutEmbeddedContent|, so that it can add more logic other
    than `Border + Padding`.
    
    This patch is a refactoring with no behavior changes, to make
    changes for fenced frames in following patches easier.
    
    Bug: 1270648
    Change-Id: I110224ef04f333308a49e5d2341ce76ef49518a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571361
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989198}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -3466,11 +3466,8 @@ gfx::Rect LocalFrameView::ConvertToContainingEmbeddedContentView(
     if (!layout_object)
       return local_rect;
 
-    gfx::Rect rect(local_rect);
-    // Add borders and padding
-    rect.Offset(
-        (layout_object->BorderLeft() + layout_object->PaddingLeft()).ToInt(),
-        (layout_object->BorderTop() + layout_object->PaddingTop()).ToInt());
+    // Add borders and padding etc.
+    gfx::Rect rect = layout_object->BorderBoxFromEmbeddedContent(local_rect);
     return ToPixelSnappedRect(
         layout_object->LocalToAbsoluteRect(PhysicalRect(rect)));
   }
@@ -3496,11 +3493,8 @@ PhysicalOffset LocalFrameView::ConvertToContainingEmbeddedContentView(
       return local_offset;
 
     PhysicalOffset point(local_offset);
-
-    // Add borders and padding
-    point += PhysicalOffset(
-        layout_object->BorderLeft() + layout_object->PaddingLeft(),
-        layout_object->BorderTop() + layout_object->PaddingTop());
+    // Add borders and padding etc.
+    point = layout_object->BorderBoxFromEmbeddedContent(point);
     return layout_object->LocalToAbsolutePoint(point);
   }
 
@@ -3515,11 +3509,9 @@ gfx::PointF LocalFrameView::ConvertToContainingEmbeddedContentView(
       return local_point;
 
     PhysicalOffset point = PhysicalOffset::FromPointFRound(local_point);
-
-    // Add borders and padding
-    point.left += layout_object->BorderLeft() + layout_object->PaddingLeft();
-    point.top += layout_object->BorderTop() + layout_object->PaddingTop();
-    return gfx::PointF(layout_object->LocalToAbsolutePoint(point));
+    // Add borders and padding etc.
+    point = layout_object->BorderBoxFromEmbeddedContent(point);
+    return static_cast<gfx::PointF>(layout_object->LocalToAbsolutePoint(point));
   }
 
   return local_point;
@@ -3540,10 +3532,8 @@ gfx::PointF LocalFrameView::ConvertFromContainingEmbeddedContentView(
       return parent_point;
 
     gfx::PointF point = layout_object->AbsoluteToLocalPoint(parent_point);
-    // Subtract borders and padding
-    point.Offset(
-        (-layout_object->BorderLeft() - layout_object->PaddingLeft()).ToFloat(),
-        (-layout_object->BorderTop() - layout_object->PaddingTop()).ToFloat());
+    // Subtract borders and padding etc.
+    point = layout_object->EmbeddedContentFromBorderBox(point);
     return point;
   }
 
