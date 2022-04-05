c0e60a5937054463af0f883b186b958b99ce47e9
Brian Salomon <bsalomon@google.com>
2022-04-05 20:10:06 +0000

    Remove use of GrContextOptions::fSharpenMipmappedTextures.
    This option is now a no-op. The behavior it used to enable is now
    always enabled.
    
    Bug: skia:13078
    Bug: 1158446
    
    Change-Id: Id5d3333b8de809e77ba4b32e56fa6e578c91f6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572501
    Auto-Submit: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989127}

--- gpu/command_buffer/service/skia_utils.cc
+++ gpu/command_buffer/service/skia_utils.cc
@@ -98,8 +98,6 @@ GrContextOptions GetDefaultGrContextOptions(GrContextType type) {
       base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableMipmapGeneration);
 
-  options.fSharpenMipmappedTextures = true;
-
   // fSupportBilerpFromGlyphAtlas is needed for Raw Draw.
   options.fSupportBilerpFromGlyphAtlas = features::IsUsingRawDraw();
 
