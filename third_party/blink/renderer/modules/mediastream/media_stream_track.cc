76245300a17622339fd2355112ce875b0c9cd254
Tony Herre <toprice@chromium.org>
2022-04-05 15:39:53 +0000

    Add basic shell of TransferredMediaStreamTrack
    Add a subclass of MediaStreamTrack which can be created synchronously
    when receiving a transfer, but when a full MediaStreamTrack instance has
    been asynchronously set up, will proxy all JS calls to it.
    
    A refactoring to make MediaStreamTrack just an interface which both
    TransferredMediaStreamTrack and the current Impl directly subclass is
    being working on in crrev.com/c/3570541
    
    Change-Id: Ib676713d003475e32d1a6e819d7bc92bd66c3aaf
    Bug: 1288839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568372
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988983}

--- third_party/blink/renderer/modules/mediastream/media_stream_track.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/media_stream_video_track.h"
 #include "third_party/blink/renderer/modules/mediastream/overconstrained_error.h"
 #include "third_party/blink/renderer/modules/mediastream/processed_local_audio_source.h"
+#include "third_party/blink/renderer/modules/mediastream/transferred_media_stream_track.h"
 #include "third_party/blink/renderer/modules/mediastream/user_media_controller.h"
 #include "third_party/blink/renderer/modules/mediastream/webaudio_media_stream_audio_sink.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
@@ -273,12 +274,7 @@ MediaStreamTrack* MediaStreamTrack::Create(ExecutionContext* context,
 MediaStreamTrack* MediaStreamTrack::Create(
     ExecutionContext* context,
     const base::UnguessableToken& token) {
-  MediaStreamSource* source = MakeGarbageCollected<MediaStreamSource>(
-      "dummy", MediaStreamSource::StreamType::kTypeVideo, "dummy",
-      false /* remote */);
-  MediaStreamComponent* component =
-      MakeGarbageCollected<MediaStreamComponent>(source);
-  return MakeGarbageCollected<MediaStreamTrack>(context, component);
+  return MakeGarbageCollected<TransferredMediaStreamTrack>(context);
 }
 
 MediaStreamTrack::MediaStreamTrack(ExecutionContext* context,
