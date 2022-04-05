5f62be934f0c59442d3b383bcfe5580e494b8041
Morten Stenshorne <mstensho@chromium.org>
2022-04-05 18:45:14 +0000

    Do less work and cause less harm in legacy multicol writeback.
    Avoid LayoutMultiColumnSet::ResetColumnHeight() from the NG side. Use
    the new function ResetColumnHeightFromNG() instead. This is a copy of
    ResetColumnHeight(), but one that only does what we need when laid out
    by NG. On that note, I was also going to set initial_height_calculated_
    to false here, but then I realized that we don't need to touch this one
    at all (at least not anymore).  LayoutMultiColumnSet::
    FinishLayoutFromNG() also used to this one to *false*, which was
    confusing. Now removed.
    
    This also fixes two virtual/layout_ng_printing/ tests, which also hit
    the DCHECK in MultiColumnFragmentainerGroup::SetColumnBlockSizeFromNG().
    
    Bug: 1308080
    Change-Id: I5a4b2dfe9ad73e42afb2935cc249ea91e4f940c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569752
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989085}

--- third_party/blink/renderer/core/layout/layout_multi_column_set.cc
+++ third_party/blink/renderer/core/layout/layout_multi_column_set.cc
@@ -429,6 +429,12 @@ void LayoutMultiColumnSet::ResetColumnHeight() {
   initial_height_calculated_ = false;
 }
 
+void LayoutMultiColumnSet::ResetColumnHeightFromNG() {
+  NOT_DESTROYED();
+  fragmentainer_groups_.DeleteExtraGroups();
+  fragmentainer_groups_.First().ResetColumnHeightFromNG();
+}
+
 void LayoutMultiColumnSet::BeginFlow(LayoutUnit offset_in_flow_thread) {
   NOT_DESTROYED();
   // At this point layout is exactly at the beginning of this set. Store block
@@ -737,7 +743,6 @@ void LayoutMultiColumnSet::FinishLayoutFromNG() {
                        /* logical_top */ LayoutUnit(), computed_values);
   SetLogicalHeight(computed_values.extent_);
   ComputeLayoutOverflow(computed_values.extent_);
-  initial_height_calculated_ = false;
 }
 
 }  // namespace blink
