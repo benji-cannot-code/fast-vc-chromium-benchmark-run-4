344fa892c534b5fd0684c9cd8327ec70e4d2ffa8
Side Yilmaz <sideyilmaz@chromium.org>
2022-04-05 12:26:27 +0000

    Clean up UpdateHistoryEntryPointsInIncognito flags in iOS.
    This CL cleans up the old UpdateHistoryEntryPointsInIncognito feature
    flag in iOS.
    
    Bug: 1308395
    Change-Id: I0b255d843b8eee938682e7a477762a2d21f5a160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568569
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988925}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -55,9 +55,6 @@ const base::Feature kIOSLocationBarUseNativeContextMenu{
 const base::Feature kSearchHistoryLinkIOS{"SearchHistoryLinkIOS",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kUpdateHistoryEntryPointsInIncognito{
-    "UpdateHistoryEntryPointsInIncognito", base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kUseLensToSearchForImage{"UseLensToSearchForImage",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
