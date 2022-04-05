10ca387d12e11f844466ab936e7e885a7d86dc5f
Xiang Ji <jxiang@google.com>
2022-04-05 20:45:24 +0000

    Enable Web RTC UDP Port Range Policy browser test on Android
    Validation: Test passed locally
    
    Bug: 1310084
    Change-Id: I83b63252fd1f1be014541c2432fda2d4d5eb17cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570668
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#989147}

--- chrome/browser/policy/test/web_rtc_udp_port_range_policy_browsertest.cc
+++ chrome/browser/policy/test/web_rtc_udp_port_range_policy_browsertest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/test/base/chrome_test_utils.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/policy/core/common/policy_types.h"
@@ -53,7 +53,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcUdpPortRangeEnabledPolicyTest,
                        WebRtcUdpPortRangeEnabled) {
   std::string port_range;
   const PrefService::Preference* pref =
-      user_prefs::UserPrefs::Get(browser()->profile())
+      user_prefs::UserPrefs::Get(chrome_test_utils::GetProfile(this))
           ->FindPreference(prefs::kWebRTCUDPPortRange);
   if (pref->GetValue()->is_string())
     port_range = pref->GetValue()->GetString();
@@ -64,7 +64,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcUdpPortRangeDisabledPolicyTest,
                        WebRtcUdpPortRangeDisabled) {
   std::string port_range;
   const PrefService::Preference* pref =
-      user_prefs::UserPrefs::Get(browser()->profile())
+      user_prefs::UserPrefs::Get(chrome_test_utils::GetProfile(this))
           ->FindPreference(prefs::kWebRTCUDPPortRange);
   if (pref->GetValue()->is_string())
     port_range = pref->GetValue()->GetString();
