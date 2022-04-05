06ff18a3f24960a0765d33ac68f54dc5ee2de1d1
Ben Joyce <bjoyce@google.com>
2022-04-05 21:02:58 +0000

    Convert tests to use BaseRobolectricTestRunner
    The base testrunner is preferred as it does some app sets up.
    
    This CL modifies SmsProviderGmsTest to remove its local setup as the
    baserunner does that.
    
    Bug: 1273881
    Change-Id: I46096fa9cbfb419d145a659563534c6a5a769962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573044
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989154}

--- content/public/android/junit/src/org/chromium/content/browser/sms/SmsProviderGmsTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/sms/SmsProviderGmsTest.java
@@ -13,16 +13,14 @@ import static org.mockito.Mockito.when;
 
 import android.content.Context;
 
-import androidx.test.core.app.ApplicationProvider;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
-import org.chromium.base.ContextUtils;
-import org.chromium.testing.local.LocalRobolectricTestRunner;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.ui.base.WindowAndroid;
 
 import java.lang.ref.WeakReference;
@@ -30,7 +28,7 @@ import java.lang.ref.WeakReference;
 /**
  * Unit tests for SmsProviderGms.
  */
-@RunWith(LocalRobolectricTestRunner.class)
+@RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class SmsProviderGmsTest {
     private Context mContext;
@@ -41,8 +39,7 @@ public class SmsProviderGmsTest {
 
     @Before
     public void setUp() {
-        mContext = ApplicationProvider.getApplicationContext();
-        ContextUtils.initApplicationContextForTests(mContext);
+        mContext = RuntimeEnvironment.application;
 
         mWindowAndroid = Mockito.mock(WindowAndroid.class);
         when(mWindowAndroid.getContext()).thenReturn(new WeakReference<Context>(mContext));
