8446c25876b1670e64041a221e0f7256f4daa7f9
Friedrich Horschig <fhorschig@chromium.org>
2022-04-05 11:18:11 +0000

    [Android][UPM] Forward Init callbacks from relevant backend
    Before this change, only the built-in backend triggered the callbacks
    passed to Init. Instead, the active backend should handle the callbacks.
    
    Since the active backend changes based on the sync status, the proxy
    will receive the signal of both backends.
    For suspected remote changes, the proxy only invoke the callback if the
    signal originated from the current main backend.
    For sync changes, all signals currently originate from the built-in
    backend. While there are no local passwords, this signal therefore can
    always be forwarded.
    
    Bug: 1279341
    Change-Id: Iea362aa50977587d782d97d6e48d6c6f70b7729a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3560442
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988906}

--- components/password_manager/core/browser/password_store_proxy_backend.h
+++ components/password_manager/core/browser/password_store_proxy_backend.h
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/types/strong_alias.h"
 #include "components/password_manager/core/browser/password_store_backend.h"
+#include "components/password_manager/core/browser/password_store_change.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefService;
 
@@ -36,6 +39,9 @@ class PasswordStoreProxyBackend : public PasswordStoreBackend {
   ~PasswordStoreProxyBackend() override;
 
  private:
+  using CallbackOriginatesFromAndroidBackend =
+      base::StrongAlias<struct CallbackOriginatesFromAndroidBackendTag, bool>;
+
   // Implements PasswordStoreBackend interface.
   void InitBackend(RemoteChangesReceived remote_form_changes_received,
                    base::RepeatingClosure sync_enabled_or_disabled_cb,
@@ -73,6 +79,18 @@ class PasswordStoreProxyBackend : public PasswordStoreBackend {
   void ClearAllLocalPasswords() override;
   void OnSyncServiceInitialized(syncer::SyncService* sync_service) override;
 
+  // Forwards the (possible) forms changes caused by a remote event to the
+  // main backend.
+  void OnRemoteFormChangesReceived(
+      CallbackOriginatesFromAndroidBackend originatesFromAndroid,
+      RemoteChangesReceived remote_form_changes_received,
+      absl::optional<PasswordStoreChangeList> changes);
+
+  // Forwards sync status changes by the backend facilitating them.
+  void OnSyncEnabledOrDisabled(
+      CallbackOriginatesFromAndroidBackend originatesFromAndroid,
+      base::RepeatingClosure sync_enabled_or_disabled_cb);
+
   PasswordStoreBackend* main_backend();
   PasswordStoreBackend* shadow_backend();
 
@@ -80,6 +98,8 @@ class PasswordStoreProxyBackend : public PasswordStoreBackend {
   const raw_ptr<PasswordStoreBackend> android_backend_;
   raw_ptr<PrefService> const prefs_ = nullptr;
   const raw_ptr<SyncDelegate> sync_delegate_;
+
+  base::WeakPtrFactory<PasswordStoreProxyBackend> weak_ptr_factory_{this};
 };
 
 }  // namespace password_manager
