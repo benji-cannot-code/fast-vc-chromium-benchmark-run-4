0ad88eda6f491713552a468cf9a83ce00c3732c8
Denis Kuznetsov <antrim@chromium.org>
2022-04-05 19:25:46 +0000

    cryptohome: wire PrepareVaultForMigration call
    Bug: 1310312
    Change-Id: Ic702c650304b868bbaaec7fb838f68056a95672c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568451
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989103}

--- chromeos/dbus/userdataauth/userdataauth_client.h
+++ chromeos/dbus/userdataauth/userdataauth_client.h
@@ -81,6 +81,8 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) UserDataAuthClient {
       DBusMethodCallback<::user_data_auth::CreatePersistentUserReply>;
   using PreparePersistentVaultCallback =
       DBusMethodCallback<::user_data_auth::PreparePersistentVaultReply>;
+  using PrepareVaultForMigrationCallback =
+      DBusMethodCallback<::user_data_auth::PrepareVaultForMigrationReply>;
   using InvalidateAuthSessionCallback =
       DBusMethodCallback<::user_data_auth::InvalidateAuthSessionReply>;
   using ExtendAuthSessionCallback =
@@ -244,6 +246,11 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) UserDataAuthClient {
   virtual void PreparePersistentVault(
       const ::user_data_auth::PreparePersistentVaultRequest& request,
       PreparePersistentVaultCallback callback) = 0;
+
+  // Makes user directory available for migration.
+  virtual void PrepareVaultForMigration(
+      const ::user_data_auth::PrepareVaultForMigrationRequest& request,
+      PrepareVaultForMigrationCallback callback) = 0;
 
   // This call is used to invalidate an AuthSession
   // once the need for one no longer exists.
