979a2cba214a48ed1b755c7c6f613c1886959224
Jacob Stanley <jacobstanley@google.com>
2022-04-05 22:01:04 +0000

    Added UMA and UKM logging after preconnected on pointerdown events with anchor elements.
    UKM Approval: https://docs.google.com/document/d/18fo8Z09aoK9l60T3Hz9WavlE_Bjva23cYDchqLZeBZ4/edit#heading=h.k5jx6iluw4yt
    
    Design Doc: https://docs.google.com/document/d/18fo8Z09aoK9l60T3Hz9WavlE_Bjva23cYDchqLZeBZ4/edit#
    
    Bug: 1297312
    Change-Id: I2da102b0dd3ead690408ec4d8787beea879b40f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3559171
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#989172}

--- chrome/browser/navigation_predictor/anchor_element_preloader.h
+++ chrome/browser/navigation_predictor/anchor_element_preloader.h
@@ -9,6 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/document_service.h"
 #include "third_party/blink/public/mojom/loader/anchor_element_interaction_host.mojom.h"
 
+extern const char kPreloadingAnchorElementPreloaderPreloadingTriggered[];
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class AnchorElementPreloaderType {
+  kUnspecified = 0,
+  kPreconnect = 1,
+  kMaxValue = kPreconnect,
+};
+
 class AnchorElementPreloader
     : content::DocumentService<blink::mojom::AnchorElementInteractionHost> {
  public:
@@ -22,8 +32,13 @@ class AnchorElementPreloader
       content::RenderFrameHost* render_frame_host,
       mojo::PendingReceiver<blink::mojom::AnchorElementInteractionHost>
           receiver);
+
   // Preconnects to the given URL `target`.
   void OnPointerDown(const GURL& target) override;
+
+  void RecordUmaPreloadedTriggered(AnchorElementPreloaderType);
+
+  void RecordUkmPreloadType(AnchorElementPreloaderType);
 };
 
 #endif  // CHROME_BROWSER_NAVIGATION_PREDICTOR_ANCHOR_ELEMENT_PRELOADER_H_
