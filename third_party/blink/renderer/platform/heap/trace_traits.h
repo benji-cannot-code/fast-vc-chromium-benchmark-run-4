5b9b5010fcd1701db520eb116178db26878a373f
Michael Lippautz <mlippautz@chromium.org>
2022-04-05 12:13:06 +0000

    heap: Fix TraceTrait for std::pair
    The trait was returning a value for the base_object_payload which must
    point to a GarbageCollected object. This is benign as the method
    should have never been used because std::pair is eagerly traced in its
    only usage site (HeapVector).
    
    Change-Id: I425acdd3a04fd4cc7e7f9f78e3aa8036834432dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568574
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988922}

--- third_party/blink/renderer/platform/heap/trace_traits.h
+++ third_party/blink/renderer/platform/heap/trace_traits.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <tuple>
 
+#include "base/notreached.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
@@ -141,16 +142,20 @@ struct TraceInCollectionTrait<kWeakHandling, blink::WeakMember<T>, Traits> {
 
 namespace cppgc {
 
-// This trace trait for std::pair will null weak members if their referent is
+// This trace trait for std::pair will clear WeakMember if their referent is
 // collected. If you have a collection that contain weakness it does not remove
-// entries from the collection that contain nulled weak members.
+// entries from the collection that contain nulled WeakMember.
 template <typename T, typename U>
 struct TraceTrait<std::pair<T, U>> {
   STATIC_ONLY(TraceTrait);
 
  public:
   static TraceDescriptor GetTraceDescriptor(const void* self) {
-    return {self, Trace};
+    // The following code should never be reached as tracing through std::pair
+    // should always happen eagerly by directly invoking `Trace()` below. This
+    // happens e.g. when being used in HeapVector<std::pair<...>>.
+    NOTREACHED();
+    return {nullptr, Trace};
   }
 
   static void Trace(Visitor* visitor, const std::pair<T, U>* pair) {
