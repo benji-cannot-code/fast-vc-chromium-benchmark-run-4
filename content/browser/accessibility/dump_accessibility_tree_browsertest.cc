0a210926c15540cd48bf4779d33b5a92e872ad12
Alexander Surkov <asurkov@igalia.com>
2022-04-05 19:40:14 +0000

    ax_inspect: run #<skip> tests with no checks
    If a test has #<skip> directive in its expectation file, then run a test
    on a platform with no checks as a crash test.
    
    Disable a number of #<skip> tests which never completes and thus are not
    suitable for being a crash test. Such tests should have never used
    #<skip> directive in the first place, they should have been disabled.
    
    Change-Id: Id5719fc2a2cb1efc3665a90ba07afa097a083b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469775
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#989105}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2183,8 +2183,18 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityInputDate) {
   RunHtmlTest(FILE_PATH_LITERAL("input-date.html"));
 }
 
+// TODO: date and time controls drop their children, including the popup button,
+// on Android
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_AccessibilityInputDateWithPopupOpen \
+  DISABLED_AccessibilityInputDateWithPopupOpen
+#else
+#define MAYBE_AccessibilityInputDateWithPopupOpen \
+  AccessibilityInputDateWithPopupOpen
+#endif
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       AccessibilityInputDateWithPopupOpen) {
+                       MAYBE_AccessibilityInputDateWithPopupOpen) {
   RunHtmlTest(FILE_PATH_LITERAL("input-date-with-popup-open.html"));
 }
 
@@ -2204,7 +2214,9 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 
 // TODO(crbug.com/1201658): Flakes heavily on Linux.
-#if BUILDFLAG(IS_LINUX)
+// TODO: date and time controls drop their children, including the popup button,
+// on Android
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)
 #define MAYBE_AccessibilityInputTimeWithPopupOpen \
   DISABLED_AccessibilityInputTimeWithPopupOpen
 #else
