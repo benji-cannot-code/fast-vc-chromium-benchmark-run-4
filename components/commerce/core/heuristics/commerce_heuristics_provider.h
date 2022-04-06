bc85a66c3715cae3068668359dcea4845dc57215
Yue Zhang <yuezhanggg@chromium.org>
2022-04-06 06:32:04 +0000

    [ConsentV2] Upstream detection logic to component
    This CL upstreams detection logic for cart visit and checkout visit to
    commerce component so that it can be used in browser process.
    
    Bug: 1310422
    Change-Id: Idafa8128321086b05579592f75c9b274ed5979fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553827
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989312}

---
+++ components/commerce/core/heuristics/commerce_heuristics_provider.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_COMMERCE_CORE_HEURISTICS_COMMERCE_HEURISTICS_PROVIDER_H_
+#define COMPONENTS_COMMERCE_CORE_HEURISTICS_COMMERCE_HEURISTICS_PROVIDER_H_
+
+#include "url/gurl.h"
+
+namespace commerce_heuristics {
+
+// Check if a URL is a cart page URL.
+bool IsVisitCart(const GURL& url);
+// Check if a URL is a checkout page URL.
+bool IsVisitCheckout(const GURL& url);
+
+}  // namespace commerce_heuristics
+
+#endif  // COMPONENTS_COMMERCE_CORE_HEURISTICS_COMMERCE_HEURISTICS_PROVIDER_H_
