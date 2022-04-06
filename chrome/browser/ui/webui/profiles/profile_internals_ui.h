477bfc4de89512f2b89141ce7e605a0ef8080deb
Gabriel Oliveira <gabolvr@google.com>
2022-04-06 11:31:40 +0000

    Create chrome://profile-internals page
    Bug: 1313447
    Change-Id: I6b6f008b5433249df5c9147ba5b2d99a720f755c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568378
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#989373}

---
+++ chrome/browser/ui/webui/profiles/profile_internals_ui.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_PROFILES_PROFILE_INTERNALS_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_PROFILES_PROFILE_INTERNALS_UI_H_
+
+#include "content/public/browser/web_ui_controller.h"
+
+// Controller for chrome://profile-internals page.
+class ProfileInternalsUI : public content::WebUIController {
+ public:
+  explicit ProfileInternalsUI(content::WebUI* web_ui);
+
+  ProfileInternalsUI(const ProfileInternalsUI&) = delete;
+  ProfileInternalsUI& operator=(const ProfileInternalsUI&) = delete;
+
+  ~ProfileInternalsUI() override;
+};
+
+#endif  // CHROME_BROWSER_UI_WEBUI_PROFILES_PROFILE_INTERNALS_UI_H_
