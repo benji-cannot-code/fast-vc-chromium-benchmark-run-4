84051c6d861e1e9ecd896d37048c3cd44173cbf3
Renato Silva <rrsilva@google.com>
2022-04-06 22:44:35 +0000

    Assistant - Fix in-session WebUi
    Replace the top level document of Assistant's in-session
    WebUI with one that is properly flattened by flattenhtml.
    
    Fixed: 1308977
    Change-Id: I9dc149e2895380b87315828a62566060669a7d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571760
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989653}

--- chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_ui.cc
+++ chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_ui.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/assistant_optin_resources.h"
 #include "chrome/grit/assistant_optin_resources_map.h"
 #include "chrome/grit/browser_resources.h"
+#include "chrome/grit/oobe_conditional_resources.h"
 #include "chromeos/assistant/buildflags.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/services/assistant/public/cpp/features.h"
@@ -89,7 +90,8 @@ AssistantOptInUI::AssistantOptInUI(content::WebUI* web_ui)
   source->UseStringsJs();
   source->AddResourcePaths(
       base::make_span(kAssistantOptinResources, kAssistantOptinResourcesSize));
-  source->SetDefaultResource(IDR_ASSISTANT_OPTIN_ASSISTANT_OPTIN_HTML);
+  source->AddResourcePath("assistant_optin.js", IDR_ASSISTANT_OPTIN_JS);
+  source->SetDefaultResource(IDR_ASSISTANT_OPTIN_HTML);
   source->AddResourcePath("voice_match_animation.json",
                           IDR_ASSISTANT_VOICE_MATCH_ANIMATION);
   source->OverrideContentSecurityPolicy(
