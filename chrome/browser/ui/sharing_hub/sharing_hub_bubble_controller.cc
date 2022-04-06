e270670644f49f863630b4d5e8187ac8e5c2447c
Elly Fong-Jones <ellyjones@chromium.org>
2022-04-06 20:35:39 +0000

    sharing: implement desktop share preview stub
    This change adds the preview surface to the desktop sharing hub, gated
    by the DesktopSharePreview field trial (or UpcomingSharingFeatures, as
    usual). Specifically, this change:
    
    1. Adds methods to SharingHubBubbleController to get the needed
       information about the pending share - these are currently stubs
       that operate on the active WebContents only, for test purposes
       (see https://crbug.com/1312524)
    2. Adds a new class PreviewView which displays that information
    3. Adds code to SharingHubBubbleViewImpl to construct a PreviewView and
       insert it into its layout when the feature is enabled
    
    A couple of design things:
    * PreviewView is fully decoupled from both SharingHubBubbleController
      and SharingHubBubbleViewImpl. SharingHubBubbleViewImpl is responsible
      for registering PreviewView for image update callbacks, so PreviewView
      has the ability to hold a provided CallbackListSubscription, but it is
      unaware of the other two involved classes. This should make it quite
      easy to unit test.
    * I considered creating a wrapper type to hold the share preview state,
      but ultimately decided against it; it led to a design where
      PreviewView (appeared to) allow updates of all parts of its visuals,
      not just the image, which is more flexibility than I wanted to add.
    
    Next steps:
    * Unit tests for PreviewView, once it has more logic in it
    * Wiring SharingHubBubbleController to the actual omnibox share state
    * Metrics gathering
    * Working with UX to reconcile the differences from the mock redlines
    
    Fixed: 1312516
    Change-Id: If55a23cfee64dbaa156d6a257d3f0da1a37bcc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571755
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989609}

--- chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.cc
+++ chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/favicon/favicon_utils.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/share/share_features.h"
 #include "chrome/browser/share/share_metrics.h"
 #include "chrome/browser/sharing_hub/sharing_hub_features.h"
 #include "chrome/browser/sharing_hub/sharing_hub_model.h"
@@ -167,6 +169,33 @@ SharingHubBubbleController::GetThirdPartyActions() {
     model->GetThirdPartyActionList(&actions);
 
   return actions;
+}
+
+bool SharingHubBubbleController::ShouldUsePreview() {
+  return share::GetDesktopSharePreviewVariant() !=
+         share::DesktopSharePreviewVariant::kDisabled;
+}
+
+std::u16string SharingHubBubbleController::GetPreviewTitle() {
+  // TODO(https://crbug.com/1312524): get passed this state from the omnibox
+  // instead.
+  return GetWebContents().GetTitle();
+}
+
+GURL SharingHubBubbleController::GetPreviewUrl() {
+  // TODO(https://crbug.com/1312524): get passed this state from the omnibox
+  // instead.
+  return GetWebContents().GetVisibleURL();
+}
+
+ui::ImageModel SharingHubBubbleController::GetPreviewImage() {
+  return ui::ImageModel::FromImage(favicon::GetDefaultFavicon());
+}
+
+base::CallbackListSubscription
+SharingHubBubbleController::RegisterPreviewImageChangedCallback(
+    PreviewImageChangedCallback callback) {
+  return preview_image_changed_callbacks_.Add(callback);
 }
 
 void SharingHubBubbleController::OnActionSelected(
