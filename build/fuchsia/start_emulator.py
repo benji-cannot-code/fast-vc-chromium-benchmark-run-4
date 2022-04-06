09082f7c0c32f4975b6d0ad4c457ca31887adcee
Chong Gu <chonggu@google.com>
2022-04-06 16:38:49 +0000

    [Fuchsia] Remove deprecated target classes.
    DeviceTarget supports specifying an image now, so there is no need to
    keep generic_x64_target.py.
    AemuTarget has been replaced by FvdlTarget.
    
    Change-Id: I0484225ef51eee52a6088138dd648801d0440f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572990
    Auto-Submit: Chong Gu <chonggu@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989466}

--- build/fuchsia/start_emulator.py
+++ build/fuchsia/start_emulator.py
@@ -13,7 +13,6 @@ import os
 import time
 import subprocess
 
-from aemu_target import AemuTarget
 from exit_on_sig_term import ExitOnSigTerm
 from fvdl_target import FvdlTarget
 
@@ -24,7 +23,6 @@ def main():
       'be re-used for multiple test runs.')
   AddLongRunningArgs(parser)
   FvdlTarget.RegisterArgs(parser)
-  AemuTarget.RegisterArgs(parser)
   common_args.AddCommonArgs(parser)
   args = parser.parse_args()
   args.out_dir = None
