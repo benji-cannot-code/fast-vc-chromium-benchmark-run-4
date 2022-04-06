1b56417da0abd07774427aab78d4778d041235e2
Denis Kuznetsov <antrim@chromium.org>
2022-04-06 18:02:51 +0000

    cryptohome modernization: API wrappers by operation
    This CL adds 3 classes that wrap interaction with UserDataAuthClient.
    Methods are groupped by their pupose: AuthPerformer contains methods
    related to manipulations with AuthSession state, MountPerformer
    contains methods that are related to operations with user home
    directories, and AuthFactorEditor contains operations that modify
    user's auth factors.
    
    Most operations share same signature, and RunOperationChain method
    can be used to run a chain of operations, breaking on any error.
    
    Errors are wrapped in CryptohomeError class, so that any future
    changes in error handling would not affect API signatures.
    
    Bug: 1310312
    Change-Id: I6d344ff5af0233adfb79246e180f42c2fe9a6abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572142
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989515}

---
+++ ash/components/login/auth/cryptohome_error.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_COMPONENTS_LOGIN_AUTH_CRYPTOHOME_ERROR_H_
+#define ASH_COMPONENTS_LOGIN_AUTH_CRYPTOHOME_ERROR_H_
+
+#include "base/component_export.h"
+#include "chromeos/dbus/cryptohome/UserDataAuth.pb.h"
+
+namespace ash {
+
+// Struct that wraps implementation details of cryptohomed error reporting.
+struct COMPONENT_EXPORT(ASH_LOGIN_AUTH) CryptohomeError {
+  user_data_auth::CryptohomeErrorCode error_code;
+};
+
+}  // namespace ash
+
+#endif  // ASH_COMPONENTS_LOGIN_AUTH_CRYPTOHOME_ERROR_H_
