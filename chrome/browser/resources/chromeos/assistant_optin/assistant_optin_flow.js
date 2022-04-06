1cfed6feabec370bbc4e65fc4833aa270040fe1a
Renato Silva <rrsilva@google.com>
2022-04-06 19:47:01 +0000

    OOBE / Assistant - Modulize setting_zippy
    Add directives to modulize setting_zippy.
    
    Change-Id: Ice3cf9b75b1b8312f2ef313ed6486ca650654687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3567682
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#989584}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // <include src="utils.js">
-// <include src="setting_zippy.js">
 // <include src="assistant_loading.js">
 // <include src="assistant_related_info.js">
 // <include src="assistant_value_prop.js">
--- chrome/browser/resources/chromeos/assistant_optin/setting_zippy.js
+++ chrome/browser/resources/chromeos/assistant_optin/setting_zippy.js
@@ -3,40 +3,51 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Polymer({
-  is: 'setting-zippy',
+/* #js_imports_placeholder */
 
-  properties: {
-    iconSrc: {
-      type: String,
-      value: null,
-    },
+/**
+ * @polymer
+ */
+class SettingZippy extends Polymer.Element {
+  static get is() {
+    return 'setting-zippy';
+  }
 
-    hideLine: {
-      type: Boolean,
-      value: false,
-    },
+  /* #html_template_placeholder */
 
-    expandStyle: {
-      type: Boolean,
-      value: false,
-    },
+  static get properties() {
+    return {
+      iconSrc: {
+        type: String,
+        value: null,
+      },
 
-    toggleStyle: {
-      type: Boolean,
-      value: false,
-    },
+      hideLine: {
+        type: Boolean,
+        value: false,
+      },
 
-    popupStyle: {
-      type: Boolean,
-      value: false,
-    },
+      expandStyle: {
+        type: Boolean,
+        value: false,
+      },
 
-    cardStyle: {
-      type: Boolean,
-      value: false,
-    },
-  },
+      toggleStyle: {
+        type: Boolean,
+        value: false,
+      },
+
+      popupStyle: {
+        type: Boolean,
+        value: false,
+      },
+
+      cardStyle: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
 
   /**
    * Wrap the icon as a image into a html snippet.
@@ -66,5 +77,7 @@ Polymer({
       </style>
     <body><img id="icon" aria-label="` +
         imageLabel + `" src="` + iconUri + '"></body></html>';
-  },
-});
+  }
+}
+
+customElements.define(SettingZippy.is, SettingZippy);
