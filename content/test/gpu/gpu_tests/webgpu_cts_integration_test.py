b43a6b004732d4cb12fb13e17a397adbf5155801
Brian Sheedy <bsheedy@chromium.org>
2022-04-06 03:04:37 +0000

    Add WebGPU exception handling
    Adds exception handling in the WebGPU Telemetry harness for asyncio
    CancelledErrors. These errors are expected since we're canceling Futures
    as a signal to stop connections, etc.
    
    Bug: 1313564
    Change-Id: If320fa505ae70a55c263dcf7ad31c6862fd02ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572917
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989254}

--- content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
+++ content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
@@ -82,6 +82,8 @@ class ServerThread(threading.Thread):
   def run(self):
     try:
       asyncio.run(StartWebsocketServer())
+    except asyncio.CancelledError:
+      pass
     except Exception as e:  # pylint:disable=broad-except
       sys.stdout.write('Server thread had exception: %s\n' % e)
 
@@ -184,8 +186,16 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
   def TearDownWebsocketServer(cls):
     if cls.connection_stopper:
       cls.connection_stopper.cancel()
+      try:
+        cls.connection_stopper.exception()
+      except asyncio.CancelledError:
+        pass
     if cls.server_stopper:
       cls.server_stopper.cancel()
+      try:
+        cls.server_stopper.exception()
+      except asyncio.CancelledError:
+        pass
     cls.server_stopper = None
     cls.connection_stopper = None
     cls.server_port = None
@@ -260,6 +270,10 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
   def CleanUpExistingWebsocket(cls):
     if cls.connection_stopper:
       cls.connection_stopper.cancel()
+      try:
+        cls.connection_stopper.exception()
+      except asyncio.CancelledError:
+        pass
     cls.connection_stopper = None
     cls.websocket = None
     cls.connection_received_event.clear()
