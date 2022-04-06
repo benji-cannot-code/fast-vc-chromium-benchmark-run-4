1f5bf213263b341cdb4c257dc7c4dcbbb419843e
Fabian Sommer <fabiansommer@chromium.org>
2022-04-06 12:48:41 +0000

    [Sheriff] Disable flaky ProfilePicker tests
    Tests are flaky on Linux CFI - disable them for that builder.
    
    Bug: 1289326
    Change-Id: I3b3fc7c1fcc50f75517aa425018db46ae11057ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572624
    Owners-Override: Fabian Sommer <fabiansommer@chromium.org>
    Auto-Submit: Fabian Sommer <fabiansommer@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#989391}

--- chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/barrier_closure.h"
 #include "base/callback_helpers.h"
+#include "base/cfi_buildflags.h"
 #include "base/json/values_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
@@ -855,8 +856,15 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
+// TODO(crbug.com/1289326) Test is flaky on Linux CFI
+#if BUILDFLAG(CFI_ICALL_CHECK) && BUILDFLAG(IS_LINUX)
+#define MAYBE_CreateSignedInProfileSettings \
+  DISABLED_CreateSignedInProfileSettings
+#else
+#define MAYBE_CreateSignedInProfileSettings CreateSignedInProfileSettings
+#endif
 IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
-                       CreateSignedInProfileSettings) {
+                       MAYBE_CreateSignedInProfileSettings) {
   ASSERT_EQ(1u, BrowserList::GetInstance()->size());
   // Simulate a successful sign-in and wait for the sign-in to propagate to the
   // flow, resulting in sync confirmation screen getting displayed.
@@ -1180,8 +1188,14 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
       metrics::StartupProfilingFinishReason::kDone, 1);
 }
 
+// TODO(crbug.com/1289326) Test is flaky on Linux CFI
+#if BUILDFLAG(CFI_ICALL_CHECK) && BUILDFLAG(IS_LINUX)
+#define MAYBE_OpenProfile_Settings DISABLED_OpenProfile_Settings
+#else
+#define MAYBE_OpenProfile_Settings OpenProfile_Settings
+#endif
 IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
-                       OpenProfile_Settings) {
+                       MAYBE_OpenProfile_Settings) {
   AvatarToolbarButton::SetIPHMinDelayAfterCreationForTesting(base::Seconds(0));
   ASSERT_EQ(1u, BrowserList::GetInstance()->size());
   // Create a second profile.
@@ -1391,8 +1405,16 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerEnterpriseCreationFlowBrowserTest,
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
+// TODO(crbug.com/1289326) Test is flaky on Linux CFI
+#if BUILDFLAG(CFI_ICALL_CHECK) && BUILDFLAG(IS_LINUX)
+#define MAYBE_CreateSignedInEnterpriseProfileSettings \
+  DISABLED_CreateSignedInEnterpriseProfileSettings
+#else
+#define MAYBE_CreateSignedInEnterpriseProfileSettings \
+  CreateSignedInEnterpriseProfileSettings
+#endif
 IN_PROC_BROWSER_TEST_F(ProfilePickerEnterpriseCreationFlowBrowserTest,
-                       CreateSignedInProfileSettings) {
+                       MAYBE_CreateSignedInEnterpriseProfileSettings) {
   ASSERT_EQ(1u, BrowserList::GetInstance()->size());
   // Simulate a successful sign-in and wait for the sign-in to propagate to the
   // flow, resulting in enterprise welcome screen getting displayed.
