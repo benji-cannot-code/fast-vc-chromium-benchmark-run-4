d7e7e65634b9e1b0b7090ab251bbc311be061e5f
Eric Willigers <ericwilligers@chromium.org>
2022-04-06 20:32:37 +0000

    base/i18n: Include chromeos_buildflags
    Source files using OS_CHROME_... now include
    build/chromeos_buildflags.h
    
    Bug: 1246565
    Change-Id: I7ca7128f8af6131ad461eb3b648b42f74bd534f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552179
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989607}

--- base/i18n/icu_mergeable_data_file.cc
+++ base/i18n/icu_mergeable_data_file.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
+#include "build/chromeos_buildflags.h"
 
 namespace base::i18n {
 
--- base/i18n/icu_util.h
+++ base/i18n/icu_util.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/memory_mapped_file.h"
 #include "base/i18n/base_i18n_export.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "icu_mergeable_data_file.h"
