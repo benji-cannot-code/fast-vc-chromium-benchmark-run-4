2f2f9e4424fc003655c87f78800bd264e7e1068c
Michael Wilson <mjwilson@chromium.org>
2022-04-06 05:50:31 +0000

    [WebAudio] Break ChannelSplitter node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: I6c6ac2f73cf8aa2a2566c86555b3c370e0da67ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572273
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989294}

--- third_party/blink/renderer/modules/webaudio/channel_splitter_node.h
+++ third_party/blink/renderer/modules/webaudio/channel_splitter_node.h
@@ -27,34 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_CHANNEL_SPLITTER_NODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_CHANNEL_SPLITTER_NODE_H_
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
 
 class BaseAudioContext;
 class ChannelSplitterOptions;
-
-class ChannelSplitterHandler final : public AudioHandler {
- public:
-  static scoped_refptr<ChannelSplitterHandler>
-  Create(AudioNode&, float sample_rate, unsigned number_of_outputs);
-
-  // AudioHandler
-  void Process(uint32_t frames_to_process) override;
-  void SetChannelCount(unsigned, ExceptionState&) final;
-  void SetChannelCountMode(const String&, ExceptionState&) final;
-  void SetChannelInterpretation(const String&, ExceptionState&) final;
-
-  double TailTime() const override { return 0; }
-  double LatencyTime() const override { return 0; }
-  bool RequiresTailProcessing() const final { return false; }
-
- private:
-  ChannelSplitterHandler(AudioNode&,
-                         float sample_rate,
-                         unsigned number_of_outputs);
-};
+class ExceptionState;
 
 class ChannelSplitterNode final : public AudioNode {
   DEFINE_WRAPPERTYPEINFO();
