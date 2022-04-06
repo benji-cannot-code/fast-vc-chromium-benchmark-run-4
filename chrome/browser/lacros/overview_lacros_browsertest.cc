ab95cfb37b70c9c7cbf1a118d040d339ffafb0ba
Eric Willigers <ericwilligers@chromium.org>
2022-04-06 17:58:30 +0000

    Check crosapi TestController method min versions
    OverviewBrowserTest checks kEnterOverviewModeMinVersion.
    
    TabletModeBrowserTest checks kEnterTabletModeMinVersion,
    instead of a numeric literal.
    
    Change-Id: Iafa3f2737d728eacbbfdf08df02754b497e9b065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574346
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989512}

--- chrome/browser/lacros/overview_lacros_browsertest.cc
+++ chrome/browser/lacros/overview_lacros_browsertest.cc
@@ -14,6 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "ui/aura/window.h"
 
+namespace {
+
+// If ash does not contain the relevant test controller functionality, then
+// there's nothing to do for this test.
+bool IsServiceAvailable() {
+  if (chromeos::LacrosService::Get()->GetInterfaceVersion(
+          crosapi::mojom::TestController::Uuid_) <
+      static_cast<int>(crosapi::mojom::TestController::MethodMinVersions::
+                           kEnterOverviewModeMinVersion)) {
+    LOG(WARNING) << "Unsupported ash version.";
+    return false;
+  }
+  return true;
+}
+
+}  // namespace
+
 using OverviewBrowserTest = InProcessBrowserTest;
 
 // We enter overview mode with a single window. When we close the window,
@@ -21,6 +38,9 @@ using OverviewBrowserTest = InProcessBrowserTest;
 // TODO(https://crbug.com/1157314): This test is not safe to run in parallel
 // with other lacros tests as overview mode applies to all processes.
 IN_PROC_BROWSER_TEST_F(OverviewBrowserTest, NoCrashWithSingleWindow) {
+  if (!IsServiceAvailable())
+    return;
+
   // Wait for the window to be visible.
   aura::Window* window = browser()->window()->GetNativeWindow();
   std::string id = browser_test_util::GetWindowId(window->GetRootWindow());
@@ -28,7 +48,6 @@ IN_PROC_BROWSER_TEST_F(OverviewBrowserTest, NoCrashWithSingleWindow) {
 
   // Enter overview mode.
   auto* lacros_service = chromeos::LacrosService::Get();
-  CHECK(lacros_service->IsAvailable<crosapi::mojom::TestController>());
   crosapi::mojom::TestControllerAsyncWaiter waiter(
       lacros_service->GetRemote<crosapi::mojom::TestController>().get());
   waiter.EnterOverviewMode();
@@ -44,6 +63,9 @@ IN_PROC_BROWSER_TEST_F(OverviewBrowserTest, NoCrashWithSingleWindow) {
 // TODO(https://crbug.com/1157314): This test is not safe to run in parallel
 // with other lacros tests as overview mode applies to all processes.
 IN_PROC_BROWSER_TEST_F(OverviewBrowserTest, NoCrashTwoWindows) {
+  if (!IsServiceAvailable())
+    return;
+
   // Wait for the window to be visible.
   aura::Window* main_window = browser()->window()->GetNativeWindow();
   std::string main_id =
@@ -63,7 +85,6 @@ IN_PROC_BROWSER_TEST_F(OverviewBrowserTest, NoCrashTwoWindows) {
 
   // Enter overview mode.
   auto* lacros_service = chromeos::LacrosService::Get();
-  CHECK(lacros_service->IsAvailable<crosapi::mojom::TestController>());
   crosapi::mojom::TestControllerAsyncWaiter waiter(
       lacros_service->GetRemote<crosapi::mojom::TestController>().get());
   waiter.EnterOverviewMode();
