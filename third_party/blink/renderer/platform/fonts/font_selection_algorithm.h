2af0698bd9c470cfe8a6910ba756f89601806e93
Yoshifumi Inoue <yosin@chromium.org>
2022-04-06 09:23:40 +0000

    Use const ref in FontSelectionAlgorithm constructor
    This patch changes `FontSelectionAlgorithm` to take const reference of
    `FontDescription` instead of object itself because it doesn't need to
    get object for improving code health.
    
    This is found during I'm working for the CL[1].
    
    [1] http://crrev.com/c/3211416 Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: I238f106c445ecaa22cf51ba4764f435cbae57520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574282
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989342}

--- third_party/blink/renderer/platform/fonts/font_selection_algorithm.h
+++ third_party/blink/renderer/platform/fonts/font_selection_algorithm.h
@@ -36,7 +36,7 @@ class PLATFORM_EXPORT FontSelectionAlgorithm {
  public:
   FontSelectionAlgorithm() = delete;
 
-  FontSelectionAlgorithm(FontSelectionRequest request,
+  FontSelectionAlgorithm(const FontSelectionRequest& request,
                          const FontSelectionCapabilities& capabilities_bounds)
       : request_(request), capabilities_bounds_(capabilities_bounds) {}
 
