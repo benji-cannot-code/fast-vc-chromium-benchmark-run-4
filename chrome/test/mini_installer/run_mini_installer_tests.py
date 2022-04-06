d0b332da838c3d2fa2c494f7763cdc8fab78d9eb
Greg Thompson <grt@chromium.org>
2022-04-06 09:59:13 +0000

    Revert "[code-health] Run mini_installer tests using python3"
    This reverts commit 30b9722ff42b244014adbb4fe5dd3028143fe824.
    
    Reason for revert: broke continuous builds.
    
    Original change's description:
    > [code-health] Run mini_installer tests using python3
    >
    > Bug: 941669
    > Cq-Include-Trybots: luci.chrome.try:win-chrome,win64-chrome
    > Change-Id: Ifcad5bb5a157c90c591fa77ed5260c7f91749ab0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561093
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#989048}
    
    Bug: 941669
    Change-Id: Id0b280092c1979aa0eb71d228eed97c10af9549f
    Cq-Include-Trybots: luci.chrome.try:win-chrome,win64-chrome
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574563
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Owners-Override: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989353}

--- chrome/test/mini_installer/run_mini_installer_tests.py
+++ chrome/test/mini_installer/run_mini_installer_tests.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright 2021 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -78,7 +78,7 @@ def main(args):
     try:
         return runner.run()[0]
     except KeyboardInterrupt:
-        sys.stderr.write("interrupted, exiting")
+        self.print_("interrupted, exiting", stream=self.host.stderr)
         return 130
 
 
