f77f8335684cebc5933c82eb66bbff1e1e32c12f
Ali Beyad <abeyad@chromium.org>
2022-04-06 22:53:05 +0000

    Remove the AcceptCHFrame base::Feature
    The feature has been enabled by default for some time, the experiment
    launched at 100%, and the Finch config was recently deleted.  Therefore,
    we are just cleaning up this no-longer-needed feature.
    
    Bug: 1312988
    Change-Id: Idb01ccb6d7554830bbe8093022e95d88acbbeb97
    Fixed: 1312988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569123
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Ali Beyad <abeyad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989657}

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -1129,8 +1129,7 @@ int URLLoader::OnConnected(net::URLRequest* url_request,
     return net::ERR_FAILED;
   }
 
-  if (!accept_ch_frame_observer_ || info.accept_ch_frame.empty() ||
-      !base::FeatureList::IsEnabled(features::kAcceptCHFrame)) {
+  if (!accept_ch_frame_observer_ || info.accept_ch_frame.empty()) {
     return net::OK;
   }
 
