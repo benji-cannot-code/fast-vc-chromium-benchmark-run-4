d42a7aaf0520b85f7f627bcc91e4cd01f0cb8fd5
Michael Wilson <mjwilson@chromium.org>
2022-04-06 05:23:59 +0000

    [WebAudio] Break Delay node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    I also updated file-level static constant style in delay_node.cc.
    
    Change-Id: If70c235dc30e96da50dcbb99a246730500d45005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568767
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989284}

--- third_party/blink/renderer/modules/webaudio/delay_node.h
+++ third_party/blink/renderer/modules/webaudio/delay_node.h
@@ -27,29 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_DELAY_NODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_DELAY_NODE_H_
 
-#include "third_party/blink/renderer/modules/webaudio/audio_basic_processor_handler.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_param.h"
 
 namespace blink {
 
-class AudioParamHandler;
 class BaseAudioContext;
 class DelayOptions;
 class ExceptionState;
-
-class DelayHandler : public AudioBasicProcessorHandler {
- public:
-  static scoped_refptr<DelayHandler> Create(AudioNode&,
-                                            float sample_rate,
-                                            AudioParamHandler& delay_time,
-                                            double max_delay_time);
-
- private:
-  DelayHandler(AudioNode&,
-               float sample_rate,
-               AudioParamHandler& delay_time,
-               double max_delay_time);
-};
 
 class DelayNode final : public AudioNode {
   DEFINE_WRAPPERTYPEINFO();
