5b6a2fd1c331214f06243a677a637637191042a7
Robbie Gibson <rkgibson@google.com>
2022-04-06 21:54:20 +0000

    [iOS] Turn off blurred background on new omnibox popup
    The blurred background design wants is more complex than this, so use a
    simple solid background for now.
    
    Bug: 1311631
    Change-Id: Iac7b26c84a2356d8b50d16bf81717e13df26dc8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574996
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#989637}

--- ios/chrome/browser/ui/omnibox/popup/shared/popup_view.swift
+++ ios/chrome/browser/ui/omnibox/popup/shared/popup_view.swift
@@ -47,6 +47,13 @@ struct PopupView: View {
     static let selfSizingListBottomMargin: CGFloat = 40
   }
 
+  /// Custom modifier for the background of the list.
+  struct ListBackgroundModifier: ViewModifier {
+    func body(content: Content) -> some View {
+      content.background(Color.cr_groupedPrimaryBackground.edgesIgnoringSafeArea(.all))
+    }
+  }
+
   @ObservedObject var model: PopupModel
   private let shouldSelfSize: Bool
   private let appearanceContainerType: UIAppearanceContainer.Type?
@@ -115,7 +122,7 @@ struct PopupView: View {
     )
     .concat(SimultaneousGestureModifier(DragGesture().onChanged { onDrag($0) }))
     .concat(ScrollOnChangeModifier(value: $model.sections, action: onNewMatches))
-    .concat(BlurredBackground())
+    .concat(ListBackgroundModifier())
 
     if shouldSelfSize {
       SelfSizingList(
