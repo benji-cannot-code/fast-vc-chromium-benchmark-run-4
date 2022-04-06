a6f61251cb8aae9e4585216a71ebff3551577cad
Curtis McMullan <curtismcmullan@chromium.org>
2022-04-06 02:29:28 +0000

    lacros: set sensitive hint on !should_do_learning
    We are currently sending the wrong value for should_do_learning from the
    lacros binary to ash, it is reversed.
    
    This CL fixes the issue by only setting the SENSITIVE_DATA hint bit when
    should_do_learning is false.
    
    BUG=b:225997761
    
    Change-Id: I5b5b5cdd77b0823c2452eef2c37ba426dde5f2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570622
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989246}

--- ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_input_method_context_unittest.cc
@@ -390,7 +390,7 @@ TEST_P(WaylandInputMethodContextTest, SetContentType) {
       .Times(1);
   input_method_context_->SetContentType(TEXT_INPUT_TYPE_URL,
                                         TEXT_INPUT_FLAG_AUTOCOMPLETE_ON,
-                                        /*should_do_learning=*/false);
+                                        /*should_do_learning=*/true);
   connection_->ScheduleFlush();
   Sync();
 }
@@ -403,7 +403,7 @@ TEST_P(WaylandInputMethodContextTest, SetContentTypeWithoutLearning) {
       .Times(1);
   input_method_context_->SetContentType(TEXT_INPUT_TYPE_URL,
                                         TEXT_INPUT_FLAG_AUTOCOMPLETE_ON,
-                                        /*should_do_learning=*/true);
+                                        /*should_do_learning=*/false);
   connection_->ScheduleFlush();
   Sync();
 }
