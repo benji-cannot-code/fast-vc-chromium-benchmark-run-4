aed09cdcfcfc1f31d95c72fe207820a8882e3a5d
Joseph Chao <josephchao@google.com>
2022-04-06 17:32:39 +0000

    Modifying native side generated files for JNI Multiplexing
    An additional header file is generated by jni_registration_generator.py,
    and this file contains switch statements that will dispatch the java
    side native method calls to their native implementations across the
    JNI boundary.
    
    This single header file includes the forwarding declarations for the
    native methods, as well as the forwarding calls to their native
    implementations. The switch statements dispatching the methods are
    grouped by each method's unique parameters and return type.
    
    The switch number has also been changed to a 64-bit long of a native
    method's hashed proxy name instead of a 32-bit int with the file path as
    a switch prefix. This is to avoid complications of synchronizing the
    java side source files with the native side source files.
    
    Tests have also been added to test the new flags and functionalities.
    
    New single header file example:
    https://paste.googleplex.com/4976477184458752
    
    Updated generated GEN_JNI.java example:
    https://paste.googleplex.com/5655698823184384
    
    Updated generated N.java example:
    https://paste.googleplex.com/4586582167257088
    
    Bug: 929682
    Change-Id: Ie3ffeca1bade3b2657c6afd946be8ecb479c265e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531411
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Joseph Chao <josephchao@google.com>
    Cr-Commit-Position: refs/heads/main@{#989499}

--- base/android/jni_generator/jni_generator.py
+++ base/android/jni_generator/jni_generator.py
@@ -980,12 +980,17 @@ class JNIFromJavaSource(object):
 class HeaderFileGeneratorHelper(object):
   """Include helper methods for header generators."""
 
-  def __init__(self, class_name, fully_qualified_class, use_proxy_hash,
-               split_name):
+  def __init__(self,
+               class_name,
+               fully_qualified_class,
+               use_proxy_hash,
+               split_name=None,
+               enable_jni_multiplexing=False):
     self.class_name = class_name
     self.fully_qualified_class = fully_qualified_class
     self.use_proxy_hash = use_proxy_hash
     self.split_name = split_name
+    self.enable_jni_multiplexing = enable_jni_multiplexing
 
   def GetStubName(self, native):
     """Return the name of the stub function for this native method.
@@ -1002,7 +1007,9 @@ class HeaderFileGeneratorHelper(object):
       else:
         method_name = EscapeClassName(native.proxy_name)
       return 'Java_%s_%s' % (EscapeClassName(
-          ProxyHelpers.GetQualifiedClass(self.use_proxy_hash)), method_name)
+          ProxyHelpers.GetQualifiedClass(
+              self.use_proxy_hash
+              or self.enable_jni_multiplexing)), method_name)
 
     template = Template('Java_${JAVA_NAME}_native${NAME}')
 
@@ -1017,8 +1024,9 @@ class HeaderFileGeneratorHelper(object):
     ret = collections.OrderedDict()
     for entry in origin:
       if isinstance(entry, NativeMethod) and entry.is_proxy:
-        ret[ProxyHelpers.GetClass(self.use_proxy_hash)] \
-          = ProxyHelpers.GetQualifiedClass(self.use_proxy_hash)
+        use_hash = self.use_proxy_hash or self.enable_jni_multiplexing
+        ret[ProxyHelpers.GetClass(use_hash)] \
+          = ProxyHelpers.GetQualifiedClass(use_hash)
         continue
       ret[self.class_name] = self.fully_qualified_class
 
@@ -1051,7 +1059,8 @@ const char kClassPath_${JAVA_CLASS}[] = \
       }
       # Since all proxy methods use the same class, defining this in every
       # header file would result in duplicated extern initializations.
-      if full_clazz != ProxyHelpers.GetQualifiedClass(self.use_proxy_hash):
+      if full_clazz != ProxyHelpers.GetQualifiedClass(
+          self.use_proxy_hash or self.enable_jni_multiplexing):
         ret += [template.substitute(values)]
 
     class_getter = """\
@@ -1082,7 +1091,8 @@ JNI_REGISTRATION_EXPORT std::atomic<jclass> g_${JAVA_CLASS}_clazz(nullptr);
       }
       # Since all proxy methods use the same class, defining this in every
       # header file would result in duplicated extern initializations.
-      if full_clazz != ProxyHelpers.GetQualifiedClass(self.use_proxy_hash):
+      if full_clazz != ProxyHelpers.GetQualifiedClass(
+          self.use_proxy_hash or self.enable_jni_multiplexing):
         ret += [template.substitute(values)]
 
     return ''.join(ret)
@@ -1102,10 +1112,12 @@ class InlHeaderFileGenerator(object):
     self.constant_fields = constant_fields
     self.jni_params = jni_params
     self.options = options
-    self.helper = HeaderFileGeneratorHelper(self.class_name,
-                                            fully_qualified_class,
-                                            self.options.use_proxy_hash,
-                                            self.options.split_name)
+    self.helper = HeaderFileGeneratorHelper(
+        self.class_name,
+        fully_qualified_class,
+        self.options.use_proxy_hash,
+        split_name=self.options.split_name,
+        enable_jni_multiplexing=self.options.enable_jni_multiplexing)
 
   def GetContent(self):
     """Returns the content of the JNI binding file."""
@@ -1608,6 +1620,9 @@ See SampleForTests.java for more details.
       action='store_true',
       help='Hashes the native declaration of methods used '
       'in @JniNatives interface.')
+  parser.add_argument('--enable_jni_multiplexing',
+                      action='store_true',
+                      help='Enables JNI multiplexing for Java native methods')
   parser.add_argument(
       '--split_name',
       help='Split name that the Java classes should be loaded from.')
