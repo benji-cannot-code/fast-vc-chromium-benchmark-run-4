8a33770fbce9e8d03a66a68a78590477c5365bad
Renato Silva <rrsilva@google.com>
2022-04-06 13:38:35 +0000

    OOBE / Assistant - Modulize voice_match_entry
    Polymer3 Migration - Modulize voice_match_entry and rewrite using
    ES6 class syntax.
    
    Bug: 1184731
    Change-Id: I086c176ef26a1e3c222d21aaba8857b9d0fa50bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529209
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#989406}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // <include src="utils.js">
 // <include src="setting_zippy.js">
-// <include src="voice_match_entry.js">
 // <include src="browser_proxy.js">
 // <include src="assistant_loading.js">
 // <include src="assistant_related_info.js">
--- chrome/browser/resources/chromeos/assistant_optin/voice_match_entry.js
+++ chrome/browser/resources/chromeos/assistant_optin/voice_match_entry.js
@@ -3,20 +3,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Polymer({
-  is: 'voice-match-entry',
+/* #js_imports_placeholder */
 
-  behaviors: [OobeI18nBehavior],
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ */
+ const VoiceMatchEntryBase = Polymer.mixinBehaviors(
+  [OobeI18nBehavior],
+  Polymer.Element);
 
-  properties: {
-    active: {
-      type: Boolean,
-      value: false,
-    },
+/**
+ * @polymer
+ */
+class VoiceMatchEntry extends VoiceMatchEntryBase {
 
-    completed: {
-      type: Boolean,
-      value: false,
-    },
-  },
-});
+  static get is() { return 'voice-match-entry'; }
+
+  /* #html_template_placeholder */
+
+  static get properties() {
+    return {
+      active: {
+        type: Boolean,
+        value: false,
+      },
+
+      completed: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+}
+
+customElements.define(VoiceMatchEntry.is, VoiceMatchEntry);
