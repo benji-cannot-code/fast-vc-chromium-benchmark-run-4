9d8b36a4960b66cbc690123f296d58938b53f81e
Chris Lu <thegreenfrog@chromium.org>
2022-04-06 16:43:06 +0000

    [ios] Remove Header from View Hierarchy in resetViewHierarchy
    Bug: 1303500, 1313412
    Change-Id: Ie729a9bcc63989148b0d24455c5cb8c8ba03bd3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572989
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#989468}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -463,6 +463,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self.feedHeaderViewController) {
     [self removeFromViewHierarchy:self.feedHeaderViewController];
   }
+  if (self.headerController) {
+    [self removeFromViewHierarchy:self.headerController];
+  }
   self.contentSuggestionsHeightConstraint.active = NO;
 }
 
