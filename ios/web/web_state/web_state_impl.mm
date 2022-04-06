c63120428be0dbfa5547dee0230c0c704e565f33
Benjamin Williams <bwwilliams@google.com>
2022-04-06 19:11:41 +0000

    Error Page Refactor, fix offline page navigation.
    This CL:
    
    1. Plumbs loadSimulatedRequest down from web_state, exposing it to
    OfflinePageTabHelper.
    
    2. Fixes incorrect double backForwardList population by removing offline
    chrome:// URL(s) across the entire app when
    web::features::IsLoadSimulatedRequestAPIEnabled().
    
    3. Fixes infinite app hang when pressing forward nav button from offline
    page.
    
    4. Properly reloads the "real" page when navigating to an offline page,
    if proper conditions are met (i.e. user has good network connection).
    
    Video recordings demonstrating new & improved navigation behavior:
    
    User journey #6 from go/error-page-refactoring-implementation-plan:
    https://drive.google.com/file/d/1rCrjtBkELkbcYEk9SViDKZ3qmGHFp7wI/view?usp=sharing&resourcekey=0-pqGJfhTen77cWndtThY9BA
    
    User journey #7 from go/error-page-refactoring-implementation-plan:
    https://drive.google.com/file/d/1-_9wlFENlBx2dw5RsOHZSHCSR-_aed0c/view?usp=sharing&resourcekey=0-AP00unVnWRpTUwbJ_kXw5g
    
    Change-Id: Ibb4a60b0f078e5e2aa0000e64699c488e0357a26
    Bug: 1301956,1301954,1306712
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546240
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#989561}

--- ios/web/web_state/web_state_impl.mm
+++ ios/web/web_state/web_state_impl.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/session/crw_session_storage.h"
 #import "ios/web/session/session_certificate_policy_cache_impl.h"
 #import "ios/web/web_state/global_web_state_event_tracker.h"
+#import "ios/web/web_state/ui/crw_web_controller.h"
 #import "ios/web/web_state/web_state_impl_realized_web_state.h"
 #import "ios/web/web_state/web_state_impl_serialized_data.h"
 #import "url/gurl.h"
@@ -414,6 +415,24 @@ base::WeakPtr<WebState> WebStateImpl::GetWeakPtr() {
 
 void WebStateImpl::OpenURL(const WebState::OpenURLParams& params) {
   RealizedState()->OpenURL(params);
+}
+
+void WebStateImpl::LoadSimulatedRequest(const GURL& url,
+                                        NSString* response_html_string) {
+  CRWWebController* web_controller = GetWebController();
+  DCHECK(web_controller);
+  [web_controller loadSimulatedRequest:url
+                    responseHTMLString:response_html_string];
+}
+
+void WebStateImpl::LoadSimulatedRequest(const GURL& url,
+                                        NSData* response_data,
+                                        NSString* mime_type) {
+  CRWWebController* web_controller = GetWebController();
+  DCHECK(web_controller);
+  [web_controller loadSimulatedRequest:url
+                          responseData:response_data
+                              MIMEType:mime_type];
 }
 
 void WebStateImpl::Stop() {
