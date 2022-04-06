b1385f46c890d9441c5fa26aaf55edd97bebcd68
Renato Silva <rrsilva@google.com>
2022-04-06 16:58:27 +0000

    OOBE / Assistant - Modulize assistant_voice_match
    Polymer3 Migration - Modulize assistant_voice_match and rewrite using
    ES6 class syntax.
    
    Bug: 1184731
    Change-Id: I0380c4f7225a0744ad6139302483c7cb1bc25c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3562943
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#989476}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // <include src="utils.js">
 // <include src="setting_zippy.js">
-// <include src="browser_proxy.js">
 // <include src="assistant_loading.js">
 // <include src="assistant_related_info.js">
 // <include src="assistant_value_prop.js">
-// <include src="assistant_voice_match.js">
 
 /**
  * @fileoverview Polymer element for displaying material design assistant
