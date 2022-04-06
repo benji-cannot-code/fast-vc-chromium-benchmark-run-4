cde4f7614bf075c5584688a0002c36c63279a134
Clark DuVall <cduvall@chromium.org>
2022-04-06 15:28:57 +0000

    Add feature to prewarm default fonts on Windows
    This builds off the AsyncFontAccess experiment, and allows prewarming
    the default fonts before page load to see if that gives any performance
    improvement.
    
    Bug: 1313590
    Change-Id: Iab0078eb53bb72b75659e55fb5c611cb69018946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572864
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989439}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -160,6 +160,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/core/timing/window_performance.h"
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
+#include "third_party/blink/renderer/platform/fonts/generic_font_family_settings.h"
 #include "third_party/blink/renderer/platform/graphics/image.h"
 #include "third_party/blink/renderer/platform/graphics/paint/cull_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_record_builder.h"
@@ -1788,6 +1789,30 @@ void WebView::ApplyWebPreferences(const web_pref::WebPreferences& prefs,
 
   RuntimeEnabledFeatures::SetTranslateServiceEnabled(
       prefs.translate_service_available);
+
+#if BUILDFLAG(IS_WIN)
+  if (web_view_impl->GetPage() &&
+      base::FeatureList::IsEnabled(features::kPrewarmDefaultFontFamilies)) {
+    if (auto* prewarmer = WebFontRendering::GetFontPrewarmer()) {
+      GenericFontFamilySettings& font_settings =
+          web_view_impl->GetPage()
+              ->GetSettings()
+              .GetGenericFontFamilySettings();
+      if (features::kPrewarmStandard.Get())
+        prewarmer->PrewarmFamily(font_settings.Standard());
+      if (features::kPrewarmFixed.Get())
+        prewarmer->PrewarmFamily(font_settings.Fixed());
+      if (features::kPrewarmSerif.Get())
+        prewarmer->PrewarmFamily(font_settings.Serif());
+      if (features::kPrewarmSansSerif.Get())
+        prewarmer->PrewarmFamily(font_settings.SansSerif());
+      if (features::kPrewarmCursive.Get())
+        prewarmer->PrewarmFamily(font_settings.Cursive());
+      if (features::kPrewarmFantasy.Get())
+        prewarmer->PrewarmFamily(font_settings.Fantasy());
+    }
+  }
+#endif
 }
 
 void WebViewImpl::ThemeChanged() {
