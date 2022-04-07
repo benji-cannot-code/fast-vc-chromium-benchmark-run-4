e85698ce736ff21fefcdb89e825e07e1b50d33be
Chong Gu <chonggu@google.com>
2022-04-07 19:51:41 +0000

    [Fuchsia] Remove six library, change shebang to vpython3
    Change all test scripts' shebang to vpython3, allowing for a controlled
    python environment to run them.
    Remove unused six import
    
    Bug: 1314370
    Change-Id: I897d4f9bbb887262d4a1770bb09a17ff69df18fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576754
    Auto-Submit: Chong Gu <chonggu@google.com>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990067}

--- build/fuchsia/pkg_repo.py
+++ build/fuchsia/pkg_repo.py
@@ -12,8 +12,6 @@ import subprocess
 import tempfile
 import time
 
-from six.moves import urllib
-
 # Maximum amount of time to block while waiting for "pm serve" to come up.
 _PM_SERVE_LISTEN_TIMEOUT_SECS = 10
 
