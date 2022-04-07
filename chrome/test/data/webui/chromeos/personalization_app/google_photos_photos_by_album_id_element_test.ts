9c4638dff925237c1c9cf3f6801c0f197edf5139
Angel Sanchez <angelsan@chromium.org>
2022-04-07 23:17:24 +0000

    personalization: GooglePhotosPhoto location field
    This CL added location optional string attribute to GooglePhotosPhoto.
    Location will be at most one string. The location string can be a
    business name, a city name, or an alias like "Home".
    
    Bug: b/222697647
    Change-Id: I317612cb676ef9661b81aea2bd81863a12c77f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522829
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Angel Sanchez <angelsan@google.com>
    Cr-Commit-Position: refs/heads/main@{#990176}

--- chrome/test/data/webui/chromeos/personalization_app/google_photos_photos_by_album_id_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/google_photos_photos_by_album_id_element_test.ts
@@ -80,13 +80,15 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
           id: '9bd1d7a3-f995-4445-be47-53c5b58ce1cb',
           name: 'foo',
           date: {data: []},
-          url: {url: 'foo.com'}
+          url: {url: 'foo.com'},
+          location: 'home1'
         },
         {
           id: '0ec40478-9712-42e1-b5bf-3e75870ca042',
           name: 'bar',
           date: {data: []},
-          url: {url: 'bar.com'}
+          url: {url: 'bar.com'},
+          location: 'home2'
         },
       ],
       [otherAlbum.id]: [
@@ -94,7 +96,8 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
           id: '0a268a37-877a-4936-81d4-38cc84b0f596',
           name: 'baz',
           date: {data: []},
-          url: {url: 'baz.com'}
+          url: {url: 'baz.com'},
+          location: 'home3'
         },
       ],
     };
@@ -200,14 +203,16 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
       id: '9bd1d7a3-f995-4445-be47-53c5b58ce1cb',
       name: 'foo',
       date: {data: []},
-      url: {url: 'foo.com'}
+      url: {url: 'foo.com'},
+      location: 'home1'
     };
 
     const anotherPhoto: GooglePhotosPhoto = {
       id: '0ec40478-9712-42e1-b5bf-3e75870ca042',
       name: 'bar',
       date: {data: []},
-      url: {url: 'bar.com'}
+      url: {url: 'bar.com'},
+      location: 'home2'
     };
 
     // Set values returned by |wallpaperProvider|.
@@ -327,6 +332,7 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
                                             name: `name-${i}`,
                                             date: {data: []},
                                             url: {url: `url-${i}`},
+                                            location: `location-${i}`,
                                           }));
 
     // Initialize |googlePhotosPhotosByAlbumIdElement|.
@@ -393,7 +399,8 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
             id: `id-${nextPhotoId}`,
             name: `name-${nextPhotoId}`,
             date: {data: []},
-            url: {url: `url-${nextPhotoId++}`},
+            url: {url: `url-${nextPhotoId}`},
+            location: `location-${nextPhotoId++}`,
           };
         }));
 
@@ -429,7 +436,8 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
             id: `id-${nextPhotoId}`,
             name: `name-${nextPhotoId}`,
             date: {data: []},
-            url: {url: `url-${nextPhotoId++}`},
+            url: {url: `url-${nextPhotoId}`},
+            location: `location-${nextPhotoId++}`,
           };
         }));
 
@@ -494,7 +502,8 @@ suite('GooglePhotosPhotosByAlbumIdTest', function() {
       id: '9bd1d7a3-f995-4445-be47-53c5b58ce1cb',
       name: 'foo',
       date: {data: []},
-      url: {url: 'foo.com'}
+      url: {url: 'foo.com'},
+      location: 'home'
     };
 
     // Initialize Google Photos data in the |personalizationStore|.
