79779a7a55e44a61628f118087ae5b2037384319
Greg Daniel <egdaniel@google.com>
2022-04-07 00:44:23 +0000

    Remove unneeded skia/src include of GrSemaphore.
    Change-Id: I0850fae77323f6cd59a0e7355280c6ff6ccf7efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573009
    Auto-Submit: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989716}

--- components/viz/service/display_embedder/skia_output_device.h
+++ components/viz/service/display_embedder/skia_output_device.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/swap_buffers_complete_params.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
-#include "third_party/skia/src/gpu/GrSemaphore.h"
+#include "third_party/skia/include/gpu/GrBackendSemaphore.h"
 #include "ui/gfx/swap_result.h"
 
 class GrDirectContext;
