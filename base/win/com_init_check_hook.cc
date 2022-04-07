307c34bcffacd4d385b11ffc7aebffbd8edf1c11
Robert Liao <robliao@chromium.org>
2022-04-07 20:34:00 +0000

    ComInitCheckHook Workaround for Runas Elevated Processes
    Windows will shim processes invoked as elevated with
    ShellExecute "runas". The loaded apphelp.dll takes the place of ole32
    for CoCreateInstance and includes a hotpatch region that is missing
    one byte, making it unusable:
    
    apphelp!InsHook_CallNextHookEx [Towards the end]:
    74d216b7 5e              pop     esi
    74d216b8 5d              pop     ebp
    74d216b9 c21000          ret     10h
    74d216bc cc              int     3 <-- Missing one additional int 3.
    74d216bd cc              int     3
    74d216be cc              int     3
    74d216bf cc              int     3
    apphelp!InsHook_CoCreateInstance:
    74d216c0 8bff            mov     edi,edi
    74d216c2 55              push    ebp
    
    The workaround here is to silently avoid patching in this detected
    state until we figure out why the hotpatch region is missing one byte.
    This means that processes with this compatibility shim will not get
    failure to initialize COM DCHECKs upon calls to CoCreateInstance.
    
    Bug: 1312659
    Change-Id: I9a4d9321d45365c63bd3d628e19045c54a81ca21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575067
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990097}

--- base/win/com_init_check_hook.cc
+++ base/win/com_init_check_hook.cc
@@ -89,6 +89,10 @@ const unsigned char g_hotpatch_placeholder_nop[] = {0x90, 0x90, 0x90, 0x90,
 const unsigned char g_hotpatch_placeholder_int3[] = {0xcc, 0xcc, 0xcc, 0xcc,
                                                      0xcc, 0x8b, 0xff};
 
+// http://crbug.com/1312659: Unusable apphelp placeholder missing one byte.
+const unsigned char g_hotpatch_placeholder_apphelp[] = {0x00, 0xcc, 0xcc, 0xcc,
+                                                        0xcc, 0x8b, 0xff};
+
 class HookManager {
  public:
   static HookManager* GetInstance() {
@@ -135,6 +139,8 @@ class HookManager {
     INT3,
     // The hotpatch placeholder used nop's in the sled.
     NOP,
+    // The hotpatch placeholder is an unusable apphelp shim.
+    APPHELP_SHIM,
     // This function has already been patched by a different component.
     EXTERNALLY_PATCHED,
   };
@@ -184,6 +190,11 @@ class HookManager {
                           reinterpret_cast<uint32_t>(&structured_hotpatch_))
                    << ">";
       return;
+    } else if (format == HotpatchPlaceholderFormat::APPHELP_SHIM) {
+      // The apphelp shim placeholder does not allocate enough bytes for a
+      // trampolined jump. In this case, we skip patching.
+      hotpatch_placeholder_format_ = format;
+      return;
     }
 
     DCHECK_EQ(hotpatch_placeholder_format_, HotpatchPlaceholderFormat::UNKNOWN);
@@ -217,6 +228,7 @@ class HookManager {
             sizeof(g_hotpatch_placeholder_nop));
         break;
       case HotpatchPlaceholderFormat::EXTERNALLY_PATCHED:
+      case HotpatchPlaceholderFormat::APPHELP_SHIM:
       case HotpatchPlaceholderFormat::UNKNOWN:
         break;
     }
@@ -245,6 +257,12 @@ class HookManager {
                  reinterpret_cast<const void*>(&g_hotpatch_placeholder_nop),
                  sizeof(g_hotpatch_placeholder_nop)) == 0) {
       return HotpatchPlaceholderFormat::NOP;
+    }
+
+    if (::memcmp(reinterpret_cast<void*>(co_create_instance_padded_address_),
+                 reinterpret_cast<const void*>(&g_hotpatch_placeholder_apphelp),
+                 sizeof(g_hotpatch_placeholder_apphelp)) == 0) {
+      return HotpatchPlaceholderFormat::APPHELP_SHIM;
     }
 
     const unsigned char* instruction_bytes =
