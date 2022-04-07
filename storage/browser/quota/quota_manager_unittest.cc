26d33319f589aded6a4fdc304cc80a26bef993ae
Christine Smith <christinesm@chromium.org>
2022-04-07 18:17:59 +0000

    (chrome://quota-internals) Fix Quota Internals DumpBucketTable crash
    - Resolves bug where the quota internals page crashes on DumpBucketTable when accessed
      before the database is populated. Results in behavior as intended
      (empty data tables appear on the page instead of the browser crashing).
    
    Bug: 1312185
    Change-Id: Id7242554bb9089d53ab583e91f5396973250f2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572764
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990028}

--- storage/browser/quota/quota_manager_unittest.cc
+++ storage/browser/quota/quota_manager_unittest.cc
@@ -2782,6 +2782,10 @@ TEST_F(QuotaManagerImplTest, GetBucketsModifiedBetweenWithDatabaseError) {
 }
 
 TEST_F(QuotaManagerImplTest, DumpBucketTable) {
+  // Dumping an unpopulated bucket table returns an empty vector.
+  const BucketTableEntries& initial_entries = DumpBucketTable();
+  EXPECT_TRUE(initial_entries.empty());
+
   const StorageKey kStorageKey = ToStorageKey("http://example.com/");
   CreateBucketForTesting(kStorageKey, kDefaultBucketName, kTemp);
   CreateBucketForTesting(kStorageKey, kDefaultBucketName, kPerm);
