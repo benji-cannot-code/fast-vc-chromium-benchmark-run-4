5e34bf906ff3e25b02326a37ca1db9822362fbf4
Joshua Pawlicki <waffles@chromium.org>
2022-04-07 20:11:30 +0000

    Updater: fix chrome compile if (ENABLE_CHROMIUM_UPDATER).
    This is a follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/3518606
    
    Bug: 1282020
    Change-Id: Ia5244e6f88e764a082e8021e5196f2477374e945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572553
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990080}

--- chrome/browser/updater/browser_updater_client_mac.mm
+++ chrome/browser/updater/browser_updater_client_mac.mm
@@ -131,6 +131,7 @@ NSString* GetAppIdForUpdaterAsNSString() {
 // Checks for update of a given app, with specified priority. Sends repeated
 // updates of progress and returns the result in the reply block.
 - (void)checkForUpdateWithAppID:(NSString* _Nonnull)appID
+               installDataIndex:(NSString* _Nullable)installDataIndex
                        priority:(CRUPriorityWrapper* _Nonnull)priority
         policySameVersionUpdate:
             (CRUPolicySameVersionUpdateWrapper* _Nonnull)policySameVersionUpdate
@@ -145,6 +146,7 @@ NSString* GetAppIdForUpdaterAsNSString() {
 
   [[_xpcConnection remoteObjectProxyWithErrorHandler:errorHandler]
       checkForUpdateWithAppID:appID
+             installDataIndex:installDataIndex
                      priority:priority
       policySameVersionUpdate:policySameVersionUpdate
                   updateState:updateState
@@ -244,6 +246,7 @@ void BrowserUpdaterClientMac::BeginUpdateCheck(
           initWithPolicySameVersionUpdate:
               updater::UpdateService::PolicySameVersionUpdate::kNotAllowed]);
   [client_ checkForUpdateWithAppID:GetAppIdForUpdaterAsNSString()
+                  installDataIndex:nil
                           priority:priority_wrapper.get()
            policySameVersionUpdate:policySameVersionUpdateWrapper.get()
                        updateState:state_observer.get()
