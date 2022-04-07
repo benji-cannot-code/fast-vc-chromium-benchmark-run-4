b62ddca855f2858bbc326f4775b784808c1c1303
Kirubel Aklilu <kaklilu@chromium.org>
2022-04-07 02:01:12 +0000

    Remove FirstPartySetsSettings singleton.
    This CL also updates all references to FirstPartySetsSettings to use
    methods on the content::FirstPartySetsHandler instead, particularly
    IsEnabled and ResetForTesting.
    
    This CL also renames IsFirstPartySetsEnabled to IsEnabled on the
    FirstPartySetsHandlerImpl.
    
    Change-Id: I7cb726d2d2f44de042bc13b0df91548182e2ba91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572308
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989743}

--- chrome/browser/extensions/chrome_extension_cookies.cc
+++ chrome/browser/extensions/chrome_extension_cookies.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/extensions/chrome_extension_cookies_factory.h"
-#include "chrome/browser/first_party_sets/first_party_sets_settings.h"
 #include "chrome/browser/net/profile_network_context_service.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
+#include "content/public/browser/first_party_sets_handler.h"
 #include "extensions/common/constants.h"
 #include "net/cookies/cookie_partition_key_collection.h"
 #include "net/cookies/first_party_set_metadata.h"
@@ -29,7 +29,7 @@ namespace extensions {
 ChromeExtensionCookies::ChromeExtensionCookies(Profile* profile)
     : profile_(profile),
       first_party_sets_enabled_(
-          FirstPartySetsSettings::Get()->IsFirstPartySetsEnabled()) {
+          content::FirstPartySetsHandler::GetInstance()->IsEnabled()) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   cookie_settings_ = CookieSettingsFactory::GetForProfile(profile);
   cookie_settings_observation_.Observe(cookie_settings_.get());
