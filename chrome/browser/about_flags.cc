6dbe340e8690f291df29aa6483f32dd5f3a96dc3
Sophie Chang <sophiechang@chromium.org>
2022-04-07 16:03:32 +0000

    Enable chrome://flags for the entities model on Android
    Bug: 1295931
    Change-Id: Ic1d6f649a3eddb3b9ef2872a75197278cf910b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575496
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989953}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5091,7 +5091,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"page-entities-page-content-annotations",
      flag_descriptions::kPageEntitiesPageContentAnnotationsName,
      flag_descriptions::kPageEntitiesPageContentAnnotationsDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_WITH_PARAMS_VALUE_TYPE(
          optimization_guide::features::kPageEntitiesPageContentAnnotations,
          kPageEntitiesPageContentAnnotationsVariations,
