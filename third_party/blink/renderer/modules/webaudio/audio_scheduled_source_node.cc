cb9e53554a98f35ac679d97b42f204c8ef3ac152
David Sanders <dsanders11@ucsbalum.com>
2022-04-07 00:03:20 +0000

    Remove unused includes from base_audio_context.h
    Also adds some includes to other files which were
    getting them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: Icefe7430dccf0e3069bb9e94a0dd76a45a1cb5af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573002
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#989690}

--- third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.cc
+++ third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "third_party/blink/public/platform/task_type.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/modules/event_modules.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 #include "third_party/blink/renderer/platform/audio/audio_utilities.h"
--- third_party/blink/renderer/modules/webaudio/iir_filter_handler.cc
+++ third_party/blink/renderer/modules/webaudio/iir_filter_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 #include "third_party/blink/renderer/modules/webaudio/iir_processor.h"
