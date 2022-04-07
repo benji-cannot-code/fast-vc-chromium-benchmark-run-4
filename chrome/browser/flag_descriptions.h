a30581db949152d33f07a2af4268e7dceded6762
Yann Dago <ydago@chromium.org>
2022-04-07 15:09:20 +0000

    Remove expired enable-sync-requires-policies-loaded flag
    Bug: 1129065
    Change-Id: I595630dabc4c57d9b6ed6bf13a0edffb31579b40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572863
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989930}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -529,9 +529,6 @@ extern const char kEnableMigrateDefaultChromeAppToWebAppsNonGSuiteDescription[];
 extern const char kEnablePreinstalledWebAppDuplicationFixerName[];
 extern const char kEnablePreinstalledWebAppDuplicationFixerDescription[];
 
-extern const char kEnableSyncRequiresPoliciesLoadedName[];
-extern const char kEnableSyncRequiresPoliciesLoadedDescription[];
-
 extern const char kEnableTLS13EarlyDataName[];
 extern const char kEnableTLS13EarlyDataDescription[];
 
