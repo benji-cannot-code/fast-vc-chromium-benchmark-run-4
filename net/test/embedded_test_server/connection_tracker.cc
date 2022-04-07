06f7302fa9f95d6af26afb3484b3cf54757bd6e7
Antonio Sartori <antoniosartori@chromium.org>
2022-04-07 05:30:44 +0000

    [test] Fix ConnectionTracker for non-connected sockets
    This is a tentative fix for the flakiness of
    WorkerFromAnonymousIframeNikBrowserTest.SharedWorkerRequestIsDoneWithPartitionedNetworkState.
    The test was flaky because the ConnectionTracker was not expecting a
    closed socket. With this change, the ConnectionTracker will just
    ignore closed sockets. Let's see if this fixes the test, or if there
    is another underlying issue.
    
    Bug: 1309684
    Change-Id: I4b2cf25b954e8a882b2547541d1f7b7fb1b902f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564008
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989782}

--- net/test/embedded_test_server/connection_tracker.cc
+++ net/test/embedded_test_server/connection_tracker.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-uint16_t GetPort(const net::StreamSocket& connection) {
+bool GetPort(const net::StreamSocket& connection, uint16_t* port) {
   // Gets the remote port of the peer, since the local port will always be
   // the port the test server is listening on. This isn't strictly correct -
   // it's possible for multiple peers to connect with the same remote port
@@ -22,8 +22,11 @@ uint16_t GetPort(const net::StreamSocket& connection) {
   // two connections. This also would be problematic if the OS reused ports,
   // but that's not something to worry about for these tests.
   net::IPEndPoint address;
-  EXPECT_EQ(net::OK, connection.GetPeerAddress(&address));
-  return address.port();
+  int result = connection.GetPeerAddress(&address);
+  if (result != net::OK)
+    return false;
+  *port = address.port();
+  return true;
 }
 
 }  // namespace
@@ -124,10 +127,12 @@ ConnectionTracker::ConnectionListener::~ConnectionListener() = default;
 std::unique_ptr<net::StreamSocket>
 ConnectionTracker::ConnectionListener::AcceptedSocket(
     std::unique_ptr<net::StreamSocket> connection) {
-  uint16_t port = GetPort(*connection);
-  task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&ConnectionTracker::AcceptedSocketWithPort,
-                                base::Unretained(tracker_), port));
+  uint16_t port;
+  if (GetPort(*connection, &port)) {
+    task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&ConnectionTracker::AcceptedSocketWithPort,
+                                  base::Unretained(tracker_), port));
+  }
   return connection;
 }
 
@@ -140,10 +145,12 @@ void ConnectionTracker::ConnectionListener::ReadFromSocket(
   // the sockets of the test server are being flushed and disconnected.
   if (rv <= 0)
     return;
-  uint16_t port = GetPort(connection);
-  task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&ConnectionTracker::ReadFromSocketWithPort,
-                                base::Unretained(tracker_), port));
+  uint16_t port;
+  if (GetPort(connection, &port)) {
+    task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&ConnectionTracker::ReadFromSocketWithPort,
+                                  base::Unretained(tracker_), port));
+  }
 }
 
 }  // namespace test_server
