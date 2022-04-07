ee1fd4afb6807f5e527b27b4c10426e357498f9f
Abhishek Gupta <aabhigupta@google.com>
2022-04-07 05:48:56 +0000

    Fallback to UserModel if key not present in UserData.
    Fail if different values present in UserModel and UserData.
    
    Bug: b/228008912
    Change-Id: I1b02deea6d117325886f010cee550d90102aeba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572664
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Abhishek Gupta <aabhigupta@google.com>
    Cr-Commit-Position: refs/heads/main@{#989787}

--- components/autofill_assistant/browser/user_data_util.h
+++ components/autofill_assistant/browser/user_data_util.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/metrics.h"
 #include "components/autofill_assistant/browser/service.pb.h"
 #include "components/autofill_assistant/browser/user_data.h"
+#include "components/autofill_assistant/browser/user_model.h"
 #include "components/autofill_assistant/browser/web/element_finder.h"
 #include "components/autofill_assistant/browser/website_login_manager.h"
 
@@ -137,6 +138,7 @@ void GetPasswordManagerValue(
 // fails with |PRECONDITION_FAILED|.
 ClientStatus GetClientMemoryStringValue(const std::string& client_memory_key,
                                         const UserData* user_data,
+                                        const UserModel* user_model,
                                         std::string* out_value);
 
 // Take a |text_value| and resolve its content to a string. Reports the result
