2c7cc91bc7e9239125a143498de5c7e12990f207
Ashley Prasad <ashleydp@google.com>
2022-04-07 20:14:46 +0000

    diagnostics: use diagnostics_log_controller for session log
    - Add condition which uses the DiagnosticsLogController when the flag is
      enabled to generated the session log.
    - Test session log generated matches expected content from
      DiagnosticsLogController implementation.
    - Add ash as a DEPs of unittests for testing purposes.
    
    Bug: b:225191282
    Test: Ran ash_webui_unittests. Deployed code and validated log contents when flag enabled and disabled.
    Change-Id: I914b205a6ecb29610ddd38709929828aa708c849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565654
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#990086}

--- ash/webui/diagnostics_ui/backend/session_log_handler.cc
+++ ash/webui/diagnostics_ui/backend/session_log_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/holding_space/holding_space_client.h"
+#include "ash/system/diagnostics/diagnostics_log_controller.h"
 #include "ash/system/diagnostics/networking_log.h"
 #include "ash/system/diagnostics/routine_log.h"
 #include "ash/system/diagnostics/telemetry_log.h"
@@ -92,11 +93,24 @@ void SessionLogHandler::RegisterMessages() {
 void SessionLogHandler::FileSelected(const base::FilePath& path,
                                      int index,
                                      void* params) {
-  task_runner_->PostTaskAndReplyWithResult(
-      FROM_HERE,
-      base::BindOnce(&SessionLogHandler::CreateSessionLog,
-                     base::Unretained(this), path),
-      base::BindOnce(&SessionLogHandler::OnSessionLogCreated, weak_ptr_, path));
+  // TODO(b/226574520): Remove SessionLogHandler::CreateSessionLog and
+  // condition as part of flag clean up.
+  if (ash::features::IsLogControllerForDiagnosticsAppEnabled()) {
+    task_runner_->PostTaskAndReplyWithResult(
+        FROM_HERE,
+        base::BindOnce(
+            &DiagnosticsLogController::GenerateSessionLogOnBlockingPool,
+            base::Unretained(DiagnosticsLogController::Get()), path),
+        base::BindOnce(&SessionLogHandler::OnSessionLogCreated, weak_ptr_,
+                       path));
+  } else {
+    task_runner_->PostTaskAndReplyWithResult(
+        FROM_HERE,
+        base::BindOnce(&SessionLogHandler::CreateSessionLog,
+                       base::Unretained(this), path),
+        base::BindOnce(&SessionLogHandler::OnSessionLogCreated, weak_ptr_,
+                       path));
+  }
   select_file_dialog_.reset();
 }
 
