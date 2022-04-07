f3275ee6fbd33f8c5918081bf075a32ca9eef226
John Lee <johntlee@chromium.org>
2022-04-07 00:02:04 +0000

    WebUI: Add an event to CrA11yAnnouncer
    This CL adds an event to CrA11yAnnouncer for when a message or messages
    are added to the live region. This allows for tests to listen for the
    event to test that messages were sent and can replace current tests
    that listen for iron-announce events.
    
    Bug: 1312084
    Change-Id: Ie261d6726f7cfb06b46066177bcfc71ad9f05a68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572985
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989687}

--- ui/webui/resources/cr_elements/cr_a11y_announcer/cr_a11y_announcer.ts
+++ ui/webui/resources/cr_elements/cr_a11y_announcer/cr_a11y_announcer.ts
@@ -12,6 +12,16 @@ import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/poly
  * @fileoverview
  */
 
+type CrA11yAnnouncerMessagesSentEvent = CustomEvent<{
+  messages: string[],
+}>;
+
+declare global {
+  interface HTMLElementEventMap {
+    'cr-a11y-announcer-messages-sent': CrA11yAnnouncerMessagesSentEvent;
+  }
+}
+
 /**
  * 150ms seems to be around the minimum time required for screen readers to
  * read out consecutively queued messages.
@@ -79,6 +89,12 @@ export class CrA11yAnnouncerElement extends PolymerElement {
         div.textContent = message;
         messagesDiv.appendChild(div);
       }
+
+      // Dispatch a custom event to allow consumers to know when certain alerts
+      // have been sent to the screen reader.
+      this.dispatchEvent(new CustomEvent(
+          'cr-a11y-announcer-messages-sent',
+          {bubbles: true, detail: {messages: this.messages_.slice()}}));
 
       this.messages_.length = 0;
       this.currentTimeout_ = null;
