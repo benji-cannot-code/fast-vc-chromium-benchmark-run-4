64875edb5ba28e64dffc05cfca5b6d5898e15dc3
Peter Kasting <pkasting@chromium.org>
2022-04-07 02:35:28 +0000

    Remove GridLayout from sharesheet_bubble_view.cc.
    Bug: 1261301
    Change-Id: I79887825037f7d5458124388887d341a8e29d217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573052
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989756}

--- chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view.h
+++ chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 namespace views {
-class GridLayout;
+class TableLayoutView;
 class Separator;
 }  // namespace views
 
@@ -84,8 +84,8 @@ class SharesheetBubbleView : public views::BubbleDialogDelegateView,
   std::unique_ptr<views::View> MakeScrollableTargetView(
       std::vector<TargetInfo> targets);
   void PopulateLayoutsWithTargets(std::vector<TargetInfo> targets,
-                                  views::GridLayout* default_layout,
-                                  views::GridLayout* expanded_layout);
+                                  views::TableLayoutView* default_view,
+                                  views::TableLayoutView* expanded_view);
   void ExpandButtonPressed();
   void AnimateToExpandedState();
   void TargetButtonPressed(TargetInfo target);
