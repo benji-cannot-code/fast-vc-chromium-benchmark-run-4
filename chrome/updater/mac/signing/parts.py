7ef0485909d3742b2236428c3ae7b068b3ddb617
Joshua Pawlicki <waffles@chromium.org>
2022-04-07 16:20:52 +0000

    Updater: remove app validation for UpdaterSetup.
    UpdaterSetup is not an app, just an executable.
    
    Change-Id: I334be4ff59ad9367a8a4e540c072827be9d8efbc
    Fixed: 1313125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575299
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989962}

--- chrome/updater/mac/signing/parts.py
+++ chrome/updater/mac/signing/parts.py
@@ -83,5 +83,4 @@ def sign_all(paths, config):
     for part in parts:
         signing.sign_part(paths, config, part)
         signing.verify_part(paths, part)
-    signing.validate_app(paths, config, parts[-2])
     signing.validate_app(paths, config, parts[-1])
