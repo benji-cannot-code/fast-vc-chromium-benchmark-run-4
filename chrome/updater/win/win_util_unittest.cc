9c46adc1a4e28d5ea9b406491d28b3fc8f81d3ab
S. Ganesh <ganesh@chromium.org>
2022-04-07 23:18:42 +0000

    Add unit test for WinUtil.RunElevated
    Bug: 1254126
    Change-Id: Ic5e04a25656ead2f141f9884088e34e36ccc1ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564657
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Auto-Submit: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990179}

--- chrome/updater/win/win_util_unittest.cc
+++ chrome/updater/win/win_util_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/updater/win/win_util.h"
 
+#include <shlobj.h>
 #include <windows.h>
 
 #include <string>
@@ -119,6 +120,21 @@ TEST(WinUtil, ShellExecuteAndWait) {
   EXPECT_HRESULT_SUCCEEDED(ShellExecuteAndWait(
       GetTestProcessCommandLine(GetTestScope()).GetProgram(), {}, {},
       &exit_code));
+  EXPECT_EQ(exit_code, 0UL);
+}
+
+TEST(WinUtil, RunElevated) {
+  // TODO(crbug.com/1314521): Click on UAC prompts in Updater tests that require
+  // elevation
+  if (!::IsUserAnAdmin())
+    return;
+
+  DWORD exit_code = 0;
+  const base::CommandLine test_process_cmd_line =
+      GetTestProcessCommandLine(GetTestScope());
+  EXPECT_HRESULT_SUCCEEDED(
+      RunElevated(test_process_cmd_line.GetProgram(),
+                  test_process_cmd_line.GetArgumentsString(), &exit_code));
   EXPECT_EQ(exit_code, 0UL);
 }
 
