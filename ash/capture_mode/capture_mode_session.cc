b7d09c17077cdcbf1ac8de3d7f13d1439105e199
conniekxu <conniekxu@chromium.org>
2022-04-07 22:57:36 +0000

    capture_selfie_cam: Update capture UIs' opacity on keyboard navigation
    This CL implements the logic to update capture UIs (capture bar, capture
    label) opacity on the keyboard navigation. When current focus is on the
    capture UI, it should have full opaque even it's overlapped with camera
    preview.
    
    Fixed: 1309243
    Test: manual, added unit-tests
    Change-Id: I12ba483e8e40912f042232bd84237e0b2602a22b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573172
    Reviewed-by: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990160}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -1057,6 +1057,7 @@ void CaptureModeSession::OnKeyEvent(ui::KeyEvent* event) {
       else if (can_exit_on_escape_)
         controller_->Stop();  // |this| is destroyed here.
 
+      MaybeUpdateCaptureUisOpacity();
       return;
     }
 
@@ -1071,8 +1072,10 @@ void CaptureModeSession::OnKeyEvent(ui::KeyEvent* event) {
       event->StopPropagation();
       event->SetHandled();
 
-      if (focus_cycler_->OnSpacePressed())
+      if (focus_cycler_->OnSpacePressed()) {
+        MaybeUpdateCaptureUisOpacity();
         return;
+      }
 
       // Create a default region if we are in region mode and there is no
       // existing region.
@@ -1087,6 +1090,7 @@ void CaptureModeSession::OnKeyEvent(ui::KeyEvent* event) {
       event->StopPropagation();
       event->SetHandled();
       focus_cycler_->AdvanceFocus(/*reverse=*/event->IsShiftDown());
+      MaybeUpdateCaptureUisOpacity();
       return;
     }
 
@@ -1370,6 +1374,14 @@ void CaptureModeSession::MaybeUpdateCaptureUisOpacity(
         opacity = 0.f;
         continue;
       }
+
+      if (focus_cycler_->CaptureBarFocused())
+        continue;
+    }
+
+    if (widget == capture_label_widget_.get() &&
+        focus_cycler_->CaptureLabelFocused()) {
+      continue;
     }
 
     if (IsWidgetOverlappedWithCameraPreview(widget))
