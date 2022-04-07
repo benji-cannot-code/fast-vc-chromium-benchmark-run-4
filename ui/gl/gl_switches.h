75f675c772b7bfe08db58c68642cd9b7b8aa4b20
Andrew Naumov <andrew-naumov@yandex-team.ru>
2022-04-07 14:10:04 +0000

    Create a feature flag DirectCompositionVerifyDrawOffset
    Chromium does not handle properly the case when
    IDCompositionSurface::BeginDraw returns updateOffset that is not equal
    to the upper-left corner of updateRect.
    Some shaders e.g. skia's effects: GrFragmentProcessor,
    CircularRRectEffect, and EllipticalRRectEffect do not work correctly,
    because they compare coordinates with gl_FragCoord.
    
    This feature verifies the draw offset by rendering to offscreen texture
    instead of direct composition surface.
    
    Bug: 1269749
    Change-Id: I7acdc9c1f89db43a99c540fc01886e28afb60e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554067
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Andrew Naumov <andrew-naumov@yandex-team.ru>
    Cr-Commit-Position: refs/heads/main@{#989901}

--- ui/gl/gl_switches.h
+++ ui/gl/gl_switches.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Defines all the command-line switches used by ui/gl.
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "ui/gl/gl_export.h"
 
 namespace gl {
@@ -99,6 +100,9 @@ GL_EXPORT extern const base::Feature kDCompTripleBufferVideoSwapChain;
 GL_EXPORT extern const base::Feature kDirectCompositionForceFullDamage;
 GL_EXPORT extern const base::Feature kDirectCompositionGpuVSync;
 GL_EXPORT extern const base::Feature kDirectCompositionLowLatencyPresentation;
+GL_EXPORT extern const base::Feature kDirectCompositionVerifyDrawOffset;
+GL_EXPORT extern const base::FeatureParam<int> kVerifyDrawOffsetX;
+GL_EXPORT extern const base::FeatureParam<int> kVerifyDrawOffsetY;
 GL_EXPORT extern const base::Feature kDirectCompositionSoftwareOverlays;
 GL_EXPORT extern const base::Feature kDefaultANGLEOpenGL;
 GL_EXPORT extern const base::Feature kDefaultANGLEMetal;
