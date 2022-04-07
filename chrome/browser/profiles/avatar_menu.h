c6947c3f8c7209b1be974769042c5902c7ec2a3b
David Sanders <dsanders11@ucsbalum.com>
2022-04-07 05:51:47 +0000

    Remove unused includes in //chrome/browser/profiles/avatar_menu.h
    Also adds some includes to other files which were
    getting them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: Ib4aab8c70a740e66195e4517c6a3ab8039c3a352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575151
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#989788}

--- chrome/browser/profiles/avatar_menu.h
+++ chrome/browser/profiles/avatar_menu.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/common/buildflags.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "ui/gfx/image/image.h"
 
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
