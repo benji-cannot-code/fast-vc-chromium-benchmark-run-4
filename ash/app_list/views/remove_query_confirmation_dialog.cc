cce891b94c535e74d9dd31ab2101bfd8d3e3cefd
Yulun Wu <yulunwu@chromium.org>
2022-04-07 22:06:41 +0000

    Add search result title to remove dialog.
    This CL also updates the dialog to be a removal dialog instead of a
    deletion dialog. Screenshot can be found in bug.
    
    Bug: 1115418
    Change-Id: I15a15968c6709d44119df7fb97e5a8da433d7e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575998
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990144}

--- ash/app_list/views/remove_query_confirmation_dialog.cc
+++ ash/app_list/views/remove_query_confirmation_dialog.cc
@@ -47,7 +47,8 @@ constexpr int kMarginBetweenButtons = 8;
 }  // namespace
 
 RemoveQueryConfirmationDialog::RemoveQueryConfirmationDialog(
-    RemovalConfirmationCallback confirm_callback)
+    RemovalConfirmationCallback confirm_callback,
+    const std::u16string& result_title)
     : confirm_callback_(std::move(confirm_callback)) {
   SetModalType(ui::MODAL_TYPE_WINDOW);
 
@@ -76,8 +77,9 @@ RemoveQueryConfirmationDialog::RemoveQueryConfirmationDialog(
   title_->GetViewAccessibility().OverrideIsIgnored(true);
 
   // Add dialog body.
-  body_ = AddChildView(std::make_unique<views::Label>(
-      l10n_util::GetStringUTF16(IDS_REMOVE_ZERO_STATE_SUGGESTION_DETAILS)));
+  body_ =
+      AddChildView(std::make_unique<views::Label>(l10n_util::GetStringFUTF16(
+          IDS_REMOVE_ZERO_STATE_SUGGESTION_DETAILS, result_title)));
   body_->SetProperty(views::kMarginsKey,
                      gfx::Insets::TLBR(kMarginBetweenTitleAndBody, 0,
                                        kMarginBetweenBodyAndButtons, 0));
