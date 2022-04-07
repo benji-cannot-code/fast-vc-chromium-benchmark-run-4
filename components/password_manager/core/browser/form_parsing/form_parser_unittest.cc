82eb92ef38faa43f62791a9ebf064075eeea861d
Elias <eliaskh@chromium.org>
2022-04-07 17:57:27 +0000

    [Passwords] Cleanup kTreatNewPasswordHeuristicsAsReliable.
    The feature is obsolete.
    
    Bug: 1313893
    Change-Id: If70732e81761e956833327673c68539239cb1311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572638
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990011}

--- components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
+++ components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
@@ -455,9 +455,6 @@ TEST(FormParserTest, SkipNotTextFields) {
 }
 
 TEST(FormParserTest, OnlyPasswordFields) {
-  const bool kTreatNewPasswordHeuristicsAsReliable =
-      base::FeatureList::IsEnabled(
-          features::kTreatNewPasswordHeuristicsAsReliable);
   CheckTestData({
       {
           .description_for_logging = "1 password field",
@@ -481,7 +478,7 @@ TEST(FormParserTest, OnlyPasswordFields) {
                    .value = u"pw",
                    .form_control_type = "password"},
               },
-          .is_new_password_reliable = kTreatNewPasswordHeuristicsAsReliable,
+          .is_new_password_reliable = false,
       },
       {
           .description_for_logging =
@@ -495,7 +492,7 @@ TEST(FormParserTest, OnlyPasswordFields) {
                    .value = u"pw2",
                    .form_control_type = "password"},
               },
-          .is_new_password_reliable = kTreatNewPasswordHeuristicsAsReliable,
+          .is_new_password_reliable = false,
       },
       {
           .description_for_logging =
@@ -512,7 +509,7 @@ TEST(FormParserTest, OnlyPasswordFields) {
                    .value = u"pw2",
                    .form_control_type = "password"},
               },
-          .is_new_password_reliable = kTreatNewPasswordHeuristicsAsReliable,
+          .is_new_password_reliable = false,
       },
       {
           .description_for_logging = "3 password fields with different values",
