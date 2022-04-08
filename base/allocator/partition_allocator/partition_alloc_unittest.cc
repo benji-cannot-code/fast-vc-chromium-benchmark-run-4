c530514ab3900cbc661e04c2f5af61ea48b709ab
Kalvin Lee <kdlee@chromium.org>
2022-04-08 03:08:31 +0000

    [PA] Namespace migration (31-omake of N)
    This CL trivially qualifies `CPU` as `base::CPU` inside the
    PartitionAlloc unit test. This is being done to separate this slightly
    noisy patch from migration CL no. 32.
    
    Bug: 1288247
    Change-Id: Id80e5693502860d33a6b4bb496b21f1baba655c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574354
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990255}

--- base/allocator/partition_allocator/partition_alloc_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_unittest.cc
@@ -2222,7 +2222,7 @@ TEST_P(PartitionAllocDeathTest, DirectMapGuardPages) {
 TEST_P(PartitionAllocTest, MTEProtectsFreedPtr) {
   // This test checks that Arm's memory tagging extension is correctly
   // protecting freed pointers. Writes to a freed pointer should cause a crash.
-  CPU cpu;
+  base::CPU cpu;
   if (!cpu.has_mte()) {
     // This test won't pass on non-MTE systems.
     GTEST_SKIP();
@@ -2256,7 +2256,7 @@ TEST_P(PartitionAllocTest, MTEProtectsFreedPtr) {
 #if !BUILDFLAG(USE_BACKUP_REF_PTR) && defined(PA_HAS_FREELIST_SHADOW_ENTRY)
 
 TEST_P(PartitionAllocDeathTest, UseAfterFreeDetection) {
-  CPU cpu;
+  base::CPU cpu;
   void* data = allocator.root()->Alloc(100, "");
   allocator.root()->Free(data);
 
@@ -2267,7 +2267,7 @@ TEST_P(PartitionAllocDeathTest, UseAfterFreeDetection) {
 }
 
 TEST_P(PartitionAllocDeathTest, FreelistCorruption) {
-  CPU cpu;
+  base::CPU cpu;
   const size_t alloc_size = 2 * sizeof(void*);
   void** fake_freelist_entry =
       static_cast<void**>(allocator.root()->Alloc(alloc_size, ""));
@@ -2291,7 +2291,7 @@ TEST_P(PartitionAllocDeathTest, FreelistCorruption) {
 // With DCHECK_IS_ON(), cookie already handles off-by-one detection.
 #if !DCHECK_IS_ON()
 TEST_P(PartitionAllocDeathTest, OffByOneDetection) {
-  CPU cpu;
+  base::CPU cpu;
   const size_t alloc_size = 2 * sizeof(void*);
   char* array = static_cast<char*>(allocator.root()->Alloc(alloc_size, ""));
   if (cpu.has_mte()) {
@@ -2314,7 +2314,7 @@ TEST_P(PartitionAllocDeathTest, OffByOneDetection) {
 }
 
 TEST_P(PartitionAllocDeathTest, OffByOneDetectionWithRealisticData) {
-  CPU cpu;
+  base::CPU cpu;
   const size_t alloc_size = 2 * sizeof(void*);
   void** array = static_cast<void**>(allocator.root()->Alloc(alloc_size, ""));
   char valid;
