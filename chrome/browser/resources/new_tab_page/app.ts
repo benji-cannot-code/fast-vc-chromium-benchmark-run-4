2dc259ff1d2a1f45dacc7f564d1a7ccb392b4492
Paul Adedeji <pauladedeji@google.com>
2022-04-08 01:50:52 +0000

    [ntp][fre] Adjust size of "Customize Chrome" button to FRE/modules
    Turn customize chrome button to pencil when it is a certain distance from modules whether or not they're encased in the FRE. This CL also includes changes to modules.ts to align variable/function names with those added to app.ts.
    
    Modules only on screen (demo): http://recall/-/c4JPivcXNOuBKuUlZnNzlk/fkcpXZ6aN0qVXR0T1l8oRt
    FRE+modules on screen (demo): http://recall/-/c4JPivcXNOuBKuUlZnNzlk/g0Akvg0ssi6vGNizM61qs1
    
    Change-Id: Icc81412a280e7f770a9bd63cbfd21a1d74320f7d
    Bug: 1312098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564064
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#990229}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -192,6 +192,11 @@ export class AppElement extends PolymerElement {
         value: () => loadTimeData.getBoolean('shortcutsEnabled'),
       },
 
+      modulesFreShown: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
+
       modulesRedesignedLayoutEnabled_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('modulesRedesignedLayoutEnabled'),
@@ -222,6 +227,11 @@ export class AppElement extends PolymerElement {
       modulesLoaded_: {
         type: Boolean,
         value: false,
+      },
+
+      modulesShownToUser: {
+        type: Boolean,
+        reflectToAttribute: true,
       },
 
       /**
@@ -262,12 +272,14 @@ export class AppElement extends PolymerElement {
   private logoEnabled_: boolean;
   private oneGoogleBarEnabled_: boolean;
   private shortcutsEnabled_: boolean;
+  private modulesFreShown: boolean;
   private modulesRedesignedLayoutEnabled_: boolean;
   private middleSlotPromoEnabled_: boolean;
   private modulesEnabled_: boolean;
   private modulesRedesignedEnabled_: boolean;
   private middleSlotPromoLoaded_: boolean;
   private modulesLoaded_: boolean;
+  private modulesShownToUser: boolean;
   private promoAndModulesLoaded_: boolean;
   private lazyRender_: boolean;
 
