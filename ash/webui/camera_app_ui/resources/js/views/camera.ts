81dcd2c940b79ceae387a7b7f27273b4371b9f2a
Pi-Hsun Shih <pihsun@chromium.org>
2022-04-08 11:48:26 +0000

    CCA: ESLint: Extend generic parameter custom rule to class property
    Extend the original custom rule that detects `const a: Map<S, T> = new
    Map();` to cover class property too.
    
    Bug: None
    Test: manually
    Test: cca.py lint
    Change-Id: Ie925c5a9d333500cdc70d80a9b68746ebc88f33e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578197
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990343}

--- ash/webui/camera_app_ui/resources/js/views/camera.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera.ts
@@ -109,7 +109,7 @@ export class Camera extends View implements CameraViewUI {
   /**
    * Event for tracking camera availability state.
    */
-  private cameraReady: WaitableEvent = new WaitableEvent();
+  private cameraReady = new WaitableEvent();
 
   /**
    * Promise for the current take of photo or recording.
