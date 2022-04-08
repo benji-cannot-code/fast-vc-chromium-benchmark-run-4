f1004559bdd354da20fffa9ec080399dbeccccdc
Henrique Nakashima <hnakashima@chromium.org>
2022-04-08 18:28:21 +0000

    [Android] Review @DisabledTests in org.chromium.chrome.features
    - Correct the bug linked to some tests.
    - Delete a test that the linked bug says is dead.
    
    This is a preparation to run disabled tests in Test Reviver.
    
    Bug: 1306975,1051961
    Change-Id: I6520a5598aa7489f2f7bbb54ec4f6831844defc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575070
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990503}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartFeedTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartFeedTest.java
@@ -87,7 +87,7 @@ public class InstantStartFeedTest {
     // clang-format off
     @CommandLineFlags.Add({ChromeSwitches.DISABLE_NATIVE_INITIALIZATION,
         INSTANT_START_TEST_BASE_PARAMS})
-    @DisabledTest(message = "https://crbug.com/1263928")
+    @DisabledTest(message = "https://crbug.com/1314456")
     public void testFeedPlaceholderFromColdStart() {
         // clang-format on
         StartSurfaceTestUtils.startMainActivityFromLauncher(mActivityTestRule);
@@ -121,7 +121,7 @@ public class InstantStartFeedTest {
     @Test
     @LargeTest
     @CommandLineFlags.Add({INSTANT_START_TEST_BASE_PARAMS})
-    @DisabledTest(message = "https://crbug.com/1263928")
+    @DisabledTest(message = "https://crbug.com/1314456")
     public void testCachedFeedVisibility() {
         StartSurfaceTestUtils.startMainActivityFromLauncher(mActivityTestRule);
         mActivityTestRule.waitForActivityNativeInitializationComplete();
@@ -171,7 +171,7 @@ public class InstantStartFeedTest {
     // clang-format off
     @CommandLineFlags.Add({ChromeSwitches.DISABLE_NATIVE_INITIALIZATION,
         INSTANT_START_TEST_BASE_PARAMS})
-    @DisabledTest(message = "https://crbug.com/1263928")
+    @DisabledTest(message = "https://crbug.com/1314456")
     public void testShowFeedPlaceholder() {
         // clang-format on
         StartSurfaceConfiguration.setFeedVisibilityForTesting(true);
