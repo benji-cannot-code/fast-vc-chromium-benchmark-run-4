8d7987b7c6518e57e4735052313df4912fe125d7
Igor Ruvinov <igorruvinov@chromium.org>
2022-04-08 19:43:19 +0000

    Re-enable flaky test WebBluetoothPolicyTest.Block on most platforms.
    This test appears to be flaky on  mac11-arm64-rel. Out of 1000 runs, it
    timed out 4 times: https://bit.ly/375A6HJ
    
    No failures were noticed on other platforms, so the test is being
    re-enabled on all platforms except ARM64 Mac.
    
    Bug: 1061063
    Change-Id: I6e15e03a6fbfc4657f2f872e41968878947ea82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577581
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990535}

--- chrome/browser/policy/test/content_settings_policy_browsertest.cc
+++ chrome/browser/policy/test/content_settings_policy_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/strcat.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
@@ -171,11 +172,11 @@ class WebBluetoothPolicyTest : public PolicyTest {
 };
 
 // crbug.com/1061063
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
 #define MAYBE_Block DISABLED_Block
 #else
 #define MAYBE_Block Block
-#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#endif  // BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
 IN_PROC_BROWSER_TEST_F(WebBluetoothPolicyTest, MAYBE_Block) {
   // Fake the BluetoothAdapter to say it's present.
   scoped_refptr<device::MockBluetoothAdapter> adapter =
