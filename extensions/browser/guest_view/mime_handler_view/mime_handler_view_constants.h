6ecd4c2c9ee86cf2791ccba54afb3a14d47d7abe
Kevin McNee <mcnee@chromium.org>
2022-04-08 22:26:24 +0000

    Rename or remove several parameters involved in creation of MimeHandler streams
    These appear to be remnants of the removed BrowserPlugin implementation
    and are now obsolete or misleadingly named.
    
    Bug: 659750
    Change-Id: I6ed410d3971e552ee9500da3f9bf60b43fae0677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572172
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Zork <zork@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990630}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_constants.h
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_constants.h
@@ -13,8 +13,8 @@ namespace mime_handler_view {
 // API namespace.
 extern const char kAPINamespace[];
 
-// Other.
-extern const char kViewId[];
+// Create parameters.
+extern const char kStreamId[];
 
 }  // namespace mime_handler_view
 
