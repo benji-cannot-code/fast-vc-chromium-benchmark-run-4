566b699ada611ca9fd08d480e2ecc160303fc064
William Mahon <wmahon@google.com>
2022-04-08 23:19:28 +0000

    ozone: evdev: Refactor input_device_factory_evdev
    Move OpenInputDevice and CreateConverter functions from
    input_device_factory_evdev to a new class in order to allow for dependency injection.
    
    Change-Id: Ib20e38740a722ffee0d748c0cadf9448d49ecdf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578112
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Commit-Queue: William Mahon <wmahon@google.com>
    Cr-Commit-Position: refs/heads/main@{#990643}

---
+++ ui/events/ozone/evdev/input_device_opener_evdev.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_EVENTS_OZONE_EVDEV_INPUT_DEVICE_OPENER_EVDEV_H_
+#define UI_EVENTS_OZONE_EVDEV_INPUT_DEVICE_OPENER_EVDEV_H_
+
+#include "ui/events/ozone/evdev/input_device_opener.h"
+
+#include "base/files/scoped_file.h"
+#include "ui/events/ozone/evdev/event_device_info.h"
+
+namespace ui {
+
+class COMPONENT_EXPORT(EVDEV) InputDeviceOpenerEvdev
+    : public InputDeviceOpener {
+ public:
+  InputDeviceOpenerEvdev() = default;
+
+  InputDeviceOpenerEvdev(const InputDeviceOpenerEvdev&) = delete;
+  InputDeviceOpenerEvdev& operator=(const InputDeviceOpenerEvdev&) = delete;
+
+  ~InputDeviceOpenerEvdev() override = default;
+
+  std::unique_ptr<EventConverterEvdev> OpenInputDevice(
+      const OpenInputDeviceParams& params) override;
+};
+}  // namespace ui
+
+#endif  // UI_EVENTS_OZONE_EVDEV_INPUT_DEVICE_OPENER_EVDEV_H_
