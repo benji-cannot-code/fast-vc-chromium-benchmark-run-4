dff7e5288ef97f4af00e97d9f83f55981ab58d0c
jhtin <jhtin@chromium.org>
2022-04-08 06:19:43 +0000

    NO-OP: Rename GetEnabledSuggestions to FetchEnabledSuggestionsThen for clarity
    Makes it clearer that the function does async fetch logic and then calls the callback after the results are received.
    
    Change-Id: Ic0d5f0e5f234c8d72db63f79868591d73ce6512d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574507
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990281}

--- chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
+++ chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
@@ -275,7 +275,7 @@ void GetBrowserUrl(GetBrowserUrlCallback callback) {
 }
 
 void ReturnEnabledSuggestions(
-    AssistiveSuggesterSwitch::GetEnabledSuggestionsCallback callback,
+    AssistiveSuggesterSwitch::FetchEnabledSuggestionsCallback callback,
     const absl::optional<GURL>& current_url) {
   if (!current_url.has_value()) {
     std::move(callback).Run(AssistiveSuggesterSwitch::EnabledSuggestions{});
@@ -318,8 +318,8 @@ bool AssistiveSuggesterClientFilter::IsPersonalInfoSuggestionAllowed() {
   return IsAllowedUrlOrAppForPersonalInfoSuggestion();
 }
 
-void AssistiveSuggesterClientFilter::GetEnabledSuggestions(
-    GetEnabledSuggestionsCallback callback) {
+void AssistiveSuggesterClientFilter::FetchEnabledSuggestionsThen(
+    FetchEnabledSuggestionsCallback callback) {
   GetBrowserUrl(base::BindOnce(ReturnEnabledSuggestions, std::move(callback)));
 }
 
