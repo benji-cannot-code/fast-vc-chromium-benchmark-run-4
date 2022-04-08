05ded10f5b2f85c7bc5ca746d388a3014338dd6d
Piotr Bialecki <bialpio@chromium.org>
2022-04-08 23:33:50 +0000

    Fix DCHECK in skia renderer when resizing captured tab
    When resizing captured window, the output rectangle of the drawn render
    pass may be different than what the caller of CopyOutputRequest API
    expected, and thus it may not meet the requirements of the pixel format.
    This could trigger a DCHECK when reading back the results to populate
    the CopyOutputResponse.
    
    Changes:
    - Move `MinimallyShrinkRectForI420()` to media_util.[h/cc].
    - Adjust result_selection on RenderPassGeometry to account for pixel
      format requirements.
    - Minor: pass RenderPassGeometry by const-ref to `CopyOutput()` in
      `SkiaOutputSurfaceImplOnGpu`
    
    Change-Id: I47c0f0b929449d6d90b936ae04ce5b7e643c3252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551199
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990657}

--- media/base/video_util_unittest.cc
+++ media/base/video_util_unittest.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <memory>
 
+#include "media/base/limits.h"
 #include "media/base/video_frame.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace {
 
@@ -409,6 +411,58 @@ TEST_F(VideoUtilTest, ComputeLetterboxRegionForI420) {
   EXPECT_TRUE(ComputeLetterboxRegionForI420(
                   gfx::Rect(0, 0, 2000000000, 2000000000), gfx::Size(0, 0))
                   .IsEmpty());
+}
+
+// Tests the MinimallyShrinkRectForI420 function.
+TEST_F(VideoUtilTest, MinimallyShrinkRectForI420) {
+  // A few no-ops:
+  EXPECT_EQ(gfx::Rect(2, 2, 100, 100),
+            MinimallyShrinkRectForI420(gfx::Rect(2, 2, 100, 100)));
+  EXPECT_EQ(gfx::Rect(2, -2, 100, 100),
+            MinimallyShrinkRectForI420(gfx::Rect(2, -2, 100, 100)));
+  EXPECT_EQ(gfx::Rect(-2, 2, 100, 100),
+            MinimallyShrinkRectForI420(gfx::Rect(-2, 2, 100, 100)));
+
+  // Origin has odd coordinates:
+  EXPECT_EQ(gfx::Rect(2, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(1, 1, 100, 100)));
+  EXPECT_EQ(gfx::Rect(0, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(-1, 1, 100, 100)));
+  EXPECT_EQ(gfx::Rect(2, 0, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(1, -1, 100, 100)));
+
+  // Size is odd:
+  EXPECT_EQ(gfx::Rect(2, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(2, 2, 99, 99)));
+  EXPECT_EQ(gfx::Rect(-2, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(-2, 2, 99, 99)));
+  EXPECT_EQ(gfx::Rect(2, -2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(2, -2, 99, 99)));
+
+  // Both are odd:
+  EXPECT_EQ(gfx::Rect(2, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(1, 1, 99, 99)));
+  EXPECT_EQ(gfx::Rect(0, 2, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(-1, 1, 99, 99)));
+  EXPECT_EQ(gfx::Rect(2, 0, 98, 98),
+            MinimallyShrinkRectForI420(gfx::Rect(1, -1, 99, 99)));
+
+  // Check the biggest rectangle that the function will accept:
+  constexpr int kMinDimension = -1 * limits::kMaxDimension;
+  if (limits::kMaxDimension % 2 == 0) {
+    EXPECT_EQ(gfx::Rect(kMinDimension, kMinDimension, 2 * limits::kMaxDimension,
+                        2 * limits::kMaxDimension),
+              MinimallyShrinkRectForI420(gfx::Rect(kMinDimension, kMinDimension,
+                                                   2 * limits::kMaxDimension,
+                                                   2 * limits::kMaxDimension)));
+  } else {
+    EXPECT_EQ(
+        gfx::Rect(kMinDimension + 1, kMinDimension + 1,
+                  2 * limits::kMaxDimension - 2, 2 * limits::kMaxDimension - 2),
+        MinimallyShrinkRectForI420(gfx::Rect(kMinDimension, kMinDimension,
+                                             2 * limits::kMaxDimension,
+                                             2 * limits::kMaxDimension)));
+  }
 }
 
 TEST_F(VideoUtilTest, ScaleSizeToEncompassTarget) {
