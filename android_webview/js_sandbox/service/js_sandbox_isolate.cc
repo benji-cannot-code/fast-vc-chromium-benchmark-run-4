a35f61030536c7b1e19d1d280e3a2be07a43ccb1
Abhijith Nair <abhijithnair@chromium.org>
2022-04-08 17:44:49 +0000

    Refactor code structure of WebView js_sandbox
    - //aw/java to //aw/js_sandbox/java
    - //aw/renderer/js_sandbox to //aw/js_sandbox/service
    - //aw/tools/js_sandbox_tests to //aw/js_sandbox/javatests
    
    Bug: 1297672
    Change-Id: If6f3e988847e20b76e786de444f3f914e3c855ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571741
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990484}

--- android_webview/js_sandbox/service/js_sandbox_isolate.cc
+++ android_webview/js_sandbox/service/js_sandbox_isolate.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/renderer/js_sandbox/js_sandbox_isolate.h"
+#include "android_webview/js_sandbox/service/js_sandbox_isolate.h"
 
 #include <algorithm>
 #include <memory>
 #include <string>
 
-#include "android_webview/js_sandbox_jni_headers/JsSandboxIsolate_jni.h"
+#include "android_webview/js_sandbox/js_sandbox_jni_headers/JsSandboxIsolate_jni.h"
 #include "base/android/callback_android.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
