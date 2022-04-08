88f98972eceb52fa5e5637efa71d0c3c31af5556
Michael Wilson <mjwilson@chromium.org>
2022-04-08 01:37:38 +0000

    [WebAudio] Break ConstantSource node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: Idfe2133a972c083c23088d939967e1e4b67f189c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573432
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990226}

--- third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.h
+++ third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.h
@@ -31,14 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_AUDIO_SCHEDULED_SOURCE_NODE_H_
 
 #include <atomic>
+
 #include "base/memory/weak_ptr.h"
+#include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
 #include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
-
-namespace base {
-class SingleThreadTaskRunner;
-}
 
 namespace blink {
 
