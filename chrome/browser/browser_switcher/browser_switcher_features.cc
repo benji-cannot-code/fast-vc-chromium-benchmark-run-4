3c55d14da3505a051bece1b458ab4abad344c8b4
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-04-08 13:55:42 +0000

    [BrowserSwitcher] Enable NoneIsGreylist by default
    Bug: 1282233
    Change-Id: I153bab7ccbd8297bfc48c037b45761a0dea562f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574759
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990369}

--- chrome/browser/browser_switcher/browser_switcher_features.cc
+++ chrome/browser/browser_switcher/browser_switcher_features.cc
@@ -13,4 +13,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(crbug.com/1282233): Remove this flag once we're confident this
 // doesn't break customers.
 extern const base::Feature kBrowserSwitcherNoneIsGreylist{
-    "BrowserSwitcherNoneIsGreylist", base::FEATURE_DISABLED_BY_DEFAULT};
+    "BrowserSwitcherNoneIsGreylist", base::FEATURE_ENABLED_BY_DEFAULT};
