7e979b232a553997ba6a15568edf295e6e12a1c6
Rohan Pavone <rohpavone@chromium.org>
2022-04-08 23:24:46 +0000

    [fuchsia][size] Moves binary size scripts to python3
    Change-Id: I288f00c48ad002187f3882ae43c845480d12a2d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577586
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990652}

--- build/fuchsia/binary_sizes.py
+++ build/fuchsia/binary_sizes.py
@@ -1,13 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python2
+#!/usr/bin/env vpython3
 #
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 '''Implements Chrome-Fuchsia package binary size checks.'''
-
-from __future__ import division
-from __future__ import print_function
 
 import argparse
 import collections
