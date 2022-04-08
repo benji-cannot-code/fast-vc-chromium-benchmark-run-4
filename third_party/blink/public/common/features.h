a321c206e5f2d20efe51d0670debc81f0533ab4b
Ari Chivukula <arichiv@chromium.org>
2022-04-08 01:25:07 +0000

    [Save Data Client Hint] (1) Remove legacy feature
    This holdout group isn't being used. At the same time, the new client
    hint implementation does need a feature flag. Let's remove the old one
    while adding a new one because the style flips (holdout defaulting off
    vs enabled defaulting on). This feature is the same one the next CL will
    use to thread SaveData into the existing CH infra.
    
    This CL is part of a series:
    (1) Remove legacy feature
    (2) Implement as Client Hint
    (3) Add/Update WPTs
    
    Bug: 1293443
    Change-Id: Ia02cb646c52af4cf2fb5df28fa8bb915b64275c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576781
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990222}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -702,6 +702,8 @@ BLINK_COMMON_EXPORT extern const base::FeatureParam<bool> kPrewarmCursive;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<bool> kPrewarmFantasy;
 #endif
 
+BLINK_COMMON_EXPORT extern const base::Feature kClientHintsSaveData;
+
 }  // namespace features
 }  // namespace blink
 
--- third_party/blink/renderer/core/css/media_values.cc
+++ third_party/blink/renderer/core/css/media_values.cc
@@ -403,8 +403,7 @@ bool MediaValues::CalculatePrefersReducedData(LocalFrame* frame) {
     if (value.IsValid())
       return value.Id() == CSSValueID::kReduce;
   }
-  return (GetNetworkStateNotifier().SaveDataEnabled() &&
-          !frame->GetSettings()->GetDataSaverHoldbackWebApi());
+  return GetNetworkStateNotifier().SaveDataEnabled();
 }
 
 ForcedColors MediaValues::CalculateForcedColors(LocalFrame* frame) {
