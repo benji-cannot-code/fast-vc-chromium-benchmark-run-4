276c095663619c885262dfb647d60703bbe72717
Olga Sharonova <olka@chromium.org>
2022-04-08 17:08:19 +0000

    Add ProcessingAudioFifo UMA
    This CL introduces two new UMAs for the ProcessingAudioFifo:
    - Media.Audio.Capture.ProcessingAudioFifo.MaxUsage reports how full the
    ProcessingAudioFifo has gotten, over a logging period.
    - Media.Audio.Capture.ProcessingAudioFifo.Overruns reports how many
    buffers were dropped (up to a cap), due to the FIFO being full.
    
    These UMAs should help us determine an appropriate FIFO size.
    
    Authors: tguilbert@, olka@
    
    Bug: 1296149
    Change-Id: I3455c40683153a9ff342bb841eadcfece96a3b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571832
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990452}

--- services/audio/processing_audio_fifo.h
+++ services/audio/processing_audio_fifo.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/sequence_checker.h"
+#include "base/strings/string_piece.h"
 #include "base/synchronization/atomic_flag.h"
 #include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
@@ -33,10 +34,13 @@ class ProcessingAudioFifo {
   using ProcessAudioCallback = base::RepeatingCallback<
       void(const media::AudioBus&, base::TimeTicks, double, bool)>;
 
+  using LogCallback = base::RepeatingCallback<void(base::StringPiece)>;
+
   // |processing_callback| will only be called back on the processing thread.
   ProcessingAudioFifo(const media::AudioParameters& input_params,
                       int fifo_size,
-                      ProcessAudioCallback processing_callback);
+                      ProcessAudioCallback processing_callback,
+                      LogCallback log_callback);
 
   // Note: This synchronously waits for |audio_processing_thread_.Stop()|.
   ~ProcessingAudioFifo();
@@ -68,6 +72,7 @@ class ProcessingAudioFifo {
  private:
   friend class ProcessingAudioFifoTest;
 
+  class StatsReporter;
   struct CaptureData;
 
   void StartInternal(base::WaitableEvent* new_data_captured);
@@ -108,6 +113,8 @@ class ProcessingAudioFifo {
   // Event signaling that there is new audio data to process. Called from the
   // capture thread, and waited for on the processing thread.
   base::WaitableEvent new_data_captured_;
+
+  std::unique_ptr<StatsReporter> stats_reporter_;
 
   SEQUENCE_CHECKER(owning_sequence_checker_);
 };
