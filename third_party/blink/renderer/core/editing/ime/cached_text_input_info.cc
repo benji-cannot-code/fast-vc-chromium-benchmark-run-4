2bde652722aee7ed794ce47ac042b08bbf4fd44b
Yoshifumi Inoue <yosin@chromium.org>
2022-04-08 05:31:45 +0000

    Make CachedTextInputInfo::GetSelection() to handle null range
    This patch changes `CachedTextInputInfo::GetSelection()` returns empty
    `PlainTextRange` when null range is specified to avoid crash in
    subsequent call of `RangeLength`
    
    Note: This is speculative change because we don't have sample page to
    reproduce.
    
    Bug: 1313231
    Change-Id: I15d9d929f4add3703988bb77668ec2e0efe9fa32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577193
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990271}

--- third_party/blink/renderer/core/editing/ime/cached_text_input_info.cc
+++ third_party/blink/renderer/core/editing/ime/cached_text_input_info.cc
@@ -203,6 +203,8 @@ PlainTextRange CachedTextInputInfo::GetPlainTextRange(
 PlainTextRange CachedTextInputInfo::GetSelection(
     const EphemeralRange& range) const {
   DCHECK(container_);
+  if (range.IsNull())
+    return PlainTextRange();
   return GetPlainTextRangeWithCache(range, &selection_);
 }
 
