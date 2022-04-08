5b08216bd4d04178536cc6aa9c338c143071cdcb
Iman Saboori <isaboori@google.com>
2022-04-08 19:45:56 +0000

    speculation-rules prefetch WPT tests
    - speculation-rules prefetch should not work for http documents.
    - prefetch algorithm should assert url’s scheme is https and not http
    - any invalid/extra field in speculation-rules script should disable
      prefetching of pages. test for anonymizationPolicy. Based on the spec,
      the “Sec-Purpose” request header should also include a parameter with
      the "anonymous-client-ip" key. To run this test following chrome
      switches should be set:
        --isolated-prerender-allow-all-domains
        --bypass-prefetch-proxy-for-host="not-web-platform.test"
    
    
    Bug: 1302365
    Change-Id: I562e78c6b116729557013d374b00793518c62a47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566078
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#990539}

--- third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/utils.sub.js
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/utils.sub.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Resolved URL to find this script.
 const SR_PREFETCH_UTILS_URL = new URL(document.currentScript.src, document.baseURI);
+// Hostname for cross origin urls.
+const PREFETCH_PROXY_BYPASS_HOST = "{{hosts[alt][]}}";
 
 class PrefetchAgent extends RemoteContext {
   constructor(uuid, t) {
@@ -12,9 +14,18 @@ class PrefetchAgent extends RemoteContext {
     this.t = t;
   }
 
-  getExecutorURL(extra = {}) {
+  getExecutorURL(options = {}) {
+    let {hostname, protocol, ...extra} = options;
     let params = new URLSearchParams({uuid: this.context_id, ...extra});
-    return new URL(`executor.sub.html?${params}`, SR_PREFETCH_UTILS_URL);
+    let url = new URL(`executor.sub.html?${params}`, SR_PREFETCH_UTILS_URL);
+    if(hostname !== undefined) {
+      url.hostname = hostname;
+    }
+    if(protocol !== undefined) {
+      url.protocol = protocol;
+      url.port = protocol === "https" ? "{{ports[https][0]}}" : "{{ports[http][0]}}";
+    }
+    return url;
   }
 
   // Requests prefetch via speculation rules.
@@ -22,10 +33,10 @@ class PrefetchAgent extends RemoteContext {
   // In the future, this should also use browser hooks to force the prefetch to
   // occur despite heuristic matching, etc., and await the completion of the
   // prefetch.
-  async forceSinglePrefetch(url) {
-    await this.execute_script((url) => {
-      insertSpeculationRules({ prefetch: [{source: 'list', urls: [url]}] });
-    }, [url]);
+  async forceSinglePrefetch(url, extra = {}) {
+    await this.execute_script((url, extra) => {
+      insertSpeculationRules({ prefetch: [{source: 'list', urls: [url], ...extra}] });
+    }, [url, extra]);
     return new Promise(resolve => this.t.step_timeout(resolve, 2000));
   }
 
@@ -48,9 +59,9 @@ class PrefetchAgent extends RemoteContext {
 }
 
 // Must also include /common/utils.js and /common/dispatcher/dispatcher.js to use this.
-async function spawnWindow(t) {
+async function spawnWindow(t, extra = {}) {
   let agent = new PrefetchAgent(token(), t);
-  let w = window.open(agent.getExecutorURL());
+  let w = window.open(agent.getExecutorURL(), extra);
   t.add_cleanup(() => w.close());
   return agent;
 }
@@ -60,4 +71,14 @@ function insertSpeculationRules(body) {
   script.type = 'speculationrules';
   script.textContent = JSON.stringify(body);
   document.head.appendChild(script);
+}
+
+function assert_prefetched (requestHeaders, description) {
+  assert_in_array(requestHeaders.purpose, ["", "prefetch"], "The vendor-specific header Purpose, if present, must be 'prefetch'.");
+  assert_equals(requestHeaders.sec_purpose, "prefetch", description);
+}
+
+function assert_not_prefetched (requestHeaders, description){
+  assert_equals(requestHeaders.purpose, "", description);
+  assert_equals(requestHeaders.sec_purpose, "", description);
 }
