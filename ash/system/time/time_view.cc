e86cd0fed68f700cad437a386d0ee3288493f7ba
Jiaming Cheng <jiamingc@chromium.org>
2022-04-08 19:59:30 +0000

    Use digital number as day of month
    We were using the date formatter to show the day of month both in the
    the monthly calendar view and in the vertical date tray view. This
    caused an issue that for some languages, the day of month is formatted
    to a digital number with some wordings, for instance:
    https://screenshot.googleplex.com/7hfjqzPHXaTf2m7
    which shows this in the calendar view:
    https://listnr.corp.google.com/product/208/report/89688986753
    
    So in this cl, we use the digital number to show day of month for both
    the date in the calendar view and the date in the vertical date tray.
    
    Also some refactor work in this cl:
    1, Created a singleton DateHelper which provided date related functions
    like time formatter and time difference. (The old DateFormatter is
    merged into this class)
    2, This singleton helper is used in the CalendarUtils and all the
    unit tests are in this calendar_utils_unittest. This change doesn't
    change any behavior so all the test are the same as before.
    
    
    Bug: 1305459
    Change-Id: Ic5b82690860c265abff961be8e4a966624fec822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576547
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990548}

--- ash/system/time/time_view.cc
+++ ash/system/time/time_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/model/clock_model.h"
 #include "ash/system/model/system_tray_model.h"
+#include "ash/system/time/calendar_utils.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/tray/tray_utils.h"
@@ -107,8 +108,10 @@ void VerticalDateView::OnThemeChanged() {
 }
 
 void VerticalDateView::UpdateText() {
-  const std::u16string new_text =
-      base::TimeFormatWithPattern(base::Time::Now(), "dd");
+  const std::u16string new_text = calendar_utils::GetDayIntOfMonth(
+      base::Time::Now() +
+      base::Minutes(
+          calendar_utils::GetTimeDifferenceInMinutes(base::Time::Now())));
   if (text_label_->GetText() == new_text)
     return;
   text_label_->SetText(new_text);
