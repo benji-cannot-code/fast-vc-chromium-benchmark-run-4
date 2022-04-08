d2a3ef971acff7f18cd2d89883b414f572e37a46
dpapad <dpapad@chromium.org>
2022-04-08 11:20:17 +0000

    WebUI: Remove another js_modulizer() usage in c/t/data/webui/chromeos/
    This is part of the effort to fully removing the Polymer3 migration
    tools.
    
    Bug: 1184053
    Change-Id: I932530f3d3e5bb4ede9c81d3349803cde713b8c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573895
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990342}

--- chrome/test/data/webui/cr_components/chromeos/network/network_config_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_config_test.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import 'chrome://resources/cr_components/chromeos/network/network_config.m.js';
 
 // #import {keyEventOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
-// #import {FakeNetworkConfig} from 'chrome://test/chromeos/fake_network_config_mojom.m.js';
+// #import {FakeNetworkConfig} from 'chrome://test/chromeos/fake_network_config_mojom.js';
 // #import {MojoInterfaceProviderImpl} from 'chrome://resources/cr_components/chromeos/network/mojo_interface_provider.m.js';
 // #import {OncMojo} from 'chrome://resources/cr_components/chromeos/network/onc_mojo.m.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
