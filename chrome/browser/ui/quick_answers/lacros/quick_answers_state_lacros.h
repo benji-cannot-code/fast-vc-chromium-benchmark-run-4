42ff73358d227cfbd2870103ec725d33324fa9fd
Yue Li <updowndota@google.com>
2022-04-09 02:42:00 +0000

    QA Lacros: Add user consent logics for lacros
    This change add user consent logics for QuickAnswersStateLacros, which
    is a preparation for fully support the feature in the lacros browser.
    
    Bug: b/228641185
    Test: Run existing tests
    Change-Id: Id1728bf75d053ada93596857d7a3c3a20cbda879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579387
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990699}

--- chrome/browser/ui/quick_answers/lacros/quick_answers_state_lacros.h
+++ chrome/browser/ui/quick_answers/lacros/quick_answers_state_lacros.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_QUICK_ANSWERS_LACROS_QUICK_ANSWERS_STATE_LACROS_H_
 #define CHROME_BROWSER_UI_QUICK_ANSWERS_LACROS_QUICK_ANSWERS_STATE_LACROS_H_
 
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_state.h"
 #include "chromeos/lacros/crosapi_pref_observer.h"
-#include "components/prefs/pref_service.h"
 
 // A class that holds Quick Answers related prefs and states in Lacros browser.
 class QuickAnswersStateLacros : public QuickAnswersState {
@@ -21,17 +22,24 @@ class QuickAnswersStateLacros : public QuickAnswersState {
   ~QuickAnswersStateLacros() override;
 
  private:
-  // TODO(b/186743778): Implement consent logics for Lacros.
-  void StartConsent() override {}
-  void OnConsentResult(ConsentResultType result) override {}
+  void StartConsent() override;
+  void OnConsentResult(ConsentResultType result) override;
 
-  void OnSettingsEnabledChanged(base::Value value);
-  void OnConsentStatusChanged(base::Value value);
-  void OnDefinitionEnabledChanged(base::Value value);
-  void OnTranslationEnabledChanged(base::Value value);
-  void OnUnitConversionEnabledChanged(base::Value value);
-  void OnApplicationLocaleChanged(base::Value value);
-  void OnPreferredLanguagesChanged(base::Value value);
+  void OnSettingsEnabledChanged(const base::Value& value);
+  void OnConsentStatusChanged(const base::Value& value);
+  void OnDefinitionEnabledChanged(const base::Value& value);
+  void OnTranslationEnabledChanged(const base::Value& value);
+  void OnUnitConversionEnabledChanged(const base::Value& value);
+  void OnApplicationLocaleChanged(const base::Value& value);
+  void OnPreferredLanguagesChanged(const base::Value& value);
+  void OnImpressionCountChanged(const base::Value& value);
+  void OnImpressionDurationChanged(const base::Value& value);
+
+  // Time when the notice is shown.
+  base::TimeTicks consent_start_time_;
+
+  int impression_count_ = 0;
+  int impression_duration_ = 0;
 
   std::unique_ptr<CrosapiPrefObserver> settings_enabled_observer_;
   std::unique_ptr<CrosapiPrefObserver> consent_status_observer_;
@@ -40,6 +48,8 @@ class QuickAnswersStateLacros : public QuickAnswersState {
   std::unique_ptr<CrosapiPrefObserver> unit_conversion_enabled_observer_;
   std::unique_ptr<CrosapiPrefObserver> application_locale_observer_;
   std::unique_ptr<CrosapiPrefObserver> preferred_languages_observer_;
+  std::unique_ptr<CrosapiPrefObserver> impression_count_observer_;
+  std::unique_ptr<CrosapiPrefObserver> impression_duration_observer_;
 };
 
 #endif  // CHROME_BROWSER_UI_QUICK_ANSWERS_LACROS_QUICK_ANSWERS_STATE_LACROS_H_
