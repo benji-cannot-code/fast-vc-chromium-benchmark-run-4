fd1906530e26b833e401b1ee2f66bf2ef868f864
Iman Saboori <isaboori@google.com>
2022-04-10 02:13:58 +0000

    speculation-rules prefetch WPT tests
    - speculation-rules base prefetch logic should be
      able to prefetch multiple urls. To pass the test
      at least two urls should be prefetched.
    
    Bug: 1302365
    Change-Id: I1a333f6c8ab350a1a09e7f389d7bb444dc8d6dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577359
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#990788}

---
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prefetch/resources/prefetch.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from wptserve.handlers import json_handler
+
+
+@json_handler
+def main(request, response):
+    uuid = request.GET[b"uuid"]
+    prefetch = request.headers.get(
+        "Sec-Purpose", b"").decode("utf-8").startswith("prefetch")
+
+    n = request.server.stash.take(uuid)
+    if n is None:
+        n = 0
+    if prefetch:
+        n += 1
+        request.server.stash.put(uuid, n)
+
+    return n
