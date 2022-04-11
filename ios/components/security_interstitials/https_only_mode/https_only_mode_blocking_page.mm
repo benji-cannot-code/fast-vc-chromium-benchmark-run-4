3ad0d5d10b658ea828580045bc225c598a3c8c99
Mustafa Emre Acer <meacer@chromium.org>
2022-04-11 21:38:38 +0000

    HTTPS-Only Mode: Implement allowlisting in the interstitial
    This CL implements hostname allowlisting in the HTTPS-Only Mode
    interstitial. The allowlist is used to prevent repeatedly asking the
    user if they want to display the fallback HTTP URL when the HTTPS
    upgrade fails.
    
    Bug: 1302509
    Change-Id: I6d9ca8c6b4103933ef56e51e1c9a553b11a45cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577161
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991207}

--- ios/components/security_interstitials/https_only_mode/https_only_mode_blocking_page.mm
+++ ios/components/security_interstitials/https_only_mode/https_only_mode_blocking_page.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/common_string_util.h"
 #include "components/security_interstitials/core/https_only_mode_ui_util.h"
 #include "components/security_interstitials/core/metrics_helper.h"
+#include "ios/components/security_interstitials/https_only_mode/https_only_mode_allowlist.h"
 #include "ios/components/security_interstitials/ios_blocking_page_controller_client.h"
 #include "ios/components/security_interstitials/ios_blocking_page_metrics_helper.h"
 
@@ -29,6 +30,11 @@ HttpsOnlyModeBlockingPage::HttpsOnlyModeBlockingPage(
       web_state_(web_state),
       controller_(std::move(client)) {
   DCHECK(web_state_);
+  controller_->metrics_helper()->RecordUserDecision(
+      security_interstitials::MetricsHelper::SHOW);
+  controller_->metrics_helper()->RecordUserInteraction(
+      security_interstitials::MetricsHelper::TOTAL_VISITS);
+
   // Creating an interstitial without showing it (e.g. from
   // chrome://interstitials) leaks memory, so don't create it here.
 }
@@ -53,11 +59,12 @@ void HttpsOnlyModeBlockingPage::PopulateInterstitialStrings(
     load_time_data->SetBoolKey("cant_go_back", true);
   }
 
+  PopulateHttpsOnlyModeStringsForSharedHTML(load_time_data);
   PopulateHttpsOnlyModeStringsForBlockingPage(load_time_data, request_url());
 }
 
 bool HttpsOnlyModeBlockingPage::ShouldDisplayURL() const {
-  return false;
+  return true;
 }
 
 void HttpsOnlyModeBlockingPage::HandleCommand(
@@ -68,12 +75,14 @@ void HttpsOnlyModeBlockingPage::HandleCommand(
   if (command == security_interstitials::CMD_DONT_PROCEED) {
     controller_->metrics_helper()->RecordUserDecision(
         security_interstitials::MetricsHelper::DONT_PROCEED);
-    // TODO(crbug.com/1302509): Record metrics here.
     controller_->GoBack();
   } else if (command == security_interstitials::CMD_PROCEED) {
+    HttpsOnlyModeAllowlist* allowlist =
+        HttpsOnlyModeAllowlist::FromWebState(web_state());
+    allowlist->AllowHttpForHost(request_url().host());
+
     controller_->metrics_helper()->RecordUserDecision(
         security_interstitials::MetricsHelper::PROCEED);
-    // TODO(crbug.com/1302509): Record metrics here.
     controller_->Proceed();
   }
 }
