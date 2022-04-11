6f9255a7f94943c86713ddf1f7dd490ae086c8a2
Reilly Grant <reillyg@chromium.org>
2022-04-11 23:33:48 +0000

    Remove SerialPortUnderlyingSource::SignalErrorImmediately
    This method is unused. All callers use SignalErrorOnClose(), which waits
    for the Mojo data pipe to close first, allowing any queued data to be
    received.
    
    Change-Id: I68efc2297e65dec027b2a62a0a1c72e2c5b36ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582744
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991266}

--- third_party/blink/renderer/modules/serial/serial_port_underlying_source.h
+++ third_party/blink/renderer/modules/serial/serial_port_underlying_source.h
@@ -27,7 +27,6 @@ class SerialPortUnderlyingSource : public UnderlyingSourceBase {
   ScriptPromise Cancel(ScriptState*, ScriptValue reason) override;
   void ContextDestroyed() override;
 
-  void SignalErrorImmediately(DOMException*);
   void SignalErrorOnClose(DOMException*);
 
   void Trace(Visitor*) const override;
