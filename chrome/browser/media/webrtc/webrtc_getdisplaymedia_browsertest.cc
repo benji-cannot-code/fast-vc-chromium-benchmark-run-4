b1a8c62032edc8eb7b81d4236dd0cbb5e905b1be
Tove Petersson <tovep@chromium.org>
2022-04-11 18:51:16 +0000

    Set ShareThisTabInsteadButtonGetDisplayMedia to DISABLED_BY_DEFAULT
    Due to https://crbug.com/1307710, 'Audio not changing using "Share this
    tab instead" button while sharing a tab'.
    
    This change is made in addition to turning the feature off using finch,
    and will be merged to branches M98-M101.
    
    ShareThisTabInsteadButtonGetDisplayMedia will be re-enabled once the
    issue has been fixed.
    
    Bug: 1307710
    Change-Id: I1d001333bab976a19dd5d598014ab53cd2f62fee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568548
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991120}

--- chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
@@ -876,6 +876,9 @@ IN_PROC_BROWSER_TEST_P(GetDisplayMediaVideoTrackBrowserTest, RunCombinedTest) {
 class GetDisplayMediaChangeSourceBrowserTest : public WebRtcTestBase {
  public:
   void SetUpInProcessBrowserTestFixture() override {
+    feature_list_.InitWithFeatures(
+        {media::kShareThisTabInsteadButtonGetDisplayMedia}, {});
+
     WebRtcTestBase::SetUpInProcessBrowserTestFixture();
 
     DetectErrorsInJavaScript();
@@ -890,6 +893,9 @@ class GetDisplayMediaChangeSourceBrowserTest : public WebRtcTestBase {
     command_line->AppendSwitchASCII(
         switches::kAutoSelectTabCaptureSourceByTitle, kCapturedTabTitle);
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(GetDisplayMediaChangeSourceBrowserTest, ChangeSource) {
