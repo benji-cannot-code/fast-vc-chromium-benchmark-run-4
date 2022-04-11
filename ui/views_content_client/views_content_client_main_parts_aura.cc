29b755b70f3cc94e45e967e1ac60fc124e151e51
Bruce Dawson <brucedawson@chromium.org>
2022-04-11 16:03:39 +0000

    Include utility where needed
    When running presubmits on ui/*.cc the one warning hit is this one:
    
        views_content_client_main_parts_aura.cc:18:  Add #include <utility> for move
    
    Fixing this will get those .cc files presubmit clean.
    
    Bug: 1309977
    Change-Id: I7abb0ef067e2c154290908b93ba406e060fbdc2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576634
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991042}

--- ui/views_content_client/views_content_client_main_parts_aura.cc
+++ ui/views_content_client/views_content_client_main_parts_aura.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views_content_client/views_content_client_main_parts_aura.h"
 
+#include <utility>
+
 #include "build/chromeos_buildflags.h"
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
