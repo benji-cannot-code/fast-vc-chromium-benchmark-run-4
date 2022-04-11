375f3c3b4406953e71ac1df72e6f48f6abbca781
Daniel Andersson <dandersson@chromium.org>
2022-04-11 22:04:53 +0000

    saved_desks: Launching a Save & Recall desk will delete the saved desk.
    In addition to just issuing the call to delete the saved desk, this
    change also moves the callback `OnNewDeskCreatedForTemplate` into
    DesksTemplatesPresenter. Back when launching a template meant exiting
    overview mode, this was required since the presenter instance would get
    removed.
    
    We are no longer exiting overview mode (this hasn't been the case for
    some time). With this change, we are making it a requirement to stay in
    overview mode, so the callback was moved into the class.
    
    Bug: 1310311
    Change-Id: Ia42eb33b958e05d58a166af5d49d567032d0c1b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575994
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991219}

--- ash/wm/desks/templates/desks_templates_presenter.h
+++ ash/wm/desks/templates/desks_templates_presenter.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class Desk;
 class DeskTemplate;
 class OverviewSession;
 enum class DeskTemplateType;
@@ -107,6 +108,13 @@ class ASH_EXPORT DesksTemplatesPresenter : desks_storage::DeskModelObserver {
       aura::Window* const root_window,
       desks_storage::DeskModel::GetEntryByUuidStatus status,
       std::unique_ptr<DeskTemplate> entry);
+
+  // Callback after creating a new desk for launching a template.
+  void OnNewDeskCreatedForTemplate(std::unique_ptr<DeskTemplate> desk_template,
+                                   base::Time time_launch_started,
+                                   base::TimeDelta delay,
+                                   aura::Window* root_window,
+                                   const Desk* new_desk);
 
   // Callback after adding or updating an entry. Will then call
   // `AddOrUpdateUIEntries` to update the UI by adding or updating the template.
