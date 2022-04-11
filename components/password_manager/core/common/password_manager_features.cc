8537ee3704e313dc50226b7deecd80dd1d8e35d6
Maria Kazinova <kazinova@google.com>
2022-04-11 13:50:24 +0000

    [UPM] Migrate non-syncable data to GMS Core when user turns sync on.
    This change is useful for users that are switching between android and
    built-in passwords store backends when enabling sync. While sync is
    disabled, non-syncable data gets saved only in the built-in backend.
    When sync gets enabled, it needs to be migrated to Android backend
    to avoid its loss.
    
    Bug: 1309868
    Change-Id: I2395def5702b0a42129ff4d14c7043d6bcda1122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550285
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#991001}

--- components/password_manager/core/common/password_manager_features.cc
+++ components/password_manager/core/common/password_manager_features.cc
@@ -272,7 +272,7 @@ bool UsesUnifiedPasswordManagerUi() {
 #endif  // IS_ANDROID
 
 #if BUILDFLAG(IS_ANDROID)
-bool RequiresInitialMigrationForUnifiedPasswordManager() {
+bool RequiresMigrationForUnifiedPasswordManager() {
   if (!base::FeatureList::IsEnabled(kUnifiedPasswordManagerAndroid))
     return false;
   UpmExperimentVariation variation = kUpmExperimentVariationParam.Get();
