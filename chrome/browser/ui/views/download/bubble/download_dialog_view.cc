85d2a7a8ca611f4a1bd2eb33be608640dca4de5b
Rohit Bhatia <bhatiarohit@google.com>
2022-04-11 19:12:01 +0000

    Close Download Bubble when empty
    For downloads that are deleted, say for dangerous downloads, or offline
    items, we remove them from the bubble. If the bubble turns up empty,
    close it.
    
    Bug: 1311584
    Bug: 1282255
    Change-Id: I92e30dfff56ed1c208746e302f52af9ef6f40154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575690
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991128}

--- chrome/browser/ui/views/download/bubble/download_dialog_view.cc
+++ chrome/browser/ui/views/download/bubble/download_dialog_view.cc
@@ -35,7 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/vector_icons/vector_icons.h"
 
 void DownloadDialogView::CloseBubble() {
-  navigation_handler_->CloseDialog();
+  navigation_handler_->CloseDialog(
+      views::Widget::ClosedReason::kCloseButtonClicked);
 }
 
 void DownloadDialogView::ShowAllDownloads() {
