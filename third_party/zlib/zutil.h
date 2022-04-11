82fd997bb5c02d56cad56843b8b9539ba42d10ca
Adenilson Cavalcanti <cavalcantii@chromium.org>
2022-04-11 22:38:27 +0000

    [zlib] Re-sync with zlib 1.2.12, patch 10 of N
    Validated build is fine for V8@Win10 with MSVC.
    
    Ported:
    - Avoid the use of ptrdiff_t
    
    Bug: 1032721
    Change-Id: I27c290bf29321c3000ffd8edfa53f21b900859d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579804
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991240}

--- third_party/zlib/zutil.h
+++ third_party/zlib/zutil.h
@@ -45,10 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #  endif
 #endif
 
-#ifdef Z_SOLO
-   typedef long ptrdiff_t;  /* guess -- will be caught if guess is wrong */
-#endif
-
 #ifndef local
 #  define local static
 #endif
@@ -186,10 +182,6 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 #if (defined(_MSC_VER) && (_MSC_VER > 600)) && !defined __INTERIX
 #  if defined(_WIN32_WCE)
 #    define fdopen(fd,mode) NULL /* No fdopen() */
-#    ifndef _PTRDIFF_T_DEFINED
-       typedef int ptrdiff_t;
-#      define _PTRDIFF_T_DEFINED
-#    endif
 #  else
 #    define fdopen(fd,type)  _fdopen(fd,type)
 #  endif
