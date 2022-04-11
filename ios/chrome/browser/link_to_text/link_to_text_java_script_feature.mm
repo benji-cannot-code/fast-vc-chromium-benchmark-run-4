7834dec5819a47d8680122fce9da2e7fea9f324b
Mike Dougherty <michaeldo@chromium.org>
2022-04-11 22:47:01 +0000

    Convert ios/chrome/browser/link_to_text:link_to_text_js
    Converts link_to_text.js to be compiled with Rollup and Terser instead
    of the closure compiler.
    
    Additionally, removes the outer anonymous function which is now
    redundant as the JavaScript produced by optimize_js always wraps the
    resulting script in an anonymous function.
    
    Bug: 1313654
    Change-Id: I213fb9a9fd548d1b42fa92f7071c1a8bd562add5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581698
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991244}

--- ios/chrome/browser/link_to_text/link_to_text_java_script_feature.mm
+++ ios/chrome/browser/link_to_text/link_to_text_java_script_feature.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-const char kScriptName[] = "link_to_text_js";
+const char kScriptName[] = "link_to_text";
 const char kGetLinkToTextFunction[] = "linkToText.getLinkToText";
 
 LinkToTextJavaScriptFeature::LinkToTextJavaScriptFeature()
