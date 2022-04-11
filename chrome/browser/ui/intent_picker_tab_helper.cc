ddc70537de8a9cba7b3ece2adbf3be2079f079b6
Tim Sergeant <tsergeant@chromium.org>
2022-04-11 16:55:29 +0000

    Intents: Fix crash showing External Protocol Dialog in Intent Chip UI
    The Intent Picker icon in the omnibox is used for showing the
    link-capturing intent picker, but can also show the External Protocol
    Dialog on Chrome OS.
    
    It is possible for the external protocol dialog to show before a
    navigation has been committed. This was causing a crash when the new
    Intent Chip UI was enabled, which tried to look up an empty URL in
    content settings.
    
    This CL fixes the crash by preventing empty URLs from being used, and by
    separating the codepaths for the two flows more clearly. Showing the
    external protocol dialog now also clears all app-based customizations
    from the Intent Chip, to prevent cases where the intent chip could show
    a different app icon to what is in the dialog.
    
    Bug: 1280507
    Change-Id: I392d3f6d688403a8e68dfb46c5a0a7d1c1a6b9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581438
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991065}

--- chrome/browser/ui/intent_picker_tab_helper.cc
+++ chrome/browser/ui/intent_picker_tab_helper.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/image_model.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image.h"
+#include "url/origin.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/apps/intent_helper/metrics/intent_handling_metrics.h"
@@ -102,6 +103,14 @@ void IntentPickerTabHelper::ShowOrHideIcon(content::WebContents* web_contents,
   IntentPickerTabHelper* tab_helper = FromWebContents(web_contents);
   if (!tab_helper)
     return;
+
+  if (apps::features::LinkCapturingUiUpdateEnabled()) {
+    tab_helper->app_icon_ = ui::ImageModel();
+    tab_helper->should_show_collapsed_chip_ = false;
+    tab_helper->last_shown_app_id_ = std::string();
+    tab_helper->last_shown_origin_ = url::Origin();
+  }
+
   tab_helper->ShowOrHideIconInternal(should_show_icon);
 }
 
@@ -127,7 +136,7 @@ void IntentPickerTabHelper::ShowIconForApps(
     }
   }
 
-  ShowOrHideIconInternal(!apps.empty());
+  ShowIconForLinkIntent(!apps.empty());
 }
 
 IntentPickerTabHelper::IntentPickerTabHelper(content::WebContents* web_contents)
@@ -195,14 +204,15 @@ void IntentPickerTabHelper::LoadAppIcon(
                                    std::move(callback), index));
 }
 
-void IntentPickerTabHelper::UpdateCollapsedState() {
-  if (!should_show_icon_) {
+void IntentPickerTabHelper::UpdateCollapsedState(bool should_show_icon) {
+  GURL url = web_contents()->GetLastCommittedURL();
+
+  if (!should_show_icon || url.is_empty()) {
     should_show_collapsed_chip_ = false;
     last_shown_origin_ = url::Origin();
     return;
   }
 
-  GURL url = web_contents()->GetLastCommittedURL();
   url::Origin origin = url::Origin::Create(url);
 
   // Determine whether to show the Chip as expanded/collapsed whenever the
@@ -231,10 +241,10 @@ void IntentPickerTabHelper::OnAppIconLoadedForChip(const std::string& app_id,
     app_icon_ = ui::ImageModel();
   }
 
-  ShowOrHideIconInternal(true);
+  ShowIconForLinkIntent(true);
 }
 
-void IntentPickerTabHelper::ShowOrHideIconInternal(bool should_show_icon) {
+void IntentPickerTabHelper::ShowIconForLinkIntent(bool should_show_icon) {
 #if BUILDFLAG(IS_CHROMEOS)
   if (should_show_icon && !should_show_icon_) {
     // This point doesn't exactly match when the icon is shown in the UI (e.g.
@@ -245,11 +255,15 @@ void IntentPickerTabHelper::ShowOrHideIconInternal(bool should_show_icon) {
   }
 #endif
 
-  should_show_icon_ = should_show_icon;
-
   if (apps::features::LinkCapturingUiUpdateEnabled()) {
-    UpdateCollapsedState();
+    UpdateCollapsedState(should_show_icon);
   }
+
+  ShowOrHideIconInternal(should_show_icon);
+}
+
+void IntentPickerTabHelper::ShowOrHideIconInternal(bool should_show_icon) {
+  should_show_icon_ = should_show_icon;
 
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents());
   if (!browser)
