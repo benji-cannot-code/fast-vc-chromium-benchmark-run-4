073649f9dab683ddb39fd0ad39ebefa92f4c5445
Henrique Nakashima <hnakashima@chromium.org>
2022-04-11 18:35:57 +0000

    [Android] Review @DisabledTests in org.chromium.chrome.browser.[o-s]
    Correct the bug linked to some tests.
    
    This is a preparation to run disabled tests in Test Reviver.
    
    Bug: 1306975
    Change-Id: I84cad5d1b3f91aab0736b2718bf49f7b2535ffcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572297
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991112}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGridLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGridLayoutTest.java
@@ -149,11 +149,10 @@ public class TileGridLayoutTest {
     }
 
     @Test
-    //@MediumTest
-    @DisabledTest(message = "crbug.com/771648")
+    @MediumTest
+    @DisabledTest(message = "crbug.com/768779")
     @Feature({"NewTabPage", "RenderTest"})
-    public void testModernTileGridAppearance_Full()
-            throws IOException, InterruptedException {
+    public void testModernTileGridAppearance_Full() throws IOException, InterruptedException {
         View tileGridLayout = renderTiles(makeSuggestions(FAKE_MOST_VISITED_URLS.length));
 
         setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, mActivityTestRule.getActivity());
@@ -175,11 +174,10 @@ public class TileGridLayoutTest {
     }
 
     @Test
-    //@MediumTest
-    @DisabledTest(message = "crbug.com/771648")
+    @MediumTest
+    @DisabledTest(message = "crbug.com/768779")
     @Feature({"NewTabPage", "RenderTest"})
-    public void testModernTileGridAppearance_Two()
-            throws IOException, InterruptedException {
+    public void testModernTileGridAppearance_Two() throws IOException, InterruptedException {
         View tileGridLayout = renderTiles(makeSuggestions(2));
 
         setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, mActivityTestRule.getActivity());
