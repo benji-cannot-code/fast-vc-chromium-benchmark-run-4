b7a99a262f5c5d9b7d18263165ebb76154404a0c
Sky Malice <skym@chromium.org>
2022-04-11 21:29:56 +0000

    Disable flaky PermissionInfoTest tests.
    Bug: 1314109
    Change-Id: Ic6913f7b8e812d92affb2a991f31be3fdb1486c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582777
    Commit-Queue: Sky Malice <skym@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991192}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/PermissionInfoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/PermissionInfoTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataBridge;
@@ -128,6 +129,7 @@ public class PermissionInfoTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
+    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_InPrimaryOTRProfile_DefaultsToAskFromBlock()
             throws Throwable {
         Profile primaryOTRProfile = getPrimaryOTRProfile();
@@ -142,6 +144,7 @@ public class PermissionInfoTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
+    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_InNonPrimaryOTRProfile_DefaultsToAskFromBlock()
             throws Throwable {
         Profile nonPrimaryOTRProfile = getNonPrimaryOTRProfile();
@@ -157,6 +160,7 @@ public class PermissionInfoTest {
     @SmallTest
     @Feature({"Preferences"})
     @RequiresRestart
+    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_RegularProfile_DefaultsToAskFromBlock() throws Throwable {
         Profile regularProfile = getRegularProfile();
         setGeolocation(DSE_ORIGIN, null, ContentSettingValues.BLOCK, regularProfile);
