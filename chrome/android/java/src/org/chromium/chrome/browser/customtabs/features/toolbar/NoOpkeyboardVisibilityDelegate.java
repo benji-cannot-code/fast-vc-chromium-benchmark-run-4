e353c1901d517080bbf7247c67b1e1b42b65411d
Peter E Conn <peconn@chromium.org>
2022-04-12 15:10:25 +0000

    🛃 Move two CustomTabToolbar inner classes to upper level.
    This change just makes the file a bit shorter.
    
    Change-Id: I85e89d22902be9f4a6f4a39d2876efa8c396775f
    Bug: 1269337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582206
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991528}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/NoOpkeyboardVisibilityDelegate.java
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.customtabs.features.toolbar;
+
+import android.content.Context;
+import android.view.View;
+
+import org.chromium.ui.KeyboardVisibilityDelegate;
+
+/** A {@link KeyboardVisibilityDelegate} that never shows the keyboard. */
+class NoOpkeyboardVisibilityDelegate extends KeyboardVisibilityDelegate {
+    @Override
+    public void showKeyboard(View view) {}
+
+    @Override
+    public boolean hideKeyboard(View view) {
+        return false;
+    }
+
+    @Override
+    public int calculateKeyboardHeight(View view) {
+        return 0;
+    }
+
+    @Override
+    public boolean isKeyboardShowing(Context context, View view) {
+        return false;
+    }
+
+    @Override
+    public void addKeyboardVisibilityListener(KeyboardVisibilityListener listener) {}
+
+    @Override
+    public void removeKeyboardVisibilityListener(KeyboardVisibilityListener listener) {}
+}
