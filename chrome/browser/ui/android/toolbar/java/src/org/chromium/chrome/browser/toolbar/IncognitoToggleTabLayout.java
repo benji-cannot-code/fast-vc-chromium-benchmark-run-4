9bdbfd2a94cd984a6ed5701eed0f3d7f76c32473
Side Yilmaz <sideyilmaz@chromium.org>
2022-04-12 15:17:44 +0000

    Add Android.TabSwitcher.IncognitoClickedIsEmpty histogram.
    This CL adds a new histogram to track whether there is any Incognito
    tab when Incognito button is clicked on tab switcher.
    
    Bug: 1309813
    Change-Id: I896885220b4dd64f6acdc7950544430d2b37e870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578754
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Auto-Submit: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991531}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/IncognitoToggleTabLayout.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/IncognitoToggleTabLayout.java
@@ -15,6 +15,7 @@ import androidx.appcompat.content.res.AppCompatResources;
 import com.google.android.material.tabs.TabLayout;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
@@ -174,6 +175,11 @@ public class IncognitoToggleTabLayout extends TabLayout implements TabCountObser
 
         mTabModelSelector.commitAllTabClosures();
         mTabModelSelector.selectModel(incognitoSelected);
+
+        if (incognitoSelected) {
+            RecordHistogram.recordBooleanHistogram("Android.TabSwitcher.IncognitoClickedIsEmpty",
+                    mTabCountProvider.getTabCount() == 0);
+        }
 
         final int stackAnnouncementId = incognitoSelected
                 ? R.string.accessibility_tab_switcher_incognito_stack_selected
