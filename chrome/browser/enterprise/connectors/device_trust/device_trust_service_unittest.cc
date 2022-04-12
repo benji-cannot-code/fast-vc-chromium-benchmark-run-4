b44d87c094f5703025e0355e574c21fefe0b6913
hmare <hmare@google.com>
2022-04-12 19:35:52 +0000

    Cleanup previous device trust reporting pipeline
    This cleanup removes the DeviceTrustReports code that was previously
    used as a pipeline for reporting device trust events.
    
    Bug: b/216469755
    Change-Id: I3ae06d7b998ec80f2811d7856d206044002799e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580704
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#991673}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_service_unittest.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_connector_service.h"
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_features.h"
 #include "chrome/browser/enterprise/connectors/device_trust/signals/mock_signals_service.h"
-#include "components/enterprise/common/proto/device_trust_report_event.pb.h"
 #include "components/prefs/testing_pref_service.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "testing/gmock/include/gmock/gmock.h"
