3d1fcb2a689ea24d1256e6609f03c7a5b28b72f3
Vadym Kargin <vadym.kargin@accenture.com>
2022-04-12 21:32:40 +0000

    Ad Auction module: Create DOMException with V8ThrowDOMException
    This CL is a part of chain of updates for replacing DOMException constructors with V8ThrowDOMException.
    
    Issue: Replace DOMException with ExceptionState/V8ThrowDOMException
    Bug: 991544
    Change-Id: Iaf952c12d53fbc400087837b21fcbb247d20a4e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581042
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#991731}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom-blink.h"
 #include "third_party/blink/public/web/web_console_message.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_usvstring_usvstringsequence.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ad_properties.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ad_request_config.h"
@@ -1158,8 +1159,8 @@ ScriptPromise NavigatorAuction::createAdRequest(
   ScriptPromise promise = resolver->Promise();
   ad_auction_service_->CreateAdRequest(
       std::move(mojo_config),
-      WTF::Bind(&NavigatorAuction::AdsRequested, WrapPersistent(this),
-                WrapPersistent(resolver)));
+      resolver->WrapCallbackInScriptScope(
+          WTF::Bind(&NavigatorAuction::AdsRequested, WrapPersistent(this))));
   return promise;
 }
 
@@ -1175,12 +1176,9 @@ ScriptPromise NavigatorAuction::createAdRequest(
 
 void NavigatorAuction::AdsRequested(ScriptPromiseResolver* resolver,
                                     const WTF::String&) {
-  if (!resolver->GetExecutionContext() ||
-      resolver->GetExecutionContext()->IsContextDestroyed())
-    return;
-
   // TODO(https://crbug.com/1249186): Add full impl of methods.
-  resolver->Reject(MakeGarbageCollected<DOMException>(
+  resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
+      resolver->GetScriptState()->GetIsolate(),
       DOMExceptionCode::kNotSupportedError,
       "createAdRequest API not yet implemented"));
 }
@@ -1215,8 +1213,8 @@ ScriptPromise NavigatorAuction::finalizeAd(ScriptState* script_state,
   ScriptPromise promise = resolver->Promise();
   ad_auction_service_->FinalizeAd(
       ads->GetGuid(), std::move(mojo_config),
-      WTF::Bind(&NavigatorAuction::FinalizeAdComplete, WrapPersistent(this),
-                WrapPersistent(resolver)));
+      resolver->WrapCallbackInScriptScope(WTF::Bind(
+          &NavigatorAuction::FinalizeAdComplete, WrapPersistent(this))));
   return promise;
 }
 
@@ -1233,14 +1231,12 @@ ScriptPromise NavigatorAuction::finalizeAd(ScriptState* script_state,
 void NavigatorAuction::FinalizeAdComplete(
     ScriptPromiseResolver* resolver,
     const absl::optional<KURL>& creative_url) {
-  if (!resolver->GetExecutionContext() ||
-      resolver->GetExecutionContext()->IsContextDestroyed())
-    return;
   if (creative_url) {
     resolver->Resolve(creative_url);
   } else {
     // TODO(https://crbug.com/1249186): Add full impl of methods.
-    resolver->Reject(MakeGarbageCollected<DOMException>(
+    resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
+        resolver->GetScriptState()->GetIsolate(),
         DOMExceptionCode::kNotSupportedError,
         "finalizeAd API not yet implemented"));
   }
