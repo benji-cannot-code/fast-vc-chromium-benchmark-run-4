d358e8a0a9c6bd2d255a528e8f32fda6ed4a194d
Roman Sorokin <rsorokin@chromium.org>
2022-04-12 08:40:25 +0000

    [signin] Ignore errors in AuthErrorObserver
    in case SigninErrorNotifier::ShouldIgnoreSyncErrorsForTesting is set
    
    Fixed: 1309119
    Change-Id: I50fb2cb91836b885b1b292ed6ba0cb19bfb88242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581727
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991416}

--- chrome/browser/ash/login/signin/signin_error_notifier.cc
+++ chrome/browser/ash/login/signin/signin_error_notifier.cc
@@ -215,6 +215,11 @@ SigninErrorNotifier::IgnoreSyncErrorsForTesting() {
 }
 
 // static
+bool SigninErrorNotifier::ShouldIgnoreSyncErrorsForTesting() {
+  return g_ignore_sync_errors_for_test_;
+}
+
+// static
 void SigninErrorNotifier::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kEduCoexistenceArcMigrationCompleted,
                                 false);
--- chrome/browser/ash/login/signin/signin_error_notifier.h
+++ chrome/browser/ash/login/signin/signin_error_notifier.h
@@ -42,7 +42,9 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
 
   ~SigninErrorNotifier() override;
 
+  // This also makes AuthErrorObserver do not report errors.
   static std::unique_ptr<base::AutoReset<bool>> IgnoreSyncErrorsForTesting();
+  static bool ShouldIgnoreSyncErrorsForTesting();
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
