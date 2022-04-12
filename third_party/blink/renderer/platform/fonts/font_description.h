5dc9a60bab7d44d247a928dcba1bc83094e8dfd7
Yoshifumi Inoue <yosin@chromium.org>
2022-04-12 10:00:29 +0000

    [parallel-text-shaping] Introduce CrossThreadCopier<FontDescription>
    This patch introduces `CrossThreadCopier<FontDescription>` to pass
    `FontDescription` as arguments to `PostCrossThread` for prepration of
    parallel text shaping[1].
    
    [1] http://crrev.com/c/3211416 Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: Ibddda89154628fb6eb356189a75e958a89296fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583510
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991441}

--- third_party/blink/renderer/platform/fonts/font_description.h
+++ third_party/blink/renderer/platform/fonts/font_description.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/typesetting_features.h"
 #include "third_party/blink/renderer/platform/text/layout_locale.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_copier.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/skia/include/core/SkFontStyle.h"
 
@@ -575,6 +576,12 @@ struct HashTraits<blink::FontDescription>
   static blink::FontDescription EmptyValue() {
     return blink::FontDescription::CreateHashTableEmptyValue();
   }
+};
+
+template <>
+struct CrossThreadCopier<blink::FontDescription>
+    : public CrossThreadCopierPassThrough<blink::FontDescription> {
+  STATIC_ONLY(CrossThreadCopier);
 };
 
 }  // namespace WTF
