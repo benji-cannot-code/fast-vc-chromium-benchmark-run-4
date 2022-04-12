261dac2fdc6bda1b173b10f49a2a5f2b3c3f85d4
Sean Harrison <harrisonsean@chromium.org>
2022-04-12 19:51:22 +0000

    [Privacy Sandbox] Add fieldtrial entry
    Add fieldtrial entry for Potassium V3 APIs and settings page
    
    Bug: 1286276
    Change-Id: I14d072ba4a9524f38cab83f4b2c9179e5a7830d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571756
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991682}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragmentTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragmentTest.java
@@ -18,8 +18,10 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.metrics.HistogramTestRule;
 import org.chromium.base.test.util.Batch;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 
 /** Tests {@link PrivacySandboxSettingsFragment}. */
@@ -44,6 +46,7 @@ public final class PrivacySandboxSettingsFragmentTest {
 
     @Test
     @SmallTest
+    @Features.DisableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_3)
     public void testCreateActivityFromPrivacySettings() {
         Bundle fragmentArgs = new Bundle();
         fragmentArgs.putInt(PrivacySandboxSettingsFragment.PRIVACY_SANDBOX_REFERRER,
@@ -59,6 +62,7 @@ public final class PrivacySandboxSettingsFragmentTest {
 
     @Test
     @SmallTest
+    @Features.DisableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_3)
     public void testCreateActivityFromCookiesSnackbar() {
         Bundle fragmentArgs = new Bundle();
         fragmentArgs.putInt(PrivacySandboxSettingsFragment.PRIVACY_SANDBOX_REFERRER,
