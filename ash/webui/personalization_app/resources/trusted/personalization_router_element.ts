93925d7d557446c68e56676e46cfe7ec112057d8
Jason Thai <jasontt@chromium.org>
2022-04-12 22:48:16 +0000

    personalization: improve a11y for user subpage
    Return title for each option in the avatar list.
    
    BUG=b/225985561
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I54936df74cf87d080acd4cbec1b7730baaa57aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581294
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991776}

--- ash/webui/personalization_app/resources/trusted/personalization_router_element.ts
+++ ash/webui/personalization_app/resources/trusted/personalization_router_element.ts
@@ -200,7 +200,11 @@ export class PersonalizationRouter extends PolymerElement {
           document.title =
               loadTimeData.getString('ambientModeTopicSourceArtGallery');
         }
+        break;
       }
+      case Paths.User:
+        document.title = loadTimeData.getString('avatarLabel');
+        break;
     }
   }
 }
