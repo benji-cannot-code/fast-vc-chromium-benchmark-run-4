321646146e6439044559061b08a2c12677f7e639
Andrea Orru <andreaorru@chromium.org>
2022-04-12 02:52:09 +0000

    System-wide UMA memory metrics for ChromeOS
    This commit adds two UMA histograms to Chrome builds in ChromeOS.
    
    1) Memory.System.MemAvailableMB: tracks the amount of
    estimated available memory in ChromeOS, without swapping.
    It's calculated from MemFree, SReclaimable, the size of the
    file LRU lists, and the low watermarks in each zone.
    
    2) Memory.System.MemUsedMB: System-wide estimate of memory
    used in ChromeOS, calculated as the difference between the
    amount of total and available memory.
    
    Test: manually on a Kled device.
    Bug: 1315096
    
    Change-Id: Ib2864bd46608d172ef7b3ac946c90485257ef9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575876
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991340}

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/process/process_metrics.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/trace_event/memory_dump_request_args.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/tab_footprint_aggregator.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/metrics/metrics_data_validation.h"
+#include "components/metrics/system_memory_stats_recorder.h"
 #include "components/performance_manager/public/graph/frame_node.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/graph/graph_operations.h"
@@ -1245,6 +1247,17 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
     // Renderer metrics-by-tab only make sense if we're visiting all render
     // processes.
     per_tab_metrics.RecordPmfs(GetUkmRecorder());
+
+#if BUILDFLAG(IS_CHROMEOS)
+    base::SystemMemoryInfoKB system_meminfo;
+    if (base::GetSystemMemoryInfo(&system_meminfo)) {
+      int mem_used_mb =
+          (system_meminfo.total - system_meminfo.available) / 1024;
+      UMA_HISTOGRAM_LARGE_MEMORY_MB("Memory.System.MemAvailableMB",
+                                    system_meminfo.available / 1024);
+      UMA_HISTOGRAM_LARGE_MEMORY_MB("Memory.System.MemUsedMB", mem_used_mb);
+    }
+#endif
   }
 
   global_dump_ = nullptr;
