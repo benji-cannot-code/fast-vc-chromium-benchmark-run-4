f2115cd7b0b4ac655398899f1a87ee9da05076e4
Ed Chin <edchin@google.com>
2022-04-12 20:38:19 +0000

    [ios][first follow] Hook up and display favicon
    Bug: 1312124
    Change-Id: I14e65f746bee2623a1a8ec748ad0692e8f542b1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581324
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991708}

--- ios/chrome/browser/ui/follow/first_follow_view_controller.h
+++ ios/chrome/browser/ui/follow/first_follow_view_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+class FaviconLoader;
 @protocol FirstFollowViewDelegate;
 @class FollowedWebChannel;
 
@@ -20,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Delegate to execute actions triggered in this UI.
 @property(nonatomic, weak) id<FirstFollowViewDelegate> delegate;
+
+// FaviconLoader retrieves favicons for a given page URL.
+@property(nonatomic, assign) FaviconLoader* faviconLoader;
 
 @end
 
