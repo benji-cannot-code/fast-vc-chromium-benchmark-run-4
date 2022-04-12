5047a6400e38162ac329cbdb9cc1dc2ff4ee28fb
Angela Yoeurng <yoangela@chromium.org>
2022-04-12 00:22:42 +0000

    [omnibox] Add starter_pack_id column to keywords table.
    This CL Adds a new starter_pack_id column to the keywords table. This
    value will be used to identify which search engines in the table are
    a starter pack scope.  This CL has no other functional changes.
    
    Bug: 1287313
    Change-Id: I3f772e2cd04f9d5e8d3cbea9305f77c3c5ba7645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552461
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991290}

--- components/search_engines/default_search_manager.h
+++ components/search_engines/default_search_manager.h
@@ -65,6 +65,7 @@ class DefaultSearchManager {
   static const char kCreatedFromPlayAPI[];
   static const char kPreconnectToSearchUrl[];
   static const char kIsActive[];
+  static const char kStarterPackId[];
 
   enum Source {
     // Default search engine chosen either from prepopulated engines set for
