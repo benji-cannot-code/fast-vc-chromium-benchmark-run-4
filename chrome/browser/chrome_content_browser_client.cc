a9ecb896887646a5e3ffbfe33328b8f587bfc0c4
Lei Zhang <thestig@chromium.org>
2022-04-12 02:21:40 +0000

    Remove unused cloud_devices code.
    After recent Cloud Print code deletions,
    components/cloud_devices/common/cloud_devices_urls* and
    components/cloud_devices/common/cloud_devices_switches* are just dead
    code now.
    
    Bug: 1162164
    Change-Id: I4c29a9efb29c648c5277b6cd9bdcbda751b3d6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572211
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991334}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -178,7 +178,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/blocked_content/popup_blocker.h"
 #include "components/browsing_topics/browsing_topics_service.h"
 #include "components/captive_portal/core/buildflags.h"
-#include "components/cloud_devices/common/cloud_devices_switches.h"
 #include "components/content_settings/browser/page_specific_content_settings.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
@@ -2426,7 +2425,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
 #endif
       switches::kAllowInsecureLocalhost,
       switches::kAppsGalleryURL,
-      switches::kCloudPrintURL,
       switches::kDisableJavaScriptHarmonyShipping,
       variations::switches::kEnableBenchmarking,
       switches::kEnableDistillabilityService,
--- chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/printing/printer_capabilities.h"
 #include "chromeos/crosapi/mojom/local_printer.mojom.h"
 #include "chromeos/printing/printer_configuration.h"
-#include "components/cloud_devices/common/cloud_devices_urls.h"
 #include "components/signin/public/identity_manager/scope_set.h"
 #include "content/public/browser/web_ui.h"
 #include "mojo/public/cpp/bindings/receiver.h"
