564bc8efe398606863eee617ae0d1b8b92cadffa
Matt Jones <mdjones@chromium.org>
2022-04-12 15:25:33 +0000

    [Shopping Service] Add plumbing to allow use in java
    This patch adds the infrastructure for accessing the shopping service
    from java in a similar way to how it is used in c++. A static method
    on a factory class in //chrome/browser/commerce provides a handle to
    the service which is defined in //components/commerce. The lifecycle
    of the jni bridge is tied to the keyed service in a 1:1 relationship.
    
    Bug: 1308785
    Change-Id: If5f346e828b6b7ad4ba2ac275ae17f22191410de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573385
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991534}

--- components/commerce/core/shopping_service.h
+++ components/commerce/core/shopping_service.h
@@ -6,11 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_COMMERCE_CORE_SHOPPING_SERVICE_H_
 #define COMPONENTS_COMMERCE_CORE_SHOPPING_SERVICE_H_
 
+#include "base/supports_user_data.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace commerce {
 
-class ShoppingService : public KeyedService {
+class ShoppingService : public KeyedService, public base::SupportsUserData {
  public:
   ShoppingService() = default;
   ~ShoppingService() override = default;
