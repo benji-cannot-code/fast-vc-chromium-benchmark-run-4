a35f490162749e45bd0aaa127ea3d48805095ff6
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-04-12 22:14:10 +0000

    [PriceTracking] Include price track offer data to subscriptions
    To make price drop notifications more accurate, we need to add offer id,
    user-seen price, and country code to subscriptions.
    
    For now, the newly added fields are only used to improve notification
    accuracy, and aren't considered as part of subscription identifier. So
    we only store them on the server, not in local db. Also we don't need
    to get these info back from the server.
    
    Bug: 1303212
    Change-Id: Idfa34b5504b3cedd3c542542cc7e2812541ec5e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557400
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991756}

--- chrome/browser/commerce/subscriptions/android/java/src/org/chromium/chrome/browser/subscriptions/CommerceSubscription.java
+++ chrome/browser/commerce/subscriptions/android/java/src/org/chromium/chrome/browser/subscriptions/CommerceSubscription.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.subscriptions;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.StringDef;
 
 import org.chromium.base.annotations.CalledByNative;
@@ -49,6 +50,22 @@ public class CommerceSubscription {
         String PRODUCT_CLUSTER_ID = "PRODUCT_CLUSTER_ID";
     }
 
+    /** The price track offer data specific to price track subscriptions. */
+    public static class PriceTrackableOffer {
+        public PriceTrackableOffer(@Nullable String offerId, @Nullable String currentPrice,
+                @Nullable String countryCode) {
+            this.offerId = offerId;
+            this.currentPrice = currentPrice;
+            this.countryCode = countryCode;
+        }
+        /** Associated offer id */
+        public final String offerId;
+        /** Current price upon subscribing */
+        public final String currentPrice;
+        /** Country code of the offer */
+        public final String countryCode;
+    }
+
     public static final long UNSAVED_SUBSCRIPTION = -1L;
 
     private final long mTimestamp;
@@ -60,7 +77,11 @@ public class CommerceSubscription {
     private final @SubscriptionManagementType String mManagementType;
     @NonNull
     private final @TrackingIdType String mTrackingIdType;
+    @Nullable
+    private final PriceTrackableOffer mSeenOffer;
 
+    // TODO(crbug.com/1311754): Clean up this api.
+    @Deprecated
     public CommerceSubscription(@NonNull @CommerceSubscriptionType String type,
             @NonNull String trackingId, @NonNull @SubscriptionManagementType String managementType,
             @NonNull @TrackingIdType String trackingIdType) {
@@ -71,11 +92,26 @@ public class CommerceSubscription {
     CommerceSubscription(@NonNull @CommerceSubscriptionType String type, @NonNull String trackingId,
             @NonNull @SubscriptionManagementType String managementType,
             @NonNull @TrackingIdType String trackingIdType, long timestamp) {
+        this(type, trackingId, managementType, trackingIdType, timestamp, null);
+    }
+
+    public CommerceSubscription(@NonNull @CommerceSubscriptionType String type,
+            @NonNull String trackingId, @NonNull @SubscriptionManagementType String managementType,
+            @NonNull @TrackingIdType String trackingIdType,
+            @Nullable PriceTrackableOffer seenOffer) {
+        this(type, trackingId, managementType, trackingIdType, UNSAVED_SUBSCRIPTION, seenOffer);
+    }
+
+    private CommerceSubscription(@NonNull @CommerceSubscriptionType String type,
+            @NonNull String trackingId, @NonNull @SubscriptionManagementType String managementType,
+            @NonNull @TrackingIdType String trackingIdType, long timestamp,
+            @Nullable PriceTrackableOffer seenOffer) {
         mTrackingId = trackingId;
         mType = type;
         mManagementType = managementType;
         mTrackingIdType = trackingIdType;
         mTimestamp = timestamp;
+        mSeenOffer = seenOffer;
     }
 
     long getTimestamp() {
@@ -99,6 +135,10 @@ public class CommerceSubscription {
     @SubscriptionManagementType
     public String getManagementType() {
         return mManagementType;
+    }
+
+    public PriceTrackableOffer getSeenOffer() {
+        return mSeenOffer;
     }
 
     @CalledByNative
