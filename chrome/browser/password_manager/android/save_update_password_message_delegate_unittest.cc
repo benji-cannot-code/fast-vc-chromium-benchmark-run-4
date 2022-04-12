61e3a487c4f25296f1351355e4f96177600a0ad2
Anna Tsvirchkova <atsvirchkova@google.com>
2022-04-12 13:25:57 +0000

    [UPM] Change cogwheel menu button caption for save password message
    Screenshot:
    - https://screenshot.googleplex.com/54RW5UgXzywySEW
    
    Bug: 1271552
    Change-Id: Icb15a15401a7474fba22c674c2d6e9aeeba8dcda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579332
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#991492}

--- chrome/browser/password_manager/android/save_update_password_message_delegate_unittest.cc
+++ chrome/browser/password_manager/android/save_update_password_message_delegate_unittest.cc
@@ -326,8 +326,6 @@ TEST_P(SaveUpdatePasswordMessageDelegateTest,
 
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_SAVE_BUTTON),
             GetMessageWrapper()->GetPrimaryButtonText());
-  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_BLOCKLIST_BUTTON),
-            GetMessageWrapper()->GetSecondaryButtonMenuText());
 
   if (IsParamFeatureEnabled()) {
     // password_manager::features::kUnifiedPasswordManagerAndroid is enabled
@@ -338,7 +336,9 @@ TEST_P(SaveUpdatePasswordMessageDelegateTest,
     EXPECT_EQ(ResourceMapper::MapToJavaDrawableId(
                   IDR_ANDROID_PASSWORD_MANAGER_LOGO_24DP),
               GetMessageWrapper()->GetIconResourceId());
-
+    EXPECT_EQ(
+        l10n_util::GetStringUTF16(IDS_PASSWORD_MESSAGE_NEVER_SAVE_MENU_ITEM),
+        GetMessageWrapper()->GetSecondaryButtonMenuText());
   } else {
     EXPECT_EQ(l10n_util::GetStringUTF16(IDS_SAVE_PASSWORD),
               GetMessageWrapper()->GetTitle());
@@ -351,6 +351,8 @@ TEST_P(SaveUpdatePasswordMessageDelegateTest,
     EXPECT_EQ(
         ResourceMapper::MapToJavaDrawableId(IDR_ANDROID_INFOBAR_SAVE_PASSWORD),
         GetMessageWrapper()->GetIconResourceId());
+    EXPECT_EQ(l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_BLOCKLIST_BUTTON),
+              GetMessageWrapper()->GetSecondaryButtonMenuText());
   }
 
   EXPECT_EQ(ResourceMapper::MapToJavaDrawableId(IDR_ANDROID_MESSAGE_SETTINGS),
