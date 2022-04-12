c494bd98c1fc45ca373addd2bfa9c93e6e3f32dc
Hans Wennborg <hans@chromium.org>
2022-04-12 16:51:15 +0000

    Make components/autofill_assistant/browser/metrics.h cheaper to include
    In particular, don't include service.pb.h, move the operator<<
    implementations out-of-line, and use forward declarations for everything
    that can be forward declared.
    
    Bug: 1297561
    Change-Id: I686dcfb4c1f8a8f23ba8afaedcf2eeaffc7bca90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582229
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991575}

--- components/autofill_assistant/browser/starter.cc
+++ components/autofill_assistant/browser/starter.cc
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill_assistant {
 
-using StartupMode = StartupUtil::StartupMode;
-
 namespace {
 
 // When starting trigger scripts, depending on incoming script parameters, we
--- components/autofill_assistant/browser/startup_util_unittest.cc
+++ components/autofill_assistant/browser/startup_util_unittest.cc
@@ -18,29 +18,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill_assistant {
 
 // Note: this operator must be defined in the same namespace as the type it is
-// intended for, i.e., StartupUtil::StartupMode.
-std::ostream& operator<<(std::ostream& out,
-                         const StartupUtil::StartupMode& result) {
+// intended for, i.e., StartupMode.
+std::ostream& operator<<(std::ostream& out, const StartupMode& result) {
   switch (result) {
-    case StartupUtil::StartupMode::FEATURE_DISABLED:
+    case StartupMode::FEATURE_DISABLED:
       out << "FEATURE_DISABLED";
       break;
-    case StartupUtil::StartupMode::MANDATORY_PARAMETERS_MISSING:
+    case StartupMode::MANDATORY_PARAMETERS_MISSING:
       out << "MANDATORY_PARAMETERS_MISSING";
       break;
-    case StartupUtil::StartupMode::SETTING_DISABLED:
+    case StartupMode::SETTING_DISABLED:
       out << "SETTING_DISABLED";
       break;
-    case StartupUtil::StartupMode::NO_INITIAL_URL:
+    case StartupMode::NO_INITIAL_URL:
       out << "NO_INITIAL_URL";
       break;
-    case StartupUtil::StartupMode::START_REGULAR:
+    case StartupMode::START_REGULAR:
       out << "START_REGULAR";
       break;
-    case StartupUtil::StartupMode::START_BASE64_TRIGGER_SCRIPT:
+    case StartupMode::START_BASE64_TRIGGER_SCRIPT:
       out << "START_BASE64_TRIGGER_SCRIPT";
       break;
-    case StartupUtil::StartupMode::START_RPC_TRIGGER_SCRIPT:
+    case StartupMode::START_RPC_TRIGGER_SCRIPT:
       out << "START_RPC_TRIGGER_SCRIPT";
       break;
   }
@@ -49,7 +48,6 @@ std::ostream& operator<<(std::ostream& out,
 
 namespace {
 
-using StartupMode = StartupUtil::StartupMode;
 using features::kAutofillAssistant;
 using features::kAutofillAssistantChromeEntry;
 using features::kAutofillAssistantLoadDFMForTriggerScripts;
@@ -126,7 +124,7 @@ std::ostream& operator<<(std::ostream& out, const TestFeatureConfig& config) {
   return out;
 }
 
-std::string ToString(const StartupUtil::StartupMode& result) {
+std::string ToString(const StartupMode& result) {
   std::ostringstream stream;
   stream << result;
   return stream.str();
