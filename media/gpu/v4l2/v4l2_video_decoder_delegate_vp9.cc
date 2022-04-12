91d8576e95854d8ecb11b3e60736698ce549b159
Chen-Yu Tsai <wenst@chromium.org>
2022-04-12 04:10:27 +0000

    media/gpu/v4l2: Fix Chromium style error for V4L2VideoDecoderDelegateVP9*
    Chromium style dictates that "virtual methods with non-empty bodies
    shouldn't be declared inline."
    
    Move the body of V4L2VideoDecoderDelegateVP9*::NeedsCompressedHeaderParsed()
    and V4L2VideoDecoderDelegateVP9*::SupportsContextProbabilityReadback()
    to their respective .cc files.
    
    BUG=none
    TEST=Build for standard Linux
    
    Change-Id: I14f239bec69d0899f712a4de4cadb91c4aec1d9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581220
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991354}

--- media/gpu/v4l2/v4l2_video_decoder_delegate_vp9.cc
+++ media/gpu/v4l2/v4l2_video_decoder_delegate_vp9.cc
@@ -338,4 +338,12 @@ bool V4L2VideoDecoderDelegateVP9::GetFrameContext(scoped_refptr<VP9Picture> pic,
   return false;
 }
 
+bool V4L2VideoDecoderDelegateVP9::NeedsCompressedHeaderParsed() const {
+  return supports_compressed_header_;
+}
+
+bool V4L2VideoDecoderDelegateVP9::SupportsContextProbabilityReadback() const {
+  return false;
+}
+
 }  // namespace media
