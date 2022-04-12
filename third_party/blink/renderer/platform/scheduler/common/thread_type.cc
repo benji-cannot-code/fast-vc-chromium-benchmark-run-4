5d9844cd602c9586210f979c2a0414d9b3031e2c
Yoshifumi Inoue <yosin@chromium.org>
2022-04-12 08:56:05 +0000

    [parallel-text-shaping] Introduce ThreadType::kFontThread type
    This patch introduces `ThreadType::kFontThread` to help analyzing metrics
    and tracing with font related tasks for preparation of parallel text
    shaping[1].
    
    Note: We can see "Font thread" in chrome://tracing.
    
    [1] http://crrev.com/c/3211416 Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: I60c3713dca3073334cb64ea5f42743b826c619a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583583
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991421}

--- third_party/blink/renderer/platform/scheduler/common/thread_type.cc
+++ third_party/blink/renderer/platform/scheduler/common/thread_type.cc
@@ -48,6 +48,8 @@ const char* GetNameForThreadType(ThreadType thread_type) {
       return "Realtime AudioWorklet thread";
     case ThreadType::kSemiRealtimeAudioWorkletThread:
       return "Semi-Realtime AudioWorklet thread";
+    case ThreadType::kFontThread:
+      return "Font thread";
   }
   return nullptr;
 }
