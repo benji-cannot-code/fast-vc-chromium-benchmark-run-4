2702221b2c8c8b4b61af507d229829ee8300f3cf
Angela Yoeurng <yoangela@chromium.org>
2022-04-12 19:40:09 +0000

    [omnibox][search_engines] Define starter pack search engines data.
    This CL Defines the starter pack search engines and provides some
    util functions.  These are not called anywhere at the moment, so
    there are no functional changes in this CL.
    
    Bug: 1287313
    Change-Id: I35dd31ad88eced4f627c1087d084187a89884c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558398
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991675}

--- components/search_engines/template_url_data_util.cc
+++ components/search_engines/template_url_data_util.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/default_search_manager.h"
 #include "components/search_engines/prepopulated_engines.h"
 #include "components/search_engines/template_url_data.h"
+#include "components/search_engines/template_url_starter_pack_data.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 namespace {
@@ -385,4 +387,17 @@ std::unique_ptr<TemplateURLData> TemplateURLDataFromOverrideDictionary(
         preconnect_to_search_url.compare("ALLOWED") == 0, *id);
   }
   return nullptr;
+}
+
+std::unique_ptr<TemplateURLData> TemplateURLDataFromStarterPackEngine(
+    const TemplateURLStarterPackData::StarterPackEngine& engine) {
+  auto turl = std::make_unique<TemplateURLData>();
+  turl->SetShortName(l10n_util::GetStringUTF16(engine.name_message_id));
+  turl->SetKeyword(u"@" + l10n_util::GetStringUTF16(engine.keyword_message_id));
+  turl->SetURL(engine.search_url);
+  turl->favicon_url = GURL(ToStringPiece(engine.favicon_url));
+  turl->starter_pack_id = engine.id;
+  turl->is_active = TemplateURLData::ActiveStatus::kTrue;
+
+  return turl;
 }
