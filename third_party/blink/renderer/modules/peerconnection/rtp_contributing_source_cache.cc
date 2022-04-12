34ed9122fb1f2efc3679bd03312b2b8f7553fa94
Henrik Boström <hbos@chromium.org>
2022-04-12 08:22:45 +0000

    RtpContributingSourceCache: Avoid theoretical use-after-free.
    The `pc_` reference is a WeakPersistent, so it could become null causing
    MaybeUpdateRtpSources() not to update the sources, resulting in
    `rtp_sources` being null. This CL adds a guard to prevent
    use-after-free.
    
    Just a sanity check - I don't think this can happen in practise due to
    RTCRtpReceiver having a strong reference to the PC which would prevent
    the PC from going away while calling getContributingSources() is still
    a possibility. But in case some assumption changes in the future...
    
    Bug: chromium:1150311
    Change-Id: If66cc8492f457c145f380adbbebaf1ab594974d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578751
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991405}

--- third_party/blink/renderer/modules/peerconnection/rtp_contributing_source_cache.cc
+++ third_party/blink/renderer/modules/peerconnection/rtp_contributing_source_cache.cc
@@ -26,6 +26,8 @@ RTCRtpSynchronizationSourcesFromRTCRtpSources(
       window->GetFrame()->Loader().GetDocumentLoader()->GetTiming();
 
   HeapVector<Member<RTCRtpSynchronizationSource>> synchronization_sources;
+  if (!rtp_sources)
+    return synchronization_sources;
   for (const auto& rtp_source : *rtp_sources) {
     if (rtp_source->SourceType() != RTCRtpSource::Type::kSSRC)
       continue;
@@ -61,6 +63,8 @@ RTCRtpContributingSourcesFromRTCRtpSources(
       window->GetFrame()->Loader().GetDocumentLoader()->GetTiming();
 
   HeapVector<Member<RTCRtpContributingSource>> contributing_sources;
+  if (!rtp_sources)
+    return contributing_sources;
   for (const auto& rtp_source : *rtp_sources) {
     if (rtp_source->SourceType() != RTCRtpSource::Type::kCSRC)
       continue;
