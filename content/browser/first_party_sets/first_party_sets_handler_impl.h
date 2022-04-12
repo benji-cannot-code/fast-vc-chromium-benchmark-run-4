79d8c2fdb42ffed752f102b2e63ca8cc2afc5514
Kirubel Aklilu <kaklilu@chromium.org>
2022-04-12 16:10:50 +0000

    Update FirstPartySetsHandlerImpl to get the First-Party Sets Overrides.
    This CL:
    - Updates the FirstPartySetsHandlerImpl to retrieve the policy value
      from the Content API.
    - Updates the FirstPartySetsLoader constructor to take in the policy
      set overrides.
    
    Bug: 1244990, 1277128
    Change-Id: I6a85d98d2e375cf63025c1e3e3dcefa95e2dcdbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573277
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991556}

--- content/browser/first_party_sets/first_party_sets_handler_impl.h
+++ content/browser/first_party_sets/first_party_sets_handler_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_annotations.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/values.h"
 #include "content/browser/first_party_sets/first_party_sets_loader.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/first_party_sets_handler.h"
--- content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "content/browser/first_party_sets/first_party_set_parser.h"
 #include "content/public/browser/first_party_sets_handler.h"
+#include "net/base/schemeful_site.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
