012ec4f56343936415e95a91597d612cbb523ecd
Joanmarie Diggs <jdiggs@igalia.com>
2022-04-13 18:24:12 +0000

    Regression test for https://crbug.com/1314270
    The check failure was fixed by crrev.com/c/3584763, but we lack
    test coverage for the regression. This commit adds that test.
    
    Bug: 1314270
    Change-Id: I561f6a368bf0cc12f23e9a46758622be6e96a3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579272
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Auto-Submit: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#992142}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
@@ -711,6 +711,12 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    public void test_immediateRefresh() {
+        performTest("immediate-refresh.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
     public void test_innerHtmlChanged() {
         performTest("inner-html-change.html", EMPTY_EXPECTATIONS_FILE);
     }
