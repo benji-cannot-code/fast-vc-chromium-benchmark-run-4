e695e6e2f86f5e46c8f3f83bd714097706c33b22
Brandon Jones <bajones@chromium.org>
2022-04-13 01:22:52 +0000

    Implement GPUQueueDescriptor, device/queue labels
    Implements GPUQueueDescriptor as well as communicating label attribute
    changes for GPUDevice/GPUQueue to Dawn. Labels from the descriptors are
    not passed to Dawn at the time of requestDevice due to how the
    descriptor is currently being passed, but is instead set using the
    setLabel() method immediately after creation. This should generally not
    be observable unless there are errors during device creation that would
    have included the label.
    
    Bug: dawn:1323
    Change-Id: I27e608cb01ed76f9cdefd1740c3dcb370661409d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569265
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991837}

--- third_party/blink/renderer/modules/webgpu/gpu_device.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_device_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_external_texture_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_feature_name.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_queue_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_render_pipeline_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_uncaptured_error_event_init.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_gpuoutofmemoryerror_gpuvalidationerror.h"
@@ -99,6 +100,9 @@ GPUDevice::GPUDevice(ExecutionContext* execution_context,
 
   if (descriptor->hasLabel())
     setLabel(descriptor->label());
+
+  if (descriptor->defaultQueue()->hasLabel())
+    queue_->setLabel(descriptor->defaultQueue()->label());
 }
 
 GPUDevice::~GPUDevice() {
