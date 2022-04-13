c6f8f0dc2bd4dab405f9eeb0f7a4fcfc25d6370e
George Benz <gbj@google.com>
2022-04-13 04:36:32 +0000

    Adds feature flag for cast device storage.
    This feature flag protects against persistent storage of Access Code
    Cast devices.
    
    Bug: b/223249548
    Change-Id: I9145fabc86d5f1c1ae7cb6e97f8c7aeec0f6bbf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584726
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: George Benz <gbj@google.com>
    Cr-Commit-Position: refs/heads/main@{#991886}

--- chrome/browser/media/router/discovery/access_code/access_code_cast_feature.cc
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_feature.cc
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #endif
 
+namespace features {
+// Enables remembering of access code cast devices.
+const base::Feature kAccessCodeCastRememberDevices{
+    "AccessCodeCastRememberDevices", base::FEATURE_DISABLED_BY_DEFAULT};
+}  // namespace features
+
 namespace media_router {
 
 #if !BUILDFLAG(IS_ANDROID)
--- chrome/browser/media/router/discovery/access_code/access_code_cast_feature.h
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_feature.h
@@ -16,6 +16,10 @@ namespace base {
 class TimeDelta;
 }
 
+namespace features {
+extern const base::Feature kAccessCodeCastRememberDevices;
+}
+
 namespace media_router {
 
 #if !BUILDFLAG(IS_ANDROID)
