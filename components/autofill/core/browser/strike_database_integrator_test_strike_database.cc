a18e3fb5685f1155be49519e24763a1b690303d4
Siyu An <siyua@chromium.org>
2022-04-13 02:46:06 +0000

    [VCN] Expose last_updated_timestamp for strike database integrator
    Follow up CL will add delay for VCN enrollment
    
    dd:https://docs.google.com/document/d/1-1pV0_j1IU-KkknTtb_7oMZdtQRebILzIVynTSbiFqc/edit#
    
    Bug: 1304328,1281695
    Change-Id: I435b5be663cb06f8254edd344afdef480924494d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566020
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991867}

--- components/autofill/core/browser/strike_database_integrator_test_strike_database.cc
+++ components/autofill/core/browser/strike_database_integrator_test_strike_database.cc
@@ -61,6 +61,12 @@ void StrikeDatabaseIntegratorTestStrikeDatabase::SetUniqueIdsRequired(
   unique_ids_required_ = unique_ids_required;
 }
 
+void StrikeDatabaseIntegratorTestStrikeDatabase::
+    SetRequiredDelaySinceLastStrike(
+        base::TimeDelta required_delay_since_last_strike) {
+  required_delay_since_last_strike_ = required_delay_since_last_strike;
+}
+
 absl::optional<size_t>
 StrikeDatabaseIntegratorTestStrikeDatabase::GetMaximumEntries() const {
   return maximum_entries_;
@@ -71,4 +77,11 @@ StrikeDatabaseIntegratorTestStrikeDatabase::GetMaximumEntriesAfterCleanup()
     const {
   return maximum_entries_after_cleanup_;
 }
+
+absl::optional<base::TimeDelta>
+StrikeDatabaseIntegratorTestStrikeDatabase::GetRequiredDelaySinceLastStrike()
+    const {
+  return required_delay_since_last_strike_;
+}
+
 }  // namespace autofill
