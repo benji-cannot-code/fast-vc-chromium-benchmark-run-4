7668fcd9773ce6324ea3fefce11a0adb5c70d1b5
Ivana Žužić <izuzic@google.com>
2022-04-13 11:51:18 +0000

    [Android][UPM] Latency and success metrics for FillMatchingLoginsAsync
    Latency and success metrics are logged for the method
    FillMatchingLoginsAsync of PasswordStoreBuiltInBackend so we can
    compare them with the metrics from the same method of
    PasswordStoreAndroidBackend.
    
    Bug: 1298467
    Change-Id: I1faf4f8c8a2d090706bcbcda0407c64cd723bcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581728
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#991982}

--- components/password_manager/core/browser/login_database_async_helper.h
+++ components/password_manager/core/browser/login_database_async_helper.h
@@ -47,8 +47,10 @@ class LoginDatabaseAsyncHelper : private PasswordStoreSync {
       PasswordStoreBackendMetricsRecorder metrics_recorder);
   LoginsResult GetAutofillableLogins(
       PasswordStoreBackendMetricsRecorder metrics_recorder);
-  LoginsResult FillMatchingLogins(const std::vector<PasswordFormDigest>& forms,
-                                  bool include_psl);
+  LoginsResult FillMatchingLogins(
+      const std::vector<PasswordFormDigest>& forms,
+      bool include_psl,
+      PasswordStoreBackendMetricsRecorder metrics_recorder);
   PasswordStoreChangeList AddLogin(
       const PasswordForm& form,
       PasswordStoreBackendMetricsRecorder metrics_recorder);
