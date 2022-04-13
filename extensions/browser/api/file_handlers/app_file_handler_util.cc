bc6876a56a91c071c4dc9e3907cbe575369f903d
Samuel Huang <huangs@chromium.org>
2022-04-13 02:44:41 +0000

    [Lacros] Extract extensions::app_file_handler_util::CreateEntryInfos().
    This CL extracts PlatformAppPathLauncher code snippet to initialize a
    vector of extensions::EntryInfo to new function
    
      extensions::app_file_handler_util::CreateEntryInfos(),
    
    with matching unit test
    
      FileHandlersAppFileHandlerUtilTest.CreateEntryInfos.
    
    The function will be used by upcoming CL to support fileBrowserHandler
    API for Extensions in Lacros.
    
    Bug: 1275654
    Change-Id: I60b7de659b899d128c06df733895f32692ecff2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577410
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991866}

--- extensions/browser/api/file_handlers/app_file_handler_util.cc
+++ extensions/browser/api/file_handlers/app_file_handler_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/containers/contains.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -41,6 +42,7 @@ namespace extensions {
 
 namespace app_file_handler_util {
 
+const char kFallbackMimeType[] = "application/octet-stream";
 const char kInvalidParameters[] = "Invalid parameters";
 const char kSecurityError[] = "Security error";
 
@@ -503,6 +505,21 @@ bool ValidateFileEntryAndGetPath(const std::string& filesystem_name,
   }
 
   return true;
+}
+
+std::vector<extensions::EntryInfo> CreateEntryInfos(
+    const std::vector<base::FilePath>& entry_paths,
+    const std::vector<std::string>& mime_types,
+    const std::set<base::FilePath>& directory_paths) {
+  CHECK_EQ(entry_paths.size(), mime_types.size());
+  std::vector<extensions::EntryInfo> entry_infos;
+  for (size_t i = 0; i < entry_paths.size(); ++i) {
+    const std::string mime_type =
+        mime_types[i].empty() ? kFallbackMimeType : mime_types[i];
+    bool is_directory = base::Contains(directory_paths, entry_paths[i]);
+    entry_infos.emplace_back(entry_paths[i], mime_type, is_directory);
+  }
+  return entry_infos;
 }
 
 }  // namespace app_file_handler_util
