896201d0c307349b1e7f2e2d6b300ce2f7f79e76
Dana Fried <dfried@chromium.org>
2022-04-13 19:42:13 +0000

    Fix ElementTrackerViews handling of newly-shown widgets.
    Previously, we had watched widgets that were not yet visible, and then
    propagated kShown events when the OnWidgetVisibilityChanged() event
    happened (at which point we stopped watching).
    
    However, that produced a corner case that was bad:
     - View with an ElementIdentifier is added to a widget
     - Widget is shown
     - OnWidgetVisibilityChanged(true) called
     - Elements are created
     - kShown ElementTracker event is sent
     - Event callback sets another view's ID
     - Because Widget::IsVisible() is not yet true*, the element is not
       considered shown and no event is sent
    
    * This is a deficiency of Views/Aura
    
    These changes ensure that if the OnWidgetVisibilityChanged() event comes
    in before Widget::IsVisible() starts to return true we remember that the
    Widget is visible for long enough to ensure that other Views added to
    the Widget during that window that we know to immediately send kShown
    events for them.
    
    We do this by keeping the WidgetObserver around for a bit longer, and
    simply remembering whether we saw it become visible or not.
    
    The move to individual (small) private observer objects rather than
    having ElementTrackerViews implement WidgetObserver is also beneficial
    because it reduces the complexity of the ElementTrackerViews interface.
    
    Change-Id: I7887191f1f6325ca45e3798d5c21ea2166ad701c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576677
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992173}

--- ui/views/interaction/element_tracker_views.h
+++ ui/views/interaction/element_tracker_views.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <vector>
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/no_destructor.h"
 #include "base/scoped_multi_source_observation.h"
@@ -16,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_tracker.h"
 #include "ui/views/views_export.h"
-#include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_observer.h"
 
 namespace views {
 
@@ -42,7 +41,7 @@ class VIEWS_EXPORT TrackedElementViews : public ui::TrackedElement {
 };
 
 // Manages TrackedElements associated with View objects.
-class VIEWS_EXPORT ElementTrackerViews : private WidgetObserver {
+class VIEWS_EXPORT ElementTrackerViews {
  public:
   using ViewList = std::vector<View*>;
 
@@ -132,25 +131,28 @@ class VIEWS_EXPORT ElementTrackerViews : private WidgetObserver {
 
  private:
   friend class base::NoDestructor<ElementTrackerViews>;
+  FRIEND_TEST_ALL_PREFIXES(ElementTrackerViewsTest, CleansUpWidgetTrackers);
   class ElementDataViews;
+  class WidgetTracker;
 
   ElementTrackerViews();
-  ~ElementTrackerViews() override;
-
-  // WidgetObserver:
-  void OnWidgetVisibilityChanged(Widget* widget, bool visible) override;
-  void OnWidgetDestroying(Widget* widget) override;
+  ~ElementTrackerViews();
 
   // We do not get notified at the View level if a view's widget has not yet
   // been shown. We need this notification to know when the view is actually
   // visible to the user. So if a view is added to the trakcer or is added to
   // a widget, and its widget is not visible, we watch it until it is (or it is
   // destroyed).
-  void MaybeObserveWidget(Widget* widget);
+  void MaybeTrackWidget(Widget* widget);
+
+  // Keep track of widgets for which we've received an
+  // OnWidgetVisibilityChanged(true) event for but which are still reporting
+  // IsVisible() = false. This happens because visibility of native window in
+  // Aura is not exactly synced with our event reporting.
+  bool IsWidgetVisible(const Widget* widget) const;
 
   std::map<ui::ElementIdentifier, ElementDataViews> element_data_;
-  base::ScopedMultiSourceObservation<Widget, WidgetObserver> widget_observer_{
-      this};
+  std::map<const Widget*, WidgetTracker> widget_trackers_;
 };
 
 }  // namespace views
