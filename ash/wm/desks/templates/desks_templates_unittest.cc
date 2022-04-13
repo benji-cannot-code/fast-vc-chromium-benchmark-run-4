f0bca1dae8cde148486ec82217e450305089c481
Daniel Andersson <dandersson@chromium.org>
2022-04-13 21:46:31 +0000

    Ash: remove OverviewHighlightController::TestApi.
    This TestApi no longer offers anything that can't be done using the
    public API.
    
    Change-Id: Iacd4070815b336e10ab7e548c2e72aa77f1e55dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583079
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992246}

--- ash/wm/desks/templates/desks_templates_unittest.cc
+++ ash/wm/desks/templates/desks_templates_unittest.cc
@@ -333,9 +333,7 @@ class DesksTemplatesTest : public OverviewTestBase {
   }
 
   OverviewHighlightableView* GetHighlightedView() {
-    return OverviewHighlightController::TestApi(
-               GetOverviewSession()->highlight_controller())
-        .GetHighlightView();
+    return GetOverviewSession()->highlight_controller()->highlighted_view();
   }
 
   // Opens overview mode and then clicks the save template button. This should
--- ash/wm/overview/overview_highlight_controller.cc
+++ ash/wm/overview/overview_highlight_controller.cc
@@ -32,23 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// -----------------------------------------------------------------------------
-// OverviewHighlightController::TestApi
-
-OverviewHighlightController::TestApi::TestApi(
-    OverviewHighlightController* highlight_controller)
-    : highlight_controller_(highlight_controller) {}
-
-OverviewHighlightController::TestApi::~TestApi() = default;
-
-OverviewHighlightableView*
-OverviewHighlightController::TestApi::GetHighlightView() const {
-  return highlight_controller_->highlighted_view_;
-}
-
-// -----------------------------------------------------------------------------
-// OverviewHighlightController
-
 OverviewHighlightController::OverviewHighlightController(
     OverviewSession* overview_session)
     : overview_session_(overview_session),
