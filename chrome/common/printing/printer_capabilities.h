38ea0756f71300094206a6ec06590b0dc759711c
Lei Zhang <thestig@chromium.org>
2022-04-13 17:49:22 +0000

    Remove unused GetPrinterNameAndDescription() declaration.
    The definition got deleted in crrev.com/664182.
    
    Change-Id: If876c41fc8102efc2d910d91ba3c9c326106f003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578231
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992124}

--- chrome/common/printing/printer_capabilities.h
+++ chrome/common/printing/printer_capabilities.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_COMMON_PRINTING_PRINTER_CAPABILITIES_H_
 
 #include <string>
-#include <utility>
 
 #include "base/memory/scoped_refptr.h"
 #include "base/values.h"
@@ -23,11 +22,6 @@ extern const char kPrinter[];
 #if BUILDFLAG(IS_WIN)
 std::string GetUserFriendlyName(const std::string& printer_name);
 #endif
-
-// Extracts the printer display name and description from the
-// appropriate fields in `printer` for the platform.
-std::pair<std::string, std::string> GetPrinterNameAndDescription(
-    const PrinterBasicInfo& printer);
 
 // Returns a value containing printer capabilities and settings for the device
 // registered as `device_name` in the `PrintBackend`.  The returned value is
