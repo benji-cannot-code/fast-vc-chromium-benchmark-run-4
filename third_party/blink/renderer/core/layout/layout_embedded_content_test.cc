b403f546c54e6f710bde31d42f3a4d8a9e6cf7fa
Khushal <khushalsagar@chromium.org>
2022-04-13 00:24:52 +0000

    blink: Use ReplacedContentTransform for object-fit scaling.
    Use the transform node generated for replaced contents to apply a scale
    when using object-fit on a fencedframe.
    
    Change-Id: Icfda5708eccdc60fac8d2683f8b84648302c52bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583244
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991819}

--- third_party/blink/renderer/core/layout/layout_embedded_content_test.cc
+++ third_party/blink/renderer/core/layout/layout_embedded_content_test.cc
@@ -61,6 +61,8 @@ TEST_F(LayoutEmbeddedContentTest, FreozenSizeReplacedContentRect) {
   Document& document = GetDocument();
   auto* element = MakeGarbageCollected<HTMLFreezableIFrameElement>(document);
   element->setAttribute(html_names::kSrcAttr, "http://example.com/");
+  element->SetInlineStyleProperty(CSSPropertyID::kObjectFit,
+                                  CSSValueID::kContain);
   document.body()->AppendChild(element);
   UpdateAllLifecyclePhasesForTest();
   auto* layout_object = element->GetLayoutFreezableIFrame();
@@ -69,8 +71,10 @@ TEST_F(LayoutEmbeddedContentTest, FreozenSizeReplacedContentRect) {
 
   layout_object->FreezeSizeForTesting(PhysicalSize(80, 50));
   UpdateAllLifecyclePhasesForTest();
-  // When the size is frozen, the content box is at the top-left corner.
-  EXPECT_EQ(layout_object->ReplacedContentRect(), PhysicalRect(2, 2, 80, 50));
+  // When the size is frozen, the content is rendered at the centre of the box
+  // and scale to fit based on object-fit:contain.
+  EXPECT_EQ(layout_object->ReplacedContentRect(),
+            PhysicalRect(32, 2, 240, 150));
 }
 
 }  // namespace blink
