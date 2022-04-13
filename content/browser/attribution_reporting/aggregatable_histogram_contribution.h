686438b79650793234cbdef31d5861f3e709556f
Alex Turner <alexmt@chromium.org>
2022-04-13 16:41:05 +0000

    Use (signed) int in AggregatableHistogramContribution value
    Per https://google.github.io/styleguide/cppguide.html#Integer_Types,
    signed integers should not be used just because the integer will always
    be non-negative. (Unfortunately, we can't do the same for the key as
    that would narrow the space of possible keys. However, the maximum
    contribution value is well below 2^31.)
    
    Bug: 1303958
    Change-Id: I32d62d316696c4327aef31e562eebecf3f73efb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510292
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992087}

--- content/browser/attribution_reporting/aggregatable_histogram_contribution.h
+++ content/browser/attribution_reporting/aggregatable_histogram_contribution.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ATTRIBUTION_REPORTING_AGGREGATABLE_HISTOGRAM_CONTRIBUTION_H_
 #define CONTENT_BROWSER_ATTRIBUTION_REPORTING_AGGREGATABLE_HISTOGRAM_CONTRIBUTION_H_
 
-#include <stdint.h>
-
 #include "content/common/content_export.h"
 #include "third_party/abseil-cpp/absl/numeric/int128.h"
 
@@ -15,7 +13,7 @@ namespace content {
 
 class CONTENT_EXPORT AggregatableHistogramContribution {
  public:
-  AggregatableHistogramContribution(absl::uint128 key, uint32_t value);
+  AggregatableHistogramContribution(absl::uint128 key, int value);
   AggregatableHistogramContribution(
       const AggregatableHistogramContribution& other) = default;
   AggregatableHistogramContribution& operator=(
@@ -28,11 +26,11 @@ class CONTENT_EXPORT AggregatableHistogramContribution {
 
   absl::uint128 key() const { return key_; }
 
-  uint32_t value() const { return value_; }
+  int value() const { return value_; }
 
  private:
   absl::uint128 key_;
-  uint32_t value_;
+  int value_;
 };
 
 }  // namespace content
