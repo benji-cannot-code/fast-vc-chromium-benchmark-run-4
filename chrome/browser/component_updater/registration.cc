19a8c001f400135a8e8ced11c3b8e71bdb8f3a29
Matt Reichhoff <mreichhoff@chromium.org>
2022-04-13 17:31:07 +0000

    [UrlParamFilter] Register classification component
    Prior commits have enabled and tested the component. This CL registers
    it for use.
    
    Bug: 1316001
    Change-Id: Id8ef76036c4339662739881d533977ad83e66df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582487
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992113}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/ssl_error_assistant_component_installer.h"
 #include "chrome/browser/component_updater/subresource_filter_component_installer.h"
 #include "chrome/browser/component_updater/trust_token_key_commitments_component_installer.h"
+#include "chrome/browser/component_updater/url_param_classification_component_installer.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_paths.h"
 #include "components/component_updater/component_updater_service.h"
@@ -217,6 +218,8 @@ void RegisterComponentsForUpdate() {
 #if BUILDFLAG(IS_LINUX)
   RegisterScreenAIComponent(cus, g_browser_process->local_state());
 #endif  // BUILDFLAG(IS_LINUX)
+
+  RegisterUrlParamClassificationComponent(cus);
 }
 
 }  // namespace component_updater
