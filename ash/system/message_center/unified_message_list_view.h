ae7d8ca2b90a3f87236a8f22d08c0efa6e7db88e
Alex Newcomer <newcomer@chromium.org>
2022-04-13 00:08:02 +0000

    Have UMLV observe MV in a safer way
    UMLV observes MessageViews and never removes itself
    from the observer list.
    
    We have some crashes on AnimationEnd which seem to crash
    in the MessageView(s) call into observers. The only thing
    I can think of causing this is something to do with shutdown,
    where MessageView is shutting down, causing the animation to
    end, and UMLV has already shutdown, but the MessageView still has
    a pointer to the owning view, UMLV, via its observer list.
    
    So to rule this case out, make UMLV observe MessageView via a
    ScopedMultiSourceObservation. This will ensure UMLV removes itself
    from MessageView's ObserverList when UMLV is destroyed.
    
    If the crash still occurs, we will dig into this one more.
    
    Bug: 1307904
    Change-Id: Ieae66fc66e4fac1f4638131b5f970d13476f986a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573020
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991810}

--- ash/system/message_center/unified_message_list_view.h
+++ ash/system/message_center/unified_message_list_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/system/unified/unified_system_tray_model.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
 #include "ui/compositor/throughput_tracker.h"
 #include "ui/message_center/message_center.h"
@@ -153,6 +154,8 @@ class ASH_EXPORT UnifiedMessageListView
   virtual message_center::MessageView* CreateMessageView(
       const message_center::Notification& notification);
 
+  void ConfigureMessageView(message_center::MessageView* message_view);
+
   // Virtual for testing.
   virtual std::vector<message_center::Notification*> GetStackedNotifications()
       const;
@@ -284,6 +287,10 @@ class ASH_EXPORT UnifiedMessageListView
   base::ScopedObservation<message_center::MessageCenter,
                           message_center::MessageCenterObserver>
       message_center_observation_{this};
+
+  base::ScopedMultiSourceObservation<message_center::MessageView,
+                                     message_center::MessageView::Observer>
+      message_view_multi_source_observation_{this};
 };
 
 }  // namespace ash
