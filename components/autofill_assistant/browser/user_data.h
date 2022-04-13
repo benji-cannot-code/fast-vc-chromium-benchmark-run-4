0e271dca1921bc2563e48a70f90c0f57d6afff9c
Hans Wennborg <hans@chromium.org>
2022-04-13 08:30:22 +0000

    Remove two dead includes from autofill_assistant's user_data.h
    The types are already forward declared, and the .cc file already had the
    includes, so not sure why these were here.
    
    Bug: 1315557
    Change-Id: Ie5c08d25121805c9f60a1f4400c105424c5497d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582007
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#991937}

--- components/autofill_assistant/browser/user_data.h
+++ components/autofill_assistant/browser/user_data.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
-#include "components/autofill/core/browser/data_model/autofill_profile.h"
-#include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill_assistant/browser/cud_condition.pb.h"
 #include "components/autofill_assistant/browser/metrics.h"
 #include "components/autofill_assistant/browser/service.pb.h"
--- components/autofill_assistant/browser/web/mock_web_controller.h
+++ components/autofill_assistant/browser/web/mock_web_controller.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/time/time.h"
+#include "components/autofill/core/browser/data_model/autofill_profile.h"
+#include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill_assistant/browser/top_padding.h"
 #include "components/autofill_assistant/browser/web/element_finder.h"
 #include "components/autofill_assistant/browser/web/element_rect_getter.h"
