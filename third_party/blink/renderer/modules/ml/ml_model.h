f38a44bcbbe7d9758ffe17dca19a4d2464fbddb6
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-04-13 11:47:47 +0000

    Revert "webml: let blink use the mojo interfaces."
    This reverts commit 0558339f88dc4a61ad7601f0aeea3a01443eb6d9.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision 0558339f88dc4a61ad7601f0aeea3a01443eb6d9 as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8817006830395061665
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2F0558339f88dc4a61ad7601f0aeea3a01443eb6d9&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+0558339f88dc4a61ad7601f0aeea3a01443eb6d9
    
    Original change's description:
    > webml: let blink use the mojo interfaces.
    > 
    > Intent to prototype:
    > https://groups.google.com/a/chromium.org/g/blink-dev/c/w7Oq2_x2D8U/m/LOAfC3SdBwAJ
    > Explainer:
    > https://github.com/webmachinelearning/model-loader/blob/main/explainer.md
    > Chrome Status:
    > https://chromestatus.com/feature/6505078216196096
    > 
    > Bug: 1298707
    > Test: combined with the other CLs, the API works.
    > Change-Id: I807276e4366df2692e2b1ca7df1a8c25ad128eba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525653
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Honglin Yu <honglinyu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#991949}
    
    
    Change-Id: I1e286a6a1506d92d9812c6c54822663982078cf0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1298707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585097
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991977}

--- third_party/blink/renderer/modules/ml/ml_model.h
+++ third_party/blink/renderer/modules/ml/ml_model.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_MODEL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_MODEL_H_
 
-#include "components/ml/mojom/web_platform_model.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_tensor.h"
@@ -16,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -28,10 +26,7 @@ class MLModel final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  MLModel(
-      ExecutionContext* context,
-      mojo::PendingRemote<ml::model_loader::mojom::blink::Model> pending_remote,
-      ml::model_loader::mojom::blink::ModelInfoPtr model_info);
+  MLModel(ExecutionContext* context, DOMArrayBuffer* buffer);
 
   ~MLModel() override;
 
@@ -44,20 +39,6 @@ class MLModel final : public ScriptWrappable {
   HeapVector<Member<MLTensorInfo>> outputs(ScriptState* script_state);
 
   void Trace(Visitor* visitor) const override;
-
- private:
-  void OnComputeResult(
-      ScriptState* script_state,
-      ScriptPromiseResolver* resolver,
-      ml::model_loader::mojom::blink::ComputeResult result,
-      const absl::optional<HashMap<String, Vector<uint8_t>>>& outputs);
-
-  HeapMojoRemote<ml::model_loader::mojom::blink::Model> remote_model_;
-
-  HashMap<String, ml::model_loader::mojom::blink::TensorInfoPtr>
-      input_tensor_name_to_info_;
-  HashMap<String, ml::model_loader::mojom::blink::TensorInfoPtr>
-      output_tensor_name_to_info_;
 };
 
 }  // namespace blink
