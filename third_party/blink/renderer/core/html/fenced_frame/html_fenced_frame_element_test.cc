eec48f65451ed0f261c21f60aad9ab721e0d9d43
Koji Ishii <kojii@chromium.org>
2022-04-13 09:23:00 +0000

    Run |HTMLFencedFrameElementTest| for both versions
    This patch parameterize |HTMLFencedFrameElementTest| so that
    it runs both with MPArch and with Shadow DOM.
    
    Bug: 1270648
    Change-Id: I4ea7c62edc244877a79fb5f6588bacae3ea88c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581672
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991955}

--- third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element_test.cc
+++ third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element_test.cc
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class HTMLFencedFrameElementTest : private ScopedFencedFramesForTest,
-                                   public RenderingTest {
+class HTMLFencedFrameElementTest
+    : private ScopedFencedFramesForTest,
+      public testing::WithParamInterface<const char*>,
+      public RenderingTest {
  public:
   HTMLFencedFrameElementTest()
       : ScopedFencedFramesForTest(true),
@@ -25,7 +27,7 @@ class HTMLFencedFrameElementTest : private ScopedFencedFramesForTest,
   void SetUp() override {
     RenderingTest::SetUp();
     base::FieldTrialParams params;
-    params["implementation_type"] = "mparch";
+    params["implementation_type"] = GetParam();
     enabled_feature_list_.InitAndEnableFeatureWithParameters(
         features::kFencedFrames, params);
 
@@ -42,10 +44,15 @@ class HTMLFencedFrameElementTest : private ScopedFencedFramesForTest,
   base::test::ScopedFeatureList enabled_feature_list_;
 };
 
-TEST_F(HTMLFencedFrameElementTest, FreezeSizePageZoomFactor) {
+INSTANTIATE_TEST_CASE_P(HTMLFencedFrameElementTest,
+                        HTMLFencedFrameElementTest,
+                        testing::Values("mparch", "shadow_dom"));
+
+TEST_P(HTMLFencedFrameElementTest, FreezeSizePageZoomFactor) {
   Document& doc = GetDocument();
   auto* fenced_frame = MakeGarbageCollected<HTMLFencedFrameElement>(doc);
   doc.body()->AppendChild(fenced_frame);
+  UpdateAllLifecyclePhasesForTest();
 
   LocalFrame& frame = GetFrame();
   const float zoom_factor = frame.PageZoomFactor();
