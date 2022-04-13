63d954dd756944c7be692340724b2c2d8ccad107
Scott Violet <sky@chromium.org>
2022-04-13 19:57:20 +0000

    avoid unnecessary IPC on mac when painting
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/2923689
    with fixes.
    
    RootCompositorFrameSinkImpl::DisplayDidReceiveCALayerParams is called
    after every frame. This in turn calls to OnDisplayReceivedCALayerParams.
    If the CALayerParams haven't changed, then this IPC should not be
    necessary. This IPC also has a side effect of updating the vsync, which
    can potentially change from frame to frame. NativeWidgetMacNSWindowHost
    queries DisplayLinkMac for vsync, but DisplayLinkMac only queries
    every 10 seconds. So, this patch sends the IPC if the params haven't
    changed, but 10 seconds has elapsed.
    
    DisplayLinkMac queries the system for vsync on a background thread, so
    that the first time through the vsync isn't available. To handle this
    case I added support for a callback once vsync is enabled. This
    ensures the vsync is properly updated the first time through.
    
    The changes roughly result in something like the following:
    
    Browser IO thread:
     . Before: https://i.imgur.com/Xa5tcIb.png
     . After: https://i.imgur.com/VM6wQTJ.png
    Browser main thread:
     . Before: https://i.imgur.com/w4boRQJ.png
     . After: https://i.imgur.com/zJHdzEp.png
    
    Change-Id: Ida80358f8331a440615d7c1944cd9d7f44e7cbe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580129
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992183}

--- components/viz/service/frame_sinks/root_compositor_frame_sink_impl.h
+++ components/viz/service/frame_sinks/root_compositor_frame_sink_impl.h
@@ -202,6 +202,14 @@ class VIZ_SERVICE_EXPORT RootCompositorFrameSinkImpl
   gfx::Size last_swap_pixel_size_;
 #endif
 
+#if BUILDFLAG(IS_APPLE)
+  gfx::CALayerParams last_ca_layer_params_;
+
+  // Used to force a call to OnDisplayReceivedCALayerParams() even if the params
+  // did not change.
+  base::TimeTicks next_forced_ca_layer_params_update_time_;
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
   // Let client control whether it wants `DidCompleteSwapWithSize`.
   bool enable_swap_competion_callback_ = false;
