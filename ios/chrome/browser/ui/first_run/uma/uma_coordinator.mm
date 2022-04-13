d3c281809b3bd18a4e89f3bfbd3b58b7cf3ee3cf
Arthur Milchior <arthurmilchior@google.com>
2022-04-13 09:29:01 +0000

    [iOS] Uma gets correct size on iPad
    Bug: 1310755
    
    Result on https://drive.google.com/drive/folders/1XCJgQAHxJ0YBaKgg7Ij3yMcof_PaDIjl?usp=sharing
    
    Change-Id: I4d8a570eb951e827a0fc8cc1a03d7a150ca41cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584104
    Auto-Submit: Arthur Milchior <arthurmilchior@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991959}

--- ios/chrome/browser/ui/first_run/uma/uma_coordinator.mm
+++ ios/chrome/browser/ui/first_run/uma/uma_coordinator.mm
@@ -45,8 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Creates the navigation controller and present.
   UINavigationController* navigationController = [[UINavigationController alloc]
       initWithRootViewController:self.viewController];
-  navigationController.presentationController.delegate = self;
   navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
+  navigationController.presentationController.delegate = self;
   if (@available(iOS 15, *)) {
     // TODO(crbug.com/1290848): Needs to set the presentation for iPad.
     UISheetPresentationController* presentationController =
