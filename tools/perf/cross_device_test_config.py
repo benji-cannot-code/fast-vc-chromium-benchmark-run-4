b67ac12516932ef2ecf4cd4a70bde40a0efdbdc2
Camillo Bruni <cbruni@chromium.org>
2022-04-13 20:10:30 +0000

    [perf] Increase repetition count for speedometer
    Increasing the iteration count seems to have yielded worse performance
    on linux-perf. This CL changes back the default iteration count for
    non-android to 10, but increases the repetitions on the perf bots.
    
    This CL tries a different approach:
    - android-pixel2-perf: 10 sharded repetitions,  3 iterations
    - android-pixel4-perf: 20 sharded repetitions,  3 iterations
    - linux-perf           20 sharded repetitions, 10 iterations
    - win-10-perf          20 sharded repetitions, 10 iterations
    
    Bug: 1315169
    Change-Id: I6fce1b9f293c95feff435b05896d5cf31b8e8347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580502
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992191}

--- tools/perf/cross_device_test_config.py
+++ tools/perf/cross_device_test_config.py
@@ -34,6 +34,9 @@ TARGET_DEVICES = {
         'system_health.common_mobile': 3,
         'system_health.memory_mobile': 3,
         'startup.mobile': 10,
+        'speedometer2': {
+            'Speedometer2': 10,
+        },
     },
     'android-go-perf': {
         'system_health.common_mobile': {
@@ -42,6 +45,11 @@ TARGET_DEVICES = {
             # cputimeToFirstContentfulPaint
             'load:search:google:2018': 10
         }
+    },
+    'android-pixel4-perf': {
+        'speedometer2': {
+            'Speedometer2': 20,
+        },
     },
     'android-pixel4a_power-perf': {
         'power.mobile': {
@@ -58,7 +66,10 @@ TARGET_DEVICES = {
             'browse:social:tumblr_infinite_scroll:2018': 10,
             'long_running:tools:gmail-background': 10,
             'browse:media:youtubetv:2019': 10
-        }
+        },
+        'speedometer2': {
+            'Speedometer2': 20,
+        },
     },
     'win-10-perf': {
         'system_health.common_desktop': {
@@ -66,7 +77,10 @@ TARGET_DEVICES = {
             'browse:media:tumblr:2018': 10,
             'browse:social:tumblr_infinite_scroll:2018': 10,
             'load:search:google:2018': 10,
-        }
+        },
+        'speedometer2': {
+            'Speedometer2': 20,
+        },
     },
     'linux-perf-calibration': {
         'speedometer2': {
