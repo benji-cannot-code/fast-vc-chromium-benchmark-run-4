37f21e03b716cf8029c089f299477608360e5445
Luciano Pacheco <lucmult@chromium.org>
2022-04-13 01:03:39 +0000

    WebUI TS: Add TS type definition for cr.ui
    The files in this CL are used by Files app, this is a preparation to
    start introducing TS to Files app.
    
    The classes ContextMenuHandler, MenuButton and MenuItem  implement the
    EventListener using `handleEvent(e: Event): void` which requires to
    implement via interface EventListenerObject, thus the auto-generated
    declaration doesn't work.
    
    The classes AlertDialog and GridSelectionController fail to compile
    because TS doesn't recognize the inheritance via __proto__ and doesn't
    allow to use the `@override`.
    
    List is used as a dependency of Grid, TS fails saying: "Cannot extend a
    class List. Class constructor is marked as private."
    
    Bug: b:228404944
    Change-Id: I399f6caf85e7a72960cdd74d0f1decb4b0ed4eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577655
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991832}

---
+++ ui/webui/resources/js/cr/ui/context_menu_handler.m.d.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {Menu} from './menu.m.js';
+import {HideType} from './menu_button.m.js';
+
+export const contextMenuHandler: ContextMenuHandler;
+
+declare class ContextMenuHandler extends EventTarget implements
+    EventListenerObject {
+  get menu(): Menu;
+  showMenu(e: Event, menu: Menu): void;
+  hideMenu(hideType?: HideType|undefined): void;
+  handleEvent(e: Event): void;
+  addContextMenuProperty(elementOrClass: Element|Function): void;
+  setContextMenu(element: Element, contextMenu: Menu): void;
+}
