4ce126886571a6531006ff51d4539da6d6a3fe86
Steve Kobes <skobes@chromium.org>
2022-04-13 22:13:26 +0000

    [Scroll Unif.] Descend into restricted shadow roots during hit test.
    The main thread hit test that scroll unification uses for non-composited
    scrollers didn't descend into user-agent shadow DOM, which broke text
    <input> controls because their shadow DOM contains a scroller.
    
    This patch makes WebFrameWidgetImpl::HitTestResultForRootFramePos enter
    restricted shadow DOM trees, so we can see scrollers inside them.
    
    There are two other callers besides unification (WFWI::MouseContextMenu
    and WFWI::GetFrameSinkIdAtPoint) but they don't seem affected by this
    change, since they only care about which frame the result lands in.
    
    WebViewTest.HitTestVideo breaks, but the behavior that test was meant
    for (in crbug.com/452884) actually occurs through a different hit test
    in ContextMenuController::ShowContextMenu, which still stops at the
    shadow host.  Rewrite this test in ContextMenuControllerTest.
    
    Bug: 1311731
    Change-Id: I763f283bd0e57d53ac8b402cda2242f62080ca3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579542
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992256}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -4148,7 +4148,6 @@ HitTestResult WebFrameWidgetImpl::HitTestResultForRootFramePos(
   HitTestResult result =
       LocalRootImpl()->GetFrame()->View()->HitTestWithThrottlingAllowed(
           location, HitTestRequest::kReadOnly | HitTestRequest::kActive);
-  result.SetToShadowHostIfInRestrictedShadowRoot();
   return result;
 }
 
