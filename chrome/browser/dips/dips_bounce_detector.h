1d2cf600d95785d5f15fde83da52701a1410e523
Ryan Tarpine <rtarpine@chromium.org>
2022-04-13 22:56:19 +0000

    DIPS: Add stateful server redirect detection.
    Bug: 1304755
    Change-Id: I751f663968513eda1c6850cf43f13b071a45271e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585763
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992279}

--- chrome/browser/dips/dips_bounce_detector.h
+++ chrome/browser/dips/dips_bounce_detector.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DIPS_DIPS_BOUNCE_DETECTOR_H_
 #define CHROME_BROWSER_DIPS_DIPS_BOUNCE_DETECTOR_H_
 
+#include "base/callback.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -13,16 +14,32 @@ class DIPSBounceDetector
     : public content::WebContentsObserver,
       public content::WebContentsUserData<DIPSBounceDetector> {
  public:
+  ~DIPSBounceDetector() override;
   DIPSBounceDetector(const DIPSBounceDetector&) = delete;
   DIPSBounceDetector& operator=(const DIPSBounceDetector&) = delete;
+
+  using RedirectHandler =
+      base::RepeatingCallback<void(content::NavigationHandle*, int)>;
+
+  void SetStatefulRedirectHandlerForTesting(RedirectHandler handler) {
+    stateful_redirect_handler_ = handler;
+  }
 
  private:
   explicit DIPSBounceDetector(content::WebContents* web_contents);
   // So WebContentsUserData::CreateForWebContents() can call the constructor.
   friend class content::WebContentsUserData<DIPSBounceDetector>;
 
-  void DidRedirectNavigation(
+  void HandleStatefulRedirect(content::NavigationHandle*, int);
+
+  void OnCookiesAccessed(content::NavigationHandle* navigation_handle,
+                         const content::CookieAccessDetails& details) override;
+  void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+
+  // By default, this just calls this->HandleStatefulRedirect(), but it can be
+  // overridden for tests.
+  RedirectHandler stateful_redirect_handler_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
