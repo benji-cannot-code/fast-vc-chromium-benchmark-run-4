832a2ec6ae9004958030c0f6d20aeed70e01f7c7
Takumi Fujimoto <takumif@chromium.org>
2022-04-13 23:54:14 +0000

    Remove unused #includes from PresentationRequestNotificationProducer
    Change-Id: I2d258fb78982f4a413d122b51c779f51dbf6120b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582726
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992312}

--- chrome/browser/ui/global_media_controls/presentation_request_notification_producer.cc
+++ chrome/browser/ui/global_media_controls/presentation_request_notification_producer.cc
@@ -8,15 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/raw_ptr.h"
-#include "base/unguessable_token.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_service.h"
 #include "components/global_media_controls/public/media_item_manager.h"
-#include "components/media_router/browser/media_router.h"
-#include "components/media_router/browser/media_router_factory.h"
-#include "components/media_router/browser/presentation/presentation_service_delegate_impl.h"
 #include "components/media_router/common/providers/cast/cast_media_source.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
