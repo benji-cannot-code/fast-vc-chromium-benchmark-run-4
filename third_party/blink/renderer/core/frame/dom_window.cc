97bad790655e5e05d231ca40bd1c9a571eb9631e
Nan Lin <linnan@chromium.org>
2022-04-13 21:22:51 +0000

    Fenced Frames: Programmatic focus: Only allow programmatic focus() to be successful if the fenced frame already has system focus.
    Design doc:
    https://docs.google.com/document/d/18W-lq8uexg-vFfq0cxM6IGshwaAInjpdit-jAhhOytI/edit?usp=sharing
    
    Currently, a fenced frame can pull focus programmatically (from a
    Javascript element.focus() call) from an embedding page, which
    can be used to open a communication channel. This change ensures that
    a fenced frame can only pull programmatic focus if it has user
    activation. Likewise, if a fenced frame already has focus, this ensures
    that other elements outside of the fenced frame tree without user
    activation cannot pull focus.
    
    Change-Id: I2207e7f384921f323ff095fa347c5bcf80a29059
    Bug: 1123606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569943
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992228}

--- third_party/blink/renderer/core/frame/dom_window.cc
+++ third_party/blink/renderer/core/frame/dom_window.cc
@@ -441,6 +441,9 @@ void DOMWindow::focus(v8::Isolate* isolate) {
   if (!page)
     return;
 
+  if (!frame->ShouldAllowScriptFocus())
+    return;
+
   RecordWindowProxyAccessMetrics(
       WebFeature::kWindowProxyCrossOriginAccessFocus,
       WebFeature::kWindowProxyCrossOriginAccessFromOtherPageFocus);
