39c8802fdbb499f0589a9df8fbcff6d065ff6931
Ana Salazar <anasalazar@chromium.org>
2022-04-13 23:22:57 +0000

    [recent-apps]: Update recent apps after uninstall
    When uninstalling an app, the suggestion view from recent apps gets
    removed. After removing enough apps; so that the recent apps are left
    with left than the minimum apps, the section will disappear from the
    launcher.
    
    This can be confusing since, on the next launcher open, a fresh set of
    available recent apps will show if there are enough suggestions.
    
    This CL will fix the issue by updating set of results shown in recent
    apps, while ignoring deleted apps. This way previously dropped results
    will be added into slots left by the removed app.
    
    The recent apps section will still vanish if there are not enough
    suggestions to show, but it will not reappear in future launcher opens.
    
    Bug: 1315935
    Change-Id: Idf1d626e314c73b81703e1beec86d91e036f52b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585984
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992292}

--- ash/app_list/views/apps_container_view.cc
+++ ash/app_list/views/apps_container_view.cc
@@ -1347,8 +1347,8 @@ void AppsContainerView::UpdateRecentApps(bool needs_layout) {
     return;
 
   AppListModelProvider* const model_provider = AppListModelProvider::Get();
-  GetRecentApps()->ShowResults(model_provider->search_model(),
-                               model_provider->model());
+  GetRecentApps()->SetModels(model_provider->search_model(),
+                             model_provider->model());
   if (needs_layout)
     Layout();
 }
