f9291d96830e7bf739371daede76773dd3f502ca
Chika Okuda <okudachika@google.com>
2022-04-13 05:23:20 +0000

    Oilpanize blink::Grid and blink::ListGrid
    Changed Grid and ListGrid to on-heap object.
    
    Bug: 1306747
    Change-Id: I714065f19d6e531ab63892baaec5de6820255846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577768
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Chika Okuda <okudachika@google.com>
    Cr-Commit-Position: refs/heads/main@{#991895}

--- third_party/blink/renderer/core/layout/layout_grid.cc
+++ third_party/blink/renderer/core/layout/layout_grid.cc
@@ -58,6 +58,7 @@ LayoutGrid::LayoutGrid(Element* element)
 LayoutGrid::~LayoutGrid() = default;
 
 void LayoutGrid::Trace(Visitor* visitor) const {
+  visitor->Trace(grid_);
   visitor->Trace(track_sizing_algorithm_);
   visitor->Trace(column_of_positioned_item_);
   visitor->Trace(row_of_positioned_item_);
@@ -573,7 +574,7 @@ MinMaxSizes LayoutGrid::ComputeIntrinsicLogicalWidths() const {
     return sizes;
   }
 
-  std::unique_ptr<Grid> grid = Grid::Create(this);
+  Grid* grid = Grid::Create(this);
   GridTrackSizingAlgorithm* algorithm =
       MakeGarbageCollected<GridTrackSizingAlgorithm>(this, *grid);
   PlaceItemsOnGrid(algorithm, absl::nullopt);
