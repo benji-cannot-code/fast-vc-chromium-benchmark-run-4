a9405818b4650efe967785818ed0ce75fc9f6ded
Ryan Tarpine <rtarpine@chromium.org>
2022-04-13 18:25:32 +0000

    DIPS: Skeleton for DIPSBounceDetector.
    Followup CLs will do the proper detection of client-side and server-side
    stateful bounces, and add metric collection.
    
    Bug: 1304755
    Change-Id: I3eb117609ac0671f32bec90173280d94ea61c9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583255
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992143}

---
+++ chrome/browser/dips/dips_bounce_detector.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/dips/dips_bounce_detector.h"
+
+using content::NavigationHandle;
+
+DIPSBounceDetector::DIPSBounceDetector(content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents),
+      content::WebContentsUserData<DIPSBounceDetector>(*web_contents) {}
+
+void DIPSBounceDetector::DidRedirectNavigation(NavigationHandle* handle) {
+  // TODO: detect if bounce is stateful and if so fire UKM metric
+}
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(DIPSBounceDetector);
