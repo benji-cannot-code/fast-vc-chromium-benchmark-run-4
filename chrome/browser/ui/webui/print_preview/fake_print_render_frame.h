e5db00fce13a87a83d8e6fbe6a2d2d6f42620d9e
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-04-14 17:27:00 +0000

    Add SnapshotForContentAnalysis printing renderer API
    This CL contains changes to implement the snapshotting renderer API
    required to cloud scan printed pages. Tests and API caller code can be
    found in crrev.com/c/3368244.
    
    It also adds the ENABLE_PRINT_CONTENT_ANALYSIS buildflag to only
    compile the new renderer API on platforms that support enterprise
    scanning.
    
    Bug: 1273922
    Change-Id: I7a16a52525eb15c537cb635e840d5828d00551ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3337977
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992598}

--- chrome/browser/ui/webui/print_preview/fake_print_render_frame.h
+++ chrome/browser/ui/webui/print_preview/fake_print_render_frame.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_remote.h"
 #include "mojo/public/cpp/bindings/scoped_interface_endpoint_handle.h"
+#include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 
 namespace printing {
@@ -39,6 +40,10 @@ class FakePrintRenderFrame : public mojom::PrintRenderFrame {
   void PrintingDone(bool success) override;
   void SetPrintingEnabled(bool enabled) override;
   void PrintNodeUnderContextMenu() override;
+#if BUILDFLAG(ENABLE_PRINT_CONTENT_ANALYSIS)
+  void SnapshotForContentAnalysis(
+      SnapshotForContentAnalysisCallback callback) override;
+#endif  // BUILDFLAG(ENABLE_PRINT_CONTENT_ANALYSIS)
 
   void BindPrintRenderFrameReceiver(mojo::ScopedInterfaceEndpointHandle handle);
 
