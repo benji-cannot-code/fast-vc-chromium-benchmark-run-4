2eaa6de519951ccf1e1d0678580ebd99dc2db0c7
Michael Chang <donchan@microsoft.com>
2022-04-14 23:10:01 +0000

    Reland: Move //chrome/updater metainstaller RC file strings to grd
    This is a reland of 52f9fae52427bd792c134814cb2a4286522b0157
    
    Original change's description:
    > Move //chrome/updater metainstaller RC file strings to grd
    >
    > To take advantage of the chromium.src infrastructure for translations,
    > move the existing string resources in
    > //chrome/updater/win/ui/resources/resources_en.rc to
    > chromium_strings.grd. During the build, utilize generate_embedded_i18n
    > along with a Python file detailing which string IDs to pick to embed the
    > translated strings into the metainstaller itself.
    >
    > Bug: 1281969
    > Change-Id: I949da93e9b3833f78cb034d7f2eedefce091d772
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579733
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Commit-Queue: Dong Sung Chang <donchan@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#992665}
    
    Bug: 1281969
    Change-Id: I76f29e1684dd00abc2787db9ce0e828cd7cbdc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587453
    Commit-Queue: Dong Sung Chang <donchan@microsoft.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992760}

--- chrome/updater/win/ui/owner_draw_controls.cc
+++ chrome/updater/win/ui/owner_draw_controls.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
-#include "chrome/updater/win/ui/resources/resources.grh"
+#include "chrome/updater/win/ui/l10n_util.h"
+#include "chrome/updater/win/ui/resources/updater_installer_strings.h"
 #include "chrome/updater/win/ui/ui_util.h"
 
 namespace updater {
@@ -157,9 +158,7 @@ void CaptionButton::set_tool_tip_text(const CString& tool_tip_text) {
 }
 
 CloseButton::CloseButton() {
-  CString tool_tip_text;
-  tool_tip_text.LoadString(IDS_CLOSE_BUTTON);
-  set_tool_tip_text(tool_tip_text);
+  set_tool_tip_text(GetLocalizedString(IDS_CLOSE_BUTTON_BASE).c_str());
 }
 
 HRGN CloseButton::GetButtonRgn(int rgn_width, int rgn_height) {
@@ -186,9 +185,7 @@ HRGN CloseButton::GetButtonRgn(int rgn_width, int rgn_height) {
 }
 
 MinimizeButton::MinimizeButton() {
-  CString tool_tip_text;
-  tool_tip_text.LoadString(IDS_MINIMIZE_BUTTON);
-  set_tool_tip_text(tool_tip_text);
+  set_tool_tip_text(GetLocalizedString(IDS_MINIMIZE_BUTTON_BASE).c_str());
 }
 
 HRGN MinimizeButton::GetButtonRgn(int rgn_width, int rgn_height) {
@@ -200,9 +197,9 @@ HRGN MinimizeButton::GetButtonRgn(int rgn_width, int rgn_height) {
 }
 
 MaximizeButton::MaximizeButton() {
-  CString tool_tip_text;
-  tool_tip_text.LoadString(IDS_MAXIMIZE_BUTTON);
-  set_tool_tip_text(tool_tip_text);
+  // TODO(crbug.com/1314812) Maximize button is not utilized. Adding a
+  // placeholder for IDS_MAXIMIZE_BUTTON_BASE.
+  set_tool_tip_text(L"");
 }
 
 HRGN MaximizeButton::GetButtonRgn(int rgn_width, int rgn_height) {
