f82a72d599767a8124e7f2686fd95014ef7ce4af
Brian Lefler <bcl@chromium.org>
2022-04-14 21:21:41 +0000

    Remove browser-induced redirects from CrossOtr metrics.
    This removes 307 Internal Redirects from the CrossOtr response histogram
    at Navigation.CrossOtr.ContextMenu.ResponseCodeExperimental, ensuring
    that the distribution reflects only server-side responses and is not
    affected by the internal Chrome decision to redirect or not.
    
    R=mreichhoff@chromium.org
    
    Bug: 1315387
    Change-Id: I01038f9c9b104d88d97d4913bcb86e35c8d17937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586306
    Auto-Submit: Brian Lefler <bcl@chromium.org>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Brian Lefler <bcl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992719}

--- chrome/browser/url_param_filter/cross_otr_observer.cc
+++ chrome/browser/url_param_filter/cross_otr_observer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/reload_type.h"
@@ -18,6 +19,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 
 namespace url_param_filter {
+namespace {
+constexpr char kInternalRedirectHeaderStatusLine[] =
+    "HTTP/1.1 307 Internal Redirect";
+
+bool IsInternalRedirect(const net::HttpResponseHeaders* headers) {
+  return base::EqualsCaseInsensitiveASCII(headers->GetStatusLine(),
+                                          kInternalRedirectHeaderStatusLine);
+}
+}  // anonymous namespace
 
 constexpr char kCrossOtrResponseCodeMetricName[] =
     "Navigation.CrossOtr.ContextMenu.ResponseCodeExperimental";
@@ -116,7 +126,8 @@ void CrossOtrObserver::DidRedirectNavigation(
   // redirects.
   // Metrics will not be collected for non intervened navigation chains and
   // navigations occurring prior to params filtering.
-  if (protecting_navigations_ && headers && did_filter_params_) {
+  if (protecting_navigations_ && headers && did_filter_params_ &&
+      !IsInternalRedirect(headers)) {
     base::UmaHistogramSparse(
         kCrossOtrResponseCodeMetricName,
         net::HttpUtil::MapStatusCodeForHistogram(headers->response_code()));
