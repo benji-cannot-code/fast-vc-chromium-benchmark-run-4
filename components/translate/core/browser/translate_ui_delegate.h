cd771bc6a252eeeccd738bd16453792cd5da7279
Scott Little <sclittle@chromium.org>
2022-04-14 01:52:22 +0000

    Add a secondary menu to the Translate Message UI.
    This secondary menu supports the same options as the Translate InfoBar
    secondary menu, including having sub-menus for picking source/target
    language.
    
    Bug: 1314442
    Change-Id: I735c12ab8c26a69b8ad7db34d8ad1d888e42a270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584210
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992358}

--- components/translate/core/browser/translate_ui_delegate.h
+++ components/translate/core/browser/translate_ui_delegate.h
@@ -158,6 +158,8 @@ class TranslateUIDelegate {
   // languages data.
   void MaybeSetContentLanguages();
 
+  static std::u16string GetUnknownLanguageDisplayName();
+
  private:
   FRIEND_TEST_ALL_PREFIXES(TranslateUIDelegateTest, GetPageHost);
   FRIEND_TEST_ALL_PREFIXES(TranslateUIDelegateTest, MaybeSetContentLanguages);
