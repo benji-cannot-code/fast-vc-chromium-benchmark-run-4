e4bf85d51ebe9d9111bdf958e34935f49ba16aac
David Sanders <dsanders11@ucsbalum.com>
2022-04-14 14:25:17 +0000

    Do IWYU for //services/network/public/cpp/resource_request_body
    Bug: 242216
    Change-Id: Id9bde2c4128935ff92063e3fe58cd90a5cb5a6f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582731
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#992522}

--- services/network/public/cpp/resource_request_body.cc
+++ services/network/public/cpp/resource_request_body.cc
@@ -5,8 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/resource_request_body.h"
 
+#include <utility>
+#include <vector>
+
+#include "base/check.h"
+#include "base/memory/scoped_refptr.h"
+#include "services/network/public/cpp/data_element.h"
 #include "services/network/public/mojom/chunked_data_pipe_getter.mojom.h"
 #include "services/network/public/mojom/data_pipe_getter.mojom.h"
+#include "services/network/public/mojom/url_request.mojom-shared.h"
 
 namespace network {
 
--- services/network/public/cpp/resource_request_body.h
+++ services/network/public/cpp/resource_request_body.h
@@ -11,13 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/component_export.h"
-#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/network/public/cpp/data_element.h"
-#include "services/network/public/mojom/url_loader.mojom-shared.h"
-#include "url/gurl.h"
+#include "services/network/public/mojom/chunked_data_pipe_getter.mojom-shared.h"
+#include "services/network/public/mojom/data_pipe_getter.mojom-shared.h"
+#include "services/network/public/mojom/url_request.mojom-shared.h"
+
+namespace base {
+class Time;
+}
 
 namespace blink {
 namespace mojom {
