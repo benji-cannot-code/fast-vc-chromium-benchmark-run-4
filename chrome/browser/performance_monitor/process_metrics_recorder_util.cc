acc5464c705ba2dc05837741efef3aa9a02fc4f8
Patrick Monette <pmonette@chromium.org>
2022-04-14 02:34:00 +0000

    Reland "Add a more precise version of AverageCPU on Windows."
    This is a reland of commit 3f111bfd0619dfb5f8adb0e8e94f1f393438c2af,
    with a fix for Arm builds.
    
    Original change's description:
    > Add a more precise version of AverageCPU on Windows.
    >
    > The new version uses QueryProcessCycleTime instead of GetProcessTimes,
    > which according to online sources, does not undercount time slices when
    > the time slice is interrupted.
    >
    > Bug: 1313216
    > Change-Id: I6c19ded795e0c493d1cdca9e860c0c1152b04dd6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564382
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Reviewed-by: Olivier Li <olivierli@chromium.org>
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#990610}
    
    Bug: 1313216
    Change-Id: Ibde575c32d45f3e19cc1d32781982ec8015ccdd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577187
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992373}

--- chrome/browser/performance_monitor/process_metrics_recorder_util.cc
+++ chrome/browser/performance_monitor/process_metrics_recorder_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 
@@ -35,25 +34,28 @@ constexpr int kCPUUsageHistogramBucketCount = 50;
 void RecordProcessHistograms(const char* histogram_suffix,
                              const ProcessMonitor::Metrics& metrics) {
   base::UmaHistogramCustomCounts(
-      base::JoinString({"PerformanceMonitor.AverageCPU2.", histogram_suffix},
-                       ""),
+      base::StrCat({"PerformanceMonitor.AverageCPU2.", histogram_suffix}),
       metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
       kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
+#if BUILDFLAG(IS_WIN)
+  base::UmaHistogramCustomCounts(
+      base::StrCat({"PerformanceMonitor.AverageCPU3.", histogram_suffix}),
+      metrics.precise_cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
+      kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
+#endif
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || \
     BUILDFLAG(IS_AIX)
   base::UmaHistogramCounts10000(
-      base::JoinString({"PerformanceMonitor.IdleWakeups.", histogram_suffix},
-                       ""),
+      base::StrCat({"PerformanceMonitor.IdleWakeups.", histogram_suffix}),
       metrics.idle_wakeups);
 #endif
 #if BUILDFLAG(IS_MAC)
   base::UmaHistogramCounts1000(
-      base::JoinString(
-          {"PerformanceMonitor.PackageExitIdleWakeups.", histogram_suffix}, ""),
+      base::StrCat(
+          {"PerformanceMonitor.PackageExitIdleWakeups.", histogram_suffix}),
       metrics.package_idle_wakeups);
   base::UmaHistogramCounts100000(
-      base::JoinString({"PerformanceMonitor.EnergyImpact.", histogram_suffix},
-                       ""),
+      base::StrCat({"PerformanceMonitor.EnergyImpact.", histogram_suffix}),
       metrics.energy_impact);
 #endif
 }
