083d8086526264918165c7f801629ec06ebdb383
Mustafa Emre Acer <meacer@chromium.org>
2022-04-14 18:25:19 +0000

    HTTPS-Only Mode: Upgrade main frame HTTP URLs on iOS
    This CL implements the HTTPS upgrade functionality of HTTPS-Ony Mode for
    iOS Chrome. This feature automatically upgrades main frame HTTP URLs to
    HTTPS and checks if the upgrade is successful. If not, it shows the
    user a security interstitial asking them if they want to proceed to the
    HTTP URL.
    
    This CL implements the upgrade using a tab helper that's also a
    WebStatePolicyDecider. This decider blocks HTTP responses when necessary
    and starts a new page load to the corresponding HTTPS URL. It then
    checks if the response is a successful HTTPS load.
    
    Future CLs will add a timeout where slow HTTPS load attempts will be
    cancelled and replaced with an HTTPS-Only mode interstitial.
    
    Bug: 1302509
    Change-Id: Ieb5c97fd11274fe49d411b4be5e2a5ee291a2943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508318
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992634}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -85,6 +85,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
 #import "ios/chrome/browser/web/web_performance_metrics/web_performance_metrics_tab_helper.h"
 #import "ios/chrome/browser/webui/net_export_tab_helper.h"
+#import "ios/components/security_interstitials/https_only_mode/feature.h"
+#import "ios/components/security_interstitials/https_only_mode/https_only_mode_allowlist.h"
+#import "ios/components/security_interstitials/https_only_mode/https_only_mode_container.h"
+#import "ios/components/security_interstitials/https_only_mode/https_only_mode_upgrade_tab_helper.h"
 #import "ios/components/security_interstitials/ios_blocking_page_tab_helper.h"
 #import "ios/components/security_interstitials/lookalikes/lookalike_url_container.h"
 #import "ios/components/security_interstitials/lookalikes/lookalike_url_tab_allow_list.h"
@@ -227,4 +231,11 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   RepostFormTabHelper::CreateForWebState(web_state);
   NetExportTabHelper::CreateForWebState(web_state);
+
+  if (base::FeatureList::IsEnabled(
+          security_interstitials::features::kHttpsOnlyMode)) {
+    HttpsOnlyModeUpgradeTabHelper::CreateForWebState(web_state);
+    HttpsOnlyModeContainer::CreateForWebState(web_state);
+    HttpsOnlyModeAllowlist::CreateForWebState(web_state);
+  }
 }
---
+++ ios/chrome/browser/web/https_only_mode_app_interface.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEB_HTTPS_ONLY_MODE_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_WEB_HTTPS_ONLY_MODE_APP_INTERFACE_H_
+
+#import <Foundation/Foundation.h>
+
+// The app interface for HTTPS-Only mode tests.
+@interface HttpsOnlyModeAppInterface : NSObject
+
++ (void)setHTTPSPortForTesting:(int)HTTPSPortForTesting;
++ (void)setHTTPPortForTesting:(int)HTTPPortForTesting;
++ (void)useFakeHTTPSForTesting:(bool)useFakeHTTPSForTesting;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_WEB_HTTPS_ONLY_MODE_APP_INTERFACE_H_
