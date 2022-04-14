a061ba843a24fe248537f3d4f3368827f9e62044
Toni Barzic <tbarzic@chromium.org>
2022-04-14 17:09:06 +0000

    Add feature to reduce bubble launcher width
    BUG=1313142
    
    Change-Id: I3a1d515c1cb2fb3696b38fb6fdeaaa10ec675d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584541
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992579}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2493,6 +2493,9 @@ extern const char kCrostiniVirtualKeyboardSupportDescription[];
 extern const char kBruschettaName[];
 extern const char kBruschettaDescription[];
 
+extern const char kCompactBubbleLauncherName[];
+extern const char kCompactBubbleLauncherDescription[];
+
 extern const char kCryptAuthV2DedupDeviceLastActivityTimeName[];
 extern const char kCryptAuthV2DedupDeviceLastActivityTimeDescription[];
 
