bc68bb84f36f48cdf07c9aeb98e4cb5358c9bfe7
Hailey Wang <haileywang@google.com>
2022-04-14 23:33:17 +0000

    [Feature Processor] Add ukm database to sql feature processor
    Send the processed queries from the sql feature processor to the ukm
    database.
    
    Test the sql feature processor with a mock database.
    
    Bug: 1300003
    Change-Id: Iaaa91772883e75d44f84067c2c47bd10886c997a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574802
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#992772}

--- components/segmentation_platform/internal/execution/feature_list_query_processor.h
+++ components/segmentation_platform/internal/execution/feature_list_query_processor.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "components/optimization_guide/proto/models.pb.h"
+#include "components/segmentation_platform/internal/database/ukm_database.h"
 #include "components/segmentation_platform/internal/execution/custom_input_processor.h"
 #include "components/segmentation_platform/internal/execution/model_execution_manager_impl.h"
 #include "components/segmentation_platform/internal/execution/query_processor.h"
@@ -30,6 +31,7 @@ class FeatureListQueryProcessor {
  public:
   FeatureListQueryProcessor(
       SignalDatabase* signal_database,
+      UkmDatabase* ukm_database,
       std::unique_ptr<FeatureAggregator> feature_aggregator);
   virtual ~FeatureListQueryProcessor();
 
@@ -70,6 +72,9 @@ class FeatureListQueryProcessor {
 
   // Signal database for uma features.
   const raw_ptr<SignalDatabase> signal_database_;
+
+  // Ukm database for sql features.
+  const raw_ptr<UkmDatabase> ukm_database_;
 
   // Feature aggregator that aggregates data for uma features.
   const std::unique_ptr<FeatureAggregator> feature_aggregator_;
