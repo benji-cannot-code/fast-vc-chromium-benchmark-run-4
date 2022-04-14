3174268f2b018053dd5de6bd3d71b660e7ffe576
Gordon Seto <gordonseto@google.com>
2022-04-14 18:37:49 +0000

    [CrOS Bluetooth] Notify delegate of Bluetooth HID status changes.
    Add Delegate class to BluetoothHidDetector that is notified whenever
    a device has been attempted to be paired with and when the pairing
    has finished.
    
    Bug: 1299099
    Test: BluetoothHidDetectorImplTest
    Change-Id: I06c98894cb9358a623e1ea841c79d09eb131004b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584205
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#992639}

--- ash/components/hid_detection/bluetooth_hid_detector.cc
+++ ash/components/hid_detection/bluetooth_hid_detector.cc
@@ -9,6 +9,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace hid_detection {
 
+BluetoothHidDetector::BluetoothHidMetadata::BluetoothHidMetadata(
+    std::string name,
+    BluetoothHidType type)
+    : name(std::move(name)), type(type) {}
+
+BluetoothHidDetector::BluetoothHidMetadata::BluetoothHidMetadata(
+    BluetoothHidMetadata&& other) {
+  name = std::move(other.name);
+  type = other.type;
+}
+
+BluetoothHidDetector::BluetoothHidMetadata&
+BluetoothHidDetector::BluetoothHidMetadata::operator=(
+    BluetoothHidMetadata&& other) {
+  name = std::move(other.name);
+  type = other.type;
+  return *this;
+}
+
+BluetoothHidDetector::BluetoothHidMetadata::~BluetoothHidMetadata() = default;
+
+BluetoothHidDetector::BluetoothHidDetectionStatus::BluetoothHidDetectionStatus(
+    absl::optional<BluetoothHidDetector::BluetoothHidMetadata>
+        current_pairing_device)
+    : current_pairing_device(std::move(current_pairing_device)) {}
+
+BluetoothHidDetector::BluetoothHidDetectionStatus::BluetoothHidDetectionStatus(
+    BluetoothHidDetectionStatus&& other) {
+  current_pairing_device = std::move(other.current_pairing_device);
+}
+
+BluetoothHidDetector::BluetoothHidDetectionStatus&
+BluetoothHidDetector::BluetoothHidDetectionStatus::operator=(
+    BluetoothHidDetectionStatus&& other) {
+  current_pairing_device = std::move(other.current_pairing_device);
+  return *this;
+}
+
+BluetoothHidDetector::BluetoothHidDetectionStatus::
+    ~BluetoothHidDetectionStatus() = default;
+
 BluetoothHidDetector::BluetoothHidDetector() {
   DCHECK(ash::features::IsBluetoothRevampEnabled());
 }
--- ash/components/hid_detection/bluetooth_hid_detector_impl.h
+++ ash/components/hid_detection/bluetooth_hid_detector_impl.h
@@ -29,8 +29,10 @@ class BluetoothHidDetectorImpl
 
   // BluetoothHidDetector:
   void StartBluetoothHidDetection(
+      Delegate* delegate,
       InputDevicesStatus input_devices_status) override;
   void StopBluetoothHidDetection() override;
+  const BluetoothHidDetectionStatus GetBluetoothHidDetectionStatus() override;
 
  private:
   // States used for internal state machine.
@@ -109,6 +111,7 @@ class BluetoothHidDetectorImpl
       chromeos::bluetooth_config::mojom::BluetoothDevicePropertiesPtr>
       current_pairing_device_;
 
+  Delegate* delegate_ = nullptr;
   InputDevicesStatus input_devices_status_;
   State state_ = kNotStarted;
 
