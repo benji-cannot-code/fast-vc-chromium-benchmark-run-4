16d5c9784b688fe9993ce9cc2a706aeb7938715d
Hyowon Kim <hyowon@igalia.com>
2022-04-14 05:40:20 +0000

    [mparch] Replace ForEachFrame with ForEachRenderFrameHost in PerFrameContentTranslateDriver.
    This CL replaces ForEachFrame with
    GetMainFrame()->ForEachRenderFrameHost in order to iterate over render
    frame hosts including fenced frames.
    
    Bug: 1208438
    Change-Id: I923d9d36f409cfafa3313a37056d535d4a88dfc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577871
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#992411}

--- components/translate/content/browser/per_frame_content_translate_driver.cc
+++ components/translate/content/browser/per_frame_content_translate_driver.cc
@@ -153,7 +153,7 @@ void PerFrameContentTranslateDriver::TranslatePage(
   stats_.Clear();
   translate_seq_no_ = IncrementSeqNo(translate_seq_no_);
 
-  web_contents()->ForEachFrame(base::BindRepeating(
+  web_contents()->GetMainFrame()->ForEachRenderFrameHost(base::BindRepeating(
       &PerFrameContentTranslateDriver::TranslateFrame, base::Unretained(this),
       translate_script, source_lang, target_lang, translate_seq_no_));
 }
@@ -190,7 +190,7 @@ void PerFrameContentTranslateDriver::RevertTranslation(int page_seq_no) {
   stats_.Clear();
   translate_seq_no_ = IncrementSeqNo(translate_seq_no_);
 
-  web_contents()->ForEachFrame(base::BindRepeating(
+  web_contents()->GetMainFrame()->ForEachRenderFrameHost(base::BindRepeating(
       &PerFrameContentTranslateDriver::RevertFrame, base::Unretained(this)));
 }
 
