20d4bb6c26cf0ed31b13c8af68460a0d1f039b2b
Andrew Grieve <agrieve@chromium.org>
2022-04-14 20:30:49 +0000

    SuperSize: Show details for nodes with no source path
    Bug: None
    Change-Id: I2d155092c1dc28084158ff48ad7819d57a3c1f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586382
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992696}

--- tools/binary_size/libsupersize/viewer/static/infocard-ui.js
+++ tools/binary_size/libsupersize/viewer/static/infocard-ui.js
@@ -107,7 +107,7 @@ const displayInfocard = (() => {
       const elements = [];
 
       // srcPath is set only for leaf nodes.
-      if (node.srcPath) {
+      if (typeof node.srcPath !== 'undefined') {
         const add_field = (title, text) => {
           const div = document.createElement('div');
           div.appendChild(dom.textElement('span', title, 'symbol-name-info'));
@@ -115,7 +115,7 @@ const displayInfocard = (() => {
           elements.push(div);
         };
         if (node.container !== '') add_field('Container: ', node.container);
-        add_field('Path: ', node.srcPath);
+        add_field('Path: ', node.srcPath || '(No path)');
         add_field('Component: ', node.component || '(No component)');
         add_field('Full Name: ', node.fullName || '');
         if (node.disassembly && node.disassembly !== '') {
