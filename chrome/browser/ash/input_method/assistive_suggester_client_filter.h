97d9ac60578c2a16b10ac9f78ab37f78f4328836
jhtin <jhtin@chromium.org>
2022-04-14 04:29:33 +0000

    Remove all instances of unused SuggesterSwitch.IsXAllowed()
    Bug: b/222218270
    Change-Id: I0ceaa0b560d5c6777a44ce9acfffa0eddbcbe8a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583104
    Auto-Submit: jhtin <jhtin@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992394}

--- chrome/browser/ash/input_method/assistive_suggester_client_filter.h
+++ chrome/browser/ash/input_method/assistive_suggester_client_filter.h
@@ -23,10 +23,7 @@ class AssistiveSuggesterClientFilter : public AssistiveSuggesterSwitch {
 
   ~AssistiveSuggesterClientFilter() override;
 
-  // AssistiveSuggesterDelegate overrides
-  bool IsEmojiSuggestionAllowed() override;
-  bool IsMultiWordSuggestionAllowed() override;
-  bool IsPersonalInfoSuggestionAllowed() override;
+  // AssistiveSuggesterSwitch overrides
   void FetchEnabledSuggestionsThen(
       FetchEnabledSuggestionsCallback callback) override;
 
