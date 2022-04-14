56a963402de18d22ff952d73b7e92bfc4ff784d0
shivanigithub <shivanisha@chromium.org>
2022-04-14 21:56:41 +0000

    [Fenced frame]: Fix shadowDOM crash on ax checks
    The shadowDOM fenced frame fails the check introduced via
    https://chromium-review.googlesource.com/c/chromium/src/+/3573756
    
    This CL adds a condition to the check to ignore shadowDOM based fenced
    frames. Manually verified that the crash doesn't get reproduced with
    this change.
    
    Bug: 1316348
    Change-Id: I3fbabb87d301b351521b2017f4a77f4b9b400dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586030
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992735}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/input/web_menu_source_type.h"
 #include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom-blink.h"
 #include "third_party/blink/public/mojom/input/focus_type.mojom-blink.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/fullscreen/fullscreen.h"
 #include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
 #include "third_party/blink/renderer/core/html/custom/element_internals.h"
+#include "third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_text_area_element.h"
@@ -640,7 +642,20 @@ void AXObject::Init(AXObject* parent) {
 
   UpdateCachedAttributeValuesIfNeeded(false);
 
-  SANITIZER_CHECK(!parent_ ||
+  // The recently introduced CHECK below fails for shadowDOM fenced
+  // frame, so bypass the CHECK below if it's a shadowDOM fenced frame.
+  // TODO(crbug.com/1316348): see if AXNodeObject::AddNodeChildren() needs to
+  // change for fenced frames similar to iframes and whether this change would
+  // then still be necessary.
+  bool is_shadow_dom_fenced_frame = false;
+  if (parent_ && parent_->GetNode()) {
+    is_shadow_dom_fenced_frame =
+        parent_ && blink::features::IsFencedFramesEnabled() &&
+        blink::features::IsFencedFramesShadowDOMBased() &&
+        IsA<HTMLFencedFrameElement>(*parent_->GetNode());
+  }
+
+  SANITIZER_CHECK(!parent_ || is_shadow_dom_fenced_frame ||
                   parent_->RoleValue() != ax::mojom::blink::Role::kIframe ||
                   RoleValue() == ax::mojom::blink::Role::kDocument)
       << "An iframe can only have a document child."
