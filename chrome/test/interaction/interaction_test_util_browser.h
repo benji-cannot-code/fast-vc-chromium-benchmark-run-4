f1be33ccd6cfe7de8863238d093dee68f0ffda12
Dana Fried <dfried@chromium.org>
2022-04-14 20:09:38 +0000

    Move more test utils into chrome/test/interaction.
    Now all of the test utils that were in c/b/ui[/views]/user_education are
    in chrome/test/interaction. In addition, these utilities are in the
    test_support_ui package rather than the test_support package as they are
    UI-specific.
    
    Change-Id: Iefa36367a7d253d4dcf35db8a3767272e0368d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584324
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992686}

--- chrome/test/interaction/interaction_test_util_browser.h
+++ chrome/test/interaction/interaction_test_util_browser.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_USER_EDUCATION_USER_EDUCATION_TEST_UTIL_H_
-#define CHROME_BROWSER_UI_VIEWS_USER_EDUCATION_USER_EDUCATION_TEST_UTIL_H_
+#ifndef CHROME_TEST_INTERACTION_INTERACTION_TEST_UTIL_BROWSER_H_
+#define CHROME_TEST_INTERACTION_INTERACTION_TEST_UTIL_BROWSER_H_
 
 #include <memory>
 
@@ -46,4 +46,4 @@ class WidgetFocusWaiter : public views::WidgetFocusChangeListener {
   views::Widget* const widget_;
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_USER_EDUCATION_USER_EDUCATION_TEST_UTIL_H_
+#endif  // CHROME_TEST_INTERACTION_INTERACTION_TEST_UTIL_BROWSER_H_
