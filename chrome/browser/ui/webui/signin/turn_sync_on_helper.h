a5853a64cade855ead29d0cf960e2f7d23e74fa2
David Roger <droger@chromium.org>
2022-04-14 13:56:07 +0000

    [Lacros] Support profile creation from the SyncTurnOnHelper
    Bug: 1260291
    Change-Id: I2d7b9e329c57e7a9dd64fd17392b9080e7c9dd93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581868
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992513}

--- chrome/browser/ui/webui/signin/turn_sync_on_helper.h
+++ chrome/browser/ui/webui/signin/turn_sync_on_helper.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_startup_tracker.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
@@ -32,6 +33,10 @@ class SigninUIError;
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 class DiceSignedInProfileCreator;
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+class ProfilePickerLacrosSignInProvider;
 #endif
 
 namespace signin {
@@ -263,6 +268,9 @@ class TurnSyncOnHelper : public SyncStartupTracker::Observer,
   std::unique_ptr<SyncStartupTracker> sync_startup_tracker_;
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   std::unique_ptr<DiceSignedInProfileCreator> dice_signed_in_profile_creator_;
+#endif
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  std::unique_ptr<ProfilePickerLacrosSignInProvider> lacros_sign_in_provider_;
 #endif
   base::CallbackListSubscription shutdown_subscription_;
   bool enterprise_account_confirmed_ = false;
