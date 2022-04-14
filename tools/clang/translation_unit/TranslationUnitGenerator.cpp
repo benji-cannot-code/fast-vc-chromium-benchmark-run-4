30dc40370f14974304d84259372f1543e9bd1f3a
Alan Zhao <ayzhao@google.com>
2022-04-14 22:18:26 +0000

    Update clang tools for PPCallbacks::InclusionDirective(...) API change
    The file parameter in PPCallbacks::InclusionDirective(...) changed type
    from changed type from const clang::FileEntry* to
    Optional<FileEntryRef>:
    https://reviews.llvm.org/D123574,
    
    Bug: 1316423
    Change-Id: I58be5521f780ec89f77de4200a4de0ff9d617706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580849
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#992744}

--- tools/clang/translation_unit/TranslationUnitGenerator.cpp
+++ tools/clang/translation_unit/TranslationUnitGenerator.cpp
@@ -62,7 +62,12 @@ class IncludeFinderPPCallbacks : public clang::PPCallbacks {
                           llvm::StringRef file_name,
                           bool is_angled,
                           clang::CharSourceRange range,
+#if defined(LLVM_FORCE_HEAD_REVISION)
+                          llvm::Optional<clang::FileEntryRef> file,
+#else
+                          // TODO(crbug.com/1316423): Remove after clang roll.
                           const clang::FileEntry* file,
+#endif
                           llvm::StringRef search_path,
                           llvm::StringRef relative_path,
                           const clang::Module* imported,
@@ -159,12 +164,17 @@ void IncludeFinderPPCallbacks::InclusionDirective(
     llvm::StringRef file_name,
     bool is_angled,
     clang::CharSourceRange range,
+#if defined(LLVM_FORCE_HEAD_REVISION)
+    llvm::Optional<clang::FileEntryRef> file,
+#else
+    // TODO(crbug.com/1316423): Remove after clang roll.
     const clang::FileEntry* file,
+#endif
     llvm::StringRef search_path,
     llvm::StringRef relative_path,
     const clang::Module* imported,
     clang::SrcMgr::CharacteristicKind /*file_type*/
-    ) {
+) {
   if (!file)
     return;
 
