ce7a41632d29fc9c77eace1d5b38fd1d23ee9345
Nan Lin <linnan@chromium.org>
2022-04-14 19:11:11 +0000

    [attribution_reporting] Add metric for navigation data host events
    This might help understand how often potential navigation sources are
    dropped and why.
    
    Change-Id: Icebf172ea5470d6a8dfbdfa765a5609207670397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585467
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992658}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
@@ -930,6 +930,10 @@ TEST_F(AttributionDataHostManagerImplTest,
 
   histograms.ExpectTimeBucketCount("Conversions.SourceEligibleDataHostLifeTime",
                                    base::Milliseconds(1), 1);
+
+  // kRegistered = 0, kProcessed = 3.
+  histograms.ExpectBucketCount("Conversions.NavigationDataHostStatus", 0, 1);
+  histograms.ExpectBucketCount("Conversions.NavigationDataHostStatus", 3, 1);
 }
 
 // Ensures correct behavior in
@@ -1168,6 +1172,10 @@ TEST_F(AttributionDataHostManagerImplTest,
   histograms.ExpectTotalCount("Conversions.TriggerQueueDelay", 0);
   histograms.ExpectTimeBucketCount("Conversions.SourceEligibleDataHostLifeTime",
                                    base::Milliseconds(1), 2);
+
+  // kRegistered = 0, kNavigationFailed = 2.
+  histograms.ExpectBucketCount("Conversions.NavigationDataHostStatus", 0, 1);
+  histograms.ExpectBucketCount("Conversions.NavigationDataHostStatus", 2, 1);
 }
 
 TEST_F(AttributionDataHostManagerImplTest,
@@ -1464,6 +1472,18 @@ TEST_F(AttributionDataHostManagerImplTest, InsecureNavigationOrigin_Dropped) {
                                                     },
                                             });
   }
+}
+
+TEST_F(AttributionDataHostManagerImplTest, NavigationDataHostNotRegistered) {
+  base::HistogramTester histograms;
+
+  const blink::AttributionSrcToken attribution_src_token;
+  data_host_manager_.NotifyNavigationForDataHost(
+      attribution_src_token, url::Origin::Create(GURL("https://page.example")),
+      url::Origin::Create(GURL("https://trigger.example")));
+
+  // kNotFound = 1.
+  histograms.ExpectUniqueSample("Conversions.NavigationDataHostStatus", 1, 1);
 }
 
 }  // namespace content
