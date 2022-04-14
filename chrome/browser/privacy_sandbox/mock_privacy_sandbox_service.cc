c7f5f4689ad751e74a229af53e4f6762baec2bb0
Olesia Marukhno <olesiamarukhno@google.com>
2022-04-14 11:00:12 +0000

    [reland][privacy sandbox settings] Handle notice dismiss in web UI
    Dismissing the notice was handled in the dialog view. The action
    didn't go through the handler pipeline and didn't trigger HaTS. It also
    recorded kNoticeClosedNoAction as well as kNoticeDismiss.
    
    Handle pressing ESC in the web UI instead to go through the same path as
    other user actions.
    
    CL was reverted because new interactive tests are flaky on ChromeOS and
    Wayland:
    https://chromium-review.googlesource.com/c/chromium/src/+/3584774
    Disable them on ChromeOS and Wayland for now.
    
    Bug: 1312042
    Change-Id: Ib9f59ca83ea2efcd3d0ec2891378690e396a1935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581996
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#992472}

---
+++ chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h"
+
+#include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/browser_context.h"
+
+MockPrivacySandboxService::MockPrivacySandboxService() = default;
+
+MockPrivacySandboxService::~MockPrivacySandboxService() = default;
+
+std::unique_ptr<KeyedService> BuildMockPrivacySandboxService(
+    content::BrowserContext* context) {
+  return std::make_unique<::testing::NiceMock<MockPrivacySandboxService>>();
+}
