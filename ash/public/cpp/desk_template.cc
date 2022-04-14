4cbaa0efd4156f8ebf44861c71d1a323463b5c5d
Richard Chui <richui@chromium.org>
2022-04-14 17:54:19 +0000

    Desk Templates: Support lacros if primary browser
    Update logic to support lacros windows only if it is the primary
    browser.
    
    Fixed: 1316074
    Test: manual
    Change-Id: I95a8ed5d8fa56c22f23919cabaa8b25346d27070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585851
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992617}

--- ash/public/cpp/desk_template.cc
+++ ash/public/cpp/desk_template.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/app_types.h"
 #include "ash/constants/ash_features.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/crosapi/cpp/lacros_startup_state.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 
@@ -26,18 +27,20 @@ DeskTemplate::~DeskTemplate() = default;
 
 // static
 bool DeskTemplate::IsAppTypeSupported(aura::Window* window) {
-  // For now we'll ignore crostini and lacros windows in desk template. We'll
-  // also ignore ARC apps unless the flag is turned on.
+  // For now we'll ignore crostini windows in desk template. We'll also ignore
+  // lacros windows if it is not the primary browser. We'll also ignore ARC apps
+  // unless the flag is turned on.
   const AppType app_type =
       static_cast<AppType>(window->GetProperty(aura::client::kAppType));
   switch (app_type) {
     case AppType::NON_APP:
     case AppType::CROSTINI_APP:
       return false;
+    case AppType::LACROS:
+      return crosapi::lacros_startup_state::IsLacrosPrimaryEnabled();
     case AppType::ARC_APP:
     case AppType::BROWSER:
     case AppType::CHROME_APP:
-    case AppType::LACROS:
     case AppType::SYSTEM_APP:
       break;
   }
