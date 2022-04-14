93dbcf1b244eb3bb0e5f9cbe9b936551e39f476b
Kent Tamura <tkent@chromium.org>
2022-04-14 09:41:24 +0000

    Move out "gfx"-related specializations of CrossThreadCopier
    ... from cross_thread_copier.h to cross_thread_copier_gfx.h.
    This CL should not have any behavior changes.
    
    Bug: 242216
    Change-Id: I7c285a3756b2cd7b39d0e9718d325ca02ce0d8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582442
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992461}

--- third_party/blink/renderer/core/loader/threaded_icon_loader.cc
+++ third_party/blink/renderer/core/loader/threaded_icon_loader.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/worker_pool.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_copier_base.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_copier_gfx.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 
 namespace blink {
