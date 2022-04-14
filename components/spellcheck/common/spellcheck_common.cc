03346c4931df5d026e5ace0516ce57e153b1de3f
Fernando Fiori <ffiori@microsoft.com>
2022-04-14 21:56:18 +0000

    Add {en, en-US} to kSupportedSpellCheckerLanguages
    Fix for spellcheck not available for English-English in win10 without
    English language package.
    
    The mapping is based on kAliasMap from ui/base/l10n/l10n_util.cc.
    
    Bug: 1314863
    Change-Id: I3e66c2be840eaa3c0102c7b0599e8c1b7f4220a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580028
    Reviewed-by: Josh Simmons <jds@google.com>
    Auto-Submit: Fernando Fiori <ffiori@microsoft.com>
    Commit-Queue: Fernando Fiori <ffiori@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#992734}

--- components/spellcheck/common/spellcheck_common.cc
+++ components/spellcheck/common/spellcheck_common.cc
@@ -41,6 +41,7 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     {"de", "de-DE"},
     {"de-DE", "de-DE"},
     {"el", "el-GR"},
+    {"en", "en-US"},
     {"en-AU", "en-AU"},
     {"en-CA", "en-CA"},
     {"en-GB", "en-GB"},
