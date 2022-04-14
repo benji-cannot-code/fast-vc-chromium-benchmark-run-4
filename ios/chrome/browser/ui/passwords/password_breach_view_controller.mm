fcb571449aa8d4fd85d231e1bf4beab2df4ba3ef
Viktor Semeniuk <vsemeniuk@google.com>
2022-04-14 11:27:59 +0000

    [Passwords] Focussing Leak password dialog when it's presented.
    Fixed: 1314726
    Change-Id: Ib0b32790c2bd7eae6e1f4346c8f0b7464eb182dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582588
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#992481}

--- ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
+++ ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super viewDidLoad];
   self.view.accessibilityIdentifier =
       kPasswordBreachViewAccessibilityIdentifier;
+  UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification,
+                                  self);
 }
 
 #pragma mark - PasswordBreachConsumer
