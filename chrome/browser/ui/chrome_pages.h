c1bdc671cd45567913db9242e2739d4b62071770
David Sanders <dsanders11@ucsbalum.com>
2022-04-15 13:45:00 +0000

    Use forward decl mojo header in //chrome/browser/ui/chrome_pages.h
    Also adds some includes to other files which were
    getting them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I0e98a7bbf4f966f35a6fe30c7ac896501c45f049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588041
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992920}

--- chrome/browser/ui/chrome_pages.h
+++ chrome/browser/ui/chrome_pages.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/content_settings/core/common/content_settings_types.h"
-#include "components/services/app_service/public/mojom/types.mojom.h"
+#include "components/services/app_service/public/mojom/types.mojom-forward.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "url/gurl.h"
 
