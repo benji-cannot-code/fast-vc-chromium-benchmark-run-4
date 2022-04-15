f9051889b6ffe735d12d8330e990df09ed40f67a
Nan Lin <linnan@chromium.org>
2022-04-15 13:47:58 +0000

    [attribution_reporting] Remove AddAggregatableAttributionForTesting
    Updated the unit tests to handle source and trigger with aggregatable
    data, and therefore `AddAggregatableAttributionForTesting()` is no
    longer needed in both `AttributionManagerImpl` and `AttributionStorage`.
    
    Change-Id: Iffe978ce8e1191887cfff0c92e60ac22c43a890d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588346
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992922}

--- content/browser/attribution_reporting/attribution_manager_impl.h
+++ content/browser/attribution_reporting/attribution_manager_impl.h
@@ -107,8 +107,6 @@ class CONTENT_EXPORT AttributionManagerImpl : public AttributionManager {
 
   void MaybeEnqueueEventForTesting(SourceOrTrigger event);
 
-  void AddAggregatableAttributionForTesting(AttributionReport report);
-
  private:
   friend class AttributionManagerImplTest;
 
