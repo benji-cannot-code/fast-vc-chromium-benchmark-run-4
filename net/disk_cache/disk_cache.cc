de373c185590d8c6e6adf59dfac93b24f27db1a4
Yutaka Hirano <yhirano@chromium.org>
2022-04-15 22:32:30 +0000

    Reland "Reland "[disk cache] Broker file operations to read from / write to entries""
    This is a reland of commit d9c17bff5cd59b39389c78ed0aca68b0dd5c3b1d
    
    Original change's description:
    > Reland "[disk cache] Broker file operations to read from / write to entries"
    >
    > This is a reland of commit b5b23e4aabb8edc70324872b14c7c77818c9dc31.
    >
    >  1) transferring a file descriptor and using ftruncate() with it fails
    >     on some devices. Let's skip the tests on such an environment.
    >  2) Fix a crash bug found by clusterfuzz (https://crbug.com/1316034).
    >
    > Original change's description:
    > > [disk cache] Broker file operations to read from / write to entries
    > >
    > > Broker file operations so that we can
    > >  - open an entry,
    > >  - write data into an entry, and
    > >  - read data from an entry,
    > >
    > > in a sandboxed process.
    > >
    > > Bug: 1289542
    > > Change-Id: Ieaafc921409f974b41560425390b8a7f82503c2a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574350
    > > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#991906}
    >
    > Bug: 1289542, 1316034, 1315933
    > Change-Id: Ia5c2220b7f49fd9336cceafcef0491c878658d35
    > Cq-Include-Trybots: luci.chromium.try:android-bfcache-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583932
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#992847}
    
    Bug: 1289542, 1316034, 1315933
    Change-Id: I652494f0bb51943694f74522d2a630c8d3403081
    Cq-Include-Trybots: luci.chromium.try:android-bfcache-rel
    Cq-Include-Trybots: luci.chromium.try:android-asan
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586953
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993086}

--- net/disk_cache/disk_cache.cc
+++ net/disk_cache/disk_cache.cc
@@ -239,6 +239,15 @@ class TrivialFileEnumerator final : public FileEnumerator {
   disk_cache::SimpleFileEnumerator enumerator_;
 };
 
+class UnboundTrivialFileOperations
+    : public disk_cache::UnboundBackendFileOperations {
+ public:
+  std::unique_ptr<disk_cache::BackendFileOperations> Bind(
+      scoped_refptr<base::SequencedTaskRunner> task_runner) override {
+    return std::make_unique<disk_cache::TrivialFileOperations>();
+  }
+};
+
 }  // namespace
 
 namespace disk_cache {
@@ -441,6 +450,9 @@ TrivialFileOperations::~TrivialFileOperations() {
 
 bool TrivialFileOperations::CreateDirectory(const base::FilePath& path) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   // This is needed for some unittests.
   if (path.empty()) {
@@ -455,6 +467,9 @@ bool TrivialFileOperations::CreateDirectory(const base::FilePath& path) {
 
 bool TrivialFileOperations::PathExists(const base::FilePath& path) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   // This is needed for some unittests.
   if (path.empty()) {
@@ -467,10 +482,24 @@ bool TrivialFileOperations::PathExists(const base::FilePath& path) {
   return result;
 }
 
+bool TrivialFileOperations::DirectoryExists(const base::FilePath& path) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
+
+  bool result = base::DirectoryExists(path);
+  return result;
+}
+
 base::File TrivialFileOperations::OpenFile(const base::FilePath& path,
                                            uint32_t flags) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   base::File file(path, flags);
   return file;
@@ -479,6 +508,9 @@ base::File TrivialFileOperations::OpenFile(const base::FilePath& path,
 bool TrivialFileOperations::DeleteFile(const base::FilePath& path) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   return base::DeleteFile(path);
 }
@@ -489,6 +521,9 @@ bool TrivialFileOperations::ReplaceFile(const base::FilePath& from_path,
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(from_path.IsAbsolute());
   DCHECK(to_path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   return base::ReplaceFile(from_path, to_path, error);
 }
@@ -497,6 +532,9 @@ absl::optional<base::File::Info> TrivialFileOperations::GetFileInfo(
     const base::FilePath& path) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
 
   base::File::Info file_info;
   if (!base::GetFileInfo(path, &file_info)) {
@@ -507,7 +545,33 @@ absl::optional<base::File::Info> TrivialFileOperations::GetFileInfo(
 
 std::unique_ptr<FileEnumerator> TrivialFileOperations::EnumerateFiles(
     const base::FilePath& path) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(path.IsAbsolute());
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+#endif
   return std::make_unique<TrivialFileEnumerator>(path);
+}
+
+std::unique_ptr<UnboundBackendFileOperations> TrivialFileOperations::Unbind() {
+#if DCHECK_IS_ON()
+  DCHECK(bound_);
+  bound_ = false;
+#endif
+  return std::make_unique<UnboundTrivialFileOperations>();
+}
+
+TrivialFileOperationsFactory::TrivialFileOperationsFactory() = default;
+TrivialFileOperationsFactory::~TrivialFileOperationsFactory() = default;
+
+std::unique_ptr<BackendFileOperations> TrivialFileOperationsFactory::Create(
+    scoped_refptr<base::SequencedTaskRunner> task_runner) {
+  return std::make_unique<TrivialFileOperations>();
+}
+
+std::unique_ptr<UnboundBackendFileOperations>
+TrivialFileOperationsFactory::CreateUnbound() {
+  return std::make_unique<UnboundTrivialFileOperations>();
 }
 
 }  // namespace disk_cache
--- net/disk_cache/simple/simple_file_tracker.h
+++ net/disk_cache/simple/simple_file_tracker.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace disk_cache {
 
+class BackendFileOperations;
 class SimpleSynchronousEntry;
 
 // This keeps track of all the files SimpleCache has open, across all the
@@ -113,7 +114,9 @@ class NET_EXPORT_PRIVATE SimpleFileTracker {
   // pressure, and that open may have failed. This should not be called twice
   // with the exact same arguments until the handle returned from the previous
   // such call is destroyed.
-  FileHandle Acquire(const SimpleSynchronousEntry* owner, SubFile subfile);
+  FileHandle Acquire(BackendFileOperations* file_operations,
+                     const SimpleSynchronousEntry* owner,
+                     SubFile subfile);
 
   // Tells SimpleFileTracker that SimpleSynchronousEntry will not be interested
   // in the file further, so it can be closed and forgotten about.  It's OK to
@@ -205,7 +208,9 @@ class NET_EXPORT_PRIVATE SimpleFileTracker {
       std::vector<std::unique_ptr<base::File>>* files_to_close);
 
   // Tries to reopen given file, updating |*owners_files| if successful.
-  void ReopenFile(TrackedFiles* owners_files, SubFile subfile);
+  void ReopenFile(BackendFileOperations* file_operations,
+                  TrackedFiles* owners_files,
+                  SubFile subfile);
 
   // Makes sure the entry is marked as most recently used, adding it to LRU
   // if needed.
