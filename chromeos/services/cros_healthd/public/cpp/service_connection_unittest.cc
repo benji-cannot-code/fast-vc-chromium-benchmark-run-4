12e180dedc7d743a297d570308f26948130d342d
Kartik Hegde <khegde@chromium.org>
2022-04-15 21:09:59 +0000

    Add tracked networks
    In order to expose traffic counters as a PSD in feedback reports,
    network guids of networks that are either currently active or have
    recently (within the last hour) been active need to be obtained.
    
    BUG=b:221436737
    TEST=none(yet)
    
    Change-Id: I8b34af6c99299295cf21903f8df6b5779f8ff838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582898
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Quick-Run: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993063}

--- chromeos/services/cros_healthd/public/cpp/service_connection_unittest.cc
+++ chromeos/services/cros_healthd/public/cpp/service_connection_unittest.cc
@@ -253,6 +253,10 @@ class MockNetworkHealthService : public NetworkHealthService {
               GetHealthSnapshot,
               (NetworkHealthService::GetHealthSnapshotCallback),
               (override));
+  MOCK_METHOD(void,
+              GetRecentlyActiveNetworks,
+              (NetworkHealthService::GetRecentlyActiveNetworksCallback),
+              (override));
 
   mojo::PendingRemote<NetworkHealthService> pending_remote() {
     return receiver_.BindNewPipeAndPassRemote();
