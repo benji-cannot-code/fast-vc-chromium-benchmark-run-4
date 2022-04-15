b0be40f43e667a05752629780cdc081723285828
ssid <ssid@chromium.org>
2022-04-15 06:31:08 +0000

    Fix redefinition of GenerateUrlId()
    This was added due to rebase issues with previous cl. The history
    delegate should use the same hashing scheme as url table.
    
    BUG=1310442
    
    Change-Id: Ic3ccf0fd23a2e31d3dcc419eb77d303a912b2268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577159
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992860}

--- components/segmentation_platform/internal/signals/history_delegate_impl.cc
+++ components/segmentation_platform/internal/signals/history_delegate_impl.cc
@@ -8,16 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/segmentation_platform/internal/database/ukm_database.h"
+#include "components/segmentation_platform/internal/database/ukm_url_table.h"
 
 namespace segmentation_platform {
-namespace {
-
-// TODO(ssid): This should be moved to URL table instead.
-UrlId GenerateUrlId(const GURL& url) {
-  return UrlId::FromUnsafeValue(base::PersistentHash(url.spec()));
-}
-
-}  // namespace
 
 HistoryDelegateImpl::HistoryDelegateImpl(
     history::HistoryService* history_service,
@@ -32,19 +25,19 @@ HistoryDelegateImpl::~HistoryDelegateImpl() {
 
 void HistoryDelegateImpl::OnUrlAdded(const GURL& url) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  cached_history_urls_.insert(GenerateUrlId(url));
+  cached_history_urls_.insert(UkmUrlTable::GenerateUrlId(url));
 }
 
 void HistoryDelegateImpl::OnUrlRemoved(const std::vector<GURL>& urls) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   for (const GURL& url : urls) {
-    cached_history_urls_.erase(GenerateUrlId(url));
+    cached_history_urls_.erase(UkmUrlTable::GenerateUrlId(url));
   }
 }
 
 bool HistoryDelegateImpl::FastCheckUrl(const GURL& url) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return cached_history_urls_.count(GenerateUrlId(url));
+  return cached_history_urls_.count(UkmUrlTable::GenerateUrlId(url));
 }
 
 void HistoryDelegateImpl::FindUrlInHistory(
@@ -54,8 +47,8 @@ void HistoryDelegateImpl::FindUrlInHistory(
   history_service_->QueryURL(
       url, /* want_visits=*/false,
       base::BindOnce(&HistoryDelegateImpl::OnHistoryQueryResult,
-                     weak_factory_.GetWeakPtr(), GenerateUrlId(url),
-                     std::move(callback)),
+                     weak_factory_.GetWeakPtr(),
+                     UkmUrlTable::GenerateUrlId(url), std::move(callback)),
       &task_tracker_);
 }
 
