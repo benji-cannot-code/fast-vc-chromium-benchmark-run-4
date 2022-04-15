68e2f729fcf1b29225e3f3082e2bcf34dbdc455c
Yuki Shiino <yukishiino@chromium.org>
2022-04-15 03:21:18 +0000

    Revert "[CrOS] Introduce Kiosk app default message for Kiosk SKU"
    This reverts commit f831febf00fd672515bfe0d673429b81555be54f.
    
    Reason for revert: Suspicious about causing test failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/28418/overview
    KioskUpdateTest.UpdateAppWithSharedModuleRemoveAllSecondaryApps
    
    Original change's description:
    > [CrOS] Introduce Kiosk app default message for Kiosk SKU
    >
    > For device with kiosk SKU, a kiosk app default message would be shown
    > if kiosk apps are not set up. (go/kiosk-sku-prd)
    >
    > Implementation screenshots
    > (https://screenshot.googleplex.com/7d298sSr6JkqhaJ)
    >
    > Bug: 1307303
    > Change-Id: Ibd32bbfd1bd07ffcf06bd6240726d039025237cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575382
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Commit-Queue: Sherri Lin <sherrilin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#992770}
    
    Bug: 1307303
    Change-Id: I5f5439e72e8aab679dc4433b59495a454e04317e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586949
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#992830}

--- ash/login/ui/lock_contents_view.h
+++ ash/login/ui/lock_contents_view.h
@@ -47,7 +47,6 @@ class BoxLayout;
 
 namespace ash {
 
-class KioskAppDefaultMessage;
 class LockScreenMediaControlsView;
 class LoginAuthUserView;
 class LoginBigUserView;
@@ -91,7 +90,6 @@ class ASH_EXPORT LockContentsView
     explicit TestApi(LockContentsView* view);
     ~TestApi();
 
-    KioskAppDefaultMessage* kiosk_default_message() const;
     LoginBigUserView* primary_big_view() const;
     LoginBigUserView* opt_secondary_big_view() const;
     AccountId focused_user() const;
@@ -155,7 +153,6 @@ class ASH_EXPORT LockContentsView
   void ShowAdbEnabled();
   void ToggleSystemInfo();
   void ShowParentAccessDialog();
-  void SetKioskAppsButtonPresence(bool is_kiosk_apps_button_present);
 
   // views::View:
   void Layout() override;
@@ -256,9 +253,6 @@ class ASH_EXPORT LockContentsView
 
   // Called for debugging to remove forced online sign-in form |user|.
   void UndoForceOnlineSignInForUserForDebug(const AccountId& user);
-
-  // Test API. Set device to have kiosk license.
-  void SetKioskLicenseModeForTesting(bool is_kiosk_license_mode);
 
   // Called by LockScreenMediaControlsView.
   void CreateMediaControlsLayout();
@@ -494,10 +488,6 @@ class ASH_EXPORT LockContentsView
   // Contains authentication user and the additional user views.
   NonAccessibleView* main_view_ = nullptr;
 
-  // If the kiosk app button is not visible, the kiosk app default message would
-  // be shown.
-  raw_ptr<KioskAppDefaultMessage> kiosk_default_message_ = nullptr;
-
   // Actions that should be executed before a new layout happens caused by a
   // display change (eg. screen rotation). A full layout pass is performed after
   // all actions are executed.
@@ -542,10 +532,6 @@ class ASH_EXPORT LockContentsView
   // Whether the lock screen note is disabled. Used to override the actual lock
   // screen note state.
   bool disable_lock_screen_note_ = false;
-
-  // TODO(1307303): Change it to the real function that checks if device is with
-  // Kiosk License
-  bool kiosk_license_mode_ = false;
 
   // Whether the system information should be displayed or not be displayed
   // forcedly according to policy settings.
