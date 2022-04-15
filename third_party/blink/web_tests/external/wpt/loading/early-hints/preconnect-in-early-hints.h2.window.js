acf9d0708dac2c5d2b2ef06e91499bbdc1b8736e
Kenichi Ishibashi <bashi@chromium.org>
2022-04-15 08:51:10 +0000

    Add a WPT for Early Hints preconnect
    The test sends a preconnect link header in an Early Hints response,
    fetches a script from the preconnected origin, then examines resource
    timing's connectStart and connectEnd to see whether the fetch request
    took no time to establish a connection.
    
    Bug: 1245107
    Change-Id: I37dce23109b62fe099ed164508041fd88ea438dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586952
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992885}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/preconnect-in-early-hints.h2.window.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+test(() => {
+    const resource_origin = CROSS_ORIGIN;
+    const resource_url = CROSS_ORIGIN + RESOURCES_PATH + "/empty.js?" + token();
+    const params = new URLSearchParams();
+    params.set("resource-origin", resource_origin);
+    params.set("resource-url", resource_url);
+    const test_url = "resources/preconnect-in-early-hints.h2.py?" + params.toString();
+    window.location.replace(new URL(test_url, window.location));
+});
