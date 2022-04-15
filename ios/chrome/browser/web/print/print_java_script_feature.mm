51ecf2c2855557158378faba9770a42f96dbb818
Mike Dougherty <michaeldo@chromium.org>
2022-04-15 08:47:28 +0000

    Convert ios/chrome/browser/web/print:print_js
    Converts print JavaScript target to be compiled with Rollup and Terser
    instead of the closure compiler.
    
    Bug: 1313654
    Change-Id: Iec5395dfa10b9071b5ecb9d3d8a2f7b00838b939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588032
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992881}

--- ios/chrome/browser/web/print/print_java_script_feature.mm
+++ ios/chrome/browser/web/print/print_java_script_feature.mm
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-const char kScriptName[] = "print_js";
+const char kScriptName[] = "print";
 const char kScriptHandlerName[] = "PrintMessageHandler";
 }  // namespace
 
