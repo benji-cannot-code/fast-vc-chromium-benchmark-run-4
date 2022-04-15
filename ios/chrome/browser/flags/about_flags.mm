e6f2a6c79ece8a9a30eb6b7404d54425e556a965
Louis Romero <lpromero@google.com>
2022-04-15 18:04:50 +0000

    Add a feature flag to experiment with UIView window KVO
    crrev/c/3579318 introduces support for KVO on -[UIView window] but does
    not yet turn it on.
    We are testing this behind an experiment flag to see if this has any
    impact on the app's stability and performance.
    
    Bug: 1315559
    Change-Id: I9e80a9343364cfbdcaca99acb67305eb360c4f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584099
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992984}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/features.h"
 #import "ios/chrome/browser/ui/toolbar_container/toolbar_container_features.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
+#include "ios/chrome/browser/ui/util/features.h"
 #include "ios/chrome/browser/web/features.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/public/provider/chrome/browser/app_utils/app_utils_api.h"
@@ -852,6 +853,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"download-calendar", flag_descriptions::kDownloadCalendarName,
      flag_descriptions::kDownloadCalendarDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kDownloadCalendar)},
+    {"uiview-window-observing", flag_descriptions::kUIViewWindowObservingName,
+     flag_descriptions::kUIViewWindowObservingDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kUIViewWindowObserving)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
