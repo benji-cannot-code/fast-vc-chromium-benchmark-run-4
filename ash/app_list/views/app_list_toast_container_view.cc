bf1a2d59617af6a61d7d4e349533bf020fcf3ab0
Andrew Xu <andrewxu@chromium.org>
2022-04-15 17:25:38 +0000

    [Launcher Sort] Record sort discovery time after education nudge show
    This CL records the time duration between showing the app list sort
    education nudge and the first sort usage on a primary user session.
    This metric is recorded when a primary user triggers app list sort for
    the first time.
    
    Bug: 1302676
    Change-Id: I6b0c7d25d4aedd0760072af7401e6c91873a17a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577454
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992971}

--- ash/app_list/views/app_list_toast_container_view.cc
+++ ash/app_list/views/app_list_toast_container_view.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/apps_grid_context_menu.h"
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/app_list_model_delegate.h"
+#include "ash/public/cpp/feature_discovery_duration_reporter.h"
+#include "ash/public/cpp/feature_discovery_metric_util.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -140,6 +142,9 @@ void AppListToastContainerView::CreateReorderNudgeView() {
         base::BindRepeating(&AppListToastContainerView::DismissReorderNudgeView,
                             base::Unretained(this)));
   }
+
+  FeatureDiscoveryDurationReporter::GetInstance()->MaybeActivateObservation(
+      feature_discovery::TrackableFeature::kAppListReorderAfterEducationNudge);
 
   toast_view_ = AddChildView(
       toast_view_builder.SetStyleForTabletMode(tablet_mode_)
