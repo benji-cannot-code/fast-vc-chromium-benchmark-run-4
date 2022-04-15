566c5e4f28edfccc5593a7e5acb1ae839198277f
Matthew Mourgos <mmourgos@chromium.org>
2022-04-15 18:48:33 +0000

    Add reorder toast fade out animation
    When the reorder nudge/toast are closed using the dismiss/close buttons,
    a fade out animation will trigger for the toast. Once this fade out
    animation has completed, the already existing app grid animation will
    begin to move the app items into their updated position.
    
    Bug: 1309971
    Change-Id: I126b599162546151a694951a1726ae34a38c43df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582793
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993002}

--- ash/app_list/views/app_list_toast_container_view.h
+++ ash/app_list/views/app_list_toast_container_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/app_list/views/app_list_nudge_controller.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -94,9 +95,6 @@ class AppListToastContainerView : public views::View {
   // Creates a reorder nudge view in the container.
   void CreateReorderNudgeView();
 
-  // Dismisses the reorder nudge view and ensures it will no longer be shown.
-  void DismissReorderNudgeView();
-
   // Removes the reorder nudge view if the nudge view is showing.
   void RemoveReorderNudgeView();
 
@@ -138,14 +136,18 @@ class AppListToastContainerView : public views::View {
   // Called when the `toast_view_`'s dismiss button is clicked.
   void OnReorderUndoButtonClicked();
 
-  // Called when the `toast_view_`'s close button is clicked.
-  void OnReorderCloseButtonClicked();
-
   // Calculates the toast text based on the temporary sorting order.
   [[nodiscard]] std::u16string CalculateToastTextFromOrder(
       AppListSortOrder order) const;
 
   std::u16string GetA11yTextOnUndoButtonFromOrder(AppListSortOrder order) const;
+
+  // Animates the opacity of `toast_view_` to fade out, then calls
+  // OnFadeOutToastViewComplete().
+  void FadeOutToastView();
+
+  // Called when the fade out animation for the `toast_view_` is finished.
+  void OnFadeOutToastViewComplete();
 
   AppListA11yAnnouncer* const a11y_announcer_;
 
@@ -171,6 +173,8 @@ class AppListToastContainerView : public views::View {
   // Caches the column of previously focused app. Used when passing focus
   // between apps grid view and recent apps.
   int focused_app_column_ = 0;
+
+  base::WeakPtrFactory<AppListToastContainerView> weak_factory_{this};
 };
 
 }  // namespace ash
