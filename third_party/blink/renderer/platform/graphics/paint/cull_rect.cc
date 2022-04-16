6abff7fd1572501de1b7855e80803ef86e49f95a
Xianzhu Wang <wangxianzhu@chromium.org>
2022-04-16 00:45:19 +0000

    Revert "Use infinite cull rect for contents of small composited scrollers"
    This reverts commit e74b6f53c6607cf1162090304bf388e8a1a8b156.
    
    Reason for revert: Need to investigate the performance regressions
    crbug.com/1316596.
    
    Original change's description:
    > Use infinite cull rect for contents of small composited scrollers
    >
    > Use infinite cull rect for contents of composited scrollers if
    > the expanded cull rect covers the whole scrolling contents.
    >
    > Also add a fast path in CullRect::ApplyPaintProperties() when the
    > input cull rect is infinite and there is no additional clip, to
    > avoid geometry calculation.
    >
    > The benefits of this CL are:
    > 1. Reduces cost of cull rect calculation for the contents cull rect
    >    of descendants of small scrollers.
    > 2. Reduces change of contents cull rect of small scrollers and forced
    >    cull rect of children on change of the scroller's contents cull
    >    rect.
    > 3. Reduces proactive cull rect update of descendants of small
    >    scrollers on scroll.
    >
    > Bug: 1253726
    > Change-Id: I68056bc966e870e075fd3269aa7d0c964da6f8de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583103
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#992821}
    
    Bug: 1253726, 1316596
    Change-Id: Id127ab5bdc03783846167ccf50cf02d0aee4c70d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588129
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#993121}

--- third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
+++ third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
@@ -29,8 +29,6 @@ static int LocalPixelDistanceToExpand(
   static constexpr int kPixelDistanceToExpand = 4000;
 
   gfx::RectF rect(0, 0, 1, 1);
-  // TODO(wangxianzhu): Optimize this if we know there are no ancestor
-  // transforms that change scale.
   GeometryMapper::SourceToDestinationRect(root_transform, local_transform,
                                           rect);
   // Now rect.Size() is the size of a screen pixel in local coordinates.
@@ -102,16 +100,6 @@ bool CullRect::ApplyScrollTranslation(
 
   // Expand the cull rect for scrolling contents for composited scrolling.
   rect_.Outset(LocalPixelDistanceToExpand(root_transform, scroll_translation));
-  if (rect_.Contains(contents_rect)) {
-    // The cull rect covers all scrolling contents. Use infinite rect to
-    // avoid unnecessary cull rect update on scroll, and to enable fast path
-    // of cull rect calculation for descendants.
-    // TODO(wangxianzhu): Optimize this further in case that we can know this
-    // before ApplyTransform().
-    *this = Infinite();
-    return false;
-  }
-
   rect_.Intersect(contents_rect);
   return true;
 }
@@ -144,13 +132,6 @@ bool CullRect::ApplyPaintProperties(
     const PropertyTreeState& source,
     const PropertyTreeState& destination,
     const absl::optional<CullRect>& old_cull_rect) {
-  // The caller should check this before calling this function.
-  DCHECK_NE(source, destination);
-
-  // Only a clip can make an infinite cull rect finite.
-  if (IsInfinite() && &destination.Clip() == &source.Clip())
-    return false;
-
   Vector<const TransformPaintPropertyNode*, 4> scroll_translations;
   Vector<const ClipPaintPropertyNode*, 4> clips;
   bool abnormal_hierarchy = false;
@@ -186,8 +167,7 @@ bool CullRect::ApplyPaintProperties(
     // Either the transform or the clip of |source| is not an ancestor of
     // |destination|. Map infinite rect from the root.
     *this = Infinite();
-    return root != destination &&
-           ApplyPaintProperties(root, root, destination, old_cull_rect);
+    return ApplyPaintProperties(root, root, destination, old_cull_rect);
   }
 
   // These are either the source transform/clip or the last scroll
