3999d7c3eaa97400c867cdbcee177230f2276b8f
David Sanders <dsanders11@ucsbalum.com>
2022-04-16 03:36:51 +0000

    Do IWYU for unserialized_message_context
    Bug: 242216
    Change-Id: Ifae0724846ae675dc574fef8d1c9c59ee5c0eaee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588035
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#993149}

--- mojo/public/cpp/bindings/lib/unserialized_message_context.h
+++ mojo/public/cpp/bindings/lib/unserialized_message_context.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/memory/raw_ptr.h"
-#include "mojo/public/c/system/types.h"
-#include "mojo/public/cpp/bindings/lib/buffer.h"
 #include "mojo/public/cpp/bindings/lib/message_internal.h"
 
 namespace mojo {
