827fefa38f42081c6ac39d347d597c4153d672fd
Michael Wilson <mjwilson@chromium.org>
2022-04-18 22:39:45 +0000

    [WebAudio] Break AudioDestination node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: I5973f95f7469d433fdf16fad575c54fbaab19024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569404
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993471}

---
+++ third_party/blink/renderer/modules/webaudio/audio_destination_handler.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webaudio/audio_destination_handler.h"
+
+namespace blink {
+
+AudioDestinationHandler::AudioDestinationHandler(AudioNode& node)
+    : AudioHandler(kNodeTypeDestination, node, 0) {
+  AddInput();
+}
+
+AudioDestinationHandler::~AudioDestinationHandler() {
+  DCHECK(!IsInitialized());
+}
+
+}  // namespace blink
