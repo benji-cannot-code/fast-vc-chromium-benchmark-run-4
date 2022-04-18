ade68718c2a59270555a001002d38454fb63bacc
Kevin Radtke <kradtke@chromium.org>
2022-04-18 18:16:23 +0000

    Add `has_dismiss_button` field to ToastData args
    Have ToastData calls explicitly state if they have a dismiss button
    through the `has_dismiss_button` argument.
    If true, the dismiss button will use the default dismiss text unless a
    `custom_dismiss_text` is given.
    
    Bug: 1314543
    Change-Id: I5f60bfaadb1e078c6857b9dbb0c9400f4fbde7d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577113
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993369}

--- ash/style/system_toast_style.h
+++ ash/style/system_toast_style.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/callback_forward.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
@@ -32,7 +31,7 @@ class ASH_EXPORT SystemToastStyle : public views::View {
 
   SystemToastStyle(base::RepeatingClosure dismiss_callback,
                    const std::u16string& text,
-                   const absl::optional<std::u16string>& dismiss_text,
+                   const std::u16string& dismiss_text,
                    const bool is_managed);
   SystemToastStyle(const SystemToastStyle&) = delete;
   SystemToastStyle& operator=(const SystemToastStyle&) = delete;
