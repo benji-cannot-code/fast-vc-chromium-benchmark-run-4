bbdd762a2796b2e5d634ebaf40e3d9bf702217a0
Andrew Paseltiner <apaseltiner@chromium.org>
2022-04-18 16:07:43 +0000

    Replace output-only attribution stringstreams with ostringstream
    This makes it clearer that the stream is only used for output, not
    input.
    
    Change-Id: I4e054deb514d28ee3107135e06eb6ebf1de1af7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587620
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993327}

--- content/test/attribution_simulator_input_parser_unittest.cc
+++ content/test/attribution_simulator_input_parser_unittest.cc
@@ -57,7 +57,7 @@ TEST(AttributionSimulatorInputParserTest, EmptyInputParses) {
 
   for (const char* json : kTestCases) {
     base::Value value = base::test::ParseJson(json);
-    std::stringstream error_stream;
+    std::ostringstream error_stream;
     EXPECT_THAT(ParseAttributionSimulationInput(std::move(value), kOffsetTime,
                                                 error_stream),
                 Optional(IsEmpty()))
@@ -124,7 +124,7 @@ TEST(AttributionSimulatorInputParserTest, ValidSourceParses) {
   ]})json";
 
   base::Value value = base::test::ParseJson(kJson);
-  std::stringstream error_stream;
+  std::ostringstream error_stream;
   EXPECT_THAT(
       ParseAttributionSimulationInput(std::move(value), kOffsetTime,
                                       error_stream),
@@ -234,7 +234,7 @@ TEST(AttributionSimulatorInputParserTest, OutputRetainsInputJSON) {
     ]})json";
 
   const base::Value value = base::test::ParseJson(kJson);
-  std::stringstream error_stream;
+  std::ostringstream error_stream;
   EXPECT_THAT(
       ParseAttributionSimulationInput(value.Clone(), kOffsetTime, error_stream),
       Optional(ElementsAre(
@@ -288,7 +288,7 @@ TEST(AttributionSimulatorInputParserTest, ValidTriggerParses) {
   ]})json";
 
   base::Value value = base::test::ParseJson(kJson);
-  std::stringstream error_stream;
+  std::ostringstream error_stream;
 
   std::vector<blink::mojom::AttributionAggregatableTriggerDataPtr>
       aggregatable_trigger_data;
@@ -394,7 +394,7 @@ TEST(AttributionSimulatorInputParserTest, ValidSourceAndTriggerParses) {
   })json";
 
   base::Value value = base::test::ParseJson(kJson);
-  std::stringstream error_stream;
+  std::ostringstream error_stream;
   EXPECT_THAT(ParseAttributionSimulationInput(std::move(value), kOffsetTime,
                                               error_stream),
               Optional(SizeIs(2)));
@@ -413,7 +413,7 @@ TEST_P(AttributionSimulatorInputParseErrorTest, InvalidInputFails) {
   const ParseErrorTestCase& test_case = GetParam();
 
   base::Value value = base::test::ParseJson(test_case.json);
-  std::stringstream error_stream;
+  std::ostringstream error_stream;
   EXPECT_EQ(ParseAttributionSimulationInput(std::move(value), kOffsetTime,
                                             error_stream),
             absl::nullopt);
