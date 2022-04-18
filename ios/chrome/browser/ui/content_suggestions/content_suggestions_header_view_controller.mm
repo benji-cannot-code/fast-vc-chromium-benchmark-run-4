80180fc982386cdc4eaa88b87d25f1e7042b79b5
Chris Lu <thegreenfrog@chromium.org>
2022-04-18 22:12:10 +0000

    [ios] Relayout Content Suggestions header when logo is removed
    With content-suggestions-header-migration enabled, the header's height
    has a new constraint that needs to be updated when the logo is hidden
    upon a change to the search engine. Also, layoutIfNeeded needs to be
    called as an equivalent to the default
    [self.collectionSynchronizer invalidateLayout] call to force a relayout.
    
    Fixed: 1316574, 1316359
    Change-Id: Ibff34e87f7c82fb5db6c1d9793e99bd18047b7fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588085
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993459}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
@@ -121,12 +121,6 @@ const NSString* kScribbleFakeboxElementId = @"fakebox";
   if (self.traitCollection.horizontalSizeClass !=
       previousTraitCollection.horizontalSizeClass) {
     [self updateFakeboxDisplay];
-    if (IsContentSuggestionsHeaderMigrationEnabled()) {
-      self.headerViewHeightConstraint.constant =
-          content_suggestions::heightForLogoHeader(
-              self.logoIsShowing, self.logoVendor.isShowingDoodle,
-              self.promoCanShow, YES, [self topInset], self.traitCollection);
-    }
   }
 }
 
@@ -516,7 +510,15 @@ const NSString* kScribbleFakeboxElementId = @"fakebox";
                       self.logoVendor.isShowingDoodle, self.traitCollection)];
   self.fakeOmnibox.hidden =
       IsRegularXRegularSizeClass(self) && !self.logoIsShowing;
-  [self.collectionSynchronizer invalidateLayout];
+  if (IsContentSuggestionsHeaderMigrationEnabled()) {
+    [self.headerView layoutIfNeeded];
+    self.headerViewHeightConstraint.constant =
+        content_suggestions::heightForLogoHeader(
+            self.logoIsShowing, self.logoVendor.isShowingDoodle,
+            self.promoCanShow, YES, [self topInset], self.traitCollection);
+  } else {
+    [self.collectionSynchronizer invalidateLayout];
+  }
 }
 
 // If Google is not the default search engine, hides the logo, doodle and
