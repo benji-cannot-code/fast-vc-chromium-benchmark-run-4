1cd3fba909f2739a6e04ba741dd1b9cf7ae2ff2c
Dmitrii Kuragin <kuragin@chromium.org>
2022-04-18 20:54:07 +0000

    Hide "open in window" option for isolated app on chrome://apps page.
    The code is old and very difficult to test. I tested the change manually
    using the following options:
     - Regular PWA
     - Isolated PWA
     - Extension.
    
    Exact apps are described in the bug.
    
    Bug: 1289457
    Change-Id: I0742c8d61771a8cd99164f288bc9acf7113c99a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579601
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Commit-Queue: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993433}

--- chrome/browser/resources/ntp4/app_info.js
+++ chrome/browser/resources/ntp4/app_info.js
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            is_deprecated_app: boolean,
  *            is_webstore: boolean,
  *            isLocallyInstalled: boolean,
+ *            hideDisplayMode: boolean,
  *            kioskEnabled: boolean,
  *            kioskMode: boolean,
  *            kioskOnly: boolean,
