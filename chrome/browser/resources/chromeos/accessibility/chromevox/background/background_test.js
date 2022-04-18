ae08691660d71c6cc5689d5fe8fe24b97fea6168
Anastasia Helfinstein <anastasi@google.com>
2022-04-18 21:02:26 +0000

    [ChromeVox] Clean up TestUtils
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: 1301002
    Test: browser_tests --gtest_filter=*ChromeVox*
    Change-Id: I0d3b531f4e079e1419c8285644b824f6dd9287bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587277
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#993435}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
@@ -3240,8 +3240,7 @@ TEST_F(
       p.doDefault();
 
       // This comes when the <select>'s value changes.
-      await TestUtils.waitForEvent(
-          application, EventType.SELECTED_VALUE_CHANGED);
+      await this.waitForEvent(application, EventType.SELECTED_VALUE_CHANGED);
 
       // Nothing should have been spoken.
       assertEquals(undefined, nextSpeech);
