162b658fd6b9239027fb29fbcd75d0fb940d0545
David Black <dmblack@google.com>
2022-04-18 20:05:15 +0000

    personalization: Show error on failure to load Google Photos albums.
    The error is rendered as a toast with a retry button to allow reattempt
    to fetch the list of albums. Similar will be done in a follow up CL for
    failure to load the lists of Google Photos photos.
    
    Demo: http://shortn/_gvzwDfsGiT [1]
    
    [1] Demo shot w/ additional local changes to force state.
    
    Bug: b:229035379
    Change-Id: Iec12d89a20da68bcd5fd4c8d3145b43eacd43d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584653
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#993417}

--- ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_by_album_id_element.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_by_album_id_element.ts
@@ -173,8 +173,7 @@ export class GooglePhotosPhotosByAlbumId extends WithPersonalizationStore {
     if (hidden) {
       // If |hidden|, the error associated with this element will have lost
       // user-facing context so it should be dismissed.
-      this.getStore().dispatch(
-          dismissErrorAction(ERROR_ID, /*fromUser=*/ false));
+      this.dispatch(dismissErrorAction(ERROR_ID, /*fromUser=*/ false));
       return;
     }
 
@@ -217,7 +216,7 @@ export class GooglePhotosPhotosByAlbumId extends WithPersonalizationStore {
     // user that allows them to make another attempt.
     if (photosByAlbumId[albumId] === null) {
       if (!this.hidden) {
-        this.getStore().dispatch(setErrorAction({
+        this.dispatch(setErrorAction({
           id: ERROR_ID,
           message: this.i18n('googlePhotosError'),
           dismiss: {
