b14cf14d318d414333582a4a61186878e83616b2
Victor Tan <victortan@chromium.org>
2022-04-18 16:36:34 +0000

    Migrate iframe-based Client Hint WPTs to import client hints lists
    Introduce two new files into "client-hints/resources" to centralize the
    dependencies of client hint token lists across javascript and python
    tests:
    
      - export.js
      - clienthintslist.py
    
    Eventually all Client Hints tests will be migrated to using these lists
    to pare down the number of tests touched when creating new hints; this
    CL pertains to the tests that depend on
    "client-hints/resources/expect-client-hints-headers-iframe.py"
    
    One of change is the query strings fed to
    "expect-client-hints-headers-iframe.py" is now generated in
    "client-hints/resources/feature-policy-navigation.js".
    
    Another change is fixing tests related to `sec-ch-ua-platform` since we
    added platform to client hints returned by default in
    getHighEntropyValues in https://crrev.com/c/3389295.
    
    Bug: 1304740
    Change-Id: Id41ddbe1ee29c10f7b7c96a283f7eca2abec438b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549556
    Reviewed-by: Ali Beyad <abeyad@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993338}

---
+++ third_party/blink/web_tests/external/wpt/client-hints/resources/clienthintslist.py
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def client_hints_list():
+  return [b"device-memory",
+          b"dpr",
+        # b"width", (Only available for images)
+          b"viewport-width",
+          b"rtt",
+          b"downlink",
+          b"ect",
+          b"sec-ch-ua",
+          b"sec-ch-ua-arch",
+          b"sec-ch-ua-platform",
+          b"sec-ch-ua-model",
+          b"sec-ch-ua-mobile",
+          b"sec-ch-ua-full-version",
+          b"sec-ch-ua-platform-version",
+          b"sec-ch-prefers-color-scheme",
+          b"sec-ch-ua-bitness",
+          b"sec-ch-viewport-height",
+          b"sec-ch-device-memory",
+          b"sec-ch-dpr",
+        # b"sec-ch-width", (Only available for images)
+          b"sec-ch-viewport-width",
+          b"sec-ch-ua-full-version-list",
+          b"sec-ch-ua-wow64",
+  ]
