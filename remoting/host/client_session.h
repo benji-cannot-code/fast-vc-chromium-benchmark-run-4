015c2940c24c13a7a598a32d284034724be927e7
Lambros Lambrou <lambroslambrou@chromium.org>
2022-04-19 22:20:17 +0000

    [remoting host] Create/remove video-streams on displays changed.
    When multi-stream is enabled, this CL causes the host to create and
    remove video-streams whenever the display-list is changed. Before this
    CL, if monitors were added/removed, you needed to reconnect to see the
    changes.
    
    Change-Id: If974207a01984b3e5967e84e0284ccbf158efe9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594497
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993922}

--- remoting/host/client_session.h
+++ remoting/host/client_session.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_HOST_CLIENT_SESSION_H_
 
 #include <cstdint>
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -318,7 +319,7 @@ class ClientSession : public protocol::HostStub,
   base::OneShotTimer max_duration_timer_;
 
   // Objects responsible for sending video, audio.
-  std::vector<VideoStreamWithComposer> video_streams_;
+  std::map<webrtc::ScreenId, VideoStreamWithComposer> video_streams_;
   std::unique_ptr<protocol::AudioStream> audio_stream_;
 
   // The set of all capabilities supported by the client.
