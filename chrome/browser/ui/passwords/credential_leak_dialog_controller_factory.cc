276049d1b0932e805a494dfbcb48b782d79b31f8
Elias <eliaskh@chromium.org>
2022-04-19 16:35:35 +0000

    [Password] Refactoring: create skeleton factory for credential leak dialog controller.
    The aim of the factory is to create the 4+growing leak dialog controllers that currently live in one cumbersome file (leak_detection_dialog_utils.cc).
    Actual implementation of the factory will follow in upcoming cls.
    
    Bug: 1313893
    Change-Id: Ic4ff82f774e3fc6d2f650d99084416ac2b4abeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585684
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993744}

---
+++ chrome/browser/ui/passwords/credential_leak_dialog_controller_factory.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/credential_leak_dialog_controller_factory.h"
+
+namespace credential_leak_dialog_factory {
+
+std::unique_ptr<CredentialLeakDialogController> CreateController(
+    password_manager::metrics_util::LeakDialogType dialog_type) {
+  // TODO(crbug/1313893): implement factory logic to instantiate the different
+  // dialog types.
+  return nullptr;
+}
+
+}  // namespace credential_leak_dialog_factory
---
+++ chrome/browser/ui/passwords/credential_leak_dialog_controller_factory.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PASSWORDS_CREDENTIAL_LEAK_DIALOG_CONTROLLER_FACTORY_H_
+#define CHROME_BROWSER_UI_PASSWORDS_CREDENTIAL_LEAK_DIALOG_CONTROLLER_FACTORY_H_
+
+#include "chrome/browser/ui/passwords/credential_leak_dialog_controller.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
+
+namespace credential_leak_dialog_factory {
+
+// Factory function for creating CredentialLeakDialogController objects.
+std::unique_ptr<CredentialLeakDialogController> CreateController(
+    password_manager::metrics_util::LeakDialogType dialog_type);
+
+}  // namespace credential_leak_dialog_factory
+
+#endif  // CHROME_BROWSER_UI_PASSWORDS_CREDENTIAL_LEAK_DIALOG_CONTROLLER_FACTORY_H_
