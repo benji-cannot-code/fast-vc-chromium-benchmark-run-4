ee2d137c0a3e02c9c9672bebb8ee75897bb9b035
Jeffrey Young <cowmoo@google.com>
2022-04-19 20:50:46 +0000

    personalization: index search results
    Add new personalization index to local_search_service.
    Index a single search result for now for the title of
    Personalization app.
    
    BUG=b:225950660
    TEST=ash_webui_unittests --gtest_filter="PersonalizationAppSearch"
    TEST=open launcher, search Personalization, click top result
    
    Change-Id: I2880a9d2d00e0faae8daa6864af636de83003c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3569081
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993884}

--- chrome/browser/ui/app_list/search/personalization_provider.cc
+++ chrome/browser/ui/app_list/search/personalization_provider.cc
@@ -37,6 +37,7 @@ namespace app_list {
 namespace {
 
 inline constexpr size_t kMinQueryLength = 3u;
+inline constexpr size_t kNumRequestedResults = 3u;
 
 }  // namespace
 
@@ -105,7 +106,7 @@ void PersonalizationProvider::Start(const std::u16string& query) {
 
   current_query_ = query;
   weak_ptr_factory_.InvalidateWeakPtrs();
-  search_handler_->Search(query,
+  search_handler_->Search(query, kNumRequestedResults,
                           base::BindOnce(&PersonalizationProvider::OnSearchDone,
                                          weak_ptr_factory_.GetWeakPtr()));
 }
