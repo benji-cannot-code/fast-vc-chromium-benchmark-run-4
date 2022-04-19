4e69b42842b5d029d57cf56ee0b1750db594fcd4
Kent Tamura <tkent@chromium.org>
2022-04-19 05:58:32 +0000

    cross_thread_copier.h includes <type_traits> instead of WTF's one
    This CL has no behavior changes.
    
    Bug: 242216
    Change-Id: Ia5f32b15c391eaf44961900397a3399447ce4f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592212
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993580}

--- third_party/blink/renderer/platform/wtf/cross_thread_copier.h
+++ third_party/blink/renderer/platform/wtf/cross_thread_copier.h
@@ -32,9 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_CROSS_THREAD_COPIER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_CROSS_THREAD_COPIER_H_
 
+#include <type_traits>
+
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
-#include "third_party/blink/renderer/platform/wtf/type_traits.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 
 namespace WTF {
