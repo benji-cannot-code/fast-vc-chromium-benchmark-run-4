73d319aa2f1ca2b7e7319cd70d92e101ac655b19
Andrew Paseltiner <apaseltiner@chromium.org>
2022-04-19 21:08:47 +0000

    Move uint64 serialization helpers to attribution_storage_sql.cc
    These helpers are only used by one file, and their names are prone
    to conflict with other functions in the content namespace.
    
    Change-Id: I77a562d45aef7738a93936c776ced2c334b217b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593932
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993894}

--- content/browser/attribution_reporting/sql_utils.h
+++ content/browser/attribution_reporting/sql_utils.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ATTRIBUTION_REPORTING_SQL_UTILS_H_
 #define CONTENT_BROWSER_ATTRIBUTION_REPORTING_SQL_UTILS_H_
 
-#include <stdint.h>
-
 #include <string>
 
 #include "base/dcheck_is_on.h"
@@ -36,14 +34,6 @@ namespace content {
 std::string SerializeOrigin(const url::Origin& origin);
 
 url::Origin DeserializeOrigin(const std::string& origin);
-
-int64_t SerializeUint64(uint64_t data);
-
-uint64_t DeserializeUint64(int64_t data);
-
-// Prevent these functions from being called in the wrong direction.
-int64_t SerializeUint64(int64_t data) = delete;
-uint64_t DeserializeUint64(uint64_t data) = delete;
 
 }  // namespace content
 
