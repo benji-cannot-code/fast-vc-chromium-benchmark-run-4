536717bd2f62cc3045d20aba0fd871842dd69e3b
Colin Blundell <blundell@chromium.org>
2022-04-19 14:18:45 +0000

    [TerminationNotification] Convert //c/b/notifications to use callback
    We are in the process of eliminating NOTIFICATION_APP_TERMINATING. This
    CL converts listeners in //chrome/browser/notifications to instead
    use browser_shutdown::AddOnAppTerminatingCallback().
    
    Bug: 1317629
    Change-Id: I0c4d91087bf1c58f3c9d39cc1d851beda04b102d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587192
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993691}

--- chrome/browser/notifications/notification_system_observer.h
+++ chrome/browser/notifications/notification_system_observer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_SYSTEM_OBSERVER_H_
 #define CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_SYSTEM_OBSERVER_H_
 
+#include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_multi_source_observation.h"
 #include "content/public/browser/notification_observer.h"
@@ -32,6 +33,8 @@ class NotificationSystemObserver : public content::NotificationObserver,
   ~NotificationSystemObserver() override;
 
  protected:
+  void OnAppTerminating();
+
   // content::NotificationObserver override.
   void Observe(int type,
                const content::NotificationSource& source,
@@ -46,6 +49,7 @@ class NotificationSystemObserver : public content::NotificationObserver,
  private:
   // Registrar for the other kind of notifications (event signaling).
   content::NotificationRegistrar registrar_;
+  base::CallbackListSubscription on_app_terminating_subscription_;
   raw_ptr<NotificationUIManager> ui_manager_;
 
   base::ScopedMultiSourceObservation<extensions::ExtensionRegistry,
