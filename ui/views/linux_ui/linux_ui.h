2bcb86c8f9724270fd569629302bf0689d655579
Keren Zhu <kerenzhu@chromium.org>
2022-04-19 20:18:38 +0000

    [Color Pipeline] Add GTK theme redirection test support
    Add a test that iterates and enables available GTK themes in order to
    verify that ColorProvider's colors match ThemeProvider's colors.
    
    The test is disabled at the moment since there're mismatched colors in
    some themes. They will be fixed in follow-ups.
    
    Once this test is enabled we can remove the SystemTheme param from
    ThemeProviderRedirectedEquivalenceTest, since that will be fully covered
    by this test.
    
    Bug: 1310397
    Change-Id: Id9bedef8e1589ac2459b171b14ff7f1f3b5c530a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585031
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993871}

--- ui/views/linux_ui/linux_ui.h
+++ ui/views/linux_ui/linux_ui.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/callback.h"
 #include "build/buildflag.h"
@@ -194,6 +195,13 @@ class VIEWS_EXPORT LinuxUI : public ui::LinuxInputMethodContextFactory,
 
   // Returns a map of KeyboardEvent code to KeyboardEvent key values.
   virtual base::flat_map<std::string, std::string> GetKeyboardLayoutMap() = 0;
+
+  // Returns the names of available system themes. Used only in test.
+  virtual std::vector<std::string> GetAvailableSystemThemeNamesForTest()
+      const = 0;
+
+  // Set the system theme by name. Used only in test.
+  virtual void SetSystemThemeByNameForTest(const std::string& theme_name) = 0;
 
  protected:
   LinuxUI();
