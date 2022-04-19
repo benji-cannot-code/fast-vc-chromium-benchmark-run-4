b9ae049293eee054c76f0c72ac0474d99d1c9135
Yutaka Hirano <yhirano@chromium.org>
2022-04-19 11:30:32 +0000

    [disk cache] Broker file operations for DoomEntry
    Sandbox file operations that is needed to Doom an entry. We use
    disk_cahe::Backend::DoomEntry rather than disk_cahe::Entry::Doom in tests
    because the former has a completion callback.
    
    Add a mode parameter to BackendFileOperations::DeleteFile to broker
    simple_util::SimpleCacheDeleteFile.
    
    Bug: 1289542
    Change-Id: I5f38bf1e909cfad53b5dbefbc9c77c0a4983c73c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585103
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993655}

--- net/disk_cache/disk_cache.cc
+++ net/disk_cache/disk_cache.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/memory/mem_backend_impl.h"
 #include "net/disk_cache/simple/simple_backend_impl.h"
 #include "net/disk_cache/simple/simple_file_enumerator.h"
+#include "net/disk_cache/simple/simple_util.h"
 
 namespace {
 
@@ -505,14 +506,24 @@ base::File TrivialFileOperations::OpenFile(const base::FilePath& path,
   return file;
 }
 
-bool TrivialFileOperations::DeleteFile(const base::FilePath& path) {
+bool TrivialFileOperations::DeleteFile(const base::FilePath& path,
+                                       DeleteFileMode mode) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(path.IsAbsolute());
 #if DCHECK_IS_ON()
   DCHECK(bound_);
 #endif
 
-  return base::DeleteFile(path);
+  bool result = false;
+  switch (mode) {
+    case DeleteFileMode::kDefault:
+      result = base::DeleteFile(path);
+      break;
+    case DeleteFileMode::kEnsureImmediateAvailability:
+      result = disk_cache::simple_util::SimpleCacheDeleteFile(path);
+      break;
+  }
+  return result;
 }
 
 bool TrivialFileOperations::ReplaceFile(const base::FilePath& from_path,
