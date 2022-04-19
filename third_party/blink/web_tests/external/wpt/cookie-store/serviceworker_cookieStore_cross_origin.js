dedcdfdf659a342e3958ed6e516a3f25dee45fb4
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-04-19 14:46:57 +0000

    Import wpt@ce3cb95ea18f80a33a66df3a5b1e0c3f348e6ec6
    Using wpt-import in Chromium 8944539a8a714e606d20758fe2baefaa1e66fd1d.
    With Chromium commits locally applied on WPT:
    aedd367d0d "Migrate iframe-based Client Hint WPTs to import client hints lists"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ayui@chromium.org, asully@chromium.org, jsbell@chromium.org:
      external/wpt/cookie-store
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
      external/wpt/css/css-sizing
    jsbell@chromium.org:
      external/wpt/resources
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I0247f975c73f68486fde1883809a5a685fc97a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593940
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#993704}

--- third_party/blink/web_tests/external/wpt/cookie-store/serviceworker_cookieStore_cross_origin.js
+++ third_party/blink/web_tests/external/wpt/cookie-store/serviceworker_cookieStore_cross_origin.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.GLOBAL = {
   isWindow: () => false,
   isWorker: () => false,
+  isShadowRealm: () => false,
 };
 
 self.addEventListener('message', async event => {
