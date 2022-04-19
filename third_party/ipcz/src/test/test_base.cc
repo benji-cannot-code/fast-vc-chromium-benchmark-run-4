30f6cd9cfd741669086c01ed0852972377f19dc6
Ken Rockot <rockot@google.com>
2022-04-19 21:52:26 +0000

    ipcz: Introduce portals
    Split out from the ipcz uber-CL: https://crrev.com/c/3570271
    
    Implements a basic OpenPortals() API as well as QueryPortalStatus().
    
    This is mostly boilerplate and plumbing to establish portals as API
    objects and to supply baseline skeletons for Router and RouterLink
    implementations.
    
    Bug: 1299283
    Change-Id: I10334d8ad84a09890b0e9775f843187b206bf59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584526
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#993910}

--- third_party/ipcz/src/test/test_base.cc
+++ third_party/ipcz/src/test/test_base.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ipcz::test {
 
 TestBase::TestBase() {
-  IpczGetAPI(&ipcz);
+  IpczGetAPI(&ipcz_);
 }
 
 TestBase::~TestBase() = default;
