5db7911190a1ebed7c5981855cc1b37a8eb010af
Mohamed Amir Yosef <mamir@chromium.org>
2022-04-19 19:37:15 +0000

    [Passwords] Update Password Generation Prompt Text in Autofill Dropdown
    This CL updates the text shows in the password generation prompt in the
    Autofill dropdown to reflect Google Password Manager branding
    
    Mocks: http://slides/1lDlG2iyXfcvkcXimycd0u6iCHmK46l_LjCIicuedh-A#slide=id.gf54ea40b34_0_198
    
    Screenshots:
    Light mode: http://screen/9SQ26DyjyyKnzPd
    Dark mode: http://screen/8HENyC7LwkVLzF5
    
    
    Bug: 1309480
    Change-Id: I27acc3dbccd434d3be52cc9d85756f05e7f0a55b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585339
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993841}

--- chrome/browser/ui/passwords/ui_utils.cc
+++ chrome/browser/ui/passwords/ui_utils.cc
@@ -206,6 +206,8 @@ GURL GetGooglePasswordManagerURL(ManagePasswordsReferrer referrer) {
         return "safe_state";
       case ManagePasswordsReferrer::kSaveUpdateBubble:
         return "save_update_password_bubble";
+      case ManagePasswordsReferrer::kPasswordGenerationPrompt:
+        return "password_generation_prompt_in_autofill_dropdown";
       case ManagePasswordsReferrer::kPasswordsAccessorySheet:
       case ManagePasswordsReferrer::kTouchToFill:
       case ManagePasswordsReferrer::kPasswordBreachDialog:
--- chrome/browser/ui/views/passwords/password_generation_popup_view_views.cc
+++ chrome/browser/ui/views/passwords/password_generation_popup_view_views.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/ui_utils.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
+#include "chrome/browser/ui/views/passwords/views_utils.h"
+#include "chrome/grit/chromium_strings.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -29,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
@@ -234,15 +238,37 @@ void PasswordGenerationPopupViewViews::CreateLayoutAndChildren() {
   AddChildView(password_view_.get());
   PasswordSelectionUpdated();
 
-  help_label_ = new views::Label(controller_->HelpText(),
-                                 views::style::CONTEXT_DIALOG_BODY_TEXT,
-                                 views::style::STYLE_SECONDARY);
-  help_label_->SetMultiLine(true);
-  help_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  help_label_->SetBorder(views::CreateEmptyBorder(
-      gfx::Insets::TLBR(kVerticalPadding, kHorizontalMargin, kVerticalPadding,
-                        kHorizontalMargin)));
-  AddChildView(help_label_.get());
+  if (!base::FeatureList::IsEnabled(
+          password_manager::features::kUnifiedPasswordManagerDesktop)) {
+    help_label_ = new views::Label(controller_->HelpText(),
+                                   views::style::CONTEXT_DIALOG_BODY_TEXT,
+                                   views::style::STYLE_SECONDARY);
+    help_label_->SetMultiLine(true);
+    help_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+    help_label_->SetBorder(views::CreateEmptyBorder(
+        gfx::Insets::TLBR(kVerticalPadding, kHorizontalMargin, kVerticalPadding,
+                          kHorizontalMargin)));
+    AddChildView(help_label_.get());
+  } else {
+    base::RepeatingClosure open_password_manager_closure = base::BindRepeating(
+        [](PasswordGenerationPopupViewViews* view) {
+          if (!view->controller_)
+            return;
+          view->controller_->OnGooglePasswordManagerLinkClicked();
+        },
+        base::Unretained(this));
+
+    help_styled_label_ = AddChildView(CreateGooglePasswordManagerLabel(
+        /*text_message_id=*/
+        IDS_PASSWORD_GENERATION_PROMPT_GOOGLE_PASSWORD_MANAGER,
+        /*link_message_id=*/
+        IDS_PASSWORD_BUBBLES_PASSWORD_MANAGER_LINK_TEXT_SYNCED_TO_ACCOUNT,
+        controller_->GetPrimaryAccountEmail(), open_password_manager_closure));
+
+    help_styled_label_->SetBorder(views::CreateEmptyBorder(
+        gfx::Insets::TLBR(kVerticalPadding, kHorizontalMargin, kVerticalPadding,
+                          kHorizontalMargin)));
+  }
 }
 
 void PasswordGenerationPopupViewViews::OnThemeChanged() {
@@ -251,7 +277,12 @@ void PasswordGenerationPopupViewViews::OnThemeChanged() {
   password_view_->UpdateBackground(controller_->password_selected()
                                        ? GetSelectedBackgroundColor()
                                        : GetBackgroundColor());
-  help_label_->SetBackgroundColor(GetFooterBackgroundColor());
+  if (help_label_) {
+    help_label_->SetBackgroundColor(GetFooterBackgroundColor());
+  } else if (help_styled_label_) {
+    help_styled_label_->SetDisplayedOnBackgroundColor(
+        GetFooterBackgroundColor());
+  }
 }
 
 void PasswordGenerationPopupViewViews::OnPaint(gfx::Canvas* canvas) {
