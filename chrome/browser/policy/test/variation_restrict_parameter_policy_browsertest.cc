8cec03d05a15aae226c16dbc8b3d08f07284db1b
Xiang Ji <jxiang@google.com>
2022-04-19 22:54:36 +0000

    Enable variation restrict parameter policy browser test on Android
    Validation: tryjobs, local test passed.
    
    Bug: 1313763
    Change-Id: I6b9cedae51699653ece8d8d998f1e3989ff626b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574774
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#993932}

--- chrome/browser/policy/test/variation_restrict_parameter_policy_browsertest.cc
+++ chrome/browser/policy/test/variation_restrict_parameter_policy_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/policy/policy_test_utils.h"
-#include "chrome/test/base/in_process_browser_test.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
