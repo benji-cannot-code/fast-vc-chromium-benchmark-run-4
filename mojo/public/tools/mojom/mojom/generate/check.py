c704e1d1bb51952419b2db91dcb88a24b75d1306
Alex Gough <ajgo@chromium.org>
2022-04-19 00:52:22 +0000

    Adds pre-generation checks to mojo bindings
    Adds scaffolding for a pre-generation check phase to the mojo
    bindings generation script.
    
    Checks will be added in a following so this CL has no functional
    effect. Tests will be added along with the first checks.
    
    Bug: 931315
    Change-Id: Id14ffef50726added46fe3c6c106667ae4dff106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588547
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993509}

---
+++ mojo/public/tools/mojom/mojom/generate/check.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Code shared by the various pre-generation mojom checkers."""
+
+
+class CheckException(Exception):
+  def __init__(self, module, message):
+    self.module = module
+    self.message = message
+    super(CheckException, self).__init__(self.message)
+
+  def __str__(self):
+    return "Failed mojo pre-generation check for {}:\n{}".format(
+        self.module.path, self.message)
+
+
+class Check(object):
+  def __init__(self, module):
+    self.module = module
+
+  def CheckModule(self):
+    """ Subclass should return True if its Checks pass, and throw an
+    exception otherwise. CheckModule will be called immediately before
+    mojom.generate.Generator.GenerateFiles()"""
+    raise NotImplementedError("Subclasses must override/implement this method")
