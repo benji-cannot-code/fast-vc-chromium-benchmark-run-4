e8704584f23ce078b091b9eef54164a3dcc527f2
Caroline Rising <corising@chromium.org>
2022-04-19 17:17:11 +0000

    Unified Side Panel: add OnEntryHidden method to SidePanelEntryObserver.
    Bug: 1270923
    Change-Id: I4bbac4bbc5d9a48300fbd800b0f54dac7a924f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591017
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993764}

--- chrome/browser/ui/views/side_panel/side_panel_entry.cc
+++ chrome/browser/ui/views/side_panel/side_panel_entry.cc
@@ -42,6 +42,11 @@ void SidePanelEntry::OnEntryShown() {
     observer.OnEntryShown(this);
 }
 
+void SidePanelEntry::OnEntryHidden() {
+  for (SidePanelEntryObserver& observer : observers_)
+    observer.OnEntryHidden(this);
+}
+
 void SidePanelEntry::AddObserver(SidePanelEntryObserver* observer) {
   observers_.AddObserver(observer);
 }
