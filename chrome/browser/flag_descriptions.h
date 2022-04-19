d98cc81f1e54bd750271b7cfa3cfdd345f97ebb2
Mei Liang <meiliang@chromium.org>
2022-04-19 16:39:06 +0000

    Remove the store-hour flag
    Change-Id: Idcc48927bdba703bed5875961181274bd610d062
    Bug: 1317533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3590842
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993746}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1336,9 +1336,6 @@ extern const char kSplitCacheByNetworkIsolationKeyDescription[];
 extern const char kStoragePressureEventName[];
 extern const char kStoragePressureEventDescription[];
 
-extern const char kStoreHoursAndroidName[];
-extern const char kStoreHoursAndroidDescription[];
-
 extern const char kStrictOriginIsolationName[];
 extern const char kStrictOriginIsolationDescription[];
 
