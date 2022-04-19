73ebbfc19687c227b5cdb8bb42320642040b9d73
Jim Shargo <jshargo@chromium.org>
2022-04-19 17:10:47 +0000

    ozone/drm: Add a very basic integration test for OzoneDRM
    This test will be used to exercise our KMS code directly against the
    system, on DUT and on VMs.
    
    This submission adds a single basic test that verifies that a display is
    present. It runs against VKMS, which is something we're actively
    exploring for tests like this to run against VMs.
    
    This test will be most useful when deployed via tast, hence the
    inclusion in preflight.
    
    There are, of course, many many tests to add, but I wanted to get this
    in early to get all the tast/cros bits in place and bikeshed location
    and naming :).
    
    TESTED=no-op, but builds and runs against VKMS-enabled VMs locally.
    
    Change-Id: I0806e20297030960735d58e1f56cdc72cf65744f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583365
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jim Shargo <jshargo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993757}

---
+++ ui/ozone/platform/drm/test/integration_test_helpers.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_DRM_TEST_INTEGRATION_TEST_HELPERS_H_
+#define UI_OZONE_PLATFORM_DRM_TEST_INTEGRATION_TEST_HELPERS_H_
+
+#include <utility>
+
+namespace base {
+class File;
+class FilePath;
+}  // namespace base
+
+namespace ui::test {
+
+using PathAndFile = std::pair<base::FilePath, base::File>;
+
+PathAndFile FindDrmDriverOrDie(std::string name);
+
+}  // namespace ui::test
+
+#endif  // UI_OZONE_PLATFORM_DRM_TEST_INTEGRATION_TEST_HELPERS_H_
