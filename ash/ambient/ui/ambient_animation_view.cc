3b3b85a9d87189f67104c3fd7612642e10ff0786
Eric Sum <esum@google.com>
2022-04-19 23:36:40 +0000

    Position attribution text box in animation.
    Final CL to fix the bug. This resizes/positions the text box for
    the photo attribution such that the bottom-right of the text box
    has 24 pixels of padding from the bottom-right of the screen. This
    matches UX requirements.
    
    Bug: b:223446754
    Change-Id: Iaf4b0aa1b4bedc73bcc068595fbe048e2988a10d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591239
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#993953}

--- ash/ambient/ui/ambient_animation_view.cc
+++ ash/ambient/ui/ambient_animation_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ambient/model/ambient_backend_model.h"
 #include "ash/ambient/model/ambient_photo_config.h"
 #include "ash/ambient/resources/ambient_animation_static_resources.h"
+#include "ash/ambient/ui/ambient_animation_attribution_transformer.h"
 #include "ash/ambient/ui/ambient_animation_player.h"
 #include "ash/ambient/ui/ambient_animation_resizer.h"
 #include "ash/ambient/ui/ambient_animation_shield_controller.h"
@@ -285,6 +286,8 @@ void AmbientAnimationView::OnViewBoundsChanged(View* observed_view) {
   gfx::Rect previous_animation_bounds = animated_image_view_->GetImageBounds();
   AmbientAnimationResizer::Resize(*animated_image_view_,
                                   GetPaddingForAnimationJitter());
+  AmbientAnimationAttributionTransformer::TransformTextBox(
+      *animated_image_view_);
   DVLOG(4)
       << "View bounds available. Resized animation with native size "
       << animated_image_view_->animated_image()->GetOriginalSize().ToString()
