fd441dc651409765ba5f6c390a274efb4d2c7c90
Rachel Wong <wrong@chromium.org>
2022-04-19 05:54:04 +0000

    [games] Register for app discovery service updates.
    The app discovery service will notify the game provider whenever there
    are updates, which will update the index accordingly.
    
    Bug: 1305880
    Change-Id: I18db0ba48245b30a0bd6766d3b783483ba3ebe2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591934
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993577}

--- chrome/browser/ui/app_list/search/games/game_provider.h
+++ chrome/browser/ui/app_list/search/games/game_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/callback_list.h"
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
@@ -49,6 +50,7 @@ class GameProvider : public SearchProvider {
  private:
   void UpdateIndex();
   void OnIndexUpdated(const GameIndex& index, apps::DiscoveryError error);
+  void OnIndexUpdatedBySubscription(const GameIndex& index);
   void OnSearchComplete(
       std::u16string query,
       std::vector<std::pair<const apps::Result*, double>> matches);
@@ -58,6 +60,7 @@ class GameProvider : public SearchProvider {
   apps::AppDiscoveryService* const app_discovery_service_;
 
   GameIndex game_index_;
+  base::CallbackListSubscription subscription_;
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<GameProvider> weak_factory_{this};
