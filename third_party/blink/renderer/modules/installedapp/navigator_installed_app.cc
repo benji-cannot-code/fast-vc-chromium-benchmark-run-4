2d9b8eb75062118f8f2da3a148d1b9795e86fe10
Dave Tapuska <dtapuska@chromium.org>
2022-04-19 20:15:48 +0000

    Installed App Provider is only used for the outermost main frame.
    Ensure that we return an error for installed apps in fenced frames.
    Also add a bad message if this does indeed get called for a compromised
    renderer.
    
    BUG=1314858
    
    Change-Id: I1b8da0905c562666ee86962ff4d70b41e10fdee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585441
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993868}

--- third_party/blink/renderer/modules/installedapp/navigator_installed_app.cc
+++ third_party/blink/renderer/modules/installedapp/navigator_installed_app.cc
@@ -37,7 +37,7 @@ ScriptPromise NavigatorInstalledApp::getInstalledRelatedApps(
     return promise;
   }
 
-  if (!navigator.DomWindow()->GetFrame()->IsMainFrame()) {
+  if (!navigator.DomWindow()->GetFrame()->IsOutermostMainFrame()) {
     auto* exception = MakeGarbageCollected<DOMException>(
         DOMExceptionCode::kInvalidStateError,
         "getInstalledRelatedApps() is only supported in "
