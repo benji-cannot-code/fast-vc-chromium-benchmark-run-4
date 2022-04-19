e0723d1a301d30c8fb52d3273cd9234373a4c051
Robert Ogden <robertogden@chromium.org>
2022-04-19 11:06:39 +0000

    Add support to experimentally set the number of threads for TFLite Infer
    Adds a new feature that allows the number of threads set on the
    TFLiteEngine to be experimenttally configured away from the current
    value. Note that the number of threads used is different in different
    places in the code:
      * base model executor: uses 1 thread
      * bert model executor: uses number of CPUs (TFLite default)
    
    The number of threads used can be set separately for different
    OptTargets.
    
    Care was taken to ensure the experimental value is only checked once
    per browsing session, and always on the UI thread.
    
    Bug: 1317116
    Change-Id: Ibb5d887469748a1c3a87257496bc7d6702e213c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588930
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993647}

--- components/optimization_guide/core/base_model_executor.h
+++ components/optimization_guide/core/base_model_executor.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/core/base_model_executor_helpers.h"
 #include "components/optimization_guide/core/execution_status.h"
+#include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/tflite_model_executor.h"
 #include "components/optimization_guide/core/tflite_op_resolver.h"
 #include "third_party/tflite_support/src/tensorflow_lite_support/cc/task/core/base_task_api.h"
@@ -29,6 +30,19 @@ class BaseModelExecutor : public TFLiteModelExecutor<OutputType, InputTypes...>,
   ~BaseModelExecutor() override = default;
   BaseModelExecutor(const BaseModelExecutor&) = delete;
   BaseModelExecutor& operator=(const BaseModelExecutor&) = delete;
+
+ public:
+  // TFLiteModelExecutor:
+  void InitializeAndMoveToExecutionThread(
+      proto::OptimizationTarget optimization_target,
+      scoped_refptr<base::SequencedTaskRunner> execution_task_runner,
+      scoped_refptr<base::SequencedTaskRunner> reply_task_runner) override {
+    num_threads_ = features::OverrideNumThreadsForOptTarget(optimization_target)
+                       .value_or(-1);
+    TFLiteModelExecutor<OutputType, InputTypes...>::
+        InitializeAndMoveToExecutionThread(
+            optimization_target, execution_task_runner, reply_task_runner);
+  }
 
  protected:
   absl::optional<OutputType> Execute(ModelExecutionTask* execution_task,
@@ -54,9 +68,12 @@ class BaseModelExecutor : public TFLiteModelExecutor<OutputType, InputTypes...>,
       return nullptr;
     }
 
+    auto compute_settings = tflite::proto::ComputeSettings();
+    compute_settings.mutable_tflite_settings()
+        ->mutable_cpu_settings()
+        ->set_num_threads(num_threads_);
     absl::Status interpreter_status =
-        tflite_engine->InitInterpreter(tflite::proto::ComputeSettings(),
-                                       /*num_threads=*/1);
+        tflite_engine->InitInterpreter(compute_settings);
     if (!interpreter_status.ok()) {
       DLOG(ERROR) << "Failed to initialize model interpreter: "
                   << interpreter_status.ToString();
@@ -74,6 +91,10 @@ class BaseModelExecutor : public TFLiteModelExecutor<OutputType, InputTypes...>,
                   InputTypes... input) override = 0;
   absl::optional<OutputType> Postprocess(
       const std::vector<const TfLiteTensor*>& output_tensors) override = 0;
+
+ private:
+  // -1 tells TFLite to use its own default number of threads.
+  int num_threads_ = -1;
 };
 
 }  // namespace optimization_guide
