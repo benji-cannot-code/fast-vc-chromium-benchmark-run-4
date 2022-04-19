aaff4bf4eeda66d2eba28ffcc38982a7dde43d86
Kartik Hegde <khegde@chromium.org>
2022-04-19 02:27:38 +0000

    Add traffic counters to PSD
    Adds traffic counter information (counters + last_reset_time) as a PSD.
    This information will be available in chrome://system and feedback
    reports.
    
    BUG=b:221436737
    TEST=Verify that traffic counters can be seen at chrome://system
    
    Change-Id: I8f2ad54babc8ea7fcf657152c10df652fc648389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584449
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993535}

--- chrome/browser/feedback/system_logs/about_system_logs_fetcher.cc
+++ chrome/browser/feedback/system_logs/about_system_logs_fetcher.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/system_logs/reven_log_source.h"
 #include "chrome/browser/ash/system_logs/shill_log_source.h"
 #include "chrome/browser/ash/system_logs/touch_log_source.h"
+#include "chrome/browser/ash/system_logs/traffic_counters_log_source.h"
 #include "chrome/browser/ash/system_logs/ui_hierarchy_log_source.h"
 #endif
 
@@ -47,6 +48,7 @@ SystemLogsFetcher* BuildAboutSystemLogsFetcher() {
 
   fetcher->AddSource(std::make_unique<TouchLogSource>());
   fetcher->AddSource(std::make_unique<ConnectedInputDevicesLogSource>());
+  fetcher->AddSource(std::make_unique<TrafficCountersLogSource>());
 
   // Data sources that directly scrub itentifiable information.
   fetcher->AddSource(std::make_unique<DebugDaemonLogSource>(scrub_data));
