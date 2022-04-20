a247cf2d02577b16b1ca9e361b433d900af44995
Emilia Paz <emiliapaz@chromium.org>
2022-04-20 20:09:22 +0000

    [Extensions] Disable flaky tests due to NotificationService observer
    Bug: 1304951
    Change-Id: Ib3ba9450d218bc9e9c4897cc7d9dfd014243d852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595539
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994329}

--- chrome/browser/ui/views/extensions/extensions_tabbed_menu_view_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_tabbed_menu_view_unittest.cc
@@ -792,7 +792,7 @@ TEST_F(
 // TODO(crbug.com/1304951): Test is flaky.
 TEST_F(
     ExtensionsTabbedMenuViewUnitTest,
-    SiteAccessTab_ExtensionInCorrectSiteAccessSectionAfterChangingSiteAccessUsingContextMenu) {
+    DISABLED_SiteAccessTab_ExtensionInCorrectSiteAccessSectionAfterChangingSiteAccessUsingContextMenu) {
   constexpr char kExtensionName[] = "Has Access Extension";
   auto extension =
       InstallExtensionWithHostPermissions(kExtensionName, {"<all_urls>"});
@@ -857,7 +857,7 @@ TEST_F(
 // TODO(crbug.com/1304951): Test is flaky.
 TEST_F(
     ExtensionsTabbedMenuViewUnitTest,
-    SiteAccessTab_ExtensionsInCorrectSiteAccessSectionAfterClickingOnAction) {
+    DISABLED_SiteAccessTab_ExtensionsInCorrectSiteAccessSectionAfterClickingOnAction) {
   constexpr char kExtensionName[] = "Has Access Extension";
   InstallExtensionWithHostPermissions(kExtensionName, {"<all_urls>"});
 
@@ -897,7 +897,7 @@ TEST_F(
 
 // TODO(crbug.com/1304959): Test is flaky.
 TEST_F(ExtensionsTabbedMenuViewUnitTest,
-       SiteAccessTab_AddAndRemoveExtensionWhenMenuIsOpen) {
+       DISABLED_SiteAccessTab_AddAndRemoveExtensionWhenMenuIsOpen) {
   constexpr char kExtensionA[] = "A Extension";
   constexpr char kExtensionC[] = "C Extension";
   InstallExtensionWithHostPermissions(kExtensionA, {"<all_urls>"});
