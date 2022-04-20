04e244f45cd2ce9e0eee20e44eb58c928025f8c6
Mitsuru Oshima <oshima@chromium.org>
2022-04-20 20:12:49 +0000

    [Cleanup] Cache the display ids for connected display
    This used to be computed from internal states in DisplayManager such
    as `hardware_mirroring_display_id_list_` and
    `software_mirroring_display_list_`.
    
    Instead, this CL changes it to cache the display ids from display
    list passed from ozone. This also adds DCHECKs to verify that
    the internal state and the cached display ids are in sync.
    
    Bug: 1313338
    Change-Id: Ie0d521ee9517cf9f9d3ce22dcac10aca724b8294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586092
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994333}

--- ui/display/manager/display_layout_store.h
+++ ui/display/manager/display_layout_store.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <map>
 #include <memory>
 
+#include "base/containers/flat_map.h"
 #include "ui/display/display.h"
 #include "ui/display/display_layout.h"
 #include "ui/display/manager/display_manager.h"
@@ -77,7 +77,7 @@ class DISPLAY_MANAGER_EXPORT DisplayLayoutStore {
   bool forced_mirror_mode_for_tablet_ = false;
 
   // Display layout per list of devices.
-  std::map<DisplayIdList, std::unique_ptr<DisplayLayout>> layouts_;
+  base::flat_map<DisplayIdList, std::unique_ptr<DisplayLayout>> layouts_;
 };
 
 }  // namespace display
