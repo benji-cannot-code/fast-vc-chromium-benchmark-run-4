dd091173fec46de32de0654a3e37ff466b2aab4b
Irem Uguz <iremuguz@google.com>
2022-04-20 09:55:30 +0000

    support-tool: Show user's preferred downloads file on file selection
    Get user's preferred downloads directory from DownloadPrefs and use it
    as a default filepath on file selection dialog.
    
    Bug: b:200511640
    Change-Id: I0a52b99a25fabd65cb7b8b3da2bda70de8078be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585442
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#994110}

--- chrome/browser/ui/webui/support_tool/support_tool_ui.cc
+++ chrome/browser/ui/webui/support_tool/support_tool_ui.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/signin_ui_util.h"
@@ -299,7 +300,8 @@ std::string GetTimestampString(base::Time timestamp) {
                             tex.day_of_month, tex.hour, tex.minute);
 }
 
-base::FilePath GetDefaultFileToExport(const std::string& case_id,
+base::FilePath GetDefaultFileToExport(base::FilePath suggested_path,
+                                      const std::string& case_id,
                                       base::Time timestamp) {
   std::string timestamp_string = GetTimestampString(timestamp);
   std::string filename =
@@ -307,7 +309,7 @@ base::FilePath GetDefaultFileToExport(const std::string& case_id,
           ? base::StringPrintf("support_packet_%s", timestamp_string.c_str())
           : base::StringPrintf("support_packet_%s_%s", case_id.c_str(),
                                timestamp_string.c_str());
-  return base::FilePath::FromASCII(filename);
+  return suggested_path.AppendASCII(filename);
 }
 
 std::set<feedback::PIIType> GetSelectedPIIToKeep(
@@ -528,11 +530,16 @@ void SupportToolMessageHandler::HandleStartDataExport(
       this,
       std::make_unique<ChromeSelectFilePolicy>(web_ui()->GetWebContents()));
 
+  DownloadPrefs* download_prefs =
+      DownloadPrefs::FromBrowserContext(web_contents->GetBrowserContext());
+  base::FilePath suggested_path = download_prefs->SaveFilePath();
+
   select_file_dialog_->SelectFile(
       ui::SelectFileDialog::SELECT_SAVEAS_FILE,
       /*title=*/std::u16string(),
       /*default_path=*/
-      GetDefaultFileToExport(handler_->GetCaseID(), data_collection_time_),
+      GetDefaultFileToExport(suggested_path, handler_->GetCaseID(),
+                             data_collection_time_),
       /*file_types=*/nullptr,
       /*file_type_index=*/0,
       /*default_extension=*/base::FilePath::StringType(), owning_window,
