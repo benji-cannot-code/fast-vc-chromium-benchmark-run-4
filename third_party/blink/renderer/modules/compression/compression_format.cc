4bb2f50b23974b12b5540e0a1b7c68c80f5498c1
Yutaka Hirano <yhirano@chromium.org>
2022-04-20 03:43:43 +0000

    Add support for "deflate-raw" in compression stream apis
    Implement "deflate-raw" in
    https://wicg.github.io/compression/#supported-formats behind an
    experimental flag.
    
    Bug: 1271220
    Change-Id: I9dda74cc430ae31c51745f5df4e8d91b294167ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586945
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994035}

--- third_party/blink/renderer/modules/compression/compression_format.cc
+++ third_party/blink/renderer/modules/compression/compression_format.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/compression/compression_format.h"
 
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
@@ -16,6 +17,9 @@ CompressionFormat LookupCompressionFormat(const AtomicString& format,
     return CompressionFormat::kGzip;
   } else if (format == "deflate") {
     return CompressionFormat::kDeflate;
+  } else if (format == "deflate-raw" &&
+             RuntimeEnabledFeatures::DeflateRawCompressionFormatEnabled()) {
+    return CompressionFormat::kDeflateRaw;
   }
 
   exception_state.ThrowTypeError("Unsupported compression format: '" + format +
