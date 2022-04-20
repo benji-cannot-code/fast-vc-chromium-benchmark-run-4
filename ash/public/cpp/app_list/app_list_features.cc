8fade86b238118f3d388c0c23369eb0e96451a43
Rachel Wong <wrong@chromium.org>
2022-04-20 06:38:22 +0000

    [launcher] Add a flag to control Play Store search.
    Play store search has been disabled, but we would like to test it behind
    a flag.
    
    Bug: 1317492
    Change-Id: I96c4db2a3e51191bd4666b0439b8a8fe0293437a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594262
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994067}

--- ash/public/cpp/app_list/app_list_features.cc
+++ ash/public/cpp/app_list/app_list_features.cc
@@ -50,6 +50,8 @@ const base::Feature kFeedbackOnContinueSectionRemove{
     "FeedbackOnContinueSectionRemove", base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kCompactBubbleLauncher{"CompactBubbleLauncher",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kLauncherPlayStoreSearch{"LauncherPlayStoreSearch",
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsAppRankerEnabled() {
   return base::FeatureList::IsEnabled(kEnableAppRanker);
@@ -151,6 +153,10 @@ bool IsFeedbackOnContinueSectionRemoveEnabled() {
 
 bool IsCompactBubbleLauncherEnabled() {
   return base::FeatureList::IsEnabled(kCompactBubbleLauncher);
+}
+
+bool IsLauncherPlayStoreSearchEnabled() {
+  return base::FeatureList::IsEnabled(kLauncherPlayStoreSearch);
 }
 
 }  // namespace app_list_features
