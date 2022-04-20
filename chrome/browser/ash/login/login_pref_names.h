7ada597dd9d47345ea0f4852cdcc1f4981c43b3f
Eriko Kurimoto <elkurin@chromium.org>
2022-04-20 17:19:01 +0000

    Revert "[CloudReady][OOBE]: Show new terms to device users after update"
    This reverts commit a31450409a40b24d8edf887b89baf7cc9711680a.
    
    Reason for revert: Breaks tast tests on reven-vmtests which is
    critical for uprev. See crbug.com/1317006 for more context.
    
    Original change's description:
    > [CloudReady][OOBE]: Show new terms to device users after update
    >
    > Part [1/2]:
    > After updating from CloudReady to Flex we need to show new terms to old
    > users. This CL makes ConsolidatedConsentScreen to be shown for all users
    > and HWDataCollectionScreen to the owner. The second part of the chain
    > will cover cases when ConsolidatedConsent isn't enabled to show
    > EulaScreen instead.
    >
    > Bug: b:196201668
    > Change-Id: I49c397f2757093847a6d1a38cdac2095ec99bb53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450657
    > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    > Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#989915}
    
    Bug: b:196201668
    Change-Id: I6dd3fd77364c7fbe0874f5833d78ac475decfb4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596670
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Owners-Override: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994192}

--- chrome/browser/ash/login/login_pref_names.h
+++ chrome/browser/ash/login/login_pref_names.h
@@ -14,7 +14,6 @@ extern const char kOobeComplete[];
 extern const char kOobeOnboardingTime[];
 extern const char kOobeMarketingOptInScreenFinished[];
 extern const char kOobeMarketingOptInChoice[];
-extern const char kOobeRevenUpdatedToFlex[];
 extern const char kOobeScreenPending[];
 extern const char kGaiaOfflineSigninTimeLimitDays[];
 extern const char kGaiaLastOnlineSignInTime[];
@@ -29,7 +28,6 @@ extern const char kSamlPasswordSyncToken[];
 extern const char kActivityTimeAfterOnboarding[];
 extern const char kOobeGuestMetricsEnabled[];
 extern const char kOobeGuestAcceptedTos[];
-extern const char kRevenOobeConsolidatedConsentAccepted[];
 
 }  // namespace prefs
 }  // namespace ash
--- chrome/browser/ash/login/startup_utils.cc
+++ chrome/browser/ash/login/startup_utils.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/components/arc/arc_prefs.h"
-#include "ash/constants/ash_features.h"
-#include "ash/constants/ash_switches.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
@@ -104,9 +102,6 @@ void StartupUtils::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kDisableHIDDetectionScreenForTests, false);
   registry->RegisterBooleanPref(prefs::kOobeGuestMetricsEnabled, false);
   registry->RegisterBooleanPref(prefs::kOobeGuestAcceptedTos, false);
-  if (switches::IsRevenBranding()) {
-    registry->RegisterBooleanPref(prefs::kOobeRevenUpdatedToFlex, false);
-  }
 }
 
 // static
@@ -125,11 +120,6 @@ void StartupUtils::RegisterOobeProfilePrefs(PrefRegistrySimple* registry) {
   // initialized along with `kOobeOnboardingTime`.
   registry->RegisterBooleanPref(
       arc::prefs::kArcPlayStoreLaunchMetricCanBeRecorded, false);
-  if (switches::IsRevenBranding() &&
-      features::IsOobeConsolidatedConsentEnabled()) {
-    registry->RegisterBooleanPref(prefs::kRevenOobeConsolidatedConsentAccepted,
-                                  false);
-  }
   OnboardingUserActivityCounter::RegisterProfilePrefs(registry);
 }
 
