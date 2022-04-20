36fc27f6201d537a5dfd6d94c2bfee595e1d7c44
Tom Lukaszewicz <tluk@google.com>
2022-04-20 22:20:24 +0000

    Revert "Move promo tests that need pixel support to browser_tests"
    This reverts commit 094a1fc0c787526c4b4aeda83b5134e1502c431d.
    
    Reason for revert: Speculative revert for Mac11 Tests failures
    
    First failing build
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/8365/overview
    
    Original change's description:
    > Move promo tests that need pixel support to browser_tests
    >
    > Previously they were running in interactive_ui_tests, which is safer,
    > but prevented Skia Gold from running. Due to how these tests work,
    > however, they should probably not flake even as browser_tests.
    >
    > Change-Id: Ia90e49bbacbbb414a83495a965ba07431f460565
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587432
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#994292}
    
    Change-Id: If0c65160fc962ba47346dac16d473bbba54bbe36
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596736
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tom Lukaszewicz <tluk@google.com>
    Owners-Override: Tom Lukaszewicz <tluk@google.com>
    Cr-Commit-Position: refs/heads/main@{#994394}

--- chrome/browser/ui/views/user_education/feature_promo_dialog_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/feature_promo_dialog_interactive_uitest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/intent_helper/intent_picker_features.h"
 #include "chrome/browser/banners/test_app_banner_manager_desktop.h"
@@ -308,17 +307,8 @@ class FeaturePromoDialogIntentChipTest : public FeaturePromoDialogTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// TODO(crbug.com/1317479): Browser tests on LaCrOS currently do not run in Ash,
-// but this test requires Ash on ChromeOS. Therefore, disable it for now until
-// the bug can be resolved.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#define MAYBE_InvokeUi_IPH_IntentChip DISABLED_InvokeUi_IPH_IntentChip
-#else
-#define MAYBE_InvokeUi_IPH_IntentChip InvokeUi_IPH_IntentChip
-#endif
-
 IN_PROC_BROWSER_TEST_F(FeaturePromoDialogIntentChipTest,
-                       MAYBE_InvokeUi_IPH_IntentChip) {
+                       InvokeUi_IPH_IntentChip) {
   set_baseline("3564824");
 
   ASSERT_TRUE(embedded_test_server()->Start());
