8162b126687c1e0bd198babf4d7e66c8bd3605f5
Mike Pinkerton <pinkerton@google.com>
2022-04-20 17:29:03 +0000

    Add interop test for std::unique_ptr<T>.
    This test currently doesn't work because Swift interop does not yet
    support unique_ptr. There is a define in the header to flip when it
    does that enables the full test.
    
    This is primarily here to document the lack of support.
    
    Bug: none
    Change-Id: I979ab7d1a7b7b7c56536a1288094ee4c89c60fc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596833
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994210}

---
+++ ios/chrome/test/swift_interop/pointer/unique_ptr.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/test/swift_interop/pointer/unique_ptr.h"
+
+ValueReturner::ValueReturner() {}
+
+ValueReturner::~ValueReturner() {
+#if !SWIFT_INTEROP_UNIQUE_PTR_WORKS
+  delete object_;
+#endif
+}
+
+Value* ValueReturner::ObjectPointer() {
+#if SWIFT_INTEROP_UNIQUE_PTR_WORKS
+  if (!object_) {
+    object_ = std::make_unique<Value>(17);
+  }
+  return object_.get();
+#else
+  if (!object_) {
+    object_ = new Value(17);
+  }
+  return object_;
+#endif
+}
+
+Value::Value(int value) : value_(value) {}
+
+Value::~Value() {}
+
+bool Value::IsValid() {
+  return true;
+}
+
+int Value::GetValue() {
+  return value_;
+}
---
+++ ios/chrome/test/swift_interop/pointer/unique_ptr_xctest.swift
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Pointer
+import UIKit
+import XCTest
+
+class UniquePointerTest: XCTestCase {
+
+  func testReturnedObjectPointer() throws {
+    var returner = ValueReturner()
+    let object = returner.ObjectPointer()!
+    XCTAssertEqual(object.pointee.IsValid(), true, "")
+    XCTAssertEqual(object.pointee.GetValue(), 17, "")
+  }
+
+}
