c0264015900369aef5d17c1d109d5637c3bedb21
Olesia Marukhno <olesiamarukhno@google.com>
2022-04-20 08:18:50 +0000

    [reland][privacy sandbox settings] Handle notice dismiss in web UI
    Dismissing the notice was handled in the dialog view. The action
    didn't go through the handler pipeline and didn't trigger HaTS. It also
    recorded kNoticeClosedNoAction as well as kNoticeDismiss.
    
    Handle pressing ESC in the web UI instead to go through the same path as
    other user actions.
    
    CL was reverted because new interactive tests are flaky on ChromeOS and
    Wayland:
    https://chromium-review.googlesource.com/c/chromium/src/+/3587187
    Disable them on ChromeOS and Wayland for now.
    
    Bug: 1312042
    Change-Id: I05b96759284f845c29022fa31e4b51f5e82a125a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593256
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#994088}

---
+++ chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PRIVACY_SANDBOX_MOCK_PRIVACY_SANDBOX_SERVICE_H_
+#define CHROME_BROWSER_PRIVACY_SANDBOX_MOCK_PRIVACY_SANDBOX_SERVICE_H_
+
+#include <memory>
+
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace content {
+class BrowserContext;
+}
+
+class KeyedService;
+
+class MockPrivacySandboxService : public PrivacySandboxService {
+ public:
+  MockPrivacySandboxService();
+  ~MockPrivacySandboxService() override;
+
+  MOCK_METHOD(void,
+              DialogActionOccurred,
+              (PrivacySandboxService::DialogAction),
+              (override));
+};
+
+std::unique_ptr<KeyedService> BuildMockPrivacySandboxService(
+    content::BrowserContext* context);
+
+#endif  // CHROME_BROWSER_PRIVACY_SANDBOX_MOCK_PRIVACY_SANDBOX_SERVICE_H_
