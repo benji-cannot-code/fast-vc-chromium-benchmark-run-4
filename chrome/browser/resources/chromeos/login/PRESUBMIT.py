3c6c1cada6da9b83db4cf5badc94377e2f8037d3
Roman Sorokin <rsorokin@chromium.org>
2022-04-20 17:09:16 +0000

    [Oobe WebUI] Add a presubmit check for the semantic colors
    Fixed: 1318018
    Bug: 1318011, 1305150
    Change-Id: If0c699754ab737eb109804f072406c455929055f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596026
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#994175}

---
+++ chrome/browser/resources/chromeos/login/PRESUBMIT.py
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+USE_PYTHON3 = True
+
+
+def CheckChangeOnUpload(input_api, output_api):
+    return _CommonChecks(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+    return _CommonChecks(input_api, output_api)
+
+
+def _CommonChecks(input_api, output_api):
+    results = []
+    try:
+        import sys
+        old_sys_path = sys.path[:]
+
+        sys.path += [input_api.os_path.join(input_api.change.RepositoryRoot(),
+                                            'ui', 'chromeos')]
+        import styles.presubmit_support
+        results += styles.presubmit_support._CheckSemanticColors(
+            input_api, output_api)
+    finally:
+        sys.path = old_sys_path
+    return results
