dcd7a4b1db68102c91e69fdf76bdb195c571895f
ckitagawa <ckitagawa@chromium.org>
2022-04-20 19:22:55 +0000

    [Code Health] Migrate most of UndoTabModelTest to JUnit
    Re-write tests that don't require native or multiple activities in
    UndoTabModelTest as unit tests. This is mostly just copy/paste of
    existing tests replacing any UiThread helpers with direct calls or
    helpers and new initialization.
    
    The only major changes are to test setup;
    * Mock native code for TabModelJniBridge and objects not under test.
    * Use undo-not-supported TabModelImpl rather than incognito TabModel.
    * Skip ChromeTabCreator in favor of just adding tabs to the model.
    
    Bug: 1318046
    Change-Id: Iba01e0ecdf20dc239b3b8503641e62e064678973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595199
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994314}

--- chrome/test/android/javatests/src/org/chromium/chrome/browser/tab/MockTab.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/browser/tab/MockTab.java
@@ -103,6 +103,7 @@ public class MockTab extends TabImpl {
     @Override
     public void destroy() {
         mIsDestroyed = true;
+        mIsInitialized = false;
         for (TabObserver observer : mObservers) observer.onDestroyed(this);
         mObservers.clear();
     }
