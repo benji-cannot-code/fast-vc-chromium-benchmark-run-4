03b8819821a27429456bb6008d60dd30ce5dbe49
L. David Baron <dbaron@chromium.org>
2022-04-20 18:07:00 +0000

    Switch NamedItemCache keys from StringImpl* to AtomicString.
    This is an attempt at a speculative fix for a crash, on the theory that
    this cache does need ownership of its keys.
    
    Bug: 1316655
    Change-Id: I8253cb886383fda34b720c19439cd10fb9dad2c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594218
    Auto-Submit: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994265}

--- third_party/blink/renderer/core/html/html_collection.h
+++ third_party/blink/renderer/core/html/html_collection.h
@@ -124,14 +124,14 @@ class CORE_EXPORT HTMLCollection : public ScriptWrappable,
 
     const HeapVector<Member<Element>>* GetElementsById(
         const AtomicString& id) const {
-      auto it = id_cache_.find(id.Impl());
+      auto it = id_cache_.find(id);
       if (it == id_cache_.end())
         return nullptr;
       return it->value;
     }
     const HeapVector<Member<Element>>* GetElementsByName(
         const AtomicString& name) const {
-      auto it = name_cache_.find(name.Impl());
+      auto it = name_cache_.find(name);
       if (it == name_cache_.end())
         return nullptr;
       return it->value;
@@ -149,14 +149,13 @@ class CORE_EXPORT HTMLCollection : public ScriptWrappable,
     }
 
    private:
-    typedef HeapHashMap<StringImpl*, Member<HeapVector<Member<Element>>>>
+    typedef HeapHashMap<AtomicString, Member<HeapVector<Member<Element>>>>
         StringToElementsMap;
     static void AddElementToMap(StringToElementsMap& map,
                                 const AtomicString& key,
                                 Element* element) {
       HeapVector<Member<Element>>* vector =
-          map.insert(key.Impl(),
-                     MakeGarbageCollected<HeapVector<Member<Element>>>())
+          map.insert(key, MakeGarbageCollected<HeapVector<Member<Element>>>())
               .stored_value->value;
       vector->push_back(element);
     }
