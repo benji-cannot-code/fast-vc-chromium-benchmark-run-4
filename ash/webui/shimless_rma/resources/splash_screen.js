9cfa90e6bb7c134003d7689005fbd8d484a18fae
Danny Wang <wangdanny@google.com>
2022-04-20 19:46:53 +0000

    Shimless: Fix spinner position in loading page
    Screenshot:
    Before: http://screen/4JVTmCisnRwZypJ
    After: http://screen/WQW22GL8Nh9HRx6
    
    Spec: go/shimless-rma-spec
    
    Bug: 1198187
    Change-Id: I6345f44400dece53c2ae01ac63566e93525d5f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583022
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#994323}

--- ash/webui/shimless_rma/resources/splash_screen.js
+++ ash/webui/shimless_rma/resources/splash_screen.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 import './base_page.js';
 import './icons.js';
 import './shimless_rma_shared_css.js';
@@ -33,6 +34,16 @@ export class SplashScreen extends SplashScreenBase {
 
   static get template() {
     return html`{__html_template__}`;
+  }
+
+  /**
+   * Display the splash instructions.
+   * @returns {string}
+   * @protected
+   */
+  getSplashInstructionsText_() {
+    return this.i18n('shimlessSplashRemembering') +
+        this.i18n('shimlessSplashLoading');
   }
 }
 
