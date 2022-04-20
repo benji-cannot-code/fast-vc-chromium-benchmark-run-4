2c730d531261d11b2f76025cf21abe78f03b47dc
Ioana Pandele <ioanap@chromium.org>
2022-04-20 16:38:38 +0000

    [Passwords] Extract FakeLifecycleHelper so that it can be used in other tests
    The previous fake had an inverted condition in UnregisterObserver, which
    allowed tests to pass without the callback being reset. This is why
    the backend tests need to explicitly call UnregisterObserver now.
    
    Bug: 1289700
    Change-Id: Ibb617fb60d5d1d29cd79f159b9e9609c8ab9e790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596000
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994119}

---
+++ chrome/browser/password_manager/android/fake_password_manager_lifecycle_helper.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_FAKE_PASSWORD_MANAGER_LIFECYCLE_HELPER_H_
+#define CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_FAKE_PASSWORD_MANAGER_LIFECYCLE_HELPER_H_
+
+#include "base/callback.h"
+#include "chrome/browser/password_manager/android/password_manager_lifecycle_helper.h"
+
+namespace password_manager {
+
+class FakePasswordManagerLifecycleHelper
+    : public PasswordManagerLifecycleHelper {
+ public:
+  FakePasswordManagerLifecycleHelper();
+  ~FakePasswordManagerLifecycleHelper() override;
+
+  // PasswordManagerLifecycleHelper implementation
+  void RegisterObserver(base::RepeatingClosure foregrounding_callback) override;
+  void UnregisterObserver() override;
+
+  void OnForegroundSessionStart();
+
+ private:
+  base::RepeatingClosure foregrounding_callback_;
+};
+
+}  // namespace password_manager
+#endif  // CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_FAKE_PASSWORD_MANAGER_LIFECYCLE_HELPER_H_
