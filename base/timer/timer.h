44597c083d39135d99c46e2dc7aff36581e92cb8
Hong Xu <xuhong@chromium.org>
2022-04-20 20:10:22 +0000

    Add non-class member example for timer
    The whole document doesn't have an example on using timer for non-class
    member functions, and it's not obvious that `BindRepeating` is required.
    Add an example for that.
    
    Bug: None
    Change-Id: I3ef913f790b185d719cf93595a3ab1d70843b31c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3589672
    Commit-Queue: Hong Xu <xuhong@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994330}

--- base/timer/timer.h
+++ base/timer/timer.h
@@ -23,8 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   class MyClass {
 //    public:
 //     void StartDoingStuff() {
-//       timer_.Start(FROM_HERE, Seconds(1),
+//       timer_.Start(FROM_HERE, base::Seconds(1),
 //                    this, &MyClass::DoStuff);
+//       // Alternative form if the callback is not bound to `this` or
+//       // requires arguments:
+//       //    timer_.Start(FROM_HERE, base::Seconds(1),
+//       //                 base::BindRepeating(&MyFunction, 42));
 //     }
 //     void StopDoingStuff() {
 //       timer_.Stop();
