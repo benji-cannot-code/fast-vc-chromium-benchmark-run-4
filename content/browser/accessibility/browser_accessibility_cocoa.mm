b7a3574c5ecb0ddbbd77d47755d45c00264bd18c
Valerie Young <spectranaut@igalia.com>
2022-04-20 21:50:36 +0000

    Rename BrowserAccessibilityCocoa rows to accessibilityRows
    To match the new accessibility API in mac.
    Also add overridden function in AXPlatformNodeCocoa as a precursor to
    calling accessibilityRows from AXPlatformNodeCocoa.
    
    Bug: 1143454
    Change-Id: Iad3fbb74ad3288ff3248b5c72a176e5913b10904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530809
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#994378}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -622,7 +622,7 @@ bool content::IsNSRange(id value) {
       {NSAccessibilityRoleAttribute, @"role"},
       {NSAccessibilityRowHeaderUIElementsAttribute, @"rowHeaders"},
       {NSAccessibilityRowIndexRangeAttribute, @"rowIndexRange"},
-      {NSAccessibilityRowsAttribute, @"rows"},
+      {NSAccessibilityRowsAttribute, @"accessibilityRows"},
       // TODO(aboxhall): expose
       // NSAccessibilityServesAsTitleForUIElementsAttribute
       {NSAccessibilityStartTextMarkerAttribute, @"startTextMarker"},
@@ -1290,7 +1290,7 @@ bool content::IsNSRange(id value) {
   return nil;
 }
 
-- (NSArray*)rows {
+- (NSArray*)accessibilityRows {
   if (![self instanceActive])
     return nil;
   NSMutableArray* ret = [[[NSMutableArray alloc] init] autorelease];
@@ -1666,7 +1666,7 @@ bool content::IsNSRange(id value) {
 - (NSArray*)visibleRows {
   if (![self instanceActive])
     return nil;
-  return [self rows];
+  return [self accessibilityRows];
 }
 
 - (id)window {
