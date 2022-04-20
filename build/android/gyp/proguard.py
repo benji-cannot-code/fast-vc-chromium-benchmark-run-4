9d0d248d1953e828a6ac2fc9318a8956c4fa6d18
Andrew Grieve <agrieve@chromium.org>
2022-04-20 17:59:02 +0000

    Android: Remove obsolete proguard config filtering
    Change-Id: I109486927ebe8daa1ca274856cfd7f4c112c8443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579642
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994259}

--- build/android/gyp/proguard.py
+++ build/android/gyp/proguard.py
@@ -660,14 +660,8 @@ def main():
   build_utils.InitLogging('PROGUARD_DEBUG')
   options = _ParseOptions()
 
-  logging.debug('Preparing configs')
-  # Temporarily skip failing proguard config until r8 is rolled: b/206957373
-  proguard_configs = [
-      cfg for cfg in options.proguard_configs if not cfg.endswith(
-          'java_com_google_privacy_one_psl_annotation_proguard.pgcfg')
-  ]
-
   # ProGuard configs that are derived from flags.
+  logging.debug('Preparing configs')
   dynamic_config_data = _CreateDynamicConfig(options)
 
   logging.debug('Looking for embedded configs')
@@ -686,7 +680,7 @@ def main():
     _ExtractEmbeddedConfigs(jar_path, embedded_configs)
 
   # ProGuard configs that are derived from flags.
-  merged_configs = _CombineConfigs(proguard_configs,
+  merged_configs = _CombineConfigs(options.proguard_configs,
                                    dynamic_config_data,
                                    embedded_configs,
                                    exclude_generated=True)
@@ -716,8 +710,9 @@ def main():
   }
   dynamic_config_data += '\n' + _CombineConfigs([], None, tools_configs)
 
-  split_contexts_by_name = _OptimizeWithR8(options, proguard_configs, libraries,
-                                           dynamic_config_data, print_stdout)
+  split_contexts_by_name = _OptimizeWithR8(options, options.proguard_configs,
+                                           libraries, dynamic_config_data,
+                                           print_stdout)
 
   if not options.disable_checks:
     logging.debug('Running tracereferences')
