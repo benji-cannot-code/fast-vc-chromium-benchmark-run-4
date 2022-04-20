bb2405660172fde51cfd0df160f1b553dada7755
Peter Beverloo <peter@chromium.org>
2022-04-20 15:57:23 +0000

    Revert "Video encode accelerator based on NDK API for Android MediaCodec"
    This reverts commit 4e00c3c5b799ac0c21d93d1a3b2cffbfb2b272d1.
    
    Reason for revert: causing assertions on at least four Android bots: android-arm-tests, android-11-x86-rel, android-marshmallow-x86-rel-non-cq and android-pie-x86-rel.
    
    I   15.090s list_tests(9A231FFAZ003YF)    [ FATAL ] ../../third_party/googletest/src/googletest/include/gtest/internal/gtest-param-util.h:581:: Condition test_param_names.count(param_name) == 0 failed. Duplicate parameterized test name '0', in ../../media/gpu/android/ndk_video_encode_accelerator_tests.cc line 291
    
    Original change's description:
    > Video encode accelerator based on NDK API for Android MediaCodec
    >
    > It looks like in Android Q NDK API for Media Codecs became stable enough
    > to be used in Chromium. This allows us to avoid JNI calls back and
    > forth java<->C++ and use async API that eliminated 10ms delays for
    > each output like we have in AndroidVideoEncodeAccelerator.
    >
    > Bug: 1303175
    > Change-Id: Ic1bacc8ecdecceb9e37e33bc08b0a8b68a9a5193
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547798
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#993913}
    
    Bug: 1303175
    Change-Id: Id9c3af797c0c3d163030e5b4aa32d22bd3fe52d7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596182
    Auto-Submit: Peter Beverloo <peter@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Owners-Override: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994116}

--- media/gpu/gpu_video_encode_accelerator_factory.cc
+++ media/gpu/gpu_video_encode_accelerator_factory.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/cxx20_erase.h"
 #include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_preferences.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #if BUILDFLAG(IS_ANDROID)
 #include "media/gpu/android/android_video_encode_accelerator.h"
-#include "media/gpu/android/ndk_video_encode_accelerator.h"
 #endif
 #if BUILDFLAG(IS_MAC)
 #include "media/gpu/mac/vt_video_encode_accelerator_mac.h"
@@ -59,13 +57,8 @@ std::unique_ptr<VideoEncodeAccelerator> CreateVaapiVEA() {
 
 #if BUILDFLAG(IS_ANDROID)
 std::unique_ptr<VideoEncodeAccelerator> CreateAndroidVEA() {
-  if (NdkVideoEncodeAccelerator::IsSupported()) {
-    return base::WrapUnique<VideoEncodeAccelerator>(
-        new NdkVideoEncodeAccelerator(base::ThreadTaskRunnerHandle::Get()));
-  } else {
-    return base::WrapUnique<VideoEncodeAccelerator>(
-        new AndroidVideoEncodeAccelerator());
-  }
+  return base::WrapUnique<VideoEncodeAccelerator>(
+      new AndroidVideoEncodeAccelerator());
 }
 #endif
 
