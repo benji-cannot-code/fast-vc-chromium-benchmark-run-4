d5e921665fbf381d04440c0c19243db19169ede5
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-04-20 20:07:53 +0000

    Revert "[FLEDGE] Make AdAuctionServiceImplTest check that pipe is closed."
    This reverts commit b78fdc9a3c975f7513871f31f64730a4de8fc80b.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision b78fdc9a3c975f7513871f31f64730a4de8fc80b as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8816335869980689521
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fb78fdc9a3c975f7513871f31f64730a4de8fc80b&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+b78fdc9a3c975f7513871f31f64730a4de8fc80b
    
    Original change's description:
    > [FLEDGE] Make AdAuctionServiceImplTest check that pipe is closed.
    > 
    > Previously, tests where the pipe is closed just flushed the pipe.
    > Switching them to wait for pipe closure both addresses a TODO, making
    > the tests more thorough, and means that once join/leaves return
    > promises, test methods that join/leave and flush the pipe can be
    > updated to wait for the callback to be invoked (which won't happen
    > in cases where the pipe is closed).
    > 
    > Also make AdAuctionServiceImpl invoke receiver()->ReportBadMessage()
    > instead of mojo::ReportBadMessage(), to ensure the pipe is closed on
    > bad message, as it should be. The receiver() accessor was recently
    > added to DocumentService, which is why it wasn't in use previously.
    > 
    > Bug: 1315805
    > Change-Id: I49949237f0b2f515c6cc43d2bfc72780a86ac29d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588271
    > Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#994322}
    
    
    Change-Id: I29ce7432fbe4dc88d7c72fb0d10dd432037860df
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1315805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597699
    Owners-Override: Tom Lukaszewicz <tluk@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tom Lukaszewicz <tluk@google.com>
    Cr-Commit-Position: refs/heads/main@{#994327}

--- content/browser/interest_group/ad_auction_service_impl.cc
+++ content/browser/interest_group/ad_auction_service_impl.cc
@@ -211,13 +211,10 @@ void AdAuctionServiceImpl::JoinInterestGroup(
   // Policy, do nothing
   if (!render_frame_host()->IsFeatureEnabled(
           blink::mojom::PermissionsPolicyFeature::kJoinAdInterestGroup)) {
-    receiver()->ReportBadMessage("Unexpected request");
+    mojo::ReportBadMessage("Unexpected request");
     return;
   }
-
-  // If the interest group API is not allowed for this origin do nothing. This
-  // is not considered a bad message because the renderer cannot currently check
-  // for this state.
+  // If the interest group API is not allowed for this origin do nothing.
   if (!IsInterestGroupAPIAllowed(
           ContentBrowserClient::InterestGroupApiOperation::kJoin,
           group.owner)) {
@@ -244,21 +241,18 @@ void AdAuctionServiceImpl::LeaveInterestGroup(const url::Origin& owner,
   // Policy, do nothing
   if (!render_frame_host()->IsFeatureEnabled(
           blink::mojom::PermissionsPolicyFeature::kJoinAdInterestGroup)) {
-    receiver()->ReportBadMessage(
+    mojo::ReportBadMessage(
         "Unexpected request: JoinAdInterestGroup feature disabled");
     return;
   }
-
-  // If the interest group API is not allowed for this origin do nothing. This
-  // is not considered a bad message because the renderer cannot currently check
-  // for this state.
+  // If the interest group API is not allowed for this origin do nothing.
   if (!IsInterestGroupAPIAllowed(
           ContentBrowserClient::InterestGroupApiOperation::kLeave, origin())) {
     return;
   }
 
   if (origin().scheme() != url::kHttpsScheme) {
-    receiver()->ReportBadMessage(
+    mojo::ReportBadMessage(
         "Unexpected request: JoinAdInterestGroup only supported for secure "
         "origins");
     return;
@@ -281,14 +275,14 @@ void AdAuctionServiceImpl::LeaveInterestGroupForDocument() {
   }
 
   if (origin().scheme() != url::kHttpsScheme) {
-    receiver()->ReportBadMessage(
+    mojo::ReportBadMessage(
         "Unexpected request: JoinAdInterestGroupForDocument only supported for "
         "secure origins");
     return;
   }
 
   if (!render_frame_host()->IsNestedWithinFencedFrame()) {
-    receiver()->ReportBadMessage(
+    mojo::ReportBadMessage(
         "Unexpected request: JoinAdInterestGroupForDocument only supported "
         "within fenced frames");
     return;
@@ -325,7 +319,7 @@ void AdAuctionServiceImpl::UpdateAdInterestGroups() {
   // Policy, do nothing
   if (!render_frame_host()->IsFeatureEnabled(
           blink::mojom::PermissionsPolicyFeature::kJoinAdInterestGroup)) {
-    receiver()->ReportBadMessage("Unexpected request");
+    mojo::ReportBadMessage("Unexpected request");
     return;
   }
   // If the interest group API is not allowed for this origin do nothing.
@@ -343,7 +337,7 @@ void AdAuctionServiceImpl::RunAdAuction(blink::mojom::AuctionAdConfigPtr config,
   // Policy, do nothing
   if (!render_frame_host()->IsFeatureEnabled(
           blink::mojom::PermissionsPolicyFeature::kRunAdAuction)) {
-    receiver()->ReportBadMessage("Unexpected request");
+    mojo::ReportBadMessage("Unexpected request");
     return;
   }
   if (!IsAuctionValid(*config, /*is_top_level_auction=*/true)) {
