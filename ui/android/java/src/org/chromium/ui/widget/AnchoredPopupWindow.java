524cbb126fbba5eaae39b3e337e5cbc30adbc140
Scott Little <sclittle@chromium.org>
2022-04-20 19:42:46 +0000

    Make AnchoredPopupWindow update the popup layout when the view does.
    This CL makes AnchoredPopupWindow listen to layout changes of the list
    menu's content view, updating the layout of the popup in response.
    
    Bug: 1316392
    Change-Id: I541234181392b55df065fba52d08c436b32676ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594445
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994321}

--- ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
@@ -139,6 +139,9 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
         public void onDismiss() {
             if (mIgnoreDismissal) return;
 
+            mPopupWindow.getContentView().removeOnLayoutChangeListener(
+                    mContentViewOnLayoutChangeListener);
+
             mHandler.removeCallbacks(mDismissRunnable);
             for (OnDismissListener listener : mDismissListeners) listener.onDismiss();
 
@@ -187,6 +190,15 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
     private boolean mUpdateOrientationOnChange;
     private boolean mSmartAnchorWithMaxWidth;
 
+    private final View.OnLayoutChangeListener mContentViewOnLayoutChangeListener =
+            new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom,
+                        int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    if (mPopupWindow.isShowing()) updatePopupLayout();
+                }
+            };
+
     /**
      * Constructs an {@link AnchoredPopupWindow} instance.
      * @param context  Context to draw resources from.
@@ -223,6 +235,8 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
 
         updatePopupLayout();
         showPopupWindow();
+
+        mPopupWindow.getContentView().addOnLayoutChangeListener(mContentViewOnLayoutChangeListener);
     }
 
     /**
