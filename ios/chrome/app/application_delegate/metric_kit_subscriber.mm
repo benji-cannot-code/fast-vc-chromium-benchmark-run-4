8469ca547595cd65bb36b970749aafc7c8b36dbd
Olivier Robin <olivierrobin@google.com>
2022-04-20 16:45:04 +0000

    Add metrickit_type to crashpad uploaded reports
    The field is missing in the Crashpad implementation and can help
    interpret the reports.
    
    Bug: 1317644
    Change-Id: Ie368f19a615a45bc8aeed0dca8b0a95b362a566c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592992
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994131}

--- ios/chrome/app/application_delegate/metric_kit_subscriber.mm
+++ ios/chrome/app/application_delegate/metric_kit_subscriber.mm
@@ -176,7 +176,8 @@ void SendDiagnostic(MXDiagnostic* diagnostic, const std::string& type)
         "MetricKit", spanpayload,
         {{"ver",
           base::SysNSStringToUTF8(diagnostic.metaData.applicationBuildVersion)},
-         {"metrickit", "true"}});
+         {"metrickit", "true"},
+         {"metrickit_type", type}});
   }
 }
 
