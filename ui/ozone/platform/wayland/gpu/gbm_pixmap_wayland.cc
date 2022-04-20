f7fb5a79fc51fb42d54fdfa24ef241c7612a1a13
Maksim Sisov <msisov@igalia.com>
2022-04-20 16:53:59 +0000

    ozone/wayland: do not create wl_buffers unless the pixmap is scheduled
    ... as overlay.
    
    Running a MotionMark.Images test with some predefined settings shows
    that up to 200 unnecessary IPCs to create buffers on the host side
    are made, but those buffers are never used.
    
    Bug: 1314698, 1288516
    Change-Id: I3e4431d2857d966d495b45f54bd2a7616c116655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582205
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#994147}

--- ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
+++ ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
@@ -34,7 +34,7 @@ GbmPixmapWayland::GbmPixmapWayland(WaylandBufferManagerGpu* buffer_manager)
       buffer_id_(buffer_manager->AllocateBufferID()) {}
 
 GbmPixmapWayland::~GbmPixmapWayland() {
-  if (gbm_bo_ && widget_ != gfx::kNullAcceleratedWidget)
+  if (created_wl_buffer_)
     buffer_manager_->DestroyBuffer(buffer_id_);
 }
 
@@ -84,8 +84,6 @@ bool GbmPixmapWayland::InitializeBuffer(
            << " usage=" << gfx::BufferUsageToString(usage);
 
   visible_area_size_ = visible_area_size ? visible_area_size.value() : size;
-  if (widget_ != gfx::kNullAcceleratedWidget)
-    CreateDmabufBasedBuffer();
   return true;
 }
 
@@ -113,8 +111,6 @@ bool GbmPixmapWayland::InitializeBufferFromHandle(
   DVLOG(3) << "Created gbm bo. format= " << gfx::BufferFormatToString(format);
 
   visible_area_size_ = size;
-  if (widget_ != gfx::kNullAcceleratedWidget)
-    CreateDmabufBasedBuffer();
   return true;
 }
 
@@ -171,8 +167,8 @@ bool GbmPixmapWayland::ScheduleOverlayPlane(
     std::vector<gfx::GpuFence> release_fences) {
   DCHECK_NE(widget, gfx::kNullAcceleratedWidget);
 
-  if (widget_ == gfx::kNullAcceleratedWidget)
-    CreateDmabufBasedBuffer();
+  if (!created_wl_buffer_)
+    CreateDmabufBasedWlBuffer();
 
   widget_ = widget;
 
@@ -219,7 +215,7 @@ gfx::NativePixmapHandle GbmPixmapWayland::ExportHandle() {
   return handle;
 }
 
-void GbmPixmapWayland::CreateDmabufBasedBuffer() {
+void GbmPixmapWayland::CreateDmabufBasedWlBuffer() {
   uint64_t modifier = gbm_bo_->GetFormatModifier();
 
   std::vector<uint32_t> strides;
@@ -238,6 +234,10 @@ void GbmPixmapWayland::CreateDmabufBasedBuffer() {
     PLOG(FATAL) << "dup";
     return;
   }
+
+  // The wl_buffer must be destroyed once this pixmap is destroyed.
+  created_wl_buffer_ = true;
+
   // Asks Wayland to create a wl_buffer based on the |file| fd.
   buffer_manager_->CreateDmabufBasedBuffer(
       std::move(fd), visible_area_size_, strides, offsets, modifiers,
