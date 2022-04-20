60d6e63cdfdc3b18bd707ba25dfece75ce163c1b
Klaus Weidner <klausw@chromium.org>
2022-04-20 22:20:46 +0000

    WebXR: Fix canvas resizing for inline sessions
    The refactor in https://crrev.com/c/3384272 caused the viewport to stay
    at its initial size for inline sessions even when the canvas got
    resized, for example when rotating a phone between landscape and
    portrait. The views list was populated one time on session start and
    remained constant after that. This was OK in the pre-refactor code since
    the information in the view was constant, but not for the refactored
    code which included the non-constant viewport size in the view.
    
    To fix this, set a flag on canvas resize which triggers recreation of
    the view and its viewport.
    
    Fixed: 1318203
    Change-Id: I8b72afc6fc082a9956e35ddcf22e434eebbfd4cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597892
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994395}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -1947,6 +1947,8 @@ void XRSession::UpdateCanvasDimensions(Element* element) {
   if (render_state_->baseLayer()) {
     render_state_->baseLayer()->OnResize();
   }
+
+  canvas_was_resized_ = true;
 }
 
 void XRSession::OnButtonEvent(
@@ -2253,6 +2255,10 @@ const HeapVector<Member<XRViewData>>& XRSession::views() {
         }
       }
     } else {
+      if (canvas_was_resized_) {
+        views_.clear();
+        canvas_was_resized_ = false;
+      }
       if (views_.IsEmpty()) {
         views_.emplace_back(MakeGarbageCollected<XRViewData>(
             device::mojom::blink::XREye::kNone,
