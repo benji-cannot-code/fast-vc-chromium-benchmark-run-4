bd35f81be324f82f54a8bb41a8b717f86c03be8f
Renato Silva <rrsilva@google.com>
2022-04-20 22:11:33 +0000

    OOBE - Add Assistant screen to the debug overlay
    Add the Assistant screen to the debug overlay and some of its
    internal states.
    
    Bug: 1317781
    Change-Id: I5cd47248859831223786b9b3bd8bb9184a2faef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593049
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#994389}

--- chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/constants/ash_switches.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/command_line.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
@@ -654,7 +656,13 @@ void AssistantOptInFlowScreenHandler::HandleFlowFinished() {
 void AssistantOptInFlowScreenHandler::HandleFlowInitialized(
     const int flow_type) {
   auto* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
-  if (!prefs->GetBoolean(chromeos::assistant::prefs::kAssistantEnabled)) {
+  // Do not skip the flow if the OOBE debug overlay is present. Otherwise it is
+  // not possible to test the screen manually.
+  const bool debugger_enabled =
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kShowOobeDevOverlay);
+  if (!prefs->GetBoolean(chromeos::assistant::prefs::kAssistantEnabled) &&
+      !debugger_enabled) {
     HandleFlowFinished();
     return;
   }
