aed3ac1a912dce1c80f0493cc6f68e2da7c24121
Xinghui Lu <xinghuilu@chromium.org>
2022-04-20 21:05:22 +0000

    Do not include downloads with empty file name in the new download UX.
    Currently the download toolbar button and download bubble show downloads
    that have empty file names (http://screen/BhXFN6QBHXqKkME). Add checks
    under DownloadItemModel::ShouldShowInBubble() to filter out downloads
    with empty file name except those blocked by local policies.
    
    Bug: 1316381
    Change-Id: I01c2dc00f273d0c6797fe8d4de528fdf692b5fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588813
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994361}

--- chrome/browser/download/bubble/download_bubble_controller_unittest.cc
+++ chrome/browser/download/bubble/download_bubble_controller_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using testing::_;
 using testing::NiceMock;
 using testing::Return;
+using testing::ReturnRefOfCopy;
 using testing::SetArgPointee;
 
 namespace {
@@ -140,6 +141,14 @@ class DownloadBubbleUIControllerTest : public testing::Test {
     EXPECT_CALL(item(index), GetGuid()).WillRepeatedly(testing::ReturnRef(id));
     EXPECT_CALL(item(index), GetState()).WillRepeatedly(Return(state));
     EXPECT_CALL(item(index), GetStartTime()).WillRepeatedly(Return(start_time));
+    EXPECT_CALL(item(index), GetTargetFilePath())
+        .WillRepeatedly(
+            ReturnRefOfCopy(base::FilePath(FILE_PATH_LITERAL("foo"))));
+    EXPECT_CALL(item(index), GetLastReason())
+        .WillRepeatedly(Return(download::DOWNLOAD_INTERRUPT_REASON_NONE));
+    EXPECT_CALL(item(index), GetMixedContentStatus())
+        .WillRepeatedly(
+            Return(download::DownloadItem::MixedContentStatus::SAFE));
     int received_bytes =
         state == download::DownloadItem::IN_PROGRESS ? 50 : 100;
     EXPECT_CALL(item(index), GetReceivedBytes())
