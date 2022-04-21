e691b0609aaf2b455b9f84a44d6bcf44b4cb0604
David Black <dmblack@google.com>
2022-04-21 00:07:11 +0000

    personalization: Polish Google Photos integration zero states.
    It is possible, though unlikely, that Google Photos APIs will tell us
    that albums exist though no photos exist. Currently, that will result in
    UI jank as zero state and the Google Photos album tab may be shown at
    the same time. This CL fixes that.
    
    It is also possible that the user will select a Google Photos album that
    will be empty by the time the Google Photos API is invoked to fetch its
    contents. Currently that results in empty white space being shown. This
    CL fixes that by showing zero state instead.
    
    Bug: b:229805240
    Change-Id: I5034a974e0270a9da8fd3d441b1b18ebcd9b262f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595582
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#994446}

--- ash/webui/personalization_app/resources/common/utils.ts
+++ ash/webui/personalization_app/resources/common/utils.ts
@@ -11,6 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from '//resources/js/load_time_data.m.js';
 
 /**
+ * Checks if argument is an array with zero length.
+ */
+export function isEmptyArray(maybeArray: unknown): maybeArray is[] {
+  return Array.isArray(maybeArray) && maybeArray.length === 0;
+}
+
+/**
  * Checks if argument is an array with non-zero length.
  */
 export function isNonEmptyArray(maybeArray: unknown): maybeArray is unknown[] {
