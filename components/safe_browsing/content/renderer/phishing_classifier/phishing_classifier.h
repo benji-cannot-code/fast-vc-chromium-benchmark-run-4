52f671468daedfc78442e58513db3d42d744cc7c
Daniel Rubery <drubery@chromium.org>
2022-04-21 15:51:08 +0000

    Add visual features to CSPP pings
    We want to achieve feature parity with PhishGuard pings, which
    currently populate visual features for extended reporting users. So
    this CL extracts the code used in PhishGuard to a common location
    and uses in both PhishGuard and CSPP.
    
    Bug: 1316024
    Change-Id: Icc1f332963ea988531698a6e9c8935f6b3bb12ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584779
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994726}

--- components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.h
+++ components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.h
@@ -37,6 +37,7 @@ class RenderFrame;
 
 namespace safe_browsing {
 class ClientPhishingRequest;
+class VisualFeatures;
 class FeatureMap;
 class PhishingDOMFeatureExtractor;
 class PhishingTermFeatureExtractor;
@@ -125,6 +126,10 @@ class PhishingClassifier {
   // complete.
   void OnPlaybackDone(std::unique_ptr<SkBitmap> bitmap);
 
+  // Callback when visual features have been extracted from the screenshot.
+  void OnVisualFeaturesExtracted(
+      std::unique_ptr<VisualFeatures> visual_features);
+
   // Callback when visual feature extraction is complete.
   // If it was successful, computes a score and runs the DoneCallback.
   // If extraction was unsuccessful, runs the DoneCallback with a
@@ -158,6 +163,7 @@ class PhishingClassifier {
   std::unique_ptr<std::set<uint32_t>> shingle_hashes_;
   const std::u16string* page_text_;  // owned by the caller
   std::unique_ptr<SkBitmap> bitmap_;
+  std::unique_ptr<VisualFeatures> visual_features_;
   DoneCallback done_callback_;
 
   // Used to record the duration of visual feature scoring.
