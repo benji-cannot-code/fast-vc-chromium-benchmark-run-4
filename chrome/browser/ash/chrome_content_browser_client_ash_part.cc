060d7a25b40fe27b3f0855d4bcbfbb37bad250a7
Alex Yang <aycyang@chromium.org>
2022-04-21 16:06:45 +0000

    Migrate TabletModePageBehavior off of Ash-only tablet state checking
    This change is in preparation to port Ash's tablet-mode rendering code
    to Lacros. This code won't be usable by Lacros until it's moved to a
    Lacros-accessible filepath, since this code currently lives in
    directories that are only compiled into Ash Chrome.
    
    This change should have no effect on functionality for Ash Chrome.
    
    Bug: 1289495
    Change-Id: I0bdaa15724e6162f7249c779a0c246a7a928a808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591060
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994733}

--- chrome/browser/ash/chrome_content_browser_client_ash_part.cc
+++ chrome/browser/ash/chrome_content_browser_client_ash_part.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/chrome_content_browser_client_ash_part.h"
 
-#include "ash/public/cpp/tablet_mode.h"
 #include "base/feature_list.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/profiles/profile.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chromeos/ui/base/tablet_state.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_view_host.h"
@@ -76,7 +76,8 @@ void OverrideWebkitPrefsForTabletMode(
     content::WebContents* contents,
     blink::web_pref::WebPreferences* web_prefs) {
   // Enable some mobile-like behaviors when in tablet mode on Chrome OS.
-  if (!TabletMode::Get() || !TabletMode::Get()->InTabletMode())
+  if (!chromeos::TabletState::Get() ||
+      !chromeos::TabletState::Get()->InTabletMode())
     return;
 
   // Do this only for webcontents displayed in browsers and are not of hosted
--- chrome/browser/ui/ash/tablet_mode_page_behavior.cc
+++ chrome/browser/ui/ash/tablet_mode_page_behavior.cc
@@ -7,26 +7,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "ash/public/cpp/tablet_mode.h"
 #include "base/bind.h"
 #include "chrome/browser/chromeos/arc/arc_web_contents_data.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_tab_strip_tracker.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chromeos/ui/base/tablet_state.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/pointer/touch_ui_controller.h"
+#include "ui/display/screen.h"
 
 TabletModePageBehavior::TabletModePageBehavior() {
-  ash::TabletMode::Get()->AddObserver(this);
-  OnTabletModeToggled(ash::TabletMode::Get()->InTabletMode());
+  display::Screen::GetScreen()->AddObserver(this);
+  OnTabletModeToggled(chromeos::TabletState::Get()->InTabletMode());
 }
 
 TabletModePageBehavior::~TabletModePageBehavior() {
-  // The Ash Shell and TabletMode instance should have been destroyed by now.
-  DCHECK(!ash::TabletMode::Get());
+  display::Screen::GetScreen()->RemoveObserver(this);
 }
 
 void TabletModePageBehavior::OnTabletModeToggled(bool enabled) {
@@ -34,20 +34,23 @@ void TabletModePageBehavior::OnTabletModeToggled(bool enabled) {
   ui::TouchUiController::Get()->OnTabletModeToggled(enabled);
 }
 
-void TabletModePageBehavior::OnTabletModeStarting() {
-  OnTabletModeToggled(true);
-}
-
-void TabletModePageBehavior::OnTabletModeEnding() {
-  OnTabletModeToggled(false);
-}
-
-void TabletModePageBehavior::OnTabletControllerDestroyed() {
-  ash::TabletMode::Get()->RemoveObserver(this);
+void TabletModePageBehavior::OnDisplayTabletStateChanged(
+    display::TabletState state) {
+  switch (state) {
+    case display::TabletState::kInTabletMode:
+      OnTabletModeToggled(true);
+      return;
+    case display::TabletState::kInClamshellMode:
+      OnTabletModeToggled(false);
+      return;
+    case display::TabletState::kEnteringTabletMode:
+    case display::TabletState::kExitingTabletMode:
+      break;
+  }
 }
 
 bool TabletModePageBehavior::ShouldTrackBrowser(Browser* browser) {
-  return ash::TabletMode::Get()->InTabletMode();
+  return chromeos::TabletState::Get()->InTabletMode();
 }
 
 void TabletModePageBehavior::OnTabStripModelChanged(
