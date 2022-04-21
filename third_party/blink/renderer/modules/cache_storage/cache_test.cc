973e48e243dbbb8a6ffa81b080f2cd493558a23c
Daniel Cheng <dcheng@chromium.org>
2022-04-21 21:14:54 +0000

    Remove some usage of "generic" MojoUnion::New() helper in Blink.
    These methods have a generic-sounding name but actually create a union
    containing the first declared subtype. In general, this behavior is
    non-intuitive; in many cases, this behavior is also inefficient, since
    it allocates storage which may then be immediately deallocated.
    
    Bug: 1261169
    Change-Id: Ib124e8c2456b0f4b45bec9e44919005c5780f2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3598771
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994910}

--- third_party/blink/renderer/modules/cache_storage/cache_test.cc
+++ third_party/blink/renderer/modules/cache_storage/cache_test.cc
@@ -152,9 +152,7 @@ class ErrorCacheForTests : public mojom::blink::CacheStorageCache {
     last_error_web_cache_method_called_ = "dispatchMatch";
     CheckUrlIfProvided(fetch_api_request->url);
     CheckCacheQueryOptionsIfProvided(query_options);
-    mojom::blink::MatchResultPtr result = mojom::blink::MatchResult::New();
-    result->set_status(error_);
-    std::move(callback).Run(std::move(result));
+    std::move(callback).Run(mojom::blink::MatchResult::NewStatus(error_));
   }
   void MatchAll(mojom::blink::FetchAPIRequestPtr fetch_api_request,
                 mojom::blink::CacheQueryOptionsPtr query_options,
@@ -164,10 +162,7 @@ class ErrorCacheForTests : public mojom::blink::CacheStorageCache {
     if (fetch_api_request)
       CheckUrlIfProvided(fetch_api_request->url);
     CheckCacheQueryOptionsIfProvided(query_options);
-    mojom::blink::MatchAllResultPtr result =
-        mojom::blink::MatchAllResult::New();
-    result->set_status(error_);
-    std::move(callback).Run(std::move(result));
+    std::move(callback).Run(mojom::blink::MatchAllResult::NewStatus(error_));
   }
   void GetAllMatchedEntries(mojom::blink::FetchAPIRequestPtr request,
                             mojom::blink::CacheQueryOptionsPtr query_options,
@@ -185,8 +180,7 @@ class ErrorCacheForTests : public mojom::blink::CacheStorageCache {
       CheckCacheQueryOptionsIfProvided(query_options);
     }
     mojom::blink::CacheKeysResultPtr result =
-        mojom::blink::CacheKeysResult::New();
-    result->set_status(error_);
+        mojom::blink::CacheKeysResult::NewStatus(error_);
     std::move(callback).Run(std::move(result));
   }
   void Batch(Vector<mojom::blink::BatchOperationPtr> batch_operations,
@@ -587,8 +581,8 @@ class MatchTestCache : public NotImplementedErrorCache {
              bool in_range_fetch_event,
              int64_t trace_id,
              MatchCallback callback) override {
-    mojom::blink::MatchResultPtr result = mojom::blink::MatchResult::New();
-    result->set_response(std::move(response_));
+    mojom::blink::MatchResultPtr result =
+        mojom::blink::MatchResult::NewResponse(std::move(response_));
     std::move(callback).Run(std::move(result));
   }
 
@@ -634,8 +628,7 @@ class KeysTestCache : public NotImplementedErrorCache {
             int64_t trace_id,
             KeysCallback callback) override {
     mojom::blink::CacheKeysResultPtr result =
-        mojom::blink::CacheKeysResult::New();
-    result->set_keys(std::move(requests_));
+        mojom::blink::CacheKeysResult::NewKeys(std::move(requests_));
     std::move(callback).Run(std::move(result));
   }
 
@@ -691,8 +684,7 @@ class MatchAllAndBatchTestCache : public NotImplementedErrorCache {
                 int64_t trace_id,
                 MatchAllCallback callback) override {
     mojom::blink::MatchAllResultPtr result =
-        mojom::blink::MatchAllResult::New();
-    result->set_responses(std::move(responses_));
+        mojom::blink::MatchAllResult::NewResponses(std::move(responses_));
     std::move(callback).Run(std::move(result));
   }
   void Batch(Vector<mojom::blink::BatchOperationPtr> batch_operations,
