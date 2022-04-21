156607dd628e68b2c5c0510108385d9238013184
David Pennington <dpenning@chromium.org>
2022-04-21 16:36:22 +0000

    Move STGButton to its own file in preparation for code split of BBV 1/4
    We are going to split the functionality of BookmarkBarViews Tab Group
    implementation out into its own view for testability and code clarity.
    First we will start with what is already implemented in BBV, the
    buttons for the Saved Tab Groups.
    
    We also create a new AX node description for the saved tab group buttons.
    
    Change-Id: I1bece0fe44ab6f79a0827afa9071ce1bc2b75332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561503
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994748}

---
+++ chrome/browser/ui/views/bookmarks/bookmark_button_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_BUTTON_UTIL_H_
+#define CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_BUTTON_UTIL_H_
+
+#include <memory>
+
+namespace views {
+class LabelButtonBorder;
+}
+
+namespace bookmark_button_util {
+
+// Max width of the buttons in the bookmark bar.
+constexpr int kMaxButtonWidth = 150;
+
+std::unique_ptr<views::LabelButtonBorder> CreateBookmarkButtonBorder();
+
+}  // namespace bookmark_button_util
+
+#endif  // CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_BUTTON_UTIL_H_
