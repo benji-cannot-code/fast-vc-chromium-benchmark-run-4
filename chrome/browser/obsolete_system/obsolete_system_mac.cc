ec3e6c63d39f3ece4f83daa87f566493fbeba988
Avi Drissman <avi@chromium.org>
2022-04-21 14:54:03 +0000

    Make M103 the end of the line
    The infobar landed in M102, but now that it's M103, indicate that
    it is the end of the line.
    
    (Use the CHROME_VERSION_ constant so that next time, only one CL
    will be needed and `IsEndOfTheLine()` will work automatically.)
    
    Bug: 1313180
    Change-Id: If6b36ced5b5343e4ebe8a1ce7acd08986b63a379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597814
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tom Burgin <bur@chromium.org>
    Commit-Queue: Tom Burgin <bur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994695}

--- chrome/browser/obsolete_system/obsolete_system_mac.cc
+++ chrome/browser/obsolete_system/obsolete_system_mac.cc
@@ -5,10 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/obsolete_system/obsolete_system.h"
 
-#include "base/cpu.h"
 #include "base/system/sys_info.h"
-#include "build/build_config.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_version.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/chromium_strings.h"
@@ -60,7 +57,7 @@ std::u16string ObsoleteSystem::LocalizedObsoleteString() {
 
 // static
 bool ObsoleteSystem::IsEndOfTheLine() {
-  return false;
+  return CHROME_VERSION_MAJOR >= 103;
 }
 
 // static
