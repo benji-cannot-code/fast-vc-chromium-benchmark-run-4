1fa869a819334a0c291a624524fa6a81d7dc1ed5
Tommy Li <tommycli@chromium.org>
2022-04-21 18:13:00 +0000

    Revert "[memories] [persist-clusters] ContinuationParams"
    This reverts commit 2bc0bcd5079ffa1e6ab22cef487d968af78d8d54.
    
    Reason for revert: Breaks Journeys WebUI because the first result sent to the WebUI is marked as a continuation, which the WebUI doesn't expect.
    
    Original change's description:
    > [memories] [persist-clusters] ContinuationParams
    >
    > Previously, the various methods across `QueryClustersState`,
    > `HistoryClustersService`, and `GetAnnotatedVisitsToCluster` used
    > `base::Time continuation_end_time` to track where the previous query
    > ended and the next should pick up.
    >
    > This CL wraps the continuation time in a struct
    > `QueryClustersContinuationParams` and replaces the params and class
    > fields with the new struct.
    >
    > This has a few benefits:
    >
    > 1) The struct has `is_continuation` and `is_done` fields, removing the
    >    need to implicitly rely on `optional<>::has_value()` and
    >    `base::Time::is_null()`.
    >
    > 2) Similarly, replaces the `QueryClustersState::is_continuation_`
    >    & `GetAnnotatedVisitsToCluster::exhausted_history_` fields.
    >
    > 3) Simplifies the logic in `QueryClustersState::OnGotRawClusters()`.
    >
    > 4) The struct has additional fields `is_partial_day` &
    >    `unclustered_visits_done` that will be needed to track state when
    >    fetching both clustered and unclustered visits.
    >
    > Bug: 1184879, 1184875, 1171352
    > Change-Id: I83dafe5ae311a419b2312514f0ef0c96d7247fa9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594392
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Commit-Queue: manuk hovanesian <manukh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#994360}
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I981e4c743c6dfc22132777ae3e0be5790c057c7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600670
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994809}

--- components/history_clusters/core/query_clusters_state_unittest.cc
+++ components/history_clusters/core/query_clusters_state_unittest.cc
@@ -44,7 +44,7 @@ class QueryClustersStateTest : public testing::Test {
                 result = std::move(cluster_batch);
                 loop.Quit();
               }),
-          raw_clusters, {});
+          raw_clusters, base::Time());
       loop.Run();
     }
     return result;
