4e65c9cbf5f4eb9f4b29e9a92eab8c0e23a03e86
Ken Rockot <rockot@google.com>
2022-04-21 23:00:21 +0000

    ipcz: Single-process reference driver, basic tests
    Split out from the ipcz uber-CL: https://crrev.com/c/3570271
    
    Implements a fully functioning single-process reference driver, and adds
    some tests to exercise it.
    
    Note that this driver is ONLY for use in tests and is not intended
    for production use in Chromium.
    
    Bug: 1299283
    Change-Id: Ibebcb7554d5e56c10c8857128cfb547306a441c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591317
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#994961}

---
+++ third_party/ipcz/src/reference_drivers/object.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "reference_drivers/object.h"
+
+#include "ipcz/ipcz.h"
+
+namespace ipcz::reference_drivers {
+
+Object::Object(Type type) : type_(type) {}
+
+Object::~Object() = default;
+
+IpczResult Object::Close() {
+  return IPCZ_RESULT_OK;
+}
+
+}  // namespace ipcz::reference_drivers
