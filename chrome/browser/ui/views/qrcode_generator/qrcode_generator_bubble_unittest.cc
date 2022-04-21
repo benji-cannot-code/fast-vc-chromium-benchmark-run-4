505c78dbe0addc5232e311160df9951881e4862b
Peter Kasting <pkasting@chromium.org>
2022-04-21 21:35:12 +0000

    Fix transparent ring in downloaded QR code image.
    Bug: 1318417
    Change-Id: I8df6e3a665b5e6920ee2a2cbfe8e2521b2e1523e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601209
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994929}

--- chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble_unittest.cc
+++ chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble_unittest.cc
@@ -48,7 +48,8 @@ TEST_F(QRCodeGeneratorBubbleTest, GeneratedCodeHasQuietZone) {
 
   auto image = QRCodeGeneratorBubble::AddQRCodeQuietZone(
       base_image,
-      gfx::Size(kBaseSizeDip / kTileToDip, kBaseSizeDip / kTileToDip));
+      gfx::Size(kBaseSizeDip / kTileToDip, kBaseSizeDip / kTileToDip),
+      SK_ColorTRANSPARENT);
 
   EXPECT_EQ(base_image.width(), kBaseSizeDip);
   EXPECT_EQ(base_image.height(), kBaseSizeDip);
