ff17d979eccd01727c06c2e6427fc5445fbc1d09
Takashi Sakamoto <tasak@google.com>
2022-04-21 14:34:46 +0000

    partition_alloc: copy logging.{h,cc} to PA library.
    Since LogMessage allocates memory, PartitionAllocator (except test)
    doesn't depend on it. So most of the features (e.g. log prefix, log
    formatting, log destination and so on.) are removed.
    If user side needs the removed features, we expect they will use
    SetLogMessageHandler() and will output messages in their own way.
    
    Bug: 1151236
    Change-Id: I0962476eec9866a9373ad45549c9b01d78b7f332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592692
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#994685}

--- base/allocator/partition_allocator/partition_alloc_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_address_space.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/bits.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/cpu.h"
+#include "base/allocator/partition_allocator/partition_alloc_base/logging.h"
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/allocator/partition_allocator/partition_cookie.h"
@@ -35,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/reservation_offset_table.h"
 #include "base/allocator/partition_allocator/tagging.h"
 #include "base/callback.h"
-#include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
@@ -171,7 +171,7 @@ void AllocateRandomly(
 }
 
 void HandleOOM(size_t unused_size) {
-  LOG(FATAL) << "Out of memory";
+  PA_LOG(FATAL) << "Out of memory";
 }
 
 int g_dangling_raw_ptr_detected_count = 0;
@@ -397,9 +397,9 @@ class PartitionAllocTest : public testing::TestWithParam<bool> {
     // platform's OOM-killing behavior. OOM-killing makes this test flaky on
     // low-memory devices.
     if (!IsLargeMemoryDevice()) {
-      LOG(WARNING)
+      PA_LOG(WARNING)
           << "Skipping test on this device because of crbug.com/678782";
-      LOG(FATAL) << "Passed DoReturnNullTest";
+      PA_LOG(FATAL) << "Passed DoReturnNullTest";
     }
 
     ASSERT_TRUE(SetAddressSpaceLimit());
@@ -460,7 +460,7 @@ class PartitionAllocTest : public testing::TestWithParam<bool> {
     allocator.root()->Free(ptrs);
 
     EXPECT_TRUE(ClearAddressSpaceLimit());
-    LOG(FATAL) << "Passed DoReturnNullTest";
+    PA_LOG(FATAL) << "Passed DoReturnNullTest";
   }
 
   void RunRefCountReallocSubtest(size_t orig_size, size_t new_size);
