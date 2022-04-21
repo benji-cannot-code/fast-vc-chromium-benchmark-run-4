463ab96cb0f15a091fc25b25ef40cd133c909613
Colin Blundell <blundell@chromium.org>
2022-04-21 14:04:26 +0000

    [Printing] Remove NOTIFICATION_PRINT_JOB_EVENT
    After recent efforts, nothing in the codebase listens for this
    notification anymore.
    
    Bug: 796051
    Change-Id: I8188f7d6ccb2976b9d68bbadc1eef1ba3bf099cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578970
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994667}

--- chrome/browser/printing/print_job.cc
+++ chrome/browser/printing/print_job.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/printing/printer_query.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/notification_service.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/printed_document.h"
 
@@ -217,12 +216,6 @@ void PrintJob::StartPrinting() {
   auto details = base::MakeRefCounted<JobEventDetails>(JobEventDetails::NEW_DOC,
                                                        0, document_.get());
   print_job_manager_->OnPrintJobEvent(this, *details.get());
-
-  // TODO(crbug.com/796051): Remove.
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_PRINT_JOB_EVENT,
-      content::Source<PrintJob>(this),
-      content::Details<JobEventDetails>(details.get()));
 }
 
 void PrintJob::Stop() {
@@ -524,11 +517,6 @@ void PrintJob::OnFailed() {
                                                        0, nullptr);
   print_job_manager_->OnPrintJobEvent(this, *details.get());
 
-  // TODO(crbug.com/796051): Remove.
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_PRINT_JOB_EVENT, content::Source<PrintJob>(this),
-      content::Details<JobEventDetails>(details.get()));
-
   for (auto& observer : observers_) {
     observer.OnFailed();
   }
@@ -540,11 +528,6 @@ void PrintJob::OnDocDone(int job_id, PrintedDocument* document) {
   auto details = base::MakeRefCounted<JobEventDetails>(
       JobEventDetails::DOC_DONE, job_id, document);
   print_job_manager_->OnPrintJobEvent(this, *details.get());
-
-  // TODO(crbug.com/796051): Remove.
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_PRINT_JOB_EVENT, content::Source<PrintJob>(this),
-      content::Details<JobEventDetails>(details.get()));
 
   for (auto& observer : observers_) {
     observer.OnDocDone(job_id, document);
@@ -568,12 +551,6 @@ void PrintJob::OnDocumentDone() {
   auto details = base::MakeRefCounted<JobEventDetails>(
       JobEventDetails::JOB_DONE, 0, document_.get());
   print_job_manager_->OnPrintJobEvent(this, *details.get());
-
-  // TODO(crbug.com/796051): Remove.
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_PRINT_JOB_EVENT,
-      content::Source<PrintJob>(this),
-      content::Details<JobEventDetails>(details.get()));
 
   for (auto& observer : observers_) {
     observer.OnJobDone();
