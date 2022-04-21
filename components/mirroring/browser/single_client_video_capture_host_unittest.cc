ce0fa4daa3ed78e6f12d55687f59e756fc817eb0
Daniel Cheng <dcheng@chromium.org>
2022-04-21 12:00:55 +0000

    Convert SHOUTY_CASE Mojo union tags to kCamelCase in //components/mirroring.
    This CL was uploaded by git cl split.
    
    R=jophba@chromium.org
    
    Bug: 1148486
    Change-Id: Ic93fd0e520c6e09d1338e776d735f167bf419529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599224
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994622}

--- components/mirroring/browser/single_client_video_capture_host_unittest.cc
+++ components/mirroring/browser/single_client_video_capture_host_unittest.cc
@@ -161,7 +161,7 @@ class MockVideoCaptureObserver final
   MOCK_METHOD1(OnStateChangedCall, void(media::mojom::VideoCaptureState state));
   MOCK_METHOD1(OnVideoCaptureErrorCall, void(media::VideoCaptureError error));
   void OnStateChanged(media::mojom::VideoCaptureResultPtr result) override {
-    if (result->which() == media::mojom::VideoCaptureResult::Tag::STATE)
+    if (result->which() == media::mojom::VideoCaptureResult::Tag::kState)
       OnStateChangedCall(result->get_state());
     else
       OnVideoCaptureErrorCall(result->get_error_code());
