5e9f088628d55c43248260572a4ad95c9345066b
Nick Burris <nburris@chromium.org>
2022-04-21 19:06:20 +0000

    [SPC] Clean up APIv3 flag
    With the API v3 enabled by default in M102, the related code can be
    removed completely in M103.
    
    Bug: 1298505
    Change-Id: Ieab865a90c179be337f5214cf1d0afa1fefdd6c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594879
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994841}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -476,10 +476,6 @@ void WebRuntimeFeatures::EnableSecurePaymentConfirmation(bool enable) {
   RuntimeEnabledFeatures::SetSecurePaymentConfirmationEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnableSecurePaymentConfirmationAPIV3(bool enable) {
-  RuntimeEnabledFeatures::SetSecurePaymentConfirmationAPIV3Enabled(enable);
-}
-
 void WebRuntimeFeatures::EnableSecurePaymentConfirmationDebug(bool enable) {
   RuntimeEnabledFeatures::SetSecurePaymentConfirmationDebugEnabled(enable);
 }
