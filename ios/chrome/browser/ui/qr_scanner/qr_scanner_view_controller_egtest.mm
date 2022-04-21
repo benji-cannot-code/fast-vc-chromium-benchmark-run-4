e0c8c00c0dfb146afc7a4453cfa45a944808cc8f
Chris Lu <thegreenfrog@chromium.org>
2022-04-21 17:14:24 +0000

    Revert "[iOS] Disable QRCode on iPhone simulator"
    This reverts commit 3470c9243b3188b887e3287ffd9618a4e75d2c73.
    
    Reason for revert: header layout fixes have landed
    
    Original change's description:
    > [iOS] Disable QRCode on iPhone simulator
    >
    > The tests are failing on small iPhone.
    >
    > Bug: 1315524
    > Change-Id: Ie0b6e995ede567b17a058bdbbde63a2fc0c33804
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581870
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#991450}
    
    Bug: 1315524
    Change-Id: I431703bfc491b2ca298528e3da396a02fa7d6988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600258
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994765}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
@@ -479,14 +479,6 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // switched off and the correct button indicating that the torch is off is shown
 // when the scanner is opened again.
 - (void)testTorchButtonIsResetWhenQRScannerIsReopened {
-  // TODO(crbug.com/1315524): Re-enable this test as it is failing on small
-  // devices (iPhone SE).
-#if TARGET_IPHONE_SIMULATOR
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_SKIPPED(@"This test doesn't pass on iPhone SE.");
-  }
-#endif
-
   id cameraControllerMock =
       [QRScannerAppInterface cameraControllerMockWithAuthorizationStatus:
                                  AVAuthorizationStatusAuthorized];
@@ -574,14 +566,6 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Tests that a UIAlertController is presented by the QRScannerViewController if
 // the camera state changes after the QRScannerViewController is presented.
 - (void)testDialogIsDisplayedIfCameraStateChanges {
-  // TODO(crbug.com/1315524): Re-enable this test as it is failing on small
-  // devices (iPhone SE).
-#if TARGET_IPHONE_SIMULATOR
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_SKIPPED(@"This test doesn't pass on iPhone SE.");
-  }
-#endif
-
   id cameraControllerMock =
       [QRScannerAppInterface cameraControllerMockWithAuthorizationStatus:
                                  AVAuthorizationStatusAuthorized];
@@ -655,14 +639,6 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that an error dialog is dismissed if the camera becomes available.
 - (void)testDialogDismissedIfCameraBecomesAvailable {
-  // TODO(crbug.com/1315524): Re-enable this test as it is failing on small
-  // devices (iPhone SE).
-#if TARGET_IPHONE_SIMULATOR
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_SKIPPED(@"This test doesn't pass on iPhone SE.");
-  }
-#endif
-
   id cameraControllerMock =
       [QRScannerAppInterface cameraControllerMockWithAuthorizationStatus:
                                  AVAuthorizationStatusAuthorized];
