35771a14cb98948451857e2a2abc30ccc1200bbe
David Sanders <dsanders11@ucsbalum.com>
2022-04-21 14:12:30 +0000

    Do IWYU for //content/browser/site_instance_impl.h
    Bug: 242216
    Change-Id: Id06f02a520b05bb8ed659966e255f745b7201693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3598821
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#994675}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/isolation_context.h"
 #include "content/browser/process_lock.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
+#include "content/browser/site_instance_group.h"
 #include "content/browser/storage_partition_impl.h"
 #include "content/public/browser/browser_or_resource_context.h"
 #include "content/public/browser/content_browser_client.h"
