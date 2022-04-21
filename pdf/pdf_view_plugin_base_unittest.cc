be6e66aecabbf43be9e3b4d775965239fef90097
K. Moon <kmoon@chromium.org>
2022-04-21 22:04:55 +0000

    Delete PaintManager::Client::BindPaintGraphics()
    Eliminates BindPaintGraphics(), as this is now equivalent to a call to
    InvalidatePluginContainer(). CreatePaintGraphics() handles the
    invalidation immediately instead.
    
    This is a slight change in the timing, but should have no practical
    effect, since everything happens on the main thread anyway.
    
    Bug: 1317832
    Change-Id: Icceff742c761a1de4c4836732d1280afc57419f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600935
    Reviewed-by: Nigi <nigi@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994941}

--- pdf/pdf_view_plugin_base_unittest.cc
+++ pdf/pdf_view_plugin_base_unittest.cc
@@ -125,8 +125,6 @@ class FakePdfViewPluginBase : public PdfViewPluginBase {
               (const gfx::Size&),
               (override));
 
-  MOCK_METHOD(bool, BindPaintGraphics, (Graphics&), (override));
-
   MOCK_METHOD(std::unique_ptr<PDFiumEngine>,
               CreateEngine,
               (PDFEngine::Client*, PDFiumFormFiller::ScriptOption),
