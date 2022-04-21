de7508da92179a105db18d5b3a578af074e47ae2
Kent Tamura <tkent@chromium.org>
2022-04-21 03:34:03 +0000

    Deferred Shaping: Positioned objects disables Deferred Shaping
    Boxes prior to positioned objects in some conditions should not be
    deferred because such positioned objects can be in the viewport and
    they need precise positions.
    
    Implementation:
    When a positioned object is inserted into the LayoutObject tree, or
    the style of positioned object is updated while it's connected to the
    LayoutObject tree, check if it meets the conditions. If so, we disable
    Deferred Shaping in the whole frame.
    
    We assume top:-9999px as "invisible" because Wikipedia uses it for
    invisible accessibility text.
    
    Bug: 1259085
    Change-Id: I602a80b2f5b880ea1edfb92b6dddfb4142e26ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3534787
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994507}

--- third_party/blink/renderer/core/layout/layout_box_model_object.h
+++ third_party/blink/renderer/core/layout/layout_box_model_object.h
@@ -589,6 +589,7 @@ class CORE_EXPORT LayoutBoxModelObject : public LayoutObject {
   virtual void LocalQuadsForSelf(Vector<gfx::QuadF>& quads) const;
 
   void WillBeDestroyed() override;
+  void InsertedIntoTree() override;
 
   PhysicalOffset AdjustedPositionRelativeTo(const PhysicalOffset&,
                                             const Element*) const;
@@ -719,6 +720,7 @@ class CORE_EXPORT LayoutBoxModelObject : public LayoutObject {
         &LayoutBoxModelObject::BorderTop, &LayoutBoxModelObject::BorderRight,
         &LayoutBoxModelObject::BorderBottom, &LayoutBoxModelObject::BorderLeft);
   }
+  void DisallowDeferredShapingIfNegativePositioned() const;
 };
 
 template <>
