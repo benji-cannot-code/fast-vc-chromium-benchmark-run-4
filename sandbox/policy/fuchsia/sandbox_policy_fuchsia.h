1f35efe0185179a493f3057fc2a9ed866366ddd0
Fabrice de Gans <fdegans@chromium.org>
2022-04-21 19:45:29 +0000

    [fuchsia] Run sub-process FilteredServiceDirectory on its own thread
    This moves sub-processes FilteredServiceDirectory on their own
    separate IO threads. This fixes a number on browser tests that attempt
    to run synchronous calls to other processes (such as the network
    process) as part of their setup flow.
    
    Currently, the FilteredServiceDirectories for sub-processes are
    handled in the main IO thread for the browser process. As a result,
    when sub-processes attempt to use an external service, they hang
    forever since the IO thread is blocked in the browser process.
    
    This fixes the issue by handling the FilteredServiceDirectories for
    sub-processes on separate threads, freeing up the main IO thread in
    the browser process.
    
    Bug: 1301356, 1303038
    Change-Id: I087c38dfc66d5842b19558c54c8cdacc83b095e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506648
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994853}

--- sandbox/policy/fuchsia/sandbox_policy_fuchsia.h
+++ sandbox/policy/fuchsia/sandbox_policy_fuchsia.h
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/zx/job.h>
 
 #include "base/memory/ref_counted.h"
+#include "base/threading/sequence_bound.h"
 #include "sandbox/policy/export.h"
 #include "sandbox/policy/mojom/sandbox.mojom.h"
 
 namespace base {
 class FilteredServiceDirectory;
 struct LaunchOptions;
-class SequencedTaskRunner;
 }  // namespace base
 
 namespace sandbox {
@@ -42,9 +42,9 @@ class SANDBOX_POLICY_EXPORT SandboxPolicyFuchsia {
   sandbox::mojom::Sandbox type_;
 
   // Services directory used for the /svc namespace of the child process.
-  std::unique_ptr<base::FilteredServiceDirectory> service_directory_;
+  base::SequenceBound<base::FilteredServiceDirectory>
+      filtered_service_directory_;
   fidl::InterfaceHandle<::fuchsia::io::Directory> service_directory_client_;
-  scoped_refptr<base::SequencedTaskRunner> service_directory_task_runner_;
 
   // Job in which the child process is launched.
   zx::job job_;
