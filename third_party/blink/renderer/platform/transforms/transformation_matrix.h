201488afaadc10c55acd319d7be1e520233f3d35
Xianzhu Wang <wangxianzhu@chromium.org>
2022-04-21 14:11:31 +0000

    Avoid integer overflow in TransformationMatrix::IsInteger2DTranslation()
    Bug: 1316279
    Change-Id: I3bc9bb612b31f645d8b0b9e86f92765350a79c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596655
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994674}

--- third_party/blink/renderer/platform/transforms/transformation_matrix.h
+++ third_party/blink/renderer/platform/transforms/transformation_matrix.h
@@ -452,7 +452,7 @@ class PLATFORM_EXPORT TransformationMatrix {
 
   bool Is2dTransform() const;
 
-  bool IsIntegerTranslation() const;
+  bool IsInteger2DTranslation() const;
 
   bool IsInvalidMatrix() const {
     for (int i = 0; i < 4; i++) {
