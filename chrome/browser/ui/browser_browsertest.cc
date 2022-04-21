d55b317e3588a9d9ce1fef03d7ead64e6f134d16
Frank Liberato <liberato@chromium.org>
2022-04-21 21:53:12 +0000

    Switch document Picture in Picture (v2) to popup
    Since we are including more "normal" browser features, like
    permissions, in picture in picture windows, it makes sense to
    normalize pip windows to be their own browser window type.
    
    Previously, pip popup windows used a window that was created and
    managed by a DocumentPictureInPictureWindowController.  This work
    is now done by the browser, similar to how regular popup windows are
    created.  In the future, the business logic will still reside in a
    special pip controller (e.g., "close the pip window when the
    opener window navigates away"), but this CL turns all of this off
    to keep the CL more focused.
    
    Since pip v2 is currently turned off behind a flag, this doesn't
    break anything.
    
    As part of this, this CL turns off some of the pip tests that don't
    work anymore.  These will be re-enabled in a follow-up CL that
    fixes the pip window controller.
    
    Change-Id: I4aa3cbffc5e1cef91d1c99b0e7eaf0583732ed7e
    Bug: 1317445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576675
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994933}

--- chrome/browser/ui/browser_browsertest.cc
+++ chrome/browser/ui/browser_browsertest.cc
@@ -1524,7 +1524,10 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, StartMaximized) {
                                           browser()->profile(), true),
       Browser::CreateParams::CreateForDevTools(browser()->profile()),
       Browser::CreateParams::CreateForAppPopup("app_name", true, gfx::Rect(),
-                                               browser()->profile(), true)};
+                                               browser()->profile(), true),
+      Browser::CreateParams(Browser::TYPE_PICTURE_IN_PICTURE,
+                            browser()->profile(), true),
+  };
   for (size_t i = 0; i < std::size(params); ++i) {
     params[i].initial_show_state = ui::SHOW_STATE_MAXIMIZED;
     AddBlankTabAndShow(Browser::Create(params[i]));
@@ -1541,7 +1544,10 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, StartMinimized) {
                                           browser()->profile(), true),
       Browser::CreateParams::CreateForDevTools(browser()->profile()),
       Browser::CreateParams::CreateForAppPopup("app_name", true, gfx::Rect(),
-                                               browser()->profile(), true)};
+                                               browser()->profile(), true),
+      Browser::CreateParams(Browser::TYPE_PICTURE_IN_PICTURE,
+                            browser()->profile(), true),
+  };
   for (size_t i = 0; i < std::size(params); ++i) {
     params[i].initial_show_state = ui::SHOW_STATE_MINIMIZED;
     AddBlankTabAndShow(Browser::Create(params[i]));
@@ -2947,4 +2953,10 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_TRUE(WaitForLoadStop(wc));
   EXPECT_TRUE(back_observer.has_committed());
   EXPECT_FALSE(back_observer.was_same_document());
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserTest, CreatePictureInPicture) {
+  Browser* popup_browser = Browser::Create(Browser::CreateParams(
+      Browser::TYPE_PICTURE_IN_PICTURE, browser()->profile(), true));
+  ASSERT_TRUE(popup_browser->is_type_picture_in_picture());
 }
