84baaae98f8ca9fa0f16b702f3fef49a3dfdf7a1
David Sanders <dsanders11@ucsbalum.com>
2022-04-21 14:15:27 +0000

    Remove unused includes in autofill_profile.h
    Bug: 242216
    Change-Id: I44a792e6108714547f71d65f082cc59a63763355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599583
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#994677}

--- components/autofill/core/browser/data_model/autofill_profile.h
+++ components/autofill/core/browser/data_model/autofill_profile.h
@@ -11,12 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <iosfwd>
 #include <list>
-#include <map>
 #include <string>
 #include <vector>
 
-#include "base/compiler_specific.h"
-#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/data_model/address.h"
@@ -24,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/birthdate.h"
 #include "components/autofill/core/browser/data_model/contact_info.h"
 #include "components/autofill/core/browser/data_model/phone_number.h"
-#include "components/autofill/core/browser/proto/server.pb.h"
 
 namespace autofill {
 
