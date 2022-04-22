2697efbd90864261eceb31cc2205837ee6b3af60
Trevor Perrier <perrier@chromium.org>
2022-04-22 22:23:52 +0000

    [AppLanguagePrompt] Add flag to not show prompt if ULP match
    This CL adds a flag that does not show the AppLanguagePrompt if the
    Chrome UI base language matches they top ULP base language.  The
    AppLanguagePrompt must be enabled for this flag to take effect.
    
    If the UI language and top ULP language do not match or there are no
    ULP languages then the previous behavior is used to determine if the
    prompt should be shown.
    
    This CL introduces the Language Bridge native code to pass the ULP
    languages back to Java from the ULPLanguageModel. This required moving
    JavaLanguageBridgeGetULPLanguagesWrapper from
    language_model_manager_factory.cc to language_bridged.cc so the JNI file
    was only imported in once location.
    
    Bug: 1315618
    Change-Id: Ie2d29e8daa8b193fd9b5bfb572d61184ca79ad8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600929
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995390}

---
+++ chrome/browser/language/android/language_bridge.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_LANGUAGE_ANDROID_LANGUAGE_BRIDGE_H_
+#define CHROME_BROWSER_LANGUAGE_ANDROID_LANGUAGE_BRIDGE_H_
+
+#include <string>
+#include <vector>
+
+namespace language {
+class LanguageBridge {
+ public:
+  // Makes a blocking call to get ULP languages for |account_name|
+  static std::vector<std::string> GetULPLanguages(std::string account_name);
+};
+
+}  // namespace language
+
+#endif  // CHROME_BROWSER_LANGUAGE_ANDROID_LANGUAGE_BRIDGE_H_
