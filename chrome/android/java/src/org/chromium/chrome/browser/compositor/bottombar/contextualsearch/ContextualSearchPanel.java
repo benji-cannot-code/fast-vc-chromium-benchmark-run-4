d8a77133cb90ec0d5845746c8f8bbf9dcb969c86
Donn Denman <donnd@chromium.org>
2022-04-22 18:30:09 +0000

    Fix clicking on the scrim when an RS is active.
    The CS Panel was destroying the RS controls when it was told to close.
    This means that destruction was happening at the time the close was
    started rather than when the close was complete. The fix is to
    simply remove these premature destroy calls. These destroy calls
    are already being made at the correct time in the CS Panel method
    destroyComponents which is called by the OverlayPanel when the
    close completes.
    
    BUG=1318546
    
    Change-Id: I95ab5cf54a1c5aec07c44f92ef355dfb2a0b1f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600947
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995276}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
@@ -630,8 +630,6 @@ public class ContextualSearchPanel extends OverlayPanel implements ContextualSea
             ContextualSearchUma.logTabPromotion(activeRelatedSearches.isShowingRelatedSearchSerp());
             mIsRelatedSearchesInContentSerp = false;
         }
-        destroyInBarRelatedSearchesControl();
-        destroyInContentRelatedSearchesControl();
     }
 
     @Override
