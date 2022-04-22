f5e1b4eb8db5d54fee62db31fa55f46a76f59c5d
Arthur Hemery <ahemery@chromium.org>
2022-04-22 15:33:32 +0000

    Base patch for COOP WPTs refactoring.
    COOP WPTs have been written in a rigid way using postMessage which makes
    things complicated to modify given that opener cuts and multiple origins
    are involved. Since, new possibilities have made cross-origin
    communications easier and we leverage the dispatcher here to
    considerably increase readability.
    
    This is the first patch in a series of refactors that should eventually
    get rid of postMessage usage and coop_tests, url_tests, etc. in
    common.js
    
    BUG=1318373
    
    Change-Id: I516795b90224ac9390b099f855879d6d538e28a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593078
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995203}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/resources/common.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/resources/common.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// To use the functions below, be sure to include the following files in your
+// test:
+// - "/common/get-host-info.sub.js" to get the different origin values.
+
 const SAME_ORIGIN = {origin: get_host_info().HTTPS_ORIGIN, name: "SAME_ORIGIN"};
 const SAME_SITE = {origin: get_host_info().HTTPS_REMOTE_ORIGIN, name: "SAME_SITE"};
 const CROSS_ORIGIN = {origin: get_host_info().HTTPS_NOTSAMESITE_ORIGIN, name: "CROSS_ORIGIN"}
