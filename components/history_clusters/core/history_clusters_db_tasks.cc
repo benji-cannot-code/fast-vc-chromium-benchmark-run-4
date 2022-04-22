11c859300b3208023662a6f971cafab4296e4e61
manukh <manukh@chromium.org>
2022-04-22 18:52:59 +0000

    [memories] [persist-clusters] Move JSON debug functions to file.
    Moves the 3 debugging functions from HistoryClustersService's anon
    namespace to a dedicated 'history_clusters_debug_jsons' file:
    - GetDebugJSONForVisits()
    - GetDebugJSONForClusters()
    - GetDebugJSONForKeywordSet()
    
    This is in preparation for the following CL which will move some of the
    callback code that makes use of these functions from
    HistoryClustersService.
    
    Also debug prints `url_cache` too; previously only `cache`, i.e. the
    keywords cache, was printed.
    
    Also cleans some unused includes.
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I2a2bbc2280557507ac49fad80f35bbf20d1f666c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3598130
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995288}

--- components/history_clusters/core/history_clusters_db_tasks.cc
+++ components/history_clusters/core/history_clusters_db_tasks.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_database.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/config.h"
-#include "components/history_clusters/core/features.h"
 
 namespace history_clusters {
 
