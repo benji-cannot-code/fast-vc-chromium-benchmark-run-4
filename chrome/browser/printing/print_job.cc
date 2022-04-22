3c374e8fd8d10727df1ab24715548b17b51b02c9
Colin Blundell <blundell@chromium.org>
2022-04-22 09:06:30 +0000

    [Printing] Remove JobEventDetails
    This is a relic of when we were sending NOTIFICATION_PRINT_JOB_EVENT.
    Instead, have PrintJob inform PrintJobManager of each event via a
    distinct method for that event.
    
    This CL also cleans up years-old commented-out code in
    print_job_unittest.cc that has gone stale in the meantime.
    
    Bug: 796051
    Change-Id: I52fd78c731698971ea114f2fca8014d9676ec000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587410
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995101}

--- chrome/browser/printing/print_job.cc
+++ chrome/browser/printing/print_job.cc
@@ -212,10 +212,7 @@ void PrintJob::StartPrinting() {
   // Set the flag right now.
   is_job_pending_ = true;
 
-  // Tell everyone!
-  auto details = base::MakeRefCounted<JobEventDetails>(JobEventDetails::NEW_DOC,
-                                                       0, document_.get());
-  print_job_manager_->OnPrintJobEvent(this, *details.get());
+  print_job_manager_->OnStarted(this);
 }
 
 void PrintJob::Stop() {
@@ -512,10 +509,7 @@ void PrintJob::OnFailed() {
 
   Stop();
 
-  // Make sure a `Cancel()` is broadcast.
-  auto details = base::MakeRefCounted<JobEventDetails>(JobEventDetails::FAILED,
-                                                       0, nullptr);
-  print_job_manager_->OnPrintJobEvent(this, *details.get());
+  print_job_manager_->OnFailed(this);
 
   for (auto& observer : observers_) {
     observer.OnFailed();
@@ -525,9 +519,7 @@ void PrintJob::OnFailed() {
 void PrintJob::OnDocDone(int job_id, PrintedDocument* document) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  auto details = base::MakeRefCounted<JobEventDetails>(
-      JobEventDetails::DOC_DONE, job_id, document);
-  print_job_manager_->OnPrintJobEvent(this, *details.get());
+  print_job_manager_->OnDocDone(this, document, job_id);
 
   for (auto& observer : observers_) {
     observer.OnDocDone(job_id, document);
@@ -548,9 +540,7 @@ void PrintJob::OnDocumentDone() {
   // Stop the worker thread.
   Stop();
 
-  auto details = base::MakeRefCounted<JobEventDetails>(
-      JobEventDetails::JOB_DONE, 0, document_.get());
-  print_job_manager_->OnPrintJobEvent(this, *details.get());
+  print_job_manager_->OnJobDone(this);
 
   for (auto& observer : observers_) {
     observer.OnJobDone();
@@ -618,15 +608,5 @@ void PrintJob::AddObserver(Observer& observer) {
 void PrintJob::RemoveObserver(Observer& observer) {
   observers_.RemoveObserver(&observer);
 }
-
-JobEventDetails::JobEventDetails(Type type,
-                                 int job_id,
-                                 PrintedDocument* document)
-    : document_(document), type_(type), job_id_(job_id) {}
-
-JobEventDetails::~JobEventDetails() {
-}
-
-PrintedDocument* JobEventDetails::document() const { return document_.get(); }
 
 }  // namespace printing
