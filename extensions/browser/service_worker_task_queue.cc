7711b780dada8c8b41ba4b299cd9ba110e9ca10f
Istiaque Ahmed <lazyboy@chromium.org>
2022-04-22 21:10:38 +0000

    [ESW] Add a temporary log to identify a crasher's extension id.
    This CL SYSLOGs extension id when the extension could not
    be found in ServiceWorkerTaskQueue::DidInitializeServiceWorkerContext.
    
    This is to verify the hypothesis that aforementioned "not found"
    extension was disabled by content verification.
    
    Bug: b/229745779
    Change-Id: I742cc094546bceca9157721792729e61edff1cdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601669
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995341}

--- extensions/browser/service_worker_task_queue.cc
+++ extensions/browser/service_worker_task_queue.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/syslog_logging.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -233,6 +234,11 @@ void ServiceWorkerTaskQueue::DidInitializeServiceWorkerContext(
   DCHECK(registry);
   const Extension* extension =
       registry->enabled_extensions().GetByID(extension_id);
+
+  // Temporary log to investigate b/229745779.
+  if (!extension)
+    SYSLOG(WARNING) << "Extension " << extension_id << " is not enabled.";
+
   DCHECK(extension);
 
   RendererStartupHelperFactory::GetForBrowserContext(browser_context_)
