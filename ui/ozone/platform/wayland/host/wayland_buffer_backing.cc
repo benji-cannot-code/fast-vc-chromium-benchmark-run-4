8caae36ef5fbcda12df77755d57cb939012a8b3e
Maksim Sisov <msisov@igalia.com>
2022-04-22 07:51:12 +0000

    ozone/wayland: can reuse the same buffer handle
    It's very inefficient to create new wl_buffers over and over
    again as Wayland compositor (like exo) may have to create
    internal backing as well and import handle from the fd passed
    to it. In case of delegated compositing, we may have >1k buffers
    created, which is very inefficient.
    
    Instead, we can reuse the same wl_buffer for each WaylandSurface.
    However, the client must use the explicit synchronization protocol
    in this case as wl_buffer.release becomes meaningless.
    
    The way it works is the following - the client requests a handle,
    the backing creates a new handle, checks if the explicit protocol
    is supported by the Wayland compositor, and then decides whether it
    must share the wl_buffer with a new handle or create a new one.
    
    The backing is also responsible for owning the wl_buffer if the
    explicit sync protocol is available. If it is not available, the
    handles that each own own wl_buffers are responsible for destroying
    the wl_buffers.
    
    Bug: 1314698, 1288516
    Change-Id: I61c66a7dcb2ad8b1a3eaf4a81cee06d1ebbd4726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582198
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#995072}

--- ui/ozone/platform/wayland/host/wayland_buffer_backing.cc
+++ ui/ozone/platform/wayland/host/wayland_buffer_backing.cc
@@ -27,6 +27,9 @@ bool WaylandBufferBacking::UseExplicitSyncRelease() const {
 
 WaylandBufferHandle* WaylandBufferBacking::EnsureBufferHandle(
     WaylandSurface* requestor) {
+  if (UseExplicitSyncRelease())
+    requestor = nullptr;
+
   auto& buffer_handle = buffer_handles_[requestor];
 
   if (buffer_handle)
@@ -52,6 +55,10 @@ WaylandBufferHandle* WaylandBufferBacking::EnsureBufferHandle(
 WaylandBufferHandle* WaylandBufferBacking::GetBufferHandle(
     WaylandSurface* requestor) {
   DCHECK(requestor);
+  if (UseExplicitSyncRelease()) {
+    requestor = nullptr;
+    DCHECK_LE(buffer_handles_.size(), 1u);
+  }
   return buffer_handles_[requestor].get();
 }
 
