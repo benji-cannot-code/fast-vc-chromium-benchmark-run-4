a155f8bec3d40b21839d2fa8080b8a0491a003ff
Tsuyoshi Horo <horo@chromium.org>
2022-04-22 04:43:41 +0000

    Introduce UMAs of Windows DNS config compatibility
    This is a follow up CL of https://crrev.com/c/3588188
    
    Bug: 171346, 1317948
    
    Change-Id: I121f05ba1af13b674620fa9e60175d7da898a76f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592036
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995040}

--- net/dns/dns_config_service_win.cc
+++ net/dns/dns_config_service_win.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/free_deleter.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -55,6 +56,30 @@ const wchar_t kDnscachePath[] =
     L"SYSTEM\\CurrentControlSet\\Services\\Dnscache\\Parameters";
 const wchar_t kPolicyPath[] =
     L"SOFTWARE\\Policies\\Microsoft\\Windows NT\\DNSClient";
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class DnsWindowsCompatibility {
+  kCompatible = 0,
+  kIncompatibleResolutionPolicy = 1,
+  kIncompatibleProxy = 1 << 1,
+  kIncompatibleVpn = 1 << 2,
+  kIncompatibleAdapterSpecificNameserver = 1 << 3,
+
+  KAllIncompatibleFlags = (1 << 4) - 1,
+  kMaxValue = KAllIncompatibleFlags
+};
+
+inline constexpr DnsWindowsCompatibility operator|(DnsWindowsCompatibility a,
+                                                   DnsWindowsCompatibility b) {
+  return static_cast<DnsWindowsCompatibility>(static_cast<int>(a) |
+                                              static_cast<int>(b));
+}
+
+inline DnsWindowsCompatibility& operator|=(DnsWindowsCompatibility& a,
+                                           DnsWindowsCompatibility b) {
+  return a = a | b;
+}
 
 // Wrapper for GetAdaptersAddresses to get unicast addresses.
 // Returns nullptr if failed.
@@ -307,6 +332,26 @@ absl::optional<std::vector<IPEndPoint>> GetNameServers(
   return nameservers;
 }
 
+bool CheckAndRecordCompatibility(bool have_name_resolution_policy,
+                                 bool have_proxy,
+                                 bool uses_vpn,
+                                 bool has_adapter_specific_nameservers) {
+  DnsWindowsCompatibility compatibility = DnsWindowsCompatibility::kCompatible;
+  if (have_name_resolution_policy)
+    compatibility |= DnsWindowsCompatibility::kIncompatibleResolutionPolicy;
+  if (have_proxy)
+    compatibility |= DnsWindowsCompatibility::kIncompatibleProxy;
+  if (uses_vpn)
+    compatibility |= DnsWindowsCompatibility::kIncompatibleVpn;
+  if (has_adapter_specific_nameservers) {
+    compatibility |=
+        DnsWindowsCompatibility::kIncompatibleAdapterSpecificNameserver;
+  }
+  base::UmaHistogramEnumeration("Net.DNS.DnsConfig.Windows.Compatibility",
+                                compatibility);
+  return compatibility == DnsWindowsCompatibility::kCompatible;
+}
+
 }  // namespace
 
 std::string ParseDomainASCII(base::WStringPiece widestr) {
@@ -431,8 +476,9 @@ absl::optional<DnsConfig> ConvertSettingsToDnsConfig(
     dns_config.use_local_ipv6 = true;
   }
 
-  if (settings.have_name_resolution_policy || settings.have_proxy || uses_vpn ||
-      has_adapter_specific_nameservers) {
+  if (!CheckAndRecordCompatibility(settings.have_name_resolution_policy,
+                                   settings.have_proxy, uses_vpn,
+                                   has_adapter_specific_nameservers)) {
     dns_config.unhandled_options = true;
   }
 
