c2a93ea331f1ef059a38948f93b3b8d9168a14b3
Adenilson Cavalcanti <cavalcantii@chromium.org>
2022-04-22 07:46:34 +0000

    [zlib] Re-sync with zlib 1.2.12, patch 11 of N
    Ported:
    - Add tables for crc32_combine(), to speed it up by a factor of 200
    - Add crc32_combine_gen() and crc32_combine_op() for fast combines
    - Speed up software CRC-32 computation by a factor of 1.5 to 3
    
    Bug: 1032721
    Change-Id: Ia6a1fd31e5d264813e2d6bb307d95123c855240c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584144
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995069}

--- third_party/zlib/chromeconf.h
+++ third_party/zlib/chromeconf.h
@@ -50,6 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define crc32 Cr_z_crc32
 #define crc32_combine Cr_z_crc32_combine
 #define crc32_combine64 Cr_z_crc32_combine64
+#define crc32_combine_gen64 Cr_z_crc32_combine_gen64
+#define crc32_combine_gen Cr_z_crc32_combine_gen
+#define crc32_combine_op Cr_z_crc32_combine_op
 #define crc32_z Cr_z_crc32_z
 #define deflate Cr_z_deflate
 #define deflateBound Cr_z_deflateBound
