d83a52349c5bc6a55bde779bbaf0148c17d48ea0
Kevin Lubick <kjlubick@google.com>
2022-04-22 21:08:28 +0000

    Fix include of SkCanvas
    Required to land https://skia-review.googlesource.com/c/skia/+/532257
    
    Bug: skia:13052
    Change-Id: I01923040c2ba86a77b3a222a6c0becf051678068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602239
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995337}

--- android_webview/browser/gfx/java_browser_view_renderer_helper.cc
+++ android_webview/browser/gfx/java_browser_view_renderer_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "third_party/skia/include/utils/SkCanvasStateUtils.h"
 
