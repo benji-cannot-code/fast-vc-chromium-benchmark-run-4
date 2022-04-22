e0314d7880bc41974122f61c5ac932fdf8439b39
Daseul Lee <dslee@chromium.org>
2022-04-22 17:21:48 +0000

    [Local Fonts] Handle unsupported platform (Android) and permission denied case by throwing an error.
    This change partially reverts crrev/c/3547299, as the new decision is to hide the API on Android instead of returning an empty set.
    
    Bug: 1296792
    Change-Id: I5ba666d40d2a6ccaf7d33d4420cec37daf8b0e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597990
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995236}

--- content/browser/font_access/font_access_manager_browsertest.cc
+++ content/browser/font_access/font_access_manager_browsertest.cc
@@ -127,8 +127,7 @@ IN_PROC_BROWSER_TEST_F(FontAccessManagerBrowserTest, EnumerationTest) {
     EXPECT_LT(0, result.ExtractInt())
         << "Enumeration should return at least one font on supported OS.";
   } else {
-    EXPECT_EQ(0, result.ExtractInt())
-        << "Enumeration should return no font on non-supported OS.";
+    EXPECT_TRUE(!result.error.empty());
   }
 }
 
@@ -145,8 +144,12 @@ IN_PROC_BROWSER_TEST_F(FontAccessManagerBrowserTest,
                                "  return fonts.length;"
                                "})()");
 
-  EXPECT_EQ(0, result.ExtractInt())
-      << "Enumeration should return no fonts for an invalid postscriptName.";
+  if (FontEnumerationDataSource::IsOsSupported()) {
+    EXPECT_EQ(0, result.ExtractInt())
+        << "Enumeration should return no fonts for an invalid postscriptName.";
+  } else {
+    EXPECT_TRUE(!result.error.empty());
+  }
 }
 
 #if BUILDFLAG(IS_WIN)
