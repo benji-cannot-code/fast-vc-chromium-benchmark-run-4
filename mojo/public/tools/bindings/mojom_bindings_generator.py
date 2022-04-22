b81d1d37174138e67243a0e23639d0591dd394c5
Alex Gough <ajgo@chromium.org>
2022-04-22 17:58:20 +0000

    Validate mojom attributes before generation
    This CL adds a mojo pre-generation check 'attributes' that ensures
    that any attributes comply with their expected usage in Chromium.
    
    This also fixes up a couple of places where attributes were used
    incorrectly. Some (like MinValue being used in `enum`) are too
    common to fix immediately so are allowed for the time being.
    
    Major things that are fixed:
    
    Enum defaults are indicated on the default value:
    ```
    [Default=kField]
    enum {
      kField,
    }
    enum {
     [Default]kField,
    }
    ```
    
    `MinVersion` does not make sense on container types, they have been
    replaced with a comment. Docs for `MinVersion` are also updated.
    
    ```
    [MinVersion=2]
    struct|module|enum etc.
    // Introduced in Version 2.
    struct|module|enum etc.
    ```
    
    `Extensible` removed from `struct` (structs can be extended already).
    
    Bug: 1230122,1234883
    Change-Id: Id35e9d7b07cf1d96ab8214f7ccc70222897d3df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595859
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995256}

--- mojo/public/tools/bindings/mojom_bindings_generator.py
+++ mojo/public/tools/bindings/mojom_bindings_generator.py
@@ -59,6 +59,7 @@ _BUILTIN_GENERATORS = {
 }
 
 _BUILTIN_CHECKS = {
+    "attributes": "mojom_attributes_check",
     "restrictions": "mojom_restrictions_check",
 }
 
@@ -316,7 +317,7 @@ def main():
                                "--checks",
                                dest="checks_string",
                                metavar="CHECKS",
-                               default="restrictions",
+                               default="attributes,restrictions",
                                help="comma-separated list of checks")
   generate_parser.add_argument(
       "--gen_dir", dest="gen_directories", action="append", metavar="directory",
