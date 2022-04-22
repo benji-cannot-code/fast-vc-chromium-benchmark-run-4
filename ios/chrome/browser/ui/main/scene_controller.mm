922dce37204f4b0a9df6c07d804f150c307b2bc4
Tina Wang <tinazwang@google.com>
2022-04-22 17:38:26 +0000

    [WebChannels] Start discover engine when launching Chrome
    Previously the discover service would start when
    init NewTabPageCoordinator. This change starts the
    discover service when start showing tab grid view in
    Foreground, because if WebChannels feature is enabled,
    the discover engine should be started when Chrome is
    showing any web content, regardless of whether it
    is a NTP.
    
    Bug: 1264872
    Change-Id: Ieef33354b4939128dbf4932e5dbab86fab69b27a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602042
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995244}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -52,6 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/crash_keys_helper.h"
 #include "ios/chrome/browser/crash_report/crash_report_helper.h"
 #import "ios/chrome/browser/crash_report/crash_restore_helper.h"
+#import "ios/chrome/browser/discover_feed/discover_feed_service.h"
+#import "ios/chrome/browser/discover_feed/discover_feed_service_factory.h"
 #import "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/geolocation/geolocation_logger.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
@@ -101,6 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/main/incognito_blocker_scene_agent.h"
 #import "ios/chrome/browser/ui/main/signin_policy_scene_agent.h"
 #import "ios/chrome/browser/ui/main/ui_blocker_scene_agent.h"
+#import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
 #import "ios/chrome/browser/ui/scoped_ui_blocker/scoped_ui_blocker.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_recent_tab_browser_agent.h"
@@ -713,6 +716,12 @@ bool IsSigninForcedByPolicy() {
     }
 
     [self handleShowStartSurfaceIfNecessary];
+
+    if (IsWebChannelsEnabled()) {
+      // Creating the DiscoverFeedService.
+      DiscoverFeedServiceFactory::GetForBrowserState(
+          self.mainInterface.browser->GetBrowserState());
+    }
   }
 
   [self recordWindowCreationForSceneState:self.sceneState];
