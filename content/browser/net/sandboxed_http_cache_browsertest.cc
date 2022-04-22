ced913efcb2cf22ed9a6a6aab408ba746a50a841
Yutaka Hirano <yhirano@chromium.org>
2022-04-22 02:50:33 +0000

    Revert "[content] Disable flaky test suite SandboxedHttpCacheBrowserTest."
    This reverts commit c4f23c7833a169d2f082d955a37275f26193f60c.
    
    Reason for revert: I'm investigating the flakiness, and we need to run the tests for investigation.
    
    Original change's description:
    > [content] Disable flaky test suite SandboxedHttpCacheBrowserTest.
    >
    > SandboxedHttpCacheBrowserTest is flaking across many suites and many
    > platforms. The suite is disabled until the issue can be investigated
    > and resolved.
    >
    > Bug: 1317799
    > Change-Id: Idda73e65828d8e2b2d34c4040bbef8b5bb606100
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601290
    > Owners-Override: Kevin Marshall <kmarshall@chromium.org>
    > Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    > Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#994940}
    
    Bug: 1317799
    Change-Id: I44845934ea92365f96b5c09a6acf9d62a936e790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601769
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995027}

--- content/browser/net/sandboxed_http_cache_browsertest.cc
+++ content/browser/net/sandboxed_http_cache_browsertest.cc
@@ -51,10 +51,9 @@ using network::mojom::SimpleCacheEntry;
 using FileEnumerationEntry =
     disk_cache::BackendFileOperations::FileEnumerationEntry;
 
-// TODO(crbug.com/1317799): Test is disabled due to flakiness.
-class DISABLED_SandboxedHttpCacheBrowserTest : public ContentBrowserTest {
+class SandboxedHttpCacheBrowserTest : public ContentBrowserTest {
  public:
-  DISABLED_SandboxedHttpCacheBrowserTest() {
+  SandboxedHttpCacheBrowserTest() {
     std::vector<base::Feature> enabled_features = {
       net::features::kSandboxHttpCache,
 #if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_FUCHSIA)
@@ -189,8 +188,7 @@ class DISABLED_SandboxedHttpCacheBrowserTest : public ContentBrowserTest {
       file_operations_factories_;
 };
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       OpeningFileIsProhibited) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, OpeningFileIsProhibited) {
   base::RunLoop run_loop;
 
   absl::optional<bool> result;
@@ -207,7 +205,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   EXPECT_EQ(result, absl::make_optional(false));
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest,
                        EnumerateFilesOnNonExistingDirectory) {
   base::ScopedAllowBlockingForTesting allow_blocking;
   base::RunLoop run_loop;
@@ -239,7 +237,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   EXPECT_TRUE(entries.empty());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, EnumerateFiles) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, EnumerateFiles) {
   base::ScopedAllowBlockingForTesting allow_blocking;
   base::RunLoop run_loop;
 
@@ -274,8 +272,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, EnumerateFiles) {
   EXPECT_EQ(entry.size, 13);
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       CreateSimpleCache) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, CreateSimpleCache) {
   base::RunLoop run_loop;
 
   network_service_test().set_disconnect_handler(run_loop.QuitClosure());
@@ -300,7 +297,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   ASSERT_TRUE(simple_cache.is_bound());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest,
                        CreateSimpleCacheOnParentDirectory) {
   base::RunLoop run_loop;
 
@@ -322,7 +319,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   IgnoreNetworkServiceCrashes();
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest,
                        CreateSimpleCacheWithParentDirectoryTraversal) {
   base::RunLoop run_loop;
 
@@ -346,7 +343,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   IgnoreNetworkServiceCrashes();
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, CreateEntry) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, CreateEntry) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
 
   ASSERT_TRUE(simple_cache.is_bound());
@@ -356,8 +353,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, CreateEntry) {
   ASSERT_TRUE(entry.is_bound());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       OpenNonExistingEntry) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, OpenNonExistingEntry) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
 
   ASSERT_TRUE(simple_cache.is_bound());
@@ -367,8 +363,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   ASSERT_FALSE(entry.is_bound());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       CreateAndOpenEntry) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, CreateAndOpenEntry) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
 
   ASSERT_TRUE(simple_cache.is_bound());
@@ -386,8 +381,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   ASSERT_TRUE(entry.is_bound());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       WriteAndReadData) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, WriteAndReadData) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
   const std::vector<uint8_t> kData = {'A', 'B', 'C', 'D', 'E'};
   const std::string kKey = "key";
@@ -440,7 +434,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   }
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest,
                        WriteTruncateAndReadData) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
   const std::vector<uint8_t> kData = {'A', 'B', 'C', 'D', 'E'};
@@ -508,8 +502,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   }
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       WriteAndReadSparseData) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, WriteAndReadSparseData) {
   const std::string kKey = "key";
   constexpr int kOffset = 1024;
   std::vector<uint8_t> original_data;
@@ -567,7 +560,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
   }
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, DoomEntry) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, DoomEntry) {
   const std::vector<uint8_t> kData = {'A', 'B', 'C'};
   const std::vector<uint8_t> kSparseData(1024, 'A');
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
@@ -628,8 +621,7 @@ IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest, DoomEntry) {
   ASSERT_FALSE(entry.is_bound());
 }
 
-IN_PROC_BROWSER_TEST_F(DISABLED_SandboxedHttpCacheBrowserTest,
-                       DoomEntryWithoutOpening) {
+IN_PROC_BROWSER_TEST_F(SandboxedHttpCacheBrowserTest, DoomEntryWithoutOpening) {
   mojo::Remote<SimpleCache> simple_cache = CreateSimpleCache();
 
   ASSERT_TRUE(simple_cache.is_bound());
