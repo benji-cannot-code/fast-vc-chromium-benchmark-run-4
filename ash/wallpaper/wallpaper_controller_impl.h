38c55ae976efb10286d6f8b9421af2e6976462a8
Sean Kau <skau@chromium.org>
2022-04-22 23:43:55 +0000

    Extract OnlineWallpaperVariants code
    Separate fetch of OnlineWallpaperVariants into a single function in a
    new class to clarify what is happening in WallpaperController. Adds two
    functions: FetchDailyWallpaper and FetchOnlineWallpaper. FetchDaily
    always returns a new daily wallpaper.  FetchOnline returns information
    appropriate to info.asset_id.
    
    Bug: 1298586
    Change-Id: I9e0d88181a972c2727adc7c5664d77dd745498fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577189
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995425}

--- ash/wallpaper/wallpaper_controller_impl.h
+++ ash/wallpaper/wallpaper_controller_impl.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/wallpaper/wallpaper_info.h"
 #include "ash/public/cpp/wallpaper/wallpaper_types.h"
 #include "ash/shell_observer.h"
+#include "ash/wallpaper/online_wallpaper_variant_info_fetcher.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_color_calculator_observer.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_resizer_observer.h"
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom-forward.h"
@@ -117,7 +118,9 @@ class ASH_EXPORT WallpaperControllerImpl
 
   // Prefer to use to obtain an new instance unless injecting non-production
   // members i.e. in tests.
-  explicit WallpaperControllerImpl(PrefService* local_state);
+  explicit WallpaperControllerImpl(
+      PrefService* local_state,
+      std::unique_ptr<OnlineWallpaperVariantInfoFetcher> fetcher);
 
   WallpaperControllerImpl(const WallpaperControllerImpl&) = delete;
   WallpaperControllerImpl& operator=(const WallpaperControllerImpl&) = delete;
@@ -456,6 +459,11 @@ class ASH_EXPORT WallpaperControllerImpl
       const OnlineWallpaperParams& params,
       const base::flat_map<std::string, base::FilePath>& url_to_file_path_map);
 
+  // Handler to receive Fetch*Wallpaper variants callbacks.
+  void OnWallpaperVariantsFetched(WallpaperType type,
+                                  SetWallpaperCallback callback,
+                                  absl::optional<OnlineWallpaperParams> params);
+
   // Used as the callback of decoding wallpapers of type
   // `WallpaperType::kOnline`. Saves the image to local file if `save_file` is
   // true, and shows the wallpaper immediately if `params.account_id` is the
@@ -689,20 +697,6 @@ class ASH_EXPORT WallpaperControllerImpl
   // If the user has a Google Photos wallpaper set.
   bool IsGooglePhotosWallpaperSet() const;
 
-  // Callback from the client providing a url to a wallpaper from the user
-  // specified collection when daily refresh is enabled. If |success| is
-  // false, fetching the |image| failed, and should be tried again soon.
-  void SetDailyWallpaper(const AccountId& account_id,
-                         const std::string& collection_id,
-                         WallpaperLayout layout,
-                         RefreshWallpaperCallback callback,
-                         bool success,
-                         const backdrop::Image& image);
-
-  // Called after attempting to download and set a daily refresh wallpaper.
-  // On failure retry again in a while.
-  void OnSetDailyWallpaper(RefreshWallpaperCallback callback, bool success);
-
   // Starts a wall clock timer, to update the wallpaper 24 hours since the last
   // wallpaper was set.
   void StartDailyRefreshTimer();
@@ -751,9 +745,6 @@ class ASH_EXPORT WallpaperControllerImpl
   void HandleDailyWallpaperInfoSyncedIn(const AccountId& account_id,
                                         const WallpaperInfo& info);
 
-  void HandleOnlineWallpaperInfoSyncedIn(const AccountId& account_id,
-                                         const WallpaperInfo& info);
-
   void HandleGooglePhotosWallpaperInfoSyncedIn(const AccountId& account_id,
                                                const WallpaperInfo& info);
 
@@ -762,15 +753,6 @@ class ASH_EXPORT WallpaperControllerImpl
   void HandleSettingOnlineWallpaperFromWallpaperInfo(
       const AccountId& account_id,
       const WallpaperInfo& info);
-
-  // Find the variants for the online wallpaper with given |params.asset_id| and
-  // |params.collection_id| and determine the right variant to set based on the
-  // system's color mode.
-  void FindAndSetOnlineWallpaperVariants(
-      const OnlineWallpaperParams& params,
-      base::OnceCallback<void(bool)> callback,
-      bool success,
-      const std::vector<backdrop::Image>& images);
 
   bool locked_ = false;
 
@@ -789,6 +771,9 @@ class ASH_EXPORT WallpaperControllerImpl
   // Manages the states of the other windows when the wallpaper app window is
   // active.
   std::unique_ptr<WallpaperWindowStateManager> window_state_manager_;
+
+  // Delegate to resolve online wallpaper variants.
+  std::unique_ptr<OnlineWallpaperVariantInfoFetcher> variant_info_fetcher_;
 
   // The prominent colors extracted from the current wallpaper.
   // kInvalidWallpaperColor is used by default or if extracting colors fails.
