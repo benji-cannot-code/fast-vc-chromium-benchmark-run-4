76efee7b09f295ba233eeef1864f861a6e8e152a
Colin Kincaid <ckincaid@chromium.org>
2022-04-22 20:59:55 +0000

    Replace SanitizedImageSource cookie option with auth token option.
    SanitizedImageSource's existing option to send data requests with
    cookies is not robust to the possibility of users who delete their
    cookies or disable cookies entirely. This CL replaces the cookie
    option with a way to specify that a data request should first fetch an
    auth token from Google Photos and then send that in the request headers.
    
    Fixed: b/227338288
    Change-Id: I09d4794c8ef03a419c4b25ffeadd357eb7b9c1c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571483
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995323}

--- ui/webui/resources/cr_elements/cr_auto_img/cr_auto_img.ts
+++ ui/webui/resources/cr_elements/cr_auto_img/cr_auto_img.ts
@@ -16,10 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png">
  *
- *      If your image needs to be fetched using cookies, you can use the
- *      with-cookies attribute as follows:
+ *      If your image URL points to Google Photos storage, meaning it needs an
+ *      auth token to be downloaded, you can use the is-google-photos attribute
+ *      as follows:
  *
- *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png" with-cookies>
+ *      <img is="cr-auto-img" auto-src="https://foo.com/bar.png"
+ *          is-google-photos>
  *
  *      If you want the image to reset to an empty state when auto-src changes
  *      and the new image is still loading, set the clear-src attribute:
@@ -34,22 +36,22 @@ const AUTO_SRC: string = 'auto-src';
 
 const CLEAR_SRC: string = 'clear-src';
 
-const WITH_COOKIES: string = 'with-cookies';
+const IS_GOOGLE_PHOTOS: string = 'is-google-photos';
 
 export class CrAutoImgElement extends HTMLImageElement {
   static get observedAttributes() {
-    return [AUTO_SRC, WITH_COOKIES];
+    return [AUTO_SRC, IS_GOOGLE_PHOTOS];
   }
 
   attributeChangedCallback(
       name: string, oldValue: string|null, newValue: string|null) {
-    if (name !== AUTO_SRC && name !== WITH_COOKIES) {
+    if (name !== AUTO_SRC && name !== IS_GOOGLE_PHOTOS) {
       return;
     }
 
-    // Changes to |WITH_COOKIES| are only interesting when the attribute is
+    // Changes to |IS_GOOGLE_PHOTOS| are only interesting when the attribute is
     // being added or removed.
-    if (name === WITH_COOKIES &&
+    if (name === IS_GOOGLE_PHOTOS &&
         ((oldValue === null) === (newValue === null))) {
       return;
     }
@@ -73,9 +75,9 @@ export class CrAutoImgElement extends HTMLImageElement {
       this.removeAttribute('src');
     } else if (url.protocol === 'data:' || url.protocol === 'chrome:') {
       this.src = url.href;
-    } else if (this.hasAttribute(WITH_COOKIES)) {
-      this.src =
-          `chrome://image?url=${encodeURIComponent(url.href)}&withCookies=true`;
+    } else if (this.hasAttribute(IS_GOOGLE_PHOTOS)) {
+      this.src = `chrome://image?url=${
+          encodeURIComponent(url.href)}&isGooglePhotos=true`;
     } else {
       this.src = 'chrome://image?' + url.href;
     }
@@ -97,16 +99,16 @@ export class CrAutoImgElement extends HTMLImageElement {
     return this.getAttribute(CLEAR_SRC)!;
   }
 
-  set withCookies(enabled: boolean) {
+  set isGooglePhotos(enabled: boolean) {
     if (enabled) {
-      this.setAttribute(WITH_COOKIES, '');
+      this.setAttribute(IS_GOOGLE_PHOTOS, '');
     } else {
-      this.removeAttribute(WITH_COOKIES);
+      this.removeAttribute(IS_GOOGLE_PHOTOS);
     }
   }
 
-  get withCookies(): boolean {
-    return this.hasAttribute(WITH_COOKIES);
+  get isGooglePhotos(): boolean {
+    return this.hasAttribute(IS_GOOGLE_PHOTOS);
   }
 }
 
