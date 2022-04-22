8ff090ff8e4a1b024620ea6351d64fb771548e77
Solomon Kinard <solomonkinard@chromium.org>
2022-04-22 22:09:00 +0000

    Extensions: Favicon: Fetch image
    Get a cached web favicon using an extension url.
    Example: chrome-extension://<id>/_favicon/?<params>
    
    Legend:
    <id>: Extension ID.
    <params>: page_url=...
    
    Description:
    Return a favicon image if its image has already been cached during a
    previous visit to the site. Error otherwise. <params> used here are
    derived from chrome://favicon2/<params>.
    
    Layers:
    ExtensionURLLoaderFactory -> ExtensionURLLoader -> GetFavicon ->
    WriteData
    
    Boundaries:
    //extensions/browser gets the favicon from //chrome/browser
    
    Change-Id: Ifcd23a0c9ff34bd7526ce1cf4990cdd617630547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3544198
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995383}

--- extensions/browser/extensions_browser_client.cc
+++ extensions/browser/extensions_browser_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/memory/ref_counted_memory.h"
 #include "components/update_client/update_client.h"
 #include "extensions/browser/extension_api_frame_id_map.h"
 #include "extensions/browser/extension_error.h"
@@ -158,5 +159,13 @@ bool ExtensionsBrowserClient::IsUsbDeviceAllowedByPolicy(
     int product_id) const {
   return false;
 }
+
+void ExtensionsBrowserClient::GetFavicon(
+    content::BrowserContext* browser_context,
+    const Extension* extension,
+    const GURL& url,
+    base::CancelableTaskTracker* tracker,
+    base::OnceCallback<void(scoped_refptr<base::RefCountedMemory> bitmap_data)>
+        callback) const {}
 
 }  // namespace extensions
