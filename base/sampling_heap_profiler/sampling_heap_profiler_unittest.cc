769a7a82f940347c6e4cdbd5c103c2942299d54d
Benoit Lize <lizeb@chromium.org>
2022-04-22 11:56:57 +0000

    Revert "Ensure PoissonAllocationSampler tests restore randomization when done."
    This reverts commit e18f113266b485798a7e70d1a76507a76e4fe067.
    
    Reason for revert: Suspected root cause for crbug.com/1318812
    
    Original change's description:
    > Ensure PoissonAllocationSampler tests restore randomization when done.
    >
    > Replaces SuppressRandomnessForTest with a scoped object that is
    > automatically deleted at the end of each test.
    >
    > Also explicitly initializes all static and thread-local globals in
    > poisson_allocation_sampler.cc. This should be a no-op since globals are
    > zero-initialized but it makes the code easier to reason about.
    >
    > Bug: 1271555
    > Change-Id: Ia5eef7dfe7fdceaa0493325570cf48453d1d3a71
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597485
    > Auto-Submit: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/main@{#994960}
    
    Bug: 1271555
    Change-Id: Ie751f1f23ba2cec975c2aaa1a175e75120d77bdf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599046
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Owners-Override: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995151}

--- base/sampling_heap_profiler/sampling_heap_profiler_unittest.cc
+++ base/sampling_heap_profiler/sampling_heap_profiler_unittest.cc
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-using ScopedSuppressRandomnessForTesting =
-    PoissonAllocationSampler::ScopedSuppressRandomnessForTesting;
-
 class SamplingHeapProfilerTest : public ::testing::Test {
  public:
   void SetUp() override {
@@ -30,11 +27,6 @@ class SamplingHeapProfilerTest : public ::testing::Test {
     allocator::InitializeAllocatorShim();
 #endif
     SamplingHeapProfiler::Init();
-
-    // Ensure the PoissonAllocationSampler starts in the default state.
-    ASSERT_FALSE(PoissonAllocationSampler::AreHookedSamplesMuted());
-    ASSERT_FALSE(PoissonAllocationSampler::ScopedMuteThreadSamples::IsMuted());
-    ASSERT_FALSE(ScopedSuppressRandomnessForTesting::IsSuppressed());
   }
 
   size_t GetNextSample(size_t mean_interval) {
@@ -83,9 +75,9 @@ class SamplesCollector : public PoissonAllocationSampler::SamplesObserver {
 };
 
 TEST_F(SamplingHeapProfilerTest, SampleObserver) {
-  ScopedSuppressRandomnessForTesting suppress;
   SamplesCollector collector(10000);
   auto* sampler = PoissonAllocationSampler::Get();
+  sampler->SuppressRandomnessForTest(true);
   sampler->SetSamplingInterval(1024);
   sampler->AddSamplesObserver(&collector);
   void* volatile p = malloc(10000);
@@ -96,9 +88,9 @@ TEST_F(SamplingHeapProfilerTest, SampleObserver) {
 }
 
 TEST_F(SamplingHeapProfilerTest, SampleObserverMuted) {
-  ScopedSuppressRandomnessForTesting suppress;
   SamplesCollector collector(10000);
   auto* sampler = PoissonAllocationSampler::Get();
+  sampler->SuppressRandomnessForTest(true);
   sampler->SetSamplingInterval(1024);
   sampler->AddSamplesObserver(&collector);
   {
@@ -112,7 +104,7 @@ TEST_F(SamplingHeapProfilerTest, SampleObserverMuted) {
 }
 
 TEST_F(SamplingHeapProfilerTest, IntervalRandomizationSanity) {
-  ASSERT_FALSE(ScopedSuppressRandomnessForTesting::IsSuppressed());
+  PoissonAllocationSampler::Get()->SuppressRandomnessForTest(false);
   constexpr int iterations = 50;
   constexpr size_t target = 10000000;
   int sum = 0;
@@ -169,8 +161,8 @@ class MyThread2 : public SimpleThread {
 };
 
 void CheckAllocationPattern(void (*allocate_callback)()) {
-  ASSERT_FALSE(ScopedSuppressRandomnessForTesting::IsSuppressed());
   auto* profiler = SamplingHeapProfiler::Get();
+  PoissonAllocationSampler::Get()->SuppressRandomnessForTest(false);
   profiler->SetSamplingInterval(10240);
   base::TimeTicks t0 = base::TimeTicks::Now();
   std::map<size_t, size_t> sums;
@@ -318,10 +310,10 @@ TEST_F(SamplingHeapProfilerTest, MAYBE_ConcurrentStartStop) {
 }
 
 TEST_F(SamplingHeapProfilerTest, HookedAllocatorMuted) {
-  ScopedSuppressRandomnessForTesting suppress;
   EXPECT_FALSE(PoissonAllocationSampler::AreHookedSamplesMuted());
 
   auto* sampler = PoissonAllocationSampler::Get();
+  sampler->SuppressRandomnessForTest(true);
   sampler->SetSamplingInterval(1024);
 
   {
