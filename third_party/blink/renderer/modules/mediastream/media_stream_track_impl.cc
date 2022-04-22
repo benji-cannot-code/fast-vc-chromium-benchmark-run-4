805a6c623342329e52a70eb90236eb2abbbaefc5
Tony Herre <toprice@chromium.org>
2022-04-22 11:09:59 +0000

    Remove CreateNativeAudioMediaStreamTrack
    Most of the body of the method was removed in crrev.com/c/3571833, so
    let's just inline it. Preperatory step to removing the ConnectToTrack()
    call here.
    
    Bug: 1302689
    Change-Id: I2fc86bd7f5192faaa46a59ab420b767aad4629cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599080
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995137}

--- third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
@@ -210,9 +210,7 @@ void DidCloneMediaStreamTrack(MediaStreamComponent* original,
 
   switch (clone->Source()->GetType()) {
     case MediaStreamSource::kTypeAudio:
-      // TODO(crbug.com/704136): Use per thread task runner.
-      MediaStreamUtils::CreateNativeAudioMediaStreamTrack(
-          clone, Thread::MainThread()->GetTaskRunner());
+      MediaStreamAudioSource::From(clone->Source())->ConnectToTrack(clone);
       break;
     case MediaStreamSource::kTypeVideo:
       CloneNativeVideoMediaStreamTrack(original, clone);
--- third_party/blink/renderer/modules/mediastream/media_stream_utils.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_utils.h
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}
-
 namespace blink {
 
 class MediaStreamComponent;
@@ -21,10 +17,6 @@ class MediaStreamUtils {
   STATIC_ONLY(MediaStreamUtils);
 
  public:
-  static void CreateNativeAudioMediaStreamTrack(
-      MediaStreamComponent*,
-      scoped_refptr<base::SingleThreadTaskRunner>);
-
   static void DidCreateMediaStreamTrack(MediaStreamComponent*);
 };
 
