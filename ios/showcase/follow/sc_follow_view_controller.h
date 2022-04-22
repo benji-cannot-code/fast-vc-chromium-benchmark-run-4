609ccc5e3bfc3012292fb3348edc6892492b26ae
Ed Chin <edchin@google.com>
2022-04-22 00:56:48 +0000

    [ios][follow] Add showcase for follow UI
    Adding a showcase for these UI screens will help
    quickly iterate during polish week.
    
    Change-Id: Ia5c07f608a34a1f13ca8c3388da1e1c0692e4f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601173
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Auto-Submit: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#994992}

---
+++ ios/showcase/follow/sc_follow_view_controller.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_SHOWCASE_FOLLOW_SC_FOLLOW_VIEW_CONTROLLER_H_
+#define IOS_SHOWCASE_FOLLOW_SC_FOLLOW_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+@interface SCFollowViewController : UIViewController
+@end
+
+#endif  // IOS_SHOWCASE_FOLLOW_SC_FOLLOW_VIEW_CONTROLLER_H_
