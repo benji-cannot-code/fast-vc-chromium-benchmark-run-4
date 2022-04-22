9c61f0da1d326cdd260978ab5a2f3c2b03c18f12
James Cook <jamescook@chromium.org>
2022-04-22 21:03:07 +0000

    Add custom icon for "show continue section" button in new launcher
    Also update the button label text to better match the mock.
    
    https://screenshot.googleplex.com/9a9skVdKqhBVDT5
    
    Bug: 1317428
    Test: visual
    Change-Id: I6933baef9b306283a6e0d900b288207cbb6450c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602646
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995328}

--- ash/style/pill_button.cc
+++ ash/style/pill_button.cc
@@ -207,6 +207,10 @@ void PillButton::SetIconColor(const SkColor icon_color) {
   OnThemeChanged();
 }
 
+void PillButton::SetUseDefaultLabelFont() {
+  label()->SetFontList(views::Label::GetDefaultFontList());
+}
+
 BEGIN_METADATA(PillButton, views::LabelButton)
 END_METADATA
 
