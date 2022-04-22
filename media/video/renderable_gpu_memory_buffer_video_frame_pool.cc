7dac7be453871b15b819d5afc72050939735c706
Zhenyao Mo <zmo@chromium.org>
2022-04-22 00:48:30 +0000

    Use the right tex target for video frame for ANGLE/Metal
    GL_TEXTURE_RECTANGLE is not supported by ANGLE/Metal.
    
    R=dalecurtis@chromium.org
    
    Bug: 1311844
    Test: Meet effects working with ANGLE/Metal backend (and OOP-R Canvas)
    Change-Id: Iae97969b52fe49a5a7d99ac2ee3f372f5ee91ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597489
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994987}

--- media/video/renderable_gpu_memory_buffer_video_frame_pool.cc
+++ media/video/renderable_gpu_memory_buffer_video_frame_pool.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/gpu_memory_buffer_manager.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
+#include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "media/base/video_frame.h"
 
@@ -166,10 +167,12 @@ bool FrameResources::Initialize() {
       gfx::BufferUsage::SCANOUT_CPU_READ_WRITE
 #endif
       ;
+  constexpr gfx::BufferFormat kBufferFormat =
+      gfx::BufferFormat::YUV_420_BIPLANAR;
 
   // Create the GpuMemoryBuffer.
-  gpu_memory_buffer_ = context->CreateGpuMemoryBuffer(
-      coded_size_, gfx::BufferFormat::YUV_420_BIPLANAR, kBufferUsage);
+  gpu_memory_buffer_ =
+      context->CreateGpuMemoryBuffer(coded_size_, kBufferFormat, kBufferUsage);
   if (!gpu_memory_buffer_) {
     DLOG(ERROR) << "Failed to allocate GpuMemoryBuffer for frame: coded_size="
                 << coded_size_.ToString()
@@ -188,17 +191,17 @@ bool FrameResources::Initialize() {
       gpu::SHARED_IMAGE_USAGE_GLES2 | gpu::SHARED_IMAGE_USAGE_RASTER |
       gpu::SHARED_IMAGE_USAGE_DISPLAY | gpu::SHARED_IMAGE_USAGE_SCANOUT;
 
+  uint32_t texture_target = GL_TEXTURE_2D;
+#if BUILDFLAG(IS_MAC)
+  // TODO(https://crbug.com/1311844): Use gpu::GetBufferTextureTarget() instead.
+  texture_target = gpu::GetPlatformSpecificTextureTarget();
+#endif
   for (size_t plane = 0; plane < kNumPlanes; ++plane) {
     context->CreateSharedImage(
         gpu_memory_buffer_.get(), kPlanes[plane], color_space_,
         kTopLeft_GrSurfaceOrigin, kPremul_SkAlphaType, kSharedImageUsage,
         mailbox_holders_[plane].mailbox, mailbox_holders_[plane].sync_token);
-    // TODO(https://crbug.com/1191956): This should be parameterized.
-#if BUILDFLAG(IS_MAC)
-    mailbox_holders_[plane].texture_target = GL_TEXTURE_RECTANGLE_ARB;
-#else
-    mailbox_holders_[plane].texture_target = GL_TEXTURE_2D;
-#endif
+    mailbox_holders_[plane].texture_target = texture_target;
   }
   return true;
 }
