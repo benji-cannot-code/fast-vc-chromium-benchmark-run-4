9f0746a679e1171df8a4e3dc19ca8ec90e59590a
James Cook <jamescook@chromium.org>
2022-04-22 17:41:44 +0000

    Add localized strings for launcher show/hide continue section UI
    Also fix a capitalization mistake for "Remove suggestion". Context
    menu items are supposed to be "Sentence case" not "Title Case".
    
    Add a placeholder icon to the button to show the continue section.
    https://screenshot.googleplex.com/9ma7JqqK9m7fsRg
    
    Bug: 1317428
    Test: visual
    Change-Id: I610dd3543e45972a69b5a3135cfbdf65e3303ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600937
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995247}

--- ash/app_list/views/app_list_bubble_apps_page.cc
+++ ash/app_list/views/app_list_bubble_apps_page.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/controls/scroll_view_gradient_helper.h"
 #include "ash/public/cpp/metrics_util.h"
 #include "ash/public/cpp/style/color_provider.h"
+#include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/pill_button.h"
 #include "base/bind.h"
 #include "base/check.h"
@@ -36,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/aura/window.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/animation_throughput_reporter.h"
 #include "ui/compositor/layer.h"
@@ -162,15 +165,16 @@ AppListBubbleAppsPage::AppListBubbleAppsPage(
     auto* layout = container->SetLayoutManager(
         std::make_unique<BoxLayout>(BoxLayout::Orientation::kVertical));
     layout->set_cross_axis_alignment(BoxLayout::CrossAxisAlignment::kCenter);
-    // TODO(crbug.com/1317428): Add expander icon to button.
-    // TODO(crbug.com/1317428): Localized label.
+    // TODO(crbug.com/1317428): Switch to custom double-chevron icon.
     show_continue_section_button_ =
         container->AddChildView(std::make_unique<PillButton>(
             base::BindRepeating(
                 &AppListBubbleAppsPage::OnPressShowContinueSection,
                 base::Unretained(this)),
-            u"Show continue section", PillButton::Type::kIconless,
-            /*icon=*/nullptr));
+            l10n_util::GetStringUTF16(IDS_ASH_LAUNCHER_SHOW_CONTINUE_SECTION),
+            PillButton::Type::kIcon, &kChevronRightIcon));
+    // Put the icon on the right.
+    show_continue_section_button_->SetHorizontalAlignment(gfx::ALIGN_RIGHT);
   }
 
   // Continue section row.
