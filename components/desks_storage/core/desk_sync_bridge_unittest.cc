5cf2d81afdb5bd6367ebcb46238de0b04902f4e2
zhumatthew <zhumatthew@google.com>
2022-04-22 22:09:29 +0000

    Desk Template: Add desk type to constructor.
    This CL makes desk templates require a desk type to be passed in to
    allow for creating of saved desks and desk templates.
    
    TEST=Ran DeskTemplatesTest*, LocalDesk*, SingleClient*, DeskSync*, DeskTemplateConversion*
    
    BUG: b/229856032
    Change-Id: Ib684bdbeb16130b228c141b2a71947831b4e503b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597473
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#995384}

--- components/desks_storage/core/desk_sync_bridge_unittest.cc
+++ components/desks_storage/core/desk_sync_bridge_unittest.cc
@@ -60,6 +60,7 @@ namespace {
 
 using ash::DeskTemplate;
 using ash::DeskTemplateSource;
+using ash::DeskTemplateType;
 using sync_pb::ModelTypeState;
 using sync_pb::WorkspaceDeskSpecifics;
 using syncer::EntityChange;
@@ -286,7 +287,8 @@ std::unique_ptr<ash::DeskTemplate> CreateTemplateWithBrowserFromScratch(
   const std::string template_name =
       base::StringPrintf(kNameFormat, template_index);
   auto desk_template = std::make_unique<ash::DeskTemplate>(
-      template_uuid, DeskTemplateSource::kUser, template_name, created_time);
+      template_uuid, DeskTemplateSource::kUser, template_name, created_time,
+      DeskTemplateType::kTemplate);
 
   auto restore_data = std::make_unique<app_restore::RestoreData>();
   auto browser_info = std::make_unique<app_restore::AppLaunchInfo>(
@@ -936,9 +938,9 @@ TEST_F(DeskSyncBridgeTest, AddEntryShouldSucceedWheSyncIsDisabled) {
 
   base::RunLoop loop;
   bridge()->AddOrUpdateEntry(
-      std::make_unique<DeskTemplate>(kTestUuid1.AsLowercaseString(),
-                                     DeskTemplateSource::kUser, "template 1",
-                                     AdvanceAndGetTime()),
+      std::make_unique<DeskTemplate>(
+          kTestUuid1.AsLowercaseString(), DeskTemplateSource::kUser,
+          "template 1", AdvanceAndGetTime(), DeskTemplateType::kTemplate),
       base::BindLambdaForTesting([&](DeskModel::AddOrUpdateEntryStatus status) {
         EXPECT_EQ(status, DeskModel::AddOrUpdateEntryStatus::kOk);
         loop.Quit();
@@ -958,9 +960,9 @@ TEST_F(DeskSyncBridgeTest, AddEntryShouldFailWhenBridgeIsNotReady) {
 
   base::RunLoop loop;
   bridge()->AddOrUpdateEntry(
-      std::make_unique<DeskTemplate>(kTestUuid1.AsLowercaseString(),
-                                     DeskTemplateSource::kUser, "template 1",
-                                     AdvanceAndGetTime()),
+      std::make_unique<DeskTemplate>(
+          kTestUuid1.AsLowercaseString(), DeskTemplateSource::kUser,
+          "template 1", AdvanceAndGetTime(), DeskTemplateType::kTemplate),
       base::BindLambdaForTesting([&](DeskModel::AddOrUpdateEntryStatus status) {
         EXPECT_EQ(status, DeskModel::AddOrUpdateEntryStatus::kFailure);
         loop.Quit();
@@ -1114,7 +1116,8 @@ TEST_F(DeskSyncBridgeTest, UpdateEntryLocally) {
   bridge()->AddOrUpdateEntry(
       std::make_unique<DeskTemplate>(kTestUuid1.AsLowercaseString(),
                                      DeskTemplateSource::kUser,
-                                     "updated template 1", AdvanceAndGetTime()),
+                                     "updated template 1", AdvanceAndGetTime(),
+                                     DeskTemplateType::kTemplate),
       base::BindLambdaForTesting([&](DeskModel::AddOrUpdateEntryStatus status) {
         EXPECT_EQ(status, DeskModel::AddOrUpdateEntryStatus::kOk);
         loop.Quit();
