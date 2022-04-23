8e15ad89874ca83685039a72708d5643137e1d00
Emilia Paz <emiliapaz@chromium.org>
2022-04-23 02:48:38 +0000

    [Extensions c2s] Remove item's combobox when extension can't be customized
    An ExtensionMenuItemView should not have site access combobox when the
    extension cannot be customized, which happens when 'grant all extensions'
    site setting is selected. Note that if 'block all extensions' is selected,
    no item should be visible on the menu so we don't need to remove the
    combobox.
    
    Bug: 1263310
    Change-Id: I5e404859fd7f9e39bf7bd64ddfcee36db46e0973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3590732
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995472}

--- chrome/browser/ui/views/extensions/extensions_toolbar_unittest.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_unittest.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "content/public/test/web_contents_tester.h"
+#include "extensions/browser/permissions_manager.h"
 #include "extensions/common/extension.h"
 
 namespace extensions {
@@ -67,6 +68,10 @@ class ExtensionsToolbarUnitTest : public TestWithBrowserView {
 
   // Triggers the press and release event of the given `button`.
   void ClickButton(views::Button* button) const;
+
+  // Returns the user's site setting for `url`.
+  extensions::PermissionsManager::UserSiteSetting GetUserSiteSetting(
+      const GURL& url);
 
   // Returns a list of the views of the currently pinned extensions, in order
   // from left to right.
