57666bf77358f14f44d8b8be8d204ce565121a17
Daniel Cheng <dcheng@chromium.org>
2022-04-23 02:39:57 +0000

    Remove use of "generic" MojoUnion::New() helper in //components/services/app_service/public/cpp
    The New() method for unions has a generic-sounding name but actually
    creates a union containing the first declared subtype. In general, this
    behavior is non-intuitive; it can even give the impression that it
    creates an empty union that doesn't contain any of its subtypes.  It is
    also inefficient: many uses of the New() method immediately populate a
    different subtype, so the initial allocation to hold the first declared
    subtype is simply allocator churn.
    
    For these reasons, usage of MojoUnion::New() is being phased out and
    removed.
    
    This CL was uploaded by git cl split.
    
    R=nigeltao@chromium.org
    
    Bug: 1261169
    Change-Id: I4c99fa2675b09dbe2aa67d0c2625f4e48812468a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602194
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995470}

--- components/services/app_service/public/cpp/app_update_mojom_unittest.cc
+++ components/services/app_service/public/cpp/app_update_mojom_unittest.cc
@@ -123,8 +123,7 @@ class AppUpdateMojomTest : public testing::Test {
       apps::mojom::TriState value) {
     apps::mojom::PermissionPtr permission = apps::mojom::Permission::New();
     permission->permission_type = permission_type;
-    permission->value = apps::mojom::PermissionValue::New();
-    permission->value->set_tristate_value(value);
+    permission->value = apps::mojom::PermissionValue::NewTristateValue(value);
     return permission;
   }
 
@@ -1073,8 +1072,7 @@ TEST_F(AppUpdateMojomTest, AppConvert) {
 
   auto permission = apps::mojom::Permission::New();
   permission->permission_type = apps::mojom::PermissionType::kCamera;
-  permission->value = apps::mojom::PermissionValue::New();
-  permission->value->set_bool_value(true);
+  permission->value = apps::mojom::PermissionValue::NewBoolValue(true);
   permission->is_managed = true;
   input->permissions.push_back(std::move(permission));
 
