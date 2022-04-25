69c59dd7a9fa06bc08381428fb44f7d2ef0b57e6
Daniel Rubery <drubery@chromium.org>
2022-04-25 23:19:30 +0000

    Remove Client Side Phishing model copy
    The model data was already copied once as we moved between threads.
    There's no value in doing another copy on the helepr thread.
    
    Bug: 1317474
    Change-Id: Ib5721bcb31a3ffff01280575284dc07dbc1d71e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605799
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995856}

--- components/safe_browsing/content/renderer/phishing_classifier/scorer.h
+++ components/safe_browsing/content/renderer/phishing_classifier/scorer.h
@@ -132,7 +132,7 @@ class Scorer {
       const SkBitmap& bitmap,
       int input_width,
       int input_height,
-      const std::string& model_data,
+      std::string model_data,
       scoped_refptr<base::SequencedTaskRunner> callback_task_runner,
       base::OnceCallback<void(std::vector<double>)> callback);
 
