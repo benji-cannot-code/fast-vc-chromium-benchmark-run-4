1cafa37da3d3993d18077492f409c3acea217058
Phillis Tang <phillis@google.com>
2022-04-25 23:59:04 +0000

    DPWA: add WebApp.Install.Result metric
    Record boolean result for installation. Uninstall is recorded as
    WebApp.Uninstall.Result in WebAppUninstallJob.
    
    Bug: 1319589
    Change-Id: I3ae3ebbe09fea45956260fc078bbf6582b96da2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606193
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995880}

--- chrome/browser/web_applications/web_app_install_task.cc
+++ chrome/browser/web_applications/web_app_install_task.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -95,6 +96,8 @@ bool IsEmptyIconBitmapsForIconUrl(const IconsMap& icons_map,
 
   return true;
 }
+
+const char* kHistogramInstallResult = "WebApp.Install.Result";
 
 #if BUILDFLAG(IS_CHROMEOS)
 struct PlayStoreIntent {
@@ -479,6 +482,7 @@ void WebAppInstallTask::CallInstallCallback(const AppId& app_id,
   }
 
   DCHECK(install_callback_);
+  base::UmaHistogramBoolean(kHistogramInstallResult, webapps::IsSuccess(code));
   std::move(install_callback_).Run(app_id, code);
 }
 
