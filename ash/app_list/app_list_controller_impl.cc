366b3c91de7a48f5b3aa093470a9e61e081cf3e4
James Cook <jamescook@chromium.org>
2022-04-25 22:10:48 +0000

    Allow hiding the launcher continue section in tablet mode
    Enable the context menu items that hide the section. Add the "show
    continue section" button to AppsContainerView::ContinueContainer, which
    is used in tablet mode with productivity launcher.
    
    https://screenshot.googleplex.com/7HLYE7ZpoDxqX2Q
    
    The button background color is too light, but we don't have a spec for
    it yet, so I'm leaving it as-is. We may have to make PillButton more
    configurable to handle this button with dark/light mode.
    
    Bug: 1317428
    Test: added to ash_unittests
    Change-Id: Ia35d28758cd9edddf670d136f76690336bd5a00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599526
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995801}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -1637,6 +1637,7 @@ bool AppListControllerImpl::ShouldHideContinueSection() const {
 void AppListControllerImpl::SetHideContinueSection(bool hide) {
   PrefService* prefs = GetLastActiveUserPrefService();
   prefs->SetBoolean(prefs::kLauncherContinueSectionHidden, hide);
+  fullscreen_presenter_->UpdateContinueSectionVisibility();
   bubble_presenter_->UpdateContinueSectionVisibility();
 }
 
