40bfd547ad89120d4d9c26af606f9d116a5f64be
Junbo Ke <juke@chromium.org>
2022-04-25 22:09:02 +0000

    Remove BrowserStartupController.setDisableLibraryLoadForCast.
    Bug: 1318550
    Test: build
    Change-Id: I9e48eb5ddc8c88551a14bb4367c620288af27df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601029
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995800}

--- content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
@@ -106,9 +106,6 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
     // of enqueued callbacks have been executed.
     private boolean mStartupSuccess;
 
-    // Whether library loading is disabled.
-    private boolean mLibraryLoadDisabled;
-
     // Tests may inject a method to be run instead of calling ContentMain() in order for them to
     // initialize the C++ system via another means.
     private Runnable mContentMainCallbackForTests;
@@ -348,11 +345,6 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
                 mFullBrowserStartupDone, mMinimalBrowserStarted, startMinimalBrowser);
     }
 
-    @Override
-    public void setDisableLibraryLoadForCast(boolean disabled) {
-        mLibraryLoadDisabled = disabled;
-    }
-
     /**
      * Asserts that library process type is one of the supported types.
      * @param libraryProcessType the type of process the shared library is loaded. It must be
@@ -444,21 +436,19 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
         Log.d(TAG, "Initializing chromium process, singleProcess=%b", singleProcess);
         mPrepareToStartCompleted = true;
         try (ScopedSysTraceEvent e = ScopedSysTraceEvent.scoped("prepareToStartBrowserProcess")) {
-            if (!mLibraryLoadDisabled) {
-                // This strictmode exception is to cover the case where the browser process is being
-                // started asynchronously but not in the main browser flow.  The main browser flow
-                // will trigger library loading earlier and this will be a no-op, but in the other
-                // cases this will need to block on loading libraries. This applies to tests and
-                // ManageSpaceActivity, which can be launched from Settings.
-                StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
-                try {
-                    // Normally Main.java will have already loaded the library asynchronously, we
-                    // only need to load it here if we arrived via another flow, e.g. bookmark
-                    // access & sync setup.
-                    LibraryLoader.getInstance().ensureInitialized();
-                } finally {
-                    StrictMode.setThreadPolicy(oldPolicy);
-                }
+            // This strictmode exception is to cover the case where the browser process is being
+            // started asynchronously but not in the main browser flow.  The main browser flow
+            // will trigger library loading earlier and this will be a no-op, but in the other
+            // cases this will need to block on loading libraries. This applies to tests and
+            // ManageSpaceActivity, which can be launched from Settings.
+            StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+            try {
+                // Normally Main.java will have already loaded the library asynchronously, we
+                // only need to load it here if we arrived via another flow, e.g. bookmark
+                // access & sync setup.
+                LibraryLoader.getInstance().ensureInitialized();
+            } finally {
+                StrictMode.setThreadPolicy(oldPolicy);
             }
 
             // TODO(yfriedman): Remove dependency on a command line flag for this.
