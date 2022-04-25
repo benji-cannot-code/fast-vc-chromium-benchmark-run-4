d3d9dd6cc3265fc225f267180ec425477743a628
Nico Weber <thakis@chromium.org>
2022-04-25 22:15:52 +0000

    Try to fix ToTLinuxASan build.
    Bug: 1319388
    Change-Id: I45788ff914bcb7ed177187d9b3d8dc6d71b6d7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602685
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995806}

--- third_party/blink/renderer/platform/heap/thread_state_scopes.h
+++ third_party/blink/renderer/platform/heap/thread_state_scopes.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "v8/include/cppgc/heap-consistency.h"
 
+#if defined(LEAK_SANITIZER)
+#include <sanitizer/lsan_interface.h>
+#endif
+
 namespace blink {
 
 // The NoAllocationScope class is used in debug mode to catch unwanted
