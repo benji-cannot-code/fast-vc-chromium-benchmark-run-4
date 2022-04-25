4da174893bff27945e0f871f106b8f5ffd399689
Xinghui Lu <xinghuilu@chromium.org>
2022-04-25 23:44:58 +0000

    Add hit reports to chrome://safe-browsing
    To better understand the information sent in hit reports, add them under
    chrome://safe-browsing/#tab-csbrr.
    
    screenshot: http://screen/Ah86EKVn3Ue7GTw
    
    Bug: 1317464
    Change-Id: I9276b1d1b0496d4f502c3303c2604542885e587f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595760
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995873}

--- components/safe_browsing/content/browser/ui_manager.cc
+++ components/safe_browsing/content/browser/ui_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
 #include "components/safe_browsing/content/browser/threat_details.h"
+#include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/core/browser/db/v4_protocol_manager_util.h"
 #include "components/safe_browsing/core/browser/ping_manager.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -204,6 +205,15 @@ void SafeBrowsingUIManager::MaybeReportSafeBrowsingHit(
            << hit_report.is_subresource << " " << hit_report.threat_type;
   delegate_->GetPingManager(web_contents->GetBrowserContext())
       ->ReportSafeBrowsingHit(hit_report);
+
+  // The following is to log this HitReport on any open chrome://safe-browsing
+  // pages.
+  auto hit_report_copy = std::make_unique<HitReport>(hit_report);
+  content::GetUIThreadTaskRunner({})->PostTask(
+      FROM_HERE,
+      base::BindOnce(&WebUIInfoSingleton::AddToHitReportsSent,
+                     base::Unretained(WebUIInfoSingleton::GetInstance()),
+                     std::move(hit_report_copy)));
 }
 
 // Static.
