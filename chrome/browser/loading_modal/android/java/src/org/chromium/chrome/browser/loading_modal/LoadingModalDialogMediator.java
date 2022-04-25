84b6d273d4123ac5255d0b9b6dc3bf642ac83724
Ivana Žužić <izuzic@google.com>
2022-04-25 18:55:02 +0000

    [Android][UPM] Add the base for the loading modal dialog
    A layout that contains a loading indicator and a coordinator for showing
    a dialog with it are created.
    A mediator that will hold the model state is also added in its initial
    form.
    The dialog should be shown when launching the new credential manager UI,
    which resides in GMS Core.
    
    Bug: 1311674
    Change-Id: Ib594cdb149c2058be24f7ef3ba6883aacc956ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599279
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#995705}

---
+++ chrome/browser/loading_modal/android/java/src/org/chromium/chrome/browser/loading_modal/LoadingModalDialogMediator.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.loading_modal;
+
+import org.chromium.ui.modaldialog.DialogDismissalCause;
+import org.chromium.ui.modaldialog.ModalDialogProperties;
+import org.chromium.ui.modaldialog.ModalDialogProperties.ButtonType;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Mediator class responsible for initializing the model state and handling button clicks. */
+class LoadingModalDialogMediator implements ModalDialogProperties.Controller {
+    public LoadingModalDialogMediator() {}
+
+    @Override
+    public void onClick(PropertyModel model, @ButtonType int buttonType) {}
+
+    @Override
+    public void onDismiss(PropertyModel model, @DialogDismissalCause int dismissalCause) {}
+}
