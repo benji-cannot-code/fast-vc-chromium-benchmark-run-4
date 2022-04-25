25f6412520d4f54e41611fb7af79b2080136fbd1
Xiaocheng Hu <xiaochengh@chromium.org>
2022-04-25 23:04:44 +0000

    [renderblocking] Dynamically inserted render-blocking stylesheets should not block scripts
    This patch fixes a bug that adding `blocking=render` to script-inserted
    stylesheets makes them script-blocking, which violates the spec as only
    parser-inserted stylesheets can be script-blocking [1].
    
    The fix idea is to add a new enum to PendingSheetType to represent
    dynamic render-blocking sheets, so that we only block rendering but not
    scripts or parser for this type.
    
    This patch is also a preparation patch to allow unblocking rendering on
    dynamic stylesheets by removing `blocking=render` [2]
    
    [1] https://html.spec.whatwg.org/#contributes-a-script-blocking-style-sheet
    [2] crrev.com/c/3601726
    
    Bug: 1271296
    Change-Id: I6eee34357ab034c802b3b022719427a1b3964c19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601502
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995849}

--- third_party/blink/renderer/platform/loader/fetch/render_blocking_behavior.h
+++ third_party/blink/renderer/platform/loader/fetch/render_blocking_behavior.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_RENDER_BLOCKING_BEHAVIOR_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_RENDER_BLOCKING_BEHAVIOR_H_
 
+#include <cstdint>
+
 namespace blink {
 enum class RenderBlockingBehavior : uint8_t {
   kUnset,                 // Render blocking value was not set.
