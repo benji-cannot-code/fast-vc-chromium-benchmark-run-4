6bd7e07efbb7be660674cd8ac60ae9de4aa0e9a8
Brian Geffon <bgeffon@chromium.org>
2022-04-25 20:06:56 +0000

    CrOS: resourced should re-post to the correct sequence
    When resourced is not up yet we reschedule our set memory
    margins dbus message to 30 seconds later. We're currently
    scheduling this on the base threadpool which is incorrect,
    we need to post it back to the proper sequence.
    
    Bug: b/230023889
    Change-Id: I7cc4b5db81c81c99452dd1b49616f2e0938d026d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601930
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995724}

--- chromeos/dbus/resourced/resourced_client.cc
+++ chromeos/dbus/resourced/resourced_client.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_metrics.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chromeos/dbus/resourced/fake_resourced_client.h"
 #include "dbus/bus.h"
@@ -247,8 +248,8 @@ void ResourcedClientImpl::HandleSetMemoryMarginBps(
     // If Chrome startup was racing with resourced startup it's possible
     // that the message was not delivered because resourced was not up yet.
     // Let's redispatch the message in 30 seconds.
-    base::ThreadPool::PostDelayedTask(
-        FROM_HERE, {base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
+    base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE,
         base::BindOnce(&ResourcedClientImpl::SetMemoryMarginsBps,
                        weak_factory_.GetWeakPtr(), critical_margin,
                        moderate_margin, std::move(callback)),
