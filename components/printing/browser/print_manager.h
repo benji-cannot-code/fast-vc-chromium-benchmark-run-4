9fcee4acf1b4094dbfbbf1ee8cb0cca65bee9747
Andrey Kosyakov <caseq@chromium.org>
2022-04-25 00:08:01 +0000

    Add failure reason to PrintManagerHost::PrintingFailed()
    This will be used in a subsequent patch to report an an error when the
    specified page range is beyond the end of the document.
    
    Bug: 1317918
    
    Change-Id: I429d42e076b1eda82599c813bfd482c1988c04a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579838
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995519}

--- components/printing/browser/print_manager.h
+++ components/printing/browser/print_manager.h
@@ -49,7 +49,8 @@ class PrintManager : public content::WebContentsObserver,
                         DidPrintDocumentCallback callback) override;
   void DidShowPrintDialog() override;
   void ShowInvalidPrinterSettingsError() override;
-  void PrintingFailed(int32_t cookie) override;
+  void PrintingFailed(int32_t cookie,
+                      mojom::PrintFailureReason reason) override;
 
   void ClearPrintRenderFramesForTesting();
 
