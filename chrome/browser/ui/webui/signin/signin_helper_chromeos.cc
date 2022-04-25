0fe61a71e05b2f0f33d4378179966d1c4b523054
Martin Rodriguez <rodmartin@google.com>
2022-04-25 12:25:30 +0000

    crOS: Display Account Manager generic error page
    Display Account Manager generic error page during
    SecondaryGoogleAccountUsage policy check. This means if any
    error occurs while checking the value of SecondaryGoogleAccountUsage
    policy, an error saying "Something went wrong" will be displayed.
    
    Bug: 1292366, b/227166207
    Test: browser_tests --gtest_filter="InlineLogin*"
    Change-Id: I70dbfebf6664e77c504122d711c89ce197cd813f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602332
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Cr-Commit-Position: refs/heads/main@{#995666}

--- chrome/browser/ui/webui/signin/signin_helper_chromeos.cc
+++ chrome/browser/ui/webui/signin/signin_helper_chromeos.cc
@@ -164,7 +164,7 @@ void SigninHelper::OnGetSecondaryGoogleAccountUsage(
   }
 
   if (status != SigninRestrictionPolicyFetcher::Status::kSuccess) {
-    // TODO(b/227166207): Display error something went wrong.
+    ShowSigninBlockedErrorPageAndExit(/*hosted_domain=*/std::string());
     return;
   }
 
