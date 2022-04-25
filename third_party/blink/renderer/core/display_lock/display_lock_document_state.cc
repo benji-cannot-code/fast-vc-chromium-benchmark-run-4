f17f531575c1c1a9b7b38b9d4e0fa51efb4b4fdb
Kent Tamura <tkent@chromium.org>
2022-04-25 07:20:10 +0000

    DeferredShaping: Speculative fix for a crash in DisplayLockDocumentState::UnlockShapingDeferredElements()
    This CL has no tests. We could not find a way to reproduce the crash.
    
    Bug: 1259085, 1319113
    Change-Id: Id04b9da46bf32b7b7957347552e931d03b3352b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3603752
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995600}

--- third_party/blink/renderer/core/display_lock/display_lock_document_state.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_document_state.cc
@@ -392,7 +392,7 @@ void DisplayLockDocumentState::UnlockShapingDeferredElements() {
   if (LockedDisplayLockCount() == DisplayLockBlockingAllActivationCount())
     return;
   for (auto& context : display_lock_contexts_) {
-    if (context->IsShapingDeferred())
+    if (context->HasElement() && context->IsShapingDeferred())
       context->SetRequestedState(EContentVisibility::kVisible);
   }
 }
