51c8b13852b987de85c27c59bf0e50815885a9f8
Tony Herre <toprice@chromium.org>
2022-04-25 21:45:07 +0000

    Remove SetPlatformTrack call from CanvasCaptureMediaStreamTrack::clone
    Currently it indirectly calls SetPlatformTrack from inside
    MediaStreamUtils::DidCreateMediaStreamTrack(). This changes to directly
    creating the MediaStreamVideoTrack instance and passing it into the
    constructor of the cloned MediaStreamComponent.
    
    Part of an effort to remove SetPlatformTrack from MediaStreamComponent,
    instead using a new constructor, to better align their lifecycles.
    
    Bug: 1302689
    Change-Id: I431d534b076bfcd13f7ecc9b5872db1f500b88f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599427
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995781}

--- third_party/blink/renderer/modules/mediacapturefromelement/canvas_capture_media_stream_track.cc
+++ third_party/blink/renderer/modules/mediacapturefromelement/canvas_capture_media_stream_track.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediacapturefromelement/on_request_canvas_draw_listener.h"
 #include "third_party/blink/renderer/modules/mediacapturefromelement/timed_canvas_draw_listener.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_utils.h"
+#include "third_party/blink/renderer/modules/mediastream/media_stream_video_track.h"
 
 namespace blink {
 
@@ -24,11 +25,15 @@ void CanvasCaptureMediaStreamTrack::requestFrame() {
 
 CanvasCaptureMediaStreamTrack* CanvasCaptureMediaStreamTrack::clone(
     ScriptState* script_state) {
-  MediaStreamComponent* cloned_component = Component()->Clone();
+  MediaStreamComponent* cloned_component =
+      Component()->Clone(std::make_unique<blink::MediaStreamVideoTrack>(
+          MediaStreamVideoSource::GetVideoSource(Component()->Source()),
+          blink::MediaStreamVideoSource::ConstraintsOnceCallback(),
+          Component()->Enabled()));
   CanvasCaptureMediaStreamTrack* cloned_track =
       MakeGarbageCollected<CanvasCaptureMediaStreamTrack>(*this,
                                                           cloned_component);
-  MediaStreamUtils::DidCreateMediaStreamTrack(cloned_component);
+
   return cloned_track;
 }
 
