c59f0304a160eddc061e509ad470ee9d422f9422
Chung-Sheng Wu <chungsheng@google.com>
2022-04-25 09:25:01 +0000

    cros_healthd: Migrate to FakeCrosHealthd in chromebox_for_meetings/
    Bug: b/210565604
    Test: CQ
    Change-Id: I54fffd1118d1e6f1637c57080bf9d41c8433310e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599452
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#995625}

--- chrome/browser/ash/chromebox_for_meetings/diagnostics/diagnostics_service_unittest.cc
+++ chrome/browser/ash/chromebox_for_meetings/diagnostics/diagnostics_service_unittest.cc
@@ -21,8 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
 #include "chromeos/ash/components/dbus/chromebox_for_meetings/fake_cfm_hotline_client.h"
-#include "chromeos/ash/components/dbus/cros_healthd/cros_healthd_client.h"
-#include "chromeos/ash/components/dbus/cros_healthd/fake_cros_healthd_client.h"
+#include "chromeos/services/cros_healthd/public/cpp/fake_cros_healthd.h"
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
 #include "content/public/test/test_utils.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -47,7 +46,7 @@ class CfmDiagnosticsServiceTest : public ::testing::Test {
       delete;
 
   void SetUp() override {
-    cros_healthd::CrosHealthdClient::InitializeFake();
+    cros_healthd::FakeCrosHealthd::Initialize();
     CfmHotlineClient::InitializeFake();
     ServiceConnection::UseFakeServiceConnectionForTesting(
         &fake_service_connection_);
@@ -57,11 +56,7 @@ class CfmDiagnosticsServiceTest : public ::testing::Test {
   void TearDown() override {
     DiagnosticsService::Shutdown();
     CfmHotlineClient::Shutdown();
-    cros_healthd::CrosHealthdClient::Shutdown();
-
-    // Wait for cros_healthd's ServiceConnection to observe the destruction of
-    // the client.
-    cros_healthd::ServiceConnection::GetInstance()->FlushForTesting();
+    cros_healthd::FakeCrosHealthd::Shutdown();
   }
 
   FakeCfmHotlineClient* GetClient() {
@@ -142,8 +137,8 @@ TEST_F(CfmDiagnosticsServiceTest, GetDeviceInfoService) {
 // information from cros_healthd.
 TEST_F(CfmDiagnosticsServiceTest, GetCrosHealthdTelemetry) {
   auto response = cros_healthd::mojom::TelemetryInfo::New();
-  cros_healthd::FakeCrosHealthdClient::Get()
-      ->SetProbeTelemetryInfoResponseForTesting(response);
+  cros_healthd::FakeCrosHealthd::Get()->SetProbeTelemetryInfoResponseForTesting(
+      response);
   base::RunLoop run_loop;
   DiagnosticsService::Get()->GetCrosHealthdTelemetry(base::BindLambdaForTesting(
       [&](cros_healthd::mojom::TelemetryInfoPtr info) {
@@ -158,8 +153,8 @@ TEST_F(CfmDiagnosticsServiceTest, GetCrosHealthdTelemetry) {
 TEST_F(CfmDiagnosticsServiceTest, GetCrosHealthdProcessInfo) {
   auto response = cros_healthd::mojom::ProcessResult::NewProcessInfo(
       cros_healthd::mojom::ProcessInfo::New());
-  cros_healthd::FakeCrosHealthdClient::Get()
-      ->SetProbeProcessInfoResponseForTesting(response);
+  cros_healthd::FakeCrosHealthd::Get()->SetProbeProcessInfoResponseForTesting(
+      response);
   base::RunLoop run_loop;
   DiagnosticsService::Get()->GetCrosHealthdProcessInfo(
       /*pid=*/10, base::BindLambdaForTesting(
