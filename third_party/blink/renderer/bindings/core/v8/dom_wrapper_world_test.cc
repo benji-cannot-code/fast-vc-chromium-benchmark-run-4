e481b4a4f781d6386f699193a98e162eccfbec0f
David Sanders <dsanders11@ucsbalum.com>
2022-04-25 00:14:20 +0000

    Do IWYU for active_script_wrappable_manager.h
    Also adds some includes to other files which were
    getting them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I3bd3d57a10b0ea098fd5194dbfcd0e5b4ea0f573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600661
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995530}

--- third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
+++ third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_copier_base.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 
 namespace blink {
 namespace {
