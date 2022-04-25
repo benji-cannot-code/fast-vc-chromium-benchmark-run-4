537e50af41985d874b72c947c59d189e07544f54
Caitlin Fischer <caitlinfischer@google.com>
2022-04-25 19:52:23 +0000

    Revert "Schedule a Local State write after updating the GPU crash count."
    This reverts commit e6eeb12e6d6b351a42ab87ab05b77e0b221aec97.
    
    Reason for revert: The original change was intended to be temporary to
    see whether it resolved a discrepancy. It didn't.
    https://screenshot.googleplex.com/9KMdUNTFEQA4S8z
    
    Original change's description:
    > Schedule a Local State write after updating the GPU crash count.
    >
    > Bug: 1300240, b/211786119
    > Change-Id: Ib105c8f9252211dd990c113cf1acc3798107cc7e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484610
    > Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    > Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#974322}
    
    Bug: 1300240, b/211786119
    Change-Id: Ia64ea77174d58a2022c0b65d16b8361dd4ec710a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3604753
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#995721}

--- components/metrics/stability_metrics_helper.cc
+++ components/metrics/stability_metrics_helper.cc
@@ -139,7 +139,6 @@ void StabilityMetricsHelper::IncreaseRendererCrashCount() {
 
 void StabilityMetricsHelper::IncreaseGpuCrashCount() {
   IncrementPrefValue(prefs::kStabilityGpuCrashCount);
-  local_state_->CommitPendingWrite();  // Schedule a Local State write.
   RecordStabilityEvent(StabilityEventType::kGpuCrash);
 }
 
