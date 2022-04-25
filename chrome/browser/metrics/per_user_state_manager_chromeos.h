6f645d73526ab0cd8b8b51a0f9b70646e4b61846
Miriam Zimmerman <mutexlox@google.com>
2022-04-25 20:47:22 +0000

    metrics: Add boot-collect-consent file.
    The boot crash collectors should respect the consent setting of the
    last-logged-in user before the reboot / panic happened. So, write the
    consent to a single file outside of the cryptohome. This file will be
    used solely for the boot collector to check consent.
    
    BUG=b:214441393
    TEST=Deploy, log in to second account, toggle consent, check file;\
         log out, check file again
    TEST=manually touch file, log in to primary account; check file is gone
    
    Change-Id: I2ea0ba384a47e9aede16893d7605668254b4c0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540925
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Auto-Submit: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995735}

--- chrome/browser/metrics/per_user_state_manager_chromeos.h
+++ chrome/browser/metrics/per_user_state_manager_chromeos.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/components/login/session/session_termination_manager.h"
 #include "base/callback_list.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -40,7 +41,8 @@ namespace metrics {
 // assumption is only true in Ash Chrome.
 class PerUserStateManagerChromeOS
     : public user_manager::UserManager::UserSessionStateObserver,
-      public user_manager::UserManager::Observer {
+      public user_manager::UserManager::Observer,
+      public ash::SessionTerminationManager::Observer {
  public:
   // Callback to handle changes in user metrics consent.
   using MetricsConsentHandler = base::RepeatingCallback<void(bool)>;
@@ -192,6 +194,9 @@ class PerUserStateManagerChromeOS
 
   // UserManager::Observer:
   void OnUserToBeRemoved(const AccountId& account_id) override;
+
+  // ash::SessionTerminationManager::Observer:
+  void OnSessionWillBeTerminated() override;
 
   // Loads appropriate prefs from |current_user_| and creates new log storage
   // using profile prefs.
