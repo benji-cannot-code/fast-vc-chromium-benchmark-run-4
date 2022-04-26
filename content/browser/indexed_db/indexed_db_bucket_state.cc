d2e58c75cabedcc1b3c5715d06881e40fd954757
Reilly Grant <reillyg@chromium.org>
2022-04-26 22:38:35 +0000

    Revert "[IndexedDB Storage Partition] (9) Store BucketLocator to IndexedDBBucketState and IndexedDBFactoryImpl"
    This reverts commit 9139d9d37365cb6c0b69132e38a3f25a53763ae7.
    
    Reason for revert: Failing on MSan. See crbug.com/1320064.
    
    Bug: 1320064
    
    Original change's description:
    > [IndexedDB Storage Partition] (9) Store BucketLocator to IndexedDBBucketState and IndexedDBFactoryImpl
    >
    > The BucketLocator was added to IndexedDBDispatcherHost in
    > https://chromium-review.googlesource.com/c/chromium/src/+/3531351.
    > Let's push it a bit beyond that point and do some cleanup with TODOs.
    > This is following the design doc:
    > https://docs.google.com/document/d/1VDm8M4vRyxCpvp_m3Ojs18W9g1WpqvSOsKf0A6hnAFM/edit#heading=h.t0xkimlxrs47
    >
    > This CL is a part of a series:
    > (1) Tentative WPT
    > (2) Add to third-party-storage-partitioning virtual suite
    > (3) Add WPT to check for persistence
    > (4) Rename IndexedDBBucketState(Handle)
    > (5) Store BucketLocator to IndexedDBCallbacks and DatabaseImpl
    > (6) Store BucketLocator to CursorImpl and TransactionImpl
    > (7) Store BucketLocator to IndexedDBDatabase
    > (8) Store BucketLocator to IndexedDBBackingStore and IndexedDBCursor
    > (9) Store BucketLocator to IndexedDBBucketState and IndexedDBFactoryImpl
    > (10) Store BucketLocator to IndexedDBContextImpl
    > (11) Cleanup after BucketLocator Refactor
    > (12) Migrate third party storage to non-origin buckets
    >
    > Bug: 1218100
    > Change-Id: If28340426d599c27d0ffc5ec4ebdf406114a24ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600662
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: Ayu Ishii <ayui@chromium.org>
    > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#996395}
    
    Bug: 1218100
    Change-Id: I4cee1c14d28af7f805b8507d2bd03b1f6b3fc463
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3609250
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Owners-Override: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996410}

--- content/browser/indexed_db/indexed_db_bucket_state.cc
+++ content/browser/indexed_db/indexed_db_bucket_state.cc
@@ -120,7 +120,7 @@ constexpr const base::TimeDelta
     IndexedDBBucketState::kMaxEarliestBucketCompactionFromNow;
 
 IndexedDBBucketState::IndexedDBBucketState(
-    storage::BucketLocator bucket_locator,
+    blink::StorageKey storage_key,
     bool persist_for_incognito,
     base::Clock* clock,
     TransactionalLevelDBFactory* transactional_leveldb_factory,
@@ -130,7 +130,7 @@ IndexedDBBucketState::IndexedDBBucketState(
     TasksAvailableCallback notify_tasks_callback,
     TearDownCallback tear_down_callback,
     std::unique_ptr<IndexedDBBackingStore> backing_store)
-    : bucket_locator_(std::move(bucket_locator)),
+    : storage_key_(std::move(storage_key)),
       persist_for_incognito_(persist_for_incognito),
       clock_(clock),
       transactional_leveldb_factory_(transactional_leveldb_factory),
