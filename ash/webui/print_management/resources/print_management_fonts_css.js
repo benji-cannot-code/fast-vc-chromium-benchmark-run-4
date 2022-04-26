b12f4854a905853a07080c9bd82736668c860c4b
dpapad <dpapad@chromium.org>
2022-04-26 02:56:48 +0000

    WebUI: Simplify how Polymer shared style modules are registered, part2.
    In this part fixing all occurrences under ash/webui/.
    
    Previously shared style modules were registered by instantiating a
    dom-module custom element, and adding it to the DOM, which eventually
    populated Polymer's internal style map.
    
    Adding the element to the DOM is unnecessary (and not free) as style
    modules can be registered directly using the dom-module's register()
    method, without adding them to the DOM, saving unnecessary operations.
    
    This is the recommendation from the Polymer team at [1].
    
    Style modules that are still used by Polymer2 (OOBE) are not affected
    in this CL.
    
    [1] https://github.com/Polymer/polymer/issues/4940#issuecomment-614213287
    
    Bug: None
    Change-Id: If019dfc13f6c94e9932a4e53277ea14322740646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602650
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995937}

--- ash/webui/print_management/resources/print_management_fonts_css.js
+++ ash/webui/print_management/resources/print_management_fonts_css.js
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-const template = document.createElement('template');
-template.innerHTML = `
-<dom-module id="print-management-fonts">{__html_template__}</dom-module>
-`;
-document.body.appendChild(template.content.cloneNode(true));
+const styleMod = document.createElement('dom-module');
+styleMod.innerHTML = `{__html_template__}`;
+styleMod.register('print-management-fonts');
