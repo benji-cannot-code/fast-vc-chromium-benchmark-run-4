ab32a8237f255c4dee1be4fc85d2b94313d0e754
Allen Bauer <kylixrd@chromium.org>
2022-04-26 23:10:47 +0000

    Favor OnWidgetDestroying over OnWidgetClosing, since the former is always called.
    Change-Id: I3021711a8bf0e1c5a2759afa2b18f3401564c02f
    Bug: 1240365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606654
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996426}

--- chrome/browser/ui/views/policy/enterprise_startup_dialog_view.h
+++ chrome/browser/ui/views/policy/enterprise_startup_dialog_view.h
@@ -83,7 +83,7 @@ class EnterpriseStartupDialogImpl : public EnterpriseStartupDialog,
   bool IsShowing() override;
 
   // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
 
  private:
   // The dialog_view_ is owned by itself.
