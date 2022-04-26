1f0ee6a210ef7c09fdba9e73ca1acf6b56019033
Peng Huang <penghuang@chromium.org>
2022-04-26 23:14:56 +0000

    RawDraw: use GrSlug for non-RawDraw as well
    Always using GrSlug. It should be able to fix the glyph cache missing
    problem for non-RawDraw.
    
    Bug: 879110
    Change-Id: I0c6ca932487bd4193e61ce35f55c592d4a69aacb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563524
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996430}

--- gpu/command_buffer/client/raster_implementation.cc
+++ gpu/command_buffer/client/raster_implementation.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/shared_memory_limits.h"
 #include "gpu/command_buffer/client/transfer_buffer.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
@@ -507,8 +506,7 @@ RasterImplementation::RasterImplementation(
       lost_(false),
       max_inlined_entry_size_(kMaxTransferCacheEntrySizeForTransferBuffer),
       transfer_cache_(this),
-      image_decode_accelerator_(image_decode_accelerator),
-      raw_draw_(features::IsUsingRawDraw()) {
+      image_decode_accelerator_(image_decode_accelerator) {
   DCHECK(helper);
   DCHECK(transfer_buffer);
   DCHECK(gpu_control);
@@ -1411,7 +1409,7 @@ void RasterImplementation::RasterCHROMIUM(const cc::DisplayItemList* list,
           raster_properties_->color_space, &skottie_serialization_history_,
           raster_properties_->can_use_lcd_text,
           capabilities().context_supports_distance_field_text,
-          capabilities().max_texture_size, raw_draw_));
+          capabilities().max_texture_size, /*raw_draw=*/true));
   if (preserve_recording) {
     serializer.Serialize(&list->paint_op_buffer_, &temp_raster_offsets_,
                          preamble);
