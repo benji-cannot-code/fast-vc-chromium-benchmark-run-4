5e1d406381e87952882877f96f670c598b5a54d9
Mike Pinkerton <pinkerton@google.com>
2022-04-26 16:23:22 +0000

    Test pure virtual methods in Swift/C++ interop.
    Pure virtual methods aren't really supported. Also make dtors virtual.
    
    Bug: none
    Change-Id: I1faeed8a120e7d3dc2fa609c0be64a8e192ce7d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3604754
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996214}

--- ios/chrome/test/swift_interop/classes/polymorphism.h
+++ ios/chrome/test/swift_interop/classes/polymorphism.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Shape {
  public:
   Shape(int width, int height) : width_(width), height_(height) {}
-  ~Shape() {}
+  virtual ~Shape() {}
 
-  virtual int Area() { return 0; }
+  virtual int Area() = 0;
   virtual int NumberOfSides() { return 0; }
 
   // Virtual or not, this cannot be called on inherited classes unless it
@@ -26,7 +26,7 @@ class Shape {
 class Rectangle : public Shape {
  public:
   Rectangle(int width, int height) : Shape(width, height) {}
-  ~Rectangle() {}
+  virtual ~Rectangle() {}
 
   virtual int Area() { return width_ * height_; }
   virtual int NumberOfSides() { return 4; }
@@ -35,7 +35,7 @@ class Rectangle : public Shape {
 class Square : public Rectangle {
  public:
   Square(int size) : Rectangle(size, size) {}
-  ~Square() {}
+  virtual ~Square() {}
 
   // Even though these should not be necessary, they are. Without them, calling
   // these methods on a Square object results in a compiler error.
@@ -46,7 +46,7 @@ class Square : public Rectangle {
 class Triangle : public Shape {
  public:
   Triangle(int width, int height) : Shape(width, height) {}
-  ~Triangle() {}
+  virtual ~Triangle() {}
 
   virtual int Area() { return (width_ * height_) / 2; }
   virtual int NumberOfSides() { return 3; }
