d54169dc50fe8affa574c6bdae6703e9cd90c092
Andrew Paseltiner <apaseltiner@chromium.org>
2022-04-26 15:33:01 +0000

    Perform realistic debug-cookie checks in attribution simulator
    This allows correct cookie configuration to be tested using the
    simulator, which otherwise always allows all debug keys to be
    propagated.
    
    The original behavior is retained via a new `skip_debug_cookie_checks`
    command-line option.
    
    Change-Id: I097e8705d2837cc1d80fe05d2847f018fb746f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599514
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996197}

--- content/test/attribution_simulator_input_parser.h
+++ content/test/attribution_simulator_input_parser.h
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/browser/attribution_reporting/attribution_trigger.h"
 #include "content/browser/attribution_reporting/storable_source.h"
+#include "net/cookies/canonical_cookie.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
+#include "url/gurl.h"
 
 namespace content {
 
@@ -24,8 +26,14 @@ struct AttributionTriggerAndTime {
   base::Time time;
 };
 
-using AttributionSimulationEvent =
-    absl::variant<StorableSource, AttributionTriggerAndTime>;
+struct AttributionSimulatorCookie {
+  net::CanonicalCookie cookie;
+  GURL source_url;
+};
+
+using AttributionSimulationEvent = absl::variant<StorableSource,
+                                                 AttributionTriggerAndTime,
+                                                 AttributionSimulatorCookie>;
 
 // The value is the raw JSON associated with the event.
 using AttributionSimulationEventAndValue =
--- tools/attribution_reporting/simulator_main.cc
+++ tools/attribution_reporting/simulator_main.cc
@@ -42,6 +42,7 @@ constexpr char kSwitchRandomizedResponseRateNavigation[] =
 constexpr char kSwitchRandomizedResponseRateEvent[] =
     "randomized_response_rate_event";
 constexpr char kSwitchRemoveAssembledReport[] = "remove_assembled_report";
+constexpr char kSwitchSkipDebugCookieChecks[] = "skip_debug_cookie_checks";
 
 constexpr const char* kAllowedSwitches[] = {
     kSwitchHelp,
@@ -58,6 +59,7 @@ constexpr const char* kAllowedSwitches[] = {
     kSwitchReportTimeFormat,
     kSwitchRandomizedResponseRateNavigation,
     kSwitchRandomizedResponseRateEvent,
+    kSwitchSkipDebugCookieChecks,
 };
 
 constexpr char kHelpMsg[] = R"(
@@ -72,6 +74,7 @@ attribution_reporting_simulator
   [--remove_report_ids]
   [--report_time_format=<format>]
   [--remove_assembled_report]
+  [--skip_debug_cookie_checks]
 
 attribution_reporting_simulator is a command-line tool that simulates the
 Attribution Reporting API for for sources and triggers specified in an input
@@ -166,6 +169,9 @@ Switches:
                               aggregatable report bodies, as they are randomly
                               generated. Use this switch to make the tool's
                               output more deterministic.
+
+  --skip_debug_cookie_checks
+                            - Optional. If present, skips debug cookie checks.
 
   --version                 - Outputs the tool version and exits.
 
@@ -370,6 +376,8 @@ int main(int argc, char* argv[]) {
       .report_time_format = report_time_format,
       .remove_assembled_report =
           command_line.HasSwitch(kSwitchRemoveAssembledReport),
+      .skip_debug_cookie_checks =
+          command_line.HasSwitch(kSwitchSkipDebugCookieChecks),
   });
 
   content::AttributionSimulatorEnvironment env(argc, argv);
