b4644788d3d5b1aa1f06e1ac9fd0f7f06ef6fe24
Colin Kincaid <ckincaid@chromium.org>
2022-04-26 18:44:01 +0000

    Verify that is-google-photos image downloads are for Google Photos URLs
    Per FIFE documentation (http://shortn/_pIK5mppnGt), image URLs served by
    the Google Photos server have ggpht, google, or googleusercontent hosts.
    This CL adds a check in SanitizedImageSource to make sure that images
    being downloaded with the is-google-photos auth token option are
    actually Google Photos images.
    
    Fixed: b/230473348
    Change-Id: I96c231f818fda7653889d48b479ab5c4e0c6b169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607155
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996299}

--- chrome/browser/ui/webui/sanitized_image_source.cc
+++ chrome/browser/ui/webui/sanitized_image_source.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -59,6 +60,20 @@ std::map<std::string, std::string> ParseParams(
                        base::StringPiece16(output.data(), output.length()))});
   }
   return params;
+}
+
+bool IsGooglePhotosUrl(const GURL& url) {
+  static const char* const kGooglePhotosHostSuffixes[] = {
+      ".ggpht.com",
+      ".google.com",
+      ".googleusercontent.com",
+  };
+
+  for (const char* const suffix : kGooglePhotosHostSuffixes) {
+    if (base::EndsWith(url.host_piece(), suffix))
+      return true;
+  }
+  return false;
 }
 
 }  // namespace
@@ -110,8 +125,10 @@ void SanitizedImageSource::StartDataRequest(
     image_url = GURL(url_it->second);
 
     auto google_photos_it = params.find("isGooglePhotos");
-    if (google_photos_it != params.end() && google_photos_it->second == "true")
+    if (google_photos_it != params.end() &&
+        google_photos_it->second == "true" && IsGooglePhotosUrl(image_url)) {
       send_auth_token = true;
+    }
   }
 
   // Download the image body.
