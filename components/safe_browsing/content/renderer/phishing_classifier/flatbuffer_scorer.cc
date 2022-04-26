46b05742b23dcbfbcbc8d255f99df370d04e1a2a
Daniel Rubery <drubery@chromium.org>
2022-04-26 17:42:47 +0000

    Join Client Side Phishing metadata on class_name rather than index
    The server-side team would prefer to join in this manner. This change is
    backward compatible since existing models all use the same class names
    in the same order.
    
    Bug: 1319581
    Change-Id: I973d060ee0d30927e28046bee55e7bc61453216d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606648
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996259}

--- components/safe_browsing/content/renderer/phishing_classifier/flatbuffer_scorer.cc
+++ components/safe_browsing/content/renderer/phishing_classifier/flatbuffer_scorer.cc
@@ -179,7 +179,8 @@ double FlatBufferModelScorer::ComputeScore(const FeatureMap& features) const {
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
 void FlatBufferModelScorer::ApplyVisualTfLiteModel(
     const SkBitmap& bitmap,
-    base::OnceCallback<void(std::vector<double>)> callback) const {
+    base::OnceCallback<void(base::flat_map<std::string, double>)> callback)
+    const {
   DCHECK(content::RenderThread::IsMainThread());
   if (visual_tflite_model_.IsValid()) {
     base::Time start_post_task_time = base::Time::Now();
@@ -197,7 +198,7 @@ void FlatBufferModelScorer::ApplyVisualTfLiteModel(
         "SBClientPhishing.TfLiteModelLoadTime.FlatbufferScorer",
         base::Time::Now() - start_post_task_time);
   } else {
-    std::move(callback).Run(std::vector<double>());
+    std::move(callback).Run(base::flat_map<std::string, double>());
   }
 }
 #endif
--- components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.h
+++ components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.h
@@ -50,7 +50,8 @@ class ProtobufModelScorer : public Scorer {
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
   void ApplyVisualTfLiteModel(
       const SkBitmap& bitmap,
-      base::OnceCallback<void(std::vector<double>)> callback) const override;
+      base::OnceCallback<void(base::flat_map<std::string, double>)> callback)
+      const override;
 #endif
 
   int model_version() const override;
