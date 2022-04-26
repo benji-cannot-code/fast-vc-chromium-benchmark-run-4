863f04e7370edeb145fcd57796c80a7e7a1b7577
Alan Screen <awscreen@chromium.org>
2022-04-26 05:40:26 +0000

    Cleanup printing features include guard for field trial params
    This appears to have been missed https://crrev.com/921724.
    
    Change-Id: I3c2fbb7f9c08f3c74824f06f27eadd1fcaeed521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601416
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996012}

--- printing/printing_features.cc
+++ printing/printing_features.cc
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "printing/buildflags/buildflags.h"
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
 #include "base/metrics/field_trial_params.h"
 #endif
 
