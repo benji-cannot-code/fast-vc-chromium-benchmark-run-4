56b518a88df1a71b8bd723c953d6cad986232b6b
Anastasia Helfinstein <anastasi@google.com>
2022-04-26 01:42:27 +0000

    [ChromeVox] Move PanelNodeMenu logic to the background context
    As part of the migration to manifest v3, reliance on the background
    page must be eliminated. To that end, this change moves the logic for
    incremental search to the background, and uses message passing to
    communicate with the UI.
    
    This change moves the calculation of the node menus in the panel into a
    new background class, which sends messages to populate the menus.
    
    AX-Relnotes: n/a.
    Bug: 1288634
    Test: browser_tests --gtest_filter=*ChromeVox*Panel*
    
    Change-Id: I250d80ae10a68b44a4871a8b96834979dba2e850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3603268
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#995904}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/background_bridge.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/background_bridge.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 goog.provide('BackgroundBridge');
 
 goog.require('BridgeHelper');
+goog.require('PanelNodeMenuData');
 
 BackgroundBridge.BrailleBackground = {
   /**
@@ -79,5 +80,27 @@ BackgroundBridge.PanelBackground = {
 
   async setRangeToISearchNode() {
     return BridgeHelper.sendMessage('PanelBackground', 'setRangeToISearchNode');
+  },
+};
+
+BackgroundBridge.PanelNodeMenuBackground = {
+  /**
+   * @param {string=} opt_activateMenuTitle
+   * @return {!Promise<!Array<!PanelNodeMenuData>>}
+   */
+  async createAllPanelNodeMenuData(opt_activateMenuTitle) {
+    return BridgeHelper.sendMessage(
+        'PanelNodeMenuBackground', 'createAllPanelNodeMenuData',
+        opt_activateMenuTitle);
+  },
+
+  /**
+   * @param {number} menuId
+   * @param {number} callbackId
+   */
+  async performMenuActionCallback(menuId, callbackId) {
+    return BridgeHelper.sendMessage(
+        'PanelNodeMenuBackground', 'performMenuActionCallback',
+        {menuId, callbackId});
   },
 };
---
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/panel_node_menu_data.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Structures to hold the data for the panel's node menus.
+ */
+goog.provide('PanelNodeMenuData');
+goog.provide('PanelNodeMenuItemData');
+
+PanelNodeMenuData = class {
+  /**
+   * @param {string} title
+   * @param {number} menuId
+   */
+  constructor(title, menuId) {
+    /** @public {string} */
+    this.title = title;
+    /** @public {number} */
+    this.menuId = menuId;
+  }
+};
+
+PanelNodeMenuItemData = class {
+  /**
+   * @param {string} title
+   * @param {number} callbackId
+   * @param {boolean} isActive True when the menu was explicitly activated and
+   *     the node is focused.
+   */
+  constructor(title, callbackId, isActive) {
+    /** @public {string} */
+    this.title = title;
+    /** @public {number} */
+    this.callbackId = callbackId;
+    /** @public {boolean} */
+    this.isActive = isActive;
+  }
+};
