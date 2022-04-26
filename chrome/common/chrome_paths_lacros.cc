1ab6584b73a15214f8688a2d1765971b942308f0
Eriko Kurimoto <elkurin@chromium.org>
2022-04-26 05:21:56 +0000

    [Resource file sharing]: Store some lacros init params inside chromeos/lacros
    This CL lets chromeos/lacros/lacros_paths.h provide ash resource dir
    and user data dir paths to enable ui/base to access them.
    
    This is a part of resource file sharing work. These params will be used
    from ui/base/resource/resource_bundle_lacros in the following CLs.
    
    Bug: 1253280
    Change-Id: I1b5faab1fff998927f58432ca00db29f3b97f88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602350
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996005}

--- chrome/common/chrome_paths_lacros.cc
+++ chrome/common/chrome_paths_lacros.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chromeos/crosapi/cpp/crosapi_constants.h"
 #include "chromeos/crosapi/mojom/crosapi.mojom.h"
+#include "chromeos/lacros/lacros_paths.h"
 
 namespace chrome {
 namespace {
@@ -25,7 +26,6 @@ struct DefaultPaths {
   base::FilePath removable_media_dir;
   base::FilePath android_files_dir;
   base::FilePath linux_files_dir;
-  base::FilePath ash_resources_dir;
   base::FilePath share_cache_dir;
 };
 
@@ -56,7 +56,10 @@ void SetLacrosDefaultPaths(const base::FilePath& documents_dir,
   GetDefaultPaths().removable_media_dir = removable_media_dir;
   GetDefaultPaths().android_files_dir = android_files_dir;
   GetDefaultPaths().linux_files_dir = linux_files_dir;
-  GetDefaultPaths().ash_resources_dir = ash_resources_dir;
+
+  // As for ash resources path, set to chromeos::lacros_paths.
+  chromeos::lacros_paths::SetAshResourcesPath(ash_resources_dir);
+
   GetDefaultPaths().share_cache_dir = share_cache_dir;
 }
 
@@ -181,13 +184,6 @@ bool GetLinuxFilesPath(base::FilePath* result) {
   if (GetDefaultPaths().linux_files_dir.empty())
     return false;
   *result = GetDefaultPaths().linux_files_dir;
-  return true;
-}
-
-bool GetAshResourcesPath(base::FilePath* result) {
-  if (GetDefaultPaths().ash_resources_dir.empty())
-    return false;
-  *result = GetDefaultPaths().ash_resources_dir;
   return true;
 }
 
