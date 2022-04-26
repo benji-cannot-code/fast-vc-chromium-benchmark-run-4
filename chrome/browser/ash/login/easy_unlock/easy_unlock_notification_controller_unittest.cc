6d48c2adc7be90fdd88f6bfe8ea69a3a72140361
Brittany Hartmire <bhartmire@google.com>
2022-04-26 22:36:51 +0000

    [SmartLock] Show Sign in with Smart Lock removed notification
    Show one-time notification to relevant users explaining Sign in with
    Smart Lock was removed, but they can still use Smart Lock to unlock
    Chromebook.
    
    Screenshot: https://screenshot.googleplex.com/5mh85B3A6RVfsnW
    
    TEST= Manually tested on an atlas DUT and added new unittests inside
    EasyUnlockNotificationControllerTest.
    
    Change-Id: I90d812945badbdbb354eede31a64c28e1aa489ff
    Bug: b/227674947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606153
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#996409}

--- chrome/browser/ash/login/easy_unlock/easy_unlock_notification_controller_unittest.cc
+++ chrome/browser/ash/login/easy_unlock/easy_unlock_notification_controller_unittest.cc
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_notification_controller.h"
 
+#include "ash/components/proximity_auth/proximity_auth_pref_names.h"
+#include "ash/constants/ash_features.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notification_types.h"
@@ -32,6 +37,7 @@ class TestableNotificationController : public EasyUnlockNotificationController {
 
   // EasyUnlockNotificationController:
   MOCK_METHOD0(LaunchEasyUnlockSettings, void());
+  MOCK_METHOD0(LaunchMultiDeviceSettings, void());
   MOCK_METHOD0(LockScreen, void());
 };
 
@@ -60,6 +66,70 @@ class EasyUnlockNotificationControllerTest : public BrowserWithTestWindowTest {
       notification_controller_;
   std::unique_ptr<NotificationDisplayServiceTester> display_service_;
 };
+
+TEST_F(EasyUnlockNotificationControllerTest,
+       TestShouldShowSignInRemovedNotification) {
+  TestingProfile* test_profile = profile();
+  PrefService* pref_service = test_profile->GetPrefs();
+
+  ASSERT_FALSE(
+      EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          test_profile));
+
+  // Check returns false when kSmartLockSignInRemoved isn't enabled.
+  pref_service->SetBoolean(
+      proximity_auth::prefs::kProximityAuthIsChromeOSLoginEnabled, true);
+  pref_service->SetBoolean(prefs::kHasSeenSmartLockSignInRemovedNotification,
+                           false);
+  ASSERT_FALSE(
+      EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          test_profile));
+
+  // Check returns false when kHasSeenSmartLockSignInRemovedNotification is
+  // true.
+  base::test::ScopedFeatureList feature_list(
+      ash::features::kSmartLockSignInRemoved);
+  pref_service->SetBoolean(prefs::kHasSeenSmartLockSignInRemovedNotification,
+                           true);
+  ASSERT_FALSE(
+      EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          test_profile));
+
+  // Check returns false when kProximityAuthIsChromeOSLoginEnabled is false.
+  pref_service->SetBoolean(
+      proximity_auth::prefs::kProximityAuthIsChromeOSLoginEnabled, false);
+  pref_service->SetBoolean(prefs::kHasSeenSmartLockSignInRemovedNotification,
+                           false);
+  ASSERT_FALSE(
+      EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          test_profile));
+
+  pref_service->SetBoolean(
+      proximity_auth::prefs::kProximityAuthIsChromeOSLoginEnabled, true);
+  ASSERT_TRUE(
+      EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          test_profile));
+}
+
+TEST_F(EasyUnlockNotificationControllerTest,
+       TestShowSignInRemovedNotification) {
+  base::test::ScopedFeatureList feature_list(
+      ash::features::kSmartLockSignInRemoved);
+  const char kNotificationId[] = "easyunlock_notification_ids.sign_in_removed";
+
+  notification_controller_->ShowSignInRemovedNotification();
+  absl::optional<message_center::Notification> notification =
+      display_service_->GetNotification(kNotificationId);
+  ASSERT_TRUE(notification);
+
+  // Clicking notification button should launch settings.
+  EXPECT_CALL(*notification_controller_, LaunchMultiDeviceSettings());
+  notification->delegate()->Click(0, absl::nullopt);
+
+  // Clicking the notification itself should also launch settings.
+  EXPECT_CALL(*notification_controller_, LaunchMultiDeviceSettings());
+  notification->delegate()->Click(absl::nullopt, absl::nullopt);
+}
 
 TEST_F(EasyUnlockNotificationControllerTest,
        TestShowChromebookAddedNotification) {
--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/chrome_restart_request.h"
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_key_manager.h"
+#include "chrome/browser/ash/login/easy_unlock/easy_unlock_notification_controller.h"
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_service.h"
 #include "chrome/browser/ash/login/existing_user_controller.h"
 #include "chrome/browser/ash/login/helper.h"
@@ -1935,6 +1936,13 @@ void UserSessionManager::ShowNotificationsIfNeeded(Profile* profile) {
       ->browser_policy_connector_ash()
       ->GetAdbSideloadingAllowanceModePolicyHandler()
       ->ShowAdbSideloadingPolicyChangeNotificationIfNeeded();
+
+  if (EasyUnlockNotificationController::ShouldShowSignInRemovedNotification(
+          profile)) {
+    easy_unlock_notification_controller_ =
+        std::make_unique<EasyUnlockNotificationController>(profile);
+    easy_unlock_notification_controller_->ShowSignInRemovedNotification();
+  }
 }
 
 void UserSessionManager::MaybeLaunchSettings(Profile* profile) {
@@ -2367,6 +2375,7 @@ void UserSessionManager::Shutdown() {
   token_observers_.clear();
   always_on_vpn_manager_.reset();
   u2f_notification_.reset();
+  easy_unlock_notification_controller_.reset();
   help_app_notification_controller_.reset();
   password_service_voted_.reset();
   password_was_saved_ = false;
