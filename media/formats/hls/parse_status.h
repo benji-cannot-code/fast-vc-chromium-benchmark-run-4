5a0a2a424533fe3794d05b9b27f58b9561892075
Will Cassella <cassew@chromium.org>
2022-04-26 17:46:49 +0000

    HLS: Implement the decimal-resolution type
    This CL implements the decimal-resolution type, as well as test
    coverage. It also improves documentation on existing HLS types with
    short descriptions and spec references where applicable.
    
    Bug: 1266991
    Change-Id: I2413eadfb83d878e1ed47a3d7857469d60a2f8ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600854
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996261}

--- media/formats/hls/parse_status.h
+++ media/formats/hls/parse_status.h
@@ -19,6 +19,7 @@ enum class ParseStatusCode : StatusCodeType {
   kFailedToParseDecimalInteger,
   kFailedToParseDecimalFloatingPoint,
   kFailedToParseSignedDecimalFloatingPoint,
+  kFailedToParseDecimalResolution,
   kFailedToParseQuotedString,
   kInvalidPlaylistVersion,
   kUnknownPlaylistType,
