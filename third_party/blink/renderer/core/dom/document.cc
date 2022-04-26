d11336a6c3f6cbc19a4e664495c63ee7f53c7044
Dave Tapuska <dtapuska@chromium.org>
2022-04-26 14:17:26 +0000

    Change some assumptions on main frame.
    Adjust meta tag handling to only be on the outermost main frame. These
    features are experimental and are intended to only be influenced the
    outermost frame.
    
    Adjust vertical scroll blocking as well. This seems to be an orphaned
    feature policy that is intending on being applied to prevent scrolling.
    
    BUG=1314858
    
    Change-Id: I7491e7b466bcbbdfc10ed9ae2618f26e40d2e558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605750
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996154}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -832,7 +832,7 @@ Document::Document(const DocumentInit& initializer,
   }
 
   is_vertical_scroll_enforced_ =
-      GetFrame() && !GetFrame()->IsMainFrame() &&
+      GetFrame() && !GetFrame()->IsOutermostMainFrame() &&
       RuntimeEnabledFeatures::ExperimentalPoliciesEnabled() &&
       !dom_window_->IsFeatureEnabled(
           mojom::blink::PermissionsPolicyFeature::kVerticalScroll);
@@ -6886,7 +6886,7 @@ void Document::BatterySavingsMetaChanged() {
   if (!RuntimeEnabledFeatures::BatterySavingsMetaEnabled(GetExecutionContext()))
     return;
 
-  if (!IsInMainFrame())
+  if (!IsInOutermostMainFrame())
     return;
 
   UseCounter::Count(GetExecutionContext(), WebFeature::kBatterySavingsMeta);
@@ -6911,7 +6911,7 @@ void Document::BatterySavingsMetaChanged() {
 }
 
 void Document::SupportsReducedMotionMetaChanged() {
-  if (!IsInMainFrame())
+  if (!IsInOutermostMainFrame())
     return;
 
   auto* root_element = documentElement();
