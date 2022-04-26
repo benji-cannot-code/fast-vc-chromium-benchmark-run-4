4ee3e5679228e7b5bfc9f07507ee25c7e19d15ff
Emilia Paz <emiliapaz@chromium.org>
2022-04-26 01:52:44 +0000

    [Extensions c2s] Add hover card as tooltip for request access button
    Request access button tooltip has icons and text, and therefore can't
    use the button text tooltip.
    
    Mock:
    https://drive.google.com/file/d/1-dCeP9bvQHzb1dORClESCUMKHawXwTay/view?usp=sharing
    
    Screenshots:
    https://drive.google.com/file/d/1kOTa9JOcixseUu-nFHWrfIF5UZ2wAIcy/view?usp=sharing
    https://drive.google.com/file/d/1CwU5lSB6DN_QWVdDuKIMmiv-tYOeUOdT/view?usp=sharing
    
    Bug: 1239772
    Change-Id: Id08d29a9e7cb8d77d38ab8cc1d5c2ba861c68d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574758
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995923}

---
+++ chrome/browser/ui/views/extensions/extensions_request_access_button_hover_card.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_REQUEST_ACCESS_BUTTON_HOVER_CARD_H_
+#define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_REQUEST_ACCESS_BUTTON_HOVER_CARD_H_
+
+#include "content/public/browser/web_contents.h"
+#include "ui/views/view.h"
+
+class ToolbarActionViewController;
+
+class ExtensionsRequestAccessButtonHoverCard {
+ public:
+  // Creates and shows the request access button bubble.
+  static void ShowBubble(content::WebContents* web_contents,
+                         views::View* anchor_view,
+                         std::vector<ToolbarActionViewController*> actions);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_REQUEST_ACCESS_BUTTON_HOVER_CARD_H_
