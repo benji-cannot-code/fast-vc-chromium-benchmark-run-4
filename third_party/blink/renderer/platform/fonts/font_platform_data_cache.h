c03dec90be092ac58c6a298228e51178ff5a88fb
Yoshifumi Inoue <yosin@chromium.org>
2022-04-27 07:11:55 +0000

    [parallel-text-shaping] Prepare FontPlatformDataCache thread-safe
    This patch changes `FontPlatformDataCache` to be thread-safe by using:
      * `LockForParallelTextShaping` will be `base::Lock`
    for preparation of parallel text shaping[1].
    
    [1] http://crrev.com/c/3211416 Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: Iadd97fd75c0b69bcb748e41ae58ff201852ed86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3593522
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996571}

--- third_party/blink/renderer/platform/fonts/font_platform_data_cache.h
+++ third_party/blink/renderer/platform/fonts/font_platform_data_cache.h
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_FONT_PLATFORM_DATA_CACHE_H_
 
 #include "third_party/blink/renderer/platform/fonts/font_cache_key.h"
+#include "third_party/blink/renderer/platform/fonts/lock_for_parallel_text_shaping.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
-#include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 
 namespace blink {
 
@@ -65,9 +65,9 @@ class FontPlatformDataCache final {
       const FontFaceCreationParams& creation_params,
       AlternateFontName alternate_font_name);
 
-  size_t ByteSize() const;
-  void Clear();
-  void Purge(const FontDataCache& font_data_cache);
+  size_t ByteSize() const LOCKS_EXCLUDED(lock_);
+  void Clear() LOCKS_EXCLUDED(lock_);
+  void Purge(const FontDataCache& font_data_cache) LOCKS_EXCLUDED(lock_);
 
  private:
   // `SizedFontPlatformDataSet` maps rounded font size to `FontPlatformData`.
@@ -91,12 +91,13 @@ class FontPlatformDataCache final {
         const FontFaceCreationParams& creation_params,
         float size,
         AlternateFontName alternate_font_name,
-        unsigned rounded_size);
+        unsigned rounded_size) LOCKS_EXCLUDED(lock_);
 
     // Returns true if `map_` is empty.
-    bool Purge(const FontDataCache& font_data_cache);
+    bool Purge(const FontDataCache& font_data_cache) LOCKS_EXCLUDED(lock_);
 
-    void Set(unsigned rounded_size, FontPlatformData* platform_data);
+    void Set(unsigned rounded_size, FontPlatformData* platform_data)
+        LOCKS_EXCLUDED(lock_);
 
    private:
     using SizeToDataMap = HashMap<unsigned,
@@ -106,12 +107,17 @@ class FontPlatformDataCache final {
 
     SizedFontPlatformDataSet();
 
+    mutable LockForParallelTextShaping lock_;
     SizeToDataMap size_to_data_map_;
   };
 
-  SizedFontPlatformDataSet& GetOrCreateSizeMap(const FontCacheKey& key);
+  SizedFontPlatformDataSet& GetOrCreateSizeMap(const FontCacheKey& key)
+      LOCKS_EXCLUDED(lock_);
 
-  HashMap<FontCacheKey, scoped_refptr<SizedFontPlatformDataSet>> map_;
+  mutable LockForParallelTextShaping lock_;
+  HashMap<FontCacheKey, scoped_refptr<SizedFontPlatformDataSet>> map_
+      GUARDED_BY(lock_);
+  ;
 
   // A maximum float value to which we limit incoming font sizes. This is the
   // smallest float so that multiplying it by
