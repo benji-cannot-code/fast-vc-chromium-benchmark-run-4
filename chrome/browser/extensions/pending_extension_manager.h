098eeff6b89db715702c185d2df8b1076527eb10
Oleg Davydov <burunduk@chromium.org>
2022-04-27 08:54:53 +0000

    [Extensions] Use map in PendingExtensionManager
    The PendingExtensionManager class has a set of extensions we would
    like to install or are installing currently. It uses std::list to store
    this set, but has logic that only one item with given extension ID
    should exist.
    
    Therefore a map (ID->info) will be more suitable in this class, it also
    can give some performance benefits when searching by ID.
    
    Bug: 1319426
    Change-Id: I948719863cb115713e858fc6e16f22b5a78bfe2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605550
    Auto-Submit: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996591}

--- chrome/browser/extensions/pending_extension_manager.h
+++ chrome/browser/extensions/pending_extension_manager.h
@@ -125,8 +125,6 @@ class PendingExtensionManager {
   std::list<std::string> GetPendingIdsForUpdateCheck() const;
 
  private:
-  typedef std::list<PendingExtensionInfo> PendingExtensionList;
-
   // Assumes an extension with id |id| is not already installed.
   // Return true if the extension was added.
   bool AddExtensionImpl(
@@ -153,7 +151,7 @@ class PendingExtensionManager {
   // The BrowserContext with which the manager is associated.
   raw_ptr<content::BrowserContext> context_;
 
-  PendingExtensionList pending_extension_list_;
+  std::map<std::string, PendingExtensionInfo> pending_extensions_;
 
   absl::optional<base::flat_set<std::string>>
       migrating_default_chrome_app_ids_cache_;
