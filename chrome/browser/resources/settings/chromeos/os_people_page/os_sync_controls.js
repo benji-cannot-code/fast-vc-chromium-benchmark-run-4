0f7f20defb9842732be7deb969f4ec26a9cadfeb
Xiaohui Chen <xiaohuic@google.com>
2022-04-27 18:54:02 +0000

    os settings: fix wallpaper sync dependency
    Previously wallpaper sync depends on apps sync, now it depends on
    settings sync. Update the os settings sync logic to reflect that.
    
    Bug: 967987
    Test: manually check turning on settings sync affects wallpaper sync
    Change-Id: Idc66880b89ade2735f52107595a7e96a02bd4cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607020
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996812}

--- chrome/browser/resources/settings/chromeos/os_people_page/os_sync_controls.js
+++ chrome/browser/resources/settings/chromeos/os_people_page/os_sync_controls.js
@@ -142,7 +142,7 @@ Polymer({
     this.osSyncPrefs = osSyncPrefs;
 
     // If apps are not registered or synced, force wallpaper off.
-    if (!this.osSyncPrefs.osAppsRegistered || !this.osSyncPrefs.osAppsSynced) {
+    if (!this.osSyncPrefs.osPreferencesSynced) {
       this.set('osSyncPrefs.wallpaperEnabled', false);
     }
   },
@@ -192,13 +192,14 @@ Polymer({
   },
 
   /**
-   * Handler for changes to the apps sync state; apps have a special handler
-   * instead of relying on onSingleSyncDataTypeChanged_() because wallpaper has
-   * a dependency on apps.
+   * Handler for changes to the settings sync state; settings have a special
+   * handler instead of relying on onSingleSyncDataTypeChanged_() because
+   * wallpaper has a dependency on it.
    * @private
    */
-  onAppsSyncedChanged_() {
-    this.set('osSyncPrefs.wallpaperEnabled', this.osSyncPrefs.osAppsSynced);
+  onSettingsSyncedChanged_() {
+    this.set(
+        'osSyncPrefs.wallpaperEnabled', this.osSyncPrefs.osPreferencesSynced);
 
     this.onSingleSyncDataTypeChanged_();
   },
@@ -237,6 +238,6 @@ Polymer({
    */
   shouldWallpaperSyncSectionBeDisabled_() {
     return this.areDataTypeTogglesDisabled_ || !this.osSyncPrefs ||
-        !this.osSyncPrefs.osAppsSynced;
+        !this.osSyncPrefs.osPreferencesSynced;
   },
 });
