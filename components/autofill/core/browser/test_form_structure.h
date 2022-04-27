bcaaa9cf4fabb00d116dfbdb0874b508b4a6cb5f
Christos Froussios <cfroussios@chromium.org>
2022-04-27 11:32:53 +0000

    [Autofill] Add way to create test forms with shadow predictions
    Bug: 1310255
    Change-Id: I3e0568814d48fb4a879bcc25821ee54420a832b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602375
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996638}

--- components/autofill/core/browser/test_form_structure.h
+++ components/autofill/core/browser/test_form_structure.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "components/autofill/core/browser/form_parsing/field_candidates.h"
 #include "components/autofill/core/browser/form_structure.h"
 
 namespace autofill {
@@ -21,8 +22,21 @@ class TestFormStructure : public FormStructure {
 
   ~TestFormStructure() override;
 
+  // Set the heuristic and server types for each field. The `heuristic_types`
+  // and `server_types` vectors must be aligned with the indices of the fields
+  // in the form.
   void SetFieldTypes(const std::vector<ServerFieldType>& heuristic_types,
                      const std::vector<ServerFieldType>& server_types);
+
+  // Set the heuristic and server types for each field. The `heuristic_types`
+  // and `server_types` vectors must be aligned with the indices of the fields
+  // in the form. For each field in `heuristic_types` there must be exactly
+  // `PredictionSource::kDefaultHeuristics` prediction and any number of
+  // alternative predictions.
+  void SetFieldTypes(
+      const std::vector<std::vector<
+          std::pair<PredictionSource, ServerFieldType>>>& heuristic_types,
+      const std::vector<ServerFieldType>& server_types);
 };
 
 }  // namespace autofill
