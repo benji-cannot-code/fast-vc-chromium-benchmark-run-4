6e10cd10ca72c5151144728a306feeeafddc7a1d
siashah <siashah@chromium.org>
2022-04-27 00:28:15 +0000

    [Refactor] Autofill tests to use a common FakeModalDialogManager
    Bug: 1249597
    Change-Id: I85b68c8a42f326da3d4c083eb516cbdb544b1e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600991
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996444}

--- chrome/browser/ui/android/autofill/test/java/src/org/chromium/chrome/browser/ui/autofill/FakeModalDialogManager.java
+++ chrome/browser/ui/android/autofill/test/java/src/org/chromium/chrome/browser/ui/autofill/FakeModalDialogManager.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.autofill.settings;
+package org.chromium.chrome.browser.ui.autofill;
 
 import org.mockito.Mockito;
 
@@ -11,17 +11,14 @@ import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.modelutil.PropertyModel;
 
-// TODO (crbug/1249597): Move this to a more suitable common directory, and deduplicate in other
-// places.
-
 /**
  * A fake ModalDialogManager for use in tests involving modals.
  */
 public class FakeModalDialogManager extends ModalDialogManager {
     private PropertyModel mShownDialogModel;
 
-    public FakeModalDialogManager() {
-        super(Mockito.mock(Presenter.class), ModalDialogType.APP);
+    public FakeModalDialogManager(int modalDialogType) {
+        super(Mockito.mock(Presenter.class), modalDialogType);
     }
 
     @Override
