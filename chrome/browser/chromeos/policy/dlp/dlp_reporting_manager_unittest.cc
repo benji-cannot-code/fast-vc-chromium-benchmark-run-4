0c4e8a21494254606e2674425afdc4c30c4f51d5
Jan Kopański <jkopanski@google.com>
2022-04-27 09:16:43 +0000

    Add test coverage for DlpReportingManager::events_reported()
    Bug: 1304196
    Change-Id: I67f0a18f645b6b6f540aca6a1a36a2fa78cc1866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606800
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Jan Kopański <jkopanski@google.com>
    Cr-Commit-Position: refs/heads/main@{#996598}

--- chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_unittest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_unittest.cc
@@ -98,6 +98,7 @@ TEST_F(DlpReportingManagerTest, ReportEvent) {
   manager_.ReportEvent(kCompanyPattern, DlpRulesManager::Restriction::kPrinting,
                        DlpRulesManager::Level::kBlock);
 
+  EXPECT_EQ(manager_.events_reported(), 1u);
   EXPECT_EQ(events_.size(), 1u);
   EXPECT_EQ(events_[0].source().url(), kCompanyPattern);
   EXPECT_FALSE(events_[0].has_destination());
@@ -111,6 +112,7 @@ TEST_F(DlpReportingManagerTest, ReportEventWithUrlDst) {
                        DlpRulesManager::Restriction::kClipboard,
                        DlpRulesManager::Level::kBlock);
 
+  EXPECT_EQ(manager_.events_reported(), 1u);
   EXPECT_EQ(events_.size(), 1u);
   EXPECT_EQ(events_[0].source().url(), kCompanyPattern);
   EXPECT_EQ(events_[0].destination().url(), dst_pattern);
@@ -135,6 +137,7 @@ TEST_F(DlpReportingManagerTest, ReportEventWithComponentDst) {
   ReportEventAndCheckComponent(
       DlpRulesManager::Component::kUnknownComponent,
       DlpPolicyEventDestination_Component_UNDEFINED_COMPONENT, 5u);
+  EXPECT_EQ(manager_.events_reported(), 6u);
 }
 
 TEST_F(DlpReportingManagerTest, MetricsReported) {
@@ -148,6 +151,7 @@ TEST_F(DlpReportingManagerTest, MetricsReported) {
                        DlpRulesManager::Restriction::kUnknownRestriction,
                        DlpRulesManager::Level::kWarn);
 
+  EXPECT_EQ(manager_.events_reported(), 3u);
   EXPECT_EQ(events_.size(), 3u);
   histogram_tester.ExpectUniqueSample(
       GetDlpHistogramPrefix() + dlp::kReportedEventStatus,
@@ -211,6 +215,7 @@ TEST_F(DlpReportingManagerTest, UserType) {
   ReportEventAndCheckUser(user_manager, active_directory_user_id,
                           active_directory_user,
                           DlpPolicyEvent_UserType_UNDEFINED_USER_TYPE, 7u);
+  EXPECT_EQ(manager_.events_reported(), 8u);
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
@@ -261,6 +266,7 @@ TEST_F(DlpReportingManagerTest, ReportEventError) {
 
   manager_.ReportEvent(kCompanyPattern, DlpRulesManager::Restriction::kPrinting,
                        DlpRulesManager::Level::kBlock);
+  EXPECT_EQ(manager_.events_reported(), 0u);
   EXPECT_EQ(events_.size(), 0u);
 }
 
@@ -285,6 +291,7 @@ TEST_F(DlpReportingManagerTest, OnEventEnqueuedError) {
   manager_.ReportEvent(kCompanyPattern, DlpRulesManager::Restriction::kPrinting,
                        DlpRulesManager::Level::kBlock);
 
+  EXPECT_EQ(manager_.events_reported(), 1u);
   EXPECT_EQ(events_.size(), 0u);
   histogram_tester.ExpectUniqueSample(
       GetDlpHistogramPrefix() + dlp::kReportedEventStatus,
