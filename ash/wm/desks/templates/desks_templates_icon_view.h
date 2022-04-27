31be7224d3cca475041067d154c7191e7a4382f0
Sammie Quon <sammiequon@chromium.org>
2022-04-27 01:51:23 +0000

    desk_templates: Fixed some a11y issues on the icon container.
    1. App count was read before app name. Fixed this by add the app icon
       before the app count label.
    2. Default icon is at the back, but may not be read last. Fixed this by
       alerting the a11y tree after reordering it to the back.
    3. Some icons are skipped. This is because they were having their icon
       identifier set as their a11y name. Pipes an app title from restore
       data or the window title to be the a11y name.
    4. We added a mechanism to keep the added template in the front and then
       sort it after the user changes the name. The a11y order was not
       updated in this case.
    
    Test: manual
    Change-Id: I0b4dbec8166cc6fd7016d8a07332987ac9b0c058
    Fixed: 1303182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605732
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996478}

--- ash/wm/desks/templates/desks_templates_icon_view.h
+++ ash/wm/desks/templates/desks_templates_icon_view.h
@@ -51,6 +51,7 @@ class DesksTemplatesIconView : public views::View {
   // all unavailable icons.
   void SetIconIdentifierAndCount(const std::string& icon_identifier,
                                  const std::string& app_id,
+                                 const std::string& app_title,
                                  int count,
                                  bool show_plus);
 
