4858d55848bbe11c8089dcff53522badb2f42ded
Kramer Ge <fangzhoug@chromium.org>
2022-04-27 18:00:15 +0000

    Revert "Reland "Map shared memory mappings backing ArrayBuffers into the V8 Sandbox""
    This reverts commit f6153ebbbacafccc10cb700b1cce9be056480490.
    
    Reason for revert: potential culprit of triggering dcheck in ~DataPipeProducerDispatcher(), crbug.com/1320321
    
    Original change's description:
    > Reland "Map shared memory mappings backing ArrayBuffers into the V8 Sandbox"
    >
    > This is a reland of commit e6b8260468792092e1914363c6e0556e616d76c9
    >
    > Original change's description:
    > > Map shared memory mappings backing ArrayBuffers into the V8 Sandbox
    > >
    > > When the V8 sandbox is enabled, ArrayBuffers are referenced through
    > > offsets rather than raw pointers to prevent their abuse by an exploit.
    > > This requires that all ArrayBuffer backing stores be mapped into the
    > > sandbox address space. As some ArrayBuffers, for example those created by
    > > MojoJS, are backed by shared memory, additional logic is required to map
    > > shared memory into the V8 sandbox. This CL achieves this as follows:
    > >
    > > 1. A new SharedMemoryMapper interface is introduced which is responsible
    > >    for mapping and unmapping shared memory regions. With that, it is
    > >    then possible to customize the mapping/unmapping logic by passing a
    > >    custom SharedMemoryMapper to *SharedMemoryRegion::Map.
    > > 2. A subclass, ArrayBufferSharedMemoryMapper is provided which maps the
    > >    shared memory regions into the V8 sandbox when that is enabled.
    > > 3. A new ArrayBufferContents constructor is created which accepts a
    > >    shared memory region as arguments and takes care of mapping it using
    > >    the appropriate SharedMemoryMapper. This constructor is now used when
    > >    mapping shared memory for MojoJS.
    > >
    > > Bug: 1218005
    > > Change-Id: Ifce853f85397f8395746904e6a4d3fe30ffd318f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596033
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > > Commit-Queue: Samuel Groß <saelo@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#996070}
    >
    > Bug: 1218005
    > Change-Id: Idef5953260ea2bdc04f59cd5b5bcf50340cb10d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608011
    > Auto-Submit: Samuel Groß <saelo@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#996469}
    
    Bug: 1218005
    Change-Id: Ib5424d7731a9a0608a8f13cf1fd843c91d432e91
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611148
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Owners-Override: Kramer Ge <fangzhoug@google.com>
    Cr-Commit-Position: refs/heads/main@{#996790}

--- gin/array_buffer.h
+++ gin/array_buffer.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/shared_memory_mapper.h"
 #include "gin/converter.h"
 #include "gin/gin_export.h"
 #include "v8/include/v8-array-buffer.h"
@@ -77,8 +76,6 @@ struct GIN_EXPORT Converter<ArrayBufferView> {
   static bool FromV8(v8::Isolate* isolate, v8::Local<v8::Value> val,
                      ArrayBufferView* out);
 };
-
-GIN_EXPORT base::SharedMemoryMapper* GetSharedMemoryMapperForArrayBuffers();
 
 }  // namespace gin
 
