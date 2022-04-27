f3dcc21b64b2a67215115b8736da8392d3a19fc5
Ed Chin <edchin@google.com>
2022-04-27 19:21:40 +0000

    [ios][while-browsing-IPH] Create tab helper
    This sets up the tab helper which will perform logic
    checks for the while-browsing-IPH after the user
    has loaded a page.
    
    Next CL will add the feature engagement tracker
    as well as a call to the follow provider to
    obtain the recommended status.
    
    Note: Since this is just a skeleton CL, unittests will be
    added in a future CL when functionality is added.
    
    Bug: 1318755
    Change-Id: I5da284c75b98248b507fba70e7081a046c6529af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3603802
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#996826}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/download/vcard_tab_helper.h"
 #include "ios/chrome/browser/favicon/favicon_service_factory.h"
 #import "ios/chrome/browser/find_in_page/find_tab_helper.h"
+#import "ios/chrome/browser/follow/follow_tab_helper.h"
 #include "ios/chrome/browser/history/history_service_factory.h"
 #include "ios/chrome/browser/history/history_tab_helper.h"
 #include "ios/chrome/browser/history/top_sites_factory.h"
@@ -49,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/language/url_language_histogram_factory.h"
 #import "ios/chrome/browser/link_to_text/link_to_text_tab_helper.h"
 #import "ios/chrome/browser/metrics/pageload_foreground_duration_tab_helper.h"
+#import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/open_in/open_in_tab_helper.h"
 #import "ios/chrome/browser/optimization_guide/optimization_guide_tab_helper.h"
@@ -241,5 +243,9 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
     HttpsOnlyModeUpgradeTabHelper::CreateForWebState(web_state);
     HttpsOnlyModeContainer::CreateForWebState(web_state);
     HttpsOnlyModeAllowlist::CreateForWebState(web_state);
+  }
+
+  if (IsWebChannelsEnabled()) {
+    FollowTabHelper::CreateForWebState(web_state);
   }
 }
