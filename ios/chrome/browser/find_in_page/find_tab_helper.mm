f044f897c0b3a7e5b36fdbd624148941789b07a8
Alexander Skvortsov <askvortsov@yandex-team.ru>
2022-04-27 23:05:56 +0000

    Fixed find in page hiding
    Same document navigation does not necessarily change DOM content of the page.
    It also can be triggered via js at arbitrary moment,
    even without explicit user gesture.
    In these cases find in page closing might be unexpected.
    
    R=rohitrao@chromium.org,thegreenfrog@chromium.org
    
    2. Start find in page feature in page menu
    3. Tap any of buttons after 'window.history.replaceState' label.
    Before fix: find in page closes
    After fix: find in page is still open
    
    TEST: 1. Go to https://ibro.best
    Change-Id: I64c77236a29bd0a11593dcdd4afde51593651139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563323
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996926}

--- ios/chrome/browser/find_in_page/find_tab_helper.mm
+++ ios/chrome/browser/find_in_page/find_tab_helper.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics_action.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_controller.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_model.h"
+#import "ios/web/public/navigation/navigation_context.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -103,6 +104,9 @@ void FindTabHelper::WebStateDestroyed(web::WebState* web_state) {
 void FindTabHelper::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
+  if (navigation_context->IsSameDocument()) {
+    return;
+  }
   if (IsFindUIActive()) {
     StopFinding();
   }
