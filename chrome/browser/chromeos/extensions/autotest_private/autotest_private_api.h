514454985258936d3005e79f32d1ca4ae45b04fa
Yuki Awano <yawano@google.com>
2022-04-27 22:25:25 +0000

    Wait DeviceApps config sync with enableAssistantAndWaitForReady
    - There is a short period where assistant status is READY but assistant
      hasn't received DeviceApps config bit. If assistant does not have the
      bit, assistant does not send Android application list to the server
      and query for opening an Android app fails.
    - This CL modifies the test API as it waits the config sync.
    
    Bug: b:228644597
    Test: tast run DUT assistant.OpenAndroidApp
    Change-Id: I94fc027d67b13402787d37554cc16903d6e9396e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592257
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Eric Sum <esum@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#996910}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -739,8 +739,7 @@ class AutotestPrivateSetAssistantEnabledFunction
 
 // Bring up the Assistant service, and wait until the ready signal is received.
 class AutotestPrivateEnableAssistantAndWaitForReadyFunction
-    : public ExtensionFunction,
-      public ash::AssistantStateObserver {
+    : public ExtensionFunction {
  public:
   AutotestPrivateEnableAssistantAndWaitForReadyFunction();
   DECLARE_EXTENSION_FUNCTION("autotestPrivate.enableAssistantAndWaitForReady",
@@ -750,15 +749,7 @@ class AutotestPrivateEnableAssistantAndWaitForReadyFunction
   ~AutotestPrivateEnableAssistantAndWaitForReadyFunction() override;
   ResponseAction Run() override;
 
-  void SubscribeToStatusChanges();
-
-  // ash::AssistantStateObserver overrides:
-  void OnAssistantStatusChanged(
-      chromeos::assistant::AssistantStatus status) override;
-
-  // A reference to keep |this| alive while waiting for the Assistant to
-  // respond.
-  scoped_refptr<ExtensionFunction> self_;
+  void OnInitializedInternal();
 };
 
 // Send text query to Assistant and return response.
