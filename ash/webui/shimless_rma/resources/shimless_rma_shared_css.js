59f64f49d64b5b370b68f64b5781fb9befd4d365
dpapad <dpapad@chromium.org>
2022-04-27 02:05:59 +0000

    CrOS SWAs: Fix incorrect imports in Polymer style modules, fixes fonts.
    1) Polymer style modules should only import dependencies using JS
       imports, not obsolete HTML imports which are no-op.
    2) External stylesheets should be imported at the top-level HTML
       document and not from shared Polymer style modules.
    
    These incorrect patterns were discovered after the CL at [1] landed,
    which modified how Polymer style modules are registered (as suggested by the Polymer team), and the incorrect patterns stopped working, where
    previously they were somewhat accidentally working.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3602650/
    
    Fixed: 1320046
    Change-Id: I0ad1e5112ad66722a914b09f644ce036af5b64ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608127
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996486}

--- ash/webui/shimless_rma/resources/shimless_rma_shared_css.js
+++ ash/webui/shimless_rma/resources/shimless_rma_shared_css.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/cr_elements/md_select_css.m.js';
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import './shimless_rma_fonts_css.js';
 
