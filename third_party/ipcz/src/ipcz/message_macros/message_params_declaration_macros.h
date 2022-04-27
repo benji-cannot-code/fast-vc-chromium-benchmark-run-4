ed8d43d1da282e33363a9984d433269319c6611c
Ken Rockot <rockot@google.com>
2022-04-27 23:58:46 +0000

    ipcz: Introduce message macros
    Split out from the ipcz uber-CL: https://crrev.com/c/3570271
    
    Defines an internal MessageBase structure and macros to generate
    subclasses for specific types of messages. These macros allow for
    convenience and legibility when defining internal control messages to
    manage ipcz internode affairs.
    
    Each message consists of a fixed-size parameter structure which is laid
    out by IPCZ_MSG_PARAM* macro invocations within an
    IPCZ_MSG_BEGIN/IPCZ_MSG_END block.
    
    Parameters may be simple inlined POD fields, serialized driver objects,
    or arrays of either. Arrays are represented by indirection with a 32-bit
    offset into the message payload.
    
    Enough support for versioning is in place to tolerate newer, larger
    versions of any defined messages, but the reverse (tolerating
    older-than-current versions) is not yet implemented since it isn't
    needed yet.
    
    This also adds some test-only messages, along with a bunch of tests to
    exercise them.
    
    Bug: 1299283
    Change-Id: Ie39543a2a5120d9da4a166aea55990cdaf1e6574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600687
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#996950}

---
+++ third_party/ipcz/src/ipcz/message_macros/message_params_declaration_macros.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// no-include-guard-because-multiply-included
+
+#define IPCZ_MSG_ID(x) static constexpr uint8_t kId = x
+#define IPCZ_MSG_VERSION(x) static constexpr uint32_t kVersion = x
+
+#define IPCZ_MSG_BEGIN(name, id_decl, version_decl) \
+  struct IPCZ_ALIGN(8) name##_Params {              \
+    name##_Params();                                \
+    ~name##_Params();                               \
+    id_decl;                                        \
+    version_decl;                                   \
+    internal::StructHeader header;
+
+#define IPCZ_MSG_END() \
+  }                    \
+  ;
+
+#define IPCZ_MSG_PARAM(type, name) type name;
+#define IPCZ_MSG_PARAM_ARRAY(type, name) uint32_t name;
+#define IPCZ_MSG_PARAM_DRIVER_OBJECT(name) internal::DriverObjectData name;
+#define IPCZ_MSG_PARAM_DRIVER_OBJECT_ARRAY(name) uint32_t name;
---
+++ third_party/ipcz/src/ipcz/test_messages.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ipcz/test_messages.h"
+
+#include "third_party/abseil-cpp/absl/types/span.h"
+
+namespace ipcz::test::msg {
+
+#pragma pack(push, 1)
+
+// clang-format off
+#include "ipcz/message_macros/message_params_definition_macros.h"
+#include "ipcz/test_messages_generator.h"
+#include "ipcz/message_macros/undef_message_macros.h"
+
+#include "ipcz/message_macros/message_definition_macros.h"
+#include "ipcz/test_messages_generator.h"
+#include "ipcz/message_macros/undef_message_macros.h"
+// clang-format on
+
+#pragma pack(pop)
+
+}  // namespace ipcz::test::msg
