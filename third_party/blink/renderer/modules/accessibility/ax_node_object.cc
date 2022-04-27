c6eece99b866339b787099cfe78007f00ba6f77c
Joanmarie Diggs <jdiggs@igalia.com>
2022-04-27 17:01:32 +0000

    Use IsLoadCompleted rather than the parser in AXNodeObject::IsLoaded
    AXNodeObject::IsLoaded was returning false if the document had a
    parser, regardless of its state (e.g. stopped, detached). Because we
    use IsLoaded to determine whether or not the "busy" state should be
    exposed on a document, timing and other accessibility events could
    result in our firing an accessible "load complete" event on a document
    which claims to be "busy" due to a parser which was stopped or detached
    but not yet destroyed.
    
    This commit stops relying upon the parser and returns false if
    Document::IsLoadCompleted is false. It also removes the DCHECK specific
    to the "busy" state being present when we fire the "load start" event
    in BrowserAccessibilityManagerAuraLinux. If a page quickly loads, it may
    no longer have the "busy" state. We still want to fire the event for
    interested consumers, but cannot count on the state still being present.
    
    AX-Relnotes: N/A
    
    Bug: 1318959, 1317591
    Change-Id: Iceaa5f8cbe91c5b17faf04711fd707c85c995b46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607953
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996754}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -1505,7 +1505,7 @@ bool AXNodeObject::IsLoaded() const {
   if (!GetDocument())
     return false;
 
-  if (GetDocument()->Parser())
+  if (!GetDocument()->IsLoadCompleted())
     return false;
 
   // Check for a navigation API single-page app navigation in progress.
