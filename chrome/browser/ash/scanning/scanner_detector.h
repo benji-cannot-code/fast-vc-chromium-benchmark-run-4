5a930e6c8b6c46a962a15be19c3c33157098ff67
Yeunjoo Choi <ychoi@igalia.com>
2022-04-27 06:41:55 +0000

    Add BUILD.gn in //chromeos/scanning
    This CL moves //chromeos/scanning files to its own BUILD.gn and makes it
    components target. So that we could simply move //chromeos/scanning to
    ash in the near future.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I815dcf778ae7ec8ca5638f1740e2572481b1fff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607531
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#996557}

--- chrome/browser/ash/scanning/scanner_detector.h
+++ chrome/browser/ash/scanning/scanner_detector.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "chromeos/chromeos_export.h"
 #include "chromeos/scanning/scanner.h"
 
 namespace ash {
--- chromeos/scanning/scanner.h
+++ chromeos/scanning/scanner.h
@@ -8,15 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/component_export.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
-#include "chromeos/chromeos_export.h"
 #include "net/base/ip_address.h"
 
 namespace chromeos {
 
 // The type of protocol used to communicate with a scanner.
-enum class CHROMEOS_EXPORT ScanProtocol {
+enum class COMPONENT_EXPORT(SCANNING) ScanProtocol {
   kUnknown,
   kEscl,           // eSCL protocol.
   kEscls,          // eSCLS protocol.
@@ -24,7 +24,7 @@ enum class CHROMEOS_EXPORT ScanProtocol {
   kLegacyUsb       // Non-eSCL(S), legacy USB protocol.
 };
 
-struct CHROMEOS_EXPORT ScannerDeviceName {
+struct COMPONENT_EXPORT(SCANNING) ScannerDeviceName {
   explicit ScannerDeviceName(const std::string& device_name);
   ScannerDeviceName(const std::string& device_name, bool usable);
   ~ScannerDeviceName();
@@ -45,7 +45,7 @@ struct CHROMEOS_EXPORT ScannerDeviceName {
   bool usable = true;
 };
 
-struct CHROMEOS_EXPORT Scanner {
+struct COMPONENT_EXPORT(SCANNING) Scanner {
   Scanner();
   ~Scanner();
 
