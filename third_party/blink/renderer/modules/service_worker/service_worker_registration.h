1a4e6db3c8a1fa7c76a313d4c7ed19e9c785b486
Hiroki Nakagawa <nhiroki@chromium.org>
2022-04-27 10:09:53 +0000

    Prerender: Defer ServiceWorkerRegistration#update from prerendered pages
    The spec requires ServiceWorkerRegistration#update to be deferred until
    page activation. This CL implements the behavior.
    https://wicg.github.io/nav-speculation/prerendering.html#patch-service-workers
    
    Bug: 1305120
    Change-Id: Iff68a9407586c238ef6a775a2829b2ada01f582f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607525
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996621}

--- third_party/blink/renderer/modules/service_worker/service_worker_registration.h
+++ third_party/blink/renderer/modules/service_worker/service_worker_registration.h
@@ -109,6 +109,9 @@ class ServiceWorkerRegistration final
       mojom::blink::ServiceWorkerUpdateViaCache update_via_cache) override;
   void UpdateFound() override;
 
+  void UpdateInternal(
+      mojom::blink::FetchClientSettingsObjectPtr mojom_settings_object,
+      ScriptPromiseResolver* resolver);
   void UnregisterInternal(ScriptPromiseResolver* resolver);
 
   Member<ServiceWorker> installing_;
