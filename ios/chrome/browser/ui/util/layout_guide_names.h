333d66e319eaab178a9b2ab73598155c28fe2cde
Louis Romero <lpromero@google.com>
2022-04-27 13:11:44 +0000

    [iOS] Fix some imports
    Bug: none
    Change-Id: Id7fe2f5d9816352a9451475b0efc5756e0ca9144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3609989
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996659}

--- ios/chrome/browser/ui/util/layout_guide_names.h
+++ ios/chrome/browser/ui/util/layout_guide_names.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_UTIL_LAYOUT_GUIDE_NAMES_H_
 #define IOS_CHROME_BROWSER_UI_UTIL_LAYOUT_GUIDE_NAMES_H_
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 typedef NSString GuideName;
 
--- ios/chrome/browser/ui/util/named_guide.h
+++ ios/chrome/browser/ui/util/named_guide.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_UTIL_NAMED_GUIDE_H_
 #define IOS_CHROME_BROWSER_UI_UTIL_NAMED_GUIDE_H_
 
+#import <UIKit/UIKit.h>
+
 #import "ios/chrome/browser/ui/util/layout_guide_names.h"
 
 // A UILayoutGuide subclass that represents the layout of a well-known piece of
