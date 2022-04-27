2e4f1fee053b395b5926547e1d3bf1d8b736c88f
Zack Han <zackhan@chromium.org>
2022-04-27 00:35:14 +0000

    Add access token to telemetry service request header.
    Fixed: 1316690
    
    Change-Id: I11c4344193d210570c6873960cffb493f40766a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553395
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996449}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_uploader.h
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_uploader.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
+#include "chrome/browser/profiles/profile.h"
 
 namespace network {
 class SharedURLLoaderFactory;
@@ -19,6 +20,8 @@ class SimpleURLLoader;
 }  // namespace network
 
 namespace safe_browsing {
+
+class SafeBrowsingTokenFetcher;
 
 // An uploader of extension telemetry reports. An upload is initiated by
 // creating an instance of this object and then calling its StartUpload method.
@@ -38,7 +41,8 @@ class ExtensionTelemetryUploader {
   ExtensionTelemetryUploader(
       OnUploadCallback callback,
       const scoped_refptr<network::SharedURLLoaderFactory>& url_loader_factory,
-      std::unique_ptr<std::string> upload_data);
+      std::unique_ptr<std::string> upload_data,
+      std::unique_ptr<SafeBrowsingTokenFetcher> token_fetcher);
 
   // Start the upload by sending a request. This method performs retries if
   // necessary and finally calls |callback_|. It must be called on the UI
@@ -48,8 +52,11 @@ class ExtensionTelemetryUploader {
   static std::string GetUploadURLForTest();
 
  private:
+  // Determines whether to send a request with access token.
+  void MaybeSendRequestWithAccessToken();
+
   // Sends a single network request.
-  void SendRequest();
+  void SendRequest(const std::string& access_token);
 
   // Callback when SimpleURLLoader gets the response.
   void OnURLLoaderComplete(std::unique_ptr<std::string> response_body);
@@ -66,6 +73,10 @@ class ExtensionTelemetryUploader {
   base::TimeDelta current_backoff_;
   int num_upload_retries_;
   base::TimeTicks upload_start_time_;
+  // The token fetcher used to attach OAuth access tokens to requests for
+  // appropriately consented users. It can be a nullptr when the user is
+  // not signed in.
+  std::unique_ptr<SafeBrowsingTokenFetcher> token_fetcher_;
 
   base::WeakPtrFactory<ExtensionTelemetryUploader> weak_factory_{this};
 };
