e209d92f190150e054a9ab632d5766bbf77fad7b
Michael Thiessen <mthiesse@chromium.org>
2022-04-27 01:50:14 +0000

    Fix UrlOverridingTest flakiness
    https://chromium-review.googlesource.com/c/chromium/src/+/3597651 tried
    to fix flakiness by clearing the previous external navigation result,
    but this races against automatic navigations like redirects and
    subframes. Instead I added a test-only callback and ignore the result
    for URLs the test doesn't care about.
    
    I had to re-write a CCT test for this change so I also de-flaked it and
    re-enabled it.
    
    Bug: 1319447, 1197727
    Change-Id: I15082db8e479617b22e8f03bf9be46d3e62b0a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605746
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996477}

--- components/external_intents/android/java/src/org/chromium/components/external_intents/InterceptNavigationDelegateImpl.java
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/InterceptNavigationDelegateImpl.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.external_intents;
 
+import android.util.Pair;
+
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
@@ -36,7 +39,7 @@ import org.chromium.url.Origin;
 public class InterceptNavigationDelegateImpl extends InterceptNavigationDelegate {
     private final AuthenticatorNavigationInterceptor mAuthenticatorHelper;
     private InterceptNavigationDelegateClient mClient;
-    private OverrideUrlLoadingResult mLastOverrideUrlLoadingResultForTesting;
+    private Callback<Pair<GURL, OverrideUrlLoadingResult>> mResultCallbackForTesting;
     private WebContents mWebContents;
     private ExternalNavigationHandler mExternalNavHandler;
 
@@ -92,19 +95,11 @@ public class InterceptNavigationDelegateImpl extends InterceptNavigationDelegate
                                                   .setIsMainFrame(true)
                                                   .build();
         OverrideUrlLoadingResult result = mExternalNavHandler.shouldOverrideUrlLoading(params);
-        mLastOverrideUrlLoadingResultForTesting = result;
+        if (mResultCallbackForTesting != null) {
+            mResultCallbackForTesting.onResult(Pair.create(url, result));
+        }
         return result.getResultType()
                 != ExternalNavigationHandler.OverrideUrlLoadingResultType.NO_OVERRIDE;
-    }
-
-    @VisibleForTesting
-    public void clearLastOverrideUrlLoadingResultForTests() {
-        mLastOverrideUrlLoadingResultForTesting = null;
-    }
-
-    @VisibleForTesting
-    public OverrideUrlLoadingResult getLastOverrideUrlLoadingResultForTests() {
-        return mLastOverrideUrlLoadingResultForTesting;
     }
 
     @Override
@@ -147,7 +142,9 @@ public class InterceptNavigationDelegateImpl extends InterceptNavigationDelegate
                 navigationHandle, redirectHandler, shouldCloseTab, escapedUrl)
                                                   .build();
         OverrideUrlLoadingResult result = mExternalNavHandler.shouldOverrideUrlLoading(params);
-        mLastOverrideUrlLoadingResultForTesting = result;
+        if (mResultCallbackForTesting != null) {
+            mResultCallbackForTesting.onResult(Pair.create(url, result));
+        }
 
         mClient.onDecisionReachedForNavigation(navigationHandle, result);
 
@@ -324,6 +321,12 @@ public class InterceptNavigationDelegateImpl extends InterceptNavigationDelegate
                 : R.string.unreachable_navigation_warning;
         mClient.getWebContents().addMessageToDevToolsConsole(ConsoleMessageLevel.WARNING,
                 ContextUtils.getApplicationContext().getString(resId, url.getSpec()));
+    }
+
+    @VisibleForTesting
+    public void setResultCallbackForTesting(
+            Callback<Pair<GURL, OverrideUrlLoadingResult>> callback) {
+        mResultCallbackForTesting = callback;
     }
 
     @NativeMethods
