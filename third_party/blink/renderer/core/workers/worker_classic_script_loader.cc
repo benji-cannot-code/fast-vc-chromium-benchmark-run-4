1e7570063a1e2bd685cba5c90ad02f50611ede56
Titouan Rigoudy <titouan@chromium.org>
2022-04-27 14:19:35 +0000

    [Private Network Access] Remove WorkerClassicScriptLoader address space.
    Also remove InstalledScriptsManager::ScriptData::ResponseAddressSpace().
    
    They are all useless since crrev.com/c/3599056.
    
    Fixed: chromium:1204028
    Change-Id: I2e3981fb5be62c5292ebe08b14db9ab433d52e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599016
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996686}

--- third_party/blink/renderer/core/workers/worker_classic_script_loader.cc
+++ third_party/blink/renderer/core/workers/worker_classic_script_loader.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/memory/scoped_refptr.h"
-#include "services/network/public/mojom/ip_address_space.mojom-blink.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -100,8 +99,7 @@ String CheckSameOriginEnforcement(const KURL& request_url,
 
 }  // namespace
 
-WorkerClassicScriptLoader::WorkerClassicScriptLoader()
-    : response_address_space_(network::mojom::IPAddressSpace::kPublic) {}
+WorkerClassicScriptLoader::WorkerClassicScriptLoader() {}
 
 void WorkerClassicScriptLoader::LoadSynchronously(
     ExecutionContext& execution_context,
@@ -232,7 +230,6 @@ void WorkerClassicScriptLoader::DidReceiveResponse(
   identifier_ = identifier;
   response_url_ = response.ResponseUrl();
   response_encoding_ = response.TextEncodingName();
-  response_address_space_ = response.AddressSpace();
 
   referrer_policy_ = response.HttpHeaderField(http_names::kReferrerPolicy);
   ProcessContentSecurityPolicy(response);
--- third_party/blink/renderer/core/workers/worker_classic_script_loader.h
+++ third_party/blink/renderer/core/workers/worker_classic_script_loader.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink-forward.h"
-#include "services/network/public/mojom/ip_address_space.mojom-blink-forward.h"
 #include "services/network/public/mojom/url_loader_factory.mojom-blink.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info_notifier.mojom-shared.h"
 #include "third_party/blink/public/platform/cross_variant_mojo_util.h"
@@ -123,10 +122,6 @@ class CORE_EXPORT WorkerClassicScriptLoader final
 
   const String& GetReferrerPolicy() const { return referrer_policy_; }
 
-  network::mojom::IPAddressSpace ResponseAddressSpace() const {
-    return response_address_space_;
-  }
-
   const Vector<String>* OriginTrialTokens() const {
     return origin_trial_tokens_.get();
   }
@@ -182,7 +177,6 @@ class CORE_EXPORT WorkerClassicScriptLoader final
   uint64_t identifier_ = 0;
   std::unique_ptr<Vector<uint8_t>> cached_metadata_;
   Member<ContentSecurityPolicy> content_security_policy_;
-  network::mojom::IPAddressSpace response_address_space_;
   std::unique_ptr<Vector<String>> origin_trial_tokens_;
   String referrer_policy_;
 
