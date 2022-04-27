23e6754b65781052faef846bc86c89be9ccfde9d
Kuan Huang <kuanhuang@chromium.org>
2022-04-27 19:30:53 +0000

    InteractionSequenceBrowserUtil::StateChange allow move constructor/assignment
    Implement move constructor assignment operator for
    InteractionSequenceBrowserUtil::StateChange that it can be generated from
    a helper function and moved for InteractionSequenceBrowserUtil::SendEventOnStateChange
    
    Bug: 1309204
    Change-Id: Id212057bd5f4a5efe5097f931e3737e2844f99fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588953
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996833}

--- chrome/test/interaction/interaction_sequence_browser_util.cc
+++ chrome/test/interaction/interaction_sequence_browser_util.cc
@@ -208,6 +208,11 @@ std::string CreateDeepQuery(
 }  // namespace
 
 InteractionSequenceBrowserUtil::StateChange::StateChange() = default;
+InteractionSequenceBrowserUtil::StateChange::StateChange(
+    InteractionSequenceBrowserUtil::StateChange&& other) = default;
+InteractionSequenceBrowserUtil::StateChange&
+InteractionSequenceBrowserUtil::StateChange::operator=(
+    InteractionSequenceBrowserUtil::StateChange&& other) = default;
 InteractionSequenceBrowserUtil::StateChange::~StateChange() = default;
 
 class InteractionSequenceBrowserUtil::NewTabWatcher
