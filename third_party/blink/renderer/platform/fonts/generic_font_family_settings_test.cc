043098e30dd650657d429d2e99323bad870004e9
Frédéric Wang <fwang@igalia.com>
2022-04-27 08:01:43 +0000

    Add Blink's page settings and web preferences for font-family: math
    The generic font family name "math" is intended for displaying math
    expressions such as MathML [1] [2]. Initial support was implemented
    in [3]. Currently, it's used by FontSelector::FamilyNameFromSettings on
    non-Android platforms and resolved to the hardcoded string
    "Latin Modern Math". This CL introduces corresponding page settings and
    web preferences in Blink so they become configurable. More work will be
    done in follow-up CLs.
    
    [1] https://docs.google.com/document/d/1bvY_Npe2zLW_705KXdmecsH6P9I9wBMpHRsZ9CxWNOI/edit#heading=h.u9hwm9tp8nuy
    [2] https://www.w3.org/TR/css-fonts-4/#valdef-font-family-math
    [3] https://chromium-review.googlesource.com/c/chromium/src/+/2119534
    
    Bug: 6606, 1228189, 1240117
    Change-Id: Ic5ea9cf357d9d1c7d2b70331f0eb92a84bdd4a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578971
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#996578}

--- third_party/blink/renderer/platform/fonts/generic_font_family_settings_test.cc
+++ third_party/blink/renderer/platform/fonts/generic_font_family_settings_test.cc
@@ -36,6 +36,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_TRUE(settings.SansSerif().IsEmpty());
   EXPECT_TRUE(settings.Cursive().IsEmpty());
   EXPECT_TRUE(settings.Fantasy().IsEmpty());
+  EXPECT_TRUE(settings.Math().IsEmpty());
 
   EXPECT_TRUE(settings.Standard(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Fixed(USCRIPT_ARABIC).IsEmpty());
@@ -43,6 +44,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_TRUE(settings.SansSerif(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Cursive(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Fantasy(USCRIPT_ARABIC).IsEmpty());
+  EXPECT_TRUE(settings.Math(USCRIPT_ARABIC).IsEmpty());
 
   settings.UpdateStandard("CustomStandard");
   settings.UpdateFixed("CustomFixed");
@@ -50,6 +52,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   settings.UpdateSansSerif("CustomSansSerif");
   settings.UpdateCursive("CustomCursive");
   settings.UpdateFantasy("CustomFantasy");
+  settings.UpdateMath("CustomMath");
 
   settings.UpdateStandard("CustomArabicStandard", USCRIPT_ARABIC);
   settings.UpdateFixed("CustomArabicFixed", USCRIPT_ARABIC);
@@ -57,6 +60,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   settings.UpdateSansSerif("CustomArabicSansSerif", USCRIPT_ARABIC);
   settings.UpdateCursive("CustomArabicCursive", USCRIPT_ARABIC);
   settings.UpdateFantasy("CustomArabicFantasy", USCRIPT_ARABIC);
+  settings.UpdateMath("CustomArabicMath", USCRIPT_ARABIC);
 
   EXPECT_EQ("CustomStandard", settings.Standard());
   EXPECT_EQ("CustomFixed", settings.Fixed());
@@ -64,6 +68,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_EQ("CustomSansSerif", settings.SansSerif());
   EXPECT_EQ("CustomCursive", settings.Cursive());
   EXPECT_EQ("CustomFantasy", settings.Fantasy());
+  EXPECT_EQ("CustomMath", settings.Math());
 
   EXPECT_EQ("CustomArabicStandard", settings.Standard(USCRIPT_ARABIC));
   EXPECT_EQ("CustomArabicFixed", settings.Fixed(USCRIPT_ARABIC));
@@ -71,6 +76,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_EQ("CustomArabicSansSerif", settings.SansSerif(USCRIPT_ARABIC));
   EXPECT_EQ("CustomArabicCursive", settings.Cursive(USCRIPT_ARABIC));
   EXPECT_EQ("CustomArabicFantasy", settings.Fantasy(USCRIPT_ARABIC));
+  EXPECT_EQ("CustomArabicMath", settings.Math(USCRIPT_ARABIC));
 
   settings.Reset();
 
@@ -80,6 +86,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_TRUE(settings.SansSerif().IsEmpty());
   EXPECT_TRUE(settings.Cursive().IsEmpty());
   EXPECT_TRUE(settings.Fantasy().IsEmpty());
+  EXPECT_TRUE(settings.Math().IsEmpty());
 
   EXPECT_TRUE(settings.Standard(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Fixed(USCRIPT_ARABIC).IsEmpty());
@@ -87,6 +94,7 @@ TEST(GenericFontFamilySettingsTest, TestAllNames) {
   EXPECT_TRUE(settings.SansSerif(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Cursive(USCRIPT_ARABIC).IsEmpty());
   EXPECT_TRUE(settings.Fantasy(USCRIPT_ARABIC).IsEmpty());
+  EXPECT_TRUE(settings.Math(USCRIPT_ARABIC).IsEmpty());
 }
 
 }  // namespace blink
