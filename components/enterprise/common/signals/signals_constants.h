7154e1ed05acc4f14c2d8a7c1c41ce190687977d
Sebastien Lalancette <seblalancette@chromium.org>
2022-04-27 16:22:03 +0000

    [NewEV] Added Basic SignalsAggregator
    New keyed-service service to be used to collect context-aware signals.
    Collectors will be injected into the constructor by the factory (future
    CLs).
    
    It will only support single signal collection to start, as multi-signal
    collection is not yet required (every signal has its own JS function).
    
    Bug: b:229737384
    Change-Id: Ib3780bdc66aa3886f2f4e0c3a7a04686c46b1a04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601066
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996732}

--- components/enterprise/common/signals/signals_constants.h
+++ components/enterprise/common/signals/signals_constants.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
-#define COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
+#ifndef COMPONENTS_ENTERPRISE_COMMON_SIGNALS_SIGNALS_CONSTANTS_H_
+#define COMPONENTS_ENTERPRISE_COMMON_SIGNALS_SIGNALS_CONSTANTS_H_
 
 namespace enterprise_signals {
 
@@ -21,4 +21,4 @@ extern const char kInstalledHotfixes[];
 
 }  // namespace enterprise_signals
 
-#endif  // COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
+#endif  // COMPONENTS_ENTERPRISE_COMMON_SIGNALS_SIGNALS_CONSTANTS_H_
