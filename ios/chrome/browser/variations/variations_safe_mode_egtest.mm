217a29cf1bd1fee43b35ea5ab5c31d62f6269768
Roger McFarlane <rogerm@google.com>
2022-04-27 15:07:15 +0000

    Re-enable VariationsSafeModeTests
    Bug: 1316325
    Change-Id: Iac3be37b27204cf4267cfad19993847cf4db46d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611083
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996705}

--- ios/chrome/browser/variations/variations_safe_mode_egtest.mm
+++ ios/chrome/browser/variations/variations_safe_mode_egtest.mm
@@ -122,8 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Corresponds to VariationsSafeModeBrowserTest.ThreeCrashesTriggerSafeMode in
 // variations_safe_mode_browsertest.cc.
-// TODO(crbug.com/1316325): Test not run or not finished.
-- (void)DISABLED_testThreeCrashesTriggerSafeMode {
+- (void)testThreeCrashesTriggerSafeMode {
   [VariationsAppInterface setTestSafeSeedAndSignature];
 
   // Persist the local state pref changes made above and in setUp().
@@ -170,8 +169,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Corresponds to VariationsSafeModeBrowserTest.FetchFailuresTriggerSafeMode in
 // variations_safe_mode_browsertest.cc.
-// TODO(crbug.com/1316325): Test not run or not finished.
-- (void)DISABLED_testFetchFailuresTriggerSafeMode {
+- (void)testFetchFailuresTriggerSafeMode {
   [VariationsAppInterface setTestSafeSeedAndSignature];
   // The fetch failure streak threshold for triggering safe mode is 25.
   [VariationsAppInterface setFetchFailureValue:25];
@@ -201,8 +199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Corresponds to VariationsSafeModeBrowserTest.DoNotTriggerSafeMode in
 // variations_safe_mode_browsertest.cc.
-// TODO(crbug.com/1316325): Test not run or not finished.
-- (void)DISABLED_testDoNotTriggerSafeMode {
+- (void)testDoNotTriggerSafeMode {
   [VariationsAppInterface setTestSafeSeedAndSignature];
   // Neither a crash streak of 2 nor a fetch failure streak of 24 will trigger
   // variations safe mode in the next session.
