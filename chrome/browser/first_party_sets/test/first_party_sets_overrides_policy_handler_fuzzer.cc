3f6cee3197d628def469ab3c5ee716a6ac9f1fbd
Kirubel Aklilu <kaklilu@chromium.org>
2022-04-27 15:24:14 +0000

    Initialize ICU in first_party_sets_overrides_policy_handler_fuzzer
    The CL implements the fix described by https://crbug.com/778929
    
    Bug: 1319094
    Change-Id: Ied61d92f4feec5a52eda0dd67218da2e3c45b50c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3603785
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996709}

--- chrome/browser/first_party_sets/test/first_party_sets_overrides_policy_handler_fuzzer.cc
+++ chrome/browser/first_party_sets/test/first_party_sets_overrides_policy_handler_fuzzer.cc
@@ -21,6 +21,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace first_party_sets {
 
+struct IcuEnvironment {
+  IcuEnvironment() { CHECK(base::i18n::InitializeICU()); }
+
+  // used by ICU integration.
+  base::AtExitManager at_exit_manager;
+};
+
+IcuEnvironment* test_case = new IcuEnvironment();
+
 DEFINE_PROTO_FUZZER(const json_proto::JsonValue& json_value) {
   json_proto::JsonProtoConverter converter;
   std::string native_input = converter.Convert(json_value);
