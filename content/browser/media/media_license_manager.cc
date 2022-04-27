2f4d910c472f2e786ce12df5363c13878c835839
Austin Sullivan <asully@chromium.org>
2022-04-27 01:22:29 +0000

    Flip flag to start using the new media license backend
    Flips the kMediaLicenseBackend flag and starts migrating media license
    data to the new backend.
    
    Bug: 1231162
    Change-Id: Idaaddb2fb2b43769ce9318c57be96d048fc79006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537342
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996468}

--- content/browser/media/media_license_manager.cc
+++ content/browser/media/media_license_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_forward.h"
 #include "base/containers/flat_map.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/memory/scoped_refptr.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/cdm_info.h"
+#include "content/public/common/content_features.h"
 #include "media/cdm/cdm_type.h"
 #include "media/media_buildflags.h"
 #include "net/base/io_buffer.h"
@@ -300,7 +302,13 @@ MediaLicenseManager::MediaLicenseManager(
         {blink::mojom::StorageType::kTemporary});
   }
 
-  // TODO(crbug.com/1231162): Consider migrating media licenses here.
+  if (base::FeatureList::IsEnabled(features::kMediaLicenseBackend)) {
+    // Ensure the file system context is kept alive until we're done migrating
+    // media license data from the Plugin Private File System to this backend.
+    MigrateMediaLicenses(
+        base::BindOnce([](scoped_refptr<storage::FileSystemContext>) {},
+                       base::WrapRefCounted(context().get())));
+  }
 }
 
 MediaLicenseManager::~MediaLicenseManager() = default;
@@ -620,6 +628,13 @@ void MediaLicenseManager::OnHostReceiverDisconnect(
     base::PassKey<MediaLicenseStorageHost> pass_key) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(host);
+
+  if (in_memory()) {
+    // Don't delete `host` for an in-memory profile, since the data is not safe
+    // to delete yet. For example, a site may be re-visited within the same
+    // incognito session. `host` will be destroyed when `this` is destroyed.
+    return;
+  }
 
   DCHECK_GT(hosts_.count(host->storage_key()), 0ul);
   DCHECK_EQ(hosts_[host->storage_key()].get(), host);
