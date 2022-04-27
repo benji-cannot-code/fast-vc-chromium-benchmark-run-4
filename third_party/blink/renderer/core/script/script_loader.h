43fbbdfc04a682047c94d93d69b02a3d86e2baf9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-04-27 19:16:00 +0000

    Make ScriptRunner PendingScriptClient and remove MovePendingScript()
    This CL fixes crbug.com/1303648 and crbug.com/1317437
    by not moving PendingScript between ScriptRunners.
    
    Bug: 1303648, 1317437
    Change-Id: I1c59916009f4d5548037dd1f6bbe584d7defec49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275119
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996822}

--- third_party/blink/renderer/core/script/script_loader.h
+++ third_party/blink/renderer/core/script/script_loader.h
@@ -23,17 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SCRIPT_SCRIPT_LOADER_H_
 
 #include "third_party/blink/public/mojom/script/script_type.mojom-blink-forward.h"
-#include "third_party/blink/public/platform/web_url_request.h"
-#include "third_party/blink/renderer/bindings/core/v8/script_source_location_type.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/cross_origin_attribute.h"
 #include "third_party/blink/renderer/core/script/pending_script.h"
-#include "third_party/blink/renderer/core/script/script.h"
-#include "third_party/blink/renderer/core/script/script_runner.h"
 #include "third_party/blink/renderer/core/script/script_scheduling_type.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
-#include "third_party/blink/renderer/platform/loader/fetch/integrity_metadata.h"
-#include "third_party/blink/renderer/platform/loader/fetch/resource_loader_options.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource_finish_observer.h"
 #include "third_party/blink/renderer/platform/loader/fetch/script_fetch_options.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_encoding.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_position.h"
@@ -50,14 +45,14 @@ class ScriptWebBundle;
 class SpeculationRuleSet;
 class Modulator;
 
-class CORE_EXPORT ScriptLoader final : public GarbageCollected<ScriptLoader>,
-                                       public PendingScriptClient,
+class CORE_EXPORT ScriptLoader final : public ResourceFinishObserver,
                                        public NameClient {
  public:
   ScriptLoader(ScriptElementBase*, const CreateElementFlags);
   ~ScriptLoader() override;
   void Trace(Visitor*) const override;
   const char* NameInHeapSnapshot() const override { return "ScriptLoader"; }
+  String DebugName() const override { return "ScriptLoader"; }
 
   enum LegacyTypeSupport {
     kDisallowLegacyTypeInTypeAttribute,
@@ -115,11 +110,6 @@ class CORE_EXPORT ScriptLoader final : public GarbageCollected<ScriptLoader>,
 
   void SetFetchDocWrittenScriptDeferIdle();
 
-  // Return non-null if controlled by ScriptRunner, or null otherwise.
-  // Only for ScriptRunner::MovePendingScript() and should be removed once
-  // crbug.com/721914 is fixed.
-  PendingScript* GetPendingScriptIfControlledByScriptRunnerForCrossDocMove();
-
  private:
   bool IgnoresLoadRequest() const;
   bool IsScriptForEventSupported() const;
@@ -140,11 +130,10 @@ class CORE_EXPORT ScriptLoader final : public GarbageCollected<ScriptLoader>,
                              Modulator*,
                              const ScriptFetchOptions&);
 
-  // Clears the connection to the PendingScript.
-  void DetachPendingScript();
-
-  // PendingScriptClient
-  void PendingScriptFinished(PendingScript*) override;
+  // ResourceFinishObserver. This should be used only for managing
+  // `resource_keep_alive_` lifetime and shouldn't be used for script
+  // evaluation.
+  void NotifyFinished() override;
 
   // Get the effective script text (after Trusted Types checking).
   String GetScriptText() const;
@@ -206,13 +195,6 @@ class CORE_EXPORT ScriptLoader final : public GarbageCollected<ScriptLoader>,
   // Later, TakePendingScript() is called, and its caller holds a reference
   // to the PendingScript instead and |prepared_pending_script_| is cleared.
   Member<PendingScript> prepared_pending_script_;
-
-  // If the script is controlled by ScriptRunner, then
-  // ScriptLoader::pending_script_ holds a reference to the PendingScript and
-  // ScriptLoader is its client.
-  // Otherwise, HTMLParserScriptRunner or XMLParserScriptRunner holds the
-  // reference and |pending_script_| here is null.
-  Member<PendingScript> pending_script_;
 
   // This is used only to keep the ScriptResource of a classic script alive
   // and thus to keep it on MemoryCache, even after script execution, as long
--- third_party/blink/renderer/core/svg/svg_script_element.h
+++ third_party/blink/renderer/core/svg/svg_script_element.h
@@ -63,7 +63,6 @@ class SVGScriptElement final : public SVGElement,
   InsertionNotificationRequest InsertedInto(ContainerNode&) override;
   void DidNotifySubtreeInsertionsToDocument() override;
   void ChildrenChanged(const ChildrenChange&) override;
-  void DidMoveToNewDocument(Document& old_document) override;
 
   void SvgAttributeChanged(const SvgAttributeChangedParams&) override;
   bool IsURLAttribute(const Attribute&) const override;
