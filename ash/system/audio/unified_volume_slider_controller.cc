a2bbd7af0bd08f1e3b75679882e8e210a64a6167
Roger Tinkoff <rtinkoff@google.com>
2022-04-28 18:56:50 +0000

    ash: Possible fix for intermittent crash when clicking to open audio settings
    ash::UnifiedVolumeSliderController::Delegate now owns its own
    base::WeakPtrFactory<>, through which the delegate's
    OnAudioSettingsButtonClicked() method is now bound as the
    PressedCallback of UnifiedVolumeView's more_button_. Previously the
    callback was bound as base::Unretained(), which made it possible for
    the callback to be invoked after the Delegate has been destroyed.
    
    Bug: 1315670
    Change-Id: I75014e8e0d13de8645659c543012d2e47f9b4419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614029
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#997320}

--- ash/system/audio/unified_volume_slider_controller.cc
+++ ash/system/audio/unified_volume_slider_controller.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+UnifiedVolumeSliderController::Delegate::Delegate() = default;
+
+UnifiedVolumeSliderController::Delegate::~Delegate() = default;
+
 UnifiedVolumeSliderController::UnifiedVolumeSliderController(
     UnifiedVolumeSliderController::Delegate* delegate)
     : delegate_(delegate) {
