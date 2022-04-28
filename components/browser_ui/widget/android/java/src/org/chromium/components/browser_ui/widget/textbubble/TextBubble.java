f39df4b60b5d1d3e016a6d9811a22b9758669b08
Lijin Shen <lazzzis@google.com>
2022-04-28 20:05:29 +0000

    Add back press manager for supporting back gesture API
    1. add back press manager to handle back press gesture api
    2. migrate text bubble to implement back gesture handler interface
    
    Bug: 1279941
    Change-Id: I1407c73f1a09adf19fb0101ac622ac1a54e51ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591045
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997361}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/textbubble/TextBubble.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/textbubble/TextBubble.java
@@ -28,6 +28,8 @@ import androidx.appcompat.content.res.AppCompatResources;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.MathUtils;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.R;
 import org.chromium.ui.widget.AnchoredPopupWindow;
@@ -52,6 +54,10 @@ public class TextBubble implements AnchoredPopupWindow.LayoutObserver {
      * bubbles on a back press event.
      */
     private static final Set<TextBubble> sBubbles = new HashSet<>();
+
+    /** A supplier which notifies of changes of text bubbles count. */
+    private static final ObservableSupplierImpl<Integer> sCountSupplier =
+            new ObservableSupplierImpl<>();
 
     protected final Context mContext;
     private final Handler mHandler;
@@ -85,6 +91,7 @@ public class TextBubble implements AnchoredPopupWindow.LayoutObserver {
         @Override
         public void onDismiss() {
             sBubbles.remove(TextBubble.this);
+            sCountSupplier.set(sBubbles.size());
         }
     };
 
@@ -347,6 +354,7 @@ public class TextBubble implements AnchoredPopupWindow.LayoutObserver {
 
         mPopupWindow.show();
         sBubbles.add(this);
+        sCountSupplier.set(sBubbles.size());
         mBubbleShowStartTime = System.currentTimeMillis();
     }
 
@@ -379,6 +387,13 @@ public class TextBubble implements AnchoredPopupWindow.LayoutObserver {
         for (TextBubble bubble : bubbles) {
             bubble.dismiss();
         }
+    }
+
+    /**
+     * @return A supplier which notifies of changes of text bubbles count.
+     * */
+    public static ObservableSupplier<Integer> getCountSupplier() {
+        return sCountSupplier;
     }
 
     /**
