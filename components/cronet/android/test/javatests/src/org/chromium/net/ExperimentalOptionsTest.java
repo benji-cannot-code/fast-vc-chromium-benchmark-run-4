8d95f5bd1f2849ee8af69e5ec4d3904b301be8ad
Boris Sazonov <bsazonov@chromium.org>
2022-04-28 17:11:02 +0000

    [Sheriff] Disable ExperimentalOptionsTest#testDetectBrokenConnectionOnNetworkFailure
    Reason: flaky.
    
    Bug: 1320725
    Change-Id: Icadbb0f7e7bc57a3543e7ed8131f9982bfc1cf11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613649
    Owners-Override: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997253}

--- components/cronet/android/test/javatests/src/org/chromium/net/ExperimentalOptionsTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/ExperimentalOptionsTest.java
@@ -274,6 +274,7 @@ public class ExperimentalOptionsTest {
         cronetEngine.shutdown();
     }
 
+    @DisabledTest(message = "crbug.com/1320725")
     @Test
     @LargeTest
     @Feature({"Cronet"})
