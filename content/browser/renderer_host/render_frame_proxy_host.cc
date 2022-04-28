69672c4e6ba41372045c3276e47eea6798fa7b8b
David Bokan <bokan@chromium.org>
2022-04-28 01:20:30 +0000

    Make ScrollRectToVisible take a RectF
    This method is used to bubble a ScrollIntoView across a remote frame
    boundary. The rect passed comes from a Blink PhysicalRect, which is
    subpixel, and is then forwarded to the parent renderer where it must be
    converted back to PhysicalRect. This causes needless drift and awkward
    conversions. Instead, use a RectF, which will be a more (though not
    perfect) faithful representation of the fixed-point PhysicalRect and has
    better defined conversion functions.
    
    Note: this is split out from https://crrev.com/c/3597454
    
    Bug: 1296183
    Change-Id: Ie5f3f68c50ccf87ccb3ff0ffbfab08b3ab05ad67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608557
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996981}

--- content/browser/renderer_host/render_frame_proxy_host.cc
+++ content/browser/renderer_host/render_frame_proxy_host.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/frame_owner_properties.mojom.h"
 #include "third_party/blink/public/mojom/messaging/transferable_message.mojom.h"
 #include "third_party/blink/public/mojom/scroll/scroll_into_view_params.mojom.h"
+#include "ui/gfx/geometry/rect_f.h"
 
 namespace content {
 
@@ -385,7 +386,7 @@ void RenderFrameProxyHost::SetFocusedFrame() {
 }
 
 void RenderFrameProxyHost::ScrollRectToVisible(
-    const gfx::Rect& rect_to_scroll,
+    const gfx::RectF& rect_to_scroll,
     blink::mojom::ScrollIntoViewParamsPtr params) {
   GetAssociatedRemoteFrame()->ScrollRectToVisible(rect_to_scroll,
                                                   std::move(params));
--- third_party/blink/renderer/core/frame/remote_frame.cc
+++ third_party/blink/renderer/core/frame/remote_frame.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/geometry/rect_f.h"
 
 namespace blink {
 
@@ -707,7 +708,7 @@ void RemoteFrame::SetPageFocus(bool is_focused) {
 }
 
 void RemoteFrame::ScrollRectToVisible(
-    const gfx::Rect& rect_to_scroll,
+    const gfx::RectF& rect_to_scroll,
     mojom::blink::ScrollIntoViewParamsPtr params) {
   Element* owner_element = DeprecatedLocalOwner();
   LayoutObject* owner_object = owner_element->GetLayoutObject();
@@ -720,11 +721,7 @@ void RemoteFrame::ScrollRectToVisible(
 
   // Schedule the scroll.
   PhysicalRect absolute_rect = owner_object->LocalToAncestorRect(
-      PhysicalRect(LayoutUnit(rect_to_scroll.x()),
-                   LayoutUnit(rect_to_scroll.y()),
-                   LayoutUnit(rect_to_scroll.width()),
-                   LayoutUnit(rect_to_scroll.height())),
-      owner_object->View());
+      PhysicalRect::EnclosingRect(rect_to_scroll), owner_object->View());
 
   if (!params->zoom_into_rect ||
       !owner_object->GetDocument().GetFrame()->LocalFrameRoot().IsMainFrame()) {
