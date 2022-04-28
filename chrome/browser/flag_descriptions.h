e7994f0e03a652c249424b0a9905bb582fc45fba
Christian Dullweber <dullweber@chromium.org>
2022-04-28 17:12:01 +0000

    AboutThisSite: Add "More about this page" flag
    Bug: 1318000
    Change-Id: Ibac1d1896b0ba5a5706e0f92da50ed79dd683037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613727
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#997254}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1095,6 +1095,9 @@ extern const char kPageEntitiesPageContentAnnotationsDescription[];
 extern const char kPageInfoAboutThisSiteName[];
 extern const char kPageInfoAboutThisSiteDescription[];
 
+extern const char kPageInfoMoreAboutThisPageName[];
+extern const char kPageInfoMoreAboutThisPageDescription[];
+
 extern const char kPageInfoHistoryDesktopName[];
 extern const char kPageInfoHistoryDesktopDescription[];
 
