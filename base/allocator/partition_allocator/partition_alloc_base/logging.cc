aba301a10e362624043a08df778bbeef9cc7565c
Kalvin Lee <kdlee@chromium.org>
2022-04-28 17:27:36 +0000

    base/logging: Include `immediate_crash`
    This IWYU CL includes `immediate_crash.h` where needed; the pertinent
    macro is used directly in `logging.cc`.
    
    Bug: 1151236
    Change-Id: I8c1ed81b7d51ca4551649b77141d9df88e75ec65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612543
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997265}

--- base/allocator/partition_allocator/partition_alloc_base/logging.cc
+++ base/allocator/partition_allocator/partition_alloc_base/logging.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_base/debug/alias.h"
 #include "base/base_export.h"
 #include "base/cxx17_backports.h"
+#include "base/immediate_crash.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_WIN)
