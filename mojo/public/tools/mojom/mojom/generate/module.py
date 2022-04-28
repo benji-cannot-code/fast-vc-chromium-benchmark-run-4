8cc18d760015f253ffd611374d60beebc06c59f6
Takuto Ikuta <tikuta@chromium.org>
2022-04-28 09:04:44 +0000

    mojo: use default pickle protocol
    This improves the performance of code generation slightly.
    
    e.g. For gen/content/common/agent_scheduling_group.mojom-forward.h
    
    $ hyperfine 'version 2' 'version 4' 'version 5'
    Benchmark 1: version 2
      Time (mean ± σ):     18.709 s ±  0.974 s    [User: 17.658 s, System: 1.029 s]
      Range (min … max):   17.810 s … 20.667 s    10 runs
    
    Benchmark 2: version 4
      Time (mean ± σ):     18.090 s ±  0.566 s    [User: 17.176 s, System: 0.892 s]
      Range (min … max):   17.604 s … 18.980 s    10 runs
    
    Benchmark 3: version 5
      Time (mean ± σ):     17.823 s ±  0.293 s    [User: 17.023 s, System: 0.780 s]
      Range (min … max):   17.561 s … 18.609 s    10 runs
    
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet PC without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Summary
      'version 5' ran
        1.01 ± 0.04 times faster than 'version 4'
        1.05 ± 0.06 times faster than 'version 2'
    
    
    Bug: 1309804
    Change-Id: I2901e7c13c8a71da6712d84b9fa7a0d83ee86d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607256
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997110}

--- mojo/public/tools/mojom/mojom/generate/module.py
+++ mojo/public/tools/mojom/mojom/generate/module.py
@@ -1527,7 +1527,7 @@ class Module(object):
       imported_module.Stylize(stylizer)
 
   def Dump(self, f):
-    pickle.dump(self, f, 2)
+    pickle.dump(self, f)
 
   @classmethod
   def Load(cls, f):
