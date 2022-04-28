bef31661c580c8dfa1ff6fb8d667c253569c6345
Kevin McNee <mcnee@chromium.org>
2022-04-28 14:25:15 +0000

    Don't crash when activating a portal in a guest view
    While it should be possible to activate a portal in a guest view, this
    has not yet been implemented. For now, we reject activation in this
    case.
    
    Bug: 1319841
    Change-Id: I30a6f528fab0c960d1720060497c81342dbe5ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611511
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997195}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -1875,6 +1875,7 @@ void RenderFrameHostImpl::OnPortalActivated(
               case blink::mojom::PortalActivateResult::
                   kRejectedDueToErrorInPortal:
               case blink::mojom::PortalActivateResult::kDisconnected:
+              case blink::mojom::PortalActivateResult::kNotImplemented:
               case blink::mojom::PortalActivateResult::kAbortedDueToBug:
                 // The renderer is misbehaving.
                 mojo::ReportBadMessage(
