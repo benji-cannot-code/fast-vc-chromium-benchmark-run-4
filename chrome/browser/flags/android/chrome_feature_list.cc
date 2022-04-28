2e8af866992043a129e58aa1e7e68b5633119e1f
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-04-28 10:02:47 +0000

    Add a feature flag for TWA notification permission delegation.
    Bug: 1320272
    Change-Id: I6f85d8bf0c1ee75512dde1ee93897448af24ef95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610406
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997128}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -290,6 +290,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kToolbarMicIphAndroid,
     &kTrustedWebActivityLocationDelegation,
     &kTrustedWebActivityNewDisclosure,
+    &kTrustedWebActivityNotificationPermissionDelegation,
     &kTrustedWebActivityPostMessage,
     &kTrustedWebActivityQualityEnforcement,
     &kTrustedWebActivityQualityEnforcementForced,
@@ -797,6 +798,10 @@ const base::Feature kTrustedWebActivityLocationDelegation{
 
 const base::Feature kTrustedWebActivityNewDisclosure{
     "TrustedWebActivityNewDisclosure", base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kTrustedWebActivityNotificationPermissionDelegation{
+    "TrustedWebActivityNotificationPermissionDelegation",
+    base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTrustedWebActivityPostMessage{
     "TrustedWebActivityPostMessage", base::FEATURE_DISABLED_BY_DEFAULT};
