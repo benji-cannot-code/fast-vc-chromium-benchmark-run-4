681eead9e05e8bdccf29077eac6fae46496b7f6a
sisidovski <sisidovski@google.com>
2022-04-28 00:04:58 +0000

    Remove Script::GetClassicScriptSizes()
    This CL will remove Script::GetClassicScriptSizes(). This method is only
    used by ServiceWorkerGlobalScopeProxy::WillEvaluateClassicScript(), and
    return values are not used anymore, so we can simply remove the method.
    
    Bug: 1320109
    Change-Id: Idd0231bd6c2a9ecd44e870953bacca9d24f77f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610882
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@google.com>
    Cr-Commit-Position: refs/heads/main@{#996954}

--- third_party/blink/renderer/modules/service_worker/service_worker_global_scope_proxy.h
+++ third_party/blink/renderer/modules/service_worker/service_worker_global_scope_proxy.h
@@ -120,8 +120,7 @@ class ServiceWorkerGlobalScopeProxy final : public WebServiceWorkerContextProxy,
   void DidFetchScript() override;
   void DidFailToFetchClassicScript() override;
   void DidFailToFetchModuleScript() override;
-  void WillEvaluateClassicScript(size_t script_size,
-                                 size_t cached_metadata_size) override;
+  void WillEvaluateClassicScript() override;
   void WillEvaluateImportedClassicScript(size_t script_size,
                                          size_t cached_metadata_size) override;
   void WillEvaluateModuleScript() override;
