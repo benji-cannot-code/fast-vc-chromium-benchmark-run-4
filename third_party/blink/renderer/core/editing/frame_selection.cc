2e35645e5c04f74fea7c688f4a242748a471495c
Kent Tamura <tkent@chromium.org>
2022-04-28 01:49:48 +0000

    Revert "DeferredShaping: Improve performance of FrameSelection::DidSetSelectionDeprecated"
    This reverts commit e9b0bed8360e4373181f30648552e2b349c58b9e.
    
    Reason for revert: It's not effective.
    
    Original change's description:
    > DeferredShaping: Improve performance of FrameSelection::DidSetSelectionDeprecated
    >
    > This CL fixes timeout of editing/deleting/delete-many-lines-of-text.html
    > with enabling DeferredShaping.
    >
    > Bug: 1259085
    > Change-Id: I74c21ce7a8342cc6c93c6d8028232d6a1400d70d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3609732
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#996612}
    
    Bug: 1259085
    Change-Id: I9c50375f7ee59f88c7e84e8414c66bea69f2bfb0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611944
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#996997}

--- third_party/blink/renderer/core/editing/frame_selection.cc
+++ third_party/blink/renderer/core/editing/frame_selection.cc
@@ -76,7 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/input/context_menu_allowed_scope.h"
 #include "third_party/blink/renderer/core/input/event_handler.h"
-#include "third_party/blink/renderer/core/layout/deferred_shaping.h"
 #include "third_party/blink/renderer/core/layout/hit_test_request.h"
 #include "third_party/blink/renderer/core/layout/hit_test_result.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
@@ -174,14 +173,6 @@ VisibleSelection FrameSelection::ComputeVisibleSelectionInDOMTreeDeprecated()
     force_locks = DisplayLockUtilities::ScopedForcedUpdate(
         base.AnchorNode(), DisplayLockContext::ForcedPhase::kLayout);
   }
-  // If the following UpdateStyleAndLayout() registered new shaping-deferred
-  // elements, it would be slow because of ScopedForcedUpdate instances.
-  // Without this DeferredShapingDisallowScope, the UpdateStyleAndLayout()
-  // takes 200 seconds in editing/deleting/delete-many-lines-of-text.html
-  // with a debug build.
-  absl::optional<DeferredShapingDisallowScope> disallow_deferred;
-  if (auto* frame_view = GetDocument().View())
-    disallow_deferred.emplace(*frame_view);
   GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kSelection);
   return ComputeVisibleSelectionInDOMTree();
 }
