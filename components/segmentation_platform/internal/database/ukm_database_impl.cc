085c805ba282c4ac47766b971c9669f975218b1c
ssid <ssid@chromium.org>
2022-04-28 21:40:00 +0000

    Fix threading in UkmDatabase
    The callback parameter sent to backend was wrong. The UkmDatabase should
    send the current task runner where the callbacks from backend should be
    posted.
    
    BUG=1316486
    
    Change-Id: Ia427e5fc9e4e372bc5912abc021134317eb1472c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611823
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997425}

--- components/segmentation_platform/internal/database/ukm_database_impl.cc
+++ components/segmentation_platform/internal/database/ukm_database_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/database/ukm_database_impl.h"
 
 #include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "components/segmentation_platform/internal/database/ukm_database_backend.h"
 
 namespace segmentation_platform {
@@ -14,20 +15,24 @@ UkmDatabaseImpl::UkmDatabaseImpl(const base::FilePath& database_path)
     : task_runner_(base::SequencedTaskRunnerHandle::Get()),
       backend_task_runner_(base::ThreadPool::CreateSequencedTaskRunner(
           {base::MayBlock(), base::TaskPriority::USER_VISIBLE})),
-      backend_(std::make_unique<UkmDatabaseBackend>(database_path,
-                                                    backend_task_runner_)) {}
+      backend_(std::make_unique<UkmDatabaseBackend>(
+          database_path,
+          base::SequencedTaskRunnerHandle::Get())) {}
 
 UkmDatabaseImpl::~UkmDatabaseImpl() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->DeleteSoon(FROM_HERE, std::move(backend_));
 }
 
 void UkmDatabaseImpl::InitDatabase(SuccessCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&UkmDatabaseBackend::InitDatabase,
                                 backend_->GetWeakPtr(), std::move(callback)));
 }
 
 void UkmDatabaseImpl::StoreUkmEntry(ukm::mojom::UkmEntryPtr ukm_entry) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&UkmDatabaseBackend::StoreUkmEntry,
                                 backend_->GetWeakPtr(), std::move(ukm_entry)));
@@ -36,6 +41,7 @@ void UkmDatabaseImpl::StoreUkmEntry(ukm::mojom::UkmEntryPtr ukm_entry) {
 void UkmDatabaseImpl::UpdateUrlForUkmSource(ukm::SourceId source_id,
                                             const GURL& url,
                                             bool is_validated) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&UkmDatabaseBackend::UpdateUrlForUkmSource,
@@ -43,12 +49,14 @@ void UkmDatabaseImpl::UpdateUrlForUkmSource(ukm::SourceId source_id,
 }
 
 void UkmDatabaseImpl::OnUrlValidated(const GURL& url) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&UkmDatabaseBackend::OnUrlValidated,
                                 backend_->GetWeakPtr(), url));
 }
 
 void UkmDatabaseImpl::RemoveUrls(const std::vector<GURL>& urls) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(FROM_HERE,
                                  base::BindOnce(&UkmDatabaseBackend::RemoveUrls,
                                                 backend_->GetWeakPtr(), urls));
@@ -56,10 +64,12 @@ void UkmDatabaseImpl::RemoveUrls(const std::vector<GURL>& urls) {
 
 void UkmDatabaseImpl::RunReadonlyQueries(const QueryList& queries,
                                          QueryCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // TODO(haileywang): Implement.
 }
 
 void UkmDatabaseImpl::DeleteEntriesOlderThan(base::Time time) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   backend_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&UkmDatabaseBackend::DeleteEntriesOlderThan,
                                 backend_->GetWeakPtr(), time));
