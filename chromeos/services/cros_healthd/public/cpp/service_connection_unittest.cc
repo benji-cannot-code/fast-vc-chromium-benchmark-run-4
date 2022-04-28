386cdac5fbdded813017b44a71392e656a322cf7
Chung-Sheng Wu <chungsheng@google.com>
2022-04-28 03:23:17 +0000

    cros_healthd: Remove unused file and dependency
    Bug: b/210565604
    Test: CQ
    Change-Id: I828e4d0208f5c7571b7cd1166b57d407bd150275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599454
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#997034}

--- chromeos/services/cros_healthd/public/cpp/service_connection_unittest.cc
+++ chromeos/services/cros_healthd/public/cpp/service_connection_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
-#include "chromeos/ash/components/dbus/cros_healthd/cros_healthd_client.h"
 #include "chromeos/services/cros_healthd/public/cpp/fake_cros_healthd.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
