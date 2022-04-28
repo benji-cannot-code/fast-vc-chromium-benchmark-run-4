5da735c6e5a6814871cf74a504ee16808a034132
Kalvin Lee <kdlee@chromium.org>
2022-04-28 18:52:50 +0000

    base/logging: Don't include what's not used
    The implementation of `//base/logging` appears not to use anything
    declared in `cxx17_backports.h`, so there's no need to include it.
    
    Bug: 1151236
    Change-Id: Icd07625964a58e0ad81b19114bffff74d117e703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610664
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997316}

--- base/logging.cc
+++ base/logging.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
-#include "base/cxx17_backports.h"
 #include "base/debug/crash_logging.h"
 #include "base/immediate_crash.h"
 #include "base/pending_task.h"
