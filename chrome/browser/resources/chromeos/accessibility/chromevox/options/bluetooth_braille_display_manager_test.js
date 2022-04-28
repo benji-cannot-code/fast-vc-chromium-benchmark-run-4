145100acc0603486250d77459299fe35f376898b
Anastasia Helfinstein <anastasi@google.com>
2022-04-28 16:46:37 +0000

    [ChromeVox] Move BluetoothBrailleDisplayManager into options/
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I75f7b6e50f83563203c9e62c36363a0d23af4984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610958
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#997236}

--- chrome/browser/resources/chromeos/accessibility/chromevox/options/bluetooth_braille_display_manager_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/options/bluetooth_braille_display_manager_test.js
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Include test fixture.
-GEN_INCLUDE(['../../testing/chromevox_webui_test_base.js']);
-GEN_INCLUDE(['../../testing/fake_objects.js']);
+GEN_INCLUDE(['../testing/chromevox_webui_test_base.js']);
+GEN_INCLUDE(['../testing/fake_objects.js']);
 
 GEN('#include "content/public/test/browser_test.h"');
 
