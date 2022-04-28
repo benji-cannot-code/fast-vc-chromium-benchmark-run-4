ec9488f7dfe087d6ab508862abce4fe778a3ec2c
Bo Liu <boliu@chromium.org>
2022-04-28 20:34:45 +0000

    gpu: Limit max texture workaround to client
    Change the behavior of max_texture_size_limit_4096 workaround to only
    limit command buffer. Remove shared image and skia from applying this
    limit. Also rename the workaround to ensure everything using the
    workaround is at least considered in this CL.
    
    Future work include
    * skia stop checking the workaround
    * removing the same workaround in angle
    * rename the name in json as well
    
    Bug: 1319451
    Change-Id: I8fac22abc6f25fdcbe91675441933bd71bfa4b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612392
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997388}

--- gpu/command_buffer/service/shared_image_backing_factory_gl_texture_unittest.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_gl_texture_unittest.cc
@@ -151,7 +151,6 @@ class SharedImageBackingFactoryGLTextureTest
       : SharedImageBackingFactoryGLTextureTestBase(false) {}
   void SetUp() override {
     GpuDriverBugWorkarounds workarounds;
-    workarounds.max_texture_size = INT_MAX - 1;
     SetUpBase(workarounds, &image_factory_);
   }
 
