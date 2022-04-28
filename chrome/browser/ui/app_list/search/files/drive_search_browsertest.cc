202bd22234db5d4ab3ba610b08a9d510ac331a54
Rachel Wong <wrong@chromium.org>
2022-04-28 07:02:25 +0000

    [launcher] Delete unused files.
    This is a reland of crrev.com/2608491 but fixing broken includes in the
    browser_tests target.
    
    The test helpers are not included in any BUILD file, and they are
    actually duplicated at chrome/browser/ui/app_list/search/test/.
    
    Bug: 1258415
    Change-Id: I63994cd56c83785a9086fe96b03528a3720714ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611434
    Reviewed-by: Guoxing Zhao <charleszhao@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997069}

--- chrome/browser/ui/app_list/search/files/drive_search_browsertest.cc
+++ chrome/browser/ui/app_list/search/files/drive_search_browsertest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/drive/drivefs_test_support.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
-#include "chrome/browser/ui/app_list/search/app_list_search_test_helper.h"
 #include "chrome/browser/ui/app_list/search/files/file_result.h"
+#include "chrome/browser/ui/app_list/search/test/app_list_search_test_helper.h"
 
 namespace app_list {
 
--- chrome/browser/ui/app_list/search/help_app_search_browsertest.cc
+++ chrome/browser/ui/app_list/search/help_app_search_browsertest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
-#include "chrome/browser/ui/app_list/search/app_list_search_test_helper.h"
+#include "chrome/browser/ui/app_list/search/test/app_list_search_test_helper.h"
 #include "chrome/browser/ui/app_list/search/test/search_results_changed_waiter.h"
 #include "chrome/browser/ui/app_list/search/test/test_continue_files_search_provider.h"
 #include "chrome/browser/ui/browser.h"
