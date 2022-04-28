4827e3d5da7b4f09d541bda1aeb3979587e7f1bb
Kent Tamura <tkent@chromium.org>
2022-04-28 07:37:00 +0000

    DeferredShaping: Improve performance of FrameSelection::DidSetSelectionDeprecated
    It was slow because ScopedForcedUpdate requires additional cost on
    locking elements.
    
    This CL fixes timeout of editing/deleting/delete-many-lines-of-text.html
    with enabling DeferredShaping.
    
    crrev.com/996612 tried to fix the same issue. However it didn't work
    well because DeferredShapingDisallowScope works only inside
    LayoutView::UpdateLayout().
    
    Bug: 1259085
    Change-Id: I35c1da7570a702be4439e83c7a4ecba11ba5154e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610666
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997083}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/font_face_set_document.h"
 #include "third_party/blink/renderer/core/css/post_style_update_scope.h"
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_document_state.h"
 #include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/document_transition/document_transition_supplement.h"
 #include "third_party/blink/renderer/core/dom/static_node_list.h"
@@ -861,7 +862,13 @@ void LocalFrameView::PerformLayout() {
           RuntimeEnabledFeatures::DeferredShapingEnabled() &&
               default_allow_deferred_shaping_ && !frame_->PagePopupOwner() &&
               !FirstMeaningfulPaintDetector::From(*frame_->GetDocument())
-                   .SeenFirstMeaningfulPaint());
+                   .SeenFirstMeaningfulPaint() &&
+              // Locking many shaping-deferred elements is very slow if we have
+              // ScopedForcedUpdate instances.
+              // Without this check, PerformPostLayoutTasks() takes 200 seconds
+              // in editing/deleting/delete-many-lines-of-text.html with a
+              // debug build.
+              !document->GetDisplayLockDocumentState().HasForcedScopes());
       DeferredShapingViewportScope viewport_scope(*this, *GetLayoutView());
       GetLayoutView()->UpdateLayout();
     }
