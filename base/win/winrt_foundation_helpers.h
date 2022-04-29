aa450ad4accd47412e7297e06ccbde7443c68f0e
Alan Zhao <ayzhao@google.com>
2022-04-29 21:45:51 +0000

    Explicitly include <algorithm> in winrt_foundation_helpers.h
    In preparation for updating libc++, std::copy_n(...) will no longer be
    present in this file's existing headers, so we need to explicitly
    include the <algorithm> header.
    
    Bug: 1273285
    Change-Id: Ifd7541c603e95aff05731ddd75874d90809d3dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616093
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997932}

--- base/win/winrt_foundation_helpers.h
+++ base/win/winrt_foundation_helpers.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.foundation.h>
 #include <wrl/client.h>
 
+#include <algorithm>
 #include <vector>
 
 #include "base/win/hstring_compare.h"
