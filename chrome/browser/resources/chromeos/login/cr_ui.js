728c012c643c359d708608d86b1e5e8bcdda5521
Renato Silva <rrsilva@google.com>
2022-04-29 20:03:36 +0000

    OOBE - Remove usages of I18nTemplate
    Remove remaining usages of i18n_template_no_process from
    OOBE code. Also move i18n_setup.html to the top level so
    that it is next to i18n_setup.js.
    
    Bug: 1184731, 1128151, 677338, 919618
    Change-Id: I8fb2e2e2403ad210f0755e8f2727d989e1eb9e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613782
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997865}

--- chrome/browser/resources/chromeos/login/cr_ui.js
+++ chrome/browser/resources/chromeos/login/cr_ui.js
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // #import {assert} from 'chrome://resources/js/assert.m.js';
-// #import {i18nTemplate} from 'chrome://resources/js/i18n_template_no_process.m.js';
 // #import {$} from 'chrome://resources/js/util.m.js';
 // #import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
 
@@ -308,10 +307,33 @@ cr.define('cr.ui', function() {
     static reloadContent(data) {
       // Reload global local strings, process DOM tree again.
       loadTimeData.overrideValues(data);
-      i18nTemplate.process(document, loadTimeData);
+      Oobe.updateDocumentLocalizedStrings();
 
       // Update localized content of the screens.
       Oobe.getInstance().updateLocalizedContent_();
+    }
+
+    /**
+     * Update localized strings in tags that are used at the `document` level.
+     * These strings are used outside of a Polymer Element and cannot leverage
+     * I18nBehavior for it.
+     */
+    static updateDocumentLocalizedStrings() {
+      // Update attributes used in the <html> tag.
+      const attrToStrMap = {
+        lang: 'language',
+        dir: 'textdirection',
+        highlight: 'highlightStrength',
+      };
+      for (const [attribute, stringName] of Object.entries(attrToStrMap)) {
+        const localizedString = loadTimeData.getValue(stringName);
+        document.documentElement.setAttribute(attribute, localizedString);
+      }
+
+      // Update this standalone div in the main document.
+      const notice = loadTimeData.getValue('missingAPIKeysNotice');
+      const apiKeysNoticeDiv = $('api-keys-notice');
+      apiKeysNoticeDiv.textContent = notice;
     }
 
     /**
