3f958ab379f5ca08030f996ed28f228980cc87aa
Hongchan Choi <hongchan@chromium.org>
2022-04-29 16:48:10 +0000

    Add mjwilson@ to OWNERS for the WebAudio-related codes
    This CL adds mjwilson@ as an owner to anywhere hongchan@ is listed.
    
    Change-Id: I4c535fd2bbbf836c0dd765e0937ae1bb58b1d692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591041
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Auto-Submit: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997746}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -703,7 +703,7 @@ class BlinkPerfDisplayLocking(_BlinkPerfBenchmark):
       ['--enable-blink-features=DisplayLocking,CSSContentSize'])
 
 
-@benchmark.Info(emails=['hongchan@chromium.org'],
+@benchmark.Info(emails=['hongchan@chromium.org', 'mjwilson@chromium.org'],
                 component='Blink>WebAudio',
                 documentation_url='https://bit.ly/blink-perf-benchmarks')
 class BlinkPerfWebAudio(_BlinkPerfBenchmark):
