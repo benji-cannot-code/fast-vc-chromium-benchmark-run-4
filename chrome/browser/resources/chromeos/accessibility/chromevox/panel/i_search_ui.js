9826afc06914bbf0896cc025dcecee00327e2a3b
Anastasia Helfinstein <anastasi@google.com>
2022-04-29 20:49:24 +0000

    [ChromeVox] Migrate the ISearch object to new background class
    This change begins moving the core ISearch logic into the background
    context by migrating the ISearch object to a new class PanelBackground
    in the background context. Follow-up changes will remove the direct
    dependencies of ISearchUI on PanelBackground, and replace them with
    the bridge interface for communication between renderers.
    
    AX-Relnotes: n/a.
    Bug: 1288634
    Test: browser_tests --gtest_filter=*ChromeVox*ISearch*
    Change-Id: I6806123ed440509efba2cacee681869806bf1135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615640
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997898}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/i_search_ui.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/i_search_ui.js
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @fileoverview The driver for the UI for incremental search.
  */
-import {ISearch} from '/chromevox/background/panel/i_search.js';
 import {ISearchHandler} from '/chromevox/background/panel/i_search_handler.js';
+import {PanelBackground} from '/chromevox/background/panel/panel_background.js';
 import {PanelInterface} from '/chromevox/panel/panel_interface.js';
 
 const AutomationNode = chrome.automation.AutomationNode;
@@ -20,10 +20,12 @@ export class ISearchUI {
     /** @private {ChromeVoxState} */
     this.background_ =
         chrome.extension.getBackgroundPage()['ChromeVoxState']['instance'];
-    this.iSearch_ = new ISearch(this.background_.currentRange.start);
+    const panelBackground =
+        chrome.extension.getBackgroundPage().panelBackground;
+    panelBackground.createNewISearch();
+    panelBackground.setISearchHandler(this);
     this.input_ = input;
     this.dir_ = Dir.FORWARD;
-    this.iSearch_.handler = this;
 
     this.onKeyDown = this.onKeyDown.bind(this);
     this.onTextInput = this.onTextInput.bind(this);
@@ -68,21 +70,16 @@ export class ISearchUI {
         PanelInterface.instance.closeMenusAndRestoreFocus();
         return false;
       case 'Enter':
-        PanelInterface.instance.setPendingCallback(function() {
-          const node = this.iSearch_.cursor.node;
-          if (!node) {
-            return;
-          }
-          chrome.extension.getBackgroundPage()
-              .ChromeVoxState.instance['navigateToRange'](
-                  cursors.Range.fromNode(node));
-        }.bind(this));
+        PanelInterface.instance.setPendingCallback(
+            () => chrome.extension.getBackgroundPage()
+                      .panelBackground.setRangeToISearchNode());
         PanelInterface.instance.closeMenusAndRestoreFocus();
         return false;
       default:
         return false;
     }
-    this.iSearch_.search(this.input_.value, this.dir_, true);
+    chrome.extension.getBackgroundPage().panelBackground.incrementalSearch(
+        this.input_.value, this.dir_, true);
     evt.preventDefault();
     evt.stopPropagation();
     return false;
@@ -95,8 +92,8 @@ export class ISearchUI {
    */
   onTextInput(evt) {
     const searchStr = evt.target.value + evt.data;
-    this.iSearch_.clear();
-    this.iSearch_.search(searchStr, this.dir_);
+    chrome.extension.getBackgroundPage().panelBackground.incrementalSearch(
+        searchStr, this.dir_);
     return true;
   }
 
@@ -138,8 +135,7 @@ export class ISearchUI {
 
   /** Unregisters event handlers. */
   destroy() {
-    this.iSearch_.handler = null;
-    this.iSearch_ = null;
+    chrome.extension.getBackgroundPage().panelBackground.destroyISearch();
     const input = this.input_;
     this.input_ = null;
     input.removeEventListener('keydown', this.onKeyDown, true);
