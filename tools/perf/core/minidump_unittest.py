57b51018af32c660845b03281228dee9b9d3994c
Greg Thompson <grt@chromium.org>
2022-04-29 10:51:46 +0000

    Revert "Re-enable Kevin minidump tests"
    This reverts commit 1a7e2ec6602001a45c345b402ac5197d8ab2f5ff.
    
    Reason for revert: still failing; see https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-kevin-rel/42238/overview
    
    Original change's description:
    > Re-enable Kevin minidump tests
    >
    > Re-enables two minidump tests on Kevin ChromeOS devices. The root cause
    > that caused them to be flaky (flaky unsymbolizable minidumps) should be
    > fixed now.
    >
    > Bug: 1149237
    > Change-Id: Idd7f575f74ef3cbdf4ef1af2bd247f5da091644c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614896
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: John Chen <johnchen@chromium.org>
    > Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#997422}
    
    Bug: 1149237
    Change-Id: I8013a745f7caf090271c5806e483c9e1c52a783b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613778
    Owners-Override: Greg Thompson <grt@google.com>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#997627}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -63,6 +63,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
+      'chromeos-board-kevin',  # https://crbug.com/1149237
       'chromeos-local',
       'mac',  # https://crbug.com/1271097
       'win7'  # https://crbug.com/1084931
@@ -109,6 +110,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
+      'chromeos-board-kevin',  # https://crbug.com/1138060
       'chromeos-local',
       'mac',  # https://crbug.com/1271097
       'win7'  # https://crbug.com/1084931
