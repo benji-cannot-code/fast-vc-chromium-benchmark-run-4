042e07111763e551ccb0787ea2f609234c1a479f
François Beaufort <beaufort.francois@gmail.com>
2022-04-29 07:34:31 +0000

    bluetooth: Show Android notification when site is connected or scanning
    This CL makes sure an Android notification is shown when a website is
    connected to a Bluetooth device (device.gatt.connect) or when it is
    scanning for nearby Bluetooth devices (device.watchAdvertisements,
    requestLEScan). A new "Bluetooth" notification channel has been created
    for this and will only appear for users who are using this feature.
    
    This functionality is behind the experimental flag disabled by default:
    chrome://flags/#enable-web-bluetooth-new-permissions-backend
    
    Screenshots: https://photos.app.goo.gl/LEi7WmCa5c2BPC32A
    
    Bug: 577953, 1105961
    Change-Id: I0d56a6d350562be70cdeed867feb71aa8c78c69f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525414
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997584}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/app/bluetooth/BluetoothNotificationService.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.app.bluetooth;
+
+import org.chromium.base.annotations.IdentifierNameString;
+import org.chromium.chrome.browser.base.SplitCompatService;
+
+/** See {@link BluetoothNotificationServiceImpl}. */
+public class BluetoothNotificationService extends SplitCompatService {
+    @IdentifierNameString
+    private static String sImplClassName =
+            "org.chromium.chrome.browser.app.bluetooth.BluetoothNotificationServiceImpl";
+
+    public BluetoothNotificationService() {
+        super(sImplClassName);
+    }
+}
---
+++ chrome/browser/bluetooth/android/bluetooth_bridge.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/bluetooth/android/jni_headers/BluetoothBridge_jni.h"
+#include "content/public/browser/web_contents.h"
+
+jboolean JNI_BluetoothBridge_IsWebContentsConnectedToBluetoothDevice(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& java_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(java_web_contents);
+  return web_contents->IsConnectedToBluetoothDevice();
+}
+
+jboolean JNI_BluetoothBridge_IsWebContentsScanningForBluetoothDevices(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& java_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(java_web_contents);
+  return web_contents->IsScanningForBluetoothDevices();
+}
