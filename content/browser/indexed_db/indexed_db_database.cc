2034c7dc7558f642e64e400359c621c3e05c87c9
Christine Smith <christinesm@chromium.org>
2022-04-29 18:30:15 +0000

    [Storage Code Health] Remove unused histogram: WebCore.IndexedDB.Database.OutstandingTransactionCount.*
    Removes the unused
    WebCore.IndexedDB.Database.OutstandingTransactionCount histogram.
    
    Bug: 1050618
    Change-Id: Ia1d37ea23cdcd2e529d3168307c45dac59f7f428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614032
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997816}

--- content/browser/indexed_db/indexed_db_database.cc
+++ content/browser/indexed_db/indexed_db_database.cc
@@ -331,9 +331,6 @@ void IndexedDBDatabase::Commit(IndexedDBTransaction* transaction) {
 }
 
 void IndexedDBDatabase::TransactionCreated() {
-  UMA_HISTOGRAM_COUNTS_1000(
-      "WebCore.IndexedDB.Database.OutstandingTransactionCount",
-      transaction_count_);
   ++transaction_count_;
 }
 
