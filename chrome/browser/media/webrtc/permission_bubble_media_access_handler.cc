803b3c6fa1639d1dce0fc78b7f99642040f643d1
Gabriel Charette <gab@chromium.org>
2022-04-29 16:13:33 +0000

    [webrtc] Migrate system_media_permissions away from deprecated base::PostTask
    Using a reply-on-requesting-thread scheme instead of binding
    BrowserThread::UI as a task trait (this was the last use case doing
    this).
    
    Stop handling NOTREACHED() cases (per Chromium style guide) and
    replace NOTREACHED() by CHECK(false) as crbug.com/1312530 made it
    apparent that this code doesn't run behind a dcheck-enabled build.
    
    Bug: 1026641, 1312530
    Change-Id: Iec21dbf6fd02595f064749bcfbd7798ccbbd0d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612125
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997737}

--- chrome/browser/media/webrtc/permission_bubble_media_access_handler.cc
+++ chrome/browser/media/webrtc/permission_bubble_media_access_handler.cc
@@ -378,8 +378,7 @@ void PermissionBubbleMediaAccessHandler::OnAccessRequestResponse(
             base::BindOnce(
                 &PermissionBubbleMediaAccessHandler::OnAccessRequestResponse,
                 weak_factory_.GetWeakPtr(), web_contents, request_id, devices,
-                result, std::move(ui)),
-            {content::BrowserThread::UI});
+                result, std::move(ui)));
         return;
       } else if (system_audio_permission == SystemPermission::kRestricted ||
                  system_audio_permission == SystemPermission::kDenied) {
@@ -406,8 +405,7 @@ void PermissionBubbleMediaAccessHandler::OnAccessRequestResponse(
             base::BindOnce(
                 &PermissionBubbleMediaAccessHandler::OnAccessRequestResponse,
                 weak_factory_.GetWeakPtr(), web_contents, request_id, devices,
-                result, std::move(ui)),
-            {content::BrowserThread::UI});
+                result, std::move(ui)));
         return;
       } else if (system_video_permission == SystemPermission::kRestricted ||
                  system_video_permission == SystemPermission::kDenied) {
--- chrome/browser/ui/content_settings/media_authorization_wrapper_test.h
+++ chrome/browser/ui/content_settings/media_authorization_wrapper_test.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AVFoundation/AVFoundation.h>
 
 #include "base/callback.h"
-#include "base/task/task_traits.h"
 #include "chrome/browser/media/webrtc/media_authorization_wrapper_mac.h"
 
 enum AuthStatus {
@@ -34,8 +33,7 @@ class MediaAuthorizationWrapperTest final
   // MediaAuthorizationWrapper:
   NSInteger AuthorizationStatusForMediaType(NSString* media_type) override;
   void RequestAccessForMediaType(NSString* media_type,
-                                 base::OnceClosure callback,
-                                 const base::TaskTraits& traits) override {}
+                                 base::OnceClosure callback) override {}
 
  private:
   AuthStatus permission_status_ = kNotDetermined;
