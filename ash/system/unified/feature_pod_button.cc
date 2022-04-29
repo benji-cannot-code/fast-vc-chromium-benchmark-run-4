fd429cca56d39102468a0bb24ec6ff5b9713a975
minch <minch@chromium.org>
2022-04-29 19:55:01 +0000

    dark_mode: Live update for the focus ring inside quick settings
    Adding color live update for the UIs inside quick settings.
    - Avatar button.
    - Feature pod label button.
    
    Bug: 1312223
    Change-Id: I2ef886e58c4278b9a9e4da8e9355cfbbee42228c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614256
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997859}

--- ash/system/unified/feature_pod_button.cc
+++ ash/system/unified/feature_pod_button.cc
@@ -93,9 +93,6 @@ FeaturePodLabelButton::FeaturePodLabelButton(PressedCallback callback)
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
 
-  views::FocusRing::Get(this)->SetColor(
-      AshColorProvider::Get()->GetControlsLayerColor(
-          ControlsLayerType::kFocusRingColor));
   views::InstallRoundRectHighlightPathGenerator(
       this, gfx::Insets(), kUnifiedFeaturePodHoverCornerRadius);
 }
@@ -157,6 +154,10 @@ const char* FeaturePodLabelButton::GetClassName() const {
 void FeaturePodLabelButton::OnThemeChanged() {
   views::Button::OnThemeChanged();
   OnEnabledChanged();
+
+  views::FocusRing::Get(this)->SetColor(
+      AshColorProvider::Get()->GetControlsLayerColor(
+          ControlsLayerType::kFocusRingColor));
 }
 
 void FeaturePodLabelButton::SetLabel(const std::u16string& label) {
