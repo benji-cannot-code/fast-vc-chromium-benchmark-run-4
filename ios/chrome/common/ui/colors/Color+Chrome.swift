dde71b9bec4ece3783af6f5c981115adfdbf8f26
Robbie Gibson <rkgibson@google.com>
2022-04-29 21:17:51 +0000

    [iOS] Update new omnibox popup treatment one background/separators
    The colors now match the existing colors for the old omnibox popup.
    
    Fixed: 1320780, 1320782
    Change-Id: I23507a8c285c67b516c831b4ed6c82ca56839d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615311
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#997916}

--- ios/chrome/common/ui/colors/Color+Chrome.swift
+++ ios/chrome/common/ui/colors/Color+Chrome.swift
@@ -7,6 +7,11 @@ import SwiftUI
 
 // Adds easy SwiftUI access to the Chrome color palette.
 extension Color {
+  /// The background color.
+  public static var cr_background: Color {
+    return Color(kBackgroundColor)
+  }
+
   /// The primary text color.
   public static var cr_textPrimaryColor: Color {
     return Color(kTextPrimaryColor)
@@ -70,5 +75,10 @@ extension Color {
   /// The table row view highlight color.
   public static var cr_tableRowViewHighlight: Color {
     return Color(kTableViewRowHighlightColor)
+  }
+
+  /// The table view sseparator color.
+  public static var cr_separator: Color {
+    return Color(kSeparatorColor)
   }
 }
