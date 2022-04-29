48191e868d25a14e486d4f9566d6bfd2c5dfed7c
Stepan Khapugin <stkhapugin@chromium.org>
2022-04-29 19:02:24 +0000

    [iOS] Limit suggestion height at 320pt.
    Makes the popup rows not exceed 320 points, which still allows for large
    dynamic type settings, but prevents SwiftUI from crashing because of the
    layout not stabilizing in one pass.
    
    Fixed: 1321149
    Change-Id: I47e05dd3d26dae14c0f782e531b5a447ce7d24c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613663
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#997831}

--- ios/chrome/browser/ui/omnibox/popup/shared/popup_match_row_view.swift
+++ ios/chrome/browser/ui/omnibox/popup/shared/popup_match_row_view.swift
@@ -20,7 +20,7 @@ struct PopupMatchRowView: View {
     static let actionButtonOuterPadding = EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0)
     static let leadingSpacing: CGFloat = 60
     static let minHeight: CGFloat = 58
-    static let maxHeight: CGFloat = 98
+    static let maxHeight: CGFloat = 320
     static let padding = EdgeInsets(top: 9, leading: 0, bottom: 9, trailing: 16)
   }
 
@@ -148,7 +148,7 @@ struct PopupMatchRowView: View {
       }
       .padding(Dimensions.padding)
     }
-    .frame(maxWidth: .infinity, minHeight: Dimensions.minHeight)
+    .frame(maxWidth: .infinity, minHeight: Dimensions.minHeight, maxHeight: Dimensions.maxHeight)
   }
 
   var backgroundColor: Color {
