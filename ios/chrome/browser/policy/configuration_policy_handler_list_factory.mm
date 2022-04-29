ff83124a58dc0f1bb3c40bf26b3d7f2957fd09bc
Sorin Jianu <sorin@chromium.org>
2022-04-29 17:47:01 +0000

    Fix ComponentUpdatesEnabled enterprise policy doesn't exist on iOS.
    This CL adds an entry for policy::key::kComponentUpdatesEnabled in
    the PolicyToPreferenceMapEntry. It looks like such an entry was
    missed was missed when this policy has been added by
    https://codereview.chromium.org/2257363002/
    
    In addition, the CL enables testing of the policy for Android.
    
    Bug: 1320552
    Change-Id: If6273713a671bdb2ede51af068a031e7777ffb7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615515
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997781}

--- ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_credit_card_policy_handler.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/bookmarks/managed/managed_bookmarks_policy_handler.h"
+#include "components/component_updater/pref_names.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/enterprise/browser/reporting/cloud_reporting_frequency_policy_handler.h"
 #include "components/enterprise/browser/reporting/cloud_reporting_policy_handler.h"
@@ -55,6 +56,9 @@ namespace {
 const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { policy::key::kAllowChromeDataInBackups,
     prefs::kAllowChromeDataInBackups,
+    base::Value::Type::BOOLEAN },
+  { policy::key::kComponentUpdatesEnabled,
+    prefs::kComponentUpdatesEnabled,
     base::Value::Type::BOOLEAN },
   { policy::key::kChromeVariations,
     variations::prefs::kVariationsRestrictionsByPolicy,
