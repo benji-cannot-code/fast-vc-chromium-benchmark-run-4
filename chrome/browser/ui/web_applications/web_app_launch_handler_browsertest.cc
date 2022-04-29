a69bfff08624f647c31ed7dd8b77a5e4c10ee73e
Carlos Caballero Grolimund <carlscab@google.com>
2022-04-29 12:03:43 +0000

    Revert "dPWA: Enable File Handling by default"
    This reverts commit 2d575eeda930149beed810d426bfddec41d315a1.
    
    Reason for revert: https://crbug.com/1321097
    
    Bug: 1321097
    
    Original change's description:
    > dPWA: Enable File Handling by default
    >
    > This will be cherry picked to m102, but should a blocker come up before
    > May 17, we'll re-disable on trunk and m102.
    >
    > i2s: https://groups.google.com/a/chromium.org/g/blink-dev/c/Wxuo4lZi4vM
    >
    > Bug: 829689,1284364
    > Change-Id: Ib5e1232ae97dacb5bd6263832b8cd34354988536
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575676
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#997318}
    
    Bug: 829689,1284364
    Change-Id: If0474c78965018ba890c5a10fff7396877312512
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613319
    Owners-Override: Carlos Caballero Grolimund <carlscab@google.com>
    Commit-Queue: Carlos Caballero Grolimund <carlscab@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#997641}

--- chrome/browser/ui/web_applications/web_app_launch_handler_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_launch_handler_browsertest.cc
@@ -439,9 +439,8 @@ IN_PROC_BROWSER_TEST_F(WebAppLaunchHandlerBrowserTest, GlobalLaunchQueue) {
 class WebAppLaunchHandlerDisabledBrowserTest : public InProcessBrowserTest {
  public:
   WebAppLaunchHandlerDisabledBrowserTest() {
-    feature_list_.InitWithFeatures({},
-                                   {blink::features::kWebAppEnableLaunchHandler,
-                                    blink::features::kFileHandlingAPI});
+    feature_list_.InitAndDisableFeature(
+        blink::features::kWebAppEnableLaunchHandler);
   }
   ~WebAppLaunchHandlerDisabledBrowserTest() override = default;
 
@@ -576,7 +575,8 @@ IN_PROC_BROWSER_TEST_F(WebAppLaunchHandlerOriginTrialBrowserTest, OriginTrial) {
       }));
 
   // Install web app with origin trial token.
-  AppId app_id = InstallWebAppFromPage(browser(), GURL(kTestWebAppUrl));
+  AppId app_id =
+      web_app::InstallWebAppFromPage(browser(), GURL(kTestWebAppUrl));
 
   // Origin trial should grant the app access.
   WebAppProvider& provider = *WebAppProvider::GetForTest(browser()->profile());
