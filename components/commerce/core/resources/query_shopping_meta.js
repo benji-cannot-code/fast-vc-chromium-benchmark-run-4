d6d9728d2505f4f60e528710842254f5341a0fb0
Matt Jones <mdjones@chromium.org>
2022-04-29 17:38:52 +0000

    Move shopping list js resource to component
    This patch moves the javascript used for on-page shopping data
    extraction to the commerce component. The existing commerce grdp file
    has been renamed for general use (outside if hints).
    
    Change-Id: I97c7ca5ff923cb4088b342c9601248c142c09007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614040
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997778}

---
+++ components/commerce/core/resources/query_shopping_meta.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+const metaTags = document.getElementsByTagName('meta');
+const output = {};
+for (let i = 0; i < metaTags.length; i++) {
+  const curMeta = metaTags[i];
+  const name = curMeta.getAttribute('property');
+  const value = curMeta.getAttribute('content');
+  if (!name || !value) {
+    continue;
+  }
+  // "og" in this context refers to "open graph" rather than "optimization
+  // guide".
+  if (name.startsWith('og:')) {
+    output[name.substring(3)] = value;
+  }
+}
+return JSON.stringify(output);
+})();
