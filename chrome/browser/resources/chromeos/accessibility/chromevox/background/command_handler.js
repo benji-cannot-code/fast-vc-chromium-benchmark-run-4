d09221e1ca969c2f270f576ecc1758a75e579697
Anastasia Helfinstein <anastasi@google.com>
2022-04-29 00:38:43 +0000

    [ChromeVox] Migrate CommandHandler to use BackgroundBridge
    Continuing the process started in this CL[1], convert another class to
    use BackgroundBridge as the interface between the background renderer
    and other renderers (e.g. options, panel).
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3585078
    
    AX-Relnotes: n/a.
    Bug: 1288634
    Test: browser_tests --gtest_filter=*ChromeVox*Panel*
    Change-Id: Ia4744b44e612961a15cd8db11dca13974dbbec2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606735
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#997494}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
@@ -1535,3 +1535,7 @@ export class CommandHandler extends CommandHandlerInterface {
 }
 
 CommandHandlerInterface.instance = new CommandHandler();
+
+BridgeHelper.registerHandler(
+    BridgeTarget.COMMAND_HANDLER, BridgeAction.ON_COMMAND,
+    (command) => CommandHandlerInterface.instance.onCommand(command));
--- chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
@@ -17,6 +17,7 @@ BridgeTarget = {
   BRAILLE_BACKGROUND: 'BrailleBackground',
   CHROMEVOX_PREFS: 'ChromeVoxPrefs',
   CHROMEVOX_STATE: 'ChromeVoxState',
+  COMMAND_HANDLER: 'CommandHandler',
   LOG_STORE: 'LogStore',
 };
 
@@ -29,6 +30,7 @@ BridgeAction = {
   CLEAR_LOG: 'clearLog',
   GET_LOGS: 'getLogs',
   GET_PREFS: 'getPrefs',
+  ON_COMMAND: 'onCommand',
   REFRESH_BRAILLE_TABLE: 'refreshBrailleTable',
   SET_LOGGING_PREFS: 'setLoggingPrefs',
   SET_PREF: 'setPref',
