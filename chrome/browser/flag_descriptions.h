0885f6d0e7eccb91cb4433533505d89a170528ff
Fergus Dall <sidereal@google.com>
2022-04-29 08:39:22 +0000

    Add a flag for the generic GuestOS installer
    Bug: b:230812129
    Change-Id: Ib0dc634d5ea959c25990115d9fdb61d65272a903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616270
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#997596}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2512,6 +2512,9 @@ extern const char kCrostiniImeSupportDescription[];
 extern const char kCrostiniVirtualKeyboardSupportName[];
 extern const char kCrostiniVirtualKeyboardSupportDescription[];
 
+extern const char kGuestOSGenericInstallerName[];
+extern const char kGuestOSGenericInstallerDescription[];
+
 extern const char kBruschettaName[];
 extern const char kBruschettaDescription[];
 
