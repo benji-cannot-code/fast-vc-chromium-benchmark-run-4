1bfd464c892eb7e9d2eb8e2f271b94cdf5fa4298
John Lee <johntlee@chromium.org>
2022-04-29 03:46:36 +0000

    Privacy Guide V2: Add animations to confetti on last page
    This CL adds separate graphics for the last step of Privacy Guide V2 and
    adds separate animations for each of the confetti pieces in the graphic.
    The confetti pieces are colored differently for light vs. dark mode
    using CSS masks instead of separate SVGs to avoid having multiple files
    for just 1 difference of color.
    
    Video of animation: crbug.com/1215630#c194
    
    Bug: 1215630
    Change-Id: If62b3bfc2332f68fa495dcd11fb3071f441aefe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612138
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997552}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
@@ -44,6 +44,11 @@ export class PrivacyGuideCompletionFragmentElement extends
         type: Boolean,
         value: false,
       },
+
+      enablePrivacyGuide2_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('privacyGuide2Enabled'),
+      },
     };
   }
 
@@ -61,7 +66,7 @@ export class PrivacyGuideCompletionFragmentElement extends
   }
 
   override focus() {
-    this.shadowRoot!.querySelector<HTMLElement>('.headline-container')!.focus();
+    this.shadowRoot!.querySelector<HTMLElement>('[focus-element]')!.focus();
   }
 
   /**
