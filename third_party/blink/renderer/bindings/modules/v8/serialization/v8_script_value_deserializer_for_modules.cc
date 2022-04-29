83c6f3c4ab7689ceb7442d05bab0d4f94a830f29
Tony Herre <toprice@chromium.org>
2022-04-29 15:35:05 +0000

    Rename TransferredMediaStreamTrack creation factory method
    The old MediaStreamTrack::Create was getting confusing - it sounded like
    a generic factory for MST instances, and is easily confused with
    eg MediaStreamTrackImpl::Create().
    
    Bug: 1288839
    Change-Id: I92970f3a0d0118e28a097d5f050df6f788c01ab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613339
    Quick-Run: Tony Herre <toprice@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997723}

--- third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.cc
+++ third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.cc
@@ -548,7 +548,7 @@ MediaStreamTrack* V8ScriptValueDeserializerForModules::ReadMediaStreamTrack() {
     return nullptr;
   }
 
-  return MediaStreamTrack::Create(
+  return MediaStreamTrack::FromTransferredState(
       GetScriptState(),
       TransferredValues{.session_id = session_id,
                         .kind = kind,
