727d5b6576f54d661a735523efebcbcd7b5bd605
Fan Yang <fayang@chromium.org>
2022-04-29 01:42:42 +0000

    Add two histograms to log the main job wait time while there is no QUIC session.
    Change-Id: I2ac4b3b885e03cc3d71fcab7cab12b988ed04087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612236
    Commit-Queue: Fan Yang <fayang@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997520}

--- net/spdy/spdy_network_transaction_unittest.cc
+++ net/spdy/spdy_network_transaction_unittest.cc
@@ -5417,6 +5417,8 @@ TEST_F(SpdyNetworkTransactionTest, GracefulGoaway) {
                      PRIVACY_MODE_DISABLED,
                      SpdySessionKey::IsProxySession::kFalse, SocketTag(),
                      NetworkIsolationKey(), SecureDnsPolicy::kAllow);
+  EXPECT_TRUE(
+      spdy_session_pool->HasAvailableSession(key, /* is_websocket = */ false));
   base::WeakPtr<SpdySession> spdy_session =
       spdy_session_pool->FindAvailableSession(
           key, /* enable_ip_based_pooling = */ true,
@@ -5452,6 +5454,8 @@ TEST_F(SpdyNetworkTransactionTest, GracefulGoaway) {
   EXPECT_EQ("hello!", response_data);
 
   // Graceful GOAWAY was received, SpdySession should be unavailable.
+  EXPECT_FALSE(
+      spdy_session_pool->HasAvailableSession(key, /* is_websocket = */ false));
   spdy_session = spdy_session_pool->FindAvailableSession(
       key, /* enable_ip_based_pooling = */ true,
       /* is_websocket = */ false, log_);
@@ -9326,6 +9330,8 @@ TEST_F(SpdyNetworkTransactionTest,
                       ProxyServer::Direct(), PRIVACY_MODE_DISABLED,
                       SpdySessionKey::IsProxySession::kFalse, SocketTag(),
                       NetworkIsolationKey(), SecureDnsPolicy::kAllow);
+  EXPECT_TRUE(helper.session()->spdy_session_pool()->HasAvailableSession(
+      key1, /* is_websocket = */ false));
   base::WeakPtr<SpdySession> spdy_session1 =
       helper.session()->spdy_session_pool()->FindAvailableSession(
           key1, /* enable_ip_based_pooling = */ true,
@@ -9341,6 +9347,8 @@ TEST_F(SpdyNetworkTransactionTest,
                       ProxyServer::Direct(), PRIVACY_MODE_DISABLED,
                       SpdySessionKey::IsProxySession::kFalse, SocketTag(),
                       NetworkIsolationKey(), SecureDnsPolicy::kAllow);
+  EXPECT_TRUE(helper.session()->spdy_session_pool()->HasAvailableSession(
+      key2, /* is_websocket = */ true));
   base::WeakPtr<SpdySession> spdy_session2 =
       helper.session()->spdy_session_pool()->FindAvailableSession(
           key1, /* enable_ip_based_pooling = */ true,
