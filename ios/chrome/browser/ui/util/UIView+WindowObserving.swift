ba0a552f98ff096e7dff0179e8df211542152071
Louis Romero <lpromero@google.com>
2022-04-29 17:53:14 +0000

    [iOS] Add LayoutGuideCenter
    This CL adds several utils and the LayoutGuideCenter.
    Context: go/extending-namedguide
    
    This is meant as a replacement to NamedGuide. This new system supports
    referencing across windows, and doesn't require to add layout guides
    at a root view like in the BVC view.
    
    Bug: 1318788
    Change-Id: I1df417de32330f5f65f8aef31c242211c9c91c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599044
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997788}

--- ios/chrome/browser/ui/util/UIView+WindowObserving.swift
+++ ios/chrome/browser/ui/util/UIView+WindowObserving.swift
@@ -8,8 +8,7 @@ import UIKit
 
 /// Extends UIView to notify via KVO when it moved to a new window.
 extension UIView {
-  /// Whether the original and alternative implementations have been swapped.
-  private static var swizzled = false
+  /// MARK: Public
 
   /// Whether all instances of UIView will notify key-value observers for their `window` property.
   ///
@@ -43,6 +42,11 @@ extension UIView {
   @objc class func automaticallyNotifiesObserversOfWindow() -> Bool {
     return false
   }
+
+  /// MARK: Private
+
+  /// Whether the original and alternative implementations have been swapped.
+  private static var swizzled = false
 
   /// Adds a call to the KVO `willChangeValue(forKey:)` method.
   @objc private func cr_willMove(toWindow newWindow: UIWindow?) {
