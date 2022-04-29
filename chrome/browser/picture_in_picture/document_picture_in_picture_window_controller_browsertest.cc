7b4cfcad874b94fab3461271428c7b23ac5a0f00
Frank Liberato <liberato@chromium.org>
2022-04-29 18:27:43 +0000

    Reland "Switch document Picture in Picture (v2) to popup"
    This is a reland of commit aa97cd0880cc55c60e989cbe5bf8b8714f79f5a4
    
    This version turns off lacros support, since it's unclear that
    wayland supports always-on-top windows in the same way as other
    windowing systems anyway.  Figuring out how to fix that is
    probably a fair bit of work.  It's preferable not to block the
    rest of pip 2.0 development on it.
    
    Original change's description:
    > Switch document Picture in Picture (v2) to popup
    >
    > Since we are including more "normal" browser features, like
    > permissions, in picture in picture windows, it makes sense to
    > normalize pip windows to be their own browser window type.
    >
    > Previously, pip popup windows used a window that was created and
    > managed by a DocumentPictureInPictureWindowController.  This work
    > is now done by the browser, similar to how regular popup windows are
    > created.  In the future, the business logic will still reside in a
    > special pip controller (e.g., "close the pip window when the
    > opener window navigates away"), but this CL turns all of this off
    > to keep the CL more focused.
    >
    > Since pip v2 is currently turned off behind a flag, this doesn't
    > break anything.
    >
    > As part of this, this CL turns off some of the pip tests that don't
    > work anymore.  These will be re-enabled in a follow-up CL that
    > fixes the pip window controller.
    >
    > Change-Id: I4aa3cbffc5e1cef91d1c99b0e7eaf0583732ed7e
    > Bug: 1317445
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576675
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Fr <beaufort.francois@gmail.com>
    > Commit-Queue: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#994933}
    
    Bug: 1317445, 1320453
    Change-Id: I209f47494a9240dc54b9da375cac12f7f7a03f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611962
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997814}

--- chrome/browser/picture_in_picture/document_picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/document_picture_in_picture_window_controller_browsertest.cc
@@ -153,7 +153,7 @@ class DocumentPictureInPictureWindowControllerBrowserTest
 // Checks the creation of the window controller, as well as basic window
 // creation, visibility and activation.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       CreationAndVisibilityAndActivation) {
+                       DISABLED_CreationAndVisibilityAndActivation) {
   LoadTabAndEnterPictureInPicture(browser());
 
   gfx::NativeWindow native_window = GetOverlayWindow()->GetNativeWindow();
@@ -164,7 +164,7 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 // window twice in a row should work, closing the old window before opening the
 // new one.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       CreateTwice) {
+                       DISABLED_CreateTwice) {
   LoadTabAndEnterPictureInPicture(browser());
 
   gfx::NativeWindow native_window_1 = GetOverlayWindow()->GetNativeWindow();
@@ -192,7 +192,7 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 
 // Tests closing the document picture-in-picture window.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       CloseWindow) {
+                       DISABLED_CloseWindow) {
   LoadTabAndEnterPictureInPicture(browser());
 
   window_controller()->Close(/*should_pause_video=*/true);
@@ -202,7 +202,7 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 
 // Tests navigating the opener closes the picture in picture window.
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       ClosePictureInPictureWhenOpenerNavigates) {
+                       DISABLED_ClosePictureInPictureWhenOpenerNavigates) {
   LoadTabAndEnterPictureInPicture(browser());
   GURL test_page_url = ui_test_utils::GetTestUrl(
       base::FilePath(base::FilePath::kCurrentDirectory),
@@ -212,8 +212,9 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 }
 
 // Navigation by the pip window to a new document should close the pip window.
-IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       CloseOnPictureInPictureNavigationToNewDocument) {
+IN_PROC_BROWSER_TEST_F(
+    DocumentPictureInPictureWindowControllerBrowserTest,
+    DISABLED_CloseOnPictureInPictureNavigationToNewDocument) {
   LoadTabAndEnterPictureInPicture(browser());
 
   content::WebContents* active_web_contents =
@@ -226,8 +227,9 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 }
 
 // Navigation within the pip window's document should not close the pip window.
-IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       DoNotCloseOnPictureInPictureNavigationInsideDocument) {
+IN_PROC_BROWSER_TEST_F(
+    DocumentPictureInPictureWindowControllerBrowserTest,
+    DISABLED_DoNotCloseOnPictureInPictureNavigationInsideDocument) {
   LoadTabAndEnterPictureInPicture(browser());
 
   content::WebContents* active_web_contents =
@@ -239,7 +241,7 @@ IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DocumentPictureInPictureWindowControllerBrowserTest,
-                       RequiresSecureContext) {
+                       DISABLED_RequiresSecureContext) {
   GURL test_page_url("http://media/picture-in-picture/blank.html");
   ASSERT_FALSE(network::IsUrlPotentiallyTrustworthy(test_page_url));
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), test_page_url));
