cdfce3e1abe8a1d1a5e54c1d5d10addfb9613f61
Jian Li <jianli@chromium.org>
2022-04-29 19:48:15 +0000

    Refresh NTP when reload button is clicked
    Bug: 1246433
    Change-Id: Id90c0fd91684195d69c534341d3f1d1d633e8a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152204
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997854}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -906,6 +906,11 @@ public class NewTabPage implements NativePage, InvalidationAwareThumbnailProvide
     public void updateForUrl(String url) {
     }
 
+    @Override
+    public void reload() {
+        mFeedSurfaceProvider.reload();
+    }
+
     // InvalidationAwareThumbnailProvider
 
     @Override
--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedSurfaceProvider.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/FeedSurfaceProvider.java
@@ -57,4 +57,9 @@ public interface FeedSurfaceProvider {
      */
     @Nullable
     FeedReliabilityLogger getReliabilityLogger();
+
+    /**
+     * Reloads the contents.
+     */
+    void reload();
 }
