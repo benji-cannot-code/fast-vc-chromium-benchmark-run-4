d6295e34e17bc5aa4411169f7bc3221fe8aa9bae
Mohamed Amir Yosef <mamir@chromium.org>
2022-04-29 12:31:16 +0000

    [Passwords] Reduce the space after the icon in the generation prompt
    Upon UX feedback, this CL reduces the space after the password manager
    icon in the generation prompt to match the distance after other icons
    in the Autofill dropdown.
    
    Screenshot: http://screen/77QiuViFcR5jicb
    
    Bug: 1309480
    Change-Id: Icc8161f37aec39f2697de87c3cbe634cb52d2ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613776
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997651}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/bubble/bubble_border.h"
+#include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
@@ -92,9 +93,21 @@ class AutofillPopupBaseView::Widget : public views::Widget {
   const raw_ptr<AutofillPopupBaseView> autofill_popup_base_view_;
 };
 
+// static
 int AutofillPopupBaseView::GetCornerRadius() {
   return ChromeLayoutProvider::Get()->GetCornerRadiusMetric(
       views::Emphasis::kMedium);
+}
+
+// static
+int AutofillPopupBaseView::GetHorizontalMargin() {
+  return views::MenuConfig::instance().item_horizontal_padding +
+         GetCornerRadius();
+}
+
+// static
+int AutofillPopupBaseView::GetHorizontalPadding() {
+  return GetHorizontalMargin();
 }
 
 SkColor AutofillPopupBaseView::GetBackgroundColor() const {
