0699b0dd520b597f36c1b3d1265e2c573446ef72
Andrew Paseltiner <apaseltiner@chromium.org>
2022-04-29 13:10:42 +0000

    Replace MaybeEnqueueEventForTesting with attribution event handlers
    MaybeEnqueueEventForTesting provides no benefit over direct use of
    HandleSource/HandleTrigger.
    
    Change-Id: I723da9408b2dd2a4d3ee4709ab6b56e9954338ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614470
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997664}

--- content/browser/attribution_reporting/attribution_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_manager_impl.cc
@@ -380,11 +380,6 @@ void AttributionManagerImpl::MaybeEnqueueEvent(SourceOrTrigger event) {
     ProcessEvents();
 }
 
-void AttributionManagerImpl::MaybeEnqueueEventForTesting(
-    SourceOrTrigger event) {
-  MaybeEnqueueEvent(std::move(event));
-}
-
 void AttributionManagerImpl::ProcessEvents() {
   struct DebugCookieOriginGetter {
     const url::Origin* operator()(const StorableSource& source) const {
