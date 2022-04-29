9479b1d4ed2a5eeef29d84862f82a5a4a6424399
Christian Xu <christianxu@google.com>
2022-04-29 20:55:01 +0000

    [iOS][Omnibox] Add iPad suggestions scrolling flag
    Add feature flag to enable suggestions scrolling on iPad and disable
    suggestions hiding when dismissing the keyboard.
    
    Bug: 1320759
    Change-Id: I1540d49cbb5b44928a22d78ea9c8686a3929e667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613317
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997900}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/first_run/fre_field_trial.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
+#import "ios/chrome/browser/ui/omnibox/omnibox_ui_features.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_features.h"
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
@@ -882,6 +883,10 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSyncInvalidationsWalletAndOfferDescription,
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(::syncer::kUseSyncInvalidationsForWalletAndOffer)},
+    {"suggestions-scrolling-ipad",
+     flag_descriptions::kEnableSuggestionsScrollingOnIPadName,
+     flag_descriptions::kEnableSuggestionsScrollingOnIPadDescription,
+     flags_ui::kOsIos, FEATURE_VALUE_TYPE(kEnableSuggestionsScrollingOnIPad)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
