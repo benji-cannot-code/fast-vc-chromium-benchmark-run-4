ae65fe65765d832d227699fe6904fd815cad14e5
Gabriel Charette <gab@chromium.org>
2022-04-29 21:35:53 +0000

    [base] Remove remaining post_task.h includes
    R=fdoray@chromium.org
    
    Bug: 1026641
    Change-Id: I81a31b7adb42cc3ac0646f6ecd547d23d8419e12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616092
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Owners-Override: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997927}

--- chrome/browser/ui/webui/image_editor/image_editor_untrusted_ui.cc
+++ chrome/browser/ui/webui/image_editor/image_editor_untrusted_ui.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_piece.h"
-#include "base/task/post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_runner_util.h"
 #include "base/task/thread_pool.h"
--- components/reporting/storage/storage_queue.cc
+++ components/reporting/storage/storage_queue.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
-#include "base/task/post_task.h"
 #include "base/task/task_runner.h"
 #include "base/task/thread_pool.h"
 #include "components/reporting/compression/compression_module.h"
