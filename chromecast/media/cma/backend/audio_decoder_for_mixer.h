4793607242f2c53d7936ff615920d209a78db8d0
Ken MacKay <kmackay@chromium.org>
2022-04-29 04:21:03 +0000

    [Chromecast] Improve audio clock simulation quality
    Use existing resampler code to simulate clock rate changes
    
    Bug: internal b/229267489
    Change-Id: I3180172d6123f7add20a894481a17b82bb4e5634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615970
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997557}

--- chromecast/media/cma/backend/audio_decoder_for_mixer.h
+++ chromecast/media/cma/backend/audio_decoder_for_mixer.h
@@ -43,7 +43,7 @@ class AudioDecoderForMixer
   ~AudioDecoderForMixer() override;
 
   virtual void Initialize();
-  virtual bool Start(int64_t pts, bool start_playback_asap);
+  virtual bool Start(int64_t pts, bool av_sync_enabled);
   void StartPlaybackAt(int64_t timestamp);
   virtual void Stop();
   virtual bool Pause();
@@ -81,7 +81,7 @@ class AudioDecoderForMixer
   void OnMixerError() override;
 
   void CreateBufferPool(const AudioConfig& config, int frame_count);
-  void CreateMixerInput(const AudioConfig& config, bool start_playback_asap);
+  void CreateMixerInput(const AudioConfig& config, bool av_sync_enabled);
   void CleanUpPcm();
   void ResetMixerInputForNewConfig(const AudioConfig& config);
   void CreateDecoder();
@@ -129,7 +129,7 @@ class AudioDecoderForMixer
   scoped_refptr<IOBufferPool> buffer_pool_;
 
   int64_t playback_start_pts_ = 0;
-  bool start_playback_asap_ = false;
+  bool av_sync_enabled_ = false;
 
   base::WeakPtrFactory<AudioDecoderForMixer> weak_factory_;
 };
