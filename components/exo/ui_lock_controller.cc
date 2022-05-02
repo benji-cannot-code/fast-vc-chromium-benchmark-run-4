dc0506db49ee3c1429d63c4f602aee2dd6a819d3
Chloe Pelling <cpelling@google.com>
2022-05-02 07:38:47 +0000

    exo: Reshow UI lock notifications on return from idle.
    Bug: b/203622329,b/227385103
    Change-Id: I3644f8491c7ec714b7601e2ef1d148a0db21a68b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3619979
    Commit-Queue: Chloe Pelling <cpelling@google.com>
    Auto-Submit: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998270}

--- components/exo/ui_lock_controller.cc
+++ components/exo/ui_lock_controller.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/user_activity/user_activity_detector.h"
+#include "ui/base/user_activity/user_activity_observer.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/dom/dom_code.h"
@@ -69,6 +71,8 @@ constexpr float kExitPopupHideHeight = 150.f;
 // Once the pointer capture notification has finished showing without
 // being interrupted, don't show it again until this long has passed.
 constexpr auto kPointerCaptureNotificationCooldown = base::Minutes(5);
+
+constexpr auto kReshowNotificationsWhenIdleFor = base::Minutes(5);
 
 constexpr int kUILockControllerSeatObserverPriority = 1;
 static_assert(
@@ -485,6 +489,7 @@ constexpr auto kExcludedFlags = ui::EF_SHIFT_DOWN | ui::EF_CONTROL_DOWN |
                                 ui::EF_ALTGR_DOWN | ui::EF_IS_REPEAT;
 
 UILockController::UILockController(Seat* seat) : seat_(seat) {
+  last_activity_time_ = base::TimeTicks::Now();
   WMHelper::GetInstance()->AddPreTargetHandler(this);
   seat_->AddObserver(this, kUILockControllerSeatObserverPriority);
 
@@ -493,9 +498,13 @@ UILockController::UILockController(Seat* seat) : seat_(seat) {
   auto* session_controller = ash::SessionController::Get();
   if (session_controller)
     session_controller->AddObserver(this);
+
+  ui::UserActivityDetector::Get()->AddObserver(this);
 }
 
 UILockController::~UILockController() {
+  ui::UserActivityDetector::Get()->RemoveObserver(this);
+
   auto* session_controller = ash::SessionController::Get();
   if (session_controller)
     session_controller->RemoveObserver(this);
@@ -590,6 +599,14 @@ void UILockController::OnPointerCaptureDisabled(Pointer* pointer,
     if (notifier)
       notifier->OnPointerCaptureDisabled();
   }
+}
+
+void UILockController::OnUserActivity(const ui::Event* event) {
+  base::TimeTicks now = base::TimeTicks::Now();
+  if (now - last_activity_time_ >= kReshowNotificationsWhenIdleFor) {
+    ReshowAllNotifications();
+  }
+  last_activity_time_ = now;
 }
 
 views::Widget* UILockController::GetPointerCaptureNotificationForTesting(
