46ae03398bbdc1d503fa2fc7d7a553498c000602
Kuan Huang <kuanhuang@chromium.org>
2022-05-02 17:43:39 +0000

    Disable MenuController{,UI}Test for Win11
    The following tests are failing consistently on Windows 11. Disable for
    mitigation and not block Windows 11 image rolling to production.
    
    MenuControllerUITest.TestMouseOverShownMenu
    MenuControllerTest.RootAndChildMenusInitializeAuraWindowWhenShown
    MenuControllerTest.SetSelectionIndices_NestedButtons
    MenuControllerTest.ContextMenuInitializesAuraWindowWhenShown
    MenuControllerTest.SetSelectionIndices_Buttons_SkipHiddenAndDisabled
    MenuControllerTest.SetSelectionIndices_MenuItemsOnly
    MenuControllerTest.SetSelectionIndices_MenuItemsOnly_SkipHiddenAndDisabled
    MenuControllerTest.SetSelectionIndices_Buttons
    MenuControllerTest.RepostEventToEmptyMenuItem
    
    Bug: 1286137
    Change-Id: Ie3f1ed3b6a3cb9cea83660afd9e70baf3d9f6d78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612343
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998433}

--- chrome/browser/ui/views/menu_interactive_uitest.cc
+++ chrome/browser/ui/views/menu_interactive_uitest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/win/windows_version.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -30,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ui/accessibility/platform/ax_platform_node.h"
+#endif
+
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_version.h"
 #endif
 
 namespace views {
@@ -111,6 +114,13 @@ class MenuControllerUITest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(MenuControllerUITest, TestMouseOverShownMenu) {
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   ui::testing::ScopedAxModeSetter ax_mode_setter(ui::kAXModeComplete);
+#endif
+#if BUILDFLAG(IS_WIN)
+  // TODO(crbug.com/1286137): This test is consistently failing on Win11.
+  if (base::win::OSInfo::GetInstance()->version() >=
+      base::win::Version::WIN11) {
+    GTEST_SKIP() << "Skipping test for WIN11_21H2 and greater";
+  }
 #endif
 
   // Create a parent widget.
