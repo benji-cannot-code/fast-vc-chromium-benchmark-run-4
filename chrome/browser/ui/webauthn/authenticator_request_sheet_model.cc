ff762312a11bad995b1eb9bbed4e173f5d2aaf61
Adam Langley <agl@chromium.org>
2022-05-02 22:31:23 +0000

    webauthn: add experiments for improving caBLE
    BUG=1319018
    
    Change-Id: Ia8962582ed4c91fe527d0f3843ab2b3a9c93f3a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615936
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#998607}

--- chrome/browser/ui/webauthn/authenticator_request_sheet_model.cc
+++ chrome/browser/ui/webauthn/authenticator_request_sheet_model.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webauthn/authenticator_request_sheet_model.h"
 
+bool AuthenticatorRequestSheetModel::IsCloseButtonVisible() const {
+  return false;
+}
+
 std::u16string AuthenticatorRequestSheetModel::GetAdditionalDescription()
     const {
   return std::u16string();
