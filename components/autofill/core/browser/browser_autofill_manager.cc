f444c99392696275bd18cd69cd070b33afffc622
Vinny Persky <vinnypersky@google.com>
2022-05-02 22:34:41 +0000

    Move Suggestions Context Struct and SuppressReason enum to a separate file
    For the work to fill merchant promo code fields, we have implemented a new  SingleFieldFormFillRouter. We need to implement a way for it to be able to route requests between the new MerchantPromoCodeSuggestionsManager that will be implemented.
    
    The clean and scalable way to do this would be to pass a SuggestionsContext struct into this OnGetSingleFieldSuggestions call: https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/browser_autofill_manager.cc;drc=6e774b7ccd5cfc073964934ef4d27150b9ed7b46;l=1048. This way we can use the SuggestionsContext to figure out where the call should route to, and in the future if we have more than autocomplete and promo code fields we can add to this suggestions context struct.
    
    Since this would now require SingleFieldFormFillRouter to know the contents of this struct, as well as the MerchantPromoCodeSuggestionsManager in the future, we should abstract it out to a separate class.
    
    Diff: https://diff.googleplex.com/#key=zHJrtCPNZ8pk
    
    Bug: 1190334
    Change-Id: I8c93206b59eb633241df6354be49faa88a785ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3609216
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#998608}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_client.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/randomized_encoder.h"
+#include "components/autofill/core/browser/suggestions_context.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/browser/validation.h"
 #include "components/autofill/core/common/autofill_clock.h"
