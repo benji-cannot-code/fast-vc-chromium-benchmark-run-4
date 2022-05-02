198f0f8d377703866a0699f4c7ecd45baaa6814f
Daniel Rubery <drubery@chromium.org>
2022-05-02 23:54:16 +0000

    Fix too large files skipping metadata checks and showing warnings
    Currently, a large file will try to deep scan, fail, and therefore
    get an UNKNOWN verdict, leading to a generic file type warning (for
    some file types). This CL makes the deep scan failures cause a
    standard consumer metadata check, so that they continue to get
    some level of scanning.
    
    Bug: 1309623
    Change-Id: Iee5dca5b83be8dd96b6b7d515ad107e08a17e576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615941
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998647}

--- chrome/browser/safe_browsing/download_protection/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service.cc
@@ -251,10 +251,13 @@ bool DownloadProtectionService::MaybeCheckClientDownload(
     // scanning request and not with a consumer check, the pre-deep scanning
     // DownloadCheckResult is considered UNKNOWN. This shouldn't trigger on
     // report-only scans to avoid skipping the consumer check.
-    UploadForDeepScanning(item, std::move(callback),
-                          DeepScanningRequest::DeepScanTrigger::TRIGGER_POLICY,
-                          DownloadCheckResult::UNKNOWN,
-                          std::move(settings.value()));
+    UploadForDeepScanning(
+        item,
+        base::BindRepeating(
+            &DownloadProtectionService::MaybeCheckMetdataAfterDeepScanning,
+            weak_ptr_factory_.GetWeakPtr(), item, std::move(callback)),
+        DeepScanningRequest::DeepScanTrigger::TRIGGER_POLICY,
+        DownloadCheckResult::UNKNOWN, std::move(settings.value()));
     return true;
   }
 
@@ -808,6 +811,17 @@ DownloadProtectionService::GetNavigationObserverManager(
     content::WebContents* web_contents) {
   return SafeBrowsingNavigationObserverManagerFactory::GetForBrowserContext(
       web_contents->GetBrowserContext());
+}
+
+void DownloadProtectionService::MaybeCheckMetdataAfterDeepScanning(
+    download::DownloadItem* item,
+    CheckDownloadRepeatingCallback callback,
+    DownloadCheckResult result) {
+  if (result == DownloadCheckResult::UNKNOWN) {
+    CheckClientDownload(item, callback);
+  } else {
+    std::move(callback).Run(result);
+  }
 }
 
 }  // namespace safe_browsing
