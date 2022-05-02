4ddb00477e269223d3312144b5e17ecf1567344c
Tsuyoshi Horo <horo@chromium.org>
2022-05-02 06:15:57 +0000

    Revive chrome://flags/#enable-async-dns on Windows
    To prepare for running a new experiment on Windows, this CL revives
    chrome://flags/#enable-async-dns on Windows. This flag had been
    introduced on Android by crrev.com/c/685036, and removed on M81 as
    written in flag-metadata.json.
    
    Bug: 1317948
    Change-Id: Ia09c16a4f5f33840aa272c23630cc3aae2df4d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3619975
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998260}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2968,9 +2968,6 @@ const char kAssistantIntentTranslateInfoDescription[] =
     "intent. This includes the page's URL and its original, current, and "
     "default target language.";
 
-const char kAsyncDnsName[] = "Async DNS resolver";
-const char kAsyncDnsDescription[] = "Enables the built-in DNS resolver.";
-
 const char kAutofillAccessoryViewName[] =
     "Autofill suggestions as keyboard accessory view";
 const char kAutofillAccessoryViewDescription[] =
@@ -3872,6 +3869,9 @@ const char kSharingDesktopSharePreviewDescription[] =
 // Windows ---------------------------------------------------------------------
 
 #if BUILDFLAG(IS_WIN)
+
+const char kAsyncDnsName[] = "Async DNS resolver";
+const char kAsyncDnsDescription[] = "Enables the built-in DNS resolver.";
 
 const char kCalculateNativeWinOcclusionName[] =
     "Calculate window occlusion on Windows";
