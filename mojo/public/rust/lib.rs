1b486d4063a5ba252b9332ca7bbf2c7086456575
Collin Baker <collinbaker@chromium.org>
2022-05-02 18:53:01 +0000

    Implement wrapper for Mojo trap
    This is the first step to remove references to outdated MojoWait and
    MojoWaitSet functions. Instead, Rust-native implementations will use
    Mojo's trap primitive (which didn't exist when the original Rust
    bindings were written).
    
    https://source.chromium.org/chromium/chromium/src/+/main:mojo/public/c/system/trap.h
    
    Bug: 1274864
    Change-Id: I436193cbc3743e795e5b13d16820028c7aab05b3
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg,android-rust-arm-rel,linux-rust-x64-dbg,linux-rust-x64-rel
    Change-Id: I436193cbc3743e795e5b13d16820028c7aab05b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584421
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998489}

--- mojo/public/rust/lib.rs
+++ mojo/public/rust/lib.rs
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![feature(maybe_uninit_slice)]
+#![feature(new_uninit)]
+
 #[macro_use]
 mod macros {
     /// This macro must be used at the top-level in any
