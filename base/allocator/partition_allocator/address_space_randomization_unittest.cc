149d6596aca7fc0cbca718a75b3e0a7b6509cfc1
Benoit Lize <lizeb@chromium.org>
2022-05-02 16:48:39 +0000

    [PartitionAlloc] Make ASLR tests faster for DCHECK() builds
    ASLR tests do extensive correlation testing on the random number
    generator we use. This PRNG is already tested (since we use the //base
    one), and almost never changes. However these tests are responsible for
    ~1/2 of the total running time of base_unittests (after the previous
    change in PA's tests, see linked bug).
    
    Make the test use the smaller settings when DCHECK_IS_ON(), which makes
    these tests go from ~9s to ~.9s locally.
    
    Bug: 1168168
    Change-Id: I12282f24d6c8a13f2dcad89e44d070224f7319bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613774
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#998411}

--- base/allocator/partition_allocator/address_space_randomization_unittest.cc
+++ base/allocator/partition_allocator/address_space_randomization_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/allocator/partition_allocator/random.h"
 #include "base/check_op.h"
+#include "base/dcheck_is_on.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -137,7 +138,12 @@ void RandomBitCorrelation(int random_bit) {
   if ((mask & (1ULL << random_bit)) == 0)
     return;  // bit is always 0.
 
-#ifdef DEBUG
+#if DCHECK_IS_ON()
+  // Do fewer checks when DCHECK_IS_ON(). Exercized code only changes when the
+  // random number generator does, which should be almost never. However it's
+  // expensive to run all the tests. So keep iterations faster for local
+  // development builds, while having the stricter version run on official build
+  // testers.
   constexpr int kHistory = 2;
   constexpr int kRepeats = 1000;
 #else
