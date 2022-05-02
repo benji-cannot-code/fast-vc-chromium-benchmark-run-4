fa39a8c67c5543cfb3b365c7d4b91b47d74bec29
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-05-02 18:08:09 +0000

    Import wpt@6d392b18560077ac3c014960b05b7047d39e0f0f
    Using wpt-import in Chromium a10f55632b135af77eb1534e7d591c7f48eff4d0.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I3491b291a60ad2818093a479099a0c1d8ebf4059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620616
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#998450}

--- third_party/blink/web_tests/external/wpt/fetch/metadata/tools/build.sh
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env sh
-set -ex
-
-cd "${0%/*}"
-virtualenv -p python3 .virtualenv
-.virtualenv/bin/pip install -r ./requirements.txt
-cd ..
-tools/.virtualenv/bin/python ./tools/generate.py ./tools/fetch-metadata.conf.yml
