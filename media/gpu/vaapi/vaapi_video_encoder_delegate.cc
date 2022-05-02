d9e7e68023ffde33ca78044a2b40da01331c0647
Hirokazu Honda <hiroh@chromium.org>
2022-05-02 05:19:42 +0000

    media/gpu/vaapiVEA: Copy video frame to surface before VaapiVED::Encode()
    VaapiWrapper::UploadVideoFrameSurface() writes the VideoFrame content
    to VASurface. It is executed VaapiVideoEncoderDelegate::Encode() if
    VideoFrame is shared memory based one. If VideoFrame is
    GpuMemoryBuffer based one, the video frame is imported to VASurface
    before calling VaapiVED::Encode().
    This CL moves the place of performing UploadVideoFrameSurface() to
    VaapiVEA. This can have the consistency the input surface content is
    ready before VaapiVED::Encode(). It also enables removing a few
    variables in VaapiVED class.
    
    Bug: b:229358029
    Test: video_encode_accelerator_tests
    Change-Id: Ic69163d19b86795f29946558ccd5e27207e5718d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3590336
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998250}

--- media/gpu/vaapi/vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/vaapi_video_encoder_delegate.cc
@@ -20,28 +20,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 VaapiVideoEncoderDelegate::EncodeJob::EncodeJob(
-    scoped_refptr<VideoFrame> input_frame,
     bool keyframe,
+    base::TimeDelta timestamp,
     VASurfaceID input_surface_id,
-    const gfx::Size& input_surface_size,
     scoped_refptr<CodecPicture> picture,
     std::unique_ptr<ScopedVABuffer> coded_buffer)
-    : input_frame_(input_frame),
-      keyframe_(keyframe),
+    : keyframe_(keyframe),
+      timestamp_(timestamp),
       input_surface_id_(input_surface_id),
-      input_surface_size_(input_surface_size),
       picture_(std::move(picture)),
       coded_buffer_(std::move(coded_buffer)) {
   DCHECK(picture_);
   DCHECK(coded_buffer_);
 }
 
-VaapiVideoEncoderDelegate::EncodeJob::EncodeJob(
-    scoped_refptr<VideoFrame> input_frame,
-    bool keyframe)
-    : input_frame_(input_frame),
-      keyframe_(keyframe),
-      input_surface_id_(VA_INVALID_ID) {}
+VaapiVideoEncoderDelegate::EncodeJob::EncodeJob(bool keyframe,
+                                                base::TimeDelta timestamp,
+                                                VASurfaceID input_surface_id)
+    : keyframe_(keyframe),
+      timestamp_(timestamp),
+      input_surface_id_(input_surface_id) {}
 
 VaapiVideoEncoderDelegate::EncodeJob::~EncodeJob() = default;
 
@@ -52,12 +50,7 @@ VaapiVideoEncoderDelegate::EncodeJob::CreateEncodeResult(
 }
 
 base::TimeDelta VaapiVideoEncoderDelegate::EncodeJob::timestamp() const {
-  return input_frame_->timestamp();
-}
-
-const scoped_refptr<VideoFrame>&
-VaapiVideoEncoderDelegate::EncodeJob::input_frame() const {
-  return input_frame_;
+  return timestamp_;
 }
 
 VABufferID VaapiVideoEncoderDelegate::EncodeJob::coded_buffer_id() const {
@@ -66,11 +59,6 @@ VABufferID VaapiVideoEncoderDelegate::EncodeJob::coded_buffer_id() const {
 
 VASurfaceID VaapiVideoEncoderDelegate::EncodeJob::input_surface_id() const {
   return input_surface_id_;
-}
-
-const gfx::Size& VaapiVideoEncoderDelegate::EncodeJob::input_surface_size()
-    const {
-  return input_surface_size_;
 }
 
 const scoped_refptr<CodecPicture>&
@@ -129,15 +117,8 @@ bool VaapiVideoEncoderDelegate::Encode(EncodeJob& encode_job) {
     return false;
   }
 
-  const VASurfaceID va_surface_id = encode_job.input_surface_id();
-  if (!native_input_mode_ && !vaapi_wrapper_->UploadVideoFrameToSurface(
-                                 *encode_job.input_frame(), va_surface_id,
-                                 encode_job.input_surface_size())) {
-    VLOGF(1) << "Failed to upload frame";
-    return false;
-  }
-
-  if (!vaapi_wrapper_->ExecuteAndDestroyPendingBuffers(va_surface_id)) {
+  if (!vaapi_wrapper_->ExecuteAndDestroyPendingBuffers(
+          encode_job.input_surface_id())) {
     VLOGF(1) << "Failed to execute encode";
     return false;
   }
