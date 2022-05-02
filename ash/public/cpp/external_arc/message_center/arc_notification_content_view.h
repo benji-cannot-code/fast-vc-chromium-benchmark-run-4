bd941a7eab727b6481d3c93f6e6c6b6a2381acfa
Tom Lukaszewicz <tluk@chromium.org>
2022-05-02 22:28:59 +0000

    [views] Eliminate OnWidgetClosing from ArcNotificationContentView
    Favor OnWidgetDestroying over OnWidgetClosing. OnWidgetClosing is
    only called where a Widget is closed directly, it fails to catch
    the case when the system destroys the NativeWidget directly. This
    can result in UAFs in situations where the OS directly destroys
    the widget.
    
    OnWidgetDestroying will be called every time a widget is destroyed
    and should be used instead in most cases.
    
    Bug: 1240365
    Change-Id: I4b2e002aa5b023eb9de153b3635efd7fcdb9d63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3621557
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998604}

--- ash/public/cpp/external_arc/message_center/arc_notification_content_view.h
+++ ash/public/cpp/external_arc/message_center/arc_notification_content_view.h
@@ -124,7 +124,7 @@ class ArcNotificationContentView
   void OnWindowDestroying(aura::Window* window) override;
 
   // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
 
   // ArcNotificationItem::Observer
