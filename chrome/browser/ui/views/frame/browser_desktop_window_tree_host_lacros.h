20e8659c43eff1dfb15698d72f6728ed249ce056
Mitsuru Oshima <oshima@chromium.org>
2022-05-02 17:13:29 +0000

    Port linux impls that were missing in crrev.com/c/3599900
    * It needs to set the host to browser frame in order to
      handle TabDraggingKind change
    * added mouse lock support
    * OnWindowStateChanged to handle fullscreen state change.
    
    Bug: 1319805
    Change-Id: I2eecd2e1e2224d666669cc29c9381f9de529ecde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615254
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998415}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_lacros.h
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_lacros.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host_linux.h"
 
 class BrowserView;
+class BrowserFrame;
 enum class TabDragKind;
 
 namespace views {
@@ -23,7 +24,8 @@ class BrowserDesktopWindowTreeHostLacros
   BrowserDesktopWindowTreeHostLacros(
       views::internal::NativeWidgetDelegate* native_widget_delegate,
       views::DesktopNativeWidgetAura* desktop_native_widget_aura,
-      BrowserView* browser_view);
+      BrowserView* browser_view,
+      BrowserFrame* browser_frame);
   BrowserDesktopWindowTreeHostLacros(
       const BrowserDesktopWindowTreeHostLacros&) = delete;
   BrowserDesktopWindowTreeHostLacros& operator=(
@@ -42,6 +44,13 @@ class BrowserDesktopWindowTreeHostLacros
   // views::DesktopWindowTreeHostPlatform:
   SkPath GetWindowMaskForClipping() const override;
   void OnSurfaceFrameLockingChanged(bool lock) override;
+  bool SupportsMouseLock() override;
+  void LockMouse(aura::Window* window) override;
+  void UnlockMouse(aura::Window* window) override;
+
+  // ui::PlatformWindowDelegate
+  void OnWindowStateChanged(ui::PlatformWindowState old_state,
+                            ui::PlatformWindowState new_state) override;
 
   BrowserView* const browser_view_;
   views::DesktopNativeWidgetAura* desktop_native_widget_aura_ = nullptr;
