0e4a94208f23d76d7ec260d443f3692a038cb1d2
Domenic Denicola <domenic@chromium.org>
2022-05-02 15:24:40 +0000

    Remove origin policy WPTs
    Note that this does not remove the server infrastructure that creates a
    bunch of subdomains for testing origin policy yet. That might need to go
    through the external web platform tests RFC process.
    
    Bug: 1320793
    Change-Id: Iab0acfa03ea044ae08920bd5226808aaa22b8878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614123
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998375}

--- third_party/blink/web_tests/external/wpt/origin-policy/features/resources/helper.mjs
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-"use strict";
-
-export function runFPTest({ camera, geolocation }) {
-  test(() => {
-    assert_equals(document.featurePolicy.allowsFeature('camera'), camera, 'camera');
-    assert_equals(document.featurePolicy.allowsFeature('geolocation'), geolocation, 'geolocation');
-  });
-}
