a65142d1a19e18893b9e8f66f0b2c88736598b98
Martin Kreichgauer <martinkr@google.com>
2022-05-02 18:33:11 +0000

    webauthn: check appids before invoking request proxy
    Checking the App ID against the caller origin is a security decision and
    should therefore occur before deciding whether the request needs to be
    handed off to a proxy extension, just as with RP IDs.
    
    Bug: 1231802
    Change-Id: I3cf9a392b5cf01b6794d73a742207f6106c456ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615538
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#998475}

--- content/browser/webauth/authenticator_common.cc
+++ content/browser/webauth/authenticator_common.cc
@@ -694,6 +694,18 @@ void AuthenticatorCommon::MakeCredential(
   caller_origin_ = caller_origin;
   relying_party_id_ = options->relying_party.id;
 
+  absl::optional<std::string> appid_exclude;
+  if (options->appid_exclude) {
+    appid_exclude = ProcessAppIdExtension(
+        GetRenderFrameHost()->GetBrowserContext(), *options->appid_exclude,
+        caller_origin, options->remote_desktop_client_override);
+    if (!appid_exclude) {
+      CompleteMakeCredentialRequest(
+          blink::mojom::AuthenticatorStatus::INVALID_DOMAIN);
+      return;
+    }
+  }
+
   // If there is an active webAuthenticationProxy extension, let it handle the
   // request.
   WebAuthenticationRequestProxy* proxy = GetWebAuthnRequestProxyIfActive();
@@ -740,18 +752,6 @@ void AuthenticatorCommon::MakeCredential(
       CompleteMakeCredentialRequest(
           blink::mojom::AuthenticatorStatus::NOT_ALLOWED_ERROR);
       return;
-  }
-
-  absl::optional<std::string> appid_exclude;
-  if (options->appid_exclude) {
-    appid_exclude = ProcessAppIdExtension(
-        GetRenderFrameHost()->GetBrowserContext(), *options->appid_exclude,
-        caller_origin, options->remote_desktop_client_override);
-    if (!appid_exclude) {
-      CompleteMakeCredentialRequest(
-          blink::mojom::AuthenticatorStatus::INVALID_DOMAIN);
-      return;
-    }
   }
 
   if (options->user.icon_url) {
@@ -1018,6 +1018,19 @@ void AuthenticatorCommon::GetAssertion(
 
   caller_origin_ = caller_origin;
   relying_party_id_ = options->relying_party_id;
+
+  if (options->appid) {
+    requested_extensions_.insert(RequestExtension::kAppID);
+    app_id_ = ProcessAppIdExtension(GetRenderFrameHost()->GetBrowserContext(),
+                                    *options->appid, caller_origin_,
+                                    options->remote_desktop_client_override);
+    if (!app_id_) {
+      CompleteGetAssertionRequest(
+          blink::mojom::AuthenticatorStatus::INVALID_DOMAIN);
+      return;
+    }
+  }
+
   WebAuthenticationRequestProxy* proxy = GetWebAuthnRequestProxyIfActive();
   if (proxy) {
     if (options->remote_desktop_client_override) {
@@ -1104,18 +1117,6 @@ void AuthenticatorCommon::GetAssertion(
       return;
     }
     empty_allow_list_ = true;
-  }
-
-  if (options->appid) {
-    requested_extensions_.insert(RequestExtension::kAppID);
-    app_id_ = ProcessAppIdExtension(GetRenderFrameHost()->GetBrowserContext(),
-                                    *options->appid, caller_origin_,
-                                    options->remote_desktop_client_override);
-    if (!app_id_) {
-      CompleteGetAssertionRequest(
-          blink::mojom::AuthenticatorStatus::INVALID_DOMAIN);
-      return;
-    }
   }
 
   if (options->large_blob_read && options->large_blob_write) {
