51f3f8804e68ebd5faa12be331e5aa93bcbc3d4f
Fabian Sommer <fabiansommer@chromium.org>
2022-05-02 13:29:58 +0000

    Reserve more space in profile picker
    Reserve space for one line of profiles in the profile picker.
    Profiles are loaded asynchronously. Reserving space prevents the title
    ("Choose a profile") to move around when the profiles are loaded, as
    long as the profiles fit within a single row.
    
    Before this change, the title would be vertically centered after the
    page renders, and then move up once profiles were loaded.
    
    Fixed: 1288183
    Change-Id: I0b4347905b3a204fdd1c20c83ac7f382dbae6f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610112
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998333}

--- chrome/test/data/webui/signin/profile_picker_main_view_test.ts
+++ chrome/test/data/webui/signin/profile_picker_main_view_test.ts
@@ -137,13 +137,13 @@ suite('ProfilePickerMainViewTest', function() {
     assertEquals(navigationElement.route, Routes.MAIN);
     await browserProxy.whenCalled('initializeMainView');
     // Hidden while profiles list is not yet defined.
-    assertTrue(mainViewElement.$.wrapper.hidden);
+    assertTrue(mainViewElement.$.profilesContainer.hidden);
     assertTrue(mainViewElement.$.askOnStartup.hidden);
     const profiles = generateProfilesList(6);
     webUIListenerCallback('profiles-list-changed', [...profiles]);
     flushTasks();
     // Profiles list defined.
-    assertTrue(!mainViewElement.$.wrapper.hidden);
+    assertTrue(!mainViewElement.$.profilesContainer.hidden);
     assertTrue(!mainViewElement.$.askOnStartup.hidden);
     assertTrue(mainViewElement.$.askOnStartup.checked);
     // Verify profile card.
@@ -227,7 +227,7 @@ suite('ProfilePickerMainViewTest', function() {
   test('AskOnStartupSingleToMultipleProfiles', async function() {
     await browserProxy.whenCalled('initializeMainView');
     // Hidden while profiles list is not yet defined.
-    assertTrue(mainViewElement.$.wrapper.hidden);
+    assertTrue(mainViewElement.$.profilesContainer.hidden);
     assertTrue(mainViewElement.$.askOnStartup.hidden);
     let profiles = generateProfilesList(1);
     webUIListenerCallback('profiles-list-changed', [...profiles]);
@@ -253,7 +253,7 @@ suite('ProfilePickerMainViewTest', function() {
   test('AskOnStartupMultipleToSingleProfile', async function() {
     await browserProxy.whenCalled('initializeMainView');
     // Hidden while profiles list is not yet defined.
-    assertTrue(mainViewElement.$.wrapper.hidden);
+    assertTrue(mainViewElement.$.profilesContainer.hidden);
     assertTrue(mainViewElement.$.askOnStartup.hidden);
     const profiles = generateProfilesList(2);
     webUIListenerCallback('profiles-list-changed', [...profiles]);
@@ -277,7 +277,7 @@ suite('ProfilePickerMainViewTest', function() {
 
     await browserProxy.whenCalled('initializeMainView');
     // Hidden while profiles list is not yet defined.
-    assertTrue(mainViewElement.$.wrapper.hidden);
+    assertTrue(mainViewElement.$.profilesContainer.hidden);
     assertTrue(mainViewElement.$.askOnStartup.hidden);
     const profiles = generateProfilesList(2);
     webUIListenerCallback('profiles-list-changed', [...profiles]);
