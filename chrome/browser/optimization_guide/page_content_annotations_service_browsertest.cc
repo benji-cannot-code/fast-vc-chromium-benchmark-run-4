78088c4ac022ffe0daca54161862805aabc5f4d4
Sophie Chang <sophiechang@chromium.org>
2022-05-03 17:55:59 +0000

    Add histogram variants for ContentAnnotationsStorageStatus
    Bug: 1321222
    Change-Id: I87e8fa1fc325beeabdb2284b2489ecace7aeeb82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622495
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998963}

--- chrome/browser/optimization_guide/page_content_annotations_service_browsertest.cc
+++ chrome/browser/optimization_guide/page_content_annotations_service_browsertest.cc
@@ -481,6 +481,10 @@ IN_PROC_BROWSER_TEST_F(PageContentAnnotationsServiceBrowserTest,
       "OptimizationGuide.PageContentAnnotationsService."
       "ContentAnnotationsStorageStatus",
       PageContentAnnotationsStorageStatus::kSuccess, 1);
+  histogram_tester.ExpectUniqueSample(
+      "OptimizationGuide.PageContentAnnotationsService."
+      "ContentAnnotationsStorageStatus.ModelAnnotations",
+      PageContentAnnotationsStorageStatus::kSuccess, 1);
 
   absl::optional<history::VisitContentAnnotations> got_content_annotations =
       GetContentAnnotationsForURL(url);
@@ -551,6 +555,10 @@ IN_PROC_BROWSER_TEST_F(PageContentAnnotationsServiceBrowserTest,
         "OptimizationGuide.PageContentAnnotationsService."
         "ContentAnnotationsStorageStatus",
         PageContentAnnotationsStorageStatus::kNoVisitsForUrl, 1);
+    histogram_tester.ExpectUniqueSample(
+        "OptimizationGuide.PageContentAnnotationsService."
+        "ContentAnnotationsStorageStatus.ModelAnnotations",
+        PageContentAnnotationsStorageStatus::kNoVisitsForUrl, 1);
 
     EXPECT_FALSE(GetContentAnnotationsForURL(history_visit.url).has_value());
   }
@@ -615,6 +623,10 @@ IN_PROC_BROWSER_TEST_F(
   histogram_tester.ExpectUniqueSample(
       "OptimizationGuide.PageContentAnnotationsService."
       "ContentAnnotationsStorageStatus",
+      PageContentAnnotationsStorageStatus::kSuccess, 1);
+  histogram_tester.ExpectUniqueSample(
+      "OptimizationGuide.PageContentAnnotationsService."
+      "ContentAnnotationsStorageStatus.ModelAnnotations",
       PageContentAnnotationsStorageStatus::kSuccess, 1);
 
   absl::optional<history::VisitContentAnnotations> got_content_annotations =
