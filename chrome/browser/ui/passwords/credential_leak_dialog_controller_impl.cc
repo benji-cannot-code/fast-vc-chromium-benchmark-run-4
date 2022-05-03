3dafeffbb930afb99adbbeb222c902b7d3dd6a08
Elias <eliaskh@chromium.org>
2022-05-03 12:12:24 +0000

    [Passwords] Introduce separate dialog trait implementations based on LeakDialogType.
    CredentialLeakType determines the LeakDialogType and thus determiens the actual dialog content. This type dictates the logic for each of the four dialogs (Checkup, Change, CheckupAndChange, ChangeAutomatically). The aim of this cl is to refactor the logic and use separate imp. for dialog traits that determine the strings/button of each of the dialogs.
    
    The previous approach of using a factory method in the
    credential_leak_dialog_controller is discarded since it won't scale for
    all platforms.
    
    Future cls will introduce this change to the other platforms and cleanup
    the redundant functions.
    
    Bug: 1317972
    Change-Id: Ied95b7a1dd8e7c715e3bd940cdadc50505b6e5eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620398
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998816}

--- chrome/browser/ui/passwords/credential_leak_dialog_controller_impl.cc
+++ chrome/browser/ui/passwords/credential_leak_dialog_controller_impl.cc
@@ -10,15 +10,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 
+using password_manager::CreateDialogTraits;
 using password_manager::CredentialLeakFlags;
 using password_manager::CredentialLeakType;
+using password_manager::LeakDialogTraitsImp;
 using password_manager::metrics_util::LeakDialogDismissalReason;
+using password_manager::metrics_util::LeakDialogType;
 using password_manager::metrics_util::LogLeakDialogTypeAndDismissalReason;
 
 CredentialLeakDialogControllerImpl::CredentialLeakDialogControllerImpl(
     PasswordsLeakDialogDelegate* delegate,
     CredentialLeakType leak_type)
-    : delegate_(delegate), leak_type_(leak_type) {}
+    : delegate_(delegate),
+      leak_type_(leak_type),
+      leak_dialog_traits_(CreateDialogTraits(leak_type)) {}
 
 CredentialLeakDialogControllerImpl::~CredentialLeakDialogControllerImpl() {
   ResetDialog();
@@ -78,24 +83,24 @@ void CredentialLeakDialogControllerImpl::ResetDialog() {
 
 std::u16string CredentialLeakDialogControllerImpl::GetAcceptButtonLabel()
     const {
-  return password_manager::GetAcceptButtonLabel(leak_type_);
+  return leak_dialog_traits_->GetAcceptButtonLabel();
 }
 
 std::u16string CredentialLeakDialogControllerImpl::GetCancelButtonLabel()
     const {
-  return password_manager::GetCancelButtonLabel(leak_type_);
+  return leak_dialog_traits_->GetCancelButtonLabel();
 }
 
 std::u16string CredentialLeakDialogControllerImpl::GetDescription() const {
-  return password_manager::GetDescription(leak_type_);
+  return leak_dialog_traits_->GetDescription();
 }
 
 std::u16string CredentialLeakDialogControllerImpl::GetTitle() const {
-  return password_manager::GetTitle(leak_type_);
+  return leak_dialog_traits_->GetTitle();
 }
 
 bool CredentialLeakDialogControllerImpl::ShouldCheckPasswords() const {
-  return password_manager::ShouldCheckPasswords(leak_type_);
+  return leak_dialog_traits_->ShouldCheckPasswords();
 }
 
 bool CredentialLeakDialogControllerImpl::ShouldOfferAutomatedPasswordChange()
@@ -104,5 +109,5 @@ bool CredentialLeakDialogControllerImpl::ShouldOfferAutomatedPasswordChange()
 }
 
 bool CredentialLeakDialogControllerImpl::ShouldShowCancelButton() const {
-  return password_manager::ShouldShowCancelButton(leak_type_);
+  return leak_dialog_traits_->ShouldShowCancelButton();
 }
