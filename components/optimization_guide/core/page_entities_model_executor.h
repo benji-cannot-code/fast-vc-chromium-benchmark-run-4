6956e1b37ea0880e215b82659cc030ab91535351
Robert Ogden <robertogden@chromium.org>
2022-05-03 18:40:43 +0000

    Tidy up the HumanReadablePageEntitiesModelExecutor
    This isn't used anywhere.
    
    Bug: 1278828
    Change-Id: I87bbaad1643da04bdefe3556d2c35fab4c4dede9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3624447
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998990}

--- components/optimization_guide/core/page_entities_model_executor.h
+++ components/optimization_guide/core/page_entities_model_executor.h
@@ -16,21 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace optimization_guide {
 
-// TODO(crbug/1278828): Remove this entirely.
-class HumanReadablePageEntitiesModelExecutor {
- public:
-  virtual ~HumanReadablePageEntitiesModelExecutor() = default;
-
-  using PageEntitiesModelExecutedCallback = base::OnceCallback<void(
-      const absl::optional<std::vector<tflite::task::core::Category>>&)>;
-
-  // Annotates |text| with page entities likely represented on the page. Invokes
-  // |callback| when done.
-  virtual void ExecuteModelWithInput(
-      const std::string& text,
-      PageEntitiesModelExecutedCallback callback) = 0;
-};
-
 // The PageEntitiesModelExecutor is responsible for executing the PAGE_ENTITIES
 // model.
 class PageEntitiesModelExecutor {
