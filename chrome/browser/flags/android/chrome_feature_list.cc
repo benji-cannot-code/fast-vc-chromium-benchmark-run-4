4b6d06cb77f931c400d989bb515b7c6ee444f612
Clark DuVall <cduvall@chromium.org>
2022-05-03 03:04:29 +0000

    Experiment with splitting up expensive Clank startup task
    This task looks like it could delay navigation, so I want to experiment
    with splitting it up. An experiment will be run to see if this helps at
    all and if it causes any problems.
    
    Bug: 1321970
    Change-Id: Id396657be1e536cd0f4540b1a0e8a4a9b6d30887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3621568
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998712}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -271,6 +271,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kSpecialLocaleWrapper,
     &kSpecialUserDecision,
     &kSuppressToolbarCaptures,
+    &kSplitCompositorTask,
     &kStoreHoursAndroid,
     &kSwapPixelFormatToFixConvertFromTranslucent,
     &kTabEngagementReportingAndroid,
@@ -737,6 +738,9 @@ const base::Feature kSpecialLocaleWrapper{"SpecialLocaleWrapper",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSpecialUserDecision{"SpecialUserDecision",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kSplitCompositorTask{"SplitCompositorTask",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kStoreHoursAndroid{"StoreHoursAndroid",
