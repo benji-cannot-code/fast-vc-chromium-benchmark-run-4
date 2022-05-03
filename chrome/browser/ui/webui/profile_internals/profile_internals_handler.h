137d3c40acb4751375150143cc6272c0a6ae9857
Gabriel Oliveira <gabolvr@google.com>
2022-05-03 16:42:33 +0000

    Add profiles list and attributes to chrome://profile-internals/
    Bug: 1313447
    Change-Id: I9cd5e24a837d439e8a51d54333d07aa67d2cf17d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613734
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998911}

--- chrome/browser/ui/webui/profile_internals/profile_internals_handler.h
+++ chrome/browser/ui/webui/profile_internals/profile_internals_handler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_PROFILE_INTERNALS_PROFILE_INTERNALS_HANDLER_H_
 
 #include "base/values.h"
+#include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
 // Handles actions on Profile Internals debug page.
@@ -23,7 +24,12 @@ class ProfileInternalsHandler : public content::WebUIMessageHandler {
   void RegisterMessages() override;
 
  private:
-  void HandleProfilesChanged(const base::Value::List& args);
+  void HandleGetProfilesList(const base::Value::List& args);
+
+  void PushProfilesList();
+
+  // Returns the list of profiles ordered by the local profile name.
+  base::Value GetProfilesList();
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_PROFILE_INTERNALS_PROFILE_INTERNALS_HANDLER_H_
