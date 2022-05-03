91f0c5efc3ee8b291b7e806eb62710b2157bfda0
Jeevan Shikaram <jshikaram@chromium.org>
2022-05-03 04:13:23 +0000

    [Provisioning] Make sure GameResult only accepts game Results.
    This CL ensures that the apps::Result that's passed into a GameResult
    has a GameExtras.
    
    Bug: 1305880
    Change-Id: I2665914c9cd0bce9c265f18e0e5c4aafe0076c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3618623
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998723}

--- chrome/browser/ui/app_list/search/games/game_result.h
+++ chrome/browser/ui/app_list/search/games/game_result.h
@@ -43,7 +43,7 @@ class GameResult : public ChromeSearchResult {
   Profile* profile_;
   AppListControllerDelegate* list_controller_;
 
-  const GURL launch_url_;
+  GURL launch_url_;
 
   base::WeakPtrFactory<GameResult> weak_ptr_factory_{this};
 };
