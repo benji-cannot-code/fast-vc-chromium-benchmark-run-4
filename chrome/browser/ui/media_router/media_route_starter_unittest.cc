ad3f76079bf31714ef256ce357f688bc1278ed3a
Dominic Battre <battre@chromium.org>
2022-05-03 17:43:18 +0000

    Fix use after free in tests
    We have a number of flaky MediaRouterViewsUITest tests and I suspect
    the problem to be here:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/media_router/media_route_starter_unittest.cc;l=137;drc=6fa3b54df432518a2966cfab92bed34ba95c6363
    Where we set a global object in a SetUp() call but don't reset it in the
    TearDown().
    
    Bug: 1321600
    Change-Id: I590cec1daee09efdfdc7607610f02170c312401b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620377
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998953}

--- chrome/browser/ui/media_router/media_route_starter_unittest.cc
+++ chrome/browser/ui/media_router/media_route_starter_unittest.cc
@@ -153,6 +153,7 @@ class MediaRouteStarterTest : public ChromeRenderViewHostTestHarness {
     DestroyMediaRouteStarter();
     profile_manager_->DeleteAllTestingProfiles();
     profile_manager_.reset();
+    WebContentsPresentationManager::SetTestInstance(nullptr);
     ChromeRenderViewHostTestHarness::TearDown();
   }
 
