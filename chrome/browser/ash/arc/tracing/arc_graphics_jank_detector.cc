9f37a75d160decf4839e7a714d02d001d1880024
Alan Zhao <ayzhao@google.com>
2022-05-03 22:20:37 +0000

    Explicitly #include <algorithm> in arc_graphics_jank_detector.cc
    This file calls std::sort(...), which won't be available in the existing
    headers once libc++ is upgraded.
    
    Bug: 1273285
    Change-Id: I326f33145adc9631177ef95ccb1919de6cd138b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625250
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999101}

--- chrome/browser/ash/arc/tracing/arc_graphics_jank_detector.cc
+++ chrome/browser/ash/arc/tracing/arc_graphics_jank_detector.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/tracing/arc_graphics_jank_detector.h"
 
+#include <algorithm>
+
 namespace arc {
 
 // static
