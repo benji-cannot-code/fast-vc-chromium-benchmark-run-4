c64349f41a086b454e47f583f18d11eead3f4815
Peter Kotwicz <pkotwicz@chromium.org>
2022-05-03 02:34:47 +0000

    [FedCM] Crop user avatars in desktop FedCM prompt
    BUG=1320410
    
    Change-Id: Iabbcd35a6135e614164390c5c2a5c7b301973bb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615018
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998704}

--- chrome/browser/favicon/favicon_utils.h
+++ chrome/browser/favicon/favicon_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_FAVICON_FAVICON_UTILS_H_
 
 #include "components/favicon/content/content_favicon_driver.h"
+#include "third_party/skia/include/core/SkColor.h"
 
 namespace content {
 class WebContents;
---
+++ chrome/browser/ui/monogram_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_MONOGRAM_UTILS_H_
+#define CHROME_BROWSER_UI_MONOGRAM_UTILS_H_
+
+#include <string>
+
+#include "third_party/skia/include/core/SkColor.h"
+
+namespace gfx {
+class Canvas;
+}
+
+namespace monogram {
+
+// Draws a monogram in a colored circle on the passed-in `canvas`.
+// |monogram_text| is a std::u16string in order to support 2 letter
+// monograms.
+void DrawMonogramInCanvas(gfx::Canvas* canvas,
+                          int canvas_size,
+                          int circle_size,
+                          const std::u16string& monogram_text,
+                          SkColor monoggram_color,
+                          SkColor background_color);
+
+}  // namespace monogram
+
+#endif  // CHROME_BROWSER_UI_MONOGRAM_UTILS_H_
