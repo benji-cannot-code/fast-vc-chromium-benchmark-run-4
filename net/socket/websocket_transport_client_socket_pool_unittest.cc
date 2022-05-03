9b9abf36815160207c5529c48d8dd0a72ba9f1f2
David Benjamin <davidben@chromium.org>
2022-05-03 15:03:01 +0000

    Dedup IPEndPoints across routes TransportConnectJob
    If an IPEndPoint appears in two routes, there is no point in trying it
    twice. In particular, we expect the most common deployment will report
    the same IPs in both A/AAAA and HTTPS routes. (It's also the only
    pattern we, for now, support.) Trying the fallback route in this case is
    unnecessary.
    
    When we're preprocessing the route list, filter out duplicate
    IPEndPoints to avoid this.
    
    (We still sometimes try some IPEndPoints twice due to our slightly
    quirky Happy Eyeballs v1 implementation. This CL doesn't address that
    one, though crbug.com/1291352 will.)
    
    Bug: 1287240
    Change-Id: I6386bf4b91348fc6c74213087652bbfb7c036879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614897
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998866}

--- net/socket/websocket_transport_client_socket_pool_unittest.cc
+++ net/socket/websocket_transport_client_socket_pool_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cxx17_backports.h"
 #include "base/location.h"
 #include "base/run_loop.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/scoped_feature_list.h"
@@ -64,7 +63,7 @@ const int kMaxSockets = 32;
 const int kMaxSocketsPerGroup = 6;
 const RequestPriority kDefaultPriority = LOW;
 
-IPAddress ParseIP(base::StringPiece ip) {
+IPAddress ParseIP(const std::string& ip) {
   IPAddress address;
   CHECK(address.AssignFromIPLiteral(ip));
   return address;
