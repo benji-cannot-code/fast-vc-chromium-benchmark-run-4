91700e1bb3232fc79dcf24954305b08d219123b7
Marc Treib <treib@chromium.org>
2022-05-03 10:31:31 +0000

    Sync: Update proto_visitors.h
    This adds a bunch of proto fields to proto_visitors.h which were
    previously missing. It also removes a few fields which have been
    deprecated for a long time.
    This was the outcome of a spot check only; in particular I didn't check
    all of the Specifics protos.
    
    Bug: 1141794
    Change-Id: I42442862f97666368af072f003e471d3674a2464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620401
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#998799}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -299,6 +299,7 @@ VISIT_PROTO_FIELDS(const sync_pb::ClientConfigParams& proto) {
   VISIT(tabs_datatype_enabled);
   VISIT(cookie_jar_mismatch);
   VISIT(single_client);
+  VISIT_REP(devices_fcm_registration_tokens);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::ClientStatus& proto) {
@@ -315,6 +316,7 @@ VISIT_PROTO_FIELDS(const sync_pb::ClientToServerMessage& proto) {
   VISIT(sync_problem_detected);
   VISIT(debug_info);
   VISIT(client_status);
+  VISIT(invalidator_client_id);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::ClientToServerResponse& proto) {
@@ -332,6 +334,7 @@ VISIT_PROTO_FIELDS(const sync_pb::ClientToServerResponse::Error& proto) {
   VISIT_ENUM(error_type);
   VISIT(error_description);
   VISIT_ENUM(action);
+  VISIT_REP(error_data_type_ids);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::CommitMessage& proto) {
@@ -339,6 +342,7 @@ VISIT_PROTO_FIELDS(const sync_pb::CommitMessage& proto) {
   VISIT(cache_guid);
   VISIT_REP(extensions_activity);
   VISIT(config_params);
+  VISIT_REP(client_contexts);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::CommitResponse& proto) {
@@ -364,6 +368,12 @@ VISIT_PROTO_FIELDS(const sync_pb::DataTypeProgressMarker& proto) {
   VISIT_BYTES(token);
   VISIT(notification_hint);
   VISIT(get_update_triggers);
+  VISIT(gc_directive);
+}
+
+VISIT_PROTO_FIELDS(const sync_pb::GarbageCollectionDirective& proto) {
+  VISIT(version_watermark);
+  VISIT(age_watermark_in_days);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::DatatypeAssociationStats& proto) {
@@ -444,7 +454,6 @@ VISIT_PROTO_FIELDS(const sync_pb::DictionarySpecifics& proto) {
 
 VISIT_PROTO_FIELDS(const sync_pb::EncryptedData& proto) {
   VISIT(key_name);
-  // TODO(akalin): Shouldn't blob be of type bytes instead of string?
   VISIT_BYTES(blob);
 }
 
@@ -548,6 +557,7 @@ VISIT_PROTO_FIELDS(const sync_pb::GetUpdatesMessage& proto) {
   VISIT(need_encryption_key);
   VISIT(create_mobile_bookmarks_folder);
   VISIT_ENUM(get_updates_origin);
+  VISIT(is_retry);
   VISIT_REP(client_contexts);
 }
 
@@ -599,6 +609,8 @@ VISIT_PROTO_FIELDS(const sync_pb::ModelTypeState& proto) {
   VISIT(type_context);
   VISIT(encryption_key_name);
   VISIT(initial_sync_done);
+  VISIT(cache_guid);
+  VISIT(authenticated_account_id);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::NavigationRedirect& proto) {
@@ -923,9 +935,7 @@ VISIT_PROTO_FIELDS(const sync_pb::SyncEntity& proto) {
   VISIT(name);
   VISIT(non_unique_name);
   VISIT(server_defined_unique_tag);
-  VISIT(position_in_parent);
   VISIT(unique_position);
-  VISIT(insert_after_item_id);
   VISIT(deleted);
   VISIT(originator_cache_guid);
   VISIT(originator_client_item_id);
