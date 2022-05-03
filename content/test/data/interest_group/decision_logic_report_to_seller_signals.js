4c11c061b6a89a2f61dfcb9e636d12b6ab819397
Fabio Tirelo <ftirelo@chromium.org>
2022-05-03 20:03:20 +0000

    Revert "Rate limit reporting URL fetches."
    This reverts commit a777545af2a75b7e23a489ed708ff8cf4ce5f50a.
    
    Reason for revert: test failing on Win7 - https://ci.chromium.org/ui/p/chromium/builders/ci/Win7%20Tests%20(1)/127749/overview
    
    Original change's description:
    > Rate limit reporting URL fetches.
    >
    > 1. Use a per-profile global queue of reporting URLs.
    > 2. Wait until one report succeeds or fails before issuing the next one.
    > 3. Wait 50ms between issuing report requests.
    > 4. The report requests queue will be cleared if it has more than 100 entries when we're about to add entries.
    >
    > Plan to add a limit on how long a round of sending report can take (drop pending reports after 10min).
    > Also plan to allow sending multiple (like 5) reports in a batch instead of 1 at a time.
    >
    > Fixed: b/219015446
    > Bug: 1186444, b/219015446
    > Change-Id: I2dccfec8b3fe5010ecfd19587951acce213834dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3543505
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    > Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#998906}
    
    Bug: 1186444, b/219015446
    Change-Id: Ic3406202adfcf1b1002153fd30559ed1d3fe6e9a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625251
    Owners-Override: Fabio Tirelo <ftirelo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#999038}

--- content/test/data/interest_group/decision_logic_report_to_seller_signals.js
+++ content/test/data/interest_group/decision_logic_report_to_seller_signals.js
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 function scoreAd(
     adMetadata, bid, auctionConfig, trustedScoringSignals, browserSignals) {
-  forDebuggingOnly.reportAdAuctionWin(
-    auctionConfig.sellerSignals.reportTo + '_debug_win_report');
   return bid;
 }
 
