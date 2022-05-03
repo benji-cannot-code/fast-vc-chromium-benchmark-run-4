68f577921d685927ab0f13b165076aa471ac613d
Mahmoud Gawad <mgawad@google.com>
2022-05-03 13:06:23 +0000

    [Telemetry] Add missing include
    cros_healthd::mojom::NullableUint32 was moved to nullable_primitives.mojom.
    
    Bug: None
    Change-Id: Ia7ac04e88f63b193362d51d887983938675f9ef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620375
    Commit-Queue: Mahmoud Aly <mgawad@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#998829}

--- ash/webui/telemetry_extension_ui/services/diagnostics_service.cc
+++ ash/webui/telemetry_extension_ui/services/diagnostics_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
+#include "chromeos/services/cros_healthd/public/mojom/nullable_primitives.mojom.h"
 
 namespace ash {
 
