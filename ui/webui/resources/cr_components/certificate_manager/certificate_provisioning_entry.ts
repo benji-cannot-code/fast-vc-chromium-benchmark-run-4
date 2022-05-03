3918faa75f53f535fb662514f8e154be552c0f06
dpapad <dpapad@chromium.org>
2022-05-03 17:42:11 +0000

    Cert Manager: Import HTML templates at runtime, stop inlining at buildtime.
    Done by using html_to_wrapper() instead of html_to_js() for files
    holding Web component definitions. This is done so that the line numbers
    of reported TS errors match the checked-in code.
    
    Besides the BUILD.gn/gni changes in this CL, the diff has been mostly
    produced with the following script https://pastebin.com/13UXcny1, along
    with running "git cl format --js chrome/browser/resources".
    
    Bug: 1292025
    Change-Id: Idd8427d41c2ec5d903104df96e9d6a8fbb220c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622323
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998951}

--- ui/webui/resources/cr_components/certificate_manager/certificate_provisioning_entry.ts
+++ ui/webui/resources/cr_components/certificate_manager/certificate_provisioning_entry.ts
@@ -13,7 +13,7 @@ import '../../cr_elements/cr_lazy_render/cr_lazy_render.m.js';
 import 'chrome://resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 import './certificate_shared_css.js';
 
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {CrActionMenuElement} from '../../cr_elements/cr_action_menu/cr_action_menu.js';
 import {CrLazyRenderElement} from '../../cr_elements/cr_lazy_render/cr_lazy_render.m.js';
@@ -21,6 +21,7 @@ import {I18nMixin} from '../../js/i18n_mixin.js';
 
 import {CertificateProvisioningViewDetailsActionEvent} from './certificate_manager_types.js';
 import {CertificateProvisioningProcess} from './certificate_provisioning_browser_proxy.js';
+import {getTemplate} from './certificate_provisioning_entry.html.js';
 
 export interface CertificateProvisioningEntryElement {
   $: {
@@ -38,7 +39,7 @@ export class CertificateProvisioningEntryElement extends
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
