89a4e293fcaec22b40bd53024adceedbe3503122
Sammie Quon <sammiequon@chromium.org>
2022-05-03 18:15:47 +0000

    saved_desks: Fix some issues with event targeting.
    The grid widget takes up nearly the fullscreen and blocks all events
    from reaching the wallpaper, even though the wallpaper is visible. Adds
    an event targeter to the grid widget so we only handle events on the
    visible portions. This allows events to fall down to windows below.
    
    This also fixes an issue with RTL targeting, where mirrored bounds we
    messing up some custom logic for knowing when to commit textfield
    changes.
    
    Test: manual, ash_unittests DesksTemplatesTest.RightClickOnWallpaperStaysInOverview
    Change-Id: I2c3030995a2b946e3e99ae1066f0cf4c47b97a27
    Fixed: 1320946, 1320948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617174
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998973}

--- ash/wm/overview/overview_grid.cc
+++ ash/wm/overview/overview_grid.cc
@@ -1804,7 +1804,7 @@ bool OverviewGrid::IsShowingDesksTemplatesGrid() const {
 
 bool OverviewGrid::IsTemplateNameBeingModified() const {
   return desks_templates_grid_view_ &&
-         desks_templates_grid_view_->GridItemBeingModified();
+         desks_templates_grid_view_->IsTemplateNameBeingModified();
 }
 
 void OverviewGrid::UpdateNoWindowsWidget(bool no_items) {
