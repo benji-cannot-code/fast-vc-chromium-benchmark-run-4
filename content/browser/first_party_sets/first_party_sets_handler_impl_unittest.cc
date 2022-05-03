60432e18fe12ef60033008e15f479bfdb8d8c113
Chris Fredrickson <cfredric@chromium.org>
2022-05-03 20:17:48 +0000

    Replace ad-hoc SetPublicFirstPartySets calls with method in ContentBrowserClient.
    This essentially requires embedders to indicate whether they will
    (maybe asynchronously) call SetPublicFirstPartySets during startup, or
    not. This makes it easier to initialize First-Party Sets properly, since
    it is now done via a pull-based interface rather than a push-based
    interface (which would require code in every embedder in order to
    set up the First-Party Sets backend). Now, there is a single place (in
    content) that handles every embedder that won't need to explicitly call
    SetPublicFirstPartySets at some point (e.g. after initializing
    Component Updater, in Chrome's case).
    
    Bug: 1321908
    Change-Id: I47eaaaf77e548079e1bd6360fd573e877aa79b32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3623985
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999047}

--- content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
@@ -368,14 +368,6 @@ TEST_F(FirstPartySetsHandlerImplDisabledTest, IgnoresValid) {
             FAIL();  // Should not be called.
           }));
 
-  // Set required inputs to be able to receive the merged sets from
-  // FirstPartySetsLoader.
-  const std::string input =
-      "{\"owner\": \"https://example.test\",\"members\": "
-      "[\"https://aaaa.test\"]}";
-  ASSERT_TRUE(base::JSONReader::Read(input));
-  SetPublicFirstPartySetsAndWait(input);
-
   env().RunUntilIdle();
 
   // TODO(shuuran@chromium.org): test site state is cleared.
@@ -398,9 +390,6 @@ TEST_F(FirstPartySetsHandlerImplDisabledTest,
             FAIL();  // Should not be called.
           }));
 
-  SetPublicFirstPartySetsAndWait(R"({"owner": "https://example.test", )"
-                                 R"("members": ["https://member.test"]})");
-
   EXPECT_EQ(
       FirstPartySetsHandlerImpl::GetInstance()->GetSetsIfEnabledAndReady(),
       absl::nullopt);
@@ -414,11 +403,6 @@ class FirstPartySetsHandlerImplEnabledTest
 };
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest, PersistedSetsNotReady) {
-  const std::string input = R"({"owner": "https://foo.test", )"
-                            R"("members": ["https://member2.test"]})";
-  ASSERT_TRUE(base::JSONReader::Read(input));
-  SetPublicFirstPartySetsAndWait(input);
-
   // Empty `user_data_dir` will fail loading persisted sets.
   FirstPartySetsHandlerImpl::GetInstance()->Init(
       /*user_data_dir=*/{},
@@ -432,6 +416,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest, PersistedSetsNotReady) {
 }
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest, PublicFirstPartySetsNotReady) {
+  FirstPartySetsHandlerImpl::GetInstance()
+      ->SetEmbedderWillProvidePublicSetsForTesting(true);
   ASSERT_TRUE(base::WriteFile(persisted_sets_path_, "{}"));
 
   // Persisted sets are expected to be loaded with the provided path.
@@ -448,6 +434,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest, PublicFirstPartySetsNotReady) {
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest,
        Successful_PersistedSetsFileNotExist) {
+  FirstPartySetsHandlerImpl::GetInstance()
+      ->SetEmbedderWillProvidePublicSetsForTesting(true);
   const std::string input = R"({"owner": "https://foo.test", )"
                             R"("members": ["https://member2.test"]})";
   ASSERT_TRUE(base::JSONReader::Read(input));
@@ -480,6 +468,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 }
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest, Successful_PersistedSetsEmpty) {
+  FirstPartySetsHandlerImpl::GetInstance()
+      ->SetEmbedderWillProvidePublicSetsForTesting(true);
   ASSERT_TRUE(base::WriteFile(persisted_sets_path_, "{}"));
 
   const std::string input = R"({"owner": "https://foo.test", )"
@@ -515,6 +505,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest, Successful_PersistedSetsEmpty) {
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest,
        GetSetsIfEnabledAndReady_AfterSetsReady) {
+  FirstPartySetsHandlerImpl::GetInstance()
+      ->SetEmbedderWillProvidePublicSetsForTesting(true);
   ASSERT_TRUE(base::WriteFile(persisted_sets_path_, "{}"));
 
   const std::string input = R"({"owner": "https://example.test", )"
@@ -550,6 +542,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest,
        GetSetsIfEnabledAndReady_BeforeSetsReady) {
+  FirstPartySetsHandlerImpl::GetInstance()
+      ->SetEmbedderWillProvidePublicSetsForTesting(true);
   ASSERT_TRUE(base::WriteFile(persisted_sets_path_, "{}"));
 
   // Call GetSetsIfEnabledAndReady before the sets are ready.
--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -1327,6 +1327,10 @@ bool ContentBrowserClient::IsFirstPartySetsEnabled() {
   return base::FeatureList::IsEnabled(features::kFirstPartySets);
 }
 
+bool ContentBrowserClient::WillProvidePublicFirstPartySets() {
+  return false;
+}
+
 base::Value::Dict ContentBrowserClient::GetFirstPartySetsOverrides() {
   return base::Value::Dict();
 }
