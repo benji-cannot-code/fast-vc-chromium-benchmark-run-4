2b10561dd495d5860791a0b9fa88e03999ae20f6
Kalvin Lee <kdlee@chromium.org>
2022-05-03 00:00:13 +0000

    [PA] Namespace migration (34-omake of N)
    This change supports the PartitionAlloc namespace migration by cutting
    some non-PA dependencies on PA internals. In particular, the use of
    PA-internal page size is substituted with a `base::` analogue.
    
    I could not find an analogue for `PageAllocationGranularity()`, so the
    PA-internal usage is retained.
    
    Bug: 1288247
    Change-Id: Ief157fed5b7aaf55801fa19fec27412c52105b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582438
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998650}

--- gin/v8_platform_page_allocator.cc
+++ gin/v8_platform_page_allocator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/random.h"
 #include "base/check_op.h"
 #include "base/cpu.h"
+#include "base/memory/page_size.h"
 #include "build/build_config.h"
 
 namespace {
@@ -55,11 +56,11 @@ namespace gin {
 PageAllocator::~PageAllocator() = default;
 
 size_t PageAllocator::AllocatePageSize() {
-  return base::PageAllocationGranularity();
+  return partition_alloc::internal::PageAllocationGranularity();
 }
 
 size_t PageAllocator::CommitPageSize() {
-  return base::SystemPageSize();
+  return base::GetPageSize();
 }
 
 void PageAllocator::SetRandomMmapSeed(int64_t seed) {
