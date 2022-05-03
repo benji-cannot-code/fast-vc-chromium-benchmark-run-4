172aab157ddeb1b971b819e59894329f639f0133
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2022-05-03 01:37:31 +0000

    dark_mode: Remove dark mode icon from settings search
    Personalization hub will use personalization icon for all features
    including dark mode search results in Chrome Settings.
    
    (NIT) This CL also add "(Deprecated)" to dark mode settings enums
    that was removed in the other CL.
    
    Bug: 1321965
    Test: Manual
    Change-Id: I02518f3a1669dfad4a437913cdee2f610e5c4b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620785
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998680}

--- chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.js
+++ chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.js
@@ -638,8 +638,6 @@ function longestCommonSubstrings(string1, string2) {
           return 'os-settings:chrome';
         case Icon.kClock:
           return 'os-settings:access-time';
-        case Icon.kDarkMode:
-          return 'os-settings:dark-mode';
         case Icon.kDeveloperTags:
           return 'os-settings:developer-tags';
         case Icon.kDisplay:
