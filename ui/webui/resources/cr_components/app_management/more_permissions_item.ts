950ff85052693e7b0b93104f1cf659ddd9a42f5d
dpapad <dpapad@chromium.org>
2022-05-03 23:25:19 +0000

    AppManagement: Import HTML templates at runtime, stop inlining at buildtime.
    Done by using html_to_wrapper() instead of html_to_js() for files
    holding Web component definitions. This is done so that the line numbers
    of reported TS errors match the checked-in code.
    
    Besides the BUILD.gn/gni changes in this CL, the diff has been mostly
    produced with the following script https://pastebin.com/13UXcny1, along
    with running "git cl format --js chrome/browser/resources".
    
    Bug: 1292025
    Change-Id: Iaaa718ba0f92c490cf72fada8876fa7ffbd0346c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622434
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999133}

--- ui/webui/resources/cr_components/app_management/more_permissions_item.ts
+++ ui/webui/resources/cr_components/app_management/more_permissions_item.ts
@@ -6,11 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './shared_style.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {App} from './app_management.mojom-webui.js';
 import {BrowserProxy} from './browser_proxy.js';
 import {AppManagementUserAction} from './constants.js';
+import {getTemplate} from './more_permissions_item.html.js';
 import {recordAppManagementUserAction} from './util.js';
 
 export class AppManagementMorePermissionsItemElement extends PolymerElement {
@@ -19,7 +20,7 @@ export class AppManagementMorePermissionsItemElement extends PolymerElement {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
