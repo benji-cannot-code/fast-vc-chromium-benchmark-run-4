021a3157365c03a4426bec5f3bbd10f124b0395f
Stephan Hartmann <stha09@googlemail.com>
2022-05-03 10:04:20 +0000

    IWYU: add cstring for std::strlen in fenced_frame_utils
    Bug: 957519
    Change-Id: Ie886531bc4115f280ca109819c6515ae20ea7115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3609973
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#998791}

--- third_party/blink/common/fenced_frame/fenced_frame_utils.cc
+++ third_party/blink/common/fenced_frame/fenced_frame_utils.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/fenced_frame/fenced_frame_utils.h"
 
+#include <cstring>
+
 #include "base/guid.h"
 #include "base/strings/string_util.h"
 
