cc2a42ba35d79f44284473ee8e6e3338656c1623
Dana Fried <dfried@chromium.org>
2022-05-03 23:49:31 +0000

    Allow smaller callback signatures for step start and end.
    InteractionSequence::StepBuilder now allows you to pass versions of step
    start and end callbacks omitting parameters you might not need. This
    allows you to write less verbose interaction testing code.
    
    Tests are provided.
    
    The template functions to push unused arguments onto the start (or end)
    of a callback to adapt it for a larger signature should probably be
    moved up into callback.h or similar.
    
    Change-Id: I84ce808a4fbe3d44aa2ae55332e3c484c7ce6431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625092
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999142}

--- ui/base/interaction/interaction_sequence.cc
+++ ui/base/interaction/interaction_sequence.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/interaction_sequence.h"
 
 #include "base/bind.h"
+#include "base/callback_forward.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_auto_reset.h"
@@ -27,6 +28,27 @@ template <typename Signature, typename... Args>
 void RunIfValid(base::OnceCallback<Signature> callback, Args... args) {
   if (callback)
     std::move(callback).Run(args...);
+}
+
+// Insert an unused argument `Arg` in the front of the argument list for
+// `callback`, and return the new callback with the dummy argument.
+template <typename Arg, typename Ret, typename... Args>
+base::OnceCallback<Ret(Arg, Args...)> PushUnusedArg(
+    base::OnceCallback<Ret(Args...)> callback) {
+  return base::BindOnce([](base::OnceCallback<Ret(Args...)> callback, Arg arg,
+                           Args... args) { std::move(callback).Run(args...); },
+                        std::move(callback));
+}
+
+// Insert two unused arguments `Arg1` and `Arg2` in the front of the argument
+// list for `callback`, and return the new callback with the dummy arguments.
+template <typename Arg1, typename Arg2, typename Ret, typename... Args>
+base::OnceCallback<Ret(Arg1, Arg2, Args...)> PushUnusedArgs2(
+    base::OnceCallback<Ret(Args...)> callback) {
+  return base::BindOnce(
+      [](base::OnceCallback<Ret(Args...)> callback, Arg1 arg1, Arg2 arg2,
+         Args... args) { std::move(callback).Run(args...); },
+      std::move(callback));
 }
 
 // Sets step->must_remain_visible if it does not have a value.
@@ -245,8 +267,32 @@ InteractionSequence::StepBuilder::SetStartCallback(
 }
 
 InteractionSequence::StepBuilder&
+InteractionSequence::StepBuilder::SetStartCallback(
+    base::OnceCallback<void(TrackedElement*)> start_callback) {
+  step_->start_callback =
+      PushUnusedArg<InteractionSequence*>(std::move(start_callback));
+  return *this;
+}
+
+InteractionSequence::StepBuilder&
+InteractionSequence::StepBuilder::SetStartCallback(
+    base::OnceClosure start_callback) {
+  step_->start_callback =
+      PushUnusedArgs2<InteractionSequence*, TrackedElement*>(
+          std::move(start_callback));
+  return *this;
+}
+
+InteractionSequence::StepBuilder&
 InteractionSequence::StepBuilder::SetEndCallback(StepEndCallback end_callback) {
   step_->end_callback = std::move(end_callback);
+  return *this;
+}
+
+InteractionSequence::StepBuilder&
+InteractionSequence::StepBuilder::SetEndCallback(
+    base::OnceClosure end_callback) {
+  step_->end_callback = PushUnusedArg<TrackedElement*>(std::move(end_callback));
   return *this;
 }
 
