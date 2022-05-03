3621bcc6362e1b7946b19dade7457786686f4412
Fred Shih <ffred@chromium.org>
2022-05-03 05:18:26 +0000

    Fix ordering of enums for wayland server positioner
    This changes the odering to match the enum delcaration.
    
    Bug: 1315760
    Change-Id: Id7755edc02235ba90d613ba61e27c1a2d649056f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622634
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998731}

--- components/exo/wayland/wayland_positioner.cc
+++ components/exo/wayland/wayland_positioner.cc
@@ -57,12 +57,12 @@ DecomposeStableAnchor(uint32_t anchor) {
     case XDG_POSITIONER_ANCHOR_TOP_LEFT:
       return std::make_pair(WaylandPositioner::Direction::kNegative,
                             WaylandPositioner::Direction::kNegative);
-    case XDG_POSITIONER_ANCHOR_TOP_RIGHT:
-      return std::make_pair(WaylandPositioner::Direction::kPositive,
-                            WaylandPositioner::Direction::kNegative);
     case XDG_POSITIONER_ANCHOR_BOTTOM_LEFT:
       return std::make_pair(WaylandPositioner::Direction::kNegative,
                             WaylandPositioner::Direction::kPositive);
+    case XDG_POSITIONER_ANCHOR_TOP_RIGHT:
+      return std::make_pair(WaylandPositioner::Direction::kPositive,
+                            WaylandPositioner::Direction::kNegative);
     case XDG_POSITIONER_ANCHOR_BOTTOM_RIGHT:
       return std::make_pair(WaylandPositioner::Direction::kPositive,
                             WaylandPositioner::Direction::kPositive);
@@ -114,12 +114,12 @@ DecomposeStableGravity(uint32_t gravity) {
     case XDG_POSITIONER_GRAVITY_TOP_LEFT:
       return std::make_pair(WaylandPositioner::Direction::kNegative,
                             WaylandPositioner::Direction::kNegative);
-    case XDG_POSITIONER_GRAVITY_TOP_RIGHT:
-      return std::make_pair(WaylandPositioner::Direction::kPositive,
-                            WaylandPositioner::Direction::kNegative);
     case XDG_POSITIONER_GRAVITY_BOTTOM_LEFT:
       return std::make_pair(WaylandPositioner::Direction::kNegative,
                             WaylandPositioner::Direction::kPositive);
+    case XDG_POSITIONER_GRAVITY_TOP_RIGHT:
+      return std::make_pair(WaylandPositioner::Direction::kPositive,
+                            WaylandPositioner::Direction::kNegative);
     case XDG_POSITIONER_GRAVITY_BOTTOM_RIGHT:
       return std::make_pair(WaylandPositioner::Direction::kPositive,
                             WaylandPositioner::Direction::kPositive);
