20c9ef7cc024e9c4681d65c5292b05ae40e3b5b4
Dominik Röttsches <drott@chromium.org>
2022-05-04 13:58:40 +0000

    Revert "Login: Log login types in association with policy and total users"
    This reverts commit 7d041aa41f681c83f77255d133a24f7c18edd67b.
    
    Reason for revert: Failing on MSAN, see https://crbug.com/1322463
    
    Original change's description:
    > Login: Log login types in association with policy and total users
    >
    > The purpose of the metrics is to understand the usage of the device
    > policy show_user_names.
    >
    > We tracked the following 4 type of login in association with policy
    > setting and total user numbers:
    > - "ephemeral" logins
    > - "new" online logins
    > - "existing" online logins
    > - offline logins
    >
    > Bug: 1298966
    > Change-Id: I680f1a2f2fda83da6cca28c96e62916f0f47f827
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596183
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Commit-Queue: Sherri Lin <sherrilin@google.com>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#999300}
    
    Bug: 1298966
    Change-Id: Idb7db254f35e3f04058f488efa51793c4f62bbe3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627636
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#999370}

--- ash/components/login/auth/login_performer.cc
+++ ash/components/login/auth/login_performer.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/metrics/login_event_recorder.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_service.h"
-#include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_names.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 
@@ -59,13 +58,6 @@ void LoginPerformer::OnAuthSuccess(const UserContext& user_context) {
 
   // Do not distinguish between offline and online success.
   metrics_recorder_->OnLoginSuccess(OFFLINE_AND_ONLINE);
-  const bool is_known_user = user_manager::UserManager::Get()->IsKnownUser(
-      user_context.GetAccountId());
-  metrics_recorder_->OnIsUserNew(is_known_user);
-  bool is_login_offline =
-      user_context.GetAuthFlow() == UserContext::AUTH_FLOW_OFFLINE ||
-      user_context.GetAuthFlow() == UserContext::AUTH_FLOW_EASY_UNLOCK;
-  metrics_recorder_->OnIsLoginOffline(is_login_offline);
 
   VLOG(1) << "LoginSuccess hash: " << user_context.GetUserIDHash();
   base::SequencedTaskRunnerHandle::Get()->PostTask(
