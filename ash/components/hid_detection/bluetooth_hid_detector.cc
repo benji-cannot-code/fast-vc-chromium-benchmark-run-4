6b16e13c27a729522403a65710375d9a6d7d8506
Gordon Seto <gordonseto@google.com>
2022-05-04 21:52:32 +0000

    [CrOS Bluetooth] Implement DisplayPinCode and DisplayPasskey pairing.
    Implement DisplayPinCode() and DisplayPasskey() pairing authorizations
    in BluetoothHidDetectorImpl that sets the Bluetooth HID detection status
    with the required code and informs the delegate what code is required
    and how many keys have been entered.
    
    Fixed: 229013487
    Test: BluetoothHidDetectorImplTest
    Change-Id: I264a70ebeabfc7db38e8d11562dd4b4984a76020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597569
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999602}

--- ash/components/hid_detection/bluetooth_hid_detector.cc
+++ ash/components/hid_detection/bluetooth_hid_detector.cc
@@ -9,6 +9,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace hid_detection {
 
+BluetoothHidPairingState::BluetoothHidPairingState(const std::string& code,
+                                                   uint8_t num_keys_entered)
+    : code(code), num_keys_entered(num_keys_entered) {}
+
+BluetoothHidPairingState::BluetoothHidPairingState(
+    BluetoothHidPairingState&& other) {
+  code = std::move(other.code);
+  num_keys_entered = other.num_keys_entered;
+}
+
+BluetoothHidPairingState& BluetoothHidPairingState::operator=(
+    BluetoothHidPairingState&& other) {
+  code = std::move(other.code);
+  num_keys_entered = other.num_keys_entered;
+  return *this;
+}
+
+BluetoothHidPairingState::~BluetoothHidPairingState() = default;
+
 BluetoothHidDetector::BluetoothHidMetadata::BluetoothHidMetadata(
     std::string name,
     BluetoothHidType type)
@@ -32,18 +51,22 @@ BluetoothHidDetector::BluetoothHidMetadata::~BluetoothHidMetadata() = default;
 
 BluetoothHidDetector::BluetoothHidDetectionStatus::BluetoothHidDetectionStatus(
     absl::optional<BluetoothHidDetector::BluetoothHidMetadata>
-        current_pairing_device)
-    : current_pairing_device(std::move(current_pairing_device)) {}
+        current_pairing_device,
+    absl::optional<BluetoothHidPairingState> pairing_state)
+    : current_pairing_device(std::move(current_pairing_device)),
+      pairing_state(std::move(pairing_state)) {}
 
 BluetoothHidDetector::BluetoothHidDetectionStatus::BluetoothHidDetectionStatus(
     BluetoothHidDetectionStatus&& other) {
   current_pairing_device = std::move(other.current_pairing_device);
+  pairing_state = std::move(other.pairing_state);
 }
 
 BluetoothHidDetector::BluetoothHidDetectionStatus&
 BluetoothHidDetector::BluetoothHidDetectionStatus::operator=(
     BluetoothHidDetectionStatus&& other) {
   current_pairing_device = std::move(other.current_pairing_device);
+  pairing_state = std::move(other.pairing_state);
   return *this;
 }
 
