d614c68265e10960aad0eff09fbc542c86de967a
Alex Gough <ajgo@chromium.org>
2022-05-04 01:05:46 +0000

    Mark ReferrerPolicy as [Extensible]
    Comment from before this was marked Stable[1]:
    // Don't make backwards-incompatible changes to this definition!
    // It's used in PageState serialization, so backwards incompatible changes
    // would cause stored PageState objects to be un-parseable. Please contact the
    // page state serialization owners before making such a change.
    
    No changes since forever[0]. Marked stable[1]. Referrer policy kDefault
    is the normal default[2] so we can select this as the [Default].
    Stability here means that previously saved values can still be parsed,
    and this change allows this. Adding future values will not break this
    promise, so Extensible is fine.
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/1212345
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2220251
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/common/page_state/page_state_serialization.cc;drc=e76fe2d8335a5ef60a65f15e915358ace28b82b4;l=535
    
    Bug: 1193875
    Change-Id: I3861b1a1ca1a69daf694562b6986aae3fd63195b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625442
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999177}

--- mojo/public/tools/bindings/checks/mojom_attributes_check.py
+++ mojo/public/tools/bindings/checks/mojom_attributes_check.py
@@ -90,7 +90,6 @@ _STABLE_ONLY_ALLOWLISTED_ENUMS = {
     'crosapi.mojom.RequestTextSelectionActionsStatus',
     'crosapi.mojom.RequestUrlHandlerListStatus',
     'crosapi.mojom.TriState',
-    'network.mojom.ReferrerPolicy',
 }
 
 
