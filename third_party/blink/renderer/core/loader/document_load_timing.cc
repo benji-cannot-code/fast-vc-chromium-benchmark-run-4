49dfe299fc4fbe1df449afca01e6cff6eb6ec89a
Dave Tapuska <dtapuska@chromium.org>
2022-05-04 20:25:08 +0000

    Adjust use tracing value to log outermost main frame.
    Since isLoadingMainFrame is ambiguous for MPArch log the outermost
    main frame as well. Catapult and devtools will be adjusted to use
    this new value if present.
    
    BUG=1314858
    
    Change-Id: I4ecea1095f02ef0e2a739e4e1264a7bcdffab01e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625434
    Reviewed-by: Deep Roy <dproy@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999559}

--- third_party/blink/renderer/core/loader/document_load_timing.cc
+++ third_party/blink/renderer/core/loader/document_load_timing.cc
@@ -130,6 +130,8 @@ void DocumentLoadTiming::WriteNavigationStartDataIntoTracedValue(
            document_loader_ ? document_loader_->Url().GetString() : "");
   dict.Add("isLoadingMainFrame",
            GetFrame() ? GetFrame()->IsMainFrame() : false);
+  dict.Add("isOutermostMainFrame",
+           GetFrame() ? GetFrame()->IsOutermostMainFrame() : false);
   dict.Add("navigationId", IdentifiersFactory::LoaderId(document_loader_));
 }
 
