7050c99580f51e6c6c641470f8ff305cc69849c7
Patrick Noland <pnoland@chromium.org>
2022-05-04 21:39:22 +0000

    [Journeys] Display journeys UI in the history activity
    This adds a "full page" version of the history clusters UI built on
    SelectableListLayout. This version of the UI is shown in place of the
    normal history UI when an intent specifying a specific extra is used
    to launch the history activity. This allows us to test further additions
    to the full page UI for the moment but will eventually be replaced with
    a scheme where we use a single content view that contains a toggle that
    allows switching between "normal" history and the journeys view.
    
    Bug: 1303171
    Change-Id: I627426b37efa5d5d991f8562e466851d02b22566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620961
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999598}

---
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersToolbar.java
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.history_clusters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import org.chromium.components.browser_ui.widget.selectable_list.SelectableListToolbar;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * Toolbar for controlling the list of history clusters in the Journeys UI.
+ */
+class HistoryClustersToolbar extends SelectableListToolbar<PropertyModel> {
+    private EditText mSearchText;
+
+    /**
+     * Constructor for inflating from XML.
+     */
+    public HistoryClustersToolbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inflateMenu(R.menu.history_clusters_menu);
+    }
+
+    @Override
+    public void initializeSearchView(
+            SearchDelegate searchDelegate, int hintStringResId, int searchMenuItemId) {
+        super.initializeSearchView(searchDelegate, hintStringResId, searchMenuItemId);
+        mSearchText = findViewById(R.id.search_text);
+    }
+
+    void setSearchText(String text) {
+        mSearchText.setText(text);
+        mSearchText.setSelection(text.length());
+    }
+}
