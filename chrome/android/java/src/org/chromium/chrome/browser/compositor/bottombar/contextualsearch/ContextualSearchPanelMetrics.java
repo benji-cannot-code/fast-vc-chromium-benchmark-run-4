010dcd7def4a24725b1c30d336a6906299152403
Gang Wu <gangwu@chromium.org>
2022-05-04 20:34:11 +0000

    [CS]Remove Search.ContextualSearchContextualCardsIntegration.ResultsSeen
    Delete the Search.ContextualSearchContextualCardsIntegration.ResultsSeen
    since the histogram is expired and no longer used.
    
    Bug: 1089462
    Change-Id: I083788338caa49926ef0d21cbfbcac3987c7b79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625596
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999566}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanelMetrics.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanelMetrics.java
@@ -131,7 +131,6 @@ public class ContextualSearchPanelMetrics {
             }
 
             if (mWasContextualCardsDataShown) {
-                ContextualSearchUma.logContextualCardsResultsSeen(mWasSearchContentViewSeen);
                 EngagementSuppression.registerContextualCardsImpression(mWasSearchContentViewSeen);
             }
             ContextualSearchUma.logCardTagSeen(mWasSearchContentViewSeen, mCardTag);
