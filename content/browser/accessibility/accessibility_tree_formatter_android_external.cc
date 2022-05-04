a7a8282055c45c026ba64cf3f35062a2b292f64e
Mark Schillaci <mschillaci@google.com>
2022-05-04 19:22:48 +0000

    Fix possible crashes in accessibility tree formatter
    This CL makes a quick fix to the accessibility tree formatters used
    during some unit tests and on the chrome://accessibility page. On
    some platforms, if no root node exists, the CHECK can fail and Chrome
    will crash. We change this to instead return an empty object and
    display nothing on the page, rather than crashing.
    
    AX-Relnotes: N/A
    Bug: 1315871
    Change-Id: I13015c0db2bab31e559e8fad8c9a7f7d8a4f3613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601235
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#999541}

--- content/browser/accessibility/accessibility_tree_formatter_android_external.cc
+++ content/browser/accessibility/accessibility_tree_formatter_android_external.cc
@@ -22,7 +22,9 @@ AccessibilityTreeFormatterAndroidExternal::
 
 base::Value AccessibilityTreeFormatterAndroidExternal::BuildTree(
     ui::AXPlatformNodeDelegate* root) const {
-  CHECK(root);
+  if (!root) {
+    return base::Value(base::Value::Type::DICTIONARY);
+  }
 
   BrowserAccessibility* root_internal =
       BrowserAccessibility::FromAXPlatformNodeDelegate(root);
