d04decf020af4b57a0844a25f26d8d4f5aa37be1
Paul Adedeji <pauladedeji@google.com>
2022-05-04 18:59:27 +0000

    [ntp] Check if sync is enabled before showing Drive module
    Drive is a sync dependent module.
    
    When a user goes to turn on sync in Chrome, their NTP registers that they are synced. When they press cancel everything on the NTP should reflect that. Currently, the Drive module hinges on the initial sync setting and doesn't register changes in sync, till refresh, even if a user cancels sync.
    
    This change only enables the Drive module after a user enables sync via action. The resulting side effect is they now have to refresh their NTP before seeing the Drive module, versus currently where they see it without refreshing.
    
    Video*: http://recall/-/fGxiewmUbUjOWhwT9DjDH1/bO8fG9hgP4vUd0fPZI5FvT
    
    * This video demonstrates the need for refresh only. Fake data can be shown without sync enabled if it is set via chrome://flags.
    
    Change-Id: I2da4a41aefccc09df41812b71de7f8f19eddc875
    Bug: 1318421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617165
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#999526}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/search_provider_logos/logo_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -61,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync/driver/sync_service.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -573,7 +575,11 @@ void NewTabPageUI::ResetProfilePrefs(PrefService* prefs) {
 
 // static
 bool NewTabPageUI::IsDriveModuleEnabled(Profile* profile) {
-  if (!base::FeatureList::IsEnabled(ntp_features::kNtpDriveModule)) {
+  // TODO(crbug.com/1321896): Explore not requiring sync for the drive
+  // module to be enabled.
+  auto* sync_service = SyncServiceFactory::GetForProfile(profile);
+  if (!base::FeatureList::IsEnabled(ntp_features::kNtpDriveModule) ||
+      !sync_service || !sync_service->IsSyncFeatureEnabled()) {
     return false;
   }
   if (base::GetFieldTrialParamValueByFeature(
@@ -581,7 +587,7 @@ bool NewTabPageUI::IsDriveModuleEnabled(Profile* profile) {
           ntp_features::kNtpDriveModuleManagedUsersOnlyParam) != "true") {
     return true;
   }
-  // TODO(https://crbug.com/1213351): Stop calling the private method
+  // TODO(crbug.com/1213351): Stop calling the private method
   // FindExtendedPrimaryAccountInfo().
   auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
   return /* Can be null if Chrome signin is disabled. */ identity_manager &&
