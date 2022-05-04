f112c76aed479ed6d600a72a32abad713474dc86
Xiaohan Wang <xhwang@chromium.org>
2022-05-04 14:58:28 +0000

    media: Check shutdown state in MediaFoundationSourceWrapper::GetInputTrustAuthority()
    From crash report, it seems this could be called during shutdown. Check
    shutdown state in this call to avoid crashes.
    
    Bug: 1322349
    Change-Id: I4147cb4a6826d77892805d592128fd1c67df7333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627114
    Reviewed-by: Frank Li <frankli@microsoft.com>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999400}

--- media/renderers/win/media_foundation_source_wrapper.cc
+++ media/renderers/win/media_foundation_source_wrapper.cc
@@ -340,6 +340,9 @@ HRESULT MediaFoundationSourceWrapper::GetInputTrustAuthority(
     IUnknown** object_out) {
   DVLOG_FUNC(1);
 
+  if (state_ == State::kShutdown)
+    return MF_E_SHUTDOWN;
+
   if (stream_id >= StreamCount())
     return E_INVALIDARG;
 
--- media/renderers/win/media_foundation_stream_wrapper.cc
+++ media/renderers/win/media_foundation_stream_wrapper.cc
@@ -308,9 +308,9 @@ void MediaFoundationStreamWrapper::ProcessRequestsIfPossible() {
     return;
   }
 
-  if (!demuxer_stream_ || pending_stream_read_) {
+  if (!demuxer_stream_ || pending_stream_read_)
     return;
-  }
+
   demuxer_stream_->Read(
       base::BindOnce(&MediaFoundationStreamWrapper::OnDemuxerStreamRead,
                      weak_factory_.GetWeakPtr()));
