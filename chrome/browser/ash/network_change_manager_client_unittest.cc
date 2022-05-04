c46016652b23fc299495166c5ea4fff899909231
Regan Hsu <hsuregan@google.com>
2022-05-04 23:27:22 +0000

    [CrOS Network] Add 5G support to ConnectionTypeFromShill()
    Updates NetworkChangeManagerClient::ConnectionTypeFromShill() to support
    5G, which it did not previously.
    
    Fixed: b/231464393
    Change-Id: I8569d9022d995633683c7a6719aadcf3d2b71059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627833
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999643}

--- chrome/browser/ash/network_change_manager_client_unittest.cc
+++ chrome/browser/ash/network_change_manager_client_unittest.cc
@@ -93,6 +93,8 @@ TEST(NetworkChangeManagerClientTest, ConnectionTypeFromShill) {
        NetworkChangeNotifier::CONNECTION_4G},
       {shill::kTypeCellular, shill::kNetworkTechnologyLteAdvanced,
        NetworkChangeNotifier::CONNECTION_4G},
+      {shill::kTypeCellular, shill::kNetworkTechnology5gNr,
+       NetworkChangeNotifier::CONNECTION_5G},
       {shill::kTypeCellular, "unknown technology",
        NetworkChangeNotifier::CONNECTION_2G}};
 
