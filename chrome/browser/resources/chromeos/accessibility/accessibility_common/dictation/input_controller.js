6e59a93c50300484ad4c516a11ac019bbfc028e3
Akihiro Ota <akihiroota@chromium.org>
2022-05-04 19:01:28 +0000

    Dictation: Fix bug where unnecessary text is committed
    This change fixes a bug where text is committed in the following
    scenario:
    
    1. Toggle Dictation
    2. Say: 'Move to the next line'
    3. Wait for the UI to show macro success
    4. Toggle Dictation off
    
    In the above case, 'Move to the next line' gets committed to the
    text field unnecessarily.
    
    committed after a command is performed.
    
    AX-Relnotes: Fixed a Dictation bug where text is unnecessarily
    Fixed: 1322188, 1216111
    Change-Id: I7e1550365352b62f55ff553b4e4453dd0493fd1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625432
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999528}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
@@ -23,12 +23,6 @@ export class InputController {
      */
     this.previousImeEngineId_ = '';
 
-    /**
-     * The current composition text, if any.
-     * @private {string}
-     */
-    this.currentComposition_ = '';
-
     /** @private {function():void} */
     this.stopDictationCallback_ = stopDictationCallback;
 
@@ -110,22 +104,10 @@ export class InputController {
    * composed, commits it.
    */
   disconnect() {
-    // Commit composition text, if any.
-    if (this.currentComposition_.length > 0) {
-      this.commitText(this.currentComposition_);
-    }
-
     // Clean up IME state and reset to the previous IME method.
     this.activeImeContextId_ = InputController.NO_ACTIVE_IME_CONTEXT_ID_;
     chrome.inputMethodPrivate.setCurrentInputMethod(this.previousImeEngineId_);
     this.previousImeEngineId_ = '';
-  }
-
-  /**
-   * @return {boolean} Whether any text is currently being composed.
-   */
-  hasCompositionText() {
-    return this.currentComposition_.length > 0;
   }
 
   /**
@@ -139,7 +121,6 @@ export class InputController {
 
     text = this.adjustCommitText_(text);
     chrome.input.ime.commitText({contextID: this.activeImeContextId_, text});
-    this.setCurrentComposition('');
   }
 
   /**
@@ -184,11 +165,6 @@ export class InputController {
     }
 
     this.editableNode_ = node;
-  }
-
-  /** @param {string} text */
-  setCurrentComposition(text) {
-    this.currentComposition_ = text;
   }
 
   /**
