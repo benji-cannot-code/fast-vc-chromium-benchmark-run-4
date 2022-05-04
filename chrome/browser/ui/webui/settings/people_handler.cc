01a98197baf30a3ff407743bc467d17a52979bdc
Alex Ilin <alexilin@chromium.org>
2022-05-04 10:54:30 +0000

    [Signin] Unify more sign-in flows between DICE and Lacros
    This CL makes all sign-in flows in profile_menu_view.cc and
    people_handler.cc use common functions in signin_ui_util.
    
    Bug: 1316608
    Change-Id: Id73e1073f92c34743cacfee66407563921e86f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3623659
    Quick-Run: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999314}

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
@@ -363,23 +364,11 @@ void PeopleHandler::DisplayGaiaLoginInNewTabOrWindow(
     SigninErrorController* error_controller =
         SigninErrorControllerFactory::GetForProfile(browser->profile());
     DCHECK(error_controller->HasError());
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
     signin_ui_util::ShowReauthForPrimaryAccountWithAuthError(browser,
                                                              access_point);
-#else
-    browser->window()->ShowAvatarBubbleFromAvatarButton(
-        BrowserWindow::AVATAR_BUBBLE_MODE_REAUTH, access_point, false);
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
   } else {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-    signin_ui_util::ShowSigninPromptAndMaybeEnableSync(
-        browser, browser->profile(), /*enable_sync=*/true, access_point,
-        signin_metrics::PromoAction::
-            PROMO_ACTION_NEW_ACCOUNT_NO_EXISTING_ACCOUNT);
-#else
-    browser->window()->ShowAvatarBubbleFromAvatarButton(
-        BrowserWindow::AVATAR_BUBBLE_MODE_SIGNIN, access_point, false);
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+    signin_ui_util::EnableSyncFromSingleAccountPromo(browser, CoreAccountInfo(),
+                                                     access_point);
   }
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
