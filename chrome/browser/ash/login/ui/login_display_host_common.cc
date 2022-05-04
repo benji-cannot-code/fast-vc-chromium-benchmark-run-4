cae3e0be71b87db02333569077f2d30b74a67848
Danila Kuzmin <dkuzmin@google.com>
2022-05-04 11:26:19 +0000

    Reland "[CloudReady][OOBE]: Show new terms to device users after update"
    This is a reland of commit a31450409a40b24d8edf887b89baf7cc9711680a
    
    Original change's description:
    > [CloudReady][OOBE]: Show new terms to device users after update
    >
    > Part [1/2]:
    > After updating from CloudReady to Flex we need to show new terms to
    > old users. This CL makes ConsolidatedConsentScreen to be shown for
    > all users and HWDataCollectionScreen to the owner. The second part
    > of the chain will cover cases when ConsolidatedConsent isn't enabled
    > to show EulaScreen instead.
    >
    > Bug: b:196201668
    > Change-Id: I49c397f2757093847a6d1a38cdac2095ec99bb53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450657
    > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    > Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#989915}
    
    Bug: b:196201668
    Change-Id: I9ce5c61a496d2f733494f6ce1cc348688db9f905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599018
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999328}

--- chrome/browser/ash/login/ui/login_display_host_common.cc
+++ chrome/browser/ash/login/ui/login_display_host_common.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/ui/login_display_host_common.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/login_accelerators.h"
 #include "base/bind.h"
 #include "base/callback.h"
@@ -451,6 +452,15 @@ void LoginDisplayHostCommon::StartManagementTransition() {
 void LoginDisplayHostCommon::ShowTosForExistingUser() {
   SetScreenAfterManagedTos(ash::OOBE_SCREEN_UNKNOWN);
   StartUserOnboarding();
+}
+
+void LoginDisplayHostCommon::ShowNewTermsForFlexUsers() {
+  // TODO(b/196201668): show EULA screen to users if consolidated consent isn't
+  //                    enabled.
+  DCHECK(features::IsOobeConsolidatedConsentEnabled());
+  SetScreenAfterManagedTos(ConsolidatedConsentScreenView::kScreenId);
+  wizard_context_->is_cloud_ready_update_flow = true;
+  StartWizard(TermsOfServiceScreenView::kScreenId);
 }
 
 void LoginDisplayHostCommon::SetAuthSessionForOnboarding(
