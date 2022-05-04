58a0c59a91cbeb37aa94bfaf250f8f3dca7ccd8f
Travis Skare <skare@chromium.org>
2022-05-04 17:22:41 +0000

    [Crow] Add metrics for UI shown/used.
    Currently independent of IPH.
    
    Bug: 1321236
    
    Change-Id: I5813292530ac1808401572699d1b8ca6824944d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616795
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999470}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/webfeed/WebFeedMainMenuItem.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/webfeed/WebFeedMainMenuItem.java
@@ -20,6 +20,7 @@ import androidx.annotation.DrawableRes;
 import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.base.Callback;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.feed.FeedFeatures;
 import org.chromium.chrome.browser.feed.FeedServiceBridge;
 import org.chromium.chrome.browser.feed.R;
@@ -272,9 +273,11 @@ public class WebFeedMainMenuItem extends FrameLayout {
         mCrowButton.getPrimaryTextView().setText(mCrowButtonDelegate.getButtonText());
         mCrowButton.setOnClickListener((view) -> {
             if (mTab == null) return;
+            RecordUserAction.record("Crow.LaunchCustomTab.AppMenu");
             Activity activity = mTab.getWindowAndroid().getActivity().get();
             mCrowButtonDelegate.launchCustomTab(activity, mUrl);
         });
+        RecordUserAction.record("Crow.EntryPointShown.AppMenu");
         mCrowButton.setVisibility(View.VISIBLE);
     }
 
