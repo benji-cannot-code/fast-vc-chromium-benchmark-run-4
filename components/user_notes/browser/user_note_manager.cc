9379d7ce7bce72587a8c977c44a40ed6834486cb
Guillaume Jenkins <gujen@google.com>
2022-05-04 18:58:27 +0000

    [UserNotes] Refactor unit tests to remove NullPage() hack
    At the time the UserNoteService and UserNoteManager unit tests were
    created, I wasn't aware that there was an elaborate test harness to work
    with content/web/public classes in unit tests. So, rather than creating
    my own mock for a Page object, I had used a hack to feed a null Page
    reference to the UserNoteManager constructor. This worked because the
    PageUserData constructor did nothing with the Page ref, but it was very
    fragile.
    
    This CL does the right thing by using the content test harness to set up
    Page objects that can be used to create a UserNoteManager via the static
    CreateForPage initializer, which in turn allows us to get rid of the
    CreateForTest initializer that had been added for these tests.
    
    Bug: 1321225
    Change-Id: I288b3b81c8cf952c8b1e078dcfa6746a57306170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615873
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#999525}

--- components/user_notes/browser/user_note_manager.cc
+++ components/user_notes/browser/user_note_manager.cc
@@ -11,13 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace user_notes {
 
-// static
-std::unique_ptr<UserNoteManager> UserNoteManager::CreateForTest(
-    content::Page& page,
-    base::SafeRef<UserNoteService> service) {
-  return base::WrapUnique(new UserNoteManager(page, service));
-}
-
 UserNoteManager::UserNoteManager(content::Page& page,
                                  base::SafeRef<UserNoteService> service)
     : PageUserData<UserNoteManager>(page), service_(service) {}
