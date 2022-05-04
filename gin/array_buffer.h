443be33463ef1e8de6edce933432419c2ce1f60d
Samuel Groß <saelo@chromium.org>
2022-05-04 15:54:32 +0000

    Use PartitionAlloc in gin::ArrayBufferAllocator
    When the V8 sandbox is enabled, ArrayBuffer backing stores must be
    located inside the sandbox for them to be accessible to V8. For buffers
    allocated through gin's ArrayBufferAllocator, this is achieved by
    creating a partition in PartitionAlloc's ConfigurablePool, which is
    itself placed inside the sandbox during initialization, and allocating
    backing stores from that partition instead of through malloc.
    
    Bug: 1218005
    Change-Id: I103947de479d923fd2edd4edb06567e91a341ba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627651
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999427}

--- gin/array_buffer.h
+++ gin/array_buffer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/shared_memory_mapper.h"
@@ -21,11 +22,16 @@ namespace gin {
 
 class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
  public:
+  ArrayBufferAllocator();
+
   void* Allocate(size_t length) override;
   void* AllocateUninitialized(size_t length) override;
   void Free(void* data, size_t length) override;
 
   GIN_EXPORT static ArrayBufferAllocator* SharedInstance();
+
+ private:
+  base::PartitionAllocator partition_;
 };
 
 class GIN_EXPORT ArrayBuffer {
