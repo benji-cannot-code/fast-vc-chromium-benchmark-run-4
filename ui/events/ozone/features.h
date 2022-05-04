b950fee9b111969efe29c335434da3511cfc09fb
Jing Wang <jiwan@chromium.org>
2022-05-04 00:46:43 +0000

    Load model version and store in model config
    We will add a beta version model for redrix in later CL, so in this CL
    we paved the road for it. This change will not affect and existing
    behavior, since the loaded model_version is not actually used anywhere.
    
    Bug: 228271213
    Change-Id: I5c18eb7a62a0182cad2cf0ed0ba083b2d5f94450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582244
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Quick-Run: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999170}

--- ui/events/ozone/features.h
+++ ui/events/ozone/features.h
@@ -36,6 +36,9 @@ COMPONENT_EXPORT(EVENTS_OZONE)
 extern const base::Feature kEnablePalmOnToolTypePalm;
 
 COMPONENT_EXPORT(EVENTS_OZONE)
+extern const base::FeatureParam<std::string> kNeuralPalmModelVersion;
+
+COMPONENT_EXPORT(EVENTS_OZONE)
 extern const base::FeatureParam<std::string> kNeuralPalmRadiusPolynomial;
 
 COMPONENT_EXPORT(EVENTS_OZONE)
@@ -66,8 +69,10 @@ COMPONENT_EXPORT(EVENTS_OZONE)
 extern const char kOzoneNNPalmTouchCompatibleProperty[];
 
 COMPONENT_EXPORT(EVENTS_OZONE)
-extern const char kOzoneNNPalmRadiusPolynomialProperty[];
+extern const char kOzoneNNPalmModelVersionProperty[];
 
+COMPONENT_EXPORT(EVENTS_OZONE)
+extern const char kOzoneNNPalmRadiusPolynomialProperty[];
 }  // namespace ui
 
 #endif  // UI_EVENTS_OZONE_FEATURES_H_
