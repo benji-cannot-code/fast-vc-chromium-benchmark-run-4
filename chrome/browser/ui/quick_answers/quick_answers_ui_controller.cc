7ec76649ae636dc05189ddd64e3ebccd4d58d0eb
Yue Li <updowndota@google.com>
2022-05-04 04:38:59 +0000

    Quick answers: Handle settings button click in Lacros
    Bug: b/231162812
    Test: tast run quickanswers.SettingsButton
    Change-Id: Ic67dfbe7a643d471f035f13e71d6495f9ca076c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625443
    Reviewed-by: Eric Sum <esum@google.com>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999231}

--- chrome/browser/ui/quick_answers/quick_answers_ui_controller.cc
+++ chrome/browser/ui/quick_answers/quick_answers_ui_controller.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"            // nogncheck
 #include "chrome/browser/ui/browser_navigator.h"         // nogncheck
 #include "chrome/browser/ui/browser_navigator_params.h"  // nogncheck
+#include "chromeos/crosapi/mojom/url_handler.mojom.h"
+#include "chromeos/lacros/lacros_service.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 namespace {
@@ -168,7 +170,18 @@ void QuickAnswersUiController::OnSettingsButtonPressed() {
   // Route dismissal through |controller_| for logging impressions.
   controller_->DismissQuickAnswers(QuickAnswersExitPoint::kSettingsButtonClick);
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   OpenUrl(GURL(kQuickAnswersSettingsUrl));
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+  // OS settings app is implemented in Ash, but OpenUrl here does not qualify
+  // for redirection in Lacros due to security limitations. Thus we need to
+  // explicitly send the request to Ash to launch the OS settings app.
+  chromeos::LacrosService* service = chromeos::LacrosService::Get();
+  DCHECK(service->IsAvailable<crosapi::mojom::UrlHandler>());
+
+  service->GetRemote<crosapi::mojom::UrlHandler>()->OpenUrl(
+      GURL(kQuickAnswersSettingsUrl));
+#endif
 }
 
 void QuickAnswersUiController::OnReportQueryButtonPressed() {
