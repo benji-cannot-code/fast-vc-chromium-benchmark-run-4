7bd30c19977d96b4fbdc80dcd5e28df8e3376055
Jonathan Freed <freedjm@chromium.org>
2022-05-04 22:57:58 +0000

    Cleaning up after landing feed mojo change.
    This also fixes the unresolved build dependency due to experimental
    typescript/mojo bindings.
    
    Bug: 1321932
    Change-Id: I8482708c7d9a88fb4beb237c8113e17b3dab8905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626370
    Commit-Queue: Jonathan Freed <freedjm@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999629}

--- chrome/browser/ui/webui/feed/feed_ui.cc
+++ chrome/browser/ui/webui/feed/feed_ui.cc
@@ -74,8 +74,6 @@ FeedUI::FeedUI(content::WebUI* web_ui) : ui::MojoBubbleWebUIController(web_ui) {
   // Configurable javascript for prototyping purposes.
   source->AddString("scriptUrl", kWebUiScriptFetchUrl.Get());
 
-  source->AddResourcePath("feed.mojom-webui.js", IDR_FEED_FEED_MOJOM_WEBUI_JS);
-
   // Register the URLDataSource
   auto* browser_context = web_ui->GetWebContents()->GetBrowserContext();
   content::WebUIDataSource::Add(browser_context, source);
