59c4cf498b1e449ca853856322997d7c45630951
Joey Arhar <jarhar@chromium.org>
2022-05-04 23:46:57 +0000

    Fix UpdateStyleAndLayout for DisplayLocking in ListBoxSelectType
    I wasn't able to reproduce the crash in a web_test, but I was able to
    reproduce it with the clusterfuzz repro tool and verified that this
    fixes the crash.
    
    Fixed: 1318401
    Change-Id: I178333c9b4aa313be5a384a59f1dec253d1ccd5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626089
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999651}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -1075,7 +1075,8 @@ void ListBoxSelectType::ScrollToOptionTask() {
   // OptionRemoved() makes sure option_to_scroll_to_ doesn't have an option
   // with another owner.
   DCHECK_EQ(option->OwnerSelectElement(), select_);
-  select_->GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kScroll);
+  select_->GetDocument().UpdateStyleAndLayoutForNode(
+      select_, DocumentUpdateReason::kScroll);
   if (!select_->GetLayoutObject())
     return;
   PhysicalRect bounds = option->BoundingBoxForScrollIntoView();
