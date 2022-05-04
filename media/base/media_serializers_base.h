6ea86e769d0fa7af4d8002d401b415ee67197567
Ted Meyer <tmathmeyer@chromium.org>
2022-05-04 20:38:31 +0000

    move utf-8 check to cover all strings
    Bug: 1321453
    Change-Id: I48f1df3ee680a7148e601c4c9da1c3cbcf8558ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625236
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999569}

--- media/base/media_serializers_base.h
+++ media/base/media_serializers_base.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/strings/string_util.h"
 #include "base/values.h"
 #include "media/base/media_export.h"
 
@@ -21,6 +22,17 @@ namespace internal {
 template <typename T>
 struct MediaSerializer {
   static inline base::Value Serialize(T value) { return base::Value(value); }
+};
+
+// a special serializer for strings, because base::Value checks
+// IsStringUTF8AllowingNoncharacters.
+template <>
+struct MediaSerializer<std::string> {
+  static inline base::Value Serialize(const std::string& string) {
+    if (base::IsStringUTF8AllowingNoncharacters(string))
+      return base::Value(string);
+    return base::Value("");
+  }
 };
 
 }  // namespace internal
