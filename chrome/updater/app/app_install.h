0a2da3a273c9643687b1acd36db4e191357e6a21
Xiaoling Bao <xiaolingbao@chromium.org>
2022-05-04 03:04:06 +0000

    Handle offline install command line.
    Bug: 1286574
    Change-Id: Ia17204c217eb3fd527aca2b4ce10722805284308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611184
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999220}

--- chrome/updater/app/app_install.h
+++ chrome/updater/app/app_install.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/splash_screen.h"
 
 namespace base {
+class FilePath;
 class Version;
 }
 
@@ -35,6 +36,11 @@ class AppInstallController
   virtual void InstallApp(const std::string& app_id,
                           const std::string& app_name,
                           base::OnceCallback<void(int)> callback) = 0;
+
+  virtual void InstallAppOffline(const std::string& app_id,
+                                 const std::string& app_name,
+                                 const base::FilePath& offline_dir,
+                                 base::OnceCallback<void(int)> callback) = 0;
 
  protected:
   virtual ~AppInstallController() = default;
