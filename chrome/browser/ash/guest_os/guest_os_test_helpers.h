953a79ad726541855b25444f59dd6937a17dedb2
David Munro <davidmunro@google.com>
2022-05-04 05:20:26 +0000

    crostini: Combine two mocks which do the same thing into one
    Bug: 1293229
    Test: CQ
    Change-Id: If4e351c376eb7960beeee0050dccdbb51819a49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626451
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#999241}

---
+++ chrome/browser/ash/guest_os/guest_os_test_helpers.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_GUEST_OS_GUEST_OS_TEST_HELPERS_H_
+#define CHROME_BROWSER_ASH_GUEST_OS_GUEST_OS_TEST_HELPERS_H_
+
+#include "chrome/browser/ash/crostini/crostini_util.h"
+#include "chrome/browser/ash/guest_os/public/guest_os_mount_provider.h"
+
+namespace guest_os {
+class MockMountProvider : public GuestOsMountProvider {
+ public:
+  MockMountProvider();
+  MockMountProvider(Profile* profile, crostini::ContainerId container_id);
+
+  std::string DisplayName() override;
+  Profile* profile() override;
+  crostini::ContainerId ContainerId() override;
+
+  Profile* profile_;
+  crostini::ContainerId container_id_;
+};
+}  // namespace guest_os
+
+#endif  // CHROME_BROWSER_ASH_GUEST_OS_GUEST_OS_TEST_HELPERS_H_
--- chrome/browser/ash/guest_os/public/guest_os_mount_provider_registry_unittest.cc
+++ chrome/browser/ash/guest_os/public/guest_os_mount_provider_registry_unittest.cc
@@ -8,20 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
+#include "chrome/browser/ash/guest_os/guest_os_test_helpers.h"
 #include "chrome/browser/ash/guest_os/public/guest_os_mount_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace guest_os {
 
 using Id = GuestOsMountProviderRegistry::Id;
-
-class MockProvider : public GuestOsMountProvider {
-  std::string DisplayName() override { return "Ptery"; }
-  Profile* profile() override { return nullptr; }
-  crostini::ContainerId ContainerId() override {
-    return crostini::ContainerId::GetDefault();
-  }
-};
 
 class MockObserver : public GuestOsMountProviderRegistry::Observer {
   using Id = GuestOsMountProviderRegistry::Id;
@@ -43,9 +36,9 @@ class GuestOsMountProviderRegistryTest : public testing::Test {};
 
 // Test that we can register, list and get providers
 TEST_F(GuestOsMountProviderRegistryTest, TestGetting) {
-  auto provider1 = std::make_unique<MockProvider>();
+  auto provider1 = std::make_unique<MockMountProvider>();
   auto* p1 = provider1.get();
-  auto provider2 = std::make_unique<MockProvider>();
+  auto provider2 = std::make_unique<MockMountProvider>();
   auto* p2 = provider2.get();
   GuestOsMountProviderRegistry registry;
   auto id1 = registry.Register(std::move(provider1));
@@ -66,8 +59,8 @@ TEST_F(GuestOsMountProviderRegistryTest, TestGetNullPtrIfMissing) {
 
 // Test register/unregister and observing/unobserving
 TEST_F(GuestOsMountProviderRegistryTest, TestObservation) {
-  auto provider1 = std::make_unique<MockProvider>();
-  auto provider2 = std::make_unique<MockProvider>();
+  auto provider1 = std::make_unique<MockMountProvider>();
+  auto provider2 = std::make_unique<MockMountProvider>();
   GuestOsMountProviderRegistry registry;
   MockObserver obs;
   registry.AddObserver(&obs);
