b45d281d140cc01d921de08ea68e8d664deb0578
Marcello Salomao <msalomao@google.com>
2022-05-04 01:38:33 +0000

    cros-disks-client: Make mount error mapping less strict
    Removing the DCHECK from the end of
    CrosDisksMountErrorToChromeMountError will let us develop
    cros-disks without worrying about chromium necessarily being in
    a compatible version.
    
    TEST=CQ
    BUG=chromium:1321944
    
    Change-Id: I2635be2c65ff84ad6a24bf88e6fd014d09ac98a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622894
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999186}

--- chromeos/dbus/cros_disks/cros_disks_client.cc
+++ chromeos/dbus/cros_disks/cros_disks_client.cc
@@ -123,7 +123,7 @@ MountError CrosDisksMountErrorToChromeMountError(
     case cros_disks::MOUNT_ERROR_NEED_PASSWORD:
       return MOUNT_ERROR_NEED_PASSWORD;
     default:
-      NOTREACHED() << "Unrecognised mount error code " << mount_error;
+      LOG(ERROR) << "Unrecognised mount error code " << mount_error;
       return MOUNT_ERROR_UNKNOWN;
   }
 }
