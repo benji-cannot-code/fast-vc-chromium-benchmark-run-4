f7a166fc6c5bccb0f00397c4feb3327a0b63ba39
Ben Reich <benreich@chromium.org>
2022-05-04 00:22:40 +0000

    [FilesTrash] Extract IO thread move operation as common utility
    The trashing operation will rely on the Move operation to move files
    from their location to the associated .Trash folder somewhere on the
    parent path. Extract this to a common location so Trash can use this
    logic as well.
    
    Bug: b:231250202
    Test: unit_tests --gtest_filter=*CopyOrMoveIOTaskTest*
    Change-Id: Id1c5a125c0111385919d8802139d7fbdc26131f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622474
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999161}

--- chrome/browser/ash/file_manager/copy_or_move_io_task.cc
+++ chrome/browser/ash/file_manager/copy_or_move_io_task.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/ash/file_manager/filesystem_api_util.h"
 #include "chrome/browser/ash/file_manager/io_task.h"
+#include "chrome/browser/ash/file_manager/io_task_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -67,23 +68,6 @@ storage::FileSystemOperationRunner::OperationID StartCopyOnIOThread(
   // TODO(crbug.com/1312336): Replace FileManagerCopyOrMoveHookDelegate with new
   // class.
   return file_system_context->operation_runner()->Copy(
-      source_url, destination_url, options,
-      storage::FileSystemOperation::ERROR_BEHAVIOR_ABORT,
-      std::make_unique<FileManagerCopyOrMoveHookDelegate>(progress_callback),
-      std::move(complete_callback));
-}
-
-// Starts the move operation via FileSystemOperationRunner.
-storage::FileSystemOperationRunner::OperationID StartMoveOnIOThread(
-    scoped_refptr<storage::FileSystemContext> file_system_context,
-    const storage::FileSystemURL& source_url,
-    const storage::FileSystemURL& destination_url,
-    storage::FileSystemOperation::CopyOrMoveOptionSet options,
-    const FileManagerCopyOrMoveHookDelegate::ProgressCallback&
-        progress_callback,
-    storage::FileSystemOperation::StatusCallback complete_callback) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-  return file_system_context->operation_runner()->Move(
       source_url, destination_url, options,
       storage::FileSystemOperation::ERROR_BEHAVIOR_ABORT,
       std::make_unique<FileManagerCopyOrMoveHookDelegate>(progress_callback),
