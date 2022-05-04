ad0e6de575674a6187d3d616bb7c0cffe06f861d
Eric Willigers <ericwilligers@chromium.org>
2022-05-04 09:08:29 +0000

    BrowserAppShelfController: Add CHECK
    We add CHECKs to ensure the observed browser app instance registry, and
    shelf model, are not null.
    
    Bug: 1296949
    Change-Id: I480766dbac78d38765c06142be7869d959fdcb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627131
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999294}

--- chrome/browser/ui/ash/shelf/browser_app_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/browser_app_shelf_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.h"
 #include "chrome/browser/ui/ash/shelf/shelf_controller_helper.h"
 #include "chrome/browser/ui/ash/shelf/shelf_spinner_controller.h"
+#include "chrome/browser/web_applications/web_app_utils.h"
 #include "components/app_constants/constants.h"
 #include "ui/aura/window.h"
 
@@ -59,6 +60,7 @@ BrowserAppShelfController::BrowserAppShelfController(
       browser_app_instance_registry_(
           *apps::AppServiceProxyFactory::GetForProfile(profile)
                ->BrowserAppInstanceRegistry()) {
+  CHECK(web_app::IsWebAppsCrosapiEnabled());
   registry_observation_.Observe(&browser_app_instance_registry_);
   shelf_model_observation_.Observe(&model);
 }
