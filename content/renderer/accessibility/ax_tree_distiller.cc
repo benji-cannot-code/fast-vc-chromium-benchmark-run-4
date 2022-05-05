b0bab0ba4d2be9f4fd048f692e94e29fab219199
Abigail Klein <abigailbklein@google.com>
2022-05-05 18:45:54 +0000

    [Read Anything] Integrate Screen2x call and callback into AXTreeDistiller.
    Screen2x distills web pages and articles for using an on-device ML
    model. The AXTreeDistiller lives in a renderer process and Screen2x runs
    in the utility process. This means that AXTreeDistiller::Distill() is
    no longer synchronous.
    
    To address this, when the render frame calls AXTreeDistiller::Distill()
    it passes the callback to AXTreeDistiller which stores it. At the
    conclusion of distillation, the callback is run, passing the results of
    distillation (snapshot and content_node_ids) back across the frame.
    
    Bug: 1266555
    Change-Id: I066d2f82a99a371896958577ee34dc2aa399f064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611104
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000009}

--- content/renderer/accessibility/ax_tree_distiller.cc
+++ content/renderer/accessibility/ax_tree_distiller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <queue>
+#include <utility>
 #include <vector>
 
 #include "base/containers/contains.h"
@@ -117,15 +118,10 @@ AXTreeDistiller::AXTreeDistiller(RenderFrameImpl* render_frame)
 
 AXTreeDistiller::~AXTreeDistiller() = default;
 
-void AXTreeDistiller::Distill() {
+void AXTreeDistiller::Distill(SnapshotAndDistillAXTreeCallback callback) {
+  callback_ = std::move(callback);
   SnapshotAXTree();
   DistillAXTree();
-
-  // TODO(https://crbug.com/1278249): Move the call to a proper place.
-#if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
-  if (features::IsReadAnythingWithScreen2xEnabled())
-    ScheduleScreen2xRun();
-#endif
 }
 
 void AXTreeDistiller::SnapshotAXTree() {
@@ -146,10 +142,21 @@ void AXTreeDistiller::SnapshotAXTree() {
 void AXTreeDistiller::DistillAXTree() {
   // If content_node_ids_ is already cached, do nothing.
   if (content_node_ids_)
-    return;
+    OnAXTreeDistilled();
   content_node_ids_ = std::make_unique<std::vector<ui::AXNodeID>>();
 
   DCHECK(snapshot_);
+
+  // If Read Anything with Screen 2x is enabled, kick off Screen 2x run, which
+  // distills the AXTree in the utility process using ML.
+#if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
+  if (features::IsReadAnythingWithScreen2xEnabled()) {
+    ScheduleScreen2xRun();
+    return;
+  }
+#endif
+
+  // Otherwise, distill the AXTree in process using the rules-based algorithm.
   ui::AXTree tree;
   bool success = tree.Unserialize(*snapshot_);
   if (!success)
@@ -164,6 +171,11 @@ void AXTreeDistiller::DistillAXTree() {
   }
 
   AddContentNodesToVector(article_node, content_node_ids_.get());
+  OnAXTreeDistilled();
+}
+
+void AXTreeDistiller::OnAXTreeDistilled() {
+  std::move(callback_).Run(*snapshot_.get(), *content_node_ids_.get());
 }
 
 #if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
@@ -176,9 +188,10 @@ void AXTreeDistiller::ScheduleScreen2xRun() {
 
 void AXTreeDistiller::ProcessScreen2xResult(
     const std::vector<ui::AXNodeID>& content_node_ids) {
-  // TODO(https://crbug.com/1278249): Use |content_node_ids|.
+  *content_node_ids_ = content_node_ids;
+  // TODO: Set |is_distillable_|.
+  OnAXTreeDistilled();
 }
-
 #endif
 
 }  // namespace content
