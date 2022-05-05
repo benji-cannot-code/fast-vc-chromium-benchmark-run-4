73d0ad2bef844eaa881ff70de99bd5d1989969e1
Renato Silva <rrsilva@google.com>
2022-05-05 19:03:59 +0000

    OOBE - Modulize hd-iron-icon
    Remove legacy Polymer1 syntax from hd-iron-icon.
    
    Bug: 1184731, 1319543
    Change-Id: Ibddbea0fb7eb2fb9f2ebc0cdbde970f18350d2d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629554
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1000022}

--- chrome/browser/resources/chromeos/login/components/hd_iron_icon.js
+++ chrome/browser/resources/chromeos/login/components/hd_iron_icon.js
@@ -3,13 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Polymer({
-  is: 'hd-iron-icon',
+/* #js_imports_placeholder */
 
-  properties: {
-    icon1x: String,
-    icon2x: String,
-    src1x: String,
-    src2x: String,
-  },
-});
+/**
+ * @polymer
+ */
+class HdIronIcon extends Polymer.Element {
+  static get is() {
+    return 'hd-iron-icon';
+  }
+
+  /* #html_template_placeholder */
+
+  static get properties() {
+    return {
+      icon1x: String,
+      icon2x: String,
+      src1x: String,
+      src2x: String,
+    };
+  }
+}
+
+customElements.define(HdIronIcon.is, HdIronIcon);
