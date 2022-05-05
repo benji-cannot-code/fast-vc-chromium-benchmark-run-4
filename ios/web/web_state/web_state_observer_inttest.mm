507e8281b14520556dc8b9281636c0c645fb8fdd
Gauthier Ambard <gambard@chromium.org>
2022-05-05 14:00:23 +0000

    [iOS] Cancel error page refactoring feature
    This CL reverts the work done to refator the error pages to use
    the new iOS 15 loadSimulatedRequest.
    It keeps the API for offline pages.
    
    Bug: 1322468
    Change-Id: If2cb12bb6cb8aa33aafefee6f5e27919fbaca296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627557
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999867}

--- ios/web/web_state/web_state_observer_inttest.mm
+++ ios/web/web_state/web_state_observer_inttest.mm
@@ -1084,10 +1084,6 @@ TEST_F(WebStateObserverTest, FailedNavigation) {
       ui::PageTransition::PAGE_TRANSITION_TYPED,
       /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
       /*has_user_gesture=*/false);
-  const WebStatePolicyDecider::RequestInfo request_info_redirect(
-      ui::PageTransition::PAGE_TRANSITION_CLIENT_REDIRECT,
-      /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
-      /*has_user_gesture=*/false);
   EXPECT_CALL(*decider_, MockShouldAllowRequest(
                              _, RequestInfoMatch(expected_request_info), _))
       .WillOnce(
@@ -1100,12 +1096,6 @@ TEST_F(WebStateObserverTest, FailedNavigation) {
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   // Load placeholder by [WKWebView loadRequest].
   EXPECT_CALL(observer_, DidStartLoading(web_state()));
-  if (features::IsLoadSimulatedRequestAPIEnabled()) {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(request_info_redirect), _))
-        .WillOnce(
-            RunOnceCallback<2>(WebStatePolicyDecider::PolicyDecision::Allow()));
-  }
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
 
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _))
@@ -1229,10 +1219,6 @@ TEST_F(WebStateObserverTest, WebViewUnsupportedSchemeNavigation) {
       ui::PageTransition::PAGE_TRANSITION_TYPED,
       /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
       /*has_user_gesture=*/false);
-  const WebStatePolicyDecider::RequestInfo request_info_redirect(
-      ui::PageTransition::PAGE_TRANSITION_CLIENT_REDIRECT,
-      /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
-      /*has_user_gesture=*/false);
   EXPECT_CALL(*decider_, MockShouldAllowRequest(
                              _, RequestInfoMatch(expected_request_info), _))
       .WillOnce(
@@ -1245,12 +1231,6 @@ TEST_F(WebStateObserverTest, WebViewUnsupportedSchemeNavigation) {
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   // Load placeholder by [WKWebView loadRequest].
   EXPECT_CALL(observer_, DidStartLoading(web_state()));
-  if (features::IsLoadSimulatedRequestAPIEnabled()) {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(request_info_redirect), _))
-        .WillOnce(
-            RunOnceCallback<2>(WebStatePolicyDecider::PolicyDecision::Allow()));
-  }
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
 
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _))
@@ -1283,10 +1263,6 @@ TEST_F(WebStateObserverTest, WebViewUnsupportedUrlNavigation) {
       ui::PageTransition::PAGE_TRANSITION_TYPED,
       /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
       /*has_user_gesture=*/false);
-  const WebStatePolicyDecider::RequestInfo request_info_redirect(
-      ui::PageTransition::PAGE_TRANSITION_CLIENT_REDIRECT,
-      /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
-      /*has_user_gesture=*/false);
   EXPECT_CALL(*decider_, MockShouldAllowRequest(
                              _, RequestInfoMatch(expected_request_info), _))
       .WillOnce(
@@ -1299,12 +1275,6 @@ TEST_F(WebStateObserverTest, WebViewUnsupportedUrlNavigation) {
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   // Load placeholder by [WKWebView loadRequest].
   EXPECT_CALL(observer_, DidStartLoading(web_state()));
-  if (features::IsLoadSimulatedRequestAPIEnabled()) {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(request_info_redirect), _))
-        .WillOnce(
-            RunOnceCallback<2>(WebStatePolicyDecider::PolicyDecision::Allow()));
-  }
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
 
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _))
@@ -2194,10 +2164,6 @@ TEST_F(WebStateObserverTest, FailedSslConnection) {
       ui::PageTransition::PAGE_TRANSITION_TYPED,
       /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
       /*has_user_gesture=*/false);
-  const WebStatePolicyDecider::RequestInfo request_info_redirect(
-      ui::PageTransition::PAGE_TRANSITION_CLIENT_REDIRECT,
-      /*target_main_frame=*/true, /*target_frame_is_cross_origin=*/false,
-      /*has_user_gesture=*/false);
   EXPECT_CALL(*decider_, MockShouldAllowRequest(
                              _, RequestInfoMatch(request_info_explicit), _))
       .WillOnce(
@@ -2209,12 +2175,6 @@ TEST_F(WebStateObserverTest, FailedSslConnection) {
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   // First, a placeholder navigation starts and finishes.
   EXPECT_CALL(observer_, DidStartLoading(web_state()));
-  if (features::IsLoadSimulatedRequestAPIEnabled()) {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(request_info_redirect), _))
-        .WillOnce(
-            RunOnceCallback<2>(WebStatePolicyDecider::PolicyDecision::Allow()));
-  }
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _));
   EXPECT_CALL(observer_,
@@ -2260,21 +2220,12 @@ TEST_F(WebStateObserverTest, DisallowRequestAndShowError) {
   NSError* error = [NSError errorWithDomain:net::kNSErrorDomain
                                        code:net::ERR_BLOCKED_BY_ADMINISTRATOR
                                    userInfo:nil];
-  if (features::IsLoadSimulatedRequestAPIEnabled()) {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(expected_request_info), _))
-        .WillRepeatedly(RunOnceCallback<2>(
-            WebStatePolicyDecider::PolicyDecision::CancelAndDisplayError(
-                error)));
-  } else {
-    EXPECT_CALL(*decider_, MockShouldAllowRequest(
-                               _, RequestInfoMatch(expected_request_info), _))
-        .WillOnce(RunOnceCallback<2>(
-            WebStatePolicyDecider::PolicyDecision::CancelAndDisplayError(
-                error)));
+  EXPECT_CALL(*decider_, MockShouldAllowRequest(
+                             _, RequestInfoMatch(expected_request_info), _))
+      .WillOnce(RunOnceCallback<2>(
+          WebStatePolicyDecider::PolicyDecision::CancelAndDisplayError(error)));
 
-    EXPECT_CALL(observer_, DidStartNavigation(web_state(), _));
-  }
+  EXPECT_CALL(observer_, DidStartNavigation(web_state(), _));
   EXPECT_CALL(observer_, DidStopLoading(web_state()));
   EXPECT_CALL(observer_, DidFinishNavigation(web_state(), _));
   EXPECT_CALL(observer_,
