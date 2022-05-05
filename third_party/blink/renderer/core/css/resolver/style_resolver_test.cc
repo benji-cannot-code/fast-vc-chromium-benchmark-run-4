87d9e74dcb763ee7df9173604334483f01747e02
Rune Lillesveen <futhark@chromium.org>
2022-05-05 13:22:34 +0000

    Remove CSSTargetTextPseudoElement runtime flag
    Feature has been stable since M89
    
    Also moved a feature comment that went astray.
    
    Bug: 1136817
    Change-Id: I5e01e938d3e0801e99ba93e34d648e28fcc010af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629349
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999853}

--- third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
@@ -546,8 +546,6 @@ TEST_F(StyleResolverTest, NoFetchForAtPage) {
 }
 
 TEST_F(StyleResolverTest, NoFetchForHighlightPseudoElements) {
-  ScopedCSSTargetTextPseudoElementForTest scoped_feature(true);
-
   GetDocument().body()->setInnerHTML(R"HTML(
     <style>
       body::target-text, body::selection {
