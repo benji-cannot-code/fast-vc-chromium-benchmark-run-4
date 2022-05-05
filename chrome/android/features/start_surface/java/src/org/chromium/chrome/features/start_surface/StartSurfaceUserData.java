14385e5438bf8d66acfea384b7b1c9e37833e578
spdonghao <spdonghao@chromium.org>
2022-05-05 21:16:49 +0000

    [Start] Remove internal/ and public/ layer in start_surface/
    This CL is a follow-up change of http://crrev/c/3606697.
    
    This CL moves files in internal/ and public/ to start_surface/. The
    files that depend on chrome_java are kept in
    start_surface_java_sources(chrome_java) and the others are in
    start_surface:public_java.
    See https://docs.google.com/document/d/1SJb3-w0tWV5_CHtMB5uTG8oJdnKbLhM0rS0ZVgPIbYs/edit?resourcekey=0-AS3a0DIb0n120OhCph9C7w#bookmark=id.qisodaxycu0c for more details.
    
    Besides, upload two translation screenshots which should have been
    uploaded in the initial CL (http://crrev/c/1699160).
    
    Bug: 1322702
    Change-Id: I13f22ea1eb717b1f65e52b4f167113812d75ba61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625216
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000086}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceUserData.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceUserData.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.features.start_surface;
 
 import androidx.annotation.Nullable;
+
 import org.chromium.base.UserData;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
