8009e26bfaebc7327915a6b53d23cfba9ed9935d
Wes Okuhara <wesokuhara@google.com>
2022-05-05 00:44:16 +0000

    CrOS Settings: Convert files/smb shares pages to class-based syntax
    Converts Polymer elements to the class-based syntax to prepare for the
    upcoming TypeScript migration.
    
    Bug: chromium:1206112, chromium:1315757
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*FilesPage*'
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*SmbPage*'
    Test: Manual UI testing
    Change-Id: Ief1a501240195b0a7be4108162061d5f5e87231f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3628154
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#999677}

--- chrome/test/data/webui/settings/chromeos/smb_shares_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/smb_shares_page_tests.js
@@ -50,12 +50,12 @@ suite('AddSmbShareDialogTests', function() {
     page = document.createElement('settings-smb-shares-page');
     document.body.appendChild(page);
 
-    const button = page.$$('#addShare');
+    const button = page.shadowRoot.querySelector('#addShare');
     assertTrue(!!button);
     button.click();
     flush();
 
-    addDialog = page.$$('add-smb-share-dialog');
+    addDialog = page.shadowRoot.querySelector('add-smb-share-dialog');
     assertTrue(!!addDialog);
 
     flush();
@@ -163,9 +163,9 @@ suite('AddSmbShareDialogTests', function() {
   });
 
   test('ControlledByPolicy', function() {
-    const button = page.$$('#addShare');
+    const button = page.shadowRoot.querySelector('#addShare');
 
-    assertFalse(!!page.$$('cr-policy-pref-indicator'));
+    assertFalse(!!page.shadowRoot.querySelector('cr-policy-pref-indicator'));
     assertFalse(button.disabled);
 
     page.prefs = {
@@ -173,7 +173,7 @@ suite('AddSmbShareDialogTests', function() {
     };
     flush();
 
-    assertTrue(!!page.$$('cr-policy-pref-indicator'));
+    assertTrue(!!page.shadowRoot.querySelector('cr-policy-pref-indicator'));
     assertTrue(button.disabled);
   });
 
@@ -226,19 +226,19 @@ suite('AddSmbShareDialogTests', function() {
     };
     document.body.appendChild(page);
 
-    const button = page.$$('#addShare');
+    const button = page.shadowRoot.querySelector('#addShare');
     assertTrue(!!button);
     assertFalse(button.disabled);
     button.click();
 
     flush();
 
-    addDialog = page.$$('add-smb-share-dialog');
+    addDialog = page.shadowRoot.querySelector('add-smb-share-dialog');
     assertTrue(!!addDialog);
 
     flush();
 
-    const openDialogButton = page.$$('#addShare');
+    const openDialogButton = page.shadowRoot.querySelector('#addShare');
     openDialogButton.click();
 
     assertEquals(expectedSmbUrl, addDialog.mountUrl_);
