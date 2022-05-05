31a636bfbcf6424d2b3ca626ff9edb25bbbf2e33
Howard Yang <hcyang@google.com>
2022-05-05 03:32:36 +0000

    metrics: Add TPM type metrics
    Report TPM type in ChromeOSMetricsProvider where other ChromeOS specific
    metrics (like hardware class) is reported. This is done by using two TPM
    manager dbus calls: GetVersionInfo and GetSupported features, and using
    the three fields `support_runtime_selection`, `family`, and
    `gsc_version` to determine the TPM type.
    
    Bug: b:230069678
    Test: chromeos_metrics_provider_unittest, including added testcases.
    Change-Id: I306366df7ea7fe0f9fef334c02c312d976917dcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615962
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#999736}

--- chrome/browser/metrics/chromeos_metrics_provider.h
+++ chrome/browser/metrics/chromeos_metrics_provider.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/metrics/perf/profile_provider_chromeos.h"
+#include "chromeos/dbus/tpm_manager/tpm_manager.pb.h"
 #include "components/metrics/metrics_log_uploader.h"
 #include "components/metrics/metrics_provider.h"
 
@@ -21,7 +22,7 @@ struct ArcFeatures;
 namespace metrics {
 class CachedMetricsProfile;
 class ChromeUserMetricsExtension;
-}
+}  // namespace metrics
 
 enum class EnrollmentStatus;
 class PrefRegistrySimple;
@@ -52,6 +53,10 @@ class ChromeOSMetricsProvider : public metrics::MetricsProvider {
   // Retrieves ARC features using ArcFeaturesParser. When this task is complete,
   // |callback| is run.
   void InitTaskGetArcFeatures(base::OnceClosure callback);
+
+  // Retrieves TPM type using TpmManagerClient. When this task is complete,
+  // |callback| is run.
+  void InitTaskGetTpmType(base::OnceClosure callback);
 
   // metrics::MetricsProvider:
   void Init() override;
@@ -84,6 +89,20 @@ class ChromeOSMetricsProvider : public metrics::MetricsProvider {
   void OnArcFeaturesParsed(base::OnceClosure callback,
                            absl::optional<arc::ArcFeatures> features);
 
+  // Sets the TPM version info (tpm family and GSC version), then calls the
+  // callback.
+  void OnTpmManagerGetVersionInfo(
+      base::OnceClosure callback,
+      const tpm_manager::GetVersionInfoReply& reply);
+
+  // Sets the TPM supported features (runtime selection), then calls the
+  // callback.
+  void OnTpmManagerGetSupportedFeatures(
+      base::OnceClosure callback,
+      const tpm_manager::GetSupportedFeaturesReply& reply);
+
+  void SetTpmType(metrics::SystemProfileProto* system_profile_proto);
+
   // Called from the ProvideCurrentSessionData(...) to record UserType.
   void UpdateUserTypeUMA();
 
@@ -111,6 +130,12 @@ class ChromeOSMetricsProvider : public metrics::MetricsProvider {
 
   // ARC release version obtained from build properties.
   absl::optional<std::string> arc_release_ = absl::nullopt;
+
+  // The following three fields together determine the TPM
+  // (go/trusted-platform-module) type.
+  absl::optional<uint32_t> tpm_family_ = absl::nullopt;
+  absl::optional<tpm_manager::GscVersion> gsc_version_ = absl::nullopt;
+  absl::optional<bool> tpm_support_runtime_selection_ = absl::nullopt;
 
   base::WeakPtrFactory<ChromeOSMetricsProvider> weak_ptr_factory_{this};
 };
