54ae7621cd23d6bf2ed7fcc4f83efe2cb440e456
Xinghui Lu <xinghuilu@chromium.org>
2022-05-05 15:36:10 +0000

    Show downloads from the main profile in Incognito.
    Currently, when a new incognito profile is opened, the download bubble
    is empty. It should instead inherits old downloads from the main
    profile. This behavior is consistent with the chrome://downloads page.
    
    Bug: 1322557
    Change-Id: Ie609528e5bbe06c53c415eaa4f4af27c2a2b3bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626297
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999901}

--- chrome/browser/download/bubble/download_bubble_controller.cc
+++ chrome/browser/download/bubble/download_bubble_controller.cc
@@ -83,6 +83,11 @@ DownloadBubbleUIController::DownloadBubbleUIController(Browser* browser)
           profile_->GetProfileKey())),
       offline_manager_(
           OfflineItemModelManagerFactory::GetForBrowserContext(profile_)) {
+  if (profile_->IsOffTheRecord()) {
+    Profile* original_profile = profile_->GetOriginalProfile();
+    original_notifier_ = std::make_unique<download::AllDownloadItemNotifier>(
+        original_profile->GetDownloadManager(), this);
+  }
   observation_.Observe(aggregator_.get());
 }
 
@@ -219,6 +224,13 @@ void DownloadBubbleUIController::OnDownloadUpdated(
     download::DownloadItem* item) {
   auto model = DownloadItemModel::Wrap(
       item, std::make_unique<DownloadUIModel::BubbleStatusTextBuilder>());
+  // manager can be different from download_notifier_ when the current profile
+  // is off the record.
+  if (manager != download_notifier_.GetManager()) {
+    display_controller_->OnUpdatedItem(model->IsDone(),
+                                       /*show_details_if_done=*/false);
+    return;
+  }
   bool show_details_if_done =
       model->ShouldShowInBubble() &&
       (browser_ == chrome::FindLastActiveWithProfile(profile_.get()));
@@ -263,6 +275,9 @@ DownloadBubbleUIController::GetAllItemsToDisplay() {
   }
   std::vector<download::DownloadItem*> download_items;
   download_manager_->GetAllDownloads(&download_items);
+  if (original_notifier_) {
+    original_notifier_->GetManager()->GetAllDownloads(&download_items);
+  }
   for (download::DownloadItem* item : download_items) {
     DownloadUIModelPtr model = DownloadItemModel::Wrap(
         item, std::make_unique<DownloadUIModel::BubbleStatusTextBuilder>());
