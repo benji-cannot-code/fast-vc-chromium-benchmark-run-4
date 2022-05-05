2b2625bf563104af3d340fd1594a29f726ca7e11
Petro Akzhygitov <pakzhygitov@google.com>
2022-05-05 10:54:17 +0000

    Move UpgradeCenter updates into a browser agent [Part 1/4]
    – Added a feature flag for upgrade center updates refactor
    
    Bug: 1272546
    Change-Id: Ib88376263ef2a65e856435bb4e510f44466f7401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627505
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#999826}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
@@ -593,6 +593,12 @@ const char kUpdateHistoryEntryPointsInIncognitoDescription[] =
     "When enabled, the entry points to history UI from Incognito mode will be "
     "removed.";
 
+const char kUpgradeCenterRefactorName[] =
+    "Enable Upgrade Center refactored code";
+const char kUpgradeCenterRefactorDescription[] =
+    "When enabled, upgrade сenter logic is provided by a Browser Agent "
+    "not BVC.";
+
 const char kUseLensToSearchForImageName[] =
     "Use Google Lens to Search for images";
 const char kUseLensToSearchForImageDescription[] =
