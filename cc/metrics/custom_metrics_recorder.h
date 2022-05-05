8169468377b39191729803a35a9024b4316f7e3e
Xiyuan Xia <xiyuan@google.com>
2022-05-05 17:26:08 +0000

    Split ash dropped frame UMA into 3
    Split ash dropped frame UMA into 3 based on session state. So that
    dropped frames at different login state could be looked at separately
    and it is easier to skip known problems around post login.
    
    Bug: 1292168
    Change-Id: I882423b30e98faaf6e147bd85dcd1fbfa96a4b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622255
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Behdad Bakhshinategh <behdadb@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999951}

---
+++ cc/metrics/custom_metrics_recorder.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CC_METRICS_CUSTOM_METRICS_RECORDER_H_
+#define CC_METRICS_CUSTOM_METRICS_RECORDER_H_
+
+#include "cc/cc_export.h"
+
+namespace cc {
+
+// Customize cc metric recording. E.g. reporting metrics under different names.
+class CC_EXPORT CustomMetricRecorder {
+ public:
+  static CustomMetricRecorder* Get();
+
+  // Invoked to report "PercentDroppedFrames_1sWindow".
+  virtual void ReportPercentDroppedFramesInOneSecoundWindow(double percent) = 0;
+
+ protected:
+  CustomMetricRecorder();
+  virtual ~CustomMetricRecorder();
+};
+
+}  // namespace cc
+
+#endif  // CC_METRICS_CUSTOM_METRICS_RECORDER_H_
--- cc/metrics/dropped_frame_counter.cc
+++ cc/metrics/dropped_frame_counter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/chromeos_buildflags.h"
 #include "cc/base/features.h"
+#include "cc/metrics/custom_metrics_recorder.h"
 #include "cc/metrics/frame_sorter.h"
 #include "cc/metrics/total_frame_counter.h"
 #include "cc/metrics/ukm_smoothness_data.h"
@@ -398,10 +399,13 @@ void DroppedFrameCounter::ReportFrames() {
 
 void DroppedFrameCounter::ReportFramesForUI() {
   DCHECK(report_for_ui_);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  UMA_HISTOGRAM_PERCENTAGE("Ash.Smoothness.PercentDroppedFrames_1sWindow",
-                           sliding_window_current_percent_dropped_);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+  auto* recorder = CustomMetricRecorder::Get();
+  if (!recorder)
+    return;
+
+  recorder->ReportPercentDroppedFramesInOneSecoundWindow(
+      sliding_window_current_percent_dropped_);
 }
 
 double DroppedFrameCounter::GetMostRecentAverageSmoothness() const {
