7f8db104dc9ba488db1fef31046cb6c61f38752e
Alex Ilin <alexilin@chromium.org>
2022-05-05 12:41:27 +0000

    Revert "Re-enable JS flow integration tests"
    This reverts commit 4681047f435d8fbd79e421c5c26d49c3d7e146c9.
    
    Reason for revert: still flaky on android-pie-x86-rel (https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-x86-rel/8837/overview)
    
    Original change's description:
    > Re-enable JS flow integration tests
    >
    > Should fix stopFlowFromJs and re-enable runInterruptDuringFlow to get flakiness stats (it only was flaky two times https://screenshot.googleplex.com/8XpR5d6tR8b9hR9).
    >
    > Also added some VLOG/DVLOG statements to make debugging JS flows easier.
    >
    > Bug: b/208420231
    > Change-Id: Ibad43bc0250782cce65ef19f30b01fed0d4f4b50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627531
    > Auto-Submit: Florian Gauger <fga@google.com>
    > Reviewed-by: Clemens Arbesser <arbesser@google.com>
    > Commit-Queue: Florian Gauger <fga@google.com>
    > Cr-Commit-Position: refs/heads/main@{#999418}
    
    Bug: b/208420231, 1307549
    Change-Id: Ibc205268e50f43014c8e0848015760117aab3dba
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629291
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#999845}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/JsFlowIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/JsFlowIntegrationTest.java
@@ -17,7 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.iterableWithSize;
 
-import static org.chromium.base.test.util.CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL;
+import static org.chromium.base.test.util.CriteriaHelper.DEFAULT_POLLING_INTERVAL;
 import static org.chromium.chrome.browser.autofill_assistant.AutofillAssistantUiTestUtil.checkElementExists;
 import static org.chromium.chrome.browser.autofill_assistant.AutofillAssistantUiTestUtil.startAutofillAssistant;
 import static org.chromium.chrome.browser.autofill_assistant.AutofillAssistantUiTestUtil.tapElement;
@@ -39,6 +39,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantTestService.ScriptsReturnMode;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipProto;
@@ -244,15 +245,14 @@ public class JsFlowIntegrationTest {
     private ActionProto toJsFlowAction(List<ActionProto> actions) {
         assert (!actions.isEmpty());
         StringBuilder jsFlow = new StringBuilder();
-        for (ActionProto action : actions) {
-            jsFlow.append("[clientStatus, value] = await runNativeAction(")
-                    .append(action.getActionInfoCase().getNumber())
+        for (int i = 0; i < actions.size(); i++) {
+            jsFlow.append("[client_status, value] = await runNativeAction(")
+                    .append(actions.get(i).getActionInfoCase().getNumber())
                     .append(", '")
-                    .append(Base64.encodeToString(getActionBytes(action), Base64.NO_WRAP))
-                    .append("');\n")
-                    .append("if (clientStatus != 2) return {status: clientStatus};\n");
+                    .append(Base64.encodeToString(getActionBytes(actions.get(i)), Base64.NO_WRAP))
+                    .append("');\nif (client_status != 2) { return {status:client_status}; }\n");
         }
-        jsFlow.append("return {status: 2};");
+        jsFlow.append("return {status:client_status};");
         return ActionProto.newBuilder()
                 .setJsFlow(JsFlowProto.newBuilder().setJsFlow(jsFlow.toString()))
                 .build();
@@ -279,8 +279,8 @@ public class JsFlowIntegrationTest {
         AutofillAssistantTestScript script = new AutofillAssistantTestScript(TEST_SCRIPT, list);
         runScript(script);
 
-        waitUntil(() -> !checkElementExists(mTestRule.getWebContents(), "touch_area_one"));
         waitUntilViewMatchesCondition(withText("After JS flow"), isCompletelyDisplayed());
+        waitUntil(() -> !checkElementExists(mTestRule.getWebContents(), "touch_area_one"));
     }
 
     @Test
@@ -318,6 +318,7 @@ public class JsFlowIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1307549")
     public void stopFlowFromJs() throws Exception {
         ArrayList<ActionProto> nestedActions = new ArrayList<>();
         nestedActions.add(ActionProto.newBuilder()
@@ -346,11 +347,12 @@ public class JsFlowIntegrationTest {
         waitUntilViewMatchesCondition(withText("Stop"), isCompletelyDisplayed());
         onView(withText("Stop")).perform(click());
         waitUntilViewAssertionTrue(
-                withId(R.id.autofill_assistant), doesNotExist(), DEFAULT_MAX_TIME_TO_POLL);
+                withId(R.id.autofill_assistant), doesNotExist(), DEFAULT_POLLING_INTERVAL);
     }
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1307549")
     public void runInterruptDuringFlow() throws Exception {
         ArrayList<AutofillAssistantTestScript> scripts = new ArrayList<>();
         ArrayList<ActionProto> nestedActions = new ArrayList<>();
--- components/autofill_assistant/browser/js_flow_util.cc
+++ components/autofill_assistant/browser/js_flow_util.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill_assistant/browser/js_flow_util.h"
 #include "base/base64.h"
-#include "base/logging.h"
 #include "base/strings/strcat.h"
 #include "components/autofill_assistant/browser/model.pb.h"
 #include "components/autofill_assistant/browser/service.pb.h"
@@ -40,11 +39,8 @@ bool IsAllowedRemoteType(runtime::RemoteObjectType type) {
     case runtime::RemoteObjectType::NUMBER:
     case runtime::RemoteObjectType::BOOLEAN:
       return true;
-    default: {
-      DVLOG(1) << "Flow return value type is not allowed: "
-               << static_cast<int>(type);
+    default:
       return false;
-    }
   }
 }
 
@@ -106,7 +102,6 @@ ClientStatus ExtractFlowReturnValue(
       devtools_reply_status, devtools_result, __FILE__, __LINE__,
       js_line_offset, num_stack_entries_to_drop);
   if (!status.ok()) {
-    DVLOG(1) << "The JS flow result did not contain a valid value.";
     return status;
   }
 
@@ -119,7 +114,6 @@ ClientStatus ExtractFlowReturnValue(
 
   if (!remote_object->HasValue() ||
       !IsAllowedRemoteType(remote_object->GetType())) {
-    DVLOG(1) << "The JS flow result did not have a value or had a bad type.";
     status.set_proto_status(INVALID_ACTION);
     status.mutable_details()
         ->mutable_unexpected_error_info()
@@ -133,8 +127,6 @@ ClientStatus ExtractFlowReturnValue(
 
   std::string error_message;
   if (!ContainsOnlyAllowedValues(*remote_object->GetValue(), error_message)) {
-    DVLOG(1) << "The JS flow result did contain disallowed values. "
-             << error_message;
     status.set_proto_status(INVALID_ACTION);
     status.mutable_details()
         ->mutable_unexpected_error_info()
@@ -155,7 +147,6 @@ ClientStatus ExtractJsFlowActionReturnValue(
   }
 
   if (!value.is_dict()) {
-    DVLOG(1) << "The JS flow result was not an object.";
     return ClientStatusWithSourceLocation(INVALID_ACTION, __FILE__, __LINE__);
   }
 
