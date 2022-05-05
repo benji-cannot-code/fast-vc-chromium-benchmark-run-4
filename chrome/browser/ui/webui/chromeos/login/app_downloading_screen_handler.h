97c92f486748227e9174116f68bd0d2254372539
Roman Sorokin <rsorokin@chromium.org>
2022-05-05 03:53:29 +0000

    [Oobe WebUI] Update AppDownloadingScreen(Handler)
    As a part of removing obsolete and redundant things
    
    Bug: 1312864, 1312869, 1312879, 1309022, 1298392
    Change-Id: Ibc921bf4dd140e0bf03b2c10402be4ceb023dd6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625717
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999740}

--- chrome/browser/ui/webui/chromeos/login/app_downloading_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/app_downloading_screen_handler.h
@@ -6,28 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_APP_DOWNLOADING_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_APP_DOWNLOADING_SCREEN_HANDLER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
-
-namespace ash {
-class AppDownloadingScreen;
-}
 
 namespace chromeos {
 
-class AppDownloadingScreenView {
+class AppDownloadingScreenView
+    : public base::SupportsWeakPtr<AppDownloadingScreenView> {
  public:
-  constexpr static StaticOobeScreenId kScreenId{"app-downloading"};
+  inline constexpr static StaticOobeScreenId kScreenId{"app-downloading",
+                                                       "AppDownloadingScreen"};
 
   virtual ~AppDownloadingScreenView() = default;
 
-  // Sets screen this view belongs to.
-  virtual void Bind(ash::AppDownloadingScreen* screen) = 0;
-
   // Shows the contents of the screen.
   virtual void Show() = 0;
-
-  // Hides the contents of the screen.
-  virtual void Hide() = 0;
 };
 
 // The sole implementation of the AppDownloadingScreenView, using WebUI.
@@ -47,18 +40,9 @@ class AppDownloadingScreenHandler : public BaseScreenHandler,
   // BaseScreenHandler:
   void DeclareLocalizedValues(
       ::login::LocalizedValuesBuilder* builder) override;
-  void RegisterMessages() override;
 
   // AppDownloadingScreenView:
-  void Bind(ash::AppDownloadingScreen* screen) override;
-  void Show() override;
-  void Hide() override;
-
- private:
-  // BaseScreenHandler:
-  void InitializeDeprecated() override;
-
-  ash::AppDownloadingScreen* screen_ = nullptr;
+  void Show() final;
 };
 
 }  // namespace chromeos
