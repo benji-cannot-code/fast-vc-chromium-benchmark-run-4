9f74fd3d43d5dc5745e507e38cc0ea113cb93c89
Donn Denman <donnd@chromium.org>
2022-05-05 19:50:36 +0000

    [TTS][Tests] Shorten the timeout used for tests
    The timeout seems way too long, so this shortens it, helping with
    test debugging. Also log a stack trace when the panel interaction
    retry fails.
    
    BUG=
    
    Change-Id: I4ee9f44b0eb5d860aebf0558b9b3038682605d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626098
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000047}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInstrumentationBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInstrumentationBase.java
@@ -305,7 +305,7 @@ public class ContextualSearchInstrumentationBase {
     protected static final String RELATED_SEARCHES_NODE = "intelligence";
 
     private static final String TAG = "CSIBase";
-    private static final int TEST_TIMEOUT = 15000;
+    private static final int TEST_TIMEOUT = 1500;
     private static final int TEST_EXPECTED_FAILURE_TIMEOUT = 1000;
 
     private static final int PANEL_INTERACTION_MAX_RETRIES = 3;
@@ -623,6 +623,8 @@ public class ContextualSearchInstrumentationBase {
                 success = true;
             } catch (AssertionError | TimeoutException e) {
                 if (tries > PANEL_INTERACTION_MAX_RETRIES) {
+                    Log.e(TAG, "ctxs Failed interactions and giving up.", e);
+                    Thread.dumpStack();
                     throw e;
                 } else {
                     Log.e(TAG, "Failed to peek panel bar, trying again.", e);
