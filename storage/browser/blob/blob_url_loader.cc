ee707064efb16550140fa6f9394fe92ce426ce15
Christine Smith <christinesm@chromium.org>
2022-05-05 18:01:44 +0000

    [Storage Code Health] Remove expired histogram: Storage.Blob.BlobUrlLoader.FailureType
    Removes expired histogram: Storage.Blob.BlobUrlLoader.FailureType
    
    Bug: 1244908
    Change-Id: I66e0b8d692214c458ccd2416cce5c18fa387aa0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625342
    Auto-Submit: Christine Smith <christinesm@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999977}

--- storage/browser/blob/blob_url_loader.cc
+++ storage/browser/blob/blob_url_loader.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
@@ -207,9 +206,6 @@ void BlobURLLoader::DidReadSideData(absl::optional<mojo_base::BigBuffer> data) {
 
 void BlobURLLoader::OnComplete(net::Error error_code,
                                uint64_t total_written_bytes) {
-  base::UmaHistogramSparse("Storage.Blob.BlobUrlLoader.FailureType",
-                           error_code);
-
   network::URLLoaderCompletionStatus status(error_code);
   status.encoded_body_length = total_written_bytes;
   status.decoded_body_length = total_written_bytes;
