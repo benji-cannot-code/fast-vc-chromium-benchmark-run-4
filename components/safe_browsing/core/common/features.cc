34d4662b9120c3a35c1d69e779ee5a19353c2a5c
Rohit Bhatia <bhatiarohit@google.com>
2022-05-05 22:20:53 +0000

    Remove Cookie Theft Experiment
    Remove cookie theft experiment artifacts and strings from Downloads,
    including the new Download Bubble.
    
    Bug: 1202691
    Bug: 1282245
    Change-Id: I253e8cc0716bde20dde5248619a0bd2e372e629e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627127
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000126}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -109,9 +109,6 @@ const base::Feature kOmitNonUserGesturesFromReferrerChain{
 const base::Feature kSafeBrowsingCsbrrWithToken{
     "SafeBrowsingCsbrrWithToken", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kSafeBrowsingCTDownloadWarning{
-    "SafeBrowsingCTDownloadWarning", base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kSafeBrowsingEnterpriseCsd{
     "SafeBrowsingEnterpriseCsd", base::FEATURE_ENABLED_BY_DEFAULT};
 
