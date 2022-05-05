5e90c22219bf6117e707de044b6ffe97d53b19f0
Scott Violet <sky@chromium.org>
2022-05-05 21:24:59 +0000

    content: removes feature to change network service thread priority
    The data shows it to be ineffective, so removing.
    
    BUG=1289300
    TEST=none
    
    Change-Id: I789a10cf1f81587305b5ea1278f7af1ced231701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630707
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000092}

--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_switches_internal.h"
 #include "content/common/partition_alloc_support.h"
 #include "content/public/common/content_client.h"
-#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "content/public/utility/content_utility_client.h"
@@ -28,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/mojom/sandbox.mojom.h"
 #include "sandbox/policy/sandbox.h"
 #include "sandbox/policy/sandbox_type.h"
-#include "services/network/public/mojom/network_service.mojom.h"
 #include "services/tracing/public/cpp/trace_startup.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/icu/source/common/unicode/unistr.h"
@@ -80,17 +78,6 @@ sandbox::TargetServices* g_utility_target_services = nullptr;
 #endif
 
 namespace content {
-namespace {
-
-base::ThreadPriority GetIOThreadPriority(const std::string& utility_sub_type) {
-  return (base::FeatureList::IsEnabled(
-              features::kNetworkServiceUsesDisplayThreadPriority) &&
-          utility_sub_type == network::mojom::NetworkService::Name_)
-             ? base::ThreadPriority::DISPLAY
-             : base::ThreadPriority::NORMAL;
-}
-
-}  // namespace
 
 // Mainline routine for running as the utility process.
 int UtilityMain(MainFunctionParams parameters) {
@@ -133,10 +120,9 @@ int UtilityMain(MainFunctionParams parameters) {
   base::SingleThreadTaskExecutor main_thread_task_executor(message_pump_type);
   base::PlatformThread::SetName("CrUtilityMain");
 
-  const std::string utility_sub_type =
-      parameters.command_line->GetSwitchValueASCII(switches::kUtilitySubType);
-
   if (parameters.command_line->HasSwitch(switches::kUtilityStartupDialog)) {
+    const std::string utility_sub_type =
+        parameters.command_line->GetSwitchValueASCII(switches::kUtilitySubType);
     auto dialog_match = parameters.command_line->GetSwitchValueASCII(
         switches::kUtilityStartupDialog);
     if (dialog_match.empty() || dialog_match == utility_sub_type) {
@@ -215,7 +201,7 @@ int UtilityMain(MainFunctionParams parameters) {
   g_utility_target_services = parameters.sandbox_info->target_services;
 #endif
 
-  ChildProcess utility_process(GetIOThreadPriority(utility_sub_type));
+  ChildProcess utility_process(base::ThreadPriority::NORMAL);
   GetContentClient()->utility()->PostIOThreadCreated(
       utility_process.io_task_runner());
   base::RunLoop run_loop;
