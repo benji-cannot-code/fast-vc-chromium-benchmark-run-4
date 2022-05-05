7b2c09e5e25a63c883a0272a91587575420eb736
Rakina Zata Amni <rakina@chromium.org>
2022-05-05 03:39:56 +0000

    Make 'original opener' functions a bit clearer
    The term 'original opener' is a bit confusing as it has a lot of
    subtleties (tracking live main frame in the opener chain, etc). This CL
    attempts to clarify the functions that expose the 'original opener' by
    explicitly calling out the subtleties in the name and modifying the
    comments. This CL does not change any functionality of the functions
    (except changing one function to return WebContents instead of RFH).
    
    Bug: 1311820
    Change-Id: I9ec6e7b9493627c6ec7642e54578994135be4551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3624259
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999737}

--- content/browser/renderer_host/render_frame_host_manager_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_manager_browsertest.cc
@@ -990,14 +990,18 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest, DisownSubframeOpener) {
                             ->GetPrimaryFrameTree()
                             .root();
   EXPECT_EQ(root, root->child_at(0)->opener());
-  EXPECT_EQ(nullptr, root->child_at(0)->original_opener());
+  EXPECT_EQ(
+      nullptr,
+      root->child_at(0)->first_live_main_frame_in_original_opener_chain());
 
   // Now disown the frame's opener.  Shouldn't crash.
   EXPECT_TRUE(ExecuteScript(shell(), "window.frames[0].opener = null;"));
 
   // Check that the subframe's opener in the browser process is disowned.
   EXPECT_EQ(nullptr, root->child_at(0)->opener());
-  EXPECT_EQ(nullptr, root->child_at(0)->original_opener());
+  EXPECT_EQ(
+      nullptr,
+      root->child_at(0)->first_live_main_frame_in_original_opener_chain());
 }
 
 // Check that window.name is preserved for top frames when they navigate
@@ -3319,9 +3323,9 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest, UpdateOpener) {
           ->GetPrimaryFrameTree()
           .root();
   EXPECT_EQ(root, foo_root->opener());
-  EXPECT_EQ(root, foo_root->original_opener());
+  EXPECT_EQ(root, foo_root->first_live_main_frame_in_original_opener_chain());
   EXPECT_EQ(root, bar_root->opener());
-  EXPECT_EQ(root, bar_root->original_opener());
+  EXPECT_EQ(root, bar_root->first_live_main_frame_in_original_opener_chain());
 
   // From the bar process, use window.open to update foo's opener to point to
   // bar. This is allowed since bar is same-origin with foo's opener.  Use
@@ -3338,7 +3342,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest, UpdateOpener) {
 
   // Check that updated opener propagated to the browser process.
   EXPECT_EQ(bar_root, foo_root->opener());
-  EXPECT_EQ(root, foo_root->original_opener());
+  EXPECT_EQ(root, foo_root->first_live_main_frame_in_original_opener_chain());
 
   // Check that foo's opener was updated in foo's process. Send a postMessage
   // to the opener and check that the right window (bar_shell) receives it.
@@ -3356,7 +3360,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest, UpdateOpener) {
   // in the browser process.
   EXPECT_TRUE(ExecuteScript(foo_shell, "window.opener = window;"));
   EXPECT_EQ(bar_root, foo_root->opener());
-  EXPECT_EQ(root, foo_root->original_opener());
+  EXPECT_EQ(root, foo_root->first_live_main_frame_in_original_opener_chain());
 }
 
 // Tests that when a popup is opened, which is then navigated cross-process and
