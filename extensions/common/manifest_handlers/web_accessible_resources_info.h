75ace08526ef98aedaf209c3784274299f2c3217
Solomon Kinard <solomonkinard@chromium.org>
2022-05-06 16:27:14 +0000

    Extensions: Resources: Allow wildcard for extension id
    Bug: 1133624
    Change-Id: I5b10fea53e528adb8ab27faf0b0882a97993a5ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2790682
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000398}

--- extensions/common/manifest_handlers/web_accessible_resources_info.h
+++ extensions/common/manifest_handlers/web_accessible_resources_info.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/manifest_handler.h"
@@ -32,7 +33,8 @@ struct WebAccessibleResourcesInfo : public Extension::ManifestData {
     Entry(URLPatternSet resources,
           URLPatternSet matches,
           std::vector<ExtensionId> extension_ids,
-          bool use_dynamic_url);
+          bool use_dynamic_url,
+          bool allow_all_extensions);
 
     // List of web accessible extension resources.
     URLPatternSet resources;
@@ -41,10 +43,13 @@ struct WebAccessibleResourcesInfo : public Extension::ManifestData {
     URLPatternSet matches;
 
     // List of extension ids allowed to access resources.
-    std::vector<ExtensionId> extension_ids;
+    base::flat_set<ExtensionId> extension_ids;
 
     // Optionally true to require dynamic urls from sites not in |matches|.
     bool use_dynamic_url;
+
+    // True if "*" is defined as an extension id in the manifest.
+    bool allow_all_extensions;
   };
 
   // Returns true if the specified resource is web accessible.
