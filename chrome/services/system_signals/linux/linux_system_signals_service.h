50db9130fa3ace6928f74cc3c1cece2c7d947eb3
Sebastien Lalancette <seblalancette@chromium.org>
2022-05-06 14:22:41 +0000

    [NewEV] Create SystemSignals Mojo Service
    This service will be in charge of collecting some of the new signals
    to be consumed by the SecureConnect extension.
    
    On Windows, it has to communicate with WMI and other OS symbols, and
    will therefore run in its own process to not impact Chrome's
    stability.
    
    On Mac and Linux, it doesn't communicate with such OS systems and
    can then run in-process.
    
    The SystemSignalsServiceHost public cpp class will be used by Chrome
    code when it wants to interact with the service, as the host will be
    in charge of that service's lifecycle. SecureConnect polls for signals
    every ~4 hours, so tearing down the process when the service is unused
    (and bringing it back up when needed) would be handled by the host.
    
    Bug: b:230612862
    Change-Id: I0d0365d038ec2a8c352787f48a5004de47a01651
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3623980
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000353}

---
+++ chrome/services/system_signals/linux/linux_system_signals_service.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_SERVICES_SYSTEM_SIGNALS_LINUX_LINUX_SYSTEM_SIGNALS_SERVICE_H_
+#define CHROME_SERVICES_SYSTEM_SIGNALS_LINUX_LINUX_SYSTEM_SIGNALS_SERVICE_H_
+
+#include <vector>
+
+#include "chrome/services/system_signals/public/mojom/system_signals.mojom.h"
+
+namespace system_signals {
+
+class LinuxSystemSignalsService : public mojom::SystemSignalsService {
+ public:
+  LinuxSystemSignalsService();
+  ~LinuxSystemSignalsService() override;
+
+  LinuxSystemSignalsService(const LinuxSystemSignalsService&) = delete;
+  LinuxSystemSignalsService& operator=(const LinuxSystemSignalsService&) =
+      delete;
+
+  // mojom::SystemSignalsService:
+  void GetBinarySignals(std::vector<mojom::BinarySignalsRequestPtr> requests,
+                        GetBinarySignalsCallback callback) override;
+};
+
+}  // namespace system_signals
+
+#endif  // CHROME_SERVICES_SYSTEM_SIGNALS_LINUX_LINUX_SYSTEM_SIGNALS_SERVICE_H_
