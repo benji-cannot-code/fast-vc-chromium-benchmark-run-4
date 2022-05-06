c60897656a74244d466965486b5b316bc8dbf987
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-05-06 16:45:52 +0000

    Revert "[Autofill Assistant] MR overrides plumbing (part 1)."
    This reverts commit 38daea91e299ae69a9a45b622840472e2ab82d2e.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision 38daea91e299ae69a9a45b622840472e2ab82d2e as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8814920257006896977
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2F38daea91e299ae69a9a45b622840472e2ab82d2e&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+38daea91e299ae69a9a45b622840472e2ab82d2e
    
    Original change's description:
    > [Autofill Assistant] MR overrides plumbing (part 1).
    > 
    > Before this change the agent (in the renderer) would request the model
    > file from the driver (browser).
    > 
    > This change extends the model file fetching mojo interface to also
    > return a serialized SemanticSelectorPolicy.
    > 
    > In order for this to work end to end there is still more plumbing
    > needed. In particular hostinig the SemanticSelectorPolicy in the service
    > providing the model.
    > 
    > BUG=b/228987849
    > 
    > Change-Id: I748aac779d53075f3e2fee4cccd54951886a3411
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602705
    > Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1000292}
    
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=b/228987849
    
    Change-Id: Ife52ffb7596aa8c4df50222ae8f9e9d2c3c69dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632567
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Owners-Override: Kevin McNee <mcnee@google.com>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000408}

--- components/autofill_assistant/content/renderer/autofill_assistant_agent.h
+++ components/autofill_assistant/content/renderer/autofill_assistant_agent.h
@@ -55,8 +55,7 @@ class AutofillAssistantAgent : public content::RenderFrameObserver,
 
   void GetAnnotateDomModel(
       base::TimeDelta model_timeout,
-      base::OnceCallback<
-          void(mojom::ModelStatus, base::File, const std::string&)> callback);
+      base::OnceCallback<void(mojom::ModelStatus, base::File)> callback);
 
   mojom::AutofillAssistantDriver& GetDriver();
 
@@ -67,8 +66,7 @@ class AutofillAssistantAgent : public content::RenderFrameObserver,
                       bool ignore_objective,
                       GetSemanticNodesCallback callback,
                       mojom::ModelStatus model_status,
-                      base::File model,
-                      const std::string& overrides_policy);
+                      base::File model);
 
   mojo::AssociatedRemote<mojom::AutofillAssistantDriver> driver_;
 
