5d6c48b94d21e660f38bc44bb7c322262885dc73
Qiyao Yuan <qiyaoyuan@tencent.com>
2022-05-06 17:37:02 +0000

    Remove unused includes of //base/task/single_thread_task_runner.h
    Bug: 242216
    Change-Id: I07c262437e30cbd1a90549182f7af093eb818db0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629050
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000438}

--- content/public/browser/url_data_source.h
+++ content/public/browser/url_data_source.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
-#include "base/task/single_thread_task_runner.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
