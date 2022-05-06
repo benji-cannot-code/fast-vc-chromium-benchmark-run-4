adbc856fed789e89002cc9a2aea1f3ef65c2cf2c
Eugene Zemtsov <eugene@chromium.org>
2022-05-06 00:45:18 +0000

    Enable NdkVideoEncodeAccelerator on Android P when it runs on 32bit ARM
    Bug: 1303175
    Change-Id: I5949670748f916ca9837edc0972f4cafb2e6f0f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629913
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000187}

--- media/gpu/android/ndk_video_encode_accelerator.cc
+++ media/gpu/android/ndk_video_encode_accelerator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "media/base/android/media_codec_util.h"
 #include "media/base/bitstream_buffer.h"
 #include "media/base/video_frame.h"
@@ -100,10 +101,17 @@ const base::Feature kAndroidNdkVideoEncoder{"AndroidNdkVideoEncoder",
                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 static bool InitMediaCodec() {
+  // We need at least Android P for AMediaCodec_getInputFormat(), but in
+  // Android P we have issues with CFI and dynamic linker on arm64.
+  const base::android::SdkVersion min_supported_version =
+#if defined(ARCH_CPU_ARMEL)
+      base::android::SDK_VERSION_P;
+#else
+      base::android::SDK_VERSION_Q;
+#endif
+
   if (base::android::BuildInfo::GetInstance()->sdk_int() <
-      base::android::SDK_VERSION_Q) {
-    // We need at least Android P for AMediaCodec_getInputFormat(), but in
-    // Android P we have issues with CFI and dynamic linker on arm64.
+      min_supported_version) {
     return false;
   }
 
