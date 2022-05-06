cd16aefc587c4e0902a5a888496a3d53b0ddd327
Chris Lu <thegreenfrog@chromium.org>
2022-05-06 15:51:55 +0000

    [ios] Hide Omnibox cancel button before finishing animation
    Hiding the cancel button is part of the UI changes in in this animation
    sequence, and if it happens after -animationFinished is called,
    a re-focus quickly after may have its unhide UI change overriden by
    the hideCancel() still finishing.
    
    Fixed: 1323136
    Change-Id: Ib859d7ea59c6cb68343c54a0ae4a1d9eabaab467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630575
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000379}

--- ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
+++ ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
@@ -323,8 +323,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                     }];
         }
         completion:^(BOOL _) {
-          [self animationFinished];
           hideCancel();
+          [self animationFinished];
         }];
   } else {
     contraction();
