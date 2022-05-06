0f91703e11b7accc8f9509d7475312c1af2f6049
Patrick Noland <pnoland@chromium.org>
2022-05-06 20:17:45 +0000

    [Journeys] Add some mediator unit tests
    Code coverage for the mediator was a bit low; this change should cover
    most of the missing lines.
    
    Bug: 1303171
    Change-Id: I4fea573576c40558f748265045ef81095b2b0a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632649
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000533}

--- chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersMediator.java
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersMediator.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
 import org.chromium.base.Promise;
@@ -140,7 +141,8 @@ class HistoryClustersMediator extends EmptyBottomSheetObserver implements Search
         }
     }
 
-    private void openHistoryClustersInFullPage(String query) {
+    @VisibleForTesting
+    void openHistoryClustersInFullPage(String query) {
         boolean isTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mContext);
         if (isTablet) {
             Tab currentTab = mTabSupplier.get();
