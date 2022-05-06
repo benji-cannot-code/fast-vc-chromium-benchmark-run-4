0fe4d3c9775b38e917012d319506d1ffa8f69ddd
jlebel <jlebel@chromium.org>
2022-05-06 14:57:44 +0000

    [iOS] Show enterprise notification when UMA reporting is managed
    A test case has been added in the list:
    https://docs.google.com/document/d/19R5BJAVUDibrPvSKM_R9kLp1y5q_tWGbUYBl70z3k4c
    
    Fixed: 1323178
    Change-Id: I9da379cfabc87d6e48cb3fe10a91f3d2a6624f72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629593
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000369}

--- ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_mediator.mm
@@ -265,6 +265,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         metricReportingDisabled
             ? SigninScreenConsumerScreenIntentWelcomeWithoutUMAAndSignin
             : SigninScreenConsumerScreenIntentWelcomeAndSignin;
+    self.consumer.managedEnabled |= self.localPrefService->IsManagedPreference(
+        metrics::prefs::kMetricsReportingEnabled);
   }
   if (signinForcedOrAvailable) {
     self.selectedIdentity = self.accountManagerService->GetDefaultIdentity();
