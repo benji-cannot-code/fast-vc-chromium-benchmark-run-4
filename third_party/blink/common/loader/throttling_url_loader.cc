511b092486c480ef778f494cfeacad616cdd0893
John Abd-El-Malek <jam@chromium.org>
2022-05-06 00:15:32 +0000

    Update plugin interception throttle in preparation of removing OnStartLoadingResponseBody.
    Since OnStartLoadingResponseBody will go away, pass the new body to ThrottlingURLLoader::InterceptResponse so that it can send it in the URLLoaderClient::OnReceiveResponse call.
    
    Bug: 1017710
    Change-Id: If1eef199409e6a44ad790d7b6c9574326a2503cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3628711
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000177}

--- third_party/blink/common/loader/throttling_url_loader.cc
+++ third_party/blink/common/loader/throttling_url_loader.cc
@@ -1036,7 +1036,7 @@ void ThrottlingURLLoader::InterceptResponse(
     mojo::ScopedDataPipeConsumerHandle* body) {
   response_intercepted_ = true;
 
-  *body = std::move(body_);
+  body->swap(body_);
   if (original_loader) {
     url_loader_->ResumeReadingBodyFromNet();
     *original_loader = url_loader_.Unbind();
