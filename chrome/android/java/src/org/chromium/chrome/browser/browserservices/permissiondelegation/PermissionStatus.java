f78f5f524995b01a4a2bb273f1af130a7d058753
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-05-06 00:21:22 +0000

    Plumb from NotificationPermissionUpdater to TrustedWebActivityClient.
    Add these to TrustedWebActivityClient:
    * PermissionCallback
    * checkNotificationPermissionSetting
    * requestNotificationPermission
    
    Use CachedFeatureFlags because NotificationPermissionUpdater.onOriginVerified
    could run before native has been initialized.
    
    Bug: 1320272
    Change-Id: I7870cc30970944a7ec3856cce57712fb064ec4a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613380
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000179}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/permissiondelegation/PermissionStatus.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.browserservices.permissiondelegation;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@IntDef({PermissionStatus.ALLOW, PermissionStatus.BLOCK, PermissionStatus.ASK})
+@Retention(RetentionPolicy.SOURCE)
+/**
+ * Represents the permission state in TWA service calls.
+ */
+public @interface PermissionStatus {
+    int ALLOW = 0;
+    int BLOCK = 1;
+    int ASK = 2;
+}
--- chrome/browser/notifications/notification_permission_context.cc
+++ chrome/browser/notifications/notification_permission_context.cc
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/android/build_info.h"
 #include "chrome/browser/android/shortcut_helper.h"
+#include "chrome/browser/flags/android/cached_feature_flags.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/installable/installed_webapp_bridge.h"
 #endif  // BUILDFLAG(IS_ANDROID)
@@ -167,7 +169,8 @@ void NotificationPermissionContext::DecidePermission(
   }
 
 #if BUILDFLAG(IS_ANDROID)
-  if (base::FeatureList::IsEnabled(
+  if (base::android::BuildInfo::GetInstance()->is_at_least_t() &&
+      chrome::android::IsJavaDrivenFeatureEnabled(
           chrome::android::
               kTrustedWebActivityNotificationPermissionDelegation) &&
       ShortcutHelper::DoesOriginContainAnyInstalledTrustedWebActivity(
