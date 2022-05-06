d6c0e8f89a4c9e604595c2acbf5d7b047ebf213d
Kevin Radtke <kradtke@chromium.org>
2022-05-06 16:53:55 +0000

    QuickSettings.UserJourneyTime metric
    Implement metric that records time the user spent in Quick Settings. Starts measuring time whenever the QS bubble is opened, and records metric when the QS bubble is closed.
    
    Bug: 1296746
    Change-Id: If402d64ee69c89e0dc9ac62e951e1b99f50ec37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608993
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000411}

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/unified_system_tray_model.h"
 #include "ash/system/unified/unified_system_tray_view.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -510,12 +511,15 @@ bool UnifiedSystemTray::PerformAction(const ui::Event& event) {
 void UnifiedSystemTray::ShowBubble() {
   // ShowBubbleInternal will be called from UiDelegate.
   if (!bubble_) {
+    time_opened_ = base::TimeTicks::Now();
     ui_delegate_->ui_controller()->ShowMessageCenterBubble();
     Shell::Get()->system_tray_notifier()->NotifySystemTrayBubbleShown();
   }
 }
 
 void UnifiedSystemTray::CloseBubble() {
+  base::UmaHistogramMediumTimes("Ash.QuickSettings.UserJourneyTime",
+                                base::TimeTicks::Now() - time_opened_);
   // HideMessageCenterBubbleInternal will be called from UiDelegate.
   ui_delegate_->ui_controller()->HideMessageCenterBubble();
 
