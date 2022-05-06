0d6f0587bd14e4613ec5416eb8e9d77335f46d1a
John Abd-El-Malek <jam@chromium.org>
2022-05-06 05:25:06 +0000

    Update StreamingSearchPrefetchURLLoader in preparation of removing OnStartLoadingResponseBody.
    Bug: 1017710
    Change-Id: I9e830550c7eeec13f71ff7e0fdbb964a86d10023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631223
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000259}

--- chrome/browser/prefetch/search_prefetch/streaming_search_prefetch_url_loader.cc
+++ chrome/browser/prefetch/search_prefetch/streaming_search_prefetch_url_loader.cc
@@ -174,8 +174,6 @@ void StreamingSearchPrefetchURLLoader::SetUpForwardingClient(
     status_->completion_time = base::TimeTicks::Now();
   }
 
-  forwarding_client_->OnReceiveResponse(std::move(resource_response_),
-                                        mojo::ScopedDataPipeConsumerHandle());
   RunEventQueue();
 }
 
@@ -256,8 +254,7 @@ void StreamingSearchPrefetchURLLoader::OnReceiveResponse(
   if (estimated_length_ > 0)
     body_content_.reserve(estimated_length_);
 
-  if (body)
-    OnStartLoadingResponseBody(std::move(body));
+  OnStartLoadingResponseBody(std::move(body));
 }
 
 void StreamingSearchPrefetchURLLoader::OnReceiveRedirect(
@@ -305,12 +302,7 @@ void StreamingSearchPrefetchURLLoader::OnTransferSizeUpdated(
 
 void StreamingSearchPrefetchURLLoader::OnStartLoadingResponseBody(
     mojo::ScopedDataPipeConsumerHandle body) {
-  if (forwarding_client_) {
-    DCHECK(!streaming_prefetch_request_);
-    forwarding_client_->OnStartLoadingResponseBody(std::move(body));
-    return;
-  }
-
+  DCHECK(!forwarding_client_);
   serving_from_data_ = true;
 
   pipe_drainer_ =
@@ -369,7 +361,8 @@ void StreamingSearchPrefetchURLLoader::OnStartLoadingResponseBodyFromData() {
       base::BindRepeating(&StreamingSearchPrefetchURLLoader::OnHandleReady,
                           weak_factory_.GetWeakPtr()));
 
-  forwarding_client_->OnStartLoadingResponseBody(std::move(consumer_handle));
+  forwarding_client_->OnReceiveResponse(std::move(resource_response_),
+                                        std::move(consumer_handle));
 
   PushData();
 }
