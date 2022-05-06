1905152c7ef9ae61b2d7ae3a637843d690b15ae8
Ahmed Nasr <anasr@google.com>
2022-05-06 22:33:03 +0000

    Change sampler collection parameter to optional #5.
    Remove deprecated Collect method from Sampler.
    
    Bug: b:209638899
    Change-Id: Ief4a06a82430cd6aea0c8910f0b23925b6484dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632652
    Reviewed-by: Tyler Garrett <tylergarrett@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1000583}

--- components/reporting/metrics/fake_sampler.h
+++ components/reporting/metrics/fake_sampler.h
@@ -22,8 +22,6 @@ class FakeSampler : public Sampler {
 
   ~FakeSampler() override;
 
-  void Collect(MetricCallback cb) override;
-
   void MaybeCollect(OptionalMetricCallback cb) override;
 
   void SetMetricData(absl::optional<MetricData> metric_data);
