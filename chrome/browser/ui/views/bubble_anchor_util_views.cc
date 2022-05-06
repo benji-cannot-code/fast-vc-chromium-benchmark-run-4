33915e898d8f97f48343d49b6583ac47ebbdf0e5
Olesia Marukhno <olesiamarukhno@google.com>
2022-05-06 10:22:00 +0000

    [privacy sandbox settings] Create bubble delegate
    Create bubble delegate and set the anchor, size and buttons.
    
    Bug: 1321587
    Change-Id: Ibd8284c8dba5fbea3dd894e9a33665c806f40312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620325
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1000311}

--- chrome/browser/ui/views/bubble_anchor_util_views.cc
+++ chrome/browser/ui/views/bubble_anchor_util_views.cc
@@ -60,6 +60,10 @@ AnchorConfiguration GetPermissionPromptBubbleAnchorConfiguration(
   return GetPageInfoAnchorConfiguration(browser);
 }
 
+AnchorConfiguration GetAppMenuAnchorConfiguration(Browser* browser) {
+  return GetPageInfoAnchorConfiguration(browser, kAppMenuButton);
+}
+
 gfx::Rect GetPageInfoAnchorRect(Browser* browser) {
   // GetPageInfoAnchorConfiguration()'s anchor_view should be preferred if
   // available.
