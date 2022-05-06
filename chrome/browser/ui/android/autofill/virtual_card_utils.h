5327e8ddc0560c662c89df6d4cf30364944ebaa7
Vishwas Uppoor <vishwasuppoor@google.com>
2022-05-06 22:20:53 +0000

    [VCN][M2] Upstream enrollment dialog
    This CL creates a parallel flow for upstream enrollment in which the enrollment info is shown in a dialog instead of an infobar.
    
    The dialog is reused from crrev.com/c/3455322.
    This CL follows the new recommended way to implement view/controller JNI interfaces documented here (https://docs.google.com/document/d/1J19BNafBk90mSzQnAjykguROCDK8ZzYiUVi3Uwj_5jg/edit?resourcekey=0-QT0PzlbhCbjNFoAaSe4_qg#heading=h.subvi3olnqaf).
    Screen record of the upstream flow: https://drive.google.com/file/d/1oUNacjoNAZOPhX890zEsu5nBUILU5Sao/view?usp=sharing&resourcekey=0-qzoH3eu2-jJoUZvxpBGyQA
    
    Bug: 1281695, 1306021
    
    Change-Id: Ifb860cf891725986dece9c5865773d20b9474f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610789
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1000581}

---
+++ chrome/browser/ui/android/autofill/virtual_card_utils.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ANDROID_AUTOFILL_VIRTUAL_CARD_UTILS_H_
+#define CHROME_BROWSER_UI_ANDROID_AUTOFILL_VIRTUAL_CARD_UTILS_H_
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+#include "components/autofill/core/browser/payments/virtual_card_enrollment_manager.h"
+
+namespace autofill {
+
+// Converts VirtualCardEnrollmentFields native object to it's Java counterpart.
+base::android::ScopedJavaLocalRef<jobject>
+CreateVirtualCardEnrollmentFieldsJavaObject(
+    autofill::VirtualCardEnrollmentFields* virtual_card_enrollment_fields);
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_ANDROID_AUTOFILL_VIRTUAL_CARD_UTILS_H_
