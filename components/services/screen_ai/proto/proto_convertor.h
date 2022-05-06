bf24fbbeaa81c02887a636b247648b64470fb67b
Ramin Halavati <rhalavati@chromium.org>
2022-05-06 09:32:22 +0000

    Add Screen2x protos to ChromeScreenAI library.
    Two new protos are added from Google3 to pass data to
    MainContentExtraction function of ChromeScreenAI library.
    
    Proto processing code in ScreenAIServiceImpl are moved to new
    proto_convertor.* files.
    
    Bug: 1278249
    Change-Id: I4c8f23a56990fdc580f448857c71cc3bca952e7c
    Ax-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3623904
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000296}

---
+++ components/services/screen_ai/proto/proto_convertor.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SERVICES_SCREEN_AI_PROTO_PROTO_CONVERTOR_H_
+#define COMPONENTS_SERVICES_SCREEN_AI_PROTO_PROTO_CONVERTOR_H_
+
+#include <string>
+
+#include "ui/accessibility/ax_tree_update.h"
+
+namespace screen_ai {
+
+// Converts serialized VisualAnnotation proto from ScreenAI to AXTreeUpdate.
+ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
+    const std::string& serialized_proto);
+
+// Converts an AXTreeUpdate snapshot to serialized ViewHierarchy proto for
+// Screen2X.
+std::string Screen2xSnapshotToViewHierarchy(const ui::AXTreeUpdate& snapshot);
+
+}  // namespace screen_ai
+#endif  // COMPONENTS_SERVICES_SCREEN_AI_PROTO_PROTO_CONVERTOR_H_
