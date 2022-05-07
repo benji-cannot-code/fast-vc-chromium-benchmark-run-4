0d8731b4e0549cce39c7a48292abe6616ac0ee3d
Mitsuru Oshima <oshima@chromium.org>
2022-05-07 08:33:51 +0000

    Use ScopedSetInternalDisplayIds in unittests
    Moved an internal ScopedSetInternalDisplayIds to display_test_util.h
    Changed DisplayUtilTest to use it.
    
    Bug=b:231607083
    
    Change-Id: I8f9a903e77b6cca8f7a72b7c50611ed0bca88f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631219
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000697}

--- ui/display/test/display_test_util.h
+++ ui/display/test/display_test_util.h
@@ -7,10 +7,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_DISPLAY_TEST_DISPLAY_TEST_UTIL_H_
 
 #include "ui/display/display.h"
+#include "ui/display/util/display_util.h"
 
 namespace display {
 
-void PrintTo(const Display& display, ::std::ostream* os) {
+// Use this class instead of calling `SetInternalDisplayIds()` in unit tests to
+// avoid leaking the state to other tests.
+class ScopedSetInternalDisplayIds {
+ public:
+  explicit ScopedSetInternalDisplayIds(const base::flat_set<int64_t> ids) {
+    SetInternalDisplayIds(ids);
+  }
+  explicit ScopedSetInternalDisplayIds(int64_t id) {
+    SetInternalDisplayIds({id});
+  }
+  ScopedSetInternalDisplayIds(const ScopedSetInternalDisplayIds&) = delete;
+  ScopedSetInternalDisplayIds operator=(const ScopedSetInternalDisplayIds&) =
+      delete;
+  ~ScopedSetInternalDisplayIds() { SetInternalDisplayIds({}); }
+};
+
+inline void PrintTo(const Display& display, ::std::ostream* os) {
   *os << display.ToString();
 }
 
