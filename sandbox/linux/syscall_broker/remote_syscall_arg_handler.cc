80bac788ef1d5a576402635fbc97038ce100b45c
Ivan Murashov <ivan.murashov@lge.com>
2022-05-07 09:03:31 +0000

    IWYU: Add <algorithm> for std::min in remote_syscall_arg_handler.cc
    Bug: 957519
    Change-Id: I03a584dd074adea3ec6c2e693bea4702bc86e6ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3633087
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000699}

--- sandbox/linux/syscall_broker/remote_syscall_arg_handler.cc
+++ sandbox/linux/syscall_broker/remote_syscall_arg_handler.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/uio.h>
 #include <unistd.h>
 
+#include <algorithm>
+
 #include "base/bits.h"
 #include "base/check_op.h"
 #include "base/containers/span.h"
