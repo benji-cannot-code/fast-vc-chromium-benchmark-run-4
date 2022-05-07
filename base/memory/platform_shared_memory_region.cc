c57e0dd7c03c65ac6d2d6b371891d87f94aa4699
Daniel Cheng <dcheng@chromium.org>
2022-05-07 00:22:42 +0000

    Allow shared memory to be mapped at arbitrary offsets.
    Requiring page alignment is an implementation detail that is not
    necessary to expose in the API. Exposing it in the API has resulted in
    several consumers implementing their own workarounds to handle this,
    e.g. media::UnalignedSharedMemory, Mojo's shared buffer handling, and
    Blink's array buffer mapping code.
    
    Change-Id: I175a4a7a7eab40969d72a38a1963a72134aa86c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625893
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000628}

--- base/memory/platform_shared_memory_region.cc
+++ base/memory/platform_shared_memory_region.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/platform_shared_memory_region.h"
 
+#include "base/bits.h"
 #include "base/memory/aligned_memory.h"
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/shared_memory_security_policy.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/numerics/checked_math.h"
+#include "base/system/sys_info.h"
 
 namespace base {
 namespace subtle {
@@ -61,6 +63,9 @@ absl::optional<span<uint8_t>> PlatformSharedMemoryRegion::MapAt(
     return absl::nullopt;
   }
 
+  // TODO(dcheng): Presumably the actual size of the mapping is rounded to
+  // `SysInfo::VMAllocationGranularity()`. Should this accounting be done with
+  // that in mind?
   if (!SharedMemorySecurityPolicy::AcquireReservationForMapping(size)) {
     RecordMappingWasBlockedHistogram(/*blocked=*/true);
     return absl::nullopt;
@@ -71,11 +76,23 @@ absl::optional<span<uint8_t>> PlatformSharedMemoryRegion::MapAt(
   if (!mapper)
     mapper = SharedMemoryMapper::GetDefaultInstance();
 
+  // The backing mapper expects offset to be aligned to
+  // `SysInfo::VMAllocationGranularity()`.
+  size_t aligned_offset =
+      bits::AlignDown(offset, SysInfo::VMAllocationGranularity());
+  size_t adjustment_for_alignment = offset - aligned_offset;
+
   bool write_allowed = mode_ != Mode::kReadOnly;
-  auto result = mapper->Map(GetPlatformHandle(), write_allowed, offset, size);
+  auto result = mapper->Map(GetPlatformHandle(), write_allowed, aligned_offset,
+                            size + adjustment_for_alignment);
 
   if (result.has_value()) {
     DCHECK(IsAligned(result.value().data(), kMapMinimumAlignment));
+    if (offset != 0) {
+      // Undo the previous adjustment so the returned mapping respects the exact
+      // requested `offset` and `size`.
+      result = result->subspan(adjustment_for_alignment);
+    }
   } else {
     SharedMemorySecurityPolicy::ReleaseReservationForMapping(size);
   }
--- base/memory/shared_memory_mapping.cc
+++ base/memory/shared_memory_mapping.cc
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/shared_memory_mapping.h"
 
+#include <cstdint>
 #include <utility>
 
+#include "base/bits.h"
 #include "base/logging.h"
 #include "base/memory/shared_memory_security_policy.h"
 #include "base/memory/shared_memory_tracker.h"
+#include "base/system/sys_info.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
 
@@ -42,6 +45,7 @@ SharedMemoryMapping::SharedMemoryMapping(span<uint8_t> mapped_span,
                                          const UnguessableToken& guid,
                                          SharedMemoryMapper* mapper)
     : mapped_span_(mapped_span), size_(size), guid_(guid), mapper_(mapper) {
+  // Note: except on Windows, `mapped_span_.size() == size_`.
   SharedMemoryTracker::GetInstance()->IncrementMemoryUsage(*this);
 }
 
@@ -55,7 +59,16 @@ void SharedMemoryMapping::Unmap() {
   SharedMemoryMapper* mapper = mapper_;
   if (!mapper)
     mapper = SharedMemoryMapper::GetDefaultInstance();
-  mapper->Unmap(mapped_span_);
+
+  // The backing mapper expects offset to be aligned to
+  // `SysInfo::VMAllocationGranularity()`, so replicate the alignment that was
+  // done when originally mapping in the region.
+  uint8_t* aligned_data =
+      bits::AlignDown(mapped_span_.data(), SysInfo::VMAllocationGranularity());
+  size_t adjusted_size =
+      mapped_span_.size() + (mapped_span_.data() - aligned_data);
+  span<uint8_t> span_to_unmap = make_span(aligned_data, adjusted_size);
+  mapper->Unmap(span_to_unmap);
 }
 
 ReadOnlySharedMemoryMapping::ReadOnlySharedMemoryMapping() = default;
