cf522165ec2cbc98b7ca1da12045ddf57a73c1e6
Kevin Ellis <kevers@chromium.org>
2022-05-09 15:31:24 +0000

    Revert "Avoid unnecessary calls to paint invalidation."
    This reverts commit 8bee5e4363e8cb5981d5a2a24b9533f4851a6dc0.
    
    Reason for revert: 
    
    crbug.com/1291090
    crbug.com/1323173
    
    Original change's description:
    > Avoid unnecessary calls to paint invalidation.
    >
    > Previously, any background color change during style resolution
    > triggered paint invalidation regardless of whether the change is
    > triggered by a composited animation. We now check if the color change
    > has a native paint worklet before deciding if paint invalidation should
    > be performed. In the event of a main thread background-color animation,
    > we prevent spinning needlessly on building a native worklet paint image
    > by caching whether the animation can be composited.
    >
    > Bug: 1301961
    >
    > Change-Id: I02a9b33267592590321af6d6c0e4da7cfbee2cc9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542797
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Kevin Ellis <kevers@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1000090}
    
    Bug: 1301961
    Change-Id: Idc764b461410adc655e158af499130c195dd770f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635297
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001026}

--- third_party/blink/renderer/core/style/style_difference.h
+++ third_party/blink/renderer/core/style/style_difference.h
@@ -30,7 +30,6 @@ class StyleDifference {
     kTextDecorationOrColorChanged = 1 << 5,
     kBlendModeChanged = 1 << 6,
     kMaskChanged = 1 << 7,
-    kBackgroundColorChanged = 1 << 8,
     // If you add a value here, be sure to update kPropertyDifferenceCount.
   };
 
@@ -152,13 +151,6 @@ class StyleDifference {
   }
   void SetMaskChanged() { property_specific_differences_ |= kMaskChanged; }
 
-  bool BackgroundColorChanged() const {
-    return property_specific_differences_ & kBackgroundColorChanged;
-  }
-  void SetBackgroundColorChanged() {
-    property_specific_differences_ |= kBackgroundColorChanged;
-  }
-
   bool ScrollAnchorDisablingPropertyChanged() const {
     return scroll_anchor_disabling_property_changed_;
   }
@@ -177,7 +169,7 @@ class StyleDifference {
   }
 
  private:
-  static constexpr int kPropertyDifferenceCount = 9;
+  static constexpr int kPropertyDifferenceCount = 8;
 
   friend CORE_EXPORT std::ostream& operator<<(std::ostream&,
                                               const StyleDifference&);
