698e51fe98efe4a088700be10fdc2b9bdd9f72ca
Nidhi Jaju <nidhijaju@chromium.org>
2022-05-09 05:53:25 +0000

    Roll src/net/third_party/quiche/src/ 339f0c80d..bfb48274c (31 commits)
    https://quiche.googlesource.com/quiche.git/+log/339f0c80dfe7..bfb48274c632
    
    $ git log 339f0c80d..bfb48274c --date=short --no-merges --format='%ad %ae %s'
    2022-05-06 birenroy Adds a test demonstrating that metadata is not sent if queued immediately before trailers.
    2022-05-06 vasilvv Move all of HTTP/2 test tools into http2/test_tools.
    2022-05-05 bnc Add missing QUICHE_EXPORT_PRIVATE at a few places.
    2022-05-05 quiche-dev Fixes for C++20 support.
    2022-05-05 haoyuewang Internal change
    2022-05-05 bnc Move IsInvalidHeaderKeyChar() from balsa_frame.cc anonymous namespace to header_properties.
    2022-05-05 vasilvv Merge third_party/http2/tools into third_party/http2/test_tools.
    2022-05-04 vasilvv Make packet_dropping_test_writer not depend on epoll-related code.
    2022-05-04 vasilvv Move SPDY test tools into //third_party/spdy/test_tools/...
    2022-05-04 bnc Use uint8_t instead of char for array indexing in quiche::header_properties.
    2022-05-04 vasilvv Fix QuicheBufferAllocatorTest
    2022-05-04 vasilvv Actually export quiche_file_utils test files.
    2022-05-04 quiche-dev Flip restart flags for staging and gfe-dev-co production locations.
    2022-05-04 bnc Use unsigned type as index for array.
    2022-05-04 bnc Change flags type in Http2FrameHeaderTypeAndFlagTest.
    2022-05-04 birenroy Uses uint8_t for an array index value.
    2022-05-04 bnc Pretty print Http2FrameHeaderTypeAndFlagTest params.
    2022-05-03 bnc Do not recalculate BalsaHeaders::iterator_base::value_ unless necessary.
    2022-05-03 fayang Log more info when retransmittable-on-wire deadline is in the past.
    2022-05-02 birenroy Adds a mode to HeaderValidator that accepts `obs-text`, and makes it the default for OgHttp2Session.
    2022-05-02 bnc Make BalsaHeaders::iterator_base and const_header_lines_iterator copy constructor implicit.
    2022-05-02 wub Deprecate --gfe2_restart_flag_quic_tls_server_support_client_cert.
    2022-05-02 bnc Remove header policy tests from public QUICHE.
    2022-05-02 dschinazi Add two vulnerable ports to QUIC blocked list
    2022-05-02 bnc Do not use old-style cast in balsa_frame_test.cc.
    2022-05-02 bnc Use EXPECT_QUICHE_BUG in BalsaHeadersTest.
    2022-05-02 bnc Remove http2_flags.h and http2_flag_utils.h.
    2022-05-02 bnc Fix signed-unsigned comparison.
    2022-05-02 wub Add three variants of selective resumption behavior, controlled by connection option 'GSR1', 'GSR2' and 'GSR3'.
    2022-05-02 bnc Remove http2_logging.h.
    2022-05-02 wub Pass TLS read/write secrets by absl::Span instead of a byte vector.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    fixed compile issues
    
    Change-Id: I2391db2ba8ca7c803a214f99b23bf7ff188fe3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631045
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000877}

--- net/quic/quic_stream_factory_test.cc
+++ net/quic/quic_stream_factory_test.cc
@@ -80,7 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quiche/src/quiche/quic/test_tools/quic_session_peer.h"
 #include "net/third_party/quiche/src/quiche/quic/test_tools/quic_spdy_session_peer.h"
 #include "net/third_party/quiche/src/quiche/quic/test_tools/quic_test_utils.h"
-#include "net/third_party/quiche/src/quiche/spdy/core/spdy_test_utils.h"
+#include "net/third_party/quiche/src/quiche/spdy/test_tools/spdy_test_utils.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
