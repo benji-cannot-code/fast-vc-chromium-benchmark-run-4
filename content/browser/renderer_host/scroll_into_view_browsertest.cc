9f3583eb7a62b59d26cd8205f4d26bb33027b592
Lingqi Chi <lingqi@chromium.org>
2022-05-09 04:06:46 +0000

    Sheriff: disable ScrollIntoViewBrowserTest.*/RemoteFrame_* on Mac
    Typical failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/6235/test-results
    
    Bug: 1323606
    Change-Id: I9f922e8cb52da9a9dc79e882f92fa9c9170efad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631850
    Quick-Run: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000861}

--- content/browser/renderer_host/scroll_into_view_browsertest.cc
+++ content/browser/renderer_host/scroll_into_view_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/strings/strcat.h"
 #include "base/test/bind.h"
+#include "build/build_config.h"
 #include "content/browser/renderer_host/render_widget_host_view_child_frame.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/test/browser_test.h"
@@ -566,11 +567,21 @@ IN_PROC_BROWSER_TEST_P(ScrollIntoViewBrowserTest, EditableInSingleNestedFrame) {
 }
 
 IN_PROC_BROWSER_TEST_P(ScrollIntoViewBrowserTest, EditableInLocalRoot) {
+// Flaky on Mac ARM64. https://crbug.com/1323606
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
+  if (!IsForceLocalFrames())
+    return;
+#endif
   ASSERT_TRUE(SetupTest("siteA(siteB(siteA))"));
   RunTest();
 }
 
 IN_PROC_BROWSER_TEST_P(ScrollIntoViewBrowserTest, EditableInDoublyNestedFrame) {
+// Flaky on Mac ARM64. https://crbug.com/1323606
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
+  if (!IsForceLocalFrames())
+    return;
+#endif
   ASSERT_TRUE(SetupTest("siteA(siteB(siteC))"));
   RunTest();
 }
@@ -578,6 +589,11 @@ IN_PROC_BROWSER_TEST_P(ScrollIntoViewBrowserTest, EditableInDoublyNestedFrame) {
 IN_PROC_BROWSER_TEST_P(
     ScrollIntoViewBrowserTest,
     CrossesEditableInDoublyNestedFrameLocalAndRemoteBoundaries) {
+// Flaky on Mac ARM64. https://crbug.com/1323606
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
+  if (!IsForceLocalFrames())
+    return;
+#endif
   ASSERT_TRUE(SetupTest("siteA(siteA(siteB(siteB)))"));
   RunTest();
 }
