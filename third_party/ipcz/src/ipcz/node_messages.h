b2be9b9456b4a65384eb6db8c34b24ab0e7d1bbc
Ken Rockot <rockot@google.com>
2022-05-09 22:55:46 +0000

    ipcz: Implement basic node connection
    Split out from the ipcz uber-CL: https://crrev.com/c/3570271
    
    Introduces NodeConnector and some associated ipcz messages which will be
    used by ConnectNode() to bootstrap new NodeLinks between nodes.
    
    A NodeConnector is a short-lived object which takes initial ownership
    of a DriverTransport to transmit and listen for appropriate handshake
    messages. Once handshake is complete, the transport is passed on to
    a newly created NodeLink and given to whichever Node initiated a
    connection on the transport.
    
    This does not actually implement the ConnectNode() API yet. This
    CL is primarily about landing the most important NodeConnector
    implementations (broker-to-non-broker, and non-broker-to-broker)
    and getting test coverage for them.
    
    Bug: 1299283
    Change-Id: I389ac8796c36446661fe1d96f63254fc96a95cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3621786
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001227}

--- third_party/ipcz/src/ipcz/node_messages.h
+++ third_party/ipcz/src/ipcz/node_messages.h
@@ -12,9 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipcz/driver_object.h"
 #include "ipcz/driver_transport.h"
 #include "ipcz/message_internal.h"
+#include "ipcz/node_name.h"
 #include "ipcz/sublink_id.h"
 
 namespace ipcz::msg {
+
+// Bump this version number up by 1 when adding new protocol features so that
+// they can be detected during NodeLink establishment.
+constexpr uint32_t kProtocolVersion = 0;
 
 #pragma pack(push, 1)
 
