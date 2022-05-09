2e1f9676435b4373f448d6d12dbcef60e89957a8
Petro Akzhygitov <pakzhygitov@google.com>
2022-05-09 15:18:00 +0000

    Move BVC's DisplaySyncErrors() logic into a browser agent [Part 3/4]
    – Integrated SyncBrowserAgent behind the flag for sync error refactor
    
    Bug: 1272545
    Change-Id: If15ae6bfbd65cf12cf125e5c9ae58c2df3c5fe75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625718
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1001019}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.h
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "base/ios/block_types.h"
+#import "ios/chrome/browser/ui/authentication/signin_presenter.h"
 #import "ios/chrome/browser/ui/find_bar/find_bar_coordinator.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_consumer.h"
 #import "ios/chrome/browser/ui/ntp/logo_animation_controller.h"
@@ -39,6 +40,7 @@ class Browser;
                         LogoAnimationControllerOwnerOwner,
                         PageInfoPresentation,
                         PrintControllerDelegate,
+                        SigninPresenter,
                         SyncPresenter,
                         ThumbStripSupporting,
                         ToolbarCoordinatorDelegate,
