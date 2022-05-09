90c899fe80b194db86d10f81a9017032e61b765a
Kent Tamura <tkent@chromium.org>
2022-05-09 04:16:18 +0000

    DeferredShaping: Unlock by some inspector protocol methods
    Some inspector protocol methods require precise element geometry.
    This CL fixes some inspector tests with enabling DeferredShaping.
    
    Bug: 1259085
    Change-Id: I170fb94b5e6b78005904c54fa4eb82681f391aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632093
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000864}

--- third_party/blink/renderer/core/inspector/inspector_contrast.cc
+++ third_party/blink/renderer/core/inspector/inspector_contrast.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_computed_style_declaration.h"
 #include "third_party/blink/renderer/core/css/css_gradient_value.h"
 #include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_document_state.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
 #include "third_party/blink/renderer/core/dom/node.h"
@@ -140,6 +141,7 @@ void InspectorContrast::CollectNodesAndBuildRTreeIfNeeded() {
   if (!layout_view)
     return;
 
+  document_->GetDisplayLockDocumentState().UnlockShapingDeferredElements();
   if (!layout_view->GetFrameView()->UpdateLifecycleToPrePaintClean(
           DocumentUpdateReason::kInspector)) {
     return;
@@ -272,6 +274,10 @@ Vector<Color> InspectorContrast::GetBackgroundColors(Element* element,
     return colors;
   }
 
+  if (RuntimeEnabledFeatures::DeferredShapingEnabled()) {
+    document_->GetDisplayLockDocumentState().UnlockShapingDeferredElements();
+    document_->UpdateStyleAndLayout(DocumentUpdateReason::kInspector);
+  }
   PhysicalRect content_bounds = GetNodeRect(text_node);
   LocalFrameView* view = text_node->GetDocument().View();
   if (!view)
