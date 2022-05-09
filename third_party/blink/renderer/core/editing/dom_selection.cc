ad0e31a4a4394cb707365e8993234fc1d9d47a08
Kent Tamura <tkent@chromium.org>
2022-05-09 08:09:15 +0000

    Remove the deprecation message for SelectionAddRangeIntersect
    It's a post-removal message, and we have shown it for a long time.
    
    Bug: 353069, 1320361
    Change-Id: Ic20218e20d2a4dd178d890f7776f928275e90b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631043
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000916}

--- third_party/blink/renderer/core/editing/dom_selection.cc
+++ third_party/blink/renderer/core/editing/dom_selection.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/selection_template.h"
 #include "third_party/blink/renderer/core/editing/set_selection_options.h"
 #include "third_party/blink/renderer/core/editing/visible_selection.h"
-#include "third_party/blink/renderer/core/frame/deprecation/deprecation.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
@@ -635,20 +634,6 @@ void DOMSelection::addRange(Range* new_range) {
       new_range->startContainer()->GetTreeScope()) {
     return;
   }
-
-  if (original_range->compareBoundaryPoints(Range::kStartToEnd, new_range,
-                                            ASSERT_NO_EXCEPTION) < 0 ||
-      new_range->compareBoundaryPoints(Range::kStartToEnd, original_range,
-                                       ASSERT_NO_EXCEPTION) < 0) {
-    return;
-  }
-
-  // TODO(tkent): "Merge the ranges if they intersect" was removed. We show a
-  // warning message for a while, and continue to collect the usage data.
-  // <https://code.google.com/p/chromium/issues/detail?id=353069>.
-  Deprecation::CountDeprecation(
-      tree_scope_->GetDocument().GetExecutionContext(),
-      WebFeature::kSelectionAddRangeIntersect);
 }
 
 // https://www.w3.org/TR/selection-api/#dom-selection-deletefromdocument
