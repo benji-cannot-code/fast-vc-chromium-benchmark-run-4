bae39fc94895d018bb4b1bf280d492c03a9416ab
Peter Kotwicz <pkotwicz@chromium.org>
2022-05-09 15:43:47 +0000

    Fix DCHECK failure when FedCM request times out during token fetch
    This CL fixes FederatedAuthRequestImpl::OnTokenResponseReceived()
    DCHECK failure when the FedCM request times out after
    IdpNetworkRequestManager::SendTokenRequest() but before
    FederatedAuthRequestImpl::OnTokenResponseReceived()
    
    BUG=1323112
    TEST=BasicFederatedAuthRequestImplTest.TokenRequestTimesOut
    
    Change-Id: Ic25b95c36ebadc7fd1ecfd0242675fc602b04bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631516
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001032}

--- content/browser/webid/federated_auth_request_impl_unittest.cc
+++ content/browser/webid/federated_auth_request_impl_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/string_util.h"
@@ -156,6 +157,7 @@ struct MockConfiguration {
   FetchStatus accounts_response;
   AccountList accounts;
   FetchStatus token_response;
+  bool delay_token_response;
   RevokeResponse revoke_response;
   bool customized_dialog;
   bool wait_for_callback;
@@ -181,6 +183,7 @@ static const MockConfiguration kConfigurationValid{
     FetchStatus::kSuccess,
     kAccounts,
     FetchStatus::kSuccess,
+    false /* delay_token_response */,
     RevokeResponse::kSuccess,
     false /* customized_dialog */,
     true /* wait_for_callback */};
@@ -228,6 +231,7 @@ class AuthRequestCallbackHelper {
  private:
   void ReceiverMethod(RequestIdTokenStatus status,
                       const absl::optional<std::string>& token) {
+    CHECK(!was_called_);
     status_ = status;
     token_ = token;
     was_called_ = true;
@@ -389,6 +393,13 @@ class TestIdpNetworkRequestManager : public MockIdpNetworkRequestManager {
     config_ = configuration;
   }
 
+  void RunDelayedCallbacks() {
+    for (base::OnceClosure& delayed_callback : delayed_callbacks_) {
+      std::move(delayed_callback).Run();
+    }
+    delayed_callbacks_.clear();
+  }
+
   void FetchManifestList(FetchManifestListCallback callback) override {
     fetched_endpoints_ |= FetchedEndpoint::MANIFEST_LIST;
     std::set<GURL> url_set(config_.manifest_list.provider_urls.begin(),
@@ -435,7 +446,12 @@ class TestIdpNetworkRequestManager : public MockIdpNetworkRequestManager {
     std::string delivered_token =
         config_.token_response == FetchStatus::kSuccess ? config_.token
                                                         : std::string();
-    std::move(callback).Run(config_.token_response, delivered_token);
+    base::OnceCallback bound_callback = base::BindOnce(
+        std::move(callback), config_.token_response, delivered_token);
+    if (config_.delay_token_response)
+      delayed_callbacks_.push_back(std::move(bound_callback));
+    else
+      std::move(bound_callback).Run();
   }
 
   void SendRevokeRequest(const GURL& revoke_url,
@@ -451,6 +467,7 @@ class TestIdpNetworkRequestManager : public MockIdpNetworkRequestManager {
  protected:
   MockConfiguration config_{kConfigurationValid};
   int fetched_endpoints_{0};
+  std::vector<base::OnceClosure> delayed_callbacks_;
 };
 
 class TestLogoutIdpNetworkRequestManager : public TestIdpNetworkRequestManager {
@@ -1727,6 +1744,24 @@ TEST_F(BasicFederatedAuthRequestImplTest, ApiBlockedForUnrelatedOrigin) {
   ASSERT_NE(main_test_rfh()->GetLastCommittedOrigin(), kUnrelatedOrigin);
   RunAuthTest(kDefaultRequestParameters, kExpectationSuccess,
               kConfigurationValid);
+}
+
+// Test that the request completes eventually in the case that the token request
+// times out.
+TEST_F(BasicFederatedAuthRequestImplTest, TokenRequestTimesOut) {
+  MockConfiguration configuration = kConfigurationValid;
+  configuration.delay_token_response = true;
+  RequestExpectations expectations = {RequestIdTokenStatus::kError,
+                                      FederatedAuthRequestResult::kError,
+                                      FETCH_ENDPOINT_ALL_REQUEST_ID_TOKEN};
+  // RunAuthTest() fast forwards time by a sufficient amount to cause the
+  // request to timeout. `MockConfiguration::delay_token_response` disables
+  // the auto-run logic for the token request response and enables emulating
+  // the server being very slow to return a token request response.
+  RunAuthTest(kDefaultRequestParameters, expectations, configuration);
+
+  // Resolve token request. The callback should not be called.
+  test_network_request_manager_->RunDelayedCallbacks();
 }
 
 class FederatedAuthRequestImplTestCancelConsistency
