c4041119cffd6b3c1438f26c399b8af6a327d3a7
Tommy C. Li <tommycli@chromium.org>
2022-05-09 23:38:21 +0000

    [journeys] Move related searches coalescing from WebUI to cross-platform
    This logic is needed for Android too, so move this coalescing logic to
    cross-platform code.
    
    Bug: 1303171
    Change-Id: Ib6ea656606eff7541e74890476ee4951923d0c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635702
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001240}

--- components/history_clusters/core/on_device_clustering_backend.cc
+++ components/history_clusters/core/on_device_clustering_backend.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "components/history_clusters/core/on_device_clustering_util.h"
 #include "components/history_clusters/core/ranking_cluster_finalizer.h"
+#include "components/history_clusters/core/related_searches_cluster_finalizer.h"
 #include "components/history_clusters/core/similar_visit_deduper_cluster_finalizer.h"
 #include "components/history_clusters/core/single_visit_cluster_finalizer.h"
 #include "components/history_clusters/core/url_deduper_cluster_finalizer.h"
@@ -444,6 +445,8 @@ OnDeviceClusteringBackend::ClusterVisitsOnBackgroundThread(
   if (GetConfig().should_label_clusters) {
     cluster_finalizers.push_back(std::make_unique<LabelClusterFinalizer>());
   }
+  cluster_finalizers.push_back(
+      std::make_unique<RelatedSearchesClusterFinalizer>());
 
   // Group visits into clusters.
   std::vector<history::Cluster> clusters =
