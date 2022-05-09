86b81655bc7fc1aacd7850d8edf237996ede730f
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-05-09 12:55:07 +0000

    [DOMStorage] Introduce SerializedStorageKey type to protocol and migrate StorageId to it
    Handle optional securityOrigin and storageKey in the backend
    
    Bug: 1296582
    Change-Id: I6454fe804834dd623d2ed048cf37b1751ea32f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447504
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000970}

--- third_party/blink/renderer/modules/storage/inspector_dom_storage_agent.cc
+++ third_party/blink/renderer/modules/storage/inspector_dom_storage_agent.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
 #include "third_party/blink/renderer/core/inspector/inspected_frames.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/modules/storage/cached_storage_area.h"
@@ -176,10 +177,24 @@ Response InspectorDOMStorageAgent::removeDOMStorageItem(
   return ToResponse(exception_state);
 }
 
+Response InspectorDOMStorageAgent::getStorageKeyForFrame(
+    const String& frame_id,
+    String* serialized_storage_key) {
+  LocalFrame* frame =
+      IdentifiersFactory::FrameById(inspected_frames_, frame_id);
+  if (!frame)
+    return Response::ServerError("Frame not found");
+  *serialized_storage_key = WTF::String(
+      static_cast<StorageKey>(frame->DomWindow()->GetStorageKey()).Serialize());
+  return Response::Success();
+}
+
 std::unique_ptr<protocol::DOMStorage::StorageId>
 InspectorDOMStorageAgent::GetStorageId(const BlinkStorageKey& storage_key,
                                        bool is_local_storage) {
   return protocol::DOMStorage::StorageId::create()
+      .setStorageKey(
+          WTF::String(static_cast<StorageKey>(storage_key).Serialize()))
       .setSecurityOrigin(storage_key.GetSecurityOrigin()->ToRawString())
       .setIsLocalStorage(is_local_storage)
       .build();
@@ -208,13 +223,34 @@ void InspectorDOMStorageAgent::DidDispatchDOMStorageEvent(
                                          new_value);
 }
 
+namespace {
+LocalFrame* FrameWithStorageKey(const String& key_raw_string,
+                                InspectedFrames& frames) {
+  for (LocalFrame* frame : frames) {
+    // any frame with given storage key would do, as it's only needed to satisfy
+    // the current API
+    if (static_cast<StorageKey>(frame->DomWindow()->GetStorageKey())
+            .Serialize() == key_raw_string.Utf8())
+      return frame;
+  }
+  return nullptr;
+}
+}  // namespace
+
 Response InspectorDOMStorageAgent::FindStorageArea(
     std::unique_ptr<protocol::DOMStorage::StorageId> storage_id,
     StorageArea*& storage_area) {
-  String security_origin = storage_id->getSecurityOrigin();
+  String security_origin = storage_id->getSecurityOrigin("");
+  String storage_key = storage_id->getStorageKey("");
   bool is_local_storage = storage_id->getIsLocalStorage();
-  LocalFrame* frame =
-      inspected_frames_->FrameWithSecurityOrigin(security_origin);
+  LocalFrame* frame = nullptr;
+  // TODO(crbug.com/1296582) Prioritize storageKey once everything is ready
+  if (security_origin) {
+    frame = inspected_frames_->FrameWithSecurityOrigin(security_origin);
+  } else if (storage_key) {
+    frame = FrameWithStorageKey(storage_key, *inspected_frames_);
+  }
+
   if (!frame) {
     return Response::ServerError(
         "Frame not found for the given security origin");
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-storage-key-functionality.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp} = await testRunner.startBlank(
+      `Tests DOMStorage functionality with storageKey\n`);
+
+  await dp.DOMStorage.enable();
+  await dp.Page.enable();
+
+  const addedPromise = dp.DOMStorage.onceDomStorageItemAdded();
+  window.localStorage.setItem("testKey", "testItem");
+  const event = await addedPromise;
+  const storageId = event.params.storageId;
+
+  testRunner.log("Set item")
+  testRunner.log(`storageId.storageKey: ${typeof storageId.storageKey}`)
+  testRunner.log(storageId.storageKey ? "not empty\n" : "empty\n");
+
+  const frameId = (await dp.Page.getResourceTree()).result.frameTree.frame.id;
+  const key = (await dp.DOMStorage.getStorageKeyForFrame({frameId: frameId})).result.storageKey;
+
+  testRunner.log("Get storage key by frame");
+  testRunner.log(`storageKey obtained: ${!!key}`)
+  testRunner.log(`storageKey in event  ${(key === storageId.storageKey) ? 'equal' : 'not equal'} to storage key by frame\n`);
+
+  const items = (await dp.DOMStorage.getDOMStorageItems({storageId: storageId})).result.entries;
+
+  testRunner.log("Get DOM storage items");
+  testRunner.log(`items array ${items?.length ? 'not empty' : 'empty'}`);
+  testRunner.log(`key: ${items[0][0]}, value: ${items[0][1]}`)
+
+  testRunner.completeTest();
+})
