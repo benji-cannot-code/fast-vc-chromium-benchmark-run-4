4e44d660bbf425018cf68c33c3242b58a2587ce6
Kam Kwankajornkiet <kamchonlathorn@google.com>
2022-05-09 05:25:47 +0000

    CCA: Add toggle in expert mode to enable FPS toggle
    Added an option in expert options to enable 30/60 fps, so the internal
    camera that have both can use the toggle in Video Resolution option to
    choose/test with the desired frame rate.
    
    Bug: b:221313040
    Test: Manually
    Change-Id: I2ee637563aea0571204e624b7c9d592b09139cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3625941
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000872}

--- ash/webui/camera_app_ui/resources/js/views/settings/video_resolution.ts
+++ ash/webui/camera_app_ui/resources/js/views/settings/video_resolution.ts
@@ -50,6 +50,11 @@ export class VideoResolutionSettings extends BaseSettings {
 
     this.cameraManager.addVideoResolutionOptionListener(
         (groups) => this.onOptionsUpdate(groups));
+
+    for (const s of [state.State.EXPERT,
+      state.State.ENABLE_FPS_PICKER_FOR_BUILTIN]) {
+      state.addObserver(s, () => this.toggleFPSPickerVisiblity());
+    }
   }
 
   private onOptionsUpdate(groups: VideoResolutionOptionGroup[]): void {
@@ -100,11 +105,17 @@ export class VideoResolutionSettings extends BaseSettings {
             SUPPORTED_CONSTANT_FPS.some((fps) => fps === fpsOption.constFps));
     const showFpsButton =
         constFpsOptions.length > 1 && facing === Facing.EXTERNAL;
+    const isFPSEnabled = state.get(state.State.EXPERT) &&
+      state.get(state.State.ENABLE_FPS_PICKER_FOR_BUILTIN);
     let resolution = new Resolution();
     for (const fps of SUPPORTED_CONSTANT_FPS) {
       const fpsButton =
           dom.getFrom(optionElement, `.fps-${fps}`, HTMLButtonElement);
-      fpsButton.hidden = !showFpsButton;
+      if (!isFPSEnabled) {
+        fpsButton.hidden = true;
+      } else if (!showFpsButton) {
+        fpsButton.classList.add('invisible');
+      }
 
       const fpsOption =
           option.fpsOptions.find((fpsOption) => fpsOption.constFps === fps);
@@ -150,6 +161,16 @@ export class VideoResolutionSettings extends BaseSettings {
 
     if (input.checked && this.focusedDeviceId === deviceId) {
       input.focus();
+    }
+  }
+
+  private toggleFPSPickerVisiblity(): void {
+    const isFPSEnabled = state.get(state.State.EXPERT) &&
+      state.get(state.State.ENABLE_FPS_PICKER_FOR_BUILTIN);
+    const fpsButtons = dom.getAllFrom(
+      this.menu, '.fps-buttons button', HTMLButtonElement);
+    for (const fpsButton of fpsButtons) {
+      fpsButton.hidden = !isFPSEnabled;
     }
   }
 }
