fab9002d06cb0cb8c037f7f2317446ba46f79025
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-05-09 18:25:00 +0000

    [TableFragmentation] Paint section/row backgrounds in cells correctly.
    This correctly calculates the part-rect when fragmentation is present.
    We use the OffsetInStitchedFragments to currently get the part size,
    and offset, and use that to correctly paint the table section/row
    backgrounds in the cell.
    
    There shouldn't be any significant performance delta due to this patch.
    
    Bug: 1078927
    Change-Id: I05eb09e9221005c4da9404508317c1bf99687cce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632829
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001100}

--- third_party/blink/renderer/core/paint/ng/ng_table_painters.cc
+++ third_party/blink/renderer/core/paint/ng/ng_table_painters.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/geometry/writing_mode_converter.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_link.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/table/layout_ng_table.h"
@@ -561,6 +562,13 @@ void NGTableSectionPainter::PaintBoxDecorationBackground(
         paint_info, paint_rect, fragment_.Style(), PhysicalBoxSides(),
         !box_decoration_data.ShouldPaintBackground());
   }
+
+  // If we are fragmented - determine the total part size, relative to the
+  // current fragment.
+  PhysicalRect part_rect = paint_rect;
+  if (!fragment_.IsOnlyForNode())
+    part_rect.offset -= OffsetInStitchedFragments(fragment_, &part_rect.size);
+
   for (const NGLink& child : fragment_.Children()) {
     const auto& child_fragment = *child;
     DCHECK(child_fragment.IsBox());
@@ -568,7 +576,7 @@ void NGTableSectionPainter::PaintBoxDecorationBackground(
       continue;
     NGTableRowPainter(To<NGPhysicalBoxFragment>(child_fragment))
         .PaintTablePartBackgroundIntoCells(
-            paint_info, *To<LayoutBox>(fragment_.GetLayoutObject()), paint_rect,
+            paint_info, *To<LayoutBox>(fragment_.GetLayoutObject()), part_rect,
             paint_rect.offset + child.offset);
   }
   if (box_decoration_data.ShouldPaintShadow()) {
@@ -602,9 +610,15 @@ void NGTableRowPainter::PaintBoxDecorationBackground(
         !box_decoration_data.ShouldPaintBackground());
   }
 
+  // If we are fragmented - determine the total part size, relative to the
+  // current fragment.
+  PhysicalRect part_rect = paint_rect;
+  if (!fragment_.IsOnlyForNode())
+    part_rect.offset -= OffsetInStitchedFragments(fragment_, &part_rect.size);
+
   PaintTablePartBackgroundIntoCells(paint_info,
                                     *To<LayoutBox>(fragment_.GetLayoutObject()),
-                                    paint_rect, paint_rect.offset);
+                                    part_rect, paint_rect.offset);
   if (box_decoration_data.ShouldPaintShadow()) {
     BoxPainterBase::PaintInsetBoxShadowWithInnerRect(paint_info, paint_rect,
                                                      fragment_.Style());
