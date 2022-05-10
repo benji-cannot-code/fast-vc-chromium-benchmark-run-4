9dbee46f93ceb161477881595732f75542cd340f
Nan Lin <linnan@chromium.org>
2022-05-10 18:27:41 +0000

    Enforce separate storage limit for aggregatable reports
    https://github.com/WICG/conversion-measurement-api/pull/420
    
    Change-Id: Ie652e21387c75d0c13298b28f96c466c6f726faf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638643
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001620}

--- content/browser/attribution_reporting/attribution_storage_sql.h
+++ content/browser/attribution_reporting/attribution_storage_sql.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
+#include "content/browser/attribution_reporting/attribution_report.h"
 #include "content/browser/attribution_reporting/attribution_storage.h"
 #include "content/browser/attribution_reporting/attribution_trigger.h"
 #include "content/browser/attribution_reporting/rate_limit_table.h"
@@ -167,8 +168,9 @@ class CONTENT_EXPORT AttributionStorageSql : public AttributionStorage {
     kError,
   };
 
-  ConversionCapacityStatus CapacityForStoringReport(const AttributionTrigger&)
-      VALID_CONTEXT_REQUIRED(sequence_checker_);
+  ConversionCapacityStatus CapacityForStoringReport(
+      const AttributionTrigger&,
+      AttributionReport::ReportType) VALID_CONTEXT_REQUIRED(sequence_checker_);
 
   enum class MaybeReplaceLowerPriorityEventLevelReportResult {
     kError,
