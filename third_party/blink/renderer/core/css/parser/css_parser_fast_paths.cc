81ab65eefdac4a2f5a17b28736d02ac9bd3f3899
Khushal <khushalsagar@chromium.org>
2022-05-10 01:32:14 +0000

    blink: Remove object-overflow CSS property.
    This change reverts the addition of object-overflow applied to replaced
    elements. With the resolution on [1], the functionality will be
    supported using the existing CSS overflow property.
    
    Also change the runtime flag name to CSSObjectViewBox since the flag
    only enables the use of the new object-view-box CSS property.
    
    [1]: https://github.com/w3c/csswg-drafts/issues/7144
    
    R=vmpstr@chromium.org
    
    Bug: 1303102
    Change-Id: Id7d9471bb68fd37d0747a2cdc92d389685ddafaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605930
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001288}

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -935,8 +935,6 @@ bool CSSParserFastPaths::IsValidKeywordPropertyAndValue(
              value_id == CSSValueID::kContain ||
              value_id == CSSValueID::kCover || value_id == CSSValueID::kNone ||
              value_id == CSSValueID::kScaleDown;
-    case CSSPropertyID::kObjectOverflow:
-      return value_id == CSSValueID::kClip || value_id == CSSValueID::kVisible;
     case CSSPropertyID::kOutlineStyle:
       return value_id == CSSValueID::kAuto || value_id == CSSValueID::kNone ||
              (value_id >= CSSValueID::kInset &&
@@ -1316,7 +1314,6 @@ bool CSSParserFastPaths::IsKeywordPropertyID(CSSPropertyID property_id) {
     case CSSPropertyID::kMathShift:
     case CSSPropertyID::kMathStyle:
     case CSSPropertyID::kObjectFit:
-    case CSSPropertyID::kObjectOverflow:
     case CSSPropertyID::kOutlineStyle:
     case CSSPropertyID::kOverflowAnchor:
     case CSSPropertyID::kOverflowBlock:
--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -4881,13 +4881,6 @@ const CSSValue* ObjectFit::CSSValueFromComputedStyleInternal(
   return CSSIdentifierValue::Create(style.GetObjectFit());
 }
 
-const CSSValue* ObjectOverflow::CSSValueFromComputedStyleInternal(
-    const ComputedStyle& style,
-    const LayoutObject*,
-    bool allow_visited_style) const {
-  return CSSIdentifierValue::Create(style.GetObjectOverflow());
-}
-
 const CSSValue* ObjectPosition::ParseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext& context,
@@ -4913,7 +4906,7 @@ const CSSValue* ObjectViewBox::ParseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext& context,
     const CSSParserLocalContext&) const {
-  DCHECK(RuntimeEnabledFeatures::CSSObjectViewBoxAndOverflowEnabled());
+  DCHECK(RuntimeEnabledFeatures::CSSObjectViewBoxEnabled());
 
   if (range.Peek().Id() == CSSValueID::kNone)
     return css_parsing_utils::ConsumeIdent(range);
@@ -4928,7 +4921,7 @@ const CSSValue* ObjectViewBox::CSSValueFromComputedStyleInternal(
     const ComputedStyle& style,
     const LayoutObject*,
     bool allow_visited_style) const {
-  if (!RuntimeEnabledFeatures::CSSObjectViewBoxAndOverflowEnabled()) {
+  if (!RuntimeEnabledFeatures::CSSObjectViewBoxEnabled()) {
     DCHECK(!style.ObjectViewBox());
     return CSSIdentifierValue::Create(CSSValueID::kNone);
   }
