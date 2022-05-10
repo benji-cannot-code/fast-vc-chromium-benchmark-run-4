54ac50873666bf4855a357cb821bfc1290e0d198
Wez <wez@chromium.org>
2022-05-10 17:15:30 +0000

    [base][fuchsia] Use correct scheduler capacities for audio & display
    Bug: 1324230
    Change-Id: I91da4aeccd6f160b33cd9f3de4749e10b801d5d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637995
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001558}

--- base/threading/platform_thread_fuchsia.cc
+++ base/threading/platform_thread_fuchsia.cc
@@ -118,12 +118,13 @@ void PlatformThread::SetCurrentThreadPriorityImpl(ThreadPriority priority) {
 
     case ThreadPriority::DISPLAY:
       ScheduleAsMediaThread("chromium.base.threading.display",
-                            kDisplaySchedulingPeriod, kAudioSchedulingCapacity);
+                            kDisplaySchedulingPeriod,
+                            kDisplaySchedulingCapacity);
       break;
 
     case ThreadPriority::REALTIME_AUDIO:
       ScheduleAsMediaThread("chromium.base.threading.realtime-audio",
-                            kAudioSchedulingPeriod, kDisplaySchedulingCapacity);
+                            kAudioSchedulingPeriod, kAudioSchedulingCapacity);
       break;
   }
 
