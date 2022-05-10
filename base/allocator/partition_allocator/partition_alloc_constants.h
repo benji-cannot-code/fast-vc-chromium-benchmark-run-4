9f636581ca6feace470083bab5c3fd1684e4b303
Kalvin Lee <kdlee@chromium.org>
2022-05-10 21:21:23 +0000

    [PA] Namespace migration (35-omake of N)
    This change adds an alias for
    `partition_alloc::internal::PartitionPageSize()`, whose usage is common
    enough outside PA that it seems wise to publicize.
    
    Bug: 1288247
    Change-Id: Iad44f731b545dffd34fb76d1668f80bfa428bc58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3636240
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001669}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -430,6 +430,10 @@ constexpr size_t kInvalidBucketSize = 1;
 
 }  // namespace internal
 
+// This function is used often enough to be worth publicizing outside the
+// `internal` namespace.
+using ::partition_alloc::internal::PartitionPageSize;
+
 }  // namespace partition_alloc
 
 namespace base {
@@ -487,7 +491,6 @@ using ::partition_alloc::internal::NumSystemPagesPerPartitionPage;
 using ::partition_alloc::internal::PartitionPageBaseMask;
 using ::partition_alloc::internal::PartitionPageOffsetMask;
 using ::partition_alloc::internal::PartitionPageShift;
-using ::partition_alloc::internal::PartitionPageSize;
 
 }  // namespace base
 
