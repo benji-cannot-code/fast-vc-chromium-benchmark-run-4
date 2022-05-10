e0fd33f223925f73900765c3a987c337fafe3f12
Miriam Polzer <mpolzer@google.com>
2022-05-10 09:09:45 +0000

    Prevent additional Chrome restart on Chrome OS
    If AttemptRelanuch is called before this change both Chrome and
    Chrome OS are restarted. The Chrome OS shutdown will stop Chrome itself,
    so there's no reason to shutdown Chrome except for faked dbus clients.
    
    For those, move a faked shutdown of Chrome into fake power manager.
    As that changes call order, two tests that relied on the previous
    behavior need to be adapted.
    
    This change should also remove flakiness of
    EnableDebuggingDevTest.ShowAndRemoveProtection.
    
    Also remove RebootPolicy, it was only used for flash updates which don't
    exist anymore.
    
    Bug: b/216129138
    Change-Id: Ia1dddadc9d4e56495a96c841b5feccb54b6a2a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506307
    Commit-Queue: Miriam Polzer <mpolzer@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001417}

--- chrome/browser/lifetime/termination_notification.cc
+++ chrome/browser/lifetime/termination_notification.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/components/login/session/session_termination_manager.h"
+#include "chrome/browser/lifetime/application_lifetime_chromeos.h"
 #include "chromeos/dbus/dbus_thread_manager.h"  // nogncheck
 #include "chromeos/dbus/power/power_policy_controller.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
@@ -24,16 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_shutdown {
 namespace {
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-chromeos::UpdateEngineClient* GetUpdateEngineClient() {
-  DCHECK(chromeos::DBusThreadManager::IsInitialized());
-  auto* update_engine_client =
-      chromeos::DBusThreadManager::Get()->GetUpdateEngineClient();
-  DCHECK(update_engine_client);
-  return update_engine_client;
-}
-#endif
 
 base::OnceClosureList& GetAppTerminatingCallbackList() {
   static base::NoDestructor<base::OnceClosureList> callback_list;
@@ -62,10 +53,6 @@ void NotifyAppTerminating() {
 }
 
 void NotifyAndTerminate(bool fast_path) {
-  NotifyAndTerminate(fast_path, RebootPolicy::kOptionalReboot);
-}
-
-void NotifyAndTerminate(bool fast_path, RebootPolicy reboot_policy) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   static bool notified = false;
   // Return if a shutdown request has already been sent.
@@ -81,9 +68,8 @@ void NotifyAndTerminate(bool fast_path, RebootPolicy reboot_policy) {
   if (chromeos::PowerPolicyController::IsInitialized())
     chromeos::PowerPolicyController::Get()->NotifyChromeIsExiting();
 
-  // Reboot if an update has been applied.
-  if (UpdatePending() || reboot_policy == RebootPolicy::kForceReboot) {
-    GetUpdateEngineClient()->RebootAfterUpdate();
+  if (chrome::UpdatePending()) {
+    chrome::RelaunchForUpdate();
     return;
   }
 
@@ -95,15 +81,5 @@ void NotifyAndTerminate(bool fast_path, RebootPolicy reboot_policy) {
   }
 #endif
 }
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-bool UpdatePending() {
-  if (!chromeos::DBusThreadManager::IsInitialized())
-    return false;
-
-  return GetUpdateEngineClient()->GetLastStatus().current_operation() ==
-         update_engine::Operation::UPDATED_NEED_REBOOT;
-}
-#endif
 
 }  // namespace browser_shutdown
