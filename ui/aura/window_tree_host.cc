193d8b09db16b2264cf2b73e2c7317d650a8b6ac
Mitsuru Oshima <oshima@chromium.org>
2022-05-10 22:29:12 +0000

    Rename method names in WindowTreeHost to be more accurate.
    This method updates the root window bounds which is in DP. Exactly how will be platform specific. (on wayland, it'll use the platfrom window size in dp). This CL also makes this protected per comment.
    UpdateRootWindowSizeInPixels =>
    UpdateRootWindowSize
    
    This method returns bounds in DP using pixel size.
    GetTransformedRootWindowBoundsInPixels =>
    GetTransformedRootWindowBoundsFromPixelSize
    
    Bug: 1318598
    Change-Id: If8a4b95812904d744f427ac17e3617ffff1a7f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635298
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001761}

--- ui/aura/window_tree_host.cc
+++ ui/aura/window_tree_host.cc
@@ -194,7 +194,7 @@ void WindowTreeHost::InitHost() {
       display::Screen::GetScreen()->GetDisplayNearestWindow(window());
   device_scale_factor_ = display.device_scale_factor();
 
-  UpdateRootWindowSizeInPixels();
+  UpdateRootWindowSize();
   InitCompositor();
   Env::GetInstance()->NotifyHostInitialized(this);
 }
@@ -224,7 +224,7 @@ gfx::Transform WindowTreeHost::GetRootTransform() const {
 
 void WindowTreeHost::SetRootTransform(const gfx::Transform& transform) {
   window()->SetTransform(transform);
-  UpdateRootWindowSizeInPixels();
+  UpdateRootWindowSize();
 }
 
 gfx::Transform WindowTreeHost::GetInverseRootTransform() const {
@@ -255,16 +255,6 @@ gfx::Transform WindowTreeHost::GetInverseRootTransformForLocalEventCoordinates()
   if (!transform.GetInverse(&invert))
     return transform;
   return invert;
-}
-
-void WindowTreeHost::UpdateRootWindowSizeInPixels() {
-  // Validate that the LocalSurfaceId does not change.
-  bool compositor_inited = !!compositor()->root_layer();
-  ScopedLocalSurfaceIdValidator lsi_validator(compositor_inited ? window()
-                                                                : nullptr);
-  gfx::Rect transformed_bounds_in_pixels =
-      GetTransformedRootWindowBoundsInPixels(GetBoundsInPixels().size());
-  window()->SetBounds(transformed_bounds_in_pixels);
 }
 
 void WindowTreeHost::UpdateCompositorScaleAndSize(
@@ -462,6 +452,16 @@ void WindowTreeHost::SetNativeWindowOcclusionState(
 
   for (WindowTreeHostObserver& observer : observers_)
     observer.OnOcclusionStateChanged(this, state, occluded_region);
+}
+
+void WindowTreeHost::UpdateRootWindowSize() {
+  // Validate that the LocalSurfaceId does not change.
+  bool compositor_inited = !!compositor()->root_layer();
+  ScopedLocalSurfaceIdValidator lsi_validator(compositor_inited ? window()
+                                                                : nullptr);
+  gfx::Rect transformed_bounds_in_dp =
+      GetTransformedRootWindowBoundsFromPixelSize(GetBoundsInPixels().size());
+  window()->SetBounds(transformed_bounds_in_dp);
 }
 
 std::unique_ptr<ScopedEnableUnadjustedMouseEvents>
@@ -680,7 +680,7 @@ void WindowTreeHost::OnHostResizedInPixels(
   if (display.is_valid())
     device_scale_factor_ = display.device_scale_factor();
 
-  UpdateRootWindowSizeInPixels();
+  UpdateRootWindowSize();
 
   // Passing |new_size_in_pixels| to set compositor size. It could be different
   // from GetBoundsInPixels() on Windows to contain extra space for window
@@ -738,7 +738,7 @@ void WindowTreeHost::OnDisplayMetricsChanged(const display::Display& display,
 #endif
 }
 
-gfx::Rect WindowTreeHost::GetTransformedRootWindowBoundsInPixels(
+gfx::Rect WindowTreeHost::GetTransformedRootWindowBoundsFromPixelSize(
     const gfx::Size& size_in_pixels) const {
   gfx::RectF new_bounds = gfx::RectF(gfx::Rect(size_in_pixels));
   GetInverseRootTransform().TransformRect(&new_bounds);
--- ui/aura/window_tree_host.h
+++ ui/aura/window_tree_host.h
@@ -132,13 +132,6 @@ class AURA_EXPORT WindowTreeHost : public ui::internal::InputMethodDelegate,
   virtual gfx::Transform GetInverseRootTransformForLocalEventCoordinates()
       const;
 
-  // Updates the root window's size using |host_size_in_pixels|, current
-  // transform and outsets.
-  // TODO(ccameron): Make this function no longer public. The interaction
-  // between this call, GetBounds, and OnHostResizedInPixels is ambiguous and
-  // allows for inconsistencies.
-  void UpdateRootWindowSizeInPixels();
-
   // Updates the compositor's size and scale from |new_size_in_pixels|,
   // |device_scale_factor_| and the compositor's transform hint.
   void UpdateCompositorScaleAndSize(const gfx::Size& new_size_in_pixels);
@@ -388,7 +381,8 @@ class AURA_EXPORT WindowTreeHost : public ui::internal::InputMethodDelegate,
   // True if |dom_code| is reserved for an active KeyboardLock request.
   virtual bool IsKeyLocked(ui::DomCode dom_code) = 0;
 
-  virtual gfx::Rect GetTransformedRootWindowBoundsInPixels(
+  // Return root window size computed from given pixel size.
+  virtual gfx::Rect GetTransformedRootWindowBoundsFromPixelSize(
       const gfx::Size& size_in_pixels) const;
 
   const base::ObserverList<WindowTreeHostObserver>::Unchecked& observers()
@@ -398,6 +392,9 @@ class AURA_EXPORT WindowTreeHost : public ui::internal::InputMethodDelegate,
 
   // Called to enabled/disable native window occlusion calculation.
   void SetNativeWindowOcclusionEnabled(bool enable);
+
+  // Updates the root window's size after WindowTreeHost's property changed.
+  void UpdateRootWindowSize();
 
  private:
   class HideHelper;
