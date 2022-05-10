b4ff6f1c778a5fd9d34b0b38f04ad9139afdaddd
Ahmed Mehfooz <amehfooz@chromium.org>
2022-05-10 21:56:52 +0000

    Reverse icons showing up in notification bar
    Show icons for notifications below the visible scroll view, before we
    reversed the order in message center notifications above scroll view
    were shown.
    Fix padding for collapsed state in NotificationsRefresh.
    
    Bug: 1316088
    Change-Id: I27114e2f45eea52e5b2b23f8bbb7a4125e33bb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635660
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001730}

--- ash/system/message_center/stacked_notification_bar.h
+++ ash/system/message_center/stacked_notification_bar.h
@@ -16,6 +16,7 @@ class Notification;
 }  // namespace message_center
 
 namespace views {
+class BoxLayout;
 class Label;
 }  // namespace views
 
@@ -109,6 +110,7 @@ class StackedNotificationBar : public views::View,
   views::View* const spacer_;
   views::Button* const clear_all_button_;
   views::Button* const expand_all_button_;
+  views::BoxLayout* const layout_manager_;
 
   base::WeakPtrFactory<StackedNotificationBar> weak_ptr_factory_{this};
 };
