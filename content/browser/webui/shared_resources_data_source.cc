87967ac4d1d773615999fbe32162baa37d5b6668
Yeunjoo Choi <ychoi@igalia.com>
2022-05-10 02:41:28 +0000

    Move //{ash=>chromeos/ash}/resources/*.grd
    This CL creates //chromeos/ash/resources as the destination of //chromeos/resources migration and moves grit resources in
    //ash/resources to //chromeos/ash/resources.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I042fa6b44b2deab6565077e91449910c5850122a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631855
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1001313}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
-#include "ash/grit/ash_resources.h"
-#include "ash/grit/ash_resources_map.h"
 #include "base/feature_list.h"
+#include "chromeos/ash/grit/ash_resources.h"
+#include "chromeos/ash/grit/ash_resources_map.h"
 #include "chromeos/grit/chromeos_resources.h"
 #include "chromeos/grit/chromeos_resources_map.h"
 #include "ui/chromeos/styles/cros_styles.h"  // nogncheck
