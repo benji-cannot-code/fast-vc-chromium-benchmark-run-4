9af95e972aeaf4b910444c37c296bd87427b839c
Fred Shih <ffred@chromium.org>
2022-05-10 23:24:12 +0000

    Revert "Don't maximize/fullscreen whe max size is set."
    This reverts commit be4554451312b98926ae8d458c15545db25a128c.
    
    Reason for revert: b/232035858 -- breaking borealis
    
    Original change's description:
    > Don't maximize/fullscreen whe max size is set.
    > Also fixes that did not explicitly set CanMaximize or CanResize even
    > though they depended on the behaviour.
    >
    > Test: covered by new WindowStateTest.ResizeSettingIsRespected.
    > Fixed: 1311484
    > Change-Id: Ia2650f2c0900e449d2805c977109fdbe547196ab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584030
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Fred Shih <ffred@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#997395}
    
    Change-Id: I3623eab6ce59803a7713e4b8280119e6429d50ac
    Bug: b/232035858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639591
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001788}

--- ash/wm/workspace_controller_unittest.cc
+++ ash/wm/workspace_controller_unittest.cc
@@ -91,8 +91,6 @@ class WorkspaceControllerTest : public AshTestBase {
     window->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_NORMAL);
     window->SetType(aura::client::WINDOW_TYPE_NORMAL);
     window->Init(ui::LAYER_TEXTURED);
-    window->SetProperty(aura::client::kResizeBehaviorKey,
-                        aura::client::kResizeBehaviorCanMaximize);
     return window;
   }
 
@@ -101,8 +99,6 @@ class WorkspaceControllerTest : public AshTestBase {
     window->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_NORMAL);
     window->SetType(aura::client::WINDOW_TYPE_NORMAL);
     window->Init(ui::LAYER_TEXTURED);
-    window->SetProperty(aura::client::kResizeBehaviorKey,
-                        aura::client::kResizeBehaviorCanMaximize);
     ParentWindowInPrimaryRootWindow(window);
     return window;
   }
