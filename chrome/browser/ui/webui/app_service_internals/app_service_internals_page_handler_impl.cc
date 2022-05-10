b1cd0871c656399ae167d13efccf0de06132d774
Tim Sergeant <tsergeant@chromium.org>
2022-05-10 07:37:28 +0000

    App Service: Disable chrome://app-service-internals in Incognito
    App Service is not available in Incognito mode, and using it does a
    DumpWithoutCrashing and gives you the App Service for the parent
    profile. Instead, we should check if the App Service is available, and
    display nothing if it is not.
    
    Bug: 1310115
    Change-Id: I9ca2cd4f00aabe0680044afaadfb5b7082dfbcf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637240
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001383}

--- chrome/browser/ui/webui/app_service_internals/app_service_internals_page_handler_impl.cc
+++ chrome/browser/ui/webui/app_service_internals/app_service_internals_page_handler_impl.cc
@@ -34,11 +34,14 @@ void AppServiceInternalsPageHandlerImpl::GetApps(GetAppsCallback callback) {
 
   std::vector<mojom::app_service_internals::AppInfoPtr> result;
 
-  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
-  if (!proxy) {
+  if (!apps::AppServiceProxyFactory::IsAppServiceAvailableForProfile(
+          profile_)) {
     std::move(callback).Run(std::move(result));
     return;
   }
+
+  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
+  DCHECK(proxy);
 
   proxy->AppRegistryCache().ForEachApp(
       [&result](const apps::AppUpdate& update) {
@@ -60,11 +63,14 @@ void AppServiceInternalsPageHandlerImpl::GetPreferredApps(
 
   std::vector<mojom::app_service_internals::PreferredAppInfoPtr> result;
 
-  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
-  if (!proxy) {
+  if (!apps::AppServiceProxyFactory::IsAppServiceAvailableForProfile(
+          profile_)) {
     std::move(callback).Run(std::move(result));
     return;
   }
+
+  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile_);
+  DCHECK(proxy);
 
   base::flat_map<std::string, std::stringstream> debug_info_map;
 
