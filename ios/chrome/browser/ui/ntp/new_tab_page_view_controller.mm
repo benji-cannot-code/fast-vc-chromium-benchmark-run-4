356e7136b20e212f18633ab99fb5f86b6846c119
Chris Lu <thegreenfrog@chromium.org>
2022-05-10 21:58:02 +0000

    [ios] Update ContentSuggestionsHeader top anchor constant upon rotation
    When the omnibox is pinned to the top, the fake omnibox is the one
    shown only in portrait mode, so if the NTP is opened in landscape mode, a
    rotation to portrait mode needs to update the top anchor constant based
    on the correct header height.
    
    Bug: 1316359
    Change-Id: I9118576f05c481a916ab68f84494364e9b4c4935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630828
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001732}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -67,6 +67,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // view.
 @property(nonatomic, strong)
     NSArray<NSLayoutConstraint*>* fakeOmniboxConstraints;
+// Constraint that pins the fake Omnibox to the top of the view. A subset of
+// |fakeOmniboxConstraints|.
+@property(nonatomic, strong) NSLayoutConstraint* headerTopAnchor;
 
 // Array of constraints used to lay out the fake Omnibox header above the
 // Content Suggestions, as opposed to pinning it to the top of the view in
@@ -285,6 +288,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   if (previousTraitCollection.horizontalSizeClass !=
       self.traitCollection.horizontalSizeClass) {
+    // Update header constant to cover rotation instances. When the omnibox is
+    // pinned to the top, the fake omnibox is the one shown only in portrait
+    // mode, so if the NTP is opened in landscape mode, a rotation to portrait
+    // mode needs to update the top anchor constant based on the correct header
+    // height.
+    self.headerTopAnchor.constant =
+        -([self stickyOmniboxHeight] + [self feedHeaderHeight]);
     [[self contentSuggestionsViewController].view setNeedsLayout];
     [[self contentSuggestionsViewController].view layoutIfNeeded];
     [self.ntpContentDelegate reloadContentSuggestions];
@@ -575,7 +585,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Takes the height of the entire header and subtracts the margin to stick the
   // fake omnibox. Adjusts this for the device by further subtracting the
   // toolbar height and safe area insets.
-  return self.headerController.view.frame.size.height -
+  return [self.headerController headerHeight] -
          ntp_header::kFakeOmniboxScrolledToTopMargin -
          ToolbarExpandedHeight(
              [UIApplication sharedApplication].preferredContentSizeCategory) -
@@ -676,12 +686,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.view addSubview:self.headerController.view];
 
   if (IsContentSuggestionsHeaderMigrationEnabled()) {
+    self.headerTopAnchor = [self.headerController.view.topAnchor
+        constraintEqualToAnchor:self.discoverFeedWrapperViewController.view
+                                    .topAnchor
+                       constant:-([self stickyOmniboxHeight] +
+                                  [self feedHeaderHeight])];
+    // This issue fundamentally comes down to the topAnchor being set just once
+    // and if it is set in landscape mode, it never is updated upon rotation.
+    // And landscape is when it doesn't matter.
     self.fakeOmniboxConstraints = @[
-      [self.headerController.view.topAnchor
-          constraintEqualToAnchor:self.discoverFeedWrapperViewController.view
-                                      .topAnchor
-                         constant:-([self stickyOmniboxHeight] +
-                                    [self feedHeaderHeight])],
+      self.headerTopAnchor,
       [self.headerController.view.leadingAnchor
           constraintEqualToAnchor:self.discoverFeedWrapperViewController.view
                                       .leadingAnchor],
