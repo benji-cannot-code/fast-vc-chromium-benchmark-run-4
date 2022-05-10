b94ad25d3fdcfac703c23eec110edafc5832564b
Andrey Kosyakov <caseq@chromium.org>
2022-05-10 00:12:26 +0000

    [Upstream VT 3/n] Ensure ApplyVirtualTimePolicy() is only called with VT enabled
    Also, do not store initial virtual time and ticks as part of VT
    state in MainThreadSchedulerImpl, just pass it to the
    AutoAdvancingVirtualTimeDomain instantly.
    
    Change-Id: I613ba103e88b40547431b13565bf1ba3a318b033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487334
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001259}

--- third_party/blink/renderer/platform/scheduler/main_thread/auto_advancing_virtual_time_domain.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/auto_advancing_virtual_time_domain.h
@@ -68,6 +68,7 @@ class PLATFORM_EXPORT AutoAdvancingVirtualTimeDomain
 
   int task_starvation_count() const { return task_starvation_count_; }
 
+  base::TimeTicks InitialTicks() const { return initial_time_ticks_; }
   // TickClock implementation:
   base::TimeTicks NowTicks() const override;
 
