22ee44b70de4e6d3cc5fab033b4fcc52ede53302
Andrew Paseltiner <apaseltiner@chromium.org>
2022-05-10 23:47:53 +0000

    Add rudimentary "unread" indicator to attribution internals tabs
    Screenshot of changes in crbug.com/1324280#c1.
    
    We will investigate additional enhancements in followup CLs.
    
    Bug: 1324280
    Change-Id: I5b20a8ae4ae517d9f85d8e8913d7ea4e81888047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638664
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001802}

--- content/browser/resources/attribution_reporting/attribution_internals.js
+++ content/browser/resources/attribution_reporting/attribution_internals.js
@@ -1107,6 +1107,24 @@ class Observer {
   }
 }
 
+/**
+ * @param {!TableModel<?>} model
+ * @param {!Element} tab
+ */
+function installUnreadIndicator(model, tab) {
+  model.rowsChangedListeners.add(() => {
+    if (!tab.selected) {
+      tab.classList.add('unread');
+    }
+  });
+
+  tab.addEventListener('selectedChange', () => {
+    if (tab.selected) {
+      tab.classList.remove('unread');
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   // Setup the mojo interface.
   pageHandler = AttributionInternalsHandler.getRemote();
@@ -1120,6 +1138,15 @@ document.addEventListener('DOMContentLoaded', function() {
       new AggregatableAttributionReportTableModel(
           getRequiredElement('show-debug-aggregatable-reports'),
           getRequiredElement('send-aggregatable-reports'));
+
+  installUnreadIndicator(sourceTableModel, getRequiredElement('sources-tab'));
+  installUnreadIndicator(triggerTableModel, getRequiredElement('triggers-tab'));
+  installUnreadIndicator(
+      eventLevelReportTableModel,
+      getRequiredElement('event-level-reports-tab'));
+  installUnreadIndicator(
+      aggregatableAttributionReportTableModel,
+      getRequiredElement('aggregatable-reports-tab'));
 
   $('refresh').addEventListener('click', updatePageData);
   $('clear-data').addEventListener('click', clearStorage);
