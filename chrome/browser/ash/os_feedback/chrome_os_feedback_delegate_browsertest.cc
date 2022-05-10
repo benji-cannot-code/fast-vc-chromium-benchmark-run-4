06aab3456fa75d8581de53417de640d02ec0b51f
xiangdong kong <xiangdongkong@google.com>
2022-05-10 21:53:58 +0000

    feedback: Include screenshot in report
    The screenshot should have been taken and stored in C++ side. Otherwise,
    the user won't be able to select it. See [2] for a sample report with
    screenshot submitted using this patch.
    
    In order to share the screenshot, the user has to opt in. We have gotten
    privacy approval [1].
    
    1. https://bugs.chromium.org/p/chromium/issues/detail?id=1151153.
    2. https://listnr.corp.google.com/report/89964130301.
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*; browser_tests --gtest_filter=ChromeOsFeedbackDelegateTest.*;
    Change-Id: I5eac246996a88fb0291061ef0f6c11a7bbc74305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632987
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1001726}

--- chrome/browser/ash/os_feedback/chrome_os_feedback_delegate_browsertest.cc
+++ chrome/browser/ash/os_feedback/chrome_os_feedback_delegate_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
@@ -90,6 +91,14 @@ class ChromeOsFeedbackDelegateTest : public InProcessBrowserTest {
     auto feedback_delegate_ =
         std::make_unique<ChromeOsFeedbackDelegate>(profile_, std::move(mock));
 
+    scoped_refptr<base::RefCountedMemory> fake_png_data;
+    {
+      const unsigned char kData[] = {12, 11, 99};
+      fake_png_data =
+          base::MakeRefCounted<base::RefCountedBytes>(kData, std::size(kData));
+    }
+    feedback_delegate_->SetPngDataForTesting(std::move(fake_png_data));
+
     base::test::TestFuture<SendReportStatus> future;
     feedback_delegate_->SendReport(std::move(report), future.GetCallback());
 
@@ -127,14 +136,17 @@ IN_PROC_BROWSER_TEST_F(ChromeOsFeedbackDelegateTest, GetSignedInUserEmail) {
 }
 
 // Test that feedback params and data are populated with correct data before
-// passed to SendFeedback method of the feedback service .
+// passed to SendFeedback method of the feedback service.
+// - System logs and histograms are included.
+// - Screenshot is included.
 // TODO(xiangdongkong): Add tests for other flags once they are supported.
 // Currently, only load_system_info and send_histograms flags are implemented.
 IN_PROC_BROWSER_TEST_F(ChromeOsFeedbackDelegateTest,
-                       FeedbackDataPopulatedIncludeSysLogs) {
+                       FeedbackDataPopulatedIncludeSysLogsAndScreenshot) {
   ReportPtr report = Report::New();
   report->feedback_context = FeedbackContext::New();
   report->description = kDescription;
+  report->include_screenshot = true;
 
   report->include_system_logs_and_histograms = true;
   const FeedbackParams expected_params{/*is_internal_email=*/false,
@@ -149,17 +161,22 @@ IN_PROC_BROWSER_TEST_F(ChromeOsFeedbackDelegateTest,
   EXPECT_EQ("", feedback_data->user_email());
   EXPECT_EQ("", feedback_data->page_url());
   EXPECT_EQ(base::UTF16ToUTF8(kDescription), feedback_data->description());
+  // Verify screenshot is added to feedback data.
+  EXPECT_GT(feedback_data->image().size(), 0);
 }
 
 // Test that feedback params and data are populated with correct data before
-// passed to SendFeedback method of the feedback service .
+// passed to SendFeedback method of the feedback service.
+// - System logs and histograms are not included.
+// - Screenshot is not included.
 IN_PROC_BROWSER_TEST_F(ChromeOsFeedbackDelegateTest,
-                       FeedbackDataPopulatedNotIncludeSysLogs) {
+                       FeedbackDataPopulatedNotIncludeSysLogsOrScreenshot) {
   ReportPtr report = Report::New();
   report->feedback_context = FeedbackContext::New();
   report->feedback_context->email = kSignedInUserEmail;
   report->feedback_context->page_url = GURL(kPageUrl);
   report->description = kDescription;
+  report->include_screenshot = false;
 
   report->include_system_logs_and_histograms = false;
   const FeedbackParams expected_params{/*is_internal_email=*/false,
@@ -174,6 +191,8 @@ IN_PROC_BROWSER_TEST_F(ChromeOsFeedbackDelegateTest,
   EXPECT_EQ(kSignedInUserEmail, feedback_data->user_email());
   EXPECT_EQ(kPageUrl, feedback_data->page_url());
   EXPECT_EQ(base::UTF16ToUTF8(kDescription), feedback_data->description());
+  // Verify no screenshot is added to feedback data.
+  EXPECT_EQ("", feedback_data->image());
 }
 
 }  // namespace ash
