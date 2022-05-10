9a25e543b1dd36d0b77bea8fc333b5716ab50a2b
Valerie Young <spectranaut@igalia.com>
2022-05-10 23:31:23 +0000

    ax_dump_tree: support API specific directives in filters file
    Filters file can optionally include API prefixes, so a single filters
    file can contain filters for ATSPI, AXAPI, UIA or IA2. Example:
    ```
    @IA2-ALLOW:valuetext*
    @UIA-ALLOW:Value*
    @ATSPI-ALLOW:valuetext*
    @AXAPI-ALLOW:AXValue*
    ```
    
    Change-Id: I3ad5a406885c799721a82880a0d86b8962453f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533187
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001794}

--- tools/accessibility/inspect/ax_utils.h
+++ tools/accessibility/inspect/ax_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/accessibility/platform/inspect/ax_api_type.h"
 #include "ui/accessibility/platform/inspect/ax_inspect.h"
 #include "ui/accessibility/platform/inspect/ax_inspect_scenario.h"
 
@@ -33,7 +34,8 @@ absl::optional<ui::AXTreeSelector> TreeSelectorFromCommandLine(
 // Returns inspect scenario from the command line arguments. Returns nullopt in
 // case of error.
 absl::optional<ui::AXInspectScenario> ScenarioFromCommandLine(
-    const base::CommandLine& command_line);
+    const base::CommandLine& command_line,
+    ui::AXApiType::Type api = ui::AXApiType::kNone);
 
 }  // namespace tools
 
