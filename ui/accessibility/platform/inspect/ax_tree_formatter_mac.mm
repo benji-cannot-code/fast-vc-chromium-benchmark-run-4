41beab41868635f82e76d8bdb9e65465516261e3
Alexander Surkov <asurkov@igalia.com>
2022-05-10 17:58:25 +0000

    ax_test: dump AXSubrole by default
    AXRole is often not very descriptive to identify an accessible object,
    dumping AXSubrole by default makes output more distinctive.
    
    The majority of tests define AXSubrole explicitly anyway, which can
    be removed now.
    
    It also helps ax_dump_tree tool. Now you don't have to deal with
    filters to make the tool useful.
    
    Change-Id: Ib5117ef54665c74cd84e3170047dfcfa0df0e2b1
    Bug: 1324146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637837
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1001602}

--- ui/accessibility/platform/inspect/ax_tree_formatter_mac.mm
+++ ui/accessibility/platform/inspect/ax_tree_formatter_mac.mm
@@ -47,8 +47,8 @@ AXTreeFormatterMac::~AXTreeFormatterMac() = default;
 void AXTreeFormatterMac::AddDefaultFilters(
     std::vector<AXPropertyFilter>* property_filters) {
   static NSArray* default_attributes = [@[
-    @"AXAutocompleteValue=*", @"AXDescription=*", @"AXRole=*", @"AXTitle=*",
-    @"AXTitleUIElement=*", @"AXHelp=*", @"AXValue=*"
+    @"AXAutocompleteValue", @"AXDescription", @"AXRole", @"AXSubrole",
+    @"AXTitle", @"AXTitleUIElement", @"AXHelp", @"AXValue"
   ] retain];
 
   for (NSString* attribute : default_attributes) {
