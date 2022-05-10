65abd94a22f7b7908f6aa091030df1bf9bed7f8f
Mehrab N <mehrab@chromium.org>
2022-05-10 07:57:20 +0000

    emoji-picker: add clear history feat for emoticons
    Presently, emoticon usage history (recently used emoticons) is shown
    to the user, but there is no feature to allow the user to clear
    this history. The feature to clear emoticon history is now added.
    
    As part of the impolementation, given emoticon is almost the same as
    emoji group with few changes, most of the new UIs are again brought
    to emoticon group from emoji group. In future, the implementation
    can be improved by generalizing and unifying one implementation for
    the both use-cases.
    
    Bug: b:216194842
    Change-Id: Ia67c6973518311bb2c618a583510f90274864212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627424
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Mehrab N <mehrab@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001393}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
@@ -215,7 +215,7 @@ export class EmojiPicker extends PolymerElement {
     this.addEventListener(
         EMOJI_BUTTON_CLICK, (ev) => this.onEmojiButtonClick(ev));
     this.addEventListener(
-        EMOJI_CLEAR_RECENTS_CLICK, ev => this.clearRecentEmoji());
+        EMOJI_CLEAR_RECENTS_CLICK, ev => this.clearRecentEmoji(ev));
     // variant popup related handlers
     this.addEventListener(
         EMOJI_VARIANTS_SHOWN,
@@ -378,7 +378,7 @@ export class EmojiPicker extends PolymerElement {
           break;
 
         default:
-          throw new Error('Unknown category');
+          throw new Error(`Unknown category "${category}."`);
       }
     }
     const searchLength =
@@ -392,9 +392,24 @@ export class EmojiPicker extends PolymerElement {
     this.apiProxy_.insertEmoji(text, isVariant, searchLength);
   }
 
-  clearRecentEmoji() {
-    this.set(['emojiHistory', 'emoji'], makeRecentlyUsed([]));
-    this.recentEmojiStore.clearRecents();
+  clearRecentEmoji(event) {
+    switch (event.detail.category) {
+      case CategoryEnum.EMOJI:
+        this.set(['emojiHistory', 'emoji'], makeRecentlyUsed([]));
+        this.recentEmojiStore.clearRecents();
+        break;
+
+      case CategoryEnum.EMOTICON:
+        this.set(['emoticonHistory', 'emoji'], makeRecentlyUsed([]));
+        this.recentEmoticonStore.clearRecents();
+        break;
+
+      default:
+        throw new Error(
+          'Clear history logic is not implemented ' +
+          `for category "${this.category}."`);
+    }
+
     afterNextRender(
         this, () => {
           this.updateActiveGroup(/*updateTabsScroll=*/ true);
@@ -675,6 +690,15 @@ export class EmojiPicker extends PolymerElement {
       this.shadowRoot.querySelector(`div[data-group="history"]`)
           .querySelector('emoji-group')
           .showClearRecents = false;
+    }
+
+    if (this.emoticonHistory.emoji.length > 0) {
+      const emoticonHistoryElement = this.shadowRoot.querySelector(
+        `div[data-group="emoticon-history"]`).querySelector('emoticon-group');
+      if (emoticonHistoryElement != null) {
+        emoticonHistoryElement.showClearRecents = false;
+      }
+
     }
   }
 
