a4b24ec4d59a444ee4f756f5ffa77f62fff04329
Hyowon Kim <hyowon@igalia.com>
2022-05-10 05:02:00 +0000

    [mparch] Use PrimaryPageChanged in InstallableManager.
    This CL replaces DidFinishNavigation with PrimaryPageChanged in
    InstallableManager. Listening to PrimaryPageChanged is preferred to
    listening to DidFinishNavigation and checking IsInPrimaryMainFrame,
    !IsSameDocument, and HasCommitted.
    
    Bug: 1269645
    Change-Id: I67518a466faf69981fec40703ed6377244b2dfcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631893
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001350}

--- components/webapps/browser/installable/installable_manager.h
+++ components/webapps/browser/installable/installable_manager.h
@@ -250,7 +250,7 @@ class InstallableManager
   void OnDestruct(content::ServiceWorkerContext* context) override;
 
   // content::WebContentsObserver overrides
-  void DidFinishNavigation(content::NavigationHandle* handle) override;
+  void PrimaryPageChanged(content::Page& page) override;
   void DidUpdateWebManifestURL(content::RenderFrameHost* rfh,
                                const GURL& manifest_url) override;
   void WebContentsDestroyed() override;
