efdd88c30804814388a24a5dcb01b9bec9bbf01d
Gabriel Oliveira <gabolvr@google.com>
2022-05-10 16:15:31 +0000

    Add keepAlives to chrome://profile-internals debug page
    Screenshot of the page:
    https://screenshot.googleplex.com/U7nxi3epG5cuW3A
    
    Bug: 1313447
    Change-Id: I74ec794d84e13d72bd21b0dad070ab34f51fc8f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629292
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001523}

--- chrome/browser/resources/profile_internals/profile_internals_browser_proxy.ts
+++ chrome/browser/resources/profile_internals/profile_internals_browser_proxy.ts
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export type KeepAlive = {
+  origin: string,
+  count: number,
+};
+
 export type ProfileState = {
   profilePath: string,
   localProfileName: string,
@@ -12,6 +17,7 @@ export type ProfileState = {
   gaiaId: string,
   userName: string,
   hostedDomain: string,
+  keepAlives: Array<KeepAlive>,
 };
 
 /**
