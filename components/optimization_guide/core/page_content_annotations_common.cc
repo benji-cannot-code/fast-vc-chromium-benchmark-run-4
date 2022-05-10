e95f9132faa6e780990e9f05a7b5855aa883db47
Tommy C. Li <tommycli@chromium.org>
2022-05-10 23:35:29 +0000

    [optimization_guide] Fix internal optimization_guide_unittests build
    When I build all targets with autoninja -C out/rel, there's a linker
    error. This CL fixes that.
    
    Bug: 1292134
    Change-Id: I12038be2bd9e63ec9864c7ae74a813394e4dc970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639123
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001799}

--- components/optimization_guide/core/page_content_annotations_common.cc
+++ components/optimization_guide/core/page_content_annotations_common.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/page_content_annotations_common.h"
 
 #include <algorithm>
+#include <ostream>
 
 #include "base/check_op.h"
 #include "base/strings/string_number_conversions.h"
