f6ff57b49960f46dbcf662356be22471705c9553
Joey Arhar <jarhar@chromium.org>
2022-05-10 21:18:03 +0000

    Improve DisplayLocking console warnings
    1. Without this patch, the first call to a DisplayLock forcing script
       method might not emit a console message because the
       ScopedForcedUpdate can't find the relevant DisplayLock because the
       DisplayLock hasn't been created by style and layout yet. By also
       emitting warnings in AddForcedUpdateScopeForContext, this is fixed.
    2. This patch adds the DisplayLocked node to the console message, which
       adds a link to the node in the elements panel in DevTools.
    3. This patch makes Range based scoped unlocks also get console
       warnings.
    
    Change-Id: I908fc07425af58a6a3700d4a75f8dba6f3c62e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614990
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001663}

--- third_party/blink/renderer/core/display_lock/display_lock_utilities.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_utilities.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editing_boundary.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
-#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/layout/layout_shift_tracker.h"
@@ -31,20 +30,6 @@ DisplayLockUtilities::LockCheckMemoizationScope*
     DisplayLockUtilities::memoizer_ = nullptr;
 
 namespace {
-
-void WarnOnForcedUpdateInNonActivatableContext(Document& document) {
-  if (!v8::Isolate::GetCurrent()->InContext())
-    return;
-  String message =
-      "Rendering was performed in a subtree hidden by "
-      "content-visibility:hidden.";
-  // Note that this is a verbose level message, since it can happen
-  // frequently and is not necessarily a problem if the developer is
-  // accessing content-visibility: hidden subtrees intentionally.
-  document.AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
-      mojom::blink::ConsoleMessageSource::kJavaScript,
-      mojom::blink::ConsoleMessageLevel::kVerbose, message));
-}
 
 // Returns the nearest non-inclusive ancestor of |node| that is display
 // locked.
@@ -251,8 +236,12 @@ DisplayLockUtilities::ActivatableLockedInclusiveAncestors(
 DisplayLockUtilities::ScopedForcedUpdate::Impl::Impl(
     const Range* range,
     DisplayLockContext::ForcedPhase phase,
-    bool only_cv_auto)
-    : node_(range->FirstNode()), phase_(phase), only_cv_auto_(only_cv_auto) {
+    bool only_cv_auto,
+    bool emit_warnings)
+    : node_(range->FirstNode()),
+      phase_(phase),
+      only_cv_auto_(only_cv_auto),
+      emit_warnings_(emit_warnings) {
   if (!node_)
     return;
 
@@ -307,11 +296,7 @@ DisplayLockUtilities::ScopedForcedUpdate::Impl::Impl(
     }
   }
   for (DisplayLockContext* context : forced_context_set_) {
-    if (context->IsLocked() &&
-        !context->IsActivatable(DisplayLockActivationReason::kAny)) {
-      WarnOnForcedUpdateInNonActivatableContext(node_->GetDocument());
-    }
-    context->NotifyForcedUpdateScopeStarted(phase_);
+    context->NotifyForcedUpdateScopeStarted(phase_, emit_warnings_);
   }
 }
 
@@ -321,7 +306,10 @@ DisplayLockUtilities::ScopedForcedUpdate::Impl::Impl(
     bool include_self,
     bool only_cv_auto,
     bool emit_warnings)
-    : node_(node), phase_(phase), only_cv_auto_(only_cv_auto) {
+    : node_(node),
+      phase_(phase),
+      only_cv_auto_(only_cv_auto),
+      emit_warnings_(emit_warnings) {
   if (!node_)
     return;
 
@@ -366,10 +354,6 @@ DisplayLockUtilities::ScopedForcedUpdate::Impl::Impl(
     if (!ancestor_node)
       continue;
     if (auto* context = ancestor_node->GetDisplayLockContext()) {
-      if (emit_warnings && context->IsLocked() &&
-          !context->IsActivatable(DisplayLockActivationReason::kAny)) {
-        WarnOnForcedUpdateInNonActivatableContext(node->GetDocument());
-      }
       ForceDisplayLockIfNeeded(context);
     }
   }
@@ -382,7 +366,7 @@ void DisplayLockUtilities::ScopedForcedUpdate::Impl::EnsureMinimumForcedPhase(
     return;
   for (auto context : forced_context_set_) {
     context->NotifyForcedUpdateScopeEnded(phase_);
-    context->NotifyForcedUpdateScopeStarted(phase);
+    context->NotifyForcedUpdateScopeStarted(phase, emit_warnings_);
   }
   phase_ = phase;
 }
@@ -410,7 +394,7 @@ void DisplayLockUtilities::ScopedForcedUpdate::Impl::ForceDisplayLockIfNeeded(
   if (!only_cv_auto_ ||
       context->IsActivatable(DisplayLockActivationReason::kViewport)) {
     forced_context_set_.insert(context);
-    context->NotifyForcedUpdateScopeStarted(phase_);
+    context->NotifyForcedUpdateScopeStarted(phase_, emit_warnings_);
   }
 }
 
--- third_party/blink/renderer/core/display_lock/display_lock_utilities.h
+++ third_party/blink/renderer/core/display_lock/display_lock_utilities.h
@@ -85,7 +85,8 @@ class CORE_EXPORT DisplayLockUtilities {
                                            emit_warnings)) {}
     explicit ScopedForcedUpdate(const Range* range,
                                 DisplayLockContext::ForcedPhase phase,
-                                bool only_cv_auto = false)
+                                bool only_cv_auto = false,
+                                bool emit_warnings = true)
         : impl_(MakeGarbageCollected<Impl>(range, phase)) {}
 
     friend class DisplayLockDocumentState;
@@ -99,7 +100,8 @@ class CORE_EXPORT DisplayLockUtilities {
            bool emit_warnings = true);
       Impl(const Range* range,
            DisplayLockContext::ForcedPhase phase,
-           bool only_cv_auto = false);
+           bool only_cv_auto = false,
+           bool emit_warnings = true);
 
       // Adds another display-lock scope to this chain. Added when a new lock is
       // created in the ancestor chain of this chain's node.
@@ -123,6 +125,7 @@ class CORE_EXPORT DisplayLockUtilities {
       HeapHashSet<Member<DisplayLockContext>> forced_context_set_;
       Member<Impl> parent_frame_impl_;
       bool only_cv_auto_;
+      bool emit_warnings_;
     };
 
     Impl* impl_ = nullptr;
