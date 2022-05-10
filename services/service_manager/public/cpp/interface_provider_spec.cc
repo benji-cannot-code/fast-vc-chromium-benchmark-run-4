e23f92aaf2313d5bb3309d4e658e13a515bbc350
Peter Kasting <pkasting@chromium.org>
2022-05-10 17:54:26 +0000

    Fixes for C++20 support.
    * u8"" no longer produces a const char*.  Switch to "", which also
      accepts UTF-8 literals.
    * "requires" is a reserved word.  Change to "needs".
    
    Bug: 1284275
    Change-Id: I5cdbae55701dcf92d370a808dc27b1059868f180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631613
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001596}

--- services/service_manager/public/cpp/interface_provider_spec.cc
+++ services/service_manager/public/cpp/interface_provider_spec.cc
@@ -25,13 +25,12 @@ InterfaceProviderSpec& InterfaceProviderSpec::operator=(
 
 bool InterfaceProviderSpec::operator==(
     const InterfaceProviderSpec& other) const {
-  return other.provides == provides && other.requires == requires;
+  return other.provides == provides && other.needs == needs;
 }
 
 bool InterfaceProviderSpec::operator<(
     const InterfaceProviderSpec& other) const {
-  return std::tie(provides, requires) <
-         std::tie(other.provides, other.requires);
+  return std::tie(provides, needs) < std::tie(other.provides, other.needs);
 }
 
 bool GetInterfaceProviderSpec(const std::string& spec_name,
