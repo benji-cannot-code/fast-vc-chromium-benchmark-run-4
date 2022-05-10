3284bf9f17b3da4e85867392a5a1682c9d99a9e3
Moe Ahmadi <mahmadi@chromium.org>
2022-05-10 19:09:39 +0000

    [RQ] Extracts Most Repeated Search Terms for use in Most Visited tiles
    Segments and Keyword Search Terms which power the most visited sites
    and the zero-prefix suggestions in the omnibox respectively, are
    queried from different tables in the database and scored differently.
    
    In order to organically show the most repeated search terms along with
    the most visited segments/sites in the MV tiles, the search terms and
    the segments can continue to be queried differently, but scored and
    ranked consistently to be ultimately merged into a single list of tiles.
    
    Segments are currently ranked using a version of frecency score which
    is accumulated across timeslots (i.e., visit days) given the visit
    count for the segment in those timeslots. To achieve a consistent score
    for the search terms, this CL introduces,
    
    1) A simple utility enumerator class which is initialized by the
       URLDatabase to enumerate KeywordSearchTermVisits ordered first by
       |normalized_search_term| then |last_visit_time| in ascending order.
    
    2) A helper class that uses the enumerator to group the visits to
       unique search terms into timeslots and produce the final score for
       the search terms accumulated across the timeslots.
    
    Note that having the visits returned by the enumerator ordered first
    by the search term allows the helper class to efficiently aggregate
    the visits to unique search terms. Also having the visits ordered then
    from the oldest to the newest helps ensure the metadata associated with
    the most recent visit such as the |last_visit_time| are present in the
    aggregated search terms produced by the helper. In the future, the
    enumerator may be used by other helper classes to produce prefix and
    zero-prefix suggestions in the omnibox.
    
    Bug: 1317829
    Change-Id: Id1aee371fb58a79bd000f388748ea330fe4ce57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611078
    Commit-Queue: Mohamad Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001651}

--- components/history/core/browser/keyword_search_term.cc
+++ components/history/core/browser/keyword_search_term.cc
@@ -9,6 +9,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace history {
 
+namespace {
+
+// Returns a KeywordSearchTermVisit populated with the columns returned from
+// |statement|. |statement| is expected to return the following columns which
+// match in order and type to the fields in the KeywordSearchTermVisit less the
+// score which is a calculated field.
+//+----------+-----------------+-------------+-----------------+
+//| term     | normalized_term | visit_count | last_visit_time |
+//+----------+-----------------+-------------+-----------------+
+//| string16 | string16        | int         | int64           |
+//+----------+-----------------+-------------+-----------------+
+std::unique_ptr<KeywordSearchTermVisit> KeywordSearchTermVisitFromStatement(
+    sql::Statement& statement) {
+  auto search_term = std::make_unique<KeywordSearchTermVisit>();
+  search_term->term = statement.ColumnString16(0);
+  search_term->normalized_term = statement.ColumnString16(1);
+  search_term->visit_count = statement.ColumnInt(2);
+  search_term->last_visit_time =
+      base::Time::FromInternalValue(statement.ColumnInt64(3));
+  return search_term;
+}
+
+}  // namespace
+
+KeywordSearchTermVisit::KeywordSearchTermVisit() = default;
+KeywordSearchTermVisit::KeywordSearchTermVisit(
+    const KeywordSearchTermVisit& other) = default;
 KeywordSearchTermVisit::~KeywordSearchTermVisit() = default;
 
 double KeywordSearchTermVisit::GetFrecency(base::Time now,
@@ -21,11 +48,15 @@ double KeywordSearchTermVisit::GetFrecency(base::Time now,
   return frequency_powered * recency_decayed;
 }
 
-KeywordSearchTermRow::KeywordSearchTermRow() : keyword_id(0), url_id(0) {}
+// KeywordSearchTermVisitEnumerator --------------------------------------------
 
-KeywordSearchTermRow::KeywordSearchTermRow(const KeywordSearchTermRow& other) =
-    default;
-
-KeywordSearchTermRow::~KeywordSearchTermRow() {}
+std::unique_ptr<KeywordSearchTermVisit>
+KeywordSearchTermVisitEnumerator::GetNextVisit() {
+  if (initialized_ && statement_.Step()) {
+    return KeywordSearchTermVisitFromStatement(statement_);
+  }
+  initialized_ = false;
+  return nullptr;
+}
 
 }  // namespace history
--- components/history/core/browser/keyword_search_term.h
+++ components/history/core/browser/keyword_search_term.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/history/core/browser/keyword_id.h"
 #include "components/history/core/browser/url_row.h"
+#include "sql/statement.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace history {
 
@@ -20,7 +22,8 @@ namespace history {
 // visit or a set of keyword visits, depending on the overloaded functions it is
 // returned from.
 struct KeywordSearchTermVisit {
-  KeywordSearchTermVisit() = default;
+  KeywordSearchTermVisit();
+  KeywordSearchTermVisit(const KeywordSearchTermVisit& other);
   ~KeywordSearchTermVisit();
 
   // Returns the frecency score of the visit based on the following formula:
@@ -39,21 +42,46 @@ struct KeywordSearchTermVisit {
   std::u16string term;             // The search term that was used.
   std::u16string normalized_term;  // The search term, in lower case and with
                                    // extra whitespaces collapsed.
-  int visit_count{0};              // The visit count.
-  base::Time last_visit_time;      // The time of the most recent visit.
+  int visit_count{0};              // The search term visit count.
+  base::Time last_visit_time;      // The time of the last visit.
+  absl::optional<double> score;    // The optional calculated frecency score.
 };
 
 // Used for URLs that have a search term associated with them.
 struct KeywordSearchTermRow {
-  KeywordSearchTermRow();
-  KeywordSearchTermRow(const KeywordSearchTermRow& other);
-  ~KeywordSearchTermRow();
+  KeywordSearchTermRow() = default;
+  KeywordSearchTermRow(const KeywordSearchTermRow& other) = default;
+  ~KeywordSearchTermRow() = default;
 
-  KeywordID keyword_id;  // ID of the keyword.
-  URLID url_id;  // ID of the url.
+  KeywordID keyword_id{0};         // ID of the keyword.
+  URLID url_id{0};                 // ID of the url.
   std::u16string term;             // The search term that was used.
   std::u16string normalized_term;  // The search term, in lower case and with
                                    // extra whitespaces collapsed.
+};
+
+// KeywordSearchTermVisitEnumerator --------------------------------------------
+
+// A basic enumerator to enumerate keyword search term visits. May be created
+// and initialized by URLDatabase only.
+class KeywordSearchTermVisitEnumerator {
+ public:
+  KeywordSearchTermVisitEnumerator(const KeywordSearchTermVisitEnumerator&) =
+      delete;
+  KeywordSearchTermVisitEnumerator& operator=(
+      const KeywordSearchTermVisitEnumerator&) = delete;
+
+  ~KeywordSearchTermVisitEnumerator() = default;
+
+  // Returns the next search term visit or nullptr if no more visits are left.
+  std::unique_ptr<KeywordSearchTermVisit> GetNextVisit();
+
+ private:
+  friend class URLDatabase;
+  KeywordSearchTermVisitEnumerator() = default;
+
+  sql::Statement statement_;  // The statement to create KeywordSearchTermVisit.
+  bool initialized_{false};   // Whether |statement_| can be executed.
 };
 
 }  // namespace history
