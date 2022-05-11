14e45fabcccf864be1d87928221ce6a5b470fa97
Charles Zhao <charleszhao@chromium.org>
2022-05-11 02:02:29 +0000

    [HPS]: metrics SnoopingProtectionNotificationSuppression enabled.
    BUG: b=202671009
    Change-Id: I3dd1102c57d9b33a2d3ef0eff21ea054ea3759b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631106
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: Guoxing Zhao <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001862}

--- ash/system/human_presence/snooping_protection_notification_blocker.cc
+++ ash/system/human_presence/snooping_protection_notification_blocker.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/check_op.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
@@ -123,13 +124,25 @@ void SnoopingProtectionNotificationBlocker::OnActiveUserPrefServiceChanged(
   pref_change_registrar_->Add(
       prefs::kSnoopingProtectionNotificationSuppressionEnabled,
       base::BindRepeating(
-          &SnoopingProtectionNotificationBlocker::OnBlockingActiveChanged,
+          &SnoopingProtectionNotificationBlocker::OnBlockingPrefChanged,
           weak_ptr_factory_.GetWeakPtr()));
 }
 
 void SnoopingProtectionNotificationBlocker::OnBlockingActiveChanged() {
   NotifyBlockingStateChanged();
   UpdateInfoNotificationIfNecessary();
+}
+
+void SnoopingProtectionNotificationBlocker::OnBlockingPrefChanged() {
+  DCHECK(pref_change_registrar_);
+  DCHECK(pref_change_registrar_->prefs());
+  const bool pref_enabled = pref_change_registrar_->prefs()->GetBoolean(
+      prefs::kSnoopingProtectionNotificationSuppressionEnabled);
+  base::UmaHistogramBoolean(
+      "ChromeOS.HPS.SnoopingProtectionNotificationSuppression.Enabled",
+      pref_enabled);
+
+  OnBlockingActiveChanged();
 }
 
 bool SnoopingProtectionNotificationBlocker::ShouldShowNotificationAsPopup(
