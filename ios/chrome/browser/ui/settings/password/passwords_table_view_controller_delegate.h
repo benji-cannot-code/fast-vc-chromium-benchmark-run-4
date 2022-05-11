f5e4700e78ed8a212b53156f9214312267a2698a
Arthur Milchior <arthurmilchior@google.com>
2022-05-11 15:54:59 +0000

    [iOS] Fix crash in password manager settings
    The solution consists simply in stating that, if the search mode is
    on, “on device encryption” section should not be shown.
    
    Note that it won’t appear when search end, which is not the expected
    behavior.
    
    This must also correct bug 1322464, as otherwise the DCHECK fail during the unit test.
    
    Tested:
    I added a unit test. I tested without the correction, and it crashes at the same place. I added the correction, and it succeed.
    
    Manually Tested:
    Open ipad with two windows, be logged-in, with password sync disabled and promotion of on-device experiment activated.
    On one window, open password-manager and click on search.
    On the second window, activate password sync and click done.
    
    It should not crash anymore.
    
    Fixed: 1322464
    Bug: 1321057
    Change-Id: I78a97358e06921848c9d9b6a076368a7c0ec24a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627553
    Commit-Queue: Arthur Milchior <arthurmilchior@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002118}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_controller_delegate.h
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_controller_delegate.h
@@ -8,6 +8,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+// State of on-device encryption used for
+// ItemTypeOnDeviceEncryptionOptInDescription, ItemTypeOnDeviceEncryptionSetUp
+// and ItemTypeOnDeviceEncryptionSetUp.
+typedef NS_ENUM(NSInteger, OnDeviceEncryptionState) {
+  // On device encryption is on.
+  // ItemTypeOnDeviceEncryptionOptInDescription is shown.
+  OnDeviceEncryptionStateOptedIn,
+  // User can opt-in on device encryption.
+  // ItemTypeOnDeviceEncryptionOptInDescription and
+  // ItemTypeOnDeviceEncryptionSetUp are shown.
+  OnDeviceEncryptionStateOfferOptIn,
+  // User can not opt-in in their current state.
+  // Currently it is either because:
+  // * User is not signed-in,
+  // * User hasn’t opted in to or disabled Sync for passwords (or equivalent
+  // enterprise policies),
+  // * User has a custom passphrase.
+  // SectionIdentifierOnDeviceEncryption is hidden.
+  OnDeviceEncryptionStateNotShown,
+};
+
 namespace password_manager {
 struct PasswordForm;
 }
@@ -30,6 +51,9 @@ struct PasswordForm;
 
 // Returns detailed information about Password Check error if applicable.
 - (NSAttributedString*)passwordCheckErrorInfo;
+
+// Returns the on-device encryption state according to the sync service.
+- (OnDeviceEncryptionState)onDeviceEncryptionState;
 
 @end
 
