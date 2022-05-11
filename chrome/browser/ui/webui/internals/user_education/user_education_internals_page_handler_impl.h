5fd520150523eb35908550271a13e73b75689ab3
Tim Sergeant <tsergeant@chromium.org>
2022-05-11 08:37:28 +0000

    Do not use a self-owned receiver on chrome://internals/user-education
    Using a self-owned receiver means that the
    UserEducationInternalsPageHandler can live longer than the
    WebUIController. If the handler receives a mojo message with the right
    timing, it is possible to trigger a UAF on either the Profile or WebUI.
    
    This CL changes the Handler so that it is owned by the WebUIController
    directly, which addresses this issue and matches the pattern used by
    other WebUI pages.
    
    Bug: 1323239
    Change-Id: Id2127380c398f5a5805001555141afc2f5d57a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640103
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001968}

--- chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
+++ chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/internals/user_education/user_education_internals.mojom.h"
 #include "components/user_education/common/tutorial_service.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 
 namespace base {
 struct Feature;
@@ -28,8 +30,12 @@ class UserEducationInternalsPageHandlerImpl
     : public mojom::user_education_internals::
           UserEducationInternalsPageHandler {
  public:
-  explicit UserEducationInternalsPageHandlerImpl(content::WebUI* web_ui,
-                                                 Profile* profile);
+  UserEducationInternalsPageHandlerImpl(
+      content::WebUI* web_ui,
+      Profile* profile,
+      mojo::PendingReceiver<
+          mojom::user_education_internals::UserEducationInternalsPageHandler>
+          receiver);
   ~UserEducationInternalsPageHandlerImpl() override;
 
   UserEducationInternalsPageHandlerImpl(
@@ -53,6 +59,10 @@ class UserEducationInternalsPageHandlerImpl
   raw_ptr<user_education::TutorialService> tutorial_service_ = nullptr;
   raw_ptr<content::WebUI> web_ui_ = nullptr;
   raw_ptr<Profile> profile_ = nullptr;
+
+  mojo::Receiver<
+      mojom::user_education_internals::UserEducationInternalsPageHandler>
+      receiver_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_INTERNALS_USER_EDUCATION_USER_EDUCATION_INTERNALS_PAGE_HANDLER_IMPL_H_
