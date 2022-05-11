7e85b8ac71d2c04ccd677b9640872e640aff968b
Chidera Olibie <colibie@google.com>
2022-05-11 13:30:24 +0000

    Cronet: Use chromium's JDK for "jar" command
    Bug: 1324274
    Change-Id: Ie90f1080ab991d6edf9d1c1035969df84a29bb80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641623
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002036}

--- components/cronet/tools/api_static_checks.py
+++ components/cronet/tools/api_static_checks.py
@@ -87,7 +87,7 @@ INTERFACE_API_VERSION_FILENAME = os.path.abspath(os.path.join(
 IMPLEMENTATION_API_VERSION_FILENAME = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', 'android',
     'implementation_api_version.txt'))
-
+JAR_PATH = os.path.join(build_utils.JAVA_HOME, 'bin', 'jar')
 
 def find_api_calls(dump, api_classes, bad_calls):
   # Given a dump of an implementation class, find calls through API classes.
@@ -130,7 +130,8 @@ def check_api_calls(opts):
   temp_dir = tempfile.mkdtemp()
 
   # Extract API class files from jar
-  jar_cmd = ['jar', 'xf', os.path.abspath(opts.api_jar)]
+  jar_cmd = [os.path.relpath(JAR_PATH, temp_dir), 'xf',
+             os.path.abspath(opts.api_jar)]
   build_utils.CheckOutput(jar_cmd, cwd=temp_dir)
   shutil.rmtree(os.path.join(temp_dir, 'META-INF'), ignore_errors=True)
 
@@ -150,7 +151,8 @@ def check_api_calls(opts):
 
   # Extract impl class files from jars
   for impl_jar in opts.impl_jar:
-    jar_cmd = ['jar', 'xf', os.path.abspath(impl_jar)]
+    jar_cmd = [os.path.relpath(JAR_PATH, temp_dir), 'xf',
+               os.path.abspath(impl_jar)]
     build_utils.CheckOutput(jar_cmd, cwd=temp_dir)
   shutil.rmtree(os.path.join(temp_dir, 'META-INF'), ignore_errors=True)
 
