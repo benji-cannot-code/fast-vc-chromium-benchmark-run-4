5f8432b9c3dfdc9d43d9a9b322d45206415cc037
Ian Vollick <vollick@chromium.org>
2022-05-11 01:59:21 +0000

    [mparch] Basic support for MPArch+FF on Android WebView
    This CL leverages boliu@'s OOPIF rendering path from
    crrev.com/c/3107612 for MPArch-based Fenced Frames.
    
    At a high level, this entailed
     - plumbing a more generic "is embedded" bit to WidgetBase
     - updating the names of some things due to this change
     - fixing the way that blink sets the viewport properties for a
       fenced frame (without this, the switch to is-embedded breaks
       assumptions in cc -- specifically, cc does not expect page
       scale machinery for embedded frame trees).
    
    This does not provide full support, but does allow a fenced
    frame to render and receive input.
    
    Thank you to boliu@ and dtapuska@ who provided a great deal of
    direct guidance in putting this patch together.
    
    Bug: 1322158,845097
    Change-Id: I99cfaddadb18aaba79e24fca66055b5898535ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620934
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001859}

--- third_party/blink/renderer/core/frame/frame_test_helpers.h
+++ third_party/blink/renderer/core/frame/frame_test_helpers.h
@@ -321,7 +321,8 @@ using CreateTestWebFrameWidgetCallback =
         bool hidden,
         bool never_composited,
         bool is_for_child_local_root,
-        bool is_for_nested_main_frame)>;
+        bool is_for_nested_main_frame,
+        bool is_for_scalable_page)>;
 
 // Convenience class for handling the lifetime of a WebView and its associated
 // mainframe in tests.
@@ -438,12 +439,14 @@ class WebViewHelper : public ScopedMockOverlayScrollbars {
       bool hidden,
       bool never_composited,
       bool is_for_child_local_root,
-      bool is_for_nested_main_frame) {
+      bool is_for_nested_main_frame,
+      bool is_for_scalable_page) {
     return MakeGarbageCollected<C>(
         std::move(pass_key), std::move(frame_widget_host),
         std::move(frame_widget), std::move(widget_host), std::move(widget),
         std::move(task_runner), frame_sink_id, hidden, never_composited,
-        is_for_child_local_root, is_for_nested_main_frame);
+        is_for_child_local_root, is_for_nested_main_frame,
+        is_for_scalable_page);
   }
 
   blink::scheduler::WebAgentGroupScheduler& GetAgentGroupScheduler() {
--- third_party/blink/renderer/core/testing/sim/sim_test.h
+++ third_party/blink/renderer/core/testing/sim/sim_test.h
@@ -69,6 +69,7 @@ class SimTest : public testing::Test {
       bool never_composited,
       bool is_for_child_local_root,
       bool is_for_nested_main_frame,
+      bool is_for_scalable_page,
       SimCompositor* compositor);
 
  private:
@@ -87,7 +88,8 @@ class SimTest : public testing::Test {
       bool hidden,
       bool never_composited,
       bool is_for_child_local_root,
-      bool is_for_nested_main_frame);
+      bool is_for_nested_main_frame,
+      bool is_for_scalable_page);
 
   // These are unique_ptrs in order to destroy them in TearDown. Subclasses
   // may override Platform::Current() and these must shutdown before the
