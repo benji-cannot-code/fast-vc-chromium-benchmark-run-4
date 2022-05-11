dda0da4e04e4a62966ed7e42b768c3c10955dca9
Avi Drissman <avi@chromium.org>
2022-05-11 15:36:05 +0000

    Clean up flags
    These flags are unused.
    
    Bug: none
    Change-Id: I2daa13e5b7660e377221f7280216dcb1626badd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638649
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002104}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -471,16 +471,6 @@ extern const base::Feature kListWebAppsSwitch;
 
 #if BUILDFLAG(IS_MAC)
 COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kMacFullSizeContentView;
-#endif
-
-#if BUILDFLAG(IS_MAC)
-COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kMacMaterialDesignDownloadShelf;
-#endif
-
-#if BUILDFLAG(IS_MAC)
-COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kMacSystemScreenCapturePermissionCheck;
 #endif
 
