58ee920ec7f51385b4b64939ff4ac95abe95c29e
Veronique Nguyen <veronguyen@google.com>
2022-05-11 16:40:50 +0000

    Clean up obsolete favicons from the Chrome app group storage (V2)
    - Run a task to remove the obsolete favicons from the storage when the
    Chrome app launches. (And remove the folder if the flag is disabled)
    - Sync favicons on launch.
    
    Note: Next CL will contain the maximum favicons verification and will
    limit the sync and clean up to once a week. Since it is all under a
    feature flag, we can land the parts separately.
    
    Bug: 1305244
    Change-Id: I2163e4a82dc64463f9e487c6dc3922b36f1e9c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637844
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002137}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/download/download_directory_util.h"
 #import "ios/chrome/browser/external_files/external_file_remover_factory.h"
 #import "ios/chrome/browser/external_files/external_file_remover_impl.h"
+#include "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #include "ios/chrome/browser/feature_engagement/tracker_factory.h"
 #import "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/mailto_handler/mailto_handler_service.h"
@@ -132,6 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/credential_provider_migrator_app_agent.h"
 #include "ios/chrome/browser/credential_provider/credential_provider_service_factory.h"
 #include "ios/chrome/browser/credential_provider/credential_provider_support.h"
+#import "ios/chrome/browser/credential_provider/credential_provider_util.h"
 #endif
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -187,6 +189,9 @@ NSString* const kEnterpriseManagedDeviceCheck = @"EnterpriseManagedDeviceCheck";
 
 // Constants for deferred deletion of leftover session state files.
 NSString* const kPurgeWebSessionStates = @"PurgeWebSessionStates";
+
+// Constants for deferred favicons clean up.
+NSString* const kFaviconsCleanup = @"FaviconsCleanup";
 
 // Adapted from chrome/browser/ui/browser_init.cc.
 void RegisterComponentsForUpdate() {
@@ -1061,6 +1066,7 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   [self initializeMailtoHandling];
   [self scheduleSaveFieldTrialValuesForExtensions];
   [self scheduleEnterpriseManagedDeviceCheck];
+  [self scheduleFaviconsCleanup];
 }
 
 - (void)scheduleTasksRequiringBVCWithBrowserState {
@@ -1105,6 +1111,18 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
       enqueueBlockNamed:kStartSpotlightBookmarksIndexing
                   block:^{
                     [spotlightManager resyncIndex];
+                  }];
+}
+
+- (void)scheduleFaviconsCleanup {
+  [[DeferredInitializationRunner sharedInstance]
+      enqueueBlockNamed:kFaviconsCleanup
+                  block:^{
+#if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
+                    UpdateFaviconsStorage(
+                        IOSChromeFaviconLoaderFactory::GetForBrowserState(
+                            self.currentBrowserState));
+#endif
                   }];
 }
 
