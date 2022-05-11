5205a1ab4f179c9f3481d058c4d1dbf7cf7732ae
Anders Hartvoll Ruud <andruud@chromium.org>
2022-05-11 10:15:05 +0000

    [mq-4] Don't convert "unknown" queries to "not all" for MQ4
    In order to do web compat research, we implemented a way for media
    queries to parse with "unknown" parts (mediaqueries-4), but still
    behave and serialize as "not all" (mediaqueries-3).
    
    Because it was convenient at the time, this "as-if-not-all" behavior
    also happened with the CSSMediaQueries4 runtime flag enabled. However,
    this is not what we ultimately want, so to prepare for shipping, this
    CL removes that behavior when the flag is enabled.
    
    Bug: 962417
    Change-Id: Id7a6690bebaa379a5c1fdefe659b09573a3f8337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629364
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001999}

--- third_party/blink/renderer/core/css/media_query.cc
+++ third_party/blink/renderer/core/css/media_query.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/media_query_exp.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/media_type_names.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
@@ -93,19 +94,19 @@ MediaQuery::MediaQuery(const MediaQuery& o)
 MediaQuery::~MediaQuery() = default;
 
 MediaQuery::RestrictorType MediaQuery::Restrictor() const {
-  if (has_unknown_)
+  if (BehaveAsNotAll())
     return MediaQuery::kNot;
   return restrictor_;
 }
 
 const MediaQueryExpNode* MediaQuery::ExpNode() const {
-  if (has_unknown_)
+  if (BehaveAsNotAll())
     return nullptr;
   return exp_node_.get();
 }
 
 const String& MediaQuery::MediaType() const {
-  if (has_unknown_) {
+  if (BehaveAsNotAll()) {
     DEFINE_STATIC_LOCAL(const AtomicString, all, ("all"));
     return all;
   }
@@ -117,19 +118,18 @@ bool MediaQuery::operator==(const MediaQuery& other) const {
   return CssText() == other.CssText();
 }
 
+bool MediaQuery::BehaveAsNotAll() const {
+  if (RuntimeEnabledFeatures::CSSMediaQueries4Enabled())
+    return false;
+  return has_unknown_;
+}
+
 // https://drafts.csswg.org/cssom/#serialize-a-list-of-media-queries
 String MediaQuery::CssText() const {
   if (serialization_cache_.IsNull())
     const_cast<MediaQuery*>(this)->serialization_cache_ = Serialize();
 
   return serialization_cache_;
-}
-
-std::unique_ptr<MediaQuery> MediaQuery::CopyIgnoringUnknownForTest() const {
-  auto query = Copy();
-  query->has_unknown_ = false;
-  query->serialization_cache_ = g_null_atom;
-  return query;
 }
 
 }  // namespace blink
