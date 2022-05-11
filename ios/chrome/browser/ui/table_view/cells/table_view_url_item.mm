076d9b3350c76c45fe8dae134798c5b51afec339
Ed Chin <edchin@google.com>
2022-05-11 14:32:20 +0000

    [ios][follow mgmt] Update spinner background color
    Screenshot:
    https://screenshot.googleplex.com/8aX6mnh8vukivqn.png
    
    Change-Id: Ie910c15c35f9b6985afabdfe6ef96f524947e173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638646
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002069}

--- ios/chrome/browser/ui/table_view/cells/table_view_url_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_url_item.mm
@@ -359,7 +359,7 @@ const char kDefaultSupplementalURLTextDelimiter[] = "•";
         constraintEqualToAnchor:self.faviconContainerView.centerYAnchor],
   ]];
   [activityView startAnimating];
-  activityView.backgroundColor = [UIColor whiteColor];
+  activityView.backgroundColor = self.faviconContainerView.backgroundColor;
 }
 
 - (void)stopAnimatingActivityIndicator {
