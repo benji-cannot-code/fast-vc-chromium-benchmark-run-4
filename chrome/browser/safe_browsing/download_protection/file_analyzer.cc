78aa685f5b17b2c4c84a7160061b7c3a5bd660c1
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-05-11 22:21:45 +0000

    Revert "Fix issues with *ArchiveAnalysisResult histograms"
    This reverts commit d67fad1a176663a4568e7e00a90e43fa9f006068.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision d67fad1a176663a4568e7e00a90e43fa9f006068 as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8814425019369672337
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fd67fad1a176663a4568e7e00a90e43fa9f006068&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+d67fad1a176663a4568e7e00a90e43fa9f006068
    
    Original change's description:
    > Fix issues with *ArchiveAnalysisResult histograms
    > 
    > The histogram logging code and histograms.xml code were out of sync in
    > https://chromium-review.googlesource.com/c/chromium/src/+/3608712, so
    > these histograms are never logged.
    > 
    > Additionally, the analysis_result field in ArchiveAnalysisResults was
    > not serialized across the process boundary, so it only would ever log
    > kUnspecified.
    > 
    > Bug: 1314481, 1314483, 1311816
    > Change-Id: I7b0878581509c67e55caeb82e38e538eb9e9022c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639843
    > Commit-Queue: Daniel Rubery <drubery@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1002339}
    
    
    Change-Id: Ida35a96520d192546fe8e0a79a8c9299f45985c6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1314481, 1314483, 1311816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642428
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Owners-Override: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002349}

--- chrome/browser/safe_browsing/download_protection/file_analyzer.cc
+++ chrome/browser/safe_browsing/download_protection/file_analyzer.cc
@@ -155,7 +155,7 @@ void FileAnalyzer::StartExtractZipFeatures() {
 
 void FileAnalyzer::OnZipAnalysisFinished(
     const ArchiveAnalyzerResults& archive_results) {
-  base::UmaHistogramEnumeration("SBClientDownload.ZipArchiveAnalysisResult",
+  base::UmaHistogramEnumeration("SBClientDownload.ZipArchiveNotValidReason",
                                 archive_results.analysis_result);
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
@@ -203,7 +203,7 @@ void FileAnalyzer::StartExtractRarFeatures() {
 void FileAnalyzer::OnRarAnalysisFinished(
     const ArchiveAnalyzerResults& archive_results) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  base::UmaHistogramEnumeration("SBClientDownload.RarArchiveAnalysisResult",
+  base::UmaHistogramEnumeration("SBClientDownload.RarArchiveNotValidReason",
                                 archive_results.analysis_result);
 
   results_.archive_is_valid =
@@ -261,7 +261,7 @@ void FileAnalyzer::ExtractFileOrDmgFeatures(
 void FileAnalyzer::OnDmgAnalysisFinished(
     const safe_browsing::ArchiveAnalyzerResults& archive_results) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  base::UmaHistogramEnumeration("SBClientDownload.DmgArchiveAnalysisResult",
+  base::UmaHistogramEnumeration("SBClientDownload.DmgArchiveNotValidReason",
                                 archive_results.analysis_result);
 
   if (archive_results.signature_blob.size() > 0) {
