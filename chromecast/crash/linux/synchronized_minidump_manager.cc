451c81d644f25a1869889d90b0c91e4b90f78703
David Bertoni <dbertoni@chromium.org>
2022-05-11 17:54:48 +0000

    [CodeHealth] Remove uses of ListValue::Append in /chromecast/crash.
    This CL removes uses of the overload that accepts a
    std::unique_ptr<base::Value>. It attempts to modernize code where
    feasible and uses base::Value::FromUniquePtrValue where it's not.
    
    This CL was uploaded by git cl split.
    
    R=juke@chromium.org
    
    Bug: 1187097
    Change-Id: I06edfd3bd18007918c0f0380d6de22a41681642f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640389
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002190}

--- chromecast/crash/linux/synchronized_minidump_manager.cc
+++ chromecast/crash/linux/synchronized_minidump_manager.cc
@@ -304,7 +304,7 @@ bool SynchronizedMinidumpManager::ParseFiles() {
     RCHECK(dump_info.has_value(), false);
     DumpInfo info(&dump_info.value());
     RCHECK(info.valid(), false);
-    dumps->Append(std::move(dump_info.value()));
+    dumps->GetList().Append(std::move(dump_info.value()));
   }
 
   JSONFileValueDeserializer deserializer(metadata_path_);
@@ -368,7 +368,8 @@ bool SynchronizedMinidumpManager::AddEntryToLockFile(
     return false;
   }
 
-  dumps_->Append(dump_info.GetAsValue());
+  dumps_->GetList().Append(
+      base::Value::FromUniquePtrValue(dump_info.GetAsValue()));
   return true;
 }
 
@@ -408,8 +409,10 @@ bool SynchronizedMinidumpManager::SetCurrentDumps(
     const std::vector<std::unique_ptr<DumpInfo>>& dumps) {
   dumps_->ClearList();
 
-  for (auto& dump : dumps)
-    dumps_->Append(dump->GetAsValue());
+  for (auto& dump : dumps) {
+    dumps_->GetList().Append(
+        base::Value::FromUniquePtrValue(dump->GetAsValue()));
+  }
 
   return true;
 }
