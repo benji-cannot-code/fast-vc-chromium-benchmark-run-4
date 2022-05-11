51e0fe72bfd904e3efa4672f1e38cc340ffa58f7
Hailey Wang <haileywang@google.com>
2022-05-11 18:32:54 +0000

    [Query tiles] Fix update decision logic
    Stop updating decision when decision time is not reached.
    
    Bug: 1324652
    Change-Id: I2c979619ed8086c7d65ae6c04f647688f1822d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642320
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002209}

--- chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileUtils.java
@@ -143,6 +143,7 @@ public class QueryTileUtils {
         if (noPreviousHistory) {
             // If this is the first time we make a decision, don't show query tiles.
             resultFromCode = false;
+            updateDisplayStatusAndNextDecisionTime(resultFromCode);
         } else if (nextDecisionTimestampReached) {
             int recentMVClicks = SharedPreferencesManager.getInstance().readInt(
                     ChromePreferenceKeys.QUERY_TILES_NUM_RECENT_MV_TILE_CLICKS, 0);
@@ -156,6 +157,7 @@ public class QueryTileUtils {
             // Otherwise, show it for a period of time.
             resultFromCode = (recentMVClicks <= mvTileClickThreshold
                     || recentMVClicks <= recentQueryTileClicks);
+            updateDisplayStatusAndNextDecisionTime(resultFromCode);
         } else {
             resultFromCode = SharedPreferencesManager.getInstance().readBoolean(
                     ChromePreferenceKeys.QUERY_TILES_SHOW_ON_NTP, false);
@@ -167,7 +169,6 @@ public class QueryTileUtils {
             recordSegmentationResultComparison(getSegmentationResult(), resultFromCode);
         }
 
-        updateDisplayStatusAndNextDecisionTime(resultFromCode);
         return resultFromCode;
     }
 
