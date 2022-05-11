be634752d92fa68f1cfa69ae9202407f95604534
Dibyajyoti Pal <dibyapal@chromium.org>
2022-05-11 22:23:57 +0000

    dPWA: Introduce UserUninstalledPreinstalledWebAppPrefs
    This CL introduces the UserUninstalledPreinstalledWebAppPrefs that will contain data about default installed apps after they have been
    uninstalled by the user and after the ExternallyInstalledWebAppPrefs
    have been migrated to the web_app DB. This data needs to be left over
    to ensure that when an user uninstalls a default app,it is not automatically reinstalled by ExternallyManagedAppManager::Synchronize.
    
    Following CLs will perform the following tasks:
    1. Write to the new pref and migrate all data from the current
    external prefs and the kWasUninstalledByUser pref.
    2. Start reading data from the new pref while making installation
    decisions from the PreinstalledWebAppManager.
    
    Bug: 1300382
    Change-Id: I5d0113f164f94a729f4a37400959d8952dde0e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3626154
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002353}

--- chrome/browser/web_applications/web_app_provider.cc
+++ chrome/browser/web_applications/web_app_provider.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/preinstalled_web_app_manager.h"
 #include "chrome/browser/web_applications/system_web_apps/system_web_app_manager.h"
+#include "chrome/browser/web_applications/user_uninstalled_preinstalled_web_app_prefs.h"
 #include "chrome/browser/web_applications/web_app_audio_focus_id_map.h"
 #include "chrome/browser/web_applications/web_app_command_manager.h"
 #include "chrome/browser/web_applications/web_app_database_factory.h"
@@ -374,6 +375,7 @@ void WebAppProvider::CheckIsConnected() const {
 // static
 void WebAppProvider::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
+  UserUninstalledPreinstalledWebAppPrefs::RegisterProfilePrefs(registry);
   ExternallyInstalledWebAppPrefs::RegisterProfilePrefs(registry);
   PreinstalledWebAppManager::RegisterProfilePrefs(registry);
   WebAppPolicyManager::RegisterProfilePrefs(registry);
