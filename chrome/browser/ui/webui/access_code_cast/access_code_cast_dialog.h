7aef3d90e8e7e23e6ddff3525fd96f3d79758473
Benjamin Zielinski <bzielinski@google.com>
2022-05-11 20:30:54 +0000

    [Cast2Class] Add managed device footnote
    Conditionally show a footnote in the access code cast dialog which
    informs the user that their cast device will be saved.
    
    Bug: b:218574288
    Change-Id: Ia2ae210d3aee4c03463975c12427675250d1339d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639845
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002294}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.h
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/media_router/media_cast_mode.h"
 #include "chrome/browser/ui/media_router/media_route_starter.h"
 #include "components/access_code_cast/common/access_code_cast_metrics.h"
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace content {
-class BrowserContext;
 class WebContents;
 }  // namespace content
 
@@ -94,7 +94,7 @@ class AccessCodeCastDialog : public ui::WebDialogDelegate,
   std::unique_ptr<media_router::MediaRouteStarter> media_route_starter_;
 
   const raw_ptr<content::WebContents> web_contents_;
-  const raw_ptr<content::BrowserContext> context_;
+  const raw_ptr<Profile> context_;
   base::Time dialog_creation_timestamp_;
 };
 
