b2e8d9cb0e2d0acc490c23e3d133b313822aabb8
Austin Sullivan <asully@chromium.org>
2022-05-11 15:48:53 +0000

    FSA: Update outdated comment in dialogs
    The `is_directory` bool was replaced with the `handle_type` enum in
    https://crrev.com/c/2255338
    
    Change-Id: I2f7f061832bf13e68d6fd210a0d628f63d4d1676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638701
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002111}

--- chrome/browser/ui/file_system_access_dialogs.cc
+++ chrome/browser/ui/file_system_access_dialogs.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/file_system_access_dialogs.h"
+#include "build/build_config.h"
 
 #include "components/permissions/permission_util.h"
 
