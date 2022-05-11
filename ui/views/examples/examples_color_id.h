ef0a5cce3d4de648b96fb24a937a75ed8645e2da
Tommy Li <tommycli@chromium.org>
2022-05-11 18:31:25 +0000

    Revert "Add border to Example Menu Button to more clearly indicate that it is a"
    This reverts commit 2e6344c4301b074379e085345fecc2dfc04620a9.
    
    Reason for revert: Breaks the tree https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20Builder%20(dbg)/137281/overview
    
    Original change's description:
    > Add border to Example Menu Button to more clearly indicate that it is a
    > button
    >
    > Bug: 1322635
    > Change-Id: I71cf3ef16b70cebe4703195153a83a2998d6e70b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627479
    > Reviewed-by: Elaine Chien <elainechien@chromium.org>
    > Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1002175}
    
    Bug: 1322635
    Change-Id: Id994c133b5df9e191e55f5fa3dd60c81d064dcc3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642601
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002208}

--- ui/views/examples/examples_color_id.h
+++ ui/views/examples/examples_color_id.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorLabelExampleCustomBackground) \
   E_CPONLY(kColorLabelExampleCustomBorder) \
   E_CPONLY(kColorLabelExampleThickBorder) \
-  E_CPONLY(kColorMenuButtonExampleBorder) \
   E_CPONLY(kColorMultilineExampleBorder) \
   E_CPONLY(kColorMultilineExampleColorRange) \
   E_CPONLY(kColorMultilineExampleForeground) \
