140e5898137943ecda9a81870ba81a32f3c401a9
Chung-Sheng Wu <chungsheng@google.com>
2022-05-11 02:15:52 +0000

    cros_healthd: Add TouchpadLibraryName implementation
    Copy the implementation of TouchpadLibraryName from ServiceConnection to
    DataCollector.
    Add HANDLE_EINTR to handle the interrupt of `open`.
    
    Bug: b:230067049
    Test: CQ
    Test: Manually test with build flag `use_libinput = true`
    Change-Id: If153ea9191840d9231197d31f4afa1b6ed3287f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629882
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1001869}

--- chromeos/services/cros_healthd/private/cpp/data_collector.cc
+++ chromeos/services/cros_healthd/private/cpp/data_collector.cc
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/cros_healthd/private/cpp/data_collector.h"
 
+#include <fcntl.h>
+
 #include "base/check_op.h"
+#include "base/files/file_enumerator.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/posix/eintr_wrapper.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "ui/events/ozone/evdev/event_device_info.h"
 
 namespace chromeos {
 namespace cros_healthd {
@@ -34,8 +39,39 @@ DataCollectorDelegateImpl::DataCollectorDelegateImpl() = default;
 DataCollectorDelegateImpl::~DataCollectorDelegateImpl() = default;
 
 std::string DataCollectorDelegateImpl::GetTouchpadLibraryName() {
-  NOTIMPLEMENTED();
-  return "";
+#if defined(USE_LIBINPUT)
+  base::FileEnumerator file_enum(base::FilePath("/dev/input/"), false,
+                                 base::FileEnumerator::FileType::FILES);
+  for (auto path = file_enum.Next(); !path.empty(); path = file_enum.Next()) {
+    base::ScopedFD fd(
+        HANDLE_EINTR(open(path.value().c_str(), O_RDWR | O_NONBLOCK)));
+    if (fd.get() < 0) {
+      LOG(ERROR) << "Couldn't open device path " << path;
+      continue;
+    }
+
+    auto devinfo = std::make_unique<ui::EventDeviceInfo>();
+    if (!devinfo->Initialize(fd.get(), path)) {
+      LOG(ERROR) << "Failed to get device info for " << path;
+      continue;
+    }
+
+    if (!devinfo->HasTouchpad() ||
+        devinfo->device_type() != ui::InputDeviceType::INPUT_DEVICE_INTERNAL) {
+      continue;
+    }
+
+    if (devinfo->UseLibinput()) {
+      return "libinput";
+    }
+  }
+#endif
+
+#if defined(USE_EVDEV_GESTURES)
+  return "gestures";
+#else
+  return "Default EventConverterEvdev";
+#endif
 }
 
 DataCollectorDelegateImpl* GetDataCollectorDelegate() {
