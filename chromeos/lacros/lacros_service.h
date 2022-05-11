c0c84ecc15738813d8f2496519aa2885f9df9159
Yuzu Saijo <yuzus@chromium.org>
2022-05-11 01:10:03 +0000

    Revert "Lacros part for component policy"
    This reverts commit 92b5fdc0b6f3e804911d777738d7894dc86e063b.
    
    Reason for revert: Tests added in this CL started to fail
    Bug:1324422
    
    Original change's description:
    > Lacros part for component policy
    >
    > This part processes the data received by PolicyLoaderLacros and includes
    > it in the resulted PolicyBundle to be consumed.
    >
    > BUG=1284279
    > TEST=Browser test
    >
    > Change-Id: I296dd3663525324fd5f48cfff295bdef6dd7086e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3592979
    > Commit-Queue: Igor <igorcov@chromium.org>
    > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1001690}
    
    Bug: 1284279
    Change-Id: I8955ab252607c99d07e2369fd4097cf961337107
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640802
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001839}

--- chromeos/lacros/lacros_service.h
+++ chromeos/lacros/lacros_service.h
@@ -59,8 +59,6 @@ class LacrosServiceNeverBlockingState;
 // documented with threading requirements.
 class COMPONENT_EXPORT(CHROMEOS_LACROS) LacrosService {
  public:
-  using ComponentPolicyMap =
-      base::flat_map<policy::PolicyNamespace, base::Value>;
   class Observer {
    public:
     // Called when the new policy data is received from Ash.
@@ -69,9 +67,6 @@ class COMPONENT_EXPORT(CHROMEOS_LACROS) LacrosService {
 
     // Called when policy fetch attempt is made in Ash.
     virtual void OnPolicyFetchAttempt() {}
-
-    // Called when the new component policy is received from Ash.
-    virtual void OnComponentPolicyUpdated(const ComponentPolicyMap& policy) {}
 
    protected:
     virtual ~Observer() = default;
@@ -130,10 +125,6 @@ class COMPONENT_EXPORT(CHROMEOS_LACROS) LacrosService {
 
   // Notifies that an attempt to update the device account policy has been made.
   void NotifyPolicyFetchAttempt();
-
-  // Notifies that the device account component policy is updated with the
-  // input data. Must be called on the affined sequence.
-  void NotifyComponentPolicyUpdated(ComponentPolicyMap policy);
 
   // Returns whether this interface uses the automatic registration system to be
   // available for immediate use at startup. Any crosapi interface can be
