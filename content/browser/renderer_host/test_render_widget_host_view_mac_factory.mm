85e0ca1837a628ff68c3ec11bb96f7ddd06cd231
Joe Mason <joenotcharles@google.com>
2022-05-11 23:39:58 +0000

    Enable RenderWidgetHostTest.Background on Mac
    C++ tests can't directly instantiate RenderWidgetHostViewMac because
    it's in an Objective-C header file. This adds a
    CreateRenderWidgetHostViewMacForTesting function that creates a new
    RenderWidgetHostViewMac object and returns it as a pointer to the C++
    base class, RenderWidgetHostViewBase.
    
    Change-Id: I0690277654d035bc95bf8ca65e25dca3acc0086d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638151
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002392}

---
+++ content/browser/renderer_host/test_render_widget_host_view_mac_factory.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/test_render_widget_host_view_mac_factory.h"
+
+#include "content/browser/renderer_host/render_widget_host_view_mac.h"
+#include "content/public/browser/render_widget_host.h"
+
+namespace content {
+
+RenderWidgetHostViewBase* CreateRenderWidgetHostViewMacForTesting(
+    RenderWidgetHost* widget) {
+  return new RenderWidgetHostViewMac(widget);
+}
+
+}  // namespace content
