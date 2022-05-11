bfe362f7d94cbcde3e591927bd3a7bbd0baddb40
Jon Mann <jonmann@chromium.org>
2022-05-11 17:13:53 +0000

    android_sms: Disable integration as part of global preinstall.
    This change puts the Messages integration behind a new flag:
    EnableMessagesCrossDeviceIntegration to allow us to re-enable if needed.
    
    This change only removes Messages from the cross-device suite, the app
    is preinstalled as part of crrev/c/3640342, and the features which used
    to require custom changes are now available with web standards.
    
          cross-device settings, and no android_sms logs show up at
          chrome://multidevice-internals.
    
    Test: Manually confirmed that the Messages row is not present in
    Bug: b/207089892
    Change-Id: I13760613ae8198341502e5ac5572269216ae7929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3382360
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002149}

--- chrome/browser/ash/android_sms/android_sms_service_factory.cc
+++ chrome/browser/ash/android_sms/android_sms_service_factory.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/android_sms/android_sms_service_factory.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/services/multidevice_setup/public/cpp/prefs.h"
 #include "ash/services/multidevice_setup/public/mojom/multidevice_setup.mojom.h"
+#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/multidevice_setup/multidevice_setup_client_factory.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -26,6 +28,11 @@ namespace android_sms {
 namespace {
 
 bool ShouldStartAndroidSmsService(Profile* profile) {
+  if (base::FeatureList::IsEnabled(
+          features::kDisableMessagesCrossDeviceIntegration)) {
+    return false;
+  }
+
   const bool multidevice_feature_allowed = multidevice_setup::IsFeatureAllowed(
       multidevice_setup::mojom::Feature::kMessages, profile->GetPrefs());
 
