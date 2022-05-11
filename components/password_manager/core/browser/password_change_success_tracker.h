6c20ec2a1e6b581f7f2efee60f9f182b253247fb
Jan Keitel <jkeitel@google.com>
2022-05-11 05:56:41 +0000

    Add UKM metrics to PasswordChangeSuccessTracker.
    This CL implements to PasswordChangeMetricsRecorder interface to
    create a UKM recorder. Importantly, this recorder records all metrics
    to |NoURLSourceId| - UKM simply provides the more convenient data
    structure than UMA.
    
    Follow-up CLs will add unit testing for the Factory on Desktop/Android
    and also register the UKM recorder in the iOS-related factories.
    
    Privacy review is at: go/password-change-success-tracker-ukm
    
    Bug: 1281844
    Change-Id: Ief844d940d82ab8b7f4a8bfb298793a5ec072262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3634482
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001925}

--- components/password_manager/core/browser/password_change_success_tracker.h
+++ components/password_manager/core/browser/password_change_success_tracker.h
@@ -40,7 +40,8 @@ class PasswordChangeSuccessTracker : public KeyedService {
   // Start and end events for automated (i.e. Autofill Assistant-driven) and
   // manual flows (i.e. Chrome opens a CCT and a user updates a password on
   // their own).
-  // These values are persisted to prefs; do not reorder or renumber entries!
+  // These values are persisted to prefs and used in enums.xml; do not reorder
+  // or renumber entries!
   enum class StartEvent {
     // An automated password change flow.
     kAutomatedFlow = 0,
@@ -68,7 +69,8 @@ class PasswordChangeSuccessTracker : public KeyedService {
     kMaxValue = kManualResetLinkFlow
   };
 
-  // These values are persisted to prefs; do not reorder or renumber entries!
+  // These values are persisted to prefs and used in enums.xml; do not reorder
+  // or renumber entries!
   enum class EndEvent {
     // Automated password change flow completed with a generated password.
     kAutomatedFlowGeneratedPasswordChosen = 0,
@@ -95,7 +97,8 @@ class PasswordChangeSuccessTracker : public KeyedService {
   };
 
   // The place in Chrome where the password change flow originated.
-  // These values are persisted to prefs; do not reorder or renumber entries!
+  // These values are persisted to prefs and used in enums.xml; do not reorder
+  // or renumber entries!
   enum class EntryPoint {
     // Started after performing a password check in settings / the password
     // manager.
