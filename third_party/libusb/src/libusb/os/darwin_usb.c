386f72fe8456c063c5c10d1c809c2bfcf983596a
Avi Drissman <avi@chromium.org>
2022-05-11 17:16:04 +0000

    Make libusb macOS 10.12 compatible
    OSAtomic* calls are deprecated starting with macOS 10.12. Backport
    the fix from upstream, and then simplify it, as the 10.12 fix
    will work on all previous OSes.
    
    Bug: 1322548
    Change-Id: I78fac7f3b2900efb3d3005985e759cd0c62882f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637848
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002153}

--- third_party/libusb/src/libusb/os/darwin_usb.c
+++ third_party/libusb/src/libusb/os/darwin_usb.c
@@ -23,13 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ctype.h>
 #include <errno.h>
 #include <pthread.h>
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <libkern/OSAtomic.h>
 
 #include <mach/clock.h>
 #include <mach/clock_types.h>
@@ -51,7 +51,7 @@ static clock_serv_t clock_realtime;
 static clock_serv_t clock_monotonic;
 
 static CFRunLoopRef libusb_darwin_acfl = NULL; /* event cf loop */
-static volatile int32_t initCount = 0;
+_Atomic int32_t initCount = ATOMIC_VAR_INIT(0);
 
 static usbi_mutex_t darwin_cached_devices_lock = PTHREAD_MUTEX_INITIALIZER;
 static struct list_head darwin_cached_devices = {&darwin_cached_devices, &darwin_cached_devices};
@@ -419,7 +419,7 @@ static int darwin_init(struct libusb_context *ctx) {
     return rc;
   }
 
-  if (OSAtomicIncrement32Barrier(&initCount) == 1) {
+  if (atomic_fetch_add(&initCount, 1) == 0) {
     /* create the clocks that will be used */
 
     if (!initted) {
@@ -444,7 +444,7 @@ static int darwin_init(struct libusb_context *ctx) {
 }
 
 static void darwin_exit (void) {
-  if (OSAtomicDecrement32Barrier(&initCount) == 0) {
+  if (atomic_fetch_sub(&initCount, 1) == 1) {
     mach_port_deallocate(mach_task_self(), clock_realtime);
     mach_port_deallocate(mach_task_self(), clock_monotonic);
 
