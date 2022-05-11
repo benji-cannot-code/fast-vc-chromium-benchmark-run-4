8d2ee6eb1fbf82e8514058f881186d61c3caad4f
Eriko Kurimoto <elkurin@chromium.org>
2022-05-11 02:48:44 +0000

    [Resource file sharing] Clear or move shared resource file on lacros launch
    This CL lets ash delete shared resource file on the initial lacros launch after ash reboot, and move it to temporal location otherwise.
    This is to avoid being accessed asynchronously before the file is verified inside DataPackWithResourceSharing.
    
    This is a part of resource file sharing work.
    
    Bug: 1253280
    Change-Id: Iaf230abacf613cc4d7bb3cb2d9f1fa920699b54b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607579
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001882}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
+#include "base/path_service.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/process/launch.h"
 #include "base/process/process_handle.h"
@@ -67,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/common/channel_info.h"
+#include "chrome/common/chrome_paths.h"
 #include "chromeos/crosapi/cpp/crosapi_constants.h"
 #include "chromeos/crosapi/cpp/lacros_startup_state.h"
 #include "chromeos/startup/startup_switches.h"
@@ -85,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/temporary_shared_resource_path_chromeos.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
@@ -193,11 +196,44 @@ bool RotateLacrosLogs() {
   return false;
 }
 
+// Return false when there is a failure that might break resource file sharing
+// feature.
+bool ClearOrMoveSharedResourceFile(bool clear_shared_resource_file) {
+  base::FilePath shared_resource_path;
+  // If shared resource pak doesn't exit, do nothing.
+  if (!base::PathService::Get(chrome::FILE_RESOURCES_FOR_SHARING_PACK,
+                              &shared_resource_path) ||
+      !base::PathExists(shared_resource_path)) {
+    return true;
+  }
+
+  // Clear shared resource file cache if `clear_shared_resource_file` is true.
+  if (clear_shared_resource_file) {
+    if (!base::DeleteFile(shared_resource_path)) {
+      LOG(ERROR) << "Failed to delete cached shared resource file.";
+      return false;
+    }
+    return true;
+  }
+
+  base::FilePath renamed_shared_resource_path =
+      ui::GetPathForTemporarySharedResourceFile(shared_resource_path);
+
+  // Move shared resource pak to `renamed_shared_resource_path`.
+  if (!base::Move(shared_resource_path, renamed_shared_resource_path)) {
+    LOG(ERROR) << "Failed to move cached shared resource file to temporary "
+               << "location.";
+    return false;
+  }
+  return true;
+}
+
 // This method runs some work on a background thread prior to launching lacros.
 // The returns struct is used by the main thread as parameters to launch Lacros.
 LaunchParamsFromBackground DoLacrosBackgroundWorkPreLaunch(
     base::FilePath lacros_dir,
-    bool cleared_user_data_dir) {
+    bool cleared_user_data_dir,
+    bool clear_shared_resource_file) {
   LaunchParamsFromBackground params;
 
   if (!RotateLacrosLogs()) {
@@ -222,6 +258,20 @@ LaunchParamsFromBackground DoLacrosBackgroundWorkPreLaunch(
   }
 
   params.logfd = base::ScopedFD(fd);
+
+  params.enable_resource_file_sharing =
+      base::FeatureList::IsEnabled(features::kLacrosResourcesFileSharing);
+  // If resource file sharing feature is disabled, clear the cached shared
+  // resource file anyway.
+  if (!params.enable_resource_file_sharing)
+    clear_shared_resource_file = true;
+
+  // Clear shared resource file cache if it's initial lacros launch after ash
+  // reboot. If not, rename shared resource file cache to temporal name on
+  // Lacros launch.
+  if (!ClearOrMoveSharedResourceFile(clear_shared_resource_file))
+    params.enable_resource_file_sharing = false;
+
   return params;
 }
 
@@ -882,10 +932,14 @@ void BrowserManager::Start(
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock()},
       base::BindOnce(&DoLacrosBackgroundWorkPreLaunch, lacros_path_,
-                     cleared_user_data_dir),
+                     cleared_user_data_dir,
+                     is_initial_lacros_launch_after_reboot_),
       base::BindOnce(&BrowserManager::StartWithLogFile,
                      weak_factory_.GetWeakPtr(),
                      std::move(initial_browser_action)));
+
+  // Set false to prepare for the next Lacros launch.
+  is_initial_lacros_launch_after_reboot_ = false;
 }
 
 void BrowserManager::StartWithLogFile(
@@ -1050,7 +1104,7 @@ void BrowserManager::StartWithLogFile(
     command_line.AppendSwitch(switches::kEnableCrashpad);
   }
 
-  if (base::FeatureList::IsEnabled(features::kLacrosResourcesFileSharing)) {
+  if (params.enable_resource_file_sharing) {
     // Pass a flag to enable resources file sharing to Lacros.
     // To use resources file sharing feature on Lacros, it's required for ash to
     // run with enabling the feature as well since the feature is based on some
