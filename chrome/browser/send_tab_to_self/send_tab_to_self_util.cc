0c22f87aa831473b0f09989840ae41666d19aa63
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-05-11 16:07:14 +0000

    send-tab-to-self/desktop: Use WC::GetLastCommittedURL()/WC::GetTitle()
    This prepares for crrev.com/c/3637732 and has behavior changes.
    Before this CL, Chrome checked WebContents::GetURL() - aka
    GetVisibleURL() - to see if send-tab-to-self was allowed, but then
    shared NavigationEntry::GetURL(). This CL starts adopting
    WebContents::GetLastComittedURL() in both codesites. Future CLs will
    make sure the code is actually shared.
    The title is also now WebContents::GetTitle() instead of
    NavigationEntry::GetTitle(), which removes the NavigationEntry
    dependency.
    
    As far as I understand, GetLastCommittedURL() is a more secure choice
    than GetVisibleURL() because it represents the page the user sees,
    rather than what's written in the omnibox.
    
    Bug: 1274173
    Change-Id: I0aa9e9c494fca7e704bebd8395c43a92a09a176a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637894
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002124}

--- chrome/browser/send_tab_to_self/send_tab_to_self_util.cc
+++ chrome/browser/send_tab_to_self/send_tab_to_self_util.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/sync/driver/sync_service.h"
-#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
 
@@ -38,10 +37,7 @@ absl::optional<EntryPointDisplayReason> GetEntryPointDisplayReason(
   if (!web_contents)
     return absl::nullopt;
 
-  if (!web_contents->GetURL().SchemeIsHTTPOrHTTPS())
-    return absl::nullopt;
-
-  if (!web_contents->GetController().GetLastCommittedEntry())
+  if (!web_contents->GetLastCommittedURL().SchemeIsHTTPOrHTTPS())
     return absl::nullopt;
 
   Profile* profile =
