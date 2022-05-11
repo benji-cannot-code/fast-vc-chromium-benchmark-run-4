d6c1b7146f433729de69f025fe382d3eca32abeb
Gyuyoung Kim <gyuyoung@igalia.com>
2022-05-11 00:53:12 +0000

    MPArch: Convert DidFinishNavigation to PrimaryPageChanged in WebTimeNavigationObserver
    As a clean-up of fixing long tail of features for MPArch, this CL
    renames DidFinishNavigation to PrimaryPageChanged in
    WebTimeNavigationObserver, because PrimaryPageChanged is preferred
    when DidFinishNavigation only handles changes to the document.
    
    And also, the MPArch test guideline[1] allows converting to
    PrimaryPageChanged without a test when the code is simple.
    
    [1] https://docs.google.com/document/d/1ELmdCXAQheGKxJZs41_WdYoY3AMcJgwlPFR-u-onJm0/edit
    
    Bug: 1269645
    Change-Id: If4ebc14cf262eee47830de115ec848130cadf6b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632094
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001832}

--- chrome/browser/ash/child_accounts/time_limits/web_time_navigation_observer.h
+++ chrome/browser/ash/child_accounts/time_limits/web_time_navigation_observer.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class WebContents;
-class NavigationHandle;
 class NavigationEntry;
 }  // namespace content
 
@@ -65,8 +64,7 @@ class WebTimeNavigationObserver
   bool IsWebApp() const;
 
   // content::WebContentsObserver:
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
+  void PrimaryPageChanged(content::Page& page) override;
   void WebContentsDestroyed() override;
   void TitleWasSet(content::NavigationEntry* entry) override;
 
