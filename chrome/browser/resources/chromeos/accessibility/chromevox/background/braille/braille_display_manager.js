cf6e0d4330fc08bf53bc0b8a215e65c6fa90fcb8
Anastasia Helfinstein <anastasi@google.com>
2022-05-12 18:26:50 +0000

    [ChromeVox] Convert BrailleCaptionsBackground to an ES6-style class
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I99850a30e0c7cdda171a66a103eedc0bfa4f750c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631504
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002753}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_display_manager.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_display_manager.js
@@ -240,7 +240,8 @@ export class BrailleDisplayManager {
       processDisplayState(newState);
       localStorage['menuBrailleCommands'] = true;
     } else {
-      BrailleCaptionsBackground.getVirtualDisplayState(processDisplayState);
+      BrailleCaptionsBackground.getVirtualDisplayState().then(
+          processDisplayState);
       localStorage['menuBrailleCommands'] = false;
     }
   }
