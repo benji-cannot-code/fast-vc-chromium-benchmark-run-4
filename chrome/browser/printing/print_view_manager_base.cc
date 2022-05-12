ff8eee8190e2baad06afadbaa76cbf917c147406
Alan Screen <awscreen@chromium.org>
2022-05-12 20:30:12 +0000

    Remove OOP interactions for in-process printing
    For in-browser printing there should never be a client registered with
    PrintBackendService for OOP print operations.  Modify start of printing
    logic to only register with the PrintBackendService if the feature
    parameter for this is enabled.
    
    Fixed: 1324715
    Change-Id: I05eb6d076e0247c90cd266f216a93c47ec721e37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643716
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002819}

--- chrome/browser/printing/print_view_manager_base.cc
+++ chrome/browser/printing/print_view_manager_base.cc
@@ -326,10 +326,12 @@ bool PrintViewManagerBase::PrintNow(content::RenderFrameHost* rfh) {
     return false;
 
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
-  // Register this worker so that the service persists as long as the user
-  // keeps the system print dialog UI displayed.
-  if (!RegisterSystemPrintClient())
-    return false;
+  if (printing::features::kEnableOopPrintDriversJobPrint.Get()) {
+    // Register this worker so that the service persists as long as the user
+    // keeps the system print dialog UI displayed.
+    if (!RegisterSystemPrintClient())
+      return false;
+  }
 #endif
 
   SetPrintingRFH(rfh);
@@ -505,9 +507,11 @@ void PrintViewManagerBase::ScriptedPrintReply(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
-  // Finished getting all settings (defaults and from user), no further need
-  // to be registered as a system print client.
-  UnregisterSystemPrintClient();
+  if (printing::features::kEnableOopPrintDriversJobPrint.Get()) {
+    // Finished getting all settings (defaults and from user), no further need
+    // to be registered as a system print client.
+    UnregisterSystemPrintClient();
+  }
 #endif
   if (!content::RenderProcessHost::FromID(process_id)) {
     // Early return if the renderer is not alive.
@@ -982,10 +986,12 @@ void PrintViewManagerBase::ReleasePrintJob() {
   printing_rfh_ = nullptr;
 
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
-  // Ensure that any residual registration of printing client is released.
-  // This might be necessary in some abnormal cases, such as the associated
-  // render process having terminated.
-  UnregisterSystemPrintClient();
+  if (printing::features::kEnableOopPrintDriversJobPrint.Get()) {
+    // Ensure that any residual registration of printing client is released.
+    // This might be necessary in some abnormal cases, such as the associated
+    // render process having terminated.
+    UnregisterSystemPrintClient();
+  }
 #endif
 
   if (!print_job_)
