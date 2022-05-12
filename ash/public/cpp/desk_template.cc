81ac9a041b167201378c8e0d43b6e337a9cd8d3c
Richard Chui <richui@chromium.org>
2022-05-12 02:13:24 +0000

    Desk Templates: lacros side-by-side compatibility
    Because there will be a prolonged period of time where lacros will be
    running side by side with the classic browser, we want to support users
    saving lacros browser windows into desk templates, even if it is not the
    primary browser.
    
    We do not differentiate between the two browsers, so we will have
    interoperability and forward compatibility where windows will open up
    in whatever the default browser is.
    
    Bug: 1322242
    Test: Manual, on device
    Change-Id: I8e262f84e0ee320545a80aafbbc3d66e9b2e32a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642705
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002442}

--- ash/public/cpp/desk_template.cc
+++ ash/public/cpp/desk_template.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/app_types.h"
 #include "ash/constants/ash_features.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chromeos/crosapi/cpp/lacros_startup_state.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 
@@ -29,9 +28,7 @@ DeskTemplate::~DeskTemplate() = default;
 
 // static
 bool DeskTemplate::IsAppTypeSupported(aura::Window* window) {
-  // For now we'll ignore crostini windows in desk template. We'll also ignore
-  // lacros windows if it is not the primary browser. We'll also ignore ARC apps
-  // unless the flag is turned on.
+  // For now we'll ignore crostini windows in desk templates.
   const AppType app_type =
       static_cast<AppType>(window->GetProperty(aura::client::kAppType));
   switch (app_type) {
@@ -39,7 +36,6 @@ bool DeskTemplate::IsAppTypeSupported(aura::Window* window) {
     case AppType::CROSTINI_APP:
       return false;
     case AppType::LACROS:
-      return crosapi::lacros_startup_state::IsLacrosPrimaryEnabled();
     case AppType::ARC_APP:
     case AppType::BROWSER:
     case AppType::CHROME_APP:
