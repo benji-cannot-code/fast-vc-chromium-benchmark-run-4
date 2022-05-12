845bb7529c7ff28f9915382da096b9c1d7e38ada
Regan Hsu <hsuregan@google.com>
2022-05-12 23:41:31 +0000

    [CrOS SLP] Surface notification on newly active user sessions.
    Surface notification during the start of each newly active user session
    if the active SIM is locked and the policy is set to true.
    
    I.e if user dismisses the notification without taking action, locks the
    device and unlocks the device, or logs out and logs back in, the
    notification will surface again until the SIM is unlocked.
    
    Bug: b/228093904
    Change-Id: I79faa5cef12ff86f6a261a00d7dcdfc6cbffaa79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640955
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002922}

--- ash/system/network/managed_sim_lock_notifier.cc
+++ ash/system/network/managed_sim_lock_notifier.cc
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/network_config_service.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/public/cpp/system_tray_client.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "base/bind.h"
 #include "components/onc/onc_constants.h"
+#include "components/session_manager/session_manager_types.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
@@ -34,15 +36,26 @@ ManagedSimLockNotifier::ManagedSimLockNotifier() {
       remote_cros_network_config_.BindNewPipeAndPassReceiver());
   remote_cros_network_config_->AddObserver(
       cros_network_config_observer_receiver_.BindNewPipeAndPassRemote());
+  Shell::Get()->session_controller()->AddObserver(this);
 }
 
-ManagedSimLockNotifier::~ManagedSimLockNotifier() {}
+ManagedSimLockNotifier::~ManagedSimLockNotifier() {
+  Shell::Get()->session_controller()->RemoveObserver(this);
+}
+
+void ManagedSimLockNotifier::OnSessionStateChanged(
+    session_manager::SessionState state) {
+  if (Shell::Get()->session_controller()->GetSessionState() ==
+      session_manager::SessionState::ACTIVE) {
+    CheckGlobalNetworkConfiguration();
+  }
+}
 
 void ManagedSimLockNotifier::OnPoliciesApplied(const std::string& userhash) {
-  CheckGlobalNetworkConfiguarion();
+  CheckGlobalNetworkConfiguration();
 }
 
-void ManagedSimLockNotifier::CheckGlobalNetworkConfiguarion() {
+void ManagedSimLockNotifier::CheckGlobalNetworkConfiguration() {
   remote_cros_network_config_->GetGlobalPolicy(
       base::BindOnce(&ManagedSimLockNotifier::OnGetGlobalPolicy,
                      weak_ptr_factory_.GetWeakPtr()));
--- ash/system/network/managed_sim_lock_notifier.h
+++ ash/system/network/managed_sim_lock_notifier.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_NETWORK_MANAGED_SIM_LOCK_NOTIFIER_H_
 
 #include "ash/ash_export.h"
+#include "ash/public/cpp/session/session_observer.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
@@ -15,15 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 // Notifies the user to unlock the currently active PIN locked SIM.
-// TODO(b/228093904): Surface notification during the start of each newly active
-// session if SIM is locked and policy is set to true.
 // TODO(b/228093904): Surface notification if active network changes to network
 // with SIM locked and policy is set to true.
 // TODO(b/228093904): Remove notification if user successfully unlocks SIM.
 // TODO(b/228093904): Remove notification if active network changes to network
 // without SIM locked and policy is set to true.
 class ASH_EXPORT ManagedSimLockNotifier
-    : public chromeos::network_config::mojom::CrosNetworkConfigObserver {
+    : public SessionObserver,
+      public chromeos::network_config::mojom::CrosNetworkConfigObserver {
  public:
   ManagedSimLockNotifier();
   ManagedSimLockNotifier(const ManagedSimLockNotifier&) = delete;
@@ -32,6 +32,9 @@ class ASH_EXPORT ManagedSimLockNotifier
 
  private:
   friend class ManagedSimLockNotifierTest;
+
+  // SessionObserver:
+  void OnSessionStateChanged(session_manager::SessionState state) override;
 
   // CrosNetworkConfigObserver:
   void OnNetworkStateListChanged() override {}
@@ -53,7 +56,7 @@ class ASH_EXPORT ManagedSimLockNotifier
       chromeos::network_config::mojom::GlobalPolicyPtr global_policy);
 
   void RemoveNotification();
-  void CheckGlobalNetworkConfiguarion();
+  void CheckGlobalNetworkConfiguration();
   void MaybeShowNotification();
   void ShowNotification();
 
