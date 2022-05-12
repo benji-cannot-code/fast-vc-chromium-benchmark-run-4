d28a9e670234671096ddc1d2db725426d87332bc
Kalvin Lee <kdlee@chromium.org>
2022-05-12 01:56:38 +0000

    [PA] Namespace migration (34.4 of N)
    This CL removes the `base` alias for `PartitionStatsDumper` from the
    `partition_alloc_forward` header.
    
    This CL is being split off from crrev.com/c/3573827 because of an
    inexplicable test failure on the win10_chromium_x64_rel_ng bot:
    
    [  FAILED  ] rendering.desktop/filter_terrain_svg higher average
    frame_times(21.664) compared to upper limit (19.173)
    
    Bug: 1288247
    Change-Id: I2681cc468a5783d9bc9ccb1848e9131da716c9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600222
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002411}

--- third_party/blink/renderer/platform/wtf/allocator/partitions.cc
+++ third_party/blink/renderer/platform/wtf/allocator/partitions.cc
@@ -224,7 +224,7 @@ void Partitions::StartPeriodicReclaim(
 // static
 void Partitions::DumpMemoryStats(
     bool is_light_dump,
-    base::PartitionStatsDumper* partition_stats_dumper) {
+    partition_alloc::PartitionStatsDumper* partition_stats_dumper) {
   // Object model and rendering partitions are not thread safe and can be
   // accessed only on the main thread.
   DCHECK(IsMainThread());
@@ -242,7 +242,8 @@ void Partitions::DumpMemoryStats(
 
 namespace {
 
-class LightPartitionStatsDumperImpl : public base::PartitionStatsDumper {
+class LightPartitionStatsDumperImpl
+    : public partition_alloc::PartitionStatsDumper {
  public:
   LightPartitionStatsDumperImpl() : total_active_bytes_(0) {}
 
