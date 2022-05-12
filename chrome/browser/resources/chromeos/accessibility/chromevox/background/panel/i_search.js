7127f2eadec8797f89dc6d8fe218bce6d0606b06
Anastasia Helfinstein <anastasi@google.com>
2022-05-12 07:47:22 +0000

    [ChromeVox] Remove unnecessary export
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I61cc61e3da1059c0a1982180ce52db740cfe5abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637040
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002526}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/i_search.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/i_search.js
@@ -30,10 +30,6 @@ export class ISearch {
 
     /** @private {number} */
     this.callbackId_ = 0;
-
-    // Global exports.
-    /** Exported for the panel script. */
-    ChromeVox = chrome.extension.getBackgroundPage()['ChromeVox'];
   }
 
   /** @param {?ISearchHandler} handler */
