ef9097e4143e66850d189ca2ee595465b13c0cf7
Ed Chin <edchin@google.com>
2022-05-12 19:30:56 +0000

    [ios][first follow] Polish UI using ConfirmationAlertViewController.
    ConfirmationAlertViewController already has styling that we need.
    
    Note that the favicon should have a larger box area, shadow, and
    a corner green checkbox badge. This will be done in a separate CL.
    
    Screenshot:
    https://screenshot.googleplex.com/7fyCnGA4rpViZi6.png
    
    Bug: 1312124
    Change-Id: I1fb49ea113b87ed3a754bd34f0de051c0679f8b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641321
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002790}

--- ios/chrome/browser/ui/follow/first_follow_view_controller.h
+++ ios/chrome/browser/ui/follow/first_follow_view_controller.h
@@ -8,19 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-@protocol FirstFollowViewDelegate;
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+
 @protocol FirstFollowFaviconDataSource;
 @class FollowedWebChannel;
 
 // The UI that informs the user about the feed and following channels the
 // first few times the user follows any channel.
-@interface FirstFollowViewController : UIViewController
+@interface FirstFollowViewController : ConfirmationAlertViewController
 
 // The web channel that was recently followed.
 @property(nonatomic, strong) FollowedWebChannel* followedWebChannel;
-
-// Delegate to execute actions triggered in this UI.
-@property(nonatomic, weak) id<FirstFollowViewDelegate> delegate;
 
 // Data source for favicons.
 @property(nonatomic, weak) id<FirstFollowFaviconDataSource> faviconDataSource;
