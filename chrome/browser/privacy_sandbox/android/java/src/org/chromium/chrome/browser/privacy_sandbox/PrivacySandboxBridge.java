cacf63ff1af37a296af15896ceb3aa9e67fc58ec
Olesia Marukhno <olesiamarukhno@google.com>
2022-05-12 16:11:10 +0000

    [privacy sandbox settings] Rename DialogAction to PromptAction
    Bug: 1321587
    Change-Id: Ideca2f0c848265ff173a8e9b493310b80812be7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637992
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002665}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxBridge.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxBridge.java
@@ -93,8 +93,8 @@ public class PrivacySandboxBridge {
         return PrivacySandboxBridgeJni.get().getRequiredPromptType();
     }
 
-    public static void dialogActionOccurred(@DialogAction int action) {
-        PrivacySandboxBridgeJni.get().dialogActionOccurred(action);
+    public static void promptActionOccurred(@PromptAction int action) {
+        PrivacySandboxBridgeJni.get().promptActionOccurred(action);
     }
 
     @NativeMethods
@@ -116,6 +116,6 @@ public class PrivacySandboxBridge {
         Topic[] getBlockedTopics();
         void setTopicAllowed(int topicId, int taxonomyVersion, boolean allowed);
         int getRequiredPromptType();
-        void dialogActionOccurred(int action);
+        void promptActionOccurred(int action);
     }
 }
--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_notice_bubble_view.cc
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_notice_bubble_view.cc
@@ -26,15 +26,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view_class_properties.h"
 
-using DialogAction = PrivacySandboxService::DialogAction;
+using PromptAction = PrivacySandboxService::PromptAction;
 
 namespace {
 
 constexpr int kDialogWidth = 448;
 
-void NotifyServiceAboutDialogAction(Profile* profile, DialogAction action) {
+void NotifyServiceAboutPromptAction(Profile* profile, PromptAction action) {
   if (auto* service = PrivacySandboxServiceFactory::GetForProfile(profile)) {
-    service->DialogActionOccurred(action);
+    service->PromptActionOccurred(action);
   }
 }
 
@@ -73,12 +73,12 @@ class PrivacySandboxNoticeBubbleDelegate : public views::BubbleDialogDelegate {
   void WindowClosing() override {
     switch (GetWidget()->closed_reason()) {
       case views::Widget::ClosedReason::kEscKeyPressed:
-        NotifyServiceAboutDialogAction(browser_->profile(),
-                                       DialogAction::kNoticeDismiss);
+        NotifyServiceAboutPromptAction(browser_->profile(),
+                                       PromptAction::kNoticeDismiss);
         break;
       case views::Widget::ClosedReason::kUnspecified:
-        NotifyServiceAboutDialogAction(
-            browser_->profile(), DialogAction::kNoticeClosedNoInteraction);
+        NotifyServiceAboutPromptAction(
+            browser_->profile(), PromptAction::kNoticeClosedNoInteraction);
         break;
       default:
         break;
@@ -109,8 +109,8 @@ void ShowPrivacySandboxNoticeBubble(Browser* browser) {
           IDS_PRIVACY_SANDBOX_DIALOG_NOTICE_ACKNOWLEDGE_BUTTON));
   bubble_delegate->SetAcceptCallback(base::BindOnce(
       [](Browser* browser) {
-        NotifyServiceAboutDialogAction(browser->profile(),
-                                       DialogAction::kNoticeAcknowledge);
+        NotifyServiceAboutPromptAction(browser->profile(),
+                                       PromptAction::kNoticeAcknowledge);
       },
       base::Unretained(browser)));
 
@@ -121,8 +121,8 @@ void ShowPrivacySandboxNoticeBubble(Browser* browser) {
   bubble_delegate->SetCancelCallback(base::BindOnce(
       [](Browser* browser) {
         chrome::ShowPrivacySandboxSettings(browser);
-        NotifyServiceAboutDialogAction(browser->profile(),
-                                       DialogAction::kNoticeOpenSettings);
+        NotifyServiceAboutPromptAction(browser->profile(),
+                                       PromptAction::kNoticeOpenSettings);
       },
       base::Unretained(browser)));
 
@@ -199,8 +199,8 @@ PrivacySandboxNoticeBubbleView::PrivacySandboxNoticeBubbleView(
     app_menu_button->AddObserver(this);
   }
 
-  NotifyServiceAboutDialogAction(browser_->profile(),
-                                 DialogAction::kNoticeShown);
+  NotifyServiceAboutPromptAction(browser_->profile(),
+                                 PromptAction::kNoticeShown);
 }
 
 PrivacySandboxNoticeBubbleView::~PrivacySandboxNoticeBubbleView() {
@@ -210,15 +210,15 @@ PrivacySandboxNoticeBubbleView::~PrivacySandboxNoticeBubbleView() {
 }
 
 void PrivacySandboxNoticeBubbleView::AppMenuShown() {
-  NotifyServiceAboutDialogAction(browser_->profile(),
-                                 DialogAction::kNoticeDismiss);
+  NotifyServiceAboutPromptAction(browser_->profile(),
+                                 PromptAction::kNoticeDismiss);
   GetWidget()->CloseWithReason(views::Widget::ClosedReason::kLostFocus);
 }
 
 void PrivacySandboxNoticeBubbleView::OpenAboutAdPersonalizationSettings() {
   chrome::ShowPrivacySandboxLearnMore(browser_);
-  NotifyServiceAboutDialogAction(browser_->profile(),
-                                 DialogAction::kNoticeLearnMore);
+  NotifyServiceAboutPromptAction(browser_->profile(),
+                                 PromptAction::kNoticeLearnMore);
   GetWidget()->CloseWithReason(
       views::Widget::ClosedReason::kCancelButtonClicked);
 }
