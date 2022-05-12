e4b5a24b3049618c775c85d26eec8000fc96b05f
Tom Anderson <thomasanderson@chromium.org>
2022-05-12 21:51:46 +0000

    [QT] Add QtUi
    For now QtUi is a stub besides
    - getting the scale factor to verify things are hooked up correctly
    - detecting font changes to ensure the QT event loop is working
    
    Bug: 1317782
    Change-Id: I8dec8a108ff6f877a89bdfcddc8a257476106abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601270
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002866}

--- ui/views/linux_ui/linux_ui_factory.cc
+++ ui/views/linux_ui/linux_ui_factory.cc
@@ -11,10 +11,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(USE_GTK)
 #include "ui/gtk/gtk_ui_factory.h"
 #endif
+#if BUILDFLAG(USE_QT)
+#include "ui/qt/qt_ui.h"
+#endif
 
 std::unique_ptr<views::LinuxUI> CreateLinuxUi() {
   // TODO(thomasanderson): LinuxUI backend should be chosen depending on the
   // environment.
+#if BUILDFLAG(USE_QT)
+  auto qt_ui = qt::CreateQtUi();
+  if (qt_ui->Initialize())
+    return qt_ui;
+  qt_ui.reset();  // Reset to prevent 2 active LinuxUI instances.
+#endif
 #if BUILDFLAG(USE_GTK)
   {
     auto gtk_ui = BuildGtkUi();
