aec54b2ca0ff8b252364033ee433bf7b9c0b00aa
Rafael Cintron <rafael.cintron@microsoft.com>
2022-05-12 16:32:36 +0000

    Register VSyncThreadWin for suspend/resume events
    VSyncThread unconditionally fires VSync events for clients so long as
    there are observers in the list.
    
    If suspend events fire, we need to avoid firing events to conserve
    battery life. Clients of the VSyncThread may keep observers in the
    list if they become suspended before they're able to remove themselves
    from the list.
    
    Bug: 1324294
    Change-Id: Ieaa9e35da4cf81ced34d51ea58ac0aa05bcc0f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641042
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002673}

--- ui/gl/vsync_thread_win.h
+++ ui/gl/vsync_thread_win.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl/client.h>
 
 #include "base/containers/flat_set.h"
+#include "base/power_monitor/power_observer.h"
 #include "base/threading/thread.h"
 #include "ui/gl/gl_export.h"
 #include "ui/gl/vsync_provider_win.h"
@@ -27,12 +28,16 @@ class VSyncObserver;
 // can be added or removed on the main thread, and the vsync thread goes to
 // sleep if there are no observers. This is used by DirectCompositionSurfaceWin
 // to plumb vsync signal back to the display compositor's BeginFrameSource.
-class GL_EXPORT VSyncThreadWin {
+class GL_EXPORT VSyncThreadWin final : public base::PowerSuspendObserver {
  public:
   static VSyncThreadWin* GetInstance();
 
   VSyncThreadWin(const VSyncThreadWin&) = delete;
   VSyncThreadWin& operator=(const VSyncThreadWin&) = delete;
+
+  // Implementation of base::PowerSuspendObserver
+  void OnSuspend() final;
+  void OnResume() final;
 
   // These methods are not rentrancy safe, and shouldn't be called inside
   // VSyncObserver::OnVSync.  It's safe to assume that these can be called only
@@ -46,8 +51,9 @@ class GL_EXPORT VSyncThreadWin {
   friend struct base::DefaultSingletonTraits<VSyncThreadWin>;
 
   VSyncThreadWin();
-  ~VSyncThreadWin();
+  ~VSyncThreadWin() final;
 
+  void PostTaskIfNeeded();
   void WaitForVSync();
 
   base::Thread vsync_thread_;
@@ -59,7 +65,8 @@ class GL_EXPORT VSyncThreadWin {
   Microsoft::WRL::ComPtr<IDXGIOutput> primary_output_;
 
   base::Lock lock_;
-  bool GUARDED_BY(lock_) is_idle_ = true;
+  bool GUARDED_BY(lock_) is_vsync_task_posted_ = false;
+  bool GUARDED_BY(lock_) is_suspended_ = false;
   base::flat_set<VSyncObserver*> GUARDED_BY(lock_) observers_;
 };
 }  // namespace gl
