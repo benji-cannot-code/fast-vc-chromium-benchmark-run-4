626981527b32fd51034396c0551d143b31781d6a
Peter Kotwicz <pkotwicz@chromium.org>
2022-05-12 02:10:08 +0000

    [FedCM] Focus continue button in FedCM bubble single account view
    This CL focuses the continue button in the FedCM bubble single account
    view after the user selects an account in the multi account view.
    
    BUG=1324022
    
    Change-Id: I3bfc655ad223fbfb79eba777df83be974a0d708e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641017
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002436}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.cc
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.cc
@@ -239,7 +239,7 @@ AccountSelectionBubbleView::CreateSingleAccountChooser(
   if (brand_text_color_)
     button->SetEnabledTextColors(brand_text_color_);
   button->SetProminent(true);
-  row->AddChildView(std::move(button));
+  continue_button_ = row->AddChildView(std::move(button));
 
   // Do not add disclosure text if this is a sign in.
   if (account.login_state == Account::LoginState::kSignIn)
@@ -439,6 +439,7 @@ void AccountSelectionBubbleView::OnSingleAccountPicked(
   SizeToContents();
   PreferredSizeChanged();
 
+  continue_button_->RequestFocus();
   SendAccessibilityEvent(GetWidget());
 }
 
