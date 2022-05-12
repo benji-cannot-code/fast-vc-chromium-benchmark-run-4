9d28d4d12a0f774f54058e1f785d087d1cc79a47
Jeffrey Young <cowmoo@google.com>
2022-05-12 14:53:47 +0000

    personalization: log time delay of search results
    Add metric Apps.AppList.PersonalizationProvider.QueryTime
    to record delay of personalization search results.
    
    BUG=b:225950973
    
    Change-Id: Iec81c157438301941ce1d7928bdefad9daf3a7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3586100
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Eric Sum <esum@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002630}

--- chrome/browser/ui/app_list/search/personalization_provider.cc
+++ chrome/browser/ui/app_list/search/personalization_provider.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/personalization_app/search/search.mojom.h"
 #include "ash/webui/personalization_app/search/search_handler.h"
 #include "base/bind.h"
+#include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/web_applications/personalization_app/personalization_app_manager.h"
@@ -104,14 +106,17 @@ void PersonalizationProvider::Start(const std::u16string& query) {
   }
 
   if (icon_.isNull()) {
+    VLOG(1) << "No personalization icon for search results";
     return;
   }
 
   current_query_ = query;
   weak_ptr_factory_.InvalidateWeakPtrs();
-  search_handler_->Search(query, kNumRequestedResults,
-                          base::BindOnce(&PersonalizationProvider::OnSearchDone,
-                                         weak_ptr_factory_.GetWeakPtr()));
+  search_handler_->Search(
+      query, kNumRequestedResults,
+      base::BindOnce(&PersonalizationProvider::OnSearchDone,
+                     weak_ptr_factory_.GetWeakPtr(),
+                     /*start_time=*/base::TimeTicks::Now()));
 }
 
 ash::AppListSearchResultType PersonalizationProvider::ResultType() const {
@@ -145,6 +150,7 @@ void PersonalizationProvider::OnAppRegistryCacheWillBeDestroyed(
     apps::AppRegistryCache* cache) {}
 
 void PersonalizationProvider::OnSearchDone(
+    base::TimeTicks start_time,
     std::vector<::ash::personalization_app::mojom::SearchResultPtr> results) {
   SearchProvider::Results search_results;
   for (const auto& result : results) {
@@ -152,6 +158,10 @@ void PersonalizationProvider::OnSearchDone(
     search_results.push_back(std::make_unique<PersonalizationResult>(
         profile_, *result, current_query_, icon_));
   }
+
+  base::UmaHistogramTimes("Apps.AppList.PersonalizationProvider.QueryTime",
+                          base::TimeTicks::Now() - start_time);
+
   SwapResults(&search_results);
 }
 
--- chrome/browser/ui/app_list/search/personalization_provider.h
+++ chrome/browser/ui/app_list/search/personalization_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/personalization_app/search/search.mojom.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_forward.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/search_provider.h"
@@ -80,6 +81,7 @@ class PersonalizationProvider
 
  private:
   void OnSearchDone(
+      base::TimeTicks start_time,
       std::vector<::ash::personalization_app::mojom::SearchResultPtr> results);
 
   void StartLoadIcon();
