b2642192228c34c203b9baea553151b483df8991
Gang Wu <gangwu@chromium.org>
2022-05-12 23:06:31 +0000

    [TTS] Delete ContextualSearchEntityHeuristic.java
    Delete ContextualSearchEntityHeuristic.java and the metrics it records.
      Search.ContextualSearchEntitySeen
    
    Bug:1325102
    
    Change-Id: I33c96ba3862fbc9121d80382012cb090504646be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642731
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002909}

--- chrome/android/junit/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchContextTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchContextTest.java
@@ -102,11 +102,7 @@ public class ContextualSearchContextTest {
         assertFalse(mContext.hasValidSelection());
         assertFalse(mContext.canResolve());
         assertNull(mContext.getWordTapped());
-        assertNull(mContext.getWordPreviousToTap());
-        assertNull(mContext.getWordFollowingTap());
-        assertEquals(INVALID, mContext.getWordTappedOffset());
         assertEquals(INVALID, mContext.getTapOffsetWithinTappedWord());
-        assertEquals(INVALID, mContext.getWordFollowingTapOffset());
         assertNull(mContext.getSurroundingText());
         assertEquals(INVALID, mContext.getSelectionStartOffset());
         assertEquals(INVALID, mContext.getSelectionEndOffset());
@@ -170,17 +166,6 @@ public class ContextualSearchContextTest {
         setupResolvingTapInBarak();
         assertEquals("Barack", mContext.getWordTapped());
         assertEquals("Ba".length(), mContext.getTapOffsetWithinTappedWord());
-        assertEquals("Now ".length(), mContext.getWordTappedOffset());
-    }
-
-    @Test
-    @Feature({"ContextualSearch", "Context"})
-    public void testAnalysisOfWordsPreviousAndFollowing() {
-        setupResolvingTapInObama();
-        assertEquals("Barack", mContext.getWordPreviousToTap());
-        assertEquals("is", mContext.getWordFollowingTap());
-        assertEquals("Now ".length(), mContext.getWordPreviousToTapOffset());
-        assertEquals("Now Barack Obama ".length(), mContext.getWordFollowingTapOffset());
     }
 
     @Test
@@ -188,10 +173,8 @@ public class ContextualSearchContextTest {
     public void testAnalysisAtStartOfText() {
         int startOffset = 0;
         mContext.setSurroundingText(SAMPLE_TEXT, startOffset, startOffset);
-        assertNull(mContext.getWordPreviousToTap());
         // We can't recognize the first word because we need a space before it to do so.
         assertNull(mContext.getWordTapped());
-        assertNull(mContext.getWordFollowingTap());
     }
 
     @Test
@@ -199,9 +182,7 @@ public class ContextualSearchContextTest {
     public void testAnalysisAtSecondWordOfText() {
         int secondWordOffset = "Now ".length();
         mContext.setSurroundingText(SAMPLE_TEXT, secondWordOffset, secondWordOffset);
-        assertNull(mContext.getWordPreviousToTap());
         assertEquals("Barack", mContext.getWordTapped());
-        assertEquals("Obama", mContext.getWordFollowingTap());
     }
 
     @Test
@@ -209,9 +190,7 @@ public class ContextualSearchContextTest {
     public void testAnalysisAtEndOfText() {
         int endOffset = SAMPLE_TEXT.length();
         mContext.setSurroundingText(SAMPLE_TEXT, endOffset, endOffset);
-        assertNull(mContext.getWordPreviousToTap());
         assertNull(mContext.getWordTapped());
-        assertNull(mContext.getWordFollowingTap());
     }
 
     @Test
@@ -219,9 +198,7 @@ public class ContextualSearchContextTest {
     public void testAnalysisAtWordBeforeEndOfText() {
         int wordBeforeEndOffset = SAMPLE_TEXT.length() - "s ambiguous.".length();
         mContext.setSurroundingText(SAMPLE_TEXT, wordBeforeEndOffset, wordBeforeEndOffset);
-        assertEquals("Clinton", mContext.getWordPreviousToTap());
         assertEquals("is", mContext.getWordTapped());
-        assertEquals("ambiguous", mContext.getWordFollowingTap());
     }
 
     @Test
