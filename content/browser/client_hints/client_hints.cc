f83810f1e71df415d369ebeaa496a3a333055ad0
Dylan Cutler <dylancutler@google.com>
2022-05-12 23:08:15 +0000

    Send Sec-CH-Partioned-Cookies:?1 to embedded frames in the CHIPS OT
    Bug: 1324687
    Change-Id: I9922232d9231dd54af7555689ee23522f31185b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641657
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002910}

--- content/browser/client_hints/client_hints.cc
+++ content/browser/client_hints/client_hints.cc
@@ -499,7 +499,7 @@ bool IsOriginTrialHintEnabledForFrame(
 
 // TODO(crbug.com/1258063): Delete this function when the UserAgentReduction and
 // SendFullUserAgentAfterReduction Origin Trial is finished.
-void RemoveAllClientHintsExceptUaReducedOrUaDeprecation(
+void RemoveAllClientHintsExceptOriginTrialHints(
     const url::Origin& origin,
     FrameTreeNode* frame_tree_node,
     ClientHintsControllerDelegate* delegate,
@@ -511,7 +511,8 @@ void RemoveAllClientHintsExceptUaReducedOrUaDeprecation(
 
   for (auto it = accept_ch->begin(); it != accept_ch->end();) {
     if (*it == WebClientHintsType::kUAReduced ||
-        *it == WebClientHintsType::kFullUserAgent) {
+        *it == WebClientHintsType::kFullUserAgent ||
+        *it == WebClientHintsType::kPartitionedCookies) {
       ++it;
     } else {
       it = accept_ch->erase(it);
@@ -1068,7 +1069,7 @@ ParseAndPersistAcceptCHForNavigation(
   // TODO(crbug.com/1258063): Delete this call when the UserAgentReduction
   // Origin Trial is finished.
   if (!frame_tree_node->IsMainFrame()) {
-    RemoveAllClientHintsExceptUaReducedOrUaDeprecation(
+    RemoveAllClientHintsExceptOriginTrialHints(
         origin, frame_tree_node, delegate, &accept_ch, &main_frame_origin,
         &third_party_origin);
     if (accept_ch.empty()) {
