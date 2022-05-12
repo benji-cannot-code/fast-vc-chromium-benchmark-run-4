f4cee91ea5651ce7ea5394214c618c0d1441a0cd
Brian Sheedy <bsheedy@chromium.org>
2022-05-12 06:27:01 +0000

    Reland "Re-enable minidump tests on Mac"
    This is a reland of commit b15d89a6e5720b2d09a5950fac8ebe016721b2c6
    
    Differences from original CL:
    * Short circuits the tests on ARM-based Macs since the tests do not
      pass there yet.
    
    Original change's description:
    > Re-enable minidump tests on Mac
    >
    > Re-enables the Telemetry minidump tests on Mac since whatever flakiness
    > caused it to be disabled is likely fixed by now.
    >
    > Fixed: 1271097
    > Change-Id: I1391d836962fb140407a44c15134031803d910b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635630
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Commit-Queue: John Chen <johnchen@chromium.org>
    > Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1001624}
    
    Change-Id: I7e1227a023955371314b088c2cc42981b5e2dd70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639735
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002512}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import logging
 import os
+import platform
 import sys
 import time
 
@@ -65,10 +66,16 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'mac',  # https://crbug.com/1271097
       'win7'  # https://crbug.com/1084931
   )
   def testSymbolizeMinidump(self):
+    # This test currently does not work properly on ARM-based Macs, and there
+    # isn't currently a way to distinguish between architectures in the Disabled
+    # decorator.
+    if sys.platform == 'darwin' and platform.machine() == 'aarch64':
+      logging.warning('Short-circuiting test due to running on ARM-based Mac')
+      return
+
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var sam = "car";', 'sam')
     self._browser.tabs.New().Navigate('chrome://gpucrash', timeout=10)
@@ -111,10 +118,16 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'mac',  # https://crbug.com/1271097
       'win7'  # https://crbug.com/1084931
   )
   def testMultipleCrashMinidumps(self):
+    # This test currently does not work properly on ARM-based Macs, and there
+    # isn't currently a way to distinguish between architectures in the Disabled
+    # decorator.
+    if sys.platform == 'darwin' and platform.machine() == 'aarch64':
+      logging.warning('Short-circuiting test due to running on ARM-based Mac')
+      return
+
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var cat = "dog";', 'cat')
     self._browser.tabs.New().Navigate('chrome://gpucrash', timeout=10)
@@ -205,7 +218,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'mac',  # https://crbug.com/1271097
       'win7'  # https://crbug.com/1084931
   )
   def testMinidumpFromRendererHang(self):
@@ -215,6 +227,13 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
     and GPU processes in such cases so we can get minidumps for diagnosing the
     root cause.
     """
+    # This test currently does not work properly on ARM-based Macs, and there
+    # isn't currently a way to distinguish between architectures in the Disabled
+    # decorator.
+    if sys.platform == 'darwin' and platform.machine() == 'aarch64':
+      logging.warning('Short-circuiting test due to running on ARM-based Mac')
+      return
+
     self._LoadPageThenWait('var cat = "dog";', 'cat')
     try:
       self._browser.tabs[-1].Navigate('chrome://hang', timeout=10)
