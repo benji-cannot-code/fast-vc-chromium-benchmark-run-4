186fee5bcdcb18e3470112e993ea5676ca79f68a
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-05-12 13:50:55 +0000

    Refactor SendTabToSelfAndroidBridgeJni to return arrays by value
    No behavior is changed. Return them by value instead of passing an
    output parameter. The JNI API is now much more natural and easier to
    mock if needed.
    
    Bug: None
    Change-Id: I10ad19ee62f77caa2247006e66b0def7ecaf24c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641636
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002608}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/DevicePickerBottomSheetContent.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/DevicePickerBottomSheetContent.java
@@ -40,7 +40,6 @@ import org.chromium.ui.text.NoUnderlineClickableSpan;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -82,8 +81,8 @@ public class DevicePickerBottomSheetContent implements BottomSheetContent, OnIte
     }
 
     private void createContentView() {
-        List<TargetDeviceInfo> targetDeviceList = new ArrayList<TargetDeviceInfo>();
-        SendTabToSelfAndroidBridgeJni.get().getAllTargetDeviceInfos(mProfile, targetDeviceList);
+        List<TargetDeviceInfo> targetDeviceList =
+                SendTabToSelfAndroidBridge.getAllTargetDeviceInfos(mProfile);
 
         // First check if sharing is unavailable, e.g. because there are no target devices. If so,
         // show |sharingUnavailableView|, modulo adjusting the strings and the visibility of the
