226b3f4ba3a643d5d7371165c5943572dda6e21f
Ben Becker <benbecker@chromium.org>
2022-05-12 05:57:31 +0000

    CloseAll: Open context menu with long-press
    This CL gives the DesksBarView the ability to open a context menu on a
    mini view when it is being long-pressed and desks are not rearranged.
    This allows for the user to open a context menu with a long press while
    still being able to start a drag with a long press as well.
    
    Bug: 1308780
    Change-Id: I4d33f0e6c5281b8d11d604a5b26a0ac5cc9f7e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612344
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002501}

--- ash/wm/desks/desks_test_api.cc
+++ ash/wm/desks/desks_test_api.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/persistent_desks_bar_view.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
+#include "ui/views/controls/menu/menu_runner.h"
 
 namespace ash {
 
@@ -107,6 +108,7 @@ const ui::SimpleMenuModel& DesksTestApi::GetContextMenuModelForDesk(int index) {
   return GetContextMenuForDesk(index)->context_menu_model_;
 }
 
+// static
 views::View* DesksTestApi::GetHighlightOverlayForDeskPreview(int index) {
   return GetDesksBarView()
       ->mini_views()[index]
@@ -127,6 +129,11 @@ bool DesksTestApi::DesksControllerHasDesk(Desk* desk) {
 // static
 bool DesksTestApi::DesksControllerCanUndoDeskRemoval() {
   return DesksController::Get()->temporary_removed_desk_ != nullptr;
+}
+
+// static
+bool DesksTestApi::IsContextMenuRunningForDesk(int index) {
+  return GetContextMenuForDesk(index)->context_menu_runner_->IsRunning();
 }
 
 // static
