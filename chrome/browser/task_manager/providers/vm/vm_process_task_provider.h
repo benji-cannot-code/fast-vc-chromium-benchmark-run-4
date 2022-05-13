3a9776b49f18be146db7b718186f131828ad747d
Anton Romanov <romanton@google.com>
2022-05-13 20:52:58 +0000

    task manager: Rework VM task manager provider
    Stop trying to parse crosvm cmdline args and talk to concierge instead
    
    BUG=b:225278685
    TEST=tast.crostini.TaskManager.bullseye_stable and manually on volteer
    
    Change-Id: I6a75be4cce948460bf35748c5adb8b3165433450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646547
    Reviewed-by: David Munro <davidmunro@google.com>
    Commit-Queue: Anton Romanov <romanton@google.com>
    Auto-Submit: Anton Romanov <romanton@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003304}

--- chrome/browser/task_manager/providers/vm/vm_process_task_provider.h
+++ chrome/browser/task_manager/providers/vm/vm_process_task_provider.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/process_snapshot_server.h"
 #include "chrome/browser/task_manager/providers/task_provider.h"
 #include "chrome/browser/task_manager/providers/vm/vm_process_task.h"
+#include "chromeos/ash/components/dbus/concierge/concierge_service.pb.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace task_manager {
 
@@ -45,12 +47,20 @@ class VmProcessTaskProvider : public TaskProvider,
 
   void OnUpdateVmProcessList(const std::vector<VmProcessData>& vm_process_list);
 
+  // Called as a response to a ListVms made to the concierge.
+  void OnListVms(const base::ProcessIterator::ProcessEntries& snapshot,
+                 absl::optional<vm_tools::concierge::ListVmsResponse> response);
+
   // The time at which the most recent process snapshot was received from the
   // `ash::ProcessSnapshotServer`.
   base::Time last_process_snapshot_time_;
 
   // Map of PIDs to the corresponding Task object for a running VM.
   base::flat_map<base::ProcessId, std::unique_ptr<VmProcessTask>> task_map_;
+
+  // Always keep this the last member of this class to make sure it's the
+  // first thing to be destructed.
+  base::WeakPtrFactory<VmProcessTaskProvider> weak_ptr_factory_{this};
 };
 
 }  // namespace task_manager
