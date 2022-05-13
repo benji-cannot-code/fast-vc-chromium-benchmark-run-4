031b3ccec6a3f013dcab84560ab9929df6f51056
Gyuyoung Kim <gyuyoung@igalia.com>
2022-05-13 03:16:20 +0000

    MPArch: Convert DidFinishNavigation to PrimaryPageChanged in SideSearchSideContentsHelper
    As a clean-up of fixing long tail of features for MPArch, this CL
    renames DidFinishNavigation to PrimaryPageChanged in
    SideSearchSideContentsHelper, because PrimaryPageChanged
    is preferred when DidFinishNavigation only handles changes to the
    document.
    
    And also, the MPArch test guideline allows converting to
    PrimaryPageChanged without a test when the code is simple.
    
    Bug: 1269645
    Change-Id: Ic585d4d9acacb816b3d36671ab955a3b445a7006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640590
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002979}

--- chrome/browser/ui/side_search/side_search_side_contents_helper.h
+++ chrome/browser/ui/side_search/side_search_side_contents_helper.h
@@ -59,8 +59,7 @@ class SideSearchSideContentsHelper
       content::NavigationHandle* handle);
 
   // content::WebContentsObserver:
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
+  void PrimaryPageChanged(content::Page& page) override;
   void PrimaryMainFrameRenderProcessGone(
       base::TerminationStatus status) override;
 
