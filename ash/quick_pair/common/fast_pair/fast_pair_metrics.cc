4d75c35b4de70426ef0d03c5673b35f5dd87c4f8
Juliet Levesque <julietlevesque@google.com>
2022-05-13 23:09:08 +0000

    [Fast Pair] Extend UX Latency metric to 25 seconds
    Deprecates the previous UX Latency metric for initial and subsequent
    pairing protocols, and adds a new one to capture up to 15 seconds.
    This is necessary since our launch quality tracker wants the 99th
    percentile to be less than 11 seconds, but we currently only
    capture up to 10 seconds, and we are seeing a lot over overflow
    in these metrics.
    
    https://screenshot.googleplex.com/BkoZTzXP7F743hZ
    
    Test: Verified on DUT
    Change-Id: Ibb641f442af4ff235e74ee62d645f779f9eec674
    Fixed: b/229860613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608793
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003389}

--- ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
@@ -51,9 +51,9 @@ const char kEngagementFlowSubsequentMetric[] =
     "Bluetooth.ChromeOS.FastPair.EngagementFunnel.Steps."
     "SubsequentPairingProtocol";
 const char kTotalUxPairTimeInitialMetric[] =
-    "Bluetooth.ChromeOS.FastPair.TotalUxPairTime.InitialPairingProtocol";
+    "Bluetooth.ChromeOS.FastPair.TotalUxPairTime.InitialPairingProtocol2";
 const char kTotalUxPairTimeSubsequentMetric[] =
-    "Bluetooth.ChromeOS.FastPair.TotalUxPairTime.SubsequentPairingProtocol";
+    "Bluetooth.ChromeOS.FastPair.TotalUxPairTime.SubsequentPairingProtocol2";
 const char kRetroactiveEngagementFlowMetric[] =
     "Bluetooth.ChromeOS.FastPair.RetroactiveEngagementFunnel.Steps";
 const char kPairingMethodMetric[] = "Bluetooth.ChromeOS.FastPair.PairingMethod";
@@ -212,13 +212,16 @@ void AttemptRecordingTotalUxPairTime(const Device& device,
                                      base::TimeDelta total_pair_time) {
   switch (device.protocol) {
     case Protocol::kFastPairInitial:
-      base::UmaHistogramTimes(kTotalUxPairTimeInitialMetric, total_pair_time);
+      base::UmaHistogramCustomTimes(kTotalUxPairTimeInitialMetric,
+                                    total_pair_time, base::Milliseconds(1),
+                                    base::Seconds(25), 50);
       break;
     case Protocol::kFastPairRetroactive:
       break;
     case Protocol::kFastPairSubsequent:
-      base::UmaHistogramTimes(kTotalUxPairTimeSubsequentMetric,
-                              total_pair_time);
+      base::UmaHistogramCustomTimes(kTotalUxPairTimeSubsequentMetric,
+                                    total_pair_time, base::Milliseconds(1),
+                                    base::Seconds(25), 50);
       break;
   }
 }
