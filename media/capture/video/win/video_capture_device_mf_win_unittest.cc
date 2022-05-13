b270fc4a0f8e70165dc95734ec130f286dbcba0d
Ilya Nikolaevskiy <ilnik@chromium.org>
2022-05-13 08:27:31 +0000

    Reland "Process STOPPED and STARTED events in MF Video Capture Device"
    Reland with fixes
    
    Original CL introduced a deadlock:
    a worker thread was waiting in WaitOnCaptureEvent() for MF_CAPTURE_ENGINE_PREVIEW_STOPPED event while holding the |lock_| in StopAndDeallocate().
    Another thread was waiting for the lock in OnEvent() while processing some different event and therefore was unable to trigger the MF_CAPTURE_ENGINE_PREVIEW_STOPPED event.
    
    The new patchsets ensure that there's no WaitOnCaptureEvent call while holding the |lock_|.
    
    
    Bug: 1093521,1267782
    Change-Id: I20df1e7f009eb820b3934536054744a714bad1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641847
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003027}

--- media/capture/video/win/video_capture_device_mf_win_unittest.cc
+++ media/capture/video/win/video_capture_device_mf_win_unittest.cc
@@ -576,6 +576,7 @@ class MockMFCaptureEngine : public MockInterface<IMFCaptureEngine> {
 
   IFACEMETHODIMP StartPreview(void) override {
     OnStartPreview();
+    FireCaptureEvent(MF_CAPTURE_ENGINE_PREVIEW_STARTED, S_OK);
     return S_OK;
   }
 
@@ -583,6 +584,7 @@ class MockMFCaptureEngine : public MockInterface<IMFCaptureEngine> {
 
   IFACEMETHODIMP StopPreview(void) override {
     OnStopPreview();
+    FireCaptureEvent(MF_CAPTURE_ENGINE_PREVIEW_STOPPED, S_OK);
     return S_OK;
   }
 
