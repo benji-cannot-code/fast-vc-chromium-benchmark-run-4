963eff647fb3651b6769caf6637673a54143dc76
Byungwoo Lee <blee@igalia.com>
2022-05-13 09:29:42 +0000

    Support invalidation for location pseudo classes inside :has()
    Support invalidation for :link, :any-link and :target pseudo class
    inside :has().
    
    Invalidation for :visited inside :has() is not needed since :visited
    is not matched inside :has() to prevent leaking visitedness.
    
    Ignored invalidation of :scope inside :has() due to an issue in the
    csswg-draft.
    - https://github.com/w3c/csswg-drafts/issues/7211
    
    Bug: 669058, 1324834
    Change-Id: Ice42f0113f0cdc69a8054c2778c9cab3cdd5ef10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645177
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Byungwoo Lee <blee@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1003038}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4302,6 +4302,7 @@ struct Element::AffectedByPseudoStateChange {
             element.AncestorsOrSiblingsAffectedByActiveInHas();
         break;
 
+      case CSSSelector::kPseudoAnyLink:
       case CSSSelector::kPseudoChecked:
       case CSSSelector::kPseudoDefault:
       case CSSSelector::kPseudoDisabled:
@@ -4309,12 +4310,14 @@ struct Element::AffectedByPseudoStateChange {
       case CSSSelector::kPseudoIndeterminate:
       case CSSSelector::kPseudoInRange:
       case CSSSelector::kPseudoInvalid:
+      case CSSSelector::kPseudoLink:
       case CSSSelector::kPseudoOutOfRange:
       case CSSSelector::kPseudoOptional:
       case CSSSelector::kPseudoPlaceholderShown:
       case CSSSelector::kPseudoReadOnly:
       case CSSSelector::kPseudoReadWrite:
       case CSSSelector::kPseudoRequired:
+      case CSSSelector::kPseudoTarget:
       case CSSSelector::kPseudoValid:
         ancestors_or_siblings = true;
         break;
