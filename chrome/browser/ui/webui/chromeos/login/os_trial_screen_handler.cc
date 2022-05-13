0353c201682b342c0cf31e670ca2fcabb621128c
Danila Kuzmin <dkuzmin@google.com>
2022-05-13 13:39:53 +0000

    [OOBE][OsTrial]: Update screen structure
    Bug: 1325264, 1309022, 1298392
    Change-Id: I0f05586c09d979bcf4b3dee0e63a1f0454798915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647055
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003097}

--- chrome/browser/ui/webui/chromeos/login/os_trial_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/os_trial_screen_handler.cc
@@ -6,24 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/login/os_trial_screen_handler.h"
 
 #include "chrome/browser/ash/login/oobe_screen.h"
-#include "chrome/browser/ash/login/screens/os_trial_screen.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
 
 namespace chromeos {
 
-// static
-constexpr StaticOobeScreenId OsTrialScreenView::kScreenId;
+OsTrialScreenHandler::OsTrialScreenHandler() : BaseScreenHandler(kScreenId) {}
 
-OsTrialScreenHandler::OsTrialScreenHandler() : BaseScreenHandler(kScreenId) {
-  set_user_acted_method_path_deprecated("login.OsTrialScreen.userActed");
-}
-
-OsTrialScreenHandler::~OsTrialScreenHandler() {
-  if (screen_)
-    screen_->OnViewDestroyed(this);
-}
+OsTrialScreenHandler::~OsTrialScreenHandler() = default;
 
 void OsTrialScreenHandler::DeclareLocalizedValues(
     ::login::LocalizedValuesBuilder* builder) {
@@ -41,20 +32,8 @@ void OsTrialScreenHandler::DeclareLocalizedValues(
   builder->Add("osTrialNextButton", IDS_OS_TRIAL_NEXT_BUTTON);
 }
 
-void OsTrialScreenHandler::InitializeDeprecated() {}
-
 void OsTrialScreenHandler::Show() {
   ShowInWebUI();
-}
-
-void OsTrialScreenHandler::Bind(ash::OsTrialScreen* screen) {
-  screen_ = screen;
-  BaseScreenHandler::SetBaseScreenDeprecated(screen_);
-}
-
-void OsTrialScreenHandler::Unbind() {
-  screen_ = nullptr;
-  BaseScreenHandler::SetBaseScreenDeprecated(nullptr);
 }
 
 }  // namespace chromeos
