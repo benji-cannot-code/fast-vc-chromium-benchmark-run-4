24a42646b6d9d46dd309ac1e80970a90a99271c2
Osama Fathy <osamafathy@google.com>
2022-05-13 21:35:12 +0000

    OOBE Consolidated Consent: Mark EULA accepted after enrollment
    When the device is successfully enterprise enrolled, EULA should
    be marked as accepted since the administrator had already
    accepted EULA.
    
    This change will not affect the showing or skipping of the
    consolidated consent, but future operations on other components
    requires EULA to be accepted, which caused the failure of
    kiosk.AppCachedOffline tast test when the flag was enabled.
    
    EULA should also be marked as accepted when
    SkipPostLoginScreensForTesting() is called, since the consolidated
    consent screen will be one of the skipped screens.
    
    Bug: b:228194180
    Change-Id: Idc38ce62cc96afe65fc5176b907ab8ebdec772e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641765
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003329}

--- chrome/browser/ash/login/screens/consolidated_consent_screen.cc
+++ chrome/browser/ash/login/screens/consolidated_consent_screen.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/consolidated_consent_screen.h"
 
 #include "ash/components/arc/arc_prefs.h"
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -120,6 +121,9 @@ void ConsolidatedConsentScreen::OnViewDestroyed(
 
 bool ConsolidatedConsentScreen::MaybeSkip(WizardContext* context) {
   if (context->skip_post_login_screens_for_tests) {
+    if (features::IsOobeConsolidatedConsentEnabled())
+      StartupUtils::MarkEulaAccepted();
+
     exit_callback_.Run(Result::NOT_APPLICABLE);
     return true;
   }
