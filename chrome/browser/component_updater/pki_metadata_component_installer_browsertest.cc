157e072dfa60dcdd8daf27166764d1752c9c6729
Hubert Chao <hchao@chromium.org>
2022-05-13 21:53:42 +0000

    PKIMetadataBrowserTest: Disable CT enforcement for root store browser text explicitly as it seems to be enabled by default for official builds.
    (https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/net/system_network_context_manager.cc;l=884-900;drc=24c62dad2a729f8c0601c46e74959630484b6943)
    
    Bug: 1323673
    Change-Id: I399f92d7c4c86f21a22f00d8cc82f37243707c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3638762
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003340}

--- chrome/browser/component_updater/pki_metadata_component_installer_browsertest.cc
+++ chrome/browser/component_updater/pki_metadata_component_installer_browsertest.cc
@@ -184,6 +184,8 @@ class PKIMetadataComponentChromeRootStoreUpdateTest
       public PKIMetadataComponentInstallerService::Observer {
  public:
   void SetUpInProcessBrowserTestFixture() override {
+    SystemNetworkContextManager::SetEnableCertificateTransparencyForTesting(
+        false);
     PKIMetadataComponentInstallerService::GetInstance()->AddObserver(this);
     InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
     ASSERT_TRUE(component_dir_.CreateUniqueTempDir());
@@ -192,6 +194,8 @@ class PKIMetadataComponentChromeRootStoreUpdateTest
 
   void TearDownInProcessBrowserTestFixture() override {
     PKIMetadataComponentInstallerService::GetInstance()->RemoveObserver(this);
+    SystemNetworkContextManager::SetEnableCertificateTransparencyForTesting(
+        absl::nullopt);
   }
 
   class CRSWaiter {
