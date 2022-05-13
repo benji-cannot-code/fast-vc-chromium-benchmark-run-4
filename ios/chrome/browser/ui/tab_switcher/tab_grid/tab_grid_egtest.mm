3798cda33ca0c45f528033e0e5625b73d23b6da3
Gauthier Ambard <gambard@chromium.org>
2022-05-13 10:19:21 +0000

    [iOS] Re-enable multi-window tests
    It is possible that an EG test fixed them.
    Try to re-enable them.
    
    Fixed: 1321550, 1320270, 1320251, 1320268, 1320722, 1320858
    Change-Id: I849fa1f1ed8a5b5519be506340d201b8e70cb9a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629581
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003049}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_egtest.mm
@@ -759,8 +759,7 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 
 // Tests that dragging a tab grid incognito item to the edge opens a new window
 // and that the tab is properly transferred, incuding navigation stack.
-// TODO(crbug.com/1320251): Test fails.
-- (void)DISABLED_testIncognitoDragAndDropAtEdgeToCreateNewWindow {
+- (void)testIncognitoDragAndDropAtEdgeToCreateNewWindow {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
 
@@ -810,8 +809,7 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 }
 
 // Tests dragging tab grid item between windows.
-// TODO(crbug.com/1320251): Test fails.
-- (void)DISABLED_testDragAndDropBetweenWindows {
+- (void)testDragAndDropBetweenWindows {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
 
@@ -893,8 +891,8 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 }
 
 // Tests dragging incognito tab grid item between windows.
-// TODO(crbug.com/1320251): Test fails.
-- (void)DISABLED_testDragAndDropIncognitoBetweenWindows {
+// TODO(crbug.com/1325246): Re-enable this test.
+- (void)FLAKY_testDragAndDropIncognitoBetweenWindows {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
 
@@ -967,8 +965,7 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 }
 
 // Tests dragging tab grid item as URL between windows.
-// TODO(crbug.com/1320251): Test fails.
-- (void)DISABLED_testDragAndDropURLBetweenWindows {
+- (void)testDragAndDropURLBetweenWindows {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
 
@@ -1029,8 +1026,7 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 }
 
 // Tests dragging tab grid incognito item as URL to a main windows.
-// TODO(crbug.com/1320251): Fails on device.
-- (void)DISABLED_testDragAndDropIncognitoURLInMainWindow {
+- (void)testDragAndDropIncognitoURLInMainWindow {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
 
@@ -1094,17 +1090,9 @@ void EchoURLDefaultSearchEngineResponseProvider::GetResponseHeadersAndBody(
 }
 
 // Tests dragging tab grid main item as URL to an incognito windows.
-// TODO(crbug.com/1320251): Fails on device.
-- (void)DISABLED_testDragAndDropMainURLInIncognitoWindow {
+- (void)testDragAndDropMainURLInIncognitoWindow {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_SKIPPED(@"Multiple windows can't be opened.");
-
-// TODO(crbug.com/1184267): Test is flaky on iPad devices.
-#if !TARGET_IPHONE_SIMULATOR
-  if ([ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_DISABLED(@"This test is flaky on iPad devices.");
-  }
-#endif
 
   // Setup first window with one incognito tab 1.
   [ChromeEarlGrey closeAllNormalTabs];
