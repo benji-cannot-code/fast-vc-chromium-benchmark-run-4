63b6cdc1394e9658c256cbc3243365e25d6eb524
Renato Silva <rrsilva@google.com>
2022-05-13 19:53:25 +0000

    OOBE - Add Polymer3 feature flag for Add Person flow
    Add a feature flag that will turn on Polymer3 in the OOBE WebUI
    when using the 'Add Person' flow.
    
    Bug: 1184731
    Change-Id: Iaea898b3820d12cd36c8696edbdc893c8513d60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647687
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003280}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -240,6 +240,8 @@ extern const base::Feature kEnableOobeChromeVoxHint;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kEnableOobePolymer3;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kOobeAddPersonPolymer3;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kEnableKioskEnrollmentInOobe;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kEnableOobeNetworkScreenSkip;
@@ -711,6 +713,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOAuthIppEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeChromeVoxHintEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeHidDetectionRevampEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobePolymer3Enabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeAddPersonPolymer3Enabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsKioskEnrollmentInOobeEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeNetworkScreenSkipEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsOobeConsolidatedConsentEnabled();
