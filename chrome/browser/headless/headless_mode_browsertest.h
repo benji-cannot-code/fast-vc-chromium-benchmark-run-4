17634c1ff1a17919750303afd84453dbe78c0b13
Peter Kvitek <kvitekp@chromium.org>
2022-05-13 19:24:08 +0000

    [headless] Mac: Fixed fullscreen native headless window visibility.
    AppKit implicitly makes the fullscreen window visible, so with the previous native headless mode Mac implementation going fullscreen will result in the desktop window being visible.
    
    This CL fixes the problem by preventing platform window from going fullscreen and adds the relevant tests.
    
    Bug: 1322563
    Change-Id: Ie1f098353f258d310f1d221c2f6d52fe26ec0a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645537
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003262}

--- chrome/browser/headless/headless_mode_browsertest.h
+++ chrome/browser/headless/headless_mode_browsertest.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 class HeadlessModeBrowserTest : public InProcessBrowserTest {
  public:
@@ -22,6 +23,30 @@ class HeadlessModeBrowserTest : public InProcessBrowserTest {
 
   void SetUpCommandLine(base::CommandLine* command_line) override;
   void SetUpOnMainThread() override;
+};
+
+enum StartWindowMode {
+  kStartWindowNormal,
+  kStartWindowMaximized,
+  kStartWindowFullscreen,
+};
+
+class HeadlessModeBrowserTestWithStartWindowMode
+    : public HeadlessModeBrowserTest,
+      public testing::WithParamInterface<StartWindowMode> {
+ public:
+  HeadlessModeBrowserTestWithStartWindowMode() = default;
+
+  HeadlessModeBrowserTestWithStartWindowMode(
+      const HeadlessModeBrowserTestWithStartWindowMode&) = delete;
+  HeadlessModeBrowserTestWithStartWindowMode& operator=(
+      const HeadlessModeBrowserTestWithStartWindowMode&) = delete;
+
+  ~HeadlessModeBrowserTestWithStartWindowMode() override = default;
+
+  void SetUpCommandLine(base::CommandLine* command_line) override;
+
+  StartWindowMode start_window_mode() const { return GetParam(); };
 };
 
 #endif  // CHROME_BROWSER_HEADLESS_HEADLESS_MODE_BROWSERTEST_H_
--- chrome/browser/headless/headless_mode_browsertest_mac.mm
+++ chrome/browser/headless/headless_mode_browsertest_mac.mm
@@ -12,7 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-IN_PROC_BROWSER_TEST_F(HeadlessModeBrowserTest,
+INSTANTIATE_TEST_SUITE_P(HeadlessModeBrowserTestWithStartWindowMode,
+                         HeadlessModeBrowserTestWithStartWindowMode,
+                         testing::Values(kStartWindowNormal,
+                                         kStartWindowMaximized,
+                                         kStartWindowFullscreen));
+
+IN_PROC_BROWSER_TEST_P(HeadlessModeBrowserTestWithStartWindowMode,
                        BrowserDesktopWindowVisibility) {
   // On Mac, the Native Headless Chrome browser window exists and is
   // visible, while the underlying platform window is hidden.
