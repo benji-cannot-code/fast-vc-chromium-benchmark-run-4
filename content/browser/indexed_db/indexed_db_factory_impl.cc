8255d738bf57668dd86507abcfd839a98f4792b9
Christine Smith <christinesm@chromium.org>
2022-05-13 20:38:23 +0000

    [Storage Code Health] Remove expired historgam WebCore.IndexedDB.DestroyCorruptBackingStoreStatus*
    Removes the expired histogram WebCore.IndexedDB.DestroyCorruptBackingStoreStatus
    
    Bug: 1125838, 1314419
    Change-Id: I1e76b6ca674c2a7abbddfeb5bd9c233f3abb758a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645629
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003298}

--- content/browser/indexed_db/indexed_db_factory_impl.cc
+++ content/browser/indexed_db/indexed_db_factory_impl.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
@@ -457,10 +456,6 @@ void IndexedDBFactoryImpl::HandleBackingStoreCorruption(
   leveldb::Status s =
       class_factory_->leveldb_factory().DestroyLevelDB(file_path);
   DLOG_IF(ERROR, !s.ok()) << "Unable to delete backing store: " << s.ToString();
-  base::UmaHistogramEnumeration(
-      "WebCore.IndexedDB.DestroyCorruptBackingStoreStatus",
-      leveldb_env::GetLevelDBStatusUMAValue(s),
-      leveldb_env::LEVELDB_STATUS_MAX);
 }
 
 std::vector<IndexedDBDatabase*> IndexedDBFactoryImpl::GetOpenDatabasesForBucket(
@@ -862,10 +857,7 @@ IndexedDBFactoryImpl::OpenAndVerifyIndexedDBBackingStore(
       // This is a special case where we want to make sure the database is
       // deleted, so we try to delete again.
       status = class_factory_->leveldb_factory().DestroyLevelDB(database_path);
-      base::UmaHistogramEnumeration(
-          "WebCore.IndexedDB.DestroyCorruptBackingStoreStatus",
-          leveldb_env::GetLevelDBStatusUMAValue(status),
-          leveldb_env::LEVELDB_STATUS_MAX);
+
       if (UNLIKELY(!status.ok())) {
         LOG(ERROR) << "Unable to delete backing store: " << status.ToString();
         return {nullptr, status, data_loss_info, /*is_disk_full=*/false};
