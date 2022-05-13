1ef03e0b6ef005283260524ca48f1e03295e1a8f
junwei <junwei.fu@intel.com>
2022-05-13 08:52:42 +0000

    webml: Add IDL definition of web neural network API.
    The CL shares ML and MLContext interface defined in webml, and more CLs will to
    be implemented the IDL definition.
    
    Intent to prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/PD6TDMDS9mg/m/5WT5rCPCAQAJ
    Explainer:
    https://github.com/webmachinelearning/webnn/blob/main/explainer.md
    Chrome Status:
    https://chromestatus.com/feature/5738583487938560
    
    Bug: 1273291
    Test: combined with the other CLs, the API works.
    Change-Id: I47a766645d7c33a4ddaebb300e5a4707e347c728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583762
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Honglin Yu <honglinyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003032}

---
+++ third_party/blink/renderer/modules/ml/webnn/ml_operand.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_OPERAND_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_OPERAND_H_
+
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/heap/visitor.h"
+
+namespace blink {
+
+class MLContext;
+
+class MLOperand : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit MLOperand(MLContext* context);
+
+  MLOperand(const MLOperand&) = delete;
+  MLOperand& operator=(const MLOperand&) = delete;
+
+  ~MLOperand() override;
+
+  void Trace(Visitor* visitor) const override;
+
+ private:
+  Member<MLContext> ml_context_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_OPERAND_H_
