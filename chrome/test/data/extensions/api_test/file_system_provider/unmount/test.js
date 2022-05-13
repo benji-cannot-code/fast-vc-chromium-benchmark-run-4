b2d6a15a5fc58612678d9d09e6eb016c0a3da216
Marcello Salomao <msalomao@google.com>
2022-05-13 04:58:33 +0000

    Reland "[Files App] Fix error handling in volume_manager_impl.js"
    This is partial a reland of commit 147b3c95076e9fa5c27b7dee71da6a9ac7bbb9d8
    
    In addition to the original code, a second patchset was added
    that removes the chrome.runtime.lastError checking from one
    of the tests where the error is actually
    expected/tolerated/ignored.
    
    Original change's description:
    > [Files App] Fix error handling in volume_manager_impl.js
    >
    > Currently, some methods in volume_manager_impl.js aren't checking
    > for synchronous errors coming from FileManagerPrivateAPI. We are
    > addressing this so chrome.runtime.lastError messages aren't left
    > unchecked in case some of such errors are triggered.
    >
    > Bug: 1238564
    > Test: CQ
    >
    > Cq-Depends: 3631859
    > Change-Id: Ibfa0e6056c6066bdf31bb70e1562ffc377509883
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637077
    > Auto-Submit: Marcello Salomao <msalomao@google.com>
    > Reviewed-by: François Degros <fdegros@chromium.org>
    > Commit-Queue: François Degros <fdegros@chromium.org>
    > Commit-Queue: Marcello Salomao <msalomao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1002396}
    
    Bug: 1238564
    Change-Id: Ie5b85d3f987cd7425a361992ed6696f2262a9318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647108
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002995}

--- chrome/test/data/extensions/api_test/file_system_provider/unmount/test.js
+++ chrome/test/data/extensions/api_test/file_system_provider/unmount/test.js
@@ -94,7 +94,9 @@ function runTests() {
 
       test_util.getVolumeInfo(SECOND_FILE_SYSTEM_ID, function(volumeInfo) {
         chrome.test.assertTrue(!!volumeInfo);
-        chrome.fileManagerPrivate.removeMount(volumeInfo.volumeId);
+        chrome.fileManagerPrivate.removeMount(volumeInfo.volumeId, () => {
+          chrome.test.assertNoLastError();
+        });
       });
     },
 
@@ -126,7 +128,7 @@ function runTests() {
         chrome.test.assertTrue(unmountRequested);
 
         // Remove the handlers and mark the test as succeeded.
-        chrome.fileManagerPrivate.removeMount(SECOND_FILE_SYSTEM_ID);
+        chrome.fileManagerPrivate.removeMount(SECOND_FILE_SYSTEM_ID, () => {});
         chrome.fileManagerPrivate.onMountCompleted.removeListener(
             onMountCompleted);
       });
@@ -137,7 +139,9 @@ function runTests() {
 
       test_util.getVolumeInfo(SECOND_FILE_SYSTEM_ID, function(volumeInfo) {
         chrome.test.assertTrue(!!volumeInfo);
-        chrome.fileManagerPrivate.removeMount(volumeInfo.volumeId);
+        chrome.fileManagerPrivate.removeMount(volumeInfo.volumeId, () => {
+          chrome.test.assertNoLastError();
+        });
       });
     }
   ]);
