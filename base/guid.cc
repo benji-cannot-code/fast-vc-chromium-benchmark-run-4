be6b27f55cc6ae98ba4883cd1848770666db2d0f
Vaclav Brozek <vabr@chromium.org>
2022-05-13 20:53:58 +0000

    Enable moving base::GUID
    In https://chromium-review.googlesource.com/c/chromium/src/+/3642713, we
    hit that base::GUID has no move constructor, so this CL adds that (and
    operator=).
    
    Bug: 1026195
    Change-Id: Ic077dc5c628a98441fd9ce8b0dd7bb18074e2d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648183
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003305}

--- base/guid.cc
+++ base/guid.cc
@@ -142,6 +142,10 @@ GUID::GUID(const GUID& other) = default;
 
 GUID& GUID::operator=(const GUID& other) = default;
 
+GUID::GUID(GUID&& other) = default;
+
+GUID& GUID::operator=(GUID&& other) = default;
+
 const std::string& GUID::AsLowercaseString() const {
   return lowercase_;
 }
