195b74a7438566f7897004f1ee6e89e3f0a29935
Colin Kincaid <ckincaid@chromium.org>
2022-05-13 18:12:31 +0000

    Fix fingerprint enrollment animations for dark mode (Part 8 of 8)
    This CL makes the fingerprint-enrollment-done check mark animation play
    when a fingerprint is enrolled in OOBE.
    
    Before: https://screenshot.googleplex.com/8a4YY2fFuRvVzra
    After: https://screenshot.googleplex.com/Ba7wZrb33kRfkL9
    
    Fixed: 1295944
    Change-Id: I208b96f7a05fb99cd8e4ec24ffc30bcd5e3762b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646357
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003220}

--- ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.js
+++ ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.js
@@ -400,6 +400,7 @@ Polymer({
     const scanningAnimation =
         /** @type {CrLottieElement|HTMLElement} */ (this.$.scanningAnimation);
     scanningAnimation.singleLoop = true;
+    scanningAnimation.autoplay = true;
     scanningAnimation.classList.remove('translucent');
     this.updateAnimationAsset_();
     this.resizeCheckMark_(scanningAnimation);
