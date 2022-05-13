52141311ccc3c866673dbc931f592088e1123d3b
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-05-13 16:17:39 +0000

    Fix dangling raw_ptr in TabStripPageHandler / WebuiTabStrip.
    The |browser_| and |embedder_| attributes from the TabStripPageHandler
    might become dangling. This may results in UAF.
    
    This patch resets the borrowed references and clear the objects
    depending on them when they go out of scope.
    
    [Motivation]
    This is a dependency of:
    https://chromium-review.googlesource.com/c/chromium/src/+/3620393
    This deletes WebContents in its own task, which causes the dangling
    raw_ptr to become real UAF and one test to fail.
    
    Bug: 1308391,1311962
    Change-Id: I1c56052069de76e304502bf76108d79e2236b2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3634520
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003148}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
@@ -119,6 +119,7 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   void CloseForEventOutsideTabStrip(TabStripUICloseAction reason);
 
   void InitializeWebView();
+  void DeinitializeWebView();
 
   // TabStripUIEmbedder:
   const ui::AcceleratorProvider* GetAcceleratorProvider() const override;
