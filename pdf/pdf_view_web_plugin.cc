85dd156d8fd60165cd6e870e76a7ae81024fe5d7
K. Moon <kmoon@chromium.org>
2022-05-14 01:04:21 +0000

    [pdf] Use mock blink::WebAssociatedURLLoader
    Switches PdfViewWebPluginTest to use a mock WebAssociatedURLLoader,
    instead of passing a test UrlLoader to PdfViewWebPlugin's
    InitializeForTesting().
    
    The latter approach doesn't handle creating more than one URL loader,
    and in practice, current tests don't expect anything from the mock
    UrlLoader.
    
    Bug: 1323307
    Change-Id: Ic5eded2b1213ff01bc6de6e8cd322396448c6534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630825
    Reviewed-by: Nigi <nigi@chromium.org>
    Commit-Queue: Nigi <nigi@chromium.org>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003429}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -174,9 +174,7 @@ bool PdfViewWebPlugin::Initialize(blink::WebPluginContainer* container) {
 }
 
 bool PdfViewWebPlugin::InitializeForTesting(
-    std::unique_ptr<PDFiumEngine> engine,
-    std::unique_ptr<UrlLoader> loader) {
-  test_loader_ = std::move(loader);
+    std::unique_ptr<PDFiumEngine> engine) {
   return InitializeCommon(std::move(engine));
 }
 
@@ -760,9 +758,6 @@ base::WeakPtr<PdfViewPluginBase> PdfViewWebPlugin::GetWeakPtr() {
 }
 
 std::unique_ptr<UrlLoader> PdfViewWebPlugin::CreateUrlLoaderInternal() {
-  if (test_loader_)
-    return std::move(test_loader_);
-
   auto loader = std::make_unique<BlinkUrlLoader>(weak_factory_.GetWeakPtr());
   loader->GrantUniversalAccess();
   return loader;
