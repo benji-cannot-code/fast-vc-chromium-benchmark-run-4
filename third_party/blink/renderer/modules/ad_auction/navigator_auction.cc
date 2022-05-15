1e8789a82bef33e4ac5069fa46ed13ca62f6f00b
Matt Menke <mmenke@chromium.org>
2022-05-15 21:02:46 +0000

    [FLEDGE] Add seller URL origin validation in the renderer.
    More specifically, validate that the decision logic URL and, if
    present, the trusted scoring signals URL are HTTPS and same-origin
    to the seller, and throw an exception on failure.
    
    Both of these were previously being checked in the browser process,
    which would complete the auction with an empty result instead of
    rejecting the promise. This change is also needed to add a typemapping,
    with validate, for AuctionAdConfig, since a config with URLs with
    mismatched origins should be considered invalid.
    
    This CL also adds parsing of the (unused) seller field for PARAKEET,
    so that it can shared the decision logic URL validation. PARAKEET
    reuses FLEDGE's AuctionAdConfig, so any Mojo validation will
    apply to PARAKEET as well, and it will require a seller field.
    PARAKEET piggybacks on FLEDGE by adding to it, and also uses
    decision_logic_urls. Even if it ignores the seller field, it should
    be fine to require it as well.
    
    The CL updates PARAKEET to consider bad AuctionAdConfig's Mojo errors,
    which happen as part of the type traits switch. It doesn't do it for
    the FLEDGE path, because most of the tests for that are integration
    tests, which I'll be removing in favor of unit tests in another CL.
    
    Bug: 1293563, 1271540
    Change-Id: Id6fb5a44f27e882488746b3037289eb3a3d182dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642756
    Reviewed-by: Brandon Maslen <brandm@microsoft.com>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003573}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -534,7 +534,19 @@ bool CopyDecisionLogicUrlFromIdlToMojo(const ExecutionContext& context,
         "cannot be resolved to a valid URL."));
     return false;
   }
-  // TODO(https://crbug.com/1271540): Validate against seller origin.
+
+  // Need to check scheme of the URL in addition to comparing origins because
+  // FLEDGE currently only supports HTTPS URLs, and some non-HTTPS URLs can have
+  // HTTPS origins.
+  if (decision_logic_url.Protocol() != url::kHttpsScheme ||
+      !output.seller->IsSameOriginWith(
+          SecurityOrigin::Create(decision_logic_url).get())) {
+    exception_state.ThrowTypeError(ErrorInvalidAuctionConfig(
+        input, "decisionLogicUrl", input.decisionLogicUrl(),
+        "must match seller origin."));
+    return false;
+  }
+
   output.decision_logic_url = decision_logic_url;
   return true;
 }
@@ -554,7 +566,19 @@ bool CopyTrustedScoringSignalsFromIdlToMojo(
         "cannot be resolved to a valid URL."));
     return false;
   }
-  // TODO(https://crbug.com/1271540): Validate against seller origin.
+
+  // Need to check scheme of the URL in addition to comparing origins because
+  // FLEDGE currently only supports HTTPS URLs, and some non-HTTPS URLs can have
+  // HTTPS origins.
+  if (trusted_scoring_signals_url.Protocol() != url::kHttpsScheme ||
+      !output.seller->IsSameOriginWith(
+          SecurityOrigin::Create(trusted_scoring_signals_url).get())) {
+    exception_state.ThrowTypeError(ErrorInvalidAuctionConfig(
+        input, "trustedScoringSignalsUrl", input.trustedScoringSignalsUrl(),
+        "must match seller origin."));
+    return false;
+  }
+
   output.trusted_scoring_signals_url = trusted_scoring_signals_url;
   return true;
 }
@@ -1346,20 +1370,20 @@ ScriptPromise NavigatorAuction::finalizeAd(ScriptState* script_state,
   auto mojo_config = mojom::blink::AuctionAdConfig::New();
 
   // For finalizing an Ad PARAKEET only really cares about the decisionLogicUrl,
-  // auctionSignals, sellerSignals, and perBuyerSignals. We can ignore
+  // auctionSignals, sellerSignals, and perBuyerSignals. Also need seller, since
+  // it's used to validate the decision logic URL. We can ignore
   // copying/validating other fields on AuctionAdConfig.
-  if (!CopyDecisionLogicUrlFromIdlToMojo(*context, exception_state, *config,
-                                         *mojo_config))
+  if (!CopySellerFromIdlToMojo(exception_state, *config, *mojo_config) ||
+      !CopyDecisionLogicUrlFromIdlToMojo(*context, exception_state, *config,
+                                         *mojo_config) ||
+      !CopyAuctionSignalsFromIdlToMojo(*script_state, exception_state, *config,
+                                       *mojo_config) ||
+      !CopySellerSignalsFromIdlToMojo(*script_state, exception_state, *config,
+                                      *mojo_config) ||
+      !CopyPerBuyerSignalsFromIdlToMojo(*script_state, exception_state, *config,
+                                        *mojo_config)) {
     return ScriptPromise();
-  if (!CopyAuctionSignalsFromIdlToMojo(*script_state, exception_state, *config,
-                                       *mojo_config))
-    return ScriptPromise();
-  if (!CopySellerSignalsFromIdlToMojo(*script_state, exception_state, *config,
-                                      *mojo_config))
-    return ScriptPromise();
-  if (!CopyPerBuyerSignalsFromIdlToMojo(*script_state, exception_state, *config,
-                                        *mojo_config))
-    return ScriptPromise();
+  }
 
   if (!ValidateAdsObject(exception_state, ads))
     return ScriptPromise();
