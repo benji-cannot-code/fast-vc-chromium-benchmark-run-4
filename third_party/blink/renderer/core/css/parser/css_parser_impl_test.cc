2ff55dc27e29c5ad3db261e748fdf0d80536b310
Anders Hartvoll Ruud <andruud@chromium.org>
2022-05-15 20:35:06 +0000

    [mq-4] Enable CSSMediaQueries4 for stable
    This requires substantial WPT adjustments, since unknown parts of
    media queries now parse as <general-enclosed> [1] instead of becoming
    "not all".
    
    test_media_queries.html:
    
     - Deduplicate lazy evaluation of string templates.
     - Check that expressions are known or unknown instead of checking
       if they parsed.
     - Add new tests for "not", nesting, or, and missing forms of
       <mf-range> [2].
    
    Other WPTs changed in this CL all use matchmedia-utils.js:
    
     - Check that queries are known or unknown instead of checking
       if they parsed.
     - Don't list the same test examples twice. Instead just require that
       known/unknown queries also parse successfully using both JS and
       CSS.
    
    Tests for the "sizes" attribute seemed to believe that (!) is not
    valid for <general-enclosed> (but it is). Changed to (]), which is not
    valid.
    
    [1] https://drafts.csswg.org/mediaqueries-4/#typedef-general-enclosed
    [2] https://drafts.csswg.org/mediaqueries-4/#typedef-mf-range
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/E4Vd7w70OsU
    Change-Id: I08243ed885ae523953f288b38a9ff4bb05370508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3634678
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003559}

--- third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
@@ -485,7 +485,7 @@ TEST(CSSParserImplTest, LayeredImportRulesInvalid) {
     auto* parsed = DynamicTo<StyleRuleImport>(ParseRule(*document, rule));
     ASSERT_TRUE(parsed);
     EXPECT_FALSE(parsed->IsLayered());
-    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
+    EXPECT_TRUE(parsed->MediaQueries()->HasUnknown());
   }
 
   {
@@ -493,7 +493,7 @@ TEST(CSSParserImplTest, LayeredImportRulesInvalid) {
     auto* parsed = DynamicTo<StyleRuleImport>(ParseRule(*document, rule));
     ASSERT_TRUE(parsed);
     EXPECT_FALSE(parsed->IsLayered());
-    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
+    EXPECT_TRUE(parsed->MediaQueries()->HasUnknown());
   }
 
   {
@@ -501,7 +501,7 @@ TEST(CSSParserImplTest, LayeredImportRulesInvalid) {
     auto* parsed = DynamicTo<StyleRuleImport>(ParseRule(*document, rule));
     ASSERT_TRUE(parsed);
     EXPECT_FALSE(parsed->IsLayered());
-    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
+    EXPECT_TRUE(parsed->MediaQueries()->HasUnknown());
   }
 }
 
@@ -530,7 +530,7 @@ TEST(CSSParserImplTest, LayeredImportRulesMultipleLayers) {
     ASSERT_TRUE(parsed->IsLayered());
     ASSERT_EQ(1u, parsed->GetLayerName().size());
     EXPECT_EQ(g_empty_atom, parsed->GetLayerName()[0]);
-    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
+    EXPECT_TRUE(parsed->MediaQueries()->HasUnknown());
   }
 
   {
