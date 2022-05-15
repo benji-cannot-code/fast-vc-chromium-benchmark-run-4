e514cb4512c1c7bc08c87249f4f5a0dcc2fdba0c
K. Moon <kmoon@chromium.org>
2022-05-15 22:55:06 +0000

    Migrate PdfViewPluginBase::SubmitForm()
    Migrates SubmitForm() from PdfViewPluginBase to PdfViewWebPlugin.
    
    Bug: 1322928
    Change-Id: I113f3c4f8e610ce27f1c80b1346c4e14bc053b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646512
    Reviewed-by: Nigi <nigi@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003583}

--- pdf/pdf_view_plugin_base.cc
+++ pdf/pdf_view_plugin_base.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/i18n/rtl.h"
 #include "base/location.h"
-#include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
@@ -346,25 +345,6 @@ void PdfViewPluginBase::Print() {
     return;
 
   InvokePrintDialog();
-}
-
-void PdfViewPluginBase::SubmitForm(const std::string& url,
-                                   const void* data,
-                                   int length) {
-  // `url` might be a relative URL. Resolve it against the document's URL.
-  // TODO(crbug.com/1322928): Probably redundant with `Client::CompleteURL()`.
-  GURL resolved_url = GURL(GetURL()).Resolve(url);
-  if (!resolved_url.is_valid())
-    return;
-
-  UrlRequest request;
-  request.url = resolved_url.spec();
-  request.method = "POST";
-  request.body.assign(static_cast<const char*>(data), length);
-
-  form_loader_ = CreateUrlLoaderInternal();
-  form_loader_->Open(
-      request, base::BindOnce(&PdfViewPluginBase::DidFormOpen, GetWeakPtr()));
 }
 
 std::unique_ptr<UrlLoader> PdfViewPluginBase::CreateUrlLoader() {
@@ -1543,12 +1523,6 @@ void PdfViewPluginBase::DidOpenPreview(std::unique_ptr<UrlLoader> loader,
                                  PDFiumFormFiller::ScriptOption::kNoJavaScript);
   preview_engine_->PluginSizeUpdated({});
   preview_engine_->HandleDocumentLoad(std::move(loader), GetURL());
-}
-
-void PdfViewPluginBase::DidFormOpen(int32_t result) {
-  // TODO(crbug.com/719344): Process response.
-  LOG_IF(ERROR, result != kSuccess) << "DidFormOpen failed: " << result;
-  form_loader_.reset();
 }
 
 void PdfViewPluginBase::OnPrintPreviewLoaded() {
