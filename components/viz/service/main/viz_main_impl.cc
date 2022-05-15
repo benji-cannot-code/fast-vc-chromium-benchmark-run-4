4b63b518e33b61e6f1bd87f1f69b1460b0ee2d6e
Daniel Cheng <dcheng@chromium.org>
2022-05-15 20:22:06 +0000

    Use //base shmem APIs for GPU activity flags.
    Direct use of handle<shared_buffer> is discouraged; the wrappers around
    the base APIs give more indication of whether a given shmem region is
    intended by read-only or writable.
    
    Bug: 1306363
    Change-Id: I5f70f03c11f8452c89dab8e1af17593f9295161d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643000
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003535}

--- components/viz/service/main/viz_main_impl.cc
+++ components/viz/service/main/viz_main_impl.cc
@@ -173,7 +173,7 @@ void VizMainImpl::CreateGpuService(
     mojo::PendingRemote<
         discardable_memory::mojom::DiscardableSharedMemoryManager>
         discardable_memory_manager,
-    mojo::ScopedSharedBufferHandle activity_flags,
+    base::UnsafeSharedMemoryRegion activity_flags_region,
     gfx::FontRenderParams::SubpixelRendering subpixel_rendering) {
   DCHECK(gpu_thread_task_runner_->BelongsToCurrentThread());
 
@@ -209,7 +209,7 @@ void VizMainImpl::CreateGpuService(
 
   gpu_service_->InitializeWithHost(
       gpu_host.Unbind(),
-      gpu::GpuProcessActivityFlags(std::move(activity_flags)),
+      gpu::GpuProcessActivityFlags(std::move(activity_flags_region)),
       gpu_init_->TakeDefaultOffscreenSurface(),
       dependencies_.sync_point_manager, dependencies_.shared_image_manager,
       dependencies_.shutdown_event);
--- gpu/command_buffer/common/activity_flags_unittest.cc
+++ gpu/command_buffer/common/activity_flags_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/common/activity_flags.h"
 
+#include "base/memory/unsafe_shared_memory_region.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace gpu {
@@ -16,7 +17,7 @@ TEST(ActivityFlagsTest, BasicUsage) {
       host_flags.IsFlagSet(ActivityFlagsBase::FLAG_LOADING_PROGRAM_BINARY));
 
   // Create the service activity flags from host memory.
-  GpuProcessActivityFlags service_flags(host_flags.CloneHandle());
+  GpuProcessActivityFlags service_flags(host_flags.CloneRegion());
 
   // Ensure we can set and re-set flags.
   {
@@ -31,8 +32,7 @@ TEST(ActivityFlagsTest, BasicUsage) {
 
 TEST(ActivityFlagsTest, NotInitialized) {
   // Get the service activity flags without providing host memory.
-  auto buffer = mojo::ScopedSharedBufferHandle();
-  GpuProcessActivityFlags service_flags(std::move(buffer));
+  GpuProcessActivityFlags service_flags{base::UnsafeSharedMemoryRegion()};
 
   // Set/Unset should not crash.
   {
