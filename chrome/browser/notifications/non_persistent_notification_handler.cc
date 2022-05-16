0ea242137e497f082928eac718ec470ad8b57578
Kamila <hkamila@google.com>
2022-05-16 10:49:31 +0000

    Record non-persistent notifications on Desktop
    Record total count of non-persistent notifications shown/interacted (i.e. calendar reminders on desktop).
    
    Bug: 1305941
    Change-Id: I552992f786d709112ea5f90e89ac46036d68a87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645465
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003693}

--- chrome/browser/notifications/non_persistent_notification_handler.cc
+++ chrome/browser/notifications/non_persistent_notification_handler.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/notifications/notification_common.h"
 #include "chrome/browser/notifications/notification_permission_context.h"
+#include "chrome/browser/permissions/notifications_engagement_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/permissions/features.h"
 #include "components/permissions/permission_uma_util.h"
 #include "components/permissions/permission_util.h"
 #include "content/public/browser/notification_event_dispatcher.h"
@@ -63,6 +65,15 @@ void NonPersistentNotificationHandler::OnClick(
               &NonPersistentNotificationHandler::DidDispatchClickEvent,
               weak_ptr_factory_.GetWeakPtr(), profile, origin, notification_id,
               std::move(completed_closure)));
+
+  if (base::FeatureList::IsEnabled(
+          permissions::features::kNotificationInteractionHistory)) {
+    auto* service =
+        NotificationsEngagementServiceFactory::GetForProfile(profile);
+    // This service might be missing for incognito profiles and in tests.
+    if (service)
+      service->RecordNotificationInteraction(origin);
+  }
 }
 
 void NonPersistentNotificationHandler::DidDispatchClickEvent(
