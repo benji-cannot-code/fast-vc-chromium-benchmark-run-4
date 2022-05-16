fa297f30335c3465c4eba0d12182abb06cae48ca
Julie Jeongeun Kim <jkim@igalia.com>
2022-05-16 06:02:16 +0000

    [MPArch] Allow global commands only for the primary main frame
    This CL replaces !GetParent() with IsInPrimaryMainFrame() for
    CanExecuteGlobalCommands() for devtools. It's called to
    execute CaptureSnapshot(), CaptureScreenshot(), and
    GetAppManifest() and they work with the top level page.
    
    Bug: 1324905
    Change-Id: I6073e1c858d2886d3136b114de9bb4591b054784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644187
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1003632}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -182,7 +182,7 @@ template <typename ProtocolCallback>
 bool CanExecuteGlobalCommands(
     RenderFrameHost* host,
     const std::unique_ptr<ProtocolCallback>& callback) {
-  if (!host || !host->GetParent())
+  if (!host || host->IsInPrimaryMainFrame())
     return true;
   callback->sendFailure(
       Response::ServerError(kCommandIsOnlyAvailableAtTopTarget));
