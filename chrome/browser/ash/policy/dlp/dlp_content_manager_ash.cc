03e5ff50b84fb3c97e60a7713aa2d8fb13ed9b08
Aida Zolic <aidazolic@chromium.org>
2022-05-16 14:40:16 +0000

    Observe Window title changes in DlpContentManager
    For some on screen restrictions, such as screen sharing or video
    recording, as they are long running, the title of confidential content
    captured can change, which we were previously not taking into account.
    For example, after switching a tab in a Lacros window, the change in
    confidentiality is detected before the window's title is updated. For
    video recordings, as the warning is shown at the end, the user can also
    change the title.
    
    This CL adds OnWindowTitleChanged() to DlpWindowObserver interface,
    which ensures that the changes in titles are detected and can be updated
    either in the DlpConfidentialContents set or directl in the warning if
    it's already been shown.
    
    Fixes: 1318007,1321126
    Change-Id: I9abd43f56a7bc2961275e6b1743faf824f92924b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596185
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003749}

--- chrome/browser/ash/policy/dlp/dlp_content_manager_ash.cc
+++ chrome/browser/ash/policy/dlp/dlp_content_manager_ash.cc
@@ -114,6 +114,11 @@ void DlpContentManagerAsh::OnWindowDestroying(aura::Window* window) {
   MaybeChangeOnScreenRestrictions();
 }
 
+void DlpContentManagerAsh::OnWindowTitleChanged(aura::Window* window) {
+  CheckRunningVideoCapture();
+  CheckRunningScreenShares();
+}
+
 DlpContentRestrictionSet DlpContentManagerAsh::GetOnScreenPresentRestrictions()
     const {
   return on_screen_restrictions_;
