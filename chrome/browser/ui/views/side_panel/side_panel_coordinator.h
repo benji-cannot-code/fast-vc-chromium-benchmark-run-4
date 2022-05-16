70e2d77e94a9a7aadefde186e3d969bdb92d3a3d
Cheick Cisse <cheickcisse@google.com>
2022-05-16 21:24:12 +0000

    [Note] Add tests for UserNoteUICoordinator
    This CL:
      - Creates tests for UserNoteUICoordinator
         - Test that UserNoteUICoordinator opens with empty view
    
    Bug: 1321883
    Change-Id: I36fea426b6dbaa6dd0bcbe31de16fe0719928509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620505
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003931}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.h
@@ -47,6 +47,8 @@ class SidePanelCoordinator final : public SidePanelRegistryObserver,
 
  private:
   friend class SidePanelCoordinatorTest;
+  FRIEND_TEST_ALL_PREFIXES(UserNoteUICoordinatorTest,
+                           ShowEmptyUserNoteSidePanel);
 
   views::View* GetContentView();
   SidePanelEntry* GetEntryForId(SidePanelEntry::Id entry_id);
--- chrome/browser/ui/views/side_panel/user_note/user_note_ui_coordinator.cc
+++ chrome/browser/ui/views/side_panel/user_note/user_note_ui_coordinator.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/fill_layout.h"
 
 namespace {
-
 // Compares two UserNoteInstances by their rect's origin, which represents their
 // position in a web page. If the UserNoteInstances have the same position,
 // compare them by their modification date.
@@ -39,9 +38,7 @@ UserNoteUICoordinator::UserNoteUICoordinator(Browser* browser)
   browser_->tab_strip_model()->AddObserver(this);
 }
 
-UserNoteUICoordinator::~UserNoteUICoordinator() {
-  browser_->tab_strip_model()->RemoveObserver(this);
-}
+UserNoteUICoordinator::~UserNoteUICoordinator() = default;
 
 void UserNoteUICoordinator::CreateAndRegisterEntry(
     SidePanelRegistry* global_registry) {
@@ -160,10 +157,12 @@ std::unique_ptr<views::View> UserNoteUICoordinator::CreateUserNotesView() {
   // [| ...                     |]
 
   auto root_view = std::make_unique<views::View>();
+  root_view->SetID(kUserNoteUIViewId);
   root_view->SetLayoutManager(std::make_unique<views::FillLayout>());
 
   auto* scroll_view =
       root_view->AddChildView(std::make_unique<views::ScrollView>());
+  scroll_view->SetID(kUserNoteScrollViewId);
   scroll_view->SetHorizontalScrollBarMode(
       views::ScrollView::ScrollBarMode::kDisabled);
   // Setting clip height is necessary to make ScrollView take into account its
@@ -171,9 +170,9 @@ std::unique_ptr<views::View> UserNoteUICoordinator::CreateUserNotesView() {
   // correctly.
   scroll_view->ClipHeightTo(0, 0);
 
-  // TODO(cheickcisse): Populate scroll content view.
   scroll_contents_view_ =
       scroll_view->SetContents(std::make_unique<views::View>());
+  scroll_contents_view_->SetID(kUserNoteScrollContentsViewId);
 
   constexpr int edge_margin = 16;
   constexpr int vertical_padding = 16;
