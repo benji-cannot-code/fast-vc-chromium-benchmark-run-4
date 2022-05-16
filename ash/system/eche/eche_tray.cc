8a393722122ea75d110192322784a609953cb7a1
Abbas Nayebi <nayebi@google.com>
2022-05-16 20:28:50 +0000

    [eche] Handling the accelerator keys.
    Note: We are going to have special treatments for some accelerator keys
    such as "minimize" in later CLs.
    
    TESTED: Manual testing in which I tried alt-shift-i (feedback report) and some other key combinations while the bubble was shown.
    Video: https://drive.google.com/file/d/1Cvv-BNwyVLp5cJkF1Q9Zjsb_UW06ZxEp/view?usp=sharing
    
    Bug: b/231991166,b/219790589,b/231053106
    Change-Id: I78e83997e5c8db45eb4606f713ad0e0321ec503b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645691
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003906}

--- ash/system/eche/eche_tray.cc
+++ ash/system/eche/eche_tray.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/components/multidevice/logging/logging.h"
 #include "ash/keyboard/ui/keyboard_ui_controller.h"
+#include "ash/public/cpp/accelerators.h"
 #include "ash/public/cpp/ash_web_view.h"
 #include "ash/public/cpp/ash_web_view_factory.h"
 #include "ash/public/cpp/shell_window_ids.h"
@@ -58,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/view.h"
+#include "ui/views/views_delegate.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -109,10 +111,32 @@ std::unique_ptr<views::Button> CreateButton(
 
 }  // namespace
 
+EcheTray::EventInterceptor::EventInterceptor(EcheTray* eche_tray)
+    : eche_tray_(eche_tray) {}
+EcheTray::EventInterceptor::~EventInterceptor() = default;
+
+void EcheTray::EventInterceptor::OnKeyEvent(ui::KeyEvent* event) {
+  // To provide consistent behavior with a menu, process accelerator as a menu
+  // is open if the event is not handled by the widget.
+  ui::Accelerator accelerator(*event);
+  if (AcceleratorController::Get()->DoesAcceleratorMatchAction(
+          accelerator, AcceleratorAction::WINDOW_MINIMIZE)) {
+    eche_tray_->CloseBubble();
+    event->StopPropagation();
+    return;
+  }
+  if (AcceleratorController::Get()->IsRegistered(accelerator)) {
+    views::ViewsDelegate::GetInstance()->ProcessAcceleratorWhileMenuShowing(
+        accelerator);
+    event->StopPropagation();
+  }
+}
+
 EcheTray::EcheTray(Shelf* shelf)
     : TrayBackgroundView(shelf),
-      icon_(tray_container()->AddChildView(
-          std::make_unique<views::ImageView>())) {
+      icon_(
+          tray_container()->AddChildView(std::make_unique<views::ImageView>())),
+      event_interceptor_(std::make_unique<EventInterceptor>(this)) {
   const int icon_padding = (kTrayItemSize - kIconSize) / 2;
 
   icon_->SetBorder(
@@ -193,6 +217,8 @@ void EcheTray::ShowBubble() {
   // We need this as `WorkspaceLayoutManager` conflicts with our resizing.
   // See b/229111865#comment5
   window_state->set_ignore_keyboard_bounds_change(true);
+  bubble_->GetBubbleWidget()->GetNativeWindow()->AddPreTargetHandler(
+      event_interceptor_.get());
 }
 
 bool EcheTray::PerformAction(const ui::Event& event) {
@@ -365,6 +391,10 @@ void EcheTray::StartGracefulClose() {
 }
 
 void EcheTray::HideBubble() {
+  if (!bubble_)
+    return;
+  bubble_->GetBubbleWidget()->GetNativeWindow()->RemovePreTargetHandler(
+      event_interceptor_.get());
   SetIsActive(false);
   bubble_->bubble_view()->SetVisible(false);
   bubble_->GetBubbleWidget()->Deactivate();
