c4e992300e088243fed9af1220a9c6748aa0ad73
Stephan Hartmann <stha09@googlemail.com>
2022-05-16 18:13:08 +0000

    IWYU: add math.h for isnan in webrtc_video_perf_history
    Bug: 957519
    Change-Id: I9d39e3ad714b562c00cd4a31d45741a28a4b8726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3649664
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1003841}

--- media/mojo/services/webrtc_video_perf_history.cc
+++ media/mojo/services/webrtc_video_perf_history.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/mojo/services/webrtc_video_perf_history.h"
 
+#include <math.h>
+
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/format_macros.h"
