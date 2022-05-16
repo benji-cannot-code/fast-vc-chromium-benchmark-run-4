8bd62fba2a1145ed17f531441c448c7dc6fc1998
Liam Brady <lbrady@google.com>
2022-05-16 20:59:12 +0000

    Fenced Frame: Have renderer check for valid URL before passing to browser for default mode fenced frames.
    If an invalid src is passed in (such as a URL with an invalid port),
    the URL will be invalidated while it is being passed from the renderer
    to the browser, and will be blank by the time the browser checks to make
    sure the URL is correct.
    
    This CL ensures that the valid URL check happens on the renderer side
    and is caught before being sent to the browser.
    
    Bug: 1325359
    Change-Id: I5e79b5225d2d43f583eb7d759734850d496a085c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650878
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003920}

--- third_party/blink/common/fenced_frame/fenced_frame_utils.cc
+++ third_party/blink/common/fenced_frame/fenced_frame_utils.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 bool IsValidFencedFrameURL(GURL url) {
+  if (!url.is_valid())
+    return false;
   return url.SchemeIs(url::kHttpsScheme) || url.IsAboutBlank() ||
          net::IsLocalhost(url);
 }
