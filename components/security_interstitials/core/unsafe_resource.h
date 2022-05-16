e1c9a95e2fd5e7c9285ed0d0ef575b8b91a52502
Daniel Rubery <drubery@chromium.org>
2022-05-16 19:36:29 +0000

    Implement UKM for CSPP
    This CL adds four UKM metrics for client side phishing protection,
    measuring the high-level behavior of the feature on a per-URL basis.
    This will help to understand regressions that only appear on a subset
    of pages.
    
    Bug: 1320895
    Change-Id: I62faa8b6767989c434b538641e7f7c8a4646d0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3636478
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003878}

--- components/security_interstitials/core/unsafe_resource.h
+++ components/security_interstitials/core/unsafe_resource.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "components/safe_browsing/core/browser/db/hit_report.h"
 #include "components/safe_browsing/core/common/safebrowsing_constants.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "services/network/public/mojom/fetch_api.mojom.h"
 #include "url/gurl.h"
 
@@ -100,7 +101,11 @@ struct UnsafeResource {
 
   // If true, this UnsafeResource is created because of the Delayed Warnings
   // experiment.
-  bool is_delayed_warning;
+  bool is_delayed_warning = false;
+
+  // The UKM ID of the navigation to the unsafe resource. This is currently only
+  // populated when `threat_source` is `CLIENT_SIDE_PHISHING`.
+  ukm::SourceId ukm_id;
 };
 
 }  // namespace security_interstitials
