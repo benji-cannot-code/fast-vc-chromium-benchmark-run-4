8cb11b4d0cc66fc9c89414fc3074e71edf701983
Kenichi Ishibashi <bashi@chromium.org>
2022-05-16 06:05:26 +0000

    Remove null checks for CorsURLLoaderFactory::context_
    It is always non-null as the constructor has DCHECK and `const` is
    specified.
    
    Bug: N/A
    Change-Id: Ideb1ab17baa5cd3384a4954631f2c25ae5ca03b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647227
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003633}

--- services/network/cors/cors_url_loader_factory.cc
+++ services/network/cors/cors_url_loader_factory.cc
@@ -323,8 +323,6 @@ void CorsURLLoaderFactory::ClearBindings() {
 }
 
 void CorsURLLoaderFactory::DeleteIfNeeded() {
-  if (!context_)
-    return;
   if (receivers_.empty() && url_loaders_.empty() && cors_url_loaders_.empty())
     context_->DestroyURLLoaderFactory(this);
 }
@@ -509,7 +507,7 @@ bool CorsURLLoaderFactory::IsValidRequest(const ResourceRequest& request,
       return false;
   }
 
-  if (context_ && !GetAllowAnyCorsExemptHeaderForBrowser() &&
+  if (!GetAllowAnyCorsExemptHeaderForBrowser() &&
       !IsValidCorsExemptHeaders(*context_->cors_exempt_header_list(),
                                 request.cors_exempt_headers)) {
     return false;
@@ -613,7 +611,7 @@ bool CorsURLLoaderFactory::IsValidRequest(const ResourceRequest& request,
 }
 
 bool CorsURLLoaderFactory::GetAllowAnyCorsExemptHeaderForBrowser() const {
-  return context_ && process_id_ == mojom::kBrowserProcessId &&
+  return process_id_ == mojom::kBrowserProcessId &&
          context_->allow_any_cors_exempt_header_for_browser();
 }
 
