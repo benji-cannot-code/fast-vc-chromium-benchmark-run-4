5c67a236836da3af55c624844867f49835e8182c
Stephan Hartmann <stha09@googlemail.com>
2022-05-16 13:17:22 +0000

    IWYU: add memory for std::unique_ptr in gl::GLDisplayManager
    Bug: 957519
    Change-Id: I9453ef83b65152bdec869d56d853297339410176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647825
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003727}

--- ui/gl/gl_display_manager.h
+++ ui/gl/gl_display_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GL_GL_DISPLAY_MANAGER_H_
 
 #include <map>
+#include <memory>
 #include <vector>
 
 #include "base/check.h"
