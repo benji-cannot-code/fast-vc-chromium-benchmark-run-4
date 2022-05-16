00e208d115e5c9ac8314977d1d0b5386ccc45e89
Joe Mason <joenotcharles@google.com>
2022-05-16 21:36:29 +0000

    Test all parameter combinations of MacWebContentsOcclusion
    This parameterizes WindowOcclusionBrowserTestMac so that each test runs
    in all configurations of the MacWebContentsOcclusion feature:
    
    * Feature disabled (uses the legacy code)
    * Feature enabled (uses WebContentsOcclusionCheckerMac without enhanced
      occlusion detection)
    * Feature enabled, plus one or more parameters
    
    Some tests aren't expected to pass in all configurations, so this also
    adds some aliases of WindowOcclusionBrowserTestMac which each only test
    a subset of configurations.
    
    Also adds a resetSharedInstanceForTesting function to reset the
    singleton WebContentsOcclusionCheckerMac between tests.
    
    Bug: 883031
    Change-Id: I3bfbc218b8e6d4d4b658583aae72ef49193b4009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614269
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003946}

--- content/app_shim_remote_cocoa/web_contents_occlusion_checker_mac.h
+++ content/app_shim_remote_cocoa/web_contents_occlusion_checker_mac.h
@@ -9,12 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AppKit/AppKit.h>
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #import "content/app_shim_remote_cocoa/web_contents_view_cocoa.h"
 #include "content/common/web_contents_ns_view_bridge.mojom.h"
+
+extern CONTENT_EXPORT const base::FeatureParam<bool>
+    kEnhancedWindowOcclusionDetection;
+extern CONTENT_EXPORT const base::FeatureParam<bool>
+    kDisplaySleepAndAppHideDetection;
 
 @interface WebContentsOcclusionCheckerMac : NSObject
 
 + (instancetype)sharedInstance;
+// Resets the state of `sharedInstance` during tests.
++ (void)resetSharedInstanceForTesting;
 // Returns YES if webcontents visibility updates will occur on the next pass
 // of the run loop.
 - (BOOL)willUpdateWebContentsVisibility;
