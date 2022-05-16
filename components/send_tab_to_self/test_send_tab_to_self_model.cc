554610820f47848a459011124f7f69ac5ae424a8
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-05-16 20:48:54 +0000

    Remove unused send-tab-to-self android APIs
    No behavior is changed.
    
    Bug: None
    Change-Id: I56a060717ce1e92781b2f9bc59f3e2b979679df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645456
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003916}

--- components/send_tab_to_self/test_send_tab_to_self_model.cc
+++ components/send_tab_to_self/test_send_tab_to_self_model.cc
@@ -11,8 +11,6 @@ std::vector<std::string> TestSendTabToSelfModel::GetAllGuids() const {
   return {};
 }
 
-void TestSendTabToSelfModel::DeleteAllEntries() {}
-
 const SendTabToSelfEntry* TestSendTabToSelfModel::GetEntryByGUID(
     const std::string& guid) const {
   return nullptr;
