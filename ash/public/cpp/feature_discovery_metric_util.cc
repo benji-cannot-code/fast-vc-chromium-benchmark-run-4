d73999db107a16436c1896c5d3a4e4a055eef1d7
Andrew Xu <andrewxu@chromium.org>
2022-05-16 17:54:31 +0000

    [Launcher] Split sort discovery duration after reorder nudge shows
    This CL splits the metric data which measures the sort discovery duration after the reorder nudge shows into two groups:
    
    1. the data collected if the initial education nudge shows in tablet
    2. the data collected if the initial education nudge shows in clamshell
    
    Bug: 1316185
    Change-Id: Ib24570b520ca9f663af4f03f9f6f69bf21980bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3636484
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003833}

--- ash/public/cpp/feature_discovery_metric_util.cc
+++ ash/public/cpp/feature_discovery_metric_util.cc
@@ -70,6 +70,11 @@ const std::array<TrackableFeatureInfo, static_cast<int>(TrackableFeature::kMax)>
         TrackableFeatureInfo{
             TrackableFeature::kAppListReorderAfterSessionActivation,
             "AppListReorderAfterSessionActivation",
-            kAppListSortDiscoveryDurationAfterActivation}};
+            kAppListSortDiscoveryDurationAfterActivation},
+        TrackableFeatureInfo{
+            TrackableFeature::kAppListReorderAfterEducationNudgePerTabletMode,
+            "AppListReorderAfterEducationNudgeSeparated",
+            kAppListSortDiscoveryDurationAfterNudgeClamshell,
+            kAppListSortDiscoveryDurationAfterNudgeTablet}};
 
 }  // namespace ash::feature_discovery
