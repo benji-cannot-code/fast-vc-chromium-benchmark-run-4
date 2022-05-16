26745cb8e2d0dc52c5158a902d27bcbf244ac0a8
dpapad <dpapad@chromium.org>
2022-05-16 21:37:18 +0000

    Signin UIs: Import Polymer templates at runtime.
    Also use css_to_wrapper() instead of html_to_js() for Polymer style
    files.
    
    Bug: 1292025,1322682
    Change-Id: I4c46bad3a8e384c2ab8a4978151aeb21f4920879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646774
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003947}

--- chrome/browser/resources/signin/sync_confirmation/sync_disabled_confirmation_app.ts
+++ chrome/browser/resources/signin/sync_confirmation/sync_disabled_confirmation_app.ts
@@ -4,13 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
-import './signin_shared_css.js';
+import './signin_shared.css.js';
 import './strings.m.js';
 
 import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {SyncConfirmationBrowserProxy, SyncConfirmationBrowserProxyImpl} from './sync_confirmation_browser_proxy.js';
+import {getTemplate} from './sync_disabled_confirmation_app.html.js';
 
 interface SyncDisabledConfirmationAppElement {
   $: {
@@ -24,7 +25,7 @@ class SyncDisabledConfirmationAppElement extends PolymerElement {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   private syncConfirmationBrowserProxy_: SyncConfirmationBrowserProxy =
