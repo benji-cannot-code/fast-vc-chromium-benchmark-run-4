cc6bd088942316cd8c3329740efebe20feaa2949
Luca Hunkeler <hluca@google.com>
2022-05-16 09:20:17 +0000

    Get PDM with browser context
    We currently get the PersonalDataManager by looking at the last used
    profile. That is a common pattern on Android but it's not ideal on
    Desktop where there might be multiple profiles.
    This CL fetches it via the current browser context.
    
    Change-Id: Ic52b6db9073783885359a7aad3492992f6829a6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637541
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003670}

--- components/autofill_assistant/browser/common_dependencies.h
+++ components/autofill_assistant/browser/common_dependencies.h
@@ -50,7 +50,8 @@ class CommonDependencies {
 
   virtual std::string GetCountryCode() const = 0;
 
-  virtual autofill::PersonalDataManager* GetPersonalDataManager() const = 0;
+  virtual autofill::PersonalDataManager* GetPersonalDataManager(
+      content::BrowserContext* browser_context) const = 0;
 
   virtual password_manager::PasswordManagerClient* GetPasswordManagerClient(
       content::WebContents* web_contents) const = 0;
