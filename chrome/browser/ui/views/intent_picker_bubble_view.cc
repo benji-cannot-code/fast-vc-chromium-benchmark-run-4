3f199921a1521cd744ab473aecb291ed084a6991
Tim Sergeant <tsergeant@chromium.org>
2022-05-16 07:48:25 +0000

    Intents: Simplify IntentPickerBubbleView tests
    This simplifies the testing APIs for IntentPickerBubbleView so that
    tests interact with the view hierarchy directly, instead of relying on
    private testing APIs to poke the bubble.
    
    This will make it easier for us to refactor and change the contents of
    the bubble without needing to rewrite all the tests as we go.
    
    Bug: 1321501
    Change-Id: I16184fc997ec54d571e1e38cc55021321770a966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644929
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003648}

--- chrome/browser/ui/views/intent_picker_bubble_view.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view.cc
@@ -100,10 +100,6 @@ class IntentPickerLabelButton : public views::LabelButton {
     views::InkDrop::Get(this)->AnimateToState(
         views::InkDropState::ACTIVATED, ui::LocatedEvent::FromIfValid(event));
   }
-
-  views::InkDropState GetTargetInkDropState() {
-    return views::InkDrop::Get(this)->GetInkDrop()->GetTargetInkDropState();
-  }
 };
 
 BEGIN_METADATA(IntentPickerLabelButton, views::LabelButton)
@@ -305,6 +301,7 @@ void IntentPickerBubbleView::Initialize() {
   auto scrollable_view = std::make_unique<views::View>();
   scrollable_view->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
+  scrollable_view->SetID(ViewId::kItemContainer);
 
   for (size_t i = 0; i < app_info_.size(); i++) {
     auto app_button = std::make_unique<IntentPickerLabelButton>(
@@ -394,6 +391,7 @@ void IntentPickerBubbleView::Initialize() {
     remember_selection_checkbox_ = AddChildView(
         std::make_unique<views::Checkbox>(l10n_util::GetStringUTF16(
             IDS_INTENT_PICKER_BUBBLE_VIEW_REMEMBER_SELECTION)));
+    remember_selection_checkbox_->SetID(ViewId::kRememberCheckbox);
     remember_selection_checkbox_->SetProperty(views::kMarginsKey, insets);
     UpdateCheckboxState();
   }
@@ -427,10 +425,6 @@ void IntentPickerBubbleView::RunCallbackAndCloseBubble(
   }
 }
 
-size_t IntentPickerBubbleView::GetScrollViewSize() const {
-  return scroll_view_->contents()->children().size();
-}
-
 void IntentPickerBubbleView::AdjustScrollViewVisibleRegion() {
   const views::ScrollBar* bar = scroll_view_->vertical_scroll_bar();
   if (bar) {
@@ -456,7 +450,7 @@ void IntentPickerBubbleView::SetSelectedAppIndex(size_t index,
 }
 
 size_t IntentPickerBubbleView::CalculateNextAppIndex(int delta) {
-  size_t size = GetScrollViewSize();
+  size_t size = scroll_view_->contents()->children().size();
   return static_cast<size_t>((selected_app_tag_ + size + delta) % size);
 }
 
@@ -484,21 +478,6 @@ void IntentPickerBubbleView::ClearIntentPickerBubbleView() {
   // else.
   if (intent_picker_bubble_ == this)
     intent_picker_bubble_ = nullptr;
-}
-
-gfx::ImageSkia IntentPickerBubbleView::GetAppImageForTesting(size_t index) {
-  return GetIntentPickerLabelButtonAt(index)->GetImage(
-      views::Button::ButtonState::STATE_NORMAL);
-}
-
-views::InkDropState IntentPickerBubbleView::GetInkDropStateForTesting(
-    size_t index) {
-  return GetIntentPickerLabelButtonAt(index)->GetTargetInkDropState();
-}
-
-void IntentPickerBubbleView::PressButtonForTesting(size_t index,
-                                                   const ui::Event& event) {
-  AppButtonPressed(index, event);
 }
 
 BEGIN_METADATA(IntentPickerBubbleView, LocationBarBubbleDelegateView)
