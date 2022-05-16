8be35a54868a32aeee30467c2b1fa3946b2d5964
Daniel Andersson <dandersson@chromium.org>
2022-05-16 20:08:39 +0000

    saved_desks: Minor cleanups in SavedDeskItemView.
    * Make `desk_template` return by const reference instead of pointer
      since the value is always going to be there.
    * Have `uuid` return by const ref instead of by value. The previous
      implementation was likely returning by value because of the
      underlying accessor in `DeskTemplate` which was also returning by
      value.
    * Have `DeskItemGridView` use the existing `uuid` function instead
      of `item->desk_template().uuid()`.
    * Make the constructor take a `unique_ptr<DeskTemplate>` to make it
      obvious that it is taking over ownership.
    * `SavedDeskIconContainer` doesn't need non-const access to the
      template.
    
    Change-Id: I333807e1304eea915b686e5cdea2210da0023bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646807
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003897}

--- ash/wm/desks/templates/saved_desk_item_view.cc
+++ ash/wm/desks/templates/saved_desk_item_view.cc
@@ -114,8 +114,9 @@ std::u16string GetTimeStr(base::Time timestamp) {
 
 }  // namespace
 
-SavedDeskItemView::SavedDeskItemView(const DeskTemplate* desk_template)
-    : desk_template_(desk_template->Clone()) {
+SavedDeskItemView::SavedDeskItemView(
+    std::unique_ptr<DeskTemplate> desk_template)
+    : desk_template_(std::move(desk_template)) {
   auto launch_template_callback = base::BindRepeating(
       &SavedDeskItemView::OnGridItemPressed, weak_ptr_factory_.GetWeakPtr());
 
@@ -618,8 +619,8 @@ SavedDeskItemView* SavedDeskItemView::FindOtherTemplateWithName(
       [this, name](const SavedDeskItemView* d) {
         // Name duplication is allowed if one of the templates is an admin
         // template.
-        return (d != this && d->desk_template()->template_name() == name &&
-                d->desk_template()->source() != DeskTemplateSource::kPolicy);
+        return (d != this && d->desk_template_->template_name() == name &&
+                d->desk_template_->source() != DeskTemplateSource::kPolicy);
       });
   return iter == templates_grid_view_items.end() ? nullptr : *iter;
 }
