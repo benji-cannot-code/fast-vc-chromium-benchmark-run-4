22a8fba7e7631e5d24f06922d68a856a9ed5b458
Clarissa Garvey <clarissagarvey@chromium.org>
2022-05-17 01:11:06 +0000

    media/gpu/vaapi: Remove encoder BitrateControl
    This CL removes the VaapiVideoEncoderDelegate::BitrateControl enum. It
    became obsolete because the type of bitrate control for encoders is
    hard-coded to codec types: currently H264 uses CBR, and VP8 and VP9 use
    CQ. The bitrate control cannot be chosen. If this changes in the future,
    it may be re-added.
    
    Bug: b:181797390
    Change-Id: I862794ec9326c34e7297e0dd4d672adcf2e611f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645242
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Clarissa Garvey <clarissagarvey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004051}

--- media/gpu/vaapi/vaapi_video_encode_accelerator_unittest.cc
+++ media/gpu/vaapi/vaapi_video_encode_accelerator_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_util.h"
 #include "media/gpu/gpu_video_encode_accelerator_helpers.h"
 #include "media/gpu/vaapi/vaapi_utils.h"
+#include "media/gpu/vaapi/vaapi_video_encoder_delegate.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #include "media/gpu/vaapi/vp9_vaapi_video_encoder_delegate.h"
 #include "media/gpu/vp9_picture.h"
@@ -42,6 +43,7 @@ constexpr Bitrate kDefaultBitrate =
     Bitrate::ConstantBitrate(kDefaultBitrateBps);
 constexpr uint32_t kDefaultFramerate = 30;
 constexpr size_t kMaxNumOfRefFrames = 3u;
+
 constexpr int kSpatialLayersResolutionDenom[][3] = {
     {1, 0, 0},  // For one spatial layer.
     {2, 1, 0},  // For two spatial layers.
@@ -96,14 +98,6 @@ bool IsSVCSupported(const VideoEncodeAccelerator::Config& config) {
          config.storage_type ==
              VideoEncodeAccelerator::Config::StorageType::kGpuMemoryBuffer &&
          config.output_profile == VP9PROFILE_PROFILE0;
-}
-
-MATCHER_P2(MatchesVaapiVideoEncoderDelegateConfig,
-           max_ref_frames,
-           bitrate_control,
-           "") {
-  return arg.max_num_ref_frames == max_ref_frames &&
-         arg.bitrate_control == bitrate_control;
 }
 
 MATCHER_P3(MatchesBitstreamBufferMetadata,
@@ -305,8 +299,6 @@ class VaapiVideoEncodeAcceleratorTest
       NO_THREAD_SAFETY_ANALYSIS {
     base::RunLoop run_loop;
     ::testing::InSequence s;
-    constexpr auto kBitrateControl = VaapiVideoEncoderDelegate::BitrateControl::
-        kConstantQuantizationParameter;
     const size_t num_spatial_layers = config.spatial_layers.size();
     // Scaling is needed only for non highest spatial layer, so here the vpp
     // number is |num_spatial_layers - 1|.
@@ -324,9 +316,12 @@ class VaapiVideoEncodeAcceleratorTest
         reinterpret_cast<VaapiVideoEncodeAccelerator*>(encoder_.get());
     vaapi_encoder->vpp_vaapi_wrapper_ = mock_vpp_vaapi_wrapper_;
 
-    EXPECT_CALL(*mock_encoder_,
-                Initialize(_, MatchesVaapiVideoEncoderDelegateConfig(
-                                  kMaxNumOfRefFrames, kBitrateControl)))
+    EXPECT_CALL(
+        *mock_encoder_,
+        Initialize(_,
+                   testing::Field(
+                       &VaapiVideoEncoderDelegate::Config::max_num_ref_frames,
+                       kMaxNumOfRefFrames)))
         .WillOnce(Return(true));
     EXPECT_CALL(*mock_vaapi_wrapper_, CreateContext(kDefaultEncodeSize))
         .WillOnce(Return(true));
