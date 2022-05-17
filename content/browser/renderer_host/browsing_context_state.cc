29dd163491f65ca2b7cccf6a3a19cab367661338
Harkiran Bolaria <hbolaria@google.com>
2022-05-17 16:18:23 +0000

    [MPArch] BrowsingContextState/proxies deleted with RenderFrameHostImpl.
    BrowsingContextState is now stored as a safe_ref rather than as a
    scoped_refptr on RenderViewHostImpl because keeping it as a
    scoped_refptr ties its lifetime to the RenderViewHostImpl. This means
    BCS and associated proxies can be accessible beyond the lifetime of
    the RenderFrameHostImpl, and causes problems regarding proxy access.
    
    Bug: 1270671
    Change-Id: I07aed933a5e324c447537038b444ae43d988b1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540184
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004289}

--- content/browser/renderer_host/browsing_context_state.cc
+++ content/browser/renderer_host/browsing_context_state.cc
@@ -437,4 +437,8 @@ void BrowsingContextState::WriteIntoTrace(
   dict.Add("this", static_cast<const void*>(this));
 }
 
+base::SafeRef<BrowsingContextState> BrowsingContextState::GetSafeRef() {
+  return weak_factory_.GetSafeRef();
+}
+
 }  // namespace content
--- content/browser/renderer_host/render_view_host_impl.h
+++ content/browser/renderer_host/render_view_host_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/safe_ref.h"
 #include "base/process/kill.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -199,7 +200,8 @@ class CONTENT_EXPORT RenderViewHostImpl
   // back, while cross-BrowsingInstances result in creating a new unrelated
   // RenderViewHost. This is not true in the legacy BCS mode, so there the
   // |main_browsing_context_state_| is null.
-  const scoped_refptr<BrowsingContextState>& main_browsing_context_state() {
+  const absl::optional<base::SafeRef<BrowsingContextState>>&
+  main_browsing_context_state() const {
     return main_browsing_context_state_;
   }
 
@@ -450,7 +452,8 @@ class CONTENT_EXPORT RenderViewHostImpl
   raw_ptr<FrameTree> frame_tree_;
 
   // See main_browsing_context_state() for more details.
-  const scoped_refptr<BrowsingContextState> main_browsing_context_state_;
+  absl::optional<base::SafeRef<BrowsingContextState>>
+      main_browsing_context_state_;
 
   base::WeakPtrFactory<RenderViewHostImpl> weak_factory_{this};
 };
