2b450e63729eee62540d14b3d90c9f77c571e19e
Takashi Toyoshima <toyoshim@chromium.org>
2022-05-17 08:55:40 +0000

    PageLoadMetrics: Enforce the FORWARD_OBSERVING availability rule
    As FORWARD_OBSERVING is designed to forward events for FencedFrames, it
    doesn't work correctly when it is returned for events other than
    OnFencedFramesStart().
    
    This patch introduces a check to enforce this rule and document it
    explicitly in the API comment.
    
    Under this rule, PageLoadMetricsForwardObserver should not see
    OnPrefetchLikely() as it's delivered only for primary pages.
    
    Bug: 1301880
    Change-Id: I06d39928489004fed374ed3ff2da6ad7da47d47e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640904
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004160}

--- components/page_load_metrics/browser/page_load_tracker.h
+++ components/page_load_metrics/browser/page_load_tracker.h
@@ -447,7 +447,9 @@ class PageLoadTracker : public PageLoadMetricsUpdateDispatcher::Client,
   using InvokeCallback =
       base::RepeatingCallback<PageLoadMetricsObserver::ObservePolicy(
           PageLoadMetricsObserverInterface*)>;
-  void InvokeAndPruneObservers(const char* trace_name, InvokeCallback callback);
+  void InvokeAndPruneObservers(const char* trace_name,
+                               InvokeCallback callback,
+                               bool permit_forwarding);
 
   void AddObserverInterface(
       std::unique_ptr<PageLoadMetricsObserverInterface> observer);
