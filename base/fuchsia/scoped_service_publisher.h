bd400a239317bd062c187552aedd4c9a35bad9a9
Sergey Ulanov <sergeyu@google.com>
2022-05-17 17:21:12 +0000

    [Fuchsia] Add DCHECK in ScopedServicePublisher for AddEntry() result.
    ScopedServicePublisher was ignoring AddEntry() result. In some cases
    the service wasn't published properly (e.g. if it's already present),
    but that error was ignored and that issue was hard to debug. Added
    DCHECK to validate the result.
    
    Change-Id: I4e9a8aa4158b81f90819664e42aaca482690f546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651203
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004331}

--- base/fuchsia/scoped_service_publisher.h
+++ base/fuchsia/scoped_service_publisher.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/zx/channel.h>
 
 #include "base/base_export.h"
+#include "base/fuchsia/fuchsia_logging.h"
 #include "base/strings/string_piece.h"
 
 namespace base {
@@ -39,8 +40,9 @@ class BASE_EXPORT ScopedServicePublisher {
                          fidl::InterfaceRequestHandler<Interface> handler,
                          base::StringPiece name = Interface::Name_)
       : pseudo_dir_(pseudo_dir), name_(name) {
-    pseudo_dir_->AddEntry(name_,
-                          std::make_unique<vfs::Service>(std::move(handler)));
+    zx_status_t status = pseudo_dir_->AddEntry(
+        name_, std::make_unique<vfs::Service>(std::move(handler)));
+    ZX_DCHECK(status == ZX_OK, status) << "vfs::PseudoDir::AddEntry";
   }
 
   ScopedServicePublisher(const ScopedServicePublisher&) = delete;
