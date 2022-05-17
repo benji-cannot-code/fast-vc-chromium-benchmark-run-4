06b99bf403a7c9195e0c85dc484ef09ec7b92e2a
Ewann <ewannpv@google.com>
2022-05-17 14:30:22 +0000

    [iOS] Updates neutral site symbol
    This CL updates neutral site symbols according to new directives.
    
    Bug: 1315544
    Change-Id: I80b39f6af1ca92bbaf4716c809d1f63fe3299aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652200
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004247}

--- ios/chrome/browser/ui/omnibox/omnibox_icon_type.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_icon_type.mm
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Specific symbol names for the location bar.
-NSString* kInfoLocationBarSymbol = @"info.circle.fill";
+NSString* kInfoLocationBarSymbol = @"info.circle";
 NSString* kSecureLocationBarSymbol = @"lock.fill";
 
 }  // namespace
