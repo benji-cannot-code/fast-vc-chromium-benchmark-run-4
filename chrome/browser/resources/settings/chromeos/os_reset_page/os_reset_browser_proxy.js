a579b16af9499e729083edadc4be88e66a9c3c20
Wes Okuhara <wesokuhara@google.com>
2022-05-17 21:38:45 +0000

    CrOS Settings: Convert reset page to class-based syntax
    Converts Polymer elements to the class-based syntax to prepare for the
    upcoming TypeScript migration. Additionally removes uses of
    addSingletonGetter().
    
    Bug: chromium:1206112, chromium:1315757
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*Reset*'
    Test: Manual UI testing
    Change-Id: I0fac2a62de4a5c18ebb41f329f5720c85665e8a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652715
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004467}

--- chrome/browser/resources/settings/chromeos/os_reset_page/os_reset_browser_proxy.js
+++ chrome/browser/resources/settings/chromeos/os_reset_page/os_reset_browser_proxy.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
-
 /** @interface */
 export class OsResetBrowserProxy {
   /**
@@ -31,6 +29,17 @@ export class OsResetBrowserProxyImpl {
   requestFactoryResetRestart() {
     chrome.send('requestFactoryResetRestart');
   }
+
+  /** @return {!OsResetBrowserProxy} */
+  static getInstance() {
+    return instance || (instance = new OsResetBrowserProxyImpl());
+  }
+
+  /** @param {!OsResetBrowserProxy} obj */
+  static setInstance(obj) {
+    instance = obj;
+  }
 }
 
-addSingletonGetter(OsResetBrowserProxyImpl);
+/** @type {?OsResetBrowserProxy} */
+let instance = null;
