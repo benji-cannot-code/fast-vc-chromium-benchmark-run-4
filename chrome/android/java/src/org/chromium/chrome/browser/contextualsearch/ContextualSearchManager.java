74a317586191b1982181ea02f397459ca75a693a
Gang Wu <gangwu@chromium.org>
2022-05-17 18:22:00 +0000

    [TTS] Delete EngagementSuppression.java
    This CL deletes EngagementSuppression.java and some
    ChromePreferenceKeys.
    This CL also deletes some ChromePreferenceKeys in
    ContextualSearchPolicy.java. ContextualSearchPolicy.java still has some
    ChromePreferenceKeys which need to be deleted. I will do it later since
    those ChromePreferenceKeys related to some metrics.
    
    Bug: 1325440
    Change-Id: If9427b721be6539df967eeb9387e52feebb83f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648315
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004366}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -1008,9 +1008,6 @@ public class ContextualSearchManager
             mQuickAnswersHeuristic.setConditionSatisfied(true);
             mQuickAnswersHeuristic.setDoesAnswer(doesAnswer);
         }
-
-        // Update Tap counters to account for a possible answer.
-        mPolicy.updateCountersForQuickAnswer(mWasActivatedByTap, doesAnswer);
     }
 
     /**
