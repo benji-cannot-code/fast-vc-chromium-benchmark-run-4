01a58a8c507bd14985bcc2c48811aa1968820298
Bruce Dawson <brucedawson@chromium.org>
2022-05-17 20:31:09 +0000

    Switch some tools PRESUBMIT scripts to Python 3
    This switches several scripts run by PRESUBMIT.py scripts in the tools
    directory to run under Python 3 instead of Python 2. The PRESUBMIT.py
    scripts are already running under Python 3, but they were launching
    child scripts with Python 2. In most cases this is because they invoked
    python_executable (vpython) instead of python3_executable (vpython3). In
    one case it was because RunUnitTests defaults to running scripts
    under Python 2. Running under Python 3 requires either updating the
    shebang or passing skip_shebang_check=True. Not running under Python 2
    requires passing run_on_python2=False.
    
    This updates a call to checkperms.py (missed in crrev.com/c/3643458),
    two calls to pretty_print.py, and one call to validate_format.py. Other
    invocations of Python 2 from the tools directory were skipped because
    they required non-trivial updates to the scripts themselves.
    
    This also fixes some bugs in checkperms.py that only shows up under
    Python 3. The capture function returns decoded output but in one case
    decode() was being called on this decoded output. Under Python 3 this
    fails because str objects don't have a decode method. The other bugs
    were comparing data read from a binary file against str constants,
    which always compares negative on Python 3. The str constants were
    changed to binary constants. And a typo was fixed.
    
    Testing was done with these commands:
    
      git cl presubmit --files tools/metrics/structured/structured.xml
      git cl presubmit --files tools/metrics/ukm/ukm.xml
      git cl presubmit --files tools/checkperms/PRESUBMIT.py
    
    Bug: 1313804
    Change-Id: Ibdb9441b13cb1d10254e3ae3e82a480ca4bb1003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648903
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004428}

--- tools/metrics/ukm/PRESUBMIT.py
+++ tools/metrics/ukm/PRESUBMIT.py
@@ -23,7 +23,7 @@ def CheckChange(input_api, output_api):
       cwd = input_api.os_path.dirname(p)
 
       exit_code = input_api.subprocess.call(
-          [input_api.python_executable, 'pretty_print.py', '--presubmit'],
+          [input_api.python3_executable, 'pretty_print.py', '--presubmit'],
           cwd=cwd)
       if exit_code != 0:
         return [
@@ -32,7 +32,7 @@ def CheckChange(input_api, output_api):
         ]
 
       exit_code = input_api.subprocess.call(
-          [input_api.python_executable, 'validate_format.py', '--presubmit'],
+          [input_api.python3_executable, 'validate_format.py', '--presubmit'],
           cwd=cwd)
       if exit_code != 0:
         return [
