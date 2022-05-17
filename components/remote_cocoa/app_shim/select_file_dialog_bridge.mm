b25f33f65c2794eb6d16b11cde48e666bcbfb2ea
Avi Drissman <avi@chromium.org>
2022-05-17 17:20:49 +0000

    Remove AppKit polyfills
    The button and textfield utils polyfill macOS 10.12 API.
    Now that macOS 10.13 is the minimum OS required, directly
    use the API.
    
    All these UI elements have been verified to still work correctly.
    
    Fixed: 1241080
    Change-Id: I4ab65ec5c88bf7ada0fdf1f70bbe7a67aefff15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647733
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004330}

--- components/remote_cocoa/app_shim/select_file_dialog_bridge.mm
+++ components/remote_cocoa/app_shim/select_file_dialog_bridge.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/hang_watcher.h"
 #include "base/threading/thread_restrictions.h"
-#import "ui/base/cocoa/controls/textfield_utils.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/strings/grit/ui_strings.h"
 
@@ -49,9 +48,9 @@ NSString* GetDescriptionFromExtension(const base::FilePath::StringType& ext) {
 
 base::scoped_nsobject<NSView> CreateAccessoryView() {
   // The label. Add attributes per-OS to match the labels that macOS uses.
-  NSTextField* label = [TextFieldUtils
-      labelWithString:l10n_util::GetNSString(
-                          IDS_SAVE_PAGE_FILE_FORMAT_PROMPT_MAC)];
+  NSTextField* label =
+      [NSTextField labelWithString:l10n_util::GetNSString(
+                                       IDS_SAVE_PAGE_FILE_FORMAT_PROMPT_MAC)];
   label.translatesAutoresizingMaskIntoConstraints = NO;
   if (base::mac::IsAtLeastOS10_14())
     label.textColor = NSColor.secondaryLabelColor;
