66eabd8a1b7650432dd5994fa86e6f8a674c728e
S. Ganesh <ganesh@chromium.org>
2022-05-17 16:36:38 +0000

    Restrict System AppCommands to EXEs residing within %ProgramFiles%
    Bug: 1325693
    Change-Id: I03ca35aee83404d0c1f92e79b3d695c66906f91d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648730
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004298}

--- chrome/updater/win/test/test_strings.cc
+++ chrome/updater/win/test/test_strings.cc
@@ -11,5 +11,6 @@ namespace updater {
 const char kTestSleepMinutesSwitch[] = "test-sleep-minutes";
 const char kTestEventToSignal[] = "test-event-to-signal";
 const char kTestEventToWaitOn[] = "test-event-to-wait-on";
+const char kTestExitCode[] = "test-exit-code";
 
 }  // namespace updater
