8053b989f490a911e62946f19c8095c82498fb26
Andrey Zaytsev <andzaytsev@google.com>
2022-05-17 15:56:16 +0000

    Privacy Sandbox Settings: Update bottom sheet UI and buttons actions
    Screenshot: https://screenshot.googleplex.com/4GQGSv5HUuC5bD3.png
    
    Bug: 1286276
    Change-Id: Ib39c0d63c703c03ac975f2c2064627e7d29ece50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650740
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004281}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.privacy_sandbox;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -44,8 +43,7 @@ public class PrivacySandboxDialogController {
                     if (bottomSheetController == null) return false;
                     PrivacySandboxBottomSheetNotice bottomSheet =
                             new PrivacySandboxBottomSheetNotice(
-                                    LayoutInflater.from(context).inflate(
-                                            R.layout.privacy_sandbox_notice_bottom_sheet, null));
+                                    context, bottomSheetController, settingsLauncher);
                     bottomSheetController.requestShowContent(bottomSheet, /* animate= */ true);
                 } else {
                     dialog = new PrivacySandboxDialogNotice(context, settingsLauncher);
