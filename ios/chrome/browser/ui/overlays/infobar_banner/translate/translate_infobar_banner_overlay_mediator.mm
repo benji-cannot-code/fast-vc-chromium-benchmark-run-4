dcaf4abd49d11e4150eb37c46210ea06c6ee28d7
Ewann <ewannpv@google.com>
2022-05-17 13:29:44 +0000

    [iOS] Fixes translate message banner symbol
    This CL fixes the wrapper used to create the custom symbol.
    
    Bug: 3644430
    Change-Id: I5a16906c92c96c017dc5afbed73057b501a824c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650538
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004224}

--- ios/chrome/browser/ui/overlays/infobar_banner/translate/translate_infobar_banner_overlay_mediator.mm
+++ ios/chrome/browser/ui/overlays/infobar_banner/translate/translate_infobar_banner_overlay_mediator.mm
@@ -65,7 +65,7 @@ NSString* const kTranslateImageName = @"infobar_translate_icon";
   [self.consumer setButtonText:[self infobarButtonText]];
 
   UIImage* iconImage = UseSymbols()
-                           ? DefaultSymbolTemplateWithPointSize(
+                           ? CustomSymbolTemplateWithPointSize(
                                  kTranslateSymbol, kSymbolImagePointSize)
                            : [UIImage imageNamed:kTranslateImageName];
   [self.consumer setIconImage:iconImage];
