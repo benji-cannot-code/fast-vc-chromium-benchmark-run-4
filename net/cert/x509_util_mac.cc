17662b0a298b24fbf801c4d9523b2d9637e13e19
David Benjamin <davidben@chromium.org>
2022-05-17 16:33:37 +0000

    Remove cssmapplePriv.h
    The symbols in this header are no longer used, as of
    https://chromium-review.googlesource.com/c/chromium/src/+/3649371.
    
    Bug: 1322548, 590914
    Change-Id: Ibeedd3b860215034e57a42179f3047cb210b9341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652436
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004297}

--- net/cert/x509_util_mac.cc
+++ net/cert/x509_util_mac.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/strings/sys_string_conversions.h"
 #include "net/cert/x509_certificate.h"
-#include "third_party/apple_apsl/cssmapplePriv.h"
 #include "third_party/boringssl/src/include/openssl/pool.h"
 
 namespace net {
