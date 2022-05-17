48b0fd21635f935b8f940f4cca9329c8c2783be8
Oleg Davydov <burunduk@chromium.org>
2022-05-17 12:46:40 +0000

    [Extensions] Simplify ExtensionDownloader::TryFetchingExtensionsFromCache
    The ExtensionDownloader::TryFetchingExtensions method fetches some
    extensions from cache, but when some other are not found there, it also
    makes some retries/notifications related to extensions left. This
    creates confusion, as different actions are requires in different
    situations.
    
    After this commit the method mentioned only works with cache, leaving
    not found extensions to the call site.
    
    Additional tests added to ensure that this change doesn't affect logic.
    
    Bug: b:232534931
    Change-Id: I62451dabfa4225a3c34ea5831c1fb0abfde472b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645052
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004218}

--- extensions/browser/updater/extension_downloader_test_helper.h
+++ extensions/browser/updater/extension_downloader_test_helper.h
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+// Backoff policy to enforce zero initial delay.
+const extern net::BackoffEntry::Policy kZeroBackoffPolicy;
+
 class MockExtensionDownloaderDelegate
     : public testing::NiceMock<ExtensionDownloaderDelegate> {
  public:
--- extensions/browser/updater/extension_downloader_unittest.cc
+++ extensions/browser/updater/extension_downloader_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/updater/extension_downloader_types.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_urls.h"
+#include "services/network/test/test_utils.h"
 
 using testing::_;
 using testing::AnyNumber;
@@ -533,6 +534,66 @@ TEST_F(ExtensionDownloaderTest, TestMultipleUpdates) {
         request->request.url.spec(), "not found", net::HTTP_NOT_FOUND);
   }
   EXPECT_EQ(2, requests_count);
+}
+
+// Tests that extension download is retried if no network found and
+// extension not found in cache.
+TEST_F(ExtensionDownloaderTest, TestNoNetworkRetryAfterCacheMiss) {
+  ExtensionDownloaderTestHelper helper;
+
+  helper.downloader().SetBackoffPolicyForTesting(&kZeroBackoffPolicy);
+
+  helper.downloader().AddPendingExtension(ExtensionDownloaderTask(
+      kTestExtensionId, extension_urls::GetWebstoreUpdateUrl(),
+      mojom::ManifestLocation::kExternalPolicyDownload,
+      false /* is_corrupt_reinstall */, 0 /* request_id */,
+      DownloadFetchPriority::kBackground));
+  auto test_extension_cache = std::make_unique<ExtensionCacheFake>();
+  helper.downloader().StartAllPending(test_extension_cache.get());
+
+  ASSERT_EQ(1, helper.test_url_loader_factory().NumPending());
+  network::mojom::URLResponseHeadPtr response_head(
+      network::CreateURLResponseHead(net::HTTP_OK));
+  helper.test_url_loader_factory().SimulateResponseForPendingRequest(
+      helper.test_url_loader_factory().GetPendingRequest(0)->request.url,
+      network::URLLoaderCompletionStatus(net::ERR_INTERNET_DISCONNECTED),
+      std::move(response_head), "" /* content*/);
+
+  // ExtensionDownloader is expected to retry the request, so number of pending
+  // ones should be one again.
+  EXPECT_EQ(1, helper.test_url_loader_factory().NumPending());
+}
+
+// Tests that manifest fetch failure is properly reported if extension not found
+// in cache.
+TEST_F(ExtensionDownloaderTest, TestManifestFetchFailureAfterCacheMiss) {
+  ExtensionDownloaderTestHelper helper;
+
+  helper.downloader().SetBackoffPolicyForTesting(&kZeroBackoffPolicy);
+
+  helper.downloader().AddPendingExtension(ExtensionDownloaderTask(
+      kTestExtensionId, extension_urls::GetWebstoreUpdateUrl(),
+      mojom::ManifestLocation::kExternalPolicyDownload,
+      false /* is_corrupt_reinstall */, 0 /* request_id */,
+      DownloadFetchPriority::kBackground));
+
+  EXPECT_CALL(
+      helper.delegate(),
+      OnExtensionDownloadFailed(
+          kTestExtensionId,
+          ExtensionDownloaderDelegate::Error::MANIFEST_FETCH_FAILED, _, _, _));
+
+  auto test_extension_cache = std::make_unique<ExtensionCacheFake>();
+  helper.downloader().StartAllPending(test_extension_cache.get());
+
+  ASSERT_EQ(1, helper.test_url_loader_factory().NumPending());
+  helper.test_url_loader_factory().SimulateResponseForPendingRequest(
+      helper.test_url_loader_factory().GetPendingRequest(0)->request.url.spec(),
+      "" /* content */, net::HTTP_NOT_FOUND);
+
+  content::RunAllTasksUntilIdle();
+
+  testing::Mock::VerifyAndClearExpectations(&helper.delegate());
 }
 
 }  // namespace extensions
