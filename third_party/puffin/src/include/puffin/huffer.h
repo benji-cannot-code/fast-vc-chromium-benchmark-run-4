c1111932c50f161f5fab8c5d66cb009694095994
James <deckerjames@google.com>
2022-05-17 17:28:00 +0000

    Adding AOSP's puffin to chromium/src.
    This is the original snapshot from AOSP which will later be modified to
    match chromium standards and build requirements.
    
    Bug: 1320481
    Change-Id: I403302dd83d848d4c21f2025ea7d112c3578cdc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639351
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: James Decker <deckerjames@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004335}

---
+++ third_party/puffin/src/include/puffin/huffer.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_INCLUDE_PUFFIN_HUFFER_H_
+#define SRC_INCLUDE_PUFFIN_HUFFER_H_
+
+#include <cstddef>
+#include <memory>
+
+#include "puffin/common.h"
+
+namespace puffin {
+
+class BitWriterInterface;
+class PuffReaderInterface;
+class HuffmanTable;
+
+class Huffer {
+ public:
+  Huffer();
+  ~Huffer();
+
+  // Creates a deflate buffer from a puffed buffer. It is the reverse of
+  // |PuffDeflate|.
+  bool HuffDeflate(PuffReaderInterface* pr, BitWriterInterface* bw) const;
+
+ private:
+  std::unique_ptr<HuffmanTable> dyn_ht_;
+  std::unique_ptr<HuffmanTable> fix_ht_;
+
+  DISALLOW_COPY_AND_ASSIGN(Huffer);
+};
+
+}  // namespace puffin
+
+#endif  // SRC_INCLUDE_PUFFIN_HUFFER_H_
