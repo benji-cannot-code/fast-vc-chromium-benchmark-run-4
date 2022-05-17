1c7e4d09afa38941274bde534e6e1a4f487f0c96
Mustafa Emre Acer <meacer@chromium.org>
2022-05-17 23:57:18 +0000

    HTTPS-Only Mode: Reset the preference to NO in tests
    Some tests are failing due to the preference not being set to NO
    properly. This CL explicitly disables the pref in the test teardown
    instead of setting it to its original value.
    
    Bug: 1302509
    Change-Id: I399ce25943ac5efc9711e7c62a2357155965a77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652896
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004527}

--- ios/chrome/browser/web/https_only_mode_egtest.mm
+++ ios/chrome/browser/web/https_only_mode_egtest.mm
@@ -130,11 +130,6 @@ std::unique_ptr<net::test_server::HttpResponse> FakeHungHTTPSResponse(
 @property(nonatomic, readonly)
     net::test_server::EmbeddedTestServer* slowHTTPSServer;
 
-// The value of the kHttpsOnlyModeEnabled pref, before this test
-// started. Saved in order to restore it back to its original value after
-// the test completes.
-@property(nonatomic, assign) BOOL originalHttpsOnlyModeEnabled;
-
 @end
 
 @implementation HttpsOnlyModeUpgradeTestCase
@@ -204,14 +199,11 @@ std::unique_ptr<net::test_server::HttpResponse> FakeHungHTTPSResponse(
   [HttpsOnlyModeAppInterface useFakeHTTPSForTesting:false];
   [HttpsOnlyModeAppInterface setFallbackDelayForTesting:kVeryLongTimeout];
 
-  self.originalHttpsOnlyModeEnabled =
-      [ChromeEarlGrey userBooleanPref:prefs::kHttpsOnlyModeEnabled];
   [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kHttpsOnlyModeEnabled];
 }
 
 - (void)tearDown {
-  [ChromeEarlGrey setBoolValue:self.originalHttpsOnlyModeEnabled
-                   forUserPref:prefs::kHttpsOnlyModeEnabled];
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kHttpsOnlyModeEnabled];
   [HttpsOnlyModeAppInterface clearAllowlist];
 
   // Release the histogram tester.
