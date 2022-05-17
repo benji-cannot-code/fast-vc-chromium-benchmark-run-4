918489e0a2779958bbd31847a9d42c947d4e1fc7
Florian Leimgruber <fleimgruber@google.com>
2022-05-17 12:30:00 +0000

    Metrics for AutofillConsiderVariationCountryCodeForPhoneNumbers.
    Parse a phone number with both the app locale and the variation country
    code as its region, to determine the impact of this feature.
    
    Bug: 1287498
    Change-Id: I790ff30888977b1d6f8878175f97e212e564c418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640948
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004213}

--- components/autofill/core/browser/metrics/autofill_metrics.cc
+++ components/autofill/core/browser/metrics/autofill_metrics.cc
@@ -3174,7 +3174,7 @@ void AutofillMetrics::
 }
 
 // static
-void AutofillMetrics::LogAddressFormImportStatustMetric(
+void AutofillMetrics::LogAddressFormImportStatusMetric(
     AutofillMetrics::AddressProfileImportStatusMetric metric) {
   base::UmaHistogramEnumeration("Autofill.AddressProfileImportStatus", metric);
 }
@@ -3403,6 +3403,16 @@ void AutofillMetrics::LogRemovedSettingInaccessibleField(
   base::UmaHistogramEnumeration(
       "Autofill.ProfileImport.InaccessibleFieldsRemoved.ByFieldType",
       ConvertSettingsVisibleFieldTypeForMetrics(field));
+}
+
+// static
+void AutofillMetrics::LogPhoneNumberImportParsingResult(
+    bool with_variation_country_code,
+    bool with_app_locale) {
+  base::UmaHistogramEnumeration(
+      "Autofill.ProfileImport.PhoneNumberParsingResult",
+      static_cast<AutofillMetrics::PhoneNumberImportParsingResult>(
+          (with_variation_country_code << 1) | with_app_locale));
 }
 
 void AutofillMetrics::LogVerificationStatusOfNameTokensOnProfileUsage(
