bbd3d8cd24a03c415605fe587068387a21df59f3
jlebel <jlebel@chromium.org>
2022-05-17 12:30:19 +0000

    [iOS] Add fade in animation for ActivityOverlayView
    This patch adds a fade in animation for ActivityOverlayView. This avoids
    UI glitch when the async task is too fast.
    For example when sign-in task is instantaneous in the FRE.
    
    Fixed: 1312444
    Change-Id: Ic9c21595e53f8347c300778d100affbc0d264d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647058
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004214}

--- ios/chrome/browser/ui/elements/activity_overlay_view.mm
+++ ios/chrome/browser/ui/elements/activity_overlay_view.mm
@@ -27,10 +27,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)didMoveToSuperview {
   if (self.subviews.count == 0) {
     // This is the first time the view is used, finish setting everything up.
-    self.backgroundColor = [UIColor colorNamed:kScrimBackgroundColor];
-    [self addSubview:_indicator];
-    AddSameCenterConstraints(self, _indicator);
-    [_indicator startAnimating];
+    [self addSubview:self.indicator];
+    AddSameCenterConstraints(self, self.indicator);
+    [self.indicator startAnimating];
+    // It is better to use background color instead of alpha, so
+    // ActivityOverlayView blocks all taps as soon as it is added in the view
+    // hierarchy.
+    self.backgroundColor = [UIColor clearColor];
+    self.indicator.alpha = 0.;
+    [UIView animateWithDuration:.3
+                     animations:^{
+                       self.backgroundColor =
+                           [UIColor colorNamed:kScrimBackgroundColor];
+                       self.indicator.alpha = 1.;
+                     }
+                     completion:NULL];
   }
   [super didMoveToSuperview];
 }
