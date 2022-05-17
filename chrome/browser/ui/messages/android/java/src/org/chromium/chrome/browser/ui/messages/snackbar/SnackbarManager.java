c09444931ea95f4df0c2576f0e30f6ed458af630
Neil Coronado <nemco@google.com>
2022-05-17 17:03:02 +0000

    Add tests for SnackbarManager#setParentView()
    Add tests for SnackbarManager#setParentView()
    
    Bug: 1325423
    Change-Id: I03acbd141c64699d1863e624e795599bf5eb258f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645677
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004315}

--- chrome/browser/ui/messages/android/java/src/org/chromium/chrome/browser/ui/messages/snackbar/SnackbarManager.java
+++ chrome/browser/ui/messages/android/java/src/org/chromium/chrome/browser/ui/messages/snackbar/SnackbarManager.java
@@ -342,4 +342,12 @@ public class SnackbarManager implements OnClickListener, ActivityStateListener, 
     public Snackbar getCurrentSnackbarForTesting() {
         return mSnackbars.getCurrent();
     }
+
+    /**
+     * @return The currently showing snackbar view. For testing only.
+     */
+    @VisibleForTesting
+    public SnackbarView getCurrentSnackbarViewForTesting() {
+        return mView;
+    }
 }
