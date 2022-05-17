31acd67a6abdf0be3c7b47f4d19e681b364d12b4
amraboelkher <amraboelkher@chromium.org>
2022-05-17 17:11:38 +0000

    PSM: PSM API that uses DMServer
    This CL is separating the PSM (PsmHelper) implementation details from
    AutoEnrollmentClientImpl into its own class. Also, it is making
    PsmResult that exist in AutoEnrollmentClientImpl to use the one that
    got introduced in PsmRlweDmserverClient by including its header.
    
    The plan to ease the reviewing process will be as follows:
    1) PSM API
    2) PSM API unittests (Have so many changes e.g. Removing RunUntilIdle())
    3) Remove PsmHelper and replace it with PSM API, and remove
    PsmHelper unittests only (not the one for the whole initial
    enrollment)
    
    Bug: 1303532
    Change-Id: I2892817552179ff6e804c94f326dd8ef541b4057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497675
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004324}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl.cc
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/ash/login/enrollment/auto_enrollment_controller.h"
+#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client.h"
 #include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_id_provider.h"
 #include "chrome/browser/ash/policy/server_backed_state/server_backed_device_state.h"
 #include "chrome/common/chrome_content_client.h"
