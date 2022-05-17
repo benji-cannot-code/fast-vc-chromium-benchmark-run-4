f518b3b73de4d30bab3d0915adc29037bc77718c
Kenneth Russell <kbr@chromium.org>
2022-05-17 03:23:24 +0000

    Roll WebGL cf04aeb..91203c5
    https://chromium.googlesource.com/external/khronosgroup/webgl.git/+log/cf04aeb..91203c5
    
    Update roll_webgl_conformance.py to use Python3 and remove
    linux-angle-rel from the manually added tryservers. Its tests are fully
    redundant with linux-rel and linux_optional_gpu_tests_rel since the
    passthrough command decoder launched on Linux.
    
    Bug: angleproject:6972
    Bug: angleproject:7073
    Bug: 953120
    Bug: 1208480
    Bug: 1305750
    Cq-Include-Trybots: luci.chromium.try:win_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ie0fa86bdf02d672fbe81b75f56380f571b89e056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645684
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004092}

--- tools/roll_webgl_conformance.py
+++ tools/roll_webgl_conformance.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -31,12 +31,6 @@ extra_trybots = [
     {
         "mastername": "luci.chromium.try",
         "buildernames": ["android_optional_gpu_tests_rel"]
-    },
-    # Include the ANGLE tryservers which run the WebGL conformance tests
-    # in some non-default configurations.
-    {
-        "mastername": "luci.chromium.try",
-        "buildernames": ["linux-angle-rel"]
     },
 ]
 
