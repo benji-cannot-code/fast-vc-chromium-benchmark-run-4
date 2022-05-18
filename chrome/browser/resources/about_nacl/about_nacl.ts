4630a8fe5f4f394cf40e6b0ab19a8de2f5a798a1
dpapad <dpapad@chromium.org>
2022-05-18 19:53:03 +0000

    AboutNacl: Migrate to TypeScript.
    Bug: 1316438
    Change-Id: I74415d810a9f05b8ae11d4cf26d6a5880f4f552b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653101
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004917}

--- chrome/browser/resources/about_nacl/about_nacl.ts
+++ chrome/browser/resources/about_nacl/about_nacl.ts
@@ -5,15 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
 import {$} from 'chrome://resources/js/util.m.js';
+
+type ModuleListData = {
+  naclInfo: Array<{key: string, value: string}>,
+};
+
+type DomBindElement = HTMLElement&{moduleListData: ModuleListData};
 
 /**
  * Asks the C++ NaClDOMHandler to get details about the NaCl and
  * re-populates the page with the data.
  */
 function initialize() {
-  sendWithPromise('requestNaClInfo').then((moduleListData) => {
+  sendWithPromise('requestNaClInfo').then((moduleListData: ModuleListData) => {
     $('loading-message').hidden = true;
     $('body-container').hidden = false;
 
@@ -22,8 +29,9 @@ function initialize() {
      * the currently available modules and populates the HTML template
      * with that data.
      */
-    const bind = document.body.querySelector('dom-bind');
-    bind.naclInfo = moduleListData.naclInfo;
+    const bind = document.body.querySelector<DomBindElement>('dom-bind');
+    assert(bind);
+    bind.moduleListData = moduleListData;
   });
 }
 
