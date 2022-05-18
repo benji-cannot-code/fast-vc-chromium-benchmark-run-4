855a2d6ff80ea34ec78de6b1042e9820a28f7fbf
Julie Jeongeun Kim <jkim@igalia.com>
2022-05-18 05:42:07 +0000

    [MPArch] Trigger beforeunload only for Page.close() in the primary page
    This CL skips dispatching beforeunload when Page.close() in devtools is
    called from a fenced frame.
    
    It removes calling DispatchBeforeUnload by WebContents and adds
    checking if the render frame host is the outermost main frame. Then, it
    dispatches beforeunload on its own render frame host, considering that
    it could be also from prerendering.
    
    Bug: 1324905
    Change-Id: Ib5f9a3578576799356cf0a4bb578a9c31e6b553e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647602
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004613}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/navigation_entry_impl.h"
 #include "content/browser/renderer_host/navigation_request.h"
 #include "content/browser/renderer_host/navigator.h"
+#include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -403,10 +404,14 @@ Response PageHandler::Crash() {
 }
 
 Response PageHandler::Close() {
-  WebContentsImpl* web_contents = GetWebContents();
-  if (!web_contents)
+  if (!host_)
     return Response::ServerError("Not attached to a page");
-  web_contents->DispatchBeforeUnload(false /* auto_cancel */);
+
+  if (!host_->IsOutermostMainFrame())
+    return Response::ServerError(kCommandIsOnlyAvailableAtTopTarget);
+
+  host_->DispatchBeforeUnload(RenderFrameHostImpl::BeforeUnloadType::TAB_CLOSE,
+                              false);
   return Response::Success();
 }
 
