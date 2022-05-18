555615047586365f155012d93f78f7849b4ce764
Alison Maher <almaher@microsoft.com>
2022-05-18 14:09:40 +0000

    OOF in nested spanner multicol
    If an OOF lived in a nested multicol, and the innermost multicol was
    a column spanner, NGOutOfFlowLayoutPart::ReplaceFragment() would fail
    to find the correct column spanner fragment to replace. To fix this,
    look at the direct children of the spanner's containing multicol for
    the spanner rather than in the multicol's fragmentainers.
    
    Bug: 1324906
    Change-Id: If098503040f7ed9670c4cfcebb6bd58a27f38196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652128
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1004745}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
@@ -2070,6 +2070,7 @@ void NGOutOfFlowLayoutPart::ReplaceFragment(
 
   if (!containing_block->IsFragmentationContextRoot()) {
     DCHECK_NE(containing_block, container_builder_->GetLayoutObject());
+    DCHECK(!box.IsColumnSpanAll());
     for (const auto& parent_fragment : containing_block->PhysicalFragments()) {
       if (parent_fragment.HasItems()) {
         // Look inside the inline formatting context to find and replace the
@@ -2087,6 +2088,7 @@ void NGOutOfFlowLayoutPart::ReplaceFragment(
         return;
     }
   } else if (containing_block == container_builder_->GetLayoutObject()) {
+    DCHECK(!box.IsColumnSpanAll());
     // We're currently laying out |containing_block|, and it's a multicol
     // container. Search inside fragmentainer children in the builder.
     for (const NGContainerFragmentBuilder::ChildWithOffset& child :
@@ -2099,9 +2101,15 @@ void NGOutOfFlowLayoutPart::ReplaceFragment(
     // container. Search inside fragmentainer children of the fragments
     // generated for the containing block.
     for (const auto& multicol : containing_block->PhysicalFragments()) {
-      for (const auto& child : multicol.Children()) {
-        if (ReplaceFragmentainerChild(*child.fragment))
+      if (box.IsColumnSpanAll()) {
+        // Column spanners are found as direct children of the multicol.
+        if (ReplaceChild(multicol))
           return;
+      } else {
+        for (const auto& child : multicol.Children()) {
+          if (ReplaceFragmentainerChild(*child.fragment))
+            return;
+        }
       }
     }
   }
