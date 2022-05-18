243e225f4162e245c1cb7f82658d9acb0612de98
Cici Ruan <cuicuiruan@google.com>
2022-05-18 19:54:28 +0000

    arc/gio: Fix the crash when closing the window with menu
    When destroying the window, the surface is already destroyed, which
    means that the overlay is already destroyed. So it gets null object when
    trying to destroy the menu view.
    
    Bug: b:188452744
    Test: manual test and unittest. No crash.
    Change-Id: I0cd3fa632e52d041631b66126403b5143a4e80dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3649313
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004919}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -87,13 +87,13 @@ void DisplayOverlayController::AddOverlay(DisplayMode display_mode) {
 }
 
 void DisplayOverlayController::RemoveOverlayIfAny() {
-  // Call |RemoveInputMenuView| explicitly to make sure UMA stats is updated.
-  RemoveInputMenuView();
-
   auto* shell_surface_base =
       exo::GetShellSurfaceBaseForWindow(touch_injector_->target_window());
-  if (shell_surface_base && shell_surface_base->HasOverlay())
+  if (shell_surface_base && shell_surface_base->HasOverlay()) {
+    // Call |RemoveInputMenuView| explicitly to make sure UMA stats is updated.
+    RemoveInputMenuView();
     shell_surface_base->RemoveOverlay();
+  }
 }
 
 void DisplayOverlayController::AddMenuEntryView(views::Widget* overlay_widget) {
