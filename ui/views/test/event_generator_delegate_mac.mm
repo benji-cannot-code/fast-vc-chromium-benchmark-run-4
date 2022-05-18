9d19d3da9f4eba3206fc98351aa386382c79ca1b
Tim Sergeant <tsergeant@chromium.org>
2022-05-18 23:40:02 +0000

    Reland "Intents: Simplify IntentPickerBubbleView tests"
    This is a reland of commit af3a07eae0811c6e5e46ac606b270caffd096ba0
    
    This includes a fix to event_generator_delegate_mac.mm, which now plumbs
    MouseEvent timestamps through NSEvent correctly. This allows
    double-clicking to work correctly in Mac tests.
    
    Original change's description:
    > Intents: Simplify IntentPickerBubbleView tests
    >
    > This simplifies the testing APIs for IntentPickerBubbleView so that
    > tests interact with the view hierarchy directly, instead of relying on
    > private testing APIs to poke the bubble.
    >
    > This will make it easier for us to refactor and change the contents of
    > the bubble without needing to rewrite all the tests as we go.
    >
    > Bug: 1321501
    > Change-Id: I16184fc997ec54d571e1e38cc55021321770a966
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644929
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1003648}
    
    Cq-Include-Trybots: luci.chromium.try:mac11-arm64-rel
    Bug: 1321501
    Change-Id: I862f2638b1a416c2b75e96d35dda38739d100d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650368
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005022}

--- ui/views/test/event_generator_delegate_mac.mm
+++ ui/views/test/event_generator_delegate_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "ui/base/cocoa/cocoa_base_utils.h"
 #include "ui/display/screen.h"
+#include "ui/events/base_event_utils.h"
 #include "ui/events/event.h"
 #include "ui/events/event_dispatcher.h"
 #include "ui/events/event_processor.h"
@@ -196,6 +197,7 @@ void EmulateSendEvent(NSWindow* window, NSEvent* event) {
 NSEvent* CreateMouseEventInWindow(NSWindow* window,
                                   ui::EventType event_type,
                                   const gfx::Point& point_in_root,
+                                  const base::TimeTicks time_stamp,
                                   int flags) {
   NSUInteger click_count = 0;
   if (event_type == ui::ET_MOUSE_PRESSED ||
@@ -213,7 +215,7 @@ NSEvent* CreateMouseEventInWindow(NSWindow* window,
   return [NSEvent mouseEventWithType:type
                             location:point
                        modifierFlags:modifiers
-                           timestamp:0
+                           timestamp:ui::EventTimeStampToSeconds(time_stamp)
                         windowNumber:[window windowNumber]
                              context:nil
                          eventNumber:0
@@ -427,7 +429,8 @@ void EventGeneratorDelegateMac::OnMouseEvent(ui::MouseEvent* event) {
       event->type() == ui::ET_MOUSEWHEEL
           ? CreateMouseWheelEventInWindow(target_window_, event)
           : CreateMouseEventInWindow(target_window_, event->type(),
-                                     event->location(), event->flags());
+                                     event->location(), event->time_stamp(),
+                                     event->flags());
 
   using Target = ui::test::EventGenerator::Target;
   switch (owner_->target()) {
