004c63bf3c110f6bf31662f9ab40a78cbb0a8c4a
Mahmoud Rashad <mmrashad@google.com>
2022-05-18 17:57:10 +0000

    [sync] Change the title of the NTP content suggestion sync promo
    This CL changes the title of the NTP content suggestion sync promo
    (which is only shown when SyncAndroidPromosWithTitle is enabled) from
    "See stories for you" to "See content for you", and fixes some typos.
    
    Bug: 1297981
    Change-Id: I1b4b69afafc05e0d342e161d9549ab10241e1cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644948
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004850}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninPromoControllerRenderTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninPromoControllerRenderTest.java
@@ -65,6 +65,7 @@ public class SigninPromoControllerRenderTest {
     @Rule
     public final RenderTestRule mRenderTestRule =
             RenderTestRule.Builder.withPublicCorpus()
+                    .setRevision(1)
                     .setBugComponent(RenderTestRule.Component.SERVICES_SIGN_IN)
                     .build();
 
