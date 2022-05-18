a04270577c9ac4518c00d19d7168d38e7eb55584
Allen Bauer <kylixrd@chromium.org>
2022-05-18 13:26:11 +0000

    [ExtensionsToolbarContainer] Use only OnWidgetDestroying.
    Change-Id: I9bf2643d09bd15c5bd8383e8bfeec40f8db8a534
    Bug: 1240365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652675
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004733}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.h
@@ -243,7 +243,6 @@ class ExtensionsToolbarContainer : public ToolbarIconContainerView,
   void OnToolbarPinnedActionsChanged() override;
 
   // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override;
   void OnWidgetDestroying(views::Widget* widget) override;
 
   // Moves the dragged extension `action_id`.
