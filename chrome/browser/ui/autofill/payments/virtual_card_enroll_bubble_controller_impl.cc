5e3066eb0fb97e46f1fdab18bb700fcf50c6c484
Vishwas Uppoor <vishwasuppoor@google.com>
2022-05-18 17:59:48 +0000

    [VCN] Fix enrollment dialog closes when links are clicked
    The virtual card enrollment dialog closes when users navigate away from the page by clicking on links. Reopen the dialog when users returns to the tab.
    
    Bug: 1281695, 1323751
    Change-Id: I0a129eeac1b8451b008e367b84d747eae64cc08f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653035
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004855}

--- chrome/browser/ui/autofill/payments/virtual_card_enroll_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/virtual_card_enroll_bubble_controller_impl.cc
@@ -217,12 +217,17 @@ void VirtualCardEnrollBubbleControllerImpl::OnDeclined(JNIEnv* env) {
 }
 
 void VirtualCardEnrollBubbleControllerImpl::OnDismissed(JNIEnv* env) {
+  // If the dialog is to be shown again because user clicked on links, do not
+  // log metrics.
+  if (reprompt_required_)
+    return;
   OnBubbleClosed(PaymentsBubbleClosedReason::kNotInteracted);
 }
 
 void VirtualCardEnrollBubbleControllerImpl::OnLinkClicked(JNIEnv* env,
                                                           jstring url,
                                                           jint link_type) {
+  reprompt_required_ = true;
   OnLinkClicked(static_cast<autofill::VirtualCardEnrollmentLinkType>(link_type),
                 GURL(base::android::ConvertJavaStringToUTF16(env, url)));
 }
@@ -239,7 +244,12 @@ bool VirtualCardEnrollBubbleControllerImpl::IsIconVisible() const {
 void VirtualCardEnrollBubbleControllerImpl::OnVisibilityChanged(
     content::Visibility visibility) {
 #if BUILDFLAG(IS_ANDROID)
-  AutofillBubbleControllerBase::OnVisibilityChanged(visibility);
+  if (visibility == content::Visibility::VISIBLE && !bubble_view() &&
+      reprompt_required_ && messages::IsSaveCardMessagesUiEnabled()) {
+    Show();
+  } else if (visibility == content::Visibility::HIDDEN) {
+    HideBubble();
+  }
 #else
   if (visibility == content::Visibility::VISIBLE && !bubble_view() &&
       bubble_state_ == BubbleState::kShowingIconAndBubble) {
@@ -263,6 +273,7 @@ void VirtualCardEnrollBubbleControllerImpl::DoShowBubble() {
       GetVirtualCardEnrollmentBubbleSource() ==
           VirtualCardEnrollmentBubbleSource::
               VIRTUAL_CARD_ENROLLMENT_UPSTREAM_SOURCE) {
+    reprompt_required_ = false;
     DCHECK(!bubble_view());
 
     set_bubble_view(
