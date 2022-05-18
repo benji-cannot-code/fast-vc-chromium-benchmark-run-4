8756c6b64d2e70f23d5963bf5c1dd1bed0940f29
David Schinazi <dschinazi@chromium.org>
2022-05-18 17:09:20 +0000

    Remove Q050 from default supported QUIC versions
    R=fayang@chromium.org
    
    Change-Id: I820b089580c0eb66d37caf88e6438a1f509990f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648380
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Fan Yang <fayang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004814}

--- components/network_session_configurator/browser/network_session_configurator_unittest.cc
+++ components/network_session_configurator/browser/network_session_configurator_unittest.cc
@@ -154,7 +154,7 @@ TEST_F(NetworkSessionConfiguratorTest, EnableQuicFromParams) {
 }
 
 TEST_F(NetworkSessionConfiguratorTest, ValidQuicParams) {
-  quic::ParsedQuicVersion version = net::DefaultSupportedQuicVersions().front();
+  quic::ParsedQuicVersion version = quic::AllSupportedVersions().front();
   std::map<std::string, std::string> field_trial_params;
   field_trial_params["enable_quic"] = "true";
   field_trial_params["channel"] = "T";
@@ -174,7 +174,7 @@ TEST_F(NetworkSessionConfiguratorTest, ValidQuicParams) {
 }
 
 TEST_F(NetworkSessionConfiguratorTest, InvalidQuicParams) {
-  quic::ParsedQuicVersion version = net::DefaultSupportedQuicVersions().front();
+  quic::ParsedQuicVersion version = quic::AllSupportedVersions().front();
   std::map<std::string, std::string> field_trial_params;
   field_trial_params["enable_quic"] = "true";
   // These params are missing channel and epoch.
@@ -582,6 +582,7 @@ TEST_F(NetworkSessionConfiguratorTest, QuicVersionFromFieldTrialParams) {
   std::map<std::string, std::string> field_trial_params;
   field_trial_params["quic_version"] =
       quic::QuicVersionToString(version.transport_version);
+  field_trial_params["obsolete_versions_allowed"] = "true";
   variations::AssociateVariationParams("QUIC", "Enabled", field_trial_params);
   base::FieldTrialList::CreateFieldTrial("QUIC", "Enabled");
 
