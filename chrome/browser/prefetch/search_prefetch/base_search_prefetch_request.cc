a50b9636f4a2c2bf91ab780ad54a9fec17936599
Simon Pelchat <spelchat@chromium.org>
2022-05-18 23:24:22 +0000

    Fix recording click time in Search Prefetch.
    time_clicked_ was often 0 because prefetches that complete before
    navigation weren't calling MarkPrefetchAsClicked.
    
    Bug: 1327070
    Change-Id: I662803f4bac7c063b6f37edc5fbfb3bcc252e49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654658
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005014}

--- chrome/browser/prefetch/search_prefetch/base_search_prefetch_request.cc
+++ chrome/browser/prefetch/search_prefetch/base_search_prefetch_request.cc
@@ -317,7 +317,6 @@ void BaseSearchPrefetchRequest::MarkPrefetchAsComplete() {
 void BaseSearchPrefetchRequest::MarkPrefetchAsClicked() {
   DCHECK(current_status_ == SearchPrefetchStatus::kCanBeServed);
   current_status_ = SearchPrefetchStatus::kCanBeServedAndUserClicked;
-  time_clicked_ = base::TimeTicks::Now();
 }
 
 void BaseSearchPrefetchRequest::MarkPrefetchAsServed() {
@@ -326,4 +325,8 @@ void BaseSearchPrefetchRequest::MarkPrefetchAsServed() {
   current_status_ = SearchPrefetchStatus::kServed;
   UMA_HISTOGRAM_TIMES("Omnibox.SearchPrefetch.ClickToNavigationIntercepted",
                       base::TimeTicks::Now() - time_clicked_);
+}
+
+void BaseSearchPrefetchRequest::RecordClickTime() {
+  time_clicked_ = base::TimeTicks::Now();
 }
