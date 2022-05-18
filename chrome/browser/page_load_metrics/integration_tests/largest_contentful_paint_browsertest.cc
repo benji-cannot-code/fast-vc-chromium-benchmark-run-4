9da689f223ebd3bbdef10b3d64ddc421e1f4f4a7
Lan Wei <lanwei@chromium.org>
2022-05-18 22:00:53 +0000

    Reenables MouseoverLCPTest in largest_contentful_paint_browsertest.cc
    We have fixed the issue that we have seen that in some debug enabled
    bots, the tests are run very slow. The image is still painting but we
    start update metrics, so we get a 0 value for the time of the largest
    contentful paint, and we always choose the smaller value which is 0 when
    we merge LCP metrics. So we could not get a valid time for LCP in
    UkmPageLoadMetricsObserver. Now we fixed it and can enable these tests.
    
    Bug: 1288027
    Change-Id: I1f08c5a542966c200973eb41cb571c90dabd55b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645152
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004982}

--- chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
@@ -299,11 +299,11 @@ class MouseoverLCPTest : public MetricIntegrationTest {
                     .error,
                 "");
     }
+    waiter->Wait();
 
     // Need to navigate away from the test html page to force metrics to get
     // flushed/synced.
     ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL("about:blank")));
-    waiter->Wait();
     ExpectUKMPageLoadMetricFlagSet(
         PageLoad::kPaintTiming_LargestContentfulPaintTypeName,
         LargestContentfulPaintTypeToUKMFlags(flag_set), expected);
@@ -311,7 +311,7 @@ class MouseoverLCPTest : public MetricIntegrationTest {
 };
 
 IN_PROC_BROWSER_TEST_F(MouseoverLCPTest,
-                       DISABLED_LargestContentfulPaint_MouseoverOverLCPImage) {
+                       LargestContentfulPaint_MouseoverOverLCPImage) {
   test_mouseover("/mouseover.html",
                  blink::mojom::LargestContentfulPaintType::kAfterMouseover,
                  /*entries=*/"2",
@@ -321,9 +321,8 @@ IN_PROC_BROWSER_TEST_F(MouseoverLCPTest,
                  /*expected=*/true);
 }
 
-IN_PROC_BROWSER_TEST_F(
-    MouseoverLCPTest,
-    DISABLED_LargestContentfulPaint_MouseoverOverLCPImageReplace) {
+IN_PROC_BROWSER_TEST_F(MouseoverLCPTest,
+                       LargestContentfulPaint_MouseoverOverLCPImageReplace) {
   test_mouseover("/mouseover.html?replace",
                  blink::mojom::LargestContentfulPaintType::kAfterMouseover,
                  /*entries=*/"2",
@@ -344,9 +343,8 @@ IN_PROC_BROWSER_TEST_F(MouseoverLCPTest,
                  /*expected=*/false);
 }
 
-IN_PROC_BROWSER_TEST_F(
-    MouseoverLCPTest,
-    DISABLED_LargestContentfulPaint_MouseoverOverLCPImageThenBody) {
+IN_PROC_BROWSER_TEST_F(MouseoverLCPTest,
+                       LargestContentfulPaint_MouseoverOverLCPImageThenBody) {
   test_mouseover("/mouseover.html?dispatch",
                  blink::mojom::LargestContentfulPaintType::kAfterMouseover,
                  /*entries=*/"2",
