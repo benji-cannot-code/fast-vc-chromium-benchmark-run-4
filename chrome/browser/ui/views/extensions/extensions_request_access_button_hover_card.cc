57a8b102ba05b88726165b194a11c02839e70da6
Scott Little <sclittle@chromium.org>
2022-05-18 16:12:09 +0000

    Revert "Make CustomViewFactory final"
    This reverts commit 5a802d7f7648373523705d689dd1e55ae8ac6942.
    
    Reason for revert: Looks like this broke compile: https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20x64%20Builder%20(dbg)/137893/overview
    
    Original change's description:
    > Make CustomViewFactory final
    >
    > This makes BubbleDialogModelHost::CustomViewFactory final by instead of
    > providing an interface using a callback and a FieldType in the
    > constructor.
    >
    > This makes calling code simpler.
    >
    > Bug: 1325460
    > Change-Id: I0a7fe3327c339dff7a62771489b4cd83d2722a35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651977
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1004770}
    
    Bug: 1325460
    Change-Id: I8e7250d41315bda4a63eeaaa252c1a9c71a9ed97
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654235
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Owners-Override: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004793}

--- chrome/browser/ui/views/extensions/extensions_request_access_button_hover_card.cc
+++ chrome/browser/ui/views/extensions/extensions_request_access_button_hover_card.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/extensions/extensions_request_access_button_hover_card.h"
-#include <memory>
 
 #include "base/bind.h"
 #include "base/strings/strcat.h"
@@ -35,31 +34,38 @@ ui::ImageModel GetIcon(ToolbarActionViewController* action,
           .AsImageSkia());
 }
 
-std::unique_ptr<views::BubbleDialogModelHost::CustomViewFactory>
-CreateExtensionItemFactory(std::u16string name, ui::ImageModel icon) {
-  return std::make_unique<views::BubbleDialogModelHost::CustomViewFactory>(
-      base::BindOnce(
-          [](std::u16string name,
-             ui::ImageModel icon) -> std::unique_ptr<views::View> {
-            const gfx::Insets content_insets =
-                ChromeLayoutProvider::Get()->GetDialogInsetsForContentType(
-                    views::DialogContentType::kText,
-                    views::DialogContentType::kText);
+class ExtensionItemFactory
+    : public views::BubbleDialogModelHost::CustomViewFactory {
+ public:
+  ExtensionItemFactory(const std::u16string& name, const ui::ImageModel& icon)
+      : name_(std::move(name)), icon_(icon) {}
+  ~ExtensionItemFactory() override = default;
 
-            return views::Builder<views::FlexLayoutView>()
-                .SetOrientation(views::LayoutOrientation::kHorizontal)
-                .SetMainAxisAlignment(views::LayoutAlignment::kStart)
-                .SetCrossAxisAlignment(views::LayoutAlignment::kCenter)
-                .SetBorder(views::CreateEmptyBorder(gfx::Insets::TLBR(
-                    0, content_insets.left(), 0, content_insets.right())))
-                .AddChildren(
-                    views::Builder<views::ImageView>().SetImage(icon),
-                    views::Builder<views::Label>().SetText(std::move(name)))
-                .Build();
-          },
-          std::move(name), std::move(icon)),
-      views::BubbleDialogModelHost::FieldType::kMenuItem);
-}
+  // views::BubbleDialogModelHost::CustomViewFactory:
+  std::unique_ptr<views::View> CreateView() override {
+    const ChromeLayoutProvider* provider = ChromeLayoutProvider::Get();
+    const gfx::Insets content_insets = provider->GetDialogInsetsForContentType(
+        views::DialogContentType::kText, views::DialogContentType::kText);
+
+    return views::Builder<views::FlexLayoutView>()
+        .SetOrientation(views::LayoutOrientation::kHorizontal)
+        .SetMainAxisAlignment(views::LayoutAlignment::kStart)
+        .SetCrossAxisAlignment(views::LayoutAlignment::kCenter)
+        .SetBorder(views::CreateEmptyBorder(gfx::Insets::TLBR(
+            0, content_insets.left(), 0, content_insets.right())))
+        .AddChildren(views::Builder<views::ImageView>().SetImage(icon_),
+                     views::Builder<views::Label>().SetText(name_))
+        .Build();
+  }
+
+  views::BubbleDialogModelHost::FieldType GetFieldType() const override {
+    return views::BubbleDialogModelHost::FieldType::kMenuItem;
+  }
+
+ private:
+  const std::u16string name_;
+  const ui::ImageModel icon_;
+};
 
 }  // namespace
 
@@ -93,7 +99,7 @@ void ExtensionsRequestAccessButtonHoverCard::ShowBubble(
         IDS_EXTENSIONS_REQUEST_ACCESS_BUTTON_TOOLTIP_MULTIPLE_EXTENSIONS,
         url)));
     for (auto* action : actions) {
-      dialog_builder.AddCustomField(CreateExtensionItemFactory(
+      dialog_builder.AddCustomField(std::make_unique<ExtensionItemFactory>(
           action->GetActionName(), GetIcon(action, web_contents)));
     }
   }
