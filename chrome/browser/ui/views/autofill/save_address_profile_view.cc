16acdb066a403f995e0770d9905674969a842b40
Florian Leimgruber <fleimgruber@google.com>
2022-05-18 15:28:01 +0000

    Remove AddressFieldToServerFieldType in favour of i18n::TypeForField.
    i18n::TypeForField implements the same conversion logic. This CL
    removes AddressFieldToServerFieldType and calls i18n::TypeForField
    instead.
    
    Bug: 1300548
    Change-Id: I28a3e884cf59185538366f136a75601957c54e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650510
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004767}

--- chrome/browser/ui/views/autofill/save_address_profile_view.cc
+++ chrome/browser/ui/views/autofill/save_address_profile_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_address_util.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/browser/geo/address_i18n.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
@@ -47,7 +48,7 @@ constexpr int kIconSize = 16;
 // RECIPIENT type.
 ServerFieldType AddressFieldToServerFieldTypeWithHonorificPrefix(
     ::i18n::addressinput::AddressField address_field) {
-  ServerFieldType type = autofill::AddressFieldToServerFieldType(address_field);
+  ServerFieldType type = autofill::i18n::TypeForField(address_field);
   return type == NAME_FULL ? NAME_FULL_WITH_HONORIFIC_PREFIX : type;
 }
 
--- components/autofill/core/browser/autofill_address_util.cc
+++ components/autofill/core/browser/autofill_address_util.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_profile_comparator.h"
+#include "components/autofill/core/browser/geo/address_i18n.h"
 #include "components/autofill/core/browser/geo/autofill_country.h"
 #include "components/autofill/core/browser/ui/country_combobox_model.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -60,29 +61,6 @@ std::vector<AddressUiComponent> GetAddressComponents(
 }
 
 }  // namespace
-
-ServerFieldType AddressFieldToServerFieldType(AddressField address_field) {
-  switch (address_field) {
-    case ::i18n::addressinput::COUNTRY:
-      return ADDRESS_HOME_COUNTRY;
-    case ::i18n::addressinput::ADMIN_AREA:
-      return ADDRESS_HOME_STATE;
-    case ::i18n::addressinput::LOCALITY:
-      return ADDRESS_HOME_CITY;
-    case ::i18n::addressinput::DEPENDENT_LOCALITY:
-      return ADDRESS_HOME_DEPENDENT_LOCALITY;
-    case ::i18n::addressinput::SORTING_CODE:
-      return ADDRESS_HOME_SORTING_CODE;
-    case ::i18n::addressinput::POSTAL_CODE:
-      return ADDRESS_HOME_ZIP;
-    case ::i18n::addressinput::STREET_ADDRESS:
-      return ADDRESS_HOME_STREET_ADDRESS;
-    case ::i18n::addressinput::ORGANIZATION:
-      return COMPANY_NAME;
-    case ::i18n::addressinput::RECIPIENT:
-      return NAME_FULL;
-  }
-}
 
 void GetAddressComponents(
     const std::string& country_code,
@@ -145,7 +123,7 @@ std::u16string GetEnvelopeStyleAddress(const AutofillProfile& profile,
         component.field == ::i18n::addressinput::RECIPIENT) {
       continue;
     }
-    ServerFieldType type = AddressFieldToServerFieldType(component.field);
+    ServerFieldType type = i18n::TypeForField(component.field);
     if (type == NAME_FULL)
       type = NAME_FULL_WITH_HONORIFIC_PREFIX;
     address += base::UTF16ToUTF8(profile.GetInfo(type, ui_language_code));
