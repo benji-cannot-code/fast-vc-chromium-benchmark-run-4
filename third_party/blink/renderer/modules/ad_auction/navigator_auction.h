602e6a336769ee8fdeb1a0b6e84810cca6ed816c
Qingxin Wu <qingxinwu@google.com>
2022-05-18 04:32:15 +0000

    Fix a wrong bad message.
    Changed JoinInterestGroupForDocument to LeaveInterestGroupForDocument in
    the latter function's bad message.
    
    Change-Id: Ifd52600c6b645e8d9896658e912a560c67a16235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652441
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004606}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.h
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.h
@@ -58,7 +58,7 @@ class MODULES_EXPORT NavigatorAuction final
                                             Navigator&,
                                             const AuctionAdInterestGroup*,
                                             ExceptionState&);
-  // implicit leaveAdInterestGroup - only supported when called from within
+  // Implicit leaveAdInterestGroup - only supported when called from within
   // a fenced frame showing FLEDGE ads.
   ScriptPromise leaveAdInterestGroupForDocument(ScriptState*, ExceptionState&);
   static ScriptPromise leaveAdInterestGroup(ScriptState*,
