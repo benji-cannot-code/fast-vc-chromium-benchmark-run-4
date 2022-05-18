c2f1cbc8eb4cbbe16b6230df3ab6ec39046224c1
danakj <danakj@chromium.org>
2022-05-18 15:28:10 +0000

    Add the most minimal possible ContentBrowserTest in Rust.
    The test is just an empty body so far. But it sets up the instructure
    to inherit from content::ContentBrowserTest for the Gtest test suite,
    and pass the Rust type for it to the test body.
    
    We shouldn't mix public and private content APIs in a single crate as
    that would allow public-API consumers to also use private APIs, unless
    we introduce some way to restrict that.
    
    So for now, we have created separate "rust_public_foo" GN targets
    for //content/public/test things. This is discussed in detail in
    https://crbug.com/1325336, and we will need to revisit how to structure
    public vs private things between Rust and C++ based on the outcome
    there.
    
    R=adetaylor@chromium.org, creis@chromium.org
    
    Bug: 1305396, 1325336
    Change-Id: Ic5df4b0ed5313d53e7e441f3800c0d89ee1c5f43
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,linux-rust-x64-dbg,android-rust-arm-rel,android-rust-arm-dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645146
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004768}

---
+++ content/public/test/content_public_browsertest_support.rs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Crate root for the content_public_browsertest_support crate.
+//!
+//! This file lists the set of source files included in the crate and defines what modules and
+//! things are made public to users of the crate.
+
+#[path = "content_browser_test.rs"]
+mod content_browser_test;
+
+pub use content_browser_test::ContentBrowserTest;
---
+++ content/test/content_private_test_support.rs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! The content-internal test support library.
+
+/// The `#[extern_browsertest_suite("cpp::Type")]` macro, which is used to implement  the
+/// `rust_gtest_interop::TestSuite` trait for Rust wrapper types of C++ subclasses of
+/// BrowserTestBase.
+///
+/// TODO(danakj): This will need to move to //content/public/ in order to support subclasses
+/// of BrowserTestBase outside of content, such as InProcessBrowserTest in //chrome.
+pub use browser_test_macro::extern_browsertest_suite;
