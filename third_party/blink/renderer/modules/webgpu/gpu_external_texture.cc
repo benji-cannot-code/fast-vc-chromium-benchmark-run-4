261b8871fa8e857675cf1dc66d50edbc9300e05c
Shaobo <shaobo.yan@intel.com>
2022-05-18 00:31:54 +0000

    Reland "WebGPU 0-copy path for video uploading on mac"
    The original patch was reverted due to a test failure on
    M1 mac (crbug.com/1325358).
    
    The original patch enables 0-copy path on mac platform and trigger
    a dawn internal shader color space conversion issue (crbug.com/1325002).
    
    brandon1.jones@intel.com has landed a CL which disable all of these tests
    (https://chromium-review.googlesource.com/c/chromium/src/+/3632067).
    
    Creating this reland CL and ensure that if these tests are enabled again, 0-copy path on mac platform will be covered.
    
    Original:
    https://chromium-review.googlesource.com/c/chromium/src/+/3479376
    
    Revert:
    https://chromium-review.googlesource.com/c/chromium/src/+/3647446
    
    Bug: 1307194, 1323647, 1325358
    Change-Id: Iea2093eeecb21a50a328b2f5a03740572a344642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651693
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004544}

--- third_party/blink/renderer/modules/webgpu/gpu_external_texture.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_external_texture.cc
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_conversions.h"
+#include "third_party/blink/renderer/modules/webgpu/gpu_adapter.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_device.h"
+#include "third_party/blink/renderer/modules/webgpu/gpu_supported_features.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_texture.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_texture_view.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
@@ -174,8 +176,13 @@ GPUExternalTexture* GPUExternalTexture::Create(
 
   // TODO(crbug.com/1306753): Use SharedImageProducer and CompositeSharedImage
   // rather than check 'is_webgpu_compatible'.
+  bool device_support_zero_copy =
+      device->adapter()->features()->FeatureNameSet().Contains(
+          "multi-planar-formats");
+
   if (media_video_frame->HasTextures() &&
       (media_video_frame->format() == media::PIXEL_FORMAT_NV12) &&
+      device_support_zero_copy &&
       media_video_frame->metadata().is_webgpu_compatible) {
     scoped_refptr<WebGPUMailboxTexture> mailbox_texture =
         WebGPUMailboxTexture::FromVideoFrame(
