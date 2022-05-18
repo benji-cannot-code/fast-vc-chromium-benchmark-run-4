fa76957f09987a4babfb42862735b3f3c3cc403f
Harkiran Bolaria <hbolaria@google.com>
2022-05-18 12:07:18 +0000

    [MPArch] Disable frame name updates from non-current RenderFrameHost.
    This is to handle the case where a name-changing message arrives after
    the navigation completes (i.e. is updating a RenderFrameHost that is
    in the back-forward cache or is a pending delete RenderFrameHost), when
    navigating away from the document.
    
    See comments on https://chromium-review.googlesource.com/c/chromium/src/+/3320291/comments/c6ea43c6_1f377830
    for additional context.
    
    Bug: 1270671
    Change-Id: I82574e41b73c742ceab51d11220be2fbda6d852d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3574576
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004709}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -283,6 +283,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_view_host_delegate_view.h"
 #endif
 
+namespace features {
+const base::Feature kDisableFrameNameUpdateOnNonCurrentRenderFrameHost{
+    "DisableFrameNameUpdateOnNonCurrentRenderFrameHost",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+}
+
 namespace content {
 
 #if defined(AX_FAIL_FAST_BUILD)
@@ -5523,10 +5529,17 @@ void RenderFrameHostImpl::DidAccessInitialMainDocument() {
   frame_tree_->DidAccessInitialMainDocument();
 }
 
-// TODO(crbug.com/1270671): Avoid duplicating name when RenderFrameHostImpl is
-// not current in its FrameTreeNode.
 void RenderFrameHostImpl::DidChangeName(const std::string& name,
                                         const std::string& unique_name) {
+  // Frame name updates used to occur in the FrameTreeNode; however, as they
+  // now occur in RenderFrameHostImpl (and by extension, BrowsingContextState),
+  // ensure that invalid updates (i.e. when in the BackForwardCache or in a
+  // pending deletion state) are not applied.
+  if ((IsInBackForwardCache() || IsPendingDeletion()) &&
+      base::FeatureList::IsEnabled(
+          features::kDisableFrameNameUpdateOnNonCurrentRenderFrameHost)) {
+    return;
+  }
   if (GetParent() != nullptr) {
     // TODO(lukasza): Call ReceivedBadMessage when |unique_name| is empty.
     DCHECK(!unique_name.empty());
