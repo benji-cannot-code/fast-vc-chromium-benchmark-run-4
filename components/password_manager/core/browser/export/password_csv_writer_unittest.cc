94a0e19062489bb0a590325749d3df565e92432a
Andrii Natiahlyi <natiahlyi@google.com>
2022-05-19 07:23:32 +0000

    Refactor CSVPassword
    This CL aims to simplify usage of CSVPassword class by storing
    credential fields as properties which are populed at the construction. This way one extra run of "parsing" is avoided.
    
    This CL should not introduce any behavioral changes.
    
    Bug: 1325290
    Fixed: 1025510
    Change-Id: Ic0fa0fb84c764f948a0ec073fd3d8bf926ba812d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647364
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005142}

--- components/password_manager/core/browser/export/password_csv_writer_unittest.cc
+++ components/password_manager/core/browser/export/password_csv_writer_unittest.cc
@@ -52,7 +52,7 @@ TEST(PasswordCSVWriterTest, SerializePasswords_SinglePassword) {
 
   std::vector<PasswordForm> pwds;
   for (const auto& pwd : seq) {
-    pwds.push_back(pwd.ParseValid());
+    pwds.push_back(pwd.ToPasswordForm());
   }
   EXPECT_THAT(pwds, ElementsAre(FormHasOriginUsernamePassword(
                         "http://example.com/", "Someone", "Secret")));
@@ -75,7 +75,7 @@ TEST(PasswordCSVWriterTest, SerializePasswords_TwoPasswords) {
 
   std::vector<PasswordForm> pwds;
   for (const auto& pwd : seq) {
-    pwds.push_back(pwd.ParseValid());
+    pwds.push_back(pwd.ToPasswordForm());
   }
   EXPECT_THAT(pwds, ElementsAre(FormHasOriginUsernamePassword(
                                     "http://example.com/", "Someone", "Secret"),
