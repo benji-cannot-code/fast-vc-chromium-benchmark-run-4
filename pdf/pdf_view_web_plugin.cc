7b7ab33034edaf1befc94ee6a0a9bd9610e1fe04
K. Moon <kmoon@chromium.org>
2022-05-19 23:31:53 +0000

    Migrate PdfViewPluginBase::LoadUrl()
    Migrates PdfViewPluginBase::LoadUrl() to PdfViewWebPlugin. A callback
    replaces the `is_print_preview` parameter; resetting
    `last_progress_sent_` is now handled by the caller.
    
    Bug: 1322928
    Change-Id: Id05012c07c89458ac6398ed3c18339bf5fbc67a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645902
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005465}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/bind.h"
 #include "base/check_op.h"
 #include "base/debug/crash_logging.h"
 #include "base/i18n/char_iterator.h"
@@ -644,6 +645,19 @@ std::string PdfViewWebPlugin::Prompt(const std::string& question,
       ->Prompt(blink::WebString::FromUTF8(question),
                blink::WebString::FromUTF8(default_answer))
       .Utf8();
+}
+
+void PdfViewWebPlugin::LoadUrl(base::StringPiece url,
+                               LoadUrlCallback callback) {
+  UrlRequest request;
+  request.url = std::string(url);
+  request.method = "GET";
+  request.ignore_redirects = true;
+
+  std::unique_ptr<UrlLoader> loader = CreateUrlLoaderInternal();
+  UrlLoader* raw_loader = loader.get();
+  raw_loader->Open(request,
+                   base::BindOnce(std::move(callback), std::move(loader)));
 }
 
 void PdfViewWebPlugin::SubmitForm(const std::string& url,
