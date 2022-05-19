e31359bb7f93e8af8fac3cc6586b3d09dbb49a99
Samuel Huang <huangs@chromium.org>
2022-05-19 23:48:52 +0000

    [File Browser Handler] Extract FileSelector{Factory}{Impl} to separate files.
    For Lacros support for chrome.fileBrowserHandler.selectFile(), we wish
    to port existing code to Lacros. Most of this code is found in
    file_browser_handler_api.ash.{cc,h}. An initial refactoring step is to
    split the file.
    
    This CL extracts
    * FileSelector
    * FileSelectorImpl
    * FileSelectorFactory
    * FileSelectorFactoryImpl
    from file_browser_handler_api.ash.{cc,h} to file_selector_{impl}.{cc,h},
    and applies various cleanup:
    * Remove FileSelector's dependency on its caller
      FileBrowserHandlerInternalSelectFileFunctionAsh (used to keep the
      instance alive and to send final results).
      * Have the class take a OnceCallback instead.
      * The caller binds itself (via scoped_refptr) to the callback, per
        common practice among extension function handlers.
    * Streamline comments.
    * Update FileBrowserHandlerExtensionTest (in browser_tests).
    
    Bug: 1275654
    Change-Id: If742ab8c7049c80fffb0fcdc5008b8d390915f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653324
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005477}

--- chrome/browser/extensions/api/file_manager/file_browser_handler_api_ash_test.cc
+++ chrome/browser/extensions/api/file_manager/file_browser_handler_api_ash_test.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
+#include "chrome/browser/extensions/api/file_manager/file_selector.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
@@ -96,7 +97,7 @@ class MockFileSelector : public file_manager::FileSelector {
       const base::FilePath& suggested_name,
       const std::vector<std::string>& allowed_extensions,
       Browser* browser,
-      FileBrowserHandlerInternalSelectFileFunctionAsh* function) override {
+      file_manager::FileSelector::OnSelectedCallback callback) override {
     // Confirm that the function suggested us the right name.
     EXPECT_EQ(suggested_name_, suggested_name);
     // Confirm that the function allowed us the right extensions.
@@ -111,9 +112,7 @@ class MockFileSelector : public file_manager::FileSelector {
     // The callback will take a reference to the function and keep it alive.
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE,
-        base::BindOnce(&FileBrowserHandlerInternalSelectFileFunctionAsh::
-                           OnFilePathSelected,
-                       function, success_, selected_path_));
+        base::BindOnce(std::move(callback), success_, selected_path_));
     delete this;
   }
 
