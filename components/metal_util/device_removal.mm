e2ff67e4768b02dfd1d9b94047834e13c9340f8e
Avi Drissman <avi@chromium.org>
2022-05-19 21:26:47 +0000

    Remove OS X 10.11 and macOS 10.12 code from components/
    Bug: 1322548
    Change-Id: I31c079ab21283ee8a05326d236d3edd2fe86ad7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654916
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005394}

--- components/metal_util/device_removal.mm
+++ components/metal_util/device_removal.mm
@@ -12,23 +12,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace metal {
 
 void RegisterGracefulExitOnDeviceRemoval() {
-  if (@available(macOS 10.13, *)) {
-    id<NSObject> deviceObserver = nil;
-    MTLCopyAllDevicesWithObserver(
-        &deviceObserver,
-        ^(id<MTLDevice> device, MTLDeviceNotificationName name) {
-          if (name == MTLDeviceRemovalRequestedNotification ||
-              name == MTLDeviceWasRemovedNotification) {
-            // Exit the GPU process without error. The browser process sees
-            // this error code as a graceful shutdown, so relaunches the GPU
-            // process without incrementing the crash count.
-            //
-            // Note this wouldn't work nicely with in-process-gpu (it would
-            // exit the browser), but we don't support that on macOS anyway.
-            base::Process::TerminateCurrentProcessImmediately(0);
-          }
-        });
-  }
+  id<NSObject> deviceObserver = nil;
+  MTLCopyAllDevicesWithObserver(
+      &deviceObserver, ^(id<MTLDevice> device, MTLDeviceNotificationName name) {
+        if (name == MTLDeviceRemovalRequestedNotification ||
+            name == MTLDeviceWasRemovedNotification) {
+          // Exit the GPU process without error. The browser process sees
+          // this error code as a graceful shutdown, so relaunches the GPU
+          // process without incrementing the crash count.
+          //
+          // Note this wouldn't work nicely with in-process-gpu (it would
+          // exit the browser), but we don't support that on macOS anyway.
+          base::Process::TerminateCurrentProcessImmediately(0);
+        }
+      });
 }
 
 }  // namespace metal
