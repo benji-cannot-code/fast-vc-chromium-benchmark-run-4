c39c30b18490df0e097d1ae7bb10a4a30b5dea15
Peter Kvitek <kvitekp@chromium.org>
2022-05-19 04:29:58 +0000

    [headless] Mac: added toggle fullscreen cycle tests.
    Switching to and from fullscreen mode may use code path different
    from the one used to start the browser in fullscreen mode. This CL
    adds tests to cover those code paths in native headless mode.
    
    Change-Id: I9eadc1e0ca9c818956e1e92e66fd6a9699531660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654654
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005110}

--- chrome/browser/headless/headless_mode_browsertest_mac.mm
+++ chrome/browser/headless/headless_mode_browsertest_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -27,5 +28,28 @@ IN_PROC_BROWSER_TEST_P(HeadlessModeBrowserTestWithStartWindowMode,
   gfx::NativeWindow native_window = browser()->window()->GetNativeWindow();
   NSWindow* ns_window = native_window.GetNativeNSWindow();
 
+  EXPECT_FALSE(ns_window.visible);
+}
+
+IN_PROC_BROWSER_TEST_F(HeadlessModeBrowserTest,
+                       ToggleFullscreenWindowVisibility) {
+  gfx::NativeWindow native_window = browser()->window()->GetNativeWindow();
+  NSWindow* ns_window = native_window.GetNativeNSWindow();
+
+  // Verify initial state.
+  ASSERT_FALSE(browser()->window()->IsFullscreen());
+  EXPECT_TRUE(browser()->window()->IsVisible());
+  EXPECT_FALSE(ns_window.visible);
+
+  // Verify fullscreen state.
+  chrome::ToggleFullscreenMode(browser());
+  ASSERT_TRUE(browser()->window()->IsFullscreen());
+  EXPECT_TRUE(browser()->window()->IsVisible());
+  EXPECT_FALSE(ns_window.visible);
+
+  // Verify back to normal state.
+  chrome::ToggleFullscreenMode(browser());
+  ASSERT_FALSE(browser()->window()->IsFullscreen());
+  EXPECT_TRUE(browser()->window()->IsVisible());
   EXPECT_FALSE(ns_window.visible);
 }
