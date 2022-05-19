e54b1045b75611fe6dce78b404db983c31b05437
Marek Behún <kabel@kernel.org>
2022-05-19 23:01:30 +0000

    chrome/browser: Fix build with ozone_platform_wayland=false
    Commit a6aa58c64be1 ("Remove
    LinuxUI::NotifyWindowManagerStartupComplete") added usage of
    base::Environment even if BUILDFLAG(OZONE_PLATFORM_WAYLAND) is disabled,
    but left including of base/environment.h in the
    BUILDFLAG(OZONE_PLATFORM_WAYLAND) guard.
    
    Move the include out of the guard.
    
    R=sky@chromium.org, thomasanderson@chromium.org
    
    Bug: 1327367
    Change-Id: I1921f35b898a145ea8014dd51bce815413ce9288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655952
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005434}

--- chrome/browser/chrome_browser_main_extra_parts_linux.cc
+++ chrome/browser/chrome_browser_main_extra_parts_linux.cc
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_browser_main_extra_parts_linux.h"
 
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "ui/ozone/buildflags.h"
 #include "ui/ozone/public/ozone_switches.h"
 
 #if BUILDFLAG(OZONE_PLATFORM_WAYLAND)
-#include "base/environment.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/nix/xdg_util.h"
