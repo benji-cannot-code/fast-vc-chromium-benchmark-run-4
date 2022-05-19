1bd4fbe61a705040b2d1a9496e03cc0952151138
Mike Pinkerton <pinkerton@google.com>
2022-05-19 17:31:37 +0000

    Add BUILDFLAG for Catalyst.
    Prefer BUILDFLAG(IS_IOS_MACCATALYST) to checking TARGET_OS_MACCATALYST.
    Also remove an instance in the web controller where the ifdef is no
    longer needed because of proper SDK support in newer Xcode versions.
    
    Bug: 1326989
    Change-Id: I02e43c0a8746ecf474635c32a3d5b8306b5167af
    No-Presubmit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3649339
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005306}

--- build/build_config.h
+++ build/build_config.h
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //  Operating System:
 //    IS_AIX / IS_ANDROID / IS_ASMJS / IS_CHROMEOS / IS_FREEBSD / IS_FUCHSIA /
-//    IS_IOS / IS_LINUX / IS_MAC / IS_NACL / IS_NETBSD / IS_OPENBSD / IS_QNX /
-//    IS_SOLARIS / IS_WIN
+//    IS_IOS / IS_IOS_MACCATALYST / IS_LINUX / IS_MAC / IS_NACL / IS_NETBSD /
+//    IS_OPENBSD / IS_QNX / IS_SOLARIS / IS_WIN
 //  Operating System family:
-//    IS_APPLE: IOS or MAC
+//    IS_APPLE: IOS or MAC or IOS_MACCATALYST
 //    IS_BSD: FREEBSD or NETBSD or OPENBSD
 //    IS_POSIX: AIX or ANDROID or ASMJS or CHROMEOS or FREEBSD or IOS or LINUX
 //              or MAC or NACL or NETBSD or OPENBSD or QNX or SOLARIS
@@ -65,6 +65,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <TargetConditionals.h>
 #if defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
 #define OS_IOS 1
+// Catalyst is the technology that allows running iOS apps on macOS. These
+// builds are both OS_IOS and OS_IOS_MACCATALYST.
+#if defined(TARGET_OS_MACCATALYST) && TARGET_OS_MACCATALYST
+#define OS_IOS_MACCATALYST
+#endif  // defined(TARGET_OS_MACCATALYST) && TARGET_OS_MACCATALYST
 #else
 #define OS_MAC 1
 #endif  // defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
@@ -179,6 +184,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BUILDFLAG_INTERNAL_IS_IOS() (1)
 #else
 #define BUILDFLAG_INTERNAL_IS_IOS() (0)
+#endif
+
+#if defined(OS_IOS_MACCATALYST)
+#define BUILDFLAG_INTERNAL_IS_IOS_MACCATALYST() (1)
+#else
+#define BUILDFLAG_INTERNAL_IS_IOS_MACCATALYST() (0)
 #endif
 
 #if defined(OS_LINUX)
--- ios/chrome/app/startup/setup_debugging.mm
+++ ios/chrome/app/startup/setup_debugging.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/logging.h"
 #include "base/strings/sys_string_conversions.h"
+#include "build/build_config.h"
 #include "components/crash/core/common/objc_zombie.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -80,7 +81,7 @@ void SwizzleUIImageImageNamed() {
         ![imageName containsString:@".FAUXBUNDLEID."]) {
 // TODO(crbug.com/1325334): Temporarily turn off DCHECK while bootstrapping
 // Catalyst. Log the error to the console instead.
-#if TARGET_OS_MACCATALYST
+#if BUILDFLAG(IS_IOS_MACCATALYST)
       DLOG(ERROR) << "Missing image: " << base::SysNSStringToUTF8(imageName);
 #else
       DCHECK(image) << "Missing image: " << base::SysNSStringToUTF8(imageName);
