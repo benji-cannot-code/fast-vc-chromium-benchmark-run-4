7c113e69f251d4277b28f7539f3af17a0bdf5633
manukh <manukh@chromium.org>
2022-05-19 14:19:10 +0000

    [memories] Hide action chip for navigation intents.
    Adds a feature param `omnibox_action_on_navigation_intents`, default
    enabled (current behavior). When disabled, action chips are not added,
    neither to search nor navigation suggestions, when the user is likely to
    perform a navigation, i.e. when the top scoring suggestion is a
    navigation (even if it doesn't keyword match) and is scored higher than
    a threshold, configured by the feature param
    `omnibox_action_navigation_intent_score_threshold`.
    
    These params are associated with the `kOmniboxAction` feature and a
    chrome://flag variation is added to its existing flag.
    
    Bug: 1325022
    Change-Id: Ia5c4daaa54b88fc68d63dde04d2502315e9d2edb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651213
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005229}

--- components/history_clusters/core/history_clusters_service_unittest.cc
+++ components/history_clusters/core/history_clusters_service_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/history/core/browser/history_context.h"
+#include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/url_row.h"
 #include "components/history/core/test/history_service_test_util.h"
