4f59d95a658bf1cf87d02cda0368523afbaa9d1f
Avi Drissman <avi@chromium.org>
2022-05-19 23:16:34 +0000

    Remove OS X 10.11 and macOS 10.12 code from net/
    Bug: 1322548, 671889
    Change-Id: I784c681408056418b1b3cfa98899c542bafaee31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653819
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005445}

--- net/dns/notify_watcher_mac.cc
+++ net/dns/notify_watcher_mac.cc
@@ -14,31 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-namespace {
-
-// Registers a dummy file descriptor to workaround a bug in libnotify
-// in macOS 10.12
-// See https://bugs.chromium.org/p/chromium/issues/detail?id=783148.
-class NotifyFileDescriptorsGlobalsHolder {
- public:
-  NotifyFileDescriptorsGlobalsHolder() {
-    int notify_fd = -1;
-    int notify_token = -1;
-    notify_register_file_descriptor("notify_file_descriptor_holder", &notify_fd,
-                                    0, &notify_token);
-  }
-};
-
-void HoldNotifyFileDescriptorsGlobals() {
-  if (base::mac::IsAtMostOS10_12()) {
-    static NotifyFileDescriptorsGlobalsHolder holder;
-  }
-}
-}  // namespace
-
-NotifyWatcherMac::NotifyWatcherMac() : notify_fd_(-1), notify_token_(-1) {
-  HoldNotifyFileDescriptorsGlobals();
-}
+NotifyWatcherMac::NotifyWatcherMac() : notify_fd_(-1), notify_token_(-1) {}
 
 NotifyWatcherMac::~NotifyWatcherMac() {
   Cancel();
