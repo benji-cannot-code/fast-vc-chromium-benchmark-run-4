cd3225196e9693f2e61411149da05a4733f3edef
amehfooz <amehfooz@chromium.org>
2022-05-19 23:18:19 +0000

    Fix grouped notifications swipe behavior in collapsed state
    Bug: 1326724
    Change-Id: I71b3585e3c88b169c251c9bc7204b136fc2bdf14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655232
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005446}

--- ash/system/message_center/ash_notification_view.cc
+++ ash/system/message_center/ash_notification_view.cc
@@ -668,6 +668,7 @@ void AshNotificationView::AddGroupNotification(
       std::make_unique<AshNotificationView>(notification,
                                             /*shown_in_popup=*/false);
   notification_view->SetGroupedChildExpanded(IsExpanded());
+  notification_view->set_parent_message_view(this);
   notification_view->set_scroller(
       scroller() ? scroller() : grouped_notifications_scroll_view_);
 
@@ -697,6 +698,7 @@ void AshNotificationView::PopulateGroupNotifications(
             message_center_style::kMaxGroupedNotificationsInCollapsedState ||
         IsExpanded());
     notification_view->SetGroupedChildExpanded(IsExpanded());
+    notification_view->set_parent_message_view(this);
     notification_view->set_scroller(
         scroller() ? scroller() : grouped_notifications_scroll_view_);
 
