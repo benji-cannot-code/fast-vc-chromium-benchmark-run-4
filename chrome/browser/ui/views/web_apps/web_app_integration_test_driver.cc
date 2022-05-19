cb740eb6a4235fa14669e602d96b4717195a0e33
Eric Willigers <ericwilligers@chromium.org>
2022-05-19 02:56:34 +0000

    Move ConvertOsLoginMode() out of WebAppPublisherHelper
    We DCHECK that WebAppPublisherHelper pointer members provider_ and delegate_ are not null.
    
    WebAppPublisherHelper is no longer instantiated in web_app_integration_test_driver.cc
    
    Existing WebAppPublisherHelper code dereferences provider_ and delegate_
    without checking for null. This code is now safe.
    
    Bug: 1326606
    Change-Id: I9adb6e303e53d7a1493a6c54d1cef0c35b3e9af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653355
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005089}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/test/web_app_test_observers.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
+#include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_icon_generator.h"
 #include "chrome/browser/web_applications/web_app_id.h"
@@ -2086,9 +2087,6 @@ WebAppIntegrationTestDriver::ConstructStateSnapshot() {
     WebAppRegistrar& registrar = GetProviderForProfile(profile)->registrar();
     auto app_ids = registrar.GetAppIds();
     base::flat_map<AppId, AppState> app_state;
-    WebAppPublisherHelper web_app_publisher_helper(profile, provider(),
-                                                   apps::AppType::kWeb,
-                                                   /*delegate=*/nullptr, true);
     for (const auto& app_id : app_ids) {
       std::string manifest_launcher_icon_filename;
       std::vector<apps::IconInfo> icon_infos =
@@ -2102,8 +2100,7 @@ WebAppIntegrationTestDriver::ConstructStateSnapshot() {
       auto state = AppState(
           app_id, registrar.GetAppShortName(app_id),
           registrar.GetAppScope(app_id),
-          web_app_publisher_helper.ConvertOsLoginMode(
-              registrar.GetAppRunOnOsLoginMode(app_id).value),
+          ConvertOsLoginMode(registrar.GetAppRunOnOsLoginMode(app_id).value),
           registrar.GetAppEffectiveDisplayMode(app_id),
           registrar.GetAppUserDisplayMode(app_id),
           manifest_launcher_icon_filename, registrar.IsLocallyInstalled(app_id),
--- chrome/browser/web_applications/web_app_constants.h
+++ chrome/browser/web_applications/web_app_constants.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chrome/browser/web_applications/user_display_mode.h"
+#include "components/services/app_service/public/cpp/run_on_os_login_types.h"
 #include "components/services/app_service/public/mojom/types.mojom-forward.h"
 #include "third_party/blink/public/common/manifest/manifest.h"
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
@@ -197,6 +198,10 @@ enum class RunOnOsLoginPolicy {
 };
 
 std::string RunOnOsLoginModeToString(RunOnOsLoginMode mode);
+
+// Converts RunOnOsLoginMode from web_app::RunOnOsLoginMode to
+// apps::RunOnOsLoginMode.
+apps::RunOnOsLoginMode ConvertOsLoginMode(web_app::RunOnOsLoginMode login_mode);
 
 // Number of times IPH can be ignored for this app before it's muted.
 constexpr int kIphMuteAfterConsecutiveAppSpecificIgnores = 3;
