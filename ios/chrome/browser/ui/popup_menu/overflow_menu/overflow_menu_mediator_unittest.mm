f282c772b68dd9885ed74ace0cfbeec824c861cc
Benjamin Williams <bwwilliams@google.com>
2022-05-19 20:05:13 +0000

    Create DestinationUsageHistory.
    This CL does the following:
    
    [1] Creates DestinationUsageHistory class
    with methods that write and read dummy data to/from Chrome local prefs.
    
    [2] Renames OverflowMenuMediator.prefService to
    OverflowMenuMediator.browserStatePrefs, and introduces
    OverflowMenuMediator.localStatePrefs.
    
    Bug: 1322519
    Change-Id: Ie73b5bee13752fc0b88cabf3ce7da6fcb6b04f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631170
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005365}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator_unittest.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator_unittest.mm
@@ -331,7 +331,7 @@ TEST_F(OverflowMenuMediatorTest, TestReadLaterDisabled) {
   mediator_.webStateList = browser_->GetWebStateList();
   mediator_.webContentAreaOverlayPresenter = OverlayPresenter::FromBrowser(
       browser_.get(), OverlayModality::kWebContentArea);
-  mediator_.prefService = prefs_.get();
+  mediator_.browserStatePrefs = prefs_.get();
 
   // Force creation of the model.
   [mediator_ overflowMenuModel];
@@ -436,7 +436,7 @@ TEST_F(OverflowMenuMediatorTest, TestBookmarksToolsMenuButtons) {
   bookmarks::AddIfNotBookmarked(bookmark_model_, bookmarkedURL,
                                 base::SysNSStringToUTF16(@"Test bookmark"));
   mediator_.webStateList = browser_->GetWebStateList();
-  mediator_.prefService = prefs_.get();
+  mediator_.browserStatePrefs = prefs_.get();
 
   // Force creation of the model.
   [mediator_ overflowMenuModel];
@@ -466,7 +466,7 @@ TEST_F(OverflowMenuMediatorTest, TestDisableBookmarksButton) {
   CreateMediator(/*is_incognito=*/NO);
   CreatePrefs();
   mediator_.webStateList = browser_->GetWebStateList();
-  mediator_.prefService = prefs_.get();
+  mediator_.browserStatePrefs = prefs_.get();
 
   // Force creation of the model.
   [mediator_ overflowMenuModel];
