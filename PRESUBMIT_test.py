1a1f7d3a91b846e8bb161e99455d79139db1b381
Daniel Cheng <dcheng@chromium.org>
2022-05-19 02:23:44 +0000

    Relax missing security reviewer warning to a notify at upload.
    Originally, this was selected to match the warning level of other
    security owners checks. However:
    
    - The other security owners check has been upgraded into an error at all
      times, to prevent less last-minute surprises from missing OWNERS.
    - Having a warning that prompts at upload time is annoying when
      repeatedly uploading CLs for something that's still heavily in
      revision.
    
    Bug: 801315
    Change-Id: I7fb2ebc3e1f491fb4274ddb2a966fa041c419e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655008
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005079}

--- PRESUBMIT_test.py
+++ PRESUBMIT_test.py
@@ -2351,7 +2351,7 @@ class IpcSecurityOwnerTest(_SecurityOwnersTestCase):
     # TODO(dcheng): This should be 1, but the PRESUBMIT currently opens the
     # OWNERS file in an unmockable way.
     self.assertEqual(2, len(results))
-    self.assertEqual('warning', results[0].type)
+    self.assertEqual('notify', results[0].type)
     self.assertEqual(
         'Found missing security reviewers:', results[0].message)
 
@@ -2449,7 +2449,7 @@ class IpcSecurityOwnerTest(_SecurityOwnersTestCase):
           if is_committing:
             self.assertEqual('error', results[0].type)
           else:
-            self.assertEqual('warning', results[0].type)
+            self.assertEqual('notify', results[0].type)
           self.assertEqual(
               'Found missing security reviewers:', results[0].message)
           self.assertEqual('error', results[1].type)
