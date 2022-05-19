2632a686e2e2296b8c3099d3034425c400259ef1
Xiaocheng Hu <xiaochengh@chromium.org>
2022-05-19 23:19:33 +0000

    [renderblocking] Remove `blocking=render` from preload and modulepreload links
    ... following the discussion in https://github.com/whatwg/html/issues/7896
    
    Bug: 1271296
    Change-Id: I2ac45f4697810350170d217a7b8d16880e6b89a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648301
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005447}

--- third_party/blink/renderer/core/loader/pending_link_preload.cc
+++ third_party/blink/renderer/core/loader/pending_link_preload.cc
@@ -67,8 +67,6 @@ void PendingLinkPreload::AddResource(Resource* resource) {
 
 // https://html.spec.whatwg.org/C/#link-type-modulepreload
 void PendingLinkPreload::NotifyModuleLoadFinished(ModuleScript* module) {
-  // Step 13. "Unblock rendering on element."
-  UnblockRendering();
   if (loader_)
     loader_->NotifyModuleLoadFinished(module);
   document_->RemovePendingLinkHeaderPreloadIfNeeded(*this);
