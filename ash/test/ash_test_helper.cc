c1e42b874da7626ef1a860fcb31103a600b4fabb
Yuta Hijikata <ythjkt@chromium.org>
2022-05-19 08:22:19 +0000

    Revert "capture_selfie_cam: Enable feature by default."
    This reverts commit a934f578048c3bfe29e41e7b4e411f438c18b84c.
    
    Reason for revert: Ash restarting causes hangs.
    
    Bug: 1325697
    
    Original change's description:
    > capture_selfie_cam: Enable feature by default.
    >
    > This CL enabled the selfie cam feature of Capture
    > Mode by default in preparation for launch in M-103.
    > Also adds some code needed to pass all tests when
    > this feature is enabled.
    >
    > Fixed: 1324256
    > Test: Existing tests.
    > Change-Id: Ifaf96ac296b5c79f13000671a3e1dbd6185f3067
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640287
    > Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1002186}
    
    Change-Id: I8404028c0ca9017fff05a1ec6a460fa24ed834e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653402
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1005156}

--- ash/test/ash_test_helper.cc
+++ ash/test/ash_test_helper.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/run_loop.h"
 #include "base/system/sys_info.h"
-#include "base/system/system_monitor.h"
 #include "chromeos/ash/components/dbus/rgbkbd/rgbkbd_client.h"
 #include "chromeos/dbus/audio/cras_audio_client.h"
 #include "chromeos/dbus/power/power_policy_controller.h"
@@ -93,8 +92,7 @@ class AshTestHelper::PowerPolicyControllerInitializer {
 };
 
 AshTestHelper::AshTestHelper(ui::ContextFactory* context_factory)
-    : AuraTestHelper(context_factory),
-      system_monitor_(std::make_unique<base::SystemMonitor>()) {
+    : AuraTestHelper(context_factory) {
   views::ViewsTestHelperAura::SetFallbackTestViewsDelegateFactory(
       &MakeTestViewsDelegate);
 
