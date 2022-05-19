68c0f4c1c6b1e2e36a968dda23bdedfa7681aab6
Mike Wasserman <msw@chromium.org>
2022-05-19 17:26:10 +0000

    Window Placement: Refine screen label comments
    Following up on feedback in https://crrev.com/c/3646632
    Also reorder an include.
    
    Bug: 1254885
    Change-Id: Ie875c682b60e9d54c2331477aa61a2d912f8b431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651733
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005297}

--- ui/display/mac/screen_mac.mm
+++ ui/display/mac/screen_mac.mm
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/strings/sys_string_conversions.h"
 #include "ui/display/screen.h"
 
 #import <AppKit/AppKit.h>
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/mac/sdk_forward_declarations.h"
+#include "base/strings/sys_string_conversions.h"
 #include "base/timer/timer.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/display/display.h"
