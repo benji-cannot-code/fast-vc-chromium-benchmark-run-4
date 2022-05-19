4ec4736b14dd9fb61cc0db89b5b4330d2cf73baa
YuhanYang <yyhyyh@google.com>
2022-05-19 21:43:14 +0000

    Shimless: Finalize strings on for kWPDisableComplete page
    Remove wpDisableSkippedText string no longer needed in the spec doc
    Update wpDisableReassembleNowText and wpDisableLeaveDisassembledText
    Add screenshots for the updated strings above
    go/wp-disable-complete-resemble-now
    go/wp-disable-complete-leave-disassembled
    
    Bug: 1198187
    Test: testing/xvfb.py ./out/Default/browser_tests --run-manual --gtest_filter=ShimlessRMABrowserTest.MANUAL_OnboardingWpDisableCompletePageTest
    Change-Id: I03b758d7d1f16423e7a841798bc81fd959715e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3631076
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005404}

--- chrome/test/data/webui/chromeos/shimless_rma/onboarding_wp_disable_complete_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/onboarding_wp_disable_complete_page_test.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
 import {FakeShimlessRmaService} from 'chrome://shimless-rma/fake_shimless_rma_service.js';
 import {setShimlessRmaServiceForTesting} from 'chrome://shimless-rma/mojo_interface_provider.js';
@@ -64,7 +65,7 @@ export function onboardingWpDisableCompletePageTest() {
         component.shadowRoot.querySelector('#writeProtectAction');
 
     assertEquals(
-        'Write protection disable complete, you can reassemble the device.',
+        loadTimeData.getString('wpDisableReassembleNowText'),
         actionComponent.textContent.trim());
   });
 
