6be299f8f616f5bc43e3c47eeee22e3344f4f647
Thiabaud Engelbrecht <thiabaud@google.com>
2022-05-19 17:18:26 +0000

    [partition_alloc] Clean up ThreadCache tests
    This follows up https://chromium-review.googlesource.com/c/chromium/src/+/3572219 by making sure the thread cache tests don't run out of space in the GigaCage in the future.
    
    Change-Id: I83bcc5ac61d92cbe8e68dd5c7346365807ab5f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597455
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005283}

--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -638,7 +638,7 @@ void PartitionRoot<thread_safe>::DestructForTesting() {
   while (curr != nullptr) {
     auto* next = curr->next;
     internal::AddressPoolManager::GetInstance().UnreserveAndDecommit(
-        pool_handle, reinterpret_cast<uintptr_t>(curr),
+        pool_handle, SuperPagesBeginFromExtent(curr),
         internal::kSuperPageSize * curr->number_of_consecutive_super_pages);
     curr = next;
   }
@@ -1239,6 +1239,7 @@ void PartitionRoot<thread_safe>::DumpStats(const char* partition_name,
   dumper->PartitionDumpTotals(partition_name, &stats);
 }
 
+// static
 template <bool thread_safe>
 void PartitionRoot<thread_safe>::DeleteForTesting(
     PartitionRoot* partition_root) {
@@ -1246,6 +1247,8 @@ void PartitionRoot<thread_safe>::DeleteForTesting(
     ThreadCache::SwapForTesting(nullptr);
     partition_root->flags.with_thread_cache = false;
   }
+
+  partition_root->DestructForTesting();  // IN-TEST
 
   delete partition_root;
 }
