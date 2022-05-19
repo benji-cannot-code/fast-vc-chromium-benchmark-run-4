f7cd551b9b5c008c69f810ea116769f320afa8ac
amraboelkher <amraboelkher@chromium.org>
2022-05-19 17:21:44 +0000

    PSM: PSM API interface that uses DMServer
    This interface is needed in order to create a Fake implementation
    of the PSM API that uses DMServer. Then, that will allow faking
    and not re-testing the PSM API while testing out PSM use case (
    i.e. during testing PSM for initial enrollment under
    auto_enrollment_client_impl, we don't need the original impl
    of PSM API).
    
    This CL is doing the following:
    1) Renaming PsmRlweDmserverClient* into PsmRlweDmserverClientImpl*
    2) Creating interface PsmRlweDmserverClient with necessary virtual functions
    3) Making PsmRlweDmserverClientImpl inherit from PsmRlweDmserverClient
    4) Changing the PsmRlweDmserverClientTest into PsmRlweDmserverClientImplTest
    5) Reflecting all these file names in the corresponding BUILD.gn
    
    Bug: 1303532, 1143634
    Change-Id: I263d590dbcc7abc3a4c65fdddf343374b0a3d048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647704
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005289}

--- chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl.cc
+++ chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client.h"
+#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl.h"
 
 #include <memory>
 #include <string>
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ash/policy/enrollment/private_membership/private_membership_rlwe_client.h"
+#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client.h"
 #include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_id_provider.h"
 #include "chrome/common/pref_names.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
@@ -33,7 +34,7 @@ namespace em = enterprise_management;
 
 namespace policy {
 
-PsmRlweDmserverClient::PsmRlweDmserverClient(
+PsmRlweDmserverClientImpl::PsmRlweDmserverClientImpl(
     DeviceManagementService* device_management_service,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
     PrivateMembershipRlweClient::Factory* psm_rlwe_client_factory,
@@ -66,11 +67,11 @@ PsmRlweDmserverClient::PsmRlweDmserverClient(
   psm_rlwe_client_ = std::move(status_or_client).value();
 }
 
-PsmRlweDmserverClient::~PsmRlweDmserverClient() {
+PsmRlweDmserverClientImpl::~PsmRlweDmserverClientImpl() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 }
 
-void PsmRlweDmserverClient::CheckMembership(CompletionCallback callback) {
+void PsmRlweDmserverClientImpl::CheckMembership(CompletionCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(callback);
 
@@ -93,17 +94,17 @@ void PsmRlweDmserverClient::CheckMembership(CompletionCallback callback) {
   // Start the protocol and its timeout timer.
   psm_timeout_.Start(
       FROM_HERE, kPsmTimeout,
-      base::BindOnce(&PsmRlweDmserverClient::StoreErrorAndStop,
+      base::BindOnce(&PsmRlweDmserverClientImpl::StoreErrorAndStop,
                      base::Unretained(this), PsmResult::kTimeout));
   SendPsmRlweOprfRequest();
 }
 
-bool PsmRlweDmserverClient::IsCheckMembershipInProgress() const {
+bool PsmRlweDmserverClientImpl::IsCheckMembershipInProgress() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return psm_request_job_ != nullptr;
 }
 
-void PsmRlweDmserverClient::StoreErrorAndStop(PsmResult psm_result) {
+void PsmRlweDmserverClientImpl::StoreErrorAndStop(PsmResult psm_result) {
   // Note that kUMAPsmResult histogram is only using initial enrollment as a
   // suffix until PSM support FRE.
   base::UmaHistogramEnumeration(kUMAPsmResult + uma_suffix_, psm_result);
@@ -118,7 +119,7 @@ void PsmRlweDmserverClient::StoreErrorAndStop(PsmResult psm_result) {
   std::move(on_completion_callback_).Run(last_psm_execution_result_.value());
 }
 
-void PsmRlweDmserverClient::SendPsmRlweOprfRequest() {
+void PsmRlweDmserverClientImpl::SendPsmRlweOprfRequest() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // Create RLWE OPRF request.
@@ -138,9 +139,9 @@ void PsmRlweDmserverClient::SendPsmRlweOprfRequest() {
   // The passed callback will not be called if |psm_request_job_| is
   // destroyed, so it's safe to use base::Unretained.
   std::unique_ptr<DMServerJobConfiguration> config =
-      CreatePsmRequestJobConfiguration(
-          base::BindOnce(&PsmRlweDmserverClient::OnRlweOprfRequestCompletion,
-                         base::Unretained(this)));
+      CreatePsmRequestJobConfiguration(base::BindOnce(
+          &PsmRlweDmserverClientImpl::OnRlweOprfRequestCompletion,
+          base::Unretained(this)));
 
   em::DeviceManagementRequest* request = config->request();
   em::PrivateSetMembershipRlweRequest* psm_rlwe_request =
@@ -150,7 +151,7 @@ void PsmRlweDmserverClient::SendPsmRlweOprfRequest() {
   psm_request_job_ = device_management_service_->CreateJob(std::move(config));
 }
 
-void PsmRlweDmserverClient::OnRlweOprfRequestCompletion(
+void PsmRlweDmserverClientImpl::OnRlweOprfRequestCompletion(
     DeviceManagementService::Job* job,
     DeviceManagementStatus status,
     int net_error,
@@ -191,7 +192,7 @@ void PsmRlweDmserverClient::OnRlweOprfRequestCompletion(
   }
 }
 
-void PsmRlweDmserverClient::SendPsmRlweQueryRequest(
+void PsmRlweDmserverClientImpl::SendPsmRlweQueryRequest(
     const em::PrivateSetMembershipResponse& psm_response) {
   // Extract the oprf_response from |psm_response|.
   const psm_rlwe::PrivateMembershipRlweOprfResponse oprf_response =
@@ -214,9 +215,9 @@ void PsmRlweDmserverClient::SendPsmRlweQueryRequest(
 
   // Prepare the RLWE query request job.
   std::unique_ptr<DMServerJobConfiguration> config =
-      CreatePsmRequestJobConfiguration(
-          base::BindOnce(&PsmRlweDmserverClient::OnRlweQueryRequestCompletion,
-                         base::Unretained(this), oprf_response));
+      CreatePsmRequestJobConfiguration(base::BindOnce(
+          &PsmRlweDmserverClientImpl::OnRlweQueryRequestCompletion,
+          base::Unretained(this), oprf_response));
 
   em::DeviceManagementRequest* request = config->request();
   em::PrivateSetMembershipRlweRequest* psm_rlwe_request =
@@ -226,7 +227,7 @@ void PsmRlweDmserverClient::SendPsmRlweQueryRequest(
   psm_request_job_ = device_management_service_->CreateJob(std::move(config));
 }
 
-void PsmRlweDmserverClient::OnRlweQueryRequestCompletion(
+void PsmRlweDmserverClientImpl::OnRlweQueryRequestCompletion(
     const psm_rlwe::PrivateMembershipRlweOprfResponse& oprf_response,
     DeviceManagementService::Job* job,
     DeviceManagementStatus status,
@@ -335,7 +336,7 @@ void PsmRlweDmserverClient::OnRlweQueryRequestCompletion(
 }
 
 std::unique_ptr<DMServerJobConfiguration>
-PsmRlweDmserverClient::CreatePsmRequestJobConfiguration(
+PsmRlweDmserverClientImpl::CreatePsmRequestJobConfiguration(
     DMServerJobConfiguration::Callback callback) {
   return std::make_unique<DMServerJobConfiguration>(
       device_management_service_,
@@ -346,7 +347,7 @@ PsmRlweDmserverClient::CreatePsmRequestJobConfiguration(
       /*oauth_token=*/absl::nullopt, url_loader_factory_, std::move(callback));
 }
 
-void PsmRlweDmserverClient::RecordPsmSuccessTimeHistogram() {
+void PsmRlweDmserverClientImpl::RecordPsmSuccessTimeHistogram() {
   // These values determine bucketing of the histogram, they should not be
   // changed.
   static const base::TimeDelta kMin = base::Milliseconds(1);
--- chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl_unittest.cc
+++ chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client.h"
+#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client_impl.h"
 
 #include <memory>
 #include <string>
@@ -89,9 +89,9 @@ bool ParseProtoFromFile(const base::FilePath& file_path,
 }  // namespace
 
 // The integer parameter represents the index of PSM test case.
-class PsmRlweDmserverClientTest : public testing::TestWithParam<int> {
+class PsmRlweDmserverClientImplTest : public testing::TestWithParam<int> {
  protected:
-  PsmRlweDmserverClientTest() {
+  PsmRlweDmserverClientImplTest() {
     // Create PSM test case, before PSM client to construct the
     // PSM RLWE testing client factory and its RLWE ID.
     CreatePsmTestCase();
@@ -100,7 +100,7 @@ class PsmRlweDmserverClientTest : public testing::TestWithParam<int> {
     CreateClient();
   }
 
-  ~PsmRlweDmserverClientTest() = default;
+  ~PsmRlweDmserverClientImplTest() = default;
 
   int GetPsmTestCaseIndex() const { return GetParam(); }
 
@@ -113,7 +113,7 @@ class PsmRlweDmserverClientTest : public testing::TestWithParam<int> {
         base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
             &url_loader_factory_);
 
-    psm_client_ = std::make_unique<PsmRlweDmserverClient>(
+    psm_client_ = std::make_unique<PsmRlweDmserverClientImpl>(
         service_.get(), shared_url_loader_factory_,
         psm_rlwe_test_client_factory_.get(),
         testing_psm_rlwe_id_provider_.get());
@@ -267,9 +267,9 @@ class PsmRlweDmserverClientTest : public testing::TestWithParam<int> {
   }
 
   // Disallow copy constructor and assignment operator.
-  PsmRlweDmserverClientTest(const PsmRlweDmserverClientTest&) = delete;
-  PsmRlweDmserverClientTest& operator=(const PsmRlweDmserverClientTest&) =
-      delete;
+  PsmRlweDmserverClientImplTest(const PsmRlweDmserverClientImplTest&) = delete;
+  PsmRlweDmserverClientImplTest& operator=(
+      const PsmRlweDmserverClientImplTest&) = delete;
 
   content::BrowserTaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
@@ -322,7 +322,7 @@ class PsmRlweDmserverClientTest : public testing::TestWithParam<int> {
       kUMASuffixInitialEnrollment;
 };
 
-TEST_P(PsmRlweDmserverClientTest, MembershipRetrievedSuccessfully) {
+TEST_P(PsmRlweDmserverClientImplTest, MembershipRetrievedSuccessfully) {
   InSequence sequence;
 
   const bool kExpectedMembershipResult = GetExpectedMembershipResult();
@@ -350,7 +350,7 @@ TEST_P(PsmRlweDmserverClientTest, MembershipRetrievedSuccessfully) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, EmptyRlweQueryResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, EmptyRlweQueryResponse) {
   InSequence sequence;
   ServerWillReplyWithPsmOprfResponse();
   ServerWillReplyWithEmptyPsmResponse();
@@ -367,7 +367,7 @@ TEST_P(PsmRlweDmserverClientTest, EmptyRlweQueryResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, EmptyRlweOprfResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, EmptyRlweOprfResponse) {
   InSequence sequence;
   ServerWillReplyWithEmptyPsmResponse();
 
@@ -383,7 +383,7 @@ TEST_P(PsmRlweDmserverClientTest, EmptyRlweOprfResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, ConnectionErrorForRlweQueryResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, ConnectionErrorForRlweQueryResponse) {
   InSequence sequence;
   ServerWillReplyWithPsmOprfResponse();
   ServerWillFailForPsm(net::ERR_FAILED, DeviceManagementService::kSuccess);
@@ -403,7 +403,7 @@ TEST_P(PsmRlweDmserverClientTest, ConnectionErrorForRlweQueryResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, ConnectionErrorForRlweOprfResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, ConnectionErrorForRlweOprfResponse) {
   InSequence sequence;
   ServerWillFailForPsm(net::ERR_FAILED, DeviceManagementService::kSuccess);
 
@@ -420,7 +420,7 @@ TEST_P(PsmRlweDmserverClientTest, ConnectionErrorForRlweOprfResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, NetworkFailureForRlweOprfResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, NetworkFailureForRlweOprfResponse) {
   InSequence sequence;
   ServerWillFailForPsm(net::OK, net::ERR_CONNECTION_CLOSED);
 
@@ -435,7 +435,7 @@ TEST_P(PsmRlweDmserverClientTest, NetworkFailureForRlweOprfResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-TEST_P(PsmRlweDmserverClientTest, NetworkFailureForRlweQueryResponse) {
+TEST_P(PsmRlweDmserverClientImplTest, NetworkFailureForRlweQueryResponse) {
   InSequence sequence;
   ServerWillReplyWithPsmOprfResponse();
   ServerWillFailForPsm(net::OK, net::ERR_CONNECTION_CLOSED);
@@ -454,8 +454,8 @@ TEST_P(PsmRlweDmserverClientTest, NetworkFailureForRlweQueryResponse) {
   VerifyPsmLastRequestJobType();
 }
 
-INSTANTIATE_TEST_SUITE_P(PsmRlweDmserverClientTest,
-                         PsmRlweDmserverClientTest,
+INSTANTIATE_TEST_SUITE_P(PsmRlweDmserverClientImplTest,
+                         PsmRlweDmserverClientImplTest,
                          // Loop over all indices starting from 0, and smaller
                          // than `kNumberOfPsmTestCases`.
                          ::testing::Range(0, kNumberOfPsmTestCases));
