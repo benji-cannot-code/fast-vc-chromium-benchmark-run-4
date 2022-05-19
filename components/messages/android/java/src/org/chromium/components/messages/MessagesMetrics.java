252bca61026ded07211094b161b575ed97186466
Hazem Ashmawy <hazems@chromium.org>
2022-05-19 16:19:25 +0000

    Revert "Message for blocked external navigation"
    This reverts commit eabf09f81ee1b3627b153762fa2584e44d649743.
    
    Reason for revert: Test failures in crbug.com/1327269
    
    Original change's description:
    > Message for blocked external navigation
    >
    > In some cases where External Navigation is blocked it makes sense to
    > prompt the user to ask if they would like to leave Chrome. The most
    > common case for this is login forms that take more than 5s to complete
    > using an XHR or similar. Other cases like bookmarks that redirect to an
    > app or similar, which we don't want to have automatically leave Chrome,
    > can also ask the user if they would like to leave Chrome.
    >
    > These cases should be extremely rare, so the Message should very rarely
    > be shown, but in cases where it does get shown it gives the user an
    > escape hatch for what would otherwise be a broken experience.
    >
    > Bug: 1320502
    > Change-Id: I7d3f1bc1ab7e49364cd375156c5ee31f7494e768
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606516
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1004832}
    
    Bug: 1320502, 1327269
    Change-Id: I8aabc8f1120eb708d6da95342435be520c85f292
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655444
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Hazem Ashmawy <hazems@chromium.org>
    Owners-Override: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005271}

--- components/messages/android/java/src/org/chromium/components/messages/MessagesMetrics.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessagesMetrics.java
@@ -142,8 +142,6 @@ public class MessagesMetrics {
                 return "Translate";
             case MessageIdentifier.OFFER_NOTIFICATION:
                 return "OfferNotification";
-            case MessageIdentifier.EXTERNAL_NAVIGATION:
-                return "ExternalNavigation";
             default:
                 return "Unknown";
         }
