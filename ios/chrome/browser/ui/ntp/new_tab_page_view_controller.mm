3f6821f7e4c54f222279e7dfed0ab2be90ed6333
adamta <adamta@google.com>
2022-05-19 17:28:27 +0000

    [iOS] [WebChannels] Improve feed header for landscape mode
    Landscape NTP does not stick fake omnibox to top, instead it uses the
    real omnibox. This was only being handled for iPads but should be
    handled for all non-compact sizes.
    
    After rotating NTP, the fake omnibox position needs to be recalculated
    in addition to the feed insets. We replace the updateFeedInsets call
    for updateNTPLayout, which updates the insets but also takes care of the
    omnibox.
    
    Bug: 1277974
    Change-Id: If29d0abbadee4856aee431b5637e95f0252ef5eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640709
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005301}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -245,11 +245,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // caclculate the new adjustedContentSuggestionsHeight value.
     // TODO(crbug.com/1170995): Remove once the Feed supports a custom
     // header.
-    [[self contentSuggestionsViewController].view setNeedsLayout];
-    [[self contentSuggestionsViewController].view layoutIfNeeded];
+    [[weakSelf contentSuggestionsViewController].view setNeedsLayout];
+    [[weakSelf contentSuggestionsViewController].view layoutIfNeeded];
 
     CGFloat heightAboveFeedDifference =
-        [self heightAboveFeed] - heightAboveFeedBeforeRotation;
+        [weakSelf heightAboveFeed] - heightAboveFeedBeforeRotation;
 
     // Rotating the device can change the content suggestions height. This
     // ensures that it is adjusted if necessary.
@@ -271,15 +271,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [weakSelf scrollPosition] < pinnedOffsetY) {
       weakSelf.collectionView.contentOffset = CGPointMake(0, pinnedOffsetY);
     }
-    if (!self.isFeedVisible) {
-      [self setMinimumHeight];
+    if (!weakSelf.isFeedVisible) {
+      [weakSelf setMinimumHeight];
     }
   };
   [coordinator
       animateAlongsideTransition:alongsideBlock
                       completion:^(
                           id<UIViewControllerTransitionCoordinatorContext>) {
-                        [self updateFeedInsetsForContentAbove];
+                        [self updateNTPLayout];
                       }];
 }
 
@@ -776,12 +776,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // On iPhones, the fake omnibox is pinned to the top so we anchor the feed
   // header to the bottom of it. The fake omnibox is not pinned for iPads, so we
   // instead anchor the feed header to the top of the NTP.
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+  if (IsCompactWidth(self)) {
     self.feedHeaderConstraints = @[
       [self.feedHeaderViewController.view.topAnchor
-          constraintEqualToAnchor:self.view.topAnchor
-                         constant:-[self.feedHeaderViewController
-                                          customSearchEngineViewHeight]],
+          constraintEqualToAnchor:self.headerController.view.bottomAnchor
+                         constant:-(content_suggestions::headerBottomPadding() +
+                                    [self.feedHeaderViewController
+                                            customSearchEngineViewHeight])],
       [self.collectionView.topAnchor
           constraintEqualToAnchor:[self contentSuggestionsViewController]
                                       .view.bottomAnchor],
@@ -789,10 +790,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     self.feedHeaderConstraints = @[
       [self.feedHeaderViewController.view.topAnchor
-          constraintEqualToAnchor:self.headerController.view.bottomAnchor
-                         constant:-(content_suggestions::headerBottomPadding() +
-                                    [self.feedHeaderViewController
-                                            customSearchEngineViewHeight])],
+          constraintEqualToAnchor:self.view.topAnchor
+                         constant:-[self.feedHeaderViewController
+                                          customSearchEngineViewHeight]],
       [self.collectionView.topAnchor
           constraintEqualToAnchor:[self contentSuggestionsViewController]
                                       .view.bottomAnchor],
