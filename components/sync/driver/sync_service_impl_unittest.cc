95a3445dfac21aae0f31a3d6255812bf4357a98b
Monica Basta <msalama@chromium.org>
2022-05-19 09:08:27 +0000

    [Lacros] Handle DISABLE_SYNC_ON_CLIENT
    This CL revokes sync consent on Lacros on Clear data from the sync
    dashboard.  This matches the behavior on other desktop platforms.
    
    Change-Id: Ic30ad10a414f5534749870b635a9188b1a866d57
    Fixed: 1315542, 1315606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599176
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005171}

--- components/sync/driver/sync_service_impl_unittest.cc
+++ components/sync/driver/sync_service_impl_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "components/policy/core/common/policy_service_impl.h"
 #include "components/prefs/testing_pref_service.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
@@ -258,6 +259,10 @@ class SyncServiceImplTest : public ::testing::Test {
   raw_ptr<SyncClientMock> sync_client_;  // Owned by |service_|.
   // The controllers are owned by |service_|.
   std::map<ModelType, FakeDataTypeController*> controller_map_;
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  base::test::ScopedFeatureList scoped_feature_list{
+      switches::kLacrosNonSyncingProfiles};
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 };
 
 class SyncServiceImplTestWithSyncInvalidationsServiceCreated
@@ -1011,10 +1016,8 @@ TEST_F(SyncServiceImplTest, DisableSyncOnClient) {
   client_cmd.action = DISABLE_SYNC_ON_CLIENT;
   service()->OnActionableError(client_cmd);
 
-#if BUILDFLAG(IS_CHROMEOS)
-  // ChromeOS does not support signout.
-  // TODO(https://crbug.com/1233933): Update this when Lacros profiles support
-  //                                  signed-in-but-not-consented-to-sync state.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // Ash does not support signout.
   EXPECT_TRUE(
       identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
   EXPECT_EQ(
