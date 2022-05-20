6c6fa9463a98dbca2157ff7f4b90bdef7e71249a
Osama Fathy <osamafathy@google.com>
2022-05-20 03:53:45 +0000

    OOBE: Use distinct IDs for webviews using web_view_loader
    The web_view_loader is implemented in a way to allow each webview
    to have a single instance of the web_view_loader.
    This requires that all webviews using the web_view_loader should
    have distinct IDs.
    
    Bug: 1324370
    Change-Id: Ia7dc5eb04c909ec07f0f410b8d1bd9e958ab8ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645426
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005610}

--- chrome/browser/resources/chromeos/login/screens/common/consolidated_consent.js
+++ chrome/browser/resources/chromeos/login/screens/common/consolidated_consent.js
@@ -257,7 +257,7 @@ class ConsolidatedConsent extends ConsolidatedConsentScreenElementBase {
     }
 
     this.isArcTosInitialized_ = true;
-    const webview = this.$.arcTosWebview;
+    const webview = this.$.consolidatedConsentArcTosWebview;
     webview.removeContentScripts(['preProcess']);
 
     var language = this.getCurrentLanguage_();
@@ -300,10 +300,11 @@ class ConsolidatedConsent extends ConsolidatedConsentScreenElementBase {
       this.googleEulaLoading_ = true;
       this.crosEulaLoading_ = true;
       this.loadEulaWebview_(
-          this.$.googleEulaWebview, this.googleEulaUrl_,
+          this.$.consolidatedConsentGoogleEulaWebview, this.googleEulaUrl_,
           false /* clear_anchors */);
       this.loadEulaWebview_(
-          this.$.crosEulaWebview, this.crosEulaUrl_, true /* clear_anchors */);
+          this.$.consolidatedConsentCrosEulaWebview, this.crosEulaUrl_,
+          true /* clear_anchors */);
     }
 
     if (this.shouldShowArcTos_(isTosHidden, isArcEnabled)) {
@@ -327,7 +328,7 @@ class ConsolidatedConsent extends ConsolidatedConsentScreenElementBase {
   }
 
   loadArcTosWebview_(online_tos_url) {
-    const webview = this.$.arcTosWebview;
+    const webview = this.$.consolidatedConsentArcTosWebview;
 
     var loadFailureCallback = () => {
       this.setUIStep(ConsolidatedConsentScreenState.ERROR);
@@ -407,7 +408,7 @@ class ConsolidatedConsent extends ConsolidatedConsentScreenElementBase {
   }
 
   onArcTosContentLoad_() {
-    const webview = this.$.arcTosWebview;
+    const webview = this.$.consolidatedConsentArcTosWebview;
     webview.executeScript({code: 'getPrivacyPolicyLink();'}, (results) => {
       if (results && results.length == 1 && typeof results[0] == 'string') {
         this.loadPrivacyPolicyWebview_(results[0]);
@@ -722,7 +723,7 @@ class ConsolidatedConsent extends ConsolidatedConsentScreenElementBase {
 
     // Enable loading content script 'playstore.js' when fetching ToS from
     // the test server.
-    var termsView = this.$.arcTosWebview;
+    var termsView = this.$.consolidatedConsentArcTosWebview;
     termsView.removeContentScripts(['postProcess']);
     termsView.addContentScripts([{
       name: 'postProcess',
