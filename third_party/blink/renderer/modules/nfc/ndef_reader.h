9e26be10b157c6065b72eaa21b08485bc40e042d
François Beaufort <beaufort.francois@gmail.com>
2022-05-20 04:03:28 +0000

    WebNFC: Use AbortSignal::Algorithm
    This change updates the read(), write(), and makeReadOnly() methods and
    their abort steps attached to the provided AbortSginal so that the
    reason provided to the AbortController is used to reject the Promise
    returned by these methods.
    
    Spec: https://github.com/w3c/web-nfc/pull/631
    
    Bug: 1326358
    Change-Id: I72bda1ca449b5a0da7c4d6b56505f5339a2095e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650615
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1005613}

--- third_party/blink/renderer/modules/nfc/ndef_reader.h
+++ third_party/blink/renderer/modules/nfc/ndef_reader.h
@@ -78,6 +78,10 @@ class MODULES_EXPORT NDEFReader : public EventTargetWithInlineData,
   void MakeReadOnlyOnMojoConnectionError();
 
  private:
+  class ReadAbortAlgorithm;
+  class WriteAbortAlgorithm;
+  class MakeReadOnlyAbortAlgorithm;
+
   // ExecutionContextLifecycleObserver overrides.
   void ContextDestroyed() override;
 
@@ -88,10 +92,12 @@ class MODULES_EXPORT NDEFReader : public EventTargetWithInlineData,
 
   void WriteAbort(AbortSignal* signal);
   void WriteOnRequestCompleted(ScriptPromiseResolver* resolver,
+                               AbortSignal* signal,
                                device::mojom::blink::NDEFErrorPtr error);
 
   void MakeReadOnlyAbort(AbortSignal* signal);
   void MakeReadOnlyOnRequestCompleted(ScriptPromiseResolver* resolver,
+                                      AbortSignal* signal,
                                       device::mojom::blink::NDEFErrorPtr error);
 
   // Read Permission handling
