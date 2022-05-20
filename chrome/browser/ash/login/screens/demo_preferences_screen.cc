40df4091ec5ea69f83086ef58d352ba040f0893e
Geng Li <ligeng@google.com>
2022-05-20 21:08:54 +0000

    [OOBE] Add retailer ID input for demo mode welcome page
    Screenshot: https://screenshot.googleplex.com/9U3rADDCHNP3yD3
    Error input screenshot: https://screenshot.googleplex.com/7DkCyZYPPozc4Vj
    
    Bug: b:231222227
    Change-Id: Ie317c49c125fea02ac36cda83fc46751cbe1efc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3622315
    Commit-Queue: Geng Li <ligeng@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005952}

--- chrome/browser/ash/login/screens/demo_preferences_screen.cc
+++ chrome/browser/ash/login/screens/demo_preferences_screen.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "chrome/browser/ash/login/screens/welcome_screen.h"
+#include "chrome/browser/ash/login/wizard_controller.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/webui/chromeos/login/demo_preferences_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/welcome_screen_handler.h"
@@ -53,6 +54,13 @@ DemoPreferencesScreen::~DemoPreferencesScreen() {
   input_method::InputMethodManager::Get()->RemoveObserver(this);
 }
 
+void DemoPreferencesScreen::SetDemoModeRetailerAndStoreIdInput(
+    const std::string& retailer_store_id_input) {
+  WizardController::default_controller()
+      ->demo_setup_controller()
+      ->set_retailer_store_id_input(retailer_store_id_input);
+}
+
 void DemoPreferencesScreen::ShowImpl() {
   if (view_)
     view_->Show();
@@ -63,11 +71,16 @@ void DemoPreferencesScreen::HideImpl() {}
 void DemoPreferencesScreen::OnUserAction(const base::Value::List& args) {
   const std::string& action_id = args[0].GetString();
   if (action_id == kUserActionContinue) {
+    CHECK_EQ(args.size(), 2);
     std::string country(
         g_browser_process->local_state()->GetString(prefs::kDemoModeCountry));
     if (country == DemoSession::kCountryNotSelectedId) {
       return;
     }
+    // Set retailer store input string regardless of pattern, let server decide
+    // what action take when it is invalid.
+    const std::string& retailer_store_id_input = args[1].GetString();
+    SetDemoModeRetailerAndStoreIdInput(retailer_store_id_input);
     exit_callback_.Run(Result::COMPLETED);
   } else if (action_id == kUserActionClose) {
     exit_callback_.Run(Result::CANCELED);
--- chrome/browser/ash/login/screens/demo_preferences_screen.h
+++ chrome/browser/ash/login/screens/demo_preferences_screen.h
@@ -38,6 +38,8 @@ class DemoPreferencesScreen
   ~DemoPreferencesScreen() override;
 
   void SetDemoModeCountry(const std::string& country_id);
+  void SetDemoModeRetailerAndStoreIdInput(
+      const std::string& retailer_store_id_input);
 
  protected:
   // BaseScreen:
