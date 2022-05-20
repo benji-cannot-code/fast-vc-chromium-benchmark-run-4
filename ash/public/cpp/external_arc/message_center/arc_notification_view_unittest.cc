e41700fdfd7f8f15a0de287a9595f5c637e32318
minch <minch@chromium.org>
2022-05-20 19:18:45 +0000

    dark_mode: Fix tests failure on enabling the feature - part 1
    This cl does not enable the flag `kDarkLightMode` but fixed some tests
    failure before doing that. Including
    - Disabling `kDarkLightMode` feature when `kNotificationsRefresh` is
      disabled, as these two are hooked up.
    - Show dark/light mode user nudge only if active user session is
      started.
    - An capture mode camera selfie test failure because of the window's
      bounds are larger than the display's bounds.
    
    Tests failure info can be found at crrev.com/c/3582620.
    
    Bug: 1326629
    Change-Id: Id32d29b215c43384878d6313b48d07795295f59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656843
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005903}

--- ash/public/cpp/external_arc/message_center/arc_notification_view_unittest.cc
+++ ash/public/cpp/external_arc/message_center/arc_notification_view_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/ime/dummy_text_input_client.h"
 #include "ui/base/ime/input_method.h"
@@ -74,8 +75,17 @@ class ArcNotificationViewTest : public AshTestBase,
   // views::ViewsTestBase
   void SetUp() override {
     scoped_feature_list_ = std::make_unique<base::test::ScopedFeatureList>();
-    scoped_feature_list_->InitWithFeatureState(features::kNotificationsRefresh,
-                                               IsNotificationsRefreshEnabled());
+    if (IsNotificationsRefreshEnabled()) {
+      scoped_feature_list_->InitWithFeatures(
+          /*enabled_features=*/{features::kNotificationsRefresh,
+                                chromeos::features::kDarkLightMode},
+          /*disabled_features=*/{});
+    } else {
+      scoped_feature_list_->InitWithFeatures(
+          /*enabled_features=*/{},
+          /*disabled_features=*/{features::kNotificationsRefresh,
+                                 chromeos::features::kDarkLightMode});
+    }
 
     AshTestBase::SetUp();
 
