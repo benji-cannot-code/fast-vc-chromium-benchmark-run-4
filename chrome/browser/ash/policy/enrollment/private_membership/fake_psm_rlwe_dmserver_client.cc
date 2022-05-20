6ff3357bf0b5554f21277815bd417b64d36fa356
amraboelkher <amraboelkher@chromium.org>
2022-05-20 04:54:21 +0000

    PSM: Fake PSM API
    This CL is adding a fake implementation of the PSM API
    that uses DMServer. That will be useful when testing out
    the initial enrollment using PSM as we don't need to re-test
    that layer again.
    
    Bug: 1303532, 1143634
    Change-Id: If8d0a5df9e4a4b4e1a9be6add0cb2db70997ed0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646294
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005622}

---
+++ chrome/browser/ash/policy/enrollment/private_membership/fake_psm_rlwe_dmserver_client.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/policy/enrollment/private_membership/fake_psm_rlwe_dmserver_client.h"
+
+#include <utility>
+
+#include "base/check.h"
+#include "chrome/browser/ash/policy/enrollment/private_membership/psm_rlwe_dmserver_client.h"
+
+namespace policy {
+
+FakePsmRlweDmserverClient::FakePsmRlweDmserverClient()
+    : result_holder_(PsmResult::kConnectionError) {}
+
+void FakePsmRlweDmserverClient::CheckMembership(CompletionCallback callback) {
+  DCHECK(callback);
+  std::move(callback).Run(result_holder_);
+}
+
+bool FakePsmRlweDmserverClient::IsCheckMembershipInProgress() const {
+  return false;
+}
+
+void FakePsmRlweDmserverClient::WillReplyWith(ResultHolder new_result_holder) {
+  result_holder_ = std::move(new_result_holder);
+}
+
+}  // namespace policy
