33d6f36a4deab6c6e8352ae96f978e3e74afca73
Le Hoang Quyen <lehoangquyen@chromium.org>
2022-05-20 07:13:10 +0000

    [Dr-Dc] Force gpu main thread priority to NORMAL.
    Force GPU main thread priority to NORMAL when DrDc is enabled.
    In that case DrDc thread continues to use DISPLAY thread
    priority and hence have higher thread priority than GPU main.
    
    Bug: 1318994
    Change-Id: I9fd66d9834627cd0c48ce059e2f86a039a525c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630098
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005647}

--- gpu/config/gpu_finch_features.h
+++ gpu/config/gpu_finch_features.h
@@ -68,6 +68,8 @@ GPU_EXPORT extern const base::Feature kReduceOpsTaskSplitting;
 
 GPU_EXPORT extern const base::Feature kEnableDrDc;
 
+GPU_EXPORT extern const base::Feature kForceGpuMainThreadToNormalPriorityDrDc;
+
 #if BUILDFLAG(IS_ANDROID)
 // This flag is use additionally with kEnableDrDc to enable the feature for
 // vulkan enabled android devices.
@@ -78,6 +80,7 @@ GPU_EXPORT extern const base::Feature kWebGPUService;
 
 GPU_EXPORT bool IsUsingVulkan();
 GPU_EXPORT bool IsDrDcEnabled();
+GPU_EXPORT bool IsGpuMainThreadForcedToNormalPriorityDrDc();
 GPU_EXPORT bool NeedThreadSafeAndroidMedia();
 GPU_EXPORT bool IsANGLEValidationEnabled();
 
