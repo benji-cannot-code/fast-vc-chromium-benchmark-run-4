1577275ca8b2e6592edcaa86858f8b5dcc6e5918
Eugene Zemtsov <eugene@chromium.org>
2022-05-20 00:13:43 +0000

    webcodecs: Fix GPU synchronization in RGB to YUV frame readback
    Changing the way media::CopyRGBATextureToVideoFrame() is synchronized
    because waiting for sync token is not enough, it doesn't guarantee
    the GPU buffer is fully ready.
    
    Bug: 1312735, 1321913
    Change-Id: Ie36a206a1a53c730dab71dc0d65a3521151e5be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646254
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005504}

--- third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.cc
+++ third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.h"
 
 #include "components/viz/common/gpu/raster_context_provider.h"
+#include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/context_support.h"
 #include "gpu/command_buffer/client/gpu_memory_buffer_manager.h"
+#include "gpu/command_buffer/client/raster_interface.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
 #include "media/base/video_frame.h"
 #include "media/renderers/video_frame_rgba_to_yuva_converter.h"
@@ -140,22 +142,48 @@ bool WebGraphicsContext3DVideoFramePool::CopyRGBATextureToVideoFrame(
     return false;
 #endif  // BUILDFLAG(IS_WIN)
 
-  scoped_refptr<media::VideoFrame> dst_frame =
-      pool_->MaybeCreateVideoFrame(src_size, dst_color_space);
+  auto dst_frame = pool_->MaybeCreateVideoFrame(src_size, dst_color_space);
   if (!dst_frame)
     return false;
 
-  gpu::SyncToken copy_done_sync_token;
+  auto* ri = raster_context_provider->RasterInterface();
+  DCHECK(ri);
+  unsigned query_id = 0;
+  ri->GenQueriesEXT(1, &query_id);
+  ri->BeginQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM, query_id);
+
   const bool copy_succeeded = media::CopyRGBATextureToVideoFrame(
       raster_context_provider, src_format, src_size, src_color_space,
-      src_surface_origin, src_mailbox_holder, dst_frame.get(),
-      copy_done_sync_token);
+      src_surface_origin, src_mailbox_holder, dst_frame.get());
   if (!copy_succeeded)
     return false;
 
   IgnoreResult(failure_runner.Release());
-  raster_context_provider->ContextSupport()->SignalSyncToken(
-      copy_done_sync_token, base::BindOnce(std::move(callback), dst_frame));
+  auto on_query_done_cb =
+      [](scoped_refptr<media::VideoFrame> frame,
+         base::WeakPtr<blink::WebGraphicsContext3DProviderWrapper> ctx_wrapper,
+         unsigned query_id, FrameReadyCallback callback) {
+        if (ctx_wrapper) {
+          if (auto* ctx_provider = ctx_wrapper->ContextProvider()) {
+            if (auto* ri_provider = ctx_provider->RasterContextProvider()) {
+              auto* ri = ri_provider->RasterInterface();
+              ri->DeleteQueriesEXT(1, &query_id);
+            }
+          }
+        }
+        std::move(callback).Run(std::move(frame));
+      };
+
+  // QueryEXT functions are used to make sure that CopyRGBATextureToVideoFrame()
+  // texture copy before we access GMB data.
+  ri->EndQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM);
+  auto* context_support = raster_context_provider->ContextSupport();
+  DCHECK(context_support);
+  context_support->SignalQuery(
+      query_id,
+      base::BindOnce(on_query_done_cb, dst_frame, weak_context_provider_,
+                     query_id, std::move(callback)));
+
   return true;
 }
 
