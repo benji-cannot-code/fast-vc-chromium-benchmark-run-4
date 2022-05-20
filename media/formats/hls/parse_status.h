60fb85e91e70aac19ac50fb6cd9023dfaf2de376
Will Cassella <cassew@chromium.org>
2022-05-20 18:55:55 +0000

    HLS: Implement ByteRange type
    This CL implements a parser for the "ByteRange" type, which appears in
    the EXT-X-BYTERANGE, EXT-X-MAP, and EXT-X-PART tags. This type is
    defined by an expression consisting of '<length>[@<offset>]'. As
    'offset' is optional, it must be deducible as the end of a previous
    byterange of the same resource, which is outside the scope of this
    parsing function.
    
    Bug: 1266991
    Change-Id: Id0309f94c65d00c0db0b551ebefda1794fa15dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651283
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005896}

--- media/formats/hls/parse_status.h
+++ media/formats/hls/parse_status.h
@@ -21,6 +21,7 @@ enum class ParseStatusCode : StatusCodeType {
   kFailedToParseSignedDecimalFloatingPoint,
   kFailedToParseDecimalResolution,
   kFailedToParseQuotedString,
+  kFailedToParseByteRange,
   kInvalidPlaylistVersion,
   kUnknownPlaylistType,
   kMalformedAttributeList,
