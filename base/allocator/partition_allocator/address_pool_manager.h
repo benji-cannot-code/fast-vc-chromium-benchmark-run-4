61243c30aeafc1d477452c718a7706cd3bbb75bb
Kalvin Lee <kdlee@chromium.org>
2022-05-20 21:52:26 +0000

    [PA] `compiler_specific` migration (3 of N)
    This CL converts the modified files to use PartitionAlloc's internal
    copy of `compiler_specific.h`.
    
    Bug: 1151236
    Change-Id: I1eae702d8fea0343018d724a23fbd549632016de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653892
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005974}

--- base/allocator/partition_allocator/address_pool_manager.h
+++ base/allocator/partition_allocator/address_pool_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/address_pool_manager_bitmap.h"
 #include "base/allocator/partition_allocator/address_pool_manager_types.h"
 #include "base/allocator/partition_allocator/partition_address_space.h"
+#include "base/allocator/partition_allocator/partition_alloc_base/compiler_specific.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
@@ -152,7 +153,7 @@ class BASE_EXPORT AddressPoolManager {
 #endif
   };
 
-  ALWAYS_INLINE Pool* GetPool(pool_handle handle) {
+  PA_ALWAYS_INLINE Pool* GetPool(pool_handle handle) {
     PA_DCHECK(0 < handle && handle <= kNumPools);
     return &pools_[handle - 1];
   }
@@ -170,15 +171,15 @@ class BASE_EXPORT AddressPoolManager {
   friend struct base::LazyInstanceTraitsBase<AddressPoolManager>;
 };
 
-ALWAYS_INLINE pool_handle GetRegularPool() {
+PA_ALWAYS_INLINE pool_handle GetRegularPool() {
   return kRegularPoolHandle;
 }
 
-ALWAYS_INLINE pool_handle GetBRPPool() {
+PA_ALWAYS_INLINE pool_handle GetBRPPool() {
   return kBRPPoolHandle;
 }
 
-ALWAYS_INLINE pool_handle GetConfigurablePool() {
+PA_ALWAYS_INLINE pool_handle GetConfigurablePool() {
   PA_DCHECK(IsConfigurablePoolAvailable());
   return kConfigurablePoolHandle;
 }
