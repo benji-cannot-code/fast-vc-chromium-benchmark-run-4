f2a9a9f2d11a42b5a0acc12727e57a9a7107002c
Ian Wells <iwells@chromium.org>
2022-05-20 03:57:47 +0000

    Feed: log when navigating back from the NTP
    This is one of several CLs for logging metrics about specific reasons users leave the feed before loading is finished. A different change will handle the back button hiding Start Surface.
    
    
    Bug: 1195940, 1326142
    Change-Id: Id79df3a4488e06732b52e0a5fd5641560a4d934a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3618350
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005612}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -58,6 +58,7 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.feed.FeedReliabilityLogger;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -652,6 +653,17 @@ public class NewTabPageTest {
         mRenderTestRule.render(
                 mActivityTestRule.getActivity().getActivityTab().getNativePage().getView(),
                 "feed_is_ablated");
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"NewTabPage", "FeedNewTabPage"})
+    public void testFeedReliabilityLoggingHideWithBack() throws IOException {
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            ChromeTabbedActivity activity = (ChromeTabbedActivity) mActivityTestRule.getActivity();
+            activity.handleBackPressed();
+            verify(mFeedReliabilityLogger).onNavigateBack();
+        });
     }
 
     private void assertThumbnailInvalidAndRecapture() {
--- chrome/android/junit/src/org/chromium/chrome/browser/toolbar/ToolbarTabControllerImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/toolbar/ToolbarTabControllerImplTest.java
@@ -39,6 +39,7 @@ import org.chromium.chrome.browser.profiles.ProfileJni;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.bottom.BottomControlsCoordinator;
+import org.chromium.chrome.browser.ui.native_page.NativePage;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.feature_engagement.EventConstants;
@@ -86,6 +87,8 @@ public class ToolbarTabControllerImplTest {
     public Profile.Natives mMockProfileNatives;
     @Mock
     private ObservableSupplier<TabModelSelector> mTabModelSelectorSupplier;
+    @Mock
+    private NativePage mNativePage;
 
     @Rule
     public TestRule mProcessor = new Features.JUnitProcessor();
@@ -99,6 +102,7 @@ public class ToolbarTabControllerImplTest {
         doReturn(false).when(mOverrideHomePageSupplier).get();
         mocker.mock(ProfileJni.TEST_HOOKS, mMockProfileNatives);
         doReturn(mProfile).when(mMockProfileNatives).fromWebContents(any());
+        doReturn(mNativePage).when(mTab).getNativePage();
         TrackerFactory.setTrackerForTests(mTracker);
         doReturn(new ObservableSupplierImpl<>()).when(mTabModelSelectorSupplier).get();
         mToolbarTabController = new ToolbarTabControllerImpl(mTabSupplier,
@@ -113,6 +117,7 @@ public class ToolbarTabControllerImplTest {
 
         assertFalse(mToolbarTabController.forward());
         assertFalse(mToolbarTabController.back());
+        verify(mNativePage, never()).notifyHidingWithBack();
     }
 
     @Test
@@ -136,6 +141,15 @@ public class ToolbarTabControllerImplTest {
         verify(mBottomControlsCoordinator).onBackPressed();
         verify(mRunnable, never()).run();
         verify(mTab, never()).goBack();
+    }
+
+    @Test
+    public void back_notifyNativePageHiding() {
+        doReturn(null).when(mBottomControlsCoordinatorSupplier).get();
+        doReturn(true).when(mTab).canGoBack();
+
+        mToolbarTabController.back();
+        verify(mNativePage).notifyHidingWithBack();
     }
 
     @Test
