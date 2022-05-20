82907888157ea2d7d04d43db7a1667ba5c73a13d
Ken Rockot <rockot@google.com>
2022-05-20 22:46:47 +0000

    ipcz: Boxes
    Implements Box() and Unbox() APIs.
    
    Also introduces a new "blob" object type within the reference driver
    library, to exercise custom driver object transmission via boxing.
    
    Bug: 1299283
    Change-Id: Ieaf7e39a8c62983b557d3e4a1d5a94248b10d4f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652004
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006004}

--- third_party/ipcz/src/api.cc
+++ third_party/ipcz/src/api.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "api.h"
 #include "ipcz/api_object.h"
+#include "ipcz/box.h"
 #include "ipcz/ipcz.h"
 #include "ipcz/node.h"
 #include "ipcz/node_link_memory.h"
@@ -220,14 +221,37 @@ IpczResult Box(IpczHandle node_handle,
                uint32_t flags,
                const void* options,
                IpczHandle* handle) {
-  return IPCZ_RESULT_UNIMPLEMENTED;
+  ipcz::Node* node = ipcz::Node::FromHandle(node_handle);
+  if (!node || driver_handle == IPCZ_INVALID_DRIVER_HANDLE || !handle) {
+    return IPCZ_RESULT_INVALID_ARGUMENT;
+  }
+
+  auto box = ipcz::MakeRefCounted<ipcz::Box>(
+      ipcz::DriverObject(ipcz::WrapRefCounted(node), driver_handle));
+  *handle = ipcz::Box::ReleaseAsHandle(std::move(box));
+  return IPCZ_RESULT_OK;
 }
 
 IpczResult Unbox(IpczHandle handle,
                  IpczUnboxFlags flags,
                  const void* options,
                  IpczDriverHandle* driver_handle) {
-  return IPCZ_RESULT_UNIMPLEMENTED;
+  if (!driver_handle) {
+    return IPCZ_RESULT_INVALID_ARGUMENT;
+  }
+
+  ipcz::Ref<ipcz::Box> box = ipcz::Box::TakeFromHandle(handle);
+  if (!box) {
+    return IPCZ_RESULT_INVALID_ARGUMENT;
+  }
+
+  if (flags & IPCZ_UNBOX_PEEK) {
+    *driver_handle = box->object().handle();
+    std::ignore = box.release();
+  } else {
+    *driver_handle = box->object().release();
+  }
+  return IPCZ_RESULT_OK;
 }
 
 constexpr IpczAPI kCurrentAPI = {
--- third_party/ipcz/src/ipcz/node_link.cc
+++ third_party/ipcz/src/ipcz/node_link.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <utility>
 
+#include "ipcz/box.h"
 #include "ipcz/ipcz.h"
 #include "ipcz/link_type.h"
 #include "ipcz/message.h"
@@ -150,6 +151,7 @@ bool NodeLink::OnAcceptParcel(msg::AcceptParcel& accept) {
       accept.GetArrayView<HandleType>(accept.params().handle_types);
   absl::Span<const RouterDescriptor> new_routers =
       accept.GetArrayView<RouterDescriptor>(accept.params().new_routers);
+  auto driver_objects = accept.driver_objects();
 
   // Note that on any validation failure below, we defer rejection at least
   // until any deserialized objects are stored in a new Parcel object. This
@@ -175,13 +177,23 @@ bool NodeLink::OnAcceptParcel(msg::AcceptParcel& accept) {
         break;
       }
 
+      case HandleType::kBox: {
+        if (driver_objects.empty()) {
+          return false;
+        }
+
+        objects[i] = MakeRefCounted<Box>(std::move(driver_objects[0]));
+        driver_objects.remove_prefix(1);
+        break;
+      }
+
       default:
         parcel_valid = false;
         break;
     }
   }
 
-  if (!new_routers.empty()) {
+  if (!new_routers.empty() || !driver_objects.empty()) {
     // There should be no unclaimed routers. If there are, it's a validation
     // failure.
     parcel_valid = false;
