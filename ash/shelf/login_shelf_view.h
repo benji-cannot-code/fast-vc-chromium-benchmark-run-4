07777afc2b85812255f64aabba1643ee311f118b
Sherri Lin <sherrilin@google.com>
2022-05-20 18:34:52 +0000

    Reland "enterprise: Update device license mode when device policy data is
    This is a reland of commit 160a7de308f70fc384925d165b96e1f5e61cb5d8
    
    Reason of Revert was that EnterpriseDomainObserver was added more than once.
    
    Now `ScopedObservation` is used instead of calling addObservation
    directly and adding the observer is also moved to the ctor, which ensures
    that it'll happen only once.
    
    Revert CL: crrev.com/c/3655395
    
    Original change's description:
    > enterprise: Update device license mode when device policy data is set
    >
    > Previously, the device license mode is not updated correctly as login
    > shelf view ctor is called before the policy is set.
    >
    > Inherit EnterpriseDomainObserver to update the device enterprise info when available.
    >
    > Bug: 1307303, 1327365
    > Change-Id: Ie5cf63da93401ce5900f0d35f7396d0acf300271
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650732
    > Commit-Queue: Sherri Lin <sherrilin@google.com>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1005422}
    
    Bug: 1307303, 1327365
    Change-Id: I5b072cc76ddb4ca6dafea7407659c3556e8e2031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654231
    Commit-Queue: Sherri Lin <sherrilin@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005887}

--- ash/shelf/login_shelf_view.h
+++ ash/shelf/login_shelf_view.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/kiosk_app_instruction_bubble.h"
 #include "ash/shelf/shelf_shutdown_confirmation_bubble.h"
 #include "ash/shutdown_controller_impl.h"
+#include "ash/system/enterprise/enterprise_domain_observer.h"
+#include "ash/system/model/enterprise_domain_model.h"
 #include "ash/tray_action/tray_action.h"
 #include "ash/tray_action/tray_action_observer.h"
 #include "base/memory/weak_ptr.h"
@@ -48,7 +50,8 @@ class ASH_EXPORT LoginShelfView : public views::View,
                                   public TrayActionObserver,
                                   public LockScreenActionBackgroundObserver,
                                   public ShutdownControllerImpl::Observer,
-                                  public LoginDataDispatcher::Observer {
+                                  public LoginDataDispatcher::Observer,
+                                  public EnterpriseDomainObserver {
  public:
   enum ButtonId {
     kShutdown = 1,          // Shut down the device.
@@ -163,6 +166,10 @@ class ASH_EXPORT LoginShelfView : public views::View,
   void OnUsersChanged(const std::vector<LoginUserInfo>& users) override;
   void OnOobeDialogStateChanged(OobeDialogState state) override;
 
+  // ash::EnterpriseDomainObserver
+  void OnDeviceEnterpriseInfoChanged() override;
+  void OnEnterpriseAccountDomainChanged() override;
+
   // Called when a locale change is detected. Updates the login shelf button
   // strings.
   void HandleLocaleChange();
@@ -247,6 +254,9 @@ class ASH_EXPORT LoginShelfView : public views::View,
 
   base::ScopedObservation<LoginDataDispatcher, LoginDataDispatcher::Observer>
       login_data_dispatcher_observation_{this};
+
+  base::ScopedObservation<EnterpriseDomainModel, EnterpriseDomainObserver>
+      enterprise_domain_model_observation_{this};
 
   // The kiosk app button will only be created for the primary display's login
   // shelf.
