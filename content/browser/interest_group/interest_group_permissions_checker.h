cb61f692c8f0d3b089dc08af67f5ed72f909d1eb
Matt Menke <mmenke@chromium.org>
2022-05-20 19:04:11 +0000

    [FLEDGE] Don't use DataDecoder objects.
    Instead use DataDecoder::ParseJSONIsolated, which doesn't use an object.
    DataDecoder appears to be broken, in that is can invoke callbacks passed
    to it after it is destroyed, which violates its API contract.
    
    InterestGroupPermissionsChecker doesn't actually need it. I had been
    thinking we'd make operations individually cancellable, to better handle
    having too many headless requests on navigate away, which is why I was
    using it in the first place. However, I decided a simpler approach
    would work well enough, so ParseJSONIsolated with a weak pointer works
    just as well, and possibly with lower overhead.
    
    Bug: 1327312
    Change-Id: Ie0c22c214e7a818b21c1faae4ae7dbcb47721a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653090
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005899}

--- content/browser/interest_group/interest_group_permissions_checker.h
+++ content/browser/interest_group/interest_group_permissions_checker.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "net/base/network_isolation_key.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
@@ -134,9 +135,6 @@ class CONTENT_EXPORT InterestGroupPermissionsChecker {
 
     // Used to fetch the .well-known URL.
     std::unique_ptr<network::SimpleURLLoader> simple_url_loader;
-
-    // Used to decode the .well-known URL's response body.
-    data_decoder::DataDecoder data_decoder;
   };
 
   // A map of interest group origins to their ActiveRequests.
@@ -162,6 +160,8 @@ class CONTENT_EXPORT InterestGroupPermissionsChecker {
   static bool AllowsOperation(Permissions permissions, Operation operation);
 
   ActiveRequestMap active_requests_;
+
+  base::WeakPtrFactory<InterestGroupPermissionsChecker> weak_factory_{this};
 };
 
 }  // namespace content
