0fada3690ae1a3416dd1b32ed5b90d829e0950f1
Osama Fathy <osamafathy@google.com>
2022-05-20 04:08:44 +0000

    OOBE ConsolidatedConsent: Remove non-owner strings
    Since crash data is already part of the PerUserMetricConsent
    feature, owner and subsequent users will be shown the same strings.
    
    Bug: 1324893
    Change-Id: I74ce08da065d199e45720a042a76de24b39205f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643877
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005614}

--- chrome/browser/ash/login/screens/consolidated_consent_screen.cc
+++ chrome/browser/ash/login/screens/consolidated_consent_screen.cc
@@ -248,9 +248,10 @@ void ConsolidatedConsentScreen::OnOwnershipStatusCheckDone(
 
   // If the user is not the owner and the owner disabled metrics, the user
   // is not allowed to update the usage opt-in.
-  if (!is_owner_.value_or(false) &&
-      !ash::StatsReportingController::Get()->IsEnabled()) {
-    view_->HideUsageOptin();
+  if (view_) {
+    view_->SetUsageOptinOptinHidden(
+        !is_owner_.value_or(false) &&
+        !ash::StatsReportingController::Get()->IsEnabled());
   }
 
   const bool is_demo = arc::IsArcDemoModeSetupFlow();
@@ -287,9 +288,6 @@ void ConsolidatedConsentScreen::OnOwnershipStatusCheckDone(
 
     UpdateMetricsMode(is_enabled, is_managed);
   }
-
-  if (view_)
-    view_->SetIsDeviceOwner(is_owner_.value());
 }
 
 void ConsolidatedConsentScreen::RecordConsents(
