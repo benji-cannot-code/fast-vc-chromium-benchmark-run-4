3cf7aa71db617c0b37e2f456a741744329940ff5
Wenbo Jie <wenbojie@chromium.org>
2022-05-20 06:16:12 +0000

    [FilesRecentFilter] Support document filter for DriveFS
    Add a new field in drivefs.mojom to support query by multiple mime
    types, then we can use the document mime type list generated by
    file_types.json5 to query DriveFS for all recent documents.
    
    Related google3 CL: http://cl/437673745
    Demo: http://shortn/_tfv3yYPLgt
    
    Bug: 881673
    Change-Id: Iaa1ccbe8d8c0b734c183b3188afe9adf6e7432bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581506
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005641}

--- chrome/browser/chromeos/fileapi/recent_drive_source.cc
+++ chrome/browser/chromeos/fileapi/recent_drive_source.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/fileapi/recent_drive_source.h"
 
+#include <iterator>
 #include <utility>
 #include <vector>
 
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/file_system/file_system_operation_runner.h"
 #include "storage/browser/file_system/file_system_url.h"
 #include "storage/common/file_system/file_system_types.h"
+#include "ui/file_manager/file_types_data.h"
 #include "url/origin.h"
 
 using content::BrowserThread;
@@ -79,6 +81,14 @@ void RecentDriveSource::GetRecentFiles(Params params) {
     case FileType::kVideo:
       query_params->mime_type = kVideoMimeType;
       break;
+    case FileType::kDocument: {
+      std::vector<std::string> doc_mime_types{
+          std::make_move_iterator(file_types_data::kDocumentMIMETypes.begin()),
+          std::make_move_iterator(file_types_data::kDocumentMIMETypes.end()),
+      };
+      query_params->mime_types = std::move(doc_mime_types);
+      break;
+    }
     default:
       // Leave the mime_type null to query all files.
       break;
