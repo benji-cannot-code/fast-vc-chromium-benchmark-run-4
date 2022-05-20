8bf2a9b64bad4b453b60809edc8e0e6dd3de8bba
sisidovski <sisidovski@chromium.org>
2022-05-20 05:21:25 +0000

    Trigger frame loading with a PostIdleTask callback after timeout
    This CL will add the feature to trigger loading on ad iframe when
    AutomaticLazyFrameLoadingToAds feature is enabled. After this CL, the
    loading is triggered after passing some timeout given as a field trial
    param. The loading is called with a PostIdleTask callback, and that is
    triggered even when the intersection observer based lazy loading is not
    started yet. If the loading is already started when the idle task
    callback is called, do nothing.
    
    Change-Id: I77d74b1e71217765e086f584d90594adf81a72d2
    Bug: 1265343, 1323067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650112
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005632}

--- third_party/blink/renderer/core/html/html_frame_owner_element.cc
+++ third_party/blink/renderer/core/html/html_frame_owner_element.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 
 namespace blink {
@@ -277,6 +278,27 @@ bool IsEligibleForLazyEmbeds(KURL url) {
   return false;
 }
 
+// If kAutomaticLazyFrameLoadingToAds is enabled, calculate the timeout in
+// advance from the field trial param, otherwilse return 0;
+base::TimeDelta GetLazyAdsTimeoutMs() {
+  const base::TimeDelta defaultTimeout = base::Milliseconds(0);
+  if (!base::FeatureList::IsEnabled(features::kAutomaticLazyFrameLoadingToAds))
+    return defaultTimeout;
+
+  const String timeout =
+      base::GetFieldTrialParamValueByFeature(
+          features::kAutomaticLazyFrameLoadingToAds, "timeout")
+          .c_str();
+  if (timeout.IsEmpty())
+    return defaultTimeout;
+
+  bool success;
+  const int timeoutMs = timeout.ToInt(&success);
+  DCHECK(success);
+
+  return base::Milliseconds(timeoutMs);
+}
+
 }  // namespace
 
 SubframeLoadingDisabler::SubtreeRootSet&
@@ -306,7 +328,8 @@ HTMLFrameOwnerElement::HTMLFrameOwnerElement(const QualifiedName& tag_name,
       content_frame_(nullptr),
       embedded_content_view_(nullptr),
       should_lazy_load_children_(DoesParentAllowLazyLoadingChildren(document)),
-      is_swapping_frames_(false) {}
+      is_swapping_frames_(false),
+      lazy_ads_timeout_ms_(GetLazyAdsTimeoutMs()) {}
 
 LayoutEmbeddedContent* HTMLFrameOwnerElement::GetLayoutEmbeddedContent() const {
   // HTMLObjectElement and HTMLEmbedElement may return arbitrary layoutObjects
@@ -643,6 +666,10 @@ bool HTMLFrameOwnerElement::LazyLoadIfPossible(
           /*record_uma=*/true)) {
     lazy_load_frame_observer_->DeferLoadUntilNearViewport(request,
                                                           frame_load_type);
+    if (IsAdRelated()) {
+      SetTimeoutToStartAdFrameLoading();
+    }
+
     return true;
   }
   return false;
@@ -837,6 +864,26 @@ bool HTMLFrameOwnerElement::IsAdRelated() const {
     return false;
 
   return content_frame_->IsAdSubframe();
+}
+
+void HTMLFrameOwnerElement::LoadIfLazyOnIdle(base::TimeTicks deadline) {
+  LoadImmediatelyIfLazy();
+}
+
+void HTMLFrameOwnerElement::SetTimeoutToStartAdFrameLoading() {
+  if (!IsAdRelated() || !base::FeatureList::IsEnabled(
+                            features::kAutomaticLazyFrameLoadingToAds)) {
+    return;
+  }
+
+  // TODO(sisidovski) FrameScheduler should have the attribution of this task,
+  // but FrameScheduler doesn't expose
+  // SingleThreadIdleTaskRunner::PostIdleTask. So we call PostIdleTask here
+  // through the main thread scheduler.
+  ThreadScheduler::Current()->PostDelayedIdleTask(
+      FROM_HERE, lazy_ads_timeout_ms_,
+      WTF::Bind(&HTMLFrameOwnerElement::LoadIfLazyOnIdle,
+                WrapWeakPersistent(this)));
 }
 
 mojom::blink::ColorScheme HTMLFrameOwnerElement::GetColorScheme() const {
