578ae294600acc193445c6879635170123b91286
avynn <avynn@google.com>
2022-05-20 19:59:08 +0000

    Desk Template: Add ToDebugString
    Adds the ToDebugString functionality of the Desk Template object
    so that the method will show all of the data associated with said
    object.  This will assist in debugging DeskTemplates.
    
    as it exists only to facilitate debugging. Manual test only.
    
    TESTED: This doesn't affect the main behavior of Desk Templates
    Bug: 1327054
    Change-Id: Ibe70ab32da5f436d4863c6f1709dc05de5d85fef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654423
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1005921}

--- ash/public/cpp/desk_template.cc
+++ ash/public/cpp/desk_template.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/app_types.h"
 #include "ash/constants/ash_features.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/values.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 
@@ -92,6 +93,43 @@ std::string DeskTemplate::ToString() const {
 
   if (desk_restore_data_)
     result += desk_restore_data_->ToString();
+  return result;
+}
+
+std::string DeskTemplate::ToDebugString() const {
+  std::string result =
+      "Template name: " + base::UTF16ToUTF8(template_name_) + "\n";
+  result += "GUID: " + uuid_.AsLowercaseString() + "\n";
+  result += "Source: ";
+  switch (source_) {
+    case DeskTemplateSource::kUnknownSource:
+      result += "unknown\n";
+      break;
+    case DeskTemplateSource::kUser:
+      result += "user\n";
+      break;
+    case DeskTemplateSource::kPolicy:
+      result += "policy\n";
+      break;
+  }
+  result += "Type: ";
+  switch (type_) {
+    case DeskTemplateType::kTemplate:
+      result += "template\n";
+      break;
+    case DeskTemplateType::kSaveAndRecall:
+      result += "save and recall\n";
+      break;
+  }
+  result += "Time created: " + base::TimeFormatHTTP(created_time_) + "\n";
+  result += "Time updated: " + base::TimeFormatHTTP(updated_time_) + "\n";
+  result += "launch id: " + base::NumberToString(launch_id_) + "\n";
+
+  // Converting to value and printing the debug string may be more
+  // intensive but gives more complete information which increases
+  // the utility of this function.
+  if (desk_restore_data_)
+    result += desk_restore_data_->ConvertToValue().DebugString();
   return result;
 }
 
