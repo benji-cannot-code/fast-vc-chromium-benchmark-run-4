67199847b26cdcb2aa9defeb65d280bb43571c68
Phillis Tang <phillis@google.com>
2022-05-20 00:53:04 +0000

    DPWA: Create richer install dialog
    Create the richer install dialog behind feature flag. This is the first
    CL that sets up the basic dialog with title, description. Follow up
    CLs will be created to draw image carousel.
    
    Design doc: go/dpwa-richer-install-design
    screenshot: https://screenshot.googleplex.com/4mkoQhCuRXhZJDU
    
    Change-Id: Id1ff05f9291c9f70825d5df1b59fcff9c50f1b37
    Bug: 1326252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088072
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005545}

--- chrome/browser/ui/web_applications/web_app_dialog_utils.cc
+++ chrome/browser/ui/web_applications/web_app_dialog_utils.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_utils.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
+#include "chrome/common/chrome_features.h"
 #include "components/webapps/browser/banners/app_banner_manager.h"
 #include "components/webapps/browser/installable/installable_metrics.h"
 #include "content/public/browser/navigation_entry.h"
@@ -48,10 +49,18 @@ void OnWebAppInstallShowInstallDialog(
   switch (flow) {
     case WebAppInstallFlow::kInstallSite:
       web_app_info->user_display_mode = UserDisplayMode::kStandalone;
-      chrome::ShowPWAInstallBubble(
-          initiator_web_contents, std::move(web_app_info),
-          std::move(web_app_acceptance_callback), iph_state);
-      return;
+      if (base::FeatureList::IsEnabled(
+              features::kDesktopPWAsDetailedInstallDialog)) {
+        chrome::ShowWebAppDetailedInstallDialog(
+            initiator_web_contents, std::move(web_app_info),
+            std::move(web_app_acceptance_callback), iph_state);
+        return;
+      } else {
+        chrome::ShowPWAInstallBubble(
+            initiator_web_contents, std::move(web_app_info),
+            std::move(web_app_acceptance_callback), iph_state);
+        return;
+      }
     case WebAppInstallFlow::kCreateShortcut:
       chrome::ShowWebAppInstallDialog(initiator_web_contents,
                                       std::move(web_app_info),
