eaeefdf531a62caec0d090c8e352e8d31941179f
Maggie Cai <mxcai@chromium.org>
2022-05-20 04:13:26 +0000

    Revert "enterprise: Update device license mode when device policy data is set"
    This reverts commit 160a7de308f70fc384925d165b96e1f5e61cb5d8.
    
    Reason for revert: This CL is likely the cause of tests failure for ash_unittests in a few test suites. Because this seems to try to add an observer twice according to the error message. Failed run started at https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/28828/test-results
    
    Original change's description:
    > enterprise: Update device license mode when device policy data is set
    >
    > Previously, the device license mode is not updated correctly as login
    > shelf view ctor is called before the policy is set.
    >
    > Inherit EnterpriseDomainObserver to update the device enterprise info when available.
    >
    > Bug: 1307303, 1327365
    > Change-Id: Ie5cf63da93401ce5900f0d35f7396d0acf300271
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650732
    > Commit-Queue: Sherri Lin <sherrilin@google.com>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1005422}
    
    Bug: 1307303, 1327365
    Change-Id: Ia798bcb3dc6cdf5b8d5679d2db12a1e4e085209b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655395
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005617}

--- ash/shelf/login_shelf_view.h
+++ ash/shelf/login_shelf_view.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/kiosk_app_instruction_bubble.h"
 #include "ash/shelf/shelf_shutdown_confirmation_bubble.h"
 #include "ash/shutdown_controller_impl.h"
-#include "ash/system/enterprise/enterprise_domain_observer.h"
 #include "ash/tray_action/tray_action.h"
 #include "ash/tray_action/tray_action_observer.h"
 #include "base/memory/weak_ptr.h"
@@ -49,8 +48,7 @@ class ASH_EXPORT LoginShelfView : public views::View,
                                   public TrayActionObserver,
                                   public LockScreenActionBackgroundObserver,
                                   public ShutdownControllerImpl::Observer,
-                                  public LoginDataDispatcher::Observer,
-                                  public EnterpriseDomainObserver {
+                                  public LoginDataDispatcher::Observer {
  public:
   enum ButtonId {
     kShutdown = 1,          // Shut down the device.
@@ -164,10 +162,6 @@ class ASH_EXPORT LoginShelfView : public views::View,
   // LoginDataDispatcher::Observer:
   void OnUsersChanged(const std::vector<LoginUserInfo>& users) override;
   void OnOobeDialogStateChanged(OobeDialogState state) override;
-
-  // ash::EnterpriseDomainObserver
-  void OnDeviceEnterpriseInfoChanged() override;
-  void OnEnterpriseAccountDomainChanged() override;
 
   // Called when a locale change is detected. Updates the login shelf button
   // strings.
