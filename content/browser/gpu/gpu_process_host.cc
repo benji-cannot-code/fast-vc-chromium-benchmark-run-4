f4ec3fe68b5ad006fb2ba2a7d4fcb292e0a71b63
Jeffrey Kardatzke <jkardatzke@google.com>
2022-05-20 21:15:06 +0000

    media: Remove encrypted requirement for HEVC decoding
    This removes the requirement that HEVC content must be encrypted in
    order to be supported. For ChromeOS, this is still limited to
    platforms that support HW protected content. For Windows, it is limited
    to devices with HW HEVC decode support.
    
    BUG=b:221247444, 1311348
    TEST=Clear and encrypted HEVC play on guybrush
    
    Change-Id: I542191494ebcd29d81302c8ebeb375457a62d2d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558754
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005958}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -276,9 +276,6 @@ static const char* const kSwitchNames[] = {
     switches::kVModule,
     switches::kUseAdapterLuid,
     switches::kWebViewDrawFunctorUsesVulkan,
-#if BUILDFLAG(ENABLE_PLATFORM_HEVC)
-    switches::kEnableClearHevcForTesting,
-#endif
 #if BUILDFLAG(IS_MAC)
     sandbox::policy::switches::kEnableSandboxLogging,
     sandbox::policy::switches::kDisableMetalShaderCache,
@@ -311,7 +308,6 @@ static const char* const kSwitchNames[] = {
     switches::kHardwareVideoDecodeFrameRate,
 #endif
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-    switches::kLacrosEnablePlatformEncryptedHevc,
     switches::kLacrosEnablePlatformHevc,
     switches::kLacrosUseChromeosProtectedMedia,
     switches::kLacrosUseChromeosProtectedAv1,
