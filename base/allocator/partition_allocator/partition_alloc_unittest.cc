a3036ba8f927c769fe54f7266e51ec9528b7008e
Ali Juma <ajuma@chromium.org>
2022-05-20 00:07:44 +0000

    [iOS] Get EG tests working with PartitionAlloc
    This CL uses a dynamically-sized GigaCage to get EG tests working
    with PartitionAlloc. In EG tests, the test process does not have
    any entitlements and therefore cannot use an extended virtual address
    space. This CL detects these test processes by searching for "Runner"
    at the end of the executable path, and in this case uses a small
    pool size. In all other cases, the regular pool size is used.
    
    This CL also removes the existing special-case that always uses
    a small pool on iOS.
    
    Bug: 1250788
    Change-Id: I60d3c3985827d5e75e171c0d67dfbc5793ae3eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550906
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005496}

--- base/allocator/partition_allocator/partition_alloc_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_unittest.cc
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/time.h>
 #endif  // BUILDFLAG(IS_POSIX)
 
-#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && BUILDFLAG(IS_APPLE)
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && BUILDFLAG(IS_MAC)
 #include <OpenCL/opencl.h>
 
 #include <base/mac/mac_util.h>
