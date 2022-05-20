6f7336c896e7927986f4bc131e68b1603ab65507
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-05-20 15:50:22 +0000

    Performance manager policy to discard tabs under HighEfficiencyMode
    Bug: 1308741
    Change-Id: I1121a1646dd1f6fe950c4d17c9321deb2a30d239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3637838
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005812}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.h
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/scoped_multi_source_observation.h"
+#include "build/build_config.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager_observer.h"
@@ -28,6 +29,10 @@ class PageLoadMetricsObserver;
 class PageLoadTrackerDecoratorHelper;
 class PerformanceManagerFeatureObserverClient;
 class PerformanceManagerLifetime;
+
+namespace policies {
+class HighEfficiencyModePolicyHelper;
+}
 }  // namespace performance_manager
 
 // Handles the initialization of the performance manager and a few dependent
@@ -95,6 +100,11 @@ class ChromeBrowserMainExtraPartsPerformanceManager
   // Needed to maintain the PageNode::IsLoading() property.
   std::unique_ptr<performance_manager::PageLoadTrackerDecoratorHelper>
       page_load_tracker_decorator_helper_;
+
+#if !BUILDFLAG(IS_ANDROID)
+  std::unique_ptr<performance_manager::policies::HighEfficiencyModePolicyHelper>
+      high_efficiency_mode_policy_helper_;
+#endif  // !BUILDFLAG(IS_ANDROID)
 };
 
 #endif  // CHROME_BROWSER_PERFORMANCE_MANAGER_CHROME_BROWSER_MAIN_EXTRA_PARTS_PERFORMANCE_MANAGER_H_
