00294cc847ef8b4d5fd611cb167a03ba5a723b57
Alexander Bolodurin <alexbn@chromium.org>
2022-05-20 17:18:59 +0000

    Reland "[lacros dgapi] shared Digital Goods types"
    This is a reland of commit 269ff05a797a02c8fce29ab161f5a10853cb4563
    
    Adding //components/digital_goods/mojom as a dependency to both
    blink_platform and blink_modules components caused linker errors in
    the Windows component build (duplicate symbols).
    
    Since blink_modules already depends indirectly on blink_platform,
    //components/digital_goods/mojom can be removed as a dependency from
    //third_party/blink/renderer/modules/* and only be included to
    //third_party/blink/renderer/platform (via
    //third_party/blink/public/common and //third_party/blink/public/mojom).
    
    Original change's description:
    > [lacros dgapi] shared Digital Goods types
    >
    > Break out shared Digital Goods types from Blink and ARC mojo APIs into a
    > component to reuse in crosapi.
    >
    > Bug: 1203666
    > Change-Id: I50f639df6c9a064473557980e4ba40a622cfccb9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583507
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    > Reviewed-by: Cait Phillips <caitkp@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1002390}
    
    Bug: 1203666
    Change-Id: I125f30bf24fbd49315e39ed00b2d3e34800ccbe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645649
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005855}

--- third_party/blink/renderer/modules/payments/goods/digital_goods_type_converters_unittest.cc
+++ third_party/blink/renderer/modules/payments/goods/digital_goods_type_converters_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/time/time.h"
+#include "components/digital_goods/mojom/digital_goods.mojom-blink.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/digital_goods/digital_goods.mojom-blink.h"
 #include "third_party/blink/public/mojom/digital_goods/digital_goods.mojom-shared.h"
