6a4ecbd19576930b60a844c26099be66fadb820a
Mike Wasserman <msw@chromium.org>
2022-05-20 00:32:53 +0000

    Window Placement: Prototype Fullscreen Capability Delegation - Part 2
    This completes the prototype started in https://crrev.com/c/3575071
    
    Support Fullscreen Capability Delegation when a flag is enabled:
      --enable-blink-features=CapabilityDelegationFullscreenRequest
    Frames can delegate via messaging another frame on activation:
    - w.postMessage(m, { targetOrigin: o, delegate: "fullscreen" });
    Another frame can use the delegated capability on message receipt:
    - window.onmessage = () => { e.requestFullscreen(); }
    
    Activate a browser-side FullscreenRequestToken in:
    - RFHI::PostMessageEvent for RemoteFrameHost's RouteMessageEvent
    - new ReceivedDelegatedCapability for LocalFrameHost local messages
    
    Focus windows that had delegation to request fullscreen, so:
    - user can press [Esc] to exit fullscreen
    - another focused window doesn't z-order above/below fullscreen
    
    Add automated tests supporting feature flag and local/remote usage
    
    Bug: 1293083
    Test: automated; Fullscreen Capability Delegation WAI with flag enabled
    Change-Id: Ie46e627cd8df033016f16afbfda4075da81517b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642842
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005525}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/url_loader_network_service_observer.mojom-forward.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/frame/frame_owner_element_type.h"
+#include "third_party/blink/public/common/frame/fullscreen_request_token.h"
 #include "third_party/blink/public/common/permissions_policy/permissions_policy.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
@@ -2150,6 +2151,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void FrameSizeChanged(const gfx::Size& frame_size) override;
   void DidChangeSrcDoc(const blink::FrameToken& child_frame_token,
                        const std::string& srcdoc_value) override;
+  void ReceivedDelegatedCapability(
+      blink::mojom::DelegatedCapability delegated_capability) override;
 
   // blink::mojom::BackForwardCacheControllerHost:
   void EvictFromBackForwardCache(blink::mojom::RendererEvictionReason) override;
@@ -4228,6 +4231,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
   // Manages a transient affordance for this frame or subframes to open a popup.
   TransientAllowPopup transient_allow_popup_;
+
+  // Manages a transient affordance for this frame to request fullscreen.
+  blink::FullscreenRequestToken fullscreen_request_token_;
 
   // Used to avoid sending AXTreeData to the renderer if the renderer has not
   // been told root ID yet. See UpdateAXTreeData() for more details.
--- third_party/blink/renderer/core/testing/fake_local_frame_host.cc
+++ third_party/blink/renderer/core/testing/fake_local_frame_host.cc
@@ -245,4 +245,7 @@ void FakeLocalFrameHost::DidChangeSrcDoc(
     const blink::FrameToken& child_frame_token,
     const WTF::String& srcdoc_value) {}
 
+void FakeLocalFrameHost::ReceivedDelegatedCapability(
+    blink::mojom::DelegatedCapability delegated_capability) {}
+
 }  // namespace blink
