e6f4c4048555d8f85f9b7197820f50c7fb20e034
Henrique Ferreiro <hferreiro@igalia.com>
2022-05-20 11:37:09 +0000

    Move c/b/ash/arc/* to //chrome/browser/ash
    Move files in chrome/browser/ash/arc/ from //chrome/browser/chromeos to
    //chrome/browser/ash.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: If9eb8a92d65587892096c36a4d0bae69b40398b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650584
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1005729}

--- chrome/browser/ash/arc/process/arc_process_service.h
+++ chrome/browser/ash/arc/process/arc_process_service.h
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/process/arc_process.h"
 #include "chrome/browser/ash/process_snapshot_server.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "services/resource_coordinator/public/cpp/memory_instrumentation/global_memory_dump.h"
-#include "services/resource_coordinator/public/mojom/memory_instrumentation/memory_instrumentation.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace content {
