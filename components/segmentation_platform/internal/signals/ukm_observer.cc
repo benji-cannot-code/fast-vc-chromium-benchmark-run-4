29e482a50229c7c6cc1d2d5ef7e30171eb3c9960
Min Qin <qinmin@chromium.org>
2022-05-21 01:42:13 +0000

    [Segmentation Platform] Fix an issue UKM allowed time pref is updated on startup
    On startup, checking UKM allowed state before profile init will always
    return false. This causes the pref timestamp to be set to Time::Max().
    Later on, observer will report a UkmAllowedStateChange, and that will
    reset the pref value to current time. This causes the platform to think
    that UKM is just enabled, and will not report structured metrics.
    This CL fixes the issue by not checking the initial value. Since
    UkmAllowedStateChange will be called if UKM is allowed, we can just wait
    for that call to update the pref. If the pref is not initialized,
    segmentation will not record any structured metrics.
    
    BUG=1327768
    
    
    validation
    
    Change-Id: Ie31d1626a221dc5557a4ea91cd0cf19e605b89d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653303
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006078}

--- components/segmentation_platform/internal/signals/ukm_observer.cc
+++ components/segmentation_platform/internal/signals/ukm_observer.cc
@@ -16,11 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace segmentation_platform {
 
-UkmObserver::UkmObserver(ukm::UkmRecorderImpl* ukm_recorder,
-                         bool is_ukm_allowed)
+UkmObserver::UkmObserver(ukm::UkmRecorderImpl* ukm_recorder)
     : ukm_recorder_(ukm_recorder), ukm_data_manager_(nullptr) {
   // Listen to |OnUkmAllowedStateChanged| event.
-  OnUkmAllowedStateChanged(is_ukm_allowed);
   ukm_recorder_->AddUkmRecorderObserver(base::flat_set<uint64_t>(), this);
 }
 
