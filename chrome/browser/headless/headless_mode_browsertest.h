654d4c09119b0d4dbf9bbb6f73a0335b4d95836c
Peter Kvitek <kvitekp@chromium.org>
2022-05-21 00:29:36 +0000

    [headless] Win: added toggle fullscreen cycle tests.
    Switching to and from fullscreen mode may use code path different
    from the one used to start the browser in fullscreen mode. This CL
    adds tests to cover those code paths in native headless mode.
    
    Also added requested headless fullscreen window state tracking
    which is necessary for tests to work and improves headless mode
    correctness.
    
    Change-Id: I1dfa390ca3c32c23ae062830f1d0f8224c1ff8e8
    Cq-Include-Trybots: luci.chromium.try:win7-blink-rel,win7-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652647
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006036}

--- chrome/browser/headless/headless_mode_browsertest.h
+++ chrome/browser/headless/headless_mode_browsertest.h
@@ -49,4 +49,7 @@ class HeadlessModeBrowserTestWithStartWindowMode
   StartWindowMode start_window_mode() const { return GetParam(); };
 };
 
+// Toggles browser fullscreen mode synchronously.
+void ToggleFullscreenModeSync(Browser* browser);
+
 #endif  // CHROME_BROWSER_HEADLESS_HEADLESS_MODE_BROWSERTEST_H_
