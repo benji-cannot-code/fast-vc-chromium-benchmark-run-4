6a33cd835f8b09bab1482e2d620ded36ff4f4d08
Alan Zhao <ayzhao@google.com>
2022-05-21 21:28:42 +0000

    Explicitly #include <utility> in os_handle_fuchsia.cc
    std::move(...) will no longer be present in the existing headers with
    the upcoming libc++ roll, so we explicitly #include <utility>.
    
    Bug: 1273285
    Change-Id: Ifd2e73179200fa9aebad8c345f2bb58a4613bc6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3658172
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006176}

--- third_party/ipcz/src/reference_drivers/os_handle_fuchsia.cc
+++ third_party/ipcz/src/reference_drivers/os_handle_fuchsia.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/zx/handle.h>
 #include <zircon/status.h>
 
+#include <utility>
+
 #include "third_party/abseil-cpp/absl/base/macros.h"
 
 namespace ipcz::reference_drivers {
