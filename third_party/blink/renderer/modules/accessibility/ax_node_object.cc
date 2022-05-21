e963c7a5afc538b2e12475e511fe093e95738114
Benjamin Beaudry <bebeaudr@microsoft.com>
2022-05-21 01:23:33 +0000

    [A11y] Don't simulate click event on AT's focus
    In CL:684474 (almost 5 years ago), we decided to add a hack to improve
    some specific scenarios in older version of JAWS. This hack turns out
    to create unwanted behaviors with other screen readers when they request
    to perform the kFocus action, so we're removing it in this CL.
    
    See bug for an example of the negative impact of this simulated click.
    
    We're removing the hack in this CL, but are actually leaving it in
    behind a flag on Windows. See https://crbug.com/1326622 for more info.
    
    Fixed: 1324797
    Change-Id: Iba673edf73274bbc56b8a0f534680527246f684c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643196
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1006066}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/input/web_keyboard_event.h"
 #include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom-blink.h"
 #include "third_party/blink/public/strings/grit/blink_strings.h"
@@ -4587,16 +4588,21 @@ bool AXNodeObject::OnNativeFocusAction() {
     return true;
   }
 
-  // If the object is not natively focusable but can be focused using an ARIA
-  // active descendant, perform a native click instead. This will enable Web
-  // apps that set accessibility focus using an active descendant to capture and
-  // act on the click event. Otherwise, there is no other way to inform the app
-  // that an AT has requested the focus to be changed, except if the app is
-  // using AOM. To be extra safe, exclude objects that are clickable themselves.
-  // This won't prevent anyone from having a click handler on the object's
-  // container.
-  if (!IsClickable() && CanBeActiveDescendant()) {
-    return OnNativeClickAction();
+  if (base::FeatureList::IsEnabled(blink::features::kSimulateClickOnAXFocus)) {
+    // If the object is not natively focusable but can be focused using an ARIA
+    // active descendant, perform a native click instead. This will enable Web
+    // apps that set accessibility focus using an active descendant to capture
+    // and act on the click event. Otherwise, there is no other way to inform
+    // the app that an AT has requested the focus to be changed, except if the
+    // app is using AOM. To be extra safe, exclude objects that are clickable
+    // themselves. This won't prevent anyone from having a click handler on the
+    // object's container.
+    //
+    // This code is in the process of being removed. See the comment above
+    // |kSimulateClickOnAXFocus| in `blink/common/features.cc`.
+    if (!IsClickable() && CanBeActiveDescendant()) {
+      return OnNativeClickAction();
+    }
   }
 
   element->Focus();
