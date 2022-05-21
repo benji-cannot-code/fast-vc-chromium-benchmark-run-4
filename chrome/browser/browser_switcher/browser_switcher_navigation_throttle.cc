fe3c26099ac28efcea77f4c5e70a3c98b420eed4
Gyuyoung Kim <gyuyoung@igalia.com>
2022-05-21 02:10:52 +0000

    MPArch: Do not create BrowserSwitcherNavigationThrottle on non primary mainframe
    This CL allows creating a new BrowserSwitcherNavigationThrottle
    object on a primary mainframe's navigation. A new unittest is
    added to ensure that.
    
    TEST: BrowserSwitcherNavigationThrottleTest.
          DoNotCreateThrottleOnNonPrimaryMainFrame
    
    Bug: 1327156
    Change-Id: Ib8d1b56b7e86ba53ddaa0069424c937c7b625038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655156
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006085}

--- chrome/browser/browser_switcher/browser_switcher_navigation_throttle.cc
+++ chrome/browser/browser_switcher/browser_switcher_navigation_throttle.cc
@@ -94,7 +94,7 @@ BrowserSwitcherNavigationThrottle::MaybeCreateThrottleFor(
   if (browser_context->IsOffTheRecord())
     return nullptr;
 
-  if (!navigation->IsInMainFrame())
+  if (!navigation->IsInPrimaryMainFrame())
     return nullptr;
 
   return std::make_unique<navigation_interception::InterceptNavigationThrottle>(
