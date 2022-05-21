b5fd852fc75c9f3abba6a2b9517e837bf47717ef
Benjamin Beaudry <bebeaudr@microsoft.com>
2022-05-21 05:18:32 +0000

    [Focusgroup] Fix Focusgroup parser with ShadowDOM
    In this CL, we fix a known issue with the parser of the focusgroup
    attribute in ShadowDOMs. The bug was caused by the fact that the nodes
    in the shadow tree is parsed *before* the parent node that connects it
    to the main tree.
    
    When parsing the focusgroup attribute for a specific element, we expect that the ancestor elements have already parsed their own focusgroup
    attribute, so extending a focusgroup when in a ShadowDOM didn't work if
    the nearest focusgroup ancestor was declared outside of the ShadowDOM.
    
    We fix this bug by re-running the parser for all nodes within a shadow
    tree when we insert the shadow host into the main tree. To avoid running
    this every time we insert a shadow host in the main tree, we added a new
    bitfield on the ShadowRoot that keeps track of whether one or more node
    in the shadow tree has the focusgroup attribute set. We then only re-run
    the parser on the shadow trees when we know that there is at least one
    focusgroup contained within that tree.
    
    KNOWN ISSUE: This CL doesn't fix the parser when shadow doms are built with nodes that are assigned to <slot> elements. I'll fix this bug in a separate CL. See https://crbug.com/1327752 for more info.
    
    Bug: 1286127
    Change-Id: Ia9409dd91590f3ae06429867cf3b1d1f7f708bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3595142
    Quick-Run: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1006107}

--- third_party/blink/renderer/core/dom/focusgroup_flags.cc
+++ third_party/blink/renderer/core/dom/focusgroup_flags.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
-#include "third_party/blink/renderer/core/dom/element_traversal.h"
+#include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
 #include "third_party/blink/renderer/core/dom/space_split_string.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -18,15 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink::focusgroup {
 
 FocusgroupFlags FindNearestFocusgroupAncestorFlags(const Element* element) {
-  // TODO(bebeaudr): We should be using FlatTreeTraversal here.
-  Element* ancestor = Traversal<Element>::FirstAncestor(*element);
+  Element* ancestor = FlatTreeTraversal::ParentElement(*element);
   while (ancestor) {
     FocusgroupFlags ancestor_flags = ancestor->GetFocusgroupFlags();
     // When this is true, we found the focusgroup to extend.
     if (ancestor_flags != FocusgroupFlags::kNone) {
       return ancestor_flags;
     }
-    ancestor = Traversal<Element>::FirstAncestor(*ancestor);
+    ancestor = FlatTreeTraversal::ParentElement(*ancestor);
   }
   return FocusgroupFlags::kNone;
 }
