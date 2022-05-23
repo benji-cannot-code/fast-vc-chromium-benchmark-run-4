e7631b2cb279f2f1bc76a7d3fe81926739eb553e
Antonio Sartori <antoniosartori@chromium.org>
2022-05-23 18:08:52 +0000

    DCHECK SystemNetworkContextManager::CreateInstance not reentrant
    This CL adds a DCHECK to ensure that the function SystemNetworkContextManager::CreateInstance is not reentrant. Serves as regression test for crbug.com/1304700.
    
    Bug: 1304700
    Change-Id: Ifae9d2d19c391ddcfc543fa5b7897e8513dcdc1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512912
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006518}

--- chrome/browser/net/system_network_context_manager.cc
+++ chrome/browser/net/system_network_context_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <utility>
 
+#include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/build_time.h"
 #include "base/command_line.h"
@@ -397,6 +398,13 @@ SystemNetworkContextManager::GetSharedURLLoaderFactory() {
 // static
 SystemNetworkContextManager* SystemNetworkContextManager::CreateInstance(
     PrefService* pref_service) {
+#if DCHECK_IS_ON()
+  // Check that this function is not reentrant.
+  static bool inside_this_function = false;
+  DCHECK(!inside_this_function);
+  base::AutoReset now_inside_this_function(&inside_this_function, true);
+#endif
+
   DCHECK(!g_system_network_context_manager);
   g_system_network_context_manager =
       new SystemNetworkContextManager(pref_service);
