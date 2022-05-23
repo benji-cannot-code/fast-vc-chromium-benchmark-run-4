fc54935d3b363c75f2c3b5d9c97ad6f633a1cfb2
Anders Hartvoll Ruud <andruud@chromium.org>
2022-05-23 21:20:13 +0000

    StylePendingImage::IsEqual must check value for equality
    Otherwise two values url('#a') and url('#a') are treated as non-equal.
    
    Change-Id: Id4d16ffcbef4c300c1a22f56422d5bf6a0c7028f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652305
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006608}

--- third_party/blink/renderer/core/style/style_pending_image.h
+++ third_party/blink/renderer/core/style/style_pending_image.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_STYLE_PENDING_IMAGE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_STYLE_PENDING_IMAGE_H_
 
+#include "base/memory/values_equivalent.h"
 #include "base/notreached.h"
 #include "third_party/blink/renderer/core/style/style_image.h"
 #include "third_party/blink/renderer/platform/graphics/image.h"
@@ -44,7 +45,7 @@ class ImageResourceObserver;
 // are not referenced by the final style.  They should only exist in a
 // ComputedStyle for non-rendered elements created with EnsureComputedStyle or
 // display:contents.
-class StylePendingImage final : public StyleImage {
+class CORE_EXPORT StylePendingImage final : public StyleImage {
  public:
   explicit StylePendingImage(const CSSValue& value)
       : value_(const_cast<CSSValue*>(&value)) {
@@ -100,7 +101,7 @@ inline bool StylePendingImage::IsEqual(const StyleImage& other) const {
   if (!other.IsPendingImage())
     return false;
   const auto& other_pending = To<StylePendingImage>(other);
-  return value_ == other_pending.value_;
+  return base::ValuesEquivalent(value_, other_pending.value_);
 }
 
 }  // namespace blink
