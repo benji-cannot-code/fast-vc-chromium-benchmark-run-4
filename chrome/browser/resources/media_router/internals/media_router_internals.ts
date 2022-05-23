b85b2ce2b5e3318c3871418174f41642e7a17ede
dpapad <dpapad@chromium.org>
2022-05-23 19:41:21 +0000

    Migrate c/b/r/media_router to TypeScript.
    Specifically:
     - Migrating chrome://cast-feedback
     - Migrating chrome://media-internals
     - Migrating corresponding tests in c/test/data/webui/media_router/
     - Also doing a couple cleanups along the way:
       1) splitting those UIs to their own sub-folders, so that each
          subfolder can have its own ts_library() target.
       2) Using html_to_wrapper() instead of html_to_js()
    
    Bug: 1316438,1292025
    Change-Id: Id217df5a7aaea09a60bf3c6d9c2d040c3810cf11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656112
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006554}

--- chrome/browser/resources/media_router/internals/media_router_internals.ts
+++ chrome/browser/resources/media_router/internals/media_router_internals.ts
@@ -6,19 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
 import {$} from 'chrome://resources/js/util.m.js';
 
-function formatJson(jsonObj) {
+function formatJson(jsonObj: object) {
   return JSON.stringify(jsonObj, null, /* spacing level = */ 2);
 }
 
 // Handles user events for the Media Router Internals UI.
 document.addEventListener('DOMContentLoaded', function() {
-  sendWithPromise('getState').then(status => {
+  sendWithPromise('getState').then((status: object) => {
     $('sink-status-div').textContent = formatJson(status);
   });
-  sendWithPromise('getProviderState', 'CAST').then(status => {
+  sendWithPromise('getProviderState', 'CAST').then((status: object) => {
     $('cast-status-div').textContent = formatJson(status);
   });
-  sendWithPromise('getLogs').then(logs => {
+  sendWithPromise('getLogs').then((logs: object) => {
     // TODO(crbug.com/687380): Present the logs in a table format.
     $('logs-div').textContent = formatJson(logs);
   });
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -225,7 +225,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "META": {"sizes": {"includes": [100]}},
     "includes": [1760],
   },
-  "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/media_router/media_router_feedback_resources.grd": {
+  "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/media_router/cast_feedback/resources.grd": {
     "META": {"sizes": {"includes": [10],}},
     "includes": [1780],
   },
