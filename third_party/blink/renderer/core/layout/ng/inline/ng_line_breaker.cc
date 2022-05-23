e6a6ad965e9f5a718ec9dc413b7ef73a0fdeb19e
Andreu Botella <abotella@igalia.com>
2022-05-23 10:01:39 +0000

    Fix a bug with calculation of ShapeResultView's previous safe offset
    A ShapeResultView's previous safe to break offset used to be
    calculated by assuming that a part's first character corresponds to
    the part's start index. For LTR text, however, the start index is
    often, if not always, 0, meaning that for ShapeResultViews which start
    at some non-zero index into the text (such as for lines in a paragraph
    other than the first), the returned previous safe offset could be much
    earlier than it should, or it could even be before the start of the
    view.
    
    Since NGLineBreaker::TruncateLineEndResult uses the previous safe
    offset to determine which parts of a new line to reshape, if the
    returned safe offset happens to be in the range of the view and in the
    middle of a ligature, the first character in the ligature would not be
    reshaped and would thus be set to the ligature glyph, while the
    characters after the returned safe index would be reshaped and be set
    to their corresponding glyphs. This leads to the appearence of the
    characters after the offset being repeated.
    
    This was fixed by changing `run_start` to be the part's start index
    plus the view's char index offset.
    
    Bug: 1304876
    Change-Id: Ied68dd5986b603814fa236e7ab2081db5a817723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644742
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Andreu Botella <abotella@igalia.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006370}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
@@ -537,8 +537,7 @@ void NGLineBreaker::PrepareNextLine(NGLineInfo* line_info) {
   ResetRewindLoopDetector();
 }
 
-void NGLineBreaker::NextLine(
-    NGLineInfo* line_info) {
+void NGLineBreaker::NextLine(NGLineInfo* line_info) {
   PrepareNextLine(line_info);
   BreakLine(line_info);
   if (UNLIKELY(HasHyphen()))
@@ -579,8 +578,7 @@ void NGLineBreaker::NextLine(
   ComputeLineLocation(line_info);
 }
 
-void NGLineBreaker::BreakLine(
-    NGLineInfo* line_info) {
+void NGLineBreaker::BreakLine(NGLineInfo* line_info) {
   DCHECK(!line_info->IsLastLine());
   const HeapVector<NGInlineItem>& items = Items();
   state_ = LineBreakState::kContinue;
@@ -1470,6 +1468,7 @@ scoped_refptr<ShapeResultView> NGLineBreaker::TruncateLineEndResult(
 
   unsigned last_safe = source_result->PreviousSafeToBreakOffset(end_offset);
   DCHECK_LE(last_safe, end_offset);
+  // TODO(abotella): Shouldn't last_safe <= start_offset trigger a reshaping?
   if (last_safe == end_offset || last_safe <= start_offset) {
     return ShapeResultView::Create(source_result, start_offset, end_offset);
   }
@@ -1910,9 +1909,8 @@ void NGLineBreaker::HandleBidiControlItem(const NGInlineItem& item,
   MoveToNextOf(item);
 }
 
-void NGLineBreaker::HandleAtomicInline(
-    const NGInlineItem& item,
-    NGLineInfo* line_info) {
+void NGLineBreaker::HandleAtomicInline(const NGInlineItem& item,
+                                       NGLineInfo* line_info) {
   DCHECK_EQ(item.Type(), NGInlineItem::kAtomicInline);
   DCHECK(item.Style());
   const ComputedStyle& style = *item.Style();
