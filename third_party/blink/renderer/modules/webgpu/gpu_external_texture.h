c72c0a78dafbc51cc18f3e35c3bfb57beca5c166
Shaobo <shaobo.yan@intel.com>
2022-05-23 07:01:59 +0000

    Add GPUExternalTexture.expired()
    WebGPU spec defines GPUExternalTextrue.expired() to help developers to import a new video frame in efficient way.
    
    This CL implements expired() in GPUExternalTextures and handles the case
    that call importExternalTexture on a GPUDevice which has been destroyed (via destroy()).
    
    Bug: 1318345, 1310172
    Change-Id: Ia28ae66a6a3ca636bbfeb06be8833e375e1081ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652008
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006328}

--- third_party/blink/renderer/modules/webgpu/gpu_external_texture.h
+++ third_party/blink/renderer/modules/webgpu/gpu_external_texture.h
@@ -23,6 +23,10 @@ class GPUExternalTexture : public DawnObject<WGPUExternalTexture> {
       GPUDevice* device,
       const GPUExternalTextureDescriptor* webgpu_desc,
       ExceptionState& exception_state);
+  static GPUExternalTexture* CreateExpired(
+      GPUDevice* device,
+      const GPUExternalTextureDescriptor* webgpu_desc,
+      ExceptionState& exception_state);
   explicit GPUExternalTexture(
       GPUDevice* device,
       WGPUExternalTexture external_texture,
@@ -33,6 +37,8 @@ class GPUExternalTexture : public DawnObject<WGPUExternalTexture> {
 
   void Destroy();
 
+  bool expired() const { return expired_; }
+
  private:
   void setLabelImpl(const String& value) override {
     std::string utf8_label = value.Utf8();
@@ -40,6 +46,9 @@ class GPUExternalTexture : public DawnObject<WGPUExternalTexture> {
   }
 
   scoped_refptr<WebGPUMailboxTexture> mailbox_texture_;
+
+  // This attribute marks whether GPUExternalTexture will be destroyed.
+  bool expired_ = false;
 };
 
 }  // namespace blink
