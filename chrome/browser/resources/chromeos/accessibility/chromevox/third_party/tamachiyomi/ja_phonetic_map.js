00f926cc9b6ef4c7c81bf663664f9a27bc6475df
David Tseng <dtseng@google.com>
2022-05-23 19:46:31 +0000

    ChromeVox: modularize JaPhoneticMap
    R=josiahk@google.com
    
    Bug: none
    Test: cq
    Change-Id: I014adf8a582abfbff944b95ebb401fe5a7ef9d6c
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661980
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006559}

--- chrome/browser/resources/chromeos/accessibility/chromevox/third_party/tamachiyomi/ja_phonetic_map.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/third_party/tamachiyomi/ja_phonetic_map.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
-goog.provide('JaPhoneticMap');
+export const JaPhoneticMap = {};
 
 /**
  * A map containing phonetic disambiguation data for Japanese.
