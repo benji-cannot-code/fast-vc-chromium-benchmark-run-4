d9cb2b563b73333bfa3af2f14bd4832a3bf59dae
Felipe Andrade <fsandrade@google.com>
2022-05-23 09:29:21 +0000

    Fix typos in system_network_context_manager
    Fix typos found in system_network_context_manager.h/.cc.
    
    Bug: None
    Change-Id: I83cb933136c3ccd08a90737d0c092695eb4acd2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655447
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006361}

--- chrome/browser/net/system_network_context_manager.cc
+++ chrome/browser/net/system_network_context_manager.cc
@@ -125,7 +125,7 @@ enum class NetworkSandboxState {
   kMaxValue = kDisabledBecauseOfFailedLaunch
 };
 
-// The global instance of the SystemNetworkContextmanager.
+// The global instance of the SystemNetworkContextManager.
 SystemNetworkContextManager* g_system_network_context_manager = nullptr;
 
 // Whether or not any instance of the system network context manager has
@@ -217,7 +217,7 @@ bool ShouldUseBuiltinCertVerifier(PrefService* local_state) {
 #endif
   // Note: intentionally checking the feature state here rather than falling
   // back to CertVerifierImpl::kDefault, as browser-side network context
-  // initializition for TrialComparisonCertVerifier depends on knowing which
+  // initialization for TrialComparisonCertVerifier depends on knowing which
   // verifier will be used.
   return base::FeatureList::IsEnabled(
       net::features::kCertVerifierBuiltinFeature);
@@ -230,7 +230,7 @@ bool ShouldUseBuiltinCertVerifier(PrefService* local_state) {
 bool ShouldUseChromeRootStore(PrefService* local_state) {
   // Note: intentionally checking the feature state here rather than falling
   // back to ChromeRootImpl::kRootDefault, as browser-side network context
-  // initializition for TrialComparisonCertVerifier depends on knowing which
+  // initialization for TrialComparisonCertVerifier depends on knowing which
   // verifier will be used.
   return base::FeatureList::IsEnabled(net::features::kChromeRootStoreUsed);
 }
