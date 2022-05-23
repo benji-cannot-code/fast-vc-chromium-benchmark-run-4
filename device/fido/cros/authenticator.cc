58d828b828094dc8c823c283fab6af5d0927a99c
Howard Yang <hcyang@google.com>
2022-05-23 20:31:38 +0000

    fido: Implement SupportsEpAtt for CrOS authenticator
    Currently CrOS authenticator doesn't override the
    SupportsEnterpriseAttestation method, so it always returns false. Under
    this condition Chrome will never send a request with attestation
    preference = "enterprise". The CrOS authenticator actually supports
    enterprise attestation when U2F/G2F mode is enabled, which is determined
    by policy and device capability. Check whether U2F mode is enabled when
    initializing the authenticator and cache the result, and returns it in
    SupportsEnterpriseAttestation.
    
    Bug: b:226453823, 1328415
    Test: manual test along with daemon side changes that a MakeCredential
    request that wants enterprise attestation will get a valid G2F cert
    instead of a software generated cert.
    Test: CQ
    
    Change-Id: I33804907e7ce48534a553200d7a27c0fad222d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3659142
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006584}

--- device/fido/cros/authenticator.cc
+++ device/fido/cros/authenticator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/barrier_closure.h"
 #include "base/bind.h"
 #include "base/containers/span.h"
 #include "base/strings/string_number_conversions.h"
@@ -75,10 +76,16 @@ ChromeOSAuthenticator::AuthenticatorTransport() const {
 
 void ChromeOSAuthenticator::InitializeAuthenticator(
     base::OnceClosure callback) {
+  auto barrier = base::BarrierClosure(2, std::move(callback));
+
   u2f::GetAlgorithmsRequest request;
   chromeos::U2FClient::Get()->GetAlgorithms(
       request, base::BindOnce(&ChromeOSAuthenticator::OnGetAlgorithmsResponse,
-                              weak_factory_.GetWeakPtr(), std::move(callback)));
+                              weak_factory_.GetWeakPtr(), barrier));
+
+  IsPowerButtonModeEnabled(
+      base::BindOnce(&ChromeOSAuthenticator::OnIsPowerButtonModeEnabled,
+                     weak_factory_.GetWeakPtr(), barrier));
 }
 
 void ChromeOSAuthenticator::OnGetAlgorithmsResponse(
@@ -97,6 +104,13 @@ void ChromeOSAuthenticator::OnGetAlgorithmsResponse(
     supported_algorithms_ = absl::nullopt;
   }
 
+  std::move(callback).Run();
+}
+
+void ChromeOSAuthenticator::OnIsPowerButtonModeEnabled(
+    base::OnceClosure callback,
+    bool enabled) {
+  u2f_enabled_ = enabled;
   std::move(callback).Run();
 }
 
@@ -441,6 +455,12 @@ bool ChromeOSAuthenticator::IsPaired() const {
 
 bool ChromeOSAuthenticator::RequiresBlePairingPin() const {
   return false;
+}
+
+bool ChromeOSAuthenticator::SupportsEnterpriseAttestation() const {
+  // Enterprise attestation is enabled in the authenticator if its U2F/G2F mode
+  // is enabled.
+  return u2f_enabled_;
 }
 
 base::WeakPtr<FidoAuthenticator> ChromeOSAuthenticator::GetWeakPtr() {
