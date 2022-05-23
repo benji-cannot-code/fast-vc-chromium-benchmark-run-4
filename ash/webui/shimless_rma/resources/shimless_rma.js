04c674b41273ebe17a5b6fdaf6a59509fefee725
Gavin Williams <gavinwill@chromium.org>
2022-05-23 16:49:24 +0000

    Shimless: Always reload Select Components page
    - This change forces the Select Components page to request the
      components list on every load
    - This prevents a stale list of components from being shown
    
    Bug: 1198187, b:230289868
    Change-Id: I1054f4074abf2dce550e50539027faabfea3f4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656820
    Auto-Submit: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006481}

--- ash/webui/shimless_rma/resources/shimless_rma.js
+++ ash/webui/shimless_rma/resources/shimless_rma.js
@@ -102,7 +102,7 @@ export const StateComponentMapping = {
   },
   [State.kSelectComponents]: {
     componentIs: 'onboarding-select-components-page',
-    requiresReloadWhenShown: false,
+    requiresReloadWhenShown: true,
     buttonNext: ButtonState.DISABLED,
     buttonCancel: ButtonState.VISIBLE,
     buttonBack: ButtonState.VISIBLE,
