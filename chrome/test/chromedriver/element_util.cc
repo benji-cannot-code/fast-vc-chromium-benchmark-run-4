0fbcaa89cedf01f4a52112e0a8e3df8515761b7d
Dan McArdle <dmcardle@chromium.org>
2022-05-23 07:50:01 +0000

    Eliminate base::LowerCaseEqualsASCII in /chrome/test/chromedriver
    This CL is a behavior-preserving refactor. It replaces calls to
    base::LowerCaseEqualsASCII with base::EqualsCaseInsensitiveASCII.
    
    We are deprecating base::LowerCaseEqualsASCII because its interface is a
    mild footgun, in that it requires the second parameter to be lowercase.
    
    See the design doc for more details.
    https://docs.google.com/document/d/18zWV0zN4oAqMZpond1Ut0QviHUeHCSwCo08mqThwHkQ/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=nechaev@chromium.org
    
    Bug: 1326529
    Change-Id: I5d2d613667302f9cc6008eacd4d24d361d56f174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655941
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006337}

--- chrome/test/chromedriver/element_util.cc
+++ chrome/test/chromedriver/element_util.cc
@@ -530,8 +530,7 @@ Status IsDocumentTypeXml(
                                "document.contentType", false, &contentType);
   if (status.IsError())
           return status;
-  if (base::LowerCaseEqualsASCII(contentType->GetString(),
-                                 "text/xml"))
+  if (base::EqualsCaseInsensitiveASCII(contentType->GetString(), "text/xml"))
     *is_xml_document = true;
   else
     *is_xml_document = false;
@@ -568,7 +567,7 @@ Status IsElementAttributeEqualToIgnoreCase(
     return status;
   if (result->is_string()) {
     *is_equal =
-        base::LowerCaseEqualsASCII(result->GetString(), attribute_value);
+        base::EqualsCaseInsensitiveASCII(result->GetString(), attribute_value);
   } else {
     *is_equal = false;
   }
