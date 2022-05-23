c3d6f04a5fedff4d0ab3bbd0460459a433a2d439
Tommy Li <tommycli@chromium.org>
2022-05-23 22:41:34 +0000

    [journeys] Remove UI configuration for unlabelled clusters
    All clusters must have a label now, so remove the UI configuration to
    support unlabelled clusters with a top visit. This simplifies the code
    dramatically.
    
    Bug: 1294348
    Change-Id: I76fd1068c593157fa4a440bcee635e62675dac81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652987
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006643}

--- ui/webui/resources/cr_components/history_clusters/cluster.ts
+++ ui/webui/resources/cr_components/history_clusters/cluster.ts
@@ -285,6 +285,12 @@ class HistoryClusterElement extends PolymerElement {
   }
 
   private computeLabel_(): string {
+    if (!this.cluster.label) {
+      // This never happens unless we misconfigured our variations config.
+      // This sentinel string matches the Android UI.
+      return 'no_label';
+    }
+
     insertHighlightedTextWithMatchesIntoElement(
         this.$.label, this.cluster.label!, this.cluster.labelMatchPositions);
     return this.cluster.label!;
@@ -294,13 +300,6 @@ class HistoryClusterElement extends PolymerElement {
     return this.cluster.visits.filter((visit: URLVisit) => {
       return !visit.hidden;
     });
-  }
-
-  /**
-   * Returns true if this should be considered a top visit.
-   */
-  private isTopVisit_(index: number, label: string): boolean {
-    return index === 0 && !label;
   }
 
   /**
