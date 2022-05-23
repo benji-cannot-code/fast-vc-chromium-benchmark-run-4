da8a3c61a705c30d23af3ba2e7e197eb4f99d3cc
Anastasiia N <anastasiian@google.com>
2022-05-23 10:36:52 +0000

    Update styling of ARC picker for Child users
    - Show "Add school account" button label
    - Use two column layout to match the rest of the pages
    
    Screenshot: http://screen/5eGqFap8GKLWVyy
    Bug: 1291993
    Change-Id: Idee0b7eba9b8ed4d0e0e23ac07cf9a5a6391a27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3634477
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006377}

--- chrome/browser/resources/chromeos/arc_account_picker/arc_account_picker_app.js
+++ chrome/browser/resources/chromeos/arc_account_picker/arc_account_picker_app.js
@@ -4,9 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '../account_manager_shared_css.js';
+import '../strings.m.js';
 
 import {getImage} from '//resources/js/icon.js';
 import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+
 import {Account, ArcAccountPickerBrowserProxy, ArcAccountPickerBrowserProxyImpl} from './arc_account_picker_browser_proxy.js';
 
 /**
@@ -39,6 +42,23 @@ export class ArcAccountPickerAppElement extends PolymerElement {
 
   static get properties() {
     return {
+      /**
+       * Whether two column layout should be used.
+       * @public {boolean}
+       */
+      useTwoColumnLayout: {
+        type: Boolean,
+        value: false,
+      },
+
+      /** @private {boolean} */
+      isChildUser_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('isChild');
+        },
+      },
+
       /**
        * Accounts which are not available in ARC and are shown on the ARC picker
        * screen.
@@ -82,6 +102,27 @@ export class ArcAccountPickerAppElement extends PolymerElement {
         resolve(true);
       });
     });
+  }
+
+  /**
+   * @return {string} A class name list for the main container.
+   * @private
+   */
+  getMainContainerClass_() {
+    if (this.useTwoColumnLayout) {
+      return 'main-container use-two-column-layout';
+    }
+
+    return 'main-container';
+  }
+
+  /**
+   * @return {string} A label for 'add account' button.
+   * @private
+   */
+  getAddAccountButtonLabel_() {
+    return loadTimeData.getString(
+        this.isChildUser_ ? 'addSchoolAccountLabel' : 'addAccountLabel');
   }
 
   /**
