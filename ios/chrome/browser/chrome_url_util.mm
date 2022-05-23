6aae07bf4007c447a1afda92fe5ab1ffeaf50fe0
Dan McArdle <dmcardle@chromium.org>
2022-05-23 11:03:19 +0000

    Eliminate base::LowerCaseEqualsASCII in /ios/chrome/browser
    This CL is a behavior-preserving refactor. It replaces calls to
    base::LowerCaseEqualsASCII with base::EqualsCaseInsensitiveASCII.
    
    We are deprecating base::LowerCaseEqualsASCII because its interface is a
    mild footgun, in that it requires the second parameter to be lowercase.
    
    See the design doc for more details.
    https://docs.google.com/document/d/18zWV0zN4oAqMZpond1Ut0QviHUeHCSwCo08mqThwHkQ/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=sdefresne@chromium.org
    
    Bug: 1326529
    Change-Id: Iee2428caf50043db67adfadcf6b87855ac29d0a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655945
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006385}

--- ios/chrome/browser/chrome_url_util.mm
+++ ios/chrome/browser/chrome_url_util.mm
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 bool UrlIsExternalFileReference(const GURL& url) {
   return url.SchemeIs(kChromeUIScheme) &&
-         base::LowerCaseEqualsASCII(url.host(), kChromeUIExternalFileHost);
+         base::EqualsCaseInsensitiveASCII(url.host(),
+                                          kChromeUIExternalFileHost);
 }
 
 bool UrlHasChromeScheme(const GURL& url) {
