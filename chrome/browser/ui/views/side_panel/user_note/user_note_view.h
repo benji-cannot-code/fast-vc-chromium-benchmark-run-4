8aa222b1b828c75683d31b42f3484da84140b8e5
Cheick Cisse <cheickcisse@google.com>
2022-05-24 19:47:42 +0000

    [Note] Implement editing state of UserNoteView
    This CL:
      - Implement OnEditUserNote()
      - Implement OnSaveUserNote()
      - Changes orphaned state name to detached
    
    Bug: 1327349
    Change-Id: I192f69bcf5288fad7e7fe424411345f7d0b5394f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652833
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007014}

--- chrome/browser/ui/views/side_panel/user_note/user_note_view.h
+++ chrome/browser/ui/views/side_panel/user_note/user_note_view.h
@@ -43,9 +43,9 @@ class UserNoteView : public views::View {
     kCreating,
     // State that will display an existing user note to be edited.
     kEditing,
-    // State that will display an orphan user note (note without a highlight in
-    // the page).
-    kOrphaned
+    // State that will display a detached user note (note without a highlight
+    // on the page).
+    kDetached
   };
 
   explicit UserNoteView(
@@ -65,14 +65,16 @@ class UserNoteView : public views::View {
                               base::Time date,
                               const std::string content,
                               const std::string quote);
-  void OnCancelNewUserNote();
+  void OnCancelUserNote(UserNoteView::State state);
   void OnAddUserNote();
+  void OnSaveUserNote();
+  void OnOpenMenu();
+  void OnMenuClosed();
   void OnEditUserNote(int event_flags);
   void OnDeleteUserNote(int event_flags);
   void OnLearnUserNote(int event_flags);
-  void OnOpenMenu();
-  void OnMenuClosed();
-  void SetCreatingOrEditState(const std::string content);
+  void SetCreatingOrEditState(const std::string content,
+                              UserNoteView::State state);
   void SetDefaultOrDetachedState(base::Time date,
                                  const std::string content,
                                  const std::string quote);
