6924edc7a04efb9f4bce311fc2ad53f4709504d5
Toby Huang <tobyhuang@chromium.org>
2022-05-24 19:20:29 +0000

    Projector: Simplify logic in OnEnablementPolicyChanged()
    If we know that the Projector app is installed, then there's no need
    to query for the app id because we know it's going to be
    kChromeUITrustedProjectorSwaAppId.
    
    Bug: b/230779397
    Change-Id: I1627cccd8ff1ba9ed6e20434ce0c24e7c682a5f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661140
    Reviewed-by: Li Lin <llin@chromium.org>
    Auto-Submit: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007000}

--- chrome/browser/ui/ash/projector/projector_client_impl.cc
+++ chrome/browser/ui/ash/projector/projector_client_impl.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/web_applications/system_web_app_ui_utils.h"
 #include "chrome/browser/web_applications/system_web_apps/system_web_app_manager.h"
-#include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chromeos/login/login_state/login_state.h"
@@ -288,11 +287,7 @@ void ProjectorClientImpl::OnEnablementPolicyChanged() {
   if (!is_enabled)
     CloseProjectorApp();
 
-  absl::optional<web_app::AppId> app_id = web_app::GetAppIdForSystemWebApp(
-      profile, ash::SystemWebAppType::PROJECTOR);
-  if (!app_id)
-    return;
-
   auto* web_app_provider = web_app::WebAppProvider::GetForWebApps(profile);
-  web_app_provider->sync_bridge().SetAppIsDisabled(app_id.value(), !is_enabled);
+  web_app_provider->sync_bridge().SetAppIsDisabled(
+      ash::kChromeUITrustedProjectorSwaAppId, !is_enabled);
 }
