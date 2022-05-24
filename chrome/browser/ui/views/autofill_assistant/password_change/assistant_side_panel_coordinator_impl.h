d007c7348a046dbc23410e9733038b5c8dd11410
Jan Keitel <jkeitel@google.com>
2022-05-24 09:02:17 +0000

    [APC Desktop] Implement AssistantDisplayDelegate for side panel.
    This CL makes AssistantSidePanelCoordinator extend
    AssistantDisplayDelegate. To decouple UI code, it also adds its own
    observer class within the AssistantSidePanelCoordinator and lets
    ApcClientImpl implement it.
    
    Bug: 1322419
    Change-Id: Iebd398c66a90c18910acdd579da7bddfdc279bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654233
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006810}

--- chrome/browser/ui/views/autofill_assistant/password_change/assistant_side_panel_coordinator_impl.h
+++ chrome/browser/ui/views/autofill_assistant/password_change/assistant_side_panel_coordinator_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_ASSISTANT_PASSWORD_CHANGE_ASSISTANT_SIDE_PANEL_COORDINATOR_IMPL_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/observer_list.h"
 #include "chrome/browser/ui/autofill_assistant/password_change/assistant_side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
 
@@ -24,14 +25,22 @@ class AssistantSidePanelCoordinatorImpl : public AssistantSidePanelCoordinator,
       const AssistantSidePanelCoordinatorImpl&) = delete;
   ~AssistantSidePanelCoordinatorImpl() override;
 
-  // AssistantSidePanelCoordinator
-  bool Shown() override;
+  // AssistantDisplayDelegate:
+  // Sets the Assistant side panel view. This method takes ownership of the view
+  // and returns a pointer to it, which can be used for later updates.
   views::View* SetView(std::unique_ptr<views::View> view) override;
+  // Gets the current view rendered in the side panel. Returns `nullptr` if the
+  // side panel is hidden or no view has been set.
   views::View* GetView() override;
+  // Removes the current view rendered in the side panel and destroys it.
   void RemoveView() override;
-  void AddObserver(SidePanelEntryObserver* observer) override;
 
-  // SidePanelEntryObserver
+  // AssistantSidePanelCoordinator:
+  bool Shown() override;
+  void AddObserver(Observer* observer) override;
+  void RemoveObserver(Observer* observer) override;
+
+  // SidePanelEntryObserver:
   void OnEntryHidden(SidePanelEntry* entry) override;
 
  private:
@@ -45,6 +54,9 @@ class AssistantSidePanelCoordinatorImpl : public AssistantSidePanelCoordinator,
   std::unique_ptr<views::View> side_panel_view_child_ = nullptr;
 
   raw_ptr<content::WebContents> web_contents_;
+
+  // List of observers to the side panel.
+  base::ObserverList<Observer> observers_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_AUTOFILL_ASSISTANT_PASSWORD_CHANGE_ASSISTANT_SIDE_PANEL_COORDINATOR_IMPL_H_
