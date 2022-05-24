b646a7ef492d1991875d0ab852a7470cc7f430a3
Xiaoling Bao <xiaolingbao@chromium.org>
2022-05-24 18:08:28 +0000

    Reland https://crrev.com/c/3643021: add offline integration test on Windows platform.
    Use system reg.exe instead of `updater_test_process.exe` because the
    latter is not a monolithic binary.
    
    Bug: 1286574
    Change-Id: I600a4475c780bb34f6d0808f173d3f58b37080da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661444
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006960}

--- chrome/updater/test/integration_tests_impl.h
+++ chrome/updater/test/integration_tests_impl.h
@@ -217,6 +217,8 @@ void InstallApp(UpdaterScope scope, const std::string& app_id);
 
 void UninstallApp(UpdaterScope scope, const std::string& app_id);
 
+void RunOfflineInstall(UpdaterScope scope);
+
 }  // namespace test
 }  // namespace updater
 
