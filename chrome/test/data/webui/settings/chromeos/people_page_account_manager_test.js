8370ce9339f8fb6b6d9de5d22737913de4034352
Wes Okuhara <wesokuhara@google.com>
2022-05-24 19:30:56 +0000

    CrOS Settings: Convert parental controls page to class-based syntax
    Converts Polymer elements to the class-based syntax to prepare for the
    upcoming TypeScript migration. Additionally removes uses of
    addSingletonGetter().
    
    Bug: chromium:1206112, chromium:1315757
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*Parental*'
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*People*'
    Test: Manual UI testing
    Change-Id: I7f4a458e773e08702dde0e2ea0f70674c5a38b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3657938
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007009}

--- chrome/test/data/webui/settings/chromeos/people_page_account_manager_test.js
+++ chrome/test/data/webui/settings/chromeos/people_page_account_manager_test.js
@@ -457,7 +457,7 @@ suite('AccountManagerAccountChildAccountTests', function() {
 
   setup(function() {
     parentalControlsBrowserProxy = new TestParentalControlsBrowserProxy();
-    ParentalControlsBrowserProxyImpl.instance_ = parentalControlsBrowserProxy;
+    ParentalControlsBrowserProxyImpl.setInstance(parentalControlsBrowserProxy);
     PolymerTest.clearBody();
 
     accountManager = document.createElement('settings-account-manager');
