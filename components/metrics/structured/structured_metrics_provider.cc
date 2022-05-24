26c49abcbd2ba15d4814f0df4fc52606e4889002
Jong Ahn <jongahn@google.com>
2022-05-24 23:27:54 +0000

    [Structured Metrics]: Integrate variable key rotations from codegen.
    Bug: 1325095
    Change-Id: Ia60f1ad4f0e99e658ef250848fa8ff683f420864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652976
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007117}

--- components/metrics/structured/structured_metrics_provider.cc
+++ components/metrics/structured/structured_metrics_provider.cc
@@ -412,7 +412,7 @@ void StructuredMetricsProvider::RecordEventFromEventBase(
   switch (event.id_type()) {
     case IdType::kProjectId:
       event_proto->set_profile_event_id(
-          key_data->Id(event.project_name_hash()));
+          key_data->Id(event.project_name_hash(), event.key_rotation_period()));
       break;
     case IdType::kUmaId:
       // TODO(crbug.com/1148168): Unimplemented.
@@ -448,7 +448,8 @@ void StructuredMetricsProvider::RecordEventFromEventBase(
     switch (metric.type) {
       case EventBase::MetricType::kHmac:
         metric_proto->set_value_hmac(key_data->HmacMetric(
-            event.project_name_hash(), metric.name_hash, metric.hmac_value));
+            event.project_name_hash(), metric.name_hash, metric.hmac_value,
+            event.key_rotation_period()));
         break;
       case EventBase::MetricType::kInt:
         metric_proto->set_value_int64(metric.int_value);
