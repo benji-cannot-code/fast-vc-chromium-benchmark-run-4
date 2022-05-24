92c8d9c104a0738e299336c380d10a544c1958e7
Florian Leimgruber <fleimgruber@google.com>
2022-05-24 13:30:58 +0000

    Clean up PhoneNumber::SetInfo.
    - Clear the entire number if it is invalid. The PhoneNumber objects
      stores the number twice; once the raw info provided to the setter and
      once the parsed representation. Currently only the raw info is reset
      for invalid numbers, which leads to inconsistencies, such as:
      EXPECT_FALSE(phone.SetInfo(PHONE_HOME_WHOLE_NUMBER, u"xzy", "US"));
      phone.GetRawInfo(PHONE_HOME_WHOLE_NUMBER) is empty.
      phone.GetInfo(PHONE_HOME_WHOLE_NUMBER, "US") equals "xzy".
      Changing this breaks two iOS tests, which uses invalid phone numbers.
      These phone numbers are changed to valid ones.
    - Verify expected values of GetInfo and GetRawInfo in the unit tests,
      instead of only GetRawInfo.
      GetRawInfo returns a formatted version of the phone number and is
      displayed in the settings, while GetInfo is used for filling and
      doesn't contain formatting marks.
    
    Bug: 1311937
    Change-Id: Ifd852aa5127bfc9fbbab0a5f6db09e9e246371cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3562939
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006865}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_address+AutofillProfile_unittest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_address+AutofillProfile_unittest.mm
@@ -45,7 +45,7 @@ TEST_F(ManualFillAddressFormAutofilliOSTest, CreationWithMiddleName) {
   NSString* city = @"Springfield";
   NSString* state = @"State";
   NSString* country = @"US";
-  NSString* phoneNumber = @"123-456-789";
+  NSString* phoneNumber = @"6502345678";
   NSString* emailAddress = @"john@doe";
 
   autofill::CountryNames::SetLocaleString("en-US");
@@ -96,7 +96,7 @@ TEST_F(ManualFillAddressFormAutofilliOSTest, CreationWithMiddleInitial) {
   NSString* city = @"Springfield";
   NSString* state = @"State";
   NSString* country = @"US";
-  NSString* phoneNumber = @"123-456-789";
+  NSString* phoneNumber = @"6502345678";
   NSString* emailAddress = @"john@doe";
 
   autofill::CountryNames::SetLocaleString("en-US");
