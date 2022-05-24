c3b8070ee8f37e15efd9eb9a6d35d91e9322b6c9
Dave Tapuska <dtapuska@chromium.org>
2022-05-24 16:49:40 +0000

    Export documentLifecycle for Web Messaging API.
    Add documentLifecycle to the MessageSender object. As a result of
    feedback from Mozilla it was suggested that we convey the
    documentLifecycle at the time the message was sent to avoid possible
    TOU (Time of Use) problems where an extension would have to look it up
    and it might have changed in the meantime.
    
    See https://bit.ly/3G4RBEn for discussion regarding this change.
    
    BUG=1264911
    
    Change-Id: I320495606ddb925f8e5231a71fa7423e2458633b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3614028
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006903}

--- chrome/test/data/extensions/api_test/messaging/connect/test.js
+++ chrome/test/data/extensions/api_test/messaging/connect/test.js
@@ -22,6 +22,7 @@ function compareSenders(expected, actual) {
     chrome.test.assertFalse(!!actual.tab);
   }
 
+  chrome.test.assertEq('active', actual.documentLifecycle);
   chrome.test.assertEq(expected.frameId, actual.frameId);
   chrome.test.assertEq(expected.url, actual.url);
   chrome.test.assertEq(expected.origin, actual.origin);
