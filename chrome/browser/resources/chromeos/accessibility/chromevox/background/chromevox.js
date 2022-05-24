9b8e12b90e2a484acc3de27766797bd9ed7f3458
Anastasia Helfinstein <anastasi@google.com>
2022-05-24 03:07:30 +0000

    [Chromevox] Remove forward declares
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I533aa3ce9cacbccdcf577e272b683e2d619a600f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646186
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006743}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox.js
@@ -7,12 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Defines a global object.
  */
 
-// Forward declare.
-goog.addDependency('../common/abstract_earcons.js', ['AbstractEarcons'], []);
-goog.addDependency(
-    '../common/braille/braille_interface.js', ['BrailleInterface'], []);
-goog.addDependency('../common/tts_interface.js', ['TtsInterface'], []);
-
 goog.provide('ChromeVox');
 
 goog.require('AbstractEarcons');
