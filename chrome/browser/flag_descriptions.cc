caf4f22a9835755cc666053256ff956456a4d892
Andrea Gomez <andreaxg@google.com>
2022-05-24 21:54:13 +0000

    Removed desktop-restructured-language-settings references
    Bug: 1328373
    Change-Id: Icc2cd408ed539bba2d6302cd7b913bc015235b62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661673
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007083}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -5759,11 +5759,6 @@ const char kSideSearchDSESupportDescription[] =
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_FUCHSIA)
 
-const char kDesktopRestructuredLanguageSettingsName[] =
-    "Restructured Language Settings (Desktop)";
-const char kDesktopRestructuredLanguageSettingsDescription[] =
-    "Enable the new restructured language settings page";
-
 const char kDesktopDetailedLanguageSettingsName[] =
     "Detailed Language Settings (Desktop)";
 const char kDesktopDetailedLanguageSettingsDescription[] =
