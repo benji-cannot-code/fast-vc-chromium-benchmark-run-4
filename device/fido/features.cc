9ba3d5db4fa55e7fb72d79ee05b6fe7b77a2d169
Adam Langley <agl@chromium.org>
2022-05-24 01:25:54 +0000

    webauthn: readd "WebAuthenticationPasskeysUI".
    This was removed, along with some other feature flags, in cf9d4c1fa0.
    But it turns out that we still need this one.
    
    Change-Id: I59681b148613ff9e464886d626e82762df8b97cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661453
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006707}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -40,4 +40,7 @@ extern const base::Feature kWebAuthnGoogleCorpRemoteDesktopClientPrivilege{
     "WebAuthenticationGoogleCorpRemoteDesktopClientPrivilege",
     base::FEATURE_DISABLED_BY_DEFAULT};
 
+extern const base::Feature kWebAuthPasskeysUI{
+    "WebAuthenticationPasskeysUI", base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace device
