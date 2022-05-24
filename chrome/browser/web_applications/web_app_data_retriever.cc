9590de8cb05fd85299dfc17c428a3f472fe26058
Eric Willigers <ericwilligers@chromium.org>
2022-05-24 17:58:18 +0000

    WebAppDataRetriever: Defensive shutdown
    We stop fetching if we detect the web contents are being destroyed.
    
    Note that WebAppInstallTask::ShouldStopInstall() and
    FetchManifestAndInstallCommand::IsWebContentsDestroyed() already have
    similar logic.
    
    We re-enable a WebAppIntegration test that was disabled on ChromeOS.
    
    BUG: 1311979
    Change-Id: Ie5d22fc7b8516239200ecaaca828f2023c28ed6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661383
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006954}

--- chrome/browser/web_applications/web_app_data_retriever.cc
+++ chrome/browser/web_applications/web_app_data_retriever.cc
@@ -40,6 +40,7 @@ WebAppDataRetriever::~WebAppDataRetriever() = default;
 void WebAppDataRetriever::GetWebAppInstallInfo(
     content::WebContents* web_contents,
     GetWebAppInstallInfoCallback callback) {
+  DCHECK(!web_contents->IsBeingDestroyed());
   Observe(web_contents);
 
   // Concurrent calls are not allowed.
@@ -88,6 +89,7 @@ void WebAppDataRetriever::CheckInstallabilityAndRetrieveManifest(
     content::WebContents* web_contents,
     bool bypass_service_worker_check,
     CheckInstallabilityCallback callback) {
+  DCHECK(!web_contents->IsBeingDestroyed());
   webapps::InstallableManager* installable_manager =
       webapps::InstallableManager::FromWebContents(web_contents);
   DCHECK(installable_manager);
@@ -116,6 +118,7 @@ void WebAppDataRetriever::GetIcons(content::WebContents* web_contents,
                                    std::vector<GURL> icon_urls,
                                    bool skip_page_favicons,
                                    GetIconsCallback callback) {
+  DCHECK(!web_contents->IsBeingDestroyed());
   Observe(web_contents);
 
   // Concurrent calls are not allowed.
@@ -237,7 +240,7 @@ void WebAppDataRetriever::CallCallbackOnError() {
 }
 
 bool WebAppDataRetriever::ShouldStopRetrieval() const {
-  return !web_contents();
+  return !web_contents() || web_contents()->IsBeingDestroyed();
 }
 
 }  // namespace web_app
