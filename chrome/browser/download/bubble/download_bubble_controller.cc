102ee1430d147517075f5ffbd17944291d49adda
thefrog <thefrog@chromium.org>
2022-05-24 23:10:24 +0000

    Make button say 'Resume' instead of 'Cancel' when download is paused
    Changes involve:
    1. Adding a "Resume" button
    2. Updating the bubble UI info if the "pause" status changes
    3. Renaming GetBubbleUIInfoForWarning function since it is more than
    just for warnings.
    
    Fixed: 1324651
    Change-Id: I63638cc678850afba355ead2bb93ef868d6b09e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645434
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007108}

--- chrome/browser/download/bubble/download_bubble_controller.cc
+++ chrome/browser/download/bubble/download_bubble_controller.cc
@@ -381,6 +381,7 @@ void DownloadBubbleUIController::ProcessDownloadButtonPress(
       [[fallthrough]];
     case DownloadCommands::DEEP_SCAN:
     case DownloadCommands::BYPASS_DEEP_SCANNING:
+    case DownloadCommands::RESUME:
       commands.ExecuteCommand(command);
       break;
     default:
