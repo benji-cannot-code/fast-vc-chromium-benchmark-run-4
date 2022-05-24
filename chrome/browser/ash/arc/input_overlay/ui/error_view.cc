20d795e0b5c580f7a01fc423914bd9adc6872e3e
Cici Ruan <cuicuiruan@google.com>
2022-05-24 23:30:24 +0000

    arc/gio: Set shadow for the error text string
    To improve the contrast, set the shadow for the error text message.
    
    Bug: b:232948415 b:188452744
    Test: manual test and unittest.
    Change-Id: I718470eec585b8b0bfa4f83a4508765d2462b995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663233
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007118}

--- chrome/browser/ash/arc/input_overlay/ui/error_view.cc
+++ chrome/browser/ash/arc/input_overlay/ui/error_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/input_overlay/ui/error_view.h"
 
+#include "ash/ambient/util/ambient_util.h"
 #include "chrome/browser/ash/arc/input_overlay/display_overlay_controller.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/background.h"
@@ -36,6 +37,8 @@ ErrorView::ErrorView(DisplayOverlayController* controller,
                             gfx::Font::Weight::MEDIUM));
   SetAutoColorReadabilityEnabled(false);
   SetEnabledColor(kTextColor);
+  // Default shadow elevation value is 2 which is expected.
+  SetShadows(ash::ambient::util::GetTextShadowValues(GetColorProvider()));
 
   auto preferred_size = GetPreferredSize();
   auto content_bounds = view->parent()->bounds();
