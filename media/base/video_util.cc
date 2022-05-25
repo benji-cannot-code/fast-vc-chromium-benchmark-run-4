8babc62a0f0c139ce6192621ad13da3b400e13bf
Eugene Zemtsov <eugene@chromium.org>
2022-05-25 00:11:31 +0000

    webcodecs: Add more tracing for video encoding
    Change-Id: I0d51b86204601983f411bab4607e667e194db76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662211
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007142}

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/numerics/safe_math.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/raster_interface.h"
 #include "media/base/limits.h"
@@ -714,6 +715,8 @@ scoped_refptr<VideoFrame> ReadbackTextureBackedFrameToMemorySync(
     VideoFramePool* pool) {
   DCHECK(ri);
 
+  TRACE_EVENT2("media", "ReadbackTextureBackedFrameToMemorySync", "timestamp",
+               txt_frame.timestamp(), "gr_ctx", !!gr_context);
   VideoPixelFormat format = ReadbackFormat(txt_frame);
   if (format == PIXEL_FORMAT_UNKNOWN) {
     DLOG(ERROR) << "Readback is not possible for this frame: "
