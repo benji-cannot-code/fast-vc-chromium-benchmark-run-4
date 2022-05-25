249b2e6d2a44a84e7a236da2535c57dc27dbf36d
Benjamin Zielinski <bzielinski@google.com>
2022-05-25 20:20:04 +0000

    [Cast2Class] Log count of saved devices
    Add uma histogram for number of saved devices, which gets logged when
    the AccessCodeCastSinkService is constructed, and whenever the network
    changes.
    
    Bug: b:231624891
    Change-Id: I9b8ee230a167aaf01b07687d32040d162b814d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655248
    Reviewed-by: George Benz <gbj@google.com>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007534}

--- components/access_code_cast/common/access_code_cast_metrics.cc
+++ components/access_code_cast/common/access_code_cast_metrics.cc
@@ -23,6 +23,8 @@ const char AccessCodeCastMetrics::kHistogramDialogLoadTime[] =
     "AccessCodeCast.Ui.DialogLoadTime";
 const char AccessCodeCastMetrics::kHistogramDialogOpenLocation[] =
     "AccessCodeCast.Ui.DialogOpenLocation";
+const char AccessCodeCastMetrics::kHistogramRememberedDevicesCount[] =
+    "AccessCodeCast.Discovery.RememberedDevicesCount";
 
 // static
 void AccessCodeCastMetrics::OnCastSessionResult(int route_request_result_code,
@@ -58,4 +60,9 @@ void AccessCodeCastMetrics::RecordDialogLoadTime(base::TimeDelta load_time) {
 void AccessCodeCastMetrics::RecordDialogOpenLocation(
     AccessCodeCastDialogOpenLocation location) {
   base::UmaHistogramEnumeration(kHistogramDialogOpenLocation, location);
+}
+
+// static
+void AccessCodeCastMetrics::RecordRememberedDevicesCount(int count) {
+  base::UmaHistogramCounts100(kHistogramRememberedDevicesCount, count);
 }
