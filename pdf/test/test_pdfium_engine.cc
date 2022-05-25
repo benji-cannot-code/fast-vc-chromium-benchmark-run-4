e9eb9adbf7101198f3f6b4a32ea3f4ecda7984ab
K. Moon <kmoon@chromium.org>
2022-05-25 04:13:47 +0000

    [pdf] Migrate HandleSaveMessage() tests
    Migrates tests exercising PdfViewPluginBase::HandleSaveMessage().
    
    Also splits TestPDFiumEngine::kSaveData into two separate constants to
    distinguish between initially loaded data, and modified data to save.
    
    Bug: 1323307
    Change-Id: I6627f233eee94c56456cbbc77edeb09f28787bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664682
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007224}

--- pdf/test/test_pdfium_engine.cc
+++ pdf/test/test_pdfium_engine.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "pdf/test/test_pdfium_engine.h"
 
+#include <stdint.h>
 #include <string.h>
 
 #include <iterator>
@@ -24,6 +25,9 @@ namespace chrome_pdf {
 
 // static
 const uint32_t TestPDFiumEngine::kPageNumber;
+
+// static
+const uint8_t TestPDFiumEngine::kLoadedData[];
 
 // static
 const uint8_t TestPDFiumEngine::kSaveData[];
@@ -55,12 +59,12 @@ base::Value::List TestPDFiumEngine::GetBookmarks() {
 }
 
 uint32_t TestPDFiumEngine::GetLoadedByteSize() {
-  return sizeof(kSaveData);
+  return sizeof(kLoadedData);
 }
 
 bool TestPDFiumEngine::ReadLoadedBytes(uint32_t length, void* buffer) {
   DCHECK_LE(length, GetLoadedByteSize());
-  memcpy(buffer, kSaveData, length);
+  memcpy(buffer, kLoadedData, length);
   return true;
 }
 
