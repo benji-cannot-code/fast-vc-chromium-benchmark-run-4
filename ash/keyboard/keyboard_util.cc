5b8356959d215aa43f2ccd4bbdc643f7bb9a11b1
Toni Barzic <tbarzic@chromium.org>
2022-05-25 18:21:24 +0000

    Consider Chrome VK state when detecting arc IME presence
    Make virtual_keyboard_model consider Chrome VK state when determining
    whether ARC IME UI is shown, and when a ARC IME specific shelf needs to
    be shown. If Chrome VK is enabled, assume that ARC IME keyboard is not
    showing (even if ARC reports that ARC IME bounds are non-empty), and
    the ARC IME specific shelf does not have to be shown, as in this case
    the shelf will be hidden behind the virtual keyboard (and for floating
    keyboard showing normal shelf UI is preferable).
    This aims to address reports of shelf remaining in ARC IME state after
    showing and hiding accessibility keyboard. It also avoids noticeable
    shelf state transition (where back button is hidden, and status tray
    animates in) after hiding chrome virtual keyboard (if the user has
    ARC windows open).
    
    BUG=1318222
    
    Change-Id: I84f65fe07e5c30a94a1e25d5ae0bf8b5d5056f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656534
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007484}

--- ash/keyboard/keyboard_util.cc
+++ ash/keyboard/keyboard_util.cc
@@ -29,7 +29,10 @@ bool CloseKeyboardIfActive() {
   }
 
   // Close the Android VK if it is visible by sending a back event.
-  if (Shell::Get()->system_tray_model()->virtual_keyboard()->visible()) {
+  if (Shell::Get()
+          ->system_tray_model()
+          ->virtual_keyboard()
+          ->arc_keyboard_visible()) {
     window_util::SendBackKeyEvent(Shell::GetPrimaryRootWindow());
     return true;
   }
