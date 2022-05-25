39fff134fa896b7de24044456d0ad5c0c16aecda
Christian Xu <christianxu@google.com>
2022-05-25 16:29:41 +0000

    [iOS][Omnibox] Add a limit on the max ZPS suggestions on iPad
    By default, iPad has the same max as iPhone.
    `kMaxZeroSuggestMatchesOnIPad` defines a hard limit on the number of ZPS
    suggestions on iPad, so if an experiment defines MaxZeroSuggestMatches
    to 15, it would be 15 on iPhone and 10 on iPad.
    
    Bug: 1326949
    Change-Id: I1c4b9468fb1666b95b91df32ada612c666382b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655479
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007412}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_fixer.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "third_party/metrics_proto/omnibox_input_type.pb.h"
+#include "ui/base/device_form_factor.h"
 #include "ui/base/l10n/l10n_util.h"
 
 using metrics::OmniboxEventProto;
@@ -88,6 +89,11 @@ size_t AutocompleteResult::GetMaxMatches(bool is_zero_suggest) {
 #elif BUILDFLAG(IS_IOS)
   constexpr size_t kDefaultMaxAutocompleteMatches = 6;
   constexpr size_t kDefaultMaxZeroSuggestMatches = 6;
+  // By default, iPad has the same max as iPhone. `kMaxZeroSuggestMatchesOnIPad`
+  // defines a hard limit on the number of ZPS suggestions on iPad, so if an
+  // experiment defines MaxZeroSuggestMatches to 15, it would be 15 on iPhone
+  // and 10 on iPad.
+  constexpr size_t kMaxZeroSuggestMatchesOnIPad = 10;
 #else
   constexpr size_t kDefaultMaxAutocompleteMatches = 8;
   constexpr size_t kDefaultMaxZeroSuggestMatches = 8;
@@ -111,6 +117,11 @@ size_t AutocompleteResult::GetMaxMatches(bool is_zero_suggest) {
         OmniboxFieldTrial::kMaxZeroSuggestMatchesParam,
         kDefaultMaxZeroSuggestMatches);
     DCHECK(kMaxAutocompletePositionValue > field_trial_value);
+#if BUILDFLAG(IS_IOS)
+    if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+      field_trial_value = MIN(field_trial_value, kMaxZeroSuggestMatchesOnIPad);
+    }
+#endif
     return field_trial_value;
   }
 
