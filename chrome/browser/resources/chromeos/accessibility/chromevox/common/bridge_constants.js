892ed52705698520aac395c446c9bf355de986a9
Anastasia Helfinstein <anastasi@google.com>
2022-05-25 21:57:06 +0000

    [ChromeVox] Restructure BridgeConstants for BrailleCommandHandler
    Following the pattern begin in this CL[1], restructure the file to group
    each target with its associated actions.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3645094
    
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I602a58084eaa5b67a4d7f411ca8110b3f5136db9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646257
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007580}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
@@ -14,7 +14,6 @@ goog.provide('BridgeTargets');
 
 /** @enum {string} */
 BridgeTargets = {
-  CHROMEVOX_STATE: 'ChromeVoxState',
   EVENT_STREAM_LOGGER: 'EventStreamLogger',
   LOG_STORE: 'LogStore',
   PANEL: 'Panel',
@@ -67,6 +66,16 @@ BridgeConstants = {
     },
   },
 
+  ChromeVoxState: {
+    /** @public {BridgeTarget} */
+    TARGET: 'ChromeVoxState',
+    /** @enum {string} */
+    Action: {
+      CLEAR_CURRENT_RANGE: 'clearCurrentRange',
+      UPDATE_PUNCTUATION_ECHO: 'updatePunctuationEcho',
+    },
+  },
+
   CommandHandler: {
     /** @public {BridgeTarget} */
     TARGET: 'CommandHandler',
@@ -100,7 +109,6 @@ BridgeConstants = {
  */
 BridgeActions = {
   ADD_MENU_ITEM: 'addMenuItem',
-  CLEAR_CURRENT_RANGE: 'clearCurrentRange',
   CLEAR_LOG: 'clearLog',
   CREATE: 'create',
   CREATE_ALL_NODE_MENU_BACKGROUNDS: 'createAllNodeMenuBackgrounds',
@@ -118,7 +126,6 @@ BridgeActions = {
   PERFORM_CUSTOM_ACTION_ON_CURRENT_NODE: 'performCustomActionOnCurrentNode',
   PERFORM_STANDARD_ACTION_ON_CURRENT_NODE: 'performStandardActionOnCurrentNode',
   SET_RANGE_TO_I_SEARCH_NODE: 'setRangeToISearchNode',
-  UPDATE_PUNCTUATION_ECHO: 'updatePunctuationEcho',
   WAIT_FOR_PANEL_COLLAPSE: 'waitForPanelCollapse',
 };
 
@@ -129,6 +136,7 @@ BridgeActions = {
  *           BridgeConstants.BrailleCommandHandler.Action |
  *           BridgeConstants.ChromeVoxBackground.Action |
  *           BridgeConstants.ChromeVoxPrefs.Action |
+ *           BridgeConstants.ChromeVoxState.Action |
  *           BridgeConstants.CommandHandler.Action |
  *           BridgeConstants.EventSourceState.Action |
  *           BridgeConstants.GestureCommandHandler.Action}
