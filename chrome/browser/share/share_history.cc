db70796531eaf9b97b917e4e17f883465b3edcce
Elly Fong-Jones <ellyjones@chromium.org>
2022-05-25 22:56:05 +0000

    clank-sh: expire history entries older than 90 days
    The original design doc called for this but it was never implemented.
    This change reuses the existing history clearing functionality, which
    already supported clearing history for arbitrary time windows; when
    history is first loaded from disk after each browser start, we erase
    entries older than 90 days so that they won't be written back to disk in
    subsequent history updates.
    
    This change also adds a too-old history entry to the unit test data set,
    which causes the existing tests to all cover history expiration as well.
    
    Fixed: 1321801
    Change-Id: I2f81641ad46d0b8e4f0ae250deab63189f016d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3657192
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007599}

--- chrome/browser/share/share_history.cc
+++ chrome/browser/share/share_history.cc
@@ -36,6 +36,8 @@ const char* const kShareHistoryFolder = "share_history";
 // do not fold these constants together.
 const char* const kShareHistoryKey = "share_history";
 
+constexpr auto kMaxHistoryAge = base::Days(90);
+
 int TodaysDay() {
   return (base::Time::Now() - base::Time::UnixEpoch()).InDays();
 }
@@ -201,7 +203,7 @@ void ShareHistory::OnInitialReadDone(
   init_finished_ = true;
   post_init_callbacks_.Notify();
 
-  // TODO(ellyjones): Expire entries older than WINDOW days.
+  Clear(base::Time(), base::Time::Now() - kMaxHistoryAge);
 }
 
 void ShareHistory::FlushToBackingDb() {
