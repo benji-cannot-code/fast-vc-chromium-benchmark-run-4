84258e1298dc562eeeaa9aa4ec7f0dce78afb0fd
Roman Sorokin <rsorokin@chromium.org>
2022-05-25 08:44:02 +0000

    [OOBE WebUI] Remove cr.ui.Oobe.showAPIKeysNotice
    Control api key notice visibility via localize vars
    
    Bug: 1100910
    Change-Id: Idfc4eeda3e206ccdaf9c3b942f203f07a9d22c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648182
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007291}

--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
@@ -59,6 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 CoreOobeHandler::CoreOobeHandler(const std::string& display_type) {
+  is_oobe_display_ = display_type == OobeUI::kOobeDisplay;
+
   ash::TabletMode::Get()->AddObserver(this);
 
   OobeConfiguration::Get()->AddAndFireObserver(this);
@@ -75,12 +77,6 @@ CoreOobeHandler::CoreOobeHandler(const std::string& display_type) {
 #endif
   UpdateClientAreaSize(
       display::Screen::GetScreen()->GetPrimaryDisplay().size());
-
-  bool has_api_keys_configured = google_apis::HasAPIKeyConfigured() &&
-                                 google_apis::HasOAuthClientConfigured();
-  CallJS("cr.ui.Oobe.showAPIKeysNotice",
-         !has_api_keys_configured && (display_type == OobeUI::kOobeDisplay ||
-                                      display_type == OobeUI::kLoginDisplay));
 
   // Don't show version label on the stable and beta channels by default.
   version_info::Channel channel = chrome::GetChannel();
@@ -114,8 +110,12 @@ void CoreOobeHandler::DeclareLocalizedValues(
   // Strings for Asset Identifier shown in version string.
   builder->Add("assetIdLabel", IDS_OOBE_ASSET_ID_LABEL);
 
-  builder->AddF("missingAPIKeysNotice", IDS_LOGIN_API_KEYS_NOTICE,
-                base::ASCIIToUTF16(google_apis::kAPIKeysDevelopersHowToURL));
+  const bool has_api_keys_configured = google_apis::HasAPIKeyConfigured() &&
+                                       google_apis::HasOAuthClientConfigured();
+  if (!has_api_keys_configured && is_oobe_display_) {
+    builder->AddF("missingAPIKeysNotice", IDS_LOGIN_API_KEYS_NOTICE,
+                  base::ASCIIToUTF16(google_apis::kAPIKeysDevelopersHowToURL));
+  }
 
   builder->Add("playAnimationAriaLabel", IDS_OOBE_PLAY_ANIMATION_MESSAGE);
   builder->Add("pauseAnimationAriaLabel", IDS_OOBE_PAUSE_ANIMATION_MESSAGE);
