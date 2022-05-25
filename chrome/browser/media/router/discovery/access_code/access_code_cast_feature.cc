df7c32c87d83f24d42e5f0c1cbcf10adf84f77ea
George Benz <gbj@google.com>
2022-05-25 23:04:17 +0000

    Enables saved devices feature by default.
    Because of approvals of this feature before feature freeze (https://bugs.chromium.org/p/chromium/issues/detail?id=1324402) -- this feature can now be enabled by default.
    
    Bug: b/233768673,1324402
    Change-Id: I396e553e40e0d524301fa40b2ad2c59217bf50aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664082
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: George Benz <gbj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007614}

--- chrome/browser/media/router/discovery/access_code/access_code_cast_feature.cc
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_feature.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 // Enables remembering of access code cast devices.
 const base::Feature kAccessCodeCastRememberDevices{
-    "AccessCodeCastRememberDevices", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AccessCodeCastRememberDevices", base::FEATURE_ENABLED_BY_DEFAULT};
 }  // namespace features
 
 namespace media_router {
