6c3d6a1007f3582e5c0ddb553b44b1f93f2698ce
Daniel White <danieltwhite@google.com>
2022-05-25 13:21:39 +0000

    Created Push Notification No-Op Provider
    In this CL, the public no-op provider for the push notification
    api was created. This is the first step to supporting push
    notifications on the client side.
    
    Bug: 1327380
    Change-Id: I302dc6501158d2e41c78f75de2d4e1c046213086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661121
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007351}

---
+++ ios/public/provider/chrome/browser/push_notification/push_notification_api.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_API_H_
+
+namespace ios {
+namespace provider {
+
+// Initializes the device to handle push notifications
+void InitializeConfiguration();
+
+// Registers the device with the server to receive push notifications
+void RegisterDevice();
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_API_H_
