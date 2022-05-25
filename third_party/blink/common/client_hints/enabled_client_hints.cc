1a42cb4f9593733406ae42b6f0fc1c1b3df871cc
Dylan Cutler <dylancutler@google.com>
2022-05-25 22:57:33 +0000

    Reland "Implement new design for CHIPS OT and delete old design"
    This is a reland of commit 5f95d4816cc8e4ddd0bc33237d99ef1ca9d0fca3
    
    Original change's description:
    > Implement new design for CHIPS OT and delete old design
    >
    > This CL deletes the client hint-based origin trial design which I
    > landed in https://crrev.com/c/3453430.
    >
    > It also implements the new origin trial design announced here:
    > https://github.com/GoogleChromeLabs/privacy-sandbox-dev-support/discussions/33
    >
    > The new origin trial design only checks the Origin-Trial header when an
    > HTTP response has a Set-Cookie header with a Partitioned attribute in
    > the cookie line. The resulting cookie from the Set-Cookie with
    > Partitioned does NOT have to be valid.
    >
    > In order to do this, I added a new field, has_partitioned_cookie, to
    > URLResponseHead. This bit indicates to the browser/renderer that the
    > response had a Set-Cookie header with Partitioned.
    >
    > The network service has no idea if the site is in the OT at request
    > time, so we will set any cookie with the Partitioned attribute as
    > partitioned even if the site is not in the OT.
    >
    > Afterwards, we then check the Origin-Trial header and token in the
    > browser for navigation requests and the renderer for subresource
    > requests. If a site is not a valid OT participant, we convert that
    > URL's partitioned cookies to unpartitioned.
    >
    > In order to do this in the renderer, we had to give
    > RestrictedCookieManager the ability to change any URL's partitioned
    > cookies to unpartitioned. Since unpartitioned cookies are already the
    > state of the art for the web, it's low risk to give renderers this
    > ability. RCM's ability to do this will be temporary and only for the
    > duration of the OT.
    >
    > The entire feature is tested end-to-end in
    > navigation_request_browsertest.cc for now.
    >
    > Bug: 1296161,1327045
    > Change-Id: I08490a7df4cbb93e121034d67b88ce4a69f32c37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650844
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Dylan Cutler <dylancutler@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1007159}
    
    Bug: 1296161,1327045
    Change-Id: I1f73a4ff04121d14a2ae8eb91f3f8e3aed9068f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666357
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007601}

--- third_party/blink/common/client_hints/enabled_client_hints.cc
+++ third_party/blink/common/client_hints/enabled_client_hints.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/time/time.h"
-#include "net/base/features.h"
 #include "net/http/http_response_headers.h"
 #include "services/network/public/cpp/client_hints.h"
 #include "third_party/blink/public/common/features.h"
@@ -86,10 +85,6 @@ bool IsDisabledByFeature(const WebClientHintsType type) {
               features::kClientHintsViewportWidth_DEPRECATED))
         return true;
       break;
-    case WebClientHintsType::kPartitionedCookies:
-      if (!base::FeatureList::IsEnabled(net::features::kPartitionedCookies))
-        return true;
-      break;
     case WebClientHintsType::kSaveData:
       if (!base::FeatureList::IsEnabled(features::kClientHintsSaveData))
         return true;
@@ -160,10 +155,6 @@ void EnabledClientHints::SetIsEnabled(
   if (enabled && type == WebClientHintsType::kFullUserAgent) {
     enabled = IsOriginTrialEnabled(url, third_party_url, response_headers,
                                    "SendFullUserAgentAfterReduction");
-  }
-  if (enabled && type == WebClientHintsType::kPartitionedCookies) {
-    enabled = IsOriginTrialEnabled(url, third_party_url, response_headers,
-                                   "PartitionedCookies");
   }
   SetIsEnabled(type, enabled);
 }
