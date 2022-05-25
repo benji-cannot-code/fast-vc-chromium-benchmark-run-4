507d0b868df8a3aeefe92cd275a0ab6703c04d99
Bartek Nowierski <bartekn@chromium.org>
2022-05-25 17:44:33 +0000

    [PA] Move TerminateBecauseOutOfMemory into PA
    Move related globals g_oom_size and kOomExceptionCode as well.
    
    This migration CL does things a little differently. Normally we copy
    //base files into .../partition_alloc_base/, but we can't do this with
    TerminateBecauseOutOfMemory, which uses g_oom_size, which can be defined
    only in one place.
    
    cl/450716134 adds the new OOM signature.
    
    Bug: 1151236
    Change-Id: I4a78bd9f90de69c8aa319aaa87c0137f775bfba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635217
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007458}

--- base/allocator/partition_allocator/partition_alloc_base/migration_adapter.h
+++ base/allocator/partition_allocator/partition_alloc_base/migration_adapter.h
@@ -26,8 +26,6 @@ class LazyInstance;
 template <typename Type>
 struct LazyInstanceTraitsBase;
 
-BASE_EXPORT void TerminateBecauseOutOfMemory(size_t size);
-
 #if BUILDFLAG(IS_ANDROID)
 template <typename CharT, typename Traits>
 class BasicStringPiece;
@@ -68,7 +66,6 @@ namespace partition_alloc::internal::base {
 using ::base::LapTimer;
 using ::base::LazyInstance;
 using ::base::LazyInstanceTraitsBase;
-using ::base::TerminateBecauseOutOfMemory;
 
 #if BUILDFLAG(IS_MAC)
 template <typename CFT>
