67b8b548682020e41a18646e45efd7bc083812f2
Rebekah Potter <rbpotter@chromium.org>
2022-05-25 21:01:19 +0000

    chrome://policy: Remove use of deprecated cr/ui.m.js
    Migrate to Web Components, instead of the outdated cr.ui.define() and
    related methods.
    
    Since this UI is also served on iOS, this CL ports the
    ShouldReplaceI18nInJS/EnableReplaceI18nInJS methods from Desktop to
    iOS. This is done to enable Web Components to work on iOS, since the
    HTML for custom elements, including $i18n{} expressions, is inlined in
    the JS file that is served at runtime.
    
    Bug: 1322977
    Change-Id: Ide884bde405709c73cde9312ebc7da1e1bad8b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648387
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007557}

--- ios/web/webui/url_data_source_ios.mm
+++ ios/web/webui/url_data_source_ios.mm
@@ -40,4 +40,8 @@ bool URLDataSourceIOS::ShouldServiceRequest(const GURL& url) const {
   return GetWebClient()->IsAppSpecificURL(url);
 }
 
+bool URLDataSourceIOS::ShouldReplaceI18nInJS() const {
+  return false;
+}
+
 }  // namespace web
