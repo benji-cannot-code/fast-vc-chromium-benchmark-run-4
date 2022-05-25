cdd62a13a985433915981ada45c2a0817cf4b383
Ian Vollick <vollick@chromium.org>
2022-05-25 18:04:44 +0000

    [mparch] Convert more incorrect IsCrossOriginToMainFrame usages
    With MPArch we can have a nested main frame, so for cases when we want
    to check that an embedded frame is cross-origin with respect to the
    outermost main frame, we should use IsCrossOriginToOutermostMainFrame.
    This CL changes a number of cases where the semantics appear to match
    outermost. I've also added some TODOs for cases where the code is
    currently correct for fenced frames, but would be incorrect for
    embedded portals.
    
    TODOs have been added for test coverage of these changes, similar to the
    approach taken in crrev.com/c/2988476 and crrev.com/c/3577677.
    
    Bug: 1318055
    Change-Id: Id12826ed70cb4ddeb653c8a351f1756d04ba2634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664079
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007476}

--- third_party/blink/renderer/modules/vibration/vibration_controller.cc
+++ third_party/blink/renderer/modules/vibration/vibration_controller.cc
@@ -84,6 +84,7 @@ void CollectHistogramMetrics(LocalDOMWindow* window) {
   if (window->GetFrame()->IsInFencedFrameTree()) {
     type = NavigatorVibrationType::kInFencedFrameTree;
   } else if (!window->GetFrame()->IsMainFrame()) {
+    // TODO(crbug.com/1254770): Update for embedded portals.
     UseCounter::Count(window, WebFeature::kNavigatorVibrateSubFrame);
     if (window->GetFrame()->IsCrossOriginToMainFrame()) {
       if (user_gesture)
@@ -190,6 +191,7 @@ bool VibrationController::Vibrate(const VibrationPattern& pattern) {
 
   if (!frame->HasStickyUserActivation()) {
     String message;
+    // TODO(crbug.com/1254770): Update for embedded portals.
     if (frame->IsCrossOriginToMainFrame()) {
       message =
           "Blocked call to navigator.vibrate inside a cross-origin "
