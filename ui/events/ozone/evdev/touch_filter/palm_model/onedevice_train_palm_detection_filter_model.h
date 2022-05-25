7e2d211761020165150f8aae772757775c73338f
Jing Wang <jiwan@chromium.org>
2022-05-25 01:43:19 +0000

    nnpalm: add palm rejection beta model
    Bug: b:228271213
    Change-Id: I900c80963e1e402d901a1cbbf9878f096577a555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3648810
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007184}

--- ui/events/ozone/evdev/touch_filter/palm_model/onedevice_train_palm_detection_filter_model.h
+++ ui/events/ozone/evdev/touch_filter/palm_model/onedevice_train_palm_detection_filter_model.h
@@ -19,7 +19,6 @@ namespace ui {
 class COMPONENT_EXPORT(EVDEV) OneDeviceTrainNeuralStylusPalmDetectionFilterModel
     : public NeuralStylusPalmDetectionFilterModel {
  public:
-  OneDeviceTrainNeuralStylusPalmDetectionFilterModel();
   explicit OneDeviceTrainNeuralStylusPalmDetectionFilterModel(
       const std::string& model_version,
       const std::vector<float>& radius_poly);
@@ -34,6 +33,7 @@ class COMPONENT_EXPORT(EVDEV) OneDeviceTrainNeuralStylusPalmDetectionFilterModel
   const NeuralStylusPalmDetectionFilterModelConfig& config() const override;
 
  private:
+  void Initialize();
   NeuralStylusPalmDetectionFilterModelConfig config_;
   size_t expected_feature_size_;
 };
