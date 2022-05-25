d749d54bad1da11d3bc79d4a120f47e4382f77ea
James Ye <jamesye@google.com>
2022-05-25 01:54:21 +0000

    Add BruschettaFeatures and BruschettaUtil
    Include a stub IsEnabled determined by the bruschetta flag.
    
    Bug: b:223728220
    Change-Id: I184e1d15f0e5ec006753623538eb4aaf3153adde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640909
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: James Ye <jamesye@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007187}

---
+++ chrome/browser/ash/bruschetta/bruschetta_util.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_BRUSCHETTA_BRUSCHETTA_UTIL_H_
+#define CHROME_BROWSER_ASH_BRUSCHETTA_BRUSCHETTA_UTIL_H_
+
+namespace bruschetta {
+
+extern const char kBruschettaVmName[];
+
+}
+
+#endif  // CHROME_BROWSER_ASH_BRUSCHETTA_BRUSCHETTA_UTIL_H_
