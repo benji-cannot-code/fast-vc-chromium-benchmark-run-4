bff51a9c1e35a8e26cc2f49a494ccbc9d9dacc45
Ken Rockot <rockot@google.com>
2022-05-25 00:16:04 +0000

    ipcz: Propagate node disconnection to routers
    Implements node disconnection propagation. When a NodeLink is severed,
    it will now notify all Routers to which it has a bound sublink. Routers
    treat this as equivalent to route closure from the appropriate end of
    the route, if that end was not already closed.
    
    Bug: 1299283
    Change-Id: I47c1a838173e354a9421278c50ff5b5b37fdbdc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3659408
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007148}

--- third_party/ipcz/src/ipcz/router_link.h
+++ third_party/ipcz/src/ipcz/router_link.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ipcz/link_type.h"
 #include "ipcz/sequence_number.h"
+#include "ipcz/sublink_id.h"
 #include "util/ref_counted.h"
 
 namespace ipcz {
 
+class NodeLink;
 class Parcel;
 class Router;
 
@@ -32,6 +34,10 @@ class RouterLink : public RefCounted {
 
   // Returns true iff this is a LocalRouterLink whose peer router is `router`.
   virtual bool HasLocalPeer(const Router& router) = 0;
+
+  // Returns true iff this is a RemoteRouterLink routing over `node_link` via
+  // `sublink`.
+  virtual bool IsRemoteLinkTo(const NodeLink& node_link, SublinkId sublink) = 0;
 
   // Passes a parcel to the Router on the other side of this link to be queued
   // and/or router further.
