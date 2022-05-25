8ecc93ec7f714ee6fca248d190e393c6226beefe
Cheick Cisse <cheickcisse@google.com>
2022-05-25 19:52:20 +0000

    [Notes] Create DB with schema.
    Implement initializing "UserNotes" DB which will create the scheme if
    needed.
    
    Bug: 1307492
    Change-Id: I52b6cccb1be49d062a19ee97d16544eede4c5445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546500
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007523}

--- components/user_notes/storage/user_note_database.h
+++ components/user_notes/storage/user_note_database.h
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace user_notes {
 
+constexpr base::FilePath::CharType kDatabaseName[] =
+    FILE_PATH_LITERAL("UserNotes.db");
+
 // Provides the backend SQLite support for user notes.
 // This class must be used on a same blocking sequence.
 class UserNoteDatabase {
@@ -28,7 +31,7 @@ class UserNoteDatabase {
   ~UserNoteDatabase();
 
   // Initialises internal database. Must be called prior to any other usage.
-  void Init();
+  bool Init();
 
   UserNoteMetadataSnapshot GetNoteMetadataForUrls(std::vector<GURL> urls);
 
@@ -53,7 +56,16 @@ class UserNoteDatabase {
   void DeleteAllNotes();
 
  private:
+  // Called by the database to report errors.
+  void DatabaseErrorCallback(int error, sql::Statement* stmt);
+
+  // Creates or migrates to the new schema if needed.
+  bool InitSchema();
+
+  bool CreateSchema();
+
   sql::Database db_ GUARDED_BY_CONTEXT(sequence_checker_);
+
   const base::FilePath db_file_path_;
 
   SEQUENCE_CHECKER(sequence_checker_);
