c95883a3357101e9f850bf1da4470e3edf026c25
Minoru Chikamune <chikamune@chromium.org>
2022-05-25 09:16:09 +0000

    Add UMA for script scheduling type
    This is relanding CL of https://crrev.com/c/1116508.
    
    This CL adds the following entries:
     - Blink.Script.SchedulingType
    
    Bug: 1328656
    Change-Id: I1e603a10771a43e40734de474fc6a9382b677dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662289
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007299}

--- third_party/blink/renderer/core/script/script_loader.cc
+++ third_party/blink/renderer/core/script/script_loader.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/script/script_loader.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/permissions_policy/permissions_policy.h"
@@ -68,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
 #include "third_party/blink/renderer/platform/bindings/parkable_string.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_client_settings_object_snapshot.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
@@ -1114,6 +1114,12 @@ void ScriptLoader::FetchModuleScriptTree(
 PendingScript* ScriptLoader::TakePendingScript(
     ScriptSchedulingType scheduling_type) {
   CHECK(prepared_pending_script_);
+
+  // Record usage histograms per script tag.
+  if (element_->GetDocument().Url().ProtocolIsInHTTPFamily()) {
+    base::UmaHistogramEnumeration("Blink.Script.SchedulingType",
+                                  scheduling_type);
+  }
 
   PendingScript* pending_script = prepared_pending_script_;
   prepared_pending_script_ = nullptr;
