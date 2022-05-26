7d88ed656d9690928885976d4bef354b7826a1f2
Timothy Loh <timloh@chromium.org>
2022-05-26 08:41:43 +0000

    Revert "ime: Change assistive windows to track anchor with padding offset."
    This reverts commit 38e670abdadfcc71d124d311a0da3666ae110010.
    
    Reason for revert: inputs.PhysicalKeyboardEmojiSuggestion failing after https://crrev.com/c/3661862, this landed on top of that CL so need to revert this too.
    
    Original change's description:
    > ime: Change assistive windows to track anchor with padding offset.
    >
    > Previously, assistive windows were placed directly below the anchor
    > (e.g. caret position). But the UX specs have changed so that the
    > window should be shifted leftwards by the padding amount.
    >
    > See http://b/233264555#comment2 for before/after images.
    >
    > Bug: b/233264555
    > Change-Id: If05015ed2b4a610561ea7e6ea8992b683768e738
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664160
    > Reviewed-by: jhtin <jhtin@chromium.org>
    > Commit-Queue: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1007724}
    
    Bug: b/233264555, 1329436
    Change-Id: Ic600fbbc1efb4e8bea4727fe7e7c0abfe874ab54
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669932
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Owners-Override: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007771}

--- chrome/browser/ash/input_method/ui/suggestion_window_view.h
+++ chrome/browser/ash/input_method/ui/suggestion_window_view.h
@@ -78,7 +78,6 @@ class UI_CHROMEOS_EXPORT SuggestionWindowView
 
  protected:
   // views::BubbleDialogDelegateView:
-  gfx::Rect GetBubbleBounds() override;
   void OnThemeChanged() override;
 
  private:
