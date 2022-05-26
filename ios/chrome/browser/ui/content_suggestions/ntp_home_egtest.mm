8fc7e2b0d540911f9d0b387dbf995336cf7c15a4
Joemer Ramos <joemerramos@chromium.org>
2022-05-26 18:34:49 +0000

    [iOS] Fix for NTP test failure on iOS 15 SDK simulator bot
    testTapFakeOmniboxScroll and
    NTPHomeTestCase/testTapFakeOmniboxScrollScrolled were failing on an
    iPhone X because the test tries to bring the fake omnibox out of focus
    but doesn't tap into the proper area to "unfocus" the omnibox.
    We have changed logic so that we tap the "dismiss keyboard" button
    instead to unfocus the omnibox.
    
    Bug: 1326523, 1326875
    Change-Id: Ic855ec038002c02130cc75cd3b0213a57da7cc6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652858
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007916}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -627,8 +627,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
                  @"The collection has not moved.");
 
   // Unfocus the omnibox.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::NTPCollectionView()]
-      performAction:grey_tapAtPoint(CGPointMake(0, offsetAfterTap.y + 100))];
+  [self unfocusFakeBox];
 
   // Check the fake omnibox is displayed again at the same position.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
@@ -662,9 +661,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
   [self focusFakebox];
 
   // Unfocus the omnibox.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::NTPCollectionView()]
-      performAction:grey_tapAtPoint(
-                        CGPointMake(0, collectionView.contentOffset.y + 100))];
+  [self unfocusFakeBox];
 
   // Check the fake omnibox is displayed again at the same position.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
@@ -1059,6 +1056,20 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
       performAction:grey_tap()];
   [ChromeEarlGrey
       waitForSufficientlyVisibleElementWithMatcher:chrome_test_util::Omnibox()];
+}
+
+// Unfocus the omnibox.
+- (void)unfocusFakeBox {
+  if ([ChromeEarlGrey isIPadIdiom]) {
+    [ChromeEarlGrey simulatePhysicalKeyboardEvent:UIKeyInputEscape flags:0];
+  } else {
+    id<GREYMatcher> cancelButton =
+        grey_accessibilityID(kToolbarCancelOmniboxEditButtonIdentifier);
+    [[EarlGrey
+        selectElementWithMatcher:grey_allOf(cancelButton,
+                                            grey_sufficientlyVisible(), nil)]
+        performAction:grey_tap()];
+  }
 }
 
 - (void)testNTPInitialPositionAndContent:(UICollectionView*)collectionView {
