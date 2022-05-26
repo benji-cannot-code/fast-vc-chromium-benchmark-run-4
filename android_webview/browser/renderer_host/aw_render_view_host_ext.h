b5e28c652977d31d4e7b29cc8a4c1e1f507186e8
John Abd-El-Malek <jam@chromium.org>
2022-05-26 19:07:29 +0000

    Remove unnecessary post task from UI thread to itself for shouldOverrideUrlLoading implementation.
    The renderer is blocked on this sync call so this adds an unnecessary delay. Also it's not safe to pass WebContents* in a task since it could be deleted.
    
    Change-Id: I11c3f4d1bdccf93f11545cf71ef8913caa76b100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668765
    Reviewed-by: Richard Coles <torne@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007935}

--- android_webview/browser/renderer_host/aw_render_view_host_ext.h
+++ android_webview/browser/renderer_host/aw_render_view_host_ext.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/common/mojom/frame.mojom.h"
 #include "base/callback_forward.h"
-#include "base/sequence_checker.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/render_frame_host_receiver_set.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
@@ -125,8 +124,6 @@ class AwRenderViewHostExt : public content::WebContentsObserver,
 
   // Associated channel to the webview LocalMainFrame extensions.
   mojo::AssociatedRemote<mojom::LocalMainFrame> local_main_frame_remote_;
-
-  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace android_webview
