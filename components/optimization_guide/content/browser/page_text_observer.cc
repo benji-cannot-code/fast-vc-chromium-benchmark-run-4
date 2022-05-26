3c8d0693c42f8ffd58967a60a337c4c4c08af95b
Miyoung Shin <myid.shin@igalia.com>
2022-05-26 07:00:22 +0000

    [mparch] Do not create a request of OOPIF AMP Subframes in PageTextObserver
    PageTextObserver class doesn't handle MPArch's pages correctly. It
    ignores a fenced frame's main frame but includes text from
    subframes of the fenced frame. It also has a similar problem in
    the prendering page.
    
    This CL changes not to create the request of OOPIF AMP frames in
    prerendering pages and fenced frames to consistent behavior,
    and adds unit tests to ensure not to call its consumers.
    
    Bug: 1327669
    Change-Id: I2a36a118e520bd776cce6c71b137556e22f28130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3657333
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007750}

--- components/optimization_guide/content/browser/page_text_observer.cc
+++ components/optimization_guide/content/browser/page_text_observer.cc
@@ -374,12 +374,11 @@ void PageTextObserver::DidFinishNavigation(content::NavigationHandle* handle) {
 }
 
 bool PageTextObserver::IsOOPIF(content::RenderFrameHost* rfh) const {
-  return rfh->GetProcess()->GetID() !=
-         rfh->GetMainFrame()->GetProcess()->GetID();
+  return rfh->IsCrossProcessSubframe();
 }
 
 void PageTextObserver::RenderFrameCreated(content::RenderFrameHost* rfh) {
-  if (!IsOOPIF(rfh)) {
+  if (!IsOOPIF(rfh) || !rfh->GetPage().IsPrimary()) {
     return;
   }
 
