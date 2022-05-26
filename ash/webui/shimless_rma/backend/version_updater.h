b436ad89b8ecd7ac795c03c3e6aa2cde2468aadc
wenyu zhang <zhangwenyu@google.com>
2022-05-26 19:25:14 +0000

    Shimless: do not skip NetworkPage if necessary
    When a user has seen the NetworkPage when going forward, user
    should be back to NetworkPage from the next page. See other user
    flow in: go/shimless-rma-back-to-network-page.
    
    This CL also fixes the bug that sometimes the back and exit button
    in NetworkPage and UpdateOsPage will disappear.
    
    Change-Id: Ied373b3e6c438b3bd612629906cb36ad5fe5c322
    Test: Unit tests, Manual tests in ShimlessRMA from VM.
    Bug: 1198187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663799
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007945}

--- ash/webui/shimless_rma/backend/version_updater.h
+++ ash/webui/shimless_rma/backend/version_updater.h
@@ -41,6 +41,7 @@ class VersionUpdater : public chromeos::UpdateEngineClient::Observer {
   bool IsUpdateEngineIdle();
 
   void UpdateStatusChangedForTesting(const update_engine::StatusResult& status);
+  void DisableUpdateOnceForTesting();
 
  private:
   // Callback from UpdateEngineClient::RequestUpdateCheck().
@@ -59,6 +60,7 @@ class VersionUpdater : public chromeos::UpdateEngineClient::Observer {
   };
   CheckUpdateState check_update_available_ = IDLE;
   std::string new_version_;
+  bool disable_update_for_testing_ = false;
 
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
