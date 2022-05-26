f2bb6482120314b8b829536a798764d13130314f
Gavin Williams <gavinwill@chromium.org>
2022-05-26 17:29:53 +0000

    Shimless: Show provisioning spinner by default
    Bug: 1198187
    Change-Id: I44384aaad1ac43b5c1c9c2fa6819d6951ca3cc2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668932
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Auto-Submit: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007895}

--- ash/webui/shimless_rma/resources/reimaging_provisioning_page.js
+++ ash/webui/shimless_rma/resources/reimaging_provisioning_page.js
@@ -57,7 +57,7 @@ export class ReimagingProvisioningPage extends ReimagingProvisioningPageBase {
       /** @protected {boolean} */
       shouldShowSpinner_: {
         type: Boolean,
-        value: false,
+        value: true,
       },
     };
   }
