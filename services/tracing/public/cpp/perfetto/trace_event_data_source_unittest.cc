3e0ddc8f1cee4603ffe44a2b30d26700ba737a9e
Mikhail Khokhlov <khokhlov@google.com>
2022-05-26 12:42:13 +0000

    [Perfetto SDK] Fix tracing service tests
    The thread time sampling has been enabled by default, so we should now
    expect one more packet in every trace.
    
    Bug: b/233885137
    Cq-Include-Trybots: luci.chromium.try:linux-perfetto-rel
    Change-Id: Ie54264f6b92e0047cb17450aff4db5d5f2eeafff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666726
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007803}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -458,7 +458,10 @@ class TraceEventDataSourceTest
 
     EXPECT_EQ(packet->track_descriptor().counter().type(),
               perfetto::protos::CounterDescriptor::COUNTER_THREAD_TIME_NS);
+    // TODO(mohitms): Support/enable microsecond thread time counters.
+#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
     EXPECT_EQ(packet->track_descriptor().counter().unit_multiplier(), 1000u);
+#endif
   }
 
   void ExpectProcessTrack(const perfetto::protos::TracePacket* packet,
@@ -525,13 +528,10 @@ class TraceEventDataSourceTest
     ExpectThreadTrack(tt_packet, /*thread_id=*/0, /*min_timestamp=*/1u,
                       privacy_filtering_enabled);
 
-#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
-    // TODO(skyostil): Add support for thread time.
     if (base::ThreadTicks::IsSupported()) {
       auto* ttt_packet = GetFinalizedPacket(packet_index++);
       ExpectThreadTimeCounterTrack(ttt_packet);
     }
-#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
     return packet_index;
   }
