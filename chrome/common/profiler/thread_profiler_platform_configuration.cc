33f0014138a66e458f2c40b52822ea2700452759
Tushar Agarwal <agarwaltushar@google.com>
2022-05-26 10:14:44 +0000

    Update `thread_profiler_browsertest.cc` to only exercise platform-specific tests when appropriate.
      * Currently, this test fails on HEAD for Android. This changes fixes this.
    
      * Also, guard `AndroidPlatformConfiguration` more "accurately", since
          the profiler is supported only on Arm 32.
    
    Change-Id: Id9494f90a17a0ad8885ad11448652f53064bac5e
    Bug: 1326430,1327335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666484
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1007784}

--- chrome/common/profiler/thread_profiler_platform_configuration.cc
+++ chrome/common/profiler/thread_profiler_platform_configuration.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/common/profiler/process_type.h"
 
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_ARMEL)
 #include "chrome/android/modules/stack_unwinder/public/module.h"
 #endif
 
@@ -113,7 +113,7 @@ bool DefaultPlatformConfiguration::IsSupportedForChannel(
          *release_channel == version_info::Channel::DEV;
 }
 
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_ARMEL)
 // The configuration to use for the Android platform. Applies to ARM32 which is
 // the only Android architecture currently supported by StackSamplingProfiler.
 // Defined in terms of DefaultPlatformConfiguration where Android does not
@@ -223,8 +223,7 @@ bool AndroidPlatformConfiguration::IsEnabledForThread(
     return true;
   }
 
-  // Otherwise enable in dedicated ThreadProfiler browser tests.
-  return browser_test_mode_enabled();
+  return false;
 }
 
 bool AndroidPlatformConfiguration::IsSupportedForChannel(
@@ -237,14 +236,14 @@ bool AndroidPlatformConfiguration::IsSupportedForChannel(
   // TODO(https://crbug.com/1004855): Enable across all browser tests.
   return browser_test_mode_enabled();
 }
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_ARMEL)
 
 }  // namespace
 
 // static
 std::unique_ptr<ThreadProfilerPlatformConfiguration>
 ThreadProfilerPlatformConfiguration::Create(bool browser_test_mode_enabled) {
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_ARMEL)
   using PlatformConfiguration = AndroidPlatformConfiguration;
 #else
   using PlatformConfiguration = DefaultPlatformConfiguration;
