3e4a064bb0329ec61983b62147d0b56994017187
Kenichi Ishibashi <bashi@chromium.org>
2022-05-26 01:16:14 +0000

    Add more traces in HttpCache::Transaction
    To improve observability of HTTP cache transaction.
    
    Bug: N/A
    Change-Id: Ia977e26590925ed42529b6386a62cefe35848139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668406
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007678}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -641,6 +641,10 @@ void HttpCache::Transaction::SetValidatingCannotProceed() {
 }
 
 void HttpCache::Transaction::WriterAboutToBeRemovedFromEntry(int result) {
+  TRACE_EVENT_WITH_FLOW0(
+      "io", "HttpCacheTransaction::WriterAboutToBeRemovedFromEntry",
+      net_log().source().id,
+      TRACE_EVENT_FLAG_FLOW_IN | TRACE_EVENT_FLAG_FLOW_OUT);
   // Since the transaction can no longer access the network transaction, save
   // all network related info now.
   if (moved_network_transaction_to_writers_ &&
@@ -659,6 +663,10 @@ void HttpCache::Transaction::WriterAboutToBeRemovedFromEntry(int result) {
 }
 
 void HttpCache::Transaction::WriteModeTransactionAboutToBecomeReader() {
+  TRACE_EVENT_WITH_FLOW0(
+      "io", "HttpCacheTransaction::WriteModeTransactionAboutToBecomeReader",
+      net_log().source().id,
+      TRACE_EVENT_FLAG_FLOW_IN | TRACE_EVENT_FLAG_FLOW_OUT);
   mode_ = READ;
   if (moved_network_transaction_to_writers_ &&
       entry_->writers->network_transaction()) {
@@ -2243,6 +2251,9 @@ int HttpCache::Transaction::DoHeadersPhaseCannotProceed(int result) {
 }
 
 int HttpCache::Transaction::DoFinishHeaders(int result) {
+  TRACE_EVENT_WITH_FLOW0("io", "HttpCacheTransaction::DoFinishHeaders",
+                         net_log().source().id,
+                         TRACE_EVENT_FLAG_FLOW_IN | TRACE_EVENT_FLAG_FLOW_OUT);
   if (!cache_.get() || !entry_ || result != OK) {
     TransitionToState(STATE_NONE);
     return result;
@@ -2273,6 +2284,9 @@ int HttpCache::Transaction::DoFinishHeaders(int result) {
 }
 
 int HttpCache::Transaction::DoFinishHeadersComplete(int rv) {
+  TRACE_EVENT_WITH_FLOW0("io", "HttpCacheTransaction::DoFinishHeadersComplete",
+                         net_log().source().id,
+                         TRACE_EVENT_FLAG_FLOW_IN | TRACE_EVENT_FLAG_FLOW_OUT);
   entry_lock_waiting_since_ = TimeTicks();
   if (rv == ERR_CACHE_RACE || rv == ERR_CACHE_LOCK_TIMEOUT) {
     TransitionToState(STATE_HEADERS_PHASE_CANNOT_PROCEED);
