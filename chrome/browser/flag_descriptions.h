532ef843237b3e4e8e10400054c985c1d354f5e2
James Cook <jamescook@chromium.org>
2022-05-26 00:13:55 +0000

    Add about:flags entry for "hide continue section" feature
    We disabled the feature kLauncherHideContinueSection due to
    crbug.com/1329227. Add an entry for it to about:flags to make it
    easier to turn back on for manual testing.
    
    Bug: 1329291
    Test: unit_tests
    Change-Id: If6e34f08cbc5ffd4bf726b9695779e4258451aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669720
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007645}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3171,6 +3171,9 @@ extern const char kLauncherGameSearchDescription[];
 extern const char kLauncherFeedbackOnContinueSectionRemoveName[];
 extern const char kLauncherFeedbackOnContinueSectionRemoveDescription[];
 
+extern const char kLauncherHideContinueSectionName[];
+extern const char kLauncherHideContinueSectionDescription[];
+
 extern const char kLauncherNudgeName[];
 extern const char kLauncherNudgeDescription[];
 
