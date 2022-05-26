91e3749628d2328f1733171950338102dcb13a03
Megan Jablonski <megjablon@google.com>
2022-05-26 19:08:50 +0000

    Languages do not need to be on the Accept Language list to be blocked
    This fixes a bug where the translate UI shows that a language is
    blocked, but we still translate.
    
    Bug: 1311120
    Change-Id: Ia44449a0ad55c485ad12d467335185e9fe817212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669787
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007936}

--- components/translate/core/browser/translate_manager.cc
+++ components/translate/core/browser/translate_manager.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "components/language/core/browser/accept_languages_service.h"
 #include "components/language/core/browser/language_model.h"
 #include "components/language/core/common/language_experiments.h"
 #include "components/language/core/common/language_util.h"
@@ -996,11 +995,8 @@ void TranslateManager::FilterForUserPrefs(
     TranslateTriggerDecision* decision,
     TranslatePrefs* translate_prefs,
     const std::string& page_language_code) {
-  language::AcceptLanguagesService* accept_languages =
-      translate_client_->GetAcceptLanguagesService();
   // Don't translate any user blocklisted languages.
-  if (!translate_prefs->CanTranslateLanguage(accept_languages,
-                                             page_language_code)) {
+  if (!translate_prefs->CanTranslateLanguage(page_language_code)) {
     decision->SetIsInLanguageBlocklist();
 
     decision->PreventAutoTranslate();
