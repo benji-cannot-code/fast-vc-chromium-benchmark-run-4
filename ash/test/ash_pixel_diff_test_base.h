88acdce97336410fd0de3fa04574e316d4c4a353
Andrew Xu <andrewxu@chromium.org>
2022-05-26 18:12:34 +0000

    [Pixel Diff Ash] Set wallpaper in the test base class
    This CL sets a pure color wallpaper so that the screenshots taken in
    ash pixel tests are stable. The screenshots are attached under the
    crbug.
    
    Bug: 1329321
    Change-Id: Iba6bf377c69f144d75ddecb37509267b76780d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668780
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007910}

--- ash/test/ash_pixel_diff_test_base.h
+++ ash/test/ash_pixel_diff_test_base.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_TEST_ASH_PIXEL_DIFF_TEST_BASE_H_
 
 #include "ash/test/ash_test_base.h"
+#include "ash/wallpaper/test_wallpaper_controller_client.h"
+#include "base/files/scoped_temp_dir.h"
 
 namespace ash {
 
@@ -35,6 +37,19 @@ class AshPixelDiffTestBase : public AshTestBase {
 
   // AshTestBase:
   void SetUp() override;
+
+ private:
+  // Sets a pure color wallpaper.
+  void SetWallPaper();
+
+  const AccountId kAccountId_;
+
+  // The temporary data directories for wallpaper setting.
+  base::ScopedTempDir user_data_dir_;
+  base::ScopedTempDir online_wallpaper_dir_;
+  base::ScopedTempDir custom_wallpaper_dir_;
+
+  TestWallpaperControllerClient client_;
 };
 
 }  // namespace ash
