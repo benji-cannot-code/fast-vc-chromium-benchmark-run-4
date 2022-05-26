fb2662d6e0a9a865089adda1b231acc921b01da5
Jianbing <jianbing@google.com>
2022-05-26 20:06:32 +0000

    Toggling off Camera Roll on the phone brings up notification opt-in view on the Phone Hub bubble
    Uupdate opt-in view description if setup mode changed.
    Resize the opt-in view.
    
    Change-Id: Idb7c9c1b353f0febf092b456b08102df09191a41
    Bug: b/233089624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656339
    Commit-Queue: Jianbing Wu <jianbing@google.com>
    Auto-Submit: Jianbing Wu <jianbing@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007961}

--- ash/system/phonehub/sub_feature_opt_in_view.h
+++ ash/system/phonehub/sub_feature_opt_in_view.h
@@ -32,6 +32,7 @@ class ASH_EXPORT SubFeatureOptInView : public views::View {
   SubFeatureOptInView(PhoneHubViewID view_id,
                       int description_string_id,
                       int set_up_button_string_id);
+  void RefreshDescription(int description_string_id);
 
  private:
   void InitLayout();
