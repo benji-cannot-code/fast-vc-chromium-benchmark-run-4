fb193c57e58026bb01f1dd336b92bce6f94d8bf2
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-05-26 05:20:41 +0000

    system-extensions: Add attributes to CrosAcceleratorEvent
    Also adds CrosAcceleratorEventInit to create the event.
    
    The event has two properties:
    
    acceleratorName: Name of the accelerator that was pressed or released.
    repeat: Whether or not the accelerator is still being pressed.
    
    Bug: b/221123297
    Change-Id: I5644dcbb5ac4e0e171326e9d0597e17843aa28de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663677
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007729}

--- third_party/blink/renderer/extensions/chromeos/system_extensions/window_management/cros_accelerator_event.h
+++ third_party/blink/renderer/extensions/chromeos/system_extensions/window_management/cros_accelerator_event.h
@@ -10,18 +10,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class CrosAcceleratorEventInit;
+
 class CrosAcceleratorEvent final : public Event {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static CrosAcceleratorEvent* Create();
+  static CrosAcceleratorEvent* Create(
+      const AtomicString& type,
+      const CrosAcceleratorEventInit* event_init);
 
-  CrosAcceleratorEvent();
+  CrosAcceleratorEvent(const AtomicString& type,
+                       const CrosAcceleratorEventInit* event_init);
   ~CrosAcceleratorEvent() override;
+
+  const String& acceleratorName() { return accelerator_name_; }
+  bool repeat() { return repeat_; }
 
   void Trace(Visitor*) const override;
 
   const AtomicString& InterfaceName() const override;
+
+ private:
+  const String accelerator_name_;
+  const bool repeat_;
 };
 
 }  // namespace blink
