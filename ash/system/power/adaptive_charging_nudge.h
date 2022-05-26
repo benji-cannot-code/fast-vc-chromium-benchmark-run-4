c50a33157a51695463b7c69bb9477673e3e98b9f
Angel Sanchez <angelsan@chromium.org>
2022-05-26 18:48:59 +0000

    Override OnThemeChanged to update existing nudge when DarkLightMode is toggled
    Here is what clipboard nudge looks like before changes: http://shortn/_wNZcu3NAI8
    Here is what clipboard nudge looks like with changes: http://shortn/_be9AXJUs7w
    Here is what dictation nudge looks like before changes:
    http://shortn/_3r8XedptvN
    Here is what dictation nudge looks like with changes: http://shortn/_yPEVjhsqe4
    Here is what adaptive charging nudge looks like before changes:
    http://shortn/_oQEwXh5OLm
    Here is what adaptive charging nudge looks like with changes: http://shortn/_oLhbLkIJGB
    Here is what dark light mode nudge looks like before changes:
    http://shortn/_1kXsdXw9rm
    Here is what dark light mode nudge looks like with changes:
    http://shortn/_6WWoCRxBi7
    
    Bug: 1233799
    Change-Id: I182e745ad0133dff4bb15a9277b133b859f75a6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3600397
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Angel Sanchez <angelsan@google.com>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Min Chen <minch@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007921}

--- ash/system/power/adaptive_charging_nudge.h
+++ ash/system/power/adaptive_charging_nudge.h
@@ -21,7 +21,7 @@ class ASH_EXPORT AdaptiveChargingNudge : public SystemNudge {
 
  private:
   // SystemNudge:
-  std::unique_ptr<views::View> CreateLabelView() const override;
+  std::unique_ptr<SystemNudgeLabel> CreateLabelView() const override;
   const gfx::VectorIcon& GetIcon() const override;
   std::u16string GetAccessibilityText() const override;
 };
