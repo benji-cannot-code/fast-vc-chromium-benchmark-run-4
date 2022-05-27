2f044d3069b686593beb92a915f89d8f07f64f56
kyraseevers <kyraseevers@chromium.org>
2022-05-27 15:05:42 +0000

    Update comparators and unittests in FileSystemURL to use buckets
    The DebugString(), IsInSameFileSystem(), operator(), and ==() comparator
    functions were updated to take a BucketLocator into account if it is
    set in the FileSystemURL. The relevant unittests were updated as well.
    
    Fixed: 1287513
    Change-Id: I8edbd959f37b8f3f051f255d03bbc50b82bfe13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668886
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008257}

--- storage/browser/file_system/file_system_url.cc
+++ storage/browser/file_system/file_system_url.cc
@@ -163,6 +163,9 @@ std::string FileSystemURL::DebugString() const {
     ss << path_.value();
   }
   ss << ", storage key: " << storage_key_.GetDebugString();
+  if (bucket_.has_value()) {
+    ss << ", bucket id: " << bucket_->id;
+  }
   ss << " }";
   return ss.str();
 }
@@ -173,7 +176,7 @@ bool FileSystemURL::IsParent(const FileSystemURL& child) const {
 
 bool FileSystemURL::IsInSameFileSystem(const FileSystemURL& other) const {
   return origin() == other.origin() && type() == other.type() &&
-         filesystem_id() == other.filesystem_id();
+         filesystem_id() == other.filesystem_id() && bucket() == other.bucket();
 }
 
 bool FileSystemURL::operator==(const FileSystemURL& that) const {
@@ -182,7 +185,7 @@ bool FileSystemURL::operator==(const FileSystemURL& that) const {
   } else {
     return storage_key() == that.storage_key() && type_ == that.type_ &&
            path_ == that.path_ && filesystem_id_ == that.filesystem_id_ &&
-           is_valid_ == that.is_valid_;
+           is_valid_ == that.is_valid_ && bucket_ == that.bucket_;
   }
 }
 
@@ -195,6 +198,8 @@ bool FileSystemURL::Comparator::operator()(const FileSystemURL& lhs,
     return lhs.type_ < rhs.type_;
   if (lhs.filesystem_id_ != rhs.filesystem_id_)
     return lhs.filesystem_id_ < rhs.filesystem_id_;
+  if (lhs.bucket_ != rhs.bucket_)
+    return lhs.bucket_ < rhs.bucket_;
   return lhs.path_ < rhs.path_;
 }
 
--- storage/browser/file_system/file_system_url_unittest.cc
+++ storage/browser/file_system/file_system_url_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
+#include "base/strings/string_number_conversions.h"
 #include "storage/common/file_system/file_system_types.h"
 #include "storage/common/file_system/file_system_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DRIVE FPL("/a/")
 #endif
 
+constexpr int kBucketId = 1;
+
 namespace storage {
 
 namespace {
@@ -37,6 +40,13 @@ FileSystemURL CreateFileSystemURL(const std::string& url_string) {
 
 std::string NormalizedUTF8Path(const base::FilePath& path) {
   return path.NormalizePathSeparators().AsUTF8Unsafe();
+}
+
+BucketLocator CreateNonDefaultBucket() {
+  return BucketLocator(
+      BucketId::FromUnsafeValue(kBucketId),
+      blink::StorageKey::CreateFromStringForTesting("http://www.example.com/"),
+      blink::mojom::StorageType::kTemporary, /*is_default=*/false);
 }
 
 }  // namespace
@@ -120,12 +130,25 @@ TEST(FileSystemURLTest, CompareURLs) {
     }
   }
 
-  const FileSystemURL a = CreateFileSystemURL(
+  FileSystemURL a = CreateFileSystemURL(
       "filesystem:http://chromium.org/temporary/dir a/file a");
-  const FileSystemURL b = CreateFileSystemURL(
+  FileSystemURL b = CreateFileSystemURL(
       "filesystem:http://chromium.org/persistent/dir a/file a");
   EXPECT_EQ(a.type() < b.type(), compare(a, b));
   EXPECT_EQ(b.type() < a.type(), compare(b, a));
+
+  // Testing comparison between FileSystemURLs that have a non-empty,
+  // non-default bucket value set.
+  BucketLocator bucket = CreateNonDefaultBucket();
+  a.SetBucket(bucket);
+  b.SetBucket(bucket);
+  // An identical bucket added to each URL does not alter type mismatch.
+  EXPECT_EQ(a.type() < b.type(), compare(a, b));
+  // c is a copy of a, just without a bucket value set.
+  const FileSystemURL c = CreateFileSystemURL(
+      "filesystem:http://chromium.org/temporary/dir a/file a");
+  // Ensure that buckets are taken into consideration for comparison.
+  EXPECT_EQ(a.bucket() < c.bucket(), compare(a, c));
 }
 
 TEST(FileSystemURLTest, IsParent) {
@@ -197,6 +220,15 @@ TEST(FileSystemURLTest, DebugString) {
   EXPECT_EQ("{ path: " + NormalizedUTF8Path(kPath) +
                 ", storage key: " + kURL1.storage_key().GetDebugString() + " }",
             kURL2.DebugString());
+  FileSystemURL kURL3 = FileSystemURL::CreateForTest(
+      blink::StorageKey::CreateFromStringForTesting("http://example.com"),
+      kFileSystemTypeTemporary, kPath);
+  kURL3.SetBucket(CreateNonDefaultBucket());
+  EXPECT_EQ("{ uri: filesystem:http://example.com/temporary/" +
+                NormalizedUTF8Path(kPath) +
+                ", storage key: " + kURL3.storage_key().GetDebugString() +
+                ", bucket id: " + base::NumberToString(kBucketId) + " }",
+            kURL3.DebugString());
 }
 
 TEST(FileSystemURLTest, IsInSameFileSystem) {
@@ -221,6 +253,18 @@ TEST(FileSystemURLTest, IsInSameFileSystem) {
   EXPECT_FALSE(url_foo_temp_a.IsInSameFileSystem(url_foo_perm_a));
   EXPECT_FALSE(url_foo_temp_a.IsInSameFileSystem(url_bar_temp_a));
   EXPECT_FALSE(url_foo_temp_a.IsInSameFileSystem(url_bar_perm_a));
+
+  // Test that non-empty, non-default bucket values are taken into account when
+  // determining of two URLs are in the same FileSystem.
+  BucketLocator bucket = CreateNonDefaultBucket();
+  url_foo_temp_a.SetBucket(bucket);
+  url_foo_temp_b.SetBucket(bucket);
+  EXPECT_TRUE(url_foo_temp_a.IsInSameFileSystem(url_foo_temp_b));
+  // url_foo_temp_c is identical to url_foo_temp_a but without a bucket value.
+  FileSystemURL url_foo_temp_c = FileSystemURL::CreateForTest(
+      blink::StorageKey::CreateFromStringForTesting("http://foo"),
+      kFileSystemTypeTemporary, base::FilePath::FromUTF8Unsafe("a"));
+  EXPECT_FALSE(url_foo_temp_a.IsInSameFileSystem(url_foo_temp_c));
 }
 
 TEST(FileSystemURLTest, ValidAfterMoves) {
