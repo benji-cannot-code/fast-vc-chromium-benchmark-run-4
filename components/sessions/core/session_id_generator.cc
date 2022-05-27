46e05bb8aac732cd13d92f6ab38406663b40f7cc
wangpei <wangpei@uniontech.com>
2022-05-27 10:29:39 +0000

    Enable persistent storage of current max SessionId.
    Initialize the SessionIdGenerator instance, providing a PrefService to ensure the persistent storage of current max SessionId.
    
    Bug: 1304535
    Change-Id: I5839241270e66684fc139c9b46f051590fd70e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506085
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008190}

--- components/sessions/core/session_id_generator.cc
+++ components/sessions/core/session_id_generator.cc
@@ -101,4 +101,8 @@ void SessionIdGenerator::IncrementValueBy(int increment) {
   last_value_ += increment;
 }
 
+bool SessionIdGenerator::IsInitializedForTest() const {
+  return local_state_ != nullptr;
+}
+
 }  // namespace sessions
