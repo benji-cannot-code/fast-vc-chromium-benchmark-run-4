4492c4be2de00be211f8aa3b35678cda81f7b31b
Ari Chivukula <arichiv@chromium.org>
2022-05-27 19:32:20 +0000

    Revert "Augmentation layer for unexpected field types."
    This reverts commit cea1a536e56fca07bb29f5bca18b78199d6b6a0d.
    
    Reason for revert: AddressFormattingTest.GetAddressComponentsWithExtensions and AddressFormattingTest.GetEnvelopeStyleAddressWithExtensions fail Linux MSan Tests
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests
    
    Original change's description:
    > Augmentation layer for unexpected field types.
    >
    > Introduces the concept of address format extensions to augment
    > Autofill's address format beyond libaddressinput. It allows defining
    > field types on a country basis which should additionally be shown in
    > the Autofill setting/kustodie dialog/edit import UI. They are not considered setting-inaccessible anymore by doing so.
    >
    > The rules are defined in autofill_country and used in
    > autofill_address_util, where the UI components are constructed.
    >
    > A unit test is added to ensure that the rules are consistent with
    > libaddressinput, in the sense that only missing fields are added.
    >
    > The CL defines address extensions for FR and GB, adding a state field.
    > They are chosen, because libaddressinput already defines appropriate string constants. This results in the following UI changes:
    > FR:
    > Settings: https://screenshot.googleplex.com/833vXetVRkZKFHD
    > Import: https://screenshot.googleplex.com/8XWhkdFQuGPRiGD
    > Edit on import: https://screenshot.googleplex.com/NVomhfs88CUbnsx
    > Update: https://screenshot.googleplex.com/5cTpbzwD7cGuwEK
    > GB:
    > Settings: https://screenshot.googleplex.com/3w34HAVUpJXMfTa
    > Import: https://screenshot.googleplex.com/AkoXdxiyHvPaANs
    > Edit on import: https://screenshot.googleplex.com/mVu2pwKF2KAtn7E
    > Update: https://screenshot.googleplex.com/5obsVwHaSecgwRc
    >
    > Guarded behind AutofillEnableExtendedAddressFormats.
    >
    > Fixed: 1064656
    > Bug: 1300548
    > Change-Id: I09403666105a401acf961a4c79fd97a4d6e28d0d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3653836
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1008288}
    
    Bug: 1300548
    Change-Id: I719e7186f574987f0f94176f18861a58415f025d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673260
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Owners-Override: Ari Chivukula <arichiv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008376}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -77,8 +77,6 @@ extern const base::Feature kAutofillEnableCompatibilitySupportForBirthdates;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillEnableDependentLocalityParsing;
 COMPONENT_EXPORT(AUTOFILL)
-extern const base::Feature kAutofillEnableExtendedAddressFormats;
-COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillEnableImportWhenMultiplePhoneNumbers;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillEnableMultiStepImports;
