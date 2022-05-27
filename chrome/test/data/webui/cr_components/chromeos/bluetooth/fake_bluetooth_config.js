32fac33ca02314222b14aecef5ddfd35f6e9de4a
Gordon Seto <gordonseto@google.com>
2022-05-27 15:56:25 +0000

    [CrOS Bluetooth] Close all mojo connections when pairing dialog closes.
    Update <bluetooth-pairing-ui> to close all Mojo connections when it
    is removed from the DOM. Previously, it would not do so, so if it is
    removed from the DOM and Bluetooth is disabled, then re-enabled, it will
    automatically start discovery because it is still observing
    CrosBluetoothConfig properties. In some cases, this behavior can cause
    Bluetooth to automatically re-enable after being disabled.
    
    Recording: https://drive.google.com/file/d/1sIuCT-NLAurxRd22asYq_qlobOCO_kOy/view?usp=sharing
    
    Fixed: b/231738454
    Change-Id: I51183eacdb9b0294d3de79d92075fa5f23345835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669998
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008266}

--- chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_bluetooth_config.js
+++ chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_bluetooth_config.js
@@ -104,6 +104,9 @@ export class FakeBluetoothConfig {
      * @private {?FakeDevicePairingHandler}
      */
     this.lastPairingHandler_ = null;
+
+    /** @private {number} */
+    this.numStartDiscoveryCalls_ = 0;
   }
 
   /**
@@ -145,6 +148,7 @@ export class FakeBluetoothConfig {
     this.lastDiscoveryDelegate_ = delegate;
     this.notifyDiscoveryStarted_();
     this.notifyDelegatesPropertiesUpdated_();
+    this.numStartDiscoveryCalls_++;
   }
 
   /**
@@ -497,5 +501,12 @@ export class FakeBluetoothConfig {
     const device = this.systemProperties_.pairedDevices.find(
         d => d.deviceProperties.id === deviceId);
     return device ? device : null;
+  }
+
+  /**
+   * @return {number}
+   */
+  getNumStartDiscoveryCalls() {
+    return this.numStartDiscoveryCalls_;
   }
 }
