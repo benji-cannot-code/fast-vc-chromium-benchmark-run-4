6b5591fad6e5a48a370ead576e563347a0e35fce
Darren Shen <shend@chromium.org>
2022-05-27 04:17:59 +0000

    lp: Move DLC install logic to its own function.
    This logic is being reused for both language packs and base payload.
    
    Bug: 1320137
    Change-Id: I1ca2300611341f1935d9e94dfe0366fc9d6bde3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3670413
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008132}

--- chromeos/language/language_packs/language_pack_manager.cc
+++ chromeos/language/language_packs/language_pack_manager.cc
@@ -119,6 +119,14 @@ absl::optional<std::string> GetDlcIdForBasePayload(
   return it->second;
 }
 
+void InstallDlc(const std::string& dlc_id,
+                DlcserviceClient::InstallCallback install_callback) {
+  dlcservice::InstallRequest install_request;
+  install_request.set_id(dlc_id);
+  DlcserviceClient::Get()->Install(install_request, std::move(install_callback),
+                                   base::DoNothing());
+}
+
 void OnInstallDlcComplete(OnInstallCompleteCallback callback,
                           const DlcserviceClient::InstallResult& dlc_result) {
   PackResult result;
@@ -193,12 +201,8 @@ void LanguagePackManager::InstallPack(const std::string& feature_id,
     return;
   }
 
-  dlcservice::InstallRequest install_request;
-  install_request.set_id(*dlc_id);
-  DlcserviceClient::Get()->Install(
-      install_request,
-      base::BindOnce(&OnInstallDlcComplete, std::move(callback)),
-      base::DoNothing());
+  InstallDlc(*dlc_id,
+             base::BindOnce(&OnInstallDlcComplete, std::move(callback)));
 }
 
 void LanguagePackManager::GetPackState(const std::string& feature_id,
@@ -247,12 +251,8 @@ void LanguagePackManager::InstallBasePayload(
     return;
   }
 
-  dlcservice::InstallRequest install_request;
-  install_request.set_id(*dlc_id);
-  DlcserviceClient::Get()->Install(
-      install_request,
-      base::BindOnce(&OnInstallDlcComplete, std::move(callback)),
-      base::DoNothing());
+  InstallDlc(*dlc_id,
+             base::BindOnce(&OnInstallDlcComplete, std::move(callback)));
 }
 
 void LanguagePackManager::AddObserver(Observer* const observer) {
