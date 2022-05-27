21630e27fd20df7d8cba5220d441d005139242c6
Illia Klimov <elklm@google.com>
2022-05-27 11:09:10 +0000

    Auto-ignore permission request if users start typing in the address bar.
    Bug: 1329164
    Change-Id: I862c746a4d32304ef0fa80182242e76ec3d94759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666861
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008198}

--- chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
@@ -44,6 +44,17 @@ bool IsFullScreenMode(content::WebContents* web_contents, Browser* browser) {
   return !location_bar || !location_bar->IsDrawn();
 }
 
+bool IsLocationBarEditingOrEmpty(Browser* browser) {
+  DCHECK(browser);
+
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
+  if (!browser_view)
+    return false;
+
+  LocationBarView* location_bar = browser_view->GetLocationBarView();
+  return location_bar && location_bar->IsEditingOrEmpty();
+}
+
 bool ShouldBubbleStartOpen(permissions::PermissionPrompt::Delegate* delegate) {
   if (base::FeatureList::IsEnabled(
           permissions::features::kPermissionChipGestureSensitive)) {
@@ -92,6 +103,11 @@ std::unique_ptr<permissions::PermissionPrompt> CreatePermissionPrompt(
 
   if (delegate->ShouldDropCurrentRequestIfCannotShowQuietly() &&
       IsFullScreenMode(web_contents, browser)) {
+    return nullptr;
+  }
+
+  // Auto-ignore the permission request if a user is typing into location bar.
+  if (IsLocationBarEditingOrEmpty(browser)) {
     return nullptr;
   }
 
