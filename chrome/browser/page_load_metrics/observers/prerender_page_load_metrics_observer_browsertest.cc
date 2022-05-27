68edf6af3434f10fc9298342c7b8c294e55807d0
Cheick Cisse <cheickcisse@google.com>
2022-05-27 18:19:31 +0000

    Sheriff: Disable failing tests on MAC
    Bug: 1329881
    Change-Id: I7befe609003f4f6f4353b9246ec141aa2691e145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673678
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Owners-Override: Ari Chivukula <arichiv@google.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008340}

--- chrome/browser/page_load_metrics/observers/prerender_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/prerender_page_load_metrics_observer_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
+
 #include "base/containers/contains.h"
 #include "chrome/browser/page_load_metrics/integration_tests/metric_integration_test.h"
 #include "chrome/browser/prerender/prerender_utils.h"
@@ -57,8 +59,14 @@ class PrerenderPageLoadMetricsObserverBrowserTest
   content::test::PrerenderTestHelper prerender_helper_;
 };
 
+// TODO(crbug.com/1329881): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_Activate_SpeculationRule DISABLED_Activate_SpeculationRule
+#else
+#define MAYBE_Activate_SpeculationRule Activate_SpeculationRule
+#endif
 IN_PROC_BROWSER_TEST_F(PrerenderPageLoadMetricsObserverBrowserTest,
-                       Activate_SpeculationRule) {
+                       MAYBE_Activate_SpeculationRule) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Navigate to an initial page.
@@ -186,8 +194,15 @@ IN_PROC_BROWSER_TEST_F(PrerenderPageLoadMetricsObserverBrowserTest,
       PrerenderPageLoad::kTiming_ActivationToLargestContentfulPaintName));
 }
 
+// TODO(crbug.com/1329881): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_Activate_Embedder_DirectURLInput \
+  DISABLED_Activate_Embedder_DirectURLInput
+#else
+#define MAYBE_Activate_Embedder_DirectURLInput Activate_Embedder_DirectURLInput
+#endif
 IN_PROC_BROWSER_TEST_F(PrerenderPageLoadMetricsObserverBrowserTest,
-                       Activate_Embedder_DirectURLInput) {
+                       MAYBE_Activate_Embedder_DirectURLInput) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   GURL prerender_url = embedded_test_server()->GetURL("/title2.html");
@@ -330,8 +345,14 @@ IN_PROC_BROWSER_TEST_F(PrerenderPageLoadMetricsObserverBrowserTest,
   EXPECT_FALSE(base::Contains(entries, prerender_url));
 }
 
+// TODO(crbug.com/1329881): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_Redirection DISABLED_Redirection
+#else
+#define MAYBE_Redirection Redirection
+#endif
 IN_PROC_BROWSER_TEST_F(PrerenderPageLoadMetricsObserverBrowserTest,
-                       Redirection) {
+                       MAYBE_Redirection) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Navigate to an initial page.
