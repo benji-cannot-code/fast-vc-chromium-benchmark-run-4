5f2cc22afc03d5c369c734e274b6cfcb2ab8e00a
Wenbo Jie <wenbojie@chromium.org>
2022-05-27 06:55:19 +0000

    [FilesDarkMode] Check bg color in preview for both light/dark mode
    Fix the quick view browser test to support checking background color in
    both light and dark mode.
    
    * Add a "isDarkModeEnabled" remote call so JS browser test can check if
    dark mode is on or not.
    * When checking background color in browser test, check both light and
    dark color based on the dark mode flag.
    
    Note: there are also other background color checks in the quick view
    test, but the expected colors work for both light/dark modes.
    
    Bug: 1329635
    Test: browser_tests --gtest_filter="*FilesAppBrowserTest*QuickView*"
    Change-Id: Iba4c09e1de05fdf0e1a4c623133f0709d3c06afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668991
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008161}

--- chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/smbfs/smbfs_mounter.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
+#include "ash/public/cpp/style/color_provider.h"
+#include "ash/public/cpp/style/scoped_light_mode_as_default.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/webui/file_manager/url_constants.h"
 #include "base/bind.h"
@@ -3004,6 +3006,12 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
 
   if (name == "isFiltersInRecentsEnabledV2") {
     *output = options.enable_filters_in_recents_v2 ? "true" : "false";
+    return;
+  }
+
+  if (name == "isDarkModeEnabled") {
+    ash::ScopedLightModeAsDefault scoped_light_mode_as_default;
+    *output = ash::ColorProvider::Get()->IsDarkModeEnabled() ? "true" : "false";
     return;
   }
 
