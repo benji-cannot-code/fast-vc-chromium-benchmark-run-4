e08307eaeb9e4cb283ad8799b49d588132e9cedb
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-05-27 17:59:21 +0000

    [send-tab-to-self/desktop] Add account footer to "no devices" bubble
    Behavior changes are behind a flag disabled by default.
    The device picker bubble already had this footer, now the "no target
    devices" bubble does too.
    Implementation screenshot: crbug.com/1219433#c12
    
    Besides that:
    - The link click handler used to have an explicit Hide() call [1]. This
    wasn't really necessary because the bubble closes anyway, so it's
    removed.
    - The horizontal margin of the text in
    send_tab_to_self_promo_bubble_view.cc now matches the one in the
    device picker.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view.cc;l=123;drc=a2d9040cbd321ef3c34829ac143c756b5b16e74d
    
    Bug: 1219433
    Change-Id: Id9227b4103dea0522248a97e1dacf02415fbe18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669248
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008325}

---
+++ chrome/browser/ui/views/send_tab_to_self/manage_account_devices_link_view.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SEND_TAB_TO_SELF_MANAGE_ACCOUNT_DEVICES_LINK_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_SEND_TAB_TO_SELF_MANAGE_ACCOUNT_DEVICES_LINK_VIEW_H_
+
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
+
+namespace views {
+class View;
+}  // namespace views
+
+namespace send_tab_to_self {
+
+class SendTabToSelfBubbleController;
+
+std::unique_ptr<views::View> BuildManageAccountDevicesLinkView(
+    base::WeakPtr<SendTabToSelfBubbleController> controller);
+
+}  // namespace send_tab_to_self
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SEND_TAB_TO_SELF_MANAGE_ACCOUNT_DEVICES_LINK_VIEW_H_
