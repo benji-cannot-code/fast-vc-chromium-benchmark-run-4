e2dd6e63ca6af6e90bbced8f3946b29a1ab9c52c
Andrey Zaytsev <andzaytsev@google.com>
2022-05-27 13:31:42 +0000

    [Sheriff] Disable flaky ExternalNavigationTest#testNonHandledExternalIntentWithFallbackUrlThatLaunchesIntentAfterRedirectBlocksFallbackIntent
    Bug: 1329813
    Change-Id: I5b1f028131ea79c3fe8a88d656dc561b88c2e848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671140
    Owners-Override: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008224}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ExternalNavigationTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ExternalNavigationTest.java
@@ -29,6 +29,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.Browser;
 import org.chromium.weblayer.Callback;
@@ -1414,6 +1415,7 @@ public class ExternalNavigationTest {
      */
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/1329813")
     public void
     testNonHandledExternalIntentWithFallbackUrlThatLaunchesIntentAfterRedirectBlocksFallbackIntent()
             throws Throwable {
