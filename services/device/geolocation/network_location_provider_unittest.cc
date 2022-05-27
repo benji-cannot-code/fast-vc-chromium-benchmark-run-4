0e77d1a9209440437b5359d4b36956c67f3f64b7
Jack Hsieh <chengweih@chromium.org>
2022-05-27 16:51:09 +0000

    Geolocation: Move technical error information to DevTools console
    Move technical error information into DevTools console in order to make
    the API error response information more human readable to end users.
    
    Additionally, It also avoids the non-2xx error message getting covered
    in url_loader by explicitly setting SetAllowHttpErrorResults() to
    manifest little more detail about the service error code.
    
    
    Bug: 1200914
    Change-Id: Id2378525601a326311e25cff8d29bbc98e3b03b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630661
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008290}

--- services/device/geolocation/network_location_provider_unittest.cc
+++ services/device/geolocation/network_location_provider_unittest.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/bind.h"
+#include "base/test/repeating_test_future.h"
 #include "base/test/task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
@@ -40,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace device {
+
+using ::base::test::RepeatingTestFuture;
 
 // Records the most recent position update and counts the number of times
 // OnLocationUpdate is called.
@@ -441,6 +445,8 @@ TEST_F(GeolocationNetworkProviderTest, MultipleWifiScansComplete) {
 
   mojom::Geoposition position = provider->GetPosition();
   EXPECT_FALSE(ValidateGeoposition(position));
+  EXPECT_EQ("Did not provide a good position fix", position.error_message);
+  EXPECT_TRUE(position.error_technical.empty());
 
   // 2. Now wifi data arrives -- SetData will notify listeners.
   const int kFirstScanAps = 6;
@@ -504,6 +510,12 @@ TEST_F(GeolocationNetworkProviderTest, MultipleWifiScansComplete) {
   // Error means we now no longer have a fix.
   position = provider->GetPosition();
   EXPECT_FALSE(ValidateGeoposition(position));
+  EXPECT_EQ("Network error. Check DevTools console for more information.",
+            position.error_message);
+  EXPECT_EQ(
+      "Network location provider at 'https://www.googleapis.com/' : "
+      "ERR_FAILED.",
+      position.error_technical);
 
   // 7. Wifi scan returns to original set: should be serviced from cache.
   wifi_data_provider_->SetData(CreateReferenceWifiScanData(kFirstScanAps));
@@ -578,6 +590,57 @@ TEST_F(GeolocationNetworkProviderTest,
 
   provider->OnPermissionGranted();
   CheckRequestIsValid(kScanCount, 0);
+}
+
+TEST_F(GeolocationNetworkProviderTest, NetworkRequestServiceBadRequest) {
+  std::unique_ptr<LocationProvider> provider(CreateProvider(true));
+  provider->StartProvider(false);
+  ASSERT_EQ(1, test_url_loader_factory_.NumPending());
+
+  RepeatingTestFuture<mojom::GeopositionPtr> future;
+  provider->SetUpdateCallback(
+      base::BindLambdaForTesting([&future](const LocationProvider* provider,
+                                           const mojom::Geoposition& position) {
+        future.AddValue(position.Clone());
+      }));
+  const std::string& request_url =
+      test_url_loader_factory_.pending_requests()->back().request.url.spec();
+  test_url_loader_factory_.AddResponse(request_url, std::string(),
+                                       net::HTTP_BAD_REQUEST);
+
+  auto position = future.Take();
+  EXPECT_EQ(
+      "Failed to query location from network service. Check the DevTools "
+      "console for more information.",
+      position->error_message);
+  EXPECT_EQ(
+      "Network location provider at 'https://www.googleapis.com/' : Returned "
+      "error code 400.",
+      position->error_technical);
+}
+
+TEST_F(GeolocationNetworkProviderTest, NetworkRequestResponseMalformed) {
+  std::unique_ptr<LocationProvider> provider(CreateProvider(true));
+  provider->StartProvider(false);
+  ASSERT_EQ(1, test_url_loader_factory_.NumPending());
+
+  RepeatingTestFuture<mojom::GeopositionPtr> future;
+  provider->SetUpdateCallback(
+      base::BindLambdaForTesting([&future](const LocationProvider* provider,
+                                           const mojom::Geoposition& position) {
+        future.AddValue(position.Clone());
+      }));
+  const std::string& request_url =
+      test_url_loader_factory_.pending_requests()->back().request.url.spec();
+  const char* kMalformedResponse =
+      "{"
+      "  \"status MALFORMED\""
+      "}";
+  test_url_loader_factory_.AddResponse(request_url, kMalformedResponse);
+
+  auto position = future.Take();
+  EXPECT_EQ("Response was malformed", position->error_message);
+  EXPECT_TRUE(position->error_technical.empty());
 }
 
 #if BUILDFLAG(IS_MAC)
