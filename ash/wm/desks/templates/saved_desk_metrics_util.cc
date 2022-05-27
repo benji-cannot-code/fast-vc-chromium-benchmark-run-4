b26d6fd08f58d482aeb6d00374e2493017639315
Daniel Andersson <dandersson@chromium.org>
2022-05-27 17:52:18 +0000

    saved_desks: Metrics for Save & Recall, part 2.
    This change adds Save & Recall metrics for:
    
    * Recalling a saved desk.
    * Window and tab counts in saved desks.
    * Count of saved desks.
    
    The metrics mirror existing ones for templates and the old functions
    that dealt with the template metrics have been modified to take a
    `DeskTemplateType` parameter. The metrics have an expiration date six
    months from now.
    
    Bug: 1328511
    Change-Id: I9ecd1e9a781fda8982d824be19b97ca1fcf9c9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668339
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008315}

--- ash/wm/desks/templates/saved_desk_metrics_util.cc
+++ ash/wm/desks/templates/saved_desk_metrics_util.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-void RecordLoadTemplateHistogram() {
+void RecordLoadSavedDeskLibraryHistogram() {
   base::UmaHistogramBoolean(kLoadTemplateGridHistogramName, true);
 }
 
@@ -23,8 +23,11 @@ void RecordDeleteSavedDeskHistogram(DeskTemplateType type) {
                             true);
 }
 
-void RecordLaunchTemplateHistogram() {
-  base::UmaHistogramBoolean(kLaunchTemplateHistogramName, true);
+void RecordLaunchSavedDeskHistogram(DeskTemplateType type) {
+  base::UmaHistogramBoolean(type == DeskTemplateType::kTemplate
+                                ? kLaunchTemplateHistogramName
+                                : kLaunchSaveAndRecallHistogramName,
+                            true);
 }
 
 void RecordNewSavedDeskHistogram(DeskTemplateType type) {
@@ -40,15 +43,21 @@ void RecordAddOrUpdateTemplateStatusHistogram(
                                 status);
 }
 
-void RecordUserTemplateCountHistogram(size_t entry_count,
-                                      size_t max_entry_count) {
-  UMA_HISTOGRAM_EXACT_LINEAR(kUserTemplateCountHistogramName, entry_count,
-                             max_entry_count);
+void RecordUserSavedDeskCountHistogram(DeskTemplateType type,
+                                       size_t entry_count,
+                                       size_t max_entry_count) {
+  if (type == DeskTemplateType::kTemplate) {
+    UMA_HISTOGRAM_EXACT_LINEAR(kUserTemplateCountHistogramName, entry_count,
+                               max_entry_count);
+  } else {
+    UMA_HISTOGRAM_EXACT_LINEAR(kUserSaveAndRecallCountHistogramName,
+                               entry_count, max_entry_count);
+  }
 }
 
-void RecordWindowAndTabCountHistogram(DeskTemplate* desk_template) {
+void RecordWindowAndTabCountHistogram(const DeskTemplate& desk_template) {
   const app_restore::RestoreData* restore_data =
-      desk_template->desk_restore_data();
+      desk_template.desk_restore_data();
   DCHECK(restore_data);
 
   int window_count = 0;
@@ -66,7 +75,7 @@ void RecordWindowAndTabCountHistogram(DeskTemplate* desk_template) {
     }
 
     for (const auto& window_iter : iter.second) {
-      absl::optional<std::vector<GURL>> urls = window_iter.second->urls;
+      const absl::optional<std::vector<GURL>>& urls = window_iter.second->urls;
       if (!urls || urls->empty())
         continue;
 
@@ -76,9 +85,19 @@ void RecordWindowAndTabCountHistogram(DeskTemplate* desk_template) {
     }
   }
 
-  base::UmaHistogramCounts100(kWindowCountHistogramName, window_count);
-  base::UmaHistogramCounts100(kTabCountHistogramName, tab_count);
-  base::UmaHistogramCounts100(kWindowAndTabCountHistogramName, total_count);
+  if (desk_template.type() == DeskTemplateType::kTemplate) {
+    base::UmaHistogramCounts100(kTemplateWindowCountHistogramName,
+                                window_count);
+    base::UmaHistogramCounts100(kTemplateTabCountHistogramName, tab_count);
+    base::UmaHistogramCounts100(kTemplateWindowAndTabCountHistogramName,
+                                total_count);
+  } else {
+    base::UmaHistogramCounts100(kSaveAndRecallWindowCountHistogramName,
+                                window_count);
+    base::UmaHistogramCounts100(kSaveAndRecallTabCountHistogramName, tab_count);
+    base::UmaHistogramCounts100(kSaveAndRecallWindowAndTabCountHistogramName,
+                                total_count);
+  }
 }
 
 void RecordUnsupportedAppDialogShowHistogram() {
--- chrome/browser/ui/ash/desks/desks_client_browsertest.cc
+++ chrome/browser/ui/ash/desks/desks_client_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_test_util.h"
+#include "ash/wm/desks/templates/saved_desk_metrics_util.h"
 #include "ash/wm/desks/templates/saved_desk_test_util.h"
 #include "ash/wm/desks/templates/saved_desk_util.h"
 #include "ash/wm/overview/overview_test_util.h"
@@ -2014,9 +2015,8 @@ IN_PROC_BROWSER_TEST_F(DesksTemplatesClientTest,
     ClickExpandedStateTemplatesButton();
   }
 
-  constexpr char kLaunchFromTemplateHistogramName[] =
-      "Ash.DeskTemplate.LaunchFromTemplate";
-  histogram_tester.ExpectTotalCount(kLaunchFromTemplateHistogramName, launches);
+  histogram_tester.ExpectTotalCount(ash::kLaunchTemplateHistogramName,
+                                    launches);
 }
 
 // Tests that launching a desk template records the appropriate performance
