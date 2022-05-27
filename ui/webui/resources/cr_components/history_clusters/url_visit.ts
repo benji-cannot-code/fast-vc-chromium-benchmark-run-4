d5d0539c51af0e6f5924d5025adb7c6a59b0dad7
Tommy Li <tommycli@chromium.org>
2022-05-27 00:45:10 +0000

    [journeys] Make the WebUI respect AllowDeletingBrowserHistory policy
    Previously, the AllowDeletingBrowserHistory policy was not respected by
    Journeys.
    
    A prior patch made it respected, but didn't update the UI. This is the
    followup patch to also hide the deletion controls from the UI when
    clusters are forbidden to be deleted by policy.
    
    It accomplishes this by splitting menu_container.ts, and moving the
    per-visit menu inline into url_visit.ts, because the action button as a
    whole should be hidden when deletion is forbidden.
    
    That isn't true for the whole-cluster menu, which still has a menu
    entry even when the deletion option is hidden.
    
    See screenshot:
    https://screenshot.googleplex.com/6dSmZbM3LqCcr69
    
    In the screenshot, the top three dot menu is the whole-cluster
    menu_container, and the bottom (circled) menu is the menu that's now
    inline in url_visit.ts.
    
    Bug: 1327743
    Change-Id: I6fabe87f2f9e2d601bdb362994fb299f34292be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671494
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008070}

--- ui/webui/resources/cr_components/history_clusters/url_visit.ts
+++ ui/webui/resources/cr_components/history_clusters/url_visit.ts
@@ -3,14 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './menu_container.js';
 import './page_favicon.js';
 import './history_clusters_shared_style.css.js';
 import '../../cr_elements/cr_action_menu/cr_action_menu.js';
+import '../../cr_elements/cr_icon_button/cr_icon_button.m.js';
 import '../../cr_elements/cr_lazy_render/cr_lazy_render.m.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {CrActionMenuElement} from '../../cr_elements/cr_action_menu/cr_action_menu.js';
+import {CrLazyRenderElement} from '../../cr_elements/cr_lazy_render/cr_lazy_render.m.js';
 import {loadTimeData} from '../../js/load_time_data.m.js';
 
 import {Annotation, URLVisit} from './history_clusters.mojom-webui.js';
@@ -40,6 +42,8 @@ declare global {
 
 interface VisitRowElement {
   $: {
+    actionMenu: CrLazyRenderElement<CrActionMenuElement>,
+    actionMenuButton: HTMLElement,
     title: HTMLElement,
     url: HTMLElement,
   };
@@ -72,6 +76,15 @@ class VisitRowElement extends PolymerElement {
       annotations_: {
         type: Object,
         computed: 'computeAnnotations_(visit)',
+      },
+
+      /**
+       * Usually this is true, but this can be false if deleting history is
+       * prohibited by Enterprise policy.
+       */
+      allowDeletingHistory_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('allowDeletingHistory'),
       },
 
       /**
@@ -111,6 +124,7 @@ class VisitRowElement extends PolymerElement {
   query: string;
   visit: URLVisit;
   private annotations_: Array<string>;
+  private allowDeletingHistory_: boolean;
   private debugInfo_: string;
   private unusedTitle_: string;
   private unusedVisibleUrl_: string;
@@ -128,7 +142,7 @@ class VisitRowElement extends PolymerElement {
     }));
   }
 
-  private onClick_(event: MouseEvent) {
+  private onClick_(event: Event) {
     // Ignore previously handled events.
     if (event.defaultPrevented) {
       return;
@@ -152,6 +166,23 @@ class VisitRowElement extends PolymerElement {
     this.onAuxClick_();
 
     OpenWindowProxyImpl.getInstance().open(this.visit.normalizedUrl.url);
+  }
+
+  private onActionMenuButtonClick_(event: Event) {
+    this.$.actionMenu.get().showAt(this.$.actionMenuButton);
+    event.preventDefault();  // Prevent default browser action (navigation).
+  }
+
+  private onRemoveSelfButtonClick_(event: Event) {
+    event.preventDefault();  // Prevent default browser action (navigation).
+
+    this.dispatchEvent(new CustomEvent('remove-visit', {
+      bubbles: true,
+      composed: true,
+      detail: this.visit,
+    }));
+
+    this.$.actionMenu.get().close();
   }
 
   //============================================================================
