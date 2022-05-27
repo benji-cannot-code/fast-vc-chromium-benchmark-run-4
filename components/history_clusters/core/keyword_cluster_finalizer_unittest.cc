b19e47ec5d42612c513347e4c7364bac45d3a42d
Sophie Chang <sophiechang@chromium.org>
2022-05-27 05:49:51 +0000

    Add search terms to Journeys keywords behind a flag
    Bug: 1320941
    Change-Id: I543fe8a1dbe8afb3308c1ceb699de793f3282ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663975
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008149}

--- components/history_clusters/core/keyword_cluster_finalizer_unittest.cc
+++ components/history_clusters/core/keyword_cluster_finalizer_unittest.cc
@@ -33,6 +33,7 @@ class KeywordClusterFinalizerTest : public ::testing::Test {
     config_.keyword_filter_on_noisy_visits = false;
     config_.keyword_filter_on_categories = false;
     config_.keyword_filter_on_entity_aliases = false;
+    config_.keyword_filter_on_search_terms = false;
     SetConfigForTesting(config_);
   }
 
@@ -56,6 +57,7 @@ TEST_F(KeywordClusterFinalizerTest, IncludesKeywordsBasedOnFeatureParameters) {
       {"github", 1}};
   visit.annotated_visit.content_annotations.model_annotations.categories = {
       {"category", 1}};
+  visit.annotated_visit.content_annotations.search_terms = u"search";
 
   history::ClusterVisit visit2 =
       testing::CreateClusterVisit(testing::CreateDefaultAnnotatedVisit(
@@ -74,6 +76,7 @@ TEST_F(KeywordClusterFinalizerTest, IncludesKeywordsBasedOnFeatureParameters) {
       {"github", 1}, {"otherentity", 1}};
   visit3.annotated_visit.content_annotations.model_annotations.categories = {
       {"category", 1}};
+  visit3.annotated_visit.content_annotations.search_terms = u"search";
 
   history::Cluster cluster;
   cluster.visits = {visit2, visit3};
@@ -92,6 +95,7 @@ class KeywordClusterFinalizerIncludeAllTest
     config_.keyword_filter_on_categories = true;
     config_.keyword_filter_on_entity_aliases = true;
     config_.max_entity_aliases_in_keywords = 1;
+    config_.keyword_filter_on_search_terms = true;
     SetConfigForTesting(config_);
   }
 
@@ -108,6 +112,7 @@ TEST_F(KeywordClusterFinalizerIncludeAllTest,
       {"github", 1}};
   visit.annotated_visit.content_annotations.model_annotations.categories = {
       {"category", 1}};
+  visit.annotated_visit.content_annotations.search_terms = u"search";
 
   history::ClusterVisit visit2 =
       testing::CreateClusterVisit(testing::CreateDefaultAnnotatedVisit(
@@ -126,13 +131,14 @@ TEST_F(KeywordClusterFinalizerIncludeAllTest,
       {"github", 1}, {"otherentity", 1}};
   visit3.annotated_visit.content_annotations.model_annotations.categories = {
       {"category", 1}};
+  visit3.annotated_visit.content_annotations.search_terms = u"search";
 
   history::Cluster cluster;
   cluster.visits = {visit2, visit3};
   FinalizeCluster(cluster);
   EXPECT_THAT(cluster.keywords,
               UnorderedElementsAre(u"github", u"category", u"onlyinnoisyvisit",
-                                   u"otherentity", u"git hub"));
+                                   u"otherentity", u"git hub", u"search"));
 }
 
 }  // namespace
