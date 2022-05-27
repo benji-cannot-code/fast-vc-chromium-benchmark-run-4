56e29c896bda1214757fba49f9e5bdd684aceea6
danakj <danakj@chromium.org>
2022-05-27 16:39:06 +0000

    Add tests for a default-constructed CheckedContiguousRange
    A default-constructed range should act like an empty one, but it
    has different code paths. Verify that they CHECK() accordingly.
    
    R=pkasting@chromium.org
    
    Bug: 1292951
    Change-Id: I1797e731db1cf71ec744f4068dc392c6e3956f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673158
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008284}

--- base/containers/checked_range_unittest.cc
+++ base/containers/checked_range_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_piece.h"
+#include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -234,16 +235,21 @@ TEST(CheckedContiguousRange, Conversions) {
       "");
 }
 
-TEST(CheckedContiguousRange, OutOfBoundsDeath) {
+TEST(CheckedContiguousRangeDeathTest, OutOfBounds) {
   std::vector<int> empty_vector;
   CheckedContiguousRange<std::vector<int>> empty_range(empty_vector);
-  ASSERT_DEATH_IF_SUPPORTED(empty_range[0], "");
-  ASSERT_DEATH_IF_SUPPORTED(empty_range.front(), "");
-  ASSERT_DEATH_IF_SUPPORTED(empty_range.back(), "");
+  EXPECT_CHECK_DEATH(empty_range[0]);
+  EXPECT_CHECK_DEATH(empty_range.front());
+  EXPECT_CHECK_DEATH(empty_range.back());
 
   static constexpr int array[] = {0, 1, 2};
   constexpr CheckedContiguousRange<const int[3]> range(array);
-  ASSERT_DEATH_IF_SUPPORTED(range[3], "");
+  EXPECT_CHECK_DEATH(range[3]);
+
+  CheckedContiguousRange<std::vector<int>> default_range;
+  EXPECT_CHECK_DEATH(default_range[0]);
+  EXPECT_CHECK_DEATH(default_range.front());
+  EXPECT_CHECK_DEATH(default_range.back());
 }
 
 }  // namespace base
