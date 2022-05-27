56cfed640c8d661df7f3d9c99d560501bbdedacc
Joel Hockey <joelhockey@chromium.org>
2022-05-27 22:11:19 +0000

    Add sharedStoragePrivate API
    http://go/terminal-gnubby-ash-lacros
    
    The sharedStoragePrivate API allows gnubbyd extension running in
    ChromeOS (ash or lacros) to store small amounts of data in ash prefs.
    This data is shared between ash and lacros versions of the extension.
    
    Once Terminal SWA runs in lacros rather than ash, we can migrate
    gnubbyd back to using chrome.storage.local and remove this private API.
    
    Bug: b/231890240
    Change-Id: I550b5f90d5d2c45164c874fe11bea8d60d5c684e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665986
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008455}

---
+++ chrome/browser/extensions/api/shared_storage/shared_storage_private_apitest.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/values.h"
+#include "build/buildflag.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "content/public/test/browser_test.h"
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/containers/contains.h"
+#include "chromeos/startup/browser_init_params.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
+namespace extensions {
+
+using SharedStoragePrivateApiTest = ExtensionApiTest;
+
+IN_PROC_BROWSER_TEST_F(SharedStoragePrivateApiTest, Test) {
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  auto capabilities = chromeos::BrowserInitParams::Get()->ash_capabilities;
+  if (!capabilities || !base::Contains(*capabilities, "b/231890240")) {
+    LOG(WARNING) << "Unsupported ash version for shared storage.";
+    return;
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
+  ASSERT_TRUE(RunExtensionTest("shared_storage_private")) << message_;
+}
+
+}  // namespace extensions
