bc983e28580ff7aa23734fc190376131595b3431
Dave Tapuska <dtapuska@chromium.org>
2022-05-27 16:39:26 +0000

    Stash the fenced frame implementation type on page.
    The lookup of the fenced frame implementation type is expensive
    because the value causes string compares. Stash the computation of the
    value on the Page object.
    
    BUG=1329402,1329393,1329318,1329273
    
    Change-Id: Ibb10fc3f31ca3f91ea31c44aff093f69c98254b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669175
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008285}

--- third_party/blink/renderer/core/page/page.h
+++ third_party/blink/renderer/core/page/page.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
 #include "base/types/pass_key.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/metrics/document_update_reason.h"
 #include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/fenced_frame/fenced_frame.mojom-blink.h"
@@ -220,6 +222,11 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
   }
   const WebWindowFeatures& GetWindowFeatures() const {
     return window_features_;
+  }
+
+  absl::optional<features::FencedFramesImplementationType>
+  FencedFramesImplementationType() const {
+    return fenced_frames_impl_;
   }
 
   PageScaleConstraintsSet& GetPageScaleConstraintsSet();
@@ -430,6 +437,9 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
   // breaks this cycle, so the frame is still properly destroyed once no
   // longer needed.
   Member<Frame> main_frame_;
+
+  // The type of fenced frames being used.
+  absl::optional<features::FencedFramesImplementationType> fenced_frames_impl_;
 
   scheduler::WebAgentGroupScheduler& agent_group_scheduler_;
   Member<PageAnimator> animator_;
