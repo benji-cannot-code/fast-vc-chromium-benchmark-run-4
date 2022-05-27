86aa5375cefb90e4e96ca9665614756f3dc31c50
Ari Chivukula <arichiv@chromium.org>
2022-05-27 22:53:26 +0000

    Revert "[ChromeVox] Restructure bridge constants for UserActionMonitor"
    This reverts commit 656ac35d44c115aa435ebf4dc01e2d081d1f99fa.
    
    Reason for revert: This seems to have broken Linux ChromeOS MSan https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/28193/overview https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/28192/overview since it's the weekend reverting rather than disabling
    
    Original change's description:
    > [ChromeVox] Restructure bridge constants for UserActionMonitor
    >
    > Following the pattern begin in this CL[1], restructure the file to group
    > each target with its associated actions.
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/3645094
    >
    > AX-Relnotes: n/a.
    > Bug: none
    > Change-Id: I15e2277d450a04596a3d2b6734ce889116360462
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662259
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1008364}
    
    Bug: 1330012
    Change-Id: I6d199c754b5499b2529c546f4667177d021a981c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673163
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ari Chivukula <arichiv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008467}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
@@ -18,6 +18,7 @@ BridgeTargets = {
   LOG_STORE: 'LogStore',
   PANEL: 'Panel',
   PANEL_BACKGROUND: 'PanelBackground',
+  USER_ACTION_MONITOR: 'UserActionMonitor',
 };
 /**
  * The class that a message is being sent to.
@@ -101,16 +102,6 @@ BridgeConstants = {
       SET_ENABLED: 'setEnabled',
     },
   },
-
-  UserActionMonitor: {
-    /** @public {BridgeTarget} */
-    TARGET: 'UserActionMonitor',
-    /** @enum {string} */
-    Action: {
-      CREATE: 'create',
-      DESTROY: 'destroy',
-    },
-  },
 };
 
 /**
@@ -119,8 +110,10 @@ BridgeConstants = {
 BridgeActions = {
   ADD_MENU_ITEM: 'addMenuItem',
   CLEAR_LOG: 'clearLog',
+  CREATE: 'create',
   CREATE_ALL_NODE_MENU_BACKGROUNDS: 'createAllNodeMenuBackgrounds',
   CREATE_NEW_I_SEARCH: 'createNewISearch',
+  DESTROY: 'destroy',
   DESTROY_I_SEARCH: 'destroyISearch',
   FOCUS_TAB: 'focusTab',
   GET_ACTIONS_FOR_CURRENT_NODE: 'getActionsForCurrentNode',
@@ -146,7 +139,6 @@ BridgeActions = {
  *           BridgeConstants.ChromeVoxState.Action |
  *           BridgeConstants.CommandHandler.Action |
  *           BridgeConstants.EventSourceState.Action |
- *           BridgeConstants.GestureCommandHandler.Action |
- *           BridgeConstants.UserActionMonitor.Action}
+ *           BridgeConstants.GestureCommandHandler.Action}
  */
 BridgeAction;
