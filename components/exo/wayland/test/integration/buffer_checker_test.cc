ba558f1cc53a4c1bf092fdc37ae77b765ed4e444
Jim Shargo <jshargo@chromium.org>
2022-05-27 05:00:03 +0000

    exo: Make it easier to write wayland integration tests
    This change makes separates the main function and the test set up into
    different files to make it easier to add new test cases with custom
    clients.
    
    BUG=n/a
    TESTED=Simple refactor. Builds + CQ
    
    Change-Id: If576e007eecb0b12d388d9853aea134967f9e848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669556
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jim Shargo <jshargo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008143}

--- components/exo/wayland/test/integration/buffer_checker_test.cc
+++ components/exo/wayland/test/integration/buffer_checker_test.cc
@@ -8,14 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <vector>
 
-#include "base/command_line.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/queue.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/task_environment.h"
-#include "base/test/test_suite.h"
 #include "components/exo/wayland/clients/client_base.h"
 #include "components/exo/wayland/clients/client_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -43,11 +40,11 @@ std::string DrmCodeToBufferFormatString(uint64_t drm_format) {
 
 namespace exo {
 namespace wayland {
-namespace clients {
+namespace test {
 
 namespace {
 
-class BufferCheckerTestClient : public ClientBase {
+class BufferCheckerTestClient : public ::exo::wayland::clients::ClientBase {
  public:
   explicit BufferCheckerTestClient() = default;
   ~BufferCheckerTestClient() override = default;
@@ -162,11 +159,11 @@ class BufferCheckerTestClient : public ClientBase {
 };
 
 }  // namespace
-}  // namespace clients
+}  // namespace test
 }  // namespace wayland
 }  // namespace exo
 
-class IntegrationTestClientTest : public testing::Test {
+class BufferCheckerClientTest : public testing::Test {
  protected:
   void SetUp() override {
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
@@ -194,8 +191,8 @@ void PrintReportedFormats(std::vector<uint32_t>& formats) {
              << base::JoinString(buffer_names, ", ");
 }
 
-TEST_F(IntegrationTestClientTest, CanUseAllReportedBuffers) {
-  exo::wayland::clients::BufferCheckerTestClient client;
+TEST_F(BufferCheckerClientTest, CanUseAllReportedBuffers) {
+  exo::wayland::test::BufferCheckerTestClient client;
   auto params = base_params_;
   // Initialize no buffers when we start, wait until we've gotten the list
   params.num_buffers = 0;
@@ -203,14 +200,4 @@ TEST_F(IntegrationTestClientTest, CanUseAllReportedBuffers) {
   PrintReportedFormats(client.reported_formats);
   for (auto format : client.reported_formats)
     EXPECT_TRUE(client.HasAnySupportedUsages(format));
-}
-
-int main(int argc, char* argv[]) {
-  base::CommandLine::Init(argc, argv);
-  base::TestSuite test_suite(argc, argv);
-
-  // Tests may not run to completion if we do not run them serially.
-  return base::LaunchUnitTestsSerially(
-      argc, argv,
-      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
 }
