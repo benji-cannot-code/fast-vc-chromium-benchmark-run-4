5849004bcedf52c954aadbcbe3c1ae7f80d2373a
Peter Kasting <pkasting@chromium.org>
2022-05-27 21:51:12 +0000

    Prevent unsafe narrowing: base/containers
    * static_casts where it seems safe, checked_casts where not
    * Better type deduction for some templates
    
    Bug: 1292951
    Change-Id: Ie4fcc2e4e87873df80eac0d1ef009610bf5ced93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668937
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008425}

--- base/containers/checked_range.h
+++ base/containers/checked_range.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <utility>
 
+#include "base/check.h"
 #include "base/containers/checked_iterators.h"
 #include "base/template_util.h"
 
@@ -128,7 +129,8 @@ class CheckedContiguousRange {
   constexpr reference back() const noexcept { return *(end() - 1); }
 
   constexpr reference operator[](size_type idx) const noexcept {
-    return *(begin() + idx);
+    CHECK(idx < size());
+    return *(begin() + static_cast<difference_type>(idx));
   }
 
   constexpr pointer data() const noexcept {
