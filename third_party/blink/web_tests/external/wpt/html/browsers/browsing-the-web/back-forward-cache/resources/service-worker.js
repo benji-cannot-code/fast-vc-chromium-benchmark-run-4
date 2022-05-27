0832d413b927174397c51486f2384e1701189f46
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-05-27 18:00:27 +0000

    [WPT] BFCache: service worker clients
    This CL adds service worker tests for BFCache:
    
    - navigator.serviceWorker.controller
    - Fetch interception
    - Clients.claim()
    - Clients.matchAll() and
    - unregister().
    
    Expected behavior:
    
    - Controlled pages should remain controlled after
      restored from BFCache, i.e.
      navigator.serviceWorker.controller should remain non-null
      and fetch should be intercepted.
    - Clients.claim() should evict pages
      that would be affected from BFCache.
    - Clients.matchAll() shouldn't list pages in BFCache.
    - unregister() should evict controlled pages from BFCache.
    
    Failing tests:
    
    - service-worker-clients-claim.https.html:
      On Safari/Firefox, Clients.claim() doesn't evict pages from BFCache.
    - service-worker-controlled-after-restore.https.html:
      On Firefox, fetches are not intercepted after restored from
      BFCache while `navigator.serviceWorker.controller` is non-null.
    - service-worker-unregister.https.html:
      Controlled pages aren't evicted by unregister() (all browsers).
    
    Bug: 1107415, 1204228, https://github.com/w3c/ServiceWorker/issues/1594
    Change-Id: I73233cf917e31dd91b974823d5490d0190f0eade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3201011
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008327}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/back-forward-cache/resources/service-worker.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('message', function(event) {
+    self.clients.claim()
+      .then(function(result) {
+          if (result !== undefined) {
+              event.data.port.postMessage(
+                  'FAIL: claim() should be resolved with undefined');
+              return;
+          }
+          event.data.port.postMessage('PASS');
+        })
+      .catch(function(error) {
+          event.data.port.postMessage('FAIL: exception: ' + error.name);
+        });
+  });
+
+self.addEventListener('fetch', e => {
+    if (e.request.url.match(/\/is-controlled/)) {
+      e.respondWith(new Response('controlled'));
+    }
+    else if (e.request.url.match(/\/get-clients-matchall/)) {
+      const options = { includeUncontrolled: true, type: 'all' };
+      e.respondWith(
+        self.clients.matchAll(options)
+          .then(clients => {
+            const client_urls = [];
+            clients.forEach(client => client_urls.push(client.url));
+            return new Response(JSON.stringify(client_urls));
+          })
+      );
+    }
+  });
