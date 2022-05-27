e17d88fadfc6783237e58442a881c4dc63944b17
Gang Wu <gangwu@chromium.org>
2022-05-27 21:50:08 +0000

    [Omnibox] Clean up Clank pedals batch1 feature and flag
    Change-Id: If6d13636f2fb236dd6ea5b5d8ef3d1836baad7b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671125
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008424}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2115,9 +2115,6 @@ extern const char kWebNotesPublishDescription[];
 extern const char kWebNotesDynamicTemplatesName[];
 extern const char kWebNotesDynamicTemplatesDescription[];
 
-extern const char kOmniboxPedalsAndroidBatch1Name[];
-extern const char kOmniboxPedalsAndroidBatch1Description[];
-
 extern const char kPasswordEditDialogWithDetailsName[];
 extern const char kPasswordEditDialogWithDetailsDescription[];
 
