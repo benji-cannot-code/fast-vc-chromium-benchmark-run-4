ed63f61302ceea56aa572a19f49f0430ee1b1d57
Satoshi Niwa <niwa@google.com>
2022-05-28 02:39:26 +0000

    arc: mojom: Add DiskQuotaHost.GetFreeDiskSpace
    Design doc : go/arcvm-virtio-blk-sparse
    
    BUG=b:233157352
    TEST=Call the mojo method from ARC
    
    Change-Id: I72fca2e090d224cef3d02c40f57a5d3de3cf3c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655198
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008537}

--- ash/components/arc/disk_quota/arc_disk_quota_bridge.h
+++ ash/components/arc/disk_quota/arc_disk_quota_bridge.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/mojom/disk_quota.mojom.h"
 #include "base/files/file_path.h"
+#include "base/memory/weak_ptr.h"
 #include "chromeos/dbus/cryptohome/UserDataAuth.pb.h"
 #include "components/account_id/account_id.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/cros_system_api/dbus/cryptohome/dbus-constants.h"
 
 namespace content {
@@ -65,10 +67,18 @@ class ArcDiskQuotaBridge : public KeyedService, public mojom::DiskQuotaHost {
                     const std::string& android_path,
                     SetProjectIdCallback callback) override;
 
+  void GetFreeDiskSpace(GetFreeDiskSpaceCallback) override;
+
  private:
+  void OnGetFreeDiskSpace(GetFreeDiskSpaceCallback callback,
+                          absl::optional<int64_t> reply);
+
   ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   AccountId account_id_;
+
+  // WeakPtrFactory to use for callbacks.
+  base::WeakPtrFactory<ArcDiskQuotaBridge> weak_factory_{this};
 };
 
 }  // namespace arc
