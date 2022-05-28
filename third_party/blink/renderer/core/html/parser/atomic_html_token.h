c392c46047ce8ea475cfb96131f3d2d63f2920fe
Hong Zheng <hong.zheng@intel.com>
2022-05-28 11:12:55 +0000

    Finding new entry with HashSet as fast path
    This CL is based on WebKit patch:
    https://trac.webkit.org/changeset/291508/webkit, which claims 1.2%
    progression on Speedometer
    
    Change-Id: I3d94b5058ce7a892376ec614dc3fb92d899aff15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666437
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Hong Zheng <hong.zheng@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1008573}

--- third_party/blink/renderer/core/html/parser/atomic_html_token.h
+++ third_party/blink/renderer/core/html/parser/atomic_html_token.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/parser/html_token.h"
 #include "third_party/blink/renderer/core/html_element_lookup_trie.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/hash_set.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h"
 
 namespace blink {
 
@@ -187,6 +189,11 @@ inline void AtomicHTMLToken::InitializeAttributes(
   if (!size)
     return;
 
+  // Track which attributes have already been inserted to avoid N^2
+  // behavior with repeated linear searches when populating `attributes_`.
+  HashSet<AtomicString> added_attributes;
+  added_attributes.ReserveCapacityForSize(size);
+
   attributes_.clear();
   attributes_.ReserveInitialCapacity(size);
   for (const auto& attribute : attributes) {
@@ -204,8 +211,7 @@ inline void AtomicHTMLToken::InitializeAttributes(
       value = g_empty_atom;
     }
     const QualifiedName& name = NameForAttribute(attribute);
-    // FIXME: This is N^2 for the number of attributes.
-    if (!FindAttributeInVector(attributes_, name)) {
+    if (added_attributes.insert(name.LocalName()).is_new_entry) {
       attributes_.push_back(Attribute(name, value));
     } else {
       duplicate_attribute_ = true;
