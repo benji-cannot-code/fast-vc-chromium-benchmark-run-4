ec46c8270b5f0f9d30f1f208dc2d7ec8a266cda6
Tommy C. Li <tommycli@chromium.org>
2022-05-28 01:00:56 +0000

    [journeys] Add a basic test for the History Clusters WebUI element
    Bug: 1329629
    Change-Id: Ifa846ddcc7f4e8c6f1329a29c519cdd9e747b7fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674283
    Auto-Submit: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008512}

--- ui/webui/resources/cr_components/history_clusters/clusters.ts
+++ ui/webui/resources/cr_components/history_clusters/clusters.ts
@@ -45,7 +45,7 @@ declare global {
   }
 }
 
-interface HistoryClustersElement {
+export interface HistoryClustersElement {
   $: {
     clusters: IronListElement,
     confirmationDialog: CrLazyRenderElement<CrDialogElement>,
@@ -54,7 +54,7 @@ interface HistoryClustersElement {
   };
 }
 
-class HistoryClustersElement extends PolymerElement {
+export class HistoryClustersElement extends PolymerElement {
   static get is() {
     return 'history-clusters';
   }
