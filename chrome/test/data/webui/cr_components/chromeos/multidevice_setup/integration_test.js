356be12c4374821db97201f6b2f652d51c35484d
Wes Okuhara <wesokuhara@google.com>
2022-05-28 00:03:56 +0000

    CrOS Settings: Check-in modulized browser tests for multidevice setup
    We no longer need to generate modulized versions of the JS test files
    (`.m.js`) at build time. Instead, we can check-in the modulized version
    and then use the test source files directly in
    `cr_components_chromeos_v3_browsertest.js`.
    
    Bug: chromium:1180979
    Test: out/Default/browser_tests --gtest_filter='*MultiDeviceSetup*'
    Change-Id: I909668a1086f19d5cfd3176949722e6a120d8db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671555
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008498}

--- chrome/test/data/webui/cr_components/chromeos/multidevice_setup/integration_test.js
+++ chrome/test/data/webui/cr_components/chromeos/multidevice_setup/integration_test.js
@@ -3,17 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// clang-format off
-// #import 'chrome://multidevice-setup/strings.m.js';
-// #import 'chrome://resources/cr_components/chromeos/multidevice_setup/multidevice_setup.m.js';
+import 'chrome://multidevice-setup/strings.m.js';
+import 'chrome://resources/cr_components/chromeos/multidevice_setup/multidevice_setup.m.js';
 
-// #import {TestMultideviceSetupBrowserProxy} from './setup_succeeded_page_test.m.js';
-// #import {FakeQuickUnlockPrivate} from '../../../settings/chromeos/fake_quick_unlock_private.js';
-// #import {BrowserProxyImpl} from 'chrome://resources/cr_components/chromeos/multidevice_setup/multidevice_setup_browser_proxy.m.js';
-// #import {FakeMojoService} from 'chrome://resources/cr_components/chromeos/multidevice_setup/fake_mojo_service.m.js';
-// #import {waitBeforeNextRender, eventToPromise} from '../../../test_util.js';
-// #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-// clang-format on
+import {FakeMojoService} from 'chrome://resources/cr_components/chromeos/multidevice_setup/fake_mojo_service.m.js';
+import {BrowserProxyImpl} from 'chrome://resources/cr_components/chromeos/multidevice_setup/multidevice_setup_browser_proxy.m.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {FakeQuickUnlockPrivate} from '../../../settings/chromeos/fake_quick_unlock_private.js';
+import {eventToPromise, waitBeforeNextRender} from '../../../test_util.js';
+
+import {TestMultideviceSetupBrowserProxy} from './setup_succeeded_page_test.js';
+
 
 /** @fileoverview Suite of integration tests for MultiDevice setup WebUI. */
 /** @implements {multidevice_setup.MultiDeviceSetupDelegate} */
@@ -106,7 +107,7 @@ suite('MultiDeviceSetup', () => {
   /** @type {!FakeMojoService} */
   let fakeMojoService;
 
-  /** @type {!settings.FakeQuickUnlockPrivate} */
+  /** @type {!FakeQuickUnlockPrivate} */
   let fakeQuickUnlockPrivate;
 
   /** @type {?TestMultideviceSetupBrowserProxy} */
@@ -120,13 +121,8 @@ suite('MultiDeviceSetup', () => {
   const WRONG_PASSWORD = 'wrongPassword';
 
   setup(async () => {
-    // The OOBE host uses polyfill which requires the test to wait until HTML
-    // imports have finished loading before initiating any tests. The Polymer 3
-    // version of the test does not use the OOBE host so this line should not
-    // execute.
-    /* #ignore */ await cr.ui.Oobe.waitForOobeToLoad();
     browserProxy = new TestMultideviceSetupBrowserProxy();
-    multidevice_setup.BrowserProxyImpl.instance_ = browserProxy;
+    BrowserProxyImpl.instance_ = browserProxy;
 
     multiDeviceSetupElement = document.createElement('multidevice-setup');
     multiDeviceSetupElement.delegate = new FakeDelegate();
@@ -135,13 +131,13 @@ suite('MultiDeviceSetup', () => {
         new FakeMojoInterfaceProviderImpl(fakeMojoService);
 
     document.body.appendChild(multiDeviceSetupElement);
-    Polymer.dom.flush();
+    flush();
 
     forwardButton = multiDeviceSetupElement.$$('button-bar').$$('#forward');
     cancelButton = multiDeviceSetupElement.$$('button-bar').$$('#cancel');
     backwardButton = multiDeviceSetupElement.$$('button-bar').$$('#backward');
 
-    fakeQuickUnlockPrivate = new settings.FakeQuickUnlockPrivate();
+    fakeQuickUnlockPrivate = new FakeQuickUnlockPrivate();
     fakeQuickUnlockPrivate.accountPassword = CORRECT_PASSWORD;
     multiDeviceSetupElement.$$(PASSWORD).quickUnlockPrivate_ =
         fakeQuickUnlockPrivate;
@@ -160,9 +156,8 @@ suite('MultiDeviceSetup', () => {
    */
   function setVisiblePage(visiblePageName) {
     multiDeviceSetupElement.visiblePageName = visiblePageName;
-    Polymer.dom.flush();
-    return test_util.waitBeforeNextRender(
-        multiDeviceSetupElement.$$(visiblePageName));
+    flush();
+    return waitBeforeNextRender(multiDeviceSetupElement.$$(visiblePageName));
   }
 
   /**
@@ -171,8 +166,8 @@ suite('MultiDeviceSetup', () => {
    */
   function enterPassword(input) {
     multiDeviceSetupElement.$$(PASSWORD).$$('#passwordInput').value = input;
-    Polymer.dom.flush();
-    return test_util.waitBeforeNextRender(multiDeviceSetupElement);
+    flush();
+    return waitBeforeNextRender(multiDeviceSetupElement);
   }
 
   function getNumSetHostDeviceCalls() {
@@ -192,7 +187,7 @@ suite('MultiDeviceSetup', () => {
   test('SetupSucceededPage forward button closes UI', () => {
     return setVisiblePage(SUCCESS).then(() => {
       const whenSetupExits =
-          test_util.eventToPromise('setup-exited', multiDeviceSetupElement);
+          eventToPromise('setup-exited', multiDeviceSetupElement);
       forwardButton.click();
       return whenSetupExits;
     });
@@ -204,7 +199,7 @@ suite('MultiDeviceSetup', () => {
     setMode(false /* isOobeMode */);
     return setVisiblePage(SUCCESS).then(() => {
       const whenSetupExits =
-          test_util.eventToPromise('setup-exited', multiDeviceSetupElement);
+          eventToPromise('setup-exited', multiDeviceSetupElement);
       multiDeviceSetupElement.$$(SUCCESS).$$('#settings-link').click();
       return whenSetupExits;
     });
@@ -228,7 +223,7 @@ suite('MultiDeviceSetup', () => {
     return setVisiblePage(START)
         .then(() => {
           const whenSetupExits =
-              test_util.eventToPromise('setup-exited', multiDeviceSetupElement);
+              eventToPromise('setup-exited', multiDeviceSetupElement);
           cancelButton.click();
           return whenSetupExits;
         })
@@ -246,8 +241,8 @@ suite('MultiDeviceSetup', () => {
         return setVisiblePage(START)
             .then(() => {
               multiDeviceSetupElement.delegate.shouldSetHostSucceed = true;
-              const whenSetupExits = test_util.eventToPromise(
-                  'setup-exited', multiDeviceSetupElement);
+              const whenSetupExits =
+                  eventToPromise('setup-exited', multiDeviceSetupElement);
               forwardButton.click();
               return whenSetupExits;
             })
@@ -264,7 +259,7 @@ suite('MultiDeviceSetup', () => {
     return setVisiblePage(START)
         .then(() => {
           const whenSetupExits =
-              test_util.eventToPromise('setup-exited', multiDeviceSetupElement);
+              eventToPromise('setup-exited', multiDeviceSetupElement);
           cancelButton.click();
           return whenSetupExits;
         })
@@ -291,7 +286,7 @@ suite('MultiDeviceSetup', () => {
     return setVisiblePage(PASSWORD)
         .then(() => {
           const whenSetupExits =
-              test_util.eventToPromise('setup-exited', multiDeviceSetupElement);
+              eventToPromise('setup-exited', multiDeviceSetupElement);
           cancelButton.click();
           return whenSetupExits;
         })
@@ -305,7 +300,7 @@ suite('MultiDeviceSetup', () => {
 
     return setVisiblePage(PASSWORD)
         .then(() => {
-          const whenPageChanges = test_util.eventToPromise(
+          const whenPageChanges = eventToPromise(
               'visible-page-name-changed', multiDeviceSetupElement);
           backwardButton.click();
           return whenPageChanges;
@@ -328,7 +323,7 @@ suite('MultiDeviceSetup', () => {
             })
             .then(() => {
               multiDeviceSetupElement.delegate.shouldSetHostSucceed = true;
-              const whenPageChanges = test_util.eventToPromise(
+              const whenPageChanges = eventToPromise(
                   'visible-page-name-changed', multiDeviceSetupElement);
               forwardButton.click();
               return whenPageChanges;
@@ -352,8 +347,8 @@ suite('MultiDeviceSetup', () => {
             .then(() => {
               multiDeviceSetupElement.delegate.shouldSetHostSucceed = true;
               forwardButton.click();
-              Polymer.dom.flush();
-              return test_util.waitBeforeNextRender(multiDeviceSetupElement);
+              flush();
+              return waitBeforeNextRender(multiDeviceSetupElement);
             })
             .then(() => {
               assertEquals(PASSWORD, multiDeviceSetupElement.visiblePageName);
@@ -373,8 +368,8 @@ suite('MultiDeviceSetup', () => {
             })
             .then(() => {
               forwardButton.click();
-              Polymer.dom.flush();
-              return test_util.waitBeforeNextRender(multiDeviceSetupElement);
+              flush();
+              return waitBeforeNextRender(multiDeviceSetupElement);
             })
             .then(() => {
               assertTrue(multiDeviceSetupElement.forwardButtonDisabled);
