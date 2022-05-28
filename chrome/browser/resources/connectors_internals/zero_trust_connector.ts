ec870cb0e10579cdbfbf32a4b875385833fb680a
dpapad <dpapad@chromium.org>
2022-05-28 00:47:57 +0000

    WebUI: Update html_to_wrapper() to also support non-Polymer elements.
    Adding a new `template` parameter to html_to_wrapper() which can be one
    of 'polymer' (default) or 'native'. The new parameter is used to inform
    which template should be used when wrapping an HTML file so that it can
    be imported via a JS import at runtime.
    
    Also deploying the new functionality in chrome://connectors-internals/
    as an example of how it should be used for non-Polymer UIs.
    
    Bug: 1292025
    Change-Id: I5a3d44bd897dc9aca1c71bc11ad30b70eb93a891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673108
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008510}

--- chrome/browser/resources/connectors_internals/zero_trust_connector.ts
+++ chrome/browser/resources/connectors_internals/zero_trust_connector.ts
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {CustomElement} from 'chrome://resources/js/custom_element.js';
-import {getTrustedHTML} from 'chrome://resources/js/static_types.js';
 
 import {KeyInfo, KeyManagerInitializedValue, KeyTrustLevel, KeyType, PageHandler, PageHandlerInterface, ZeroTrustState} from './connectors_internals.mojom-webui.js';
+import {getTemplate} from './zero_trust_connector.html.js';
 
 const TrustLevelStringMap = {
   [KeyTrustLevel.UNSPECIFIED]: 'Unspecified',
@@ -26,7 +26,7 @@ export class ZeroTrustConnectorElement extends CustomElement {
   }
 
   static override get template() {
-    return getTrustedHTML`{__html_template__}`;
+    return getTemplate();
   }
 
   public set enabledString(str: string) {
---
+++ tools/polymer/tests/html_to_wrapper/foo_native_expected.html.ts
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {getTrustedHTML} from 'chrome://resources/js/static_types.js';
+export function getTemplate() {
+  return getTrustedHTML`<!--_html_template_start_--><style>
+  div {
+    font-size: 2rem;
+  }
+</style>
+<div>Hello world</div>
+<!--_html_template_end_-->`;
+}
