1ed1ee355ae3dca20e7f4b4a0c07bb4e565c4c86
Irem Uguz <iremuguz@google.com>
2022-05-30 16:54:21 +0000

    support-tool: Restrict in guest sessions on consumer devices
    Make Support Tool disabled in guest sessions on consumer devices. It
    will be available in enrolled devices in guest sessions.
    The change is made after the privacy reviewer's comment here
    (https://bugs.chromium.org/p/chromium/issues/detail?id=1319930#c36)
    
    Bug: b:233718408
    Change-Id: Id2eee2062d992a41cda0ddd9efc255d3975039e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663228
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008873}

--- chrome/browser/ui/webui/support_tool/support_tool_ui.h
+++ chrome/browser/ui/webui/support_tool/support_tool_ui.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_SUPPORT_TOOL_SUPPORT_TOOL_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_SUPPORT_TOOL_SUPPORT_TOOL_UI_H_
 
+#include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/web_ui_controller.h"
 
 // The C++ back-end for the chrome://support-tool webui page.
@@ -17,6 +18,11 @@ class SupportToolUI : public content::WebUIController {
   SupportToolUI& operator=(const SupportToolUI&) = delete;
 
   ~SupportToolUI() override;
+
+  // Returns if Support Tool should be enabled. Support Tool is only
+  // enabled in managed devices and logged-in sessions on consumer devices. It
+  // won't be available in guest sessions of consumer-owned devices.
+  static bool IsEnabled(Profile* profile);
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SUPPORT_TOOL_SUPPORT_TOOL_UI_H_
