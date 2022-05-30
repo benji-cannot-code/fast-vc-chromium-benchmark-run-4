e9e0d86338525c2542c603ef15525ef542384c4f
Xiang Ji <jxiang@google.com>
2022-05-30 18:16:26 +0000

    Import category for test decorator
    Bug: 1305327
    Change-Id: Icc2ed6eb2e241d6b83a44d9bff3cbf3d3bcf87c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3675772
    Auto-Submit: Xiang Ji <jxiang@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008893}

--- chrome/test/enterprise/e2e/policy/translate_enabled/translate_enabled.py
+++ chrome/test/enterprise/e2e/policy/translate_enabled/translate_enabled.py
@@ -4,7 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import os
-from chrome_ent_test.infra.core import environment, before_all, test
+from chrome_ent_test.infra.core import before_all
+from chrome_ent_test.infra.core import category
+from chrome_ent_test.infra.core import environment
+from chrome_ent_test.infra.core import test
 from infra import ChromeEnterpriseTestCase
 
 
