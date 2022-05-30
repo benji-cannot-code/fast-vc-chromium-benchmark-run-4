635a0b6a46a3c8d053bb009238413f78e4ad3125
Clement Yan <clamclamyan@google.com>
2022-05-30 19:17:42 +0000

    [System Extensions] Change CrosWindowManagement to hold render process id over browser context
    Previously the management could outlive the browser context leading to a UAF
    
    Bug: crbug.com/1293820
    Change-Id: Iec56e925916d816406fe050bdbb863263440a41f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655199
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008906}

--- chrome/browser/ash/system_extensions/api/window_management/window_management_impl.h
+++ chrome/browser/ash/system_extensions/api/window_management/window_management_impl.h
@@ -7,12 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_SYSTEM_EXTENSIONS_API_WINDOW_MANAGEMENT_WINDOW_MANAGEMENT_IMPL_H_
 
 #include "base/unguessable_token.h"
+#include "chrome/browser/profiles/profile.h"
 #include "third_party/blink/public/mojom/chromeos/system_extensions/window_management/cros_window_management.mojom.h"
 #include "ui/aura/window.h"
-
-namespace content {
-class BrowserContext;
-}
 
 namespace views {
 class Widget;
@@ -22,7 +19,7 @@ namespace ash {
 
 class WindowManagementImpl : public blink::mojom::CrosWindowManagement {
  public:
-  explicit WindowManagementImpl(content::BrowserContext* browser_context);
+  explicit WindowManagementImpl(int32_t render_process_host_id);
   ~WindowManagementImpl() override = default;
 
   void GetAllWindows(GetAllWindowsCallback callback) override;
@@ -62,13 +59,16 @@ class WindowManagementImpl : public blink::mojom::CrosWindowManagement {
   void Close(const base::UnguessableToken& id, CloseCallback callback) override;
 
  private:
+  // Returns profile attached to the render process host id.
+  Profile* GetProfile();
+
   // Returns ptr to top level window from window at given id.
   aura::Window* GetWindow(const base::UnguessableToken& id);
 
   // Returns ptr to top level widget from window at given id.
   views::Widget* GetWidget(const base::UnguessableToken& id);
 
-  content::BrowserContext* browser_context_;
+  int32_t render_process_host_id_;
 };
 
 }  // namespace ash
