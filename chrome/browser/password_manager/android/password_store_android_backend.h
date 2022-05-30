20eed61db8e6733b3fd0686661988172fcf58a9e
Maxim Anufriev <maxan@google.com>
2022-05-30 17:36:01 +0000

    [UPM] Send crash dump on GMS Backend DEVELOPER_ERROR
    When DEVELOPER_ERROR is returned by the GMS Core 1P API, a crash dump
    will be sent without actual crashing.
    Additionally, `origin` and `signon_realm` values are attached when
    available.
    
    As CrashKeys has a length limit, values are possibly truncated.
    
    Bug: 1324588
    Change-Id: I75dc40900f84061915f85efa9ac2f7ad8055e88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3677056
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008887}

--- chrome/browser/password_manager/android/password_store_android_backend.h
+++ chrome/browser/password_manager/android/password_store_android_backend.h
@@ -65,9 +65,11 @@ class PasswordStoreAndroidBackend
 
     JobReturnHandler();
     JobReturnHandler(LoginsOrErrorReply callback,
-                     PasswordStoreBackendMetricsRecorder metrics_recorder);
+                     PasswordStoreBackendMetricsRecorder metrics_recorder,
+                     base::OnceClosure crash_dump_callback);
     JobReturnHandler(PasswordStoreChangeListReply callback,
-                     PasswordStoreBackendMetricsRecorder metrics_recorder);
+                     PasswordStoreBackendMetricsRecorder metrics_recorder,
+                     base::OnceClosure crash_dump_callback);
     JobReturnHandler(JobReturnHandler&&);
     JobReturnHandler& operator=(JobReturnHandler&&);
     ~JobReturnHandler();
@@ -84,11 +86,14 @@ class PasswordStoreAndroidBackend
 
     void RecordMetrics(absl::optional<AndroidBackendError> error) const;
     base::TimeDelta GetElapsedTimeSinceStart() const;
+    // TODO(crbug.com/1324588): Remove after disabling crash dumps.
+    void SendCrashDump();
 
    private:
     absl::variant<LoginsOrErrorReply, PasswordStoreChangeListReply>
         success_callback_;
     PasswordStoreBackendMetricsRecorder metrics_recorder_;
+    base::OnceClosure crash_dump_callback_;
   };
 
   using JobId = PasswordStoreAndroidBackendBridge::JobId;
@@ -149,8 +154,14 @@ class PasswordStoreAndroidBackend
   void OnError(PasswordStoreAndroidBackendBridge::JobId job_id,
                AndroidBackendError error) override;
 
+  // TODO(crbug.com/1324588): Remove signon_realm and origin after disabling
+  // crash dumps.
   template <typename Callback>
-  void QueueNewJob(JobId job_id, Callback callback, MetricInfix metric_infix);
+  void QueueNewJob(JobId job_id,
+                   Callback callback,
+                   MetricInfix metric_infix,
+                   absl::optional<std::string> signon_realm,
+                   absl::optional<std::string> origin);
   absl::optional<JobReturnHandler> GetAndEraseJob(JobId job_id);
 
   // Gets logins matching |form|.
