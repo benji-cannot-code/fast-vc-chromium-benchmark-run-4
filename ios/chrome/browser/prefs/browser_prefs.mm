2de98c00c32b5b1b5511844ce1f572ea60138df7
Quentin Pubert <qpubert@google.com>
2022-05-30 17:35:10 +0000

    [iOS] Add browser pref for 3P intents in Incognito interstitial
    A boolean browser preference is added to store whether the user wants to
    have a choice to open links from other apps in Incognito. The preference
    is only added if the flag "3p-intents-in-incognito" is enabled.
    
    Bug: 1318750
    Change-Id: I53cfcb1771bd9af0ed89573b5e01063c0e162e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3677058
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1008886}

--- ios/chrome/browser/prefs/browser_prefs.mm
+++ ios/chrome/browser/prefs/browser_prefs.mm
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.h"
 #include "ios/chrome/browser/ui/first_run/fre_field_trial.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_scene_agent.h"
+#import "ios/chrome/browser/ui/ui_feature_flags.h"
 #include "ios/chrome/browser/voice/voice_search_prefs_registration.h"
 #import "ios/chrome/browser/web/font_size/font_size_tab_helper.h"
 #import "ios/web/common/features.h"
@@ -345,6 +346,12 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(kOptimizationGuideRemoteFetchingEnabled, true);
 
   registry->RegisterBooleanPref(prefs::kHttpsOnlyModeEnabled, false);
+
+  // Register pref storing whether the Incognito interstitial for third-party
+  // intents is enabled.
+  if (base::FeatureList::IsEnabled(kIOS3PIntentsInIncognito)) {
+    registry->RegisterBooleanPref(prefs::kIncognitoInterstitialEnabled, false);
+  }
 
   // Register pref used to determine whether the User Policy notification was
   // already shown.
