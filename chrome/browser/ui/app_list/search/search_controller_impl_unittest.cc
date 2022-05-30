bb965e634ecffe9c931f5d0deb8d29d2e482aba7
Amanda Deacon <amandadeacon@chromium.org>
2022-05-30 23:16:50 +0000

    Reland "[Launcher health] De-duplicate ChromeSearchResult test fakes."
    A first attempt at this introduced a bug to
    
      search_controller_impl_new_unittest
    
    where relevance scores were no longer being set.
    
    Original change's description:
    > [Launcher health] De-duplicate ChromeSearchResult test fakes.
    >
    > Multiple unit test files define their own test fakes for the
    > ChromeSearchResult class. De-duplicate these into a common test util
    > location.
    >
    > Part of the go/launcher-health effort.
    >
    > Test: none introduced; refactor only
    > Bug: 1258415
    > Change-Id: Ib6fb884c7bf4b6dd0cf5f60d6dac9a2963af1463
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3672158
    > Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    > Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1008709}
    
    Test: unit tests
    Bug: 1258415
    Change-Id: I5fb941ee075c8192d4769be5c5effd0aec9cef44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676576
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008941}

--- chrome/browser/ui/app_list/search/search_controller_impl_unittest.cc
+++ chrome/browser/ui/app_list/search/search_controller_impl_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/search_controller.h"
+#include "chrome/browser/ui/app_list/search/test/ranking_test_util.h"
 #include "chrome/browser/ui/app_list/test/test_app_list_controller_delegate.h"
 #include "chrome/test/base/chrome_ash_test_base.h"
 
@@ -20,19 +21,6 @@ namespace app_list {
 namespace test {
 
 using ::test::TestAppListControllerDelegate;
-
-// TestSearchResult ------------------------------------------------------------
-
-class TestSearchResult : public ChromeSearchResult {
- public:
-  TestSearchResult() = default;
-  TestSearchResult(const TestSearchResult&) = delete;
-  TestSearchResult& operator=(const TestSearchResult&) = delete;
-  ~TestSearchResult() override = default;
-
- private:
-  void Open(int event_flags) override { NOTIMPLEMENTED(); }
-};
 
 // SearchControllerImplTest
 // --------------------------------------------------------
@@ -90,7 +78,7 @@ TEST_F(SearchControllerImplTest,
                         /*request_to_dismiss_view=*/true,
                         /*expect_to_dismiss_view=*/true});
 
-  auto result = std::make_unique<TestSearchResult>();
+  auto result = std::make_unique<TestResult>();
   for (auto& test_case : test_cases) {
     ash::ShellTestApi().SetTabletModeEnabledForTest(test_case.is_tablet_mode);
 
--- chrome/browser/ui/app_list/search/search_result_ranker/chip_ranker_unittest.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/chip_ranker_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/mixer.h"
+#include "chrome/browser/ui/app_list/search/test/ranking_test_util.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -28,14 +29,10 @@ namespace {
 
 using ResultType = ash::AppListSearchResultType;
 
-class TestSearchResult : public ChromeSearchResult {
+class ChipTestResult : public TestResult {
  public:
-  TestSearchResult(const std::string& id, ResultType type)
-      : instance_id_(instantiation_count++) {
-    set_id(id);
-    SetTitle(base::UTF8ToUTF16(id));
-    SetResultType(type);
-
+  ChipTestResult(const std::string& id, ResultType type)
+      : TestResult(id, type), instance_id_(instantiation_count++) {
     switch (type) {
       case ResultType::kFileChip:
       case ResultType::kDriveChip:
@@ -56,10 +53,10 @@ class TestSearchResult : public ChromeSearchResult {
     }
   }
 
-  TestSearchResult(const TestSearchResult&) = delete;
-  TestSearchResult& operator=(const TestSearchResult&) = delete;
+  ChipTestResult(const ChipTestResult&) = delete;
+  ChipTestResult& operator=(const ChipTestResult&) = delete;
 
-  ~TestSearchResult() override {}
+  ~ChipTestResult() override {}
 
   // ChromeSearchResult overrides:
   void Open(int event_flags) override {}
@@ -70,7 +67,7 @@ class TestSearchResult : public ChromeSearchResult {
   int instance_id_;
 };
 
-int TestSearchResult::instantiation_count = 0;
+int ChipTestResult::instantiation_count = 0;
 
 MATCHER_P(HasId, id, "") {
   bool match = base::UTF16ToUTF8(arg.result->title()) == id;
@@ -129,10 +126,10 @@ class ChipRankerTest : public testing::Test {
 
   std::unique_ptr<ChipRanker> ranker_;
 
-  // This is used only to make the ownership clear for the TestSearchResult
+  // This is used only to make the ownership clear for the ChipTestResult
   // objects that the return value of MakeSearchResults() contains raw pointers
   // to.
-  std::list<TestSearchResult> results_;
+  std::list<ChipTestResult> results_;
 };
 
 // Check that ranking an empty list has no effect.
