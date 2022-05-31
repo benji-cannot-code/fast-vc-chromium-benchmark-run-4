6787ce23f9c04a225743ef40ee7f07219ca3b17f
keno <kenoss@chromium.org>
2022-05-31 11:28:22 +0000

    PageLoadMetrics: AMPPageLoadMetricsObserver for FencedFrames
    This CL makes AMPPageLoadMetricsObserver return FORWARD_OBSERVING
    on OnFencedFrameStart to collect metrics at the observer running
    at the outermost tracker.
    
    This CL also fix
    AMPPageLoadMetricsObserver::OnDidFinishSubFrameNavigation to collect
    metrics correctly in the case that the navigation_handle comes from FF.
    
    Bug: 1317494
    Change-Id: Ic643215bb94dc46b75d2240d019037176caca028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661808
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009050}

--- chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer.h
@@ -62,6 +62,7 @@ class AMPPageLoadMetricsObserver
   ~AMPPageLoadMetricsObserver() override;
 
   // page_load_metrics::PageLoadMetricsObserver:
+  const char* GetObserverName() const override;
   ObservePolicy OnFencedFramesStart(
       content::NavigationHandle* navigation_handle,
       const GURL& currently_committed_url) override;
