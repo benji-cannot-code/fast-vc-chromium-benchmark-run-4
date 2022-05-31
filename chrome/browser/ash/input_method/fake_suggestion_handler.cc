84abc73a8425ff1a09f6687fc9d524d9ddb9593f
jhtin <jhtin@chromium.org>
2022-05-31 23:49:12 +0000

    Diacritics selection keyevents get handled. (Arrowkeys, enter, etc.)
    Left, right arrow to change selection (wraps around at end of list).
    Enter to accept.
    Esc to dismiss.
    
    Minor changes to fake_suggestion_handler to check acceptance and dismissal details.
    
    Bug: b/217560706
    Change-Id: Idc8f14bf3236a50c4b213996f726eb04e6b01730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3670673
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009351}

--- chrome/browser/ash/input_method/fake_suggestion_handler.cc
+++ chrome/browser/ash/input_method/fake_suggestion_handler.cc
@@ -38,6 +38,7 @@ bool FakeSuggestionHandler::AcceptSuggestion(int context_id,
                                              std::string* error) {
   showing_suggestion_ = false;
   accepted_suggestion_ = true;
+  accepted_suggestion_text_ = suggestion_text_;
   suggestion_text_ = u"";
   confirmed_length_ = 0;
   return true;
@@ -54,6 +55,7 @@ bool FakeSuggestionHandler::SetButtonHighlighted(
     bool highlighted,
     std::string* error) {
   highlighted_suggestion_ = highlighted;
+  highlighted_button_ = button;
   return false;
 }
 
@@ -66,17 +68,18 @@ bool FakeSuggestionHandler::AcceptSuggestionCandidate(
     int context_id,
     const std::u16string& candidate,
     std::string* error) {
-  return false;
+  showing_suggestion_ = false;
+  accepted_suggestion_ = true;
+  accepted_suggestion_text_ = candidate;
+  suggestion_text_ = u"";
+  confirmed_length_ = 0;
+  return true;
 }
 
 bool FakeSuggestionHandler::SetAssistiveWindowProperties(
     int context_id,
     const AssistiveWindowProperties& assistive_window,
     std::string* error) {
-  if (assistive_window.candidates.empty()) {
-    return true;
-  }
-
   if (assistive_window.visible) {
     context_id_ = context_id;
     showing_suggestion_ = true;
