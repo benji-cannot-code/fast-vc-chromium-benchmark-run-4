9e5c892e8522c19eedbc45566d2c8035b9157765
Clark DuVall <cduvall@chromium.org>
2022-05-31 22:01:24 +0000

    Cleanup some unused code in HTMLPreloadScanner
    The checkpoint functionality is no longer used, and the templated
    functions are only called with a single type. This also simplifies
    CachedDocumentParameters a bit, removing some fields that are no longer
    used.
    
    Bug: 1329535
    Change-Id: Ida31d85bc16c73d765161c5fa8993da80a936a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680561
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009296}

--- third_party/blink/renderer/core/html/parser/html_preload_scanner.h
+++ third_party/blink/renderer/core/html/parser/html_preload_scanner.h
@@ -47,13 +47,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-typedef wtf_size_t TokenPreloadScannerCheckpoint;
-
 class HTMLParserOptions;
 class HTMLTokenizer;
-class LazyLoadImageObserver;
 class SegmentedString;
-class SubresourceRedirectOriginsPreloader;
 
 bool Match(const StringImpl* impl, const QualifiedName& q_name);
 const StringImpl* TagImplFor(const HTMLToken::DataVector& data);
@@ -72,9 +68,6 @@ struct CORE_EXPORT CachedDocumentParameters {
   network::mojom::ReferrerPolicy referrer_policy;
   SubresourceIntegrity::IntegrityFeatures integrity_features;
   LocalFrame::LazyLoadImageSetting lazy_load_image_setting;
-  WeakPersistent<LazyLoadImageObserver> lazy_load_image_observer;
-  WeakPersistent<SubresourceRedirectOriginsPreloader>
-      subresource_redirect_origins_preloader;
   HashSet<String> disabled_image_types;
   WeakPersistent<LocalDOMWindow> local_dom_window;
 };
@@ -104,50 +97,19 @@ class TokenPreloadScanner {
     predicted_base_element_url_ = url;
   }
 
-  // A TokenPreloadScannerCheckpoint is valid until the next call to rewindTo,
-  // at which point all outstanding checkpoints are invalidated.
-  TokenPreloadScannerCheckpoint CreateCheckpoint();
-  void RewindTo(TokenPreloadScannerCheckpoint);
-
  private:
   class StartTagScanner;
 
-  template <typename Token>
-  void HandleMetaNameAttribute(const Token& token,
+  void HandleMetaNameAttribute(const HTMLToken& token,
                                absl::optional<ViewportDescription>* viewport);
 
-  template <typename Token>
-  inline void ScanCommon(const Token&,
+  inline void ScanCommon(const HTMLToken&,
                          const SegmentedString&,
                          PreloadRequestStream& requests,
                          absl::optional<ViewportDescription>*,
                          bool* is_csp_meta_tag);
 
-  template <typename Token>
-  void UpdatePredictedBaseURL(const Token&);
-
-  struct Checkpoint {
-    Checkpoint(
-        const KURL& predicted_base_element_url,
-        bool in_style,
-        bool in_script,
-        bool in_script_web_bundle,
-        size_t template_count,
-        scoped_refptr<const PreloadRequest::ExclusionInfo> exclusion_info)
-        : predicted_base_element_url(predicted_base_element_url),
-          in_style(in_style),
-          in_script(in_script),
-          in_script_web_bundle(in_script_web_bundle),
-          template_count(template_count),
-          exclusion_info(std::move(exclusion_info)) {}
-
-    KURL predicted_base_element_url;
-    bool in_style;
-    bool in_script;
-    bool in_script_web_bundle;
-    size_t template_count;
-    scoped_refptr<const PreloadRequest::ExclusionInfo> exclusion_info;
-  };
+  void UpdatePredictedBaseURL(const HTMLToken&);
 
   struct PictureData {
     PictureData() : source_size(0.0), source_size_set(false), picked(false) {}
@@ -179,10 +141,6 @@ class TokenPreloadScanner {
   // cannot determine an Origin Trial's status, so we accept this information in
   // the constructor and set this flag accordingly.
   bool priority_hints_origin_trial_enabled_;
-
-  bool did_rewind_ = false;
-
-  Vector<Checkpoint> checkpoints_;
 };
 
 class CORE_EXPORT HTMLPreloadScanner {
