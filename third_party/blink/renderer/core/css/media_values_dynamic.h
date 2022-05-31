94f3c737e972e71377311ac38d3f9c524703dc71
Anders Hartvoll Ruud <andruud@chromium.org>
2022-05-31 07:17:31 +0000

    Let MediaValues implement CSSLengthResolver
    This unifies MediaValues::ComputeLengthImpl with CSSLengthResolver::
    ZoomedComputedPixels, which is a step towards making mixed-unit math
    functions work with media queries.
    
    Bug: 1312000
    Change-Id: Ic21e779175373a275e6498ddf05302da9526a7d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673768
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009008}

--- third_party/blink/renderer/core/css/media_values_dynamic.h
+++ third_party/blink/renderer/core/css/media_values_dynamic.h
@@ -55,6 +55,11 @@ class CORE_EXPORT MediaValuesDynamic : public MediaValues {
   void Trace(Visitor*) const override;
 
  protected:
+  // CSSLengthResolver
+  float EmFontSize() const override;
+  float RemFontSize() const override;
+  float ExFontSize() const override;
+  float ChFontSize() const override;
   double ViewportWidth() const override;
   double ViewportHeight() const override;
   double SmallViewportWidth() const override;
@@ -63,10 +68,8 @@ class CORE_EXPORT MediaValuesDynamic : public MediaValues {
   double LargeViewportHeight() const override;
   double DynamicViewportWidth() const override;
   double DynamicViewportHeight() const override;
-  float EmSize() const override;
-  float RemSize() const override;
-  float ExSize() const override;
-  float ChSize() const override;
+  double ContainerWidth() const override;
+  double ContainerHeight() const override;
   WritingMode GetWritingMode() const override {
     return WritingMode::kHorizontalTb;
   }
