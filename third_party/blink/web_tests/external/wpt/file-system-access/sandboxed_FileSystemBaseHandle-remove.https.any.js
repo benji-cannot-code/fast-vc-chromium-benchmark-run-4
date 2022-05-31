83d90174c0bbf484108a398533cd3dd971d7c39d
Austin Sullivan <asully@chromium.org>
2022-05-31 22:33:05 +0000

    FSA: Update error code when attempting to remove sandbox root
    Before: reject promise with InvalidStateError
    After:  reject promise with NoModificationAllowedError
    
    Also updates wpt to run on both sandboxed and local backends, which
    will allow us to move it to the fs/ repo once the remove() PR lands.
    
    Bug: 1114923, 1327741
    Change-Id: I54629eb0849e71d95881e1e03f25bfae4211dae8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671699
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009320}

--- third_party/blink/web_tests/external/wpt/file-system-access/sandboxed_FileSystemBaseHandle-remove.https.any.js
+++ third_party/blink/web_tests/external/wpt/file-system-access/sandboxed_FileSystemBaseHandle-remove.https.any.js
@@ -2,7 +2,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=resources/test-helpers.js
 // META: script=../fs/resources/sandboxed-fs-test-helpers.js
 // META: script=script-tests/FileSystemBaseHandle-remove.js
-
-directory_test(async (t, root) => {
-    await promise_rejects_dom(t, 'InvalidStateError', root.remove());
-}, 'cannot remove the root of a sandbox file system');
