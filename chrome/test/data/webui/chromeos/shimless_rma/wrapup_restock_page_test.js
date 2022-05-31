0d5315dad718d4ce7ffd98aa63e8ff9922628c4d
Danny Wang <wangdanny@google.com>
2022-05-31 18:39:51 +0000

    Shimless: Remove powerwash dialog from restock page
    Clicking the button doesn't actually powerwash, don't need that
    powerwash dialog warning.
    
    Spec: go/shimless-rma-spec
    
    Screenshot: http://screen/5eri4t4wwG9iXoW
    
    ShimlessRMABrowserTest.MANUAL_WrapupRestockPageTest
    
    Bug: 1198187
    Test: browser_tests --gtest_filter=
    Change-Id: I7ad19bb1860791c333ee3e1e030313cec6a356d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664341
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009192}

--- chrome/test/data/webui/chromeos/shimless_rma/wrapup_restock_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/wrapup_restock_page_test.js
@@ -43,9 +43,7 @@ export function wrapupRestockPageTest() {
     service.reset();
   });
 
-  /**
-   * @return {!Promise}
-   */
+  /** @return {!Promise} */
   function initializeRestockPage() {
     assertFalse(!!component);
 
@@ -61,15 +59,12 @@ export function wrapupRestockPageTest() {
 
     return flushTasks();
   }
-  /**
-   * @param {string} buttonNameSelector
-   * @return {!Promise}
-   */
-  function clickButton(buttonNameSelector) {
-    assertTrue(!!component);
 
-    const button = component.shadowRoot.querySelector(buttonNameSelector);
-    button.click();
+  /** @return {!Promise} */
+  function clickShutdownButton() {
+    const shutdownComponent = component.shadowRoot.querySelector('#shutdown');
+    assertTrue(!!shutdownComponent);
+    shutdownComponent.click();
     return flushTasks();
   }
 
@@ -96,7 +91,7 @@ export function wrapupRestockPageTest() {
     assertEquals(1, callCounter);
   });
 
-  test('RestockPagePowerwashButtonCallsShutdownForRestock', async () => {
+  test('RestockPageOnShutdownCallsShutdownForRestock', async () => {
     const resolver = new PromiseResolver();
     await initializeRestockPage();
     let restockCallCounter = 0;
@@ -105,59 +100,9 @@ export function wrapupRestockPageTest() {
       return resolver.promise;
     };
 
-    await clickButton('#powerwashButton');
+    await clickShutdownButton();
 
     assertEquals(1, restockCallCounter);
-  });
-
-  test('ShutDownButtonOpensPowerwashDialog', async () => {
-    const resolver = new PromiseResolver();
-    await initializeRestockPage();
-
-    let callCount = 0;
-    service.shutdownForRestock = () => {
-      callCount++;
-      return resolver.promise;
-    };
-    await flushTasks();
-
-    await clickButton('#shutdown');
-
-    // Don't shut down immediately.
-    assertEquals(0, callCount);
-    // Show the dialog instead.
-    const powerwashDialog =
-        component.shadowRoot.querySelector('#powerwashDialog');
-    assertTrue(!!powerwashDialog);
-    assertTrue(powerwashDialog.open);
-  });
-
-  test('CancelButtonClosesPowerwashDialog', async () => {
-    await initializeRestockPage();
-    await flushTasks();
-    const powerwashDialog =
-        component.shadowRoot.querySelector('#powerwashDialog');
-    assertTrue(!!powerwashDialog);
-
-    await clickButton('#shutdown');
-    assertTrue(powerwashDialog.open);
-
-    await clickButton('#closePowerwashDialogButton');
-    assertFalse(powerwashDialog.open);
-  });
-
-  test('PowerwashDialogClosesWhenCompleted', async () => {
-    await initializeRestockPage();
-
-    const powerwashDialog =
-        component.shadowRoot.querySelector('#powerwashDialog');
-    assertTrue(!!powerwashDialog);
-
-    await clickButton('#shutdown');
-    assertTrue(powerwashDialog.open);
-
-    await clickButton('#powerwashButton');
-    assertFalse(powerwashDialog.open);
   });
 
   test('RestockPageButtonsDisabled', async () => {
