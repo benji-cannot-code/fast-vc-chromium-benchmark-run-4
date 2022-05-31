e2ce4c2ad6ac77c5da32e9227dc05c17423cbc9b
Brandon Jones <bajones@chromium.org>
2022-05-31 20:18:37 +0000

    Rename compositingAlphaMode -> alphaMode
    Syncs up Blink with change recently made to the WebGPU spec.
    See: https://github.com/gpuweb/gpuweb/pull/2905
    
    Bug: 1069302
    Change-Id: I091aceabaa1d2998dc39dd575bc0de92e596026f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673936
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009248}

--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
@@ -20,20 +20,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-GPUSwapChain::GPUSwapChain(
-    GPUCanvasContext* context,
-    GPUDevice* device,
-    WGPUTextureUsage usage,
-    WGPUTextureFormat format,
-    cc::PaintFlags::FilterQuality filter_quality,
-    V8GPUCanvasCompositingAlphaMode::Enum compositing_alpha_mode,
-    gfx::Size size)
+GPUSwapChain::GPUSwapChain(GPUCanvasContext* context,
+                           GPUDevice* device,
+                           WGPUTextureUsage usage,
+                           WGPUTextureFormat format,
+                           cc::PaintFlags::FilterQuality filter_quality,
+                           V8GPUCanvasAlphaMode::Enum alpha_mode,
+                           gfx::Size size)
     : DawnObjectBase(device->GetDawnControlClient()),
       device_(device),
       context_(context),
       usage_(usage),
       format_(format),
-      compositing_alpha_mode_(compositing_alpha_mode),
+      alpha_mode_(alpha_mode),
       size_(size) {
   // TODO: Use label from GPUObjectDescriptorBase.
   swap_buffers_ = base::AdoptRef(new WebGPUSwapBufferProvider(
@@ -42,8 +41,8 @@ GPUSwapChain::GPUSwapChain(
 
   // Note: SetContentsOpaque is only an optimization hint. It doesn't
   // actually make the contents opaque.
-  switch (compositing_alpha_mode) {
-    case V8GPUCanvasCompositingAlphaMode::Enum::kOpaque: {
+  switch (alpha_mode) {
+    case V8GPUCanvasAlphaMode::Enum::kOpaque: {
       CcLayer()->SetContentsOpaque(true);
 
       WGPUShaderModuleWGSLDescriptor wgsl_desc = {
@@ -92,7 +91,7 @@ GPUSwapChain::GPUSwapChain(
       GetProcs().shaderModuleRelease(shader_module);
       break;
     }
-    case V8GPUCanvasCompositingAlphaMode::Enum::kPremultiplied:
+    case V8GPUCanvasAlphaMode::Enum::kPremultiplied:
       CcLayer()->SetContentsOpaque(false);
       break;
   }
@@ -378,10 +377,9 @@ GPUTexture* GPUSwapChain::getCurrentTexture() {
 void GPUSwapChain::OnTextureTransferred() {
   DCHECK(texture_);
   // The texture is about to be transferred to the compositor.
-  // For compositing alpha mode Opaque, clear the alpha channel to
-  // 1.0.
-  switch (compositing_alpha_mode_) {
-    case V8GPUCanvasCompositingAlphaMode::Enum::kOpaque: {
+  // For alpha mode Opaque, clear the alpha channel to 1.0.
+  switch (alpha_mode_) {
+    case V8GPUCanvasAlphaMode::Enum::kOpaque: {
       WGPUTextureView attachment_view =
           GetProcs().textureCreateView(texture_->GetHandle(), nullptr);
 
@@ -422,7 +420,7 @@ void GPUSwapChain::OnTextureTransferred() {
       GetProcs().textureViewRelease(attachment_view);
       break;
     }
-    case V8GPUCanvasCompositingAlphaMode::Enum::kPremultiplied:
+    case V8GPUCanvasAlphaMode::Enum::kPremultiplied:
       break;
   }
   texture_ = nullptr;
--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_SWAP_CHAIN_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_SWAP_CHAIN_H_
 
-#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_canvas_compositing_alpha_mode.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_canvas_alpha_mode.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -32,7 +32,7 @@ class GPUSwapChain final : public GarbageCollected<GPUSwapChain>,
                         WGPUTextureUsage,
                         WGPUTextureFormat,
                         cc::PaintFlags::FilterQuality,
-                        V8GPUCanvasCompositingAlphaMode::Enum,
+                        V8GPUCanvasAlphaMode::Enum,
                         gfx::Size);
 
   GPUSwapChain(const GPUSwapChain&) = delete;
@@ -82,7 +82,7 @@ class GPUSwapChain final : public GarbageCollected<GPUSwapChain>,
   Member<GPUCanvasContext> context_;
   const WGPUTextureUsage usage_;
   const WGPUTextureFormat format_;
-  const V8GPUCanvasCompositingAlphaMode::Enum compositing_alpha_mode_;
+  const V8GPUCanvasAlphaMode::Enum alpha_mode_;
   const gfx::Size size_;
 
   Member<GPUTexture> texture_;
