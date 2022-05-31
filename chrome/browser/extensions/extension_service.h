48276732831ad714027e9ba22331c4f4ab2c2310
Alice Wang <aliceywang@chromium.org>
2022-05-31 10:03:31 +0000

    [TerminationNotification] Convert extension_service.h/cc to use callback
    This CL migrates extension_service.h/cc to use browser shutdown
    callback.
    
    Bug: 1317629
    Change-Id: Idaa8f9514a45a48eaa75cc132d166bfbaa5e9b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3677019
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009040}

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -503,6 +503,8 @@ class ExtensionService : public ExtensionServiceInterface,
       content::BrowserContext* browser_context,
       ExtensionHost* extension_host) override;
 
+  void OnAppTerminating();
+
   // content::NotificationObserver implementation:
   void Observe(int type,
                const content::NotificationSource& source,
@@ -679,6 +681,7 @@ class ExtensionService : public ExtensionServiceInterface,
   // Our extension updater, if updates are turned on.
   std::unique_ptr<ExtensionUpdater> updater_;
 
+  base::CallbackListSubscription on_app_terminating_subscription_;
   content::NotificationRegistrar registrar_;
 
   // Keeps track of loading and unloading component extensions.
