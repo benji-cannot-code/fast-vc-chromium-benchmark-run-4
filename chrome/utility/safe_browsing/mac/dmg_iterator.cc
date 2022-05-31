b04fba660cf353bd01bf363f8b98cc61ab51f613
Daniel Rubery <drubery@chromium.org>
2022-05-31 23:19:07 +0000

    Add further ArchiveAnalysisResult buckets
    DMG parsing can fail in two ways:
    - We fail to parse the UDIF structure of the file.
    - We don't know how to parse any of the contained partitions.
    This CL adds a specific bucket for not knowing how to parse the
    partitions.
    
    Similarly, ZIP parsing can currently fail in two ways:
    - We fail to parse the contents at the beginning
    - Something goes wrong during iteration
    This CL splits these into two separate buckets so we know which to
    investigate further.
    
    Bug: 1311816, 1314481
    Change-Id: I6651463e21d5dedd718bf5f102bc753603ca38fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665796
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009342}

--- chrome/utility/safe_browsing/mac/dmg_iterator.cc
+++ chrome/utility/safe_browsing/mac/dmg_iterator.cc
@@ -44,7 +44,7 @@ bool DMGIterator::Open() {
   }
   base::UmaHistogramBoolean("SBClientDownload.DmgHasAPFS", has_apfs);
 
-  return partitions_.size() > 0;
+  return true;
 }
 
 const std::vector<uint8_t>& DMGIterator::GetCodeSignature() {
@@ -94,6 +94,10 @@ std::u16string DMGIterator::GetPath() {
 
 std::unique_ptr<ReadStream> DMGIterator::GetReadStream() {
   return hfs_->GetReadStream();
+}
+
+bool DMGIterator::IsEmpty() {
+  return partitions_.empty();
 }
 
 }  // namespace dmg
