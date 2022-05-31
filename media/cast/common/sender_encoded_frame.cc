4a33b9de64a60b20fae98685d869f675c19d80ae
Jordan Bayles <jophba@chromium.org>
2022-05-31 21:37:43 +0000

    [Cast Streaming] rename lossy_utilization
    This patch renames lossy_utilization, a relatively
    confusing name representing how degraded the encoder had
    to encode the VideoFrame to meet the bit rate requirements,
    to lossiness. This minor cleanup fixes a long standing TODO
    and hopefully makes the codebase slightly clearer.
    
    Change-Id: I4c5030c4924219ec021bfb64f82b9636f2cd3676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668774
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009283}

--- media/cast/common/sender_encoded_frame.cc
+++ media/cast/common/sender_encoded_frame.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace cast {
 
-SenderEncodedFrame::SenderEncodedFrame()
-    : EncodedFrame(), encoder_utilization(-1.0), lossy_utilization(-1.0) {}
-
+SenderEncodedFrame::SenderEncodedFrame() = default;
 SenderEncodedFrame::~SenderEncodedFrame() = default;
 
 }  //  namespace cast
