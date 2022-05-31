0deefa642026664d314e31bac7ae61f73f70c65c
Zufeng Wang <zufeng@google.com>
2022-05-31 06:46:22 +0000

    media-app: Fix MediaAppUIGtestBrowserTest.NotifyCurrentFile
    Switch to a test parameterization that explicitly enables the flag
    instead of relying on the default behavior.
    
    Bug: b:234039943
    Change-Id: Ic07ea97c058713a2ae54f4462f89b7573a7e629d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676173
    Reviewed-by: Callistus Tan <callistus@google.com>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009003}

--- ash/webui/media_app_ui/test/media_app_ui_gtest_browsertest.js
+++ ash/webui/media_app_ui/test/media_app_ui_gtest_browsertest.js
@@ -209,9 +209,11 @@ TEST_F('MediaAppUIGtestBrowserTest', 'MultipleSelectionLaunch', () => {
   runMediaAppTest('MultipleSelectionLaunch');
 });
 
-TEST_F('MediaAppUIGtestBrowserTest', 'NotifyCurrentFile', () => {
-  runMediaAppTest('NotifyCurrentFile');
-});
+TEST_F(
+    'MediaAppUIWithDarkLightModeGtestBrowserTest', 'NotifyCurrentFileLight',
+    () => {
+      runMediaAppTest('NotifyCurrentFileLight');
+    });
 
 TEST_F(
     'MediaAppUIWithDarkLightModeDarkGtestBrowserTest', 'NotifyCurrentFileDark',
