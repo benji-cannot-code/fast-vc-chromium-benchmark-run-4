98cec4f10b531ceb90edb9c0807be59b2d70b35d
Marcello Salomao <msalomao@google.com>
2022-05-31 08:28:38 +0000

    [Files App] Clean up FilesArchivemount code
    Bug: 1295892
    Test: tast run dut filemanager.ZipMount.mount_single_7z
    Change-Id: Idd0420b07f4390e81b65b96471ac830ee5e90354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676600
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009025}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2806,9 +2806,6 @@ extern const char kMagnifierContinuousMouseFollowingModeSettingDescription[];
 extern const char kDockedMagnifierResizingName[];
 extern const char kDockedMagnifierResizingDescription[];
 
-extern const char kFilesArchivemountName[];
-extern const char kFilesArchivemountDescription[];
-
 extern const char kFilesArchivemount2Name[];
 extern const char kFilesArchivemount2Description[];
 
