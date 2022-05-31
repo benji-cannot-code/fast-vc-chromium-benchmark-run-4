3b381dc62a3bc1f6888db0831feec2ce214b735c
Tomasz Moniuszko <tmoniuszko@opera.com>
2022-05-31 16:49:01 +0000

    Add missing theme_properties.h include
    Change-Id: Ife12275fa5d700b0275cbb8193cdc7e67981689a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669052
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009133}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_COLOR_CHROME_COLOR_ID_H_
 #define CHROME_BROWSER_UI_COLOR_CHROME_COLOR_ID_H_
 
+#include "chrome/browser/themes/theme_properties.h"
 #include "ui/color/color_id.h"
 
 // TODO(pkasting): Add the rest of the colors.
