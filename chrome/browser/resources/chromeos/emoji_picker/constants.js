0b7d9a91dda0810856b8d845eea1fb036cf44c6b
Mehrab N <mehrab@chromium.org>
2022-05-31 22:07:16 +0000

    Emoji-picker: refactor constant defs and usage
    The following improvements are applied:
    
    * Imports of individual UI and events constants are replaced with
      a module-wise one. Due to large number of such definitions, this change
      helps to make imports section cleaner and more readable and improves
      readability of the codes using these constants as easier to understand
      their owner module.
    * New constants are defined for a few style related numbers that
      were previously hard-coded inside logics (e.g. element size).
    * Metadata for V1 Group tabs are moved to metadata_extension module
      and merged with its duplicate version in V2.
    * metadata_extension.js is better organised now making future refactors
      easier.
    
    The changes focused on the above points and optimisation or major
    style improvements are considered out of the scope and left for future
    work.
    
    Bug: b:233271372
    Change-Id: I2d71abf201736cb49f453a6401048cc3f8413ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3672097
    Commit-Queue: Mehrab N <mehrab@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009300}

--- chrome/browser/resources/chromeos/emoji_picker/constants.js
+++ chrome/browser/resources/chromeos/emoji_picker/constants.js
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // height and width should match the dialog size in EmojiPickerDialog.
+export const EMOJI_NUM_TABS_IN_FIRST_PAGE = 8;
+export const EMOJI_HIGHLIGHTER_WIDTH = 24;
 export const EMOJI_PICKER_HEIGHT = 454;
 export const EMOJI_PICKER_WIDTH = 396;
 export const EMOJI_ICON_SIZE = 28;
@@ -15,6 +17,7 @@ export const EMOJI_PICKER_TOP_PADDING = 20;
 export const GROUP_ICON_SIZE =
     (EMOJI_PICKER_WIDTH - 2 * EMOJI_PICKER_SIDE_PADDING) / EMOJI_PER_ROW;
 
+export const EMOJI_HIGHLIGHTER_WIDTH_PX = `${EMOJI_HIGHLIGHTER_WIDTH}px`;
 export const EMOJI_PICKER_HEIGHT_PX = `${EMOJI_PICKER_HEIGHT}px`;
 export const EMOJI_PICKER_WIDTH_PX = `${EMOJI_PICKER_WIDTH}px`;
 export const EMOJI_SIZE_PX = `${EMOJI_ICON_SIZE}px`;
