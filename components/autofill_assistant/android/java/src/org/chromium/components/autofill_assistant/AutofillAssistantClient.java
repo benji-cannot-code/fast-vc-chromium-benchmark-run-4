5dc194e16e9625b8d51218f294776ce207cecbd4
sandromaggi <sandromaggi@google.com>
2022-05-31 08:51:55 +0000

    Do not trigger DA for supervised users
    This prevents triggering a direct action (DA) for logged in
    users that are explicitly declared as "under parental control".
    
    Bug: b:228298871
    Change-Id: I14549a5ebb900cce592e4eeac6a11f020af3ae0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666857
    Auto-Submit: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009026}

--- components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/AutofillAssistantClient.java
+++ components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/AutofillAssistantClient.java
@@ -195,6 +195,16 @@ public class AutofillAssistantClient {
                 mNativeClientAndroid, AutofillAssistantClient.this);
     }
 
+    /**
+     * Check whether the user is supervised.
+     * @return supervised state
+     */
+    public boolean isSupervisedUser() {
+        if (mNativeClientAndroid == 0) return false;
+        return AutofillAssistantClientJni.get().isSupervisedUser(
+                mNativeClientAndroid, AutofillAssistantClient.this);
+    }
+
     @CalledByNative
     private void chooseAccountAsyncIfNecessary(@Nullable String userName) {
         if (mAccountInitializationStarted) return;
@@ -401,6 +411,7 @@ public class AutofillAssistantClient {
                 String actionId, String experimentId, String[] argumentNames,
                 String[] argumentValues, @Nullable AssistantOverlayCoordinator overlayCoordinator);
         void showFatalError(long nativeClientAndroid, AutofillAssistantClient caller);
+        boolean isSupervisedUser(long nativeClientAndroid, AutofillAssistantClient caller);
         void onSpokenFeedbackAccessibilityServiceChanged(
                 long nativeClientAndroid, AutofillAssistantClient caller, boolean enabled);
         AssistantDependencies getDependencies(
