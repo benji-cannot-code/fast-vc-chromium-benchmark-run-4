b57f50661a10dc9f5f355b2109b9e63048f4dcb5
Nan Lin <linnan@chromium.org>
2022-05-31 17:08:51 +0000

    Allow source event id to be sanitized in storage delegate
    source event id cardinality is a vendor-specific parameter.
    https://wicg.github.io/attribution-reporting-api/#vendor-specific-values
    
    Change-Id: I7f6d3a049c2f96fec6ac7d417d302a0b140c9751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674178
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009142}

--- content/browser/attribution_reporting/attribution_storage_delegate_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_storage_delegate_impl_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <limits>
 #include <vector>
 
 #include "base/containers/flat_map.h"
@@ -399,6 +400,12 @@ TEST(AttributionStorageDelegateImplTest, SanitizeTriggerData) {
               AttributionStorageDelegateImpl().SanitizeTriggerData(
                   test_case.trigger_data, test_case.source_type));
   }
+}
+
+TEST(AttributionStorageDelegateImplTest, SanitizeSourceEventId) {
+  EXPECT_EQ(AttributionStorageDelegateImpl().SanitizeSourceEventId(
+                std::numeric_limits<uint64_t>::max()),
+            std::numeric_limits<uint64_t>::max());
 }
 
 }  // namespace content
