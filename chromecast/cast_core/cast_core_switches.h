5c0e77444852a0c009a426784474aa14096b6bab
Vigen Issahhanjan <vigeni@google.com>
2022-05-31 21:29:18 +0000

    [cast_core] Share Cast Core Runtime switches.
    Bug: b:232140331
    Test: Ran on a device
    Change-Id: Iec7ef6672300d20ab26dadc1180c8638274f7797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673346
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009278}

---
+++ chromecast/cast_core/cast_core_switches.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_CAST_CORE_CAST_CORE_SWITCHES_H_
+#define CHROMECAST_CAST_CORE_CAST_CORE_SWITCHES_H_
+
+namespace cast {
+namespace core {
+
+// Specifies the Cast Core runtime ID, --cast-core-runtime-id=<runtime_id>.
+constexpr char kCastCoreRuntimeIdSwitch[] = "cast-core-runtime-id";
+
+// Specifies the Cast Core runtime gRPC endpoint,
+// --runtime-service-path=<endpoint>.
+constexpr char kRuntimeServicePathSwitch[] = "runtime-service-path";
+
+}  // namespace core
+}  // namespace cast
+
+#endif  // CHROMECAST_CAST_CORE_CAST_CORE_SWITCHES_H_
--- chromecast/cast_core/runtime/browser/cast_runtime_service.cc
+++ chromecast/cast_core/runtime/browser/cast_runtime_service.cc
@@ -8,15 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/process/process.h"
 #include "chromecast/browser/cast_browser_process.h"
+#include "chromecast/cast_core/cast_core_switches.h"
 #include "chromecast/metrics/cast_event_builder_simple.h"
 
 namespace chromecast {
-namespace {
-
-const char kCastCoreRuntimeIdSwitch[] = "cast-core-runtime-id";
-const char kRuntimeServicePathSwitch[] = "runtime-service-path";
-
-}  // namespace
 
 CastRuntimeService::CastRuntimeService(
     CastWebService* web_service,
@@ -38,9 +33,9 @@ void CastRuntimeService::FinalizeInternal() {}
 void CastRuntimeService::StartInternal() {
   auto* command_line = base::CommandLine::ForCurrentProcess();
   std::string runtime_id =
-      command_line->GetSwitchValueASCII(kCastCoreRuntimeIdSwitch);
+      command_line->GetSwitchValueASCII(cast::core::kCastCoreRuntimeIdSwitch);
   std::string runtime_service_path =
-      command_line->GetSwitchValueASCII(kRuntimeServicePathSwitch);
+      command_line->GetSwitchValueASCII(cast::core::kRuntimeServicePathSwitch);
   if (!app_dispatcher_.Start(runtime_id, runtime_service_path)) {
     base::Process::TerminateCurrentProcessImmediately(1);
   }
