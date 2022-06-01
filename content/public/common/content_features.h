4720d4662f41186b119c9dd0b2a94802a508c3b2
Yutaka Hirano <yhirano@chromium.org>
2022-06-01 15:51:33 +0000

    Rename "file ops brokering on disk cache" feature
    Rename the feature as we're going to run an experiment with the feature
    (but without sandboxing). The current name is misleading.
    
    Also add a chrome flag so that users can enable and disable the
    feature manually.
    
    Change-Id: I183ec326d63b06234f5c35ae0239856d331507c1
    Bug: 1289542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676716
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009632}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -46,6 +46,8 @@ CONTENT_EXPORT extern const base::Feature
     kBlockInsecurePrivateNetworkRequestsDeprecationTrial;
 CONTENT_EXPORT extern const base::Feature
     kBlockInsecurePrivateNetworkRequestsForNavigations;
+CONTENT_EXPORT extern const base::Feature
+    kBrokerFileOperationsOnDiskCacheInNetworkService;
 CONTENT_EXPORT extern const base::Feature kBrowserUseDisplayThreadPriority;
 CONTENT_EXPORT extern const base::Feature
     kBrowserVerifiedUserActivationKeyboard;
