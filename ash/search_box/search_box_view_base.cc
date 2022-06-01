bffe319df4784bca76db5bdf58835bbd9514735e
Yulun Wu <yulunwu@chromium.org>
2022-06-01 23:07:55 +0000

    Update search box icon animation times to spec.
    Bug: 1329617
    Change-Id: Ibf72419cd7b139e6208a571da07864e3994e2ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682243
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009844}

--- ash/search_box/search_box_view_base.cc
+++ ash/search_box/search_box_view_base.cc
@@ -44,7 +44,7 @@ namespace {
 
 // The duration for the animation which changes the search icon.
 constexpr base::TimeDelta kSearchIconAnimationDuration =
-    base::Milliseconds(250);
+    base::Milliseconds(150);
 
 constexpr int kInnerPadding = 16;
 
@@ -62,7 +62,7 @@ constexpr base::TimeDelta kButtonFadeOutDuration = base::Milliseconds(50);
 constexpr base::TimeDelta kButtonFadeInDelay = base::Milliseconds(50);
 
 // The duration for the button fade in animation.
-constexpr base::TimeDelta kButtonFadeInDuration = base::Milliseconds(200);
+constexpr base::TimeDelta kButtonFadeInDuration = base::Milliseconds(100);
 
 }  // namespace
 
