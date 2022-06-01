301c65a20f1f573a01d573c28cb6900e8c1ae5be
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-06-01 07:57:26 +0000

    Revert "[attribution-reporting] remove source_site from aggregatable reports"
    This reverts commit 37319b87597285da6a8c5155d25a9aac598a8d63.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1330836#c1
    
    Original change's description:
    > [attribution-reporting] remove source_site from aggregatable reports
    >
    > See explainer update here:
    > https://github.com/WICG/attribution-reporting-api/pull/445
    >
    >
    > Bug: 1329588
    > Change-Id: Ibe9fa953802bbafadbee342858beece19b398816
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674468
    > Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: John Delaney <johnidel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1009322}
    
    Bug: 1329588
    Change-Id: I72d3dc9d9041e7e84ce26949595987a12c02f52e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3683141
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1009483}

--- content/browser/attribution_reporting/attribution_interop_parser_unittest.cc
+++ content/browser/attribution_reporting/attribution_interop_parser_unittest.cc
@@ -104,6 +104,7 @@ TEST(AttributionInteropParserTest, ValidOutput) {
         "report_url": "https://r.example/path",
         "report": {
           "attribution_destination": "https://d.test",
+          "source_site": "https://s.test"
         },
         "test_info": {
           "histograms": [{
@@ -131,6 +132,7 @@ TEST(AttributionInteropParserTest, ValidOutput) {
         "report_url": "https://r.example/path",
         "payload": {
           "attribution_destination": "https://d.test",
+          "source_site": "https://s.test",
           "histograms": [{
             "key": "key",
             "value": "0x159"
