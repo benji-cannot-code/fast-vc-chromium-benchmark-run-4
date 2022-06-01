f45891fdc3be2e787e2550146963dae650ff52e9
Dan Clark <daniec@microsoft.com>
2022-06-01 16:14:24 +0000

    Highlight pseudos use the forced-color status of their originating element
    Per the resolution in [1], forced-color-adjust should not be one of the
    supported properties [2] in highlight pseudos, and the forced-color
    state of highlights should be taken from the originating element. This
    change updates the HighlightInheritance implementation to match the
    resolution.
    
    There are a few parts to this:
    - css_properties.json5 is updated so that forced-color-adjust is no
      longer a valid property for highlight pseudos. But, since shipping
      this change for ::selection may cause compatibility issues, we
      introduce a new valid_for_highlight_legacy parameter that maintains
      the old behavior and we update valid_for_highlight, which will be used
      only for highlights with the new inheritance model enabled, to use the
      new behavior.
    - This uncovered an older bug where DetermineValidPropertyFilter wasn't
      using any filter at all for ::highlight(), ::spelling-error, and
      ::grammar-error. This is now fixed.
    - StyleResolver::ApplyInheritance is updated to propagate forced-colors
      status from the originating element to the corresponding highlight
      pseudo.
    
    The new test is disabled on Mac/Linux due to some small differences
    in text decoration painting between those from highlight pseudos
    and from non-highlight rules, unrelated to the contents of this change.
    
    [1] https://github.com/w3c/csswg-drafts/issues/7264#issuecomment-1130239967
    [2] https://drafts.csswg.org/css-pseudo-4/#highlight-styling
    
    
    Bug: 1309835, 1024156
    Change-Id: I732afd24bbba2619a6718270faa0a1d2fcb98512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665644
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Delan Azabani <dazabani@igalia.com>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1009639}

--- third_party/blink/renderer/core/css/properties/css_property.h
+++ third_party/blink/renderer/core/css/properties/css_property.h
@@ -26,7 +26,7 @@ class LayoutObject;
 
 class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
  public:
-  using Flags = uint32_t;
+  using Flags = uint64_t;
 
   static const CSSProperty& Get(CSSPropertyID);
 
@@ -66,7 +66,6 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
   bool IsValidForFirstLine() const { return flags_ & kValidForFirstLine; }
   bool IsValidForCue() const { return flags_ & kValidForCue; }
   bool IsValidForMarker() const { return flags_ & kValidForMarker; }
-  bool IsValidForHighlight() const { return flags_ & kValidForHighlight; }
   bool IsValidForCanvasFormattedText() const {
     return flags_ & kValidForCanvasFormattedText;
   }
@@ -168,8 +167,10 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
     kBorderRadius = 1 << 17,
     // Set if the property values are tree-scoped references.
     kTreeScopedValue = 1 << 18,
-    // https://drafts.csswg.org/css-pseudo-4/#highlight-styling
-    kValidForHighlight = 1 << 19,
+    // Similar to the list at
+    // https://drafts.csswg.org/css-pseudo-4/#highlight-styling, with some
+    // differences for compatibility reasons.
+    kValidForHighlightLegacy = 1 << 19,
     // https://drafts.csswg.org/css-logical/#logical-property-group
     kInLogicalPropertyGroup = 1 << 20,
     // https://drafts.csswg.org/css-pseudo-4/#first-line-styling
@@ -196,7 +197,9 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
     // See valid_for_keyframes in css_properties.json5
     kValidForKeyframe = 1 << 30,
     // See valid_for_position_fallback in css_properties.json5
-    kValidForPositionFallback = 1u << 31,
+    kValidForPositionFallback = 1ull << 31,
+    // https://drafts.csswg.org/css-pseudo-4/#highlight-styling
+    kValidForHighlight = 1ull << 32,
   };
 
   constexpr CSSProperty(CSSPropertyID property_id,
--- third_party/blink/renderer/core/css/properties/longhands/variable.h
+++ third_party/blink/renderer/core/css/properties/longhands/variable.h
@@ -39,7 +39,8 @@ class CORE_EXPORT Variable : public Longhand {
   explicit constexpr Variable(CSSProperty::Flags flags)
       : Longhand(CSSPropertyID::kVariable,
                  kProperty | kValidForFirstLetter | kValidForFirstLine |
-                     kValidForMarker | kValidForHighlight | flags,
+                     kValidForMarker | kValidForHighlightLegacy |
+                     kValidForHighlight | flags,
                  '\0') {}
 };
 
