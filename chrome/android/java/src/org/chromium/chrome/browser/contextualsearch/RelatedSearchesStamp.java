4ce5be8b435087791b318c8a5a52c792acc59a6d
Gang Wu <gangwu@chromium.org>
2022-06-01 23:18:24 +0000

    [RSearches] Add default language allow list
    Bug:1329665
    
    Change-Id: I37e66036d525da63c20f1643d8277120837ef1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680878
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009848}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/RelatedSearchesStamp.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/RelatedSearchesStamp.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
@@ -42,6 +43,7 @@ class RelatedSearchesStamp {
             + RELATED_SEARCHES_EXPERIMENT_RECIPE_STAGE + RELATED_SEARCHES_NO_EXPERIMENT;
 
     private final ContextualSearchPolicy mPolicy;
+    private boolean mDisableDefaultAllowedLanguagesForTesting;
 
     /**
      * Verbosity param used to control requested results.
@@ -115,9 +117,7 @@ class RelatedSearchesStamp {
     String getRelatedSearchesStamp(String basePageLanguage) {
         if (!isRelatedSearchesQualifiedAndEnabled(basePageLanguage)) return "";
 
-        boolean isLanguageRestricted =
-                !TextUtils.isEmpty(ContextualSearchFieldTrial.getRelatedSearchesParam(
-                        ContextualSearchFieldTrial.RELATED_SEARCHES_LANGUAGE_ALLOWLIST_PARAM_NAME));
+        boolean isLanguageRestricted = !TextUtils.isEmpty(getAllowedLanguages());
         return buildRelatedSearchesStamp(isLanguageRestricted);
     }
 
@@ -169,8 +169,7 @@ class RelatedSearchesStamp {
      * @return whether the supplied parameter satisfies the current language requirement.
      */
     private boolean isLanguageQualified(String basePageLanguage) {
-        String allowedLanguages = ContextualSearchFieldTrial.getRelatedSearchesParam(
-                ContextualSearchFieldTrial.RELATED_SEARCHES_LANGUAGE_ALLOWLIST_PARAM_NAME);
+        String allowedLanguages = getAllowedLanguages();
         return TextUtils.isEmpty(allowedLanguages) || allowedLanguages.contains(basePageLanguage);
     }
 
@@ -245,5 +244,21 @@ class RelatedSearchesStamp {
         String resultsToReturnCode = getNumberOfRelatedSearchesToRequestCode();
         if (resultsToReturnCode.length() > 0) stampBuilder.append(resultsToReturnCode);
         return stampBuilder.toString();
+    }
+
+    private String getAllowedLanguages() {
+        String allowedLanguages = ContextualSearchFieldTrial.getRelatedSearchesParam(
+                ContextualSearchFieldTrial.RELATED_SEARCHES_LANGUAGE_ALLOWLIST_PARAM_NAME);
+        // If there is no language found, we use default language list.
+        if (TextUtils.isEmpty(allowedLanguages) && !mDisableDefaultAllowedLanguagesForTesting) {
+            allowedLanguages =
+                    ContextualSearchFieldTrial.RELATED_SEARCHES_LANGUAGE_DEFAULT_ALLOWLIST;
+        }
+        return allowedLanguages;
+    }
+
+    @VisibleForTesting
+    void disableDefaultAllowedLanguagesForTesting(boolean disableDefaultAllowedLanguages) {
+        mDisableDefaultAllowedLanguagesForTesting = disableDefaultAllowedLanguages;
     }
 }
--- chrome/android/junit/src/org/chromium/chrome/browser/contextualsearch/RelatedSearchesStampTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/contextualsearch/RelatedSearchesStampTest.java
@@ -163,6 +163,7 @@ public class RelatedSearchesStampTest {
         resetShadows();
         mPolicy = new ContextualSearchPolicy(null, null);
         mStamp = new RelatedSearchesStamp(mPolicy);
+        mStamp.disableDefaultAllowedLanguagesForTesting(true);
     }
 
     //====================================================================================
