43531be6018776de35d41fe4aedeb31b618564d2
Sophie Chang <sophiechang@chromium.org>
2022-06-01 00:09:37 +0000

    [journeys] Add an option to remove keywords that may look like the visit's host
    This also adds a chrome://flags entry for the various keyword filtering experiments we are running
    
    Bug: 1320941
    Change-Id: I27b7ad00a6e713e564c529bdfada50701a4d4d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680645
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009368}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1609,6 +1609,12 @@ const char kJourneysOnDeviceClusteringBackendName[] =
 const char kJourneysOnDeviceClusteringBackendDescription[] =
     "Enables variations for the on-device clustering backend";
 
+const char kJourneysOnDeviceClusteringKeywordFilteringName[] =
+    "History Journeys On-Device Clustering Keyword Filtering";
+const char kJourneysOnDeviceClusteringKeywordFilteringDescription[] =
+    "Enables variations for the keywords output by the on-device clustering "
+    "for Journeys";
+
 const char kLargeFaviconFromGoogleName[] = "Large favicons from Google";
 const char kLargeFaviconFromGoogleDescription[] =
     "Request large favicons from Google's favicon service";
