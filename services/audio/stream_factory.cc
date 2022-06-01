de4d0082f8e35953463c74dcc75dd156004a7cf2
Thomas Guilbert <tguilbert@chromium.org>
2022-06-01 14:09:39 +0000

    Configure audio workers' realtime periods
    This CL updates the loopback worker thread and the AudioManager worker
    thread to use a 10ms realtime period on Mac.
    
    This should improve power consumption relative to the default period.
    
    Bug: 1187156
    Change-Id: I9c64a54c908163a68efd4b6336d04edd3f58b4cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680724
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009592}

--- services/audio/stream_factory.cc
+++ services/audio/stream_factory.cc
@@ -26,9 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace audio {
 
-#if BUILDFLAG(CHROME_WIDE_ECHO_CANCELLATION)
 namespace {
-
+#if BUILDFLAG(CHROME_WIDE_ECHO_CANCELLATION)
 std::unique_ptr<OutputDeviceMixerManager> MaybeCreateOutputDeviceMixerManager(
     media::AudioManager* audio_manager) {
   if (!media::IsChromeWideEchoCancellationEnabled())
@@ -37,9 +36,15 @@ std::unique_ptr<OutputDeviceMixerManager> MaybeCreateOutputDeviceMixerManager(
   return std::make_unique<OutputDeviceMixerManager>(
       audio_manager, base::BindRepeating(&OutputDeviceMixer::Create));
 }
-
-}  // namespace
 #endif  // BUILDFLAG(CHROME_WIDE_ECHO_CANCELLATION)
+
+// Ideally, this would be based on the incoming audio's buffer durations.
+// However, we might deal with multiple streams, with multiple buffer durations.
+// Using a 10ms constant instead is acceptable (and better than the default)
+// since there are no super-strict realtime requirements (no system audio calls
+// waiting on these threads).
+constexpr base::TimeDelta kReatimeThreadPeriod = base::Milliseconds(10);
+}  // namespace
 
 StreamFactory::StreamFactory(media::AudioManager* audio_manager,
                              AecdumpRecordingManager* aecdump_recording_manager)
@@ -49,7 +54,7 @@ StreamFactory::StreamFactory(media::AudioManager* audio_manager,
       output_device_mixer_manager_(
           MaybeCreateOutputDeviceMixerManager(audio_manager)),
 #endif
-      loopback_worker_thread_("Loopback Worker") {
+      loopback_worker_thread_("Loopback Worker", kReatimeThreadPeriod) {
 }
 
 StreamFactory::~StreamFactory() {
