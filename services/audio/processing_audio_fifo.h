cccf0179926118cae5e6949f7d9efd8daa063742
Thomas Guilbert <tguilbert@chromium.org>
2022-06-01 08:31:44 +0000

    Configure ProcessingAudioFifo realtime period
    This CL adds the RealtimeAudioThread class, which allows us to specify
    a realtime thread's period on Mac (and is a regular base::Thread on any
    other platform).
    
    ProcessingAudioFifo then uses a RealtimeAudioThread to set a realtime
    period and save on power efficiency, relative to the default.
    
    Bug: 1187156
    Change-Id: Ifd57adb6749f699fb1ca225eb4ea254fe0acb472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671539
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009493}

--- services/audio/processing_audio_fifo.h
+++ services/audio/processing_audio_fifo.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/thread_annotations.h"
-#include "base/threading/thread.h"
 #include "media/base/audio_parameters.h"
+#include "services/audio/realtime_audio_thread.h"
 
 namespace media {
 class AudioBus;
@@ -103,7 +103,7 @@ class ProcessingAudioFifo {
 
   // Real-time audio processing thread, on which |processing_callback_| is
   // called.
-  base::Thread audio_processing_thread_;
+  RealtimeAudioThread audio_processing_thread_;
 
   // Processes captured audio data. Only run on |audio_processing_thread_|.
   ProcessAudioCallback processing_callback_;
---
+++ services/audio/realtime_audio_thread.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/audio/realtime_audio_thread.h"
+
+namespace audio {
+RealtimeAudioThread::RealtimeAudioThread(const std::string& name,
+                                         base::TimeDelta realtime_period)
+    : base::Thread(name), realtime_period_(realtime_period) {}
+
+RealtimeAudioThread::~RealtimeAudioThread() {
+  // As per API contract. See base/threading/thread.h.
+  Stop();
+}
+
+#if BUILDFLAG(IS_APPLE)
+base::TimeDelta RealtimeAudioThread::GetRealtimePeriod() {
+  return realtime_period_;
+}
+#endif
+}  // namespace audio
