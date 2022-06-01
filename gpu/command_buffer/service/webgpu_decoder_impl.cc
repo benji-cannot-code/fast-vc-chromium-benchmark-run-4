07dd18b3fa3f004f3ac92030ffda5bb60d0bbfd6
jchen10 <jie.a.chen@intel.com>
2022-06-01 10:58:50 +0000

    Expose DawnMultiPlanarFormats in WebGPUDecoderImpl
    The 0-copy video import needs to check this feature.
    
    Bug: 1258986
    Change-Id: I5d691137c7d62c2a23fd1900e731eeafdf17f381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682192
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1009529}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1033,6 +1033,7 @@ bool WebGPUDecoderImpl::IsFeatureExposed(WGPUFeatureName feature) const {
     case WGPUFeatureName_TimestampQuery:
     case WGPUFeatureName_PipelineStatisticsQuery:
     case WGPUFeatureName_ChromiumExperimentalDp4a:
+    case WGPUFeatureName_DawnMultiPlanarFormats:
       return allow_unsafe_apis_;
     case WGPUFeatureName_Depth24UnormStencil8:
     case WGPUFeatureName_Depth32FloatStencil8:
