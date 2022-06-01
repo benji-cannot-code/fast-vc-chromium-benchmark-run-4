2f4f826e639ebc696475e7f789582624c32a7a63
François Beaufort <beaufort.francois@gmail.com>
2022-06-01 05:47:07 +0000

    hid: Do not exclude HID device with no collections
    This CL makes sure the HID blocklist does not exclude HID devices with
    no collections so that developers can see them in the browser picker.
    
    Bug: 1319434
    Change-Id: I1edde15afa347e2e1136b829fe4522db663cbfce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3607963
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1009450}

--- chrome/browser/hid/hid_chooser_context.cc
+++ chrome/browser/hid/hid_chooser_context.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/observer_list.h"
 #include "base/strings/string_number_conversions.h"
@@ -25,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "content/public/browser/device_service.h"
 #include "extensions/buildflags/buildflags.h"
-#include "services/device/public/cpp/hid/hid_blocklist.h"
-#include "services/device/public/cpp/hid/hid_switches.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -454,18 +451,12 @@ void HidChooserContext::RevokeEphemeralDevicePermission(
 bool HidChooserContext::HasDevicePermission(
     const url::Origin& origin,
     const device::mojom::HidDeviceInfo& device) {
-  const bool has_fido_collection =
-      base::Contains(device.collections, device::mojom::kPageFido,
-                     [](const auto& c) { return c->usage->usage_page; });
-  if (has_fido_collection) {
-    if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-            switches::kDisableHidBlocklist) &&
-        !IsFidoAllowedForOrigin(origin)) {
-      // Exclude `device` if it is FIDO and FIDO is not allowed for `origin`.
+  if (device.is_excluded_by_blocklist) {
+    const bool has_fido_collection =
+        base::Contains(device.collections, device::mojom::kPageFido,
+                       [](const auto& c) { return c->usage->usage_page; });
+    if (!has_fido_collection || !IsFidoAllowedForOrigin(origin))
       return false;
-    }
-  } else if (device::HidBlocklist::IsDeviceExcluded(device)) {
-    return false;
   }
 
   if (CanApplyPolicy() &&
--- services/device/hid/hid_device_info.cc
+++ services/device/hid/hid_device_info.cc
@@ -71,6 +71,8 @@ HidDeviceInfo::HidDeviceInfo(HidPlatformDeviceId platform_device_id,
       HidBlocklist::kReportTypeOutput, vendor_id, product_id, collections);
   auto protected_feature_report_ids = HidBlocklist::Get().GetProtectedReportIds(
       HidBlocklist::kReportTypeFeature, vendor_id, product_id, collections);
+  auto is_excluded_by_blocklist =
+      HidBlocklist::Get().IsVendorProductBlocked(vendor_id, product_id);
 
   std::vector<uint8_t> report_ids;
   if (has_report_id) {
@@ -90,7 +92,7 @@ HidDeviceInfo::HidDeviceInfo(HidPlatformDeviceId platform_device_id,
       std::move(collections), has_report_id, max_input_report_size,
       max_output_report_size, max_feature_report_size, device_node,
       protected_input_report_ids, protected_output_report_ids,
-      protected_feature_report_ids);
+      protected_feature_report_ids, is_excluded_by_blocklist);
 }
 
 HidDeviceInfo::HidDeviceInfo(HidPlatformDeviceId platform_device_id,
@@ -122,6 +124,8 @@ HidDeviceInfo::HidDeviceInfo(HidPlatformDeviceId platform_device_id,
       HidBlocklist::kReportTypeOutput, vendor_id, product_id, collections);
   auto protected_feature_report_ids = HidBlocklist::Get().GetProtectedReportIds(
       HidBlocklist::kReportTypeFeature, vendor_id, product_id, collections);
+  auto is_excluded_by_blocklist =
+      HidBlocklist::Get().IsVendorProductBlocked(vendor_id, product_id);
 
   device_ = mojom::HidDeviceInfo::New(
       base::GenerateGUID(), physical_device_id, vendor_id, product_id,
@@ -129,7 +133,8 @@ HidDeviceInfo::HidDeviceInfo(HidPlatformDeviceId platform_device_id,
       /*report_descriptor=*/std::vector<uint8_t>{}, std::move(collections),
       has_report_id, max_input_report_size, max_output_report_size,
       max_feature_report_size, /*device_node=*/"", protected_input_report_ids,
-      protected_output_report_ids, protected_feature_report_ids);
+      protected_output_report_ids, protected_feature_report_ids,
+      is_excluded_by_blocklist);
 }
 
 HidDeviceInfo::~HidDeviceInfo() = default;
@@ -179,6 +184,10 @@ void HidDeviceInfo::AppendDeviceInfo(scoped_refptr<HidDeviceInfo> device_info) {
       HidBlocklist::Get().GetProtectedReportIds(
           HidBlocklist::kReportTypeFeature, device_->vendor_id,
           device_->product_id, device_->collections);
+
+  device_->is_excluded_by_blocklist =
+      HidBlocklist::Get().IsVendorProductBlocked(device_->vendor_id,
+                                                 device_->product_id);
 }
 
 const mojom::HidCollectionInfo* HidDeviceInfo::FindCollectionWithReport(
