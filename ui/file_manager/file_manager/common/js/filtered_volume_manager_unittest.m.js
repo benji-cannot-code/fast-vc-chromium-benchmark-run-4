b136b8f99f585ba221f493df09e23c65e060f14c
Noel Gordon <noel@chromium.org>
2022-06-01 09:21:23 +0000

    fusebox: add FilteredVolumeManager isFuseBoxOnlyFilterEnabled getter
    Bug: 1292825, 1327586, 1330833
    Test: browser_tests --gtest_filter="FileManagerJsTest.FilteredVolume*"
    Change-Id: I3c4a668c3fb3787e1de7c3928a1d92b599c4a6d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3678760
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009512}

--- ui/file_manager/file_manager/common/js/filtered_volume_manager_unittest.m.js
+++ ui/file_manager/file_manager/common/js/filtered_volume_manager_unittest.m.js
@@ -89,6 +89,9 @@ export function testVolumeDefaultFilter(done) {
       AllowedPaths.ANY_PATH_OR_URL, false, Promise.resolve(volumeManager), []);
 
   filteredVolumeManager.ensureInitialized(() => {
+    // Check: isFuseBoxOnlyFilterEnabled getter should return false.
+    assertFalse(filteredVolumeManager.isFuseBoxOnlyFilterEnabled);
+
     // Check: filteredVolumeManager.volumeInfoList should have 4 volumes.
     assertEquals(4, filteredVolumeManager.volumeInfoList.length);
 
@@ -172,6 +175,9 @@ export function testVolumeFuseboxOnlyFilter(done) {
       ['fusebox-only']);
 
   filteredVolumeManager.ensureInitialized(() => {
+    // Check: isFuseBoxOnlyFilterEnabled getter should return true.
+    assertTrue(filteredVolumeManager.isFuseBoxOnlyFilterEnabled);
+
     // Check: filteredVolumeManager.volumeInfoList should have 3 volumes.
     assertEquals(3, filteredVolumeManager.volumeInfoList.length);
 
