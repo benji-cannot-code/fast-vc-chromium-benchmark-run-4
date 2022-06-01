412e57b90de74dc7d18631f826688cd92c1022f1
Ken Buchanan <kenrb@chromium.org>
2022-06-01 17:02:39 +0000

    [WebAuthn] Improve UI for Touch-To-Fill WebAuthn Credentials
    Discussions around the UX for Conditional UI appearing in autofill
    surfaces is still continuing, but this updates the Touch-to-Fill sheet
    to match one of the drafts.
    
    These changes remain gated by
    --enable-features=WebAuthenticationConditionalUI.
    
    New UI screenshot: https://screenshot.googleplex.com/7dF7pSboMjN3Q36
    
    Bug: 1318942
    Change-Id: I6560baba84c73ddfad6cc5281e970524f481a4f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669598
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009664}

--- chrome/browser/touch_to_fill/touch_to_fill_webauthn_credential.h
+++ chrome/browser/touch_to_fill/touch_to_fill_webauthn_credential.h
@@ -8,12 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/types/strong_alias.h"
+
 // Represents a Web Authentication credential to be displayed on the
 // Touch-To-Fill sheet for user selection.
 class TouchToFillWebAuthnCredential {
  public:
-  TouchToFillWebAuthnCredential(const std::u16string& username,
-                                const std::string& backend_id);
+  using Username = base::StrongAlias<struct UsernameTag, std::u16string>;
+  using DisplayName = base::StrongAlias<struct DisplayNameTag, std::u16string>;
+  using BackendId = base::StrongAlias<struct BackendIdTag, std::string>;
+
+  TouchToFillWebAuthnCredential(const Username& username,
+                                const DisplayName& display_name,
+                                const BackendId& backend_id);
   ~TouchToFillWebAuthnCredential();
 
   TouchToFillWebAuthnCredential(const TouchToFillWebAuthnCredential&);
@@ -23,13 +30,16 @@ class TouchToFillWebAuthnCredential {
   TouchToFillWebAuthnCredential(TouchToFillWebAuthnCredential&&);
   TouchToFillWebAuthnCredential& operator=(TouchToFillWebAuthnCredential&&);
 
-  const std::u16string& username() const { return username_; }
+  const Username& username() const { return username_; }
 
-  const std::string& id() const { return backend_id_; }
+  const DisplayName& display_name() const { return display_name_; }
+
+  const BackendId& id() const { return backend_id_; }
 
  private:
-  std::u16string username_;
-  std::string backend_id_;
+  Username username_;
+  DisplayName display_name_;
+  BackendId backend_id_;
 };
 
 bool operator==(const TouchToFillWebAuthnCredential& lhs,
