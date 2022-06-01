a84cdb8a23a5ab24f77eb2166f193a65aa1a5fa2
Yoichi Osato <yoichio@chromium.org>
2022-06-01 21:51:32 +0000

    Make sure ProxyConfigServiceAndroid is created on BrowserThread::UI.
    As well as ProxyConfigMonitor [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/net/proxy_config_monitor.cc;drc=bd4f01fbf3c5d13ebfa43103a07ff592f5e70ac9;l=61
    
    Bug: 1320182
    Change-Id: I003745da25a8a9fb59f5273aa458b4cfd1d99fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676586
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009827}

--- android_webview/browser/network_service/aw_proxy_config_monitor.cc
+++ android_webview/browser/network_service/aw_proxy_config_monitor.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/no_destructor.h"
 #include "base/trace_event/trace_event.h"
+#include "content/public/browser/browser_thread.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/mojom/network_context.mojom.h"
@@ -55,6 +56,7 @@ constexpr net::NetworkTrafficAnnotationTag kProxyConfigTrafficAnnotation =
 }  // namespace
 
 AwProxyConfigMonitor::AwProxyConfigMonitor() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   TRACE_EVENT0("startup", "AwProxyConfigMonitor");
   proxy_config_service_android_ =
       std::make_unique<net::ProxyConfigServiceAndroid>(
