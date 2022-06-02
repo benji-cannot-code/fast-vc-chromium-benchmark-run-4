8124b4523a1c8ff47a19eed4e34451c38e7670f3
Loko Kung <lokokung@google.com>
2022-06-02 20:14:18 +0000

    Add implementation of caching interface for Dawn.
    - Adds implementation and infra for initial set of unit tests.
    - Adds mock classes for testing disk cache usage.
    
    Bug: dawn:549
    Change-Id: I65d8891b48b106bd92c791ee031f2d0a1a3e2c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617222
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010249}

---
+++ net/disk_cache/mock/mock_entry_impl.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/disk_cache/mock/mock_entry_impl.h"
+
+namespace disk_cache {
+
+EntryMock::EntryMock() = default;
+EntryMock::~EntryMock() = default;
+
+}  // namespace disk_cache
