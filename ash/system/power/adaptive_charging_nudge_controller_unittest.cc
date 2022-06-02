6a950721398691c9e67b09ef51b8cc18cb7819da
Tom Lukaszewicz <tluk@chromium.org>
2022-06-02 00:43:25 +0000

    [reland][views] Eliminate OnWidgetClosing from NudgeWidgetObserver
    Favor OnWidgetDestroying over OnWidgetClosing. OnWidgetClosing is
    only called where a Widget is closed directly, it fails to catch
    the case when the system destroys the NativeWidget directly. This
    can result in UAFs in situations where the OS directly destroys
    the widget.
    
    OnWidgetDestroying will be called every time a widget is destroyed
    and should be used instead in most cases.
    
    Bug: 1240365
    Change-Id: I65646d4aa75744582b7857268e584bd40b4f344b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641295
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1002735}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3660881
    Cr-Commit-Position: refs/heads/main@{#1009881}

--- ash/system/power/adaptive_charging_nudge_controller_unittest.cc
+++ ash/system/power/adaptive_charging_nudge_controller_unittest.cc
@@ -16,39 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/test/widget_test.h"
 #include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_observer.h"
 
 namespace ash {
 
 namespace {
-
-// Utility class to wait/observe the closing of the nudge.
-class NudgeWidgetObserver : public views::WidgetObserver {
- public:
-  NudgeWidgetObserver(views::Widget* widget) {
-    if (!widget)
-      return;
-
-    widget_observation_.Observe(widget);
-  }
-
-  void WaitForClose() {
-    run_loop_ = std::make_unique<base::RunLoop>();
-    run_loop_->Run();
-  }
-
-  // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override {
-    if (run_loop_)
-      run_loop_->Quit();
-  }
-
- private:
-  base::ScopedObservation<views::Widget, views::WidgetObserver>
-      widget_observation_{this};
-  std::unique_ptr<base::RunLoop> run_loop_;
-};
 
 // Enables or disables the user pref for the entire feature.
 void SetAdaptiveChargingPref(bool enabled) {
@@ -76,8 +49,8 @@ class AdaptiveChargingNudgeControllerTest : public AshTestBase {
 
   AdaptiveChargingNudgeController* GetController() { return controller_.get(); }
 
-  void WaitForWidgetClose(AdaptiveChargingNudgeController* controller,
-                          SystemNudge* nudge) {
+  void WaitForWidgetDestruction(AdaptiveChargingNudgeController* controller,
+                                SystemNudge* nudge) {
     views::Widget* nudge_widget = nudge->widget();
     ASSERT_TRUE(nudge_widget);
     EXPECT_FALSE(nudge_widget->IsClosed());
@@ -87,12 +60,11 @@ class AdaptiveChargingNudgeControllerTest : public AshTestBase {
         ui::ScopedAnimationDurationScaleMode::SLOW_DURATION);
 
     // Pretend the hide nudge timer has elapsed.
-    NudgeWidgetObserver widget_close_observer(nudge_widget);
     controller->FireHideNudgeTimerForTesting();
-
     EXPECT_TRUE(nudge_widget->GetLayer()->GetAnimator()->is_animating());
 
-    widget_close_observer.WaitForClose();
+    views::test::WidgetDestroyedWaiter widget_destroyed_waiter(nudge_widget);
+    widget_destroyed_waiter.Wait();
   }
 
  private:
@@ -110,7 +82,7 @@ TEST_F(AdaptiveChargingNudgeControllerTest, ShowsAndHidesNudge) {
   SystemNudge* nudge = controller->GetSystemNudgeForTesting();
   ASSERT_TRUE(nudge);
 
-  WaitForWidgetClose(controller, nudge);
+  WaitForWidgetDestruction(controller, nudge);
 }
 
 TEST_F(AdaptiveChargingNudgeControllerTest, NoNudgeShowForDisabledFeature) {
@@ -136,7 +108,7 @@ TEST_F(AdaptiveChargingNudgeControllerTest, NudgeShowExactlyOnce) {
   controller->GetNudgeDelayTimerForTesting()->FireNow();
   SystemNudge* nudge1 = controller->GetSystemNudgeForTesting();
   ASSERT_TRUE(nudge1);
-  WaitForWidgetClose(controller, nudge1);
+  WaitForWidgetDestruction(controller, nudge1);
 
   // No nudge for the second time.
   controller->ShowNudge();
