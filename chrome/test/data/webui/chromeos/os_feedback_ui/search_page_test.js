7ef864389b7544a80575a53e79d3562f3dd3a8cb
Longbo Wei <longbowei@google.com>
2022-06-02 02:50:54 +0000

    Feedback: Add localized strings in Search Page
    *Add localized string for feedback_tips && description_empty_error.
    
    *Screenshot: http://screen/4dRQABEkxnmVfHP
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I0d02ff87f0794a70760c504107a4ef4338e6a47c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680293
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009917}

--- chrome/test/data/webui/chromeos/os_feedback_ui/search_page_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/search_page_test.js
@@ -62,6 +62,8 @@ export function searchPageTestSuite() {
     const writingGuidanceLink =
         page.shadowRoot.querySelector('#feedbackWritingGuidance');
     assertTrue(!!writingGuidanceLink);
+    assertEquals(
+        'Tips on writing feedback', writingGuidanceLink.textContent.trim());
     assertEquals('_blank', writingGuidanceLink.target);
     assertEquals(
         'https://support.google.com/chromebook/answer/2982029',
@@ -195,6 +197,8 @@ export function searchPageTestSuite() {
     buttonContinue.click();
     // Verify that the message is not hidden now.
     assertFalse(errorMsg.hidden);
+    assertEquals('Description is required', errorMsg.textContent.trim());
+
     // Verify that the textarea received focus again.
     assertEquals(page.shadowRoot.activeElement, textInput);
 
