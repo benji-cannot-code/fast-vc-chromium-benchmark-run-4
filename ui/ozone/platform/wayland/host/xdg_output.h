2e9289eff44694c2d37107b35c210ceee557fbb6
Addison Luh <aluh@chromium.org>
2022-06-02 00:46:36 +0000

    [lacros] Fix output origin and logical size handling.
    The lacros client was handling wl_output.geometry.x/y as physical coordinates, but it should be in DIP screen coordinates. This change fixes that.
    
    In addition, this change also implements the handling of zxdg_output.logical_position and uses it as the logical origin of the display. If this event was not sent, it falls back to using wl_output.geometry's position.
    
    Further, this change fixes the sending of logical size via zxdg_output.logical_size. Before, that event sent bounds that had been rotated by the panel rotation, which was incorrect. But it didn't cause any issues because it was only used to calculate fractional scale against the physical size sent by wl_output.geometry, which also was rotated by the panel rotation.
    
    This change fixes zxdg_output.logical_size to send the proper unrotated logical size and updates the scaling calculation. Then it uses this logical size to set the client display's bounds. If this event was not sent, it falls back to calculating the logical size from wl_output.mode's physical size.
    
    Bug: 1325487
    Change-Id: I2bed90e21ffd5b4d9f76ebdd82e51fdfc46b491c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661448
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009883}

--- ui/ozone/platform/wayland/host/xdg_output.h
+++ ui/ozone/platform/wayland/host/xdg_output.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 
@@ -20,6 +22,9 @@ class XDGOutput {
   XDGOutput& operator=(const XDGOutput&) = delete;
   ~XDGOutput();
 
+  absl::optional<gfx::Point> logical_position() const {
+    return logical_position_;
+  }
   gfx::Size logical_size() const { return logical_size_; }
 
  private:
@@ -41,6 +46,7 @@ class XDGOutput {
                                       const char* description);
 
   wl::Object<zxdg_output_v1> xdg_output_;
+  absl::optional<gfx::Point> logical_position_;
   gfx::Size logical_size_;
 };
 
