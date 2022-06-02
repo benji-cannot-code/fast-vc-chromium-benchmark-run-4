b4a5d71f0e2f76d09490e4a5d875f5965b8e3103
keno <kenoss@chromium.org>
2022-06-02 07:26:02 +0000

    PageLoadMetrics: LocalNetworkRequestsPageLoadMetricsObserver for FencedFrames
    This CL makes LocalNetworkRequestsPageLoadMetricsObserver return
    FORWARD_OBSERVING on OnFencedFrameStart to collect metrics at the
    observer running at the outermost tracker.
    
    Bug: 1317494
    Change-Id: I402fcfe993e167c78f61744c348c64a5b9d25281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666017
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009974}

--- chrome/browser/page_load_metrics/observers/local_network_requests_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/local_network_requests_page_load_metrics_observer.h
@@ -73,6 +73,7 @@ class LocalNetworkRequestsPageLoadMetricsObserver
   ~LocalNetworkRequestsPageLoadMetricsObserver() override;
 
   // page_load_metrics::PageLoadMetricsObserver
+  const char* GetObserverName() const override;
   ObservePolicy OnFencedFramesStart(
       content::NavigationHandle* navigation_handle,
       const GURL& currently_committed_url) override;
