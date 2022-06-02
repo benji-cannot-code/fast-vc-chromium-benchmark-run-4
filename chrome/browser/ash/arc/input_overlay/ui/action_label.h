37bbd5b1ba31ec5aa1028a4f5cb3f1daf2bcf159
Cici Ruan <cuicuiruan@google.com>
2022-06-02 21:44:31 +0000

    arc/gio: Remove |ActionLabel| size change
    On mouse hover and view focus, |ActionLabel| size stays the same as
    required by UI design.
    
    Bug: b:232948415 b:188452744
    Test: manual test and unittest
    Change-Id: If37e972e558fd28c26e49e8ead8cbdac7291910d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688229
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010298}

--- chrome/browser/ash/arc/input_overlay/ui/action_label.h
+++ chrome/browser/ash/arc/input_overlay/ui/action_label.h
@@ -68,7 +68,6 @@ class ActionLabel : public views::LabelButton {
   void SetToEditUnBind();
 
   MouseAction mouse_action_ = MouseAction::NONE;
-  EditState edit_state_ = EditState::kNone;
 };
 }  // namespace input_overlay
 }  // namespace arc
