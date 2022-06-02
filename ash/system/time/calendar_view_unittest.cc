f67583a36a9cf100c1599a6a041bc01e376e8634
Elliot Tuck <etuck@chromium.org>
2022-06-02 21:12:11 +0000

    Increase font weight when date cell is selected
    Today date cell selected (before): https://screenshot.googleplex.com/yBccy6yWawAqLVC
    Non-today date cell selected (before): https://screenshot.googleplex.com/BZnf7QCyoKzngJU
    Today date cell selected (after): https://screenshot.googleplex.com/6YBmL5MbRsQeVah
    Non-today date cell selected (after): https://screenshot.googleplex.com/4G2PaegZwrjRAwD
    
    Bug: 1304496,1304497
    Change-Id: I9d328c7dd6da48e99ce6109f43564bbd5fbacd7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3597539
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010281}

--- ash/system/time/calendar_view_unittest.cc
+++ ash/system/time/calendar_view_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/time/calendar_view.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/public/cpp/ash_typography.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/close_button.h"
@@ -54,6 +55,10 @@ class CalendarViewTest : public AshTestBase {
   CalendarViewTest& operator=(const CalendarViewTest&) = delete;
   ~CalendarViewTest() override = default;
 
+  static int GetTextStyle(const CalendarDateCellView* date_cell) {
+    return date_cell->label()->GetTextStyle();
+  }
+
   void SetUp() override {
     AshTestBase::SetUp();
 
@@ -77,12 +82,12 @@ class CalendarViewTest : public AshTestBase {
   }
 
   // Gets date cell of a given CalendarMonthView and numerical `day`.
-  const views::LabelButton* GetDateCell(CalendarMonthView* month,
-                                        std::u16string day) {
-    const views::LabelButton* date_cell = nullptr;
+  const ash::CalendarDateCellView* GetDateCell(CalendarMonthView* month,
+                                               std::u16string day) {
+    const ash::CalendarDateCellView* date_cell = nullptr;
     for (const auto* child_view : month->children()) {
       auto* current_date_cell =
-          static_cast<const views::LabelButton*>(child_view);
+          static_cast<const ash::CalendarDateCellView*>(child_view);
       if (day != current_date_cell->GetText())
         continue;
 
@@ -986,6 +991,42 @@ TEST_F(CalendarViewTest, EventListBoundsTest) {
   EXPECT_EQ(bottom_of_scroll_view_visible_area, top_of_event_list_view);
 }
 
+// Tests that grayed-out dates are not emphasized when corresponding
+// non-grayed-out dates are selected.
+TEST_F(CalendarViewTest, GrayedOutDatesNotEmphasized) {
+  ui::ScopedAnimationDurationScaleMode test_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::ZERO_DURATION);
+  // Create a month view based on June 29, 2021.
+  base::Time date;
+  ASSERT_TRUE(base::Time::FromString("29 Jun 2021 10:00 GMT", &date));
+  SetFakeNow(date);
+  base::subtle::ScopedTimeClockOverrides time_override(
+      &CalendarViewTest::FakeTimeNow, /*time_ticks_override=*/nullptr,
+      /*thread_ticks_override=*/nullptr);
+  CreateCalendarView();
+
+  // Select today's date.
+  ClickDateCell(GetDateCell(/*month=*/current_month(), /*day=*/u"29"));
+
+  // Expect grayed-out version of today's date cell to not be emphasized.
+  const auto* today_date_cell_grayed_out =
+      GetDateCell(/*month=*/next_month(), /*day=*/u"29");
+  EXPECT_EQ(views::style::TextStyle::STYLE_PRIMARY,
+            GetTextStyle(today_date_cell_grayed_out));
+
+  // Close event list.
+  CloseEventList();
+
+  // Select non-today's date cell.
+  ClickDateCell(GetDateCell(/*month=*/current_month(), /*day=*/u"30"));
+
+  // Expect grayed-out version of non-today's date cell to not be emphasized.
+  const auto* tomorrow_date_cell_grayed_out =
+      GetDateCell(/*month=*/next_month(), /*day=*/u"30");
+  EXPECT_EQ(views::style::TextStyle::STYLE_PRIMARY,
+            GetTextStyle(tomorrow_date_cell_grayed_out));
+}
+
 // A test class for testing animation. This class cannot set fake now since it's
 // using `MOCK_TIME` to test the animations, and it can't inherit from
 // CalendarAnimationTest due to the same reason.
@@ -1026,12 +1067,12 @@ class CalendarViewAnimationTest : public AshTestBase {
   }
 
   // Gets date cell of a given CalendarMonthView and numerical `day`.
-  const views::LabelButton* GetDateCell(CalendarMonthView* month,
-                                        std::u16string day) {
-    const views::LabelButton* date_cell = nullptr;
+  const ash::CalendarDateCellView* GetDateCell(CalendarMonthView* month,
+                                               std::u16string day) {
+    const ash::CalendarDateCellView* date_cell = nullptr;
     for (const auto* child_view : month->children()) {
       auto* current_date_cell =
-          static_cast<const views::LabelButton*>(child_view);
+          static_cast<const ash::CalendarDateCellView*>(child_view);
       if (day != current_date_cell->GetText())
         continue;
 
