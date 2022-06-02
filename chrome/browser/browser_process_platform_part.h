a4b62d3dcacce3d6d534615d2f2970d3c38cc1fc
Tom Lukaszewicz <tluk@chromium.org>
2022-06-02 21:31:11 +0000

    [lacros] Align Startup URLs startup pref behavior with ash-chrome
    This CL aligns behavior across ash-chrome and lacros-chrome when
    restoring browsers with the URLS and LAST_AND_URLS session startup
    pref.
    
    BrowserProcessPlatformPartChromeOS is split into a class for chromeos
    generally and a subclass for ash specifically.
    
    Both ash-chrome and lacros-chrome can open new browser windows from
    a pre-existing windowless state and are expected to honor user
    startup prefs while doing so. Having both share the same code path
    ensures that this behavior is kept in lock-step between the two.
    
    This tests adds ChromeOS specific test cases and opts lacros-chrome
    out of session restore tests that were asserting the old behavior.
    
    Bug: 1281021
    Change-Id: I8a87ff11aff41a1dd4ba883f383d2443b0f46856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635671
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010291}

--- chrome/browser/browser_process_platform_part.h
+++ chrome/browser/browser_process_platform_part.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part_android.h"
 #elif BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/browser_process_platform_part_ash.h"
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/browser_process_platform_part_chromeos.h"
+class BrowserProcessPlatformPart : public BrowserProcessPlatformPartChromeOS {};
 #elif BUILDFLAG(IS_MAC)
 #include "chrome/browser/browser_process_platform_part_mac.h"
 #elif BUILDFLAG(IS_WIN)
