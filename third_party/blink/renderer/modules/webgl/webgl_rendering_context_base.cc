29826564be18cc7a779c0b56f5d29634e75775f5
Kenneth Russell <kbr@chromium.org>
2022-06-02 00:56:55 +0000

    Remove WEBKIT_ prefixes from WebGL extensions.
    Remove the legacy WEBKIT_ prefixes from the following WebGL
    extensions:
      EXT_texture_filter_anisotropic
      WEBGL_compressed_texture_pvrtc
      WEBGL_compressed_texture_s3tc
      WEBGL_depth_texture
      WEBGL_lose_context
    
    It's been years since the WebGL working group switched to the current
    extension approval process, eliminating the need for these vendor
    prefixes, and applications should not be referencing them.
    
    Bug: 967062
    Change-Id: If037807448a5ef2bdb6d7fa00378c3e772d56d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680622
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009888}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -3202,23 +3202,10 @@ GLenum WebGLRenderingContextBase::getError() {
   return ContextGL()->GetError();
 }
 
-const char* const* WebGLRenderingContextBase::ExtensionTracker::Prefixes()
-    const {
-  static const char* const kUnprefixed[] = {
-      "",
-      nullptr,
-  };
-  return prefixes_ ? prefixes_ : kUnprefixed;
-}
-
-bool WebGLRenderingContextBase::ExtensionTracker::MatchesNameWithPrefixes(
+bool WebGLRenderingContextBase::ExtensionTracker::MatchesName(
     const String& name) const {
-  const char* const* prefix_set = Prefixes();
-  for (; *prefix_set; ++prefix_set) {
-    String prefixed_name = String(*prefix_set) + ExtensionName();
-    if (DeprecatedEqualIgnoringCase(prefixed_name, name)) {
-      return true;
-    }
+  if (DeprecatedEqualIgnoringCase(ExtensionName(), name)) {
+    return true;
   }
   return false;
 }
@@ -3257,7 +3244,7 @@ ScriptValue WebGLRenderingContextBase::getExtension(ScriptState* script_state,
 
   if (!isContextLost()) {
     for (ExtensionTracker* tracker : extensions_) {
-      if (tracker->MatchesNameWithPrefixes(name)) {
+      if (tracker->MatchesName(name)) {
         if (ExtensionSupportedAndAllowed(tracker)) {
           extension = tracker->GetExtension(this);
           if (extension) {
@@ -3996,11 +3983,7 @@ WebGLRenderingContextBase::getSupportedExtensions() {
 
   for (ExtensionTracker* tracker : extensions_) {
     if (ExtensionSupportedAndAllowed(tracker)) {
-      const char* const* prefixes = tracker->Prefixes();
-      for (; *prefixes; ++prefixes) {
-        String prefixed_name = String(*prefixes) + tracker->ExtensionName();
-        result.push_back(prefixed_name);
-      }
+      result.push_back(tracker->ExtensionName());
     }
   }
 
