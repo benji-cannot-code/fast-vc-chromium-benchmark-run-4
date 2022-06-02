25211f969bc98f6da6ad9a1971142eab3cf90498
Matt Menke <mmenke@chromium.org>
2022-06-02 00:29:45 +0000

    Update a couple net/log APIs to return base::Value::Dicts.
    Several net/log APIs were returning base::Values to callers that expect
    to receive dictionaries. The new way to do this is to return
    base::Value::Dicts instead.
    
    
    Bug: 1187001
    Change-Id: Ia60398d646e36661d810063fe1c48b6119f45249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3677456
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009876}

--- fuchsia/engine/browser/web_engine_net_log_observer.cc
+++ fuchsia/engine/browser/web_engine_net_log_observer.cc
@@ -19,21 +19,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-std::unique_ptr<base::DictionaryValue> GetWebEngineConstants() {
-  std::unique_ptr<base::DictionaryValue> constants_dict =
-      base::DictionaryValue::From(
-          base::Value::ToUniquePtrValue(net::GetNetConstants()));
+std::unique_ptr<base::Value> GetWebEngineConstants() {
+  base::Value::Dict constants_dict = net::GetNetConstants();
 
-  base::DictionaryValue dict;
-  dict.SetKey("name", base::Value("WebEngine"));
-  dict.SetKey(
-      "command_line",
-      base::Value(
-          base::CommandLine::ForCurrentProcess()->GetCommandLineString()));
+  base::Value::Dict dict;
+  dict.Set("name", "WebEngine");
+  dict.Set("command_line",
+           base::CommandLine::ForCurrentProcess()->GetCommandLineString());
 
-  constants_dict->SetKey("clientInfo", std::move(dict));
+  constants_dict.Set("clientInfo", std::move(dict));
 
-  return constants_dict;
+  return std::make_unique<base::Value>(std::move(constants_dict));
 }
 
 }  // namespace
