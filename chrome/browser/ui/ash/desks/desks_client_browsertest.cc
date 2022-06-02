b6e179c4f5e132a4bd24478cca71455f6d587635
Morten Stenshorne <mstensho@chromium.org>
2022-06-02 10:40:41 +0000

    Revert "Don't maximize in browser tests"
    This reverts commit 63a9ef88018617b46c0e26af7355d76baac65e6f.
    
    Reason for revert: Speculative revert for FullscreenOnScreensChangeFullscreenControllerInteractiveTest.FullscreenOnScreensChange failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/26728/overview
    
    Original change's description:
    > Don't maximize in browser tests
    >
    > Currently we maximize it first then restore it which is
    > problematic with lacros because state change happens in async
    > and can make tests flaky.
    >
    > Just don't maximize the browser window in browser tests.
    >
    > Bug: 1261321
    >
    > Change-Id: I15518eb2d7c9baca8ee2ec8d8f72fd1149961705
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3679802
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1009922}
    
    Bug: 1261321
    Change-Id: I77179ed6274c58addf7113295bcc44ca088f179f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686572
    Owners-Override: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010019}

--- chrome/browser/ui/ash/desks/desks_client_browsertest.cc
+++ chrome/browser/ui/ash/desks/desks_client_browsertest.cc
@@ -1711,10 +1711,7 @@ IN_PROC_BROWSER_TEST_F(DesksTemplatesClientTest,
   auto* screen = display::Screen::GetScreen();
   EXPECT_EQ(screen->GetDisplayNearestWindow(window).id(),
             data->display_id.value());
-  auto normalize_state = [](ui::WindowShowState state) {
-    return state == ui::SHOW_STATE_DEFAULT ? ui::SHOW_STATE_NORMAL : state;
-  };
-  EXPECT_EQ(normalize_state(window->GetProperty(aura::client::kShowStateKey)),
+  EXPECT_EQ(window->GetProperty(aura::client::kShowStateKey),
             chromeos::ToWindowShowState(data->window_state_type.value()));
   // We don't capture the window's desk_id as a template will always
   // create in a new desk.
@@ -1736,9 +1733,9 @@ IN_PROC_BROWSER_TEST_F(DesksTemplatesClientTest,
   EXPECT_FALSE(data2->desk_id.has_value());
   EXPECT_EQ(screen->GetDisplayNearestWindow(window).id(),
             data->display_id.value());
-  EXPECT_EQ(normalize_state(window->GetProperty(aura::client::kShowStateKey)),
+  EXPECT_EQ(window->GetProperty(aura::client::kShowStateKey),
             chromeos::ToWindowShowState(data->window_state_type.value()));
-  EXPECT_EQ(normalize_state(window->GetProperty(aura::client::kShowStateKey)),
+  EXPECT_EQ(window->GetProperty(aura::client::kShowStateKey),
             chromeos::ToWindowShowState(data->window_state_type.value()));
   EXPECT_FALSE(data2->desk_id.has_value());
 }
