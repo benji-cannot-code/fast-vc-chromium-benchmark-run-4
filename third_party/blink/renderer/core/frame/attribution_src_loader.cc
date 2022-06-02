28872615b1f6a1cf2223a8e1cb49dc589c3d277c
Andrew Paseltiner <apaseltiner@chromium.org>
2022-06-02 13:03:43 +0000

    Set Attribution-Reporting-Eligible header on attributionsrc requests
    The value of this header is a structured dictionary representing a set
    of zero or more tokens {navigation-source, event-source, trigger} that
    is used to tell the reporting origin which registrations are allowed, if
    any.
    
    In this CL, the renderer doesn't interpret the value of this header, as
    it is entirely under the renderer's control. In a followup CL, however,
    user-initiated requests (e.g. window.fetch) will be able to opt in to
    registration by setting this header appropriately, at which point the
    browser will interpret the header's complete absence to be equivalent to
    `trigger`, and otherwise parse the request header as a structured
    dictionary to determine which registrations to allow, if any, in the
    response.
    
    We make this a structured dictionary to give it a standard format and to
    support future extensions, e.g. parameterizations of the individual
    fields.
    
    https://github.com/WICG/attribution-reporting-api/blob/main/EVENT.md#registration-requests
    
    Bug: 1325874
    Change-Id: Ifd29d4f8f4121f9f1fa4a60d43d3ef4877b1ff3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668982
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010048}

--- third_party/blink/renderer/core/frame/attribution_src_loader.cc
+++ third_party/blink/renderer/core/frame/attribution_src_loader.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
@@ -235,6 +236,25 @@ AttributionSrcLoader::ResourceClient* AttributionSrcLoader::DoRegistration(
 
   request.SetKeepalive(true);
   request.SetRequestContext(mojom::blink::RequestContextType::ATTRIBUTION_SRC);
+
+  const char* eligible = [src_type,
+                          associated_with_navigation]() -> const char* {
+    switch (src_type) {
+      case SrcType::kSource:
+        return associated_with_navigation ? "navigation-source"
+                                          : "event-source";
+      case SrcType::kTrigger:
+        NOTREACHED();
+        return nullptr;
+      case SrcType::kUndetermined:
+        DCHECK(!associated_with_navigation);
+        return "event-source, trigger";
+    }
+  }();
+
+  request.SetHttpHeaderField(http_names::kAttributionReportingEligible,
+                             eligible);
+
   FetchParameters params(std::move(request),
                          local_frame_->DomWindow()->GetCurrentWorld());
   params.MutableOptions().initiator_info.name =
