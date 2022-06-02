bb9e6707f68f5bdb3a5c6bbc4912abd6afee5d1f
Tom Lukaszewicz <tluk@chromium.org>
2022-06-02 15:37:20 +0000

    [views] Eliminate OnWidgetClosing from clipboard nudge unittest
    This CL removes an unnecessary NudgeWigetObserver from the clipboard
    nudge unittest. It has been replaced with a check against
    Widget::IsClosed() which achieves the same result.
    
    Removal of this observer helps with existing efforts to deprectate
    Widget::OnWidgetClosing.
    
    Bug: 1240365
    Change-Id: I769aced1165706c5326c0ac34adb0710e2875777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3684877
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010111}

--- ash/clipboard/clipboard_nudge_controller_unittest.cc
+++ ash/clipboard/clipboard_nudge_controller_unittest.cc
@@ -26,33 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_observer.h"
 
 namespace ash {
-namespace {
-
-class NudgeWigetObserver : public views::WidgetObserver {
- public:
-  NudgeWigetObserver(views::Widget* widget) : widget_(widget) {
-    if (widget_)
-      widget_->AddObserver(this);
-  }
-
-  ~NudgeWigetObserver() override { CleanupWidget(); }
-
-  bool WidgetClosed() const { return !widget_; }
-
-  // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override { CleanupWidget(); }
-
-  void CleanupWidget() {
-    if (widget_) {
-      widget_->RemoveObserver(this);
-      widget_ = nullptr;
-    }
-  }
-
- private:
-  views::Widget* widget_;
-};
-}  // namespace
 
 class ClipboardNudgeControllerTest : public AshTestBase {
  public:
@@ -273,11 +246,9 @@ TEST_F(ClipboardNudgeControllerTest, ShowZeroStateNudgeAfterOngoingNudge) {
   EXPECT_FALSE(nudge_widget->IsClosed());
   EXPECT_EQ(ClipboardNudgeType::kOnboardingNudge, nudge->nudge_type());
 
-  NudgeWigetObserver widget_close_observer(nudge_widget);
-
   ShowNudgeForType(ClipboardNudgeType::kZeroStateNudge);
   // Verify the old nudge widget was closed.
-  EXPECT_TRUE(widget_close_observer.WidgetClosed());
+  EXPECT_TRUE(nudge_widget->IsClosed());
 
   nudge = static_cast<ClipboardNudge*>(
       nudge_controller_->GetSystemNudgeForTesting());
