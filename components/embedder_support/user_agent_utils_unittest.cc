abef5d7ffab9fcca43f0fa19f0baa86caec239b8
Ari Chivukula <arichiv@chromium.org>
2022-06-02 01:02:26 +0000

    [Fix UserAgentReduction Policy] (1) Make ForceMajorVersionToMinorPosition a class enum
    Before we fix the issue with UserAgentReduction we should make the first
    argument to GetProductAndVersion safer so that it won't be confused with
    the new second argument.
    
    This CL is part of a series:
    (1) Make ForceMajorVersionToMinorPosition a class enum
    (2) Fix GetProductAndVersion
    
    Bug: 1330588
    Change-Id: Ic6e43a67410a030b4bb12f5eb646907bc199303f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3681901
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009892}

--- components/embedder_support/user_agent_utils_unittest.cc
+++ components/embedder_support/user_agent_utils_unittest.cc
@@ -429,7 +429,8 @@ TEST_F(UserAgentUtilsTest, UserAgentStringReduced) {
        blink::features::kForceMajorVersionInMinorPositionInUserAgent},
       {});
   {
-    std::string buffer = GetReducedUserAgent(kForceDisabled);
+    std::string buffer =
+        GetReducedUserAgent(ForceMajorVersionToMinorPosition::kForceDisabled);
     std::string device_compat = "Mobile ";
     EXPECT_EQ(buffer,
               base::StringPrintf(content::frozen_user_agent_strings::kAndroid,
@@ -466,7 +467,8 @@ TEST_F(UserAgentUtilsTest, UserAgentStringReduced) {
        blink::features::kForceMajorVersionInMinorPositionInUserAgent},
       {});
   {
-    std::string buffer = GetReducedUserAgent(kForceDisabled);
+    std::string buffer =
+        GetReducedUserAgent(ForceMajorVersionToMinorPosition::kForceDisabled);
     EXPECT_EQ(buffer, base::StringPrintf(
                           content::frozen_user_agent_strings::kDesktop,
                           content::GetUnifiedPlatform().c_str(),
@@ -548,7 +550,8 @@ TEST_F(UserAgentUtilsTest, UserAgentMetadata) {
 
   // ForceMajorVersionToMinorPosition: kForceDisabled
   pref_service.registry()->RegisterIntegerPref(
-      kForceMajorVersionToMinorPosition, kForceDisabled);
+      kForceMajorVersionToMinorPosition,
+      static_cast<int>(ForceMajorVersionToMinorPosition::kForceDisabled));
   metadata = GetUserAgentMetadata(&pref_service);
   EXPECT_EQ(metadata.full_version, full_version);
   EXPECT_TRUE(ContainsBrandVersion(metadata.brand_version_list,
@@ -570,7 +573,9 @@ TEST_F(UserAgentUtilsTest, UserAgentMetadata) {
                                     major_to_minor_product_brand_full_version));
 
   // ForceMajorVersionToMinorPosition: kForceEnabled
-  pref_service.SetInteger(kForceMajorVersionToMinorPosition, kForceEnabled);
+  pref_service.SetInteger(
+      kForceMajorVersionToMinorPosition,
+      static_cast<int>(ForceMajorVersionToMinorPosition::kForceEnabled));
   metadata = GetUserAgentMetadata(&pref_service);
   EXPECT_EQ(metadata.full_version, major_to_minor_full_version);
   EXPECT_TRUE(ContainsBrandVersion(metadata.brand_version_list,
@@ -1008,7 +1013,8 @@ TEST_F(UserAgentUtilsTest, GetProductAndVersion) {
   EXPECT_EQ(minor_version, "0");
 
   // Ensure policy is respected if ForceMajorToMinor is force enabled
-  product = GetProductAndVersion(/*force_major_to_minor=*/kForceEnabled);
+  product =
+      GetProductAndVersion(ForceMajorVersionToMinorPosition::kForceEnabled);
   EXPECT_TRUE(re2::RE2::FullMatch(product, kChromeProductVersionRegex,
                                   &major_version, &minor_version));
   EXPECT_EQ(major_version, "99");
@@ -1039,7 +1045,8 @@ TEST_F(UserAgentUtilsTest, GetProductAndVersion) {
       /*enabled_features=*/{blink::features::
                                 kForceMajorVersionInMinorPositionInUserAgent},
       /*disabled_features=*/{blink::features::kReduceUserAgentMinorVersion});
-  product = GetProductAndVersion(/*force_major_to_minor=*/kForceDisabled);
+  product =
+      GetProductAndVersion(ForceMajorVersionToMinorPosition::kForceDisabled);
   EXPECT_TRUE(re2::RE2::FullMatch(product, kChromeProductVersionRegex,
                                   &major_version, &minor_version,
                                   &build_version));
