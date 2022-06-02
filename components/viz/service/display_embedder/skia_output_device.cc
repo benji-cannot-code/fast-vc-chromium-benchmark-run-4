b83fde7d048a391021c2e256d75871b00b9d1f50
Abdias Dagbekpo <adagbekpo@google.com>
2022-06-02 00:38:27 +0000

    [Task]: Introduced finch experiment for AsyncGpuLatencyReporting feature
    crrev.com/c/2242634 moved the reporting of GPU swap buffer latency to an asynchronous BEST_EFFORT task, based on the observation that it could
    take time to execute. In reality, the task takes 0.5ms to execute at the 99th percentile on Mac. It's possible that posting the task is more costly than running it synchronously. We want to experiment with running the "ReportLatency" function synchronously and measure if there is any impact on frame throughput.
    
    Bug: 1327733
    Change-Id: Iff5741f2e551db0b4f86488a10111c1e355afa9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3677372
    Commit-Queue: Abdias Dagbekpo <adagbekpo@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009879}

--- components/viz/service/display_embedder/skia_output_device.cc
+++ components/viz/service/display_embedder/skia_output_device.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/notreached.h"
+#include "base/task/task_features.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
@@ -28,11 +30,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 namespace {
 
+const base::Feature kAsyncGpuLatencyReporting CONSTINIT{
+    "AsyncGpuLatencyReporting", base::FEATURE_ENABLED_BY_DEFAULT};
+
 using ::perfetto::protos::pbzero::ChromeLatencyInfo;
 
 scoped_refptr<base::SequencedTaskRunner> CreateLatencyTracerRunner() {
   if (!base::ThreadPoolInstance::Get())
     return nullptr;
+
+  if (!base::FeatureList::IsEnabled(kAsyncGpuLatencyReporting))
+    return nullptr;
+
   return base::ThreadPool::CreateSequencedTaskRunner(
       {base::TaskPriority::BEST_EFFORT,
        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
