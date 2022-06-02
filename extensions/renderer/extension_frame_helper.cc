6677405cca7293d79b00c28481c470406bd3f844
Clark DuVall <cduvall@chromium.org>
2022-06-02 00:51:16 +0000

    Avoid script context being created too early in ExtensionFrameHelper
    The call to ScriptContextSet::GetMainWorldContextForFrame() in the
    GetExtensionFromFrame() function can force a script context to be
    created if one does not exist yet for the frame. This can reduce the
    benefit of optimizations which try to run before the script context is
    created such as http://crrev.com/c/3656060 and
    http://crrev.com/c/3400186.
    
    This change moves the extensions logic to DidCreateScriptContext() which
    will be called when the script context for the frame is created. The
    change is behind a base::Feature since it may have a measurable impact
    on performance.
    
    Bug: 1328448
    Change-Id: Id0e5d887e93d4ad51bd5e2baa9b5b868597c0d7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674405
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009885}

--- extensions/renderer/extension_frame_helper.cc
+++ extensions/renderer/extension_frame_helper.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/api/messaging/message.h"
 #include "extensions/common/api/messaging/port_id.h"
 #include "extensions/common/constants.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/extension_messages.h"
 #include "extensions/common/manifest_handlers/background_info.h"
 #include "extensions/renderer/api/automation/automation_api_helper.h"
@@ -365,6 +367,10 @@ void ExtensionFrameHelper::ReadyToCommitNavigation(
 
 void ExtensionFrameHelper::DidCommitProvisionalLoad(
     ui::PageTransition transition) {
+  if (base::FeatureList::IsEnabled(
+          extensions_features::kAvoidEarlyExtensionScriptContextCreation)) {
+    return;
+  }
   // Grant cross browsing instance frame lookup if we are an extension. This
   // should match the conditions in FindFrame.
   content::RenderFrame* frame = render_frame();
@@ -567,6 +573,19 @@ void ExtensionFrameHelper::ExecuteDeclarativeScript(
 
 void ExtensionFrameHelper::UpdateBrowserWindowId(int32_t window_id) {
   browser_window_id_ = window_id;
+}
+
+void ExtensionFrameHelper::NotifyDidCreateScriptContext(int32_t world_id) {
+  did_create_script_context_ = true;
+  if (world_id == blink::kMainDOMWorldId &&
+      base::FeatureList::IsEnabled(
+          extensions_features::kAvoidEarlyExtensionScriptContextCreation)) {
+    // Grant cross browsing instance frame lookup if we are an extension. This
+    // should match the conditions in FindFrame.
+    content::RenderFrame* frame = render_frame();
+    if (GetExtensionFromFrame(frame))
+      frame->SetAllowsCrossBrowsingInstanceFrameLookup();
+  }
 }
 
 void ExtensionFrameHelper::OnDestruct() {
