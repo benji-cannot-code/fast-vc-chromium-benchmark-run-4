5a46a63dbb3b498f285f0d7847c9a7d6da60154e
Christine Smith <christinesm@chromium.org>
2022-06-02 18:00:13 +0000

    [Storage Code Health] Remove expired histograms: Storage.Blob.I*
    Removes the expired histograms:
    	Storage.Blob.IDBRequestLoader.ReadError
    	Storage.Blob.InvalidReference
    	Storage.Blob.InvalidURLRegister
    
    Bug: 1244912, 1314419
    Change-Id: I4c95c74a5d19e6a274138c8056baeafc928da109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662600
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010183}

--- third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -131,10 +130,6 @@ void IDBRequestLoader::DidFail(FileErrorCode) {
   DCHECK(file_reader_loading_);
   file_reader_loading_ = false;
 #endif  // DCHECK_IS_ON()
-
-  base::UmaHistogramSparse("Storage.Blob.IDBRequestLoader.ReadError",
-                           std::max(0, -loader_->GetNetError()));
-
   ReportError();
 }
 
