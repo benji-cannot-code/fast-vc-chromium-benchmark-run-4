73206492e523f5fe8b6ded2a734681829466bd7a
Vignesh Shenvi <vshenvi@google.com>
2022-06-02 23:13:21 +0000

    Facilitate setting up mocks for the MissiveClient
    This change updates the MissiveClient and extends the InitializeFake
    interface so we can also set up a mock client if necessary. This will
    continue using the FakeMissiveClient as a fallback option if no type is
    specified so the global dbus helpers continue using them as necessary.
    
    This will enable us to inspect records being sent over the wire.
    
    Bug: b:229663388, b:233397216
    Change-Id: I91af11a217ae6af8f76ec90273b9492f13f401d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674273
    Reviewed-by: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010339}

--- chromeos/dbus/missive/missive_client.cc
+++ chromeos/dbus/missive/missive_client.cc
@@ -337,7 +337,7 @@ void MissiveClient::Initialize(dbus::Bus* bus) {
 
 // static
 void MissiveClient::InitializeFake() {
-  (new FakeMissiveClient())->Init();
+  MissiveClient::InitializeFake<FakeMissiveClient>();
 }
 
 // static
