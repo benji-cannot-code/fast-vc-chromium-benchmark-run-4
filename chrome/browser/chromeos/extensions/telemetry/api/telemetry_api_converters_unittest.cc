c5030c86d0de15b30696bb9fc09472649156e2bd
Bastian Kersting <bkersting@google.com>
2022-06-02 15:55:25 +0000

    [Telemetry] Change EXPECT in converters test
    Currently the telemetry_converters test uses EXPECT_EQ to for
    the size of an array, but as discussed in crrev/c/3671960,
    ASSERT would be better in this situation.
    
    R=lamzin@google.com
    
    Bug: None
    Change-Id: I40a94a5bffae5a3d1758616aac54dd016dcf1707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687132
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010116}

--- chrome/browser/chromeos/extensions/telemetry/api/telemetry_api_converters_unittest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/telemetry_api_converters_unittest.cc
@@ -89,7 +89,7 @@ TEST(TelemetryApiConverters, LogicalCpuInfo) {
   ASSERT_TRUE(result.idle_time_ms);
   EXPECT_EQ(kIdleTime, *result.idle_time_ms);
 
-  EXPECT_EQ(1u, result.c_states.size());
+  ASSERT_EQ(1u, result.c_states.size());
 
   ASSERT_TRUE(result.c_states[0].name);
   EXPECT_EQ(kCpuCStateName, *result.c_states[0].name);
@@ -129,7 +129,7 @@ TEST(TelemetryApiConverters, PhysicalCpuInfo) {
   ASSERT_TRUE(result.model_name);
   EXPECT_EQ(kModelName, *result.model_name);
 
-  EXPECT_EQ(1u, result.logical_cpus.size());
+  ASSERT_EQ(1u, result.logical_cpus.size());
 
   ASSERT_TRUE(result.logical_cpus[0].max_clock_speed_khz);
   EXPECT_EQ(kMaxClockSpeedKhz,
@@ -148,7 +148,7 @@ TEST(TelemetryApiConverters, PhysicalCpuInfo) {
   ASSERT_TRUE(result.logical_cpus[0].idle_time_ms);
   EXPECT_EQ(kIdleTime, *result.logical_cpus[0].idle_time_ms);
 
-  EXPECT_EQ(1u, result.logical_cpus[0].c_states.size());
+  ASSERT_EQ(1u, result.logical_cpus[0].c_states.size());
 
   ASSERT_TRUE(result.logical_cpus[0].c_states[0].name);
   EXPECT_EQ(kCpuCStateName, *result.logical_cpus[0].c_states[0].name);
