648d55d8fb349544a80c8274e8552d7fcaac2044
Peter Kasting <pkasting@chromium.org>
2022-06-02 14:37:23 +0000

    Revert "Revert "Prevent unsafe narrowing: base/*/*""
    This reverts commit 16934145bc49be128ade0ebb050c984b83904b6c.
    
    Bug: 1292951
    Change-Id: Ic3f1f7b764a0ba53a6abe5b8688bd5d024b2fd1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682678
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010085}

--- base/task/thread_pool/priority_queue.h
+++ base/task/thread_pool/priority_queue.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/containers/intrusive_heap.h"
+#include "base/stl_util.h"
 #include "base/task/common/checked_lock.h"
 #include "base/task/thread_pool/task_source.h"
 #include "base/task/thread_pool/task_source_sort_key.h"
@@ -67,7 +68,7 @@ class BASE_EXPORT PriorityQueue {
 
   // Returns the number of TaskSources with |priority|.
   size_t GetNumTaskSourcesWithPriority(TaskPriority priority) const {
-    return num_task_sources_per_priority_[static_cast<int>(priority)];
+    return num_task_sources_per_priority_[base::to_underlying(priority)];
   }
 
   // Set the PriorityQueue to empty all its TaskSources of Tasks when it is
