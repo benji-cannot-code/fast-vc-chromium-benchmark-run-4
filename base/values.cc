4f8ddd7f009facc70dfe6a30707525761080ecce
Daniel Cheng <dcheng@chromium.org>
2022-06-02 02:42:02 +0000

    Reland "Fix comparisons between base::Value and string literals."
    This is a reland of commit 2c977ecdff920e4dcc862cc4fb4d8544fd0ca3c7 with
    BASE_EXPORT added to base::Value::DoubleStorage's declaration.
    
    Original change's description:
    > Fix comparisons between base::Value and string literals.
    >
    > This was unintentionally using the bool overload instead of the
    > StringPiece overload, since a standard conversion sequence is always
    > better than a user-defined conversion sequence.
    >
    > ValueView has a similar ambiguity and has also been fixed to correctly
    > treat string literals as, well, strings.
    >
    > Bug: 646113
    > Change-Id: I0210e9fc29bf1eebb0bbe9c5b072e91502baa78e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671368
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1008092}
    
    Bug: 646113
    Change-Id: I3c6c4cfd9688b18433a0e88020c3babd09a89cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671828
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009900}

--- base/values.cc
+++ base/values.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cxx17_backports.h"
 #include "base/cxx20_to_address.h"
 #include "base/json/json_writer.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
