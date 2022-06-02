84d36d6d3372eca26aba1dd30878b61e035a699a
Kelvin Jiang <kelvinjiang@chromium.org>
2022-06-02 04:08:05 +0000

    [Extensions] Add dialog for toggling host permissions
    If the enableEnhancedSiteControls flag is enabled, this CL adds a
    warning dialog if the user toggles on a host permission that is also a
    user restricted site, for an extension that does not specify <all_urls>
    in its host permissions.
    
    This CL also contains a small UX refactor to align with the new UX mocks
    if the flag is enabled.
    
    Screenshot: https://imgur.com/a/m2rvPrX
    
    Bug: 1253673
    Change-Id: I987ec35baaa7073f20df5eb720a9f8af91ac93e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601878
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009937}

--- chrome/test/data/webui/extensions/host_permissions_toggle_list_test.ts
+++ chrome/test/data/webui/extensions/host_permissions_toggle_list_test.ts
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {ExtensionsHostPermissionsToggleListElement, ExtensionsToggleRowElement, UserAction} from 'chrome://extensions/extensions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
 import {TestService} from './test_service.js';
 
@@ -18,13 +19,20 @@ suite('HostPermissionsToggleList', function() {
   const EXAMPLE_COM = 'https://example.com/*';
   const GOOGLE_COM = 'https://google.com/*';
   const CHROMIUM_ORG = 'https://chromium.org/*';
+  const RESTRICTED_COM = '*://restricted.com/*';
+  const userSiteSettings: chrome.developerPrivate.UserSiteSettings = {
+    permittedSites: [],
+    restrictedSites: ['http://restricted.com', 'https://other.com']
+  };
 
   setup(function() {
     document.body.innerHTML = '';
     element = document.createElement('extensions-host-permissions-toggle-list');
     delegate = new TestService();
+    delegate.userSiteSettings = userSiteSettings;
     element.delegate = delegate;
     element.itemId = ITEM_ID;
+    element.enableEnhancedSiteControls = true;
 
     document.body.appendChild(element);
   });
@@ -181,6 +189,9 @@ suite('HostPermissionsToggleList', function() {
   // Tests that clicking the "allow on the following sites" toggle when it is in
   // the "off" state calls the delegate as expected.
   test('clicking all hosts toggle from off to on', async function() {
+    // Note: `RESTRICTED_COM` is a user restricted site, but clicking the all
+    // hosts toggle should not cause the matching restricted sites dialog to
+    // show.
     const permissions = {
       hostAccess: HostAccess.ON_CLICK,
       hasAllHosts: false,
@@ -188,6 +199,7 @@ suite('HostPermissionsToggleList', function() {
         {host: EXAMPLE_COM, granted: false},
         {host: GOOGLE_COM, granted: false},
         {host: CHROMIUM_ORG, granted: false},
+        {host: RESTRICTED_COM, granted: false},
       ],
     };
     element.permissions = permissions;
@@ -196,6 +208,10 @@ suite('HostPermissionsToggleList', function() {
     assertTrue(!!element);
     const allSites = element.$.allHostsToggle;
     allSites.getLabel().click();
+
+    flush();
+    assertFalse(!!element.getRestrictedSitesDialog());
+
     const [id, access] = await delegate.whenCalled('setItemHostAccess');
     assertEquals(ITEM_ID, id);
     assertEquals(HostAccess.ON_ALL_SITES, access);
@@ -269,4 +285,82 @@ suite('HostPermissionsToggleList', function() {
     metricName = await delegate.whenCalled('recordUserAction');
     assertEquals(UserAction.SPECIFIC_TOGGLED_ON, metricName);
   });
+
+  test(
+      'toggling specific site removes matching restricted sites',
+      async function() {
+        await delegate.whenCalled('getUserSiteSettings');
+
+        const permissions = {
+          hostAccess: HostAccess.ON_SPECIFIC_SITES,
+          hasAllHosts: false,
+          hosts: [
+            {host: RESTRICTED_COM, granted: false},
+          ],
+        };
+
+        element.permissions = permissions;
+        flush();
+
+        const hostToggles =
+            element.shadowRoot!.querySelectorAll<ExtensionsToggleRowElement>(
+                '.host-toggle');
+        assertEquals(1, hostToggles.length);
+        assertEquals(RESTRICTED_COM, hostToggles[0]!.innerText!.trim());
+        assertFalse(hostToggles[0]!.checked);
+
+        hostToggles[0]!.getLabel().click();
+
+        flush();
+
+        // Check that the matching restricted sites dialog is visible and the
+        // host's toggle is checked, even though the host has not been granted.
+        assertTrue(hostToggles[0]!.checked);
+
+        let dialog = element.getRestrictedSitesDialog()!;
+        assertTrue(!!dialog);
+        assertTrue(dialog.isOpen());
+
+        const cancel =
+            dialog.$.dialog.querySelector<HTMLElement>('.cancel-button');
+        assertTrue(!!cancel);
+
+        const whenClosed = eventToPromise('close', dialog);
+        cancel.click();
+        await whenClosed;
+        assertFalse(dialog.wasConfirmed());
+        flush();
+
+        // Cancelling the dialog should uncheck the host.
+        assertFalse(!!element.getRestrictedSitesDialog());
+        assertFalse(hostToggles[0]!.checked);
+
+        hostToggles[0]!.getLabel().click();
+        flush();
+
+        dialog = element.getRestrictedSitesDialog()!;
+        assertTrue(!!dialog);
+        assertTrue(dialog.isOpen());
+
+        const addHostPermissionPromise =
+            delegate.whenCalled('addRuntimeHostPermission');
+        const allow =
+            dialog.$.dialog.querySelector<HTMLElement>('.action-button');
+        assertTrue(!!allow);
+        allow.click();
+
+        // Accepting the dialog should grant the host and remove any matching
+        // restricted sites.
+        const [id, site] = await addHostPermissionPromise;
+        assertEquals(ITEM_ID, id);
+        assertEquals(RESTRICTED_COM, site);
+
+        const [siteSet, removedSites] =
+            await delegate.whenCalled('removeUserSpecifiedSites');
+        assertEquals(chrome.developerPrivate.UserSiteSet.RESTRICTED, siteSet);
+        assertDeepEquals(['http://restricted.com'], removedSites);
+
+        const metricName = await delegate.whenCalled('recordUserAction');
+        assertEquals(UserAction.SPECIFIC_TOGGLED_ON, metricName);
+      });
 });
