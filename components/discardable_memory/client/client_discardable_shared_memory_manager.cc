957bd55a62cca27513fe853f6b1aa0e7357f70d7
Thiabaud Engelbrecht <thiabaud@google.com>
2022-06-02 17:47:22 +0000

    [discardable_memory] Periodic purge should hit younger memory.
    This CL sets up an experiment to check whether we improve memory usage
    by decreasing the minimum age which we purge.
    
    This change is gated behind a feature flag, so it should not change
    anything when it lands.
    
    Bug: 1123679
    Change-Id: Id101aceb5cd9c6e2ce1a742ed8587dac233870c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664002
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010178}

--- components/discardable_memory/client/client_discardable_shared_memory_manager.cc
+++ components/discardable_memory/client/client_discardable_shared_memory_manager.cc
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace discardable_memory {
 namespace {
 
+const base::Feature kShorterPeriodicPurge{"ShorterPeriodicPurge",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Global atomic to generate unique discardable shared memory IDs.
 base::AtomicSequenceNumber g_next_discardable_shared_memory_id;
 
@@ -421,8 +424,13 @@ void ClientDiscardableSharedMemoryManager::ScheduledPurge() {
   // recover the memory without adverse latency effects.
   // TODO(crbug.com/1123679): Determine if |kMinAgeForScheduledPurge| and the
   // constant from |ScheduledPurge| need to be tuned.
-  PurgeUnlockedMemory(
-      ClientDiscardableSharedMemoryManager::kMinAgeForScheduledPurge);
+  if (base::FeatureList::IsEnabled(kShorterPeriodicPurge)) {
+    PurgeUnlockedMemory(
+        ClientDiscardableSharedMemoryManager::kMinAgeForScheduledPurge / 2);
+  } else {
+    PurgeUnlockedMemory(
+        ClientDiscardableSharedMemoryManager::kMinAgeForScheduledPurge);
+  }
 
   bool should_schedule = false;
   {
