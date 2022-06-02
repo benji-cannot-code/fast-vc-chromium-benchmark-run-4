c5c02079c5a516018658c58fb8cd4f06d9386150
dpapad <dpapad@chromium.org>
2022-06-02 18:02:44 +0000

    SupportTool: Use css_to_wrapper() for Polymer styles.
    Bug: 1322682
    Change-Id: I5cd319928ba1cb3e7fbbd1855f83f3c00399c319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3684894
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010185}

--- chrome/browser/resources/support_tool/data_export_done.ts
+++ chrome/browser/resources/support_tool/data_export_done.ts
@@ -3,13 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './support_tool_shared_css.js';
+import './support_tool_shared.css.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/cr_elements/action_link_css.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 import {BrowserProxy, BrowserProxyImpl} from './browser_proxy.js';
 import {getTemplate} from './data_export_done.html.js';
 
