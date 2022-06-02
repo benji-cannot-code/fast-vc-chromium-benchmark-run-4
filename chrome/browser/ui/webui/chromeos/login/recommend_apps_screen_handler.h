d183279537d75e10c71aefce75f2ef39147cbbc7
Danila Kuzmin <dkuzmin@google.com>
2022-06-02 15:16:13 +0000

    [OOBE][RecommendApps]: Update screen/handler structure
    Bug: 1330548, 1070917, 1309022, 1298392
    Change-Id: I5ce0bcc57a74d4be8e1eabc00729c1a87bda767b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3683833
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010099}

--- chrome/browser/ui/webui/chromeos/login/recommend_apps_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/recommend_apps_screen_handler.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_RECOMMEND_APPS_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_RECOMMEND_APPS_SCREEN_HANDLER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
-#include "components/prefs/pref_service.h"
 
 namespace ash {
 class RecommendAppsScreen;
@@ -18,14 +18,13 @@ namespace chromeos {
 
 // Interface for dependency injection between RecommendAppsScreen and its
 // WebUI representation.
-class RecommendAppsScreenView {
+class RecommendAppsScreenView
+    : public base::SupportsWeakPtr<RecommendAppsScreenView> {
  public:
-  constexpr static StaticOobeScreenId kScreenId{"recommend-apps"};
+  inline constexpr static StaticOobeScreenId kScreenId{"recommend-apps",
+                                                       "RecommendAppsScreen"};
 
   virtual ~RecommendAppsScreenView() = default;
-
-  // Sets screen this view belongs to.
-  virtual void Bind(ash::RecommendAppsScreen* screen) = 0;
 
   // Shows the contents of the screen.
   virtual void Show() = 0;
@@ -60,36 +59,16 @@ class RecommendAppsScreenHandler : public BaseScreenHandler,
   void DeclareLocalizedValues(
       ::login::LocalizedValuesBuilder* builder) override;
   void GetAdditionalParameters(base::Value::Dict* dict) override;
-  void RegisterMessages() override;
 
   // RecommendAppsScreenView:
-  void Bind(ash::RecommendAppsScreen* screen) override;
   void Show() override;
   void Hide() override;
   void OnLoadSuccess(base::Value app_list) override;
   void OnParseResponseError() override;
 
-  // BaseScreenHandler:
-  void InitializeDeprecated() override;
-
  private:
-  void OnUserSkip();
-
   // Call the JS function to load the list of apps in the WebView.
   void LoadAppListInUI(base::Value app_list);
-
-  void HandleSkip();
-  void HandleRetry();
-  void HandleInstall(const base::Value::List& args);
-
-  ash::RecommendAppsScreen* screen_ = nullptr;
-
-  PrefService* pref_service_;
-
-  int recommended_app_count_ = 0;
-
-  // If true, InitializeDeprecated() will call Show().
-  bool show_on_init_ = false;
 };
 
 }  // namespace chromeos
