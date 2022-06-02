2504b5107ed4d4f3fb0e780e01a17b1a2014e642
Juanmi Huertas <juanmihd@google.com>
2022-06-02 16:44:53 +0000

    Making debug_colors use SkColor4f
    Bug: 1308932
    Change-Id: I9b757eda5985740d7da3d6b71d6a746940f28e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673441
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010139}

--- cc/layers/render_surface_impl.cc
+++ cc/layers/render_surface_impl.cc
@@ -112,7 +112,8 @@ SkBlendMode RenderSurfaceImpl::BlendMode() const {
 }
 
 SkColor RenderSurfaceImpl::GetDebugBorderColor() const {
-  return DebugColors::SurfaceBorderColor();
+  // TODO(crbug/1308932): Remove toSkColor and make all SkColor4f.
+  return DebugColors::SurfaceBorderColor().toSkColor();
 }
 
 float RenderSurfaceImpl::GetDebugBorderWidth() const {
