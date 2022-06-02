78a02b89f6acb11a4e118faa3a5515b1e9dfed31
Leonid Baraz <lbaraz@chromium.org>
2022-06-02 20:26:50 +0000

    Streamline Speculative queue async implementation.
    Store record producers aside rather than records, and run them only
    after the actual queue has been attached.
    
    Bug: b:233426237
    Change-Id: Ia71c3044143240417dc9262b883cce2c6b22bed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3685219
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010259}

--- components/reporting/client/mock_report_queue.cc
+++ components/reporting/client/mock_report_queue.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/reporting/client/mock_report_queue.h"
 
+#include <utility>
+
 #include "testing/gmock/include/gmock/gmock.h"
 
 using ::testing::Invoke;
--- components/reporting/client/mock_report_queue.h
+++ components/reporting/client/mock_report_queue.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_REPORTING_CLIENT_MOCK_REPORT_QUEUE_H_
 
 #include <memory>
+#include <string>
 
 #include "base/callback.h"
 #include "components/reporting/client/report_queue.h"
