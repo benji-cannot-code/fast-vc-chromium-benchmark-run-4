0b554a57e383f539b36b93ec333531da9dfd514b
Ryan Keane <rwkeane@google.com>
2022-06-02 03:38:46 +0000

    Revert "[Cast Convergence] Remove is_chromecast from misc places"
    This reverts commit 52026271db38d3de51d14ca222c5061985a14bc6.
    
    Reason for revert: Need to address comments added after the CL had already landed. Will re-land this CL after
    
    Original change's description:
    > [Cast Convergence] Remove is_chromecast from misc places
    >
    > This CL updates misc code to remove `is_chromecast` and replace it
    > with new `is_castos` and `enable_cast_receiver` flags as appropriate,
    > which signify either "a chromecast-hardware device" or "a platform
    > which can act as a cast receiver" (such as ATV, Fuchsia, etc)
    > respectively
    >
    > Bug: b/217624595
    > Change-Id: Ic578114f6d019146369b7c7c5a12a45971284811
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632828
    > Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    > Commit-Queue: Ryan Keane <rwkeane@google.com>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1001779}
    
    Bug: b/217624595
    Change-Id: Ic5dae46f7cf8777c5fd0dbcabf89bf78c1182287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3685214
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009930}

--- gpu/command_buffer/service/shared_image_factory.cc
+++ gpu/command_buffer/service/shared_image_factory.cc
@@ -85,8 +85,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-#if BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS_ASH) &&      \
-    !BUILDFLAG(IS_CHROMEOS_LACROS) && !BUILDFLAG(IS_CASTOS) && \
+#if BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS_ASH) &&          \
+    !BUILDFLAG(IS_CHROMEOS_LACROS) && !BUILDFLAG(IS_CHROMECAST) && \
     BUILDFLAG(ENABLE_VULKAN)
 
 namespace {
@@ -389,7 +389,7 @@ SharedImageFactory::SharedImageFactory(
   }
 #elif defined(USE_OZONE)
 #if BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS_ASH) && \
-    !BUILDFLAG(IS_CHROMEOS_LACROS) && !BUILDFLAG(IS_CASTOS)
+    !BUILDFLAG(IS_CHROMEOS_LACROS) && !BUILDFLAG(IS_CHROMECAST)
   // Desktop Linux, not ChromeOS.
   if (ShouldUseOzoneFactory()) {
     auto ozone_factory =
