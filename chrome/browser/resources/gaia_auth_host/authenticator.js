7a76b848c948cef70b2f119e0d25bdbea58e31eb
Danila Kuzmin <dkuzmin@google.com>
2022-06-03 10:40:52 +0000

    [OOBE][ChildAccount]: Reuse local user type on GAIA done timeout
    Bug: 1288039
    Change-Id: I5e250d02f26f638651379f4ac6db0f7f32e340a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652201
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010510}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -287,6 +287,7 @@ cr.define('cr.login', function() {
     },
     'userInfo'(msg) {
       this.services_ = msg.services;
+      this.servicesProvided_ = true;
       if (!this.authCompletedFired_) {
         const metric = this.authFlow === AuthFlow.SAML ?
             GAIA_MESSAGE_SAML_USER_INFO :
@@ -429,6 +430,7 @@ cr.define('cr.login', function() {
       this.missingGaiaInfoCallback = null;
       this.needPassword = true;
       this.services_ = null;
+      this.servicesProvided_ = false;
       this.waitApiPasswordConfirm_ = false;
       this.gaiaDoneTimer_ = null;
       /** @private {boolean} */
@@ -476,6 +478,7 @@ cr.define('cr.login', function() {
       this.samlHandler_.reset();
       this.videoEnabled = false;
       this.services_ = null;
+      this.servicesProvided_ = false;
       this.waitApiPasswordConfirm_ = false;
       this.maybeClearGaiaTimeout_();
       this.syncTrustedVaultKeys_ = null;
@@ -1210,8 +1213,9 @@ cr.define('cr.login', function() {
               sessionIndex: this.sessionIndex_ || '',
               trusted: this.trusted_,
               services: this.services_ || [],
+              servicesProvided: this.servicesProvided_,
               passwordAttributes: passwordAttributes,
-              syncTrustedVaultKeys: this.syncTrustedVaultKeys_ || {}
+              syncTrustedVaultKeys: this.syncTrustedVaultKeys_ || {},
             }
           }));
       this.resetStates();
