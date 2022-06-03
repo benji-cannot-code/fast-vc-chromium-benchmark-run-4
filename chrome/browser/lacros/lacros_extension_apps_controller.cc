28295fad55d052254bdb0f215a4ac6ebd7d88b8f
Jenny Zhang <jennyz@chromium.org>
2022-06-03 19:07:16 +0000

    Launch MS Doc with quickoffice in Lacros.
    The quickoffice extension use file_handlers to register
    their handlers for MS Doc files, which is the way chrome apps
    do. Therefore, we need to have quickoffice extensions handled
    in the same way as chrome apps do for intents.
    
    I will remove quickoffice extensions from ash keep list after
    this cl is landed.
    
    Test: Right click an MS Doc in File app, you should see two
    menu items "Open with Office Editing for Docs, Sheet & Slides"
    . The one marked as default is from ash, the other is from Lacros.
    Choose the one from Lacros, it will open the MS Doc in Lacros.
    
    Bug: 1247874
    Change-Id: I14850726979d6a2a5e352e73597ae72006b52522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688676
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010692}

--- chrome/browser/lacros/lacros_extension_apps_controller.cc
+++ chrome/browser/lacros/lacros_extension_apps_controller.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/uninstall_reason.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_urls.h"
 #include "ui/base/window_open_disposition.h"
@@ -300,7 +301,8 @@ void LacrosExtensionAppsController::FinallyLaunch(
   auto params = apps::ConvertCrosapiToLaunchParams(launch_params, profile);
   params.app_id = extension->id();
 
-  if (which_type_.IsChromeApps()) {
+  if (which_type_.IsChromeApps() ||
+      extension_misc::IsQuickOfficeExtension(extension->id())) {
     OpenApplication(profile, std::move(params));
 
     // TODO(https://crbug.com/1225848): Store the resulting instance token,
@@ -342,6 +344,7 @@ void LacrosExtensionAppsController::FinallyLaunch(
 
   } else {
     NOTREACHED();
+    std::move(callback).Run(std::move(result));
   }
 }
 
