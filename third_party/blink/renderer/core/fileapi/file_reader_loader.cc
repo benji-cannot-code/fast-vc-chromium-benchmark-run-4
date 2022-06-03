80bf8b78f686c8c8ef498fd80c085f557f835bb0
Christine Smith <christinesm@chromium.org>
2022-06-03 00:37:32 +0000

    [Storage Code Health]: Remove expired histograms: Storage.Blob.FileReaderLoader.*
    Removes the following expired histograms, and related code that is no
    longer needed:
    	Storage.Blob.FileReaderLoader.FailureType
    	Storage.Blob.FileReaderLoader.ReadError
    
    Bug: 1244911, 1314419
    Change-Id: Idbc9732d8c2551099092df36f85e2343e5032bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661825
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010378}

--- third_party/blink/renderer/core/fileapi/file_reader_loader.cc
+++ third_party/blink/renderer/core/fileapi/file_reader_loader.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/histogram_functions.h"
 #include "mojo/public/cpp/system/wait.h"
 #include "third_party/blink/public/common/blob/blob_utils.h"
 #include "third_party/blink/public/platform/web_url_request.h"
@@ -95,7 +94,7 @@ void FileReaderLoader::Start(scoped_refptr<BlobDataHandle> blob_data) {
   mojo::ScopedDataPipeProducerHandle producer_handle;
   MojoResult rv = CreateDataPipe(&options, producer_handle, consumer_handle_);
   if (rv != MOJO_RESULT_OK) {
-    Failed(FileErrorCode::kNotReadableErr, FailureType::kMojoPipeCreation);
+    Failed(FileErrorCode::kNotReadableErr);
     return;
   }
 
@@ -109,16 +108,14 @@ void FileReaderLoader::Start(scoped_refptr<BlobDataHandle> blob_data) {
     if (received_on_complete_)
       return;
     if (!received_all_data_) {
-      Failed(FileErrorCode::kNotReadableErr,
-             FailureType::kSyncDataNotAllLoaded);
+      Failed(FileErrorCode::kNotReadableErr);
       return;
     }
 
     // Wait for OnComplete
     receiver_.WaitForIncomingCall();
     if (!received_on_complete_) {
-      Failed(FileErrorCode::kNotReadableErr,
-             FailureType::kSyncOnCompleteNotReceived);
+      Failed(FileErrorCode::kNotReadableErr);
     }
   }
 }
@@ -212,13 +209,11 @@ void FileReaderLoader::Cleanup() {
   }
 }
 
-void FileReaderLoader::Failed(FileErrorCode error_code, FailureType type) {
+void FileReaderLoader::Failed(FileErrorCode error_code) {
   // If an error was already reported, don't report this error again.
   if (error_code_ != FileErrorCode::kOK)
     return;
   error_code_ = error_code;
-  base::UmaHistogramEnumeration("Storage.Blob.FileReaderLoader.FailureType",
-                                type);
   Cleanup();
   if (client_)
     client_->DidFail(error_code_);
@@ -234,7 +229,7 @@ void FileReaderLoader::OnStartLoading(uint64_t total_bytes) {
     // so to call ArrayBuffer's create function.
     // FIXME: Support reading more than the current size limit of ArrayBuffer.
     if (total_bytes > std::numeric_limits<unsigned>::max()) {
-      Failed(FileErrorCode::kNotReadableErr, FailureType::kTotalBytesTooLarge);
+      Failed(FileErrorCode::kNotReadableErr);
       return;
     }
 
@@ -242,8 +237,7 @@ void FileReaderLoader::OnStartLoading(uint64_t total_bytes) {
                                     ArrayBufferContents::kNotShared,
                                     ArrayBufferContents::kDontInitialize);
     if (!raw_data_.IsValid()) {
-      Failed(FileErrorCode::kNotReadableErr,
-             FailureType::kArrayBufferBuilderCreation);
+      Failed(FileErrorCode::kNotReadableErr);
       return;
     }
   }
@@ -276,8 +270,7 @@ void FileReaderLoader::OnReceivedData(const char* data, unsigned data_length) {
   if (bytes_loaded_ + data_length > raw_data_.DataLength()) {
     raw_data_.Reset();
     bytes_loaded_ = 0;
-    Failed(FileErrorCode::kNotReadableErr,
-           FailureType::kArrayBufferBuilderAppend);
+    Failed(FileErrorCode::kNotReadableErr);
     return;
   }
   memcpy(static_cast<char*>(raw_data_.Data()) + bytes_loaded_, data,
@@ -327,18 +320,14 @@ void FileReaderLoader::OnCalculatedSize(uint64_t total_size,
 }
 
 void FileReaderLoader::OnComplete(int32_t status, uint64_t data_length) {
-  base::UmaHistogramSparse("Storage.Blob.FileReaderLoader.ReadError",
-                           std::max(0, -net_error_));
-
   if (status != net::OK) {
     net_error_ = status;
     Failed(status == net::ERR_FILE_NOT_FOUND ? FileErrorCode::kNotFoundErr
-                                             : FileErrorCode::kNotReadableErr,
-           FailureType::kBackendReadError);
+                                             : FileErrorCode::kNotReadableErr);
     return;
   }
   if (data_length != total_bytes_) {
-    Failed(FileErrorCode::kNotReadableErr, FailureType::kReadSizesIncorrect);
+    Failed(FileErrorCode::kNotReadableErr);
     return;
   }
 
@@ -350,8 +339,7 @@ void FileReaderLoader::OnComplete(int32_t status, uint64_t data_length) {
 void FileReaderLoader::OnDataPipeReadable(MojoResult result) {
   if (result != MOJO_RESULT_OK) {
     if (!received_all_data_) {
-      Failed(FileErrorCode::kNotReadableErr,
-             FailureType::kDataPipeNotReadableWithBytesLeft);
+      Failed(FileErrorCode::kNotReadableErr);
     }
     return;
   }
@@ -373,14 +361,12 @@ void FileReaderLoader::OnDataPipeReadable(MojoResult result) {
     if (pipe_result == MOJO_RESULT_FAILED_PRECONDITION) {
       // Pipe closed.
       if (!received_all_data_) {
-        Failed(FileErrorCode::kNotReadableErr,
-               FailureType::kMojoPipeClosedEarly);
+        Failed(FileErrorCode::kNotReadableErr);
       }
       return;
     }
     if (pipe_result != MOJO_RESULT_OK) {
-      Failed(FileErrorCode::kNotReadableErr,
-             FailureType::kMojoPipeUnexpectedReadError);
+      Failed(FileErrorCode::kNotReadableErr);
       return;
     }
 
