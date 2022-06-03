35d20c895091637b30434a5335ae2fdd7eda4f36
Miriam Zimmerman <mutexlox@google.com>
2022-06-03 17:22:16 +0000

    metrics: Fix variation flag loading on ash-chrome
    Plumb through variations IDs in addition to parameters on chromeOS.
    
    TEST=Set omnibox-on-focus-suggestions-contextual-web@2, verify that \
         trigger-id 3317583 is present in x-client-data when visiting   \
         https://www.google.com
    
    Fixed: 805766
    Change-Id: I5033828216d045cc544a1557f256005344e1c5e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664437
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010627}

--- components/flags_ui/flags_state_unittest.cc
+++ components/flags_ui/flags_state_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/testing_pref_service.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/variations/variations_associated_data.h"
+#include "components/variations/variations_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace flags_ui {
@@ -98,7 +99,7 @@ const FeatureEntry::FeatureVariation kTestVariations2[] = {
 const FeatureEntry::FeatureVariation kTestVariations3[] = {
     {"dummy description 1", kTestVariationOther1, 1, nullptr},
     {"dummy description 2", kTestVariationOther2, 1, nullptr},
-    {"dummy description 3", kTestVariationOther3, 2, nullptr}};
+    {"dummy description 3", kTestVariationOther3, 2, "t123456"}};
 
 const char kTestVariation3Cmdline[] =
     "FeatureName3:param1/value/param%3A%2F3/value";
@@ -344,6 +345,11 @@ TEST_F(FlagsStateTest, ConvertFlagsToSwitches) {
   EXPECT_TRUE(command_line3.HasSwitch(kEnableFeatures));
   EXPECT_EQ(command_line3.GetSwitchValueASCII(kEnableFeatures),
             kTestVariation3Cmdline);
+  EXPECT_TRUE(
+      command_line3.HasSwitch(variations::switches::kForceVariationIds));
+  EXPECT_EQ(command_line3.GetSwitchValueASCII(
+                variations::switches::kForceVariationIds),
+            "t123456");
 }
 
 TEST_F(FlagsStateTest, RegisterAllFeatureVariationParameters) {
