d44dbd3fdb3479e765f3aa2733244c3c0da572ee
Tony Herre <toprice@chromium.org>
2022-06-03 10:36:52 +0000

    Reduce BoB UnderlyingSource test leak flakes by waiting on IO thread tasks
    The test attempted to wait for all tasks on the IO
    thread to finish by calling RunUntilIdle on the
    IOTaskRunnerTestingPlatformSupport, but this actually just runs the main
    thread until idle. Instead add RunIOUntilIdle, to wait for all
    outstanding IO thread tasks, as is used in
    MediaStreamVideoRendererSinkTest.
    
    Bug: 1231154
    Change-Id: Id12394f770f37c76d824dc861b758161605505a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688510
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010509}

--- third_party/blink/renderer/modules/breakout_box/media_stream_video_track_underlying_source_test.cc
+++ third_party/blink/renderer/modules/breakout_box/media_stream_video_track_underlying_source_test.cc
@@ -47,7 +47,7 @@ class MediaStreamVideoTrackUnderlyingSourceTest : public testing::Test {
             base::WrapUnique(pushable_video_source_))) {}
 
   ~MediaStreamVideoTrackUnderlyingSourceTest() override {
-    platform_->RunUntilIdle();
+    RunIOUntilIdle();
     WebHeap::CollectAllGarbageForTesting();
   }
 
@@ -72,6 +72,16 @@ class MediaStreamVideoTrackUnderlyingSourceTest : public testing::Test {
     return CreateSource(script_state, track, 1u);
   }
 
+ private:
+  void RunIOUntilIdle() const {
+    // Make sure that tasks on IO thread are completed before moving on.
+    base::RunLoop run_loop;
+    Platform::Current()->GetIOTaskRunner()->PostTaskAndReply(
+        FROM_HERE, base::BindOnce([] {}), run_loop.QuitClosure());
+    run_loop.Run();
+    base::RunLoop().RunUntilIdle();
+  }
+
  protected:
   void PushFrame(
       const absl::optional<base::TimeDelta>& timestamp = absl::nullopt) {
@@ -80,7 +90,7 @@ class MediaStreamVideoTrackUnderlyingSourceTest : public testing::Test {
     if (timestamp)
       frame->set_timestamp(*timestamp);
     pushable_video_source_->PushFrame(frame, base::TimeTicks());
-    platform_->RunUntilIdle();
+    RunIOUntilIdle();
   }
 
   static MediaStreamSource* CreateDevicePushableSource(
