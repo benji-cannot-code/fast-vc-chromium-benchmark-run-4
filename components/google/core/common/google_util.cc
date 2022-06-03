68aee762f2d3097ced9b20637022f45aa9b2bf49
Dan McArdle <dmcardle@chromium.org>
2022-06-03 15:30:52 +0000

    Eliminate base::LowerCaseEqualsASCII in /components/google
    This CL is a behavior-preserving refactor. It replaces calls to
    base::LowerCaseEqualsASCII with base::EqualsCaseInsensitiveASCII.
    
    We are deprecating base::LowerCaseEqualsASCII because its interface is a
    mild footgun, in that it requires the second parameter to be lowercase.
    
    See the design doc for more details.
    https://docs.google.com/document/d/18zWV0zN4oAqMZpond1Ut0QviHUeHCSwCo08mqThwHkQ/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=isherman@chromium.org
    
    Bug: 1326529
    Change-Id: Id3f2010d1b2a616e1a7a507dabb15f3bd1292228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3657577
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010587}

--- components/google/core/common/google_util.cc
+++ components/google/core/common/google_util.cc
@@ -73,7 +73,7 @@ bool IsValidHostName(base::StringPiece host,
   if (!allowed_tlds.contains(tld))
     return false;
 
-  if (base::LowerCaseEqualsASCII(host_minus_tld, domain_in_lower_case))
+  if (base::EqualsCaseInsensitiveASCII(host_minus_tld, domain_in_lower_case))
     return true;
 
   if (subdomain_permission == ALLOW_SUBDOMAIN) {
@@ -83,7 +83,7 @@ bool IsValidHostName(base::StringPiece host,
   }
 
   std::string www_domain = base::StrCat({"www.", domain_in_lower_case});
-  return base::LowerCaseEqualsASCII(host_minus_tld, www_domain);
+  return base::EqualsCaseInsensitiveASCII(host_minus_tld, www_domain);
 }
 
 // True if |url| is a valid URL with HTTP or HTTPS scheme. If |port_permission|
@@ -312,7 +312,7 @@ bool IsGoogleAssociatedDomainUrl(const GURL& url) {
       "googleweblight.com",
   };
   for (size_t i = 0; i < std::size(kHostsToSetHeadersFor); ++i) {
-    if (base::LowerCaseEqualsASCII(host, kHostsToSetHeadersFor[i]))
+    if (base::EqualsCaseInsensitiveASCII(host, kHostsToSetHeadersFor[i]))
       return true;
   }
 
