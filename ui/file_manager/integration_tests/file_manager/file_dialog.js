92cb2a5605743accd334a8dad2e673487847687b
Bo Majewski <majewski@chromium.org>
2022-06-03 08:59:06 +0000

    [Files]: Move call to chooseEntry to the foreground page.
    Bug: 736930
    Change-Id: Iddba4d402d45411fd95da7de578213c2715eccc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674928
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010488}

--- ui/file_manager/integration_tests/file_manager/file_dialog.js
+++ ui/file_manager/integration_tests/file_manager/file_dialog.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {addEntries, ENTRIES, getCaller, pending, repeatUntil, sendBrowserTestCommand, sendTestMessage, TestEntryInfo} from '../test_util.js';
 import {testcase} from '../testcase.js';
 
-import {openAndWaitForClosingDialog, remoteCall} from './background.js';
+import {openAndWaitForClosingDialog, openEntryChoosingWindow, pollForChosenEntry, remoteCall} from './background.js';
 import {BASIC_LOCAL_ENTRY_SET} from './test_data.js';
 
 /**
@@ -249,7 +249,7 @@ export async function waitForDialog() {
  */
 async function checkFeedbackDisplayHidden(type) {
   // Open dialog of the specified 'type'.
-  chrome.fileSystem.chooseEntry({type: type}, (entry) => {});
+  await openEntryChoosingWindow({type: type});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -283,7 +283,7 @@ testcase.openFileDialogDownloads = () => {
  */
 testcase.openFileDialogAriaMultipleSelect = async () => {
   // Open File dialog.
-  chrome.fileSystem.chooseEntry({type: 'openFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'openFile'});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -303,7 +303,7 @@ testcase.openFileDialogAriaMultipleSelect = async () => {
  */
 testcase.saveFileDialogAriaSingleSelect = async () => {
   // Open Save as dialog.
-  chrome.fileSystem.chooseEntry({type: 'saveFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'saveFile'});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -331,7 +331,7 @@ testcase.saveFileDialogDownloads = () => {
  */
 testcase.saveFileDialogDownloadsNewFolderButton = async () => {
   // Open Save as dialog.
-  chrome.fileSystem.chooseEntry({type: 'saveFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'saveFile'});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -493,7 +493,7 @@ testcase.openFileDialogEscapeDrive = () => {
  * Tests opening file dialog, then closing it with an 'unload' event.
  */
 testcase.openFileDialogUnload = async () => {
-  chrome.fileSystem.chooseEntry({type: 'openFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'openFile'});
   const dialog = await waitForDialog();
   await unloadOpenFileDialog(dialog);
 };
@@ -508,7 +508,7 @@ testcase.openFileDialogDefaultFilter = async () => {
     accepts: [{extensions: ['jpg']}],
     acceptsAllTypes: true,
   };
-  chrome.fileSystem.chooseEntry(params, (entry) => {});
+  await openEntryChoosingWindow(params);
   const dialog = await waitForDialog();
 
   // Check: 'JPEG image' should be selected.
@@ -527,7 +527,7 @@ testcase.saveFileDialogDefaultFilter = async () => {
     accepts: [{extensions: ['jpg']}],
     acceptsAllTypes: true,
   };
-  chrome.fileSystem.chooseEntry(params, (entry) => {});
+  await openEntryChoosingWindow(params);
   const dialog = await waitForDialog();
 
   // Check: 'All files' should be selected.
@@ -547,7 +547,7 @@ testcase.saveFileDialogDefaultFilterKeyNavigation = async () => {
     accepts: [{extensions: ['jpg']}],
     acceptsAllTypes: true,
   };
-  chrome.fileSystem.chooseEntry(params, (entry) => {});
+  await openEntryChoosingWindow(params);
   const dialog = await waitForDialog();
 
   // Check: 'All files' should be selected.
@@ -707,7 +707,7 @@ testcase.saveFileDialogSingleFilterNoAcceptAll = async () => {
     accepts: [{extensions: ['jpg']}],
     acceptsAllTypes: false,
   };
-  chrome.fileSystem.chooseEntry(params, (entry) => {});
+  await openEntryChoosingWindow(params);
   const dialog = await waitForDialog();
 
   // Check: 'JPEG image' should be selected.
@@ -726,20 +726,21 @@ testcase.saveFileDialogSingleFilterNoAcceptAll = async () => {
  * @return {!Promise<string>} The name of the entry from chooseEntry().
  */
 async function showSaveAndConfirmExpecting(extraParams, expectName) {
+  const caller = getCaller();
+
   const params = {
     type: 'saveFile',
     accepts: [{extensions: ['jpg']}],
   };
-  const result = new Promise(resolve => {
-    chrome.fileSystem.chooseEntry(Object.assign(params, extraParams), resolve);
-  });
+  await openEntryChoosingWindow(Object.assign(params, extraParams));
   const dialog = await waitForDialog();
 
   // Ensure the input field is ready.
   await remoteCall.waitForElement(dialog, '#filename-input-textbox');
 
   await clickOkButtonExpectName(dialog, expectName, 'saveAs');
-  return (await result).name;
+  const entry = await pollForChosenEntry(caller);
+  return entry.name;
 }
 
 /**
@@ -793,7 +794,7 @@ testcase.openFileDialogFileListShowContextMenu = async () => {
   await addEntries(['local'], BASIC_LOCAL_ENTRY_SET);
 
   // Open file picker dialog.
-  chrome.fileSystem.chooseEntry({type: 'openFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'openFile'});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -845,7 +846,7 @@ testcase.openFileDialogFileListShowContextMenu = async () => {
  */
 testcase.openFileDialogSelectAllDisabled = async () => {
   // Open file picker dialog.
-  chrome.fileSystem.chooseEntry({type: 'openFile'}, (entry) => {});
+  await openEntryChoosingWindow({type: 'openFile'});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
@@ -873,8 +874,7 @@ testcase.openMultiFileDialogSelectAllEnabled = async () => {
   sendBrowserTestCommand({name: 'setLastDownloadDir'}, () => {});
 
   // Open file picker dialog with support for selecting multiple files.
-  chrome.fileSystem.chooseEntry(
-      {type: 'openFile', acceptsMultiple: true}, (entry) => {});
+  await openEntryChoosingWindow({type: 'openFile', acceptsMultiple: true});
   const appId = await waitForDialog();
 
   // Wait to finish initial load.
--- ui/file_manager/integration_tests/file_manager/holding_space.js
+++ ui/file_manager/integration_tests/file_manager/holding_space.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {getCaller, pending, repeatUntil, RootPath, sendTestMessage} from '../test_util.js';
 import {testcase} from '../testcase.js';
 
-import {navigateWithDirectoryTree, remoteCall, setupAndWaitUntilReady} from './background.js';
+import {navigateWithDirectoryTree, openEntryChoosingWindow, remoteCall, setupAndWaitUntilReady} from './background.js';
 import {waitForDialog} from './file_dialog.js';
 
 /**
@@ -44,8 +44,9 @@ testcase.holdingSpaceWelcomeBanner = async () => {
  * using the new banners framework.
  */
 testcase.holdingSpaceWelcomeBannerWillShowForModalDialogs = async () => {
-  // Open Save as dialog.
-  chrome.fileSystem.chooseEntry({type: 'saveFile'}, entry => {});
+  // Open Save as dialog in the foreground window.
+  await openEntryChoosingWindow({type: 'saveFile'});
+
   const appId = await waitForDialog();
 
   // Ensure the Holding space welcome banner is the only banner prioritised.
