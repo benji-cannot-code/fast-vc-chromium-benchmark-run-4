97b7ccab4d6085f5669ce80a1c1e9fcf5ae986e0
Ahmed Mehfooz <amehfooz@chromium.org>
2022-06-03 20:25:54 +0000

    Add resize animation for grouped notification
    This CL adds an animation to resize a grouped notification
    after one of it's children has been removed.
    This firstly involves moving up
    all child notifications below the removed notification.
    Secondly, resizing the parent notification along with the MessagePopupCollection / UnifiedMessageListView based on where
    the notification is displayed.
    
    Bug: 1233698
    
    Change-Id: Icaa6a90e4ba1a015fc02d469bb7af8f7ef3ad9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671738
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010727}

--- ash/system/message_center/message_center_utils.cc
+++ ash/system/message_center/message_center_utils.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/message_center.h"
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 
 namespace {
 
@@ -89,6 +90,16 @@ GetActiveNotificationViewControllerForDisplay(int64_t display_id) {
       ->unified_system_tray()
       ->GetNotificationGroupingController()
       ->GetActiveNotificationViewController();
+}
+
+message_center::NotificationViewController*
+GetActiveNotificationViewControllerForNotificationView(
+    views::View* notification_view) {
+  aura::Window* window = notification_view->GetWidget()->GetNativeWindow();
+  auto display_id =
+      display::Screen::GetScreen()->GetDisplayNearestWindow(window).id();
+
+  return GetActiveNotificationViewControllerForDisplay(display_id);
 }
 
 void InitLayerForAnimations(views::View* view) {
