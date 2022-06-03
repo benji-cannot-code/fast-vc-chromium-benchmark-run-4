f9d64a4952658b8c2c2b41583a735a90cc53174a
avynn <avynn@google.com>
2022-06-03 18:59:05 +0000

    Desk Templates: Add storage support for tab groups.
    This CL adds in support for tab groups for the desk template storage.
    This support includes the ability for the storage layer to serialize
    and deserialize templates that have browsers containing tab groups.
    
    Adding support for capturing and launching templates with tab groups
    is upcoming.
    
    Design Doc: https://docs.google.com/document/d/1QxH9A0QSAfXBDixB_-yk1O-aX_HbUt_5R8YUnm_Qi74/edit?usp=sharing
    
    TESTED: Added and passed test coverage in template conversion, sync
    bridge and app restore tests.
    
    Bug: 1310318
    Change-Id: I93ec8ec199ef952d10477182a715c152731dcf26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627313
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010684}

--- components/app_restore/app_restore_data.h
+++ components/app_restore/app_restore_data.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "chromeos/ui/base/window_state_type.h"
+#include "components/app_restore/tab_group_info.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/ui_base_types.h"
@@ -82,6 +83,7 @@ struct COMPONENT_EXPORT(APP_RESTORE) AppRestoreData {
   apps::mojom::WindowInfoPtr GetAppWindowInfo() const;
 
   // App launch parameters.
+  // TODO(crbug.com/1326250): Remove optional wrappers around vector fields.
   absl::optional<int32_t> event_flag;
   absl::optional<int32_t> container;
   absl::optional<int32_t> disposition;
@@ -106,6 +108,11 @@ struct COMPONENT_EXPORT(APP_RESTORE) AppRestoreData {
   // `snap_percentage` of 60 when the display is in portrait means the height is
   // 60 percent of the work area height.
   absl::optional<uint32_t> snap_percentage;
+  // For Browsers only, represents tab groups associtated with this browser
+  // instance if there are any. This is only used in Desks Storage, tab groups
+  // in full restore are persistsed by sessions.  This field is not converted to
+  // base::value in base value conversions.
+  absl::optional<std::vector<TabGroupInfo>> tab_group_infos;
 
   // Extra ARC window's information.
   absl::optional<gfx::Size> minimum_size;
