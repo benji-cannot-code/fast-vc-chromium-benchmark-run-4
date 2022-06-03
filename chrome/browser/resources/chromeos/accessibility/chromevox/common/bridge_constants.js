0db3277d923eb831df7fed7b7eafa8af686e86ff
Anastasia Helfinstein <anastasi@google.com>
2022-06-03 22:57:13 +0000

    [ChromeVox] Fix regression to node actions in ChromeVox panel
    menu, and verify that the Actions menu contains "perform default action"
    
    AX-Relnotes: n/a.
    Bug: 1331203
    Test: Manually inspect behavior -- focus ChromeVox on a link, open the
    Change-Id: I764d75b3eef70d858eac52858163d18586a7a054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687550
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010778}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/bridge_constants.js
@@ -126,6 +126,7 @@ BridgeConstants = {
  * @enum {string}
  */
 BridgeActions = {
+  CLEAR_SAVED_NODE: 'clearSavedNode',
   CREATE: 'create',
   CREATE_ALL_NODE_MENU_BACKGROUNDS: 'createAllNodeMenuBackgrounds',
   CREATE_NEW_I_SEARCH: 'createNewISearch',
@@ -139,6 +140,7 @@ BridgeActions = {
   NOTIFY_EVENT_STREAM_FILTER_CHANGED: 'notifyEventStreamFilterChanged',
   PERFORM_CUSTOM_ACTION_ON_CURRENT_NODE: 'performCustomActionOnCurrentNode',
   PERFORM_STANDARD_ACTION_ON_CURRENT_NODE: 'performStandardActionOnCurrentNode',
+  SAVE_CURRENT_NODE: 'saveCurrentNode',
   SET_RANGE_TO_I_SEARCH_NODE: 'setRangeToISearchNode',
   WAIT_FOR_PANEL_COLLAPSE: 'waitForPanelCollapse',
 };
--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel.js
@@ -279,7 +279,7 @@ export class Panel extends PanelInterface {
    * @param {Event=} opt_event An optional event that triggered this.
    * @param {*=} opt_activateMenuTitle Title msg id of menu to open.
    */
-  static onOpenMenus(opt_event, opt_activateMenuTitle) {
+  static async onOpenMenus(opt_event, opt_activateMenuTitle) {
     // If the menu was already open, close it now and exit early.
     if (Panel.mode_ !== PanelMode.COLLAPSED) {
       Panel.setMode(PanelMode.COLLAPSED);
@@ -293,6 +293,7 @@ export class Panel extends PanelInterface {
       opt_event.preventDefault();
     }
 
+    await BackgroundBridge.PanelBackground.saveCurrentNode();
     Panel.setMode(PanelMode.FULLSCREEN_MENUS);
 
     const onFocusDo = async () => {
@@ -1029,6 +1030,7 @@ export class Panel extends PanelInterface {
 
     // Make sure all menus are cleared to avoid bogus output when we re-open.
     Panel.clearMenus();
+    BackgroundBridge.PanelBackground.clearSavedNode();
 
     // Make sure we're not in full-screen mode.
     Panel.setMode(PanelMode.COLLAPSED);
