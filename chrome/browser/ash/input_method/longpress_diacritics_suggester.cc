e0b80f8204495a13e782a5d72ccb24c4b4f465fb
jhtin <jhtin@chromium.org>
2022-06-03 00:59:53 +0000

    Diacritics should return false on unexpected text change
    This hides the suggestion window in AssistiveSuggester.
    context: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/input_method/assistive_suggester.cc;l=502
    
    Bug: b/217560706
    Change-Id: I8e6ce379dfbed256d584f053e63a458147887831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3683059
    Auto-Submit: jhtin <jhtin@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010388}

--- chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
+++ chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
@@ -140,7 +140,14 @@ bool LongpressDiacriticsSuggester::TrySuggestWithSurroundingText(
     const std::u16string& text,
     int cursor_pos,
     int anchor_pos) {
-  // TODO(b/217560706): Should dismiss when surrounding text changes.
+  if (displayed_window_base_character_ == absl::nullopt ||
+      text_changed_since_longpress_ || anchor_pos != cursor_pos ||
+      cursor_pos <= 0 || cursor_pos > text.size() ||
+      text[cursor_pos - 1] != *displayed_window_base_character_) {
+    return false;
+  }
+
+  text_changed_since_longpress_ = true;
   return true;
 }
 
@@ -242,6 +249,7 @@ LongpressDiacriticsSuggester::GetCurrentShownDiacritics() {
 }
 
 void LongpressDiacriticsSuggester::Reset() {
+  text_changed_since_longpress_ = false;
   displayed_window_base_character_ = absl::nullopt;
   highlighted_index_ = absl::nullopt;
 }
