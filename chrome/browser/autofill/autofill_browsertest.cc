33a38778e02357cf048cb49c1fc86eadea3f13ef
Fergal Daly <fergal@chromium.org>
2022-06-03 10:10:55 +0000

    Update WaitForNotification callers to check return value.
    This only applies to files in /chrome/browser/autofill.
    
    Sheriffs, please do not revert this. If this is causing flakes, please
    just remove the newly-added ASSERT/EXPECT and file a bug. You might
    also need to add a `std::ignore = ` if WaitForNotification has been
    marked `[[ nodiscard ]]` by the time you're doing it.
    
    This updates callers of
    AccessibilityNotificationWaiter::WaitForNotification to check that it
    actually received a notification.
    
    Test waiters may return because the test has timed out. Without this
    check, a timeout will cause the test to continue running and may pass
    erroneously or fail but on a later EXPECT/ASSERT giving a confusing
    reason.
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    BUG: 1330182
    Bug: 1330182
    Change-Id: I271f70480b22ec8f0125eb9d24b2dc95b2ae5ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686696
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010503}

--- chrome/browser/autofill/autofill_browsertest.cc
+++ chrome/browser/autofill/autofill_browsertest.cc
@@ -666,7 +666,7 @@ IN_PROC_BROWSER_TEST_F(AutofillAccessibilityTest, DISABLED_TestAutofillState) {
   content::AccessibilityNotificationWaiter layout_waiter_one(
       web_contents(), ui::kAXModeComplete, ax::mojom::Event::kLoadComplete);
   ui_test_utils::NavigateToURL(&params);
-  layout_waiter_one.WaitForNotification();
+  ASSERT_TRUE(layout_waiter_one.WaitForNotification());
 
   // Focus target form field.
   const std::string focus_name_first_js =
@@ -706,7 +706,7 @@ IN_PROC_BROWSER_TEST_F(AutofillAccessibilityTest, DISABLED_TestAutofillState) {
   content::AccessibilityNotificationWaiter layout_waiter_two(
       web_contents(), ui::kAXModeComplete, ax::mojom::Event::kLoadComplete);
   ui_test_utils::NavigateToURL(&params);
-  layout_waiter_two.WaitForNotification();
+  ASSERT_TRUE(layout_waiter_two.WaitForNotification());
 
   // Focus target form field.
   ASSERT_TRUE(content::ExecuteScript(web_contents(), focus_name_first_js));
@@ -738,7 +738,7 @@ IN_PROC_BROWSER_TEST_F(AutofillAccessibilityTest,
   content::AccessibilityNotificationWaiter layout_waiter_one(
       web_contents(), ui::kAXModeComplete, ax::mojom::Event::kLoadComplete);
   ui_test_utils::NavigateToURL(&params);
-  layout_waiter_one.WaitForNotification();
+  ASSERT_TRUE(layout_waiter_one.WaitForNotification());
 
   // Focus target form field.
   const std::string focus_name_first_js =
@@ -774,7 +774,7 @@ IN_PROC_BROWSER_TEST_F(AutofillAccessibilityTest,
   content::AccessibilityNotificationWaiter layout_waiter_two(
       web_contents(), ui::kAXModeComplete, ax::mojom::Event::kLoadComplete);
   ui_test_utils::NavigateToURL(&params);
-  layout_waiter_two.WaitForNotification();
+  ASSERT_TRUE(layout_waiter_two.WaitForNotification());
 
   // Focus target form field.
   ASSERT_TRUE(content::ExecuteScript(web_contents(), focus_name_first_js));
