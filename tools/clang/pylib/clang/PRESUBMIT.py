ee8db0f53ba6a6962162a38f0b33aea624340296
Bruce Dawson <brucedawson@chromium.org>
2022-06-03 19:52:15 +0000

    Stop compile_db_test.py from running on Python 2
    compile_db_test.py has been running on Python 3 for a long time, but it
    is also running on Python 2, which is unnecessary. This change turns off
    Python 2 for this script. The change was tested with this command:
    
      git cl presubmit --files tools/clang/pylib/clang/compile_db_test.py
    
    Bug: 1313804
    Change-Id: Ib35801ed5e1ad1113df762b1da9276c14c78b0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687784
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Amy Huang <akhuang@google.com>
    Commit-Queue: Amy Huang <akhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010716}

--- tools/clang/pylib/clang/PRESUBMIT.py
+++ tools/clang/pylib/clang/PRESUBMIT.py
@@ -15,6 +15,7 @@ def CheckChangeOnCommit(input_api, output_api):
       input_api.canned_checks.RunUnitTestsInDirectory(input_api,
                                                       output_api,
                                                       '.', [r'^.+_test\.py$'],
+                                                      run_on_python2=False,
                                                       skip_shebang_check=True))
 
   return results
