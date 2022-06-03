9bcba028eda57c39fee515f0e0f6fba5e37356cc
Cici Ruan <cuicuiruan@google.com>
2022-06-03 23:12:11 +0000

    arc/gio: Rename the file name
    Rename the file name which is missing from crrev.com/c/3656514.
    
    Bug: b:188452744
    Test: build
    Change-Id: Ie40ad1e214421d513342064c31e65c97e4194c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688857
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010789}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/pill_button.h"
 #include "base/bind.h"
 #include "chrome/app/vector_icons/vector_icons.h"
-#include "chrome/browser/ash/arc/input_overlay/ui/edit_mode_exit_view.h"
+#include "chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/educational_view.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/error_view.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/input_menu_view.h"
--- chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.h
+++ chrome/browser/ash/arc/input_overlay/ui/edit_finish_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_MODE_EXIT_VIEW_H_
-#define CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_MODE_EXIT_VIEW_H_
+#ifndef CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_FINISH_VIEW_H_
+#define CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_FINISH_VIEW_H_
 
 #include <memory>
 
@@ -66,4 +66,4 @@ class EditFinishView : public views::View {
 }  // namespace input_overlay
 }  // namespace arc
 
-#endif  // CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_MODE_EXIT_VIEW_H_
+#endif  // CHROME_BROWSER_ASH_ARC_INPUT_OVERLAY_UI_EDIT_FINISH_VIEW_H_
