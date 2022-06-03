4e041f77506b996ab0fd1ab6967543ebc7371195
Roman Sorokin <rsorokin@chromium.org>
2022-06-03 14:10:01 +0000

    [OOBE] Remove (on)FocusReturned logic
    Not used since http://crrev.com/c/3168887
    
    Bug: 1100910
    Change-Id: Iade86a2c329de601cc2113ed8639d4b3ed7017c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688070
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010557}

--- chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.cc
+++ chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.cc
@@ -99,12 +99,6 @@ class OobeWebDialogView : public views::WebDialogView {
     web_contents()->FocusThroughTabTraversal(reverse);
     GetWidget()->Activate();
     web_contents()->Focus();
-
-    if (!GetOobeUI())
-      return;
-    CoreOobeView* view = GetOobeUI()->GetCoreOobeView();
-    if (view)
-      view->FocusReturned(reverse);
   }
 
  private:
