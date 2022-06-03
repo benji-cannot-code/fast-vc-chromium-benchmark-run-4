5e364b229563d49de71495ef13f55b9b71bdfa9e
David Benjamin <davidben@chromium.org>
2022-06-03 15:08:18 +0000

    Remove Key::serialized_key_data_
    Every asymmetric WebCrypto key holds a serialized copy of its EVP_PKEY,
    so that it can be serialized from the main thread. But this is
    unnecessary. EVP_PKEYs are thread-safe, provided you don't try to mutate
    them. It is fine to have one thread try to serialize it while another
    thread is signing, etc., with it.
    
    Instead, just serialize it on-demand, rather than keep a redundant copy
    of the key in memory.
    
    Fixed: 1180244
    Change-Id: I702f1f4500cebaa8d942a7887487f8da0a09ed76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3675412
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010577}

--- components/webcrypto/algorithm_implementation.cc
+++ components/webcrypto/algorithm_implementation.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webcrypto/algorithm_implementation.h"
 
+#include "base/notreached.h"
+#include "components/webcrypto/algorithms/asymmetric_key_util.h"
 #include "components/webcrypto/blink_key_handle.h"
 #include "components/webcrypto/status.h"
 
@@ -95,8 +97,31 @@ Status AlgorithmImplementation::ExportKey(blink::WebCryptoKeyFormat format,
 Status AlgorithmImplementation::SerializeKeyForClone(
     const blink::WebCryptoKey& key,
     blink::WebVector<uint8_t>* key_data) const {
-  *key_data = GetSerializedKeyData(key);
-  return Status::Success();
+  switch (key.GetType()) {
+    case blink::kWebCryptoKeyTypeSecret:
+      *key_data = GetSymmetricKeyData(key);
+      return Status::Success();
+
+    case blink::kWebCryptoKeyTypePublic: {
+      std::vector<uint8_t> vec;
+      Status status = ExportPKeySpki(GetEVP_PKEY(key), &vec);
+      if (status.IsSuccess()) {
+        *key_data = vec;
+      }
+      return status;
+    }
+
+    case blink::kWebCryptoKeyTypePrivate: {
+      std::vector<uint8_t> vec;
+      Status status = ExportPKeyPkcs8(GetEVP_PKEY(key), &vec);
+      if (status.IsSuccess()) {
+        *key_data = vec;
+      }
+      return status;
+    }
+  }
+  NOTREACHED();
+  return Status::ErrorUnexpected();
 }
 
 Status AlgorithmImplementation::DeserializeKeyForClone(
