a41e0ea6440b55b0e1bcfcbe9f251c70d94c042c
Emilia Paz <emiliapaz@chromium.org>
2022-06-03 00:32:52 +0000

    [Extensions cleanup] Update site access items based on the new section
    Instead of comparing the new site interaction with the current section's
    site interaction, we can compare directly the sections. Basically we get
    the new section and reorder/remove/reinsert based on the current section.
    
    This allows us to remove `site_interaction` from SiteAccessSection,
    which is wanted because user site permissions may affect the items in
    the section.
    
    Bug: 1263310
    Change-Id: I30c87aca3d1e209de11e96144965738825ed4057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3685373
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010375}

--- chrome/browser/ui/views/extensions/extensions_tabbed_menu_view.cc
+++ chrome/browser/ui/views/extensions/extensions_tabbed_menu_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/check.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/notreached.h"
@@ -218,11 +219,10 @@ ExtensionsTabbedMenuView::ExtensionsTabbedMenuView(
       allow_pinning_(allow_pinning),
       requests_access_{
           nullptr, nullptr, nullptr,
-          IDS_EXTENSIONS_MENU_SITE_ACCESS_TAB_REQUESTS_ACCESS_SECTION_TITLE,
-          extensions::SitePermissionsHelper::SiteInteraction::kPending},
-      has_access_{nullptr, nullptr, nullptr,
-                  IDS_EXTENSIONS_MENU_SITE_ACCESS_TAB_HAS_ACCESS_SECTION_TITLE,
-                  extensions::SitePermissionsHelper::SiteInteraction::kActive} {
+          IDS_EXTENSIONS_MENU_SITE_ACCESS_TAB_REQUESTS_ACCESS_SECTION_TITLE},
+      has_access_{
+          nullptr, nullptr, nullptr,
+          IDS_EXTENSIONS_MENU_SITE_ACCESS_TAB_HAS_ACCESS_SECTION_TITLE} {
   views::Builder<ExtensionsTabbedMenuView>(this)
       .SetLayoutManager(std::make_unique<views::BoxLayout>(
           views::BoxLayout::Orientation::kVertical))
@@ -684,10 +684,18 @@ void ExtensionsTabbedMenuView::UpdateSiteAccessMenuItems(
       continue;
     }
 
+    DCHECK(section);
+    auto* new_section = GetSectionForSiteInteraction(
+        item->view_controller()->GetSiteInteraction(GetActiveWebContents()));
+
+    // Remove item when it is no section at all.
+    if (!new_section) {
+      section->items->RemoveChildViewT(item);
+      return;
+    }
+
     // Reorder item when it is in the same section.
-    auto site_interaction =
-        item->view_controller()->GetSiteInteraction(GetActiveWebContents());
-    if (site_interaction == section->site_interaction) {
+    if (new_section == section) {
       item->Update();
       int new_index =
           FindIndex(section->items, item->view_controller()->GetActionName());
@@ -695,14 +703,9 @@ void ExtensionsTabbedMenuView::UpdateSiteAccessMenuItems(
       return;
     }
 
-    // Remove item when it is in a different section or no section at all.
+    // Re insert item when it is in the incorrect section.
     std::unique_ptr<SiteAccessMenuItemView> item_to_move =
         section->items->RemoveChildViewT(item);
-    auto* new_section = GetSectionForSiteInteraction(site_interaction);
-    if (!new_section)
-      return;
-
-    // Re insert item to the correct section.
     item_to_move->Update();
     InsertSiteAccessItem(std::move(item_to_move), new_section);
   }
