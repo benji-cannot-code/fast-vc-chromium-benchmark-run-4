90bb57c3f17815df2866e6ce56c31920d32526a2
Tom Anderson <thomasanderson@chromium.org>
2022-06-03 22:36:22 +0000

    Move native_color_mixers_linux.cc to //ui/gtk
    This is in preparation for adding a QT color mixer.  The current
    native_color_mixers_linux.cc is tightly coupled with the GTK colors, so
    it doesn't make sense to reuse them for QT.  A followup CL will remove
    the Linux color constants from PLATFORM_SPECIFIC_COLOR_IDS.
    
    R=tluk
    
    Bug: 1317782
    Change-Id: I25737d6576dee4df5f9bc86d0916a91db26b2877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688762
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010769}

--- ui/color/native_color_mixers.cc
+++ ui/color/native_color_mixers.cc
@@ -10,13 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_MAC) && \
-    !BUILDFLAG(IS_WIN)
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_WIN)
 void AddNativeCoreColorMixer(ColorProvider* provider,
                              const ColorProviderManager::Key& key) {}
 #endif
 
-#if !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_WIN)
+#if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_WIN)
 void AddNativeUiColorMixer(ColorProvider* provider,
                            const ColorProviderManager::Key& key) {}
 #endif
