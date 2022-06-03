dd1d4a274a80f732c9a32433bbdfe9b77fa1dff4
Matt Menke <mmenke@chromium.org>
2022-06-03 00:58:43 +0000

    Make chromecast::mojom::MessageCallback use a mojom::ListValue.
    It was using DeprecatedListValue, which maps to the (also deprecated)
    base::ListValue. Somewhat confusingly, mojom::ListValue maps to the
    (non-deprecated) base::Value::List class.
    
    Bug: 1301889
    Change-Id: I83e02b14869fd87f033df5f74fd51a1fee838e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686479
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010387}

--- chromecast/browser/webui/cast_webui.h
+++ chromecast/browser/webui/cast_webui.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/memory/weak_ptr.h"
+#include "base/values.h"
 #include "chromecast/browser/webui/mojom/webui.mojom.h"
 #include "chromecast/chromecast_buildflags.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -46,7 +47,8 @@ class CastWebUI : public mojom::WebUi, public content::WebUIController {
  private:
   void AddWebviewSupport();
   void OnWebUIReady(const base::ListValue* args);
-  void InvokeCallback(const std::string& message, const base::ListValue* args);
+  void InvokeCallback(const std::string& message,
+                      const base::Value::List& args);
 
   // mojom::WebUI implementation:
   void RegisterMessageCallback(
