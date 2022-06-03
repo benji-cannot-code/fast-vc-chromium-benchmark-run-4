299b512e51fa0ccb6b9776dedc9888eec6c68f7f
Peter Kotwicz <pkotwicz@chromium.org>
2022-06-03 23:14:33 +0000

    [FedCM] Fix fetching manifest list for localhost
    This CL adds special-handling for:
    - fetching the manifest list for localhost
    - computing the identity provider and relying party URLs to display in
      the FedCM prompt
    
    BUG=1330022
    TEST=IdpNetworkRequestManagerTest.ComputeManifestListUrl
    
    Change-Id: I16c684ddcc12f6c618ed3dadec7c9b5a9bf36d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674437
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010791}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionViewTest.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionViewTest.java
@@ -109,8 +109,8 @@ public class AccountSelectionViewTest {
             mModel.set(ItemProperties.HEADER,
                     new PropertyModel.Builder(HeaderProperties.ALL_KEYS)
                             .with(HeaderProperties.TYPE, HeaderType.SIGN_IN)
-                            .with(HeaderProperties.FORMATTED_RP_ETLD_PLUS_ONE, "example.org")
-                            .with(HeaderProperties.FORMATTED_IDP_ETLD_PLUS_ONE, "idp.org")
+                            .with(HeaderProperties.RP_FOR_DISPLAY, "example.org")
+                            .with(HeaderProperties.IDP_FOR_DISPLAY, "idp.org")
                             .build());
         });
         pollUiThread(() -> mContentView.getVisibility() == View.VISIBLE);
@@ -129,8 +129,8 @@ public class AccountSelectionViewTest {
             mModel.set(ItemProperties.HEADER,
                     new PropertyModel.Builder(HeaderProperties.ALL_KEYS)
                             .with(HeaderProperties.TYPE, HeaderType.VERIFY)
-                            .with(HeaderProperties.FORMATTED_RP_ETLD_PLUS_ONE, "example.org")
-                            .with(HeaderProperties.FORMATTED_IDP_ETLD_PLUS_ONE, "idp.org")
+                            .with(HeaderProperties.RP_FOR_DISPLAY, "example.org")
+                            .with(HeaderProperties.IDP_FOR_DISPLAY, "idp.org")
                             .build());
         });
         pollUiThread(() -> mContentView.getVisibility() == View.VISIBLE);
@@ -227,8 +227,8 @@ public class AccountSelectionViewTest {
             mModel.set(ItemProperties.HEADER,
                     new PropertyModel.Builder(HeaderProperties.ALL_KEYS)
                             .with(HeaderProperties.TYPE, HeaderType.AUTO_SIGN_IN)
-                            .with(HeaderProperties.FORMATTED_RP_ETLD_PLUS_ONE, "example.org")
-                            .with(HeaderProperties.FORMATTED_IDP_ETLD_PLUS_ONE, "idp.org")
+                            .with(HeaderProperties.RP_FOR_DISPLAY, "example.org")
+                            .with(HeaderProperties.IDP_FOR_DISPLAY, "idp.org")
                             .build());
         });
         pollUiThread(() -> mContentView.getVisibility() == View.VISIBLE);
@@ -332,7 +332,7 @@ public class AccountSelectionViewTest {
     private PropertyModel buildDataSharingConsentItem(String idpEtldPlusOne) {
         DataSharingConsentProperties.Properties properties =
                 new DataSharingConsentProperties.Properties();
-        properties.mFormattedIdpEtldPlusOne = idpEtldPlusOne;
+        properties.mIdpForDisplay = idpEtldPlusOne;
         properties.mTermsOfServiceUrl = "https://rp.com/tos";
         properties.mPrivacyPolicyUrl = "https://rp.com/privacy";
 
