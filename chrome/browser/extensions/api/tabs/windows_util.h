e2a378ad79b00c4bb0ff08af47f82176e1874818
Brandon Wylie <wylieb@chromium.org>
2022-06-03 18:58:56 +0000

    [bookmarks] Check incognito compatibility when opening a new window
    * Check incognito availability preferences.
    * Check URL eligibility for incognito (e.g. chrome://history).
    
    Bug: 1330853
    Change-Id: I578879d76a9730e9dba2893ca072f7db9ed846e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3684192
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010683}

--- chrome/browser/extensions/api/tabs/windows_util.h
+++ chrome/browser/extensions/api/tabs/windows_util.h
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_API_TABS_WINDOWS_UTIL_H__
 
 #include <string>
+#include <vector>
 
 #include "chrome/browser/extensions/window_controller_list.h"
 
 class ExtensionFunction;
+class Profile;
+class GURL;
 
 namespace extensions {
 class WindowController;
@@ -35,6 +38,20 @@ bool GetBrowserFromWindowID(ExtensionFunction* function,
 bool CanOperateOnWindow(const ExtensionFunction* function,
                         const extensions::WindowController* controller,
                         extensions::WindowController::TypeFilter filter);
+
+// Enum return value for `ShouldOpenIncognitoWindow`, indicating whether to use
+// incognito or the presence of an error.
+enum IncognitoResult { kRegular, kIncognito, kError };
+
+// Returns whether the window should be created in incognito mode. `incognito`
+// is the optional caller preference. `urls` is the list of urls to open. If
+// we are creating an incognito window, the function will remove these urls
+// which may not be opened in incognito mode.  If window creation leads the
+// browser into an erroneous state, `error` is populated.
+IncognitoResult ShouldOpenIncognitoWindow(Profile* profile,
+                                          absl::optional<bool> incognito,
+                                          std::vector<GURL>* urls,
+                                          std::string* error);
 
 }  // namespace windows_util
 
