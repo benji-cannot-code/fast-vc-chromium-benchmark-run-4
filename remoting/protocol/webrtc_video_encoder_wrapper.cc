00e94909efb7c65bb61154059821ee785f23d19e
Joe Downing <joedow@google.com>
2022-06-03 18:14:34 +0000

    AV1 encoder wrapper tweaks for Chrome Remote Desktop
    This CL sets a number of encoder parameters tht we suggested by the WebM
    folks. Initial testing with these values indicates a ~2X increase in
    framerate and makes the codec usable (though it still seems a tad slower
    than VP9 in my initial testing). I've also removed a NOTIMPL log from the
    encoding path as it is way too chatty when using AV1.
    
    Change-Id: Ieccc2a2e2f96e94f6dd5137c5ccb22492da3c878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687980
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010661}

--- remoting/protocol/webrtc_video_encoder_wrapper.cc
+++ remoting/protocol/webrtc_video_encoder_wrapper.cc
@@ -403,7 +403,6 @@ WebrtcVideoEncoderWrapper::ReturnEncodedFrame(
 #endif
   } else if (frame.codec == webrtc::kVideoCodecAV1) {
     // TODO(joedow): Set codec specific params for AV1 here.
-    NOTIMPLEMENTED();
   } else {
     NOTREACHED();
   }
