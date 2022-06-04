15f4307a02b591593e2ca424e488fb2640488910
Nektarios Paisios <nektar@chromium.org>
2022-06-04 11:32:25 +0000

    Sets the bounding box of the snapshot or OCRed page that has been annotated by Screen AI
    Even though all object descriptors that come back from the Screen AI library
    have corresponding bounding boxes in the proto, the input image to the library does not
    return its bounding box in that proto. We need to pass it in
    manually when calling the function that converts the proto into
    an AXTreeUpdate.
    
    R=rhalavati@chromium.org, dtseng@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 1248380
    Change-Id: I4084243f64333243ec5e60dbff1f21290901a066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687443
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010837}

--- components/services/screen_ai/proto/proto_convertor.cc
+++ components/services/screen_ai/proto/proto_convertor.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/accessibility/ax_role_properties.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/transform.h"
 
@@ -313,7 +314,8 @@ void AddSubTree(const std::vector<ui::AXNodeData>& nodes,
 namespace screen_ai {
 
 ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
-    const std::string& serialized_proto) {
+    const std::string& serialized_proto,
+    const gfx::Rect& image_rect) {
   ui::AXTreeUpdate update;
 
   chrome_screen_ai::VisualAnnotation visual_annotation;
@@ -368,8 +370,7 @@ ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
     ui::AXNodeData& rootnode = nodes[index++];
     rootnode.role = ax::mojom::Role::kDialog;
     rootnode.id = GetNextNodeID();
-    // TODO(nektar): Set the bounding box of `rootnode` to the bounding box of
-    // the screen snapshot.
+    rootnode.relative_bounds.bounds = gfx::RectF(image_rect);
     for (const auto& ui_component : visual_annotation.ui_component())
       SerializeUIComponent(ui_component, index++, rootnode, nodes);
   }
@@ -381,8 +382,7 @@ ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
     page_node.id = GetNextNodeID();
     page_node.AddBoolAttribute(ax::mojom::BoolAttribute::kIsPageBreakingObject,
                                true);
-    // TODO(nektar): Set the bounding box of `page_node` to the bounding box of
-    // the captured image.
+    page_node.relative_bounds.bounds = gfx::RectF(image_rect);
     for (const auto& block_to_lines_pair : blocks_to_lines_map) {
       for (const auto& line_sequence_number_to_index_pair :
            block_to_lines_pair.second) {
--- components/services/screen_ai/proto/proto_convertor.h
+++ components/services/screen_ai/proto/proto_convertor.h
@@ -10,11 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/accessibility/ax_tree_update.h"
 
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
 namespace screen_ai {
 
-// Converts serialized VisualAnnotation proto from ScreenAI to AXTreeUpdate.
+// Converts serialized VisualAnnotation proto from ScreenAI to AXTreeUpdate. The
+// argument `image_rect` is the bounding box of the image from which the visual
+// annotation was created.
 ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
-    const std::string& serialized_proto);
+    const std::string& serialized_proto,
+    const gfx::Rect& image_rect);
 
 // Converts an AXTreeUpdate snapshot to serialized ViewHierarchy proto for
 // Screen2X.
