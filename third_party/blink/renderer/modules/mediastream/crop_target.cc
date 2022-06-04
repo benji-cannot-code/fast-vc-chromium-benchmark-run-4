04dd9555d86cd5348b86fcd3c03d81e05b0f22d8
Elad Alon <eladalon@chromium.org>
2022-06-04 11:28:06 +0000

    [Region Capture] Introduce CropTarget
    Introduce a skeleton implementation of CropTarget.
    https://w3c.github.io/mediacapture-region/#dom-croptarget
    
    Implementation in upcoming CLs.
    
    Bug: 1291140
    Change-Id: Ie51f9a05ca4eefc1881573ce94533cdc8297983a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688393
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010829}

---
+++ third_party/blink/renderer/modules/mediastream/crop_target.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/mediastream/crop_target.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+
+namespace blink {
+
+CropTarget::CropTarget() = default;
+
+ScriptPromise CropTarget::fromElement(ExecutionContext* execution_context,
+                                      Element* element,
+                                      ExceptionState& exception_state) {
+  // TODO(crbug.com/1291140): Continue implementation.
+  return ScriptPromise();
+}
+
+}  // namespace blink
