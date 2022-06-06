bc354df3facfb5678e7da63f6dadb95bbf1e337f
Jeffrey Young <cowmoo@google.com>
2022-06-06 21:37:19 +0000

    personalization: number collection grid items
    Use role=listbox and role=option and number each collection
    for chromevox. This mirrors the a11y attributes already present
    in the images grid for an individual collection.
    https://photos.app.goo.gl/nGtrnWhSLacLBccX6
    
    BUG=b:234060594
    
    Change-Id: I96112d7a12f0d90214619def2b30a1b98ee1731d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689049
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011136}

--- ash/webui/personalization_app/resources/untrusted/collections_grid.ts
+++ ash/webui/personalization_app/resources/untrusted/collections_grid.ts
@@ -469,6 +469,10 @@ export class CollectionsGrid extends PolymerElement {
       elem.removeAttribute('hidden');
     }
   }
+
+  private getAriaIndex_(index: number): number {
+    return index + 1;
+  }
 }
 
 customElements.define(CollectionsGrid.is, CollectionsGrid);
