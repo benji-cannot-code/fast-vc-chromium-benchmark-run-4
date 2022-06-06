0e46189f070ff0abf8fc0ccb2fc39348268e4a9f
Elad Alon <eladalon@chromium.org>
2022-06-06 12:07:35 +0000

    [Region Capture] Implement CropTarget.fromElement()
    CropTarget.fromElement() calls are piped to the
    existing mediaDevices.produceCropId().
    
    Right now:
    * CropTarget.fromElement is NOT Web-exposed.
    * mediaDevices.produceCropId() IS Web-exposed.
    
    This will be inverted by later CLs.
    
    Bug: 1291140
    Change-Id: Ief216ac7563f5de98aadf5775e2f7283a07ac3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689498
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1010998}

--- third_party/blink/renderer/modules/mediastream/crop_target.h
+++ third_party/blink/renderer/modules/mediastream/crop_target.h
@@ -7,18 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_CROP_TARGET_H_
 
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
 namespace blink {
 
-class ExecutionContext;
+class ScriptState;
 
 class MODULES_EXPORT CropTarget final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static ScriptPromise fromElement(ExecutionContext* execution_context,
+  static ScriptPromise fromElement(ScriptState* script_state,
                                    Element* element,
                                    ExceptionState& exception_state);
 
