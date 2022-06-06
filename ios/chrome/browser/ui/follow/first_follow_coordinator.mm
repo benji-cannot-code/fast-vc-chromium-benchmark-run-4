586d14d20a92ef0ddd6ad77d26161d0e17cdec77
Tina Wang <tinazwang@google.com>
2022-06-06 21:32:27 +0000

    [Follow] Add metrics for first follow
    Add metrics for first follow:
    - shown
    - go to feed tapped
    - got it tapped
    
    Bug: 1332875
    Change-Id: Ic888bbce6651b4da7c9e2b1c6fb5168bb358f71b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688790
    Reviewed-by: edchin <edchin@google.com>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011133}

--- ios/chrome/browser/ui/follow/first_follow_coordinator.mm
+++ ios/chrome/browser/ui/follow/first_follow_coordinator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/follow/first_follow_coordinator.h"
 
+#include "ios/chrome/browser/discover_feed/discover_feed_service.h"
+#include "ios/chrome/browser/discover_feed/discover_feed_service_factory.h"
 #import "ios/chrome/browser/favicon/favicon_loader.h"
 #import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #import "ios/chrome/browser/main/browser.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/follow/first_follow_favicon_data_source.h"
 #import "ios/chrome/browser/ui/follow/first_follow_view_controller.h"
 #import "ios/chrome/browser/ui/follow/followed_web_channel.h"
+#import "ios/chrome/browser/ui/ntp/feed_metrics_recorder.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 #import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #import "ios/chrome/common/ui/favicon/favicon_constants.h"
@@ -37,6 +40,9 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 // The view controller is owned by the view hierarchy.
 @property(nonatomic, weak) FirstFollowViewController* firstFollowViewController;
 
+// Feed metrics recorder.
+@property(nonatomic, weak) FeedMetricsRecorder* feedMetricsRecorder;
+
 @end
 
 @implementation FirstFollowCoordinator
@@ -47,6 +53,9 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
   FirstFollowViewController* firstFollowViewController =
       [[FirstFollowViewController alloc] init];
   firstFollowViewController.followedWebChannel = self.followedWebChannel;
+  self.feedMetricsRecorder = DiscoverFeedServiceFactory::GetForBrowserState(
+                                 self.browser->GetBrowserState())
+                                 ->GetFeedMetricsRecorder();
   // Ownership is passed to VC so this object is not retained after VC closes.
   self.followedWebChannel = nil;
   self.firstFollowViewController = firstFollowViewController;
@@ -74,9 +83,13 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
         UIModalPresentationFormSheet;
   }
 
-  [self.baseViewController presentViewController:firstFollowViewController
-                                        animated:YES
-                                      completion:nil];
+  __weak __typeof(self) weakSelf = self;
+  [self.baseViewController
+      presentViewController:firstFollowViewController
+                   animated:YES
+                 completion:^() {
+                   [weakSelf.feedMetricsRecorder recordFirstFollowShown];
+                 }];
 }
 
 - (void)stop {
@@ -88,6 +101,7 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 #pragma mark - ConfirmationAlertActionHandler
 
 - (void)confirmationAlertPrimaryAction {
+  [self.feedMetricsRecorder recordFirstFollowTappedGoToFeed];
   if (self.firstFollowViewController.followedWebChannel.available) {
     [self.newTabPageCommandsHandler
         openNTPScrolledIntoFeedType:FeedTypeFollowing];
@@ -98,6 +112,7 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 }
 
 - (void)confirmationAlertSecondaryAction {
+  [self.feedMetricsRecorder recordFirstFollowTappedGotIt];
   if (self.baseViewController.presentedViewController) {
     [self.baseViewController dismissViewControllerAnimated:YES completion:nil];
   }
