4a7a05395abfdd7f4d3c1a772ba1a9f5810b0399
Geoff Lang <geofflang@google.com>
2022-06-06 17:18:45 +0000

    Block changing the GL_DEBUG_OUTPUT state in the cmd decoder.
    The passthrough command decoder uses debug output to catch errors and
    maintain a consistant state with the driver. Don't allow clients to
    modify this state.
    
    This is not an issue for other states that I know of because they are
    part of extensions which are not enabled. The debug extension must
    always be enabled for the command decoder to function.
    
    Bug: chromium:1299144
    Change-Id: Iaef4be3a2af82123a5b54ffcff4dcb8bc37bfbbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687397
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011063}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
@@ -1184,6 +1184,11 @@ error::Error GLES2DecoderPassthroughImpl::DoDetachShader(GLuint program,
 }
 
 error::Error GLES2DecoderPassthroughImpl::DoDisable(GLenum cap) {
+  if (IsIgnoredCap(cap)) {
+    InsertError(GL_INVALID_ENUM, "Invalid cap.");
+    return error::kNoError;
+  }
+
   api()->glDisableFn(cap);
   return error::kNoError;
 }
@@ -1251,6 +1256,11 @@ error::Error GLES2DecoderPassthroughImpl::DoDrawElementsIndirect(
 }
 
 error::Error GLES2DecoderPassthroughImpl::DoEnable(GLenum cap) {
+  if (IsIgnoredCap(cap)) {
+    InsertError(GL_INVALID_ENUM, "Invalid cap.");
+    return error::kNoError;
+  }
+
   api()->glEnableFn(cap);
   return error::kNoError;
 }
