56ac67bf0bb6de0acf20b0c98a9a64764449cf31
Eric Sum <esum@google.com>
2022-06-06 21:46:36 +0000

    Add metrics for photo orientation match in ambient mode.
    This is to track how often a portrait photo gets assigned to a portrait
    asset (and same for landscape) in the animation. Ideally, this is 100%
    to minimize the amount of photo cropping we do. But we're adding this
    metric to track how ideal our photo assignment is in the field.
    
    Bug: b:233842172
    Change-Id: I44ee26f167b57886dafd0e4c302846d341e853dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690454
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011138}

--- ash/ambient/test/fake_ambient_animation_static_resources.cc
+++ ash/ambient/test/fake_ambient_animation_static_resources.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "ash/constants/ambient_animation_theme.h"
 #include "base/check.h"
 #include "base/notreached.h"
 #include "cc/paint/skottie_wrapper.h"
@@ -48,7 +47,7 @@ gfx::ImageSkia FakeAmbientAnimationStaticResources::GetStaticImageAsset(
 
 AmbientAnimationTheme
 FakeAmbientAnimationStaticResources::GetAmbientAnimationTheme() const {
-  return AmbientAnimationTheme::kFeelTheBreeze;
+  return ambient_animation_theme_;
 }
 
 }  // namespace ash
