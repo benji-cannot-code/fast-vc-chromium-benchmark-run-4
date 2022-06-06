61f70ce844ae47ffb1bfc38f5bda8d5a6762847b
Fergal Daly <fergal@chromium.org>
2022-06-06 09:27:53 +0000

    Add bug reference and TODO https://crbug.com/1333354.
    std::ignore was added in https://crrev.com/c/3689319 by sheriff.
    
    
    Bug: 1330182
    Change-Id: I33845a2e3304d360cb7a9aca069837c6a70107e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689320
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010979}

--- chrome/browser/ash/accessibility/dictation_browsertest.cc
+++ chrome/browser/ash/accessibility/dictation_browsertest.cc
@@ -347,6 +347,8 @@ class DictationTest
         browser()->tab_strip_model()->GetActiveWebContents(),
         ui::kAXModeComplete, ax::mojom::Event::kValueChanged);
     SendFinalResultAndWait(result);
+    // TODO(https://crbug.com/1333354): Investigate why this does not always
+    // return true.
     std::ignore = waiter.WaitForNotification();
     WaitForTextAreaValue(value);
   }
@@ -360,6 +362,8 @@ class DictationTest
     content::BoundingBoxUpdateWaiter bounding_box_waiter(web_contents);
     SendFinalResultAndWait(result);
     bounding_box_waiter.Wait();
+    // TODO(https://crbug.com/1333354): Investigate why this does not always
+    // return true.
     std::ignore = selection_waiter.WaitForNotification();
   }
 
@@ -371,6 +375,8 @@ class DictationTest
         browser()->window()->GetNativeWindow()->GetHost()->GetInputMethod());
     SendFinalResultAndWait(result);
     caret_waiter.Wait();
+    // TODO(https://crbug.com/1333354): Investigate why this does not always
+    // return true.
     std::ignore = selection_waiter.WaitForNotification();
   }
 
