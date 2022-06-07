40ec3e9ad85792e1cd76966ff3ccdd8ada889ed1
Elie Maamari <emaamari@google.com>
2022-06-07 18:08:48 +0000

    Call AuthPerformer from AuthenticationDialog
    Following the implementation of Auth Sessions, all attempts at
    authentication should be routed through `AuthPerformer`. In-session
    authentication in other dialogs is currently done using
    `ExtendedAuthenticator`, this will be changed in the near future. Right
    now, this CL is a small step towards that goal, as
    `AuthenticationDialog` will eventually replace all in-session
    authentication dialogs.
    
    Bug: b/231568585
    Change-Id: I9b95dbb477af7951a76faac4dfbe8425f718003a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3664136
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011560}

--- ash/in_session_auth/in_session_auth_dialog_controller_impl.h
+++ ash/in_session_auth/in_session_auth_dialog_controller_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_IN_SESSION_AUTH_IN_SESSION_AUTH_DIALOG_CONTROLLER_IMPL_H_
 
 #include "ash/public/cpp/in_session_auth_dialog_controller.h"
+#include "ash/public/cpp/in_session_auth_token_provider.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -22,6 +23,16 @@ class InSessionAuthDialogControllerImpl : public InSessionAuthDialogController {
 
   // InSessionAuthDialogController overrides
   void ShowAuthDialog(Reason reason, OnAuthComplete on_auth_complete) override;
+
+  void SetTokenProvider(InSessionAuthTokenProvider* auth_token_provider);
+
+ private:
+  // Non owning pointer, initialized and owned by
+  // `ChromeBrowserMainExtraPartsAsh`.
+  // `auth_token_provider_` will outlive this controller since the controller
+  // is part of `ash::Shell` and will be destroyed as part of `AshShellInit`
+  // before `auth_token_provider`.
+  base::raw_ptr<InSessionAuthTokenProvider> auth_token_provider_;
 };
 
 }  // namespace ash
