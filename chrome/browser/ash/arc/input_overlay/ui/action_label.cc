5ae04a4f89f9bda88e80873ca84b8aed0d8a563f
Cici Ruan <cuicuiruan@google.com>
2022-06-07 23:14:25 +0000

    arc/gio: Add two stacking colors on the ActionLabel background
    This improves the contrast on both dark and light background.
    
    Bug: b:232948415 b:188452744
    Test: manual test and unittest
    Change-Id: I16b265f6146740003db7cb2148f0966e6aabae4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688318
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011718}

--- chrome/browser/ash/arc/input_overlay/ui/action_label.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_label.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/highlight_path_generator.h"
@@ -30,7 +31,8 @@ constexpr char kFontStyle[] = "Google Sans";
 constexpr int kFontSize = 16;
 
 // About colors.
-constexpr SkColor kViewModeBgColor = SkColorSetA(SK_ColorGRAY, 0x99);
+constexpr SkColor kViewModeForeColor = SkColorSetA(SK_ColorBLACK, 0x29);
+constexpr SkColor kViewModeBackColor = SkColorSetA(gfx::kGoogleGrey800, 0xCC);
 constexpr SkColor kEditModeBgColor = SK_ColorWHITE;
 constexpr SkColor kEditedUnboundBgColor = gfx::kGoogleRed300;
 constexpr SkColor kViewTextColor = SK_ColorWHITE;
@@ -290,8 +292,10 @@ void ActionLabel::SetToViewMode() {
     }
   }
 
-  SetBackground(
-      views::CreateRoundedRectBackground(kViewModeBgColor, kCornerRadiusView));
+  SetBackground(views::CreateRoundedRectBackground(
+      color_utils::GetResultingPaintColor(kViewModeForeColor,
+                                          kViewModeBackColor),
+      kCornerRadiusView));
   SetPreferredSize(CalculatePreferredSize());
 }
 
