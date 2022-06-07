adf7ea8a3dcf1a391b39c306416c9630c53ece42
Xiaocheng Hu <xiaochengh@chromium.org>
2022-06-07 11:44:25 +0000

    [anchor-position] Add @position-fallback rule map at ScopedStyleResolver
    This patch introduces a name-to-rule map for @position-fallback at
    ScopedStyleResolver, so that we can perform name-to-rule queries.
    
    Currently it's just for the Document scope without proper handling for
    invalidations and cascade layers, which will be added when the prototype
    reaches a more mature stage.
    
    No test because this patch is just straightforward boilerplates without
    any web-visible changes.
    
    Bug: 1309178
    Change-Id: Ibede974fb64f140d89dc6685e5d98aecd2bd7368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690151
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011382}

--- third_party/blink/renderer/core/css/rule_set.h
+++ third_party/blink/renderer/core/css/rule_set.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/cascade_layer.h"
 #include "third_party/blink/renderer/core/css/css_keyframes_rule.h"
+#include "third_party/blink/renderer/core/css/css_position_fallback_rule.h"
 #include "third_party/blink/renderer/core/css/media_query_evaluator.h"
 #include "third_party/blink/renderer/core/css/resolver/media_query_result.h"
 #include "third_party/blink/renderer/core/css/rule_feature_set.h"
@@ -276,6 +277,10 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
       const {
     return scroll_timeline_rules_;
   }
+  const HeapVector<Member<StyleRulePositionFallback>>& PositionFallbackRules()
+      const {
+    return position_fallback_rules_;
+  }
   const HeapVector<RuleData>* SlottedPseudoElementRules() const {
     return &slotted_pseudo_element_rules_;
   }
@@ -365,6 +370,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
   void AddScrollTimelineRule(StyleRuleScrollTimeline*);
   void AddCounterStyleRule(StyleRuleCounterStyle*);
   void AddFontPaletteValuesRule(StyleRuleFontPaletteValues*);
+  void AddPositionFallbackRule(StyleRulePositionFallback*);
 
   bool MatchMediaForAddRules(const MediaQueryEvaluator& evaluator,
                              const MediaQuerySet* media_queries);
@@ -453,6 +459,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
   HeapVector<Member<StyleRuleProperty>> property_rules_;
   HeapVector<Member<StyleRuleCounterStyle>> counter_style_rules_;
   HeapVector<Member<StyleRuleScrollTimeline>> scroll_timeline_rules_;
+  HeapVector<Member<StyleRulePositionFallback>> position_fallback_rules_;
   HeapVector<MediaQuerySetResult> media_query_set_results_;
 
   // Whether there is a ruleset bucket for rules with a selector on
