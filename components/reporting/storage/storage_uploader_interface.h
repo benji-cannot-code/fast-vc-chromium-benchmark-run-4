5a1280bd607cc66bd8107020bdbc641e7083cac4
Leonid Baraz <lbaraz@chromium.org>
2022-06-07 07:42:05 +0000

    Add memory accounting to upload
    Bug: b:233089187
    Change-Id: I11fd07e53431bd43dc18ce030468c0477ce02955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690697
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011319}

--- components/reporting/storage/storage_uploader_interface.h
+++ components/reporting/storage/storage_uploader_interface.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/proto/synced/record_constants.pb.h"
+#include "components/reporting/resources/resource_interface.h"
 #include "components/reporting/util/status.h"
 #include "components/reporting/util/statusor.h"
 
@@ -63,6 +64,7 @@ class UploaderInterface {
   // the record or error status has been processed, with true if next record
   // needs to be delivered and false if the Uploader should stop.
   virtual void ProcessRecord(EncryptedRecord record,
+                             ScopedReservation scoped_reservation,
                              base::OnceCallback<void(bool)> processed_cb) = 0;
 
   // Makes a note of a gap [start, start + count). Expects |processed_cb| to
