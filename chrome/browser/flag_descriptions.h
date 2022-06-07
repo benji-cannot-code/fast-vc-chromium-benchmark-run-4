a1c90f12ffc547e7abc5d2055b15814a4ad5ba33
Mahmoud Rashad <mmrashad@google.com>
2022-06-07 15:37:14 +0000

    [sync] Add support for alternative titles on android promos
    This CL replaces the promos titles that show up when SYNC_ANDROID_PROMOS_WITH_TITLE flag is enabled with alternative titles if SYNC_ANDROID_PROMOS_WITH_ALTERNATIVE_TITLE flag is enabled.
    
    Fixed: 1330348
    Change-Id: I60a0f6f4b89d4e0af9082372f21bc845f6280853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686553
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011468}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2066,6 +2066,9 @@ extern const char kFeedPositionAndroidDescription[];
 extern const char kStrictSiteIsolationName[];
 extern const char kStrictSiteIsolationDescription[];
 
+extern const char kSyncAndroidPromosWithAlternativeTitleName[];
+extern const char kSyncAndroidPromosWithAlternativeTitleDescription[];
+
 extern const char kSyncAndroidPromosWithIllustrationName[];
 extern const char kSyncAndroidPromosWithIllustrationDescription[];
 
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -376,6 +376,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &switches::kTangibleSync,
     &syncer::kEnableSyncImmediatelyInFRE,
     &syncer::kSyncTrustedVaultPassphraseRecovery,
+    &syncer::kSyncAndroidPromosWithAlternativeTitle,
     &syncer::kSyncAndroidPromosWithIllustration,
     &syncer::kSyncAndroidPromosWithSingleButton,
     &syncer::kSyncAndroidPromosWithTitle,
