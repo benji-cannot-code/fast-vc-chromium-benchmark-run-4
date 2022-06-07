a54519597d440d34e85f08a08df3685022b73218
Dan McArdle <dmcardle@chromium.org>
2022-06-07 18:52:05 +0000

    Eliminate base::LowerCaseEqualsASCII in /components/policy
    This CL is a behavior-preserving refactor. It replaces calls to
    base::LowerCaseEqualsASCII with base::EqualsCaseInsensitiveASCII.
    
    We are deprecating base::LowerCaseEqualsASCII because its interface is a
    mild footgun, in that it requires the second parameter to be lowercase.
    
    See the design doc for more details.
    https://docs.google.com/document/d/18zWV0zN4oAqMZpond1Ut0QviHUeHCSwCo08mqThwHkQ/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=ftirelo@chromium.org
    
    Bug: 1326529
    Change-Id: Idf47dc623e7ae22b70a81276167eb28531217002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3657578
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011584}

--- components/policy/core/common/policy_loader_common.cc
+++ components/policy/core/common/policy_loader_common.cc
@@ -89,8 +89,8 @@ bool FilterSensitiveExtensionsInstallForcelist(PolicyMap::Entry* map_entry) {
       continue;
 
     // Only allow custom update urls in enterprise environments.
-    if (!base::LowerCaseEqualsASCII(entry.substr(pos + 1),
-                                    kChromeWebstoreUpdateURL)) {
+    if (!base::EqualsCaseInsensitiveASCII(entry.substr(pos + 1),
+                                          kChromeWebstoreUpdateURL)) {
       policy_list_value->GetListDeprecated()[i] =
           base::Value(kBlockedExtensionPrefix + entry);
       has_invalid_policies = true;
@@ -132,8 +132,8 @@ bool FilterSensitiveExtensionSettings(PolicyMap::Entry* map_entry) {
       continue;
     }
     std::string* update_url = entry.second.FindStringKey(kUpdateUrl);
-    if (!update_url ||
-        base::LowerCaseEqualsASCII(*update_url, kChromeWebstoreUpdateURL)) {
+    if (!update_url || base::EqualsCaseInsensitiveASCII(
+                           *update_url, kChromeWebstoreUpdateURL)) {
       continue;
     }
 
