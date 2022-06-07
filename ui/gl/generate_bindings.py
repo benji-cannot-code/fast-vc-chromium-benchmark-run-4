4d568e0c087accb0a83099c28bb748ecef3af10a
Zhenyao Mo <zmo@chromium.org>
2022-06-07 05:41:35 +0000

    Minor fix in generate_bindings.py to improve readability.
    This is to address a comment from
    https://chromium-review.googlesource.com/c/chromium/src/+/3687558
    
    TEST=run the script and nothing changes
    R=kbr@chromium.org
    
    Bug: chromium:1251724
    Change-Id: Ie0eee98ee8a6ffc768396d714cee5f647d62e149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687567
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011294}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -3364,6 +3364,9 @@ namespace gl {
              'memcmp(this_bytes, this_bytes + 1, sizeof(*this) - 1) == 0);\n');
   file.write('\n')
 
+  def BindingsAreAllStatic(api_set_name):
+    return api_set_name == 'egl'
+
   def WriteFuncBinding(file, known_as, version_name):
     file.write(
         '  fn.%sFn = reinterpret_cast<%sProc>(GetGLProcAddress("%s"));\n' %
@@ -3372,7 +3375,7 @@ namespace gl {
   for func in functions:
     if 'static_binding' in func:
       WriteFuncBinding(file, func['known_as'], func['static_binding'])
-    elif set_name == 'egl':
+    elif BindingsAreAllStatic(set_name):
       assert len(func['versions']) == 1
       version = func['versions'][0]
       WriteFuncBinding(file, func['known_as'], version['name'])
@@ -3469,8 +3472,8 @@ void Driver%s::InitializeExtensionBindings() {
   OutputExtensionSettings(
     'extensions',
     sorted(used_client_extensions),
-    '' if set_name == 'egl' else 'ext.')
-  if set_name == 'gl' or set_name == 'glx':
+    '' if BindingsAreAllStatic(set_name) else 'ext.')
+  if not BindingsAreAllStatic(set_name):
     OutputExtensionBindings(
       [ f for f in functions if IsClientExtensionFunc(f) ])
 
@@ -3488,8 +3491,8 @@ void ExtensionsEGL::InitializeExtensionSettings(GLDisplayEGL* display) {
   OutputExtensionSettings(
     'extensions',
     sorted(used_extensions),
-    '' if set_name == 'egl' else 'ext.')
-  if set_name == 'gl' or set_name == 'glx':
+    '' if BindingsAreAllStatic(set_name) else 'ext.')
+  if not BindingsAreAllStatic(set_name):
     OutputExtensionBindings(
       [ f for f in functions if not IsClientExtensionFunc(f) ])
   file.write('}\n')
