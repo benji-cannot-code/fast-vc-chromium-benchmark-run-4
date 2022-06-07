d9e2996a60b3c1d90b9f063f81208b439c237311
Chris Lu <thegreenfrog@chromium.org>
2022-06-07 14:53:52 +0000

    [ios] Update Fake Omnibox colors for Start UI Refresh
    Bug: 1327463
    Change-Id: I90db2346d3c60cdadc247c7c14b676feb0393568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691980
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011447}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/elements/extended_touch_target_button.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_header_constants.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/named_guide_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/elements/gradient_view.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #import "ui/gfx/ios/NSString+CrStringDrawing.h"
@@ -155,7 +157,9 @@ CGFloat ToolbarHeight() {
   // Omnibox, used for animations.
   // TODO(crbug.com/936811): See if it is possible to share some initialization
   // code with the real Omnibox.
-  UIColor* color = [UIColor colorNamed:kTextfieldPlaceholderColor];
+  UIColor* color = IsContentSuggestionsUIModuleRefreshEnabled()
+                       ? [UIColor colorNamed:kGrey700Color]
+                       : [UIColor colorNamed:kTextfieldPlaceholderColor];
   OmniboxContainerView* omnibox =
       [[OmniboxContainerView alloc] initWithFrame:CGRectZero
                                         textColor:color
@@ -455,8 +459,18 @@ CGFloat ToolbarHeight() {
     _fakeLocationBar.userInteractionEnabled = NO;
     _fakeLocationBar.clipsToBounds = YES;
     _fakeLocationBar.backgroundColor =
-        [UIColor colorNamed:kTextfieldBackgroundColor];
+        IsContentSuggestionsUIModuleRefreshEnabled()
+            ? [UIColor clearColor]
+            : [UIColor colorNamed:kTextfieldBackgroundColor];
     _fakeLocationBar.translatesAutoresizingMaskIntoConstraints = NO;
+    if (IsContentSuggestionsUIModuleRefreshEnabled()) {
+      GradientView* gradientView = [[GradientView alloc]
+          initWithTopColor:[UIColor colorNamed:kGrey300Color]
+               bottomColor:[UIColor colorNamed:kGrey200Color]];
+      gradientView.translatesAutoresizingMaskIntoConstraints = NO;
+      [_fakeLocationBar addSubview:gradientView];
+      AddSameConstraints(_fakeLocationBar, gradientView);
+    }
 
     _fakeLocationBarHighlightView = [[UIView alloc] init];
     _fakeLocationBarHighlightView.userInteractionEnabled = NO;
