f061ec08534c979abe016520606693ac6f746f88
Shashank Jain <jainshashank@google.com>
2022-06-07 17:25:05 +0000

    [Autofill Assistant] QR Code Scan Dialog
    - Setup "guided_browsing" component
    - Setup DialogFragment + ScanCoordinator
    - Close Button + ToolBarTitle
    - Main Scan View
    - Camera Permissions View
    - Camera w/Overlay View
    - Open Settings View
    - Camera Preview Callback
    - Permissions Utils
    - Manual Test setup to trigger Scan
    
    Bug: b/216265782
    Change-Id: I1ce10c6639b21f50f85451974417b3a8fed20f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568770
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Shashank Jain <jainshashank@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011531}

---
+++ components/autofill_assistant/guided_browsing/android/java/src/org/chromium/components/autofill_assistant/guided_browsing/qr_code/AssistantQrCodeController.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.autofill_assistant.guided_browsing.qr_code;
+
+import android.app.Activity;
+
+import org.chromium.components.autofill_assistant.guided_browsing.qr_code.camera_scan.AssistantQrCodeCameraScanDialog;
+import org.chromium.components.autofill_assistant.guided_browsing.qr_code.camera_scan.AssistantQrCodeCameraScanModel;
+import org.chromium.ui.base.WindowAndroid;
+
+/** Controller to expose QR Code Scan functionality. */
+public class AssistantQrCodeController {
+    /** Prompts the user for QR Code Scanning. */
+    public static void promptQrCodeScan(
+            Activity activity, WindowAndroid windowAndroid, AssistantQrCodeCameraScanModel model) {
+        AssistantQrCodeCameraScanDialog.newInstance(activity, windowAndroid, model)
+                .show(activity.getFragmentManager(), /* tag= */ null);
+    }
+}
---
+++ components/autofill_assistant/guided_browsing/android/java/src/org/chromium/components/autofill_assistant/guided_browsing/qr_code/AssistantQrCodeDelegate.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.autofill_assistant.guided_browsing.qr_code;
+
+/** Delegate interface for Assistant QR Code Scan actions. */
+public interface AssistantQrCodeDelegate {
+    /** Called when QR Code Scan is successfully completed. */
+    public void onScanResult(String value);
+
+    /** Called when QR Code Scan is cancelled. */
+    public void onScanCancelled();
+
+    /** Called when QR Code Scan fails because of Camera Error. */
+    public void onCameraError();
+}
