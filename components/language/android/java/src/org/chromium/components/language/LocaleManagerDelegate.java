50d5c86bd203f3e7f4e907344a9bdd701d21312a
Trevor Perrier <perrier@chromium.org>
2022-06-07 02:16:23 +0000

    [LocaleManager] Add initial LocaleManagerDelegate
    This CL add the initial public implementation of LocaleManagerDelegate.
    
    A downstream CL will add Android T specific implementation.
    
    Bug: 1323955
    Change-Id: I2ef21a99dbaf706ffb928ec08f3d966bff2bc389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687356
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011233}

---
+++ components/language/android/java/src/org/chromium/components/language/LocaleManagerDelegate.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.language;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Interface for {@link LocaleManager} APIs.
+ */
+public interface LocaleManagerDelegate {
+    /**
+     * Gets the currently set system App locale. Returns null if no override
+     * locale is set.
+     * @return List of system App locales.
+     */
+    public Locale getApplicationLocale();
+
+    /**
+     * Sets the system App locale.
+     * @param Language tag to use as for the App UI at the system level.
+     */
+    public void setApplicationLocale(String languageName);
+
+    /**
+     * @return The current system locales.
+     */
+    public List<Locale> getSystemLocales();
+}
