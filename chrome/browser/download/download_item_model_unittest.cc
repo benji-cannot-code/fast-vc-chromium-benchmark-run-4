b7fd8c686c230bfffd502126c00447c2e663b1dc
Xinghui Lu <xinghuilu@chromium.org>
2022-06-07 03:42:20 +0000

    [DownloadBubble]Update deep scanning UI for enterprise connector
    Replace "Scanning" with "Checking with your organization's security
    policies..." when scanning by enterprise connector. Also remove the
    "Open now" button since it shouldn't be bypassable.
    
    Update the string when the scan is completed (for both consumer and
    enterprise scan) to match the UX deck.
    
    Bug: 1322176
    Change-Id: Ib0104df9bb8b802633a6d0c5d39ede51b50fa59f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689890
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011260}

--- chrome/browser/download/download_item_model_unittest.cc
+++ chrome/browser/download/download_item_model_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/simple_test_clock.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/mock_download_item.h"
 #include "components/enterprise/common/download_item_reroute_info.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -612,6 +613,11 @@ TEST_F(DownloadItemModelTest, CompletedStatus) {
     EXPECT_EQ(base::UTF16ToUTF8(model().GetStatusText()),
               test_case.expected_bubble_status_msg);
   }
+
+  EXPECT_CALL(item(), GetDangerType())
+      .WillRepeatedly(Return(download::DOWNLOAD_DANGER_TYPE_DEEP_SCANNED_SAFE));
+  EXPECT_EQ("2 B \xE2\x80\xA2 Done, no issues found",
+            base::UTF16ToUTF8(model().GetStatusText()));
 
 #if BUILDFLAG(IS_MAC)
   EXPECT_EQ("Show in Finder", base::UTF16ToUTF8(model().GetShowInFolderText()));
