e2d51355ee02f31bee072012e67b2dfee64203d3
Sylvie Liu <sylvieliu@chromium.org>
2022-06-07 21:26:23 +0000

    Added the loading indicator for the CalendarView
    - When there is an on-screen month that hasn't finished fetching events,
    there's a loading bar underneath the calendar month header.
    - When all the on-screen months finished fetching, the loading bar
    disappears.
    - When a month is fetching events, the tooltip of each date cell will
    show "Month Day, Year, Loading events.".
    - When a month is finished fetching, the tooltip of each date cell will
    show the date and events number.
    
    We added the screenshot for the loading tooltip of date cells for
    translation, and skipped the screenshot for the accessibility name for
    the loading bar since it cannot be focused.
    
    Bug: 1298357
    Skip-Translation-Screenshots-Check: True
    Change-Id: Ic5717eae16128bf1061832ffb754c1e833213adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642877
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011670}

--- ash/system/tray/tray_detailed_view.h
+++ ash/system/tray/tray_detailed_view.h
@@ -48,6 +48,9 @@ class ASH_EXPORT TrayDetailedView : public views::View,
   // Don't override this --- override HandleViewClicked.
   void OnViewClicked(views::View* sender) final;
 
+  // Setter for `progress_bar_` accessibility label.
+  void OverrideProgressBarAccessibleName(const std::u16string& name);
+
  protected:
   // views::View:
   void Layout() override;
@@ -127,6 +130,11 @@ class ASH_EXPORT TrayDetailedView : public views::View,
   views::ScrollView* scroller() const { return scroller_; }
   views::View* scroll_content() const { return scroll_content_; }
 
+  // Gets called in the constructor of the `CalendarView`, or any other views in
+  // the future that don't have a separator to modify the value of
+  // `has_separator` to false.
+  void IgnoreSeparator();
+
  private:
   friend class TrayDetailedViewTest;
 
@@ -156,6 +164,13 @@ class ASH_EXPORT TrayDetailedView : public views::View,
   views::Label* sub_header_label_ = nullptr;
   views::ImageView* sub_header_image_view_ = nullptr;
   const gfx::VectorIcon* sub_header_icon_ = nullptr;
+
+  // Gets modified to false in the constructor of the view if it doesn't have a
+  // separator.
+  bool has_separator_ = true;
+
+  // The accessible name for the `progress_bar_`.
+  absl::optional<std::u16string> progress_bar_accessible_name_;
 };
 
 }  // namespace ash
