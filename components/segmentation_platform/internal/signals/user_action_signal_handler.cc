c7a663c7261235c8d03100ac70240cdc991636c9
Shakti Sahu <shaktisahu@chromium.org>
2022-06-07 10:28:35 +0000

    Contextual Page Actions: Added page load observer
    This CL adds page load observer (tab helper) for observing page load
    events and notifying segmentation platform which would then do further
    processing and run model for relevant clients interested in page
    load events.
    
    Bug: 1322001
    Change-Id: Ie595f0d63d92af846ede208c651fe9313bc69cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671456
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011363}

--- components/segmentation_platform/internal/signals/user_action_signal_handler.cc
+++ components/segmentation_platform/internal/signals/user_action_signal_handler.cc
@@ -20,12 +20,12 @@ UserActionSignalHandler::UserActionSignalHandler(
 }
 
 UserActionSignalHandler::~UserActionSignalHandler() {
-  if (metrics_enabled_)
+  if (metrics_enabled_ && base::GetRecordActionTaskRunner())
     base::RemoveActionCallback(action_callback_);
 }
 
 void UserActionSignalHandler::EnableMetrics(bool enable_metrics) {
-  if (metrics_enabled_ == enable_metrics)
+  if (metrics_enabled_ == enable_metrics || !base::GetRecordActionTaskRunner())
     return;
 
   metrics_enabled_ = enable_metrics;
