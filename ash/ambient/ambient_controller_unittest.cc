0292c1e6156ed71ff87105f49b7f57a9579849a9
Eric Sum <esum@google.com>
2022-06-07 09:22:58 +0000

    Add metric for ambient mode startup time.
    This it to track the amount of time to download/decode the initial
    set of photos from IMAX, load the animation, etc. There are also
    some past reports of the screensaver not starting at all. This should
    capture that if it happens in the field.
    
    Bug: b:233842172
    Change-Id: I1fe83aac9b6e5d34214d9a019a59ca74f269bfda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688044
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011343}

--- ash/ambient/ambient_controller_unittest.cc
+++ ash/ambient/ambient_controller_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/ambient/ambient_prefs.h"
 #include "ash/public/cpp/ambient/ambient_ui_model.h"
+#include "ash/public/cpp/ambient/fake_ambient_backend_controller_impl.h"
 #include "ash/public/cpp/assistant/controller/assistant_interaction_controller.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
@@ -1221,6 +1222,44 @@ TEST_P(AmbientControllerTestForAnyTheme, MetricsEngagementTime) {
   histogram_tester.ExpectTimeBucketCount(
       base::StrCat({"Ash.AmbientMode.EngagementTime.", ToString(GetParam())}),
       base::Minutes(1), 2);
+}
+
+TEST_P(AmbientControllerTestForAnyTheme, MetricsStartupTime) {
+  base::HistogramTester histogram_tester;
+  LockScreen();
+  FastForwardToLockScreenTimeout();
+  FastForwardTiny();
+  ASSERT_TRUE(ambient_controller()->IsShown());
+
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({"Ash.AmbientMode.StartupTime.", ToString(GetParam())}), 1);
+
+  UnlockScreen();
+  ASSERT_FALSE(ambient_controller()->IsShown());
+
+  LockScreen();
+  FastForwardToLockScreenTimeout();
+  FastForwardTiny();
+  ASSERT_TRUE(ambient_controller()->IsShown());
+
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({"Ash.AmbientMode.StartupTime.", ToString(GetParam())}), 2);
+}
+
+TEST_P(AmbientControllerTestForAnyTheme, MetricsStartupTimeFailedToStart) {
+  // Simulate IMAX outage that doesn't return any photos.
+  backend_controller()->SetFetchScreenUpdateInfoResponseSize(0);
+
+  base::HistogramTester histogram_tester;
+  LockScreen();
+  FastForwardToLockScreenTimeout();
+  task_environment()->FastForwardBy(base::Minutes(1));
+  ASSERT_TRUE(GetContainerViews().empty());
+
+  UnlockScreen();
+  histogram_tester.ExpectUniqueTimeSample(
+      base::StrCat({"Ash.AmbientMode.StartupTime.", ToString(GetParam())}),
+      base::Minutes(1), 1);
 }
 
 }  // namespace ash
