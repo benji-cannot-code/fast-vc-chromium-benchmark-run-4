1f6448e6438973c6219b91b391020bfae290513c
Yue Li <updowndota@google.com>
2022-06-07 21:24:14 +0000

    Quick Answers: Increase confidence threshold for language detection
    Language detection model returns high confidence score that identifies
    random combo of letters and numbers as foreign language. Increase the
    confidence threshold to improve the accuracy.
    
    Bug: b/229288688
    Test: Manual test
    Change-Id: I78bd6e54d4c4eb44e50d921fc792fc29854d3fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687323
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011666}

--- chromeos/components/quick_answers/utils/language_detector.cc
+++ chromeos/components/quick_answers/utils/language_detector.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace quick_answers {
 namespace {
 
-constexpr double kSelectedTextConfidenceThreshold = 0.8;
+constexpr double kSelectedTextConfidenceThreshold = 0.9;
 
 constexpr double kSurroundingTextConfidenceThreshold = 0.9;
 
