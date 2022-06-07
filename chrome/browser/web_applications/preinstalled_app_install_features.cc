19eb451a9ffe8f342fa2da6fc88eadb653ef69d5
Jeevan Shikaram <jshikaram@chromium.org>
2022-06-07 04:18:29 +0000

    [Default Apps] Allow app pre-installs to target Cloud Gaming devices.
    This CL allows app pre-install configs to specify that the app should
    only be installed on cloud gaming devices.
    
    Bug: 1317499
    Change-Id: I34a2a6aaef7541da9ce25d3a0a329122f983e428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3658267
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011275}

--- chrome/browser/web_applications/preinstalled_app_install_features.cc
+++ chrome/browser/web_applications/preinstalled_app_install_features.cc
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/constants/chromeos_features.h"
+#endif  // IS_CHROMEOS
+
 namespace web_app {
 
 // A hard coded list of features available for externally installed apps to
@@ -22,6 +26,7 @@ constexpr const base::Feature* kPreinstalledAppInstallFeatures[] = {
 #if BUILDFLAG(IS_CHROMEOS)
     &kCursiveStylusPreinstall,
     &kMessagesPreinstall,
+    &::chromeos::features::kCloudGamingDevice,
 #endif
 };
 
