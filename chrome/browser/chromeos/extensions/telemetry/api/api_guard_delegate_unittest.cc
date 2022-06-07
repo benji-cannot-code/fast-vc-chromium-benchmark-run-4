a2fc2791e7a17ed322eea635e7e51e21539a8e23
Mahmoud Gawad <mgawad@google.com>
2022-06-07 10:00:34 +0000

    Reland "[Telemetry] Enforce secure conn in ApiGuard"
    This is a reland of commit 4bcbd6c7b69fd5e46bed40a7c7d791b0f03bd1b3
    
    Original change's description:
    > [Telemetry] Enforce secure conn in ApiGuard
    >
    > Enforce that the PWA connection is secure (e.g. no cert errors) before
    > accessing the telemetry APIs.
    >
    > Bug: b:229774016
    > Test: browser_tests --gtest_fitler="*TelemetryExtension*"
    > Test: unit_tests --gtest_filter="*ApiGuardDelegate*"
    > Test: tast run <IP> telemetryextension.APIAvailableRoutines.*
    >
    > Change-Id: I333e50387467a875f4364a060dd27579dfb5a516
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666977
    > Reviewed-by: Oleh Lamzin <lamzin@google.com>
    > Commit-Queue: Mahmoud Aly <mgawad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1009588}
    
    Bug: b:229774016
    Change-Id: I828e2fa4fe18d0fc64a7e18d6b4e777868fdd968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688972
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011354}

--- chrome/browser/chromeos/extensions/telemetry/api/api_guard_delegate_unittest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/api_guard_delegate_unittest.cc
@@ -17,9 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/ssl_status.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/extension_urls.h"
+#include "net/base/net_errors.h"
+#include "net/cert/cert_status_flags.h"
+#include "net/cert/x509_certificate.h"
+#include "net/test/cert_test_util.h"
+#include "net/test/test_data_directory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
@@ -42,7 +49,7 @@ struct ExtensionInfoTestParams {
 const std::vector<ExtensionInfoTestParams> kAllExtensionInfoTestParams{
     ExtensionInfoTestParams(
         /*extension_id=*/"gogonhoemckpdpadfnjnpgbjpbjnodgc",
-        /*pwa_page_url=*/"http://www.google.com",
+        /*pwa_page_url=*/"https://www.google.com",
         /*matches_origin=*/"*://www.google.com/*"),
     ExtensionInfoTestParams(
         /*extension_id=*/"alnedpmllcfpgldkagbfbjkloonjlfjb",
@@ -121,6 +128,24 @@ class ApiGuardDelegateTest
         hardware_info_delegate_factory_.get());
   }
 
+  void OpenPwaUrlAndSetCertificateWithStatus(net::CertStatus cert_status) {
+    const base::FilePath certs_dir = net::GetTestCertsDirectory();
+    scoped_refptr<net::X509Certificate> test_cert(
+        net::ImportCertFromFile(certs_dir, "ok_cert.pem"));
+    ASSERT_TRUE(test_cert);
+
+    // Open the PWA page url and set valid certificate to bypass the
+    // IsPwaUiOpenAndSecure() check.
+    AddTab(browser(), GURL(pwa_page_url()));
+
+    // AddTab() adds a new tab at index 0.
+    auto* web_contents = browser()->tab_strip_model()->GetWebContentsAt(0);
+    auto* entry = web_contents->GetController().GetVisibleEntry();
+    content::SSLStatus& ssl = entry->GetSSL();
+    ssl.certificate = test_cert;
+    ssl.cert_status = cert_status;
+  }
+
  private:
   void CreateExtension() {
     extension_ =
@@ -167,12 +192,24 @@ TEST_P(ApiGuardDelegateTest, PwaNotOpen) {
                                    future.GetCallback());
 
   ASSERT_TRUE(future.Wait());
-  EXPECT_EQ("Companion PWA UI is not open", future.Get());
+  EXPECT_EQ("Companion PWA UI is not open or not secure", future.Get());
+}
+
+TEST_P(ApiGuardDelegateTest, PwaIsOpenButNotSecure) {
+  OpenPwaUrlAndSetCertificateWithStatus(
+      /*cert_status=*/net::CERT_STATUS_INVALID);
+
+  auto api_guard_delegate = ApiGuardDelegate::Factory::Create();
+  base::test::TestFuture<std::string> future;
+  api_guard_delegate->CanAccessApi(profile(), extension(),
+                                   future.GetCallback());
+
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ("Companion PWA UI is not open or not secure", future.Get());
 }
 
 TEST_P(ApiGuardDelegateTest, ManufacturerNotAllowed) {
-  // Open the PWA page url to bypass IsPwaUiOpen() check.
-  AddTab(browser(), GURL(pwa_page_url()));
+  OpenPwaUrlAndSetCertificateWithStatus(/*cert_status=*/net::OK);
 
   // Make sure device manufacturer is not allowed.
   SetDeviceManufacturer("GOOGLE");
@@ -188,8 +225,7 @@ TEST_P(ApiGuardDelegateTest, ManufacturerNotAllowed) {
 }
 
 TEST_P(ApiGuardDelegateTest, NoError) {
-  // Open the PWA page url to bypass IsPwaUiOpen() check.
-  AddTab(browser(), GURL(pwa_page_url()));
+  OpenPwaUrlAndSetCertificateWithStatus(/*cert_status=*/net::OK);
 
   auto api_guard_delegate = ApiGuardDelegate::Factory::Create();
   base::test::TestFuture<std::string> future;
@@ -247,7 +283,30 @@ TEST_P(ApiGuardDelegateAffiliatedUserTest, PwaNotOpen) {
                                    future.GetCallback());
 
   ASSERT_TRUE(future.Wait());
-  EXPECT_EQ("Companion PWA UI is not open", future.Get());
+  EXPECT_EQ("Companion PWA UI is not open or not secure", future.Get());
+}
+
+TEST_P(ApiGuardDelegateAffiliatedUserTest, PwaIsOpenButNotSecure) {
+  {
+    extensions::ExtensionManagementPrefUpdater<
+        sync_preferences::TestingPrefServiceSyncable>
+        updater(profile()->GetTestingPrefService());
+    // Make sure the extension is marked as force-installed.
+    updater.SetIndividualExtensionAutoInstalled(
+        extension_id(), extension_urls::kChromeWebstoreUpdateURL,
+        /*forced=*/true);
+  }
+
+  OpenPwaUrlAndSetCertificateWithStatus(
+      /*cert_status=*/net::CERT_STATUS_INVALID);
+
+  auto api_guard_delegate = ApiGuardDelegate::Factory::Create();
+  base::test::TestFuture<std::string> future;
+  api_guard_delegate->CanAccessApi(profile(), extension(),
+                                   future.GetCallback());
+
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ("Companion PWA UI is not open or not secure", future.Get());
 }
 
 TEST_P(ApiGuardDelegateAffiliatedUserTest, ManufacturerNotAllowed) {
@@ -261,8 +320,7 @@ TEST_P(ApiGuardDelegateAffiliatedUserTest, ManufacturerNotAllowed) {
         /*forced=*/true);
   }
 
-  // Open the PWA page url to bypass IsPwaUiOpen() check.
-  AddTab(browser(), GURL(pwa_page_url()));
+  OpenPwaUrlAndSetCertificateWithStatus(/*cert_status=*/net::OK);
 
   // Make sure device manufacturer is not allowed.
   SetDeviceManufacturer("GOOGLE");
@@ -288,8 +346,7 @@ TEST_P(ApiGuardDelegateAffiliatedUserTest, NoError) {
         /*forced=*/true);
   }
 
-  // Open the PWA page url to bypass IsPwaUiOpen() check.
-  AddTab(browser(), GURL(pwa_page_url()));
+  OpenPwaUrlAndSetCertificateWithStatus(/*cert_status=*/net::OK);
 
   auto api_guard_delegate = ApiGuardDelegate::Factory::Create();
   base::test::TestFuture<std::string> future;
