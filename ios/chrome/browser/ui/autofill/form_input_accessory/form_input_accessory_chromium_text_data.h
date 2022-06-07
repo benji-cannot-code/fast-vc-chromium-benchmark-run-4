f724415c01ef8b6f7296651ce6f1aa814f909cfd
Gauthier Ambard <gambard@chromium.org>
2022-06-07 14:51:39 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/ui/autofill/form_input_accessory
    Please review the CL to ensure that nothing is broken.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1311955
    Change-Id: I06332da8f268ce698c855e8de5f58fd4c8e1a4c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3693144
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011446}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_chromium_text_data.h
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_chromium_text_data.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @class FormInputAccessoryViewTextData;
 
-// A version of |FormInputAccessoryViewTextData| with all the fields filled in
+// A version of `FormInputAccessoryViewTextData` with all the fields filled in
 // with Chromium data.
 FormInputAccessoryViewTextData* ChromiumAccessoryViewTextData();
 
