2ecd7029c8c7292cbf1870098ab50a446485bb9b
Florian Leimgruber <fleimgruber@google.com>
2022-06-07 09:52:22 +0000

    Support for empty labels in phone number heuristics.
    This CL adds support for phone number inputs of 2 or 3 consecutive
    input fields, where only the first field has a label.
    They are classified as
    <country code> <city and number> and
    <country code> <area code> <number>, respectively.
    
    Guarded behind AutofillEnableParsingEmptyPhoneNumberLabels.
    
    Bug: 1311937
    Change-Id: I88b7033863481a3071134b4e35286c18f0c6c0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663061
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011351}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -88,6 +88,8 @@ COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<base::TimeDelta>
     kAutofillMultiStepImportCandidateTTL;
 COMPONENT_EXPORT(AUTOFILL)
+extern const base::Feature kAutofillEnableParsingEmptyPhoneNumberLabels;
+COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillEnableRankingFormula;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<int> kAutofillRankingFormulaUsageHalfLife;
