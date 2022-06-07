a2154114858e8e68fe0dbc2c3a76dc880f353c66
Noel Gordon <noel@chromium.org>
2022-06-07 04:37:54 +0000

    filesapp: exclude unindexed volumes for Android MediaStore filter
    If the 'media-store-files-only' filter is enabled for Android ARC file
    picker, do not add Drive, Crostini or GuestOS volumes to the directory
    tree, since their volume content is not indexed by the MediaStore.
    
    Bug: 1333385
    Change-Id: I071e2eac6569d3700d75d81f8efa2055dce8e9af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691247
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011281}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -1312,15 +1312,18 @@ export class FileManager extends EventTarget {
    * @private
    */
   async initDirectoryTree_() {
-    const directoryTree = /** @type {DirectoryTree} */
-        (this.dialogDom_.querySelector('#directory-tree'));
+    this.navigationUma_ = new NavigationUma(assert(this.volumeManager_));
+
     const fakeEntriesVisible =
         this.dialogType !== DialogType.SELECT_SAVEAS_FILE;
-    this.navigationUma_ = new NavigationUma(assert(this.volumeManager_));
+
+    const directoryTree = /** @type {DirectoryTree} */
+        (this.dialogDom_.querySelector('#directory-tree'));
     DirectoryTree.decorate(
         directoryTree, assert(this.directoryModel_),
         assert(this.volumeManager_), assert(this.metadataModel_),
         assert(this.fileOperationManager_), fakeEntriesVisible);
+
     directoryTree.dataModel = new NavigationListModel(
         assert(this.volumeManager_), assert(this.folderShortcutsModel_),
         fakeEntriesVisible &&
@@ -1332,6 +1335,20 @@ export class FileManager extends EventTarget {
         assert(this.directoryModel_), assert(this.androidAppListModel_));
 
     this.ui_.initDirectoryTree(directoryTree);
+
+    // If 'media-store-files-only' volume filter is enabled, then Android ARC
+    // SelectFile opened files app to pick files from volumes that are indexed
+    // by the Android MediaStore. Never add Drive, Crostini, GuestOS, to the
+    // directory tree in that case: their volume content is not indexed by the
+    // Android MediaStore, and being indexed there is needed for this Android
+    // ARC SelectFile MediaStore filter mode to work: crbug.com/1333385
+    if (this.volumeManager_.getMediaStoreFilesOnlyFilterEnabled()) {
+      return;
+    }
+
+    // Drive add/removes itself from directory tree in onPreferencesChanged_.
+    // Setup a prefs change listener then call onPreferencesChanged_() to add
+    // Drive to the directory tree if Drive is enabled by prefs.
     chrome.fileManagerPrivate.onPreferencesChanged.addListener(() => {
       this.onPreferencesChanged_();
     });
@@ -1361,8 +1378,8 @@ export class FileManager extends EventTarget {
   }
 
   /**
-   * Listens for the enable/disable events in order to show/hide
-   * the 'Linux files' root.
+   * Listens for the enable and disable events in order to add or remove the
+   * directory tree 'Linux files' root item.
    *
    * @param {chrome.fileManagerPrivate.CrostiniEvent} event
    * @return {!Promise<void>}
@@ -1728,9 +1745,9 @@ export class FileManager extends EventTarget {
   }
 
   /**
-   * Refreshes Drive prefs when they change. If Drive has been enabled or
-   * disabled, add or remove, respectively, the fake Drive item, creating it if
-   * necessary.
+   * Add or remove fake Drive item from the directory tree when Drive prefs
+   * change. If Drive has been enabled by prefs, add the fake Drive item.
+   * Remove the fake Drive item if Drive has been disabled.
    */
   onPreferencesChanged_() {
     chrome.fileManagerPrivate.getPreferences(
