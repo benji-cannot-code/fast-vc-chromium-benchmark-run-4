1f8eedc3ef10f7d0f19de45e307eabbca090bf50
Angus L. M. McLean IV <angusmclean@chromium.org>
2022-06-07 23:13:39 +0000

    personalization: Show pending state for Daily Google Photos
    This change is a much tighter scoped replacement for the big refactor
    I had going last week. One of the issues I was attempting to fix was
    actually fixed when I wasn't looking, and was actually coming from
    proto -> JSON serialization. So instead, this CL just uses existing
    UI events to show the pending animation while waiting for the Wallpaper
    to be set.
    
    This issue here is that the attribution info for Google Photos, unlike
    Backdrop, currently requires an async call. So to avoid a messy
    experience where the previous wallpaper shows for a moment after the
    pending UI, I've opted to wait until the observer sees the updated info
    to fire the event, and specifically only do this for Daily Google
    Photos.
    
    This is a bit of a bandage fix, but has the benefit of being extremely
    tightly scoped, since it's restricted by WallpaperType and only
    modifies two files, which is nice this close to branch.
    
    If desired, I could create separate actions for this instead, but they would ultimately do the same thing as the existing action. Renaming
    the `*UpdateDailyRefreshImageAction` actions to make the name fit both
    use cases is also an option.
    
    There are a few options to make this more sustainable and cleaner,
    like changing when the pending UI is shown for all wallpapers to be
    like this, which honestly makes more sense anyway, but I think that's
    best saved for after branch.
    
    Bug: b:231339356
    Change-Id: I1f5d54ae3c73f8dadcd0cfcf23784858adf72f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692080
    Commit-Queue: Angus McLean <angusmclean@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011717}

--- ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_observer.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_observer.ts
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CurrentWallpaper, WallpaperObserverInterface, WallpaperObserverReceiver, WallpaperProviderInterface} from '../personalization_app.mojom-webui.js';
+import {CurrentWallpaper, WallpaperObserverInterface, WallpaperObserverReceiver, WallpaperProviderInterface, WallpaperType} from '../personalization_app.mojom-webui.js';
 import {PersonalizationStore} from '../personalization_store.js';
 
-import {setSelectedImageAction} from './wallpaper_actions.js';
+import {setSelectedImageAction, setUpdatedDailyRefreshImageAction} from './wallpaper_actions.js';
 import {getDailyRefreshState} from './wallpaper_controller.js';
 import {getWallpaperProvider} from './wallpaper_interface_provider.js';
 
@@ -62,6 +62,10 @@ export class WallpaperObserver implements WallpaperObserverInterface {
       initialLoadTimeout = null;
     }
     store.dispatch(setSelectedImageAction(currentWallpaper));
+    if (currentWallpaper.type == WallpaperType.kDailyGooglePhotos ||
+        currentWallpaper.type == WallpaperType.kDefault) {
+      store.dispatch(setUpdatedDailyRefreshImageAction());
+    }
     // Daily Refresh state should also get updated when wallpaper changes.
     getDailyRefreshState(getWallpaperProvider(), store);
   }
