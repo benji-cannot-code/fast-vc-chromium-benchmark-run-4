42c642d772b6fb722585f29d986bee2bc21fd281
Luciano Pacheco <lucmult@chromium.org>
2022-06-07 00:34:56 +0000

    Files app: Add Store in TS
    Add the "lib/" folder to host libraries/code used by Files app.
    
    Add the BaseStore implementation to "lib/".
    
    Add the "state/" folder to host the Files app's state management. This
    includes: the Store; Action and all Reducers.
    
    Add the Store initialization behind the flag "FilesAppExperimental".
    
    Remove the placeholder "hello.ts".
    
    Bug: b:228112522
    Change-Id: I4965fc0377026efb8feb1fbb74bab0537f51f645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650690
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011220}

---
+++ ui/file_manager/file_manager/lib/README.md
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Libraries used by the Files app
+
+Any file in this folder cannot depend or import in any file in the rest of the Files app.
+
+
+The libraries written in this folder are used by the Files app.
--- ui/file_manager/file_manager/state/state.ts
+++ ui/file_manager/file_manager/state/state.ts
@@ -3,12 +3,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Placeholder function to test the build of TS in Files app.
- *
- * @param name any name or text to be greeted.
- * @returns a greeting string
- */
-export function hello(name: string): string {
-  return `Hello ${name}`;
-}
+/** TODO(lucmult): Document this. */
+export interface State {}
