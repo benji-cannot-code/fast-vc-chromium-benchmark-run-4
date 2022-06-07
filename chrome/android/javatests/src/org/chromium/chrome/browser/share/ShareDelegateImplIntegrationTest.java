0700e5a3095ec7f07ffb465497e0f1c75de86bee
Sophey <sophey@chromium.org>
2022-06-07 17:15:50 +0000

    [SharingHub] Pipe Profile supplier through to ShareDelegateImpl.
    This will allow the SharingHub to always get the current Profile at the
    beginning of each share. Reduction of getLastUsedProfile() has been done
    in https://crrev.com/c/3688237, so most of the SharingHub is using this
    new current Profile.
    
    Fixed: 1332915
    Change-Id: If5852bd1e1f2e13dcf342348e6b67a28a4f9e263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692335
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011519}

--- chrome/android/javatests/src/org/chromium/chrome/browser/share/ShareDelegateImplIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/share/ShareDelegateImplIntegrationTest.java
@@ -16,12 +16,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.Callback;
+import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.MetricsUtils.HistogramDelta;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.share.ShareDelegateImpl.ShareSheetDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -122,8 +124,8 @@ public class ShareDelegateImplIntegrationTest {
                         BottomSheetController controller,
                         ActivityLifecycleDispatcher lifecycleDispatcher, Supplier<Tab> tabProvider,
                         Supplier<TabModelSelector> tabModelSelectorProvider,
-                        Callback<Tab> printCallback, int shareOrigin, long shareStartTime,
-                        boolean sharingHubEnabled) {
+                        Supplier<Profile> profileSupplier, Callback<Tab> printCallback,
+                        int shareOrigin, long shareStartTime, boolean sharingHubEnabled) {
                     paramsRef.set(params);
                     helper.notifyCalled();
                 }
@@ -134,7 +136,8 @@ public class ShareDelegateImplIntegrationTest {
                                           .getBottomSheetController(),
                     mActivityTestRule.getActivity().getLifecycleDispatcher(),
                     mActivityTestRule.getActivity().getActivityTabProvider(),
-                    mActivityTestRule.getActivity().getTabModelSelectorSupplier(), delegate, false)
+                    mActivityTestRule.getActivity().getTabModelSelectorSupplier(),
+                    new ObservableSupplierImpl<>(), delegate, false)
                     .share(mActivityTestRule.getActivity().getActivityTab(), false,
                             /*shareOrigin=*/0);
         });
