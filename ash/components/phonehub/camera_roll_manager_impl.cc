bd5f5cbcac1b9e079f5ad23179e085e60fe044f3
Jason Sun <jasonsun@google.com>
2022-06-07 21:08:15 +0000

    [Camera Roll] Fix weak ptr invalidation
    Weak ptrs used for file transfer callbacks were being accidentally
    invalidated when pending thumbnail decoder requests are canceled. This
    led to an issue where the file download progress would stop updating
    in Tote after receiving a PhoneStatusUpdate.
    
    Use a dedicated WeakPtrFactory for thumbnail requests so that they can
    be invalidated without affecting other callbacks.
    
    TEST=download a photo from Recent Photos and delete the photo from the phone before the download completes on the Chromebook. Verify that the in-progress download is removed from Tote.
    
    Bug: b/233218310
    Change-Id: Ie2cb12b3e0790e1e0ba5242179e31faa6a96b8e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3684309
    Commit-Queue: Jason Sun <jasonsun@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011657}

--- ash/components/phonehub/camera_roll_manager_impl.cc
+++ ash/components/phonehub/camera_roll_manager_impl.cc
@@ -175,7 +175,7 @@ void CameraRollManagerImpl::OnFetchCameraRollItemsResponseReceived(
   thumbnail_decoder_->BatchDecode(
       response, current_items(),
       base::BindOnce(&CameraRollManagerImpl::OnItemThumbnailsDecoded,
-                     weak_ptr_factory_.GetWeakPtr()));
+                     thumbnail_decoder_weak_ptr_factory_.GetWeakPtr()));
 }
 
 void CameraRollManagerImpl::SendFetchCameraRollItemsRequest() {
@@ -215,7 +215,7 @@ void CameraRollManagerImpl::OnItemThumbnailsDecoded(
 }
 
 void CameraRollManagerImpl::CancelPendingThumbnailRequests() {
-  weak_ptr_factory_.InvalidateWeakPtrs();
+  thumbnail_decoder_weak_ptr_factory_.InvalidateWeakPtrs();
 }
 
 bool CameraRollManagerImpl::IsCameraRollSettingEnabled() {
