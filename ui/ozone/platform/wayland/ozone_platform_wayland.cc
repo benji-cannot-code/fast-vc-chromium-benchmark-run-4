0820fc7fb31cdbfbc35cc5ef2826ce6fcb1c1744
Sean O'Brien <seobrien@google.com>
2022-06-07 23:09:10 +0000

    [lacros] Add WaylandInputController to support haptics
    The interface for haptic touchpads uses InputController.  This CL
    replaces lacros's StubInputController with WaylandInputController,
    and supports triggering haptic touchpad effects.
    
    Bug: b:205702807
    
    Change-Id: I861ec8d89bec594a8a4f0fd484e0554ea9f57dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371263
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011709}

--- ui/ozone/platform/wayland/ozone_platform_wayland.cc
+++ ui/ozone/platform/wayland/ozone_platform_wayland.cc
@@ -39,13 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/wayland_buffer_manager_host.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/wayland_exchange_data_provider.h"
+#include "ui/ozone/platform/wayland/host/wayland_input_controller.h"
 #include "ui/ozone/platform/wayland/host/wayland_input_method_context_factory.h"
 #include "ui/ozone/platform/wayland/host/wayland_menu_utils.h"
 #include "ui/ozone/platform/wayland/host/wayland_output_manager.h"
 #include "ui/ozone/platform/wayland/host/wayland_window.h"
 #include "ui/ozone/platform/wayland/wayland_utils.h"
 #include "ui/ozone/public/gpu_platform_support_host.h"
-#include "ui/ozone/public/input_controller.h"
 #include "ui/ozone/public/ozone_platform.h"
 #include "ui/ozone/public/platform_menu_utils.h"
 #include "ui/ozone/public/system_input_injector.h"
@@ -235,7 +235,7 @@ class OzonePlatformWayland : public OzonePlatform,
 #else
     cursor_factory_ = std::make_unique<WaylandCursorFactory>(connection_.get());
 #endif
-    input_controller_ = CreateStubInputController();
+    input_controller_ = CreateWaylandInputController(connection_.get());
     gpu_platform_support_host_.reset(CreateStubGpuPlatformSupportHost());
 
     supported_buffer_formats_ =
