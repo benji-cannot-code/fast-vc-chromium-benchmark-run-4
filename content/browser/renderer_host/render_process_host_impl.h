1ffb38c3b7c3ef0afc1f1d758518c0149f67ff51
Dave Tapuska <dtapuska@chromium.org>
2022-06-07 18:55:01 +0000

    [code cache] Delete per-process CodeCacheHost interface
    Resurrect https://crrev.com/c/3069266. That CL wasn't landed because
    of issues with the code cache migration, and the original author no
    longer is working on Chromium. Those changes were successfully
    landed in https://crrev.com/2bf63c3a. So this is a rebase of the origin
    cleanup proposal.
    
    Earlier when we were in the process of migrating code caches to use
    context specific interfaces we used to fallback on using per-process
    interface for paths that weren't migrated yet. Now that the migration
    is complete we don't need to use per-process interface anymore \o/.
    
    Bug: chromium:1213818
    Change-Id: If19595095ae6f8b875ced402d385529c3c74fc77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688418
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011589}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/public/cpp/buckets/bucket_info.h"
 #include "components/services/storage/public/cpp/quota_error_or.h"
 #include "content/browser/child_process_launcher.h"
-#include "content/browser/renderer_host/code_cache_host_impl.h"
 #include "content/browser/renderer_host/media/aec_dump_manager_impl.h"
 #include "content/browser/renderer_host/render_process_host_internal_observer.h"
 #include "content/browser/storage_partition_impl.h"
@@ -788,8 +787,6 @@ class CONTENT_EXPORT RenderProcessHostImpl
       mojo::PendingReceiver<viz::mojom::CompositingModeReporter> receiver);
   void CreateDomStorageProvider(
       mojo::PendingReceiver<blink::mojom::DomStorageProvider> receiver);
-  void CreateCodeCacheHost(
-      mojo::PendingReceiver<blink::mojom::CodeCacheHost> receiver);
   void CreateRendererHost(
       mojo::PendingAssociatedReceiver<mojom::RendererHost> receiver);
   void BindMediaInterfaceProxy(
@@ -1119,14 +1116,6 @@ class CONTENT_EXPORT RenderProcessHostImpl
 
   // The memory allocator, if any, in which the renderer will write its metrics.
   std::unique_ptr<base::PersistentMemoryAllocator> metrics_allocator_;
-
-  // TODO(mythria): Currently we are in the process of migrating CodeCacheHost
-  // interface to use execution specific contexts. Once the migration is
-  // complete remove CodeCacheHost interface from the RenderProcessHost.
-  // Currently fetching code caches from main thread use the interface
-  // associated with the RenderFrameHost. All others (fetches from worker
-  // threads, writing into code caches) use per-process interface.
-  CodeCacheHostImpl::ReceiverSet code_cache_host_receivers_;
 
   bool channel_connected_;
   bool sent_render_process_ready_;
--- content/renderer/renderer_blink_platform_impl.h
+++ content/renderer/renderer_blink_platform_impl.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom.h"
-#include "third_party/blink/public/mojom/loader/code_cache.mojom.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "components/services/font/public/cpp/font_loader.h"  // nogncheck
@@ -91,23 +90,6 @@ class CONTENT_EXPORT RendererBlinkPlatformImpl : public BlinkPlatformImpl {
   blink::WebString FullUserAgent() override;
   blink::WebString ReducedUserAgent() override;
   blink::UserAgentMetadata UserAgentMetadata() override;
-  void CacheMetadata(blink::mojom::CodeCacheType cache_type,
-                     const blink::WebURL&,
-                     base::Time,
-                     const uint8_t*,
-                     size_t) override;
-  void FetchCachedCode(blink::mojom::CodeCacheType cache_type,
-                       const blink::WebURL&,
-                       FetchCachedCodeCallback) override;
-  void ClearCodeCacheEntry(blink::mojom::CodeCacheType cache_type,
-                           const GURL&) override;
-  void CacheMetadataInCacheStorage(
-      const blink::WebURL&,
-      base::Time,
-      const uint8_t*,
-      size_t,
-      const blink::WebSecurityOrigin& cacheStorageOrigin,
-      const blink::WebString& cacheStorageCacheName) override;
   bool IsRedirectSafe(const GURL& from_url, const GURL& to_url) override;
   blink::WebResourceRequestSenderDelegate* GetResourceRequestSenderDelegate()
       override;
@@ -269,10 +251,6 @@ class CONTENT_EXPORT RendererBlinkPlatformImpl : public BlinkPlatformImpl {
  private:
   bool CheckPreparsedJsCachingEnabled() const;
 
-  // Return the mojo interface for making CodeCache calls. Safe to call from
-  // other threads, as it returns the SharedRemote by copy.
-  mojo::SharedRemote<blink::mojom::CodeCacheHost> GetCodeCacheHost();
-
   void Collect3DContextInformation(blink::Platform::GraphicsInfo* gl_info,
                                    const gpu::GPUInfo& gpu_info) const;
 
@@ -293,12 +271,6 @@ class CONTENT_EXPORT RendererBlinkPlatformImpl : public BlinkPlatformImpl {
   blink::scheduler::WebThreadScheduler* main_thread_scheduler_;
 
   TopLevelBlameContext top_level_blame_context_;
-
-  base::Lock code_cache_host_lock_;
-  mojo::PendingRemote<blink::mojom::CodeCacheHost> code_cache_host_remote_
-      GUARDED_BY(code_cache_host_lock_);
-  mojo::SharedRemote<blink::mojom::CodeCacheHost> code_cache_host_
-      GUARDED_BY(code_cache_host_lock_);
 
   // Event that signals `io_thread_id_` is set and ready to be read.
   mutable base::WaitableEvent io_thread_id_ready_event_;
