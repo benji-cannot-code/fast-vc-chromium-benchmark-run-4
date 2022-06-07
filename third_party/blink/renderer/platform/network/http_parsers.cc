96484059dcbe4575130a13fa2ff55eba984bb1c4
Sohom <sohom.datta@learner.manipal.edu>
2022-06-07 17:11:46 +0000

    Fix failing server-timing header parsing wpt tests
    Fix 67th and 68th subtest for
    server-timing/server_timing_header-parsing.https.html.
    
    The parser will ignore extraneous data between the
    timing header name and a semicolon or comma delimiter
    in a server-timing header.
    
    Bug: 1301932
    Change-Id: I947863c4bd3c201fbdef0997246dd47f4fe34e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686931
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011515}

--- third_party/blink/renderer/platform/network/http_parsers.cc
+++ third_party/blink/renderer/platform/network/http_parsers.cc
@@ -797,6 +797,8 @@ std::unique_ptr<ServerTimingHeaderVector> ParseServerTimingHeader(
 
       ServerTimingHeader header(name.ToString());
 
+      tokenizer.ConsumeBeforeAnyCharMatch({',', ';'});
+
       while (tokenizer.Consume(';')) {
         StringView parameter_name;
         if (!tokenizer.ConsumeToken(ParsedContentType::Mode::kNormal,
