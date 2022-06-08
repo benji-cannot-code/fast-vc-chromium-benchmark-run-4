4b1078dda286424713387db2e0db5c2cd91402bb
Ed Chin <edchin@google.com>
2022-06-08 23:01:17 +0000

    [ios][feed] Expose enable ghost cards flag
    Bug: 1329931
    Change-Id: I758fc74f1daeaeb83c77b76dc1c202b69a239977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696479
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012210}

--- ios/chrome/browser/ui/ntp/new_tab_page_feature.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the feed context menu.
 extern const base::Feature kEnableDiscoverFeedPreview;
 
+// Feature flag to show ghost cards when refreshing the discover feed.
+extern const base::Feature kDiscoverFeedGhostCardsEnabled;
+
 // Feature flag to enable shorter cache so that more ghost cards appear.
 extern const base::Feature kEnableDiscoverFeedShorterCache;
 
--- ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
@@ -17,6 +17,9 @@ const base::Feature kEnableDiscoverFeedPreview{
 const base::Feature kEnableDiscoverFeedAppFlows{
     "EnableDiscoverFeedAppFlows", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kDiscoverFeedGhostCardsEnabled{
+    "DiscoverFeedGhostCardsEnabled", base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kEnableDiscoverFeedShorterCache{
     "EnableDiscoverFeedShorterCache", base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -45,15 +48,16 @@ const char kDiscoverFeedTopSyncPromoStyleCompact[] = "compact";
 const base::Feature kEnableFeedAblation{"FeedAblationEnabled",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kDiscoverFeedGhostCardsEnabled{
-    "DiscoverFeedGhostCardsEnabled", base::FEATURE_DISABLED_BY_DEFAULT};
-
 bool IsDiscoverFeedPreviewEnabled() {
   return base::FeatureList::IsEnabled(kEnableDiscoverFeedPreview);
 }
 
 bool IsDiscoverFeedAppFlowsEnabled() {
   return base::FeatureList::IsEnabled(kEnableDiscoverFeedAppFlows);
+}
+
+bool IsDiscoverFeedGhostCardsEnabled() {
+  return base::FeatureList::IsEnabled(kDiscoverFeedGhostCardsEnabled);
 }
 
 bool IsDiscoverFeedShorterCacheEnabled() {
@@ -70,8 +74,4 @@ bool IsDiscoverFeedTopSyncPromoEnabled() {
 
 bool IsFeedAblationEnabled() {
   return base::FeatureList::IsEnabled(kEnableFeedAblation);
-}
-
-bool IsDiscoverFeedGhostCardsEnabled() {
-  return base::FeatureList::IsEnabled(kDiscoverFeedGhostCardsEnabled);
 }
