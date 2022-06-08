56e6b9a253a807a61bdb670d8e7d4aedb2df2d5b
Cici Ruan <cuicuiruan@google.com>
2022-06-08 19:17:16 +0000

    arc/gio: Show the message in multiple lines
    Show the message in multiple lines if the width is not long enough.
    
    Bug: b:232948415 b:188452744
    Test: manual test and unittest
    Change-Id: I0264bfab83c754358eba57e69dc25e489724a211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691870
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012097}

--- chrome/browser/ash/arc/input_overlay/ui/message_view.cc
+++ chrome/browser/ash/arc/input_overlay/ui/message_view.cc
@@ -67,6 +67,7 @@ MessageView::MessageView(DisplayOverlayController* controller,
   label()->SetFontList(gfx::FontList({kFontStyle}, gfx::Font::NORMAL, kFontSize,
                                      gfx::Font::Weight::NORMAL));
   label()->SetLineHeight(kLineHeight);
+  label()->SetMultiLine(true);
 
   SetImageLabelSpacing(kImageLabelSpace);
   image()->SetHorizontalAlignment(views::ImageView::Alignment::kLeading);
