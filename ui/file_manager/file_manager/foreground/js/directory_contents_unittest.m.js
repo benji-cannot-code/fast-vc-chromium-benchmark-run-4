c73c05e9ce8f633f71dee2eda7b4ad97607fdfbb
Noel Gordon <noel@chromium.org>
2022-06-08 03:08:45 +0000

    filesapp: prevent console.errors due to Recents in MediaStore filter
    Per crbug.com/1333385/#c17 when Android MediaStore is enabled in Files
    app, console.errors and /var/log/chrome/chrome LOG(ERROR) can occur if
    the user presses any Recents button in the files app UI.
    
    Add a FilesFilter to prevent the error. If the feature is enabled, add
    a files filter to remove entry from disallowed volumes.
    
    This fix is a mitigation for now. TODO(RECENTS): consider updating the
    directoryModel.RecentContentScanner to handle disallowed volumes.
    
    Bug: 1333385, 1334442
    Change-Id: I26501d7ac04bb1a6acdfa05b5322111042d20316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3694537
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011805}

--- ui/file_manager/file_manager/foreground/js/directory_contents_unittest.m.js
+++ ui/file_manager/file_manager/foreground/js/directory_contents_unittest.m.js
@@ -45,6 +45,9 @@ export function testHiddenFiles() {
         rootType: volumeManagerRootType,
       });
     },
+    getMediaStoreFilesOnlyFilterEnabled: () => {
+      return false;
+    },
   });
 
   const entry = (fullPath) =>
