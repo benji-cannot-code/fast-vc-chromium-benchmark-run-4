2e15e038d327e6340a2ec84f8edaa6a0b8a9f02d
Solomon Kinard <solomonkinard@chromium.org>
2022-06-08 19:13:08 +0000

    Extensions: Favicon: Promote to stable from canary
    * Promote to stable from canary.
    * Extension feature enabled by default.
    
    Fixed: 104102
    Change-Id: Ibf6e9674341a16caa6cbd26ae8f79399b4ea7c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635900
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012095}

--- chrome/browser/extensions/api/favicon/favicon_apitest.cc
+++ chrome/browser/extensions/api/favicon/favicon_apitest.cc
@@ -16,10 +16,7 @@ namespace extensions {
 
 class FaviconApiTest : public ExtensionApiTest {
  public:
-  FaviconApiTest() {
-    feature_list_.InitAndEnableFeature(
-        extensions_features::kNewExtensionFaviconHandling);
-  }
+  FaviconApiTest() = default;
 
  protected:
   void SetUpOnMainThread() override {
@@ -27,10 +24,6 @@ class FaviconApiTest : public ExtensionApiTest {
     host_resolver()->AddRule("*", "127.0.0.1");
     ASSERT_TRUE(StartEmbeddedTestServer());
   }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-  ScopedCurrentChannel current_channel_{version_info::Channel::CANARY};
 };
 
 // TODO(crbug.com/1319934): Test is flaky on Mac.
