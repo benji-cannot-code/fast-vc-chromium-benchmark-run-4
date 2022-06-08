25206db79f6831f375a4f390695964150387b3a2
Joshua Hood <jdh@chromium.org>
2022-06-08 16:05:43 +0000

    [DIPS] Add logic for detecting client-side redirects
    happening within the primary main frame.
    
    Validate-Test-Flakiness: skip
    Change-Id: I69cdb29532f381db8c98db17180f6447828322bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3634920
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011992}

---
+++ chrome/browser/dips/dips_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DIPS_DIPS_UTILS_H_
+#define CHROME_BROWSER_DIPS_DIPS_UTILS_H_
+
+#include <ostream>
+
+#include "base/strings/string_piece_forward.h"
+
+/* Redirect Type Methods */
+enum class DIPSRedirectType { kClient, kServer };
+
+base::StringPiece GetHistogramPiece(DIPSRedirectType type);
+
+const char* DIPSRedirectTypeToString(DIPSRedirectType type);
+
+std::ostream& operator<<(std::ostream& os, DIPSRedirectType type);
+
+#endif  // CHROME_BROWSER_DIPS_DIPS_UTILS_H_
