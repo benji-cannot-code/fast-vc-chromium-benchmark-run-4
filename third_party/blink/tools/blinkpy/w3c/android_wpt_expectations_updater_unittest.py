e3213ff769d9fe6f3a6b343f4b04deb381c12941
Jonathan Lee <jonathanjlee@google.com>
2022-06-08 18:01:04 +0000

    Revert "[blinkpy] Importer updates all flag-specific baselines and expectations"
    This reverts commit a75dd7a20ed903f11c7b0258ae7bce427f79ec91.
    
    Reason for revert: Importer breakage:
      https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket/8811928157466130497/+/u/Import_changes_from_WPT_to_Chromium/stdout
    
    Original change's description:
    > [blinkpy] Importer updates all flag-specific baselines and expectations
    >
    > This change allows the importer to rebaseline and update expectations
    > for any flag-specific suites that run on a try builder recognized by
    > 'builders.json'. Previously, only 'disable-site-isolation-trials' and
    > 'disable-layout-ng' had hardcoded support.
    >
    > Because 'rebaseline-cl' handles flag-specific baselines robustly now,
    > this change also removes the flag-specific code path in the expectations
    > updater that largely duplicates the generic case.
    >
    > Bug: 1291020
    > Change-Id: I2cec602086faa34644db82735469088674fccce2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689149
    > Reviewed-by: Weizhong Xia <weizhong@google.com>
    > Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1011072}
    
    Bug: 1291020
    Change-Id: I06a6665d901a605a3c454d2cca15029a9774e453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696417
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012059}

--- third_party/blink/tools/blinkpy/w3c/android_wpt_expectations_updater_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/android_wpt_expectations_updater_unittest.py
@@ -56,12 +56,9 @@ class AndroidWPTExpectationsUpdaterTest(LoggingTestCase):
         host.builders = BuilderList({
             'MOCK Android Weblayer - Pie': {
                 'port_name': 'test-android-pie',
-                'specifiers':
-                ['Precise', 'Release', 'anDroid', 'android_Weblayer'],
+                'specifiers': ['Precise', 'Release',
+                               'anDroid', 'android_Weblayer'],
                 'is_try_builder': True,
-                'steps': {
-                    'weblayer_shell_wpt (with patch)': {},
-                },
             },
         })
 
