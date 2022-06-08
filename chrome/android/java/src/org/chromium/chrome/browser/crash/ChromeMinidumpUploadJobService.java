f1bfde61d20e18e864492b2a5dd470ff316ddbd7
Shakti Sahu <shaktisahu@chromium.org>
2022-06-08 20:06:08 +0000

    Record background restrictions split by android and user
    This is a follow up CL on
    https://chromium-review.googlesource.com/c/chromium/src/+/3646407
    We are recording the metrics here split by user enabled background
    restrictions.
    
    Bug: 1291584
    Change-Id: I1c68c67b72b1085a766d13304fa434aea448ca6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687422
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012114}

--- chrome/android/java/src/org/chromium/chrome/browser/crash/ChromeMinidumpUploadJobService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/ChromeMinidumpUploadJobService.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.crash;
 
 import org.chromium.base.annotations.IdentifierNameString;
 import org.chromium.chrome.browser.base.SplitCompatMinidumpUploadJobService;
+import org.chromium.chrome.browser.metrics.UmaUtils;
 
 /** See {@link ChromeMinidumpUploadJobServiceImpl}. */
 public class ChromeMinidumpUploadJobService extends SplitCompatMinidumpUploadJobService {
@@ -16,5 +17,10 @@ public class ChromeMinidumpUploadJobService extends SplitCompatMinidumpUploadJob
 
     public ChromeMinidumpUploadJobService() {
         super(sImplClassName);
+    }
+
+    @Override
+    protected void recordMinidumpUploadingTime(long taskDurationMs) {
+        UmaUtils.recordMinidumpUploadingTime(taskDurationMs);
     }
 }
