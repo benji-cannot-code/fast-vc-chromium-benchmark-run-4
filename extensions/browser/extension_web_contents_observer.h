827fb12207074dcdcd6fc64d9d0d237c9a993c09
Lukasz Anforowicz <lukasza@chromium.org>
2022-06-08 22:10:23 +0000

    Explicit ExtensionWebContentsObserver::extension_frame_host_for_testing
    This CL removes a `friend` declaration between
    `ExtensionWebContentsObserver` and `ExtensionFrameHostBrowserTest` and
    replaces it with an explicit `extension_frame_host_for_testing`
    accessor.  This helps in follow-up CLs by allowing them to reuse the
    accessor (instead of having to declare friendship across //chrome and
    //extensions layers).
    
    Bug: 1183604
    Change-Id: I7cf4710e56323baab92d5aa18cac4647bb612e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691767
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012183}

--- extensions/browser/extension_web_contents_observer.h
+++ extensions/browser/extension_web_contents_observer.h
@@ -102,6 +102,10 @@ class ExtensionWebContentsObserver
   // ExtensionWebContentsObserver has already been initialized.
   void ListenToWindowIdChangesFrom(sessions::SessionTabHelper* helper);
 
+  ExtensionFrameHost* extension_frame_host_for_testing() {
+    return extension_frame_host_.get();
+  }
+
  protected:
   explicit ExtensionWebContentsObserver(content::WebContents* web_contents);
   ~ExtensionWebContentsObserver() override;
@@ -144,8 +148,6 @@ class ExtensionWebContentsObserver
 
  private:
   using PassKey = base::PassKey<ExtensionWebContentsObserver>;
-
-  friend class ExtensionFrameHostBrowserTest;
 
   void OnWindowIdChanged(const SessionID& id);
 
