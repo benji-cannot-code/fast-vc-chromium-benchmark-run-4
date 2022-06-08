a0eef370f1ec68a3e87bf2f678ac7a5256673331
Sherri Lin <sherrilin@google.com>
2022-06-08 08:37:21 +0000

    enterprise: Update device license mode when device policy data is set
    Previously, the device license mode is not updated correctly as login
    shelf view ctor is called before the policy is set.
    
    Inherit EnterpriseDomainObserver to update the device enterprise info
    when available.
    
    Also, update visibility of Kiosk default message by the direct logic of
    if there is Kiosk app or not.
    
    BUG=b:234765162
    
    Change-Id: I43e02cbafb8abd840c86578d12243b894b6b5986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687961
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Sherri Lin <sherrilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011857}

--- ash/login/ui/lock_contents_view.h
+++ ash/login/ui/lock_contents_view.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/login_types.h"
 #include "ash/public/cpp/smartlock_state.h"
 #include "ash/public/cpp/system_tray_observer.h"
+#include "ash/system/enterprise/enterprise_domain_observer.h"
+#include "ash/system/model/enterprise_domain_model.h"
 #include "base/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -72,7 +74,8 @@ class ASH_EXPORT LockContentsView
       public SystemTrayObserver,
       public display::DisplayObserver,
       public KeyboardControllerObserver,
-      public chromeos::PowerManagerClient::Observer {
+      public chromeos::PowerManagerClient::Observer,
+      public EnterpriseDomainObserver {
  public:
   METADATA_HEADER(LockContentsView);
   class AuthErrorBubble;
@@ -155,7 +158,7 @@ class ASH_EXPORT LockContentsView
   void ShowAdbEnabled();
   void ToggleSystemInfo();
   void ShowParentAccessDialog();
-  void SetKioskAppsButtonPresence(bool is_kiosk_apps_button_present);
+  void SetHasKioskApp(bool has_kiosk_apps);
 
   // views::View:
   void Layout() override;
@@ -239,6 +242,10 @@ class ASH_EXPORT LockContentsView
 
   // chromeos::PowerManagerClient::Observer:
   void SuspendImminent(power_manager::SuspendImminent::Reason reason) override;
+
+  // ash::EnterpriseDomainObserver
+  void OnDeviceEnterpriseInfoChanged() override;
+  void OnEnterpriseAccountDomainChanged() override;
 
   void ShowAuthErrorMessageForDebug(int unlock_attempt);
 
@@ -465,6 +472,10 @@ class ASH_EXPORT LockContentsView
   // Shows GAIA sign-in page.
   void OnBackToSigninButtonTapped();
 
+  // Update visibility of Kiosk default message. Called only if
+  // kiosk_license_mode_ is true.
+  void UpdateKioskDefaultMessageVisibility();
+
   const LockScreen::ScreenType screen_type_;
 
   std::vector<UserState> users_;
@@ -505,6 +516,9 @@ class ASH_EXPORT LockContentsView
 
   display::ScopedDisplayObserver display_observer_{this};
 
+  base::ScopedObservation<EnterpriseDomainModel, EnterpriseDomainObserver>
+      enterprise_domain_model_observation_{this};
+
   // All error bubbles and the tooltip view are child views of LockContentsView,
   // and will be torn down when LockContentsView is torn down.
   // Bubble for displaying authentication error.
@@ -543,9 +557,10 @@ class ASH_EXPORT LockContentsView
   // screen note state.
   bool disable_lock_screen_note_ = false;
 
-  // TODO(1307303): Change it to the real function that checks if device is with
-  // Kiosk License
+  // Whether the device is enrolled with Kiosk SKU.
   bool kiosk_license_mode_ = false;
+  // Whether any kiosk app is added.
+  bool has_kiosk_apps_ = false;
 
   // Whether the system information should be displayed or not be displayed
   // forcedly according to policy settings.
