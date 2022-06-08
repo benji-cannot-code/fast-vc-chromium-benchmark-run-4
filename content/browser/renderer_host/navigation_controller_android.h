2e6d74b751a926859fa68a14e199fa885b8273b1
Rakina Zata Amni <rakina@chromium.org>
2022-06-08 14:53:50 +0000

    Make SetUseDesktopUserAgent asynchronous to avoid navigation re-entrancy
    Sometimes it's possible to call
    NavigationControllerAndroid::SetUseDesktopUserAgent()  in response to
    a navigation to a pending entry (see crbug.com/1327907#c5). In this
    case, we should avoid triggering another navigation synchronously, as
    it will crash due to navigation re-entrancy checks. To do that, post
    a task to update the UA and reload asynchronously. This avoids the
    crash we're seeing in the linked bug while still allowing the UA
    update to take effect at a later time.
    
    Bug: 1327907
    Change-Id: I8657a07756b57dbfefe99ab407656f51ca18f35c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691337
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011957}

--- content/browser/renderer_host/navigation_controller_android.h
+++ content/browser/renderer_host/navigation_controller_android.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "url/origin.h"
 
@@ -147,8 +148,12 @@ class CONTENT_EXPORT NavigationControllerAndroid {
       jint index);
 
  private:
+  void SetUseDesktopUserAgentInternal(bool enabled,
+                                      bool reload_on_state_change);
+
   raw_ptr<NavigationControllerImpl> navigation_controller_;
   base::android::ScopedJavaGlobalRef<jobject> obj_;
+  base::WeakPtrFactory<NavigationControllerAndroid> weak_factory_{this};
 };
 
 }  // namespace content
