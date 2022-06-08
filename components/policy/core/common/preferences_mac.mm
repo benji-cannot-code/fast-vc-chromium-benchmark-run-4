f38e4bb2979f232425b96f76cc301389736c7a1e
Owen Min <zmin@chromium.org>
2022-06-08 01:07:16 +0000

    Fix memory leak in PreferencesMac
    Using base::scoped_nsobject<id> for the result of copyValueForKey in
    function MachinePolicyScope::IsManagedPolicyAvailable() to make sure the
    value is released after function returned.
    
    Bug: 1317523
    Change-Id: Ica4a7f2e0e684104a5383cacd28cd635f6f8bb3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690104
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011765}

--- components/policy/core/common/preferences_mac.mm
+++ components/policy/core/common/preferences_mac.mm
@@ -84,7 +84,8 @@ class MachinePolicyScope : public MacPreferences::PolicyScope {
     }
     if (!machine_scope_)
       return YES;
-    return [machine_scope_ copyValueForKey:base::mac::CFToNSCast(key)] != nil;
+    return base::scoped_nsobject<id>([machine_scope_
+               copyValueForKey:base::mac::CFToNSCast(key)]) != nil;
   }
 
  private:
