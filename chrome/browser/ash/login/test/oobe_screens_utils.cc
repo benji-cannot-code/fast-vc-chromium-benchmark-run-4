cf0e587079deefb7e8a392a122930a94e6324ae7
Renato Silva <rrsilva@google.com>
2022-06-08 21:06:32 +0000

    OOBE - Add OobeUiDestroyedWaiter
    Add OobeUiDestroyedWaiter and JsChecker method for cliking
    on things and waiting for OOBE to be destroyed. This will
    be used in tests that click on buttons in the OOBE UI that
    leads to a restart/reset. Tests are flaky without this,
    because sometimes the OobeUI is destroyed before we return
    from the JsChecker methods.
    
    Bug: 1325788
    Change-Id: I262317de7dd2aab0f70cc57ed7dfd30121a8f207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695238
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012154}

--- chrome/browser/ash/login/test/oobe_screens_utils.cc
+++ chrome/browser/ash/login/test/oobe_screens_utils.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/login/user_creation_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/welcome_screen_handler.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 
@@ -233,6 +234,46 @@ void LanguageReloadObserver::Wait() {
 
 LanguageReloadObserver::~LanguageReloadObserver() {
   welcome_screen_->RemoveObserver(this);
+}
+
+OobeUiDestroyedWaiter::OobeUiDestroyedWaiter(OobeUI* oobe_ui) {
+  oobe_ui_observation_.Observe(oobe_ui);
+}
+
+OobeUiDestroyedWaiter::~OobeUiDestroyedWaiter() {}
+
+void OobeUiDestroyedWaiter::Wait() {
+  if (was_destroyed_)
+    return;
+
+  run_loop_ = std::make_unique<base::RunLoop>();
+  run_loop_->Run();
+  run_loop_.reset();
+
+  ASSERT_TRUE(was_destroyed_)
+      << "Timed out while waiting for OobeUI to be destroyed!";
+  oobe_ui_observation_.Reset();
+}
+
+void OobeUiDestroyedWaiter::OnDestroyingOobeUI() {
+  was_destroyed_ = true;
+  run_loop_->Quit();
+}
+
+// Start observing, tap/click and wait.
+void TapOnPathAndWaitForOobeToBeDestroyed(
+    std::initializer_list<base::StringPiece> element_ids) {
+  // Get the OOBE WebUI Controller (OobeUI) and start observing.
+  content::WebContents* web_contents =
+      LoginDisplayHost::default_host()->GetOobeWebContents();
+  CHECK(web_contents);
+  OobeUI* oobe_ui =
+      static_cast<OobeUI*>(web_contents->GetWebUI()->GetController());
+  OobeUiDestroyedWaiter observer{oobe_ui};
+
+  test::OobeJS().TapOnPathAsync(element_ids);
+
+  observer.Wait();
 }
 
 }  // namespace test
