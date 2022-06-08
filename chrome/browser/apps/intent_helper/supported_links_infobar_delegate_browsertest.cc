ac82130ddd0a520e2c5db490d5be8802d620cd1e
Nancy Wang <nancylingwang@chromium.org>
2022-06-08 12:26:40 +0000

    Fix the AcceptInfoBarChangesSupportedLinks test for Lacros.
    When the AcceptInfoBarChangesSupportedLinks test runs in Lacros, it
    failed if enable the AppServicePreferredAppsWithoutMojom flag:
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-lacros-rel/b8812013035453652145/test-results?q=ExactID%3Aninja%3A%2F%2Fchrome%2Ftest%3Alacros_chrome_browsertests_run_in_series%2FSupportedLinksInfoBarDelegateBrowserTest.AcceptInfoBarChangesSupportedLinks+VHash%3Ad69baac6a52d382d
    
    Because the web app runs in the Lacros side, and it forwards the
    preferred app info via mojom. So when the web app CrosApi is enabled, we still need to wait for the Crosapi mojom calls.
    
    BUG=1253250
    
    Change-Id: Ic28f4a066ec8e69d72a3b67f0a904e5bf952568c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690206
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011901}

--- chrome/browser/apps/intent_helper/supported_links_infobar_delegate_browsertest.cc
+++ chrome/browser/apps/intent_helper/supported_links_infobar_delegate_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/web_app_utils.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar.h"
 #include "components/services/app_service/public/cpp/features.h"
@@ -96,8 +97,11 @@ IN_PROC_BROWSER_TEST_F(SupportedLinksInfoBarDelegateBrowserTest,
   EXPECT_TRUE(infobar);
   GetDelegate(infobar)->Accept();
 
-  if (!base::FeatureList::IsEnabled(apps::kAppServicePreferredAppsWithoutMojom))
+  if (web_app::IsWebAppsCrosapiEnabled() ||
+      !base::FeatureList::IsEnabled(
+          apps::kAppServicePreferredAppsWithoutMojom)) {
     WaitForPreferredAppUpdate();
+  }
 
   ASSERT_TRUE(
       app_service_proxy()->PreferredAppsList().IsPreferredAppForSupportedLinks(
@@ -119,8 +123,11 @@ IN_PROC_BROWSER_TEST_F(SupportedLinksInfoBarDelegateBrowserTest,
   }
 
   app_service_proxy()->SetSupportedLinksPreference(test_web_app_id());
-  if (!base::FeatureList::IsEnabled(apps::kAppServicePreferredAppsWithoutMojom))
+  if (web_app::IsWebAppsCrosapiEnabled() ||
+      !base::FeatureList::IsEnabled(
+          apps::kAppServicePreferredAppsWithoutMojom)) {
     WaitForPreferredAppUpdate();
+  }
 
   Browser* browser = OpenTestWebApp();
   auto* contents = browser->tab_strip_model()->GetActiveWebContents();
