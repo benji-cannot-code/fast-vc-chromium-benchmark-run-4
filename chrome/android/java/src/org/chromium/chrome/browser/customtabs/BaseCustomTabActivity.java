5121fb90d651ff91dd5c777a5ba42495c14524f8
Shakti Sahu <shaktisahu@chromium.org>
2022-06-08 21:23:05 +0000

    Contextual Page Actions: Added price tracking button controller
    This CL adds a button controller to provide price tracking related UI
    functionality for contextual page actions. The controller will be
    instantiated in RootUiCoordinator and hooked up to
    ContextualPageActionController in a later CL. Clicking on the button
    triggers a bookmark with price tracking toggle flow which is same as
    the one from bookmark button in app menu.
    
    It also involves a small refactor that extracts out the bookmarking
    related code out of Chrome*Activity into a separate class TabBookmarker.
    The bookmarker will be plumbed to the button controller through a
    supplier interface through the RootUiCoordinator.
    
    UX: https://docs.google.com/presentation/d/1dz5qgDB5C2W2OEvX9YyPPgsy7sOnJ_MmmkTkK0lMIvw/edit?resourcekey=0-fy2DZAkYWFFM_NWBp8P6fw#slide=id.g125c3ccf3eb_2_163
    
    Bug: 1322001
    Change-Id: I13f6fa29f4a4833ca6799e3fa7ce95adfbde0e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674338
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012162}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -148,8 +148,8 @@ public abstract class BaseCustomTabActivity extends ChromeActivity<BaseCustomTab
     protected RootUiCoordinator createRootUiCoordinator() {
         // clang-format off
         mBaseCustomTabRootUiCoordinator = new BaseCustomTabRootUiCoordinator(this,
-                getShareDelegateSupplier(),
-                getActivityTabProvider(), mTabModelProfileSupplier, mBookmarkBridgeSupplier,
+                getShareDelegateSupplier(), getActivityTabProvider(), mTabModelProfileSupplier,
+                mBookmarkBridgeSupplier, mTabBookmarkerSupplier,
                 getContextualSearchManagerSupplier(), getTabModelSelectorSupplier(),
                 getBrowserControlsManager(), getWindowAndroid(), getLifecycleDispatcher(),
                 getLayoutManagerSupplier(),
