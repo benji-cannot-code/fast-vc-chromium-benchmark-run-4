66f9042e4d012338f801c02a3464eb7d6c24819c
Md Shahadat Hossain Shahin <shahinmd@google.com>
2022-06-08 10:38:20 +0000

    [Privacy Hub] Add camera toggle to the dummy UI
    This is the third in a series of CLs that implement a dummy UI for the
    privacy hub.
    
    The CL adds a camera toggle button in the privacy hub page.
    
    Bug: b/231310765
    Change-Id: I854274ecc2d36c8eca9104f89be0f7ab87515a3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669895
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Auto-Submit: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011883}

--- chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_page.js
+++ chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_page.js
@@ -8,12 +8,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'os-settings-privacy-hub-page' contains privacy hub configurations.
  */
 
+import '../../controls/settings_toggle_button.js';
+
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {Route} from '../../router.js';
+import {DeepLinkingBehavior, DeepLinkingBehaviorInterface} from '../deep_linking_behavior.js';
+import {routes} from '../os_route.js';
+import {RouteObserverBehavior, RouteObserverBehaviorInterface} from '../route_observer_behavior.js';
+
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {DeepLinkingBehaviorInterface}
+ * @implements {RouteObserverBehaviorInterface}
+ */
+const SettingsPrivacyHubPageBase = mixinBehaviors(
+    [DeepLinkingBehavior, RouteObserverBehavior], PolymerElement);
 
 /** @polymer */
-class SettingsPrivacyHubPage extends PolymerElement {
+class SettingsPrivacyHubPage extends SettingsPrivacyHubPageBase {
   static get is() {
     return 'settings-privacy-hub-page';
   }
@@ -26,6 +42,41 @@ class SettingsPrivacyHubPage extends PolymerElement {
   ready() {
     super.ready();
     assert(loadTimeData.getBoolean('showPrivacyHub'));
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Preferences state.
+       */
+      prefs: {
+        type: Object,
+        notify: true,
+      },
+
+      /**
+       * Used by DeepLinkingBehavior to focus this page's deep links.
+       * @type {!Set<!chromeos.settings.mojom.Setting>}
+       */
+      supportedSettingIds: {
+        type: Object,
+        value: () => new Set([
+          chromeos.settings.mojom.Setting.kCameraOnOff,
+        ]),
+      },
+    };
+  }
+
+  /**
+   * RouteObserverBehavior
+   * @param {!Route} route
+   */
+  currentRouteChanged(route) {
+    // Does not apply to this page.
+    if (route !== routes.PRIVACY_HUB) {
+      return;
+    }
+    this.attemptDeepLink();
   }
 }
 
