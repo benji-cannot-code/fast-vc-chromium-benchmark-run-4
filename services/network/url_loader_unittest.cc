2c878b214bd2296ab267f9f2bf271c91c0ffaeaf
Titouan Rigoudy <titouan@chromium.org>
2022-06-08 19:14:28 +0000

    [Private Network Access] Exempt cached & proxied resources from checks.
    Proxied resources are exempt from checks since crrev.com/c/3320317 due
    to crbug.com/1253239. Cached resources are checked as if they had come
    directly from the network since crrev.com/c/3552562.
    
    Resources that were proxied over the network then cached should be
    treated as if freshly proxied - that is, they should be exempt from
    checks by being treated as belonging to the `kUnknown` address space.
    
    Otherwise, if a resource is partially cached and partially loaded
    through a proxy, PNA checks can get confused, think the address spaces
    are inconsistent, and fail the request.
    
    Fixed: chromium:1332495
    Change-Id: I6356a67a7f86a88d4df65dfbfe053c33f42f97a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688979
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012096}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -1965,6 +1965,7 @@ mojom::IPAddressSpace ResponseAddressSpace(
     case net::TransportType::kCached:
       return params.endpoint_address_space;
     case net::TransportType::kProxied:
+    case net::TransportType::kCachedFromProxy:
       return mojom::IPAddressSpace::kUnknown;
   }
 }
@@ -2160,6 +2161,31 @@ constexpr URLLoaderFakeTransportInfoTestParams
             mojom::IPAddressSpace::kPrivate,
             mojom::IPAddressSpace::kLocal,
             net::TransportType::kProxied,
+            net::OK,
+        },
+        // TransportType: kCachedFromProxy
+        {
+            mojom::IPAddressSpace::kUnknown,
+            mojom::IPAddressSpace::kLocal,
+            net::TransportType::kCachedFromProxy,
+            net::OK,
+        },
+        {
+            mojom::IPAddressSpace::kPublic,
+            mojom::IPAddressSpace::kLocal,
+            net::TransportType::kCachedFromProxy,
+            net::OK,
+        },
+        {
+            mojom::IPAddressSpace::kPublic,
+            mojom::IPAddressSpace::kPrivate,
+            net::TransportType::kCachedFromProxy,
+            net::OK,
+        },
+        {
+            mojom::IPAddressSpace::kPrivate,
+            mojom::IPAddressSpace::kLocal,
+            net::TransportType::kCachedFromProxy,
             net::OK,
         },
         // TransportType: kCached. We only test a local target for brevity.
