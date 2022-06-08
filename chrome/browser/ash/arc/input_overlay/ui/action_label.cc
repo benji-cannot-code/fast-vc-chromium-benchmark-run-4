bcbfd2716a71673ce612779b106c13b0544793c1
Cici Ruan <cuicuiruan@google.com>
2022-06-08 08:25:14 +0000

    arc/gio: Support Tab key as A11y requirement
    Tab key is used to traverse the focus on the UIs.
    
    - kPreMenu is introduced which is a mode before entering into kMenu
      mode for supporting Tab traverse the UI as required by A11y.
      If the menu entry is pressed by mouse or touch, the mode change is
      kView->kMenu.
      If using keyboard to press the menu entry, such as Tab->Space/Enter,
      the mode change is kView->kPreMenu->kMenu.
    - kPreMenu is triggered by the Tab key from kView mode.
    - Space and Enter key is used to enter into kMenu mode from kPreMenu
      mode.
    - Once in kPreMenu mode, any key can get back to kView mode except
      Space and Enter key as above.
    - Once in kPreMenu mode, any located events not happening on the menu
      entry button can get back to kView mode.
    
    Some other changes made when using Tab key to traverse the focus on the
    UIs.
    
    - Once the ActionLabel is unfocused, remove the message view.
    - MessageView can't be focused.
    
    Bug: b:188452744
    Test: manual test and unittest
    Change-Id: I9ff996f41197e5c4348ac079ef733af588872ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692396
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011836}

--- chrome/browser/ash/arc/input_overlay/ui/action_label.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_label.cc
@@ -191,8 +191,8 @@ void ActionLabel::SetImageActionLabel(MouseAction mouse_action) {
 }
 
 void ActionLabel::SetDisplayMode(DisplayMode mode) {
-  DCHECK(mode != DisplayMode::kMenu);
-  if (mode == DisplayMode::kMenu)
+  DCHECK(mode != DisplayMode::kMenu && mode != DisplayMode::kPreMenu);
+  if (mode == DisplayMode::kMenu || mode == DisplayMode::kPreMenu)
     return;
 
   switch (mode) {
@@ -214,7 +214,7 @@ void ActionLabel::SetDisplayMode(DisplayMode mode) {
       SetToEditError();
       break;
     case DisplayMode::kRestore:
-      if (!ClearFocus())
+      if (HasFocus())
         SetToEditDefault();
       break;
     default:
@@ -254,13 +254,13 @@ void ActionLabel::OnMouseExited(const ui::MouseEvent& event) {
 void ActionLabel::OnFocus() {
   SetToEditFocus();
   LabelButton::OnFocus();
-  auto* parent_view = static_cast<ActionView*>(parent());
-  parent_view->ShowInfoMsg(kEditInfoMessage, this);
+  static_cast<ActionView*>(parent())->ShowInfoMsg(kEditInfoMessage, this);
 }
 
 void ActionLabel::OnBlur() {
   SetToEditDefault();
   LabelButton::OnBlur();
+  static_cast<ActionView*>(parent())->RemoveMessage();
 }
 
 bool ActionLabel::ClearFocus() {
