d6597513c5a2e99e672c54a3e79ec4c2424517ed
Jeevan Shikaram <jshikaram@chromium.org>
2022-06-08 22:31:03 +0000

    [Default Apps] Define app order for cloud gaming app.
    This CL defines the app order for the cloud gaming app.
    
    Bug: 1317499
    Change-Id: I7c83c281c36ca1f4b0100129f1bd1c4723534996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689325
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012195}

--- chrome/browser/chromeos/extensions/default_app_order.cc
+++ chrome/browser/chromeos/extensions/default_app_order.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/page_break_constants.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/common/extensions/extension_constants.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/app_constants/constants.h"
 #include "extensions/common/constants.h"
 
@@ -184,6 +185,10 @@ void GetDefault(std::vector<std::string>* app_ids) {
     extension_misc::kGooglePlusAppId,
   });
   // clang-format on
+
+  if (chromeos::features::IsCloudGamingDeviceEnabled()) {
+    app_ids->push_back(web_app::kCloudGamingPartnerPlatform);
+  }
 }
 
 }  // namespace
