0ae4314100b621e46dcd22bf9be664ed03c27110
Dan Clark <daniec@microsoft.com>
2022-06-08 16:38:26 +0000

    Revert "[SelectMenu] Allow the UA provided shadow DOM to be replaced via attachShadow."
    This reverts commit f0c3efe4e884df8226b3a3a2dff98dec86597f6d.
    Some conflicts were resolved manually.
    
    Per the OpenUI resolution at [1], <selectmenu> will not support
    attachShadow(). This CL updates the implementation to match
    that resolution by reverting the earlier change that added
    this functionality.
    
    [1] https://github.com/openui/open-ui/issues/528
    
    Bug: 1121840
    Change-Id: Ie7b606b327c07e5e3f477e0c0982e2158515ef07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692009
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1012007}

--- third_party/blink/renderer/core/dom/shadow_root.h
+++ third_party/blink/renderer/core/dom/shadow_root.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/tree_scope.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
@@ -76,7 +75,6 @@ class CORE_EXPORT ShadowRoot final : public DocumentFragment, public TreeScope {
     return *To<Element>(ParentOrShadowHostNode());
   }
   ShadowRootType GetType() const { return static_cast<ShadowRootType>(type_); }
-  void UpdateType(ShadowRootType type);
   String mode() const {
     switch (GetType()) {
       case ShadowRootType::kUserAgent:
