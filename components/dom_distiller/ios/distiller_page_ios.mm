973f738a12c4fea70efee413cebda994acb50e4a
Mike Dougherty <michaeldo@chromium.org>
2022-06-08 11:37:51 +0000

    Convert DistillerPageIOS to execute JS on WebFrame
    Bug: 1322482
    Change-Id: Ie47eb8816a50ea53b45c724177027d2a261b34e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689984
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Éric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011892}

--- components/dom_distiller/ios/distiller_page_ios.mm
+++ components/dom_distiller/ios/distiller_page_ios.mm
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "ios/web/public/browser_state.h"
+#import "ios/web/public/js_messaging/web_frame.h"
+#import "ios/web/public/js_messaging/web_frame_util.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "ios/web/public/navigation/web_state_policy_decider.h"
 #import "ios/web/public/web_state.h"
@@ -211,9 +213,16 @@ void DistillerPageIOS::OnLoadURLDone(
     HandleJavaScriptResult(nil);
     return;
   }
+
+  web::WebFrame* main_frame = web::GetMainFrame(web_state_.get());
+  if (!main_frame) {
+    HandleJavaScriptResult(nil);
+    return;
+  }
+
   // Inject the script.
   base::WeakPtr<DistillerPageIOS> weak_this = weak_ptr_factory_.GetWeakPtr();
-  web_state_->ExecuteJavaScript(
+  main_frame->ExecuteJavaScript(
       base::UTF8ToUTF16(script_),
       base::BindOnce(&DistillerPageIOS::HandleJavaScriptResult, weak_this));
 }
