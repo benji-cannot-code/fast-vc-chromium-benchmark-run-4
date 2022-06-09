7803928ccec4fdae49c8edefa8fd783333f20576
Keren Zhu <kerenzhu@chromium.org>
2022-06-09 00:20:27 +0000

    Migrate Tab Group off ThemeProvider to ColorProvider
    Bug: 1334664
    Change-Id: I4e056c0adecb8ce1205e0bc9091dbe731cfd367d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686341
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012246}

--- chrome/browser/app_controller_mac.h
+++ chrome/browser/app_controller_mac.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_change_registrar.h"
 
 class AppControllerProfileObserver;
+class AppControllerNativeThemeObserver;
 @class AppShimMenuController;
 class BookmarkMenuBridge;
 class CommandUpdater;
@@ -36,7 +37,7 @@ class ScopedKeepAlive;
 class TabMenuBridge;
 
 namespace ui {
-class ThemeProvider;
+class ColorProvider;
 }  // namespace ui
 
 // The application controller object, created by loading the MainMenu nib.
@@ -59,6 +60,10 @@ class ThemeProvider;
   // when a profile has been deleted.
   std::unique_ptr<AppControllerProfileObserver>
       _profileAttributesStorageObserver;
+
+  // The NativeThemeObserver observes system-wide theme related settings
+  // change.
+  std::unique_ptr<AppControllerNativeThemeObserver> _nativeThemeObserver;
 
   // Management of the bookmark menu which spans across all windows
   // (and Browser*s). |profileBookmarkMenuBridgeMap_| is a cache that owns one
@@ -116,6 +121,8 @@ class ThemeProvider;
 
   // Request to keep the browser alive during that object's lifetime.
   std::unique_ptr<ScopedKeepAlive> _keep_alive;
+
+  const ui::ColorProvider* _lastActiveColorProvider;
 }
 
 @property(readonly, nonatomic) BOOL startupComplete;
@@ -197,9 +204,11 @@ class ThemeProvider;
 // the original or the incognito profile.
 - (void)setLastProfile:(Profile*)profile;
 
-// Returns the last active ThemeProvider. It is only valid to call this with a
-// last available profile.
-- (const ui::ThemeProvider&)lastActiveThemeProvider;
+// Returns the last active ColorProvider.
+- (const ui::ColorProvider&)lastActiveColorProvider;
+
+// This is called when the system wide light or dark mode changes.
+- (void)nativeThemeDidChange;
 
 // Certain NSMenuItems [Close Tab and Close Window] have different
 // keyEquivalents depending on context. This must be invoked in two locations:
