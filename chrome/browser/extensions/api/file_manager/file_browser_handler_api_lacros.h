78d2fd58c71c08715cb1205fd54da21f7ab79678
Samuel Huang <huangs@chromium.org>
2022-06-09 00:50:18 +0000

    [Lacros] Add fileBrowserHandler.selectFile() support.
    This CL implements FileBrowserHandlerInternalSelectFileFunctionLacros
    so fileBrowserHandler.selectFile() works in Lacros.
    
    Caveat: If a user chooses to create a file, then the file gets
    immediately created, instead of requiring the extension to do so (the
    behavior on Ash). This is due to (in JS code) replacing
      GetExternalFileEntry()
    with
      GetIsolatedFileSystem(fileSystemId).root.getFile(
          baseName, {create: true}, ...)
    to create a FileEntry to a new file, but the call would create the file.
    
    Details:
    * file_browser_handler_internal.json:
      * Add type FileEntryInfoForGetFile for getFile() consumption in
        Lacros.
      * Add the above as an optional field |entryForGetFile| to
        fileBrowserHandlerInternal.selectFile() return result type,
        alongside optional field |entry| for Ash.
    * file_browser_handler_custom_bindings.js:
      * Add |canCreate| param to GetFileEntry() to pass {create: true} to
        getFile().
      * Change selectFile() handler to use GetFileEntry(), and pass the
        available |entry*| field (dependent on Ash or Lacros).
    * FileBrowserHandlerInternalSelectFileFunctionLacros:
      * Remove stub Run() override.
      * OnFilePathSelected(): Add logic to grant file permissions, populate
        results, and respond.
    
    Bug: 1275654
    Change-Id: I841260573255c825839c973de873136c9c245af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688870
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012256}

--- chrome/browser/extensions/api/file_manager/file_browser_handler_api_lacros.h
+++ chrome/browser/extensions/api/file_manager/file_browser_handler_api_lacros.h
@@ -25,10 +25,6 @@ class FileBrowserHandlerInternalSelectFileFunctionLacros
   // The class is ref counted, so destructor should not be public.
   ~FileBrowserHandlerInternalSelectFileFunctionLacros() override;
 
-  // ExtensionFunction implementation.
-  // Runs the extension function implementation.
-  ResponseAction Run() override;
-
  private:
   DECLARE_EXTENSION_FUNCTION("fileBrowserHandlerInternal.selectFile",
                              FILEBROWSERHANDLERINTERNAL_SELECTFILE)
