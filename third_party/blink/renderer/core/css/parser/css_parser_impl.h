2e844b790ba2ab5f7e9a8a73ca415fe96034e436
Anders Hartvoll Ruud <andruud@chromium.org>
2022-06-09 20:22:01 +0000

    Cache MediaQuerySet in CSSParserImpl
    Some pages use a very large number of identical media queries. Since
    MediaQuerySet now is immutable, there is no reason to allocate unique
    instances for all of these.
    
    Bug: 1312000, 1331260
    Change-Id: Ia38ccb936eb688dc97424a0ec32f2ce4a95ee91d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687952
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012674}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.h
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token_range.h"
 #include "third_party/blink/renderer/core/css/parser/css_tokenized_value.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -216,6 +217,8 @@ class CORE_EXPORT CSSParserImpl {
   CSSParserObserver* observer_;
 
   CSSLazyParsingState* lazy_state_;
+
+  HeapHashMap<String, Member<const MediaQuerySet>> media_query_cache_;
 };
 
 }  // namespace blink
