aa5392787b71ccbbf08dfb4258d42fbeeff1b3ca
Yuta Hijikata <ythjkt@google.com>
2022-06-09 01:14:05 +0000

    Check migration mode before restart.
    Instead of checking migration mode during the migration, check it before
    the Ash restart and pass that to session_manager by
    `SessionManagerClient::RequestBrowserDataMigration()`. Then when calling
    `BrowserDataMigrator::Migrate()` from `LacrosDataMigrationScreen`, check
    if `kBrowserDataMigrationMoveMode` switch is passed.
    
    This is in preparation to tying move migration to Lacros only because
    once Ash is restarted and Ash is already in migration flow, there is no
    `Profile` instance to easily check if LacrosOnly is enabled or not.
    
    Submit only after crrev.com/c/3647228 is landed and LKGM is updated.
    
    tast run $DUT lacros.Migrate.* with crrev.com/c/3647228.
    
    Test: unit_tests, brower_tests
    Bug: 1328643
    Change-Id: Id0fb9f5aae12636081fe6b71b6a245d2fe1e900c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3659825
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012261}

--- chromeos/dbus/session_manager/session_manager_client.cc
+++ chromeos/dbus/session_manager/session_manager_client.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/writable_shared_memory_region.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/unguessable_token.h"
 #include "chromeos/dbus/common/blocking_method_caller.h"
@@ -404,18 +405,24 @@ class SessionManagerClientImpl : public SessionManagerClient {
   }
 
   bool RequestBrowserDataMigration(
-      const cryptohome::AccountIdentifier& cryptohome_id) override {
+      const cryptohome::AccountIdentifier& cryptohome_id,
+      const bool is_move) override {
     dbus::MethodCall method_call(
         login_manager::kSessionManagerInterface,
         login_manager::kSessionManagerStartBrowserDataMigration);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString(cryptohome_id.account_id());
+    writer.AppendBool(is_move);
     dbus::ScopedDBusError error;
     std::unique_ptr<dbus::Response> response =
         blocking_method_caller_->CallMethodAndBlockWithError(&method_call,
                                                              &error);
     if (!response) {
-      LOG(ERROR) << "RequestBrowserDataMigration failed.";
+      LOG(ERROR) << "RequestBrowserDataMigration failed"
+                 << (error.is_set()
+                         ? base::StringPrintf(" :%s:%s", error.name(),
+                                              error.message())
+                         : ".");
       return false;
     }
 
