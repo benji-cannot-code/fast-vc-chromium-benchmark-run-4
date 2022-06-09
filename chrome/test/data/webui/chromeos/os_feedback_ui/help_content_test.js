3792d9641edd65dbacb4ccd412d2ec43a877f3f2
Longbo Wei <longbowei@google.com>
2022-06-09 17:45:24 +0000

    Feedback: Finalize string > search page > topHelpContent
     * Add screenshot and hash for topHelpContent string.
     * Screenshot: http://screen/5UhgQ4ngSAUpGca
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I91ff80bacecb2b665515183388491487785dd3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696419
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012594}

--- chrome/test/data/webui/chromeos/os_feedback_ui/help_content_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/help_content_test.js
@@ -105,7 +105,7 @@ export function helpContentTestSuite() {
     // Verify the title is in the helpContentElement.
     const title = getElement('#helpContentLabel');
     assertTrue(!!title);
-    assertEquals('Popular help content', title.textContent);
+    assertEquals('Top help content', title.textContent);
 
     verifyPopularHelpContent();
   });
