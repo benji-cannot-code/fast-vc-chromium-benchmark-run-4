04923e1ad304385da542fc2020f72cf828738abf
Angela Yoeurng <yoangela@chromium.org>
2022-06-09 16:32:44 +0000

    [omnibox][search_engines] Add @Tabs data to starter pack engines.
    Bug: 1287313
    Change-Id: If06fabfa07d2c7002542a688cb80047354dbc358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696180
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012545}

--- components/omnibox/browser/builtin_provider_unittest.cc
+++ components/omnibox/browser/builtin_provider_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/test_scheme_classifier.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/omnibox_focus_type.h"
+#include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_starter_pack_data.h"
@@ -348,9 +349,11 @@ TEST_F(BuiltinProviderTest, StarterPack) {
       GURL(TemplateURLStarterPackData::bookmarks.destination_url);
   const GURL kHistoryUrl =
       GURL(TemplateURLStarterPackData::history.destination_url);
+  const GURL kTabsUrl = GURL(TemplateURLStarterPackData::tabs.destination_url);
 
   const std::u16string kBookmarksKeyword = u"@bookmarks";
   const std::u16string kHistoryKeyword = u"@history";
+  const std::u16string kTabsKeyword = u"@tabs";
 
   // Populate template URL with starter pack entries
   std::vector<std::unique_ptr<TemplateURLData>> turls =
@@ -369,9 +372,10 @@ TEST_F(BuiltinProviderTest, StarterPack) {
       {u"history", {}},
       {u"@historyasdjflk", {}},
       {u"@bookmarksasld", {}},
+      {u"tabs", {}},
 
       // Typing '@' should give all the starter pack suggestions.
-      {u"@", {kBookmarksUrl, kHistoryUrl}},
+      {u"@", {kBookmarksUrl, kHistoryUrl, kTabsUrl}},
 
       // Typing a portion of "@bookmarks" should give the bookmarks suggestion.
       {kBookmarksKeyword.substr(0, 3), {kBookmarksUrl}},
@@ -380,6 +384,10 @@ TEST_F(BuiltinProviderTest, StarterPack) {
       // Typing a portion of "@history" should give the default urls.
       {kHistoryKeyword.substr(0, 3), {kHistoryUrl}},
       {kHistoryKeyword, {kHistoryUrl}},
+
+      // Typing a portion of "@tabs" should give the default urls.
+      {kTabsKeyword.substr(0, 3), {kTabsUrl}},
+      {kTabsKeyword, {kTabsUrl}},
   };
 
   RunTest(typing_scheme_cases, std::size(typing_scheme_cases));
--- components/search_engines/template_url_starter_pack_data.cc
+++ components/search_engines/template_url_starter_pack_data.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace TemplateURLStarterPackData {
 
-const int kCurrentDataVersion = 1;
+const int kCurrentDataVersion = 2;
 
 const StarterPackEngine bookmarks = {
     .name_message_id = IDS_SEARCH_ENGINES_STARTER_PACK_BOOKMARKS_NAME,
@@ -36,9 +36,23 @@ const StarterPackEngine history = {
     .type = SEARCH_ENGINE_STARTER_PACK_HISTORY,
 };
 
+const StarterPackEngine tabs = {
+    .name_message_id = IDS_SEARCH_ENGINES_STARTER_PACK_TABS_NAME,
+    .keyword_message_id = IDS_SEARCH_ENGINES_STARTER_PACK_TABS_KEYWORD,
+    .favicon_url = nullptr,
+    // This search_url and destination_url are placeholder URLs to make
+    // templateURL happy.  chrome://tabs does not currently exist and the tab
+    // search engine will only provide suggestions from the OpenTabProvider.
+    .search_url = "chrome://tabs/?q={searchTerms}",
+    .destination_url = "chrome://tabs",
+    .id = StarterPackID::kTabs,
+    .type = SEARCH_ENGINE_STARTER_PACK_TABS,
+};
+
 const StarterPackEngine* engines[] = {
     &bookmarks,
     &history,
+    &tabs,
 };
 
 int GetDataVersion() {
