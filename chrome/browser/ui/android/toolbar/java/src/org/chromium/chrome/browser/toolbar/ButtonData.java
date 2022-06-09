a66f348f5dec3ad826f6dfdb26164278f2088fe8
Shakti Sahu <shaktisahu@chromium.org>
2022-06-09 00:21:11 +0000

    Contextual Page Actions: Implemented ContextualPageActionController
    This CL adds the ContextualPageActionsController that contains the
    initialization logic. It is owned by RootUiCoordinator and registers
    itself as an observer for the segmentation platform. When new actions
    are available, it invokes AdaptiveToolbarButtonController that takes
    care of setting the right ButtonData to be shown on next get() call from
    the optional toolbar button. Added a concept of isDynamicAction() to
    differentiate between the existing magic toolbar static buttons from
    these dynamic buttons that are shown for only one page load event.
    
    Bug: 1322001
    Change-Id: I44ee20dff28bcf085f3f49139ba1f4db499a8635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674728
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012247}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ButtonData.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ButtonData.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 
+import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarFeatures;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarFeatures.AdaptiveToolbarButtonVariant;
 import org.chromium.chrome.browser.user_education.IPHCommandBuilder;
 
@@ -50,6 +51,7 @@ public interface ButtonData {
         private final IPHCommandBuilder mIPHCommandBuilder;
         @AdaptiveToolbarButtonVariant
         private final int mButtonVariant;
+        private final boolean mIsDynamicAction;
 
         public ButtonSpec(@NonNull Drawable drawable, @NonNull View.OnClickListener onClickListener,
                 @Nullable View.OnLongClickListener onLongClickListener, int contentDescriptionResId,
@@ -62,6 +64,7 @@ public interface ButtonData {
             mSupportsTinting = supportsTinting;
             mIPHCommandBuilder = iphCommandBuilder;
             mButtonVariant = buttonVariant;
+            mIsDynamicAction = AdaptiveToolbarFeatures.isDynamicAction(mButtonVariant);
         }
 
         /** Returns the {@link Drawable} for the button icon. */
@@ -107,6 +110,11 @@ public interface ButtonData {
         @AdaptiveToolbarButtonVariant
         public int getButtonVariant() {
             return mButtonVariant;
+        }
+
+        /** Returns {@code true} if the button is a contextual page action. False otherwise. */
+        public boolean isDynamicAction() {
+            return mIsDynamicAction;
         }
     }
 }
