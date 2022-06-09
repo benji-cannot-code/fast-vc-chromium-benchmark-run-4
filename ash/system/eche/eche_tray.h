7ffdbfe12e39a44e67cdc7b3aef7970be79a76e4
Abbas Nayebi <nayebi@google.com>
2022-06-09 20:19:02 +0000

    [eche] Making the header always be shown.
    Also, a testing macro is added for easier testing of the bubble window.
    
    Bug: b/235440652
    Change-Id: Ied2ab2a94c50cb96de8040a537fd45efb3357a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697369
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Abbas Nayebi <nayebi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012673}

--- ash/system/eche/eche_tray.h
+++ ash/system/eche/eche_tray.h
@@ -92,6 +92,7 @@ class ASH_EXPORT EcheTray : public TrayBackgroundView,
   bool PerformAction(const ui::Event& event) override;
   TrayBubbleView* GetBubbleView() override;
   views::Widget* GetBubbleWidget() const override;
+  void OnVirtualKeyboardVisibilityChanged() override;
 
   // TrayBubbleView::Delegate:
   std::u16string GetAccessibleNameForBubble() override;
