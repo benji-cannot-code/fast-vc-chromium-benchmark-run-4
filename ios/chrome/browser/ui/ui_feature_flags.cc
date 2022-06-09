2950678249f2bce6e39d19ee2b86da0af98ba7f5
Elmehdi Rahmaoui <erahmaoui@google.com>
2022-06-09 18:18:36 +0000

    [ios] Create flag for enabling Apple Calendar for experience kit
    Bug: 1318762
    Change-Id: I6f5f55b7156bd13fcfb5dc44153af08d3b40ae0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698403
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012616}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -76,3 +76,6 @@ const base::Feature kUseSFSymbols{"UseSFSymbols",
 
 const base::Feature kCalendarExperienceKit{"CalendarExperienceKit",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kEnableExpKitAppleCalendar{
+    "EnableExpKitAppleCalendar", base::FEATURE_DISABLED_BY_DEFAULT};
