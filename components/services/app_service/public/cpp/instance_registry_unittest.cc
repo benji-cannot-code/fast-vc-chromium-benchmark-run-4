375685dec68dca2e372920a701762f5353fb5d98
Eric Willigers <ericwilligers@chromium.org>
2022-06-09 03:55:16 +0000

    Add instance_registry() method to Observer
    Derived classes no longer need to keep their own copy
    of the InstanceRegistry pointer.
    
    Change-Id: I5fcb21b616fa325e13e77b7036570f5ad6c75702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695721
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012331}

--- components/services/app_service/public/cpp/instance_registry_unittest.cc
+++ components/services/app_service/public/cpp/instance_registry_unittest.cc
@@ -88,9 +88,7 @@ class InstanceRegistryTest : public testing::Test,
 class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
  public:
   explicit InstanceRecursiveObserver(apps::InstanceRegistry* instance_registry)
-      : instance_registry_(instance_registry) {
-    Observe(instance_registry);
-  }
+      : apps::InstanceRegistry::Observer(instance_registry) {}
 
   ~InstanceRecursiveObserver() override = default;
 
@@ -125,7 +123,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
   // apps::InstanceRegistry::Observer overrides.
   void OnInstanceUpdate(const apps::InstanceUpdate& outer) override {
     int num_instance = 0;
-    instance_registry_->ForEachInstance(
+    instance_registry()->ForEachInstance(
         [&outer, &num_instance](const apps::InstanceUpdate& inner) {
           if (outer.InstanceId() == inner.InstanceId()) {
             ExpectEq(outer, inner);
@@ -133,12 +131,12 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
           num_instance++;
         });
 
-    EXPECT_TRUE(instance_registry_->ForOneInstance(
+    EXPECT_TRUE(instance_registry()->ForOneInstance(
         outer.InstanceId(), [&outer](const apps::InstanceUpdate& inner) {
           ExpectEq(outer, inner);
         }));
 
-    EXPECT_TRUE(instance_registry_->ForInstancesWithWindow(
+    EXPECT_TRUE(instance_registry()->ForInstancesWithWindow(
         outer.Window(), [&outer](const apps::InstanceUpdate& inner) {
           if (outer.InstanceId() == inner.InstanceId()) {
             ExpectEq(outer, inner);
@@ -157,7 +155,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
         super_recursive_instance_params_.pop_back();
         break;
       }
-      instance_registry_->CreateOrUpdateInstance(std::move(*params));
+      instance_registry()->CreateOrUpdateInstance(std::move(*params));
       super_recursive_instance_params_.pop_back();
     }
 
@@ -169,7 +167,7 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
         super_recursive_instances_.pop_back();
         break;
       }
-      instance_registry_->OnInstance(std::move(instance));
+      instance_registry()->OnInstance(std::move(instance));
       super_recursive_instances_.pop_back();
     }
 
@@ -192,7 +190,6 @@ class InstanceRecursiveObserver : public apps::InstanceRegistry::Observer {
     EXPECT_EQ(outer.BrowserContext(), inner.BrowserContext());
   }
 
-  apps::InstanceRegistry* instance_registry_ = nullptr;
   int expected_num_instances_ = -1;
   int num_instances_seen_on_instance_update_ = 0;
 
