423e1568d1d2f3e32672862cb4c7c739e1f57280
Maxim Kolosovskiy <kolos@chromium.org>
2022-06-09 09:51:04 +0000

    [Passwords] Deprecate PasswordManager.ButtonTitlePerformance.* metrics
    Bug: 967496
    Change-Id: I1154292f8c9fb6652fdad69055ce166ede9a2b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689669
    Auto-Submit: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012414}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -2343,13 +2343,9 @@ ButtonTitleList GetButtonTitles(const WebFormElement& web_form,
   if (web_form.IsNull()) {
     const WebElement& body = document.Body();
     if (!body.IsNull()) {
-      SCOPED_UMA_HISTOGRAM_TIMER(
-          "PasswordManager.ButtonTitlePerformance.NoFormTag");
       button_titles = InferButtonTitlesForForm(body);
     }
   } else {
-    SCOPED_UMA_HISTOGRAM_TIMER(
-        "PasswordManager.ButtonTitlePerformance.HasFormTag");
     button_titles = InferButtonTitlesForForm(web_form);
   }
   form_position->second = std::move(button_titles);
