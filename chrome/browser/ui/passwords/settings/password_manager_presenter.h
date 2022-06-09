ee2624686c22a918e20291833e453b47a5c854e7
Viktor Semeniuk <vsemeniuk@google.com>
2022-06-09 16:25:24 +0000

    Simplifying bulk passwords moving into account store
    This change is supposed to make easier for a caller to move passwords in
    bulk to account storage.
    
    Bug: 1201643
    Change-Id: I591a2b4ae24cb463a577c186456251e03a3e0519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695411
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012539}

--- chrome/browser/ui/passwords/settings/password_manager_presenter.h
+++ chrome/browser/ui/passwords/settings/password_manager_presenter.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace password_manager {
-class MovePasswordToAccountStoreHelper;
 class PasswordManagerClient;
 struct PasswordForm;
 }  // namespace password_manager
@@ -144,9 +143,6 @@ class PasswordManagerPresenter
       std::map<std::string,
                std::vector<std::unique_ptr<password_manager::PasswordForm>>>;
 
-  using MovePasswordToAccountStoreHelperList = std::list<
-      std::unique_ptr<password_manager::MovePasswordToAccountStoreHelper>>;
-
   // Attempts to remove the entries corresponding to |index| from |form_map|.
   // This will also add a corresponding undo operation to |undo_manager_|.
   // Returns whether removing the entry succeeded.
@@ -175,11 +171,6 @@ class PasswordManagerPresenter
   void SetPasswordList();
   void SetPasswordExceptionList();
 
-  // Called when the helper pointed by |done_helper_it| has finished the moving
-  // task. Removes it from |move_to_account_helpers_|.
-  void OnMovePasswordToAccountCompleted(
-      MovePasswordToAccountStoreHelperList::iterator done_helper_it);
-
   PasswordFormMap password_map_;
   PasswordFormMap exception_map_;
 
@@ -190,9 +181,6 @@ class PasswordManagerPresenter
 
   // UI view that owns this presenter.
   raw_ptr<PasswordUIView> password_view_;
-
-  // Contains the helpers currently executing moving tasks.
-  MovePasswordToAccountStoreHelperList move_to_account_helpers_;
 
   base::WeakPtrFactory<PasswordManagerPresenter> weak_ptr_factory_{this};
 };
