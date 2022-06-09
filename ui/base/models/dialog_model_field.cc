4bbd967dca32d5f5e093f5f9f823b6414612dd66
Olesia Marukhno <olesiamarukhno@google.com>
2022-06-09 12:53:32 +0000

    Add support for accessible name for StyledLabel links
    Add support to set accessible name for link range in StyledLabel and
    support to create links through DialogModel with accessible name.
    Use this for privacy sandbox notice bubble.
    
    Bug: 1321587
    Change-Id: I38f62e4262541d6dafd61410f2c1dceee65a93a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695180
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012453}

--- ui/base/models/dialog_model_field.cc
+++ ui/base/models/dialog_model_field.cc
@@ -11,13 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-DialogModelLabel::Link::Link(int message_id, Callback callback)
-    : message_id(message_id), callback(std::move(callback)) {}
-DialogModelLabel::Link::Link(int message_id, base::RepeatingClosure closure)
+DialogModelLabel::Link::Link(int message_id,
+                             Callback callback,
+                             std::u16string accessible_name)
+    : message_id(message_id),
+      callback(std::move(callback)),
+      accessible_name(accessible_name) {}
+DialogModelLabel::Link::Link(int message_id,
+                             base::RepeatingClosure closure,
+                             std::u16string accessible_name)
     : Link(message_id,
            base::BindRepeating([](base::RepeatingClosure closure,
                                   const Event& event) { closure.Run(); },
-                               std::move(closure))) {}
+                               std::move(closure)),
+           accessible_name) {}
 DialogModelLabel::Link::Link(const Link&) = default;
 DialogModelLabel::Link::~Link() = default;
 
