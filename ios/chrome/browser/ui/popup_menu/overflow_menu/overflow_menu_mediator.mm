1ba180707832a0068540ed98e7e275135c1434c4
Gauthier Ambard <gambard@chromium.org>
2022-06-09 15:58:37 +0000

    [iOS] Change "Bookmark" string in activity
    This CL changes the string used to bookmark a page from the share menu
    based on the feature flag.
    
    Bug: 1329496
    Change-Id: Iee29137964f26a86f3640c6ca63de297a1768857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698407
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012526}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.h"
 
 #import "base/ios/ios_util.h"
-#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -575,16 +574,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
       self.followAction = action;
     }
 
-    std::string label_variant = base::GetFieldTrialParamValueByFeature(
-        kBookmarkString, kPopupMenuBookmarkStringParamName);
-    NSInteger addBookmarkStringID = IDS_IOS_TOOLS_MENU_BOOKMARK;
-    if (label_variant == kPopupMenuBookmarkStringParamAddABookmark) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_ADD_A_BOOKMARK;
-    } else if (label_variant == kPopupMenuBookmarkStringParamAddToBookmarks) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_ADD_TO_BOOKMARKS;
-    } else if (label_variant == kPopupMenuBookmarkStringParamBookmarkThisPage) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_BOOKMARK_THIS_PAGE;
-    }
+    NSInteger addBookmarkStringID = GetBookmarkStringID();
 
     self.addBookmarkAction =
         CreateOverflowMenuAction(addBookmarkStringID, kAddBookmarkActionSymbol,
@@ -706,16 +696,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
       self.followAction = action;
     }
 
-    std::string label_variant = base::GetFieldTrialParamValueByFeature(
-        kBookmarkString, kPopupMenuBookmarkStringParamName);
-    NSInteger addBookmarkStringID = IDS_IOS_TOOLS_MENU_BOOKMARK;
-    if (label_variant == kPopupMenuBookmarkStringParamAddABookmark) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_ADD_A_BOOKMARK;
-    } else if (label_variant == kPopupMenuBookmarkStringParamAddToBookmarks) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_ADD_TO_BOOKMARKS;
-    } else if (label_variant == kPopupMenuBookmarkStringParamBookmarkThisPage) {
-      addBookmarkStringID = IDS_IOS_TOOLS_MENU_BOOKMARK_THIS_PAGE;
-    }
+    NSInteger addBookmarkStringID = GetBookmarkStringID();
 
     self.addBookmarkAction = CreateOverflowMenuAction(
         addBookmarkStringID, @"overflow_menu_action_bookmark",
--- ios/chrome/browser/ui/popup_menu/public/features.mm
+++ ios/chrome/browser/ui/popup_menu/public/features.mm
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/popup_menu/public/features.h"
 
+#include "base/metrics/field_trial_params.h"
+#include "ios/chrome/grit/ios_strings.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -20,3 +23,16 @@ const char kPopupMenuBookmarkStringParamBookmarkThisPage[] =
 
 const base::Feature kBookmarkString{"BookmarkString",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
+
+int GetBookmarkStringID() {
+  std::string label_variant = base::GetFieldTrialParamValueByFeature(
+      kBookmarkString, kPopupMenuBookmarkStringParamName);
+  if (label_variant == kPopupMenuBookmarkStringParamAddABookmark) {
+    return IDS_IOS_TOOLS_MENU_ADD_A_BOOKMARK;
+  } else if (label_variant == kPopupMenuBookmarkStringParamAddToBookmarks) {
+    return IDS_IOS_TOOLS_MENU_ADD_TO_BOOKMARKS;
+  } else if (label_variant == kPopupMenuBookmarkStringParamBookmarkThisPage) {
+    return IDS_IOS_TOOLS_MENU_BOOKMARK_THIS_PAGE;
+  }
+  return IDS_IOS_TOOLS_MENU_BOOKMARK;
+}
