159cfa9af99fb993a748d1e700c8a23271ece957
Sherri Lin <sherrilin@google.com>
2022-06-09 17:11:26 +0000

    enterprise: Fix container ID of Kiosk Instruction Bubble
    Kiosk instruction bubble is not in the same
    window container as the anchor. Kiosk instruction bubble inherits
    ShelfBubble, which assumes that the anchors would be always in
    kShellWindowId_SettingBubbleContainer. However, the anchor of Kiosk
    instruction bubble is the kiosk app menu button and it's in
    kShellWindowId_LockScreenRelatedContainersContainer.
    
    Bug: b/234773714
    Change-Id: I35a0bcb4acdf2d67524b8ff84ec93cd9b0c8c4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695212
    Commit-Queue: Sherri Lin <sherrilin@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012566}

--- ash/shelf/kiosk_app_instruction_bubble.cc
+++ ash/shelf/kiosk_app_instruction_bubble.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/kiosk_app_instruction_bubble.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/public/cpp/shell_window_ids.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/tray/tray_popup_utils.h"
@@ -66,6 +67,14 @@ KioskAppInstructionBubble::KioskAppInstructionBubble(views::View* anchor,
   title_->SetText(l10n_util::GetStringUTF16(IDS_SHELF_KIOSK_APP_INSTRUCTION));
   title_->SetMultiLine(true);
   title_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+  // TODO(crbug.com/1334998): We should inherit directly from
+  // views::BubbleDialogDelegateView as ShelfBubble by default puts the bubble
+  // in the container of kShellWindowId_SettingBubbleContainer.
+
+  set_parent_window(
+      anchor_widget()->GetNativeWindow()->GetRootWindow()->GetChildById(
+          kShellWindowId_LockScreenRelatedContainersContainer));
 
   CreateBubble();
 
