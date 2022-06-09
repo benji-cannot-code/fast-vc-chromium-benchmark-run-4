b51569aed9ce1d00adc9c39b8a465b1662589b87
Ashley Prasad <ashleydp@google.com>
2022-06-09 19:02:48 +0000

    ksv: use GetBackgroundColorInMode
    - Update code to set color using GetBackgroundColorInMode ensures
    color matches UX specs. Previously GetBaseLayer returned the expected
    background and frame color. Currently it uses the wallpaper to change
    the background color causing color mismatch with other elements
    (see screenshot).
    - Update test to match expectation.
    
    Screenshot: http://screen/BFec8RLPoa6yZ4q
    
    Bug: None
    Test: Deploy to DUT and validate background color match color mode.
    Change-Id: Ib10112fd1312d547b24949c86aba1070dc6254c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691819
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012643}

--- ash/shortcut_viewer/views/keyboard_shortcut_view.h
+++ ash/shortcut_viewer/views/keyboard_shortcut_view.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
-#include "ash/public/cpp/style/color_provider.h"
 #include "ash/search_box/search_box_view_delegate.h"
+#include "ash/style/ash_color_provider.h"
 #include "base/timer/timer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/widget_delegate.h"
@@ -142,7 +142,7 @@ class KeyboardShortcutView : public views::WidgetDelegateView,
   // initialization of background panes in the following frame.
   bool did_first_paint_ = false;
 
-  ash::ColorProvider* color_provider_;  // Not owned.
+  ash::AshColorProvider* color_provider_;
 
   base::WeakPtrFactory<KeyboardShortcutView> weak_factory_{this};
 };
