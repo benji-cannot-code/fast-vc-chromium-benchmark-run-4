3610b00d3b1f44bee8f4c71b6ebc2ad2b1535611
Jun Zou <junzou@chromium.org>
2022-06-09 19:11:54 +0000

    Add entity collection UMA metrics for Omnibox Journey chips.
    Bug: 1322690
    Change-Id: I31de54cc1072f3cc060028e16db0cba8bdc2f2b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690385
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012645}

--- components/omnibox/browser/actions/history_clusters_action.cc
+++ components/omnibox/browser/actions/history_clusters_action.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/actions/omnibox_action.h"
 #include "components/omnibox/browser/actions/omnibox_action_concepts.h"
 #include "components/omnibox/browser/autocomplete_result.h"
+#include "components/optimization_guide/core/entity_metadata.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
 
@@ -50,6 +51,16 @@ int TopRelevance(const AutocompleteResult& result, bool search) {
                           : 0;
              })
       ->relevance;
+}
+
+// Record the entity collection level CTR metric for the journey chip.
+void RecordEntityCollectionCtrForJourney(const std::string& collection_label,
+                                         bool executed) {
+  // Append an entity collection label.
+  std::string uma_metric_name = base::StringPrintf(
+      "Omnibox.SuggestionUsed.ResumeJourney.PageEntityCollection.%s.CTR",
+      collection_label.c_str());
+  base::UmaHistogramBoolean(uma_metric_name, executed);
 }
 
 }  // namespace
@@ -86,6 +97,27 @@ void HistoryClustersAction::RecordActionShown(size_t position,
 
   base::UmaHistogramBoolean("Omnibox.SuggestionUsed.ResumeJourneyCTR",
                             executed);
+
+  if (matched_keyword_data_.entity_collections.empty()) {
+    return;
+  }
+
+  // Record entity collection UMA metrics.
+  const auto& collection_str = matched_keyword_data_.entity_collections.front();
+  const optimization_guide::PageEntityCollection collection =
+      optimization_guide::GetPageEntityCollectionForString(collection_str);
+
+  base::UmaHistogramEnumeration(
+      "Omnibox.ResumeJourneyShown.PageEntityCollection", collection);
+  if (executed) {
+    base::UmaHistogramEnumeration(
+        "Omnibox.SuggestionUsed.ResumeJourney.PageEntityCollection",
+        collection);
+  }
+
+  const auto collection_label =
+      optimization_guide::GetPageEntityCollectionLabel(collection_str);
+  RecordEntityCollectionCtrForJourney(collection_label, executed);
 }
 
 int32_t HistoryClustersAction::GetID() const {
