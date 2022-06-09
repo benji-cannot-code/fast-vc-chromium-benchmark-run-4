6d72443a760f94f6eb37eda3c78fe670ab1f954a
Andres Calderon Jaramillo <andrescj@chromium.org>
2022-06-09 22:13:04 +0000

    StableVideoDecoderService: Implement Decode().
    This CL implements the StableVideoDecoderService::Decode() method which
    simply forwards that call to the underlying mojom::VideoDecoder.
    
    The unit tests are expanded to cover the new code.
    
    Bug: b:195769334
    Test: ./media_unittests --gtest_filter="StableVideoDecoderServiceTest.*" in amd64-generic VM.
    Change-Id: Ie54beeee6e730b75a6c3db7655f9e662b23ec1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3681200
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012731}

--- media/mojo/services/stable_video_decoder_service.cc
+++ media/mojo/services/stable_video_decoder_service.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/mojo/services/stable_video_decoder_service.h"
 
+#include "media/mojo/common/media_type_converters.h"
+
 namespace media {
 
 StableVideoDecoderService::StableVideoDecoderService(
@@ -73,6 +75,7 @@ void StableVideoDecoderService::Initialize(
     InitializeCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!video_decoder_client_receiver_.is_bound()) {
+    DVLOG(2) << __func__ << " Construct() must be called first";
     std::move(callback).Run(DecoderStatus::Codes::kFailedToCreateDecoder,
                             /*needs_bitstream_conversion=*/false,
                             /*max_decode_requests=*/1,
@@ -100,7 +103,17 @@ void StableVideoDecoderService::Decode(
     const scoped_refptr<DecoderBuffer>& buffer,
     DecodeCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  NOTIMPLEMENTED();
+  if (!video_decoder_client_receiver_.is_bound()) {
+    DVLOG(2) << __func__ << " Construct() must be called first";
+    std::move(callback).Run(DecoderStatus::Codes::kFailedToCreateDecoder);
+    return;
+  }
+
+  CHECK(buffer);
+  mojom::DecoderBufferPtr mojo_buffer = mojom::DecoderBuffer::From(*buffer);
+  CHECK(mojo_buffer);
+  dst_video_decoder_remote_->Decode(std::move(mojo_buffer),
+                                    std::move(callback));
 }
 
 void StableVideoDecoderService::Reset(ResetCallback callback) {
