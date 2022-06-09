8be92a39fa05b7c8b2bebafe22afcaac316b69d0
Mustafa Emre Acer <meacer@chromium.org>
2022-06-09 18:16:42 +0000

    [ios] Move HTTPS Only Code to ios/chrome/browser/https_upgrades
    By now the bulk of the HTTPS upgrade code is under its own directory
    at  ios/chrome/browser/https_upgrades. Move the test and app interface
    code as well.
    
    Bug: 1195440
    Change-Id: I45f493544471b2ddff857057ee2ece605a871679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3693928
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012614}

--- ios/chrome/browser/https_upgrades/https_only_mode_app_interface_stub.mm
+++ ios/chrome/browser/https_upgrades/https_only_mode_app_interface_stub.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/web/https_only_mode_app_interface.h"
+#import "ios/chrome/browser/https_upgrades/https_only_mode_app_interface.h"
 
 #import <TestLib/EarlGreyImpl/EarlGrey.h>
 
