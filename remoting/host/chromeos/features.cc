2ef0afd725fe29ea903d5e33d34763604865bc9f
Jeroen Dhollander <jeroendh@google.com>
2022-06-09 16:16:49 +0000

    Enable CRD multi monitor support by default
    This is in preparation for the launch in M104
    (launch tracking bug is crbug.com/1322927).
    
    Fixed: b/235448234
    Test: Manually launched ChromeOS and verified the feature was working
    Change-Id: I9818efce6f90abbf16626ca6c670765831fe75be
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695329
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012535}

--- remoting/host/chromeos/features.cc
+++ remoting/host/chromeos/features.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting::features {
 
-const base::Feature kEnableMultiMonitorsInCrd{
-    "EnableMultiMonitorsInCrd", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kEnableMultiMonitorsInCrd{"EnableMultiMonitorsInCrd",
+                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace remoting::features
