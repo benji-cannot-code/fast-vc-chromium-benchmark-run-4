47b1d9d7f5984edc6e3c224cad279565f523aa42
Yuta Hijikata <ythjkt@google.com>
2022-06-09 06:10:34 +0000

    Enable move migration if LacrosOnly.
    Currently migraiton is done with `MoveMigrator` only if
    `kLacrosMoveProfileMigration` feature is enabled. This patch makes it so
    that move migration is enabled by default if LacrosOnly is also enabled.
    
    manual test to see if 1. enabling Lacros triggers copy migration and 2.
    enabling LacrosOnly enables move migration. Both done with flags.
    
    Test: unit_tests, tast run $DUT lacros.Migrate.*,
    Bug: 1328643
    Change-Id: I5075b07a77bfed4b7a2b9cd41c19248fa97badfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663546
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012357}

--- chrome/browser/ash/crosapi/browser_util.h
+++ chrome/browser/ash/crosapi/browser_util.h
@@ -300,6 +300,10 @@ ComponentInfo GetLacrosComponentInfo();
 version_info::Channel GetLacrosSelectionUpdateChannel(
     LacrosSelection selection);
 
+// Exposed for testing. Sets lacros-availability cache for testing.
+void SetCachedLacrosAvailabilityForTesting(
+    LacrosAvailability lacros_availability);
+
 // Exposed for testing. Returns the lacros integration suggested by the policy
 // lacros-availability, modified by Finch flags and user flags as appropriate.
 LacrosAvailability GetCachedLacrosAvailabilityForTesting();
--- chromeos/dbus/session_manager/fake_session_manager_client.cc
+++ chromeos/dbus/session_manager/fake_session_manager_client.cc
@@ -412,7 +412,7 @@ bool FakeSessionManagerClient::RequestBrowserDataMigration(
     const cryptohome::AccountIdentifier& cryptohome_id,
     const bool is_move) {
   request_browser_data_migration_called_ = true;
-
+  request_browser_data_migration_for_move_called_ = is_move;
   return true;
 }
 
