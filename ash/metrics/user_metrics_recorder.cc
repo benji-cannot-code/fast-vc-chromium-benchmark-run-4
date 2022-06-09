2466ca61d1855a5aa5d6f14d2c8c7043b8df2fd2
Judy Wang <shidi@chromium.org>
2022-06-09 00:15:25 +0000

    ChromeOS: Add Floated State and Float Event.
    Introducing float state and float event.
    
    Test: manual
    Fixed: 1329965
    Bug: 1329965
    Change-Id: I50a0aa576c49d192a7a752b4cbce547ea6b2e15f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682758
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Judy Wang <shidi@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012242}

--- ash/metrics/user_metrics_recorder.cc
+++ ash/metrics/user_metrics_recorder.cc
@@ -45,6 +45,7 @@ enum ActiveWindowStateType {
   ACTIVE_WINDOW_STATE_TYPE_PINNED,
   ACTIVE_WINDOW_STATE_TYPE_TRUSTED_PINNED,
   ACTIVE_WINDOW_STATE_TYPE_PIP,
+  ACTIVE_WINDOW_STATE_TYPE_FLOATED,
   ACTIVE_WINDOW_STATE_TYPE_COUNT,
 };
 
@@ -72,6 +73,9 @@ ActiveWindowStateType GetActiveWindowState() {
         break;
       case WindowStateType::kPip:
         active_window_state_type = ACTIVE_WINDOW_STATE_TYPE_PIP;
+        break;
+      case WindowStateType::kFloated:
+        active_window_state_type = ACTIVE_WINDOW_STATE_TYPE_FLOATED;
         break;
       case WindowStateType::kDefault:
       case WindowStateType::kNormal:
--- ash/wm/default_state.cc
+++ ash/wm/default_state.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/screen_util.h"
 #include "ash/shell.h"
 #include "ash/wm/desks/desks_util.h"
+#include "ash/wm/float/float_controller.h"
 #include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_metrics_controller.h"
@@ -22,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/wm_event.h"
 #include "ash/wm/workspace_controller.h"
 #include "base/bind.h"
+#include "base/check.h"
+#include "base/check_op.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "chromeos/ui/base/window_state_type.h"
@@ -435,6 +438,20 @@ void DefaultState::EnterToNextState(WindowState* window_state,
   window_state->UpdateWindowPropertiesFromStateType();
   window_state->NotifyPreStateTypeChange(previous_state_type);
 
+  auto* const float_controller = Shell::Get()->float_controller();
+  auto* window = window_state->window();
+  if (state_type_ == WindowStateType::kFloated) {
+    DCHECK_EQ(next_state_type, WindowStateType::kFloated);
+    // Add window to float container.
+    float_controller->Float(window);
+  }
+
+  // Unfloat floated window when exiting float state to another state.
+  if (previous_state_type == WindowStateType::kFloated) {
+    // Remove float window from float container.
+    float_controller->Unfloat(window);
+  }
+
   // Don't update the window if the window is detached from parent.
   // This can happen during dragging.
   // TODO(oshima): This was added for DOCKED windows. Investigate if
@@ -574,6 +591,11 @@ void DefaultState::UpdateBoundsFromState(WindowState* window_state,
       break;
     case WindowStateType::kInactive:
     case WindowStateType::kAutoPositioned:
+    case WindowStateType::kFloated:
+      // TODO(crbug.com/1331078): Handle Float Size and Position requirement.
+      // Temporarily set to current bounds.
+      bounds_in_parent = window->bounds();
+      break;
     case WindowStateType::kPip:
       return;
   }
