42f0ca8ef4aa12ce72dc2828d55590839e3a1680
Finnur Thorarinsson <finnur@chromium.org>
2022-06-09 09:45:03 +0000

    [AppIdentity]: Show explanation string only if icon changes.
    Bug: 1302660
    Change-Id: I1e80ba26b77a13f49ebbab28299e3500aeb7a4f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692109
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012412}

--- chrome/browser/ui/views/web_apps/web_app_identity_update_confirmation_view.cc
+++ chrome/browser/ui/views/web_apps/web_app_identity_update_confirmation_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/web_apps/web_app_identity_update_confirmation_view.h"
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_view.h"
 #include "chrome/browser/web_applications/web_app_callback_app_identity.h"
@@ -89,8 +90,11 @@ WebAppIdentityUpdateConfirmationView::WebAppIdentityUpdateConfirmationView(
                   DISTANCE_CONTROL_LIST_VERTICAL)))
       .AddChildren(
           views::Builder<views::Label>()
+              .SetID(VIEW_ID_APP_IDENTITY_UPDATE_HEADER)
               .SetTextContext(views::style::CONTEXT_LABEL)
-              .SetText(l10n_util::GetStringUTF16(IDS_WEBAPP_UPDATE_EXPLANATION))
+              .SetText(icon_change ? l10n_util::GetStringUTF16(
+                                         IDS_WEBAPP_UPDATE_EXPLANATION)
+                                   : std::u16string())
               .SetHorizontalAlignment(gfx::ALIGN_LEFT)
               .SetMultiLine(true),
           views::Builder<views::TableLayoutView>()
