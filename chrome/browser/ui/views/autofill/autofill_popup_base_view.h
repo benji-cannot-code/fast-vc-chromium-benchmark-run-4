16ac299901ef4dde5758947e38a73ee97b70a652
Nina Satragno <nsatragno@chromium.org>
2022-06-09 14:28:17 +0000

    [autofill] End AX focus override on view close
    Under some circumstances (see bug), it's possible for another popup to
    steal focus after AutofillPopupBaseView::DoClose() is called but before
    the view is notified of the visibility change. This triggers a DCHECK on
    ViewAXPlatformNodeDelegate.
    
    This CL changes the base view to end the accessibility popup focus
    override when an AutofillPopupBaseView is closed instead of when the
    visibility change is fired, which addresses the issue.
    
    PasswordGenerationPopupViewViews::GetAccessibleNodeData needs to check
    that controller_ is not null now, since the a11y code will eventually
    call `GetAccessibleNodeData` during the DoHide function, which requires
    the pointer to the controller to be alive.
    
    Fixed: 1331005
    Change-Id: If8046a43c57ef54107451d1421d7d270f669e26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3684529
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012492}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.h
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.h
@@ -54,9 +54,6 @@ class AutofillPopupBaseView : public views::WidgetDelegateView,
   // text).
   static int GetHorizontalPadding();
 
-  // views::View:
-  void VisibilityChanged(View* starting_from, bool is_visible) override;
-
   // Notify accessibility that an item has been selected.
   void NotifyAXSelection(View*);
 
