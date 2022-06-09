8ebe040732bf53f745061c8e6ce30feedcc2f44d
Yuta Hijikata <ythjkt@google.com>
2022-06-09 07:34:44 +0000

    Distinguish completion of move migration from copy migration.
    This patch adds a new pref `kProfileMoveMigrationCompletedForUserPref`
    which has to be set on top of `kProfileMigrationCompletedForUserPref` if
    move migration is enabled. This ensures that move migration is run for
    users even for users who have gone through copy migration before.
    
    Test: unit_tests
    Bug: 1328643
    Change-Id: Ia14c5c8b01c5260196a09e76960c366fe76dff9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668492
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012375}

--- chrome/browser/ash/crosapi/browser_data_migrator.cc
+++ chrome/browser/ash/crosapi/browser_data_migrator.cc
@@ -276,8 +276,9 @@ bool BrowserDataMigratorImpl::MaybeRestartToMigrateInternal(
     crosapi::browser_util::RecordDataVer(local_state, user_id_hash,
                                          version_info::GetVersion());
 
-    crosapi::browser_util::SetProfileMigrationCompletedForUser(local_state,
-                                                               user_id_hash);
+    crosapi::browser_util::SetProfileMigrationCompletedForUser(
+        local_state, user_id_hash,
+        crosapi::browser_util::GetMigrationMode(user, policy_init_state));
     // TODO(crbug.com/1277848): Once `BrowserDataMigrator` stabilises, remove
     // this log message.
     LOG(WARNING) << "Setting migration as completed since it is a new user.";
@@ -296,7 +297,7 @@ bool BrowserDataMigratorImpl::MaybeRestartToMigrateInternal(
     return false;
   }
 
-  if (crosapi::browser_util::IsDataWipeRequired(user_id_hash)) {
+  if (crosapi::browser_util::IsDataWipeRequired(local_state, user_id_hash)) {
     // TODO(crbug.com/1277848): Once `BrowserDataMigrator` stabilises, remove
     // this log message.
     LOG(WARNING)
@@ -306,12 +307,25 @@ bool BrowserDataMigratorImpl::MaybeRestartToMigrateInternal(
     return true;
   }
 
-  if (crosapi::browser_util::IsProfileMigrationCompletedForUser(local_state,
-                                                                user_id_hash)) {
+  crosapi::browser_util::MigrationMode mode =
+      crosapi::browser_util::GetMigrationMode(user, policy_init_state);
+
+  if (crosapi::browser_util::IsProfileMigrationCompletedForUser(
+          local_state, user_id_hash, mode)) {
     // TODO(crbug.com/1277848): Once `BrowserDataMigrator` stabilises,
     // remove this log message.
     LOG(WARNING) << "Profile migration has been completed already.";
     return false;
+  }
+
+  // TODO(crbug.com/1277848): Once `BrowserDataMigrator` stabilises,
+  // remove this log message.
+  if (mode == crosapi::browser_util::MigrationMode::kMove &&
+      crosapi::browser_util::IsProfileMigrationCompletedForUser(
+          local_state, user_id_hash,
+          crosapi::browser_util::MigrationMode::kCopy)) {
+    LOG(WARNING) << "Only copy migration is marked as completed. Running "
+                    "profile move migraiton.";
   }
 
   return true;
@@ -398,7 +412,7 @@ void BrowserDataMigratorImpl::Migrate(crosapi::browser_util::MigrationMode mode,
           cancel_flag_, local_state_,
           base::BindOnce(
               &BrowserDataMigratorImpl::MigrateInternalFinishedUIThread,
-              weak_factory_.GetWeakPtr()));
+              weak_factory_.GetWeakPtr(), mode));
       break;
     case crosapi::browser_util::MigrationMode::kCopy:
       LOG(WARNING) << "Initializing CopyMigrator.";
@@ -407,7 +421,7 @@ void BrowserDataMigratorImpl::Migrate(crosapi::browser_util::MigrationMode mode,
           cancel_flag_,
           base::BindOnce(
               &BrowserDataMigratorImpl::MigrateInternalFinishedUIThread,
-              weak_factory_.GetWeakPtr()));
+              weak_factory_.GetWeakPtr(), mode));
       break;
   }
 
@@ -420,6 +434,7 @@ void BrowserDataMigratorImpl::Cancel() {
 }
 
 void BrowserDataMigratorImpl::MigrateInternalFinishedUIThread(
+    crosapi::browser_util::MigrationMode mode,
     MigrationResult result) {
   DCHECK(local_state_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -441,8 +456,8 @@ void BrowserDataMigratorImpl::MigrateInternalFinishedUIThread(
   }
 
   if (result.data_migration_result.kind == ResultKind::kSucceeded) {
-    crosapi::browser_util::SetProfileMigrationCompletedForUser(local_state_,
-                                                               user_id_hash_);
+    crosapi::browser_util::SetProfileMigrationCompletedForUser(
+        local_state_, user_id_hash_, mode);
 
     ClearMigrationAttemptCountForUser(local_state_, user_id_hash_);
   }
--- chrome/browser/ash/crosapi/browser_manager_unittest.cc
+++ chrome/browser/ash/crosapi/browser_manager_unittest.cc
@@ -139,9 +139,11 @@ class BrowserManagerTest : public testing::Test {
 TEST_F(BrowserManagerTest, LacrosKeepAlive) {
   AddRegularUser("user@test.com");
   browser_util::SetProfileMigrationCompletedForUser(
-      local_state_.Get(), ash::ProfileHelper::Get()
-                              ->GetUserByProfile(&testing_profile_)
-                              ->username_hash());
+      local_state_.Get(),
+      ash::ProfileHelper::Get()
+          ->GetUserByProfile(&testing_profile_)
+          ->username_hash(),
+      browser_util::MigrationMode::kCopy);
   EXPECT_TRUE(browser_util::IsLacrosEnabled());
   EXPECT_TRUE(browser_util::IsLacrosAllowedToLaunch());
 
@@ -183,9 +185,11 @@ TEST_F(BrowserManagerTest, LacrosKeepAlive) {
 TEST_F(BrowserManagerTest, LacrosKeepAliveReloadsWhenUpdateAvailable) {
   AddRegularUser("user@test.com");
   browser_util::SetProfileMigrationCompletedForUser(
-      local_state_.Get(), ash::ProfileHelper::Get()
-                              ->GetUserByProfile(&testing_profile_)
-                              ->username_hash());
+      local_state_.Get(),
+      ash::ProfileHelper::Get()
+          ->GetUserByProfile(&testing_profile_)
+          ->username_hash(),
+      browser_util::MigrationMode::kCopy);
   EXPECT_TRUE(browser_util::IsLacrosEnabled());
   EXPECT_TRUE(browser_util::IsLacrosAllowedToLaunch());
 
@@ -228,9 +232,11 @@ TEST_F(BrowserManagerTest, LacrosKeepAliveReloadsWhenUpdateAvailable) {
 TEST_F(BrowserManagerTest, NewWindowReloadsWhenUpdateAvailable) {
   AddRegularUser("user@test.com");
   browser_util::SetProfileMigrationCompletedForUser(
-      local_state_.Get(), ash::ProfileHelper::Get()
-                              ->GetUserByProfile(&testing_profile_)
-                              ->username_hash());
+      local_state_.Get(),
+      ash::ProfileHelper::Get()
+          ->GetUserByProfile(&testing_profile_)
+          ->username_hash(),
+      browser_util::MigrationMode::kCopy);
   EXPECT_TRUE(browser_util::IsLacrosEnabled());
   EXPECT_TRUE(browser_util::IsLacrosAllowedToLaunch());
 
@@ -265,9 +271,11 @@ TEST_F(BrowserManagerTest, NewWindowReloadsWhenUpdateAvailable) {
 TEST_F(BrowserManagerTest, LacrosKeepAliveDoesNotBlockRestart) {
   AddRegularUser("user@test.com");
   browser_util::SetProfileMigrationCompletedForUser(
-      local_state_.Get(), ash::ProfileHelper::Get()
-                              ->GetUserByProfile(&testing_profile_)
-                              ->username_hash());
+      local_state_.Get(),
+      ash::ProfileHelper::Get()
+          ->GetUserByProfile(&testing_profile_)
+          ->username_hash(),
+      browser_util::MigrationMode::kCopy);
   EXPECT_TRUE(browser_util::IsLacrosEnabled());
   EXPECT_TRUE(browser_util::IsLacrosAllowedToLaunch());
 
