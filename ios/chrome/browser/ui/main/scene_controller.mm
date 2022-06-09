70174144494ad04df279b6c5e1d1ab4e556a2785
jlebel <jlebel@chromium.org>
2022-06-09 13:51:16 +0000

    [iOS] Add AuthenticationOperationSigninOnly sign-in command
    This command triggers consistency web sign-in dialog. This is required
    for crrev.com/c/3682803.
    
    Bug: 1264471
    Change-Id: I30cb1787ba024de0b3a4f3ffec1c6e1faa9eb7cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687858
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012473}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -1577,6 +1577,12 @@ bool IsSigninForcedByPolicy() {
                                           accessPoint:command.accessPoint
                                           promoAction:command.promoAction];
       break;
+    case AuthenticationOperationSigninOnly:
+      self.signinCoordinator = [SigninCoordinator
+          consistencyPromoSigninCoordinatorWithBaseViewController:
+              baseViewController
+                                                          browser:mainBrowser];
+      break;
     case AuthenticationOperationAddAccount:
       self.signinCoordinator = [SigninCoordinator
           addAccountCoordinatorWithBaseViewController:baseViewController
