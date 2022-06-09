5d2ec9aee67042f123b3335d5e9f0c032f0f8a46
Jeffrey Young <cowmoo@google.com>
2022-06-09 01:45:13 +0000

    personalization: use h2, h3 where appropriate
    Use h2 or h3 tags where appropriate to make it easier for
    screen readers to navigate to important content in Personalization App.
    https://photos.app.goo.gl/yKFH5vQwJAqAziH28
    https://photos.app.goo.gl/Z9Vhh5J2Y4H1f3iQ7
    
    BUG=b:235380025
    
    Change-Id: Icbd80069fc9e7cf58de232e31cde3bf6ea668133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697058
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012281}

--- ash/webui/personalization_app/resources/trusted/user/user_preview_element.ts
+++ ash/webui/personalization_app/resources/trusted/user/user_preview_element.ts
@@ -89,10 +89,6 @@ export class UserPreview extends WithPersonalizationStore {
     initializeUserData(getUserProvider(), this.getStore());
   }
 
-  private onClickUserEmail_() {
-    window.open('chrome://os-settings/accountManager');
-  }
-
   private onClickUserSubpageLink_() {
     PersonalizationRouter.instance().goToRoute(Paths.USER);
   }
