68d0f78386791b18041a52d2bcbeeee0f031453b
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-06-09 13:30:30 +0000

    Dangling ptr: BiDiLineIterator
    The raw_ptr<UBiDi> is used to store ownership of an object vended by the
    ICU C library.
    
    The dangling pointer detector detects the pointer become dangling when
    calling `ubidi_close`.
    
    Solution are:
    1. Use ubidi_close(bidi_->UnWrap()); This release the raw_ptr before
       sending it toward the ICU library.
    2. Turn the raw_ptr into an unique_ptr.
    
    Bug: 1291138
    Change-Id: If15fef1d48e0676b9269490a5c305d96de119c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650764
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012465}

--- ui/gfx/bidi_line_iterator.h
+++ ui/gfx/bidi_line_iterator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_BIDI_LINE_ITERATOR_H_
 #define UI_GFX_BIDI_LINE_ITERATOR_H_
 
+#include <memory>
 #include <string>
 
 #include "base/i18n/rtl.h"
@@ -16,6 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 namespace gfx {
+
+class UBiDiDeleter {
+ public:
+  void operator()(UBiDi* ptr) { ubidi_close(ptr); }
+};
 
 // A simple wrapper class for the bidirectional iterator of ICU.
 // This class uses the bidirectional iterator of ICU to split a line of
@@ -43,7 +49,7 @@ class GFX_EXPORT BiDiLineIterator {
   void GetLogicalRun(int start, int* end, UBiDiLevel* level) const;
 
  private:
-  raw_ptr<UBiDi> bidi_;
+  std::unique_ptr<UBiDi, UBiDiDeleter> bidi_;
 };
 
 }  // namespace gfx
