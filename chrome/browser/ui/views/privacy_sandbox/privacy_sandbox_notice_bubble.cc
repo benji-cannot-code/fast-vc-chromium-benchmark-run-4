4ce693e82c46b997acec3094deef1256d2630c47
Olesia Marukhno <olesiamarukhno@google.com>
2022-06-10 09:19:14 +0000

    [privacy sandbox settings] Announce the notice bubble
    Update BubbleDialogModelHost to set the correct bubble role on Win.
    Update logic that activates the first inactive dialog to active the
    dialog instead of focusing it when it's needed.
    
    Use these new changes for PrivacySandboxNoticeBubble. Announce that the
    dialog isn't focused now and explain how to focus it.
    
    Bug: 1321587
    Change-Id: I6e3d501bbd886b1c677d73f2a1697fc07201a5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687958
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012869}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_notice_bubble.cc
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_notice_bubble.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/bubble/bubble_dialog_model_host.h"
 
 using PromptAction = PrivacySandboxService::PromptAction;
@@ -94,6 +95,7 @@ void ShowPrivacySandboxNoticeBubble(Browser* browser) {
           .SetTitle(l10n_util::GetStringUTF16(
               IDS_PRIVACY_SANDBOX_BUBBLE_NOTICE_TITLE))
           .SetInternalName(kPrivacySandboxNoticeBubbleName)
+          .SetIsAlertDialog()
           .SetIcon(ui::ImageModel::FromImageSkia(*bundle.GetImageSkiaNamed(
                        IDR_PRIVACY_SANDBOX_CONFIRMATION_BANNER)),
                    ui::ImageModel::FromImageSkia(*bundle.GetImageSkiaNamed(
@@ -135,10 +137,14 @@ void ShowPrivacySandboxNoticeBubble(Browser* browser) {
   auto bubble = std::make_unique<views::BubbleDialogModelHost>(
       std::move(dialog_model), configuration.anchor_view,
       configuration.bubble_arrow);
+  views::Widget* const widget =
+      views::BubbleDialogDelegate::CreateBubble(std::move(bubble));
   // The bubble isn't opened as a result of a user action. It shouldn't take the
-  // focus away from the current tasks. Open the bubble starting as inactive
+  // focus away from the current task. Open the bubble starting as inactive
   // also avoids unintentionally closing it due to focus loss until the user has
   // interacted with it. After a user interaction, it may be closed on focus
   // loss.
-  views::BubbleDialogDelegate::CreateBubble(std::move(bubble))->ShowInactive();
+  widget->ShowInactive();
+  widget->GetRootView()->GetViewAccessibility().AnnounceText(
+      l10n_util::GetStringUTF16(IDS_SHOW_BUBBLE_INACTIVE_DESCRIPTION));
 }
