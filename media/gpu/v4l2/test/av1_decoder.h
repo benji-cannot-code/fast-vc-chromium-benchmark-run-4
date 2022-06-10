cb2625b29be22d6ec30011c4c4b9e1f5855b44d6
Steve Cho <stevecho@google.com>
2022-06-10 18:46:43 +0000

    media/gpu/v4l2: setup V4L2 AV1 decoding frame params
    This CL prepares the following frame parameters for AV1 decoding using
    V4L2 API.
    - 5.9.14. Segmentation params syntax
    - 5.9.19. CDEF params syntax
    - 5.9.20. Loop restoration params syntax
    https://aomediacodec.github.io/av1-spec/av1-spec.pdf
    
    These parameters are needed to make VIDIOC_S_EXT_CTRLS V4L2 ioctl call.
    Another CL will follow to setup remaining frame params.
    
    Bug: b:228534730
    TEST: v4l2_stateless_decoder --video=00000527_20210205.ivf --frames=1 --v=4 on cherry/tomato using mainline
    Change-Id: I235d76804fa6b2bd2b26892f4ded106f6bb1ea61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688809
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013057}

--- media/gpu/v4l2/test/av1_decoder.h
+++ media/gpu/v4l2/test/av1_decoder.h
@@ -28,6 +28,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // base/logging.h for ChromeOS.
 #define ANALYZER_ALLOW_UNUSED(var) static_cast<void>(var);
 
+// TODO(stevecho): RESTORATION_TILESIZE_MAX in the spec is not available in the
+// AV1 uAPI. It was recommended to be added in the userspace code. If the uAPI
+// stays as it is for upstreaming, then #ifndef can be removed. If the uAPI ends
+// up adding this constant, then we can remove this define at that time.
+// https://patchwork.linuxtv.org/project/linux-media/patch/20210810220552.298140-2-daniel.almeida@collabora.com/
+#ifndef V4L2_AV1_RESTORATION_TILESIZE_MAX
+#define V4L2_AV1_RESTORATION_TILESIZE_MAX 256
+#endif
+
 namespace media {
 namespace v4l2_test {
 
