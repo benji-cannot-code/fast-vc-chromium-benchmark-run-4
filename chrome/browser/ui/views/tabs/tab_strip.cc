d764ab61d7b11bc02a1adfee3e92587c1c54d7a2
Keren Zhu <kerenzhu@chromium.org>
2022-06-10 00:16:32 +0000

    Reland "Migrate Tab Group off ThemeProvider to ColorProvider"
    This is a reland of commit 2f4c3ab6af16167432825c16ff7be24f68c77c14
    
    Fix failed mac browser tests: https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/9349/test-results. See patchset 1..3.
    
    Original change's description:
    > Migrate Tab Group off ThemeProvider to ColorProvider
    >
    > Bug: 1334664
    > Change-Id: I4e056c0adecb8ce1205e0bc9091dbe731cfd367d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686341
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1012246}
    
    Bug: 1334664
    Change-Id: I948ef082efb21363e95763322be39c8917771680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698662
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012781}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1602,7 +1602,7 @@ absl::optional<int> TabStrip::GetLastTabInGroup(
 
 SkColor TabStrip::GetPaintedGroupColor(
     const tab_groups::TabGroupColorId& color_id) const {
-  return GetThemeProvider()->GetColor(
+  return GetColorProvider()->GetColor(
       GetTabGroupTabStripColorId(color_id, ShouldPaintAsActiveFrame()));
 }
 
