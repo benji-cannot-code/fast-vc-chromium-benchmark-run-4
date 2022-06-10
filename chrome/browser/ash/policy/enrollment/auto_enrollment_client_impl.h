bee79aeb50f703ebf54f28051dd8120112687ec8
Artem Sumaneev <asumaneev@google.com>
2022-06-10 15:13:58 +0000

    [Enrollment] Move StateDownloadMessageProcessor into separate file.
    Move StateDownloadMessageProcessor and its children from
    AutoEnrollmentClientImpl to the separate file. It is now
    AutoEnrollmentStateDownloadMessageProcessor with the same prefix as
    other classes related to auto-enrollment.
    
    Bug: chromium:1294843
    Test: None
    Change-Id: I0b73219347e3427d3787f072623c08aaaa35c867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564002
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012953}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl.h
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl.h
@@ -30,6 +30,8 @@ class DeviceManagementResponse;
 
 namespace policy {
 
+class AutoEnrollmentStateMessageProcessor;
+
 // Interacts with the device management service and determines whether this
 // machine should automatically enter the Enterprise Enrollment screen during
 // OOBE.
@@ -41,11 +43,6 @@ class AutoEnrollmentClientImpl
   // server-backed state key is used. It will set the identifier for the
   // DeviceAutoEnrollmentRequest.
   class DeviceIdentifierProviderFRE;
-
-  // Subclasses of this class generate the request to download the device state
-  // (after determining that there is server-side device state) and parse the
-  // response.
-  class StateDownloadMessageProcessor;
 
   class FactoryImpl : public Factory {
    public:
@@ -107,7 +104,7 @@ class AutoEnrollmentClientImpl
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       std::unique_ptr<DeviceIdentifierProviderFRE>
           device_identifier_provider_fre,
-      std::unique_ptr<StateDownloadMessageProcessor>
+      std::unique_ptr<AutoEnrollmentStateMessageProcessor>
           state_download_message_processor,
       int power_initial,
       int power_limit,
@@ -244,7 +241,7 @@ class AutoEnrollmentClientImpl
   std::unique_ptr<DeviceIdentifierProviderFRE> device_identifier_provider_fre_;
 
   // Fills and parses state retrieval request / response.
-  std::unique_ptr<StateDownloadMessageProcessor>
+  std::unique_ptr<AutoEnrollmentStateMessageProcessor>
       state_download_message_processor_;
 
   // Obtains the device state using PSM protocol. Handles all communications
