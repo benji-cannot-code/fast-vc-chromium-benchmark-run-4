3a5132c629978b491d36d4fd3f87e1819fbe2d7f
Matt Menke <mmenke@chromium.org>
2022-06-10 16:55:22 +0000

    Remove use of URLFetcher in chrome/browser/android/contextualsearch
    It was pulling a constant from URLFetcher, despite not actually using
    URLFetcher.
    
    Bug: 1010491
    Change-Id: I5ed6b5355bec5732409f933fe3764309185769c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696031
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012996}

--- chrome/browser/android/contextualsearch/resolved_search_term.cc
+++ chrome/browser/android/contextualsearch/resolved_search_term.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/contextualsearch/resolved_search_term.h"
 
-#include "net/url_request/url_fetcher.h"
-
 ResolvedSearchTerm::ResolvedSearchTerm(int response_code)
-    : is_invalid(response_code == net::URLFetcher::RESPONSE_CODE_INVALID),
+    : is_invalid(response_code == kResponseCodeUninitialized),
       response_code(response_code),
       search_term(""),
       display_text(""),
