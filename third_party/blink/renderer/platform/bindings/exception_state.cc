4fef953138312f374e88208360fe6f807ccd75dc
Yuki Shiino <yukishiino@chromium.org>
2022-06-10 05:55:59 +0000

    bindings: Remove redundant error message of required dict member
    In case of missing a required dictionary member, the context
    information (dictionary name, member name) was shown twice
    (duplicated) in the exception message.  This patch removes the
    dup part.
    
    Bug: 1202291
    Change-Id: Idb1a6cb66acc2840c5c790a49826e8ec77ca5514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697961
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012828}

--- third_party/blink/renderer/platform/bindings/exception_state.cc
+++ third_party/blink/renderer/platform/bindings/exception_state.cc
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
-#include <ostream>
-
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/notreached.h"
