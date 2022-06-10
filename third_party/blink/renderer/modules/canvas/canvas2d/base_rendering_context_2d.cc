ae13f31d473bd79f6372e425f3f9962c210c55ff
perryuwang <perryuwang@tencent.com>
2022-06-10 14:02:09 +0000

    Use SkPathBuilder to create SkPath
    Bug: 1111419
    Change-Id: Ieae58f45d9d99c4dbb06535bbf3d445dfcd04215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697706
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Auto-Submit: Yupei Wang <perryuwang@tencent.com>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012918}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/video_frame_image_util.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/skia/include/core/SkPathBuilder.h"
 #include "ui/gfx/geometry/quad_f.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 
@@ -1132,11 +1133,11 @@ static void StrokeRectOnCanvas(const gfx::RectF& rect,
   DCHECK_EQ(flags->getStyle(), cc::PaintFlags::kStroke_Style);
   if ((rect.width() > 0) != (rect.height() > 0)) {
     // When stroking, we must skip the zero-dimension segments
-    SkPath path;
+    SkPathBuilder path;
     path.moveTo(rect.x(), rect.y());
     path.lineTo(rect.right(), rect.bottom());
     path.close();
-    canvas->drawPath(path, *flags);
+    canvas->drawPath(path.detach(), *flags);
     return;
   }
   canvas->drawRect(gfx::RectFToSkRect(rect), *flags);
