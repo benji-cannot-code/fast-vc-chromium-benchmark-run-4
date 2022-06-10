cb38a8fdd22a9ff085eec3355530c2d6e807026c
Khushal <khushalsagar@chromium.org>
2022-06-10 13:53:37 +0000

    document-transition: Fix crash when canceling transitions.
    R=vmpstr@chromium.org
    
    Change-Id: I039878e94347275552f4f2a628f8d0dcb25e7f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3693031
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012915}

--- third_party/blink/renderer/core/document_transition/document_transition.cc
+++ third_party/blink/renderer/core/document_transition/document_transition.cc
@@ -226,7 +226,6 @@ void DocumentTransition::NotifyCaptureFinished(uint32_t sequence_id) {
   StartDeferringCommits();
   if (!capture_resolved_callback_) {
     state_ = State::kCaptured;
-    start_script_state_ = nullptr;
     NotifyPostCaptureCallbackResolved(/*success=*/true);
     return;
   }
@@ -250,7 +249,6 @@ void DocumentTransition::NotifyCaptureFinished(uint32_t sequence_id) {
                                            post_capture_reject_callable_));
 
   capture_resolved_callback_ = nullptr;
-  start_script_state_ = nullptr;
   state_ = State::kCaptured;
 }
 
@@ -267,6 +265,7 @@ void DocumentTransition::NotifyStartFinished(uint32_t sequence_id) {
   DCHECK(start_promise_resolver_);
   start_promise_resolver_->Resolve();
   start_promise_resolver_ = nullptr;
+  start_script_state_ = nullptr;
 
   // Resolve the promise to notify script when animations finish but don't
   // remove the pseudo element tree.
@@ -492,7 +491,6 @@ void DocumentTransition::ResetTransitionState(bool abort_style_tracker) {
 
 void DocumentTransition::ResetScriptState(const char* abort_message) {
   capture_resolved_callback_ = nullptr;
-  start_script_state_ = nullptr;
 
   if (post_capture_success_callable_) {
     DCHECK(post_capture_reject_callable_);
@@ -504,7 +502,8 @@ void DocumentTransition::ResetScriptState(const char* abort_message) {
     post_capture_reject_callable_ = nullptr;
   }
 
-  if (start_promise_resolver_) {
+  if (start_promise_resolver_ && start_script_state_->ContextIsValid()) {
+    ScriptState::Scope scope(start_script_state_);
     if (abort_message) {
       start_promise_resolver_->Reject(V8ThrowDOMException::CreateOrDie(
           start_promise_resolver_->GetScriptState()->GetIsolate(),
@@ -512,8 +511,9 @@ void DocumentTransition::ResetScriptState(const char* abort_message) {
     } else {
       start_promise_resolver_->Detach();
     }
-    start_promise_resolver_ = nullptr;
   }
+  start_promise_resolver_ = nullptr;
+  start_script_state_ = nullptr;
 }
 
 }  // namespace blink
