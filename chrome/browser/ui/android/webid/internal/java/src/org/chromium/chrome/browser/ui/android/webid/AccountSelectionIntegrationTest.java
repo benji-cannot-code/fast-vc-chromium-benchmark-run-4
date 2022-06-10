16645873d472022b81aa9a2ee145e3d9733c5467
Zachary Tan <tanzachary@chromium.org>
2022-06-10 15:34:49 +0000

    Convert initializer method of AccountSelectionComponent/Coordinator into constructor
    Bug: 1243007
    Change-Id: I5d994a42cd28e9dbbbc1a6880a02ae1f87d9340a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698670
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012965}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionIntegrationTest.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionIntegrationTest.java
@@ -117,12 +117,11 @@ public class AccountSelectionIntegrationTest {
     @Before
     public void setUp() throws InterruptedException {
         MockitoAnnotations.initMocks(this);
-        mAccountSelection = new AccountSelectionCoordinator();
         mActivityTestRule.startMainActivityOnBlankPage();
         runOnUiThreadBlocking(() -> {
             mBottomSheetController = BottomSheetControllerProvider.from(
                     mActivityTestRule.getActivity().getWindowAndroid());
-            mAccountSelection.initialize(
+            mAccountSelection = new AccountSelectionCoordinator(
                     mActivityTestRule.getActivity(), mBottomSheetController, mMockBridge);
         });
 
--- chrome/browser/ui/android/webid/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionComponent.java
+++ chrome/browser/ui/android/webid/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionComponent.java
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.android.webid;
 
-import android.content.Context;
-
 import org.chromium.chrome.browser.ui.android.webid.data.Account;
 import org.chromium.chrome.browser.ui.android.webid.data.ClientIdMetadata;
 import org.chromium.chrome.browser.ui.android.webid.data.IdentityProviderMetadata;
-import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 
 import java.util.List;
 
@@ -41,14 +38,6 @@ public interface AccountSelectionComponent {
          */
         void onAutoSignInCancelled();
     }
-
-    /**
-     * Initializes the component.
-     * @param context A {@link Context} to create views and retrieve resources.
-     * @param sheetController A {@link BottomSheetController} used to show/hide the sheet.
-     * @param delegate A {@link Delegate} that handles dismiss events.
-     */
-    void initialize(Context context, BottomSheetController sheetController, Delegate delegate);
 
     /**
      * Displays the given accounts in a new bottom sheet.
