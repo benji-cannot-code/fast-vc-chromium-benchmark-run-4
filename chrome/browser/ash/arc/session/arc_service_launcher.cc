f9879aab3172535ccac379621a8e8c13b20f6f75
Theodore Tefera <ttefera@google.com>
2022-06-10 17:52:02 +0000

    Adding ArcSystemUIBridge to ArcServiceLauncher
    TEST=Manually tested. "Enable Dark/light mode of system ui" chrome flag
    and then toggle Dark Theme from system tray.
    
    BUG: b:219591999
    Change-Id: Ia6936e15cd9ce8c650d343691e9d01e4d5971dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686251
    Commit-Queue: Theodore Tefera <ttefera@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013024}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/session/arc_session.h"
 #include "ash/components/arc/session/arc_session_runner.h"
 #include "ash/components/arc/storage_manager/arc_storage_manager.h"
+#include "ash/components/arc/system_ui/arc_system_ui_bridge.h"
 #include "ash/components/arc/timer/arc_timer_bridge.h"
 #include "ash/components/arc/usb/usb_host_bridge.h"
 #include "ash/components/arc/volume_mounter/arc_volume_mounter_bridge.h"
@@ -293,6 +294,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcSettingsService::GetForBrowserContext(profile);
   ArcSharesheetBridge::GetForBrowserContext(profile);
   ArcSurveyService::GetForBrowserContext(profile);
+  ArcSystemUIBridge::GetForBrowserContext(profile);
   ArcTimerBridge::GetForBrowserContext(profile);
   ArcTracingBridge::GetForBrowserContext(profile);
   ArcTtsService::GetForBrowserContext(profile);
