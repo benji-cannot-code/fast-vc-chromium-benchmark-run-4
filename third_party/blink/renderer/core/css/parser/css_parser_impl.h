77dd4b03bed14765ef9ec032a49fed97595d72c3
Nidhi Jaju <nidhijaju@chromium.org>
2022-06-10 02:16:08 +0000

    Revert "Cache MediaQuerySet in CSSParserImpl"
    This reverts commit 968556fb302713b93061210c4e86807194e77b4e.
    
    Reason for revert: This causes crashes in WebKit Linux MSAN and ASAN builders (i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20ASAN/b8811803423190518241/overview)
    
    Original change's description:
    > Cache MediaQuerySet in CSSParserImpl
    >
    > Some pages use a very large number of identical media queries. Since
    > MediaQuerySet now is immutable, there is no reason to allocate unique
    > instances for all of these.
    >
    > Bug: 1312000, 1331260
    > Change-Id: Ia38ccb936eb688dc97424a0ec32f2ce4a95ee91d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687952
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1012674}
    
    Bug: 1312000, 1331260, 1335270
    Change-Id: Iacaceb11de47c2595cf71def165efc9cbef6ba3f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698345
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012819}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.h
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token_range.h"
 #include "third_party/blink/renderer/core/css/parser/css_tokenized_value.h"
-#include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -217,8 +216,6 @@ class CORE_EXPORT CSSParserImpl {
   CSSParserObserver* observer_;
 
   CSSLazyParsingState* lazy_state_;
-
-  HeapHashMap<String, Member<const MediaQuerySet>> media_query_cache_;
 };
 
 }  // namespace blink
