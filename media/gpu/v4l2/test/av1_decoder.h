9ac93e4bdc888b21f9d8c3b67c2e53aeabbf9d0f
Steve Cho <stevecho@google.com>
2022-06-10 17:50:42 +0000

    Reland "media/gpu/v4l2: setup V4L2 AV1 decoding loop filter params"
    This is a reland of commit 608ec0dede18ffba1b6bc63f34baf0b8606ddf79
    
    Previous CL was reverted with crrev/c/3670419. Build error with kevin
    (14844.0.0) was happening even though the fix already landed in
    14841.0.0 with crrev/c/3647887. The issue was reproduced in local
    environment, but checked that it is no longer happening with ToT.
    
    Original change's description:
    > media/gpu/v4l2: setup V4L2 AV1 decoding loop filter params
    >
    > This CL prepares the following loop filter parameters for AV1 decoding
    > using V4L2 API.
    > - Section 5.9.11. Loop filter params syntax in AV1 spec.
    > - Section 5.9.18. Loop filter delta parameters syntax.
    >   https://aomediacodec.github.io/av1-spec/av1-spec.pdf
    >
    > These parameters are needed to make VIDIOC_S_EXT_CTRLS V4L2 ioctl call.
    >
    > Bug: b:228534730
    > TEST: v4l2_stateless_decoder --video=00000527_20210205.ivf --frames=1 --v=4 on cherry/tomato using mainline
    > Change-Id: I158ecaf45c02c8eb9275239a8e9c7871539c2db3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652013
    > Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    > Commit-Queue: Steve Cho <stevecho@chromium.org>
    > Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1008008}
    
    Bug: b:234141528
    Change-Id: Ia124fb2c91878945712de5174b53ea786e778b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3683923
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Auto-Submit: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013021}

--- media/gpu/v4l2/test/av1_decoder.h
+++ media/gpu/v4l2/test/av1_decoder.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/test/video_decoder.h"
 
+// TODO(b/234019411): Move this include to v4l2_stateless_decoder.cc
+// once the bug is fixed.
+#include <linux/media/av1-ctrls.h>
+
 #include <set>
 
 #include "base/files/memory_mapped_file.h"
