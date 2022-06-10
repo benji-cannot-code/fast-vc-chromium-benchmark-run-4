0be6f2f60ac238464d364808512e4b8773aa874b
Michael Martis <martis@chromium.org>
2022-06-10 05:59:13 +0000

    [Adaptive charging] Enable by default.
    The feature is intended to be opt-out. Note, since the pref isn't used
    at all when the feature is disabled, that unconditionally setting the
    pref default to true is a no-op when the feature is disabled.
    
    Bug: b:216035280
    Change-Id: Ia0f0bd39229d108d61a0960f95196e3ee869c9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697756
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Christopher Lam <calamity@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012833}

--- chrome/browser/ui/webui/settings/chromeos/device_power_handler_browsertest.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_power_handler_browsertest.cc
@@ -76,7 +76,7 @@ class PowerHandlerTest : public InProcessBrowserTest {
         PowerPolicyController::ACTION_SUSPEND;
     bool lid_closed_controlled = false;
     bool has_lid = true;
-    bool adaptive_charging = false;
+    bool adaptive_charging = true;
   };
 
   PowerHandlerTest() = default;
@@ -435,9 +435,9 @@ IN_PROC_BROWSER_TEST_F(PowerHandlerTest, SetAdaptiveCharging) {
       GetPrefs()->FindPreference(ash::prefs::kPowerAdaptiveChargingEnabled);
   ASSERT_NE(nullptr, pref);
 
-  EXPECT_EQ(false, pref->GetValue()->GetBool());
-  test_api_->SetAdaptiveCharging(true);
   EXPECT_EQ(true, pref->GetValue()->GetBool());
+  test_api_->SetAdaptiveCharging(false);
+  EXPECT_EQ(false, pref->GetValue()->GetBool());
 }
 
 }  // namespace settings
