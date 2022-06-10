e4e25da9479beacea2940f653a75dd2feab5b087
mavishsu <mavishsu@google.com>
2022-06-10 07:41:56 +0000

    [Phone Hub] Fix Web GAR issues - provide learn more aria label for TTS.
    Bug: b/232199523
    Test: Use crtl+alt+z to enable TTS and verify that we can hear "Learn more about Phone Hub"
    
    Change-Id: Ie1b65f648e0851a99fa67a1abc08cfe2f677d3b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3694879
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Mavis Hsu <mavishsu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012849}

--- chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_permissions_setup_dialog.js
+++ chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_permissions_setup_dialog.js
@@ -227,6 +227,12 @@ Polymer({
       type: Boolean,
       value: false,
     },
+
+    /** @private */
+    learnMoreButtonAriaLabel_: {
+      type: String,
+      computed: 'getLearnMoreButtonAriaLabel_()',
+    },
   },
 
   listeners: {
@@ -626,4 +632,12 @@ Polymer({
         this.isPhoneScreenLockEnabled && !this.isChromeosScreenLockEnabled &&
         this.showAppStreaming;
   },
+
+  /**
+   * @return {string} A aria label about learn more button.
+   * @private
+   */
+  getLearnMoreButtonAriaLabel_() {
+    return this.i18n('multidevicePhoneHubLearnMoreAriaLabel');
+  }
 });
