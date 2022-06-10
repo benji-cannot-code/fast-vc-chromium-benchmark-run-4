1075438ae0ac88040e090a284ded42d0a85a6fcf
Hui Yingst <nigi@chromium.org>
2022-06-10 18:25:28 +0000

    Remove an extra header in pdf/pdfium/pdfium_engine.cc
    "base/numerics/safe_conversions.h" was accidentally added in
    https://crrev.com/c/3696844. Remove it since it's unnecessary.
    
    Change-Id: I9669ef878952b98101ad2a925c6b2a363e95138d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700471
    Commit-Queue: Nigi <nigi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013041}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/location.h"
 #include "base/notreached.h"
-#include "base/numerics/safe_conversions.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
