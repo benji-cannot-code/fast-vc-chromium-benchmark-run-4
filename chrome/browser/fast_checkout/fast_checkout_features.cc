aac3d249616611c35e441b9ff33c8560272d49aa
Norge Vizcay <vizcay@google.com>
2022-06-10 08:33:14 +0000

    [FastCheckout] Add fast checkout feature flag.
    The CL adds the initial feature flag to enable the fast checkout experience in Chrome.
    
    Bug: 1334642
    
    Change-Id: I4d76e28c4bf468e559e1c2640fc11b2ce46e1d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695400
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1012862}

---
+++ chrome/browser/fast_checkout/fast_checkout_features.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/fast_checkout/fast_checkout_features.h"
+
+namespace features {
+
+#if BUILDFLAG(IS_ANDROID)
+
+const base::Feature kFastCheckout{"FastCheckout",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
+#endif  // BUILDFLAG(IS_ANDROID)
+
+}  // namespace features
