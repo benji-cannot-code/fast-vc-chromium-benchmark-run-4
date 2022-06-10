b9868bee071a8fd1bcec3bda3200b3b826776006
Christian Dullweber <dullweber@chromium.org>
2022-06-10 14:28:58 +0000

    Privacy Sandbox Settings: Add Fledge sites to AdPersonalizationFragment
    Add Fledge sites to the AdPersonalizationFragment.
    
    Screenshots: https://crbug.com/1334933#c1
    Bug: 1334933
    Change-Id: I42604b689980ec6113b501e921445e609373543a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697143
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012933}

--- chrome/browser/privacy_sandbox/android/privacy_sandbox_bridge.cc
+++ chrome/browser/privacy_sandbox/android/privacy_sandbox_bridge.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread_checker.h"
 #include "base/time/time.h"
 #include "chrome/browser/privacy_sandbox/android/jni_headers/PrivacySandboxBridge_jni.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/privacy_sandbox/canonical_topic.h"
 #include "components/privacy_sandbox/privacy_sandbox_settings.h"
+#include "content/public/browser/browser_thread.h"
 
 using base::android::ConvertUTF16ToJavaString;
 using base::android::ScopedJavaLocalRef;
@@ -140,6 +142,7 @@ static void JNI_PrivacySandboxBridge_GetFledgeJoiningEtldPlusOneForDisplay(
       base::BindOnce(
           [](const base::android::JavaRef<jobject>& j_callback,
              std::vector<std::string> strings) {
+            DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
             JNIEnv* env = base::android::AttachCurrentThread();
             base::android::RunObjectCallbackAndroid(
                 j_callback, base::android::ToJavaArrayOfStrings(env, strings));
