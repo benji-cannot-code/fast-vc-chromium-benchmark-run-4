a6f33966d03b892885359e453583384f091df40c
Mahmoud Rashad <mmrashad@google.com>
2022-06-10 08:31:27 +0000

    [sync] Unify all of the margins across all of android sync promos
    As discussed with UX, this CL makes sure that all of android sync promos
    margins are consistent without any feature flags.
    
    Attached to crbug.com/1314490 a screenshots for how it would look after
    this change.
    
    Bug: 1314490
    Change-Id: Ifc6e0117377ec27b2a43b798088bf86c8098f479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695331
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012861}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedPromoRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedPromoRenderTest.java
@@ -40,6 +40,9 @@ import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.test.util.NightModeTestUtils;
 import org.chromium.ui.test.util.UiDisableIf;
 
+// TODO(crbug.com/1334912): This class should be removed, since we have a similar tests in
+// SigninPromoControllerRenderTest.
+
 /**
  * Tests for the personalized signin promo on the Bookmarks page.
  */
@@ -64,7 +67,7 @@ public class BookmarkPersonalizedPromoRenderTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(3)
+                    .setRevision(4)
                     .setBugComponent(ChromeRenderTestRule.Component.SERVICES_SIGN_IN)
                     .build();
 
--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninPromoControllerRenderTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninPromoControllerRenderTest.java
@@ -70,7 +70,7 @@ public class SigninPromoControllerRenderTest {
     @Rule
     public final RenderTestRule mRenderTestRule =
             RenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(4)
+                    .setRevision(5)
                     .setBugComponent(RenderTestRule.Component.SERVICES_SIGN_IN)
                     .build();
 
