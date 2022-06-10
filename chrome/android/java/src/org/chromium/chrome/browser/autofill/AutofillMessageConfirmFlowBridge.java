4838348e0ac6b9f2ae0152a8809a4ad5a8a3716f
Lijin Shen <lazzzis@google.com>
2022-06-10 19:43:44 +0000

    [Messages][SaveCard] Fix metrics recording error
    1. for fix name dialogs, "Dismissed" is never called and is counted as
    "InteractedAndIgnored", because
    CardNameFixFlowControllerImpl::OnDismissed is never called by
    CardNameFixFlowViewAndroid when dialog is dismissed by user.
    In uma dashboard, there is almost no record of Autofill.CardholderNameFixFlowPrompt.Events#Dismissed.
    
    2. for message dialogs, "Dismissed" is sometimes counted as
    "InteractedAndIgnored" by mistake, because kDenied is not recorded
    when the dialog is dismissed by user.
    
    Bug: 1190531
    Change-Id: If57410bc52caaa0325f38d49898690f4e1b6eb46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688046
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013079}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillMessageConfirmFlowBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillMessageConfirmFlowBridge.java
@@ -71,9 +71,11 @@ public class AutofillMessageConfirmFlowBridge
                 mNativeSaveCardMessageConfirmDelegate, month, year);
     }
 
-    // no-op
     @Override
-    public void onUserDismiss() {}
+    public void onUserDismiss() {
+        AutofillMessageConfirmFlowBridgeJni.get().onUserDismiss(
+                mNativeSaveCardMessageConfirmDelegate);
+    }
 
     @Override
     public void onLinkClicked(String url) {
@@ -186,6 +188,7 @@ public class AutofillMessageConfirmFlowBridge
         void onDateConfirmed(long nativeSaveCardMessageConfirmDelegate, String month, String year);
         void onNameConfirmed(long nativeSaveCardMessageConfirmDelegate, String name);
         void onSaveCardConfirmed(long nativeSaveCardMessageConfirmDelegate);
+        void onUserDismiss(long nativeSaveCardMessageConfirmDelegate);
         void onLinkClicked(long nativeSaveCardMessageConfirmDelegate, String url);
         void dialogDismissed(long nativeSaveCardMessageConfirmDelegate);
     }
