5c7162cd8ea449e6cb27213052377bf75d7130f7
Jan Keitel <jkeitel@google.com>
2022-06-10 17:23:43 +0000

    [APC Desktop] Add ui test for actual onboarding dialog model.
    Bug: 1322387
    Change-Id: I88ac6031dd05cd9061dc8498de8471b0672eb96b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689553
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013005}

--- chrome/browser/autofill_assistant/password_change/apc_onboarding_coordinator_impl.h
+++ chrome/browser/autofill_assistant/password_change/apc_onboarding_coordinator_impl.h
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_AUTOFILL_ASSISTANT_PASSWORD_CHANGE_APC_ONBOARDING_COORDINATOR_IMPL_H_
 #define CHROME_BROWSER_AUTOFILL_ASSISTANT_PASSWORD_CHANGE_APC_ONBOARDING_COORDINATOR_IMPL_H_
 
-#include "chrome/browser/autofill_assistant/password_change/apc_onboarding_coordinator.h"
-
 #include <memory>
 #include <string>
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/autofill_assistant/password_change/apc_onboarding_coordinator.h"
 #include "chrome/browser/ui/autofill_assistant/password_change/assistant_onboarding_controller.h"
 #include "chrome/browser/ui/autofill_assistant/password_change/assistant_onboarding_prompt.h"
 
