ea0d3e82bb2e0506897cf74827c97a0b11ee6cc3
Khushal <khushalsagar@chromium.org>
2022-06-10 18:26:55 +0000

    inspector: Fix pseudo element destruction order assumption.
    Since the pseudo element tree created for a document transition is
    destroyed with in-order traversal, we remove the parent node
    (::page-transition) before its children (::page-transition-container).
    Fix this assumption.
    
    R=caseq@chromium.org
    
    Change-Id: I25deb94b415996f68858fb96fab60c79298e7373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692953
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013044}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/frame_tree.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/scroll/scroll_into_view_util.h"
+#include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/core/xml/document_xpath_evaluator.h"
 #include "third_party/blink/renderer/core/xml/xpath_result.h"
@@ -2343,7 +2344,10 @@ void InspectorDOMAgent::PseudoElementDestroyed(PseudoElement* pseudo_element) {
   Element* parent = pseudo_element->ParentOrShadowHostElement();
   DCHECK(parent);
   int parent_id = BoundNodeId(parent);
-  DCHECK(parent_id);
+  // Since the pseudo element tree created for a document transition is destroyed with in-order
+  // traversal, the parent node (::page-transition) are destroyed before its children
+  // (::page-transition-container).
+  DCHECK(parent_id || IsTransitionPseudoElement(pseudo_element->GetPseudoId()));
 
   Unbind(pseudo_element);
   GetFrontend()->pseudoElementRemoved(parent_id, pseudo_element_id);
