1ba893a33d5f2a58334aaa0b74205e9d3318167c
Curt Clemens <cclem@google.com>
2022-06-10 17:28:33 +0000

    [QuickStart] Rename BootstrapController->ConnectionBroker, stub methods
    Since the Bluetooth functionality currently in
    TargetDeviceBootstrapController really belongs in
    TargetDeviceConnectionBroker, rename the BootstrapController and stub
    out the advertising methods that belong in the ConnectionBroker.
    
    The BootstrapController will be added back in a followup CL.
    
    Bug: b/234655072
    Test: cros_tests --gtest_filter='TargetDeviceConnectionBroker*'
    Change-Id: Idefe217e073676d75a352057bccf356892058965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696424
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013007}

---
+++ chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker_factory.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker_factory.h"
+
+#include "chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker_impl.h"
+
+namespace ash::quick_start {
+
+// static
+std::unique_ptr<TargetDeviceConnectionBroker>
+TargetDeviceConnectionBrokerFactory::Create() {
+  if (test_factory_) {
+    return test_factory_->CreateInstance();
+  }
+
+  return std::make_unique<TargetDeviceConnectionBrokerImpl>();
+}
+
+// static
+void TargetDeviceConnectionBrokerFactory::SetFactoryForTesting(
+    TargetDeviceConnectionBrokerFactory* test_factory) {
+  test_factory_ = test_factory;
+}
+
+// static
+TargetDeviceConnectionBrokerFactory*
+    TargetDeviceConnectionBrokerFactory::test_factory_ = nullptr;
+
+}  // namespace ash::quick_start
