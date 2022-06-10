a025bdb12100784f8b3c40984f79e1ef226161b1
Sebastien Lalancette <seblalancette@chromium.org>
2022-06-10 14:56:40 +0000

    [NewEV] Collect Antivirus Signal from WMI
    Adds a function to WmiClient which can be used to query
    information from WMI about antivirus software installed on the
    current device. It goes through an undocument source in WMI. For that
    reason, this function is meant to be used on Win7 as a fallback to not
    being able to use WSC (the documented approach available on Win8+).
    
    Bug: b:231743012
    Change-Id: I3c61e296e3fd56b500b9a85855f1fa5f14f884ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3649357
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012945}

--- components/device_signals/core/common/win/mock_wmi_client.h
+++ components/device_signals/core/common/win/mock_wmi_client.h
@@ -16,6 +16,7 @@ class MockWmiClient : public WmiClient {
   MockWmiClient();
   ~MockWmiClient() override;
 
+  MOCK_METHOD(WmiAvProductsResponse, GetAntiVirusProducts, (), (override));
   MOCK_METHOD(WmiHotfixesResponse, GetInstalledHotfixes, (), (override));
 };
 
