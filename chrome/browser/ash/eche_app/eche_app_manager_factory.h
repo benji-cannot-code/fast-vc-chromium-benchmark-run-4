8738c000f1cd1ea2b8c3c81fac9e1909525703ea
Guanru Lee <guanrulee@chromium.org>
2022-06-12 23:20:27 +0000

    Improve test coverage of EcheAppManagerFactoryTest
    Bug: b/235248893
    Test: unit_tests --gtest_filter="EcheAppNotificationControllerTest.*"
    Change-Id: Idcf3d541ab092942f17397d27b7de41735ae1504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695644
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013325}

--- chrome/browser/ash/eche_app/eche_app_manager_factory.h
+++ chrome/browser/ash/eche_app/eche_app_manager_factory.h
@@ -60,7 +60,7 @@ class LaunchedAppInfo {
     gfx::Image icon_;
   };
 
-  LaunchedAppInfo();
+  LaunchedAppInfo() = delete;
   LaunchedAppInfo(const LaunchedAppInfo&) = delete;
   LaunchedAppInfo& operator=(const LaunchedAppInfo&) = delete;
   ~LaunchedAppInfo();
@@ -112,6 +112,7 @@ class EcheAppManagerFactory : public BrowserContextKeyedServiceFactory {
 
  private:
   friend struct base::DefaultSingletonTraits<EcheAppManagerFactory>;
+  friend class EcheAppManagerFactoryTest;
 
   EcheAppManagerFactory();
   ~EcheAppManagerFactory() override;
