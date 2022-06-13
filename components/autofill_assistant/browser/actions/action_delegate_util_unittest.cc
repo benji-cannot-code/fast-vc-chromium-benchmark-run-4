d04acda1b26d51573f1deb2b6af8ca19fbb70e53
sandromaggi <sandromaggi@google.com>
2022-06-13 10:37:46 +0000

    Make legacy method Test only
    This moves the `SetRenderFrameHost` that has been made
    legacy to a Test only `SetRenderFrameHostForTest`.
    
    This change also cleans up the remaining (hopefully
    unproblematic) uses outside of tests. They are deemed
    unproblematic as they used the frame right after
    retrieving it without any delay in between.
    
    Bug: 1335205
    Change-Id: I2ae0669f41c3c0f09f085a933d33691153a9a57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702370
    Auto-Submit: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013394}

--- components/autofill_assistant/browser/actions/action_delegate_util_unittest.cc
+++ components/autofill_assistant/browser/actions/action_delegate_util_unittest.cc
@@ -219,7 +219,7 @@ TEST_F(ActionDelegateUtilTest, PerformWithPasswordManagerValue) {
   auto element = std::make_unique<ElementFinderResult>();
   content::WebContentsTester::For(web_contents_.get())
       ->NavigateAndCommit(GURL("https://www.example.com"));
-  element->SetRenderFrameHost(web_contents_->GetPrimaryMainFrame());
+  element->SetRenderFrameHostForTest(web_contents_->GetPrimaryMainFrame());
 
   user_data_.selected_login_ = absl::make_optional<WebsiteLoginManager::Login>(
       GURL("https://www.example.com"), "username");
@@ -242,7 +242,7 @@ TEST_F(ActionDelegateUtilTest, PerformWithPasswordManagerValue) {
 
 TEST_F(ActionDelegateUtilTest, PerformWithFailingPasswordManagerValue) {
   auto element = std::make_unique<ElementFinderResult>();
-  element->SetRenderFrameHost(web_contents_->GetPrimaryMainFrame());
+  element->SetRenderFrameHostForTest(web_contents_->GetPrimaryMainFrame());
 
   user_data_.selected_login_ = absl::make_optional<WebsiteLoginManager::Login>(
       GURL("https://www.example.com"), "username");
