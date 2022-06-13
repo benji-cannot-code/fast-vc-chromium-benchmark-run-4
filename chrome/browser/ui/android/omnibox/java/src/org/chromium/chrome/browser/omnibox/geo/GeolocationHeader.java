9d2b723b234afea1dbbc377cedb1d1944a4554f2
Andy Paicu <andypaicu@chromium.org>
2022-06-13 17:24:41 +0000

    Fix geolocation header tests and add new ones around header priming
    As requested in https://chromium-review.googlesource.com/c/chromium/src/+/3673772 this adds a instrumentation tests that verify the header priming logic when DSE
    it not autogranted the permission.
    
    Since the tests were also flaky I've also speculatively applied a fix by mocking the visible networks similar to what GeolocationUnitTest does.
    
    Bug: 1328339
    Fixed: 1315315
    Change-Id: Iada6bddab3ce28f79731d6ca908eff5ba228fe23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699804
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013528}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeader.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeader.java
@@ -506,6 +506,11 @@ public class GeolocationHeader {
         sUseAppPermissionGrantedForTesting = true;
     }
 
+    @VisibleForTesting
+    static long getFirstLocationTimeForTesting() {
+        return sFirstLocationTime;
+    }
+
     /** Records a data point for the Geolocation.HeaderSentOrNot histogram. */
     private static void recordHistogram(int result) {
         RecordHistogram.recordEnumeratedHistogram("Geolocation.HeaderSentOrNot", result, UMA_MAX);
