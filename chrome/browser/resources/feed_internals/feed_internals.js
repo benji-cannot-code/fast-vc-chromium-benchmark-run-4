ab397130199e5820dfe106aa419e39992de74bf9
Dan Harrington <harringtond@chromium.org>
2022-06-13 19:14:14 +0000

    Revert "Revert "Optionally force feed-query requests through snippets-internals""
    This reverts commit 4490af303a53e474295a7f9530add6195c5b90ad.
    
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/3665319
    
    Bug: b/233757219
    Change-Id: I86ef02d7c3f390d7cbe92e5469e8d26223d4ebb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3691748
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013603}

--- chrome/browser/resources/feed_internals/feed_internals.js
+++ chrome/browser/resources/feed_internals/feed_internals.js
@@ -30,8 +30,7 @@ function updatePageWithProperties() {
     $('enable-webfeed-follow-intro-debug').checked =
         properties.isWebFeedFollowIntroDebugEnabled;
     $('enable-webfeed-follow-intro-debug').disabled = false;
-    $('use-feed-query-requests-for-web-feeds').checked =
-        properties.useFeedQueryRequestsForWebFeeds;
+    $('use-feed-query-requests').checked = properties.useFeedQueryRequests;
 
     switch (properties.followingFeedOrder) {
       case FeedOrder.kUnspecified:
@@ -149,11 +148,9 @@ function setupEventListeners() {
     $('enable-webfeed-follow-intro-debug').disabled = true;
   });
 
-  $('use-feed-query-requests-for-web-feeds')
-      .addEventListener('click', function() {
-        pageHandler.setUseFeedQueryRequestsForWebFeeds(
-            $('use-feed-query-requests-for-web-feeds').checked);
-      });
+  $('use-feed-query-requests').addEventListener('click', function() {
+    pageHandler.SetUseFeedQueryRequests($('use-feed-query-requests').checked);
+  });
 
   const orderRadioClickListener = function(order) {
     $('following-feed-order-grouped').disabled = true;
