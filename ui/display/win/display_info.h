5c5f036cdee205e85e026296d598d0d92de061b8
Brad Triebwasser <btriebw@chromium.org>
2022-06-13 23:20:51 +0000

    Add EDID labels for displays on Windows platform.
    Bug: 1254885
    Change-Id: Ica2ca29aaf4dc40772f33887f6e7e868bd8cc4f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697197
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013711}

--- ui/display/win/display_info.h
+++ ui/display/win/display_info.h
@@ -24,7 +24,9 @@ class DISPLAY_EXPORT DisplayInfo final {
               Display::Rotation rotation,
               int display_frequency,
               const gfx::Vector2dF& pixels_per_inch,
-              DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY output_technology);
+              DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY output_technology,
+              const std::string& label);
+  DisplayInfo(const DisplayInfo& other);
   ~DisplayInfo();
 
   static int64_t DeviceIdFromDeviceName(const wchar_t* device_name);
@@ -40,6 +42,10 @@ class DISPLAY_EXPORT DisplayInfo final {
   DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY output_technology() const {
     return output_technology_;
   }
+  const std::string& label() const { return label_; }
+
+  bool operator==(const DisplayInfo& rhs) const;
+  bool operator!=(const DisplayInfo& rhs) const { return !(*this == rhs); }
 
  private:
   int64_t id_;
@@ -53,6 +59,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   // monitors. In non-touch cases, it will be set to Zero.
   gfx::Vector2dF pixels_per_inch_;
   DISPLAYCONFIG_VIDEO_OUTPUT_TECHNOLOGY output_technology_;
+  std::string label_;
 };
 
 }  // namespace win
