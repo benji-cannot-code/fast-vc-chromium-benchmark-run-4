aaccb2da279e19568b8f8646227268f474e088e5
Yue Zhang <yuezhanggg@chromium.org>
2022-06-13 21:14:05 +0000

    [ChromeCart][Android] Add feature for commerce hint android experiment
    Bug: 1332878
    Change-Id: I0c4c2a02df678b15b9a8d8e9444912af04b97b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690055
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013651}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -323,6 +323,9 @@ extern const char kCheckOfflineCapabilityDescription[];
 extern const char kChromeLabsName[];
 extern const char kChromeLabsDescription[];
 
+extern const char kCommerceHintAndroidName[];
+extern const char kCommerceHintAndroidDescription[];
+
 extern const char kConsolidatedSiteStorageControlsName[];
 extern const char kConsolidatedSiteStorageControlsDescription[];
 
