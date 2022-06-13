14b861b632a4fd81cf649967a179117b76d5922d
Yue Zhang <yuezhanggg@chromium.org>
2022-06-13 20:14:11 +0000

    [CodeHealth] Re-enable TabGridIphTest#testSwipeToDismiss_IPH
    Bug: 1331659
    Change-Id: I1681876c8744401aff238d85e786dca71753abd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703154
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013630}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
@@ -336,7 +336,6 @@ public class TabGridIphTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/1245260")
     public void testSwipeToDismiss_IPH() {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         enterTabSwitcher(cta);
