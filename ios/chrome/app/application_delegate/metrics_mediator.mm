93850136ad22658edd07321a212121ca4bbc7fb0
Petro Akzhygitov <pakzhygitov@google.com>
2022-06-13 14:52:59 +0000

    Remove NTP feature flag (Part 4)
    – Removed NTP feature flag
    – Removed obsolete code behind the feature flag
    
    Bug: 1335370, 1329099
    Change-Id: Iea7121ac7bff399971dc43c813403555119629ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702494
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013462}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/pref_names.h"
 #import "ios/chrome/browser/signin/signin_util.h"
 #include "ios/chrome/browser/system_flags.h"
-#import "ios/chrome/browser/ui/browser_view/browser_view_controller.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_utils.h"
 #import "ios/chrome/browser/ui/main/browser_interface_provider.h"
 #import "ios/chrome/browser/ui/main/connection_information.h"
@@ -389,8 +388,6 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
         numNTPTabs++;
       }
     }
-    BrowserViewController* bvc = scene.interfaceProvider.mainInterface.bvc;
-    numLiveNTPTabs += [bvc liveNTPCount];
   }
 
   if (startupInformation.isColdStart) {
