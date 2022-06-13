adb2b1fd18d79eb218c295309e9378607e403b16
Ian Helmke <helmke@google.com>
2022-06-13 19:04:57 +0000

    Reland "lacros - add observers for extension-controlled prefs"
    This change adds an ash_capability to ensure that the observers
    are never added unless we know for sure that ash supports it.
    
    It also adjusts the test to make use of the capability rather than
    relying on the ash version, and adds a new test to ensure that ash
    does not crash even if the observers are created.
    
    Original change:
    > This change implements the onChange event for extension-controlled
    > prefs where the underlying pref lives in Ash.
    >
    > A second call to Ash is required because the onChange event also
    > contains information about the control state of the pref which is not
    > returned with the current observer.
    >
    > Bug: 1218145
    > Change-Id: I280b5a74b3ee73fb15be231eb09c7be0d609a2d5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641706
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Ian Helmke <helmke@google.com>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1011942}
    
    Bug: 1218145
    Change-Id: Ia8c882b3f2d96895ced98cba7ca2700a509584d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698545
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Ian Helmke <helmke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013596}

--- chrome/browser/extensions/api/preference/preference_api_lacros_browsertest.cc
+++ chrome/browser/extensions/api/preference/preference_api_lacros_browsertest.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chromeos/crosapi/mojom/prefs.mojom-test-utils.h"
 #include "chromeos/lacros/lacros_service.h"
+#include "chromeos/lacros/lacros_test_helper.h"
+#include "chromeos/startup/browser_init_params.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
@@ -98,6 +100,17 @@ class ExtensionPreferenceApiLacrosBrowserTest
     return true;
   }
 
+  bool DoesAshSupportObservers() {
+    // Versions of ash without this capability cannot create observers for prefs
+    // writing to the ash standalone browser prefstore.
+    constexpr char kExtensionControlledPrefObserversCapability[] =
+        "crbug/1334964";
+    return chromeos::BrowserInitParams::Get()->ash_capabilities.has_value() &&
+           base::Contains(
+               chromeos::BrowserInitParams::Get()->ash_capabilities.value(),
+               kExtensionControlledPrefObserversCapability);
+  }
+
   raw_ptr<Profile> profile_ = nullptr;
   std::unique_ptr<ScopedKeepAlive> keep_alive_;
 };
@@ -147,4 +160,28 @@ IN_PROC_BROWSER_TEST_P(ExtensionPreferenceApiLacrosBrowserTest, Lacros) {
     listener.Reply("");
   }
   CheckPreferencesCleared();
+}
+
+IN_PROC_BROWSER_TEST_P(ExtensionPreferenceApiLacrosBrowserTest, OnChange) {
+  if (!IsServiceAvailable()) {
+    return;
+  }
+  if (!DoesAshSupportObservers()) {
+    LOG(WARNING) << "Ash does not support observers, skipping the test.";
+    return;
+  }
+  EXPECT_TRUE(RunExtensionTest("preference/onchange_lacros", {},
+                               {.allow_in_incognito = false}))
+      << message_;
+}
+
+// The extension controlled pref observers are only instantiated when a listener
+// is actually attached. The purpose of running this test is to ensure that they
+// are instantiated as we use the prefs api to create the observer. See also
+// crbug/1334985.
+IN_PROC_BROWSER_TEST_P(ExtensionPreferenceApiLacrosBrowserTest,
+                       CreateObservers) {
+  EXPECT_TRUE(
+      RunExtensionTest("preference/onchange", {}, {.allow_in_incognito = true}))
+      << message_;
 }
