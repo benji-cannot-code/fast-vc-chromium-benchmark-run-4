74b1c5d1c68024e48171e457c92bd3a756eef8af
Min Qin <qinmin@chromium.org>
2022-06-13 19:19:05 +0000

    [Segmentation Platform] Fix a bug  error boolean is used reversely
    FeatureProcessorState returns error to input callback, but this was
    interpreted as success in TrainingDataCollectorImpl
    
    BUG=1335740
    
    Change-Id: I56e0be3de518ce1d1c6bbede4ba2b69d48666534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3701587
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013607}

--- components/segmentation_platform/internal/data_collection/training_data_collector_impl.h
+++ components/segmentation_platform/internal/data_collection/training_data_collector_impl.h
@@ -66,7 +66,7 @@ class TrainingDataCollectorImpl : public TrainingDataCollector,
 
   void OnGetTrainingTensors(const absl::optional<ImmediaCollectionParam>& param,
                             const proto::SegmentInfo& segment_info,
-                            bool success,
+                            bool has_error,
                             const std::vector<float>& input_tensors,
                             const std::vector<float>& output_tensors);
 
