f8cbd0510d677aecf9f81672cf86a89050a6ad6f
Yoshiki Iguchi <yoshiki@chromium.org>
2022-06-13 05:06:29 +0000

    Revert "Source Demo Mode SWA content from downloaded demo-mode-app Chrome Component"
    This reverts commit 43d80ae0d5091af45c4629abb61e09804e192855.
    
    Reason for revert: Chrome OS release builds fails due to the build error on demo-app
    
    Original change's description:
    > Source Demo Mode SWA content from downloaded demo-mode-app Chrome Component
    >
    > We use a WebUIDataSource with a RequestFilter instead of a separate
    > URLDataSource because we want some built-in resources for e.g.
    > exposing a JS bridge to our Mojo APIs.
    >
    > Change-Id: I31fea5f5c2d2dca25cf02895a3dfb1004dccc988
    > Bug: b/228388936
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3575540
    > Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1013047}
    
    Bug: b/235761472 (for reverting)
    Change-Id: Ie0412cb9bcd37e170d6eb0580bd83db2c9d3c4c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699419
    Owners-Override: N/A <yosin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013346}

--- chrome/browser/ui/webui/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs_chromeos.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chrome_untrusted_web_ui_configs_chromeos.h"
 
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "content/public/browser/webui_config_map.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -23,9 +22,7 @@ void RegisterAshChromeWebUIConfigs() {
 #if !defined(OFFICIAL_BUILD)
   auto& map = content::WebUIConfigMap::GetInstance();
   map.AddWebUIConfig(std::make_unique<ash::SampleSystemWebAppUIConfig>());
-  map.AddWebUIConfig(
-      std::make_unique<ash::DemoModeAppUIConfig>(base::BindRepeating(
-          [] { return ash::DemoSession::Get()->DemoAppComponentPath(); })));
+  map.AddWebUIConfig(std::make_unique<ash::DemoModeAppUIConfig>());
 #endif  // !defined(OFFICIAL_BUILD)
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
