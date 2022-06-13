e0328b374a694ed1481262517f5c072c4c8b0cb6
W. James MacLean <wjmaclean@chromium.org>
2022-06-13 22:01:54 +0000

    Add SiteInstance isolated-sandbox status to process-internals.
    When a SiteInstance is used for processr-isolating a sandboxed iframe,
    reflect that status in chrome://process-internals.
    
    Bug: 510122
    Change-Id: Icbf5eb9aa66a29d378a48a57f7ebc1dcec387643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704316
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013677}

--- content/browser/resources/process/process_internals.ts
+++ content/browser/resources/process/process_internals.ts
@@ -114,6 +114,9 @@ function frameToTreeItem(frame: FrameInfo):
   if (frame.siteInstance.requiresOriginKeyedProcess) {
     itemLabel += ', origin-keyed';
   }
+  if (frame.siteInstance.isSandboxed) {
+    itemLabel += ', iframe-sandbox';
+  }
   if (frame.siteInstance.isGuest) {
     itemLabel += ', guest';
   }
