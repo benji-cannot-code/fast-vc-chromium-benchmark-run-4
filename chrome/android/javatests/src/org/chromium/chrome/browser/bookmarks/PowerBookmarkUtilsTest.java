a1807f4310322f9fdb5b2c82ac77b81f63cf870d
Brandon Wylie <wylieb@chromium.org>
2022-06-14 16:36:07 +0000

    [Power Bookmarks] Convert to on-device unit tests
    Bug: 1335984, 1331862,1332093
    Change-Id: I54b72c2a836c0710ee2bb955d55db8f49499af01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703937
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014005}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/PowerBookmarkUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/PowerBookmarkUtilsTest.java
@@ -20,7 +20,6 @@ import com.google.common.primitives.UnsignedLongs;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -33,7 +32,6 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.browser.subscriptions.CommerceSubscription;
 import org.chromium.chrome.browser.subscriptions.SubscriptionsManager;
-import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.bookmarks.BookmarkType;
 import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
@@ -48,9 +46,6 @@ import java.util.List;
 @RunWith(BaseJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
 public class PowerBookmarkUtilsTest {
-    @Rule
-    public final ChromeBrowserTestRule mChromeBrowserTestRule = new ChromeBrowserTestRule();
-
     @Mock
     private BookmarkModel mMockBookmarkModel;
 
