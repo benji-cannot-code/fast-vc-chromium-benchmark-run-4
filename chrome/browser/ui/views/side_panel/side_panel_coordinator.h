768a25c0bff5539ca03f377541f35488c4671bf4
Caroline Rising <corising@chromium.org>
2022-06-14 16:32:56 +0000

    Unified side panel: make combobox match webui delays.
    If we are delaying switching between content in the side panel due to
    loading content delays the combobox must also delay updating so that
    the entry listed in the combobox matches the visible content.
    
    Bug: 1319886
    Change-Id: I134502542950a0c80afe938c43cc27a9c877703a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692086
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014004}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator.h
@@ -53,6 +53,8 @@ class SidePanelCoordinator final : public SidePanelRegistryObserver,
     return current_entry_.get();
   }
 
+  SidePanelEntry::Id GetComboboxDisplayedEntryIdForTesting() const;
+
  private:
   friend class SidePanelCoordinatorTest;
   FRIEND_TEST_ALL_PREFIXES(UserNoteUICoordinatorTest,
@@ -60,7 +62,7 @@ class SidePanelCoordinator final : public SidePanelRegistryObserver,
   FRIEND_TEST_ALL_PREFIXES(UserNoteUICoordinatorTest,
                            PopulateUserNoteSidePanel);
 
-  views::View* GetContentView();
+  views::View* GetContentView() const;
   SidePanelEntry* GetEntryForId(SidePanelEntry::Id entry_id);
 
   // Creates header and SidePanelEntry content container within the side panel.
@@ -89,7 +91,12 @@ class SidePanelCoordinator final : public SidePanelRegistryObserver,
 
   std::unique_ptr<views::View> CreateHeader();
   std::unique_ptr<views::Combobox> CreateCombobox();
-  void OnComboboxChanged();
+
+  // This is called after a user has made a selection in the combobox dropdown
+  // and before any selected id and combobox model change takes place. This
+  // allows us to make the entry displayed in the combobox follow the same
+  // delays as the side panel content when there are delays for loading content.
+  bool OnComboboxChangeTriggered(int index);
 
   // SidePanelRegistryObserver:
   void OnEntryRegistered(SidePanelEntry* entry) override;
