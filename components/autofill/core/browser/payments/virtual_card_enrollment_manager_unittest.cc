f8d110627793d59b45219c3eb71e1580e4f06634
Vishwas Uppoor <vishwasuppoor@google.com>
2022-06-14 16:28:52 +0000

    [VCN][M2] Metrics on Android do not match
    During Clank Settings page virtual card (un)enrollment, if users exit the Settings page immediately after accepting (un)enrollment dialog, the `Payments Client` gets destroyed which results in server call failure. This CL fixes the issue by adding a callback that triggers the Settings page cleanup after the server call is completed.
    
    The callback returns the update enrollment success status which is used to update the virtual card enrollment button text in real time.
    
    This CL also updates metrics logging for Settings page enrollment dialog close.
    
    Bug: 1281695, 1325437
    Change-Id: I68d9f265bb5eb1273faedd9fe10089b207be66fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647727
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014002}

--- components/autofill/core/browser/payments/virtual_card_enrollment_manager_unittest.cc
+++ components/autofill/core/browser/payments/virtual_card_enrollment_manager_unittest.cc
@@ -434,7 +434,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, Enroll) {
 
     payments_client_->set_update_virtual_card_enrollment_result(
         AutofillClient::PaymentsRpcResult::kSuccess);
-    virtual_card_enrollment_manager_->Enroll();
+    virtual_card_enrollment_manager_->Enroll(
+        /*virtual_card_enrollment_update_response_callback=*/absl::nullopt);
 
     payments::PaymentsClient::UpdateVirtualCardEnrollmentRequestDetails
         request_details =
@@ -475,7 +476,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, Enroll) {
     // Starts another request and makes sure it fails.
     payments_client_->set_update_virtual_card_enrollment_result(
         AutofillClient::PaymentsRpcResult::kVcnRetrievalPermanentFailure);
-    virtual_card_enrollment_manager_->Enroll();
+    virtual_card_enrollment_manager_->Enroll(
+        /*virtual_card_enrollment_update_response_callback=*/absl::nullopt);
 
     // Verifies the logging.
     histogram_tester.ExpectUniqueSample(
@@ -495,7 +497,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, Unenroll) {
       AutofillClient::PaymentsRpcResult::kNone);
 
   virtual_card_enrollment_manager_->Unenroll(
-      /*instrument_id=*/9223372036854775807);
+      /*instrument_id=*/9223372036854775807,
+      /*virtual_card_enrollment_update_response_callback=*/absl::nullopt);
 
   payments::PaymentsClient::UpdateVirtualCardEnrollmentRequestDetails
       request_details =
@@ -524,7 +527,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, Unenroll) {
   payments_client_->set_update_virtual_card_enrollment_result(
       AutofillClient::PaymentsRpcResult::kVcnRetrievalPermanentFailure);
   virtual_card_enrollment_manager_->Unenroll(
-      /*instrument_id=*/9223372036854775807);
+      /*instrument_id=*/9223372036854775807,
+      /*virtual_card_enrollment_update_response_callback=*/absl::nullopt);
 
   // Verifies the logging.
   histogram_tester.ExpectUniqueSample(
@@ -631,7 +635,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, StrikeDatabase_BubbleAccepted) {
       1);
 
   // Ensure a strike has been removed after enrollment accepted.
-  virtual_card_enrollment_manager_->Enroll();
+  virtual_card_enrollment_manager_->Enroll(
+      /*virtual_card_enrollment_update_response_callback=*/absl::nullopt);
   EXPECT_EQ(
       virtual_card_enrollment_manager_->GetVirtualCardEnrollmentStrikeDatabase()
           ->GetStrikes(
