759812918a67e58af79539c14bcf609c27c8c931
Bryant Chandler <bryantchandler@chromium.org>
2022-06-14 16:16:36 +0000

    [fuchsia] Make GetUserCacheDirectory adapt to profile_dir
    This is necessary in order to have caches separated for multiple
    profiles.
    
    Bug: 1321162, 1263566
    Change-Id: Ib5a744d18792b66914755c687efcaa2b3c7d8e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700815
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013994}

--- chrome/common/chrome_paths_fuchsia.cc
+++ chrome/common/chrome_paths_fuchsia.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/fuchsia/file_utils.h"
 #include "base/notreached.h"
+#include "base/path_service.h"
 
 namespace chrome {
 namespace {
@@ -28,7 +29,16 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
 
 void GetUserCacheDirectory(const base::FilePath& profile_dir,
                            base::FilePath* result) {
-  *result = base::FilePath(base::kPersistedCacheDirectoryPath);
+  *result = profile_dir;
+
+  base::FilePath user_data_dir;
+  if (!base::PathService::Get(DIR_USER_DATA, &user_data_dir))
+    return;
+  base::FilePath cache_dir(base::kPersistedCacheDirectoryPath);
+  if (!user_data_dir.AppendRelativePath(profile_dir, &cache_dir))
+    return;
+
+  *result = cache_dir;
 }
 
 bool GetUserDocumentsDirectory(base::FilePath* result) {
