db2a593d973312a44cbf762dc9368c115bd39bcc
Cici Ruan <cuicuiruan@google.com>
2022-06-14 01:54:20 +0000

    arc/gio: Show info/edu message when in kEdit mode
    After entering into kEdit mode, show the info/edu message to users to
    click on one label and press any label to customize. This info/edu
    message will show up again when nothing is focused.
    
    Bug: b:232948415 b:188452744
    Test: manual test and unittest
    Change-Id: Ie72210da964efd647d95bbfc5ef14d3a27aacca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700229
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013764}

--- chrome/browser/ash/arc/input_overlay/ui/action_label.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_label.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "ash/style/style_util.h"
-#include "base/strings/string_piece.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/action_label.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/action_view.h"
@@ -199,6 +198,8 @@ void ActionLabel::SetDisplayMode(DisplayMode mode) {
     case DisplayMode::kEdit:
       SetToEditMode();
       SetFocusBehavior(FocusBehavior::ALWAYS);
+      static_cast<ActionView*>(parent())->ShowInfoMsg(
+          l10n_util::GetStringUTF8(IDS_INPUT_OVERLAY_EDIT_INSTRUCTIONS), this);
       break;
     case DisplayMode::kEditedSuccess:
       SetToEditFocus();
@@ -266,14 +267,10 @@ void ActionLabel::OnFocus() {
   LabelButton::OnFocus();
   if (IsUnbound()) {
     static_cast<ActionView*>(parent())->ShowErrorMsg(
-        base::StringPiece(
-            l10n_util::GetStringUTF8(IDS_INPUT_OVERLAY_EDIT_MISSING_BINDING)),
-        this);
+        l10n_util::GetStringUTF8(IDS_INPUT_OVERLAY_EDIT_MISSING_BINDING), this);
   } else {
     static_cast<ActionView*>(parent())->ShowInfoMsg(
-        base::StringPiece(
-            l10n_util::GetStringUTF8(IDS_INPUT_OVERLAY_EDIT_FOCUSED_KEY)),
-        this);
+        l10n_util::GetStringUTF8(IDS_INPUT_OVERLAY_EDIT_FOCUSED_KEY), this);
   }
 }
 
