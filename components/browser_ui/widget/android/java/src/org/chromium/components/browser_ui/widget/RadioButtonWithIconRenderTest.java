5a2c7db329c180cd4aab39b506589e920c973753
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-06-14 11:24:25 +0000

    Revert "[Fixit] Migrate components/browser_ui/widget/android:javatest"
    This reverts commit fedfb78f21452d5f78175165f5943b4333c41c86.
    
    Reason for revert: speculative revert for: https://crbug.com/1336188
    
    Original change's description:
    > [Fixit] Migrate components/browser_ui/widget/android:javatest
    >
    > Move 19 test cases:
    > test_ic_more_vert_black_24dp.xml is accidentally referenced in
    > radio_button_with_icon_render_test.xml, so moving it into
    > //component/browser_ui/widget/android as well.
    >
    > Bug: 1331983
    > Change-Id: I60ef4f3b4bcbc52eec468263c115b3efb89c18ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703319
    > Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Reviewed-by: Gang Wu <gangwu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1013807}
    
    Bug: 1331983, 1336188
    Change-Id: I48ef6e62996f60c72e189bf677e9fe8a7db5d1a7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705206
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013899}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithIconRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithIconRenderTest.java
@@ -22,7 +22,6 @@ import org.chromium.base.test.params.ParameterAnnotations.ClassParameter;
 import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
-import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.browser_ui.widget.test.R;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -37,7 +36,6 @@ import java.util.List;
  */
 @RunWith(ParameterizedRunner.class)
 @UseRunnerDelegate(BaseJUnit4RunnerDelegate.class)
-@Batch(Batch.UNIT_TESTS)
 public class RadioButtonWithIconRenderTest extends BlankUiTestActivityTestCase {
     @ClassParameter
     private static List<ParameterSet> sClassParams =
--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/promo/PromoCardViewRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/promo/PromoCardViewRenderTest.java
@@ -25,7 +25,6 @@ import org.chromium.base.test.params.ParameterAnnotations.ClassParameter;
 import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
-import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
@@ -44,7 +43,6 @@ import java.util.List;
  */
 @RunWith(ParameterizedRunner.class)
 @UseRunnerDelegate(BaseJUnit4RunnerDelegate.class)
-@Batch(Batch.UNIT_TESTS)
 public class PromoCardViewRenderTest extends BlankUiTestActivityTestCase {
     @ClassParameter
     private static List<ParameterSet> sClassParams =
