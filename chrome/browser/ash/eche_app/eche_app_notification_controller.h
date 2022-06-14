4fc45802db4902c3297fdeeaf1b7f45cf01b4f2b
Guanru Lee <guanrulee@chromium.org>
2022-06-14 23:01:44 +0000

    Remove learn more buttons of notifications of Eche App
    Bug: b/232995139
    Test: unit_tests --gtest_filter=EcheAppNotificationControllerTest.*
    Change-Id: Icdd2c9773f11a3eb8bc8afa82f73300e4d9e5479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702902
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014186}

--- chrome/browser/ash/eche_app/eche_app_notification_controller.h
+++ chrome/browser/ash/eche_app/eche_app_notification_controller.h
@@ -50,7 +50,6 @@ class EcheAppNotificationController {
  protected:
   // Exposed for testing.
   virtual void LaunchSettings();
-  virtual void LaunchLearnMore();
   virtual void LaunchTryAgain();
 
  private:
