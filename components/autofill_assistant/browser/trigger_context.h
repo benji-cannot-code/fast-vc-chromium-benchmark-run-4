75ecdfed958054aaafc48d527ae0b546a916cf1d
Norge Vizcay <vizcay@google.com>
2022-06-14 16:17:44 +0000

    Allow headless API clients to show their own onboarding.
    The CL allows headless api android clients bypass autobot default onboarding and offer their own. At the moment, autobot's default consent screen is *always* shown for headless and non-headless runs for android clients.
    
    More context on https://docs.google.com/document/d/1Q3TahCExPaicxS-v9BnsZQ6-KfzfQggKXvKGFbZiiBQ/edit#bookmark=id.6n968u3nsi6b
    
    Bug: 1334642, b/201964911
    Change-Id: Ic9bc9d8e75b0cd0e5f5aa71276fc583fe8118dad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689422
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013996}

--- components/autofill_assistant/browser/trigger_context.h
+++ components/autofill_assistant/browser/trigger_context.h
@@ -24,7 +24,8 @@ class TriggerContext {
             bool onboarding_shown,
             bool is_direct_action,
             const std::string& initial_url,
-            bool is_in_chrome_triggered);
+            bool is_in_chrome_triggered,
+            bool is_externally_triggered);
     Options();
     ~Options();
     std::string experiment_ids;
@@ -33,6 +34,7 @@ class TriggerContext {
     bool is_direct_action = false;
     std::string initial_url;
     bool is_in_chrome_triggered = false;
+    bool is_externally_triggered = false;
   };
 
   // Creates an empty trigger context.
@@ -54,7 +56,8 @@ class TriggerContext {
                  bool onboarding_shown,
                  bool is_direct_action,
                  const std::string& initial_url,
-                 bool is_in_chrome_triggered);
+                 bool is_in_chrome_triggered,
+                 bool is_externally_triggered);
 
   // Creates a trigger context that contains the merged contents of all input
   // instances at the time of calling (does not reference |contexts| after
@@ -101,6 +104,10 @@ class TriggerContext {
   // i.e., a button or link on a website, or whether this is from within Chrome.
   virtual bool GetInChromeTriggered() const;
 
+  // Returns whether the triggering source will handle its own onboarding flow
+  // and the default onboarding flow should be skipped.
+  virtual bool GetIsExternallyTriggered() const;
+
   // Returns the trigger type of the trigger script that was shown and accepted
   // at the beginning of the flow, if any.
   virtual TriggerScriptProto::TriggerUIType GetTriggerUIType() const;
@@ -120,6 +127,7 @@ class TriggerContext {
   bool onboarding_shown_ = false;
   bool direct_action_ = false;
   bool is_in_chrome_triggered_ = false;
+  bool is_externally_triggered_ = false;
 
   // The initial url at the time of triggering.
   std::string initial_url_;
--- components/autofill_assistant/browser/trigger_context_unittest.cc
+++ components/autofill_assistant/browser/trigger_context_unittest.cc
@@ -34,7 +34,8 @@ TEST(TriggerContextTest, Create) {
       /* onboarding_shown = */ true,
       /* is_direct_action = */ true,
       /* initial_url = */ "https://www.example.com",
-      /* is_in_chrome_triggered = */ true};
+      /* is_in_chrome_triggered = */ true,
+      /* is_externally_triggered = */ true};
   EXPECT_THAT(
       context.GetScriptParameters().ToProto(),
       UnorderedElementsAreArray(base::flat_map<std::string, std::string>(
@@ -45,6 +46,7 @@ TEST(TriggerContextTest, Create) {
   EXPECT_TRUE(context.GetDirectAction());
   EXPECT_EQ(context.GetInitialUrl(), "https://www.example.com");
   EXPECT_TRUE(context.GetInChromeTriggered());
+  EXPECT_TRUE(context.GetIsExternallyTriggered());
   EXPECT_EQ(context.GetTriggerUIType(),
             TriggerScriptProto::UNSPECIFIED_TRIGGER_UI_TYPE);
 
@@ -106,7 +108,8 @@ TEST(TriggerContextTest, MergeNonEmptyWithNonEmpty) {
       /* onboarding_shown = */ true,
       /* is_direct_action = */ true,
       /* initial_url = */ "https://www.example.com",
-      /* is_in_chrome_triggered = */ true};
+      /* is_in_chrome_triggered = */ true,
+      /* is_externally_triggered = */ true};
   context2.SetTriggerUIType(
       TriggerScriptProto::SHOPPING_CHECKOUT_FIRST_TIME_USER);
 
@@ -123,6 +126,7 @@ TEST(TriggerContextTest, MergeNonEmptyWithNonEmpty) {
   EXPECT_TRUE(merged.GetDirectAction());
   EXPECT_EQ(merged.GetInitialUrl(), "https://www.example.com");
   EXPECT_TRUE(merged.GetInChromeTriggered());
+  EXPECT_TRUE(merged.GetIsExternallyTriggered());
   EXPECT_EQ(merged.GetTriggerUIType(),
             TriggerScriptProto::SHOPPING_CHECKOUT_FIRST_TIME_USER);
 }
