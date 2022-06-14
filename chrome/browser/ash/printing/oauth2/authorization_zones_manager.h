f1e01235bbd8b3630e92551e4d7ccd7a45826af1
Piotr Pawliczek <pawliczek@chromium.org>
2022-06-14 17:51:58 +0000

    OAuth2 for IPP: Factory for AuthorizationZonesManager
    AuthorizationZonesManager is going to be responsible for handling all
    existing instances of AuthorizationZone. This patch contains a factory
    for this class.
    
    BUG=b:201567116
    TEST=none
    
    Change-Id: I776a4b70fcca8c2caffa1b76c448ea962139a41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588884
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014046}

---
+++ chrome/browser/ash/printing/oauth2/authorization_zones_manager.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_PRINTING_OAUTH2_AUTHORIZATION_ZONES_MANAGER_H_
+#define CHROME_BROWSER_ASH_PRINTING_OAUTH2_AUTHORIZATION_ZONES_MANAGER_H_
+
+#include <memory>
+
+#include "components/keyed_service/core/keyed_service.h"
+
+class Profile;
+
+namespace ash {
+namespace printing {
+namespace oauth2 {
+
+class AuthorizationZonesManager : public KeyedService {
+ public:
+  static std::unique_ptr<AuthorizationZonesManager> Create(Profile* profile);
+  ~AuthorizationZonesManager() override;
+
+ protected:
+  AuthorizationZonesManager();
+};
+
+}  // namespace oauth2
+}  // namespace printing
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_PRINTING_OAUTH2_AUTHORIZATION_ZONES_MANAGER_H_
