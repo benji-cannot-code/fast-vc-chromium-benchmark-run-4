5c52b732c1dc33dea60725059f52f85c8737c897
Christian Dullweber <dullweber@chromium.org>
2022-06-14 09:10:34 +0000

    AboutThisSite: Add parameter to open minimal UI in bottomsheet
    Add 'ilrm=minimal' to the 'more about' URL to remove the top UI bar.
    Modify ephemeral tabs to take a 'fullPageUrl' parameter to show the full
    UI when opened in a regular tab.
    
    Bug: 1320685
    Change-Id: Ib57b6195c83c81cdb0cd6ace8e8f585806ebc3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702375
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013862}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.page_info;
 
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -92,10 +93,15 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
         mMainController.recordAction(action);
         if (mEphemeralTabCoordinatorSupplier != null
                 && mEphemeralTabCoordinatorSupplier.get() != null) {
-            String title =
-                    mRowView.getContext().getString(R.string.page_info_about_this_page_title);
-            mEphemeralTabCoordinatorSupplier.get().requestOpenSheet(
-                    new GURL(url), title, /*isIncognito=*/false);
+            // Append parameter to open the page with reduced UI elements in the bottomsheet.
+            Uri.Builder builder = Uri.parse(url).buildUpon();
+            if (mSiteInfo.hasMoreAbout() && url.equals(mSiteInfo.getMoreAbout().getUrl())) {
+                builder.appendQueryParameter("ilrm", "minimal");
+            }
+            GURL bottomSheetUrl = new GURL(builder.toString());
+            GURL fullPageUrl = new GURL(url);
+            mEphemeralTabCoordinatorSupplier.get().requestOpenSheetWithFullPageUrl(
+                    bottomSheetUrl, fullPageUrl, getTitle(), /*isIncognito=*/false);
             mMainController.dismiss();
         } else {
             new TabDelegate(/*incognito=*/false)
@@ -127,9 +133,7 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
         assert mSiteInfo.hasDescription();
         String subtitle = mSiteInfo.getDescription().getDescription();
         PageInfoRowView.ViewParams rowParams = new PageInfoRowView.ViewParams();
-        rowParams.title = mRowView.getContext().getResources().getString(more_info_enabled
-                        ? R.string.page_info_about_this_page_title
-                        : R.string.page_info_about_this_site_title);
+        rowParams.title = getTitle();
         rowParams.subtitle = subtitle;
         rowParams.singleLineSubTitle = true;
         rowParams.visible = true;
@@ -141,6 +145,13 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
                         PageInfoAction.PAGE_INFO_ABOUT_THIS_SITE_PAGE_OPENED)
                 : this::launchSubpage;
         mRowView.setParams(rowParams);
+    }
+
+    private String getTitle() {
+        return mRowView.getContext().getResources().getString(
+                ChromeFeatureList.isEnabled(ChromeFeatureList.PAGE_INFO_ABOUT_THIS_SITE_MORE_INFO)
+                        ? R.string.page_info_about_this_page_title
+                        : R.string.page_info_about_this_site_title);
     }
 
     private @Nullable SiteInfo getSiteInfo() {
