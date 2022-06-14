27cd1d56980fccb6c000744d252a58e438925161
Andy Paicu <andypaicu@chromium.org>
2022-06-14 09:30:32 +0000

    Enabled no-longer-flaky DSE geolocation tests
    Fixed: 1314109
    Fixed: 1333209
    Change-Id: I6526bfa26a2417396ed6317155b874656f6823de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705356
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013868}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/PermissionInfoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/PermissionInfoTest.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataBridge;
@@ -129,7 +128,6 @@ public class PermissionInfoTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_InPrimaryOTRProfile_DefaultsToAskFromBlock()
             throws Throwable {
         Profile primaryOTRProfile = getPrimaryOTRProfile();
@@ -144,7 +142,6 @@ public class PermissionInfoTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_InNonPrimaryOTRProfile_DefaultsToAskFromBlock()
             throws Throwable {
         Profile nonPrimaryOTRProfile = getNonPrimaryOTRProfile();
@@ -160,7 +157,6 @@ public class PermissionInfoTest {
     @SmallTest
     @Feature({"Preferences"})
     @RequiresRestart
-    @DisabledTest(message = "Flaky - https://crbug.com/1314109")
     public void testResetDSEGeolocation_RegularProfile_DefaultsToAskFromBlock() throws Throwable {
         Profile regularProfile = getRegularProfile();
         setGeolocation(DSE_ORIGIN, null, ContentSettingValues.BLOCK, regularProfile);
