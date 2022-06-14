dabebc4fc19a3fb38560264c6831c13e54e68f46
jhtin <jhtin@chromium.org>
2022-06-14 01:20:19 +0000

    Diacritics Suggest should disable highlight of previous button.
    When new highlight occurs the previous highlight should be disabled.
    
    Bug: b/217560706
    Change-Id: I3cccfec466ac5c0ef082a60fe6c14110e2c5e7fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697663
    Auto-Submit: jhtin <jhtin@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013750}

--- chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
+++ chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
@@ -120,6 +120,7 @@ SuggestionStatus LongpressDiacriticsSuggester::HandleKeyEvent(
         new_index =
             (code == kNextDomCode) ? 0 : GetCurrentShownDiacritics().size() - 1;
       } else {
+        SetButtonHighlighted(*highlighted_index_, false);
         if (code == kNextDomCode) {
           new_index =
               (*highlighted_index_ + 1) % GetCurrentShownDiacritics().size();
@@ -129,7 +130,8 @@ SuggestionStatus LongpressDiacriticsSuggester::HandleKeyEvent(
                           : GetCurrentShownDiacritics().size() - 1;
         }
       }
-      SetButtonHighlighted(new_index);
+      SetButtonHighlighted(new_index, true);
+      highlighted_index_ = new_index;
       return SuggestionStatus::kBrowsing;
     default:
       return SuggestionStatus::kNotHandled;
@@ -215,7 +217,8 @@ LongpressDiacriticsSuggester::GetSuggestions() {
   return {};
 }
 
-void LongpressDiacriticsSuggester::SetButtonHighlighted(size_t index) {
+void LongpressDiacriticsSuggester::SetButtonHighlighted(size_t index,
+                                                        bool highlighted) {
   if (!focused_context_id_.has_value()) {
     LOG(ERROR) << "suggest: Failed to set button highlighted. No context id.";
     return;
@@ -224,13 +227,11 @@ void LongpressDiacriticsSuggester::SetButtonHighlighted(size_t index) {
   suggestion_handler_->SetButtonHighlighted(
       *focused_context_id_,
       CreateButtonFor(index, GetCurrentShownDiacritics()[index]),
-      /* highlighted=*/true, &error);
+      /* highlighted=*/highlighted, &error);
 
   if (!error.empty()) {
     LOG(ERROR) << "suggest: Failed to set button highlighted. " << error;
   }
-
-  highlighted_index_ = index;
 }
 
 std::vector<std::u16string>
