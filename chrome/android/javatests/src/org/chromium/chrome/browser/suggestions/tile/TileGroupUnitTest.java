d0f25cc178b8fd761eb7795419329e098059f3dc
Wenyu Fu <wenyufu@google.com>
2022-06-14 19:06:06 +0000

    Disable flaky test cases for TileGroupUnitTest
    The tests will fail if running on their self, but not when batched with
    other tests.
    
    Bug: 1293208, 1330627
    Change-Id: I0c6c544663b0bb6b14d6d3df8e76342020071ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703061
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014091}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
@@ -44,6 +44,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
@@ -202,6 +203,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testReceiveNewTilesWithDataChanges_TrackLoad() {
         TileGroup tileGroup = initialiseTileGroup(/* deferLoad: */ true, URLS);
 
@@ -295,6 +297,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testRenderTileView() {
         SuggestionsUiDelegate uiDelegate = mSuggestionsUiDelegate;
         when(uiDelegate.getImageFetcher()).thenReturn(mImageFetcher);
@@ -391,6 +394,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testIconLoadingForInit() {
         TileGroup tileGroup = initialiseTileGroup(URLS);
         Tile tile = tileGroup.getTileSections().get(TileSectionType.PERSONALIZED).get(0);
@@ -427,6 +431,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testIconLoading_Sync() {
         TileGroup tileGroup = initialiseTileGroup();
         mImageFetcher.fulfillLargeIconRequests();
@@ -446,6 +451,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testIconLoading_AsyncNoTrack() {
         TileGroup tileGroup = initialiseTileGroup(/* deferLoad: */ true);
         mImageFetcher.fulfillLargeIconRequests();
@@ -466,6 +472,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1330627, https://crbug.com/1293208")
     public void testIconLoading_AsyncTrack() {
         TileGroup tileGroup = initialiseTileGroup(/* deferLoad: */ true);
         mImageFetcher.fulfillLargeIconRequests();
