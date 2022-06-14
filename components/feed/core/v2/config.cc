d1f50639fa9ed507078e97616aba8314e47e2dfc
Dan Harrington <harringtond@chromium.org>
2022-06-14 19:56:52 +0000

    Revert "Reland "Revert "Optionally force feed-query requests through snippets-internals"""
    This reverts commit ec418810388c8d2224df90658d18945a4511af9d.
    
    Fix is to use setUseFeedQueryRequests instead of
                  SetUseFeedQueryRequests
    
    Bug: b/233757219
    Change-Id: I426c71883f40daf0646a81896370a66ea5aadeb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703783
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014116}

--- components/feed/core/v2/config.cc
+++ components/feed/core/v2/config.cc
@@ -172,9 +172,8 @@ void OverrideWithFinch(Config& config) {
 }
 
 void OverrideWithSwitches(Config& config) {
-  config.use_feed_query_requests_for_web_feeds =
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          "webfeed-legacy-feedquery");
+  config.use_feed_query_requests =
+      base::CommandLine::ForCurrentProcess()->HasSwitch("use-legacy-feedquery");
 }
 
 }  // namespace
@@ -190,10 +189,9 @@ const Config& GetFeedConfig() {
 }
 
 // This is a dev setting that updates Config, which is supposed to be constant.
-// TODO(crbug/1152592): remove when not needed anymore.
-void SetUseFeedQueryRequestsForWebFeeds(const bool use_legacy) {
+void SetUseFeedQueryRequests(const bool use_legacy) {
   Config& config = const_cast<Config&>(GetFeedConfig());
-  config.use_feed_query_requests_for_web_feeds = use_legacy;
+  config.use_feed_query_requests = use_legacy;
 }
 
 void SetFeedConfigForTesting(const Config& config) {
