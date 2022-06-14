b1cd19bfaef29cc70e62130c56e9f5d6eab36c81
Sebastien Lalancette <seblalancette@chromium.org>
2022-06-14 14:57:45 +0000

    [NewEV] Get AV Signal from WMI on Win7
    This CL stitches WMI AV usage into the AV signal retrieval flow on
    Win7. It also fixes a few typing issues and missing COM initialization.
    
    Fixed: 231743012
    Change-Id: I890049b09059939325d88945f58e9dbd0c76fd4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697441
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1013954}

--- chrome/services/system_signals/win/win_system_signals_service.h
+++ chrome/services/system_signals/win/win_system_signals_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/win/scoped_com_initializer.h"
 #include "components/device_signals/core/common/mojom/system_signals.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -53,6 +54,7 @@ class WinSystemSignalsService
   mojo::Receiver<device_signals::mojom::SystemSignalsService> receiver_;
   std::unique_ptr<device_signals::WmiClient> wmi_client_;
   std::unique_ptr<device_signals::WscClient> wsc_client_;
+  base::win::ScopedCOMInitializer scoped_com_initializer_;
 };
 
 }  // namespace system_signals
--- components/device_signals/core/common/win/wsc_client_impl_unittest.cc
+++ components/device_signals/core/common/win/wsc_client_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/test/task_environment.h"
+#include "base/win/scoped_com_initializer.h"
 #include "base/win/windows_version.h"
 #include "components/device_signals/core/common/win/com_fakes.h"
 #include "components/device_signals/core/common/win/win_types.h"
@@ -40,7 +41,7 @@ class WscClientImplTest : public testing::Test {
       : wsc_client_(base::BindRepeating(&WscClientImplTest::CreateProductList,
                                         base::Unretained(this))) {}
 
-  HRESULT CreateProductList(IWSCProductList** product_list) {
+  HRESULT CreateProductList(ComPtr<IWSCProductList>* product_list) {
     if (fail_list_creation_) {
       return E_FAIL;
     }
@@ -224,6 +225,8 @@ TEST_F(WscClientImplTest, GetAntiVirusProducts_ProductErrors) {
 // Smoke/sanity test to verify that Defender's instance GUID does not change
 // over time. This test actually calls WSC.
 TEST(RealWscClientImplTest, SmokeWsc_GetAntiVirusProducts) {
+  base::win::ScopedCOMInitializer scoped_com_initializer;
+
   // That part of the display name is not translated when getting it from WSC,
   // so it can be used quite simply.
   constexpr char kPartialDefenderName[] = "Microsoft Defender";
