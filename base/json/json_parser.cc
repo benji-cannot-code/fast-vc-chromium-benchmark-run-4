94fcecbe82c8d32a32af25601a87761f44db017b
Matt Menke <mmenke@chromium.org>
2022-06-14 20:17:39 +0000

    Remove base::Value::DeprecatedDictStorage.
    Bug: 1187001
    Change-Id: I5193e0c113c297f7170e0e371252d28a8758d7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696767
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014125}

--- base/json/json_parser.cc
+++ base/json/json_parser.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_parser.h"
 
 #include <cmath>
+#include <iterator>
 #include <utility>
 #include <vector>
 
@@ -424,7 +425,7 @@ absl::optional<Value> JSONParser::ConsumeDictionary() {
     return absl::nullopt;
   }
 
-  std::vector<Value::DeprecatedDictStorage::value_type> dict_storage;
+  std::vector<std::pair<std::string, Value>> values;
 
   Token token = GetNextToken();
   while (token != T_OBJECT_END) {
@@ -454,7 +455,7 @@ absl::optional<Value> JSONParser::ConsumeDictionary() {
       return absl::nullopt;
     }
 
-    dict_storage.emplace_back(key.DestructiveAsString(), std::move(*value));
+    values.emplace_back(key.DestructiveAsString(), std::move(*value));
 
     token = GetNextToken();
     if (token == T_LIST_SEPARATOR) {
@@ -473,8 +474,9 @@ absl::optional<Value> JSONParser::ConsumeDictionary() {
   ConsumeChar();  // Closing '}'.
   // Reverse |dict_storage| to keep the last of elements with the same key in
   // the input.
-  ranges::reverse(dict_storage);
-  return Value(Value::DeprecatedDictStorage(std::move(dict_storage)));
+  ranges::reverse(values);
+  return Value(Value::Dict(std::make_move_iterator(values.begin()),
+                           std::make_move_iterator(values.end())));
 }
 
 absl::optional<Value> JSONParser::ConsumeList() {
