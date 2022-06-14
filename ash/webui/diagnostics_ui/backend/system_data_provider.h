47ee5f7229730bddf73c1fb3c902949439b9335f
David Padlipsky <dpad@google.com>
2022-06-14 23:15:55 +0000

    Diagnostics: Reset mojo receivers before binding
    A requirement of binding to a mojo receiver is that the receiver is
    not bound at the time Bind() is called. When a user refreshes the
    Diagnostics app, the mojo receivers are re-bound without being reset.
    This causes a DCHECK error resulting in a crash when DCHECK is enabled.
    This change resets the receivers every time before calling Bind().
    
    This was previously thought to be fixed by unbinding the receivers when
    the corresponding remote is closed. This did not help because on page
    refresh, the old remote is not closed.
    
    Bug: b/235844295
    Test: ash_webui_unittests
    Change-Id: Ie9a57394bcb64275ff98c16a8300ee67a7dffc96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703195
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014192}

--- ash/webui/diagnostics_ui/backend/system_data_provider.h
+++ ash/webui/diagnostics_ui/backend/system_data_provider.h
@@ -69,10 +69,6 @@ class SystemDataProvider : public mojom::SystemDataProvider,
 
   void SetCpuUsageTimerForTesting(std::unique_ptr<base::RepeatingTimer> timer);
 
-  // Handler for when remote attached to |receiver_| disconnects.
-  void OnBoundInterfaceDisconnect();
-  bool ReceiverIsBound();
-
  private:
   void BindCrosHealthdProbeServiceIfNeccessary();
 
