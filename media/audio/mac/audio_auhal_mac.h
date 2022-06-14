4557b6fa5b9b5e977895c848f2b5658ce5cb3a98
Sam Zackrisson <saza@chromium.org>
2022-06-14 13:58:01 +0000

    Report OS audio render glitch metrics every 10 seconds
    Before this CL, OS glitch metrics are reported per stream lifetime, which gives disproportionate weight to short streams and transient issues on stream start.
    
    Text logs are still reported once per active stream.
    
    Tested: Manually tested on Windows, the reported glitch metrics looked reasonable.
    
    Bug: chromium:1331255
    Change-Id: I4d9863c8be12b15a41286de90ae2377041da12e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695571
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013933}

--- media/audio/mac/audio_auhal_mac.h
+++ media/audio/mac/audio_auhal_mac.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_manager.h"
 #include "media/audio/mac/scoped_audio_unit.h"
+#include "media/audio/system_output_glitch_reporter.h"
 #include "media/base/audio_parameters.h"
 
 namespace media {
@@ -201,9 +202,10 @@ class AUHALStream : public AudioOutputStream {
   // NOTE: Float64 and UInt32 types are used for native API compatibility.
   Float64 last_sample_time_ GUARDED_BY(lock_);
   UInt32 last_number_of_frames_ GUARDED_BY(lock_);
-  UInt32 total_lost_frames_ GUARDED_BY(lock_);
-  UInt32 largest_glitch_frames_ GUARDED_BY(lock_);
-  int glitches_detected_ GUARDED_BY(lock_);
+
+  // Used to aggregate and report glitch metrics to UMA (periodically) and to
+  // text logs (when a stream ends).
+  SystemOutputGlitchReporter glitch_reporter_ GUARDED_BY(lock_);
 
   // Used to defer Start() to workaround http://crbug.com/160920.
   base::CancelableOnceClosure deferred_start_cb_;
