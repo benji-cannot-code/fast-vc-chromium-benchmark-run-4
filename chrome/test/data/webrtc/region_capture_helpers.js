2797d9128a24e0c9d37bd00966229ca162e7c652
Rakina Zata Amni <rakina@chromium.org>
2022-06-15 05:43:04 +0000

    Revert "[cropToPromise][#6] Resolve cropTo() Promises at the right time"
    This reverts commit e429d0108ad76a7e1789965e220e4057fdf233e3.
    
    Reason for revert: Potentially causing test failures, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20Tests%20(sandboxed)/90599/overview
    
    Original change's description:
    > [cropToPromise][#6] Resolve cropTo() Promises at the right time
    >
    > Tie the CL chain together by observing the crop_version on the
    > metadata of delivered frames in Blink, and allowing the Promise
    > which cropTo() returned to be resolved, only after the crop_version
    > has been observed. (Or a later one.)
    >
    > Bug: 1303138, 1266378
    > Change-Id: I1cd4fb2059d67368fd0cb5dd198642633b91c675
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497676
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1014107}
    
    Bug: 1303138, 1266378
    Change-Id: I97cc222dcf6af95910d84e823a12a818e34596d1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703127
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014311}

--- chrome/test/data/webrtc/region_capture_helpers.js
+++ chrome/test/data/webrtc/region_capture_helpers.js
@@ -66,12 +66,6 @@ async function createNewDivElement(targetFrame, divId) {
   }
 }
 
-function animate(element) {
-  const animationCallback = function() {
-    element.innerHTML = parseInt(element.innerHTML) + 1;
-  };
-  setInterval(animationCallback, 20);
-}
 
 /////////////////////////////////////////
 // Main actions from C++ test fixture. //
--- third_party/blink/renderer/modules/mediastream/browser_capture_media_stream_track.h
+++ third_party/blink/renderer/modules/mediastream/browser_capture_media_stream_track.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_BROWSER_CAPTURE_MEDIA_STREAM_TRACK_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_BROWSER_CAPTURE_MEDIA_STREAM_TRACK_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/mediastream/crop_target.h"
 #include "third_party/blink/renderer/modules/mediastream/focusable_media_stream_track.h"
@@ -33,16 +32,8 @@ class MODULES_EXPORT BrowserCaptureMediaStreamTrack final
                                  const String& descriptor_id,
                                  bool is_clone = false);
 
-  ~BrowserCaptureMediaStreamTrack() override = default;
-
 #if !BUILDFLAG(IS_ANDROID)
   void Trace(Visitor*) const override;
-
-  // Allows tests to invoke OnCropVersionObserved() directly, since triggering
-  // it via mocks would be prohibitively difficult.
-  void OnCropVersionObservedForTesting(uint32_t crop_version) {
-    OnCropVersionObserved(crop_version);
-  }
 #endif
 
   ScriptPromise cropTo(ScriptState*, CropTarget*, ExceptionState&);
@@ -51,48 +42,9 @@ class MODULES_EXPORT BrowserCaptureMediaStreamTrack final
 
  private:
 #if !BUILDFLAG(IS_ANDROID)
-  struct CropPromiseInfo : GarbageCollected<CropPromiseInfo> {
-    explicit CropPromiseInfo(ScriptPromiseResolver* promise_resolver)
-        : promise_resolver(promise_resolver) {}
-
-    void Trace(Visitor* visitor) const { visitor->Trace(promise_resolver); }
-
-    const Member<ScriptPromiseResolver> promise_resolver;
-    absl::optional<media::mojom::CropRequestResult> crop_result;
-    bool crop_version_observed = false;
-  };
-
-  using CropVersionToPromiseInfoMap =
-      HeapHashMap<uint32_t,
-                  Member<BrowserCaptureMediaStreamTrack::CropPromiseInfo>>;
-  using PromiseMapIterator = CropVersionToPromiseInfoMap::iterator;
-
-  // Each cropTo() call is associated with a unique |crop_version| which
-  // identifies this specific cropTo() invocation. When the browser process
-  // responds with the result of the cropTo() invocation, it triggers
-  // a call to OnResultFromBrowserProcess() with that |crop_version|.
-  void OnResultFromBrowserProcess(uint32_t crop_version,
-                                  media::mojom::CropRequestResult result);
-
-  // OnCropVersionObserved() is posted as a callback, bound to a unique
-  // |crop_version|. This callback be invoked when the first frame is observed
-  // which is associated with that |crop_version|.
-  // TODO(crbug.com/1266378): The Promise should also be resolved if a
-  // a barrier event is observed. (That is, although no frame is delivered,
-  // there is a guarantee that all future frames will be of this version
-  // or later. This would happen if cropping a muted track, for instance.)
-  void OnCropVersionObserved(uint32_t crop_version);
-
-  // The Promise that cropTo() issued is resolved when both conditions
-  // are fulfulled:
-  // 1. OnResultFromBrowserProcess(kSuccess) called.
-  // 2. OnCropVersionObserved() called for the associated |crop_version|.
-  //
-  // The order of fulfillment does not matter.
-  //
-  // The Promise is rejected if OnResultFromBrowserProcess() is called with
-  // an error value.
-  void MaybeFinalizeCropPromise(PromiseMapIterator iter);
+  // Resolves the Promise associated with |crop_version|.
+  void ResolveCropPromise(uint32_t crop_version,
+                          media::mojom::CropRequestResult result);
 
   // Each time cropTo() is called on a given track, its crop version increments.
   // Associate each Promise with its crop version, so that Viz can easily stamp
@@ -103,7 +55,7 @@ class MODULES_EXPORT BrowserCaptureMediaStreamTrack final
   //
   // Note that frames before the first call to cropTo() will be associated
   // with a version of 0, both here and in Viz.
-  HeapHashMap<uint32_t, Member<CropPromiseInfo>> pending_promises_;
+  HeapHashMap<uint32_t, Member<ScriptPromiseResolver>> pending_promises_;
 #endif  // !BUILDFLAG(IS_ANDROID)
 };
 
