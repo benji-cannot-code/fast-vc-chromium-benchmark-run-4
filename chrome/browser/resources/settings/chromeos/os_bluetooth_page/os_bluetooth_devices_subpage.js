1cc862a3a11803fba1bd72d8026637d4df336601
Alex Kingsborough <akingsb@google.com>
2022-06-15 23:07:22 +0000

    [Fast Pair] Add link row for saved devices
    Add a link row to take users to the saved devices page.
    Link row action will be added later
    
    Change-Id: I2fc60ef6ed3475d369c8284f8f5952834703882c
    Bug: b/235412471
    TESTED: Manually checked UI on DUT and added unit test
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700738
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014694}

--- chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_devices_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_devices_subpage.js
@@ -111,6 +111,16 @@ class SettingsBluetoothDevicesSubpageElement extends
       },
 
       /**
+       * @private
+       */
+      savedDevicesSublabel_: {
+        type: String,
+        value() {
+          return loadTimeData.getString('sublableWithEmail');
+        },
+      },
+
+      /**
        * @private {!Array<!PairedBluetoothDeviceProperties>}
        */
       unconnectedDevices_: {
@@ -299,6 +309,15 @@ class SettingsBluetoothDevicesSubpageElement extends
   isFastPairToggleVisible_() {
     return this.isFastPairSupportedByDevice_ &&
         loadTimeData.getBoolean('enableFastPairFlag');
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  isFastPairSavedDevicesRowVisible_() {
+    return this.isFastPairSupportedByDevice_ &&
+        loadTimeData.getBoolean('enableSavedDevicesFlag');
   }
 }
 
