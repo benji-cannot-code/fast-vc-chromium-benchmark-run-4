06ebaea8bb8bf39d6c69dd955b0fe8b4a1f67e1a
Chu-Hsuan Yang <chuhsuan@chromium.org>
2022-06-15 09:54:20 +0000

    CCA: Adjust options focus order
    * Change focus order of PTZ buttons and review options.
    * Set primary button of Review manually instead of using first option.
    * Set name attribute on radio buttons in settings to make keyboard
    navigation behavior consistent.
    
    Bug: b:232562096
    Test: Manually
    Change-Id: I49a2ed00edb6a0b72d6cbcee84f88c04f5eaa202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703899
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014365}

--- ash/webui/camera_app_ui/resources/js/views/review.ts
+++ ash/webui/camera_app_ui/resources/js/views/review.ts
@@ -19,6 +19,7 @@ interface UIArgs {
   text?: I18nString;
   label?: I18nString;
   templateId?: string;
+  primary?: boolean;
 }
 
 /**
@@ -159,8 +160,12 @@ export class Review<T> extends View {
     }
     for (const btnGroup of this.btnGroups) {
       const addButton =
-          ({uiArgs: {text, label, templateId}, exitValue, callback, hasPopup}:
-               Option<T|null>) => {
+          ({
+            uiArgs: {text, label, templateId, primary},
+            exitValue,
+            callback,
+            hasPopup,
+          }: Option<T|null>) => {
             const templ = instantiateTemplate(
                 templateId !== undefined ? `#${templateId}` :
                                            '#text-button-template');
@@ -171,7 +176,7 @@ export class Review<T> extends View {
             if (label !== undefined) {
               btn.setAttribute('i18n-label', label);
             }
-            if (this.primaryBtn === null) {
+            if (this.primaryBtn === null && primary === true) {
               btn.classList.add('primary');
               this.primaryBtn = btn;
             } else {
