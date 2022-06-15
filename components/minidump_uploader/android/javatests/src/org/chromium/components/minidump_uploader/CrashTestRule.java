08df6c72ec4a4265eff8ede7489d175bd3a88297
Yaron Friedman <yfriedman@chromium.org>
2022-06-15 16:09:19 +0000

    [Fixit] Migrated minidump_uploader tests to Robolectric.
    As part of this, changed CrashTestRule to not init the browser process, and moved that init to the one test that cared about.
    Fixed: 1332331, 1332332, 1332333, 1332334
    
    Change-Id: Ib374f962bdd73ae12d75f97b6bd7f2a50f557f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703465
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014466}

--- components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/CrashTestRule.java
+++ components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/CrashTestRule.java
@@ -11,8 +11,6 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
-import org.chromium.base.library_loader.LibraryLoader;
-import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.components.minidump_uploader.util.CrashReportingPermissionManager;
 
 import java.io.File;
@@ -28,7 +26,6 @@ public class CrashTestRule implements TestRule {
 
     private File mCrashDir;
     private File mCacheDir;
-    private @LibraryProcessType int mLibraryProcessType;
 
     @Override
     public Statement apply(final Statement base, final Description desc) {
@@ -50,17 +47,7 @@ public class CrashTestRule implements TestRule {
         return mCacheDir;
     }
 
-    public CrashTestRule(@LibraryProcessType int libraryProcessType) {
-        mLibraryProcessType = libraryProcessType;
-    }
-
-    public CrashTestRule() {
-        this(LibraryProcessType.PROCESS_BROWSER);
-    }
-
     private void setUp() throws Exception {
-        LibraryLoader.getInstance().setLibraryProcessType(mLibraryProcessType);
-        LibraryLoader.getInstance().ensureInitialized();
         if (mCacheDir == null) {
             mCacheDir = getExistingCacheDir();
         }
--- components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/MinidumpUploadCallableTest.java
+++ components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/MinidumpUploadCallableTest.java
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 package org.chromium.components.minidump_uploader;
-
 import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
@@ -12,8 +11,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
-import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.minidump_uploader.CrashTestRule.MockCrashReportingPermissionManager;
 import org.chromium.components.minidump_uploader.MinidumpUploadCallable.MinidumpUploadStatus;
@@ -27,7 +27,8 @@ import java.io.IOException;
 /**
  * Unittests for {@link MinidumpUploadCallable}.
  */
-@RunWith(BaseJUnit4ClassRunner.class)
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class MinidumpUploadCallableTest {
     private static final String LOCAL_CRASH_ID = "123_log";
     private static final String LOG_FILE_NAME = "chromium_renderer-123_log.dmp224";
