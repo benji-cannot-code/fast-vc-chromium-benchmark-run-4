cd0e543c50049f09872f04bb5ac0ca63fb0f7615
Phillis Tang <phillis@chromium.org>
2022-06-15 22:01:47 +0000

    Revert "Add a browser test of ContentMainDelegate entry points"
    This reverts commit 34857a06916073a5361240cfc659032f3c52d80d.
    
    Reason for revert: cause failure in tsan build
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/76554/overview
    
    Original change's description:
    > Add a browser test of ContentMainDelegate entry points
    >
    > This tests that ContentMainRunnerImpl invokes the ContentMainDelegate
    > entry points in the expected order, with the expected parameters, in
    > both browser and child processes.
    >
    > The test crashes on Android, which uses custom startup code in
    > tests instead of calling ContentMain, so it is not enabled there.
    >
    > R=​gab
    >
    > Bug: 1327069
    > Change-Id: Ifaf6a7ed6a61638f6ddb73fdd57dd4863c7e6f46
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697377
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Auto-Submit: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1014301}
    
    Bug: 1327069
    Change-Id: I149cd7c2319f07a148016094fba1554b3d928911
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708952
    Owners-Override: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1014663}

--- content/public/test/browser_test_base.h
+++ content/public/test/browser_test_base.h
@@ -43,7 +43,6 @@ class ScopedDisableCrosapiForTesting;
 
 namespace content {
 class BrowserMainParts;
-class ContentMainDelegate;
 class WebContents;
 
 class BrowserTestBase : public ::testing::Test {
@@ -123,12 +122,6 @@ class BrowserTestBase : public ::testing::Test {
   // Called after the BrowserMainParts have been created, and before
   // PreEarlyInitialization() has been called.
   virtual void CreatedBrowserMainParts(BrowserMainParts* browser_main_parts) {}
-
-  // Override this to provide a custom ContentMainDelegate for the test. The
-  // returned object must live at least until
-  // TearDownInProcessBrowserTextFixture is called. If nullptr is returned the
-  // test will use the default delegate.
-  virtual ContentMainDelegate* GetCustomContentMainDelegate();
 
   // GTest assertions that the connection to `network_service_test_` did not get
   // dropped unexpectedly.
