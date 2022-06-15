e5efe51e0e91219fc4dc862c4d88b513b2259197
William Liu <liuwilliam@chromium.org>
2022-06-15 19:31:18 +0000

    Display FencedFrame and Prerender2 tasks in TaskManager.
    This is the follow up CL to 3641650 that introduces the two tasks in TaskManager. To accommodate the two tasks, the task creation flow has been modified, that the majority of the tasks are created in |DidFinishNavigation| as opposed to |RenderFrameCreated|. DD: go/fix-tskmgr (internal access).
    
    The changes are:
    - CreateAllTasks: create tasks for each RFH of the WC (instead of each frame of the WC).
    - RenderFrameCreated: method removed.
    - RenderFrameHostChanged: no longer creates tasks for new_host.
    - RenderFrameHostStateChanged: also deletes tasks for activated prerender page.
    - DidFinishNavigation: also creates tasks (in addition to updates).
    
    
    Bug: 1267167,1248383,1247223
    Change-Id: I4f00346c2125afeee7492f0c3ffbbe729665aa66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3649959
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014575}

--- chrome/browser/task_manager/providers/web_contents/fenced_frame_task.cc
+++ chrome/browser/task_manager/providers/web_contents/fenced_frame_task.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/site_instance.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
@@ -20,14 +21,17 @@ FencedFrameTask::FencedFrameTask(content::RenderFrameHost* render_frame_host,
           /*title=*/u"",
           /*icon=*/nullptr,
           /*subframe=*/render_frame_host),
-      render_frame_host_(render_frame_host),
+      site_instance_(render_frame_host->GetSiteInstance()),
       embedder_task_(embedder_task) {
   set_title(GetTitle());
 }
 
 void FencedFrameTask::Activate() {
-  DCHECK(embedder_task_);
   embedder_task_->Activate();
+}
+
+const task_manager::Task* FencedFrameTask::GetParentTask() const {
+  return embedder_task_;
 }
 
 void FencedFrameTask::UpdateTitle() {
@@ -35,13 +39,10 @@ void FencedFrameTask::UpdateTitle() {
 }
 
 std::u16string FencedFrameTask::GetTitle() const {
-  DCHECK(render_frame_host_);
-  const auto message_id =
-      render_frame_host_->GetBrowserContext()->IsOffTheRecord()
-          ? IDS_TASK_MANAGER_FENCED_FRAME_INCOGNITO_PREFIX
-          : IDS_TASK_MANAGER_FENCED_FRAME_PREFIX;
-  const auto title =
-      base::UTF8ToUTF16(render_frame_host_->GetLastCommittedURL().spec());
+  const auto message_id = site_instance_->GetBrowserContext()->IsOffTheRecord()
+                              ? IDS_TASK_MANAGER_FENCED_FRAME_INCOGNITO_PREFIX
+                              : IDS_TASK_MANAGER_FENCED_FRAME_PREFIX;
+  const auto title = base::UTF8ToUTF16(site_instance_->GetSiteURL().spec());
   return l10n_util::GetStringFUTF16(message_id, title);
 }
 
--- chrome/browser/task_manager/providers/web_contents/prerender_task.cc
+++ chrome/browser/task_manager/providers/web_contents/prerender_task.cc
@@ -7,20 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/task_manager/providers/web_contents/web_contents_task_provider.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/grit/theme_resources.h"
-#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 namespace task_manager {
 
-PrerenderTask::PrerenderTask(content::RenderFrameHost* render_frame_host)
+PrerenderTask::PrerenderTask(content::RenderFrameHost* render_frame_host,
+                             WebContentsTaskProvider* task_provider)
     : RendererTask(
           /*title=*/u"",
           /*icon=*/nullptr,
           /*subframe=*/render_frame_host),
-      render_frame_host_(render_frame_host) {
+      render_frame_host_(render_frame_host),
+      task_provider_(task_provider) {
   DCHECK(render_frame_host_);
   DCHECK_EQ(render_frame_host->GetLifecycleState(),
             content::RenderFrameHost::LifecycleState::kPrerendering);
@@ -29,12 +32,15 @@ PrerenderTask::PrerenderTask(content::RenderFrameHost* render_frame_host)
 
 PrerenderTask::~PrerenderTask() = default;
 
+const Task* PrerenderTask::GetParentTask() const {
+  return task_provider_->GetTaskOfFrame(web_contents()->GetPrimaryMainFrame());
+}
+
 void PrerenderTask::UpdateTitle() {
   set_title(GetTitle());
 }
 
 std::u16string PrerenderTask::GetTitle() const {
-  DCHECK(render_frame_host_);
   const auto title =
       base::UTF8ToUTF16(render_frame_host_->GetLastCommittedURL().spec());
   return l10n_util::GetStringFUTF16(IDS_TASK_MANAGER_PRERENDER_PREFIX, title);
