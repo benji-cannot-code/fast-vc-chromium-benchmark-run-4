44591d580638f41c1f90ce3081f0a325e446e8be
YuhanYang <yyhyyh@google.com>
2022-06-15 05:00:05 +0000

    Feedback: Localize attach screenshot string
    Screenshot: https://screenshot.googleplex.com/ASR4Sbc2z5LUJGz
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: Ia5629af7b847a0d6965ab383e0faae6869060f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700813
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014302}

--- chrome/test/data/webui/chromeos/os_feedback_ui/share_data_page_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/share_data_page_test.js
@@ -109,6 +109,7 @@ export function shareDataPageTestSuite() {
     // Screenshot elements.
     assertTrue(!!getElement('#screenshotCheckbox'));
     assertEquals('Screenshot', getElementContent('#screenshotCheckLabel'));
+    assertTrue(page.i18nExists('attachScreenshotLabel'));
     assertTrue(!!getElement('#screenshotImage'));
 
     // Add file attachment element.
