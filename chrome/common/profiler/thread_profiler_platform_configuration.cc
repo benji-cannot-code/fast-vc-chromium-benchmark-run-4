103152c8c202927712723efc2cabfb53420473c1
Tushar Agarwal <agarwaltushar@google.com>
2022-06-15 16:39:10 +0000

    Enable profiling for stack sampled metrics for the DEV channel (in addition to the already-enabled CANARY channel) and development builds on Android.
    This should (hopefully) provide more/better signals for future
    optimization work.
    
    Please refer to the attached bug for testing details.
    
    Change-Id: I55a660dc3337e7791cbf4cd83399860e40ccd069
    Bug: 1326430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644739
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014477}

--- chrome/common/profiler/thread_profiler_platform_configuration.cc
+++ chrome/common/profiler/thread_profiler_platform_configuration.cc
@@ -137,10 +137,6 @@ class AndroidPlatformConfiguration : public DefaultPlatformConfiguration {
   bool IsEnabledForThread(
       metrics::CallStackProfileParams::Process process,
       metrics::CallStackProfileParams::Thread thread) const override;
-
- protected:
-  bool IsSupportedForChannel(
-      absl::optional<version_info::Channel> release_channel) const override;
 };
 
 AndroidPlatformConfiguration::AndroidPlatformConfiguration(
@@ -235,17 +231,6 @@ bool AndroidPlatformConfiguration::IsEnabledForThread(
     default:
       return false;
   }
-}
-
-bool AndroidPlatformConfiguration::IsSupportedForChannel(
-    absl::optional<version_info::Channel> release_channel) const {
-  // Enable on canary, for now.
-  if (release_channel && *release_channel == version_info::Channel::CANARY)
-    return true;
-
-  // Otherwise enable in dedicated ThreadProfiler browser tests.
-  // TODO(https://crbug.com/1004855): Enable across all browser tests.
-  return browser_test_mode_enabled();
 }
 #endif  // BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_ARMEL)
 
