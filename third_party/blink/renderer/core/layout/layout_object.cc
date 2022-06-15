cd49599a0594413b4d433360555b9f30da3da14f
Kent Tamura <tkent@chromium.org>
2022-06-15 03:51:25 +0000

    ForeignObject NG: Handle collect-inlines requests correctly
    LayoutObject::SetNeedsCollectInlines() should support
    LayoutNGSVGForeignObject. Otherwise, we fail to update inline contents.
    
    Bug: 1335655
    Change-Id: Ib4aa677c0b65d91cddb5578d46dfea08e9d54f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703803
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014285}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -1271,7 +1271,7 @@ void LayoutObject::SetNeedsCollectInlines() {
     return;
 
   if (UNLIKELY(IsSVGChild() && !IsNGSVGText() && !IsSVGInline() &&
-               !IsSVGInlineText()))
+               !IsSVGInlineText() && !IsNGSVGForeignObject()))
     return;
 
   // Don't mark |LayoutFlowThread| because |CollectInlines()| skips them.
