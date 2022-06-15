162510d73450e12a1f553fcf32926869ffb3a0df
Chong Gu <chonggu@google.com>
2022-06-15 12:12:56 +0000

    [Fuchsia][CFv2 Script] Add processing executable test specific args
    This allows supporting executable specific arguments, which saves the
    need to copy files from the device.
    The only test_args in //build/fuchsia/test_runner.py that still
    need to be migrated should be enable-test-server and
    test-launcher-jobs, both of which requires further hashing out.
    Fuchsia test executables build in Chromium can now be invoked via
    `./build/fuchsia/test/run_test.py url_unittests -C out/fuchsia
    --test-launcher-summary-output=summary_file`
    
    Bug: 1318583
    Change-Id: Ifdbc9f253768f6c620d57cbdfedd1222ad40e15b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3685436
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014402}

--- build/fuchsia/test/run_blink_test.py
+++ build/fuchsia/test/run_blink_test.py
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import subprocess
 
+from argparse import Namespace
+
 from common import DIR_SRC_ROOT, resolve_packages
 from test_runner import TestRunner
 
@@ -17,8 +19,8 @@ _BLINK_TEST_SCRIPT = os.path.join(DIR_SRC_ROOT, 'third_party', 'blink',
 class BlinkTestRunner(TestRunner):
     """Test runner for running blink web tests."""
 
-    def _get_packages(self):
-        return ['content_shell']
+    def __init__(self, out_dir: str, test_args: Namespace) -> None:
+        super().__init__(out_dir, test_args, ['content_shell'])
 
     def run_test(self):
         resolve_packages(self.packages)
