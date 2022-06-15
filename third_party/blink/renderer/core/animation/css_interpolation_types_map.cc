33aeec8a546114619e11cca776cd3fc0f7d8b882
Rakina Zata Amni <rakina@chromium.org>
2022-06-15 01:04:03 +0000

    Revert "Implementing grid-template-columns/rows interpolation support"
    This reverts commit b266b143ec994a7a97349cd2d54e18f52bf9534f.
    
    Reason for revert: Potentially causing MSAN failures, example: https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20MSAN/15301/overview
    
    Original change's description:
    > Implementing grid-template-columns/rows interpolation support
    >
    > This CL adds new interpolation types to represent grid track lists and
    > their track sizes, allowing interpolation of grid-template-columns and
    > grid-template-rows. A more detailed explanation of the design can be
    > found in this doc: https://docs.google.com/document/d/1byO4UU-gFRNrqAEuhL3AKgUuFprz4ZQYNEeXSF9T6w0/edit?usp=sharing
    >
    > Bug: 759665
    > Change-Id: Iecef825cd843c60bf6268cdbfc2f9180c884f853
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403003
    > Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    > Commit-Queue: Ana Sollano Kim <ansollan@microsoft.com>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1013983}
    
    Bug: 759665
    Change-Id: I0d708b9ea6faa0b4781aa190bd14a6190b63b6cf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704863
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1014226}

--- third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
+++ third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css_font_stretch_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_variation_settings_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_weight_interpolation_type.h"
-#include "third_party/blink/renderer/core/animation/css_grid_template_property_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_image_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_image_list_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_image_slice_interpolation_type.h"
@@ -58,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/permissions_policy/layout_animations_policy.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -190,15 +188,6 @@ const InterpolationTypes& CSSInterpolationTypesMap::Get(
       case CSSPropertyID::kAspectRatio:
         applicable_types->push_back(
             std::make_unique<CSSAspectRatioInterpolationType>(used_property));
-        break;
-      case CSSPropertyID::kGridTemplateColumns:
-      case CSSPropertyID::kGridTemplateRows:
-        if (RuntimeEnabledFeatures::
-                CSSGridTemplatePropertyInterpolationEnabled()) {
-          applicable_types->push_back(
-              std::make_unique<CSSGridTemplatePropertyInterpolationType>(
-                  used_property));
-        }
         break;
       case CSSPropertyID::kContainIntrinsicWidth:
       case CSSPropertyID::kContainIntrinsicHeight:
