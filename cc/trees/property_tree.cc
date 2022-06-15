2f10991d7c8b2ec16ddb1d8698548f067f2d0c11
Rahul Arakeri <arakeri@microsoft.com>
2022-06-15 20:34:55 +0000

    Revert "[FixedElementsDontOverscroll] Unified overscroll_node_id_"
    This reverts commit 72464ed9cc6e56e89cc2fae69a5ef5334637bea0.
    
    Reason for revert: overscroll_node_id_ is different on Mac for
    TransformTree and ClipTree and as such, should not be used to look
    up ClipTree::nodes_
    
    Original change's description:
    > [FixedElementsDontOverscroll] Unified overscroll_node_id_
    >
    > This CL unifies the overscroll_node_id_ from TransformTree and ClipTree
    > since both are generated during PaintArtifactCompositor::Update. This
    > way, we don't need to worry about keeping the overscroll_node_id_ in
    > sync.
    >
    > Bug: 585766
    > Change-Id: I369519cda80f4884a31e45a0bd96b24f1dec64ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699107
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1013226}
    
    Bug: 585766
    Change-Id: I5b4caf192dd7943f3d8e92ccd28dc4d2f6f6c905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707763
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1014618}

--- cc/trees/property_tree.cc
+++ cc/trees/property_tree.cc
@@ -476,7 +476,7 @@ void TransformTree::UpdateFixedNodeTransformAndClip(
       gfx::ScaleVector2d(overscroll_offset, 1.f / page_scale_factor());
 
   ClipTree& clip_tree = property_trees()->clip_tree_mutable();
-  ClipNode* clip_node = clip_tree.Node(overscroll_node_id_);
+  ClipNode* clip_node = clip_tree.Node(clip_tree.overscroll_node_id());
 
   if (clip_node) {
     // Inflate the clip rect based on the overscroll direction.
@@ -1266,7 +1266,8 @@ EffectTree::CopyRequestMap EffectTree::TakeCopyRequests() {
 }
 
 ClipTree::ClipTree(PropertyTrees* property_trees)
-    : PropertyTree<ClipNode>(property_trees) {}
+    : PropertyTree<ClipNode>(property_trees),
+      overscroll_node_id_(kInvalidPropertyNodeId) {}
 
 void ClipTree::SetViewportClip(gfx::RectF viewport_rect) {
   if (size() < 2)
@@ -1286,7 +1287,8 @@ gfx::RectF ClipTree::ViewportClip() const {
 
 #if DCHECK_IS_ON()
 bool ClipTree::operator==(const ClipTree& other) const {
-  return PropertyTree::operator==(other);
+  return PropertyTree::operator==(other) &&
+         overscroll_node_id_ == other.overscroll_node_id();
 }
 #endif
 
