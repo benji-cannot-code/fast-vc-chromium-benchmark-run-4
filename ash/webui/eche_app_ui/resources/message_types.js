b19b89c15af5fe83fd54ed5cd7634d9d3ff752d0
patricktu <patricktu@google.com>
2022-06-15 01:43:08 +0000

    Support on-screen virtual keyboard
    When user manually enable the on-screen keyboard, the virtual keyboard
    icon will appear in the system tray. If users open the VK and click the
    chars, it does not react in the Eche streaming window. This CL makes the
    key events can be received from the EcheSWA.
    
    Bug: b/233869356
    Change-Id: Ie3a8f25add743fd8a05ccc6e6f6166b69f05668f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676715
    Auto-Submit: Patrick Tu <patricktu@google.com>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Patrick Tu <patricktu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014241}

--- ash/webui/eche_app_ui/resources/message_types.js
+++ ash/webui/eche_app_ui/resources/message_types.js
@@ -102,4 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   START_STREAMING: 'start_streaming',
   // Message for stream action
   STREAM_ACTION: 'stream_action',
+  // Message for virtual keyboard state
+  IS_VIRTUAL_KEYBOARD_ENABLED: 'is_virtual_keyboard_enabled',
 };
