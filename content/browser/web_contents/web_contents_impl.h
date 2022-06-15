7bd396239d43f06151ae98bbc5d078a3558cd5ee
Dave Tapuska <dtapuska@chromium.org>
2022-06-15 16:45:59 +0000

    Remove WebContents::GetMainFrame.
    Reland 1522398b4b6f6d353cbbbe2aa23ee6c3bdf50dbd with one additional
    test fixed. This change previously landed but was reverted due to
    a test being added while this change was in the CQ.
    
    All usages have been rewritten to GetPrimaryMainFrame. Remove the
    deprecated API.
    
    BUG=1250404
    
    Change-Id: I6c65a5b102295c8feb9193d2c1de76ff01bc4f8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707931
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014480}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -334,7 +334,6 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   const GURL& GetURL() override;
   const GURL& GetVisibleURL() override;
   const GURL& GetLastCommittedURL() override;
-  RenderFrameHostImpl* GetMainFrame() override;
   RenderFrameHostImpl* GetPrimaryMainFrame() override;
   PageImpl& GetPrimaryPage() override;
   RenderFrameHostImpl* GetFocusedFrame() override;
