59edadc5a0552ea2bdd51e2ada3440ac759cc634
Andrea Gomez <andreaxg@google.com>
2022-06-15 20:59:57 +0000

    Add 'chrome language changed' histogram to Windows and add a test for the metric being recorded.
    Bug: 1164639
    Change-Id: Ie2f79205e7fdaedc07a3071b417d2e2f9e29ff94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692591
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014630}

--- chrome/browser/resources/settings/languages_page/languages_subpage.ts
+++ chrome/browser/resources/settings/languages_page/languages_subpage.ts
@@ -416,6 +416,8 @@ export class SettingsLanguagesSubpageElement extends
         this.detailLanguage_!.language.code);
     this.languageHelper.moveLanguageToFront(
         this.detailLanguage_!.language.code);
+    LanguageSettingsMetricsProxyImpl.getInstance().recordSettingsMetric(
+        LanguageSettingsActionType.CHANGE_CHROME_LANGUAGE);
 
     this.closeMenuSoon_();
   }
