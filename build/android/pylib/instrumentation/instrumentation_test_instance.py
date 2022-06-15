3b41ba60ab2c5c207eaf1089efe3d8832f1b1f68
Rupert Ben Wiser <bewise@chromium.org>
2022-06-15 07:51:59 +0000

    android/webview: Add CTS instant app tests to waterfall
    This introduces CTS tests in instant app mode for
    WebView for the Android versions CTS supports this.
    
    We have gotten the go ahead to add 1 shard per
    builder from zhiyuans@chromium.org.
    
    In order to enale this, we also needed to make
    additional APKs install as instant apps. CTS
    sometimes expects some APKs to be installed
    as regular APKs so in those instances we need to
    ignore this.
    
    We then run the CTS tests are variants between instant
    and full modes. These show differently in our test results
    if WebView developers need to investigate them.
    
    We had to move some of the tests to be matrix compound tests
    to support these variants.
    
    Bug: 1082333
    Change-Id: I681dca198a86f80d61979189a01f5c073647ed0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702279
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Reviewed-by: Benjamin Joyce <bjoyce@chromium.org>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014333}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -631,6 +631,7 @@ class InstrumentationTestInstance(test_instance.TestInstance):
 
     self._additional_apks = []
     self._forced_queryable_additional_apks = []
+    self._instant_additional_apks = []
     self._apk_under_test = None
     self._apk_under_test_incremental_install_json = None
     self._modules = None
@@ -809,7 +810,8 @@ class InstrumentationTestInstance(test_instance.TestInstance):
           '(This may just mean that the test package is '
           'currently being installed.)', self._test_package)
 
-    for x in set(args.additional_apks + args.forced_queryable_additional_apks):
+    for x in set(args.additional_apks + args.forced_queryable_additional_apks +
+                 args.instant_additional_apks):
       if not os.path.exists(x):
         error_func('Unable to find additional APK: %s' % x)
 
@@ -818,6 +820,9 @@ class InstrumentationTestInstance(test_instance.TestInstance):
 
       if x in args.forced_queryable_additional_apks:
         self._forced_queryable_additional_apks.append(apk)
+
+      if x in args.instant_additional_apks:
+        self._instant_additional_apks.append(apk)
 
   def _initializeDataDependencyAttributes(self, args, data_deps_delegate):
     self._data_deps = []
@@ -1137,6 +1142,9 @@ class InstrumentationTestInstance(test_instance.TestInstance):
 
   def IsApkForceQueryable(self, apk):
     return apk in self._forced_queryable_additional_apks
+
+  def IsApkInstant(self, apk):
+    return apk in self._instant_additional_apks
 
   # pylint: disable=no-self-use
   def _InflateTests(self, tests):
