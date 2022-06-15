bdc5c5ab788403dba83d4d0130b2d03f3fed0d0f
Nancy Wang <nancylingwang@chromium.org>
2022-06-15 09:28:57 +0000

    Migrate PreferredApps tests to AppServiceProxy tests.
    When the flag kAppServicePreferredAppsWithoutMojom is enabled, AppServiceMojomImplTest won't do preferred apps operations, and all functions will be moved to AppServiceProxy, so the tests for preferred apps in AppServiceMojomImplTest will be moved to AppServiceProxyPreferredAppsTest to verify the functions done by AppServiceProxy.
    
    This CL migrates below PreferredApps tests from AppServiceMojomImplTest
    to AppServiceProxyPreferredAppsTest:
    PreferredApps
    PreferredAppsOverlap
    PreferredAppsDuplicated
    
    BUG=1253250
    
    Change-Id: I8c56e82fbc07b7dbc9dc4d06ef37ebd56373d64e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697709
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014358}

--- chrome/browser/apps/app_service/subscriber_crosapi.cc
+++ chrome/browser/apps/app_service/subscriber_crosapi.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/ash/app_management/app_management_uma.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_types.h"
+#include "components/services/app_service/public/cpp/features.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 
 namespace {
@@ -192,8 +193,13 @@ void SubscriberCrosapi::LoadIcon(const std::string& app_id,
 
 void SubscriberCrosapi::AddPreferredApp(const std::string& app_id,
                                         crosapi::mojom::IntentPtr intent) {
-  proxy_->AddPreferredApp(
-      app_id, apps_util::ConvertCrosapiToAppServiceIntent(intent, profile_));
+  if (base::FeatureList::IsEnabled(kAppServicePreferredAppsWithoutMojom)) {
+    proxy_->AddPreferredApp(
+        app_id, apps_util::CreateAppServiceIntentFromCrosapi(intent, profile_));
+  } else {
+    proxy_->AddPreferredApp(
+        app_id, apps_util::ConvertCrosapiToAppServiceIntent(intent, profile_));
+  }
 }
 
 void SubscriberCrosapi::ShowAppManagementPage(const std::string& app_id) {
