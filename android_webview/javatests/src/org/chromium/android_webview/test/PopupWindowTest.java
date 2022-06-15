f1cc3960ae640aeafe424933c6a62e743b042fe1
Oksana Zhuravlova <oksamyt@chromium.org>
2022-06-15 17:14:15 +0000

    Disable flaky test testSingleWindowModeJsInjection
    Bug: 1336342
    Change-Id: Ie8cca1da73a9d42da7fec2794d050aa6780e40ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706237
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014492}

--- android_webview/javatests/src/org/chromium/android_webview/test/PopupWindowTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/PopupWindowTest.java
@@ -29,6 +29,7 @@ import org.chromium.android_webview.test.TestAwContentsClient.ShouldInterceptReq
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.android_webview.test.util.JSUtils;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
@@ -495,6 +496,7 @@ public class PopupWindowTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "https://crbug.com/1336342")
     public void testSingleWindowModeJsInjection() throws Throwable {
         // Choose a free port which is different from |mWebServer| so they have different origins.
         TestWebServer crossOriginWebServer = TestWebServer.startAdditional();
