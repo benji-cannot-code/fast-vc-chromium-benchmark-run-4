eeeff48d98dc5fb0579eccfbd5e69399cf54ab73
Henrique Ferreiro <hferreiro@igalia.com>
2022-06-16 19:25:46 +0000

    Normalize paths from mojom import statements
    If an import statement of a mojom file uses '//' as path separator, the
    CheckStableMojomChanges presubmit check might fail if the referenced
    file is also changed. This is due to file paths being used to index some
    dictionaries, and the keys wouldn't match in that case.
    
    Make sure that the mojom parser stores normalized paths.
    
    Change-Id: I386f4bd846d77fe1fe023e0a11d26aef13f04e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702307
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015030}

--- mojo/public/tools/mojom/mojom/parse/ast.py
+++ mojo/public/tools/mojom/mojom/parse/ast.py
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # and lineno). You may also define __repr__() to help with analyzing test
 # failures, especially for more complex types.
 
+import os.path
+
 
 class NodeBase(object):
   """Base class for nodes in the AST."""
@@ -188,7 +190,8 @@ class Import(NodeBase):
     assert isinstance(import_filename, str)
     super(Import, self).__init__(**kwargs)
     self.attribute_list = attribute_list
-    self.import_filename = import_filename
+    # TODO(crbug.com/953884): Use pathlib once we're migrated fully to Python 3.
+    self.import_filename = os.path.normpath(import_filename).replace('\\', '/')
 
   def __eq__(self, other):
     return super(Import, self).__eq__(other) and \
