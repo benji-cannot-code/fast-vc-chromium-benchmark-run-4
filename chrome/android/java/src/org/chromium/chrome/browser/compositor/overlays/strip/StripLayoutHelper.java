dcfbce02c2fde15779bb11f782574e82101aeddf
Gaurav Jayasawal <gauravjj@google.com>
2022-06-16 23:37:17 +0000

    Add tests for fling and drag
    Bug: 1335979
    Change-Id: I47015f403ee4bf4ee281f2488c8968a5260bba5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709837
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Gaurav Jayasawal <gauravjj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015139}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -2266,6 +2266,11 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
         return mMultiStepTabCloseAnimRunning;
     }
 
+    @VisibleForTesting
+    protected boolean isInReorderModeForTesting() {
+        return mInReorderMode;
+    }
+
     private void setAccessibilityDescription(StripLayoutTab stripTab, Tab tab) {
         if (tab != null) setAccessibilityDescription(stripTab, tab.getTitle(), tab.isHidden());
     }
