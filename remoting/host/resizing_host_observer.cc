b47cfd238bb22031388ea98964cf8d8883712b9c
Lambros Lambrou <lambroslambrou@chromium.org>
2022-06-16 00:52:27 +0000

    Reland "[remoting] Pass desktop display info to ResizingHostObserver."
    This is a reland of commit 09d9bb902e18b8a68f2c42a5f8e81fb325aedc4c
    
    Original change's description:
    > [remoting] Pass desktop display info to ResizingHostObserver.
    >
    > This modifies DesktopDisplayInfoMonitor to use base::CallbackList to
    > allow multiple subscribers to receive the display-info.
    >
    > This CL also adds ResizingHostObserver as a subscriber, but the
    > returned display-info is not yet used.
    >
    > Bug: 1326339
    > Change-Id: I0e5531ac83c0ab8ac7bca5d55b913cc7c1e488fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703229
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1014672}
    
    Bug: 1326339
    Change-Id: Idf29fc477b47337731b2e3e1db63b07f29e45e27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708889
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014733}

--- remoting/host/resizing_host_observer.cc
+++ remoting/host/resizing_host_observer.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "base/time/tick_clock.h"
 #include "remoting/host/base/screen_resolution.h"
+#include "remoting/host/desktop_display_info_monitor.h"
 #include "remoting/host/desktop_resizer.h"
 
 namespace remoting {
@@ -133,6 +134,12 @@ ResizingHostObserver::~ResizingHostObserver() {
     RestoreScreenResolution();
 }
 
+void ResizingHostObserver::RegisterForDisplayChanges(
+    DesktopDisplayInfoMonitor& monitor) {
+  monitor.AddCallback(base::BindRepeating(
+      &ResizingHostObserver::OnDisplayInfoChanged, weak_factory_.GetWeakPtr()));
+}
+
 void ResizingHostObserver::SetScreenResolution(
     const ScreenResolution& resolution,
     absl::optional<webrtc::ScreenId> screen_id) {
@@ -209,6 +216,12 @@ void ResizingHostObserver::RestoreScreenResolution() {
     desktop_resizer_->RestoreResolution(original_resolution_, absl::nullopt);
     original_resolution_ = ScreenResolution();
   }
+}
+
+void ResizingHostObserver::OnDisplayInfoChanged(
+    const DesktopDisplayInfo& display_info) {
+  // TODO(crbug.com/1326339): Implement this as part of the cross-platform
+  // resizing logic.
 }
 
 }  // namespace remoting
