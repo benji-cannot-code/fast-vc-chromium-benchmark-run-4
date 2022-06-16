7d677a16588f272afc120379b0604f730325d3ac
Kevin Ellis <kevers@google.com>
2022-06-16 21:03:38 +0000

    Fix animation count with sroll-timline linked animations.
    With https://chromium-review.googlesource.com/c/chromium/src/+/3553394
    Finished animations that reached the effect's end time were no longer
    tallied in AnimationsNeedingUpdateCount.  Scroll-linked animations should be exempt from this exclusion since timing updates are non-
    monotonic.
    
    Bug: 1336723
    Change-Id: Ic8a6da931703d1e57a7759d914d27f89f2e68c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708093
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015094}

--- third_party/blink/renderer/core/animation/scroll_timeline_test.cc
+++ third_party/blink/renderer/core/animation/scroll_timeline_test.cc
@@ -45,15 +45,9 @@ void ExpectVectorDoubleEqual(const WTF::Vector<double>& expected,
 
 HeapVector<Member<ScrollTimelineOffset>> CreateScrollOffsets(
     ScrollTimelineOffset* start_scroll_offset =
-        MakeGarbageCollected<ScrollTimelineOffset>(
-            CSSNumericLiteralValue::Create(
-                10.0,
-                CSSPrimitiveValue::UnitType::kPixels)),
+        MakeGarbageCollected<ScrollTimelineOffset>(),
     ScrollTimelineOffset* end_scroll_offset =
-        MakeGarbageCollected<ScrollTimelineOffset>(
-            CSSNumericLiteralValue::Create(
-                90.0,
-                CSSPrimitiveValue::UnitType::kPixels))) {
+        MakeGarbageCollected<ScrollTimelineOffset>()) {
   HeapVector<Member<ScrollTimelineOffset>> scroll_offsets;
   scroll_offsets.push_back(start_scroll_offset);
   scroll_offsets.push_back(end_scroll_offset);
@@ -394,7 +388,7 @@ TEST_F(ScrollTimelineTest, AnimationPersistsWhenFinished) {
   SimulateFrame();
 
   // Scroll to finished:
-  scrollable_area->SetScrollOffset(ScrollOffset(0, 91),
+  scrollable_area->SetScrollOffset(ScrollOffset(0, 100),
                                    mojom::blink::ScrollType::kProgrammatic);
   SimulateFrame();
   EXPECT_EQ("finished", animation->playState());
@@ -440,7 +434,7 @@ TEST_F(ScrollTimelineTest, AnimationPersistsWhenSourceBecomesNonScrollable) {
 
   // Scroll to 50%:
   ASSERT_TRUE(scroller->GetScrollableArea());
-  ScrollOffset offset_50(0, 50);  // 10 + (90 - 10) * 0.5 = 50
+  ScrollOffset offset_50(0, 50);
   scroller->GetScrollableArea()->SetScrollOffset(
       offset_50, mojom::blink::ScrollType::kProgrammatic);
   SimulateFrame();
@@ -473,9 +467,9 @@ TEST_F(ScrollTimelineTest, AnimationPersistsWhenSourceBecomesNonScrollable) {
   ASSERT_TRUE(scroller->GetScrollableArea());
 
   // Scroll to 40%:
-  ScrollOffset offset_42(0, 42);  // 10 + (90 - 10) * 0.4 = 42
+  ScrollOffset offset_40(0, 40);
   scroller->GetScrollableArea()->SetScrollOffset(
-      offset_42, mojom::blink::ScrollType::kProgrammatic);
+      offset_40, mojom::blink::ScrollType::kProgrammatic);
   SimulateFrame();
   EXPECT_TIME_NEAR(40.0, animation->CurrentTimeInternal()
                              .value_or(AnimationTimeDelta())
@@ -696,7 +690,7 @@ TEST_F(ScrollTimelineTest, FinishedAnimationPlaysOnReversedScrolling) {
   scroll_animation->play();
   UpdateAllLifecyclePhasesForTest();
   // Scroll to finished state.
-  scrollable_area->SetScrollOffset(ScrollOffset(0, 91),
+  scrollable_area->SetScrollOffset(ScrollOffset(0, 100),
                                    mojom::blink::ScrollType::kProgrammatic);
   // Simulate a new animation frame  which allows the timeline to compute a new
   // current time.
@@ -796,7 +790,7 @@ TEST_F(ScrollTimelineTest,
   scroll_animation->play();
   UpdateAllLifecyclePhasesForTest();
   // Scroll to finished state.
-  scrollable_area->SetScrollOffset(ScrollOffset(0, 91),
+  scrollable_area->SetScrollOffset(ScrollOffset(0, 100),
                                    mojom::blink::ScrollType::kProgrammatic);
   // Simulate a new animation frame  which allows the timeline to compute a new
   // current time.
@@ -816,7 +810,7 @@ TEST_F(ScrollTimelineTest,
   EXPECT_FALSE(event_listener->EventReceived());
 
   // Scroll forward to finished state.
-  scrollable_area->SetScrollOffset(ScrollOffset(0, 91),
+  scrollable_area->SetScrollOffset(ScrollOffset(0, 100),
                                    mojom::blink::ScrollType::kProgrammatic);
   SimulateFrame();
   // Verify animation finished event is fired.
