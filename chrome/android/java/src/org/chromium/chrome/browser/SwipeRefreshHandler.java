f046f4d52f763840c3e83b235aa5414806aa8b29
Yaron Friedman <yfriedman@chromium.org>
2022-06-16 17:58:32 +0000

    [Jank] Add experiment to optimize SwipeRefreshHandler.
    Currently the SwipeRefreshHandler sets z-order while pulling and
    animating. However, this causes a re-layout of the UI. Moreover, when
    completing the pull-refresh gesture, it ends up enqueing 5x layouts
    while animating away and removing control. Add an experiment flag to
    measure impact of doing a single re-layout at the start.
    
    Bug: 1335416
    Change-Id: I191a64e23dbdc0ed855cbb89d20b97ca0339006c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703643
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014996}

--- chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
@@ -18,6 +18,7 @@ import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.gesturenav.HistoryNavigationCoordinator;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
@@ -196,7 +197,8 @@ public class SwipeRefreshHandler
         if (type == OverscrollAction.PULL_TO_REFRESH) {
             if (mSwipeRefreshLayout == null) initSwipeRefreshLayout(mTab.getContext());
             attachSwipeRefreshLayoutIfNecessary();
-            return mSwipeRefreshLayout.start();
+            return mSwipeRefreshLayout.start(ChromeFeatureList.isEnabled(
+                    ChromeFeatureList.OPTIMIZE_LAYOUTS_FOR_PULL_REFRESH));
         } else if (type == OverscrollAction.HISTORY_NAVIGATION) {
             if (mNavigationCoordinator != null) {
                 mNavigationCoordinator.startGesture();
