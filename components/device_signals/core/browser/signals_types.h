9ea89536bc4e071c781de8a31344bdce92bcf3de
Sebastien Lalancette <seblalancette@chromium.org>
2022-06-16 15:58:36 +0000

    [NewEV] Add Extension APIs for AV and Hotfix Signals
    These APIs are private and only accessible by the SecureConnect
    extension, and are only available on Windows.
    
    The execution of these APIs is gated behind:
    - A feature flag (with a kill-switch param for each function),
    - A browser management check,
    - A check that the user ID contained in the function parameter
      represents the profile user,
    - A check that the org managing the profile is affiliated with the org
      managing the browser.
    
    Bug: b:229737798
    Change-Id: Idfa538d6a64b240a053ac41f96bec5dad6f7e615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700832
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014931}

--- components/device_signals/core/browser/signals_types.h
+++ components/device_signals/core/browser/signals_types.h
@@ -35,8 +35,11 @@ enum class SignalCollectionError {
   kUnaffiliatedUser,
   kUnsupported,
   kMissingSystemService,
-  kMaxValue = kMissingSystemService
+  kMissingBundle,
+  kMaxValue = kMissingBundle
 };
+
+const std::string ErrorToString(SignalCollectionError error);
 
 // Base struct type that each specific signal bundle types should extend. The
 // derived signal bundles/responses should group a set of signals that
--- components/device_signals/core/common/signals_constants.h
+++ components/device_signals/core/common/signals_constants.h
@@ -55,6 +55,7 @@ extern const char kConsentRequired[];
 extern const char kUnaffiliatedUser[];
 extern const char kUnsupported[];
 extern const char kMissingSystemService[];
+extern const char kMissingBundle[];
 
 }  // namespace errors
 
