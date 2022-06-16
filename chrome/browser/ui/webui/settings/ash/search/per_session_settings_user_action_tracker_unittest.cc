cf1e8edffe67f22b2bb3f4ce2cb1609cbdb7a727
Yeunjoo Choi <ychoi@igalia.com>
2022-06-16 09:36:48 +0000

    Reland "Move //chrome/browser/ui/webui/settings/{chromeos=>ash} [2/N]"
    This is a reland of commit c04530a58e4a11f121ce74795ee59e87d01ecd91
    
    Original change's description:
    > Move //chrome/browser/ui/webui/settings/{chromeos=>ash} [2/N]
    >
    > This CL moves search/ to ash from chromeos.
    >
    > This CL is part of the Chrome OS source code directory migration:
    > https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    >
    > Change-Id: I35636415c5ad5710c75a53d9607c9238d69a8083
    > Bug: 1164001
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466857
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Owners-Override: James Cook <jamescook@chromium.org>
    > Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#974943}
    
    Bug: 1164001
    Change-Id: I8650cdddb94c87eb39652baa1a340b268045dfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499884
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1014857}

--- chrome/browser/ui/webui/settings/ash/search/per_session_settings_user_action_tracker_unittest.cc
+++ chrome/browser/ui/webui/settings/ash/search/per_session_settings_user_action_tracker_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/settings/chromeos/search/per_session_settings_user_action_tracker.h"
+#include "chrome/browser/ui/webui/settings/ash/search/per_session_settings_user_action_tracker.h"
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
--- chrome/browser/ui/webui/settings/ash/search/search_tag_registry_unittest.cc
+++ chrome/browser/ui/webui/settings/ash/search/search_tag_registry_unittest.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry.h"
+#include "chrome/browser/ui/webui/settings/ash/search/search_tag_registry.h"
 
 #include "base/no_destructor.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/ui/webui/settings/ash/search/search_concept.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
-#include "chrome/browser/ui/webui/settings/chromeos/search/search_concept.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/components/local_search_service/public/mojom/index.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
