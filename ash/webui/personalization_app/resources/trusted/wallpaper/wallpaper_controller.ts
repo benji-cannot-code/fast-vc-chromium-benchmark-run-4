db7e13ce084d1059f5bc347bd1b9559e10c5eab7
Jeffrey Young <cowmoo@google.com>
2022-06-16 19:27:49 +0000

    personalization: googlePhotos.enabled early return
    Replace an assert with an early return. After a dom-if restamp was
    added, now google photos may attempt to initialize google photos
    enablement state multiple times.
    
    BUG=b:236185235
    
    Change-Id: Id1dd18714c5ef3e57df59efa3dfb11aa04b5ae84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708886
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015035}

--- ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_controller.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_controller.ts
@@ -156,7 +156,9 @@ async function fetchGooglePhotosEnabled(
     provider: WallpaperProviderInterface,
     store: PersonalizationStore): Promise<void> {
   // Whether access is allowed should only be fetched once.
-  assert(store.data.wallpaper.googlePhotos.enabled === undefined);
+  if (store.data.wallpaper.googlePhotos.enabled !== undefined) {
+    return;
+  }
 
   store.dispatch(action.beginLoadGooglePhotosEnabledAction());
   const {state} = await provider.fetchGooglePhotosEnabled();
