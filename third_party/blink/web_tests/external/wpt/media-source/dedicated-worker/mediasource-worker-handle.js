2295491bb540c8772348a79ab820b8459ca1cd81
Matt Wolenetz <wolenetz@chromium.org>
2022-06-16 01:48:37 +0000

    Reland: MSE-in-Workers: srcObject part 5: Conditionally fail worker objectURL
    With the underlying leak fixed by
    https://chromium-review.googlesource.com/c/chromium/src/+/3704191, this
    change can be relanded now.
    
    Previous revert:
    > Revert "MSE-in-Workers: srcObject part 5: Conditionally fail worker objectURL"
    >
    > This reverts commit 6315549b8c2ece3dbbf3062c1a87347589a5e115.
    >
    > Reason for revert: This is causing failures on the WebKit Linux Leak builder
    > i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20Leak/39394/overview
    >
    > Original change's description:
    > > MSE-in-Workers: srcObject part 5: Conditionally fail worker objectURL
    > >
    > > If the MediaSourceInWorkersUsingHandle feature is enabled, this change
    > > prevents successful ability of obtaining an objectURL that would succeed
    > > in loading a worker-owned MediaSource.
    > >
    > > It changes the wpt tests to use handle for attachment and verifies
    > > expected new behavior of getHandle and that worker objectURL attachment
    > > fails (these tests run on experimental builds of Chromium with
    > > currently-experimental MediaSourceInWorkersUsingHandle feature enabled,
    > > just like the currently-experimental MediaSourceInWorkers feature.)
    > >
    > > References:
    > > Full prototype CL for the parts 1-4 that have already landed:
    > >     https://chromium-review.googlesource.com/c/chromium/src/+/3515334
    > > MSE spec issue:
    > >     https://github.com/w3c/media-source/issues/175
    > > MSE spec feature updates switching from worker MSE attachment via
    > >   object URL to attachment via srcObject MediaSourceHandle:
    > >   * https://github.com/w3c/media-source/pull/305
    > >   * further clarifications in discussion at
    > >     https://github.com/w3c/media-source/pull/306#issuecomment-1144180822
    > >
    > > BUG=878133
    > >
    > > Change-Id: I60ddca79ee0f95c87b6d84e4f44ad9c283f359a3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698231
    > > Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    > > Auto-Submit: Matthew Wolenetz <wolenetz@chromium.org>
    > > Reviewed-by: Will Cassella <cassew@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1012712}
    >
    > Bug: 878133
    > Change-Id: I1e405ae1de146d1f3183592b00a43bd3c38d849d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695890
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    > Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1012823}
    
    Bug: 878133
    Change-Id: I56e4ecd4d8b58d9d58ed3c575b0fb52f596b6fae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708465
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014755}

--- third_party/blink/web_tests/external/wpt/media-source/dedicated-worker/mediasource-worker-handle.js
+++ third_party/blink/web_tests/external/wpt/media-source/dedicated-worker/mediasource-worker-handle.js
@@ -14,21 +14,33 @@ test(t => {
 }, "MediaSource in DedicatedWorker context must have true-valued canConstructInDedicatedWorker if Window context had it");
 
 test(t => {
+  assert_true("getHandle" in MediaSource.prototype, "dedicated worker MediaSource must have getHandle");
+  assert_true(self.hasOwnProperty("MediaSourceHandle"), "dedicated worker must have MediaSourceHandle visibility");
+}, "MediaSource prototype in DedicatedWorker context must have getHandle, and worker must have MediaSourceHandle");
+
+test(t => {
   const ms = new MediaSource();
   assert_equals(ms.readyState, "closed");
 }, "MediaSource construction succeeds with initial closed readyState in DedicatedWorker");
 
 test(t => {
   const ms = new MediaSource();
-  const url = URL.createObjectURL(ms);
-}, "URL.createObjectURL(mediaSource) in DedicatedWorker does not throw exception");
+  const handle = ms.getHandle();
+  assert_not_equals(handle, null, "must have a non-null getHandle result");
+  assert_true(handle instanceof MediaSourceHandle, "must be a MediaSourceHandle");
+}, "mediaSource.getHandle() in DedicatedWorker returns a MediaSourceHandle");
 
 test(t => {
   const ms = new MediaSource();
-  const url1 = URL.createObjectURL(ms);
-  const url2 = URL.createObjectURL(ms);
-  URL.revokeObjectURL(url1);
-  URL.revokeObjectURL(url2);
-}, "URL.revokeObjectURL(mediaSource) in DedicatedWorker with two url for same MediaSource");
+  const handle1 = ms.getHandle();
+  let handle2 = null;
+  assert_throws_dom("InvalidStateError", function()
+    {
+      handle2 = ms.getHandle();
+    }, "getting second handle from MediaSource instance");
+  assert_equals(handle2, null, "getting second handle from same MediaSource must have failed");
+  assert_not_equals(handle1, null, "must have a non-null result of the first getHandle");
+  assert_true(handle1 instanceof MediaSourceHandle, "first getHandle result must be a MediaSourceHandle");
+}, "mediaSource.getHandle() must not succeed more than precisely once for a MediaSource instance");
 
 done();
