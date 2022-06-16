a0f153ecba4c192354819d536337b006c68539de
Matt Menke <mmenke@chromium.org>
2022-06-16 18:38:16 +0000

    Remove url_fetcher.h includes from gcm/
    The class has been deprecated for quite some time, and we're working on
    removing it.
    
    Also remove some other unused net/ includes.
    
    Bug: 1010491
    Change-Id: I337f60d6f4170c8fcc803af63c40043c04d42532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698673
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015013}

--- google_apis/gcm/engine/instance_id_delete_token_request_handler.cc
+++ google_apis/gcm/engine/instance_id_delete_token_request_handler.cc
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "google_apis/gcm/engine/instance_id_delete_token_request_handler.h"
 
+#include "base/check.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "google_apis/gcm/base/gcm_util.h"
-#include "net/url_request/url_fetcher.h"
-#include "net/url_request/url_request_context_getter.h"
 
 namespace gcm {
 
--- google_apis/gcm/engine/instance_id_get_token_request_handler.cc
+++ google_apis/gcm/engine/instance_id_get_token_request_handler.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "google_apis/gcm/base/gcm_util.h"
-#include "net/url_request/url_request_context_getter.h"
 
 namespace gcm {
 
