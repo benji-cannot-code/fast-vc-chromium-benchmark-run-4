911f869e109eb0c364d4f22a243bdb4626e2bc21
Ender <ender@google.com>
2022-06-16 18:46:30 +0000

    Disable highly flaky TileGroupUnitTests.
    Also drops deprecated @FlakyTest annotation.
    
    Change-Id: I2f63d2d7aa857a5fa348503deaf392ad0b4e813b
    Links: http://shortn/_FZshQkOaFi
    Fixed: 1336867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709838
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015016}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
@@ -45,7 +45,6 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.DisabledTest;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.suggestions.ImageFetcher;
@@ -118,7 +117,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
-    @FlakyTest(message = "https://crbug.com/1292469")
+    @DisabledTest(message = "https://crbug.com/1292469")
     public void testInitialiseWithTileList() {
         mMostVisitedSites.setTileSuggestions(URLS);
 
@@ -157,6 +156,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1336867")
     public void testReceiveNewTilesWithoutChanges() {
         TileGroup tileGroup = initialiseTileGroup(URLS);
 
@@ -171,6 +171,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1336867")
     public void testReceiveNewTilesWithoutChanges_TrackLoad() {
         TileGroup tileGroup = initialiseTileGroup(/* deferLoad: */ true, URLS);
 
@@ -340,7 +341,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
-    @FlakyTest(message = "Test is flaky, see crbug.com/1286755")
+    @DisabledTest(message = "Test is flaky, see crbug.com/1286755")
     public void testRenderTileViewReplacing() {
         SuggestionsUiDelegate uiDelegate = mSuggestionsUiDelegate;
         when(uiDelegate.getImageFetcher()).thenReturn(mMockImageFetcher);
@@ -414,7 +415,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
-    @FlakyTest(message = "Test is flaky, see crbug.com/1288425")
+    @DisabledTest(message = "Test is flaky, see crbug.com/1288425")
     public void testIconLoadingWhenTileNotRegistered() {
         TileGroup tileGroup = initialiseTileGroup();
         Tile tile = new Tile(createSiteSuggestion("title", URLS[0]), 0);
