67e54bec27aad1a57a925a21125284dfbe684be7
Dmitrii Kuragin <kuragin@chromium.org>
2022-06-16 09:35:07 +0000

    Define `operator=` for web app command.
    It is always better to define operator and consttructor in pairs (copy
    and/or move). Style guide says(go/cstyle#Copyable_Movable_Types):
    
    ```
    Explicitly declaring or deleting all four copy/move operations is permitted,
    but not required. If you provide a copy or move assignment operator, you must
    also provide the corresponding constructor.
    ```
    
    Change-Id: I9b7104f09dd90b0cf03f83c7c4eedf0a8dfacc77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708560
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014855}

--- chrome/browser/web_applications/commands/web_app_command.cc
+++ chrome/browser/web_applications/commands/web_app_command.cc
@@ -34,6 +34,7 @@ WebAppCommandLock::WebAppCommandLock(base::flat_set<AppId> app_ids,
       lock_requests_(std::move(lock_requests)) {}
 
 WebAppCommandLock::WebAppCommandLock(WebAppCommandLock&&) = default;
+WebAppCommandLock& WebAppCommandLock::operator=(WebAppCommandLock&&) = default;
 
 WebAppCommandLock::~WebAppCommandLock() = default;
 
--- chrome/browser/web_applications/commands/web_app_command.h
+++ chrome/browser/web_applications/commands/web_app_command.h
@@ -31,6 +31,7 @@ class WebAppCommandLock {
       base::flat_set<content::DisjointRangeLockManager::LeveledLockRequest>;
 
   WebAppCommandLock(WebAppCommandLock&&);
+  WebAppCommandLock& operator=(WebAppCommandLock&&);
 
   WebAppCommandLock(const WebAppCommandLock&) = delete;
   WebAppCommandLock& operator=(const WebAppCommandLock&) = delete;
