19f0282b1d1bc9fbac8cf055a6f5df519f088a17
Keren Zhu <kerenzhu@chromium.org>
2022-06-16 00:03:55 +0000

    Increase timeout of WidgetActivationWaiter for mac
    There are some minor flakiness on mac in some tests that wait on widget
    creation + activation. Thus, increase timeout of WidgetActivationWaiter
    to deflake.
    
    Bug: 1327590
    Change-Id: I9240aac9c972f074d87355a36a9bfe08b12e947f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707796
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014702}

--- ui/views/test/widget_test.cc
+++ ui/views/test/widget_test.cc
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/native_widget_factory.h"
 #include "ui/views/widget/root_view.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/test/scoped_run_loop_timeout.h"
+#include "base/test/test_timeouts.h"
+#endif
+
 #if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && \
     !BUILDFLAG(IS_CHROMECAST)
 #include "ui/views/test/test_desktop_screen_ozone.h"
@@ -224,8 +229,15 @@ WidgetActivationWaiter::WidgetActivationWaiter(Widget* widget, bool active)
 WidgetActivationWaiter::~WidgetActivationWaiter() = default;
 
 void WidgetActivationWaiter::Wait() {
-  if (!observed_)
+  if (!observed_) {
+#if BUILDFLAG(IS_MAC)
+    // Some tests waiting on widget creation + activation are flaky due to
+    // timeout. crbug.com/1327590.
+    const base::test::ScopedRunLoopTimeout increased_run_timeout(
+        FROM_HERE, TestTimeouts::action_max_timeout());
+#endif
     run_loop_.Run();
+  }
 }
 
 void WidgetActivationWaiter::OnWidgetActivationChanged(Widget* widget,
