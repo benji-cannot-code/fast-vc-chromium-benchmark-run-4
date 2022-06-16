375b4c63a5cb3138664b3510226c32b6887cc431
Steve Kobes <skobes@chromium.org>
2022-06-16 19:23:35 +0000

    Let WebViewHelper create an InputHandlerProxy.
    A TestWebFrameWidget already has a real LayerTreeHost and a real
    WidgetInputHandlerManager connected to a WebFakeThreadScheduler.  With
    just a few tweaks it can synchronously run the cc input handling code,
    similar to how single-threaded web tests work.
    
    Now tests that use WebViewHelper, either directly or via SimTest, can
    perform "real" scrolls compatible with scroll unification.
    
    This lets us do a fairly mechanical migration of lots of tests that
    currently send gesture events directly to main-thread handlers, by
    switching them to the new DispatchThroughCcInputHandler API.
    
    Bug: 1322078
    Change-Id: I69668717aed5ddbecd1f916da3cf4430370c2b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704239
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015026}

--- third_party/blink/renderer/core/frame/frame_test_helpers.cc
+++ third_party/blink/renderer/core/frame/frame_test_helpers.cc
@@ -76,8 +76,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/network/http_parsers.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
+#include "third_party/blink/renderer/platform/scheduler/test/fake_task_runner.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
+#include "third_party/blink/renderer/platform/widget/input/widget_input_handler_manager.h"
+#include "third_party/blink/renderer/platform/widget/widget_base.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
@@ -575,6 +578,10 @@ TestWebFrameWidget* WebViewHelper::CreateFrameWidgetAndInitializeCompositing(
   frame_widget->InitializeCompositing(frame_widget->GetAgentGroupScheduler(),
                                       initial_screen_infos,
                                       &layer_tree_settings);
+  // This runs WidgetInputHandlerManager::InitOnInputHandlingThread, which will
+  // set up the InputHandlerProxy.
+  frame_widget->FlushInputHandlerTasks();
+
   frame_widget->SetCompositorVisible(true);
   return frame_widget;
 }
@@ -618,7 +625,11 @@ TestWebFrameWidget* WebViewHelper::GetMainFrameWidget() const {
 }
 
 void WebViewHelper::Resize(const gfx::Size& size) {
-  GetWebView()->MainFrameWidget()->Resize(size);
+  // In addition to calling WebFrameWidgetImpl::Resize(), this updates the
+  // LayerTreeHost::device_viewport_rect(), which is used to set up the
+  // compositor's clip tree.  (In a real browser this would happen through
+  // Widget.UpdateVisualProperties).
+  GetMainFrameWidget()->SetWindowRectSynchronouslyForTesting(gfx::Rect(size));
 }
 
 void WebViewHelper::InitializeWebView(
@@ -820,6 +831,26 @@ TestWidgetInputHandlerHost* TestWebFrameWidget::GetInputHandlerHost() {
   if (!widget_input_handler_host_)
     widget_input_handler_host_ = std::make_unique<TestWidgetInputHandlerHost>();
   return widget_input_handler_host_.get();
+}
+
+WidgetInputHandlerManager* TestWebFrameWidget::GetWidgetInputHandlerManager()
+    const {
+  return widget_base_for_testing()->widget_input_handler_manager();
+}
+
+void TestWebFrameWidget::FlushInputHandlerTasks() {
+  auto* main_task_runner = static_cast<scheduler::FakeTaskRunner*>(
+      GetWidgetInputHandlerManager()->main_task_runner_for_testing());
+  main_task_runner->RunUntilIdle();
+}
+
+void TestWebFrameWidget::DispatchThroughCcInputHandler(
+    const WebInputEvent& event) {
+  GetWidgetInputHandlerManager()->DispatchEvent(
+      std::make_unique<WebCoalescedInputEvent>(event.Clone(),
+                                               ui::LatencyInfo()),
+      mojom::blink::WidgetInputHandler::DispatchEventCallback());
+  FlushInputHandlerTasks();
 }
 
 display::ScreenInfo TestWebFrameWidget::GetInitialScreenInfo() {
--- third_party/blink/renderer/core/testing/sim/sim_test.cc
+++ third_party/blink/renderer/core/testing/sim/sim_test.cc
@@ -63,7 +63,7 @@ void SimTest::SetUp() {
   compositor_->SetLayerTreeHost(
       local_frame_root_->FrameWidgetImpl()->LayerTreeHostForTesting());
 
-  WebView().MainFrameViewWidget()->Resize(gfx::Size(300, 200));
+  ResizeView(gfx::Size(300, 200));
 }
 
 void SimTest::TearDown() {
@@ -168,6 +168,10 @@ SimCompositor& SimTest::Compositor() {
 
 Vector<String>& SimTest::ConsoleMessages() {
   return web_frame_client_->ConsoleMessages();
+}
+
+void SimTest::ResizeView(const gfx::Size& size) {
+  web_view_helper_->Resize(size);
 }
 
 SimWebFrameWidget* SimTest::CreateSimWebFrameWidget(
