b83ae97441b2508456420f43de3bde5f111f2f60
Longbo Wei <longbowei@google.com>
2022-06-17 02:56:03 +0000

    Feedback: Implement view privacy, terms and conditions
    * Implement privacy note with links.
    * Finalize the privateNoteText string and add screenshot, hash
    
    Screenshot: http://screen/5Tgkdk77jXkwrjK
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I36881d7de0b859c7e58632f8a46c189e9a9a0cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705866
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015214}

---
+++ ash/webui/os_feedback_ui/resources/feedback_constants.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export const FEEDBACK_LEGAL_HELP_URL =
+    'https://support.google.com/legal/answer/3110420';
+export const FEEDBACK_PRIVACY_POLICY_URL =
+    'https://policies.google.com/privacy';
+export const FEEDBACK_TERMS_OF_SERVICE_URL =
+    'https://policies.google.com/terms';
