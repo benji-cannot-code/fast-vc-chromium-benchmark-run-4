fc79e45c090512ad21fc7b36c9a4ee04c9385f4a
Rebekah Potter <rbpotter@chromium.org>
2022-06-17 02:33:45 +0000

    WebUI: Move cr/ui/splitter and test to last user (file_manager)
    All other WebUI surfaces now use cr_splitter.
    
    Also delete unused dialogs.css.
    
    Bug: 1322977
    Change-Id: Ibb3b46e1c0425286c78eceba86bf8167ff5eb49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708758
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015207}

--- ui/file_manager/file_manager/foreground/js/ui/splitter_unittest.js
+++ ui/file_manager/file_manager/foreground/js/ui/splitter_unittest.js
@@ -5,14 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {decorate} from 'chrome://resources/js/cr/ui.m.js';
-import {Splitter} from 'chrome://resources/js/cr/ui/splitter.js';
 import {getRequiredElement} from 'chrome://resources/js/util.m.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://test/chai_assert.js';
 
-import {assertEquals, assertFalse, assertTrue} from '../../../chai_assert.js';
+import {Splitter} from './splitter.js';
 
 // clang-format on
 
-function setUp() {
+export function setUp() {
   const html = `
     <div id="previous"></div>
     <div id="splitter"></div>
@@ -21,7 +21,7 @@ function setUp() {
   document.body.innerHTML = html;
 }
 
-function testSplitter_IgnoresRightMouse() {
+export function testSplitterIgnoresRightMouse() {
   const splitter = getRequiredElement('splitter');
   decorate(splitter, Splitter);
 
@@ -34,7 +34,7 @@ function testSplitter_IgnoresRightMouse() {
   assertTrue(downLeft.defaultPrevented);
 }
 
-function testSplitter_ResizePreviousElement() {
+export function testSplitterResizePreviousElement() {
   const splitter = getRequiredElement('splitter');
   decorate(splitter, Splitter);
   splitter.resizeNextElement = false;
@@ -63,7 +63,7 @@ function testSplitter_ResizePreviousElement() {
   assertEquals(100, afterWidth - beforeWidth);
 }
 
-function testSplitter_ResizeNextElement() {
+export function testSplitterResizeNextElement() {
   const splitter = getRequiredElement('splitter');
   decorate(splitter, Splitter);
   splitter.resizeNextElement = true;
@@ -89,10 +89,3 @@ function testSplitter_ResizeNextElement() {
   const afterWidth = parseFloat(nextElement.style.width);
   assertEquals(100, afterWidth - beforeWidth);
 }
-
-Object.assign(window, {
-  setUp,
-  testSplitter_IgnoresRightMouse,
-  testSplitter_ResizePreviousElement,
-  testSplitter_ResizeNextElement,
-});
--- ui/file_manager/file_manager/foreground/js/ui/table/table_splitter.js
+++ ui/file_manager/file_manager/foreground/js/ui/table/table_splitter.js
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import {dispatchSimpleEvent, getPropertyDescriptor} from 'chrome://resources/js/cr.m.js';
-import {Splitter} from 'chrome://resources/js/cr/ui/splitter.js';
+
+import {Splitter} from '../splitter.js';
 
 import {Table} from './table.js';
 
