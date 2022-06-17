9401f188b5af01f458299a6c942c2619d3c16bba
Rebekah Potter <rbpotter@chromium.org>
2022-06-17 18:24:23 +0000

    Sync Internals: Replace cr/ui/list with a simple custom list
    cr/ui/list is deprecated and contains a lot of logic/optimizations that
    are leftover from when it was used for large user facing UIs like
    bookmarks and settings, and are not needed for this internal debug
    page. To facilitate removing cr/ui/list from builds, replace its use in
    components/sync/driver/resources with minimal custom list display
    logic.
    
    This also makes it feasible to migrate sync internals to TypeScript in
    the future, since cr/ui/list and cr/ui/array_data_model are not
    supported for TypeScript pages.
    
    Bug: 1322977
    Change-Id: I0c84d18bbc975c09377a7f31e70ce2c4f18c2772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708128
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015437}

--- components/sync/driver/resources/search.js
+++ components/sync/driver/resources/search.js
@@ -5,18 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_splitter/cr_splitter.js';
 
-import {ArrayDataModel} from 'chrome://resources/js/cr/ui/array_data_model.m.js';
-import {List} from 'chrome://resources/js/cr/ui/list.m.js';
 import {$, getRequiredElement} from 'chrome://resources/js/util.m.js';
 
-import {decorateQuickQueryControls, decorateSearchControls} from './sync_search.js';
+import {decorateQuickQueryControls, SyncSearchManager} from './sync_search.js';
 
 decorateQuickQueryControls(
     document.getElementsByClassName('sync-search-quicklink'),
     /** @type {!HTMLButtonElement} */ ($('sync-search-submit')),
     /** @type {!HTMLInputElement} */ ($('sync-search-query')));
 
-decorateSearchControls(
+const manager = new SyncSearchManager(
     /** @type {!HTMLInputElement} */ ($('sync-search-query')),
     /** @type {!HTMLButtonElement} */ ($('sync-search-submit')),
     getRequiredElement('sync-search-status'),
@@ -25,6 +23,5 @@ decorateSearchControls(
 
 // Add a way to override the data model for the sync results list for testing.
 window.setupSyncResultsListForTest = function(data) {
-  List.decorate($('sync-results-list'));
-  $('sync-results-list').dataModel = new ArrayDataModel(data);
+  manager.setDataForTest(data);
 };
