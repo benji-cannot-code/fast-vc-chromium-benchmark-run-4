027d407f2cf165e4fe4dd86c20b90e3ea9a01167
Ryan Keane <rwkeane@google.com>
2022-06-17 16:02:15 +0000

    Reland "[Cast Convergence] Remove is_chromecast from //media"
    This is a reland of commit 031f07f03453a07a1557dad4967746e283758bba
    
    Reason for revert: Original CL causes flaky compilation failures in two
    fuchsia cast builders. e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast/165801
    See crbug.com/1336931 for further details
    
    Fix: Updated `media/mojo/mojom/BUILD.gn` to build
    `cast_application_media_info_manager.mojom` on Fuchsia-cast bots
    
    Original change's description:
    > [Cast Convergence] Remove is_chromecast from //media
    >
    > This CL updates the //media code to remove is_chromecast and replace it
    > with new `is_castos`, `is_cast_android` and `enable_cast_receiver` flags
    > as appropriate, which signify either "a nest chromecast-hardware device",
    > "an android cast receiving hardware device", or "a platform
    > which can act as a cast receiver" (such as ATV, Fuchsia, etc)
    > respectively
    >
    > Bug: 1219802, 1293592
    > Change-Id: I41bdfe69949d44605cebbfb53fb5d77212682444
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3621456
    > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Reviewed-by: Sean Topping <seantopping@chromium.org>
    > Reviewed-by: Sam Zackrisson <saza@chromium.org>
    > Commit-Queue: Ryan Keane <rwkeane@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1014745}
    
    Bug: 1219802, 1293592, 1336931
    Change-Id: I94f93f0cd057564a765cd10efcff685011b8816c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709839
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015369}

--- media/mojo/services/media_metrics_provider.cc
+++ media/mojo/services/media_metrics_provider.cc
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "media/filters/decrypting_video_decoder.h"
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif
 
-#if BUILDFLAG(IS_FUCHSIA) || (BUILDFLAG(IS_CHROMECAST) && BUILDFLAG(IS_ANDROID))
+#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_CAST_ANDROID)
 #include "media/mojo/services/playback_events_recorder.h"
 #endif
 
@@ -351,7 +351,7 @@ void MediaMetricsProvider::AcquireVideoDecodeStatsRecorder(
 
 void MediaMetricsProvider::AcquirePlaybackEventsRecorder(
     mojo::PendingReceiver<mojom::PlaybackEventsRecorder> receiver) {
-#if BUILDFLAG(IS_FUCHSIA) || (BUILDFLAG(IS_CHROMECAST) && BUILDFLAG(IS_ANDROID))
+#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_CAST_ANDROID)
   PlaybackEventsRecorder::Create(std::move(receiver));
 #endif
 }
