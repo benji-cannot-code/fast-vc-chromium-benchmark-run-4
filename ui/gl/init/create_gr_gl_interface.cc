b9951fbb67613e1838afb35b9b41708c1b6c51c4
Brian Salomon <bsalomon@google.com>
2022-06-17 14:53:40 +0000

    Add Skia bindings for glCopyBufferSubData
    Bug: skia:12720
    Change-Id: Ia4cd9b64a1ba1c8737ad7c677ce5edb97f237457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3710691
    Auto-Submit: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015351}

--- ui/gl/init/create_gr_gl_interface.cc
+++ ui/gl/init/create_gr_gl_interface.cc
@@ -326,6 +326,7 @@ sk_sp<GrGLInterface> CreateGrGLInterface(
   BIND(CompileShader, Slow);
   BIND(CompressedTexImage2D, Slow, NeedFlushOnMac);
   BIND(CompressedTexSubImage2D, Slow);
+  BIND(CopyBufferSubData);
   BIND(CopyTexSubImage2D, Slow);
 #if BUILDFLAG(IS_APPLE)
   functions->fCreateProgram = [func = gl->glCreateProgramFn]() {
