b96a1bcf0c6b3f065718105ef8cd3a95edcc603c
Neil Coronado <nemco@google.com>
2022-06-17 00:39:18 +0000

    [TabStrip] Add tests for StripLayoutHelper#onDown()
    Add tests for StripLayoutHelper#onDown().
    
    Bug: 1336424
    Change-Id: I70a166981a012544f87e42765ab69985737b163e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705833
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Gaurav Jayasawal <gauravjj@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015152}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -2249,6 +2249,14 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
     }
 
     /**
+     * @return The currently interacting tab.
+     */
+    @VisibleForTesting
+    StripLayoutTab getInteractingTab() {
+        return mInteractingTab;
+    }
+
+    /**
      * Disables animations for testing purposes.
      */
     @VisibleForTesting
