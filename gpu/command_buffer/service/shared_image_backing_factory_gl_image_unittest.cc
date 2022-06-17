e4481879d10f65066d82abd2031a249da361b9c3
Andres Calderon Jaramillo <andrescj@chromium.org>
2022-06-17 04:52:00 +0000

    Plumb color space to gl::GLImageNativePixmap.
    This CL plumbs the gfx::ColorSpace from the SharedImage system into the
    gl::GLImageNativePixmap. This is important because until now,
    gl::GLImageNativePixmap has assumed that YUV NativePixmaps can be
    sampled as RGB assuming an encoding of BT.601 with limited range. This
    is not correct in general (and we have found a case in the wild for
    this in ChromeOS).
    
    Bug: b:220336463, b:233667677
    Test: None
    Change-Id: I3eaa42612946ac3ac1bc3aa75e6edc67cfda19dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662321
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015234}

--- gpu/command_buffer/service/shared_image_backing_factory_gl_image_unittest.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_gl_image_unittest.cc
@@ -666,6 +666,7 @@ class SharedImageBackingFactoryGLImageWithGMBTest
       gfx::GpuMemoryBufferHandle handle,
       const gfx::Size& size,
       gfx::BufferFormat format,
+      const gfx::ColorSpace& color_space,
       gfx::BufferPlane plane,
       int client_id,
       gpu::SurfaceHandle surface_handle) override {
