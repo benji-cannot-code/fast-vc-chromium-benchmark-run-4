8381b1d2644e6000b190bfd55717f151386ca79c
Tsuyoshi Horo <horo@chromium.org>
2022-06-17 01:37:38 +0000

    [/net] modernize-use-override
    This change was automated with run-clang-tidy.py and
    `git cl format`.
    
    This should cause no functional changes.
    
    Bug: 1334056
    Change-Id: I1d46bb8e60cd934138082953abcecdd2172349bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697964
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015179}

--- net/cert/x509_certificate_unittest.cc
+++ net/cert/x509_certificate_unittest.cc
@@ -1093,7 +1093,7 @@ const struct CertificateFormatTestData {
 class X509CertificateParseTest
     : public testing::TestWithParam<CertificateFormatTestData> {
  public:
-  virtual ~X509CertificateParseTest() = default;
+  ~X509CertificateParseTest() override = default;
   void SetUp() override { test_data_ = GetParam(); }
   void TearDown() override {}
 
