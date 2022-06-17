2d132c3c824d600a620514c3d77efe1f853e5cb3
Andrew Dear <andrewdear@google.com>
2022-06-17 19:33:53 +0000

    [CrOS Bluetooth] Add BluetoothPairingAttempts metric to the legacy HID screen.
    Keeps track of the total number of Bluetooth device pairing attempts
    made while HID detection screen is shown. Emits the metric on the next
    step after the HID detection screen.
    
    Fixed: b/232850175
    Change-Id: Ie04509c2bc07acdbf71ea1c0135d0038cd93a094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3679787
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015478}

--- chrome/browser/ash/login/screens/hid_detection_screen.cc
+++ chrome/browser/ash/login/screens/hid_detection_screen.cc
@@ -138,6 +138,7 @@ void HIDDetectionScreen::OverrideInputDeviceManagerBinderForTesting(
 }
 
 void HIDDetectionScreen::OnContinueButtonClicked() {
+  hid_detection::RecordBluetoothPairingAttempts(num_pairing_attempts_);
   CleanupOnExit();
   Exit(Result::NEXT);
 }
@@ -340,6 +341,7 @@ void HIDDetectionScreen::ConnectBTDevice(device::BluetoothDevice* device) {
     mouse_is_pairing_ = true;
     keyboard_is_pairing_ = true;
   }
+  ++num_pairing_attempts_;
   device->Connect(this, base::BindOnce(&HIDDetectionScreen::OnConnect,
                                        weak_ptr_factory_.GetWeakPtr(),
                                        device->GetAddress(), device_type));
