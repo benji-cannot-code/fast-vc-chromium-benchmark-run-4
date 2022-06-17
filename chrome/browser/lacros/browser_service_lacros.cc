1d593d44bd7e9d099de84c117b4a5fc3d60b946f
Georg Neis <neis@chromium.org>
2022-06-17 00:46:38 +0000

    Lacros: Always show browser window on NewTab
    Make Lacros match the Ash behavior when the window is in the background
    or minimized.
    
    As an intended side effect, this change also makes NewTab add a new tab
    when it restores a session.
    
    Bug: chromium:1327976
    Change-Id: I189cc8704b75ab0c24f577cda6d4486c233484ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652367
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015156}

--- chrome/browser/lacros/browser_service_lacros.cc
+++ chrome/browser/lacros/browser_service_lacros.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/profile_picker.h"
+#include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/browser/ui/startup/lacros_first_run_service.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/startup/startup_tab.h"
@@ -600,12 +601,16 @@ void BrowserServiceLacros::NewTabWithProfile(
     return;
   }
 
-  Browser* browser = chrome::FindBrowserWithProfile(profile);
-  if (browser) {
-    chrome::NewTab(browser);
-  } else {
-    chrome::NewEmptyWindow(profile, should_trigger_session_restore);
+  Browser* browser;
+  {
+    chrome::ScopedTabbedBrowserDisplayer displayer(
+        profile, should_trigger_session_restore);
+    browser = displayer.browser();
+    if (browser)
+      chrome::NewTab(browser);
   }
+  if (browser)
+    browser->SetFocusToLocationBar();
   std::move(callback).Run();
 }
 
--- chrome/browser/ui/scoped_tabbed_browser_displayer.cc
+++ chrome/browser/ui/scoped_tabbed_browser_displayer.cc
@@ -11,11 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-ScopedTabbedBrowserDisplayer::ScopedTabbedBrowserDisplayer(Profile* profile) {
+ScopedTabbedBrowserDisplayer::ScopedTabbedBrowserDisplayer(
+    Profile* profile,
+    bool should_trigger_session_restore) {
   browser_ = FindTabbedBrowser(profile, false);
   if (!browser_ && Browser::GetCreationStatusForProfile(profile) ==
                        Browser::CreationStatus::kOk) {
-    browser_ = Browser::Create(Browser::CreateParams(profile, true));
+    Browser::CreateParams params(profile, /*user_gesture=*/true);
+    params.should_trigger_session_restore = should_trigger_session_restore;
+    browser_ = Browser::Create(params);
   }
 }
 
