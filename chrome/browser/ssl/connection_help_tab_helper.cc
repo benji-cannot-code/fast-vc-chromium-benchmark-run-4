aa199e6e156d394f43db0fd85978d1ecf9cfc88d
dpapad <dpapad@chromium.org>
2022-06-17 01:29:18 +0000

    Use kChromeUIConnectionHelpURL in connection_help_tab_helper.cc
    This removes a redundant string, and also makes it obvious when
    searching the code that chrome://connection-help is not dead code.
    
    Bug: 811382
    Change-Id: I038ca216a8bd875d0b80ed229ca0100351b7e926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708720
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015176}

--- chrome/browser/ssl/connection_help_tab_helper.cc
+++ chrome/browser/ssl/connection_help_tab_helper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/security_interstitials/content/ssl_blocking_page.h"
+#include "components/security_interstitials/content/urls.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/referrer.h"
@@ -17,14 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const char kHelpCenterConnectionHelpUrl[] =
     "https://support.google.com/chrome/answer/6098869";
-const char kBundledConnectionHelpUrl[] = "chrome://connection-help";
 
 void RedirectToBundledHelp(content::WebContents* web_contents) {
   GURL::Replacements replacements;
   std::string error_code = web_contents->GetLastCommittedURL().ref();
   replacements.SetRefStr(error_code);
   web_contents->GetController().LoadURL(
-      GURL(kBundledConnectionHelpUrl).ReplaceComponents(replacements),
+      GURL(security_interstitials::kChromeUIConnectionHelpURL)
+          .ReplaceComponents(replacements),
       content::Referrer(), ui::PageTransition::PAGE_TRANSITION_AUTO_TOPLEVEL,
       std::string());
 }
