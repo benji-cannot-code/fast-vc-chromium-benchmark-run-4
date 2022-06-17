d57d011297875023d52c1666b348d409268f4f2c
Maksim Sisov <msisov@igalia.com>
2022-06-17 10:02:31 +0000

    viz: do not request width or height to be larger than max texture size.
    Resize optimization that is used on ozone/wayland may request the
    surface's size to be larger by 256 pixels so that resize is smooth
    (the final size is adjusted with the viewport protocol).
    
    However, it may request the size larger than the max texture size
    supported by gl. Thus, limit that to that size.
    
    Bug: 1290477
    Change-Id: I0bc29bc20d18f1e0c5392693c74d1b477ee12656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703846
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1015291}

--- components/viz/service/display_embedder/skia_output_device.cc
+++ components/viz/service/display_embedder/skia_output_device.cc
@@ -114,6 +114,7 @@ SkiaOutputDevice::SkiaOutputDevice(
       latency_tracker_runner_(CreateLatencyTracerRunner()) {
   DCHECK(gr_context);
   capabilities_.max_render_target_size = gr_context->maxRenderTargetSize();
+  capabilities_.max_texture_size = gr_context->maxTextureSize();
 }
 
 SkiaOutputDevice::~SkiaOutputDevice() {
