c5bf62e4bed8bd80cee371a05ac8455c84415d8e
Donn Denman <donnd@chromium.org>
2022-06-17 00:58:39 +0000

    [TTS][Reviver] Revive testTranslateCaption limited
    Revives a limited form of the testTranslateCaption test.
    
    BUG=1333146,1333151
    
    Change-Id: I2ee85fc028860b75680acd371c081a480502e6a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709251
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015163}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextualsearch;
 
-import static org.chromium.base.test.util.CriteriaHelper.DEFAULT_POLLING_INTERVAL;
 import static org.chromium.base.test.util.Restriction.RESTRICTION_TYPE_NON_LOW_END_DEVICE;
 import static org.chromium.chrome.browser.multiwindow.MultiWindowTestHelper.waitForSecondChromeTabbedActivity;
 import static org.chromium.chrome.browser.multiwindow.MultiWindowTestHelper.waitForTabs;
@@ -45,7 +44,6 @@ import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
-import org.chromium.base.test.util.Manual;
 import org.chromium.base.test.util.MaxAndroidSdkLevel;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
@@ -486,13 +484,8 @@ public class ContextualSearchManagerTest extends ContextualSearchInstrumentation
     }
 
     /**
-     * Tests the Translate Caption on a resolve gesture.
-     * This test is disabled because it relies on the network and a live search result,
-     * which would be flaky for bots.
-     * TODO(donnd) Load a fake SERP into the panel to trigger SERP-translation and similar
-     * features.
+     * Tests the Translate Caption on a resolve gesture forces a translation.
      */
-    @Manual(message = "Useful for manual testing when a network is connected.")
     @Test
     @LargeTest
     @Feature({"ContextualSearch"})
@@ -505,17 +498,6 @@ public class ContextualSearchManagerTest extends ContextualSearchInstrumentation
         Assert.assertTrue("Translation was not forced with the current request URL: "
                         + mManager.getRequest().getSearchUrl(),
                 mManager.getRequest().isTranslationForced());
-
-        // Wait for the translate caption to be shown in the Bar.
-        int waitFactor = 5; // We need to wait an extra long time for the panel content to render.
-        CriteriaHelper.pollUiThread(() -> {
-            ContextualSearchBarControl barControl = mPanel.getSearchBarControl();
-            Criteria.checkThat(barControl, Matchers.notNullValue());
-            Criteria.checkThat(barControl.getCaptionVisible(), Matchers.is(true));
-            Criteria.checkThat(barControl.getCaptionText(), Matchers.notNullValue());
-            Criteria.checkThat(
-                    barControl.getCaptionText().toString(), Matchers.not(Matchers.isEmptyString()));
-        }, 3000 * waitFactor, DEFAULT_POLLING_INTERVAL * waitFactor);
     }
 
     //============================================================================================
