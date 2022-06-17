90ab3464f56a580c34fddbcec352d8aa056b7a92
Thoren Paulson <thoren@google.com>
2022-06-17 21:54:05 +0000

    [Chromecast] Attach a "window" while apps are in background.
    Some media decoding is blocked when WebContents doesn't
    have a top level window. This CL uses CWCService to host
    WebContents while CWCActivity is in the background.
    
    Bug: b/229413393
    Test: cast a bunch of apps, try backgrounding and changing tracks
    
    Change-Id: Id2670cb827319493d19ba48546daef8f41f5b10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692056
    Reviewed-by: Thoren Paulson <thoren@chromium.org>
    Commit-Queue: Thoren Paulson <thoren@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015556}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsService.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsService.java
@@ -11,7 +11,6 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
 
@@ -60,9 +59,6 @@ public class CastWebContentsService extends Service {
         });
         mWebContentsState.map(this::getMediaSessionImpl)
                 .subscribe(Observers.onEnter(MediaSessionImpl::requestSystemAudioFocus));
-        // Inform CastContentWindowAndroid we're detaching.
-        Observable<String> instanceIdState = mIntentState.map(Intent::getData).map(Uri::getPath);
-        instanceIdState.subscribe(Observers.onExit(CastWebContentsComponent::onComponentClosed));
 
         if (DEBUG) {
             mWebContentsState.subscribe(x -> {
