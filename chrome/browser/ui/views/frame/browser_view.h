13bff4f59a1dd9d36e3005799063d9a68e2cd3df
Mark Mentovai <mark@chromium.org>
2022-06-18 02:41:02 +0000

    Revert "Feature to stop rendering loading animation when window is hidden."
    This reverts commit c2e8fba07482b33d80fbfbc5f785fbdb4a43fa23.
    
    Reason for revert: https://crbug.com/1337425
    
    Original change's description:
    > Feature to stop rendering loading animation when window is hidden.
    >
    > Bug: 1322081
    > Change-Id: Ic9e181c5c693212cbd77118b2ce2b2fb725d563c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686624
    > Commit-Queue: Alex Attar <aattar@google.com>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1014491}
    
    Bug: 1322081
    Change-Id: Ie8245ca58a6b66391e0a96a49eff2a90a08c5e72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711022
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mark Mentovai <mark@chromium.org>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015632}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -149,8 +149,6 @@ class BrowserView : public BrowserWindow,
   // (and hide immediately).
   static void SetDisableRevealerDelayForTesting(bool disable);
 
-  bool IsLoadingAnimationRunningForTesting() const;
-
   // Returns a Browser instance of this view.
   Browser* browser() { return browser_.get(); }
   const Browser* browser() const { return browser_.get(); }
@@ -630,7 +628,6 @@ class BrowserView : public BrowserWindow,
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
-  void OnWidgetVisibilityChanged(views::Widget* widget, bool visible) override;
 
   // views::ClientView:
   views::CloseRequestResult OnWindowCloseRequested() override;
