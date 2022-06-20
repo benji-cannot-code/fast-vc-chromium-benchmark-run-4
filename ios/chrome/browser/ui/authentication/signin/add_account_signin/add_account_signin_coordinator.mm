2f3433e458d4e79e22b4f0852e1dcf79464acc11
jlebel <jlebel@chromium.org>
2022-06-20 18:05:54 +0000

    [iOS] Add activity overlay in AddAccountSigninCoordinator
    The add account sign-in manager can take some time to be displayed if
    the network is slow (or all packets are dropped). While waiting for
    this dialog to be presented, we need to display an activity overlay
    to make sure the user can't tap on buttons.
    
    Fixed: 1334017
    Change-Id: I2cd135a256e9ee9c1061f9ab3988134680aea740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706892
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015961}

--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
 #import "ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_manager.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_coordinator+protected.h"
+#import "ios/chrome/browser/ui/elements/activity_overlay_coordinator.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.h"
@@ -50,6 +51,11 @@ using signin_metrics::PromoAction;
 @property(nonatomic, assign, readonly) AddAccountSigninIntent signinIntent;
 // Account manager service to retrieve Chrome identities.
 @property(nonatomic, assign) ChromeAccountManagerService* accountManagerService;
+// Activity overlay coordinator needed while waiting for the add account sign-in
+// manager to be presented. This is needed if the network is slow or not
+// responding.
+@property(nonatomic, strong)
+    ActivityOverlayCoordinator* activityOverlayCoordinator;
 
 @end
 
@@ -105,6 +111,10 @@ using signin_metrics::PromoAction;
 
 - (void)start {
   [super start];
+  self.activityOverlayCoordinator = [[ActivityOverlayCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                         browser:self.browser];
+  [self.activityOverlayCoordinator start];
   self.accountManagerService =
       ChromeAccountManagerServiceFactory::GetForBrowserState(
           self.browser->GetBrowserState());
@@ -128,6 +138,9 @@ using signin_metrics::PromoAction;
 
 - (void)stop {
   [super stop];
+  DCHECK(self.activityOverlayCoordinator);
+  [self.activityOverlayCoordinator stop];
+  self.activityOverlayCoordinator = nil;
   // If one of those 3 DCHECK() fails, -[AddAccountSigninCoordinator
   // runCompletionCallbackWithSigninResult] has not been called.
   DCHECK(!self.addAccountSigninManager);
