1b502557086bd280cde6060dea2fc3ae3c494fdc
Saifuddin Hitawala <hitawala@chromium.org>
2022-06-20 17:40:24 +0000

    Use SIBackingOzone on Lacros Chrome
    SharedImageBackingOzone is currently used on ChromeOS Ash, Linux Wayland
    and X11 and Fuchsia platforms. This change uses it for Lacros Chrome as
    well.
    
    Bug: 1241537
    Change-Id: Ie362236f91156f173ba65d3567fe7361c1e4f9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652505
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015952}

--- gpu/command_buffer/service/shared_image_factory.cc
+++ gpu/command_buffer/service/shared_image_factory.cc
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_image_backing_factory_ahardwarebuffer.h"
 #elif BUILDFLAG(IS_MAC)
 #include "gpu/command_buffer/service/shared_image_backing_factory_iosurface.h"
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "gpu/command_buffer/service/shared_image_backing_factory_ozone.h"
 #endif
 
@@ -405,7 +405,7 @@ SharedImageFactory::SharedImageFactory(
     factories_.push_back(std::move(external_vk_image_factory));
   }
   vulkan_context_provider_ = context_state->vk_context_provider();
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
   auto ozone_factory = std::make_unique<SharedImageBackingFactoryOzone>(
       context_state, workarounds);
   factories_.push_back(std::move(ozone_factory));
