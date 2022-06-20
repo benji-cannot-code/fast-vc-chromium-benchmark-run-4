21dba64ff10013531238e9a8758a9d9f9af8b040
Eriko Kurimoto <elkurin@chromium.org>
2022-06-20 21:31:24 +0000

    [ArcMigration] Introduce data field to app_service_type Intent
    This CL adds data field to Intent struct in app_service_type.mojom.
    `data` field is responsible for the url and query to launch.
    Without this field, some info drops on intent handling for example the
    search info.
    
    Bug: 1336891
    Change-Id: I27389a49d1e7b7371ef913bf24b2d16da58061cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709391
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015997}

--- chrome/browser/apps/app_service/intent_util.cc
+++ chrome/browser/apps/app_service/intent_util.cc
@@ -1221,6 +1221,9 @@ crosapi::mojom::IntentPtr ConvertAppServiceToCrosapiIntent(
   if (app_service_intent->activity_name.has_value()) {
     crosapi_intent->activity_name = app_service_intent->activity_name.value();
   }
+  if (app_service_intent->data.has_value())
+    crosapi_intent->data = app_service_intent->data.value();
+
   return crosapi_intent;
 }
 
@@ -1314,6 +1317,8 @@ apps::mojom::IntentPtr ConvertCrosapiToAppServiceIntent(
   if (crosapi_intent->activity_name.has_value()) {
     app_service_intent->activity_name = crosapi_intent->activity_name.value();
   }
+  if (crosapi_intent->data.has_value())
+    app_service_intent->data = crosapi_intent->data.value();
   return app_service_intent;
 }
 
