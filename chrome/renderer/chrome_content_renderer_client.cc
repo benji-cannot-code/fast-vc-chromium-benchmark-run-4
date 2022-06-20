80a8629569bb916970449306407cf183a8fbb621
Tushar Agarwal <agarwaltushar@google.com>
2022-06-20 17:08:25 +0000

    Enable stack sampling for tracing on the renderer compositor thread.
    Per nuskos@ and altimin@, this would be helpful for studying and
    improving jank and efficiency.
    
    Change-Id: I22ba85568046e7a836c67f701778e6218f92be5f
    Bug: b/235485402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702301
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015941}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1345,6 +1345,10 @@ void ChromeContentRendererClient::PostCompositorThreadCreated(
       FROM_HERE,
       base::BindOnce(&ThreadProfiler::StartOnChildThread,
                      metrics::CallStackProfileParams::Thread::kCompositor));
+  // Enable stack sampling for tracing.
+  compositor_thread_task_runner->PostTask(
+      FROM_HERE,
+      base::BindOnce(&tracing::TracingSamplerProfiler::CreateOnChildThread));
 }
 
 bool ChromeContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
