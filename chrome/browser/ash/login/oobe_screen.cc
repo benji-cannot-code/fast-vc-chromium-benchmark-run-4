8ad8208ad11acfdd1d30d9caaf599d2b691b11fd
Brahim Chikhaoui <bchikhaoui@google.com>
2022-06-20 15:52:18 +0000

    OOBE: Introduce WizardController::FindScreenByName
    Update the Screen Manager to include method GetScreenByName
    Update the Wizard Controller to include method FindScreenByName
    Both new introduced methods return OobeScreenId including name and
    external_api_prefix.
    
    Bug: 1312880
    Change-Id: Ifc2ae02c49339c2084d0ec84c86abe8958eea74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707675
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1015918}

--- chrome/browser/ash/login/oobe_screen.cc
+++ chrome/browser/ash/login/oobe_screen.cc
@@ -11,6 +11,10 @@ namespace ash {
 
 OobeScreenId::OobeScreenId(const std::string& name) : name(name) {}
 
+OobeScreenId::OobeScreenId(const std::string& name,
+                           const std::string& api_prefix)
+    : name(name), external_api_prefix(api_prefix) {}
+
 OobeScreenId::OobeScreenId(const StaticOobeScreenId& id)
     : name(id.name) {
   if (id.external_api_prefix)
