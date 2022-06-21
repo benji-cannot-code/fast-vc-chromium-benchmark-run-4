d7c97de8685647943e743238fadac4473fb96584
Matt Menke <mmenke@chromium.org>
2022-06-21 23:27:35 +0000

    Modernize base::Value usage in net/http/
    In particular, make all list and dictionary accesses and creation use
    base::Value::List and base::Value::Dict.
    
    That means replacing uses of ListValue, Value::ListStorage,
    Value::ListView, Value::GetListDeprecated(), Value::Append(),
    Value::Find*Key(), Value::Set*Key(), etc.
    
    Also update one net/dns/ consumer of vector<Value> (which
    Value::ListStorage), which was overlooked in an earlier CL.
    
    Bug: 1187001
    Change-Id: Icdd94445d9af2961e6ed7f77c972f7b9c77aafb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715533
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016419}

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -194,7 +194,7 @@ bool IsTransportSocketPoolStalled(HttpNetworkSession* session) {
 std::string GetHeaders(const base::Value& params) {
   if (!params.is_dict())
     return "";
-  const base::Value* header_list = params.FindListKey("headers");
+  const base::Value::List* header_list = params.GetDict().FindList("headers");
   if (!header_list)
     return "";
   std::string headers;
