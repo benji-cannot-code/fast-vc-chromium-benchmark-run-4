332886dee4e5abbffcf420b2f22a6cd4dc108a72
Philip Rogers <pdr@chromium.org>
2022-06-21 02:00:44 +0000

    Remove OptimizeViewportConstrainedPaintInvalidation experiment
    Bug: 1310584
    Change-Id: Ieb9f6267fd4de2d48113434550f8d0de28f56979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695752
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016028}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -311,8 +311,6 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
   PaintLayer* ContainingLayer(const PaintLayer* ancestor = nullptr,
                               bool* skipped_ancestor = nullptr) const;
 
-  bool HasAncestorWithFilterThatMovesPixels() const;
-
   void ConvertToLayerCoords(const PaintLayer* ancestor_layer,
                             PhysicalOffset&) const;
   void ConvertToLayerCoords(const PaintLayer* ancestor_layer,
