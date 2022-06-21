0982e28fc9499f35395b1851d61250b390a6f2f1
Zachary Tan <tanzachary@chromium.org>
2022-06-21 15:23:37 +0000

    Record request timeout in metrics
    Bug: 1337356
    Change-Id: I7627648feeee1a8d8eb70c49ecb4f0a994d50313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3712166
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016210}

--- content/browser/webid/fedcm_metrics.h
+++ content/browser/webid/fedcm_metrics.h
@@ -47,8 +47,9 @@ enum class FedCmRequestIdTokenStatus {
   kManifestNotInManifestList,
   kManifestListTooBig,
   kDisabledEmbargo,
+  kUserInterfaceTimedOut,
 
-  kMaxValue = kDisabledEmbargo
+  kMaxValue = kUserInterfaceTimedOut
 };
 
 // This enum describes the status of a revocation call to the FedCM API.
