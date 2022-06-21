b7197385ec43177cffe1f180392086a38c1f09ca
Chris Bookholt <bookholt@chromium.org>
2022-06-21 19:50:56 +0000

    MediaStreamManager teardown should happen only on the IO thread in order to match behavior in a real browser setting
    Bug: 1338178
    Change-Id: Iac0c0b691bf99cde83b6d2d2f1d7dac9c1a17696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714872
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016356}

--- content/test/fuzzer/media_stream_dispatcher_host_mojolpm_fuzzer.cc
+++ content/test/fuzzer/media_stream_dispatcher_host_mojolpm_fuzzer.cc
@@ -185,6 +185,8 @@ void MediaStreamDispatcherHostTestcase::TearDown(
     base::OnceClosure done_closure) {
   mojolpm::GetContext()->EndTestcase();
 
+  media_stream_manager_->WillDestroyCurrentMessageLoop();
+
   content::GetUIThreadTaskRunner({})->PostTask(
       FROM_HERE,
       base::BindOnce(&MediaStreamDispatcherHostTestcase::TearDownOnUIThread,
@@ -194,8 +196,6 @@ void MediaStreamDispatcherHostTestcase::TearDown(
 void MediaStreamDispatcherHostTestcase::TearDownOnUIThread(
     base::OnceClosure done_closure) {
   audio_manager_->Shutdown();
-
-  media_stream_manager_->WillDestroyCurrentMessageLoop();
 
   std::move(done_closure).Run();
 }
