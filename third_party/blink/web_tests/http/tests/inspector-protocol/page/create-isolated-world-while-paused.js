24834c71f1645ec3dfbd5e0d06829f1ec41a128f
Andrey Kosyakov <caseq@chromium.org>
2022-06-21 07:07:28 +0000

    Make Page.createIsolatedWorld work when invoked before commit
    An attempt to create an isolated world before commit hits a CHECK() in
    LocalWindowProxy::Initialize(), so defer the world creation till
    DidClearDocumentOfWindowObject().
    
    Change-Id: I275f263623719665a01837f9e7384024cd5ce58e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711053
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016069}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/create-isolated-world-while-paused.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank('Tests that Page.createIsolatedWorld works when issued on paused target.');
+
+  await dp.Target.setAutoAttach(
+    {autoAttach: true, waitForDebuggerOnStart: true, flatten: true});
+
+  session.navigate(testRunner.url('../resources/site_per_process_main.html'));
+  const attached = await dp.Target.onceAttachedToTarget();
+  const dp2 = session.createChild(attached.params.sessionId).protocol;
+  if (!attached.params.waitingForDebugger)
+    testRunner.log("FAIL: not waiting for debugger");
+  const frameId = attached.params.targetInfo.targetId;
+  dp2.Page.enable();
+  const responsePromise = dp2.Page.createIsolatedWorld({frameId, worldName: 'Test world'});
+  await dp2.Runtime.runIfWaitingForDebugger();
+  testRunner.log((await responsePromise).result);
+  testRunner.completeTest();
+})
