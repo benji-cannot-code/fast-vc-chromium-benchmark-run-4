139d7e892ed13b4abcb1a270f542a56939283037
Mohit Saini <mohitms@google.com>
2022-06-21 15:40:18 +0000

    startup_trace in SamplingProfilerDS migration
    The CL makes sampling profiler data source work in startup_trace as well
    in the SDK build of chrome.
    (SDK build = USE_PERFETTO_CLIENT_LIBRARY flag ON)
    
    Cq-Include-Trybots: luci.chromium.try:linux-perfetto-rel
    Bug: b/236404175
    Change-Id: I270744d538260f39a157a2bb834f26f437a957f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706992
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Mohit Saini <mohitms@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016217}

--- services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler_unittest.cc
+++ services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler_unittest.cc
@@ -279,11 +279,7 @@ TEST_F(TracingSampleProfilerTest, JoinRunningTracing) {
   ValidateReceivedEvents();
 }
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
-TEST_F(TracingSampleProfilerTest, DISABLED_TestStartupTracing) {
-#else
 TEST_F(TracingSampleProfilerTest, TestStartupTracing) {
-#endif
   auto profiler = TracingSamplerProfiler::CreateOnMainThread();
   TracingSamplerProfiler::SetupStartupTracingForTesting();
   base::RunLoop().RunUntilIdle();
@@ -317,11 +313,7 @@ TEST_F(TracingSampleProfilerTest, TestStartupTracing) {
   }
 }
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
-TEST_F(TracingSampleProfilerTest, DISABLED_JoinStartupTracing) {
-#else
 TEST_F(TracingSampleProfilerTest, JoinStartupTracing) {
-#endif
   TracingSamplerProfiler::SetupStartupTracingForTesting();
   base::RunLoop().RunUntilIdle();
   auto profiler = TracingSamplerProfiler::CreateOnMainThread();
@@ -501,7 +493,9 @@ TEST_F(TracingProfileBuilderTest, ValidModule) {
   base::TestModule module;
   TracingSamplerProfiler::TracingProfileBuilder profile_builder(
       base::PlatformThreadId(),
-#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+      false /* is_startup_tracing */,
+#else
       std::make_unique<TestTraceWriter>(producer()),
 #endif
       false);
@@ -512,7 +506,9 @@ TEST_F(TracingProfileBuilderTest, ValidModule) {
 TEST_F(TracingProfileBuilderTest, InvalidModule) {
   TracingSamplerProfiler::TracingProfileBuilder profile_builder(
       base::PlatformThreadId(),
-#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+      false /* is_startup_tracing */,
+#else
       std::make_unique<TestTraceWriter>(producer()),
 #endif
       false);
