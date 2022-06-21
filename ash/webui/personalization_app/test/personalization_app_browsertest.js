34246c5f7b7e4942f9c6deb53c2801a19c02c72e
Jeffrey Young <cowmoo@google.com>
2022-06-21 17:53:04 +0000

    personalization: remove kWallpaperWebUI from tests
    Feature has been launched for a while.
    
    BUG=b:193788853
    TEST=browser_tests --gtest_filter="*PersonalizationApp*"
    
    Change-Id: Ibf39f9f43b275d07eeb76f9a951268922959b5f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711094
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016293}

--- ash/webui/personalization_app/test/personalization_app_browsertest.js
+++ ash/webui/personalization_app/test/personalization_app_browsertest.js
@@ -27,7 +27,6 @@ class PersonalizationAppBrowserTest extends testing.Test {
   get featureList() {
     return {
       enabled: [
-        'ash::features::kWallpaperWebUI',
         'ash::features::kPersonalizationHub',
         'chromeos::features::kDarkLightMode',
       ]
--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_browsertest.js
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_browsertest.js
@@ -20,12 +20,7 @@ var PersonalizationAppComponentBrowserTest = class extends PolymerTest {
   }
 
   get featureList() {
-    return {
-      enabled: [
-        'chromeos::features::kWallpaperWebUI',
-        'ash::features::kWallpaperGooglePhotosIntegration'
-      ]
-    };
+    return {enabled: ['ash::features::kWallpaperGooglePhotosIntegration']};
   }
 };
 
