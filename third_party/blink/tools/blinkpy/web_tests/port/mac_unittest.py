09288929877078193dcda8bceabf5f3425bdf646
Jonathan Lee <jonathanjlee@google.com>
2022-06-21 15:54:41 +0000

    [blinkpy] Derive smoke test default from port OS version, not host
    'copy-existing-baselines-internal' did not skip predecessor Mac
    versions that only ran smoke tests by default. This was because the
    smoke test default used the rebaselining system's OS version, which is
    'linux-trusty' on the importer, not the version of the port copied to.
    
    Fixed: 1322304
    Change-Id: I1bde713b06295eccb8eec93ce9f1916803a8d8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715655
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016228}

--- third_party/blink/tools/blinkpy/web_tests/port/mac_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/port/mac_unittest.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import optparse
 
+from blinkpy.common.system.platform_info_mock import MockPlatformInfo
 from blinkpy.web_tests.port import mac
 from blinkpy.web_tests.port import port_testcase
 
@@ -88,3 +89,20 @@ class MacPortTest(port_testcase.PortTestCase):
             port.path_to_apache_config_file(),
             '/mock-checkout/third_party/blink/tools/apache_config/apache2-httpd-2.4-php7.conf'
         )
+
+    def test_default_smoke_test_only(self):
+        """Verify that older Mac versions run only smoke tests by default.
+
+        The smoke test default should not depend on the current host's platform.
+        """
+        port = self.make_port(os_version='mac11')
+        self.assertFalse(port.default_smoke_test_only())
+        all_tests_platform = MockPlatformInfo('mac', 'mac11')
+
+        port = self.make_port(os_version='mac10.13')
+        port.host.platform = all_tests_platform
+        self.assertTrue(port.default_smoke_test_only())
+
+        port = self.make_port(os_version='mac10.14')
+        port.host.platform = all_tests_platform
+        self.assertTrue(port.default_smoke_test_only())
