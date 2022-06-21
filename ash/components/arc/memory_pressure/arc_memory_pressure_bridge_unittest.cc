2ec18b493c21e1ec98fca1c5c5f3781abd0ee1d9
Mohsen Izadi <mohsen@chromium.org>
2022-06-21 14:44:07 +0000

    Revert "autotest_private: getArcAppKills method"
    This reverts commit 905a70654943ff0efcd76060ba64e7e4e4b880a3.
    
    Reason for revert: Suspected culprit for failures of AutotestPrivateApiTest.AutotestPrivateArcEnabled on Linux ChromiumOS MSan Tests; e.g.: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/28626/overview.
    
    Original change's description:
    > autotest_private: getArcAppKills method
    >
    > Add a method that can query ArcProcessService for the app kill counters
    > that are also logged as UMA metrics.
    >
    > This will be used to assert that no apps are killed during boot in
    > tests, and for extra app kill counts metrics in tests that do expect to
    > kill apps.
    >
    > Also refactored arc::FakeProcessInstance to use PostTask so that it can
    > be used with the JS autotest_private tests.
    >
    > Bug: b:202070334
    > Test: tast run dut arc.PerfBoot.vm, kill counts are 0
    > tast run dut multivm.Lifecycle.arc, kill counts are reasonable
    > ./out/Default/browser_tests --gtest_filter='*AutotestPrivateApiTest*'
    > ./testing/xvfb.py ./out/Default/ash_components_unittests
    > --gtest_filter='*ArcMemoryPressureBridge*'
    > ./testing/xvfb.py ./out/Default/ash_components_unittests
    > --gtest_filter='*ArcMetricsService*'
    >
    > Change-Id: Iaffd1147597c4eb50af9693202ea075a511a69bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697884
    > Commit-Queue: Charles William Dick <cwd@google.com>
    > Reviewed-by: Yury Khmel <khmel@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1016037}
    
    Bug: b:202070334
    Change-Id: I0b1699cb3c65c51eea442862e4faddcea8c039b0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716762
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016187}

--- ash/components/arc/memory_pressure/arc_memory_pressure_bridge_unittest.cc
+++ ash/components/arc/memory_pressure/arc_memory_pressure_bridge_unittest.cc
@@ -117,19 +117,15 @@ TEST_F(ArcMemoryPressureBridgeTest, PressureNone) {
 // priority.
 TEST_F(ArcMemoryPressureBridgeTest, PressureCached) {
   ASSERT_NE(nullptr, bridge());
-  // Check for overflow for large reclaim values by passing 5 GiB to the
-  // callback, and then check that we report 5 MiB * KiB estimated_freed_kib.
-  process_instance().set_apply_host_memory_pressure_response(
-      1 /* killed */, UINT64_C(5368709120) /* reclaimed */);
   resourced().FakeArcVmMemoryPressure(
       chromeos::ResourcedClient::PressureLevelArcVm::CACHED,
       1 /* reclaim_target_kb */);
   ASSERT_TRUE(process_instance().CheckLastHostMemoryPressure(
       mojom::PressureLevel::kCached, 1024 /* reclaim_target */));
-
-  // Run the ApplyHostMemoryPressure callback.
-  base::RunLoop().RunUntilIdle();
-
+  // Check for overflow for large reclaim values by passing 5 GiB to the
+  // callback, and then check that we report 5 MiB * KiB estimated_freed_kib.
+  process_instance().RunHostMemoryPressureCallback(
+      1 /* killed */, UINT64_C(5368709120) /* reclaimed */);
   ASSERT_TRUE(kill_observer().CheckLastMemoryPressureKill(
       1 /* count */, 5242880 /* estimated_freed_kb */));
 }
@@ -137,17 +133,13 @@ TEST_F(ArcMemoryPressureBridgeTest, PressureCached) {
 // Tests that PERCEPTIBLE memory pressure triggers kills of R_TOP priority.
 TEST_F(ArcMemoryPressureBridgeTest, PressurePerceptible) {
   ASSERT_NE(nullptr, bridge());
-  process_instance().set_apply_host_memory_pressure_response(
-      1 /* killed */, 2048 /* reclaimed */);
   resourced().FakeArcVmMemoryPressure(
       chromeos::ResourcedClient::PressureLevelArcVm::PERCEPTIBLE,
       1 /* reclaim_target_kb */);
   ASSERT_TRUE(process_instance().CheckLastHostMemoryPressure(
       mojom::PressureLevel::kPerceptible, 1024 /* reclaim_target */));
-
-  // Run the ApplyHostMemoryPressure callback.
-  base::RunLoop().RunUntilIdle();
-
+  process_instance().RunHostMemoryPressureCallback(1 /* killed */,
+                                                   2048 /* reclaimed */);
   ASSERT_TRUE(kill_observer().CheckLastMemoryPressureKill(
       1 /* count */, 2 /* estimated_freed_kb */));
 }
@@ -155,17 +147,13 @@ TEST_F(ArcMemoryPressureBridgeTest, PressurePerceptible) {
 // Tests that FOREGROUND memory pressure triggers kills of R_TOP priority.
 TEST_F(ArcMemoryPressureBridgeTest, PressureForeground) {
   ASSERT_NE(nullptr, bridge());
-  process_instance().set_apply_host_memory_pressure_response(
-      1 /* killed */, 2048 /* reclaimed */);
   resourced().FakeArcVmMemoryPressure(
       chromeos::ResourcedClient::PressureLevelArcVm::FOREGROUND,
       1 /* reclaim_target_kb */);
   ASSERT_TRUE(process_instance().CheckLastHostMemoryPressure(
       mojom::PressureLevel::kForeground, 1024 /* reclaim_target */));
-
-  // Run the ApplyHostMemoryPressure callback.
-  base::RunLoop().RunUntilIdle();
-
+  process_instance().RunHostMemoryPressureCallback(1 /* killed */,
+                                                   2048 /* reclaimed */);
   ASSERT_TRUE(kill_observer().CheckLastMemoryPressureKill(
       1 /* count */, 2 /* estimated_freed_kb */));
 }
@@ -174,8 +162,6 @@ TEST_F(ArcMemoryPressureBridgeTest, PressureForeground) {
 // only cause one call into ARCVM.
 TEST_F(ArcMemoryPressureBridgeTest, DebouncePressure) {
   ASSERT_NE(nullptr, bridge());
-  process_instance().set_apply_host_memory_pressure_response(
-      1 /* killed */, 2048 /* reclaimed */);
 
   // FakeProcessInstance::HostMemoryPressure will DCHECK if
   // FakeProcessInstance::CheckLastHostMemoryPressure is not called first. So
@@ -191,25 +177,19 @@ TEST_F(ArcMemoryPressureBridgeTest, DebouncePressure) {
   // did not get forwarded to Mojo.
   ASSERT_TRUE(process_instance().CheckLastHostMemoryPressure(
       mojom::PressureLevel::kPerceptible, 1024 /* reclaim_target */));
-
-  // Run the ApplyHostMemoryPressure callback.
-  base::RunLoop().RunUntilIdle();
-
+  process_instance().RunHostMemoryPressureCallback(1 /* killed */,
+                                                   2048 /* reclaimed */);
   ASSERT_TRUE(kill_observer().CheckLastMemoryPressureKill(
       1 /* count */, 2 /* estimated_freed_kb */));
 
   // Check that we do forward the next call after the callback is executed.
-  process_instance().set_apply_host_memory_pressure_response(
-      3 /* killed */, 4096 /* reclaimed */);
   resourced().FakeArcVmMemoryPressure(
       chromeos::ResourcedClient::PressureLevelArcVm::PERCEPTIBLE,
       3 /* reclaim_target_kb */);
   ASSERT_TRUE(process_instance().CheckLastHostMemoryPressure(
       mojom::PressureLevel::kPerceptible, 3072 /* reclaim_target */));
-
-  // Run the ApplyHostMemoryPressure callback.
-  base::RunLoop().RunUntilIdle();
-
+  process_instance().RunHostMemoryPressureCallback(3 /* killed */,
+                                                   4096 /* reclaimed */);
   ASSERT_TRUE(kill_observer().CheckLastMemoryPressureKill(
       3 /* count */, 4 /* estimated_freed_kb */));
 }
