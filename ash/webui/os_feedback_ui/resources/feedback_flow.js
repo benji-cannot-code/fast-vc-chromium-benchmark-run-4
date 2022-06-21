8e77ad6e0d1c430be9fc3437cc1b93fcb79d8f5d
xiangdong kong <xiangdongkong@google.com>
2022-06-21 17:55:31 +0000

    feedback: focus on the description input when starting a new report
    Screenshot: https://screenshot.googleplex.com/6bkymobYEtaxFhZ.png
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I0008fd56c32ea58d93ad5cb0e5766d731c6373be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697201
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016296}

--- ash/webui/os_feedback_ui/resources/feedback_flow.js
+++ ash/webui/os_feedback_ui/resources/feedback_flow.js
@@ -141,8 +141,7 @@ export class FeedbackFlowElement extends PolymerElement {
   handleGoBackClick_(event) {
     switch (event.detail.currentState) {
       case FeedbackFlowState.SHARE_DATA:
-        this.currentState_ = FeedbackFlowState.SEARCH;
-        this.shadowRoot.querySelector('search-page').focusInputElement();
+        this.navigateToSearchPage_();
         break;
       case FeedbackFlowState.CONFIRMATION:
         // Remove the text from previous search.
@@ -153,11 +152,17 @@ export class FeedbackFlowElement extends PolymerElement {
         const shareDataPage = this.shadowRoot.querySelector('share-data-page');
         shareDataPage.reEnableSendReportButton();
 
-        this.currentState_ = FeedbackFlowState.SEARCH;
+        this.navigateToSearchPage_();
         break;
       default:
         console.warn('unexpected state: ', event.detail.currentState);
     }
+  }
+
+  /** @private */
+  navigateToSearchPage_() {
+    this.currentState_ = FeedbackFlowState.SEARCH;
+    this.shadowRoot.querySelector('search-page').focusInputElement();
   }
 
   /**
