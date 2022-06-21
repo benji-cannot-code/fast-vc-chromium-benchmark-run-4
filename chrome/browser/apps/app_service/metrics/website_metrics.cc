6fa9fb69fcca11ac1d6555a81fcfbda2896f34f9
Nancy Wang <nancylingwang@chromium.org>
2022-06-21 00:41:25 +0000

    Move the ActiveTabWebContentsObserver definition to the .h file.
    This CL is used to prepare for CL:3714570 to add tests for WebsiteMetrics. Move the ActiveTabWebContentsObserver definition to the .h file to be used in the test code.
    
    BUG=1334173
    
    Change-Id: Iec5e9b8ef68700dfce34ae9cc3c63342aa778ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715790
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016018}

--- chrome/browser/apps/app_service/metrics/website_metrics.cc
+++ chrome/browser/apps/app_service/metrics/website_metrics.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "ui/aura/window.h"
 
 namespace {
@@ -57,29 +55,15 @@ wm::ActivationClient* GetActivationClientWithTabStripModel(
 
 namespace apps {
 
-// This class monitors the WebContent of the all tab and notifies a navigation
-// to the WebsiteMetrics.
-class WebsiteMetrics::ActiveTabWebContentsObserver
-    : public content::WebContentsObserver {
- public:
-  ActiveTabWebContentsObserver(content::WebContents* contents,
-                               WebsiteMetrics* owner)
-      : content::WebContentsObserver(contents), owner_(owner) {}
+WebsiteMetrics::ActiveTabWebContentsObserver::ActiveTabWebContentsObserver(
+    content::WebContents* contents,
+    WebsiteMetrics* owner)
+    : content::WebContentsObserver(contents), owner_(owner) {}
 
-  ActiveTabWebContentsObserver(const ActiveTabWebContentsObserver&) = delete;
-  ActiveTabWebContentsObserver& operator=(const ActiveTabWebContentsObserver&) =
-      delete;
-
-  ~ActiveTabWebContentsObserver() override = default;
-
-  // content::WebContentsObserver
-  void PrimaryPageChanged(content::Page& page) override {
-    owner_->OnWebContentsUpdated(web_contents());
-  }
-
- private:
-  WebsiteMetrics* owner_;
-};
+void WebsiteMetrics::ActiveTabWebContentsObserver::PrimaryPageChanged(
+    content::Page& page) {
+  owner_->OnWebContentsUpdated(web_contents());
+}
 
 WebsiteMetrics::WebsiteMetrics(Profile* profile)
     : browser_tab_strip_tracker_(this, nullptr) {
