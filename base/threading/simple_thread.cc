0cccac870b991cd03770e3d94157f70e3f1afe12
Peter Kasting <pkasting@chromium.org>
2022-06-21 23:30:09 +0000

    Prevent unsafe narrowing: base/threading
    Bug: 1292951
    Change-Id: I4b6fa2edec1d4372e9dd0589b8e011447ab33bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709036
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016422}

--- base/threading/simple_thread.cc
+++ base/threading/simple_thread.cc
@@ -102,7 +102,7 @@ void DelegateSimpleThread::Run() {
 
 DelegateSimpleThreadPool::DelegateSimpleThreadPool(
     const std::string& name_prefix,
-    int num_threads)
+    size_t num_threads)
     : name_prefix_(name_prefix),
       num_threads_(num_threads),
       dry_(WaitableEvent::ResetPolicy::MANUAL,
@@ -116,7 +116,7 @@ DelegateSimpleThreadPool::~DelegateSimpleThreadPool() {
 
 void DelegateSimpleThreadPool::Start() {
   DCHECK(threads_.empty()) << "Start() called with outstanding threads.";
-  for (int i = 0; i < num_threads_; ++i) {
+  for (size_t i = 0; i < num_threads_; ++i) {
     std::string name(name_prefix_);
     name.push_back('/');
     name.append(NumberToString(i));
@@ -133,7 +133,7 @@ void DelegateSimpleThreadPool::JoinAll() {
   AddWork(nullptr, num_threads_);
 
   // Join and destroy all the worker threads.
-  for (int i = 0; i < num_threads_; ++i) {
+  for (size_t i = 0; i < num_threads_; ++i) {
     threads_[i]->Join();
     delete threads_[i];
   }
@@ -141,9 +141,10 @@ void DelegateSimpleThreadPool::JoinAll() {
   DCHECK(delegates_.empty());
 }
 
-void DelegateSimpleThreadPool::AddWork(Delegate* delegate, int repeat_count) {
+void DelegateSimpleThreadPool::AddWork(Delegate* delegate,
+                                       size_t repeat_count) {
   AutoLock locked(lock_);
-  for (int i = 0; i < repeat_count; ++i)
+  for (size_t i = 0; i < repeat_count; ++i)
     delegates_.push(delegate);
   // If we were empty, signal that we have work now.
   if (!dry_.IsSignaled())
