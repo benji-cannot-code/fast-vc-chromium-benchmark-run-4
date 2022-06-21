875be1aecddc59244845e3d2467d474e93122b7b
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-06-21 16:51:50 +0000

    [DOMStorage] Suppress no frame found message in test
    Bug: 1296582
    Change-Id: Ic14aaaf7a32faef3174e46b2959383dcb62d5293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714599
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016250}

--- third_party/blink/web_tests/http/tests/devtools/network/network-iframe-load-and-delete.js
+++ third_party/blink/web_tests/http/tests/devtools/network/network-iframe-load-and-delete.js
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
   `);
 
+  ProtocolClient.test.suppressRequestErrors = true;
   NetworkTestRunner.recordNetwork();
   ConsoleTestRunner.addConsoleSniffer(step2);
   TestRunner.evaluateInPage('loadIframe()');
