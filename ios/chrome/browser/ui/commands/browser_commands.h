3b4aba4532d085acc5cec835f43c5fc53aa6e52e
Petro Akzhygitov <pakzhygitov@google.com>
2022-06-21 17:46:02 +0000

    Move the showHelpPage command out of the BVC
    – Moved the `showHelpPage` command out of the BVC
    
    Bug: 1329106
    Change-Id: If035b29ee54d9a75fbe79d879d1c9c8b09885b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706881
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016288}

--- ios/chrome/browser/ui/commands/browser_commands.h
+++ ios/chrome/browser/ui/commands/browser_commands.h
@@ -56,9 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Closes all tabs.
 - (void)closeAllTabs;
 
-// Shows the online help page in a tab.
-- (void)showHelpPage;
-
 // Shows the bookmarks manager.
 - (void)showBookmarksManager;
 
