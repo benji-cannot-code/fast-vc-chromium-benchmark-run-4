8564d42f90d958df849011f9058a6f427834087b
Sophie Wen <sophiewen@google.com>
2022-06-21 18:19:38 +0000

    Update shelf hover previews to work with DarkLightMode and a11y.
    This fixes the `--shelf-hover-previews` switch to run without crashing.
    
    Fixed: 1338207
    Change-Id: I43e5f99189a7ed0ecd380abdf0c9b50e2f396e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706196
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016317}

--- ash/shelf/window_preview.cc
+++ ash/shelf/window_preview.cc
@@ -47,7 +47,7 @@ WindowPreview::WindowPreview(aura::Window* window, Delegate* delegate)
   title_ = new views::Label(window->GetTitle());
   close_button_ = new views::ImageButton(base::BindRepeating(
       &WindowPreview::CloseButtonPressed, base::Unretained(this)));
-  close_button_->SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
+  close_button_->SetFocusBehavior(FocusBehavior::NEVER);
 
   AddChildView(preview_container_view_);
   AddChildView(preview_view_);
@@ -136,6 +136,7 @@ const char* WindowPreview::GetClassName() const {
 }
 
 void WindowPreview::OnThemeChanged() {
+  views::View::OnThemeChanged();
   const auto* color_provider = GetColorProvider();
   SkColor background_color =
       color_provider->GetColor(ui::kColorTooltipBackground);
