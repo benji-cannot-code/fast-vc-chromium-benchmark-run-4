9b79e8975e11beb6b05ee7771066484dc48ca380
Chris Lu <thegreenfrog@chromium.org>
2022-06-21 23:35:27 +0000

    [ios] Always pass locationBarDidResignFirstResponder to NTPCoordinator
    In crrev.com/c/3697529, the locationBarDidResignFirstResponder signal
    is only passed to NTPCoordinator only if
    |isNTPActiveForCurrentWebState|, but NTPCoordinator needs to know
    every time since there is just one per Browser. Thus, it needs to
    reset its state even if the user has navigated away.
    
    Fixed: 1336240
    Change-Id: If9c08f852b87b8fd9e1267404edcc9fa47cc8315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716623
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016427}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -3679,9 +3679,8 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   _keyCommandsProvider.canDismissModals = NO;
   [_sideSwipeController setEnabled:YES];
 
-  if (self.isNTPActiveForCurrentWebState) {
-    [_ntpCoordinator locationBarDidResignFirstResponder];
-  }
+  [_ntpCoordinator locationBarDidResignFirstResponder];
+
   [UIView animateWithDuration:0.3
       animations:^{
         [self.typingShield setAlpha:0.0];
