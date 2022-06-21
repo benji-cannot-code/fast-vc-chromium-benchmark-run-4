9239ac80df7a44794c1c0ceab699a5d8fa29a7f6
Mohamed Mohamed <mmelsayed@google.com>
2022-06-21 17:32:42 +0000

    Introduce Flag class for defining feature flags in Java
    In this cl, we have added a type of Flag, CachedFlag.
    This type of flag will be used to replace the current
    implementation of CachedFlags in the CachedFeatureFlags class.
    
    CachedFlags will now be instantiated as objects where the
    isEnabled function would be called from them.
    
    This cl will enable programmers to specify the type of flag
    that they wish to use. CachedFlag, (and in future CLs: SafeFlags
    and PostNative flags). Hence avoiding confusion for the programmer
    on the type of flag that they wish to code, making code much cleaner
    and simpler to understand.
    
    Bug: 1322153
    Change-Id: I6c6ddf1e5fbaf19c9977e9c02fe139696aa5bdc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687339
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Mohamed Mohamed <mmelsayed@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016270}

---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFlag.java
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.flags;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+
+/**
+ * Flags of this type may be used before native is loaded and return the value read
+ * from native and cached to SharedPreferences in a previous run.
+ * @see {@link CachedFeatureFlags#isEnabled(String, boolean)}
+ */
+public class CachedFlag extends Flag {
+    private final boolean mDefaultValue;
+
+    public CachedFlag(String name, boolean defaultValue) {
+        super(name);
+        mDefaultValue = defaultValue;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return CachedFeatureFlags.isEnabled(mFeatureName, mDefaultValue);
+    }
+
+    @VisibleForTesting
+    public void setForTesting(String name, @Nullable Boolean value) {
+        CachedFeatureFlags.setForTesting(name, value);
+    }
+
+    public void cacheFeature() {
+        CachedFeatureFlags.cacheFeature(mFeatureName);
+    }
+}
---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/Flag.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.flags;
+
+import java.util.HashSet;
+
+/**
+ * Defines a feature flag for use in Java.
+ *
+ * Duplicate flag definitions are not permitted, so only a single
+ * instance can be created with a given feature name
+ */
+public abstract class Flag {
+    private static HashSet<String> sFlagsCreated = new HashSet<>();
+    protected final String mFeatureName;
+
+    Flag(String name) {
+        assert !sFlagsCreated.contains(name);
+        mFeatureName = name;
+        sFlagsCreated.add(mFeatureName);
+    }
+
+    /**
+     * Checks if a feature flag is enabled.
+     * @return whether the feature should be considered enabled.
+     */
+    public abstract boolean isEnabled();
+
+    static void resetFlagsForTesting() {
+        sFlagsCreated.clear();
+    }
+}
