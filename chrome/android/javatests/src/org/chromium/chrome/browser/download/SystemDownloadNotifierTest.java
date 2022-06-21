031f8e18b15bd29cf1e021a3cc5537e0fa45377f
David Trainor <dtrainor@chromium.org>
2022-06-21 06:46:15 +0000

    Migrate download tests to unit test target
    Bug:1331876,1331877,1332150,1331874
    
    Change-Id: I5ed30545d44c4d3a4ea85f9bc2fdad1f6481b99c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709091
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016068}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/SystemDownloadNotifierTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/SystemDownloadNotifierTest.java
@@ -14,16 +14,16 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
-import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.components.browser_ui.notifications.ThrottlingNotificationScheduler;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.OfflineItemSchedule;
@@ -38,11 +38,11 @@ import java.util.UUID;
 @RunWith(BaseJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
 public class SystemDownloadNotifierTest {
-    @ClassRule
-    public static final ChromeBrowserTestRule sBrowserTestRule = new ChromeBrowserTestRule();
-
     private final SystemDownloadNotifier mSystemDownloadNotifier = new SystemDownloadNotifier();
     private MockDownloadNotificationService mMockDownloadNotificationService;
+
+    @Mock
+    DownloadManagerService mDownloadManagerService;
 
     @BeforeClass
     public static void beforeClass() {
@@ -51,7 +51,9 @@ public class SystemDownloadNotifierTest {
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         TestThreadUtils.runOnUiThreadBlocking(() -> {
+            DownloadManagerService.setDownloadManagerService(mDownloadManagerService);
             mMockDownloadNotificationService = new MockDownloadNotificationService();
             mSystemDownloadNotifier.setDownloadNotificationService(
                     mMockDownloadNotificationService);
@@ -61,6 +63,8 @@ public class SystemDownloadNotifierTest {
     @After
     public void tearDown() {
         ThrottlingNotificationScheduler.getInstance().clear();
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> { DownloadManagerService.setDownloadManagerService(null); });
     }
 
     private DownloadInfo getDownloadInfo(ContentId id) {
