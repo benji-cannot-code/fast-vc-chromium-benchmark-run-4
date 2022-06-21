f8110d547c5e1f03c0ebbcc41f79f6bde86bee7e
Jeffrey Young <cowmoo@google.com>
2022-06-21 17:47:41 +0000

    wallpaper: remove usage of kWallpaperWebUI
    Not needed in test since it is on by default.
    
    BUG=b:193788853
    TEST=ash_unittests --gtest_filter="*WallpaperPrefManager*"
    
    Change-Id: I1b000d79cf97b2b9a06e1c2f4c5d751cf99fed4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711093
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016289}

--- ash/wallpaper/wallpaper_pref_manager_unittest.cc
+++ ash/wallpaper/wallpaper_pref_manager_unittest.cc
@@ -231,7 +231,6 @@ TEST_F(WallpaperPrefManagerTest, SetWallpaperInfoLocal) {
 }
 
 TEST_F(WallpaperPrefManagerTest, SetWallpaperInfoSynced) {
-  base::test::ScopedFeatureList scoped_features(features::kWallpaperWebUI);
   profile_helper_->RegisterPrefsForAccount(account_id_1);
 
   WallpaperInfo info = InfoWithType(WallpaperType::kOnline);
@@ -242,9 +241,6 @@ TEST_F(WallpaperPrefManagerTest, SetWallpaperInfoSynced) {
 }
 
 TEST_F(WallpaperPrefManagerTest, SetWallpaperInfoSyncDisabled) {
-  base::test::ScopedFeatureList scoped_features;
-  scoped_features.InitAndEnableFeature(features::kWallpaperWebUI);
-
   profile_helper_->RegisterPrefsForAccount(account_id_1);
   // This needs to be saved before sync is disabled or we can't get a pref
   // service.
