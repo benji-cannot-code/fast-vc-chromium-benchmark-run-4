37e742dced215026c213f42bd6ead0998fd5d24e
Jason Thai <jasontt@chromium.org>
2022-06-22 19:02:15 +0000

    personalization: add metric for active user's rgbkbd color
    This metric, instead of recording the transient choice that the user
    makes when selecting the rgbkbd color, records the actual color that is
    saved to the user's pref and emitted once for every UMA upload.
    
    BUG=b/235999492
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I0bc0d3f28fbbac7cf32bb48203be54b97b55ca07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708708
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016821}

---
+++ chrome/browser/ash/web_applications/personalization_app/keyboard_backlight_color_metrics_provider.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_WEB_APPLICATIONS_PERSONALIZATION_APP_KEYBOARD_BACKLIGHT_COLOR_METRICS_PROVIDER_H_
+#define CHROME_BROWSER_ASH_WEB_APPLICATIONS_PERSONALIZATION_APP_KEYBOARD_BACKLIGHT_COLOR_METRICS_PROVIDER_H_
+
+#include "components/metrics/metrics_provider.h"
+
+class KeyboardBacklightColorMetricsProvider : public metrics::MetricsProvider {
+ public:
+  KeyboardBacklightColorMetricsProvider();
+
+  KeyboardBacklightColorMetricsProvider(
+      const KeyboardBacklightColorMetricsProvider&) = delete;
+  KeyboardBacklightColorMetricsProvider& operator=(
+      const KeyboardBacklightColorMetricsProvider&) = delete;
+
+  ~KeyboardBacklightColorMetricsProvider() override;
+
+  // metrics::MetricsProvider:
+  void ProvideCurrentSessionData(
+      metrics::ChromeUserMetricsExtension* uma_proto_unused) override;
+};
+
+#endif  // CHROME_BROWSER_ASH_WEB_APPLICATIONS_PERSONALIZATION_APP_KEYBOARD_BACKLIGHT_COLOR_METRICS_PROVIDER_H_
