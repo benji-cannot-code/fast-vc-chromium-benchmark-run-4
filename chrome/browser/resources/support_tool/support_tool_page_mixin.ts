36e23c800a9fcd92230403fad812819fc92dd4ed
Irem Uguz <iremuguz@google.com>
2022-06-22 11:54:32 +0000

    support-tool: Fix the issues about UI element focus and labels
    Fix the focus of the elements and make sure the right component gets the
    focus in the Support Tool flow to ensure a smooth experience for the
    screen-reader users. Ensure that all the input fields have
    programmatically attached labels.
    
    Bug: b:236099230
    Test: Manual testing with Chrome Vox screen-reader
    Change-Id: I94022c28c3f9150f07fd5aff3d21487a8b5dc1c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714132
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016656}

---
+++ chrome/browser/resources/support_tool/support_tool_page_mixin.ts
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Mixin to be used by Polymer elements that define Support Tool
+ * pages.
+ */
+
+import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export const SupportToolPageMixin = dedupingMixin(
+    <T extends Constructor<PolymerElement>>(superClass: T): T&
+    Constructor<SupportToolPageMixinInterface> => {
+      class SupportToolPageMixin extends superClass implements
+          SupportToolPageMixinInterface {
+        $$<E extends Element = Element>(query: string) {
+          return this.shadowRoot!.querySelector<E>(query);
+        }
+
+        // Puts the focus on the first header (h1) element of the page. Every
+        // Support Tool page Polymer element that implements this mixin should
+        // have a focusable header to describe the step.
+        ensureFocusOnPageHeader() {
+          this.$$<HTMLElement>('h1')!.focus();
+        }
+      }
+      return SupportToolPageMixin;
+    });
+
+export interface SupportToolPageMixinInterface {
+  $$<E extends Element = Element>(query: string): E|null;
+
+  ensureFocusOnPageHeader(): void;
+}
