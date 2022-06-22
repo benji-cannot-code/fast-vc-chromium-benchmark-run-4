b850d6cd7eb4a4953e4ba6745ea275e0e5394ac6
Andrew Paseltiner <apaseltiner@chromium.org>
2022-06-22 16:18:31 +0000

    Add WPT for source and trigger debug keys
    Change-Id: I93be46ac83456be9395bded357422b3c06fdbed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718360
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016743}

--- third_party/blink/web_tests/wpt_internal/attribution-reporting/resources/helpers.js
+++ third_party/blink/web_tests/wpt_internal/attribution-reporting/resources/helpers.js
@@ -3,12 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Helper functions for attribution reporting API tests.
  */
 
-attribution_reporting_promise_test = f => promise_test(async t => {
-  t.add_cleanup(() => internals.resetAttributionReporting());
-  t.add_cleanup(() => resetEventLevelReports());
-  t.add_cleanup(() => resetAggregatableReports());
-  return f(t);
-});
+const blankURL = () => new URL('/resources/blank.html', window.location);
+
+const attribution_reporting_promise_test = (f, name) =>
+    promise_test(async t => {
+      t.add_cleanup(() => internals.resetAttributionReporting());
+      t.add_cleanup(() => resetEventLevelReports());
+      t.add_cleanup(() => resetAggregatableReports());
+      return f(t);
+    }, name);
 
 const eventLevelReportsUrl =
     '/.well-known/attribution-reporting/report-event-attribution';
@@ -43,7 +46,7 @@ const encodeForPipe =
  * Registers either a source or trigger.
  */
 const registerAttributionSrc = (header, body, cookie = '') => {
-  const url = new URL('/resources/blank.html', window.location);
+  const url = blankURL();
   // , and ) in header values must be escaped with \
   const attributionHeader =
       `header(${header},${encodeForPipe(JSON.stringify(body))})`;
@@ -77,10 +80,18 @@ const pollEventLevelReports = interval =>
 const pollAggregatableReports = interval =>
     pollAttributionReports(aggregatableReportsUrl, interval);
 
-const validateReportHeaders = headers => {
-  assert_array_equals(headers['content-type'], ['application/json']);
-  assert_array_equals(headers['cache-control'], ['no-cache']);
-  assert_own_property(headers, 'user-agent');
-  assert_not_own_property(headers, 'cookie');
-  assert_not_own_property(headers, 'referer');
-}
+const validateReportHeaders =
+    headers => {
+      assert_array_equals(headers['content-type'], ['application/json']);
+      assert_array_equals(headers['cache-control'], ['no-cache']);
+      assert_own_property(headers, 'user-agent');
+      assert_not_own_property(headers, 'cookie');
+      assert_not_own_property(headers, 'referer');
+    }
+
+const clearCookie = name => {
+  const url = blankURL();
+  const cookieHeader = `header(Set-Cookie,${encodeForPipe(name)};Max-Age=0)`;
+  url.searchParams.set('pipe', cookieHeader);
+  return fetch(url);
+};
