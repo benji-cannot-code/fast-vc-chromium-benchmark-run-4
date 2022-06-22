e42058fe0646e3a6218dfd9d4c0a1e6dbcf97e6d
Joseph Wang <jwx@google.com>
2022-06-22 00:33:25 +0000

    [fuchsia] Support multiple navigations in Cast Streaming-enabled Frames
    This CL adds support for multiple navigations in the same renderer using
    cast streaming, avoiding crashing. This is necessary for pixel tests
    which navigate multiple times before the test starts.
    
    This also fixes the ReceiverSessionClient API to match the new parameter
    type name. CastStreamingReceiver was renamed to DemuxerConnector.
    
    Bug: 1330697
    Change-Id: I3dc566b5ca2ba9cd7c1d03f173fe0cfad74cfd48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714879
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016452}

--- fuchsia_web/webengine/browser/receiver_session_client.cc
+++ fuchsia_web/webengine/browser/receiver_session_client.cc
@@ -21,7 +21,7 @@ ReceiverSessionClient::ReceiverSessionClient(
 
 ReceiverSessionClient::~ReceiverSessionClient() = default;
 
-void ReceiverSessionClient::SetCastStreamingReceiver(
+void ReceiverSessionClient::SetDemuxerConnector(
     mojo::AssociatedRemote<cast_streaming::mojom::DemuxerConnector>
         demuxer_connector) {
   DCHECK(message_port_request_);
@@ -49,4 +49,8 @@ void ReceiverSessionClient::SetCastStreamingReceiver(
           },
           std::move(message_port_request_)));
   receiver_session_->StartStreamingAsync(std::move(demuxer_connector));
+}
+
+bool ReceiverSessionClient::HasReceiverSession() {
+  return !!receiver_session_;
 }
