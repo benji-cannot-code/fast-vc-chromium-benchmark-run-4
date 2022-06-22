0048204799030a0d5095ef5f487a97ddac221900
Ben Reich <benreich@chromium.org>
2022-06-22 23:48:26 +0000

    [FilesTrash] Wire up the trash IOTask's to the startIOTask
    The trash IOTask's will be the way forward (in SWA world) to perform
    trashing operations. This sets up the appropriate paths to enable
    startIOTask to take the IOTaskType's associated with trashing and also
    ensures the progress center doesn't respond to the TRASH operation as it
    defers to a toast instead.
    
    Bug: b:236806824
    Test: browser_tests --gtest_filter=*trash*
    Test: Ensure chrome builds, follow up CLs will migrate tests
    Change-Id: Ifc18b9be14efd4c3f2b0d4209cc2772b7d9283f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715487
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016924}

--- chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
@@ -37,11 +37,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/drive/file_system_util.h"
 #include "chrome/browser/ash/file_manager/copy_or_move_io_task.h"
 #include "chrome/browser/ash/file_manager/delete_io_task.h"
+#include "chrome/browser/ash/file_manager/empty_trash_io_task.h"
 #include "chrome/browser/ash/file_manager/extract_io_task.h"
 #include "chrome/browser/ash/file_manager/file_manager_copy_or_move_hook_delegate.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/ash/file_manager/io_task.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
+#include "chrome/browser/ash/file_manager/restore_io_task.h"
 #include "chrome/browser/ash/file_manager/trash_io_task.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/ash/file_manager/zip_io_task.h"
@@ -403,10 +405,14 @@ absl::optional<file_manager::io_task::OperationType> IOTaskTypeToChromeEnum(
       return file_manager::io_task::OperationType::kCopy;
     case api::file_manager_private::IO_TASK_TYPE_DELETE:
       return file_manager::io_task::OperationType::kDelete;
+    case api::file_manager_private::IO_TASK_TYPE_EMPTY_TRASH:
+      return file_manager::io_task::OperationType::kEmptyTrash;
     case api::file_manager_private::IO_TASK_TYPE_EXTRACT:
       return file_manager::io_task::OperationType::kExtract;
     case api::file_manager_private::IO_TASK_TYPE_MOVE:
       return file_manager::io_task::OperationType::kMove;
+    case api::file_manager_private::IO_TASK_TYPE_RESTORE:
+      return file_manager::io_task::OperationType::kRestore;
     case api::file_manager_private::IO_TASK_TYPE_TRASH:
       return file_manager::io_task::OperationType::kTrash;
     case api::file_manager_private::IO_TASK_TYPE_ZIP:
@@ -1620,6 +1626,14 @@ FileManagerPrivateInternalStartIOTaskFunction::Run() {
     }
   }
 
+  std::vector<std::string> restore_paths;
+  if (base::FeatureList::IsEnabled(chromeos::features::kFilesTrash) &&
+      params->params.restore_paths) {
+    for (const auto& path : *params->params.restore_paths) {
+      restore_paths.emplace_back(path);
+    }
+  }
+
   std::unique_ptr<file_manager::io_task::IOTask> task;
   switch (type.value()) {
     case file_manager::io_task::OperationType::kCopy:
@@ -1637,6 +1651,29 @@ FileManagerPrivateInternalStartIOTaskFunction::Run() {
       task = std::make_unique<file_manager::io_task::DeleteIOTask>(
           std::move(source_urls), file_system_context);
       break;
+    case file_manager::io_task::OperationType::kEmptyTrash:
+      if (base::FeatureList::IsEnabled(chromeos::features::kFilesTrash)) {
+        task = std::make_unique<file_manager::io_task::EmptyTrashIOTask>(
+            blink::StorageKey(render_frame_host()->GetLastCommittedOrigin()),
+            profile, file_system_context,
+            /*base_path=*/base::FilePath());
+        break;
+      } else {
+        return RespondNow(Error("Invalid operation type"));
+      }
+    case file_manager::io_task::OperationType::kRestore:
+      if (base::FeatureList::IsEnabled(chromeos::features::kFilesTrash)) {
+        if (source_urls.size() != restore_paths.size()) {
+          return RespondNow(Error("Invalid number of restore paths"));
+        }
+        task = std::make_unique<file_manager::io_task::RestoreIOTask>(
+            std::move(source_urls), std::move(restore_paths), profile,
+            file_system_context,
+            /*base_path=*/base::FilePath());
+        break;
+      } else {
+        return RespondNow(Error("Invalid operation type"));
+      }
     case file_manager::io_task::OperationType::kTrash:
       if (base::FeatureList::IsEnabled(chromeos::features::kFilesTrash)) {
         task = std::make_unique<file_manager::io_task::TrashIOTask>(
