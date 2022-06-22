9d6bbc36d1bafcad51b824283ffe7d40a8454195
Shakti Sahu <shaktisahu@chromium.org>
2022-06-22 05:05:25 +0000

    Enable AddToHomescreenIPH by default
    This CL
    - Enables the feature flag by default
    - Enables message variant by default
    - Removes field trial config
    
    Bug: 1337501
    Change-Id: I6795275ab4bdf078889c0905000448d23471e628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3712252
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016551}

--- chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenIPHController.java
+++ chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenIPHController.java
@@ -104,7 +104,7 @@ public class AddToHomescreenIPHController {
             showTextBubbleIPH();
         } else if (ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
                            ChromeFeatureList.ADD_TO_HOMESCREEN_IPH, VARIATION_KEY_USE_MESSAGE,
-                           false)) {
+                           true)) {
             showMessageIPH(tab);
         }
     }
