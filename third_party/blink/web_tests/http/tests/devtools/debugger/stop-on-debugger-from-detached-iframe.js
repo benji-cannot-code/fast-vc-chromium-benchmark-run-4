9d857d29e47612d61a0d851c2fbfa50ea973f19f
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-06-22 14:10:04 +0000

    [DOMStorage] Fix stop-on-debugger-from-detached-iframe.js web test
    Bug: 1296582
    Change-Id: Ib8f78a6c4e11d9b4bb96e5eb5ec1febbe2ad9004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706599
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016699}

--- third_party/blink/web_tests/http/tests/devtools/debugger/stop-on-debugger-from-detached-iframe.js
+++ third_party/blink/web_tests/http/tests/devtools/debugger/stop-on-debugger-from-detached-iframe.js
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     w.eval('debugger;window');
   `;
 
+  ProtocolClient.test.suppressRequestErrors = true;
   SourcesTestRunner.runDebuggerTestSuite([function (next) {
     SourcesTestRunner.waitUntilPaused(paused);
     TestRunner.evaluateInPage(script);
