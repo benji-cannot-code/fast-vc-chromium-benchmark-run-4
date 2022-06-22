f14678a6a5175932e9783868716d29ef477db0fc
Gang Wu <gangwu@chromium.org>
2022-06-22 19:00:22 +0000

    Reland "Deprecate IS_ONLINE_DETECTION_DISABLED"
    This is a reland of commit 22b0b58d2281b2ef8bcaf842a85411aa5b99311c
    
    This reland changed
    ChromeFeatureList.CONTEXTUAL_SEARCH_DISABLE_ONLINE_DETECTION from
    @CommandLineFlags.Add to @EnableFeatures.
    
    Original change's description:
    > Deprecate IS_ONLINE_DETECTION_DISABLED
    >
    > Deprecate IS_ONLINE_DETECTION_DISABLED, and convert it to
    > ChromeFeatureList.CONTEXTUAL_SEARCH_DISABLE_ONLINE_DETECTION.
    >
    > Bug:1322692
    >
    > Change-Id: I48d9eb49d16bf3d0421401fee07e68813faeb49b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714876
    > Reviewed-by: Donn Denman <donnd@chromium.org>
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1016373}
    
    Bug: 1322692
    Change-Id: I3bd3dd4a7bde2b4ae6cfe3c439a2694049217c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717189
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016819}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -211,6 +211,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kContextMenuPopupStyle,
     &kContextualSearchDebug,
     &kContextualSearchDelayedIntelligence,
+    &kContextualSearchDisableOnlineDetection,
     &kContextualSearchForceCaption,
     &kContextualSearchMlTapSuppression,
     &kContextualSearchTapDisableOverride,
@@ -588,6 +589,10 @@ const base::Feature kContextualSearchDebug{"ContextualSearchDebug",
 
 const base::Feature kContextualSearchDelayedIntelligence{
     "ContextualSearchDelayedIntelligence", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kContextualSearchDisableOnlineDetection{
+    "ContextualSearchDisableOnlineDetection",
+    base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kContextualSearchForceCaption{
     "ContextualSearchForceCaption", base::FEATURE_DISABLED_BY_DEFAULT};
