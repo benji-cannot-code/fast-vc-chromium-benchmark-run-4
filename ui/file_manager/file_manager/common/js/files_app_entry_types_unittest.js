db65d16fb578d00b3c7692b3653282cc6284d202
Luciano Pacheco <lucmult@chromium.org>
2022-06-22 05:41:04 +0000

    Files app: Rename .m.js to .js
    Rename all the remaining JS files with the extension .m.js to .js.
    Most of the files are unittests but there some main, background, etc
    too.
    
    Bug: b:228114096
    Change-Id: Id35eb54478c0b6a1a890eeea4dd730ce70812851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714572
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016559}

--- ui/file_manager/file_manager/common/js/files_app_entry_types_unittest.js
+++ ui/file_manager/file_manager/common/js/files_app_entry_types_unittest.js
@@ -423,7 +423,7 @@ export function testVolumeEntryCreateReaderUnresolved(testReportCallback) {
     label: 'Fake Filesystem label',
     volumeType: VolumeManagerCommon.VolumeType.DOWNLOADS,
     resolveDisplayRoot: (successCallback, errorCallback) => {
-      // Do nothing here.
+        // Do nothing here.
     },
   });
 
