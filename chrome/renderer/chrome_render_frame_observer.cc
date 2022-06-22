975cecaaecaa24867f1d1d6606d068136f2d3097
Abigail Klein <abigailbklein@google.com>
2022-06-22 22:51:22 +0000

    [Read Anything] Introduce the ReadAnythingAppController.
    ReadAnythingAppController is a C++ class in the renderer process.
    It serves two purposes:
    1. Communicate with ReadAnythingPageHandler (C++) via mojom.
    2. Communicate with ReadAnythingApp (ts) via gin bindings.
    The ReadAnythingAppController unserializes the AXTreeSnapshot and
    exposes methods to the ts resource (ReadAnythingApp) for accessing
    information about the AXTree.
    
    This CL is first in a series of CLs to unserialize the AXTreeUpdate
    in the renderer process in C++ and use gin bindings to enable the ts
    resource to query the AXTree. The reasoning for this is 3-fold:
    1. The AXTree::Unserialize method is complex (it performs tree
       manipulation) and is written in C++.
    2. The AXTree::Unserialize method is a performance bottleneck and so
       when possible it is best to avoid using it in the main (browser)
       process.
    3. The feature will require manipulation of some values in the AXTree
       which must be done in an unprivileged process.
    
    More information here: https://docs.google.com/document/d/1CV2IX56z5FuUCiuNsBcnKyZ5_0Zd0_LZ4dBora93utc/edit#heading=h.30lobn7jy68d
    
    Bug: 1266555
    Change-Id: I15cac25e2955817d1a1c79e81e186d6f4fed9322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3710123
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016884}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -65,7 +65,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if !BUILDFLAG(IS_ANDROID)
+#include "chrome/renderer/accessibility/read_anything_app_controller.h"
 #include "chrome/renderer/searchbox/searchbox_extension.h"
+#include "ui/accessibility/accessibility_features.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
@@ -316,6 +318,16 @@ void ChromeRenderFrameObserver::DidClearWindowObject() {
       *base::CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kInstantProcess))
     SearchBoxExtension::Install(render_frame()->GetWebFrame());
+
+  // Install ReadAnythingAppController on render frames belonging to a WebUIs.
+  // ReadAnythingAppController installs v8 bindings in the chrome.readAnything
+  // namespace which are consumed by read_anything/app.ts, the resource of the
+  // Read Anything WebUI.
+  if (features::IsReadAnythingEnabled() &&
+      render_frame()->GetEnabledBindings() &
+          content::kWebUIBindingsPolicyMask) {
+    ReadAnythingAppController::Install(render_frame());
+  }
 #endif  // !BUILDFLAG(IS_ANDROID)
 }
 
