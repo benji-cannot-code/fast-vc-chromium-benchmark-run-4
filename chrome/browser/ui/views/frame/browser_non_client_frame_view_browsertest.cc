53e3ac56f9904c661a2069c69711450a07ca2eb7
Elaine Chien <elainechien@chromium.org>
2022-06-22 00:55:24 +0000

    Reland "Replace BrowserNonClientFrameView frame colors with ColorProvider::GetColor"
    This CL was reverted in crrev.com/v/3712250 for failing
    BrowserNonClientFrameViewBrowserTest.BrowserFrameColorThemed
    on linux-chromeos-chrome. This test failed consistently after merge
    but the failure is not reproducable locally or through the trybot
    on the CL (see patchset 1). This CL changes
    BrowserNonClientFrameViewBrowserTest.BrowserFrameColorThemed to use
    the ColorProvider as this should not be a real color difference.
    Original test only made sure color returned by ThemeProvider was the
    same as colors actually set. This keeps that behavior. Remaining
    tests should cover other theme variations.
    
    This is a reland of commit 67593592dd0cf0a625eff784d7c3198fa7aa7745
    
    Original change's description:
    > Replace BrowserNonClientFrameView frame colors with ColorProvider::GetColor
    >
    > This CL replaces ThemeProvider::GetColor with ColorProvider::GetColor
    > for the frame colors in BrowserNonClientFrameView. This CL modifies
    > the WebAppOpaqueBrowserFrameViewTest to ensure that on Linux the color provider uses the default theme rather than the gtk theme. When the PWA
    > is being instantiated there is a period of time where the theme profile
    > hasn't been set yet in BrowserFrame::AddedToWidget, and we return a
    > GTK color rather than the default color through ColorProvider::GetColor calls. This is because
    > ThemeServiceAuraLinux::ShouldUseSystemThemeForProfile returns true with
    > a null profile. We need to manually set the UseSystemThemeCallback to always return false.
    > WebAppOpaqueBrowserFrameViewTest.OriginTextVisibility is
    > sensitive to this difference, so this modification ensures that
    > we maintain the original default theme behavior. (Although see comment
    > and bug documenting the flaws of this test.) Since the PWA would not be
    > visible before AddedToWidget is called and the profile is set
    > correctly, this change does not affect the origin text animation in
    > production.
    >
    > Bug: 1334664
    > Change-Id: I9001c0e26f717f4b702e294b6b7f3137b5fef443
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711320
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Elaine Chien <elainechien@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1015385}
    
    Bug: 1334664
    Change-Id: I99becb2c57648bf1fd9ea0d36329c71860b939d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3712588
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016462}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/fullscreen.mojom.h"
 #include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
 #include "ui/base/theme_provider.h"
+#include "ui/color/color_id.h"
+#include "ui/color/color_provider.h"
 
 class BrowserNonClientFrameViewBrowserTest
     : public extensions::ExtensionBrowserTest {
@@ -108,11 +110,11 @@ IN_PROC_BROWSER_TEST_F(BrowserNonClientFrameViewBrowserTest,
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   const BrowserNonClientFrameView* frame_view =
       browser_view->frame()->GetFrameView();
-  const ui::ThemeProvider* theme_provider = frame_view->GetThemeProvider();
+  const ui::ColorProvider* color_provider = frame_view->GetColorProvider();
   const SkColor expected_active_color =
-      theme_provider->GetColor(ThemeProperties::COLOR_FRAME_ACTIVE);
+      color_provider->GetColor(ui::kColorFrameActive);
   const SkColor expected_inactive_color =
-      theme_provider->GetColor(ThemeProperties::COLOR_FRAME_INACTIVE);
+      color_provider->GetColor(ui::kColorFrameInactive);
 
   EXPECT_EQ(expected_active_color,
             frame_view->GetFrameColor(BrowserFrameActiveState::kActive));
