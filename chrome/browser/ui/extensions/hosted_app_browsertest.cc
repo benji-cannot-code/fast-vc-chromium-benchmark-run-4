1c7dce84239ed515c142033944ea3d28f372cd08
Eric Willigers <ericwilligers@chromium.org>
2022-06-22 03:00:44 +0000

    Revert "HostedOrWebAppTest waits for browser activation"
    This reverts commit 292f023b331f60c170cff7ee9248e0ca32fc9972.
    
    Reason for revert: browser_tests on Mac-11 failing
    
    Bug: 1338346
    
    Original change's description:
    > HostedOrWebAppTest waits for browser activation
    >
    > LaunchAppBrowser uses BrowserChangeObserver to identify the newly
    > created browser.
    >
    > We use BrowserActivationWaiter to wait for the app browser to be
    > activated.
    >
    > The tests HostedOrWebAppTest.CtrlClickLink are now enabled for Linux,
    > including Wayland.
    >
    > Bug: 1190448
    > Change-Id: I676530a9b8d52fdecd1821a9501304a9237ad5c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716134
    > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1016351}
    
    Bug: 1190448
    Change-Id: I5c43d5ef9f20cad5d4db4ed26c430f7f13acc904
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715479
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016517}

--- chrome/browser/ui/extensions/hosted_app_browsertest.cc
+++ chrome/browser/ui/extensions/hosted_app_browsertest.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/sessions/core/tab_restore_service.h"
 #include "components/webapps/browser/installable/installable_metrics.h"
@@ -222,7 +221,6 @@ class HostedOrWebAppTest : public extensions::ExtensionBrowserTest,
 
       // Launch app in a window.
       app_browser_ = web_app::LaunchWebAppBrowser(profile(), app_id_);
-      ui_test_utils::BrowserActivationWaiter(app_browser_).WaitForActivation();
     }
 
     ASSERT_FALSE(app_id_.empty());
@@ -376,7 +374,13 @@ IN_PROC_BROWSER_TEST_P(HostedOrWebAppTest, DISABLED_OpenLinkInNewTab) {
 }
 
 // Tests that Ctrl + Clicking a link opens a foreground tab.
-IN_PROC_BROWSER_TEST_P(HostedOrWebAppTest, CtrlClickLink) {
+// TODO(crbug.com/1190448): Flaky on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_CtrlClickLink DISABLED_CtrlClickLink
+#else
+#define MAYBE_CtrlClickLink CtrlClickLink
+#endif
+IN_PROC_BROWSER_TEST_P(HostedOrWebAppTest, MAYBE_CtrlClickLink) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Set up an app which covers app.com URLs.
