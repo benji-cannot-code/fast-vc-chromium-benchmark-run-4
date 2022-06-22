e7e19f33fd7dbafd49e298a2901f6c71f617ca07
Byoungchan Lee <daniel.l@hpcnt.com>
2022-06-22 01:08:21 +0000

    Move libevent into third_party
    A few years ago, libevent was moved from third_party to base/third_party
    for two reasons;  GN is using Chromium base, and libevent is not used
    outside Chromium base.
    
    Unfortunately, neither is true today; WebRTC's rtc_base also uses
    libevent, and GN doesn't use libevent. So it is better to move libevent
    into third_party.
    
    Bug: 1335194
    Change-Id: I771185474f10b1a2137d15773dcec7c4c28f43d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695888
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel.L (Byoungchan) Lee <daniel.l@hpcnt.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016473}

--- base/message_loop/message_pump_libevent.cc
+++ base/message_loop/message_pump_libevent.cc
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "base/posix/eintr_wrapper.h"
-#include "base/third_party/libevent/event.h"
 #include "base/time/time.h"
 #include "base/trace_event/base_tracing.h"
 #include "build/build_config.h"
+#include "third_party/libevent/event.h"
 
 // Lifecycle of struct event
 // Libevent uses two main data structures:
