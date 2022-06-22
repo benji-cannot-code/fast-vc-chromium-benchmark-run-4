380b42182489ddb084cacfc0ab754fbc57ec4742
manukh <manukh@chromium.org>
2022-06-22 18:37:03 +0000

    [omnibox][post-ac] Clean up ZSP callbacks and `done_`.
    The Zero Service Provider is the only provider to support prefetch
    requests. Currently, its prefetch and non-prefetch requests share the
    `done_` bool state. In contrast, the `AutocompleteController` only uses
    its `done_` to track non-prefetch requests. This leads to
    inconsistencies where `AC::done_` is true while `ZPS::done_` is false.
    
    In another CL, I'll be adding a DCHECK to make sure the AC and
    providers' done's are consistent. In preparation for that CL, this CL
    updates ZPS to only use `done_` for non-prefetch requests. It'll instead
    use `prefetching_done_` for tracking prefetch requests.
    
    This CL also removes the `start_callback` called synchronously as 1)
    synchronous calls are easier to track than synchronous callbacks, and 2)
    it avoids the need for an intermediate 'loader created but not sent'
    state in ZPS.
    
    No behavior change.
    
    Change-Id: Idd802c6a3f9c78f65dc54d566789214aa36f1d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707505
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016807}

--- components/omnibox/browser/remote_suggestions_service_unittest.cc
+++ components/omnibox/browser/remote_suggestions_service_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/test/bind.h"
 #include "base/test/test_mock_time_task_runner.h"
-#include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/variations/scoped_variations_ids_provider.h"
 #include "net/base/load_flags.h"
@@ -37,8 +36,6 @@ class RemoteSuggestionsServiceTest : public testing::Test {
 
   void RunAndWait() { mock_task_runner_->FastForwardUntilNoTasksRemain(); }
 
-  void OnRequestStart(std::unique_ptr<network::SimpleURLLoader> loader) {}
-
   void OnRequestComplete(const network::SimpleURLLoader* source,
                          std::unique_ptr<std::string> response_body) {}
 
@@ -60,10 +57,8 @@ TEST_F(RemoteSuggestionsServiceTest, EnsureAttachCookies) {
   TemplateURLService template_url_service(nullptr, 0);
   TemplateURLRef::SearchTermsArgs search_terms_args;
   search_terms_args.current_page_url = "https://www.google.com/";
-  service.CreateSuggestionsRequest(
+  service.StartSuggestionsRequest(
       search_terms_args, &template_url_service,
-      base::BindOnce(&RemoteSuggestionsServiceTest::OnRequestStart,
-                     base::Unretained(this)),
       base::BindOnce(&RemoteSuggestionsServiceTest::OnRequestComplete,
                      base::Unretained(this)));
 
@@ -88,10 +83,8 @@ TEST_F(RemoteSuggestionsServiceTest, EnsureBypassCache) {
   TemplateURLRef::SearchTermsArgs search_terms_args;
   search_terms_args.current_page_url = "https://www.google.com/";
   search_terms_args.bypass_cache = true;
-  service.CreateSuggestionsRequest(
+  service.StartSuggestionsRequest(
       search_terms_args, &template_url_service,
-      base::BindOnce(&RemoteSuggestionsServiceTest::OnRequestStart,
-                     base::Unretained(this)),
       base::BindOnce(&RemoteSuggestionsServiceTest::OnRequestComplete,
                      base::Unretained(this)));
 
