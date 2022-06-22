40ef06af62682e4a1e8776c64162e43392a3919d
Wamia Said <wamia@google.com>
2022-06-22 22:47:44 +0000

    Add coupon card chip on tab switcher UI
    This CL provides the frontend for a coupon annotation on the tab
    switcher UI. These annotations will appear when the COMMERCE_COUPONS
    flag is enabled and there is a coupon available for the product being
    shown in a particular tab. Part of the TabGridViewBinder is also
    refactored in order to allow for code reuse in fetching a Price Drop
    so that coupon annotations can appear on the tab switcher when price
    drop annotations are not applicable for a particular tab. Lastly, a
    skeleton for CouponPersistedTabData is created so that a minimal mock
    for the backend can be provided for testing purposes.
    
    
    Bug: 1335107
    Change-Id: Ie9bb3c8b3c5b7b3d276afbd737c47c81eacedb77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705782
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Wamia Said <wamia@google.com>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016877}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/CouponUtilities.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+import org.chromium.chrome.browser.flags.CachedFeatureFlags;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/**
+ * Utility class for coupons in Tab UI.
+ */
+public class CouponUtilities {
+    /**
+     * @return Whether commerce coupons feature flag is enabled.
+     */
+    public static boolean isCouponsOnTabsEnabled() {
+        return CachedFeatureFlags.isEnabled(ChromeFeatureList.COMMERCE_COUPONS);
+    }
+}
--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/PersistedTabDataConfiguration.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/PersistedTabDataConfiguration.java
@@ -21,6 +21,7 @@ public enum PersistedTabDataConfiguration {
     ENCRYPTED_CRITICAL_PERSISTED_TAB_DATA("ECPTDFB"),
     MOCK_PERSISTED_TAB_DATA("MPTD"),
     ENCRYPTED_MOCK_PERSISTED_TAB_DATA("EMPTD"),
+    COUPON_PERSISTED_TAB_DATA("COPTD"),
     SHOPPING_PERSISTED_TAB_DATA("SPTD"),
     STORE_PERSISTED_TAB_DATA("STPTD"),
     EMPTY_BYTE_BUFFER_TEST_CONFIG("EBBTC"),
@@ -74,6 +75,8 @@ public enum PersistedTabDataConfiguration {
 
     static {
         // TODO(crbug.com/1060187) remove static initializer and initialization lazy
+        sLookup.put(CouponPersistedTabData.class, COUPON_PERSISTED_TAB_DATA);
+        sEncryptedLookup.put(CouponPersistedTabData.class, COUPON_PERSISTED_TAB_DATA);
         sLookup.put(CriticalPersistedTabData.class, CRITICAL_PERSISTED_TAB_DATA);
         sEncryptedLookup.put(CriticalPersistedTabData.class, ENCRYPTED_CRITICAL_PERSISTED_TAB_DATA);
         sLookup.put(MockPersistedTabData.class, MOCK_PERSISTED_TAB_DATA);
@@ -82,6 +85,8 @@ public enum PersistedTabDataConfiguration {
         sEncryptedLookup.put(ShoppingPersistedTabData.class, SHOPPING_PERSISTED_TAB_DATA);
         sLookup.put(StorePersistedTabData.class, STORE_PERSISTED_TAB_DATA);
         sEncryptedLookup.put(StorePersistedTabData.class, STORE_PERSISTED_TAB_DATA);
+
+        COUPON_PERSISTED_TAB_DATA.mStorageFactory = new LevelDBPersistedTabDataStorageFactory();
 
         CRITICAL_PERSISTED_TAB_DATA.mStorageFactory = () -> {
             return getFilePersistedTabDataStorage();
