b229c31dac9152aa052d57ce6c138316b90bc69a
Julie Jeongeun Kim <jkim@igalia.com>
2022-06-23 06:52:51 +0000

    [mparch] Check if the page is primary before generating MHTML
    This CL ensures the page is primary before accessing WebContents
    in order to generate MHTML.
    
    It updates AssureTopLevelActiveFrame helper to a member function
    of PageHandler and DevToolsMHTMLHelper uses it.
    
    Bug: 1324905
    Change-Id: I10c5106abc96ab3ec9b0e03be8faf99c97ba9fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689107
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017045}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -181,6 +181,8 @@ class PageHandler : public DevToolsDomainHandler,
   Response AddCompilationCache(const std::string& url,
                                const Binary& data) override;
 
+  Response AssureTopLevelActiveFrame();
+
  private:
   enum EncodingFormat { PNG, JPEG };
 
