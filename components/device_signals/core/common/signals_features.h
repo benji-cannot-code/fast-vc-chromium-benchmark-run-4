7ed4d0c2854c1211c4a9fa5aacb98ee237772396
Zonghan Xu <xzonghan@google.com>
2022-06-23 00:19:01 +0000

    Move signal features enable check to components/device_signals
    - Moving signals_feature (.cc, .h and unittest) from chrome/browser/enterprise/signals/ to components/device_signals/core/common
    - Change BUILD files to reflect this refactor
    - Change various paths that imports this feature
    
    Bug: b/230332958
    Change-Id: Ie094219305fad86a92ae0ce269128e47bddcc68a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708568
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016941}

--- components/device_signals/core/common/signals_features.h
+++ components/device_signals/core/common/signals_features.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ENTERPRISE_SIGNALS_SIGNALS_FEATURES_H_
-#define CHROME_BROWSER_ENTERPRISE_SIGNALS_SIGNALS_FEATURES_H_
+#ifndef COMPONENTS_DEVICE_SIGNALS_CORE_COMMON_SIGNALS_FEATURES_H_
+#define COMPONENTS_DEVICE_SIGNALS_CORE_COMMON_SIGNALS_FEATURES_H_
 
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
@@ -30,4 +30,4 @@ bool IsNewFunctionEnabled(NewEvFunction new_ev_function);
 
 }  // namespace enterprise_signals::features
 
-#endif  // CHROME_BROWSER_ENTERPRISE_SIGNALS_SIGNALS_FEATURES_H_
+#endif  // COMPONENTS_DEVICE_SIGNALS_CORE_COMMON_SIGNALS_FEATURES_H_
