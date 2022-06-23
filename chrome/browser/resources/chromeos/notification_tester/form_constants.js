9d170cce1a25a78bee87fd3ffb6f84a0365dd543
Arvind Ganesh <ganesharvind@google.com>
2022-06-23 03:09:49 +0000

    Created notification tester front-end w/ Polymer data bindings.
    - Created <select-custom>, similar to a native <select>, but clicking the "Custom" option shows an <input> tag. Demo here: https://drive.google.com/file/d/15SPKMXWQQAGBPRkOt4o1Kpx-XDtB0Upx/view?usp=sharing&resourcekey=0-LM-1lqWBTgofTsZfOJ-PYA
    - Added data bindings from the <select-custom> component to the <notification-tester> component to save configured notification metadata.
    
    
    Bug: 1337375
    Change-Id: Ic26d93291fcfdf7ce2c80ee6d3a3d8decf783f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709212
    Commit-Queue: Arvind Ganesh <ganesharvind@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017001}

---
+++ chrome/browser/resources/chromeos/notification_tester/form_constants.js
@@ -0,0 +1,81 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * @fileoverview Stores the content of the notification tester form select
+ * elements.
+ * @externs
+ */
+
+/*
+ * Stores the content of the notification tester form select
+ * elements. Options correspond to the naming scheme defined in
+ * ui/message_center/public/cpp/notification.h.
+ * @enum {Array}
+ */
+export const FormSelectOptions = {
+  TITLE_OPTIONS: [
+    {displayText: 'Short Sentence (LTR)', value: 'Notification Title'},
+    {displayText: 'Short Sentence (RTL)', value: 'כותרת הודעה'}, {
+      displayText: 'Long Sentence (LTR)',
+      value:
+          'Hamburgers: the cornerstone of any nutritious breakfast. Ch-cheeseburgers'
+    },
+    {
+      displayText: 'Long Sentence (RTL)',
+      value: 'המבורגרים: אבן הפינה של כל ארוחת בוקר מזינה. ציזבורגר'
+    },
+    {
+      displayText: 'Repetitive Characters (LTR)',
+      value: 'sshhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+    },
+    {
+      displayText: 'Repetitive Characters (RTL)',
+      value: 'שששששששששששששששששששששששששששששששששששששששששששששששששששש'
+    }
+  ],
+  MESSAGE_OPTIONS: [
+    {displayText: 'One Sentence (LTR)', value: 'Notification content'},
+    {displayText: 'One Sentence (RTL)', value: 'תוכן הודעה'},
+    {
+      displayText: 'Multiple Sentences (LTR)',
+      value:
+          'This is the notification\'s message.It may be able to stretch over multiple lines, or become visible when the notification is expanded by the user, depending on the notification center that\'s being used.'
+    },
+    {
+      displayText: 'Multiple Sentences (RTL)',
+      value:
+          'זהו המסר של ההודעה. זה עשוי להיות מסוגל למתוח על קווים מרובים, או להיות גלוי, כאשר ההודעה מורחבת על ידי המשתמש, בהתאם להודעה שהמרכז נמצא בשימוש'
+    },
+    {
+      displayText: 'Repetitive Characters (LTR)',
+      value:
+          'sshhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+    },
+    {
+      displayText: 'Repetitive Characters (RTL)',
+      value:
+          'ששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששש'
+    },
+    {displayText: 'Unicode Emojis', value: '🌇😃🍈😆🍜🍻😋⛅⛳😚ඞ'},
+    {displayText: 'Empty', value: ''},
+  ],
+  BADGE_OPTIONS: [
+    {displayText: 'Cat Badge', value: 'cat'},
+    {displayText: 'No Badge', value: 'none'}
+  ],
+  IMAGE_OPTIONS: [
+    {displayText: 'No Image', value: 'none'},
+    {displayText: 'Cat Image (JPEG, 3072x1728)', value: 'cat'},
+  ],
+  ICON_OPTIONS: [
+    {displayText: 'No Icon', value: 'none'},
+    {displayText: 'Normal PNG Icon (256x256)', value: 'normal_png'},
+    {displayText: 'Static GIF Icon (256x256)', value: 'static_gif'},
+    {displayText: 'Animated GIF Icon (256x256)', value: 'animated_gif'},
+    {displayText: 'Tiny Icon (PNG, 32x32)', value: 'tiny_icon'},
+    {displayText: 'Huge Icon (PNG, 3333x5230)', value: 'huge_icon'},
+  ],
+};
