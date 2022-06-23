1b7ac660b39170f3ff448adc378409555d5a8afd
Vivian Pao <vpao@google.com>
2022-06-23 01:58:49 +0000

    [Files App] Change handler verb for App Service app task descriptors
    When we right-click on a set of files in Files App, we show a menu with all the apps that the user can open the selected files in. The apps
    available are represented inconsistently, with the apps from App Service
    having the words “Open with” prepended to the app name and with all
    other apps being represented with only the app name. This change is to
    remove that prepended string from App Service apps to maintain
    consistency with how other apps are shown.
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I811d5734c2b35e93f793c397cc0d2ee29774ada8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715826
    Reviewed-by: Mitch McDermott <mcdermottm@google.com>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016976}

--- chrome/browser/ash/file_manager/app_service_file_tasks_unittest.cc
+++ chrome/browser/ash/file_manager/app_service_file_tasks_unittest.cc
@@ -492,7 +492,7 @@ TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppText) {
   EXPECT_EQ("Baz", tasks[0].task_title);
   EXPECT_TRUE(tasks[0].is_generic_file_handler);
   EXPECT_TRUE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 // File extension matches with bar, but there is a generic * type as well,
@@ -508,7 +508,7 @@ TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppBar) {
   EXPECT_EQ("Baz", tasks[0].task_title);
   EXPECT_TRUE(tasks[0].is_generic_file_handler);
   EXPECT_TRUE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 // Check that we can get web apps and Chrome apps in the same call.
@@ -540,7 +540,7 @@ TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppImage) {
   EXPECT_EQ("Baz", tasks[0].task_title);
   EXPECT_FALSE(tasks[0].is_generic_file_handler);
   EXPECT_FALSE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppWithVerbs) {
@@ -556,7 +556,7 @@ TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppWithVerbs) {
   EXPECT_EQ("plain_text", tasks[0].task_descriptor.action_id);
   EXPECT_FALSE(tasks[0].is_generic_file_handler);
   EXPECT_FALSE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppWithVerbs_Html) {
@@ -571,7 +571,7 @@ TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceChromeAppWithVerbs_Html) {
   EXPECT_EQ("html_handler", tasks[0].task_descriptor.action_id);
   EXPECT_FALSE(tasks[0].is_generic_file_handler);
   EXPECT_FALSE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 TEST_F(AppServiceFileTasksTestEnabled,
@@ -587,7 +587,7 @@ TEST_F(AppServiceFileTasksTestEnabled,
   EXPECT_EQ("any_with_directories", tasks[0].task_descriptor.action_id);
   EXPECT_TRUE(tasks[0].is_generic_file_handler);
   EXPECT_FALSE(tasks[0].is_file_extension_match);
-  EXPECT_EQ(Verb::VERB_OPEN_WITH, tasks[0].task_verb);
+  EXPECT_EQ(Verb::VERB_NONE, tasks[0].task_verb);
 }
 
 TEST_F(AppServiceFileTasksTestEnabled, FindAppServiceExtension) {
