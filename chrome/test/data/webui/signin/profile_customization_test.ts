fa127b4fdb22f86af73a438d8115dccb4908e7ee
Gabriel Oliveira <gabolvr@google.com>
2022-06-23 16:11:23 +0000

    [Signin] Use regex pattern in static title in profile customization test
    Fixed: 1336935
    Change-Id: I9bd13aade15879f2e173609de5a56c422d8abc5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721016
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017191}

--- chrome/test/data/webui/signin/profile_customization_test.ts
+++ chrome/test/data/webui/signin/profile_customization_test.ts
@@ -26,7 +26,7 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
     const AVATAR_URL_2 = 'chrome://theme/IDR_PROFILE_AVATAR_2';
     const WELCOME_TEXT_1 = 'Welcome, Bob';
     const WELCOME_TEXT_2 = 'Hi, Elisa';
-    const STATIC_TITLE = 'Customize your Chromium profile';
+    const STATIC_TITLE_PATTERN = /^Customize your (Chromium|Chrome) profile$/g;
 
     setup(function() {
       loadTimeData.overrideValues({
@@ -100,10 +100,10 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
     test('ProfileInfo', function() {
       const header = app.shadowRoot!.querySelector<HTMLElement>('#header')!;
       // Check initial info.
-      assertEquals(
-          app.$.title.innerText,
-          inDialogDesign ? STATIC_TITLE : WELCOME_TEXT_1);
-      if (!inDialogDesign) {
+      if (inDialogDesign) {
+        assertTrue(app.$.title.innerText.match(STATIC_TITLE_PATTERN) != null);
+      } else {
+        assertEquals(app.$.title.innerText, WELCOME_TEXT_1);
         assertEquals(
             'rgb(0, 255, 0)', getComputedStyle(header).backgroundColor);
       }
@@ -117,10 +117,10 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
         isManaged: true,
         welcomeTitle: WELCOME_TEXT_2,
       });
-      assertEquals(
-          app.$.title.innerText,
-          inDialogDesign ? STATIC_TITLE : WELCOME_TEXT_2);
-      if (!inDialogDesign) {
+      if (inDialogDesign) {
+        assertTrue(app.$.title.innerText.match(STATIC_TITLE_PATTERN) != null);
+      } else {
+        assertEquals(app.$.title.innerText, WELCOME_TEXT_2);
         assertEquals(color2, getComputedStyle(header).backgroundColor);
       }
       checkImageUrl('#avatar', AVATAR_URL_2);
