e3c2d947f54af5774cb8e56599f2fe93f514ca73
Clark DuVall <cduvall@chromium.org>
2022-06-23 22:35:49 +0000

    Avoid scanning for inline scripts in synchronous parsing mode
    There were some speedometer regressions when PrecompileInlineScripts was
    enabled in the testing config, it looks like this was from trying to
    scan every time we set innerHTML.
    
    Bug: 1338447
    Change-Id: I6620c5c0d8ad0d550dce85146b51051c60720387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722256
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017386}

--- third_party/blink/renderer/core/html/parser/html_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser.cc
@@ -1495,8 +1495,10 @@ std::string HTMLDocumentParser::GetPreloadHistogramSuffix() {
 }
 
 void HTMLDocumentParser::ScanInBackground(const String& source) {
+  if (task_runner_state_->IsSynchronous() || !GetDocument()->Url().IsValid())
+    return;
+
   if (ThreadedPreloadScannerEnabled() && preloader_ &&
-      !task_runner_state_->IsSynchronous() && GetDocument()->Url().IsValid() &&
       // TODO(crbug.com/1329535): Support scanning prefetch documents in the
       // background.
       !GetDocument()->IsPrefetchOnly() &&
