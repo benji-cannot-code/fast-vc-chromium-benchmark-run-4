3a2683053a34e883d7c336805fd15d8b23c3c9cb
Eric Willigers <ericwilligers@chromium.org>
2022-06-23 17:27:45 +0000

    CustomTabBarViewBrowserTest: Re-enable test
    We avoid using BrowserActivationWaiter, as it is not needed,
    and causes flakiness on Mac.
    
    Bug: 1338544
    Change-Id: I0d9c4f34836805b90e68f169ad5ccf965de6d9f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3719719
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017232}

--- chrome/browser/ui/views/location_bar/custom_tab_bar_view_browsertest.cc
+++ chrome/browser/ui/views/location_bar/custom_tab_bar_view_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/security_interstitials/content/security_interstitial_controller_client.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
@@ -97,9 +96,7 @@ Browser* OpenPopup(content::WebContents* web_contents, const GURL& target_url) {
   EXPECT_TRUE(content::ExecuteScript(web_contents, script));
   nav_observer.Wait();
 
-  Browser* browser = browser_change_observer.Wait();
-  ui_test_utils::BrowserActivationWaiter(browser).WaitForActivation();
-  return browser;
+  return browser_change_observer.Wait();
 }
 
 // Navigates to |target_url| and waits for navigation to complete.
@@ -261,13 +258,7 @@ IN_PROC_BROWSER_TEST_F(CustomTabBarViewBrowserTest,
   EXPECT_FALSE(custom_tab_bar_);
 }
 
-// TODO(crbug.com/1338068): Re-enable this test
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_IsNotCreatedInPopup DISABLED_IsNotCreatedInPopup
-#else
-#define MAYBE_IsNotCreatedInPopup IsNotCreatedInPopup
-#endif
-IN_PROC_BROWSER_TEST_F(CustomTabBarViewBrowserTest, MAYBE_IsNotCreatedInPopup) {
+IN_PROC_BROWSER_TEST_F(CustomTabBarViewBrowserTest, IsNotCreatedInPopup) {
   Browser* popup = OpenPopup(browser_view_->GetActiveWebContents(),
                              GURL("http://example.com"));
   EXPECT_TRUE(popup);
