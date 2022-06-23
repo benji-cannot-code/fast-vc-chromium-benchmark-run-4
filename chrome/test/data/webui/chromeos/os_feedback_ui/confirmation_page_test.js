71a1a9ae4695a59a551ae55386a43cb7a8ee79a4
YuhanYang <yyhyyh@google.com>
2022-06-23 00:04:04 +0000

    Feedback: Localize ask community title on confirmation page
    Screenshot: https://screenshot.googleplex.com/4SaUdcrPyiXqndH
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I42b5e3324a6f442dfa9460480cf5909ca9c9ab06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718053
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016933}

--- chrome/test/data/webui/chromeos/os_feedback_ui/confirmation_page_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/confirmation_page_test.js
@@ -141,7 +141,7 @@ export function confirmationPageTest() {
     assertEquals(
         'Ask the experts in the Chromebook help forum',
         getElementContent(communityLink, '#subLabel'));
-
+    assertTrue(page.i18nExists('askCommunityLabel'));
     // Verify buttons.
     assertEquals(
         'Send new report', getElementContent(page, '#buttonNewReport'));
