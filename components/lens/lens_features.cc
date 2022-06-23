fc6d49b19c2117c892232ae5912e2e5a9b8361e5
Juan Mojica <juanmojica@google.com>
2022-06-23 22:29:21 +0000

    Image search string should have same provider attribution as region search.
    This ensures that if the region search menu item is using an alternative
    text option that modifies the provider name when using Lens, that it is
    kept consistent with the provider name used by the image search menu
    item text.
    
    Bug: 1339012, b:236862006
    Change-Id: I7ad21c112932dba9d29b771b8d534bf10d63fc24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721799
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017382}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -17,6 +17,9 @@ const base::Feature kLensStandalone{"LensStandalone",
 const base::Feature kLensFullscreenSearch{"LensFullscreenSearch",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::FeatureParam<bool> kUseGoogleAsVisualSearchProvider{
+    &kLensStandalone, "use-google-as-visual-search-provider", false};
+
 const base::FeatureParam<bool> kRegionSearchMacCursorFix{
     &kLensStandalone, "region-search-mac-cursor-fix", true};
 
@@ -25,12 +28,6 @@ const base::FeatureParam<bool> kRegionSearchUseMenuItemAltText1{
 
 const base::FeatureParam<bool> kRegionSearchUseMenuItemAltText2{
     &kLensStandalone, "use-menu-item-alt-text-2", false};
-
-const base::FeatureParam<bool> kRegionSearchUseMenuItemAltText3{
-    &kLensStandalone, "use-menu-item-alt-text-3", false};
-
-const base::FeatureParam<bool> kRegionSearchUseMenuItemAltText4{
-    &kLensStandalone, "use-menu-item-alt-text-4", false};
 
 const base::FeatureParam<bool> kEnableUKMLoggingForRegionSearch{
     &kLensStandalone, "region-search-enable-ukm-logging", true};
@@ -90,14 +87,9 @@ bool UseRegionSearchMenuItemAltText2() {
          kRegionSearchUseMenuItemAltText2.Get();
 }
 
-bool UseRegionSearchMenuItemAltText3() {
+bool UseGoogleAsVisualSearchProvider() {
   return base::FeatureList::IsEnabled(kLensStandalone) &&
-         kRegionSearchUseMenuItemAltText3.Get();
-}
-
-bool UseRegionSearchMenuItemAltText4() {
-  return base::FeatureList::IsEnabled(kLensStandalone) &&
-         kRegionSearchUseMenuItemAltText4.Get();
+         kUseGoogleAsVisualSearchProvider.Get();
 }
 
 bool IsLensFullscreenSearchEnabled() {
