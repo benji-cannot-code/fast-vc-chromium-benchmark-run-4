7c7176b9fc8850ed811811d48f6122a91a6177f9
Juanmi Huertas <juanmihd@google.com>
2022-06-23 03:04:39 +0000

    Changing PaintFlags get/setAlpha to use only floats
    This CL is part of the effort of increasing color precision and
    color gamut support in chrome.
    https://docs.google.com/document/d/1y5qhBuK7sFkXt7c6vcrAJeIofj3h2UQ3btX3Hv1hrz8/edit#heading=h.69111d60ji6h
    particularly
    https://docs.google.com/document/d/1kvl_hwnIG3lAXcSxRndgcqbDGEvGoiD52-VwOuVfveE/edit?resourcekey=0-uvlZUrkCQ-o0H8ohYepsqA
    To do so we are changing viz, cc, and other chrome parts to use
    SkColor4f instead of SkColor.
    
    PaintFlags already stores SkColor4f instead of SkColor, this CL is only
    changing the getAlpha and setAlpha interface, as to make explicit from
    users of the code that alpha is a float now. This CL saves some
    conversions between SkColor4f and SkColor that shouldn't be necessary.
    
    The next steps for PaintFlags would be to also remove the setColor that
    expects SkColor.
    The next steps for some of the callers of PaintFlags::getAlpha/setAlpha
    would be to make them use the float received without having to convert
    it to int.
    
    Bug: 1308932
    Change-Id: I3f0f1e9d134f08c21b5c6de1e2f7b67744ea7fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696922
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@google.com>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016997}

--- cc/paint/scoped_raster_flags.cc
+++ cc/paint/scoped_raster_flags.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/paint/scoped_raster_flags.h"
 
+#include <utility>
 #include "cc/paint/image_provider.h"
 #include "cc/paint/image_transfer_cache_entry.h"
 #include "cc/paint/paint_filter.h"
@@ -34,7 +35,9 @@ ScopedRasterFlags::ScopedRasterFlags(const PaintFlags* flags,
 
   if (alpha != 255) {
     DCHECK(flags->SupportsFoldingAlpha());
-    MutableFlags()->setAlpha(SkMulDiv255Round(flags->getAlpha(), alpha));
+    // TODO(crbug/1308932): ScopedRasterFlags should use a float alpha and not
+    // an int, simplify this operation below when that's fixed.
+    MutableFlags()->setAlpha(flags->getAlpha() * alpha / 255.0f);
   }
 
   AdjustStrokeIfNeeded(ctm);
@@ -147,8 +150,8 @@ void ScopedRasterFlags::AdjustStrokeIfNeeded(const SkMatrix& ctm) {
     // Use modulated hairline when possible, as it is faster and produces
     // results closer to the original intent.
     MutableFlags()->setStrokeWidth(0);
-    MutableFlags()->setAlpha(std::round(
-        flags()->getAlpha() * std::sqrt(stroke_vec.x() * stroke_vec.y())));
+    MutableFlags()->setAlpha(flags()->getAlpha() *
+                             std::sqrt(stroke_vec.x() * stroke_vec.y()));
     return;
   }
 
