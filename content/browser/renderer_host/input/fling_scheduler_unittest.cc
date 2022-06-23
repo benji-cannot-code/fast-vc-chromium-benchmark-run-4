fd0068af48edb3d2b4b245ebecb697aad3e08aa4
Mike Wasserman <msw@chromium.org>
2022-06-23 00:08:33 +0000

    Restore FlingSchedulerTest's ScopedScreenWin usage
    Deprecated class use was speculatively removed in crrev.com/c/3685951
    That made FlingSchedulerTest.ScheduleNextFlingProgress start failing
    
    Followup work can determine a non-deprecated way to support that test.
    
    Fixed: 1337693
    Test: Automated
    Change-Id: If0929bb7f7ab5d073672d958e5810f2053f502d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716854
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016936}

--- content/browser/renderer_host/input/fling_scheduler_unittest.cc
+++ content/browser/renderer_host/input/fling_scheduler_unittest.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/test/test_render_widget_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "ui/display/win/test/scoped_screen_win.h"
+#endif
+
 namespace content {
 
 class FakeFlingScheduler : public FlingScheduler {
@@ -136,6 +140,10 @@ class FlingSchedulerTest : public testing::Test,
   scoped_refptr<SiteInstanceGroup> site_instance_group_;
   std::unique_ptr<TestRenderWidgetHostView> view_;
   std::unique_ptr<MockRenderWidgetHostDelegate> delegate_;
+#if BUILDFLAG(IS_WIN)
+  // This is necessary for static methods of `display::ScreenWin`.
+  display::win::test::ScopedScreenWin scoped_screen_win_;
+#endif
 };
 
 TEST_F(FlingSchedulerTest, ScheduleNextFlingProgress) {
