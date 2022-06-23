6941d4bdcb0211ea7f25b36a6b8b3f40bda8fd22
Dmitrii Kuragin <kuragin@chromium.org>
2022-06-23 22:33:11 +0000

    Add a way to track `LoadUrl` parameters in test url loader.
    Bug: 1333966
    Change-Id: Id4186f2e870d8e86c5772b5aac5da499317e7183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721738
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017384}

--- chrome/browser/web_applications/test/test_web_app_url_loader.h
+++ chrome/browser/web_applications/test/test_web_app_url_loader.h
@@ -11,13 +11,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/queue.h"
+#include "base/memory/raw_ptr.h"
 #include "chrome/browser/web_applications/web_app_url_loader.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
 namespace web_app {
 
 class TestWebAppUrlLoader : public WebAppUrlLoader {
  public:
+  struct LoadUrlCall {
+    GURL url;
+    base::raw_ptr<content::WebContents> web_contents;
+    UrlComparison url_comparison;
+  };
+
   TestWebAppUrlLoader();
   TestWebAppUrlLoader(const TestWebAppUrlLoader&) = delete;
   TestWebAppUrlLoader& operator=(const TestWebAppUrlLoader&) = delete;
@@ -49,6 +57,10 @@ class TestWebAppUrlLoader : public WebAppUrlLoader {
   void SetPrepareForLoadResultLoaded();
   void AddPrepareForLoadResults(const std::vector<Result>& results);
 
+  absl::optional<LoadUrlCall> last_load_url_call() const {
+    return last_load_url_call_;
+  }
+
  private:
   bool should_save_requests_ = false;
 
@@ -64,6 +76,7 @@ class TestWebAppUrlLoader : public WebAppUrlLoader {
 
   std::queue<std::pair<GURL, ResultCallback>> pending_requests_;
 
+  absl::optional<LoadUrlCall> last_load_url_call_ = absl::nullopt;
 };
 
 }  // namespace web_app
