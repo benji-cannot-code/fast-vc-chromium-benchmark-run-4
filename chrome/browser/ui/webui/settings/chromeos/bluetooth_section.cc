bc6b40d0cbde57bf2b17da3b723274a329d29453
Alex Kingsborough <akingsb@google.com>
2022-06-23 01:07:23 +0000

    [Fast Pair] Create blank Saved Devices page
    Create a blank saved devices subpage that is routed to by the link-
    row in the bluetooth subpage.  Future cls will populate this subpage
    with a saved device list.
    
    in future cls for this page.
    
    Test: Manually tested by DUT, further automated tests will be added
    Change-Id: I5da0b3d4145f60f55232ed490dce11d309f843bc
    Fixed: b/235413071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708963
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016962}

--- chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
@@ -319,6 +319,7 @@ void BluetoothSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
       {"enableFastPairSubtitle", IDS_BLUETOOTH_ENABLE_FAST_PAIR_SUBTITLE},
       {"savedDevicesLabel", IDS_BLUETOOTH_SAVED_DEVICES_LABEL},
       {"savedDevicesSubtitle", IDS_BLUETOOTH_SAVED_DEVICES_SUBTITLE},
+      {"savedDevicesPageName", IDS_SETTINGS_BLUETOOTH_SAVED_DEVICES},
       {"bluetoothPrimaryUserControlled",
        IDS_SETTINGS_BLUETOOTH_PRIMARY_USER_CONTROLLED},
       {"bluetoothDeviceWithConnectionStatus",
@@ -414,6 +415,7 @@ void BluetoothSection::RegisterHierarchy(HierarchyGenerator* generator) const {
       mojom::Setting::kBluetoothConnectToDevice,
       mojom::Setting::kBluetoothDisconnectFromDevice,
   };
+
   RegisterNestedSettingBulk(mojom::Subpage::kBluetoothDevices,
                             kBluetoothDevicesSettings, generator);
   generator->RegisterTopLevelAltSetting(mojom::Setting::kBluetoothOnOff);
@@ -427,6 +429,13 @@ void BluetoothSection::RegisterHierarchy(HierarchyGenerator* generator) const {
                                 ? mojom::Subpage::kBluetoothDeviceDetail
                                 : mojom::Subpage::kBluetoothDevices,
                             kBluetoothDevicesSettingsLegacy, generator);
+
+  generator->RegisterNestedSubpage(IDS_SETTINGS_BLUETOOTH_SAVED_DEVICES,
+                                   mojom::Subpage::kBluetoothSavedDevices,
+                                   mojom::Subpage::kBluetoothDevices,
+                                   mojom::SearchResultIcon::kBluetooth,
+                                   mojom::SearchResultDefaultRank::kMedium,
+                                   mojom::kBluetoothSavedDevicesSubpagePath);
 }
 
 void BluetoothSection::AdapterPresentChanged(device::BluetoothAdapter* adapter,
