fd8982c8ba7d5a986f22d039410630f35f097f09
Ayush Sharma <ayushsha@google.com>
2022-06-23 21:57:29 +0000

    Expose EnterpriseAuthenticationAppLinkPolicy APIs
    Bug: b/201408457
    Change-Id: I956f453fbc0b4adcaf33341542c4cc1b05ec1abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3709074
    Reviewed-by: Susanne Westphal <swestphal@chromium.org>
    Commit-Queue: Ayush Sharma <ayushsha@google.com>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017362}

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
@@ -65,4 +65,7 @@ public interface WebSettingsBoundaryInterface {
     void setRequestedWithHeaderMode(@RequestedWithHeaderMode int mode);
     @RequestedWithHeaderMode
     int getRequestedWithHeaderMode();
+
+    void setEnterpriseAuthenticationAppLinkPolicyEnabled(boolean enabled);
+    boolean getEnterpriseAuthenticationAppLinkPolicyEnabled();
 }
