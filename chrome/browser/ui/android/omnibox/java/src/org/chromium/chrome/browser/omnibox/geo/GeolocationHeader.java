3f81e5eb55b4ba0030e6900be46d1ddbf9d84c78
Andy Paicu <andypaicu@chromium.org>
2022-06-23 21:28:34 +0000

    Add some info logs to try to debug flaky GeolocationHeader tests
    The flake logs don't make it clear why the returned header is null and
    I've been unable to reproduce any flakiness locally. Adding these logs
    to at least identify which code path leads to a null header.
    
    Bug: 1338183
    Change-Id: I4ec0900460bcdfbd79c3147c4e36477ff4abda9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718088
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017347}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeader.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/geo/GeolocationHeader.java
@@ -306,7 +306,11 @@ public class GeolocationHeader {
     @Nullable
     public static String getGeoHeader(String url, Tab tab) {
         Profile profile = Profile.fromWebContents(tab.getWebContents());
-        if (profile == null) return null;
+        Log.i(TAG, "[getGeoHeader] getGeoHeader for url " + url);
+        if (profile == null) {
+            Log.i(TAG, "[getGeoHeader] getGeoHeader failed because of a null profile");
+            return null;
+        }
 
         return getGeoHeader(url, profile, tab);
     }
@@ -357,6 +361,7 @@ public class GeolocationHeader {
             long locationAge = Long.MAX_VALUE;
             @HeaderState
             int headerState = geoHeaderStateForUrl(profile, url, true);
+            Log.i(TAG, "[getGeoHeader] headerState: " + headerState);
             if (headerState == HeaderState.HEADER_ENABLED) {
                 locationToAttach = GeolocationTracker.getLastKnownLocation(
                         ContextUtils.getApplicationContext());
@@ -408,6 +413,10 @@ public class GeolocationHeader {
             String locationProtoEncoding = encodeProtoLocation(locationToAttach);
             String visibleNetworksProtoEncoding =
                     encodeProtoVisibleNetworks(visibleNetworksToAttach);
+
+            Log.i(TAG, "[getGeoHeader] locationProtoEncoding: " + locationProtoEncoding);
+            Log.i(TAG,
+                    "[getGeoHeader] visibleNetworksProtoEncoding: " + visibleNetworksProtoEncoding);
 
             if (locationProtoEncoding == null && visibleNetworksProtoEncoding == null) return null;
 
