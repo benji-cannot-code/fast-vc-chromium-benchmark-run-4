d832d46fece6088bc019958042809feca51a696f
Jeroen Dhollander <jeroendh@google.com>
2022-06-23 10:08:20 +0000

    Improve logging of CRD events
    Our enterprise customers can fetch the device logs through the `capture
    logs` remote command.
    This CL improves the CRD related logs that can be captured.
    
    Old logs:
    
    [remote_command_job.cc(92)] Remote command type 6 with id 1655818249673 initialized.
    [remote_commands_service.cc(388)] Remote command 1655818249673 finished with result 1
    // No log here to indicate why the CRD connection as terminated
    
    New logs:
    
    [remote_command_job.cc(92)] Remote command type DEVICE_START_CRD_SESSION (6) with id 1655818249673 initialized.
    [remote_commands_service.cc(388)] Remote command 1655818249673 finished with result RESULT_FAILURE (1)
    [crd_host_delegate.cc(92)] CRD session disconnected with reason: OK
    
    Bug: b/236702160
    Test: manually deployed
    Change-Id: I2dddd3719c9930658f677195326386f5ce355140
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714384
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017097}

--- components/policy/core/common/remote_commands/remote_command_job.cc
+++ components/policy/core/common/remote_commands/remote_command_job.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/strings/stringprintf.h"
 #include "base/syslog_logging.h"
 
 namespace policy {
@@ -16,6 +17,33 @@ namespace {
 
 constexpr base::TimeDelta kDefaultCommandTimeout = base::Minutes(10);
 constexpr base::TimeDelta kDefaultCommandExpirationTime = base::Minutes(10);
+
+std::string ToString(enterprise_management::RemoteCommand::Type type) {
+#define CASE(_name)                                 \
+  case enterprise_management::RemoteCommand::_name: \
+    return #_name;
+
+  switch (type) {
+    CASE(COMMAND_ECHO_TEST);
+    CASE(DEVICE_REBOOT);
+    CASE(DEVICE_SCREENSHOT);
+    CASE(DEVICE_SET_VOLUME);
+    CASE(DEVICE_FETCH_STATUS);
+    CASE(USER_ARC_COMMAND);
+    CASE(DEVICE_WIPE_USERS);
+    CASE(DEVICE_START_CRD_SESSION);
+    CASE(DEVICE_REMOTE_POWERWASH);
+    CASE(DEVICE_REFRESH_ENTERPRISE_MACHINE_CERTIFICATE);
+    CASE(DEVICE_GET_AVAILABLE_DIAGNOSTIC_ROUTINES);
+    CASE(DEVICE_RUN_DIAGNOSTIC_ROUTINE);
+    CASE(DEVICE_GET_DIAGNOSTIC_ROUTINE_UPDATE);
+    CASE(BROWSER_CLEAR_BROWSING_DATA);
+    CASE(DEVICE_RESET_EUICC);
+    CASE(BROWSER_ROTATE_ATTESTATION_CREDENTIAL);
+  }
+  return base::StringPrintf("Unknown type %i", type);
+#undef CASE
+}
 
 }  // namespace
 
@@ -62,8 +90,9 @@ bool RemoteCommandJob::Init(
     return false;
   }
 
-  SYSLOG(INFO) << "Remote command type " << command.type() << " with id "
-               << command.command_id() << " initialized.";
+  SYSLOG(INFO) << "Remote command type " << ToString(command.type()) << " ("
+               << command.type() << ")"
+               << " with id " << command.command_id() << " initialized.";
 
   status_ = NOT_STARTED;
   return true;
