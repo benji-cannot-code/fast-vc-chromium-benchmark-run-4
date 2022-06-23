72c3e8630d35342160af8c2c9ed20f676bcc6cad
Ewann <ewannpv@google.com>
2022-06-23 14:19:22 +0000

    [iOS][Symbols] Adds wrappers for Settings symbols.
    This CL adds a new file 'settings_icon' that contains new wrappers for Settings symbols.
    
    Bug: 1315544
    Change-Id: Iab252f24c95c8918c77bd9d3f0cd8361e34b64f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718521
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017152}

---
+++ ios/chrome/browser/ui/icons/settings_icon.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_ICONS_SETTINGS_ICON_H_
+#define IOS_CHROME_BROWSER_UI_ICONS_SETTINGS_ICON_H_
+
+#import <UIKit/UIKit.h>
+
+// The size of the symbol image displayed in the Settings root screen.
+extern NSInteger kSettingsRootSymbolImagePointSize;
+
+// Returns a SF symbol named |symbolName| configured for the Settings root
+// screen.
+UIImage* DefaultSettingsRootSymbol(NSString* symbolName);
+
+// Returns a custom symbol named |symbolName| configured for the Settings root
+// screen.
+UIImage* CustomSettingsRootSymbol(NSString* symbolName);
+
+#endif  // IOS_CHROME_BROWSER_UI_ICONS_SETTINGS_ICON_H_
