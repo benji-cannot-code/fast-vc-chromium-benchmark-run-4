deeaa7713015bac9498a45fc7145379e0658dbd9
Sam Zackrisson <saza@chromium.org>
2022-06-23 10:29:03 +0000

    Revert "Report capture OS glitch metrics every 1000 callbacks"
    This reverts commit 915c1bb310ba40af648c0a4076d4834e8f3f58a6.
    
    Reason for revert: incorrect metric names
    
    Original change's description:
    > Report capture OS glitch metrics every 1000 callbacks
    >
    > Before this CL, OS capture glitch metrics are reported per stream lifetime, which gives disproportionate weight to short streams and transient issues on stream start.
    > After, metrics are reported and reset every 1000 OS callbacks (every 10 seconds when buffers are 10 ms).
    >
    > Text logs are still reported once per active stream.
    >
    > This uses the same method of reporting glitch metrics as for render streams in https://crrev.com/c/3695571.
    >
    >
    > Bug: chromium:1336899
    > Change-Id: Ic39d0b988cc28efc96d18f644e42ea20feb93866
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707681
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Reviewed-by: Johannes Kron <kron@chromium.org>
    > Commit-Queue: Sam Zackrisson <saza@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1016119}
    
    Bug: chromium:1336899
    Change-Id: Ie8fd94131fdfe30fd0e67d1052975d9c30b8e02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714102
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017102}

--- media/audio/system_output_glitch_reporter.cc
+++ media/audio/system_output_glitch_reporter.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/audio/system_glitch_reporter.h"
+#include "media/audio/system_output_glitch_reporter.h"
 
 #include "base/metrics/histogram_functions.h"
 
@@ -14,20 +14,8 @@ namespace {
 constexpr static int kCallbacksPerLogPeriod = 1000;
 }  // namespace
 
-SystemGlitchReporter::SystemGlitchReporter(StreamType stream_type)
-    : num_glitches_detected_metric_name_(stream_type == StreamType::kCapture
-                                             ? "Media.Audio.Capture.Glitches2"
-                                             : "Media.Audio.Render.Glitches2"),
-      largest_glitch_duration_metric_name_(
-          stream_type == StreamType::kCapture
-              ? "Media.Audio.Capture.LostFramesInMs2"
-              : "Media.Audio.Render.LostFramesInMs2"),
-      total_glitch_duration_metric_name_(
-          stream_type == StreamType::kCapture
-              ? "Media.Audio.Capture.LargestGlitchMs2"
-              : "Media.Audio.Render.LargestGlitchMs2") {}
-
-SystemGlitchReporter::Stats SystemGlitchReporter::GetLongTermStatsAndReset() {
+SystemOutputGlitchReporter::Stats
+SystemOutputGlitchReporter::GetLongTermStatsAndReset() {
   Stats result = long_term_stats_;
   callback_count_ = 0;
   short_term_stats_ = {};
@@ -35,7 +23,7 @@ SystemGlitchReporter::Stats SystemGlitchReporter::GetLongTermStatsAndReset() {
   return result;
 }
 
-void SystemGlitchReporter::UpdateStats(base::TimeDelta glitch_duration) {
+void SystemOutputGlitchReporter::UpdateStats(base::TimeDelta glitch_duration) {
   ++callback_count_;
 
   if (glitch_duration.is_positive()) {
@@ -56,15 +44,15 @@ void SystemGlitchReporter::UpdateStats(base::TimeDelta glitch_duration) {
 
   // We record the glitch count even if there aren't any glitches, to get a
   // feel for how often we get no glitches vs the alternative.
-  base::UmaHistogramCounts1000(num_glitches_detected_metric_name_,
+  base::UmaHistogramCounts1000("Media.Audio.Render.Glitches2",
                                short_term_stats_.glitches_detected);
 
   if (short_term_stats_.glitches_detected != 0) {
     base::UmaHistogramCounts1M(
-        total_glitch_duration_metric_name_,
+        "Media.Audio.Render.LostFramesInMs2",
         short_term_stats_.total_glitch_duration.InMilliseconds());
     base::UmaHistogramCounts1M(
-        largest_glitch_duration_metric_name_,
+        "Media.Audio.Render.LargestGlitchMs2",
         short_term_stats_.largest_glitch_duration.InMilliseconds());
   }
 
