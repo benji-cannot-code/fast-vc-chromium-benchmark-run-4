715f1d9768282dac43647bd38a17dd6f40fe5b91
Jesse McKenna <jessemckenna@google.com>
2022-06-23 21:59:40 +0000

    Remove unused windows.h include
    This change removes an unused windows.h include.
    
    It seems to have been added in an early iteration of the file [1] and
    inadvertently left behind after uses of windows.h were removed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1815220/17
    
    Change-Id: I3ed9ac20ac588c6f08636fe59dc79325c7cd65a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722082
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017366}

--- chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.cc
+++ chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.h"
 
-#include <windows.h>
-
 #include "base/bind.h"
 #include "base/check.h"
 #include "base/files/file_enumerator.h"
