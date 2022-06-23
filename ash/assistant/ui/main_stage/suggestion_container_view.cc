8116f1252f50b1e919b36465eb332db1ce4ce290
Angela Xiao <angelaxiao@chromium.org>
2022-06-23 22:45:23 +0000

    Assistant: fix ChromeVox traversal on disabled chips
    This CL fixes an a11y issue where disabled Assistant suggestion
    chips were focused on by ChromeVox. The disabled chips are non-visible
    and should therefore be ignored by ChromeVox traversal.
    
    Tests: tested locally on nocturne DUT
    Bug: b/179643517
    Change-Id: I4fb8d9383e472bb9c813d6c984f856410bee9de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715860
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017390}

--- ash/assistant/ui/main_stage/suggestion_container_view.cc
+++ ash/assistant/ui/main_stage/suggestion_container_view.cc
@@ -73,6 +73,7 @@ class SuggestionChipAnimator : public ElementAnimator {
         layer()->GetAnimator(), CreateAnimateInAnimation(), observer,
         base::BindRepeating<void(const std::string&, int)>(
             base::UmaHistogramPercentage, kAssistantSuggestionChipHistogram));
+    ElementAnimator::view()->SetVisible(true);
   }
 
   void AnimateOut(ui::CallbackLayerAnimationObserver* observer) override {
@@ -80,6 +81,7 @@ class SuggestionChipAnimator : public ElementAnimator {
         layer()->GetAnimator(), CreateAnimateOutAnimation(), observer,
         base::BindRepeating<void(const std::string&, int)>(
             base::UmaHistogramPercentage, kAssistantSuggestionChipHistogram));
+    ElementAnimator::view()->SetVisible(false);
   }
 
   void FadeOut(ui::CallbackLayerAnimationObserver* observer) override {
