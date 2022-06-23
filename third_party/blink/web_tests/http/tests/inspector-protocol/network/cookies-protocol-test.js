0892462405a351a3b9739cf44ef67d10371a07e3
Danil Somsikov <dsv@chromium.org>
2022-06-23 14:04:41 +0000

    Delete all cookies before checking anything else to avoid cookies leaking from another test.
    Bug: 1325514
    Change-Id: I1ad80147e7db6d37f5938c9db276695bc3bf8617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720998
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017143}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/cookies-protocol-test.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/cookies-protocol-test.js
@@ -84,6 +84,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Network.enable();
 
   testRunner.runTestSuite([
+    deleteAllCookies,
+
     async function simpleCookieAdd() {
       await setCookie({url: 'http://127.0.0.1', name: 'foo', value: 'bar1'});
     },
