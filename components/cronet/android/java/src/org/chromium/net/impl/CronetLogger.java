bd8b01a8b81d328c2d2994769fb060de6dda3e08
Stefano Duo <stefanoduo@google.com>
2022-06-23 17:37:57 +0000

    CronetLogger: add logging calls to native cronet code
    Logging calls happen respectively after the creation of the native
    CronetEngine and before calling Listener#onRequestFinished.
    
    Currently:
    * All engine creation logging calls are reported as originating
      from statically linked/bundled Cronet
    * Bidirectional streams don't report traffic info events
    
    Bug: b:226554121
    
    Change-Id: Iaa9ce6047851f5116c25ca760f8788d08753959d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3672897
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017235}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetLogger.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetLogger.java
@@ -301,5 +301,11 @@ public abstract class CronetLogger {
         public int getPatchVersion() {
             return mPatchVersion;
         }
+
+        @Override
+        public String toString() {
+            return "" + mMajorVersion + "." + mMinorVersion + "." + mBuildVersion + "."
+                    + mPatchVersion;
+        }
     }
 }
