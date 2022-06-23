c16436675932738794158b7b5661a49595dcdccb
Matt Menke <mmenke@chromium.org>
2022-06-23 17:51:26 +0000

    Extensions: Make Event use base::Value::List, part 8.
    Update code in chrome/browser/ash and chrome/browser/ui/ash to use
    base::Value::List when constructing Events.
    
    Bug: 1338341
    Change-Id: Id42e889a423df8c85af008f0ae6d6abaa153034a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718565
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017245}

--- chrome/browser/ash/accessibility/accessibility_event_rewriter_delegate_impl.cc
+++ chrome/browser/ash/accessibility/accessibility_event_rewriter_delegate_impl.cc
@@ -113,8 +113,8 @@ void AccessibilityEventRewriterDelegateImpl::SendSwitchAccessCommand(
   extensions::EventRouter* event_router =
       extensions::EventRouter::Get(AccessibilityManager::Get()->profile());
 
-  std::vector<base::Value> event_args;
-  event_args.push_back(base::Value(ToString(command)));
+  base::Value::List event_args;
+  event_args.Append(ToString(command));
 
   auto event = std::make_unique<extensions::Event>(
       extensions::events::ACCESSIBILITY_PRIVATE_ON_SWITCH_ACCESS_COMMAND,
@@ -134,8 +134,8 @@ void AccessibilityEventRewriterDelegateImpl::SendPointScanPoint(
   point_dict.SetDoubleKey("x", point.x());
   point_dict.SetDoubleKey("y", point.y());
 
-  std::vector<base::Value> event_args;
-  event_args.push_back(std::move(point_dict));
+  base::Value::List event_args;
+  event_args.Append(std::move(point_dict));
 
   auto event = std::make_unique<extensions::Event>(
       extensions::events::ACCESSIBILITY_PRIVATE_ON_POINT_SCAN_SET,
@@ -151,8 +151,8 @@ void AccessibilityEventRewriterDelegateImpl::SendMagnifierCommand(
   extensions::EventRouter* event_router =
       extensions::EventRouter::Get(AccessibilityManager::Get()->profile());
 
-  std::vector<base::Value> event_args;
-  event_args.push_back(base::Value(ToString(command)));
+  base::Value::List event_args;
+  event_args.Append(ToString(command));
 
   auto event = std::make_unique<extensions::Event>(
       extensions::events::ACCESSIBILITY_PRIVATE_ON_SWITCH_ACCESS_COMMAND,
