e68f2e97f0ad47d5c08c1935b8572da27ad61466
David Maunder <davidjm@chromium.org>
2022-06-23 21:27:25 +0000

    Move MerchantSignalContentProto to //components/commerce
    Part of migrating ProfileProtoDB from chrome/browser to components
    necessitates moving the protos it depends on to components.
    
    Bug: 1337653
    Change-Id: Ic1eb819a668a2f98a0c95400ea9b67e162cc48d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3712995
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017345}

--- chrome/browser/persisted_state_db/profile_proto_db_factory.h
+++ chrome/browser/persisted_state_db/profile_proto_db_factory.h
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cart/cart_db_content.pb.h"
 #include "chrome/browser/commerce/coupons/coupon_db_content.pb.h"
 #else
-#include "chrome/browser/commerce/merchant_viewer/merchant_signal_db_content.pb.h"
 #include "chrome/browser/commerce/subscriptions/commerce_subscription_db_content.pb.h"
+#include "components/commerce/core/proto/merchant_signal_db_content.pb.h"
 #endif
 
 namespace {
