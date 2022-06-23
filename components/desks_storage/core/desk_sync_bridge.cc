2bee275d8fc9b904b0622b60ee24943a3fe0c2a9
Yanzhu Du <yzd@google.com>
2022-06-23 22:25:42 +0000

    Ignore unknown desk type in synced desk storage
    Bug: b/236882963
    Change-Id: Id48aa49105a298947a1a73e7b2956e7f5f5f119c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718928
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017380}

--- components/desks_storage/core/desk_sync_bridge.cc
+++ components/desks_storage/core/desk_sync_bridge.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/json/json_writer.h"
 #include "base/memory/ptr_util.h"
+#include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -851,6 +852,9 @@ void FillDeskType(const DeskTemplate* desk_template,
       out_entry_proto->set_desk_type(
           SyncDeskType::WorkspaceDeskSpecifics_DeskType_SAVE_AND_RECALL);
       return;
+    case DeskTemplateType::kUnknown:
+      NOTREACHED();
+      return;
   }
 }
 
@@ -861,6 +865,7 @@ DeskTemplateType GetDeskTemplateTypeFromProtoType(
   switch (proto_type) {
     // Treat unknown desk types as templates.
     case SyncDeskType::WorkspaceDeskSpecifics_DeskType_UNKNOWN_TYPE:
+      return DeskTemplateType::kUnknown;
     case SyncDeskType::WorkspaceDeskSpecifics_DeskType_TEMPLATE:
       return DeskTemplateType::kTemplate;
     case SyncDeskType::WorkspaceDeskSpecifics_DeskType_SAVE_AND_RECALL:
@@ -894,12 +899,19 @@ std::unique_ptr<DeskTemplate> DeskSyncBridge::FromSyncProto(
   const base::Time created_time = desk_template_conversion::ProtoTimeToTime(
       pb_entry.created_time_windows_epoch_micros());
 
-  // Protobuf parsing enforces UTF-8 encoding for all strings.
-  auto desk_template = std::make_unique<DeskTemplate>(
-      uuid, ash::DeskTemplateSource::kUser, pb_entry.name(), created_time,
+  const ash::DeskTemplateType desk_type =
       pb_entry.has_desk_type()
           ? GetDeskTemplateTypeFromProtoType(pb_entry.desk_type())
-          : ash::DeskTemplateType::kTemplate);
+          : ash::DeskTemplateType::kTemplate;
+
+  if (desk_type == ash::DeskTemplateType::kUnknown) {
+    return nullptr;
+  }
+
+  // Protobuf parsing enforces UTF-8 encoding for all strings.
+  auto desk_template =
+      std::make_unique<DeskTemplate>(uuid, ash::DeskTemplateSource::kUser,
+                                     pb_entry.name(), created_time, desk_type);
 
   if (pb_entry.has_updated_time_windows_epoch_micros()) {
     desk_template->set_updated_time(desk_template_conversion::ProtoTimeToTime(
--- components/desks_storage/core/desk_template_conversion.cc
+++ components/desks_storage/core/desk_template_conversion.cc
@@ -53,6 +53,7 @@ constexpr char kDesk[] = "desk";
 constexpr char kDeskType[] = "desk_type";
 constexpr char kDeskTypeTemplate[] = "TEMPLATE";
 constexpr char kDeskTypeSaveAndRecall[] = "SAVE_AND_RECALL";
+constexpr char kDeskTypeUnknown[] = "UNKNOWN";
 constexpr char kDisplayId[] = "display_id";
 constexpr char kEventFlag[] = "event_flag";
 constexpr char kIsAppTypeBrowser[] = "is_app";
@@ -963,6 +964,9 @@ std::string SerializeDeskTypeAsString(ash::DeskTemplateType desk_type) {
       return kDeskTypeTemplate;
     case ash::DeskTemplateType::kSaveAndRecall:
       return kDeskTypeSaveAndRecall;
+    case ash::DeskTemplateType::kUnknown:
+      NOTREACHED();
+      return kDeskTypeUnknown;
   }
 }
 
