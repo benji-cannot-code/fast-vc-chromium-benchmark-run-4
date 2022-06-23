6572fb56a5a3e765bb153002a716975a9c6daf32
Alexander Bolodurin <alexbn@chromium.org>
2022-06-23 01:12:20 +0000

    [lacros pwa install] Store web app info in prefs
    Store WebAppInfo from arc::mojom::PackageInfo sent by ARC in prefs
    together with other metadata, so it can be retrieved later on Lacros
    startup.
    
    ArcAppListPrefs only sends full arc::mojom::PackageInfo in notifications
    to observers in OnPackageInstalled callbacks, which happens only once
    when an app is installed. After OnPackageInstalled is called, there is
    no way for a client of ArcAppListPrefs client to retrieve the original
    arc::mojom::PackageInfo object (specifically the WebAppInfo field), only
    a subset of it (as ArcAppListPrefs::PackageInfo).
    
    When Lacros browser is not running, the initial OnPackageInstalled
    callback will be missed and not rerun later, so we need a full list of
    ARC apps with WebAppInfo available at the point of Lacros startup in
    order to reconcile ARC and Lacros installed app list to install missing
    apps, so this information needs to be cached.
    
    Bug: 1329727
    Change-Id: Iebd1fed1568a4520c5b3e649eb067a6f27d7adac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697664
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016964}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.h
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.h
@@ -162,7 +162,8 @@ class ArcAppListPrefs : public KeyedService,
                 bool system,
                 bool vpn_provider,
                 base::flat_map<arc::mojom::AppPermission,
-                               arc::mojom::PermissionStatePtr> permissions);
+                               arc::mojom::PermissionStatePtr> permissions,
+                arc::mojom::WebAppInfoPtr web_app_info);
     ~PackageInfo();
 
     std::string package_name;
@@ -175,6 +176,7 @@ class ArcAppListPrefs : public KeyedService,
     // Maps app permission to permission states
     base::flat_map<arc::mojom::AppPermission, arc::mojom::PermissionStatePtr>
         permissions;
+    arc::mojom::WebAppInfoPtr web_app_info;
   };
 
   class Observer : public base::CheckedObserver {
