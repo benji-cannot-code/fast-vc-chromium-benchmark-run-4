1b585d7c762b4aa9b2ef3eb18f446b116476f98a
Austin Eng <enga@chromium.org>
2022-06-23 03:12:49 +0000

    WebGPU: Mark the context lost on GPU context lost
    Fixes a bug where completely destructing the context instead of
    marking it lost when receiving a context lost notification freed
    memory still accessible by the page.
    
    Fixed: 1336014
    Change-Id: I662e531102af91362b4f62700bfbee507fc44d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708646
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017003}

--- third_party/blink/renderer/modules/webgpu/gpu.h
+++ third_party/blink/renderer/modules/webgpu/gpu.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -50,9 +51,9 @@ struct BoxedMappableWGPUBufferHandles
   HashSet<void*> contents_;
 };
 
-class GPU final : public ScriptWrappable,
-                  public Supplement<NavigatorBase>,
-                  public ExecutionContextLifecycleObserver {
+class MODULES_EXPORT GPU final : public ScriptWrappable,
+                                 public Supplement<NavigatorBase>,
+                                 public ExecutionContextLifecycleObserver {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -89,6 +90,9 @@ class GPU final : public ScriptWrappable,
   BoxedMappableWGPUBufferHandles* mappable_buffer_handles() const {
     return mappable_buffer_handles_.get();
   }
+
+  void SetDawnControlClientHolderForTesting(
+      scoped_refptr<DawnControlClientHolder> dawn_control_client);
 
  private:
   void OnRequestAdapterCallback(ScriptState* script_state,
