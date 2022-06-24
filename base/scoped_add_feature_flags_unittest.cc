4fc9db131220eb07dcb0a99fc4f08540bfeef483
Xiaocheng Hu <xiaochengh@chromium.org>
2022-06-24 20:44:57 +0000

    Don't create duplicate switches when using ScopedAddFeatureFlags
    Currently, ScopedAddFeatureFlags simply appends --enable-features
    and --disable-features switches to the command line. If the command
    line already have these switches, then it will end up with duplicate
    switches.
    
    This patch fixes it by removing the old switches (if any) before
    appending new switch values.
    
    Fixed: 1338872
    Change-Id: I26a1a6eb8331d2e51a58e148af01f351de920f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722815
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017800}

--- base/scoped_add_feature_flags_unittest.cc
+++ base/scoped_add_feature_flags_unittest.cc
@@ -5,14 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_add_feature_flags.h"
 
+#include <cstring>
 #include <string>
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/strings/string_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
+
+namespace {
+
+// Converts a string to CommandLine::StringType, which is std::wstring on
+// Windows and std::string on other platforms.
+CommandLine::StringType ToCommandLineStringType(StringPiece s) {
+  return CommandLine::StringType(s.begin(), s.end());
+}
+
+}  // namespace
 
 TEST(ScopedAddFeatureFlags, ConflictWithExistingFlags) {
   CommandLine command_line(CommandLine::NO_PROGRAM);
@@ -41,6 +53,14 @@ TEST(ScopedAddFeatureFlags, ConflictWithExistingFlags) {
             command_line.GetSwitchValueASCII(switches::kEnableFeatures));
   EXPECT_EQ(std::string("ExistingDisabledFoo,ExistingDisabledBar,DisabledBaz"),
             command_line.GetSwitchValueASCII(switches::kDisableFeatures));
+
+  // There should not be duplicate --enable-features or --disable-features flags
+  EXPECT_EQ(
+      ToCommandLineStringType(
+          " --enable-features=ExistingEnabledFoo,ExistingEnabledBar,EnabledBaz"
+          " --disable-features=ExistingDisabledFoo,ExistingDisabledBar,"
+          "DisabledBaz"),
+      JoinString(command_line.argv(), ToCommandLineStringType(" ")));
 }
 
 TEST(ScopedAddFeatureFlags, FlagWithParameter) {
