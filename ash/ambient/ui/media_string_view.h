6a6e51b27f4ec72d3eecdd031e98b7bc98b75aee
minch <minch@chromium.org>
2022-06-24 01:13:10 +0000

    migration: Clean up and correct usage of ColorModeObserver
    - Remove ColorModeObserver::OnColorModeThemed(), which is not needed
      after crbug/1300327.
    - Correct the usage of ColorModeObserver. Move the colors update to
      OnThemeChanged() for MediaStringView, thus it does not need to
      inherit ColorModeObserver.
    
    Bug: 1338318
    Change-Id: I0b9ac5be8e4f919f42d78c65034a670840807f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715233
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017450}

--- ash/ambient/ui/media_string_view.h
+++ ash/ambient/ui/media_string_view.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/public/cpp/style/color_mode_observer.h"
-#include "ash/style/ash_color_provider.h"
 #include "base/scoped_observation.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -36,8 +34,7 @@ class FadeoutLayerDelegate;
 class MediaStringView : public views::View,
                         public views::ViewObserver,
                         public media_session::mojom::MediaControllerObserver,
-                        public ui::ImplicitAnimationObserver,
-                        public ColorModeObserver {
+                        public ui::ImplicitAnimationObserver {
  public:
   struct Settings {
     SkColor icon_light_mode_color;
@@ -75,9 +72,6 @@ class MediaStringView : public views::View,
 
   // ui::ImplicitAnimationObserver:
   void OnImplicitAnimationsCompleted() override;
-
-  // ColorModeObserver:
-  void OnColorModeChanged(bool dark_mode_enabled) override;
 
  private:
   friend class AmbientAshTestBase;
@@ -121,9 +115,6 @@ class MediaStringView : public views::View,
 
   base::ScopedObservation<views::View, views::ViewObserver> observed_view_{
       this};
-
-  base::ScopedObservation<AshColorProvider, ColorModeObserver>
-      color_provider_observer_{this};
 
   base::WeakPtrFactory<MediaStringView> weak_factory_{this};
 };
