93c32513023bc1ffc9d4637ff6b3414f547b4ce9
Luciano Pacheco <lucmult@chromium.org>
2022-06-24 06:36:58 +0000

    Revert "partial_split: Add SnapWMEvent class that contains snap ratio type."
    This reverts commit 43ccdd0344c39519204c1c098b25968016aa24b3.
    
    Reason for revert: Multiple Window-related tests failing this CL is a suspect culprit:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/44045/blamelist 
    
    Original change's description:
    > partial_split: Add SnapWMEvent class that contains snap ratio type.
    >
    > Bug: 1323394
    > Test: existing
    > Change-Id: Ie73cfaba9dde969f5e1bc4fae580ca81ae544508
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699025
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1017459}
    
    Bug: 1323394
    Change-Id: Id5f698657682e2f044948c91fd4bc031cf2213a5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722842
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017520}

--- ash/wm/base_state.cc
+++ ash/wm/base_state.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_utils.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_positioning_utils.h"
-#include "ash/wm/window_state.h"
 #include "ash/wm/wm_event.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "ui/aura/client/aura_constants.h"
@@ -211,14 +210,6 @@ void BaseState::UpdateMinimizedState(WindowState* window_state,
 gfx::Rect BaseState::GetSnappedWindowBoundsInParent(
     aura::Window* window,
     const WindowStateType state_type) {
-  return BaseState::GetSnappedWindowBoundsInParent(window, state_type,
-                                                   kDefaultSnapRatio);
-}
-
-gfx::Rect BaseState::GetSnappedWindowBoundsInParent(
-    aura::Window* window,
-    const WindowStateType state_type,
-    float snap_ratio) {
   gfx::Rect bounds_in_parent;
   if (ShouldAllowSplitView()) {
     bounds_in_parent =
@@ -226,15 +217,13 @@ gfx::Rect BaseState::GetSnappedWindowBoundsInParent(
             (state_type == WindowStateType::kPrimarySnapped)
                 ? SplitViewController::LEFT
                 : SplitViewController::RIGHT,
-            window, snap_ratio);
+            window);
   } else {
-    // Use `window_positioning_utils` to calculate the snapped window bounds.
-    bounds_in_parent = ash::GetSnappedWindowBoundsInParent(
-        window,
-        state_type == WindowStateType::kPrimarySnapped
-            ? SnapViewType::kPrimary
-            : SnapViewType::kSecondary,
-        snap_ratio);
+    bounds_in_parent = (state_type == WindowStateType::kPrimarySnapped)
+                           ? GetDefaultSnappedWindowBoundsInParent(
+                                 window, SnapViewType::kPrimary)
+                           : GetDefaultSnappedWindowBoundsInParent(
+                                 window, SnapViewType::kSecondary);
   }
   return bounds_in_parent;
 }
