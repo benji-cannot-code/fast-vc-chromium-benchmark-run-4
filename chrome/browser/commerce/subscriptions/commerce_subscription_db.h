0f2a6272ed8918f9eeb90419869eb7a99a46344f
David Maunder <davidjm@chromium.org>
2022-06-24 20:45:21 +0000

    Move CommerceSubscriptionContentProto to //components/commerce
    Part of migrating ProfileProtoDB from chrome/browser to components
    necessitates moving the protos it depends on to components.
    
    Bug: 1337813
    Change-Id: If7a3d87d18c3ceab90b40f3129a66bd1c64b7cae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714793
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017801}

--- chrome/browser/commerce/subscriptions/commerce_subscription_db.h
+++ chrome/browser/commerce/subscriptions/commerce_subscription_db.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/commerce/subscriptions/commerce_subscription_db_content.pb.h"
+#include "components/commerce/core/proto/commerce_subscription_db_content.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/leveldb_proto/public/proto_database.h"
 
