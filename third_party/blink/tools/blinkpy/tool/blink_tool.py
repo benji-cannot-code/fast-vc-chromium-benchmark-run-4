2c289bd0f67db0e65101011ad7e1c8de0d7e4ea4
Jonathan Lee <jonathanjlee@google.com>
2022-06-24 16:34:03 +0000

    [blinkpy] Remove unused '--directory' option from blink_tool.py
    Fixed: 1338762
    Change-Id: Id08f7a5d7c03d2a1eb85675ff6459c4459df0f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722698
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017678}

--- third_party/blink/tools/blinkpy/tool/blink_tool.py
+++ third_party/blink/tools/blinkpy/tool/blink_tool.py
@@ -67,12 +67,6 @@ class BlinkTool(Host):
             dest='verbose',
             default=False,
             help='enable all logging'),
-        optparse.make_option(
-            '-d',
-            '--directory',
-            action='append',
-            default=[],
-            help='Directory to look at for changed files'),
     ]
 
     def __init__(self, path):
