865a976ccc3332e0043478bb44d902ba15cfeb0f
Bastian Kersting <bkersting@google.com>
2022-06-24 15:33:54 +0000

    [Telemetry] Allowlist extension id
    Allow an extension id to access chromeos_system_extension features.
    
    R=lamzin@google.com
    
          --gtest_filter="*ExtensionManifestChromeOSSystemExtensionTest*"
    
    Bug: b:188897824, b:1334537
    Test: unit_tests
    Test: unit_tests --gtest_filter="*ChromeOSSystemExtensionInfo*"
    Change-Id: Ifab255a98fa0979e8d8c9e8b00435fb93542f520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695074
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017648}

--- chrome/common/chromeos/extensions/chromeos_system_extension_info_unittest.cc
+++ chrome/common/chromeos/extensions/chromeos_system_extension_info_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(ChromeOSSystemExtensionInfo, AllowlistedExtensionsSizeEqualsToOne) {
-  ASSERT_EQ(2u, chromeos::GetChromeOSSystemExtensionInfosSize());
+TEST(ChromeOSSystemExtensionInfo, CheckAllowlistedExtensionsSize) {
+  ASSERT_EQ(3u, chromeos::GetChromeOSSystemExtensionInfosSize());
 }
 
 TEST(ChromeOSSystemExtensionInfo, GoogleExtension) {
@@ -30,6 +30,16 @@ TEST(ChromeOSSystemExtensionInfo, HPExtension) {
       chromeos::GetChromeOSExtensionInfoForId(hp_extension_id);
   EXPECT_EQ("HP", extension_info.manufacturer);
   EXPECT_EQ("https://hpcs-appschr.hpcloud.hp.com/*", extension_info.pwa_origin);
+}
+
+TEST(ChromeOSSystemExtensionInfo, ASUSExtension) {
+  const auto& asus_extension_id = "hdnhcpcfohaeangjpkcjkgmgmjanbmeo";
+  ASSERT_TRUE(chromeos::IsChromeOSSystemExtension(asus_extension_id));
+
+  const auto extension_info =
+      chromeos::GetChromeOSExtensionInfoForId(asus_extension_id);
+  EXPECT_EQ("ASUS", extension_info.manufacturer);
+  EXPECT_EQ("https://dlcdnccls.asus.com/*", extension_info.pwa_origin);
 }
 
 TEST(ChromeOSSystemExtensionInfo, ManufacturerOverride) {
