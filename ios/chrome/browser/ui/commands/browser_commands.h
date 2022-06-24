28735a551312eb79495b16c67ff261c572e3beab
Petro Akzhygitov <pakzhygitov@google.com>
2022-06-24 16:17:24 +0000

    Factor LensCommands out of BrowserCommands
    – Removed LensCommands from BrowserCommands
    
    Bug: 1323783
    Change-Id: I442929d3e10940b604997dd9e0af293878928ac8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721654
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017668}

--- ios/chrome/browser/ui/commands/browser_commands.h
+++ ios/chrome/browser/ui/commands/browser_commands.h
@@ -33,9 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // TODO(crbug.com/1323775): Remove QRScannerCommands conformance.
     QRScannerCommands,
     // TODO(crbug.com/1323778): Remove SnackbarCommands conformance.
-    SnackbarCommands,
-    // TODO(crbug.com/1323783): Remove LensCommands conformance.
-    LensCommands>
+    SnackbarCommands>
 
 // Closes the current tab.
 // TODO(crbug.com/1272498): Refactor this command away; call sites should close
