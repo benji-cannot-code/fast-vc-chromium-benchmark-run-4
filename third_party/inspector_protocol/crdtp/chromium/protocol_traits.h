22fe1217e93f6f76317c80e41b50c48fa1fe4bd1
Matt Menke <mmenke@chromium.org>
2022-06-24 20:53:10 +0000

    DevTools: Update inspector_protocol to use base::Value::List.
    It was using std::Vector<base::Value>, also known as
    base::Value::DeprecatedListStorage.
    
    This does not break compatibility with serialized data from the old
    version of the code.
    
    Bug: 1338341
    Change-Id: I02fb7128f738516f68f3de40942fe88f8772ac4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721566
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017815}

--- third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
+++ third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
@@ -26,7 +26,7 @@ namespace traits {
 using String = std::string;
 using Value = base::Value;
 using DictionaryValue = base::Value::Dict;
-using ListValue = std::vector<base::Value>;
+using ListValue = base::Value::List;
 
 }  // namespace traits
 
@@ -47,6 +47,13 @@ struct CRDTP_EXPORT ProtocolTypeTraits<traits::DictionaryValue> {
   static bool Deserialize(DeserializerState* state,
                           traits::DictionaryValue* value);
   static void Serialize(const traits::DictionaryValue& value,
+                        std::vector<uint8_t>* bytes);
+};
+
+template <>
+struct CRDTP_EXPORT ProtocolTypeTraits<traits::ListValue> {
+  static bool Deserialize(DeserializerState* state, traits::ListValue* value);
+  static void Serialize(const traits::ListValue& value,
                         std::vector<uint8_t>* bytes);
 };
 
