95a39c7cb69a3e74e7d14debc4fd84da0738608b
Alexander Dunaev <adunaev@igalia.com>
2022-06-24 16:23:03 +0000

    [lacros] Fixed the radius of the window corner.
    ChromeOS has sharper corners than Linux.  This patch fixes radius of
    corners for Lacros frames.
    
    Bug: 1338845
    Change-Id: I0a2ff95cabb084595851e009374b1d33b8f91d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721320
    Auto-Submit: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017670}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_lacros.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_lacros.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/desktop_browser_frame_lacros.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "chromeos/ui/base/chromeos_ui_constants.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "ui/compositor/layer.h"
 #include "ui/platform_window/extensions/wayland_extension.h"
@@ -51,8 +52,7 @@ void BrowserDesktopWindowTreeHostLacros::UpdateFrameHints() {
 
   std::vector<gfx::Rect> opaque_region;
   if (showing_frame) {
-    float corner_radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
-        views::Emphasis::kHigh);
+    const float corner_radius = chromeos::kTopCornerRadiusWhenRestored;
     GetContentWindow()->layer()->SetRoundedCornerRadius(
         gfx::RoundedCornersF(corner_radius, corner_radius, 0, 0));
     GetContentWindow()->layer()->SetIsFastRoundedCorner(true);
