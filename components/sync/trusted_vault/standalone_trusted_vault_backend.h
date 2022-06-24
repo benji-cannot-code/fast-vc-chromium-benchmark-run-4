af99d3b3cbc123dd94f957dd4045907708dc72b5
Mikel Astiz <mastiz@chromium.org>
2022-06-24 15:10:27 +0000

    [sync] Record metric for a dry-run trusted vault key download
    New logic is gated behind a feature toggle that is disabled by default.
    
    The idea implemented here is to instrument a metric that predicts what
    the outcome of downloading keys would be, i.e. what the pre-existing
    metric Sync.TrustedVaultDownloadKeysStatus would record, if the user
    opted into the feature.
    
    The main goal is to verify that, if the local state indicates that the
    device is registered, then the server should report a consistent state,
    which in most cases should imply bucket 2 (no new keys).
    
    Change-Id: Ie7cf16b59f4cad40aec794da867b2309da02596b
    Bug: 1334939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720639
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017639}

--- components/sync/trusted_vault/standalone_trusted_vault_backend.h
+++ components/sync/trusted_vault/standalone_trusted_vault_backend.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/sync/driver/trusted_vault_histograms.h"
 #include "components/sync/protocol/local_trusted_vault.pb.h"
@@ -199,6 +198,8 @@ class StandaloneTrustedVaultBackend
   // for deletion due to accounts in cookie jar changes.
   void RemoveNonPrimaryAccountKeysIfMarkedForDeletion();
 
+  void VerifyDeviceRegistrationForUMA(const std::string& gaia_id);
+
   const base::FilePath file_path_;
 
   const std::unique_ptr<Delegate> delegate_;
@@ -242,6 +243,8 @@ class StandaloneTrustedVaultBackend
 
   // Destroying this will cancel the ongoing request.
   std::unique_ptr<TrustedVaultConnection::Request> ongoing_connection_request_;
+  std::unique_ptr<TrustedVaultConnection::Request>
+      ongoing_verify_registration_request_;
 
   // Same as above, but specifically used for recoverability-related requests.
   // TODO(crbug.com/1201659): Move elsewhere.
