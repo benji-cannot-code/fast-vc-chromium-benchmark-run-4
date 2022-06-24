e707f1c20abd256f8708a3544db777d6c0f45631
Rebekah Potter <rbpotter@chromium.org>
2022-06-24 00:11:18 +0000

    Sync Internals: Migrate more files to TypeScript
    Migrating user_events, sync_node_browser, sync_index, and sync_log.
    
    Bug: 986001
    Change-Id: I64f217d8e4fa7be6be0c0a75a45bc8dcf76a20c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716296
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017425}

--- components/sync/driver/resources/sync_search.ts
+++ components/sync/driver/resources/sync_search.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
-import {getAllNodes, SyncNodeList, SyncNodeMap, Timer} from './chrome_sync.js';
+import {getAllNodes, SyncNode, SyncNodeMap, Timer} from './chrome_sync.js';
 
 const ERROR_ATTR: string = 'error';
 const SELECTED_ATTR: string = 'selected';
@@ -132,7 +132,7 @@ export class SyncSearchManager {
       const regex = new RegExp(query);
       getAllNodes((nodeMap: SyncNodeMap) => {
         // Put all nodes into one big list that ignores the type.
-        const nodes: SyncNodeList =
+        const nodes: SyncNode[] =
             nodeMap.map(x => x.nodes).reduce((a, b) => a.concat(b));
         if (this.currSearchId_ !== searchId) {
           return;
