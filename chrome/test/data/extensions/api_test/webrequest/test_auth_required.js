12cead9dd5a4892e0e46938523ddea0ca1a6c384
David Bertoni <dbertoni@chromium.org>
2022-06-24 22:15:47 +0000

    [Extensions] Re-enable a WebRequestAuthRequired test.
    This test was disabled for for flakiness and became stale. Due to changes in the navigation layer for mparch, these tests now interfere with each other, so the solution is to run them separately within the same test fixture. This both fixes the tests and the flakiness.
    
    They passed 3000 runs on the bots with minimal failures. There are some unrelated failures on Windows that are tracked by crbug.com/1154864.
    
    Bug: 1003598
    Change-Id: Ifbc01fbc28099114dda583b097286632a68e14d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711835
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017851}

--- chrome/test/data/extensions/api_test/webrequest/test_auth_required.js
+++ chrome/test/data/extensions/api_test/webrequest/test_auth_required.js
@@ -11,7 +11,7 @@ function getURLAuthRequired(realm, subpath = 'subpath') {
       'auth-basic/' + realm + '/' + subpath + '?realm=' + realm);
 }
 
-runTests([
+var availableTests = [
   // onAuthRequired is not a blocking function in this variant.
   function authRequiredNonBlocking() {
     var realm = 'nonblock';
@@ -339,4 +339,17 @@ runTests([
       ["responseHeaders", "blocking"]);
     navigateAndWait(url);
   },
-]);
+];
+
+chrome.test.getConfig(function(config) {
+  var args = JSON.parse(config.customArg);
+  var tests = availableTests.filter(function(op) {
+    return args.testName == op.name;
+  });
+  if (tests.length !== 1) {
+    chrome.test.notifyFail('Test not found: ' + args.testName);
+    return;
+  }
+
+  runTests(tests);
+});
