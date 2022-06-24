fa5c7b3a3674c96bc8409fd8999227ac79a70911
Daniel Vogelheim <vogelheim@chromium.org>
2022-06-24 13:18:31 +0000

    [Sanitizer] Remove name normalization and adapt tests.
    Adapt Sanitizer API to the proposed
    https://github.com/WICG/sanitizer-api/issues/145
    This removes normalization of element and attribute names
    and makes comparisons case-insensitive.
    
    Bug: 1326827
    Change-Id: Ib7cebd9e74cec895c2716aa88b4eeb71bb7d169e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706742
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017597}

--- third_party/blink/renderer/modules/sanitizer_api/builtins.cc
+++ third_party/blink/renderer/modules/sanitizer_api/builtins.cc
@@ -22,17 +22,6 @@ namespace {
 // arguments. Thus we have this typedef as a work-around.
 typedef HashMap<String, String> StringMap;
 
-StringMap MixedCaseNames(const char* const* names) {
-  HashMap<String, String> map;
-  for (const char* const* iter = names; *iter; ++iter) {
-    String name(*iter);
-    if (!name.IsLowerASCII()) {
-      map.insert(name.LowerASCII(), name);
-    }
-  }
-  return map;
-}
-
 SanitizerConfigImpl::ElementList ElementsFromAPI(const char* const* elements) {
   SanitizerConfigImpl::ElementList element_list;
   for (const char* const* elem = elements; *elem; ++elem) {
@@ -92,18 +81,6 @@ const SanitizerConfigImpl::AttributeList& GetBaselineAllowAttributes() {
   return attributes_;
 }
 
-const HashMap<String, String>& GetMixedCaseElementNames() {
-  DEFINE_STATIC_LOCAL(StringMap, element_names_,
-                      (MixedCaseNames(kBaselineElements)));
-  return element_names_;
-}
-
-const HashMap<String, String>& GetMixedCaseAttributeNames() {
-  DEFINE_STATIC_LOCAL(StringMap, attribute_names_,
-                      (MixedCaseNames(kBaselineAttributes)));
-  return attribute_names_;
-}
-
 }  // namespace default_config_names
 
 namespace with_namespace_names {
@@ -125,18 +102,6 @@ const SanitizerConfigImpl::AttributeList& GetBaselineAllowAttributes() {
   DEFINE_STATIC_LOCAL(SanitizerConfigImpl::AttributeList, attributes_,
                       (AttributesFromAPI(kBaselineAttributes)));
   return attributes_;
-}
-
-const HashMap<String, String>& GetMixedCaseElementNames() {
-  DEFINE_STATIC_LOCAL(StringMap, element_names_,
-                      (MixedCaseNames(kBaselineElements)));
-  return element_names_;
-}
-
-const HashMap<String, String>& GetMixedCaseAttributeNames() {
-  DEFINE_STATIC_LOCAL(StringMap, attribute_names_,
-                      (MixedCaseNames(kBaselineAttributes)));
-  return attribute_names_;
 }
 
 }  // namespace with_namespace_names
@@ -162,15 +127,4 @@ const SanitizerConfigImpl::AttributeList& GetBaselineAllowAttributes() {
   return WithNamespaces() ? with_namespace_names::GetBaselineAllowAttributes()
                           : default_config_names::GetBaselineAllowAttributes();
 }
-
-const HashMap<String, String>& GetMixedCaseElementNames() {
-  return WithNamespaces() ? with_namespace_names::GetMixedCaseElementNames()
-                          : default_config_names::GetMixedCaseElementNames();
-}
-
-const HashMap<String, String>& GetMixedCaseAttributeNames() {
-  return WithNamespaces() ? with_namespace_names::GetMixedCaseAttributeNames()
-                          : default_config_names::GetMixedCaseAttributeNames();
-}
-
 }  // namespace blink
