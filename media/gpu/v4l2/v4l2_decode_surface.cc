bfd8a7e360551860bf717f9eab873707650faaeb
Chen-Yu Tsai <wenst@chromium.org>
2022-06-24 05:54:33 +0000

    media/gpu/v4l2: Split upstream compatible and ChromeOS specific code
    Various parts of the V4L2 acceleration code conform to upstream
    specifications. However, there are some parts that are ChromeOS
    specific. These include support for pre-upstream, pre-finalized
    stateless decoders, and DMAbuf offsets used in the encoders.
    
    Split the V4L2 code along the "upstream compatible" line, such that
    the upstream compatible subset can be built for standard Linux.
    
    Currently this involves either building natively with a sufficiently
    recent set of kernel headers, or unpacking the latest (5.17.x as of
    this writing) linux-libc-dev package into the sysroots by hand:
    
        wget http://ftp.us.debian.org/debian/pool/main/l/linux/linux-libc-dev_5.17.1-1~exp1_amd64.deb
        dpkg-deb -X linux-libc-dev_5.17.1-1~exp1_amd64.deb build/linux/debian_bullseye_amd64-sysroot/
        wget http://ftp.us.debian.org/debian/pool/main/l/linux/linux-libc-dev_5.17.1-1~exp1_arm64.deb
        dpkg-deb -X linux-libc-dev_5.17.1-1~exp1_arm64.deb build/linux/debian_bullseye_arm64-sysroot/
        wget http://ftp.us.debian.org/debian/pool/main/l/linux/linux-libc-dev_5.17.1-1~exp1_armhf.deb
        dpkg-deb -X linux-libc-dev_5.17.1-1~exp1_armhf.deb build/linux/debian_bullseye_arm-sysroot/
        wget http://ftp.us.debian.org/debian/pool/main/l/linux/linux-libc-dev_5.17.1-1~exp1_i386.deb
        dpkg-deb -X linux-libc-dev_5.17.1-1~exp1_i386.deb build/linux/debian_bullseye_i386-sysroot/
    
    For arm64, the amd64 headers also need to be updated, as V8 snapshot is
    currently built for amd64, and for some reason the V4L2 stuff is built,
    even though it is not used. Same goes for armhf and i386.
    
    This allows building V4L2 decoders for VP8, H.264 and VP9 videos on for
    standard Linux, which would allow partners and the broader ecosystem to
    test and compare Chromium V4L2 decoder implementations against others,
    such as Gstreamer or FFmpeg.
    
    This would also allow further development to actually make V4L2 usable
    on Linux. Currently other parts of the media pipeline, such as buffer
    allocation, still depend on things not normally found on standard Linux.
    
    BUG=none
    TEST=Build for arm64 linux with above header updates and
         use_v4l2_codec=true proprietary_codecs=true ffmpeg_branding="Chrome"
    
    Change-Id: Iff3c2a53d7dca7395424c3c6e923fd4bc9a86476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380426
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017513}

--- media/gpu/v4l2/v4l2_decode_surface.cc
+++ media/gpu/v4l2/v4l2_decode_surface.cc
@@ -104,6 +104,8 @@ std::string V4L2DecodeSurface::ToString() const {
   return out;
 }
 
+// ConfigStore is ChromeOS-specific legacy stuff
+#if BUILDFLAG(IS_CHROMEOS)
 void V4L2ConfigStoreDecodeSurface::PrepareSetCtrls(
     struct v4l2_ext_controls* ctrls) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -141,6 +143,7 @@ bool V4L2ConfigStoreDecodeSurface::Submit() {
 
   return false;
 }
+#endif
 
 void V4L2RequestDecodeSurface::PrepareSetCtrls(
     struct v4l2_ext_controls* ctrls) const {
