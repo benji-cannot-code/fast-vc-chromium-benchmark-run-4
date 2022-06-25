53939cd155fdcf8b664b50e727266e493c5789eb
Robert Lin <robertlin@chromium.org>
2022-06-25 03:00:21 +0000

    prerender: Make WebNavigation be aware of prerendering.
    This CL makes WebNavigation be aware of prerendering frames and add a
    relate test.
    
    Bug: 1278141
    Change-Id: I5d46eead97eafce90dd4988b6200a55bf026fe4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692509
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017914}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -249,7 +249,13 @@ class WebNavigationApiTestWithContextType
     : public WebNavigationApiTest,
       public testing::WithParamInterface<ContextType> {
  public:
-  WebNavigationApiTestWithContextType() : WebNavigationApiTest(GetParam()) {}
+  WebNavigationApiTestWithContextType() : WebNavigationApiTest(GetParam()) {
+    feature_list_.InitWithFeaturesAndParameters(
+        {
+            {blink::features::kPrerender2, {}},
+        },
+        {});
+  }
   ~WebNavigationApiTestWithContextType() override = default;
   WebNavigationApiTestWithContextType(
       const WebNavigationApiTestWithContextType&) = delete;
@@ -262,6 +268,9 @@ class WebNavigationApiTestWithContextType
     return RunExtensionTest(name, {},
                             {.allow_in_incognito = allow_in_incognito});
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, Api) {
@@ -269,6 +278,7 @@ IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, Api) {
 }
 
 IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, GetFrame) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webnavigation/getFrame")) << message_;
 }
 
