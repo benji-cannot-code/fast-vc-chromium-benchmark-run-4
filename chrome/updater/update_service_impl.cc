fe8a633c3ed4ab0eecb4add7aa989c57660a9a4d
S. Ganesh <ganesh@chromium.org>
2022-06-25 00:54:58 +0000

    Add support for running AutoRunOnOSUpgrade app commands on OS upgrades
    The AutoRunOnOsUpgrade task runs AutoRunOnOSUpgrade app commands
    registered for the updater apps on Windows as a part of the update
    service's periodic tasks.
    
    Bug: 1316682
    Change-Id: I2eb918f165b5ffd484c125dcf4f3033a1bc2f5cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717426
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017895}

--- chrome/updater/update_service_impl.cc
+++ chrome/updater/update_service_impl.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/version.h"
+#include "chrome/updater/auto_run_on_os_upgrade_task.h"
 #include "chrome/updater/check_for_updates_task.h"
 #include "chrome/updater/configurator.h"
 #include "chrome/updater/constants.h"
@@ -309,6 +310,11 @@ void UpdateServiceImpl::RunPeriodicTasks(base::OnceClosure callback) {
                      base::MakeRefCounted<CheckForUpdatesTask>(
                          config_, base::BindOnce(&UpdateServiceImpl::UpdateAll,
                                                  this, base::DoNothing()))));
+  new_tasks.push_back(
+      base::BindOnce(&AutoRunOnOsUpgradeTask::Run,
+                     base::MakeRefCounted<AutoRunOnOsUpgradeTask>(
+                         GetUpdaterScope(), persisted_data_)));
+
   const auto barrier_closure =
       base::BarrierClosure(new_tasks.size(), std::move(callback));
   for (auto& task : new_tasks) {
