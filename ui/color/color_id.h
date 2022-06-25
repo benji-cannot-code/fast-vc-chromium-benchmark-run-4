218488bb2712f296098539d14f7dbccb7629a8dc
Rohit Bhatia <bhatiarohit@google.com>
2022-06-25 06:31:38 +0000

    [DownloadBubble] Add Paused state to ProgressBar
    Paused: https://screenshot.googleplex.com/9ebTC7fYC3bzcpe, https://screenshot.googleplex.com/6CqK57Thf35pF2L
    
    Bug: 1335221
    Change-Id: I897cfebd387731a172a03919f38cae21ee22546e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699006
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017933}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -129,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorOverlayScrollbarStrokeHoveredDark) \
   E_CPONLY(kColorOverlayScrollbarStrokeHoveredLight) \
   E_CPONLY(kColorProgressBar) \
+  E_CPONLY(kColorProgressBarPaused) \
   E_CPONLY(kColorSeparator) \
   E_CPONLY(kColorShadowBase) \
   E_CPONLY(kColorShadowValueAmbientShadowElevationSixteen) \
