0e0d60fa372eb4591d3acaa722e4ba584074e5ad
Eric Willigers <ericwilligers@chromium.org>
2022-06-27 04:57:51 +0000

    Lacros web apps: Source set for tests that rely on App Service
    Some web applications browser tests rely on the App Service.
    
    These tests can now be run as part of a new source set
    //chrome/browser/web_applications:app_service_browser_tests
    used by lacros_chrome_browsertests_run_in_series
    
    Bug: 1208258
    Change-Id: Id541493c37f476f90ae6680fef1db76d1bc0dbe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3725728
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018102}

--- chrome/browser/web_applications/preinstalled_web_app_migration_browsertest.cc
+++ chrome/browser/web_applications/preinstalled_web_app_migration_browsertest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/preinstalled_app_install_features.h"
 #include "chrome/browser/web_applications/preinstalled_web_app_manager.h"
 #include "chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.h"
+#include "chrome/browser/web_applications/test/app_registration_waiter.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
@@ -503,10 +504,15 @@ class PreinstalledWebAppMigratePlatformAppBrowserTest
 IN_PROC_BROWSER_TEST_F(PreinstalledWebAppMigratePlatformAppBrowserTest,
                        MigratePlatformAppPreferences) {
   // Install platform app to migrate.
-  ASSERT_EQ(InstallExtension(
-                test_data_dir_.AppendASCII("platform_apps/app_window_2"), 1)
-                ->id(),
-            kPlatformAppId);
+  {
+    AppRegistrationWaiter extension_app_registration_waiter(profile(),
+                                                            kPlatformAppId);
+    ASSERT_EQ(InstallExtension(
+                  test_data_dir_.AppendASCII("platform_apps/app_window_2"), 1)
+                  ->id(),
+              kPlatformAppId);
+    extension_app_registration_waiter.Await();
+  }
 
   // Migrate extension app to web app.
   {
