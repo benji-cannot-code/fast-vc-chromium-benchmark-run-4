5fdbbd22ebce27666066079a700c27c5acf8da7d
Joel Hockey <joelhockey@chromium.org>
2022-06-27 02:25:09 +0000

    Move GetVshSession from CrostiniManager into terminal_private_api
    We are removing all crostini deps from terminal so that it can be used
    for all guests.
    
    Change-Id: I00ab69e95a62b9565b47638e044b029b47b60cb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715420
    Reviewed-by: Jason Lin <lxj@google.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1018083}

--- chrome/browser/extensions/api/terminal/terminal_private_api.h
+++ chrome/browser/extensions/api/terminal/terminal_private_api.h
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/crostini/crostini_simple_types.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chromeos/ash/components/dbus/cicerone/cicerone_service.pb.h"
 #include "components/value_store/value_store.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/extension_function.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefChangeRegistrar;
 
@@ -75,12 +77,11 @@ class TerminalPrivateOpenTerminalProcessFunction : public ExtensionFunction {
   void OpenVmshellProcess(const std::string& user_id_hash,
                           base::CommandLine cmdline);
 
-  void OnGetVshSession(const std::string& user_id_hash,
-                       base::CommandLine cmdline,
-                       const std::string& terminal_id,
-                       bool success,
-                       const std::string& failure_reason,
-                       int32_t container_shell_pid);
+  void OnGetVshSession(
+      const std::string& user_id_hash,
+      base::CommandLine cmdline,
+      const std::string& terminal_id,
+      absl::optional<vm_tools::cicerone::GetVshSessionResponse>);
 
   void OpenProcess(const std::string& user_id_hash,
                    base::CommandLine cmdline);
