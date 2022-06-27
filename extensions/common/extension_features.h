80cf7e91523dfd08fe36851a8ce17474759860fd
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-06-27 22:58:39 +0000

    [Extensions Offscreen Documents] Start implementing offscreen documents
    Offscreen documents are a new tool in Manifest V3 to allow extensions
    to spawn a WebContents offscreen for purposes that require a DOM.
    Unlike extension background pages, offscreen documents cannot register
    "lazy" event listeners and be woken up by the browser in response to
    API events.
    
    This CL introduces a first basic version of an offscreen document
    behind a feature flag, "ExtensionsOffscreenDocuments". This involves
    introducing a new ExtensionHost type, OffscreenDocumentHost. Though
    other implementations of WebContents wrappers, such as
    media::OffscreenTab, were considered, ExtensionHost was ultimately a
    better fit: it already knows how to host extension content, is wired
    into the extensions system, works well with process manager
    integration, and more.
    
    Since offscreen documents will be a relatively core part of extensions,
    the implementation of OffscreenDocumentHost is not defined in an API
    file (however, the API for creation and extension-driven management of
    them will be). This does not yet expose offscreen document creation to
    extensions.
    
    Also add a basic test exercising the creation and proper registration
    of offscreen documents.
    
    Bug: 1339382
    Change-Id: I8395bfcbc6eebb24b01b23f58ef9c749d7b86c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722498
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018443}

--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -40,6 +40,8 @@ extern const base::Feature kExtensionsMenuAccessControl;
 
 extern const base::Feature kAvoidEarlyExtensionScriptContextCreation;
 
+extern const base::Feature kExtensionsOffscreenDocuments;
+
 }  // namespace extensions_features
 
 #endif  // EXTENSIONS_COMMON_EXTENSION_FEATURES_H_
