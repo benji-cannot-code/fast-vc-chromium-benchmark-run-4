9ed66a9415bf2ccef7b86edff0c8bfeb04c52fd9
Matt Menke <mmenke@chromium.org>
2022-06-27 15:52:56 +0000

    Update base::Value list construction in [ios/]chrome/browser/policy/
    Switch from using vector<Value> (also known as Value::ListStorage),
    which is deprecated, to using base::Value::List().
    
    Bug: 1338341
    Change-Id: I1a650da1cbe48992f00ebcab973d4bf141a6abf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3725652
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018253}

--- chrome/browser/policy/test/ssl_error_overriding_allowed_policy_browsertest.cc
+++ chrome/browser/policy/test/ssl_error_overriding_allowed_policy_browsertest.cc
@@ -87,8 +87,8 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
 
   // Add a policy to allow overriding on specific sites only. Since
   // kSSLErrorOverrideAllowed is enabled, this should do nothing.
-  std::vector<base::Value> allow_list;
-  allow_list.emplace_back(base::Value("example.com"));
+  base::Value::List allow_list;
+  allow_list.Append("example.com");
   PolicyMap policies;
   policies.Set(key::kSSLErrorOverrideAllowedForOrigins, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
@@ -176,8 +176,8 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
                nullptr);
   // Add a policy to allow overriding on specific sites only.
-  std::vector<base::Value> allow_list;
-  allow_list.emplace_back(base::Value("example.com"));
+  base::Value::List allow_list;
+  allow_list.Append("example.com");
   policies.Set(key::kSSLErrorOverrideAllowedForOrigins, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(allow_list)), nullptr);
@@ -226,10 +226,10 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
   policies.Set(key::kSSLErrorOverrideAllowed, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
                nullptr);
-  std::vector<base::Value> allow_list;
+  base::Value::List allow_list;
   // We ignore "*" or badly formed patterns as inputs.
-  allow_list.emplace_back(base::Value("*"));
-  allow_list.emplace_back(base::Value("bad 127.0.0.1 input"));
+  allow_list.Append("*");
+  allow_list.Append("bad 127.0.0.1 input");
   policies.Set(key::kSSLErrorOverrideAllowedForOrigins, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(allow_list)), nullptr);
@@ -278,7 +278,7 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
                nullptr);
   // The policy is intentionally configured with an empty list for this test.
-  std::vector<base::Value> allow_list;
+  base::Value::List allow_list;
   policies.Set(key::kSSLErrorOverrideAllowedForOrigins, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(allow_list)), nullptr);
@@ -332,8 +332,8 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
                nullptr);
   // Add a policy to allow overriding on specific sites only. The path should be
   // ignored.
-  std::vector<base::Value> allow_list;
-  allow_list.emplace_back(base::Value("127.0.0.1/my/path/to/file.ext"));
+  base::Value::List allow_list;
+  allow_list.Append("127.0.0.1/my/path/to/file.ext");
   policies.Set(key::kSSLErrorOverrideAllowedForOrigins, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
                base::Value(std::move(allow_list)), nullptr);
