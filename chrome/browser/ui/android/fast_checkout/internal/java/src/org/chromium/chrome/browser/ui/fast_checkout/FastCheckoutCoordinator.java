4c23319090f4d3375cddb9e56ecd35efdf5d5848
Norge Vizcay <vizcay@google.com>
2022-06-27 10:52:24 +0000

    [FastCheckout] Add Java interface and implementation for coordinator.
    This CL adds the public FastCheckoutComponent interface, which is implemented by the FastCheckoutCoordinator.
    
    The interface allows to request that the FastCheckout component shows
    the list of addresses and credit card options, and ensures that the delegate is notified when users dismiss the sheet without selecting anything.
    
    The Java Coordinator is the feature UI entry point on the Java side. It will be owned by the (upcoming) `FastCheckoutBridge.java`, in charge of communicating directives back and forth with the C++ side of the bridge.
    
    
    
    Bug: 1334642
    Change-Id: I51890633e20ef6a5093ab5156fab336a63f1296a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706867
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018169}

---
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutCoordinator.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.fast_checkout;
+
+import android.content.Context;
+
+import org.chromium.chrome.browser.ui.fast_checkout.data.FastCheckoutAutofillProfile;
+import org.chromium.chrome.browser.ui.fast_checkout.data.FastCheckoutCreditCard;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+
+class FastCheckoutCoordinator implements FastCheckoutComponent {
+    @Override
+    public void initialize(Context context, BottomSheetController sheetController,
+            FastCheckoutComponent.Delegate delegate) {
+        // TODO(crbug.com/1334642): Implement.
+    }
+
+    @Override
+    public void showOptions(
+            FastCheckoutAutofillProfile[] profiles, FastCheckoutCreditCard[] creditCards) {
+        // TODO(crbug.com/1334642): Implement.
+    }
+}
