19965c7cfc53b4ebd8fb6f221a704ad821082a6f
Jeroen Dhollander <jeroendh@google.com>
2022-06-27 11:03:47 +0000

    Revert "Prevent unsafe narrowing: base/"
    This reverts commit 5d4fcae17b4f7f2b9838ff1c9396b78962095d4b.
    
    Reason for revert: Breaks MSAN build. See crbug/1339749
    
    Original change's description:
    > Prevent unsafe narrowing: base/
    >
    > Bug: 1292951
    > Change-Id: I17b223cfd5bd63752a20b36ffb446d4dc63a2733
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717404
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Owners-Override: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1017830}
    
    Bug: 1292951
    Bug: 1339749
    Change-Id: I210221bb692ae43bc9d2aaa31ff8117bea8405d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726010
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1018171}

--- base/pickle.cc
+++ base/pickle.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 // static
-const size_t Pickle::kPayloadUnit = 64;
+const int Pickle::kPayloadUnit = 64;
 
 static const size_t kCapacityReadOnly = static_cast<size_t>(-1);
 
@@ -58,8 +58,9 @@ inline const char* PickleIterator::GetReadPointerAndAdvance() {
   return current_read_ptr;
 }
 
-const char* PickleIterator::GetReadPointerAndAdvance(size_t num_bytes) {
-  if (num_bytes > end_index_ - read_index_) {
+const char* PickleIterator::GetReadPointerAndAdvance(int num_bytes) {
+  if (num_bytes < 0 ||
+      end_index_ - read_index_ < static_cast<size_t>(num_bytes)) {
     read_index_ = end_index_;
     return nullptr;
   }
@@ -69,10 +70,10 @@ const char* PickleIterator::GetReadPointerAndAdvance(size_t num_bytes) {
 }
 
 inline const char* PickleIterator::GetReadPointerAndAdvance(
-    size_t num_elements,
+    int num_elements,
     size_t size_element) {
-  // Check for size_t overflow.
-  size_t num_bytes;
+  // Check for int32_t overflow.
+  int num_bytes;
   if (!CheckMul(num_elements, size_element).AssignIfValid(&num_bytes))
     return nullptr;
   return GetReadPointerAndAdvance(num_bytes);
@@ -139,8 +140,8 @@ bool PickleIterator::ReadDouble(double* result) {
 }
 
 bool PickleIterator::ReadString(std::string* result) {
-  size_t len;
-  if (!ReadLength(&len))
+  int len;
+  if (!ReadInt(&len))
     return false;
   const char* read_from = GetReadPointerAndAdvance(len);
   if (!read_from)
@@ -151,8 +152,8 @@ bool PickleIterator::ReadString(std::string* result) {
 }
 
 bool PickleIterator::ReadStringPiece(StringPiece* result) {
-  size_t len;
-  if (!ReadLength(&len))
+  int len;
+  if (!ReadInt(&len))
     return false;
   const char* read_from = GetReadPointerAndAdvance(len);
   if (!read_from)
@@ -163,8 +164,8 @@ bool PickleIterator::ReadStringPiece(StringPiece* result) {
 }
 
 bool PickleIterator::ReadString16(std::u16string* result) {
-  size_t len;
-  if (!ReadLength(&len))
+  int len;
+  if (!ReadInt(&len))
     return false;
   const char* read_from = GetReadPointerAndAdvance(len, sizeof(char16_t));
   if (!read_from)
@@ -175,8 +176,8 @@ bool PickleIterator::ReadString16(std::u16string* result) {
 }
 
 bool PickleIterator::ReadStringPiece16(StringPiece16* result) {
-  size_t len;
-  if (!ReadLength(&len))
+  int len;
+  if (!ReadInt(&len))
     return false;
   const char* read_from = GetReadPointerAndAdvance(len, sizeof(char16_t));
   if (!read_from)
@@ -186,11 +187,11 @@ bool PickleIterator::ReadStringPiece16(StringPiece16* result) {
   return true;
 }
 
-bool PickleIterator::ReadData(const char** data, size_t* length) {
+bool PickleIterator::ReadData(const char** data, int* length) {
   *length = 0;
   *data = nullptr;
 
-  if (!ReadLength(length))
+  if (!ReadInt(length))
     return false;
 
   return ReadBytes(data, *length);
@@ -198,7 +199,7 @@ bool PickleIterator::ReadData(const char** data, size_t* length) {
 
 bool PickleIterator::ReadData(base::span<const uint8_t>* data) {
   const char* ptr;
-  size_t length;
+  int length;
 
   if (!ReadData(&ptr, &length))
     return false;
@@ -207,7 +208,7 @@ bool PickleIterator::ReadData(base::span<const uint8_t>* data) {
   return true;
 }
 
-bool PickleIterator::ReadBytes(const char** data, size_t length) {
+bool PickleIterator::ReadBytes(const char** data, int length) {
   const char* read_from = GetReadPointerAndAdvance(length);
   if (!read_from)
     return false;
@@ -232,12 +233,12 @@ Pickle::Pickle()
   header_->payload_size = 0;
 }
 
-Pickle::Pickle(size_t header_size)
+Pickle::Pickle(int header_size)
     : header_(nullptr),
       header_size_(bits::AlignUp(header_size, sizeof(uint32_t))),
       capacity_after_header_(0),
       write_offset_(0) {
-  DCHECK_GE(header_size, sizeof(Header));
+  DCHECK_GE(static_cast<size_t>(header_size), sizeof(Header));
   DCHECK_LE(header_size, kPayloadUnit);
   Resize(kPayloadUnit);
   header_->payload_size = 0;
@@ -248,10 +249,10 @@ Pickle::Pickle(const char* data, size_t data_len)
       header_size_(0),
       capacity_after_header_(kCapacityReadOnly),
       write_offset_(0) {
-  if (data_len >= sizeof(Header))
+  if (data_len >= static_cast<int>(sizeof(Header)))
     header_size_ = data_len - header_->payload_size;
 
-  if (header_size_ > data_len)
+  if (header_size_ > static_cast<unsigned int>(data_len))
     header_size_ = 0;
 
   if (header_size_ != bits::AlignUp(header_size_, sizeof(uint32_t)))
@@ -301,20 +302,22 @@ Pickle& Pickle::operator=(const Pickle& other) {
 }
 
 void Pickle::WriteString(const StringPiece& value) {
-  WriteData(value.data(), value.size());
+  WriteInt(static_cast<int>(value.size()));
+  WriteBytes(value.data(), static_cast<int>(value.size()));
 }
 
 void Pickle::WriteString16(const StringPiece16& value) {
-  WriteInt(checked_cast<int>(value.size()));
-  WriteBytes(value.data(), value.size() * sizeof(char16_t));
+  WriteInt(static_cast<int>(value.size()));
+  WriteBytes(value.data(), static_cast<int>(value.size()) * sizeof(char16_t));
 }
 
-void Pickle::WriteData(const char* data, size_t length) {
-  WriteInt(checked_cast<int>(length));
+void Pickle::WriteData(const char* data, int length) {
+  DCHECK_GE(length, 0);
+  WriteInt(length);
   WriteBytes(data, length);
 }
 
-void Pickle::WriteBytes(const void* data, size_t length) {
+void Pickle::WriteBytes(const void* data, int length) {
   WriteBytesCommon(data, length);
 }
 
--- base/power_monitor/moving_average.cc
+++ base/power_monitor/moving_average.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 #include "base/check_op.h"
-#include "base/numerics/clamped_math.h"
 
 namespace {
 constexpr int kIntMax = std::numeric_limits<int>::max();
@@ -36,17 +35,15 @@ void MovingAverage::AddSample(int sample) {
 }
 
 int MovingAverage::GetAverageRoundedDown() const {
-  if (Size() == 0 || uint64_t{Size()} > static_cast<uint64_t>(kInt64Max)) {
+  if (Size() == 0)
     return 0;
-  }
-  return static_cast<int>(sum_ / static_cast<int64_t>(Size()));
+  return sum_ / Size();
 }
 
 int MovingAverage::GetAverageRoundedToClosest() const {
-  if (Size() == 0 || uint64_t{Size()} > static_cast<uint64_t>(kInt64Max))
+  if (Size() == 0)
     return 0;
-  return static_cast<int>((base::ClampedNumeric<int64_t>(sum_) + Size() / 2) /
-                          static_cast<int64_t>(Size()));
+  return (sum_ + Size() / 2) / Size();
 }
 
 double MovingAverage::GetUnroundedAverage() const {
