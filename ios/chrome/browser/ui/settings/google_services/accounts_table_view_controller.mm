26f658900ba2a04c6e6e961ce821027a3d0af708
jlebel <jlebel@chromium.org>
2022-06-27 00:11:40 +0000

    [iOS] Block UI while add account dialog is opened
    While add account dialog is opened, the UI needs to be block since the
    dialog might not appear right away.
    This patch adds following calls around the add account dialog:
    -[SettingsRootTableViewController preventUserInteraction]
    -[SettingsRootTableViewController allowUserInteraction]
    
    AddAccountSigninCoordinator already uses ActivityOverlayCoordinator to
    block the UI, but this doesn't block the navigation bar in the
    settings (see crrev.com/c/3706892).
    
    This patch needs to be simple to be merged into M104. I will fix the
    double UI blockers in the following patch: crbug.com/1338995.
    
    Fixed: 1334017
    Change-Id: I7c05142a58490333e695436bc6414277b0b70323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720515
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018071}

--- ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
@@ -477,6 +477,9 @@ typedef NS_ENUM(NSInteger, ItemType) {
   DCHECK(!self.removeOrMyGoogleChooserAlertCoordinator);
   _authenticationOperationInProgress = YES;
 
+  // TODO(crbug.com/1338990): Remove the following line when todo bug will be
+  // fixed.
+  [self preventUserInteraction];
   __weak __typeof(self) weakSelf = self;
   ShowSigninCommand* command = [[ShowSigninCommand alloc]
       initWithOperation:AuthenticationOperationAddAccount
@@ -491,6 +494,9 @@ typedef NS_ENUM(NSInteger, ItemType) {
 }
 
 - (void)handleDidAddAccount:(BOOL)success {
+  // TODO(crbug.com/1338990): Remove the following line when todo bug will be
+  // fixed.
+  [self allowUserInteraction];
   [self handleAuthenticationOperationDidFinish];
   if (success && _closeSettingsOnAddAccount) {
     [self.dispatcher closeSettingsUI];
