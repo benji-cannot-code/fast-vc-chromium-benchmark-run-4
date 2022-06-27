904c8835f7a4b6927a76160690a96c2312e3d006
Dominic Battre <battre@chromium.org>
2022-06-27 15:50:13 +0000

    Fix a few UI glitches in chrome://autofill-internals
    This CL just fixes a few minor UI glitches that bother me each time
    using the UI:
    - line wrapping between checkboxes and labels
    - line height
    
    It turns this paragon of pulchritude
    https://screenshot.googleplex.com/6tF6KiqZjYmm2vt into this:
    https://screenshot.googleplex.com/5phLy69c8QSqwRF
    
    Bug: 928595
    Change-Id: Id5de80234d12240cd739d0ce40c49dfd004b8837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726449
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018252}

--- components/autofill/core/browser/autofill_and_password_manager_internals/autofill_and_password_manager_internals.js
+++ components/autofill/core/browser/autofill_and_password_manager_internals/autofill_and_password_manager_internals.js
@@ -169,8 +169,7 @@ function setUpPasswordManagerInternals() {
 }
 
 function enableResetCacheButton() {
-  document.getElementById('reset-cache-fake-button').style.display =
-      'inline-block';
+  document.getElementById('reset-cache-fake-button').style.display = 'inline';
 }
 
 function notifyAboutIncognito(isIncognito) {
@@ -295,9 +294,8 @@ function setUpLogDisplayConfig() {
     input.addEventListener('change', changeHandler);
     changeHandler();  // Call once to initialize |logDiv|'s classes.
     const label = document.createElement('label');
-    label.setAttribute('for', `checkbox-${scope}`);
-    label.innerText = scope;
-    checkboxPlaceholder.appendChild(input);
+    label.appendChild(input);
+    label.appendChild(document.createTextNode(' ' + scope));
     checkboxPlaceholder.appendChild(label);
   }
 }
