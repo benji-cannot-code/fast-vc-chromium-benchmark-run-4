b79c1c485e19293e76654056d12b6fff4d0f538a
Ben Reich <benreich@chromium.org>
2022-06-27 02:58:59 +0000

    [FilesTrash] Wire up EmptyTrashIOTask to its UI action, enabling tests
    The EmptyTrashIOTask should now work in the SWA world. This can be wired
    up and the tests disabled on SWA can now be enabled.
    
    Bug: b:235646598, b:189173190
    Test: browser_tests --gtest_filter=*trash*
    Change-Id: I05402b991173b11761ca9dcc9fca7542d75d894d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717610
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018087}

--- ui/file_manager/file_manager/common/js/util.js
+++ ui/file_manager/file_manager/common/js/util.js
@@ -1311,6 +1311,15 @@ util.isRecentsFilterV2Enabled = () => {
 };
 
 /**
+ * Returns true if FilesTrash feature flag is enabled.
+ * @returns {boolean}
+ */
+util.isTrashEnabled = () => {
+  return loadTimeData.valueExists('FILES_TRASH_ENABLED') &&
+      loadTimeData.getBoolean('FILES_TRASH_ENABLED');
+};
+
+/**
  * Returns true if Files SWA feature flag is enabled.
  * @return {boolean}
  */
