f89f51706d03f4fd0e1a7eef5cfb28b082d5bfe9
Zachary Tan <tanzachary@chromium.org>
2022-06-27 16:14:24 +0000

    Merge login() to get()
    https://github.com/fedidcg/FedCM/pull/279
    
    Bug: 1335234
    Change-Id: I3b085cce9eecf2a5e570dae146b42d1b8b0d18db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697448
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018257}

--- third_party/blink/renderer/modules/credentialmanagement/federated_credential.h
+++ third_party/blink/renderer/modules/credentialmanagement/federated_credential.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CredentialRequestOptions;
-class FederatedAccountLoginRequest;
 class FederatedCredentialInit;
 
 class MODULES_EXPORT FederatedCredential final : public Credential {
@@ -35,7 +34,8 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
   static FederatedCredential* Create(const KURL& provider_url,
                                      const String& client_id,
                                      const String& hint,
-                                     const CredentialRequestOptions* options);
+                                     const CredentialRequestOptions* options,
+                                     const String& id_token);
 
   static bool IsRejectingPromiseDueToCSP(ContentSecurityPolicy* policy,
                                          ScriptPromiseResolver* resolver,
@@ -49,7 +49,8 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
   FederatedCredential(const KURL& provider_url,
                       const String& client_id,
                       const String& hint,
-                      const CredentialRequestOptions* options);
+                      const CredentialRequestOptions* options,
+                      const String& id_token);
 
   void Trace(Visitor*) const override;
 
@@ -67,14 +68,14 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
   }
   const String& name() const { return name_; }
   const KURL& iconURL() const { return icon_url_; }
+  const String& idToken() const { return id_token_; }
   const String& protocol() const {
     // TODO(mkwst): This is a stub, as we don't yet have any support on the
     // Chromium-side.
     return g_empty_string;
   }
 
-  ScriptPromise login(ScriptState* script_state,
-                      FederatedAccountLoginRequest* request);
+  ScriptPromise login(ScriptState* script_state);
 
   static ScriptPromise logoutRps(
       ScriptState*,
@@ -87,6 +88,7 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
   const KURL provider_url_;
   const String client_id_;
   Member<const CredentialRequestOptions> options_;
+  const String id_token_;
 };
 
 }  // namespace blink
