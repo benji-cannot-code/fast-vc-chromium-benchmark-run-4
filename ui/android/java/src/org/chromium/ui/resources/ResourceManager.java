8b93bf36f1476afbc4825dd850a875e2d4c76c71
Sky Malice <skym@chromium.org>
2022-06-27 18:15:56 +0000

    Refactor DynamicResources to support pushing bitmaps.
    Should contain no functional changes.
    
    The approach this refactor takes is to leave the existing
    ResourceManager and ResourceLoader infrastructure alone, because they
    already support async requests. DynamicResource does support async
    Bitmap requests, but the interface is a bit awkward. To get around this,
    the thin shim of DynamicResourceWrapper is used to allow
    ViewResourceAdpater's interface to be more convenient.
    
    Bug: 1338202
    Change-Id: I450bb2055f4e5312f0d2f875d98057008ad10792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716983
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018317}

--- ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
+++ ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
@@ -18,7 +18,6 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.display.DisplayAndroid;
 import org.chromium.ui.resources.ResourceLoader.ResourceLoaderCallback;
 import org.chromium.ui.resources.dynamics.BitmapDynamicResource;
-import org.chromium.ui.resources.dynamics.DynamicResource;
 import org.chromium.ui.resources.dynamics.DynamicResourceLoader;
 import org.chromium.ui.resources.statics.StaticResourceLoader;
 import org.chromium.ui.resources.system.SystemResourceLoader;
@@ -81,7 +80,7 @@ public class ResourceManager implements ResourceLoaderCallback {
 
     /**
      * @return A reference to the {@link DynamicResourceLoader} that provides
-     *         {@link DynamicResource} objects to this class.
+     *         {@link Resource} objects to this class.
      */
     public DynamicResourceLoader getDynamicResourceLoader() {
         return (DynamicResourceLoader) mResourceLoaders.get(
---
+++ ui/android/java/src/org/chromium/ui/resources/dynamics/DynamicResourceSnapshot.java
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.resources.dynamics;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
+import org.chromium.ui.resources.Resource;
+import org.chromium.ui.resources.statics.NinePatchData;
+
+/** The current state of a dynamic resource. */
+public class DynamicResourceSnapshot implements Resource {
+    private final Bitmap mBitmap;
+    private final boolean mShouldRemoveResourceOnNullBitmap;
+    private final Rect mBitmapSize;
+    private final long mNativeResourceId;
+
+    public DynamicResourceSnapshot(Bitmap bitmap, boolean shouldRemoveResourceOnNullBitmap,
+            Rect bitmapSize, long nativeResourceId) {
+        mBitmap = bitmap;
+        mShouldRemoveResourceOnNullBitmap = shouldRemoveResourceOnNullBitmap;
+        mBitmapSize = bitmapSize;
+        mNativeResourceId = nativeResourceId;
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return mBitmap;
+    }
+
+    @Override
+    public boolean shouldRemoveResourceOnNullBitmap() {
+        return mShouldRemoveResourceOnNullBitmap;
+    }
+
+    @Override
+    public Rect getBitmapSize() {
+        return mBitmapSize;
+    }
+
+    @Override
+    public NinePatchData getNinePatchData() {
+        return null;
+    }
+
+    @Override
+    public long createNativeResource() {
+        return mNativeResourceId;
+    }
+}
