a0178bc48f4267ae6bf7cdcfbb191a5be807afd9
Martin Bidlingmaier <mbid@google.com>
2022-06-27 12:59:52 +0000

    [CrOS] Add fake impl for AuthFactorConfig mojo service
    This commit adds a fake implementation of the AuthFactorConfig and
    RecoveryFactorEditor services. The fake implementation only maintains a
    boolean corresponding to the current state. No changes are sent to
    cryptohome. This fake implementation is available iff the
    CryptohomeRecoverySetup feature flag is enabled.
    
    Bug: 1327627
    Change-Id: I6c9a27c20c6ddf8e64d8257b8756a1d2c556838d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704504
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Auto-Submit: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018197}

---
+++ chromeos/ash/services/auth_factor_config/recovery_factor_editor.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/services/auth_factor_config/recovery_factor_editor.h"
+#include "ash/constants/ash_features.h"
+#include "chromeos/ash/services/auth_factor_config/auth_factor_config.h"
+
+namespace ash::auth {
+
+RecoveryFactorEditor::RecoveryFactorEditor(AuthFactorConfig* auth_factor_config)
+    : auth_factor_config_(auth_factor_config) {}
+RecoveryFactorEditor::~RecoveryFactorEditor() = default;
+
+void RecoveryFactorEditor::BindReceiver(
+    mojo::PendingReceiver<mojom::RecoveryFactorEditor> receiver) {
+  receivers_.Add(this, std::move(receiver));
+}
+
+void RecoveryFactorEditor::Configure(
+    const std::string& auth_token,
+    bool enabled,
+    base::OnceCallback<void(ConfigureResult)> callback) {
+  if (!features::IsCryptohomeRecoverySetupEnabled()) {
+    // Log always, crash on debug builds.
+    LOG(ERROR) << "AuthFactorConfig::Configure is a fake";
+    NOTIMPLEMENTED();
+    std::move(callback).Run(ConfigureResult::kClientError);
+    return;
+  }
+
+  auth_factor_config_->recovery_configured_ = enabled;
+  std::move(callback).Run(ConfigureResult::kSuccess);
+  auth_factor_config_->NotifyFactorObservers(mojom::AuthFactor::kRecovery);
+}
+
+}  // namespace ash::auth
