c9b7c0d6953296dc7b0afabee65ebbbcc40765af
bttk <bttk@google.com>
2022-06-27 20:34:53 +0000

    Revert "Remove partnerbookmarks from AppHooks"
    This reverts commit 8e847d0c7d98bedcf2972f02e92ea607885dd396.
    
    Reason for revert: crbug.com/1339248
    
    Original change's description:
    > Remove partnerbookmarks from AppHooks
    >
    > Bug: 1291655
    > Change-Id: I777e1552dff8aacb8fc1008940d3fc44b832023a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669962
    > Quick-Run: Brandon Wylie <wylieb@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: bttk - <bttk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1010299}
    
    Bug: 1291655
    Change-Id: I69aab0f2912d8a523e0d285ef17689e393201e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727417
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: bttk - <bttk@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018391}

--- chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksShim.java
+++ chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksShim.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.partnerbookmarks;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 
+import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.components.version_info.VersionInfo;
 
@@ -34,7 +35,7 @@ public class PartnerBookmarksShim {
 
         PartnerBookmarksReader reader =
                 new PartnerBookmarksReader(context, PartnerBrowserCustomizations.getInstance(),
-                        new PartnerBookmarksDelegateImpl()::createIterator);
+                        AppHooks.get()::getPartnerBookmarkIterator);
 
         boolean systemOrPreStable =
                 (context.getApplicationInfo().flags & ApplicationInfo.FLAG_SYSTEM) == 1
