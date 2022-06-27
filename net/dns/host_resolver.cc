5ca6949b83594347feb45e6fd42f1bdc39fb863f
Tsuyoshi Horo <horo@chromium.org>
2022-06-27 09:50:21 +0000

    Use NOTREACHED() in GetExperimentalResultsForTesting()
    IMMEDIATE_CRASH() in GetExperimentalResultsForTesting() is making it
    difficult to find the actual reason of crash, because the code is
    optimized by the compiler.
    
    So this CL changes IMMEDIATE_CRASH() to NOTREACHED().
    
    Bug: 1284770, 1283071
    Change-Id: I2fe8ede6186f35c0df7e427d43246eb2579ef0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3725735
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018152}

--- net/dns/host_resolver.cc
+++ net/dns/host_resolver.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check.h"
-#include "base/immediate_crash.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
@@ -98,7 +97,8 @@ bool EndpointResultIsNonProtocol(const HostResolverEndpointResult& result) {
 
 const std::vector<bool>*
 HostResolver::ResolveHostRequest::GetExperimentalResultsForTesting() const {
-  IMMEDIATE_CRASH();
+  NOTREACHED();
+  return nullptr;
 }
 
 const size_t HostResolver::ManagerOptions::kDefaultRetryAttempts =
