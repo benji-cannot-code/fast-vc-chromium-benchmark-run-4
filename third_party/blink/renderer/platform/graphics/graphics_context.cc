24b6cfb4af23e05f16b149376f325e9c9d5d18b2
Xianzhu Wang <wangxianzhu@chromium.org>
2022-06-27 16:56:43 +0000

    Uniform gaps in a dashed/dotted stroke along closed path
    Now leave a gap between the first dash/dot and the last dash/dot
    for a dashed/dotted stroke along a closed path (e.g. a rounded
    border).
    
    Fixed: 864367
    Change-Id: Ib9f1a4e221ee0282ef743d230b354c3707d3fe93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724101
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018277}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -1077,7 +1077,8 @@ void GraphicsContext::StrokePath(const Path& path_to_stroke,
     return;
 
   DrawPath(path_to_stroke.GetSkPath(),
-           ImmutableState()->StrokeFlags(length, dash_thickness),
+           ImmutableState()->StrokeFlags(length, dash_thickness,
+                                         path_to_stroke.IsClosed()),
            auto_dark_mode);
 }
 
