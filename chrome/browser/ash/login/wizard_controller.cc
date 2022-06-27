02317ff04f1eb663114a32440fdbf3aec07bb4ad
Roman Aleksandrov <raleksandrov@google.com>
2022-06-27 10:48:39 +0000

    oobe: Fix Network screen exit status
    If screen is skipped it should exit with NOT_APPLICABLE exit code.
    
    Fixed: 1324144
    Change-Id: I5eb310206c63d61dd0a2c4ec5691a41d702cfe41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721017
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018167}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -1398,6 +1398,7 @@ void WizardController::OnNetworkScreenExit(NetworkScreen::Result result) {
       ShowEulaScreen();
       break;
     case NetworkScreen::Result::CONNECTED_DEMO:
+    case NetworkScreen::Result::NOT_APPLICABLE_CONNECTED_DEMO:
       DCHECK(demo_setup_controller_);
       demo_setup_controller_->set_demo_config(
           DemoSession::DemoModeConfig::kOnline);
@@ -1411,6 +1412,8 @@ void WizardController::OnNetworkScreenExit(NetworkScreen::Result result) {
       InitiateOOBEUpdate();
       break;
     case NetworkScreen::Result::CONNECTED_DEMO_CONSOLIDATED_CONSENT:
+    case NetworkScreen::Result::
+        NOT_APPLICABLE_CONNECTED_DEMO_CONSOLIDATED_CONSENT:
       DCHECK(demo_setup_controller_);
       demo_setup_controller_->set_demo_config(
           DemoSession::DemoModeConfig::kOnline);
