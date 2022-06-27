f53abf2b3e29806f54798623222725e95f26ccbb
Fritz Koenig <frkoenig@chromium.org>
2022-06-27 15:12:41 +0000

    media/gpu/v4l2: Fill out bytesused when queueing compressed data
    V4L2 drivers need to know the size of the compressed data
    so that they don't continue reading.
    
    BUG=b:222352323
    TEST=tast run cherry_dut video.PlatformDecoding.v4l2_stateless_vp9_0_group1_buf
    
    Change-Id: I77e843fa7d6937c8c83777a946a3c4138c295c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722353
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018238}

--- media/gpu/v4l2/test/v4l2_ioctl_shim.h
+++ media/gpu/v4l2/test/v4l2_ioctl_shim.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_GPU_V4L2_TEST_V4L2_IOCTL_SHIM_H_
 
 #include <linux/videodev2.h>
+#include <string.h>
 
 #include "base/files/memory_mapped_file.h"
+#include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -26,17 +28,22 @@ class MmapedBuffer : public base::RefCounted<MmapedBuffer> {
   class MmapedPlane {
    public:
     void* start_addr;
-    size_t length;
+    const size_t length;
+    size_t bytes_used;
 
-    MmapedPlane(void* start, size_t len) {
-      start_addr = start;
-      length = len;
+    MmapedPlane(void* start, size_t len) : start_addr(start), length(len){};
+
+    void CopyIn(const uint8_t* frame_data, size_t frame_size) {
+      LOG_ASSERT(frame_size < length)
+          << "Not enough memory allocated to copy into.";
+      bytes_used = frame_size;
+      memcpy(static_cast<uint8_t*>(start_addr), frame_data, frame_size);
     }
   };
 
   using MmapedPlanes = std::vector<MmapedPlane>;
 
-  MmapedPlanes mmaped_planes() const { return mmaped_planes_; }
+  MmapedPlanes& mmaped_planes() { return mmaped_planes_; }
 
   uint32_t buffer_id() const { return buffer_id_; }
   void set_buffer_id(uint32_t buffer_id) { buffer_id_ = buffer_id; }
