4440764039811f05a1d369669aa9cf3676cbb439
Peter Boström <pbos@chromium.org>
2022-06-27 17:58:28 +0000

    Revert "prerender: Make WebNavigation be aware of prerendering."
    This reverts commit aa5328fe0006e04c0b69b2c7ea68f380ca572881.
    
    Reason for revert: Top crasher for DCHECK-enabled builds in the wild.
    
    Bug: 1339859
    
    Original change's description:
    > prerender: Make WebNavigation be aware of prerendering.
    >
    > This CL makes WebNavigation be aware of prerendering frames and add a
    > relate test.
    >
    > Bug: 1278141
    > Change-Id: I5d46eead97eafce90dd4988b6200a55bf026fe4d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3692509
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1017914}
    
    Bug: 1278141
    Change-Id: Icd0d08730867ca4823cacd0467fdc8508aa742d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727249
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018305}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -249,13 +249,7 @@ class WebNavigationApiTestWithContextType
     : public WebNavigationApiTest,
       public testing::WithParamInterface<ContextType> {
  public:
-  WebNavigationApiTestWithContextType() : WebNavigationApiTest(GetParam()) {
-    feature_list_.InitWithFeaturesAndParameters(
-        {
-            {blink::features::kPrerender2, {}},
-        },
-        {});
-  }
+  WebNavigationApiTestWithContextType() : WebNavigationApiTest(GetParam()) {}
   ~WebNavigationApiTestWithContextType() override = default;
   WebNavigationApiTestWithContextType(
       const WebNavigationApiTestWithContextType&) = delete;
@@ -268,9 +262,6 @@ class WebNavigationApiTestWithContextType
     return RunExtensionTest(name, {},
                             {.allow_in_incognito = allow_in_incognito});
   }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, Api) {
@@ -278,7 +269,6 @@ IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, Api) {
 }
 
 IN_PROC_BROWSER_TEST_P(WebNavigationApiTestWithContextType, GetFrame) {
-  ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webnavigation/getFrame")) << message_;
 }
 
