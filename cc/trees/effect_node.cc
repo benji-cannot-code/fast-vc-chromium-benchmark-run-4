chromium/chromium

commit 93a0e962f03797cad07b8a87331942d9e655b9a2

gfx: Add LinearGradient to MaskFilterInfo
* Steps to up 6. This number is necessary for scrollable shelf.
* Support all angles
* Rotation on the same layer is not supported.
* Uses clip bounds's diagonal as start/end.

Design doc: https://docs.google.com/document/d/1AXgfk66ROEr225roqAuPxrqWNsnDh91lHCQJ_cw8RrI/edit?usp=sharing

      LayerAnimationElementTest.GradientMaskElement
      LayerWithDelegateTest.GradientMask
      LinearGradientTest*,
      PropertyTreeBuilderTest.GradientMask,
      PropertyTreeBuilderTest.NestedGradientMask

Bug: 1039003
Test: DrawPropertiesTestWithLayerTree.GradientMaskOnRenderSurface
Change-Id: I119c0576a8d525d462518bbba0c22c335c274da7
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3483177
Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
Reviewed-by: Robert Sesek <rsesek@chromium.org>
Commit-Queue: Sasha McIntosh <sashamcintosh@chromium.org>
Cr-Commit-Position: refs/heads/main@{#987597}

cc/trees/effect_node.cc
@@ -134,6 +134,8 @@ const char* RenderSurfaceReasonToString(RenderSurfaceReason reason) {
       return "backdrop filter animation";
     case RenderSurfaceReason::kRoundedCorner:
       return "rounded corner";
+    case RenderSurfaceReason::kGradientMask:
+      return "gradient mask";
     case RenderSurfaceReason::kClipPath:
       return "clip path";
     case RenderSurfaceReason::kClipAxisAlignment:
@@ -183,6 +185,10 @@ void EffectNode::AsValueInto(base::trace_event::TracedValue* value) const {
           mask_filter_info.rounded_corner_bounds(), value);
       value->SetBoolean("mask_filter_is_fast_rounded_corner",
                         is_fast_rounded_corner);
+    }
+    if (mask_filter_info.HasGradientMask()) {
+      MathUtil::AddToTracedValue("mask_filter_gradient_mask",
+                                 mask_filter_info.gradient_mask(), value);
     }
   }
   value->SetString("blend_mode", SkBlendMode_Name(blend_mode));

ui/gfx/geometry/mask_filter_info.cc
@@ -11,13 +11,19 @@
 namespace gfx {
 
 bool MaskFilterInfo::Transform(const gfx::Transform& transform) {
-  return rounded_corner_bounds_.IsEmpty()
-             ? false
-             : transform.TransformRRectF(&rounded_corner_bounds_);
+  if (rounded_corner_bounds_.IsEmpty())
+    return false;
+
+  if (!transform.TransformRRectF(&rounded_corner_bounds_))
+    return false;
+
+  gradient_mask_.Transform(transform);
+  return true;
 }
 
 std::string MaskFilterInfo::ToString() const {
-  return "MaskFilterInfo{" + rounded_corner_bounds_.ToString() + "}";
+  return "MaskFilterInfo{" + rounded_corner_bounds_.ToString() +
+         ", gradient_mask=" + gradient_mask_.ToString() + "}";
 }
 
 }  // namespace gfx

ui/gfx/geometry/mask_filter_info.h
@@ -6,6 +6,7 @@
 #define UI_GFX_GEOMETRY_MASK_FILTER_INFO_H_
 
 #include "ui/gfx/geometry/geometry_skia_export.h"
+#include "ui/gfx/geometry/linear_gradient.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/rrect_f.h"
 
@@ -19,8 +20,12 @@ class GEOMETRY_SKIA_EXPORT MaskFilterInfo {
   MaskFilterInfo() = default;
   explicit MaskFilterInfo(const RRectF& rrect)
       : rounded_corner_bounds_(rrect) {}
-  MaskFilterInfo(const RectF& bounds, const RoundedCornersF& radii)
-      : rounded_corner_bounds_(bounds, radii) {}
+  MaskFilterInfo(const RRectF& rrect, const gfx::LinearGradient& gradient_mask)
+      : rounded_corner_bounds_(rrect), gradient_mask_(gradient_mask) {}
+  MaskFilterInfo(const RectF& bounds,
+                 const RoundedCornersF& radii,
+                 const gfx::LinearGradient& gradient_mask)
+      : rounded_corner_bounds_(bounds, radii), gradient_mask_(gradient_mask) {}
   MaskFilterInfo(const MaskFilterInfo& copy) = default;
   ~MaskFilterInfo() = default;
 
@@ -36,6 +41,13 @@ class GEOMETRY_SKIA_EXPORT MaskFilterInfo {
            rounded_corner_bounds_.GetType() != RRectF::Type::kRect;
   }
 
+  const gfx::LinearGradient& gradient_mask() const { return gradient_mask_; }
+
+  // True if this contains an effective gradient mask (requires filter bounds).
+  bool HasGradientMask() const {
+    return !rounded_corner_bounds_.IsEmpty() && !gradient_mask_.IsEmpty();
+  }
+
   // True if this contains no effective mask information.
   bool IsEmpty() const { return rounded_corner_bounds_.IsEmpty(); }
 
@@ -49,6 +61,9 @@ class GEOMETRY_SKIA_EXPORT MaskFilterInfo {
   // The rounded corner bounds. This also defines the bounds that the mask
   // filter will be applied to.
   RRectF rounded_corner_bounds_;
+
+  // Shader based linear gradient mask to be applied to a layer.
+  gfx::LinearGradient gradient_mask_;
 };
 
 inline bool operator==(const MaskFilterInfo& lhs, const MaskFilterInfo& rhs) {

ui/gfx/mojom/mask_filter_info.mojom
@@ -5,8 +5,10 @@
 module gfx.mojom;
 
 import "ui/gfx/mojom/rrect_f.mojom";
+import "ui/gfx/mojom/linear_gradient.mojom";
 
-// See ui/gfx/mask_filter_info.h.
+// See ui/gfx/geometry/mask_filter_info.h.
 struct MaskFilterInfo {
   gfx.mojom.RRectF rounded_corner_bounds;
+  gfx.mojom.LinearGradient? gradient_mask;
 };

ui/gfx/mojom/mask_filter_info_mojom_traits.cc
@@ -12,7 +12,15 @@ bool StructTraits<gfx::mojom::MaskFilterInfoDataView, gfx::MaskFilterInfo>::
   gfx::RRectF bounds;
   if (!data.ReadRoundedCornerBounds(&bounds))
     return false;
-  *out = gfx::MaskFilterInfo(bounds);
+
+  absl::optional<gfx::LinearGradient> gradient_mask;
+  if (!data.ReadGradientMask(&gradient_mask))
+    return false;
+
+  if (gradient_mask.has_value() && !gradient_mask->IsEmpty())
+    *out = gfx::MaskFilterInfo(bounds, gradient_mask.value());
+  else
+    *out = gfx::MaskFilterInfo(bounds);
   return true;
 }
 

ui/gfx/mojom/mask_filter_info_mojom_traits.h
@@ -5,7 +5,9 @@
 #ifndef UI_GFX_MOJOM_MASK_FILTER_INFO_MOJOM_TRAITS_H_
 #define UI_GFX_MOJOM_MASK_FILTER_INFO_MOJOM_TRAITS_H_
 
+#include "ui/gfx/geometry/linear_gradient.h"
 #include "ui/gfx/geometry/mask_filter_info.h"
+#include "ui/gfx/mojom/linear_gradient_mojom_traits.h"
 #include "ui/gfx/mojom/mask_filter_info.mojom-shared.h"
 #include "ui/gfx/mojom/rrect_f_mojom_traits.h"
 
@@ -15,6 +17,11 @@ struct StructTraits<gfx::mojom::MaskFilterInfoDataView, gfx::MaskFilterInfo> {
   static const gfx::RRectF& rounded_corner_bounds(
       const gfx::MaskFilterInfo& info) {
     return info.rounded_corner_bounds();
+  }
+
+  static const gfx::LinearGradient& gradient_mask(
+      const gfx::MaskFilterInfo& info) {
+    return info.gradient_mask();
   }
 
   static bool Read(gfx::mojom::MaskFilterInfoDataView data,

cc/trees/property_tree_builder_unittest.cc
@@ -42,6 +42,9 @@ class PropertyTreeBuilderTest : public LayerTreeImplTestBase,
   RenderSurfaceImpl* GetRenderSurfaceImpl(const scoped_refptr<Layer>& layer) {
     return GetRenderSurface(ImplOf(layer));
   }
+  LayerImpl* GetLayerImpl(const scoped_refptr<Layer>& layer) {
+    return host()->host_impl()->active_tree()->LayerById(layer->id());
+  }
 
   // Updates main thread draw properties, commits main thread tree to
   // impl-side pending tree, and updates pending tree draw properties.
@@ -745,6 +748,252 @@ TEST_F(PropertyTreeBuilderTest, RenderSurfaceListForTrilinearFiltering) {
   // scale matrix to (0,0 25x25).
   EXPECT_EQ(gfx::RectF(0, 0, 25, 25),
             GetRenderSurfaceImpl(parent)->DrawableContentRect());
+}
+
+TEST_F(PropertyTreeBuilderTest, GradientMask) {
+  auto root = Layer::Create();
+  host()->SetRootLayer(root);
+  root->SetBounds(gfx::Size(200, 200));
+  root->SetIsDrawable(true);
+
+  auto child1 = Layer::Create();
+  root->AddChild(child1);
+  child1->SetBounds(gfx::Size(100, 100));
+  child1->SetIsDrawable(true);
+
+  gfx::LinearGradient gradient_mask(45);
+  gradient_mask.AddStep(50, 0x50);
+  child1->SetGradientMask(gradient_mask);
+
+  // Without render surface.
+  CommitAndActivate();
+  {
+    auto* effect_node1 = GetEffectNode(child1.get());
+    EXPECT_FALSE(effect_node1->mask_filter_info.HasRoundedCorners());
+    EXPECT_EQ(gfx::RectF(100, 100), effect_node1->mask_filter_info.bounds());
+    EXPECT_TRUE(effect_node1->mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gradient_mask, effect_node1->mask_filter_info.gradient_mask());
+    EXPECT_FALSE(effect_node1->HasRenderSurface());
+    auto* layer_impl1 = GetLayerImpl(child1);
+    EXPECT_TRUE(
+        layer_impl1->draw_properties().mask_filter_info.HasGradientMask());
+  }
+
+  // Scale and translate should work.
+  gfx::Transform scale_and_translate_transform;
+  scale_and_translate_transform.Translate({10.f, 10.f});
+  scale_and_translate_transform.Scale(3.f, 2.f);
+  child1->SetTransform(scale_and_translate_transform);
+  CommitAndActivate();
+  {
+    // |mask_info| is in the coordinate space of the transform node associated
+    // with this effect node.
+    auto* effect_node1 = GetEffectNode(child1.get());
+    EXPECT_FALSE(effect_node1->mask_filter_info.HasRoundedCorners());
+    EXPECT_TRUE(effect_node1->mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gfx::RectF(100, 100), effect_node1->mask_filter_info.bounds());
+    EXPECT_EQ(gradient_mask, effect_node1->mask_filter_info.gradient_mask());
+    EXPECT_FALSE(effect_node1->HasRenderSurface());
+
+    // |mask_info| coordinates are in the target space of the layer.
+    auto* layer_impl1 = GetLayerImpl(child1);
+    EXPECT_FALSE(layer_impl1->draw_properties().mask_filter_info.IsEmpty());
+    EXPECT_FALSE(
+        layer_impl1->draw_properties().mask_filter_info.HasRoundedCorners());
+    EXPECT_TRUE(
+        layer_impl1->draw_properties().mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gfx::RectF(10, 10, 300, 200),
+              layer_impl1->draw_properties().mask_filter_info.bounds());
+    // |angle| is updated by the scale transform.
+    EXPECT_EQ(33, layer_impl1->draw_properties()
+                      .mask_filter_info.gradient_mask()
+                      .angle());
+    EXPECT_EQ(gradient_mask.steps(), layer_impl1->draw_properties()
+                                         .mask_filter_info.gradient_mask()
+                                         .steps());
+  }
+
+  // Rotate transform eliminates gradient mask.
+  gfx::Transform rotate_transform;
+  rotate_transform.Rotate(45);
+  child1->SetTransform(rotate_transform);
+  CommitAndActivate();
+  {
+    auto* layer_impl1 = GetLayerImpl(child1);
+    EXPECT_EQ(gfx::RRectF::Type::kEmpty,
+              layer_impl1->draw_properties()
+                  .mask_filter_info.rounded_corner_bounds()
+                  .GetType());
+    EXPECT_FALSE(
+        layer_impl1->draw_properties().mask_filter_info.HasGradientMask());
+  }
+
+  // Reset transform
+  child1->SetTransform(gfx::Transform());
+
+  // A child layer will create a render surface.
+  auto grand_child1 = Layer::Create();
+  child1->AddChild(grand_child1);
+  grand_child1->SetBounds(gfx::Size(100, 100));
+  grand_child1->SetIsDrawable(true);
+  CommitAndActivate();
+  EXPECT_TRUE(GetEffectNode(child1.get())->HasRenderSurface());
+  {
+    auto* effect_node1 = GetEffectNode(child1.get());
+    EXPECT_TRUE(effect_node1->mask_filter_info.HasGradientMask());
+    EXPECT_FALSE(effect_node1->mask_filter_info.HasRoundedCorners());
+    EXPECT_EQ(gfx::RectF(100, 100), effect_node1->mask_filter_info.bounds());
+    EXPECT_EQ(gradient_mask, effect_node1->mask_filter_info.gradient_mask());
+    EXPECT_TRUE(effect_node1->HasRenderSurface());
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().IsEmpty());
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().HasRoundedCorners());
+    EXPECT_TRUE(render_surface_impl1->mask_filter_info().HasGradientMask());
+    EXPECT_EQ(gfx::RectF(100, 100),
+              render_surface_impl1->mask_filter_info().bounds());
+    EXPECT_EQ(gradient_mask,
+              render_surface_impl1->mask_filter_info().gradient_mask());
+  }
+
+  child1->SetTransform(scale_and_translate_transform);
+  CommitAndActivate();
+  {
+    // |mask_info| is in the coordinate space of the transform node associated
+    // with this effect node.
+    auto* effect_node1 = GetEffectNode(child1.get());
+    EXPECT_TRUE(effect_node1->mask_filter_info.HasGradientMask());
+    EXPECT_FALSE(effect_node1->mask_filter_info.HasRoundedCorners());
+    EXPECT_EQ(gfx::RectF(100, 100), effect_node1->mask_filter_info.bounds());
+    EXPECT_EQ(gradient_mask, effect_node1->mask_filter_info.gradient_mask());
+    EXPECT_TRUE(effect_node1->HasRenderSurface());
+
+    // |mask_info| coordinates are in the target space of the render surface's
+    // layer.
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().IsEmpty());
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().HasRoundedCorners());
+    EXPECT_TRUE(render_surface_impl1->mask_filter_info().HasGradientMask());
+    EXPECT_EQ(gfx::RectF(10, 10, 300, 200),
+              render_surface_impl1->mask_filter_info().bounds());
+    // |angle| is updated by the scale transform.
+    EXPECT_EQ(33,
+              render_surface_impl1->mask_filter_info().gradient_mask().angle());
+    EXPECT_EQ(gradient_mask.steps(),
+              render_surface_impl1->mask_filter_info().gradient_mask().steps());
+  }
+
+  // Rotate transform eliminates gradient mask.
+  child1->SetTransform(rotate_transform);
+  CommitAndActivate();
+  {
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_EQ(gfx::RRectF::Type::kEmpty,
+              render_surface_impl1->mask_filter_info()
+                  .rounded_corner_bounds()
+                  .GetType());
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().HasGradientMask());
+  }
+}
+
+TEST_F(PropertyTreeBuilderTest, NestedGradientMask) {
+  auto root = Layer::Create();
+  host()->SetRootLayer(root);
+  root->SetBounds(gfx::Size(200, 200));
+  root->SetIsDrawable(true);
+
+  auto child1 = Layer::Create();
+  root->AddChild(child1);
+  child1->SetBounds(gfx::Size(100, 100));
+  child1->SetIsDrawable(true);
+
+  auto grand_child1 = Layer::Create();
+  child1->AddChild(grand_child1);
+  grand_child1->SetBounds(gfx::Size(50, 50));
+  grand_child1->SetIsDrawable(true);
+
+  gfx::LinearGradient gradient_mask1(30);
+  gradient_mask1.AddStep(50, 0x50);
+  child1->SetGradientMask(gradient_mask1);
+
+  gfx::LinearGradient gradient_mask2(45);
+  gradient_mask2.AddStep(0, 0xFF);
+  gradient_mask2.AddStep(100, 0x0);
+  grand_child1->SetGradientMask(gradient_mask2);
+
+  CommitAndActivate();
+  EXPECT_TRUE(GetEffectNode(child1.get())->HasRenderSurface());
+  {
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_EQ(gradient_mask1,
+              render_surface_impl1->mask_filter_info().gradient_mask());
+
+    auto* effect_node2 = GetEffectNode(grand_child1.get());
+    EXPECT_FALSE(effect_node2->mask_filter_info.IsEmpty());
+    EXPECT_FALSE(effect_node2->mask_filter_info.HasRoundedCorners());
+    EXPECT_TRUE(effect_node2->mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gfx::RectF(50, 50), effect_node2->mask_filter_info.bounds());
+    EXPECT_EQ(gradient_mask2, effect_node2->mask_filter_info.gradient_mask());
+    EXPECT_FALSE(effect_node2->HasRenderSurface());
+    auto& draw_properties2 = GetLayerImpl(grand_child1)->draw_properties();
+    EXPECT_FALSE(draw_properties2.mask_filter_info.IsEmpty());
+    EXPECT_FALSE(draw_properties2.mask_filter_info.HasRoundedCorners());
+    EXPECT_TRUE(draw_properties2.mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gfx::RectF(50, 50), draw_properties2.mask_filter_info.bounds());
+    EXPECT_EQ(gradient_mask2,
+              draw_properties2.mask_filter_info.gradient_mask());
+  }
+
+  gfx::Transform scale_and_translate_transform1;
+  scale_and_translate_transform1.Translate({10.f, 10.f});
+  scale_and_translate_transform1.Scale(3.f, 2.f);
+  child1->SetTransform(scale_and_translate_transform1);
+  gfx::Transform scale_and_translate_transform2;
+  scale_and_translate_transform2.Translate({10.f, 5.f});
+  scale_and_translate_transform2.Scale(2.f, 1.5f);
+  grand_child1->SetTransform(scale_and_translate_transform2);
+
+  CommitAndActivate();
+  EXPECT_TRUE(GetEffectNode(child1.get())->HasRenderSurface());
+  {
+    // |mask_info| coordinates are in the target space of the render surface's
+    // layer.
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_EQ(gradient_mask1.steps(),
+              render_surface_impl1->mask_filter_info().gradient_mask().steps());
+    // |angle| is updated by the scale transform.
+    EXPECT_EQ(21,
+              render_surface_impl1->mask_filter_info().gradient_mask().angle());
+
+    // |mask_info| is in the coordinate space of the transform node associated
+    // with this effect node.
+    auto* effect_node2 = GetEffectNode(grand_child1.get());
+    EXPECT_FALSE(effect_node2->HasRenderSurface());
+
+    // |mask_info| coordinates are in the target space of the layer.
+    auto& draw_properties2 = GetLayerImpl(grand_child1)->draw_properties();
+    EXPECT_FALSE(draw_properties2.mask_filter_info.IsEmpty());
+    EXPECT_FALSE(draw_properties2.mask_filter_info.HasRoundedCorners());
+    EXPECT_TRUE(draw_properties2.mask_filter_info.HasGradientMask());
+    EXPECT_EQ(gfx::RectF(30, 10, 300, 150),
+              draw_properties2.mask_filter_info.bounds());
+    // |angle| is updated by the scale transform.
+    EXPECT_EQ(26, draw_properties2.mask_filter_info.gradient_mask().angle());
+    EXPECT_EQ(gradient_mask2.steps(),
+              draw_properties2.mask_filter_info.gradient_mask().steps());
+  }
+
+  gfx::Transform rotate_transform;
+  rotate_transform.Rotate(45);
+  child1->SetTransform(rotate_transform);
+  CommitAndActivate();
+  {
+    auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
+    EXPECT_EQ(gfx::RRectF::Type::kEmpty,
+              render_surface_impl1->mask_filter_info()
+                  .rounded_corner_bounds()
+                  .GetType());
+    EXPECT_FALSE(render_surface_impl1->mask_filter_info().HasGradientMask());
+  }
 }
 
 TEST_F(PropertyTreeBuilderTest, RoundedCornerBounds) {

cc/base/math_util.cc
@@ -15,6 +15,7 @@
 #include "base/trace_event/traced_value.h"
 #include "base/values.h"
 #include "ui/gfx/geometry/angle_conversions.h"
+#include "ui/gfx/geometry/linear_gradient.h"
 #include "ui/gfx/geometry/quad_f.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
@@ -1005,6 +1006,18 @@ void MathUtil::AddCornerRadiiToTracedValue(
   res->AppendDouble(rect.GetCornerRadii(gfx::RRectF::Corner::kLowerRight).y());
   res->AppendDouble(rect.GetCornerRadii(gfx::RRectF::Corner::kLowerLeft).x());
   res->AppendDouble(rect.GetCornerRadii(gfx::RRectF::Corner::kLowerLeft).y());
+}
+
+void MathUtil::AddToTracedValue(const char* name,
+                                const gfx::LinearGradient& gradient,
+                                base::trace_event::TracedValue* res) {
+  res->BeginArray(name);
+  res->AppendInteger(gradient.angle());
+  res->AppendInteger(gradient.step_count());
+  for (size_t i = 0; i < gradient.step_count(); i++) {
+    res->AppendDouble(gradient.steps()[i].percent);
+    res->AppendInteger(gradient.steps()[i].alpha);
+  }
   res->EndArray();
 }
 

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 302c82d8d1243998de89b284982f3ad24d053a51

ui,gfx: Reland "gfx: Add LinearGradient to MaskFilterInfo"
This fixes the performance regression caused by commit 93a0e962f03797cad07b8a87331942d9e655b9a2.

The regression was caused by storing empty values for LinearGradient
rather than null values.

Bug: 1312261,1039003
Change-Id: I6965191d4d36826f94fb33a1c0b205ca9642ce0a
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702743
Commit-Queue: Avi Drissman <avi@chromium.org>
Auto-Submit: Sasha McIntosh <sashamcintosh@chromium.org>
Reviewed-by: Will Harris <wfh@chromium.org>
Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
Reviewed-by: Avi Drissman <avi@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1018271}

cc/trees/effect_node.cc
@@ -190,7 +190,8 @@ void EffectNode::AsValueInto(base::trace_event::TracedValue* value) const {
     }
     if (mask_filter_info.HasGradientMask()) {
       MathUtil::AddToTracedValue("mask_filter_gradient_mask",
-                                 mask_filter_info.gradient_mask(), value);
+                                 mask_filter_info.gradient_mask().value(),
+                                 value);
     }
   }
   value->SetString("blend_mode", SkBlendMode_Name(blend_mode));

components/viz/common/quads/shared_quad_state.cc
@@ -56,7 +56,8 @@ void SharedQuadState::AsValueInto(base::trace_event::TracedValue* value) const {
   }
   if (mask_filter_info.HasGradientMask()) {
     cc::MathUtil::AddToTracedValue("mask_filter_gradient_mask",
-                                   mask_filter_info.gradient_mask(), value);
+                                   mask_filter_info.gradient_mask().value(),
+                                   value);
   }
 
   if (clip_rect) {

ui/gfx/geometry/mask_filter_info.cc
@@ -18,15 +18,19 @@ bool MaskFilterInfo::Transform(const gfx::Transform& transform) {
   if (!transform.TransformRRectF(&rounded_corner_bounds_))
     return false;
 
-  if (!gradient_mask_.IsEmpty())
-    gradient_mask_.Transform(transform);
+  if (gradient_mask_ && !gradient_mask_->IsEmpty())
+    gradient_mask_->Transform(transform);
 
   return true;
 }
 
 std::string MaskFilterInfo::ToString() const {
-  return "MaskFilterInfo{" + rounded_corner_bounds_.ToString() +
-         ", gradient_mask=" + gradient_mask_.ToString() + "}";
+  std::string result = "MaskFilterInfo{" + rounded_corner_bounds_.ToString();
+
+  if (gradient_mask_)
+    result += ", gradient_mask=" + gradient_mask_->ToString() + "}";
+
+  return result;
 }
 
 }  // namespace gfx

ui/gfx/geometry/mask_filter_info.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_GEOMETRY_MASK_FILTER_INFO_H_
 #define UI_GFX_GEOMETRY_MASK_FILTER_INFO_H_
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/geometry_skia_export.h"
 #include "ui/gfx/geometry/linear_gradient.h"
 #include "ui/gfx/geometry/rect_f.h"
@@ -42,11 +43,16 @@ class GEOMETRY_SKIA_EXPORT MaskFilterInfo {
            rounded_corner_bounds_.GetType() != RRectF::Type::kRect;
   }
 
-  const gfx::LinearGradient& gradient_mask() const { return gradient_mask_; }
+  const absl::optional<gfx::LinearGradient>& gradient_mask() const {
+    return gradient_mask_;
+  }
 
   // True if this contains an effective gradient mask (requires filter bounds).
   bool HasGradientMask() const {
-    return !rounded_corner_bounds_.IsEmpty() && !gradient_mask_.IsEmpty();
+    if (rounded_corner_bounds_.IsEmpty())
+      return false;
+
+    return gradient_mask_ && !gradient_mask_->IsEmpty();
   }
 
   // True if this contains no effective mask information.
@@ -64,7 +70,7 @@ class GEOMETRY_SKIA_EXPORT MaskFilterInfo {
   RRectF rounded_corner_bounds_;
 
   // Shader based linear gradient mask to be applied to a layer.
-  gfx::LinearGradient gradient_mask_ = gfx::LinearGradient::GetEmpty();
+  absl::optional<gfx::LinearGradient> gradient_mask_;
 };
 
 inline bool operator==(const MaskFilterInfo& lhs, const MaskFilterInfo& rhs) {

ui/gfx/mojom/mask_filter_info.mojom
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 module gfx.mojom;
 
 import "ui/gfx/mojom/rrect_f.mojom";
+import "ui/gfx/mojom/linear_gradient.mojom";
 
-// See ui/gfx/mask_filter_info.h.
+// See ui/gfx/geometry/mask_filter_info.h.
 struct MaskFilterInfo {
   gfx.mojom.RRectF rounded_corner_bounds;
-  // TODO(crbug.com/1039003): add gradient mask field
+  gfx.mojom.LinearGradient? gradient_mask;
 };

ui/gfx/mojom/mask_filter_info_mojom_traits.cc
@@ -13,7 +13,15 @@ bool StructTraits<gfx::mojom::MaskFilterInfoDataView, gfx::MaskFilterInfo>::
   gfx::RRectF bounds;
   if (!data.ReadRoundedCornerBounds(&bounds))
     return false;
-  *out = gfx::MaskFilterInfo(bounds);
+
+  absl::optional<gfx::LinearGradient> gradient_mask;
+  if (!data.ReadGradientMask(&gradient_mask))
+    return false;
+
+  if (gradient_mask && !gradient_mask->IsEmpty())
+    *out = gfx::MaskFilterInfo(bounds, gradient_mask.value());
+  else
+    *out = gfx::MaskFilterInfo(bounds);
   return true;
 }
 

ui/gfx/mojom/mask_filter_info_mojom_traits.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_MOJOM_MASK_FILTER_INFO_MOJOM_TRAITS_H_
 #define UI_GFX_MOJOM_MASK_FILTER_INFO_MOJOM_TRAITS_H_
 
+#include "ui/gfx/geometry/linear_gradient.h"
 #include "ui/gfx/geometry/mask_filter_info.h"
+#include "ui/gfx/mojom/linear_gradient_mojom_traits.h"
 #include "ui/gfx/mojom/mask_filter_info.mojom-shared.h"
 #include "ui/gfx/mojom/rrect_f_mojom_traits.h"
 
@@ -16,6 +18,11 @@ struct StructTraits<gfx::mojom::MaskFilterInfoDataView, gfx::MaskFilterInfo> {
   static const gfx::RRectF& rounded_corner_bounds(
       const gfx::MaskFilterInfo& info) {
     return info.rounded_corner_bounds();
+  }
+
+  static const absl::optional<gfx::LinearGradient>& gradient_mask(
+      const gfx::MaskFilterInfo& info) {
+    return info.gradient_mask();
   }
 
   static bool Read(gfx::mojom::MaskFilterInfoDataView data,

cc/trees/property_tree_builder_unittest.cc
@@ -810,10 +810,10 @@ TEST_F(PropertyTreeBuilderTest, GradientMask) {
     // |angle| is updated by the scale transform.
     EXPECT_EQ(33, layer_impl1->draw_properties()
                       .mask_filter_info.gradient_mask()
-                      .angle());
+                      ->angle());
     EXPECT_EQ(gradient_mask.steps(), layer_impl1->draw_properties()
                                          .mask_filter_info.gradient_mask()
-                                         .steps());
+                                         ->steps());
   }
 
   // Rotate transform eliminates gradient mask.
@@ -879,10 +879,11 @@ TEST_F(PropertyTreeBuilderTest, GradientMask) {
     EXPECT_EQ(gfx::RectF(10, 10, 300, 200),
               render_surface_impl1->mask_filter_info().bounds());
     // |angle| is updated by the scale transform.
-    EXPECT_EQ(33,
-              render_surface_impl1->mask_filter_info().gradient_mask().angle());
-    EXPECT_EQ(gradient_mask.steps(),
-              render_surface_impl1->mask_filter_info().gradient_mask().steps());
+    EXPECT_EQ(
+        33, render_surface_impl1->mask_filter_info().gradient_mask()->angle());
+    EXPECT_EQ(
+        gradient_mask.steps(),
+        render_surface_impl1->mask_filter_info().gradient_mask()->steps());
   }
 
   // Rotate transform eliminates gradient mask.
@@ -961,11 +962,12 @@ TEST_F(PropertyTreeBuilderTest, NestedGradientMask) {
     // |mask_info| coordinates are in the target space of the render surface's
     // layer.
     auto* render_surface_impl1 = GetRenderSurfaceImpl(child1);
-    EXPECT_EQ(gradient_mask1.steps(),
-              render_surface_impl1->mask_filter_info().gradient_mask().steps());
+    EXPECT_EQ(
+        gradient_mask1.steps(),
+        render_surface_impl1->mask_filter_info().gradient_mask()->steps());
     // |angle| is updated by the scale transform.
-    EXPECT_EQ(21,
-              render_surface_impl1->mask_filter_info().gradient_mask().angle());
+    EXPECT_EQ(
+        21, render_surface_impl1->mask_filter_info().gradient_mask()->angle());
 
     // |mask_info| is in the coordinate space of the transform node associated
     // with this effect node.
@@ -980,9 +982,9 @@ TEST_F(PropertyTreeBuilderTest, NestedGradientMask) {
     EXPECT_EQ(gfx::RectF(30, 10, 300, 150),
               draw_properties2.mask_filter_info.bounds());
     // |angle| is updated by the scale transform.
-    EXPECT_EQ(26, draw_properties2.mask_filter_info.gradient_mask().angle());
+    EXPECT_EQ(26, draw_properties2.mask_filter_info.gradient_mask()->angle());
     EXPECT_EQ(gradient_mask2.steps(),
-              draw_properties2.mask_filter_info.gradient_mask().steps());
+              draw_properties2.mask_filter_info.gradient_mask()->steps());
   }
 
   gfx::Transform rotate_transform;