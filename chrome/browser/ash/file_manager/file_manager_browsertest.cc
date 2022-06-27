e2e07b153904f1962095881d1436e22f64824f9a
Wenbo Jie <wenbojie@chromium.org>
2022-06-27 00:41:19 +0000

    [FilesRecentFilter] split the browser test to prevent flakiness
    Split the "recentsAllowCut" browser test into 3 separate tests so each
    test is only responsible for one source, which will make the test much
    faster and more stable.
    
    Bug: 1324048
    Test: browser_tests --gtest_filter="*recentsAllowCut*"
    Change-Id: Ie21c1b52a4ea785389c11ac77eba927f3692e2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722985
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018073}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1716,11 +1716,29 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
     ::testing::Values(
         TestCase("recentsA11yMessages").EnableFiltersInRecents(),
         TestCase("recentsA11yMessages").EnableFiltersInRecents().FilesSwa(),
-        TestCase("recentsAllowCut")
+        TestCase("recentsAllowCutForDownloads")
             .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2(),
-        TestCase("recentsAllowCut")
+        TestCase("recentsAllowCutForDownloads")
+            .EnableArc()
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2()
+            .FilesSwa(),
+        TestCase("recentsAllowCutForDrive")
+            .EnableArc()
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2(),
+        TestCase("recentsAllowCutForDrive")
+            .EnableArc()
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2()
+            .FilesSwa(),
+        TestCase("recentsAllowCutForPlayFiles")
+            .EnableArc()
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2(),
+        TestCase("recentsAllowCutForPlayFiles")
             .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2()
