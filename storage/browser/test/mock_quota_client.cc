c30f002804993bc2190ab4d9facbbd0c1b35a86e
Christine Smith <christinesm@chromium.org>
2022-06-28 23:43:16 +0000

    Fix flaky QuotaManagerImplTest.RetrieveBucketsTable
    Removes the call to ModifyStorageKeyAndNotify within the flaky unittest,
    which is now believed to be the cause of the flake.
    
    Bug: 1329201
    Change-Id: If9135312083c56182e517008251018acdc053e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718052
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018882}

--- storage/browser/test/mock_quota_client.cc
+++ storage/browser/test/mock_quota_client.cc
@@ -47,8 +47,7 @@ void MockQuotaClient::AddBucketsData(
 void MockQuotaClient::ModifyStorageKeyAndNotify(
     const blink::StorageKey& storage_key,
     blink::mojom::StorageType storage_type,
-    int64_t delta,
-    base::OnceClosure callback) {
+    int64_t delta) {
   auto it = std::find_if(
       bucket_data_.begin(), bucket_data_.end(),
       [storage_key, storage_type](std::pair<BucketLocator, int64_t> entry) {
@@ -63,7 +62,7 @@ void MockQuotaClient::ModifyStorageKeyAndNotify(
   // TODO(tzik): Check quota to prevent usage exceed
   quota_manager_proxy_->NotifyStorageModified(
       client_type_, storage_key, storage_type, delta, IncrementMockTime(),
-      base::SequencedTaskRunnerHandle::Get(), std::move(callback));
+      base::SequencedTaskRunnerHandle::Get(), base::DoNothing());
 }
 
 void MockQuotaClient::ModifyBucketAndNotify(BucketId bucket_id, int64_t delta) {
