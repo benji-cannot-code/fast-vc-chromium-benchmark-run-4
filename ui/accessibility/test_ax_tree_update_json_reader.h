99abfa46b26b4f8524911af4d8497a01825036e3
Ramin Halavati <rhalavati@chromium.org>
2022-06-28 15:39:28 +0000

    Add unittest for Screen2x proto generation.
    Accessibility tree is read from JSON format, Screen2x proto is
    generated based on it, and it is partially compared with the expected proto.
    Next CLs will make the comparison more complete and add more test cases.
    
    Bug: 1278249
    Change-Id: I7690edfd6ecb4380519e86d7a1909f1c66dc8740
    Ax-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647354
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018699}

---
+++ ui/accessibility/test_ax_tree_update_json_reader.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_ACCESSIBILITY_TEST_AX_TREE_UPDATE_JSON_READER_H_
+#define UI_ACCESSIBILITY_TEST_AX_TREE_UPDATE_JSON_READER_H_
+
+#include "base/values.h"
+#include "ui/accessibility/ax_tree.h"
+
+namespace ui {
+
+// This function assumes that the JSON input is properly formatted and any
+// error in parsing can result in a runtime error.
+// The JSON format is based on the output of the following code and should
+// stay in sync with that:
+// http://google3/knowledge/cerebra/sense/im2query/screenai/screen2x/crawl/web/crawler.ts
+AXTreeUpdate AXTreeUpdateFromJSON(const base::Value& json);
+
+}  // namespace ui
+#endif  // UI_ACCESSIBILITY_TEST_AX_TREE_UPDATE_JSON_READER_H_
