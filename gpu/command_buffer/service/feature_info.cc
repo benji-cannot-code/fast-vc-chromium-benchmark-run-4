a1a26c5c1490f6e35419e0164b6d413c017e4ef6
Colin Blundell <blundell@chromium.org>
2022-06-28 17:04:13 +0000

    [Graphics] Remove unused include of native_image_buffer.h
    Removing this include clarifies that NativeImageBuffer, which directly
    uses GLImage, is included only by shared_image_backing_egl_image.cc and
    hence is actually internal to the SharedImage implementation. This is
    important from the perspective of analyzing what needs to be done for
    gl_image.h includes over the codebase.
    
    Bug: 1310018
    Change-Id: I0b362d584493ce7bf6d0404eabb0880fd31276b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726080
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018737}

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromecast_buildflags.h"
 #include "build/chromeos_buildflags.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
-#include "gpu/command_buffer/service/native_image_buffer.h"
 #include "gpu/config/gpu_switches.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_fence.h"
