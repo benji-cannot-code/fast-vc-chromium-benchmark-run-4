c31b731b717ff3c7a23b70b7522580435895e5ac
spdonghao <spdonghao@chromium.org>
2022-06-28 01:44:13 +0000

    Fieldtrial config for FeedPositionAndroid.
    Change-Id: I8eabe0bcc9d0459dea2c382c01be5a76842218a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721459
    Auto-Submit: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018508}

--- chrome/android/javatests/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPageTest.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.test.util.NightModeTestUtils;
@@ -57,6 +58,7 @@ import java.util.ArrayList;
 @RunWith(ParameterizedRunner.class)
 @ParameterAnnotations.UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DisableFeatures({ChromeFeatureList.FEED_POSITION_ANDROID})
 public class ExploreSitesPageTest {
     // clang-format on
 
