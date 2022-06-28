9c3cc48a34c291ab995a90bba6f231534a84f3b8
Klaus Weidner <klausw@chromium.org>
2022-06-28 19:24:22 +0000

    Clarify context_ lifetime in WebContentsFrameTracker
    The context_ object's lifetime should be equivalent to
    the corresponding web_contents() value, but this wasn't
    strictly enforced. Update the comments and add DCHECKs
    to clarify. Also skip trying to update HiDPI scaling
    when there are no active web_contents().
    
    Bug: 1338591
    Change-Id: I8da4e741b742f42e325b045ff2a8cf8e41b55425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721469
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018797}

--- content/browser/media/capture/web_contents_frame_tracker.cc
+++ content/browser/media/capture/web_contents_frame_tracker.cc
@@ -41,11 +41,13 @@ namespace content {
 
 namespace {
 
-// Note on lifetime: this context should be deleted when the WebContents
-// is destroyed.
+// Note on lifetime: this context is deleted via WebContentsObserver's
+// WebContentsDestroyed() method when the WebContents is destroyed.
 class WebContentsContext : public WebContentsFrameTracker::Context {
  public:
-  explicit WebContentsContext(WebContents* contents) : contents_(contents) {}
+  explicit WebContentsContext(WebContents* contents) : contents_(contents) {
+    DCHECK(contents_);
+  }
   ~WebContentsContext() override = default;
 
   // WebContextFrameTracker::Context overrides.
@@ -151,6 +153,9 @@ void WebContentsFrameTracker::DidStopCapturingWebContents() {
 
 void WebContentsFrameTracker::SetCapturedContentSize(
     const gfx::Size& content_size) {
+  if (!web_contents())
+    return;
+
   // For efficiency, this function should only be called when the captured
   // content size changes. The caller is responsible for enforcing that.
 
@@ -293,8 +298,6 @@ float WebContentsFrameTracker::CalculatePreferredScaleFactor(
            << ", current_content_size=" << current_content_size.ToString()
            << ", unscaled_current_content_size="
            << unscaled_current_content_size.ToString()
-           << ", context_->GetScaleOverrideForCapture()="
-           << context_->GetScaleOverrideForCapture()
            << ", factors.x()=" << factors.x() << " factors.y()=" << factors.y()
            << ", largest_factor=" << largest_factor
            << ", preferred factor=" << preferred_factor;
@@ -340,6 +343,7 @@ void WebContentsFrameTracker::RenderFrameHostChanged(
 
 void WebContentsFrameTracker::WebContentsDestroyed() {
   is_capturing_ = false;
+  context_ = nullptr;
   Observe(nullptr);
   OnPossibleTargetChange();
 }
@@ -352,7 +356,10 @@ void WebContentsFrameTracker::SetWebContentsAndContextFromRoutingId(
     const GlobalRenderFrameHostId& id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   Observe(WebContents::FromRenderFrameHost(RenderFrameHost::FromID(id)));
-  context_ = std::make_unique<WebContentsContext>(web_contents());
+  if (web_contents()) {
+    // If the routing ID was invalid, don't set up a context.
+    context_ = std::make_unique<WebContentsContext>(web_contents());
+  }
   OnPossibleTargetChange();
 }
 
@@ -409,6 +416,7 @@ void WebContentsFrameTracker::SetWebContentsAndContextForTesting(
 void WebContentsFrameTracker::OnPossibleTargetChange() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   if (!web_contents()) {
+    DCHECK(!context_);
     device_task_runner_->PostTask(
         FROM_HERE,
         base::BindOnce(&WebContentsVideoCaptureDevice::OnTargetPermanentlyLost,
@@ -455,6 +463,7 @@ void WebContentsFrameTracker::SetTargetView(gfx::NativeView view) {
 }
 
 void WebContentsFrameTracker::SetCaptureScaleOverride(float new_value) {
+  DCHECK(context_);
   if (new_value != capture_scale_override_) {
     capture_scale_override_ = new_value;
     context_->SetScaleOverrideForCapture(new_value);
