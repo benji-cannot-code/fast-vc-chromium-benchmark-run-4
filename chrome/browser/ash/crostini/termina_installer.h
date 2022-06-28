2951a358c5a10ea5a2d2e193aa9d5f8f0f0fced2
Timothy Loh <timloh@chromium.org>
2022-06-28 05:25:37 +0000

    Wait for cancelled DLC installation to end before finishing restarts
    We currently complete Crostini restarts immediately if they are
    cancelled while installing DLC. However, a cancelled installation will
    proceed to try and uninstall Crostini, which would often fail as DLC
    would still be busy trying to install termina and we don't currently
    have retry logic there.
    
    Address this by allowing DLC installation to complete (either success
    or failure is okay) before finishing a restart. If the update engine
    is busy for other reasons, uninstall will probably still fail, but at
    least we won't end up leaving termina installed.
    
    Bug: b/236350805
    Change-Id: I45a533fbbcfd33aa1adb4a737277c4bd787dae36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720687
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018542}

--- chrome/browser/ash/crostini/termina_installer.h
+++ chrome/browser/ash/crostini/termina_installer.h
@@ -37,6 +37,8 @@ class TerminaInstaller {
     Offline,
     // The device must be updated before termina can be installed.
     NeedUpdate,
+    // The install request was cancelled.
+    Cancelled,
   };
 
   // This is really a bool, but std::vector<bool> has weird properties that stop
@@ -67,8 +69,11 @@ class TerminaInstaller {
   absl::optional<std::string> GetDlcId();
 
   // Attempt to cancel a pending install. The DLC service does not support
-  // this, but we have some retry logic that can be aborted.
-  void Cancel();
+  // this, but we have some retry logic that can be aborted. The result
+  // callback is run on completion with a result of Cancelled.
+  // If there are multiple concurrent install requests, the wrong request may
+  // end up cancelled.
+  void CancelInstall();
 
  private:
   void InstallDlc(base::OnceCallback<void(InstallResult)> callback,
@@ -76,6 +81,8 @@ class TerminaInstaller {
   void OnInstallDlc(base::OnceCallback<void(InstallResult)> callback,
                     bool is_initial_install,
                     const chromeos::DlcserviceClient::InstallResult& result);
+  void RetryInstallDlc(base::OnceCallback<void(InstallResult)> callback,
+                       bool is_initial_install);
 
   void RemoveComponentIfPresent(base::OnceCallback<void()> callback,
                                 UninstallResult* result);
@@ -88,6 +95,9 @@ class TerminaInstaller {
 
   absl::optional<base::FilePath> termina_location_{absl::nullopt};
   absl::optional<std::string> dlc_id_{};
+
+  bool is_cancelled_ = false;
+
   base::WeakPtrFactory<TerminaInstaller> weak_ptr_factory_{this};
 };
 
