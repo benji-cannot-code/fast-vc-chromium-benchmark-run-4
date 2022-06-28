10b40f0a7ac35af07a9ff6a2df033b1b68563cc0
Howard Yang <hcyang@google.com>
2022-06-28 16:02:57 +0000

    in_session_auth: Check PIN availability after each auth
    The lock screen checks PIN availability after each auth so it could hide
    the PIN input field and prompts the user for their password for correct
    UX. WebAuthn dialog currently tries to match this behavior by keeping a
    PIN attempt counter, but that's incorrect because the incorrect attempt
    count is not necessarily 0 when the dialog is summoned.
    
    Fix the implementation by checking PIN availability again after each
    authentication failure to determine whether PIN field should be
    disabled, instead of by keeping a counter.
    
    The mentioned WebauthnPINLockout test to test this behavior is added in
    https://crrev.com/c/3721192.
    
    Bug: b:237244202
    Test: manual test that "too many attempts" appear after lockout
    Test: run the newly added WebauthnPINLockout test
    Change-Id: I7a05881d67b40287eb048cd664b610c6bbe57453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726427
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1018709}

--- ash/public/cpp/webauthn_dialog_controller.h
+++ ash/public/cpp/webauthn_dialog_controller.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_public_export.h"
 #include "ash/public/cpp/in_session_auth_dialog_client.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace aura {
 class Window;
@@ -19,11 +18,11 @@ namespace ash {
 // WebAuthNDialogController manages the webauthn auth dialog.
 class ASH_PUBLIC_EXPORT WebAuthNDialogController {
  public:
-  // Callback for authentication checks. |success| is nullopt if an
-  // authentication check did not run, otherwise it is true/false if auth
-  // succeeded/failed.
+  // Callback for authentication checks. |success| true/false if auth
+  // succeeded/failed, and |can_use_pin| indicates whether PIN can still be used
+  // (not locked out) after the previous authentication.
   using OnAuthenticateCallback =
-      base::OnceCallback<void(absl::optional<bool> success)>;
+      base::OnceCallback<void(bool success, bool can_use_pin)>;
   // Callback for overall authentication flow result.
   using FinishCallback = base::OnceCallback<void(bool success)>;
 
