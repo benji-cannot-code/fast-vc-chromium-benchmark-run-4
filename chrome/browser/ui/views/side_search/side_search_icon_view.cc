8e215f04b90ba30f5834c2db0bec3c8fe00e270f
Tom Lukaszewicz <tluk@chromium.org>
2022-06-28 17:55:52 +0000

    [side search] Null check the BrowserView ptr in SideSearchIconView
    This CL adds a null check and early return to SideSearchIconView to
    address cases where the browser view returned from
    GetBrowserViewForBrowser() can sometimes return null for a valid
    browser.
    
    Follow up CLs will address the root cause of the issue.
    
    Bug: 1339789
    Change-Id: Ib7088193584bd274bb9a465065c157fc149b51bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732788
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018764}

--- chrome/browser/ui/views/side_search/side_search_icon_view.cc
+++ chrome/browser/ui/views/side_search/side_search_icon_view.cc
@@ -96,6 +96,13 @@ void SideSearchIconView::UpdateImpl() {
     return;
 
   auto* browser_view = BrowserView::GetBrowserViewForBrowser(browser_);
+
+  // TODO(crbug.com/1339789): BrowserView should never be null here, investigate
+  // why GetBrowserViewForBrowser() is returning null in certain circumstances
+  // and remove this check.
+  if (!browser_view)
+    return;
+
   const bool was_visible = GetVisible();
   const bool should_show =
       tab_contents_helper->CanShowSidePanelForCommittedNavigation() &&
@@ -130,6 +137,13 @@ void SideSearchIconView::OnExecuting(PageActionIconView::ExecuteSource source) {
   HidePageActionLabel();
 
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser_);
+
+  // TODO(crbug.com/1339789): BrowserView should never be null here, investigate
+  // why GetBrowserViewForBrowser() is returning null in certain circumstances
+  // and remove this check.
+  if (!browser_view)
+    return;
+
   if (base::FeatureList::IsEnabled(features::kUnifiedSidePanel)) {
     content::WebContents* active_contents =
         browser_view->GetActiveWebContents();
