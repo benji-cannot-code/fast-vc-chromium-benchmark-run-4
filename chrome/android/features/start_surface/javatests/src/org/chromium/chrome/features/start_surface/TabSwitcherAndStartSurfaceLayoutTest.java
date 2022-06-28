b232fbac112a5de0f8aa36a7b5cc9ec74cf5aafe
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-06-28 00:58:54 +0000

    [CodeHealth] Re-enable testCloseButtonDescription
    Re-enable because this test has been consistently passing on the test
    reviver bot.
    
    Bug: 1335780
    Change-Id: I0cc071fecc01c9e125dd43538ed8ff4c9d8a4748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721861
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Auto-Submit: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018493}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
@@ -1437,7 +1437,6 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
     @Test
     @MediumTest
     // clang-format off
-    @DisabledTest(message = "https://crbug.com/1335780")
     @EnableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID,
             ChromeFeatureList.TAB_GROUPS_CONTINUATION_ANDROID + "<Study"})
     @CommandLineFlags.Add({BASE_PARAMS + "/enable_launch_polish/true"})
