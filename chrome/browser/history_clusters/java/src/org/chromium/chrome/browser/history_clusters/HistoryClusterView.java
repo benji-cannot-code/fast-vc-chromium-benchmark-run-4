5c60982386343c13166f9d4253715aec351b9bc7
Patrick Noland <pnoland@google.com>
2022-06-28 18:59:08 +0000

    [Journeys] Make cluster carat onclick also expand/collapse
    Bug: 130317
    Change-Id: I988b08e468bf09bf3100f8cc81129a568581f73e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722335
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018787}

--- chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClusterView.java
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClusterView.java
@@ -54,4 +54,8 @@ class HistoryClusterView extends SelectableItemView<HistoryCluster> {
         mEndButtonView.setVisibility(VISIBLE);
         mEndButtonView.setImageDrawable(drawable);
     }
+
+    public void setEndButtonClickListener(OnClickListener clickListener) {
+        mEndButtonView.setOnClickListener(clickListener);
+    }
 }
