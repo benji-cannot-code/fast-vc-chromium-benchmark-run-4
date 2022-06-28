ebf5283ec0be48a06575c29532d7b9a0db87dfc0
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-06-28 01:00:14 +0000

    [CodeHealth] Re-enable testTabGroupManualSelection_DisabledForSingleTab
    Re-enable because this test has been consistently passing on the test
    reviver bot.
    
    Bug: 1333098
    Change-Id: I6a229aa2b37ae34f9fe946a8e66a66c93f2d4f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724030
    Auto-Submit: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018495}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
@@ -1574,7 +1574,6 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1333098")
     @EnableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID})
     public void testTabGroupManualSelection_DisabledForSingleTab() {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
