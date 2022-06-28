216d25ae1c88e34ed98ddd8ef931c47fb1769c8e
Orin Jaworski <orinj@google.com>
2022-06-28 00:03:43 +0000

    Add guard for null browser_ and revert suspected crash culprit CL
    This CL does two things:
    - Adds a null guard to ChromeOmniboxEditController browser_ usage.
    - Reverts the suspected culprit CL (see bug) just in case something
      about it had unintended consequences.
    
    Bug: 1294004
    Change-Id: I1d80c3f9ecf11f7902e020399c81cefb919fdf2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724014
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018474}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -1662,6 +1662,10 @@ void OmniboxEditModel::OnCurrentMatchChanged() {
                      additional_text);
 }
 
+// static
+const char OmniboxEditModel::kCutOrCopyAllTextHistogram[] =
+    "Omnibox.CutOrCopyAllText";
+
 void OmniboxEditModel::SetAccessibilityLabel(const AutocompleteMatch& match) {
   view_->SetAccessibilityLabel(view_->GetText(), match, true);
 }
