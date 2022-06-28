adea01142bfe1940137cbdf3ad156c98588a21f2
Wenbo Jie <wenbojie@chromium.org>
2022-06-28 05:51:28 +0000

    [FilesRecentFilter] Add group heading support for file list
    Support time period group headings (e.g. Today/Yesterday/Earlier this
    week) in Recent view.
    
    * maintain a group by snapshot in FileListModel to pre-calculate group
      result when the list changes (e.g. splice) so the view
      (file_table_list) can use it for redraw.
    * override couple of methods from List in FileTableList to make sure
      the height related calculation also takes group headings into
      consideration:
        * when calculating the item begin/end index for the viewport
        * when calculating the item top for a specific index
        * when calculating the height for after filler
    * use a :before CSS to show group heading for a list item so we don't
      need to add a physical dom element for it
    
    UI spec: http://shortn/_fefFpm9np1 (Time-period hedings section)
    Demo: http://shortn/_rLgdTmLfUv
    
    Bug: 1324051
    Change-Id: I5b26a33a2a8885a996b6d021c308ad5df0e7f21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687275
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018550}

--- ui/file_manager/file_manager/common/js/recent_date_bucket.js
+++ ui/file_manager/file_manager/common/js/recent_date_bucket.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Recent date bucket definition and util functions.
  */
 
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {util} from './util.js';
 
 /**
  * Given a date and now date, return the date bucket it belongs to.
@@ -31,7 +31,7 @@ export function getRecentDateBucket(date, now) {
     return chrome.fileManagerPrivate.RecentDateBucket.YESTERDAY;
   }
   const startOfThisWeek = new Date(startOfToday);
-  const localeBasedWeekStart = loadTimeData.getInteger('WEEK_START_FROM') || 0;
+  const localeBasedWeekStart = util.getLocaleBasedWeekStart();
   const daysDiff = (startOfToday.getDay() - localeBasedWeekStart + 7) % 7;
   startOfThisWeek.setDate(startOfToday.getDate() - daysDiff);
   if (date >= startOfThisWeek) {
@@ -46,4 +46,39 @@ export function getRecentDateBucket(date, now) {
     return chrome.fileManagerPrivate.RecentDateBucket.EARLIER_THIS_YEAR;
   }
   return chrome.fileManagerPrivate.RecentDateBucket.OLDER;
+}
+
+/**
+ * @param {!chrome.fileManagerPrivate.RecentDateBucket} dateBucket
+ * @return {!string}
+ */
+export function getTranslationKeyForDateBucket(dateBucket) {
+  /** @type {Map<chrome.fileManagerPrivate.RecentDateBucket, string>} */
+  const DATE_BUCKET_TO_TRANSLATION_KEY_MAP = new Map([
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.TODAY,
+      'RECENT_TIME_HEADING_TODAY'
+    ],
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.YESTERDAY,
+      'RECENT_TIME_HEADING_YESTERDAY'
+    ],
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.EARLIER_THIS_WEEK,
+      'RECENT_TIME_HEADING_THIS_WEEK'
+    ],
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.EARLIER_THIS_MONTH,
+      'RECENT_TIME_HEADING_THIS_MONTH'
+    ],
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.EARLIER_THIS_YEAR,
+      'RECENT_TIME_HEADING_THIS_YEAR'
+    ],
+    [
+      chrome.fileManagerPrivate.RecentDateBucket.OLDER,
+      'RECENT_TIME_HEADING_OLDER'
+    ],
+  ]);
+  return DATE_BUCKET_TO_TRANSLATION_KEY_MAP.get(dateBucket);
 }
