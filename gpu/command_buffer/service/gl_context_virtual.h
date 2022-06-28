a537236d495f2dc73fa95a014938761b148cdcee
Zhenyao Mo <zmo@chromium.org>
2022-06-28 19:03:06 +0000

    GLContextVirtual's GetGLDisplayEGL should not return null.
    Instead, it should return the underlying shared context's pointer.
    
    TEST=kevin bot
    R=kbr@chromium.org
    
    Bug: chromium:1337998
    Change-Id: I83443c180e644cb8f1c05c68a73172e7f79af71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3730329
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018788}

--- gpu/command_buffer/service/gl_context_virtual.h
+++ gpu/command_buffer/service/gl_context_virtual.h
@@ -55,6 +55,10 @@ class GPU_GLES2_EXPORT GLContextVirtual : public gl::GLContext {
   void FlushForDriverCrashWorkaround() override;
 #endif
 
+#if defined(USE_EGL)
+  gl::GLDisplayEGL* GetGLDisplayEGL() override;
+#endif
+
  protected:
   ~GLContextVirtual() override;
   void ResetExtensions() override;
