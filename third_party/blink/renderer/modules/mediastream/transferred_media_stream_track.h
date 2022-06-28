e65883a3ab17761aab5dd53d6dc6799d3f75fcff
Tove Petersson <tovep@chromium.org>
2022-06-28 13:36:41 +0000

    Add basic shell of TransferredMediaStreamComponent
    Also use the TransferredMediaStreamComponent in TransferredMediaStreamTrack before an initialized implementation track has been set.
    
    Bug: 1288839
    Change-Id: I62e01dbc6832ed8f61ff2877ffa929c0c5da57fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714291
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018656}

--- third_party/blink/renderer/modules/mediastream/transferred_media_stream_track.h
+++ third_party/blink/renderer/modules/mediastream/transferred_media_stream_track.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_descriptor.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
+#include "third_party/blink/renderer/platform/mediastream/transferred_media_stream_component.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_scheduler.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -121,6 +122,7 @@ class MODULES_EXPORT TransferredMediaStreamTrack : public MediaStreamTrack {
     Member<TransferredMediaStreamTrack> transferred_track_;
   };
 
+  Member<TransferredMediaStreamComponent> transferred_component_;
   Member<MediaStreamTrack> track_;
   using ConstraintsPair =
       std::pair<ScriptPromiseResolver*, const MediaTrackConstraints*>;
