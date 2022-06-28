2ec0ed458bbb0df380036968c88978f95f366d1f
Dana Fried <dfried@chromium.org>
2022-06-28 14:25:13 +0000

    [WebUI IPH] Add C++ handler and ElementTracker and HelpBubble support.
    Adds the C++ side of the help bubble polymer component and associated
    mojo interface, provides a helper that can be dropped into any
    WebUIController that supports Mojo, and hooks the whole thing up to
    the ElementTracker and HelpBubbleFactory systems so that we can
    seamlessly trigger help bubbles in WebUI the same as in Views.
    
    This is insufficient to provide support for IPH and Tutorials cases, but
    only because of a lack of bubble functionality on the WebUI side; that
    is, the bubbles still lack full anchoring, close/action buttons, and
    timeouts.
    
    Otherwise everything works exactly as expected, and we will add in
    examples/demos to demonstrate that a help bubble in a WebUI can be
    triggered from an IPH (in a follow-up CL).
    
    Bug: 1339000
    
    
    Change-Id: Id4915c06a437791eb723f7993727fe5f0edec0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718941
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018668}

---
+++ components/user_education/webui/tracked_element_webui.h
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_USER_EDUCATION_WEBUI_TRACKED_ELEMENT_WEBUI_H_
+#define COMPONENTS_USER_EDUCATION_WEBUI_TRACKED_ELEMENT_WEBUI_H_
+
+#include "base/memory/raw_ptr.h"
+#include "ui/base/interaction/element_identifier.h"
+#include "ui/base/interaction/element_tracker.h"
+#include "ui/base/interaction/framework_specific_implementation.h"
+
+namespace user_education {
+
+class HelpBubbleHandlerBase;
+
+class TrackedElementWebUI : public ui::TrackedElement {
+ public:
+  TrackedElementWebUI(HelpBubbleHandlerBase* handler,
+                      ui::ElementIdentifier identifier,
+                      ui::ElementContext context);
+  ~TrackedElementWebUI() override;
+
+  DECLARE_FRAMEWORK_SPECIFIC_METADATA()
+
+  HelpBubbleHandlerBase* handler() { return handler_; }
+
+ private:
+  friend class HelpBubbleHandlerBase;
+
+  void SetVisible(bool visible);
+  bool visible() const { return visible_; }
+
+  const base::raw_ptr<HelpBubbleHandlerBase> handler_;
+  bool visible_ = false;
+};
+
+}  // namespace user_education
+
+#endif  // COMPONENTS_USER_EDUCATION_WEBUI_TRACKED_ELEMENT_WEBUI_H_
