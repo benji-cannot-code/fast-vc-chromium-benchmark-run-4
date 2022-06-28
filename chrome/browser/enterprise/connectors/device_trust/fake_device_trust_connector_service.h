352e5bd80c306f143fbeae094866e92d62c87cfa
Matt Menke <mmenke@chromium.org>
2022-06-28 17:16:59 +0000

    Update base::Value list construction in chrome/browser/enterprise/
    Switch from using vector<Value> (also known as Value::ListStorage),
    which is deprecated, to using base::Value::List().
    
    Bug: 1338341
    Change-Id: Ic178c3dbc69a2ba8ecfeb50b2ac98dde02e97abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724212
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018741}

--- chrome/browser/enterprise/connectors/device_trust/fake_device_trust_connector_service.h
+++ chrome/browser/enterprise/connectors/device_trust/fake_device_trust_connector_service.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_FAKE_DEVICE_TRUST_CONNECTOR_SERVICE_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_FAKE_DEVICE_TRUST_CONNECTOR_SERVICE_H_
 
-#include <memory>
-
 #include "base/memory/raw_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_connector_service.h"
@@ -24,7 +22,7 @@ class FakeDeviceTrustConnectorService : public DeviceTrustConnectorService {
       sync_preferences::TestingPrefServiceSyncable* profile_prefs);
   ~FakeDeviceTrustConnectorService() override;
 
-  void update_policy(std::unique_ptr<base::ListValue> new_urls);
+  void update_policy(base::Value::List new_urls);
 
  private:
   raw_ptr<sync_preferences::TestingPrefServiceSyncable> test_prefs_;
--- chrome/browser/enterprise/util/managed_browser_utils_unittest.cc
+++ chrome/browser/enterprise/util/managed_browser_utils_unittest.cc
@@ -121,19 +121,17 @@ class AutoSelectCertificateTest : public testing::Test {
         {client_1_, client_2_});
   }
 
-  void SetPolicyValueInContentSettings(
-      const std::vector<base::Value>& filters) {
+  void SetPolicyValueInContentSettings(base::Value::List filters) {
     HostContentSettingsMap* m =
         HostContentSettingsMapFactory::GetForProfile(profile());
 
-    std::unique_ptr<base::DictionaryValue> root =
-        std::make_unique<base::DictionaryValue>();
-    root->SetKey("filters", base::Value(std::move(filters)));
+    base::Value::Dict root;
+    root.Set("filters", std::move(filters));
 
     m->SetWebsiteSettingDefaultScope(
         GURL(kRequestingUrl), GURL(),
         ContentSettingsType::AUTO_SELECT_CERTIFICATE,
-        base::Value::FromUniquePtrValue(std::move(root)));
+        base::Value(std::move(root)));
   }
 
   base::Value CreateFilterValue(const std::string& issuer,
@@ -191,10 +189,10 @@ TEST_F(AutoSelectCertificateTest,
   net::ClientCertIdentityList client_certs_list = GetDefaultClientCertList();
 
   // client_1.pem has "B CA" as its issuer, so set up filters to select it
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("B CA", ""));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("B CA", ""));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
   chrome::enterprise_util::AutoSelectCertificates(
@@ -211,10 +209,10 @@ TEST_F(AutoSelectCertificateTest,
   net::ClientCertIdentityList client_certs_list = GetDefaultClientCertList();
 
   // client_2.pem has "E CA" as its issuer, so set up filters to select it
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("E CA", ""));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("E CA", ""));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
 
@@ -233,10 +231,10 @@ TEST_F(AutoSelectCertificateTest,
 
   // client_1.pem has "Client Cert A" as its subject, so set up filters to
   // select it
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("", "Client Cert A"));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("", "Client Cert A"));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
 
@@ -255,10 +253,10 @@ TEST_F(AutoSelectCertificateTest,
 
   // client_2.pem has "Client Cert D" as its subject, so set up filters to
   // select it
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("", "Client Cert D"));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("", "Client Cert D"));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
 
@@ -274,10 +272,10 @@ TEST_F(AutoSelectCertificateTest, IssuerNotMatchingDoesntSelectCerts) {
   GURL requesting_url(kRequestingUrl);
   net::ClientCertIdentityList client_certs_list = GetDefaultClientCertList();
 
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("Bad Issuer", "Client Cert D"));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("Bad Issuer", "Client Cert D"));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
 
@@ -294,10 +292,10 @@ TEST_F(AutoSelectCertificateTest, SubjectNotMatchingDoesntSelectCerts) {
   GURL requesting_url(kRequestingUrl);
   net::ClientCertIdentityList client_certs_list = GetDefaultClientCertList();
 
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("E CA", "Bad Subject"));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("E CA", "Bad Subject"));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
 
@@ -314,10 +312,10 @@ TEST_F(AutoSelectCertificateTest, MatchingCertOnDifferentUrlDoesntSelectCerts) {
   GURL requesting_url("http://other.domain.example.com");
   net::ClientCertIdentityList client_certs_list = GetDefaultClientCertList();
 
-  std::vector<base::Value> filters;
-  filters.push_back(CreateFilterValue("E CA", ""));
+  base::Value::List filters;
+  filters.Append(CreateFilterValue("E CA", ""));
 
-  SetPolicyValueInContentSettings(filters);
+  SetPolicyValueInContentSettings(std::move(filters));
 
   net::ClientCertIdentityList matching_certs_list, nonmatching_certs_list;
   chrome::enterprise_util::AutoSelectCertificates(
