1c61a3c1126fd89f53481e7e159a232c719df2e1
Ted Meyer <tmathmeyer@chromium.org>
2022-06-29 01:06:16 +0000

    Add comment to crc ukm & remove extra data
    Privacy review feedback :)
    
    R=eugene@chromium.org
    
    Change-Id: I6a649855750305fe49be3e20fbd0aedb7ad802e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732339
    Auto-Submit: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018919}

--- media/base/status_unittest.cc
+++ media/base/status_unittest.cc
@@ -656,7 +656,7 @@ TEST_F(StatusTest, UKMSerializerTest) {
   result.ToUKM(builder);
   ASSERT_EQ(builder.status.bits.code,
             static_cast<StatusCodeType>(SerializeStatus::Codes::kFoo));
-  ASSERT_EQ(builder.status.bits.extra_data, 154u);
+  ASSERT_EQ(builder.status.bits.extra_data, 0u);
 
   // Wrap the code with extra data to ensure that |.root.extra_data| is
   // serialized.
@@ -666,7 +666,7 @@ TEST_F(StatusTest, UKMSerializerTest) {
   ASSERT_NE(builder.root.packed, 0u);
   ASSERT_EQ(builder.root.bits.code,
             static_cast<StatusCodeType>(SerializeStatus::Codes::kFoo));
-  ASSERT_EQ(builder.root.bits.extra_data, 154u);
+  ASSERT_EQ(builder.root.bits.extra_data, 0u);
   ASSERT_NE(builder.status.packed, 0u);
   ASSERT_EQ(builder.status.bits.code,
             static_cast<StatusCodeType>(SerializeStatus::Codes::kBar));
@@ -678,7 +678,7 @@ TEST_F(StatusTest, UKMSerializerTest) {
   ASSERT_NE(builder.root.packed, 0u);
   ASSERT_EQ(builder.root.bits.code,
             static_cast<StatusCodeType>(SerializeStatus::Codes::kFoo));
-  ASSERT_EQ(builder.root.bits.extra_data, 154u);
+  ASSERT_EQ(builder.root.bits.extra_data, 0u);
   ASSERT_NE(builder.status.packed, 0u);
   ASSERT_EQ(builder.status.bits.code,
             static_cast<StatusCodeType>(SerializeStatus::Codes::kBar));
