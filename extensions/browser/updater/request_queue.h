d7439bb8db3bd2b30b13a737f0cf03418199fb67
Oleg Davydov <burunduk@chromium.org>
2022-06-29 09:54:02 +0000

    [Extensions] Use RequestQueue<>::Request instead of two fields
    The RequestQueue template has an internal type, Request, and a couple of
    fields, active_request_ and active_backoff_entry_, which fit into this
    type perfectly.
    
    Bug: b:237277768
    Change-Id: I264c788ac54f5c28e31b96273d5b21854e2529b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726458
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019034}

--- extensions/browser/updater/request_queue.h
+++ extensions/browser/updater/request_queue.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "net/base/backoff_entry.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace extensions {
 
@@ -84,18 +85,18 @@ class RequestQueue {
 
  private:
   struct Request {
-    Request(net::BackoffEntry* backoff_entry, T* request)
-        : backoff_entry(backoff_entry), request(request) {}
+    Request(std::unique_ptr<net::BackoffEntry> backoff_entry,
+            std::unique_ptr<T> fetch)
+        : backoff_entry(std::move(backoff_entry)), fetch(std::move(fetch)) {}
     std::unique_ptr<net::BackoffEntry> backoff_entry;
-    std::unique_ptr<T> request;
+    std::unique_ptr<T> fetch;
   };
 
   // Compares the release time of two pending requests.
   static bool CompareRequests(const Request& a, const Request& b);
 
   // Pushes a request with a given backoff entry onto the queue.
-  void PushImpl(std::unique_ptr<T> request,
-                std::unique_ptr<net::BackoffEntry> backoff_entry);
+  void PushImpl(Request request);
 
   // The backoff policy used to determine backoff delays.
   raw_ptr<const net::BackoffEntry::Policy> backoff_policy_;
@@ -107,9 +108,8 @@ class RequestQueue {
   // the code needs to be able to iterate over all pending requests.
   base::circular_deque<Request> pending_requests_;
 
-  // Active request and its associated backoff entry.
-  std::unique_ptr<T> active_request_;
-  std::unique_ptr<net::BackoffEntry> active_backoff_entry_;
+  // Active entry with its associated backoff.
+  absl::optional<Request> active_request_;
 
   // Timer to schedule calls to StartNextRequest, if the first pending request
   // hasn't passed its release time yet.
@@ -123,8 +123,8 @@ class RequestQueue<T>::iterator {
  public:
   iterator() = default;
 
-  T* operator*() { return it_->request.get(); }
-  T* operator->() { return it_->request.get(); }
+  T* operator*() { return it_->fetch.get(); }
+  T* operator->() { return it_->fetch.get(); }
   iterator& operator++() {
     ++it_;
     return *this;
