4aea308fbd644e687f2c5004d4f078640fc42fcb
Adem Derinel <derinel@google.com>
2022-06-29 10:47:45 +0000

    Settings UI: Password View: Show toast after copying username/password
    This CL introduces a copy toast when the user interacts with username /
    password copy buttons. The toast is also announced by screen readers
    (e.g: tested in Windows 10 narrator).
    
    The toast is immediately hidden when the user navigates to somewhere
    else, so that the same toast will not be shown when user re-navigates to
    the password view page.
    
    Bug: 1298027
    Change-Id: I1ff849f4b86fae34bafd42aadd7d8df98d6cbc21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706893
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019051}

--- chrome/browser/resources/settings/autofill_page/password_view.ts
+++ chrome/browser/resources/settings/autofill_page/password_view.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import '../controls/settings_textarea.js';
 import '../i18n_setup.js';
 // <if expr="chromeos_ash or chromeos_lacros">
@@ -20,6 +21,7 @@ import './password_edit_dialog.js';
 import './password_remove_dialog.js';
 import './passwords_shared.css.js';
 
+import {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {focusWithoutInk} from 'chrome://resources/js/cr/ui/focus_without_ink.m.js';
 import {I18nMixin, I18nMixinInterface} from 'chrome://resources/js/i18n_mixin.js';
@@ -40,6 +42,12 @@ import {PasswordRemovalMixin, PasswordRemovalMixinInterface} from './password_re
 import {PasswordRemoveDialogPasswordsRemovedEvent} from './password_remove_dialog.js';
 import {PasswordRequestorMixin, PasswordRequestorMixinInterface} from './password_requestor_mixin.js';
 import {getTemplate} from './password_view.html.js';
+
+export interface PasswordViewElement {
+  $: {
+    toast: CrToastElement,
+  };
+}
 
 const PasswordViewElementBase =
     PasswordRemovalMixin(PasswordRequestorMixin(MergePasswordsStoreCopiesMixin(
@@ -105,6 +113,11 @@ export class PasswordViewElement extends PasswordViewElementBase {
         value: () => [],
       },
 
+      toastText_: {
+        type: String,
+        value: '',
+      },
+
       credential: {
         type: Object,
         value: null,
@@ -162,6 +175,7 @@ export class PasswordViewElement extends PasswordViewElementBase {
   }
 
   private activeDialogAnchorStack_: Array<HTMLElement>;
+  private toastText_: string;
   credential: MultiStorePasswordUiEntry|null;
   private inAccount_: boolean|undefined;
   private isPasswordNotesEnabled_: boolean;
@@ -202,6 +216,7 @@ export class PasswordViewElement extends PasswordViewElementBase {
       this.recentlyEdited_ = false;
       this.password_ = '';
       this.credential = null;
+      this.hideToast_();
       return;
     }
     const queryParameters = Router.getInstance().getQueryParameters();
@@ -291,12 +306,19 @@ export class PasswordViewElement extends PasswordViewElementBase {
     this.requestPlaintextPassword(
             this.credential!.getAnyId(),
             chrome.passwordsPrivate.PlaintextReason.COPY)
+        .then(() => {
+          this.toastText_ = this.i18n('passwordCopiedToClipboard');
+          this.showToast_();
+        })
         .catch(() => {});
   }
 
   /** Handler to copy the username from the username field. */
   private onCopyUsernameButtonClick_() {
-    navigator.clipboard.writeText(this.credential!.username);
+    navigator.clipboard.writeText(this.credential!.username).then(() => {
+      this.toastText_ = this.i18n('passwordUsernameCopiedToClipboard');
+      this.showToast_();
+    });
     recordPasswordViewInteraction(
         PasswordViewPageInteractions.USERNAME_COPY_BUTTON_CLICKED);
   }
@@ -334,6 +356,7 @@ export class PasswordViewElement extends PasswordViewElementBase {
 
   private onSavedPasswordEdited_(event: SavedPasswordEditedEvent) {
     this.recentlyEdited_ = true;
+
     const newUsername = event.detail.username;
     if (event.detail.username !== this.credential!.username ||
         event.detail.password !== this.credential!.password ||
@@ -455,6 +478,14 @@ export class PasswordViewElement extends PasswordViewElementBase {
     this.recentlyEdited_ = false;
     recordPasswordViewInteraction(
         PasswordViewPageInteractions.CREDENTIAL_FOUND);
+  }
+
+  private hideToast_() {
+    this.$.toast.hide();
+  }
+
+  private showToast_() {
+    this.$.toast.show();
   }
 }
 
