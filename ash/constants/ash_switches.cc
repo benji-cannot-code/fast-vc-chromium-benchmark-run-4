bbde54d90aaa797eb18c621efdcb5de589d617cb
Andrew Xu <andrewxu@chromium.org>
2022-06-29 23:13:20 +0000

    [Launcher] Add a switch to skip the reorder nudge duration threshold
    Currently, the reorder nudge has to show for at least one second
    before it is considered to be shown. This duration threshold makes
    the Tast test on the reorder nudge become time-consuming.
    
    This CL adds a switch so that when the switch is turned on, the
    one-second duration threshold is ignored.
    
    Bug: b/233624351
    Change-Id: I775dcff0ca94a47e99ff0708acf954826e7d8b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736746
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019386}

--- ash/constants/ash_switches.cc
+++ ash/constants/ash_switches.cc
@@ -804,6 +804,11 @@ const char kShowTaps[] = "show-taps";
 const char kSkipForceOnlineSignInForTesting[] =
     "skip-force-online-signin-for-testing";
 
+// Used to skip the threshold duration that the reorder nudge has to show before
+// the nudge is considered as shown.
+const char kSkipReorderNudgeShowThresholdDurationForTest[] =
+    "skip-reorder-nudge-show-threshold-duration";
+
 // If set, the device will be forced to stay in clamshell UI mode but screen
 // auto rotation will be supported. E.g, chromebase device Dooly.
 const char kSupportsClamshellAutoRotation[] =
@@ -1002,6 +1007,11 @@ bool ShouldClearFastInkBuffer() {
 
 bool HasHps() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(kHasHps);
+}
+
+bool IsSkipRecorderNudgeShowThresholdDurationEnabled() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      kSkipReorderNudgeShowThresholdDurationForTest);
 }
 
 }  // namespace switches
