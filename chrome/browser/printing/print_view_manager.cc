768795dab681b7563f83fc38bd61b1cceec85838
Lei Zhang <thestig@chromium.org>
2022-06-29 05:48:29 +0000

    Always check OOP print feature when calling RegisterSystemPrintClient().
    As is, https://crrev.com/1002819 is incomplete and missed this check for
    RegisterSystemPrintClient() in the system print dialog code path. This
    puts the browser process in a bad state, and the system print dialog no
    longer works correctly.
    
    Fix this by consistently checking the OOP print feature before calling
    RegisterSystemPrintClient(). Add DCHECKs to RegisterSystemPrintClient()
    and UnregisterSystemPrintClient() to sanity check that they are being
    called only when appropriate.
    
    Bug: 1340119
    Change-Id: Ib68363525986c47adfc5d8a98b51d2245265549f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733413
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018980}

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "printing/buildflags/buildflags.h"
+#include "printing/printing_features.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_manager.h"
@@ -114,10 +115,12 @@ bool PrintViewManager::PrintForSystemDialogNow(
   }
 
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
-  // Register this worker so that the service persists as long as the user
-  // keeps the system print dialog UI displayed.
-  if (!RegisterSystemPrintClient())
-    return false;
+  if (printing::features::kEnableOopPrintDriversJobPrint.Get()) {
+    // Register this worker so that the service persists as long as the user
+    // keeps the system print dialog UI displayed.
+    if (!RegisterSystemPrintClient())
+      return false;
+  }
 #endif
 
   SetPrintingRFH(print_preview_rfh_);
