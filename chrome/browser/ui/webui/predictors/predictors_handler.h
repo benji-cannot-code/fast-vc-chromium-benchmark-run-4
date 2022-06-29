e63c477b2e1ff2ae127a81ae007eef5c86964dba
Maks Orlovich <morlovich@chromium.org>
2022-06-29 01:28:26 +0000

    [Code health] Convert /chrome/browser/ui/webui/predictors away from WebUI::RegisterDeprecatedMessageCallback
    ...to WebUI::RegisterMessageCallback. (In some cases some other deprecated
    usages were converted, when fairly local).
    
    This CL was uploaded by git cl split.
    
    R=ryansturm@chromium.org
    
    Bug: 1243386
    Change-Id: I60dbe7a08700c0d7c077fb492cfbeb29b7ac30a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733271
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018928}

--- chrome/browser/ui/webui/predictors/predictors_handler.h
+++ chrome/browser/ui/webui/predictors/predictors_handler.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
-namespace base {
-class ListValue;
-}
-
 namespace predictors {
 class AutocompleteActionPredictor;
 class LoadingPredictor;
@@ -37,16 +33,16 @@ class PredictorsHandler : public content::WebUIMessageHandler {
  private:
   // Synchronously fetches the database from AutocompleteActionPredictor and
   // calls into JS with the resulting DictionaryValue.
-  void RequestAutocompleteActionPredictorDb(const base::ListValue* args);
+  void RequestAutocompleteActionPredictorDb(const base::Value::List& args);
 
   // Fetches stats for the ResourcePrefetchPredictor and returns it as a
   // DictionaryValue to the JS.
-  void RequestResourcePrefetchPredictorDb(const base::ListValue* args);
+  void RequestResourcePrefetchPredictorDb(const base::Value::List& args);
 
   // Helpers for RequestResourcePrefetchPredictorDb.
   void AddOriginDataMapToListValue(
       const std::map<std::string, predictors::OriginData>& data_map,
-      base::ListValue* db) const;
+      base::Value::List* db) const;
 
   raw_ptr<predictors::AutocompleteActionPredictor>
       autocomplete_action_predictor_;
