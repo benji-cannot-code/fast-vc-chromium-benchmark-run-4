40de7a84c60d7c52926a1bc8ca9ba64a45f1a4e5
Simon Pelchat <spelchat@chromium.org>
2022-06-29 23:41:22 +0000

    Simplify using declaration.
    Change-Id: I40cb5325cd0703733c663b012c3e9539b74c2254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736701
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Auto-Submit: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019396}

--- chrome/browser/navigation_predictor/anchor_element_preloader_browsertest.cc
+++ chrome/browser/navigation_predictor/anchor_element_preloader_browsertest.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 using UkmEntry = ukm::TestUkmRecorder::HumanReadableUkmEntry;
-using Preloading_Attempt = ukm::builders::Preloading_Attempt;
+using ukm::builders::Preloading_Attempt;
 
 class AnchorElementPreloaderBrowserTest
     : public subresource_filter::SubresourceFilterBrowserTest,
