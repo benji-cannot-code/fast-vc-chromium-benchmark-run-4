6d00a0b18f8e6160d8bc4d2abfb6785b921687e9
Nigel Tao <nigeltao@chromium.org>
2022-06-29 12:51:22 +0000

    Have fusebox_moniker.cc use map not unordered_map
    base/containers/README.md#map-and-set-selection says "Generally avoid...
    std::unordered_map... Use std::map and std::set if you can‘t decide.
    Even if they’re not great, they're unlikely to be bad or surprising."
    
    Bug: 1249754
    Change-Id: I41dd50c12ccc832b6542a2e9fa6a58e8ff8f4543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734368
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Auto-Submit: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019100}

--- chrome/browser/ash/file_manager/fusebox_moniker.cc
+++ chrome/browser/ash/file_manager/fusebox_moniker.cc
@@ -5,15 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/file_manager/fusebox_moniker.h"
 
-#include <unordered_map>
+#include <map>
 #include "base/no_destructor.h"
 #include "content/public/browser/browser_thread.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace {
 
-base::NoDestructor<
-    std::unordered_map<base::Token, storage::FileSystemURL, base::TokenHash>>
+base::NoDestructor<std::map<base::Token, storage::FileSystemURL>>
     fusebox_monikers;
 
 }  // namespace
