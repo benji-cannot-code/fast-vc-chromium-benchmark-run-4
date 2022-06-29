63eacb154b9b4b089bda4632ef932aabee3961c4
Osama Fathy <osamafathy@google.com>
2022-06-29 15:54:04 +0000

    OOBE: Fix guest button visibility
    `is_first_signin_step_` is a flag in LoginShelfView that is required to
    be set to true for the guest button to be visible.
    
    It's set to true in ShowImpl() method of UserCreationScreen, and is
    later updated by the GAIA Screen to update the visibility of the guest
    button.
    
    In gaia_signin.js, a call to onIsFirstSigninStepChanged() is triggered
    when isFirstSigninStep_ is updated, which itself observes properties in
    gaia_signin.js and gets updated according to the changes of these
    properties.
    
    One of the properties that isFirstSigninStep_ depends on is `uiStep`
    which has an empty string until the screen is shown. An empty string
    will result isFirstSigninStep_ to be false.
    
    So, any change of other properties that updates isFirstSigninStep_
    before the GAIA screen is shown, will result in the guest button to be
    hidden.
    
    This change prevents the Gaia screen from updating
    `is_first_signin_step_` when the screen is not shown yet.
    
    Bug: 1324105
    Change-Id: I20e73e0b91e4519c06221982fee7df29b69e2058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726069
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019176}

--- chrome/browser/resources/chromeos/login/screens/common/gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screens/common/gaia_signin.js
@@ -400,7 +400,9 @@ class GaiaSigninElement extends GaiaSigninElementBase {
   }
 
   onIsFirstSigninStepChanged(isFirstSigninStep) {
-    chrome.send('setIsFirstSigninStep', [isFirstSigninStep]);
+    if (this.isShown_) {
+      chrome.send('setIsFirstSigninStep', [isFirstSigninStep]);
+    }
   }
 
   /**
