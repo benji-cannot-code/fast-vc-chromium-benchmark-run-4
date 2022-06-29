d19fd0482af16c15fed371d1c528632e0f5c1beb
Sinan Sahin <sinansahin@google.com>
2022-06-29 22:19:04 +0000

    🤠 Disable DownloadActivityV2Test#testRenameItem on Android 12
    Bug: 1338140
    Change-Id: If75a58a1aaabba1a5da4257bf5fb5fc5d4b45494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735739
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019332}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
@@ -26,6 +26,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Pair;
@@ -51,6 +52,7 @@ import org.chromium.base.DiscardableReferencePool;
 import org.chromium.base.FeatureList;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.task.PostTask;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.download.home.list.ListUtils;
@@ -432,6 +434,7 @@ public class DownloadActivityV2Test extends BlankUiTestActivityTestCase {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.R, message = "https://crbug.com/1338140")
     public void testRenameItem() throws Exception {
         TestThreadUtils.runOnUiThreadBlocking(() -> { setUpUi(); });
 
