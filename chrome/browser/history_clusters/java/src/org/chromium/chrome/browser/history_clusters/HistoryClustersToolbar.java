ed68f292ef081f0c43602ccc8261fb575624cafd
Patrick Noland <pnoland@google.com>
2022-06-29 17:48:39 +0000

    [Journeys] Hide keyboard for programmatic search text changes
    Bug: 1303171
    Change-Id: I7a58302c51aed56681775e59d3c9f50f00f1e800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724007
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019241}

--- chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersToolbar.java
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersToolbar.java
@@ -48,5 +48,7 @@ class HistoryClustersToolbar extends SelectableListToolbar<PropertyModel> {
     void setSearchText(String text) {
         mSearchText.setText(text);
         mSearchText.setSelection(text.length());
+        hideKeyboard();
+        mSearchText.clearFocus();
     }
 }
