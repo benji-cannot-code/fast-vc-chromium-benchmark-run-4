e70318be73311992bddd3a467dbaefea29e1a0e3
Keren Zhu <kerenzhu@chromium.org>
2022-06-29 02:25:29 +0000

    Use WebUI Color Pipeline in WebUI Tab Strip
    This CL migrates WebUI Tab Strip from the ad hoc theme support to the
    more generic WebUI Color Pipeline. Specifically,
    - Remove the tab strip mojo used for color passing. Use colors from
      chrome://theme/colors.css instead.
    - Use color_change_listener that will refresh WebUI colors on color
      update (due to theme change or light/dark mode change)
    - Assert that color_change_listener is only in desktop build.
    
    This is the first WebUI that uses color_change_listener and
    chrome://theme/colors.css.
    
    Doc: use of color id in WebUI
    https://docs.google.com/document/d/1lZzeWvvczGaI-zVo4ekwd53-Mmss6_tdVbPiEwj88_o/edit?usp=sharing
    
    Bug: 1261912
    Change-Id: I9f3206c9d293b4358f160c1f6a198e54fb5e726e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702665
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018954}

--- chrome/browser/resources/tab_strip/tab_list.ts
+++ chrome/browser/resources/tab_strip/tab_list.ts
@@ -7,6 +7,7 @@ import './strings.m.js';
 import './tab.js';
 import './tab_group.js';
 
+import {startColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {addWebUIListener, removeWebUIListener, WebUIListener} from 'chrome://resources/js/cr.m.js';
 import {FocusOutlineManager} from 'chrome://resources/js/cr/ui/focus_outline_manager.m.js';
@@ -249,8 +250,7 @@ export class TabListElement extends CustomElement implements
         () => this.onReceivedKeyboardFocus_());
 
     callbackRouter.themeChanged.addListener(() => {
-      // Refetch theme colors, group color and tab favicons on theme change.
-      this.fetchAndUpdateColors_();
+      // Refetch theme group color and tab favicons on theme change.
       this.fetchAndUpdateGroupData_();
       this.fetchAndUpdateTabs_();
     });
@@ -274,6 +274,8 @@ export class TabListElement extends CustomElement implements
 
     const dragManager = new DragManager(this);
     dragManager.startObserving();
+
+    startColorChangeUpdater();
   }
 
   private addAnimationPromise_(promise: Promise<void>) {
@@ -337,7 +339,6 @@ export class TabListElement extends CustomElement implements
   connectedCallback() {
     this.tabsApi_.getLayout().then(
         ({layout}) => this.applyCSSDictionary_(layout));
-    this.fetchAndUpdateColors_();
 
     const getTabsStartTimestamp = Date.now();
     this.tabsApi_.getTabs().then(({tabs}) => {
@@ -394,11 +395,6 @@ export class TabListElement extends CustomElement implements
   private findTabGroupElement_(groupId: string): TabGroupElement|null {
     return this.$<TabGroupElement>(
         `tabstrip-tab-group[data-group-id="${groupId}"]`);
-  }
-
-  private fetchAndUpdateColors_() {
-    this.tabsApi_.getColors().then(
-        ({colors}) => this.applyCSSDictionary_(colors));
   }
 
   private fetchAndUpdateGroupData_() {
