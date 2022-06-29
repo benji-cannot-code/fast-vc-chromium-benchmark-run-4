715caafabac9a6feff6b7a80550a354672479098
Brian Ho <hob@chromium.org>
2022-06-29 20:01:58 +0000

    viz: Reject overlay candidates if there is no cursor plane
    Some platforms (e.g. AMD) lack a dedicated cursor plane, so the
    display hardware blits the cursor on topmost plane instead [1]. If
    the topmost plane is scaled/translated (e.g. ARC++ app or video),
    the cursor will then be transformed along with it in the final
    presentation.  Thus, we need to reject the topmost candidate if its
    buffer is transformed at all or else the resulting cursor
    position/size will be incorrect.
    
    [1] https://www.mail-archive.com/amd-gfx@lists.freedesktop.org/msg66991.html
    
    Bug: b:194335274
    Change-Id: I156ab965e3d1a4c8a958c69384044cc467292ecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721360
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Yacoub <markyacoub@google.com>
    Commit-Queue: Brian Ho <hob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019308}

--- ui/ozone/platform/drm/gpu/drm_device.cc
+++ ui/ozone/platform/drm/gpu/drm_device.cc
@@ -679,4 +679,8 @@ bool DrmDevice::SetGammaRamp(
                               &g[0], &b[0]) == 0);
 }
 
+absl::optional<std::string> DrmDevice::GetDriverName() const {
+  return GetDrmDriverNameFromFd(file_.GetPlatformFile());
+}
+
 }  // namespace ui
