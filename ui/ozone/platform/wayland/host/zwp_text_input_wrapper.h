01b7e5d4a510ad0e6c6388604ae90d18bc7e111f
Jing Wang <jiwan@chromium.org>
2022-06-29 00:55:25 +0000

    grammar: support grammar in lacros browser
    Add new wayland APIs to transfer grammar check results between ash and
    lacros, so that grammar is now fully working in lacros browser.
    
    Test: manually tested on DUT that grammar check works properly in
    lacros chrome.
    
    Bug: b:225063764
    Change-Id: I6ab6482e962c7dcccad5304a6a477e8281c13c66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663982
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018915}

--- ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
+++ ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/string_piece.h"
+#include "ui/base/ime/grammar_fragment.h"
 #include "ui/base/ime/text_input_mode.h"
 #include "ui/base/ime/text_input_type.h"
 
@@ -69,6 +70,14 @@ class ZWPTextInputWrapperClient {
                                   uint32_t length,
                                   const std::vector<SpanStyle>& spans) = 0;
 
+  // Called when client needs to clear all grammar fragments in |range|. All
+  // indices are measured in UTF-8 bytes.
+  virtual void OnClearGrammarFragments(const gfx::Range& range) = 0;
+
+  // Called when client requests to add a new grammar marker. All indices are
+  // measured in UTF-8 bytes.
+  virtual void OnAddGrammarFragment(const ui::GrammarFragment& fragment) = 0;
+
   // Called when the visibility state of the input panel changed.
   // There's no detailed spec of |state|, and no actual implementor except
   // components/exo is found in the world at this moment.
@@ -108,6 +117,9 @@ class ZWPTextInputWrapper {
                               ui::TextInputMode mode,
                               uint32_t flags,
                               bool should_do_learning) = 0;
+
+  virtual void SetGrammarFragmentAtCursor(
+      const ui::GrammarFragment& fragment) = 0;
 };
 
 }  // namespace ui
