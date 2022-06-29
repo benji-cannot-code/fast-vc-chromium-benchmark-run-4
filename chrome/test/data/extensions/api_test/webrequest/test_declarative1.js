1ddfb65869de70bfdd13d8d30476e4740fc84d0b
David Bertoni <dbertoni@chromium.org>
2022-06-29 23:05:25 +0000

    [Extensions] Re-enable the WebRequestDeclarative1 test.
    This test was disabled for flakiness and became stale. Some of the tests are still broken or flaky, so this CL splits the tests into a set of tests that work and are not flaky, and a set of tests are are broken and/or flaky.
    
    Bug: 733395, 846555
    Change-Id: I54257e30b33c1f280bd371acf1bbd1e5014b2c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727778
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019379}

--- chrome/test/data/extensions/api_test/webrequest/test_declarative1.js
+++ chrome/test/data/extensions/api_test/webrequest/test_declarative1.js
@@ -145,7 +145,7 @@ function cancelThirdPartyExpectedOrder() {
     ];
 }
 
-runTests([
+let allTests = [
 
   function testCancelRequest() {
     ignoreUnexpected = true;
@@ -545,4 +545,36 @@ runTests([
       function() {navigateAndWait(getURLHttpSimple());}
     );
   },
-  ]);
+];
+
+// All tests that are currently working.
+let workingTests = [
+  'testCancelRequest',
+  'testPostponeCancelRequest',
+  'testSiteForCookiesUrl',
+  'testRedirectRequest',
+  'testRedirectRequestByContentType',
+  'testRedirectByRegEx',
+  'testRegexFilter',
+];
+
+// All tests that are broken or flaky.
+let brokenTests = [
+  'testThirdParty',  // Generates unexpected events.
+  'testFirstParty',  // Generates unexpected events,
+  'testRedirectRequest2',  // Hangs.
+];
+
+chrome.test.getConfig(function(config) {
+  let args = JSON.parse(config.customArg);
+  if (args.testSuite == 'normal') {
+    runTests(allTests.filter(function(op) {
+      return workingTests.includes(op.name);
+    }));
+  } else {
+    chrome.test.assertEq('broken', args.testSuite);
+    runTests(allTests.filter(function(op) {
+      return brokenTests.includes(op.name);
+    }));
+  }
+});
