fde3231d3cd4a739e92b55a1112a1285a3d0529a
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-06-30 02:16:16 +0000

    [Extensions Offscreen Documents] Prevent navigation
    Offscreen documents will be managed through a dedicated API, and should
    not be able to navigate, even to another extension resource. Restrict
    offscreen document navigation in the ExtensionNavigationThrottle.
    
    Add a test to cover the same.
    
    Bug: 1339382
    Change-Id: I0f653e48e925a4e44ef12bc31e249becae723a41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732670
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019426}

--- extensions/browser/extension_host_registry.cc
+++ extensions/browser/extension_host_registry.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/extension_host_registry.h"
 
+#include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/no_destructor.h"
 #include "base/observer_list.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+#include "content/public/browser/render_frame_host.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extensions_browser_client.h"
 
@@ -160,6 +162,18 @@ std::vector<ExtensionHost*> ExtensionHostRegistry::GetHostsForExtension(
       hosts.push_back(host);
   }
   return hosts;
+}
+
+ExtensionHost* ExtensionHostRegistry::GetExtensionHostForPrimaryMainFrame(
+    content::RenderFrameHost* render_frame_host) {
+  DCHECK(render_frame_host->IsInPrimaryMainFrame())
+      << "GetExtensionHostForPrimaryMainFrame() should only be called with "
+      << "the primary main frame.";
+  for (ExtensionHost* host : extension_hosts_) {
+    if (host->main_frame_host() == render_frame_host)
+      return host;
+  }
+  return nullptr;
 }
 
 void ExtensionHostRegistry::AddObserver(Observer* observer) {
