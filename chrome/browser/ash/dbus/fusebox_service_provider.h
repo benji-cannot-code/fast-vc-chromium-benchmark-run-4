661dd465c81f1a9bba6cbaf846be4545b0a200d6
Nigel Tao <nigeltao@chromium.org>
2022-06-30 04:28:28 +0000

    fusebox: factor out fusebox_server.cc
    This splits the existing fusebox_service_provider.cc file into two: one
    half focused on "business logic" and one half focused on "D-Bus protocol
    logic". For example, this makes it easier to confirm that D-Bus requests
    and responses have the same "shape" (the same number and type of
    arguments) regardless of the code path taken to serve that request.
    
    Bug: 1249754
    Change-Id: I48c8e237b1a784c5c30e0ccdf467901b504daf36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720980
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019470}

--- chrome/browser/ash/dbus/fusebox_service_provider.h
+++ chrome/browser/ash/dbus/fusebox_service_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ash/fusebox/fusebox_server.h"
 #include "chromeos/ash/components/dbus/services/cros_dbus_service.h"
 #include "dbus/exported_object.h"
 #include "storage/browser/file_system/file_system_context.h"
@@ -43,6 +44,8 @@ class FuseBoxServiceProvider
                dbus::ExportedObject::ResponseSender sender);
   void Stat(dbus::MethodCall* method_call,
             dbus::ExportedObject::ResponseSender sender);
+
+  fusebox::Server server_;
 
   // base::WeakPtr{this} factory.
   base::WeakPtrFactory<FuseBoxServiceProvider> weak_ptr_factory_{this};
