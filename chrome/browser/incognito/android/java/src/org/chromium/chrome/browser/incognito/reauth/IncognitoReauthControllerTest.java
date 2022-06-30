c292fdaa95a73f9433f8a904bbe83a0ac212e6eb
Rohit Agarwal <roagarwal@chromium.org>
2022-06-30 18:43:15 +0000

    [Android][IncognitoReauth] Add back press logic for full-screen re-auth.
    This CL:
    a) Doesn't connect the back press logic within the actual re-auth
    screen. The actual connection would be followed up later.
    
    b) Only adds the back press logic for re-auth full screen.
    
    c) Only adds the back press logic if done within the context of
    ChromeTabbedActivity. CustomTabActivity would be followed later.
    
    d) Supplies opening the regular overview mode, inside the back press
    logic.
    
    Bug: 1227656
    Change-Id: Icae2dbee3d21a9134ad51fdee37648f305140868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736382
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019760}

--- chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerTest.java
+++ chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerTest.java
@@ -80,6 +80,8 @@ public class IncognitoReauthControllerTest {
     private UserPrefs.Natives mUserPrefsJniMock;
     @Mock
     private PrefService mPrefServiceMock;
+    @Mock
+    private Runnable mBackPressInReauthFullScreenRunnableMock;
 
     @Captor
     ArgumentCaptor<TabModelSelectorObserver> mTabModelSelectorObserverCaptor;
@@ -151,11 +153,11 @@ public class IncognitoReauthControllerTest {
         mLayoutStateProviderOneshotSupplier = new OneshotSupplierImpl<>();
         mLayoutStateProviderOneshotSupplier.set(mLayoutStateProviderMock);
         mProfileObservableSupplier = new ObservableSupplierImpl<>();
-        IncognitoReauthManager.setIsIncognitoReauthFeatureAvailableForTesting(true);
 
         mIncognitoReauthController = new IncognitoReauthController(mTabModelSelectorMock,
                 mActivityLifecycleDispatcherMock, mLayoutStateProviderOneshotSupplier,
-                mProfileObservableSupplier, mIncognitoReauthCoordinatorFactoryMock);
+                mProfileObservableSupplier, mIncognitoReauthCoordinatorFactoryMock,
+                mBackPressInReauthFullScreenRunnableMock);
         mProfileObservableSupplier.set(mProfileMock);
     }
 
