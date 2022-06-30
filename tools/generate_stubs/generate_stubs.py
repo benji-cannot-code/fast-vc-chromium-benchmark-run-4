a8d8270df92b0f049587e4b8c48340522656b92a
Byoungchan Lee <daniel.l@hpcnt.com>
2022-06-30 14:57:36 +0000

    Make generate_stubs can work without //base
    Like base/logging.h can be overridden, make base/compiler_specific.h
    header or //base dependency overridable.
    
    Bug: webrtc:13662
    Change-Id: I07527f9d38bcc1e0d0faaada047f02c03c8f46a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721997
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Daniel.L (Byoungchan) Lee <daniel.l@hpcnt.com>
    Cr-Commit-Position: refs/heads/main@{#1019629}

--- tools/generate_stubs/generate_stubs.py
+++ tools/generate_stubs/generate_stubs.py
@@ -179,7 +179,7 @@ IMPLEMENTATION_PREAMBLE = """// This is generated file. Do not modify directly.
 
 #include <cstdarg>
 
-#include "base/compiler_specific.h"
+#include "%(macro_include)s"
 #include "%(logging_include)s"
 """
 
@@ -533,8 +533,7 @@ class PosixStubWriter(object):
   functions plus initialization code for them.
   """
 
-  def __init__(self, module_name, export_macro, signatures, logging_function,
-               logging_include):
+  def __init__(self, module_name, export_macro, signatures, logging_function):
     """Initializes PosixStubWriter for this set of signatures and module_name.
 
     Args:
@@ -544,13 +543,11 @@ class PosixStubWriter(object):
       signatures: The list of signature hashes, as produced by ParseSignatures,
                   to create stubs for.
       logging_function: Function call that will be used for error logging.
-      logging_include: Header file where the logging function is defined.
     """
     self.signatures = signatures
     self.module_name = module_name
     self.export_macro = export_macro
     self.logging_function = logging_function
-    self.logging_include = logging_include
 
   @classmethod
   def CStyleIdentifier(cls, identifier):
@@ -708,17 +705,20 @@ class PosixStubWriter(object):
       }
 
   @classmethod
-  def WriteImplementationPreamble(cls, header_path, outfile, logging_include):
+  def WriteImplementationPreamble(cls, header_path, outfile, logging_include,
+                                  macro_include):
     """Write the necessary includes for the implementation file.
 
     Args:
       header_path: The path to the header file.
       outfile: The file handle to populate.
     """
-    outfile.write(IMPLEMENTATION_PREAMBLE % {
-        'header_path': header_path,
-        'logging_include': logging_include
-    })
+    outfile.write(
+        IMPLEMENTATION_PREAMBLE % {
+            'header_path': header_path,
+            'logging_include': logging_include,
+            'macro_include': macro_include,
+        })
 
   @classmethod
   def WriteUmbrellaInitializer(cls, module_names, namespace, outfile,
@@ -996,6 +996,10 @@ def CreateOptionParser():
                     dest='logging_include',
                     default='base/logging.h',
                     help=('Header file where the logging function is defined.'))
+  parser.add_option('--macro-include',
+                    dest='macro_include',
+                    default='base/compiler_specific.h',
+                    help=('Header file where DISABLE_CFI_ICALL is defined.'))
 
   return parser
 
@@ -1126,8 +1130,8 @@ def CreateWindowsDefForSigFiles(sig_files, out_dir, module_name):
 
 def CreatePosixStubsForSigFiles(sig_files, stub_name, out_dir, intermediate_dir,
                                 path_from_source, extra_stub_header,
-                                export_macro, logging_function,
-                                logging_include):
+                                export_macro, logging_function, logging_include,
+                                macro_include):
   """Create a POSIX stub library with a module for each signature file.
 
   Args:
@@ -1143,6 +1147,7 @@ def CreatePosixStubsForSigFiles(sig_files, stub_name, out_dir, intermediate_dir,
                   an EXPORT marking, to control visibility.
     logging_function: Function call that will be used for error logging.
     logging_include: Header file where the logging function is defined.
+    header_include: Header file where the logging function is defined.
   """
   header_base_name = stub_name + '.h'
   header_path = os.path.join(out_dir, header_base_name)
@@ -1159,7 +1164,7 @@ def CreatePosixStubsForSigFiles(sig_files, stub_name, out_dir, intermediate_dir,
     # Open the file, and create the preamble which consists of a file
     # header plus any necessary includes.
     PosixStubWriter.WriteImplementationPreamble(header_include_path, impl_file,
-                                                logging_include)
+                                                logging_include, macro_include)
     if extra_stub_header is not None:
       extra_header_file = open(extra_stub_header, 'r')
       try:
@@ -1183,8 +1188,7 @@ def CreatePosixStubsForSigFiles(sig_files, stub_name, out_dir, intermediate_dir,
         signatures = ParseSignatures(infile)
       finally:
         infile.close()
-      writer = PosixStubWriter(name, export_macro, signatures, logging_function,
-                               logging_include)
+      writer = PosixStubWriter(name, export_macro, signatures, logging_function)
       writer.WriteImplementationContents(namespace, impl_file)
 
     # Lastly, output the umbrella function for the file.
@@ -1217,7 +1221,7 @@ def main():
                                 intermediate_dir, options.path_from_source,
                                 options.extra_stub_header, options.export_macro,
                                 options.logging_function,
-                                options.logging_include)
+                                options.logging_include, options.macro_include)
   elif options.type == FILE_TYPE_WIN_DEF:
     CreateWindowsDefForSigFiles(args, out_dir, options.module_name)
 
