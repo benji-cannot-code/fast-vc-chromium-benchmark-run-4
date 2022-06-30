581934f0e728df3db653fb313b5f918930b93938
Dave Tapuska <dtapuska@chromium.org>
2022-06-30 14:13:57 +0000

    Do not call trace/probe events if frame is detached.
    The DispatchLoadEvent can be PostTasked and the frame can become
    disconnected before it executes but not collected. This can cause
    tracing to call methods after it has been detached. In
    crrev.com/1018696 a null frame was accessed (even thought it would have
    been before this showed up in some crash reports).
    
    BUG=1314858,1340483
    
    Change-Id: I4356e9e9d0ce2d59b550ea7bd49e6437efa67fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735525
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019614}

--- third_party/blink/renderer/core/inspector/inspector_trace_events.cc
+++ third_party/blink/renderer/core/inspector/inspector_trace_events.cc
@@ -1128,6 +1128,7 @@ void inspector_paint_event::Data(perfetto::TracedValue context,
 }
 
 void FrameEventData(perfetto::TracedDictionary& dict, LocalFrame* frame) {
+  DCHECK(frame);
   dict.Add("isMainFrame", frame->IsMainFrame());
   dict.Add("isOutermostMainFrame", frame->IsOutermostMainFrame());
   // TODO(dgozman): this does not work with OOPIF, so everyone who
@@ -1136,6 +1137,7 @@ void FrameEventData(perfetto::TracedDictionary& dict, LocalFrame* frame) {
 }
 
 void FillCommonFrameData(perfetto::TracedDictionary& dict, LocalFrame* frame) {
+  DCHECK(frame);
   dict.Add("frame", IdentifiersFactory::FrameId(frame));
   dict.Add("url", UrlForFrame(frame));
   dict.Add("name", frame->Tree().GetName());
