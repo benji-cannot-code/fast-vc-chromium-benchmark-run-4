0f7cfe46ba42ac466ad9eb8219ff5980cfb93438
minch <minch@chromium.org>
2022-06-30 17:07:30 +0000

    dark_mode: Small polish of PillButton
    Definition of function `GetPillButtonTextColor` should be removed from
    PillButton class in crrev.com/c/3425796.
    
    Bug: None
    Change-Id: I9ef8d9e69e10125fbefa0305e539fe0147a2a1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736216
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019700}

--- ash/style/pill_button.h
+++ ash/style/pill_button.h
@@ -75,9 +75,6 @@ class ASH_EXPORT PillButton : public views::LabelButton {
   void SetUseDefaultLabelFont();
 
  private:
-  // Get text's color depending on the type used.
-  SkColor GetPillButtonTextColor(Type type);
-
   const Type type_;
   const gfx::VectorIcon* const icon_;
 
