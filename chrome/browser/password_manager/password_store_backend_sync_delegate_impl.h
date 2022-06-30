3dd0d15751f0e51194bfc0a90c09b8f1f55b2d3f
Maria Kazinova <kazinova@google.com>
2022-06-30 17:20:10 +0000

    Revert "[UPM] Remove backend SyncDelegate from PasswordStoreAndroidBackend"
    This reverts commit 7a5e8ddcc307122e062405a3d068fc043f4c7748.
    
    Reason for revert: Causes crashes: crbug.com/1340829
    
    Original change's description:
    > [UPM] Remove backend SyncDelegate from PasswordStoreAndroidBackend
    >
    > This CL:
    > 1) Removes PasswordStoreBackend::SyncDelegate usage from
    > PasswordStoreAndroidBackend (because SyncDelegate duplicates
    > functionality already provided by the access to SyncService)
    > 2) Moves GetSyncingAccount() method from SyncDelegate to
    > password_sync_util.
    >
    > Bug: 1327686
    > Change-Id: I2585437035d9d96b9569b3d65a53da082bd20b5a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721317
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Maria Kazinova <kazinova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1019041}
    
    Bug: 1327686
    Change-Id: I310d35e33b2b10e790645906462b0224bd8b3775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738319
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019708}

--- chrome/browser/password_manager/password_store_backend_sync_delegate_impl.h
+++ chrome/browser/password_manager/password_store_backend_sync_delegate_impl.h
@@ -20,6 +20,7 @@ class PasswordStoreBackendSyncDelegateImpl
  private:
   // SyncDelegate implementation.
   bool IsSyncingPasswordsEnabled() override;
+  absl::optional<std::string> GetSyncingAccount() override;
 
   base::raw_ptr<Profile> profile_;
 };
--- components/password_manager/core/browser/mock_password_store_backend.h
+++ components/password_manager/core/browser/mock_password_store_backend.h
@@ -27,6 +27,8 @@ class MockPasswordBackendSyncDelegate
   ~MockPasswordBackendSyncDelegate() override;
 
   MOCK_METHOD(bool, IsSyncingPasswordsEnabled, (), (override));
+
+  MOCK_METHOD(absl::optional<std::string>, GetSyncingAccount, (), (override));
 };
 
 class MockPasswordStoreBackend : public PasswordStoreBackend {
