dd636830508267e87d78641e56152983bef8e09d
Mikhail Khokhlov <khokhlov@google.com>
2022-06-30 11:55:25 +0000

    Optimize query in FindFirstPageURLForHost
    Data from 99th percentile power investigation show that FindFirstPageURLForHost can be quite expensive in some cases.
    It seems that we can make it significantly faster by optimizing
    the SQL query in it.
    
    This CL removes the use of LIKE operator and substitutes it with
    string comparisons. Local synthetic test shows ~20x speed improvement
    for this query.
    
    Bug: 1270809
    Change-Id: Icb33828c5bf09a90196428eebd632282175fc471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3731810
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019569}

---
+++ components/database_utils/upper_bound_string.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/database_utils/upper_bound_string.h"
+
+#include <limits>
+
+namespace database_utils {
+
+std::string UpperBoundString(const std::string& prefix) {
+  std::string upper_bound = prefix;
+  upper_bound.push_back(std::numeric_limits<unsigned char>::max());
+  return upper_bound;
+}
+
+}  // namespace database_utils
