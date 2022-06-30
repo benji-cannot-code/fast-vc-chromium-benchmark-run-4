bbe143b92e1950b0696080f6af2d6d0f0bf3cd60
Peter Kasting <pkasting@chromium.org>
2022-06-30 17:18:48 +0000

    Prevent unsafe narrowing: base/ for Linux part 1 of n
    Bug: 1292951
    Change-Id: I16708061d2357b3e8c68e1cc00a0502644308f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736689
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019705}

--- base/debug/buffered_dwarf_reader.cc
+++ base/debug/buffered_dwarf_reader.cc
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifdef USE_SYMBOLIZE
 
-#include "base/third_party/symbolize/symbolize.h"
-
 #include <algorithm>
 #include <cstring>
 
-namespace base {
-namespace debug {
+#include "base/numerics/safe_conversions.h"
+#include "base/third_party/symbolize/symbolize.h"
+
+namespace base::debug {
 
 BufferedDwarfReader::BufferedDwarfReader(int fd, uint64_t position)
     : fd_(fd), next_chunk_start_(position), last_chunk_start_(position) {}
@@ -21,10 +21,10 @@ BufferedDwarfReader::BufferedDwarfReader(int fd, uint64_t position)
 size_t BufferedDwarfReader::ReadCString(uint64_t max_position,
                                         char* out,
                                         size_t out_size) {
-  uint8_t character;
+  char character;
   size_t bytes_written = 0;
   do {
-    if (!ReadInt8(character)) {
+    if (!ReadChar(character)) {
       return 0;
     }
 
@@ -177,15 +177,18 @@ bool BufferedDwarfReader::BufferedRead(void* out, const size_t bytes) {
   while (bytes_left > 0) {
     // Refresh the buffer.
     if (unconsumed_amount_ == 0) {
-      unconsumed_amount_ =
-          google::ReadFromOffset(fd_, buf_, sizeof(buf_), next_chunk_start_);
-      if (unconsumed_amount_ == 0) {
+      if (!base::IsValueInRangeForNumericType<off_t>(next_chunk_start_))
+        return false;
+      const ssize_t unconsumed_amount = google::ReadFromOffset(
+          fd_, buf_, sizeof(buf_), static_cast<off_t>(next_chunk_start_));
+      if (unconsumed_amount_ <= 0) {
         // Read error.
         return false;
       }
+      unconsumed_amount_ = static_cast<size_t>(unconsumed_amount);
 
       last_chunk_start_ = next_chunk_start_;
-      next_chunk_start_ = next_chunk_start_ + unconsumed_amount_;
+      next_chunk_start_ += unconsumed_amount_;
       cursor_in_buffer_ = 0;
     }
 
@@ -198,7 +201,6 @@ bool BufferedDwarfReader::BufferedRead(void* out, const size_t bytes) {
   return true;
 }
 
-}  // namespace debug
-}  // namespace base
+}  // namespace base::debug
 
 #endif
