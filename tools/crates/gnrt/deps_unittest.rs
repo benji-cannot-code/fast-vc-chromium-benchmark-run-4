4fb0e36083788832fd7447b0b9116fac1fcbbd2f
Collin Baker <collinbaker@chromium.org>
2022-06-30 20:52:46 +0000

    Collect dependencies for each third-party crate
    The gnrt implementation collected a flat list of all transitive
    dependencies. However, each dependency's own dependency list will be
    needed when generating build files.
    
    Add logic to collect this list for each third-party dep.
    
    Bug: 1291994
    Change-Id: I771127cd9576b84273ca171c3cfc4a12e72213c6
    
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,android-rust-arm-rel,linux-rust-x64-dbg,android-rust-arm-dbg
    Change-Id: I771127cd9576b84273ca171c3cfc4a12e72213c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735353
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019816}

--- tools/crates/gnrt/deps_unittest.rs
+++ tools/crates/gnrt/deps_unittest.rs
@@ -3,13 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use rust_gtest_interop::prelude::*;
-
 use gnrt_lib::deps::*;
+
+use std::str::FromStr;
+
+use cargo_platform::Platform;
+use rust_gtest_interop::prelude::*;
 
 #[gtest(DepsTest, CollectDependenciesOnSampleOutput)]
 fn test() {
-    use gnrt_lib::crates::Epoch;
+    use gnrt_lib::crates::{Epoch, NormalizedName};
 
     let metadata: cargo_metadata::Metadata = serde_json::from_str(SAMPLE_CARGO_METADATA).unwrap();
     let mut dependencies = collect_dependencies(&metadata);
@@ -18,6 +21,8 @@ fn test() {
     });
 
     let empty_str_slice: &'static [&'static str] = &[];
+
+    expect_eq!(dependencies.len(), 12);
 
     expect_eq!(dependencies[0].package_name, "cc");
     expect_eq!(dependencies[0].epoch, Epoch::Major(1));
@@ -60,12 +65,50 @@ fn test() {
         dependencies[5].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
         &["default", "derive", "serde_derive", "std"]
     );
+    expect_eq!(dependencies[5].dependencies.len(), 1);
+    expect_eq!(dependencies[5].build_dependencies.len(), 0);
+    expect_eq!(dependencies[5].dev_dependencies.len(), 0);
+    expect_eq!(
+        dependencies[5].dependencies[0],
+        DepOfDep {
+            normalized_name: NormalizedName::new("serde_derive").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
 
     expect_eq!(dependencies[6].package_name, "serde_derive");
     expect_eq!(dependencies[6].epoch, Epoch::Major(1));
     expect_eq!(
         dependencies[6].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
         &["default"]
+    );
+    expect_eq!(dependencies[6].dependencies.len(), 3);
+    expect_eq!(dependencies[6].build_dependencies.len(), 0);
+    expect_eq!(dependencies[6].dev_dependencies.len(), 0);
+    expect_eq!(
+        dependencies[6].dependencies[0],
+        DepOfDep {
+            normalized_name: NormalizedName::new("proc_macro2").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
+    expect_eq!(
+        dependencies[6].dependencies[1],
+        DepOfDep {
+            normalized_name: NormalizedName::new("quote").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
+    expect_eq!(
+        dependencies[6].dependencies[2],
+        DepOfDep {
+            normalized_name: NormalizedName::new("syn").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
     );
 
     expect_eq!(dependencies[7].package_name, "syn");
@@ -74,6 +117,33 @@ fn test() {
         dependencies[7].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
         &["clone-impls", "default", "derive", "parsing", "printing", "proc-macro", "quote"]
     );
+    expect_eq!(dependencies[7].dependencies.len(), 3);
+    expect_eq!(dependencies[7].build_dependencies.len(), 0);
+    expect_eq!(dependencies[7].dev_dependencies.len(), 0);
+    expect_eq!(
+        dependencies[7].dependencies[0],
+        DepOfDep {
+            normalized_name: NormalizedName::new("proc_macro2").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
+    expect_eq!(
+        dependencies[7].dependencies[1],
+        DepOfDep {
+            normalized_name: NormalizedName::new("quote").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
+    expect_eq!(
+        dependencies[7].dependencies[2],
+        DepOfDep {
+            normalized_name: NormalizedName::new("unicode_ident").unwrap(),
+            epoch: Epoch::Major(1),
+            platform: None,
+        }
+    );
 
     expect_eq!(dependencies[8].package_name, "termcolor");
     expect_eq!(dependencies[8].epoch, Epoch::Major(1));
@@ -81,12 +151,62 @@ fn test() {
         dependencies[8].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
         empty_str_slice
     );
+    expect_eq!(dependencies[8].dependencies.len(), 1);
+    expect_eq!(dependencies[8].build_dependencies.len(), 0);
+    expect_eq!(dependencies[8].dev_dependencies.len(), 0);
+    expect_eq!(
+        dependencies[8].dependencies[0],
+        DepOfDep {
+            normalized_name: NormalizedName::new("winapi_util").unwrap(),
+            epoch: Epoch::Minor(1),
+            platform: Some(Platform::from_str("cfg(windows)").unwrap()),
+        }
+    );
 
     expect_eq!(dependencies[9].package_name, "unicode-ident");
     expect_eq!(dependencies[9].epoch, Epoch::Major(1));
     expect_eq!(
         dependencies[9].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
         empty_str_slice
+    );
+
+    expect_eq!(dependencies[10].package_name, "winapi");
+    expect_eq!(dependencies[10].epoch, Epoch::Minor(3));
+    expect_eq!(
+        dependencies[10].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
+        &[
+            "consoleapi",
+            "errhandlingapi",
+            "fileapi",
+            "minwindef",
+            "processenv",
+            "std",
+            "winbase",
+            "wincon",
+            "winerror",
+            "winnt"
+        ]
+    );
+    expect_eq!(dependencies[10].dependencies.len(), 0);
+    expect_eq!(dependencies[10].build_dependencies.len(), 0);
+    expect_eq!(dependencies[10].dev_dependencies.len(), 0);
+
+    expect_eq!(dependencies[11].package_name, "winapi-util");
+    expect_eq!(dependencies[11].epoch, Epoch::Minor(1));
+    expect_eq!(
+        dependencies[11].dependency_kinds.get(&DependencyKind::Normal).unwrap().features,
+        empty_str_slice
+    );
+    expect_eq!(dependencies[11].dependencies.len(), 1);
+    expect_eq!(dependencies[11].build_dependencies.len(), 0);
+    expect_eq!(dependencies[11].dev_dependencies.len(), 0);
+    expect_eq!(
+        dependencies[11].dependencies[0],
+        DepOfDep {
+            normalized_name: NormalizedName::new("winapi").unwrap(),
+            epoch: Epoch::Minor(3),
+            platform: Some(Platform::from_str("cfg(windows)").unwrap()),
+        }
     );
 }
 
