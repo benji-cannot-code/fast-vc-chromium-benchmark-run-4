87689020835b19a4c7a983e63cba54fb95ab5c20
Jérôme Lebel <jlebel@chromium.org>
2022-06-30 16:56:59 +0000

    [iOS] Add EGTests for UMA in 2 steps MICE FRE
    Tests FRE with default UMA value, and test FRE when turning off UMA.
    
    Bug: 1337718
    Change-Id: Ifa7d7801d6b8b68fe0f7238d26a71e6de1fd5cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3716603
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019692}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
@@ -420,6 +421,8 @@ bool GetStatusForSigninPolicy() {
                         IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_TEXT
                   detailStringID:
                       IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_DETAIL];
+      improveChromeItem.accessibilityIdentifier =
+          kImproveChromeItemAccessibilityIdentifier;
       [items addObject:improveChromeItem];
     }
     if (self.userPrefService->IsManagedPreference(
