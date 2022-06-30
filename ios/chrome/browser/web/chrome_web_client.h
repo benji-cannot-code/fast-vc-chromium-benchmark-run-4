fb04c46db70c0f29ab31af297f717067161a5769
Gauthier Ambard <gambard@chromium.org>
2022-06-30 09:11:16 +0000

    [iOS] Fix the logging for the default mode on page load
    This CL makes sure to record the IOS.PageLoad.DefaultModeMobile metric
    at the same time as the official PageLoad metric.
    Previously it was called too many times (in particular, it was called
    for iframes).
    
    Fixed: 1340297
    Change-Id: I82e53331d1d44796c31360dc41afaf83ed660748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733208
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019523}

--- ios/chrome/browser/web/chrome_web_client.h
+++ ios/chrome/browser/web/chrome_web_client.h
@@ -55,7 +55,9 @@ class ChromeWebClient : public web::WebClient {
   bool EnableLongPressAndForceTouchHandling() const override;
   bool EnableLongPressUIContextMenu() const override;
   web::UserAgentType GetDefaultUserAgent(web::WebState* web_state,
-                                         const GURL& url) override;
+                                         const GURL& url) const override;
+  void LogDefaultUserAgent(web::WebState* web_state,
+                           const GURL& url) const override;
   bool RestoreSessionFromCache(web::WebState* web_state) const override;
   void CleanupNativeRestoreURLs(web::WebState* web_state) const override;
   void WillDisplayMediaCapturePermissionPrompt(
