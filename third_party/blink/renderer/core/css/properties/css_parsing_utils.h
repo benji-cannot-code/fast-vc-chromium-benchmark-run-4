85b10873006a552b2ec09a0de9bb41778cfc9f47
L. David Baron <dbaron@chromium.org>
2022-06-30 20:39:32 +0000

    Update CSS toggles parsing/computation code to spec changes.
    (These properties are behind a runtime flag that is off.)
    
    I believe the complete list of changes is:
    
     - Toggle states can be described as either custom idents or integers
       rather than only as integers;
       the set of named states (if given) is given inside [] in the
       toggle-root (or toggle) properties; a single named state is given as
       the initial value in toggle-root (or toggle) and after the set verb
       in toggle-trigger.
    
     - toggle-root (and toggle) take <toggle-states> [at <initial-state>]?
       rather than [<initial-state> /]? <maximum-state>.
    
     - toggle-root (and toggle) take cycle | cycle-on | sticky, where cycle
       is the default, where there was previously only a (non-default)
       sticky boolean.
    
     - toggle-trigger has prev/next/set verbs, rather than just taking an
       <integer> (implying set) or nothing (implying 'next 1').
    
    Bug: 1250716
    Change-Id: I9f3f2c5d2a681b4e8639ac64d921e2b02ff297a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735886
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019810}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.h
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.h
@@ -155,6 +155,8 @@ CSSIdentifierValue* ConsumeIdent(CSSParserTokenRange&);
 
 CSSCustomIdentValue* ConsumeCustomIdent(CSSParserTokenRange&,
                                         const CSSParserContext&);
+CSSCustomIdentValue* ConsumeCustomIdentConservatively(CSSParserTokenRange&,
+                                                      const CSSParserContext&);
 CSSCustomIdentValue* ConsumeDashedIdent(CSSParserTokenRange&,
                                         const CSSParserContext&);
 CSSStringValue* ConsumeString(CSSParserTokenRange&);
