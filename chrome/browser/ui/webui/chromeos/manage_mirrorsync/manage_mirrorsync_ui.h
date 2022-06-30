c79ae9a5e91835018d9db8ec849eb14bedb614c1
Ben Reich <benreich@chromium.org>
2022-06-30 02:34:00 +0000

    [MirrorSync] Add dialog to manage MirrorSync settings
    MirrorSync will be available via a few entry points so follow the same
    method as the SMB shares dialog to ensure the various entry surfaces can
    load the dialog. This adds a basic skeleton dialog with a Mojo pipe that
    will be used to access DriveFS interface and receive messages from the
    delegate.
    
    Bug: b:237066325
    Test: chromeos-on-linux and ensure chrome://manage-mirrorsync loads
    Test: Disable flag and ensure it doesn't load
    Change-Id: If96a4a775f7db5526acda572b54df389056b16c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722143
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019455}

---
+++ chrome/browser/ui/webui/chromeos/manage_mirrorsync/manage_mirrorsync_ui.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_MANAGE_MIRRORSYNC_MANAGE_MIRRORSYNC_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_MANAGE_MIRRORSYNC_MANAGE_MIRRORSYNC_UI_H_
+
+#include "ui/web_dialogs/web_dialog_ui.h"
+
+namespace chromeos {
+
+// The WebUI for chrome://manage-mirrorsync
+class ManageMirrorSyncUI : public ui::MojoWebDialogUI {
+ public:
+  explicit ManageMirrorSyncUI(content::WebUI* web_ui);
+
+  ManageMirrorSyncUI(const ManageMirrorSyncUI&) = delete;
+  ManageMirrorSyncUI& operator=(const ManageMirrorSyncUI&) = delete;
+
+  ~ManageMirrorSyncUI() override;
+
+ private:
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_MANAGE_MIRRORSYNC_MANAGE_MIRRORSYNC_UI_H_
