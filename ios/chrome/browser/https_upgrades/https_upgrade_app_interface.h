5638b68fb4aa36a43cf5eb4bb396bcb9842fb677
Mustafa Emre Acer <meacer@chromium.org>
2022-06-30 08:05:09 +0000

    [ios] Implement Omnibox HTTPS upgrade fallback
    This CL implements the initial version of Omnibox HTTPS Upgrades (aka
    Typed Navigation Upgrades) on iOS.
    
    This feature automatically upgrades scheme-less navigations to https://.
    If the user types "example.com" in the omnibox (without http:// in
    front), previously Chrome would load http://example.com. With this
    change, it will now attempt to load https://example.com first. If the
    load fails, it will fall back to http://example.com.
    
    The upgrade is done by the autocomplete code. This CL introduces the
    fallback logic and corresponding tests.
    
    See the design doc at https://docs.google.com/document/d/1YfO5wCR-nC8-hxB_o3Ic--Cxcur3foTJWH2pxp363Dk/edit for details.
    
    Bug: 1195440
    Change-Id: Iea52e9aa2df54acc3c63ff65fc015b2fcbf227f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682242
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019512}

--- ios/chrome/browser/https_upgrades/https_upgrade_app_interface.h
+++ ios/chrome/browser/https_upgrades/https_upgrade_app_interface.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface HttpsUpgradeAppInterface : NSObject
 
 + (void)setHTTPSPortForTesting:(int)HTTPSPort useFakeHTTPS:(bool)useFakeHTTPS;
++ (void)setFallbackHttpPortForTesting:(int)HTTPPort;
 + (void)setFallbackDelayForTesting:(int)fallbackDelayInMilliseconds;
 + (BOOL)isTimerRunning;
 + (void)clearAllowlist;
--- ios/chrome/browser/https_upgrades/https_upgrade_app_interface.mm
+++ ios/chrome/browser/https_upgrades/https_upgrade_app_interface.mm
@@ -31,9 +31,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ->SetHttpsPortForTesting(HTTPSPort, useFakeHTTPS);
 }
 
++ (void)setFallbackHttpPortForTesting:(int)HTTPPort {
+  HttpsUpgradeServiceFactory::GetForBrowserState(
+      chrome_test_util::GetOriginalBrowserState())
+      ->SetFallbackHttpPortForTesting(HTTPPort);
+
+  HttpsUpgradeServiceFactory::GetForBrowserState(
+      chrome_test_util::GetCurrentIncognitoBrowserState())
+      ->SetFallbackHttpPortForTesting(HTTPPort);
+}
+
 + (void)setFallbackDelayForTesting:(int)fallbackDelayInMilliseconds {
-  web::WebState* web_state = chrome_test_util::GetCurrentWebState();
-  HttpsOnlyModeUpgradeTabHelper::FromWebState(web_state)
+  HttpsUpgradeServiceFactory::GetForBrowserState(
+      chrome_test_util::GetOriginalBrowserState())
+      ->SetFallbackDelayForTesting(
+          base::Milliseconds(fallbackDelayInMilliseconds));
+
+  HttpsUpgradeServiceFactory::GetForBrowserState(
+      chrome_test_util::GetCurrentIncognitoBrowserState())
       ->SetFallbackDelayForTesting(
           base::Milliseconds(fallbackDelayInMilliseconds));
 }
