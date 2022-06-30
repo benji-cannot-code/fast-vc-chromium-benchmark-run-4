ba700a35635d40e2c6ceef9e2e766be4af101d82
Yoav Weiss <yoavweiss@chromium.org>
2022-06-30 21:31:33 +0000

    SoftNav heuristics - take <main>'s descendants into account
    It seems like the initial heuristic for direct changes to <main> and
    its children was overly limited, and doesn't cover common cases.
    This CL expands the heuristic to cover any changes for a descendent of
    <main> to qualify as DOM modifications that count towards soft
    navigation heuristics.
    
    Bug: 1291125
    Change-Id: Iea8a0cfd5e96dfa64a48dc675d75cf500fd6554a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706832
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019834}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/spatial_navigation.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
+#include "third_party/blink/renderer/core/timing/soft_navigation_heuristics.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_script.h"
 #include "third_party/blink/renderer/core/xml_names.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -177,6 +178,42 @@ HTMLElement* GetParentForDirectionality(const HTMLElement& element,
   // recalc, and delay the check for later for a performance reason.
   SlotAssignmentRecalcForbiddenScope forbid_slot_recalc(element.GetDocument());
   return DynamicTo<HTMLElement>(FlatTreeTraversal::ParentElement(element));
+}
+
+bool IsDescendentOfMainElement(const Node* node) {
+  DCHECK(node);
+  do {
+    if (IsA<HTMLMainElement>(node)) {
+      return true;
+    }
+    node = node->parentNode();
+  } while (node);
+  return false;
+}
+
+void CheckSoftNavigationHeuristicsTracking(const Document& document,
+                                           const Node* insertion_point) {
+  DCHECK(insertion_point);
+  if (document.IsTrackingSoftNavigationHeuristics() &&
+      IsDescendentOfMainElement(insertion_point)) {
+    LocalDOMWindow* window = document.domWindow();
+    if (!window) {
+      return;
+    }
+    LocalFrame* frame = window->GetFrame();
+    if (!frame || !frame->IsMainFrame()) {
+      return;
+    }
+    ScriptState* script_state = ToScriptStateForMainWorld(frame);
+    if (!script_state) {
+      return;
+    }
+
+    SoftNavigationHeuristics* heuristics =
+        SoftNavigationHeuristics::From(*window);
+    DCHECK(heuristics);
+    heuristics->ModifiedMain(script_state);
+  }
 }
 
 }  // anonymous namespace
@@ -1295,6 +1332,10 @@ void HTMLElement::ChildrenChanged(const ChildrenChange& change) {
           !ElementAffectsDirectionality(element))
         element->UpdateDirectionalityAndDescendant(CachedDirectionality());
     }
+  }
+  if (change.IsChildInsertion()) {
+    CheckSoftNavigationHeuristicsTracking(GetDocument(),
+                                          change.sibling_changed);
   }
 }
 
--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
@@ -60,10 +60,22 @@ SoftNavigationHeuristics* SoftNavigationHeuristics::From(
   return heuristics;
 }
 
+void SoftNavigationHeuristics::SetIsTrackingSoftNavigationHeuristicsOnDocument(
+    bool value) const {
+  LocalDOMWindow* window = GetSupplementable();
+  if (!window) {
+    return;
+  }
+  if (Document* document = window->document()) {
+    document->SetIsTrackingSoftNavigationHeuristics(value);
+  }
+}
+
 void SoftNavigationHeuristics::ResetHeuristic() {
   // Reset previously seen indicators and task IDs.
   flag_set_.Clear();
   potential_soft_navigation_task_ids_.clear();
+  SetIsTrackingSoftNavigationHeuristicsOnDocument(false);
 }
 
 void SoftNavigationHeuristics::UserInitiatedClick(ScriptState* script_state) {
@@ -74,6 +86,7 @@ void SoftNavigationHeuristics::UserInitiatedClick(ScriptState* script_state) {
   DCHECK(scheduler->GetTaskAttributionTracker());
   ResetHeuristic();
   scheduler->GetTaskAttributionTracker()->RegisterObserver(this);
+  SetIsTrackingSoftNavigationHeuristicsOnDocument(true);
 }
 
 bool SoftNavigationHeuristics::IsCurrentTaskDescendantOfClickEventHandler(
@@ -127,6 +140,7 @@ void SoftNavigationHeuristics::ModifiedMain(ScriptState* script_state) {
     LogToConsole(script_state, mojom::blink::ConsoleMessageLevel::kVerbose,
                  String("Modified main element."));
   }
+  SetIsTrackingSoftNavigationHeuristicsOnDocument(false);
 }
 
 void SoftNavigationHeuristics::CheckSoftNavigation(ScriptState* script_state) {
