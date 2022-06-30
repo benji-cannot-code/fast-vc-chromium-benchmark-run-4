affdaab9562f23e68730f8350059610126719b9e
Sergey Poromov <poromov@chromium.org>
2022-06-30 13:55:18 +0000

    Revert "emoji-picker: refactor search & improve its perf"
    This reverts commit 8c44c7df22cdf782111f315996ad7e5dee47dea0.
    
    Reason for revert: Preventively reverting as failed EmojiPickerMainTest.All is causing many issues (e.g.blocking src-internal uprev):
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/57706/test-results
    
    Original change's description:
    > emoji-picker: refactor search & improve its perf
    >
    > Emoji search functionalities and code have been improved:
    >
    > * It is generalized and decoupled from top-level logics, so there is no
    >   specific handling of different data categories anymore (e.g. emoji, emoticon)
    >   . As a result, all the duplicated codes for the UI and logic of different
    >   categories are removed.
    > * Lazy indexing feature is added and indexing process is changed
    >   allowing increase in efficiency by running the indexing at the right time.
    >
    > Based on emoji search changes, a few improvements are applied to
    > emoji-picker:
    >
    > * Load time is reduced: Previously, the same data could be indexed
    >   multiple times during the data loading stage which was inefficient
    >   increasing load time. Now, indexing is done only once when all the data is
    >   loaded or user interacts with the search.
    > * Data loading related processes are centralized allowing future code
    >   improvements and extensions.
    > * An attribute of Category is added to EmojiGroup type.
    >
    > Bug: b:235418846
    > Change-Id: I271623fd1a6ffb04741ecf3fbe64e35bff71e5cd
    > Fixed: b:235419092,b:235419258
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697697
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Reviewed-by: John Palmer <jopalmer@chromium.org>
    > Commit-Queue: Mehrab N <mehrab@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1019365}
    
    Bug: b:235418846
    Change-Id: Ic49c507350113113f3c5803b189b35894c0c5b64
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734882
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019609}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
@@ -60,13 +60,10 @@ export class EmojiPicker extends PolymerElement {
       /** @private {?EmojiGroupData} */
       emojiData: {
         type: Array,
-        value: () => ([]),
         observer: 'onEmojiDataChanged',
       },
-      /** @private {EmojiGroupData} */
-      categoriesData: {type: Array, value: () => ([])},
       /** @type {?EmojiGroupData} */
-      emoticonData: {type: Array, value: () => ([])},
+      emoticonData: {type: Array, value: []},
       /** @private {Object<string,string>} */
       preferenceMapping: {type: Object},
       /** @private {!EmojiGroup} */
@@ -77,8 +74,6 @@ export class EmojiPicker extends PolymerElement {
       pagination: {type: Number, value: 1, observer: 'onPaginationChanged'},
       /** @private {string} */
       search: {type: String, value: '', observer: 'onSearchChanged'},
-      /** @private {boolean} */
-      searchLazyIndexing: {type: Boolean, value: true},
       /** @private {boolean} */
       textSubcategoryBarEnabled: {
         type: Boolean,
@@ -107,11 +102,8 @@ export class EmojiPicker extends PolymerElement {
     this.emojiData = [];
 
     // TODO(b/216475720): rename the data structure below for a generic naming.
-    this.emojiHistory = {
-      'group': 'Recently used', 'emoji': [], 'category': CategoryEnum.EMOJI};
-    this.emoticonHistory = {
-      'group': 'Recently used', 'emoji': [],
-      'category': CategoryEnum.EMOTICON};
+    this.emojiHistory = {'group': 'Recently used', 'emoji': []};
+    this.emoticonHistory = {'group': 'Recently used', 'emoji': []};
 
     this.preferenceMapping = {};
 
@@ -208,7 +200,7 @@ export class EmojiPicker extends PolymerElement {
             ev => this.onCategoryButtonClick(ev.detail.categoryName));
         this.addEventListener(events.EMOJI_REMAINING_DATA_LOADED, () => {
           this.fetchOrderingData(this.emoticonDataUrl).then((data) => {
-            this.updateCategoryData(data, CategoryEnum.EMOTICON, true);
+            this.emoticonData = data;
             this.dispatchEvent(events.createCustomEvent(
               events.V2_CONTENT_LOADED));
           });
@@ -257,44 +249,6 @@ export class EmojiPicker extends PolymerElement {
       xhr.open('GET', url);
       xhr.send();
     });
-  }
-
-  /**
-   * Processes a new category data and updates any needed variables and UIs
-   * accordingly.
-   *
-   * @param {!EmojiGroupData} data The category data to be processes.
-   *    Note: category field will be added to the each EmojiGroup in data.
-   * @param {!CategoryEnum} category Category of the data.
-   * @param {boolean} lastPartition True if no future data updates are
-   *      expected.
-   */
-  updateCategoryData(data, category, lastPartition=false) {
-    // TODO(b/233270589): Add category to the underlying data.
-    // Add category field to the data.
-    data.forEach((emojiGroup) => {
-      emojiGroup.category = category;
-    });
-
-    // TODO(b/235418846): Remove the following.
-    // Update the data variable of the category.
-    switch (category) {
-      case CategoryEnum.EMOJI:
-        this.push('emojiData', ...data);
-        break;
-      case CategoryEnum.EMOTICON:
-        this.push('emoticonData', ...data);
-        break;
-      default:
-        throw new Error(`Unknown category ${category}.`);
-    }
-
-    this.push('categoriesData', ...data);
-
-    // If all data is fetched, trigger search index.
-    if (lastPartition) {
-      this.searchLazyIndexing = false;
-    }
   }
 
   onSearchChanged(newValue) {
@@ -815,7 +769,7 @@ export class EmojiPicker extends PolymerElement {
     // There is quite a lot of emoji data to load which causes slow rendering.
     // Just load the first emoji category immediately, and defer loading of the
     // other categories (which will be off screen).
-    this.updateCategoryData([data[0]], CategoryEnum.EMOJI);
+    this.emojiData = [data[0]];
     afterNextRender(
         this,
         () => this.fetchOrderingData(`${this.emojiDataUrl}_remaining.json`)
@@ -823,7 +777,7 @@ export class EmojiPicker extends PolymerElement {
   }
 
   onEmojiDataLoadedRemaining(data) {
-    this.updateCategoryData(data, CategoryEnum.EMOJI, !this.v2Enabled);
+    this.push('emojiData', ...data);
     this.dispatchEvent(events.createCustomEvent(
       events.EMOJI_REMAINING_DATA_LOADED));
     afterNextRender(this, () => {
