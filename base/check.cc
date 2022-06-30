75a5f947aa98306e35d34af87af3ee2e399c6d46
Peter Boström <pbos@chromium.org>
2022-06-30 18:03:08 +0000

    Report DCHECK_MESSAGE crash key for DCHECK message
    This adds a crash key for DCHECK messages as to help diagnose DCHECK
    errors without having the knowledge to probe the minidump stack for the
    log message.
    
    Bug: 1340649
    Change-Id: I59f3d2e0671b3b19bd92bff0c91fedacef2cb58b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737073
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019732}

--- base/check.cc
+++ base/check.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/debug/alias.h"
+#include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/thread_annotations.h"
@@ -34,9 +35,15 @@ void DCheckDumpOnceWithoutCrashing(LogMessage* log_message) {
   // reporting and at most report once per thread.
   static std::atomic<bool> has_dumped = false;
   if (!has_dumped.load(std::memory_order_relaxed)) {
+    const std::string str = log_message->str();
     // Copy the LogMessage string to stack memory to make sure it can be
     // recovered in crash dumps.
-    DEBUG_ALIAS_FOR_CSTR(log_message_str, log_message->str().c_str(), 1024);
+    // TODO(pbos): Surface DCHECK_MESSAGE well in crash reporting to make this
+    // redundant, then remove it.
+    DEBUG_ALIAS_FOR_CSTR(log_message_str, str.c_str(), 1024);
+
+    // Report the log message as DCHECK_MESSAGE in the dump we're about to do.
+    SCOPED_CRASH_KEY_STRING256("Logging", "DCHECK_MESSAGE", str);
 
     // Note that dumping may fail if the crash handler hasn't been set yet. In
     // that case we want to try again on the next failing DCHECK.
