8082d9e3f8dca3ee774baa83511944d4c7225c32
Longbo Wei <longbowei@google.com>
2022-06-30 19:52:31 +0000

    Feedback: Add preview when selected file is image type
    * When the uploaded file is an image type, show a preview of that image.
    * Style will be updated in later CL.
    * screenshot: http://screen/AZKndzQQxnthVFW
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    
    Change-Id: I351225f4fca5af04e4f5a16ab7367cc1c0b737d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732533
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019781}

--- chrome/test/data/webui/chromeos/os_feedback_ui/file_attachment_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/file_attachment_test.js
@@ -131,7 +131,10 @@ export function fileAttachmentTestSuite() {
 
     // Set selected file manually.
     /** @type {!File} */
-    const fakeFile = /** @type {!File} */ ({name: 'fake.zip'});
+    const fakeFile = /** @type {!File} */ ({
+      name: 'fake.zip',
+      type: 'application/zip',
+    });
     page.setSelectedFileForTesting(fakeFile);
 
     // The selected file name is set properly.
@@ -168,6 +171,7 @@ export function fileAttachmentTestSuite() {
     /** @type {!File} */
     const fakeFile = /** @type {!File} */ ({
       name: 'fake.zip',
+      type: 'application/zip',
       size: MAX_ATTACH_FILE_SIZE,
     });
     page.setSelectedFileForTesting(fakeFile);
@@ -190,6 +194,7 @@ export function fileAttachmentTestSuite() {
     /** @type {!File} */
     const fakeFile = /** @type {!File} */ ({
       name: 'fake.zip',
+      type: 'application/zip',
       size: MAX_ATTACH_FILE_SIZE,
       arrayBuffer: async () => {
         return new Uint8Array(fakeData).buffer;
@@ -221,6 +226,7 @@ export function fileAttachmentTestSuite() {
     /** @type {!File} */
     const fakeFile = /** @type {!File} */ ({
       name: 'fake.zip',
+      type: 'application/zip',
       size: MAX_ATTACH_FILE_SIZE + 1,
       arrayBuffer: async () => {
         return new Uint8Array(fakeData).buffer;
@@ -240,5 +246,30 @@ export function fileAttachmentTestSuite() {
     const attachedFile = await page.getAttachedFile();
     // AttachedFile should be null.
     assertTrue(!attachedFile);
+  });
+
+  // Test that files that are image type have a preview.
+  test('imageFilePreview', async () => {
+    await initializePage();
+    const selectFileCheckbox = getElement('#selectFileCheckbox');
+    const fakeData = [12, 11, 99];
+
+    /** @type {!File} */
+    const fakeImageFile = /** @type {!File} */ ({
+      name: 'fake.png',
+      type: 'image/png',
+      size: MAX_ATTACH_FILE_SIZE,
+      arrayBuffer: async () => {
+        return new Uint8Array(fakeData).buffer;
+      },
+    });
+    // getImageUrl_ should return an url when file is image type.
+    const imageUrl = await page.getImageUrl_(fakeImageFile);
+    assertTrue(imageUrl.length > 0);
+    // There should be a preview image.
+    page.selectedImageUrl_ = imageUrl;
+    const selectedImage = getElement('#selectedFileImage');
+    assertTrue(!!selectedImage.src);
+    assertEquals(imageUrl, selectedImage.src);
   });
 }
