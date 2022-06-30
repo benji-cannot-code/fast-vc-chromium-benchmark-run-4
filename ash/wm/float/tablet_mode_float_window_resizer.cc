afe2441e733a7f34184338d6e5a0b9f7116a0948
Sammie Quon <sammiequon@chromium.org>
2022-06-30 18:27:41 +0000

    float: Dragging in tablet mode part 1.
    This CL enables dragging a floated window in tablet mode. On release,
    the window will magnetize to the closest corner.
    
    Part two will implement "tucked windows", which is a state where a
    fling on swipe can move a floated window off screen.
    
    Bug: 1338715
    Change-Id: Id71c234171edd6b4bb14443640270766ccbb1816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724235
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019751}

--- ash/wm/float/tablet_mode_float_window_resizer.cc
+++ ash/wm/float/tablet_mode_float_window_resizer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/float/tablet_mode_float_window_resizer.h"
 
+#include "ash/shell.h"
+#include "ash/wm/float/float_controller.h"
 #include "ash/wm/window_state.h"
 #include "chromeos/ui/wm/features.h"
 #include "ui/aura/window.h"
@@ -23,20 +25,26 @@ TabletModeFloatWindowResizer::~TabletModeFloatWindowResizer() {
 
 void TabletModeFloatWindowResizer::Drag(const gfx::PointF& location_in_parent,
                                         int event_flags) {
+  last_location_in_parent_ = location_in_parent;
+
   gfx::Rect bounds = CalculateBoundsForDrag(location_in_parent);
   if (bounds != GetTarget()->bounds())
     SetBoundsDuringResize(bounds);
 }
 
 void TabletModeFloatWindowResizer::CompleteDrag() {
-  // Keep the bounds where it was last dragged to.
-  // TODO(crbug.com/1338715): Magnetize to the closest corner on release.
+  aura::Window* float_window = GetTarget();
+  auto* float_controller = Shell::Get()->float_controller();
+  DCHECK(float_controller->IsFloated(float_window));
+  float_controller->OnDragCompleted(last_location_in_parent_);
 }
 
 void TabletModeFloatWindowResizer::RevertDrag() {
   GetTarget()->SetBounds(details().initial_bounds_in_parent);
 }
 
-void TabletModeFloatWindowResizer::FlingOrSwipe(ui::GestureEvent* event) {}
+void TabletModeFloatWindowResizer::FlingOrSwipe(ui::GestureEvent* event) {
+  // TODO(crbug.com/1338715): Tuck the window to the side on fling or swipe.
+}
 
 }  // namespace ash
