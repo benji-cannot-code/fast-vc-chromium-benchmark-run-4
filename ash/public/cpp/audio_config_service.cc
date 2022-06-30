d07fa8be0ef7a55b06df5fba3de14e67d7b1974c
Owen Zhang <owenzhang@google.com>
2022-06-30 22:13:51 +0000

    [CrOS Audio]: Initialize CrosAudioConfig.
    This CL creates an in-process instance of CrosAudioConfig, which is lazily initialized by the first client that binds to it. This CL also creates a GetAudioConfigService() function in //ash/public/cpp, which is intended to the main entrypoint to the API by both WebUI and Ash.
    
    Bug: 1092970
    Change-Id: Ib2e3febfd99d3c7ef6a67a3d0070ed5af87e8555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3723689
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019846}

---
+++ ash/public/cpp/audio_config_service.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/audio_config_service.h"
+
+#include "ash/components/audio/in_process_instance.h"
+
+namespace ash {
+
+void GetAudioConfigService(
+    mojo::PendingReceiver<ash::audio_config::mojom::CrosAudioConfig> receiver) {
+  ash::audio_config::BindToInProcessInstance(std::move(receiver));
+}
+
+}  // namespace ash
