1da47573a4e411047c101daed85cec2f605e7516
Brian Sheedy <bsheedy@chromium.org>
2022-06-30 02:22:00 +0000

    Work around WebGPU parallel node issue
    Changes the directory that is used for generating the WebGPU test list
    from a fixed directory to a random temporary directory. This works
    around the flaky issue we're seeing where parallel tests can step on
    each other due to the shared directory.
    
    Bug: 1336393
    Change-Id: I61ee32a7132e4e66575627748fd86a3d043d9296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736685
    Reviewed-by: Austin Eng <enga@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019436}

--- content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
+++ content/test/gpu/gpu_tests/webgpu_cts_integration_test.py
@@ -10,6 +10,7 @@ import logging
 import os
 import subprocess
 import sys
+import tempfile
 import threading
 import typing
 import unittest
@@ -26,7 +27,6 @@ EXPECTATIONS_FILE = os.path.join(gpu_path_util.CHROMIUM_SRC_DIR, 'third_party',
                                  'dawn', 'webgpu-cts', 'expectations.txt')
 LIST_SCRIPT = os.path.join(gpu_path_util.CHROMIUM_SRC_DIR, 'third_party',
                            'dawn', 'webgpu-cts', 'scripts', 'list.py')
-TYPESCRIPT_DIR = os.path.join(gpu_path_util.GPU_DIR, '.webgpu_typescript')
 
 MULTI_PAYLOAD_TIMEOUT = 0.5
 TEST_RUNS_BETWEEN_CLEANUP = 1000
@@ -110,6 +110,7 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   _build_dir = None
 
+  _typescript_tempdir = tempfile.TemporaryDirectory()
   _test_list = None
 
   total_tests_run = 0
@@ -183,6 +184,7 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
   @classmethod
   def SetUpProcess(cls) -> None:
     super(WebGpuCtsIntegrationTest, cls).SetUpProcess()
+
     cls.SetUpWebsocketServer()
     browser_args = [
         '--enable-unsafe-webgpu',
@@ -245,10 +247,12 @@ class WebGpuCtsIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     cls._enable_dawn_backend_validation = options.enable_dawn_backend_validation
     cls._use_webgpu_adapter = options.use_webgpu_adapter
     if cls._test_list is None:
-      p = subprocess.run(
-          [sys.executable, LIST_SCRIPT, '--js-out-dir', TYPESCRIPT_DIR],
-          stdout=subprocess.PIPE,
-          check=True)
+      p = subprocess.run([
+          sys.executable, LIST_SCRIPT, '--js-out-dir',
+          cls._typescript_tempdir.name
+      ],
+                         stdout=subprocess.PIPE,
+                         check=True)
       cls._test_list = p.stdout.decode('utf-8').splitlines()
     for line in cls._test_list:  # pylint:disable=not-an-iterable
       if not line:
