e29f3c8301b83bb4560cf2c4ad289f8cb810a3e8
Rohit Bhatia <bhatiarohit@google.com>
2022-06-30 22:41:03 +0000

    [DownloadBubble] Remove WithoutTaskRunner Method for GetAllDownloads
    Now that DownloadUIModel does not delete on TaskRunner, we can use
    GetAllDownloads directly.
    
    Bug: 1315554
    Change-Id: I07430beeb17fe91f5ec51469d99b269ff8e43340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737022
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019861}

--- chrome/browser/download/bubble/download_display_controller.cc
+++ chrome/browser/download/bubble/download_display_controller.cc
@@ -80,7 +80,7 @@ void DownloadDisplayController::OnUpdatedItem(bool is_done,
 
 void DownloadDisplayController::OnRemovedItem(const ContentId& id) {
   std::vector<std::unique_ptr<DownloadUIModel>> all_models =
-      bubble_controller_->GetAllItemsToDisplayWithoutTaskRunnerDeletion();
+      bubble_controller_->GetAllItemsToDisplay();
   // Hide the button if there is only one download item left and that item is
   // about to be removed.
   if (all_models.size() == 1 && all_models[0]->GetContentId() == id) {
@@ -130,7 +130,7 @@ void DownloadDisplayController::UpdateToolbarButtonState() {
   bool has_deep_scanning_download = false;
 
   std::vector<std::unique_ptr<DownloadUIModel>> all_models =
-      bubble_controller_->GetAllItemsToDisplayWithoutTaskRunnerDeletion();
+      bubble_controller_->GetAllItemsToDisplay();
   if (all_models.empty()) {
     HideToolbarButton();
     return;
@@ -207,8 +207,7 @@ void DownloadDisplayController::MaybeShowButtonWhenCreated() {
                                  last_complete_time)) {
     return;
   }
-  if (bubble_controller_->GetAllItemsToDisplayWithoutTaskRunnerDeletion()
-          .empty()) {
+  if (bubble_controller_->GetAllItemsToDisplay().empty()) {
     return;
   }
   // If the last download complete time is less than
@@ -247,7 +246,7 @@ DownloadDisplayController::GetProgress() {
   int64_t total_bytes = 0;
 
   std::vector<std::unique_ptr<DownloadUIModel>> all_models =
-      bubble_controller_->GetAllItemsToDisplayWithoutTaskRunnerDeletion();
+      bubble_controller_->GetAllItemsToDisplay();
   for (const auto& model : all_models) {
     if (IsModelInProgress(model.get())) {
       ++progress_info.download_count;
