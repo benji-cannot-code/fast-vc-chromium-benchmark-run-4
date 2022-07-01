d618063a896217c12677dbad344b85a5edbf9a39
Ryo Hashimoto <hashimoto@chromium.org>
2022-07-01 04:30:58 +0000

    arc: Delete DiskQuotaHost::SetProjectID()
    This is no longer needed after ag/18669207
    
    BUG=b:175764830
    TEST=tast run arc.QuotaProjectID
    
    Change-Id: I1582285b4c861c25f3048243789e4207575e7b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686852
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019952}

--- ash/components/arc/disk_quota/arc_disk_quota_bridge.h
+++ ash/components/arc/disk_quota/arc_disk_quota_bridge.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_COMPONENTS_ARC_DISK_QUOTA_ARC_DISK_QUOTA_BRIDGE_H_
 
 #include "ash/components/arc/mojom/disk_quota.mojom.h"
-#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/dbus/cryptohome/UserDataAuth.pb.h"
 #include "components/account_id/account_id.h"
@@ -31,15 +30,6 @@ class ArcDiskQuotaBridge : public KeyedService, public mojom::DiskQuotaHost {
   static ArcDiskQuotaBridge* GetForBrowserContext(
       content::BrowserContext* context);
 
-  // Converts an Android path to a pair of (parent_path, child_path) to be
-  // passed to SetProjectId() on cryptohome.
-  // Returns false if SetProjectId() is not allowed for the path.
-  // (go/arc-project-quota)
-  static bool convertPathForSetProjectId(
-      const base::FilePath& android_path,
-      user_data_auth::SetProjectIdAllowedPathType* parent_path_out,
-      base::FilePath* child_path_out);
-
   ArcDiskQuotaBridge(content::BrowserContext* context,
                      ArcBridgeService* bridge_service);
 
@@ -62,10 +52,6 @@ class ArcDiskQuotaBridge : public KeyedService, public mojom::DiskQuotaHost {
   void GetCurrentSpaceForProjectId(
       uint32_t project_id,
       GetCurrentSpaceForProjectIdCallback callback) override;
-
-  void SetProjectId(uint32_t project_id,
-                    const std::string& android_path,
-                    SetProjectIdCallback callback) override;
 
   void GetFreeDiskSpace(GetFreeDiskSpaceCallback) override;
 
