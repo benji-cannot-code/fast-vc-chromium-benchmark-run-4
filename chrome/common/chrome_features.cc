8ed5abb874072ec726742a0c5feb3d918f73a7da
Dibyajyoti Pal <dibyapal@chromium.org>
2022-07-01 18:28:32 +0000

    dPWA: Enable kUseWebAppDBInsteadOfExternalPrefs
    This CL enables the flag to read external data from the web_app DB in
    WebAppRegistrar operations instead of from the external prefs.
    
    Test failures are handled in crrev.com/c/3721892.
    
    Bug: 1029410
    Change-Id: Ib727da7e74991b1a517af0000fe41552b8aa340d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726413
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020179}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1193,6 +1193,6 @@ const base::Feature kSupportsRtcWakeOver24Hours{
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 const base::Feature kUseWebAppDBInsteadOfExternalPrefs{
-    "UseWebAppDBInsteadOfExternalPrefs", base::FEATURE_DISABLED_BY_DEFAULT};
+    "UseWebAppDBInsteadOfExternalPrefs", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
