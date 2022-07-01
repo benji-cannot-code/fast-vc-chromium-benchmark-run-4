160c08db94e9b7fa1622eb8e4e559ab161b15d0e
Abhijith Nair <abhijithnair@chromium.org>
2022-07-01 19:55:56 +0000

    Disallow multiple JsSandboxes to coexist
    Since we only have a single service definition, the JsSandbox can only
    use that as the backend for API<=29. This change ensures that we error
    out if multiple sandboxes try to bind to the same service.
    
    Bug: 1297672
    Change-Id: Id8f78c2cd7d2f85d2935d18e4494ecee00fceef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733107
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020207}

--- android_webview/js_sandbox/java/src/org/chromium/android_webview/js_sandbox/client/JsSandbox.java
+++ android_webview/js_sandbox/java/src/org/chromium/android_webview/js_sandbox/client/JsSandbox.java
@@ -33,6 +33,7 @@ import java.lang.annotation.Target;
 import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.concurrent.GuardedBy;
 
@@ -45,6 +46,7 @@ public class JsSandbox implements AutoCloseable {
     private static final String TAG = "JsSandbox";
     private static final String JS_SANDBOX_SERVICE_NAME =
             "org.chromium.android_webview.js_sandbox.service.JsSandboxService0";
+    private static AtomicBoolean sIsReadyToConnect = new AtomicBoolean(true);
     private final Object mLock = new Object();
     private CloseGuardHelper mGuard = CloseGuardHelper.create();
 
@@ -143,6 +145,7 @@ public class JsSandbox implements AutoCloseable {
                 mJsSandbox.doClose(new SandboxDeadException());
             } else {
                 mContext.unbindService(this);
+                sIsReadyToConnect.set(true);
             }
             if (mCompleter != null) {
                 mCompleter.setException(e);
@@ -189,21 +192,26 @@ public class JsSandbox implements AutoCloseable {
         intent.setComponent(compName);
         return CallbackToFutureAdapter.getFuture(completer -> {
             ConnectionSetup connectionSetup = new ConnectionSetup(context, completer);
-            try {
-                boolean isBinding = context.bindService(intent, connectionSetup, flag);
-                if (isBinding) {
-                    Executor mainExecutor;
-                    mainExecutor = ContextCompat.getMainExecutor(context);
-                    completer.addCancellationListener(
-                            () -> context.unbindService(connectionSetup), mainExecutor);
-                } else {
+            if (sIsReadyToConnect.compareAndSet(true, false)) {
+                try {
+                    boolean isBinding = context.bindService(intent, connectionSetup, flag);
+                    if (isBinding) {
+                        Executor mainExecutor;
+                        mainExecutor = ContextCompat.getMainExecutor(context);
+                        completer.addCancellationListener(
+                                () -> { context.unbindService(connectionSetup); }, mainExecutor);
+                    } else {
+                        context.unbindService(connectionSetup);
+                        completer.setException(
+                                new RuntimeException("bindService() returned false " + intent));
+                    }
+                } catch (SecurityException e) {
                     context.unbindService(connectionSetup);
-                    completer.setException(
-                            new RuntimeException("bindService() returned false " + intent));
+                    completer.setException(e);
                 }
-            } catch (SecurityException e) {
-                context.unbindService(connectionSetup);
-                completer.setException(e);
+            } else {
+                completer.setException(
+                        new IllegalStateException("Binding to already bound service"));
             }
 
             // Debug string.
@@ -240,7 +248,6 @@ public class JsSandbox implements AutoCloseable {
         }
     }
 
-    // Called from only the constructor and as a result mJsSandboxService will not be null.
     @GuardedBy("mLock")
     private void populateClientFeatureSet() {
         mClientSideFeatureSet = new HashSet<String>();
@@ -297,6 +304,10 @@ public class JsSandbox implements AutoCloseable {
             }
             cancelPendingEvaluationsLocked(cancelPendingWith);
             mConnection.mContext.unbindService(mConnection);
+            // Currently we consider that we are ready for a new connection once we unbind. This
+            // might not be true if the process is not immediately killed by ActivityManager once it
+            // is unbound.
+            sIsReadyToConnect.set(true);
             mJsSandboxService = null;
         }
     }
