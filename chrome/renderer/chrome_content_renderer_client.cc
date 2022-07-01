d73b568a7ca9b233a87b2407f738bade8e0520e5
Jack Hsieh <chengweih@chromium.org>
2022-07-01 16:33:16 +0000

    webhid: Make enable-web-hid-on-extension-service-worker effective
    This is part of exposing WebHID to extension service workers changes.
    This change makes the chrome feature flag
    enable-web-hid-on-extension-service-worker effective by checking this
    flag and enabling the blink feature WebHIDOnServiceWorkers accordingly
    in the extension environment.
    
    Bug: 1303195
    Change-Id: If5f4d3959ebdd1493995b6f9f743b8beb61913e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682179
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020117}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1597,6 +1597,16 @@ void ChromeContentRendererClient::
            features::kSupportSearchSuggestionForPrerender2))) {
     blink::WebRuntimeFeatures::EnablePrerender2RelatedFeatures(true);
   }
+
+#if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_EXTENSIONS)
+  // WebHID on service workers is only available in extension for now with
+  // feature enabled.
+  if (IsStandaloneContentExtensionProcess() &&
+      base::FeatureList::IsEnabled(
+          features::kEnableWebHidOnExtensionServiceWorker)) {
+    blink::WebRuntimeFeatures::EnableWebHIDOnServiceWorkers(true);
+  }
+#endif
 }
 
 bool ChromeContentRendererClient::AllowScriptExtensionForServiceWorker(
