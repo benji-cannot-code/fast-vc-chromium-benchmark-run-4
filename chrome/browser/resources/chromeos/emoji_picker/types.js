f2f6d3d7980f92e35605cbdc77014388887d1f46
Mehrab N <mehrab@chromium.org>
2022-07-01 05:43:21 +0000

    Reland "emoji-picker: refactor search & improve its perf"
    This is a reland of commit 8c44c7df22cdf782111f315996ad7e5dee47dea0
    
    Changed: Nothing has changed in this CL.
    
    Original change's description:
    > emoji-picker: refactor search & improve its perf
    >
    > Emoji search functionalities and code have been improved:
    >
    > * It is generalized and decoupled from top-level logics, so there is no
    >   specific handling of different data categories anymore (e.g. emoji, emoticon)
    >   . As a result, all the duplicated codes for the UI and logic of different
    >   categories are removed.
    > * Lazy indexing feature is added and indexing process is changed
    >   allowing increase in efficiency by running the indexing at the right time.
    >
    > Based on emoji search changes, a few improvements are applied to
    > emoji-picker:
    >
    > * Load time is reduced: Previously, the same data could be indexed
    >   multiple times during the data loading stage which was inefficient
    >   increasing load time. Now, indexing is done only once when all the data is
    >   loaded or user interacts with the search.
    > * Data loading related processes are centralized allowing future code
    >   improvements and extensions.
    > * An attribute of Category is added to EmojiGroup type.
    >
    > Bug: b:235418846
    > Change-Id: I271623fd1a6ffb04741ecf3fbe64e35bff71e5cd
    > Fixed: b:235419092,b:235419258
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697697
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Reviewed-by: John Palmer <jopalmer@chromium.org>
    > Commit-Queue: Mehrab N <mehrab@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1019365}
    
    Bug: b:235418846
    Change-Id: I3c792ccf66860f4cb21e0c5065bf347709d2fb3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740079
    Auto-Submit: Mehrab N <mehrab@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019963}

--- chrome/browser/resources/chromeos/emoji_picker/types.js
+++ chrome/browser/resources/chromeos/emoji_picker/types.js
@@ -19,7 +19,8 @@ export let Emoji;
 export let EmojiVariants;
 
 /**
- * @typedef {{group: string, emoji: !Array<EmojiVariants>}} EmojiGroup
+ * @typedef {{category: CategoryEnum, group: string,
+ *            emoji: !Array<EmojiVariants>}} EmojiGroup
  */
 export let EmojiGroup;
 
