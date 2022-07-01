34f1c1ca925d211c4ec9fa5607f67c8814fa1cbd
Gauthier Ambard <gambard@chromium.org>
2022-07-01 10:54:47 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/app_launcher
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    Bug: 1311955
    Change-Id: Ibd07e5599a35e799d39c2a0774026f878562618e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714612
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020036}

--- ios/chrome/browser/app_launcher/fake_app_launcher_abuse_detector.h
+++ ios/chrome/browser/app_launcher/fake_app_launcher_abuse_detector.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // An AppLauncherAbuseDetector for testing.
 @interface FakeAppLauncherAbuseDetector : AppLauncherAbuseDetector
 
-// The policy returned by |-launchPolicyforURL:fromSourcePageURL:|.  Default
+// The policy returned by `-launchPolicyforURL:fromSourcePageURL:`.  Default
 // value is ExternalAppLaunchPolicyAllow.
 @property(nonatomic, assign) ExternalAppLaunchPolicy policy;
 
