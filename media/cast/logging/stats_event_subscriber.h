8655ddc35097896c79f489919d6546d1edf305b0
Nan Lin <linnan@chromium.org>
2022-07-01 05:39:30 +0000

    [Code Health] Replace base::DictionaryValue in gpu/ and media/
    Bug: 1187027
    Change-Id: I08865acf67353bc215688ba3b9017024b7e9d375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735303
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019962}

--- media/cast/logging/stats_event_subscriber.h
+++ media/cast/logging/stats_event_subscriber.h
@@ -19,14 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "media/cast/logging/logging_defines.h"
 #include "media/cast/logging/raw_event_subscriber.h"
 #include "media/cast/logging/receiver_time_offset_estimator.h"
-
-namespace base {
-class DictionaryValue;
-class ListValue;
-}
 
 namespace media {
 namespace cast {
@@ -55,7 +51,7 @@ class StatsEventSubscriber final : public RawEventSubscriber {
   // The inner dictionary consists of string - double entries, where the string
   // describes the name of the stat, and the double describes
   // the value of the stat. See CastStat and StatsMap below.
-  std::unique_ptr<base::DictionaryValue> GetStats() const;
+  base::Value::Dict GetStats() const;
 
   // Resets stats in this object.
   void Reset();
@@ -110,7 +106,7 @@ class StatsEventSubscriber final : public RawEventSubscriber {
 
     void Reset();
 
-    std::unique_ptr<base::ListValue> GetHistogram() const;
+    base::Value::List GetHistogram() const;
 
    private:
     int64_t min_;
