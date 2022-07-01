63c367b548f0857208de97e92efe81e480163eb0
Justin Green <greenjustin@google.com>
2022-07-01 20:26:26 +0000

    media/gpu/vaapi/test: Extending the VAAPI video decoding tests to support H.264 files.
    Bug: 203598067
    Change-Id: Ifbec1c41242aadf60ead47529ed371fe13f23228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256161
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020221}

--- media/gpu/vaapi/test/video_decoder.h
+++ media/gpu/vaapi/test/video_decoder.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/test/shared_va_surface.h"
 
 namespace media {
-
-class IvfParser;
-
 namespace vaapi_test {
 
 class VaapiDevice;
@@ -26,8 +23,7 @@ class VideoDecoder {
     kEOStream,
   };
 
-  VideoDecoder(std::unique_ptr<IvfParser> ivf_parser,
-               const VaapiDevice& va_device,
+  VideoDecoder(const VaapiDevice& va_device,
                SharedVASurface::FetchPolicy fetch_policy);
   // Implementations of VideoDecoder are expected to handle the destruction of
   // |last_decoded_surface_| and in particular ensure it is done in the right
@@ -59,9 +55,6 @@ class VideoDecoder {
   bool LastDecodedFrameVisible() const { return last_decoded_frame_visible_; }
 
  protected:
-  // Parser for the IVF stream to decode.
-  const std::unique_ptr<IvfParser> ivf_parser_;
-
   // VA handles.
   const VaapiDevice& va_device_;
   scoped_refptr<SharedVASurface> last_decoded_surface_;
