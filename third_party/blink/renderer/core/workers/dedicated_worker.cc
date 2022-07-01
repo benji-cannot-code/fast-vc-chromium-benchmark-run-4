92e1712afef7e94616a3d3f3891d54029d79ad94
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2022-07-01 06:55:20 +0000

    Send error to console when the fetch failed for dedicated workers.
    For ease of understanding the failure of fetching scripts in
    dedicated workers, the failure will be shown in the console log.
    
    Developers will see the error log if the load was prohibited due to
    COEP for example.
    
    Change-Id: I6fce6f2b0f49e96f5e9ff41f06f2d8ad8383d701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721722
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019973}

--- third_party/blink/renderer/core/workers/dedicated_worker.cc
+++ third_party/blink/renderer/core/workers/dedicated_worker.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/web_frame_widget_impl.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/inspector/main_thread_debugger.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_loader.h"
@@ -325,7 +326,10 @@ void DedicatedWorker::OnScriptLoadStartFailed() {
 
 void DedicatedWorker::DispatchErrorEventForScriptFetchFailure() {
   DCHECK(!GetExecutionContext() || GetExecutionContext()->IsContextThread());
-  // TODO(nhiroki): Add a console error message.
+  GetExecutionContext()->AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
+      mojom::blink::ConsoleMessageSource::kWorker,
+      mojom::blink::ConsoleMessageLevel::kError,
+      "new Worker() has failed to fetch script."));
   DispatchEvent(*Event::CreateCancelable(event_type_names::kError));
 }
 
