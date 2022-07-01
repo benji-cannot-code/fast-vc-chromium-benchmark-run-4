5ba6b179869c136aa452ae1d95e612c34391ec6a
Gavin Williams <gavinwill@chromium.org>
2022-07-01 21:34:37 +0000

    Shimless: Set the active profile for accessibility features
    The Shimless RMA app launches before (in place of) OOBE or the login
    screen so certain start up steps are skipped. One of the skipped steps
    is AccessibilityManager::OnLoginOrLockScreenVisible() being invoked
    which sets the active profile and enables the accessibility features for
    OOBE and the login screen.
    
    This change adds a dedicated function for when Shimless RMA is launched,
    the profile is set in AccessibilityManager which enables accessibility
    features.
    
    AX-Relnotes: Enables accessibility features in Shimless RMA
    Bug: 1198187
    Tests: ash_webui_unittests
    Change-Id: I8cd5b69239bfceed44dd78408faea55add8edbca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739411
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020249}

--- chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.cc
+++ chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.h"
 
 #include "ash/constants/ash_switches.h"
+#include "base/check.h"
 #include "base/command_line.h"
+#include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/login/chrome_restart_request.h"
 #include "chrome/browser/ash/system/device_disabling_manager.h"
 #include "chrome/browser/ui/webui/chromeos/diagnostics_dialog.h"
@@ -34,6 +36,12 @@ void ChromeShimlessRmaDelegate::ShowDiagnosticsDialog() {
   }
 
   chromeos::DiagnosticsDialog::ShowDialog();
+}
+
+void ChromeShimlessRmaDelegate::RefreshAccessibilityManagerProfile() {
+  AccessibilityManager* accessibility_manager = AccessibilityManager::Get();
+  DCHECK(accessibility_manager);
+  accessibility_manager->OnShimlessRmaLaunched();
 }
 
 }  // namespace shimless_rma
