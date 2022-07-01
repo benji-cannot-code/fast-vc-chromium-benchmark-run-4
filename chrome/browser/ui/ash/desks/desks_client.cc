847b5d0608c26773017726538b95075eb3e9446b
zhumatthew <zhumatthew@google.com>
2022-07-01 00:51:50 +0000

    Desk Template: Check for sync flag for testing
    This CL checks the sync flag to make sure we get the proper desk
    model. Used mainly for testing because we don't want to sync desk
    templates to servers.
    
    BUG=b/237598089
    TEST=Manually tested and verifed
    
    Change-Id: Ieb19af618fdac5ce852a54525292d8f0ad16470e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736757
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019903}

--- chrome/browser/ui/ash/desks/desks_client.cc
+++ chrome/browser/ui/ash/desks/desks_client.cc
@@ -202,7 +202,8 @@ void DesksClient::OnActiveUserSessionChanged(const AccountId& account_id) {
         std::make_unique<desks_storage::LocalDeskDataManager>(
             active_profile_->GetPath(), account_id);
 
-    if (ash::saved_desk_util::AreDesksTemplatesEnabled()) {
+    if (ash::saved_desk_util::AreDesksTemplatesEnabled() &&
+        chromeos::features::IsDeskTemplateSyncEnabled()) {
       saved_desk_storage_manager_ =
           std::make_unique<desks_storage::DeskModelWrapper>(
               save_and_recall_desks_storage_manager_.get());
@@ -416,7 +417,8 @@ void DesksClient::LaunchAppsFromTemplate(
 
 desks_storage::DeskModel* DesksClient::GetDeskModel() {
   if (chromeos::features::IsSavedDesksEnabled()) {
-    if (!ash::saved_desk_util::AreDesksTemplatesEnabled()) {
+    if (!ash::saved_desk_util::AreDesksTemplatesEnabled() ||
+        !chromeos::features::IsDeskTemplateSyncEnabled()) {
       DCHECK(save_and_recall_desks_storage_manager_.get());
       return save_and_recall_desks_storage_manager_.get();
     }
