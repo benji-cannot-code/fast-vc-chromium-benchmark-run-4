df994eb82a94cf28c0f0dc9c115590d9d6a49f5d
Danil Somsikov <dsv@chromium.org>
2022-07-01 14:38:20 +0000

    Dump all proctocol messages to investigate the test timeouts.
    Bug: 1327867
    Change-Id: I21eeaed1e7c5aab0ef88e9e72a58787c88c62bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738605
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020078}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/initiator-minified.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/initiator-minified.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   const {page, session, dp} = await testRunner.startBlank(
     `Tests that the initiator position is correct even when that initiator is minified.`);
-
+  testRunner.startDumpingProtocolMessages();
   window.onerror = (msg) => testRunner.log('onerror: ' + msg);
   window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
   let errorForLog = new Error();
