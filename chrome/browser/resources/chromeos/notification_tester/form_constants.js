fb93374363a4188d568eca45c054344f6cb0c694
Arvind Ganesh <ganesharvind@google.com>
2022-07-01 00:06:30 +0000

    Added form elements to configure all notification options
    Screenshot of new UI: http://shortn/_MUJNKNB3nN
    
    Bug: 1337375
    Change-Id: I77bee4f527f75b584863da127f2e55bb16b7aa4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718890
    Commit-Queue: Arvind Ganesh <ganesharvind@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019889}

--- chrome/browser/resources/chromeos/notification_tester/form_constants.js
+++ chrome/browser/resources/chromeos/notification_tester/form_constants.js
@@ -70,6 +70,6 @@ export const FormSelectOptions = {
   ],
   IMAGE_OPTIONS: [
     {displayText: 'No Image', value: 'none'},
-    {displayText: 'ChromeOS Logo (1218x317, PNG)', value: 'chromeos_logo_main'},
+    {displayText: 'CrOS Logo (1218x317, PNG)', value: 'chromeos_logo_main'},
   ],
 };
