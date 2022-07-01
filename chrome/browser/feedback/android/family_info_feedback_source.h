29bef234fbb78edc697462ec10f06729f7e8c587
Nohemi Fernandez <fernandex@chromium.org>
2022-07-01 18:04:13 +0000

    Add FamilyInfoFeedbackSource unittest to check object destruction.
    Adds two tests to ensure that FamilyInfoFeedbackSource is destroyed
    on completion of GetFamilyMember* methods.
    
    Bug: 1338550
    Change-Id: Icaab4b741a1af0b49611f2ef63bb156c54fff497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740650
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020163}

--- chrome/browser/feedback/android/family_info_feedback_source.h
+++ chrome/browser/feedback/android/family_info_feedback_source.h
@@ -52,6 +52,8 @@ class FamilyInfoFeedbackSource : public FamilyInfoFetcher::Consumer {
   raw_ptr<signin::IdentityManager> identity_manager_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   base::android::ScopedJavaGlobalRef<jobject> java_ref_;
+
+  base::WeakPtrFactory<FamilyInfoFeedbackSource> weak_factory_{this};
 };
 
 }  // namespace chrome::android
