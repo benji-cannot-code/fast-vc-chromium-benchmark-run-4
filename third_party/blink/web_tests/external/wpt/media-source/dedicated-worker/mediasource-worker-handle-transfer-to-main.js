07ff5a9e770f9c49b1f5b5c3a92d5866d416cc97
Matt Wolenetz <wolenetz@chromium.org>
2022-07-01 20:56:27 +0000

    MSE-in-Workers: Increase wpt coverage of MediaSourceHandle transfer
    Adds wpt test cases that verify expected transfer-only semantics for a
    (worker MediaSource generated) MediaSourceHandle:
      * MediaSourceHandle serialization without transfer must fail, tested
        in window context
      * Same MediaSourceHandle transferred multiple times in single
        postMessage must fail, tested in window context
      * Attempt to transfer detached MediaSourceHandle must fail, tested in
        window context
      * MediaSourceHandle cannot be transferred, immediately after set as
        srcObject, even if srcObject immediately reset to null
      * MediaSourceHandle cannot be transferred, if it was srcObject when
        asynchronous load starts (loadstart), even if srcObject is then
        immediately reset to null
      * MediaSourceHandle serialization without transfer must fail, tested
        in worker
      * Same MediaSourceHandle transferred multiple times in single
        postMessage must fail, tested in worker
    
    BUG=878133,1338956
    
    Change-Id: Ia90ef1c9db92be252cefb813544e0a450bd85965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739665
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020233}

---
+++ third_party/blink/web_tests/external/wpt/media-source/dedicated-worker/mediasource-worker-handle-transfer-to-main.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('mediasource-message-util.js');
+
+// Note, we do not use testharness.js utilities within the worker context
+// because it also communicates using postMessage to the main HTML document's
+// harness, and would confuse the test case message parsing there.
+
+// Just obtain a MediaSourceHandle and transfer it to creator of our context.
+let handle = new MediaSource().getHandle();
+postMessage(
+    {subject: messageSubject.HANDLE, info: handle}, {transfer: [handle]});
---
+++ third_party/blink/web_tests/external/wpt/media-source/dedicated-worker/mediasource-worker-handle-transfer.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/resources/testharness.js');
+
+test(t => {
+  let handle = new MediaSource().getHandle();
+  assert_true(handle instanceof MediaSourceHandle);
+  assert_throws_dom('DataCloneError', function() {
+    postMessage(handle);
+  }, 'serializing handle without transfer');
+}, 'MediaSourceHandle serialization without transfer must fail, tested in worker');
+
+test(t => {
+  let handle = new MediaSource().getHandle();
+  assert_true(handle instanceof MediaSourceHandle);
+  assert_throws_dom('DataCloneError', function() {
+    postMessage(handle, [handle, handle]);
+  }, 'transferring same handle more than once in same postMessage');
+}, 'Same MediaSourceHandle transferred multiple times in single postMessage must fail, tested in worker');
+
+done();
