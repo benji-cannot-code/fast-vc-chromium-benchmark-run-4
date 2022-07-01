228e2a0f135a5bbe2cb49248b5e19bc474277b12
sangheon77.kim <sangheon77.kim@samsung.com>
2022-07-01 02:36:29 +0000

    Call OnBeginFrameSourcePausedChanged when observer is removed
    Renderer's main thread is blocked until commit is completed in
    impl thread. In the following case, the commit is not handle, so
    the main thread is stuck.
    
    1. ShouldCommit is ignored because has_pending_tree is true.
    2. ShouldActivate is ignored because active_tree_needs_first_draw_
       is true.
    3. ShouldDraw is ignored because did_draw_ is true, so scheduler
       is waiting next OnBeginFrame
    4. At this time, user navigate to other page (e.g,. back, click)
    5. HostFrameSinkManager's UnregisterFrameSinkHierarchy is called
       during navigation, so OnBeginFrame is never called
    6. But scheduler doesn't know about this situation, and keep
       waiting next OnBeginFrame.
    
    This patch send observer OnBeginFrameSourcePausedChanged(), so that
    observer does not wait for next OnBeginFrame and cancel pending
    jobs.
    
    Bug: 1335000
    Change-Id: I6408d60f4a24d7882e94e5934c5d73e89dbe439b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697960
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sangheon Kim <sangheon77.kim@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1019930}

--- cc/test/test_layer_tree_frame_sink.h
+++ cc/test/test_layer_tree_frame_sink.h
@@ -88,6 +88,7 @@ class TestLayerTreeFrameSink : public LayerTreeFrameSink,
   void SetDisplayColorSpace(const gfx::ColorSpace& output_color_space);
 
   viz::Display* display() const { return display_.get(); }
+  void UnregisterBeginFrameSource();
 
   // LayerTreeFrameSink implementation.
   bool BindToClient(LayerTreeFrameSinkClient* client) override;
@@ -107,7 +108,7 @@ class TestLayerTreeFrameSink : public LayerTreeFrameSink,
   void OnBeginFrame(const viz::BeginFrameArgs& args,
                     const viz::FrameTimingDetailsMap& timing_details) override;
   void ReclaimResources(std::vector<viz::ReturnedResource> resources) override;
-  void OnBeginFramePausedChanged(bool paused) override {}
+  void OnBeginFramePausedChanged(bool paused) override;
   void OnCompositorFrameTransitionDirectiveProcessed(
       uint32_t sequence_id) override {}
 
