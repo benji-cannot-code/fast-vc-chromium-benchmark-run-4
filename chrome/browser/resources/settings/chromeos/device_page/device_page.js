06c6c0a133729f41709ea323e821cead16d91f31
Owen Zhang <owenzhang@google.com>
2022-07-02 00:45:37 +0000

    [CrOS Audio]: Create audio row item.
    Create the audio row UI element in the device settings page, and test the UI element with its feature flag's on and off state.
    
    Bug: 1092970
    Test: browser_tests -- gtest_filter=*OSSettingsDevicePage*
    Change-Id: I2fb1d607aa726fb9c48ac1939a70d46af490c239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726965
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1020311}

--- chrome/browser/resources/settings/chromeos/device_page/device_page.js
+++ chrome/browser/resources/settings/chromeos/device_page/device_page.js
@@ -94,6 +94,18 @@ class SettingsDevicePageElement extends SettingsDevicePageElementBase {
       },
 
       /**
+       * Whether audio management info should be shown.
+       * @protected
+       */
+      showAudioInfo_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('enableAudioSettingsPage');
+        },
+        readOnly: true,
+      },
+
+      /**
        * Whether storage management info should be hidden.
        * @private
        */
@@ -124,6 +136,7 @@ class SettingsDevicePageElement extends SettingsDevicePageElementBase {
           if (routes.DISPLAY) {
             map.set(routes.DISPLAY.path, '#displayRow');
           }
+          // TODO(crbug.com/1092970): Create routes.Audio page.
           if (routes.STORAGE) {
             map.set(routes.STORAGE.path, '#storageRow');
           }
