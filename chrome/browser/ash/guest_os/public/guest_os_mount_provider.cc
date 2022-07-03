00b47e7a623f7c908a6ab9f69d3f3c14778c15c8
David Munro <davidmunro@google.com>
2022-07-03 22:54:07 +0000

    crostini: Unmount sftp mount on container shutdown
    Bug: b:217469540
    Test: Unit, manual
    Change-Id: I549866e8b1e79cc5683c5e129fce3cf25f5604f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737602
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020464}

--- chrome/browser/ash/guest_os/public/guest_os_mount_provider.cc
+++ chrome/browser/ash/guest_os/public/guest_os_mount_provider.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/ash/borealis/infra/expected.h"
-#include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/ash/guest_os/infra/cached_callback.h"
