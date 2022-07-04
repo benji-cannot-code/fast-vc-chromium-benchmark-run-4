c9f9c01a5a79ca3d08f7e70316bc290ec78be1be
Kalvin Lee <kdlee@chromium.org>
2022-07-04 19:58:33 +0000

    MTECheckedPtr: Un-rewrite select members
    Note: This CL introduces zero behavioral change, as MTECheckedPtr is
    not enabled by default for anybody.
    
    Before we can enable MTECheckedPtr for anybody, we need to pass CQ.
    To that end, we can't chase down every potential UaF surfaced by the
    unit tests (at least, not yet). This change un-rewrites various
    members (this CL is confined to /components/performance_manager) that cause various test
    suites to fail. The failing suites are named in the TODO line. This
    change will be undone when we conclude experimentation with
    MTECheckedPtr.
    
    This CL was uploaded by git cl split.
    
    R=pmonette@chromium.org
    
    Bug: 1298696
    Change-Id: I3309f261cc840b59625c832b8b1f511981deb4f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740121
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020659}

--- components/performance_manager/graph/process_node_impl_unittest.cc
+++ components/performance_manager/graph/process_node_impl_unittest.cc
@@ -142,7 +142,9 @@ class LenientMockObserver : public ProcessNodeImpl::Observer {
   }
 
  private:
-  raw_ptr<const ProcessNode> notified_process_node_ = nullptr;
+  // TODO(crbug.com/1298696): Breaks components_unittests.
+  raw_ptr<const ProcessNode, DegradeToNoOpWhenMTE> notified_process_node_ =
+      nullptr;
 };
 
 using MockObserver = ::testing::StrictMock<LenientMockObserver>;
