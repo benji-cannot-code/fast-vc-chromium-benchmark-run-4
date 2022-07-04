dc0fdf02e9acccf8ec843a8699f2814140008166
Luciano Pacheco <lucmult@chromium.org>
2022-07-04 04:19:28 +0000

    Files app: Remove unnecessary nohover style
    The CSS class nohover introduced in this CL:
    https://chromiumcodereview.appspot.com/10079002
    https://code.google.com/archive/p/chromium-os/issues/22836
    
    These don't apply anymore.
    
    Add the hover effect for File Grid, when dragging a file over a folder
    in the Grid View we weren't showing any hover effect, this CL fixes by
    using the same style as File List.
    
    Change-Id: Ib1cdb7d3fa993a2d9b1e478e49fdd3a4cab42ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740119
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020486}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -839,10 +839,6 @@ export class FileManager extends EventTarget {
     this.setContextMenuForInput_(this.ui_.listContainer.renameInput);
     this.setContextMenuForInput_(
         this.directoryTreeNamingController_.getInputElement());
-
-    this.document_.addEventListener(
-        'command',
-        this.ui_.listContainer.clearHover.bind(this.ui_.listContainer));
     CommandHandler.registerUndoDeleteToast(this);
   }
 
