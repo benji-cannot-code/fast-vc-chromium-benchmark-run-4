7a33e51767ef1eed534b9e0e9efd57ef79c20578
Kenichi Ishibashi <bashi@chromium.org>
2022-07-04 06:51:21 +0000

    Store response into network service memory cache
    This CL adds the following two classes to store HTTP responses to a
    in-memory cache:
    * NetworkServiceMemoryCache, which is a LRU cache for responses.
    * NetworkServiceMemoryCacheWriter, which stores responses to the
      in-memory cache.
    
    This CL just stores responses. Subsequent CLs implement serving stored
    responses.
    
    See the design document [1] for the motivation and overall
    implementation plan.
    
    [1] https://docs.google.com/document/d/15e1mzyCiBm8ERTO-rM2iXoSAXL98J9fcpejwphlYIEI/edit?usp=sharing
    
    Bug: 1339708
    Change-Id: Idad9e45398000a527ed93a815dbb7acdbbe46a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726637
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020495}

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "services/network/chunked_data_pipe_upload_data_stream.h"
 #include "services/network/data_pipe_element_reader.h"
+#include "services/network/network_service_memory_cache_writer.h"
 #include "services/network/public/cpp/client_hints.h"
 #include "services/network/public/cpp/constants.h"
 #include "services/network/public/cpp/corb/orb_impl.h"
@@ -1120,6 +1121,8 @@ void URLLoader::FollowRedirect(
   // requests by the same client.
   private_network_access_checker_.ResetForRedirect();
 
+  memory_cache_writer_.reset();
+
   // Removing headers can't make the set of pre-existing headers unsafe, but
   // adding headers can.
   if (!AreRequestHeadersSafe(modified_headers) ||
@@ -1546,6 +1549,11 @@ void URLLoader::OnResponseStarted(net::URLRequest* url_request, int net_error) {
     return;
   }
 
+  if (memory_cache_) {
+    memory_cache_writer_ = memory_cache_->MaybeCreateWriter(
+        url_request_.get(), request_destination_, response_);
+  }
+
   ContinueOnResponseStarted();
 }
 
@@ -1744,6 +1752,11 @@ void URLLoader::ReadMore() {
 void URLLoader::DidRead(int num_bytes, bool completed_synchronously) {
   DCHECK(read_in_progress_);
   read_in_progress_ = false;
+
+  if (memory_cache_writer_ && pending_write_) {
+    memory_cache_writer_->OnDataRead(
+        pending_write_->buffer() + pending_write_buffer_offset_, num_bytes);
+  }
 
   size_t new_data_offset = pending_write_buffer_offset_;
   if (num_bytes > 0) {
@@ -2088,6 +2101,9 @@ void URLLoader::NotifyCompleted(int error_code) {
 
     status.pervasive_payload_requested = pervasive_payload_requested_;
 
+    if (memory_cache_writer_)
+      memory_cache_writer_->OnCompleted(status);
+
     url_loader_client_.Get()->OnComplete(status);
   }
 
@@ -2379,11 +2395,15 @@ void URLLoader::CompleteBlockedResponse(
   status.decoded_body_length = 0;
   status.should_report_corb_blocking = should_report_corb_blocking;
   status.blocked_by_response_reason = reason;
+
+  if (memory_cache_writer_)
+    memory_cache_writer_->OnCompleted(status);
   url_loader_client_.Get()->OnComplete(status);
 
   // Reset the connection to the URLLoaderClient.  This helps ensure that we
   // won't accidentally leak any data to the renderer from this point on.
   url_loader_client_.Reset();
+  memory_cache_writer_.reset();
 }
 
 URLLoader::BlockResponseForCorbResult URLLoader::BlockResponseForCorb(
