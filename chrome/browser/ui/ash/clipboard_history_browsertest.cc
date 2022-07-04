56417d47e3abfe5cf2471c31031da0f7c61b0c9c
Melissa Zhang <melzhang@google.com>
2022-07-04 07:22:18 +0000

    [Sheriff] Disable flakey test VerifyClipboardHistoryAcrossMultiUser
    Bug: 1341601
    Change-Id: I5eabbd1d8197a202de55bc7d534382f3618ca7bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3742900
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Owners-Override: Melissa Zhang <melzhang@chromium.org>
    Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020497}

--- chrome/browser/ui/ash/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard_history_browsertest.cc
@@ -1094,8 +1094,9 @@ class ClipboardHistoryMultiProfileBrowserTest
 
 // Verify that the clipboard data history is recorded as expected in the
 // Multiuser environment.
+// TODO(http://crbug.com/1341601): Flakily crashes under ChromeOS
 IN_PROC_BROWSER_TEST_F(ClipboardHistoryMultiProfileBrowserTest,
-                       VerifyClipboardHistoryAcrossMultiUser) {
+                       DISABLED_VerifyClipboardHistoryAcrossMultiUser) {
   EXPECT_TRUE(GetClipboardItems().empty());
 
   // Store text when the user1 is active.
