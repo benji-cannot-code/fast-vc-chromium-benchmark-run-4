02761615c5b891715751cbb6aef05e14a77ed44c
David Bokan <bokan@chromium.org>
2022-07-04 16:45:11 +0000

    fencedframes: Bubble logical scrolls
    Logical scrolls (i.e. keyboard scrolls) should bubble out of a fenced
    frame so that users can keyboard scroll a document if focus is inside a
    fenced frame.
    
    The existing bubbling machinery for remote frames relied on the parent
    frame to initiate the IPC (from the child). Since a fenced frame doesn't
    have access to the parent frame tree, this logic was moved into
    LocalFrame::BubbleLogicalScrollToParentFrame.
    
    Bug: 1310364
    Change-Id: I7c02501fbb1ced6cf6685b29f3bfa7ae474ddb8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727310
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020614}

--- third_party/blink/renderer/core/frame/remote_frame.cc
+++ third_party/blink/renderer/core/frame/remote_frame.cc
@@ -403,17 +403,6 @@ void RemoteFrame::SetInheritedEffectiveTouchAction(TouchAction touch_action) {
   inherited_effective_touch_action_ = touch_action;
 }
 
-bool RemoteFrame::BubbleLogicalScrollFromChildFrame(
-    mojom::blink::ScrollDirection direction,
-    ui::ScrollGranularity granularity,
-    Frame* child) {
-  DCHECK(child->Client());
-  To<LocalFrame>(child)
-      ->GetLocalFrameHostRemote()
-      .BubbleLogicalScrollInParentFrame(direction, granularity);
-  return false;
-}
-
 void RemoteFrame::RenderFallbackContent() {
   Frame::RenderFallbackContent();
 }
@@ -649,10 +638,19 @@ void RemoteFrame::SetNeedsOcclusionTracking(bool needs_tracking) {
 
 void RemoteFrame::BubbleLogicalScroll(mojom::blink::ScrollDirection direction,
                                       ui::ScrollGranularity granularity) {
-  Frame* parent_frame = Parent();
-  DCHECK(parent_frame);
-  DCHECK(parent_frame->IsLocalFrame());
+  LocalFrame* parent_frame = nullptr;
+  if (auto* parent = DynamicTo<LocalFrame>(Parent())) {
+    parent_frame = parent;
+  } else {
+    // This message can be received by an embedded frame tree's placeholder
+    // RemoteFrame in which case Parent() is not connected to the outer frame
+    // tree.
+    auto* owner_element = DynamicTo<HTMLFrameOwnerElement>(Owner());
+    DCHECK(owner_element);
+    parent_frame = owner_element->GetDocument().GetFrame();
+  }
 
+  DCHECK(parent_frame);
   parent_frame->BubbleLogicalScrollFromChildFrame(direction, granularity, this);
 }
 
