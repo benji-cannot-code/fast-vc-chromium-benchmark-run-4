3f34ce770e95910f9d73a9176e330c3990315a2a
Sam Zackrisson <saza@chromium.org>
2022-07-04 21:42:01 +0000

    Add trace for OS audio glitches
    The current histogram metrics only provide aggregate statistics, this CL lets us identify specific glitches in chrome://tracing traces.
    
    Tested: Forced glitches on Windows and inspected chrome://tracing output.
    Bug: chromium:1338920
    Change-Id: I7e4b4b644661d472634169db4ae0090824e80d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735284
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020674}

--- media/audio/system_glitch_reporter.cc
+++ media/audio/system_glitch_reporter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/system_glitch_reporter.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "base/trace_event/trace_event.h"
 
 namespace media {
 
@@ -51,6 +52,10 @@ void SystemGlitchReporter::UpdateStats(base::TimeDelta glitch_duration) {
   ++callback_count_;
 
   if (glitch_duration.is_positive()) {
+    TRACE_EVENT_INSTANT1("audio", "OsGlitchDetected", TRACE_EVENT_SCOPE_THREAD,
+                         "glitch_duration_ms",
+                         glitch_duration.InMilliseconds());
+
     if (callback_count_ <= kCallbacksPerLogPeriod)
       early_glitch_detected_ = true;
 
