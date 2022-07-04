f1e754ff41f20cb1205f45cc40ae0aa65e36050c
Jérôme Lebel <jlebel@chromium.org>
2022-07-04 10:20:22 +0000

    Revert "[iOS] Add activity overlay in AddAccountSigninCoordinator"
    This reverts commit 5314fc721222de8187fb9bf560120a6503aab959.
    
    Reason for revert:
    ActivityOverlayCoordinator should not be used in a
    UINavigationController. ActivityOverlayViewController is added as part
    of view controllers in the navigation [1]. This cause a bug to compute
    the size of the web sign-in bottom sheet [2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/elements/activity_overlay_coordinator.mm;l=28;drc=40b8b75c96327e081bfaf82c527e559e3cf37991
    [2] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_sheet/consistency_sheet_navigation_controller.mm;l=41;drc=d0c45494484484e766d928516e9f2e351ccd0f9c
    
    Original change's description:
    > [iOS] Add activity overlay in AddAccountSigninCoordinator
    >
    > The add account sign-in manager can take some time to be displayed if
    > the network is slow (or all packets are dropped). While waiting for
    > this dialog to be presented, we need to display an activity overlay
    > to make sure the user can't tap on buttons.
    >
    > Fixed: 1334017
    > Change-Id: I2cd135a256e9ee9c1061f9ab3988134680aea740
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706892
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    > Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1015961}
    
    Change-Id: Id1cc0a7c743ce871d63fd8a8b7cccc553f51007f
    Fixed: 1340951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738329
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020527}

--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
 #import "ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_manager.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_coordinator+protected.h"
-#import "ios/chrome/browser/ui/elements/activity_overlay_coordinator.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.h"
@@ -51,11 +50,6 @@ using signin_metrics::PromoAction;
 @property(nonatomic, assign, readonly) AddAccountSigninIntent signinIntent;
 // Account manager service to retrieve Chrome identities.
 @property(nonatomic, assign) ChromeAccountManagerService* accountManagerService;
-// Activity overlay coordinator needed while waiting for the add account sign-in
-// manager to be presented. This is needed if the network is slow or not
-// responding.
-@property(nonatomic, strong)
-    ActivityOverlayCoordinator* activityOverlayCoordinator;
 
 @end
 
@@ -111,10 +105,6 @@ using signin_metrics::PromoAction;
 
 - (void)start {
   [super start];
-  self.activityOverlayCoordinator = [[ActivityOverlayCoordinator alloc]
-      initWithBaseViewController:self.baseViewController
-                         browser:self.browser];
-  [self.activityOverlayCoordinator start];
   self.accountManagerService =
       ChromeAccountManagerServiceFactory::GetForBrowserState(
           self.browser->GetBrowserState());
@@ -138,9 +128,6 @@ using signin_metrics::PromoAction;
 
 - (void)stop {
   [super stop];
-  DCHECK(self.activityOverlayCoordinator);
-  [self.activityOverlayCoordinator stop];
-  self.activityOverlayCoordinator = nil;
   // If one of those 3 DCHECK() fails, -[AddAccountSigninCoordinator
   // runCompletionCallbackWithSigninResult] has not been called.
   DCHECK(!self.addAccountSigninManager);
