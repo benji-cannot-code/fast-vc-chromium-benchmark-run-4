33c6a897f9545b559af588cd98046d4af2d83bee
Nancy Wang <nancylingwang@chromium.org>
2022-07-04 02:34:18 +0000

    Remove the kAppServiceOnAppTypeInitializedWithoutMojom flag.
    The kAppServiceOnAppTypeInitializedWithoutMojom flag is enabled in M101.
    So it can be removed in M105.
    
    Remove AppRegistryCacheMojomTest, because all tests have been migrated
    to AppRegistryCacheTest.
    
    BUG=1253250
    
    Change-Id: Ib030aaa35cc081d7fbaa839741fa3e3990a6af42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739624
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020475}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -7,10 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
-const base::Feature kAppServiceOnAppTypeInitializedWithoutMojom{
-    "AppServiceOnAppTypeInitializedWithoutMojom",
-    base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kAppServiceOnAppUpdateWithoutMojom{
     "AppServiceOnAppUpdateWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
--- components/services/app_service/public/cpp/features.h
+++ components/services/app_service/public/cpp/features.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace apps {
 
 COMPONENT_EXPORT(APP_TYPES)
-extern const base::Feature kAppServiceOnAppTypeInitializedWithoutMojom;
-COMPONENT_EXPORT(APP_TYPES)
 extern const base::Feature kAppServiceOnAppUpdateWithoutMojom;
 COMPONENT_EXPORT(APP_TYPES)
 extern const base::Feature kAppServiceCrosApiOnAppsWithoutMojom;
