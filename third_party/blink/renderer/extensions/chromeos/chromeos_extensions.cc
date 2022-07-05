59849def8926093e27a672aa5c4fd8bd2268b424
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-07-05 05:58:03 +0000

    system-extensions: Eagerly init CrosWindowManagement to set up a mojo connection
    Changes CrosWindowManagement to setup a Mojo connection to the browser
    in preparation for the browser sending events to it.
    
    Also changes CrosWindowManagement to be created when the
    ServiceWorkerGlobalScope is created.
    
    Bug: b/221123297
    Change-Id: I65da1f5fcd2b9a31dd988b47911dab08975cacf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3742841
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020737}

--- third_party/blink/renderer/extensions/chromeos/chromeos_extensions.cc
+++ third_party/blink/renderer/extensions/chromeos/chromeos_extensions.cc
@@ -30,8 +30,7 @@ void ChromeOSDataPropertyGetCallback(
 
 void InstallChromeOSExtensions(ScriptState* script_state) {
   auto* execution_context = ExecutionContext::From(script_state);
-  if (!execution_context ||
-      !ExecutionContext::From(script_state)->IsServiceWorkerGlobalScope() ||
+  if (!execution_context || !execution_context->IsServiceWorkerGlobalScope() ||
       !RuntimeEnabledFeatures::BlinkExtensionChromeOSEnabled()) {
     return;
   }
@@ -45,6 +44,12 @@ void InstallChromeOSExtensions(ScriptState* script_state) {
                             v8::Local<v8::Value>(), v8::DontEnum,
                             v8::SideEffectType::kHasNoSideEffect)
       .ToChecked();
+
+  // Eagerly initialize objects. This is usually done so that they set up a
+  // connection to the browser to receive events.
+  if (RuntimeEnabledFeatures::BlinkExtensionChromeOSWindowManagementEnabled()) {
+    std::ignore = CrosWindowManagement::From(*execution_context);
+  }
 }
 
 }  // namespace
--- third_party/blink/renderer/extensions/chromeos/system_extensions/window_management/cros_window_management.cc
+++ third_party/blink/renderer/extensions/chromeos/system_extensions/window_management/cros_window_management.cc
@@ -45,7 +45,12 @@ CrosWindowManagement::CrosWindowManagement(ExecutionContext& execution_context)
     : Supplement(execution_context),
       ExecutionContextClient(&execution_context),
       cros_window_management_(&execution_context),
-      receiver_(this, &execution_context) {}
+      receiver_(this, &execution_context) {
+  auto receiver = cros_window_management_.BindNewPipeAndPassReceiver(
+      execution_context.GetTaskRunner(TaskType::kMiscPlatformAPI));
+  execution_context.GetBrowserInterfaceBroker().GetInterface(
+      std::move(receiver));
+}
 
 void CrosWindowManagement::Trace(Visitor* visitor) const {
   visitor->Trace(cros_window_management_);
@@ -74,10 +79,7 @@ CrosWindowManagement::GetCrosWindowManagementOrNull() {
   }
 
   if (!cros_window_management_.is_bound()) {
-    auto receiver = cros_window_management_.BindNewPipeAndPassReceiver(
-        execution_context->GetTaskRunner(TaskType::kMiscPlatformAPI));
-    execution_context->GetBrowserInterfaceBroker().GetInterface(
-        std::move(receiver));
+    return nullptr;
   }
   return cros_window_management_.get();
 }
