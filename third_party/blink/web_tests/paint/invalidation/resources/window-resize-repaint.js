95b6f0edff8f32a92fc24e2ccde0e3a75e52b792
Dave Tapuska <dtapuska@chromium.org>
2022-07-05 19:35:17 +0000

    Await resize event in the paint invalidation tests.
    Change the tests to be async and await the resize promise.
    
    BUG=1155202
    
    Change-Id: Ifc6fc8c1bfb00015b500f242fc5d31521185f9d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746979
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020961}

--- third_party/blink/web_tests/paint/invalidation/resources/window-resize-repaint.js
+++ third_party/blink/web_tests/paint/invalidation/resources/window-resize-repaint.js
@@ -13,7 +13,7 @@ var repaintRects = "";
 if (window.internals)
     internals.runtimeFlags.paintUnderInvalidationCheckingEnabled = true;
 
-function doTest() {
+async function doTest() {
     if (sizeIndex) {
         repaintRects += internals.layerTreeAsText(document, window.internals.LAYER_TREE_INCLUDES_INVALIDATIONS);
         internals.stopTrackingRepaints(document);
@@ -21,7 +21,9 @@ function doTest() {
     ++sizeIndex;
     if (sizeIndex < testSizes.length) {
         internals.startTrackingRepaints(document);
+        var resizePromise = new Promise(resolve => window.onresize = resolve);
         window.resizeTo(testSizes[sizeIndex].width, testSizes[sizeIndex].height);
+        await resizePromise;
         runAfterLayoutAndPaint(doTest);
     } else if (window.testRunner) {
         testRunner.setCustomTextOutput(repaintRects);
@@ -30,10 +32,11 @@ function doTest() {
 }
 
 if (window.testRunner) {
-    testRunner.useUnfortunateSynchronousResizeMode();
     testRunner.waitUntilDone();
-    onload = function() {
+    onload = async function() {
+        var resizePromise = new Promise(resolve => window.onresize = resolve);
         window.resizeTo(testSizes[0].width, testSizes[0].height);
+        await resizePromise;
         runAfterLayoutAndPaint(doTest);
     };
 }
