2ff18d42d2733bdb82c323d41bdd70690dac2dd7
Mohamed Mohamed <mmelsayed@google.com>
2022-07-05 04:27:39 +0000

    Introduce MutableFlagWithSafeDefault and PostNative for defining some feature flags in java
    In this cl, we have added two types of flags,
    PostNativeFlag and MutableFlagWithSafeDefault.
    PostNativeFlag covers feature flags that
    assume native is loaded.
    MutableFlagWithSafeDefault covers feature flags that may
    be called before native but not primarily.
    
    This cl will enable programmers to specify the type of flag
    that they wish to use. CachedFlag, MutableFlagWithSafeDefault
    and PostNative flags.
    
    Bug: 1322153
    Change-Id: Ib89b33c1ebdb658211c0175661b50891f23db20e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696594
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Mohamed Mohamed <mmelsayed@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020725}

---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/PostNativeFlag.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.flags;
+
+/**
+ * Flags of this type assume native is loaded and the value can be retrieved directly from native.
+ */
+public class PostNativeFlag extends Flag {
+    public PostNativeFlag(String featureName) {
+        super(featureName);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ChromeFeatureList.isEnabled(mFeatureName);
+    }
+}
