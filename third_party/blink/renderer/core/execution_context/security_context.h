98ae1e93a752bf34b0fc5f1d702f623dd312a6d2
Glen Robertson <glenrob@chromium.org>
2022-07-05 08:15:05 +0000

    Fixup: security_context.h should not include itself
    Change-Id: I5a4a68c004f96319539df45d6c0095bd5fa7428c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3743627
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020756}

--- third_party/blink/renderer/core/execution_context/security_context.h
+++ third_party/blink/renderer/core/execution_context/security_context.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/security_context/insecure_request_policy.mojom-blink-forward.h"
 #include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
