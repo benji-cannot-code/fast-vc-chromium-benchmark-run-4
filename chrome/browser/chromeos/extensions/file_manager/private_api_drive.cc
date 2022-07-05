03dae60abc1b51486bf3bf3c9b5b50a4d4824a35
Ben Reich <benreich@chromium.org>
2022-07-05 01:42:30 +0000

    [MirrorSync] Wire up the manage-mirrorsync dialog to Files app
    The chrome://manage-mirrorsync dialog is where to select folders to sync
    and view useful information about the mirror subsystem. This wires it up
    to the Files gear menu under the name "Manage synced folders". This
    functionality is still behind a flag, so the UX hasn't completely been
    decided.
    
    This removes some method overrides that I forgot to remove before and a
    followup CL will add an integration test to make sure this item doesn't
    show when the flag is disabled. All the plumbing for the flag in the
    browser test framework was blowing this CL up too large.
    
    Bug: b:237066325
    Test: browser_tests --gtest_filter=*showManageMirrorSync*
    Change-Id: I4cad2fa91f76fcb4783c7978ffccaec65ddd5798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740039
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020705}

--- chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/ui/webui/chromeos/manage_mirrorsync/manage_mirrorsync_dialog.h"
 #include "chrome/common/extensions/api/file_manager_private_internal.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chromeos/network/network_handler.h"
@@ -961,6 +962,15 @@ FileManagerPrivatePollDriveHostedFilePinStatesFunction::Run() {
     if (integration_service) {
       integration_service->PollHostedFilePinStates();
     }
+  }
+  return RespondNow(NoArguments());
+}
+
+ExtensionFunction::ResponseAction
+FileManagerPrivateOpenManageSyncSettingsFunction::Run() {
+  if (ash::features::IsDriveFsMirroringEnabled()) {
+    chromeos::ManageMirrorSyncDialog::Show(
+        Profile::FromBrowserContext(browser_context()));
   }
   return RespondNow(NoArguments());
 }
