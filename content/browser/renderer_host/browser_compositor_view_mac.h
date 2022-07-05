2b5bd6ec64af9a1eec17d0a1335d627463fba976
Joe Mason <joenotcharles@google.com>
2022-07-05 22:30:08 +0000

    Stop logging TabSwitchResult::kUnhandled on Mac
    This was expected to be rare, but in the experiment results 100% of tab
    switches with saved frames are recording Unhandled while the control
    group logs mostly Success. This indicates that the UseParentCompositor
    state is actually very common and contrary to the comments in
    https://crrev.com/c/3646659, frames presented by the parent compositor
    are recorded correctly by the tab switch metric.
    
    Bug: 1164477
    Change-Id: I7a187f163508e0762b3097943e231e4590956d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735306
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021001}

--- content/browser/renderer_host/browser_compositor_view_mac.h
+++ content/browser/renderer_host/browser_compositor_view_mac.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/surfaces/scoped_surface_id_allocator.h"
 #include "content/browser/renderer_host/delegated_frame_host.h"
 #include "content/common/content_export.h"
-#include "third_party/blink/public/mojom/widget/record_content_to_visible_time_request.mojom-forward.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_observer.h"
 #include "ui/compositor/layer_observer.h"
@@ -111,13 +110,6 @@ class CONTENT_EXPORT BrowserCompositorMac : public DelegatedFrameHostClient,
       base::OnceCallback<void()> allocation_task);
   const viz::LocalSurfaceId& GetRendererLocalSurfaceId();
   void TransformPointToRootSurface(gfx::PointF* point);
-
-  // Sends `visible_time_request` to the DelegatedFrameHost. Use this instead of
-  // passing the request directly to DelegatedFrameHost::WasShown or
-  // DelegatedFrameHost::RequestPresentationTimeForNextFrame because it will
-  // record the right metrics for the UseParentLayerCompositor state.
-  void RequestPresentationTimeForNextFrame(
-      blink::mojom::RecordContentToVisibleTimeRequestPtr visible_time_request);
 
   // Indicate that the recyclable compositor should be destroyed, and no future
   // compositors should be recycled.
