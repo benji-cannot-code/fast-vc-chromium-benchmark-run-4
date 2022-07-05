18f11a19f27e8cc510619edab6c4335876a8c8e7
Stefano Duo <stefanoduo@google.com>
2022-07-05 12:48:01 +0000

    Refactor CronetTestRule
    1) Plumb isTestingJavaImpl flag all the way into CronetTestFramework:
       This allows us to directly create the correct engine, instead of
       first creating a native engine (regardless of what's needed) and then
       substitute that with a Java one if required.
    2) Wrap the tearDown call (inside the evaluate method) in a finally
       block, so that we cleanup resources even when a test fails
    3) Create a new RequiresMinAndroidApi annotation: this will allow us to
       flag the minimum Android API level required to run the test in a
       more obvious way
    4) Add a new OnlyRunJavaCronet to the list of supported annotations:
       similar to OnlyRunNativeCronet, when OnlyRunJavaCronet is specified
       the test will only run against Cronet's Java implementation
    
    Bug: b:226554121
    
    Change-Id: I7856787118dff05660ed84cebee21b36554f8c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3723564
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020829}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRuleTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRuleTest.java
@@ -7,6 +7,7 @@ package org.chromium.net;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import android.support.test.runner.AndroidJUnit4;
@@ -22,6 +23,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Feature;
 import org.chromium.net.CronetTestRule.CronetTestFramework;
+import org.chromium.net.CronetTestRule.OnlyRunJavaCronet;
 import org.chromium.net.CronetTestRule.OnlyRunNativeCronet;
 import org.chromium.net.CronetTestRule.RequiresMinApi;
 import org.chromium.net.impl.CronetUrlRequestContext;
@@ -97,5 +99,16 @@ public class CronetTestRuleTest {
         assertFalse(mTestWasRun);
         mTestWasRun = true;
         assertEquals(mTestFramework.mCronetEngine.getClass(), CronetUrlRequestContext.class);
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Cronet"})
+    @OnlyRunJavaCronet
+    public void testRunOnlyJavaMustRun() {
+        assertTrue(mTestRule.testingJavaImpl());
+        assertFalse(mTestWasRun);
+        mTestWasRun = true;
+        assertEquals(mTestFramework.mCronetEngine.getClass(), JavaCronetEngine.class);
     }
 }
