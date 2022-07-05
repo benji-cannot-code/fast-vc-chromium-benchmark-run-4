c4951ac5f5e88123a90fd2caacc2516ceebc5fb6
Khushal Sagar <khushalsagar@chromium.org>
2022-07-05 00:16:19 +0000

    blink: Enable overflow for image elements.
    Enable the code-path to apply overflow, overflow-clip-margin and paint
    containment to image elements similar to other elements.
    
    R=pdr@chromium.org, vmpstr@chromium.org
    
    Bug: 1321217
    Change-Id: I9210728ba4fb82ac8a86a89db77c18ea9aa011ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715462
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020691}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -8608,7 +8608,6 @@ bool Element::IsReplacedElementRespectingCSSOverflow() const {
   // TODO(khushalsagar): Add the following elements:
   // 1) SVGElement
   // 2) HTMLFrameOwnerElement
-  // 3) HTMLImageElement
   // See https://github.com/w3c/csswg-drafts/issues/7144 for details on enabling
   // ink overflow for replaced elements.
   if (GetPseudoId() == kPseudoIdPageTransitionIncomingImage ||
@@ -8618,7 +8617,8 @@ bool Element::IsReplacedElementRespectingCSSOverflow() const {
   if (!RuntimeEnabledFeatures::CSSOverflowForReplacedElementsEnabled())
     return false;
 
-  return IsA<HTMLVideoElement>(this) || IsA<HTMLCanvasElement>(this);
+  return IsA<HTMLVideoElement>(this) || IsA<HTMLCanvasElement>(this) ||
+         IsA<HTMLImageElement>(this);
 }
 
 const ComputedStyle* Element::StyleForPositionFallback(unsigned index) {
