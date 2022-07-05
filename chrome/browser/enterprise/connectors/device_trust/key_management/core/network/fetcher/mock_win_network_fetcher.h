ca791d2c6bd768b09c0c679778ff7312dc760bf4
hmare <hmare@google.com>
2022-07-05 20:09:08 +0000

    Implement windows network fetcher
    This is a wrapper for the windows http network fetcher. This is needed
    because the windows network delegate will contain retry logic that in
    its asynchronous flow, and extracting this fetcher makes testing and
    implementation cleaner and easier
    
    Bug: b/237686121
    Change-Id: I56100fbd9d0565cea7b78960f407bb9b1a8e3463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3740185
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020969}

---
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/network/fetcher/mock_win_network_fetcher.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_NETWORK_FETCHER_MOCK_WIN_NETWORK_FETCHER_H_
+#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_NETWORK_FETCHER_MOCK_WIN_NETWORK_FETCHER_H_
+
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/network/fetcher/win_network_fetcher.h"
+
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace enterprise_connectors {
+namespace test {
+
+// Mocked implementation of the WinNetworkFetcher interface.
+class MockWinNetworkFetcher : public WinNetworkFetcher {
+ public:
+  MockWinNetworkFetcher();
+  ~MockWinNetworkFetcher() override;
+
+  MOCK_METHOD(void, Fetch, (FetchCompletedCallback), (override));
+};
+
+}  // namespace test
+}  // namespace enterprise_connectors
+
+#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_NETWORK_FETCHER_MOCK_WIN_NETWORK_FETCHER_H_
