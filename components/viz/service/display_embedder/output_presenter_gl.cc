e954393c74ad2b567c71e264e3e4b9dd4243a232
Saifuddin Hitawala <hitawala@chromium.org>
2022-07-05 15:34:19 +0000

    SharedImageRepresentationOverlay provides a single GpuFenceHandle
    Overlays can only read shared images and cannot write to them. A
    representation of shared image for overlay can only be read by the
    Overlay stream. As a consequence, a fence should be provided to wait
    upon when the shared image is being read on Overlay stream i.e.
    by SharedImageRepresentationOverlay.
    
    This CL modifies the representation to provide a single GpuFenceHandle
    to be waited upon changing from an array of GpuFences.
    
    Bug: 1310771
    Change-Id: If78b90d20b99ecac5a85603f4a55298e5c3e7345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735218
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020877}

--- components/viz/service/display_embedder/output_presenter_gl.cc
+++ components/viz/service/display_embedder/output_presenter_gl.cc
@@ -40,11 +40,10 @@ namespace viz {
 
 namespace {
 
-// Helper function for moving a GpuFence from a vector to a unique_ptr.
-std::unique_ptr<gfx::GpuFence> TakeGpuFence(std::vector<gfx::GpuFence> fences) {
-  DCHECK(fences.empty() || fences.size() == 1u);
-  return fences.empty() ? nullptr
-                        : std::make_unique<gfx::GpuFence>(std::move(fences[0]));
+// Helper function for moving a GpuFence from a fence handle to a unique_ptr.
+std::unique_ptr<gfx::GpuFence> TakeGpuFence(gfx::GpuFenceHandle fence) {
+  return fence.is_null() ? nullptr
+                         : std::make_unique<gfx::GpuFence>(std::move(fence));
 }
 
 class PresenterImageGL : public OutputPresenter::Image {
@@ -149,7 +148,7 @@ gl::GLImage* PresenterImageGL::GetGLImage(
     std::unique_ptr<gfx::GpuFence>* fence) {
   DCHECK(scoped_overlay_read_access_);
   if (fence) {
-    *fence = TakeGpuFence(scoped_overlay_read_access_->TakeAcquireFences());
+    *fence = TakeGpuFence(scoped_overlay_read_access_->TakeAcquireFence());
   }
   return scoped_overlay_read_access_->gl_image();
 }
@@ -426,7 +425,7 @@ void OutputPresenterGL::ScheduleOverlayPlane(
 #endif
     gl_surface_->ScheduleOverlayPlane(
         gl_image,
-        (access ? TakeGpuFence(access->TakeAcquireFences())
+        (access ? TakeGpuFence(access->TakeAcquireFence())
                 : std::move(acquire_fence)),
         gfx::OverlayPlaneData(
             overlay_plane_candidate.plane_z_order,
