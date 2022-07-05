4bbf88a9ed17fa9fa1a0321e33e475ecde845912
Renato Silva <rrsilva@google.com>
2022-07-05 13:28:44 +0000

    OOBE - Add FeatureParameterInterface to OobeTest
    Add a feature parameter interface to OobeTest so that it can
    be easily tested with features.
    
    Change-Id: Ia2cb40d03b20698e88979cc989c2c1fff192f8ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720667
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020837}

--- chrome/browser/ash/login/oobe_browsertest.cc
+++ chrome/browser/ash/login/oobe_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/ash/login/login_pref_names.h"
 #include "chrome/browser/ash/login/test/fake_gaia_mixin.h"
+#include "chrome/browser/ash/login/test/feature_parameter_interface.h"
 #include "chrome/browser/ash/login/test/local_state_mixin.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
@@ -56,17 +57,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class OobeTest : public OobeBaseTest, public testing::WithParamInterface<bool> {
+class OobeTest : public OobeBaseTest, public FeatureAsParameterInterface<1> {
  public:
-  OobeTest() : is_auth_session_enabled_(GetParam()) {
-    if (is_auth_session_enabled_) {
-      scoped_feature_list_.InitAndEnableFeature(
-          features::kUseAuthsessionAuthentication);
-    } else {
-      scoped_feature_list_.InitAndDisableFeature(
-          features::kUseAuthsessionAuthentication);
-    }
-  }
+  OobeTest() = default;
 
   OobeTest(const OobeTest&) = delete;
   OobeTest& operator=(const OobeTest&) = delete;
@@ -99,12 +92,8 @@ class OobeTest : public OobeBaseTest, public testing::WithParamInterface<bool> {
         ->login_window_for_test();
   }
 
- protected:
-  bool is_auth_session_enabled_;
-
  private:
   FakeGaiaMixin fake_gaia_{&mixin_host_};
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_P(OobeTest, NewUser) {
@@ -130,7 +119,8 @@ IN_PROC_BROWSER_TEST_P(OobeTest, NewUser) {
   // Create does not make sense in the context of AuthSession,
   // so we only check passed params when AuthSession feature
   // is disabled.
-  if (!is_auth_session_enabled_) {
+  if (!IsFeatureEnabledInThisTestCase(
+          features::kUseAuthsessionAuthentication)) {
     // Key that was passed as mount authentication does not actually matter,
     // but checking it for the sake of completeness.
     const cryptohome::AuthorizationRequest& cryptohome_auth =
@@ -172,6 +162,14 @@ IN_PROC_BROWSER_TEST_P(OobeTest, Accelerator) {
                             false);  // command
   OobeScreenWaiter(EnrollmentScreenView::kScreenId).Wait();
 }
+
+const auto kAllFeatureVariations = FeatureAsParameterInterface<1>::Generator(
+    {&features::kUseAuthsessionAuthentication});
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         OobeTest,
+                         testing::ValuesIn(kAllFeatureVariations),
+                         FeatureAsParameterInterface<1>::ParamInfoToString);
 
 // Checks that update screen is shown with both legacy and actual name stored
 // in the local state.
@@ -272,7 +270,5 @@ IN_PROC_BROWSER_TEST_F(DisplayOobeTest, OobeMeets2kDisplay) {
   EXPECT_EQ(display.width(), 2560);
   EXPECT_EQ(display.height(), 1440);
 }
-
-INSTANTIATE_TEST_SUITE_P(All, OobeTest, testing::Bool());
 
 }  // namespace ash
