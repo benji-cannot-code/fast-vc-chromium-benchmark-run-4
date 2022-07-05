4ebe49f30f0f43fb832cfb3e4fcdfa73b08692d8
Yutaka Hirano <yhirano@chromium.org>
2022-07-05 01:49:41 +0000

    Disallow streaming upload on HTTP/1.1
    We decided not to allow the streaming upload feature on HTTP/1.1. Remove
    the feature, and move tests accordingly.
    
    See also: https://github.com/whatwg/fetch/issues/966
    
    Bug: 688906
    Change-Id: I4e616469aad2378495ad81ba9034ca034f8ab1b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734308
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020706}

--- third_party/blink/renderer/core/fetch/fetch_request_data.h
+++ third_party/blink/renderer/core/fetch/fetch_request_data.h
@@ -155,13 +155,6 @@ class CORE_EXPORT FetchRequestData final
     trust_token_params_ = std::move(trust_token_params);
   }
 
-  void SetAllowHTTP1ForStreamingUpload(bool allow) {
-    allow_http1_for_streaming_upload_ = allow;
-  }
-  bool AllowHTTP1ForStreamingUpload() const {
-    return allow_http1_for_streaming_upload_;
-  }
-
   void Trace(Visitor*) const;
 
  private:
@@ -213,7 +206,6 @@ class CORE_EXPORT FetchRequestData final
   HeapMojoRemote<network::mojom::blink::URLLoaderFactory> url_loader_factory_;
   base::UnguessableToken window_id_;
   Member<ExecutionContext> execution_context_;
-  bool allow_http1_for_streaming_upload_ = false;
 };
 
 }  // namespace blink
--- third_party/blink/web_tests/external/wpt/fetch/api/basic/request-upload.h2.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/api/basic/request-upload.h2.any.js
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/common/utils.js
 // META: script=/common/get-host-info.sub.js
 
+const duplex = "half";
+
 function testUpload(desc, url, method, createBody, expectedBody) {
   const requestInit = {method};
   promise_test(async () => {
@@ -16,6 +18,17 @@ function testUpload(desc, url, method, createBody, expectedBody) {
     const text = await resp.text();
     assert_equals(text, expectedBody);
   }, desc);
+}
+
+function createStream(chunks) {
+  return new ReadableStream({
+    start: (controller) => {
+      for (const chunk of chunks) {
+        controller.enqueue(chunk);
+      }
+      controller.close();
+    }
+  });
 }
 
 const url = RESOURCES_DIR + "echo-content.h2.py"
@@ -50,7 +63,7 @@ promise_test(async (test) => {
     "/fetch/connection-pool/resources/network-partition-key.py?"
     + `status=421&uuid=${token()}&partition_id=${self.origin}`
     + `&dispatch=check_partition&addcounter=true`,
-    {method: "POST", body: body});
+    {method: "POST", body: body, duplex});
   assert_equals(resp.status, 421);
   const text = await resp.text();
   assert_equals(text, "ok. Request was sent 1 times. 1 connections were created.");
@@ -82,4 +95,45 @@ promise_test(async (test) => {
   const response = await fetch(request);
   assert_equals(await response.text(), 'test', `Response has correct body`);
 }, "Feature detect for POST with ReadableStream, using request object");
+
+promise_test(async (t) => {
+  const body = createStream(["hello"]);
+  const method = "POST";
+  await promise_rejects_js(t, TypeError, fetch(url, { method, body, duplex }));
+}, "Streaming upload with body containing a String");
+
+promise_test(async (t) => {
+  const body = createStream([null]);
+  const method = "POST";
+  await promise_rejects_js(t, TypeError, fetch(url, { method, body, duplex }));
+}, "Streaming upload with body containing null");
+
+promise_test(async (t) => {
+  const body = createStream([33]);
+  const method = "POST";
+  await promise_rejects_js(t, TypeError, fetch(url, { method, body, duplex }));
+}, "Streaming upload with body containing a number");
+
+promise_test(async (t) => {
+  const url = "/fetch/api/resources/redirect.h2.py?location=/common/blank.html";
+  const body = createStream([]);
+  const method = "POST";
+  await promise_rejects_js(t, TypeError, fetch(url, { method, body, duplex }));
+}, "Streaming upload should fail on redirect (302)");
+
+promise_test(async (t) => {
+  const url = "/fetch/api/resources/redirect.h2.py?" +
+              "redirect_status=303&location=/common/blank.html";
+  const body = createStream([]);
+  const method = "POST";
+  const resp = await fetch(url, { method, body, duplex });
+  assert_equals(resp.status, 200, 'status');
+}, "Streaming upload should work with 303");
+
+promise_test(async (t) => {
+  const url = "/fetch/api/resources/authentication.py?realm=test";
+  const body = createStream([]);
+  const method = "POST";
+  await promise_rejects_js(t, TypeError, fetch(url, { method, body, duplex }));
+}, "Streaming upload should fail on a 401 response");
 
