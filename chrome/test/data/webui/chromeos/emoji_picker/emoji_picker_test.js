291d12281c938789b8dd31917447cbcce8beaca2
Mehrab N <mehrab@chromium.org>
2022-07-05 01:26:41 +0000

    Reland "emoji-picker: unifying category data presentation"
    This is a reland of commit ab2a3b6c78f87efb44e948f56c547f0c8ae4abe4
    
    Changed: Nothing has changed in the CL. The reason for reverting the
    original CL was a test issue in an ancestor CL which is resolved now.
    
    Original change's description:
    > emoji-picker: unifying category data presentation
    >
    > Continuing some recent refactors, category data presentation is unified and the following improvements are applied:
    >
    > * The separate data sources for different categories are removed and replaced by one single source of data where category is only an attribute of each record.
    > * Logics for rendering of different categories are centralised and unified.
    > * Rendering of history groups are now handled the same way as any other emoji groups rather than a special case.
    > * EmojiGroupElements are created as a transformation of emoji group data helping to extend UI features of emoji groups easier and independent from other features such as search.
    > * Minor changes to metadata is applied and some unused codes are removed.
    >
    > Fixed: b:235418846,b:233270330
    > Change-Id: I81cc05dd535b3e98f3f07225fc62045e3432459a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695832
    > Reviewed-by: Keith Lee <keithlee@chromium.org>
    > Commit-Queue: Mehrab N <mehrab@chromium.org>
    > Reviewed-by: John Palmer <jopalmer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1019372}
    
    Fixed: b:235418846,b:233270330
    Change-Id: I564b6fb6e7db26d696976c367b55471031f19c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744521
    Commit-Queue: Mehrab N <mehrab@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020699}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test.js
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test.js
@@ -113,7 +113,7 @@ suite('<emoji-picker>', () => {
   test('recently used should be hidden when empty', () => {
     const recentlyUsed =
         findInEmojiPicker('[data-group="emoji-history"] > emoji-group');
-    assert(!recentlyUsed);
+    assert(recentlyUsed.classList.contains('hidden'));
   });
 
   test(
@@ -212,7 +212,7 @@ suite('<emoji-picker>', () => {
 
         const recentlyUsed =
             findInEmojiPicker('[data-group="emoji-history"] > emoji-group');
-        assert(!recentlyUsed);
+        assert(recentlyUsed.classList.contains('hidden'));
       });
 
   test('recently used should be empty after clearing', async () => {
@@ -239,8 +239,9 @@ suite('<emoji-picker>', () => {
 
     // Expect no more history.
     await waitForCondition(
-        () => findInEmojiPicker('[data-group="emoji-history"] > emoji-group')
-                  .style.display === 'none',
+        () => findInEmojiPicker(
+          '[data-group="emoji-history"] > emoji-group')
+                  .classList.contains('hidden'),
         'history failed to disappear');
   });
 
