7f5179e771ab917f49a35ab88c0b4c18e44aa1f4
Jérôme Lebel <jlebel@chromium.org>
2022-07-05 22:39:15 +0000

    [iOS] Change accessory symbol size from 22 to 18
    The size of the accessory symbol needs to be 18 (instead of 22).
    Reviewed by Carolina.
    
    Before:
    https://drive.google.com/file/d/1Ii9eB4DEGRB1Q2TPAzOEfOuhN7rGDKCb/view?usp=sharing&resourcekey=0-IzR5egtENfk4KUksKJsAEA
    https://drive.google.com/file/d/1_grTzG_PddNGf1qL-IDzhehpopiy-kOW/view?usp=sharing&resourcekey=0-abpYjVXbfPRE3CLOiJpiCQ
    
    After:
    https://drive.google.com/file/d/1XpI0Frlrf-HacMzcH_L82-I_3TCOVwJA/view?usp=sharing&resourcekey=0-lAkLY9UVDysRX3j1ukNICQ
    https://drive.google.com/file/d/1gLQAHhkkmqCOOhm7SsRhGrONEOIKgBFt/view?usp=sharing&resourcekey=0-WXc89sFHn9lGrecnSHkbBw
    
    Fixed: 1334989
    Change-Id: If353dbb75d9e637226d5365432538010882b6bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745467
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021005}

--- ios/chrome/browser/ui/icons/item_icon.mm
+++ ios/chrome/browser/ui/icons/item_icon.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-const CGFloat kSymbolAccessoryPointSize = 22.;
+const CGFloat kSymbolAccessoryPointSize = 18.;
 
 NSString* const kExternalLinkSmbol = @"arrow.up.forward.square";
 NSString* const kChevronForwardSymbol = @"chevron.forward";
