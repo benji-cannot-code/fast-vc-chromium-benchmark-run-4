74682c47c9a27fcae6bdba322397de6ed6d4d398
Maxim Anufriev <maxan@google.com>
2022-07-05 12:43:11 +0000

    [UPM] Use DCHECK to detect incorrect ignored credentials
    Replace CHECKs introduced in http://crrev.com/c/3732387 with DCHECK.
    Check was used to determine the root cause of the GMS backend
    DEVELOPER_ERROR and expectedly results in chrome crash.
    
    Bug: 1324588, 1341914
    Change-Id: I3c0ffaedd5527df1536b8054614090ea27d239a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3743319
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020826}

--- chrome/browser/password_manager/android/password_store_android_backend.cc
+++ chrome/browser/password_manager/android/password_store_android_backend.cc
@@ -437,9 +437,8 @@ void PasswordStoreAndroidBackend::FillMatchingLoginsAsync(
 void PasswordStoreAndroidBackend::AddLoginAsync(
     const PasswordForm& form,
     PasswordChangesOrErrorReply callback) {
-  // TODO(crbug.com/1324588): remove this check
-  CHECK(!form.blocked_by_user ||
-        (form.username_value.empty() && form.password_value.empty()));
+  DCHECK(!form.blocked_by_user ||
+         (form.username_value.empty() && form.password_value.empty()));
   JobId job_id =
       bridge_->AddLogin(form, GetAccount(GetSyncingAccount(sync_service_)));
   QueueNewJob(job_id, std::move(callback), MetricInfix("AddLoginAsync"));
@@ -448,9 +447,8 @@ void PasswordStoreAndroidBackend::AddLoginAsync(
 void PasswordStoreAndroidBackend::UpdateLoginAsync(
     const PasswordForm& form,
     PasswordChangesOrErrorReply callback) {
-  // TODO(crbug.com/1324588): remove this check
-  CHECK(!form.blocked_by_user ||
-        (form.username_value.empty() && form.password_value.empty()));
+  DCHECK(!form.blocked_by_user ||
+         (form.username_value.empty() && form.password_value.empty()));
   JobId job_id =
       bridge_->UpdateLogin(form, GetAccount(GetSyncingAccount(sync_service_)));
   QueueNewJob(job_id, std::move(callback), MetricInfix("UpdateLoginAsync"));
--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -110,9 +110,8 @@ bool PasswordStore::Init(
 
 void PasswordStore::AddLogin(const PasswordForm& form) {
   DCHECK(main_task_runner_->RunsTasksInCurrentSequence());
-  // TODO(crbug.com/1324588): Replace by DCHECK
-  CHECK(!form.blocked_by_user ||
-        (form.username_value.empty() && form.password_value.empty()));
+  DCHECK(!form.blocked_by_user ||
+         (form.username_value.empty() && form.password_value.empty()));
   if (!backend_)
     return;  // Once the shutdown started, ignore new requests.
   backend_->AddLoginAsync(
@@ -124,9 +123,8 @@ void PasswordStore::AddLogin(const PasswordForm& form) {
 
 void PasswordStore::UpdateLogin(const PasswordForm& form) {
   DCHECK(main_task_runner_->RunsTasksInCurrentSequence());
-  // TODO(crbug.com/1324588): Replace by DCHECK
-  CHECK(!form.blocked_by_user ||
-        (form.username_value.empty() && form.password_value.empty()));
+  DCHECK(!form.blocked_by_user ||
+         (form.username_value.empty() && form.password_value.empty()));
   if (!backend_)
     return;  // Once the shutdown started, ignore new requests.
   backend_->UpdateLoginAsync(
