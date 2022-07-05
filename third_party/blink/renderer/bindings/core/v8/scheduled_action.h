3aa6b8138daa7b9d8c8fc03b16b319865143c24f
Yoav Weiss <yoavweiss@chromium.org>
2022-07-05 15:04:59 +0000

    Properly handle TaskAttribution for setTimeout with a string
    Current TaskAttribution implementation doesn't handle well setTimeout with explicit code passed to it, in the form of a string. (rather than
    a callback function)
    
    This CL changes that, by explicitly creating a TaskScope before running
    such code, where the task scope's parent is the task registering the
    timeout.
    
    Bug: 1341990
    Change-Id: I524aa0bccd20ef1d7cd51631cd08cff61c9fcaa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745563
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020866}

--- third_party/blink/renderer/bindings/core/v8/scheduled_action.h
+++ third_party/blink/renderer/bindings/core/v8/scheduled_action.h
@@ -32,9 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCHEDULED_ACTION_H_
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCHEDULED_ACTION_H_
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/scheduler/public/task_id.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "v8/include/v8.h"
@@ -77,6 +79,7 @@ class ScheduledAction final : public GarbageCollected<ScheduledAction>,
   Member<V8Function> function_;
   HeapVector<ScriptValue> arguments_;
   String code_;
+  absl::optional<scheduler::TaskId> code_parent_task_id_;
 };
 
 }  // namespace blink
