5e747d435edc4c99de79f7e68c2df23008bc898a
Benoit Lize <lizeb@chromium.org>
2022-07-05 10:40:44 +0000

    [cc] Move away from banned <random> to sample metrics
    Change-Id: Ia7f6002b28f2de4ee93bebaef50c1d8867a3a64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737743
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020798}

--- cc/raster/gpu_raster_buffer_provider.h
+++ cc/raster/gpu_raster_buffer_provider.h
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 #include <memory>
-#include <random>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/rand_util.h"
 #include "base/time/time.h"
 #include "cc/raster/raster_buffer_provider.h"
 #include "cc/raster/raster_query_queue.h"
@@ -146,9 +146,9 @@ class CC_EXPORT GpuRasterBufferProvider : public RasterBufferProvider {
 
   const raw_ptr<RasterQueryQueue> pending_raster_queries_;
 
+  const double raster_metric_probability_;
   // Accessed with the worker context lock acquired.
-  std::mt19937 random_generator_;
-  std::bernoulli_distribution bernoulli_distribution_;
+  base::MetricsSubSampler metrics_subsampler_;
   const bool is_using_raw_draw_;
 };
 
