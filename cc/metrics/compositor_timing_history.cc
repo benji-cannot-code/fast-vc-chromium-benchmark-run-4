faa398849bfed2c0c0dc855d043a0bf04d9c715e
Mingjing Zhang <mjzhang@chromium.org>
2022-07-05 17:34:38 +0000

    Remove expired Scheduling.Browser/Renderer metrics
    This CL removes the following expired metrics:
    
    Scheduling.Browser.CommitToReadyToActivateDuration2.*
    Scheduling.Renderer.CommitToReadyToActivateDuration2.*
    
    Bug: 880139, 926070, 1021376, 1048452
    Change-Id: If6dbbdc6b7dea4b7f69e7f9435cecbd04e6552dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744243
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020926}

--- cc/metrics/compositor_timing_history.cc
+++ cc/metrics/compositor_timing_history.cc
@@ -31,8 +31,6 @@ class CompositorTimingHistory::UMAReporter {
 
   // Latency measurements
   virtual void AddBeginImplFrameLatency(base::TimeDelta delta) = 0;
-  virtual void AddCommitToReadyToActivateDuration(base::TimeDelta duration,
-                                                  TreePriority priority) = 0;
   virtual void AddInvalidationToReadyToActivateDuration(
       base::TimeDelta duration,
       TreePriority priority) = 0;
@@ -347,13 +345,6 @@ class RendererUMAReporter : public CompositorTimingHistory::UMAReporter {
         "Scheduling.Renderer.BeginImplFrameLatency", delta);
   }
 
-  void AddCommitToReadyToActivateDuration(base::TimeDelta duration,
-                                          TreePriority priority) override {
-    UMA_HISTOGRAM_READY_TO_ACTIVATE(
-        "Scheduling.Renderer.CommitToReadyToActivateDuration", duration,
-        priority);
-  }
-
   void AddInvalidationToReadyToActivateDuration(
       base::TimeDelta duration,
       TreePriority priority) override {
@@ -390,13 +381,6 @@ class BrowserUMAReporter : public CompositorTimingHistory::UMAReporter {
         "Scheduling.Browser.BeginImplFrameLatency", delta);
   }
 
-  void AddCommitToReadyToActivateDuration(base::TimeDelta duration,
-                                          TreePriority priority) override {
-    UMA_HISTOGRAM_READY_TO_ACTIVATE(
-        "Scheduling.Browser.CommitToReadyToActivateDuration", duration,
-        priority);
-  }
-
   void AddInvalidationToReadyToActivateDuration(
       base::TimeDelta duration,
       TreePriority priority) override {
@@ -424,8 +408,6 @@ class NullUMAReporter : public CompositorTimingHistory::UMAReporter {
   void AddDrawIntervalWithCustomPropertyAnimations(
       base::TimeDelta inverval) override {}
   void AddBeginImplFrameLatency(base::TimeDelta delta) override {}
-  void AddCommitToReadyToActivateDuration(base::TimeDelta duration,
-                                          TreePriority priority) override {}
   void AddInvalidationToReadyToActivateDuration(
       base::TimeDelta duration,
       TreePriority priority) override {}
@@ -756,8 +738,6 @@ void CompositorTimingHistory::ReadyToActivate() {
 
     base::TimeDelta commit_to_ready_to_activate_estimate =
         CommitToReadyToActivateDurationEstimate();
-    uma_reporter_->AddCommitToReadyToActivateDuration(time_since_commit,
-                                                      tree_priority_);
     rendering_stats_instrumentation_->AddCommitToActivateDuration(
         time_since_commit, commit_to_ready_to_activate_estimate);
 
