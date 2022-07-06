60f9246634964131f5bead487d828f1dc72febf3
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-06 15:14:57 +0000

    [rust] Initial `build/rust/rs_bindings_from_cc.gni`.
    Initial integration of Crubit's `rs_bindings_from_cc` into Chromium's
    build system by introducing an `rs_bindings_from_cc` GN template.  Lots
    of limitations and disclaimers for now:
    
    * This has only been tested to work on my machine.  In particular, it
      depends on `third_party/rust-toolchain/bin/rs_bindings_from_cc`.  See
      the TODOs and the commented-out targets in build/rust/tests/BUILD.gn
      and the build instructions in run_rs_bindings_from_cc.py
    * Only a single input header can be passed via `--public_header` cmdline
      argument of `rs_bindings_from_cc`
    * Only the simplest possible, self-contained C++ header with a single
      inline function has been tested so far:
      * No tests where the imported C++ headers depend on the C++ stdlib
      * No tests where the imported C++ headers contain structs
        (in this case the generated bindings will depend on additional
         Rust crates: `memoffset` and `static_assertions`)
      * No tests where the imported C++ headers depend on other GN
        targets under //build/rust/tests.  (Solving this will require
        some work around `--targets_and_headers` which currently
        only specifies an entry for a single target and is oblivious
        to transitive dependencies.)
    * It is not clear if a separate GN template is the right solution for
      the long-term.  (Crubit's equivalent Blaze integration just needs a
      dependency declaration between a `rust_library` and `cc_library` with
      no need to an explicit, separate declaration of an
      `rs_bindings_from_cc` target/dependency.)
    
    Bug: 1329611
    Change-Id: Ida983fba16a7cc3acf13687746016771243dc0ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739407
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021194}

---
+++ build/rust/tests/test_rs_bindings_from_cc/self_contained_header.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_HEADER_H_
+#define BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_HEADER_H_
+
+inline int MultiplyViaCc(int x, int y) {
+  return x * y;
+}
+
+#endif  // BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_HEADER_H_
