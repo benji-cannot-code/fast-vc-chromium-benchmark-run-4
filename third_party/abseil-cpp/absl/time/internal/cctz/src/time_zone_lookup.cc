3335e300d05739566e346adbe142ce2401214653
Danil Chapovalov <danilchap@chromium.org>
2022-07-06 19:46:33 +0000

    Roll abseil_revision 736458b555..4bbdb02689
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/736458b555..4bbdb02689
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/736458b555..4bbdb02689
    
    No .def changes
    
    Bug: None
    Change-Id: Iab46fd785eeea036cbdadb3df5df8735d87060c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747819
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021319}

--- third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_lookup.cc
+++ third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_lookup.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(__Fuchsia__)
 #include <fuchsia/intl/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
-#include <lib/sys/cpp/component_context.h>
+#include <lib/fdio/directory.h>
 #include <zircon/types.h>
 #endif
 
@@ -161,11 +161,11 @@ time_zone local_time_zone() {
     // would be set to null when the loop is destroyed, causing any other FIDL
     // code running on the same thread to crash.
     async::Loop loop(&kAsyncLoopConfigNeverAttachToThread);
-    std::unique_ptr<sys::ComponentContext> context =
-        sys::ComponentContext::Create();
 
     fuchsia::intl::PropertyProviderHandle handle;
-    zx_status_t status = context->svc()->Connect(handle.NewRequest());
+    zx_status_t status = fdio_service_connect_by_name(
+        fuchsia::intl::PropertyProvider::Name_,
+        handle.NewRequest().TakeChannel().release());
     if (status != ZX_OK) {
       return;
     }
