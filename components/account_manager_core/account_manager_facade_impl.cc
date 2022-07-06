3bce7323c1bb04b5611ebd27f2bd386a22b6d8e6
Kush Sinha <sinhak@chromium.org>
2022-07-06 10:43:23 +0000

    crOS: Add histograms for Account Manager disconnection
    Add histograms for Mojo pipe disconnections for ChromeOS Account
    Manager, as observed by the remote in `AccountManagerFacadeImpl`. These
    histograms will be logged by the remote in Lacros, as well as Ash.
    
    Bug: 1342001
    Change-Id: I996b5e489ae8724599d74768d2baa10dbb6b0f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745919
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021128}

--- components/account_manager_core/account_manager_facade_impl.cc
+++ components/account_manager_core/account_manager_facade_impl.cc
@@ -40,6 +40,8 @@ const char kAccountAdditionResultStatus[] =
     "AccountManager.AccountAdditionResultStatus";
 const char kGetAccountsMojoStatus[] =
     "AccountManager.FacadeGetAccountsMojoStatus";
+const char kMojoDisconnectionsAccountManagerRemote[] =
+    "AccountManager.MojoDisconnections.AccountManagerRemote";
 
 void UnmarshalAccounts(
     base::OnceCallback<void(const std::vector<Account>&)> callback,
@@ -280,7 +282,10 @@ AccountManagerFacadeImpl::AccountManagerFacadeImpl(
                      weak_factory_.GetWeakPtr()));
 }
 
-AccountManagerFacadeImpl::~AccountManagerFacadeImpl() = default;
+AccountManagerFacadeImpl::~AccountManagerFacadeImpl() {
+  base::UmaHistogramCounts100(kMojoDisconnectionsAccountManagerRemote,
+                              num_remote_disconnections_);
+}
 
 void AccountManagerFacadeImpl::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
@@ -564,6 +569,7 @@ void AccountManagerFacadeImpl::RunOnMojoDisconnection(
 
 void AccountManagerFacadeImpl::OnMojoError() {
   LOG(ERROR) << "Account Manager disconnected";
+  num_remote_disconnections_++;
   for (auto& cb : mojo_disconnection_handlers_) {
     std::move(cb).Run();
   }
