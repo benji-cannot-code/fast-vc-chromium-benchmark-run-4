a63a45823f42f9591dba73482429cf9ee44af565
Wenbo Jie <wenbojie@chromium.org>
2022-07-06 05:34:12 +0000

    [FilesRecentFilter] Add browser test for time-period headings
    Bug: 1324051
    Test: browser_tests --gtest_filter="*recentsTimePeriodHeadings*"
    Change-Id: I3134cad8b3ce4c878fc200b2bf73898787dce2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744701
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021057}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1732,20 +1732,16 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("recentsA11yMessages").EnableFiltersInRecents(),
         TestCase("recentsA11yMessages").EnableFiltersInRecents().FilesSwa(),
         TestCase("recentsAllowCutForDownloads")
-            .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2(),
         TestCase("recentsAllowCutForDownloads")
-            .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2()
             .FilesSwa(),
         TestCase("recentsAllowCutForDrive")
-            .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2(),
         TestCase("recentsAllowCutForDrive")
-            .EnableArc()
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2()
             .FilesSwa(),
@@ -1851,6 +1847,13 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2(),
         TestCase("recentsReadOnlyHidden")
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2()
+            .FilesSwa(),
+        TestCase("recentsTimePeriodHeadings")
+            .EnableFiltersInRecents()
+            .EnableFiltersInRecentsV2(),
+        TestCase("recentsTimePeriodHeadings")
             .EnableFiltersInRecents()
             .EnableFiltersInRecentsV2()
             .FilesSwa(),
