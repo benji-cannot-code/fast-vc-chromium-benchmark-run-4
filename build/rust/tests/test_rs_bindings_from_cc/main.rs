f7d9d85014d3c085583407955726d2b83f9c8402
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-06 16:46:05 +0000

    [rust] rs_bindings_from_cc.gni: Support for *multiple* `public_headers`.
    After this CL the `rs_bindings_from_cc` target template accepts a *list*
    of `public_headers` (rather than just a single *string*).
    
    Bug: 1329611
    Change-Id: I1d927a405690d53ce4eea95c63d79bba452df3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3741423
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021224}

--- build/rust/tests/test_rs_bindings_from_cc/main.rs
+++ build/rust/tests/test_rs_bindings_from_cc/main.rs
@@ -5,13 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 fn main() {
     println!("Hello world!");
-    println!("MultiplyViaCc(100,42) = {}", ::self_contained_header_rs_api::MultiplyViaCc(100, 42));
+    println!("AddViaCc(100,42) = {}", ::self_contained_target_rs_api::AddViaCc(100, 42));
+    println!("MultiplyViaCc(100,42) = {}", ::self_contained_target_rs_api::MultiplyViaCc(100, 42));
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_call_via_crubit() {
-        assert_eq!(100 * 42, ::self_contained_header_rs_api::MultiplyViaCc(100, 42));
+    fn test_self_contained_target() {
+        assert_eq!(100 + 42, ::self_contained_target_rs_api::AddViaCc(100, 42));
+        assert_eq!(100 * 42, ::self_contained_target_rs_api::MultiplyViaCc(100, 42));
     }
 }
---
+++ build/rust/tests/test_rs_bindings_from_cc/self_contained_target_header2.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_TARGET_HEADER2_H_
+#define BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_TARGET_HEADER2_H_
+
+int AddViaCc(int x, int y);
+
+#endif  // BUILD_RUST_TESTS_TEST_RS_BINDINGS_FROM_CC_SELF_CONTAINED_TARGET_HEADER2_H_
