1423fb82d595c10acfeb4342aabdbae4f7c2d8eb
Bowen Li <lbowen@google.com>
2022-07-06 21:24:32 +0000

    Assign Blazey devices to user admin-{country}-blazey@cros-demo-mode.com
    doc: go/demo-blazey-customization
    
    Bug: b/237584726
    Change-Id: I858f88173f24916521b3451e887ff890b4eefae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737798
    Commit-Queue: Bowen Li <lbowen@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021375}

--- chrome/browser/ash/login/demo_mode/demo_setup_controller.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_controller.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/system/statistics_provider.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
@@ -499,6 +500,11 @@ std::string DemoSetupController::GetSubOrganizationEmail() {
                 std::end(DemoSession::kSupportedCountries),
                 country_uppercase) !=
       std::end(DemoSession::kSupportedCountries)) {
+    if (chromeos::features::IsCloudGamingDeviceEnabled()) {
+      return base::StringPrintf("admin-%s-blazey@%s", country_lowercase.c_str(),
+                                policy::kDemoModeDomain);
+    }
+
     return "admin-" + country_lowercase + "@" + policy::kDemoModeDomain;
   }
   return std::string();
