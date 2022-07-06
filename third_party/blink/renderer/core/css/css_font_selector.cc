501355c8a26274c27fadacb53fdb7c35a6f049f4
Xiaocheng Hu <xiaochengh@chromium.org>
2022-07-06 19:58:42 +0000

    Revert "[parallel-text-shaping] CSS changes"
    This reverts commit 1f36b64dd81cc0c4f4443598fb09307b78c7ac84.
    
    Reason for revert: Caused a nullptr deref crbug.com/1335246#c23
    
    Original change's description:
    > [parallel-text-shaping] CSS changes
    >
    > This patch changes "core/css", "platform/{fonts,testing}" to make font
    > loading, both system and web, to work with font threads for prepration
    > of parallel text shaping[1].
    >
    > This patch is split from the CL[2][3] to exclude `CSSSegmentedFontFace`
    > and `CSSFontFace` changes to isolate memory related issues
    >
    >
    > Note: `USE_PARALLEL_TEXT_SHAPING` will be defined on Win and MacOS.
    >
    > core/css
    >   * Make objects thread-safe. Mostly adding locks.
    >   * Utilize `ExecutionContext::GetTaskRunner()` to call
    >     `FontMatchingMetrics` in owner execution context from font
    >     threads.
    >
    > platform/fonts/font_selector.{cc,h}
    >   * Make `SimpleFontData` parameter in reporting function can be cross
    >     thread.
    >
    > platform/fonts/font_test_helper.cc
    >   * Adapt for `FontSelector` changes.
    >
    > [1] http://crrev.com/c/3211416 Parallel Text Shaping implementation
    > [2] http://crrev.com/c/3670315: [parallel-text-shaping] CSS changes
    > [3] http://crrev.com/c/3703810: [parallel-text-shaping] Use
    > Persistent<FontFaceList> in CSSSegmentedFontFace
    >
    > Design Doc: https://bit.ly/3GqpJLD
    > Font slid: https://bit.ly/3JFTSHa
    >
    > Bug: 1264280
    > Change-Id: Ia1dbb155a6cd7d653cd2b52e03e1945db823bf42
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708289
    > Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1015233}
    
    Bug: 1264280, 1335246
    Change-Id: I80943ab56c6c86234f857f392e95edfb9e775403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3748643
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021325}

--- third_party/blink/renderer/core/css/css_font_selector.cc
+++ third_party/blink/renderer/core/css/css_font_selector.cc
@@ -41,11 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 CSSFontSelector::CSSFontSelector(const TreeScope& tree_scope)
-    : CSSFontSelectorBase(
-          tree_scope.GetDocument().GetExecutionContext()->GetTaskRunner(
-              TaskType::kInternalDefault)),
-      tree_scope_(&tree_scope) {
-  DCHECK(tree_scope.GetDocument().GetExecutionContext()->IsContextThread());
+    : tree_scope_(&tree_scope) {
   DCHECK(tree_scope.GetDocument().GetFrame());
   generic_font_family_settings_ = tree_scope.GetDocument()
                                       .GetFrame()
@@ -62,8 +58,7 @@ CSSFontSelector::CSSFontSelector(const TreeScope& tree_scope)
 CSSFontSelector::~CSSFontSelector() = default;
 
 UseCounter* CSSFontSelector::GetUseCounter() const {
-  auto* const context = GetExecutionContext();
-  return context && context->IsContextThread() ? context : nullptr;
+  return GetExecutionContext();
 }
 
 void CSSFontSelector::RegisterForInvalidationCallbacks(
@@ -154,7 +149,7 @@ scoped_refptr<FontData> CSSFontSelector::GetFontData(
       FontCache::Get().GetFontData(request_description, settings_family_name);
 
   ReportFontLookupByUniqueOrFamilyName(settings_family_name,
-                                       request_description, font_data);
+                                       request_description, font_data.get());
 
   return font_data;
 }
@@ -169,10 +164,6 @@ void CSSFontSelector::UpdateGenericFontFamilySettings(Document& document) {
 
 FontMatchingMetrics* CSSFontSelector::GetFontMatchingMetrics() const {
   return GetDocument().GetFontMatchingMetrics();
-}
-
-bool CSSFontSelector::IsAlive() const {
-  return tree_scope_;
 }
 
 void CSSFontSelector::Trace(Visitor* visitor) const {
