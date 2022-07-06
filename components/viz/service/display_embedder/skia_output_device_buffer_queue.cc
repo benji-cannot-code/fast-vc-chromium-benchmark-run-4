6f11645cdd0f0e772c6ca51c36b7532254ad5d6d
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-07-06 08:36:11 +0000

    Revert "[GPU] Remove unneeded include of gl_image.h from texture_manager.h"
    This reverts commit 9015301b5cd65081dc61e0604a828d676646bd9f.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision 9015301b5cd65081dc61e0604a828d676646bd9f as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8809404543368316561
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2F9015301b5cd65081dc61e0604a828d676646bd9f&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+9015301b5cd65081dc61e0604a828d676646bd9f
    
    Original change's description:
    > [GPU] Remove unneeded include of gl_image.h from texture_manager.h
    > 
    > A forward declaration is sufficient for texture_manager.h's usage of
    > GLImage. The benefit of this change is that it exposes clients of
    > GLImage that were previously getting the include transitively via
    > texture_manager.h, thus making them visible and easier to analyze.
    > 
    > Bug: 1310018
    > Change-Id: I5acdb355fd46f071e0bcc3b82ede6b52ee2450f6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745938
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1021080}
    
    
    Change-Id: I124d26bf39f428a0c57584579cda8d9d0ff4c450
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1310018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746929
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Owners-Override: Andy Paicu <andypaicu@google.com>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021092}

--- components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
+++ components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/gpu_fence_handle.h"
 #include "ui/gfx/swap_result.h"
 #include "ui/gl/gl_fence.h"
-#include "ui/gl/gl_image.h"
 #include "ui/gl/gl_surface.h"
 
 namespace {
