9669cb2b82102527f8f816ec94b3888b5f19deee
David Bertoni <dbertoni@chromium.org>
2022-07-06 23:18:55 +0000

    [Extensions] Modernize some webRequest API tests.
    This CL modifies some of the webRequest API tests so they eventually
    can be run from service worker-based extensions. This mostly involves
    creating individual extensions for most of the tests and running them
    through the extension's background script.
    
    Bug: 1093066
    Change-Id: Ia8409a5b305a8acd971166f2d4c5dc0bff9b23b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732798
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021428}

---
+++ chrome/test/data/extensions/api_test/webrequest/test_extra_headers_auth/test_extra_headers_auth.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var callbackPass = chrome.test.callbackPass;
+
+const scriptUrl = '_test_resources/api_test/webrequest/framework.js';
+let loadScript = chrome.test.loadScript(scriptUrl);
+
+loadScript.then(async function() {
+  runTests([
+  function testSpecialResponseHeadersVisibleForAuth() {
+    var url = getServerURL('auth-basic?set-cookie-if-challenged');
+    var extraHeadersListener = callbackPass(function(details) {
+      checkHeaders(details.responseHeaders, ['set-cookie'], []);
+    });
+    chrome.webRequest.onAuthRequired.addListener(extraHeadersListener,
+        {urls: [url]}, ['responseHeaders', 'extraHeaders']);
+
+    var standardListener = callbackPass(function(details) {
+      checkHeaders(details.responseHeaders, [], ['set-cookie']);
+    });
+    chrome.webRequest.onAuthRequired.addListener(standardListener,
+        {urls: [url]}, ['responseHeaders']);
+
+    navigateAndWait(url, function() {
+      chrome.webRequest.onAuthRequired.removeListener(extraHeadersListener);
+      chrome.webRequest.onAuthRequired.removeListener(standardListener);
+    });
+  }
+])});
--- chrome/test/data/extensions/api_test/webrequest_public_session/test.js
+++ chrome/test/data/extensions/api_test/webrequest_public_session/test.js
@@ -15,7 +15,12 @@ function getStrippedURL(url) {
   return getServerURL('');
 }
 
-runTests([
+const scriptUrl =
+      '_test_resources/api_test/webrequest_public_session/framework.js';
+let loadScript = chrome.test.loadScript(scriptUrl);
+
+loadScript.then(async function() {
+  runTests([
   // Navigates to a page with a blocking handler that cancels the page request.
   // The page will not load. This tests the following:
   // - listeners can be registered on the webRequest API with empty URL
@@ -108,4 +113,4 @@ runTests([
       ['blocking']);
       navigateAndWait(targetUrl);
   },
-]);
+])});
