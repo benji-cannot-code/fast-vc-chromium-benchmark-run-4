f8640abd1ac151b4e5e865eac0a8b5bddb454299
Maks Orlovich <morlovich@chromium.org>
2022-07-06 19:40:51 +0000

    [Code health] Convert /content/browser/media away from WebUI::RegisterDeprecatedMessageCallback
    ...to WebUI::RegisterMessageCallback. (In some cases some other deprecated
    usages were converted, when fairly local).
    
    This CL was uploaded by git cl split.
    
    Bug: 1243386
    Change-Id: Id6de16511bb0977a2cd5fc77f23f6580079554a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733092
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021314}

--- content/browser/media/media_internals_handler.h
+++ content/browser/media/media_internals_handler.h
@@ -7,11 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_MEDIA_MEDIA_INTERNALS_HANDLER_H_
 
 #include "base/memory/ref_counted.h"
+#include "base/values.h"
 #include "content/public/browser/web_ui_message_handler.h"
-
-namespace base {
-class ListValue;
-}
 
 namespace content {
 class MediaInternalsProxy;
@@ -32,7 +29,7 @@ class MediaInternalsMessageHandler : public WebUIMessageHandler {
   void RegisterMessages() override;
 
   // Javascript message handlers.
-  void OnGetEverything(const base::ListValue* list);
+  void OnGetEverything(const base::Value::List& list);
 
   // MediaInternals message handlers.
   void OnUpdate(const std::u16string& update);
