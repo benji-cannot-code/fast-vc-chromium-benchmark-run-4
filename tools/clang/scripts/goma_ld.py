74aa29000bff6e4e8b91f148284d3e27ded1f121
Takuto Ikuta <tikuta@chromium.org>
2022-07-06 09:27:50 +0000

    tools: drop python2 support from goma_{ld,link}.py
    Bug: b/203606263
    Change-Id: Ie62780488806e1a5554303f43d5ec2837b154493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744965
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Bob Haarman <inglorion@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021111}

--- tools/clang/scripts/goma_ld.py
+++ tools/clang/scripts/goma_ld.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # Copyright (c) 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -10,16 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # E.g. original: clang++ -o foo foo.o
 # Becomes: goma-ld clang++ -o foo foo.o
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-import goma_link
-
 import os
 import re
 import sys
+
+import goma_link
 
 
 class GomaLinkUnix(goma_link.GomaLinkBase):
