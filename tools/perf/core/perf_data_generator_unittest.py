4af70117119d4e756503ac07f0946d8c710258de
Wenbin Zhang <wenbinzhang@google.com>
2022-07-07 23:49:10 +0000

    [perf] resolve deprecated-method in pylint 2.7
    Bug: chromium:1340675
    Change-Id: Ia40ebb046044147110f7b1c700921ac715f54cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750745
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021928}

--- tools/perf/core/perf_data_generator_unittest.py
+++ tools/perf/core/perf_data_generator_unittest.py
@@ -27,7 +27,7 @@ from core.perf_data_generator import BenchmarkMetadata
 class PerfDataGeneratorTest(unittest.TestCase):
   def setUp(self):
     # Test config can be big, so set maxDiff to None to see the full comparision
-    # diff when assertEquals fails.
+    # diff when assertEqual fails.
     self.maxDiff = None
 
   def test_get_scheduled_non_telemetry_benchmarks(self):
@@ -95,8 +95,8 @@ class TestIsPerfBenchmarksSchedulingValid(unittest.TestCase):
     valid = perf_data_generator.is_perf_benchmarks_scheduling_valid(
         'dummy', self.test_stream)
 
-    self.assertEquals(self.test_stream.getvalue(), '')
-    self.assertEquals(valid, True)
+    self.assertEqual(self.test_stream.getvalue(), '')
+    self.assertEqual(valid, True)
 
   def test_UnscheduledCppBenchmarks(self):
     self.get_non_telemetry_benchmarks.return_value = {'honda'}
@@ -109,7 +109,7 @@ class TestIsPerfBenchmarksSchedulingValid(unittest.TestCase):
     valid = perf_data_generator.is_perf_benchmarks_scheduling_valid(
         'dummy', self.test_stream)
 
-    self.assertEquals(valid, False)
+    self.assertEqual(valid, False)
     self.assertIn('Benchmark toyota is tracked but not scheduled',
         self.test_stream.getvalue())
 
@@ -123,7 +123,7 @@ class TestIsPerfBenchmarksSchedulingValid(unittest.TestCase):
     valid = perf_data_generator.is_perf_benchmarks_scheduling_valid(
         'dummy', self.test_stream)
 
-    self.assertEquals(valid, False)
+    self.assertEqual(valid, False)
     self.assertIn(
         'Benchmark tesla is scheduled on perf waterfall but not tracked',
         self.test_stream.getvalue())
