ad66629a20184310eb3c0c109dd91bf97ef5763f
Florian Jacky <fjacky@chromium.org>
2022-07-07 18:15:03 +0000

    Extract queue request logic from PermissionRequestManager into separate class
    Bug: 1312333
    Change-Id: Ie86afac56d2a6e1aa5bc0adf985754159daaeb2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721040
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021768}

--- components/permissions/permission_request_manager.cc
+++ components/permissions/permission_request_manager.cc
@@ -11,22 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/containers/circular_deque.h"
-#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/observer_list.h"
 #include "base/stl_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
-#include "build/build_config.h"
 #include "components/autofill_assistant/browser/public/runtime_manager.h"
 #include "components/back_forward_cache/back_forward_cache_disable.h"
 #include "components/permissions/features.h"
 #include "components/permissions/permission_decision_auto_blocker.h"
 #include "components/permissions/permission_prompt.h"
 #include "components/permissions/permission_request.h"
-#include "components/permissions/permission_request_id.h"
 #include "components/permissions/permissions_client.h"
 #include "components/permissions/request_type.h"
 #include "components/permissions/switches.h"
@@ -77,10 +73,10 @@ constexpr char kAbusiveNotificationContentWarningMessage[] =
 
 namespace {
 
-// When there are multiple permissions requests in `queued_requests_`, we try to
-// reorder them based on the acceptance rates. Notifications and Geolocations
-// have one of the lowest acceptance, hence they have the lowest priority and
-// will be shown the last.
+// When there are multiple permissions requests in
+// `pending_permission_requests_`, we try to prioritize them based on the
+// acceptance rates. Notifications and Geolocations have one of the lowest
+// acceptance, hence they have the lowest priority and will be shown the last.
 bool IsLowPriorityRequest(PermissionRequest* request) {
   return request->request_type() == RequestType::kNotifications ||
          request->request_type() == RequestType::kGeolocation;
@@ -141,7 +137,7 @@ bool PermissionRequestManager::PermissionRequestSource::
 PermissionRequestManager::~PermissionRequestManager() {
   DCHECK(!IsRequestInProgress());
   DCHECK(duplicate_requests_.empty());
-  DCHECK(queued_requests_.empty());
+  DCHECK(pending_permission_requests_.IsEmpty());
 }
 
 void PermissionRequestManager::AddRequest(
@@ -326,7 +322,7 @@ PermissionRequestManager::GetCurrentRequestFateInFaceOfNewRequest(
 void PermissionRequestManager::QueueRequest(
     content::RenderFrameHost* source_frame,
     PermissionRequest* request) {
-  PushQueuedRequest(request);
+  pending_permission_requests_.Push(request);
   request_sources_map_.emplace(
       request, PermissionRequestSource({source_frame->GetProcess()->GetID(),
                                         source_frame->GetRoutingID()}));
@@ -335,7 +331,7 @@ void PermissionRequestManager::QueueRequest(
 void PermissionRequestManager::PreemptAndRequeueCurrentRequest() {
   ResetViewStateForCurrentRequest();
   for (auto* current_request : requests_) {
-    PushQueuedRequest(current_request);
+    pending_permission_requests_.Push(current_request);
   }
   requests_.clear();
 }
@@ -378,12 +374,11 @@ void PermissionRequestManager::DidFinishNavigation(
     return;
   }
 
-  if (!queued_requests_.empty() || IsRequestInProgress()) {
-    // |queued_requests_| and |requests_| will be deleted below, which
-    // might be a problem for back-forward cache — the page might be restored
-    // later, but the requests won't be.
-    // Disable bfcache here if we have any requests here to prevent this
-    // from happening.
+  if (!pending_permission_requests_.IsEmpty() || IsRequestInProgress()) {
+    // |pending_permission_requests_| and |requests_| will be deleted below,
+    // which might be a problem for back-forward cache — the page might be
+    // restored later, but the requests won't be. Disable bfcache here if we
+    // have any requests here to prevent this from happening.
     content::BackForwardCache::DisableForRenderFrameHost(
         navigation_handle->GetPreviousRenderFrameHostId(),
         back_forward_cache::DisabledReason(
@@ -616,8 +611,8 @@ void PermissionRequestManager::DequeueRequestIfNeeded() {
   }
 
   // Find first valid request.
-  while (!queued_requests_.empty()) {
-    PermissionRequest* next = PopNextQueuedRequest();
+  while (!pending_permission_requests_.IsEmpty()) {
+    PermissionRequest* next = pending_permission_requests_.Pop();
     PermissionRequestSource& source = request_sources_map_.find(next)->second;
     if (!source.IsSourceFrameInactiveAndDisallowActivation()) {
       requests_.push_back(next);
@@ -633,8 +628,9 @@ void PermissionRequestManager::DequeueRequestIfNeeded() {
   }
 
   // Find additional requests that can be grouped with the first one.
-  for (; !queued_requests_.empty(); PopNextQueuedRequest()) {
-    PermissionRequest* front = PeekNextQueuedRequest();
+  for (; !pending_permission_requests_.IsEmpty();
+       pending_permission_requests_.Pop()) {
+    PermissionRequest* front = pending_permission_requests_.Peek();
     PermissionRequestSource& source = request_sources_map_.find(front)->second;
     if (source.IsSourceFrameInactiveAndDisallowActivation()) {
       front->Cancelled();
@@ -848,12 +844,13 @@ void PermissionRequestManager::FinalizeCurrentRequests(
 }
 
 void PermissionRequestManager::CleanUpRequests() {
-  for (auto* queued_request : queued_requests_) {
-    CancelledIncludingDuplicates(queued_request);
-    RequestFinishedIncludingDuplicates(queued_request);
-    request_sources_map_.erase(request_sources_map_.find(queued_request));
+  for (; !pending_permission_requests_.IsEmpty();
+       pending_permission_requests_.Pop()) {
+    auto* pending_request = pending_permission_requests_.Peek();
+    CancelledIncludingDuplicates(pending_request);
+    RequestFinishedIncludingDuplicates(pending_request);
+    request_sources_map_.erase(request_sources_map_.find(pending_request));
   }
-  queued_requests_.clear();
 
   if (IsRequestInProgress()) {
     std::vector<PermissionRequest*>::iterator requests_iter;
@@ -875,19 +872,15 @@ PermissionRequest* PermissionRequestManager::GetExistingRequest(
     if (request->IsDuplicateOf(existing_request))
       return existing_request;
   }
-  for (PermissionRequest* queued_request : queued_requests_) {
-    if (request->IsDuplicateOf(queued_request))
-      return queued_request;
-  }
-  return nullptr;
+  return pending_permission_requests_.FindDuplicate(request);
 }
 
 void PermissionRequestManager::PermissionGrantedIncludingDuplicates(
     PermissionRequest* request,
     bool is_one_time) {
-  DCHECK_EQ(1, base::STLCount(requests_, request) +
-                   base::STLCount(queued_requests_, request))
-      << "Only requests in [queued_[frame_]]requests_ can have duplicates";
+  DCHECK_EQ(1ul, base::STLCount(requests_, request) +
+                     pending_permission_requests_.Count(request))
+      << "Only requests in [pending_permission_]requests_ can have duplicates";
   request->PermissionGranted(is_one_time);
   auto range = duplicate_requests_.equal_range(request);
   for (auto it = range.first; it != range.second; ++it)
@@ -896,9 +889,9 @@ void PermissionRequestManager::PermissionGrantedIncludingDuplicates(
 
 void PermissionRequestManager::PermissionDeniedIncludingDuplicates(
     PermissionRequest* request) {
-  DCHECK_EQ(1, base::STLCount(requests_, request) +
-                   base::STLCount(queued_requests_, request))
-      << "Only requests in [queued_]requests_ can have duplicates";
+  DCHECK_EQ(1ul, base::STLCount(requests_, request) +
+                     pending_permission_requests_.Count(request))
+      << "Only requests in [pending_permission_]requests_ can have duplicates";
   request->PermissionDenied();
   auto range = duplicate_requests_.equal_range(request);
   for (auto it = range.first; it != range.second; ++it)
@@ -907,9 +900,9 @@ void PermissionRequestManager::PermissionDeniedIncludingDuplicates(
 
 void PermissionRequestManager::CancelledIncludingDuplicates(
     PermissionRequest* request) {
-  DCHECK_EQ(1, base::STLCount(requests_, request) +
-                   base::STLCount(queued_requests_, request))
-      << "Only requests in [queued_]requests_ can have duplicates";
+  DCHECK_EQ(1ul, base::STLCount(requests_, request) +
+                     pending_permission_requests_.Count(request))
+      << "Only requests in [pending_permission_]requests_ can have duplicates";
   request->Cancelled();
   auto range = duplicate_requests_.equal_range(request);
   for (auto it = range.first; it != range.second; ++it)
@@ -918,9 +911,9 @@ void PermissionRequestManager::CancelledIncludingDuplicates(
 
 void PermissionRequestManager::RequestFinishedIncludingDuplicates(
     PermissionRequest* request) {
-  DCHECK_EQ(1, base::STLCount(requests_, request) +
-                   base::STLCount(queued_requests_, request))
-      << "Only requests in [queued_]requests_ can have duplicates";
+  DCHECK_EQ(1ul, base::STLCount(requests_, request) +
+                     pending_permission_requests_.Count(request))
+      << "Only requests in [pending_permission_]requests_ can have duplicates";
   request->RequestFinished();
   // Beyond this point, |request| has probably been deleted.
   auto range = duplicate_requests_.equal_range(request);
@@ -1091,30 +1084,6 @@ void PermissionRequestManager::DoAutoResponseForTesting() {
       break;
     case NONE:
       NOTREACHED();
-  }
-}
-
-PermissionRequest* PermissionRequestManager::PeekNextQueuedRequest() {
-  return base::FeatureList::IsEnabled(features::kPermissionChip)
-             ? queued_requests_.back()
-             : queued_requests_.front();
-}
-
-PermissionRequest* PermissionRequestManager::PopNextQueuedRequest() {
-  PermissionRequest* next = PeekNextQueuedRequest();
-  if (base::FeatureList::IsEnabled(features::kPermissionChip))
-    queued_requests_.pop_back();
-  else
-    queued_requests_.pop_front();
-  return next;
-}
-
-void PermissionRequestManager::PushQueuedRequest(PermissionRequest* request) {
-  if (base::FeatureList::IsEnabled(features::kPermissionQuietChip) &&
-      !base::FeatureList::IsEnabled(features::kPermissionChip)) {
-    queued_requests_.push_front(request);
-  } else {
-    queued_requests_.push_back(request);
   }
 }
 
