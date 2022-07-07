82011fe4625df37d1dc319c6354dd9d9d97a5684
Tushar Agarwal <agarwaltushar@google.com>
2022-07-07 07:57:53 +0000

    Use explicit deferred installation for the stack unwinder module.
    With `installDeferred`, the module will be completely, asynchronously
    installed at some point in the future (e.g. sleep, app restart, etc.)
    
    With `install`, the module is installed in "splitcompat mode"
    immediately, and "properly" (i.e. completely) installed at some later
    point in the future.
    
    Therefore, I think `installDeferred` better represents the intentions
    behind `StackUnwinderModuleProvider.installModule`.
    
    From what I understand, "splitcompat mode" can also result in the
    browser process thinking the module is installed, but the child
    processes thinking it is not.
    
    Change-Id: I07c2fc36e679f49bfd0054cab2ce61282e7c2487
    Bug: 1326430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749619
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021561}

--- chrome/android/modules/stack_unwinder/provider/java/src/org/chromium/chrome/modules/stack_unwinder/StackUnwinderModuleProvider.java
+++ chrome/android/modules/stack_unwinder/provider/java/src/org/chromium/chrome/modules/stack_unwinder/StackUnwinderModuleProvider.java
@@ -22,7 +22,7 @@ public class StackUnwinderModuleProvider {
      */
     @CalledByNative
     public static void installModule() {
-        StackUnwinderModule.install((boolean success) -> {});
+        StackUnwinderModule.installDeferred();
     }
 
     /**
