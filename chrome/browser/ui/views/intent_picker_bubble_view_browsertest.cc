667313eb79a40729e32f78a3672b9d319617829c
Tim Sergeant <tsergeant@chromium.org>
2022-07-07 01:21:24 +0000

    Intents: Change intent picker persistence text to "Remember this choice"
    "Remember this choice" was suggested by our UXW instead of "Remember my
    choice", since we don't normally use first-person in UI.
    
    Bug: 1321501
    Change-Id: I675fe7f2bd94cc9414f72bb25c377820b510783b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3742640
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021470}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -448,6 +448,7 @@ class IntentPickerDialogTest : public DialogBrowserTest {
 #define MAYBE_InvokeUi_default InvokeUi_default
 #endif
 IN_PROC_BROWSER_TEST_F(IntentPickerDialogTest, MAYBE_InvokeUi_default) {
+  set_baseline("3742640");
   ShowAndVerifyUi();
 }
 
@@ -482,6 +483,6 @@ class IntentPickerDialogGridViewTest : public IntentPickerDialogTest {
 };
 
 IN_PROC_BROWSER_TEST_F(IntentPickerDialogGridViewTest, InvokeUi_default) {
-  set_baseline("3652664");
+  set_baseline("3742640");
   ShowAndVerifyUi();
 }
