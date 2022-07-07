f59e24a46ab304f67af8e895602936c0add02b17
Artem Sumaneev <asumaneev@google.com>
2022-07-07 10:14:35 +0000

    [Enrollment] Remove Enterprise.AutoEnrollmentExtraTime metric.
    The metric has expired and is never tracked. Remove the metric and the
    code recording it.
    
    Bug: chromium:1294843
    Test: unit_tests --gtest_filter="*AutoEnrollmentClientImplTest*"
    Test: unit_tests --gtest_filter="*AutoEnrollmentClientImplFREToInitialEnrollmentTest*"
    Test: unit_tests --gtest_filter="*PsmHelperInitialEnrollmentTest*"
    Test: manual on device
    Change-Id: I2d5c04d1022d856f222d22cf599b40ab37530f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3676865
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021596}

--- components/policy/core/common/cloud/enterprise_metrics.cc
+++ components/policy/core/common/cloud/enterprise_metrics.cc
@@ -141,7 +141,6 @@ const char kUMAHashDanceProtocolTime[] =
     "Enterprise.AutoEnrollmentProtocolTime";
 const char kUMAHashDanceBucketDownloadTime[] =
     "Enterprise.AutoEnrollmentBucketDownloadTime";
-const char kUMAHashDanceExtraTime[] = "Enterprise.AutoEnrollmentExtraTime";
 const char kUMAHashDanceRequestStatus[] =
     "Enterprise.AutoEnrollmentRequestStatus";
 const char kUMAHashDanceNetworkErrorCode[] =
