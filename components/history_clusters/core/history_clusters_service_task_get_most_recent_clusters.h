884ef927d05451a86c0b055c88745cfc814a8e00
manukh <manukh@chromium.org>
2022-07-07 22:34:42 +0000

    [memories][persist-clusters] Return persisted clusters.
    Currently `HistoryClustersServiceTaskGetMostRecentClusters` clusters
    unclustered visits (possibly in combination with clustered visits) and
    returns the newly created clusters.
    
    When it exhausts unclustered visits, it stops returning more clusters.
    
    With this CL, when it exhausts unclustered visits, it begins returning
    the persisted clusters.
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I870e02aaffb2b48775fd21193453d867fa69892a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3723226
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021887}

--- components/history_clusters/core/history_clusters_service_task_get_most_recent_clusters.h
+++ components/history_clusters/core/history_clusters_service_task_get_most_recent_clusters.h
@@ -11,15 +11,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
-#include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/clustering_backend.h"
 #include "components/history_clusters/core/history_clusters_types.h"
+
+namespace history {
+class HistoryService;
+}
 
 namespace history_clusters {
 
 class HistoryClustersService;
 
+// `HistoryClustersServiceTaskGetMostRecentClusters` 1st gets newly generated
+// clusters from the clustering backend using unclustered visits from the
+// history backend. Then, once the unclustered visits are exhausted, it switches
+// to getting persisted clusters from the history backend.
+// It is an extension of `HistoryClustersService`; rather than pollute the
+// latter's namespace with a bunch of callbacks, this class groups those
+// callbacks.
 class HistoryClustersServiceTaskGetMostRecentClusters {
  public:
   HistoryClustersServiceTaskGetMostRecentClusters(
@@ -36,6 +46,19 @@ class HistoryClustersServiceTaskGetMostRecentClusters {
   bool Done() { return done_; }
 
  private:
+  // When there remain unclustered visits, cluster unclustered visits (possibly
+  // in combination with clustered visits) and return the newly created
+  // clusters:
+  //   Start() ->
+  //   OnGotAnnotatedVisitsToCluster() ->
+  //   OnGotModelClusters()
+  // But when unclustered visits are or were exhausted, return persisted
+  // clusters:
+  //   Start() ->
+  //   [optional] OnGotAnnotatedVisitsToCluster() ->
+  //   ReturnMostRecentPersistedClusters() ->
+  //   OnGotMostRecentPersistedClusters()
+
   // Invoked during construction. Will asyncly request annotated visits from
   // `GetAnnotatedVisitsToCluster`.
   void Start();
@@ -49,9 +72,17 @@ class HistoryClustersServiceTaskGetMostRecentClusters {
       QueryClustersContinuationParams continuation_params);
 
   // Invoked after `OnGotAnnotatedVisitsToCluster()` asyncly obtains clusters.
-  // Will synchronously invoke `callback_`.
+  // Will syncly invoke `callback_`.
   void OnGotModelClusters(QueryClustersContinuationParams continuation_params,
                           std::vector<history::Cluster> clusters);
+
+  // Invoked syncly when there are no unclustered visits to cluster. Will
+  // asyncly request existing (i.e. persisted) clusters from `HistoryService`.
+  void ReturnMostRecentPersistedClusters(base::Time exclusive_max_time);
+
+  // Invoked after `ReturnMostRecentPersistedClusters()` asyncly fetches
+  // clusters. Will syncly invoke `callback_`.
+  void OnGotMostRecentPersistedClusters(std::vector<history::Cluster> clusters);
 
   // Never nullptr.
   base::WeakPtr<HistoryClustersService> weak_history_clusters_service_;
