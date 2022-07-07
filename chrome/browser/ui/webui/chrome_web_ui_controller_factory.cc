4ce321bff8d8e5696d14c063c7106fe4b69b535c
Osama Fathy <osamafathy@google.com>
2022-07-07 04:44:48 +0000

    OOBE: Prevent OOBE WebUI creation inside the session
    Bug: 1337304
    Change-Id: I4cfb2e3965161d0ceee8db71a31f1794d7e5a4d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733222
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021519}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -228,6 +228,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/os_feedback/chrome_os_feedback_delegate.h"
 #include "chrome/browser/ash/printing/print_management/printing_manager.h"
 #include "chrome/browser/ash/printing/print_management/printing_manager_factory.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ash/scanning/chrome_scanning_app_delegate.h"
 #include "chrome/browser/ash/scanning/scan_service.h"
 #include "chrome/browser/ash/scanning/scan_service_factory.h"
@@ -282,8 +283,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/nearby_internals/nearby_internals_ui.h"
 #include "chrome/browser/ui/webui/nearby_share/nearby_share_dialog_ui.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_ui.h"
+#include "chrome/common/chrome_switches.h"
 #include "chromeos/services/network_health/public/mojom/network_diagnostics.mojom.h"  // nogncheck
 #include "chromeos/services/network_health/public/mojom/network_health.mojom.h"  // nogncheck
+#include "content/public/common/content_switches.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #endif
 
@@ -992,8 +995,18 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::multidevice_setup::MultiDeviceSetupDialogUI>;
   if (url.host_piece() == chrome::kChromeUINetworkHost)
     return &NewWebUI<chromeos::NetworkUI>;
-  if (url.host_piece() == chrome::kChromeUIOobeHost)
-    return &NewWebUI<chromeos::OobeUI>;
+  if (url.host_piece() == chrome::kChromeUIOobeHost) {
+    // TODO(crbug.com/1329058): Eliminate chrome://oobe/login and fix OOBE tests
+    // running inside the session.
+    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+    bool is_running_test = command_line->HasSwitch(::switches::kTestName) ||
+                           command_line->HasSwitch(::switches::kTestType);
+    if (ash::ProfileHelper::IsSigninProfile(profile) ||
+        (url.path() == "/login" && is_running_test)) {
+      return &NewWebUI<chromeos::OobeUI>;
+    }
+    return nullptr;
+  }
   if (url.host_piece() == chrome::kChromeUIOSSettingsHost)
     return &NewWebUI<chromeos::settings::OSSettingsUI>;
   if (url.host_piece() == chrome::kChromeUIPowerHost)
