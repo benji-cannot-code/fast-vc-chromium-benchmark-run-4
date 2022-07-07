4387467f3d13729b0c42604f1338c65437a0f664
Tushar Agarwal <agarwaltushar@google.com>
2022-07-07 19:52:00 +0000

    Update `perf_benchmark_with_profiling_unittest.py` to account for changes in https://crrev.com/c/3745582.
    Also, manually roll catapult into chromium, in lieu of
    https://crrev.com/c/3750195.
    
    Change-Id: I67b310b72d84a4629de8518bf1ba6f5bf314cc95
    Bug: chromium:1338581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749263
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021811}

--- tools/perf/core/perf_benchmark_with_profiling_unittest.py
+++ tools/perf/core/perf_benchmark_with_profiling_unittest.py
@@ -112,7 +112,7 @@ class PerfBenchmarkWithProfilingTest(unittest.TestCase):
           fill_policy: DISCARD
         }
         buffers {
-          size_kb: 2048
+          size_kb: 190464
         }
 
         buffers {
@@ -123,6 +123,8 @@ class PerfBenchmarkWithProfilingTest(unittest.TestCase):
             name: "linux.process_stats"
             target_buffer: 1
             process_stats_config {
+                scan_all_processes_on_start: true
+                record_thread_names: true
                 proc_stats_poll_ms: 100
             }
           }
