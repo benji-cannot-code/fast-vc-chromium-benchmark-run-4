85bd1e10bef4b86088e77d98cbe797748530b718
Wenbin Zhang <wenbinzhang@google.com>
2022-07-07 23:35:50 +0000

    [perf] resolve arguments-differ in pylint 2.7
    Some changes are in catapult for simplicity.
    https://chromium-review.googlesource.com/c/catapult/+/3751890
    
    bug: chromium:1340675
    Change-Id: I3e7c0ae3aa912c754b84808dce2384b05fd33b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751375
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021921}

--- tools/perf/contrib/vr_benchmarks/shared_vr_page_state.py
+++ tools/perf/contrib/vr_benchmarks/shared_vr_page_state.py
@@ -163,8 +163,8 @@ class AndroidSharedVrPageState(_SharedVrPageState):
                      'gvr-android-sdk', 'test-apks', 'vr_keyboard',
                      'vr_keyboard_current.apk'))
 
-  def WillRunStory(self, page):
-    super(AndroidSharedVrPageState, self).WillRunStory(page)
+  def WillRunStory(self, story):
+    super(AndroidSharedVrPageState, self).WillRunStory(story)
     if not self._finder_options.disable_screen_reset:
       self._CycleScreen()
 
@@ -231,8 +231,8 @@ class WindowsSharedVrPageState(_SharedVrPageState):
                 self._finder_options, self._possible_browser)
     self._desktop_runtime.Setup()
 
-  def WillRunStory(self, page):
-    super(WindowsSharedVrPageState, self).WillRunStory(page)
+  def WillRunStory(self, story):
+    super(WindowsSharedVrPageState, self).WillRunStory(story)
     self._desktop_runtime.WillRunStory()
 
   def TearDownState(self):
--- tools/perf/page_sets/idle_platform.py
+++ tools/perf/page_sets/idle_platform.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import time
 
 from telemetry.page import shared_page_state
-from telemetry import story
+from telemetry import story as story_module
 
 
 class _IdleSharedState(shared_page_state.SharedPageState):
@@ -14,8 +14,8 @@ class _IdleSharedState(shared_page_state.SharedPageState):
         test, finder_options, story_set, possible_browser)
     self._current_story = None
 
-  def WillRunStory(self, current_story):
-    self._current_story = current_story
+  def WillRunStory(self, story):
+    self._current_story = story
     assert self.platform.tracing_controller.is_tracing_running
 
   def RunStory(self, _):
@@ -25,7 +25,7 @@ class _IdleSharedState(shared_page_state.SharedPageState):
     self._current_story = None
 
 
-class _IdleStory(story.Story):
+class _IdleStory(story_module.Story):
   def __init__(self, name, duration):
     super(_IdleStory, self).__init__(
         shared_state_class=_IdleSharedState, name=name)
@@ -44,7 +44,7 @@ class _IdleStory(story.Story):
     return self._url
 
 
-class IdleStorySet(story.StorySet):
+class IdleStorySet(story_module.StorySet):
   def __init__(self):
     super(IdleStorySet, self).__init__()
     self.AddStory(_IdleStory('IdleStory_10s', 10))
