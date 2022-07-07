346ffbf88c4745b14fc915749e44c578af1ceebf
Jan Keitel <jkeitel@google.com>
2022-07-07 19:25:22 +0000

    [APC Desktop] Refresh password scripts cache on password section load.
    This CL ensures that the password scripts cache is refreshed (if it is
    stale) when a user entered the passwords section in settings.
    For now, the cache load is gated behind a (disabled) feature.
    
    Bug: 1340073
    Change-Id: Ic746354e94987350baf8c2e834bc66bb43570e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3743307
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021797}

--- chrome/test/data/webui/settings/passwords_section_test.ts
+++ chrome/test/data/webui/settings/passwords_section_test.ts
@@ -275,6 +275,7 @@ suite('PasswordsSection', function() {
     PasswordManagerImpl.setInstance(passwordManager);
     elementFactory = new PasswordSectionElementFactory(document);
     loadTimeData.overrideValues({
+      enableAutomaticPasswordChangeInSettings: false,
       enablePasswordViewPage: false,
       unifiedPasswordManagerEnabled: false,
     });
@@ -1800,6 +1801,14 @@ suite('PasswordsSection', function() {
     const interaction =
         await testHatsBrowserProxy.whenCalled('trustSafetyInteractionOccurred');
     assertEquals(TrustSafetyInteraction.OPENED_PASSWORD_MANAGER, interaction);
+  });
+
+  test('passwordScriptsRefreshedOnOpen', async function() {
+    loadTimeData.overrideValues(
+        {enableAutomaticPasswordChangeInSettings: true});
+    elementFactory.createPasswordsSection(passwordManager, [], []);
+    Router.getInstance().navigateTo(routes.PASSWORDS);
+    await passwordManager.whenCalled('refreshScriptsIfNecessary');
   });
 
   test(
