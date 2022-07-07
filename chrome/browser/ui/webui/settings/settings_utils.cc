0a73d050539a8c907de999cf1f4214cb8ed61bff
Antonio Sartori <antoniosartori@chromium.org>
2022-07-07 05:09:25 +0000

    Settings: Remove unused ResolveFontList,MaybeGetLocalizedFontName helpers.
    These functions are not used anywhere.
    
    Change-Id: Id77be6c83bf2198dccd4824b24514914cc560ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745529
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021524}

--- chrome/browser/ui/webui/settings/settings_utils.cc
+++ chrome/browser/ui/webui/settings/settings_utils.cc
@@ -5,13 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/settings/settings_utils.h"
 
-#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/url_formatter/url_fixer.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/gfx/font_list.h"
 #include "url/gurl.h"
 
 namespace settings_utils {
@@ -36,17 +34,5 @@ base::RefCountedMemory* GetPrivacySandboxFaviconResourceBytes(
   return ui::ResourceBundle::GetSharedInstance().LoadDataResourceBytesForScale(
       IDR_FLAGS_FAVICON, scale_factor);
 }
-
-std::string ResolveFontList(const std::string& font_name_or_list) {
-  if (!font_name_or_list.empty() && font_name_or_list[0] == ',')
-    return gfx::FontList::FirstAvailableOrFirst(font_name_or_list);
-  return font_name_or_list;
-}
-
-#if !BUILDFLAG(IS_WIN)
-std::string MaybeGetLocalizedFontName(const std::string& font_name_or_list) {
-  return ResolveFontList(font_name_or_list);
-}
-#endif
 
 }  // namespace settings_utils
