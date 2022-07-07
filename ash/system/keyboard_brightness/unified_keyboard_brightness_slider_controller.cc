6a5dd2183df7b1d33c533d381eccd007cae37ac4
Jason Thai <jasontt@chromium.org>
2022-07-07 19:58:39 +0000

    personalization: update rgbkbd icon color on brightness slider
    Update the icon color based on the darkness of the button background
    color.
    
    BUG=b/237600006
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I85d45835f24d52ce5ab2ef15a3609f44d67eac02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737350
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021818}

--- ash/system/keyboard_brightness/unified_keyboard_brightness_slider_controller.cc
+++ ash/system/keyboard_brightness/unified_keyboard_brightness_slider_controller.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/color_utils.h"
 
 namespace ash {
 
@@ -91,8 +92,12 @@ class UnifiedKeyboardBrightnessView : public UnifiedSliderView,
           rb.GetImageSkiaNamed(IDR_SETTINGS_RGB_KEYBOARD_RAINBOW_COLOR_48_PNG);
       button->SetBackgroundImage(*image);
     } else {
-      button->SetBackgroundColor(
-          ConvertBacklightColorToIconBackgroundColor(backlight_color));
+      SkColor color =
+          ConvertBacklightColorToIconBackgroundColor(backlight_color);
+      button->SetBackgroundColor(color);
+      button->SetIconColor(color_utils::GetLuma(color) < 125
+                               ? gfx::kGoogleGrey200
+                               : gfx::kGoogleGrey900);
     }
     button->SetBorder(views::CreateRoundedRectBorder(
         /*thickness=*/4, /*corner_radius=*/16,
