b111ddf5141f9d8e3753400137b2dd9311ee0993
Ali Tofigh <alito@chromium.org>
2022-07-07 12:23:22 +0000

    Add absl::string_view version of overridden webrtc functions
    WebRTC is adopting absl::string_view in its codebase. As part of this
    large scale change, code that calls WebRTC functions or overrides
    WebRTC member functions may need to use absl::string_view.
    
    This CL adds absl::string_view version of
    webrtc::RtcEventLogOutput::Write() and to subclasses along with other
    required changes. Once the adoption in WebRTC has come further along,
    the std::string version will be removed.
    
    Bug: 1337249
    Change-Id: Ic39a3e1c44ff8faaaa7f07b2e7bad49f58eef89e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749916
    Commit-Queue: Ali Tofigh <alito@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021631}

--- third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.h"
 
 #include "base/check.h"
+#include "third_party/abseil-cpp/absl/strings/string_view.h"
 #include "third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -24,6 +25,10 @@ bool RtcEventLogOutputSinkProxy::IsActive() const {
 }
 
 bool RtcEventLogOutputSinkProxy::Write(const std::string& output) {
+  return Write(absl::string_view(output));
+}
+
+bool RtcEventLogOutputSinkProxy::Write(absl::string_view output) {
   WTF::Vector<uint8_t> converted_output;
   converted_output.AppendRange(output.begin(), output.end());
 
