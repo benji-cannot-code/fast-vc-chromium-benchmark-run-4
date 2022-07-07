1d8680a9e9f1bb9114b2de27d115804bb9bd01f2
Corentin Wallez <cwallez@chromium.org>
2022-07-07 20:03:54 +0000

    Fix accesses to other GC objects in the GPUDevice destructor.
    Use a PreFinalizer instead.
    
    Bug: chromium:1342155
    Change-Id: I252c314101595a23c0725f5ecead80b979bff49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751162
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021820}

--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -61,6 +61,7 @@ class GPUDevice final : public EventTargetWithInlineData,
                         public ExecutionContextClient,
                         public DawnObject<WGPUDevice> {
   DEFINE_WRAPPERTYPEINFO();
+  USING_PRE_FINALIZER(GPUDevice, Dispose);
 
  public:
   explicit GPUDevice(ExecutionContext* execution_context,
@@ -152,6 +153,9 @@ class GPUDevice final : public EventTargetWithInlineData,
  private:
   using LostProperty =
       ScriptPromiseProperty<Member<GPUDeviceLostInfo>, ToV8UndefinedGenerator>;
+
+  // Used by USING_PRE_FINALIZER.
+  void Dispose();
 
   void DestroyAllExternalTextures();
 
