a304222826f52a8c368f3f793c18aaec484b1114
Justin Cohen <justincohen@google.com>
2022-07-07 20:42:49 +0000

    Revert "Remove tab helper setup out of the BVC"
    This reverts commit 269a1115563d511c52f446621f5bb616bb0752ed.
    
    Reason for revert: Breaks NTP animation and NTP active flag.
    
    Original change's description:
    > Remove tab helper setup out of the BVC
    >
    > – Removed tab helper setup out of the BVC
    > – Removed the unused related code
    >
    > Bug: 1290819
    > Change-Id: Ia921a2487bf4ce8d7d190a8f8f89af28cb0fb126
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739511
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1020528}
    
    Bug: 1290819, 1342648, 1342615
    Change-Id: I075bd96f0f026b56e012ef352bea0428c77b6704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3748513
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021839}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/follow/follow_tab_helper.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/ntp/features.h"
-#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/prerender/preload_controller_delegate.h"
 #import "ios/chrome/browser/prerender/prerender_service.h"
 #import "ios/chrome/browser/prerender/prerender_service_factory.h"
@@ -1652,11 +1651,6 @@ constexpr base::TimeDelta kLegacyFullscreenControllerToolbarAnimationDuration =
     CaptivePortalTabHelper::FromWebState(webState)->SetTabInsertionBrowserAgent(
         insertionAgent);
   }
-
-  if (NewTabPageTabHelper::FromWebState(webState)) {
-    NewTabPageTabHelper::FromWebState(webState)->SetDelegate(
-        self.viewController);
-  }
 }
 
 // Uninstalls delegates for `webState`.
@@ -1684,10 +1678,6 @@ constexpr base::TimeDelta kLegacyFullscreenControllerToolbarAnimationDuration =
   if (CaptivePortalTabHelper::FromWebState(webState)) {
     CaptivePortalTabHelper::FromWebState(webState)->SetTabInsertionBrowserAgent(
         nil);
-  }
-
-  if (NewTabPageTabHelper::FromWebState(webState)) {
-    NewTabPageTabHelper::FromWebState(webState)->SetDelegate(nil);
   }
 }
 
