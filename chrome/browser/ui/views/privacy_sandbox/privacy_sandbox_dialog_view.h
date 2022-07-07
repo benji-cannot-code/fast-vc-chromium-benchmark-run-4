a08cb94de0cad16a301e4a1cb486ee84405a113d
Olesia Marukhno <olesiamarukhno@google.com>
2022-07-07 10:53:13 +0000

    [privacy sandbox settings] Add tests for expanded consent dialog
    Add pixel test for expanded consent dialog. To expand the dialog,
    execute JavaScript in the webcontents of the dialog. Also, add WebUI
    test to check the scroll position of the content area after expanding
    and collapsing the section.
    
    Bug: 1336640, 1336642
    Change-Id: I6b8ef9c657e583b5894e5f1c43155d74dabaaa5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745371
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1021606}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h
@@ -25,6 +25,8 @@ class PrivacySandboxDialogView : public views::View {
   PrivacySandboxDialogView(Browser* browser,
                            PrivacySandboxService::PromptType dialog_type);
 
+  static constexpr int kViewIdWebView = 1;
+
   void Close();
 
  private:
