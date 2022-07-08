81e813ba2019c01eec6d38e162bcce171e47b8a9
rudranshd <rudranshd@google.com>
2022-07-08 19:33:36 +0000

    [CrOS WiFi] Always mark new WiFi networks not hidden
    Updated network configuration file to mark newly configured networks as
    not hidden by default. Test written to verify functionality.
    
    Bug: b/236148589
    Test: [1] Test for hidden network initialization by running JS test
    Change-Id: I3db690392e6153b1d38baac61491ad791b29580a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724015
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Rudransh Dikshit <rudranshd@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022265}

--- chrome/test/data/webui/chromeos/fake_network_config_mojom.js
+++ chrome/test/data/webui/chromeos/fake_network_config_mojom.js
@@ -37,6 +37,11 @@ export class FakeNetworkConfig {
      */
     this.managedProperties_ = new Map();
 
+    /**
+     * @private {!chromeos.networkConfig.mojom.ConfigProperties|undefined}
+     */
+    this.propertiesToSet_ = undefined;
+
     /** @private {!chromeos.networkConfig.mojom.GlobalPolicy|undefined} */
     this.globalPolicy_ = undefined;
 
@@ -128,6 +133,7 @@ export class FakeNetworkConfig {
     this.networkStates_ = [eth0];
 
     this.managedProperties_ = new Map();
+    this.propertiesToSet_ = undefined;
 
     this.vpnProviders_ = [];
 
@@ -337,6 +343,9 @@ export class FakeNetworkConfig {
    */
   configureNetwork(properties, shared) {
     return new Promise(resolve => {
+      this.propertiesToSet_ =
+          /** @type(!chromeos.networkConfig.mojom.ConfigProperties)*/
+          (Object.assign({}, properties));
       this.methodCalled('configureNetwork');
       resolve({guid: 'test_guid', errorMessage: ''});
     });
@@ -349,6 +358,9 @@ export class FakeNetworkConfig {
    */
   setProperties(guid, properties) {
     return new Promise(resolve => {
+      this.propertiesToSet_ =
+          /** @type(!chromeos.networkConfig.mojom.ConfigProperties)*/ (
+              Object.assign({}, properties));
       this.methodCalled('setProperties');
       resolve({success: true, errorMessage: ''});
     });
@@ -370,6 +382,11 @@ export class FakeNetworkConfig {
    */
   getDeviceStateForTest(type) {
     return this.deviceStates_.get(type) || null;
+  }
+
+  /** @return {!chromeos.networkConfig.mojom.ConfigProperties|undefined} */
+  getPropertiesToSetForTest() {
+    return this.propertiesToSet_;
   }
 
   /** @param {!Array<!chromeos.networkConfig.mojom.VpnProvider>} providers */
