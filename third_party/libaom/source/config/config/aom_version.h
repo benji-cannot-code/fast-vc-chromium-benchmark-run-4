085dab6ae95e9a96d316bbd275bb35aa5c137591
Wan-Teh Chang <wtc@google.com>
2022-07-08 19:00:15 +0000

    Roll src/third_party/libaom/source/libaom/ 7ace1184f..8dcdafc6d (7 commits)
    https://aomedia.googlesource.com/aom.git/+log/7ace1184f110..8dcdafc6d4a2
    
    $ git log 7ace1184f..8dcdafc6d --date=short --no-merges --format='%ad %ae %s'
    2022-06-30 chiyotsai RTC: Fill in missing enums for adaptive_rd_thresh speed feature
    2022-06-24 chiyotsai RTC: Allow mode entropy cost update on some frames
    2022-07-01 jingning Properly set the frame qp through ducky_encode
    2022-07-01 jingning Capture display order through ducky_encode
    2022-07-01 jayasanker.j row_mt dec: Delay top-right sync when intraBC is enabled
    2022-06-30 jingning Properly capture the frame size and PSNR
    2022-06-22 jayasanker.j row_mt enc: Delay top-right sync when intraBC is enabled
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@chromium.org
    
    Change-Id: Id8c5c29d5b388e81daa2e40169b94558cd405c2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752548
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022244}

--- third_party/libaom/source/config/config/aom_version.h
+++ third_party/libaom/source/config/config/aom_version.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 4
 #define VERSION_PATCH 0
-#define VERSION_EXTRA "138-g7ace1184f"
+#define VERSION_EXTRA "145-g8dcdafc6d"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "3.4.0-138-g7ace1184f"
-#define VERSION_STRING " 3.4.0-138-g7ace1184f"
+#define VERSION_STRING_NOSP "3.4.0-145-g8dcdafc6d"
+#define VERSION_STRING " 3.4.0-145-g8dcdafc6d"
