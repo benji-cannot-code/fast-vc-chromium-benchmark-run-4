4f760aef56b50630f94e586bc1ca196c000da160
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-08 22:46:12 +0000

    [rust] Add `memoffset` and `static_assertions` to `//third_party/rust`.
    Motivation: Rust code generated by Crubit's `rs_bindings_from_cc`
    depends on these 2 crates.
    
    Audit of the `memoffset` crate:
    https://docs.google.com/document/d/1v9SjdDwE00pRIfQ6A6gdD-WrD-lXSiniQXGVbc-cirg/edit?usp=sharing
    
    Audit of the `static_assertions` crate:
    https://docs.google.com/document/d/1p4X5bOliH77djacPu9as8H7_4mxdMkYo1eryN5aYyvM/edit?usp=sharing
    
    Bug: 1329611
    Change-Id: I5820c269ce25eefc105b46030e98b3455719a12b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736562
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022335}

---
+++ third_party/rust/memoffset/v0_6/crate/ci/miri.sh
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+set -ex
+
+# Install Miri.
+MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
+echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+rustup default "$MIRI_NIGHTLY"
+rustup component add miri
+
+# Run tests.
+cargo miri test
+cargo miri test --all-features
+
+# Restore old state in case Travis uses this cache for other jobs.
+rustup default nightly
