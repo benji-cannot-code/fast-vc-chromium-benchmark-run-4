36a0026295aa5f557e40415083a141ef0450202e
Jan Keitel <jkeitel@google.com>
2022-07-08 14:14:41 +0000

    [APC Desktop] Add a new user consent proto.
    This is the first CL in a series to log consent for Autofill
    Assistant on Desktop in ARI.
    
    Bug: 1322387
    Change-Id: Ica87fde6d20cf4458aed967d8db9733f8e8911e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749620
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022140}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1094,6 +1094,7 @@ VISIT_PROTO_FIELDS(const sync_pb::UserConsentSpecifics& proto) {
   VISIT(arc_play_terms_of_service_consent);
   VISIT(assistant_activity_control_consent);
   VISIT(account_passwords_consent);
+  VISIT(autofill_assistant_consent);
 }
 
 VISIT_PROTO_FIELDS(
@@ -1137,6 +1138,13 @@ VISIT_PROTO_FIELDS(const sync_pb::UserConsentTypes::UnifiedConsent& proto) {
 
 VISIT_PROTO_FIELDS(
     const sync_pb::UserConsentTypes::AccountPasswordsConsent& proto) {
+  VISIT_REP(description_grd_ids);
+  VISIT(confirmation_grd_id);
+  VISIT_ENUM(status);
+}
+
+VISIT_PROTO_FIELDS(
+    const sync_pb::UserConsentTypes::AutofillAssistantConsent& proto) {
   VISIT_REP(description_grd_ids);
   VISIT(confirmation_grd_id);
   VISIT_ENUM(status);
