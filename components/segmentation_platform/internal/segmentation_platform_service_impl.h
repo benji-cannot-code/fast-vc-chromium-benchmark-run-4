01dce45e683693c5ca9fa3c0548a2021eda5bfa9
Shakti Sahu <shaktisahu@chromium.org>
2022-07-08 07:06:24 +0000

    Cache platform API calls to run after initialization
    This CL adds a caching layer to cache the method calls with their
    inputs to the segmentation platform. These methods will be invoked
    after DB initialization is complete.
    
    Bug: 1342422
    Change-Id: I6a1c485df0c7e48005f6a51fdaedc6471f87bd0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751951
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022032}

--- components/segmentation_platform/internal/segmentation_platform_service_impl.h
+++ components/segmentation_platform/internal/segmentation_platform_service_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/containers/circular_deque.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/gtest_prod_util.h"
@@ -183,6 +184,9 @@ class SegmentationPlatformServiceImpl : public SegmentationPlatformService {
   // For metrics only:
   const base::Time creation_time_;
   base::Time init_time_;
+
+  // For caching any method calls that were received before initialization.
+  base::circular_deque<base::OnceClosure> pending_actions_;
 
   base::WeakPtrFactory<SegmentationPlatformServiceImpl> weak_ptr_factory_{this};
 };
