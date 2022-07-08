5b05367f0110c313b5977ae935c664a193d1abe5
Viktor Semeniuk <vsemeniuk@google.com>
2022-07-08 10:35:14 +0000

    Replacing EditSavedPasswords with EditSavedCredentials
    This CL simplifies the SavedPasswordsPresenter by unifying
    EditSavedCredentials and EditSavedPasswords. EditSavedCredentials now
    returns an enum to reflect the result of the operation. Also some refactoring is done by extracting logic into helper functions to make code more readable.
    
    Bug: 1184691, 1339145
    Change-Id: Ic86cd0f9330ed2af0acf5a4608fee70b856b7a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3720652
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022088}

--- components/password_manager/core/browser/ui/insecure_credentials_manager.cc
+++ components/password_manager/core/browser/ui/insecure_credentials_manager.cc
@@ -289,7 +289,8 @@ bool InsecureCredentialsManager::MuteCredential(
       password_issue.second.is_muted = IsMuted(true);
     }
   }
-  return presenter_->EditSavedCredentials(updated_credential);
+  return presenter_->EditSavedCredentials(updated_credential) ==
+         SavedPasswordsPresenter::EditResult::kSuccess;
 }
 
 bool InsecureCredentialsManager::UnmuteCredential(
@@ -301,7 +302,8 @@ bool InsecureCredentialsManager::UnmuteCredential(
       password_issue.second.is_muted = IsMuted(false);
     }
   }
-  return presenter_->EditSavedCredentials(updated_credential);
+  return presenter_->EditSavedCredentials(updated_credential) ==
+         SavedPasswordsPresenter::EditResult::kSuccess;
 }
 
 bool InsecureCredentialsManager::UpdateCredential(
