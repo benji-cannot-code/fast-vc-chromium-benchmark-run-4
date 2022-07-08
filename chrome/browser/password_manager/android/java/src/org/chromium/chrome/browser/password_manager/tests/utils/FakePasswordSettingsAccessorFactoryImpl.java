a58fa4cc815c91752b6be783e442719089f9a9fa
Ivana Žužić <izuzic@google.com>
2022-07-08 08:07:46 +0000

    [PMIntegrationTests] Add PasswordSettingsAccessor fakes for tests
    The fakes have been added for writing Password Manager integration tests
    that will be created in follow-up CLs.
    
    Bug: 1336641
    Change-Id: Iced3102269719283fd77c0414b624590614b7444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726190
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022051}

---
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/tests/utils/FakePasswordSettingsAccessorFactoryImpl.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager.tests.utils;
+
+import org.chromium.chrome.browser.password_manager.PasswordSettingsAccessor;
+import org.chromium.chrome.browser.password_manager.PasswordSettingsAccessorFactory;
+
+/**
+ * The factory for creating fake {@link PasswordSettingsAccessor} to be used in integration
+ * tests.
+ */
+public class FakePasswordSettingsAccessorFactoryImpl extends PasswordSettingsAccessorFactory {
+    /**
+     * Returns the fake implementation of {@link PasswordSettingsAccessor} used for tests.
+     */
+    @Override
+    public PasswordSettingsAccessor createAccessor() {
+        return new FakePasswordSettingsAccessor();
+    }
+
+    @Override
+    public boolean canCreateAccessor() {
+        return true;
+    }
+}
---
+++ chrome/browser/password_manager/android/junit/src/org/chromium/chrome/browser/password_manager/FakePasswordSettingsAccessorFactoryImplTest.java
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Batch;
+import org.chromium.chrome.browser.password_manager.tests.utils.FakePasswordSettingsAccessor;
+import org.chromium.chrome.browser.password_manager.tests.utils.FakePasswordSettingsAccessorFactoryImpl;
+
+/** Tests for the methods of {@link FakePasswordSettingsAccessorFactoryImpl}. */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+@Batch(Batch.PER_CLASS)
+public class FakePasswordSettingsAccessorFactoryImplTest {
+    FakePasswordSettingsAccessorFactoryImpl mFakePasswordSettingsAccessorFactoryImpl;
+    @Before
+    public void setUp() {
+        mFakePasswordSettingsAccessorFactoryImpl = new FakePasswordSettingsAccessorFactoryImpl();
+    }
+
+    @Test
+    public void testCreateAccessor() {
+        assertTrue(mFakePasswordSettingsAccessorFactoryImpl.createAccessor()
+                           instanceof FakePasswordSettingsAccessor);
+    }
+
+    @Test
+    public void testCanCreateAccessor() {
+        assertTrue(mFakePasswordSettingsAccessorFactoryImpl.canCreateAccessor());
+    }
+}
