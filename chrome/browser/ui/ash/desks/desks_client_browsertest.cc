07405e35916721b64f5aab5fa5488e1ab9ba5eaa
Nancy Wang <nancylingwang@chromium.org>
2022-07-08 23:54:14 +0000

    Use the non mojom LaunchContainer for the desk template.
    Mojom in AppService will be removed. So use the non mojom
    LaunchContainer for the desk template.
    
    This CL is pure refactor, and no function change.
    
    BUG=1253250
    
    Change-Id: I983dd24108f399348eea4e65d96e7e490ac79450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752205
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022352}

--- chrome/browser/ui/ash/desks/desks_client_browsertest.cc
+++ chrome/browser/ui/ash/desks/desks_client_browsertest.cc
@@ -661,9 +661,8 @@ IN_PROC_BROWSER_TEST_F(DesksTemplatesClientTest,
   auto app_restore_data_iter2 = iter2->second.find(settings_window_id);
   ASSERT_TRUE(app_restore_data_iter2 != iter2->second.end());
   const auto& data2 = app_restore_data_iter2->second;
-  EXPECT_EQ(
-      static_cast<int>(apps::mojom::LaunchContainer::kLaunchContainerWindow),
-      data2->container.value());
+  EXPECT_EQ(static_cast<int>(apps::LaunchContainer::kLaunchContainerWindow),
+            data2->container.value());
   EXPECT_EQ(static_cast<int>(WindowOpenDisposition::NEW_WINDOW),
             data2->disposition.value());
   // Verify window info are correctly captured.
@@ -1860,9 +1859,8 @@ IN_PROC_BROWSER_TEST_F(DesksTemplatesClientTest,
   auto app_restore_data_iter2 = iter2->second.find(settings_window_id);
   ASSERT_NE(iter->second.end(), app_restore_data_iter2);
   const auto& data2 = app_restore_data_iter2->second;
-  EXPECT_EQ(
-      static_cast<int>(apps::mojom::LaunchContainer::kLaunchContainerWindow),
-      data2->container.value());
+  EXPECT_EQ(static_cast<int>(apps::LaunchContainer::kLaunchContainerWindow),
+            data2->container.value());
   EXPECT_EQ(static_cast<int>(WindowOpenDisposition::NEW_WINDOW),
             data2->disposition.value());
   // Verify window info are correctly captured.
