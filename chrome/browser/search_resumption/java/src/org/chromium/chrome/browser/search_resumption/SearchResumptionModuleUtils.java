3f7231dec59e019d2a7ef435d606b309e3d35506
Xi Han <hanxi@google.com>
2022-07-08 18:06:52 +0000

    [SearchResumption] Fix crash when creating a NTP without any Tab.
    The crash was caused due to the Tab to track is null.
    
    Bug: 1342969
    Change-Id: I7aa6fff4a4c3efbb329607e08f0296817eca6a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749763
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022229}

--- chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleUtils.java
+++ chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleUtils.java
@@ -54,6 +54,8 @@ public class SearchResumptionModuleUtils {
         if (!shouldShowSearchResumptionModule(profile, currentTab)) return null;
 
         Tab tabToTrack = TabModelUtils.getMostRecentTab(tabModel, currentTab.getId());
+        if (tabToTrack == null) return null;
+
         if (!isTabToTrackValid(tabToTrack)) return null;
 
         return new SearchResumptionModuleCoordinator(
