78387785237d81871b88aa86bad247085a7b8117
Abdias Dagbekpo <adagbekpo@google.com>
2022-07-08 16:10:00 +0000

    Added fake heap handle to Task class in base/task/threadpool. We plan on having an intrusive heap of tasks (delayed) so having a heap handle in the Task class is necessary
    Bug: 1127430
    Change-Id: I0e21236298ae190f906e556756f813b522686f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751611
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Abdias Dagbekpo <adagbekpo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022172}

--- base/task/thread_pool/task.h
+++ base/task/thread_pool/task.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/callback.h"
+#include "base/containers/intrusive_heap.h"
 #include "base/location.h"
 #include "base/pending_task.h"
 #include "base/task/sequenced_task_runner.h"
@@ -49,6 +50,17 @@ struct BASE_EXPORT Task : public PendingTask {
   ~Task() = default;
 
   Task& operator=(Task&& other);
+
+  void SetScheduled();
+
+  // Required by IntrusiveHeap.
+  void SetHeapHandle(const HeapHandle& handle) {}
+
+  // Required by IntrusiveHeap.
+  void ClearHeapHandle() {}
+
+  // Required by IntrusiveHeap.
+  HeapHandle GetHeapHandle() const { return HeapHandle::Invalid(); }
 };
 
 }  // namespace internal
