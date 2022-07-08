8425ec0e6bf96ff95ac5f4246157c1b8609a5161
Jenny Zhang <jennyz@chromium.org>
2022-07-08 19:05:04 +0000

    Fix the issue causing quickoffice extensions not published.
    Quickoffice extensions are special in the way that they
    don't use file browser handler to register their handlers
    for MS office files, like all other extensions do. Instead,
    they use file handlers to register the MS Office files
    handlers like the chrome platform apps do. So we need to do
    special case handling for quickoffice extensions in the related
    areas.
    
    Bug: 1341381
    Change-Id: I76b8fab18214131e77a949303b3c1368da4228de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751677
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022248}

--- chrome/browser/lacros/for_which_extension_type.cc
+++ chrome/browser/lacros/for_which_extension_type.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "chrome/browser/lacros/lacros_extensions_util.h"
 #include "chrome/common/extensions/api/file_browser_handlers/file_browser_handler.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 
 /******** ForWhichExtensionType ********/
@@ -26,8 +27,16 @@ bool ForWhichExtensionType::Matches(
     return lacros_extensions_util::IsExtensionApp(extension);
 
   if (extension->is_extension()) {
-    // This is for non-chrome-app extensions. We should only publish them if
-    // they have file handlers.
+    // QuickOffice extensions do not use file browser handler manifest key
+    // to register their handlers for MS Office files; instead, they use
+    // file_handler manifest key(like the way chrome apps do). We should
+    // always publish quickoffice extensions since they are the default handlers
+    // for MS Office files.
+    if (extension_misc::IsQuickOfficeExtension(extension->id()))
+      return true;
+
+    // For the regular extensions, we should only publish them if they have file
+    // handlers registered using file browser handlers.
     FileBrowserHandler::List* handler_list =
         FileBrowserHandler::GetHandlers(extension);
     return handler_list;
