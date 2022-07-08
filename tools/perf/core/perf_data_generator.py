044dce2d795a3e3ac1aa7b5be41d805cc574f214
Ryan Heise <heiserya@google.com>
2022-07-08 18:28:51 +0000

    lacros-x86-perf: Change grabbiter to octopus
    Bug: 1159092
    Change-Id: I4159bf1c5e35f3a5ca4cf3628b68ca9817acd3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751878
    Auto-Submit: Ryan Heise <heiserya@google.com>
    Commit-Queue: Ryan Heise <heiserya@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022239}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -1370,7 +1370,7 @@ BUILDERS = {
             'gpu': None,
             'os': 'ChromeOS',
             'device_status': 'available',
-            'device_type': 'grabbiter',
+            'device_type': 'octopus',
         },
     },
 }
