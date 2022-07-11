3b34a3f78376f40432b4f31fe21d9deb097ffc48
Dale Rogerson <dalerogerson@google.com>
2022-07-11 18:41:28 +0000

    Removing feature::kNtlmV2Enabled flag
    featureList::IsEnabled() and other methods are not valid until
    SetInstance is called. We are going to add a DCHECK to enforce this
    condition. See this change:
    https://chromium-review.googlesource.com/c/chromium/src/+/3727250
    
    However, FeatureList::IsEnabled(features::kNtlmV2Enabled) is called
    before SetInstance has been called. This will cause the DCHECK above to fire.
    Since this feature flag is no longer needed, this change removes it.
    
    
    Bug: 1123504, 22532
    Change-Id: I426c227fdd7daf8ac49d6de2691d0dfd26b64550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724348
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Dale Rogerson <dalerogerson@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022804}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -494,10 +494,6 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kNotificationDurationLongForRequireInteraction;
 #endif
 
-#if BUILDFLAG(IS_POSIX)
-COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kNtlmV2Enabled;
-#endif
-
 #if !BUILDFLAG(IS_ANDROID)
 COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kOnConnectNative;
 #endif
