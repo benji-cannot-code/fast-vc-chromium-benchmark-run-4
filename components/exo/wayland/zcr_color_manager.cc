8037dace32f953214e102a0518f16dd26c80e5ee
Kramer Ge <fangzhoug@chromium.org>
2022-07-11 20:54:57 +0000

    Reland "[Exo]Unbind data for other wl_resources when ~WaylandDisplayHandler"
    This is a reland of commit 3d14005f36c1e8469cfb85d0d71c62449cc4f5ac
    SendLogicalPositionAndSize test is not destroying xdg_output_resource_
    properly, which causes UAF.
    
    Original change's description:
    > [Exo]Unbind data for other wl_resources when ~WaylandDisplayHandler
    >
    > WaylandDisplayHandler is destroyed when wl_output_release() is
    > requested, but it is also the bound data object for xdg_output and
    > color_management_output. Their data should be set to nullptr to
    > prevent further access.
    >
    > Bug: 1304483
    > Change-Id: Ic1f395c856e8a0d81c2610f9ef4407f90f62d89c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747043
    > Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1022264}
    
    Bug: 1304483, 1343271
    Change-Id: Ib5eecaac780392ff4ae60decff5b898d01c7e1f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749167
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022877}

--- components/exo/wayland/zcr_color_manager.cc
+++ components/exo/wayland/zcr_color_manager.cc
@@ -189,6 +189,8 @@ void color_management_output_get_color_space(
     uint32_t id) {
   auto* display_handler =
       GetUserDataAs<WaylandDisplayHandler>(color_management_output_resource);
+  if (!display_handler)
+    return;
 
   display::Display display;
   bool exists = display::Screen::GetScreen()->GetDisplayWithDisplayId(
@@ -218,7 +220,8 @@ void color_management_output_destroy(
     struct wl_resource* color_management_output_resource) {
   auto* display_handler =
       GetUserDataAs<WaylandDisplayHandler>(color_management_output_resource);
-  display_handler->UnsetColorManagementOutputResource();
+  if (display_handler)
+    display_handler->UnsetColorManagementOutputResource();
   wl_resource_destroy(color_management_output_resource);
 }
 
@@ -436,6 +439,9 @@ void color_manager_get_color_management_output(
       client, &zcr_color_management_output_v1_interface, 1, id);
 
   auto* display_handler = GetUserDataAs<WaylandDisplayHandler>(output);
+  if (!display_handler)
+    return;
+
   wl_resource_set_implementation(color_management_output_resource,
                                  &color_management_output_v1_implementation,
                                  display_handler, nullptr);
