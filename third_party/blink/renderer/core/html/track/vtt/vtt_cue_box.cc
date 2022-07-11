ce01a08361c4298b9772a0ec70b7b9905c16454a
Kent Tamura <tkent@chromium.org>
2022-07-11 08:42:16 +0000

    VTTCue NG: Attach LayoutNGBlockFlows to TextTrackContainer and VTTCueBox
    ... if LayoutNGVTTCue flag is enabled.
    
    The mediaControls.css change is not user-visible.
    
    Bug: 1335309
    Change-Id: I58dc0df8369f123a014ca8c780d7c41b16185335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752786
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022613}

--- third_party/blink/renderer/core/html/track/vtt/vtt_cue_box.cc
+++ third_party/blink/renderer/core/html/track/vtt/vtt_cue_box.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/track/vtt/vtt_cue.h"
+#include "third_party/blink/renderer/core/layout/layout_object_factory.h"
 #include "third_party/blink/renderer/core/layout/layout_vtt_cue.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 
@@ -125,6 +126,9 @@ LayoutObject* VTTCueBox::CreateLayoutObject(const ComputedStyle& style,
   // have positioning information.
   if (style.GetPosition() == EPosition::kRelative)
     return HTMLDivElement::CreateLayoutObject(style, legacy);
+
+  if (RuntimeEnabledFeatures::LayoutNGVTTCueEnabled())
+    return LayoutObjectFactory::CreateBlockFlow(*this, style, legacy);
 
   UseCounter::Count(GetDocument(), WebFeature::kLegacyLayoutByVTTCue);
   return MakeGarbageCollected<LayoutVTTCue>(this, snap_to_lines_position_);
