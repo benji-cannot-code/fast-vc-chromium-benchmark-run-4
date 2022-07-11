af6e28f248948875e5c7d2a14cd97a619ec0e6c2
Michael Thiessen <mthiesse@chromium.org>
2022-07-11 21:19:33 +0000

    Get full referrer URL from WebContents in ExternalNavigationHandler
    ExternalNavigationHandler for some reason uses the NavigationController
    directly to get the last committed URL. This was added initially in
    https://codereview.chromium.org/2397153003/ which gives no
    justification for why the NavigationController was used rather than the
    last committed URL from the WebContents.
    
    In my testing I couldn't find any cases where the last committed URL
    on the webcontents was different than using the NavigationController.
    Note that we can't just use getReferrer() from the
    ExternalNavigationParams because we need the full path to detect the
    Serp referrer.
    
    Bug: 1194721
    Change-Id: Iba293f98e3b47e0bee8db85a3c51ac700b926acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756361
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022895}

--- components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
@@ -63,8 +63,6 @@ import org.chromium.components.messages.PrimaryActionClickBehavior;
 import org.chromium.components.webapk.lib.client.ChromeWebApkHostSignature;
 import org.chromium.components.webapk.lib.client.WebApkValidator;
 import org.chromium.content_public.browser.LoadUrlParams;
-import org.chromium.content_public.browser.NavigationController;
-import org.chromium.content_public.browser.NavigationEntry;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.common.ContentUrlConstants;
@@ -2269,29 +2267,12 @@ public class ExternalNavigationHandler {
                 && mDelegate.getWindowAndroid().canRequestPermission(permissionNeeded);
     }
 
-    @Nullable
-    // TODO(https://crbug.com/1194721): Investigate whether or not we can use
-    // getLastCommittedUrl() instead of the NavigationController. Or maybe we can just replace this
-    // with ExternalNavigationParams#getReferrerUrl?
-    private GURL getReferrerUrl() {
-        if (!mDelegate.hasValidTab() || mDelegate.getWebContents() == null) return null;
-
-        NavigationController nController = mDelegate.getWebContents().getNavigationController();
-        int index = nController.getLastCommittedEntryIndex();
-        if (index == -1) return null;
-
-        NavigationEntry entry = nController.getEntryAtIndex(index);
-        if (entry == null) return null;
-
-        return entry.getUrl();
-    }
-
     /**
      * @return whether this navigation is from the search results page.
      */
     @VisibleForTesting
     protected boolean isSerpReferrer() {
-        GURL referrerUrl = getReferrerUrl();
+        GURL referrerUrl = getLastCommittedUrl();
         if (referrerUrl == null || referrerUrl.isEmpty()) return false;
 
         return UrlUtilitiesJni.get().isGoogleSearchUrl(referrerUrl.getSpec());
@@ -2302,7 +2283,7 @@ public class ExternalNavigationHandler {
      */
     @VisibleForTesting
     protected boolean isGoogleReferrer() {
-        GURL referrerUrl = getReferrerUrl();
+        GURL referrerUrl = getLastCommittedUrl();
         if (referrerUrl == null || referrerUrl.isEmpty()) return false;
 
         return UrlUtilitiesJni.get().isGoogleSubDomainUrl(referrerUrl.getSpec());
