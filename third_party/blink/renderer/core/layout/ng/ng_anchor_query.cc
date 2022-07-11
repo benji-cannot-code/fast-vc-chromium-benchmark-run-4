941daf0c47cff273b023126a03963be4a1f2e0ec
Koji Ishii <kojii@chromium.org>
2022-07-11 19:49:45 +0000

    [anchor-position] Add an interface for |AnchorSizeValue|
    The `anchor-size()` function uses a different enum type
    |AnchorSizeValue|, so |AnchorEvaluator::Evaluate| can't handle
    it.
    
    This patch:
    * Adds |EvaluateAnchorSize| to the |AnchorEvaluator| class.
    * Renames |Evaluate| to |EvaluateAnchor|.
    * Adds default functions for both so that implementations can
      provide only necessary functions.
    
    Bug: 1309178
    Change-Id: I7fea1e4ddd152b31153b2778acf30d7f71008145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752783
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022843}

--- third_party/blink/renderer/core/layout/ng/ng_anchor_query.cc
+++ third_party/blink/renderer/core/layout/ng/ng_anchor_query.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-absl::optional<LayoutUnit> NGLogicalAnchorQuery::Evaluate(
+absl::optional<LayoutUnit> NGLogicalAnchorQuery::EvaluateAnchor(
     const AtomicString& anchor_name,
     AnchorValue anchor_value,
     LayoutUnit available_size,
