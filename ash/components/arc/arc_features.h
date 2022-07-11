53affd004f1558b9a2d215e130173660efaf946f
Satoshi Niwa <niwa@google.com>
2022-07-11 04:23:34 +0000

    arc: Add "use-lvm" feature param for VirtioBlkForData
    BUG=b:226686468
    TEST=ash_components_unittests
    TEST=arc.PlayStore.vm with the feature flag enabled
    
    Change-Id: I4f90733bd6067bd1f531d0f6b40da47c8d3ab1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3748158
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022571}

--- ash/components/arc/arc_features.h
+++ ash/components/arc/arc_features.h
@@ -26,6 +26,7 @@ extern const base::Feature kEnableUnifiedAudioFocusFeature;
 extern const base::Feature kEnableUnmanagedToManagedTransitionFeature;
 extern const base::Feature kEnableUsap;
 extern const base::Feature kEnableVirtioBlkForData;
+extern const base::FeatureParam<bool> kEnableVirtioBlkForDataUseLvm;
 extern const base::Feature kFilePickerExperimentFeature;
 extern const base::Feature kGameModeFeature;
 extern const base::Feature kGmsCoreLowMemoryKillerProtection;
