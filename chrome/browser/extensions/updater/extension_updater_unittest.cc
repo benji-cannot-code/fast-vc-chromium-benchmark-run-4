2c5a6397e04ee031e1d0b74155d61b840a5d746f
Oleg Davydov <burunduk@chromium.org>
2022-07-11 12:55:38 +0000

    [Extensions] Don't merge fetches into active request
    Merging fetches to the active request requires active request to be
    available (which blocks other changes) and makes unclear when merging is
    allowed and when not.
    
    This CL saves old behavior by merging requests from the queue when we
    finished fetching one. Two tests are added to ensure that there was no
    behavior change.
    
    The TestMultipleManifestDownloading test had to be updated as it relied
    on order stability of the fetches, and the heap used in RequestQueue<>
    doesn't guarantee it.
    
    Bug: b:237409519
    Change-Id: Ia83670d43e2365bd974fafbce945ad9c71f5d0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732768
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022662}

--- chrome/browser/extensions/updater/extension_updater_unittest.cc
+++ chrome/browser/extensions/updater/extension_updater_unittest.cc
@@ -1202,14 +1202,11 @@ class ExtensionUpdaterTest : public testing::Test {
     GURL fetch2_url = fetch2->full_url();
     GURL fetch3_url = fetch3->full_url();
     GURL fetch4_url = fetch4->full_url();
-    helper.StartUpdateCheck(std::move(fetch1));
-    helper.StartUpdateCheck(std::move(fetch2));
-    helper.StartUpdateCheck(std::move(fetch3));
-    helper.StartUpdateCheck(std::move(fetch4));
-    RunUntilIdle();
 
     // fetch1_url
     {
+      helper.StartUpdateCheck(std::move(fetch1));
+      RunUntilIdle();
       helper.test_url_loader_factory().AddResponse(fetch1_url.spec(), "",
                                                    net::HTTP_BAD_REQUEST);
       EXPECT_CALL(
@@ -1228,6 +1225,8 @@ class ExtensionUpdaterTest : public testing::Test {
 
     // fetch2_url
     {
+      helper.StartUpdateCheck(std::move(fetch2));
+      RunUntilIdle();
       const std::string kInvalidXml = "invalid xml";
       helper.test_url_loader_factory().AddResponse(fetch2_url.spec(),
                                                    kInvalidXml, net::HTTP_OK);
@@ -1247,6 +1246,8 @@ class ExtensionUpdaterTest : public testing::Test {
 
     // fetch3_url
     {
+      helper.StartUpdateCheck(std::move(fetch3));
+      RunUntilIdle();
       const std::string kNoUpdate =
           "<?xml version='1.0' encoding='UTF-8'?>"
           "<gupdate xmlns='http://www.google.com/update2/response'"
@@ -1278,6 +1279,8 @@ class ExtensionUpdaterTest : public testing::Test {
 
     // fetch4_url
     {
+      helper.StartUpdateCheck(std::move(fetch4));
+      RunUntilIdle();
       // The last fetcher has an update.
       NotificationsObserver observer;
       const std::string kUpdateAvailable =
