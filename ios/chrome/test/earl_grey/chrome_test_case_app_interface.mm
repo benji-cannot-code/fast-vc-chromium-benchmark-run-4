d5be7157f8a01f053f5eff642c6b978a93bddb22
Jérôme Lebel <jlebel@chromium.org>
2022-07-11 09:12:55 +0000

    [iOS] Add EGTests for 2 step FRE
    Adding:
    - (void)testUMAUncheckedAndCheckItAgain;
    - (void)testWithUMAUncheckedAndSignin;
    - (void)testWithUMACheckedAndSignin;
    - (void)testWithUMACheckedAndSigninAndNoSync;
    - (void)testAdvancedSettingsAndDisableTwoDataTypes;
    - (void)testAdvancedSettingsWithSyncPassphrase;
    
    Bug: 1337718
    Change-Id: I99cb62bf3aca316a0ed4bab32d686b0a5c841e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747787
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022622}

--- ios/chrome/test/earl_grey/chrome_test_case_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_test_case_app_interface.mm
@@ -34,6 +34,7 @@ NSMutableSet* invokedCompletionUUID = nil;
   chrome_test_util::ResetSigninPromoPreferences();
   chrome_test_util::ResetMockAuthentication();
   chrome_test_util::ResetUserApprovedAccountListManager();
+  chrome_test_util::ResetSyncSelectedDataTypes();
 }
 
 + (void)removeInfoBarsAndPresentedStateWithCompletionUUID:
