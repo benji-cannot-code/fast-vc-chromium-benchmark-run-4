3c059e9fe36a1b5e510719e3d3ae44274e71e95d
Sky Malice <skym@chromium.org>
2022-07-11 21:22:16 +0000

    Add wait to SigninFirstRunFragmentTest.
    Bug: 1343519
    Change-Id: I76560f4c67c8aa7b26c5a81e39ed43043140a49b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756186
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022896}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
@@ -709,7 +709,7 @@ public class SigninFirstRunFragmentTest {
         TestThreadUtils.runOnUiThreadBlocking(() -> { mFragment.onNativeInitialized(); });
         launchActivityWithFragment();
 
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
 
         onView(withText(R.string.signin_fre_uma_dialog_title))
                 .inRoot(isDialog())
@@ -735,7 +735,7 @@ public class SigninFirstRunFragmentTest {
     public void testFragmentWhenDismissingUmaDialog() {
         TestThreadUtils.runOnUiThreadBlocking(() -> { mFragment.onNativeInitialized(); });
         launchActivityWithFragment();
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
 
         onView(withText(R.string.done)).perform(click());
 
@@ -747,7 +747,7 @@ public class SigninFirstRunFragmentTest {
     public void testDismissButtonWhenAllowCrashUploadTurnedOff() {
         TestThreadUtils.runOnUiThreadBlocking(() -> { mFragment.onNativeInitialized(); });
         launchActivityWithFragment();
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
         onView(withId(R.id.fre_uma_dialog_switch)).perform(click());
         onView(withText(R.string.done)).perform(click());
 
@@ -762,11 +762,11 @@ public class SigninFirstRunFragmentTest {
     public void testUmaDialogSwitchIsOffWhenAllowCrashUploadWasTurnedOffBefore() {
         TestThreadUtils.runOnUiThreadBlocking(() -> { mFragment.onNativeInitialized(); });
         launchActivityWithFragment();
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
         onView(withId(R.id.fre_uma_dialog_switch)).check(matches(isChecked())).perform(click());
         onView(withText(R.string.done)).perform(click());
 
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
 
         onView(withId(R.id.fre_uma_dialog_switch))
                 .check(matches(not(isChecked())))
@@ -784,7 +784,7 @@ public class SigninFirstRunFragmentTest {
         TestThreadUtils.runOnUiThreadBlocking(() -> { mFragment.onNativeInitialized(); });
         mSigninTestRule.addAccount(TEST_EMAIL1, FULL_NAME1, GIVEN_NAME1, null);
         launchActivityWithFragment();
-        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        clickOnUmaDialogLinkAndWait();
         onView(withId(R.id.fre_uma_dialog_switch)).perform(click());
         onView(withText(R.string.done)).perform(click());
 
@@ -1151,6 +1151,15 @@ public class SigninFirstRunFragmentTest {
             signinProgressSpinner.setIndeterminateDrawable(new ColorDrawable(
                     SemanticColorUtils.getDefaultBgColor(mFragment.getContext())));
         });
+    }
+
+    /**
+     * The dialog does not open instantly, and if we do not wait we get a small percentage of
+     * flakes. See https://crbug.com/1343519.
+     */
+    private void clickOnUmaDialogLinkAndWait() {
+        onView(withId(R.id.signin_fre_footer)).perform(clickOnUmaDialogLink());
+        ViewUtils.onViewWaiting(withText(R.string.done)).check(matches(isDisplayed()));
     }
 
     private ViewAction clickOnUmaDialogLink() {
