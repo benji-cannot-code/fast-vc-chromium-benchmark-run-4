b994a2dd01715f3ff1c3cb0f24070dcc760832dd
Aaron Leventhal <aleventhal@google.com>
2022-07-11 05:04:14 +0000

    Reland: Do not fire events in detached child frames
    Original: CL:3727378
    Revert: CL:3700452
    Reason for revert: crbug.com/1342326,crbug.com/1342425 (sometimes
    load complete events could be dropped, causing failing tests)
    
    Only fire events on nodes that in documents that are attached
    to the root document of the a11y tree. Child trees will begin firing events as soon as all of their parent documents are serialized.
    This prevents a bug in AppKit that causes VoiceOver to get stuck repeating "not responding" messages. The bug goes away as long as
    events are only fired on objects that are attached to the root.
    
    We had the same bug in Views, and it was fixed in CL:3595084.
    One of the bugs listed in the CL description is bug 1221262, which refers to getting stuck saying "busy busy busy", that was the previous message that VoiceOver would speak -- "[appname] is not responding" appears to be the same issue.
    
    Also enforces tree relationship rules via additional CHECKs/DCHECKs.
    
    This CL currently applies only to non-Chrome OS web contents. For it to generalize to other trees not based on BrowserAccessibilityManager including all of Chrome OS, views, and others, we will need to bubble
    the logic up to ui/accessibility (AXTreeManagerBase).
    So, this should be an intermediate state until we can get AXTreeManager in as a base concrete class e.g. CL:3646162.
    
    Follow-ups:
    - Notify Apple of issue and workaround
    - Consider not generating events when CanFireEvents() returns false.
      See CL:3726240
    - Look into bfcache/focus check failure. See TODO in
      BrowserAccessibilityManager::GetLastFocusedNode()
    - Do not use/allow null delegates in BrowserAccessibilityManager (currently a workaround for unit tests): crbug.com/1340004
    - Explore whether Mac a11y still needs separate concept of ignored nodes: crbug.com/1339975
    - Clarify child tree serialization rules (CL:3727378)
    - Look into whether order of iframe serialization can be enforced to be parents first. See CL:3731609.
    - Try to remove generated load complete events as they are redundant
      with Blink load complete events (crbug.com/1342649)
    - Move logic to AXTreeManagerBase
    
    Bug: 1311759
    Test: https://github.com/password_reset
    Change-Id: I4337e731632427c11f776d3a351cd674ae655501
    AX-RelNotes: potential fix for issue where VoiceOver gets stuck saying "Chrome is not responding" repeatedly.
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750239
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022578}

--- content/browser/accessibility/cross_platform_accessibility_browsertest.cc
+++ content/browser/accessibility/cross_platform_accessibility_browsertest.cc
@@ -1961,8 +1961,7 @@ IN_PROC_BROWSER_TEST_F(CrossPlatformAccessibilityBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(CrossPlatformAccessibilityBrowserTest,
-                       // TODO(crbug.com/1342326): Re-enable this test
-                       DISABLED_NavigateInIframe) {
+                       NavigateInIframe) {
   LoadInitialAccessibilityTreeFromHtmlFilePath(
       "/accessibility/regression/iframe-navigation.html");
   WaitForAccessibilityTreeToContainNodeWithName(shell()->web_contents(),
@@ -1976,9 +1975,9 @@ IN_PROC_BROWSER_TEST_F(CrossPlatformAccessibilityBrowserTest,
 
   // Press enter to activate the link, wait for the second iframe to load.
   {
-    AccessibilityNotificationWaiter waiter(
-        shell()->web_contents(), ui::kAXModeComplete,
-        ui::AXEventGenerator::Event::LOAD_COMPLETE);
+    AccessibilityNotificationWaiter waiter(shell()->web_contents(),
+                                           ui::kAXModeComplete,
+                                           ax::mojom::Event::kLoadComplete);
     SimulateKeyPress(shell()->web_contents(), ui::DomKey::ENTER,
                      ui::DomCode::ENTER, ui::VKEY_RETURN, false, false, false,
                      false);
