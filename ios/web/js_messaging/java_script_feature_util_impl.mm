587138ff48a5b757800c120f6f49c0016b0b1788
Daniel White <danieltwhite@google.com>
2022-07-11 15:54:02 +0000

    Converted gCrWeb to TypeScript
    To prepare for the next phase of JavaScript to TypeScript file
    conversions, gCrWeb was converted to TypeScript and exported.
    
    Bug: 1325547
    Change-Id: Ie230041ba5ec0bbe7a4b85a912cba3ab129e18fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738564
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022718}

--- ios/web/js_messaging/java_script_feature_util_impl.mm
+++ ios/web/js_messaging/java_script_feature_util_impl.mm
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 namespace {
 
-const char kBaseScriptName[] = "base";
+const char kBaseScriptName[] = "gcrweb";
 const char kCommonScriptName[] = "common";
 const char kMessageScriptName[] = "message";
 const char kPluginPlaceholderScriptName[] = "plugin_placeholder";
