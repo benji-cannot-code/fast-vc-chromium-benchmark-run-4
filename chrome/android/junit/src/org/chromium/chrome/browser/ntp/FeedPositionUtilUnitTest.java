b4c1e31ad5a07b4809f85dea395a9e863ffc069a
spdonghao <spdonghao@chromium.org>
2022-07-11 20:53:52 +0000

    Remove setLibrariesLoadedForNativeTests() in FeedPositionUtilUnitTest.
    Bug: 1329288, 1343385
    Change-Id: Ibb02decae1eb71df368842f964fab0240314146d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751630
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022875}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/FeedPositionUtilUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/FeedPositionUtilUnitTest.java
@@ -24,7 +24,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureList.TestValues;
-import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -51,9 +50,6 @@ public class FeedPositionUtilUnitTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        LibraryLoader.getInstance().setLibrariesLoadedForNativeTests();
-        Assert.assertTrue(LibraryLoader.getInstance().isInitialized());
-
         Profile.setLastUsedProfileForTesting(mProfile);
         SegmentationPlatformServiceFactory.setForTests(mSegmentationPlatformService);
         when(mSegmentationPlatformService.getCachedSegmentResult(FEED_USER_SEGMENT_KEY))
