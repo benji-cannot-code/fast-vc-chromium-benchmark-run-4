601901898e35725fc756cb9abb614fc58edba218
Viktor Semeniuk <vsemeniuk@google.com>
2022-07-11 08:04:54 +0000

    [WebUI] Updating Password Settings UI to expect no duplicates
    Since deduplication happens in C++ PasswordUiEntry and ExceptionEntry
    can be updated to reflect this. It will allow to remove
    multi_store_id_handler.ts
    multi_store_exception_entry.ts
    multi_store_password_ui_entry.ts
    merge_passwords_store_copies_mixin.ts and
    merge_exceptions_store_copies_mixin.ts in the follow up CLs.
    
    Bug: 1201643
    Change-Id: I1fd991bf1c480ec3418b599172cd11bc795d63b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702305
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1022604}

--- third_party/closure_compiler/externs/passwords_private.js
+++ third_party/closure_compiler/externs/passwords_private.js
@@ -82,8 +82,7 @@ chrome.passwordsPrivate.UrlCollection;
  *   username: string,
  *   federationText: (string|undefined),
  *   id: number,
- *   frontendId: number,
- *   fromAccountStore: boolean,
+ *   storedIn: !chrome.passwordsPrivate.PasswordStoreSet,
  *   passwordNote: string
  * }}
  */
@@ -92,9 +91,7 @@ chrome.passwordsPrivate.PasswordUiEntry;
 /**
  * @typedef {{
  *   urls: !chrome.passwordsPrivate.UrlCollection,
- *   id: number,
- *   frontendId: number,
- *   fromAccountStore: boolean
+ *   id: number
  * }}
  */
 chrome.passwordsPrivate.ExceptionEntry;
