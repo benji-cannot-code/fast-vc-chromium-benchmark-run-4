6f4ba777dbd52b1a699a0f9c2e3dc2a642664ff1
Bruno Pitrus <brunopitrus@hotmail.com>
2022-07-11 19:20:46 +0000

    Do not make assumptions about Abseil ABI.
    When building Chromium against system abseil-cpp on linux distros,
    absl::uint128 and absl::Status are typedefs for other types,
    not structs. This causes linker errors.
    
    See “You may not forward declare Abseil APIs” here:
    https://abseil.io/about/compatibility
    
    Change-Id: Ia28e77495a69366f9fa44de0a82c9397651302b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753696
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022826}

--- third_party/maldoca/src/maldoca/service/common/process_doc_wrapper.h
+++ third_party/maldoca/src/maldoca/service/common/process_doc_wrapper.h
@@ -9,10 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "maldoca/service/proto/doc_type.pb.h"
 #include "maldoca/service/proto/maldoca_service.pb.h"
 #include "maldoca/service/proto/processing_config.pb.h"
-
-namespace absl {
-class Status;
-}  // namespace absl
+#include "third_party/abseil-cpp/absl/status/status.h"
 
 namespace third_party_maldoca {
 
