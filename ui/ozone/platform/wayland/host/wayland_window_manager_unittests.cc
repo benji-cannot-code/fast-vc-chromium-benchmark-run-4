eb6196c27c131a995ecef259f2d1b890e66e12c6
Stephen McGruer <smcgruer@chromium.org>
2022-07-12 15:35:53 +0000

    Revert "Reland "Make sure to dispatch an event to the target only.""
    This reverts commit 720153925dc21ea25f6db9e8f39736b84ccc2366.
    
    Reason for revert: Broke build - https://ci.chromium.org/p/chromium/builders/ci/lacros-amd64-generic-rel/29597?
    
    Original change's description:
    > Reland "Make sure to dispatch an event to the target only."
    >
    > This reverts commit eb0a2d9796ac66ff6712a4ce40b90236a36ed25f.
    >
    > Reason for revert: fix the build (in PS2)
    >
    > Original change's description:
    > > Revert "Make sure to dispatch an event to the target only."
    > >
    > > This reverts commit b6f23c59957c3b8cf6ec747e5c5d4b249ee9c133.
    > >
    > > Reason for revert:
    > > Findit (https://goo.gl/kROfz5) identified this CL at revision b6f23c59957c3b8cf6ec747e5c5d4b249ee9c133 as
    > > the culprit for failures in the continuous build including:
    > >
    > > Sample Failed Build: https://ci.chromium.org/b/8808865077173955809
    > > Sample Failed Step: compile
    > >
    > > If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fb6f23c59957c3b8cf6ec747e5c5d4b249ee9c133&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+b6f23c59957c3b8cf6ec747e5c5d4b249ee9c133
    > >
    > > Original change's description:
    > > > Make sure to dispatch an event to the target only.
    > > >
    > > > The focus may be changed during dispatching and the current impl
    > > > may dispatch the event twice. To address the issue, this CL contains
    > > > the following changes:
    > > >
    > > > 1) WaylandWindow::CanDispatchEvent accepts only if the event is
    > > > targeted to itself, instead of using has_pointer_focus,
    > > > has_keyboard_focus variable, which may be updated during the dispatch.
    > > > 2) Replace has_pointer_focus, has_keyboard_focus with the pointer to
    > > > the focused windows in WaylandWindowManager.
    > > >
    > > > I think has_touch_focus can also be removed, but I'll look into it
    > > > separately.
    > > >
    > > >
    > > > Bug: 1339082
    > > > Test: Covered by unittest. (WaylandWindowTest.ChangeFocusDuringDispatch)
    > > >
    > > > Change-Id: I11a7a4cdc424f16fe7284ff9c9cefbf8c1e33ed7
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747489
    > > > Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    > > > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > > > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1023089}
    > >
    > >
    > > Change-Id: I4de9db6a14dc1bb5f75a39c00d8b830f536e317c
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 1339082
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757113
    > > Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1023092}
    >
    > Bug: 1339082
    > Change-Id: Id1ce605a3dbadfb91f80bf8f790703615425251d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757175
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1023217}
    
    Bug: 1339082
    Change-Id: I0402eb74ddd1a56db1bc8fc6a76a8107d5cd3bc8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758246
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Owners-Override: Stephen McGruer <smcgruer@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1023230}

--- ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
+++ ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
@@ -95,7 +95,8 @@ TEST_P(WaylandWindowManagerTest, GetCurrentFocusedWindow) {
   auto window1 = CreateWaylandWindowWithParams(PlatformWindowType::kWindow,
                                                kDefaultBounds, &delegate);
   // When window is shown, it automatically gets keyboard focus. Reset it.
-  connection_->wayland_window_manager()->SetKeyboardFocusedWindow(nullptr);
+  window_->set_keyboard_focus(false);
+  window1->set_keyboard_focus(false);
 
   Sync();
 
@@ -138,7 +139,8 @@ TEST_P(WaylandWindowManagerTest, GetCurrentKeyboardFocusedWindow) {
   auto window1 = CreateWaylandWindowWithParams(PlatformWindowType::kWindow,
                                                kDefaultBounds, &delegate);
   // When window is shown, it automatically gets keyboard focus. Reset it.
-  connection_->wayland_window_manager()->SetKeyboardFocusedWindow(nullptr);
+  window_->set_keyboard_focus(false);
+  window1->set_keyboard_focus(false);
 
   Sync();
 
