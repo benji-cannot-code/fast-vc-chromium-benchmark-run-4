cf3bdde5b48942b4bf24605ddea8671d7bd06c02
Andrew Xu <andrewxu@chromium.org>
2022-07-12 22:38:52 +0000

    [Shelf] Add an internal feature flag for using the login shelf widget
    This CL adds an internal feature flag. When it is enabled, the login
    shelf lives in its own widget instead of sharing `ShelfWidget` with
    other components.
    
    Bug: 1197052
    Change-Id: Ic0cad8cb4618a965491624dd51ddc4498557ab25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759328
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023458}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -608,6 +608,8 @@ extern const base::Feature kUseAuthsessionAuthentication;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kUseBluetoothSystemInAsh;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kUseLoginShelfWidget;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kUseMessagesStagingUrl;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kUseSearchClickForRightClick;
@@ -842,6 +844,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsTouchscreenInDiagnosticsAppEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsTrafficCountersEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsTrilinearFilteringEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsUploadOfficeToCloudEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsUseLoginShelfWidgetEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsUseStorkSmdsServerAddressEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsWallpaperFastRefreshEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsWallpaperFullScreenPreviewEnabled();
