131e893adf5e0ba2de4c62cd1f1efa8d86995b76
Takashi Toyoshima <toyoshim@chromium.org>
2022-07-12 13:21:42 +0000

    Prerender2: verify content scripts work with / without all_frames
    This patch adds basic tests for content scripts. These tests
    check if content scripts runs on matched pre-rendering frames.
    
    Also, this patch fixes not to inject duplicated content scripts
    again on their page activation.
    
    Bug: 1278141
    Change-Id: I9f15424e35529d800f7fee836fe7bd4d101a1b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3731231
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023178}

--- chrome/browser/extensions/content_script_apitest.cc
+++ chrome/browser/extensions/content_script_apitest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/prerender_test_util.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/browsertest_util.h"
@@ -2159,6 +2160,31 @@ IN_PROC_BROWSER_TEST_F(SubresourceWebBundlesContentScriptApiTest,
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), page_url));
   ASSERT_TRUE(listener.WaitUntilSatisfied());
   EXPECT_EQ(uuid_html_url, listener.message());
+}
+
+class ContentScriptApiPrerenderingTest : public ContentScriptApiTest {
+ public:
+  ContentScriptApiPrerenderingTest()
+      : prerender_helper_(base::BindRepeating(
+            &ContentScriptApiPrerenderingTest::GetWebContents,
+            base::Unretained(this))) {}
+  ~ContentScriptApiPrerenderingTest() override = default;
+
+ private:
+  content::WebContents* GetWebContents() {
+    return browser()->tab_strip_model()->GetWebContentsAt(0);
+  }
+  void SetUp() override {
+    prerender_helper_.SetUp(embedded_test_server());
+    ContentScriptApiTest::SetUp();
+  }
+
+  content::test::PrerenderTestHelper prerender_helper_;
+};
+
+IN_PROC_BROWSER_TEST_F(ContentScriptApiPrerenderingTest, Prerendering) {
+  ASSERT_TRUE(StartEmbeddedTestServer());
+  ASSERT_TRUE(RunExtensionTest("content_scripts/prerendering")) << message_;
 }
 
 }  // namespace extensions
