9fac6088a9a4412e9380c4271ba90596f2161d96
Monica Basta <msalama@chromium.org>
2022-07-12 09:38:22 +0000

    [Signin] Move Auth error handling and storage to ProfileOAuth2TokenServiceDelegate
    Logic for handling and storing auth errors has 5 implementations that
    are very similar. This CL moves that logic to the base class
    ProfileOAuth2TokenServiceDelegate.
    
    Bug: 1336627
    Change-Id: I8d6c4fe412330c0f9fa6f4fa6e78f9b66e3d3b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736388
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023135}

--- components/signin/internal/identity_manager/test_profile_oauth2_token_service_delegate_chromeos.h
+++ components/signin/internal/identity_manager/test_profile_oauth2_token_service_delegate_chromeos.h
@@ -47,7 +47,8 @@ class TestProfileOAuth2TokenServiceDelegateChromeOS
       OAuth2AccessTokenConsumer* consumer) override;
   bool RefreshTokenIsAvailable(const CoreAccountId& account_id) const override;
   void UpdateAuthError(const CoreAccountId& account_id,
-                       const GoogleServiceAuthError& error) override;
+                       const GoogleServiceAuthError& error,
+                       bool fire_auth_error_changed) override;
   GoogleServiceAuthError GetAuthError(
       const CoreAccountId& account_id) const override;
   std::vector<CoreAccountId> GetAccounts() const override;
@@ -60,6 +61,9 @@ class TestProfileOAuth2TokenServiceDelegateChromeOS
   void RevokeCredentials(const CoreAccountId& account_id) override;
   void RevokeAllCredentials() override;
   const net::BackoffEntry* BackoffEntry() const override;
+  void ClearAuthError(const absl::optional<CoreAccountId>& account_id) override;
+  GoogleServiceAuthError BackOffError() const override;
+  void ResetBackOffEntry() override;
 
   // |ProfileOAuth2TokenServiceObserver| implementation.
   void OnRefreshTokenAvailable(const CoreAccountId& account_id) override;
