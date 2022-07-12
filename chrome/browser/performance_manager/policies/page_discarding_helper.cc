8e3c50e8c712ff9f8ef0e67a15ab480b094c00af
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-07-12 09:08:00 +0000

    Add the user and policy controllable no-discard URL list pref
    Bug: 1308741
    Change-Id: I218802a2b268e1dd6c63d692bbd18a379fa22669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696260
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023124}

--- chrome/browser/performance_manager/policies/page_discarding_helper.cc
+++ chrome/browser/performance_manager/policies/page_discarding_helper.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/graph/node_data_describer_registry.h"
 #include "components/performance_manager/public/graph/page_node.h"
 #include "components/performance_manager/public/graph/process_node.h"
+#include "components/url_matcher/url_matcher.h"
+#include "components/url_matcher/url_util.h"
 #include "url/gurl.h"
 
 namespace performance_manager {
@@ -288,6 +290,20 @@ void PageDiscardingHelper::OnIsAudibleChanged(const PageNode* page_node) {
     last_change_to_non_audible_time_[page_node] = base::TimeTicks::Now();
 }
 
+void PageDiscardingHelper::SetNoDiscardPatternsForProfile(
+    const std::string& browser_context_id,
+    const std::vector<std::string>& patterns) {
+  std::unique_ptr<url_matcher::URLMatcher>& entry =
+      profiles_no_discard_patterns_[browser_context_id];
+  entry = std::make_unique<url_matcher::URLMatcher>();
+  url_matcher::util::AddAllowFilters(entry.get(), patterns);
+}
+
+void PageDiscardingHelper::ClearNoDiscardPatternsForProfile(
+    const std::string& browser_context_id) {
+  profiles_no_discard_patterns_.erase(browser_context_id);
+}
+
 void PageDiscardingHelper::SetMockDiscarderForTesting(
     std::unique_ptr<mechanism::PageDiscarder> discarder) {
   page_discarder_ = std::move(discarder);
@@ -377,6 +393,11 @@ PageDiscardingHelper::CanUrgentlyDiscard(
   if (!main_frame->GetURL().is_valid() || main_frame->GetURL().is_empty())
     return CanUrgentlyDiscardResult::kProtected;
 
+  if (IsPageOptedOutOfDiscarding(page_node->GetBrowserContextID(),
+                                 main_frame->GetURL())) {
+    return CanUrgentlyDiscardResult::kProtected;
+  }
+
   const auto* live_state_data = GetPageNodeLiveStateData(page_node);
 
   // The live state data won't be available if none of these events ever
@@ -422,6 +443,25 @@ PageDiscardingHelper::CanUrgentlyDiscard(
   // strip.
 
   return CanUrgentlyDiscardResult::kEligible;
+}
+
+bool PageDiscardingHelper::IsPageOptedOutOfDiscarding(
+    const std::string& browser_context_id,
+    const GURL& url) const {
+  if (!base::FeatureList::IsEnabled(features::kHighEfficiencyModeAvailable) &&
+      !base::FeatureList::IsEnabled(features::kBatterySaverModeAvailable)) {
+    // This list takes effect regardless of which mode the user is operating
+    // under, but its launch is gated on these finch experiments for launch
+    // considerations.
+    return false;
+  }
+
+  auto it = profiles_no_discard_patterns_.find(browser_context_id);
+  // TODO(crbug.com/1308741): Change the CHECK to a DCHECK in Sept 2022, after
+  // verifying that there are no crash reports.
+  CHECK(it != profiles_no_discard_patterns_.end());
+
+  return !it->second->MatchURL(url).empty();
 }
 
 base::Value PageDiscardingHelper::DescribePageNodeData(
--- chrome/browser/performance_manager/policies/page_discarding_helper.h
+++ chrome/browser/performance_manager/policies/page_discarding_helper.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/graph/page_node.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
+namespace url_matcher {
+class URLMatcher;
+}  // namespace url_matcher
+
 namespace performance_manager {
 
 namespace mechanism {
@@ -64,6 +68,10 @@ class PageDiscardingHelper : public GraphOwned,
   void OnBeforePageNodeRemoved(const PageNode* page_node) override;
   void OnIsAudibleChanged(const PageNode* page_node) override;
 
+  void SetNoDiscardPatternsForProfile(const std::string& browser_context_id,
+                                      const std::vector<std::string>& patterns);
+  void ClearNoDiscardPatternsForProfile(const std::string& browser_context_id);
+
   void SetMockDiscarderForTesting(
       std::unique_ptr<mechanism::PageDiscarder> discarder);
   bool CanUrgentlyDiscardForTesting(
@@ -104,6 +112,9 @@ class PageDiscardingHelper : public GraphOwned,
       const PageNode* page_node,
       bool consider_minimum_protection_time = true) const;
 
+  bool IsPageOptedOutOfDiscarding(const std::string& browser_context_id,
+                                  const GURL& url) const;
+
   // NodeDataDescriber implementation:
   base::Value DescribePageNodeData(const PageNode* node) const override;
 
@@ -126,6 +137,9 @@ class PageDiscardingHelper : public GraphOwned,
   // The mechanism used to do the actual discarding.
   std::unique_ptr<performance_manager::mechanism::PageDiscarder>
       page_discarder_;
+
+  std::map<std::string, std::unique_ptr<url_matcher::URLMatcher>>
+      profiles_no_discard_patterns_;
 
   raw_ptr<Graph> graph_ = nullptr;
 
