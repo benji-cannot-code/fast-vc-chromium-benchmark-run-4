1b900b90eec79b06e08a618ddf7861f7c9d2f62d
Andy Phan <andyphan@chromium.org>
2022-07-12 01:56:15 +0000

    Add PageOutputQuality capability to CDD
    To expose the page output quality capability to Print Preview,
    PrinterSemanticCapsAndDefaultsToCdd() needs to parse the
    PageOutputQuality field in the PrinterSemanticCapsAndDefaults
    data structure and add it to the CDD.
    
    Bug: 1291257
    Change-Id: Ib6816d1cc55e79886203f854add731ff3ee9c362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739939
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023014}

--- components/printing/common/cloud_print_cdd_conversion.cc
+++ components/printing/common/cloud_print_cdd_conversion.cc
@@ -28,6 +28,11 @@ namespace cloud_print {
 
 namespace {
 
+#if BUILDFLAG(IS_WIN)
+constexpr char kIdPageOutputQuality[] = "page_output_quality";
+constexpr char kDisplayNamePageOutputQuality[] = "Page output quality";
+#endif  // BUILDFLAG(IS_WIN)
+
 printer::DuplexType ToCloudDuplexType(printing::mojom::DuplexMode mode) {
   switch (mode) {
     case printing::mojom::DuplexMode::kSimplex:
@@ -168,6 +173,22 @@ printer::VendorCapabilities GetVendorCapabilities(
 }
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
+#if BUILDFLAG(IS_WIN)
+printer::SelectVendorCapability GetPageOutputQualityCapabilities(
+    const printing::PrinterSemanticCapsAndDefaults& semantic_info) {
+  printer::SelectVendorCapability page_output_quality_capabilities;
+  const absl::optional<printing::PageOutputQuality>& page_output_quality =
+      semantic_info.page_output_quality;
+  for (const auto& attribute : page_output_quality->qualities) {
+    page_output_quality_capabilities.AddDefaultOption(
+        printer::SelectVendorCapabilityOption(attribute.name,
+                                              attribute.display_name),
+        attribute.name == page_output_quality->default_quality);
+  }
+  return page_output_quality_capabilities;
+}
+#endif  // BUILDFLAG(IS_WIN)
+
 }  // namespace
 
 base::Value PrinterSemanticCapsAndDefaultsToCdd(
@@ -244,6 +265,16 @@ base::Value PrinterSemanticCapsAndDefaultsToCdd(
     vendor_capabilities.SaveTo(&description);
   }
 #endif  // BUILDFLAG(IS_CHROMEOS)
+
+#if BUILDFLAG(IS_WIN)
+  if (semantic_info.page_output_quality) {
+    printer::VendorCapabilities vendor_capabilities;
+    vendor_capabilities.AddOption(printer::VendorCapability(
+        kIdPageOutputQuality, kDisplayNamePageOutputQuality,
+        GetPageOutputQualityCapabilities(semantic_info)));
+    vendor_capabilities.SaveTo(&description);
+  }
+#endif  // BUILDFLAG(IS_WIN)
 
   return std::move(description).ToValue();
 }
