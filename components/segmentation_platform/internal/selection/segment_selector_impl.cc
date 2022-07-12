d2245e50c4781fa4f2fa745fd5eb47c80e377b6c
Salvador Guerrero <salg@google.com>
2022-07-12 02:34:19 +0000

    Fixed CPA URL param and trigger
    Bug: 1342355
    Change-Id: I8694a8f2aaa2b59bb7f808bf5aec789b88e62008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756792
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023022}

--- components/segmentation_platform/internal/selection/segment_selector_impl.cc
+++ components/segmentation_platform/internal/selection/segment_selector_impl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/public/config.h"
 #include "components/segmentation_platform/public/field_trial_register.h"
 #include "components/segmentation_platform/public/model_provider.h"
+#include "components/segmentation_platform/public/segment_selection_result.h"
 
 namespace segmentation_platform {
 namespace {
@@ -269,6 +270,11 @@ void SegmentSelectorImpl::OnGetResultForSegmentSelection(
   if (!result->rank) {
     stats::RecordSegmentSelectionFailure(config_->segmentation_key,
                                          GetFailureReason(result->state));
+    if (config_->on_demand_execution && !callback.is_null()) {
+      base::ThreadTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE,
+          base::BindOnce(std::move(callback), SegmentSelectionResult()));
+    }
     return;
   }
   ranks->insert(std::make_pair(current_segment_id, *result->rank));
