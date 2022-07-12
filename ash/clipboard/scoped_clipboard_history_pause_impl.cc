dbdc1f327e81c013970d7726f065872ddc3bc375
Colin Kincaid <ckincaid@chromium.org>
2022-07-12 22:50:31 +0000

    multipaste: Change clipboard history pause override semantics
    This CL replaces the ScopedClipboardHistoryPauseImpl "metrics_only"
    mode with a pause behavior argument, currently supporting default
    behavior as well as an "allow reorder on paste" option. The former
    could be misleading in that it isn't immediately obvious what it means
    to pause only metrics. The latter is hopefully clearer in semantics:
    Pauses can have specific behavior that does not always prevent
    clipboard history from being modified, and in the reorder-on-paste scenario, modifications are allowed. When clipboard history is changed with the allow reorder on paste behavior specified, metrics will still
    be paused, but the ScopedClipboardHistoryPauseImpl client doesn't have
    to understand that that is one of the option's intended effects.
    
    This change also provides scaffolding for adding reorder metrics.
    
    Bug: 1334428,1342294
    Change-Id: Ic20db2dde5899191aaf401d9c79353509dd9f453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749537
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023463}

--- ash/clipboard/scoped_clipboard_history_pause_impl.cc
+++ ash/clipboard/scoped_clipboard_history_pause_impl.cc
@@ -6,25 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/clipboard/scoped_clipboard_history_pause_impl.h"
 
 #include "ash/clipboard/clipboard_history.h"
+#include "ash/clipboard/clipboard_history_util.h"
 
 namespace ash {
+
+using PauseBehavior = ClipboardHistoryUtil::PauseBehavior;
 
 ScopedClipboardHistoryPauseImpl::ScopedClipboardHistoryPauseImpl(
     ClipboardHistory* clipboard_history)
     : ScopedClipboardHistoryPauseImpl(clipboard_history,
-                                      /*metrics_only=*/false) {}
+                                      PauseBehavior::kDefault) {}
 
 ScopedClipboardHistoryPauseImpl::ScopedClipboardHistoryPauseImpl(
     ClipboardHistory* clipboard_history,
-    bool metrics_only)
-    : clipboard_history_(clipboard_history->GetWeakPtr()),
-      metrics_only_(metrics_only) {
-  clipboard_history_->Pause(metrics_only_);
-}
+    PauseBehavior pause_behavior)
+    : pause_id_(clipboard_history->Pause(pause_behavior)),
+      clipboard_history_(clipboard_history->GetWeakPtr()) {}
 
 ScopedClipboardHistoryPauseImpl::~ScopedClipboardHistoryPauseImpl() {
   if (clipboard_history_)
-    clipboard_history_->Resume(metrics_only_);
+    clipboard_history_->Resume(pause_id_);
 }
 
 }  // namespace ash
