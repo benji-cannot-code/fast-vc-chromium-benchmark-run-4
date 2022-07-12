73a0ca5ba3a03c3659109261c20a79f12bfca86b
Rohit Agarwal <roagarwal@chromium.org>
2022-07-12 10:53:20 +0000

    [Android][IncognitoReauth] Refactor the re-auth code and add more tests.
    This CL refactors the existing IncognitoReauthCoordinator into separate
    parts {FullScreen|TabSwitcher}IncognitoReauthCoordiantor and converts
    the former into interface which can be used agnostically by the
    re-auth controller.
    
    This also allows to reduce the code complexity in the
    RootUiCoordinator and instead delegates the responsibility for
    creating the controller to {Tabbed/BaseCustomTab}RootUiCoordinator
    which also helps to reason better on the various responsibilities.
    
    This CL also adds new units tests for:
    a) IncognitoReauthCoordinatorFactory.
    b) FullScreenIRC
    c) TabSwitcherIRC
    d) IncognitoReauthDialog
    
    Bug: 1227656
    Change-Id: I4f1c4f3c44515585f1a19f779f7d1932e3a2ac84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3742276
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023147}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -34,9 +34,12 @@ import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ActivityType;
 import org.chromium.chrome.browser.fullscreen.BrowserControlsManager;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
+import org.chromium.chrome.browser.incognito.reauth.IncognitoReauthCoordinatorFactory;
+import org.chromium.chrome.browser.incognito.reauth.IncognitoReauthManager;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.reengagement.ReengagementNotificationController;
+import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.tabmodel.TabCreatorManager;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -167,6 +170,7 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
     @Override
     public void onFinishNativeInitialization() {
         super.onFinishNativeInitialization();
+
         if (!ReengagementNotificationController.isEnabled()) return;
         new OneShotCallback<>(mProfileSupplier, mCallbackController.makeCancelable(profile -> {
             assert profile != null : "Unexpectedly null profile from TabModel.";
@@ -176,6 +180,17 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
                     mActivity, tracker, ReengagementActivity.class);
             controller.tryToReengageTheUser();
         }));
+    }
+
+    @Override
+    protected IncognitoReauthCoordinatorFactory getIncognitoReauthCoordinatorFactory() {
+        return new IncognitoReauthCoordinatorFactory(mActivity, mTabModelSelectorSupplier.get(),
+                mModalDialogManagerSupplier.get(), new IncognitoReauthManager(),
+                new SettingsLauncherImpl(),
+                /*tabSwitcherCustomViewManagerOneshotSupplier= */ null,
+                /*incognitoReauthTopToolbarDelegate= */ null,
+                /*layoutManager=*/null,
+                /*isTabbedActivity= */ false);
     }
 
     @Override
--- chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthMediator.java
+++ chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthMediator.java
@@ -7,14 +7,11 @@ package org.chromium.chrome.browser.incognito.reauth;
 import androidx.annotation.NonNull;
 
 import org.chromium.chrome.browser.incognito.reauth.IncognitoReauthManager.IncognitoReauthCallback;
-import org.chromium.chrome.browser.tabmodel.TabModel;
-import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
 /**
  * The mediator responsible for handling the interactions with the incognito re-auth view.
  */
 class IncognitoReauthMediator {
-    private final TabModelSelector mTabModelSelector;
     private final Runnable mShowTabSwitcherRunnable;
     // The entity responsible for actually calling the underlying system re-authentication.
     private final IncognitoReauthManager mIncognitoReauthManager;
@@ -22,19 +19,15 @@ class IncognitoReauthMediator {
     private IncognitoReauthCallback mIncognitoReauthCallback;
 
     /**
-     * @param tabModelSelector The {@link TabModelSelector} which helps to switch to regular {@link
-     *         TabModel}.
      * @param incognitoReauthCallback incognitoReauthCallback The {@link IncognitoReauthCallback}
      *                               which would be executed after an authentication attempt.
      * @param incognitoReauthManager The {@link IncognitoReauthManager} instance which would be used
      *         to initiate re-authentication.
      * @param showTabSwitcherRunnable A {link Runnable} to show the tab switcher UI.
      */
-    IncognitoReauthMediator(@NonNull TabModelSelector tabModelSelector,
-            @NonNull IncognitoReauthCallback incognitoReauthCallback,
+    IncognitoReauthMediator(@NonNull IncognitoReauthCallback incognitoReauthCallback,
             @NonNull IncognitoReauthManager incognitoReauthManager,
             @NonNull Runnable showTabSwitcherRunnable) {
-        mTabModelSelector = tabModelSelector;
         mIncognitoReauthCallback = incognitoReauthCallback;
         mIncognitoReauthManager = incognitoReauthManager;
         mShowTabSwitcherRunnable = showTabSwitcherRunnable;
@@ -48,8 +41,6 @@ class IncognitoReauthMediator {
         // We observe {@link TabModel} changes in {@link IncognitoReauthController} and when we
         // will change to regular {@link TabModel} the controller will detect that and hide
         // the dialog.
-        mTabModelSelector.selectModel(/*incognito=*/false);
-
         mShowTabSwitcherRunnable.run();
     }
 }
