065cba1cec5cb9a1c947bd8f71ed3b9f6204fdf3
Patrick Noland <pnoland@google.com>
2022-07-12 16:40:10 +0000

    [Journeys] Fix a few strings and add empty search text
    * The toggle should read "List" instead of History
    * The toolbar text should always be History
    
    Bug: 1303171
    Change-Id: If296e794e4b622fcf72a798ea97fee6a300c1ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696181
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023263}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
@@ -224,6 +224,11 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
                 public void removeMarkedItems() {
                     mHistoryProvider.removeItems();
                 }
+
+                @Override
+                public String getSearchEmptyString() {
+                    return HistoryManager.this.getSearchEmptyString();
+                }
             };
 
             mHistoryClustersCoordinator =
@@ -273,7 +278,7 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
             mContentView = mHistoryClustersCoordinator.getActivityContentView();
             QueryState queryState = TextUtils.isEmpty(historyClustersQuery)
                     ? QueryState.forQueryless()
-                    : QueryState.forQuery(historyClustersQuery);
+                    : QueryState.forQuery(historyClustersQuery, getSearchEmptyString());
             mHistoryClustersCoordinator.setQueryState(queryState);
         } else {
             mContentView = mSelectableListLayout;
@@ -368,13 +373,7 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
         } else if (item.getItemId() == R.id.search_menu_id) {
             mContentManager.removeHeader();
             mToolbar.showSearchView();
-            String dse = getDefaultSearchEngine();
-            String searchEmptyString = "";
-            if (dse == null) {
-                searchEmptyString = mActivity.getString(R.string.history_manager_no_results_no_dse);
-            } else {
-                searchEmptyString = mActivity.getString(R.string.history_manager_no_results, dse);
-            }
+            String searchEmptyString = getSearchEmptyString();
             mSelectableListLayout.onStartSearch(searchEmptyString);
             recordUserAction("Search");
             mIsSearching = true;
@@ -395,12 +394,14 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
         mShouldShowPrivacyDisclaimerSupplier.set(shouldShowInfoHeader);
     }
 
-    private String getDefaultSearchEngine() {
+    private String getSearchEmptyString() {
         String defaultSearchEngineName = null;
         TemplateUrl dseTemplateUrl =
                 TemplateUrlServiceFactory.get().getDefaultSearchEngineTemplateUrl();
         if (dseTemplateUrl != null) defaultSearchEngineName = dseTemplateUrl.getShortName();
-        return defaultSearchEngineName;
+        return defaultSearchEngineName == null
+                ? mActivity.getString(R.string.history_manager_no_results_no_dse)
+                : mActivity.getString(R.string.history_manager_no_results, defaultSearchEngineName);
     }
 
     /**
