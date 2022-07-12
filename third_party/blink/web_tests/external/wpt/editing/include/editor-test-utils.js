43a50c4a0e8681198016802b5071841b9fc5d266
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-07-12 16:53:12 +0000

    Import wpt@247522a0d1ee1054bbc5965d64c39bdc8ca14d62
    Using wpt-import in Chromium e04fd982e785c586dd49d085b4b41c07b6d3dab9.
    With Chromium commits locally applied on WPT:
    6532d00334 "Revert "Add WPTs for COOP: restrict-properties 3/*""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-sizing
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    iclelland@chromium.org:
      external/wpt/event-timing
    jsbell@chromium.org:
      external/wpt/resources
    mkwst@chromium.org:
      external/wpt/cookies
    mustaq@chromium.org:
      external/wpt/html/interaction/focus
    rego@igalia.com:
      external/wpt/css/css-contain
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    yosin@chromium.org, pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I45a642e62d3c70a1ad7d5077984ed89bcdaae369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757908
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1023269}

--- third_party/blink/web_tests/external/wpt/editing/include/editor-test-utils.js
+++ third_party/blink/web_tests/external/wpt/editing/include/editor-test-utils.js
@@ -124,6 +124,9 @@ class EditorTestUtils {
         if (node.hasChildNodes()) {
           return inclusiveDeepestFirstChildNode(node);
         }
+        if (node === this.editingHost) {
+          return null;
+        }
         if (node.nextSibling) {
           return inclusiveDeepestFirstChildNode(node.nextSibling);
         }
---
+++ third_party/blink/web_tests/external/wpt/fetch/content-type/multipart.window.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Ensure capital letters can be used in the boundary value.
+setup({ single_test: true });
+(async () => {
+  const form_string =
+    "--Boundary_with_capital_letters\r\n" +
+    "Content-Type: application/json\r\n" +
+    'Content-Disposition: form-data; name="does_this_work"\r\n' +
+    "\r\n" +
+    'YES\r\n' +
+    "--Boundary_with_capital_letters--\r\n";
+
+  const r = new Response(new Blob([form_string]), {
+    headers: [
+      [
+        "Content-Type",
+        "multipart/form-data; boundary=Boundary_with_capital_letters",
+      ],
+    ],
+  });
+
+  var s = "";
+  try {
+    const fd = await r.formData();
+    for (const [key, value] of fd.entries()) {
+      s += (`${key} = ${value}`);
+    }
+  } catch (ex) {
+    s = ex;
+  }
+
+  assert_equals(s, "does_this_work = YES");
+  done();
+})();
