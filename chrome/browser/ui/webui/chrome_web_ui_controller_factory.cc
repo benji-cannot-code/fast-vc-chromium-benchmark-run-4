4f0c8caad881bcddd5350131dabe65115d3aaa53
Maggie Cai <mxcai@google.com>
2022-07-12 07:31:03 +0000

    [Lacros] Support os://app-service-internals page in Lacros.
    This CL adds the os://app-service-internals page to allow looking at the
    ash app service data in Lacros Only mode.
    
    BUG=1333368
    
    Change-Id: I88d45826556fbd6ffcb6c1f95c30c5a0df918dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753610
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023104}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -1550,6 +1550,7 @@ std::vector<GURL> ChromeWebUIControllerFactory::GetListOfAcceptableURLs() {
       GURL(chrome::kOsUIAddSupervisionURL),
       GURL(chrome::kChromeUIAppDisabledURL),
       GURL(chrome::kOsUIAppDisabledURL),
+      GURL(chrome::kOsUIAppServiceInternalsURL),
       GURL(chrome::kChromeUIArcGraphicsTracingURL),
       GURL(chrome::kChromeUIArcOverviewTracingURL),
       GURL(chrome::kChromeUIArcPowerControlURL),
