3644e990a9e9b698b2e6d7ef38d13dc4ed051f58
Yohanes Shimelis <yohanes@google.com>
2022-07-12 01:38:43 +0000

    [Omnibox] Removed launched Feature kActiveSearchEngines.
    All checks for Feature kActiveSearchEngines are changed to assume it is enabled. Additionally, the feature is now removed from chrome/browser/aboutflags.cc, and the associated flag descriptions are removed from the flag description files in chrome/browser/.
    
    Bug: 1340259
    Change-Id: I446f0c6256c5ff685c3f446396074cf3fda63e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739418
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Yohanes Shimelis <yohanes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1023010}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1039,9 +1039,6 @@ extern const char kOriginAgentClusterDefaultDescription[];
 extern const char kOmitCorsClientCertName[];
 extern const char kOmitCorsClientCertDescription[];
 
-extern const char kOmniboxActiveSearchEnginesName[];
-extern const char kOmniboxActiveSearchEnginesDescription[];
-
 extern const char kOmniboxAdaptiveSuggestionsCountName[];
 extern const char kOmniboxAdaptiveSuggestionsCountDescription[];
 
--- components/omnibox/common/omnibox_features.h
+++ components/omnibox/common/omnibox_features.h
@@ -90,7 +90,6 @@ extern const base::Feature kBlurWithEscape;
 
 // Settings Page - these affect the appearance of the Search Engines settings
 // page
-extern const base::Feature kActiveSearchEngines;
 extern const base::Feature kSiteSearchStarterPack;
 
 // Experiment to introduce new security indicators for HTTPS.
