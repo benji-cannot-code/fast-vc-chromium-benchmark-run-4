a3a7ac97a5d8efb9e9d8726ff79a94c65f7dcbe2
Kevin Haslett <khaslett@chromium.org>
2022-07-12 19:39:39 +0000

    Use BufferQueue to create backing for primary plane on Android
    When on Android using SkiaOutputDeviceBufferQueue, the following will
    change:
    - SkiaRenderer's BufferQueue manages mailboxes for the root render pass
    - output_surface_plane is scheduled through ScheduleOverlays rather than
      ScheduleOutputSurfaceAsOverlay
    - Dynamic frame buffer allocation works in BufferQueue
    - SkiaOutputDeviceBufferQueue allocates no buffers when BufferQueue is
      used
    
    Change-Id: I886f40d7c2f6125bf4ad4ec6b858f635bff0379d
    Bug: 1322528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699881
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023352}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1380,7 +1380,6 @@ void SkiaOutputSurfaceImplOnGpu::DestroyCopyOutputResourcesOnGpuThread(
 }
 
 void SkiaOutputSurfaceImplOnGpu::CopyOutput(
-    AggregatedRenderPassId id,
     const copy_output::RenderPassGeometry& geometry,
     const gfx::ColorSpace& color_space,
     std::unique_ptr<CopyOutputRequest> request,
@@ -1393,7 +1392,7 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutput(
   if (context_is_lost_)
     return;
 
-  bool from_framebuffer = !id;
+  bool from_framebuffer = mailbox.IsZero();
   DCHECK(scoped_output_device_paint_ || !from_framebuffer);
 
   SkSurface* surface;
