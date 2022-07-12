c6ad5fceaf6c72895b745fb46ff360fad95d0acf
minch <minch@chromium.org>
2022-07-12 18:06:42 +0000

    migration: Gets tray bubble background color from ui::ColorProvider
    Let the tray bubble get background color from ui::ColorProvider
    instead of AshColorProvider. As AshColorProvider will be entirely
    migrated to ui::ColorProvider.
    
    Bug: 1340362
    Change-Id: Idc0fd0f1b63615ae8101f0792b3d663269aed539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733038
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023304}

--- ash/system/tray/tray_bubble_view.cc
+++ ash/system/tray/tray_bubble_view.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/style/color_provider.h"
 #include "ash/public/cpp/view_shadow.h"
 #include "ash/shell.h"
-#include "ash/style/ash_color_provider.h"
+#include "ash/style/ash_color_id.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/unified/unified_system_tray_view.h"
 #include "third_party/skia/include/core/SkCanvas.h"
@@ -523,16 +523,14 @@ void TrayBubbleView::OnThemeChanged() {
     SetBorder(std::make_unique<views::HighlightBorder>(
         params_.corner_radius, views::HighlightBorder::Type::kHighlightBorder1,
         /*use_light_colors=*/false));
-    set_color(AshColorProvider::Get()->GetBaseLayerColor(
-        AshColorProvider::BaseLayerType::kTransparent80));
+    set_color(GetColorProvider()->GetColor(kColorAshShieldAndBase80));
     return;
   }
 
   DCHECK(layer());
   if (layer()->type() != ui::LAYER_SOLID_COLOR)
     return;
-  layer()->SetColor(AshColorProvider::Get()->GetBaseLayerColor(
-      AshColorProvider::BaseLayerType::kTransparent80));
+  layer()->SetColor(GetColorProvider()->GetColor(kColorAshShieldAndBase80));
 }
 
 void TrayBubbleView::MouseMovedOutOfHost() {
