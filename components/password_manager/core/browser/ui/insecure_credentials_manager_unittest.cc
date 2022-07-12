543ca3ad9a9fc1f84b292fc6237b8196b6737f86
Jan Keitel <jkeitel@google.com>
2022-07-12 15:57:13 +0000

    Make kPasswordChangeInSettingsWithForcedWarningForEverySite work again.
    This CL adds an additional check for the field trial parameter
    kPasswordChangeInSettingsWithForcedWarningForEverySite that was missed
    during the refactoring to CredentialUIEntry.
    
    Fixed: 1343727
    Change-Id: Ied336800b29e2fa90b72e935d5a1f3a5e6604e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757316
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1023235}

--- components/password_manager/core/browser/ui/insecure_credentials_manager_unittest.cc
+++ components/password_manager/core/browser/ui/insecure_credentials_manager_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/timer/elapsed_timer.h"
 #include "build/build_config.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/test_password_store.h"
 #include "components/password_manager/core/browser/ui/credential_ui_entry.h"
 #include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -52,6 +54,7 @@ constexpr int kDelay = 2;
 using ::testing::ElementsAre;
 using ::testing::ElementsAreArray;
 using ::testing::IsEmpty;
+using ::testing::SizeIs;
 
 struct MockInsecureCredentialsManagerObserver
     : InsecureCredentialsManager::Observer {
@@ -1089,6 +1092,27 @@ TEST_F(InsecureCredentialsManagerTest, UnMuteReusedPasswordNoOp) {
                   .back()
                   .password_issues.at(InsecureType::kReused)
                   .is_muted.value());
+}
+
+TEST_F(InsecureCredentialsManagerTest, GetInsecureCredentialEntriesWithFlag) {
+  PasswordForm password_form =
+      MakeSavedPassword(kExampleCom, kUsername1, kPassword1);
+
+  store().AddLogin(password_form);
+  RunUntilIdle();
+
+  EXPECT_THAT(provider().GetInsecureCredentialEntries(), IsEmpty());
+
+  // Turn on the field trial parameter to force password leaks everywhere.
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeatureWithParameters(
+      password_manager::features::kPasswordChangeInSettings,
+      {{password_manager::features::
+            kPasswordChangeInSettingsWithForcedWarningForEverySite,
+        "true"}});
+
+  // The credential is now returned as insecure.
+  EXPECT_THAT(provider().GetInsecureCredentialEntries(), SizeIs(1u));
 }
 
 // Test verifies that editing Compromised Credential via provider change the
