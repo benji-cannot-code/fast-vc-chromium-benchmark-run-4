bd6040d877499843c5a50cce569b5ad0b18fa1dc
Ken Buchanan <kenrb@chromium.org>
2022-07-12 20:14:28 +0000

    Reland: [WebAuthn] GetCredentials caller to expect non-discoverable creds
    The Android WebAuthn implementation uses the Play Services API
    GetCredentials method to retrieve a list of discoverable credentials
    from the device. The method is being modified to return non-discoverable
    credentials as well.
    
    This change filters out non-discoverable credentials for Conditional UI
    and removes the requirement for some returned credential fields to be
    non-null.
    
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/3750008.
    Fido2ApiTestHelper.java has been correctly updated to account for the
    change.
    
    Fixed: 1343681
    
    Change-Id: Ie674c924230acaa791190c4caa0bcf48ff87e9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758244
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023375}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
@@ -75,6 +75,7 @@ import org.chromium.url.Origin;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -104,6 +105,7 @@ public class Fido2CredentialRequestTest {
     private EmbeddedTestServer mTestServer;
     private MockAuthenticatorRenderFrameHost mFrameHost;
     private MockBrowserBridge mMockBrowserBridge;
+    private MockFido2ApiCallHelper mFido2ApiCallHelper;
     private Origin mOrigin;
     private InternalAuthenticator.Natives mTestAuthenticatorImplJni;
     private Fido2CredentialRequest mRequest;
@@ -188,12 +190,18 @@ public class Fido2CredentialRequestTest {
     }
 
     private static class MockFido2ApiCallHelper extends Fido2ApiCallHelper {
+        private WebAuthnCredentialDetails mReturnedCredentialDetails;
+
         @Override
         public void invokeFido2GetCredentials(String relyingPartyId, int supportLevel,
                 OnSuccessListener<List<WebAuthnCredentialDetails>> successCallback,
                 OnFailureListener failureCallback) {
-            WebAuthnCredentialDetails credential = Fido2ApiTestHelper.getCredentialDetails();
-            successCallback.onSuccess(Arrays.asList(new WebAuthnCredentialDetails[] {credential}));
+            successCallback.onSuccess(
+                    Arrays.asList(new WebAuthnCredentialDetails[] {mReturnedCredentialDetails}));
+        }
+
+        public void setReturnedCredentialDetails(WebAuthnCredentialDetails details) {
+            mReturnedCredentialDetails = details;
         }
     }
 
@@ -456,7 +464,9 @@ public class Fido2CredentialRequestTest {
                 mIntentSender, WebAuthenticationDelegate.Support.BROWSER);
         Fido2ApiHandler.overrideInstanceForTesting(new MockFido2ApiHandler(mRequest));
 
-        Fido2ApiCallHelper.overrideInstanceForTesting(new MockFido2ApiCallHelper());
+        mFido2ApiCallHelper = new MockFido2ApiCallHelper();
+        mFido2ApiCallHelper.setReturnedCredentialDetails(Fido2ApiTestHelper.getCredentialDetails());
+        Fido2ApiCallHelper.overrideInstanceForTesting(mFido2ApiCallHelper);
 
         mMockBrowserBridge = new MockBrowserBridge();
         mRequest.overrideBrowserBridgeForTesting(mMockBrowserBridge);
@@ -1327,6 +1337,30 @@ public class Fido2CredentialRequestTest {
         mMockBrowserBridge.setSelectedCredentialId(new byte[0]);
         mMockBrowserBridge.setExpectedCredentialDetailsList(Arrays.asList(
                 new WebAuthnCredentialDetails[] {Fido2ApiTestHelper.getCredentialDetails()}));
+        mRequestOptions.allowCredentials = new PublicKeyCredentialDescriptor[0];
+        mRequestOptions.isConditional = true;
+
+        mRequest.handleGetAssertionRequest(mRequestOptions, mFrameHost, mOrigin, /*payment=*/null,
+                (responseStatus, response)
+                        -> mCallback.onSignResponse(responseStatus, response),
+                errorStatus -> mCallback.onError(errorStatus));
+        mCallback.blockUntilCalled();
+        Assert.assertEquals(
+                Integer.valueOf(AuthenticatorStatus.UNKNOWN_ERROR), mCallback.getStatus());
+        Assert.assertNull(mCallback.getGetAssertionResponse());
+        Fido2ApiTestHelper.verifyRespondedBeforeTimeout(mStartTimeMs);
+    }
+
+    @Test
+    @SmallTest
+    public void testGetAssertion_conditionalUiNondiscoverableCredential_failure() {
+        mIntentSender.setNextResultIntent(Fido2ApiTestHelper.createSuccessfulGetAssertionIntent());
+        WebAuthnCredentialDetails nonDiscoverableCredDetails =
+                Fido2ApiTestHelper.getCredentialDetails();
+        nonDiscoverableCredDetails.mIsDiscoverable = false;
+        mFido2ApiCallHelper.setReturnedCredentialDetails(nonDiscoverableCredDetails);
+        mMockBrowserBridge.setExpectedCredentialDetailsList(new ArrayList<>());
+
         mRequestOptions.allowCredentials = new PublicKeyCredentialDescriptor[0];
         mRequestOptions.isConditional = true;
 
