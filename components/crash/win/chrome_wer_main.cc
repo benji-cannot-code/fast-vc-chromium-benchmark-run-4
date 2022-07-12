c0b4d2f7a6df24c9aea18c1f268d12bf6bcf3143
Alex Gough <ajgo@chromium.org>
2022-07-12 22:13:56 +0000

    Builds chrome_wer.dll - a WER runtime exception helper module
    This builds a DLL which can be registered as a runtime exception
    helper using WerRegisterExceptionHelperModule[0]. We do not yet
    register or distribute the module - this will happen in later CLs.
    
    Adds a test that validates that the module can be loaded and has
    the expected exported function. As the dll wraps a crashpad library,
    most of its testing occurs within crashpad's tests and is not
    repeated here.
    
    [0] https://docs.microsoft.com/en-us/windows/win32/api/werapi/nf-werapi-werregisterruntimeexceptionmodule
    
    Bug: 865632
    Change-Id: Ib66ee12ef893c308de3fca534b763c187b54eb13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688951
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023446}

---
+++ components/crash/win/chrome_wer_main.cc
@@ -0,0 +1,64 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <Windows.h>
+
+// werapi.h must be after Windows.h
+#include <werapi.h>
+
+#include "third_party/crashpad/crashpad/handler/win/wer/crashpad_wer.h"
+
+extern "C" {
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+  return true;
+}
+
+HRESULT OutOfProcessExceptionEventCallback(
+    PVOID pContext,
+    const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+    BOOL* pbOwnershipClaimed,
+    PWSTR pwszEventName,
+    PDWORD pchSize,
+    PDWORD pdwSignatureCount) {
+  // Exceptions that are not collected by crashpad's in-process handlers.
+  std::vector<DWORD> wanted_exceptions = {
+      0xC0000409,  // STATUS_STACK_BUFFER_OVERRUN
+  };
+  bool result = crashpad::wer::ExceptionEvent(wanted_exceptions, pContext,
+                                              pExceptionInformation);
+
+  if (result) {
+    *pbOwnershipClaimed = TRUE;
+    // Technically we failed as we terminated the process.
+    return E_FAIL;
+  }
+  // Could not dump for whatever reason, so let other helpers/wer have a chance.
+  *pbOwnershipClaimed = FALSE;
+  return S_OK;
+}
+
+HRESULT OutOfProcessExceptionEventSignatureCallback(
+    PVOID pContext,
+    const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+    DWORD dwIndex,
+    PWSTR pwszName,
+    PDWORD pchName,
+    PWSTR pwszValue,
+    PDWORD pchValue) {
+  // This function should never be called.
+  return E_FAIL;
+}
+
+HRESULT OutOfProcessExceptionEventDebuggerLaunchCallback(
+    PVOID pContext,
+    const PWER_RUNTIME_EXCEPTION_INFORMATION pExceptionInformation,
+    PBOOL pbIsCustomDebugger,
+    PWSTR pwszDebuggerLaunch,
+    PDWORD pchDebuggerLaunch,
+    PBOOL pbIsDebuggerAutolaunch) {
+  // This function should never be called.
+  return E_FAIL;
+}
+}  // extern "C"
