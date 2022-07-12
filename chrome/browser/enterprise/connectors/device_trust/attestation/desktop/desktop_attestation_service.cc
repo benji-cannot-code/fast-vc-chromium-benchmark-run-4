33c5b912444d7bf1a8af3330e7c02ee83cb41a6d
Philippe Leblanc <pleblanc@google.com>
2022-07-12 20:12:12 +0000

    Fix typo kChallengResponseNonceBytesSize -> kChallengeResponseNonceBytesSize
    Change-Id: Ieae0098c8f6a5ed21f8a652c3847a4f1e93c062f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759164
    Commit-Queue: Philippe Leblanc <pleblanc@google.com>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Philippe Leblanc <pleblanc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1023373}

--- chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service.cc
@@ -30,7 +30,7 @@ namespace {
 constexpr VAType kVAType = VAType::DEFAULT_VA;
 
 // Size of nonce for challenge response.
-const size_t kChallengResponseNonceBytesSize = 32;
+const size_t kChallengeResponseNonceBytesSize = 32;
 
 // Verifies that the `signed_challenge_data` comes from Verified Access.
 bool ChallengeComesFromVerifiedAccess(
@@ -56,8 +56,8 @@ absl::optional<std::string> CreateChallengeResponseString(
   *response_pb.mutable_challenge() = signed_challenge_data;
 
   crypto::RandBytes(base::WriteInto(response_pb.mutable_nonce(),
-                                    kChallengResponseNonceBytesSize + 1),
-                    kChallengResponseNonceBytesSize);
+                                    kChallengeResponseNonceBytesSize + 1),
+                    kChallengeResponseNonceBytesSize);
 
   std::string key;
   if (!CryptoUtility::EncryptWithSeed(
