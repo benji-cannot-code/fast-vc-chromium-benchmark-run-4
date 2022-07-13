2c6d6e6afd5968acc95596cc8acdd1d98817f5c6
Kenichi Ishibashi <bashi@chromium.org>
2022-07-13 00:09:54 +0000

    Move Accept-Encoding construction to HttpRequestHeaders
    This is a preparation for proper Vary checks in network service memory
    cache. This CL decouples Accept-Encoding calculation from URLRequest and
    URLRequestHttpJob so that we can share the calculation in the network
    service.
    
    This should have no behavior change.
    
    Bug: 1339708
    Change-Id: Id9862edf2801d0960d3f36a8afa82fcf9e80209d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752261
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023495}

--- net/http/http_request_headers.h
+++ net/http/http_request_headers.h
@@ -15,9 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/strings/string_piece.h"
 #include "net/base/net_export.h"
+#include "net/filter/source_stream.h"
 #include "net/log/net_log_capture_mode.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "url/gurl.h"
 
 namespace base {
 class Value;
@@ -186,6 +190,14 @@ class NET_EXPORT HttpRequestHeaders {
                            NetLogCaptureMode capture_mode) const;
 
   const HeaderVector& GetHeaderVector() const { return headers_; }
+
+  // Sets Accept-Encoding header based on `url` and `accepted_stream_types`, if
+  // it does not exist. "br" is appended only when `enable_brotli` is true.
+  void SetAcceptEncodingIfMissing(
+      const GURL& url,
+      const absl::optional<base::flat_set<SourceStream::SourceType>>&
+          accepted_stream_types,
+      bool enable_brotli);
 
  private:
   HeaderVector::iterator FindHeader(const base::StringPiece& key);
