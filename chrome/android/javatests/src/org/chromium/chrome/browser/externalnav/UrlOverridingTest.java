834248a605021b6c1d7627c1e3b7d2c9fc79179c
Michael Thiessen <mthiesse@chromium.org>
2022-07-13 14:44:07 +0000

    Introduce 15s timeout for navigation chains launching apps
    Websites should not be given indefinitely long to redirect to an app,
    as we don't want an unattended page to launch an app.
    
    This change unifies the xhr-initiated and non-xhr-initiated
    navigation chains to timeout after 15 seconds from when the user
    initiated the navigation (or xhr). (Technically if the site waits
    until the user activation in blink is about to expire before kicking
    off the navigation then up to 20 seconds can be taken.) If the
    navigation chain is expired and no fallback URL is provided, a
    Message is shown to the user asking if they would like to launch the
    app.
    
    Future changes would ideally replace the UserGestureCarryover IPC
    with the time the user activation took place and set a bit on the
    NavigationHandle for navigations that happened during a resource
    request callback in javascript. However, there's no urgent need to
    make such changes.
    
    Doc: https://docs.google.com/document/d/1NVk_3eNRJ3aTXePTKk-9tC36LtNVvMUFoSeIc9cBi48/edit#
    
    Bug: 1311399
    Change-Id: I40b13dc37195d1f9ccda9bf304a4c1483edbf695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746265
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023725}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -18,6 +18,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.lifecycle.Stage;
 import android.text.TextUtils;
@@ -36,6 +37,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
@@ -134,8 +136,6 @@ public class UrlOverridingTest {
             BASE_PATH + "navigation_from_xhr_callback_parent_frame.html";
     private static final String NAVIGATION_FROM_XHR_CALLBACK_AND_SHORT_TIMEOUT_PAGE =
             BASE_PATH + "navigation_from_xhr_callback_and_short_timeout.html";
-    private static final String NAVIGATION_FROM_XHR_CALLBACK_AND_LONG_TIMEOUT_PAGE =
-            BASE_PATH + "navigation_from_xhr_callback_and_long_timeout.html";
     private static final String NAVIGATION_WITH_FALLBACK_URL_PAGE =
             BASE_PATH + "navigation_with_fallback_url.html";
     private static final String NAVIGATION_WITH_FALLBACK_URL_PARENT_FRAME_PAGE =
@@ -171,6 +171,9 @@ public class UrlOverridingTest {
 
     @Mock
     private RedirectHandler mRedirectHandler;
+
+    @Spy
+    private RedirectHandler mSpyRedirectHandler;
 
     private static class TestTabObserver extends EmptyTabObserver {
         private final CallbackHelper mFinishCallback;
@@ -580,11 +583,30 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
-    public void testNavigationFromXHRCallbackAndLongTimeout() {
+    public void testNavigationFromXHRCallbackAndLongTimeout() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
-        loadUrlAndWaitForIntentUrl(
-                mTestServer.getURL(NAVIGATION_FROM_XHR_CALLBACK_AND_LONG_TIMEOUT_PAGE), true,
+
+        final Tab tab = mActivityTestRule.getActivity().getActivityTab();
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> RedirectHandlerTabHelper.swapHandlerFor(tab, mSpyRedirectHandler));
+
+        // This is a little fragile to code changes, but better than waiting 15 real seconds.
+        Mockito.doReturn(SystemClock.elapsedRealtime()) // Initial Navigation create
+                .doReturn(SystemClock.elapsedRealtime()) // Initial Navigation shouldOverride
+                .doReturn(SystemClock.elapsedRealtime()) // XHR Navigation create
+                .doReturn(SystemClock.elapsedRealtime()
+                        + RedirectHandler.NAVIGATION_CHAIN_TIMEOUT_MILLIS + 1) // xhr callback
+                .when(mSpyRedirectHandler)
+                .currentRealtime();
+
+        @OverrideUrlLoadingResultType
+        int result = loadUrlAndWaitForIntentUrl(
+                mTestServer.getURL(NAVIGATION_FROM_XHR_CALLBACK_AND_SHORT_TIMEOUT_PAGE), true,
                 false);
+
+        Assert.assertEquals(OverrideUrlLoadingResultType.OVERRIDE_WITH_ASYNC_ACTION, result);
+
+        assertMessagePresent();
     }
 
     @Test
--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -440,7 +440,7 @@ void TabWebContentsDelegateAndroid::UpdateUserGestureCarryoverInfo(
   auto* intercept_navigation_delegate =
       navigation_interception::InterceptNavigationDelegate::Get(web_contents);
   if (intercept_navigation_delegate)
-    intercept_navigation_delegate->UpdateLastUserGestureCarryoverTimestamp();
+    intercept_navigation_delegate->OnResourceRequestWithGesture();
 }
 
 content::PictureInPictureResult
