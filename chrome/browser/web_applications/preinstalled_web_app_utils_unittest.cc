661df429a6a0cb9f51e5e62e9656a7801d5a7077
Jeevan Shikaram <jshikaram@chromium.org>
2022-07-13 17:23:53 +0000

    [Default Apps] Allow offline manifests to declare maskable icons.
    Bug: 1338379
    Change-Id: Ife11147b428e6023755d37f2b52791e55e4f9440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750635
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023806}

--- chrome/browser/web_applications/preinstalled_web_app_utils_unittest.cc
+++ chrome/browser/web_applications/preinstalled_web_app_utils_unittest.cc
@@ -215,6 +215,7 @@ TEST_F(PreinstalledWebAppUtilsTest, MAYBE_OfflineManifestValid) {
       "scope": "https://test.org/",
       "display": "standalone",
       "icon_any_pngs": ["icon.png"],
+      "icon_maskable_pngs": ["icon.png"],
       "theme_color_argb_hex": "AABBCCDD"
     }
   )")
@@ -227,6 +228,9 @@ TEST_F(PreinstalledWebAppUtilsTest, MAYBE_OfflineManifestValid) {
   EXPECT_EQ(app_info->display_mode, DisplayMode::kStandalone);
   EXPECT_EQ(app_info->icon_bitmaps.any.size(), 1u);
   EXPECT_EQ(app_info->icon_bitmaps.any.at(192).getColor(0, 0), SK_ColorBLUE);
+  EXPECT_EQ(app_info->icon_bitmaps.maskable.size(), 1u);
+  EXPECT_EQ(app_info->icon_bitmaps.maskable.at(192).getColor(0, 0),
+            SK_ColorBLUE);
   EXPECT_EQ(app_info->theme_color, SkColorSetARGB(0xFF, 0xBB, 0xCC, 0xDD));
 }
 
@@ -430,6 +434,47 @@ TEST_F(PreinstalledWebAppUtilsTest, OfflineManifestIconAnyPngs) {
       "icon_any_pngs": ["basic.html"]
     }
   )")) << "icon_any_pngs is a PNG";
+}
+
+TEST_F(PreinstalledWebAppUtilsTest, OfflineManifestIconMaskablePngs) {
+  EXPECT_FALSE(ParseOfflineManifest(R"(
+    {
+      "name": "Test App",
+      "start_url": "https://test.org/start.html",
+      "scope": "https://test.org/",
+      "display": "standalone"
+    }
+  )")) << "icon_any_pngs or icon_maskable_pngs is required";
+
+  EXPECT_FALSE(ParseOfflineManifest(R"(
+    {
+      "name": "Test App",
+      "start_url": "https://test.org/start.html",
+      "scope": "https://test.org/",
+      "display": "standalone",
+      "icon_maskable_pngs": "icon.png"
+    }
+  )")) << "icon_maskable_pngs is valid";
+
+  EXPECT_FALSE(ParseOfflineManifest(R"(
+    {
+      "name": "Test App",
+      "start_url": "https://test.org/start.html",
+      "scope": "https://test.org/",
+      "display": "standalone",
+      "icon_maskable_pngs": ["does-not-exist.png"]
+    }
+  )")) << "icon_maskable_pngs exists";
+
+  EXPECT_FALSE(ParseOfflineManifest(R"(
+    {
+      "name": "Test App",
+      "start_url": "https://test.org/start.html",
+      "scope": "https://test.org/",
+      "display": "standalone",
+      "icon_maskable_pngs": ["basic.html"]
+    }
+  )")) << "icon_maskable_pngs is a PNG";
 }
 
 TEST_F(PreinstalledWebAppUtilsTest, OfflineManifestThemeColorArgbHex) {
