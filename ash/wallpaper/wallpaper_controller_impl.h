8b37dcc504da7bdd6341bf572ac8e8360f1d3139
Jason Thai <jasontt@chromium.org>
2022-07-13 00:14:22 +0000

    rgbkbd: show default color for nearly black or white wallpaper color
    Some nearly white colors show up badly on the keyboard whereas black
    color turns off the backlight completely. This CL adds a fallback to
    using |kDefaultColor| for these colors.
    
    BUG=b/238671031
    TEST=ash_unittests "KeyboardBacklightControllerTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I5b5e9c8deb5e7c335218df96e6544fab0b95c200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756633
    Reviewed-by: Eric Sum <esum@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023498}

--- ash/wallpaper/wallpaper_controller_impl.h
+++ ash/wallpaper/wallpaper_controller_impl.h
@@ -381,6 +381,7 @@ class ASH_EXPORT WallpaperControllerImpl
                            WallpaperMovementDuringUnlock);
   friend class WallpaperControllerTest;
   friend class WallpaperControllerTestApi;
+  friend class KeyboardBacklightColorControllerTest;
 
   enum WallpaperMode { WALLPAPER_NONE, WALLPAPER_IMAGE };
 
