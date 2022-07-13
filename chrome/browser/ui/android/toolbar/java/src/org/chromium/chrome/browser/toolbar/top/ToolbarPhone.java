6fbd9873cc23adffc740fe05c286f1738fe74219
Sky Malice <skym@chromium.org>
2022-07-13 20:39:07 +0000

    Move where ToolbarPhone updates its snapshot state.
    When the hardware draw approach is used with RenderNodes, an Android
    animation optimization avoids calling most protected/overridable draw
    methods for parent views, and goes directly to child views. This
    resulted in the old location never being triggered for about half of
    the captures.
    
    This CL moves it to somewhere that is guaranteed to be hit for both
    software and hardware draw approaches.
    
    Bug: 1344203
    Change-Id: I16e2696091f2c67f1e1ca22d9f7600c7fff2f2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760075
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023911}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -1339,8 +1339,6 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
                     mToolbarButtonsContainer, canvas, rgbAlpha);
         }
 
-        mToolbarSnapshotState = generateToolbarSnapshotState();
-
         canvas.restore();
     }
 
@@ -1794,6 +1792,11 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
             setVisibility(mPreTextureCaptureVisibility);
             updateShadowVisibility();
             mPreTextureCaptureAlpha = 1f;
+
+            // When texture mode is turned off, we know a capture has just been completed. Update
+            // our snapshot so that we can suppress correctly on the next
+            // #isReadyForTextureCapture() call.
+            mToolbarSnapshotState = generateToolbarSnapshotState();
         }
     }
 
