a3d4e9ea674f1d8fac25a796bd0358be1fdf3c94
Sky Malice <skym@chromium.org>
2022-07-13 18:23:02 +0000

    HardwareDraw now pushes bitmaps on completion.
    Instead of requiring two sequential #onResourceRequested() calls where
    #isDirty() returns true for both, the first should now be sufficient
    to get the captured bitmap into native and become accessible to the
    renderer.
    
    This means that the hardware path no longer needs to overload
    #isDirty(), and the View code can safely manipulate #isDirty() without
    causing unintended side effects.
    
    Bug: 1338202
    Change-Id: I24a411514885d9684fb0735f5886dc0e8854d4eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717430
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023848}

--- ui/android/java/src/org/chromium/ui/resources/dynamics/SoftwareDraw.java
+++ ui/android/java/src/org/chromium/ui/resources/dynamics/SoftwareDraw.java
@@ -11,8 +11,8 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.view.View;
 
+import org.chromium.base.Callback;
 import org.chromium.base.TraceEvent;
-import org.chromium.ui.resources.dynamics.ViewResourceAdapter.CaptureResult;
 
 /** Simple bitmap capture approach simply calling {@link View#draw(Canvas)}. */
 public class SoftwareDraw implements ViewResourceAdapter.CaptureMechanism {
@@ -20,11 +20,6 @@ public class SoftwareDraw implements ViewResourceAdapter.CaptureMechanism {
 
     @Override
     public boolean shouldRemoveResourceOnNullBitmap() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldPretendIsDirty() {
         return false;
     }
 
@@ -37,8 +32,8 @@ public class SoftwareDraw implements ViewResourceAdapter.CaptureMechanism {
     }
 
     @Override
-    public CaptureResult syncCaptureBitmap(
-            View view, Rect dirtyRect, float scale, CaptureObserver observer) {
+    public boolean startBitmapCapture(View view, Rect dirtyRect, float scale,
+            CaptureObserver observer, Callback<Bitmap> onBitmapCapture) {
         try (TraceEvent e = TraceEvent.scoped("SoftwareDraw:syncCaptureBitmap")) {
             int scaledWidth = (int) (view.getWidth() * scale);
             int scaledHeight = (int) (view.getHeight() * scale);
@@ -66,7 +61,9 @@ public class SoftwareDraw implements ViewResourceAdapter.CaptureMechanism {
                 assert mBitmap.getWidth() == 1 && mBitmap.getHeight() == 1;
                 mBitmap.setPixel(0, 0, Color.TRANSPARENT);
             }
-            return new CaptureResult(mBitmap, !isEmpty);
+
+            onBitmapCapture.onResult(mBitmap);
+            return !isEmpty;
         }
     }
 }
