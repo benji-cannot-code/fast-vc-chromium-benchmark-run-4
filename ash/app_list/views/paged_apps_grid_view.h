806330bb0edaa37c2e7e1a51e5630c3d711f04fb
Matthew Mourgos <mmourgos@chromium.org>
2022-07-13 01:12:03 +0000

    Remove unused includes from *_apps_grid_view files.
    Change-Id: I09ce12a032e5805a3a519e22b5b359f591472349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758953
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023524}

--- ash/app_list/views/paged_apps_grid_view.h
+++ ash/app_list/views/paged_apps_grid_view.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/public/cpp/pagination/pagination_model.h"
 #include "ash/public/cpp/pagination/pagination_model_observer.h"
-#include "base/memory/ref_counted.h"
 #include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/compositor/layer_animation_observer.h"
