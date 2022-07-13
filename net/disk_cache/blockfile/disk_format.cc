6a0cbbd56205ba4318db11e1653d5d0773be748b
Maks Orlovich <morlovich@chromium.org>
2022-07-13 14:58:36 +0000

    Blockfile backend: switch the current size field to 64-bit
    While the cache size limit is nowhere near the 32-bit fields limit, in practice it's quite possible for the backend's actually use to overflow it, since eviction is somewhat rate limited, and so a sufficiently fast writer can overcome it.
    
    This bumps the file format version to 3.0, automatically migrating from previous 2.0/2.1 variations, as it's quite easy to do so.
    
    Bug: 1341826
    Change-Id: Iccedcc10d04944fbcb8ca1bde2b22168199535cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755305
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023730}

--- net/disk_cache/blockfile/disk_format.cc
+++ net/disk_cache/blockfile/disk_format.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace disk_cache {
 
+static_assert(sizeof(IndexHeader) == 368);
+
 IndexHeader::IndexHeader() {
   memset(this, 0, sizeof(*this));
   magic = kIndexMagic;
