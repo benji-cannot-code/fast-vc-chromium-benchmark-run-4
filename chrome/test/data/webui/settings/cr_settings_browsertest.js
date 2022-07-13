e7ba5c3fc13a65ffcd608ae98c7567a777f4e440
Florian Leimgruber <fleimgruber@google.com>
2022-07-13 09:44:28 +0000

    Reland "Add AutofillEnableExtendedAddressFormats to field trial config."
    This is a reland of commit c01bae6451cc587a85c26fb71f7e91cdbd62e98e
    
    The original CL renamed "showHonorific" to "EnableHonorificPrefix" for
    consistency, but missed several spots, causing the failure. This CL simply leaves "showHonorifics" as is.
    
    Original change's description:
    > Add AutofillEnableExtendedAddressFormats to field trial config.
    >
    > This breaks a browser test that verifies how the setting pages look.
    > The logic checks the US and GB formats, the latter of which is changed
    > by this feature (County field, see
    > https://screenshot.googleplex.com/3w34HAVUpJXMfTa). This is resolved by
    > adapting the expectations accordingly, similar to what is done for
    > honorifix prefixes already.
    >
    > Bug: 1300548
    > Change-Id: Iae55e02024f3eb3244b96d17ca7be4246a2c7487
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751159
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1022062}
    
    Bug: 1300548
    Change-Id: I52b97afa773e301d728a0b592ee89b4d3509a32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751041
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1023645}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -238,6 +238,7 @@ TEST_F('CrSettingsAutofillSectionCompanyEnabledTest', 'All', function() {
   const loadTimeDataOverride = {};
   loadTimeDataOverride['EnableCompanyName'] = true;
   loadTimeDataOverride['showHonorific'] = true;
+  loadTimeDataOverride['EnableExtendedAddressFormat'] = true;
   loadTimeData.overrideValues(loadTimeDataOverride);
   mocha.run();
 });
