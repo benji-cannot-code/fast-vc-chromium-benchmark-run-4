63bbf75be4c4eb8f430dd43be06b75bfb2888a4a
Matt Menke <mmenke@chromium.org>
2022-07-13 17:35:33 +0000

    Update base::Value usage in components/password_manager.
    In particular, use Value::List and Value::Dict to manipulate Lists and
    Dicts, instead of deprecated APIs (Creating them from vector<Value>,
    GetListDeprecated(), Value::SetKey(), Value::FindKey(), etc).
    
    Bug: 1187001, 1338341
    Change-Id: Ie4f8e1f066f0adff020893be12cf99b8cdd82292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747968
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023813}

--- components/password_manager/core/browser/password_manager_features_util_unittest.cc
+++ components/password_manager/core/browser/password_manager_features_util_unittest.cc
@@ -20,11 +20,11 @@ namespace password_manager {
 namespace features_util {
 namespace {
 
-base::Value CreateOptedInAccountPref() {
-  base::Value global_pref(base::Value::Type::DICTIONARY);
-  base::Value account_pref(base::Value::Type::DICTIONARY);
-  account_pref.SetBoolKey("opted_in", true);
-  global_pref.SetKey("some_gaia_hash", std::move(account_pref));
+base::Value::Dict CreateOptedInAccountPref() {
+  base::Value::Dict global_pref;
+  base::Value::Dict account_pref;
+  account_pref.Set("opted_in", true);
+  global_pref.Set("some_gaia_hash", std::move(account_pref));
   return global_pref;
 }
 }  // namespace
@@ -144,8 +144,8 @@ TEST_F(PasswordManagerFeaturesUtilWithoutAccountStorageTest,
 
 TEST_P(PasswordManagerFeaturesUtilTest, ShowAccountStorageResignIn) {
   // Add an account to prefs which opted into using the account-storage.
-  pref_service_.Set(prefs::kAccountStoragePerAccountSettings,
-                    CreateOptedInAccountPref());
+  pref_service_.SetDict(prefs::kAccountStoragePerAccountSettings,
+                        CreateOptedInAccountPref());
 
   // SyncService is not running (because no user is signed-in).
   SetSyncStateNotSignedIn();
@@ -157,8 +157,8 @@ TEST_P(PasswordManagerFeaturesUtilTest, ShowAccountStorageResignIn) {
 TEST_F(PasswordManagerFeaturesUtilWithoutAccountStorageTest,
        ShowAccountStorageReSignin) {
   // Add an account to prefs which opted into using the account-storage.
-  pref_service_.Set(prefs::kAccountStoragePerAccountSettings,
-                    CreateOptedInAccountPref());
+  pref_service_.SetDict(prefs::kAccountStoragePerAccountSettings,
+                        CreateOptedInAccountPref());
 
   // SyncService is not running (because no user is signed-in).
   SetSyncStateNotSignedIn();
@@ -170,8 +170,8 @@ TEST_F(PasswordManagerFeaturesUtilWithoutAccountStorageTest,
 TEST_P(PasswordManagerFeaturesUtilTest,
        DontShowAccountStorageResignIn_SyncActive) {
   // Add an account to prefs which opted into using the account-storage.
-  pref_service_.Set(prefs::kAccountStoragePerAccountSettings,
-                    CreateOptedInAccountPref());
+  pref_service_.SetDict(prefs::kAccountStoragePerAccountSettings,
+                        CreateOptedInAccountPref());
 
   // SyncService is running (for a different signed-in user).
   CoreAccountInfo account;
@@ -198,8 +198,8 @@ TEST_P(PasswordManagerFeaturesUtilTest,
 TEST_P(PasswordManagerFeaturesUtilTest,
        DontShowAccountStorageResignIn_GaiaUrl) {
   // Add an account to prefs which opted into using the account-storage.
-  pref_service_.Set(prefs::kAccountStoragePerAccountSettings,
-                    CreateOptedInAccountPref());
+  pref_service_.SetDict(prefs::kAccountStoragePerAccountSettings,
+                        CreateOptedInAccountPref());
 
   // SyncService is not running (because no user is signed-in).
   SetSyncStateNotSignedIn();
