253428f5c9939eff910485cffc6fccefe383d982
Lingqi Chi <lingqi@chromium.org>
2022-07-13 06:52:51 +0000

    SearchPre*: Stop prerender after prefetch requests expire
    After prefetch requests expire, the corresponding prerenders should be
    destroyed as well, because the pages that they are loading are stale.
    
    
    This is at the fourth step:
    1. unify triggering path
    2. trigger prerender after prefetch responses succeeds
    3. * make prerender reuse the prefetched response
    3.1 manage the state machine: for now we remove the prerendered
        prefetch requests upon users click the ACMatch, which varies from
        prefetch.
    3.2 ensure timestamps are recorded correctly.
    3.4 fix some edge cases and ensure prerender requests cannot enter net
        stack.
    3.5 remove the prefetch parameter from prerender's url, as prerender
        requests no longer enter net stack.
    4. * prefetch cancels prerender when needed.
    4.1 * cancel prerender on prefetch expiration.
    5. metrics
    6. allow prerender to fallback to prefetch when prerender is canceled.
    6.1 manage the response data reader.
    
    Bug: 1295170
    Validate-Test-Flakiness: skip
    Change-Id: If324bc2b8a86940142f7c6f57c2033540f43c04f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750654
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023600}

--- chrome/browser/prefetch/search_prefetch/search_prefetch_request.cc
+++ chrome/browser/prefetch/search_prefetch/search_prefetch_request.cc
@@ -118,7 +118,9 @@ SearchPrefetchRequest::SearchPrefetchRequest(
       navigation_prefetch_(navigation_prefetch),
       report_error_callback_(std::move(report_error_callback)) {}
 
-SearchPrefetchRequest::~SearchPrefetchRequest() = default;
+SearchPrefetchRequest::~SearchPrefetchRequest() {
+  StopPrerender();
+}
 
 // static
 net::NetworkTrafficAnnotationTag
