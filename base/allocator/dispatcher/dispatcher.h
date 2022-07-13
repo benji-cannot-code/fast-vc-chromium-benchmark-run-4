f3dfc210a6c569471d516c43aea0faac34a2a123
André Kempe <andre.kempe@arm.com>
2022-07-13 10:28:03 +0000

    Isolate Allocation Hooks for PoisonAllocationSampler
    To improve developer experience when using Arm's Memory Tagging
    Extension, we want to introduce a new allocation observer for recording stack traces. Since PartitionAllocator supports only a single observer, we plan to implement a dispatcher which allows forwarding a single notification to multiple observers.
    
    This CL moves allocator dispatch functions from PoissonAllocationSampler to a new dispatcher module. This way we can reuse them in the new
    dispatcher, but also prepare the sampler.
    
    Bug: 1137393
    Change-Id: I89f1a3ae51d5e1211a420d59181019fa0cba2d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650722
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jonathan Wright <jonathan.wright@arm.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023656}

---
+++ base/allocator/dispatcher/dispatcher.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_ALLOCATOR_DISPATCHER_DISPATCHER_H_
+#define BASE_ALLOCATOR_DISPATCHER_DISPATCHER_H_
+
+#include "base/base_export.h"
+
+namespace base::allocator::dispatcher {
+
+void BASE_EXPORT InstallStandardAllocatorHooks();
+void BASE_EXPORT RemoveStandardAllocatorHooksForTesting();
+
+}  // namespace base::allocator::dispatcher
+
+#endif  // BASE_ALLOCATOR_DISPATCHER_DISPATCHER_H_
