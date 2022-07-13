1a45101a079dca8370f828db774ac283a9598971
Tim Sergeant <tsergeant@chromium.org>
2022-07-13 00:35:02 +0000

    Intents: Add ChromeOS device type to Intent Chip IPH bubble string
    On ChromeOS devices, the Intent Chip IPH bubble will refer to the
    device type by name (e.g. "your Chromebook"). Other platforms will
    continue to use the generic "your device" string.
    
    Bug: 1304529
    Change-Id: I380871d87417315d2cff5fcae7eb01d55051295d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752207
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023509}

--- chrome/browser/ui/views/user_education/feature_promo_dialog_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/feature_promo_dialog_interactive_uitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/intent_helper/intent_picker_features.h"
 #include "chrome/browser/banners/test_app_banner_manager_desktop.h"
@@ -57,6 +58,10 @@ user_education::FeaturePromoSpecification::StringReplacements
 GetReplacementsForFeature(const base::Feature& feature) {
   if (&feature == &feature_engagement::kIPHDesktopPwaInstallFeature)
     return {u"Placeholder Text"};
+#if BUILDFLAG(IS_CHROMEOS)
+  if (&feature == &feature_engagement::kIPHIntentChipFeature)
+    return {u"Chrome device"};
+#endif
   return {};
 }
 
