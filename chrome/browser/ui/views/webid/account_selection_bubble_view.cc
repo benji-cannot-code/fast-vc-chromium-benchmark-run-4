ef6d4e23930c6d0d4b21af9eea9f0a5887aa6ac7
Nicolás Peña Moreno <npm@chromium.org>
2022-07-13 20:28:31 +0000

    [FedCM] Add scroller to desktop account picker
    If the account picker has more than 2 accounts, instead show a scroller
    such that the first 2 accounts and a bit of the third are visible.
    
    Demo: https://photos.app.goo.gl/Q4A6EbVcR6DqNMEt8
    
    Bug: 1343516
    Change-Id: I64495444b4448266940f8081a16a77e5640a7035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758896
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023909}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.cc
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/scroll_view.h"
 #include "ui/views/controls/separator.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
@@ -73,6 +74,10 @@ constexpr int kRightMargin = 40;
 // The size of the space between the top boundary of the WebContents and the top
 // boundary of the bubble.
 constexpr int kTopMargin = 16;
+// The maximum height that the multi-account-picker can have. This value was
+// chosen so that if there are more than two accounts, the picker will show up
+// as a scrollbar.
+constexpr int kMaxScrollViewHeight = 150;
 
 constexpr char kImageFetcherUmaClient[] = "FedCMAccountChooser";
 
@@ -551,13 +556,17 @@ AccountSelectionBubbleView::CreateSingleAccountChooser(
 std::unique_ptr<views::View>
 AccountSelectionBubbleView::CreateMultipleAccountChooser(
     base::span<const content::IdentityRequestAccount> accounts) {
-  auto row = std::make_unique<views::View>();
+  auto scroll_view = std::make_unique<views::ScrollView>();
+  scroll_view->SetHorizontalScrollBarMode(
+      views::ScrollView::ScrollBarMode::kDisabled);
+  scroll_view->ClipHeightTo(0, kMaxScrollViewHeight);
+  views::View* row = scroll_view->SetContents(std::make_unique<views::View>());
   row->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
   for (const auto& account : accounts) {
     row->AddChildView(CreateAccountRow(account, /*should_hover=*/true));
   }
-  return row;
+  return scroll_view;
 }
 
 std::unique_ptr<views::View> AccountSelectionBubbleView::CreateAccountRow(
