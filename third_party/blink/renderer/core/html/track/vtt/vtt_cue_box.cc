c406e82db2bfe7ad6806be044ea66393fde47fa4
Kent Tamura <tkent@chromium.org>
2022-07-13 03:19:22 +0000

    VTTCue NG: Add a skeleton of VttCueLayoutAlgorithm class
    * It's called from |VttCueBoxResizeDelegate::OnResize()|.
    * Supports |To<VTTCueBox>()| by the element.h and vtt_cue_box.h changes
    
    This CL has no behavior changes.
    
    Bug: 1335309
    Change-Id: I26c1790ea6f5fe16c63ed754757b43255fccfabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759344
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023563}

--- third_party/blink/renderer/core/html/track/vtt/vtt_cue_box.cc
+++ third_party/blink/renderer/core/html/track/vtt/vtt_cue_box.cc
@@ -33,9 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/track/vtt/vtt_cue.h"
+#include "third_party/blink/renderer/core/html/track/vtt/vtt_cue_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/layout_object_factory.h"
 #include "third_party/blink/renderer/core/layout/layout_vtt_cue.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
+#include "third_party/blink/renderer/core/resize_observer/resize_observer_entry.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 
 namespace blink {
@@ -47,7 +49,7 @@ class VttCueBoxResizeDelegate final : public ResizeObserver::Delegate {
   void OnResize(
       const HeapVector<Member<ResizeObserverEntry>>& entries) override {
     DCHECK_EQ(entries.size(), 1u);
-    // TODO(crbug.com/1335309): Do adjustment here.
+    VttCueLayoutAlgorithm(*To<VTTCueBox>(entries[0]->target())).Layout();
   }
 };
 
