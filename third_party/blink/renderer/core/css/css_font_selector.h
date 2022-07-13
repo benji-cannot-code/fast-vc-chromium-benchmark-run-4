e1ef47dc0887b271e3969901ae83de7a898c5468
Xiaocheng Hu <xiaochengh@chromium.org>
2022-07-13 20:24:16 +0000

    Reland "[parallel-text-shaping] CSS changes"
    This reverts commit a2f82f37ee3c62c899778e18169ae00f0582c365.
    
    Reason for revert: Relanded with a speculative fix. See comparison
    against Patchset 1.
    
    Original change's description:
    > Revert "[parallel-text-shaping] CSS changes"
    >
    > This reverts commit 1f36b64dd81cc0c4f4443598fb09307b78c7ac84.
    >
    > Reason for revert: Caused a nullptr deref crbug.com/1335246#c23
    >
    > Original change's description:
    > > [parallel-text-shaping] CSS changes
    > >
    > > This patch changes "core/css", "platform/{fonts,testing}" to make font
    > > loading, both system and web, to work with font threads for prepration
    > > of parallel text shaping[1].
    > >
    > > This patch is split from the CL[2][3] to exclude `CSSSegmentedFontFace`
    > > and `CSSFontFace` changes to isolate memory related issues
    > >
    > >
    > > Note: `USE_PARALLEL_TEXT_SHAPING` will be defined on Win and MacOS.
    > >
    > > core/css
    > >   * Make objects thread-safe. Mostly adding locks.
    > >   * Utilize `ExecutionContext::GetTaskRunner()` to call
    > >     `FontMatchingMetrics` in owner execution context from font
    > >     threads.
    > >
    > > platform/fonts/font_selector.{cc,h}
    > >   * Make `SimpleFontData` parameter in reporting function can be cross
    > >     thread.
    > >
    > > platform/fonts/font_test_helper.cc
    > >   * Adapt for `FontSelector` changes.
    > >
    > > [1] http://crrev.com/c/3211416 Parallel Text Shaping implementation
    > > [2] http://crrev.com/c/3670315: [parallel-text-shaping] CSS changes
    > > [3] http://crrev.com/c/3703810: [parallel-text-shaping] Use
    > > Persistent<FontFaceList> in CSSSegmentedFontFace
    > >
    > > Design Doc: https://bit.ly/3GqpJLD
    > > Font slid: https://bit.ly/3JFTSHa
    > >
    > > Bug: 1264280
    > > Change-Id: Ia1dbb155a6cd7d653cd2b52e03e1945db823bf42
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708289
    > > Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    > > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > > Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1015233}
    >
    > Bug: 1264280, 1335246
    > Change-Id: I80943ab56c6c86234f857f392e95edfb9e775403
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3748643
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1021325}
    
    Bug: 1264280, 1335246
    Change-Id: Ie91cc5241d2e9755c32a9014514edc35f4fb8f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750740
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023906}

--- third_party/blink/renderer/core/css/css_font_selector.h
+++ third_party/blink/renderer/core/css/css_font_selector.h
@@ -83,6 +83,7 @@ class CORE_EXPORT CSSFontSelector : public CSSFontSelectorBase {
   void DispatchInvalidationCallbacks(FontInvalidationReason);
 
   // `CSSFontSelectorBase` overrides
+  bool IsAlive() const override;
   FontMatchingMetrics* GetFontMatchingMetrics() const override;
   UseCounter* GetUseCounter() const override;
 
