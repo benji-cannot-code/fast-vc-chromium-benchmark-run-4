8620a6d9c56082411f091cf84c2f8ae9e23ffcd9
dpapad <dpapad@chromium.org>
2022-07-13 21:17:00 +0000

    WebUI: Remove type assertions on object literals where possible.
    Per the TypeScript styleguide at [1], type assertions (aka type casts)
    on object literals are not allowed. Such violations are revealed by
    locally adding `objectLiteralTypeAssertions: 'allow-as-parameter'`
    to the @typescript-eslint/consistent-type-assertions ESLint config and
    then running `git cl presubmit --files="*.ts"`
    
    Fixing most violations in this CL, but deferring enabling the ESLint
    check for a follow-up until all violations are addressed.
    
    [1] https://google.github.io/styleguide/tsguide.html#type-assertions-and-object-literals
    
    Bug: 720034
    Change-Id: Ib8a6bf10aa4bd2d493573e6326f222be30e3e88b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755019
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023938}

--- chrome/browser/resources/settings/route.ts
+++ chrome/browser/resources/settings/route.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 
 import {pageVisibility} from './page_visibility.js';
@@ -13,7 +14,8 @@ import {SettingsRoutes} from './settings_routes.js';
  * Add all of the child routes that originate from the privacy route,
  * regardless of whether the privacy section under basic or advanced.
  */
-function addPrivacyChildRoutes(r: SettingsRoutes) {
+function addPrivacyChildRoutes(r: Partial<SettingsRoutes>) {
+  assert(r.PRIVACY);
   r.CLEAR_BROWSER_DATA = r.PRIVACY.createChild('/clearBrowserData');
   r.CLEAR_BROWSER_DATA.isNavigableDialog = true;
 
@@ -103,8 +105,8 @@ function addPrivacyChildRoutes(r: SettingsRoutes) {
 /**
  * Adds Route objects for each path.
  */
-function createBrowserSettingsRoutes(): SettingsRoutes {
-  const r = {} as SettingsRoutes;
+function createBrowserSettingsRoutes(): Partial<SettingsRoutes> {
+  const r: Partial<SettingsRoutes> = {};
 
   // Root pages.
   r.BASIC = new Route('/');
@@ -128,6 +130,7 @@ function createBrowserSettingsRoutes(): SettingsRoutes {
 
   // <if expr="not chromeos_ash">
   if (visibility.people !== false) {
+    assert(r.PEOPLE);
     r.MANAGE_PROFILE = r.PEOPLE.createChild('/manageProfile');
   }
   // </if>
@@ -222,7 +225,11 @@ function createBrowserSettingsRoutes(): SettingsRoutes {
  * @return A router with the browser settings routes.
  */
 export function buildRouter(): Router {
-  return new Router(createBrowserSettingsRoutes());
+  return new Router(createBrowserSettingsRoutes() as {
+    BASIC: Route,
+    ADVANCED: Route,
+    ABOUT: Route,
+  });
 }
 
 Router.setInstance(buildRouter());
