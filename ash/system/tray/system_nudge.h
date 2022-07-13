6ca46e9575876375627c8ee0a74fe8addb945b0a
Pi-Hsun Shih <pihsun@chromium.org>
2022-07-13 04:42:20 +0000

    Reposition system nudge on shelf alignment change
    The system nudge position depends on the shelf alignment. Add observer
    for shelf alignment so the system nudge position will be updated with
    transition animation if shelf alignment is changed while the system
    nudge shown.
    
    Video for the reposition animation: http://shortn/_yFFIulZ18G
    
    Also update the unittest to initialize an instance of SystemNudge
    instead of just testing the CalculateWidgetBound function, so this also
    test that the shelf alignment observer works. Also clean up unused
    includes.
    
    Bug: None
    Test: manually test changing shelf alignment while nudge shown
    Change-Id: I448daf75c3d61d3a9d29db4dbc70b3bee44c91c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750634
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023579}

--- ash/system/tray/system_nudge.h
+++ ash/system/tray/system_nudge.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_observer.h"
+#include "ash/shell.h"
+#include "ash/shell_observer.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/tray/system_nudge_label.h"
 #include "base/scoped_observation.h"
@@ -32,7 +34,7 @@ namespace ash {
 // Creates and manages the nudge widget and its contents view for a contextual
 // system nudge. The nudge displays an icon and a label view in a shelf-colored
 // system bubble with rounded corners.
-class ASH_EXPORT SystemNudge : public ShelfObserver {
+class ASH_EXPORT SystemNudge : public ShelfObserver, ShellObserver {
  public:
   SystemNudge(const std::string& name,
               int icon_size,
@@ -49,6 +51,10 @@ class ASH_EXPORT SystemNudge : public ShelfObserver {
   void OnAutoHideStateChanged(ShelfAutoHideState new_state) override;
   void OnHotseatStateChanged(HotseatState old_state,
                              HotseatState new_state) override;
+
+  // ShellObserver:
+  void OnShelfAlignmentChanged(aura::Window* root_window,
+                               ShelfAlignment old_alignment) override;
 
   // Displays the nudge.
   void Show();
@@ -73,17 +79,8 @@ class ASH_EXPORT SystemNudge : public ShelfObserver {
   // screen reader is enabled when the view is shown.
   virtual std::u16string GetAccessibilityText() const = 0;
 
-  // Calculates the expected bounds of nudge widget.
-  static gfx::Rect CalculateWidgetBounds(const gfx::Rect& display_bounds,
-                                         Shelf* shelf,
-                                         int nudge_width,
-                                         int nudge_height,
-                                         bool anchor_status_area);
-
  private:
   class SystemNudgeView;
-
-  friend class SystemNudgeTest;
 
   struct SystemNudgeParams {
     // The name for the widget.
@@ -116,6 +113,11 @@ class ASH_EXPORT SystemNudge : public ShelfObserver {
   SystemNudgeParams params_;
 
   base::ScopedObservation<Shelf, ShelfObserver> shelf_observation_{this};
+  base::ScopedObservation<Shell,
+                          ShellObserver,
+                          &Shell::AddShellObserver,
+                          &Shell::RemoveShellObserver>
+      shell_observation_{this};
 };
 
 }  // namespace ash
