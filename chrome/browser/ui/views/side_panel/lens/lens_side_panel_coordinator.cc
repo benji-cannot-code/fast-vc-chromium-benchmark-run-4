0adb86bde4a211bb40698432b3257c22b42d154c
Keishi Hattori <keishi@chromium.org>
2022-07-13 04:33:41 +0000

    Revert "Add lens icon to the combo box drop down."
    This reverts commit b5ea448d85bf580daaddf99666e9fc0df9d2a298.
    
    Reason for revert: broke compile step on a bunch of bots
    e.g.:
    https://ci.chromium.org/p/chrome/builders/ci/lacros-arm64-generic-chrome/196
    https://ci.chromium.org/p/chrome/builders/ci/linux-chrome/51659
    
    Original change's description:
    > Add lens icon to the combo box drop down.
    >
    > Screenshot with icon: https://screenshot.googleplex.com/BgRPi2UprWzr26h.png
    >
    > Change-Id: Ia6602fe718cd8976f7d2c401c32f285411fb894f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758919
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Anudeep Palanki <apalanki@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1023569}
    
    Change-Id: Ia85c0678c6accf009cc46f839642ef3492975660
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758168
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023574}

--- chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/scoped_observation.h"
-#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/side_panel/lens/lens_unified_side_panel_view.h"
@@ -38,7 +37,7 @@ LensSidePanelCoordinator::~LensSidePanelCoordinator() {
         ->side_panel_coordinator()
         ->RemoveSidePanelViewStateObserver(this);
   }
-}
+};
 
 void LensSidePanelCoordinator::OnSidePanelDidClose() {
   lens_side_panel_view_ = nullptr;
@@ -61,7 +60,9 @@ void LensSidePanelCoordinator::RegisterEntryAndShow(
     global_registry->Register(std::make_unique<SidePanelEntry>(
         SidePanelEntry::Id::kLens,
         l10n_util::GetStringUTF16(IDS_SIDE_PANEL_COMBO_BOX_GOOGLE_LENS_LABEL),
-        ui::ImageModel::FromVectorIcon(kGoogleLensLogoIcon, ui::kColorIcon),
+        // leaving the star icon for this CL, will change this to lens icon in a
+        // different CL.
+        ui::ImageModel::FromVectorIcon(omnibox::kStarIcon, ui::kColorIcon),
         base::BindRepeating(&LensSidePanelCoordinator::CreateLensWebView,
                             base::Unretained(this), params)));
   }
