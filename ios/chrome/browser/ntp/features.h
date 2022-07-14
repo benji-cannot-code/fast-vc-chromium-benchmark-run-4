d22ce73095aa58679b36b7744f25b5d81d52047a
Ed Chin <edchin@google.com>
2022-07-14 00:01:52 +0000

    [ios][feed] Add IsFeedBackgroundRefreshEnabled() flag
    bug: 1343695
    Change-Id: I6995f5c338cb015d77fc17d04adfeba729af1c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759506
    Auto-Submit: edchin <edchin@google.com>
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024008}

--- ios/chrome/browser/ntp/features.h
+++ ios/chrome/browser/ntp/features.h
@@ -11,11 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Feature flag to enable NTP UI pending loader blocker.
 extern const base::Feature kBlockNewTabPagePendingLoad;
 
+// Feature flag to enable feed background refresh.
+// Use IsFeedBackgroundRefreshEnabled() instead of this constant directly.
+extern const base::Feature kEnableFeedBackgroundRefresh;
+
 // Feature flag to enable the Following feed in the NTP.
 // Use IsWebChannelsEnabled() instead of this constant directly.
 extern const base::Feature kEnableWebChannels;
 
 // Whether the Following Feed is enabled on NTP.
 bool IsWebChannelsEnabled();
+
+// Whether feed background refresh is enabled.
+bool IsFeedBackgroundRefreshEnabled();
 
 #endif  // IOS_CHROME_BROWSER_NTP_FEATURES_H_
