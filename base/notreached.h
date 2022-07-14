912c20282d3ba7b8b18a2c469095e01956b14caf
Sebastien Lalancette <seblalancette@chromium.org>
2022-07-14 18:36:22 +0000

    Revert "Update NOTREACHED() macro to dump once on ash"
    This reverts commit 0877630af0544722fad407c4966a64af8c7fa2c3.
    
    Reason for revert: CheckTest.NotReached test consistently failing.
    
    First failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-rel/65871/overview
    
    Original change's description:
    > Update NOTREACHED() macro to dump once on ash
    >
    > It's not immediately obvious that ash will get DCHECK_IS_CONFIGURABLE
    > builds, so this provides some extra NOTREACHED() information for
    > non-DCHECK builds where NOTREACHED()s are not optimized out but are
    > logged anyways.
    >
    > This adds DCheckDumpOnceWithoutCrashing support to NOTREACHED() under
    > the gn arg `enable_log_error_not_reached`.
    >
    > For other release builds (including official non-DCHECK builds) this is
    > optimized away through EAT_CHECK_STREAM_PARAMS().
    >
    > notreached.cc is removed in favor of using CheckError and friends.
    > NOTREACHED() as a macro should probably move into perhaps check.h.
    >
    > Bug: 596231
    > Change-Id: I46719d638304d62bc6818cdfd8fea50d26985c7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751727
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024276}
    
    Bug: 596231
    Change-Id: Ifd268a38f53cbffa1b71d822c84072c0890c37ca
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762695
    Auto-Submit: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024338}

--- base/notreached.h
+++ base/notreached.h
@@ -13,14 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace logging {
 
-// Under these conditions NOTREACHED() will effectively either log or DCHECK.
-#if BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED) || DCHECK_IS_ON()
-#define NOTREACHED() \
-  LAZY_CHECK_STREAM( \
-      ::logging::CheckError::NotReached(__FILE__, __LINE__).stream(), true)
+#if BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED)
+void BASE_EXPORT LogErrorNotReached(const char* file, int line);
+#define NOTREACHED()                                       \
+  true ? ::logging::LogErrorNotReached(__FILE__, __LINE__) \
+       : EAT_CHECK_STREAM_PARAMS()
 #else
-#define NOTREACHED() EAT_CHECK_STREAM_PARAMS()
-#endif  // BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED) || DCHECK_IS_ON()
+#define NOTREACHED() DCHECK(false)
+#endif
 
 // The NOTIMPLEMENTED() macro annotates codepaths which have not been
 // implemented yet. If output spam is a serious concern,
