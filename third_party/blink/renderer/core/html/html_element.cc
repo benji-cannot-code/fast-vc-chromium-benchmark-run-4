169d6bc641d1ce012787c944482ceca31d8b5832
Theodore Olsauskas-Warren <sauski@google.com>
2022-07-14 09:11:51 +0000

    Revert "Add the beginning of hover-trigger for pop-ups"
    This reverts commit cdbdb4520d6be65fdb04fc85aea5d12953f1a1da.
    
    Reason for revert: Suspected cause of 1344436
    
    Original change's description:
    > Add the beginning of hover-trigger for pop-ups
    >
    > This CL adds the 'hoverpopup' attribute, to go along with the other
    > invoking attributes (togglepopup, showpopup, hidepopup). This attribute,
    > when places on *any* element, triggers a pop-up when hovered. With this
    > CL, it is always triggered after a fixed 100ms delay, but that will be
    > made configurable via CSS in subsequent CLs. There is also no "hide"
    > trigger when any element is de-hovered - again that will be added
    > later.
    >
    > See this spec discussion:
    >  https://github.com/openui/open-ui/issues/526#issuecomment-1161917067
    >
    > Bug: 1307772
    > Change-Id: I7af88dad9efa015f833843ea76bed41b4aa42c4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759184
    > Reviewed-by: David Baron <dbaron@chromium.org>
    > Auto-Submit: Mason Freed <masonf@chromium.org>
    > Commit-Queue: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024141}
    
    Bug: 1307772, 1344436
    Change-Id: I65a1e36ead974e887c37b6a90ff6504fdaa45d71
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760676
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Owners-Override: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024173}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/serializers/serialization.h"
 #include "third_party/blink/renderer/core/editing/spellcheck/spell_checker.h"
-#include "third_party/blink/renderer/core/event_type_names.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -444,23 +443,20 @@ AttributeTriggers* HTMLElement::TriggersForAttributeName(
   static AttributeTriggers attribute_triggers[] = {
       {html_names::kDirAttr, kNoWebFeature, kNoEvent,
        &HTMLElement::OnDirAttrChanged},
+      {html_names::kFocusgroupAttr, kNoWebFeature, kNoEvent,
+       &HTMLElement::OnFocusgroupAttrChanged},
       {html_names::kFormAttr, kNoWebFeature, kNoEvent,
        &HTMLElement::OnFormAttrChanged},
       {html_names::kLangAttr, kNoWebFeature, kNoEvent,
        &HTMLElement::OnLangAttrChanged},
       {html_names::kNonceAttr, kNoWebFeature, kNoEvent,
        &HTMLElement::OnNonceAttrChanged},
-
-      {html_names::kFocusgroupAttr, kNoWebFeature, kNoEvent,
-       &HTMLElement::ReparseAttribute},
       {html_names::kTabindexAttr, kNoWebFeature, kNoEvent,
-       &HTMLElement::ReparseAttribute},
+       &HTMLElement::OnTabIndexAttrChanged},
       {xml_names::kLangAttr, kNoWebFeature, kNoEvent,
-       &HTMLElement::ReparseAttribute},
+       &HTMLElement::OnXMLLangAttrChanged},
       {html_names::kPopupAttr, kNoWebFeature, kNoEvent,
-       &HTMLElement::ReparseAttribute},
-      {html_names::kHoverpopupAttr, kNoWebFeature, kNoEvent,
-       &HTMLElement::ReparseAttribute},
+       &HTMLElement::OnPopupAttrChanged},
 
       {html_names::kOnabortAttr, kNoWebFeature, event_type_names::kAbort,
        nullptr},
@@ -2026,7 +2022,8 @@ void HTMLElement::OnDirAttrChanged(const AttributeModificationParams& params) {
   }
 }
 
-void HTMLElement::ReparseAttribute(const AttributeModificationParams& params) {
+void HTMLElement::OnFocusgroupAttrChanged(
+    const AttributeModificationParams& params) {
   Element::ParseAttribute(params);
 }
 
@@ -2043,6 +2040,21 @@ void HTMLElement::OnNonceAttrChanged(
     const AttributeModificationParams& params) {
   if (params.new_value != g_empty_atom)
     setNonce(params.new_value);
+}
+
+void HTMLElement::OnTabIndexAttrChanged(
+    const AttributeModificationParams& params) {
+  Element::ParseAttribute(params);
+}
+
+void HTMLElement::OnXMLLangAttrChanged(
+    const AttributeModificationParams& params) {
+  Element::ParseAttribute(params);
+}
+
+void HTMLElement::OnPopupAttrChanged(
+    const AttributeModificationParams& params) {
+  Element::ParseAttribute(params);
 }
 
 ElementInternals* HTMLElement::attachInternals(
