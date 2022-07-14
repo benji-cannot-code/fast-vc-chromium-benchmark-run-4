ead86a9e623a3ce82ed22adb1458e391b6cb9a12
James Shen <zhiyuans@google.com>
2022-07-14 00:15:09 +0000

    Disable telemetry_perf_unittests on android-nougat-x86-rel.
    telemetry_perf_unittests contain the same failed tests and more.
    
    This test suite keeps failing on android-nougat-x86-rel. Re-enable them once the failures are fixed.
    
    Disabled test list:
    benchmarks.blink_perf_unittest.BlinkPerfTest.testBlinkPerfTracingMetricsForMeasureFrameTime
    core.benchmark_runner_test.BenchmarkRunnerIntegrationTest.testTimelineBasedEndToEnd
    core.minidump_unittest.BrowserMinidumpTest.testMinidumpFromRendererHang
    core.minidump_unittest.BrowserMinidumpTest.testMultipleCrashMinidumps
    core.minidump_unittest.BrowserMinidumpTest.testSymbolizeMinidump
    core.perf_benchmark_unittest.PerfBenchmarkTest.testVariationArgs
    core.stacktrace_unittest.TabStackTraceTest.testValidDump
    measurements.rasterize_and_record_micro_unittest.RasterizeAndRecordMicroUnitTest.testRasterizeAndRecordMicro
    
    Bug: 1342706, 1303439
    Change-Id: I390a060a5f6e08b3688e71b9037d8137f1c56f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760078
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: James Shen <zhiyuans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024012}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -65,7 +65,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'win7'  # https://crbug.com/1084931
+      'win7',  # https://crbug.com/1084931
+      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testSymbolizeMinidump(self):
     # Wait for the browser to restart fully before crashing
@@ -110,7 +111,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'win7'  # https://crbug.com/1084931
+      'win7',  # https://crbug.com/1084931
+      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testMultipleCrashMinidumps(self):
     # Wait for the browser to restart fully before crashing
@@ -202,7 +204,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'win7'  # https://crbug.com/1084931
+      'win7',  # https://crbug.com/1084931
+      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testMinidumpFromRendererHang(self):
     """Tests that renderer hangs result in minidumps.
