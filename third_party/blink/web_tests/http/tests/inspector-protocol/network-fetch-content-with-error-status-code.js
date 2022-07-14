495f4fd36aa466099225bff787449b59deb3456f
Danil Somsikov <dsv@chromium.org>
2022-07-14 14:34:23 +0000

    Follow up to the crrev.com/c/3757730: Wait for both loadingFinished events and remove failure test expectations.
    Bug: 1228246
    Change-Id: I2e2672bc5abceb5deb16c3e222d3f13f3353662d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762491
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024243}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network-fetch-content-with-error-status-code.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network-fetch-content-with-error-status-code.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const gotAllEvents = new Promise((resolve) => {
     let eventHandler = (event) => {
       events.push(event);
-      if (events.length == 9) {
+      if (events.length == 10) {
         resolve();
       }
     };
