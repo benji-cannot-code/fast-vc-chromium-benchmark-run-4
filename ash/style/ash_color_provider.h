6300d26639a094ec15ba26d01cfcea084840b43e
Sean Kau <skau@chromium.org>
2022-07-14 18:57:26 +0000

    Replace colors for ControlsLayer with ColorProvider lookup
    As we move AshColorProvider to depend on ui::ColorProvider,
    we need the ColorMixers to not depend on AshColorProvider. This
    replaces the mappings for ControlLayer colors.
    
    Bug: chromium:1334850
    Change-Id: I09332038af23a05805cc48b88d99522819f349a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3703758
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024353}

--- ash/style/ash_color_provider.h
+++ ash/style/ash_color_provider.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/color_palette.h"
 
+namespace ui {
+class ColorProvider;
+}
+
 namespace ash {
 
 // TODO(minch): AshColorProvider is not needed to be a class now.
@@ -78,8 +82,7 @@ class ASH_EXPORT AshColorProvider : public ColorProvider {
   SkColor GetBaseLayerColorImpl(BaseLayerType type, bool inverted) const;
   // Gets the color of |type| of the corresponding layer. Returns the color on
   // dark mode if |use_dark_color| is true.
-  SkColor GetControlsLayerColorImpl(ControlsLayerType type,
-                                    bool use_dark_color) const;
+  SkColor GetControlsLayerColorImpl(ControlsLayerType type) const;
   SkColor GetContentLayerColorImpl(ContentLayerType type,
                                    bool use_dark_color) const;
 
@@ -95,6 +98,10 @@ class ASH_EXPORT AshColorProvider : public ColorProvider {
   // dark mode if |use_dark_color| is true.
   SkColor GetBackgroundThemedColorImpl(SkColor default_color,
                                        bool use_dark_color) const;
+
+  // Returns a ColorProvider for the current NativeTheme which will correctly
+  // reflect the current ColorMode.
+  ui::ColorProvider* GetColorProvider() const;
 };
 
 }  // namespace ash
