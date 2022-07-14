9f481eb8a46ebdd60afca6b7f3f00b5683b84176
Stephan Hartmann <stha09@googlemail.com>
2022-07-14 14:04:34 +0000

    IWYU: add cmath for std::sqrt in UnivariateStats::CalculateStats()
    Bug: 957519
    Change-Id: Ia5cc7dd81def04d291a92d39caea07099a2f41c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762449
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1024234}

--- components/page_load_metrics/browser/observers/ad_metrics/univariate_stats.cc
+++ components/page_load_metrics/browser/observers/ad_metrics/univariate_stats.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/page_load_metrics/browser/observers/ad_metrics/univariate_stats.h"
 
+#include <cmath>
+
 #include "base/check_op.h"
 
 namespace page_load_metrics {
