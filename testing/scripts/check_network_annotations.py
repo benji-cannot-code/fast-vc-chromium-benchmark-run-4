640434d334eff18de0401a55a05f3181b9737126
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-07-14 19:58:40 +0000

    [Traffic Annotation] Supplement developer documentation
    An offline conversation revealed that the instructions on running
    auditor.py can be hard to find from just CQ output (which runs
    check_network_annotations.py).
    
    Add a comment in check_network_annotations.py telling contributors
    where they can find instructions on running auditor.py locally.
    
    Also, update the README to include the "fast" way to run the auditor
    (building the `traffic_annotation_proto` target rather than the `chrome`
    target).
    
    Change-Id: I214ea41ae45c58ce39b55f15dbaaa2ef839ee320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738258
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024385}

--- testing/scripts/check_network_annotations.py
+++ testing/scripts/check_network_annotations.py
@@ -7,7 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """//testing/scripts wrapper for the network traffic annotations checks.
 This script is used to run check_annotations.py on the trybots to ensure that
 all network traffic annotations have correct syntax and semantics, and all
-functions requiring annotations have one."""
+functions requiring annotations have one.
+
+This is a wrapper around tools/traffic_annotation/scripts/auditor.py.
+
+See tools/traffic_annotation/scripts/auditor/README.md for instructions on
+running locally."""
 
 import json
 import os
