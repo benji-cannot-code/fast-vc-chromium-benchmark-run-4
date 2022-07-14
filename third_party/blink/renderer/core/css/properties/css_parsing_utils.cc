ee0cae8667b7e67038f3675b8ce0c03ef0bbe810
Eric Willigers <ericwilligers@chromium.org>
2022-07-14 14:45:02 +0000

    Always support contain-intrinsic-size: auto
    The runtime-enabled feature ContainIntrinsicSizeAuto has been stable
    since 2021 and is no longer needed.
    
    Bug: 1199460
    Change-Id: I6d82a5388e3e1aa619daab13897fc185d7276737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759800
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024245}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -2514,9 +2514,7 @@ CSSValue* ConsumeIntrinsicSizeLonghandNew(CSSParserTokenRange& range,
 
 CSSValue* ConsumeIntrinsicSizeLonghand(CSSParserTokenRange& range,
                                        const CSSParserContext& context) {
-  if (RuntimeEnabledFeatures::ContainIntrinsicSizeAutoEnabled())
-    return ConsumeIntrinsicSizeLonghandNew(range, context);
-  return ConsumeIntrinsicSizeLonghandOld(range, context);
+  return ConsumeIntrinsicSizeLonghandNew(range, context);
 }
 
 static CSSValue* ConsumeCrossFade(CSSParserTokenRange& args,
