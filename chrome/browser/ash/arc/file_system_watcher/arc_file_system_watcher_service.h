101842cdc97084c1e5c98a010a73f8782458948b
Youkichi Hosoi <youkichihosoi@chromium.org>
2022-07-14 15:09:02 +0000

    arc: Ensure to send mount events after file system watchers are ready
    Call VolumeMounterInstance::OnMountEvent() only after file system
    watchers are ready to watch file system changes so that every file in
    MyFiles and removable media is indexed in Android's MediaStore.
    
    Bug: b:182877037
    Test: arc.MyFiles*, arc.Downloads*, arc.RemovableMedia*
    Change-Id: Ie5ad6ebfb4903d4120565edf43c33da6b2231196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752247
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024249}

--- chrome/browser/ash/arc/file_system_watcher/arc_file_system_watcher_service.h
+++ chrome/browser/ash/arc/file_system_watcher/arc_file_system_watcher_service.h
@@ -58,17 +58,19 @@ class ArcFileSystemWatcherService
   void OnConnectionClosed() override;
 
   // ArcVolumeMounterBridge::Delegate overrides.
+  bool IsWatchingFileSystemChanges() override;
   void StartWatchingRemovableMedia(const std::string& fs_uuid,
                                    const std::string& mount_path,
                                    base::OnceClosure callback) override;
-
   void StopWatchingRemovableMedia(const std::string& mount_path) override;
 
  private:
   class FileSystemWatcher;
 
   void StartWatchingFileSystem();
-  void StopWatchingFileSystem(base::OnceClosure);
+  void StopWatchingFileSystem();
+
+  void OnMyFilesWatcherStarted();
 
   void TriggerSendAllMountEvents() const;
 
@@ -80,6 +82,8 @@ class ArcFileSystemWatcherService
 
   content::BrowserContext* const context_;
   ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
+
+  bool watching_file_system_changes_ = false;
 
   std::unique_ptr<FileSystemWatcher> myfiles_watcher_;
   // A map from mount path to watcher.
