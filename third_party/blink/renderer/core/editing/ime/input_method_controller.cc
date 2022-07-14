67158268c5181132376c14f055caa8f5ce2050a5
Eric Willigers <ericwilligers@chromium.org>
2022-07-14 14:16:11 +0000

    Retire flag EnterKeyHintAttribute
    Enter Key Hint shipped in M77.
    
    Bug: 634330
    Change-Id: Ib9d40ffb693f9a24d2908dc2d403186da8c3a01e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762117
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024236}

--- third_party/blink/renderer/core/editing/ime/input_method_controller.cc
+++ third_party/blink/renderer/core/editing/ime/input_method_controller.cc
@@ -1687,9 +1687,6 @@ int InputMethodController::ComputeWebTextInputNextPreviousFlags() const {
 }
 
 ui::TextInputAction InputMethodController::InputActionOfFocusedElement() const {
-  if (!RuntimeEnabledFeatures::EnterKeyHintAttributeEnabled())
-    return ui::TextInputAction::kDefault;
-
   AtomicString action =
       GetEnterKeyHintAttribute(GetDocument().FocusedElement());
 
