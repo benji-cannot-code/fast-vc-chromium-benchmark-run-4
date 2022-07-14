efe34cd0d8fb824e5d87157b99c7078deee6e9ea
Chris Lu <thegreenfrog@chromium.org>
2022-07-14 17:41:15 +0000

    Revert "[ios] Shrink MVTiles/Shortcuts text if they can just fit into one row"
    This reverts commit c7ead0501f0d0acaade3d67886af4391974708d4.
    
    Reason for revert: Undoing feature 
    
    Original change's description:
    > [ios] Shrink MVTiles/Shortcuts text if they can just fit into one row
    >
    > When ContentSuggestionsUIModuleRefresh is enabled, the width allocated
    > to the caption is not as much, so shrink the font size if it can just
    > barely fit one row instead of breaking up a word.
    >
    > Bug: 1342616
    > Change-Id: I8a5101fba0314df5861ed7e75836582c5e13c185
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753370
    > Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1022686}
    
    Bug: 1342616
    Change-Id: I932f7cabb0f724a45b0c27c2358969be537c984a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763766
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024305}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_tile_view.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_tile_view.mm
@@ -43,10 +43,6 @@ const CGFloat kPreferredMaxWidth = 73;
     _titleLabel.textAlignment = NSTextAlignmentCenter;
     _titleLabel.preferredMaxLayoutWidth = kPreferredMaxWidth;
     _titleLabel.numberOfLines = kLabelNumLines;
-    if (IsContentSuggestionsUIModuleRefreshEnabled()) {
-      _titleLabel.adjustsFontSizeToFitWidth = YES;
-      _titleLabel.minimumScaleFactor = 0.7;
-    }
 
     _imageContainerView = [[UIView alloc] init];
     _titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
