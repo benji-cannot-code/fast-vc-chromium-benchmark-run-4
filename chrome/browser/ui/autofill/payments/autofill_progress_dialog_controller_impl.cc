6b173aa8bb648820f190917de6b1341fef35f840
Yi An <yiian@google.com>
2022-07-14 18:35:17 +0000

    Pass Type into Progress Dialog Metrics
    We introduced a new dialog type(kAndroidFIDOProgressDialog) in the Autofill Progress Dialog and this CL is to accommodate the new type in metrics.
    
    Bug: 1261529
    Change-Id: I64343961d8025abde68b93d5a860615dde9a5e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3741426
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Yi An <yiian@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024336}

--- chrome/browser/ui/autofill/payments/autofill_progress_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/autofill_progress_dialog_controller_impl.cc
@@ -36,9 +36,7 @@ void AutofillProgressDialogControllerImpl::ShowDialog(
       AutofillProgressDialogView::CreateAndShow(this);
 
   if (autofill_progress_dialog_view_)
-    // TODO(crbug.com/1261529): Pass in the flow type once we have another use
-    // case so that the progress dialog can be shared across multiple use cases.
-    AutofillMetrics::LogProgressDialogShown();
+    AutofillMetrics::LogProgressDialogShown(autofill_progress_dialog_type);
 }
 
 void AutofillProgressDialogControllerImpl::DismissDialog(
@@ -55,12 +53,11 @@ void AutofillProgressDialogControllerImpl::OnDismissed(
     bool is_canceled_by_user) {
   // Dialog is being dismissed so set the pointer to nullptr.
   autofill_progress_dialog_view_ = nullptr;
-  autofill_progress_dialog_type_ = AutofillProgressDialogType::kUnspecified;
   if (is_canceled_by_user)
     std::move(cancel_callback_).Run();
-  // TODO(crbug.com/1261529): Pass in the flow type once we have another use
-  // case so that the progress dialog can be shared across multiple use cases.
-  AutofillMetrics::LogProgressDialogResultMetric(is_canceled_by_user);
+  AutofillMetrics::LogProgressDialogResultMetric(
+      is_canceled_by_user, autofill_progress_dialog_type_);
+  autofill_progress_dialog_type_ = AutofillProgressDialogType::kUnspecified;
   cancel_callback_.Reset();
 }
 
--- components/autofill/core/browser/metrics/autofill_metrics.h
+++ components/autofill/core/browser/metrics/autofill_metrics.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_profile_import_process.h"
+#include "components/autofill/core/browser/autofill_progress_dialog_type.h"
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -1459,8 +1460,11 @@ class AutofillMetrics {
   static void LogOfferNotificationInfoBarResultMetric(
       OfferNotificationInfoBarResultMetric metric);
   static void LogOfferNotificationInfoBarShown();
-  static void LogProgressDialogResultMetric(bool is_canceled_by_user);
-  static void LogProgressDialogShown();
+  static void LogProgressDialogResultMetric(
+      bool is_canceled_by_user,
+      AutofillProgressDialogType autofill_progress_dialog_type);
+  static void LogProgressDialogShown(
+      AutofillProgressDialogType autofill_progress_dialog_type);
   static void LogVirtualCardManualFallbackBubbleShown(bool is_reshow);
   static void LogVirtualCardManualFallbackBubbleResultMetric(
       VirtualCardManualFallbackBubbleResultMetric metric,
