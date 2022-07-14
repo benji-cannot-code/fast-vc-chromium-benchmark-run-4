db22fe7e3c9f633fc62331450caf48580e5c8678
Juliet Levesque <julietlevesque@google.com>
2022-07-14 17:11:21 +0000

    [Fast Pair] Opt in user when they elect to pair a device
    If under the loose interpretation of opt-in status, the Saved Device
    subfeature will opt-in the user whenever they elect to pair a device.
    This matches the Android implementation of Fast Pair.
    
    Fixed: b/235608206
    Test: verified on DUT
    Change-Id: I683cd63178e41ad6484c8b063957e6e7f96b49e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751845
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024295}

--- ash/quick_pair/repository/fake_fast_pair_repository.cc
+++ ash/quick_pair/repository/fake_fast_pair_repository.cc
@@ -92,6 +92,10 @@ void FakeFastPairRepository::SetOptInStatus(
   status_ = status;
 }
 
+nearby::fastpair::OptInStatus FakeFastPairRepository::GetOptInStatus() {
+  return status_;
+}
+
 // Unimplemented.
 void FakeFastPairRepository::CheckOptInStatus(
     CheckOptInStatusCallback callback) {
@@ -114,10 +118,12 @@ void FakeFastPairRepository::DeleteAssociatedDeviceByAccountKey(
   std::move(callback).Run(/*success=*/false);
 }
 
-// Unimplemented.
 void FakeFastPairRepository::UpdateOptInStatus(
     nearby::fastpair::OptInStatus opt_in_status,
-    UpdateOptInStatusCallback callback) {}
+    UpdateOptInStatusCallback callback) {
+  status_ = opt_in_status;
+  std::move(callback).Run(/*success=*/true);
+}
 
 // Unimplemented.
 void FakeFastPairRepository::FetchDeviceImages(scoped_refptr<Device> device) {
--- ash/quick_pair/repository/fake_fast_pair_repository.h
+++ ash/quick_pair/repository/fake_fast_pair_repository.h
@@ -55,6 +55,7 @@ class FakeFastPairRepository : public FastPairRepository {
   }
 
   void SetOptInStatus(nearby::fastpair::OptInStatus status);
+  nearby::fastpair::OptInStatus GetOptInStatus();
 
   void SetSavedDevices(nearby::fastpair::OptInStatus status,
                        std::vector<nearby::fastpair::FastPairDevice> devices);
