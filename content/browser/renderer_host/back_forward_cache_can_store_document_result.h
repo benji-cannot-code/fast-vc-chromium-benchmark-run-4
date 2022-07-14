d5f2d6a7f3c99439de02b47609542946e4cde7e8
Yuzu Saijo <yuzus@chromium.org>
2022-07-14 04:20:04 +0000

    [bfcache] Add a flag to evict on ax events
    This CL adds a flag to evict bfcache entries on accessibility events.
    This is necessary because we would like to keep enabling bfcache for
    screen readers, but evict whenever AX events happen.
    
    This CL is restoring what we removed in the CL below with a flag.
    https://chromium-review.googlesource.com/c/chromium/src/+/3527935
    
    Since the crash is only increasing on Android with bfcache expriment,
    we enable this flag only on Android.
    
    Bug: 1341975, 1341507
    Change-Id: Ie3948d9d99c2e65fe3f522a05475930f5bb77ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744532
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024108}

--- content/browser/renderer_host/back_forward_cache_can_store_document_result.h
+++ content/browser/renderer_host/back_forward_cache_can_store_document_result.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
+#include "ui/accessibility/ax_event.h"
 
 namespace content {
 
@@ -61,6 +62,8 @@ class CONTENT_EXPORT BackForwardCacheCanStoreDocumentResult {
   void NoDueToDisableForRenderFrameHostCalled(
       const std::set<BackForwardCache::DisabledReason>& reasons);
   void NoDueToDisallowActivation(uint64_t reason);
+  // TODO(crbug.com/1341507): Remove this function.
+  void NoDueToAXEvents(const std::vector<ui::AXEvent>& events);
 
   // The conditions for storing and restoring the pages are different in that
   // pages with cache-control:no-store can enter back/forward cache depending on
@@ -88,6 +91,8 @@ class CONTENT_EXPORT BackForwardCacheCanStoreDocumentResult {
     return disallow_activation_reasons_;
   }
 
+  const std::set<ax::mojom::Event>& ax_events() const { return ax_events_; }
+
   std::string ToString() const;
 
   void WriteIntoTrace(
@@ -105,6 +110,8 @@ class CONTENT_EXPORT BackForwardCacheCanStoreDocumentResult {
   std::set<BackForwardCache::DisabledReason> disabled_reasons_;
   absl::optional<ShouldSwapBrowsingInstance> browsing_instance_swap_result_;
   std::set<uint64_t> disallow_activation_reasons_;
+  // The list of the accessibility events that made the page bfcache ineligible.
+  std::set<ax::mojom::Event> ax_events_;
 };
 
 }  // namespace content
