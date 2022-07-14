0d55272aeff4b84042745eefe737e26d335619e2
Khalid Peer <khalidpeer@chromium.org>
2022-07-14 01:30:28 +0000

    [omnibox] Add want_asynchronous_matches checks to various providers
    Although the AutocompleteController currently assumes that all
    autocomplete providers will make proper use of
    want_asynchronous_matches() to gate the usage of any asynchronous logic,
    it turns out that ZeroSuggestProvider, ClipboardProvider, and
    MostVisitedSitesProvider actually do not employ any gating based on the
    value of want_asynchronous_matches() at this time, despite using
    asynchronous logic as part of their implementation.
    
    As part of a more general initiative to clean up usage of
    want_asynchronous_matches(), this CL adds the necessary checks to the
    various autocomplete providers listed above to ensure consistent usage
    of this function.
    
    Bug: 1339425
    Change-Id: I55007655e5718babfb264dcfbc9978a217e481c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751948
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024048}

--- components/omnibox/browser/most_visited_sites_provider_unittest.cc
+++ components/omnibox/browser/most_visited_sites_provider_unittest.cc
@@ -144,6 +144,7 @@ class MostVisitedSitesProviderTest : public testing::Test,
 };
 
 size_t MostVisitedSitesProviderTest::NumMostVisitedMatches() {
+  // TODO(khalidpeer): Update to make direct use of the provider's matches.
   const auto& result = controller_->result();
   size_t count = 0;
   for (const auto& match : result) {
@@ -158,6 +159,7 @@ size_t MostVisitedSitesProviderTest::NumMostVisitedMatches() {
 const AutocompleteMatch* MostVisitedSitesProviderTest::GetMatch(
     AutocompleteMatchType::Type type,
     size_t index) {
+  // TODO(khalidpeer): Update to make direct use of the provider's matches.
   const auto& result = controller_->result();
   for (const auto& match : result) {
     if (match.type == type) {
@@ -176,6 +178,7 @@ void MostVisitedSitesProviderTest::CheckMatchesEquivalentTo(
   // Note that additional matches may be offered if other providers are also
   // registered in the same category as MostVisitedSitesProvider.
   // We ignore all matches that are not ours.
+  // TODO(khalidpeer): Update to make direct use of the provider's matches.
   const auto& result = controller_->result();
 
   size_t match_index = 0;
@@ -227,7 +230,7 @@ void MostVisitedSitesProviderTest::SetUp() {
   controller_ = std::make_unique<AutocompleteController>(
       std::move(client), AutocompleteProvider::TYPE_MOST_VISITED_SITES);
 
-  // Inject a few URLs to
+  // Inject a few URLs to test MostVisitedSitesProvider behavior.
   std::array<history::MostVisitedURL, 5> test_data{{
       {GURL("http://www.a.art/"), u"A art"},
       {GURL("http://www.b.biz/"), u"B biz"},
@@ -262,12 +265,23 @@ TEST_F(MostVisitedSitesProviderWithMatchesTest, TestMostVisitedCallback) {
 
   controller_->Start(input);
   controller_->Stop(false);
-  EXPECT_EQ(0u, NumMostVisitedMatches());
+  // Since this provider's async logic is still in-flight (`EmitURLs()` has not
+  // been called yet), the AutocompleteController will transfer old matches from
+  // the previous provider run.
+  CheckMatchesEquivalentTo(top_sites_->urls(), /* expect_tiles=*/false);
+
+  history::MostVisitedURLList old_urls = top_sites_->urls();
+  // Update the list of top sites so that we can clearly identify when
+  // matches have been transferred from the previous provider run.
+  std::array<history::MostVisitedURL, 1> new_urls{{
+      {GURL("http://www.g.gov/"), u"G gov"},
+  }};
+  top_sites_->urls().assign(new_urls.begin(), new_urls.end());
 
   // Most visited results arriving after Stop() has been called, ensure they
   // are not displayed.
   EXPECT_TRUE(top_sites_->EmitURLs());
-  EXPECT_EQ(0u, NumMostVisitedMatches());
+  CheckMatchesEquivalentTo(old_urls, /* expect_tiles=*/false);
 
   controller_->Start(input);
   controller_->Stop(false);
@@ -275,11 +289,11 @@ TEST_F(MostVisitedSitesProviderWithMatchesTest, TestMostVisitedCallback) {
 
   // Stale results should get rejected.
   EXPECT_TRUE(top_sites_->EmitURLs());
-  EXPECT_EQ(0u, NumMostVisitedMatches());
+  CheckMatchesEquivalentTo(old_urls, /* expect_tiles=*/false);
 
   // Results for the second Start() action should be recorded.
   EXPECT_TRUE(top_sites_->EmitURLs());
-  EXPECT_EQ(top_sites_->urls().size(), NumMostVisitedMatches());
+  CheckMatchesEquivalentTo(top_sites_->urls(), /* expect_tiles=*/false);
   controller_->Stop(false);
 }
 
@@ -358,6 +372,20 @@ TEST_P(ParameterizedMostVisitedSitesProviderTest,
   EXPECT_EQ(0u, NumMostVisitedMatches());
   // Accept only direct TopSites data, confirm no matches are built.
   EXPECT_TRUE(top_sites_->EmitURLs());
+  EXPECT_EQ(0u, NumMostVisitedMatches());
+}
+
+TEST_P(ParameterizedMostVisitedSitesProviderTest,
+       NoMatchesWhenTopSitesNotLoadedAndWantAsyncMatchesFalse) {
+  // Assume that top sites list has not been loaded yet from the DB.
+  ASSERT_FALSE(top_sites_->loaded());
+  auto input = BuildAutocompleteInputForWebOnFocus();
+  input.set_want_asynchronous_matches(false);
+  controller_->Start(input);
+  EXPECT_TRUE(provider_->done());
+  EXPECT_EQ(0u, NumMostVisitedMatches());
+  // No callbacks should have been added due to early return.
+  EXPECT_FALSE(top_sites_->EmitURLs());
   EXPECT_EQ(0u, NumMostVisitedMatches());
 }
 
