6db4fdd4e9bb75dac722ac62691320ba42d3d34d
Thomas Lukaszewicz <tluk@chromium.org>
2022-07-14 17:04:38 +0000

    [side search] Add helper to check for browser support
    This CL adds a utility to test side search browser support. This
    avoids ugly future refactors if the conditions for side search
    support ever change and keeps things more readable.
    
    Bug: None
    Change-Id: I4070f18e6faa745f4fc59716166bd0c1981ec159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763163
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024292}

--- chrome/browser/ui/side_search/side_search_utils.cc
+++ chrome/browser/ui/side_search/side_search_utils.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sessions/session_service_factory.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/side_search/side_search_prefs.h"
 #include "chrome/browser/ui/side_search/side_search_tab_contents_helper.h"
 #include "chrome/browser/ui/side_search/side_search_tab_data.pb.h"
@@ -113,6 +114,10 @@ bool IsSidePanelWebContents(content::WebContents* web_contents) {
 bool IsDSESupportEnabled(const Profile* profile) {
   return base::FeatureList::IsEnabled(features::kSideSearchDSESupport) &&
          IsSideSearchEnabled(profile);
+}
+
+bool IsEnabledForBrowser(const Browser* browser) {
+  return IsSideSearchEnabled(browser->profile()) && browser->is_type_normal();
 }
 
 }  // namespace side_search
--- chrome/browser/ui/side_search/side_search_utils.h
+++ chrome/browser/ui/side_search/side_search_utils.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
+class Browser;
 class Profile;
 
 namespace content {
@@ -45,6 +46,9 @@ bool IsSidePanelWebContents(content::WebContents* web_contents);
 // Returns true if side search with DSE support for participating search engines
 // is enabled.
 bool IsDSESupportEnabled(const Profile* profile);
+
+// Returns true if side search is enabled and is supported for `browser`.
+bool IsEnabledForBrowser(const Browser* browser);
 
 }  // namespace side_search
 
