8f109f421a519e0167d5568a00fc4f4971821f00
Max Curran <curranmax@chromium.org>
2022-07-15 17:40:04 +0000

    Correct logic of Retry-After check in PrefetchService.
    Bug: 1299059
    Change-Id: Idd6255f6d063fc4610a2e8f12214a4aebe0be5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763595
    Auto-Submit: Max Curran <curranmax@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024822}

--- content/browser/preloading/prefetch/prefetch_service.cc
+++ content/browser/preloading/prefetch/prefetch_service.cc
@@ -299,7 +299,7 @@ void PrefetchService::CheckEligibilityOfPrefetch(
 
   // If we have recently received a "retry-after" for the origin, then don't
   // send new prefetches.
-  if (delegate_ && delegate_->IsOriginOutsideRetryAfterWindow(
+  if (delegate_ && !delegate_->IsOriginOutsideRetryAfterWindow(
                        prefetch_container->GetURL())) {
     std::move(result_callback)
         .Run(prefetch_container, false,
