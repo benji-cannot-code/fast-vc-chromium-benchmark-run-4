87200238f465cf9ce0b4912f643f1c3060bcbe49
Reilly Grant <reillyg@chromium.org>
2022-07-15 00:57:19 +0000

    geolocation: Add metric for WiFi scanning time
    Making a network location request requires first collecting information
    about nearby access points. On Windows, macOS and Linux this is done
    with a blocking call to the operating system and this new metric will
    help us understand what sort of delay this introduces before a
    location fix is available.
    
    Bug: 1344182
    Change-Id: I9bcb08f82951699495fbae63141c711386aefcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764307
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024517}

--- services/device/geolocation/wifi_data_provider_common.cc
+++ services/device/geolocation/wifi_data_provider_common.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/location.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
@@ -67,6 +68,9 @@ void WifiDataProviderCommon::DoWifiScanTask() {
   // Abort the wifi scan if the provider is already being torn down.
   if (!wlan_api_)
     return;
+
+  SCOPED_UMA_HISTOGRAM_TIMER(
+      "Geolocation.WifiDataProviderCommon.WifiScanTaskTime");
 
   bool update_available = false;
   WifiData new_data;
