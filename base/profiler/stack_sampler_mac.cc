3e2e6025255fdf39a65cfad2ba75838ccfcc4dd5
Ian Barkley-Yeung <iby@chromium.org>
2022-07-15 02:52:19 +0000

    Reland "Initial implementation of ChromeOS X86_64 stack walker"
    This reverts commit 9acb188e0cfad0cb12bd7e467c5e08f6832f827e.
    
    Reason for revert: Disable failing tests under MSan. Tests try to copy
    and examine uninitialized parts of the stack; I don't think MSan will
    ever be happy with these tests.
    
    Original change's description:
    > Revert "Initial implementation of ChromeOS X86_64 stack walker"
    >
    > This reverts commit a1d168747ed75f8b13feb7d19982cc14eeb66a4a.
    >
    > Reason for revert: Suspected culprit for https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/29103/blamelist
    >
    > Original change's description:
    > > Initial implementation of ChromeOS X86_64 stack walker
    > >
    > > Apple and ChromeOS X86_64 both have frame pointers, so moved the
    > > Apple-specific native unwinder to a "frame-pointer-having" native
    > > unwinder for both Apple and ChromeOS X86_64.
    > >
    > > Passes unit tests on device but haven't yet tried with real Chrome.
    > >
    > > This shouldn't have any effect on end-user devices as the experiment
    > > isn't on for ChromeOS devices;
    > > StackSamplingProfiler::IsSupportedForCurrentPlatform() still returns
    > > false.
    > >
    > > BUG=b:214117401,chromium:1324337
    > >
    > > Change-Id: I594f3c6034c70138b87feb44e1094114e151a1ae
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749997
    > > Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    > > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1024086}
    >
    > Bug: b:214117401,chromium:1324337, 1344414
    > Change-Id: I476bddacbe24379c4a879cd0dd63fd771e10a92a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762075
    > Commit-Queue: Zain Afzal <zafzal@google.com>
    > Reviewed-by: Zain Afzal <zafzal@google.com>
    > Owners-Override: Zain Afzal <zafzal@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1024149}
    
    Bug: b:214117401,chromium:1324337
    Change-Id: I36104a5cbbc6af1c19e4305cf58858019a724529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763511
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024559}

--- base/profiler/stack_sampler_mac.cc
+++ base/profiler/stack_sampler_mac.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/profiler/stack_sampler.h"
 
+#include <memory>
+
 #include "base/bind.h"
 #include "base/check.h"
-#include "base/profiler/native_unwinder.h"
+#include "base/profiler/frame_pointer_unwinder.h"
 #include "base/profiler/stack_copier_suspend.h"
 #include "base/profiler/stack_sampler_impl.h"
 #include "base/profiler/suspendable_thread_delegate_mac.h"
@@ -17,10 +19,9 @@ namespace base {
 
 namespace {
 
-std::vector<std::unique_ptr<Unwinder>> CreateUnwinders(
-    ModuleCache* module_cache) {
+std::vector<std::unique_ptr<Unwinder>> CreateUnwinders() {
   std::vector<std::unique_ptr<Unwinder>> unwinders;
-  unwinders.push_back(CreateNativeUnwinder(module_cache));
+  unwinders.push_back(std::make_unique<FramePointerUnwinder>());
   return unwinders;
 }
 
@@ -37,7 +38,7 @@ std::unique_ptr<StackSampler> StackSampler::Create(
   return std::make_unique<StackSamplerImpl>(
       std::make_unique<StackCopierSuspend>(
           std::make_unique<SuspendableThreadDelegateMac>(thread_token)),
-      BindOnce(&CreateUnwinders, Unretained(module_cache)), module_cache,
+      BindOnce(&CreateUnwinders), module_cache,
       std::move(record_sample_callback), test_delegate);
 }
 
