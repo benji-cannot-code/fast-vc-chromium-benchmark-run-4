1aeb252eab74ff69d4b6065e85b824054cc62fb7
Nancy Wang <nancylingwang@chromium.org>
2022-07-15 05:14:17 +0000

    Check whether Lacros is enabled when publish extensions.
    When the Lacros is enabled, extensions are published in the Lacros side,
    but in the Ash side, ExtensionAppsChromeOs::Accepts, there is no
    checking whether Lacros is enabled.
    
    Add IsLacrosChromeAppsEnabled checking, because when IsLacrosChromeAppsEnabled is true, extensions_publisher_ in Lacros is
    created:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/lacros/chrome_browser_main_extra_parts_lacros.cc;l=132
    
    BUG=1343838
    
    Change-Id: I30fe6b863fd1f6021d2d9e3c1ea7cf061bf0c9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3761792
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024588}

--- chrome/browser/apps/app_service/publishers/extension_apps_chromeos.cc
+++ chrome/browser/apps/app_service/publishers/extension_apps_chromeos.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/extension_apps_utils.h"
 #include "chrome/browser/apps/app_service/intent_util.h"
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/apps/app_service/menu_util.h"
@@ -710,12 +711,19 @@ bool ExtensionAppsChromeOs::Accepts(const extensions::Extension* extension) {
     if (!extension->is_extension() || IsBlocklisted(extension->id())) {
       return false;
     }
+
     // QuickOffice has file_handlers which we need to register.
     if (extension_misc::IsQuickOfficeExtension(extension->id())) {
       // Don't publish quickoffice in ash if 1st party ash extension keep list
       // is enforced, since quickoffice extension is published in Lacros.
       return !crosapi::browser_util::ShouldEnforceAshExtensionKeepList();
     }
+
+    // Do not publish extensions in Ash if it should run in Lacros instead.
+    if (!apps::ShouldMuxExtensionIds()) {
+      return false;
+    }
+
     // Only accept extensions with file_browser_handlers.
     FileBrowserHandler::List* handler_list =
         FileBrowserHandler::GetHandlers(extension);
