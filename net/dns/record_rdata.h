af66277ba98610b32f31da18046b7dc475ff9b48
Sukrit Ganesh <sukritganesh@google.com>
2022-07-15 17:18:27 +0000

    Adding multimap functionality to the OptRecordRdata class.
    Currently, a list of Opts is stored in a vector. This will be changed to
    be a multimap, where the key will be the option code. Changes must be
    made to getter methods and access protocols as well.
    
    Bug: 1343514
    
    Change-Id: I247aaa06ceb41973bcdca2e8aa81eb6e58cb2b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3754090
    Auto-Submit: Sukrit Ganesh <sukritganesh@google.com>
    Commit-Queue: Sukrit Ganesh <sukritganesh@google.com>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024804}

--- net/dns/record_rdata.h
+++ net/dns/record_rdata.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -266,17 +267,19 @@ class NET_EXPORT_PRIVATE OptRecordRdata : public RecordRdata {
   uint16_t Type() const override;
 
   const std::vector<char>& buf() const { return buf_; }
+  const std::multimap<uint16_t, Opt>& opts() { return opts_; }
 
-  const std::vector<Opt>& opts() const { return opts_; }
   void AddOpt(const Opt& opt);
 
   // Add all Opts from |other| to |this|.
   void AddOpts(const OptRecordRdata& other);
 
+  // Checks if an Opt with the specified opt_code is in opts_.
   bool ContainsOptCode(uint16_t opt_code) const;
 
  private:
-  std::vector<Opt> opts_;
+  // Opt objects are stored in a multimap; key is the opt code.
+  std::multimap<uint16_t, Opt> opts_;
   std::vector<char> buf_;
 };
 
