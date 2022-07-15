573f89223353771fcfe4ba6c58be3b9de2e28a87
Sirisha Kavuluru <skavuluru@google.com>
2022-07-15 22:47:44 +0000

    [ToolbarTablet] Reland of using cached feature flag
    Use of cached feature flag was reverted in CL https://chromium-review.googlesource.com/c/chromium/src/+/3763612 since it was causing a unit test to fail when enabled. Relanding fix with updated UT.
    
    Bug: 1344608
    Change-Id: I5f503f0ffcedbd659b97a9fd8732d6e45720289f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763218
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024940}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
@@ -18,7 +18,6 @@ import android.os.Looper;
 import android.view.View;
 import android.widget.ImageButton;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,7 +30,6 @@ import org.robolectric.Shadows;
 import org.robolectric.annotation.LooperMode;
 import org.robolectric.shadows.ShadowToast;
 
-import org.chromium.base.FeatureList;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -81,11 +79,7 @@ public final class ToolbarTabletUnitTest {
         LocationBarLayout locationBarLayout = mToolbarTablet.findViewById(R.id.location_bar);
         locationBarLayout.setStatusCoordinatorForTesting(mStatusCoordinator);
         mToolbarTablet.setMenuButtonCoordinatorForTesting(mMenuButtonCoordinator);
-    }
-
-    @After
-    public void tearDown() {
-        disableGridTabSwitcher();
+        ToolbarTablet.GTS_ENABLE_LAUNCH_POLISH.setForTesting(false);
     }
 
     @Test
@@ -181,7 +175,7 @@ public final class ToolbarTabletUnitTest {
     @EnableFeatures(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS)
     @Test
     public void testSetTabSwitcherPolishModeOff_toolbarStillVisible() {
-        enableGridTabSwitcher(true);
+        ToolbarTablet.GTS_ENABLE_LAUNCH_POLISH.setForTesting(true);
         assertEquals("Initial Toolbar visibility is not as expected", View.VISIBLE,
                 mToolbarTablet.getVisibility());
         // Call
@@ -194,7 +188,7 @@ public final class ToolbarTabletUnitTest {
     @EnableFeatures(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS)
     @Test
     public void testSetTabSwitcherPolishModeOn_toolbarStillVisible() {
-        enableGridTabSwitcher(true);
+        ToolbarTablet.GTS_ENABLE_LAUNCH_POLISH.setForTesting(true);
         assertEquals("Initial Toolbar visibility is not as expected", View.VISIBLE,
                 mToolbarTablet.getVisibility());
         // Call
@@ -287,19 +281,5 @@ public final class ToolbarTabletUnitTest {
         assertTrue("Toast is not as expected",
                 ShadowToast.showedCustomToast(
                         mActivity.getResources().getString(stringId), R.id.toast_text));
-    }
-
-    private void enableGridTabSwitcher(boolean enablePolish) {
-        FeatureList.TestValues testValues = new FeatureList.TestValues();
-        testValues.addFeatureFlagOverride(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS, true);
-        testValues.addFieldTrialParamOverride(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS,
-                "enable_launch_polish", String.valueOf(enablePolish));
-        FeatureList.setTestValues(testValues);
-    }
-
-    private void disableGridTabSwitcher() {
-        FeatureList.TestValues testValues = new FeatureList.TestValues();
-        testValues.addFeatureFlagOverride(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS, false);
-        FeatureList.setTestValues(testValues);
     }
 }
