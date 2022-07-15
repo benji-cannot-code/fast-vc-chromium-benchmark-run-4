72940bd70f660f1a29e259c2b2918f1206f8d6f5
Owen Zhang <owenzhang@google.com>
2022-07-15 22:30:12 +0000

    [CrOS Audio] Generate Mojo bindings for CrosAudioConfig.
    This CL generates mojom-webui JavaScript bindings for CrosAudioConfig in chromeos settings. Bindings have been verified manually in settings-audio subpage.
    
    Screenshot: http://screen/8aqPJDdjUQiBt8u
    Bug: 1092970
    Change-Id: I1e9b196d0eaa4d0c62efeb789df97436c241ef85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760869
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024934}

--- chrome/browser/resources/settings/chromeos/device_page/audio.js
+++ chrome/browser/resources/settings/chromeos/device_page/audio.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_slider/cr_slider.js';
 import '../../icons.html.js';
-import '../../controls/settings_slider.js';
 import '../../settings_shared_css.js';
 
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
