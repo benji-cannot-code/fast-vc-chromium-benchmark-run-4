457887f66b9ab495aba1b7f638374c1f5bd457ca
Robert Flack <flackr@chromium.org>
2022-07-15 19:35:27 +0000

    Disable ReadAnythingModelTest.* on ASAN Mac
    These tests started failing on
    https://ci.chromium.org/p/chromium/builders/ci/Mac%20ASan%2064%20Tests%20%281%29/b8808590483016198481
    with a memory leak during the browser destruction.
    
    Bug: 1344891
    Change-Id: Ia11dbd9f9c3ffbf77ea756ae400ff2904849c14f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763055
    Reviewed-by: Chong Gu <chonggu@google.com>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Owners-Override: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024870}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_model_unittest.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_model_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -54,6 +55,10 @@ class ReadAnythingModelTest : public TestWithBrowserView {
   MockReadAnythingModelObserver model_observer_2_;
   MockReadAnythingModelObserver model_observer_3_;
 };
+
+// TODO(crbug.com/1344891): Fix the memory leak on destruction observed on these
+// tests on asan mac.
+#if !BUILDFLAG(IS_MAC) || !defined(ADDRESS_SANITIZER)
 
 TEST_F(ReadAnythingModelTest, AddingModelObserverNotifiesAllObservers) {
   model_->AddObserver(&model_observer_1_);
@@ -167,3 +172,5 @@ TEST_F(ReadAnythingModelTest, FontModelGetCurrentFontName) {
   EXPECT_EQ("Webdings", GetFontModel()->GetFontNameAt(7));
   EXPECT_EQ("Impact", GetFontModel()->GetFontNameAt(8));
 }
+
+#endif  // !defined(ADDRESS_SANITIZER)
