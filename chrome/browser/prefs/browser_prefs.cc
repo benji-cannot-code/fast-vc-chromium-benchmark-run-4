9c6313b0225ad1ebb14f3616a393e9918e59afd3
Hamda <hmare@google.com>
2022-07-15 18:27:38 +0000

    [Mac DT] Add new device trust key rotation local preference
    Adds a new kDeviceTrustDisableKeyCreationPref to the connectors
    preference. This local preference is for setting the mac key creation
    eligibility for the device. When disabled the key creation process will
    not be performed. Also deletes unused previous device trust connector
    kDeviceTrustPrivateKeyPref and kDeviceTrustPublicKeyPref preferences.
    
    This change is a solution to the con mentioned in the mac key rotation
    design document to handle multiple users on a device performing the key
    rotation.
    
    DD: go/mac-key-rotation-dd#heading=h.by0sq63o58fd
    
    Bug: b/235366066
    Change-Id: I5469cd1bf67994b9e8f8bf9cfba810681cc772cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764827
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024841}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1213,6 +1213,7 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
 #if BUILDFLAG(IS_MAC)
   confirm_quit::RegisterLocalState(registry);
   QuitWithAppsController::RegisterPrefs(registry);
+  enterprise_connectors::RegisterLocalPrefs(registry);
   system_media_permissions::RegisterSystemMediaPermissionStatesPrefs(registry);
   AppShimRegistry::Get()->RegisterLocalPrefs(registry);
 #endif
