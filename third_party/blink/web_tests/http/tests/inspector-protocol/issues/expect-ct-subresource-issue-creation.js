b3f55657900f6413afc29010367be4e63a3959bc
Emily Stark <estark@google.com>
2022-07-15 17:55:55 +0000

    Add deprecation issue for Expect-CT header
    See https://groups.google.com/a/chromium.org/g/blink-dev/c/bGLVLwSKNJY/m/nbg4hWckAwAJ
    
    Bug: 1232560
    Change-Id: I6e28c40dfa86515331270e0c8b86966642ecec39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759196
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024828}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/expect-ct-subresource-issue-creation.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+    `Verifies that Expect-CT deprecation issue is created from subresource with Expect-CT header.\n`);
+
+  await dp.Audits.enable();
+  page.navigate('http://127.0.0.1:8000/inspector-protocol/resources/expect-ct-subresource.html');
+  const issue = await dp.Audits.onceIssueAdded();
+
+  testRunner.log(issue.params, "Inspector issue: ");
+  testRunner.completeTest();
+})
