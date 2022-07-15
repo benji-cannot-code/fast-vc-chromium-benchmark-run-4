149c4bf27377a441a48b91832e6ee83fdc34b864
Felipe Andrade <fsandrade@google.com>
2022-07-15 04:05:27 +0000

    Reland "Dynamically enable/disable Kerberos in SmbService"
    This is a reland of commit ff199a709e0aa668856d358634385a795ce74010
    
    Original change's description:
    > Dynamically enable/disable Kerberos in SmbService
    >
    > Change SmbKerberosCredentialsUpdater, making it listen to Kerberos
    > enabled state changes. With that, Kerberos authentication will get
    > enabled/disabled dynamically in SmbService.
    >
    > Besides improving the user experience, as described in
    > crbug.com/1040138, this change also fixes an issue when #lacros-support
    > flag is enabled, as described in crbug.com/1321568. Since it's a
    > per-user flag, it changes value after the user session has already
    > started. With that, a new KerberosCredentialsManager is created,
    > starting with Kerberos disabled, then updating it to enabled later.
    >
    > Bug: 1040138, 1321568
    > Change-Id: I7d83cb4e4a479d9cf398cf4d335e80f34cd928ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673080
    > Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    > Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    > Reviewed-by: Ben Reich <benreich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1009546}
    
    Bug: 1040138, 1321568
    Change-Id: I2f0c1dd1935a62ba62928600bb6de896de02994f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738610
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024575}

--- chrome/browser/ash/smb_client/smb_kerberos_credentials_updater.h
+++ chrome/browser/ash/smb_client/smb_kerberos_credentials_updater.h
@@ -10,13 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/raw_ptr.h"
 #include "chrome/browser/ash/kerberos/kerberos_credentials_manager.h"
 
 namespace ash {
 namespace smb_client {
 
 // Updates Kerberos credentials in SmbService after receiving a
-// OnAccountsChanged notification from KerberosCredentialsManager.
+// `OnKerberosEnabledStateChanged` or `OnAccountsChanged` notification from
+// `KerberosCredentialsManager`.
 class SmbKerberosCredentialsUpdater
     : public KerberosCredentialsManager::Observer {
  public:
@@ -26,10 +28,12 @@ class SmbKerberosCredentialsUpdater
   SmbKerberosCredentialsUpdater(
       KerberosCredentialsManager* credentials_manager,
       ActiveAccountChangedCallback active_account_changed_callback);
+  ~SmbKerberosCredentialsUpdater() override;
+
+  // Disallow copy and assignment.
   SmbKerberosCredentialsUpdater(const SmbKerberosCredentialsUpdater&) = delete;
   SmbKerberosCredentialsUpdater& operator=(
       const SmbKerberosCredentialsUpdater&) = delete;
-  ~SmbKerberosCredentialsUpdater() override;
 
   // Checks if Kerberos is enabled by asking KerberosCredentialsManager.
   bool IsKerberosEnabled() const;
@@ -39,11 +43,17 @@ class SmbKerberosCredentialsUpdater
   }
 
  private:
+  // Updates `active_account_name_`, if the given `account_name` has a different
+  // value. In that case, calls `active_account_changed_callback_` with the new
+  // value.
+  void UpdateActiveAccount(const std::string& account_name);
+
   // KerberosCredentialsManager::Observer:
+  void OnKerberosEnabledStateChanged() override;
   void OnAccountsChanged() override;
 
   // Not owned.
-  KerberosCredentialsManager* credentials_manager_;
+  raw_ptr<KerberosCredentialsManager> credentials_manager_;
   std::string active_account_name_;
   const ActiveAccountChangedCallback active_account_changed_callback_;
 };
