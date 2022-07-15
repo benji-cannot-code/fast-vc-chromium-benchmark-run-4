6763807c8240453c7d12238053bd1b3c284b5f7c
Tim Judkins <tjudkins@chromium.org>
2022-07-15 02:22:39 +0000

    Reland "[Extension bindings] Add result modifier to identity.getAuthToken"
    This is a reland of commit 621fb3298b54ecbddaffd752b54c1e80406e4bd9
    With the change in crrev.com/c/3759432 fixing the underlying problem
    this originally highlighted, this change should be fine to reland now.
    
    Original change's description:
    > [Extension bindings] Add result modifier to identity.getAuthToken
    >
    > This CL converts the API signature for identity.getAuthToken to have an
    > asynchronous return of an object instead of two separate parameters and
    > adds promise support to it. The associated apitests have also been
    > updated to expect the new object based async return and the other APIs
    > in chrome.identity have had promise support added too.
    >
    > In order to not break support for previous callers that were using the
    > callback form, this CL also adds a new hooks delegate for the identity
    > API which converts the object returned into the previous form of
    > multiple parameters for callback based calls to the API. An associated
    > test for this behavior has also been added.
    >
    > Bug: 1313625
    > Change-Id: Icb3ccac51860bcf53f2e068672095451e4b0276b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722202
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Tim <tjudkins@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1022999}
    
    Bug: 1313625
    Change-Id: Ib42514f9d62653e1e3d6484067459ad4bb1cd776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764170
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024548}

---
+++ chrome/renderer/extensions/identity_hooks_delegate.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_RENDERER_EXTENSIONS_IDENTITY_HOOKS_DELEGATE_H_
+#define CHROME_RENDERER_EXTENSIONS_IDENTITY_HOOKS_DELEGATE_H_
+
+#include <vector>
+
+#include "extensions/renderer/bindings/api_binding_hooks_delegate.h"
+#include "extensions/renderer/bindings/api_signature.h"
+#include "v8/include/v8-forward.h"
+
+namespace extensions {
+
+// Custom native hooks for the identity API.
+class IdentityHooksDelegate : public APIBindingHooksDelegate {
+ public:
+  IdentityHooksDelegate();
+
+  IdentityHooksDelegate(const IdentityHooksDelegate&) = delete;
+  IdentityHooksDelegate& operator=(const IdentityHooksDelegate&) = delete;
+
+  ~IdentityHooksDelegate() override;
+
+  // APIBindingHooksDelegate:
+  APIBindingHooks::RequestResult HandleRequest(
+      const std::string& method_name,
+      const APISignature* signature,
+      v8::Local<v8::Context> context,
+      std::vector<v8::Local<v8::Value>>* arguments,
+      const APITypeReferenceMap& refs) override;
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_RENDERER_EXTENSIONS_IDENTITY_HOOKS_DELEGATE_H_
