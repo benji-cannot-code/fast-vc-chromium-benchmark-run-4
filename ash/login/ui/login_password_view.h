3200fc0f58c9357f2bd2452bf1c0f48ca8362bbd
Denis Kuznetsov <antrim@chromium.org>
2022-07-15 17:30:41 +0000

    Add debug logging to identify cause of crash
    This CL adds excessive logging to understand the action sequence
    behind triggering second attempt in LoginScreenController before
    first one is finished and resolved.
    
    Bug: 1339004
    Change-Id: I161310b127ee09581fde21e8cedf757ddbddfac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3765689
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024812}

--- ash/login/ui/login_password_view.h
+++ ash/login/ui/login_password_view.h
@@ -135,6 +135,7 @@ class ASH_EXPORT LoginPasswordView : public views::View,
 
   // Makes the textfield read-only and enables/disables submitting.
   void SetReadOnly(bool read_only);
+  bool IsReadOnly() const;
 
   // views::View:
   const char* GetClassName() const override;
