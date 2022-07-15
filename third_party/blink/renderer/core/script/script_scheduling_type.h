d18df041586b7b3a025dbc3a69a6ab48492250ef
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-07-15 00:26:10 +0000

    Reland DeferAllScript behind the flag
    To revisit DeferAllScript (without Lite Mode control),
    this CL reverts [1] that deleted the previous
    DeferAllScript experiment.
    
    The non-trivial changes (compared to simple revert) are:
    
    - Lite mode-related code are not relanded.
      The eligibility logic `ShouldForceDeferScript()` is now
      quite simple, and we will refine it later.
    - The force-defer logic of `ScriptRunner` is migrated
      to `ScriptRunnerDelayer` because `ScriptRunner` was much simplified
      since [1]. Conflicts with DelayAsyncScriptExecution experiment
      [2][3] are also resolved manually by `ScriptRunnerDelayer`.
    - `base::Feature` is used instead of `RuntimeEnabledFeatures`.
    
    This CL shouldn't affect the behavior without the flag.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2691539
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/3085825
    [3] https://chromium-review.googlesource.com/c/chromium/src/+/3744964
    
    Bug: 1339112
    Change-Id: I7f3369d8ca9c0da04e09e9f0675c237dce8af8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699503
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024501}

--- third_party/blink/renderer/core/script/script_scheduling_type.h
+++ third_party/blink/renderer/core/script/script_scheduling_type.h
@@ -70,11 +70,9 @@ enum class ScriptSchedulingType {
   // execute after parsing completes (due to ForceDeferScriptIntervention).
   //
   // Spec: not yet spec'ed. https://crbug.com/976061
-  // kDeprecatedForceDefer is deprecated, but kept here to ensure metrics are
-  // recorded correctly in-order.
-  kDeprecatedForceDefer,
+  kForceDefer,
 
-  kMaxValue = kDeprecatedForceDefer,
+  kMaxValue = kForceDefer,
 };
 
 }  // namespace blink
