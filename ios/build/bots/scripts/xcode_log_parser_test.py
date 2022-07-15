05f5aea238f8387fc5e113e62b1337b4283f6e72
Nico Weber <thakis@chromium.org>
2022-07-15 16:54:57 +0000

    ios bot scripts: Use vpython3 instead of vpython
    CL created by running:
    
       git grep -l '#!.*vpython$'  ios/build | \
           xargs sed -i '' '1 s/vpython/vpython3/'
    
    
    Bug: 941669,1329052
    Change-Id: I65b3c9395e4efc50937e2ac6d926576e61a5f5e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763049
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024793}

--- ios/build/bots/scripts/xcode_log_parser_test.py
+++ ios/build/bots/scripts/xcode_log_parser_test.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 # Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
