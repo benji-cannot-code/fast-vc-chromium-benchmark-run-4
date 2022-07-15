c251790c0357aff88b6dc14bd3fa736c602d0d92
Elaine Chien <elainechien@chromium.org>
2022-07-15 05:16:05 +0000

    [Color Pipeline] Fix delay in WebContents getting correct ColorProviderSource
    This CL sets the ColorProviderSource on the WebContents when it is
    inserted into the TabStripModel. At this point we know which
    BrowserView the WebContents will be associated with. If we move
    ColorProvider code out of views/ then creation of the
    ColorProviderBrowserHelper should also be moved out of views/
    (potentially to the Browser class).
    
    The reason this CL is necessary is because currently we only set the
    ColorProviderSource when the WebContents is associated with the
    WebView. However, this only happens when the user navigates to the
    tab that will display the WebContents. This does not always happen
    immediately when the tab is created. For example when the tab is
    opened up through a bookmarks folder. (See bug). With this CL, the
    WebContents will have the correct ColorProviderSource before the user
    navigates to the tab.
    
    Bug: 1341905
    Change-Id: Ia9749833d166546b3a4e8840964d1f8eba52c060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751977
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024591}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/autofill_bubble_handler_impl.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bubble_view.h"
+#include "chrome/browser/ui/views/color_provider_browser_helper.h"
 #include "chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h"
 #include "chrome/browser/ui/views/download/download_in_progress_dialog_view.h"
 #include "chrome/browser/ui/views/download/download_shelf_view.h"
@@ -863,6 +864,8 @@ BrowserView::BrowserView(std::unique_ptr<Browser> browser)
   top_container_ = AddChildView(std::make_unique<TopContainerView>(this));
   tab_strip_region_view_ = top_container_->AddChildView(
       std::make_unique<TabStripRegionView>(std::move(tabstrip)));
+
+  ColorProviderBrowserHelper::CreateForBrowser(browser_.get());
 
   // Create WebViews early so |webui_tab_strip_| can observe their size.
   auto devtools_web_view =
