1f460e2ce14887e9ee6f268656278f14b7a09b1e
Rushan Suleymanov <rushans@google.com>
2022-07-15 17:37:06 +0000

    [Sync] Fix sending NIGORI invalidation after ClearData in tests
    This CL fixes tests which are failing when sync standalone invalidations are enabled.
    
    Sync standalone invalidations use DeviceInfo to store FCM registration
    tokens. In tests, DeviceInfos are read after commit has been processed
    and hence after clearing all data from the fake server, all the
    information about subscriptions gets lost. This CL uses FCM handlers to
    simulate subscription to NIGORI data type to deliver those
    invalidations.
    
    Bug: 1331206
    Change-Id: I8d5bd71a6143f7fce789e427cc04f52101a6ff16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762634
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024817}

--- components/sync/test/fake_server/fake_server.cc
+++ components/sync/test/fake_server/fake_server.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
-#include "base/guid.h"
 #include "base/hash/hash.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
@@ -19,16 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "components/sync/engine/net/server_connection_manager.h"
 #include "components/sync/protocol/data_type_progress_marker.pb.h"
 #include "components/sync/protocol/proto_value_conversions.h"
 #include "components/sync/protocol/sync_entity.pb.h"
 #include "components/sync/protocol/sync_enums.pb.h"
-#include "net/base/net_errors.h"
 #include "net/http/http_status_code.h"
 
 using syncer::GetModelTypeFromSpecifics;
@@ -252,6 +248,7 @@ net::HttpStatusCode FakeServer::HandleParsedCommand(
       break;
     case sync_pb::ClientToServerMessage::COMMIT:
       last_commit_message_ = message;
+      OnWillCommit();
       break;
     case sync_pb::ClientToServerMessage::CLEAR_SERVER_DATA:
       // Don't care.
@@ -413,6 +410,7 @@ void FakeServer::InjectEntity(std::unique_ptr<LoopbackServerEntity> entity) {
 
   const ModelType model_type = entity->GetModelType();
 
+  OnWillCommit();
   loopback_server_->SaveEntity(std::move(entity));
 
   // Notify observers so invalidations are mimic-ed.
@@ -426,6 +424,8 @@ base::Time FakeServer::SetWalletData(
   ModelType model_type =
       GetModelTypeFromSpecifics(wallet_entities[0].specifics());
   DCHECK(model_type == syncer::AUTOFILL_WALLET_DATA);
+
+  OnWillCommit();
   wallet_entities_ = wallet_entities;
 
   const base::Time now = base::Time::Now();
@@ -453,6 +453,8 @@ base::Time FakeServer::SetOfferData(
   ModelType model_type =
       GetModelTypeFromSpecifics(offer_entities[0].specifics());
   DCHECK(model_type == syncer::AUTOFILL_WALLET_OFFER);
+
+  OnWillCommit();
   offer_entities_ = offer_entities;
 
   const base::Time now = base::Time::Now();
@@ -483,6 +485,7 @@ base::Time FakeServer::GetProgressMarkerTimestamp(
 bool FakeServer::ModifyEntitySpecifics(
     const std::string& id,
     const sync_pb::EntitySpecifics& updated_specifics) {
+  OnWillCommit();
   if (!loopback_server_->ModifyEntitySpecifics(id, updated_specifics)) {
     return false;
   }
@@ -499,6 +502,7 @@ bool FakeServer::ModifyBookmarkEntity(
     const std::string& id,
     const std::string& parent_id,
     const sync_pb::EntitySpecifics& updated_specifics) {
+  OnWillCommit();
   if (!loopback_server_->ModifyBookmarkEntity(id, parent_id,
                                               updated_specifics)) {
     return false;
@@ -514,6 +518,7 @@ bool FakeServer::ModifyBookmarkEntity(
 void FakeServer::ClearServerData() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
+  OnWillCommit();
   {
     base::ScopedAllowBlockingForTesting allow_blocking;
     loopback_server_->ClearServerData();
@@ -683,6 +688,12 @@ void FakeServer::LogForTestFailure(const base::Location& location,
       location.file_name(), location.line_number(),
       base::StringPrintf("--- %s %d (reverse chronological order) ---\n%s",
                          title.c_str(), request_counter_, body.c_str())));
+}
+
+void FakeServer::OnWillCommit() {
+  for (Observer& observer : observers_) {
+    observer.OnWillCommit();
+  }
 }
 
 }  // namespace fake_server
