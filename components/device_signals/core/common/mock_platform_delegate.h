ba74f941c4736b862d6edf024dc2ac50da95d04a
Sebastien Lalancette <seblalancette@chromium.org>
2022-07-15 15:12:27 +0000

    [NewEV] Update Presence Signal
    Updated the logic around gathering the Presence signal due to a
    discrepancy between behaviours on POSIX and Windows platforms. Path
    resolution logic on POSIX will simply fail whenever the given path
    does not point to any file system resource - the logic on living in
    WinPlatformDelegate has therefore been updated to mimic that behaviour.
    
    Also, resolved paths should also have any relative portion resolved
    (i.e. not just the environment variables). That is a requirement of
    base::File, which we must use when calculating the SHA256 hash of a
    given file.
    
    Fixed: b:238110608
    Change-Id: I072a8e95ee8a2c76dfd2b795b8074a14bdb436bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3765286
    Reviewed-by: Hamda Mare <hmare@google.com>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024749}

--- components/device_signals/core/common/mock_platform_delegate.h
+++ components/device_signals/core/common/mock_platform_delegate.h
@@ -18,7 +18,6 @@ class MockPlatformDelegate : public PlatformDelegate {
   MockPlatformDelegate();
   ~MockPlatformDelegate() override;
 
-  MOCK_METHOD(bool, PathExists, (const base::FilePath&), (const override));
   MOCK_METHOD(bool, PathIsReadable, (const base::FilePath&), (const override));
   MOCK_METHOD(bool, DirectoryExists, (const base::FilePath&), (const override));
   MOCK_METHOD(bool,
--- components/device_signals/core/system_signals/win/win_platform_delegate.cc
+++ components/device_signals/core/system_signals/win/win_platform_delegate.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/strings/string_util.h"
 #include "components/device_signals/core/common/common_types.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -50,7 +51,12 @@ bool WinPlatformDelegate::ResolveFilePath(const base::FilePath& file_path,
     return false;
   }
 
-  *resolved_file_path = base::FilePath(expanded_path_wstring.value());
+  auto expanded_file_path = base::FilePath(expanded_path_wstring.value());
+  if (!base::PathExists(expanded_file_path)) {
+    return false;
+  }
+
+  *resolved_file_path = base::MakeAbsoluteFilePath(expanded_file_path);
   return true;
 }
 
