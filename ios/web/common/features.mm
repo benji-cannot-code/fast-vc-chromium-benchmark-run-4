f83025ac73ac04f90eab094137230493aaf1328b
ginnnnnnny <ginnyhuang@chromium.org>
2022-07-15 20:09:51 +0000

    [iOS][media-permissions] Enable media permissions by default
    CL to clean up the respective base::Feature would come after.
    
    Bug: 1284228
    Change-Id: Id5a35f3705bf569d6790fcb3eef4d292aaa955c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764826
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024885}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -54,7 +54,7 @@ const base::Feature kEnableUnrealizedWebStates{
     "EnableUnrealizedWebStates", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMediaPermissionsControl{"MediaPermissionsControl",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 extern const base::Feature kEnableFullscreenAPI{
     "EnableFullscreenAPI", base::FEATURE_DISABLED_BY_DEFAULT};
