81a5adbee2a61d3f3bf2c28f14e99129807323fd
Yue Li <updowndota@google.com>
2022-07-16 02:10:41 +0000

    Quick Answers: Add metrics for network error response code
    Bug: b/227388758
    Test: None
    Change-Id: I47b597b2fab2c4226f8ad12c7e59793ee29eaa8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767035
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024990}

--- chromeos/components/quick_answers/utils/quick_answers_metrics.cc
+++ chromeos/components/quick_answers/utils/quick_answers_metrics.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace quick_answers {
 
 namespace {
+
 const char kQuickAnswerActiveImpression[] = "QuickAnswers.ActiveImpression";
 const char kQuickAnswerClick[] = "QuickAnswers.Click";
 const char kQuickAnswerResult[] = "QuickAnswers.Result";
@@ -26,12 +27,14 @@ const char kQuickAnswersTtsEngineEvent[] =
     "QuickAnswers.TextToSpeech.EngineEvent";
 const char kQuickAnswersDictionaryIntentSource[] =
     "QuickAnswers.DictionaryIntent.Source";
+const char kQuickAnswersNetworkError[] = "QuickAnswers.NetworkError.IntentType";
+const char kQuickAnswersNetworkResponseCode[] =
+    "QuickAnswers.NetworkError.ResponseCode";
 
 const char kDurationSuffix[] = ".Duration";
 const char kDefinitionSuffix[] = ".Definition";
 const char kTranslationSuffix[] = ".Translation";
 const char kUnitConversionSuffix[] = ".UnitConversion";
-const char kQuickAnswersNetworkError[] = "QuickAnswers.NetworkError.IntentType";
 
 std::string ResultTypeToString(ResultType result_type) {
   switch (result_type) {
@@ -122,8 +125,25 @@ void RecordIntentType(IntentType intent_type) {
   base::UmaHistogramEnumeration(kQuickAnswerIntent, intent_type);
 }
 
-void RecordNetworkError(IntentType intent_type) {
+void RecordNetworkError(IntentType intent_type, int response_code) {
   base::UmaHistogramEnumeration(kQuickAnswersNetworkError, intent_type);
+
+  std::string histogram_name = kQuickAnswersNetworkResponseCode;
+  switch (intent_type) {
+    case IntentType::kDictionary:
+      histogram_name += kDefinitionSuffix;
+      break;
+    case IntentType::kTranslation:
+      histogram_name += kTranslationSuffix;
+      break;
+    case IntentType::kUnit:
+      histogram_name += kUnitConversionSuffix;
+      break;
+    case IntentType::kUnknown:
+      return;
+  }
+
+  base::UmaHistogramSparse(histogram_name, response_code);
 }
 
 void RecordTtsEngineEvent(TtsEngineEvent event) {
