7f6ba211a413c2be53bc8abd41de0065d129b27f
Johann <johannkoenig@google.com>
2022-07-17 00:07:31 +0000

    PageLoadMetrics: HttpsEngagementPLO for Prerendering
    HttpsEngagement metrics are only reported once the page has
    been in the foreground.
    
    Bug: 1317494
    Change-Id: Ifa05d946589a31be44ecf38635f40471a12eb58f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750717
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025054}

--- chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_page_load_metrics_observer.h
@@ -35,6 +35,8 @@ class HttpsEngagementPageLoadMetricsObserver
   ObservePolicy OnFencedFramesStart(
       content::NavigationHandle* navigation_handle,
       const GURL& currently_committed_url) override;
+  ObservePolicy OnPrerenderStart(content::NavigationHandle* navigation_handle,
+                                 const GURL& currently_committed_url) override;
   void OnComplete(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
 
