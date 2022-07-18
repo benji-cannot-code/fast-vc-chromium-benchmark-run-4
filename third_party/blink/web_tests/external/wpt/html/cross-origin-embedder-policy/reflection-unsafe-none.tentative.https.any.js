0b085117e95148017cdf920202129d9e49668465
Jonathan Hao <phao@chromium.org>
2022-07-18 16:22:04 +0000

    Policy container inheritance for shared workers loaded from modules
    Similar to https://chromium-review.googlesource.com/c/chromium/src/+/3714442 but for the code paths that loads shared workers from modules.
    
    Bug: 1177199
    Change-Id: I688f47eae21cd42deeb288dabd14be3f34914683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757788
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025273}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/reflection-unsafe-none.tentative.https.any.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/reflection-unsafe-none.tentative.https.any.js
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: global=window,dedicatedworker,sharedworker,serviceworker
+// META: global=window,worker,sharedworker-module
 test(t => assert_equals(crossOriginEmbedderPolicy, "unsafe-none"));
