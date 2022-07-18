623dcc83c85543c1e70c4682dc824aa85808ed97
Brian Sheedy <bsheedy@chromium.org>
2022-07-18 22:26:44 +0000

    Re-enable Android x86 minidump tests
    Re-enables minidump tests on Android x86 emulators since they should be
    fixed with crrev.com/c/3760908.
    
    Bug: 1342706
    Change-Id: I7731d44112396c8dcd2aa9fbf29eb20a451bb39f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763430
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025435}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -66,7 +66,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Disabled(
       'chromeos-local',
       'win7',  # https://crbug.com/1084931
-      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testSymbolizeMinidump(self):
     # Wait for the browser to restart fully before crashing
@@ -112,7 +111,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Disabled(
       'chromeos-local',
       'win7',  # https://crbug.com/1084931
-      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testMultipleCrashMinidumps(self):
     # Wait for the browser to restart fully before crashing
@@ -205,7 +203,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Disabled(
       'chromeos-local',
       'win7',  # https://crbug.com/1084931
-      'android-nougat'  # Flaky: https://crbug.com/1342706
   )
   def testMinidumpFromRendererHang(self):
     """Tests that renderer hangs result in minidumps.
