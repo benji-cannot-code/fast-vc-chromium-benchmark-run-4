5243725825abac06a8cde7fc026df9f9967f8451
Nakul Vaidya <nakulvaidya@google.com>
2022-07-18 20:58:04 +0000

    Fix AddTestFormFieldData() arguments
    Bug: 1335549
    Change-Id: I7f2c33e71a0e53f1521ffabdfefda960c528fdb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766755
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Commit-Queue: Nakul Vaidya <nakulvaidya@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025396}

--- components/autofill/core/browser/form_parsing/merchant_promo_code_field_unittest.cc
+++ components/autofill/core/browser/form_parsing/merchant_promo_code_field_unittest.cc
@@ -46,7 +46,7 @@ INSTANTIATE_TEST_SUITE_P(
 
 // Match promo(tion|tional)?[-_. ]*code
 TEST_P(MerchantPromoCodeFieldTest, ParsePromoCode) {
-  AddTextFormFieldData("Enter promo code here", "promoCodeField",
+  AddTextFormFieldData("promoCodeField", "Enter promo code here",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::PARSED);
@@ -54,7 +54,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParsePromoCode) {
 
 // Match promo(tion|tional)?[-_. ]*code
 TEST_P(MerchantPromoCodeFieldTest, ParsePromotionalCode) {
-  AddTextFormFieldData("Use the promotional code here", "promoCodeField",
+  AddTextFormFieldData("promoCodeField", "Use the promotional code here",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::PARSED);
@@ -70,7 +70,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParsePromoCodeWithPrefixAndSuffix) {
 
 // Match coupon[-_. ]*code
 TEST_P(MerchantPromoCodeFieldTest, ParseCouponCode) {
-  AddTextFormFieldData("Enter new coupon__code", "couponCodeField",
+  AddTextFormFieldData("couponCodeField", "Enter new coupon__code",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::PARSED);
@@ -78,7 +78,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParseCouponCode) {
 
 // Match gift[-_. ]*code
 TEST_P(MerchantPromoCodeFieldTest, ParseGiftCode) {
-  AddTextFormFieldData("Check out with gift.codes", "giftCodeField",
+  AddTextFormFieldData("giftCodeField", "Check out with gift.codes",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::PARSED);
@@ -86,7 +86,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParseGiftCode) {
 
 // Match discount[-_. ]*code
 TEST_P(MerchantPromoCodeFieldTest, ParseDiscountCode) {
-  AddTextFormFieldData("Check out with discount-code", "discountCodeField",
+  AddTextFormFieldData("discountCodeField", "Check out with discount-code",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::PARSED);
@@ -94,7 +94,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParseDiscountCode) {
 
 TEST_P(MerchantPromoCodeFieldTest, ParseNonPromoCode) {
   // Regex relies on "promo/coupon/gift" + "code" together.
-  AddTextFormFieldData("Field for gift card or promo details", "otherField",
+  AddTextFormFieldData("otherField", "Field for gift card or promo details",
                        UNKNOWN_TYPE);
 
   ClassifyAndVerify(ParseResult::NOT_PARSED);
@@ -104,7 +104,7 @@ TEST_P(MerchantPromoCodeFieldTest, ParsePromoCodeFlagOff) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitAndDisableFeature(
       features::kAutofillParseMerchantPromoCodeFields);
-  AddTextFormFieldData("Enter promo code here", "promoCodeField",
+  AddTextFormFieldData("promoCodeField", "Enter promo code here",
                        MERCHANT_PROMO_CODE);
 
   ClassifyAndVerify(ParseResult::NOT_PARSED);
