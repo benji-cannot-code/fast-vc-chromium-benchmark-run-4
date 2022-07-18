c7725408b92a0b6b35ebc3da07e9e78cc309f1a7
Andrey Zaytsev <andzaytsev@google.com>
2022-07-18 21:53:13 +0000

    Site Data Improvements: Add Clear Browsing Data link
    Screenshot: https://screenshot.googleplex.com/4GuduFwq5ke8DwK.png
    
    Bug: 1342991
    Change-Id: I279086b38fb868e784d366db098a31a1cc0b4c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766674
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025423}

--- weblayer/browser/java/org/chromium/weblayer_private/settings/WebLayerSiteSettingsDelegate.java
+++ weblayer/browser/java/org/chromium/weblayer_private/settings/WebLayerSiteSettingsDelegate.java
@@ -140,5 +140,13 @@ public class WebLayerSiteSettingsDelegate
     public void dismissPrivacySandboxSnackbar() {}
 
     @Override
+    public boolean canLaunchClearBrowsingDataDialog() {
+        return false;
+    }
+
+    @Override
+    public void launchClearBrowsingDataDialog(Activity currentActivity) {}
+
+    @Override
     public void onDestroyView() {}
 }
