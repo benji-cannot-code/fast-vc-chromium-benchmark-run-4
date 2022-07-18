471e257fcd864f370f1cc387b9c5f655a490d008
Ben Franz <bfranz@google.com>
2022-07-18 11:06:35 +0000

    Fix clangd unused-includes warning for browser_process_platform_parh.h
    This header transiently exports the platform specific header,
    which caused the clangd linter to raise the "unused include" warning.
    
    By adding the IWYU pragma clangd stops complaining.
    
    
    Bug: N/A
    Change-Id: I55448b1c68a808fc9705ed6f983fd40508fff41c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763362
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025197}

--- chrome/browser/browser_process_platform_part.h
+++ chrome/browser/browser_process_platform_part.h
@@ -11,18 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Include the appropriate BrowserProcessPlatformPart based on the platform.
 #if BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/browser_process_platform_part_android.h"
+#include "chrome/browser/browser_process_platform_part_android.h"  // IWYU pragma: export
 #elif BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/browser_process_platform_part_ash.h"
+#include "chrome/browser/browser_process_platform_part_ash.h"  // IWYU pragma: export
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chrome/browser/browser_process_platform_part_chromeos.h"
+#include "chrome/browser/browser_process_platform_part_chromeos.h"  // IWYU pragma: export
 class BrowserProcessPlatformPart : public BrowserProcessPlatformPartChromeOS {};
 #elif BUILDFLAG(IS_MAC)
-#include "chrome/browser/browser_process_platform_part_mac.h"
+#include "chrome/browser/browser_process_platform_part_mac.h"  // IWYU pragma: export
 #elif BUILDFLAG(IS_WIN)
-#include "chrome/browser/browser_process_platform_part_win.h"
+#include "chrome/browser/browser_process_platform_part_win.h"  // IWYU pragma: export
 #else
-#include "chrome/browser/browser_process_platform_part_base.h"
+#include "chrome/browser/browser_process_platform_part_base.h"  // IWYU pragma: export
 class BrowserProcessPlatformPart : public BrowserProcessPlatformPartBase {};
 #endif
 
