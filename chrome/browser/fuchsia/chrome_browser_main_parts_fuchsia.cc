e341929b4bc4331895392740004ea27ca13a3674
Fabrice de Gans <fdegans@chromium.org>
2022-07-18 16:32:25 +0000

    [fuchsia] Skip serving the outgoing directory for tests
    The call to ServeFromStartupInfo() fails for browser tests due to the
    browser process not having a PA_DIRECTORY_REQUEST handle when running
    tests in a test batch process.
    
    Bug: 1344932
    Change-Id: I9ca9c3be411c084a14bfe4283980c901cb0a76c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764166
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025275}

--- chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.cc
+++ chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
+#include "content/public/common/content_switches.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/switches.h"
 #include "ui/ozone/public/ozone_switches.h"
@@ -669,9 +670,13 @@ int ChromeBrowserMainPartsFuchsia::PreMainMessageLoopRun() {
     }
   }
 
-  zx_status_t status =
-      base::ComponentContextForProcess()->outgoing()->ServeFromStartupInfo();
-  ZX_CHECK(status == ZX_OK, status);
+  // We should only call ServerFromStartupInfo() when the browser is running as
+  // a stand-alone component.
+  if (!is_integration_test()) {
+    zx_status_t status =
+        base::ComponentContextForProcess()->outgoing()->ServeFromStartupInfo();
+    ZX_CHECK(status == ZX_OK, status);
+  }
 
   // Publish the fuchsia.process.lifecycle.Lifecycle service to allow graceful
   // teardown. If this is an integration test then graceful shutdown is not
