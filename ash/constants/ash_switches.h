018052e50840d2a175ef5925b96339ff96451b50
Brahim Chikhaoui <bchikhaoui@google.com>
2022-07-18 14:06:06 +0000

    [OOBE] - Remove switch related to  blazey screens
    Remove unused command-line switch blazey related screens
    as we use a brand config field kCloudGamingDevice.
    
    Bug: 1345218
    Change-Id: I9d2880f1b57b70279fc7c86ea2de4c6c04024777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768506
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025233}

--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -235,7 +235,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeForceTabletFirstRun[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kOobeLargeScreenSpecialScaling[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeScreenshotDirectory[];
-COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeShowBlazeyScreen[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeSkipPostLogin[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeSkipToLogin[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeTimerInterval[];
