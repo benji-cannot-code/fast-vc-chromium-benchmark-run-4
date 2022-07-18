da82070c2f21aa8b9f7322e53027358c9dd541ed
Christopher Lam <calamity@chromium.org>
2022-07-18 07:37:53 +0000

    [style_var_gen] Remove CSS Debug Color hack.
    This CL removes the template expression hack for generating debug colors
    in WebUIs. The WebUI debug color system should be accessed through the
    ColorProvider system at chrome://theme/colors.css instead.
    
    This CL 'correctly' breaks debug colors for all WebUIs not hooked
    up to the new ColorProvider system.
    
    Bug: b:234400002
    Change-Id: Ic1a7ee9279c808fc90b2c4ccdf38489b23e96b02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768268
    Commit-Queue: Christopher Lam <calamity@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025158}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -22,13 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/webui_generated_resources_map.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "ash/constants/ash_features.h"
-#include "base/feature_list.h"
 #include "chromeos/ash/grit/ash_resources.h"
 #include "chromeos/ash/grit/ash_resources_map.h"
 #include "chromeos/grit/chromeos_resources.h"
 #include "chromeos/grit/chromeos_resources_map.h"
-#include "ui/chromeos/styles/cros_styles.h"  // nogncheck
 #endif
 
 namespace content {
@@ -138,12 +135,6 @@ void PopulateSharedResourcesDataSource(WebUIDataSource* source) {
                kChromeosResourcesSize, source);
   AddResources(GetAshMojoResourceIds(), kAshResources, kAshResourcesSize,
                source);
-
-  source->AddString(
-      "crosColorsDebugOverrides",
-      base::FeatureList::IsEnabled(ash::features::kSemanticColorsDebugOverride)
-          ? cros_styles::kDebugOverrideCssString
-          : "");
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
   source->AddString("fontFamily", webui::GetFontFamily());
