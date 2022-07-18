97fcdc59a15917d86d3bfaa03c976b63a6c2a0aa
Melissa Zhang <melzhang@google.com>
2022-07-18 03:45:43 +0000

    [OobeAppRecs] Add device fingerprint to Play resource request.
    This CL adds the device fingerprint to the Play request so that
    Play can check that the apps returned can be downloaded by the
    device.
    
    Bug: b/235421974,1223321
    Change-Id: I2f261a800d178a962253ec9f5705a40b84d4443a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753609
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025133}

--- chrome/browser/ash/login/screens/recommend_apps/recommend_apps_fetcher_impl.cc
+++ chrome/browser/ash/login/screens/recommend_apps/recommend_apps_fetcher_impl.cc
@@ -222,6 +222,10 @@ gfx::ExtensionSet GetGLExtensions(const gpu::GPUInfo& gpu_info) {
   return extensionSet;
 }
 
+const std::string& GetDeviceFingerprint(const arc::ArcFeatures& arc_features) {
+  return arc_features.build_props.at("ro.build.fingerprint");
+}
+
 const std::string& GetAndroidSdkVersion(const arc::ArcFeatures& arc_features) {
   return arc_features.build_props.at("ro.build.version.sdk");
 }
@@ -405,6 +409,10 @@ void RecommendAppsFetcherImpl::OnArcFeaturesRead(
     play_store_version_ = read_result.value().play_store_version;
 
     android_sdk_version_ = GetAndroidSdkVersion(read_result.value());
+
+    if (base::FeatureList::IsEnabled(features::kAppDiscoveryForOobe)) {
+      device_fingerprint_ = GetDeviceFingerprint(read_result.value());
+    }
   }
 
   MaybeStartCompressAndEncodeProtoMessage();
@@ -437,6 +445,8 @@ void RecommendAppsFetcherImpl::StartDownload() {
   auto resource_request = std::make_unique<network::ResourceRequest>();
   if (base::FeatureList::IsEnabled(features::kAppDiscoveryForOobe)) {
     resource_request->url = GURL(kGetRevisedAppListUrl);
+    resource_request->headers.SetHeader("X-DFE-Device-Fingerprint",
+                                        device_fingerprint_);
   } else {
     resource_request->url = GURL(kGetAppListUrl);
   }
--- chrome/browser/ash/login/screens/recommend_apps/recommend_apps_fetcher_impl.h
+++ chrome/browser/ash/login/screens/recommend_apps/recommend_apps_fetcher_impl.h
@@ -150,6 +150,8 @@ class RecommendAppsFetcherImpl : public RecommendAppsFetcher {
 
   std::string play_store_version_;
 
+  std::string device_fingerprint_;
+
   std::string encoded_device_configuration_proto_;
 
   bool ash_ready_ = false;
