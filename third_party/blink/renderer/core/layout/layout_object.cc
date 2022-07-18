b0e3cd47d4bf800dae3273b4fd28c4e56f53d589
Xianzhu Wang <wangxianzhu@chromium.org>
2022-07-18 21:25:05 +0000

    Fix LayoutObject::MapLocalToAncestor() with kIgnoreScrollOffset for fixed position
    Previously the result was incorrect with the scroll offset applied in
    the reverse direction. The cause is that
    LayoutView::MapLocalToAncestor() didn't consider kIgnoreScrollOffset
    when undoing the scroll offset applied by the caller.
    
    Now let LayoutObject::OffsetFromScrollableContainer() return zero
    for fixed-position contained by LayoutView.
    
    With this we can remove kFixed from MapCoordinatesFlags, after
    cleanup of LayoutView::MapToVisualRectInAncestorSpace().
    
    Bug: 881555
    Change-Id: I2eb4a189ce618b5f1549ffbf177b3f3bcce0bfd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753529
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025410}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -3165,13 +3165,6 @@ void LayoutObject::MapLocalToAncestor(const LayoutBoxModelObject* ancestor,
     transform_state.Move(-ancestor->OffsetFromAncestor(container),
                          preserve3d ? TransformState::kAccumulateTransform
                                     : TransformState::kFlattenTransform);
-    // If the ancestor is fixed, then the rect is already in its coordinates so
-    // doesn't need viewport-adjusting.
-    auto* layout_view = DynamicTo<LayoutView>(container);
-    if (ancestor->StyleRef().GetPosition() != EPosition::kFixed &&
-        layout_view && StyleRef().GetPosition() == EPosition::kFixed) {
-      transform_state.Move(layout_view->OffsetForFixedPosition());
-    }
     return;
   }
 
@@ -3230,13 +3223,6 @@ void LayoutObject::MapAncestorToLocal(const LayoutBoxModelObject* ancestor,
   if (skip_info.AncestorSkipped()) {
     container_offset = ancestor->OffsetFromAncestor(container);
     transform_state.Move(-container_offset);
-    // If the ancestor is fixed, then the rect is already in its coordinates so
-    // doesn't need viewport-adjusting.
-    auto* layout_view = DynamicTo<LayoutView>(container);
-    if (ancestor->StyleRef().GetPosition() != EPosition::kFixed &&
-        layout_view && StyleRef().GetPosition() == EPosition::kFixed) {
-      transform_state.Move(layout_view->OffsetForFixedPosition());
-    }
   }
 }
 
@@ -3396,6 +3382,10 @@ PhysicalOffset LayoutObject::OffsetFromScrollableContainer(
     bool ignore_scroll_offset) const {
   NOT_DESTROYED();
   DCHECK(container->IsScrollContainer());
+
+  if (IsFixedPositioned() && container->IsLayoutView())
+    return PhysicalOffset();
+
   const auto* box = To<LayoutBox>(container);
   if (!ignore_scroll_offset)
     return -box->ScrolledContentOffset();
--- third_party/blink/renderer/core/layout/map_coordinates_flags.h
+++ third_party/blink/renderer/core/layout/map_coordinates_flags.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 enum MapCoordinatesMode {
-  kIsFixed = 1 << 0,
-
   // Only needed in some special cases to intentionally ignore transforms.
   kIgnoreTransforms = 1 << 2,
 
