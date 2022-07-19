1956eacc422a0da060526bd85cc9c254425fdf21
Dmitrii Kuragin <kuragin@chromium.org>
2022-07-19 16:32:52 +0000

    Implement isolated app loading.
    This CL is a part of adding install isolated app command.
    
    Bug: 1333966
    Change-Id: I559db6917f68508f592eb5fa057111a2935ec2fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721410
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Dmitrii Kuragin <kuragin@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025775}

--- chrome/browser/web_applications/commands/install_isolated_app_command.h
+++ chrome/browser/web_applications/commands/install_isolated_app_command.h
@@ -6,16 +6,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_INSTALL_ISOLATED_APP_COMMAND_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_INSTALL_ISOLATED_APP_COMMAND_H_
 
+#include <string>
+
+#include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/values.h"
 #include "chrome/browser/web_applications/commands/web_app_command.h"
 
 namespace web_app {
+class WebAppUrlLoader;
+class WebAppDataRetriever;
+
+enum class InstallIsolatedAppCommandResult {
+  kOk,
+  kUnknownError,
+};
 
 class InstallIsolatedAppCommand : public WebAppCommand {
  public:
-  explicit InstallIsolatedAppCommand(base::StringPiece application_url);
+  explicit InstallIsolatedAppCommand(
+      base::StringPiece application_url,
+      WebAppUrlLoader& url_loader,
+      base::OnceCallback<void(InstallIsolatedAppCommandResult)> callback);
   ~InstallIsolatedAppCommand() override;
 
   base::Value ToDebugValue() const override;
@@ -24,7 +38,23 @@ class InstallIsolatedAppCommand : public WebAppCommand {
   void OnSyncSourceRemoved() override;
   void OnShutdown() override;
 
+  void SetDataRetrieverForTesting(
+      std::unique_ptr<WebAppDataRetriever> data_retriever);
+
  private:
+  void ReportFailure();
+  void Report(bool success);
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  std::string url_;
+
+  WebAppUrlLoader& url_loader_;
+
+  std::unique_ptr<WebAppDataRetriever> data_retriever_;
+
+  base::OnceCallback<void(InstallIsolatedAppCommandResult)> callback_;
+
   base::WeakPtr<InstallIsolatedAppCommand> weak_this_;
   base::WeakPtrFactory<InstallIsolatedAppCommand> weak_factory_{this};
 };
