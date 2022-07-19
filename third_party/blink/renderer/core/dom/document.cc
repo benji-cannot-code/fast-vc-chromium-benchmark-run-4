e4d1eeaa984d4a8dc7d0de982883e8f9027bf47d
Eric Willigers <ericwilligers@chromium.org>
2022-07-19 07:11:34 +0000

    Retire flag ScrollSnapAfterLayout
    SnapAfterLayout shipped in M81.
    https://chromium-review.googlesource.com/c/chromium/src/+/2025321
    
    Bug: 866127
    Change-Id: I9dc5743ab5a14e05edecf90501fb90cf732ddce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764793
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025599}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7481,8 +7481,7 @@ void Document::PerformScrollSnappingTasks() {
   if (!snap_coordinator.AnySnapContainerDataNeedsUpdate())
     return;
   snap_coordinator.UpdateAllSnapContainerDataIfNeeded();
-  if (RuntimeEnabledFeatures::ScrollSnapAfterLayoutEnabled())
-    snap_coordinator.ResnapAllContainersIfNeeded();
+  snap_coordinator.ResnapAllContainersIfNeeded();
 }
 
 void Document::SetContextFeatures(ContextFeatures& features) {
