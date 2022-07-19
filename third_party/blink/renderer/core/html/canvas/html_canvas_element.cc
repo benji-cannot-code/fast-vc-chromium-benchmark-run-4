cb6e11ba0bdb5298db9b0cfcf8e7431d9122636d
Mingjing Zhang <mjzhang@chromium.org>
2022-07-19 20:49:33 +0000

    Revert "OneCopyCanvasCapture: Implement for WebGPU canvas"
    This reverts commit 35a0879fa2f18e895be895380f8e9de1a4c4e0f8.
    
    Reason for revert: crbug.com/1345767 failures in the following tests: WebGPUTraceTest_WebGPUCanvasDisableOneCopyCapture WebGPUTraceTest_WebGPUCanvasOneCopyCapture
    
    Original change's description:
    > OneCopyCanvasCapture: Implement for WebGPU canvas
    >
    > Implement GPUCanvasContext::CopyRenderingResultsToVideoFrame()
    > which invokes newly added WebGPUSwapBufferProvider::CopyToVideoFrame()
    > method.
    >
    > WebGPUSwapBufferProvider::CopyToVideoFrame() needs Dawn relinquishing
    > access to the swapchain texture via ReleaseWGPUTextureAccessIfNeeded().
    >
    > Add new test:
    > src/content/test/data/gpu/pixel_webgpu_canvas_capture_to_video.html
    > which captures WebGPU canvas and stream to a video player.
    >
    > Bug: 1310123
    > Change-Id: Ied9460549ad5e8148f6b586a1e259cac48f2d9eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3717946
    > Reviewed-by: Austin Eng <enga@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1025756}
    
    Bug: 1310123
    Change-Id: I5c707a1a826516357b95b390e28fbd6ef8a580a2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774037
    Auto-Submit: Mingjing Zhang <mjzhang@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mingjing Zhang <mjzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025921}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/checked_math.h"
 #include "base/numerics/safe_conversions.h"
-#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -424,7 +423,8 @@ CanvasRenderingContext* HTMLCanvasElement::GetCanvasRenderingContextInternal(
       return nullptr;
     SetNeedsUnbufferedInputEvents(true);
     frame_dispatcher_ = std::make_unique<CanvasResourceDispatcher>(
-        nullptr, surface_layer_bridge_->GetFrameSinkId().client_id(),
+        nullptr,
+        surface_layer_bridge_->GetFrameSinkId().client_id(),
         surface_layer_bridge_->GetFrameSinkId().sink_id(),
         CanvasResourceDispatcher::kInvalidPlaceholderCanvasId, size_);
     // We don't actually need the begin frame signal when in low latency mode,
@@ -746,8 +746,6 @@ void HTMLCanvasElement::NotifyListenersCanvasChanged() {
                   SharedGpuContext::ContextProviderWrapper()),
               kBackBuffer, gfx::ColorSpace::CreateREC709(),
               std::move(split_callback.first))) {
-        TRACE_EVENT1("blink", "HTMLCanvasElement::NotifyListenersCanvasChanged",
-                     "OneCopyCanvasCapture", true);
         continue;
       }
     }
