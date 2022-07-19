89b272605eead94d724e9062bddad0aa92a51a3c
Ewann <ewannpv@google.com>
2022-07-19 14:38:23 +0000

    [iOS] Updates `systemImageNamed:` ban rule
    This CL excludes the file that creates wrappers around the banned
    function.
    
    Change-Id: I2aaaecaff0c150759744104018f47c93be39e2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3771182
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025711}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -344,6 +344,9 @@ _BANNED_IOS_OBJC_FUNCTIONS = (
         'ios/chrome/browser/ui/icons/chrome_symbol.h'
       ),
       True,
+      excluded_paths=(
+        'ios/chrome/browser/ui/icons/chrome_symbol.mm',
+      ),
     ),
 )
 
