37b207825b3e091d31bc7b3477f2046eafe3d61d
Jeffrey Young <cowmoo@google.com>
2022-07-19 17:36:58 +0000

    personalization: prevent ambient crash
    Crash has been present since M93 in ambient_mode_handler.cc
    and was ported over to personalization_app_ambient_provider_impl.cc.
    If ambient mode pref is off, and server responds with
    ambient settings with topic source kGooglePhotos
    and no albums, chromeOS will crash. Check if ambient is enabled
    and short circuit if disabled.
    
    BUG=b:236723933
    TEST=unit_tests --gtest_filter="*AmbientProviderImplTest*"
    
    Change-Id: I34ced19ee825f2da9c502954a9fcbc3e26975ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773339
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025811}

--- chrome/browser/ui/webui/settings/chromeos/ambient_mode_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/ambient_mode_handler.cc
@@ -400,7 +400,10 @@ void AmbientModeHandler::UpdateSettings() {
   settings_->show_weather = true;
 
   settings_sent_for_update_ = settings_;
-  ash::AmbientBackendController::Get()->UpdateSettings(
+  auto* ambient_backend_controller = ash::AmbientBackendController::Get();
+  CHECK(ambient_backend_controller)
+      << "Ambient backend controller required to update settings";
+  ambient_backend_controller->UpdateSettings(
       *settings_, base::BindOnce(&AmbientModeHandler::OnUpdateSettings,
                                  backend_weak_factory_.GetWeakPtr()));
 }
@@ -573,7 +576,9 @@ void AmbientModeHandler::MaybeUpdateTopicSource(
     return;
 
   settings_->topic_source = topic_source;
-  UpdateSettings();
+  if (IsAmbientModeEnabled()) {
+    UpdateSettings();
+  }
   SendTopicSource();
 }
 
