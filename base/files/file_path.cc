b2e2ada7c57fdf9dd2711a809c9141ac0690b08c
mczeng <zengmcong@gmail.com>
2022-07-19 12:11:14 +0000

    [base] Fix IWYU issue for checked_cast<> in FilePath
    This problem occurs when enable_base_tracing=false
    
    Change-Id: I137151970c3936b6d2d13a56d53a5a13729813cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768267
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025664}

--- base/files/file_path.cc
+++ base/files/file_path.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/files/safe_base_name.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/pickle.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
