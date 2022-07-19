28965fd07880e8fc8caeb4b634f8c98f66a43a54
Paul Semel <paulsemel@chromium.org>
2022-07-19 14:28:16 +0000

    DanglingPtr: add newest untriaged dangling ptrs
    Those pointers are now showing up as dangling. Let's mark them so we can
    keep track of those.
    
    Bug: 1291138
    Change-Id: I521004b89e27c2837948ec817ba69e90316436fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3772653
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025705}

--- content/browser/preloading/prerender/prerender_host.h
+++ content/browser/preloading/prerender/prerender_host.h
@@ -238,7 +238,7 @@ class CONTENT_EXPORT PrerenderHost : public WebContentsObserver {
   base::ObserverList<Observer> observers_;
 
   // Stores the attempt corresponding to this prerender to log various metrics.
-  raw_ptr<PreloadingAttemptImpl> attempt_;
+  raw_ptr<PreloadingAttemptImpl, DanglingUntriaged> attempt_;
 
   // Navigation parameters for the navigation which loaded the main document of
   // the prerendered page, copied immediately after BeginNavigation when
