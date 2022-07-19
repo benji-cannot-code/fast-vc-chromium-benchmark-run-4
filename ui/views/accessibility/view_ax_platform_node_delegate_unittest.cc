a48ea97635bb875a5788543ba60d2382bf194f1e
Matt Reichhoff <mreichhoff@chromium.org>
2022-07-19 20:34:14 +0000

    Revert "Remove WIDGET_OWNS_NATIVE_WIDGET from ui/views/accessibility"
    This reverts commit e09ab3c38e40f61b25c895a94d4685ca0a8f2a22.
    
    Reason for revert: appears to cause issues with ASAN builders.
    One of the unit test files now appears to leak memory.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/44569/test-results
    
    Original change's description:
    > Remove WIDGET_OWNS_NATIVE_WIDGET from ui/views/accessibility
    >
    > WIDGET_OWNS_NATIVE_WIDGET is known to cause subtle lifetime issue.
    > Remove it from UI ui/views/accessibility in favor of
    > NATIVE_WIDGET_OWNS_WIDGET. Use UniqueWidgetPtr to manage client code
    > initiated widget destroy.
    >
    > Bug: 1218885
    > Change-Id: I8c365e50f300a0f33075b1d71228e88b4eaab4a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760374
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1025805}
    
    Bug: 1218885
    Change-Id: I527c268d6fae12e20be60afb28238dfff138e0e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773900
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025911}

--- ui/views/accessibility/view_ax_platform_node_delegate_unittest.cc
+++ ui/views/accessibility/view_ax_platform_node_delegate_unittest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/table/table_view.h"
 #include "ui/views/test/menu_test_utils.h"
 #include "ui/views/test/views_test_base.h"
-#include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
 
 namespace views {
@@ -253,6 +252,7 @@ class ViewAXPlatformNodeDelegateMenuTest
 
     owner_ = std::make_unique<Widget>();
     Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_POPUP);
+    params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
     owner_->Init(std::move(params));
     owner_->Show();
 
@@ -304,7 +304,7 @@ class ViewAXPlatformNodeDelegateMenuTest
   raw_ptr<SubmenuView> submenu_ = nullptr;
   std::unique_ptr<TestMenuDelegate> menu_delegate_;
   std::unique_ptr<MenuRunner> runner_;
-  UniqueWidgetPtr owner_;
+  std::unique_ptr<Widget> owner_;
 };
 
 TEST_F(ViewAXPlatformNodeDelegateTest, FocusBehaviorShouldAffectIgnoredState) {
@@ -1117,8 +1117,9 @@ TEST_F(AXViewTest, LayoutCalledInvalidateRootView) {
   // this observer to simulate it.
   AXAuraObjCache cache;
   TestAXEventObserver observer(&cache);
-  UniqueWidgetPtr widget = std::make_unique<Widget>();
+  std::unique_ptr<Widget> widget(new Widget);
   Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_POPUP);
+  params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   widget->Init(std::move(params));
   widget->Show();
 
