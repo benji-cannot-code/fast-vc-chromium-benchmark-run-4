49b6d6fdd84153cd2efd102d73dba20183f3abc4
Abigail Klein <abigailbklein@google.com>
2022-07-19 02:04:03 +0000

    [Read Anything] Only distill AXTree when UI exists.
    Before this CL, we distilled the AXTree when it was created. This CL
    ensures that we distill the AXTree every time that the UI is created,
    and prevent AXTrees from being distilled when the UI is destroyed.
    
    The difference between the UI being activated vs. ready:
     - Activated: UI is visible in the Side Panel
     - Created: UI classes are constructed
    
    Bug: 1266555
    Change-Id: I5231e1b83da28d8f6289504a7570bcf1afbc4c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770174
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1025524}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_controller.h
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_controller.h
@@ -62,6 +62,7 @@ class ReadAnythingController : public ReadAnythingToolbarView::Delegate,
 
   // ReadAnythingPageHandler::Delegate:
   void OnUIReady() override;
+  void OnUIDestroyed() override;
 
   // TabStripModelObserver:
   void OnTabStripModelChanged(
@@ -97,6 +98,10 @@ class ReadAnythingController : public ReadAnythingToolbarView::Delegate,
   // Whether the Read Anything feature is currently active. The feature is
   // active when it is currently shown in the Side Panel.
   bool active_ = false;
+
+  // Whether the Read Anything feature's UI is ready. This is set to true when
+  // the UI is constructed and false when it is destroyed.
+  bool ui_ready_ = false;
 
   base::WeakPtrFactory<ReadAnythingController> weak_pointer_factory_{this};
 };
