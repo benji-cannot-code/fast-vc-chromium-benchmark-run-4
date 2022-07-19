258340b56238c7c7fddd5f2afa8a1c957f944b80
Dmitrii Kuragin <kuragin@chromium.org>
2022-07-19 22:38:19 +0000

    Remove unnecessary constant definition.
    Since C++17, `static constexpr` data member doesn't require definition because
    it is `inline` implicitly. See https://abseil.io/tips/168 for details.
    
    Change-Id: I6decad4197640f3540c257d9c157e3a205dce45c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773896
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025971}

--- chrome/browser/web_applications/web_app_url_loader.cc
+++ chrome/browser/web_applications/web_app_url_loader.cc
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace web_app {
-
-constexpr base::TimeDelta WebAppUrlLoader::kSecondsToWaitForWebContentsLoad;
-
 namespace {
 using UrlComparison = WebAppUrlLoader::UrlComparison;
 
