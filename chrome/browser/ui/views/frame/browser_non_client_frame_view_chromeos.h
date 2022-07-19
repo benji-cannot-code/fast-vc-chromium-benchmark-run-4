fdbfe2cda2c72de65c3ceb331404e7a0f8eef760
Nancy Wang <nancylingwang@chromium.org>
2022-07-19 05:43:23 +0000

    Revert "dark_mode: Add highlight border for non-browser windows"
    This reverts commit 95d966153a7255330b432568e592c34e0361caa6.
    
    Reason for revert:
    Break NonClientFrameViewAshTests.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/29207
    
    Original change's description:
    > dark_mode: Add highlight border for non-browser windows
    >
    > Move the class HighlightBorderOverlay created in the CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/3698761 to
    > chromeos and apply it for non-browser windows to add highlight border
    > for non-browser windows.
    >
    > Fixed: 1334858
    > Change-Id: I9e8eac214640fa438f2c1438ebd7f9bbed8b982d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763767
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    > Commit-Queue: Connie Xu <conniekxu@chromium.org>
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Min Chen <minch@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1025533}
    
    Change-Id: I79199dc518f01596ed310ce316f018bbf6448894
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3771282
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Owners-Override: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025582}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/browser/ui/views/tab_icon_view_model.h"
-#include "chromeos/ui/frame/highlight_border_overlay.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -28,12 +27,12 @@ namespace {
 class WebAppNonClientFrameViewAshTest;
 }
 
+class HighlightBorderOverlay;
 class ProfileIndicatorIcon;
 class TabIconView;
 
 namespace chromeos {
 class FrameCaptionButtonContainerView;
-class HighlightBorderOverlay;
 }  // namespace chromeos
 
 // Provides the BrowserNonClientFrameView for Chrome OS.
