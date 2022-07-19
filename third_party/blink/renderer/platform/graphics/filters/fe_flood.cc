bfe4f2114ec429b3bb14c50c9dfa6998109ecda4
Aaron Krajeski <aaronhk@chromium.org>
2022-07-19 18:58:01 +0000

    FEFloodFilter to SkColor4f
    viz folder is done, cc is in progress. Starting on Blink and this seems like an easy thing to chip off to get started.
    
    Bug: 1308932
    Change-Id: I8fa1f0f54577edcf2c1575504480ce7d1c416d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758489
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025866}

--- third_party/blink/renderer/platform/graphics/filters/fe_flood.cc
+++ third_party/blink/renderer/platform/graphics/filters/fe_flood.cc
@@ -30,18 +30,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-FEFlood::FEFlood(Filter* filter, const Color& flood_color, float flood_opacity)
+FEFlood::FEFlood(Filter* filter,
+                 const SkColor4f& flood_color,
+                 float flood_opacity)
     : FilterEffect(filter),
       flood_color_(flood_color),
       flood_opacity_(flood_opacity) {
   FilterEffect::SetOperatingInterpolationSpace(kInterpolationSpaceSRGB);
 }
 
-Color FEFlood::FloodColor() const {
+SkColor4f FEFlood::FloodColor() const {
   return flood_color_;
 }
 
-bool FEFlood::SetFloodColor(const Color& color) {
+bool FEFlood::SetFloodColor(const SkColor4f& color) {
   if (flood_color_ == color)
     return false;
   flood_color_ = color;
@@ -60,11 +62,14 @@ bool FEFlood::SetFloodOpacity(float flood_opacity) {
 }
 
 sk_sp<PaintFilter> FEFlood::CreateImageFilter() {
-  Color color = FloodColor().CombineWithAlpha(FloodOpacity());
+  SkColor4f color = flood_color_;
+  color.fA *= flood_opacity_;
   absl::optional<PaintFilter::CropRect> crop_rect = GetCropRect();
   return sk_make_sp<ColorFilterPaintFilter>(
-      SkColorFilters::Blend(color.Rgb(), SkBlendMode::kSrc), nullptr,
-      base::OptionalOrNullptr(crop_rect));
+      // TODO(crbug.com/1308932): SkColorFilters::Blend to SkColor4f
+      SkColorFilters::Blend(Color::FromSkColor4f(color).Rgb(),
+                            SkBlendMode::kSrc),
+      nullptr, base::OptionalOrNullptr(crop_rect));
 }
 
 WTF::TextStream& FEFlood::ExternalRepresentation(WTF::TextStream& ts,
@@ -72,7 +77,9 @@ WTF::TextStream& FEFlood::ExternalRepresentation(WTF::TextStream& ts,
   WriteIndent(ts, indent);
   ts << "[feFlood";
   FilterEffect::ExternalRepresentation(ts);
-  ts << " flood-color=\"" << FloodColor().NameForLayoutTreeAsText() << "\" "
+  // TODO(crbug.com/1308932): Color::NameForLayoutTreeAsText to SkColor4f
+  ts << " flood-color=\""
+     << Color::FromSkColor4f(FloodColor()).NameForLayoutTreeAsText() << "\" "
      << "flood-opacity=\"" << FloodOpacity() << "\"]\n";
   return ts;
 }
