cc21d152a4cac44d73bb9b87ce5125dfe913902b
Lei Zhang <thestig@chromium.org>
2022-07-19 19:05:18 +0000

    Improve casting for PrintPreviewUI.
    Use WebUIController::GetAs() to cast to PrintPreviewUI, instead of
    simply casting with static_cast().
    
    Bug: 1345034
    Change-Id: Ib827218592a313ab16ecdbefdc677817ee9437be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770968
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025870}

--- chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
@@ -81,8 +81,8 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewData) {
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   EXPECT_TRUE(IsShowingWebContentsModalDialog(initiator));
 
-  PrintPreviewUI* preview_ui = static_cast<PrintPreviewUI*>(
-      preview_dialog->GetWebUI()->GetController());
+  PrintPreviewUI* preview_ui =
+      preview_dialog->GetWebUI()->GetController()->GetAs<PrintPreviewUI>();
   ASSERT_TRUE(preview_ui);
   preview_ui->SetPreviewUIId();
 
@@ -126,8 +126,8 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewDraftPages) {
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   EXPECT_TRUE(IsShowingWebContentsModalDialog(initiator));
 
-  PrintPreviewUI* preview_ui = static_cast<PrintPreviewUI*>(
-      preview_dialog->GetWebUI()->GetController());
+  PrintPreviewUI* preview_ui =
+      preview_dialog->GetWebUI()->GetController()->GetAs<PrintPreviewUI>();
   ASSERT_TRUE(preview_ui);
   preview_ui->SetPreviewUIId();
 
@@ -184,8 +184,8 @@ TEST_F(PrintPreviewUIUnitTest, ShouldCancelRequest) {
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
   EXPECT_TRUE(IsShowingWebContentsModalDialog(initiator));
 
-  PrintPreviewUI* preview_ui = static_cast<PrintPreviewUI*>(
-      preview_dialog->GetWebUI()->GetController());
+  PrintPreviewUI* preview_ui =
+      preview_dialog->GetWebUI()->GetController()->GetAs<PrintPreviewUI>();
   ASSERT_TRUE(preview_ui);
   preview_ui->SetPreviewUIId();
 
@@ -227,7 +227,7 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewFailureCancelsPendingActions) {
   EXPECT_TRUE(IsShowingWebContentsModalDialog(initiator));
 
   PrintPreviewUI* preview_ui =
-      static_cast<PrintPreviewUI*>(preview_dialog->GetWebUI()->GetController());
+      preview_dialog->GetWebUI()->GetController()->GetAs<PrintPreviewUI>();
   ASSERT_TRUE(preview_ui);
   preview_ui->SetPreviewUIId();
 
