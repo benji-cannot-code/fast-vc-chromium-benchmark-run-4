996ea61f75ef490b30982cbac046876c7ef98273
Eric Willigers <ericwilligers@chromium.org>
2022-07-19 05:46:04 +0000

    Retire flag InteractionId
    InteractionID in EventTiming shipped in M96.
    
    Bug: 1236758
    Change-Id: I2424ca8cab9d5da68d254fa04dfb8358abd94362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767036
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025583}

--- third_party/blink/renderer/core/timing/window_performance.cc
+++ third_party/blink/renderer/core/timing/window_performance.cc
@@ -537,10 +537,8 @@ void WindowPerformance::ReportEventTimings(
     }
   }
 
-  if (RuntimeEnabledFeatures::InteractionIdEnabled(GetExecutionContext())) {
-    // Use |end_time| as a proxy for the current time.
-    responsiveness_metrics_->MaybeFlushKeyboardEntries(end_time);
-  }
+  // Use |end_time| as a proxy for the current time.
+  responsiveness_metrics_->MaybeFlushKeyboardEntries(end_time);
 }
 
 void WindowPerformance::NotifyAndAddEventTimingBuffer(
@@ -580,10 +578,6 @@ void WindowPerformance::NotifyAndAddEventTimingBuffer(
 
 void WindowPerformance::MaybeNotifyInteractionAndAddEventTimingBuffer(
     PerformanceEventTiming* entry) {
-  if (!RuntimeEnabledFeatures::InteractionIdEnabled(GetExecutionContext())) {
-    return;
-  }
-
   NotifyAndAddEventTimingBuffer(entry);
 }
 
@@ -599,12 +593,10 @@ bool WindowPerformance::SetInteractionIdAndRecordLatency(
   // disabled.
   if (pointer_id.has_value()) {
     return responsiveness_metrics_->SetPointerIdAndRecordLatency(
-               entry, *pointer_id, event_timestamps) ||
-           !RuntimeEnabledFeatures::InteractionIdEnabled(GetExecutionContext());
+        entry, *pointer_id, event_timestamps);
   }
   return responsiveness_metrics_->SetKeyIdAndRecordLatency(entry, key_code,
-                                                           event_timestamps) ||
-         !RuntimeEnabledFeatures::InteractionIdEnabled(GetExecutionContext());
+                                                           event_timestamps);
 }
 
 void WindowPerformance::AddElementTiming(const AtomicString& name,
