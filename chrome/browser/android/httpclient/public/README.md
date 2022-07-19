64976bd5793eb36e3411ce35ef5ed4cd9f2f786b
Mohamed Heikal <mheikal@chromium.org>
2022-07-19 14:47:30 +0000

    Add a common Java side Http client that uses chrome's network stack
    The aim is to migrate Survey and Chime off SurveyHttpClientBridge and
    onto AnnotatedHttpClient.
    
    Bug: 1322186
    Change-Id: I61608bcd6177d0db5a7b056f87ffc6da3c0baf3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656098
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025715}

---
+++ chrome/browser/android/httpclient/public/README.md
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This directory is synced out of the chromium repo. Any classes here cannot
+reference any other chromium classes or deps. See https://crbug.com/1322186
---
+++ chrome/browser/android/httpclient/public/src/org/chromium/chrome/browser/android/httpclient/HttpClient.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.android.httpclient;
+
+import java.util.Map;
+
+/** A simple HttpClient interface. */
+public interface HttpClient {
+    void send(String url, String requestType, byte[] body, Map<String, String> headers,
+            HttpResponseCallback responseCallback);
+}
