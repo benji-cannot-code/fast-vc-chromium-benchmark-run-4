a78424e6019d55cb3321c074346025625e91f728
Emily Stark <estark@google.com>
2022-07-19 17:16:31 +0000

    Reland "Add deprecation issue for Expect-CT header"
    This is a reland of commit 1a1b5a6df86820fbfe5d0598d0e7e98d8f3c6c60
    
    Original change's description:
    > Add deprecation issue for Expect-CT header
    >
    > See https://groups.google.com/a/chromium.org/g/blink-dev/c/bGLVLwSKNJY/m/nbg4hWckAwAJ
    >
    > Bug: 1232560
    > Change-Id: I6e28c40dfa86515331270e0c8b86966642ecec39
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759196
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Emily Stark <estark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024828}
    
    Bug: 1232560
    Change-Id: Ia3c0d0cbf76d075c114c7989417482f16d06c04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770032
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025792}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.h
@@ -52,6 +52,7 @@ enum class DeprecationIssueType {
   kDeprecationExample,
   kDocumentDomainSettingWithoutOriginAgentClusterHeader,
   kEventPath,
+  kExpectCTHeader,
   kGeolocationInsecureOrigin,
   kGeolocationInsecureOriginDeprecatedNotRemoved,
   kGetUserMediaInsecureOrigin,
