8f256ca2ad526d79ece092dda7b8620ace9b4395
Boris Sazonov <bsazonov@chromium.org>
2022-07-19 15:57:18 +0000

    [Android] 🤠 Disable flaky tests in TabSuspensionTest
    Disable testEagerSuspension, testMultiWindow and
    testNavigationFromSuspendedTabToInterstitial which are flaky on
    android-11-x86-rel and android-12-x64-rel.
    
    Bug: 1345655
    Change-Id: I46b78400960b2d83835949b827eeda089b56b87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773553
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025750}

--- chrome/android/javatests/src/org/chromium/chrome/browser/usage_stats/TabSuspensionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/usage_stats/TabSuspensionTest.java
@@ -29,6 +29,7 @@ import org.mockito.MockitoAnnotations;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.R;
@@ -188,6 +189,7 @@ public class TabSuspensionTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1345655")
     public void testEagerSuspension() {
         mActivityTestRule.loadUrl(mStartingUrl);
         CriteriaHelper.pollUiThread(() -> !mTab.isLoading());
@@ -233,6 +235,7 @@ public class TabSuspensionTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1345655")
     public void testMultiWindow() {
         mActivityTestRule.loadUrl(mStartingUrl);
         Tab tab2 = mActivityTestRule.loadUrlInNewTab(mDifferentUrl);
@@ -320,6 +323,7 @@ public class TabSuspensionTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1345655")
     public void testNavigationFromSuspendedTabToInterstitial() {
         doReturn(true).when(mSuspensionTracker).isWebsiteSuspended(STARTING_FQDN);
         startLoadingUrl(mTab, mStartingUrl);
