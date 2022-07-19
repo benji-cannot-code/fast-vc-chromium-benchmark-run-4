512b58f33d04570b12a8cca288a49074e71a123e
Ewann <ewannpv@google.com>
2022-07-19 07:26:33 +0000

    [iOS] Adds Settings symbols
    This CL adds symbol images to the Settings root screen.
    
    Bug: 131554
    Change-Id: Ib029c6fe0a518463c20bc61fbee8cbb6e2de0b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769867
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025601}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -954,6 +954,18 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
   } else {
     title = l10n_util::GetNSString(IDS_OPTIONS_ADVANCED_SECTION_TITLE_PRIVACY);
   }
+
+  if (UseSymbols()) {
+    return [self detailItemWithType:SettingsItemTypePrivacy
+                               text:title
+                         detailText:nil
+                         symbolView:ElevatedTableViewSymbolWithBackground(
+                                        DefaultSettingsRootSymbol(
+                                            kPrivacySecuritySymbol),
+                                        [UIColor colorNamed:kBlue500Color])
+            accessibilityIdentifier:kSettingsPrivacyCellId];
+  }
+
   return [self detailItemWithType:SettingsItemTypePrivacy
                              text:title
                        detailText:nil
@@ -971,6 +983,18 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
 }
 
 - (TableViewItem*)contentSettingsDetailItem {
+  if (UseSymbols()) {
+    return [self
+             detailItemWithType:SettingsItemTypeContentSettings
+                           text:l10n_util::GetNSString(
+                                    IDS_IOS_CONTENT_SETTINGS_TITLE)
+                     detailText:nil
+                     symbolView:ElevatedTableViewSymbolWithBackground(
+                                    DefaultSettingsRootSymbol(kGearShapeSymbol),
+                                    [UIColor colorNamed:kGrey500Color])
+        accessibilityIdentifier:kSettingsContentSettingsCellId];
+  }
+
   return [self detailItemWithType:SettingsItemTypeContentSettings
                              text:l10n_util::GetNSString(
                                       IDS_IOS_CONTENT_SETTINGS_TITLE)
@@ -989,6 +1013,17 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
 }
 
 - (TableViewItem*)aboutChromeDetailItem {
+  if (UseSymbols()) {
+    return [self detailItemWithType:SettingsItemTypeAboutChrome
+                               text:l10n_util::GetNSString(IDS_IOS_PRODUCT_NAME)
+                         detailText:nil
+                         symbolView:ElevatedTableViewSymbolWithBackground(
+                                        DefaultSettingsRootSymbol(
+                                            kInfoCircleSymbol),
+                                        [UIColor colorNamed:kGrey500Color])
+            accessibilityIdentifier:kSettingsAboutCellId];
+  }
+
   return [self detailItemWithType:SettingsItemTypeAboutChrome
                              text:l10n_util::GetNSString(IDS_IOS_PRODUCT_NAME)
                        detailText:nil
