a08d18ab58e0e569307aa46e2c586745ecda8878
Peter Kvitek <kvitekp@chromium.org>
2022-07-19 23:36:04 +0000

    [headless] Floor values explicitely when calculating margins.
    Change-Id: I21ca31e63f6cdb88249f1dc673ca523d9cd967c1
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773984
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026005}

--- components/printing/browser/print_to_pdf/pdf_print_utils.cc
+++ components/printing/browser/print_to_pdf/pdf_print_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/printing/browser/print_to_pdf/pdf_print_utils.h"
 
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -133,10 +134,14 @@ GetPrintPagesParams(const GURL& page_url,
     return "bottom margin is negative";
 
   printing::PageMargins margins_in_points;
-  margins_in_points.left = margin_left_in_inches * printing::kPointsPerInch;
-  margins_in_points.right = margin_right_in_inches * printing::kPointsPerInch;
-  margins_in_points.top = margin_top_in_inches * printing::kPointsPerInch;
-  margins_in_points.bottom = margin_bottom_in_inches * printing::kPointsPerInch;
+  margins_in_points.left =
+      base::ClampFloor(margin_left_in_inches * printing::kPointsPerInch);
+  margins_in_points.right =
+      base::ClampFloor(margin_right_in_inches * printing::kPointsPerInch);
+  margins_in_points.top =
+      base::ClampFloor(margin_top_in_inches * printing::kPointsPerInch);
+  margins_in_points.bottom =
+      base::ClampFloor(margin_bottom_in_inches * printing::kPointsPerInch);
   print_settings.SetCustomMargins(margins_in_points);
 
   double paper_width_in_inches =
