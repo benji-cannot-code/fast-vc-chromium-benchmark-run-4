62cf3e51e16803be729d75a1e3f51a50c6454b46
Alex Turner <alexmt@chromium.org>
2022-07-19 23:33:54 +0000

    Ensure budget storage database deletion task is posted early enough
    The lifetime of the PrivateAggregationBudgetStorage object is extended
    by the reply callback. This can sometimes extend the object's lifetime
    into the shutdown phase when new tasks may not be posted (e.g. to delete
    the database). So, we instead introduce a new Shutdown() method and pass
    a callback to it to the storage in the budgeter to run during
    initialization, if necessary.
    
    Bug: 1323320
    Change-Id: I08967a030b1315eb8ddf50cfe19db0ed133d6795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767862
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026004}

--- content/browser/private_aggregation/private_aggregation_budgeter.cc
+++ content/browser/private_aggregation/private_aggregation_budgeter.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
 #include "base/numerics/checked_math.h"
-#include "base/ranges/algorithm.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 #include "content/browser/private_aggregation/private_aggregation_budget_key.h"
@@ -55,7 +54,7 @@ PrivateAggregationBudgeter::PrivateAggregationBudgeter(
     bool exclusively_run_in_memory,
     const base::FilePath& path_to_db_dir) {
   DCHECK(db_task_runner);
-  PrivateAggregationBudgetStorage::CreateAsync(
+  shutdown_initializing_storage_ = PrivateAggregationBudgetStorage::CreateAsync(
       std::move(db_task_runner), exclusively_run_in_memory, path_to_db_dir,
       /*on_done_initializing=*/
       base::BindOnce(&PrivateAggregationBudgeter::OnStorageDoneInitializing,
@@ -64,7 +63,15 @@ PrivateAggregationBudgeter::PrivateAggregationBudgeter(
 
 PrivateAggregationBudgeter::PrivateAggregationBudgeter() = default;
 
-PrivateAggregationBudgeter::~PrivateAggregationBudgeter() = default;
+PrivateAggregationBudgeter::~PrivateAggregationBudgeter() {
+  if (shutdown_initializing_storage_) {
+    // As the budget storage's lifetime is extended until initialization is
+    // complete, its destructor could run after browser shutdown has begun (when
+    // tasks can no longer be posted). We post the database deletion task now
+    // instead.
+    std::move(shutdown_initializing_storage_).Run();
+  }
+}
 
 void PrivateAggregationBudgeter::ConsumeBudget(
     int budget,
@@ -88,6 +95,7 @@ void PrivateAggregationBudgeter::ConsumeBudget(
 
 void PrivateAggregationBudgeter::OnStorageDoneInitializing(
     std::unique_ptr<PrivateAggregationBudgetStorage> storage) {
+  DCHECK(shutdown_initializing_storage_);
   DCHECK(!storage_);
   DCHECK_EQ(storage_status_, StorageStatus::kInitializing);
 
@@ -97,6 +105,7 @@ void PrivateAggregationBudgeter::OnStorageDoneInitializing(
   } else {
     storage_status_ = StorageStatus::kInitializationFailed;
   }
+  shutdown_initializing_storage_.Reset();
 
   ProcessAllPendingCalls();
 }
--- content/browser/private_aggregation/private_aggregation_budgeter.h
+++ content/browser/private_aggregation/private_aggregation_budgeter.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
-#include "base/callback_forward.h"
+#include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "content/browser/private_aggregation/private_aggregation_budget_key.h"
@@ -117,6 +117,10 @@ class CONTENT_EXPORT PrivateAggregationBudgeter {
   // Otherwise, owned by this class until destruction. Iff present,
   // `storage_status_` should be `kOpen`.
   std::unique_ptr<PrivateAggregationBudgetStorage> storage_;
+
+  // Holds a closure that will shut down the initializing storage until
+  // initialization is complete. After then, it is null.
+  base::OnceClosure shutdown_initializing_storage_;
 
   base::WeakPtrFactory<PrivateAggregationBudgeter> weak_factory_{this};
 };
