fe7b02c8ddaee04d31a7cd9e08960c1ae81bda8c
Ben Reich <benreich@chromium.org>
2022-07-20 06:00:19 +0000

    [FilesTrash] Enable TrashService launch override to enable unit tests
    The RestoreIOTask has extensive unit tests and when migrating the logic
    to use the utility service, the process won't launch as starting up
    multiple processes in unit tests is not supported.
    
    Instead follow the pattern in
    //components/services/unzip/content/unzip_service.{cc,h} to
    override the launcher to enable an in process service to be setup.
    
    Bug: b:238943248
    Change-Id: Ia579154bbd60c1b5225e2c5df41f6d57dd82f24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773574
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026121}

--- chromeos/ash/components/trash_service/public/cpp/trash_service.cc
+++ chromeos/ash/components/trash_service/public/cpp/trash_service.cc
@@ -5,12 +5,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/trash_service/public/cpp/trash_service.h"
 
+#include <utility>
+
+#include "base/no_destructor.h"
 #include "chromeos/ash/components/trash_service/public/mojom/trash_service.mojom.h"
 #include "content/public/browser/service_process_host.h"
 
 namespace chromeos::trash_service {
 
+namespace {
+
+LaunchCallback& GetLaunchOverride() {
+  static base::NoDestructor<LaunchCallback> callback;
+  return *callback;
+}
+
+}  // namespace
+
 mojo::PendingRemote<mojom::TrashService> LaunchTrashService() {
+  auto& launcher = GetLaunchOverride();
+  if (launcher)
+    return launcher.Run();
+
   mojo::PendingRemote<mojom::TrashService> remote;
   content::ServiceProcessHost::Launch<mojom::TrashService>(
       remote.InitWithNewPipeAndPassReceiver(),
@@ -18,6 +34,10 @@ mojo::PendingRemote<mojom::TrashService> LaunchTrashService() {
           .WithDisplayName("ChromeOS Trash Service")
           .Pass());
   return remote;
+}
+
+void SetTrashServiceLaunchOverrideForTesting(LaunchCallback callback) {
+  GetLaunchOverride() = std::move(callback);
 }
 
 }  // namespace chromeos::trash_service
