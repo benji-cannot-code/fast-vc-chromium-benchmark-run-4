5ae2946fcc0ed90c9454a404618115f47f7baec9
Eric Sum <esum@google.com>
2022-07-20 16:10:19 +0000

    Add observer notification for when lottie animation is destroyed.
    This is for long-lived observes that may outlive the lottie animation.
    They can removed themselves and update their internal book-keeping
    when notified that an animation is being destroyed.
    
    Bug: b:224973194
    Change-Id: I0ef27f3b70eef252c189e19c7cd1a66c7d3106c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774537
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026268}

--- ui/lottie/animation_unittest.cc
+++ ui/lottie/animation_unittest.cc
@@ -108,6 +108,11 @@ class TestAnimationObserver : public AnimationObserver {
     last_frame_painted_ = t;
   }
 
+  void AnimationIsDeleting(const Animation* animation) override {
+    animation_is_deleted_ = true;
+    observation_.Reset();
+  }
+
   void Reset() {
     animation_cycle_ended_ = false;
     animation_will_start_playing_ = false;
@@ -119,6 +124,7 @@ class TestAnimationObserver : public AnimationObserver {
     return animation_will_start_playing_;
   }
   bool animation_resuming() const { return animation_resuming_; }
+  bool animation_is_deleted() const { return animation_is_deleted_; }
   const absl::optional<float>& last_frame_painted() const {
     return last_frame_painted_;
   }
@@ -128,6 +134,7 @@ class TestAnimationObserver : public AnimationObserver {
   bool animation_cycle_ended_ = false;
   bool animation_will_start_playing_ = false;
   bool animation_resuming_ = false;
+  bool animation_is_deleted_ = false;
   absl::optional<float> last_frame_painted_;
 };
 
@@ -1203,6 +1210,16 @@ TEST_F(AnimationTest, HandlesChangingAnimationStateWithinObserverCall) {
   animation_->Paint(canvas(), NowTicks(), animation_->GetOriginalSize());
   EXPECT_FLOAT_EQ(animation_->GetCurrentProgress(),
                   kAdvance / kAnimationDuration);
+}
+
+TEST_F(AnimationTest, NotifiesAnimationIsDeleting) {
+  TestAnimationObserver observer_1(animation_.get());
+  TestAnimationObserver observer_2(animation_.get());
+  ASSERT_FALSE(observer_1.animation_is_deleted());
+  ASSERT_FALSE(observer_2.animation_is_deleted());
+  animation_.reset();
+  EXPECT_TRUE(observer_1.animation_is_deleted());
+  EXPECT_TRUE(observer_2.animation_is_deleted());
 }
 
 }  // namespace lottie
