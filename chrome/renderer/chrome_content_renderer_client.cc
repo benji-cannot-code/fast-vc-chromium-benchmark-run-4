e438cdf55b9d99c3eaf3aed1f91e84102ab7649c
Patrick Noland <pnoland@google.com>
2022-07-20 19:59:45 +0000

    [Journeys] Fix related searches extraction on Android
    Bug: 1336276
    Change-Id: If099f8bff1aecfd97cea4dd3b18ed83e2e166eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774734
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026400}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -599,7 +599,6 @@ void ChromeContentRendererClient::RenderFrameCreated(
   if (render_frame->IsMainFrame())
     new webapps::WebPageMetadataAgent(render_frame);
 
-#if !BUILDFLAG(IS_ANDROID)
   const bool search_result_extractor_enabled =
       render_frame->IsMainFrame() &&
       history_clusters::GetConfig().is_journeys_enabled_no_locale_check &&
@@ -608,7 +607,6 @@ void ChromeContentRendererClient::RenderFrameCreated(
   if (search_result_extractor_enabled) {
     continuous_search::SearchResultExtractorImpl::Create(render_frame);
   }
-#endif
 
   new NetErrorHelper(render_frame);
 
