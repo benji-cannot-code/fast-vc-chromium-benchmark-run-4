e9513f038e39000bc4c7c0d4a758afde43f2288d
Danan S <danan@chromium.org>
2022-07-20 14:01:38 +0000

    Disable remote web permission request button after it is clicked.
    This is to prevent double request creation.
    
    Bug: b:211107409
    Change-Id: Ie56264f6165645fbc18e3f8222d9fa680d71aae8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764296
    Auto-Submit: Dan S <danan@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026221}

--- chrome/browser/supervised_user/supervised_user_error_page/resources/supervised_user_block_interstitial.js
+++ chrome/browser/supervised_user/supervised_user_error_page/resources/supervised_user_block_interstitial.js
@@ -96,6 +96,7 @@ function initialize() {
       $('remote-approvals-button').classList.add('secondary-button');
     }
     $('remote-approvals-button').onclick = function(event) {
+      $('remote-approvals-button').disabled = true;
       sendCommand('requestUrlAccessRemote');
     };
     $('local-approvals-button').onclick = function(event) {
@@ -179,7 +180,7 @@ function requestCreated(isSuccessful, isMainFrame) {
     }
   } else {
     $('request-failed-message').hidden = false;
-    $('remote-approvals-button').hidden = false;
+    $('remote-approvals-button').disabled = false;
     $('show-details-link').hidden = false;
   }
 }
