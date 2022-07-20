97cdc801f65dd4eacf633fe951f273b448c41a1f
Eric Willigers <ericwilligers@chromium.org>
2022-07-20 17:02:04 +0000

    Retire flag EncryptedMediaSessionClosedReason
    MediaKeySession Closed Reason shipped in M96.
    
    Bug: 1208618
    Change-Id: Ia8b3dbfd26406812e18a4d2e0466db9302367852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762195
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026293}

--- third_party/blink/renderer/modules/encryptedmedia/media_key_session.cc
+++ third_party/blink/renderer/modules/encryptedmedia/media_key_session.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/instance_counters.h"
 #include "third_party/blink/renderer/platform/network/mime/content_type.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/text/ascii_ctype.h"
 
@@ -996,12 +995,8 @@ void MediaKeySession::OnSessionClosed(media::CdmSessionClosedReason reason) {
   OnSessionExpirationUpdate(std::numeric_limits<double>::quiet_NaN());
 
   // 7. Resolve promise.
-  if (RuntimeEnabledFeatures::EncryptedMediaSessionClosedReasonEnabled()) {
-    closed_promise_->Resolve(
-        V8MediaKeySessionClosedReason(ConvertSessionClosedReason(reason)));
-  } else {
-    closed_promise_->ResolveWithUndefined();
-  }
+  closed_promise_->Resolve(
+      V8MediaKeySessionClosedReason(ConvertSessionClosedReason(reason)));
 
   // Fail any pending events, except if it's a close request.
   action_timer_.Stop();
