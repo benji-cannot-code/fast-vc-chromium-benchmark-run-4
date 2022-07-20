e6ad5173476190a701d0222eb7411aec2ac403c8
David Bokan <bokan@chromium.org>
2022-07-20 19:10:54 +0000

    Fix crash in text fragment anchor
    The TextFragmentAnchor attempts to keep a match in view when invoked
    while waiting to perform scriptable actions. However, the
    kScriptableActions state can be entered without any matches (see
    TextFragmentAnchor::DidFinishSearch) so it should only attempt to scroll
    first_match_ into view if a match was found.
    
    An existing test exercising this code path is slightly amended to hit
    this case.
    
    Bug: 1313253
    Change-Id: Ic61347212a8056a131eb0dd4fbe27b992fb0869a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3772913
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026375}

--- third_party/blink/renderer/core/fragment_directive/text_fragment_anchor_test.cc
+++ third_party/blink/renderer/core/fragment_directive/text_fragment_anchor_test.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/empty_clients.h"
 #include "third_party/blink/renderer/core/page/context_menu_controller.h"
+#include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/scroll/scrollable_area.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
@@ -1423,15 +1424,20 @@ TEST_F(TextFragmentAnchorTest, NoMatchFoundFallsBackToElementFragment) {
   )HTML");
   RunAsyncMatchingTasks();
 
-  Compositor().BeginFrame();
+  Element& p = *GetDocument().getElementById("element");
 
-  // The TextFragmentAnchor needs another frame to invoke the element anchor
+  // At this point, the anchor should exist and have entered kScriptableActions
+  // state without any matches. Ensure invoking (which can happen arbitrarily
+  // from layout which can  be forced from bindings) is a no-op.
+  ASSERT_TRUE(GetDocument().View()->GetFragmentAnchor());
+  GetDocument().View()->GetFragmentAnchor()->Invoke();
+  EXPECT_EQ(ScrollOffset(), LayoutViewport()->GetScrollOffset());
+
+  // In the next frame, the anchor will PerformScriptableActions and invoke the
+  // element anchor.
   Compositor().BeginFrame();
-  RunAsyncMatchingTasks();
 
   EXPECT_EQ(GetDocument().Url(), "https://example.com/test.html#element");
-
-  Element& p = *GetDocument().getElementById("element");
 
   EXPECT_EQ(p, *GetDocument().CssTarget());
   EXPECT_TRUE(ViewportRect().Contains(BoundingRectInFrame(p)))
