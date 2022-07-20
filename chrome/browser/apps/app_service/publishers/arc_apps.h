05b8a96687398d1098f671ace0b3fc94449813a6
Nancy Wang <nancylingwang@chromium.org>
2022-07-20 13:57:48 +0000

    Implement the non mojom LaunchAppWithIntent for ArcApps.
    This CL is used to prepare for CL:3774201 to implement the non mojom
    LaunchAppWithIntent interface.
    
    BUG=1253250
    
    Change-Id: I622d39da2e3b6fd7921793a7ede9c315c9583afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773576
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026218}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
+#include "components/services/app_service/public/cpp/intent.h"
 #include "components/services/app_service/public/cpp/publisher_base.h"
 #include "components/services/app_service/public/mojom/app_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -110,6 +111,12 @@ class ArcApps : public KeyedService,
               int32_t event_flags,
               LaunchSource launch_source,
               WindowInfoPtr window_info) override;
+  void LaunchAppWithIntent(const std::string& app_id,
+                           int32_t event_flags,
+                           IntentPtr intent,
+                           LaunchSource launch_source,
+                           WindowInfoPtr window_info,
+                           base::OnceCallback<void(bool)> callback);
   void LaunchAppWithParams(AppLaunchParams&& params,
                            LaunchCallback callback) override;
   void LaunchShortcut(const std::string& app_id,
