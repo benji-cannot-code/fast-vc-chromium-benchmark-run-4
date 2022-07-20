78150da50c5ab15c5b35ad2593cd452f9c811d16
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-07-20 20:39:27 +0000

    webui: Migrate /chrome/browser/ui/webui URLDataSources to GetMimeType(GURL)
    Changes to override the GURL variant of GetMimeType and where appropriate
    changes the overrides' implementations to use the GURL to get a path.
    
    Context:
    
    Many overrides of GetMimeType() do their own parsing, for example, to
    turn it back into a GURL[1][2][3], get query params[4], etc. This is
    dangerous specially when dealing with untrusted data. Additionally,
    the path passed to GetMimeType includes query params and not all
    implementations[5] take that into account.
    
    There is now a GetMimeType() virtual function that takes a GURL,
    so overrides can use methods like path(), GetPathForRequest(), query(),
    etc. to avoid doing manual parsing.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/search/most_visited_iframe_source.cc;l=58;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/devtools_ui_data_source.cc;l=35-40;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [3] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/test_data_source.cc;l=68;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [4] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/new_tab_page/untrusted_source.cc;l=196;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [5] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/web_applications/terminal_source.cc;l=183;drc=1560c59051181c15267c40ae37dae98c235feaa0
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1344742
    Change-Id: If32cc4a817528a199a9594e42ada603bcad4195c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774560
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026423}

--- chrome/browser/ui/webui/prefs_internals_source.cc
+++ chrome/browser/ui/webui/prefs_internals_source.cc
@@ -24,7 +24,7 @@ std::string PrefsInternalsSource::GetSource() {
   return chrome::kChromeUIPrefsInternalsHost;
 }
 
-std::string PrefsInternalsSource::GetMimeType(const std::string& path) {
+std::string PrefsInternalsSource::GetMimeType(const GURL& url) {
   return "text/plain";
 }
 
