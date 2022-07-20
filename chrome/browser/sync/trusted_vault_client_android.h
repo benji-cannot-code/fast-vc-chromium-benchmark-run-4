dd432c56224f639ad59921b3b56e9e9a6552bd4a
Mikel Astiz <mastiz@chromium.org>
2022-07-20 09:59:49 +0000

    [sync] Plumb API for adding recovery methods to Android Java code
    The code is currently unreachable because the API is never exposed to
    Javascript, but this is about to change with
    https://crrev.com/c/3738190.
    
    In this patch, the JNI plumbing is introduced, propagating the event
    from TrustedVaultClientAndroid to the newly-introduced
    TrustedVaultClient.Backend.addTrustedRecoveryMethod().
    
    Change-Id: I36ebb8aa1196844e3f047f2970c90fd000b00928
    Bug: 1341279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766679
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026169}

--- chrome/browser/sync/trusted_vault_client_android.h
+++ chrome/browser/sync/trusted_vault_client_android.h
@@ -26,7 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This class must be accessed from the UI thread.
 class TrustedVaultClientAndroid : public syncer::TrustedVaultClient {
  public:
-  TrustedVaultClientAndroid();
+  // Callback that returns account information identified by |gaia_id| or an
+  // empty CoreAccountInfo if the account is not found.
+  using GetAccountInfoByGaiaIdCallback =
+      base::RepeatingCallback<CoreAccountInfo(const std::string& gaia_id)>;
+
+  explicit TrustedVaultClientAndroid(
+      const GetAccountInfoByGaiaIdCallback& gaia_account_info_by_gaia_id_cb);
   ~TrustedVaultClientAndroid() override;
 
   TrustedVaultClientAndroid(const TrustedVaultClientAndroid&) = delete;
@@ -54,6 +60,11 @@ class TrustedVaultClientAndroid : public syncer::TrustedVaultClient {
   void GetIsRecoverabilityDegradedCompleted(JNIEnv* env,
                                             jint request_id,
                                             jboolean is_degraded);
+
+  // Called from Java to notify the completion of a
+  // AddTrustedRecoveryMethod() operation previously initiated from C++ and
+  // identified by |request_id|.
+  void AddTrustedRecoveryMethodCompleted(JNIEnv* env, jint request_id);
 
   // Called from Java to notify that the keys in the vault may have changed.
   void NotifyKeysChanged(JNIEnv* env);
@@ -118,13 +129,26 @@ class TrustedVaultClientAndroid : public syncer::TrustedVaultClient {
     base::OnceCallback<void(bool)> callback;
   };
 
+  // Struct representing an in-flight AddTrustedRecoveryMethod() invoked from
+  // C++.
+  struct OngoingAddTrustedRecoveryMethod {
+    explicit OngoingAddTrustedRecoveryMethod(base::OnceClosure callback);
+    OngoingAddTrustedRecoveryMethod(OngoingAddTrustedRecoveryMethod&&);
+    ~OngoingAddTrustedRecoveryMethod();
+
+    base::OnceClosure callback;
+  };
+
   using RequestId = int32_t;
   using OngoingRequest = absl::variant<OngoingFetchKeys,
                                        OngoingMarkLocalKeysAsStale,
-                                       OngoingGetIsRecoverabilityDegraded>;
+                                       OngoingGetIsRecoverabilityDegraded,
+                                       OngoingAddTrustedRecoveryMethod>;
 
   RequestId RegisterNewOngoingRequest(OngoingRequest request);
   OngoingRequest GetAndUnregisterOngoingRequest(RequestId id);
+
+  const GetAccountInfoByGaiaIdCallback gaia_account_info_by_gaia_id_cb_;
 
   // Each in-flight request gets assigned an auto-increment ID and gets cached
   // in a map, until the response from Java is received.
