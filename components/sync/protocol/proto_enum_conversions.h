db5c7a8ac6c8c1396d142df0459abcecb06678dc
Ken Buchanan <kenrb@chromium.org>
2022-07-20 15:43:01 +0000

    [WebAuthn] Change SPC value in protobuf to a boolean
    The tri-state enum is not necessary since there is no need for NONE.
    Credentials are enabled for payments on third-party sites, or can be
    used for first party only.
    
    Change-Id: Icaa9017c4ba12522d2d83a5881e9ded612f69b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773977
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026261}

--- components/sync/protocol/proto_enum_conversions.h
+++ components/sync/protocol/proto_enum_conversions.h
@@ -163,9 +163,6 @@ const char* ProtoEnumToString(
     sync_pb::UserConsentTypes::AssistantActivityControlConsent::SettingType
         setting_type);
 
-const char* ProtoEnumToString(
-    sync_pb::WebauthnCredentialSpecifics::PaymentsSupport payments_support);
-
 const char* ProtoEnumToString(sync_pb::WorkspaceDeskSpecifics::DeskType type);
 
 const char* ProtoEnumToString(
