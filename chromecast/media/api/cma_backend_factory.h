e7d1ef4eadf2aa74582288d7cb3166ca59338a0b
ziyangch <ziyangch@google.com>
2022-07-20 20:44:39 +0000

    [Chromecast] Add GetMediaTaskRunner in CmaBackendFactory
    Bug: internal b/203599632
    
    Test: Cast from Youtube
    
    Change-Id: I3868a178824a4167bc4c900e772e56a71919734a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767430
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Ziyang Cheng <ziyangch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026425}

--- chromecast/media/api/cma_backend_factory.h
+++ chromecast/media/api/cma_backend_factory.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/memory/scoped_refptr.h"
+#include "base/task/sequenced_task_runner.h"
+
 namespace service_manager {
 class Connector;
 }  // namespace service_manager
@@ -32,6 +35,9 @@ class CmaBackendFactory {
   // |media_task_runner_|.
   virtual std::unique_ptr<CmaBackend> CreateBackend(
       const MediaPipelineDeviceParams& params) = 0;
+
+  // Returns |media_task_runner_|.
+  virtual scoped_refptr<base::SequencedTaskRunner> GetMediaTaskRunner() = 0;
 };
 
 }  // namespace media
