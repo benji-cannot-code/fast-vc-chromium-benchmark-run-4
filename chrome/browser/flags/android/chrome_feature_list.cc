2a77d5135c3d9997f7c12b3db42745f659f02ac6
Basia Zimirska <basiaz@google.com>
2022-07-20 16:54:45 +0000

    Consent-less flow for Assistant recognition (no personalization).
    *** The proposed changes are pending privacy approvals ***.
    
    This cl includes:
    - adding a new flag controlling the new behavior
    - flagging off a couple of existing behaviors:
      1) hiding the menu toggle in the Google settings controlling the Assistant (not needed if no PII is sent or logged)
      2) not sending email, page url or info about source and current languages
      3) modifying the Assistant service to not require showing consent if
         the new flow is on
    
    Bug: 1344574
    Change-Id: I3de1eda7eb311f7c0d3cc6b355a6f5ad6fb553e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764260
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Florian Gauger <fga@google.com>
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026280}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -171,6 +171,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAssistantIntentExperimentId,
     &kAssistantIntentPageUrl,
     &kAssistantIntentTranslateInfo,
+    &kAssistantNonPersonalizedVoiceSearch,
     &kAppLaunchpad,
     &kAppMenuMobileSiteOption,
     &kAppToWebAttribution,
@@ -464,6 +465,9 @@ const base::Feature kAssistantIntentPageUrl{"AssistantIntentPageUrl",
 
 const base::Feature kAssistantIntentTranslateInfo{
     "AssistantIntentTranslateInfo", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kAssistantNonPersonalizedVoiceSearch{
+    "AssistantNonPersonalizedVoiceSearch", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAppLaunchpad{"AppLaunchpad",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
