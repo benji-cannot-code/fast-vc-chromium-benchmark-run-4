c29d5688709b7f27ce59ba8e6db7443511b86506
Lakshmi Lokesh <lakshmilokesh@google.com>
2022-07-20 09:26:00 +0000

    [Autofill Assistant] Auto open keyboard in Input text box
    Demo: https://drive.google.com/file/d/1G9yqWKtQv8WfH0Iil-jN5JNiOAxTIgvf/view?usp=sharing&resourcekey=0-aZTdXl-jFf8nuXdVKRzQFA
    
    
    Bug: b/237371327
    Change-Id: I5a9056e3ace331dfcea59eb8235c296df5e9f21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755621
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Shashank Jain <jainshashank@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Lakshmi Lokesh <lakshmilokesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026163}

--- components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/generic_ui/AssistantViewFactory.java
+++ components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/generic_ui/AssistantViewFactory.java
@@ -159,7 +159,8 @@ public class AssistantViewFactory {
     /** Creates a {@code EditorTextField} view. */
     @CalledByNative
     public static View createTextInputView(Context context, AssistantGenericUiDelegate delegate,
-            String viewIdentifier, int type, String hint, String modelIdentifier) {
+            String viewIdentifier, int type, String hint, String modelIdentifier,
+            boolean focusAndShowKeyboard) {
         View view = new EditorTextField(context,
                 EditorFieldModel.createTextInput(type, hint, /* suggestions = */ null,
                         /* formatter = */ null, /* validator = */ null,
@@ -181,7 +182,7 @@ public class AssistantViewFactory {
                         delegate.onValueChanged(modelIdentifier,
                                 AssistantValue.createForStrings(new String[] {value.toString()}));
                     }
-                }, /* focusAndShowKeyboard= */ false);
+                }, focusAndShowKeyboard);
         view.setTag(viewIdentifier);
         return view;
     }
