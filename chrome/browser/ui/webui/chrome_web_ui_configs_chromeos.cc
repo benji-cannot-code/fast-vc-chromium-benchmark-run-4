d358a4af4dd336a53c3cc1bc1187fe7057eba8d3
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-07-20 02:55:08 +0000

    webui: Migrate NotificationTesterUI to WebUIConfig
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Change-Id: I7c679fa578c0f3c8acf8393e49dfac99b8904847
    Bug: 1317510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769677
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026079}

--- chrome/browser/ui/webui/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs_chromeos.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/webui_config_map.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ui/webui/chromeos/notification_tester/notification_tester_ui.h"
 #if !defined(OFFICIAL_BUILD)
 #include "ash/webui/sample_system_web_app_ui/sample_system_web_app_ui.h"
 #endif  // !defined(OFFICIAL_BUILD)
@@ -18,8 +19,9 @@ namespace {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 void RegisterAshChromeWebUIConfigs() {
   // Add `WebUIConfig`s for Ash ChromeOS to the list here.
-#if !defined(OFFICIAL_BUILD)
   auto& map = content::WebUIConfigMap::GetInstance();
+  map.AddWebUIConfig(std::make_unique<chromeos::NotificationTesterUIConfig>());
+#if !defined(OFFICIAL_BUILD)
   map.AddWebUIConfig(std::make_unique<ash::SampleSystemWebAppUIConfig>());
 #endif  // !defined(OFFICIAL_BUILD)
 }
