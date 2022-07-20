45ad3fa1ca35bbf71abddcfcb1d4897564885860
Sylvain Defresne <sdefresne@chromium.org>
2022-07-20 22:52:14 +0000

    [ios] Additional fixes in FollowTabHelper
    Fix a last block that captures `this` and thus could lead to
    undefined behaviour (due to use-after-free) if a tab is closed
    immediately after the navigation completes.
    
    Fix style of the code, mostly how non-inline functions are named,
    using `{}` for complex `if` blocks, and backticks instead of pipe
    when referencing variable names in comments.
    
    Fix some typos in comments.
    
    Bug: none
    Change-Id: I2c004902cdbef328d0d76ac7bdb58b433be7c98f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3772430
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026497}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -256,7 +256,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
   }
 
   if (self.webState && self.followAction) {
-    FollowTabHelper::FromWebState(self.webState)->remove_follow_menu_updater();
+    FollowTabHelper::FromWebState(self.webState)->RemoveFollowMenuUpdater();
   }
 
   self.destinationUsageHistory = nil;
@@ -314,7 +314,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
   self.webState = (_webStateList) ? _webStateList->GetActiveWebState() : nil;
 
   if (self.webState && !self.isIncognito && IsWebChannelsEnabled()) {
-    FollowTabHelper::FromWebState(self.webState)->set_follow_menu_updater(self);
+    FollowTabHelper::FromWebState(self.webState)->SetFollowMenuUpdater(self);
   }
 
   if (_webStateList) {
@@ -1168,7 +1168,7 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
                      reason:(ActiveWebStateChangeReason)reason {
   self.webState = newWebState;
   if (self.webState && self.followAction) {
-    FollowTabHelper::FromWebState(self.webState)->set_follow_menu_updater(self);
+    FollowTabHelper::FromWebState(self.webState)->SetFollowMenuUpdater(self);
   }
 }
 
