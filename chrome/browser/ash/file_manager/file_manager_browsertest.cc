6f434583c4a5dcf0ece781b963d27ac0a7d3f5e8
Maggie Cai <mxcai@chromium.org>
2022-07-20 07:47:20 +0000

    Revert "[Files App] update gear menu with Pooled Storage changes"
    This reverts commit bb5a02c8b12bba9a930bb5cdd4ecdaf0bf2bcc63.
    
    Reason for revert: This is possibly cause tast test failures on a few builders.
    Failures: filemanager.DrivefsUI, arc.Drivefs
    First failures: https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-eve-chrome/23584/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-cfi-thin-lto-chrome/37593/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-kevin-chrome/23824/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome/23964/overview
    
    Original change's description:
    > [Files App] update gear menu with Pooled Storage changes
    >
    > The gear menu now accounts for Pooled Storage, using
    > a new endpoint specifically made for Drive to fetch
    > Drive metadata related to the user's quota. Therefore it's
    > now able to display special cases for unlimited users
    > and exceeded organization quota.
    >
    > Bug: 1324367
    > Test: Manual && browser_tests --gtest_filter=*.DriveSyncHandlerTest && browser_tests --gtest_filter=*GearMenu*
    > Change-Id: I3823bd43fd0639d655fdc66fca1dd3a5b67b89d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755062
    > Reviewed-by: Austin Tankiang <austinct@chromium.org>
    > Auto-Submit: Marcello Salomao <msalomao@google.com>
    > Commit-Queue: Austin Tankiang <austinct@chromium.org>
    > Commit-Queue: Marcello Salomao <msalomao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1026060}
    
    Bug: 1324367
    Change-Id: Ic3b6209f1d546b4f4617a0a2eee8f5812066bbdc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774565
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026144}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1640,8 +1640,10 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("enableDisableStorageSettingsLink").FilesSwa(),
         TestCase("showAvailableStorageMyFiles"),
         TestCase("showAvailableStorageMyFiles").FilesSwa(),
-        TestCase("showAvailableStorageDrive"),
-        TestCase("showAvailableStorageDrive").FilesSwa(),
+        // Disabled until Drive quota can be properly displayed.
+        // crbug.com/1177203
+        // TestCase("showAvailableStorageDrive"),
+        // TestCase("showAvailableStorageDrive").FilesSwa(),
         TestCase("showAvailableStorageSmbfs"),
         TestCase("showAvailableStorageSmbfs").FilesSwa(),
         TestCase("showAvailableStorageDocProvider")
