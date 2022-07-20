2f6f3cec696de9cfeebde601f15a1a30108cd528
Ed Chin <edchin@google.com>
2022-07-20 01:48:36 +0000

    [ios][feed] Add additional experimental settings for Background Refresh
    There are too many permutations of background refresh configurations
    to put into about_flags variations.
    
    These are supplemental configs, intended only for manipulation by
    dev, canary, and beta users. Their default values are used in the
    about_flags variations.
    
    Screenshot:
    https://screenshot.googleplex.com/4uN6erKnR8JLYRr.png
    
    Bug: 1343695
    Change-Id: Iaf0cbcd29368d1c2da9ccb0228c276baa3a46474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774402
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026057}

--- ios/chrome/browser/system_flags.mm
+++ ios/chrome/browser/system_flags.mm
@@ -37,6 +37,12 @@ NSString* const kWhatsNewPromoStatus = @"WhatsNewPromoStatus";
 NSString* const kClearApplicationGroup = @"ClearApplicationGroup";
 const base::Feature kEnableThirdPartyKeyboardWorkaround{
     "EnableThirdPartyKeyboardWorkaround", base::FEATURE_ENABLED_BY_DEFAULT};
+
+NSString* const kForceBackgroundRefreshForFollowingFeedEnabled =
+    @"ForceBackgroundRefreshForFollowingFeedEnabled";
+NSString* const kBackgroundRefreshMaxAgeInSeconds =
+    @"BackgroundRefreshMaxAgeInSeconds";
+
 }  // namespace
 
 namespace experimental_flags {
@@ -85,6 +91,16 @@ void DidResetFirstFollowCount() {
 bool ShouldAlwaysShowFirstFollow() {
   return [[NSUserDefaults standardUserDefaults]
       boolForKey:@"AlwaysShowFirstFollow"];
+}
+
+bool IsForceBackgroundRefreshForFollowingFeedEnabled() {
+  return [[NSUserDefaults standardUserDefaults]
+      boolForKey:kForceBackgroundRefreshForFollowingFeedEnabled];
+}
+
+double GetBackgroundRefreshMaxAgeInSeconds() {
+  return [[NSUserDefaults standardUserDefaults]
+      doubleForKey:kBackgroundRefreshMaxAgeInSeconds];
 }
 
 bool IsMemoryDebuggingEnabled() {
