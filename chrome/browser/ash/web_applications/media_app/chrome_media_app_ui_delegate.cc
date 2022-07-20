21ceab3fc30e5d31256b0013ba7ff72c51bd2f16
Tim Sergeant <tsergeant@chromium.org>
2022-07-20 00:01:45 +0000

    Media app: Pass Intent extra to keep Photos open after editing
    Photos closes by default after saving an edited file. This CL overwrites
    this behavior by passing through a new extra in the launch Intent, which
    instructs Photos to stay open after editing.
    
    To make this work, we need to pass all of the Intent extras through the
    ARC OpenUrlRequest, rather than just the shared text and title.
    
    Bug: b/239134613
    Change-Id: Ief2495336e814d6567182240ba704b200d3ea2d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764790
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026013}

--- chrome/browser/ash/web_applications/media_app/chrome_media_app_ui_delegate.cc
+++ chrome/browser/ash/web_applications/media_app/chrome_media_app_ui_delegate.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/media_app_ui/file_system_access_helpers.h"
 #include "ash/webui/media_app_ui/url_constants.h"
 #include "base/bind.h"
+#include "base/containers/flat_map.h"
 #include "base/notreached.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -134,6 +135,10 @@ void ChromeMediaAppUIDelegate::EditInPhotosImpl(
     const std::string& mime_type,
     base::OnceCallback<void()> edit_in_photos_callback,
     absl::optional<storage::FileSystemURL> url) {
+  constexpr char kPhotosKeepOpenExtraName[] =
+      "com.google.android.apps.photos.editor.contract.keep_photos_open";
+  constexpr char kPhotosKeepOpenExtraValue[] = "true";
+
   if (!url.has_value()) {
     std::move(edit_in_photos_callback).Run();
     return;
@@ -149,6 +154,8 @@ void ChromeMediaAppUIDelegate::EditInPhotosImpl(
       &filesystem_url);
 
   auto intent = apps_util::CreateEditIntentFromFile(filesystem_url, mime_type);
+  intent->extras = {
+      std::make_pair(kPhotosKeepOpenExtraName, kPhotosKeepOpenExtraValue)};
 
   proxy->LaunchAppWithIntent(
       arc::kGooglePhotosAppId, ui::EF_NONE, std::move(intent),
