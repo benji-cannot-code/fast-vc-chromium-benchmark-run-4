06fe205517520a0785c7f098565e099676d7bc74
Ian McKellar <ianloic@chromium.org>
2022-07-20 20:23:41 +0000

    [base] Support binding noexcept functors
    In C++17 a function with the noexcept specifier has a different
    signature than the same without the noexcept specifier. See:
    https://en.cppreference.com/w/cpp/language/noexcept_spec#Explanation
    
    The base::BindOnce function does not support functors with the noexcept
    specifier in C++17. It works for functions but the
    base::internal::ExtractCallableRuntypeImpl helper template doesn't
    include specializations for noexcept functors.
    
    Support for noexcept functions was added in https://crrev.com/c/1027530
    
    Bug: 1346020
    Change-Id: I7c24ddb4005a1a12f996d85c3a8e5babb6a57be8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777773
    Auto-Submit: Ian McKellar <ianloic@chromium.org>
    Commit-Queue: Ian McKellar <ianloic@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026410}

--- base/bind_unittest.cc
+++ base/bind_unittest.cc
@@ -320,6 +320,16 @@ int Noexcept() noexcept {
   return 42;
 }
 
+class NoexceptFunctor {
+ public:
+  int operator()() noexcept { return 42; }
+};
+
+class ConstNoexceptFunctor {
+ public:
+  int operator()() noexcept { return 42; }
+};
+
 class BindTest : public ::testing::Test {
  public:
   BindTest() {
@@ -1735,6 +1745,8 @@ TEST_F(BindTest, BindNoexcept) {
   EXPECT_EQ(
       42, base::BindOnce(&BindTest::ConstNoexceptMethod, base::Unretained(this))
               .Run());
+  EXPECT_EQ(42, base::BindOnce(NoexceptFunctor()).Run());
+  EXPECT_EQ(42, base::BindOnce(ConstNoexceptFunctor()).Run());
 }
 
 int PingPong(int* i_ptr) {
