e3c6dffb50eefbf95b9791ecf48e703f8ccf394d
Ed Chin <edchin@google.com>
2022-07-21 00:46:46 +0000

    [ios][feed] Fill out Background Refresh logic
    This CL DOES NOT register or schedule background refreshes.
    It merely adds logic to handle the task and scheduling.
    
    Bug: 1343695
    Change-Id: Ic2101dd800e9f3035e52f9c31c504cd417e3ac1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778326
    Auto-Submit: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026555}

--- ios/chrome/app/feed_app_agent.h
+++ ios/chrome/app/feed_app_agent.h
@@ -13,12 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // a Website, etc. This also manages feed background refresh.
 // TODO(crbug.com/1344866): Coordinate background tasks when more are added.
 @interface FeedAppAgent : SceneObservingAppAgent
-// Registers handler for the background refresh task. According to
-// documentation, this must complete before the end of
-// `applicationDidFinishLaunching`.
-// TODO(crbug.com/1343695): Do not call this until it is protected by a build
-// flag.
-- (void)registerBackgroundRefreshTask;
 @end
 
 #endif  // IOS_CHROME_APP_FEED_APP_AGENT_H_
