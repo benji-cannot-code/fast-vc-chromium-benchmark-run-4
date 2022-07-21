1d11e5e84176430e5d70999204113e76b84d6a92
Cici Ruan <cuicuiruan@google.com>
2022-07-21 17:19:11 +0000

    arc/gio: Fix the issue for unregistering the window in edit mode
    This CL removes pending input bind if the window is unfocused and
    unregistered from GIO. Pending input bind is created in edit mode and
    there is a change.
    
          after unassigned and change focus.
    
    Bug: b:239760049
    Test: unittest and manual test to check if the label disappeared
    Change-Id: I15faa26155c96641718d5d048867903c77578642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778747
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026835}

--- chrome/browser/ash/arc/input_overlay/touch_injector.cc
+++ chrome/browser/ash/arc/input_overlay/touch_injector.cc
@@ -178,6 +178,9 @@ void TouchInjector::UnRegisterEventRewriter() {
     return;
   DispatchTouchCancelEvent();
   observation_.Reset();
+  // Need reset pending input bind if it is unregistered in edit mode.
+  for (auto& action : actions_)
+    action->ResetPendingBind();
 }
 
 void TouchInjector::Update() {
