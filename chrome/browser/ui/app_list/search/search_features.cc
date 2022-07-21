024063df9e4a80dfcd742ae1a5fa6181d5786d1b
Rachel Wong <wrong@chromium.org>
2022-07-21 06:23:34 +0000

    [games] Only enable game search for the productivity launcher.
    Result ranking and fairness is a major consideration for gaming search,
    and we are not doing any testing for this in the classic launcher.
    So only allow the feature for the productivity launcher.
    
    Bug: 1305880
    Change-Id: I4c22a20e70d13942e65356b4717b8e812075f8f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777803
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026643}

--- chrome/browser/ui/app_list/search/search_features.cc
+++ chrome/browser/ui/app_list/search/search_features.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/app_list/search/search_features.h"
 
+#include "ash/constants/ash_features.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -15,6 +16,9 @@ const base::Feature kLauncherGameSearch{"LauncherGameSearch",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsLauncherGameSearchEnabled() {
+  if (!ash::features::IsProductivityLauncherEnabled())
+    return false;
+
   return base::FeatureList::IsEnabled(kLauncherGameSearch) ||
          chromeos::features::IsCloudGamingDeviceEnabled();
 }
