19e1803fe1b581ff0b92e3bcfe39501fdef94c18
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-07-21 16:24:07 +0000

    [Traffic Annotations] Prevent concurrent sheet updates
    Now that bots like linux-chromeos-annotator-rel and win-annotator-rel
    also run on release branches, we can have multiple bots trying to
    update the same Google Sheets sheet at the same time.
    
    Add a guard, that checks the version stored in the sheet and compares
    it against the current checkout's Chrome version (from chrome/VERSION).
    If the sheet is newer than this Chrome version, don't update the
    sheet.
    
    Also remove the 'Changes Stats' logic, as it's not useful and adds a
    bit of complexity to this script.
    
    TESTED=ran update_annotations_sheet.py manually
    
    Bug: 1344197
    Change-Id: I213fdeec178a9a093726420d7e99ddbe195c2b85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777698
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026810}

--- testing/scripts/test_traffic_annotation_auditor.py
+++ testing/scripts/test_traffic_annotation_auditor.py
@@ -24,7 +24,7 @@ from scripts import common
 WINDOWS_SHEET_CONFIG = {
   "spreadsheet_id": "1TmBr9jnf1-hrjntiVBzT9EtkINGrtoBYFMWad2MBeaY",
   "annotations_sheet_name": "Annotations",
-  "changes_sheet_name": "Changes Stats",
+  "chrome_version_sheet_name": "Chrome Version",
   "silent_change_columns": [],
   "last_update_column_name": "Last Update",
 }
@@ -33,7 +33,7 @@ WINDOWS_SHEET_CONFIG = {
 CHROMEOS_SHEET_CONFIG = {
   "spreadsheet_id": "1928goWKy6LVdF9Nl5nV1OD260YC10dHsdrnHEGdGsg8",
   "annotations_sheet_name": "Annotations",
-  "changes_sheet_name": "Changes Stats",
+  "chrome_version_sheet_name": "Chrome Version",
   "silent_change_columns": [],
   "last_update_column_name": "Last Update",
 }
@@ -106,7 +106,7 @@ def main_run(args):
         vpython_path,
         os.path.join(common.SRC_DIR, 'tools', 'traffic_annotation', 'scripts',
                    'update_annotations_sheet.py'),
-        '--force',
+        '--yes',
         '--config-file',
         config_filename,
         '--annotations-file',
