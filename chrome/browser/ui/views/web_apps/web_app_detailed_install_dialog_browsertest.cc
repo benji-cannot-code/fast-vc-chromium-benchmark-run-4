b4ae75fd9b12c4f0e0f9440e481853db60e3f4e4
Phillis Tang <phillis@google.com>
2022-07-21 22:19:46 +0000

    DPWA: add more tests for detailed install dialog
    Bug: 1326252
    Change-Id: I20d1dc4a20572c5a126c76ebb3569d21a9226c8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3771064
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027000}

--- chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog_browsertest.cc
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
-#include "chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.h"
-#include "chrome/browser/web_applications/test/web_app_icon_test_utils.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
+#include "components/webapps/common/constants.h"
 #include "content/public/test/browser_test.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace web_app {
@@ -24,13 +23,25 @@ class WebAppDetailedInstallDialogBrowserTest : public DialogBrowserTest {
     install_info->title = u"test";
     install_info->description = u"This is a test app";
 
-    AddGeneratedIcon(&install_info->icon_bitmaps.any, kIconSize, kIconColor);
+    install_info->icon_bitmaps.any[kIconSize] =
+        CreateSolidColorIcon(kIconSize, kIconSize, kIconColor);
 
-    const std::vector<SkBitmap> screenshots = {
-        CreateSquareIcon(kScreenshotSize, SK_ColorGREEN),
-        CreateSquareIcon(kScreenshotSize, SK_ColorBLACK),
-        CreateSquareIcon(kScreenshotSize, SK_ColorBLUE)};
-
+    std::vector<SkBitmap> screenshots;
+    if (name == "single_screenshot") {
+      screenshots.push_back(CreateSolidColorIcon(
+          kScreenshotSize, kScreenshotSize, SK_ColorGREEN));
+    } else if (name == "multiple_screenshots") {
+      screenshots.push_back(CreateSolidColorIcon(
+          kScreenshotSize, kScreenshotSize, SK_ColorGREEN));
+      screenshots.push_back(CreateSolidColorIcon(
+          kScreenshotSize, kScreenshotSize, SK_ColorBLACK));
+      screenshots.push_back(
+          CreateSolidColorIcon(kScreenshotSize, kScreenshotSize, SK_ColorBLUE));
+    } else if (name == "max_ratio_screenshot") {
+      screenshots.push_back(CreateSolidColorIcon(
+          webapps::kMaximumScreenshotRatio * kScreenshotSize, kScreenshotSize,
+          SK_ColorGREEN));
+    }
     chrome::ShowWebAppDetailedInstallDialog(
         browser()->tab_strip_model()->GetWebContentsAt(0),
         std::move(install_info), base::DoNothing(), screenshots,
@@ -38,13 +49,30 @@ class WebAppDetailedInstallDialogBrowserTest : public DialogBrowserTest {
   }
 
  private:
+  SkBitmap CreateSolidColorIcon(int width, int height, SkColor color) {
+    SkBitmap bitmap;
+    bitmap.allocN32Pixels(width, height);
+    bitmap.eraseColor(color);
+    return bitmap;
+  };
+
   static constexpr int kIconSize = 40;
   static constexpr int kScreenshotSize = 300;
   static constexpr SkColor kIconColor = SK_ColorGREEN;
 };
 
 IN_PROC_BROWSER_TEST_F(WebAppDetailedInstallDialogBrowserTest,
-                       InvokeUi_Default) {
+                       InvokeUi_single_screenshot) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppDetailedInstallDialogBrowserTest,
+                       InvokeUi_multiple_screenshots) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppDetailedInstallDialogBrowserTest,
+                       InvokeUi_max_ratio_screenshot) {
   ShowAndVerifyUi();
 }
 
