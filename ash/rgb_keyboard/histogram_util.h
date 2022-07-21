74df829228dbc25e344132414f451ac03254b538
Michael Checo <michaelcheco@google.com>
2022-07-21 22:36:46 +0000

    rgbkbd: Add metric for type of device used when rgb backlight changes
    Bug: b/214077425
    Test: ash_unittests
    Change-Id: I8cbdb3c42de940dd8c89aff6a4a3c43ebb021060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760422
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027009}

--- ash/rgb_keyboard/histogram_util.h
+++ ash/rgb_keyboard/histogram_util.h
@@ -6,16 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_RGB_KEYBOARD_HISTOGRAM_UTIL_H_
 #define ASH_RGB_KEYBOARD_HISTOGRAM_UTIL_H_
 
+#include <string>
+
+#include "ash/ash_export.h"
 #include "third_party/cros_system_api/dbus/rgbkbd/dbus-constants.h"
 
 namespace ash::rgb_keyboard::metrics {
-
+constexpr char kRgbKeyboardHistogramPrefix[] = "ChromeOS.RgbKeyboard.";
 constexpr char kRgbKeyboardCapabilityTypeHistogramName[] =
     "ChromeOS.RgbKeyboard.RgbKeyboardCapabilityType";
 
 // The enums below are used in histograms, do not remove/renumber entries. If
 // you're adding to any of these enums, update the corresponding enum listing in
-// tools/metrics/histograms/enums.xml: RgbKeyboardCapabilityType.
+// tools/metrics/histograms/enums.xml:
+// RgbKeyboardCapabilityType/RgbKeyboardBacklightChangeType.
 // RgbKeyboardCapabilityType gets its values from RgbKeyboardCapabilities
 // defined in: third_party/cros_system_api/dbus/rgbkbd/dbus-constants.h.
 enum class RgbKeyboardCapabilityType {
@@ -27,8 +31,20 @@ enum class RgbKeyboardCapabilityType {
   kMaxValue = kFourZoneFifteenLed,
 };
 
+enum class RgbKeyboardBacklightChangeType {
+  kStaticBackgroundColorChanged = 0,
+  kRainbowModeSelected = 1,
+  kMaxValue = kRainbowModeSelected,
+};
+
+ASH_EXPORT std::string GetCapabilityTypeStr(
+    rgbkbd::RgbKeyboardCapabilities capabilities);
+
 void EmitRgbKeyboardCapabilityType(
     rgbkbd::RgbKeyboardCapabilities capabilities);
+
+void EmitRgbBacklightChangeType(RgbKeyboardBacklightChangeType type,
+                                rgbkbd::RgbKeyboardCapabilities capabilities);
 
 }  // namespace ash::rgb_keyboard::metrics
 
--- ash/rgb_keyboard/rgb_keyboard_manager.cc
+++ ash/rgb_keyboard/rgb_keyboard_manager.cc
@@ -70,6 +70,10 @@ void RgbKeyboardManager::SetStaticBackgroundColor(uint8_t r,
 
   VLOG(1) << "Setting RGB keyboard color to R:" << static_cast<int>(r)
           << " G:" << static_cast<int>(g) << " B:" << static_cast<int>(b);
+  ash::rgb_keyboard::metrics::EmitRgbBacklightChangeType(
+      ash::rgb_keyboard::metrics::RgbKeyboardBacklightChangeType::
+          kStaticBackgroundColorChanged,
+      capabilities_);
   RgbkbdClient::Get()->SetStaticBackgroundColor(r, g, b);
 }
 
@@ -81,6 +85,10 @@ void RgbKeyboardManager::SetRainbowMode() {
   }
 
   VLOG(1) << "Setting RGB keyboard to rainbow mode";
+  ash::rgb_keyboard::metrics::EmitRgbBacklightChangeType(
+      ash::rgb_keyboard::metrics::RgbKeyboardBacklightChangeType::
+          kRainbowModeSelected,
+      capabilities_);
   RgbkbdClient::Get()->SetRainbowMode();
 }
 
