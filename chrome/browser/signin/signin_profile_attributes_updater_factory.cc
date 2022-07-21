63fde9ab5607436a78d30c79a262c8eeedeed74b
Ryan Sultanem <rsult@google.com>
2022-07-21 15:55:03 +0000

    ProfileKeyedServiceFactory implementaion: Fix Flaky tests
    Bug: 1284664
    Change-Id: I9441ac1a185d6046df7141cec6819406467793b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780099
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026788}

--- chrome/browser/signin/signin_profile_attributes_updater_factory.cc
+++ chrome/browser/signin/signin_profile_attributes_updater_factory.cc
@@ -11,17 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_profile_attributes_updater.h"
 
-namespace {
-ProfileSelections BuildServicesForSigninProfileAttributesUpdater() {
-  // Some tests don't have a ProfileManager, disable this service.
-  if (!g_browser_process || !g_browser_process->profile_manager()) {
-    return ProfileSelections::BuildNoServicesForAllProfiles();
-  }
-
-  return ProfileSelections::BuildDefault();
-}
-}  // namespace
-
 // static
 SigninProfileAttributesUpdater*
 SigninProfileAttributesUpdaterFactory::GetForProfile(Profile* profile) {
@@ -36,9 +25,7 @@ SigninProfileAttributesUpdaterFactory::GetInstance() {
 }
 
 SigninProfileAttributesUpdaterFactory::SigninProfileAttributesUpdaterFactory()
-    : ProfileKeyedServiceFactory(
-          "SigninProfileAttributesUpdater",
-          BuildServicesForSigninProfileAttributesUpdater()) {
+    : ProfileKeyedServiceFactory("SigninProfileAttributesUpdater") {
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
@@ -48,6 +35,11 @@ SigninProfileAttributesUpdaterFactory::
 KeyedService* SigninProfileAttributesUpdaterFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
+
+  // Some tests don't have a ProfileManager, disable this service.
+  if (!g_browser_process->profile_manager()) {
+    return nullptr;
+  }
 
   return new SigninProfileAttributesUpdater(
       IdentityManagerFactory::GetForProfile(profile),
