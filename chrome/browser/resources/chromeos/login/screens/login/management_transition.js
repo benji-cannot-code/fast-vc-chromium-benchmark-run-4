57f2df4098957477d6867ee8798d03a92ccde66e
Brahim Chikhaoui <bchikhaoui@google.com>
2022-07-21 10:45:57 +0000

    [OOBE][ManagementTransition]: Update screen and handler structure
    - Update CallJS ;
    - Remove Bind/Unbind logic;
    - Remove OnViewDestroyed;
    - Remove InitializeDeprecated();
    - Migrate to OnUserAction with args list and external API prefix;
    - Remove RegisterMessages() and OnManagementTransitionFinished Logic
     from handler and moved to Screen;
    - Update browserTest as we moved GetTimerForTesting to screen.
    
    Bug: 1345236, 1309022, 1298392
    Change-Id: I089ab80a0ddef9b77d6f65428fdffc9d3d868764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766743
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026701}

--- chrome/browser/resources/chromeos/login/screens/login/management_transition.js
+++ chrome/browser/resources/chromeos/login/screens/login/management_transition.js
@@ -149,7 +149,7 @@ class ManagementTransitionScreen extends ManagementTransitionScreenBase {
    * @private
    */
   onAcceptAndContinue_() {
-    chrome.send('finishManagementTransition');
+    this.userActed(['finish-management-transition']);
   }
 }
 
