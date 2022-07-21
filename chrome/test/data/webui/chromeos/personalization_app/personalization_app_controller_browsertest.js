c3f9e33dcc722d297e53a1836efac4bfba760c7d
Jeffrey Young <cowmoo@google.com>
2022-07-21 14:32:18 +0000

    personalization: ban mojom calls during some tests
    Mojom calls are mocked out in js during browser_tests.
    Make sure that no real mojom calls are issued by setting up
    StrictMock versions of all the mojom providers during test.
    
    Move calls to WallpaperObserver.initWallpaperObserverIfNeeded
    after polymer component initialization to prevent real
    PersonalizationAppWallpaperProviderImpl::BindInterface calls
    being issued during test.
    
    BUG=b:239638634
    TEST="browser_tests --gtest_filter="*PersonalizationApp*"
    
    Change-Id: I37953f37fe4586f8d91b031a530665100f108750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777320
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026762}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_controller_browsertest.js
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_controller_browsertest.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
 GEN('#include "ash/constants/ash_features.h"');
+GEN('#include "ash/webui/personalization_app/test/personalization_app_mojom_banned_browsertest_fixture.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
 var PersonalizationAppControllerBrowserTest = class extends PolymerTest {
@@ -18,6 +19,11 @@ var PersonalizationAppControllerBrowserTest = class extends PolymerTest {
     return 'chrome://personalization/test_loader.html' +
         '?module=chromeos/personalization_app/' +
         'personalization_app_controller_test.js';
+  }
+
+  get typedefCppFixture() {
+    return 'ash::personalization_app::' +
+        'PersonalizationAppMojomBannedBrowserTestFixture';
   }
 };
 
