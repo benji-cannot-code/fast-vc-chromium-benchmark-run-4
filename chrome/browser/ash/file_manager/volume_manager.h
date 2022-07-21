57d6b9a5146850a7bfd18c92a2bea65a8ee1513e
Noel Gordon <noel@chromium.org>
2022-07-21 10:33:32 +0000

    fusebox: restore FuseBox FSP volumes in VolumeManager::Initialize
    Regular FSP volumes can be restored from VolumeManager::Initialize but
    the code does not support FuseBox FSP volume creation so ...
    
    Convert that code into a helper and call OnProvidedFileSystemMount for
    each regular FSP volume. No change in behavior for them.
    
    Difference is: OnProvidedFileSystemMount creates a FuseBox FSP volume,
    if the fusebox feature is enabled.
    
    Bug: 1292825
    Change-Id: I07fde4588a8e41b7da8d1dd6adfcb039ed6b77cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777808
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026698}

--- chrome/browser/ash/file_manager/volume_manager.h
+++ chrome/browser/ash/file_manager/volume_manager.h
@@ -529,6 +529,7 @@ class VolumeManager : public KeyedService,
   }
 
  private:
+  void RestoreProvidedFileSystems();
   void OnDiskMountManagerRefreshed(bool success);
   void OnStorageMonitorInitialized();
   void DoAttachMtpStorage(const storage_monitor::StorageInfo& info,
