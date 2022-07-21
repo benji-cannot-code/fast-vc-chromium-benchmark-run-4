5dd515fc2cd480c7d93750ea105ddc52abd10c54
Ali Hijazi <ahijazi@chromium.org>
2022-07-21 11:32:19 +0000

    AnonymousIframe: Fix FencedFrame ShadowDom bug
    Anonymous iframe defines a per-page “nonce”. We consider the inside and outside of a FencedFrame to be part of a different page, so an anonymous iframe created outside and inside the FencedFrame must not have the same “nonce”. Unfortunately, the ShadowDom implementation does not really create a new PageImpl.  We end up using the same nonce. To fix this, the nonce definition is moved temporarily from the PageImpl to the main RenderFrameHost.
    
    For more information, please check: https://docs.google.com/document/d/1bIjqZ9tDN-DhH99h6iAb1O0-TIArGjQJXR6STmdvaCQ
    
    Bug: 1287458
    Change-Id: I095f51e3c7aa258a2d91794fe938b7fe16ad4409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3760617
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026713}

--- content/browser/navigation_browsertest.cc
+++ content/browser/navigation_browsertest.cc
@@ -5920,7 +5920,7 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTestAnonymousIframe,
   // An anonymous document has a storage key with a nonce.
   EXPECT_TRUE(child->current_frame_host()->storage_key().nonce().has_value());
   base::UnguessableToken anonymous_nonce =
-      current_frame_host()->GetPage().anonymous_iframes_nonce();
+      current_frame_host()->anonymous_iframes_nonce();
   EXPECT_EQ(anonymous_nonce,
             child->current_frame_host()->storage_key().nonce().value());
 
@@ -6022,7 +6022,7 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTestAnonymousIframe,
 
   EXPECT_TRUE(child_b->current_frame_host()->storage_key().nonce().has_value());
   base::UnguessableToken anonymous_nonce_b =
-      current_frame_host()->GetPage().anonymous_iframes_nonce();
+      current_frame_host()->anonymous_iframes_nonce();
   EXPECT_NE(anonymous_nonce, anonymous_nonce_b);
   EXPECT_EQ(anonymous_nonce_b,
             child_b->current_frame_host()->storage_key().nonce().value());
