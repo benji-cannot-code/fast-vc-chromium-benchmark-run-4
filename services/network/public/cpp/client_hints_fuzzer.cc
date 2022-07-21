af8ef2051529a1d84ce50bc566c2961cba051014
Ari Chivukula <arichiv@chromium.org>
2022-07-21 15:39:01 +0000

    [Client Hints Fuzzer] Fix file URLs
    When building a fuzzer that creates GURL, it turns out that you need to
    load the ICU library https://crbug.com/778929. Although all file URLs
    are invalid in this context, we should fix the fuzzer rather than trying
    to block files so the code flow doesn't have to change.
    
    Bug: 1346176
    Change-Id: I7b14ce66bc7101e050d728c9a7f2e31960e5853a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779839
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026782}

--- services/network/public/cpp/client_hints_fuzzer.cc
+++ services/network/public/cpp/client_hints_fuzzer.cc
@@ -5,6 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/client_hints.h"
 
+#include "base/at_exit.h"
+#include "base/i18n/icu_util.h"
+
+// This is a workaround for https://crbug.com/778929.
+struct Environment {
+  Environment() { CHECK(base::i18n::InitializeICU()); }
+  base::AtExitManager at_exit_manager;
+};
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   const std::string test_data(reinterpret_cast<const char*>(data), size);
   network::ParseClientHintToDelegatedThirdPartiesHeader(
