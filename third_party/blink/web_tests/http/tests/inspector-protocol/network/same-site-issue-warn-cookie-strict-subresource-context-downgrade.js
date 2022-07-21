d16ce5c786bdce78e927cb23ed6ec2eaf263ff8f
Danil Somsikov <dsv@chromium.org>
2022-07-21 18:07:25 +0000

    Do not await on the evaluate when we await on the issue event.
    Bug: 1228246
    Change-Id: I0aafb579dfb939072e219e2b890d23afe89f38c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780805
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026870}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/same-site-issue-warn-cookie-strict-subresource-context-downgrade.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/same-site-issue-warn-cookie-strict-subresource-context-downgrade.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   const setCookieUrl = 'https://cookie.test:8443/inspector-protocol/network/resources/set-cookie.php?cookie='
       + encodeURIComponent('name=value; SameSite=Strict');
-  await session.evaluate(`fetch('${setCookieUrl}', {method: 'POST', credentials: 'include'})`);
+  session.evaluate(`fetch('${setCookieUrl}', {method: 'POST', credentials: 'include'})`);
   const issue = await dp.Audits.onceIssueAdded();
   testRunner.log(issue.params, 'Inspector issue:');
 
