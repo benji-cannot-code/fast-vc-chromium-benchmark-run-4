e83cedb30995f45397f8822bdb11787badfc81e0
Zofia Salata <zsalata@google.com>
2022-07-21 16:23:31 +0000

    [PageInfo] Add flag for hiding site settings
    Add chrome://flags#page-info-hide-site-settings for
    hiding the site settings row in page info main view.
    
    Change-Id: Ie100e4a71ad89aa83d0ace5392b3eca4c1884909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762448
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Zofia Salata <zsalata@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026809}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1188,6 +1188,9 @@ extern const char kPageInfoAboutThisSiteDescription[];
 extern const char kPageInfoMoreAboutThisPageName[];
 extern const char kPageInfoMoreAboutThisPageDescription[];
 
+extern const char kPageInfoHideSiteSettingsName[];
+extern const char kPageInfoHideSiteSettingsDescription[];
+
 extern const char kPageInfoHistoryDesktopName[];
 extern const char kPageInfoHistoryDesktopDescription[];
 
