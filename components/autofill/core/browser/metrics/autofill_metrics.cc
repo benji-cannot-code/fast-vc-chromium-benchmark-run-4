e3217a9b90763c83172253f02045c06af1bb743e
Vidhan <vidhanj@google.com>
2022-07-21 20:38:12 +0000

    Introduce IsValueNotAutofilledOverExistingValueSameAsSubmittedValue2 metric
    With this CL, IsValueNotAutofilledOverExistingValueSameAsSubmittedValue
    metric becomes obsolete. Instead IsValueNotAutofilledOverExistingValueSameAsSubmittedValue2 is introduced. This metric records, for each field that had an initial
    value and did not get autofilled due to it, in how many cases would
    the user submitted field value was same as the one autofill would
    have suggested.
    
    Bug: 1275649
    Change-Id: I33f0bc42f4585b2bbcbbadc9e350547728729f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745389
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026940}

--- components/autofill/core/browser/metrics/autofill_metrics.cc
+++ components/autofill/core/browser/metrics/autofill_metrics.cc
@@ -3599,7 +3599,7 @@ void AutofillMetrics::LogOtpInputDialogNewOtpRequested() {
 void AutofillMetrics::
     LogIsValueNotAutofilledOverExistingValueSameAsSubmittedValue(bool is_same) {
   base::UmaHistogramBoolean(
-      "Autofill.IsValueNotAutofilledOverExistingValueSameAsSubmittedValue",
+      "Autofill.IsValueNotAutofilledOverExistingValueSameAsSubmittedValue2",
       is_same);
 }
 
