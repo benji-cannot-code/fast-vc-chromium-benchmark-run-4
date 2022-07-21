8d816fc7f37db0eece6fd47d499bc49ec15083a8
Tsuyoshi Horo <horo@chromium.org>
2022-07-21 03:09:54 +0000

    [/net] Stop using new operator
    This should cause no functional changes.
    
    Bug: 479898
    Change-Id: I37b7c8c3083d6001f34d41d06bc5010995413d7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3771761
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026597}

--- net/ssl/threaded_ssl_private_key.cc
+++ net/ssl/threaded_ssl_private_key.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/threaded_ssl_private_key.h"
 
 #include <string>
+#include <tuple>
 #include <utility>
 
 #include "base/bind.h"
@@ -18,10 +19,11 @@ namespace net {
 
 namespace {
 
-void DoCallback(const base::WeakPtr<ThreadedSSLPrivateKey>& key,
-                SSLPrivateKey::SignCallback callback,
-                std::vector<uint8_t>* signature,
-                Error error) {
+void DoCallback(
+    const base::WeakPtr<ThreadedSSLPrivateKey>& key,
+    SSLPrivateKey::SignCallback callback,
+    std::tuple<Error, std::unique_ptr<std::vector<uint8_t>>> result) {
+  auto [error, signature] = std::move(result);
   if (!key)
     return;
   std::move(callback).Run(error, *signature);
@@ -37,10 +39,12 @@ class ThreadedSSLPrivateKey::Core
 
   ThreadedSSLPrivateKey::Delegate* delegate() { return delegate_.get(); }
 
-  Error Sign(uint16_t algorithm,
-             base::span<const uint8_t> input,
-             std::vector<uint8_t>* signature) {
-    return delegate_->Sign(algorithm, input, signature);
+  std::tuple<Error, std::unique_ptr<std::vector<uint8_t>>> Sign(
+      uint16_t algorithm,
+      base::span<const uint8_t> input) {
+    auto signature = std::make_unique<std::vector<uint8_t>>();
+    auto error = delegate_->Sign(algorithm, input, signature.get());
+    return std::make_tuple(error, std::move(signature));
   }
 
  private:
@@ -67,14 +71,12 @@ std::vector<uint16_t> ThreadedSSLPrivateKey::GetAlgorithmPreferences() {
 void ThreadedSSLPrivateKey::Sign(uint16_t algorithm,
                                  base::span<const uint8_t> input,
                                  SSLPrivateKey::SignCallback callback) {
-  std::vector<uint8_t>* signature = new std::vector<uint8_t>;
   base::PostTaskAndReplyWithResult(
       task_runner_.get(), FROM_HERE,
       base::BindOnce(&ThreadedSSLPrivateKey::Core::Sign, core_, algorithm,
-                     std::vector<uint8_t>(input.begin(), input.end()),
-                     base::Unretained(signature)),
+                     std::vector<uint8_t>(input.begin(), input.end())),
       base::BindOnce(&DoCallback, weak_factory_.GetWeakPtr(),
-                     std::move(callback), base::Owned(signature)));
+                     std::move(callback)));
 }
 
 ThreadedSSLPrivateKey::~ThreadedSSLPrivateKey() = default;
