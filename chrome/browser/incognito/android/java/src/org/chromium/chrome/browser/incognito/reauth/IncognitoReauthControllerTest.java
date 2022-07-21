ab3d12f450252daffdb51c65077ccb77b6197717
Rohit Agarwal <roagarwal@chromium.org>
2022-07-21 12:10:28 +0000

    [Android][IncognitoReauth] Add Incognito re-auth support on iCCT.
    We currently don't show the re-auth dialog when iCCT is opened because
    one of the condition to show the dialog is to have a not null
    |mLayoutStateProvider| instance which for CCT will always be the case
    as we only set its supplier for TabbedRootUiCoordinator.
    
    This CL updates the condition to also consider whether the Activity
    in consideration is a TabbedActivity or not.
    
    UX before:
    https://drive.google.com/file/d/1MbpQv5kqcwhcQVYbMq4j2Lfvcn-e9ouu/view?usp=sharing
    
    UX after:
    https://drive.google.com/file/d/1ACBbZYZPxy7rN2sVWKEvhJ_elJyfsyl-/view?usp=sharing
    
    Bug: 1227656
    Change-Id: Iddd8742b560d11ed44e89af114f5afce4f2907f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766682
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026729}

--- chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerTest.java
+++ chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerTest.java
@@ -148,6 +148,7 @@ public class IncognitoReauthControllerTest {
         doReturn(mIncognitoReauthCoordinatorMock)
                 .when(mIncognitoReauthCoordinatorFactoryMock)
                 .createIncognitoReauthCoordinator(any(), /*showFullScreen=*/anyBoolean());
+        doReturn(true).when(mIncognitoReauthCoordinatorFactoryMock).getIsTabbedActivity();
         doNothing().when(mIncognitoReauthCoordinatorMock).show();
 
         mLayoutStateProviderOneshotSupplier = new OneshotSupplierImpl<>();
