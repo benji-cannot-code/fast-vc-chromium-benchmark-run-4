12e04ffa26ec35edf39e522da265f4a089264c0e
Christoph Schwering <schwering@google.com>
2022-07-21 11:06:27 +0000

    [Autofill] Parse forms asynchronously.
    AutofillManager::ParseForm() takes 80 ms in the 99th percentile on
    Android, which causes significant jankiness because it runs on the
    UI thread.
    
    This CL moves ParseForm() off the main thread. This implies a
    fundamental change of AutofillManager's form cache:
    
    Before this CL, parsing happened lazily on demand, in particular by
    AutofillManager::GetCachedFormAndField() (which despite it names
    had significant side effects on both the form cache and the form
    itself).
    
    After this CL, every event AutofillManager::OnFoo() that takes a
    FormData and is called by the renderer parses the form asynchronously
    on a different thread and then also asynchronously calls
    AutofillManager::OnFooImpl() on the UI thread.
    
    For thread-safety reasons, the asynchronous DetermineHeuristicTypes()
    calls emit no UKM logs (which only concers the rationalization at the
    moment) and does not append any logs to chrome://autofill-internals.
    
    This change is behind the a feature AutofillParseAsync. The CL has
    low test coverage because the existing tests need a large number of
    fixes (essentially to wait for the asynchronous event). These fixes
    will land in follow-up CLs.
    
    Bug: 1309848, 1345089
    Change-Id: I8016fda4a3a811613468040f9beda9a132f781d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3632115
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026705}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -154,6 +154,8 @@ extern const base::FeatureParam<int>
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillPageLanguageDetection;
 COMPONENT_EXPORT(AUTOFILL)
+extern const base::Feature kAutofillParseAsync;
+COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillParsingPatternProvider;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<std::string>
