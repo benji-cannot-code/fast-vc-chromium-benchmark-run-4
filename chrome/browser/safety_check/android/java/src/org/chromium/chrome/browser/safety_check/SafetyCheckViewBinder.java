a7bed00bec26dcc1a833ab695533a12d347bcd0e
Maxim Anufriev <maxan@google.com>
2022-07-21 21:53:04 +0000

    [UPM] Handle outdated PasswordCheckup backend in safety check
    This CL enables GMS update notification from Safety Check UI when
    the Unified Password Manager is enabled, but the installed
    GMS Core version is not supported.
    
    Request: http://go/scrcast/NTIzNzQxOTg4MTI2NzIwMHw1MzU0MTNjZC1jYQ
    Install: http://go/scrcast/NTY1Mzc5MTk5NjExNjk5MnxhOGYxZTBiZC00Nw
    
    Bug: 1327578
    Change-Id: Ida046e40f997539b13ea31c7c95cc101be552bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776638
    Auto-Submit: Maxim Anufriev <maxan@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026988}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckViewBinder.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckViewBinder.java
@@ -48,6 +48,8 @@ class SafetyCheckViewBinder {
                 return context.getResources().getQuantityString(
                         R.plurals.safety_check_passwords_compromised_exist, compromised,
                         compromised);
+            case PasswordsState.BACKEND_VERSION_NOT_SUPPORTED:
+                return context.getString(R.string.safety_check_passwords_update_play_services);
             default:
                 assert false : "Unknown PasswordsState value.";
         }
@@ -69,6 +71,7 @@ class SafetyCheckViewBinder {
             case PasswordsState.QUOTA_LIMIT:
             case PasswordsState.OFFLINE:
             case PasswordsState.ERROR:
+            case PasswordsState.BACKEND_VERSION_NOT_SUPPORTED:
                 return R.drawable.ic_info_outline_grey_24dp;
             default:
                 assert false : "Unknown PasswordsState value.";
