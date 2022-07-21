1bc97829ca5d3a0a412b222b1a6603060099ed9a
Ming-Ying Chung <mych@chromium.org>
2022-07-21 10:24:17 +0000

    [beacon-api] Add UseCounter for the data types supported by `navigator.sendBeacon()`
    We would like to learn the distribution of usage among different
    supported data types to decide if we can remove support for them in the
    new beacon API. See the tracking issue for more context.
    
    Bug: 1346187
    Change-Id: I9c6f87fefd4ff750c42b0ef2b6599732145d5611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777020
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026695}

--- third_party/blink/renderer/modules/beacon/navigator_beacon.cc
+++ third_party/blink/renderer/modules/beacon/navigator_beacon.cc
@@ -88,6 +88,8 @@ bool NavigatorBeacon::SendBeaconImpl(
     switch (data->GetContentType()) {
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::
           kArrayBuffer: {
+        UseCounter::Count(execution_context,
+                          WebFeature::kSendBeaconWithArrayBuffer);
         auto* data_buffer = data->GetAsArrayBuffer();
         if (!base::CheckedNumeric<wtf_size_t>(data_buffer->ByteLength())
                  .IsValid()) {
@@ -104,6 +106,8 @@ bool NavigatorBeacon::SendBeaconImpl(
       }
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::
           kArrayBufferView: {
+        UseCounter::Count(execution_context,
+                          WebFeature::kSendBeaconWithArrayBufferView);
         auto* data_view = data->GetAsArrayBufferView().Get();
         if (!base::CheckedNumeric<wtf_size_t>(data_view->byteLength())
                  .IsValid()) {
@@ -118,11 +122,14 @@ bool NavigatorBeacon::SendBeaconImpl(
         break;
       }
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::kBlob:
+        UseCounter::Count(execution_context, WebFeature::kSendBeaconWithBlob);
         allowed = PingLoader::SendBeacon(*script_state, frame, url,
                                          data->GetAsBlob());
         break;
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::
           kFormData:
+        UseCounter::Count(execution_context,
+                          WebFeature::kSendBeaconWithFormData);
         allowed = PingLoader::SendBeacon(*script_state, frame, url,
                                          data->GetAsFormData());
         break;
@@ -133,11 +140,15 @@ bool NavigatorBeacon::SendBeaconImpl(
         return false;
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::
           kURLSearchParams:
+        UseCounter::Count(execution_context,
+                          WebFeature::kSendBeaconWithURLSearchParams);
         allowed = PingLoader::SendBeacon(*script_state, frame, url,
                                          data->GetAsURLSearchParams());
         break;
       case V8UnionReadableStreamOrXMLHttpRequestBodyInit::ContentType::
           kUSVString:
+        UseCounter::Count(execution_context,
+                          WebFeature::kSendBeaconWithUSVString);
         allowed = PingLoader::SendBeacon(*script_state, frame, url,
                                          data->GetAsUSVString());
         break;
