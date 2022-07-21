80c37d07549be47442e191815bd20b07ced8334e
James Cook <jamescook@chromium.org>
2022-07-21 20:35:14 +0000

    chromeos: Delete class DBusClientsBrowser
    All the D-Bus clients have migrated to initializing themselves. This
    class now does nothing. Delete it.
    
    Bug: 647367
    Test: existing chromeos_unittests
    Change-Id: Ia12aef031d900f0259a68a3751bd8c696848c9e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774536
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026938}

--- chromeos/dbus/dbus_thread_manager.cc
+++ chromeos/dbus/dbus_thread_manager.cc
@@ -9,24 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
-#include "base/memory/ptr_util.h"
-#include "base/message_loop/message_pump_type.h"
 #include "chromeos/dbus/common/dbus_client.h"
-#include "chromeos/dbus/dbus_clients_browser.h"
 #include "chromeos/dbus/shill/shill_clients.h"
 
 namespace chromeos {
 
 static DBusThreadManager* g_dbus_thread_manager = nullptr;
 
-DBusThreadManager::DBusThreadManager()
-    : clients_browser_(
-          std::make_unique<DBusClientsBrowser>(use_real_clients_)) {}
+DBusThreadManager::DBusThreadManager() = default;
 
-DBusThreadManager::~DBusThreadManager() {
-  // Delete all D-Bus clients before shutting down the system bus.
-  clients_browser_.reset();
-}
+DBusThreadManager::~DBusThreadManager() = default;
 
 void DBusThreadManager::InitializeClients() {
   // Some clients call DBusThreadManager::Get() during initialization.
@@ -36,10 +28,7 @@ void DBusThreadManager::InitializeClients() {
   // that require Shill clients. https://crbug.com/948390.
   shill_clients::Initialize(GetSystemBus());
 
-  if (clients_browser_)
-    clients_browser_->Initialize(GetSystemBus());
-
-  if (use_real_clients_)
+  if (!IsUsingFakes())
     VLOG(1) << "DBusThreadManager initialized for ChromeOS";
   else
     VLOG(1) << "DBusThreadManager created for testing";
