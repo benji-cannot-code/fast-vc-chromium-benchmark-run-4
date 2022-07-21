901d8e656dcb27de8db7b1d488da3d8b4e03be2c
Toni Barzic <tbarzic@chromium.org>
2022-07-21 20:29:35 +0000

    Enable mic mute switch for guest users
    Whether the feature is available on the device is controlled by a
    command line switch, which should be forwarded to guest sessions.
    
    BUG=b:237041069
    
    Change-Id: I4fb6f3e4a6518eeeeaa4712597a4e79f45e88fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3765978
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Roger Tinkoff <rtinkoff@google.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026934}

--- ui/events/ozone/evdev/input_device_opener_evdev.cc
+++ ui/events/ozone/evdev/input_device_opener_evdev.cc
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/memory/ptr_util.h"
 #include "base/trace_event/trace_event.h"
+#include "ui/events/event_switches.h"
 #include "ui/events/ozone/evdev/event_converter_evdev_impl.h"
 #include "ui/events/ozone/evdev/gamepad_event_converter_evdev.h"
 #include "ui/events/ozone/evdev/microphone_mute_switch_event_converter_evdev.h"
 #include "ui/events/ozone/evdev/stylus_button_event_converter_evdev.h"
-#include "ui/events/ozone/evdev/switches.h"
 #include "ui/events/ozone/evdev/tablet_event_converter_evdev.h"
 #include "ui/events/ozone/evdev/touch_event_converter_evdev.h"
 
@@ -90,7 +90,7 @@ std::unique_ptr<EventConverterEvdev> CreateConverter(
   }
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          kEnableMicrophoneMuteSwitchDeviceSwitch) &&
+          switches::kEnableMicrophoneMuteSwitchDeviceSwitch) &&
       devinfo.IsMicrophoneMuteSwitchDevice()) {
     return base::WrapUnique<EventConverterEvdev>(
         new MicrophoneMuteSwitchEventConverterEvdev(
