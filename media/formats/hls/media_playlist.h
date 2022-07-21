048d6d179b862b0cdf938a633551c09a6ab94130
Will Cassella <cassew@chromium.org>
2022-07-21 00:51:16 +0000

    HLS: Integrate the EXT-X-MAP tag with the media playlist parser
    This CL integrates the EXT-X-MAP tag with the rest of the media playlist
    parser, adding unit tests related to its behavior in the context of the
    whole playlist. This tag is required to support fragmented MP4.
    
    Bug: 1266991
    Change-Id: Ie6f15661fb46472f384bf34aaff5f05d9c2c8914
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766914
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026557}

--- media/formats/hls/media_playlist.h
+++ media/formats/hls/media_playlist.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/formats/hls/parse_status.h"
 #include "media/formats/hls/playlist.h"
 #include "media/formats/hls/tags.h"
-#include "media/formats/hls/types.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
--- media/formats/hls/media_segment.h
+++ media/formats/hls/media_segment.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_FORMATS_HLS_MEDIA_SEGMENT_H_
 #define MEDIA_FORMATS_HLS_MEDIA_SEGMENT_H_
 
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/time/time.h"
 #include "media/base/media_export.h"
 #include "media/formats/hls/types.h"
@@ -15,10 +17,40 @@ namespace media::hls {
 
 class MEDIA_EXPORT MediaSegment {
  public:
+  class MEDIA_EXPORT InitializationSegment
+      : public base::RefCounted<InitializationSegment> {
+    friend class base::RefCounted<InitializationSegment>;
+
+   public:
+    InitializationSegment(GURL uri, absl::optional<types::ByteRange>);
+    InitializationSegment(const InitializationSegment& copy) = delete;
+    InitializationSegment(InitializationSegment&& copy) = delete;
+    InitializationSegment& operator=(const InitializationSegment& copy) =
+        delete;
+    InitializationSegment& operator=(InitializationSegment&& copy) = delete;
+
+    // The URI, resolved against the playlist URI, of the resource containing
+    // the initialization segment.
+    const GURL& GetUri() const { return uri_; }
+
+    // If the initialization segment is a subrange of its resource, this
+    // indicates the range.
+    absl::optional<types::ByteRange> GetByteRange() const {
+      return byte_range_;
+    }
+
+   private:
+    ~InitializationSegment();
+
+    GURL uri_;
+    absl::optional<types::ByteRange> byte_range_;
+  };
+
   MediaSegment(base::TimeDelta duration,
                types::DecimalInteger media_sequence_number,
                types::DecimalInteger discontinuity_sequence_number,
                GURL uri,
+               scoped_refptr<InitializationSegment> initialization_segment,
                absl::optional<types::ByteRange> byte_range,
                absl::optional<types::DecimalInteger> bitrate,
                bool has_discontinuity,
@@ -47,6 +79,13 @@ class MEDIA_EXPORT MediaSegment {
   // `gap` is true, in which case this URI should not be used.
   const GURL& GetUri() const { return uri_; }
 
+  // Returns the initialization segment for this media segment, which may be
+  // null if this segment has none. Subsequent media segments may also share the
+  // same initialization segment.
+  scoped_refptr<InitializationSegment> GetInitializationSegment() const {
+    return initialization_segment_;
+  }
+
   // If this media segment is a subrange of its resource, this indicates the
   // range.
   absl::optional<types::ByteRange> GetByteRange() const { return byte_range_; }
@@ -68,6 +107,7 @@ class MEDIA_EXPORT MediaSegment {
   types::DecimalInteger media_sequence_number_;
   types::DecimalInteger discontinuity_sequence_number_;
   GURL uri_;
+  scoped_refptr<InitializationSegment> initialization_segment_;
   absl::optional<types::ByteRange> byte_range_;
   absl::optional<types::DecimalInteger> bitrate_;
   bool has_discontinuity_;
