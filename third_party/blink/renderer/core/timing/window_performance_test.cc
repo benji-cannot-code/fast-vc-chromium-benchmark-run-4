bdca172bca4a9d03495d5a32098311fdc058981a
Yi Gu <yigu@chromium.org>
2022-07-21 15:39:41 +0000

    [FedCM] Disable flaky test TimeOriginStableWhenSystemClockChanges
    Bug: 1346004
    Change-Id: I9bec6419ede5bfa3de83f131fbda4a7e4732268b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780683
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026783}

--- third_party/blink/renderer/core/timing/window_performance_test.cc
+++ third_party/blink/renderer/core/timing/window_performance_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/test/trace_event_analyzer.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/responsiveness_metrics/user_interaction_latency.h"
@@ -1589,7 +1590,8 @@ base::Time FakeTimeNow() {
 
 class TimeOriginSyncTest : public testing::Test {};
 
-TEST_F(TimeOriginSyncTest, TimeOriginStableWhenSystemClockChanges) {
+// Test is flaky on all platforms: https://crbug.com/1346004
+TEST_F(TimeOriginSyncTest, DISABLED_TimeOriginStableWhenSystemClockChanges) {
   base::subtle::ScopedTimeClockOverrides clock_overrides(&FakeTimeNow, nullptr,
                                                          nullptr);
   base::TimeTicks before = base::TimeTicks::Now();
