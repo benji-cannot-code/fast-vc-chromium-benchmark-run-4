03b36b6c7142ae539f4f143ed9e62ce46f603d9e
Rezvan Mahdavi Hezaveh <rezvan@google.com>
2022-07-21 01:06:49 +0000

    Lookalikes: Add a console message for Combo Squatting heuristic
    Chrome prints a console message when a domain is flagged with any of the
    lookalike heuristics to allow site owners to find the appeals links. For
    new heuristics, before fully launching them, the message has an extra
    line explaining the future behavior of Chrome.
    
    This CL prints a console message for domains that are flagged by the new
    Combo Squatting heuristic. The message will be removed from the console
    once the heuristic is fully launched in future Chrome versions.
    Additionally, the code for printing lookalike console messages is
    refactored to increase readability and usability.
    
    Bug: 1344981
    Change-Id: I34b80772550dddcd9c1fc33d547a28f193fad71d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3765257
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Rezvan Mahdavi Hezaveh <rezvan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026560}

--- components/lookalikes/core/lookalike_url_util.cc
+++ components/lookalikes/core/lookalike_url_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
 #include "base/time/default_clock.h"
@@ -43,6 +44,19 @@ const char kHistogramName[] = "NavigationSuggestion.Event2";
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterListPref(prefs::kLookalikeWarningAllowlistDomains);
+}
+
+std::string GetConsoleMessage(const GURL& lookalike_url,
+                              bool is_new_heuristic) {
+  const char* const kNewHeuristicMessage =
+      "Future Chrome versions will show a warning on this domain name.\n";
+  return base::StringPrintf(
+      "Chrome has determined that %s could be fake or fraudulent.\n\n"
+      "%s"
+      "If you believe this is shown in error please visit "
+      "https://g.co/chrome/lookalike-warnings",
+      lookalike_url.host().c_str(),
+      is_new_heuristic ? kNewHeuristicMessage : "");
 }
 
 }  // namespace lookalikes
