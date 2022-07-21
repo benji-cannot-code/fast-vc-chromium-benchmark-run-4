8f781b25d4063d3e72a1723907870a15f6cdf6ed
Rebekah Potter <rbpotter@chromium.org>
2022-07-21 19:14:52 +0000

    WebUI: Migrate focus_grid and drag_wrapper to TypeScript
    Bug: 1316438
    Change-Id: I4d3b6f8613fa2dae5b4634b7a7a97525d1dc6614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778508
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026903}

--- ui/webui/resources/js/cr/ui/focus_row_behavior.js
+++ ui/webui/resources/js/cr/ui/focus_row_behavior.js
@@ -31,7 +31,7 @@ cr.define('cr.ui', function() {
      * @param {!Event} e
      */
     onFocus(row, e) {
-      const element = /** @type {!Element} */ (e.composedPath()[0]);
+      const element = /** @type {!HTMLElement} */ (e.composedPath()[0]);
       const focusableElement = cr.ui.FocusRow.getFocusableElement(element);
       if (element !== focusableElement) {
         focusableElement.focus();
@@ -120,7 +120,7 @@ cr.define('cr.ui', function() {
         observer: 'focusRowIndexChanged',
       },
 
-      /** @type {Element} */
+      /** @type {HTMLElement} */
       lastFocused: {
         type: Object,
         notify: true,
@@ -395,7 +395,7 @@ cr.define('cr.ui', function() {
       /** @type {number} */
       this.focusRowIndex;
 
-      /** @type {?Element} */
+      /** @type {?HTMLElement} */
       this.lastFocused;
 
       /** @type {number} */
