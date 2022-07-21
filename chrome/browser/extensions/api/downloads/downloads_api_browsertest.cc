8264542b0f13f82f79ec8f67c2cffab56c9373f2
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-07-21 16:46:39 +0000

    Revert "[DownloadBubble] Support bubble in the new extension download API"
    This reverts commit d9aed66f99dbf7653a9d5c3e36c14f575941f274.
    
    Reason for revert: Newly added test
    DownloadExtensionBubbleEnabledTest.DownloadExtensionBubbleEnabledTest_SetUiOptionsOffTheRecord
    is failing on Linux MSAN.
    
    Sample failed run:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/33871/overview
    
    Original change's description:
    > [DownloadBubble] Support bubble in the new extension download API
    >
    > Previously the downloads.setUiOptions API only controls the download
    > shelf. In this CL, extend the API to control the new download bubble
    > UI when the new UI is enabled. It still controls the shelf when the
    > new UI is disabled.
    >
    > The old downloads.setShelfEnabled API is also extended to control the
    > bubble, so extensions calling it are still compatible with the
    > download bubble. This API will eventually be deprecated.
    >
    > Note that the old and new APIs are completely equivalent under the
    > current implementation. For example, if an extension calls
    > "setShelfEnabled(false)" and then calls
    > "setUiOptions({enabled:true})", the final UI state will be true.
    >
    > Bug: 1340400
    > Change-Id: I6305f75ab6c056b84a54abeb7d92f17826d8bc37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3761369
    > Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1026618}
    
    Bug: 1340400
    Change-Id: Ibd467f3ae6d11ff60c4d061d8155cbcf986d331f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780544
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Owners-Override: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026818}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "chrome/browser/download/bubble/download_bubble_controller.h"
-#include "chrome/browser/download/bubble/download_display.h"
-#include "chrome/browser/download/bubble/download_display_controller.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
 #include "chrome/browser/download/download_file_icon_extractor.h"
@@ -45,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
-#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/extension_action_test_helper.h"
 #include "chrome/common/extensions/api/downloads.h"
 #include "chrome/common/pref_names.h"
@@ -54,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_item.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/content/common/file_type_policies_test_util.h"
-#include "components/safe_browsing/core/common/features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -4586,11 +4581,11 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   EXPECT_TRUE(RunFunction(new DownloadsSetShelfEnabledFunction(), "[false]"));
   EXPECT_FALSE(DownloadCoreServiceFactory::GetForBrowserContext(
                    current_browser()->profile())
-                   ->IsDownloadUiEnabled());
+                   ->IsShelfEnabled());
   EXPECT_TRUE(RunFunction(new DownloadsSetShelfEnabledFunction(), "[true]"));
   EXPECT_TRUE(DownloadCoreServiceFactory::GetForBrowserContext(
                   current_browser()->profile())
-                  ->IsDownloadUiEnabled());
+                  ->IsShelfEnabled());
   // TODO(benjhayden) Test that existing shelves are hidden.
   // TODO(benjhayden) Test multiple extensions.
   // TODO(benjhayden) Test disabling extensions.
@@ -4612,12 +4607,12 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                           R"([{"enabled": false}])"));
   EXPECT_FALSE(DownloadCoreServiceFactory::GetForBrowserContext(
                    current_browser()->profile())
-                   ->IsDownloadUiEnabled());
+                   ->IsShelfEnabled());
   EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
                           R"([{"enabled": true}])"));
   EXPECT_TRUE(DownloadCoreServiceFactory::GetForBrowserContext(
                   current_browser()->profile())
-                  ->IsDownloadUiEnabled());
+                  ->IsShelfEnabled());
 }
 
 void OnDangerPromptCreated(DownloadDangerPrompt* prompt) {
@@ -4726,84 +4721,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                                          R"(    "current": false}}])",
                                          result_id)));
 }
-
-// The DownloadExtensionBubbleEnabledTest relies on the download surface, which
-// ChromeOS_ASH doesn't use (see crbug.com/1323505).
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-class DownloadExtensionBubbleEnabledTest : public DownloadExtensionTest {
- public:
-  DownloadExtensionBubbleEnabledTest() {
-    feature_list_.InitAndEnableFeature(safe_browsing::kDownloadBubble);
-  }
-
-  bool IsDownloadToolbarButtonShowing() {
-    return current_browser()
-        ->window()
-        ->GetDownloadBubbleUIController()
-        ->GetDownloadDisplayController()
-        ->download_display_for_testing()
-        ->IsShowing();
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(DownloadExtensionBubbleEnabledTest,
-                       DownloadExtensionBubbleEnabledTest_SetUiOptions) {
-  DownloadManager::DownloadVector items;
-  CreateTwoDownloads(&items);
-  ScopedItemVectorCanceller delete_items(&items);
-  LoadExtension("downloads_split");
-
-  EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
-                          R"([{"enabled": true}])"));
-  EXPECT_TRUE(IsDownloadToolbarButtonShowing());
-
-  EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
-                          R"([{"enabled": false}])"));
-  EXPECT_FALSE(IsDownloadToolbarButtonShowing());
-
-  items[0]->Cancel(true);
-  // Remain hidden on download updates.
-  EXPECT_FALSE(IsDownloadToolbarButtonShowing());
-}
-
-IN_PROC_BROWSER_TEST_F(
-    DownloadExtensionBubbleEnabledTest,
-    DownloadExtensionBubbleEnabledTest_SetUiOptionsBeforeDownloadStart) {
-  LoadExtension("downloads_split");
-  EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
-                          R"([{"enabled": false}])"));
-  DownloadManager::DownloadVector items;
-  CreateTwoDownloads(&items);
-  ScopedItemVectorCanceller delete_items(&items);
-  EXPECT_FALSE(IsDownloadToolbarButtonShowing());
-}
-
-IN_PROC_BROWSER_TEST_F(
-    DownloadExtensionBubbleEnabledTest,
-    DownloadExtensionBubbleEnabledTest_SetUiOptionsOffTheRecord) {
-  LoadExtension("downloads_split");
-  EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
-                          R"([{"enabled": false}])"));
-  DownloadManager::DownloadVector items;
-  CreateTwoDownloads(&items);
-  ScopedItemVectorCanceller delete_items(&items);
-  EXPECT_FALSE(IsDownloadToolbarButtonShowing());
-
-  GoOffTheRecord();
-  EXPECT_FALSE(IsDownloadToolbarButtonShowing());
-
-  EXPECT_TRUE(RunFunction(new DownloadsSetUiOptionsFunction(),
-                          R"([{"enabled": true}])"));
-  items[0]->Cancel(true);
-  EXPECT_TRUE(IsDownloadToolbarButtonShowing());
-
-  GoOnTheRecord();
-  EXPECT_TRUE(IsDownloadToolbarButtonShowing());
-}
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 class DownloadsApiTest : public ExtensionApiTest {
  public:
--- chrome/browser/ui/browser_close_unittest.cc
+++ chrome/browser/ui/browser_close_unittest.cc
@@ -75,7 +75,7 @@ class TestingDownloadCoreService : public DownloadCoreService {
     ADD_FAILURE();
   }
 
-  bool IsDownloadUiEnabled() override { return true; }
+  bool IsShelfEnabled() override { return true; }
 
   // KeyedService
   void Shutdown() override {}
