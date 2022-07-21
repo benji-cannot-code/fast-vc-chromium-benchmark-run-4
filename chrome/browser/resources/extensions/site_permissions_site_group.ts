998d056101b04f78ecefda7905a7b6b7cfae750a
Kelvin Jiang <kelvinjiang@chromium.org>
2022-07-21 00:26:02 +0000

    [Extensions] Add edit permissions dialog for all sites page
    This CL adds an arrow on the right of each site on the all sites page
    which when clicked, opens up the dialog to edit the site's permissions.
    
    Screenshot: https://imgur.com/a/230UgUw
    
    Bug: 1253673
    Change-Id: I5405713a0c92cc8708870c94719c31afaee99491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3775315
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026542}

--- chrome/browser/resources/extensions/site_permissions_site_group.ts
+++ chrome/browser/resources/extensions/site_permissions_site_group.ts
@@ -10,11 +10,14 @@ import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import './strings.m.js';
 import './shared_style.css.js';
 import './shared_vars.css.js';
+import './site_permissions_edit_permissions_dialog.js';
 
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {DomRepeatEvent, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './site_permissions_site_group.html.js';
+import {SiteSettingsDelegate} from './site_settings_mixin.js';
 import {getFaviconUrl} from './url_util.js';
 
 export interface SitePermissionsSiteGroupElement {
@@ -36,6 +39,12 @@ export class SitePermissionsSiteGroupElement extends PolymerElement {
   static get properties() {
     return {
       data: Object,
+      delegate: Object,
+
+      listIndex: {
+        type: Number,
+        value: -1,
+      },
 
       expanded_: {
         type: Boolean,
@@ -46,12 +55,26 @@ export class SitePermissionsSiteGroupElement extends PolymerElement {
         type: Boolean,
         computed: 'computeIsExpandable_(data.sites)',
       },
+
+      showEditSitePermissionsDialog_: {
+        type: Boolean,
+        value: false,
+      },
+
+      siteToEdit_: {
+        type: Object,
+        value: null,
+      },
     };
   }
 
   data: chrome.developerPrivate.SiteGroup;
+  delegate: SiteSettingsDelegate;
+  listIndex: number;
   private expanded_: boolean;
   private isExpandable_: boolean;
+  private showEditSitePermissionsDialog_: boolean;
+  private siteToEdit_: chrome.developerPrivate.SiteInfo|null;
 
   private getEtldOrSiteFaviconUrl_(): string {
     return getFaviconUrl(this.getDisplayUrl_());
@@ -63,6 +86,10 @@ export class SitePermissionsSiteGroupElement extends PolymerElement {
 
   private computeIsExpandable_(): boolean {
     return this.data.sites.length > 1;
+  }
+
+  private getClassForIndex_(): string {
+    return this.listIndex > 0 ? 'hr' : '';
   }
 
   private getDisplayUrl_(): string {
@@ -94,6 +121,23 @@ export class SitePermissionsSiteGroupElement extends PolymerElement {
         siteList === chrome.developerPrivate.UserSiteSet.PERMITTED ?
             'permittedSites' :
             'restrictedSites');
+  }
+
+  private onEditSiteClick_() {
+    this.siteToEdit_ = this.data.sites[0];
+    this.showEditSitePermissionsDialog_ = true;
+  }
+
+  private onEditSiteInListClick_(
+      e: DomRepeatEvent<chrome.developerPrivate.SiteInfo>) {
+    this.siteToEdit_ = e.model.item;
+    this.showEditSitePermissionsDialog_ = true;
+  }
+
+  private onEditSitePermissionsDialogClose_() {
+    this.showEditSitePermissionsDialog_ = false;
+    assert(this.siteToEdit_, 'Site To Edit');
+    this.siteToEdit_ = null;
   }
 }
 
