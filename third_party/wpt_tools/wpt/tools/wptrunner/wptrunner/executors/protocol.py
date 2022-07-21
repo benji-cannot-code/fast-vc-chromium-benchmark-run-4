e02ec4e1caa1fd33ec975622f33b3c8437daf67a
Jonathan Lee <jonathanjlee@google.com>
2022-07-21 19:15:39 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 6cc880d5283965ca9c9b6eb2a8af4ba0c024f872
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,android-weblayer-pie-x86-wpt-smoketest,linux-blink-rel
    Change-Id: Ia9af6b24f566a5d9a0b1ebb624697a636603438f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781282
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026904}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/protocol.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/protocol.py
@@ -385,6 +385,9 @@ class ActionSequenceProtocolPart(ProtocolPart):
         :param actions: A protocol-specific handle to an array of actions."""
         pass
 
+    def release(self):
+        pass
+
 
 class TestDriverProtocolPart(ProtocolPart):
     """Protocol part that implements the basic functionality required for
