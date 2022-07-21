df81bbdafdba6fa51c1030832891586dc6d9705a
Tim Sergeant <tsergeant@chromium.org>
2022-07-21 01:23:09 +0000

    Revert "WebVTT: Store VTTCue::cue_alignment_ as V8AlignSetting::Enum"
    This reverts commit 8a2a106e0f0bc5bc1d689ac52b818945ebb9fc13.
    
    Reason for revert: Suspected cause of compile no-op failures on ChromeOS: https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-lacros-dbg/b8808073504746002145/overview
    
    Original change's description:
    > WebVTT: Store VTTCue::cue_alignment_ as V8AlignSetting::Enum
    >
    > This CL reduces Android binary size by 536 Bytes.
    >
    > Change-Id: Idfaa98198fefa73bc864a38cdd452d64ca4b2f97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759799
    > Auto-Submit: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1026539}
    
    Bug: 1346148
    Change-Id: If5cf02a79d589ccdb7cdb185d4985b7d8475c5e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779080
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026574}

--- third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
+++ third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_TRACK_VTT_VTT_CUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_TRACK_VTT_VTT_CUE_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/v8_align_setting.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/track/text_track_cue.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
@@ -48,7 +47,6 @@ class VTTCueBox;
 class VTTRegion;
 class VTTScanner;
 
-using AlignSetting = V8AlignSetting::Enum;
 using VTTRegionMap = HeapHashMap<String, Member<VTTRegion>>;
 
 struct VTTDisplayParameters {
@@ -123,8 +121,8 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   double size() const { return cue_size_; }
   void setSize(double, ExceptionState&);
 
-  V8AlignSetting align() const;
-  void setAlign(const V8AlignSetting&);
+  const String& align() const;
+  void setAlign(const String&);
 
   const String& text() const { return text_; }
   void setText(const String&);
@@ -154,6 +152,16 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   };
   WritingDirection GetWritingDirection() const { return writing_direction_; }
 
+  enum CueAlignment {
+    kStart = 0,
+    kCenter,
+    kEnd,
+    kLeft,
+    kRight,
+    kNumberOfAlignments
+  };
+  CueAlignment GetCueAlignment() const { return cue_alignment_; }
+
   ExecutionContext* GetExecutionContext() const override;
 
 #ifndef NDEBUG
@@ -178,7 +186,7 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
 
   VTTDisplayParameters CalculateDisplayParameters() const;
   double CalculateComputedTextPosition() const;
-  AlignSetting CalculateComputedCueAlignment() const;
+  CueAlignment CalculateComputedCueAlignment() const;
 
   enum CueSetting {
     kNone,
@@ -196,7 +204,7 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   double text_position_;
   double cue_size_;
   WritingDirection writing_direction_;
-  AlignSetting cue_alignment_ = AlignSetting::kCenter;
+  CueAlignment cue_alignment_;
 
   Member<VTTRegion> region_;
   Member<DocumentFragment> vtt_node_tree_;
