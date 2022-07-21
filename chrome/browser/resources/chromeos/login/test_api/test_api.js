953291d1669f769e20883dddef1ae3846c77095a
Jackson Tadie <jacksontadie@google.com>
2022-07-21 20:31:25 +0000

    Add screens to OOBE test_api to support Demo Mode setup tast tests
    First tast test is added in https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3617134
    
    Bug: b/230754291
    Test: Run tast test in linked CL
    Change-Id: Ifd889849253df8f21a423c062ccf71f673c0d8e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616620
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026935}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -162,6 +162,8 @@ class HIDDetectionScreenTester extends ScreenElementApi {
 class WelcomeScreenTester extends ScreenElementApi {
   constructor() {
     super('connect');
+    this.demoModeConfirmationDialog =
+        new PolymerElementApi(this, '#demoModeConfirmationDialog');
   }
 
   /** @override */
@@ -173,6 +175,9 @@ class WelcomeScreenTester extends ScreenElementApi {
 
     assert(this.nextButton);
     this.nextButton.click();
+  }
+  getDemoModeOkButtonName() {
+    return loadTimeData.getString('enableDemoModeDialogConfirm');
   }
 }
 
@@ -543,6 +548,29 @@ class ErrorScreenTester extends ScreenElementApi {
   }
 }
 
+class DemoPreferencesScreenTester extends ScreenElementApi {
+  constructor() {
+    super('demo-preferences');
+  }
+
+  getDemoPreferencesNextButtonName() {
+    return loadTimeData.getString('demoPreferencesNextButtonLabel');
+  }
+}
+
+class ArcTosScreenTester extends ScreenElementApi {
+  constructor() {
+    super('arc-tos');
+  }
+
+  // Note that the Accept Button text key is different depending on whether
+  // the device in Demo Mode setup. Key for non-demo setup is
+  // "arcTermsOfServiceAcceptButton"
+  getArcTosDemoModeAcceptButtonName() {
+    return loadTimeData.getString('arcTermsOfServiceAcceptAndContinueButton');
+  }
+}
+
 class OobeApiProvider {
   constructor() {
     this.screens = {
@@ -564,6 +592,8 @@ class OobeApiProvider {
       GuestTosScreen: new GuestTosScreenTester(),
       ErrorScreen: new ErrorScreenTester(),
       OfflineLoginScreen: new OfflineLoginScreenTester(),
+      DemoPreferencesScreen: new DemoPreferencesScreenTester(),
+      ArcTosScreen: new ArcTosScreenTester(),
     };
 
     this.loginWithPin = function(username, pin) {
