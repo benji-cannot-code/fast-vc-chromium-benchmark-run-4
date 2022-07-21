ff4fdc6b84dcd2e28e813b815252f4e74806b54c
Kent Tamura <tkent@chromium.org>
2022-07-21 00:24:49 +0000

    WebVTT: Store VTTCue::cue_alignment_ as V8AlignSetting::Enum
    This CL reduces Android binary size by 536 Bytes.
    
    Change-Id: Idfaa98198fefa73bc864a38cdd452d64ca4b2f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759799
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026539}

--- third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
+++ third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_TRACK_VTT_VTT_CUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_TRACK_VTT_VTT_CUE_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_align_setting.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/track/text_track_cue.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
@@ -47,6 +48,7 @@ class VTTCueBox;
 class VTTRegion;
 class VTTScanner;
 
+using AlignSetting = V8AlignSetting::Enum;
 using VTTRegionMap = HeapHashMap<String, Member<VTTRegion>>;
 
 struct VTTDisplayParameters {
@@ -121,8 +123,8 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   double size() const { return cue_size_; }
   void setSize(double, ExceptionState&);
 
-  const String& align() const;
-  void setAlign(const String&);
+  V8AlignSetting align() const;
+  void setAlign(const V8AlignSetting&);
 
   const String& text() const { return text_; }
   void setText(const String&);
@@ -152,16 +154,6 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   };
   WritingDirection GetWritingDirection() const { return writing_direction_; }
 
-  enum CueAlignment {
-    kStart = 0,
-    kCenter,
-    kEnd,
-    kLeft,
-    kRight,
-    kNumberOfAlignments
-  };
-  CueAlignment GetCueAlignment() const { return cue_alignment_; }
-
   ExecutionContext* GetExecutionContext() const override;
 
 #ifndef NDEBUG
@@ -186,7 +178,7 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
 
   VTTDisplayParameters CalculateDisplayParameters() const;
   double CalculateComputedTextPosition() const;
-  CueAlignment CalculateComputedCueAlignment() const;
+  AlignSetting CalculateComputedCueAlignment() const;
 
   enum CueSetting {
     kNone,
@@ -204,7 +196,7 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   double text_position_;
   double cue_size_;
   WritingDirection writing_direction_;
-  CueAlignment cue_alignment_;
+  AlignSetting cue_alignment_ = AlignSetting::kCenter;
 
   Member<VTTRegion> region_;
   Member<DocumentFragment> vtt_node_tree_;
