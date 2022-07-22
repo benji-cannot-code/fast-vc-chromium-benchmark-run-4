a1d0713db2ec11146625b23ad9b089ee465bdd87
Xiaodan Zhu <zxdan@chromium.org>
2022-07-22 22:48:58 +0000

    cros: add shadow to system toast
    This cl adds a shadow to system toast to differentiate it from the
    light background in light mode.
    
    Since toast has a very large corner radius, we need to use the
    shadow on texture layer.
    
    The final effects are in the bug comment.
    
    Bug: 1296359
    Change-Id: I065c943277028da69e7511d88e413fa018cdc0e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783314
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027452}

--- ash/style/system_toast_style.h
+++ ash/style/system_toast_style.h
@@ -20,6 +20,7 @@ class LabelButton;
 namespace ash {
 
 class ScopedA11yOverrideWindowSetter;
+class SystemShadow;
 
 // A view that has rounded corner with label and button inside. The label shows
 // the information. The button inside is optional and has certain functionality
@@ -54,6 +55,7 @@ class ASH_EXPORT SystemToastStyle : public views::View {
 
  private:
   // views::View:
+  void AddedToWidget() override;
   void OnThemeChanged() override;
 
   views::Label* label_ = nullptr;
@@ -62,6 +64,8 @@ class ASH_EXPORT SystemToastStyle : public views::View {
 
   // Tells the toast if the dismiss button is already highlighted if one exists.
   bool is_dismiss_button_highlighted_ = false;
+
+  std::unique_ptr<SystemShadow> shadow_;
 
   // Updates the current a11y override window when the dismiss button is being
   // highlighted.
