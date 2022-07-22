bd82d91d28f7b6bc0cd1164d553dbca8aa45a8e0
Jeffrey Young <cowmoo@google.com>
2022-07-22 19:44:35 +0000

    personalization: remove more untrusted legacy code
    Remove IframeApi communication between wallpaper_collections_element
    and collections_grid. These are no longer separated by an iframe, so
    can use normal data flow.
    
    BUG=b:237323063
    TEST=browser_tests --gtest_filter="*PersonalizationAppWallpaper*"
    
    Change-Id: Ie6121bca574fce253d1e12ca4588e9dd29dbde2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3763708
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Eric Sum <esum@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027386}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_test_utils.ts
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_test_utils.ts
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * SWA.
  */
 
-import {emptyState, IFrameApi, PersonalizationState, setAmbientProviderForTesting, setKeyboardBacklightProviderForTesting, setThemeProviderForTesting, setUserProviderForTesting, setWallpaperProviderForTesting} from 'chrome://personalization/trusted/personalization_app.js';
+import {emptyState, PersonalizationState, setAmbientProviderForTesting, setKeyboardBacklightProviderForTesting, setThemeProviderForTesting, setUserProviderForTesting, setWallpaperProviderForTesting} from 'chrome://personalization/trusted/personalization_app.js';
 import {flush, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 import {flushTasks} from 'chrome://webui-test/test_util.js';
 
 import {TestAmbientProvider} from './test_ambient_interface_provider.js';
@@ -72,13 +71,4 @@ export function baseSetup(initialState: PersonalizationState = emptyState()) {
     wallpaperProvider,
     personalizationStore,
   };
-}
-
-/**
- * Helper function to setup a mock `IFrameApi` singleton.
- */
-export function setupTestIFrameApi(): IFrameApi&TestBrowserProxy<IFrameApi> {
-  const testProxy = TestBrowserProxy.fromClass(IFrameApi);
-  IFrameApi.setInstance(testProxy);
-  return testProxy;
 }
