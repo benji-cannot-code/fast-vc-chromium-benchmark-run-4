05ab3b7618bcad7d224bbcbf67d12da58ca3d82d
Danil Somsikov <dsv@chromium.org>
2022-07-22 17:56:57 +0000

    Also check pending navigation entry ExtensionMayAttachToWebContents
    Bug: 1346236
    Change-Id: I2634a7bee919d4b9f4b5c70c699b96e3c02a1f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780487
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027321}

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/devtools_agent_host.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
@@ -216,6 +217,12 @@ bool ExtensionMayAttachToWebContents(const Extension& extension,
   // former being a 'virtual' URL as obtained from NavigationEntry.
   if (!ExtensionMayAttachToURL(extension, extension_profile,
                                web_contents.GetLastCommittedURL(), error)) {
+    return false;
+  }
+  if (web_contents.GetController().GetPendingEntry() &&
+      !ExtensionMayAttachToURL(
+          extension, extension_profile,
+          web_contents.GetController().GetPendingEntry()->GetURL(), error)) {
     return false;
   }
 
