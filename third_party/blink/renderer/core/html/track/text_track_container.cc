86fddc3a3034957ebb0e01ecefb161fc567bf5cd
Kent Tamura <tkent@chromium.org>
2022-07-22 04:55:55 +0000

    WebVTT: Support <ruby> rendering
    * text_track_container.*:
     Make sure we don't mix legacy LayoutObjects and NG LayoutObjects.
    
    * vtt_element.*:
     Creates ruby LayoutObjects for VTT <ruby> and <rt>.
    
    * mediaControls.css:
     VTT <rt> should have properties same as HTML <rt>.
    
    Bug: 1346197
    Change-Id: Ifb1f55e3de7d4574361ac82f307cae42cff19c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756250
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027123}

--- third_party/blink/renderer/core/html/track/text_track_container.cc
+++ third_party/blink/renderer/core/html/track/text_track_container.cc
@@ -104,6 +104,10 @@ void TextTrackContainer::RemovedFrom(ContainerNode& insertion_point) {
   }
 }
 
+bool TextTrackContainer::TypeShouldForceLegacyLayout() const {
+  return !RuntimeEnabledFeatures::LayoutNGVTTCueEnabled();
+}
+
 LayoutObject* TextTrackContainer::CreateLayoutObject(const ComputedStyle& style,
                                                      LegacyLayout legacy) {
   if (RuntimeEnabledFeatures::LayoutNGVTTCueEnabled())
