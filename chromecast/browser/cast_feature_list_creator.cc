dbc9159d160b891b732ba6d4a651e2bff6879305
Travis Skare <skare@chromium.org>
2022-07-22 18:39:57 +0000

    [CodeHealthRotation] Migrate Cast features+metrics base::Value v1->v2
    Try to use base::Value::Dict where base::Value is certain to be a
    dictionary and it makes stylistic sense
    
    Bug: 1303949, 1345676
    Change-Id: I93b3d6a9aa32247ea18787d273aac14c9a58ff73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769659
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027356}

--- chromecast/browser/cast_feature_list_creator.cc
+++ chromecast/browser/cast_feature_list_creator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/strings/string_util.h"
+#include "base/values.h"
 #include "chromecast/base/cast_features.h"
 #include "chromecast/base/pref_names.h"
 #include "chromecast/browser/metrics/cast_metrics_prefs.h"
@@ -46,13 +47,13 @@ void CastFeatureListCreator::CreatePrefServiceAndFeatureList(
   pref_service_ = shell::PrefServiceHelper::CreatePrefService(
       pref_registry.get(), process_type);
 
-  const auto* features_dict =
-      pref_service_->GetDictionary(prefs::kLatestDCSFeatures);
-  const auto* experiment_ids =
-      pref_service_->GetList(prefs::kActiveDCSExperiments);
+  const base::Value::Dict& features_dict =
+      pref_service_->GetValueDict(prefs::kLatestDCSFeatures);
+  const base::Value::List& experiment_ids =
+      pref_service_->GetValueList(prefs::kActiveDCSExperiments);
   auto* command_line = base::CommandLine::ForCurrentProcess();
   InitializeFeatureList(
-      *features_dict, *experiment_ids,
+      features_dict, experiment_ids,
       command_line->GetSwitchValueASCII(switches::kEnableFeatures),
       command_line->GetSwitchValueASCII(switches::kDisableFeatures),
       extra_enable_features_, extra_disable_features_);
