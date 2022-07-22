626a0ab90a09802e952086ce23b126c254f59dcc
Nicholas Hollingum <hollingum@google.com>
2022-07-22 02:50:15 +0000

    borealis: Use SecurityDelegate to control self-activation
    The steam app requires self-activation because its windows do not
    respect normal WM activation signals (see
    https://github.com/ValveSoftware/steam-for-linux/issues/5286 for
    details).
    
    We switch borealis to use its SecurityDelegate to control
    self-activation, rather than the legacy exo::Permissions. Crostini is
    unable to use borealis' SecurityDelegate and so unable to self-activate
    its windows this way. See go/secure-exo-ids for details.
    
    Bug: b/190141156
    Change-Id: I7ce1cffa692848bd52fda4557c3c1138de224777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774204
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027077}

--- chrome/browser/ash/borealis/borealis_context.h
+++ chrome/browser/ash/borealis/borealis_context.h
@@ -24,7 +24,6 @@ class BorealisEngagementMetrics;
 class BorealisGameModeController;
 class BorealisLifetimeObserver;
 class BorealisPowerController;
-class SelfActivationPermissionGranter;
 
 // An object to track information about the state of the Borealis VM.
 // BorealisContext objects should only be created by the Borealis Context
@@ -96,8 +95,6 @@ class BorealisContext {
   std::unique_ptr<BorealisDiskManager> disk_manager_;
 
   std::unique_ptr<BorealisPowerController> power_controller_;
-
-  std::unique_ptr<SelfActivationPermissionGranter> self_activation_granter_;
 };
 
 }  // namespace borealis
