a4d67991f86a81f4357c42503b513308e34f0ac1
Nick Diego Yamane <nickdiego@igalia.com>
2022-07-22 10:52:55 +0000

    wayland: log last system error when connection fails
    Drive-by. This makes easier crash diagnoses, when needed.
    
    R=tonikitoo@igalia.com
    
    Bug: None.
    Change-Id: Ifce92179b580deea8f3b48c3397ae6bbb5bab625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781759
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1027175}

--- ui/ozone/platform/wayland/host/wayland_connection.cc
+++ ui/ozone/platform/wayland/host/wayland_connection.cc
@@ -213,7 +213,7 @@ bool WaylandConnection::Initialize() {
 
   display_.reset(wl_display_connect(nullptr));
   if (!display_) {
-    LOG(ERROR) << "Failed to connect to Wayland display";
+    PLOG(ERROR) << "Failed to connect to Wayland display";
     return false;
   }
 
