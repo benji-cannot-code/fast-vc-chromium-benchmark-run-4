7b5f5043492c14291226ce0ae91d1757fb3e628a
Sky Malice <skym@chromium.org>
2022-07-22 00:20:02 +0000

    Revert "[Android][Sheriff] Disable GlobalNightModeStateControllerTest"
    This reverts commit afb6a1cd82674d0dc525849d3cf9d8ec21c8f945.
    
    Reason for revert: Fixed by https://crrev.com/c/3781177
    
    Original change's description:
    > [Android][Sheriff] Disable GlobalNightModeStateControllerTest
    >
    > The tests fail after observing an unexpected TestActivity. This may be a
    > batching issue with other tests but the source isn't clear and the
    > failure (despite being consistent) doesn't reproduce locally.
    >
    > Bug: 1338271
    > Change-Id: If43c22a8ced759d6c2ec3711e2d5a6f6f1c881c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780098
    > Owners-Override: Friedrich Horschig <fhorschig@chromium.org>
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Auto-Submit: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1026718}
    
    Bug: 1338271
    Change-Id: I5d58180a5654748533e6a72e3a0ec4c2e1ddf6f0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781748
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027056}

--- chrome/android/junit/src/org/chromium/chrome/browser/night_mode/GlobalNightModeStateControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/night_mode/GlobalNightModeStateControllerTest.java
@@ -30,7 +30,6 @@ import org.mockito.stubbing.VoidAnswer1;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MaxAndroidSdkLevel;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 
@@ -94,7 +93,6 @@ public class GlobalNightModeStateControllerTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void testUpdateNightMode_PowerSaveMode_DefaultsToSystem() {
         // Set preference to system default and verify that the night mode isn't enabled.
         SharedPreferencesManager.getInstance().writeInt(UI_THEME_SETTING, ThemeType.SYSTEM_DEFAULT);
@@ -112,7 +110,6 @@ public class GlobalNightModeStateControllerTest {
     @Test
     @MaxAndroidSdkLevel(value = Build.VERSION_CODES.P,
             reason = "Default to light parameter is only applicable pre-Q.")
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void
     testUpdateNightMode_PowerSaveMode_DefaultsToLight() {
         // Enable power save mode and verify night mode is not enabled.
@@ -121,7 +118,6 @@ public class GlobalNightModeStateControllerTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void testUpdateNightMode_SystemNightMode_DefaultsToSystem() {
         // Set preference to system default and verify that the night mode isn't enabled.
         SharedPreferencesManager.getInstance().writeInt(UI_THEME_SETTING, ThemeType.SYSTEM_DEFAULT);
@@ -139,7 +135,6 @@ public class GlobalNightModeStateControllerTest {
     @Test
     @MaxAndroidSdkLevel(value = Build.VERSION_CODES.P,
             reason = "Default to light parameter is only applicable pre-Q.")
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void
     testUpdateNightMode_SystemNightMode_DefaultsToLight() {
         // Enable system night mode and verify night mode is not enabled.
@@ -148,7 +143,6 @@ public class GlobalNightModeStateControllerTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void testUpdateNightMode_Preference() {
         // Set preference to dark theme and verify night mode is enabled.
         SharedPreferencesManager.getInstance().writeInt(UI_THEME_SETTING, ThemeType.DARK);
@@ -168,7 +162,6 @@ public class GlobalNightModeStateControllerTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void testStopAndRestart() {
         // Simulate to stop listening to night mode state changes. Verify that night mode state is
         // not changed.
@@ -200,7 +193,6 @@ public class GlobalNightModeStateControllerTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/1338271")
     public void testObserver() {
         mGlobalNightModeStateController.addObserver(mObserver);
 
