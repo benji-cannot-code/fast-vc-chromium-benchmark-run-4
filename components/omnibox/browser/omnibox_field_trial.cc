06d28bc3b06200bda5fa1b1337366f92888c61af
Orin Jaworski <orinj@google.com>
2022-07-22 18:27:11 +0000

    [omnibox] Add fuzzy counterfactual feature param and logic
    This CL adds a boolean feature param for the FuzzyUrlSuggestions
    feature and uses it to clear any generated matches at the end of
    processing within the HistoryFuzzyProvider.
    
    Bug: 1286859
    Change-Id: I372f93111a7f8c43655c29ececb4fb946ef83515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780853
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027339}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -596,6 +596,12 @@ bool OmniboxFieldTrial::IsFuzzyUrlSuggestionsEnabled() {
   return base::FeatureList::IsEnabled(omnibox::kOmniboxFuzzyUrlSuggestions);
 }
 
+const base::FeatureParam<bool>
+    OmniboxFieldTrial::kFuzzyUrlSuggestionsCounterfactual(
+        &omnibox::kOmniboxFuzzyUrlSuggestions,
+        "FuzzyUrlSuggestionsCounterfactual",
+        false);
+
 bool OmniboxFieldTrial::IsExperimentalKeywordModeEnabled() {
   return base::FeatureList::IsEnabled(omnibox::kExperimentalKeywordMode);
 }
