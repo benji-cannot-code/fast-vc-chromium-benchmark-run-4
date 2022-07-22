8297847b24ef2f9b5b7c266cff29b9643915bba5
Matthias Körber <koerber@google.com>
2022-07-22 23:47:57 +0000

    feature compatibility: alternative state names and structured addresses
    This CL integrates merge logic that is based on canonicalized states
    names retrieved from the AlternativeStateMap into the logic of
    structured address components.
    
    Change-Id: I4a03f16598de19cc5be708061a1e44d9339ed37a
    Bug: 1125978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776985
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027474}

--- components/autofill/core/browser/data_model/autofill_structured_address.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address.cc
@@ -7,21 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 #include "base/containers/contains.h"
-#include "base/i18n/case_conversion.h"
-#include "base/strings/strcat.h"
+#include "base/feature_list.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "components/autofill/core/browser/address_rewriter.h"
 #include "components/autofill/core/browser/autofill_type.h"
-#include "components/autofill/core/browser/data_model/autofill_structured_address_constants.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_regex_provider.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_utils.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/browser/geo/alternative_state_name_map.h"
+#include "components/autofill/core/common/autofill_features.h"
 
-namespace autofill {
-
-namespace structured_address {
+namespace autofill::structured_address {
 
 std::u16string AddressComponentWithRewriter::RewriteValue(
     const std::u16string& value,
@@ -317,9 +314,38 @@ State::State(AddressComponent* parent)
     : AddressComponentWithRewriter(
           ADDRESS_HOME_STATE,
           parent,
-          MergeMode::kPickShorterIfOneContainsTheOther | kReplaceEmpty) {}
+          kPickShorterIfOneContainsTheOther |
+              (base::FeatureList::IsEnabled(
+                   features::kAutofillUseAlternativeStateNameMap)
+                   ? MergeMode::kMergeBasedOnCanonicalizedValues
+                   : 0) |
+              kReplaceEmpty) {}
 
 State::~State() = default;
+
+absl::optional<std::u16string> State::GetCanonicalizedValue() const {
+  if (!base::FeatureList::IsEnabled(
+          features::kAutofillUseAlternativeStateNameMap)) {
+    return absl::nullopt;
+  }
+
+  std::string country_code =
+      base::UTF16ToUTF8(GetRootNode().GetValueForType(ADDRESS_HOME_COUNTRY));
+
+  if (country_code.empty()) {
+    return absl::nullopt;
+  }
+
+  absl::optional<AlternativeStateNameMap::CanonicalStateName>
+      canonicalized_state_name = AlternativeStateNameMap::GetCanonicalStateName(
+          country_code, GetValue());
+
+  if (!canonicalized_state_name.has_value()) {
+    return absl::nullopt;
+  }
+
+  return canonicalized_state_name.value().value();
+}
 
 // Zips are mergeable when one is a substring of the other one.
 // For merging, the shorter substring is taken.
@@ -390,6 +416,4 @@ void Address::MigrateLegacyStructure(bool is_verified_profile) {
   }
 }
 
-}  // namespace structured_address
-
-}  // namespace autofill
+}  // namespace autofill::structured_address
