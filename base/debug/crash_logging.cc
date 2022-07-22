c168784c4fbe75f6bb1290359957da904fb9b319
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-22 00:05:03 +0000

    Avoiding ':' character in names used by ScopedRequestParamsCrashKeys.
    Bug: 1341077
    Change-Id: I7d517166038afa3b14195b612d83d18800e6fc12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750076
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027050}

--- base/debug/crash_logging.cc
+++ base/debug/crash_logging.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/crash_logging.h"
 
-namespace base {
-namespace debug {
+#include "base/strings/string_piece.h"
+#include "build/build_config.h"
+
+namespace base::debug {
 
 namespace {
 
@@ -18,6 +20,24 @@ CrashKeyString* AllocateCrashKeyString(const char name[],
                                        CrashKeySize value_length) {
   if (!g_crash_key_impl)
     return nullptr;
+
+    // TODO(https://crbug.com/1341077): It would be great if the DCHECKs below
+    // could also be enabled on Android, but debugging tryjob failures was a bit
+    // difficult... :-/
+#if DCHECK_IS_ON() && !BUILDFLAG(IS_ANDROID)
+  base::StringPiece name_piece = name;
+
+  // Some `CrashKeyImplementation`s reserve certain characters and disallow
+  // using them in crash key names.  See also https://crbug.com/1341077.
+  DCHECK_EQ(base::StringPiece::npos, name_piece.find(':'))
+      << "; name_piece = " << name_piece;
+
+  // Some `CrashKeyImplementation`s support only short crash key names (e.g. see
+  // the DCHECK in crash_reporter::internal::CrashKeyStringImpl::Set).
+  // Enforcing this restrictions here ensures that crash keys will work for all
+  // `CrashKeyStringImpl`s.
+  DCHECK_LT(name_piece.size(), 40u);
+#endif
 
   return g_crash_key_impl->Allocate(name, value_length);
 }
@@ -61,5 +81,4 @@ void SetCrashKeyImplementation(std::unique_ptr<CrashKeyImplementation> impl) {
   g_crash_key_impl = impl.release();
 }
 
-}  // namespace debug
-}  // namespace base
+}  // namespace base::debug
