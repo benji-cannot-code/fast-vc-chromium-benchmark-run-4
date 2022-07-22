ce67024a5002e62400f55d159a6d4c6908941b87
Jazz Xu <jazzhsu@chromium.org>
2022-07-22 18:58:43 +0000

    Android pip: fix control not working properly.
    This CL fix the issue where default controls in Picture-in-picture window is controlling wrong media when pip'ed media isn't active. This is caused by default controls in pip window are meant for active media session. The CL fix the issue by adding custom controls (play/pause, previous track, next track) to Picture-in-picture params to override the default controls.
    
    Bug: 1345255
    
    Change-Id: I92d87513d12c919eb0a4ebc7d37d4de6fec10766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773420
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027363}

--- chrome/browser/ui/android/overlay/overlay_window_android.h
+++ chrome/browser/ui/android/overlay/overlay_window_android.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/raw_ptr.h"
 #include "content/public/browser/overlay_window.h"
+#include "third_party/blink/public/mojom/mediasession/media_session.mojom.h"
 #include "ui/android/window_android.h"
 #include "ui/android/window_android_observer.h"
 #include "ui/gfx/geometry/size.h"
@@ -36,7 +37,9 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
       const base::android::JavaParamRef<jobject>& obj,
       const base::android::JavaParamRef<jobject>& jwindow_android);
   void Destroy(JNIEnv* env);
-  void Play(JNIEnv* env);
+  void TogglePlayPause(JNIEnv* env);
+  void NextTrack(JNIEnv* env);
+  void PreviousTrack(JNIEnv* env);
   void CompositorViewCreated(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& compositor_view);
@@ -63,11 +66,12 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
   void UpdateNaturalSize(const gfx::Size& natural_size) override;
 
   // VideoOverlayWindow implementation
-  void SetPlaybackState(PlaybackState playback_state) override {}
+  void SetPlaybackState(PlaybackState playback_state) override;
   void SetPlayPauseButtonVisibility(bool is_visible) override;
   void SetSkipAdButtonVisibility(bool is_visible) override {}
-  void SetNextTrackButtonVisibility(bool is_visible) override {}
-  void SetPreviousTrackButtonVisibility(bool is_visible) override {}
+  void SetNextTrackButtonVisibility(bool is_visible) override;
+  void SetPreviousTrackButtonVisibility(bool is_visible) override;
+  // TODO(crbug.com/1331269): Implement video conferencing actions.
   void SetMicrophoneMuted(bool muted) override {}
   void SetCameraState(bool turned_on) override {}
   void SetToggleMicrophoneButtonVisibility(bool is_visible) override {}
@@ -77,6 +81,13 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
   cc::Layer* GetLayerForTesting() override;
 
  private:
+  // Notify PictureInPictureActivity that visible actions have changed.
+  void MaybeNotifyVisibleActionsChanged();
+
+  // Maybe update visible actions. Returns true if update happened.
+  bool MaybeUpdateVisibleAction(
+      const media_session::mojom::MediaSessionAction& action,
+      bool is_visible);
   void CloseInternal();
 
   // A weak reference to Java PictureInPictureActivity object.
@@ -87,7 +98,8 @@ class OverlayWindowAndroid : public content::VideoOverlayWindow,
   gfx::Rect bounds_;
   gfx::Size video_size_;
 
-  bool is_play_pause_button_visible_ = false;
+  bool is_playing_ = false;
+  std::unordered_set<int> visible_actions_;
 
   raw_ptr<content::VideoPictureInPictureWindowController> controller_;
 };
