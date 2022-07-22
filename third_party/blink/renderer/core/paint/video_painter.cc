61b15f45caabd77e107fc23acc95813f13314a29
Manuel Rego Casasnovas <rego@igalia.com>
2022-07-22 05:19:16 +0000

    Avoid notifying PaintArtifactCompositor on VideoPainter
    Change-Id: I593c95441170d738565fc648b5e0029b6c88b554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776919
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1027126}

--- third_party/blink/renderer/core/paint/video_painter.cc
+++ third_party/blink/renderer/core/paint/video_painter.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/platform/geometry/layout_point.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
-#include "third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/foreign_layer_display_item.h"
 
@@ -64,13 +63,6 @@ void VideoPainter::PaintReplaced(const PaintInfo& paint_info,
                                 paint_offset);
     context.SetURLForRect(layout_video_.GetDocument().Url(),
                           snapped_replaced_rect);
-  }
-
-  // Since we may have changed the location of the replaced content, we need to
-  // notify PaintArtifactCompositor.
-  if (layout_video_.GetFrameView()) {
-    layout_video_.GetFrameView()->SetPaintArtifactCompositorNeedsUpdate(
-        PaintArtifactCompositorUpdateReason::kVideoPainterPaintReplaced);
   }
 
   // Video frames are only painted in software for printing or capturing node
