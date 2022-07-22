2861b04da61d4e93d07335997e750e881af4c286
Brahim Chikhaoui <bchikhaoui@google.com>
2022-07-22 16:16:37 +0000

    [OOBE][EncryptionMigration]: Update screen and handler structure
    - Remove Bind/Unbind logic;
    - Remove OnViewDestroyed;
    - Remove show_on_init;
    - Migrate to OnUserAction with args list and external API prefix;
    - Remove IsJavascriptAllowed;
    
    Bug: 1343802, 1309022, 1298392
    Change-Id: I824446b9f1df2d6088e3766108c3e6bc1309cb54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758605
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027264}

--- chrome/browser/ui/webui/chromeos/login/encryption_migration_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/encryption_migration_screen_handler.h
@@ -6,17 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_ENCRYPTION_MIGRATION_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_ENCRYPTION_MIGRATION_SCREEN_HANDLER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
-
-namespace ash {
-class EncryptionMigrationScreen;
-}
 
 namespace chromeos {
 
-class EncryptionMigrationScreenView {
+class EncryptionMigrationScreenView
+    : public base::SupportsWeakPtr<EncryptionMigrationScreenView> {
  public:
-  constexpr static StaticOobeScreenId kScreenId{"encryption-migration"};
+  inline constexpr static StaticOobeScreenId kScreenId{
+      "encryption-migration", "EncryptionMigrationScreen"};
 
   // Enumeration for migration UI state. These values must be kept in sync with
   // EncryptionMigrationUIState in JS code, and match the numbering for
@@ -31,11 +30,9 @@ class EncryptionMigrationScreenView {
     COUNT
   };
 
-  virtual ~EncryptionMigrationScreenView() {}
+  virtual ~EncryptionMigrationScreenView() = default;
 
   virtual void Show() = 0;
-  virtual void Hide() = 0;
-  virtual void SetDelegate(ash::EncryptionMigrationScreen* delegate) = 0;
   virtual void SetBatteryState(double batteryPercent,
                                bool isEnoughBattery,
                                bool isCharging) = 0;
@@ -64,8 +61,6 @@ class EncryptionMigrationScreenHandler : public EncryptionMigrationScreenView,
 
   // EncryptionMigrationScreenView implementation:
   void Show() override;
-  void Hide() override;
-  void SetDelegate(ash::EncryptionMigrationScreen* delegate) override;
   void SetBatteryState(double batteryPercent,
                        bool isEnoughBattery,
                        bool isCharging) override;
@@ -79,11 +74,6 @@ class EncryptionMigrationScreenHandler : public EncryptionMigrationScreenView,
   // BaseScreenHandler implementation:
   void DeclareLocalizedValues(
       ::login::LocalizedValuesBuilder* builder) override;
-  void InitializeDeprecated() override;
-
- private:
-  ash::EncryptionMigrationScreen* delegate_ = nullptr;
-  bool show_on_init_ = false;
 };
 
 }  // namespace chromeos
