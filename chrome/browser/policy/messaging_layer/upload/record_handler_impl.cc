79fbccd2104c311f7b5058f43c3eea56e00f57df
Leonid Baraz <lbaraz@chromium.org>
2022-07-22 05:37:24 +0000

    Stop noisy logging in non-managed case.
    Noisy messages reported by the bug where caused by recent change that
    made access to cloud policy client into a singleton, and among other
    modifications removed returning
    Status(INVALID_ARGUMENT, "Client was null") when cloud policy
    client was not found.
    This was happening on unmanaged devices only.
    
    The current change handles the same cause differently: the error
    returned in this case is Status(NOT_FOUND) which so far was never
    returned. If this status is detected, logging is skipped.
    
    Also, since ReportingServerConnector now makes a callback with StatusOr,
    we can propagate its status() directly, as opposed to what was done
    earlier based on std::optional has_value() only. As a result, some of
    the failure tests now expect different error status.
    
    The change was tested on unmanaged and on managed VM (no difference
    in the latter case).
    
    Bug: 1345660
    Change-Id: I6b4a9ad2de2dcb8c69fe1645a28ae18bdc96ff82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780961
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027130}

--- chrome/browser/policy/messaging_layer/upload/record_handler_impl.cc
+++ chrome/browser/policy/messaging_layer/upload/record_handler_impl.cc
@@ -89,7 +89,7 @@ class RecordHandlerImpl::ReportUploader
 
   void StartUpload();
   void OnUploadComplete(StatusOr<base::Value::Dict> response);
-  void HandleFailedUpload();
+  void HandleFailedUpload(Status status);
   void HandleSuccessfulUpload();
   void Complete(DmServerUploadService::CompletionResponse result);
 
@@ -214,7 +214,7 @@ void RecordHandlerImpl::ReportUploader::OnUploadComplete(
 
   if (!response.ok()) {
     Schedule(&RecordHandlerImpl::ReportUploader::HandleFailedUpload,
-             base::Unretained(this));
+             base::Unretained(this), response.status());
     return;
   }
   last_response_ = std::move(response.ValueOrDie());
@@ -222,7 +222,7 @@ void RecordHandlerImpl::ReportUploader::OnUploadComplete(
            base::Unretained(this));
 }
 
-void RecordHandlerImpl::ReportUploader::HandleFailedUpload() {
+void RecordHandlerImpl::ReportUploader::HandleFailedUpload(Status status) {
   if (highest_sequence_information_.has_value()) {
     Complete(DmServerUploadService::SuccessfulUploadResponse{
         .sequence_information =
@@ -231,7 +231,7 @@ void RecordHandlerImpl::ReportUploader::HandleFailedUpload() {
     return;
   }
 
-  Complete(Status(error::INTERNAL, "Unable to upload any records"));
+  Complete(status);
 }
 
 void RecordHandlerImpl::ReportUploader::HandleSuccessfulUpload() {
