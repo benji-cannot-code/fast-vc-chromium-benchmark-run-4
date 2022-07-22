b70f2cdb913312511e23f28cb1ff9d31f4a418c4
Marc Treib <treib@chromium.org>
2022-07-22 18:05:43 +0000

    History Sync: Add DB metrics about foreign visits
    The new metrics record how many foreign visits exist, and how many of
    them are "unmapped", i.e. their originator referrer/opener IDs haven't
    been remapped into local visit IDs.
    Like the existing DB metrics, these metrics are recorded with 1%
    probability during DB startup - computing them may be expensive, so it
    shouldn't happen too often.
    Also, a timing histogram is added that tracks how long it took to
    compute the metrics.
    Finally, the existing timing histograms
    History.Database[Basic|Advanced]MetricsTime are extended (they had
    expired since M77) so that we can compare the values, and are also
    assigned to new owners.
    
    Bug: 1318028, 1335055
    Change-Id: I028962097554502a8cefc0fae150969af0a0d233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779547
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027325}

--- components/history/core/browser/history_database.h
+++ components/history/core/browser/history_database.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
-#include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/history/core/browser/download_database.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/visit_annotations_database.h"
 #include "components/history/core/browser/visit_database.h"
 #include "components/history/core/browser/visitsegment_database.h"
-#include "components/sync/model/model_type_store_base.h"
 #include "sql/database.h"
 #include "sql/init_status.h"
 #include "sql/meta_table.h"
