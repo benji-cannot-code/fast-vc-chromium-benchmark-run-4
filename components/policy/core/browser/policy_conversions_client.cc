b1d8cd1f6295ef589f5e250ab5dfc9f7ea1b7b32
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-07-22 17:55:23 +0000

    Revert "Stop generating unsupported policies on Chrome OS"
    This reverts commit cc88398dcdbfdcf6a14f2c02f174ad2d85b46314.
    
    Reason for revert: Closed the tree, see https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-dbg/9350/overview
    
    Original change's description:
    > Stop generating unsupported policies on Chrome OS
    >
    > Bug: 223616
    > Change-Id: I8159bf6607931cbe1c24db9018ef372f074dcbb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757928
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Commit-Queue: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027287}
    
    Bug: 223616
    Change-Id: Ic0d0bec2fdf995021104e4459eb8f598505da367
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780309
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1027319}

--- components/policy/core/browser/policy_conversions_client.cc
+++ components/policy/core/browser/policy_conversions_client.cc
@@ -148,7 +148,6 @@ base::Value::Dict PolicyConversionsClient::GetPrecedencePolicies() {
 base::Value::List PolicyConversionsClient::GetPrecedenceOrder() {
   DCHECK(HasUserPolicies());
 
-#if !BUILDFLAG(IS_CHROMEOS)
   PolicyNamespace policy_namespace =
       PolicyNamespace(POLICY_DOMAIN_CHROME, std::string());
   const PolicyMap& chrome_policies =
@@ -196,12 +195,6 @@ base::Value::List PolicyConversionsClient::GetPrecedenceOrder() {
                           IDS_POLICY_PRECEDENCE_CLOUD_USER};
     }
   }
-#else
-  std::vector<int> precedence_order{IDS_POLICY_PRECEDENCE_PLATFORM_MACHINE,
-                                    IDS_POLICY_PRECEDENCE_CLOUD_MACHINE,
-                                    IDS_POLICY_PRECEDENCE_PLATFORM_USER,
-                                    IDS_POLICY_PRECEDENCE_CLOUD_USER};
-#endif  // !BUILDFLAG(IS_CHROMEOS)
 
   base::Value::List precedence_order_localized;
   for (int label_id : precedence_order) {
--- components/policy/core/common/policy_map_unittest.cc
+++ components/policy/core/common/policy_map_unittest.cc
@@ -1277,7 +1277,6 @@ TEST_F(PolicyMapTest, Affiliation) {
   EXPECT_TRUE(policies.IsUserAffiliated());
 }
 
-#if !BUILDFLAG(IS_CHROMEOS)
 class PolicyMapMergeTest
     : public PolicyMapTestBase,
       public testing::TestWithParam<
@@ -1676,6 +1675,7 @@ INSTANTIATE_TEST_SUITE_P(PolicyMapMergeTestInstance,
                                           testing::Values(false, true),
                                           testing::Values(false, true)));
 
+#if !BUILDFLAG(IS_CHROMEOS)
 class PolicyMapPriorityTest
     : public testing::TestWithParam<
           std::tuple</*cloud_policy_overrides_platform_policy=*/bool,
