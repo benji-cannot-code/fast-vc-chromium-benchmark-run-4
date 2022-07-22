7f9201ee9cf11e68d10d332d8a95b1be078f08e0
Matt Menke <mmenke@chromium.org>
2022-07-22 00:04:33 +0000

    Remove base::Value constructors that take a DeprecatedListStorage.
    Also make ListStorage a protected member of base::Value/
    
    Bug: 1338341
    Change-Id: I12ec60b288fe8c3b4adacafa7f30e06b119d6e62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766886
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027049}

--- base/values.h
+++ base/values.h
@@ -208,8 +208,6 @@ class BASE_EXPORT GSL_OWNER Value {
   using BlobStorage = std::vector<uint8_t>;
 
   using DeprecatedListStorage = std::vector<Value>;
-  // TODO(https://crbug.com/1291666): Make this private.
-  using ListStorage = DeprecatedListStorage;
 
   // Like `DictStorage`, but with std::unique_ptr in the mapped type. This is
   // due to legacy reasons, and should be replaced with
@@ -217,8 +215,9 @@ class BASE_EXPORT GSL_OWNER Value {
   // anymore.
   using LegacyDictStorage = flat_map<std::string, std::unique_ptr<Value>>;
 
-  using DeprecatedListView = CheckedContiguousRange<ListStorage>;
-  using DeprecatedConstListView = CheckedContiguousConstRange<ListStorage>;
+  using DeprecatedListView = CheckedContiguousRange<DeprecatedListStorage>;
+  using DeprecatedConstListView =
+      CheckedContiguousConstRange<DeprecatedListStorage>;
   // TODO(https://crbug.com/1291666): Make these private.
   using ListView = DeprecatedListView;
   using ConstListView = DeprecatedConstListView;
@@ -302,10 +301,6 @@ class BASE_EXPORT GSL_OWNER Value {
 
   // Constructor for `Value::Type::LIST`.
   explicit Value(List&& value) noexcept;
-
-  // DEPRECATED: prefer `Value(List&&)`.
-  explicit Value(span<const Value> value);
-  explicit Value(ListStorage&& value) noexcept;
 
   ~Value();
 
@@ -1164,6 +1159,10 @@ class BASE_EXPORT GSL_OWNER Value {
   }
 
  protected:
+  // TODO(https://crbug.com/1187091): Once deprecated list methods and ListView
+  // have been removed, make this a private member of List.
+  using ListStorage = DeprecatedListStorage;
+
   // Checked convenience accessors for dict and list.
   const LegacyDictStorage& dict() const { return GetDict().storage_; }
   LegacyDictStorage& dict() { return GetDict().storage_; }
