7bddbdeb200c57868b386c77d3f5cf4f52b4ecac
Nancy Wang <nancylingwang@chromium.org>
2022-07-22 01:50:47 +0000

    Add the non mojom LaunchAppWithIntent interface for AppPublisher.
    BUG=1253250
    
    Change-Id: I0fb294a6e29016f0bf8f22d7bb82ee9e48fa9958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774201
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027070}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -116,7 +116,7 @@ class ArcApps : public KeyedService,
                            IntentPtr intent,
                            LaunchSource launch_source,
                            WindowInfoPtr window_info,
-                           base::OnceCallback<void(bool)> callback);
+                           base::OnceCallback<void(bool)> callback) override;
   void LaunchAppWithParams(AppLaunchParams&& params,
                            LaunchCallback callback) override;
   void LaunchShortcut(const std::string& app_id,
