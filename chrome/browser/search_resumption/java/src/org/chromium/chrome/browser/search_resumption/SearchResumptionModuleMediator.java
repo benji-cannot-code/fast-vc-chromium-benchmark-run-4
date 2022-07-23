13c685038b1e1d17b9e36ebc4efe8bb1d3ff9816
Xi Han <hanxi@google.com>
2022-07-23 02:36:27 +0000

    [SearchResumption] Add metrics and record more user actions.
    Add histogram to indicate whether the module is expanded or collapsed
    when it is shown. Besides, record user actions when users click the
    module to make it collapsed/expanded.
    
    Bug: 1332596
    Change-Id: I1124220cb9e011d4703b683d77c35971c8e6c28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782269
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027511}

--- chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleMediator.java
+++ chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleMediator.java
@@ -36,9 +36,6 @@ import java.util.List;
  */
 public class SearchResumptionModuleMediator
         implements OnSuggestionsReceivedListener, SignInStateObserver {
-    static final String ACTION_CLICK = "SearchResumptionModule.NTP.Click";
-    private static final String ACTION_SHOW = "SearchResumptionModule.NTP.Show";
-
     private final ViewStub mStub;
     private final Tab mTabToTrackSuggestion;
     private final SearchResumptionTileBuilder mTileBuilder;
@@ -100,6 +97,7 @@ public class SearchResumptionModuleMediator
         if (!initializeModule()) return;
         mTileBuilder.buildSuggestionTile(autocompleteResult.getSuggestionsList(),
                 mModuleLayoutView.findViewById(R.id.search_resumption_module_tiles_container));
+        SearchResumptionModuleUtils.recordModuleShown();
     }
 
     /**
@@ -109,6 +107,7 @@ public class SearchResumptionModuleMediator
         if (!initializeModule()) return;
         mTileBuilder.buildSuggestionTile(texts, urls,
                 mModuleLayoutView.findViewById(R.id.search_resumption_module_tiles_container));
+        SearchResumptionModuleUtils.recordModuleShown();
     }
 
     void destroy() {
@@ -211,7 +210,6 @@ public class SearchResumptionModuleMediator
                 mModel, mModuleLayoutView, new SearchResumptionModuleViewBinder());
         mModel.set(SearchResumptionModuleProperties.EXPAND_COLLAPSE_CLICK_CALLBACK,
                 this::onExpandedOrCollapsed);
-        RecordUserAction.record(ACTION_SHOW);
         return true;
     }
 
@@ -224,9 +222,12 @@ public class SearchResumptionModuleMediator
     /**
      * Saves the user's choice of expanding/collapsing of the suggestions in the SharedPreference.
      */
-    private void onExpandedOrCollapsed(Boolean expanded) {
+    @VisibleForTesting
+    void onExpandedOrCollapsed(Boolean expanded) {
         SharedPreferencesManager.getInstance().writeBoolean(
                 ChromePreferenceKeys.SEARCH_RESUMPTION_MODULE_COLLAPSE_ON_NTP, !expanded);
+        RecordUserAction.record(expanded ? SearchResumptionModuleUtils.ACTION_EXPAND
+                                         : SearchResumptionModuleUtils.ACTION_COLLAPSE);
     }
 
     @VisibleForTesting
--- chrome/browser/search_resumption/junit/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleMediatorUnitTest.java
+++ chrome/browser/search_resumption/junit/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleMediatorUnitTest.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewStub;
 
 import androidx.test.filters.MediumTest;
+import androidx.test.filters.SmallTest;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -34,8 +35,10 @@ import org.robolectric.annotation.Config;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteController;
@@ -43,6 +46,7 @@ import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteController.On
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteControllerJni;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
+import org.chromium.chrome.browser.search_resumption.SearchResumptionModuleUtils.ModuleShowStatus;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.tab.Tab;
@@ -125,6 +129,7 @@ public class SearchResumptionModuleMediatorUnitTest {
     @Mock
     private SigninManager mSignInManager;
 
+    private UserActionTester mActionTester;
     private SearchResumptionModuleMediator mMediator;
 
     @Before
@@ -153,6 +158,7 @@ public class SearchResumptionModuleMediatorUnitTest {
         IdentityServicesProvider.setInstanceForTests(mIdentityServicesProvider);
         doReturn(mSignInManager).when(mIdentityServicesProvider).getSigninManager(any());
 
+        mActionTester = new UserActionTester();
         mMediator =
                 new SearchResumptionModuleMediator(mParent, mTabToTrack, mProfile, mTileBuilder);
         verify(mAutocompleteController).addOnSuggestionsReceivedListener(mListener.capture());
@@ -172,6 +178,7 @@ public class SearchResumptionModuleMediatorUnitTest {
         IdentityServicesProvider.setInstanceForTests(null);
         ShadowChromeFeatureList.sEnableSearchResumptionModule = false;
         ShadowChromeFeatureList.sParamValues.clear();
+        mActionTester.tearDown();
     }
 
     @Test
@@ -182,6 +189,9 @@ public class SearchResumptionModuleMediatorUnitTest {
 
         mMediator.onSuggestionsReceived(mAutocompleteResult, "", true);
         verify(mParent, times(0)).inflate();
+        Assert.assertEquals(0,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        SearchResumptionModuleUtils.UMA_MODULE_SHOW, ModuleShowStatus.EXPANDED));
     }
 
     @Test
@@ -194,6 +204,9 @@ public class SearchResumptionModuleMediatorUnitTest {
         mMediator.onSuggestionsReceived(mAutocompleteResult, "", true);
         verify(mParent, times(1)).inflate();
         Assert.assertEquals(View.VISIBLE, mSuggestionTilesContainerView.getVisibility());
+        Assert.assertEquals(1,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        SearchResumptionModuleUtils.UMA_MODULE_SHOW, ModuleShowStatus.EXPANDED));
     }
 
     @Test
@@ -204,6 +217,9 @@ public class SearchResumptionModuleMediatorUnitTest {
 
         mMediator.onSuggestionsAvailable(texts, gUrls);
         verify(mParent, times(0)).inflate();
+        Assert.assertEquals(0,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        SearchResumptionModuleUtils.UMA_MODULE_SHOW, ModuleShowStatus.EXPANDED));
     }
     @Test
     @MediumTest
@@ -214,6 +230,17 @@ public class SearchResumptionModuleMediatorUnitTest {
         mMediator.onSuggestionsAvailable(texts, gUrls);
         verify(mParent, times(1)).inflate();
         Assert.assertEquals(View.VISIBLE, mSuggestionTilesContainerView.getVisibility());
+        Assert.assertEquals(1,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        SearchResumptionModuleUtils.UMA_MODULE_SHOW, ModuleShowStatus.EXPANDED));
+
+        mMediator.onExpandedOrCollapsed(true /* expand */);
+        Assert.assertTrue(
+                mActionTester.getActions().contains(SearchResumptionModuleUtils.ACTION_EXPAND));
+
+        mMediator.onExpandedOrCollapsed(false /* expand */);
+        Assert.assertTrue(
+                mActionTester.getActions().contains(SearchResumptionModuleUtils.ACTION_COLLAPSE));
     }
 
     @Test
@@ -236,7 +263,7 @@ public class SearchResumptionModuleMediatorUnitTest {
     }
 
     @Test
-    @MediumTest
+    @SmallTest
     public void testDestroy() {
         testShowModuleWithEnoughResults();
         mMediator.destroy();
