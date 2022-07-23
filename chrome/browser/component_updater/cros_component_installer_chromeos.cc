31ec3e2609b14bf088830451d47d2f51c7071b35
Bowen Li <lbowen@google.com>
2022-07-23 02:02:08 +0000

    Add country attribute in demo mode Omaha request.
    Refer to details in the cl: cl/462171887
    
    Bug: b/239243178
    Change-Id: Ibd9a6720cf1c7bce35964daa12f30659660a43c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782574
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Bowen Li <lbowen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027508}

--- chrome/browser/component_updater/cros_component_installer_chromeos.cc
+++ chrome/browser/component_updater/cros_component_installer_chromeos.cc
@@ -274,6 +274,8 @@ DemoAppInstallerPolicy::GetInstallerAttributes() const {
       prefs->GetString(prefs::kDemoModeRetailerId);
   demo_app_installer_attributes["store_id"] =
       prefs->GetString(prefs::kDemoModeStoreId);
+  demo_app_installer_attributes["demo_country"] =
+      prefs->GetString(prefs::kDemoModeCountry);
   return demo_app_installer_attributes;
 }
 
