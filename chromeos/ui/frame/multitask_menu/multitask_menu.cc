640f616ca75f2eb3f824e730a7a29a028eca3738
Sophie Wen <sophiewen@google.com>
2022-07-25 18:38:58 +0000

    partial_split: Implement partial split from the multitask menu button.
    Test: MultitaskMenuTest.TestMultitaskMenuPartialSplit
    Bug: 1323394
    Change-Id: I164eb947b634fa6bc8840defc8f185261a7f27db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778557
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027865}

--- chromeos/ui/frame/multitask_menu/multitask_menu.cc
+++ chromeos/ui/frame/multitask_menu/multitask_menu.cc
@@ -133,7 +133,8 @@ void MultitaskMenu::SplitButtonPressed(SnapDirection snap) {
 }
 
 void MultitaskMenu::PartialButtonPressed(SnapDirection snap) {
-  // TODO(shidi/sophiewen): Link Partial Split function here.
+  SnapController::Get()->CommitSnap(parent_window(), snap,
+                                    chromeos::SnapRatio::kTwoThirdSnapRatio);
   HideBubble();
 }
 
