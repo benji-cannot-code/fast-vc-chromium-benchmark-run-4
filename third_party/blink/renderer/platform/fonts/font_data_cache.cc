acdc02f077a861f5c9cc02112ba9eceeff8ceb18
Yoshifumi Inoue <yosin@chromium.org>
2022-07-25 08:27:50 +0000

    Move FontDataCache::PurgeLeastRecentlyUsed() to work the main thread and DOM worker threads
    This patch changes shared `is_purging` to member variable of `FontDataCache`
    to avoid accessing it from the main thread and DOM worker threads.
    
    Note: Instances for `FontDataCache` are thread local and hold by
    `FontGlobalContext` via `FontCache`.
    
    Bug: 1313831
    Change-Id: I33437a7ca204a37e70cb05da9c3185d1cc482758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784471
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027656}

--- third_party/blink/renderer/platform/fonts/font_data_cache.cc
+++ third_party/blink/renderer/platform/fonts/font_data_cache.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/font_data_cache.h"
 
+#include "base/auto_reset.h"
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
 
@@ -141,13 +142,12 @@ bool FontDataCache::Purge(PurgeSeverity purge_severity) {
 bool FontDataCache::PurgeLeastRecentlyUsed(int count) {
   // Guard against reentry when e.g. a deleted FontData releases its small caps
   // FontData.
-  static bool is_purging;
-  if (is_purging)
+  if (is_purging_)
     return false;
 
   lock_.AssertAcquired();
 
-  is_purging = true;
+  base::AutoReset<bool> is_purging_auto_reset(&is_purging_, true);
 
   Vector<scoped_refptr<SimpleFontData>, 20> font_data_to_delete;
   auto end = inactive_font_data_.end();
@@ -171,8 +171,6 @@ bool FontDataCache::PurgeLeastRecentlyUsed(int count) {
   bool did_work = font_data_to_delete.size();
 
   font_data_to_delete.clear();
-
-  is_purging = false;
 
   return did_work;
 }
