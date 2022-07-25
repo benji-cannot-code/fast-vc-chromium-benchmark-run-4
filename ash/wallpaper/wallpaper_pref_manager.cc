59f14bd08a69b436acbeba6140ab11142590c2ae
Sean Kau <skau@chromium.org>
2022-07-25 15:10:47 +0000

    Remove ephemeral parameter from WallpaperPrefManager
    Now that WallpaperControllerImpl is using SessionController,
    WallpaperPrefManager can lookup UserSessions for is_ephemeral
    information.
    
    Bug: chromium:1329567
    Change-Id: I75692cd52df7f344e9c531c680d707cf60c0ea5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3724334
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027763}

--- ash/wallpaper/wallpaper_pref_manager.cc
+++ ash/wallpaper/wallpaper_pref_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/public/cpp/session/session_types.h"
 #include "ash/public/cpp/wallpaper/wallpaper_controller_client.h"
 #include "ash/public/cpp/wallpaper/wallpaper_info.h"
 #include "ash/public/cpp/wallpaper/wallpaper_types.h"
@@ -273,6 +274,18 @@ class WallpaperProfileHelperImpl : public WallpaperProfileHelper {
     return Shell::Get()->session_controller()->IsActiveUserSessionStarted();
   }
 
+  bool IsEphemeral(const AccountId& account_id) const override {
+    const UserSession* user_session =
+        Shell::Get()->session_controller()->GetUserSessionByAccountId(
+            account_id);
+    if (!user_session) {
+      // User is not logged in. Thus, they are not ephemeral.
+      return false;
+    }
+
+    return user_session->user_info.is_ephemeral;
+  }
+
  private:
   base::raw_ptr<WallpaperControllerClient> wallpaper_controller_client_ =
       nullptr;  // not owned
@@ -295,9 +308,8 @@ class WallpaperPrefManagerImpl : public WallpaperPrefManager {
   }
 
   bool GetUserWallpaperInfo(const AccountId& account_id,
-                            bool ephemeral,
                             WallpaperInfo* info) const override {
-    if (ephemeral) {
+    if (profile_helper_->IsEphemeral(account_id)) {
       // Ephemeral users do not save anything to local state. Return true if the
       // info can be found in the map, otherwise return false.
       auto it = ephemeral_users_wallpaper_info_.find(account_id);
@@ -312,9 +324,8 @@ class WallpaperPrefManagerImpl : public WallpaperPrefManager {
   }
 
   bool SetUserWallpaperInfo(const AccountId& account_id,
-                            bool ephemeral,
                             const WallpaperInfo& info) override {
-    if (ephemeral) {
+    if (profile_helper_->IsEphemeral(account_id)) {
       ephemeral_users_wallpaper_info_.insert_or_assign(account_id, info);
       return true;
     }
