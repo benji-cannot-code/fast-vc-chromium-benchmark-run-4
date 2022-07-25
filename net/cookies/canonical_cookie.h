98d70fc54d359e7f3b4aa1e947dec02a0ffa1f01
sbingler <bingler@chromium.org>
2022-07-25 23:36:46 +0000

    Don't allow cookies with hidden cookie prefixes
    Prevent the creation of any cookies that have an empty name field and
    whose value impersonates a cookie name prefix.
    
    This will also delete any previously stored cookies that meet the
    conditions by causing them to fail their IsCanonical() check.
    
    Bug: 1345193
    Change-Id: I7e1adef3391bb7caee183204bb609cd63bcdaea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782906
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028000}

--- net/cookies/canonical_cookie.h
+++ net/cookies/canonical_cookie.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/gtest_prod_util.h"
+#include "base/strings/string_piece.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
 #include "net/cookies/cookie_access_result.h"
@@ -431,6 +432,7 @@ class NET_EXPORT CanonicalCookie {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(CanonicalCookieTest, TestPrefixHistograms);
+  FRIEND_TEST_ALL_PREFIXES(CanonicalCookieTest, TestHasHiddenPrefixName);
 
   // This constructor does not validate or canonicalize their inputs;
   // the resulting CanonicalCookies should not be relied on to be canonical
@@ -494,6 +496,9 @@ class NET_EXPORT CanonicalCookie {
   // SameSite computation yourself.
   CookieEffectiveSameSite GetEffectiveSameSite(
       CookieAccessSemantics access_semantics) const;
+
+  // Checks for values that could be misinterpreted as a cookie name prefix.
+  static bool HasHiddenPrefixName(const base::StringPiece cookie_value);
 
   // Returns whether the cookie was created at most |age_threshold| ago.
   bool IsRecentlyCreated(base::TimeDelta age_threshold) const;
