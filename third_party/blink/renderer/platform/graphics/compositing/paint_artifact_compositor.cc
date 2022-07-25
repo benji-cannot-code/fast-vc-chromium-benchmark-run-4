abb37397392a5f28c85c6c3724daa1677a2a5dc7
Peter Kasting <pkasting@chromium.org>
2022-07-25 18:25:30 +0000

    Fix undefined symbol error in C++20.
    For some reason I don't understand, inlining the histogram macro in the
    header results in an undefined symbol relating to atomics when in C++20
    mode.  Fix it by out-of-lining the function.
    
    Bug: 1284275
    Change-Id: I5d2c26cd0a06b49e1f5cc6a34158e01056cb66f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782985
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027858}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.cc
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.cc
@@ -1124,4 +1124,12 @@ ContentLayerClientImpl* PaintArtifactCompositor::ContentLayerClientForTesting(
   return nullptr;
 }
 
+void PaintArtifactCompositor::SetNeedsUpdate(
+    PaintArtifactCompositorUpdateReason reason) {
+  UMA_HISTOGRAM_ENUMERATION("Blink.Paint.PaintArtifactCompositorUpdateReason",
+                            reason,
+                            PaintArtifactCompositorUpdateReason::kCount);
+  needs_update_ = true;
+}
+
 }  // namespace blink
