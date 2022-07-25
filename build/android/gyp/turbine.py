33a41309b6238bb69913714122e47ceb1a66859a
Peter Wen <wnwen@chromium.org>
2022-07-25 22:42:04 +0000

    Android: Avoid adding hints when class missing
    When a class is missing and cannot be found via lookup_deps, avoid
    showing "one of: " with no choices after it.
    
    Bug: 1346746
    Change-Id: I2df12a835dd9e25af92534981342a75eaf61804a
    Fixed: 1346746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783645
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027973}

--- build/android/gyp/turbine.py
+++ build/android/gyp/turbine.py
@@ -8,14 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import functools
 import logging
-import os
-import shutil
 import sys
 import time
 
 import javac_output_processor
 from util import build_utils
-from util import server_utils
 
 
 def ProcessJavacOutput(output, target_name):
