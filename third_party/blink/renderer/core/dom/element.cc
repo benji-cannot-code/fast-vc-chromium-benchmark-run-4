26cfd358288bc3ab2f37c4419acd6043f907d1fd
Rune Lillesveen <futhark@chromium.org>
2022-07-25 15:53:09 +0000

    [@container] Clear ContainerQueryData on detach
    If not re-attaching, there should be no reason to keep the container
    query data. Also fixes a DCHECK/crash that if the element had a
    container query evaluator the last lifecycle update, and still has the
    same one, the old ComputedStyle should be non-null.
    
    Bug: 1342750
    Change-Id: I743abe62ec7597eccf1f9b146d60668d861b62d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782882
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027777}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3703,8 +3703,10 @@ void Element::DetachLayoutTree(bool performing_reattach) {
   HTMLFrameOwnerElement::PluginDisposeSuspendScope suspend_plugin_dispose;
   if (HasRareData()) {
     ElementRareData* data = GetElementRareData();
-    if (!performing_reattach)
+    if (!performing_reattach) {
       data->ClearPseudoElements();
+      data->ClearContainerQueryData();
+    }
 
     if (ElementAnimations* element_animations = data->GetElementAnimations()) {
       if (!performing_reattach) {
--- third_party/blink/renderer/core/dom/element_rare_data.h
+++ third_party/blink/renderer/core/dom/element_rare_data.h
@@ -119,6 +119,7 @@ class ElementSuperRareData : public GarbageCollected<ElementSuperRareData> {
   ContainerQueryData* GetContainerQueryData() const {
     return container_query_data_;
   }
+  void ClearContainerQueryData() { container_query_data_ = nullptr; }
 
   // Returns the crop-ID if one was set, or nullptr otherwise.
   const RegionCaptureCropId* GetRegionCaptureCropId() const {
@@ -558,6 +559,10 @@ class ElementRareData final : public NodeRareData {
     if (super_rare_data_)
       return super_rare_data_->GetContainerQueryData();
     return nullptr;
+  }
+  void ClearContainerQueryData() {
+    if (super_rare_data_)
+      super_rare_data_->ClearContainerQueryData();
   }
 
   ContainerQueryEvaluator* GetContainerQueryEvaluator() const {
