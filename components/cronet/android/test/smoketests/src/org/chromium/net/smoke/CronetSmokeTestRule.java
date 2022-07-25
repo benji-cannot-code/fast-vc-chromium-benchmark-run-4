0ec64ffd86f61eee1ac98a3101b2e76c2eb0a09c
Torne (Richard Coles) <torne@google.com>
2022-07-25 18:59:55 +0000

    Suppress T SDK lints in Cronet.
    Suppress lints for some uses of getIdentifier() in Cronet where there's
    no alternative.
    
    Bug: 1339376
    Change-Id: I47be8650048300431e1da9e5b7f56e8722d237a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779845
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027878}

--- components/cronet/android/test/smoketests/src/org/chromium/net/smoke/CronetSmokeTestRule.java
+++ components/cronet/android/test/smoketests/src/org/chromium/net/smoke/CronetSmokeTestRule.java
@@ -108,6 +108,7 @@ public class CronetSmokeTestRule implements TestRule {
      *
      * @throws Exception if the class cannot be instantiated.
      */
+    @SuppressWarnings("DiscouragedApi")
     private void initTestSupport() throws Exception {
         Context ctx = InstrumentationRegistry.getTargetContext();
         String packageName = ctx.getPackageName();
