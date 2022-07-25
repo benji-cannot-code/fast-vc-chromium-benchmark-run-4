7c05121ce86edb2fccef68b702b5308dd47c5cb7
Yue Zhang <yuezhanggg@chromium.org>
2022-07-25 23:49:37 +0000

    [ChromeCart] Enable heuristics improvement param by default
    Bug: 1346368
    Change-Id: Ib98c1d174eb8ec8ffc116a294a81307e5b933ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780559
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028007}

--- chrome/renderer/cart/commerce_hint_agent.cc
+++ chrome/renderer/cart/commerce_hint_agent.cc
@@ -210,7 +210,7 @@ bool IsCartHeuristicsImprovementEnabled() {
 #if !BUILDFLAG(IS_ANDROID)
   return base::GetFieldTrialParamByFeatureAsBool(
       ntp_features::kNtpChromeCartModule,
-      ntp_features::kNtpChromeCartModuleHeuristicsImprovementParam, false);
+      ntp_features::kNtpChromeCartModuleHeuristicsImprovementParam, true);
 #else
   return base::GetFieldTrialParamByFeatureAsBool(
       commerce::kCommerceHintAndroid,
