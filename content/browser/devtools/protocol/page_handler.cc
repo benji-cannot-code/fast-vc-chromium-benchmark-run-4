c95d6db2939bb418cf44c56c70288f16ec5baf90
Asami Doi <asamidoi@chromium.org>
2022-07-25 08:24:09 +0000

    Prerender: add new FinalStatuses for concurrent prerenders
    This is a part of the concurrent prerenders effort.
    Originally, I tried to implement concurrent prerenders in one CL
    (https://chromium-review.googlesource.com/c/chromium/src/+/3757811)
    but it was reverted because MultiplePrerendersWithLimitedMemoryBrowserTest.AddSpeculationRulesMultipleTimes
    fails in android-pie-x86-rel
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-x86-rel/9562/test-results
    
    This CL derived from the above original CL and it only adds new
    FinalStatuses (MemoryLimitExceeded and FailToGetMemoryUsage).
    These statuses are not used for now.
    
    Plan for supporting concurrent prerenders.
    1. add new FinalStatuses <-- this CL
    2. add new Finch params
    3. allow concurrent prerenders up to the number defined by a Finch
    param and add tests
    4. add metrics
    
    The original CL (https://chromium-review.googlesource.com/c/chromium/src/+/3757811) included 1, 2 and 3
    and there is no change between a part of the original CL and this CL.
    
    Bug: 1273341
    Change-Id: If50fbbfaf05550a6d673ae242a7b85301c2dbb6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767048
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027655}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -1425,6 +1425,8 @@ Page::PrerenderFinalStatus PrerenderFinalStatusToProtocol(
     case PrerenderHost::FinalStatus::kEmbedderTriggeredAndSameOriginRedirected:
       return Page::PrerenderFinalStatusEnum::
           EmbedderTriggeredAndSameOriginRedirected;
+    case PrerenderHost::FinalStatus::kFailToGetMemoryUsage:
+      return Page::PrerenderFinalStatusEnum::FailToGetMemoryUsage;
     case PrerenderHost::FinalStatus::kInProgressNavigation:
       return Page::PrerenderFinalStatusEnum::InProgressNavigation;
     case PrerenderHost::FinalStatus::kInvalidSchemeNavigation:
@@ -1439,6 +1441,8 @@ Page::PrerenderFinalStatus PrerenderFinalStatusToProtocol(
       return Page::PrerenderFinalStatusEnum::MainFrameNavigation;
     case PrerenderHost::FinalStatus::kMaxNumOfRunningPrerendersExceeded:
       return Page::PrerenderFinalStatusEnum::MaxNumOfRunningPrerendersExceeded;
+    case PrerenderHost::FinalStatus::kMemoryLimitExceeded:
+      return Page::PrerenderFinalStatusEnum::MemoryLimitExceeded;
     case PrerenderHost::FinalStatus::kMixedContent:
       return Page::PrerenderFinalStatusEnum::MixedContent;
     case PrerenderHost::FinalStatus::kMojoBinderPolicy:
