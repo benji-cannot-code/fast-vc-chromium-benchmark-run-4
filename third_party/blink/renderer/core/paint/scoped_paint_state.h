42ad9a56cc15ae26873e5053e714a7a39a77e942
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-07-25 09:20:20 +0000

    Revert "Pixel area based content outside viewport width"
    This reverts commit 770df5b9f5ea632b230f16c3aba379c15fdcdc62.
    
    Reason for revert: https://crbug.com/1346981#c1
    
    Original change's description:
    > Pixel area based content outside viewport width
    >
    > TextContentOutsideViewportPercentage UKM metrics currently evaluated based on content width vs viewport width.
    > This implementation does not capture real mobile friendliness well.
    > Users see contents not by scroll bar but text/image contents, so we should evaluate it based on contents.
    > More description go/refine-covwp
    >
    > Bug: 1317567
    > Change-Id: I993a024642c1ae1b66ab81553a949b3670488527
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654276
    > Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027407}
    
    Bug: 1317567,1346981
    Change-Id: Ia4162245c3a42749c3923a75d8bc255dd1f70a45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782844
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1027670}

--- third_party/blink/renderer/core/paint/scoped_paint_state.h
+++ third_party/blink/renderer/core/paint/scoped_paint_state.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h"
-#include "third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/platform/graphics/paint/scoped_paint_chunk_properties.h"
 
@@ -123,8 +122,6 @@ class ScopedBoxContentsPaintState : public ScopedPaintState {
 
  private:
   void AdjustForBoxContents(const LayoutBox&);
-  absl::optional<MobileFriendlinessChecker::IgnoreBeyondViewportScope>
-      mf_ignore_scope_;
 };
 
 }  // namespace blink
