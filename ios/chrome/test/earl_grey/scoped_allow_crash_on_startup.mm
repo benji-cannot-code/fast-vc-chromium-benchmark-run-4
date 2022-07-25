a9c20ce6e284f3282e395ef69a2d013924fdf3f1
Justin Cohen <justincohen@google.com>
2022-07-25 20:16:59 +0000

    ios: Fix swizzledRecordIssue on iOS16.
    Add a new error description for iOS16 and don't throw XCTIssue any
    ignorable error.
    
    Bug: 1347060
    Change-Id: I7af7084224dabfa802ed752b5a23c57acfa1c682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785103
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027927}

--- ios/chrome/test/earl_grey/scoped_allow_crash_on_startup.mm
+++ ios/chrome/test/earl_grey/scoped_allow_crash_on_startup.mm
@@ -78,9 +78,10 @@ const GREYHostApplicationCrashHandler kHostApplicationCrashHandler = ^{
           containsString:@"Couldn’t communicate with a helper application. Try "
                          @"your operation again. If that fails, quit and "
                          @"relaunch the application and try again."] ||
-      [description containsString:@" crashed in "]) {
-    [[NSException exceptionWithName:@"XCTIssue" reason:description
-                           userInfo:nil] raise];
+      [description containsString:@" crashed in "] ||
+      [description containsString:@"Failed to get matching snapshot"]) {
+    // Ignore these exceptions, since we expect to crash.
+    return;
   }
   INVOKE_ORIGINAL_IMP1(void, @selector(swizzledRecordIssue:), issue);
 }
