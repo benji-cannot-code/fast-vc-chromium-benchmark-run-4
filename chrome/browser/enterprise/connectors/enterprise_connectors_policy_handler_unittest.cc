72c06083bf107c6a8cc382fd1c3e95b61e059b7c
Steffen Seckler <sseckler@google.com>
2022-07-25 13:09:01 +0000

    Add FileTransfer connector to more tests.
    Add FileTransfer enum value to CloudBinaryUploadService and
    EnterpriseConnectorsPolicyHandler test.
    
    Bug: 1345644
    Change-Id: I9a6ddf95808f19c088e603b3f3e4efc9af5765ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780814
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Steffen Seckler <sseckler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027726}

--- chrome/browser/enterprise/connectors/enterprise_connectors_policy_handler_unittest.cc
+++ chrome/browser/enterprise/connectors/enterprise_connectors_policy_handler_unittest.cc
@@ -211,6 +211,9 @@ class EnterpriseConnectorsPolicyHandlerLocalTest
     if (policy_pref() != kOnFileAttachedPref ||
         policy_pref() != kOnFileDownloadedPref ||
         policy_pref() != kOnBulkDataEntryPref ||
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+        policy_pref() != kOnFileTransferPref ||
+#endif
         policy_pref() != kOnPrintPref) {
       return false;
     }
@@ -254,6 +257,9 @@ INSTANTIATE_TEST_SUITE_P(
                                      kOnFileDownloadedPref,
                                      kOnBulkDataEntryPref,
                                      kOnPrintPref,
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+                                     kOnFileTransferPref,
+#endif
                                      kOnSecurityEventPref,
                                      kSendDownloadToCloudPref),
                      testing::Bool()));
