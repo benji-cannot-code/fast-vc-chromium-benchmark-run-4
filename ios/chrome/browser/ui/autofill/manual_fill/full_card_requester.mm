887cc5502497afed3d3780008de43275144ad236
Ernesto Izquierdo Clua <eic@google.com>
2022-07-25 20:26:33 +0000

    [CVC] Gate presentation of new prompt behind feature flag
    This patch adds a function to create the proper card unmask prompt view bridge based on a feature flag. When the flag is on, the new bridge and thus the new cvc component will be used. When it is off, the legacy component is used.
    
    Fixed: 1345459
    Change-Id: Ia53c37beb2d7ddf0f2744887d4f6d6ef0c367149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769150
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027935}

--- ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.mm
@@ -7,22 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/autofill/core/browser/browser_autofill_manager.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/ui/autofill/legacy_card_unmask_prompt_view_bridge.h"
+#import "ios/chrome/browser/ui/autofill/create_card_unmask_prompt_view_bridge.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-
-autofill::CardUnmaskPromptView* CreateCardUnmaskPromptViewBridge(
-    autofill::CardUnmaskPromptControllerImpl* controller,
-    UIViewController* base_view_controller) {
-  return new autofill::LegacyCardUnmaskPromptViewBridge(controller,
-                                                        base_view_controller);
-}
-
-}
 
 FullCardRequester::FullCardRequester(UIViewController* base_view_controller,
                                      ChromeBrowserState* browser_state)
@@ -50,7 +39,7 @@ void FullCardRequester::ShowUnmaskPrompt(
     autofill::AutofillClient::UnmaskCardReason reason,
     base::WeakPtr<autofill::CardUnmaskDelegate> delegate) {
   unmask_controller_.ShowPrompt(
-      base::BindOnce(&CreateCardUnmaskPromptViewBridge,
+      base::BindOnce(&autofill::CreateCardUnmaskPromptViewBridge,
                      base::Unretained(&unmask_controller_),
                      base::Unretained(base_view_controller_)),
       card, reason, delegate);
