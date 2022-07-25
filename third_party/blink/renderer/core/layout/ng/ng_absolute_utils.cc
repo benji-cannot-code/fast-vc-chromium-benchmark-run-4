7c44cd7854b80c1597e6ff200a30de227b305dd7
Koji Ishii <kojii@chromium.org>
2022-07-25 14:47:25 +0000

    [anchor-position] Support `min-`/`max-height`
    Following r1026474 crrev.com/c/3774553 which supported
    `min-`/`max-width`, this patch supports `min-`/`max-height`.
    
    When computing block size of out-of-flow objects:
    1. First compute intrinsic size.
    2. Then use `SetIsFixedBlockSize` to force the block size
       computed by |NGOutOfFlowLayoutPart|.
    
    The step 1 computes `min-` and `max-` block size properties,
    but descendant layouts don't have `AnchorEvaluator`, making
    the result as if these properties are `0` if they have
    `anchor-size()` functions.
    
    This patch computes these properties beforehand, and pass them
    through `NGConstraintSpace` so that descendant layouts can see
    the correct values.
    
    Bug: 1309178
    Change-Id: Iffb0b904540ff8fcefe582403e7c27fa968425dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779858
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027758}

--- third_party/blink/renderer/core/layout/ng/ng_absolute_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_absolute_utils.cc
@@ -496,6 +496,9 @@ const NGLayoutResult* ComputeOutOfFlowBlockDimensions(
 
   const auto& style = node.Style();
   const bool is_table = node.IsTable();
+  MinMaxSizes min_max_block_sizes = ComputeMinMaxBlockSizes(
+      space, style, border_padding, computed_available_size.block_size,
+      anchor_evaluator);
 
   auto IntrinsicBlockSizeFunc = [&]() -> LayoutUnit {
     DCHECK(!node.IsReplaced());
@@ -510,6 +513,9 @@ const NGLayoutResult* ComputeOutOfFlowBlockDimensions(
           {dimensions->size.inline_size, space.AvailableSize().block_size});
       builder.SetIsFixedInlineSize(true);
       builder.SetPercentageResolutionSize(space.PercentageResolutionSize());
+      // Use the computed |MinMaxSizes| because |node.Layout()| can't resolve
+      // the `anchor-size()` function.
+      builder.SetOverrideMinMaxBlockSizes(min_max_block_sizes);
 
       if (space.IsInitialColumnBalancingPass()) {
         // The |fragmentainer_offset_delta| will not make a difference in the
@@ -553,9 +559,6 @@ const NGLayoutResult* ComputeOutOfFlowBlockDimensions(
     LayoutUnit main_block_size = ResolveMainBlockLength(
         space, style, border_padding, main_block_length, IntrinsicBlockSizeFunc,
         computed_available_size.block_size, anchor_evaluator);
-    MinMaxSizes min_max_block_sizes = ComputeMinMaxBlockSizes(
-        space, style, border_padding, computed_available_size.block_size,
-        anchor_evaluator);
 
     // Manually resolve any intrinsic/content min/max block-sizes.
     // TODO(crbug.com/1135207): |ComputeMinMaxBlockSizes()| should handle this.
