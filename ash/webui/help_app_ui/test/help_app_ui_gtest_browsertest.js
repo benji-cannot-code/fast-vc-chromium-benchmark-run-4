048b1686a22f95e5c32e085069ff51680e952f77
Zufeng Wang <zufeng@google.com>
2022-07-25 14:16:35 +0000

    help-app: Add browsertest for loadTimeData
    It checks if loadTimeData is available in the guest frame. Similar to
    the test in the media app (see bug).
    
    Bug: b:239890610
    Change-Id: I98dae515035d8f330f10904ccac6245b826950d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779860
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027748}

--- ash/webui/help_app_ui/test/help_app_ui_gtest_browsertest.js
+++ ash/webui/help_app_ui/test/help_app_ui_gtest_browsertest.js
@@ -181,6 +181,10 @@ TEST_F('HelpAppUIGtestBrowserTest', 'GuestHasLang', () => {
   runHelpAppTestInGuest('GuestHasLang');
 });
 
+TEST_F('HelpAppUIGtestBrowserTest', 'GuestLoadsLoadTimeData', () => {
+  runHelpAppTestInGuest('GuestLoadsLoadTimeData');
+});
+
 TEST_F('HelpAppUIGtestBrowserTest', 'GuestCanSearchWithHeadings', () => {
   runHelpAppTestInGuest('GuestCanSearchWithHeadings');
 });
