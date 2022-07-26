f14c5c597807497c25f1b9f6020e060975efd48a
Sophie Chang <sophiechang@chromium.org>
2022-07-26 16:13:01 +0000

    Add per-cluster metrics for visit, keywords, contains search
    Bug: 1347285
    Change-Id: If5709acd982cc3290b1d1fe943bf33bad5625df6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785060
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028293}

--- components/history_clusters/core/on_device_clustering_backend.cc
+++ components/history_clusters/core/on_device_clustering_backend.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/history_clusters_util.h"
 #include "components/history_clusters/core/keyword_cluster_finalizer.h"
 #include "components/history_clusters/core/label_cluster_finalizer.h"
+#include "components/history_clusters/core/metrics_cluster_finalizer.h"
 #include "components/history_clusters/core/noisy_cluster_finalizer.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "components/history_clusters/core/on_device_clustering_util.h"
@@ -328,7 +329,8 @@ void OnDeviceClusteringBackend::OnAllVisitsFinishedProcessing(
   base::OnceCallback<std::vector<history::Cluster>()> clustering_callback =
       base::BindOnce(
           &OnDeviceClusteringBackend::ClusterVisitsOnBackgroundThread,
-          engagement_score_provider_ != nullptr, std::move(cluster_visits),
+          clustering_request_source, engagement_score_provider_ != nullptr,
+          std::move(cluster_visits),
           std::move(human_readable_entity_name_to_entity_metadata_map));
 
   switch (clustering_request_source) {
@@ -346,6 +348,7 @@ void OnDeviceClusteringBackend::OnAllVisitsFinishedProcessing(
 // static
 std::vector<history::Cluster>
 OnDeviceClusteringBackend::ClusterVisitsOnBackgroundThread(
+    ClusteringRequestSource clustering_request_source,
     bool engagement_score_provider_is_valid,
     std::vector<history::ClusterVisit> visits,
     base::flat_map<std::string, optimization_guide::EntityMetadata>
@@ -391,6 +394,12 @@ OnDeviceClusteringBackend::ClusterVisitsOnBackgroundThread(
       human_readable_entity_name_to_entity_metadata_map));
   if (GetConfig().should_label_clusters) {
     cluster_finalizers.push_back(std::make_unique<LabelClusterFinalizer>());
+  }
+  if (clustering_request_source ==
+      ClusteringRequestSource::kKeywordCacheGeneration) {
+    // Only log metrics for whole clusters when it is a query-less state to get
+    // a better lay of the land. We estimate this by using the request source.
+    cluster_finalizers.push_back(std::make_unique<MetricsClusterFinalizer>());
   }
 
   // Group visits into clusters.
