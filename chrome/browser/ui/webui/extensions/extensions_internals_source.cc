9f35c7cf6cdd60a2fffcdff67ee4cb47f1de45a5
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-07-26 17:48:17 +0000

    webui: Migrate /chrome/browser/ui/webui/extensions URLDataSources to GetMimeType(GURL)
    Changes to override the GURL variant of GetMimeType and where appropriate
    changes the overrides' implementations to use the GURL to get a path.
    
    Context:
    
    Many overrides of GetMimeType() do their own parsing, for example, to
    turn it back into a GURL[1][2][3], get query params[4], etc. This is
    dangerous specially when dealing with untrusted data. Additionally,
    the path passed to GetMimeType includes query params and not all
    implementations[5] take that into account.
    
    There is now a GetMimeType() virtual function that takes a GURL,
    so overrides can use methods like path(), GetPathForRequest(), query(),
    etc. to avoid doing manual parsing.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/search/most_visited_iframe_source.cc;l=58;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/devtools_ui_data_source.cc;l=35-40;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [3] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/test_data_source.cc;l=68;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [4] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/webui/new_tab_page/untrusted_source.cc;l=196;drc=1560c59051181c15267c40ae37dae98c235feaa0
    [5] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/web_applications/terminal_source.cc;l=183;drc=1560c59051181c15267c40ae37dae98c235feaa0
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 1344742
    Change-Id: I052ea18ac6a8d54f174617b0eca8f233bc0897af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774956
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028337}

--- chrome/browser/ui/webui/extensions/extensions_internals_source.cc
+++ chrome/browser/ui/webui/extensions/extensions_internals_source.cc
@@ -474,7 +474,7 @@ std::string ExtensionsInternalsSource::GetSource() {
   return chrome::kChromeUIExtensionsInternalsHost;
 }
 
-std::string ExtensionsInternalsSource::GetMimeType(const std::string& path) {
+std::string ExtensionsInternalsSource::GetMimeType(const GURL& url) {
   return "text/plain";
 }
 
