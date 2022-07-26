3d1cb48cfe3bb9e96c9e8b0f063b0526518f7651
Joey Arhar <jarhar@chromium.org>
2022-07-26 19:42:57 +0000

    Don't re-lock DisplayLocks during forced unlock
    When a DisplayLock is unlocked via ForceUnlockIfNeeded, subsequent
    updates to the DisplayLock can cause it to become locked again which is
    problematic.
    
    This patch prevents the DisplayLock from being locked again until the
    next frame.
    
    Fixed: 1338135
    Change-Id: I07790658e25ea9fe2f4e8de154e3a58e7e08892b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751710
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028405}

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -1057,6 +1057,9 @@ bool DisplayLockContext::ForceUnlockIfNeeded() {
               layout_invalidation_reason::kDisplayLock);
         }
       }
+      // If we forced unlock, then we need to prevent subsequent calls to
+      // Lock() until the next frame.
+      SetRequestedState(EContentVisibility::kVisible);
     }
     return true;
   }
