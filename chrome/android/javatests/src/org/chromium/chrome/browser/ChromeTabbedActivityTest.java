42f8dbd3548b6a2816e92f8ad5769774e03cafa5
Salvador Guerrero <salg@google.com>
2022-07-26 17:33:15 +0000

    [Sheriff] Disabled failing tests in ChromeTabbedActivityTest
    Bug: 1347506
    Change-Id: I26411ca69d9ab7b0477b1352a942737a33832146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785458
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Owners-Override: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028328}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ChromeTabbedActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ChromeTabbedActivityTest.java
@@ -27,6 +27,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -74,6 +75,7 @@ public class ChromeTabbedActivityTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1347506")
     public void testTabVisibility() {
         // Create two tabs - tab[0] in the foreground and tab[1] in the background.
         final TabImpl[] tabs = new TabImpl[2];
@@ -121,6 +123,7 @@ public class ChromeTabbedActivityTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1347506")
     public void testMultiUrlIntent() {
         Intent viewIntent = new Intent(
                 Intent.ACTION_VIEW, Uri.parse(sActivityTestRule.getTestServer().getURL("/first")));
