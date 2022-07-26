8a464bf11e6e11016bdafe49a1ea197351910223
Vladimir Levin <vmpstr@chromium.org>
2022-07-26 16:49:23 +0000

    SET: Avoid hit testing shared elements in the real dom
    This patch ensures we don't hit test the elements that we don't paint.
    In other words, elements whose contents are displayed via pseudo
    elements are not hit-tested in their dom locations.
    
    R=khushalsagar@chromium.org
    
    Bug: 1335432
    Change-Id: I84ee68e481b8193c264d21f1d552128cd9d050fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781416
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028313}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/scroll_timeline.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/style_request.h"
+#include "third_party/blink/renderer/core/document_transition/document_transition_supplement.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -1483,6 +1484,18 @@ PaintLayer* PaintLayer::HitTestLayer(
        HitTestRequest::kIgnoreZeroOpacityObjects) &&
       !layout_object.HasNonZeroEffectiveOpacity()) {
     return nullptr;
+  }
+
+  // TODO(vmpstr): We need to add a simple document flag which says whether
+  // there is an ongoing transition, since this may be too heavy of a check for
+  // each hit test.
+  if (auto* supplement = DocumentTransitionSupplement::FromIfExists(
+          layout_object.GetDocument())) {
+    // This means that the contents of the object are drawn elsewhere.
+    if (supplement->GetTransition()->IsRepresentedViaPseudoElements(
+            layout_object)) {
+      return nullptr;
+    }
   }
 
   ShouldRespectOverflowClipType clip_behavior = kRespectOverflowClip;
