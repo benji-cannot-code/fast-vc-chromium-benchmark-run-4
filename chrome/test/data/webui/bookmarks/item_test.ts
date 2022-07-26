ea4a8f6df275cd9403e9a95e94a8a27455d53bb7
Rebekah Potter <rbpotter@chromium.org>
2022-07-26 18:58:53 +0000

    Reland WebUI: Migrate cr/ui/store to TypeScript
    Add a TypeScript version of cr/ui/store (cr/ui/store_ts.ts), following
    the previous example of assert_ts. Use this new version in bookmarks,
    the only Desktop user, so that the closure target and JS file can be
    made CrOS only.
    
    Note: externs approach is not viable for Store since it is a class that
    is extended by some JS users in OS settings.
    
    Bug: 1316438
    Change-Id: Iadfde5fa21505630b599ca2e9e6bd2c48a98a746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785389
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028383}

--- chrome/test/data/webui/bookmarks/item_test.ts
+++ chrome/test/data/webui/bookmarks/item_test.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {BookmarksItemElement, selectItem} from 'chrome://bookmarks/bookmarks.js';
+import {BookmarkNode, BookmarksItemElement, selectItem} from 'chrome://bookmarks/bookmarks.js';
 import {assertDeepEquals, assertEquals, assertNotEquals} from 'chrome://webui-test/chai_assert.js';
 
 import {TestStore} from './test_store.js';
@@ -31,7 +31,9 @@ suite('<bookmarks-item>', function() {
 
   test('changing the url changes the favicon', function() {
     const favicon = item.$.icon.style.backgroundImage;
-    store.data.nodes['2'] = createItem('0', {url: 'https://mail.google.com'});
+    store.data.nodes['2'] =
+        (createItem('0', {url: 'https://mail.google.com'}) as unknown as
+         BookmarkNode);
     store.notifyObservers();
     assertNotEquals(favicon, item.$.icon.style.backgroundImage);
   });
--- chrome/test/data/webui/bookmarks/test_store.ts
+++ chrome/test/data/webui/bookmarks/test_store.ts
@@ -3,15 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {createEmptyState, reduceAction, Store} from 'chrome://bookmarks/bookmarks.js';
-import {TestStore as CrUiTestStore} from 'chrome://webui-test/test_store.js';
+import {BookmarksPageState, createEmptyState, reduceAction, Store} from 'chrome://bookmarks/bookmarks.js';
+import {TestStore as CrUiTestStore} from 'chrome://webui-test/test_store_ts.js';
 
-export class TestStore extends CrUiTestStore {
+export class TestStore extends CrUiTestStore<BookmarksPageState> {
   constructor(data: any) {
-    super(data, Store, createEmptyState(), reduceAction);
+    super(data as BookmarksPageState, createEmptyState(), reduceAction);
   }
 
-  override replaceSingleton() {
+  replaceSingleton() {
     Store.setInstance(this);
   }
 }
