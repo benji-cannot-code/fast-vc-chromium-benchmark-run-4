01d606cdd4434250dbd604b3c03560bdda916bfa
Hamda <hmare@google.com>
2022-07-26 23:11:15 +0000

    [MacDT] Create the Secure Enclave key client
    This change creates the secure enclave key client which interacts
    with the Secure Enclave Helper and also the Secure Enclave for key
    management. This client is needed for following the Mac key rotation
    flow discussed in the design document http://go/mac-key-rotation-dd
    
    Bug: b/235363918
    Change-Id: Ib50d600dd866aab8ca72a4e0f4d848a440147da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750303
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028501}

--- chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.cc
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.h"
 
+#include "build/build_config.h"
+
 namespace enterprise_connectors {
 
 namespace constants {
@@ -14,6 +16,13 @@ const char kBinaryFileName[] = "chrome-management-service";
 const char kGroupName[] = "chromemgmt";
 
 const char kSigningKeyFilePath[] = "enrollment/DeviceTrustSigningKey";
+
+#if BUILDFLAG(IS_MAC)
+const char kTemporaryDeviceTrustSigningKeyLabel[] =
+    "GoogleChromeEnterpriseTempDTSigningKey";
+
+const char kDeviceTrustSigningKeyLabel[] = "GoogleChromeEnterpriseDTSigningKey";
+#endif
 
 }  // namespace constants
 
