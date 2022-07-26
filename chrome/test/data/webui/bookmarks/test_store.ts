a56fd8bedcc46ba9dd9ea5ee13e7f04d666e2635
Collin Baker <collinbaker@google.com>
2022-07-26 17:16:27 +0000

    Revert "WebUI: Migrate cr/ui/store to TypeScript"
    This reverts commit 5988588388348b73b96361efae283dc299f7cac3.
    
    Reason for revert: suspected cause of build failure https://ci.chromium.org/ui/p/chromium/builders/ci/linux-archive-rel/72103/overview
    
    Original change's description:
    > WebUI: Migrate cr/ui/store to TypeScript
    >
    > Add a TypeScript version of cr/ui/store (cr/ui/store_ts.ts), following
    > the previous example of assert_ts. Use this new version in bookmarks,
    > the only Desktop user, so that the closure target and JS file can be
    > made CrOS only.
    >
    > Note: externs approach is not viable for Store since it is a class that
    > is extended by some JS users in OS settings.
    >
    > Bug: 1316438
    > Change-Id: Ia85c0002339f5e3fd2accea217d6e5ec5e2fee17
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781756
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1028310}
    
    Bug: 1316438
    Change-Id: I9ca64b66491023fb2389f86a3d0249abdff4d98a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784256
    Owners-Override: Collin Baker <collinbaker@google.com>
    Commit-Queue: Collin Baker <collinbaker@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1028321}

--- chrome/test/data/webui/bookmarks/test_store.ts
+++ chrome/test/data/webui/bookmarks/test_store.ts
@@ -3,15 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {BookmarksPageState, createEmptyState, reduceAction, Store} from 'chrome://bookmarks/bookmarks.js';
-import {TestStore as CrUiTestStore} from 'chrome://webui-test/test_store_ts.js';
+import {createEmptyState, reduceAction, Store} from 'chrome://bookmarks/bookmarks.js';
+import {TestStore as CrUiTestStore} from 'chrome://webui-test/test_store.js';
 
-export class TestStore extends CrUiTestStore<BookmarksPageState> {
+export class TestStore extends CrUiTestStore {
   constructor(data: any) {
-    super(data as BookmarksPageState, createEmptyState(), reduceAction);
+    super(data, Store, createEmptyState(), reduceAction);
   }
 
-  replaceSingleton() {
+  override replaceSingleton() {
     Store.setInstance(this);
   }
 }
