49a8e81b5f32bfd0e5cd486d33af8c11329b22c4
Georg Neis <neis@chromium.org>
2022-07-26 02:40:14 +0000

    Expose BrowserManager::state_ via autotestPrivate's LacrosInfo
    This lets tests inspect the state of Lacros, for example in order to
    ensure equal conditions in performance tests.
    
    A follow-up CL will remove the isRunning predicate from LacrosInfo,
    since it is subsumed by the state.
    
    Change-Id: I04e7729e699d2090e62e7ab8e69c2ec929fbc084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780044
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028091}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -16,9 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/rotator/screen_rotation_animator_observer.h"
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chrome/browser/ash/printing/cups_printers_manager.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/web_applications/web_app_id.h"
+#include "chrome/common/extensions/api/autotest_private.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom-forward.h"
 #include "chromeos/services/machine_learning/public/mojom/model.mojom.h"
 #include "chromeos/ui/base/window_state_type.h"
@@ -333,6 +335,8 @@ class AutotestPrivateGetLacrosInfoFunction : public ExtensionFunction {
  private:
   ~AutotestPrivateGetLacrosInfoFunction() override;
   ResponseAction Run() override;
+  static api::autotest_private::LacrosState ToLacrosState(
+      crosapi::BrowserManager::State state);
 };
 
 class AutotestPrivateGetArcAppFunction : public ExtensionFunction {
