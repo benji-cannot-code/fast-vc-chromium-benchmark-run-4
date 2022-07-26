20c488ab881c7d1d201e412a28ab0643990b3fee
Rebekah Potter <rbpotter@chromium.org>
2022-07-26 16:42:24 +0000

    WebUI: Migrate cr/ui/store to TypeScript
    Add a TypeScript version of cr/ui/store (cr/ui/store_ts.ts), following
    the previous example of assert_ts. Use this new version in bookmarks,
    the only Desktop user, so that the closure target and JS file can be
    made CrOS only.
    
    Note: externs approach is not viable for Store since it is a class that
    is extended by some JS users in OS settings.
    
    Bug: 1316438
    Change-Id: Ia85c0002339f5e3fd2accea217d6e5ec5e2fee17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781756
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028310}

--- chrome/browser/resources/bookmarks/api_listener.ts
+++ chrome/browser/resources/bookmarks/api_listener.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {addWebUIListener, removeWebUIListener} from 'chrome://resources/js/cr.m.js';
-import {Action} from 'chrome://resources/js/cr/ui/store.js';
+import {Action} from 'chrome://resources/js/cr/ui/store_ts.js';
 
 import {createBookmark, editBookmark, moveBookmark, refreshNodes, removeBookmark, reorderChildren, setCanEditBookmarks, setIncognitoAvailability} from './actions.js';
 import {BrowserProxyImpl} from './browser_proxy.js';
