1142beb22bbbee28a3e2d383b10833762878ebc0
Brahim Chikhaoui <bchikhaoui@google.com>
2022-07-26 14:22:37 +0000

    [OOBE][FamilyLinkNoticeScreen]: Update screen and handler structure
    - Remove Bind/Unbind logic;
    - Remove OnViewDestroyed;
    - Remove InitializeDeprecated();
    - Migrate to OnUserAction with args list and external API prefix;
    
    Bug: b:240246687 , 1309022, 1298392
    Change-Id: I31fe67536a39b96498dbe62a1756fde35fa3f138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768286
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028247}

--- chrome/browser/ash/login/screens/family_link_notice_screen.cc
+++ chrome/browser/ash/login/screens/family_link_notice_screen.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/screens/family_link_notice_screen.h"
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -29,24 +30,13 @@ std::string FamilyLinkNoticeScreen::GetResultString(Result result) {
 }
 
 FamilyLinkNoticeScreen::FamilyLinkNoticeScreen(
-    FamilyLinkNoticeView* view,
+    base::WeakPtr<FamilyLinkNoticeView> view,
     const ScreenExitCallback& exit_callback)
     : BaseScreen(FamilyLinkNoticeView::kScreenId, OobeScreenPriority::DEFAULT),
-      view_(view),
-      exit_callback_(exit_callback) {
-  if (view_)
-    view_->Bind(this);
-}
+      view_(std::move(view)),
+      exit_callback_(exit_callback) {}
 
-FamilyLinkNoticeScreen::~FamilyLinkNoticeScreen() {
-  if (view_)
-    view_->Unbind();
-}
-
-void FamilyLinkNoticeScreen::OnViewDestroyed(FamilyLinkNoticeView* view) {
-  if (view_ == view)
-    view_ = nullptr;
-}
+FamilyLinkNoticeScreen::~FamilyLinkNoticeScreen() = default;
 
 bool FamilyLinkNoticeScreen::MaybeSkip(WizardContext* context) {
   if (!context->skip_post_login_screens_for_tests &&
@@ -76,12 +66,12 @@ void FamilyLinkNoticeScreen::ShowImpl() {
 
 void FamilyLinkNoticeScreen::HideImpl() {}
 
-void FamilyLinkNoticeScreen::OnUserActionDeprecated(
-    const std::string& action_id) {
+void FamilyLinkNoticeScreen::OnUserAction(const base::Value::List& args) {
+  const std::string& action_id = args[0].GetString();
   if (action_id == kUserActionContinue) {
     exit_callback_.Run(Result::DONE);
   } else {
-    BaseScreen::OnUserActionDeprecated(action_id);
+    BaseScreen::OnUserAction(args);
   }
 }
 
