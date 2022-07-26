41d9da8a35f1a08a68109c7a05c7cb654dceec96
Hiroki Kumazaki <kumagi@google.com>
2022-07-26 03:23:53 +0000

    Reland "Pixel area based content outside viewport width"
    This is a reland of commit 770df5b9f5ea632b230f16c3aba379c15fdcdc62
    
    Original change's description:
    > Pixel area based content outside viewport width
    >
    > TextContentOutsideViewportPercentage UKM metrics currently evaluated based on content width vs viewport width.
    > This implementation does not capture real mobile friendliness well.
    > Users see contents not by scroll bar but text/image contents, so we should evaluate it based on contents.
    > More description go/refine-covwp
    >
    > Bug: 1317567
    > Change-Id: I993a024642c1ae1b66ab81553a949b3670488527
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654276
    > Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027407}
    
    Bug: 1317567
    Change-Id: I46876a7dcecc930851918f7d45178ea49d59d543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784647
    Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028100}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -2866,6 +2866,10 @@ bool LocalFrameView::PaintTree(PaintBenchmarkMode benchmark_mode,
 
   DCHECK(GetFrame().IsLocalRoot());
 
+  absl::optional<MobileFriendlinessChecker::PaintScope> mf_scope;
+  if (mobile_friendliness_checker_)
+    mf_scope.emplace(*mobile_friendliness_checker_);
+
   auto* layout_view = GetLayoutView();
   DCHECK(layout_view);
 
--- third_party/blink/renderer/core/paint/replaced_painter.cc
+++ third_party/blink/renderer/core/paint/replaced_painter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/layout/layout_replaced.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_root.h"
+#include "third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h"
 #include "third_party/blink/renderer/core/paint/box_painter.h"
 #include "third_party/blink/renderer/core/paint/highlight_painting_utils.h"
 #include "third_party/blink/renderer/core/paint/object_painter.h"
@@ -36,6 +37,10 @@ class ScopedReplacedContentPaintState : public ScopedPaintState {
  public:
   ScopedReplacedContentPaintState(const ScopedPaintState& input,
                                   const LayoutReplaced& replaced);
+
+ private:
+  absl::optional<MobileFriendlinessChecker::IgnoreBeyondViewportScope>
+      mf_ignore_scope_;
 };
 
 ScopedReplacedContentPaintState::ScopedReplacedContentPaintState(
@@ -70,6 +75,18 @@ ScopedReplacedContentPaintState::ScopedReplacedContentPaintState(
     chunk_properties_.emplace(input_paint_info_.context.GetPaintController(),
                               new_properties, replaced,
                               input_paint_info_.DisplayItemTypeForClipping());
+  }
+
+  if (input_paint_info_.phase == PaintPhase::kForeground) {
+    if (auto* mf_checker =
+            MobileFriendlinessChecker::From(replaced.GetDocument())) {
+      PhysicalRect content_rect = replaced.ReplacedContentRect();
+      content_rect.Move(paint_offset_);
+      content_rect.Intersect(PhysicalRect(GetPaintInfo().GetCullRect().Rect()));
+      mf_checker->NotifyPaintReplaced(content_rect);
+
+      mf_ignore_scope_.emplace(*mf_checker);
+    }
   }
 }
 
