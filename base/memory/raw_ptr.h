22bd3f302f5f2a6481264220ebe4445613d6a6f0
Bartek Nowierski <bartekn@chromium.org>
2022-07-26 10:04:57 +0000

    Add missing noexcept to raw_ptr
    Change-Id: I67c55adf8012fc40c8ff28f9e3f79bee981e3f0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780518
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028176}

--- base/memory/raw_ptr.h
+++ base/memory/raw_ptr.h
@@ -789,7 +789,7 @@ class TRIVIAL_ABI GSL_POINTER raw_ptr {
     p.wrapped_ptr_ = nullptr;
   }
 
-  ALWAYS_INLINE raw_ptr& operator=(const raw_ptr& p) {
+  ALWAYS_INLINE raw_ptr& operator=(const raw_ptr& p) noexcept {
     // Duplicate before releasing, in case the pointer is assigned to itself.
     T* new_ptr = Impl::Duplicate(p.wrapped_ptr_);
     Impl::ReleaseWrappedPtr(wrapped_ptr_);
@@ -831,7 +831,7 @@ class TRIVIAL_ABI GSL_POINTER raw_ptr {
   ALWAYS_INLINE raw_ptr& operator=(const raw_ptr&) noexcept = default;
   ALWAYS_INLINE raw_ptr& operator=(raw_ptr&&) noexcept = default;
 
-  ALWAYS_INLINE ~raw_ptr() = default;
+  ALWAYS_INLINE ~raw_ptr() noexcept = default;
 
 #endif  // BUILDFLAG(USE_BACKUP_REF_PTR)
 
