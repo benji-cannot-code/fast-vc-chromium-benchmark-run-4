82a2263cde134b84bcff3271929fef5f4d2eb674
Andrew Rayskiy <greengrape@google.com>
2022-07-26 01:02:04 +0000

    CrosapiAsh: Fix ODR violation on std::unique_ptr<PrintingMetricsAsh>
    The ODR violation happened due to std::unique_ptr<PrintingMetricsAsh>
    being conditionally defined based on the USE_CUPS flag -- that caused
    units not depending on the chrome/browser/ash/crosapi source set directly
    (and therefore not importing the corresponding gni definition for this flag)
    to provide invalid offsets for crosapi function calls.
    
    Bug: b/239445586
    Change-Id: I7532db2a4a2c00b2cb736676c528543f62820dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781350
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028049}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -134,6 +134,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_CUPS)
 #include "chrome/browser/ash/crosapi/printing_metrics_ash.h"
+#else
+#include "chrome/browser/ash/crosapi/fake_printing_metrics_ash.h"
 #endif  // defined(USE_CUPS)
 
 namespace crosapi {
