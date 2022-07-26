075a68d285a8051233e5004063dc02c8ada8ff0d
Ken Rockot <rockot@google.com>
2022-07-26 22:42:44 +0000

    Fix dtor ordering in FakePermissionBrokerClient
    FakePermissionBrokerClient may retain open file descriptors and
    FdWatchControllers for them; but when they're destroyed, they're
    destroyed in the wrong order. This trips a DCHECK in the new Linux
    MessagePumpEpoll implementation, which is meant to enforce a constraint
    required by the MessagePump API but which is apparently not well
    enforced by MessagePumpLibevent.
    
    This CL switches the field order of the retained ScopedFD and its
    FdWatchController to ensure the latter is destroyed before the FD is
    closed.
    
    Fixed: 1347573
    Change-Id: I5ae54ef741886813b8be7fae14cc025bc8d86e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787404
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028486}

--- chromeos/dbus/permission_broker/fake_permission_broker_client.cc
+++ chromeos/dbus/permission_broker/fake_permission_broker_client.cc
@@ -149,7 +149,7 @@ void FakePermissionBrokerClient::OpenPathAndRegisterClient(
   std::string client_id;
   do {
     client_id = base::UnguessableToken::Create().ToString();
-  } while (base::Contains(clients, client_id));
+  } while (base::Contains(clients_, client_id));
 
   base::ScopedFD dup_lifeline_fd(HANDLE_EINTR(dup(lifeline_fd)));
   if (!dup_lifeline_fd.is_valid()) {
@@ -173,8 +173,8 @@ void FakePermissionBrokerClient::OpenPathAndRegisterClient(
                                 dup_lifeline_fd.get()));
     return;
   }
-  clients.emplace(client_id, UsbInterfaces(path, std::move(controller),
-                                           std::move(dup_lifeline_fd)));
+  clients_.emplace(client_id, UsbInterfaces(path, std::move(dup_lifeline_fd),
+                                            std::move(controller)));
 
   // No concern of OpenPath failure causing orphan client record here, as the
   // inserted client's record will still be removed when requester does error
@@ -186,8 +186,8 @@ void FakePermissionBrokerClient::OpenPathAndRegisterClient(
 void FakePermissionBrokerClient::DetachInterface(const std::string& client_id,
                                                  uint8_t iface_num,
                                                  ResultCallback callback) {
-  auto client_it = clients.find(client_id);
-  if (client_it == clients.end()) {
+  auto client_it = clients_.find(client_id);
+  if (client_it == clients_.end()) {
     LOG(ERROR) << "Unknown client_id: " << client_id;
     std::move(callback).Run(false);
     return;
@@ -204,8 +204,8 @@ void FakePermissionBrokerClient::DetachInterface(const std::string& client_id,
 void FakePermissionBrokerClient::ReattachInterface(const std::string& client_id,
                                                    uint8_t iface_num,
                                                    ResultCallback callback) {
-  auto client_it = clients.find(client_id);
-  if (client_it == clients.end()) {
+  auto client_it = clients_.find(client_id);
+  if (client_it == clients_.end()) {
     LOG(ERROR) << "Unknown client_id: " << client_id;
     std::move(callback).Run(false);
     return;
@@ -348,11 +348,11 @@ bool FakePermissionBrokerClient::RequestPortImpl(uint16_t port,
 
 FakePermissionBrokerClient::UsbInterfaces::UsbInterfaces(
     const std::string& path,
-    std::unique_ptr<base::FileDescriptorWatcher::Controller> controller,
-    base::ScopedFD lifeline_fd)
+    base::ScopedFD lifeline_fd,
+    std::unique_ptr<base::FileDescriptorWatcher::Controller> controller)
     : path(std::move(path)),
-      controller(std::move(controller)),
-      lifeline_fd(std::move(lifeline_fd)) {}
+      lifeline_fd(std::move(lifeline_fd)),
+      controller(std::move(controller)) {}
 
 FakePermissionBrokerClient::UsbInterfaces::~UsbInterfaces() = default;
 
--- chromeos/dbus/permission_broker/fake_permission_broker_client.h
+++ chromeos/dbus/permission_broker/fake_permission_broker_client.h
@@ -113,8 +113,8 @@ class COMPONENT_EXPORT(PERMISSION_BROKER) FakePermissionBrokerClient
   struct UsbInterfaces {
     UsbInterfaces(
         const std::string& path,
-        std::unique_ptr<base::FileDescriptorWatcher::Controller> controller,
-        base::ScopedFD lifeline_fd);
+        base::ScopedFD lifeline_fd,
+        std::unique_ptr<base::FileDescriptorWatcher::Controller> controller);
     ~UsbInterfaces();
     UsbInterfaces(UsbInterfaces&&);
     UsbInterfaces& operator=(UsbInterfaces&&);
@@ -123,8 +123,11 @@ class COMPONENT_EXPORT(PERMISSION_BROKER) FakePermissionBrokerClient
     UsbInterfaces& operator=(const UsbInterfaces&) = delete;
 
     std::string path;
-    std::unique_ptr<base::FileDescriptorWatcher::Controller> controller;
+
+    // NOTE: The ordering of these fields is important: `controller` must be
+    // destroyed before `lifetime_fd` is closed.
     base::ScopedFD lifeline_fd;
+    std::unique_ptr<base::FileDescriptorWatcher::Controller> controller;
   };
 
   bool RequestPortImpl(uint16_t port,
@@ -133,7 +136,7 @@ class COMPONENT_EXPORT(PERMISSION_BROKER) FakePermissionBrokerClient
                        RuleSet* hole_set);
 
   void HandleClosedClient(const std::string& client_id) {
-    clients.erase(client_id);
+    clients_.erase(client_id);
   }
 
   RuleSet tcp_hole_set_;
@@ -145,7 +148,7 @@ class COMPONENT_EXPORT(PERMISSION_BROKER) FakePermissionBrokerClient
   RuleSet tcp_deny_rule_set_;
   RuleSet udp_deny_rule_set_;
 
-  std::map<std::string, UsbInterfaces> clients;
+  std::map<std::string, UsbInterfaces> clients_;
 
   base::WeakPtrFactory<FakePermissionBrokerClient> weak_factory_{this};
 };
