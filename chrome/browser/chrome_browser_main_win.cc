129e6ccb9e303d09bedb3a7ea37c1d768d7b9f13
Owen Min <zmin@chromium.org>
2022-07-26 21:31:03 +0000

    Obsolete Enterprise.EnterCriticalPolicySection* metrics
    Obsolete the following three metrics:
    Enterprise.EnterCriticalPolicySectionDelay.Total
    Enterprise.EnterCriticalPolicySectionDelay.{Scope}.{Result}
    Enterprise.EnterCriticalPolicySectionError
    
    The associated project is paused and those metrics are not monitored
    actively. In the meantime, the data is static and we have got enough
    information.
    
    Bug: 1321631, 1341711
    Change-Id: I70e64b290adae0c989606ca7b1e91f6e59bc8bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777144
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028456}

--- chrome/browser/chrome_browser_main_win.cc
+++ chrome/browser/chrome_browser_main_win.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
-#include "chrome/browser/enterprise/util/critical_policy_section_metrics_win.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/os_crypt/app_bound_encryption_metrics_win.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -699,8 +698,6 @@ void ChromeBrowserMainPartsWin::PostBrowserStart() {
   }
 
   base::ImportantFileWriterCleaner::GetInstance().Start();
-
-  chrome::enterprise_util::MeasureAndReportCriticalPolicySectionAcquisition();
 }
 
 // static
