64bc2fc0708d84aa35a27ca8f53d9c75eb933b2f
Peter Kasting <pkasting@chromium.org>
2022-07-26 13:36:29 +0000

    Speculative fix for Android/CrOS crash.
    Perhaps we can claim to have less memory available than in
    `active_file`?
    
    Bug: 1346531
    Change-Id: Ice92d6aae9586d5a1eaddfede60ce0fae97e7919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783406
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028231}

--- base/system/sys_info_linux.cc
+++ base/system/sys_info_linux.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <algorithm>
 #include <limits>
 #include <sstream>
 
@@ -66,7 +67,7 @@ uint64_t SysInfo::AmountOfAvailablePhysicalMemory(
   // The fallback logic (when there is no MemAvailable) would be more precise
   // if we had info about zones watermarks (/proc/zoneinfo).
   int res_kb = info.available != 0
-                   ? info.available - info.active_file
+                   ? std::max(info.available - info.active_file, 0)
                    : info.free + info.reclaimable + info.inactive_file;
   return checked_cast<uint64_t>(res_kb) * 1024;
 }
