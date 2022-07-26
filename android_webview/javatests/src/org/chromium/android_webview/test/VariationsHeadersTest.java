55467389f50f78eb091163c2d33ab1fa3e68dab8
Clark DuVall <cduvall@chromium.org>
2022-07-26 00:14:45 +0000

    Add histogram to track WebView variations size
    This will help debug issues with the variations header.
    
    Bug: 977027
    Change-Id: Ib46bbc61cecf5e10912dae38d5b7703f4749edb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783228
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028018}

--- android_webview/javatests/src/org/chromium/android_webview/test/VariationsHeadersTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/VariationsHeadersTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.android_webview.AwContentsStatics;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 
 /**
@@ -28,6 +29,10 @@ public class VariationsHeadersTest {
     @Test
     public void testGetVariationsHeader() throws Throwable {
         // Check the value is equal to the base64 encoded proto with the forced variations IDs.
-        Assert.assertEquals("CAQICggi", AwContentsStatics.getVariationsHeader());
+        String expectedHeader = "CAQICggi";
+        Assert.assertEquals(expectedHeader, AwContentsStatics.getVariationsHeader());
+        Assert.assertEquals(1,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        "Android.WebView.VariationsHeaderLength", expectedHeader.length()));
     }
 }
