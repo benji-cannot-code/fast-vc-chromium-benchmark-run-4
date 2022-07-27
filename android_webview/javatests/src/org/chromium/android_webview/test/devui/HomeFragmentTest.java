1a8f1e7265ca6656c6aeaba77cd7a3f5d7f2c00d
Nate Fischer <ntfschr@chromium.org>
2022-07-27 19:00:16 +0000

    AW: deflake Dev UI tests
    No change to production logic, only tests. This adds several
    ViewUtils.waitForView() statements in Dev UI espresso tests.
    
    Bug: 1312662
    Test: run_webview_instrumentation_test_apk -f *devui*Test.*
    Change-Id: Ied3652cb54130e3035e220cda3b5ae59d8b832ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787161
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028848}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/HomeFragmentTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/HomeFragmentTest.java
@@ -42,6 +42,7 @@ import androidx.test.filters.SmallTest;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,6 +57,7 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
+import org.chromium.ui.test.util.ViewUtils;
 
 import java.util.Locale;
 
@@ -74,6 +76,14 @@ public class HomeFragmentTest {
 
     @Rule
     public BaseActivityTestRule mRule = new BaseActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        // Espresso is normally configured to automatically wait for the main thread to go idle, but
+        // BaseActivityTestRule turns that behavior off so we must explicitly wait for the View
+        // hierarchy to inflate.
+        ViewUtils.waitForView(withId(R.id.main_info_list));
+    }
 
     @After
     public void tearDown() {
@@ -245,6 +255,7 @@ public class HomeFragmentTest {
         String expectedErrorMessage = String.format(Locale.US,
                 WebViewPackageError.DIFFERENT_WEBVIEW_PROVIDER_ERROR_MESSAGE,
                 WebViewPackageHelper.loadLabel(context));
+        ViewUtils.waitForView(withId(R.id.main_error_view));
         onView(withId(R.id.main_error_view)).check(matches(isDisplayed()));
         onView(withId(R.id.error_text)).check(matches(withText(expectedErrorMessage)));
         // Since the current provider is set to a fake package not an actual installed WebView
@@ -273,7 +284,9 @@ public class HomeFragmentTest {
         String dialogExpectedMessage = String.format(Locale.US,
                 WebViewPackageError.DIFFERENT_WEBVIEW_PROVIDER_DIALOG_MESSAGE,
                 WebViewPackageHelper.loadLabel(context));
+        ViewUtils.waitForView(withId(R.id.main_error_view));
         onView(withId(R.id.main_error_view)).perform(click());
+        ViewUtils.waitForView(withText(dialogExpectedMessage));
         onView(withText(dialogExpectedMessage)).check(matches(isDisplayed()));
         // Since the current provider is set to a fake package not an actual installed WebView
         // provider, the UI should only offer to change the system WebView provider and should not
@@ -305,6 +318,7 @@ public class HomeFragmentTest {
         String expectedErrorMessage = String.format(Locale.US,
                 WebViewPackageError.DIFFERENT_WEBVIEW_PROVIDER_ERROR_MESSAGE,
                 WebViewPackageHelper.loadLabel(context));
+        ViewUtils.waitForView(withId(R.id.main_error_view));
         onView(withId(R.id.main_error_view)).check(matches(isDisplayed()));
         onView(withId(R.id.error_text)).check(matches(withText(expectedErrorMessage)));
         // Since the current provider is set to a fake package not an actual installed WebView
@@ -332,7 +346,9 @@ public class HomeFragmentTest {
         String dialogExpectedMessage = String.format(Locale.US,
                 WebViewPackageError.DIFFERENT_WEBVIEW_PROVIDER_DIALOG_MESSAGE,
                 WebViewPackageHelper.loadLabel(context));
+        ViewUtils.waitForView(withId(R.id.main_error_view));
         onView(withId(R.id.main_error_view)).perform(click());
+        ViewUtils.waitForView(withText(dialogExpectedMessage));
         onView(withText(dialogExpectedMessage)).check(matches(isDisplayed()));
         // Since the current provider is set to a fake package not an actual installed WebView
         // provider, the UI shouldn't offer opening current WebView provider dev UI. It should not
