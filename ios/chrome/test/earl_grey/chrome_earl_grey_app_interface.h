465b05b099e5fc17de70961569450fcd53f78155
Robbie Gibson <rkgibson@google.com>
2022-07-27 16:15:33 +0000

    [iOS] Add isLensEnabled method to ChromeEarlGrey
    The disabled tests from the linked bug interact with lens, so this
    method is needed to reenable them.
    
    Bug: 1293289
    Change-Id: I0fe3ff5cf72f2180ac94b2d2711868bb96a1d26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782903
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028778}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -541,6 +541,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns whether the OmniboxPopupUpdatedUI feature is enabled.
 + (BOOL)isNewOmniboxPopupEnabled;
 
+// Returns whether the UseLensToSearchForImage feature is enabled.
++ (BOOL)isUseLensToSearchForImageEnabled;
+
 // Returns whether the Thumbstrip feature is enabled for window with given
 // number.
 + (BOOL)isThumbstripEnabledForWindowWithNumber:(int)windowNumber;
--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -1130,6 +1130,10 @@ NSString* SerializedValue(const base::Value* value) {
   return base::FeatureList::IsEnabled(kIOSOmniboxUpdatedPopupUI);
 }
 
++ (BOOL)isUseLensToSearchForImageEnabled {
+  return base::FeatureList::IsEnabled(kUseLensToSearchForImage);
+}
+
 + (BOOL)isThumbstripEnabledForWindowWithNumber:(int)windowNumber {
   return ShowThumbStripInTraitCollection(
       [self windowWithNumber:windowNumber].traitCollection);
