bdc8de07cb11b9174ff1f5e76cf59b41679627ad
Angela Xiao <angelaxiao@chromium.org>
2022-07-27 23:26:31 +0000

    Quick Answers: truncate card title
    Fix title overflow behavior by truncating the title text when the length exceeds the card bounds. Using default ellipsis behavior.
    
    Bug: b/239716107
    Change-Id: I291eeba22657e5a8388d44ea779f72ecde0e989e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788736
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028985}

--- chrome/browser/ui/quick_answers/ui/quick_answers_view.h
+++ chrome/browser/ui/quick_answers/ui/quick_answers_view.h
@@ -70,6 +70,8 @@ class QuickAnswersView : public views::View {
       View* container);
   void AddAssistantIcon();
   void AddGoogleIcon();
+  int GetBoundsWidth();
+  int GetLabelWidth();
   void ResetContentView();
   void UpdateBounds();
   void UpdateQuickAnswerResult(const quick_answers::QuickAnswer& quick_answer);
