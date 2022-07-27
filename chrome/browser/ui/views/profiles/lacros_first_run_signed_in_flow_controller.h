ae255338b84860a17cd7b572b424894d21fb603d
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-07-27 18:41:08 +0000

    Add logging to track sources of Lacros FRE exit
    Bug: 1340791
    Change-Id: Ia429f3d845a82b160b54c180170d6e8e8212b21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784995
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028841}

--- chrome/browser/ui/views/profiles/lacros_first_run_signed_in_flow_controller.h
+++ chrome/browser/ui/views/profiles/lacros_first_run_signed_in_flow_controller.h
@@ -25,7 +25,7 @@ class LacrosFirstRunSignedInFlowController
       ProfilePickerWebContentsHost* host,
       Profile* profile,
       std::unique_ptr<content::WebContents> contents,
-      ProfilePicker::FirstRunExitedCallback first_run_exited_callback);
+      ProfilePicker::DebugFirstRunExitedCallback first_run_exited_callback);
   ~LacrosFirstRunSignedInFlowController() override;
   LacrosFirstRunSignedInFlowController(
       const ProfilePickerSignedInFlowController&) = delete;
@@ -43,7 +43,7 @@ class LacrosFirstRunSignedInFlowController
   bool sync_confirmation_seen_ = false;
 
   // Callback that gets called if the onboarding finishes successfully.
-  ProfilePicker::FirstRunExitedCallback first_run_exited_callback_;
+  ProfilePicker::DebugFirstRunExitedCallback first_run_exited_callback_;
 
   std::unique_ptr<signin::IdentityManager::Observer> can_retry_init_observer_;
 
