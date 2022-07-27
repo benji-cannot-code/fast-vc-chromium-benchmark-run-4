e851750e117eb0f0df38d1be2aaa0f0b50754778
Alan Cutter <alancutter@chromium.org>
2022-07-27 06:46:19 +0000

    Remove use of WebAppProvider::GetForTest() in prod code
    This CL removes the use of WebAppProvider::GetForTest() in the Tast test
    API.
    
    This fixes a deadlock due to the internal use of a nested
    base::RunLoop in prod code.
    
    CHECK_IS_TEST() has been added to help catch this case in future.
    
    Bug: 1344400
    Change-Id: Ie793282466aba4c514e24c7337e8bbd99ebc0967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764791
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028601}

--- chrome/browser/web_applications/web_app_provider.cc
+++ chrome/browser/web_applications/web_app_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/check_is_test.h"
 #include "base/feature_list.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
@@ -78,6 +79,11 @@ WebAppProvider* WebAppProvider::GetForLocalAppsUnchecked(Profile* profile) {
 
 // static
 WebAppProvider* WebAppProvider::GetForTest(Profile* profile) {
+  // Running a nested base::RunLoop outside of tests causes a deadlock. Crash
+  // immediately instead of deadlocking for easier debugging (especially for
+  // TAST tests which use prod binaries).
+  CHECK_IS_TEST();
+
   WebAppProvider* provider = GetForLocalAppsUnchecked(profile);
 
   if (provider->on_registry_ready().is_signaled())
