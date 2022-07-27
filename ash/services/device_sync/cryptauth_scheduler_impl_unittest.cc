b093c352e0aae894aaa621e411277a1512445686
Bastian Kersting <bkersting@google.com>
2022-07-27 06:57:51 +0000

    Migrate PrefService::Get to PrefService::GetValue
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1342019
    Test: ash_unittest --gtest_filter="*DeviceSyncCryptAuthSchedulerImplTest*"
    Change-Id: I3c448ccaaf8cf36aba58d652fd90e092249b8e41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779767
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028605}

--- ash/services/device_sync/cryptauth_scheduler_impl_unittest.cc
+++ ash/services/device_sync/cryptauth_scheduler_impl_unittest.cc
@@ -254,8 +254,9 @@ class DeviceSyncCryptAuthSchedulerImplTest : public testing::Test {
 
   void VerifyClientDirective(
       const cryptauthv2::ClientDirective& expected_client_directive) {
-    EXPECT_EQ(util::EncodeProtoMessageAsValueString(&expected_client_directive),
-              *pref_service_.Get(prefs::kCryptAuthSchedulerClientDirective));
+    EXPECT_EQ(
+        util::EncodeProtoMessageAsValueString(&expected_client_directive),
+        pref_service_.GetValue(prefs::kCryptAuthSchedulerClientDirective));
     EXPECT_EQ(
         base::Milliseconds(expected_client_directive.checkin_delay_millis()),
         scheduler()->GetRefreshPeriod());
@@ -340,7 +341,7 @@ class DeviceSyncCryptAuthSchedulerImplTest : public testing::Test {
       const cryptauthv2::ClientMetadata& expected_enrollment_request) {
     EXPECT_EQ(
         util::EncodeProtoMessageAsValueString(&expected_enrollment_request),
-        *pref_service_.Get(
+        pref_service_.GetValue(
             prefs::kCryptAuthSchedulerNextEnrollmentRequestClientMetadata));
   }
 
@@ -348,7 +349,7 @@ class DeviceSyncCryptAuthSchedulerImplTest : public testing::Test {
       const cryptauthv2::ClientMetadata& expected_device_sync_request) {
     EXPECT_EQ(
         util::EncodeProtoMessageAsValueString(&expected_device_sync_request),
-        *pref_service_.Get(
+        pref_service_.GetValue(
             prefs::kCryptAuthSchedulerNextDeviceSyncRequestClientMetadata));
   }
 
