63727c6000564473803e2b02b237d68536768db7
Kevin McNee <mcnee@chromium.org>
2022-07-27 22:40:04 +0000

    Remove guest view dead code involving detachment
    With OOPIF based guest view, there is no detachment operation. They can
    only be destroyed.
    
    We also add a test to cover the case when a guest is destroyed by
    attaching a newwindow guest to an existing webview element, as this
    did not appear to be covered.
    
    Bug: 533069
    Change-Id: I9c0d4a0166da30f7315114e76d6b724cc581fcbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770156
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028969}

--- components/guest_view/browser/guest_view_base.cc
+++ components/guest_view/browser/guest_view_base.cc
@@ -414,10 +414,6 @@ const GURL& GuestViewBase::GetOwnerSiteURL() const {
       ->GetSiteURL();
 }
 
-bool GuestViewBase::ShouldDestroyOnDetach() const {
-  return false;
-}
-
 void GuestViewBase::Destroy(bool also_delete) {
   if (is_being_destroyed_)
     return;
