d8a78e92cfe7792ff50da87edf1cd06074bf47c4
Tim Sergeant <tsergeant@chromium.org>
2022-07-27 21:10:40 +0000

    Intents: Use platform focus behavior for Intent Chip Button
    This matches the new behavior of page action icons following CL:3736498.
    
    Bug: 1346435
    Change-Id: I8f514c482312122db9a4b5f3b700de65f676cadb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780334
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028910}

--- chrome/browser/ui/views/location_bar/intent_chip_button.cc
+++ chrome/browser/ui/views/location_bar/intent_chip_button.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/style/platform_style.h"
 #include "ui/views/view_class_properties.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -36,7 +37,7 @@ IntentChipButton::IntentChipButton(Browser* browser,
       browser_(browser),
       delegate_(delegate) {
   SetText(l10n_util::GetStringUTF16(IDS_INTENT_CHIP_OPEN_IN_APP));
-  SetFocusBehavior(views::View::FocusBehavior::ACCESSIBLE_ONLY);
+  SetFocusBehavior(views::PlatformStyle::kDefaultFocusBehavior);
   SetTooltipText(l10n_util::GetStringUTF16(IDS_INTENT_CHIP_OPEN_IN_APP));
   SetProperty(views::kElementIdentifierKey, kIntentChipElementId);
 }
