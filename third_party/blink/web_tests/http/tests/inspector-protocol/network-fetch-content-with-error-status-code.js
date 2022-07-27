08f26cc192bcf57005785b79b4546262dd67857b
Danil Somsikov <dsv@chromium.org>
2022-07-27 08:47:39 +0000

    Logging extra info to investifate the tests timeouts.
    Bug: 1228246
    Change-Id: Iff56a183352fe5097a3e666cbd4f1edc0808ddc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788156
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028629}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network-fetch-content-with-error-status-code.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network-fetch-content-with-error-status-code.js
@@ -3,6 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const {page, session, dp} = await testRunner.startBlank(
       `Test to make sure if an xhr is fetched with the response as a blob and cross origin devtools can get body.`);
 
+  const protocolMessages = [];
+  const originalDispatchMessage = DevToolsAPI.dispatchMessage;
+  DevToolsAPI.dispatchMessage = (message) => {
+    protocolMessages.push(message);
+    originalDispatchMessage(message);
+  };
+  window.onerror = (msg) => testRunner.log('onerror: ' + msg);
+  window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
+  let errorForLog = new Error();
+  setTimeout(() => {
+    testRunner.log(protocolMessages);
+    testRunner.die('Timeout', errorForLog);
+  }, 28000);
+
   // This url should be cross origin.
   const url = 'https://127.0.0.1:8443/inspector-protocol/resources/cors-data.php';
 
@@ -34,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   `);
   testRunner.log('Evaled fetch command in page');
   await gotAllEvents;
+  errorForLog = new Error();
 
   let getRequestEventParams;
   let optionsRequestEventParams;
@@ -114,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     testRunner.log(`  Has extra info: ${optionsResponseExtra}`);
 
     const message = await dp.Network.getResponseBody({requestId: getRequestEventParams.requestId});
+    errorForLog = new Error();
     if (message.error) testRunner.log(message.error);
     testRunner.log('Response Body: ' + message.result.body);
     testRunner.completeTest();
