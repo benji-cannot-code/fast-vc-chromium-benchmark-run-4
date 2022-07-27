26a3c5e7df4e8533aceb558bd6537f4b4be68ee8
Dave Tapuska <dtapuska@chromium.org>
2022-07-27 18:49:52 +0000

    Remove Canvas histograms that are no longer recorded.
    These histograms are no expired and no longer needed. IsComposited
    was originally removed in crrev.com/c/2625289
    
    BUG=1299567
    
    Change-Id: I7964818eb3c0a952cfe161c225d7644bd178ee3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783402
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028844}

--- third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_async_blob_creator.cc
@@ -81,11 +81,6 @@ bool IsEncodeRowDeadlineNearOrPassed(base::TimeTicks deadline,
          deadline - row_encode_time_delta - kEncodeRowSlackBeforeDeadline;
 }
 
-void RecordIdleTaskStatusHistogram(
-    CanvasAsyncBlobCreator::IdleTaskStatus status) {
-  UMA_HISTOGRAM_ENUMERATION("Blink.Canvas.ToBlob.IdleTaskStatus", status);
-}
-
 void RecordInitiateEncodingTimeHistogram(ImageEncodingMimeType mime_type,
                                          base::TimeDelta elapsed_time) {
   if (mime_type == kMimeTypePng) {
@@ -451,8 +446,6 @@ void CanvasAsyncBlobCreator::ForceEncodeRowsOnCurrentThread() {
 }
 
 void CanvasAsyncBlobCreator::CreateBlobAndReturnResult() {
-  RecordIdleTaskStatusHistogram(idle_task_status_);
-
   Blob* result_blob = Blob::Create(encoded_image_.data(), encoded_image_.size(),
                                    ImageEncodingMimeTypeName(mime_type_));
   if (function_type_ == kHTMLCanvasToBlobCallback) {
@@ -514,10 +507,8 @@ void CanvasAsyncBlobCreator::RecordIdentifiabilityMetric() {
 }
 
 void CanvasAsyncBlobCreator::CreateNullAndReturnResult() {
-  RecordIdleTaskStatusHistogram(idle_task_status_);
   if (function_type_ == kHTMLCanvasToBlobCallback) {
     DCHECK(IsMainThread());
-    RecordIdleTaskStatusHistogram(idle_task_status_);
     context_->GetTaskRunner(TaskType::kCanvasBlobSerialization)
         ->PostTask(
             FROM_HERE,
