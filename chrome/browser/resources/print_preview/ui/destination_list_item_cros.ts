6d31d36fd61508ff8c011592ee170040e362ee11
Rebekah Potter <rbpotter@chromium.org>
2022-07-27 23:46:35 +0000

    WebUI: Migrate search_highlight_utils to TypeScript
    Bug: 1316438
    Change-Id: Ic46dd59d5bd681e87e67003f89382ad5c895ea21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788770
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028995}

--- chrome/browser/resources/print_preview/ui/destination_list_item_cros.ts
+++ chrome/browser/resources/print_preview/ui/destination_list_item_cros.ts
@@ -102,7 +102,7 @@ export class PrintPreviewDestinationListItemElement extends
   private isDestinationCrosLocal_: boolean;
   private configurationStatus_: DestinationConfigStatus;
 
-  private highlights_: Node[] = [];
+  private highlights_: HTMLElement[] = [];
 
   private onDestinationPropertiesChange_() {
     this.title = this.destination.displayName;
