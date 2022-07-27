97aae10341c6eb4a95a2c82d3fb4edd8ba92010f
Danil Somsikov <dsv@chromium.org>
2022-07-27 08:19:59 +0000

    Fix flakiness of pageNavigateToFragment.js by waiting for the page to load before the second requests. This ensures that the next navigation is always same document and loaderId is not reported.
    Bug: 1345479
    Change-Id: Ia7f254d872810caa39052e8bdbd8e2106b9679c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780169
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028618}

--- third_party/blink/web_tests/inspector-protocol/page/pageNavigateToFragment.js
+++ third_party/blink/web_tests/inspector-protocol/page/pageNavigateToFragment.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Page.enable();
   let result = await dp.Page.navigate({url: testRunner.url('../resources/blank.html')});
   testRunner.log(result);
+  await dp.Page.onceLoadEventFired();
   result = await dp.Page.navigate({url: testRunner.url('../resources/blank.html#fragment')});
   testRunner.log(result);
 
