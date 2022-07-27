1ccc14ecd161c21872bc2ad67b8570b68999c3e6
Ed Chin <edchin@google.com>
2022-07-27 18:55:54 +0000

    [ios][feed] Check buildflag in IsFeedBackgroundRefreshEnabled()
    - IsFeedBackgroundRefreshEnabled() returns false if buildflag is not defined, so it now is gated by both buildflag and feature flag.
    - Registers background refresh behind IsFeedBackgroundRefreshEnabled()
    - Allows crashes when DiscoverFeedService is not available since it is expected even when the app is launched into the background.
    
    Bug: 1343695
    Change-Id: I805d0ca0643381319673b65d86b9a178a8befb43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770962
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028847}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #import "base/metrics/field_trial_params.h"
+#import "ios/chrome/app/background_mode_buildflags.h"
 #import "ios/chrome/browser/system_flags.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -50,6 +51,9 @@ bool IsWebChannelsEnabled() {
 }
 
 bool IsFeedBackgroundRefreshEnabled() {
+#if !BUILDFLAG(IOS_BACKGROUND_MODE_ENABLED)
+  return false;
+#endif  // BUILDFLAG(IOS_BACKGROUND_MODE_ENABLED)
   static bool feedBackgroundRefreshEnabled =
       [[NSUserDefaults standardUserDefaults]
           boolForKey:kEnableFeedBackgroundRefreshForNextColdStart];
@@ -65,7 +69,7 @@ void SaveFeedBackgroundRefreshEnabledForNextColdStart() {
 
 bool IsFollowingFeedBackgroundRefreshEnabled() {
   if (experimental_flags::IsForceBackgroundRefreshForFollowingFeedEnabled()) {
-    return YES;
+    return true;
   }
   return base::GetFieldTrialParamByFeatureAsBool(
       kEnableFeedBackgroundRefresh, kEnableFollowingFeedBackgroundRefresh,
