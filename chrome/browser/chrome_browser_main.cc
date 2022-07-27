3eb60e659de4750efaa01486a33c762df60be415
Tushar Agarwal <agarwaltushar@google.com>
2022-07-27 12:20:48 +0000

    Pass new unwinders to TracingSamplerProfiler in the browser io thread.
    We pass in CreateCoreUnwindersFactory here since it lives in the
    chrome/ layer while TracingSamplerProfiler is outside of chrome/.
    
    Change-Id: Ib24e11b61f9558681386e4a608843a8217a5c3ef
    Bug: b/231934478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779877
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028685}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -120,6 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/profiler/thread_profiler.h"
 #include "chrome/common/profiler/thread_profiler_configuration.h"
+#include "chrome/common/profiler/unwind_util.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/installer/util/google_update_settings.h"
@@ -1144,9 +1145,13 @@ void ChromeBrowserMainParts::PostCreateThreads() {
 // Sampling multiple threads might cause overhead on Android and we don't want
 // to enable it unless the data is needed.
 #if !BUILDFLAG(IS_ANDROID)
+  // We pass in CreateCoreUnwindersFactory here since it lives in the chrome/
+  // layer while TracingSamplerProfiler is outside of chrome/.
   content::GetIOThreadTaskRunner({})->PostTask(
       FROM_HERE,
-      base::BindOnce(&tracing::TracingSamplerProfiler::CreateOnChildThread));
+      base::BindOnce(&tracing::TracingSamplerProfiler::
+                         CreateOnChildThreadWithCustomUnwinders,
+                     base::BindRepeating(&CreateCoreUnwindersFactory)));
 #endif
 
   tracing::SetupBackgroundTracingFieldTrial();
