2cea70074ddfdac51bc5131267a274c006b9846c
Eva Herencsarova <evih@google.com>
2022-07-27 08:56:18 +0000

    [Autofill] Add autocomplete attribute parsing testcase and modify comment
    Bug: 1007974
    Change-Id: Ie4ac8e2ed866f1b57ad82b1f7c4f3045ca282b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779532
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Eva Herencsarova <evih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028630}

--- components/autofill/core/browser/form_processing/autocomplete_attribute_processing_util_unittest.cc
+++ components/autofill/core/browser/form_processing/autocomplete_attribute_processing_util_unittest.cc
@@ -68,6 +68,7 @@ const AutocompleteAttributeTestcase kAutocompleteTestcases[]{
     {"section tel", absl::nullopt},
     {"no_section tel", absl::nullopt},
     {"no_section work tel", absl::nullopt},
+    {"section-random", {{"", HTML_MODE_NONE, HTML_TYPE_UNRECOGNIZED}}},
 
     // "webauthn" shouldn't prevent parsing, but is otherwise ignored.
     {"name webauthn", {{"", HTML_MODE_NONE, HTML_TYPE_NAME}}},
