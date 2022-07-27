617962f36e019d5603513c7d6e072e737c063d15
Pi-Hsun Shih <pihsun@chromium.org>
2022-07-27 01:19:23 +0000

    assistant: Open ChromeOS Camera App at Ash side
    ChromeOS Camera App is in Ash. Add the URL to allowlist for
    AssistantBrowserDelegateImpl::OpenUrl and UrlHandlerAsh::OpenUrlInternal
    to open it in Ash.
    
    Bug: 1337230
    Test: manually
    Change-Id: I539bf1526f98bbcae3b8a8341abe9fa5c837cafe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3723352
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028538}

--- chrome/browser/ash/crosapi/url_handler_ash.cc
+++ chrome/browser/ash/crosapi/url_handler_ash.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/crosapi/url_handler_ash.h"
 
+#include "ash/webui/camera_app_ui/url_constants.h"
 #include "ash/webui/connectivity_diagnostics/url_constants.h"
 #include "ash/webui/diagnostics_ui/url_constants.h"
 #include "ash/webui/firmware_update_ui/url_constants.h"
@@ -140,6 +141,9 @@ bool UrlHandlerAsh::OpenUrlInternal(const GURL& url) {
   } else if (target_url == GURL(chrome::kOsUIFirmwareUpdaterAppURL)) {
     app_id = ash::SystemWebAppType::FIRMWARE_UPDATE;
     target_url = GURL(ash::kChromeUIFirmwareUpdateAppURL);
+  } else if (short_target_url == GURL(ash::kChromeUICameraAppURL)) {
+    app_id = ash::SystemWebAppType::CAMERA;
+    target_url = url;
   } else if (ChromeWebUIControllerFactory::GetInstance()->CanHandleUrl(
                  target_url)) {
     app_id = ash::SystemWebAppType::OS_URL_HANDLER;
