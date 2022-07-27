9270a9076688c720cbdad1119e30edfec4bd276c
Fangzhen Song <songfangzhen@bytedance.com>
2022-07-27 22:16:34 +0000

    Window Placement: Enable VirtualDisplayMacUtil on Mac 11
    With the HEADLESS BOT FLAKY fix[*], we can enable
    `VirtualDisplayMacUtil` on all platforms higher than version 10.13. To
    maintain stability, I think we'd better enable them one by one. Below
    is the plan:
    - [done] enable on Mac 11.
    - [to be done] enable on Mac 10.15.
    - [to be done] enable on Mac 10.14.
    
    [*] https://chromium-review.googlesource.com/c/chromium/src/+/3779178
    
    Bug: 1126278
    Change-Id: If3ebabc186199a6c4785ce82ea3937a8c91b200e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782701
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028957}

--- ui/display/mac/test/virtual_display_mac_util_interactive_uitest.mm
+++ ui/display/mac/test/virtual_display_mac_util_interactive_uitest.mm
@@ -20,7 +20,7 @@ class VirtualDisplayMacUtilInteractiveUitest : public testing::Test {
 
   void SetUp() override {
     if (!display::test::VirtualDisplayMacUtil::IsAPIAvailable()) {
-      GTEST_SKIP() << "Skipping test for MacOS 11.0 and older.";
+      GTEST_SKIP() << "Skipping test for unsupported MacOS version.";
     }
 
     testing::Test::SetUp();
