e5d6b8314edea91555b0c8d5af86cc29d4c7af01
Tushar Agarwal <agarwaltushar@google.com>
2022-07-27 14:31:50 +0000

    Pass new unwinders to TracingSamplerProfiler in the renderer compositor thread.
    We pass in CreateCoreUnwindersFactory here since it lives in the
    chrome/ layer while TracingSamplerProfiler is outside of chrome/.
    
    Change-Id: Ic00e0535cecfc9a131dd909a55639e855e7dc553
    Bug: b/231934478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779897
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1028729}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pepper_permission_util.h"
 #include "chrome/common/privacy_budget/privacy_budget_settings_provider.h"
 #include "chrome/common/profiler/thread_profiler.h"
+#include "chrome/common/profiler/unwind_util.h"
 #include "chrome/common/secure_origin_allowlist.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
@@ -1343,9 +1344,13 @@ void ChromeContentRendererClient::PostCompositorThreadCreated(
       base::BindOnce(&ThreadProfiler::StartOnChildThread,
                      metrics::CallStackProfileParams::Thread::kCompositor));
   // Enable stack sampling for tracing.
+  // We pass in CreateCoreUnwindersFactory here since it lives in the chrome/
+  // layer while TracingSamplerProfiler is outside of chrome/.
   compositor_thread_task_runner->PostTask(
       FROM_HERE,
-      base::BindOnce(&tracing::TracingSamplerProfiler::CreateOnChildThread));
+      base::BindOnce(&tracing::TracingSamplerProfiler::
+                         CreateOnChildThreadWithCustomUnwinders,
+                     base::BindRepeating(&CreateCoreUnwindersFactory)));
 }
 
 bool ChromeContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
