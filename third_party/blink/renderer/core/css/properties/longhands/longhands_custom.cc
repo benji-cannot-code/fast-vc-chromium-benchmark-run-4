d381a2a32d0c63a64f8dec416285ca925fb2c2f1
Eric Willigers <ericwilligers@chromium.org>
2022-07-27 23:52:46 +0000

    Retire flag ScrollbarGutter
    CSS Overflow scrollbar-gutter has been enabled since M94.
    
    Bug: 710214
    Change-Id: I0f3929df1daac52f42e22731f2c84c8961794599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764792
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028996}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -6140,9 +6140,6 @@ const CSSValue* ScrollbarGutter::ParseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext& context,
     const CSSParserLocalContext&) const {
-  if (!RuntimeEnabledFeatures::ScrollbarGutterEnabled())
-    return nullptr;
-
   if (auto* value = css_parsing_utils::ConsumeIdent<CSSValueID::kAuto>(range))
     return value;
 
