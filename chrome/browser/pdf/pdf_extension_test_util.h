bb51b1ee85ad543bc50c5e3ee7834ee84e92cde6
K. Moon <kmoon@chromium.org>
2022-07-28 22:21:51 +0000

    Remove CountPpapiPluginProcessesForProfile() API
    Removes the CountPpapiPluginProcessesForProfile() API from
    PluginService. This API is only used externally by
    plugin_response_interceptor_url_loader_throttle_browsertest.cc for
    counting PDF plugin processes, but is unfit for this purpose ever since
    the PDF viewer migrated from Pepper (crbug.com/702993).
    
    This API is still used internally by PluginServiceImpl, but an anonymous
    free function in plugin_service_impl.cc suffices.
    
    Bug: 1306610
    Change-Id: I055c5ed2d2c192c859c594a100faeb6dc8bdfc3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789927
    Reviewed-by: Nigi <nigi@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029489}

--- chrome/browser/pdf/pdf_extension_test_util.h
+++ chrome/browser/pdf/pdf_extension_test_util.h
@@ -6,9 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PDF_PDF_EXTENSION_TEST_UTIL_H_
 #define CHROME_BROWSER_PDF_PDF_EXTENSION_TEST_UTIL_H_
 
+#include <stddef.h>
+
+#include <vector>
+
 #include "testing/gtest/include/gtest/gtest.h"
 
+class Browser;
+
 namespace content {
+class RenderFrameHost;
 class ToRenderFrameHost;
 class WebContents;
 }  // namespace content
@@ -18,6 +25,13 @@ class Point;
 }  // namespace gfx
 
 namespace pdf_extension_test_util {
+
+// Gets all the PDF plugin frames for a given `WebContents`.
+std::vector<content::RenderFrameHost*> GetPdfPluginFrames(
+    content::WebContents* contents);
+
+// Counts the total number of unique PDF plugin processes.
+size_t CountPdfPluginProcesses(Browser* browser);
 
 // Ensures, inside the given `frame`, that a PDF has either finished
 // loading or prompted a password. The result indicates success if the PDF loads
