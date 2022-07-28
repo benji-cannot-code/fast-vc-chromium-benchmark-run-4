c90148c4df86a5106724776c7999f45cd42c3132
Sonja Laurila <laurila@google.com>
2022-07-28 12:14:04 +0000

    [dPWA] Expose borderless in display-mode CSS media query
    Enable querying display-mode with `@media (display-mode: borderless)`
    when app has display_override set to borderless.
    
    Note: This doesn't yet take into account whether the Window Management
    API permission is granted.
    
    Design doc: go/borderless-design-doc
    Explainer: https://github.com/sonkkeli/borderless/blob/main/EXPLAINER.md
    Project doc: tinyurl.com/borderless-project-doc
    
    Bug: 1325830
    Change-Id: Ie7bed936ebc144f2bad34e186bac954ebaea0a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766684
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029193}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1997,6 +1997,11 @@ blink::mojom::DisplayMode Browser::GetDisplayMode(
     if (app_controller_ && app_controller_->AppUsesWindowControlsOverlay())
       return blink::mojom::DisplayMode::kWindowControlsOverlay;
 
+    // TODO(crbug.com/1325830): Add check for the Window Management API
+    // permission status.
+    if (app_controller_ && app_controller_->AppUsesBorderlessMode())
+      return blink::mojom::DisplayMode::kBorderless;
+
     return blink::mojom::DisplayMode::kStandalone;
   }
 
