316cb40875e5cc32d02a5b710b740ba6af0ec4d4
Daniel d'Andrada <dandrader@google.com>
2022-07-28 09:31:32 +0000

    Refactor unit tests to use PrefService::GetValueDict
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341998
    Change-Id: I4563bed322a04d1053ccdabfff79a2179d6cffc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789688
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029154}

--- chrome/browser/ash/crosapi/browser_util_unittest.cc
+++ chrome/browser/ash/crosapi/browser_util_unittest.cc
@@ -605,9 +605,9 @@ TEST_F(BrowserUtilTest, RecordDataVer) {
 
   base::Value expected{base::Value::Type::DICTIONARY};
   expected.SetStringKey(user_id_hash, version.GetString());
-  const base::Value* dict =
-      pref_service_.GetDictionary(browser_util::kDataVerPref);
-  EXPECT_EQ(*dict, expected);
+  const base::Value::Dict& dict =
+      pref_service_.GetValueDict(browser_util::kDataVerPref);
+  EXPECT_EQ(dict, expected);
 }
 
 TEST_F(BrowserUtilTest, RecordDataVerOverrides) {
@@ -621,9 +621,9 @@ TEST_F(BrowserUtilTest, RecordDataVerOverrides) {
   base::Value expected{base::Value::Type::DICTIONARY};
   expected.SetStringKey(user_id_hash, version2.GetString());
 
-  const base::Value* dict =
-      pref_service_.GetDictionary(browser_util::kDataVerPref);
-  EXPECT_EQ(*dict, expected);
+  const base::Value::Dict& dict =
+      pref_service_.GetValueDict(browser_util::kDataVerPref);
+  EXPECT_EQ(dict, expected);
 }
 
 TEST_F(BrowserUtilTest, RecordDataVerWithMultipleUsers) {
@@ -644,9 +644,9 @@ TEST_F(BrowserUtilTest, RecordDataVerWithMultipleUsers) {
   expected.SetStringKey(user_id_hash_1, version3.GetString());
   expected.SetStringKey(user_id_hash_2, version2.GetString());
 
-  const base::Value* dict =
-      pref_service_.GetDictionary(browser_util::kDataVerPref);
-  EXPECT_EQ(*dict, expected);
+  const base::Value::Dict& dict =
+      pref_service_.GetValueDict(browser_util::kDataVerPref);
+  EXPECT_EQ(dict, expected);
 }
 
 TEST_F(BrowserUtilTest, IsDataWipeRequiredInvalid) {
