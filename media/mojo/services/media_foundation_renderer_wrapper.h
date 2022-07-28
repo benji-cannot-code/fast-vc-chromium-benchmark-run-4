ca89bb218021117705fe5b1bd24fd4d74f7d1a43
Bill Carr <wicarr@microsoft.com>
2022-07-28 00:06:18 +0000

    Use GPU Adapter for Media Foundation D3D11Device
    This change passes the LUID of the GPU adapter used by the GPU process
    to the Media Foundation utility process to allow the MF process to
    create its D3D11Device w/ the same adapter.
    
    Without this change it was possible on dual adapter systems when
    using Window's dynamic gpu selection that the GPU process & the MF
    utility process could end up with different adapters. When MF for
    clear was in use in this case textures created for the texture pool
    in the MF process would result in a failure in the GPU process when the
    shared handle was attempted to be opened.
    
    Bug: 1321817
    Change-Id: I7025a6197b372c698a53662f5f842c5e78003974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714796
    Commit-Queue: William Carr <wicarr@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029005}

--- media/mojo/services/media_foundation_renderer_wrapper.h
+++ media/mojo/services/media_foundation_renderer_wrapper.h
@@ -28,7 +28,8 @@ namespace media {
 class MediaFoundationRendererWrapper final
     : public Renderer,
       public mojom::MediaFoundationRendererExtension,
-      public mojom::MuteStateObserver {
+      public mojom::MuteStateObserver,
+      public mojom::GpuInfoObserver {
  public:
   using RendererExtension = mojom::MediaFoundationRendererExtension;
   using ClientExtension = mojom::MediaFoundationRendererClientExtension;
@@ -71,6 +72,9 @@ class MediaFoundationRendererWrapper final
   // mojom::MuteStateObserver implementation.
   void OnMuteStateChange(bool muted) override;
 
+  // mojom::GpuInfoObserver
+  void OnGpuLuidChange(const CHROME_LUID& adapter_luid) override;
+
  private:
   void OnReceiveDCOMPSurface(GetDCOMPSurfaceCallback callback,
                              base::win::ScopedHandle handle,
@@ -92,6 +96,7 @@ class MediaFoundationRendererWrapper final
   mojo::Remote<media::mojom::MediaFoundationRendererClientExtension>
       client_extension_remote_;
   mojo::Receiver<mojom::MuteStateObserver> site_mute_observer_;
+  mojo::Receiver<mojom::GpuInfoObserver> gpu_info_observer_;
 
   float volume_ = 1.0;
   bool muted_ = false;  // Whether the site (WebContents) is muted.
