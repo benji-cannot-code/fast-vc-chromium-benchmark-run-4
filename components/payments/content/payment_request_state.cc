f0115eba5f119a43442556b95d90001728b6c0d5
Stephen McGruer <smcgruer@chromium.org>
2022-07-28 14:22:33 +0000

    [basic-card] Remove CvcUnmaskViewController
    Basic card is now fully deprecated and disabled, and we are removing its
    code from the codebase. This CL deletes CvcUnmaskViewController.
    
    Bug: 1209835
    Change-Id: If3c8f17382cc31e132e99d62e0c603ad8c6f34d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788805
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029240}

--- components/payments/content/payment_request_state.cc
+++ components/payments/content/payment_request_state.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/core/method_strings.h"
 #include "components/payments/core/payment_request_data_util.h"
 #include "components/payments/core/payments_experimental_features.h"
+#include "components/webauthn/core/browser/internal_authenticator.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_features.h"
--- components/payments/core/payment_request_base_delegate.h
+++ components/payments/core/payment_request_base_delegate.h
@@ -10,13 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
-#include "components/autofill/core/browser/payments/full_card_request.h"
 
 class GURL;
 
 namespace autofill {
 class AddressNormalizer;
-class CreditCard;
 class PersonalDataManager;
 class RegionDataLoader;
 }  // namespace autofill
@@ -46,12 +44,6 @@ class PaymentRequestBaseDelegate {
 
   // Returns the URL of the page that is currently being displayed.
   virtual const GURL& GetLastCommittedURL() const = 0;
-
-  // Starts a FullCardRequest to unmask |credit_card|.
-  virtual void DoFullCardRequest(
-      const autofill::CreditCard& credit_card,
-      base::WeakPtr<autofill::payments::FullCardRequest::ResultDelegate>
-          result_delegate) = 0;
 
   // Returns a pointer to the address normalizer to use for the duration of this
   // Payment Request.
