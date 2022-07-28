63ddf999fcc1de44f2495cb594094042a225c00d
James Cook <jamescook@chromium.org>
2022-07-28 18:06:07 +0000

    chromeos: Clean up DBusThreadManager and docs
    * Eliminate unnecessary InitializeClients() method (since clients are
      not initialized here anymore)
    * Remove references to Shill clients from docs. These clients are now
      initialized just like all the others.
    * Remove an un-implemented method declaration from ShillIPConfigClient
      (found because the comment mentioned DBusThreadManager)
    
    Bug: 647367
    Change-Id: I645f107d47d2086aea838603f3456afb4bb34b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790251
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029357}

--- chromeos/dbus/dbus_thread_manager.cc
+++ chromeos/dbus/dbus_thread_manager.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
-#include "chromeos/dbus/common/dbus_client.h"
 
 namespace chromeos {
 
@@ -19,21 +18,15 @@ DBusThreadManager::DBusThreadManager() = default;
 
 DBusThreadManager::~DBusThreadManager() = default;
 
-void DBusThreadManager::InitializeClients() {
-  // Some clients call DBusThreadManager::Get() during initialization.
-  DCHECK(g_dbus_thread_manager);
-
-  if (!IsUsingFakes())
-    VLOG(1) << "DBusThreadManager initialized for ChromeOS";
-  else
-    VLOG(1) << "DBusThreadManager created for testing";
-}
-
 // static
 void DBusThreadManager::Initialize() {
   CHECK(!g_dbus_thread_manager);
   g_dbus_thread_manager = new DBusThreadManager();
-  g_dbus_thread_manager->InitializeClients();
+
+  if (!g_dbus_thread_manager->IsUsingFakes())
+    VLOG(1) << "DBusThreadManager initialized for ChromeOS";
+  else
+    VLOG(1) << "DBusThreadManager created for testing";
 }
 
 // static
