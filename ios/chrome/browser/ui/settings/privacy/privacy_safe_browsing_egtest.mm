6dd8699e360a719793d60d86fae7e9eb7caf98b2
Joemer Ramos <joemerramos@chromium.org>
2022-07-28 14:13:34 +0000

    [iOS] Multiwindow EGtest for Privacy Safe Browsing settings
    EG test coverage for multi-window to make sure that Safe Browsing
    preferences and UI are updated properly. Additionally, there were
    linker issues when trying to depending on
    i/c/b/ui/settings/privacy/safe_browsing so the constants were separated
    from the original source set similar to i/c/b/ui/settings/privacy's
    BUILD.gn.
    
    Bug: 1329733
    Change-Id: Iefdcaa08068a635dd1dd7dd9de3da854ce43fbd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789620
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029239}

--- ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_egtest.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_egtest.mm
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/safe_browsing/core/common/features.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h"
 #import "ios/chrome/browser/ui/settings/privacy/privacy_constants.h"
+#import "ios/chrome/browser/ui/settings/privacy/safe_browsing/safe_browsing_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::SettingsDoneButton;
 using chrome_test_util::SettingsMenuPrivacyButton;
+using chrome_test_util::WindowWithNumber;
 using l10n_util::GetNSString;
 
 namespace {
@@ -53,6 +56,9 @@ namespace {
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(safe_browsing::kEnhancedProtection);
+  // TODO (crbug.com/1285974) Remove when bug is resolved.
+  config.features_disabled.push_back(kNewOverflowMenu);
+
   return config;
 }
 
@@ -60,10 +66,15 @@ namespace {
   [super setUp];
   // Ensure that Safe Browsing opt-out starts in its default (opted-in) state.
   [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kSafeBrowsingEnabled];
+  // Ensure that Enhanced Safe Browsing opt-in starts in its default (opted-out)
+  // state.
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kSafeBrowsingEnhanced];
 }
 
 - (void)tearDown {
+  // Reset preferences back to default values.
   [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kSafeBrowsingEnabled];
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kSafeBrowsingEnhanced];
   [super tearDown];
 }
 
@@ -150,6 +161,40 @@ namespace {
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
                                           kPrivacySafeBrowsingTableViewId)]
       assertWithMatcher:grey_nil()];
+}
+
+// Tests UI and preference value updates between multiple windows.
+- (void)testPrivacySafeBrowsingMultiWindow {
+  if (![ChromeEarlGrey areMultipleWindowsSupported])
+    EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
+
+  [self openPrivacySafeBrowsingSettings];
+
+  // Open privacy safe browsing settings on second window and select enhanced
+  // protection.
+  [ChromeEarlGrey openNewWindow];
+  [ChromeEarlGrey waitUntilReadyWindowWithNumber:1];
+  [ChromeEarlGrey waitForForegroundWindowCount:2];
+  [EarlGrey setRootMatcherForSubsequentInteractions:WindowWithNumber(1)];
+  [self openPrivacySafeBrowsingSettings];
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_accessibilityID(kSettingsSafeBrowsingEnhancedProtectionCellId)]
+      performAction:grey_tap()];
+
+  // Check that the second window updated the first window correctly by tapping
+  // the same option. If updated correctly, tapping the enhanced protection
+  // option again should show the enhanced protection table view.
+  [EarlGrey setRootMatcherForSubsequentInteractions:WindowWithNumber(0)];
+  [[EarlGrey selectElementWithMatcher:
+                 grey_allOf(grey_accessibilityID(
+                                kSettingsSafeBrowsingEnhancedProtectionCellId),
+                            grey_sufficientlyVisible(), nil)]
+      performAction:grey_tap()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(
+                                   kSafeBrowsingEnhancedProtectionTableViewId)]
+      assertWithMatcher:grey_notNil()];
 }
 
 #pragma mark - Helpers
