8f125dcc779c9084e88c0547cb769731e6a42cb2
Moe Ahmadi <mahmadi@chromium.org>
2022-07-28 21:57:51 +0000

    [ZPS] Ensures StartPrefetch() works independently of provider state.
    Reorganizes the code to ensure ReadStoredResponse() and StoreRemoteResponse() do not depend on the provider state paving the
    way for posting prefetch tasks on a thread pool to minimize contention
    on page load.
    
    Bug: 1262373
    Change-Id: I0ccaaa9540e0c29180e83b0aaa7664e09ac0a720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782279
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029479}

--- components/omnibox/browser/zero_suggest_provider_unittest.cc
+++ components/omnibox/browser/zero_suggest_provider_unittest.cc
@@ -594,7 +594,7 @@ TEST_F(ZeroSuggestProviderTest, StartStop) {
   EXPECT_FALSE(provider_->matches().empty());
   // Expect that network request was sent.
   EXPECT_TRUE(test_loader_factory()->IsPending(suggest_url.spec()));
-  // Expect the provider to have notified the provider listener.
+  // Expect the provider to not have notified the provider listener yet.
   EXPECT_FALSE(provider_did_notify_);
 
   // Make sure valid input restarts the provider.
@@ -604,7 +604,7 @@ TEST_F(ZeroSuggestProviderTest, StartStop) {
   EXPECT_FALSE(provider_->matches().empty());
   // Expect that network request was sent.
   EXPECT_TRUE(test_loader_factory()->IsPending(suggest_url.spec()));
-  // Expect the provider to have notified the provider listener.
+  // Expect the provider to not have notified the provider listener yet.
   EXPECT_FALSE(provider_did_notify_);
 
   // Make sure invalid input stops the provider.
@@ -615,7 +615,8 @@ TEST_F(ZeroSuggestProviderTest, StartStop) {
   EXPECT_TRUE(provider_->matches().empty());
   // Expect that network request was not sent.
   EXPECT_FALSE(test_loader_factory()->IsPending(suggest_url.spec()));
-  // Expect the provider to not have notified the provider listener.
+  // Expect the provider to not have notified the provider listener since the
+  // request was invalidated.
   EXPECT_FALSE(provider_did_notify_);
 
   // Make sure valid input restarts the provider.
@@ -625,7 +626,7 @@ TEST_F(ZeroSuggestProviderTest, StartStop) {
   EXPECT_FALSE(provider_->matches().empty());
   // Expect that network request was sent.
   EXPECT_TRUE(test_loader_factory()->IsPending(suggest_url.spec()));
-  // Expect the provider to not have notified the provider listener.
+  // Expect the provider to not have notified the provider listener yet.
   EXPECT_FALSE(provider_did_notify_);
 }
 
@@ -703,7 +704,8 @@ TEST_F(ZeroSuggestProviderTest,
   // has been explicitly disabled (`omit_asynchronous_matches_ == true`).
   ASSERT_FALSE(test_loader_factory()->IsPending(suggest_url.spec()));
 
-  // Expect the provider not to have notified the provider listener.
+  // Expect the provider to not have notified the provider listener since the
+  // request was not sent.
   EXPECT_FALSE(provider_did_notify_);
 }
 
@@ -742,8 +744,9 @@ TEST_F(ZeroSuggestProviderTest, TestPsuggestZeroSuggestHasCachedResults) {
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(provider_->done());
 
-  // Expect the provider to have notified the provider listener.
-  EXPECT_TRUE(provider_did_notify_);
+  // Expect the provider to not have notified the provider listener when using
+  // the cached response.
+  EXPECT_FALSE(provider_did_notify_);
 
   // Expect correct histograms to have been logged.
   histogram_tester.ExpectTotalCount(
@@ -897,7 +900,8 @@ TEST_F(ZeroSuggestProviderTest, TestPsuggestZeroSuggestPrefetchThenNTPOnFocus) {
         "Omnibox.ZeroSuggestProvider.NoURL.Prefetch",
         4 /*REMOTE_RESPONSE_CACHED*/, 1);
 
-    // Expect the provider not to have notified the provider listener.
+    // Expect the provider to not have notified the provider listener since the
+    // matches were not updated.
     EXPECT_FALSE(provider_did_notify_);
 
     // Expect the same empty results after the response has been handled.
@@ -955,8 +959,9 @@ TEST_F(ZeroSuggestProviderTest, TestPsuggestZeroSuggestPrefetchThenNTPOnFocus) {
         "Omnibox.ZeroSuggestProvider.NoURL.NonPrefetch",
         4 /*REMOTE_RESPONSE_CACHED*/, 1);
 
-    // Expect the provider to have notified the provider listener.
-    EXPECT_TRUE(provider_did_notify_);
+    // Expect the provider to not have notified the provider listener since the
+    // matches were not updated.
+    EXPECT_FALSE(provider_did_notify_);
 
     // Expect the same results after the response has been handled.
     ASSERT_EQ(3U, provider_->matches().size());  // 3 results, no verbatim match
