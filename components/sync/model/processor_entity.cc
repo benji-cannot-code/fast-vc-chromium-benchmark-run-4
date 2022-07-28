abadb074fdfb0faf6957c66232a28880cec8fc78
Rushan Suleymanov <rushans@google.com>
2022-07-28 09:51:42 +0000

    [Sync] Add debug logs to investigate test flakiness
    Bug: 1299874
    Change-Id: I4126ef100c5f713ba99235ec03f9ff0252df3f81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789337
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029160}

--- components/sync/model/processor_entity.cc
+++ components/sync/model/processor_entity.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/hash/sha1.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/features.h"
@@ -153,6 +152,7 @@ void ProcessorEntity::RecordIgnoredRemoteUpdate(
          metadata_.server_id() == update.entity.id);
   metadata_.set_server_id(update.entity.id);
   metadata_.set_server_version(update.response_version);
+  OnServerVersionUpdated();
   // Either these already matched, acked was just bumped to squash a pending
   // commit and this should follow, or the pending commit needs to be requeued.
   commit_requested_sequence_number_ = metadata_.acked_sequence_number();
@@ -279,6 +279,7 @@ void ProcessorEntity::ReceiveCommitResponse(const CommitResponseData& data,
   metadata_.set_server_id(data.id);
   metadata_.set_acked_sequence_number(data.sequence_number);
   metadata_.set_server_version(data.response_version);
+  OnServerVersionUpdated();
   if (!IsUnsynced()) {
     // Clear pending commit data if there hasn't been another commit request
     // since the one that is currently getting acked.
@@ -335,6 +336,20 @@ void ProcessorEntity::UpdateSpecificsHash(
   } else {
     metadata_.clear_specifics_hash();
   }
+}
+
+void ProcessorEntity::PrintLastServerVersionUpdateStackTrace() const {
+#if DCHECK_IS_ON()
+  if (debug_stack_last_version_update_) {
+    debug_stack_last_version_update_->Print();
+  }
+#endif
+}
+
+void ProcessorEntity::OnServerVersionUpdated() {
+#if DCHECK_IS_ON()
+  debug_stack_last_version_update_ = base::debug::StackTrace();
+#endif
 }
 
 }  // namespace syncer
