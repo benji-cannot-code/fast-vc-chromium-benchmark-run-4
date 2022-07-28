e38119518c2549d85309074230b47c368b2ee978
Chris Fredrickson <cfredric@chromium.org>
2022-07-28 14:34:53 +0000

    Add mojom machinery for FirstPartySetMetadata.
    This will be used in future CLs, but is being done here separately to
    make the changes easier to review.
    
    Change-Id: I3a2384455466635db9f440c1fcce692432b8cbc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787006
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029244}

--- services/network/public/cpp/cookie_manager_mojom_traits.h
+++ services/network/public/cpp/cookie_manager_mojom_traits.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_inclusion_status.h"
 #include "net/cookies/cookie_options.h"
 #include "net/cookies/cookie_partition_key_collection.h"
+#include "net/cookies/first_party_set_metadata.h"
 #include "net/cookies/same_party_context.h"
 #include "services/network/public/cpp/cookie_manager_shared_mojom_traits.h"
 #include "services/network/public/mojom/cookie_manager.mojom-forward.h"
@@ -381,6 +382,41 @@ struct StructTraits<network::mojom::SamePartyContextDataView,
 
   static bool Read(network::mojom::SamePartyContextDataView bundle,
                    net::SamePartyContext* out);
+};
+
+template <>
+struct EnumTraits<network::mojom::FirstPartySetsContextType,
+                  net::FirstPartySetsContextType> {
+  static network::mojom::FirstPartySetsContextType ToMojom(
+      net::FirstPartySetsContextType input);
+  static bool FromMojom(network::mojom::FirstPartySetsContextType input,
+                        net::FirstPartySetsContextType* output);
+};
+
+template <>
+struct StructTraits<network::mojom::FirstPartySetMetadataDataView,
+                    net::FirstPartySetMetadata> {
+  static net::SamePartyContext context(const net::FirstPartySetMetadata& m) {
+    return m.context();
+  }
+
+  static absl::optional<net::SchemefulSite> frame_owner(
+      const net::FirstPartySetMetadata& m) {
+    return m.frame_owner();
+  }
+
+  static absl::optional<net::SchemefulSite> top_frame_owner(
+      const net::FirstPartySetMetadata& m) {
+    return m.top_frame_owner();
+  }
+
+  static net::FirstPartySetsContextType first_party_sets_context_type(
+      const net::FirstPartySetMetadata& m) {
+    return m.first_party_sets_context_type();
+  }
+
+  static bool Read(network::mojom::FirstPartySetMetadataDataView metadata,
+                   net::FirstPartySetMetadata* out);
 };
 
 }  // namespace mojo
