b48837f7de67fbcf054e1c1f346d2de693860a40
Danil Somsikov <dsv@chromium.org>
2022-07-28 07:37:39 +0000

    Logging extra info to investigate the test failure.
    Bug: 1228246
    Change-Id: I42347c858b11497b993f43a52a8a50a7aca04171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790381
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029122}

--- third_party/blink/web_tests/http/tests/inspector-protocol/fetch/request-paused-network-id-cors.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/request-paused-network-id-cors.js
@@ -5,6 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   const url = 'http://localhost:8000/inspector-protocol/fetch/resources/post-echo.pl';
 
+  const protocolMessages = [];
+  const originalDispatchMessage = DevToolsAPI.dispatchMessage;
+  DevToolsAPI.dispatchMessage = (message) => {
+    protocolMessages.push(message);
+    originalDispatchMessage(message);
+  };
+
   await dp.Network.enable();
   await dp.Fetch.enable();
 
@@ -27,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const request1 = (await dp.Fetch.onceRequestPaused()).params;
   testRunner.log(`request 1: ${request1.request.method} ${request1.request.url} networkId: ${typeof request1.networkId}`);
   if (request1.request.method !== 'OPTIONS') {
+    testRunner.log(protocolMessages);
     testRunner.fail(`FAIL: preflight request expected`);
     return;
   }
