e71b2a899f8d85113e25aba6ae1a6937fcb1d2c9
Jan Lanik <janlanik@google.com>
2022-07-28 14:07:53 +0000

    [Privacy Hub] Camera integration into Privacy Hub
    Switches in Privacy Hub are connected to the camera backend.
    Mock tests included.
    
    TEST: Unit tests.
    
    Bug: b/235339277
    Bug: b/236677157
    Change-Id: Ib74395770c75a2f6da3357e1fc7273056b0187d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747799
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Jan Láník <janlanik@google.com>
    Commit-Queue: Jan Láník <janlanik@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029236}

--- ash/shell.cc
+++ ash/shell.cc
@@ -145,6 +145,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/power_prefs.h"
 #include "ash/system/power/power_status.h"
 #include "ash/system/power/video_activity_notifier.h"
+#include "ash/system/privacy_hub/camera_privacy_switch_controller.h"
 #include "ash/system/privacy_hub/microphone_privacy_switch_controller.h"
 #include "ash/system/screen_layout_observer.h"
 #include "ash/system/screen_security/screen_switch_check_controller.h"
@@ -628,6 +629,9 @@ Shell::~Shell() {
 
   hud_display::HUDDisplayView::Destroy();
 
+  // Observes `SessionController` and must be destroyed before it.
+  camera_privacy_switch_controller_.reset();
+
   for (auto& observer : shell_observers_)
     observer.OnShellDestroying();
 
@@ -998,6 +1002,11 @@ void Shell::Init(
 
   // These controllers call Shell::Get() in their constructors, so they cannot
   // be in the member initialization list.
+  // Privacy hub controllers.
+  // TODO(b/239400029): Move this to the unified privacy hub controller
+  // after it is created.
+  camera_privacy_switch_controller_ =
+      std::make_unique<CameraPrivacySwitchController>();
   touch_devices_controller_ = std::make_unique<TouchDevicesController>();
   if (!ash::features::IsBluetoothRevampEnabled()) {
     bluetooth_power_controller_ =
--- ash/shell.h
+++ ash/shell.h
@@ -104,6 +104,7 @@ class BluetoothNotificationController;
 class BluetoothPowerController;
 class BrightnessControlDelegate;
 class CalendarController;
+class CameraPrivacySwitchController;
 class CaptureModeController;
 class ControlVHistogramRecorder;
 class CrosDisplayConfig;
@@ -383,6 +384,9 @@ class ASH_EXPORT Shell : public SessionObserver,
   }
   CalendarController* calendar_controller() {
     return calendar_controller_.get();
+  }
+  CameraPrivacySwitchController* camera_privacy_switch_controller() {
+    return camera_privacy_switch_controller_.get();
   }
   CrosDisplayConfig* cros_display_config() {
     return cros_display_config_.get();
@@ -799,6 +803,8 @@ class ASH_EXPORT Shell : public SessionObserver,
   std::unique_ptr<BacklightsForcedOffSetter> backlights_forced_off_setter_;
   std::unique_ptr<BrightnessControlDelegate> brightness_control_delegate_;
   std::unique_ptr<CalendarController> calendar_controller_;
+  std::unique_ptr<CameraPrivacySwitchController>
+      camera_privacy_switch_controller_;
   std::unique_ptr<CrosDisplayConfig> cros_display_config_;
   std::unique_ptr<DarkLightModeControllerImpl> dark_light_mode_controller_;
   std::unique_ptr<DesksController> desks_controller_;
