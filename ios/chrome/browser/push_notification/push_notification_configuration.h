ed38692cefe66197023b26a0c8a91feaed480e46
Daniel White <danieltwhite@google.com>
2022-07-28 12:52:52 +0000

    Update Push Notification No-op Provider
    In this CL, the push notification no-op provider was refactored into
    using the keyed service pattern. The previous pure free function
    provider pattern was not sufficient for push notifications because it
    did not allow for saving state information.
    
    In addition, this CL merges the device's connection initialization with
    the push notification server with registering the device. These
    functions were merged because there were no use cases where the device
    would need to register itself with a new device token to the push
    notification server outside of initialization. However, a standalone
    UnregisterDevice function was implemented to address the need to
    disassociate the device to any accounts during account sign out.
    
    This is a multi-part CL:
      => https://crrev.com/c/3757931
      -  https://crrev.com/i/4823917
    
    Change-Id: I31523c3e02b2a68fc62566f599dfc5749a741480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757931
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029205}

---
+++ ios/chrome/browser/push_notification/push_notification_configuration.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_CONFIGURATION_H_
+#define IOS_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_CONFIGURATION_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol SingleSignOnService;
+
+// This class is intended to store the information needed to initialize and
+// register the device to receive push notifications from the push notification
+// server.
+@interface PushNotificationConfiguration : NSObject
+
+// A list of the application's currently logged in users' GAIA (Google Accounts
+// ID Administration) IDs.
+@property(nonatomic) NSArray<NSString*>* accountIDs;
+
+// An Apple Push Notification Service (APNS) device token. The device token is
+// used to associate the `accountIDs` to the device.
+@property(nonatomic) NSData* deviceToken;
+
+// SingleSignOnService used by PushNotificationService.
+@property(nonatomic) id<SingleSignOnService> ssoService;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_PUSH_NOTIFICATION_PUSH_NOTIFICATION_CONFIGURATION_H_
