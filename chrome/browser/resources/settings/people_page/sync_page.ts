ab2a34b6903f59e4c750dcdc4faba8d5d754af12
dpapad <dpapad@chromium.org>
2022-07-28 10:06:44 +0000

    Settings CrOS: Fix runtime error thrown when going back from syncSetup/
    Fixed: 1348107
    Change-Id: I3f768d110528cffd1c09d3c3da52c8a44b0ea524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790169
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029167}

--- chrome/browser/resources/settings/people_page/sync_page.ts
+++ chrome/browser/resources/settings/people_page/sync_page.ts
@@ -192,10 +192,12 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
             'syncPrefs.trustedVaultKeysRequired)',
       },
 
+      // <if expr="not chromeos_ash">
       showSetupCancelDialog_: {
         type: Boolean,
         value: false,
       },
+      // </if>
 
       enterPassphraseLabel_: {
         type: String,
@@ -229,7 +231,11 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
   private signedIn_: boolean;
   private syncDisabledByAdmin_: boolean;
   private syncSectionDisabled_: boolean;
+
+  // <if expr="not chromeos_ash">
   private showSetupCancelDialog_: boolean;
+  // </if>
+
   private enterPassphraseLabel_: string;
   private existingPassphraseLabel_: string;
 
@@ -392,6 +398,7 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
     });
   }
 
+  // <if expr="not chromeos_ash">
   private onSetupCancelDialogBack_() {
     this.shadowRoot!.querySelector<CrDialogElement>(
                         '#setupCancelDialog')!.cancel();
@@ -412,6 +419,7 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
   private onSetupCancelDialogClose_() {
     this.showSetupCancelDialog_ = false;
   }
+  // </if>
 
   override currentRouteChanged() {
     const router = Router.getInstance();
@@ -433,6 +441,7 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
       return;
     }
 
+    // <if expr="not chromeos_ash">
     const userActionCancelsSetup = this.syncStatus &&
         this.syncStatus.firstSetupInProgress && this.didAbort_;
     if (userActionCancelsSetup && !this.setupCancelConfirmed_) {
@@ -456,6 +465,8 @@ export class SettingsSyncPageElement extends SettingsSyncPageElementBase {
 
     // Reset variable.
     this.setupCancelConfirmed_ = false;
+
+    // </if>
 
     this.onNavigateAwayFromPage_();
   }
