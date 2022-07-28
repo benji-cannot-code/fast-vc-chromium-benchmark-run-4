95f9a5c1ee2a23e46fc08baa97d30b5f0a49cbf0
Bohdan Tyshchenko <bohdanty@google.com>
2022-07-28 09:30:46 +0000

    [OOBE] - Extend test_api.js with the GestureNavigation screen
    Add GestureNavigation screen to the OOBE test_api.js to be able to call
    some of the items on the page using the test api.
    
    Bug: b:240376954,b:239801295
    Change-Id: Id25c7abae6bd44760d579368be5c7eadb7a2e14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785022
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029153}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -601,6 +601,7 @@ class OobeApiProvider {
       DemoPreferencesScreen: new DemoPreferencesScreenTester(),
       ArcTosScreen: new ArcTosScreenTester(),
       ThemeSelectionScreen: new ThemeSelectionScreenTester(),
+      GestureNavigation: new GestureNavigationScreenTester(),
     };
 
     this.loginWithPin = function(username, pin) {
@@ -649,6 +650,22 @@ class GuestTosScreenTester extends ScreenElementApi {
   /** @return {string} */
   getNextButtonName() {
     return loadTimeData.getString('guestTosAccept');
+  }
+}
+
+class GestureNavigationScreenTester extends ScreenElementApi {
+  constructor() {
+    super('gesture-navigation');
+  }
+
+  /** @return {string} */
+  getNextButtonName() {
+    return loadTimeData.getString('gestureNavigationIntroNextButton');
+  }
+
+  /** @return {string} */
+  getGetStartedButtonName() {
+    return loadTimeData.getString('welcomeScreenGetStarted');
   }
 }
 
