98ff0792735644aad68cd0d59a340d3e6973b2cd
ssid <ssid@chromium.org>
2022-07-28 23:23:35 +0000

    [segmentation] Add code to parse the config from strings
    Add UMA trial and group names as part of the config, so adding new
    enums need not update multiple code paths with string conversions.
    The parsing code will be used to configure the segmentation platform.
    This minimizes the code changes needed on the client to configure new
    segmentation types.
    
    BUG=1346389
    
    Change-Id: I2f584d7e86ded5b1a928c3f02a2b26018b1842d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781603
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029515}

--- components/segmentation_platform/internal/selection/experimental_group_recorder.h
+++ components/segmentation_platform/internal/selection/experimental_group_recorder.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace segmentation_platform {
 
+struct Config;
 class FieldTrialRegister;
 
 // Records experimental sub groups for the given optimization target.
@@ -25,8 +26,8 @@ class ExperimentalGroupRecorder {
   // recording is complete, can be used only once.
   ExperimentalGroupRecorder(SegmentResultProvider* result_provider,
                             FieldTrialRegister* field_trial_register,
-                            const std::string& segmentation_key,
-                            proto::SegmentId selected_segment);
+                            const Config& config,
+                            proto::SegmentId segment_id);
   ~ExperimentalGroupRecorder();
 
   ExperimentalGroupRecorder(ExperimentalGroupRecorder&) = delete;
@@ -37,7 +38,7 @@ class ExperimentalGroupRecorder {
       std::unique_ptr<SegmentResultProvider::SegmentResult> result);
 
   const raw_ptr<FieldTrialRegister> field_trial_register_;
-  const std::string segmentation_key_;
+  const std::string subsegment_trial_name_;
   const proto::SegmentId segment_id_;
 
   base::WeakPtrFactory<ExperimentalGroupRecorder> weak_ptr_factory_{this};
