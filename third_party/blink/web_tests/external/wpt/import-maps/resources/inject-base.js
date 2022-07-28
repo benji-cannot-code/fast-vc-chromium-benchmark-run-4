fa58c8d488f0dd53c782419fb75bc39c641ba05c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-07-28 00:40:11 +0000

    Import wpt@6473beec7ac068080691984b8efbfd69323fbb7e
    Using wpt-import in Chromium cd482b66cc7232d3082746d5f5e0fe27f5d73920.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-sizing
    domenic@chromium.org, hiroshige@chromium.org:
      external/wpt/import-maps
    meade@chromium.org:
      external/wpt/web-animations
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Iad4f0de5659d14a6a494e89887537a98518d9ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789860
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029019}

---
+++ third_party/blink/web_tests/external/wpt/import-maps/resources/inject-base.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const el = document.createElement("base");
+el.href = "{{GET[baseurl]}}";
+document.currentScript.after(el);
