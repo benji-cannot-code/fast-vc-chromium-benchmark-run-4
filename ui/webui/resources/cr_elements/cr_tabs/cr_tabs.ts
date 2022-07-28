8535f7142c30ada9842204e80cd057ab45fff55a
dpapad <dpapad@chromium.org>
2022-07-28 21:59:04 +0000

    WebUI: Fix cr-tabs keyboard focus outline to work the first time.
    Previously the first time a tab was focused via keyboard, no focus
    outline was shown. Blurring and re-focusing a second time would result
    in keyboard focus outline to be shown.
    
    This was happening because onKeyDown_ which adds the 'keyboard-focus'
    class does not trigger when the element is focused via the 'tab' key.
    It only triggers if the element is already focused. The second time it
    accidentally worked, because 'keyboard-focus' was added when the tab was
    blurred via keyboard.
    
    Removing custom 'keyboard-focus' logic, and using the standard 'focus-outline-visible' mechanism used across WebUI instead.
    
    Bug: 1308918
    Change-Id: I3982bab4cdd9fe96e131bea0c1548e4cac1e653c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790173
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029480}

--- ui/webui/resources/cr_elements/cr_tabs/cr_tabs.ts
+++ ui/webui/resources/cr_elements/cr_tabs/cr_tabs.ts
@@ -76,7 +76,6 @@ export class CrTabsElement extends PolymerElement {
 
     this.setAttribute('role', 'tablist');
     this.addEventListener('keydown', this.onKeyDown_.bind(this));
-    this.addEventListener('mousedown', this.onMouseDown_);
   }
 
   private getAriaSelected_(index: number): string {
@@ -127,12 +126,7 @@ export class CrTabsElement extends PolymerElement {
     this.updateIndicator_(newIndicator, newTabRect, leftmostEdge, fullWidth);
   }
 
-  private onMouseDown_() {
-    this.classList.remove('keyboard-focus');
-  }
-
   private onKeyDown_(e: KeyboardEvent) {
-    this.classList.add('keyboard-focus');
     const count = this.tabNames.length;
     let newSelection;
     if (e.key === 'Home') {
