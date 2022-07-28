020ec2f15701cf576ece43b1f5751f1ead491193
Tim Sergeant <tsergeant@chromium.org>
2022-07-28 01:43:07 +0000

    Preloads: Clean up calculator web app preinstall flag
    This feature has been default enabled since October 2021, and can be
    safely cleaned up.
    
    Bug: None
    Change-Id: Ib193ae79fa6d30e1a4a9a028cef28a94702a3697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784577
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029040}

--- chrome/browser/web_applications/preinstalled_app_install_features.cc
+++ chrome/browser/web_applications/preinstalled_app_install_features.cc
@@ -22,7 +22,6 @@ namespace web_app {
 constexpr const base::Feature* kPreinstalledAppInstallFeatures[] = {
     &kMigrateDefaultChromeAppToWebAppsGSuite,
     &kMigrateDefaultChromeAppToWebAppsNonGSuite,
-    &kDefaultCalculatorWebApp,
 #if BUILDFLAG(IS_CHROMEOS)
     &kCursiveStylusPreinstall,
     &kCursiveManagedStylusPreinstall,
@@ -54,11 +53,6 @@ const base::Feature kMigrateDefaultChromeAppToWebAppsGSuite{
 const base::Feature kMigrateDefaultChromeAppToWebAppsNonGSuite{
     "MigrateDefaultChromeAppToWebAppsNonGSuite",
     base::FEATURE_ENABLED_BY_DEFAULT};
-
-// Enables installing the PWA version of the chrome os calculator instead of the
-// deprecated chrome app.
-const base::Feature kDefaultCalculatorWebApp{"DefaultCalculatorWebApp",
-                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 #if BUILDFLAG(IS_CHROMEOS)
 // Whether to allow the MigrateDefaultChromeAppToWebAppsGSuite and
