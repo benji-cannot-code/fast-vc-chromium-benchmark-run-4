9021964a73b317fc7a6ceff88d0302e82fc440f0
Yunke Zhou <yunkez@google.com>
2022-07-28 10:48:30 +0000

    Reset PIN when user changed
    Bug: 1326813
    Change-Id: I27d7770d11d4cfa223b63b992a29c2d43e81a74a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784615
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029177}

--- ash/login/ui/login_pin_input_view.h
+++ ash/login/ui/login_pin_input_view.h
@@ -45,6 +45,7 @@ class ASH_EXPORT LoginPinInputView : public views::View {
 
     views::View* code_input();
     absl::optional<std::string> GetCode();
+    bool IsEmpty();
 
    private:
     LoginPinInputView* const view_;
