034994dabc142ce735c9c0d22d16c150bdc21055
Kent Tamura <tkent@chromium.org>
2022-07-28 05:22:49 +0000

    FrameSet: Move CanResizeRow(), CanResizeColumn(), and HitTestSplit()
    ... from LayoutFrameSet to HTMLFrameSetElement.
    
    This CL has no behavior changes.
    
    Bug: 1347700
    Change-Id: I4f6707590e19fbf23c5a4bc136e5eedc48cbefb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789548
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029093}

--- third_party/blink/renderer/core/layout/layout_frame_set.h
+++ third_party/blink/renderer/core/layout/layout_frame_set.h
@@ -96,9 +96,6 @@ class LayoutFrameSet final : public LayoutBox {
 
   FrameEdgeInfo EdgeInfo() const;
 
-  bool CanResizeRow(const gfx::Point&) const;
-  bool CanResizeColumn(const gfx::Point&) const;
-
   void NotifyFrameEdgeInfoChanged();
   HTMLFrameSetElement* FrameSet() const;
 
@@ -172,8 +169,6 @@ class LayoutFrameSet final : public LayoutBox {
   void ComputeEdgeInfo();
   void FillFromEdgeInfo(const FrameEdgeInfo&, int r, int c);
   void PositionFrames();
-
-  int HitTestSplit(const GridAxis&, int position) const;
 
   LayoutObjectChildList children_;
 
