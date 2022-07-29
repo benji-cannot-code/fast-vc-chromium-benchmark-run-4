a569ecc7db6d10150e8644c2d87d4abbd052b1a8
jhtin <jhtin@chromium.org>
2022-07-29 02:07:44 +0000

    Add settings for diacritics on longpress
    Will only appear if diacritics flag is enabled.
    
    go/tscreen/624b6692084b44747efaaf3017a2845f1fe55197
    
    Bug: b/217560706
    Change-Id: I1d36adfd226d7f8eb30428d729d8a319575c38c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715077
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029583}

--- chrome/browser/resources/settings/chromeos/os_languages_page/input_page.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/input_page.js
@@ -288,7 +288,8 @@ class OsSettingsInputPageElement extends OsSettingsInputPageElementBase {
    */
   hasOptionsPageInSettings_(id) {
     return hasOptionsPageInSettings(
-        id, loadTimeData.getBoolean('allowPredictiveWriting'));
+        id, loadTimeData.getBoolean('allowPredictiveWriting'),
+        loadTimeData.getBoolean('allowDiacriticsOnPhysicalKeyboardLongpress'));
   }
 
   /**
