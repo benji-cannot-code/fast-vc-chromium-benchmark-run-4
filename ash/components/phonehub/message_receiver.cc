112e29dd65cd04c6749513cd2a4e412423b0f1fa
Pu Shi <pushi@google.com>
2022-07-29 02:02:24 +0000

    [PhoneHub] Process FeatureSetupResponse from connected phone to handle onboarding error
    -All permission denied/setup interrupted:
     https://screenshot.googleplex.com/8fCjUmSHNQxGy4f.png
    -Only camera roll access granted (notification setup
     declined/timeout/interrupted):
     https://screenshot.googleplex.com/8MTGCprvTsTsGpE.png
    -Only notification access granted:
     https://screenshot.googleplex.com/3qTXjYTonbcNzrx.png
    -All permission granted:
     https://screenshot.googleplex.com/8ZQgoi3ytvmHbmg.png
    
    Related GMS core cl: cl/457031369
    
    TEST=Verified with all combination of chromeOS flag and gms core flag
    (on/on, on/off, off/off, off/on). In each combination, verified
    onboarding results of: interrupt camera roll setup, decline/grant
    permission and interrupt notification setup, grant camera roll and
    decline notification, decline camera roll and go to notification
    settings page (wait for timeout period or grant permission),grant
    both features, set up each feature individually,
    
    Bug: b/234046703
    Change-Id: If9c08d16961037c9cf0c304eb5433fdd280617b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769275
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029580}

--- ash/components/phonehub/message_receiver.cc
+++ ash/components/phonehub/message_receiver.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/components/phonehub/message_receiver.h"
+#include "ash/components/phonehub/proto/phonehub_api.pb.h"
 
 namespace ash {
 namespace phonehub {
@@ -29,6 +30,12 @@ void MessageReceiver::NotifyPhoneStatusUpdateReceived(
     proto::PhoneStatusUpdate phone_status_update) {
   for (auto& observer : observer_list_)
     observer.OnPhoneStatusUpdateReceived(phone_status_update);
+}
+
+void MessageReceiver::NotifyFeatureSetupResponseReceived(
+    proto::FeatureSetupResponse response) {
+  for (auto& observer : observer_list_)
+    observer.OnFeatureSetupResponseReceived(response);
 }
 
 void MessageReceiver::NotifyFetchCameraRollItemsResponseReceived(
