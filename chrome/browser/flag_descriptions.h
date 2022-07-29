c3c78f6b33da3127b346ffcc97d15162c2af2554
Pu Shi <pushi@google.com>
2022-07-29 00:58:34 +0000

    [PhoneHub] Set up flag for Phone Hub feature setup error handling
    Test=unit_tests --gtest_filter='AboutFlagsHistogramTest.*' and unit_tests --gtest_filter='AboutFlags*'
    
    Bug: b/234046703
    Change-Id: Ica3f79c1d2a6837461f7fef0d0c61acad1c5f059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751912
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029557}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3166,6 +3166,9 @@ extern const char kPhoneHubCallNotificationDescription[];
 extern const char kPhoneHubCameraRollName[];
 extern const char kPhoneHubCameraRollDescription[];
 
+extern const char kPhoneHubFeatureSetupErrorHandlingName[];
+extern const char kPhoneHubFeatureSetupErrorHandlingDescription[];
+
 extern const char kProductivityLauncherName[];
 extern const char kProductivityLauncherDescription[];
 
