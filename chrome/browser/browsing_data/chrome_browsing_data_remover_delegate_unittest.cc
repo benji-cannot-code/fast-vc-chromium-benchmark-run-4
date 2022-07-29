791b45561dad41379e902189478b9bb55c6c1c4c
Roland Bock <rbock@google.com>
2022-07-29 13:51:59 +0000

    Refactor unit tests to use PrefService::GetValueList
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341998
    Test: Refactored unit tests
    Change-Id: I2e0fdc99174b21ed62e0167fd167c842ddca041e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793608
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029749}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -1693,8 +1693,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   list.Append("http://foo.com");
   prefs->Set(site_isolation::prefs::kUserTriggeredIsolatedOrigins, list);
   EXPECT_FALSE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   base::Value dict(base::Value::Type::DICTIONARY);
   dict.SetKey("https://bar.com", base::TimeToValue(base::Time::Now()));
@@ -1707,8 +1706,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   BlockUntilBrowsingDataRemoved(base::Time(), base::Time::Max(),
                                 constants::DATA_TYPE_HISTORY, false);
   EXPECT_TRUE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   EXPECT_TRUE(
       prefs->GetValueDict(site_isolation::prefs::kWebTriggeredIsolatedOrigins)
@@ -1717,8 +1715,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   // Re-add foo.com and bar.com to stored isolated origins.
   prefs->Set(site_isolation::prefs::kUserTriggeredIsolatedOrigins, list);
   EXPECT_FALSE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   prefs->Set(site_isolation::prefs::kWebTriggeredIsolatedOrigins, dict);
   EXPECT_FALSE(
@@ -1731,8 +1728,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   BlockUntilBrowsingDataRemoved(AnHourAgo(), base::Time::Max(),
                                 constants::DATA_TYPE_SITE_DATA, false);
   EXPECT_TRUE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   EXPECT_TRUE(
       prefs->GetValueDict(site_isolation::prefs::kWebTriggeredIsolatedOrigins)
@@ -1741,8 +1737,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   // Re-add foo.com and bar.com.
   prefs->Set(site_isolation::prefs::kUserTriggeredIsolatedOrigins, list);
   EXPECT_FALSE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   prefs->Set(site_isolation::prefs::kWebTriggeredIsolatedOrigins, dict);
   EXPECT_FALSE(
@@ -1753,8 +1748,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   BlockUntilBrowsingDataRemoved(base::Time(), base::Time::Max(),
                                 constants::DATA_TYPE_ISOLATED_ORIGINS, false);
   EXPECT_TRUE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   EXPECT_TRUE(
       prefs->GetValueDict(site_isolation::prefs::kWebTriggeredIsolatedOrigins)
@@ -1763,8 +1757,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
   // Re-add foo.com and bar.com.
   prefs->Set(site_isolation::prefs::kUserTriggeredIsolatedOrigins, list);
   EXPECT_FALSE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   prefs->Set(site_isolation::prefs::kWebTriggeredIsolatedOrigins, dict);
   EXPECT_FALSE(
@@ -1777,8 +1770,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, RemovePersistentIsolatedOrigins) {
       base::Time(), base::Time::Max(),
       constants::DATA_TYPE_HISTORY | constants::DATA_TYPE_SITE_DATA, false);
   EXPECT_TRUE(
-      prefs->GetList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
-          ->GetListDeprecated()
+      prefs->GetValueList(site_isolation::prefs::kUserTriggeredIsolatedOrigins)
           .empty());
   EXPECT_TRUE(
       prefs->GetValueDict(site_isolation::prefs::kWebTriggeredIsolatedOrigins)
