ceb26b20a1b91ceaa9f596b07d04cd7910a7442c
Daniel d'Andrada <dandrader@google.com>
2022-07-29 11:34:45 +0000

    Refactor unit tests to use PrefService::GetValueDict
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341998
    Change-Id: Ia6c79b363bdf3e8c096c356a8a25f20544ba22ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790977
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029715}

--- chrome/browser/enterprise/reporting/extension_request/extension_request_observer_unittest.cc
+++ chrome/browser/enterprise/reporting/extension_request/extension_request_observer_unittest.cc
@@ -134,8 +134,8 @@ class ExtensionRequestObserverTest : public BrowserWithTestWindowTest {
     size_t number_of_existing_requests =
         profile()
             ->GetPrefs()
-            ->GetDictionary(prefs::kCloudExtensionRequestIds)
-            ->DictSize();
+            ->GetValueDict(prefs::kCloudExtensionRequestIds)
+            .size();
 
     // Close the notification
     base::RunLoop close_run_loop;
@@ -214,8 +214,8 @@ TEST_F(ExtensionRequestObserverTest, NotificationClosedWithoutUserConfirmed) {
   EXPECT_EQ(pending_list.size(),
             profile()
                 ->GetPrefs()
-                ->GetDictionary(prefs::kCloudExtensionRequestIds)
-                ->DictSize());
+                ->GetValueDict(prefs::kCloudExtensionRequestIds)
+                .size());
   histogram_tester()->ExpectTotalCount(kPendingListUpdateMetricsName, 0);
 }
 
@@ -271,8 +271,8 @@ TEST_F(ExtensionRequestObserverTest, ExtensionRequestPolicyToggle) {
   EXPECT_EQ(pending_list.size(),
             profile()
                 ->GetPrefs()
-                ->GetDictionary(prefs::kCloudExtensionRequestIds)
-                ->DictSize());
+                ->GetValueDict(prefs::kCloudExtensionRequestIds)
+                .size());
   histogram_tester()->ExpectTotalCount(kPendingListUpdateMetricsName, 0);
 }
 
