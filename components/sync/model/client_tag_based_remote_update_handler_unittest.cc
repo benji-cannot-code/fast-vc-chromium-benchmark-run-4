9b20f69322ab3eda069454e32e92c18a0b3cb768
rgod <rgod@google.com>
2022-07-29 07:33:40 +0000

    [Sync] Fix unnecessary trimming for matching deleted entities
    Removes trimming remote specifics for caching in the case of a conflict between a remote update and a local entity commit, when they are
    matching deletions.
    
    This prevents trimming to fail its precondition about entity having a
    password field.
    
    Bug: 1346576
    Change-Id: I8446c23dd2d631cbbad1d776bb684446819be826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779754
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029671}

--- components/sync/model/client_tag_based_remote_update_handler_unittest.cc
+++ components/sync/model/client_tag_based_remote_update_handler_unittest.cc
@@ -133,6 +133,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest, ShouldProcessRemoteCreation) {
   ProcessSingleUpdate(GenerateUpdate(kKey1, kValue1));
   EXPECT_EQ(1u, db()->data_count());
   EXPECT_EQ(1u, db()->metadata_count());
+  EXPECT_EQ(0u, bridge()->trimmed_specifics_change_count());
 
   const EntityData& data = db()->GetData(kKey1);
   EXPECT_FALSE(data.id.empty());
@@ -190,6 +191,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest,
   ASSERT_EQ(1U, ProcessorEntityCount());
   ASSERT_EQ(1U, db()->data_change_count());
   ASSERT_EQ(1U, db()->metadata_change_count());
+  ASSERT_EQ(1U, bridge()->trimmed_specifics_change_count());
 
   // Redundant update should not clear trimmed specifics.
   UpdateResponseData update2 = GenerateUpdate(kKey1, kValue1);
@@ -197,6 +199,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest,
   ProcessSingleUpdate(std::move(update2));
   EXPECT_EQ(1U, db()->data_change_count());
   EXPECT_EQ(2U, db()->metadata_change_count());
+  ASSERT_EQ(2U, bridge()->trimmed_specifics_change_count());
   EXPECT_EQ(kUnknownField, db()->GetMetadata(kKey1)
                                .possibly_trimmed_base_specifics()
                                .unknown_fields());
@@ -240,6 +243,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest, ShouldProcessRemoteUpdates) {
   ASSERT_EQ(1U, ProcessorEntityCount());
   ASSERT_EQ(1U, db()->data_change_count());
   ASSERT_EQ(1U, db()->metadata_change_count());
+  ASSERT_EQ(0U, bridge()->trimmed_specifics_change_count());
 
   // Should update both data and metadata.
   ProcessSingleUpdate(GenerateUpdate(kKey1, kValue2));
@@ -290,6 +294,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest,
   ASSERT_EQ(1U, db()->data_change_count());
   ASSERT_EQ(1U, db()->metadata_change_count());
   ASSERT_EQ(1U, db()->GetMetadata(kKey1).server_version());
+  ASSERT_EQ(0U, bridge()->trimmed_specifics_change_count());
 
   // Mark local entity as changed.
   entity_tracker()->IncrementSequenceNumberForAllExcept({});
@@ -302,6 +307,7 @@ TEST_F(ClientTagBasedRemoteUpdateHandlerTest,
   ProcessSingleUpdate(std::move(update));
 
   EXPECT_EQ(1U, db()->data_change_count());
+  ASSERT_EQ(0U, bridge()->trimmed_specifics_change_count());
   EXPECT_EQ(2U, db()->GetMetadata(kKey1).server_version());
   EXPECT_EQ(1U, ProcessorEntityCount());
   EXPECT_FALSE(entity_tracker()->HasLocalChanges());
