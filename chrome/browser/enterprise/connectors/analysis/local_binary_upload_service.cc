adcc29d019ffc7b235d64b2ad4bffb5507befda4
Roger Tawa <rogerta@chromium.org>
2022-07-29 14:18:07 +0000

    Fix large/encrypted/unsupported files in LocalBinaryUploadService.
    Bug: b:240462967
    Change-Id: Ib2350eea9ec9fce278d4eaab3fce353171cb0d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781532
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029763}

--- chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.cc
+++ chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "chrome/browser/enterprise/connectors/analysis/analysis_settings.h"
 #include "chrome/browser/enterprise/connectors/analysis/content_analysis_sdk_manager.h"
+#include "chrome/browser/enterprise/connectors/common.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "content/public/browser/browser_thread.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/content_analysis_sdk/src/browser/include/content_analysis/sdk/analysis_client.h"
@@ -99,7 +101,7 @@ void DoLocalContentAnalysis(
     safe_browsing::BinaryUploadService::Result result,
     safe_browsing::BinaryUploadService::Request::Data data) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (result != safe_browsing::BinaryUploadService::Result::SUCCESS) {
+  if (LocalResultIsFailure(result)) {
     request->FinishRequest(result, ContentAnalysisResponse());
     return;
   }
