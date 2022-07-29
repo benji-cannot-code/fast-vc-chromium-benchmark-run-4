68760eba46263db144d1a295448a50d47b7543ab
Alexander Bolodurin <alexbn@chromium.org>
2022-07-29 17:58:26 +0000

    Reland "[lacros pwa install] query ArcAppListPrefs instead of web_app_apks prefs"
    This reverts commit cd936678a96423c278727fea8c9d862d9126e5da.
    
    New changes: none. The original CL was reverted because parent CL
    needed to be reverted, so no changes required.
    
    Original change's description:
    > Revert "[lacros pwa install] query ArcAppListPrefs instead of web_app_apks prefs"
    >
    > This reverts commit 997ee4ef6882e05c1fda8d3bd871a6f34db62f08.
    >
    > Reason for revert: Parent CL: https://crrev.com/c/3789441 is
    > likely culprit for ASAN failures.
    >
    > Original change's description:
    > > [lacros pwa install] query ArcAppListPrefs instead of web_app_apks prefs
    > >
    > > As of https://crrev.com/c/3697664, "Is web-only TWA" flag and Android
    > > package certificate fingerprint are already stored in ArcAppListPrefs,
    > > so query it instead of duplicates in web_app_apks prefs.
    > >
    > > Bug: 1329727
    > > Change-Id: I846610f7e42854e618860b95852abcbe4ff68f19
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3752216
    > > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > > Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1028560}
    >
    > Bug: 1329727
    > Change-Id: I1473f69a0b84e628f3e0bb488633d887da3ca8a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788925
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1029084}
    
    Bug: 1329727
    Change-Id: I8f2b28a2992b2a3ad97a447577a5dba2d48fe1c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791268
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Quick-Run: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029867}

--- chrome/browser/ash/apps/apk_web_app_service.h
+++ chrome/browser/ash/apps/apk_web_app_service.h
@@ -96,6 +96,7 @@ class ApkWebAppService : public KeyedService,
   void SetArcAppListPrefsForTesting(ArcAppListPrefs* prefs);
 
   bool IsWebOnlyTwa(const web_app::AppId& app_id);
+  bool IsWebOnlyTwaDeprecated(const web_app::AppId& app_id);
 
   bool IsWebAppInstalledFromArc(const web_app::AppId& web_app_id);
 
@@ -110,6 +111,8 @@ class ApkWebAppService : public KeyedService,
       const std::string& package_name);
 
   absl::optional<std::string> GetCertificateSha256Fingerprint(
+      const web_app::AppId& app_id);
+  absl::optional<std::string> GetCertificateSha256FingerprintDeprecated(
       const web_app::AppId& app_id);
 
   using WebAppCallbackForTesting =
