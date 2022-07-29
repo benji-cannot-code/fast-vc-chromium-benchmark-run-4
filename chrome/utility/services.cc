607c2bb95bc272d42e1ff82aea2fad85770be620
Maria Timbur <mtimbur@google.com>
2022-07-29 06:29:43 +0000

    Create a Mojo service to calculate the strength of a password
    Design Doc:
    https://docs.google.com/document/d/1JldYMt5dSTFnHz0KegYiG-zIh6wRH_5NBgsR4lj9cRQ/edit?usp=sharing
    
    Bug: 1345766
    Change-Id: I92870461374982d2f4c6660b83456ef00e888955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780465
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Maria Timbur <mtimbur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029653}

--- chrome/utility/services.cc
+++ chrome/utility/services.cc
@@ -60,6 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/importer/profile_import.mojom.h"
 #include "chrome/utility/importer/profile_import_impl.h"
 #include "components/mirroring/service/mirroring_service.h"
+#include "components/password_manager/services/password_strength/password_strength_calculator_impl.h"
+#include "components/password_manager/services/password_strength/public/mojom/password_strength_calculator.mojom.h"
 #include "services/proxy_resolver/proxy_resolver_factory_impl.h"  // nogncheck
 #include "services/proxy_resolver/public/mojom/proxy_resolver.mojom.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
@@ -236,6 +238,15 @@ auto RunMirroringService(
     mojo::PendingReceiver<mirroring::mojom::MirroringService> receiver) {
   return std::make_unique<mirroring::MirroringService>(
       std::move(receiver), content::UtilityThread::Get()->GetIOTaskRunner());
+}
+
+auto RunPasswordStrengthCalculator(
+    mojo::PendingReceiver<password_manager::mojom::PasswordStrengthCalculator>
+        receiver) {
+  DCHECK(base::FeatureList::IsEnabled(
+      password_manager::features::kPasswordStrengthIndicator));
+  return std::make_unique<password_manager::PasswordStrengthCalculatorImpl>(
+      std::move(receiver));
 }
 
 #endif  // !BUILDFLAG(IS_ANDROID)
@@ -424,6 +435,10 @@ void RegisterMainThreadServices(mojo::ServiceFactory& services) {
 #if !BUILDFLAG(IS_ANDROID)
   services.Add(RunProfileImporter);
   services.Add(RunMirroringService);
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kPasswordStrengthIndicator)) {
+    services.Add(RunPasswordStrengthCalculator);
+  }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(ENABLE_BROWSER_SPEECH_SERVICE)
---
+++ components/password_manager/services/password_strength/password_strength_calculator_service.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_SERVICES_PASSWORD_STRENGTH_PASSWORD_STRENGTH_CALCULATOR_SERVICE_H_
+#define COMPONENTS_PASSWORD_MANAGER_SERVICES_PASSWORD_STRENGTH_PASSWORD_STRENGTH_CALCULATOR_SERVICE_H_
+
+#include "components/password_manager/services/password_strength/public/mojom/password_strength_calculator.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace password_manager {
+
+// Launches a new instance of the PasswordStrengthCalculator service in an
+// isolated, sandboxed process, and returns a remote interface to control the
+// service. The lifetime of the process is tied to that of the Remote. May be
+// called from any thread.
+mojo::Remote<password_manager::mojom::PasswordStrengthCalculator>
+LaunchPasswordStrengthCalculator();
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_SERVICES_PASSWORD_STRENGTH_PASSWORD_STRENGTH_CALCULATOR_SERVICE_H_
