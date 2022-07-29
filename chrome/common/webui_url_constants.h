c843631ab420afca6ca8b86abf285bcdeb21da09
Rachel Wong <wrong@chromium.org>
2022-07-29 01:10:53 +0000

    [launcher] Add a URL for the launcher internals page in ash.
    This allows os://launcher-internals to work when in Lacros only mode.
    This is an ash-only page that was originally accessible from
    chrome://launcher-internals.
    
    Bug: 1347689
    Change-Id: I4c2380323e4602bd23288f083821a268d7266d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787530
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029566}

--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -368,6 +368,7 @@ extern const char kOsUIEmojiPickerURL[];
 extern const char kOsUIGpuURL[];
 extern const char kOsUIHistogramsURL[];
 extern const char kOsUIInvalidationsURL[];
+extern const char kOsUILauncherInternalsURL[];
 extern const char kOsUILockScreenNetworkURL[];
 extern const char kOsUINetExportURL[];
 extern const char kOsUIMultiDeviceInternalsURL[];
