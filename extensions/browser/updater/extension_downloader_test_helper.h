85e23ae262e4b8e0388bb0b27e4b528dd6340097
Oleg Davydov <burunduk@chromium.org>
2022-07-29 13:57:27 +0000

    [Extensions] Allow duplicate IDs in ManifestFetchData
    Despite our previous assumption, ManifestFetchData could contain
    duplicate extension IDs. It happens when two (or more) update manifest
    requests have exactly same full URLs and therefore got merged.
    
    This CL removed previously added check on ID uniqueness, stores cache
    results in a map (so we won't call cache twice) and adds a test which
    shows this behavior.
    
    Bug: b:239541510
    Change-Id: I0839ca409afecf73ef2f1b8bd4c82590c9453891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783017
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029755}

--- extensions/browser/updater/extension_downloader_test_helper.h
+++ extensions/browser/updater/extension_downloader_test_helper.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "extensions/browser/updater/extension_cache.h"
 #include "extensions/browser/updater/extension_downloader.h"
 #include "extensions/browser/updater/extension_downloader_delegate.h"
 #include "extensions/browser/updater/extension_downloader_types.h"
@@ -74,6 +75,27 @@ class MockExtensionDownloaderDelegate
 
  private:
   base::RepeatingClosure quit_closure_;
+};
+
+class MockExtensionCache : public ExtensionCache {
+ public:
+  MockExtensionCache();
+  ~MockExtensionCache() override;
+
+  void Start(base::OnceClosure callback) override;
+  void Shutdown(base::OnceClosure callback) override;
+  MOCK_METHOD1(AllowCaching, void(const ExtensionId&));
+  MOCK_METHOD4(GetExtension,
+               bool(const ExtensionId&,
+                    const std::string& expected_hash,
+                    base::FilePath* path,
+                    std::string* version));
+  MOCK_METHOD5(PutExtension,
+               void(const ExtensionId&,
+                    const std::string& hash,
+                    const base::FilePath& path,
+                    const std::string& version,
+                    PutExtensionCallback callback));
 };
 
 // Creates ExtensionDownloader for tests, with mocked delegate and
