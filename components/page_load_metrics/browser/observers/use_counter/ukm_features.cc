554a6a4dd5f1a8139f668c3561d1aab96c2ec842
Noam Rosenthal <nrosenthal@chromium.org>
2022-07-29 12:50:37 +0000

    Add prefetch-as-doc to UKM
    Bug: 1347403
    Change-Id: Ie8ed9a4e48f9d6545a58bac8afe7f8b10d543ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3785320
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029730}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -247,6 +247,8 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kClientHintsMetaEquivDelegateCH,
           WebFeature::kAuthorizationCoveredByWildcard,
           WebFeature::kImageAd,
+          WebFeature::kLinkRelPrefetchAsDocumentSameOrigin,
+          WebFeature::kLinkRelPrefetchAsDocumentCrossOrigin,
       }));
   return *opt_in_features;
 }
