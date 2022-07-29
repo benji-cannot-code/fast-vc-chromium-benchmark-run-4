e4c2540c7068856f3d262aea3db5b356af0de2d1
Elizabeth Popova <lizapopova@google.com>
2022-07-29 13:45:42 +0000

    [TTFCC] Introduce TouchToFillCreditCard{Controller,View}
    The added skeleton only defines basic Show/Hide methods, which are
    triggered from the ChromeAutofillClient. The logic will be extended in
    the follow-up CLs.
    
    Bug: 1247698
    Change-Id: I5826f5fc29f12a998c089e0dbea075b8f6d81718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784380
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029747}

---
+++ chrome/browser/touch_to_fill/payments/android/touch_to_fill_credit_card_view_impl.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/touch_to_fill/payments/android/touch_to_fill_credit_card_view_impl.h"
+
+#include "base/notreached.h"
+
+namespace autofill {
+
+TouchToFillCreditCardViewImpl::TouchToFillCreditCardViewImpl() = default;
+TouchToFillCreditCardViewImpl::~TouchToFillCreditCardViewImpl() = default;
+
+bool TouchToFillCreditCardViewImpl::Show() {
+  // TODO(crbug.com/1247698): Show Android view.
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void TouchToFillCreditCardViewImpl::Hide() {
+  // TODO(crbug.com/1247698): Hide Android view.
+  NOTIMPLEMENTED();
+}
+
+}  // namespace autofill
