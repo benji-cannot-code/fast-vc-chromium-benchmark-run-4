eb71243748567cd38466dda468f4a62bab4f5025
Wez <wez@chromium.org>
2022-07-29 17:49:48 +0000

    [fuchsia] Remove errant include of non-existent header
    Change-Id: I53f75f01dc14e863b55959be3fdfe049493f0ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794057
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029863}

--- fuchsia_web/webengine/context_provider_impl.cc
+++ fuchsia_web/webengine/context_provider_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "fuchsia_web/webengine/fidl/chromium/internal/cpp/fidl.h"
 
 ContextProviderImpl::ContextProviderImpl() = default;
 
