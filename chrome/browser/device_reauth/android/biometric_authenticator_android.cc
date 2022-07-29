e9b7b5a2b8c64853f10e2d698b8969dcf291debb
Karol Sygiet <sygiet@google.com>
2022-07-29 13:55:36 +0000

    Refactor BiometricAuthenticatorAndroid class.
    Moved 60 seconds auth validity check to newly added common class that
    will be used on different platforms. In the follow-up
    ChromeBiometricAuthenticatorCommon will take care of the memory leak
    issue.
    
    Bug: 1342024
    Change-Id: I994a5016f8402209231ac03f293fe0b42bcc7508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789742
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029754}

--- chrome/browser/device_reauth/android/biometric_authenticator_android.cc
+++ chrome/browser/device_reauth/android/biometric_authenticator_android.cc
@@ -35,8 +35,6 @@ using password_manager::UiCredential;
 
 namespace {
 
-constexpr unsigned int kAuthValidSeconds = 60;
-
 bool IsSuccessfulResult(BiometricAuthUIResult result) {
   return result == BiometricAuthUIResult::kSuccessWithUnknownMethod ||
          result == BiometricAuthUIResult::kSuccessWithBiometrics ||
@@ -129,9 +127,7 @@ void BiometricAuthenticatorAndroid::Authenticate(
 
   LogAuthRequester(requester);
 
-  if (use_last_valid_auth && last_good_auth_timestamp_.has_value() &&
-      base::TimeTicks::Now() - last_good_auth_timestamp_.value() <
-          base::Seconds(kAuthValidSeconds)) {
+  if (use_last_valid_auth && !NeedsToAuthenticate()) {
     LogAuthResult(requester, BiometricAuthFinalResult::kAuthStillValid);
     std::move(callback_).Run(/*success=*/true);
     requester_ = absl::nullopt;
@@ -174,7 +170,6 @@ BiometricAuthenticatorAndroid::CreateForTesting(
 
 void BiometricAuthenticatorAndroid::OnAuthenticationCompleted(
     BiometricAuthUIResult ui_result) {
-  bool success = IsSuccessfulResult(ui_result);
   // OnAuthenticationCompleted is called aysnchronously and by the time it's
   // invoked Chrome can cancel the authentication via
   // BiometricAuthenticatorAndroid::Cancel which resets the callback_.
@@ -182,9 +177,7 @@ void BiometricAuthenticatorAndroid::OnAuthenticationCompleted(
     return;
   }
 
-  if (success) {
-    last_good_auth_timestamp_ = base::TimeTicks::Now();
-  }
+  bool success = RecordAuthenticationResult(IsSuccessfulResult(ui_result));
 
   LogAuthResult(requester_.value(), MapUIResultToFinal(ui_result));
   std::move(callback_).Run(success);
