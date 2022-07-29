2d1b7cb23acfb281b9288df4b4ad549318a94cba
Sirisha Kavuluru <skavuluru@google.com>
2022-07-29 00:19:10 +0000

    Also checking if supplier has value to get dialog visibility supplier
    Bug: 1348339
    Change-Id: I121ae91dfa99969c125eb746923f79f4d9d46fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791729
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029539}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1807,7 +1807,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                 Supplier<Boolean> tabSwitcherDialogVisibilitySupplier =
                         isTabSwitcherOnlyRefactorEnabled
                         ? mTabSwitcherSupplier.get().getTabGridDialogVisibilitySupplier()
-                        : mStartSurfaceSupplier == null
+                        : (mStartSurfaceSupplier == null || !mStartSurfaceSupplier.hasValue())
                         ? null
                         : mStartSurfaceSupplier.get().getTabGridDialogVisibilitySupplier();
 
