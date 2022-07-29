80793a859e83ec567b3899226dfb6fa0784d8196
Justin Donnelly <jdonnelly@chromium.org>
2022-07-29 02:28:32 +0000

    [omnibox] Format URLs in the contents of open tab matches.
    Before: https://screenshot.googleplex.com/3nqCrg4p5sq3JkC.png
    After: https://screenshot.googleplex.com/Bsr8ahmgQBdSmwu.png
    
    Bug: 1287313
    Change-Id: I55239587a18aa33f716cc4b4406348f7d7a87ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792212
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029594}

--- components/omnibox/browser/open_tab_provider.cc
+++ components/omnibox/browser/open_tab_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/in_memory_url_index_types.h"
 #include "components/omnibox/browser/keyword_provider.h"
 #include "components/omnibox/browser/tab_matcher.h"
+#include "components/url_formatter/url_formatter.h"
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 #include "content/public/browser/web_contents.h"
@@ -85,7 +86,16 @@ AutocompleteMatch OpenTabProvider::CreateOpenTabMatch(
   // another default match over an open tab result.
   match.allowed_to_be_default_match = true;
 
-  match.contents = base::UTF8ToUTF16(url.spec());
+  // For display in the suggestion UI, elide all optional parts. The user has
+  // already opened these URLs so there's no need for them to make nuanced
+  // judgements about the URL (for example, whether the scheme is http:// or
+  // https:// or whether there's a "www" subdomain).
+  match.contents = url_formatter::FormatUrl(
+      url,
+      AutocompleteMatch::GetFormatTypes(/*preserve_scheme=*/false,
+                                        /*preserve_subdomain=*/false),
+      base::UnescapeRule::SPACES, nullptr, nullptr, nullptr);
+
   auto contents_terms = FindTermMatches(input.text(), match.contents);
   match.contents_class = ClassifyTermMatches(
       contents_terms, match.contents.size(),
