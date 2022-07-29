602d0577f97982a57d71359bec7ae514d1311be2
Moe Ahmadi <mahmadi@chromium.org>
2022-07-29 02:27:33 +0000

    [ZPS] Simplifies suggest providers
    Removes unnecessary base class methods requiring derived classes to
    store a reference to the autocomplete input making the code harder
    to read and maintain.
    
    This refactoring should not change the behavior of the providers.
    
    Bug: 1262373
    Change-Id: I1b319780b6ec071ade4540a6df881a1dc8134151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788800
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029593}

--- components/omnibox/browser/voice_suggest_provider.cc
+++ components/omnibox/browser/voice_suggest_provider.cc
@@ -37,8 +37,6 @@ VoiceSuggestProvider::~VoiceSuggestProvider() = default;
 
 void VoiceSuggestProvider::Start(const AutocompleteInput& input,
                                  bool minimal_changes) {
-  autocomplete_input_ = &input;
-
   MatchMap map;
   int index = 0;
   for (const auto& score_and_suggestion_pair : voice_matches_) {
@@ -52,7 +50,10 @@ void VoiceSuggestProvider::Start(const AutocompleteInput& input,
             AutocompleteMatchType::VOICE_SUGGEST, {}, false,
             ConfidenceScoreToSuggestionScore(score_and_suggestion_pair.first),
             false, {}),
-        {}, index, false, false, &map);
+        {}, input,
+        client()->GetTemplateURLService()->GetDefaultSearchProvider(),
+        client()->GetTemplateURLService()->search_terms_data(), index, false,
+        false, &map);
     ++index;
 
     // Stop if the first voice suggestion has a high relevance score suggesting
@@ -65,19 +66,6 @@ void VoiceSuggestProvider::Start(const AutocompleteInput& input,
   for (auto& match_pair : map) {
     matches_.push_back(std::move(match_pair.second));
   }
-
-  autocomplete_input_ = nullptr;
-}
-
-const TemplateURL* VoiceSuggestProvider::GetTemplateURL(bool is_keyword) const {
-  DCHECK(!is_keyword);
-  return client()->GetTemplateURLService()->GetDefaultSearchProvider();
-}
-
-const AutocompleteInput VoiceSuggestProvider::GetInput(bool is_keyword) const {
-  DCHECK(!is_keyword);
-  DCHECK(autocomplete_input_);
-  return *autocomplete_input_;
 }
 
 bool VoiceSuggestProvider::ShouldAppendExtraParams(
