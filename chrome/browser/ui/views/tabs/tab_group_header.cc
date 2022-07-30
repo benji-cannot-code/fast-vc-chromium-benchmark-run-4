415c1c986d182e9e4e45d791885d7ae05dfe3026
Taylor Bergquist <tbergquist@chromium.org>
2022-07-30 00:54:42 +0000

    Don't exit tab closing mode while a tab group editor bubble is open.
    Today's plumbing adventure, this CL links the existing widget observer in TabGroupHeader up to the TabContainer via the TabSlotController interface implemented by TabStrip.
    
    Bug: 1318122
    Change-Id: Id7f8eef179620ddb7d8a8219f6e453b96cab2685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794909
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030006}

--- chrome/browser/ui/views/tabs/tab_group_header.cc
+++ chrome/browser/ui/views/tabs/tab_group_header.cc
@@ -88,7 +88,8 @@ class TabGroupHighlightPathGenerator : public views::HighlightPathGenerator {
 
 TabGroupHeader::TabGroupHeader(TabSlotController* tab_slot_controller,
                                const tab_groups::TabGroupId& group)
-    : tab_slot_controller_(tab_slot_controller) {
+    : tab_slot_controller_(tab_slot_controller),
+      editor_bubble_tracker_(tab_slot_controller) {
   DCHECK(tab_slot_controller);
 
   set_group(group);
@@ -514,10 +515,15 @@ BEGIN_METADATA(TabGroupHeader, TabSlotView)
 ADD_READONLY_PROPERTY_METADATA(int, DesiredWidth)
 END_METADATA
 
+TabGroupHeader::EditorBubbleTracker::EditorBubbleTracker(
+    TabSlotController* tab_slot_controller)
+    : tab_slot_controller_(tab_slot_controller) {}
+
 TabGroupHeader::EditorBubbleTracker::~EditorBubbleTracker() {
   if (is_open_) {
     widget_->RemoveObserver(this);
     widget_->CloseWithReason(views::Widget::ClosedReason::kUnspecified);
+    OnWidgetDestroyed(widget_);
   }
   CHECK(!IsInObserverList());
 }
@@ -528,9 +534,11 @@ void TabGroupHeader::EditorBubbleTracker::Opened(views::Widget* bubble_widget) {
   widget_ = bubble_widget;
   is_open_ = true;
   bubble_widget->AddObserver(this);
+  tab_slot_controller_->NotifyTabGroupEditorBubbleOpened();
 }
 
 void TabGroupHeader::EditorBubbleTracker::OnWidgetDestroyed(
     views::Widget* widget) {
   is_open_ = false;
+  tab_slot_controller_->NotifyTabGroupEditorBubbleClosed();
 }
