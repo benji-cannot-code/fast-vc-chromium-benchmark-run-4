fce62683d32b78f89cbefe22868234eb43653078
Anastasia Helfinstein <anastasi@google.com>
2022-07-30 00:22:13 +0000

    [Chromevox] Clean up comments
    Quick CL to cleanup comments. This is a pure refactor.
    
    R=josiahk
    
    AX-Relnotes: n/a.
    Bug: none
    Change-Id: I54709d68bf19a2eb318c30aa54e4214840df7efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793430
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029999}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/range_automation_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/range_automation_handler.js
@@ -24,9 +24,7 @@ const EventType = chrome.automation.EventType;
 const RoleType = chrome.automation.RoleType;
 const StateType = chrome.automation.StateType;
 
-/**
- * @implements {ChromeVoxStateObserver}
- */
+/** @implements {ChromeVoxStateObserver} */
 export class RangeAutomationHandler extends BaseAutomationHandler {
   /** @private */
   constructor() {
@@ -114,9 +112,7 @@ export class RangeAutomationHandler extends BaseAutomationHandler {
     this.addListener_(EventType.SORT_CHANGED, this.onAriaAttributeChanged);
   }
 
-  /**
-   * @param {!ChromeVoxEvent} evt
-   */
+  /** @param {!ChromeVoxEvent} evt */
   onEventIfInRange(evt) {
     if (BaseAutomationHandler.disallowEventFromAction(evt)) {
       return;
@@ -162,9 +158,7 @@ export class RangeAutomationHandler extends BaseAutomationHandler {
     }
   }
 
-  /**
-   * @param {!ChromeVoxEvent} evt
-   */
+  /** @param {!ChromeVoxEvent} evt */
   onAriaAttributeChanged(evt) {
     // Don't report changes on editable nodes since they interfere with text
     // selection changes. Users can query via Search+k for the current state
