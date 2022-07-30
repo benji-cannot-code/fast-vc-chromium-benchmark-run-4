19fdbb78a494cc76909bbb67d2727c4230fb0404
Toni Barzic <tbarzic@chromium.org>
2022-07-30 01:00:46 +0000

    Remove back button from SearchBoxViewBase
    The back button is no longer used - neither for app list not keyboard
    shortcut viewer search box.
    
    Bug: None
    Change-Id: Iba8d7865119a3ef4e8e4a00cf01a84c30457646e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792627
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030007}

--- ash/shortcut_viewer/views/ksv_search_box_view.cc
+++ ash/shortcut_viewer/views/ksv_search_box_view.cc
@@ -77,9 +77,6 @@ void KSVSearchBoxView::OnKeyEvent(ui::KeyEvent* event) {
 void KSVSearchBoxView::OnThemeChanged() {
   ash::SearchBoxViewBase::OnThemeChanged();
 
-  back_button()->SetImage(
-      views::ImageButton::STATE_NORMAL,
-      gfx::CreateVectorIcon(ash::kKsvSearchBackIcon, GetBackButtonColor()));
   close_button()->SetImage(
       views::ImageButton::STATE_NORMAL,
       gfx::CreateVectorIcon(ash::kKsvSearchCloseIcon, GetCloseButtonColor()));
@@ -131,22 +128,6 @@ void KSVSearchBoxView::SetupCloseButton() {
   close->SetVisible(false);
 }
 
-void KSVSearchBoxView::SetupBackButton() {
-  views::ImageButton* back = back_button();
-  back->SetHasInkDropActionOnClick(true);
-  back->SetImage(
-      views::ImageButton::STATE_NORMAL,
-      gfx::CreateVectorIcon(ash::kKsvSearchBackIcon, GetBackButtonColor()));
-  back->SetPreferredSize(gfx::Size(kIconSize, kIconSize));
-  back->SetImageHorizontalAlignment(views::ImageButton::ALIGN_CENTER);
-  back->SetImageVerticalAlignment(views::ImageButton::ALIGN_MIDDLE);
-  const std::u16string back_button_label(
-      l10n_util::GetStringUTF16(IDS_KSV_BACK_ACCESSIBILITY_NAME));
-  back->SetAccessibleName(back_button_label);
-  back->SetTooltipText(back_button_label);
-  back->SetVisible(false);
-}
-
 void KSVSearchBoxView::UpdatePlaceholderTextStyle() {
   SetPlaceholderTextAttributes();
 }
@@ -169,10 +150,6 @@ SkColor KSVSearchBoxView::GetBackgroundColor() {
   return ShouldUseFocusedColors()
              ? gfx::kGoogleGrey100
              : ash::AppListColorProvider::Get()->GetSearchBoxBackgroundColor();
-}
-
-SkColor KSVSearchBoxView::GetBackButtonColor() {
-  return ShouldUseDarkThemeColors() ? gfx::kGoogleBlue300 : gfx::kGoogleBlue500;
 }
 
 SkColor KSVSearchBoxView::GetBorderColor() {
