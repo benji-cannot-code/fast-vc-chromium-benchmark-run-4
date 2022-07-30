1a4ebfc06298a81e78f2ac8a6ed142ab8a980550
dpapad <dpapad@chromium.org>
2022-07-30 08:41:16 +0000

    Settings: Remove a few unnecessary "not chromeos_ash" expressions.
    These are unnecessary in files that are exculded from chromeos_ash
    anyway.
    
    Bug: None
    Change-Id: I7ebf30b46fbbc9b7814694af04a4ed6405a440d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792240
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030037}

--- chrome/browser/resources/settings/languages_page/languages_page.ts
+++ chrome/browser/resources/settings/languages_page/languages_page.ts
@@ -25,9 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  import './languages.js';
  import '../controls/settings_toggle_button.js';
  import '../icons.html.js';
- // <if expr="not chromeos_ash">
  import '../relaunch_confirmation_dialog.js';
- // </if>
  import '../settings_shared.css.js';
  import '../settings_vars.css.js';
 
