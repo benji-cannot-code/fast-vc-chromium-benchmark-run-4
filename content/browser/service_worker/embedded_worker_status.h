cf261416a96367aa075cedc18e63f9c13e9d573f
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2022-08-01 02:21:58 +0000

    Add metrics to distinguish the service worker reuse.
    To understand how we can improve the performance by skipping the service
    worker starts, we at least need to know how many requests are actually
    starting the service workers.  This is the metrics to understand that.
    
    Bug: 1346542
    Change-Id: I2dea6e9dab8a8215b5906dab97dcd064c72aad90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783392
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030091}

--- content/browser/service_worker/embedded_worker_status.h
+++ content/browser/service_worker/embedded_worker_status.h
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class EmbeddedWorkerStatus {
-  STOPPED,
-  STARTING,
-  RUNNING,
-  STOPPING,
+  STOPPED = 0,
+  STARTING = 1,
+  RUNNING = 2,
+  STOPPING = 3,
+  kMaxValue = STOPPING,
 };
 
 }  // namespace content
--- content/browser/service_worker/service_worker_metrics.h
+++ content/browser/service_worker/service_worker_metrics.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/time/time.h"
+#include "content/browser/service_worker/embedded_worker_status.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/service_worker_context.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
@@ -151,6 +152,12 @@ class ServiceWorkerMetrics {
   // Records the result of trying to start an installed worker.
   static void RecordStartInstalledWorkerStatus(
       blink::ServiceWorkerStatusCode status,
+      EventType purpose);
+
+  // Records the running status of the worker to receive a task.
+  // Usually recorded for the fetch handler.
+  static void RecordRunAfterStartWorkerStatus(
+      EmbeddedWorkerStatus running_status,
       EventType purpose);
 
   // Records the time taken to successfully start a worker. |is_installed|
