c52df140f77517c8da8f5765c1411827c575b6c2
Nektarios Paisios <nektar@chromium.org>
2022-08-01 19:08:48 +0000

    Removes DumpWithoutCrashing in GetUnignoredParent because crashes do not occur for that reason any more
    This is a precursor to the patch that moves unignored parent computation to AXComputedNodeData.
    https://chromium-review.googlesource.com/c/chromium/src/+/3792133
    Looking in the internal crash database, we could not find crashes that were
    caused by the bug for which the DumpWithoutCrashing was originally put in.
    
    R=abigailbklein@google.com, aleventhal@chromium.org
    
    AX-Relnotes: n/a.
    Change-Id: If91fdf00cf7abcf5c431b7a60ae7aff97d538b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794345
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030216}

--- ui/accessibility/ax_node_data.h
+++ ui/accessibility/ax_node_data.h
@@ -50,7 +50,7 @@ struct AX_BASE_EXPORT AXNodeData {
 
   AXNodeData(const AXNodeData& other);
   AXNodeData(AXNodeData&& other);
-  AXNodeData& operator=(AXNodeData other);
+  AXNodeData& operator=(const AXNodeData& other);
 
   // Accessing accessibility attributes:
   //
