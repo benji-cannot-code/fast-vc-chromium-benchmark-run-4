1dfd5ca1a5414d2bece10878d752e846b1a115d9
Petro Akzhygitov <pakzhygitov@google.com>
2022-08-01 17:02:38 +0000

    Fixed failing tests in ExternalURLSigninTestCase.
    – Reverted logic form observing active web state to observe all web
    states.
    
    Bug: 1348638
    Change-Id: Ib7a831daaf3e2e418bb72152561360ad88986021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3796951
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030150}

--- ios/chrome/browser/ui/browser_view/tab_events_mediator.mm
+++ ios/chrome/browser/ui/browser_view/tab_events_mediator.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_coordinator.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
-#import "ios/chrome/browser/web_state_list/active_web_state_observation_forwarder.h"
+#import "ios/chrome/browser/web_state_list/all_web_state_observation_forwarder.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/web/public/ui/crw_web_view_proxy.h"
 #import "ios/web/public/web_state.h"
@@ -26,10 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Bridges C++ WebStateObserver methods to this mediator.
   std::unique_ptr<web::WebStateObserverBridge> _webStateObserverBridge;
 
-  // Forwards observer methods for active WebStates in the WebStateList to
+  // Forwards observer methods for all WebStates in the WebStateList to
   // this mediator.
-  std::unique_ptr<ActiveWebStateObservationForwarder>
-      _activeWebStateObservationForwarder;
+  std::unique_ptr<AllWebStateObservationForwarder>
+      _allWebStateObservationForwarder;
 
   WebStateList* _webStateList;
   __weak NewTabPageCoordinator* _ntpCoordinator;
@@ -45,15 +45,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     _webStateObserverBridge =
         std::make_unique<web::WebStateObserverBridge>(self);
-    _activeWebStateObservationForwarder =
-        std::make_unique<ActiveWebStateObservationForwarder>(
-            webStateList, _webStateObserverBridge.get());
+    _allWebStateObservationForwarder =
+        std::make_unique<AllWebStateObservationForwarder>(
+            _webStateList, _webStateObserverBridge.get());
   }
   return self;
 }
 
 - (void)disconnect {
-  _activeWebStateObservationForwarder = nullptr;
+  _allWebStateObservationForwarder = nullptr;
   _webStateObserverBridge = nullptr;
 
   _webStateList = nullptr;
