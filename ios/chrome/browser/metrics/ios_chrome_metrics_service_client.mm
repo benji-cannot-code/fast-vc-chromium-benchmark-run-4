4511bec4c46c405a43aba3a6c34a1ec73c7b96d0
Chris Lu <thegreenfrog@chromium.org>
2022-08-01 15:08:44 +0000

    [ios] Create IOSFeedEnabledMetricsProvider
    This metrics provider will log a new ContentSuggestions.Feed.CanBeShown
    metric. This will be used as a dashboard filter.
    
    Bug: 1347857
    Change-Id: I533a74a63f1519c5cef4ff1244afdeabbbf4aeda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782146
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030109}

--- ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
+++ ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/metrics/ios_chrome_default_browser_metrics_provider.h"
 #include "ios/chrome/browser/metrics/ios_chrome_signin_and_sync_status_metrics_provider.h"
 #include "ios/chrome/browser/metrics/ios_chrome_stability_metrics_provider.h"
+#import "ios/chrome/browser/metrics/ios_feed_enabled_metrics_provider.h"
 #include "ios/chrome/browser/metrics/ios_profile_session_metrics_provider.h"
 #include "ios/chrome/browser/metrics/mobile_session_shutdown_metrics_provider.h"
 #include "ios/chrome/browser/sync/device_info_sync_service_factory.h"
@@ -344,6 +345,13 @@ void IOSChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
 
   metrics_service_->RegisterMetricsProvider(
       CreateIOSProfileSessionMetricsProvider());
+
+  ChromeBrowserState* browser_state = GetApplicationContext()
+                                          ->GetChromeBrowserStateManager()
+                                          ->GetLastUsedBrowserState();
+  metrics_service_->RegisterMetricsProvider(
+      std::make_unique<IOSFeedEnabledMetricsProvider>(
+          browser_state->GetPrefs()));
 }
 
 void IOSChromeMetricsServiceClient::RegisterUKMProviders() {
