a9854fa598dbe6819c095c7a06d46597d773733e
Yutaka Hirano <yhirano@chromium.org>
2022-08-02 06:04:30 +0000

    Roll src/net/third_party/quiche/src/ dcc80b3a0..fe9c73029 (7 commits)
    https://quiche.googlesource.com/quiche.git/+log/dcc80b3a0bbb..fe9c73029c24
    
    $ git log dcc80b3a0..fe9c73029 --date=short --no-merges --format='%ad %ae %s'
    2022-08-01 vasilvv Support negative timeouts in QuicPollEventLoop.
    2022-08-01 vasilvv Add QuicDefaultClient, a QuicEventLoop-based version of QuicClient
    2022-08-01 quiche-dev Internal change
    2022-08-01 wub Pass path validation start time to QuicPathValidator::ResultDelegate::OnPathValidationSuccess.
    2022-08-01 abhisinghx Internal change
    2022-08-01 wub Do not fire QUIC_BUG 'quic_has_pending_frames_unexpectedly' if connection is disconnected.
    2022-08-01 wub Deprecate --gfe2_reloadable_flag_quic_tls_handshaker_check_connection_closed.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I51f5e39867c336f803452912a1e1e9a46efe207a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3800834
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030417}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -812,7 +812,8 @@ QuicChromiumClientSession::ConnectionMigrationValidationResultDelegate::
 
 void QuicChromiumClientSession::ConnectionMigrationValidationResultDelegate::
     OnPathValidationSuccess(
-        std::unique_ptr<quic::QuicPathValidationContext> context) {
+        std::unique_ptr<quic::QuicPathValidationContext> context,
+        quic::QuicTime start_time) {
   auto* chrome_context =
       static_cast<QuicChromiumPathValidationContext*>(context.get());
   session_->OnConnectionMigrationProbeSucceeded(
@@ -839,7 +840,8 @@ QuicChromiumClientSession::PortMigrationValidationResultDelegate::
 
 void QuicChromiumClientSession::PortMigrationValidationResultDelegate::
     OnPathValidationSuccess(
-        std::unique_ptr<quic::QuicPathValidationContext> context) {
+        std::unique_ptr<quic::QuicPathValidationContext> context,
+        quic::QuicTime start_time) {
   auto* chrome_context =
       static_cast<QuicChromiumPathValidationContext*>(context.get());
   session_->OnPortMigrationProbeSucceeded(
--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -465,7 +465,8 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
         QuicChromiumClientSession* session);
 
     void OnPathValidationSuccess(
-        std::unique_ptr<quic::QuicPathValidationContext> context) override;
+        std::unique_ptr<quic::QuicPathValidationContext> context,
+        quic::QuicTime start_time) override;
 
     void OnPathValidationFailure(
         std::unique_ptr<quic::QuicPathValidationContext> context) override;
@@ -484,7 +485,8 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
         QuicChromiumClientSession* session);
 
     void OnPathValidationSuccess(
-        std::unique_ptr<quic::QuicPathValidationContext> context) override;
+        std::unique_ptr<quic::QuicPathValidationContext> context,
+        quic::QuicTime start_time) override;
 
     void OnPathValidationFailure(
         std::unique_ptr<quic::QuicPathValidationContext> context) override;
