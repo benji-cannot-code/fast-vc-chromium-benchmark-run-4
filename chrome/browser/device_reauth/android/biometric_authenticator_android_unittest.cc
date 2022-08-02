3c4b6aaa5fba1cbcc6bad60b7ccfa57d2758710c
Christian Dullweber <dullweber@chromium.org>
2022-08-02 19:22:21 +0000

    [Android][IncognitoReauth] Refactor the logic of canAuthenticate method
    The current use of canAuthenticate returns true only if biometrics are
    enrolled. It doesn't take into account if screenlock might be enabled
    for authentication instead. This was done so to support the password
    manager's requirement.
    
    However, with new clients like Incognito reauth, the requirement for
    only relying on biometrics is not true in that case. Hence to support
    that, this CL:
    
    a) Renames the canAuthenticate method in BiometricAuthenticatorBridge
    to canAuthenticateWithBiometrics and adds a new method canAuthenticateWithBiometricsOrScreenlock to consider the general
    case stated above.
    
    b) Updates the BiometricAuthenticatorAndroid#canAuthenticate to check
    on the requester and invoke canAuthenticateWithBiometricsOrScreenlock
    for reauth page and canAuthenticateWithBiometric otherwise.
    
    Bug: 1227656
    Change-Id: I2b15461dd6c962650bb84ca8ae3bd33abb938611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791348
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030703}

--- chrome/browser/device_reauth/android/biometric_authenticator_android_unittest.cc
+++ chrome/browser/device_reauth/android/biometric_authenticator_android_unittest.cc
@@ -35,7 +35,11 @@ using testing::Return;
 
 class MockBiometricAuthenticatorBridge : public BiometricAuthenticatorBridge {
  public:
-  MOCK_METHOD(BiometricsAvailability, CanAuthenticate, (), (override));
+  MOCK_METHOD(BiometricsAvailability,
+              CanAuthenticateWithBiometric,
+              (),
+              (override));
+  MOCK_METHOD(bool, CanAuthenticateWithBiometricOrScreenLock, (), (override));
   MOCK_METHOD(void,
               Authenticate,
               (base::OnceCallback<void(device_reauth::BiometricAuthUIResult)>
@@ -76,7 +80,7 @@ class BiometricAuthenticatorAndroidTest : public testing::Test {
 TEST_F(BiometricAuthenticatorAndroidTest, CanAuthenticateCallsBridge) {
   base::HistogramTester histogram_tester;
 
-  EXPECT_CALL(bridge(), CanAuthenticate)
+  EXPECT_CALL(bridge(), CanAuthenticateWithBiometric)
       .WillOnce(Return(BiometricsAvailability::kAvailable));
   EXPECT_TRUE(authenticator()->CanAuthenticate(
       device_reauth::BiometricAuthRequester::kAllPasswordsList));
@@ -86,12 +90,13 @@ TEST_F(BiometricAuthenticatorAndroidTest, CanAuthenticateCallsBridge) {
       BiometricsAvailability::kAvailable, 1);
 }
 
-TEST_F(BiometricAuthenticatorAndroidTest,
-       CanAuthenticateDoesNotReecordHistogramForNonPasswordManager) {
+TEST_F(
+    BiometricAuthenticatorAndroidTest,
+    CanAuthenticateDoesNotReecordHistogramForNonPasswordManagerForIncognito) {
   base::HistogramTester histogram_tester;
 
-  EXPECT_CALL(bridge(), CanAuthenticate)
-      .WillOnce(Return(BiometricsAvailability::kAvailable));
+  EXPECT_CALL(bridge(), CanAuthenticateWithBiometricOrScreenLock)
+      .WillOnce(Return(true));
   EXPECT_TRUE(authenticator()->CanAuthenticate(
       device_reauth::BiometricAuthRequester::kIncognitoReauthPage));
 
