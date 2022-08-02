888eaea1fa74875425cbd7bbe60e6b86bceb4b64
Marijn Kruisselbrink <mek@chromium.org>
2022-08-02 05:13:46 +0000

    Fix StorageKey calculation in NavigationRequest::CommitNavigation.
    Both the AncestorChainBit and top-level site calculations were done
    incorrectly in NavigationRequest::CommitNavigation. Neither were
    taking into account the extension permission exceptions we added to
    support extensions embedding their own sites as first party content.
    
    Additionally the ancestor chain bit calculation was also incorrectly
    using the state of the frame before the navigation commits to figure
    out if it is same site or cross site, rather than the state as it
    would be after the navigation commits.
    
    This CL fixes both these issues by using the RFHI::CalculateStorageKey
    method to calculate the storage key.
    
    This issue would largely only effect filesystem: URL loading, since
    that is the only place where we get the storage key from the commit
    params directly. Every other access is done after RFHI recalculates
    the storage key after the navigation commits.
    
    This also adds filesystem API web tests to the storage partitioning virtual test suite to ensure this works (and keeps working) with that flag enabled.
    
    Bug: 1345402
    Change-Id: Ibad7643784fa33b1084cbd2af607c75a0c3cdc03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792845
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030404}

---
+++ third_party/blink/web_tests/wpt_internal/file-system-api/fetch_fs_url.window.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=resources/fs_helpers.js
+
+promise_test(async t => {
+    const fs = await getFileSystem(self.TEMPORARY);
+    const path = '/test-path.txt';
+    const contents = 'Hello World!';
+    await writeFile(fs, path, contents);
+    const entry = await getFileSystemFileEntry(fs, path);
+    const url = entry.toURL();
+
+    // Use XHR rather than fetch to load the url, since Chrome's fetch
+    // implementation doesn't support loading filesystem: URLs.
+    const response = await new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.onload = t.step_func(e => {
+            resolve(req.responseText);
+        });
+        req.onerror = reject;
+        req.onabort = reject;
+        req.open('GET', url);
+        req.send();
+    });
+    assert_equals(response, contents);
+}, 'Loading a filesystem: url works.');
