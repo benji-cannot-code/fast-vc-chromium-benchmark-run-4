bc45ee22642266e7369ff45598f73e704288638d
Thomas Guilbert <tguilbert@chromium.org>
2022-08-02 04:51:46 +0000

    Extract device listening logic
    This CL moves the wired headset listener, USB audio listener, and
    Bluetooth headset listener into one AudioDeviceListener class, which
    will be reused for devices pre and post S.
    
    The Bluetooth SCO state listener seems to be only relevant in Pre S
    devices, and will remain local to the AudioDeviceSelectorPreS.
    
    This CL also aggregates some state machine logic into the
    AudioDeviceSelector.Devices class. Notably, each listener is now only
    responsible for signaling the existence of their respective devices.
    A list of available devices is then computed based of the the existing
    devices, enforcing mutual exclusion between the availability of
    certain device types.
    
    Bug: 1317548
    Change-Id: I4f4d77c0e1389ef04a6d4b5c82d9494ce8f7f21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790249
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030399}

--- media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
+++ media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
@@ -166,6 +166,8 @@ class AudioManagerAndroid {
             return;
         }
 
+        // TODO(crbug.com/1317548): Should we exit early if we are already in/out of
+        // communication mode?
         if (on) {
             // Store microphone mute state and speakerphone state so it can
             // be restored when closing.
@@ -242,7 +244,7 @@ class AudioManagerAndroid {
             return false;
         }
 
-        return mAudioDeviceSelector.setDevice(deviceId);
+        return mAudioDeviceSelector.selectDevice(deviceId);
     }
 
     /**
