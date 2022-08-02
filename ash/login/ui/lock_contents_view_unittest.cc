888fcfe0c955d1a65ef03253682e248d1da61543
Andrew Xu <andrewxu@chromium.org>
2022-08-02 19:32:56 +0000

    [Test] Make `LoginTestBase` inherit `NoSessionAshTestBase`
    This CL makes `LoginTestBase` inherit `NoSessionAshTestBase`. If a
    test case needs to log in, it should call `set_start_session(true)`
    explicitly.
    
    Bug: 1347171
    Change-Id: Ie672a2553f97347c877a2e33f60d62ac237abd4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798859
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030708}

--- ash/login/ui/lock_contents_view_unittest.cc
+++ ash/login/ui/lock_contents_view_unittest.cc
@@ -123,8 +123,8 @@ AuthDisabledData GetTestDisabledAuthData() {
 using LockContentsViewKeyboardUnitTest = LoginKeyboardTestBase;
 
 class LockContentsViewUnitTest : public LoginTestBase {
- protected:
-  LockContentsViewUnitTest() = default;
+ public:
+  LockContentsViewUnitTest() { set_start_session(true); }
   LockContentsViewUnitTest(LockContentsViewUnitTest&) = delete;
   LockContentsViewUnitTest& operator=(LockContentsViewUnitTest&) = delete;
   ~LockContentsViewUnitTest() override = default;
@@ -3319,7 +3319,6 @@ class LockContentsViewWithKioskLicenseTest : public LoginTestBase {
   ~LockContentsViewWithKioskLicenseTest() override = default;
 
   void SetUp() override {
-    set_start_session(false);
     LoginTestBase::SetUp();
     login_shelf_view_ = GetPrimaryShelf()->shelf_widget()->GetLoginShelfView();
     // Set initial states.
--- ash/login/ui/lock_screen_media_controls_view_unittest.cc
+++ ash/login/ui/lock_screen_media_controls_view_unittest.cc
@@ -115,6 +115,8 @@ class LockScreenMediaControlsViewTest : public LoginTestBase {
   ~LockScreenMediaControlsViewTest() override = default;
 
   void SetUp() override {
+    set_start_session(true);
+
     // Enable media controls.
     feature_list.InitAndEnableFeature(features::kLockScreenMediaControls);
 
