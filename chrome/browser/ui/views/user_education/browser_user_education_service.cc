0537036617e5652472fcd65b42784552ce656f78
Dana Fried <dfried@chromium.org>
2022-08-02 16:41:42 +0000

    [User Education] Fix Tutorials histogram.
    Tutorials histograms cannot use the same prefix for different tutorials.
    This replaces a duplicate with a new, unique value.
    
    A follow-up CL will ensure that duplicates are not registered.
    
    Change-Id: I3cf94aea483d86050b98c25b6cbfa2979e58fdc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804085
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030607}

--- chrome/browser/ui/views/user_education/browser_user_education_service.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kTabGroupTutorialMetricPrefix[] = "TabGroup";
+const char kTabGroupWithGroupTutorialMetricPrefix[] = "TabGroupWithGroup";
 constexpr char kTabGroupHeaderElementName[] = "TabGroupHeader";
 
 class BrowserHelpBubbleDelegate : public user_education::HelpBubbleDelegate {
@@ -375,9 +376,9 @@ void MaybeRegisterChromeTutorials(
     tutorial_registry.AddTutorial(kTabGroupTutorialId,
                                   std::move(without_group_description));
 
-    with_group_description.histograms =
-        user_education::MakeTutorialHistograms<kTabGroupTutorialMetricPrefix>(
-            with_group_description.steps.size());
+    with_group_description.histograms = user_education::MakeTutorialHistograms<
+        kTabGroupWithGroupTutorialMetricPrefix>(
+        with_group_description.steps.size());
     tutorial_registry.AddTutorial(kTabGroupWithExistingGroupTutorialId,
                                   std::move(with_group_description));
   }
