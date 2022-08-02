6c316d18a42a7a8df15bebb6c701cb9ca3deb9f2
Brittany Hartmire <bhartmire@google.com>
2022-08-02 22:39:08 +0000

    [QuickStart] Make RandomSessionId inside TargetDeviceConnectionBroker
    Generate a RandomSessionId inside TargetDeviceConnectionBroker and pass
    it inside FastPairAdvertiser. FastPairAdvertiser then includes this id
    in the manufacturer data of its BLE advertisement.
    
    TEST=add TestGenerateManufacturerMetadata unittest inside FastPairAdvertiserTest. Manually tested that Fast Pair half sheet still appears as expected on Android device.
    
    Bug: b/234655072
    Change-Id: I4d4319f7c2a56760979c85ffecf16cadbc37aee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789656
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030789}

--- chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker_impl.h
+++ chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_OOBE_QUICK_START_CONNECTIVITY_TARGET_DEVICE_CONNECTION_BROKER_IMPL_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/unguessable_token.h"
 #include "chromeos/ash/components/oobe_quick_start/connectivity/target_device_connection_broker.h"
 
 class FastPairAdvertiser;
@@ -44,6 +45,7 @@ class TargetDeviceConnectionBrokerImpl : public TargetDeviceConnectionBroker {
   scoped_refptr<device::BluetoothAdapter> bluetooth_adapter_;
 
   std::unique_ptr<FastPairAdvertiser> fast_pair_advertiser_;
+  base::UnguessableToken random_session_id_;
 
   base::WeakPtrFactory<TargetDeviceConnectionBrokerImpl> weak_ptr_factory_{
       this};
