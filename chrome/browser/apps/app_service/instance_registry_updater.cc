6e8f56f0552a236a5117bd846b652c29d5f45117
Alexander Bolodurin <alexbn@chromium.org>
2022-08-02 06:58:46 +0000

    Simplify window visibility tracking for instance registry
    Instead of trying to start/stop tracking Aura windows based on app
    instance events, start tracking all lacros and ash browser windows when
    they are initialised, stop tracking when they are destroyed, and filter
    windows in window event handlers. App instances may get its windows set
    to null in the future (lacros tabs in transit between browsers), so this
    way of tracking is easier to reason about and depends less on app
    instance semantics.
    
    Bug: 1327389
    Change-Id: Ic1cef18d820515ca422d691f82c29121bf4106e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802254
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030428}

--- chrome/browser/apps/app_service/instance_registry_updater.cc
+++ chrome/browser/apps/app_service/instance_registry_updater.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/instance_registry_updater.h"
 
+#include "chrome/browser/ash/crosapi/browser_util.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
+#include "ui/aura/window.h"
 
 namespace apps {
 
@@ -25,6 +29,16 @@ static InstanceState GetState(bool visible, bool active) {
   return state;
 }
 
+bool IsAshBrowserWindow(aura::Window* aura_window) {
+  for (auto* browser : *BrowserList::GetInstance()) {
+    BrowserWindow* window = browser->window();
+    if (window && window->GetNativeWindow() == aura_window) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }  // namespace
 
 InstanceRegistryUpdater::InstanceRegistryUpdater(
@@ -41,7 +55,6 @@ InstanceRegistryUpdater::~InstanceRegistryUpdater() = default;
 void InstanceRegistryUpdater::OnBrowserWindowAdded(
     const BrowserWindowInstance& instance) {
   OnBrowserWindowUpdated(instance);
-  window_observations_.AddObservation(instance.window);
 }
 
 void InstanceRegistryUpdater::OnBrowserWindowUpdated(
@@ -55,15 +68,11 @@ void InstanceRegistryUpdater::OnBrowserWindowRemoved(
     const BrowserWindowInstance& instance) {
   OnInstance(instance.id, instance.GetAppId(), instance.window,
              InstanceState::kDestroyed);
-  window_observations_.RemoveObservation(instance.window);
 }
 
 void InstanceRegistryUpdater::OnBrowserAppAdded(
     const BrowserAppInstance& instance) {
   OnBrowserAppUpdated(instance);
-  if (instance.type == BrowserAppInstance::Type::kAppWindow) {
-    window_observations_.AddObservation(instance.window);
-  }
 }
 
 void InstanceRegistryUpdater::OnBrowserAppUpdated(
@@ -78,13 +87,18 @@ void InstanceRegistryUpdater::OnBrowserAppRemoved(
     const BrowserAppInstance& instance) {
   OnInstance(instance.id, instance.app_id, instance.window,
              InstanceState::kDestroyed);
-  if (instance.type == BrowserAppInstance::Type::kAppWindow) {
-    window_observations_.RemoveObservation(instance.window);
-  }
+}
+
+void InstanceRegistryUpdater::OnWindowInitialized(aura::Window* window) {
+  window_observations_.AddObservation(window);
 }
 
 void InstanceRegistryUpdater::OnWindowVisibilityChanged(aura::Window* window,
                                                         bool visible) {
+  if (!crosapi::browser_util::IsLacrosWindow(window) &&
+      !IsAshBrowserWindow(window)) {
+    return;
+  }
   for (const BrowserAppInstance* instance :
        browser_app_instance_registry_.SelectAppInstances(
            [window](const BrowserAppInstance& instance) {
@@ -99,6 +113,10 @@ void InstanceRegistryUpdater::OnWindowVisibilityChanged(aura::Window* window,
            })) {
     OnBrowserWindowUpdated(*instance);
   }
+}
+
+void InstanceRegistryUpdater::OnWindowDestroying(aura::Window* window) {
+  window_observations_.RemoveObservation(window);
 }
 
 void InstanceRegistryUpdater::OnInstance(
