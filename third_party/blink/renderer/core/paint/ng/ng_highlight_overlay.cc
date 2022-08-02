18e802704de7dfb5e666b36d00f71fb86fe8a761
Delan Azabani <dazabani@igalia.com>
2022-08-02 14:43:18 +0000

    HighlightOverlayPainting: fix buffer confusion and clamping bugs
    Custom highlights and marker-based highlights are defined in terms of
    DOM ranges in a Text node. Generated text either has no Text node or
    does not derive its content from the text node, so the active ranges
    of these highlights should be ignored, but we fail to do so for soft
    hyphens (a kind of generated text).
    
    On its own, this can cause spurious paints of things like highlight
    backgrounds near soft hyphens if [0,1) is highlighted.
    
    CL:3720688 also changes the logic of ComputeParts, requiring parts to
    be added to the result outside the main loop if originating fragment
    offsets extend past the first or last highlight edges. For example,
    with an originating fragment [a,b) and first highlight edge [c,d)
    where a<c<b<d, we add originating part [a,c) before the main loop.
    
    But if a<b<c<d, for example, the originating part should be [a,b), not
    [a,c), and this can cause the failures in bug 1346809 and bug 1346810.
    
    This patch fixes both bugs by ignoring marker-based highlight ranges
    for all generated text including soft hyphens (like we already do for
    ellipsis) and clamping parts to the originating fragment even when
    they are added outside the main loop.
    
    Fixed: 1346810, 1346809
    Change-Id: If3369bfb1a44ebce190db8fa8477065eeba00d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793072
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Delan Azabani <dazabani@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1030541}

--- third_party/blink/renderer/core/paint/ng/ng_highlight_overlay.cc
+++ third_party/blink/renderer/core/paint/ng/ng_highlight_overlay.cc
@@ -343,10 +343,11 @@ Vector<HighlightPart> NGHighlightOverlay::ComputeParts(
     return result;
   }
   if (originating.from < edges.front().offset) {
-    result.push_back(HighlightPart{originating_layer,
-                                   originating.from,
-                                   edges.front().offset,
-                                   {originating_layer}});
+    result.push_back(
+        HighlightPart{originating_layer,
+                      originating.from,
+                      ClampOffset(edges.front().offset, originating),
+                      {originating_layer}});
   }
   for (const HighlightEdge& edge : edges) {
     // If there is actually some text between the previous and current edges...
@@ -377,10 +378,11 @@ Vector<HighlightPart> NGHighlightOverlay::ComputeParts(
     prev_offset.emplace(edge.offset);
   }
   if (edges.back().offset < originating.to) {
-    result.push_back(HighlightPart{originating_layer,
-                                   edges.back().offset,
-                                   originating.to,
-                                   {originating_layer}});
+    result.push_back(
+        HighlightPart{originating_layer,
+                      ClampOffset(edges.back().offset, originating),
+                      originating.to,
+                      {originating_layer}});
   }
   return result;
 }
