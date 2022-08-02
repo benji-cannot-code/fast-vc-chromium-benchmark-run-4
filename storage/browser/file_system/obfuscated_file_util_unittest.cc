75d7ce53341bbf0f5c176e2ae512d4bf064cc2b5
kyraseevers <kyraseevers@chromium.org>
2022-08-02 22:09:48 +0000

    Replacing type_string with absl::optional<FileSystemType>
    Instead of using string comparison, this CL instead directly pipes in the FileSystemType and converts to quota storage type when determining if a bucket or url is temporary. This is not only a more accurate comparison but also makes it clear to callers that the type may not be provided rather than relying on callers providing the empty string to denote this.
    
    As this CL calls GetTypeString() directly, it also removes the references to CallGetTypeStringForURL() to reduce confusion and fragility.
    
    Bug: 1344157
    Change-Id: If89b45b037a18ee43d76a56a22615d7c5aa0f3cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788214
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030772}

--- storage/browser/file_system/obfuscated_file_util_unittest.cc
+++ storage/browser/file_system/obfuscated_file_util_unittest.cc
@@ -860,11 +860,10 @@ class ObfuscatedFileUtilTest : public testing::Test,
     ASSERT_TRUE(db != nullptr);
 
     // Destroy it.
-    (is_non_default_bucket())
-        ? ofu()->DestroyDirectoryDatabaseForBucket(url.bucket().value(),
-                                                   GetTypeString(url.type()))
-        : ofu()->DestroyDirectoryDatabaseForStorageKey(
-              url.storage_key(), GetTypeString(url.type()));
+    (is_non_default_bucket()) ? ofu()->DestroyDirectoryDatabaseForBucket(
+                                    url.bucket().value(), url.type())
+                              : ofu()->DestroyDirectoryDatabaseForStorageKey(
+                                    url.storage_key(), url.type());
     ASSERT_TRUE(ofu()->directories_.empty());
   }
 
@@ -1864,9 +1863,8 @@ TEST_P(ObfuscatedFileUtilTest, TestInconsistency) {
 
   // Destroy database to make inconsistency between database and filesystem.
   (is_non_default_bucket())
-      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type_string())
-      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(),
-                                                     type_string());
+      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type())
+      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(), type());
 
   // Try to get file info of broken file.
   EXPECT_FALSE(PathExists(kPath1));
@@ -1887,9 +1885,8 @@ TEST_P(ObfuscatedFileUtilTest, TestInconsistency) {
 
   // Destroy again.
   (is_non_default_bucket())
-      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type_string())
-      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(),
-                                                     type_string());
+      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type())
+      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(), type());
 
   // Repair broken `kPath1`.
   context = NewContext(nullptr);
@@ -1908,9 +1905,8 @@ TEST_P(ObfuscatedFileUtilTest, TestInconsistency) {
                                   true /* copy */));
 
   (is_non_default_bucket())
-      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type_string())
-      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(),
-                                                     type_string());
+      ? ofu()->DestroyDirectoryDatabaseForBucket(custom_bucket_, type())
+      : ofu()->DestroyDirectoryDatabaseForStorageKey(storage_key(), type());
   context = NewContext(nullptr);
   created = false;
   EXPECT_EQ(base::File::FILE_OK,
@@ -2583,7 +2579,7 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForBucketAndType) {
 
   // Delete a directory for default_bucket_'s persistent filesystem.
   ASSERT_TRUE(ofu()->DeleteDirectoryForBucketAndType(
-      default_bucket_, GetTypeString(kFileSystemTypePersistent)));
+      default_bucket_, kFileSystemTypePersistent));
 
   // The directory for default_bucket_'s temporary filesystem should not be
   // removed.
@@ -2616,7 +2612,7 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForBucketAndType) {
 
   // Deleting directories which don't exist is not an error.
   ASSERT_TRUE(ofu()->DeleteDirectoryForBucketAndType(
-      alternate_custom_bucket_, GetTypeString(kFileSystemTypePersistent)));
+      alternate_custom_bucket_, kFileSystemTypePersistent));
 }
 
 TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForStorageKeyAndType) {
@@ -2665,7 +2661,7 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForStorageKeyAndType) {
 
   // Delete a directory for storage_key1's persistent filesystem.
   ASSERT_TRUE(ofu()->DeleteDirectoryForStorageKeyAndType(
-      storage_key1, GetTypeString(kFileSystemTypePersistent)));
+      storage_key1, kFileSystemTypePersistent));
 
   // The directory for storage_key1's temporary filesystem should not be
   // removed.
@@ -2711,9 +2707,9 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForStorageKeyAndType) {
 
   // Deleting directories which don't exist is not an error.
   ASSERT_TRUE(ofu()->DeleteDirectoryForStorageKeyAndType(
-      storage_key3, GetTypeString(kFileSystemTypeTemporary)));
+      storage_key3, kFileSystemTypeTemporary));
   ASSERT_TRUE(ofu()->DeleteDirectoryForStorageKeyAndType(
-      storage_key3, GetTypeString(kFileSystemTypePersistent)));
+      storage_key3, kFileSystemTypePersistent));
 }
 
 TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForBucketAndType_DeleteAll) {
@@ -2757,7 +2753,7 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForBucketAndType_DeleteAll) {
                    .is_error());
 
   // Delete all directories for default_bucket_.
-  ofu()->DeleteDirectoryForBucketAndType(default_bucket_, std::string());
+  ofu()->DeleteDirectoryForBucketAndType(default_bucket_, absl::nullopt);
 
   // The directories for default_bucket_ should be removed.
   ASSERT_EQ(ofu()
@@ -2829,7 +2825,7 @@ TEST_P(ObfuscatedFileUtilTest, DeleteDirectoryForStorageKeyAndType_DeleteAll) {
                    .is_error());
 
   // Delete all directories for storage_key1.
-  ofu()->DeleteDirectoryForStorageKeyAndType(storage_key1, std::string());
+  ofu()->DeleteDirectoryForStorageKeyAndType(storage_key1, absl::nullopt);
 
   // The directories for storage_key1 should be removed.
   ASSERT_EQ(ofu()
