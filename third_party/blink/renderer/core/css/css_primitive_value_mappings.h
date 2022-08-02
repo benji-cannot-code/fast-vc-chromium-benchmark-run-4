498a7952f6f43dd5962b80c0ca07fdc3b5d7638c
Rune Lillesveen <futhark@chromium.org>
2022-08-02 22:22:55 +0000

    [@container] style no longer a container-type
    All elements are style containers per spec change. Adjust implementation
    and spec to reflect that.
    
    Bug: 1302630
    Change-Id: I0a834daa1f7622ec62d1df31b268de8b3f2074c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802732
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030776}

--- third_party/blink/renderer/core/css/css_primitive_value_mappings.h
+++ third_party/blink/renderer/core/css/css_primitive_value_mappings.h
@@ -1651,8 +1651,6 @@ inline EContainerType CSSIdentifierValue::ConvertTo() const {
       return kContainerTypeInlineSize;
     case CSSValueID::kSize:
       return kContainerTypeSize;
-    case CSSValueID::kStyle:
-      return kContainerTypeStyle;
     default:
       break;
   }
