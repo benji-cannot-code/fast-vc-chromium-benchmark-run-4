4c523deec070c639054874bb71290b116bd6d7dc
Stephen McGruer <smcgruer@chromium.org>
2022-08-02 20:39:51 +0000

    [basic-card] Make PaymentRequestPayerDetailChangeTest not depend on basic card
    This test was missed in the earlier test migrations on Android. Fixing
    it is straight-forward; instead of opening a basic-card payment method
    up, we use two URL methods (to avoid skip-the-sheet) and slightly change
    the interaction with the sheet.
    
    Bug: 1209835
    Change-Id: I25b9b9fe2f4dfb21ae36058f5b7506df262a77b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805042
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030736}

--- components/test/data/payments/onpayerdetailchange.js
+++ components/test/data/payments/onpayerdetailchange.js
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var gPaymentResponse = null;
 var gRetryPromise = null;
 
+const bobPayMethod = Object.freeze({
+  supportedMethods: 'https://bobpay.com',
+});
+
 /**
  * Launches the PaymentRequest UI
  */
@@ -17,7 +21,7 @@ function buy() { // eslint-disable-line no-unused-vars
     requestPayerName: true,
     requestPayerPhone: true,
   };
-  getPaymentResponse(options)
+  getPaymentResponseWithMethod(options, [bobPayMethod])
       .then(function(response) {
         gPaymentResponse = response;
         var eventPromise = new Promise(function(resolve) {
