9617ce2db3a72bd36cd369ca6c85789d0fa129e0
Karol Sygiet <sygiet@google.com>
2022-08-02 17:26:02 +0000

    Add function that prevents BiometricAuthenticatorAndroid from being deleted.
    Extended existing method to ensure that object lives for the next
    60seconds since the last valid authentication. This behaviour will be
    used in the followup CL to fix memory leak issue.Added tests for
    ChromeBiometricAuthenticatorCommon methods.
    
    Bug: 1342024
    Change-Id: I2a8c5a8e784efb26f0d9d4bf3ac9e30cf468f53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790996
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030633}

--- chrome/browser/device_reauth/chrome_biometric_authenticator_common.cc
+++ chrome/browser/device_reauth/chrome_biometric_authenticator_common.cc
@@ -5,13 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/device_reauth/chrome_biometric_authenticator_common.h"
 
+#include "base/bind.h"
+#include "base/callback_helpers.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
+#include "components/password_manager/core/browser/password_access_authenticator.h"
 
 namespace {
-// For how long after the last successful authentication a user is considered
-// authenticated without repeating the challenge and how long authenticator
-// object is being preserved from being deleted.
-constexpr base::TimeDelta kAuthValidityPeriod = base::Seconds(60);
+
+using password_manager::PasswordAccessAuthenticator;
 
 }  // namespace
 
@@ -24,12 +27,26 @@ bool ChromeBiometricAuthenticatorCommon::RecordAuthenticationResult(
     bool success) {
   if (success) {
     last_good_auth_timestamp_ = base::TimeTicks::Now();
+
+    // Holds scoped_refptr for kAuthValidityPeriod seconds, preventing object
+    // from being deleted.
+    base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(
+            [](scoped_refptr<ChromeBiometricAuthenticatorCommon> ptr) {},
+            base::WrapRefCounted(this)),
+        PasswordAccessAuthenticator::kAuthValidityPeriod);
   }
   return success;
 }
 
-bool ChromeBiometricAuthenticatorCommon::NeedsToAuthenticate() {
+bool ChromeBiometricAuthenticatorCommon::NeedsToAuthenticate() const {
   return !last_good_auth_timestamp_.has_value() ||
          base::TimeTicks::Now() - last_good_auth_timestamp_.value() >=
-             kAuthValidityPeriod;
+             PasswordAccessAuthenticator::kAuthValidityPeriod;
+}
+
+base::WeakPtr<ChromeBiometricAuthenticatorCommon>
+ChromeBiometricAuthenticatorCommon::GetWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
 }
--- chrome/browser/device_reauth/chrome_biometric_authenticator_common.h
+++ chrome/browser/device_reauth/chrome_biometric_authenticator_common.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DEVICE_REAUTH_CHROME_BIOMETRIC_AUTHENTICATOR_COMMON_H_
 #define CHROME_BROWSER_DEVICE_REAUTH_CHROME_BIOMETRIC_AUTHENTICATOR_COMMON_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "components/device_reauth/biometric_authenticator.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -17,11 +18,14 @@ class ChromeBiometricAuthenticatorCommon
  public:
   ChromeBiometricAuthenticatorCommon();
 
+  // Returns a weak pointer to this authenticator.
+  base::WeakPtr<ChromeBiometricAuthenticatorCommon> GetWeakPtr();
+
  protected:
   ~ChromeBiometricAuthenticatorCommon() override;
 
   // Checks whether user needs to reauthenticate.
-  bool NeedsToAuthenticate();
+  bool NeedsToAuthenticate() const;
 
   // Records authentication time if authentication was successful.
   bool RecordAuthenticationResult(bool success);
@@ -29,6 +33,10 @@ class ChromeBiometricAuthenticatorCommon
  private:
   // Time of last successful re-auth. nullopt if there hasn't been an auth yet.
   absl::optional<base::TimeTicks> last_good_auth_timestamp_;
+
+  // Factory for weak pointers to this class.
+  base::WeakPtrFactory<ChromeBiometricAuthenticatorCommon> weak_ptr_factory_{
+      this};
 };
 
 #endif  // CHROME_BROWSER_DEVICE_REAUTH_CHROME_BIOMETRIC_AUTHENTICATOR_COMMON_H_
