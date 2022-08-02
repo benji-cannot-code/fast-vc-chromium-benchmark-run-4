daa56ca34bf52b386df3e106fb01469ffb8db34d
Josh Simmons <jds@google.com>
2022-08-02 23:51:39 +0000

    Remove the private context_ copy from ContextualSearchDelegate.
    This tightly coupled the GatherAndSaveSurroundingText and
    StartSearchTermResolutionRequest calls. Since the former is unneeded by
    partial translate, that made using the search term resolution call
    difficult.
    
    Bug: 1340303
    Change-Id: I9f95384d3d41579799948de0b7c1036349dfeea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794911
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Josh Simmons <jds@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030818}

--- chrome/browser/android/contextualsearch/contextual_search_manager.cc
+++ chrome/browser/android/contextualsearch/contextual_search_manager.cc
@@ -44,13 +44,7 @@ ContextualSearchManager::ContextualSearchManager(JNIEnv* env,
   Profile* profile = ProfileManager::GetActiveUserProfile();
   delegate_ = std::make_unique<ContextualSearchDelegate>(
       profile->GetURLLoaderFactory(),
-      TemplateURLServiceFactory::GetForProfile(profile),
-      base::BindRepeating(
-          &ContextualSearchManager::OnSearchTermResolutionResponse,
-          base::Unretained(this)),
-      base::BindRepeating(
-          &ContextualSearchManager::OnTextSurroundingSelectionAvailable,
-          base::Unretained(this)));
+      TemplateURLServiceFactory::GetForProfile(profile));
 }
 
 ContextualSearchManager::~ContextualSearchManager() {
@@ -75,8 +69,11 @@ void ContextualSearchManager::StartSearchTermResolutionRequest(
       NativeContextualSearchContext::FromJavaContextualSearchContext(
           j_contextual_search_context);
   // Calls back to OnSearchTermResolutionResponse.
-  delegate_->StartSearchTermResolutionRequest(contextual_search_context,
-                                              base_web_contents);
+  delegate_->StartSearchTermResolutionRequest(
+      contextual_search_context, base_web_contents,
+      base::BindRepeating(
+          &ContextualSearchManager::OnSearchTermResolutionResponse,
+          base::Unretained(this)));
 }
 
 void ContextualSearchManager::GatherSurroundingText(
@@ -90,8 +87,11 @@ void ContextualSearchManager::GatherSurroundingText(
   base::WeakPtr<NativeContextualSearchContext> contextual_search_context =
       NativeContextualSearchContext::FromJavaContextualSearchContext(
           j_contextual_search_context);
-  delegate_->GatherAndSaveSurroundingText(contextual_search_context,
-                                          base_web_contents);
+  delegate_->GatherAndSaveSurroundingText(
+      contextual_search_context, base_web_contents,
+      base::BindRepeating(
+          &ContextualSearchManager::OnTextSurroundingSelectionAvailable,
+          base::Unretained(this)));
 }
 
 void ContextualSearchManager::OnSearchTermResolutionResponse(
