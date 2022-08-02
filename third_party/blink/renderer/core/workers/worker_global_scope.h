ecc8caf95d0b2fa46c52fa1365892186c7a3df6e
Dave Tapuska <dtapuska@chromium.org>
2022-08-02 17:12:43 +0000

    [MBI] Migrate CanvasResourceDispatcher to use per-ASG CompositorTaskRunner
    This is a reland of crrev.com/7db085bb but with additional checks
    around SharedWorker's compositor task runner being null. OffscreenCanvas usage from a SharedWorker shouldn't really be
    allowed and hopefully jbroman's binding changes for serialization
    crrev.com/c/3758405 will help address this in the future. A null
    check of the CompositorWorker is fine here because there is no
    guarantee that a SharedWorker would end up in the same render
    process as canvas element.
    
    Before this CL, CanvasResourceDispatcher uses per-thread
    CompositorTaskRunner.
    
    After this CL, CanvasResourceDispatcher will use per-ASG (Agent
    Scheduling Group) CompositorTaskRunner.
    
    CanvasResourceDispatcher is used by OffscreenCanvas. OffscreenCanvas
    provides a functionality to draw the Canvas from Worker
    thread. Previously, CanvasResourceDispatcher was accessing the main
    thread’s objects from the worker thread by using
    `Thread::MainThread()->Scheduler()->CompositorTaskRunner()`. But this
    time, we want to use the per-AgentSchedulingGroup compositor task
    runner instead. To plumb per-ASG compositor task runner from main
    thread to worker thread, this CL added
    agent_group_scheduler_compositor_task_runner to WorkerGlobalScope.
    
    Bug: 1105403
    Change-Id: If4a352cd374c5555f94497c09977ee11105ded2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787685
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030622}

--- third_party/blink/renderer/core/workers/worker_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_global_scope.h
@@ -139,6 +139,11 @@ class CORE_EXPORT WorkerGlobalScope
   void AddConsoleMessageImpl(ConsoleMessage*, bool discard_duplicates) final;
   const BrowserInterfaceBrokerProxy& GetBrowserInterfaceBroker() const final;
 
+  scoped_refptr<base::SingleThreadTaskRunner>
+  GetAgentGroupSchedulerCompositorTaskRunner() final {
+    return agent_group_scheduler_compositor_task_runner_;
+  }
+
   OffscreenFontSelector* GetFontSelector() { return font_selector_; }
 
   CoreProbeSink* GetProbeSink() final;
@@ -303,6 +308,11 @@ class CORE_EXPORT WorkerGlobalScope
   mutable Member<TrustedTypePolicyFactory> trusted_types_;
 
   WorkerThread* thread_;
+
+  // The compositor task runner associated with the |AgentGroupScheduler| this
+  // worker belongs to.
+  scoped_refptr<base::SingleThreadTaskRunner>
+      agent_group_scheduler_compositor_task_runner_;
 
   bool closing_ = false;
 
