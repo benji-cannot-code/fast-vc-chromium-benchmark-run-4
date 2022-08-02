ee27a41038bf47840bca7229b1fdef8a40b32112
Rayan Kanso <rayankans@google.com>
2022-08-02 09:29:58 +0000

    [BrowserFragment] Use ViewModel for restoring fragment state
    This will support screen rotations the fragment. The Browser and the Delegate are stored in the ViewModel, as is the SurfaceView to avoid re-attaching the cross-process view multiple times.
    
    Change-Id: If0c9ceed5fa15878f3eca5de25d7f12d0d1039a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791043
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Susanne Westphal <swestphal@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030448}

--- weblayer/public/java/org/chromium/weblayer/BrowserFragmentDelegate.java
+++ weblayer/public/java/org/chromium/weblayer/BrowserFragmentDelegate.java
@@ -107,11 +107,7 @@ class BrowserFragmentDelegate extends IBrowserFragmentDelegate.Stub {
 
     @Override
     public void onDestroy() {
-        mHandler.post(() -> {
-            mSurfaceControlViewHost.release();
-            mFragment.onDestroy();
-            mSurfaceControlViewHost = null;
-        });
+        mHandler.post(() -> mFragment.onDestroy());
     }
 
     @Override
@@ -143,6 +139,11 @@ class BrowserFragmentDelegate extends IBrowserFragmentDelegate.Stub {
     @Override
     public void onPause() {
         mHandler.post(() -> mFragment.onPause());
+    }
+
+    @Override
+    public void onCleared() {
+        mHandler.post(() -> mSurfaceControlViewHost.release());
     }
 
     @Override
