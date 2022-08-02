7e238130dc72eb39ced2c5dab7a2770f0dff24a7
Katie Dektar <katie@chromium.org>
2022-08-02 16:09:21 +0000

    [A11y Services] Remove obsolete code to remove aria-hidden from Google Docs.
    Google Docs renders with Canvas and annotates the Canvas for
    specific extensions using the gdocs_script. We no longer need to
    remove aria-hidden from Google Docs.
    
    Also fixes AccessibilityLiveSiteTest to test this. Note that
    this test does not run on CQ but can be built and run manually
    in the test suite accessibility_live_site_tests.
    
    Test: Manual with Select-to-Speak,ChromeVox. AccessibilityLiveSiteTest.
    Bug: None
    Change-Id: Iefaea5946ad6fba1b6729c32f1257175df9962c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3795800
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030583}

--- chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
+++ chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var tries = 10;
-
 function triggerDocsCanvasAnnotationMode() {
   const scriptContents = `
     window['_docs_annotate_canvas_by_ext'] = "${chrome.runtime.id}";
@@ -14,27 +12,8 @@ function triggerDocsCanvasAnnotationMode() {
   document.documentElement.appendChild(script);
 }
 
-function RemoveAriaHiddenFromGoogleDocsContent() {
-  var element = document.querySelector('.kix-zoomdocumentplugin-outer');
-  if (element) {
-    element.setAttribute('aria-hidden', 'false');
-  } else {
-    tries--;
-    if (tries > 0) {
-      window.setTimeout(RemoveAriaHiddenFromGoogleDocsContent, 1000);
-    }
-  }
-}
-
-// Docs will soon only render its contents in canvas. As a stop gap measure,
-// trigger Docs' canvas annotation rendering mode. This needs to run within the
-// page's context, so install a script to be executed.
+// Docs renders content in Canvas without annotations by default. This script is
+// used to trigger annotated Canvas which allows the a11y tree to be built for
+// the document contents. This needs to run within the page's context, so
+// install a script to be executed.
 triggerDocsCanvasAnnotationMode();
-
-// Google Docs isn't compatible with non-screen reader accessibility services by
-// default because in order to provide screen reader support, most of the
-// rendered document has aria-hidden set on it, which has the side effect of
-// hiding it from non-screen reader accessibility features too. Fix it by
-// changing aria-hidden to false. Try multiple times in case the page isn't
-// fully loaded when the content script runs.
-RemoveAriaHiddenFromGoogleDocsContent();
