a71f88af169f1911ab9bc907c6dfa10477a23da9
Trevor Perrier <perrier@chromium.org>
2022-08-02 00:18:23 +0000

    [ULP] Add NeverLanguages not in ULP histograms
    This CL adds two histograms to record the never translate languages
    that are not in a users ULP languages.
    
    Bug: 1347106
    Change-Id: I49c72671a420d077a2e36e14612c90b0d9fad9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793205
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030333}

--- components/language/core/browser/ulp_metrics_logger_unittest.cc
+++ components/language/core/browser/ulp_metrics_logger_unittest.cc
@@ -5,11 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/language/core/browser/ulp_metrics_logger.h"
 
+#include "base/metrics/metrics_hashes.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace language {
+
+using ::testing::ElementsAre;
+using ::testing::IsEmpty;
 
 TEST(ULPMetricsLoggerTest, TestLanguageCount) {
   ULPMetricsLogger logger;
@@ -62,6 +66,29 @@ TEST(ULPMetricsLoggerTest, TestAcceptLanguagesULPOverlap) {
 
   histogram.ExpectUniqueSample(kInitiationAcceptLanguagesULPOverlapHistogram,
                                21, 1);
+}
+
+TEST(ULPMetricsLoggerTest, TestNeverLanguagesMissingFromULP) {
+  ULPMetricsLogger logger;
+  base::HistogramTester histogram;
+
+  std::vector<std::string> never_languages_not_in_ulp = {"en-US", "de"};
+  logger.RecordInitiationNeverLanguagesMissingFromULP(
+      never_languages_not_in_ulp);
+
+  histogram.ExpectBucketCount(kInitiationNeverLanguagesMissingFromULP,
+                              base::HashMetricName("en-US"), 1);
+  histogram.ExpectBucketCount(kInitiationNeverLanguagesMissingFromULP,
+                              base::HashMetricName("de"), 1);
+}
+
+TEST(ULPMetricsLoggerTest, TestNeverLanguagesMissingFromULPCount) {
+  ULPMetricsLogger logger;
+  base::HistogramTester histogram;
+
+  logger.RecordInitiationNeverLanguagesMissingFromULPCount(3);
+  histogram.ExpectUniqueSample(kInitiationNeverLanguagesMissingFromULPCount, 3,
+                               1);
 }
 
 TEST(ULPMetricsLoggerTest, TestDetermineLanguageStatus) {
@@ -123,6 +150,23 @@ TEST(ULPMetricsLoggerTest, TestULPLanguagesInAcceptLanguagesRatio) {
   EXPECT_EQ(100,
             logger.ULPLanguagesInAcceptLanguagesRatio(
                 {"en-US", "af", "pt-BR", "es", "de", "fr-CA"}, ulp_languages));
+}
+
+TEST(ULPMetricsLoggerTest, TestRemoveULPLanguages) {
+  ULPMetricsLogger logger;
+  std::vector<std::string> ulp_languages = {"en-US", "es", "pt-BR", "de"};
+
+  EXPECT_THAT(
+      logger.RemoveULPLanguages({"af", "en", "am", "as"}, ulp_languages),
+      ElementsAre("af", "am", "as"));
+
+  EXPECT_THAT(logger.RemoveULPLanguages(
+                  {"en-GB", "af", "en-AU", "am", "pt", "as"}, ulp_languages),
+              ElementsAre("af", "am", "as"));
+
+  EXPECT_THAT(
+      logger.RemoveULPLanguages({"en", "pt-BR", "es-MX"}, ulp_languages),
+      IsEmpty());
 }
 
 }  // namespace language
