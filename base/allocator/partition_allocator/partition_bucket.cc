c7200ece447a3614a387a40907770e0a4cffeb02
Kalvin Lee <kdlee@chromium.org>
2022-08-02 04:30:38 +0000

    [PA] Unqualify `ScopedUnlockGuard`
    With namespace migration mostly complete, there's no need to qualify or
    alias this type at most sites inside PartitionAlloc.
    
    Bug: 1288247
    Change-Id: Ib34436b25730802f9b0cfb9166f16a00d3803646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792990
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030394}

--- base/allocator/partition_allocator/partition_bucket.cc
+++ base/allocator/partition_allocator/partition_bucket.cc
@@ -177,8 +177,6 @@ SlotSpanMetadata<thread_safe>* PartitionDirectMap(
     unsigned int flags,
     size_t raw_size,
     size_t slot_span_alignment) {
-  using ::partition_alloc::internal::ScopedUnlockGuard;
-
   PA_DCHECK((slot_span_alignment >= PartitionPageSize()) &&
             base::bits::IsPowerOfTwo(slot_span_alignment));
 
@@ -688,7 +686,7 @@ PA_ALWAYS_INLINE uintptr_t PartitionBucket<thread_safe>::AllocNewSuperPage(
       return 0;
 
     // Didn't manage to get a new uncommitted super page -> address space issue.
-    ::partition_alloc::internal::ScopedUnlockGuard unlock{root->lock_};
+    ScopedUnlockGuard unlock{root->lock_};
     PartitionOutOfMemoryMappingFailure(root, kSuperPageSize);
   }
 
@@ -1331,7 +1329,7 @@ uintptr_t PartitionBucket<thread_safe>::SlowPathAlloc(
     if (flags & AllocFlags::kReturnNull)
       return 0;
     // See comment in PartitionDirectMap() for unlocking.
-    ::partition_alloc::internal::ScopedUnlockGuard unlock{root->lock_};
+    ScopedUnlockGuard unlock{root->lock_};
     root->OutOfMemory(raw_size);
     PA_IMMEDIATE_CRASH();  // Not required, kept as documentation.
   }
