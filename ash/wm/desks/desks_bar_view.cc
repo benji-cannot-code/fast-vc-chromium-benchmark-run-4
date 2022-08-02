7f569220c1d53d70efa4121e0fe9fc40db1d77d8
Hongyu Long <hongyulong@chromium.org>
2022-08-02 19:53:15 +0000

    saved_desks: Unfocus desk name when clicking Library button
    This CL fixed the issue that when we stay in templates grid and
    make the desk name view of one mini view active, it's still in
    focus when we click the Library button again.
    After the fix, when we re-click the Library button in templates
    grid, we can unfocus the desk name view if it was active
    previously.
    
    Fixed: 1344994
    Test: Unit test & Manual
    Change-Id: Ibfdaafafe6186484c55ef295e0ab2c59565cb7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783329
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030720}

--- ash/wm/desks/desks_bar_view.cc
+++ ash/wm/desks/desks_bar_view.cc
@@ -1277,6 +1277,8 @@ int DesksBarView::GetAdjustedUncroppedScrollPosition(int position) const {
 
 void DesksBarView::OnDesksTemplatesButtonPressed() {
   RecordLoadSavedDeskLibraryHistogram();
+  if (IsDeskNameBeingModified())
+    DeskNameView::CommitChanges(GetWidget());
   overview_grid_->overview_session()->ShowDesksTemplatesGrids(
       IsZeroState(), base::GUID(), /*saved_desk_name=*/u"",
       GetWidget()->GetNativeWindow()->GetRootWindow());
