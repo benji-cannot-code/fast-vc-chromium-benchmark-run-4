5ee08649cb082a60055bd69a10f6dc79bc9044ce
Andreu Botella <abotella@igalia.com>
2022-08-02 05:33:37 +0000

    Revert "Fix positioning of the line box rect when the line has hanging spaces"
    This reverts commit be88478c057c1bd36b4098fb11dac6f5f74db729.
    
    Reason for revert: This change was causing the memory and paint performance regressions reported in bugs 1347396 and 1347629.
    
    Original change's description:
    > Fix positioning of the line box rect when the line has hanging spaces
    >
    > It used to be that the rect for a line box covered the entirety of the
    > non-hanging text, and didn't cover the hanging width. Since carets are
    > clamped to the line box rect, however, this behavior was changed in
    > https://crrev.com/c/3387789 and it was made so that in RTL text, in
    > some cases the line box's left edge was placed at the left edge of the
    > hanging width. This resulted in the rect not covering the start of the
    > line, which again caused issues with carets.
    >
    > This change makes it so the offset where the line is placed is always
    > offset by the hang width for RTL lines, so that the text's left edge
    > falls on the left edge of the line box rect. But to avoid regressing
    > https://crbug.com/1278559, it also changes
    > |ComputeLocalCaretRectAtTextOffset| to have it compute an enlarged
    > line box rect which does include the hanging width, and it uses that
    > to position the caret.
    >
    > Bug: 1330093
    > Change-Id: I349674428a8206ddc25fe13b7bccf5ad0e609df5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695393
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027904}
    
    Bug: 1330093, 1347396, 1347629
    Change-Id: I34d680f4679e4ea5b2b001163b34872711585ee7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794469
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Andreu Botella <abotella@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030410}

--- third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h
@@ -50,9 +50,7 @@ class CORE_EXPORT NGPhysicalLineBoxFragment final : public NGPhysicalFragment {
   bool HasPropagatedDescendants() const { return has_propagated_descendants_; }
 
   // True if there is any hanging white-space or similar.
-  bool HasHanging() const { return hanging_inline_size_ > 0; }
-
-  LayoutUnit HangingInlineSize() const { return hanging_inline_size_; }
+  bool HasHanging() const { return has_hanging_; }
 
   const FontHeight& Metrics() const { return metrics_; }
 
@@ -92,7 +90,6 @@ class CORE_EXPORT NGPhysicalLineBoxFragment final : public NGPhysicalFragment {
 
  private:
   FontHeight metrics_;
-  LayoutUnit hanging_inline_size_;
 };
 
 template <>
