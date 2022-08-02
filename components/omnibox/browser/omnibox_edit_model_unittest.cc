c8405390605789d668b2e72b802db9f5041bce12
Moe Ahmadi <mahmadi@chromium.org>
2022-08-02 23:51:55 +0000

    [omnibox] Fixes crash caused by looking up visibility state for group ID
    This crash is caused when the OmniboxEditModel tries to look up the
    visibility state for a group ID that does not have any value for the
    server provided group ID. Since the group ID is present on the match
    without a server provided group ID, this is probably caused by the
    Journeys dedicated row. This Cl changes the implementation of
    AutocompleteResult::IsSuggestionGroupHidden() to check whether an
    original group ID is present or returns false otherwise.
    
    Bug: 1348030
    Change-Id: If6beb4b8b2b61cd3de5287541bfe00bd1993bd3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3799200
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030819}

--- components/omnibox/browser/omnibox_edit_model_unittest.cc
+++ components/omnibox/browser/omnibox_edit_model_unittest.cc
@@ -750,7 +750,8 @@ TEST_F(OmniboxEditModelPopupTest, PopupStepSelection) {
 
   SuggestionGroupsMap suggestion_groups_map;
   suggestion_groups_map[kNewGroupId].header = u"header";
-  suggestion_groups_map[kNewGroupId].original_group_id = 12345;
+  // Do not set the original_group_id on purpose to test that default visibility
+  // can be safely queried via AutocompleteResult::IsSuggestionGroupHidden().
   result->MergeSuggestionGroupsMap(suggestion_groups_map);
 
   AutocompleteInput input(u"match", metrics::OmniboxEventProto::NTP,
@@ -837,6 +838,8 @@ TEST_F(OmniboxEditModelPopupTest, PopupStepSelectionWithHiddenGroupIds) {
   SuggestionGroupsMap suggestion_groups_map;
   suggestion_groups_map[kNewGroupId].header = u"header";
   suggestion_groups_map[kNewGroupId].original_group_id = 12345;
+  // Setting the original_group_id allows the default visibility to be set via
+  // AutocompleteResult::SetSuggestionGroupHidden().
   result->MergeSuggestionGroupsMap(suggestion_groups_map);
   result->SetSuggestionGroupHidden(pref_service(), kNewGroupId,
                                    /*hidden=*/true);
@@ -910,7 +913,8 @@ TEST_F(OmniboxEditModelPopupTest, PopupInlineAutocompleteAndTemporaryText) {
 
   SuggestionGroupsMap suggestion_groups_map;
   suggestion_groups_map[kNewGroupId].header = u"header";
-  suggestion_groups_map[kNewGroupId].original_group_id = 12345;
+  // Do not set the original_group_id on purpose to test that default visibility
+  // can be safely queried via AutocompleteResult::IsSuggestionGroupHidden().
   result->MergeSuggestionGroupsMap(suggestion_groups_map);
 
   AutocompleteInput input(u"a", metrics::OmniboxEventProto::NTP,
