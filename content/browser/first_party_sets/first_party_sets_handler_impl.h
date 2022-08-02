0cc0b527f6793fc14ebffa33360962502356da81
Chris Fredrickson <cfredric@chromium.org>
2022-08-02 23:14:05 +0000

    Move normalization logic to FirstPartySetHandlerImpl.
    This logic is already tested (indirectly) by unittests for
    FirstPartySetsHandlerImpl, so we can remove the direct unittests of
    the function.
    
    Change-Id: I0e630a8ea57058d8729db0924e40cc30ef3089db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806262
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030805}

--- content/browser/first_party_sets/first_party_sets_handler_impl.h
+++ content/browser/first_party_sets/first_party_sets_handler_impl.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
-#include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/values.h"
 #include "content/browser/first_party_sets/first_party_set_parser.h"
--- content/browser/first_party_sets/first_party_sets_loader.h
+++ content/browser/first_party_sets/first_party_sets_loader.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
-#include "base/containers/flat_set.h"
 #include "base/files/file.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
@@ -30,8 +29,7 @@ class CONTENT_EXPORT FirstPartySetsLoader {
   using LoadCompleteOnceCallback = base::OnceCallback<void(
       base::flat_map<net::SchemefulSite, net::SchemefulSite>)>;
   using FlattenedSets = base::flat_map<net::SchemefulSite, net::SchemefulSite>;
-  using SingleSet =
-      std::pair<net::SchemefulSite, base::flat_set<net::SchemefulSite>>;
+  using SingleSet = FirstPartySetParser::SingleSet;
 
   explicit FirstPartySetsLoader(LoadCompleteOnceCallback on_load_complete);
 
@@ -55,19 +53,6 @@ class CONTENT_EXPORT FirstPartySetsLoader {
 
   // Close the file on thread pool that allows blocking.
   void DisposeFile(base::File sets_file);
-
-  // Handles addition sets which overlap by intersecting with the same existing
-  // set, known as a transitive-overlap.
-  //
-  // This uses a Union-Find algorithm to select the earliest-provided addition
-  // set as the representative of all other addition sets that
-  // transitively-overlap with it.
-  //
-  // The "earliest-provided" tie-breaker is determined using a set's index in
-  // `addition_sets`.
-  static std::vector<SingleSet> NormalizeAdditionSets(
-      const FlattenedSets& existing_sets,
-      const std::vector<SingleSet>& addition_sets);
 
  private:
   // Parses the contents of `raw_sets` as a collection of First-Party Set
