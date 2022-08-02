21fe90a1e6c4422a56871154ffec3113cf2a0c0e
Luciano Pacheco <lucmult@chromium.org>
2022-08-02 04:19:19 +0000

    Files F2: Refactor reducers to a folder
    No change in behavior.
    
    Bug: b:228112522
    Change-Id: Ia6c02f1435210e0fb6fead2f72fcc4030c937567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3801693
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030392}

---
+++ ui/file_manager/file_manager/state/reducers/root.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {State} from '../../externs/ts/state.js';
+import {Action, Actions, ChangeDirectoryAction} from '../actions.js';
+
+import {cacheEntries} from './all_entries.js';
+import {changeDirectory} from './current_directory.js';
+
+/**
+ * Root reducer for Files app.
+ * It dispatches to other reducers to update different parts of the State.
+ */
+export function rootReducer(currentState: State, action: Action): State {
+  // Before any actual Reducer, we cache the entries, so the reducers can just
+  // use any entry from `allEntries`.
+  const state = cacheEntries(currentState, action);
+
+  switch (action.type) {
+    case Actions.CHANGE_DIRECTORY:
+      return Object.assign(state, {
+        currentDirectory:
+            changeDirectory(state, action as ChangeDirectoryAction),
+      });
+
+    default:
+      console.error(`invalid action: ${action.type}`);
+      return state;
+  }
+}
