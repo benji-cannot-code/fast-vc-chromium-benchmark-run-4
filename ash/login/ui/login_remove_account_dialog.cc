a776704cc2eebe27bc14f17a74c3d2a8f2cb84be
Martin Bidlingmaier <mbid@google.com>
2022-08-02 17:36:36 +0000

    [CrOS] Propagate theme change in account on login screen
    Specifically, this is about the disclaimer label in the account removal dialog.
    
    Fixed: 1338768
    Change-Id: I403386046291eb0ccff81a3897d822818a45530e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794212
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030639}

--- ash/login/ui/login_remove_account_dialog.cc
+++ ash/login/ui/login_remove_account_dialog.cc
@@ -255,6 +255,12 @@ void LoginRemoveAccountDialog::OnThemeChanged() {
               AshColorProvider::ContentLayerType::kTextColorPrimary));
     }
   }
+
+  if (management_disclosure_label_) {
+    management_disclosure_label_->SetEnabledColor(
+        AshColorProvider::Get()->GetContentLayerColor(
+            AshColorProvider::ContentLayerType::kTextColorPrimary));
+  }
 }
 
 void LoginRemoveAccountDialog::RequestFocus() {
