432c8b8b2191f9e0a8108826f989ef3f718726f5
cammie <cammie@chromium.org>
2022-08-02 17:12:50 +0000

    Shared Storage: Delete each origin's data 30 days from creation date.
    This CL changes the origin expiration design so that every origin
    expires 30 days from its creation date and has all of it data
    purged. (The previous behavior was only to purge data from origins
    that had not had any reads or writes within the last 60 days.)
    
    We also add an API on the backend database to get the most recent
    creation time for an origin, `GetCreationTime()`.
    
    Bug: 1218540,1317487
    Change-Id: I4391f39a44d3963b2925d12dba863db275f1b695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781906
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030623}

--- components/services/storage/shared_storage/shared_storage_manager.cc
+++ components/services/storage/shared_storage/shared_storage_manager.cc
@@ -212,10 +212,10 @@ void SharedStorageManager::PurgeMatchingOrigins(
 }
 
 void SharedStorageManager::FetchOrigins(
-    base::OnceCallback<void(std::vector<mojom::StorageUsageInfoPtr>)>
-        callback) {
+    base::OnceCallback<void(std::vector<mojom::StorageUsageInfoPtr>)> callback,
+    bool exclude_empty_origins) {
   DCHECK(database_);
-  database_->FetchOrigins(std::move(callback));
+  database_->FetchOrigins(std::move(callback), exclude_empty_origins);
 }
 
 void SharedStorageManager::MakeBudgetWithdrawal(
@@ -247,21 +247,39 @@ void SharedStorageManager::GetRemainingBudget(
                                 std::move(new_callback));
 }
 
+void SharedStorageManager::GetCreationTime(
+    url::Origin context_origin,
+    base::OnceCallback<void(TimeResult)> callback) {
+  DCHECK(callback);
+  DCHECK(database_);
+  auto new_callback = base::BindOnce(
+      [](base::WeakPtr<SharedStorageManager> manager,
+         base::OnceCallback<void(TimeResult)> callback, TimeResult result) {
+        if (manager)
+          manager->OnOperationResult(result.result);
+        std::move(callback).Run(std::move(result));
+      },
+      weak_ptr_factory_.GetWeakPtr(), std::move(callback));
+
+  database_->GetCreationTime(std::move(context_origin),
+                             std::move(new_callback));
+}
+
 void SharedStorageManager::SetOnDBDestroyedCallbackForTesting(
     base::OnceCallback<void(bool)> callback) {
   DCHECK(callback);
   on_db_destroyed_callback_for_testing_ = std::move(callback);
 }
 
-void SharedStorageManager::OverrideLastUsedTimeForTesting(
+void SharedStorageManager::OverrideCreationTimeForTesting(
     url::Origin context_origin,
-    base::Time new_last_used_time,
+    base::Time new_creation_time,
     base::OnceCallback<void(bool)> callback) {
   DCHECK(callback);
   DCHECK(database_);
   static_cast<AsyncSharedStorageDatabaseImpl*>(database_.get())
-      ->OverrideLastUsedTimeForTesting(  // IN-TEST
-          std::move(context_origin), new_last_used_time, std::move(callback));
+      ->OverrideCreationTimeForTesting(  // IN-TEST
+          std::move(context_origin), new_creation_time, std::move(callback));
 }
 
 void SharedStorageManager::OverrideSpecialStoragePolicyForTesting(
@@ -384,7 +402,6 @@ void SharedStorageManager::PurgeStaleOrigins() {
   DCHECK(database_);
 
   database_->PurgeStaleOrigins(
-      options_->origin_staleness_threshold,
       base::BindOnce(&SharedStorageManager::OnStaleOriginsPurged,
                      weak_ptr_factory_.GetWeakPtr()));
 }
