f68f7b035c142f635095f604ef50b392d3f5cfcd
Joseph Koshy <jkoshy@chromium.org>
2022-08-02 15:30:02 +0000

    content: Remove the TopLevelBlameContext class.
    This change list is part of an effort to remove
    TRACE_EVENT_PHASE_(ENTER|LEAVE)_CONTEXT trace
    events and associated instrumentation.
    
    BUG=1349313
    
    Change-Id: Icd44300b62fde87bd08d6d2466d684a64f069c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805320
    Commit-Queue: Joseph Koshy <jkoshy@chromium.org>
    Auto-Submit: Joseph Koshy <jkoshy@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030566}

--- content/renderer/renderer_blink_platform_impl.h
+++ content/renderer/renderer_blink_platform_impl.h
@@ -17,11 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/thread_annotations.h"
+#include "base/threading/thread_checker.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "content/child/blink_platform_impl.h"
 #include "content/common/content_export.h"
-#include "content/renderer/top_level_blame_context.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/shared_remote.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
@@ -176,7 +176,6 @@ class CONTENT_EXPORT RendererBlinkPlatformImpl : public BlinkPlatformImpl {
   void SetThreadType(base::PlatformThreadId thread_id,
                      base::ThreadType) override;
 #endif
-  blink::BlameContext* GetTopLevelBlameContext() override;
   std::unique_ptr<blink::WebDedicatedWorkerHostFactoryClient>
   CreateDedicatedWorkerHostFactoryClient(
       blink::WebDedicatedWorker*,
@@ -254,8 +253,6 @@ class CONTENT_EXPORT RendererBlinkPlatformImpl : public BlinkPlatformImpl {
 
   // NOT OWNED
   blink::scheduler::WebThreadScheduler* main_thread_scheduler_;
-
-  TopLevelBlameContext top_level_blame_context_;
 
   // Event that signals `io_thread_id_` is set and ready to be read.
   mutable base::WaitableEvent io_thread_id_ready_event_;
