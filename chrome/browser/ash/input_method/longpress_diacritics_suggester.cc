072ccf5930487a340b9929d558b6fd706712ddb6
jhtin <jhtin@chromium.org>
2022-08-03 02:19:17 +0000

    Add ability to accept diacritics using number keys.
    The diacritics suggestions are mapped using 1 as the first suggestion.
    If you type the valid number of a suggestion, then it will be accepted.
    If not, it will not be handled.
    
    Bug: b/217560706
    Change-Id: I3bee88b6c515ef2ccc642f204a0cf1b70bfc3cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792991
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030858}

--- chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
+++ chrome/browser/ash/input_method/longpress_diacritics_suggester.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/input_method/ui/assistive_delegate.h"
@@ -134,6 +135,21 @@ SuggestionStatus LongpressDiacriticsSuggester::HandleKeyEvent(
       highlighted_index_ = new_index;
       return SuggestionStatus::kBrowsing;
     default:
+      size_t key_number = 0;
+      // If the key value is a number then accept the corresponding suggestion.
+      if (base::StringToSizeT(std::u16string(1, event.GetCharacter()),
+                              &key_number)) {
+        // Ignore 0 values, make sure the key numbers are valid.
+        if (1 <= key_number && key_number <= 9 &&
+            key_number <= GetCurrentShownDiacritics().size()) {
+          // The "key" char value starts from 1.
+          // The actual index of the suggestions start at 0.
+          size_t index_to_accept = key_number - 1;
+          if (AcceptSuggestion(index_to_accept)) {
+            return SuggestionStatus::kAccept;
+          }
+        }
+      }
       return SuggestionStatus::kNotHandled;
   }
 }
