ee72c0f43cc6ff163ac6e6d6723dde670058c638
Ari Chivukula <arichiv@chromium.org>
2022-08-03 20:46:26 +0000

    [CodeHealth] Remove DictionaryValue::GetString(u16) from components/*
    Bug: 1187036
    Change-Id: I3f9d2f1750c3d35424540294d22eb1e7688c410f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804904
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031191}

--- components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
+++ components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
@@ -1560,25 +1561,25 @@ void RemoteSuggestionsProviderImpl::RestoreCategoriesFromPrefs() {
   const base::Value::List& list =
       pref_service_->GetValueList(prefs::kRemoteSuggestionCategories);
   for (const base::Value& entry : list) {
-    const base::DictionaryValue* dict = nullptr;
-    if (!entry.GetAsDictionary(&dict)) {
+    if (!entry.is_dict()) {
       DLOG(WARNING) << "Invalid category pref value: " << entry;
       continue;
     }
-    absl::optional<int> id = dict->FindIntKey(kCategoryContentId);
+    const base::Value::Dict& dict = entry.GetDict();
+    absl::optional<int> id = dict.FindInt(kCategoryContentId);
     if (!id) {
       DLOG(WARNING) << "Invalid category pref value, missing '"
                     << kCategoryContentId << "': " << entry;
       continue;
     }
-    std::u16string title;
-    if (!dict->GetString(kCategoryContentTitle, &title)) {
+    const std::string* title = dict.FindString(kCategoryContentTitle);
+    if (!title) {
       DLOG(WARNING) << "Invalid category pref value, missing '"
                     << kCategoryContentTitle << "': " << entry;
       continue;
     }
     absl::optional<bool> included_in_last_server_response =
-        dict->FindBoolKey(kCategoryContentProvidedByServer);
+        dict.FindBool(kCategoryContentProvidedByServer);
     if (!included_in_last_server_response) {
       DLOG(WARNING) << "Invalid category pref value, missing '"
                     << kCategoryContentProvidedByServer << "': " << entry;
@@ -1586,7 +1587,7 @@ void RemoteSuggestionsProviderImpl::RestoreCategoriesFromPrefs() {
     }
     // This wasn't always around, so it's okay if it's missing.
     bool allow_fetching_more_results =
-        dict->FindBoolKey(kCategoryContentAllowFetchingMore).value_or(false);
+        dict.FindBool(kCategoryContentAllowFetchingMore).value_or(false);
 
     Category category = Category::FromIDValue(*id);
     // The ranker may not persist the order of remote categories.
@@ -1600,7 +1601,8 @@ void RemoteSuggestionsProviderImpl::RestoreCategoriesFromPrefs() {
     CategoryInfo info =
         category == articles_category_
             ? BuildArticleCategoryInfo(absl::nullopt)
-            : BuildRemoteCategoryInfo(title, allow_fetching_more_results);
+            : BuildRemoteCategoryInfo(base::UTF8ToUTF16(*title),
+                                      allow_fetching_more_results);
     CategoryContent* content = UpdateCategoryInfo(category, info);
     content->included_in_last_server_response =
         included_in_last_server_response.value();
