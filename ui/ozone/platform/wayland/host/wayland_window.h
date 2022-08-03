5e0e013e7afa266937571502d7a0aacfd8652d68
Alexander Dunaev <adunaev@igalia.com>
2022-08-03 19:47:15 +0000

    [linux/wayland] Removed some unused code.
    The WaylandWindow has remnants of the code that was supposed to work
    with the window shape region, but now they are no longer used.
    
    This CL removes them.
    
    Bug: 1340457
    Change-Id: Idf4eddb18ada28b40d220de5f58bac176ef3b0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804874
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1031161}

--- ui/ozone/platform/wayland/host/wayland_window.h
+++ ui/ozone/platform/wayland/host/wayland_window.h
@@ -263,8 +263,6 @@ class WaylandWindow : public PlatformWindow,
   virtual void OnDragLeave();
   virtual void OnDragSessionClose(ui::mojom::DragOperation operation);
 
-  virtual absl::optional<std::vector<gfx::Rect>> GetWindowShape() const;
-
   // Tells if the surface has already been configured.
   virtual bool IsSurfaceConfigured() = 0;
 
