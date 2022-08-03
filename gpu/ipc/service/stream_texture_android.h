248648e5e4d5942a68cec9dd1240b3580cbbeb10
Joseph Koshy <jkoshy@chromium.org>
2022-08-03 13:59:57 +0000

    Explicitly include the header defining the THREAD_CHECKER symbol.
    This change list does not change program behavior.
    
    BUG=1349313
    
    Change-Id: I4ea0268a12bd1eabf7962a47f0fb699033c6099e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805331
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Joseph Koshy <jkoshy@chromium.org>
    Auto-Submit: Joseph Koshy <jkoshy@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031029}

--- gpu/ipc/service/stream_texture_android.h
+++ gpu/ipc/service/stream_texture_android.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/thread_checker.h"
 #include "base/unguessable_token.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/stream_texture_shared_image_interface.h"
