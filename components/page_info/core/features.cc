4e3ea022f4acf14507e30ca4b697367dcc7c61dc
Olesia Marukhno <olesiamarukhno@google.com>
2022-08-03 14:08:37 +0000

    [site data] Add PageSpecificSiteDataDialog feature
    Bug: 1344787
    Change-Id: Ia449dbbee7ff5291c7d5f5e05eb8f75bcf9d236b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807424
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031032}

--- components/page_info/core/features.cc
+++ components/page_info/core/features.cc
@@ -53,6 +53,10 @@ const base::Feature kPageInfoHideSiteSettings{
 
 const base::Feature kPageInfoCookiesSubpage{"PageInfoCookiesSubpage",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kPageSpecificSiteDataDialog{
+    "PageSpecificSiteDataDialog", base::FEATURE_DISABLED_BY_DEFAULT};
+
 #endif
 
 }  // namespace page_info
