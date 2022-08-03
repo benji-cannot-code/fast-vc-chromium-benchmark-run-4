0d4076e2a6718bafa7013d6995ba4a5d67e9c43b
Anders Hartvoll Ruud <andruud@chromium.org>
2022-08-03 10:12:14 +0000

    Avoid base::DictionaryValue::SetDouble in /tools
    Bug: 1187027
    Change-Id: I1afb0bcc26cfbaf074f41e97b8ca15cdb9e2739c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793787
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030973}

--- tools/json_schema_compiler/test/crossref_unittest.cc
+++ tools/json_schema_compiler/test/crossref_unittest.cc
@@ -20,7 +20,7 @@ namespace {
 
 base::DictionaryValue CreateTestTypeValue() {
   base::DictionaryValue value;
-  value.SetDouble("number", 1.1);
+  value.GetDict().Set("number", 1.1);
   value.SetInteger("integer", 4);
   value.SetString("string", "bling");
   value.SetBoolean("boolean", true);
