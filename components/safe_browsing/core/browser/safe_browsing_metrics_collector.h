3ffc32e5bcf9874ed85f5e1ec9ed72aa055d6c79
Roland Bock <rbock@google.com>
2022-08-03 06:23:59 +0000

    Refactor safe_browsing to use PrefService::GetValue*
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1342019
    Test: Existing unit tests
    Change-Id: I4d8691acc87d403b9dfb3cc7d447aee378daa530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798346
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030913}

--- components/safe_browsing/core/browser/safe_browsing_metrics_collector.h
+++ components/safe_browsing/core/browser/safe_browsing_metrics_collector.h
@@ -10,16 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "base/values.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/safe_browsing/core/browser/db/hit_report.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefService;
-
-namespace base {
-class Value;
-}  // namespace base
 
 namespace safe_browsing {
 
@@ -169,7 +166,7 @@ class SafeBrowsingMetricsCollector : public KeyedService {
   absl::optional<SafeBrowsingMetricsCollector::Event>
   GetLatestEventFromEventTypeFilter(UserState user_state,
                                     EventTypeFilter event_type_filter);
-  const base::Value* GetSafeBrowsingEventDictionary(UserState user_state);
+  const base::Value::Dict* GetSafeBrowsingEventDictionary(UserState user_state);
   int GetEventCountSince(UserState user_state,
                          EventType event_type,
                          base::Time since_time);
