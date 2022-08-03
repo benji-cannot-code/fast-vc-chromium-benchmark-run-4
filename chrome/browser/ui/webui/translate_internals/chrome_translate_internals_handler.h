903bcf965bf22a02fefc0a3f1e3840ca0362f775
Claudio DeSouza <cdesouza@igalia.com>
2022-08-03 09:39:47 +0000

    [Codehealth] Simplifying JS runtime calls
    This CL further provides base::ValueView for JS runtime calls, which
    will allow the removal of a singificant number of extra copies from
    `base::Value::Dict`, and `base::Value::List`, to `base::Value`. The
    changes introduced provide support so all handlers may be able to use
    `base::ValueView` when forwarding arguments to runtime calls.
    
    Bug: 1187001
    Change-Id: I9569b87bc7d4b4bb7315393b85dd83434c020e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783858
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1030959}

--- chrome/browser/ui/webui/translate_internals/chrome_translate_internals_handler.h
+++ chrome/browser/ui/webui/translate_internals/chrome_translate_internals_handler.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_TRANSLATE_INTERNALS_CHROME_TRANSLATE_INTERNALS_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_TRANSLATE_INTERNALS_CHROME_TRANSLATE_INTERNALS_HANDLER_H_
 
-#include <string>
-
 #include "base/callback_list.h"
 #include "components/translate/translate_internals/translate_internals_handler.h"
 #include "content/public/browser/web_ui_message_handler.h"
@@ -29,9 +27,9 @@ class ChromeTranslateInternalsHandler
   // translate::TranslateInternalsHandler.
   translate::TranslateClient* GetTranslateClient() override;
   variations::VariationsService* GetVariationsService() override;
-  void RegisterMessageCallback(const std::string& message,
+  void RegisterMessageCallback(base::StringPiece message,
                                MessageCallback callback) override;
-  void CallJavascriptFunction(const std::string& function_name,
+  void CallJavascriptFunction(base::StringPiece function_name,
                               base::span<const base::ValueView> args) override;
 
   // content::WebUIMessageHandler methods:
