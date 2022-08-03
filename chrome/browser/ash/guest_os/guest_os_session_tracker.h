546ac9a5f737e824daf4a6b9b4fde3fea168fec9
David Munro <davidmunro@google.com>
2022-08-03 12:40:25 +0000

    bruschetta: File sharing working now
    Wire up the vsock port reported by garcon, so Bruschetta shares in the
    files app can mount.
    
    Bug: b:217469540
    Test: CQ, deploy and view files
    Change-Id: Id6bdffc82c5ec3284564a0bf677e77c6e8df9a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802949
    Auto-Submit: David Munro <davidmunro@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031018}

--- chrome/browser/ash/guest_os/guest_os_session_tracker.h
+++ chrome/browser/ash/guest_os/guest_os_session_tracker.h
@@ -23,7 +23,8 @@ struct GuestInfo {
             int64_t cid,
             std::string username,
             base::FilePath homedir,
-            std::string ipv4_address);
+            std::string ipv4_address,
+            uint32_t sftp_vsock_port);
   ~GuestInfo();
   GuestInfo(GuestInfo&&);
   GuestInfo(const GuestInfo&);
@@ -34,6 +35,7 @@ struct GuestInfo {
   std::string username;
   base::FilePath homedir;
   std::string ipv4_address;
+  uint32_t sftp_vsock_port;
 };
 
 class GuestOsSessionTracker : protected ash::ConciergeClient::VmObserver,
