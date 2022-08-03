0c20d1bb9db679aada55cf326eac6985ee85b917
Gavin Williams <gavinwill@chromium.org>
2022-08-03 20:50:16 +0000

    Shimless: Enable WiFi on select network page
    This CL copies the OOBE behavior of turning on the WiFi adapter on the
    network selection page if it's already disabled.
    
    Bug: 1198187
    Test: ash_webui_unittests
    Change-Id: Ie0e6fb0c04cec19074f45bf250ab3fc7c5c052df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3800488
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031194}

--- ash/webui/shimless_rma/backend/shimless_rma_service.cc
+++ ash/webui/shimless_rma/backend/shimless_rma_service.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/rmad/rmad_client.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
+#include "chromeos/ash/components/network/network_type_pattern.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/services/network_config/in_process_instance.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
@@ -205,6 +206,16 @@ void ShimlessRmaService::BeginFinalization(BeginFinalizationCallback callback) {
       rmad::WelcomeState::RMAD_CHOICE_FINALIZE_REPAIR);
 
   if (!HaveAllowedNetworkConnection()) {
+    // Enable WiFi on the device.
+    chromeos::NetworkStateHandler* network_state_handler =
+        chromeos::NetworkHandler::Get()->network_state_handler();
+    if (!network_state_handler->IsTechnologyEnabled(
+            chromeos::NetworkTypePattern::WiFi())) {
+      network_state_handler->SetTechnologyEnabled(
+          chromeos::NetworkTypePattern::WiFi(), /*enabled=*/true,
+          network_handler::ErrorCallback());
+    }
+
     user_has_seen_network_page_ = true;
     mojo_state_ = mojom::State::kConfigureNetwork;
     std::move(callback).Run(
