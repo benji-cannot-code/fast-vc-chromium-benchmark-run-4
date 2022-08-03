02d76678f2fc3b2fc420f1adbff963ca6996f42c
ritikagup <ritikagup@google.com>
2022-08-03 13:10:27 +0000

    Mocking ShoppingService for testing Segmentation usage
    Bug: 1346711
    Change-Id: Ifb7a186c658ce8258616cf7428ec1b41ca419c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780029
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031021}

---
+++ components/commerce/core/mock_shopping_service.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/commerce/core/mock_shopping_service.h"
+
+namespace commerce {
+
+MockShoppingService::MockShoppingService()
+    : commerce::ShoppingService(nullptr, nullptr, nullptr, nullptr, nullptr) {}
+
+MockShoppingService::~MockShoppingService() = default;
+
+void MockShoppingService::GetProductInfoForUrl(
+    const GURL& url,
+    commerce::ProductInfoCallback callback) {
+  std::move(callback).Run(url, product_info_);
+}
+
+void MockShoppingService::SetResponseForGetProductInfoForUrl(
+    absl::optional<commerce::ProductInfo> product_info) {
+  product_info_ = product_info;
+}
+
+void MockShoppingService::GetMerchantInfoForUrl(const GURL& url,
+                                                MerchantInfoCallback callback) {
+  std::move(callback).Run(url, std::move(merchant_info_));
+}
+
+void MockShoppingService::SetResponseForGetMerchantInfoForUrl(
+    absl::optional<commerce::MerchantInfo> merchant_info) {
+  merchant_info_ = std::move(merchant_info);
+}
+
+}  // namespace commerce
