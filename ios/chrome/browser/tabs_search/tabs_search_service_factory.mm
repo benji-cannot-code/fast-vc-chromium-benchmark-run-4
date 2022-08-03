4034a670a6fbfc19b62c20defdde1185e7030b7d
Mike Dougherty <michaeldo@chromium.org>
2022-08-03 20:48:29 +0000

    Add missing DependsOn statements for TabsSearchServiceFactory
    Fixed: 1346607
    Change-Id: I41fe99d8b9b33a61b92c5495993675ffb56c4fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807996
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031193}

--- ios/chrome/browser/tabs_search/tabs_search_service_factory.mm
+++ ios/chrome/browser/tabs_search/tabs_search_service_factory.mm
@@ -8,6 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/history/history_service_factory.h"
+#import "ios/chrome/browser/history/web_history_service_factory.h"
+#import "ios/chrome/browser/main/browser_list_factory.h"
+#import "ios/chrome/browser/sessions/ios_chrome_tab_restore_service_factory.h"
+#import "ios/chrome/browser/signin/identity_manager_factory.h"
+#import "ios/chrome/browser/sync/session_sync_service_factory.h"
+#import "ios/chrome/browser/sync/sync_service_factory.h"
 #import "ios/chrome/browser/tabs_search/tabs_search_service.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -28,7 +35,14 @@ TabsSearchServiceFactory* TabsSearchServiceFactory::GetInstance() {
 TabsSearchServiceFactory::TabsSearchServiceFactory()
     : BrowserStateKeyedServiceFactory(
           "TabsSearchService",
-          BrowserStateDependencyManager::GetInstance()) {}
+          BrowserStateDependencyManager::GetInstance()) {
+  DependsOn(BrowserListFactory::GetInstance());
+  DependsOn(IOSChromeTabRestoreServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(ios::HistoryServiceFactory::GetInstance());
+  DependsOn(SessionSyncServiceFactory::GetInstance());
+  DependsOn(SyncServiceFactory::GetInstance());
+}
 
 TabsSearchServiceFactory::~TabsSearchServiceFactory() = default;
 
