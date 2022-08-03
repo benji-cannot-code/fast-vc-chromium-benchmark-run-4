05f811aebadc179e9ce17c6fb222f02a1a176b10
conniekxu <conniekxu@chromium.org>
2022-08-03 02:31:26 +0000

    dark_mode: Update highlight border's corner radius for pip windows
    This CL moves kPipRoundedCornerRadius from ash to ChromeOS to make sure there's a single source of truth for rounded corner radius of pip windows.
    
    Fixed: 1346342
    Test: Manual
    Change-Id: I186a698f6552eae9a03c49927ed5a7519c57f58d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781646
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030861}

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ui/base/chromeos_ui_constants.h"
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/picture_in_picture_window_controller.h"
 #include "content/public/browser/web_contents.h"
@@ -296,7 +297,7 @@ void OverlayWindowViews::DoShowInactive() {
   // For rounded corners.
   if (ash::features::IsPipRoundedCornersEnabled()) {
     ash::SetCornerRadius(GetNativeWindow(), GetRootView()->layer(),
-                         ash::kPipRoundedCornerRadius);
+                         chromeos::kPipRoundedCornerRadius);
   }
 #endif
 
