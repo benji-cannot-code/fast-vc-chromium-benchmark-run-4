8106ae70bd5c1ae1c20a07f4eab970f4c7c8f9d8
Dave Tapuska <dtapuska@chromium.org>
2022-08-03 16:24:21 +0000

    Remove references to RenderFrameProxy.
    Clean up documentation to correctly refer to blink::RemoteFrame instead
    of RenderFrameProxy since the intermediate class has been removed.
    
    BUG=1166719
    
    Change-Id: Ic65777a7e240794feccf1c09ba2aadf0c4ba6c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806225
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031077}

--- content/browser/renderer_host/render_frame_host_manager_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_manager_browsertest.cc
@@ -3375,7 +3375,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest,
 
 // Tests that navigating cross-process and reusing an existing RenderViewHost
 // (whose process has been killed/crashed) recreates properly the RenderView and
-// RenderFrameProxy on the renderer side.
+// `blink::RemoteFrame` on the renderer side.
 // See https://crbug.com/544271
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest,
                        RenderViewInitAfterProcessKill) {
@@ -8893,9 +8893,9 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest,
   // TODO(https://crbug.com/1148793): Remove this early return. This doesn't
   // work for RenderDocumentLevel::kSubframe or greater because cancelling the
   // navigation when detaching the subtree tries to restore the replaced
-  // RenderFrameProxy (which doesn't exist in the same-site RenderDocument case
-  // because the replaced object wasn't a RenderFrameProxy, but instead a
-  // RenderFrame).
+  // `blink::RemoteFrame` (which doesn't exist in the same-site RenderDocument
+  // case because the replaced object wasn't a `blink::RemoteFrame`, but instead
+  // a RenderFrame).
   if (ShouldCreateNewHostForSameSiteSubframe())
     return;
   AssertCanRemoveSubframeInUnload(/*same_site=*/true);
