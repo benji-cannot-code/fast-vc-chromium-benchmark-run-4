38b80bb98b97e19b9629a18cae628becb7bf1907
Jack Lynch <jacklynch@google.com>
2022-08-03 18:57:16 +0000

    Treat empty required fields as valid
    This CL reduces the verbosity of the accessibility tree and harmonizes
    the behavior of aria-required and the required attribute by ensuring
    that empty text inputs are treated as valid, even if they are required.
    The exception is empty text inputs with custom error messages, which are
    set to invalid to avoid hiding the custom error.
    
    AX-Relnotes: Screen readers will no longer identify empty but otherwise
    valid required inputs as invalid.
    
    Bug: 1318969
    Change-Id: I9686c253aaf3181a7d8d99fd0e24c71edf6ed30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790223
    Commit-Queue: Jack Lynch <jacklynch@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031136}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/dcheck_is_on.h"
 #include "third_party/blink/renderer/core/editing/markers/document_marker.h"
+#include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_object.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 
@@ -169,6 +170,7 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
   // Properties of interactive elements.
   ax::mojom::blink::AriaCurrentState GetAriaCurrentState() const final;
   ax::mojom::blink::InvalidState GetInvalidState() const final;
+  bool IsValidFormControl(ListedElement* form_control) const;
   bool ValueForRange(float* out_value) const override;
   bool MaxValueForRange(float* out_value) const override;
   bool MinValueForRange(float* out_value) const override;
