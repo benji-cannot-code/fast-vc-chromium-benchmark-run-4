5c86871e4d8459c4736ef808b66b48c06359004e
Wei Lee <wtlee@google.com>
2022-08-03 07:32:59 +0000

    CCA: Tweak doc scan DLC nudge to meet new UI design
    Bug: b:226262670
    Test: Manually
    
    Change-Id: I57ba48c68b4fb82d90db8df20d9b517c7bf51f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789006
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030926}

--- ash/webui/camera_app_ui/resources/js/custom_effect.ts
+++ ash/webui/camera_app_ui/resources/js/custom_effect.ts
@@ -4,10 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import * as animation from './animation.js';
-import {assertExists, assertInstanceof, assertNotReached} from './assert.js';
+import {assertExists, assertNotReached} from './assert.js';
 import * as dom from './dom.js';
 import {I18nString} from './i18n_string.js';
 import * as loadTimeData from './models/load_time_data.js';
+import {ChromeHelper} from './mojo/chrome_helper.js';
+import * as state from './state.js';
 import * as util from './util.js';
 
 /**
@@ -78,7 +80,9 @@ const TOAST_POSITION_UPDATE_MS = 500;
 
 enum PositionProperty {
   BOTTOM = 'bottom',
+  CENTER = 'center',
   LEFT = 'left',
+  MIDDLE = 'middle',
   RIGHT = 'right',
   TOP = 'top',
 }
@@ -96,12 +100,32 @@ type PositionInfos = Array<{
 
 export enum IndicatorType {
   DOWNLOAD_DOCUMENT_SCANNER = 'download_document_scanner',
+  DOC_SCAN_AVAILABLE = 'doc_scan_available',
+}
+
+/**
+ * Setup the required state observers to dismiss toasts when changing
+ * modes/cameras.
+ */
+export function setup(): void {
+  state.addObserver(state.State.CAMERA_SWITCHING, (val) => {
+    if (val) {
+      hide();
+    }
+  });
+  state.addObserver(state.State.MODE_SWITCHING, (val) => {
+    if (val) {
+      hide();
+    }
+  });
 }
 
 function getIndicatorI18nStringId(indicatorType: IndicatorType): I18nString {
   switch (indicatorType) {
     case IndicatorType.DOWNLOAD_DOCUMENT_SCANNER:
       return I18nString.DOWNLOADING_DOCUMENT_SCANNING_FEATURE;
+    case IndicatorType.DOC_SCAN_AVAILABLE:
+      return I18nString.NEW_DOCUMENT_SCAN_TOAST;
     default:
       assertNotReached();
   }
@@ -111,6 +135,8 @@ function getIndicatorIcon(indicatorType: IndicatorType): string|null {
   switch (indicatorType) {
     case IndicatorType.DOWNLOAD_DOCUMENT_SCANNER:
       return '/images/download_dlc_toast_icon.svg';
+    case IndicatorType.DOC_SCAN_AVAILABLE:
+      return '/images/new_feature_toast_icon.svg';
     default:
       return null;
   }
@@ -152,7 +178,15 @@ function updatePosition(
   }
   style.clear();
   for (const {elProperty, toastProperty, offset} of properties) {
-    let value = rect[elProperty] + offset;
+    let value;
+    if (elProperty === PositionProperty.CENTER) {
+      value = rect.left + offset + rect.width / 2;
+    } else if (elProperty === PositionProperty.MIDDLE) {
+      value = rect.top + offset + rect.height / 2;
+    } else {
+      value = rect[elProperty] + offset;
+    }
+
     if (toastProperty === PositionProperty.RIGHT) {
       value = window.innerWidth - value;
     } else if (toastProperty === PositionProperty.BOTTOM) {
@@ -192,7 +226,7 @@ class Toast {
     this.anchor.removeAttribute('aria-owns');
     clearInterval(this.cancelHandle);
     for (const {target} of this.positionInfos) {
-      document.body.removeChild(target);
+      target.remove();
     }
   }
 }
@@ -367,27 +401,21 @@ export function showPtzToast(): void {
 }
 
 /**
- * Shows feature visual effect for document mode entry.
+ * Shows document scan feature is available indicator on the scan mode button.
  */
-export function showDocToast(): void {
+export function showDocScanAvailableIndicator(): void {
   const scanModeButton = dom.get('input[data-mode="scan"]', HTMLInputElement);
-  const scanModeItem =
-      assertInstanceof(scanModeButton.parentElement, HTMLDivElement);
-  // aria-owns don't work on HTMLInputElement, show toast on parent div
-  // instead.
-  const {hide} = showNewFeature(scanModeItem);
-  scanModeButton.addEventListener('click', hide, {once: true});
+  showIndicator(scanModeButton, IndicatorType.DOC_SCAN_AVAILABLE);
 }
 
 /**
  * Shows loading indicator toast for document mode when it's supported but not
  * yet ready.
  */
-export function showDocIndicator(): void {
-  const scanModeButton = dom.get('input[data-mode="scan"]', HTMLInputElement);
-  const scanModeItem =
-      assertInstanceof(scanModeButton.parentElement, HTMLDivElement);
+export async function showDownloadingDocScanIndicator(): Promise<void> {
+  const docModeButton = dom.get('#scan-document-option', HTMLDivElement);
   const {hide} =
-      showIndicator(scanModeItem, IndicatorType.DOWNLOAD_DOCUMENT_SCANNER);
-  scanModeButton.addEventListener('click', hide, {once: true});
+      showIndicator(docModeButton, IndicatorType.DOWNLOAD_DOCUMENT_SCANNER);
+  await ChromeHelper.getInstance().waitUntilDocumentModeReady();
+  hide();
 }
