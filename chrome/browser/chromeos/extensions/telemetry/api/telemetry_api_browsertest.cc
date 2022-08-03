303aea66bb184e8094c8ea65902f2c3342739e58
Oleh Lamzin <lamzin@google.com>
2022-08-03 11:04:37 +0000

    [Telemetry] Decouple fake service and fake factory
    Decouple fake service and fake factory since we won't need fake factory
    in Lacros, but will need it in Ash.
    
    This is part of Making Telemetry Extensions work in LaCrOS:
    go/dd-telemetry-extensions-lacros.
    
    This CL is part of crrev.com/c/3613280/8 split which is very big to
    submit.
    
    Bug: b:219514064
    Test: ash_webui_unittests --gtest_filter="*DiagnosticsService*"
    Test: browser_tests --gtest_filter="*TelemetryExtension*"
    Test: unit_tests --gtest_filter="*ProbeService*"
    Change-Id: I59fc13e54d07e00d6a3a399fe8c0611aa707350f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749635
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Mahmoud Aly <mgawad@google.com>
    Quick-Run: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030988}

--- chrome/browser/chromeos/extensions/telemetry/api/telemetry_api_browsertest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/telemetry_api_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/ash/telemetry_extension/fake_probe_service.h"
+#include "chrome/browser/ash/telemetry_extension/fake_probe_service_factory.h"
 #include "chrome/browser/ash/telemetry_extension/probe_service_ash.h"
 #include "chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.h"
 #include "content/public/test/browser_test.h"
@@ -35,7 +36,7 @@ class TelemetryExtensionTelemetryApiBrowserTest
         std::move(fake_diagnostics_service_impl));
   }
 
-  ash::FakeProbeService::Factory fake_probe_factory_;
+  ash::FakeProbeServiceFactory fake_probe_factory_;
 };
 
 IN_PROC_BROWSER_TEST_F(TelemetryExtensionTelemetryApiBrowserTest,
