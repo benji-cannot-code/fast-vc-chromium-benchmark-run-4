8f8bb1b419b24a867682aca84c4f69fd22061e23
Yutaka Hirano <yhirano@chromium.org>
2022-08-03 04:46:55 +0000

    Disable DefaultFrameHeaderTest tests
    They're flaky.
    
    Bug: 1349633, 1349573
    Change-Id: I440e0bfe51b54618c20c4d0430de6cabde1309a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805088
    Owners-Override: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030896}

--- ash/frame/default_frame_header_unittest.cc
+++ ash/frame/default_frame_header_unittest.cc
@@ -176,7 +176,8 @@ class FramePaintWaiter : public ui::CompositorObserver {
   FrameHeader* frame_header_ = nullptr;
 };
 
-TEST_F(DefaultFrameHeaderTest, DeleteDuringAnimation) {
+// TODO(crbug.com/1349573): Revive this test.
+TEST_F(DefaultFrameHeaderTest, DISABLED_DeleteDuringAnimation) {
   const auto bounds = gfx::Rect(100, 100);
   auto win0 = CreateAppWindow(bounds, AppType::BROWSER);
   auto win1 = CreateAppWindow(bounds, AppType::BROWSER);
@@ -214,8 +215,9 @@ TEST_F(DefaultFrameHeaderTest, DeleteDuringAnimation) {
   EXPECT_TRUE(checker.destroyed());
 }
 
+// TODO(crbug.com/1349633): Revive this test.
 // Make sure that the animation is canceled when resized.
-TEST_F(DefaultFrameHeaderTest, ResizeAndReorderDuringAnimation) {
+TEST_F(DefaultFrameHeaderTest, DISABLED_ResizeAndReorderDuringAnimation) {
   const auto bounds = gfx::Rect(100, 100);
   auto win_0 = CreateAppWindow(bounds, AppType::BROWSER);
   auto win_1 = CreateAppWindow(bounds, AppType::BROWSER);
