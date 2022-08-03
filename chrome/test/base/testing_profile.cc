1112985dd00384a93149cc26e3128898e52805f8
Hidehiko Abe <hidehiko@chromium.org>
2022-08-03 00:43:37 +0000

    Use AccountId to get fake user id hash.
    To make it closer to the real world situation, and to adapt
    upcoming refactoring.
    Along with the change, a few testing accounts are suffixed "@test",
    so that they can be consistent with the faked accounts.
    
    BUG=1325210
    TEST=Ran tryjobs.
    
    Change-Id: I794038f853efe2fd34d99f02e710f822648885d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3800832
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030836}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -160,7 +160,7 @@ std::unique_ptr<KeyedService> BuildPersonalDataManagerInstanceFor(
 }  // namespace
 
 // static
-const char TestingProfile::kDefaultProfileUserName[] = "testing_profile";
+const char TestingProfile::kDefaultProfileUserName[] = "testing_profile@test";
 
 // static
 #if BUILDFLAG(IS_CHROMEOS_ASH)
--- chrome/test/base/testing_profile_manager.cc
+++ chrome/test/base/testing_profile_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/memory/ref_counted.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/test_file_util.h"
 #include "build/build_config.h"
@@ -90,9 +91,14 @@ TestingProfile* TestingProfileManager::CreateTestingProfile(
   if (profile_name != chrome::kInitialProfile &&
       profile_name != chrome::kLockScreenProfile &&
       profile_name != ash::ProfileHelper::GetLockScreenAppProfileName()) {
+    const std::string fake_email =
+        profile_name.find('@') == std::string::npos
+            ? base::ToLowerASCII(profile_name) + "@test"
+            : profile_name;
     profile_path =
         profile_path.Append(ash::ProfileHelper::Get()->GetUserProfileDir(
-            ash::ProfileHelper::GetUserIdHashByUserIdForTesting(profile_name)));
+            ash::ProfileHelper::GetUserIdHashByUserIdForTesting(
+                AccountId::FromUserEmail(fake_email).GetUserEmail())));
   } else {
     profile_path = profile_path.AppendASCII(profile_name);
   }
