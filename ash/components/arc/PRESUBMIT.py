5fe140c30e0f7180aed3ce1ccb3399d0eabaaec1
Bruce Dawson <brucedawson@chromium.org>
2022-08-03 00:29:56 +0000

    Always run cpplint on ash presubmits, fix one error
    cpplint (CheckChangeLintsClean) was running only on upload presubmits,
    when it is produces warnings not errors, which is probably why one
    cpplint error remained. This change fixes that error (although it is not
    a good error) and gets cpplint to run on commit and on upload. This will
    keep ash cpplint clean. This CL was tested with this command:
    
        git cl presubmit --force --upload --files "ash/components/arc/*"
    
    Bug: 1309977
    Change-Id: I8606a33996e9fe23fd147cfd8be5d11a7b3f9364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805775
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030834}

--- ash/components/arc/PRESUBMIT.py
+++ ash/components/arc/PRESUBMIT.py
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 USE_PYTHON3 = True
+PRESUBMIT_VERSION = '2.0.0'
 
-def CheckChangeOnUpload(input_api, output_api):
+def CheckCommon(input_api, output_api):
   results = []
   results += input_api.canned_checks.CheckChangeLintsClean(
       input_api, output_api)
--- ash/components/arc/dark_theme/arc_dark_theme_bridge_unittest.cc
+++ ash/components/arc/dark_theme/arc_dark_theme_bridge_unittest.cc
@@ -35,7 +35,7 @@ class ArcDarkThemeBridgeTest : public testing::Test {
     bridge_->Shutdown();
   }
 
-  ArcDarkThemeBridgeTest(const ArcDarkThemeBridge&) = delete;
+  explicit ArcDarkThemeBridgeTest(const ArcDarkThemeBridge&) = delete;
   ArcDarkThemeBridgeTest& operator=(const ArcDarkThemeBridge&) = delete;
 
   content::BrowserTaskEnvironment task_environment_;
