6dcc858c21359c4823a3a7eb337a85b191cf9f62
minch <minch@chromium.org>
2022-08-04 15:33:58 +0000

    migration: Fix an issue of crrev.com/c/3787678
    - Update tray power image as well if only the `info_` changes.
    - Put `UpdateStatus` to OnThemeChanged instead of the constructor,
      which can guarantee the `GetColorProvider` will not be nullptr.
    
    Bug: 1347612
    Change-Id: I09d24d71855e07ab3d681542c01a90a5b8e1ea2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807901
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031510}

--- ash/system/power/tray_power.cc
+++ ash/system/power/tray_power.cc
@@ -43,7 +43,6 @@ namespace ash {
 
 PowerTrayView::PowerTrayView(Shelf* shelf) : TrayItemView(shelf) {
   CreateImageView();
-  UpdateStatus();
 
   PowerStatus::Get()->AddObserver(this);
 }
@@ -79,6 +78,7 @@ const char* PowerTrayView::GetClassName() const {
 
 void PowerTrayView::OnThemeChanged() {
   TrayItemView::OnThemeChanged();
+  UpdateStatus();
   UpdateImage(/*icon_color_changed=*/true);
 }
 
@@ -118,9 +118,6 @@ void PowerTrayView::UpdateImage(bool icon_color_changed) {
   if (info_ && info_->ApproximatelyEqual(info) && !icon_color_changed)
     return;
   info_ = info;
-
-  if (!icon_color_changed)
-    return;
 
   // Note: The icon color (both fg and bg) changes when the UI in in OOBE mode.
   const SkColor icon_fg_color = TrayIconColor(session_state_);
