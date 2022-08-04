32987149d822699dd1972ce0e9845317c5f5fc35
Josh Graydus <jgraydus@google.com>
2022-08-04 23:00:01 +0000

    [Nearby Presence] move construction of shared remotes out of NearbyConnections constructor
    NearbyConnections currently handles building the SharedRemote objects for various dependencies (bluetooth adapter, socket manager, etc.). However, NearbyConnections doesn't use these SharedRemote objects. Rather, ImplementationPlatform accesses them through getters on the (singleton) NearbyConnections. This CL moves the construction of the SharedRemote objects to the parent component (SharingImpl) and makes them available via a static pointer. We believe this is a simpler pattern and a better separation of concerns.
    
    Change-Id: Idf6f662bf24d5f064c1a6cca0d4cdf60b6bd9a46
    Tested: not tested yet
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781674
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Josh Graydus <jgraydus@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031645}

---
+++ chrome/services/sharing/nearby/nearby_shared_remotes.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/nearby_shared_remotes.h"
+
+namespace location::nearby {
+
+namespace {
+
+// This object is owned by SharingImpl and made available for
+// ImplementationPlatform which implements a completely static interface.
+NearbySharedRemotes* g_instance = nullptr;
+
+}  // namespace
+
+// static
+NearbySharedRemotes* NearbySharedRemotes::GetInstance() {
+  return g_instance;
+}
+
+// static
+void NearbySharedRemotes::SetInstance(NearbySharedRemotes* instance) {
+  g_instance = instance;
+}
+
+NearbySharedRemotes::NearbySharedRemotes() = default;
+NearbySharedRemotes::~NearbySharedRemotes() = default;
+
+}  // namespace location::nearby
