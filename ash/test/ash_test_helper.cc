79672cc0b09b46bb9bf0a660af3d23c228faca1e
Sean Kau <skau@chromium.org>
2022-08-04 16:47:52 +0000

    Stop accumulating ColorProviderManagerInitializers
    Because ColorProviderManager is a global, when running multiple tests
    in the same process, we end up accumulating
    ColorProviderManagerInitializers causing exponential slowdown. Fix this
    by clearing the ColorProviderManager on TearDown for each test.
    
    Confirmed by running DragDropControllerTest. Previously, test cases
    6-10 took 500, 700, 1400, 1900, 3200 ms.  Now, they take ~500ms each.
    
    Bug: 1349232
    Change-Id: I55c1ba9327f21b7ece1f93251c50491910fb9ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807993
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031533}

--- ash/test/ash_test_helper.cc
+++ ash/test/ash_test_helper.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ime/ash/mock_input_method_manager.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/display/display_switches.h"
 #include "ui/display/manager/display_manager.h"
 #include "ui/display/test/display_manager_test_api.h"
@@ -190,6 +191,10 @@ void AshTestHelper::TearDown() {
   prefs_provider_.reset();
   statistics_provider_.reset();
   command_line_.reset();
+
+  // Purge ColorProviderManager between tests so that we don't accumulate
+  // ColorProviderInitializers. crbug.com/1349232.
+  ui::ColorProviderManager::ResetForTesting();
 
   AuraTestHelper::TearDown();
 
