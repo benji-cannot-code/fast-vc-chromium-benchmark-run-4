b7f063ac79683f4a68d03ad1651ac8f4c9d4e15e
Jihwan Kim <bluewhale.marc@gmail.com>
2022-08-04 06:29:42 +0000

    Show the validation message when hovering <textarea> tag that has 'required' attribute.
    This is not the spec, but other vendors shows message on <textarea>.
    For this reason, this CL shows validation message either.
    
    Bug: 1339750
    Change-Id: Iccbd19f26cd9dcab3686f9d7fa4eedc452d18dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798838
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Jihwan Kim <bluewhale.marc@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1031376}

--- third_party/blink/renderer/core/html/forms/html_text_area_element.h
+++ third_party/blink/renderer/core/html/forms/html_text_area_element.h
@@ -73,6 +73,8 @@ class CORE_EXPORT HTMLTextAreaElement final : public TextControlElement {
   void setCols(unsigned);
   void setRows(unsigned);
 
+  String DefaultToolTip() const override;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(HTMLTextAreaElementTest, SanitizeUserInputValue);
 
