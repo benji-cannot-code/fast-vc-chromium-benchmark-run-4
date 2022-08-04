15296a048eb88a5ece5c8260b92614394d0f2242
sauski <sauski@google.com>
2022-08-04 09:09:02 +0000

    Privacy Sandbox Settings: Remove unused inheritance
    A previous CL removed any sync / policy / identity observer logic from
    the PrivacySandboxService class, but did not remove the inheritance
    from the observer classes.
    
    This CL removes the unused inheritances.
    
    Bug: 1166665
    Change-Id: I7b4533d3177549c88e229c3d586af69f38d22004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807422
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031413}

--- chrome/browser/ui/privacy_sandbox/privacy_sandbox_prompt_helper.cc
+++ chrome/browser/ui/privacy_sandbox/privacy_sandbox_prompt_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/privacy_sandbox/privacy_sandbox_prompt.h"
+#include "components/sync/driver/sync_service.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
