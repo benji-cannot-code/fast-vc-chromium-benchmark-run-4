fd5cfec1f10d66b9e9fee82271e239a80bdc086e
Antonio Gomes <tonikitoo@igalia.com>
2022-08-04 22:28:58 +0000

    [ozone/wayland] Use more 'auto' in WaylandPointer class
    R=adunaev@igalia.com
    BUG=NONE
    
    Change-Id: I999794d0350dccfacc7219f1bee8e5fb759bed82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812177
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1031629}

--- ui/ozone/platform/wayland/host/wayland_event_source.cc
+++ ui/ozone/platform/wayland/host/wayland_event_source.cc
@@ -58,8 +58,7 @@ EventTarget* GetRootTarget(EventTarget* target) {
 
 gfx::Point GetOriginInScreen(WaylandWindow* target) {
   gfx::Point origin = target->GetBoundsInDIP().origin();
-  WaylandWindow* parent =
-      static_cast<WaylandWindow*>(target->GetParentTarget());
+  auto* parent = static_cast<WaylandWindow*>(target->GetParentTarget());
   while (parent) {
     origin += parent->GetBoundsInDIP().origin().OffsetFromOrigin();
     parent = static_cast<WaylandWindow*>(parent->GetParentTarget());
@@ -68,7 +67,7 @@ gfx::Point GetOriginInScreen(WaylandWindow* target) {
 }
 
 gfx::Point GetLocationInScreen(LocatedEvent* event) {
-  WaylandWindow* root_window =
+  auto* root_window =
       static_cast<WaylandWindow*>(GetRootTarget(event->target()));
   return event->root_location() +
          root_window->GetBoundsInDIP().origin().OffsetFromOrigin();
@@ -76,7 +75,7 @@ gfx::Point GetLocationInScreen(LocatedEvent* event) {
 
 void SetRootLocation(LocatedEvent* event) {
   gfx::PointF location = event->location_f();
-  WaylandWindow* target = static_cast<WaylandWindow*>(event->target());
+  auto* target = static_cast<WaylandWindow*>(event->target());
 
   while (target->GetParentTarget()) {
     location += target->GetBoundsInDIP().origin().OffsetFromOrigin();
@@ -128,7 +127,7 @@ WaylandEventSource::TouchFrame::~TouchFrame() = default;
 // static
 void WaylandEventSource::ConvertEventToTarget(const EventTarget* new_target,
                                               LocatedEvent* event) {
-  WaylandWindow* current_target = static_cast<WaylandWindow*>(event->target());
+  auto* current_target = static_cast<WaylandWindow*>(event->target());
   gfx::Vector2d diff = GetOriginInScreen(current_target) -
                        GetOriginInScreen(static_cast<WaylandWindow*>(
                            const_cast<EventTarget*>(new_target)));
--- ui/ozone/platform/wayland/host/wayland_pointer.cc
+++ ui/ozone/platform/wayland/host/wayland_pointer.cc
@@ -47,8 +47,7 @@ void WaylandPointer::Enter(void* data,
                            wl_surface* surface,
                            wl_fixed_t surface_x,
                            wl_fixed_t surface_y) {
-  DCHECK(data);
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   pointer->connection_->serial_tracker().UpdateSerial(
       wl::SerialType::kMouseEnter, serial);
 
@@ -65,8 +64,7 @@ void WaylandPointer::Leave(void* data,
                            wl_pointer* obj,
                            uint32_t serial,
                            wl_surface* surface) {
-  DCHECK(data);
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   pointer->connection_->serial_tracker().ResetSerial(
       wl::SerialType::kMouseEnter);
 
@@ -80,7 +78,7 @@ void WaylandPointer::Motion(void* data,
                             uint32_t time,
                             wl_fixed_t surface_x,
                             wl_fixed_t surface_y) {
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   gfx::PointF location(wl_fixed_to_double(surface_x),
                        wl_fixed_to_double(surface_y));
   const WaylandWindow* target = pointer->delegate_->GetPointerTarget();
@@ -96,7 +94,7 @@ void WaylandPointer::Button(void* data,
                             uint32_t time,
                             uint32_t button,
                             uint32_t state) {
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   int changed_button;
   switch (button) {
     case BTN_LEFT:
@@ -136,7 +134,7 @@ void WaylandPointer::Axis(void* data,
                           uint32_t axis,
                           wl_fixed_t value) {
   static const double kAxisValueScale = 10.0;
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   gfx::Vector2dF offset;
   // Wayland compositors send axis events with values in the surface coordinate
   // space. They send a value of 10 per mouse wheel click by convention, so
@@ -164,7 +162,7 @@ void WaylandPointer::Axis(void* data,
 
 // static
 void WaylandPointer::Frame(void* data, wl_pointer* obj) {
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   // The frame event ends the sequence of pointer events.  Clear the flag.  The
   // next frame will set it when necessary.
   pointer->axis_source_received_ = false;
@@ -175,7 +173,7 @@ void WaylandPointer::Frame(void* data, wl_pointer* obj) {
 void WaylandPointer::AxisSource(void* data,
                                 wl_pointer* obj,
                                 uint32_t axis_source) {
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   pointer->axis_source_received_ = true;
   pointer->delegate_->OnPointerAxisSourceEvent(axis_source);
 }
@@ -185,7 +183,7 @@ void WaylandPointer::AxisStop(void* data,
                               wl_pointer* obj,
                               uint32_t time,
                               uint32_t axis) {
-  WaylandPointer* pointer = static_cast<WaylandPointer*>(data);
+  auto* pointer = static_cast<WaylandPointer*>(data);
   pointer->delegate_->OnPointerAxisStopEvent(axis);
 }
 
