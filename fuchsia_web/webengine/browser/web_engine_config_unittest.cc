84956635e418dc47e138e2f82a1bb729251a9120
Fabrice de Gans <fdegans@chromium.org>
2022-08-04 22:42:01 +0000

    [fuchsia] Improve use of config files on WebEngine
    This fixes an issue when setting a config file would trigger a
    DCHECK. In addition, setting a switch via command line argument and
    via a config file would trigger a DCHECK. This fixes the issue by
    always preferring the command line argument value over the config
    file value.
    
    Bug: 1343878
    Change-Id: Icd99f57c8594fc3d277fade4d44080d6a6a3e0cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3808173
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031638}

--- fuchsia_web/webengine/browser/web_engine_config_unittest.cc
+++ fuchsia_web/webengine/browser/web_engine_config_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia_web/webengine/browser/web_engine_config.h"
 
 #include "base/command_line.h"
+#include "base/metrics/field_trial.h"
 #include "base/values.h"
 #include "fuchsia_web/webengine/switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -25,7 +26,28 @@ base::Value CreateConfigWithSwitchValue(std::string switch_name,
 
 }  // namespace
 
-TEST(WebEngineConfig, CommandLineArgs) {
+class WebEngineConfigTest : public testing::Test {
+ public:
+  WebEngineConfigTest() = default;
+  ~WebEngineConfigTest() override = default;
+
+  WebEngineConfigTest(const WebEngineConfigTest&) = delete;
+  WebEngineConfigTest& operator=(const WebEngineConfigTest&) = delete;
+
+  void SetUp() override {
+    backup_field_trial_list_ = base::FieldTrialList::BackupInstanceForTesting();
+  }
+
+  void TearDown() override {
+    base::FieldTrialList::RestoreInstanceForTesting(backup_field_trial_list_);
+    backup_field_trial_list_ = nullptr;
+  }
+
+ private:
+  base::FieldTrialList* backup_field_trial_list_ = nullptr;
+};
+
+TEST_F(WebEngineConfigTest, CommandLineArgs) {
   // Specify a configuration that sets valid args with valid strings.
   auto config = CreateConfigWithSwitchValue("renderer-process-limit", "0");
   base::CommandLine command(base::CommandLine::NO_PROGRAM);
@@ -33,7 +55,7 @@ TEST(WebEngineConfig, CommandLineArgs) {
   EXPECT_EQ(command.GetSwitchValueASCII("renderer-process-limit"), "0");
 }
 
-TEST(WebEngineConfig, DisallowedCommandLineArgs) {
+TEST_F(WebEngineConfigTest, DisallowedCommandLineArgs) {
   // Specify a configuration that sets a disallowed command-line argument.
   auto config = CreateConfigWithSwitchValue("kittens-are-nice", "0");
   base::CommandLine command(base::CommandLine::NO_PROGRAM);
@@ -41,7 +63,7 @@ TEST(WebEngineConfig, DisallowedCommandLineArgs) {
   EXPECT_FALSE(command.HasSwitch("kittens-are-nice"));
 }
 
-TEST(WebEngineConfig, WronglyTypedCommandLineArgs) {
+TEST_F(WebEngineConfigTest, WronglyTypedCommandLineArgs) {
   base::Value::Dict config;
 
   // Specify a configuration that sets valid args with invalid value.
@@ -54,7 +76,7 @@ TEST(WebEngineConfig, WronglyTypedCommandLineArgs) {
                                                &command));
 }
 
-TEST(WebEngineConfig, WithGoogleApiKeyValue) {
+TEST_F(WebEngineConfigTest, WithGoogleApiKeyValue) {
   constexpr char kDummyApiKey[] = "apikey123";
   auto config = CreateConfigWithSwitchValue("google-api-key", kDummyApiKey);
   base::CommandLine command(base::CommandLine::NO_PROGRAM);
