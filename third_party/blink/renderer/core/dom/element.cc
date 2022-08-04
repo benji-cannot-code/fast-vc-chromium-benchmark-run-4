355fc874f6a40fd798e472ae8fbabdc1069c6a67
Koji Ishii <kojii@chromium.org>
2022-08-04 23:19:00 +0000

    [anchor-position] Layout support for `@position-fallback`
    This patch supports laying out `@position-fallback`.
    
    The fallback tries are computed by using a separate
    |ComputedStyle| than the element's default style, obtained by
    |StyleForPositionFallback|.
    
    |NGOutOfFlowLayoutPart| computes the offset and size by using
    the specified style, and layout is supposed to honor the given
    offset and size.
    
    This patch includes one basic test. There are several
    different code branches in the code path, and that not all of
    them are tested in this test. More tests will be in future
    patches.
    
    No known TODOs at this point, including block fragmentation,
    but more tests are likely to uncover missed code paths.
    
    Bug: 1309178
    Change-Id: Iff7f22eaccd914f8e6a27453897814b554ce9b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793668
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031659}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -9010,6 +9010,7 @@ const ComputedStyle* Element::StyleForPositionFallback(unsigned index) {
   const ComputedStyle* base_style = GetComputedStyle();
   if (!base_style)
     return nullptr;
+  DCHECK(base_style->PositionFallback());
   if (const ComputedStyle* cached_style =
           base_style->GetCachedPositionFallbackStyle(index))
     return cached_style;
--- third_party/blink/renderer/core/layout/ng/ng_absolute_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_absolute_utils.h
@@ -23,6 +23,19 @@ class NGLayoutResult;
 struct NGLogicalStaticPosition;
 
 struct CORE_EXPORT NGLogicalOutOfFlowDimensions {
+  LayoutUnit MarginBoxInlineStart() const {
+    return inset.inline_start - margins.inline_start;
+  }
+  LayoutUnit MarginBoxBlockStart() const {
+    return inset.block_start - margins.block_start;
+  }
+  LayoutUnit MarginBoxInlineEnd() const {
+    return inset.inline_start + size.inline_size + margins.inline_end;
+  }
+  LayoutUnit MarginBoxBlockEnd() const {
+    return inset.block_start + size.block_size + margins.block_end;
+  }
+
   NGBoxStrut inset;
   LogicalSize size = {kIndefiniteSize, kIndefiniteSize};
   NGBoxStrut margins;
@@ -64,6 +77,7 @@ ComputeOutOfFlowAvailableSize(const NGBlockNode&,
 // Will return true if |NGBlockNode::ComputeMinMaxSizes| was called.
 CORE_EXPORT bool ComputeOutOfFlowInlineDimensions(
     const NGBlockNode&,
+    const ComputedStyle& style,
     const NGConstraintSpace&,
     const NGLogicalOutOfFlowInsets&,
     const NGBoxStrut& border_padding,
@@ -78,6 +92,7 @@ CORE_EXPORT bool ComputeOutOfFlowInlineDimensions(
 // otherwise it will return nullptr.
 CORE_EXPORT const NGLayoutResult* ComputeOutOfFlowBlockDimensions(
     const NGBlockNode&,
+    const ComputedStyle& style,
     const NGConstraintSpace&,
     const NGLogicalOutOfFlowInsets&,
     const NGBoxStrut& border_padding,
