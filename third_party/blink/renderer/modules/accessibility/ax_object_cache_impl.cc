14e070bb7a1f4edcb88fa51db7db2212ea6bf0c6
Aaron Leventhal <aleventhal@google.com>
2022-08-04 02:38:38 +0000

    ChildrenChanged(node) must use SafeGet()
    Bug: 1349640
    Change-Id: Ia4f16faf1e31ebb107f8da891e7cf88634e9f7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805396
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031336}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -2318,7 +2318,8 @@ void AXObjectCacheImpl::SlotAssignmentWillChange(Node* node) {
 }
 
 void AXObjectCacheImpl::ChildrenChanged(Node* node) {
-  ChildrenChanged(Get(node));
+  // Use SafeGet() because there is no guarantee that layout is clean right now.
+  ChildrenChanged(SafeGet(node));
 }
 
 // ChildrenChanged gets called a lot. For the accessibility tests that
