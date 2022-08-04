059d3dbb9b1f0fed1cc8c67b4b33a7e8424ac1b7
Evan Green <evgreen@chromium.org>
2022-08-04 20:11:13 +0000

    hiberman: Add auth_session ResumeFromHibernate
    Add support to the hiberman dbus client for the new
    ResumeFromHibernateAS dbus method, which resumes via an auth_session_id
    token rather than an account_id. This mechanism is preferred as the
    AuthSession flow will soon become the only flow.
    
    BUG=b:233103309
    TEST=hibernate and resume
    
    Change-Id: I560df1e2e53a5d65091df0e963a6fe0c6a41fe50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735901
    Commit-Queue: Evan Green <evgreen@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031565}

--- chromeos/ash/components/dbus/hiberman/hiberman_client_unittest.cc
+++ chromeos/ash/components/dbus/hiberman/hiberman_client_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "dbus/mock_bus.h"
 #include "dbus/mock_object_proxy.h"
 #include "dbus/object_path.h"
@@ -82,6 +83,15 @@ class HibermanClientTest : public testing::Test {
       // The Resume method should have an account_id string.
       ASSERT_TRUE(reader.PopString(&account_id));
       // There's no reply data for this method.
+    } else if (method_call->GetMember() ==
+               ::hiberman::kResumeFromHibernateASMethod) {
+      dbus::MessageReader reader(method_call);
+      const uint8_t* bytes = nullptr;
+      size_t length = 0;
+      // The ResumeFromHibernateAS method should have an auth_session_id byte
+      // array.
+      EXPECT_TRUE(reader.PopArrayOfBytes(&bytes, &length));
+      EXPECT_NE(length, static_cast<size_t>(0));
     } else {
       ASSERT_FALSE(true) << "Unrecognized member: " << method_call->GetMember();
     }
@@ -96,17 +106,21 @@ class HibermanClientTest : public testing::Test {
 };
 
 TEST_F(HibermanClientTest, ResumeFromHibernate) {
-  bool callback_called = false;
-  auto callback = base::BindOnce(
-      [](bool* callback_was_called,
-         bool method_call_success) {
-        *callback_was_called = true;
-      },
-      &callback_called);
-
-  client_->ResumeFromHibernate("test@google.com", std::move(callback));
+  base::test::TestFuture<bool> future;
+  client_->ResumeFromHibernate("test@google.com", future.GetCallback());
   base::RunLoop().RunUntilIdle();
-  ASSERT_TRUE(callback_called);
+  // Assert that the callback was called and that the method_call_success
+  // parameter returned true.
+  ASSERT_TRUE(future.Get<0>());
+}
+
+TEST_F(HibermanClientTest, ResumeFromHibernateAS) {
+  base::test::TestFuture<bool> future;
+  client_->ResumeFromHibernateAS("fake_auth_session_id", future.GetCallback());
+  base::RunLoop().RunUntilIdle();
+  // Assert that the callback was called and that the method_call_success
+  // parameter returned true.
+  ASSERT_TRUE(future.Get<0>());
 }
 
 }  // namespace ash
