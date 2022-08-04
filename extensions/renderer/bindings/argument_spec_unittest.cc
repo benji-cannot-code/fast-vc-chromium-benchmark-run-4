a2c5e28d1a1fd6ff999ef36686663b9dbd2c91e9
Tim Judkins <tjudkins@chromium.org>
2022-08-04 17:26:51 +0000

    [Extensions] Check for NaN and Infinity in ArgumentSpec
    This CL adds a new error message for when NaN or Infinity values are
    passed to API functions that are expecting the argument to be a number
    value. Also adds to a test to cover these cases.
    
    Bug: 1347116
    Change-Id: I3dce350355e750b6a4d0dec64c855d49719cbcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807987
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031549}

--- extensions/renderer/bindings/argument_spec_unittest.cc
+++ extensions/renderer/bindings/argument_spec_unittest.cc
@@ -33,6 +33,7 @@ using api_errors::kTypeObject;
 using api_errors::kTypeString;
 using api_errors::kTypeUndefined;
 using api_errors::MissingRequiredProperty;
+using api_errors::NumberIsNaNOrInfinity;
 
 using V8Validator = base::OnceCallback<void(v8::Local<v8::Value>)>;
 
@@ -210,6 +211,8 @@ TEST_F(ArgumentSpecUnitTest, Test) {
     ExpectFailure(spec, "'1'", InvalidType(kTypeInteger, kTypeString));
     ExpectFailure(spec, "({})", InvalidType(kTypeInteger, kTypeObject));
     ExpectFailure(spec, "[1]", InvalidType(kTypeInteger, kTypeList));
+    ExpectFailure(spec, "NaN", InvalidType(kTypeInteger, kTypeDouble));
+    ExpectFailure(spec, "Infinity", InvalidType(kTypeInteger, kTypeDouble));
   }
 
   {
@@ -226,6 +229,8 @@ TEST_F(ArgumentSpecUnitTest, Test) {
     ExpectFailure(spec, "'1.1'", InvalidType(kTypeDouble, kTypeString));
     ExpectFailure(spec, "({})", InvalidType(kTypeDouble, kTypeObject));
     ExpectFailure(spec, "[1.1]", InvalidType(kTypeDouble, kTypeList));
+    ExpectFailure(spec, "NaN", NumberIsNaNOrInfinity());
+    ExpectFailure(spec, "Infinity", NumberIsNaNOrInfinity());
   }
 
   {
