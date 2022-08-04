c72fc0ca105a2ad082646fc289614431d16c271c
Bartek Nowierski <bartekn@chromium.org>
2022-08-04 15:42:50 +0000

    [PA] Lower the GigaCage size on 64-bit Windows <8.1
    There is a number of ERROR_COMMITMENT_LIMIT crashes on Windows <8.1 when
    allocating a large GigaCage, hence we're reducing the GigaCage pool size
    from 8GB to 4GB. This may results in a slightly increase in OOMs due to
    VA space exhaustion, but nonetheless 4GB is significantly larger than
    what's available on 32-bit systems.
    
    This requires enabling dynamically sized GigaCage on Windows, which
    converts some constexpr to variables there. We can no longer make the
    decision at compile time, because there is only 1 release binary built
    for all Windows. Not ideal, but we expect the perf impact to be
    negligible, since the variables are placed in their own, write-once
    cacheline, which we expect to be hot. This benchmarks suggests that
    there should be no performance difference
    https://quick-bench.com/q/I6aiDNT2zgLkU8h6ZXLNtDaTfVA
    
    Bug: 1101421, 1217759
    Change-Id: Icf609979991cc3815481df9ae031329bdb018e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473273
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031524}

--- base/allocator/partition_allocator/partition_address_space.cc
+++ base/allocator/partition_allocator/partition_address_space.cc
@@ -26,7 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
+#if defined(PA_USE_DYNAMICALLY_SIZED_GIGA_CAGE)
+#include <VersionHelpers.h>  // For IsWindows8Point1OrGreater().
 #endif
+#endif  // BUILDFLAG(IS_WIN)
 
 namespace partition_alloc::internal {
 
@@ -118,10 +121,12 @@ PA_ALWAYS_INLINE size_t PartitionAddressSpace::BRPPoolSize() {
 }
 #else
 PA_ALWAYS_INLINE size_t PartitionAddressSpace::RegularPoolSize() {
-  return kRegularPoolSize;
+  return IsWindows8Point1OrGreater() ? kRegularPoolSize
+                                     : kRegularPoolSizeForLegacyWindows;
 }
 PA_ALWAYS_INLINE size_t PartitionAddressSpace::BRPPoolSize() {
-  return kBRPPoolSize;
+  return IsWindows8Point1OrGreater() ? kBRPPoolSize
+                                     : kBRPPoolSizeForLegacyWindows;
 }
 #endif  // BUILDFLAG(IS_IOS)
 #endif  // defined(PA_USE_DYNAMICALLY_SIZED_GIGA_CAGE)
