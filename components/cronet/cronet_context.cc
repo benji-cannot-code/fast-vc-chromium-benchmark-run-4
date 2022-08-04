810eeb3022bae25b0e115db6940baf193b88ad39
Chidera Olibie <colibie@google.com>
2022-08-04 09:32:19 +0000

    [cronet] Operation cleanup
    remove redundant dependency on //components/metrics
    Histogram manager refactor removed the need to directly depend
    on //components/metrics cc crrev/c/2159701
    
    Bug: b:226555152
    Change-Id: Ia722a6e17602b0eb000c607c0b70ecbf6ead5f62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776664
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031420}

--- components/cronet/cronet_context.cc
+++ components/cronet/cronet_context.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/message_loop/message_pump_type.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/metrics/statistics_recorder.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_restrictions.h"
