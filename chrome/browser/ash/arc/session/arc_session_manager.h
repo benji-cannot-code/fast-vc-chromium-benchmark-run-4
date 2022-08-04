d3c6ad115cc540d2eb45b3f852acd389df2f67c2
Ryo Hashimoto <hashimoto@chromium.org>
2022-08-04 02:16:03 +0000

    arc: Make ArcAndroidManagementChecker less confusing
    Code around ArcAndroidManagementChecker is confusing because it's
    difficult to figure out the difference between Android management and
    Chrome OS managed user.
    Also, what MANAGED returned from CheckResult() actually means is that
    the user is not a managed ChromeOS user, but Android management is
    enabled for them.
    
    Make this less confusing by:
    - Changing the return type of ArcAndroidManagementChecker::StartCheck().
    - Explicitly say "Android management" if it's not about Chrome OS managed
      users.
    - Rewrite some old comments (e.g. words like "commercial" are confusing
      these days because it has the same meaning as entierprise).
    
    BUG=b:241182687
    TEST=unit tests
    
    Change-Id: Ia6081f0b3946e35f562f469dad5e1f4a182e1b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806700
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031331}

--- chrome/browser/ash/arc/session/arc_session_manager.h
+++ chrome/browser/ash/arc/session/arc_session_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ash/arc/arc_support_host.h"
+#include "chrome/browser/ash/arc/policy/arc_android_management_checker.h"
 #include "chrome/browser/ash/arc/session/adb_sideloading_availability_delegate_impl.h"
 #include "chrome/browser/ash/arc/session/arc_app_id_provider_impl.h"
 #include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
@@ -42,7 +43,6 @@ constexpr const char kGeneratedBuildPropertyFilePath[] =
 constexpr const char kGeneratedCombinedPropertyFilePathVm[] =
     "/run/arcvm/host_generated/combined.prop";
 
-class ArcAndroidManagementChecker;
 class ArcDataRemover;
 class ArcDlcInstaller;
 class ArcFastAppReinstallStarter;
@@ -310,7 +310,7 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
 
   // Invokes OnBackgroundAndroidManagementChecked as if the check is done.
   void OnBackgroundAndroidManagementCheckedForTesting(
-      policy::AndroidManagementClient::Result result);
+      ArcAndroidManagementChecker::CheckResult result);
 
   void reset_property_files_expansion_result() {
     property_files_expansion_result_.reset();
@@ -365,7 +365,7 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // Called when the Android management check is done in opt-in flow or
   // OOBE flow.
   void OnAndroidManagementChecked(
-      policy::AndroidManagementClient::Result result);
+      ArcAndroidManagementChecker::CheckResult result);
 
   // Starts Android management check in background (in parallel with starting
   // ARC). This is for secondary or later ARC enabling.
@@ -379,7 +379,7 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // Called when the background Android management check is done. It is
   // triggered when the second or later ARC boot timing.
   void OnBackgroundAndroidManagementChecked(
-      policy::AndroidManagementClient::Result result);
+      ArcAndroidManagementChecker::CheckResult result);
 
   // Requests to start ARC instance. Also, updates the internal state to
   // ACTIVE.
