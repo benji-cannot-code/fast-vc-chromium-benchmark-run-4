6639b7ecbf3673bf45be2df3b1cd300258838741
Joanmarie Diggs <jdiggs@igalia.com>
2022-08-04 09:40:44 +0000

    AccessibilityChecker: Check for role, add details to name error text
    This commit causes the AccessibilityChecker to check that focusable
    Views have a valid role. It also adds text to the name error clarifying
    that the accessible name should be end-user consumable.
    
    Change-Id: I36c00fcead54e3435d09e56c9293ec6116c276c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804564
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031422}

--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -1743,6 +1743,8 @@ class DesktopAuraPaintWidgetTest : public DesktopWidgetTest {
   class ContentsView : public View {
     void GetAccessibleNodeData(ui::AXNodeData* node_data) override {
       node_data->SetNameExplicitlyEmpty();
+      // Focusable Views need a valid role.
+      node_data->role = ax::mojom::Role::kDialog;
     }
   };
 
