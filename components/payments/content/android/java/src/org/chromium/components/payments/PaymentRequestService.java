b76945c134a5e727d1759a85f40d841122aabfe2
Stephen McGruer <smcgruer@chromium.org>
2022-08-04 19:57:02 +0000

    [basic-card] Remove Android AutofillPaymentAppFactory
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. This CL deletes
    AutofillPaymentAppFactory.
    
    This CL will effectively disable basic-card entirely on Android, even
    when the flag is set. By no longer installing the factory, it will be
    impossible to get a basic-card experience. A long series of follow-on
    CLs will be needed to actually remove all the code.
    
    Bug: 1209835
    Change-Id: I959539c0f396298d7df9dc2dce45d565a77da345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792815
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031557}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
@@ -584,7 +584,6 @@ public class PaymentRequestService
 
     private void startPaymentAppService() {
         PaymentAppService service = mDelegate.getPaymentAppService();
-        mBrowserPaymentRequest.addPaymentAppFactories(service, /*delegate=*/this);
 
         String paymentAppServiceBridgeId = PaymentAppServiceBridge.class.getName();
         if (!service.containsFactory(paymentAppServiceBridgeId)) {
