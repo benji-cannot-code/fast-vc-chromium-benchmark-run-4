23dbb470b89ddd45fd04ec153b2a891cfc1888ff
Olesia Marukhno <olesiamarukhno@google.com>
2022-08-04 09:54:53 +0000

    [site data] Create PageSpecificSiteDataDialog
    When the feature is enabled, show PageSpecificSiteDataDialog instead of
    CollectedCookiesView. PageSpecificSiteDataDialog is empty for now.
    
    After showing the web modal, the widget ref is needed to store is as the
    user data for the current web contents. Update contrained_window API to
    return the widget.
    
    Bug: 1344787
    Change-Id: I8afb136c3c3227d1301090db1c4af85194023afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805330
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031430}

---
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/site_data/page_specific_site_data_dialog.h"
+
+#include "chrome/grit/generated_resources.h"
+#include "components/constrained_window/constrained_window_views.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/models/dialog_model.h"
+
+// static
+views::Widget* ShowPageSpecificSiteDataDialog(
+    content::WebContents* web_contents) {
+  auto builder = ui::DialogModel::Builder();
+  builder.SetTitle(
+      l10n_util::GetStringUTF16(IDS_COLLECTED_COOKIES_DIALOG_TITLE));
+  // TODO(crbug.com/1344787): Build the rest of the dialog. Add action handling.
+  // Remove the dialog from WebContentsUserData when destroyed.
+  return constrained_window::ShowWebModal(builder.Build(), web_contents);
+}
