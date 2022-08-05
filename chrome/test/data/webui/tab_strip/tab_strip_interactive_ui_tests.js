e1cd39604d30f6f32fd8615fe199bcdced9f5ec2
Tom Lukaszewicz <tluk@chromium.org>
2022-08-05 20:23:33 +0000

    [webui tab strip] Re-enable tab lists tests
    This CL re-enabled tab list tests for the WebUI Tab Strip. These
    test the interactive behavior of the WebUI Tab Strip and have thus
    been moved to the interactive ui test suite. This should help
    to avoid existing flaky behavior on the try-bots.
    
    Bug: 1247687
    Change-Id: I840cff0d047d11a083d60382506b2aeeffb562fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806342
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032088}

---
+++ chrome/test/data/webui/tab_strip/tab_strip_interactive_ui_tests.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+GEN_INCLUDE(['//chrome/test/data/webui/polymer_interactive_ui_test.js']);
+
+GEN('#include "content/public/test/browser_test.h"');
+
+var TabStripInteractiveUITest = class extends PolymerInteractiveUITest {
+  get isAsync() {
+    return true;
+  }
+
+  get webuiHost() {
+    return 'tab-strip.top-chrome';
+  }
+};
+
+var TabStripTabListTest = class extends TabStripInteractiveUITest {
+  get browsePreload() {
+    return 'chrome://tab-strip.top-chrome/test_loader.html?module=tab_strip/tab_list_test.js';
+  }
+};
+
+TEST_F('TabStripTabListTest', 'All', function() {
+  mocha.run();
+});
