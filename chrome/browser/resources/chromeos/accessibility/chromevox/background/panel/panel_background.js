c1fa9a7441b7323c1e9a5685733282e5713986ea
Anastasia Helfinstein <anastasi@google.com>
2022-08-05 22:55:46 +0000

    [ChromeVox] Use JS spread operator to simplify bridge code
    This change is a cleanup and pure refactor.
    
    R=josiahk
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: Id366d5eb7b11485423a351cd546d3cb50dd4921f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813538
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032155}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/panel_background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/panel_background.js
@@ -58,8 +58,7 @@ export class PanelBackground {
         () => PanelBackground.instance.destroyISearch_());
     BridgeHelper.registerHandler(
         Constants.TARGET, Constants.Action.FOCUS_TAB,
-        ({windowId, tabId}) =>
-            PanelTabMenuBackground.focusTab(windowId, tabId));
+        (windowId, tabId) => PanelTabMenuBackground.focusTab(windowId, tabId));
     BridgeHelper.registerHandler(
         Constants.TARGET, Constants.Action.GET_ACTIONS_FOR_CURRENT_NODE,
         () => PanelBackground.instance.getActionsForCurrentNode_());
@@ -68,7 +67,7 @@ export class PanelBackground {
         () => PanelTabMenuBackground.getTabMenuData());
     BridgeHelper.registerHandler(
         Constants.TARGET, Constants.Action.INCREMENTAL_SEARCH,
-        ({searchStr, dir, opt_nextObject}) =>
+        (searchStr, dir, opt_nextObject) =>
             PanelBackground.instance.incrementalSearch_(
                 searchStr, dir, opt_nextObject));
     BridgeHelper.registerHandler(
--- chrome/browser/resources/chromeos/accessibility/chromevox/common/background_bridge.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/background_bridge.js
@@ -83,7 +83,7 @@ BackgroundBridge.ChromeVoxPrefs = {
   async setLoggingPrefs(key, value) {
     return BridgeHelper.sendMessage(
         BridgeConstants.ChromeVoxPrefs.TARGET,
-        BridgeConstants.ChromeVoxPrefs.Action.SET_LOGGING_PREFS, {key, value});
+        BridgeConstants.ChromeVoxPrefs.Action.SET_LOGGING_PREFS, key, value);
   },
 
   /**
@@ -95,7 +95,7 @@ BackgroundBridge.ChromeVoxPrefs = {
   async setPref(key, value) {
     return BridgeHelper.sendMessage(
         BridgeConstants.ChromeVoxPrefs.TARGET,
-        BridgeConstants.ChromeVoxPrefs.Action.SET_PREF, {key, value});
+        BridgeConstants.ChromeVoxPrefs.Action.SET_PREF, key, value);
   },
 };
 
@@ -171,7 +171,7 @@ BackgroundBridge.EventStreamLogger = {
         BridgeConstants.EventStreamLogger.TARGET,
         BridgeConstants.EventStreamLogger.Action
             .NOTIFY_EVENT_STREAM_FILTER_CHANGED,
-        {name, enabled});
+        name, enabled);
   },
 };
 
@@ -243,7 +243,7 @@ BackgroundBridge.PanelBackground = {
   async focusTab(windowId, tabId) {
     return BridgeHelper.sendMessage(
         BridgeConstants.PanelBackground.TARGET,
-        BridgeConstants.PanelBackground.Action.FOCUS_TAB, {windowId, tabId});
+        BridgeConstants.PanelBackground.Action.FOCUS_TAB, windowId, tabId);
   },
 
   /**
@@ -274,8 +274,8 @@ BackgroundBridge.PanelBackground = {
   async incrementalSearch(searchStr, dir, opt_nextObject) {
     return BridgeHelper.sendMessage(
         BridgeConstants.PanelBackground.TARGET,
-        BridgeConstants.PanelBackground.Action.INCREMENTAL_SEARCH,
-        {searchStr, dir, opt_nextObject});
+        BridgeConstants.PanelBackground.Action.INCREMENTAL_SEARCH, searchStr,
+        dir, opt_nextObject);
   },
 
   /**
