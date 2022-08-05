789de52bc67da4a457ef9c03a421964650e7b998
Allen Bauer <kylixrd@chromium.org>
2022-08-05 22:40:27 +0000

    Moved dark mode support initialization to later in the startup.
    This moves the initial call into chrome.dll. It was previously in chrome.exe, which would cause two separate instances of the underlying dark mode support structure.
    
    Bug: 1349903
    Change-Id: Ic065ce92d483ef1fee1d7a8cb3ffde051e25e305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812282
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032149}

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/win/current_module.h"
-#include "base/win/dark_mode_support.h"
 #include "base/win/registry.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
@@ -385,9 +384,6 @@ int main() {
   // Only enable High DPI support for browser and GPU process.
   if (process_type.empty() || process_type == switches::kGpuProcess)
     base::win::EnableHighDPISupport();
-  // Only enable dark mode support for the browser process.
-  if (process_type.empty())
-    base::win::AllowDarkModeForApp(true);
 
   if (AttemptFastNotify(*command_line))
     return 0;
