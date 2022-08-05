273de30a52e9f257d1cc5b45ecb531c31b520db6
Florian Gauger <fga@google.com>
2022-08-05 16:54:01 +0000

    Check initiator origin to enable Autofill Assistant
    Tested:
    - [showtimes sf] on google.com
    - [showtimes sao paulo] on google.com
    - [showtimes london] on google.co.uk
    - [covid vaccine] on google.co.in (download vaccine)
    
    Bug: 1339247
    Change-Id: I6031fb48aec3c4e517e9d7cf52080f46720beeb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3773556
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sergei Glazunov <glazunov@google.com>
    Auto-Submit: Florian Gauger <fga@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031966}

--- components/external_intents/android/external_intents_features.h
+++ components/external_intents/android/external_intents_features.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace external_intents {
 
 // Alphabetical:
+extern const base::Feature kAutofillAssistantGoogleInitiatorOriginCheck;
 extern const base::Feature kScaryExternalNavigationRefactoring;
 
 }  // namespace external_intents
