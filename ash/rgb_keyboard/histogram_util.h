626a07f2efe029b87d14f5d51f836463d5b6da60
David Padlipsky <dpad@google.com>
2022-08-05 21:32:16 +0000

    RGB Keyboard: Switch kFourZoneFifteenLed support to kFourZoneFourLed
    Updates metric enum and tests to use kFourZoneFourLed instead of
    kFourZoneFifteenLed.
    
    Bug: b/214077425
    Test: ash_unittests
    Change-Id: I7f70382ff1e59cadf05b51c0e1d67aeba6c18eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805442
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032118}

--- ash/rgb_keyboard/histogram_util.h
+++ ash/rgb_keyboard/histogram_util.h
@@ -27,8 +27,8 @@ enum class RgbKeyboardCapabilityType {
   kIndividualKey = 1,
   kFourZoneFortyLed = 2,
   kFourZoneTwelveLed = 3,
-  kFourZoneFifteenLed = 4,
-  kMaxValue = kFourZoneFifteenLed,
+  kFourZoneFourLed = 4,
+  kMaxValue = kFourZoneFourLed,
 };
 
 enum class RgbKeyboardBacklightChangeType {
