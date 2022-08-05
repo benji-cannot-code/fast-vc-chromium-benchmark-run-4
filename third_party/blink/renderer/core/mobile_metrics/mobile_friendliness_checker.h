1114b9bbfb0c7a26b2ca714dfcd4beaa411598de
Hiroki Kumazaki <kumagi@google.com>
2022-08-05 09:12:18 +0000

    Map paint_rect from the local transform space to the viewport space.
    Add a offset transform based on PaintController by LocalFlameView.
    
    Bug: 1113750
    Change-Id: Id07edbd0227145b917e192bd737c489a859eb1c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784766
    Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031828}

--- third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
+++ third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
@@ -19,6 +19,7 @@ namespace blink {
 
 class Document;
 class LocalFrameView;
+class TransformPaintPropertyNodeOrAlias;
 struct ViewportDescription;
 
 // Calculates the mobile usability of current page, especially friendliness on
@@ -40,8 +41,13 @@ class CORE_EXPORT MobileFriendlinessChecker
   void NotifyPaintEnd();
   void WillBeRemovedFromFrame();
   void NotifyViewportUpdated(const ViewportDescription&);
-  void NotifyPaintTextFragment(const PhysicalRect& paint_rect, int font_size);
-  void NotifyPaintReplaced(const PhysicalRect& paint_rect);
+  void NotifyPaintTextFragment(
+      const PhysicalRect& paint_rect,
+      int font_size,
+      const TransformPaintPropertyNodeOrAlias& current_transform);
+  void NotifyPaintReplaced(
+      const PhysicalRect& paint_rect,
+      const TransformPaintPropertyNodeOrAlias& current_transform);
 
   void Trace(Visitor* visitor) const override;
 
@@ -88,10 +94,16 @@ class CORE_EXPORT MobileFriendlinessChecker
   int ComputeBadTapTargetsRatio();
 
   void UpdateTextAreaSizes(const PhysicalRect& text_rect, int font_size);
-  void UpdateBeyondViewportAreaSizes(const PhysicalRect& paint_rect);
+  void UpdateBeyondViewportAreaSizes(
+      const PhysicalRect& paint_rect,
+      const TransformPaintPropertyNodeOrAlias& current_transform);
 
  private:
   Member<LocalFrameView> frame_view_;
+  const TransformPaintPropertyNodeOrAlias* viewport_transform_ = nullptr;
+  const TransformPaintPropertyNodeOrAlias* previous_transform_ = nullptr;
+  float current_x_offset_ = 0.0;
+  float viewport_width_ = 0.0;
   HeapTaskRunnerTimer<MobileFriendlinessChecker> timer_;
   double viewport_scalar_;
   double initial_scale_ = 1.0;
