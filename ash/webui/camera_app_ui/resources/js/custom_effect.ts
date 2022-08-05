e62d5dcba90ac22eb8ca408a0622ba4592cbaf09
Wei Lee <wtlee@google.com>
2022-08-05 08:30:26 +0000

    CCA: Speak the message out when showing the toast message
    By default, ChromeVox won't speak it if it is not focusable.
    Therefore, this CL explicitly speak the toast message out.
    
    Bug: b:226262670
    Test: Manually
    Change-Id: Ie3fcc1e1b418bdf609eae4ba905419a79b157ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812974
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031817}

--- ash/webui/camera_app_ui/resources/js/custom_effect.ts
+++ ash/webui/camera_app_ui/resources/js/custom_effect.ts
@@ -9,6 +9,7 @@ import * as dom from './dom.js';
 import {I18nString} from './i18n_string.js';
 import * as loadTimeData from './models/load_time_data.js';
 import {ChromeHelper} from './mojo/chrome_helper.js';
+import {speakMessage} from './spoken_msg.js';
 import * as state from './state.js';
 import * as util from './util.js';
 
@@ -206,6 +207,7 @@ class Toast {
       protected readonly anchor: HTMLElement,
       protected readonly template: DocumentFragment,
       protected readonly toast: HTMLDivElement,
+      protected readonly message: string,
       protected readonly positionInfos: PositionInfos) {
     this.cancelHandle = setInterval(() => {
       updatePositions(anchor, positionInfos);
@@ -215,6 +217,7 @@ class Toast {
 
   show(): void {
     document.body.appendChild(this.template);
+    speakMessage(this.message);
   }
 
   focus(): void {
@@ -246,7 +249,7 @@ class NewFeatureToast extends Toast {
         loadTimeData.getI18nMessage(I18nString.NEW_CONTROL_NAVIGATION, text);
     toast.setAttribute('aria-label', ariaLabel);
 
-    super(anchor, template, toast, [{
+    super(anchor, template, toast, text, [{
             target: toast,
             properties: getOffsetProperties(anchor, 'toast'),
           }]);
@@ -277,7 +280,7 @@ class IndicatorToast extends Toast {
 
     const indicatorDot =
         dom.getFrom(template, '#indicator-dot', HTMLDivElement);
-    super(anchor, template, toast, [
+    super(anchor, template, toast, text, [
       {
         target: toast,
         properties: getOffsetProperties(anchor, 'toast'),
