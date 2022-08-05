444a852b04802fdfd94d64b668aedd77c007e1cf
Leonid Baraz <lbaraz@chromium.org>
2022-08-05 21:17:53 +0000

    Refactor ReportQueueProvider
    The provider has been greatly simplified.
    Removed use of SharedQueue.
    Added a lot more testing.
    
    Bug: b:241279100
    Change-Id: I0661af689e32295dfe2c038b10c3d33e49abeb5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807116
    Reviewed-by: Hong Xu <xuhong@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032113}

--- components/reporting/client/mock_report_queue_provider.cc
+++ components/reporting/client/mock_report_queue_provider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/task/thread_pool.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/reporting/client/mock_report_queue.h"
--- components/reporting/client/report_queue_factory.cc
+++ components/reporting/client/report_queue_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/strings/string_piece.h"
 #include "base/task/bind_post_task.h"
+#include "base/task/thread_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/reporting/client/report_queue_configuration.h"
 #include "components/reporting/client/report_queue_provider.h"
