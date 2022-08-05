c62d9774b26ed4fd18960f23067a00f68bc17aba
Jeroen Dhollander <jeroendh@google.com>
2022-08-05 16:56:44 +0000

    Fix clangd unused-includes warning for gmock files
    The gmock wrappers are transiently exporting the actual gmock headers,
    which caused the clangd linter to raise the "unused include" warning
    wherever any of these headers were used.
    
    By adding the IWYU pragma clangd stops complaining.
    
    Bug: N/A
    Test: compiled
    Change-Id: I39d19352c170ef9f5c005bb21c03ad359371e436
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810482
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031968}

--- testing/gmock/include/gmock/gmock.h
+++ testing/gmock/include/gmock/gmock.h
@@ -11,6 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // and testing/gmock, instead of directly including files in
 // third_party/googletest.
 
-#include "third_party/googletest/src/googlemock/include/gmock/gmock.h"
+#include "third_party/googletest/src/googlemock/include/gmock/gmock.h"  // IWYU pragma: export
 
 #endif  // TESTING_GMOCK_INCLUDE_GMOCK_GMOCK_H_
