7e88eda33e16fa342414d30194dbc44f8592e171
David Munro <davidmunro@google.com>
2022-08-05 02:35:45 +0000

    guest_os: Session tracker stays in sync through Chrome crashes
    We need to track some state in order to handle home directory sharing in
    the files app, but we lose all that state if Chrome crashes. Use the new
    ListRunningContainers and GetGarconSessionInfo methods to get back in
    sync after a crash, which allows Crostini containers (once
    http://crrev/c/3801773 lands), to still share files after a Chrome
    crash.
    
    Bug: b:217469540
    Test: Unit, deploy, crash Chrome and remount (with above CL)
    
    Change-Id: I1ee8a55e0cf14ceac81dce1a2a18dd2e2e41c2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802295
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031748}

--- chromeos/ash/components/dbus/cicerone/fake_cicerone_client.h
+++ chromeos/ash/components/dbus/cicerone/fake_cicerone_client.h
@@ -160,6 +160,14 @@ class COMPONENT_EXPORT(CICERONE) FakeCiceroneClient : public CiceroneClient {
           callback) override;
   void FileSelected(
       const vm_tools::cicerone::FileSelectedSignal& signal) override;
+  void ListRunningContainers(
+      const vm_tools::cicerone::ListRunningContainersRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::ListRunningContainersResponse>
+          callback) override;
+  void GetGarconSessionInfo(
+      const vm_tools::cicerone::GetGarconSessionInfoRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::GetGarconSessionInfoResponse>
+          callback) override;
   void WaitForServiceToBeAvailable(
       dbus::ObjectProxy::WaitForServiceToBeAvailableCallback callback) override;
 
@@ -362,6 +370,17 @@ class COMPONENT_EXPORT(CICERONE) FakeCiceroneClient : public CiceroneClient {
     detach_usb_from_container_response_ =
         std::move(detach_usb_from_container_response);
   }
+  void set_list_containers_response(
+      vm_tools::cicerone::ListRunningContainersResponse
+          list_container_response) {
+    list_containers_response_ = std::move(list_container_response);
+  }
+  void set_get_garcon_session_info_response(
+      vm_tools::cicerone::GetGarconSessionInfoResponse
+          get_garcon_session_info_response) {
+    get_garcon_session_info_response_ =
+        std::move(get_garcon_session_info_response);
+  }
 
   void set_send_container_starting_signal_delay(base::TimeDelta delay) {
     send_container_starting_signal_delay_ = delay;
@@ -523,6 +542,9 @@ class COMPONENT_EXPORT(CICERONE) FakeCiceroneClient : public CiceroneClient {
       attach_usb_to_container_response_;
   vm_tools::cicerone::DetachUsbFromContainerResponse
       detach_usb_from_container_response_;
+  vm_tools::cicerone::ListRunningContainersResponse list_containers_response_;
+  vm_tools::cicerone::GetGarconSessionInfoResponse
+      get_garcon_session_info_response_;
 
   base::TimeDelta send_container_starting_signal_delay_;
   base::TimeDelta send_container_started_signal_delay_;
