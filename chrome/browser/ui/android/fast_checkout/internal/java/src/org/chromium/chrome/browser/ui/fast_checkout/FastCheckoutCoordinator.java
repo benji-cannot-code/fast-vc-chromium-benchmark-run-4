eaf714a83e1df98dd108804737d8ce82967b4547
Norge Vizcay <vizcay@google.com>
2022-08-05 17:48:48 +0000

    [FastCheckout] Add FastCheckout UI Coordinator and basic MVC structure.
    The CL introduces the basic MVC folder structure on the Java side of Fast Checkout UI. The CL also includes the logic for showing and dismissing the bottom sheet.
    
    In principle we follow the invariant "If bottom sheet can't be immediately shown, then dismiss". The only case we skip this principle is if the consent screen is still showing, in which case we wait until it's hidden to show the FC sheet.
    
    Context:
    UI Proposal: go/fast-checkout-ux-m2
    DD: go/fast-checkout-dd
    PRD: go/fast-checkout-prd
    
    Bug: 1334642
    Change-Id: Ieb350ad14c4ccd310952dc912a053d4b8ac7da9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790916
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031991}

--- chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutCoordinator.java
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutCoordinator.java
@@ -6,21 +6,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.fast_checkout;
 
 import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import org.chromium.chrome.browser.ui.fast_checkout.data.FastCheckoutAutofillProfile;
 import org.chromium.chrome.browser.ui.fast_checkout.data.FastCheckoutCreditCard;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
 class FastCheckoutCoordinator implements FastCheckoutComponent {
+    private FastCheckoutMediator mMediator = new FastCheckoutMediator();
+
     @Override
     public void initialize(Context context, BottomSheetController sheetController,
             FastCheckoutComponent.Delegate delegate) {
-        // TODO(crbug.com/1334642): Implement.
+        PropertyModel model = FastCheckoutModel.createDefaultModel(mMediator::onDismissed);
+        mMediator.initialize(delegate, model, sheetController);
+
+        LinearLayout rootView = new LinearLayout(context);
+        rootView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        rootView.setOrientation(LinearLayout.VERTICAL);
+
+        // TODO(crbug.com/1334642): Create views for all 3 screens.
+
+        setUpModelChangeProcessors(model, new FastCheckoutView(rootView, sheetController));
     }
 
     @Override
     public void showOptions(
             FastCheckoutAutofillProfile[] profiles, FastCheckoutCreditCard[] creditCards) {
-        // TODO(crbug.com/1334642): Implement.
+        mMediator.showOptions(profiles, creditCards);
+    }
+
+    static void setUpModelChangeProcessors(PropertyModel model, FastCheckoutView view) {
+        PropertyModelChangeProcessor.create(
+                model, view, FastCheckoutViewBinder::bindFastCheckoutView);
     }
 }
