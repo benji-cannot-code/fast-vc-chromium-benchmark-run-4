baf5d65da4d27e2151be08e60ad1c40119135d5f
EmiliaPaz <emiliapaz@chromium.org>
2022-08-05 20:15:20 +0000

    [Extensions c2s] Add permissions page option to context menu
    Under the feature extensions-menu-access-control, this CL adds:
    - New context menu entry that opens chrome://extensions/sitePermissions
    - Updated strings for the page access submenu options
    
    Bug: 1306679
    Change-Id: I052bd630bc5047616e73272be0a31ebcd7cc2116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790242
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032080}

--- chrome/browser/extensions/chrome_extension_browser_constants.cc
+++ chrome/browser/extensions/chrome_extension_browser_constants.cc
@@ -11,4 +11,8 @@ namespace chrome_extension_constants {
 const char kRuntimeHostPermissionsHelpURL[] =
     "https://support.google.com/chrome?p=enable_extensions";
 
+// The link to the site permissions settings page.
+const char kExtensionsSitePermissionsURL[] =
+    "chrome://extensions/sitePermissions";
+
 }  // namespace chrome_extension_constants
