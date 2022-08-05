170d7a786c3af9ace0303e8f7cc7a405a69eed69
Shuran Huang <shuuran@chromium.org>
2022-08-05 15:26:09 +0000

    Return early when FPSs is disabled for the browser context.
    Answer queries with default result directly in FirstPartySetsAccessDelegate without delegating queries to
    FirstPartySetsManager when the given browser context disables FPSs.
    We didn't do this previously because we still want to collect metrics
    in FirstPartySetsManager. That metric got removed in
    https://crrev.com/c/3791949.
    
    This change also simplifies unit tests.
    
    Bug: 1325050
    
    Change-Id: I5f7b45b254b646e23c57bbc021f81db366d073f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810552
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031922}

--- services/network/first_party_sets/first_party_sets_access_delegate.cc
+++ services/network/first_party_sets/first_party_sets_access_delegate.cc
@@ -51,6 +51,10 @@ FirstPartySetsAccessDelegate::ComputeMetadata(
     const std::set<net::SchemefulSite>& party_context,
     base::OnceCallback<void(net::FirstPartySetMetadata)> callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (!context_config_.is_enabled()) {
+    return {net::FirstPartySetMetadata()};
+  }
   if (pending_queries_) {
     // base::Unretained() is safe because `this` owns `pending_queries_` and
     // `pending_queries_` will not run the enqueued callbacks after `this` is
@@ -72,6 +76,11 @@ FirstPartySetsAccessDelegate::FindOwner(
     base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnerResult)>
         callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (!context_config_.is_enabled()) {
+    return absl::make_optional<FirstPartySetsManager::OwnerResult>(
+        absl::nullopt);
+  }
   if (pending_queries_) {
     // base::Unretained() is safe because `this` owns `pending_queries_` and
     // `pending_queries_` will not run the enqueued callbacks after `this` is
@@ -91,6 +100,10 @@ FirstPartySetsAccessDelegate::FindOwners(
     base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnersResult)>
         callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (!context_config_.is_enabled())
+    return {{}};
+
   if (pending_queries_) {
     // base::Unretained() is safe because `this` owns `pending_queries_` and
     // `pending_queries_` will not run the enqueued callbacks after `this` is
@@ -110,6 +123,8 @@ void FirstPartySetsAccessDelegate::ComputeMetadataAndInvoke(
     const std::set<net::SchemefulSite>& party_context,
     base::OnceCallback<void(net::FirstPartySetMetadata)> callback) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(context_config_.is_enabled());
+
   std::pair<base::OnceCallback<void(net::FirstPartySetMetadata)>,
             base::OnceCallback<void(net::FirstPartySetMetadata)>>
       callbacks = base::SplitOnceCallback(std::move(callback));
@@ -128,6 +143,8 @@ void FirstPartySetsAccessDelegate::FindOwnerAndInvoke(
     base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnerResult)>
         callback) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(context_config_.is_enabled());
+
   std::pair<base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnerResult)>,
             base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnerResult)>>
       callbacks = base::SplitOnceCallback(std::move(callback));
@@ -144,6 +161,8 @@ void FirstPartySetsAccessDelegate::FindOwnersAndInvoke(
     base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnersResult)>
         callback) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(context_config_.is_enabled());
+
   std::pair<
       base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnersResult)>,
       base::OnceCallback<void(FirstPartySetsAccessDelegate::OwnersResult)>>
