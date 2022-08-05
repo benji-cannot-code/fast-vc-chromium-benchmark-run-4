a7c7cef55c9d80882017f63d9a01bc8f652ddbd9
keno <kenoss@chromium.org>
2022-08-05 07:51:58 +0000

    PageLoadMetrics: Fix subframe judge in LargestContentfulPaintHandler for FencedFrames
    Before MPArch, LargestContentfulPaintHandler can judge that the given
    frame is a subframe or not using RenderFrameHost.GetParent. Under
    MPArch, LargestContentfulPaintHandler manages LCPs for all frames under
    the root frame bound to PageLoadTracker and can't use the above method.
    
    This CL makes PageLoadTracker explicitly tell
    LargestContentfulPaintHandler that the frame is the root frame under
    management.
    
    This CL adds RecordMainFrameTimingAux, which will be removed in another
    refctooring CL.
    
    This bug and fix are originally found by jxck@chromium.org.
    
    Bug: 1317494
    Change-Id: I9ed7430129f8d428caae7554600fec740860f2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805024
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031811}

--- components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.h
+++ components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.h
@@ -119,7 +119,12 @@ class LargestContentfulPaintHandler {
       ContentfulPaintTimingInfo::LargestContentTextOrImage*
           largest_content_type);
 
-  void RecordTiming(
+  void RecordMainFrameTiming(
+      const page_load_metrics::mojom::LargestContentfulPaintTiming&
+          largest_contentful_paint,
+      const absl::optional<base::TimeDelta>&
+          first_input_or_scroll_notified_timestamp);
+  void RecordSubFrameTiming(
       const page_load_metrics::mojom::LargestContentfulPaintTiming&
           largest_contentful_paint,
       const absl::optional<base::TimeDelta>&
@@ -161,7 +166,7 @@ class LargestContentfulPaintHandler {
   void OnSubFrameDeleted(int frame_tree_node_id);
 
  private:
-  void RecordSubframeTiming(
+  void RecordSubFrameTimingInternal(
       const page_load_metrics::mojom::LargestContentfulPaintTiming&
           largest_contentful_paint,
       const absl::optional<base::TimeDelta>&
@@ -173,7 +178,7 @@ class LargestContentfulPaintHandler {
       const page_load_metrics::mojom::LargestContentfulPaintTiming&
           largest_contentful_paint,
       const base::TimeDelta& navigation_start_offset);
-  void RecordMainFrameTiming(
+  void RecordMainFrameTimingInternal(
       const page_load_metrics::mojom::LargestContentfulPaintTiming&
           largest_contentful_paint,
       const absl::optional<base::TimeDelta>&
