40563b5a0faa58bc8d310ce7f13729d161682d8e
Jonathan Lee <jonathanjlee@google.com>
2022-08-05 02:18:24 +0000

    [blinkpy] Prevent propagation of bad logs in wptserve tests
    Some wptserve unit tests failed flakily because a worker object's
    destruction could pollute the root logger's output. This change silences
    the irrelevant logs and reenables the disabled tests.
    
    Bug: 1347706, 1308877
    Test: ./run_blinkpy_tests.py --repeat=20
    Change-Id: I918b1117738e3190b927420166d237d6a86db23f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811185
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031719}

--- third_party/blink/tools/blinkpy/common/system/log_testing.py
+++ third_party/blink/tools/blinkpy/common/system/log_testing.py
@@ -113,8 +113,8 @@ class LogTesting(object):
         # root logger seems to suffice.
         return logging.getLogger()
 
-    @staticmethod
-    def setUp(test_case, logging_level=logging.INFO):
+    @classmethod
+    def setUp(cls, test_case, logging_level=logging.INFO):
         """Configures logging for unit testing.
 
         Configures the root logger to log to a testing log stream.
@@ -147,7 +147,7 @@ class LogTesting(object):
         # logger.setLevel().  This ensures that we have not interfered
         # with how the code being tested may have configured the root
         # logger.
-        logger = LogTesting._getLogger()
+        logger = cls._getLogger()
         logger.setLevel(logging_level)
         logger.addHandler(handler)
 
