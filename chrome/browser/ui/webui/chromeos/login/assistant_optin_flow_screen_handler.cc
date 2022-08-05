8a55c5c012823827904571fa96d93037a7cf57ef
Danila Kuzmin <dkuzmin@google.com>
2022-08-05 14:36:23 +0000

    [AssistantOptInFlow]: Prevent usage of non-existing objects
    Bug: 1348415
    Change-Id: Ie77f317f1178314c8628e38ba7d090820219fdda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810361
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031902}

--- chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
@@ -292,6 +292,7 @@ void AssistantOptInFlowScreenHandler::ShowNextScreen() {
 void AssistantOptInFlowScreenHandler::OnActivityControlOptInResult(
     bool opted_in) {
   Profile* profile = ProfileManager::GetActiveUserProfile();
+  CHECK(!pending_consent_data_.empty());
   auto data = pending_consent_data_.front();
   pending_consent_data_.pop_front();
   RecordActivityControlConsent(profile, data.ui_audit_key, opted_in,
@@ -299,6 +300,7 @@ void AssistantOptInFlowScreenHandler::OnActivityControlOptInResult(
   if (opted_in) {
     has_opted_in_any_consent_ = true;
     RecordAssistantActivityControlOptInStatus(data.setting_type, opted_in);
+    CHECK(assistant::AssistantSettings::Get());
     assistant::AssistantSettings::Get()->UpdateSettings(
         GetSettingsUiUpdate(data.consent_token).SerializeAsString(),
         base::BindOnce(
@@ -364,6 +366,7 @@ void AssistantOptInFlowScreenHandler::SendGetSettingsRequest() {
   }
 
   assistant::SettingsUiSelector selector = GetSettingsUiSelector();
+  CHECK(assistant::AssistantSettings::Get());
   assistant::AssistantSettings::Get()->GetSettingsWithHeader(
       selector.SerializeAsString(),
       base::BindOnce(&AssistantOptInFlowScreenHandler::OnGetSettingsResponse,
@@ -374,6 +377,7 @@ void AssistantOptInFlowScreenHandler::SendGetSettingsRequest() {
 void AssistantOptInFlowScreenHandler::StopSpeakerIdEnrollment() {
   DCHECK(voice_match_enrollment_started_);
   voice_match_enrollment_started_ = false;
+  CHECK(assistant::AssistantSettings::Get());
   assistant::AssistantSettings::Get()->StopSpeakerIdEnrollment();
   // Reset the mojom receiver of |SpeakerIdEnrollmentClient|.
   ResetReceiver();
@@ -615,6 +619,7 @@ void AssistantOptInFlowScreenHandler::HandleVoiceMatchScreenUserAction(
 
     DCHECK(!voice_match_enrollment_started_);
     voice_match_enrollment_started_ = true;
+    CHECK(assistant::AssistantSettings::Get());
     assistant::AssistantSettings::Get()->StartSpeakerIdEnrollment(
         flow_type_ == ash::FlowType::kSpeakerIdRetrain,
         weak_factory_.GetWeakPtr());
