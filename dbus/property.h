a37fd3f25a9fa5642b672535fc5655a6c08944a2
Lei Zhang <thestig@chromium.org>
2022-08-05 23:44:08 +0000

    Fix typos for the word "propagated".
    Fix this typo across the code base to quell Tricium SpellChecker
    warnings.
    
    Change-Id: I8d1faee3117d121a0b16e857843fb1cbe8252858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810768
    Owners-Override: Erik Staab <estaab@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032172}

--- dbus/property.h
+++ dbus/property.h
@@ -98,7 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // This now allows code using the client implementation to access properties
 // in a type-safe manner, and assuming the PropertyChanged callback is
-// propogated up to observers, be notified of changes. A typical access of
+// propagated up to observers, be notified of changes. A typical access of
 // the current value of the name property would be:
 //
 //   ExampleClient::Properties* p = example_client->GetProperties(object_path);
