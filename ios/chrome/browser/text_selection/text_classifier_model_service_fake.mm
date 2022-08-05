0c8168a2ee8fcb499622202fca87e18fc1ab852b
Zhixiang Teoh <zhteoh@google.com>
2022-08-05 19:19:27 +0000

    [iOS] Add test support for ExpKitCalendar TextClassifier metrics tests
    Add TextClassifierModelServiceFake for use in tests (via TestChromeBrowserState::AddTestingFactory). See also https://crrev.com/i/4911041.
    
    Bug: 1345426
    Test: out/Debug-iphonesimulator/iossim -d 'iPhone 13 Pro' -c --gtest_filter="InternalContextMenuProviderTest.*" out/Debug-iphonesimulator/ios_internal_chrome_unittests.app
    Change-Id: I4b9d91d9335e49d16be53d4a2db6bf6e98e13079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3803091
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Zhixiang Teoh <zhteoh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032045}

---
+++ ios/chrome/browser/text_selection/text_classifier_model_service_fake.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/text_selection/text_classifier_model_service_fake.h"
+
+#import <memory>
+
+#import "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/optimization_guide/optimization_guide_service.h"
+#import "ios/chrome/browser/optimization_guide/optimization_guide_service_factory.h"
+#import "ios/chrome/browser/text_selection/text_classifier_model_service.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// static
+std::unique_ptr<KeyedService>
+TextClassifierModelServiceFake::CreateTextClassifierModelService(
+    web::BrowserState* context) {
+  ChromeBrowserState* browser_state =
+      ChromeBrowserState::FromBrowserState(context);
+  auto* opt_guide =
+      OptimizationGuideServiceFactory::GetForBrowserState(browser_state);
+  auto service =
+      base::WrapUnique(new TextClassifierModelServiceFake(opt_guide));
+  return service;
+}
+
+TextClassifierModelServiceFake::~TextClassifierModelServiceFake() {}
+
+TextClassifierModelServiceFake::TextClassifierModelServiceFake(
+    OptimizationGuideService* opt_guide)
+    : TextClassifierModelService(opt_guide) {}
