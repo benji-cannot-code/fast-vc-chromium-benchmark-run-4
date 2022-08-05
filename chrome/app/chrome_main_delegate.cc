d1b311cd9282658a0cbef19744c0f4ede7ec002d
Etienne Bergeron <etienneb@chromium.org>
2022-08-05 16:59:04 +0000

    Remove useless semicolon statement
    I introduced this line by mistake.
      https://chromium-review.googlesource.com/c/chromium/src/+/3750909
    
    No code change.
    
    R=rockot@chromium.org
    
    Change-Id: I8af666757df617383a0aedb51ae35586b779efe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813674
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Auto-Submit: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031969}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -619,7 +619,6 @@ absl::optional<int> ChromeMainDelegate::PostEarlyInitialization(
   if (!invoked_in_browser) {
     CommonEarlyInitialization();
     return absl::nullopt;
-    ;
   }
 
 #if BUILDFLAG(IS_WIN)
