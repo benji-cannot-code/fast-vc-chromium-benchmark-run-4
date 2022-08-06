3e45708781ee61cc23aed91c4a190e89278266c0
Nancy Wang <nancylingwang@chromium.org>
2022-08-06 13:23:34 +0000

    Implement the non mojom SetPermission for ArcApps.
    Modify the mojom SetPermission to use the non mojom interface.
    
    BUG=1253250
    
    Change-Id: I917c7e8c6d22d79ef8957835a7a056d1931f7ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811017
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032280}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
 #include "components/services/app_service/public/cpp/intent.h"
+#include "components/services/app_service/public/cpp/permission.h"
 #include "components/services/app_service/public/cpp/publisher_base.h"
 #include "components/services/app_service/public/mojom/app_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -122,6 +123,7 @@ class ArcApps : public KeyedService,
   void LaunchShortcut(const std::string& app_id,
                       const std::string& shortcut_id,
                       int64_t display_id) override;
+  void SetPermission(const std::string& app_id, PermissionPtr permission);
   void OnPreferredAppSet(
       const std::string& app_id,
       IntentFilterPtr intent_filter,
