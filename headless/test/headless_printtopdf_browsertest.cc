5eb12e9a45e26b7aefdf8805ae481687ff3dca6b
Peter Kvitek <kvitekp@chromium.org>
2022-08-06 00:55:35 +0000

    [headless] Added print compositor support for OOPIF printing.
    This CL consolidates headless and headful Page.printToPDF()
    handling into a single class and adds PDF composition support
    to allow OOPIF printing to PDF.
    
    Bug: 1348323
    Change-Id: If7206d84fa8cb725de8d9af8651169bb62c2849c
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805932
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032200}

--- headless/test/headless_printtopdf_browsertest.cc
+++ headless/test/headless_printtopdf_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/size_f.h"
 
 namespace headless {
