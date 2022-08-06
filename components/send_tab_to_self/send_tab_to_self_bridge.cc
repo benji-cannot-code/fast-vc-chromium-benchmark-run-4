06af8d483313ff623622cd6f16d04ce0d5207eab
Jinyoung Hur <hur.ims@navercorp.com>
2022-08-06 00:09:55 +0000

    SendTabToSelf: Fix a broken title with a specific Korean character
    Because the title is encoded as UTF-8, it is right to use
    base::CollapseWhitespace() rather than base::CollapseWhitespaceASCII().
    
    Bug: 1350446
    Change-Id: I4e62e2dc1cc67391ec0bbb65a26291fc9cee66a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810766
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Jinyoung Hur <hur.ims@navercorp.com>
    Cr-Commit-Position: refs/heads/main@{#1032182}

--- components/send_tab_to_self/send_tab_to_self_bridge.cc
+++ components/send_tab_to_self/send_tab_to_self_bridge.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/observer_list.h"
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "components/history/core/browser/history_service.h"
@@ -304,7 +305,8 @@ const SendTabToSelfEntry* SendTabToSelfBridge::AddEntry(
   std::string trimmed_title = "";
 
   if (base::IsStringUTF8(title)) {
-    trimmed_title = base::CollapseWhitespaceASCII(title, false);
+    trimmed_title = base::UTF16ToUTF8(
+        base::CollapseWhitespace(base::UTF8ToUTF16(title), false));
   }
 
   auto entry = std::make_unique<SendTabToSelfEntry>(
