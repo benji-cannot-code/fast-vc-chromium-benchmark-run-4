22152a964af883c6457e8cff088fc084d297e8e4
Nancy Wang <nancylingwang@chromium.org>
2022-08-06 16:14:26 +0000

    Implement the non mojom SetPermission for WebApps.
    Modify the mojom SetPermission to use the non mojom interface.
    
    BUG=1253250
    
    Change-Id: I2f3eb760f933a427949a5ac319945f53d6585679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812717
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032297}

--- chrome/browser/web_applications/app_service/web_apps.h
+++ chrome/browser/web_applications/app_service/web_apps.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
 #include "components/services/app_service/public/cpp/intent.h"
+#include "components/services/app_service/public/cpp/permission.h"
 #include "components/services/app_service/public/cpp/publisher_base.h"
 #include "components/services/app_service/public/mojom/app_service.mojom.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -112,6 +113,7 @@ class WebApps : public apps::PublisherBase,
   void LaunchShortcut(const std::string& app_id,
                       const std::string& shortcut_id,
                       int64_t display_id) override;
+  void SetPermission(const std::string& app_id, apps::PermissionPtr permission);
 
   // apps::mojom::Publisher overrides.
   void Connect(mojo::PendingRemote<apps::mojom::Subscriber> subscriber_remote,
