61f707aa32c2d0c58181c0ce359bfbb171f41fff
Nakul Vaidya <nakulvaidya@google.com>
2022-08-08 17:47:19 +0000

    [IBAN] Rename flag
    This change is a followup to the comment https://chromium-review.googlesource.com/c/chromium/src/+/3807417/comments/40b68258_0873b425.
    
    Bug: 1335549
    Change-Id: I8de9a95f47aaced0ca4aa9abcf3d1a7e3559e355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816004
    Commit-Queue: Nakul Vaidya <nakulvaidya@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032609}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8899,9 +8899,9 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(syncer::kIgnoreSyncEncryptionKeysLongMissing)},
 
     {"autofill-parse-iban-fields",
-     flag_descriptions::kAutofillParseIbanFieldsName,
-     flag_descriptions::kAutofillParseIbanFieldsDescription, kOsAll,
-     FEATURE_VALUE_TYPE(autofill::features::kAutofillParseIbanFields)},
+     flag_descriptions::kAutofillParseIBANFieldsName,
+     flag_descriptions::kAutofillParseIBANFieldsDescription, kOsAll,
+     FEATURE_VALUE_TYPE(autofill::features::kAutofillParseIBANFields)},
 
 #if BUILDFLAG(IS_ANDROID)
     {"autofill-enable-fido-progress-dialog",
