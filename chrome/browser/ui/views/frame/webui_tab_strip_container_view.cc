8d5cad5c96790048bd201a034e2ea9c0aab7cfb8
Tom Lukaszewicz <tluk@chromium.org>
2022-08-08 20:37:41 +0000

    [webui tab strip] Integrate with BrowserView::IsTabStripEditable()
    This CL updates BrowserView::IsTabStripEditable() to return false
    while the WebUI Tab Strip is in the middle of a drag session.
    
    This ensures tab strip model updates from the drag are not in
    conflict with modifications made simultaneously by extensions
    which could leave the tab strip model in an inconsistent state.
    
    Bug: 1350025
    Change-Id: If4a76067bb48ce5ff78f92ceb903ef4cec20d0e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812266
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032707}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.cc
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "ui/accessibility/ax_mode.h"
 #include "ui/accessibility/platform/ax_platform_node.h"
+#include "ui/aura/client/drag_drop_client.h"
 #include "ui/aura/window.h"
 #include "ui/base/clipboard/clipboard_format_type.h"
 #include "ui/base/clipboard/custom_data_helper.h"
@@ -537,8 +538,24 @@ void WebUITabStripContainerView::OpenForTabDrag() {
   SetContainerTargetVisibility(true, WebUITabStripOpenCloseReason::kOther);
 }
 
+bool WebUITabStripContainerView::IsTabStripEditable() {
+  gfx::NativeWindow root_window = GetContentRootWindow();
+  if (root_window == nullptr)
+    return true;
+
+  aura::client::DragDropClient* dnd_client =
+      aura::client::GetDragDropClient(root_window);
+  return !(dnd_client && dnd_client->IsDragDropInProgress());
+}
+
 views::NativeViewHost* WebUITabStripContainerView::GetNativeViewHost() {
   return web_view_->holder();
+}
+
+gfx::NativeWindow WebUITabStripContainerView::GetContentRootWindow() {
+  return GetNativeViewHost() && GetNativeViewHost()->GetNativeViewContainer()
+             ? GetNativeViewHost()->GetNativeViewContainer()->GetRootWindow()
+             : nullptr;
 }
 
 std::unique_ptr<views::View> WebUITabStripContainerView::CreateNewTabButton() {
