79ea17afc60dd58917ca578da16f82c6fbbdc2a8
Mike Dougherty <michaeldo@chromium.org>
2022-08-08 17:58:32 +0000

    [iOS] Convert helper functions and move constants to new files
    Move constants out of find_in_page_native_api.js to
    find_in_page_constants.ts and helper functions into
    find_in_page_utils.ts
    
    The `findAbsolutePosition_` function is removed completely as it was
    unused.
    
    Bug: 1348327
    Change-Id: I2b0221608b23e39da439d4e9c3765115bd1f660f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811466
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032616}

--- ios/web/find_in_page/resources/find_in_page.ts
+++ ios/web/find_in_page/resources/find_in_page.ts
@@ -3,11 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Class name of CSS element that selects a highlighted match with orange.
- * @type {string}
- */
-const CSS_CLASS_NAME_SELECT = 'find_selected';
+import {CSS_CLASS_NAME_SELECT}
+    from '//ios/web/find_in_page/resources/find_in_page_constants.js';
 
 /**
  * Returns the width of the document.body.  Sometimes though the body lies to
@@ -273,4 +270,4 @@ class Timer {
   }
 }
 
-export {CSS_CLASS_NAME_SELECT, Match, PartialMatch, Replacement, Section, Timer}
+export {Match, PartialMatch, Replacement, Section, Timer}
