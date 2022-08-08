a4eae1c09bf6988c02c63c09791c3f6131ffcb4a
Jerome Jiang <jianj@google.com>
2022-08-08 20:19:54 +0000

    Roll src/third_party/libaom/source/libaom/ 60e4e24a4..121f38514 (35 commits)
    https://aomedia.googlesource.com/aom.git/+log/60e4e24a410d..121f3851459d
    
    $ git log 60e4e24a4..121f38514 --date=short --no-merges --format='%ad %ae %s'
    2022-08-07 wtc Declare DuckyEncode::pending_ctx_size_ as size_t
    2022-08-04 wachsler Add range checks for max_ref_frames and max_depth.
    2022-08-05 jingning Properly packetize frame bit-streams in ducky_encode
    2022-08-05 neeraj.gadgil rtc: Adjust tune_thresh_based_on_qindex_window conservatively
    2022-07-30 jzern x86: normalize types used with _mm_cvtsi128_si32
    2022-08-03 wachsler Add max_ref_frames and max_depth paramters to API
    2022-08-03 wachsler Check that TPL stats are compatible with GopStruct
    2022-07-28 siekyleb Port libgav1 scalar optimizations for update_cdf
    2022-08-02 marpan rtc: Disable gf_refresh_based_on_qp if gld_fixed_slot=0
    2022-08-02 jingning Rework temporal delimiter placement
    (...)
    2022-07-15 deepa.kg rtc: Fix stale data access of seg_map
    2022-07-27 jzern encode_test_driver: normalize frame_flags type
    2022-07-26 jzern y4m_input_fetch_frame: fix ubsan null/zero offset warning
    2022-07-15 deepa.kg rtc: Fix uv sad calculation
    2022-07-27 jzern test.cmake: remove allintra_end_to_end_test.cc w/rt-only
    2022-07-26 chiyotsai RTC: Account for missing comp_ref_type_cost
    2022-07-21 yunqingwang Optimize PC_TREE allocation in RTC encoding
    2022-07-22 chengchen Fix issue 3296.
    2022-07-26 marpan rtc-svc: Update compound mode for svc
    2022-07-26 jingning Write temporal delimiter after coding a display frame
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Change-Id: I4d8357f0fdd45800d5d5b011b0dd108a40726687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817865
    Reviewed-by: James Zern <jzern@google.com>
    Commit-Queue: Jerome Jiang <jianj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032695}

--- third_party/libaom/source/config/ios/arm-neon/config/aom_dsp_rtcd.h
+++ third_party/libaom/source/config/ios/arm-neon/config/aom_dsp_rtcd.h
@@ -1280,14 +1280,6 @@ void aom_hadamard_8x8_neon(const int16_t* src_diff,
                            tran_low_t* coeff);
 #define aom_hadamard_8x8 aom_hadamard_8x8_neon
 
-void aom_hadamard_8x8_dual_c(const int16_t* src_diff,
-                             ptrdiff_t src_stride,
-                             int16_t* coeff);
-void aom_hadamard_8x8_dual_neon(const int16_t* src_diff,
-                                ptrdiff_t src_stride,
-                                int16_t* coeff);
-#define aom_hadamard_8x8_dual aom_hadamard_8x8_dual_neon
-
 void aom_hadamard_lp_16x16_c(const int16_t* src_diff,
                              ptrdiff_t src_stride,
                              int16_t* coeff);
@@ -1303,6 +1295,14 @@ void aom_hadamard_lp_8x8_neon(const int16_t* src_diff,
                               ptrdiff_t src_stride,
                               int16_t* coeff);
 #define aom_hadamard_lp_8x8 aom_hadamard_lp_8x8_neon
+
+void aom_hadamard_lp_8x8_dual_c(const int16_t* src_diff,
+                                ptrdiff_t src_stride,
+                                int16_t* coeff);
+void aom_hadamard_lp_8x8_dual_neon(const int16_t* src_diff,
+                                   ptrdiff_t src_stride,
+                                   int16_t* coeff);
+#define aom_hadamard_lp_8x8_dual aom_hadamard_lp_8x8_dual_neon
 
 void aom_ifft16x16_float_c(const float* input, float* temp, float* output);
 #define aom_ifft16x16_float aom_ifft16x16_float_c
