6c1e6cb12c88db2e73affb8617f8848adeab3f3d
Ravjit <ravjit@chromium.org>
2022-08-08 16:21:47 +0000

    Fix flakiness in permission predicition browser test
    Bug: 1351001
    Change-Id: I6862417860e0900fd0c22a01729455b4648f083b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816924
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Auto-Submit: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032582}

--- chrome/browser/permissions/prediction_based_permission_ui_selector.cc
+++ chrome/browser/permissions/prediction_based_permission_ui_selector.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
-#include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/rand_util.h"
 #include "base/time/default_clock.h"
