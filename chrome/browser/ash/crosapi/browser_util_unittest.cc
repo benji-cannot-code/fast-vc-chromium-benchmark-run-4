82b4973115467bd50af28a4eb1deffa421b297bc
Michael Ershov <miersh@google.com>
2022-08-08 11:14:23 +0000

    Revert "[Lacros] Expose `LacrosMode` status in `LacrosInfo`"
    This reverts commit 9d2f70d058c892d0e9024bbbf6909d72921fbbef.
    
    Reason for revert:
    Lacros tast tests started to fail on builders:
    https://ci.chromium.org/p/chrome/builders/ci/chromeos-eve-chrome
    https://ci.chromium.org/p/chrome/builders/ci/chromeos-kevin-chrome
    https://ci.chromium.org/p/chromium/builders/ci/chromeos-kevin-rel
    
    Original change's description:
    > [Lacros] Expose `LacrosMode` status in `LacrosInfo`
    >
    > This CL adds the `LacrosMode` in the `LacrosInfo` struct that is exposed
    > by `autotest_private_api` to tast tests. So that tast tests can use
    > `LacrosInfo` to figure out whether it's testing against Lacros Only or
    > Lacros Primary, or any other listed  mode. The four supported modes are
    > `Disabled`, `SideBySide`, `Primary` and `Only`.
    >
    > Ultimately, this CL is one of a series of CLs that enable tast tests to
    > test LacrosOnly scenarios.
    >
    > Bug: 1328994,1328851
    > Change-Id: I29747b3e0c462e50bda560819fd1079cd5908f47
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783852
    > Reviewed-by: Ben Wells <benwells@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Di Wu <diwux@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1032436}
    
    Bug: 1328994,1328851
    Change-Id: I35072b1f2566f3ffc06c8989aaae376eb4b296cd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810569
    Owners-Override: Michael Ershov <miersh@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Auto-Submit: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032498}

--- chrome/browser/ash/crosapi/browser_util_unittest.cc
+++ chrome/browser/ash/crosapi/browser_util_unittest.cc
@@ -405,7 +405,7 @@ TEST_F(BrowserUtilTest, IsAshWebBrowserDisabledByFlags) {
   }
 }
 
-TEST_F(LacrosSupportBrowserUtilTest, LacrosPrimaryOrOnlyBrowserByFlags) {
+TEST_F(LacrosSupportBrowserUtilTest, LacrosPrimaryBrowserByFlags) {
   AddRegularUser("user@test.com");
   const user_manager::User* const user =
       ash::ProfileHelper::Get()->GetUserByProfile(&testing_profile_);
@@ -413,19 +413,6 @@ TEST_F(LacrosSupportBrowserUtilTest, LacrosPrimaryOrOnlyBrowserByFlags) {
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kDisabled,
-              browser_util::GetLacrosMode());
-  }
-
-  // Just enabling LacrosSupport feature is not enough.
-  {
-    base::test::ScopedFeatureList feature_list;
-    feature_list.InitAndEnableFeature(chromeos::features::kLacrosSupport);
-    EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowser());
-    EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowserForMigration(
-        user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kSideBySide,
-              browser_util::GetLacrosMode());
   }
 
   // Just enabling LacrosPrimary feature is not enough.
@@ -435,11 +422,9 @@ TEST_F(LacrosSupportBrowserUtilTest, LacrosPrimaryOrOnlyBrowserByFlags) {
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kDisabled,
-              browser_util::GetLacrosMode());
   }
 
-  // Both LacrosPrimary and LacrosSupport are needed for LacrosPrimary
+  // Both LacrosPrimary and LacrosSupport are needed.
   {
     base::test::ScopedFeatureList feature_list;
     feature_list.InitWithFeatures({chromeos::features::kLacrosPrimary,
@@ -448,21 +433,6 @@ TEST_F(LacrosSupportBrowserUtilTest, LacrosPrimaryOrOnlyBrowserByFlags) {
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kPrimary,
-              browser_util::GetLacrosMode());
-  }
-
-  // All LacrosPrimary, LacrosOnly and LacrosSupport are needed for LacrosOnly
-  {
-    base::test::ScopedFeatureList feature_list;
-    feature_list.InitWithFeatures(
-        {chromeos::features::kLacrosPrimary, chromeos::features::kLacrosSupport,
-         chromeos::features::kLacrosOnly},
-        {});
-    EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
-    EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
-        user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kOnly, browser_util::GetLacrosMode());
   }
 }
 
@@ -480,7 +450,6 @@ TEST_F(BrowserUtilTest, LacrosPrimaryBrowser) {
   EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
   EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
       user, browser_util::PolicyInitState::kAfterInit));
-  EXPECT_EQ(browser_util::LacrosMode::kPrimary, browser_util::GetLacrosMode());
 }
 
 TEST_F(BrowserUtilTest, LacrosPrimaryBrowserAllowed) {
@@ -508,8 +477,6 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosPrimary) {
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kDisabled,
-              browser_util::GetLacrosMode());
   }
 
   {
@@ -520,8 +487,6 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosPrimary) {
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_FALSE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kSideBySide,
-              browser_util::GetLacrosMode());
   }
 
   {
@@ -532,8 +497,6 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosPrimary) {
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kPrimary,
-              browser_util::GetLacrosMode());
   }
 
   {
@@ -544,7 +507,6 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosPrimary) {
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
     EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
         user, browser_util::PolicyInitState::kAfterInit));
-    EXPECT_EQ(browser_util::LacrosMode::kOnly, browser_util::GetLacrosMode());
   }
 }
 
@@ -1036,36 +998,6 @@ TEST_F(BrowserUtilTest, LacrosGoogleRolloutPrimary) {
   EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
   EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
       user, browser_util::PolicyInitState::kAfterInit));
-  // What you see here is Finch overrides policy.
-  // This is due to a special logic only to Googlers.
-  // See IsAshWebBrowserEnabled() impl for more details.
-  EXPECT_EQ(browser_util::LacrosMode::kOnly, browser_util::GetLacrosMode());
-  EXPECT_FALSE(browser_util::IsAshWebBrowserEnabled());
-  EXPECT_FALSE(browser_util::IsAshWebBrowserEnabledForMigration(
-      user, browser_util::PolicyInitState::kAfterInit));
-}
-
-TEST_F(BrowserUtilTest, LacrosGoogleRolloutOnly) {
-  AddRegularUser("user@google.com");
-  const user_manager::User* const user =
-      ash::ProfileHelper::Get()->GetUserByProfile(&testing_profile_);
-  // Lacros availability is set by policy to only.
-  ScopedLacrosAvailabilityCache cache(LacrosAvailability::kLacrosOnly);
-
-  // We enable 3 features: LacrosSupport, LacrosPrimary, LacrosOnly
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitWithFeatures(
-      {chromeos::features::kLacrosSupport, chromeos::features::kLacrosPrimary,
-       chromeos::features::kLacrosOnly},
-      {});
-
-  // Check that Lacros is allowed, enabled, and set to lacros-only.
-  EXPECT_TRUE(browser_util::IsLacrosAllowedToBeEnabled());
-  EXPECT_TRUE(browser_util::IsLacrosEnabled());
-  EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowser());
-  EXPECT_TRUE(browser_util::IsLacrosPrimaryBrowserForMigration(
-      user, browser_util::PolicyInitState::kAfterInit));
-  EXPECT_EQ(browser_util::LacrosMode::kOnly, browser_util::GetLacrosMode());
   EXPECT_FALSE(browser_util::IsAshWebBrowserEnabled());
   EXPECT_FALSE(browser_util::IsAshWebBrowserEnabledForMigration(
       user, browser_util::PolicyInitState::kAfterInit));
