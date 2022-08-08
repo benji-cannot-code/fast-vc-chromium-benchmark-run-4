c3ab0824ca16c28fa5bfbdebaf5b4498f8430635
Christian Dullweber <dullweber@chromium.org>
2022-08-08 16:01:18 +0000

    AboutThisSite: Add field trial config
    Bug: 1318000
    Change-Id: Id84b4d6279b2c964c980c64c3abe04028b547b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813472
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032571}

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteTest.java
@@ -233,6 +233,7 @@ public class PageInfoAboutThisSiteTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @Features.DisableFeatures(ChromeFeatureList.PAGE_INFO_ABOUT_THIS_SITE_MORE_INFO)
     public void testAboutThisSiteSubPageRendering() {
         mockResponse(createDescription());
         openPageInfo();
@@ -243,6 +244,7 @@ public class PageInfoAboutThisSiteTest {
 
     @Test
     @MediumTest
+    @Features.DisableFeatures(ChromeFeatureList.PAGE_INFO_ABOUT_THIS_SITE_MORE_INFO)
     public void testAboutThisSiteSubPageSourceClicked()
             throws ExecutionException, TimeoutException {
         assertEquals(0, mHistogramTester.getHistogramTotalCount("WebsiteSettings.Action"));
@@ -273,6 +275,7 @@ public class PageInfoAboutThisSiteTest {
 
     @Test
     @MediumTest
+    @Features.DisableFeatures(ChromeFeatureList.PAGE_INFO_ABOUT_THIS_SITE_MORE_INFO)
     public void testAboutThisSiteSubPageSourceClickedWithoutEphemeralTabCreator()
             throws ExecutionException, TimeoutException {
         // Test the path without ephemeralTabCreator.
