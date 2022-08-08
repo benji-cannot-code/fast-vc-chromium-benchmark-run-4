27b4f68c63a927435cfafba148d07c95e3acf941
Jan Keitel <jkeitel@google.com>
2022-08-08 18:49:57 +0000

    Enforce the same sync requirements for APC flows across entry points.
    With the expansion to Desktop, some of the conditions for offering
    automatic password change differ between entry points. This CL
    moves checking the sync requirements into a single place and disables
    password changes for account store users.
    Once the necessary changes to password scripts fetching and the
    WebsiteLoginManager used by autofill_assistant are made (i.e. using
    the account store instead of the local store), we can re-enable APC
    for account store users.
    
    Bug: 1349782
    Change-Id: Ie469d124ecdc163379f8222a95215c2a581fa05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804635
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032652}

--- chrome/browser/extensions/api/passwords_private/password_check_delegate.cc
+++ chrome/browser/extensions/api/passwords_private/password_check_delegate.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/leak_detection/bulk_leak_check.h"
 #include "components/password_manager/core/browser/leak_detection/encryption_utils.h"
 #include "components/password_manager/core/browser/password_change_success_tracker.h"
+#include "components/password_manager/core/browser/password_feature_manager_impl.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
@@ -262,6 +263,10 @@ PasswordCheckDelegate::PasswordCheckDelegate(
                 int,
                 password_manager::CredentialUIEntry::Less>* id_generator)
     : profile_(profile),
+      password_feature_manager_(
+          std::make_unique<password_manager::PasswordFeatureManagerImpl>(
+              profile->GetPrefs(),
+              SyncServiceFactory::GetForProfile(profile))),
       saved_passwords_presenter_(presenter),
       insecure_credentials_manager_(presenter,
                                     PasswordStoreFactory::GetForProfile(
@@ -670,15 +675,10 @@ PasswordScriptsFetcher* PasswordCheckDelegate::GetPasswordScriptsFetcher()
 
 bool PasswordCheckDelegate::IsAutomatedPasswordChangeFromSettingsEnabled()
     const {
-  // Do not offer password change to non-syncing users, as it is required
-  // for generating passwords.
-  if (password_manager_util::GetPasswordSyncState(
-          SyncServiceFactory::GetForProfile(profile_)) ==
-      password_manager::SyncState::kNotSyncing) {
-    return false;
-  }
-  return base::FeatureList::IsEnabled(
-      password_manager::features::kPasswordChange);
+  return password_feature_manager_
+             ->AreRequirementsForAutomatedPasswordChangeFulfilled() &&
+         base::FeatureList::IsEnabled(
+             password_manager::features::kPasswordChange);
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/api/passwords_private/password_check_delegate.h
+++ chrome/browser/extensions/api/passwords_private/password_check_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_PASSWORDS_PRIVATE_PASSWORD_CHECK_DELEGATE_H_
 #define CHROME_BROWSER_EXTENSIONS_API_PASSWORDS_PRIVATE_PASSWORD_CHECK_DELEGATE_H_
 
+#include <memory>
+
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
@@ -29,6 +31,7 @@ class Profile;
 
 namespace password_manager {
 class PasswordChangeSuccessTracker;
+class PasswordFeatureManager;
 class PasswordScriptsFetcher;
 }  // namespace password_manager
 
@@ -176,6 +179,11 @@ class PasswordCheckDelegate
 
   // Raw pointer to the underlying profile. Needs to outlive this instance.
   raw_ptr<Profile> profile_ = nullptr;
+
+  // A password feature manager instance used to determine whether to offer
+  // automated password changes.
+  const std::unique_ptr<password_manager::PasswordFeatureManager>
+      password_feature_manager_;
 
   // Used by `insecure_credentials_manager_` to obtain the list of saved
   // passwords.
