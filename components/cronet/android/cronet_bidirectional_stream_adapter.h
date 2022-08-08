ea0595e2332b0a4bb64e46b0ba84daabf918804c
Fabio Tirelo <ftirelo@chromium.org>
2022-08-08 16:25:17 +0000

    Revert "Roll src/net/third_party/quiche/src/ fe9c73029..fe1c281a8 (12 commits)"
    This reverts commit 410434de961e79e0aea207016a32ffb399b09193.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-rel/66640/overview
    
    Original change's description:
    > Roll src/net/third_party/quiche/src/ fe9c73029..fe1c281a8 (12 commits)
    >
    > https://quiche.googlesource.com/quiche.git/+log/fe9c73029c24..fe1c281a815f
    >
    > $ git log fe9c73029..fe1c281a8 --date=short --no-merges --format='%ad %ae %s'
    > 2022-08-05 bnc Delete quic_flags_impl.{h,cc}.
    > 2022-08-05 bnc Tighten up AppropriateActionTakenWhenHeadersTooLongWithTooMuchInput test.
    > 2022-08-05 vasilvv Switch QuicTestClient to use QuicDefaultClient instead of QuicClient
    > 2022-08-04 vasilvv Remove leftover epoll dependencies for quic_server.
    > 2022-08-04 martinduke Extract Quiche default connection ID generation algorithms into a library. When integrated into quiche, similar functions can be removed.
    > 2022-08-03 diannahu Move NoopEncoderStreamErrorDelegate from qpack_decoder_test_utils to qpack_decoder.
    > 2022-08-03 martinduke Proposed interface for "Connection ID generators" that will allow different algorithms for different platforms that deploy QUICHE.
    > 2022-08-03 martinduke Avoid creating session when server_connection_id matches, and send CONNECTION_CLOSE instead of delivering the packet if the original connection IDs don't match.
    > 2022-08-03 abhisinghx Internal change
    > 2022-08-03 haoyuewang Internal change
    > 2022-08-02 bnc Remove SpdyHeaderBlock alias.
    > 2022-08-02 bnc Remove qpack_encoder_test_utils.
    >
    > Created with:
    >   roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    >
    > Change-Id: Ia4de1fcaab6f9eb4a679fe13ecd82d7120167da1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813165
    > Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    > Auto-Submit: Bence Béky <bnc@chromium.org>
    > Commit-Queue: Bence Béky <bnc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1032533}
    
    Change-Id: I341bd78627eea7bcdfac638d15bb2c573c18a86b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817862
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Owners-Override: Fabio Tirelo <ftirelo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1032583}

--- components/cronet/android/cronet_bidirectional_stream_adapter.h
+++ components/cronet/android/cronet_bidirectional_stream_adapter.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "net/base/network_handle.h"
 #include "net/http/bidirectional_stream.h"
-#include "net/third_party/quiche/src/quiche/spdy/core/http2_header_block.h"
+#include "net/third_party/quiche/src/quiche/spdy/core/spdy_header_block.h"
 
 namespace net {
 struct BidirectionalStreamRequestInfo;
