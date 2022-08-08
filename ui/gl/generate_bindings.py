d52671de0fee49eb03fde51909bc7a9ffea02944
Yonggang Luo <luoyonggang@gmail.com>
2022-08-08 17:42:42 +0000

    ui/gl: Remove EGL/eglextchromium.h
    EGL/eglextchromium.h is not used by any code in chromium repository,
    And indeed EGL/eglextchromium.h are broken by looking the end of it,
    it's end with '#define  // UI_GL_EGL_EGLEXTCHROMIUM_H_', that's should
    be '#endif // UI_GL_EGL_EGLEXTCHROMIUM_H_'
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Change-Id: Ie7f192ddcfb6aab12fa18d16fce904b1efc92f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798835
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032608}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -3025,8 +3025,6 @@ FUNCTION_SETS = [
   ],
   [EGL_FUNCTIONS, 'egl', [
       'EGL/eglext.h',
-      # Files below are Chromium-specific and shipped with Chromium sources.
-      'EGL/eglextchromium.h',
     ],
     [
       'EGL_ANGLE_d3d_share_handle_client_buffer',
