7b0f94a97a819faedf676b50f22d46bf00b41f44
Jonathan Lee <jonathanjlee@google.com>
2022-08-08 19:32:38 +0000

    [blinkpy] 1/N: Refactor wptreport fetching
    Because `update-metadata` will also consume wptreports, this change
    moves wptreport fetching from `wpt_uploader` to `TestResultsFetcher`.
    The pRPC client implementation has been decoupled from filename parsing
    and moved to `net.rpc`, which will eventually consolidate duplicated
    BB/ResultDB calls.
    
    Bug: 1299650, 1213998
    Change-Id: Ia5c124987cf30cb2bbc7df072dcfb1806bab9280
    Test: ./wpt_upload.py --dry-run
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788767
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032675}

--- third_party/blink/tools/blinkpy/common/host_mock.py
+++ third_party/blink/tools/blinkpy/common/host_mock.py
@@ -115,7 +115,7 @@ class MockHost(MockSystemHost):
                 'is_try_builder': True,
             },
         })
-        self.results_fetcher = MockTestResultsFetcher(self.builders)
+        self.results_fetcher = MockTestResultsFetcher.from_host(self)
 
     def git(self, path=None):
         if path:
