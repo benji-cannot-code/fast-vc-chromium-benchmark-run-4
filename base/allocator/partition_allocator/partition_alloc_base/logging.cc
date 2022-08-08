3808322210888331936bf0d8f224d1f6b273ccd5
Qi Tiezheng <qitiezheng@360.cn>
2022-08-08 03:46:09 +0000

    [PA] Fix some cpplint errors in base/allocator/partition_allocator part2
    base/allocator/partition_allocator/partition_alloc_base/logging.cc:45:
      "base/allocator/partition_allocator/partition_alloc_base/debug/alias.h" already included at base/allocator/partition_allocator/partition_alloc_base/logging.cc:17  [build/include] [4]
    
    Bug: None
    Change-Id: Id18ceb80ea378865d036ae910aee14e4355eccd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802398
    Auto-Submit: Qi Tiezheng <qitiezheng@360.cn>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032413}

--- base/allocator/partition_allocator/partition_alloc_base/logging.cc
+++ base/allocator/partition_allocator/partition_alloc_base/logging.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <string>
 
-#include "base/allocator/partition_allocator/partition_alloc_base/debug/alias.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/posix/eintr_wrapper.h"
 
 #if BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
