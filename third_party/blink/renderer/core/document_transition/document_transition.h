51f80e72aa7093130ed39cd4753b63c9192c3fdd
Vladimir Levin <vmpstr@chromium.org>
2022-08-08 18:44:54 +0000

    SET: Propagate the callback error to the caller / devtools.
    This patch propagates the error up from the callback. Without this
    patch we just know that an exception happened in a promise, but with
    this we actually know what the error is, which makes it easier to
    debug callbacks.
    
    R=khushalsagar@chromium.org
    
    Change-Id: I85a70f613e01fb1206365e7460edb805edd5fe53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807891
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032649}

--- third_party/blink/renderer/core/document_transition/document_transition.h
+++ third_party/blink/renderer/core/document_transition/document_transition.h
@@ -134,7 +134,9 @@ class CORE_EXPORT DocumentTransition
   // executing.
   class PostCaptureResolved : public ScriptFunction::Callable {
    public:
-    explicit PostCaptureResolved(DocumentTransition* transition, bool success);
+    explicit PostCaptureResolved(DocumentTransition* transition,
+                                 bool success,
+                                 Document*);
     ~PostCaptureResolved() override;
 
     ScriptValue Call(ScriptState*, ScriptValue) override;
@@ -145,6 +147,7 @@ class CORE_EXPORT DocumentTransition
    private:
     Member<DocumentTransition> transition_;
     const bool success_;
+    Member<Document> document_;
   };
 
   void NotifyHasChangesToCommit();
