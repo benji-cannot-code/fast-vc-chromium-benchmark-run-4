24982739fddf5db9f34b97d4fb8539d8bde1ab75
Ari Chivukula <arichiv@chromium.org>
2022-08-08 23:22:42 +0000

    [CodeHealth] Use `is_chromeos` in Grit files (/components/version_ui)
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1316150
    Change-Id: I48b9fe91f1a25e2d5babf3ffebf56b1044ace1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811441
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032780}

--- components/version_ui/resources/about_version.js
+++ components/version_ui/resources/about_version.js
@@ -83,7 +83,7 @@ function returnCustomizationId(response) {
 }
 // </if>
 
-// <if expr="chromeos_ash or chromeos_lacros">
+// <if expr="is_chromeos">
 /**
  * Callback from the backend to inform if Lacros is primary or not.
  * @param {string} isPrimary True if it is primary.
@@ -125,7 +125,7 @@ function onLoadWork() {
   addWebUIListener('return-os-firmware-version', returnOsFirmwareVersion);
   addWebUIListener('return-arc-version', returnARCVersion);
   // </if>
-  // <if expr="chromeos_ash or chromeos_lacros">
+  // <if expr="is_chromeos">
   addWebUIListener('return-lacros-primary', returnLacrosPrimary);
   // </if>
 
