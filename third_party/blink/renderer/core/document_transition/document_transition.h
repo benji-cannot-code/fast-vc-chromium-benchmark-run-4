48b38e1c7d65a2fff19ae7765862949ca06ca3ac
Katie Dektar <katie@chromium.org>
2022-08-08 21:14:02 +0000

    [Sheriff] Revert "SET: Propagate the callback error to the caller / devtools."
    This reverts commit 169c59eea4ed4384f76bc671c5813554f5de7ae5.
    
    Reason for revert: Seems to have caused test failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20Leak/41422/test-results
    by adding unexpected console errors:
    +CONSOLE ERROR: Uncaught (in promise) AbortError: Aborted due to failure in DocumentTransitionCallback
    
    Original owner is out for the day so speculatively reverting to 
    reduce failures until fix can be made later.
    
    Original change's description:
    > SET: Propagate the callback error to the caller / devtools.
    >
    > This patch propagates the error up from the callback. Without this
    > patch we just know that an exception happened in a promise, but with
    > this we actually know what the error is, which makes it easier to
    > debug callbacks.
    >
    > R=​khushalsagar@chromium.org
    >
    > Change-Id: I85a70f613e01fb1206365e7460edb805edd5fe53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807891
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1032649}
    
    Change-Id: I783551933525b0fe415a40d9a58d09840186975f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1351145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816825
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032728}

--- third_party/blink/renderer/core/document_transition/document_transition.h
+++ third_party/blink/renderer/core/document_transition/document_transition.h
@@ -134,9 +134,7 @@ class CORE_EXPORT DocumentTransition
   // executing.
   class PostCaptureResolved : public ScriptFunction::Callable {
    public:
-    explicit PostCaptureResolved(DocumentTransition* transition,
-                                 bool success,
-                                 Document*);
+    explicit PostCaptureResolved(DocumentTransition* transition, bool success);
     ~PostCaptureResolved() override;
 
     ScriptValue Call(ScriptState*, ScriptValue) override;
@@ -147,7 +145,6 @@ class CORE_EXPORT DocumentTransition
    private:
     Member<DocumentTransition> transition_;
     const bool success_;
-    Member<Document> document_;
   };
 
   void NotifyHasChangesToCommit();
