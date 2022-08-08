92d6eef1f1776679489d587dc9f492400657412c
Xianzhu Wang <wangxianzhu@chromium.org>
2022-08-08 21:41:51 +0000

    Remove destroyed sticky layer from PaintLayerScrollableArea
    Previously I thought removing a sticky layer will mark the LayoutBox
    of ContainingScrollContainerLayer() needing layout, but that's not
    always the case e.g. when there is a layout barrier.
    
    Bug: 1350397
    Change-Id: Ib80d7f1999cd420ac6b0810f753bbfc9d02fa354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813956
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032745}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
@@ -519,6 +519,7 @@ class CORE_EXPORT PaintLayerScrollableArea final
   }
 
   void AddStickyLayer(PaintLayer*);
+  void RemoveStickyLayer(PaintLayer*);
   bool HasStickyLayer(PaintLayer* layer) const {
     return rare_data_ && rare_data_->sticky_layers_.Contains(layer);
   }
