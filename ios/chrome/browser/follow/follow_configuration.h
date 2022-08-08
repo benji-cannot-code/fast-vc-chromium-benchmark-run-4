ce3345e2a01321e85d59b8e36100bc3be68e74a5
Sylvain Defresne <sdefresne@chromium.org>
2022-08-08 13:58:15 +0000

    [ios] Add new follow provider API
    This introduce a new class FollowService which is a KeyedService
    and the provider API exposes a factory function. The API allows to
    pass a callback invoked when a follow/unfollow request completes,
    which will allow to fix multi-window issues.
    
    This is part of a multi CLs change:
     => https://crrev.com/c/3757671
     -  https://crrev.com/i/4874197
     -  https://crrev.com/c/3760445
     -  https://crrev.com/i/4875937
     -  https://crrev.com/c/3758209
    
    Bug: 1201182, 1342003
    Change-Id: Ie32c7c5e7758ab218842819154eaed2c1139796b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757671
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032532}

---
+++ ios/chrome/browser/follow/follow_configuration.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_FOLLOW_FOLLOW_CONFIGURATION_H_
+#define IOS_CHROME_BROWSER_FOLLOW_FOLLOW_CONFIGURATION_H_
+
+#import <Foundation/Foundation.h>
+
+class DiscoverFeedService;
+
+// Configuration object used by the FollowService.
+@interface FollowConfiguration : NSObject
+
+// DiscoverFeedService used by FollowService.
+@property(nonatomic, assign) DiscoverFeedService* feedService;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_FOLLOW_FOLLOW_CONFIGURATION_H_
