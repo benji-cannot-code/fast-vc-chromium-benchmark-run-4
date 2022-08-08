3c7e31db8b3e4b4ffd5e6583b088f92bf3b3ea41
Shahbaz Youssefi <syoussefi@chromium.org>
2022-08-08 02:33:27 +0000

    Update ANGLE translator compile options interface
    Bug: angleproject:7559
    Change-Id: I76a9f77018e8ace17e2178c4fb1b27fd71a573fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3815262
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Shahbaz Youssefi <syoussefi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032401}

--- gpu/command_buffer/service/shader_translator.h
+++ gpu/command_buffer/service/shader_translator.h
@@ -46,7 +46,7 @@ class ShaderTranslatorInterface
                     ShShaderSpec shader_spec,
                     const ShBuiltInResources* resources,
                     ShShaderOutput shader_output_language,
-                    ShCompileOptions driver_bug_workarounds,
+                    const ShCompileOptions& driver_bug_workarounds,
                     bool gl_shader_interm_output) = 0;
 
   // Translates the given shader source.
@@ -102,7 +102,7 @@ class GPU_GLES2_EXPORT ShaderTranslator : public ShaderTranslatorInterface {
             ShShaderSpec shader_spec,
             const ShBuiltInResources* resources,
             ShShaderOutput shader_output_language,
-            ShCompileOptions driver_bug_workarounds,
+            const ShCompileOptions& driver_bug_workarounds,
             bool gl_shader_interm_output) override;
 
   // Overridden from ShaderTranslatorInterface.
@@ -125,7 +125,7 @@ class GPU_GLES2_EXPORT ShaderTranslator : public ShaderTranslatorInterface {
  private:
   ~ShaderTranslator() override;
 
-  ShCompileOptions GetCompileOptions() const;
+  const ShCompileOptions& GetCompileOptions() const;
 
   ShHandle compiler_;
   ShCompileOptions compile_options_;
