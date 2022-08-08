7f843d2abfbd31d10596ac4e1abbde9a4a819f92
Jan Lanik <janlanik@google.com>
2022-08-08 20:50:47 +0000

    [Privacy Hub] Future work feature flag
    A flag for features that don't go to the MVP.
    
    Bug: b/239822334
    Change-Id: I229da18c6004d4c1c66ea8eb7479b44524d69b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810213
    Auto-Submit: Jan Láník <janlanik@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032714}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -158,6 +158,8 @@ extern const base::Feature kCrosNextWMP;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCrosPrivacyHub;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kCrosPrivacyHubFuture;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCrostiniBullseyeUpgrade;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kDesksTemplates;
@@ -703,6 +705,7 @@ bool IsClipboardHistoryNudgeSessionResetEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsClipboardHistoryReorderEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsConsumerAutoUpdateToggleAllowed();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCrosPrivacyHubEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCrosPrivacyHubFutureEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCrosNextWMPEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsDesksCloseAllEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCryptohomeRecoveryFlowEnabled();
