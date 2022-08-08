bbd5ecf31e24eac1a0f6855111c2b8d0a572d8e3
Anna Sato <annasato@google.com>
2022-08-08 07:08:24 +0000

    Remove unnecessary include.
    Change-Id: I8aa1d4cbea3907ce5811cdf156e75f655e926f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812708
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Anna Sato <annasato@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032449}

--- chrome/browser/ash/crosapi/browser_version_service_ash.cc
+++ chrome/browser/ash/crosapi/browser_version_service_ash.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/crosapi/browser_version_service_ash.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chrome/browser/browser_process.h"
 
 namespace {
 
