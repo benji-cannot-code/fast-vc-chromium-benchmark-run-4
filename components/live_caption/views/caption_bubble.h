5aa8eb774274eca5a91a02e75faa9a45778fe13e
evliu <evliu@google.com>
2022-08-08 21:07:18 +0000

    Add pin/unpin functionality to the caption bubble
    This CL adds a button to the caption bubble that will allow users to
    pin or unpin the caption bubble. The caption bubble will not hide due
    to inactivity if it is pinned, but users can still close the bubble by
    clicking the close icon or by disabling Live Caption. Pinning does not
    affect the user's ability to expand/collapse or move the caption bubble.
    
    Bug: 1342709
    Change-Id: Ib4a68b72050ec0844df9a6a5169f82798782397a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759525
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032723}

--- components/live_caption/views/caption_bubble.h
+++ components/live_caption/views/caption_bubble.h
@@ -68,7 +68,7 @@ using ResetInactivityTimerCallback = base::RepeatingCallback<void()>;
 class CaptionBubble : public views::BubbleDialogDelegateView {
  public:
   METADATA_HEADER(CaptionBubble);
-  CaptionBubble(base::OnceClosure destroyed_callback, bool hide_on_inactivity);
+  explicit CaptionBubble(base::OnceClosure destroyed_callback);
   CaptionBubble(const CaptionBubble&) = delete;
   CaptionBubble& operator=(const CaptionBubble&) = delete;
   ~CaptionBubble() override;
@@ -125,6 +125,10 @@ class CaptionBubble : public views::BubbleDialogDelegateView {
   void BackToTabButtonPressed();
   void CloseButtonPressed();
   void ExpandOrCollapseButtonPressed();
+  void PinOrUnpinButtonPressed();
+  void SwapButtons(views::Button* first_button,
+                   views::Button* second_button,
+                   bool show_first_button);
 
   // Called by CaptionBubbleModel to notify this object that the model's text
   // has changed. Sets the text of the caption bubble to the model's text.
@@ -141,10 +145,6 @@ class CaptionBubble : public views::BubbleDialogDelegateView {
   void OnErrorChanged(CaptionBubbleErrorType error_type,
                       OnErrorClickedCallback callback,
                       OnDoNotShowAgainClickedCallback error_silenced_callback);
-
-  // Called when the caption bubble expanded state has changed. Changes the
-  // number of lines displayed.
-  void OnIsExpandedChanged();
 
   // The caption bubble manages its own visibility based on whether there's
   // space for it to be shown, and if it has an error or text to display.
@@ -196,6 +196,8 @@ class CaptionBubble : public views::BubbleDialogDelegateView {
   raw_ptr<views::ImageButton> close_button_;
   raw_ptr<views::ImageButton> expand_button_;
   raw_ptr<views::ImageButton> collapse_button_;
+  raw_ptr<views::ImageButton> pin_button_;
+  raw_ptr<views::ImageButton> unpin_button_;
   raw_ptr<CaptionBubbleFrameView> frame_;
 
 #if BUILDFLAG(IS_WIN)
@@ -218,10 +220,10 @@ class CaptionBubble : public views::BubbleDialogDelegateView {
   // Whether the caption bubble is expanded to show more lines of text.
   bool is_expanded_ = false;
 
-  bool has_been_shown_ = false;
+  // Whether the caption bubble is pinned or if it should hide on inactivity.
+  bool is_pinned_ = false;
 
-  // Whether we should hide the caption bubble on inactivity.
-  bool const hide_on_inactivity_;
+  bool has_been_shown_ = false;
 
   // Used to determine whether to propagate theme changes to the widget.
   SkColor text_color_ = gfx::kPlaceholderColor;
