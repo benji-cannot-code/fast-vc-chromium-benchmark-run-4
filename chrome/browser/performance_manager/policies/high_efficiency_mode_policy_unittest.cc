00bb9d6c881ee1823db8141e0ac09c73fb269ccd
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-08-09 18:07:54 +0000

    Introduce UserPerformanceTuningManager
    This CL introduces a new component that manages the state of User
    Controlled Performance features, and surfaces signals related to them
    so that UI can decide when and what to display.
    
    This CL also moves the existing High Efficiency and Battery Saver Mode
    behaviors to this new component. A follow up will finish implementing
    the different signals and expose the manager to other code.
    
    Design Doc: go/uv-performance-bsm-manager
    
    Bug: 1348590
    Change-Id: I51158c3e262d649c331c5f5db36a7bd996a11f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817866
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033113}

--- chrome/browser/performance_manager/policies/high_efficiency_mode_policy_unittest.cc
+++ chrome/browser/performance_manager/policies/high_efficiency_mode_policy_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
-#include "chrome/browser/performance_manager/policies/high_efficiency_mode_policy_helper.h"
 #include "chrome/browser/performance_manager/test_support/page_discarding_utils.h"
 #include "components/performance_manager/public/features.h"
 #include "components/performance_manager/public/user_tuning/prefs.h"
