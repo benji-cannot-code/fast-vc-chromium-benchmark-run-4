ac8c0b12528f798f03f69b8c594e6a3c7737424e
Peter Boström <pbos@chromium.org>
2022-08-09 22:36:27 +0000

    Move LOG_FATAL crash-key collection into ~LogMessage
    This LogMessageHandler was known to compete with viz on the GPU process
    and at least in some instances on the renderer process. When it did
    compete with another LogMessageHandler the LOG_FATAL crash key never got
    set, resulting in less actionable crash dumps.
    
    The presence of a g_log_message_handler also causes a significant amount
    of non-FATAL LogMessages to be constructed, their arguments evaluated
    and streamed even though they're eventually discarded in ~LogMessage as
    no-ops. I've confirmed locally that ShouldCreateLogMessage() returns
    false very frequently after this change, where it otherwise would've
    returned true, but I have not measured any actual performance impact of
    this change. Can't hurt though, there's probably some LOG() statements
    in fairly hot loops somewhere.
    
    This change also deduplicates code between from crash_helper.mm and
    crashpad.cc into base/logging.cc. Theoretically any other platforms that
    initialize //base crashkey support and capture crash keys should be able
    to benefit from this change. Presumably there are no such (significant)
    platforms.
    
    Bug: 1344940
    Change-Id: I36576799ccdfe22f7fe428fe6e77256dbe675232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812231
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033242}

--- base/logging.h
+++ base/logging.h
@@ -654,9 +654,6 @@ class BASE_EXPORT LogMessage {
 
   // Gets file:line: message in a format suitable for crash reporting.
   std::string BuildCrashString() const;
-  static std::string BuildCrashString(const char* file,
-                                      int line,
-                                      const char* message_without_prefix);
 
  private:
   void Init(const char* file, int line);
