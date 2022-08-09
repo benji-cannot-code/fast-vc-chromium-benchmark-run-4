9f2bc5daf8a6dd3538a524ef10bd6e8a2f08cb13
Emily Shack <emshack@chromium.org>
2022-08-09 22:10:43 +0000

    Make the side panel an accessible pane
    Bug: 1351171
    Change-Id: I8cd7618568514f35a6b349ba5e1e3bc5154d7e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816990
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033227}

--- chrome/browser/ui/views/side_panel/side_panel.h
+++ chrome/browser/ui/views/side_panel/side_panel.h
@@ -9,13 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/accessible_pane_view.h"
 #include "ui/views/controls/resize_area_delegate.h"
-#include "ui/views/view.h"
 #include "ui/views/view_observer.h"
 
 class BrowserView;
 
-class SidePanel : public views::View,
+class SidePanel : public views::AccessiblePaneView,
                   public views::ViewObserver,
                   public views::ResizeAreaDelegate {
  public:
