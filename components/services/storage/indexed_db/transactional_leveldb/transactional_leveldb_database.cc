5ccd1e7990e104a5946e9513456176750c03df93
Paul Semel <paulsemel@chromium.org>
2022-08-09 17:54:24 +0000

    DanglingPtr: Fix dangling ptr in LevelDBSnapshot
    `snapshot_` is getting release upon a call to `ReleaseSnapshot`.
    However, it is hard to make this a smart pointer, because it actually
    depends on another member (`db_`), so it'd require a bigger
    rearchitecturing of the snapshots ownership model.
    
    Bug: 1291138
    Change-Id: I00a5ba0535e2dcf7adbb6a8a26b470915330f5f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819765
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033089}

--- components/services/storage/indexed_db/transactional_leveldb/transactional_leveldb_database.cc
+++ components/services/storage/indexed_db/transactional_leveldb/transactional_leveldb_database.cc
@@ -68,7 +68,7 @@ LevelDBSnapshot::LevelDBSnapshot(TransactionalLevelDBDatabase* db)
     : db_(db->db()), snapshot_(db_->GetSnapshot()) {}
 
 LevelDBSnapshot::~LevelDBSnapshot() {
-  db_->ReleaseSnapshot(snapshot_);
+  db_->ReleaseSnapshot(snapshot_.ExtractAsDangling());
 }
 
 // static
