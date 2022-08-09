9ddc4deb6c64617501363edf0a929a85bf874953
Zonghan Xu <xzonghan@google.com>
2022-08-09 22:31:58 +0000

    Add new policy: UnmanagedDeviceSignalsConsentFlowEnabled, this policy will be later used to implement the user consent collection page for sharing device signals (with minor BUG fix on Fuchsia x64)
    Bug: b/236728396
    Change-Id: Iadb4ccb9d642785310c2aa9e91b0773a0ff17f51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805384
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1033056}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3821363
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033240}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -189,6 +189,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/browser/pref_names.h"
 #endif  // BUILDFLAG(ENABLE_SPELLCHECK)
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#include "components/device_signals/core/browser/pref_names.h"
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_FUCHSIA)
 #include "chrome/browser/web_applications/policy/web_app_settings_policy_handler.h"
@@ -1401,6 +1405,11 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     prefs::kBackgroundModeEnabled,
     base::Value::Type::BOOLEAN },
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+  { key::kUnmanagedDeviceSignalsConsentFlowEnabled,
+    device_signals::prefs::kUnmanagedDeviceSignalsConsentFlowEnabled,
+    base::Value::Type::BOOLEAN },
+#endif // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) \
     || BUILDFLAG(IS_FUCHSIA)
   { key::kDefaultBrowserSettingEnabled,
