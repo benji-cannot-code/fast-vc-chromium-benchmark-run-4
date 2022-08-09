b852f7e6d240f7404b4346f683d99a3c1853d122
Peng Huang <penghuang@chromium.org>
2022-08-09 21:22:01 +0000

    Use base::flat_map and base::circular_deque to replace std::map and std::deque
    Change-Id: I02fbfc125a746463a496f7475c1bd3d1fe70f3c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820201
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033200}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_IMPL_ON_GPU_H_
 #define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_IMPL_ON_GPU_H_
 
-#include <deque>
-#include <map>
 #include <memory>
 #include <utility>
 #include <vector>
 
 #include "base/callback_forward.h"
+#include "base/containers/circular_deque.h"
+#include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/threading/thread_checker.h"
@@ -508,7 +509,7 @@ class SkiaOutputSurfaceImplOnGpu
   // Overlayed render passes need to keep their write access open until after
   // submit. These will be set in FinishPaintRenderPass() if |is_overlay| is
   // true and destroyed in PostSubmit().
-  std::map<gpu::Mailbox, OverlayPassAccess> overlay_pass_accesses_;
+  base::flat_map<gpu::Mailbox, OverlayPassAccess> overlay_pass_accesses_;
 
   absl::optional<OverlayProcessorInterface::OutputSurfaceOverlayPlane>
       output_surface_plane_;
@@ -533,7 +534,7 @@ class SkiaOutputSurfaceImplOnGpu
   // Pending release fence callbacks. These callbacks can be delayed if Vulkan
   // external semaphore type has copy transference, which means importing
   // semaphores has to be delayed until submission.
-  std::deque<std::pair<GrBackendSemaphore,
+  base::circular_deque<std::pair<GrBackendSemaphore,
                        base::OnceCallback<void(gfx::GpuFenceHandle)>>>
       pending_release_fence_cbs_;
 
