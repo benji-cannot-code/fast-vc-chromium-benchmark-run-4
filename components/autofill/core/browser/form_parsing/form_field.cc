ed3c894d004f039ec60f71aa65e760a300673534
Nakul Vaidya <nakulvaidya@google.com>
2022-08-09 01:53:38 +0000

    Reland "[IBAN] field detection implementation"
    This is a reland of commit 305b08d3999d9e98a30f50d3c739835111b5f3a8
    
    The change disables failing tests on linux-chromeos as changing
    values in chrome://flags does not work for linux-chromeos.
    
    Refer to bullet three (https://chromium.googlesource.com/chromium/src/+/master/docs/chromeos_build_instructions.md#Flags)
    
    Original change's description:
    > [IBAN] field detection implementation
    >
    > This change implements regex, unit tests and field detection
    > logic for International Bank Account Number (IBAN).
    >
    > Bug: 1335549
    > Change-Id: I8f910d3cc20d82fdf80a16072c4b743424f4f9fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759485
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Reviewed-by: Matthias Körber <koerber@google.com>
    > Commit-Queue: Nakul Vaidya <nakulvaidya@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027861}
    
    Bug: 1335549
    Change-Id: Ia686bc7ffd4845e49a291a0ea96ef84467b09754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807417
    Commit-Queue: Nakul Vaidya <nakulvaidya@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032830}

--- components/autofill/core/browser/form_parsing/form_field.cc
+++ components/autofill/core/browser/form_parsing/form_field.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_parsing/birthdate_field.h"
 #include "components/autofill/core/browser/form_parsing/credit_card_field.h"
 #include "components/autofill/core/browser/form_parsing/email_field.h"
+#include "components/autofill/core/browser/form_parsing/iban_field.h"
 #include "components/autofill/core/browser/form_parsing/merchant_promo_code_field.h"
 #include "components/autofill/core/browser/form_parsing/name_field.h"
 #include "components/autofill/core/browser/form_parsing/phone_field.h"
@@ -184,6 +185,12 @@ void FormField::ParseSingleFieldForms(
     ParseFormFieldsPass(MerchantPromoCodeField::Parse, processed_fields,
                         field_candidates, page_language, pattern_source,
                         log_manager);
+  }
+
+  // IBAN pass.
+  if (base::FeatureList::IsEnabled(features::kAutofillParseIBANFields)) {
+    ParseFormFieldsPass(IBANField::Parse, processed_fields, field_candidates,
+                        page_language, pattern_source, log_manager);
   }
 }
 
@@ -495,7 +502,7 @@ bool FormField::MatchesFormControlType(base::StringPiece type,
 
 // static
 bool FormField::IsSingleFieldParseableType(ServerFieldType field_type) {
-  return field_type == MERCHANT_PROMO_CODE;
+  return field_type == MERCHANT_PROMO_CODE || field_type == IBAN_VALUE;
 }
 
 }  // namespace autofill
