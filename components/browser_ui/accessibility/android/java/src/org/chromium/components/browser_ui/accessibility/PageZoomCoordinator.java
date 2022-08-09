1e0c4af011de188070a98270a65ee30a50928b59
Amanda Lin Dietz <aldietz@google.com>
2022-08-09 18:03:20 +0000

    [Page Zoom] Translate slider when bottom controls are visible
    When bottom controls are visible, such as the tab strip, shift the
    slider up by the height of the controls.
    
    Bug: 1232536
    Change-Id: I747087fcbb47de3b635ad697d60a8a28ceeba722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811085
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033105}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomCoordinator.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomCoordinator.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.accessibility;
 
 import android.view.View;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -33,6 +34,7 @@ public class PageZoomCoordinator {
     private WebContentsObserver mWebContentsObserver;
     private GestureListenerManager mGestureListenerManager;
     private GestureStateListener mGestureListener;
+    private int mBottomControlsOffset;
 
     private View mView;
 
@@ -75,6 +77,9 @@ public class PageZoomCoordinator {
             mView.setVisibility(View.VISIBLE);
             mView.startAnimation(getInAnimation());
         }
+
+        // Adjust bottom margin for any bottom controls
+        setBottomMargin(mBottomControlsOffset);
 
         mMediator.setWebContents(webContents);
         mWebContentsObserver = new WebContentsObserver(webContents) {
@@ -125,6 +130,19 @@ public class PageZoomCoordinator {
     }
 
     /**
+     * Handle when height of bottom controls changes
+     *
+     * @param bottomControlsOffset the height of the bottom controls (if they are visible) by which
+     *         the slider should be offset in the y direction. 0 otherwise.
+     */
+    public void onBottomControlsHeightChanged(int bottomControlsOffset) {
+        mBottomControlsOffset = bottomControlsOffset;
+
+        // Set margin in case view is currently visible
+        setBottomMargin(mBottomControlsOffset);
+    }
+
+    /**
      * Clean-up views and children during destruction.
      */
     public void destroy() {
@@ -156,5 +174,15 @@ public class PageZoomCoordinator {
                 AnimationUtils.loadAnimation(mView.getContext(), R.anim.slide_out_child_bottom);
         a.setStartTime(AnimationUtils.currentAnimationTimeMillis());
         return a;
+    }
+
+    private void setBottomMargin(int bottomOffset) {
+        if (mView != null) {
+            MarginLayoutParams layout = (MarginLayoutParams) mView.getLayoutParams();
+            layout.setMargins(layout.leftMargin, layout.topMargin, layout.rightMargin,
+                    mView.getContext().getResources().getDimensionPixelSize(
+                            R.dimen.page_zoom_view_margins)
+                            + bottomOffset);
+        }
     }
 }
