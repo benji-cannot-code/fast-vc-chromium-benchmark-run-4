f919372ee983f82e9ef50785af1206b187206faf
Claudio DeSouza <cdesouza@igalia.com>
2022-08-09 19:06:29 +0000

    [CodeHealth] Settings WebUI base::Value refactoring - Part 1
    This CL corrects the use of base value for the webui handlers of a
    couple of files under webui/settings.
    
    Bug: 1187001
    Change-Id: I4dc51df9ae48773d6e1fee292f7e7ba1dd9e83ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810934
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033140}

--- chrome/browser/ui/webui/settings/on_startup_handler.h
+++ chrome/browser/ui/webui/settings/on_startup_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/values.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
@@ -42,7 +43,7 @@ class OnStartupHandler : public SettingsPageUIHandler,
                            HandleValidateStartupPage_Invalid);
 
   // Info for extension controlling the NTP or empty value.
-  base::Value GetNtpExtension();
+  base::Value::Dict GetNtpExtension();
 
   // Handler for the "getNtpExtension" message. No arguments.
   void HandleGetNtpExtension(const base::Value::List& args);
