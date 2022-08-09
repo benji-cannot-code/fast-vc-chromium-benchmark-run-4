80c7c3bfa40a39f3889985a3c804f1b10f99f78b
Patrick Monette <pmonette@chromium.org>
2022-08-09 17:59:58 +0000

    Move the NoWakeUpsForCanceledTasks feature to task_features.h
    This is in preparation of creating a WebView flag.
    
    Bug: 1189304
    Change-Id: Ia35ea466b49d914ea250da59154bad9c84217075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819983
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033099}

--- base/task/sequence_manager/sequence_manager_impl.cc
+++ base/task/sequence_manager/sequence_manager_impl.cc
@@ -195,10 +195,6 @@ SequenceManagerImpl* SequenceManagerImpl::GetCurrent() {
   return GetTLSSequenceManagerImpl()->Get();
 }
 
-// static
-const Feature SequenceManagerImpl::kNoWakeUpsForCanceledTasks{
-    "NoWakeUpsForCanceledTasks", FEATURE_DISABLED_BY_DEFAULT};
-
 SequenceManagerImpl::SequenceManagerImpl(
     std::unique_ptr<internal::ThreadController> controller,
     SequenceManager::Settings settings)
