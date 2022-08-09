5650ca36ce6158a37baca0d617b2f4ef3429dd2d
Fergal Daly <fergal@chromium.org>
2022-08-09 08:57:30 +0000

    Adds `unload` to Permissions-Policy.
    This policy controls whether unload handlers can be set.
    
    See https://github.com/w3c/webappsec-permissions-policy/issues/444
    
    Bug: 1324111
    Change-Id: Ia7c418e7ca3131f5102fde407011e00048a94182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750822
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032917}

---
+++ third_party/blink/web_tests/external/wpt/permissions-policy/experimental-features/unload-disallowed.tentative.window.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title='unload' Policy : disallowed when header is ()
+// META: script=/common/dispatcher/dispatcher.js
+// META: script=/common/utils.js
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+// META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
+// META: script=./resources/unload-helper.js
+
+'use strict';
+
+// Check that unload can be disabled by policy in main frame and subframe.
+promise_test(async t => {
+  const rcHelper =
+      new RemoteContextHelper({scripts: ['./resources/unload-helper.js']});
+  // In the same browsing context group to ensure BFCache is not used.
+  const main = await rcHelper.addWindow(
+      {headers: [['Permissions-Policy', 'unload=()']]},
+  );
+  const subframe = await main.addIframe();
+  await assertWindowRunsUnload(subframe, 'subframe', {shouldRunUnload: false});
+  await assertWindowRunsUnload(main, 'main', {shouldRunUnload: false});
+});
