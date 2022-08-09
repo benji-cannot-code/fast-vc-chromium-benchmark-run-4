4f459687dca8d12ae3237d93515b591b30892440
James Cook <jamescook@chromium.org>
2022-08-09 22:39:32 +0000

    chromeos: Remove unneeded calls to DBusThreadManager::Initialize, pt 9
    Now that D-Bus clients are initialized individually, most tests don't
    need to call DBusThreadManager::Initialize(). Clean up a test in
    //components/soda.
    
    Bug: 647367
    Test: components_unittests
    Change-Id: I913265c2471bcead654ee822b24e61cd55946fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820164
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033246}

--- components/soda/soda_installer_impl_chromeos_unittest.cc
+++ components/soda/soda_installer_impl_chromeos_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
-#include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/dlcservice/fake_dlcservice_client.h"
 #include "components/live_caption/pref_names.h"
 #include "components/prefs/testing_pref_service.h"
@@ -49,7 +48,6 @@ class SodaInstallerImplChromeOSTest : public testing::Test {
     pref_service_->registry()->RegisterStringPref(
         prefs::kLiveCaptionLanguageCode, kUsEnglishLocale);
 
-    chromeos::DBusThreadManager::Initialize();
     chromeos::DlcserviceClient::InitializeFake();
     fake_dlcservice_client_ = static_cast<chromeos::FakeDlcserviceClient*>(
         chromeos::DlcserviceClient::Get());
@@ -58,7 +56,6 @@ class SodaInstallerImplChromeOSTest : public testing::Test {
   void TearDown() override {
     soda_installer_impl_.reset();
     pref_service_.reset();
-    chromeos::DBusThreadManager::Shutdown();
     chromeos::DlcserviceClient::Shutdown();
   }
 
