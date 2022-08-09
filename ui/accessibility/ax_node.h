c7d9b91255584b78e03bdfd82f0335f4124d5d6d
Nektarios Paisios <nektar@chromium.org>
2022-08-09 01:39:00 +0000

    Views Unification: Move GetInlineTextRect to AXNode and fix UTF16 conversion bugs
    This patch adds a UTF8 and a UTF16 version of a method that computes the bounding box of
    a range of characters inside an AXNode.
    The corresponding method in BrowserAccessibility is redirected to use the new methods.
    The new methods are given names that match with the convensions in AXNode.
    The UTF-8 version of the method can now correctly convert from UTF-8 to UTF-16 offsets
    because (weirdly) the AXNodeData always stores the node's character offsets assuming UTF-16 encoding.
    
    R=aleventhal@chromium.org, dlibby@microsoft.com, benjamin.beaudry@microsoft.com
    
    AX-Relnotes: n/a.
    Bug: 1049261
    Change-Id: I97efe934ef349a6decc4ff9a58747a2f95b3261a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802993
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032824}

--- ui/accessibility/ax_node.h
+++ ui/accessibility/ax_node.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/accessibility/ax_text_attributes.h"
 #include "ui/accessibility/ax_tree_id.h"
+#include "ui/gfx/geometry/rect_f.h"
 
 namespace ui {
 
@@ -515,6 +516,18 @@ class AX_EXPORT AXNode final {
   // grapheme clusters.
   int GetTextContentLengthUTF8() const;
   int GetTextContentLengthUTF16() const;
+
+  // Returns the smallest bounding box that can enclose the given range of
+  // characters in the node's text contents. The bounding box is relative to
+  // this node's coordinate system as specified in
+  // `AXNodeData::relative_bounds`.
+  //
+  // Note that `start_offset` and `end_offset` are either in UTF8 or UTF16 code
+  // units, not in grapheme clusters.
+  gfx::RectF GetTextContentRangeBoundsUTF8(int start_offset,
+                                           int end_offset) const;
+  gfx::RectF GetTextContentRangeBoundsUTF16(int start_offset,
+                                            int end_offset) const;
 
   // Returns a string representing the language code.
   //
