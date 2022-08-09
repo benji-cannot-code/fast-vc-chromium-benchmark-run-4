06a74e4ce6b0bdec44a7b8779a46813c26ac5286
Daniel Rubery <drubery@chromium.org>
2022-08-09 05:08:19 +0000

    Obsolete PasswordProtection.DomFeatureExtractionDuration
    This histogram has been historically stable, and we aren't closely
    tracking the latency due to this feature this far after the launch.
    
    Fixed: 1349022
    Change-Id: Icb98129b0021f69e7040f1d928d5ac213af4cdb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3818028
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032876}

--- components/safe_browsing/content/browser/password_protection/password_protection_request_content.cc
+++ components/safe_browsing/content/browser/password_protection/password_protection_request_content.cc
@@ -156,7 +156,6 @@ void PasswordProtectionRequestContent::GetDomFeatures() {
               &PasswordProtectionRequestContent::OnGetDomFeatureTimeout,
               AsWeakPtr()),
           base::Milliseconds(kDomFeatureTimeoutMs));
-  dom_feature_start_time_ = base::TimeTicks::Now();
 }
 
 void PasswordProtectionRequestContent::OnGetDomFeatures(
@@ -194,9 +193,6 @@ void PasswordProtectionRequestContent::OnGetDomFeatures(
     request_proto_->mutable_dom_features()->set_model_version(
         dom_features_request.model_version());
   }
-
-  UMA_HISTOGRAM_TIMES("PasswordProtection.DomFeatureExtractionDuration",
-                      base::TimeTicks::Now() - dom_feature_start_time_);
 
   if (IsVisualFeaturesEnabled()) {
     MaybeCollectVisualFeatures();
