cb8fddd871d8ef804f60c23939ee67e819a56d14
Ayu Ishii <ayui@chromium.org>
2022-08-09 21:44:09 +0000

    Quota: Update tests away from webkitStorageInfo
    This change updates tests away from legacy webkitStorageInfo
    quota API in preparation for its removal.
    
    I2R: https://groups.google.com/u/1/a/chromium.org/g/blink-dev/c/HPuDXPKwW_s
    
    Bug: 695586
    Change-Id: I78c30d2d35b89b935c7bfbb9a5d932fc2804f0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817163
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033211}

--- third_party/blink/web_tests/external/wpt/reporting/resources/generate-report-once.py
+++ third_party/blink/web_tests/external/wpt/reporting/resources/generate-report-once.py
@@ -30,6 +30,6 @@ def main(request, response):
 <meta charset=utf-8>
 <title>Generate deprecation report</title>
 <script>
-  window.webkitStorageInfo;
+  webkitRequestAnimationFrame(() => {});
 </script>
 """
