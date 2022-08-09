cb069e34b204ca00e8d02de4ddde9ada1368bb9e
Dmitrii Kuragin <kuragin@chromium.org>
2022-08-09 01:36:13 +0000

    Simplify |GetValidIconUrlsToDownload| implementation.
    Extract Function[s] refactoring in order to :
     - Make the implementation shorter.
     - Replace explanatory comments by function names.
     - Remove duplicates.
    
    Change-Id: Id13987f8efc9d1105437ff87662058bb580a00c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3803710
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032821}

--- chrome/browser/web_applications/web_app_install_utils.cc
+++ chrome/browser/web_applications/web_app_install_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <array>
+#include <iterator>
 #include <map>
 #include <ostream>
 #include <set>
@@ -18,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/containers/contains.h"
+#include "base/containers/cxx20_erase_map.h"
+#include "base/containers/extend.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/containers/flat_tree.h"
@@ -27,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_base.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -43,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_sources.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
+#include "components/services/app_service/public/cpp/file_handler.h"
 #include "components/services/app_service/public/cpp/icon_info.h"
 #include "components/services/app_service/public/cpp/protocol_handler_info.h"
 #include "components/services/app_service/public/cpp/share_target.h"
@@ -596,38 +601,66 @@ void UpdateWebAppInfoFromManifest(const blink::mojom::Manifest& manifest,
   web_app_info->tab_strip = manifest.tab_strip;
 }
 
-base::flat_set<GURL> GetValidIconUrlsToDownload(
-    const WebAppInstallInfo& web_app_info) {
-  base::flat_set<GURL> web_app_info_icon_urls;
-  // App icons.
+namespace {
+
+std::vector<GURL> GetAppIconUrls(const WebAppInstallInfo& web_app_info) {
+  std::vector<GURL> urls;
+
   for (const apps::IconInfo& info : web_app_info.manifest_icons) {
-    if (!info.url.is_valid())
-      continue;
-    web_app_info_icon_urls.insert(info.url);
+    urls.push_back(info.url);
   }
 
-  // Shortcut icons.
-  for (const auto& shortcut : web_app_info.shortcuts_menu_item_infos) {
+  return urls;
+}
+
+std::vector<GURL> GetShortcutIcons(const WebAppInstallInfo& web_app_info) {
+  std::vector<GURL> urls;
+  for (const WebAppShortcutsMenuItemInfo& shortcut :
+       web_app_info.shortcuts_menu_item_infos) {
     for (IconPurpose purpose : kIconPurposes) {
-      for (const auto& icon :
+      for (const WebAppShortcutsMenuItemInfo::Icon& icon :
            shortcut.GetShortcutIconInfosForPurpose(purpose)) {
-        if (!icon.url.is_valid())
-          continue;
-        web_app_info_icon_urls.insert(icon.url);
+        urls.push_back(icon.url);
       }
     }
   }
 
-  // File handling icons.
-  for (const auto& file_handler : web_app_info.file_handlers) {
-    for (const auto& icon : file_handler.downloaded_icons) {
-      if (!icon.url.is_valid())
-        continue;
-      web_app_info_icon_urls.insert(icon.url);
+  return urls;
+}
+
+std::vector<GURL> GetFileHandlingIcons(const WebAppInstallInfo& web_app_info) {
+  std::vector<GURL> urls;
+
+  for (const apps::FileHandler& file_handler : web_app_info.file_handlers) {
+    for (const apps::IconInfo& icon : file_handler.downloaded_icons) {
+      urls.push_back(icon.url);
     }
   }
 
-  return web_app_info_icon_urls;
+  return urls;
+}
+
+base::flat_set<GURL> RemoveDuplicates(const std::vector<GURL>& from_urls) {
+  return base::flat_set<GURL>{from_urls};
+}
+
+void RemoveInvalidUrls(std::vector<GURL>& urls) {
+  base::EraseIf(urls, [](const GURL& url) { return !url.is_valid(); });
+}
+
+}  // namespace
+
+base::flat_set<GURL> GetValidIconUrlsToDownload(
+    const WebAppInstallInfo& web_app_info) {
+  std::vector<GURL> icon_urls;
+
+  base::Extend(icon_urls, GetAppIconUrls(web_app_info));
+  base::Extend(icon_urls, GetShortcutIcons(web_app_info));
+  base::Extend(icon_urls, GetFileHandlingIcons(web_app_info));
+
+  RemoveInvalidUrls(std::ref(icon_urls));
+
+  return RemoveDuplicates(icon_urls);
 }
 
 void PopulateOtherIcons(WebAppInstallInfo* web_app_info,
