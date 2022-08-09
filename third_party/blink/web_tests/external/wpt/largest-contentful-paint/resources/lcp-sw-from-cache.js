86cce1949bb85287140fba3571c2d2fd1a0a5532
Noam Rosenthal <nrosenthal@chromium.org>
2022-08-09 12:40:16 +0000

    Constructed service-worker responses are TAO-OK
    Service worker constructed responses are same-origin,
    so they are also OK for Timing Allow Origin (TAO).
    
    This fixes a bug where LCP erroneously thought that constructed responses (e.g. Response(...)) are not TAO-protected.
    
    This hasn't popped as a bug for ResourceTiming (RT) because RT does an extra same-origin check.
    
    Bug: 1348001
    Change-Id: I09f1ee2cd75a19fb092b0b6032c3b94ee82b803b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798708
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032982}

---
+++ third_party/blink/web_tests/external/wpt/largest-contentful-paint/resources/lcp-sw-from-cache.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener("fetch", e => {
+    if (e.request.url.endsWith('green.svg')) {
+        e.respondWith(new Response(`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="50">
+        <rect fill="lime" width="100" height="50"/>
+        </svg>
+        `, { headers: { 'Content-Type': 'image/svg+xml' } }));
+    }
+});
