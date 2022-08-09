b501a0b493182f7d0e00257cf6169496d1b20522
ginnnnnnny <ginnyhuang@chromium.org>
2022-08-09 21:15:21 +0000

    [iOS] Add metric to how long it takes user to accept ToS
    This metric is fired when the first promo screen in FRE will dismiss.
    This is based on the understanding that in all experiments, tapping the
    primary button on the first FRE promo screen indicates user's acceptance
    of Chrome's Terms of Services.
    
    Bug: 1351254
    Change-Id: Ieae7b98d82eb95b234e7d820c4fdd6fa1bf65bda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3818144
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033196}

--- ios/chrome/browser/ui/first_run/first_run_coordinator.mm
+++ ios/chrome/browser/ui/first_run/first_run_coordinator.mm
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "base/metrics/histogram_functions.h"
-#include "base/notreached.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#include "ios/chrome/browser/first_run/first_run_metrics.h"
-#include "ios/chrome/browser/main/browser.h"
+#import "base/notreached.h"
+#import "base/time/time.h"
+#import "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/first_run/first_run_metrics.h"
+#import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/ui/authentication/signin_sync/signin_sync_coordinator.h"
 #import "ios/chrome/browser/ui/first_run/default_browser/default_browser_screen_coordinator.h"
 #import "ios/chrome/browser/ui/first_run/first_run_screen_delegate.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) ScreenProvider* screenProvider;
 @property(nonatomic, strong) ChromeCoordinator* childCoordinator;
 @property(nonatomic, strong) UINavigationController* navigationController;
+@property(nonatomic, strong) NSDate* firstScreenStartTime;
 
 // YES if First Run was completed.
 @property(nonatomic, assign) BOOL completed;
@@ -57,8 +59,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   [self presentScreen:[self.screenProvider nextScreenType]];
+  __weak FirstRunCoordinator* weakSelf = self;
   void (^completion)(void) = ^{
     base::UmaHistogramEnumeration("FirstRun.Stage", first_run::kStart);
+    weakSelf.firstScreenStartTime = [NSDate now];
   };
   [self.navigationController setNavigationBarHidden:YES animated:NO];
   [self.baseViewController presentViewController:self.navigationController
@@ -90,6 +94,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)willFinishPresenting {
   [self.childCoordinator stop];
   self.childCoordinator = nil;
+  // Usually, finishing presenting the first FRE screen signifies that the user
+  // has accepted Terms of Services. Therefore, we can use the time it takes the
+  // first screen to be visible as the time it takes a user to accept Terms of
+  // Services.
+  if (self.firstScreenStartTime) {
+    base::TimeDelta delta =
+        base::Time::Now() - base::Time::FromNSDate(self.firstScreenStartTime);
+    base::UmaHistogramTimes("FirstRun.TermsOfServicesPromoDisplayTime", delta);
+    self.firstScreenStartTime = nil;
+  }
   [self presentScreen:[self.screenProvider nextScreenType]];
 }
 
