3e76a14ff07cc35cc5993f094663595b81b0d870
Martin Bidlingmaier <mbid@google.com>
2022-08-09 08:22:21 +0000

    [CrOS] Add cryptohome recovery settings toggle
    This commit adds a toggle to enable or disable cryptohome recovery on
    the lock screen subpage of the os settings webui app. The toggle is only
    shown if recovery is supported for the current user. As of this commit,
    the mojo service backing this toggle is a fake, and reports recovery to
    be available only if the CryptohomeRecoverySetup feature is enabled.
    
    Postponed to later CLs: Testing, and perhaps a speed bump to warn the
    user about potential data loss.
    
    Bug: b/239416325
    Change-Id: Ie8709532311a8f5b9f3801cf78583ae079bac0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579110
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032913}

--- chrome/browser/resources/settings/chromeos/os_people_page/lock_state_behavior.js
+++ chrome/browser/resources/settings/chromeos/os_people_page/lock_state_behavior.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
 import {WebUIListenerBehavior, WebUIListenerBehaviorInterface} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
-import {AuthFactorConfig, RecoveryFactorEditor} from 'chrome://resources/mojo/chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom-webui.js';
+import {AuthFactorConfig, AuthFactorConfigInterface, RecoveryFactorEditor, RecoveryFactorEditorInterface} from 'chrome://resources/mojo/chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom-webui.js';
 
 /**
  * @fileoverview
@@ -209,6 +209,20 @@ export class LockStateBehaviorInterface {
      * @type {QuickUnlockPrivate}
      */
     this.quickUnlockPrivate;
+
+    /**
+     * Interface for calls to the ash AuthFactorConfig service. May be
+     * overridden by tests.
+     * @type {AuthFactorConfigInterface}
+     */
+    this.authFactorConfig;
+
+    /**
+     * Interface for calls to the ash RecoveryFactorEditor service.  May be
+     * overridden by tests.
+     * @type {RecoveryFactorEditorInterface}
+     */
+    this.recoveryFactorEditor;
   }
 
   /**
