95d8cad8e05a0ee37c6eca4be78bbe1c6f3c3f15
Sammie Quon <sammiequon@chromium.org>
2022-08-09 18:04:32 +0000

    float: Update float button when entering or exiting float state.
    This will help with making float work for lacros as it gives a visual
    indication of whether float was successful on the header, which will
    be on the lacros side.
    
    Bug: 1347534
    Test: manual
    Change-Id: I6876fc2ccb862654d66e2f7ff9c9742d82003592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817664
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033107}

--- chromeos/ui/frame/frame_header.cc
+++ chromeos/ui/frame/frame_header.cc
@@ -280,6 +280,10 @@ void FrameHeader::OnShowStateChanged(ui::WindowShowState show_state) {
   LayoutHeaderInternal();
 }
 
+void FrameHeader::OnFloatStateChanged() {
+  LayoutHeaderInternal();
+}
+
 void FrameHeader::SetLeftHeaderView(views::View* left_header_view) {
   left_header_view_ = left_header_view;
 }
@@ -416,6 +420,7 @@ void FrameHeader::LayoutHeaderInternal() {
   caption_button_container()->SetButtonImage(
       views::CAPTION_BUTTON_ICON_MAXIMIZE_RESTORE,
       use_restore_frame ? maximize_icon : restore_icon);
+  caption_button_container()->UpdateFloatButton();
   UpdateSnapIcons();
 
   caption_button_container()->UpdateSizeButtonTooltip(use_restore_frame);
