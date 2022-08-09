7353b008cfd648bcd7717eac7b30888e3f32be7d
Bence Béky <bnc@chromium.org>
2022-08-09 18:31:12 +0000

    Roll src/net/third_party/quiche/src/ fe9c73029..3ebaf6872 (17 commits)
    https://quiche.googlesource.com/quiche.git/+log/fe9c73029c24..3ebaf6872ded
    
    $ git log fe9c73029..3ebaf6872 --date=short --no-merges --format='%ad %ae %s'
    2022-08-09 vasilvv Disable some of the QUIC end-to-end tests in Chrome.
    2022-08-08 haoyuewang Internal change
    2022-08-08 diannahu Move MockHttpDecoderVisitor from http_decoder_test.cc to quic_test_utils.h.
    2022-08-08 vasilvv Fix flakiness in QuicEventLoop unit tests
    2022-08-08 vasilvv Clarify CONTRIBUTING.md
    2022-08-05 bnc Delete quic_flags_impl.{h,cc}.
    2022-08-05 bnc Tighten up AppropriateActionTakenWhenHeadersTooLongWithTooMuchInput test.
    2022-08-05 vasilvv Switch QuicTestClient to use QuicDefaultClient instead of QuicClient
    2022-08-04 vasilvv Remove leftover epoll dependencies for quic_server.
    2022-08-04 martinduke Extract Quiche default connection ID generation algorithms into a library. When integrated into quiche, similar functions can be removed.
    2022-08-03 diannahu Move NoopEncoderStreamErrorDelegate from qpack_decoder_test_utils to qpack_decoder.
    2022-08-03 martinduke Proposed interface for "Connection ID generators" that will allow different algorithms for different platforms that deploy QUICHE.
    2022-08-03 martinduke Avoid creating session when server_connection_id matches, and send CONNECTION_CLOSE instead of delivering the packet if the original connection IDs don't match.
    2022-08-03 abhisinghx Internal change
    2022-08-03 haoyuewang Internal change
    2022-08-02 bnc Remove SpdyHeaderBlock alias.
    2022-08-02 bnc Remove qpack_encoder_test_utils.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: Ie97de5ca00eb95adb4f98f0662e1992808d20e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819708
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033125}

--- components/cronet/android/cronet_bidirectional_stream_adapter.cc
+++ components/cronet/android/cronet_bidirectional_stream_adapter.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_util.h"
 #include "net/ssl/ssl_info.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_packets.h"
-#include "net/third_party/quiche/src/quiche/spdy/core/spdy_header_block.h"
+#include "net/third_party/quiche/src/quiche/spdy/core/http2_header_block.h"
 #include "net/url_request/url_request_context.h"
 #include "url/gurl.h"
 
