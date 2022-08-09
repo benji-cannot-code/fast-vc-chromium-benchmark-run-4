519250fdd488f169b6ffa59c14c06c8f2090fd95
Jeroen Dhollander <jeroendh@google.com>
2022-08-09 11:04:58 +0000

    Fix clangd auto include of base::expected
    When using `base::expected`, clangd auto-imported
    "base/type/expected_internal.h" instead of "base/types/expected.h"
    
    Bug: N/A
    Test: Manually ensured it worked
    Change-Id: I7fbbb186f0863552df516d02d74b9028d2f92f69
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819265
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032954}

--- base/types/expected.h
+++ base/types/expected.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
-#include "base/types/expected_internal.h"
+#include "base/types/expected_internal.h"  // IWYU pragma: export
 #include "third_party/abseil-cpp/absl/utility/utility.h"
 
 // Class template `expected<T, E>` is a vocabulary type which contains an
--- base/types/expected_internal.h
+++ base/types/expected_internal.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_TYPES_EXPECTED_INTERNAL_H_
 #define BASE_TYPES_EXPECTED_INTERNAL_H_
 
+// IWYU pragma: private, include "base/types/expected.h"
 #include <type_traits>
 #include <utility>
 
