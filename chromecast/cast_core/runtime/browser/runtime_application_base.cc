ce335714f0158494a310bfb1a8a1b882f73075af
Vigen Issahhanjan <vigeni@google.com>
2022-08-09 19:16:50 +0000

    Notify Core that app started when page is loaded.
    Bug: none
    Test: Ran on device
    Change-Id: I48ad278fbc2650545808c87b08f01f8fc5053dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813954
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033144}

--- chromecast/cast_core/runtime/browser/runtime_application_base.cc
+++ chromecast/cast_core/runtime/browser/runtime_application_base.cc
@@ -337,6 +337,8 @@ void RuntimeApplicationBase::OnPageLoaded() {
     cast_web_view_->window()->CreateWindow(mojom::ZOrder::APP,
                                            VisibilityPriority::HIDDEN);
   }
+
+  NotifyApplicationStarted();
 }
 
 void RuntimeApplicationBase::HandlePostMessage(
