fa4e7e511dba817f26ca03285935955c96903abc
Steven Bennetts <stevenjb@chromium.org>
2022-08-09 17:42:09 +0000

    NetworkState: Add additional unit tests for PortalState
    The NetworkState unit tests include PoralState tests, however
    the support classes do not. This CL helps address that gap.
    
    BUG=b:207069182
    TEST=unit_tests, chromeos_unittests, browser_tests
    
    Change-Id: Iab62e4c4e632013120b86bd0ae9433e4cf21793e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813736
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033069}

--- chrome/browser/ash/net/network_portal_detector_impl_browsertest.cc
+++ chrome/browser/ash/net/network_portal_detector_impl_browsertest.cc
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/network/network_portal_notification_controller.h"
 #include "chrome/common/pref_names.h"
+#include "chromeos/ash/components/network/network_handler.h"
+#include "chromeos/ash/components/network/network_state.h"
+#include "chromeos/ash/components/network/network_state_handler.h"
 #include "chromeos/ash/components/network/portal_detector/network_portal_detector.h"
 #include "chromeos/ash/components/network/portal_detector/network_portal_detector_strategy.h"
 #include "chromeos/dbus/shill/shill_service_client.h"
@@ -174,6 +177,15 @@ IN_PROC_BROWSER_TEST_F(NetworkPortalDetectorImplBrowserTest,
   RestartDetection();
   EXPECT_EQ(kProbeUrl, get_probe_url());
   CompleteURLFetch(net::OK, 200, nullptr);
+
+  // Ensure that NetworkStateHandler is updated.
+  const chromeos::NetworkState* default_network =
+      chromeos::NetworkHandler::Get()
+          ->network_state_handler()
+          ->DefaultNetwork();
+  ASSERT_TRUE(default_network);
+  EXPECT_EQ(default_network->GetPortalState(),
+            chromeos::NetworkState::PortalState::kPortal);
 
   // Check that the default network is detected as behind a portal and that
   // notification is displayed.
