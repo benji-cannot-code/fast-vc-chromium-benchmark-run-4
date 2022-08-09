8fe826cfc3c7630b05622694b6581cfc9961f7bf
Benjamin Williams <bwwilliams@google.com>
2022-08-09 19:56:48 +0000

    Expose appIsTerminating from AppState.
    The Promos Manager needs to know whether or not the app is currently
    terminating. This change exposes the app termination state for the
    Promos Manager to use.
    
    Bug: 1349481
    Change-Id: I151548496f891187e05ab696087c530c160573d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806421
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033157}

--- ios/chrome/browser/promos_manager/promos_manager.mm
+++ ios/chrome/browser/promos_manager/promos_manager.mm
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/promos_manager/promos_manager.h"
 
-#import <iostream>  // TODO(crbug.com/1347825): Remove import
-
-#import "base/json/json_writer.h"  // TODO(crbug.com/1347825): Remove import
 #import "base/values.h"
 #import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/pref_names.h"
