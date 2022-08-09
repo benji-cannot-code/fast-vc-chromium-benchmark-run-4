1d0eb4637ae1049603d534ae373aa21ebffe274d
Sylvain Defresne <sdefresne@chromium.org>
2022-08-09 12:23:26 +0000

    [ios] Fix typo in tos_commands.h
    In https://crrev.com/c/3738284, a copy-n-paste mishap caused
    a file name to be added to a comment in tos_commands.h. This
    CL removes this file name.
    
    Bug: none
    Change-Id: I02bcf32f0ac094e79357683e841cd88510a1b777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817990
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032979}

--- ios/chrome/browser/ui/commands/tos_commands.h
+++ ios/chrome/browser/ui/commands/tos_commands.h
@@ -15,8 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Shows the ToS page.
 - (void)showTOSPage;
 
-// Closesios/chrome/browser/ui/first_run/welcome/tos_coordinator.mm the ToS
-// page.
+// Closes the ToS page.
 - (void)closeTOSPage;
 
 @end
