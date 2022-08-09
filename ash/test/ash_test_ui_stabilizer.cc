c9acc7f8b2848e86fdda3b753faf1b268ffd8bb6
Andrew Xu <andrewxu@chromium.org>
2022-08-09 17:52:34 +0000

    [Pixel Diff Ash] Make pixel tests under RTL easier
    This CL adds a bool member to the pixel test init data structure.
    The new member specifies whether the UI right-to-left (i.e. RTL)
    layout should be used.
    
    This CL also adapts an existing pixel test to run under RTL.
    
    Bug: 1347171
    Change-Id: I140c149269da37db96e881045356b03d69e3dcc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811353
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033086}

--- ash/test/ash_test_ui_stabilizer.cc
+++ ash/test/ash_test_ui_stabilizer.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/style/dark_light_mode_controller_impl.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
+#include "base/command_line.h"
+#include "base/i18n/base_i18n_switches.h"
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
@@ -39,7 +41,12 @@ gfx::ImageSkia CreateImage(const gfx::Size& image_size, SkColor color) {
 AshTestUiStabilizer::AshTestUiStabilizer(const pixel_test::InitParams& params)
     : params_(params),
       scoped_locale_(base::test::ScopedRestoreICUDefaultLocale(kLocale)),
-      time_zone_(base::test::ScopedRestoreDefaultTimezone(kTimeZone)) {}
+      time_zone_(base::test::ScopedRestoreDefaultTimezone(kTimeZone)) {
+  if (params.under_rtl) {
+    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        ::switches::kForceUIDirection, ::switches::kForceDirectionRTL);
+  }
+}
 
 AshTestUiStabilizer::~AshTestUiStabilizer() = default;
 
