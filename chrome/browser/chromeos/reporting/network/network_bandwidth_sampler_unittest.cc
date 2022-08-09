d17224b290922e7b4d1c4355f0fbc812776775fb
Vignesh Shenvi <vshenvi@google.com>
2022-08-09 01:37:17 +0000

    Move NetworkBandwidthSampler to //chrome/browser/chromeos
    This change moves the NetworkBandwidthSampler to
    //chrome/browser/chromeos so it can be leveraged by Lacros to
    report network bandwidth usage metrics for the Contact Center use case.
    We also update pre-existing Ash references to use this new source path.
    
    Bug: b:241486730
    Change-Id: I545421aa74262bd5e68d6be1307dda8d10b3ef0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814158
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Auto-Submit: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1032822}

--- chrome/browser/chromeos/reporting/network/network_bandwidth_sampler_unittest.cc
+++ chrome/browser/chromeos/reporting/network/network_bandwidth_sampler_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/network/network_bandwidth_sampler.h"
+#include "chrome/browser/chromeos/reporting/network/network_bandwidth_sampler.h"
 
 #include "base/test/task_environment.h"
 #include "chrome/browser/browser_process.h"
