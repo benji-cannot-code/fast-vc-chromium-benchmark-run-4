1e52c661b8aa54f215cd6a2634445f618f3b1367
Ivana Žužić <izuzic@google.com>
2022-08-09 15:55:07 +0000

    [PMIntegrationTests] Add a password updating integration test
    The test checks if a stored credential is updated after using
    a different password for the same site while using the same username.
    
    The test has the following workflow:
    - Fake password store is filled with a credential for the test page
    - Test loads a page which contains a form for changing the password
    - The test waits until the password field is labeled
    - Password field is focused to bring up TTF
    - The test waits for TTF to show
    - TTF button is clicked so it inserts the credential in the form
    - The test waits for TTF to close
    - The new password is entered and repeated
    - The form is submitted
    - The test waits for the success page to load
    - The test waits for the update message to show
    - The update is confirmed
    - It's checked that the credential was updated
    
    A form that changes the password was convenient for this test because it does not trigger the TouchToFill autosubmission. The new simple_change_password_form.html file was added because there was no other simple form for changing a password.
    
    Bug: 1329856
    Change-Id: I6ebf6fe3bbe173dda65755b7885c6264e2da1c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793849
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033044}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreBridge.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreBridge.java
@@ -113,6 +113,14 @@ public class PasswordStoreBridge {
     }
 
     /**
+     * Empties the password store.
+     */
+    @VisibleForTesting
+    public void clearAllPasswords() {
+        PasswordStoreBridgeJni.get().clearAllPasswords(mNativePasswordStoreBridge);
+    }
+
+    /**
      * Destroys its C++ counterpart.
      */
     public void destroy() {
@@ -156,6 +164,7 @@ public class PasswordStoreBridge {
         int getPasswordStoreCredentialsCount(long nativePasswordStoreBridge);
         void getAllCredentials(
                 long nativePasswordStoreBridge, PasswordStoreCredential[] credentials);
+        void clearAllPasswords(long nativePasswordStoreBridge);
         void destroy(long nativePasswordStoreBridge);
     }
 }
