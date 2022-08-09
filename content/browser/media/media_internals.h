a4c7cc9b8b3a42029f4719a494fd2f855d4bef11
Iman Saboori <isaboori@google.com>
2022-08-09 12:53:16 +0000

    Remove deprecated base::Value APIs
    [Code Health Rotation] Update media internals to remove deprecated base::Value APIs.
    
    Bug: 1351111
    Change-Id: I9d4e072288ecac7f3936350c1cee709884d8cba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816867
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032985}

--- content/browser/media/media_internals.h
+++ content/browser/media/media_internals.h
@@ -157,7 +157,7 @@ class CONTENT_EXPORT MediaInternals : public media::AudioLogFactory,
   void UpdateAudioLog(AudioLogUpdateType type,
                       const std::string& cache_key,
                       const std::string& function,
-                      const base::Value& value);
+                      const base::Value::Dict& value);
 
   std::unique_ptr<AudioLogImpl> CreateAudioLogImpl(AudioComponent component,
                                                    int component_id,
@@ -171,7 +171,7 @@ class CONTENT_EXPORT MediaInternals : public media::AudioLogFactory,
   std::map<int, std::list<media::MediaLogRecord>> saved_events_by_process_;
 
   // Must only be accessed on the IO thread.
-  base::Value video_capture_capabilities_cached_data_{base::Value::Type::LIST};
+  base::Value::List video_capture_capabilities_cached_data_;
 
   NotificationRegistrar registrar_;
 
@@ -182,7 +182,7 @@ class CONTENT_EXPORT MediaInternals : public media::AudioLogFactory,
   // All variables below must be accessed under |lock_|.
   base::Lock lock_;
   bool can_update_;
-  base::DictionaryValue audio_streams_cached_data_;
+  base::Value::Dict audio_streams_cached_data_;
   int owner_ids_[media::AudioLogFactory::AUDIO_COMPONENT_MAX];
 };
 
