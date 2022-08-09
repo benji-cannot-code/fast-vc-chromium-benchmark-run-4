fb56d6eaa5c821b7eae7ef7f9ee58eb317189516
Adam Rice <ricea@chromium.org>
2022-08-09 11:36:36 +0000

    Revert "Add CHECKs to narrow down HttpCache::DoneWithEntry crash"
    This reverts commit e0254a14b8e2706d80ef4c0a4adecc82aa93e95f.
    
    Reason for revert: No longer needed as we have identified the
    CHECK failure: https://bugs.chromium.org/p/chromium/issues/detail?id=1348096#c11
    
    Original change's description:
    > Add CHECKs to narrow down HttpCache::DoneWithEntry crash
    >
    > Bug: 1348096
    > Change-Id: I1c0679aee42637620b84e5dd784e30828fffd7f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788934
    > Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1029144}
    
    Bug: 1348096
    Change-Id: Id92278915f2a6bcc0d96814ded196538db67af68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3818046
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1032964}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -3486,8 +3486,7 @@ void HttpCache::Transaction::DoneWithEntry(bool entry_is_complete) {
   if (!safe_entry_)
     return;
 
-  cache_->DoneWithEntry(entry(), this, entry_is_complete, partial_ != nullptr,
-                        safe_entry_.value());
+  cache_->DoneWithEntry(entry(), this, entry_is_complete, partial_ != nullptr);
   safe_entry_ = absl::nullopt;
   mode_ = NONE;  // switch to 'pass through' mode
 }
@@ -3497,7 +3496,7 @@ void HttpCache::Transaction::DoneWithEntryForRestartWithCache() {
     return;
 
   cache_->DoneWithEntry(entry(), this, /*entry_is_complete=*/true,
-                        partial_ != nullptr, safe_entry_.value());
+                        partial_ != nullptr);
   safe_entry_ = absl::nullopt;
   new_entry_ = nullptr;
 }
@@ -3525,7 +3524,7 @@ int HttpCache::Transaction::OnCacheReadError(int result, bool restart) {
     // or setting mode to NONE at this point by invoking the wrapper
     // DoneWithEntry.
     cache_->DoneWithEntry(entry(), this, true /* entry_is_complete */,
-                          partial_ != nullptr, safe_entry_.value());
+                          partial_ != nullptr);
     safe_entry_ = absl::nullopt;
     is_sparse_ = false;
     // It's OK to use PartialData::RestoreHeaders here as |restart| is only set
@@ -3567,7 +3566,7 @@ void HttpCache::Transaction::DoomPartialEntry(bool delete_object) {
   }
 
   cache_->DoneWithEntry(entry(), this, false /* entry_is_complete */,
-                        partial_ != nullptr, safe_entry_.value());
+                        partial_ != nullptr);
   safe_entry_ = absl::nullopt;
   is_sparse_ = false;
   truncated_ = false;
