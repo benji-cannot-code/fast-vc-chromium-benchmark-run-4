6707147ae32062789e49bf8e36068aaa147dee76
Tim Sergeant <tsergeant@chromium.org>
2022-08-09 22:55:03 +0000

    Intents: Show intent picker bubble using 'ShowForReason'
    Showing the bubble with 'ShowForReason' sets a11y properties correctly
    and results in a better screenreader experience than just showing the
    widget directly.
    
    Bug: 1351282, 706077
    Change-Id: I12a11fe4557017dd9c2228bb9564faf6ccad8143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819223
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033253}

--- chrome/browser/ui/views/intent_picker_bubble_view.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view.cc
@@ -526,7 +526,7 @@ views::Widget* IntentPickerBubbleView::ShowBubble(
   }
 
   DCHECK(intent_picker_bubble_->HasCandidates());
-  widget->Show();
+  intent_picker_bubble_->ShowForReason(DisplayReason::USER_GESTURE);
 
   intent_picker_bubble_->SelectDefaultItem();
   return widget;
