a15134431e7ca8d7088a38c1709ce57cfec25f63
Ahmed Mehfooz <amehfooz@chromium.org>
2022-08-09 16:23:02 +0000

    Speculative fix for crash when notifications popups are closed
    This fix addresses a crash that can happen when notification
    popups are closed while an animation is running. Explicitly
    aborting all animations before closing the widget for each
    popup should resolve this crash.
    
    Bug: 1337661
    
    Change-Id: I98d28d6674f92515386b4122607650a4f6c2c19f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817472
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033054}

--- ash/system/message_center/ash_notification_view.h
+++ ash/system/message_center/ash_notification_view.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/views/notification_view.h"
 #include "ui/message_center/views/notification_view_base.h"
 #include "ui/views/metadata/view_factory.h"
+#include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace message_center {
 class Notification;
@@ -38,7 +40,8 @@ class NotificationGroupingController;
 // and list) except custom notification.
 class ASH_EXPORT AshNotificationView
     : public message_center::NotificationViewBase,
-      public message_center::MessageCenterObserver {
+      public message_center::MessageCenterObserver,
+      public views::WidgetObserver {
  public:
   static const char kViewClassName[];
 
@@ -82,6 +85,7 @@ class ASH_EXPORT AshNotificationView
       const message_center::Notification& notification) const override;
 
   // message_center::NotificationViewBase:
+  void AddedToWidget() override;
   void Layout() override;
   void UpdateViewForExpandedState(bool expanded) override;
   void UpdateWithNotification(
@@ -192,6 +196,14 @@ class ASH_EXPORT AshNotificationView
   // message_center::MessageCenterObserver:
   void OnNotificationRemoved(const std::string& notification_id,
                              bool by_user) override;
+
+  // views::WidgetObserver:
+  void OnWidgetClosing(views::Widget* widget) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+  // Abort all currently running layer animations. This includes any animatios
+  // on child notifications for parent notification views.
+  void AbortAllAnimations();
 
   // Create or update the customized snooze button in action buttons row
   // according to the given notification.
@@ -308,6 +320,8 @@ class ASH_EXPORT AshNotificationView
 
   base::ScopedObservation<message_center::MessageCenter, MessageCenterObserver>
       message_center_observer_{this};
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
 
   base::WeakPtrFactory<AshNotificationView> weak_factory_{this};
 };
