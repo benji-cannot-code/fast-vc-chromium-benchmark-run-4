71c199e7f8f04bf4eaa44f5d88318589225321d0
Ahmed Moussa <ahmedmoussa@google.com>
2022-08-10 02:06:05 +0000

    Allowing reporting Cast Issue from within Guest/Incognito mode
    Bug: b/241147923
    Change-Id: Ib9ebab6accb1ae64faa685385e38daf54444d478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820170
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033308}

--- chrome/browser/ui/views/global_media_controls/media_toolbar_button_contextual_menu.cc
+++ chrome/browser/ui/views/global_media_controls/media_toolbar_button_contextual_menu.cc
@@ -51,8 +51,7 @@ MediaToolbarButtonContextualMenu::CreateMenuModel() {
       IDS_MEDIA_TOOLBAR_CONTEXT_SHOW_OTHER_SESSIONS);
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  if (!browser_->profile()->IsOffTheRecord() &&
-      browser_->profile()->GetPrefs()->GetBoolean(
+  if (browser_->profile()->GetPrefs()->GetBoolean(
           prefs::kUserFeedbackAllowed)) {
     menu_model->AddItemWithStringId(
         IDC_MEDIA_TOOLBAR_CONTEXT_REPORT_CAST_ISSUE,
