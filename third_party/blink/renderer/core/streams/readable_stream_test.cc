50aa5240c375d1cf6c7927a077773813c5356b73
Reilly Grant <reillyg@chromium.org>
2022-08-10 07:06:26 +0000

    Remove ExceptionState from ReadableStream::CreateByteStream()
    The version of CreateByteStream() that is called by internal C++ stream
    implementations cannot throw an exception because the start algorithm it
    provides to the underlying CreateByteStream() method never throws (it is
    a no-op). This change replaces the DCHECK that no exception has been
    thrown after construction with passing a NonThrowableExceptionState.
    
    This has the same semantic meaning but allows the removal of the input
    ExceptionState parameter and will generate a better stack trace if this
    assumption is ever violated.
    
    Bug: 614302
    Change-Id: I152725a798bf56987518df250eddaddb764b5cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820428
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033382}

--- third_party/blink/renderer/core/streams/readable_stream_test.cc
+++ third_party/blink/renderer/core/streams/readable_stream_test.cc
@@ -596,10 +596,9 @@ class ReadableByteStreamTest : public testing::Test {
   ReadableStream* Stream() const { return stream_; }
 
   void Init(ScriptState* script_state,
-            UnderlyingByteSourceBase* underlying_byte_source,
-            ExceptionState& exception_state) {
-    stream_ = ReadableStream::CreateByteStream(
-        script_state, underlying_byte_source, exception_state);
+            UnderlyingByteSourceBase* underlying_byte_source) {
+    stream_ =
+        ReadableStream::CreateByteStream(script_state, underlying_byte_source);
   }
 
   // This takes the |stream| property of ReadableStream and copies it onto the
@@ -685,8 +684,7 @@ class MockUnderlyingByteSource : public UnderlyingByteSourceBase {
 TEST_F(ReadableByteStreamTest, Construct) {
   V8TestingScope scope;
   Init(scope.GetScriptState(),
-       MakeGarbageCollected<TestUnderlyingByteSource>(scope.GetScriptState()),
-       ASSERT_NO_EXCEPTION);
+       MakeGarbageCollected<TestUnderlyingByteSource>(scope.GetScriptState()));
   EXPECT_TRUE(Stream());
 }
 
@@ -694,7 +692,7 @@ TEST_F(ReadableByteStreamTest, PullIsCalled) {
   V8TestingScope scope;
   auto* mock =
       MakeGarbageCollected<MockUnderlyingByteSource>(scope.GetScriptState());
-  Init(scope.GetScriptState(), mock, ASSERT_NO_EXCEPTION);
+  Init(scope.GetScriptState(), mock);
   // Need to run microtasks so the startAlgorithm promise resolves.
   v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
   CopyStreamToGlobal(scope);
@@ -714,7 +712,7 @@ TEST_F(ReadableByteStreamTest, CancelIsCalled) {
   V8TestingScope scope;
   auto* mock =
       MakeGarbageCollected<MockUnderlyingByteSource>(scope.GetScriptState());
-  Init(scope.GetScriptState(), mock, ASSERT_NO_EXCEPTION);
+  Init(scope.GetScriptState(), mock);
   // Need to run microtasks so the startAlgorithm promise resolves.
   v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
   CopyStreamToGlobal(scope);
@@ -771,8 +769,7 @@ TEST_F(ReadableByteStreamTest, ThrowFromPull) {
   V8TestingScope scope;
   auto* script_state = scope.GetScriptState();
   Init(script_state,
-       MakeGarbageCollected<ThrowFromPullUnderlyingByteSource>(script_state),
-       ASSERT_NO_EXCEPTION);
+       MakeGarbageCollected<ThrowFromPullUnderlyingByteSource>(script_state));
 
   auto* reader =
       Stream()->GetBYOBReaderForTesting(script_state, ASSERT_NO_EXCEPTION);
@@ -802,8 +799,7 @@ TEST_F(ReadableByteStreamTest, ThrowFromCancel) {
   V8TestingScope scope;
   auto* script_state = scope.GetScriptState();
   Init(script_state,
-       MakeGarbageCollected<ThrowFromCancelUnderlyingByteSource>(script_state),
-       ASSERT_NO_EXCEPTION);
+       MakeGarbageCollected<ThrowFromCancelUnderlyingByteSource>(script_state));
 
   auto* reader =
       Stream()->GetBYOBReaderForTesting(script_state, ASSERT_NO_EXCEPTION);
