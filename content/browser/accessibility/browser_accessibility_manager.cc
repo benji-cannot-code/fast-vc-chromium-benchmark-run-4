7d7169ce5f63543a47117526fcbb38c707015c12
Aaron Leventhal <aleventhal@google.com>
2022-08-10 06:53:45 +0000

    Reland "Exercise all a11y properties on all nodes in web tests"
    This is a reland of commit db00218f2d1b420c7214d0195839caae69c55888
    
    Reason for revert: bot compile failed
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Builder/40344/overview
    
    Original change's description:
    > Exercise all a11y properties on all nodes in web tests
    >
    > Enable debugging and sanitizer builds run with --force-renderer-accessibility to exercise the code used to fetch all a11y properties
    > for every node. By exercising the code, we can catch crashes and DCHECKs.
    >
    > Both conditions need to be present, and this restricts where runs
    > while being useful to catch issues:
    > - Runs for all web tests on builder
    > linux-blink-web-tests-force-accessibility-rel
    > - Runs for clusterfuzz when --force-renderer-accessibility is used
    > - Runs for local users that run a debug build with --force-renderer-accessibility.
    > - Does not run for a11y dump tree/event/etc. tests
    > - Does not run in Albatross builds unless users run with --force-renderer-accessibility, in which case it may provide some useful results.
    >
    > Bug: None
    > Change-Id: Iaa2a4c6dc0a0bd812639f4450ae21260105b3dd2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814279
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    > Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    > Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1033245}
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng
    Change-Id: Idf506c2038aa80b00bfdd1e73a1d3729a591d738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822281
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033380}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -31,6 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_tree_serializer.h"
 #include "ui/base/buildflags.h"
 
+#if defined(AX_FAIL_FAST_BUILD)
+#include "base/command_line.h"
+#include "content/public/browser/ax_inspect_factory.h"
+#include "content/public/common/content_switches.h"
+#endif
+
 namespace content {
 
 namespace {
@@ -730,6 +736,30 @@ bool BrowserAccessibilityManager::OnAccessibilityEvents(
 
   // Allow derived classes to do event post-processing.
   FinalizeAccessibilityEvents();
+
+#if defined(AX_FAIL_FAST_BUILD)
+  // When running a debugging/sanitizer build with
+  // --force-renderer-accessibility, exercise the properties for every node, to
+  // ensure no crashes or assertions are triggered. This helpfully runs for all
+  // web tests on builder linux-blink-web-tests-force-accessibility-rel, as well
+  // as for some clusterfuzz runs.
+  static int g_max_ax_tree_exercise_iterations = 3;  // Avoid timeouts.
+  static int count = 0;
+  if (GetRoot()->GetChildCount() > 0 &&
+      !GetRoot()->GetBoolAttribute(ax::mojom::BoolAttribute::kBusy) &&
+      ++count <= g_max_ax_tree_exercise_iterations) {
+    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(::switches::kForceRendererAccessibility)) {
+      std::unique_ptr<ui::AXTreeFormatter> formatter(
+          AXInspectFactory::CreatePlatformFormatter());
+      formatter->SetPropertyFilters({{"*", ui::AXPropertyFilter::ALLOW}});
+      std::string formatted_tree = formatter->Format(GetRoot());
+      VLOG(1) << "\n\n******** Formatted tree ********\n\n"
+              << formatted_tree << "\n*********************************\n\n";
+    }
+  }
+#endif
+
   return true;
 }
 
