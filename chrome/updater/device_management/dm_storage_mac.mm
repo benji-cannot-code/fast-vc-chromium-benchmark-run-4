32031b72a64720e2a5b4416c773457e604ae2f01
Xiaoling Bao <xiaolingbao@chromium.org>
2022-08-10 01:59:52 +0000

    Update default DMStorage location on macOS
    Change the default DMStorage to be under
    /Library/{CompanyName}/{KEYSTONE_NAME}/DeviceManagement for macOS.
    The policies are always persisted under this folder for security
    and backward compatibility reasons.
    
    Bug: 1345407
    Change-Id: I59aa1adc12bc3d739b9a5ea60a6aa3c6374a2739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820028
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033298}

--- chrome/updater/device_management/dm_storage_mac.mm
+++ chrome/updater/device_management/dm_storage_mac.mm
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_ioobject.h"
 #include "base/mac/scoped_nsobject.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "chrome/updater/mac/mac_util.h"
 #include "chrome/updater/updater_branding.h"
 
 namespace updater {
@@ -151,5 +153,17 @@ bool TokenService::DeleteDmToken() {
 
 DMStorage::DMStorage(const base::FilePath& policy_cache_root)
     : DMStorage(policy_cache_root, std::make_unique<TokenService>()) {}
+
+scoped_refptr<DMStorage> GetDefaultDMStorage() {
+  absl::optional<base::FilePath> sys_library_path =
+      GetLibraryFolderPath(UpdaterScope::kSystem);
+  if (!sys_library_path)
+    return nullptr;
+
+  return base::MakeRefCounted<DMStorage>(
+      sys_library_path->AppendASCII(COMPANY_SHORTNAME_STRING)
+          .Append(FILE_PATH_LITERAL(KEYSTONE_NAME))
+          .AppendASCII("DeviceManagement"));
+}
 
 }  // namespace updater
--- chrome/updater/device_management/dm_storage_win.cc
+++ chrome/updater/device_management/dm_storage_win.cc
@@ -7,8 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/base_paths_win.h"
+#include "base/files/file_path.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/path_service.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/win/registry.h"
+#include "chrome/updater/updater_branding.h"
 #include "chrome/updater/win/win_constants.h"
 #include "chrome/updater/win/win_util.h"
 
@@ -106,5 +111,17 @@ std::string TokenService::GetDmToken() const {
 
 DMStorage::DMStorage(const base::FilePath& policy_cache_root)
     : DMStorage(policy_cache_root, std::make_unique<TokenService>()) {}
+
+scoped_refptr<DMStorage> GetDefaultDMStorage() {
+  base::FilePath program_filesx86_dir;
+  if (!base::PathService::Get(base::DIR_PROGRAM_FILESX86,
+                              &program_filesx86_dir)) {
+    return nullptr;
+  }
+
+  return base::MakeRefCounted<DMStorage>(
+      program_filesx86_dir.AppendASCII(COMPANY_SHORTNAME_STRING)
+          .AppendASCII("Policies"));
+}
 
 }  // namespace updater
