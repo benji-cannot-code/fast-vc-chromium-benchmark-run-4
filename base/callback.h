db2e6d49604c26e714dfe91b67ada7efec6a8c2a
Jeroen Dhollander <jeroendh@google.com>
2022-08-10 14:19:50 +0000

    Fix unused-include warnings for OnceClosure
    base/callback_forward.h forward declares both `OnceCallback` and the
    convenience wrapper `OnceClosure`.
    
    base/callback.h on the other hand implements `OnceCallback` but has no
    mentioning of `OnceClosure`.
    
    To prevent users of `OnceClosure` having to include both headers,
    `base/callback.h` also includes `base/callback_forward.h`.
    
    Clangd doesn't know about this and complains about `unused includes`
    if a file only uses `OnceClosure` (like
    `ui/display/manager/display_manager.h`).
    
    This CL fixes this `unused includes` warning.
    
    Bug: N/A
    Test: Manually verified the warning is gone.
    Change-Id: I371914b172b1f88ed3c8ca75d5dcc5e342971f78
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819681
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033493}

--- base/callback.h
+++ base/callback.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/callback_forward.h"
+#include "base/callback_forward.h"  // IWYU pragma: export
 #include "base/callback_internal.h"
 #include "base/check.h"
 #include "base/functional/function_ref.h"
