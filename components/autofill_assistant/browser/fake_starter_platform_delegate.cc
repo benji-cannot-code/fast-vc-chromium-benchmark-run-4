fae60e25258b47a5cd2f555d614b9065fdf7c05a
Luca Hunkeler <hluca@google.com>
2022-08-10 09:57:56 +0000

    Reland "Add tests for headless flows"
    This is a reland of commit 0ae12021cc015f01aba2002478930466e6db9a43
    
    Original change's description:
    > Add tests for headless flows
    >
    > This CL refactors the headless script controller and client to allow for the necessary injections to be able to test a full headless flow. This CL also includes a couple of basic tests. More coverage will follow.
    >
    >
    > Bug: b/201964911
    > Change-Id: I8fc67dbbd064e4b65da7e8a4dcd52c946dc3ca75
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807499
    > Reviewed-by: Clemens Arbesser <arbesser@google.com>
    > Commit-Queue: Luca Hunkeler <hluca@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1031959}
    
    Bug: b/201964911
    Change-Id: I018b7fdab53dd40d3df21278f14e5adaab36ef46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813721
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033434}

--- components/autofill_assistant/browser/fake_starter_platform_delegate.cc
+++ components/autofill_assistant/browser/fake_starter_platform_delegate.cc
@@ -143,7 +143,7 @@ FakeStarterPlatformDelegate::GetCommonDependencies() const {
 
 const PlatformDependencies*
 FakeStarterPlatformDelegate::GetPlatformDependencies() const {
-  return nullptr;
+  return &fake_platform_dependencies_;
 }
 
 base::WeakPtr<StarterPlatformDelegate>
