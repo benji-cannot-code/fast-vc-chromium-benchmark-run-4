f5c4797ed82c96493c1ddc02bc452763f60624b1
Torne (Richard Coles) <torne@google.com>
2022-08-10 20:51:36 +0000

    Use proper autofill resource IDs where possible.
    The O MR1 SDK has been available for some time, so update the autofill
    code to use the android.R constants for autofill resource IDs.
    
    Unfortunately, the constant for the autofill string was not made public
    until O MR1, so on O we still have to look this up by name at runtime.
    The constant for the autofill menu item ID was public in O, so doesn't
    need this treatment.
    
    Fixed: 740628
    Change-Id: I2cec8161a9cfa02bef286371175f16a98ff78bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781707
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033704}

--- components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillActionModeCallback.java
+++ components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillActionModeCallback.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.autofill;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,30 +19,31 @@ public class AutofillActionModeCallback implements ActionMode.Callback {
     private final Context mContext;
     private final AutofillProvider mAutofillProvider;
     private final int mAutofillMenuItemTitle;
-    private final int mAutofillMenuItem;
 
     // using getIdentifier to work around not-exposed framework resource ID
     @SuppressWarnings("DiscouragedApi")
     public AutofillActionModeCallback(Context context, AutofillProvider autofillProvider) {
         mContext = context;
         mAutofillProvider = autofillProvider;
-        // TODO(michaelbai): Uses the resource directly after sdk roll to Android O MR1.
-        // crbug.com/740628
-        mAutofillMenuItemTitle =
-                mContext.getResources().getIdentifier("autofill", "string", "android");
-        mAutofillMenuItem = mContext.getResources().getIdentifier("autofill", "id", "android");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            mAutofillMenuItemTitle = android.R.string.autofill;
+        } else {
+            // The string resource was not made public until O MR1, so on O we look it up by name.
+            mAutofillMenuItemTitle =
+                    mContext.getResources().getIdentifier("autofill", "string", "android");
+        }
     }
 
     @Override
     public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-        return mAutofillMenuItemTitle != 0 && mAutofillMenuItem != 0;
+        return mAutofillMenuItemTitle != 0;
     }
 
     @Override
     public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
         if (mAutofillMenuItemTitle != 0 && mAutofillProvider.shouldQueryAutofillSuggestion()) {
-            MenuItem item = menu.add(
-                    Menu.NONE, mAutofillMenuItem, Menu.CATEGORY_SECONDARY, mAutofillMenuItemTitle);
+            MenuItem item = menu.add(Menu.NONE, android.R.id.autofill, Menu.CATEGORY_SECONDARY,
+                    mAutofillMenuItemTitle);
             item.setShowAsActionFlags(
                     MenuItem.SHOW_AS_ACTION_NEVER | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
         }
@@ -50,7 +52,7 @@ public class AutofillActionModeCallback implements ActionMode.Callback {
 
     @Override
     public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-        if (item.getItemId() == mAutofillMenuItem) {
+        if (item.getItemId() == android.R.id.autofill) {
             mAutofillProvider.queryAutofillSuggestion();
             mode.finish();
             return true;
