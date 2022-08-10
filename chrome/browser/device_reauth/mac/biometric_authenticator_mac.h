1f81e9cb40fba54cd2d56b065f4ca17f51fb581f
Karol Sygiet <sygiet@google.com>
2022-08-10 07:50:14 +0000

    Fix Chrome crash that occurred when 2 different authentications were triggered by the user on Mac.
    Each authentication attempt should use its own touchIdAuthObject. Any ongoing authentications will be canceled, before new one appear.
    
    Bug: 1342024
    Change-Id: I1d5af376d1d897560460eec611f1cc3876e3a7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787847
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033397}

--- chrome/browser/device_reauth/mac/biometric_authenticator_mac.h
+++ chrome/browser/device_reauth/mac/biometric_authenticator_mac.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DEVICE_REAUTH_MAC_BIOMETRIC_AUTHENTICATOR_MAC_H_
 #define CHROME_BROWSER_DEVICE_REAUTH_MAC_BIOMETRIC_AUTHENTICATOR_MAC_H_
 
+#include "base/callback.h"
 #include "chrome/browser/device_reauth/chrome_biometric_authenticator_common.h"
 #include "chrome/browser/device_reauth/chrome_biometric_authenticator_factory.h"
 #include "components/device_reauth/biometric_authenticator.h"
@@ -54,9 +55,18 @@ class BiometricAuthenticatorMac : public ChromeBiometricAuthenticatorCommon {
   BiometricAuthenticatorMac();
   ~BiometricAuthenticatorMac() override;
 
+  // Called when the authentication compeletes with the result.
+  void OnAuthenticationCompleted(bool result);
+
+  // Callback to be executed after the authentication completes.
+  AuthenticateCallback callback_;
+
   // TouchId authenticator object that will handle biometric authentication
-  // itself
+  // itself.
   std::unique_ptr<device::fido::mac::TouchIdContext> touch_id_auth_context_;
+
+  // Factory for weak pointers to this class.
+  base::WeakPtrFactory<BiometricAuthenticatorMac> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_DEVICE_REAUTH_MAC_BIOMETRIC_AUTHENTICATOR_MAC_H_
