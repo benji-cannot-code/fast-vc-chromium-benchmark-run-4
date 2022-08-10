6f96e0c99393e3f61505244cb93cdb6a80753fe2
Xiaochen Zhou <xiaochenzh@chromium.org>
2022-08-10 16:40:46 +0000

    Fenced frames: Unify fenced frame browser tests in one file.
    Move fenced frame test from frame_tree_browsertest.cc to
    fenced_frame_browsertest.cc.
    
    Bug: 1348640
    Change-Id: Ied43a7e676e0f6edf70c3e3316dd1bf3d7819e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791735
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033555}

--- content/browser/renderer_host/frame_tree.h
+++ content/browser/renderer_host/frame_tree.h
@@ -526,7 +526,7 @@ class CONTENT_EXPORT FrameTree {
   friend class FrameTreeTest;
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostImplBrowserTest, RemoveFocusedFrame);
   FRIEND_TEST_ALL_PREFIXES(PortalBrowserTest, NodesForIsLoading);
-  FRIEND_TEST_ALL_PREFIXES(FencedFrameBrowserTest, NodesForIsLoading);
+  FRIEND_TEST_ALL_PREFIXES(FencedFrameMPArchBrowserTest, NodesForIsLoading);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostManagerTest,
                            CreateRenderViewAfterProcessKillAndClosedProxy);
 
