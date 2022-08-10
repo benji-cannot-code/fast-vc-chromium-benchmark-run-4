fc2e518af406a6a6ca12127e985bdc418aace2e9
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-08-10 02:01:49 +0000

    system-extensions: Implement uninstalling a System Extension
    Implements basic uninstalling of system extensions i.e.
    removing the WebUIConfig, deleting assets, and removing
    the extension from the registry.
    
    Bug: b/221121088
    Change-Id: I0a1c4f23f0d3192999f68302f5b131eef906bd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3809194
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033301}

--- chrome/browser/ash/system_extensions/system_extensions_registry_manager.cc
+++ chrome/browser/ash/system_extensions/system_extensions_registry_manager.cc
@@ -18,4 +18,9 @@ void SystemExtensionsRegistryManager::AddSystemExtension(
   registry_.AddSystemExtension(std::move(system_extension));
 }
 
+void SystemExtensionsRegistryManager::RemoveSystemExtension(
+    const SystemExtensionId& system_extension_id) {
+  registry_.RemoveSystemExtension(system_extension_id);
+}
+
 }  // namespace ash
