f4b00ce5a3f55073ee0b38c0dd8b70f0ff475a0b
Daniel Libby <dlibby@microsoft.com>
2022-08-10 18:58:59 +0000

    Create WebPreference control ::-webkit-scrollbar* parsing
    This CL introduces a mechanism by which WebContents hosts can opt out
    of allowing ::-webkit-scrollbar styling. Custom scrollbar styling can
    be problematic for accessibility when colors with low contrast are used.
    
    The new flag on WebPreferences persists to a RuntimeEnabledFeature
    state, which is read at parse time to determine whether we should
    recognize the set of webkit-prefixed pseudo-elements used to custom
    style scrollbars and their parts.
    
    Bug: 1012117
    Change-Id: Id92d755f615e520094d6fd786aa0cab7696d4a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822241
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1033621}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1510,6 +1510,9 @@ void WebView::ApplyWebPreferences(const web_pref::WebPreferences& prefs,
 
   settings->SetHideScrollbars(prefs.hide_scrollbars);
 
+  RuntimeEnabledFeatures::SetWebKitScrollbarStylingEnabled(
+      prefs.enable_webkit_scrollbar_styling);
+
   // Enable gpu-accelerated 2d canvas if requested on the command line.
   RuntimeEnabledFeatures::SetAccelerated2dCanvasEnabled(
       prefs.accelerated_2d_canvas_enabled);
