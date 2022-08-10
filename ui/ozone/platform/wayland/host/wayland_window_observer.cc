d7e509211c50c8afadd3146820cc467f9791b1f6
Erik Chen <erikchen@chromium.org>
2022-08-10 17:21:52 +0000

    lacros: Make ui_controls more deterministic. (part 2)
    This CL makes two changes.
    
    Change 1:
    The main difference between the semantics of weston_test protocol and
    interactive_ui_tests is that interactive_ui_tests expects windows to be
    created or shown synchronously, whereas in wayland and weston_test
    windows are created or shown asynchronously.
    
    Previously, the logic for events to wait for window creation tied the
    events to the windows themselves. This is incorrect because it makes the
    assumptions that a series of events will be associated with a window.
    However, there is no way to know the side effects of earlier events. For
    example: imagine queueing two events: mouse-down and mouse-up, both
    associated with a single window. If the mouse-down event results in
    hiding of the window, then the mouse-up event queued with that window
    semantically makes no sense.
    
    This CL modifies wayland_input_emulate to use a global event queue
    rather than a window-scoped event queue.
    
    Change 2:
    The previous signal to queue events in wayland_input_emulate was
    triggered via the default value of buffer_attached_and_configured on
    creation of TestWindow. This was set during Window creation. However,
    there was no signal to queue events if a window was hidden and then
    shown again, also an asynchronous process. This caused the followed
    sequence of events to be non-deterministic:
      1) Create a window (asynchronous, starts queueing events)
      2) Show the window (goes through role assignation, asynchronous)
      3) Send an event (waits for creation, sends event, stops queueing)
      4) Hide the window (role unassigned).
      5) Show the window (role assigned, buffer commit is asynchronous)
      6) Send an event.
    
    This CL plumbs a signal for role assignation into wayland_input_emulate.
    
    Change-Id: I39132c7ce0c21a14bb1d5d76d8cbd0c1bc157c06
    Bug: 1344238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792278
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033572}

--- ui/ozone/platform/wayland/host/wayland_window_observer.cc
+++ ui/ozone/platform/wayland/host/wayland_window_observer.cc
@@ -15,6 +15,8 @@ void WaylandWindowObserver::OnWindowRemoved(WaylandWindow* window) {}
 
 void WaylandWindowObserver::OnWindowConfigured(WaylandWindow* window) {}
 
+void WaylandWindowObserver::OnWindowRoleAssigned(WaylandWindow* window) {}
+
 void WaylandWindowObserver::OnSubsurfaceAdded(WaylandWindow* window,
                                               WaylandSubsurface* subsurface) {}
 
