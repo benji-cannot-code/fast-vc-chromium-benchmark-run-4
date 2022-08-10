edea700c34f481993b573b5695d818b4c22b9f51
Paul Semel <paulsemel@chromium.org>
2022-08-10 12:44:01 +0000

    DanglingPtr: Fix dangling ptr in TileManager
    We need to cleanup pending raster queries ptr while cleaning up the tile
    manager.
    
    Bug: 1291138
    Change-Id: I1230e8b5a0c8904d3369c93762ffbf612892f3b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816145
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033472}

--- cc/tiles/tile_manager.h
+++ cc/tiles/tile_manager.h
@@ -452,8 +452,7 @@ class CC_EXPORT TileManager : CheckerImageTrackerClient {
 
   const TileManagerSettings tile_manager_settings_;
   bool use_gpu_rasterization_;
-  raw_ptr<RasterQueryQueue, DanglingUntriaged> pending_raster_queries_ =
-      nullptr;
+  raw_ptr<RasterQueryQueue> pending_raster_queries_ = nullptr;
 
   std::unordered_map<Tile::Id, Tile*> tiles_;
 
