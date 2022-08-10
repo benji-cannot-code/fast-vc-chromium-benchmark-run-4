60a14efdb6f8e2320d94fa7c58430b68a0849feb
Matthew DeVore <matvore@chromium.org>
2022-08-10 23:15:26 +0000

    Create GameModeController in non-Borealis code path
    Make GameModeController not owned by BorealisContext, so that it can
    more naturally start to support ARC game mode.
    
    This causes the GameModeController to be run in more configurations than
    before, including when Borealis is not enabled. This includes test
    cases, which necessitates a fix in the focus change callback.
    
    Bug: b:228405479
    Test: GameModeControllerTest
    Test: Run Steam game; verify Game Mode signal maintained for 2 minutes
    
    Change-Id: I7453efd51e8bbee76c8310210403a5f829e32ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784567
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033766}

--- chrome/browser/ash/borealis/borealis_context.cc
+++ chrome/browser/ash/borealis/borealis_context.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/borealis/borealis_shutdown_monitor.h"
 #include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/ash/borealis/borealis_window_manager.h"
-#include "chrome/browser/ash/game_mode/game_mode_controller.h"
 #include "chrome/browser/ash/guest_os/guest_os_stability_monitor.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/grit/generated_resources.h"
@@ -201,7 +200,6 @@ BorealisContext::BorealisContext(Profile* profile)
       guest_os_stability_monitor_(
           std::make_unique<guest_os::GuestOsStabilityMonitor>(
               kBorealisStabilityHistogram)),
-      game_mode_controller_(std::make_unique<game_mode::GameModeController>()),
       engagement_metrics_(std::make_unique<BorealisEngagementMetrics>(profile)),
       disk_manager_(std::make_unique<BorealisDiskManagerImpl>(this)),
       power_controller_(std::make_unique<BorealisPowerController>()) {}
--- chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
+#include "chrome/browser/ash/game_mode/game_mode_controller.h"
 #include "chrome/browser/ash/login/signin/signin_error_notifier_factory.h"
 #include "chrome/browser/ash/night_light/night_light_client.h"
 #include "chrome/browser/ash/policy/display/display_resolution_handler.h"
@@ -304,6 +305,7 @@ void ChromeBrowserMainExtraPartsAsh::PostProfileInit(Profile* profile,
       std::make_unique<quick_answers::QuickAnswersClient>(
           g_browser_process->shared_url_loader_factory(),
           QuickAnswersController::Get()->GetQuickAnswersDelegate()));
+  game_mode_controller_ = std::make_unique<game_mode::GameModeController>();
 
   // Initialize TabScrubberChromeOS after the Ash Shell has been initialized.
   TabScrubberChromeOS::GetInstance();
@@ -342,6 +344,7 @@ void ChromeBrowserMainExtraPartsAsh::PostMainMessageLoopRun() {
   tab_cluster_ui_client_.reset();
 
   // Initialized in PostProfileInit (which may not get called in some tests).
+  game_mode_controller_.reset();
   quick_answers_controller_.reset();
   ash_web_view_factory_.reset();
   network_portal_notification_controller_.reset();
