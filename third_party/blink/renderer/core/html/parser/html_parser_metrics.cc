45b9121f464d0b24480274bf5c26e662a60ab223
Scott Violet <sky@chromium.org>
2022-08-10 18:42:30 +0000

    blink: adds histogram for time spent in HTMLTokenizer
    This patch adds a histogram for the time spent in
    HTMLTokenizer::NextToken.
    
    BUG=1345267
    
    Change-Id: I900c587e512432ca0b6135d905e183bfb8918028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3821102
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033593}

--- third_party/blink/renderer/core/html/parser/html_parser_metrics.cc
+++ third_party/blink/renderer/core/html/parser/html_parser_metrics.cc
@@ -15,7 +15,8 @@ HTMLParserMetrics::HTMLParserMetrics(int64_t source_id,
     : source_id_(source_id), recorder_(recorder) {}
 
 void HTMLParserMetrics::AddChunk(base::TimeDelta elapsed_time,
-                                 unsigned tokens_parsed) {
+                                 unsigned tokens_parsed,
+                                 base::TimeDelta time_in_next_token) {
   DCHECK(base::TimeTicks::IsHighResolution());
 
   ++chunk_count_;
@@ -31,6 +32,8 @@ void HTMLParserMetrics::AddChunk(base::TimeDelta elapsed_time,
     min_tokens_parsed_ = tokens_parsed;
   if (tokens_parsed > max_tokens_parsed_)
     max_tokens_parsed_ = tokens_parsed;
+
+  accumulated_time_in_next_token_ += time_in_next_token;
 }
 
 void HTMLParserMetrics::AddYieldInterval(base::TimeDelta elapsed_time) {
@@ -60,6 +63,9 @@ void HTMLParserMetrics::ReportUMAs() {
   UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
       "Blink.HTMLParsing.ParsingTimeTotal3", accumulated_parsing_time_,
       base::Microseconds(1), base::Seconds(100), 1000);
+  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+      "Blink.HTMLParsing.NextTokenTimeTotal", accumulated_time_in_next_token_,
+      base::Microseconds(1), base::Seconds(10), 1000);
   UMA_HISTOGRAM_COUNTS_1M("Blink.HTMLParsing.TokensParsedMax2",
                           max_tokens_parsed_);
   UMA_HISTOGRAM_COUNTS_10000("Blink.HTMLParsing.TokensParsedMin2",
