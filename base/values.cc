8566edb50d6d04f2fe6190ca32118c8682588b56
Maksim Ivanov <emaxx@chromium.org>
2022-08-10 01:56:48 +0000

    base::Value DCHECKs in Get...() accessors
    Add DCHECKs that the value type is expected into Value::Get...()
    accessor methods.
    
    This makes it simpler to debug such failures, because without this CL
    the failure ends up in an uninformative "Bad variant access" crash
    message.
    
    Bug: none
    Change-Id: Ifa574bb626e8e5b39c5a92e31a7a0c9ec8b751db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804887
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033293}

--- base/values.cc
+++ base/values.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/as_const.h"
 #include "base/bit_cast.h"
+#include "base/check.h"
 #include "base/check_op.h"
 #include "base/containers/checked_iterators.h"
 #include "base/containers/cxx20_erase_vector.h"
@@ -346,10 +347,12 @@ Value::List* Value::GetIfList() {
 }
 
 bool Value::GetBool() const {
+  DCHECK(is_bool());
   return absl::get<bool>(data_);
 }
 
 int Value::GetInt() const {
+  DCHECK(is_int());
   return absl::get<int>(data_);
 }
 
@@ -363,30 +366,37 @@ double Value::GetDouble() const {
 }
 
 const std::string& Value::GetString() const {
+  DCHECK(is_string());
   return absl::get<std::string>(data_);
 }
 
 std::string& Value::GetString() {
+  DCHECK(is_string());
   return absl::get<std::string>(data_);
 }
 
 const Value::BlobStorage& Value::GetBlob() const {
+  DCHECK(is_blob());
   return absl::get<BlobStorage>(data_);
 }
 
 const Value::Dict& Value::GetDict() const {
+  DCHECK(is_dict());
   return absl::get<Dict>(data_);
 }
 
 Value::Dict& Value::GetDict() {
+  DCHECK(is_dict());
   return absl::get<Dict>(data_);
 }
 
 const Value::List& Value::GetList() const {
+  DCHECK(is_list());
   return absl::get<List>(data_);
 }
 
 Value::List& Value::GetList() {
+  DCHECK(is_list());
   return absl::get<List>(data_);
 }
 
