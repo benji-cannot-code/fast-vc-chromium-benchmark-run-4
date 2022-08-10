db76df2c11f82c6fe3c6ef82ae8824514a65ea0e
Jeroen Dhollander <jeroendh@google.com>
2022-08-10 12:01:13 +0000

    Fix unused-include warnings for base::StringPiece
    base::StringPiece is forward declared in `string_piece_forward.h`,
    which caused clangd to complain about unused include if somebody
    included `string_piece.h` instead.
    
    Test: Manually verified in `ui/display/display_layout.h`
    Change-Id: If4e99a103a2397b589174f3196c632691b46ee08
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822702
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033463}

--- base/strings/string_piece.h
+++ base/strings/string_piece.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/compiler_specific.h"
-#include "base/strings/string_piece_forward.h"
+#include "base/strings/string_piece_forward.h"  // IWYU pragma: export
 #include "build/build_config.h"
 
 namespace base {
