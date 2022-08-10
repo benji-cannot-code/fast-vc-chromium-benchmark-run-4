4cdd7229e88624858e0ed6c885f740b1195f6dfa
Nick Diego Yamane <nickdiego@igalia.com>
2022-08-10 16:32:13 +0000

    wayland: skip frames till window's surface is configured
    In order to avoid "xdg_surface has never been configured" fatal protocol
    errors, like in the linked crbug, we must ensure the initial configure
    sequence is acked before attempting to attach buffers to its root
    wl_surface.
    
    Due to the multi-process async-oriented design of Chrome graphics, the
    current configure handling may be done both:
    
    1. Instantly, upon xdg_surface.configure event (simple case), and
    2. Deferred to be notified to the Wayland compositor
       (set_window_geometry + ack_configure requests, amongst others) just
       before attaching the buffer(s) corresponding to the new frame.
    
    In the case (2), some obscure problems were observed in the wild, where
    the configure sequence is not acked before trying to attach the first
    buffer (surface mapping), resulting in crashes due to fatal protocol
    error. In order to fix it, this CL patches ozone/wayland's frame manager
    and toplevel surface code such that frames playback is deferred until
    its corresponding shell surface gets configured. Moreover, unit test is
    added to ensure it does not regresses unnoticeably.
    
    Currently, Exo does not send protocol error in such situation, so it was
    not yet observed in that case. In addition, rounding issues were
    observed (at least when fractional scale is used, see crbug.com/1346534)
    which makes this fix unreliable, so it's kept disabled for Lacros for
    now.
    
    As a side-effect, this refactors MaybeProcessPendingFrame and
    PlayBackFrame functions in WaylandFrameManager, such that them more
    consistent with their names, i.e: PlayBackFrame assumes (and DCHECKs)
    the "discard frame" cases are handled in the call-site, with the
    additional condition of its |window_| being fully configured before to
    proceed.
    
    Bug: 1313023
    Test: ozone_unittests WaylandBufferManagerTest.CommitBufferConditionsWithDeferredAckConfigure
    Change-Id: I6787a2edfb38a465188d0d40b5b9397a725f6caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3697539
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1033544}

--- ui/ozone/platform/wayland/host/wayland_frame_manager.h
+++ ui/ozone/platform/wayland/host/wayland_frame_manager.h
@@ -123,6 +123,8 @@ class WaylandFrameManager {
 
  private:
   void PlayBackFrame(std::unique_ptr<WaylandFrame> frame);
+  void DiscardFrame(std::unique_ptr<WaylandFrame> frame);
+
   // Configures |surface| but does not commit wl_surface states yet.
   void ApplySurfaceConfigure(WaylandFrame* frame,
                              WaylandSurface* surface,
@@ -176,6 +178,8 @@ class WaylandFrameManager {
   // |frame::buffer_lost| is set and false is returned. That means that the
   // frame must not be used for the further submission.
   bool EnsureWlBuffersExist(WaylandFrame& frame);
+
+  bool IsSurfaceConfigured() const;
 
   const raw_ptr<WaylandWindow> window_;
 
