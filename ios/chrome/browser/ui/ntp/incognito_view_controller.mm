91507532c9dda2c9157df9c1c63a0cdeefb466a5
Robbie Gibson <rkgibson@google.com>
2022-08-10 15:44:28 +0000

    [iOS] Fix incognito NTP animation bug
    The root cause of the bug is that BVC's -viewDidLayoutSubviews isn't
    called when the layout guides are added.
    
    This is a quick solution to manually set the frame of the incognito
    NTP. A better solution will be added in M106.
    
    Fixed: 1347444
    Change-Id: I49c9296bab35a1f2a142f1f94262925eadd30bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814153
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033530}

--- ios/chrome/browser/ui/ntp/incognito_view_controller.mm
+++ ios/chrome/browser/ui/ntp/incognito_view_controller.mm
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        showTopIncognitoImageAndTitle:YES];
     view.URLLoaderDelegate = self;
     self.incognitoView = view;
-
   } else {
     IncognitoView* view = [[IncognitoView alloc] initWithFrame:self.view.bounds
                                  showTopIncognitoImageAndTitle:YES];
