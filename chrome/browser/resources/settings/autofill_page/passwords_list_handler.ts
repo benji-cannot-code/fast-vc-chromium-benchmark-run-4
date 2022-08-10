0c5a2417ff7d7d9ececf6ba8840185953808d295
Adem Derinel <derinel@google.com>
2022-08-10 14:42:31 +0000

    Passwords: Move tokenRequestManager to PasswordRequestorMixin
    Bug: 1345899, 1274569
    Change-Id: Ia780218845f8ac1ddf1f573dff92bc70911f74a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820002
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033503}

--- chrome/browser/resources/settings/autofill_page/passwords_list_handler.ts
+++ chrome/browser/resources/settings/autofill_page/passwords_list_handler.ts
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import '../i18n_setup.js';
+// <if expr="is_chromeos">
+import '../controls/password_prompt_dialog.js';
+// </if>
 import './password_edit_dialog.js';
 import './password_move_to_account_dialog.js';
 import './password_remove_dialog.js';
