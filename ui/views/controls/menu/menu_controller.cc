ba01ef2f2a9e3c571921b3b7da2eca4f2003c36f
Artsiom Mitrokhin <amitrokhin@google.com>
2022-08-10 19:21:20 +0000

    Call `ShouldExecuteCommandWithoutClosingMenu` for tap and kb events
    Currently it works for click events only.
    
    BUG=844786
    
    Change-Id: I2068c99c2d013b5e8a847f7dd41454796f978c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820263
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033660}

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -939,7 +939,13 @@ void MenuController::OnGestureEvent(SubmenuView* source,
   } else if (event->type() == ui::ET_GESTURE_TAP) {
     if (!part.is_scroll() && part.menu &&
         !(part.should_submenu_show && part.menu->HasSubmenu())) {
-      if (part.menu->GetDelegate()->IsTriggerableEvent(part.menu, *event)) {
+      const int command = part.menu->GetCommand();
+      if (part.menu->GetDelegate()->ShouldExecuteCommandWithoutClosingMenu(
+              command, *event)) {
+        item_selected_by_touch_ = true;
+        part.menu->GetDelegate()->ExecuteCommand(command, 0);
+      } else if (part.menu->GetDelegate()->IsTriggerableEvent(part.menu,
+                                                              *event)) {
         item_selected_by_touch_ = true;
         Accept(part.menu, event->flags());
       }
@@ -1607,7 +1613,13 @@ bool MenuController::OnKeyPressed(const ui::KeyEvent& event) {
           handled_key_code = true;
           if (!SendAcceleratorToHotTrackedView(event.flags()) &&
               pending_state_.item->GetEnabled()) {
-            Accept(pending_state_.item, event.flags());
+            const int command = pending_state_.item->GetCommand();
+            if (pending_state_.item->GetDelegate()
+                    ->ShouldExecuteCommandWithoutClosingMenu(command, event)) {
+              pending_state_.item->GetDelegate()->ExecuteCommand(command, 0);
+            } else {
+              Accept(pending_state_.item, event.flags());
+            }
           }
         }
       }
