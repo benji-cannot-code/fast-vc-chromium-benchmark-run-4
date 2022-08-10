b9296ebe52840270cc269ea332a005a2ccbce1b3
Sean Kau <skau@chromium.org>
2022-08-10 15:42:36 +0000

    Switch AssistantButton and AssistantDialogPlate to ui::ColorIds
    Switch to ColorIds from ContentLayerType. Also remove ScopedLightMode
    usage. Deleted one of the AssistantButton tests because its covered
    by the AssistantDialogPlate test.
    
    Bug: 1346394,1348376
    Change-Id: Ibd1c11e8c41947ecc8fcf66bc652d795b4d93969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813085
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033529}

--- ash/assistant/ui/base/assistant_button_unittest.cc
+++ ash/assistant/ui/base/assistant_button_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
@@ -91,32 +92,6 @@ TEST_F(AssistantButtonTest, IconColor) {
       *button->GetImage(views::Button::STATE_NORMAL).bitmap()));
 }
 
-TEST_F(AssistantButtonTest, IconColorTypeDefaultLight) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures(
-      /*enabled_features=*/{},
-      /*disabled_features=*/{features::kNotificationsRefresh,
-                             chromeos::features::kDarkLightMode});
-
-  AssistantButton::InitParams params;
-  params.size_in_dip = kSizeInDip;
-  params.icon_size_in_dip = kIconSizeInDip;
-  params.accessible_name_id = IDS_ASH_ASSISTANT_DIALOG_PLATE_KEYBOARD_ACCNAME;
-  params.icon_color_type = ColorProvider::ContentLayerType::kIconColorPrimary;
-
-  std::unique_ptr<AssistantButton> button = AssistantButton::Create(
-      nullptr, vector_icons::kKeyboardIcon,
-      AssistantButtonId::kKeyboardInputToggle, std::move(params));
-
-  EXPECT_TRUE(gfx::test::AreBitmapsEqual(
-      *gfx::CreateVectorIcon(vector_icons::kKeyboardIcon, kIconSizeInDip,
-                             ash::features::IsProductivityLauncherEnabled()
-                                 ? gfx::kGoogleGrey200
-                                 : gfx::kGoogleGrey900)
-           .bitmap(),
-      *button->GetImage(views::Button::STATE_NORMAL).bitmap()));
-}
-
 TEST_F(AssistantButtonTest, IconColorType) {
   base::test::ScopedFeatureList scoped_feature_list_enable_dark_light_mode(
       chromeos::features::kDarkLightMode);
@@ -131,7 +106,7 @@ TEST_F(AssistantButtonTest, IconColorType) {
   params.size_in_dip = kSizeInDip;
   params.icon_size_in_dip = kIconSizeInDip;
   params.accessible_name_id = IDS_ASH_ASSISTANT_DIALOG_PLATE_KEYBOARD_ACCNAME;
-  params.icon_color_type = ColorProvider::ContentLayerType::kIconColorPrimary;
+  params.icon_color_type = cros_tokens::kColorPrimary;
 
   std::unique_ptr<views::Widget> widget = CreateTestWidget();
   AssistantButton* button =
@@ -175,7 +150,7 @@ TEST_F(AssistantButtonTest, FocusAndHoverColor) {
   params.size_in_dip = kSizeInDip;
   params.icon_size_in_dip = kIconSizeInDip;
   params.accessible_name_id = IDS_ASH_ASSISTANT_DIALOG_PLATE_KEYBOARD_ACCNAME;
-  params.icon_color_type = ColorProvider::ContentLayerType::kIconColorPrimary;
+  params.icon_color_type = cros_tokens::kColorPrimaryDark;
 
   std::unique_ptr<views::Widget> widget = CreateTestWidget();
   AssistantButton* button =
@@ -232,7 +207,7 @@ TEST_F(AssistantButtonTest, FocusAndHoverColorDarkLightMode) {
   params.size_in_dip = kSizeInDip;
   params.icon_size_in_dip = kIconSizeInDip;
   params.accessible_name_id = IDS_ASH_ASSISTANT_DIALOG_PLATE_KEYBOARD_ACCNAME;
-  params.icon_color_type = ColorProvider::ContentLayerType::kIconColorPrimary;
+  params.icon_color_type = cros_tokens::kColorPrimary;
 
   std::unique_ptr<views::Widget> widget = CreateTestWidget();
   AssistantButton* button =
