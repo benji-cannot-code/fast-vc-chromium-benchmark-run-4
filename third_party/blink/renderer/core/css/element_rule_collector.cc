26f5c3a29d010e5366e9693efee0b9a76c6b2848
Steinar H. Gunderson <sesse@chromium.org>
2022-08-10 11:09:44 +0000

    Make RuleSet return base::span for its buckets.
    This allows us to change the underlying storage more easily,
    instead of changing all call sites whenever we change from HeapVector
    to something else (or even just change the inline size). Semantically
    and performance-wise, it should be a no-op.
    
    Change-Id: I26d264b8b2d4b0c80bd8f64d164cfc362f083c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820208
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033452}

--- third_party/blink/renderer/core/css/element_rule_collector.cc
+++ third_party/blink/renderer/core/css/element_rule_collector.cc
@@ -332,7 +332,7 @@ static bool RulesApplicableInCurrentTreeScope(
 
 template <bool perf_trace_enabled>
 void ElementRuleCollector::CollectMatchingRulesForListInternal(
-    const HeapVector<RuleData>* rules,
+    base::span<const RuleData> rules,
     const MatchRequest& match_request,
     const RuleSet* rule_set,
     const CSSStyleSheet* style_sheet,
@@ -356,10 +356,12 @@ void ElementRuleCollector::CollectMatchingRulesForListInternal(
   unsigned fast_rejected = 0;
   unsigned matched = 0;
   SelectorStatisticsCollector selector_statistics_collector;
-  if (perf_trace_enabled)
-    selector_statistics_collector.ReserveCapacity(rules->size());
+  if (perf_trace_enabled) {
+    selector_statistics_collector.ReserveCapacity(
+        static_cast<wtf_size_t>(rules.size()));
+  }
 
-  for (const RuleData& rule_data : *rules) {
+  for (const RuleData& rule_data : rules) {
     if (perf_trace_enabled) {
       selector_statistics_collector.EndCollectionForCurrentRule();
       selector_statistics_collector.BeginCollectionForRule(&rule_data);
@@ -445,7 +447,7 @@ void ElementRuleCollector::CollectMatchingRulesForListInternal(
   if (!style_engine.Stats())
     return;
 
-  unsigned rejected = rules->size() - fast_rejected - matched;
+  size_t rejected = rules.size() - fast_rejected - matched;
   INCREMENT_STYLE_STATS_COUNTER(style_engine, rules_rejected, rejected);
   INCREMENT_STYLE_STATS_COUNTER(style_engine, rules_fast_rejected,
                                 fast_rejected);
@@ -453,7 +455,7 @@ void ElementRuleCollector::CollectMatchingRulesForListInternal(
 }
 
 void ElementRuleCollector::CollectMatchingRulesForList(
-    const HeapVector<RuleData>* rules,
+    base::span<const RuleData> rules,
     const MatchRequest& match_request,
     const RuleSet* rule_set,
     const CSSStyleSheet* style_sheet,
@@ -464,7 +466,7 @@ void ElementRuleCollector::CollectMatchingRulesForList(
   // instead of inside CollectMatchingRulesForListInternal(), we're usually
   // inlined into the caller (which saves on stack setup and call overhead
   // in that common case).
-  if (!rules || rules->IsEmpty())
+  if (rules.empty())
     return;
 
   // To reduce branching overhead for the common case, we use a template
@@ -613,10 +615,11 @@ void ElementRuleCollector::CollectMatchingRules(
               : attribute_name;
       for (const auto bundle : match_request.AllRuleSets()) {
         if (bundle.rule_set->HasAnyAttrRules()) {
-          const HeapVector<RuleData>* list =
+          base::span<const RuleData> list =
               bundle.rule_set->AttrRules(lower_name);
-          if (list && !bundle.rule_set->CanIgnoreEntireList(
-                          list, lower_name, attributes[attr_idx].Value())) {
+          if (!list.empty() &&
+              !bundle.rule_set->CanIgnoreEntireList(
+                  list, lower_name, attributes[attr_idx].Value())) {
             CollectMatchingRulesForList(bundle.rule_set->AttrRules(lower_name),
                                         match_request, bundle.rule_set,
                                         bundle.style_sheet,
