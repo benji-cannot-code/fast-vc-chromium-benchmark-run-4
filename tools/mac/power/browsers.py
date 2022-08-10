f328605ccfbc546742ca5bfad6d7edbd76932efa
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-08-10 16:09:57 +0000

    Minor improvements to tools/mac/power benchmark scripts
    This CL makes a few small tweaks to the mac power benchmark:
    - Improves documentation
    - Adds a command line flag to forward command line arguments to the browser
    - Changes the sign of the `difference` field output by `compare.py`. This makes
      it clearer which direction the alternative is moving the measurements in.
    
    Bug: None
    Change-Id: I736722bb2b2ac860469928735d1cee474ae09a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822849
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033536}

--- tools/mac/power/browsers.py
+++ tools/mac/power/browsers.py
@@ -168,7 +168,8 @@ def MakeBrowserDriver(browser_name: str,
                       variation: str,
                       chrome_user_dir=None,
                       output_dir=None,
-                      tracing_mode=False) -> BrowserDriver:
+                      tracing_mode=False,
+                      extra_command_line=None) -> BrowserDriver:
   """Creates browser driver by name.
 
   Args:
@@ -195,6 +196,9 @@ def MakeBrowserDriver(browser_name: str,
       chrome_extra_arg += [
           f'--trace-startup-file={os.path.abspath(trace_path)}'
       ]
+
+    if extra_command_line:
+      chrome_extra_arg += [extra_command_line]
 
     if browser_name == "chrome":
       return Chrome(variation, extra_args=chrome_extra_arg)
