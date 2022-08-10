4ee38e7bbd1ac0cdd8eed209ff384b7b254b37b4
Rushan Suleymanov <rushans@google.com>
2022-08-10 07:40:26 +0000

    [Sync] Use the real version of committed tombstones in loopback server
    Prior to this CL, it was possible that there are two consecutive
    requests with the same entity which would return the same version (which
    violates DCHECK for increasing versions):
    1. Commit request with a tombstone.
    2. Commit request with new specifics using the same entity.
    
    Normally, a successful commit would just remove the entity on the first
    commit response. However in rare cases, when there was a local change
    (un-deletion) during the first commit request with the tombstone, the
    entity would be kept with a version which is ahead by 1 from the actual.
    That's why on the next commit request, loopback server would return the
    same version and violate DCHECK.
    
    The change is behind an enabled-by-default feature toggle.
    
    Bug: 1299874
    Change-Id: Ice62c5dcc6adcb3d6615c017bb6ace3c91d52a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820101
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033392}

--- components/sync/engine/loopback_server/loopback_server.cc
+++ components/sync/engine/loopback_server/loopback_server.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/cxx20_erase.h"
+#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/format_macros.h"
 #include "base/guid.h"
@@ -63,6 +64,14 @@ static const char kOtherBookmarksFolderServerTag[] = "other_bookmarks";
 static const char kOtherBookmarksFolderName[] = "Other Bookmarks";
 static const char kSyncedBookmarksFolderServerTag[] = "synced_bookmarks";
 static const char kSyncedBookmarksFolderName[] = "Synced Bookmarks";
+
+// Returns entity's version without increasing it by one for tombstones. The
+// version is updated and set in SaveEntity() and there is no need to increment
+// it again in CommitResponse. Otherwise, it would be possible that the next
+// commit request would return the same version.
+const base::Feature kSyncReturnRealVersionOnCommitInLoopbackServer{
+    "SyncReturnRealVersionOnCommitInLoopbackServer",
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 int GetServerMigrationVersion(
     const std::map<ModelType, int>& server_migration_versions,
@@ -582,7 +591,9 @@ void LoopbackServer::BuildEntryResponseForSuccessfulCommit(
                                         : sync_pb::CommitResponse::SUCCESS);
   entry_response->set_id_string(entity.GetId());
 
-  if (entity.IsDeleted()) {
+  if (entity.IsDeleted() &&
+      !base::FeatureList::IsEnabled(
+          kSyncReturnRealVersionOnCommitInLoopbackServer)) {
     entry_response->set_version(entity.GetVersion() + 1);
   } else {
     entry_response->set_version(entity.GetVersion());
