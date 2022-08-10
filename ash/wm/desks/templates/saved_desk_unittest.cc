b6cfc5986e3923d7eda3c98157883da1b4d2cd58
Daniel Andersson <dandersson@chromium.org>
2022-08-10 18:57:51 +0000

    saved_desks: Handle blocking modal dialogs when saving a desk for later.
    This CL makes a fairly substantial change to the way windows and desks
    are handled when the user saves a desk for later (Save & Recall).
    
    Previously, we would use some of the functionality from CloseAll to
    remove the desk and its windows after the desk definition had been
    saved. The logic there was synchronous and, while simple, did not handle
    blocking dialogs gracefully.
    
    With this change, we will now start a watcher that monitors the windows
    that are to be closed and also detects modal dialogs. The goal is to
    allow the user to deal with these dialogs that may appear. If the user
    elects to close the window(s), they will be taken to the saved desk
    library and the newly created saved desk is focused. If they decide to
    leave windows open, then the watcher terminates itself and the user is
    left on the desk.
    
    If there are no windows that show blocking dialogs, the flow should be
    the same as before this CL.
    
    Tested: Manually and with browser tests.
    
    Bug: 1334926
    Change-Id: I79707b63a0ab2874b2430821dd9b9158998a3b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806033
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033619}

--- ash/wm/desks/templates/saved_desk_unittest.cc
+++ ash/wm/desks/templates/saved_desk_unittest.cc
@@ -396,6 +396,8 @@ class SavedDeskTest : public OverviewTestBase {
         GetOverviewGridForRoot(root)->IsSaveDeskForLaterButtonVisible());
     ClickOnView(save_desk_button);
     WaitForDesksTemplatesUI();
+    WaitForDesksTemplatesUI();
+
     // Clicking the save desk button selects the newly saved desk's name
     // field. We can press enter or escape or click to select out of it.
     SendKey(ui::VKEY_RETURN);
@@ -4046,6 +4048,7 @@ TEST_F(SavedDeskTest, FocusedDeskItemFullyVisible) {
       GetSaveDeskForLaterButtonForRoot(Shell::Get()->GetPrimaryRootWindow());
   ClickOnView(save_desk_button);
   WaitForDesksTemplatesUI();
+  WaitForDesksTemplatesUI();
 
   // The newly saved desk item should be fully visible.
   SavedDeskItemView* item_view = GetItemViewFromTemplatesGrid(6);
@@ -4261,7 +4264,8 @@ TEST_F(DeskSaveAndRecallTest, SaveDeskWithDuplicateName) {
     ToggleOverview();
     ClickOnView(
         GetSaveDeskForLaterButtonForRoot(Shell::Get()->GetPrimaryRootWindow()));
-    WaitForDesksTemplatesUI();
+    for (int i = 0; i != 3; ++i)
+      WaitForDesksTemplatesUI();
 
     // Expect that the last added template item name view has focus, and verify
     // that we have a saved desk with the expected `name`.
