a5afcc6a85cbe441bc49bd20cde64f598f89fb22
Christian Dullweber <dullweber@chromium.org>
2022-08-11 10:00:30 +0000

    AboutThisSite: Add description placeholder
    Add placeholder for AboutThisSite description if no description is
    available. This change is guarded by a flag.
    
    Bug: 1351714
    Change-Id: Ic62d6ed89117a0571898b511734a89a0a533bccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822905
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033911}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.page_info;
 
+import android.content.res.Resources;
 import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
@@ -130,8 +131,10 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
         boolean more_info_enabled =
                 ChromeFeatureList.isEnabled(ChromeFeatureList.PAGE_INFO_ABOUT_THIS_SITE_MORE_INFO);
 
-        assert mSiteInfo.hasDescription();
-        String subtitle = mSiteInfo.getDescription().getDescription();
+        Resources resources = mRowView.getContext().getResources();
+        String subtitle = mSiteInfo.hasDescription()
+                ? mSiteInfo.getDescription().getDescription()
+                : resources.getString(R.string.page_info_about_this_page_description_placeholder);
         PageInfoRowView.ViewParams rowParams = new PageInfoRowView.ViewParams();
         rowParams.title = getTitle();
         rowParams.subtitle = subtitle;
--- components/page_info/core/about_this_site_service.cc
+++ components/page_info/core/about_this_site_service.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/page_info/core/about_this_site_service.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/optimization_guide/core/optimization_metadata.h"
@@ -71,13 +72,16 @@ absl::optional<proto::SiteInfo> AboutThisSiteService::GetAboutThisSiteInfo(
   if (kShowSampleContent.Get()) {
     page_info::proto::SiteInfo site_info;
     if (url == GURL("https://example.com")) {
-      auto* description = site_info.mutable_description();
-      description->set_name("Example website");
-      description->set_subtitle("Website");
-      description->set_description(
-          "A domain used in illustrative examples in documents.");
-      description->mutable_source()->set_url("https://example.com");
-      description->mutable_source()->set_label("Example source");
+      if (!base::FeatureList::IsEnabled(
+              kPageInfoAboutThisSiteDescriptionPlaceholder)) {
+        auto* description = site_info.mutable_description();
+        description->set_name("Example website");
+        description->set_subtitle("Website");
+        description->set_description(
+            "A domain used in illustrative examples in documents.");
+        description->mutable_source()->set_url("https://example.com");
+        description->mutable_source()->set_label("Example source");
+      }
       site_info.mutable_more_about()->set_url(
           "https://example.com/#more-about");
       return site_info;
