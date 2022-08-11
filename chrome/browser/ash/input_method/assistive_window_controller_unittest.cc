b0fe4d0ce38d23768d9f73032000fd6065e76b4d
jhtin <jhtin@chromium.org>
2022-08-11 22:39:19 +0000

    Revert "suggest: fix shifting suggestion window"
    This reverts commit 7dc39e7baed47caca7f41461e251c3e6979fa026.
    
    Reason for revert: Causes diacritics window to pop up on the top-left corner.
    
    https://screenshot.googleplex.com/4ogxKR6U4uMtBsE.png
    Bisect log: https://paste.googleplex.com/6667540586561536
    
    Original change's description:
    > suggest: fix shifting suggestion window
    >
    > Currently the multi word suggestion window aligns itself underneath the
    > relevant text in the currently focused input when a suggestion candidate
    > is triggered and shown to the user. The alignment works very well,
    > however there is a noticeable "shifting" of the window as the
    > AssistiveWindowController class attempts to align the suggestion window
    > based on events received.
    >
    > This is due to an unexpected ordering of events received to the
    > AssistiveWindowController. A ShowSuggestion event may be received prior
    > to SetBounds event (this is because surrounding text and cursor bounds
    > updates are sent as two distinct events).
    >
    > To accomodate this, the CL adds a delay before showing a multi word
    > suggestion to the user. The delay allows the two related events
    > (ShowSuggestion and SetBounds) to be received before making the
    > suggestion window visible to the user.
    >
    > BUG=b:241321719
    >
    > Change-Id: If14fe976b244d29deea0cd08925808e7a2221e03
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811193
    > Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1032789}
    
    Bug: b:241321719
    Change-Id: Ic00f81ee39433c6c524f9337d72b9c67640009ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826337
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: jhtin <jhtin@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034213}

--- chrome/browser/ash/input_method/assistive_window_controller_unittest.cc
+++ chrome/browser/ash/input_method/assistive_window_controller_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/test/base/chrome_ash_test_base.h"
 #include "chrome/test/base/testing_profile.h"
-#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window.h"
 #include "ui/base/ime/ash/ime_assistive_window_handler_interface.h"
@@ -32,8 +31,6 @@ const char16_t kAnnounceString[] = u"announce string";
 
 namespace ash {
 namespace input_method {
-
-constexpr size_t kShowSuggestionDelayMs = 5;
 
 class MockDelegate : public AssistiveWindowControllerDelegate {
  public:
@@ -57,10 +54,7 @@ class TestAccessibilityView : public ui::ime::AssistiveAccessibilityView {
 
 class AssistiveWindowControllerTest : public ChromeAshTestBase {
  protected:
-  AssistiveWindowControllerTest()
-      : ChromeAshTestBase(std::make_unique<content::BrowserTaskEnvironment>(
-            base::test::TaskEnvironment::TimeSource::MOCK_TIME)) {}
-
+  AssistiveWindowControllerTest() = default;
   ~AssistiveWindowControllerTest() override = default;
 
   void SetUp() override {
@@ -112,11 +106,6 @@ class AssistiveWindowControllerTest : public ChromeAshTestBase {
         /*disabled_features=*/{});
   }
 
-  void WaitForSuggestionWindowDelay() {
-    task_environment()->FastForwardBy(
-        base::Milliseconds(kShowSuggestionDelayMs + 1));
-  }
-
   base::test::ScopedFeatureList feature_list_;
   std::unique_ptr<AssistiveWindowController> controller_;
   std::unique_ptr<MockDelegate> delegate_ = std::make_unique<MockDelegate>();
@@ -127,42 +116,10 @@ class AssistiveWindowControllerTest : public ChromeAshTestBase {
   std::unique_ptr<TestAccessibilityView> accessibility_view_;
 };
 
-TEST_F(AssistiveWindowControllerTest, ShowSuggestionDelaysWindowDisplay) {
-  ui::ime::SuggestionDetails details;
-  details.text = u"asdf";
-  details.confirmed_length = 3;
-
-  controller_->ShowSuggestion(details);
-  ui::ime::SuggestionWindowView* window_before_delay =
-      controller_->GetSuggestionWindowViewForTesting();
-  WaitForSuggestionWindowDelay();
-  ui::ime::SuggestionWindowView* window_after_delay =
-      controller_->GetSuggestionWindowViewForTesting();
-
-  EXPECT_EQ(window_before_delay, nullptr);
-  EXPECT_NE(window_after_delay, nullptr);
-}
-
-TEST_F(AssistiveWindowControllerTest,
-       SetBoundsAfterShowSuggestionCancelsDelay) {
-  ui::ime::SuggestionDetails details;
-  details.text = u"asdf";
-  details.confirmed_length = 3;
-  gfx::Rect caret_bounds(0, 0, 100, 100);
-
-  controller_->ShowSuggestion(details);
-  controller_->SetBounds(Bounds{.caret = caret_bounds});
-  ui::ime::SuggestionWindowView* suggestion_window_view =
-      controller_->GetSuggestionWindowViewForTesting();
-
-  EXPECT_NE(suggestion_window_view, nullptr);
-}
-
 TEST_F(AssistiveWindowControllerTest, ShowSuggestionSetsConfirmedLength) {
   ui::ime::SuggestionDetails details;
   details.text = u"asdf";
   details.confirmed_length = 3;
-
   controller_->ShowSuggestion(details);
 
   EXPECT_EQ(controller_->GetConfirmedLength(), 3u);
@@ -172,18 +129,16 @@ TEST_F(AssistiveWindowControllerTest, ConfirmedLength0SetsBoundsToCaretBounds) {
   ui::ime::SuggestionDetails details;
   details.text = suggestion_;
   details.confirmed_length = 0;
-
   controller_->ShowSuggestion(details);
-  WaitForSuggestionWindowDelay();
   ui::ime::SuggestionWindowView* suggestion_view =
       controller_->GetSuggestionWindowViewForTesting();
 
-  ASSERT_NE(suggestion_view, nullptr);
   gfx::Rect current_bounds = suggestion_view->GetAnchorRect();
   gfx::Rect caret_bounds(0, 0, 100, 100);
   Bounds bounds;
   bounds.caret = caret_bounds;
   controller_->SetBounds(bounds);
+
   EXPECT_NE(suggestion_view->GetAnchorRect(), current_bounds);
   EXPECT_EQ(suggestion_view->GetAnchorRect(), caret_bounds);
 }
@@ -192,18 +147,16 @@ TEST_F(AssistiveWindowControllerTest, ConfirmedLengthNSetsBoundsToCaretBounds) {
   ui::ime::SuggestionDetails details;
   details.text = suggestion_;
   details.confirmed_length = 1;
-
   controller_->ShowSuggestion(details);
-  WaitForSuggestionWindowDelay();
   ui::ime::SuggestionWindowView* suggestion_view =
       controller_->GetSuggestionWindowViewForTesting();
 
-  ASSERT_NE(suggestion_view, nullptr);
   gfx::Rect current_bounds = suggestion_view->GetAnchorRect();
   gfx::Rect caret_bounds(0, 0, 100, 100);
   Bounds bounds;
   bounds.caret = caret_bounds;
   controller_->SetBounds(bounds);
+
   EXPECT_NE(suggestion_view->GetAnchorRect(), current_bounds);
   EXPECT_EQ(suggestion_view->GetAnchorRect(), caret_bounds);
 }
@@ -212,13 +165,10 @@ TEST_F(AssistiveWindowControllerTest, WindowTracksCaretBounds) {
   ui::ime::SuggestionDetails details;
   details.text = suggestion_;
   details.confirmed_length = 0;
-
   controller_->ShowSuggestion(details);
-  WaitForSuggestionWindowDelay();
   ui::ime::SuggestionWindowView* suggestion_view =
       controller_->GetSuggestionWindowViewForTesting();
 
-  ASSERT_NE(suggestion_view, nullptr);
   gfx::Rect current_bounds = suggestion_view->GetAnchorRect();
   gfx::Rect caret_bounds_after_one_key(current_bounds.width() + 1,
                                        current_bounds.height());
@@ -245,9 +195,7 @@ TEST_F(AssistiveWindowControllerTest,
   ui::ime::SuggestionDetails details;
   details.text = suggestion_;
   details.confirmed_length = 1;
-
   controller_->ShowSuggestion(details);
-  WaitForSuggestionWindowDelay();
 
   gfx::Rect current_bounds =
       controller_->GetSuggestionWindowViewForTesting()->GetAnchorRect();
