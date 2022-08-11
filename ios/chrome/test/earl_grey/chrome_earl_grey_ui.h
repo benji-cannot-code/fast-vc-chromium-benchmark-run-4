9e546ae8462a714d1f0db0e5adfe6aa1c40b3e79
Mike Dougherty <michaeldo@chromium.org>
2022-08-11 19:20:18 +0000

    Create `[ChromeEarlGreyUI openTabGrid]` helper
    Attempting to open the tab grid via a standard grey_tap on the
    ShowTabsButton sometimes acts like a long press.
    
    A workaround already exists for this specific problem in
    NTPHomeTestCase, so this CL moves it to ChromeEarlGreyUI so that it can
    be used by other tests.
    
    Bug: 1350734
    Change-Id: Id2c2ffaf0a57564e8e84a55ad3c7d4d206335e71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827286
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034126}

--- ios/chrome/test/earl_grey/chrome_earl_grey_ui.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_ui.h
@@ -115,6 +115,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Opens a new incognito tab via the tools menu.
 - (void)openNewIncognitoTab;
 
+// Opens the tab grid.
+- (void)openTabGrid;
+
 // Opens and clear browsing data from history.
 - (void)openAndClearBrowsingDataFromHistory;
 
