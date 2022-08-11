ad400b2b7c7207fe7b501848b7fe943ba1e6baa3
Maksim Sisov <msisov@igalia.com>
2022-08-11 07:46:47 +0000

    ozone/wayland: update window scale on leaving output.
    WaylandWindow may have wrong scale factor set. If it's located on
    two outputs at the same time with the first having scale factor 1
    and the second having scale factor 2, the window uses the highest
    possible scale factor. However, when the window leaves one of the
    outputs, it won't have its scale factor updated as a "enter output"
    event won't be sent. It'll only going to be a "leave output" event.
    
    Thus, the window may always use scale factor 2 despite being on a
    display with lower scale factor.
    
    To fix that, make it always update scale factor whenever enter/leave
    output events are sent.
    
    Bug: 1351166
    Change-Id: I1ea35880a4c2f71da943c3e55f6b9e98e57e67c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822706
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1033877}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -698,8 +698,8 @@ void WaylandWindow::OnLeftOutput() {
   // event.
   if (AsWaylandPopup())
     return;
-  // Do not update the window scale where. It'll be updated when entring a new
-  // output.
+
+  UpdateWindowScale(true);
 }
 
 WaylandWindow* WaylandWindow::GetTopMostChildWindow() {
