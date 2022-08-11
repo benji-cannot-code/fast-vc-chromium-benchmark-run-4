e8982ba6ecf37aafc7cf1b6e48e9707b6e4d103f
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-08-11 17:50:05 +0000

    [Extensions] Move some util methods to NetworkPermissionsUpdater
    Move the browser-side methods to update the origin access lists for
    extensions out from extension_util.* to NetworkPermissionsUpdater.
    This keeps the methods centralized and declutters our utils a bit.
    
    This CL has no behavior change.
    
    Bug: None
    Change-Id: If30a646f86686beca9283a973a61d920019e0c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810623
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034075}

--- extensions/browser/extension_util.cc
+++ extensions/browser/extension_util.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crx_file/id_util.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/child_process_security_policy.h"
-#include "content/public/browser/cors_origin_pattern_setter.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/storage_partition_config.h"
 #include "content/public/common/url_constants.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extensions_browser_client.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/ui_util.h"
-#include "extensions/common/cors_util.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/features/behavior_feature.h"
 #include "extensions/common/features/feature.h"
@@ -50,28 +48,6 @@ bool IsSigninProfileTestExtensionOnTestImage(const Extension* extension) {
   return true;
 }
 #endif
-
-void SetCorsOriginAccessListForExtensionHelper(
-    const std::vector<content::BrowserContext*>& browser_contexts,
-    const Extension& extension,
-    std::vector<network::mojom::CorsOriginPatternPtr> allow_patterns,
-    std::vector<network::mojom::CorsOriginPatternPtr> block_patterns,
-    base::OnceClosure closure) {
-  auto barrier_closure =
-      BarrierClosure(browser_contexts.size(), std::move(closure));
-  for (content::BrowserContext* browser_context : browser_contexts) {
-    // SetCorsOriginAccessListForExtensionHelper should only affect an incognito
-    // profile if the extension is actually allowed to run in an incognito
-    // profile (not just by the extension manifest, but also by user
-    // preferences).
-    if (browser_context->IsOffTheRecord())
-      DCHECK(IsIncognitoEnabled(extension.id(), browser_context));
-
-    content::CorsOriginPatternSetter::Set(
-        browser_context, extension.origin(), mojo::Clone(allow_patterns),
-        mojo::Clone(block_patterns), barrier_closure);
-  }
-}
 
 }  // namespace
 
@@ -242,22 +218,6 @@ int GetBrowserContextId(content::BrowserContext* context) {
   }
   DCHECK(iter->second != kUnspecifiedContextId);
   return iter->second;
-}
-
-void SetCorsOriginAccessListForExtension(
-    const std::vector<content::BrowserContext*>& browser_contexts,
-    const Extension& extension,
-    base::OnceClosure closure) {
-  SetCorsOriginAccessListForExtensionHelper(
-      browser_contexts, extension, CreateCorsOriginAccessAllowList(extension),
-      CreateCorsOriginAccessBlockList(extension), std::move(closure));
-}
-
-void ResetCorsOriginAccessListForExtension(
-    content::BrowserContext* browser_context,
-    const Extension& extension) {
-  SetCorsOriginAccessListForExtensionHelper({browser_context}, extension, {},
-                                            {}, base::DoNothing());
 }
 
 // Returns whether the |extension| should be loaded in the given
