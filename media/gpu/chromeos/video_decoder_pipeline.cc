21523aed4744e20c57a342c8e17920609f399ad2
Jeffrey Kardatzke <jkardatzke@google.com>
2022-08-11 19:57:23 +0000

    Remote CdmContext handling for Lacros/OOP video decoding
    This contains both ends of the implementation for being able to remote
    a CdmContext for the purpose of out of process video decoding, which
    will mainly be used for Lacros.
    
    The StableCdmContextImpl is what will be used in lacros-gpu which
    implements the StableCdmContext mojo interface so it can be passed to
    the OOP video decoder. Then in the OOP video decoder, it can put the
    mojo interface into a RemoteCdmContext object which implements
    media::CdmContext for passing into a video decoder implementation.
    
    This also updates the MojoCdmServiceContext to be able to convert
    between the CDM ID and the CdmContext linked to the RemoteCdmContext in
    the utility process running OOP video decoding.
    
    This does not actually enable HWDRM for Lacros. That will be done in a
    follow up CL after we have enabled OOPVideoDecoding. HEVC for Lacros
    will also be enabled at the same time as that.
    
    BUG=b:194429120
    TEST=L1 playback works in Lacros on volteer
    
    Change-Id: Ib44c2fc60420446a0bc857d2fbca0842562531be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366612
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034138}

--- media/gpu/chromeos/video_decoder_pipeline.cc
+++ media/gpu/chromeos/video_decoder_pipeline.cc
@@ -296,9 +296,9 @@ VideoDecoderPipeline::~VideoDecoderPipeline() {
   main_frame_pool_.reset();
   frame_converter_.reset();
   decoder_.reset();
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   buffer_transcryptor_.reset();
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 // static
@@ -452,7 +452,7 @@ void VideoDecoderPipeline::OnInitializeDone(InitCB init_cb,
   MEDIA_LOG(INFO, media_log_)
       << "VideoDecoderPipeline |decoder_| Initialize() successful";
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (decoder_ && decoder_->NeedsTranscryption()) {
     if (!cdm_context) {
       VLOGF(1) << "CdmContext required for transcryption";
@@ -471,7 +471,7 @@ void VideoDecoderPipeline::OnInitializeDone(InitCB init_cb,
     // In case this was created on a prior initialization but no longer needed.
     buffer_transcryptor_.reset();
   }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   client_task_runner_->PostTask(FROM_HERE,
                                 base::BindOnce(std::move(init_cb), status));
@@ -503,10 +503,10 @@ void VideoDecoderPipeline::OnResetDone(base::OnceClosure reset_cb) {
     image_processor_->Reset();
   frame_converter_->AbortPendingFrames();
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (buffer_transcryptor_)
     buffer_transcryptor_->Reset(DecoderStatus::Codes::kAborted);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   CallFlushCbIfNeeded(DecoderStatus::Codes::kAborted);
 
@@ -546,7 +546,7 @@ void VideoDecoderPipeline::DecodeTask(scoped_refptr<DecoderBuffer> buffer,
   }
 
   const bool is_flush = buffer->end_of_stream();
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (buffer_transcryptor_) {
     buffer_transcryptor_->EnqueueBuffer(
         std::move(buffer),
@@ -554,7 +554,7 @@ void VideoDecoderPipeline::DecodeTask(scoped_refptr<DecoderBuffer> buffer,
                        is_flush, std::move(decode_cb)));
     return;
   }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   decoder_->Decode(
       std::move(buffer),
@@ -657,10 +657,10 @@ void VideoDecoderPipeline::OnError(const std::string& msg) {
   MEDIA_LOG(ERROR, media_log_) << "VideoDecoderPipeline " << msg;
 
   has_error_ = true;
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (buffer_transcryptor_)
     buffer_transcryptor_->Reset(DecoderStatus::Codes::kFailed);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
   CallFlushCbIfNeeded(DecoderStatus::Codes::kFailed);
 }
 
@@ -878,7 +878,7 @@ VideoDecoderPipeline::PickDecoderOutputFormat(
                               gfx::NativePixmapHandle::kNoModifier};
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
 void VideoDecoderPipeline::OnBufferTranscrypted(
     scoped_refptr<DecoderBuffer> transcrypted_buffer,
     DecodeCB decode_callback) {
@@ -892,6 +892,6 @@ void VideoDecoderPipeline::OnBufferTranscrypted(
 
   decoder_->Decode(std::move(transcrypted_buffer), std::move(decode_callback));
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace media
