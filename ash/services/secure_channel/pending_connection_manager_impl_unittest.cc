116a0734cdd26e2451db13303e2673e46f531d9c
Nico Weber <thakis@chromium.org>
2022-08-11 01:36:48 +0000

    Enable -Wunqualified-std-cast-call
    Bug: 1300731
    Change-Id: I4d380265150d1928c555d7a38d5b1f1236f131b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825237
    Owners-Override: Nico Weber <thakis@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033820}

--- ash/services/secure_channel/pending_connection_manager_impl_unittest.cc
+++ ash/services/secure_channel/pending_connection_manager_impl_unittest.cc
@@ -684,7 +684,8 @@ class SecureChannelPendingConnectionManagerImplTest : public testing::Test {
         ClientParamsListToRawPtrs(clients_to_send);
 
     attempt->set_client_data_for_extraction(std::move(clients_to_send));
-    attempt->OnConnectionAttemptSucceeded(move(fake_authenticated_channel));
+    attempt->OnConnectionAttemptSucceeded(
+        std::move(fake_authenticated_channel));
 
     EXPECT_FALSE(GetActiveBleInitiatorAttempt(details_for_attempt));
     EXPECT_EQ(num_received_connections_before_call + 1u,
@@ -719,7 +720,8 @@ class SecureChannelPendingConnectionManagerImplTest : public testing::Test {
         ClientParamsListToRawPtrs(clients_to_send);
 
     attempt->set_client_data_for_extraction(std::move(clients_to_send));
-    attempt->OnConnectionAttemptSucceeded(move(fake_authenticated_channel));
+    attempt->OnConnectionAttemptSucceeded(
+        std::move(fake_authenticated_channel));
 
     EXPECT_FALSE(GetActiveBleListenerAttempt(details_for_attempt));
     EXPECT_EQ(num_received_connections_before_call + 1u,
@@ -754,7 +756,8 @@ class SecureChannelPendingConnectionManagerImplTest : public testing::Test {
         ClientParamsListToRawPtrs(clients_to_send);
 
     attempt->set_client_data_for_extraction(std::move(clients_to_send));
-    attempt->OnConnectionAttemptSucceeded(move(fake_authenticated_channel));
+    attempt->OnConnectionAttemptSucceeded(
+        std::move(fake_authenticated_channel));
 
     EXPECT_FALSE(GetActiveNearbyInitiatorAttempt(details_for_attempt));
     EXPECT_EQ(num_received_connections_before_call + 1u,
