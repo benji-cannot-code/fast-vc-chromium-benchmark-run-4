1ede3e6d76bbd908745c1ebea6139dd71e288a79
Liam Brady <lbrady@google.com>
2022-08-11 23:33:47 +0000

    Fenced frame: enable attribution reporting for opaque-ads
    Opaque-ads fenced frames need to have attribution reporting enabled
    to be able to support FLEDGE. Until the opaque URN attributes feature
    is in place, this is a temporary solution to allow a fenced frame to
    have attribution reporting.
    
    - An opaque-ads fenced frame will not be allowed to load if attribution reporting is explicitly disabled in its embedder.
    - If the embedder doesn't specify any attribution reporting policy in
    its header, the opaque-ads fenced frame will act as if it was enabled
    via allow (*) and allow itself to load.
    - If an opaque-ads fenced frame loads, it will turn on attribution reporting in its permissions policy.
    - Update fenced frame creation/initialization to do most of the
    initialization after the fenced frame constructor. This ensures that the
    fenced frame is properly read into the fenced frames vector before
    initialization, allowing us to determine the mode for MPArch (which
    expects the fenced frame to be in the vector).
    
    See design doc:
    https://docs.google.com/document/d/1vnCoglukP61ucDY4ob6zgizM1aoilmlFOq4Gcs8OVX8/edit?usp=sharing&resourcekey=0-9UtvE--vXmeNJHiKZgx7GA
    
    Change-Id: Ib53c4be57e7701bbf052377fcdf7bfd2570b2e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779724
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034236}

--- third_party/blink/public/common/permissions_policy/permissions_policy.h
+++ third_party/blink/public/common/permissions_policy/permissions_policy.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/common/permissions_policy/permissions_policy_features.h"
+#include "third_party/blink/public/mojom/fenced_frame/fenced_frame.mojom-shared.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom-forward.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-forward.h"
 #include "url/origin.h"
@@ -195,9 +196,12 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
       const PermissionsPolicy*);
 
   // Creates a PermissionsPolicy for a fenced frame. All permissions are
-  // disabled in fenced frames.
+  // disabled in fenced frames except for attribution reporting (which are only
+  // enabled for opaque-ads fenced frames). Permissions do not inherit from the
+  // parent to prevent cross-channel communication.
   static std::unique_ptr<PermissionsPolicy> CreateForFencedFrame(
-      const url::Origin& origin);
+      const url::Origin& origin,
+      blink::mojom::FencedFrameMode mode);
 
   static std::unique_ptr<PermissionsPolicy> CreateFromParsedPolicy(
       const ParsedPermissionsPolicy& parsed_policy,
@@ -274,7 +278,8 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
 
   static std::unique_ptr<PermissionsPolicy> CreateForFencedFrame(
       const url::Origin& origin,
-      const PermissionsPolicyFeatureList& features);
+      const PermissionsPolicyFeatureList& features,
+      blink::mojom::FencedFrameMode mode);
 
   bool InheritedValueForFeature(
       const PermissionsPolicy* parent_policy,
---
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/attribution-reporting-helper.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This is a helper file used for the attribution-reporting-*.https.html tests.
+// To use this, make sure you import these scripts:
+// <script src="/resources/testharness.js"></script>
+// <script src="/resources/testharnessreport.js"></script>
+// <script src="/common/utils.js"></script>
+// <script src="/common/dispatcher/dispatcher.js"></script>
+// <script src="resources/utils.js"></script>
+// <script src="/common/get-host-info.sub.js"></script>
+
+async function runAttributionReportingTest(t, should_load, fenced_origin) {
+  const fencedframe = attachFencedFrameContext({
+      attributes: [["mode", "opaque-ads"]],
+      origin: fenced_origin});
+
+  if (!should_load) {
+    const fencedframe_blocked = new Promise(r => t.step_timeout(r, 1000));
+    const fencedframe_loaded = fencedframe.execute(() => {});
+    assert_equals("blocked", await Promise.any([
+      fencedframe_blocked.then(() => "blocked"),
+      fencedframe_loaded.then(() => "loaded"),
+    ]), "The fenced frame should not be loaded.");
+    return;
+  }
+
+  await fencedframe.execute(() => {
+    assert_true(
+        document.featurePolicy.allowsFeature('attribution-reporting'),
+        "Attribution reporting should be allowed if the fenced " +
+        " frame loaded.");
+  });
+}
