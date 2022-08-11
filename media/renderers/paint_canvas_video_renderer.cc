0e25aace94b3133f6d99d52d58d89edd39fb3076
Nate Fischer <ntfschr@chromium.org>
2022-08-11 01:50:38 +0000

    Revert "PaintCanvasVideoRenderer: Consolidate paths that copy to WebGL"
    This reverts commit 3f3978ad0b083fdaf584c860e953c1a3a9d0811f.
    
    Reason for revert: tests are failing (crbug.com/1351957)
    
    Original change's description:
    > PaintCanvasVideoRenderer: Consolidate paths that copy to WebGL
    >
    > Consolidate these paths and rename them so that it's more clear that
    > they do the same thing.
    >
    > Bug: 1348566
    > Change-Id: If36cdefe5a1771c88498c8e8d6845b2b15dee083
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817022
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    > Commit-Queue: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1033582}
    
    Bug: 1348566
    Change-Id: Id10cda5586a22986f02e3d2bf71ab0fbdce7fef1
    Fixed: 1351957
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825637
    Owners-Override: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033822}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -1361,19 +1361,18 @@ void PaintCanvasVideoRenderer::ConvertVideoFrameToRGBPixels(
   }
 }
 
-bool PaintCanvasVideoRenderer::
-    CopyVideoFrameToWebGLTexture_ViaCopyToSharedImage(
-        viz::RasterContextProvider* raster_context_provider,
-        gpu::gles2::GLES2Interface* destination_gl,
-        scoped_refptr<VideoFrame> video_frame,
-        unsigned int target,
-        unsigned int texture,
-        unsigned int internal_format,
-        unsigned int format,
-        unsigned int type,
-        int level,
-        bool premultiply_alpha,
-        bool flip_y) {
+bool PaintCanvasVideoRenderer::CopyVideoFrameTexturesToGLTexture(
+    viz::RasterContextProvider* raster_context_provider,
+    gpu::gles2::GLES2Interface* destination_gl,
+    scoped_refptr<VideoFrame> video_frame,
+    unsigned int target,
+    unsigned int texture,
+    unsigned int internal_format,
+    unsigned int format,
+    unsigned int type,
+    int level,
+    bool premultiply_alpha,
+    bool flip_y) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(video_frame);
   DCHECK(video_frame->HasTextures());
@@ -1394,9 +1393,9 @@ bool PaintCanvasVideoRenderer::
     // accurate.
     if ((media::IsOpaque(video_frame->format()) || premultiply_alpha) &&
         level == 0 && video_frame->NumTextures() > 1) {
-      if (CopyVideoFrameToWebGLTexture_Direct(
-              raster_context_provider, destination_gl, video_frame.get(),
-              target, texture, internal_format, format, type, flip_y)) {
+      if (UploadVideoFrameToGLTexture(raster_context_provider, destination_gl,
+                                      video_frame.get(), target, texture,
+                                      internal_format, format, type, flip_y)) {
         return true;
       }
     }
@@ -1458,7 +1457,7 @@ bool PaintCanvasVideoRenderer::
   return true;
 }
 
-bool PaintCanvasVideoRenderer::CopyVideoFrameToWebGLTexture_Direct(
+bool PaintCanvasVideoRenderer::UploadVideoFrameToGLTexture(
     viz::RasterContextProvider* raster_context_provider,
     gpu::gles2::GLES2Interface* destination_gl,
     scoped_refptr<VideoFrame> video_frame,
@@ -1611,43 +1610,7 @@ bool PaintCanvasVideoRenderer::PrepareVideoFrameForWebGL(
   return true;
 }
 
-bool PaintCanvasVideoRenderer::CopyVideoFrameToWebGLTexture(
-    viz::RasterContextProvider* raster_context_provider,
-    gpu::gles2::GLES2Interface* destination_gl,
-    bool gpu_teximage_is_slow,
-    scoped_refptr<VideoFrame> video_frame,
-    unsigned int target,
-    unsigned int texture,
-    unsigned int internal_format,
-    unsigned int format,
-    unsigned int type,
-    int level,
-    bool unpack_premultiply_alpha,
-    bool unpack_flip_y) {
-  // Go through the fast path doing a GPU-GPU textures copy without a readback
-  // to system memory if possible.  Otherwise, it will fall back to the normal
-  // SW path.
-  if (video_frame->HasTextures() &&
-      CopyVideoFrameToWebGLTexture_ViaCopyToSharedImage(
-          raster_context_provider, destination_gl, video_frame, target, texture,
-          internal_format, format, type, level, unpack_premultiply_alpha,
-          unpack_flip_y)) {
-    return true;
-  }
-
-  if (!video_frame->HasTextures() && media::IsOpaque(video_frame->format()) &&
-      !gpu_teximage_is_slow &&
-      CopyVideoFrameToWebGLTexture_ViaUploadToYUVCache(
-          raster_context_provider, destination_gl, video_frame, target, texture,
-          internal_format, format, type, level, unpack_premultiply_alpha,
-          unpack_flip_y)) {
-    return true;
-  }
-
-  return false;
-}
-
-bool PaintCanvasVideoRenderer::CopyVideoFrameToWebGLTexture_ViaUploadToYUVCache(
+bool PaintCanvasVideoRenderer::CopyVideoFrameYUVDataToGLTexture(
     viz::RasterContextProvider* raster_context_provider,
     gpu::gles2::GLES2Interface* destination_gl,
     scoped_refptr<VideoFrame> video_frame,
@@ -1689,9 +1652,9 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameToWebGLTexture_ViaUploadToYUVCache(
   // accurate.
   if ((media::IsOpaque(video_frame->format()) || premultiply_alpha) &&
       level == 0) {
-    if (CopyVideoFrameToWebGLTexture_Direct(
-            raster_context_provider, destination_gl, video_frame, target,
-            texture, internal_format, format, type, flip_y)) {
+    if (UploadVideoFrameToGLTexture(raster_context_provider, destination_gl,
+                                    video_frame, target, texture,
+                                    internal_format, format, type, flip_y)) {
       return true;
     }
   }
