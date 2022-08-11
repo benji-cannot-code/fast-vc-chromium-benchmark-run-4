21a93559a837e1b932339a474a733890f1b70e1c
adamta <adamta@google.com>
2022-08-11 23:14:19 +0000

    [iOS] Reset feed type when opening new NTP
    Bug: 1351890
    Change-Id: I282d8fe0039a439681e67a3911906b29f49e506d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824046
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034228}

--- ios/chrome/browser/ui/ntp/feed_header_view_controller.h
+++ ios/chrome/browser/ui/ntp/feed_header_view_controller.h
@@ -65,6 +65,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Updates the header for when the Following Feed visibility has changed.
 - (void)updateForFollowingFeedVisibilityChanged;
 
+// Updates the segmented control and sort button for the current feed type.
+- (void)updateForSelectedFeed;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_NTP_FEED_HEADER_VIEW_CONTROLLER_H_
