162105f94b98af5c29790e24dd752b0ef7e245e3
Roger Tawa <rogerta@chromium.org>
2022-08-11 19:17:27 +0000

    Revert "[ViewsAX] Move AXTree reference to AXTreeManager"
    This reverts commit 51fa600fe83a2d52bd65dd8cb6caf5a4fd19d093.
    
    Reason for revert: Caused a build break:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-builder-rel/34106/overview
    
    Original change's description:
    > [ViewsAX] Move AXTree reference to AXTreeManager
    >
    > This is the first change of many to refactor the AXTreeManager and
    > extending classes to allow a maximum of code reusability between the
    > BrowserAccessibilityManager and the ViewsAXTreeManager.
    >
    > In this CL, we move the reference to the AXTree (and AXTreeID) from
    > the subclasses to the AXTreeManager itself. This means that
    > AXTreeManager is no longer an abstract pure class. More changes will
    > follow to move more of the logic from the extending classes to the
    > AXTreeManager root class.
    >
    > Bug: 1049261
    > Change-Id: Icf3ce1f76d203c965558633f3d3db7071d8de043
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810990
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1034116}
    
    Bug: 1049261
    Change-Id: Ib6ad8170ce31c1bccc25f6c73824ea0b51cac6ec
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827902
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Owners-Override: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034125}

--- ui/accessibility/test_ax_tree_manager.h
+++ ui/accessibility/test_ax_tree_manager.h
@@ -31,7 +31,7 @@ class AX_EXPORT TestAXTreeManager : public AXTreeManager {
   // Takes ownership of |tree|.
   explicit TestAXTreeManager(std::unique_ptr<AXTree> tree);
 
-  ~TestAXTreeManager() override;
+  virtual ~TestAXTreeManager();
 
   TestAXTreeManager(const TestAXTreeManager& manager) = delete;
   TestAXTreeManager& operator=(const TestAXTreeManager& manager) = delete;
@@ -55,6 +55,9 @@ class AX_EXPORT TestAXTreeManager : public AXTreeManager {
   AXNode* GetRootAsAXNode() const override;
   AXNode* GetParentNodeFromParentTreeAsAXNode() const override;
   std::string ToString() const override;
+
+ private:
+  std::unique_ptr<AXTree> tree_;
 };
 
 }  // namespace ui
