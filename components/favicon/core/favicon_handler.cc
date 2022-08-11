58b9bee11e930e5d5e19367154382856951a7884
Eric Seckler <eseckler@chromium.org>
2022-08-11 18:05:49 +0000

    Revert "[blink/renderer] Fix image download logic in renderer to filter instead"
    This reverts commit 45351855e193fbb7caee43ab2da8178a490a41d6.
    
    Reason for revert: likely causes crashes on perf bots (crbug/1352191)
    
    Original change's description:
    > [blink/renderer] Fix image download logic in renderer to filter instead
    > of resize
    >
    > This CL introduces logic in blink/renderer to filter out images instead
    > of resizing the smallest image in case no images are found about a
    > specific max size. This also moves the filter logic into the callsites
    > instead of renderer, so that this is handler on a use-case basis.
    >
    > Doc: go/blink_image
    >
    > Bug: 1176678, 1348264
    > Change-Id: If1cae73c5f5bfb2146156a6872dac5d4b41aa577
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781583
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1033659}
    
    Bug: 1176678, 1348264
    Bug: 1352191
    Change-Id: I50dc02aa3411f9e921dda59bad5f7da3daa55406
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825876
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034081}

--- components/favicon/core/favicon_handler.cc
+++ components/favicon/core/favicon_handler.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon_base/favicon_util.h"
 #include "components/favicon_base/select_favicon_frames.h"
 #include "skia/ext/image_operations.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_util.h"
@@ -715,8 +714,9 @@ void FaviconHandler::ScheduleImageDownload(const GURL& image_url,
   image_download_request_.Reset(
       base::BindOnce(&FaviconHandler::OnDidDownloadFavicon,
                      base::Unretained(this), icon_type));
-  // The maximal icon size is passed in to set the preferred size for vector
-  // images. See FaviconHandler::Delegate::DownloadImage() for more info.
+  // A max bitmap size is specified to avoid receiving huge bitmaps in
+  // OnDidDownloadFavicon(). See FaviconDriver::StartDownload()
+  // for more details about the max bitmap size.
   const int download_id = delegate_->DownloadImage(
       image_url, GetMaximalIconSize(handler_type_, !manifest_url_.is_empty()),
       image_download_request_.callback());
