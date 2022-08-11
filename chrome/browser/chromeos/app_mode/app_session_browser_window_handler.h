1754faee1476a36b6704b3295829f4205f23206a
Irina Fedorova <irfedorova@google.com>
2022-08-11 09:23:57 +0000

    kiosk: add AppSessionPolicies class to manage AppSession policies
    AppSessionPolicies saved all policies locally for quicker access and
    updates these values automatically.
    
    This class manages only kNewWindowsInKioskAllowed policy for now, but
    has interface to add new policies in the future.
    
    AppSessionPolicies class will be used in AppSessionBrowserWindowHandler
    in the future CLs.
    
    Bug: b/235362946
    Test: AppSessionPolociesTest.*
    Change-Id: I2a8c4d872419c3ac61f01daf88d32accf9ed783a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816905
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033900}

--- chrome/browser/chromeos/app_mode/app_session_browser_window_handler.h
+++ chrome/browser/chromeos/app_mode/app_session_browser_window_handler.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/chromeos/app_mode/app_session_policies.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 
 namespace chromeos {
@@ -69,6 +69,9 @@ class AppSessionBrowserWindowHandler : public BrowserListObserver {
   // Browser in which settings are shown, restricted by
   // KioskSettingsNavigationThrottle.
   raw_ptr<Browser> settings_browser_ = nullptr;
+
+  // Provides access to app session related policies.
+  std::unique_ptr<AppSessionPolicies> app_session_policies_;
 
   base::WeakPtrFactory<AppSessionBrowserWindowHandler> weak_ptr_factory_{this};
 };
