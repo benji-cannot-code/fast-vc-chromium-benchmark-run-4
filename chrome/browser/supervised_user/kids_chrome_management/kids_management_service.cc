0eeb90d9bee96aad91e972906fb5de70f25ff3d7
Tomasz Jurkiewicz <tju@google.com>
2022-08-11 08:21:43 +0000

    Create Kids external API service layer (the factory boilerplate).
    The solution might lack some features like instrumentation, logging or
    auxiliary API.
    
    Bug: 1336631
    Change-Id: Ifffd3848fb2efdf2a29cfed702c41c4474a32c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804802
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033889}

---
+++ chrome/browser/supervised_user/kids_chrome_management/kids_management_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/supervised_user/kids_chrome_management/kids_management_service.h"
+
+#include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/browser_context.h"
+
+namespace chrome::kids {
+// Builds the service instance and its local dependencies.
+// The profile dependency is needed to verify the dynamic child account status.
+KeyedService* KidsManagementServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* browser_context) const {
+  return new KidsManagementService();
+}
+}  // namespace chrome::kids
