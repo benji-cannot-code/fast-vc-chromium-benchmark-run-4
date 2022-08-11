8442ba94eeb1c877a1ccb903a117bf1680bbac42
Christoph Schwering <schwering@google.com>
2022-08-11 07:56:30 +0000

    Revert "Use autocomplete types as part of local heuristics."
    This reverts commit 02f4c030e33191ab5698b2cbf049d91595ddd7dc.
    
    Reason for revert: Culprit for
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-on-arm64-rel/37481/overview
    
    Original change's description:
    > Use autocomplete types as part of local heuristics.
    >
    > Interpret the fields' parsable_name() (id or name attribute) as an
    > autocomplete type and classify them by it. E.g. <input id=given-name>.
    >
    > Bug: 1345879
    > Change-Id: Ia40bb619af23798f0ba29d393e4eb8b2de4dee45
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766676
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1033875}
    
    Bug: 1345879
    Change-Id: I7dc562e960099a252fc29333a9445c33de42c0cf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825898
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033878}

--- components/autofill/core/browser/form_processing/autocomplete_attribute_processing_util.cc
+++ components/autofill/core/browser/form_processing/autocomplete_attribute_processing_util.cc
@@ -138,7 +138,6 @@ absl::optional<HtmlFieldType> ParseProposedAutocompleteAttribute(
       base::MakeFixedFlatMap<base::StringPiece, HtmlFieldType>({
           {"address", HTML_TYPE_STREET_ADDRESS},
           {"coupon-code", HTML_TYPE_MERCHANT_PROMO_CODE},
-          // TODO(crbug.com/1351760): Investigate if this mapping makes sense.
           {"username", HTML_TYPE_EMAIL},
       });
 
@@ -184,8 +183,9 @@ bool ShouldIgnoreAutocompleteValue(base::StringPiece value) {
   return MatchesRegex<kRegex>(base::UTF8ToUTF16(value));
 }
 
-}  // namespace
-
+// Returns the Chrome Autofill-supported field type corresponding to a given
+// autocomplete `value`, if there is one, in the context of the given
+// `field`.
 HtmlFieldType FieldTypeFromAutocompleteAttributeValue(
     std::string value,
     const AutofillField& field) {
@@ -220,6 +220,8 @@ HtmlFieldType FieldTypeFromAutocompleteAttributeValue(
              ? HTML_TYPE_UNSPECIFIED
              : HTML_TYPE_UNRECOGNIZED;
 }
+
+}  // namespace
 
 absl::optional<AutocompleteParsingResult> ParseAutocompleteAttribute(
     const AutofillField& field) {
--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -154,8 +154,6 @@ extern const base::Feature kAutofillPageLanguageDetection;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillParseAsync;
 COMPONENT_EXPORT(AUTOFILL)
-extern const base::Feature kAutofillParseNameAsAutocompleteType;
-COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillParsingPatternProvider;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<std::string>
