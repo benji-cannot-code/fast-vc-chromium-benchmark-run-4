99525810bbf0f9d14c1b9b476787bcebdc838cc2
Norge Vizcay <vizcay@google.com>
2022-08-11 08:53:30 +0000

    [FastCheckout] Update CreditCard model resources.
    Bug: 1334642
    Change-Id: I5b8e7ec4d83b92b11da2cfd47b7d456304902dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822725
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033892}

--- chrome/browser/ui/android/fast_checkout/java/src/org/chromium/chrome/browser/ui/fast_checkout/data/FastCheckoutCreditCard.java
+++ chrome/browser/ui/android/fast_checkout/java/src/org/chromium/chrome/browser/ui/fast_checkout/data/FastCheckoutCreditCard.java
@@ -5,14 +5,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.fast_checkout.data;
 
+import android.content.Context;
+
 import org.chromium.base.annotations.CalledByNative;
+import org.chromium.chrome.browser.ui.fast_checkout.R;
 import org.chromium.components.autofill.VirtualCardEnrollmentState;
 import org.chromium.url.GURL;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A credit card, similar to the one used by the PersonalDataManager.
  */
 public class FastCheckoutCreditCard {
+    // Mappings from name: chrome/browser/ui/autofill/autofill_popup_controller_utils.cc
+    // Mappings to resource: chrome/browser/android/resource_id.h
+    private static final Map<String, Integer> sResourceMap = new HashMap<String, Integer>() {
+        {
+            put("americanExpressCC", R.drawable.amex_card);
+            put("dinersCC", R.drawable.diners_card);
+            put("discoverCC", R.drawable.discover_card);
+            put("eloCC", R.drawable.elo_card);
+            put("genericCC", R.drawable.ic_credit_card_black);
+            put("jcbCC", R.drawable.jcb_card);
+            put("masterCardCC", R.drawable.mc_card);
+            put("mirCC", R.drawable.mir_card);
+            put("troyCC", R.drawable.troy_card);
+            put("unionPayCC", R.drawable.unionpay_card);
+            put("visaCC", R.drawable.visa_card);
+            put("googlePay", R.drawable.google_pay);
+        }
+    };
     private final String mGUID;
     private final String mOrigin;
     private final boolean mIsLocal;
@@ -144,5 +168,20 @@ public class FastCheckoutCreditCard {
     @CalledByNative
     public String getProductDescription() {
         return mProductDescription;
+    }
+
+    public String getFormattedExpirationDate(Context context) {
+        return getMonth()
+                + context.getResources().getString(R.string.autofill_expiration_date_separator)
+                + getYear();
+    }
+
+    public int getIssuerIconDrawableId() {
+        String issuerIconDrawable = getIssuerIconString();
+        if (sResourceMap.containsKey(issuerIconDrawable)) {
+            return sResourceMap.get(issuerIconDrawable);
+        } else {
+            return R.drawable.ic_credit_card_black;
+        }
     }
 }
