05848513eb5560eceff698f821f9069425b9e63a
Maks Orlovich <morlovich@chromium.org>
2022-08-11 14:51:59 +0000

    [Code health] Avoid Value::GetAsDictionary (in /chrome/browser/media/webrtc)
    This method is deprecated, so port away from it, along with other nearby deprecated base::Value APIs that can be modernized at least somewhat locally.
    
    This CL was uploaded by git cl split.
    
    R=tommi@chromium.org
    
    Bug: 1187011
    Change-Id: If049d368d797ae76af3d9318bd8338d9ff6f2aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826112
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033987}

--- chrome/browser/media/webrtc/webrtc_browsertest_perf.h
+++ chrome/browser/media/webrtc/webrtc_browsertest_perf.h
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-namespace base {
-class DictionaryValue;
-}
+#include "base/values.h"
 
 namespace test {
 
@@ -27,16 +25,16 @@ namespace test {
 // the video_tx_oneway bucket.
 // If |video_codec| is a non-empty string, the codec name is appended last for
 // video metrics, e.g. 'video_tx_oneway_VP9'.
-void PrintBweForVideoMetrics(const base::DictionaryValue& pc_dict,
+void PrintBweForVideoMetrics(const base::Value::Dict& pc_dict,
                              const std::string& modifier,
                              const std::string& video_codec);
-void PrintMetricsForAllStreams(const base::DictionaryValue& pc_dict,
+void PrintMetricsForAllStreams(const base::Value::Dict& pc_dict,
                                const std::string& modifier,
                                const std::string& video_codec);
-void PrintMetricsForSendStreams(const base::DictionaryValue& pc_dict,
+void PrintMetricsForSendStreams(const base::Value::Dict& pc_dict,
                                 const std::string& modifier,
                                 const std::string& video_codec);
-void PrintMetricsForRecvStreams(const base::DictionaryValue& pc_dict,
+void PrintMetricsForRecvStreams(const base::Value::Dict& pc_dict,
                                 const std::string& modifier,
                                 const std::string& video_codec);
 
