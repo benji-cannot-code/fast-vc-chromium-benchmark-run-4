d23fd3bd654ecd1138ef8dc14bc1caa98dfd4368
kylechar <kylechar@chromium.org>
2022-08-11 18:57:58 +0000

    Add BGRX support to GLTextureImageBacking
    GLES2 doesn't support BGRX directly but it can be stored in GL_RGB
    internal format which still works. GLImageBacking with shared memory
    currently works with BGRX_8888 format so in order to remove it we need
    GLTextureImageBacking to also support it. Make viz::GLSupportsFormat()
    return true for BGRX_8888.
    
    Bug: 1293509
    Change-Id: I394e81e06c4851c9c626862d08780bd745d61407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824778
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034115}

--- components/viz/common/resources/resource_format_utils.cc
+++ components/viz/common/resources/resource_format_utils.cc
@@ -543,7 +543,6 @@ ResourceFormat GetResourceFormat(gfx::BufferFormat format) {
 bool GLSupportsFormat(ResourceFormat format) {
   switch (format) {
     case BGR_565:
-    case BGRX_8888:
     case YVU_420:
     case YUV_420_BIPLANAR:
     case P010:
