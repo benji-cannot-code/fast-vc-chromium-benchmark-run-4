0d778d2ea9e803bee85b21fef2257a49fd07cbb4
Nicolás Peña Moreno <npm@chromium.org>
2022-08-11 21:44:06 +0000

    [FedCM] Add a flag for multiple identity provider support
    The flag is used on the renderer side when performing checks against the
    get() call as well as on the browser side to perform the fetches and
    UI updates required to support multiple identity providers. This CL just
    adds a flag, and it will be used in followup patches.
    
    Bug: 1307709, 1351137
    Change-Id: I36b5108303506e449423324a7dc9d77543e5aa56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824458
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034186}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -220,6 +220,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableDocumentPolicy, features::kDocumentPolicy},
     {wf::EnableDocumentPolicyNegotiation, features::kDocumentPolicyNegotiation},
     {wf::EnableFedCm, features::kFedCm, kSetOnlyIfOverridden},
+    {wf::EnableFedCmMultipleIdentityProviders,
+     features::kFedCmMultipleIdentityProviders, kDefault},
     {wf::EnableFencedFrames, features::kPrivacySandboxAdsAPIsOverride,
      kSetOnlyIfOverridden},
     {wf::EnableSharedStorageAPI, features::kPrivacySandboxAdsAPIsOverride,
