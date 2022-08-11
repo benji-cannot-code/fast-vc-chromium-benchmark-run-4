9baa49174925e802216b4b7c527ac4f75d1dba6c
Kelvin Jiang <kelvinjiang@chromium.org>
2022-08-11 22:23:56 +0000

    [Extensions] Indicate installed-by-default extensions
    This CL removes the incorrect/misleading "added by third party" badge
    for extensions that are installed by default and adds a new type
    "INSTALLED_BY_DEFAULT" in developerPrivate.Location. While technically they may be added by a third party, this becomes misleading when they're
    added by default for a profile.
    
    Screenshots: https://imgur.com/a/VpRIyFk
    
    Bug: 859607
    Change-Id: I6c394ff9a6d2e30505847887da714c323843653c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813957
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034206}

--- chrome/browser/resources/extensions/item_util.ts
+++ chrome/browser/resources/extensions/item_util.ts
@@ -13,6 +13,7 @@ export enum SourceType {
   POLICY = 'policy',
   SIDELOADED = 'sideloaded',
   UNPACKED = 'unpacked',
+  INSTALLED_BY_DEFAULT = 'installed-by-default',
   UNKNOWN = 'unknown',
 }
 
@@ -94,6 +95,8 @@ export function getItemSource(item: chrome.developerPrivate.ExtensionInfo):
       return SourceType.UNKNOWN;
     case chrome.developerPrivate.Location.FROM_STORE:
       return SourceType.WEBSTORE;
+    case chrome.developerPrivate.Location.INSTALLED_BY_DEFAULT:
+      return SourceType.INSTALLED_BY_DEFAULT;
     default:
       assertNotReached(item.location);
   }
@@ -109,6 +112,8 @@ export function getItemSourceString(source: SourceType): string {
       return loadTimeData.getString('itemSourceUnpacked');
     case SourceType.WEBSTORE:
       return loadTimeData.getString('itemSourceWebstore');
+    case SourceType.INSTALLED_BY_DEFAULT:
+      return loadTimeData.getString('itemSourceInstalledByDefault');
     case SourceType.UNKNOWN:
       // Nothing to return. Calling code should use
       // chrome.developerPrivate.ExtensionInfo's |locationText| instead.
