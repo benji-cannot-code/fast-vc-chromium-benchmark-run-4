4c3bb3fc1c2468b30ae77d3c66ebca5d3de1d28c
Sunny Sachanandani <sunnyps@chromium.org>
2022-08-11 13:46:30 +0000

    Revert "cc/paint: Use std::unique_ptr to wrap paths in PaintOps"
    This reverts commit 85aae9fe2967ba3005d91de85878fe4e9cc87bb8.
    
    Reason for revert: ~3.3% motionmark regression - see crbug.com/1347691
    
    Original change's description:
    > cc/paint: Use std::unique_ptr to wrap paths in PaintOps
    >
    > We want to statically assert that PaintOps are trivially relocatable so
    > that the PaintOpBuffer memcpy on realloc optimization is safe. One way
    > of doing that is to mark PaintOps with clang's trivial_abi attribute
    > which only works if the types contained are also trivially relocatable.
    >
    > ThreadsafePath is a thin subclass of SkPath contained in ClipPathOp and
    > DrawPathOp. Since SkPath contains std::atomics which lack copy ctors, it
    > cannot be simply marked as trivial_abi so the next best thing is to wrap
    > paths in a std::unique_ptr which is what this CL does. Also add move
    > constructors since these will be required when the types are marked as
    > trivial_abi in a subsequent CL.
    >
    > Bug: 1302745
    > Change-Id: I4879a6ade6620bea344f61cbf046e4a9e6b6a9c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787235
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1028444}
    
    Bug: 1302745, 1347691
    Change-Id: I2e9ab91ab687bb725140130f2086a32f8c24ba19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825577
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033961}

--- cc/test/paint_op_helper.h
+++ cc/test/paint_op_helper.h
@@ -36,7 +36,7 @@ class PaintOpHelper {
       }
       case PaintOpType::ClipPath: {
         const auto* op = static_cast<const ClipPathOp*>(base_op);
-        str << "ClipPathOp(path=" << PaintOpHelper::SkiaTypeToString(*op->path)
+        str << "ClipPathOp(path=" << PaintOpHelper::SkiaTypeToString(op->path)
             << ", op=" << PaintOpHelper::SkiaTypeToString(op->op)
             << ", antialias=" << op->antialias
             << ", use_cache=" << (op->use_cache == UsePaintCache::kEnabled)
@@ -126,7 +126,7 @@ class PaintOpHelper {
       }
       case PaintOpType::DrawPath: {
         const auto* op = static_cast<const DrawPathOp*>(base_op);
-        str << "DrawPathOp(path=" << PaintOpHelper::SkiaTypeToString(*op->path)
+        str << "DrawPathOp(path=" << PaintOpHelper::SkiaTypeToString(op->path)
             << ", flags=" << PaintOpHelper::FlagsToString(op->flags)
             << ", use_cache=" << (op->use_cache == UsePaintCache::kEnabled)
             << ")";
@@ -357,6 +357,10 @@ class PaintOpHelper {
 
   static std::string SkiaTypeToString(const sk_sp<SkData> data) {
     return data ? "<SkData>" : "(nil)";
+  }
+
+  static std::string SkiaTypeToString(const ThreadsafePath& path) {
+    return SkiaTypeToString(static_cast<const SkPath&>(path));
   }
 
   static std::string SkiaTypeToString(const SkPath& path) {
