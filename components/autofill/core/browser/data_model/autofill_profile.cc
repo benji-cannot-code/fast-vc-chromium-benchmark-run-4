c2bd6c27fad180b051e0a5fa8b53879a8815782b
Qihui Zhao <qihuizhao@google.com>
2022-08-11 00:16:19 +0000

    [IBAN Filling] Add GetIdentifierStringForAutofillDisplay for IBAN.
    GetIdentifierStringForAutofillDisplay will create masked name of IBAN value (e.g., DE** **** **** **** **** 89) and it will be shown in suggestions for user.
    
    Bug: 1344590
    Change-Id: I8636779f48599a56af7d4a169f00fcc0c47eb5f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3800402
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033788}

--- components/autofill/core/browser/data_model/autofill_profile.cc
+++ components/autofill/core/browser/data_model/autofill_profile.cc
@@ -287,7 +287,7 @@ AutofillMetadata AutofillProfile::GetMetadata() const {
   return metadata;
 }
 
-bool AutofillProfile::SetMetadata(const AutofillMetadata metadata) {
+bool AutofillProfile::SetMetadata(const AutofillMetadata& metadata) {
   // Make sure the ids matches.
   if (metadata.id != (record_type_ == LOCAL_PROFILE ? guid() : server_id_))
     return false;
