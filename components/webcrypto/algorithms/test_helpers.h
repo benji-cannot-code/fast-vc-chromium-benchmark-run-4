cbdf90afaba365514c624a763a3f083d7cf2355f
Maks Orlovich <morlovich@chromium.org>
2022-08-11 16:33:21 +0000

    [Code health] Avoid Value::GetAsDictionary (in /components/webcrypto)
    This method is deprecated, so port away from it, along with other nearby deprecated base::Value APIs that can be modernized at least somewhat locally.
    
    This CL was uploaded by git cl split.
    
    R=davidben@chromium.org
    
    Bug: 1187011
    Change-Id: Ifd95ec80fd62dbda1030624176ef79f7866840b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826008
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034027}

--- components/webcrypto/algorithms/test_helpers.h
+++ components/webcrypto/algorithms/test_helpers.h
@@ -28,11 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXPECT_BYTES_EQ_HEX(expected_hex, actual_bytes) \
   EXPECT_BYTES_EQ(HexStringToBytes(expected_hex), actual_bytes)
 
-namespace base {
-class DictionaryValue;
-class Value;
-}
-
 namespace blink {
 class WebCryptoAlgorithm;
 }
@@ -150,13 +145,13 @@ Status ImportKeyJwkFromDict(const base::DictionaryValue& dict,
                             blink::WebCryptoKey* key);
 
 // Parses a vector of JSON into a dictionary.
-absl::optional<base::DictionaryValue> GetJwkDictionary(
+absl::optional<base::Value::Dict> GetJwkDictionary(
     const std::vector<uint8_t>& json);
 
 // Verifies the input dictionary contains the expected values. Exact matches are
 // required on the fields examined.
 ::testing::AssertionResult VerifyJwk(
-    const std::unique_ptr<base::DictionaryValue>& dict,
+    const base::Value::Dict& dict,
     const std::string& kty_expected,
     const std::string& alg_expected,
     blink::WebCryptoKeyUsageMask use_mask_expected);
