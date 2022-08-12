86c7d5f686ae446b0cf5d9c91d55d307938e5c5d
Danan S <danan@chromium.org>
2022-08-12 17:28:49 +0000

    Fix broken parent access flow
    The flow broke for multiple reasons:
    - The ParentAccessCallback protocol buffer changed (fields added) and
    we were too strict in interpreting its contents.
    - The app wasn't expecting multiple ParentAccessCallbacks and the new
    unrecognized callback caused it to exit before the
    ParentVerified message was received.
    
    In addition, this CL changes the parsed callback value returned by the
    MojoWebUIHandler to be more future proof by return a struct instead
    of only an enum.
    
    Bug: b:240294909
    Change-Id: I16e013f4d2e69348da90f43d9e96717fa5e667c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807043
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034534}

--- chrome/test/data/webui/chromeos/parent_access/parent_access_browsertest.js
+++ chrome/test/data/webui/chromeos/parent_access/parent_access_browsertest.js
@@ -46,10 +46,12 @@ var ParentAccessControllerTest = class extends testing.Test {
   }
 };
 
-TEST_F('ParentAccessControllerTest', 'ParentAccessResultFnCalled', function() {
-  this.runMochaTest(
-      parent_access_controller_tests.TestNames.ParentAccessResultFnCalled);
-});
+TEST_F(
+    'ParentAccessControllerTest', 'ParentAccessCallbackReceivedFnCalled',
+    function() {
+      this.runMochaTest(parent_access_controller_tests.TestNames
+                            .ParentAccessCallbackReceivedFnCalled);
+    });
 
 var ParentAccessUITest = class extends PolymerTest {
   /** @override */
@@ -102,7 +104,9 @@ var ParentAccessUIHandlerTest = class extends testing.Test {
   }
 };
 
-TEST_F('ParentAccessUIHandlerTest', 'TestOnParentAccessResult', function() {
-  this.runMochaTest(
-      parent_access_ui_handler_tests.TestNames.TestOnParentAccessResult);
-});
+TEST_F(
+    'ParentAccessUIHandlerTest', 'TestOnParentAccessCallbackReceived',
+    function() {
+      this.runMochaTest(parent_access_ui_handler_tests.TestNames
+                            .TestOnParentAccessCallbackReceived);
+    });
