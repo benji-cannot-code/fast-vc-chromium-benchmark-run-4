c16e3b5fc182dde6e14b529d96736a7a9553409c
Hamda <hmare@google.com>
2022-08-12 18:56:57 +0000

    [MacDT] Add support for a Mac key rotation command
    This adds the  mac key rotation command needed to kick off the key
    rotation on mac platforms. Also added a small change to the secure client's ExportPublicKey method due to invalid public key server
    failures. Details of the entire key rotation flow can be found here: http://go/mac-key-rotation-dd.
    
    Screencast mac key creation and rotation:
    https://screencast.googleplex.com/cast/NDgyNDc3MzkzMTU2NTA1NnxkZGQ4MzAyMi1lNg
    
    Screencast linux key creation and rotation:
    https://screencast.googleplex.com/cast/NDcwMzM4NDEzMDAyNzUyMHxhMTRhZDcwOC0wYw
    
    Bug: b/235364370
    Change-Id: I00aaff0e23264a2472af552641ca9096af041cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806969
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034598}

--- chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/key_rotation_command_factory.cc
+++ chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/key_rotation_command_factory.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/win_key_rotation_command.h"
 #elif BUILDFLAG(IS_LINUX)
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/linux_key_rotation_command.h"
+#elif BUILDFLAG(IS_MAC)
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/mac_key_rotation_command.h"
 #endif  // BUILDFLAG(IS_WIN)
 
 namespace enterprise_connectors {
@@ -45,8 +47,9 @@ std::unique_ptr<KeyRotationCommand> KeyRotationCommandFactory::CreateCommand(
 #if BUILDFLAG(IS_WIN)
   return std::make_unique<WinKeyRotationCommand>();
 #elif BUILDFLAG(IS_LINUX)
-  return std::make_unique<LinuxKeyRotationCommand>(
-      std::move(url_loader_factory));
+  return std::make_unique<LinuxKeyRotationCommand>(url_loader_factory);
+#elif BUILDFLAG(IS_MAC)
+  return std::make_unique<MacKeyRotationCommand>(url_loader_factory);
 #else
   return nullptr;
 #endif  // BUILDFLAG(IS_WIN)
