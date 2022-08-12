f076bc103ec14c52a855eede6239b6a1c55f27e8
Frank Liberato <liberato@chromium.org>
2022-08-12 19:16:03 +0000

    Revert "Updater: Change test configurations a bit, re-enable some tests."
    This reverts commit c5cdb04548f431e2dc87e7c92cb999a8964ce090.
    
    Reason for revert: SelfUpdateFromOldReal started flaking again.
    See https://ci.chromium.org/ui/p/chromium/builders/ci/Win%207%20Tests%20x64%20(1)/93416/overview
    
    Original change's description:
    > Updater: Change test configurations a bit, re-enable some tests.
    >
    > IntegrationTest.SelfUpdateFromOldReal has not flaked in the past month,
    > according to Flake Portal, so re-enabling. Sheriffs: I'm not confident
    > that flake portal is correctly picking up flakes. If you see the test
    > failing on CI bots, and this CL is less than a week old, please revert
    > and link us to the failure.
    >
    > This CL also disables most of the integration tests on component builds.
    > (Previously, they would run for Windows component builds.) Although they
    > technically pass on Windows, they rarely run well or under default flags
    > because the component builds are significantly larger and take much more
    > time to uncompress, install, etc. The overall position of the Chromium
    > project is that component builds are not portable outside of the build
    > directory, and so even if they work by accident, let's not commit to it.
    >
    > The CL also re-enables the stress test. This test can likely be
    > re-enabled without causing problems after
    > https://chromium-review.googlesource.com/c/chromium/src/+/3712502.
    >
    > Thank you!
    >
    > Bug: 1341471
    > Change-Id: I2d561036cbc3f736af77a739d244dc38f3bb1741
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828081
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1034458}
    
    Bug: 1341471
    Change-Id: Ic8c0f149f3ff09a108c7fd73ee7c6c5f28630879
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829347
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Owners-Override: Mohsen Izadi <mohsen@chromium.org>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034616}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -55,7 +55,7 @@ namespace updater {
 namespace test {
 namespace {
 
-#if !defined(COMPONENT_BUILD)
+#if BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 void ExpectNoUpdateSequence(ScopedServer* test_server,
                             const std::string& app_id) {
@@ -79,7 +79,7 @@ void ExpectNoUpdateSequence(ScopedServer* test_server,
                          app_id.c_str()));
 }
 
-#endif  // !defined(COMPONENT_BUILD)
+#endif  // BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 }  // namespace
 
@@ -329,7 +329,7 @@ class IntegrationTest : public ::testing::Test {
 // the build directory. Therefore, installation of component builds is not
 // expected to work and these tests do not run on component builders.
 // See crbug.com/1112527.
-#if !defined(COMPONENT_BUILD)
+#if BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 TEST_F(IntegrationTest, InstallUninstall) {
   Install();
@@ -713,7 +713,14 @@ TEST_F(IntegrationTest, UnregisterUnownedApp) {
 #endif  // BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(CHROMIUM_BRANDING) || BUILDFLAG(GOOGLE_CHROME_BRANDING)
-TEST_F(IntegrationTest, SelfUpdateFromOldReal) {
+#if !defined(COMPONENT_BUILD)
+// Disabled on Windows due to high flake rate; see https://crbug.com/1341471.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_SelfUpdateFromOldReal DISABLED_SelfUpdateFromOldReal
+#else
+#define MAYBE_SelfUpdateFromOldReal SelfUpdateFromOldReal
+#endif
+TEST_F(IntegrationTest, MAYBE_SelfUpdateFromOldReal) {
   ScopedServer test_server(test_commands_);
 
   SetupRealUpdaterLowerVersion();
@@ -737,8 +744,16 @@ TEST_F(IntegrationTest, SelfUpdateFromOldReal) {
   Uninstall();
 }
 #endif
+#endif
 
-TEST_F(IntegrationTest, UpdateServiceStress) {
+// TODO(crbug.com/1336591) - enable test after investigating crbug.com/1336591
+// or open a new crbug for debugging this test if it is the culprit.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_UpdateServiceStress DISABLED_UpdateServiceStress
+#else
+#define MAYBE_UpdateServiceStress UpdateServiceStress
+#endif
+TEST_F(IntegrationTest, MAYBE_UpdateServiceStress) {
   Install();
   ExpectInstalled();
   StressUpdateService();
@@ -851,7 +866,7 @@ TEST_F(IntegrationTest, OfflineInstall) {
   Uninstall();
 }
 
-#endif  // !defined(COMPONENT_BUILD)
+#endif  // BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 }  // namespace test
 }  // namespace updater
