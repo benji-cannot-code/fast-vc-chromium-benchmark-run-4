1df293b5c8e624fe4f6bf37ce0d92ce51d07ff59
Khushal Sagar <khushalsagar@chromium.org>
2022-08-12 00:24:27 +0000

    blink: Fix measurement for replaced elements painted with overflow.
    The current code made an incorrect measurement that VisualOverflow is
    only tracked when an element paints outside its border box. But this
    value is tracked if overflow is set to any value other than visible [1].
    
    Update the code to fix the incorrect assumption.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/layout/layout_box.cc;l=7038;drc=58fd901f44beaee7fda15a2c493d4cd0ffd0c65e
    
    R=vmpstr@chromium.org
    
    Bug: 1350363
    Change-Id: I5fb17b7426a211f590f2ec86fb89ebe2e4da008d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828144
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034257}

--- third_party/blink/renderer/core/paint/replaced_painter.h
+++ third_party/blink/renderer/core/paint/replaced_painter.h
@@ -28,6 +28,7 @@ class ReplacedPainter {
 
  private:
   bool ShouldPaintBoxDecorationBackground(const PaintInfo&);
+  void MeasureOverflowMetrics() const;
 
   const LayoutReplaced& layout_replaced_;
 };
