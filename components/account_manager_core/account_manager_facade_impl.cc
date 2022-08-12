c40c932a34afba4f8ae7817f91e6197fe9fb3385
Kush Sinha <sinhak@chromium.org>
2022-08-12 12:37:49 +0000

    Lacros: Add account manager Mojo methods to report errors
    Add methods in `AccountManager` and `AccountManagerObserver` Mojo
    interfaces to implement a pubsub model of OAuth error propagation. Any
    application can report errors to `AccountManager` - which will then be
    fanned out to all `AccountManagerObserver`s.
    
    This will be used to share OAuth error states between Ash and Lacros.
    Currently both of them are oblivious to the account errors discovered by
    the other.
    
    This CL adds stub methods. Follow up CLs will add the actual
    implementation.
    
    Design Doc: http://shortn/_hKoc0VqbIt
    
    Bug: 1226055, 1312897
    Change-Id: I661c08b2e7a09ca6f8b0c42765067cd894036b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825905
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034431}

--- components/account_manager_core/account_manager_facade_impl.cc
+++ components/account_manager_core/account_manager_facade_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/crosapi/mojom/account_manager.mojom.h"
 #include "components/account_manager_core/account.h"
@@ -547,6 +548,12 @@ void AccountManagerFacadeImpl::OnAccountRemoved(
   for (auto& observer : observer_list_) {
     observer.OnAccountRemoved(maybe_account.value());
   }
+}
+
+void AccountManagerFacadeImpl::OnAuthErrorChanged(
+    crosapi::mojom::AccountKeyPtr account,
+    crosapi::mojom::GoogleServiceAuthErrorPtr error) {
+  NOTIMPLEMENTED();
 }
 
 void AccountManagerFacadeImpl::GetAccountsInternal(
--- components/account_manager_core/account_manager_facade_impl_unittest.cc
+++ components/account_manager_core/account_manager_facade_impl_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback_helpers.h"
+#include "base/notreached.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
@@ -201,6 +202,12 @@ class FakeAccountManager : public crosapi::mojom::AccountManager {
     access_token_fetcher_->Bind(
         pending_remote.InitWithNewPipeAndPassReceiver());
     std::move(callback).Run(std::move(pending_remote));
+  }
+
+  void ReportAuthError(
+      crosapi::mojom::AccountKeyPtr account,
+      crosapi::mojom::GoogleServiceAuthErrorPtr error) override {
+    NOTIMPLEMENTED();
   }
 
   mojo::Remote<crosapi::mojom::AccountManager> CreateRemote() {
