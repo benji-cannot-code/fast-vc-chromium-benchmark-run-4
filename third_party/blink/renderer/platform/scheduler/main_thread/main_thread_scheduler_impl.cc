8eca3bff08b01850c8c800d17d21b05325f0ce21
Scott Haseley <shaseley@chromium.org>
2022-08-12 19:11:04 +0000

    [scheduler] Don't filter on input event result for prioritization
    For prioritize-rendering-after-input, we were ignoring input events
    that weren't consumed, meaning either preventDefault() was called or
    the default action was taken. But this filters out potential cases
    where the UI is updated and there is not default action, e.g. clicking
    on a div and updating DOM in response. This CL removes that filtering
    to cover more cases. In the event the input did not cause a frame, we
    we would keep the compositor TQ prioritized until the next frame,
    whenever that is, but this seems unlikely to be problematic or cause
    significant overhead (we already keep the TQ prioritized after a delay
    without issue).
    
    Bug: 853771
    Change-Id: I8a1482fad270cd262cfb5718d1dd0eeae269672a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824360
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034611}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -1363,9 +1363,7 @@ void MainThreadSchedulerImpl::DidHandleInputEventOnMainThread(
       UpdatePolicyLocked(UpdateType::kMayEarlyOutIfPolicyUnchanged);
     }
   }
-  if (result != WebInputEventResult::kNotHandled &&
-      result != WebInputEventResult::kHandledSuppressed &&
-      !PendingUserInput::IsContinuousEventType(web_input_event.GetType())) {
+  if (!PendingUserInput::IsContinuousEventType(web_input_event.GetType())) {
     main_thread_only().did_handle_discrete_input_event = true;
   }
 }
