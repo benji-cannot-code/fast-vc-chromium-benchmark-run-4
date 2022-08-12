3600c22a080d44a9091114c3526c28135e2c0dc2
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2022-08-12 20:46:59 +0000

    [ViewsAX] Move ScopedObservation to AXTreeManager
    In this CL, we make the AXTreeManager registers itself as an
    AXTreeObserver on its AXTree. We use base::ScopedObservation so that
    it removes itself from the tree's observers list automatically at
    destruction.
    
    Bug: 1049261
    Change-Id: Ic30eff5373fa50689a19db9372f7e91b0addc2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817182
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034682}

--- content/browser/accessibility/browser_accessibility_manager.h
+++ content/browser/accessibility/browser_accessibility_manager.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
-#include "base/scoped_observation.h"
 #include "build/build_config.h"
 #include "cc/base/rtree.h"
 #include "content/browser/accessibility/browser_accessibility.h"
@@ -494,8 +493,6 @@ class CONTENT_EXPORT BrowserAccessibilityManager
   ui::AXPlatformNode* GetPlatformNodeFromTree(
       const ui::AXNodeID node_id) const override;
   ui::AXPlatformNode* GetPlatformNodeFromTree(const ui::AXNode&) const override;
-  void AddObserver(ui::AXTreeObserver* observer) override;
-  void RemoveObserver(ui::AXTreeObserver* observer) override;
   ui::AXTreeID GetTreeID() const override;
   ui::AXTreeID GetParentTreeID() const override;
   ui::AXNode* GetRootAsAXNode() const override;
@@ -712,15 +709,6 @@ class CONTENT_EXPORT BrowserAccessibilityManager
   // Only used on the root node for AXTree hit testing as an alternative to
   // ApproximateHitTest when used without a renderer.
   std::unique_ptr<cc::RTree<ui::AXNodeID>> cached_node_rtree_;
-
-  // Automatically stops observing notifications from the AXTree when this class
-  // is destructed.
-  //
-  // This member needs to be destructed before any observed AXTrees. Since
-  // destructors for non-static member fields are called in the reverse order of
-  // declaration, do not move this member above other members.
-  base::ScopedObservation<ui::AXTree, ui::AXTreeObserver> tree_observation_{
-      this};
 };
 
 }  // namespace content
--- ui/accessibility/ax_tree_manager.h
+++ ui/accessibility/ax_tree_manager.h
@@ -42,11 +42,6 @@ class AX_EXPORT AXTreeManager : public AXTreeObserver {
   // Returns nullptr if |node_id| is not found.
   virtual AXNode* GetNodeFromTree(const AXNodeID node_id) const = 0;
 
-  // Use `AddObserver` and `RemoveObserver` when you want to be notified when
-  // changes happen to an `XTree`
-  virtual void AddObserver(AXTreeObserver* observer) {}
-  virtual void RemoveObserver(AXTreeObserver* observer) {}
-
   // Returns the tree id of the tree managed by this AXTreeManager.
   virtual AXTreeID GetTreeID() const = 0;
 
@@ -111,6 +106,14 @@ class AX_EXPORT AXTreeManager : public AXTreeObserver {
   friend class TestAXTreeManager;
 
   static AXTreeManagerMap& GetMap();
+
+  // Automatically stops observing notifications from the AXTree when this class
+  // is destructed.
+  //
+  // This member needs to be destructed before any observed AXTrees. Since
+  // destructors for non-static member fields are called in the reverse order of
+  // declaration, do not move this member above other members.
+  base::ScopedObservation<AXTree, AXTreeObserver> tree_observation_{this};
 };
 
 }  // namespace ui
