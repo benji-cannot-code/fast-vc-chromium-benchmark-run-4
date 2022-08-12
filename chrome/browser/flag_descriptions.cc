2b7feeecadbc52d9cc345fbf462ce7459b31a83c
Steffen Seckler <sseckler@google.com>
2022-08-12 07:26:00 +0000

    Add FileTransferEnterpriseConnector feature flag
    Bug: 1339818
    Change-Id: Icee96add4a28a3b9de4da7b577f3f76cb5f31fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825942
    Auto-Submit: Steffen Seckler <sseckler@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Steffen Seckler <sseckler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034368}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -5231,6 +5231,11 @@ const char kDockedMagnifierResizingDescription[] =
     "Enable feature which adds ability for user to grab and resize divider of "
     "Docked Magnifier.";
 
+const char kFileTransferEnterpriseConnectorName[] =
+    "Enable Files Transfer Enterprise Connector.";
+const char kFileTransferEnterpriseConnectorDescription[] =
+    "Enable the File Transfer Enterprise Connector.";
+
 const char kFilesAppExperimentalName[] =
     "Experimental UI features for Files app";
 const char kFilesAppExperimentalDescription[] =
