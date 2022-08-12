073ba75d37b718049df14cc15b04b8f435723423
Scott Violet <sky@chromium.org>
2022-08-12 18:39:29 +0000

    blink: introduces enum for html tag names
    This is a patch in a series of patches to make the parsing code
    use enums for html tags rather than strings.
    
    This patch modifies the code generator to generate an enum for HTML
    tags. It also modifies the lookup code to return the enum, as well as
    a function to convert from the enum to the QualifiedName. The one
    place using the conversion function is updated.
    
    The semantics of the new function are to set an argument supplied
    to the lookup function, and then return true. This necessitates
    updating the existing signatures to explicitly return as the base
    template can no longer have the return.
    
    BUG=1351151
    TEST=covered by tests
    
    Change-Id: I0e31dabeb7ea6bd7ca50586d10e01354ba47dbed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817787
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034581}

--- third_party/blink/renderer/core/html/parser/atomic_html_token.h
+++ third_party/blink/renderer/core/html/parser/atomic_html_token.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/attribute.h"
 #include "third_party/blink/renderer/core/html/parser/html_token.h"
 #include "third_party/blink/renderer/core/html_element_lookup_trie.h"
+#include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h"
@@ -129,11 +130,13 @@ class CORE_EXPORT AtomicHTMLToken {
       case HTMLToken::kStartTag:
       case HTMLToken::kEndTag: {
         self_closing_ = token.SelfClosing();
-        if (const AtomicString& tag_name =
-                lookupHTMLTag(token.GetName().data(), token.GetName().size()))
-          name_ = tag_name;
-        else
+        const html_names::HTMLTag html_tag =
+            lookupHTMLTag(token.GetName().data(), token.GetName().size());
+        if (html_tag != html_names::HTMLTag::kUnknown) {
+          name_ = html_names::TagToQualifedName(html_tag).LocalName();
+        } else {
           name_ = token.GetName().AsAtomicString();
+        }
         const HTMLToken::AttributeList& attributes = token.Attributes();
 
         // This limit is set fairly arbitrarily; the main point is to avoid
