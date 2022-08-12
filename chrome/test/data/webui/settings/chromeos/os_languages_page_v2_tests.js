bb5cfba4b07c605279a1e6d08bf4653e97042f26
mlcui <mlcui@google.com>
2022-08-12 02:00:14 +0000

    OsSettingsLanguages: Always move device language to top of web languages
    We've had reports that users are confused that websites are displaying
    in a different language to their device language after switching device
    languages. Users can still move the language down the web languages list
    after the restart.
    
    This also works around a related issue in the Personalization app - see
    http://shortn/_svBSKNgEhY for more details.
    
    
    Bug: 1329947, b/237708882
    Fixed: 1330209
    Change-Id: I73dab482316f78c0711f6c235cec644e7eb8f5c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687011
    Auto-Submit: Michael Cui <mlcui@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034298}

--- chrome/test/data/webui/settings/chromeos/os_languages_page_v2_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_languages_page_v2_tests.js
@@ -519,7 +519,7 @@ suite('languages page', () => {
         });
 
     test(
-        'setting device language does not move already enabled language to front',
+        'setting device language moves already enabled language to front',
         async () => {
           languageHelper.setPrefValue(
               'intl.accept_languages', 'en-US,sw,en-CA');
@@ -535,8 +535,8 @@ suite('languages page', () => {
           assertEquals(
               'en-CA',
               await browserProxy.whenCalled('setProspectiveUILanguage'));
-          assertFalse(languageHelper.getPref('intl.accept_languages')
-                          .value.startsWith('en-CA'));
+          assertTrue(languageHelper.getPref('intl.accept_languages')
+                         .value.startsWith('en-CA'));
         });
 
     // Test that searching languages works whether the displayed or native
