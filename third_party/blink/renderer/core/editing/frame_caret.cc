e0fc5bae8cb117e23e86c899acfce8fa3facf32a
Philip Rogers <pdr@chromium.org>
2022-08-12 18:58:04 +0000

    Remove CompositedCaret feature flag
    This has fully launched.
    
    Bug: 1123630
    Change-Id: If876a224fb1cc7ec3afe0ecfc5cec579f622f2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829487
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034599}

--- third_party/blink/renderer/core/editing/frame_caret.cc
+++ third_party/blink/renderer/core/editing/frame_caret.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/frame_caret.h"
 
 #include "base/location.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/editing/caret_display_item_client.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
@@ -38,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/html/forms/text_control_element.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
@@ -62,8 +60,6 @@ FrameCaret::FrameCaret(LocalFrame& frame,
       caret_blink_timer_(frame.GetTaskRunner(TaskType::kInternalDefault),
                          this,
                          &FrameCaret::CaretBlinkTimerFired),
-      is_composited_caret_enabled_(
-          base::FeatureList::IsEnabled(features::kCompositedCaret)),
       effect_(EffectPaintPropertyNode::Create(
           EffectPaintPropertyNode::Root(),
           CaretEffectNodeState(/*visible*/ true,
@@ -96,10 +92,7 @@ EffectPaintPropertyNode::State FrameCaret::CaretEffectNodeState(
       (CompositorElementIdFromUniqueObjectId(
           NewUniqueObjectId(), CompositorElementIdNamespace::kPrimaryEffect)));
   state.compositor_element_id = element_id;
-  if (is_composited_caret_enabled_) {
-    state.direct_compositing_reasons =
-        CompositingReason::kActiveOpacityAnimation;
-  }
+  state.direct_compositing_reasons = CompositingReason::kActiveOpacityAnimation;
   return state;
 }
 
@@ -217,13 +210,11 @@ void FrameCaret::SetVisibleIfActive(bool visible) {
   auto change_type = effect_->Update(
       *effect_->Parent(),
       CaretEffectNodeState(visible, effect_->LocalTransformSpace()));
-  if (is_composited_caret_enabled_) {
-    DCHECK_EQ(PaintPropertyChangeType::kChangedOnlySimpleValues, change_type);
-    if (auto* compositor = frame_->View()->GetPaintArtifactCompositor()) {
-      if (compositor->DirectlyUpdateCompositedOpacityValue(*effect_)) {
-        effect_->CompositorSimpleValuesUpdated();
-        return;
-      }
+  DCHECK_EQ(PaintPropertyChangeType::kChangedOnlySimpleValues, change_type);
+  if (auto* compositor = frame_->View()->GetPaintArtifactCompositor()) {
+    if (compositor->DirectlyUpdateCompositedOpacityValue(*effect_)) {
+      effect_->CompositorSimpleValuesUpdated();
+      return;
     }
   }
   // Fallback to full update if direct update is not available.
