5c47f486d78950adea5b7aa73db39dacbbc4ff05
Khalid Peer <khalidpeer@chromium.org>
2022-08-12 23:18:08 +0000

    [ZPS] Add feature flag to toggle Zero-Prefix Suggest prefetching on Web
    In order to pave the way for enabling ZPS prefetching on the Web
    (non-NTP and non-SRP URLs), this CL adds a new feature flag which
    is intended to enable/disable ZPS prefetch logic on the Web. Please
    refer to the linked bug for more context regarding this change.
    
    Bug: 1344004
    Change-Id: I6a05c2fd668a8a605f3d9280cadab60bbd65a84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827170
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034741}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1146,6 +1146,9 @@ extern const char kOmniboxZeroSuggestPrefetchingDescription[];
 extern const char kOmniboxZeroSuggestPrefetchingOnSRPName[];
 extern const char kOmniboxZeroSuggestPrefetchingOnSRPDescription[];
 
+extern const char kOmniboxZeroSuggestPrefetchingOnWebName[];
+extern const char kOmniboxZeroSuggestPrefetchingOnWebDescription[];
+
 extern const char kOmniboxMaxZeroSuggestMatchesName[];
 extern const char kOmniboxMaxZeroSuggestMatchesDescription[];
 
