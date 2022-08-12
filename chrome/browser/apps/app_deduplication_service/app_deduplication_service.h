7092de1576231b3ecdecebd6025b2bddd92f478f
Maggie Cai <mxcai@google.com>
2022-08-12 08:57:33 +0000

    [Deduplication] Test with binary proto data.
    This CL adds a binary proto data for testing with some real apps.
    
    BUG=b/238394602
    
    Change-Id: I604958b498e6231790ab207104f04abaa6f40c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817428
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034395}

--- chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
+++ chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
@@ -35,6 +35,7 @@ class AppDeduplicationService : public KeyedService,
   friend class AppDeduplicationServiceTest;
   FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest,
                            OnDuplicatedAppsMapUpdated);
+  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest, ExactDuplicate);
 
   // AppProvisioningDataManager::Observer:
   void OnDuplicatedAppsMapUpdated(
---
+++ chrome/test/data/app_deduplication_service/binary_test_data.pb
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+^
+SkypeU
+
+com.skype.raiderJphonehub
+
+com.skype.raiderJarc
+
+http://web.skype.com/Jweb
+\
+WhatsAppP
+
+com.whatsappJphonehub
+
+com.whatsappJarc
+
+http://web.whatsapp.com/Jweb
