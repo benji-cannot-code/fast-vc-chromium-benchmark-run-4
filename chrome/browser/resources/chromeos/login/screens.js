bf3808bce3a590b203015898368b3f1b86162305
Renato Silva <rrsilva@google.com>
2022-08-12 18:23:59 +0000

    OOBE: Polymer3 - Check-in arc-tos
    Check-in the modularised version of Arc Terms of Service screen.
    
    Bug: 1184731
    Change-Id: Ia899af00e35f9348a1723b7d41ec333af34eb3ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813933
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034572}

--- chrome/browser/resources/chromeos/login/screens.js
+++ chrome/browser/resources/chromeos/login/screens.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './screens/common/adb_sideloading.js';
 import './screens/common/app_downloading.m.js';
 import './screens/common/app_launch_splash.m.js';
-import './screens/common/arc_terms_of_service.m.js';
+import './screens/common/arc_terms_of_service.js';
 import './screens/common/assistant_optin.m.js';
 import './screens/common/autolaunch.m.js';
 import './screens/common/consolidated_consent.m.js';
