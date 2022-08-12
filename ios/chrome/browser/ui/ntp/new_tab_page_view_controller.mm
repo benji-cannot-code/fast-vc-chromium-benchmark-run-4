3ee1f70c6179d6796d09e75cbd07aea23197b823
adamta <adamta@google.com>
2022-08-12 17:43:40 +0000

    [iOS] Update feed insets when view did appear
    Scrolling into the feed, navigating to an article and coming back broke
    because the insets were set before setting the sticky header.
    
    The sticky header is adjusted in viewDidAppear, and the insets should
    only be set after that.
    
    Bug: 1351240
    Change-Id: I927db621624148338ca1109b804399e179f0d68a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826161
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034543}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -166,10 +166,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   [self updateNTPLayout];
-
-  if (self.isFeedVisible) {
-    [self updateFeedInsetsForMinimumHeight];
-  }
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -184,7 +180,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.shouldFocusFakebox = NO;
   }
 
-  if (!self.isFeedVisible) {
+  if (self.isFeedVisible) {
+    [self updateFeedInsetsForMinimumHeight];
+  } else {
     [self setMinimumHeight];
   }
 
