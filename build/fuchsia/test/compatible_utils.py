9c92a0a7feaf466a65344247bafcb489c20f48de
Chong Gu <chonggu@google.com>
2022-08-12 14:25:34 +0000

    [Fuchsia][CFv2 Script] Support device flashing
    Allow the CFv2 script to pave the device via setting the --os-check flag
    
    Change-Id: I2496715225092b1502bd94f7d426fefe997d3fd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807765
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034457}

--- build/fuchsia/test/compatible_utils.py
+++ build/fuchsia/test/compatible_utils.py
@@ -4,9 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 """Functions used in both v1 and v2 scripts."""
 
+import os
+import re
+
 from typing import Tuple
 
+# File indicating version of an image downloaded to the host
+_BUILD_ARGS = "buildargs.gn"
+
 _FILTER_DIR = 'testing/buildbot/filters'
+
+
+class VersionNotFoundError(Exception):
+    """Thrown when version info cannot be retrieved from device."""
 
 
 def parse_host_port(host_port_pair: str) -> Tuple[str, int]:
@@ -41,3 +51,34 @@ def map_filter_file_to_package_file(filter_file: str) -> str:
         raise ValueError('CFv2 tests only support registered filter files '
                          'present in the test package')
     return '/pkg/' + filter_file[filter_file.index(_FILTER_DIR):]
+
+
+def get_sdk_hash(system_image_dir: str) -> Tuple[str, str]:
+    """Read version of hash in pre-installed package directory.
+    Returns:
+        Tuple of (product, version) of image to be installed.
+    Raises:
+        VersionNotFoundError: if contents of buildargs.gn cannot be found or the
+        version number cannot be extracted.
+    """
+
+    # TODO(crbug.com/1261961): Stop processing buildargs.gn directly.
+    with open(os.path.join(system_image_dir, _BUILD_ARGS)) as f:
+        contents = f.readlines()
+    if not contents:
+        raise VersionNotFoundError('Could not retrieve %s' % _BUILD_ARGS)
+    version_key = 'build_info_version'
+    product_key = 'build_info_product'
+    info_keys = [product_key, version_key]
+    version_info = {}
+    for line in contents:
+        for key in info_keys:
+            match = re.match(r'%s = "(.*)"' % key, line)
+            if match:
+                version_info[key] = match.group(1)
+    if not (version_key in version_info and product_key in version_info):
+        raise VersionNotFoundError(
+            'Could not extract version info from %s. Contents: %s' %
+            (_BUILD_ARGS, contents))
+
+    return (version_info[product_key], version_info[version_key])
