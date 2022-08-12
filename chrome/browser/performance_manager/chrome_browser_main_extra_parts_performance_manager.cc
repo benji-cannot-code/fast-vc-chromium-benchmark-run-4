c4c3977c08fbb42a472acf47d58131e99f60fa5d
Kuo-Hsin Yang <vovoy@chromium.org>
2022-08-12 08:33:59 +0000

    Add OOM score manager for Lacros
    This change ports the OOM score assignment logic from Ash Chrome to
    Lacros Chrome. The Ash OOM score asgignment logic is at
    //chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.cc.
    
    Bug: 1307563
    Change-Id: I4a9d7636cd1977348f2e587590d7bd11bca886de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768103
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034389}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -44,6 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/performance_manager/policies/oom_score_policy_lacros.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/performance_manager/extension_watcher.h"
 #endif
@@ -109,6 +113,11 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
 #endif  // defined(ARCH_CPU_X86_64)
 
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  graph->PassToGraph(
+      std::make_unique<performance_manager::policies::OomScorePolicyLacros>());
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 #if !BUILDFLAG(IS_ANDROID)
   graph->PassToGraph(FormInteractionTabHelper::CreateGraphObserver());
