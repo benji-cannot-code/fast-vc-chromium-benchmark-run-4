501f041067a2083722559bc03f165011c069244f
Joseph Hwang <josephsih@chromium.org>
2022-08-12 16:52:16 +0000

    [Bluetooth] Add a chrome flag for Bluetooth Quality Report
    Add a chrome flag for the Bluetooth Quality Report feature.
    
    BUG=b:237255118
    TEST=The new "Bluetooth Quality Report" flag shows up in
    chrome://flags.
    
    Change-Id: I37227a441dc37bd92129ef8b2f7912cc02312f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3742858
    Commit-Queue: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034516}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -237,6 +237,10 @@ const base::Feature kBentoBar{"BentoBar", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kBluetoothFixA2dpPacketSize{
     "BluetoothFixA2dpPacketSize", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Enables or disables the Bluetooth Quality Report feature.
+const base::Feature kBluetoothQualityReport{"BluetoothQualityReport",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables or disables the ChromeOS Bluetooth Revamp, which updates Bluetooth
 // system UI and related infrastructure. See https://crbug.com/1010321.
 const base::Feature kBluetoothRevamp{"BluetoothRevamp",
@@ -1800,6 +1804,10 @@ bool IsBackgroundBlurEnabled() {
 
 bool IsBentoBarEnabled() {
   return base::FeatureList::IsEnabled(kBentoBar);
+}
+
+bool IsBluetoothQualityReportEnabled() {
+  return base::FeatureList::IsEnabled(kBluetoothQualityReport);
 }
 
 bool IsBluetoothRevampEnabled() {
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2631,6 +2631,9 @@ extern const char kAvatarsCloudMigrationDescription[];
 extern const char kBluetoothFixA2dpPacketSizeName[];
 extern const char kBluetoothFixA2dpPacketSizeDescription[];
 
+extern const char kBluetoothQualityReportName[];
+extern const char kBluetoothQualityReportDescription[];
+
 extern const char kBluetoothRevampName[];
 extern const char kBluetoothRevampDescription[];
 
