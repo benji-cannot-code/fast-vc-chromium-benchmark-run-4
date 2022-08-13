6c71483092d48f8c168d01b4e3741dea398c4988
David Padlipsky <dpad@google.com>
2022-08-13 00:10:49 +0000

    rgbkbd: Fix no response log from outputting in tests
    Use IsRunningOnChromeOS to only run when on a CrOS device.
    
    Bug: b/241822854
    Test: ash_unittests
    Change-Id: I7dbc566bae18a8c8e6165c69b319d24ce81abade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828717
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034755}

--- ash/rgb_keyboard/rgb_keyboard_manager.cc
+++ ash/rgb_keyboard/rgb_keyboard_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/logging.h"
+#include "base/system/sys_info.h"
 #include "chromeos/ash/components/dbus/rgbkbd/rgbkbd_client.h"
 
 namespace ash {
@@ -122,7 +123,9 @@ void RgbKeyboardManager::OnCapabilityUpdatedForTesting(
 void RgbKeyboardManager::OnGetRgbKeyboardCapabilities(
     absl::optional<rgbkbd::RgbKeyboardCapabilities> reply) {
   if (!reply.has_value()) {
-    LOG(ERROR) << "No response received for GetRgbKeyboardCapabilities";
+    if (base::SysInfo::IsRunningOnChromeOS()) {
+      LOG(ERROR) << "No response received for GetRgbKeyboardCapabilities";
+    }
     return;
   }
 
