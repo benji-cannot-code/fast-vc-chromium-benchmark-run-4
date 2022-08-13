5d0ca6e13c7ebf44ab309fd897c9c71a01e9e20e
Luke Cao <caott@google.com>
2022-08-13 01:10:42 +0000

    Update line height to match Chrome spec
    Bug: b/238244922
    Change-Id: I578bb6266b736eab91e9f8dc491b909b7d4e2c71
    Screenshot: https://screenshot.googleplex.com/8fq3G5gyxH2JSpH.png
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828486
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034768}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.cc
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.cc
@@ -35,6 +35,11 @@ void SetCurrentDialog(std::unique_ptr<AccessCodeCastDialog> dialog) {
 // The corner radius for system dialogs.
 constexpr int kSystemDialogCornerRadiusDp = 12;
 
+// The default width, height without footnote, height with footnote for the dialog container.
+const int kDialogWidthDefault = 448;
+const int kDialogHeightDefault = 271;
+const int kDialogHeightFootnote = 330;
+
 AccessCodeCastDialog::AccessCodeCastDialog(
     const CastModeSet& cast_mode_set,
     std::unique_ptr<MediaRouteStarter> media_route_starter)
@@ -157,14 +162,11 @@ void AccessCodeCastDialog::GetWebUIMessageHandlers(
     std::vector<content::WebUIMessageHandler*>* handlers) const {}
 
 void AccessCodeCastDialog::GetDialogSize(gfx::Size* size) const {
-  const int kDefaultWidth = 448;
-  const int kDefaultHeight = 271;
-  const int kRememberDevicesHeight = 310;
   base::TimeDelta duration_pref = GetAccessCodeDeviceDurationPref(
       context_->GetPrefs());
   bool rememberDevices = duration_pref != base::Seconds(0);
-  size->SetSize(kDefaultWidth,
-      rememberDevices ? kRememberDevicesHeight : kDefaultHeight);
+  size->SetSize(kDialogWidthDefault,
+      rememberDevices ? kDialogHeightFootnote : kDialogHeightDefault);
 }
 
 std::string AccessCodeCastDialog::GetDialogArgs() const {
