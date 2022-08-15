9517e65b76d395accf8235f859ae8c55237e39dd
Ricky Liang <jcliang@chromium.org>
2022-08-15 19:02:12 +0000

    CCA: set trace event name using lambda function
    The trace event names from CCA's Mojo interface is dynamic, so we
    should set the trace name dynamically.
    
    Bug: b:212231270
    Test: Capture a trace that contains a photo-taking event followed by
    mode-switching events. Verify that all the events are named properly in
    the trace output.
    
    Change-Id: Ia3c2fd79a29a5e165083ebaad1da03f5af250341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827937
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035143}

--- ash/webui/camera_app_ui/camera_app_helper_impl.cc
+++ ash/webui/camera_app_ui/camera_app_helper_impl.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/trace_event/trace_event.h"
+#include "base/trace_event/typed_macros.h"
 #include "chromeos/utils/pdf_conversion.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/url_util.h"
@@ -144,11 +144,13 @@ void CameraAppHelperImpl::IsTabletMode(IsTabletModeCallback callback) {
 }
 
 void CameraAppHelperImpl::StartPerfEventTrace(const std::string& event) {
-  TRACE_EVENT_BEGIN0("camera", event.c_str());
+  TRACE_EVENT_BEGIN("camera", nullptr, [&](perfetto::EventContext ctx) {
+    ctx.event()->set_name(event);
+  });
 }
 
 void CameraAppHelperImpl::StopPerfEventTrace(const std::string& event) {
-  TRACE_EVENT_END0("camera", event.c_str());
+  TRACE_EVENT_END("camera");
 }
 
 void CameraAppHelperImpl::SetTabletMonitor(
