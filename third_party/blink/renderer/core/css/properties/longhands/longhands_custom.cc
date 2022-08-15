96d53b926412d54aecf0b602ef966c924c19fa80
Joonghun Park <pjh0718@gmail.com>
2022-08-15 03:31:43 +0000

    CSS: Unprefix -webkit-hyphenate-character property
    This CL renames -webkit-hyphenate-character to hyphenate-character.
    
    Intent to ship: https://bit.ly/3JJkoAM
    
    Bug: 1281208
    Change-Id: Ie96df4a63cfe77caeb1e70c54cd6492ef19a65c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812328
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Joonghun Park <pjh0718@gmail.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034956}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -8030,7 +8030,7 @@ const CSSValue* WebkitHighlight::CSSValueFromComputedStyleInternal(
   return MakeGarbageCollected<CSSStringValue>(style.Highlight());
 }
 
-const CSSValue* WebkitHyphenateCharacter::ParseSingleValue(
+const CSSValue* HyphenateCharacter::ParseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext&,
     const CSSParserLocalContext&) const {
@@ -8039,7 +8039,7 @@ const CSSValue* WebkitHyphenateCharacter::ParseSingleValue(
   return css_parsing_utils::ConsumeString(range);
 }
 
-const CSSValue* WebkitHyphenateCharacter::CSSValueFromComputedStyleInternal(
+const CSSValue* HyphenateCharacter::CSSValueFromComputedStyleInternal(
     const ComputedStyle& style,
     const LayoutObject*,
     bool allow_visited_style) const {
