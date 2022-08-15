b3f0f218bdb6f9289f1617656a95a25985cd4a1b
David Bertoni <dbertoni@chromium.org>
2022-08-15 18:06:20 +0000

    [Extensions] Enable webRequest API tests for service workers.
    This CL parameterizes some webRequest API tests to run with both
    persistent background and service worker-based extensions.
    
    Bug: 1093066
    Change-Id: I14148b5fcd2429154b09862c5d937f1f1d1c5d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722817
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035117}

--- chrome/browser/extensions/extension_with_management_policy_apitest.cc
+++ chrome/browser/extensions/extension_with_management_policy_apitest.cc
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/http_request.h"
 
-ExtensionApiTestWithManagementPolicy::ExtensionApiTestWithManagementPolicy() {}
-ExtensionApiTestWithManagementPolicy::~ExtensionApiTestWithManagementPolicy() {}
+ExtensionApiTestWithManagementPolicy::ExtensionApiTestWithManagementPolicy(
+    ContextType context_type)
+    : ExtensionApiTest(context_type) {}
+ExtensionApiTestWithManagementPolicy::~ExtensionApiTestWithManagementPolicy() =
+    default;
 
 void ExtensionApiTestWithManagementPolicy::SetUpInProcessBrowserTestFixture() {
   extensions::ExtensionApiTest::SetUpInProcessBrowserTestFixture();
