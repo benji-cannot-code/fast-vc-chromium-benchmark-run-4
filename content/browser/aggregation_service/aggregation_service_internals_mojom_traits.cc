84e1006b6b6a9749c4daf024ba7226b22dfd9a17
Nan Lin <linnan@chromium.org>
2022-08-15 23:31:35 +0000

    Implement aggregation service internals handler for the web UI
    This CL adds the backing mojo interface and handler for the WebUI, which
    is used in a followup CL (crrev.com/c/3789670).
    
    There is two way communication between the handler and the WebUI over
    mojo, and this also proxies callbacks to the observer functions.
    
    Bug: 1348029
    Change-Id: Ibac65ad3195bdab394acc3d579bf13b6a7dd9528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3797345
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035263}

---
+++ content/browser/aggregation_service/aggregation_service_internals_mojom_traits.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/aggregation_service/aggregation_service_internals_mojom_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<
+    aggregation_service_internals::mojom::AggregatableReportRequestIDDataView,
+    content::AggregationServiceStorage::RequestId>::
+    Read(aggregation_service_internals::mojom::
+             AggregatableReportRequestIDDataView data,
+         content::AggregationServiceStorage::RequestId* out) {
+  *out = content::AggregationServiceStorage::RequestId(data.value());
+  return true;
+}
+
+}  // namespace mojo
--- content/browser/aggregation_service/aggregation_service_storage_sql.h
+++ content/browser/aggregation_service/aggregation_service_storage_sql.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
+#include "base/time/time.h"
 #include "content/browser/aggregation_service/aggregation_service_storage.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/storage_partition.h"
@@ -24,7 +25,6 @@ class GURL;
 
 namespace base {
 class Clock;
-class Time;
 }  // namespace base
 
 namespace sql {
