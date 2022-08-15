a635e18bb808017fed72ab062337b6698096f378
Nan Lin <linnan@chromium.org>
2022-08-15 19:45:13 +0000

    Add methods to support getting reports for and sending reports from aggregation service internals WebUI
    Bug: 1348029
    Change-Id: I28fd52314c38a47654049373287f592679669e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794786
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035160}

--- content/browser/aggregation_service/aggregation_service_impl_unittest.cc
+++ content/browser/aggregation_service/aggregation_service_impl_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/aggregation_service/aggregation_service_impl.h"
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include <map>
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/time/time.h"
 #include "content/browser/aggregation_service/aggregatable_report.h"
@@ -30,11 +32,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
 namespace content {
+
+namespace {
+using aggregation_service::RequestIdIs;
+using testing::ElementsAre;
+}  // namespace
 
 // TODO(alexmt): Consider rewriting these tests using gmock.
 
@@ -50,6 +58,11 @@ class TestAggregatableReportAssembler : public AggregatableReportAssembler {
   void AssembleReport(AggregatableReportRequest request,
                       AssemblyCallback callback) override {
     callbacks_.emplace(unique_id_counter_++, std::move(callback));
+
+    if (callbacks_.size() < min_requests_count_)
+      return;
+
+    wait_loop_.Quit();
   }
 
   void TriggerResponse(int64_t report_id,
@@ -62,9 +75,19 @@ class TestAggregatableReportAssembler : public AggregatableReportAssembler {
     callbacks_.erase(report_id);
   }
 
+  void WaitForRequests(size_t num_requests) {
+    min_requests_count_ = num_requests;
+    if (callbacks_.size() >= num_requests)
+      return;
+    wait_loop_.Run();
+  }
+
  private:
   int64_t unique_id_counter_ = 0;
   std::map<int64_t, AssemblyCallback> callbacks_;
+
+  size_t min_requests_count_ = 0;
+  base::RunLoop wait_loop_;
 };
 
 class TestAggregatableReportSender : public AggregatableReportSender {
@@ -210,6 +233,12 @@ class AggregationServiceImplTest : public testing::Test {
 
   void ScheduleReport(AggregatableReportRequest request) {
     service()->ScheduleReport(std::move(request));
+  }
+
+  void StoreReport(AggregatableReportRequest request) {
+    service()
+        ->storage_.AsyncCall(&AggregationServiceStorage::StoreRequest)
+        .WithArgs(std::move(request));
   }
 
   AggregationServiceImpl* service() { return service_impl_.get(); }
@@ -452,6 +481,34 @@ TEST_F(AggregationServiceImplTest,
       scheduler()
           ->WasRequestSuccessful(AggregationServiceStorage::RequestId(2))
           .value());
+}
+
+TEST_F(AggregationServiceImplTest, GetPendingReportRequestsForWebUI) {
+  StoreReport(aggregation_service::CreateExampleRequest());
+  StoreReport(aggregation_service::CreateExampleRequest());
+
+  base::RunLoop run_loop;
+  service()->GetPendingReportRequestsForWebUI(base::BindLambdaForTesting(
+      [&](std::vector<AggregationServiceStorage::RequestAndId>
+              requests_and_ids) {
+        // IDs autoincrement from 1.
+        EXPECT_THAT(
+            requests_and_ids,
+            ElementsAre(RequestIdIs(AggregationServiceStorage::RequestId(1)),
+                        RequestIdIs(AggregationServiceStorage::RequestId(2))));
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
+
+TEST_F(AggregationServiceImplTest, SendReportsForWebUI) {
+  StoreReport(aggregation_service::CreateExampleRequest());
+
+  // IDs autoincrement from 1.
+  service()->SendReportsForWebUI({AggregationServiceStorage::RequestId(1)},
+                                 base::DoNothing());
+
+  assembler()->WaitForRequests(/*num_requests=*/1);
 }
 
 }  // namespace content
--- content/browser/aggregation_service/aggregation_service_storage_sql_unittest.cc
+++ content/browser/aggregation_service/aggregation_service_storage_sql_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/database.h"
 #include "sql/meta_table.h"
 #include "sql/test/test_helpers.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
@@ -33,6 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
+
+using aggregation_service::RequestIdIs;
+using testing::ElementsAre;
+
+using RequestId = AggregationServiceStorage::RequestId;
 
 const char kExampleUrl[] =
     "https://helper.test/.well-known/aggregation-service/keys.json";
@@ -458,8 +464,7 @@ TEST_F(AggregationServiceStorageSqlTest, StoreRequest_ExpectedResult) {
   ASSERT_EQ(stored_requests_and_ids.size(), 1u);
 
   // IDs autoincrement from 1.
-  EXPECT_EQ(stored_requests_and_ids[0].id,
-            AggregationServiceStorage::RequestId(1));
+  EXPECT_EQ(stored_requests_and_ids[0].id, RequestId(1));
   EXPECT_TRUE(aggregation_service::ReportRequestsEqual(
       stored_requests_and_ids[0].request, request));
 }
@@ -475,7 +480,7 @@ TEST_F(AggregationServiceStorageSqlTest, DeleteRequest_ExpectedResult) {
             1u);
 
   // IDs autoincrement from 1.
-  storage_->DeleteRequest(AggregationServiceStorage::RequestId(1));
+  storage_->DeleteRequest(RequestId(1));
   EXPECT_TRUE(
       storage_->GetRequestsReportingOnOrBefore(base::Time::Max()).empty());
 }
@@ -556,6 +561,22 @@ TEST_F(AggregationServiceStorageSqlTest,
   }
 }
 
+TEST_F(AggregationServiceStorageSqlTest, GetRequests_ReturnValuesAlignWithIds) {
+  OpenDatabase();
+
+  AggregatableReportRequest request =
+      aggregation_service::CreateExampleRequest();
+
+  storage_->StoreRequest(aggregation_service::CloneReportRequest(request));
+  storage_->StoreRequest(aggregation_service::CloneReportRequest(request));
+  storage_->StoreRequest(std::move(request));
+
+  // IDs autoincrement from 1.
+  EXPECT_THAT(
+      storage_->GetRequests({RequestId(1), RequestId(3), RequestId(4)}),
+      ElementsAre(RequestIdIs(RequestId(1)), RequestIdIs(RequestId(3))));
+}
+
 TEST_F(AggregationServiceStorageSqlTest,
        NextReportTimeAfter_ReturnValuesAlignWithReportTime) {
   OpenDatabase();
@@ -628,12 +649,10 @@ TEST_F(AggregationServiceStorageSqlTest,
       storage_->GetRequestsReportingOnOrBefore(kExampleTime);
   ASSERT_EQ(example_time_reports.size(), 2u);
 
-  EXPECT_EQ(base::flat_set<AggregationServiceStorage::RequestId>(
+  EXPECT_EQ(base::flat_set<RequestId>(
                 {example_time_reports[0].id, example_time_reports[1].id}),
             // Request IDs autoincrement from 1.
-            base::flat_set<AggregationServiceStorage::RequestId>(
-                {AggregationServiceStorage::RequestId(1),
-                 AggregationServiceStorage::RequestId(2)}));
+            base::flat_set<RequestId>({RequestId(1), RequestId(2)}));
 
   ASSERT_TRUE(storage_->NextReportTimeAfter(kExampleTime).has_value());
   EXPECT_EQ(storage_->NextReportTimeAfter(kExampleTime).value(),
@@ -648,7 +667,7 @@ TEST_F(AggregationServiceStorageSqlTest,
   std::vector<AggregationServiceStorage::RequestAndId> all_reports =
       storage_->GetRequestsReportingOnOrBefore(kExampleTime + base::Hours(1));
   ASSERT_EQ(all_reports.size(), 3u);
-  EXPECT_EQ(all_reports[2].id, AggregationServiceStorage::RequestId(3));
+  EXPECT_EQ(all_reports[2].id, RequestId(3));
 
   EXPECT_FALSE(
       storage_->NextReportTimeAfter(kExampleTime + base::Hours(1)).has_value());
@@ -699,7 +718,7 @@ TEST_F(AggregationServiceStorageSqlTest,
   ASSERT_EQ(stored_reports.size(), 1u);
 
   // Only the last request should be left. Request IDs start from 1.
-  EXPECT_EQ(stored_reports[0].id, AggregationServiceStorage::RequestId(3));
+  EXPECT_EQ(stored_reports[0].id, RequestId(3));
 }
 
 TEST_F(AggregationServiceStorageSqlTest,
@@ -738,7 +757,7 @@ TEST_F(AggregationServiceStorageSqlTest,
   ASSERT_EQ(stored_reports.size(), 1u);
 
   // Only the last request should be left. Request IDs start from 1.
-  EXPECT_EQ(stored_reports[0].id, AggregationServiceStorage::RequestId(3));
+  EXPECT_EQ(stored_reports[0].id, RequestId(3));
 }
 
 TEST_F(AggregationServiceStorageSqlTest,
