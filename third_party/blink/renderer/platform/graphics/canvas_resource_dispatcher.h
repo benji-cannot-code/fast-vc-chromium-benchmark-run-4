1c4b8d3c2576369279649f198e0dec3d9513d505
Justin Novosad <junov@chromium.org>
2022-08-15 17:35:25 +0000

    Fix data races when returning OffscreenCanvas placeholder frames.
    Before this change, it was possible for the placeholder canvas to
    return a CanvasResource object to the canvas resource dispatcher
    when there are still outstanding references to it.  With this
    change, the ref count falling to zero is used as a trigger for
    returning the resource, which guarantees that a resource will
    not be destroyed or recycled while there are still pending
    accesses to its data.
    
    Bug: 1348616
    Change-Id: I6c4018bf50b03b699926cbffb71e5973eab5d9ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826420
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035103}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
@@ -64,7 +64,9 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
                      const SkIRect& damage_rect,
                      bool needs_vertical_flip,
                      bool is_opaque);
-  void ReclaimResource(viz::ResourceId, scoped_refptr<CanvasResource>&&);
+  // virtual for mocking
+  virtual void ReclaimResource(viz::ResourceId,
+                               scoped_refptr<CanvasResource>&&);
   void DispatchFrameSync(scoped_refptr<CanvasResource>&&,
                          base::TimeTicks commit_start_time,
                          const SkIRect& damage_rect,
@@ -96,6 +98,7 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
   void SetPlaceholderCanvasDispatcher(int placeholder_canvas_id);
 
  private:
+  friend class OffscreenCanvasPlaceholderTest;
   friend class CanvasResourceDispatcherTest;
   struct FrameResource;
 
