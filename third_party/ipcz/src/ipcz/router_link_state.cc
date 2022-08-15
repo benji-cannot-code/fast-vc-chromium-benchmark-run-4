878d2957554dc6c78a3dc205d48573424a9e71f5
Stephan Hartmann <stha09@googlemail.com>
2022-08-15 15:47:42 +0000

    ipcz: add two missing includes
    Bug: 957519
    Change-Id: I6073659d7a5b358d1f3fc55e1a221505c9a27dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829223
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035068}

--- third_party/ipcz/src/ipcz/router_link_state.cc
+++ third_party/ipcz/src/ipcz/router_link_state.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipcz/router_link_state.h"
 
 #include <cstring>
+#include <limits>
 
 #include "ipcz/link_side.h"
 
