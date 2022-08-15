f7baeb8d8798bc6219541cb08877553978bc1a5d
Steven Bennetts <stevenjb@chromium.org>
2022-08-15 20:04:28 +0000

    NetworkPortalDetectorImpl: Use PortalStateChanged
    This modifies NetworkPortalDetectorImpl to observe PortalStateChanged
    instead of observing DefaultNetworkChanged and performing its own state
    tracking.
    
    This also updates and improves tests to ensure that the behavior remains
    as expected.
    
    BUG=b:207069182
    TEST=unit_tests, chromeos_unittests, browser_tests
    
    Change-Id: I1948189ee794ff7aba0db4dc302fd8b8f464f4f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810587
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035169}

--- chrome/browser/ash/net/network_portal_detector_impl.h
+++ chrome/browser/ash/net/network_portal_detector_impl.h
@@ -29,10 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "url/gurl.h"
 
-namespace base {
-class Value;
-}
-
 namespace network {
 class SharedURLLoaderFactory;
 namespace mojom {
@@ -84,7 +80,7 @@ class NetworkPortalDetectorImpl : public NetworkPortalDetector,
   void RetryDetection();
 
   // Initiates Captive Portal detection attempt after |delay|.
-  void ScheduleAttempt(const base::TimeDelta& delay);
+  void ScheduleAttempt(const base::TimeDelta& delay = base::TimeDelta());
 
   // Starts detection attempt.
   void StartAttempt();
@@ -108,8 +104,9 @@ class NetworkPortalDetectorImpl : public NetworkPortalDetector,
   void SetStrategy(PortalDetectorStrategy::StrategyId id) override;
 
   // NetworkStateHandlerObserver implementation:
-  void DefaultNetworkChanged(const NetworkState* network) override;
   void OnShuttingDown() override;
+  void PortalStateChanged(const NetworkState* default_network,
+                          NetworkState::PortalState portal_state) override;
 
   // PortalDetectorStrategy::Delegate implementation:
   int NoResponseResultCount() override;
@@ -121,11 +118,8 @@ class NetworkPortalDetectorImpl : public NetworkPortalDetector,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
-  // Called synchronously from OnAttemptCompleted with the current default
-  // network. Stores the captive portal status and notifies observers.
   void DetectionCompleted(const NetworkState* network,
-                          const CaptivePortalStatus& results,
-                          int response_code);
+                          const CaptivePortalStatus& results);
 
   State state() const { return state_; }
 
@@ -174,12 +168,6 @@ class NetworkPortalDetectorImpl : public NetworkPortalDetector,
 
   // Unique identifier of the default network.
   std::string default_network_id_;
-
-  // Connection state of the default network.
-  std::string default_connection_state_;
-
-  // Proxy configuration of the default network.
-  base::Value default_proxy_config_;
 
   CaptivePortalStatus default_portal_status_ = CAPTIVE_PORTAL_STATUS_UNKNOWN;
   int response_code_for_testing_ = -1;
