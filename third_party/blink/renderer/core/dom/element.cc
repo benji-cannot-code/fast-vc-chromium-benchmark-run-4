f5e1f8ef51389c2a3ee4906d64f2c3f36db22af4
Mason Freed <masonf@chromium.org>
2022-08-15 21:31:48 +0000

    Move the pop-up "show" event earlier and make it cancelable
    Per [1], there's a desire from Mozilla to move the show event
    earlier in the process, and make it cancelable. I see no issues
    with doing that, so pending a discussion (at [1]), I'm going to
    make this change.
    
    [1] https://github.com/openui/open-ui/issues/579
    
    Bug: 1307772
    Change-Id: I87fcec84146f99434bb88d2af12941a2c9156586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826329
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035213}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element_rare_data.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
+#include "third_party/blink/renderer/core/dom/events/event_dispatch_result.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatcher.h"
 #include "third_party/blink/renderer/core/dom/events/event_path.h"
 #include "third_party/blink/renderer/core/dom/first_letter_pseudo_element.h"
@@ -2513,6 +2514,17 @@ void Element::showPopUp(ExceptionState& exception_state) {
         "Invalid on already-showing or disconnected popup elements");
   }
 
+  // Fire the show event (bubbles, cancelable).
+  Event* event = Event::CreateCancelableBubble(event_type_names::kShow);
+  event->SetTarget(this);
+  if (DispatchEvent(*event) != DispatchEventResult::kNotCanceled)
+    return;
+
+  // The 'show' event handler could have changed this pop-up, e.g. by changing
+  // its type, removing it from the document, or calling showPopUp().
+  if (!HasValidPopupAttribute() || !isConnected() || popupOpen())
+    return;
+
   bool should_restore_focus = false;
   auto& document = GetDocument();
   if (PopupType() == PopupValueType::kAuto ||
@@ -2561,17 +2573,6 @@ void Element::showPopUp(ExceptionState& exception_state) {
   }
   DCHECK(!document.AllOpenPopUps().Contains(this));
   document.AllOpenPopUps().insert(this);
-
-  // Fire the show event (bubbles, not cancelable).
-  Event* event = Event::CreateBubble(event_type_names::kShow);
-  event->SetTarget(this);
-  auto result = DispatchEvent(*event);
-  DCHECK_EQ(result, DispatchEventResult::kNotCanceled);
-
-  // The 'show' event handler could have changed this pop-up, e.g. by changing
-  // its type, removing it from the document, or calling showPopUp().
-  if (!HasValidPopupAttribute() || !isConnected() || popupOpen())
-    return;
 
   GetPopupData()->setAnimationFinishedListener(nullptr);
   GetPopupData()->setPreviouslyFocusedElement(
