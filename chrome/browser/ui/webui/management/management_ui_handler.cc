f8721ceb0dc834186a9ecea3e49cda5ef9c74843
Yann Dago <ydago@chromium.org>
2022-08-15 19:17:28 +0000

    Flex Org CBCM: Show flex org admin email when browser managed by CBCM
    This feature is behind the feature flag FlexOrgManagementDisclosure
    that will act as a kill switch. This feature is disabled on ChromeOS.
    
    Bug: 1351741
    Change-Id: I1658f53af6734137d4ac68e9fc23e399547b0084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823681
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035150}

--- chrome/browser/ui/webui/management/management_ui_handler.cc
+++ chrome/browser/ui/webui/management/management_ui_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/callback_helpers.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/string_util.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/managed_ui.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/strings/grit/components_strings.h"
@@ -496,7 +498,14 @@ void AddThreatProtectionPermission(const char* title,
 }
 
 std::string GetAccountManager(Profile* profile) {
-  return chrome::GetAccountManagerIdentity(profile).value_or(std::string());
+  absl::optional<std::string> manager =
+      chrome::GetAccountManagerIdentity(profile);
+  if (!manager &&
+      base::FeatureList::IsEnabled(features::kFlexOrgManagementDisclosure)) {
+    manager = chrome::GetDeviceManagerIdentity();
+  }
+
+  return manager.value_or(std::string());
 }
 
 }  // namespace
