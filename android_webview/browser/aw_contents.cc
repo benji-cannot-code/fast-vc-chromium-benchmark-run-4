902840fdbf9c501a80a8cf7a9616658ff903036b
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-08-15 13:24:23 +0000

    Log distinct origin count per day for WebView for http and https.
    Bug: 1352070
    
    Change-Id: I191091b081acb7beb587d26374b9ef876ae53991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822587
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035031}

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -97,6 +97,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image.h"
+#include "url/origin.h"
+#include "url/url_constants.h"
+
 struct AwDrawSWFunctionTable;
 
 using autofill::ContentAutofillDriverFactory;
@@ -1458,12 +1461,23 @@ void AwContents::RenderViewHostChanged(content::RenderViewHost* old_host,
 }
 
 void AwContents::PrimaryPageChanged(content::Page& page) {
-  std::string scheme = page.GetMainDocument().GetLastCommittedURL().scheme();
+  const url::Origin& origin = page.GetMainDocument().GetLastCommittedOrigin();
+  const std::string& scheme = origin.scheme();
   if (scheme_ != scheme) {
     scheme_ = scheme;
     AwBrowserProcess::GetInstance()
         ->visibility_metrics_logger()
         ->ClientVisibilityChanged(this);
+  }
+
+  if (scheme == url::kHttpsScheme || scheme == url::kHttpScheme) {
+    JNIEnv* env = AttachCurrentThread();
+    ScopedJavaLocalRef<jobject> j_ref = java_ref_.get(env);
+    if (j_ref) {
+      uint32_t origin_hash = base::PersistentHash(origin.Serialize());
+      jlong j_origin_hash = static_cast<jlong>(origin_hash);
+      Java_AwContents_logOriginVisit(env, j_ref, j_origin_hash);
+    }
   }
 }
 
--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -55,6 +55,7 @@ import org.chromium.android_webview.gfx.AwFunctor;
 import org.chromium.android_webview.gfx.AwGLFunctor;
 import org.chromium.android_webview.gfx.AwPicture;
 import org.chromium.android_webview.gfx.RectUtils;
+import org.chromium.android_webview.metrics.AwOriginVisitLogger;
 import org.chromium.android_webview.permission.AwGeolocationCallback;
 import org.chromium.android_webview.permission.AwPermissionRequest;
 import org.chromium.android_webview.renderer_priority.RendererPriority;
@@ -74,6 +75,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.ScopedSysTraceEvent;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.PostTask;
+import org.chromium.base.task.TaskTraits;
 import org.chromium.blink_public.common.BlinkFeatures;
 import org.chromium.components.autofill.AutofillActionModeCallback;
 import org.chromium.components.autofill.AutofillProvider;
@@ -3642,6 +3644,13 @@ public class AwContents implements SmartClipProvider {
     @CalledByNative
     private void onPermissionRequestCanceled(AwPermissionRequest awPermissionRequest) {
         mContentsClient.onPermissionRequestCanceled(awPermissionRequest);
+    }
+
+    @CalledByNative
+    private void logOriginVisit(long originHash) {
+        if (isDestroyed(NO_WARN)) return;
+        PostTask.postTask(TaskTraits.BEST_EFFORT_MAY_BLOCK,
+                () -> AwOriginVisitLogger.logOriginVisit(originHash));
     }
 
     @CalledByNative
