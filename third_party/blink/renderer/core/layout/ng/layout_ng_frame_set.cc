825891b9157d03d49e50bbd32b681c48bfec528a
Kent Tamura <tkent@chromium.org>
2022-08-15 04:21:32 +0000

    FrameSet NG: Add three methods to LayoutNGFrameSet
    This CL adds:
     * IsChildAllowed()
     * UpdateBlockLayout()
     * GetCursor()
    
    Implementations of IsChildAllowed() and GetCursor() are very similar to
    ones of LayoutFrameSet. UpdateBlockLayout() implementation is same as
    ones of LayoutNGGrid and LayoutNGFlexibleBox.
    
    This CL has no behaivor changes due to a disabled runtime flag.
    
    Bug: 1346221
    Change-Id: Iad457b7c41ee78b559a17886a5df2efc4063781a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828775
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034969}

--- third_party/blink/renderer/core/layout/ng/layout_ng_frame_set.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_frame_set.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_frame_set.h"
 
 #include "third_party/blink/renderer/core/html/html_frame_set_element.h"
+#include "third_party/blink/renderer/platform/cursors.h"
+#include "ui/base/cursor/cursor.h"
 
 namespace blink {
 
@@ -21,6 +23,35 @@ const char* LayoutNGFrameSet::GetName() const {
 bool LayoutNGFrameSet::IsOfType(LayoutObjectType type) const {
   NOT_DESTROYED();
   return type == kLayoutObjectNGFrameSet || LayoutNGBlock::IsOfType(type);
+}
+
+bool LayoutNGFrameSet::IsChildAllowed(LayoutObject* child,
+                                      const ComputedStyle&) const {
+  NOT_DESTROYED();
+  return child->IsFrame() || child->IsLayoutNGFrameSet();
+}
+
+void LayoutNGFrameSet::UpdateBlockLayout(bool relayout_children) {
+  if (IsOutOfFlowPositioned())
+    UpdateOutOfFlowBlockLayout();
+  else
+    UpdateInFlowBlockLayout();
+}
+
+CursorDirective LayoutNGFrameSet::GetCursor(const PhysicalOffset& point,
+                                            ui::Cursor& cursor) const {
+  NOT_DESTROYED();
+  const auto& frame_set = *To<HTMLFrameSetElement>(GetNode());
+  gfx::Point rounded_point = ToRoundedPoint(point);
+  if (frame_set.CanResizeRow(rounded_point)) {
+    cursor = RowResizeCursor();
+    return kSetCursor;
+  }
+  if (frame_set.CanResizeColumn(rounded_point)) {
+    cursor = ColumnResizeCursor();
+    return kSetCursor;
+  }
+  return LayoutBox::GetCursor(point, cursor);
 }
 
 }  // namespace blink
