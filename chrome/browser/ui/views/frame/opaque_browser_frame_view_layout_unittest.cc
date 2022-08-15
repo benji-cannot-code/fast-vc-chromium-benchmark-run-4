f4d1f5da0e76440387e972f1e6595f315444e52a
Elaine Chien <elainechien@chromium.org>
2022-08-15 15:48:08 +0000

    Remove remaining Layout() calls in tests in c/b/ui/view/frame/
    Bug: 1350521
    Change-Id: Ic74aa13cb48ab5041dbc60f3444a8d0e1074047f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829749
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035069}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view_layout_unittest.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view_layout_unittest.cc
@@ -371,7 +371,7 @@ class OpaqueBrowserFrameViewLayoutTest
 TEST_P(OpaqueBrowserFrameViewLayoutTest, BasicWindow) {
   // Tests the layout of a default chrome window with a tabstrip and no window
   // title.
-  root_view_->Layout();
+  RunScheduledLayout(root_view_);
   ExpectCaptionButtons(false, 0);
   ExpectTabStripAndMinimumSize(false);
   ExpectWindowIcon(false);
@@ -386,7 +386,7 @@ TEST_P(OpaqueBrowserFrameViewLayoutTest, WindowButtonsOnLeft) {
   leading_buttons.push_back(views::FrameButton::kMaximize);
   layout_manager_->SetButtonOrdering(leading_buttons, trailing_buttons);
 
-  root_view_->Layout();
+  RunScheduledLayout(root_view_);
   ExpectCaptionButtons(true, 0);
   ExpectTabStripAndMinimumSize(true);
   ExpectWindowIcon(true);
@@ -397,7 +397,7 @@ TEST_P(OpaqueBrowserFrameViewLayoutTest, WithoutCaptionButtons) {
   // should force the tab strip to be condensed).
   delegate_->set_show_caption_buttons(false);
 
-  root_view_->Layout();
+  RunScheduledLayout(root_view_);
   ExpectCaptionButtons(false, 0);
   ExpectTabStripAndMinimumSize(false);
   ExpectWindowIcon(false);
@@ -408,7 +408,7 @@ TEST_P(OpaqueBrowserFrameViewLayoutTest, WindowWithTitleAndIcon) {
   delegate_->set_window_title(u"Window Title");
   AddWindowTitleIcons();
 
-  root_view_->Layout();
+  RunScheduledLayout(root_view_);
   ExpectCaptionButtons(false, 0);
   ExpectWindowIcon(false);
   ExpectWindowTitle();
