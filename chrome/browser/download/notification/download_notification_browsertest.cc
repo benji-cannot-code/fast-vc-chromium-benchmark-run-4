3c775e22fc94cf75f78b3cd0ec8de8bcfd3f43b4
Rohit Bhatia <bhatiarohit@google.com>
2022-08-16 23:37:33 +0000

    [DownloadBubble] Update Field Trial for beta and stable experiment
    Update field trials, and disable the download bubble feature for download notification tests on lacros. When the download bubble feature is active, chromeos notifications are disabled, so to test them, the feature needs to be disabled.
    
    Bug: 1265640
    Change-Id: I9c876c22483648df6b268cbe8bed6cab70a84ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831931
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035776}

--- chrome/browser/download/notification/download_notification_browsertest.cc
+++ chrome/browser/download/notification/download_notification_browsertest.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/download/public/common/download_item.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/core/common/features.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
@@ -302,6 +303,9 @@ class DownloadNotificationTestBase
     scoped_feature_list_.InitWithFeatureState(
         ash::features::kHoldingSpaceInProgressDownloadsNotificationSuppression,
         IsHoldingSpaceInProgressDownloadsNotificationSuppressionEnabled());
+#else
+    scoped_feature_list_.InitWithFeatures(
+        {}, {safe_browsing::kDownloadBubble, safe_browsing::kDownloadBubbleV2});
 #endif
   }
 
