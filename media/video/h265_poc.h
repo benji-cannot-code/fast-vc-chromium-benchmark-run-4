16b4ea574d11ddd95056977bf57c6a08daa27d4a
Sida Zhu <zhusida@bytedance.com>
2022-08-16 20:29:05 +0000

    Video: Do not parse nalu headers of the same picture multiple times
    When they are referring to the same picture, this is not necessary,
    also add a comment about the HEVC Rext Profile support details.
    
    Bug: 1300444
    Change-Id: Ie3f9714d32b972d70da9f92207805406038e6ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798837
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035673}

--- media/video/h265_poc.h
+++ media/video/h265_poc.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "media/base/media_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace media {
