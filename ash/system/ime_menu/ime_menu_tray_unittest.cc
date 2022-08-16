8409022c688c8ee38e650af0998b9b0091a61272
Irina Zelentsova <irinazel@google.com>
2022-08-16 09:27:34 +0000

    Disable Settings button in IME Tray for Kiosk
    Previous IME tray:
    https://screenshot.googleplex.com/7mgW2BRw4VPv7YC
    New IME tray in Kiosk session:
    https://screenshot.googleplex.com/cDxA35HafiZprTD
    IME tray in user session with settings button:
    https://screenshot.googleplex.com/AfHbajdFnQq8M7t
    
    Bug: b/242173879
    
    Test: ash_unittests
    --gtest_filter="ImeMenuTrayTest.*"
    
    Change-Id: I2bb2a53efd39fc70fde5975621909f3def4c8c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826305
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Irina Zelentsova <irinazel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035419}

--- ash/system/ime_menu/ime_menu_tray_unittest.cc
+++ ash/system/ime_menu/ime_menu_tray_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ime/ime_controller_impl.h"
 #include "ash/ime/test_ime_controller_client.h"
 #include "ash/public/cpp/ime_info.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/system/ime_menu/ime_list_view.h"
 #include "ash/system/status_area_widget.h"
@@ -35,6 +36,7 @@ namespace ash {
 namespace {
 
 const int kEmojiButtonId = 1;
+const int kSettingsButtonId = 2;
 
 ImeMenuTray* GetTray() {
   return StatusAreaWidgetTestHelper::GetStatusAreaWidget()->ime_menu_tray();
@@ -86,6 +88,17 @@ class ImeMenuTrayTest : public AshTestBase {
   views::Button* GetEmojiButton() const {
     return static_cast<views::Button*>(
         GetTray()->bubble_->bubble_view()->GetViewByID(kEmojiButtonId));
+  }
+
+  views::View* GetSettingsButton() const {
+    return static_cast<views::View*>(
+        GetTray()->bubble_->bubble_view()->GetViewByID(kSettingsButtonId));
+  }
+
+  void SetUpKioskSession() {
+    SessionInfo info;
+    info.is_running_in_app_mode = true;
+    Shell::Get()->session_controller()->SetSessionInfo(info);
   }
 
   // Verifies the IME menu list has been updated with the right IME list.
@@ -416,6 +429,33 @@ TEST_F(ImeMenuTrayTest, ShouldShowBottomButtonsSeperate) {
   EXPECT_TRUE(IsEmojiEnabled());
   EXPECT_FALSE(IsHandwritingEnabled());
   EXPECT_FALSE(IsVoiceEnabled());
+}
+
+TEST_F(ImeMenuTrayTest, KioskImeTraySettingsButton) {
+  SetUpKioskSession();
+  Shell::Get()->ime_controller()->ShowImeMenuOnShelf(true);
+  ASSERT_TRUE(IsVisible());
+
+  // Open the menu.
+  ui::GestureEvent tap(0, 0, 0, base::TimeTicks(),
+                       ui::GestureEventDetails(ui::ET_GESTURE_TAP));
+  GetTray()->PerformAction(tap);
+
+  views::View* settings_button = GetSettingsButton();
+  EXPECT_FALSE(settings_button);
+}
+
+TEST_F(ImeMenuTrayTest, UserSessionImeTraySettingsButton) {
+  Shell::Get()->ime_controller()->ShowImeMenuOnShelf(true);
+  ASSERT_TRUE(IsVisible());
+
+  // Open the menu.
+  ui::GestureEvent tap(0, 0, 0, base::TimeTicks(),
+                       ui::GestureEventDetails(ui::ET_GESTURE_TAP));
+  GetTray()->PerformAction(tap);
+
+  views::View* settings_button = GetSettingsButton();
+  EXPECT_TRUE(settings_button);
 }
 
 TEST_F(ImeMenuTrayTest, ShowOnScreenKeyboardToggle) {
