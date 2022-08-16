9c2c9956819c07630969d821c8ddcc96bbc1be3e
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2022-08-16 23:53:50 +0000

    Add metrics to count the number of fetch handlers.
    We intend to categorize the fetch handlers and tell it via
    EmbeddedWorkerInstanceHost::OnStarted.  However, multiple fetch handlers
    can be set for a service worker, and we need further consideration
    what to return.
    To understand current status, we intend to gather metrics.
    
    Bug: 1347319
    Change-Id: I0a7d20ee6693001f7e4ead52ff7e371f047ea826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805195
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035785}

--- third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
@@ -384,6 +384,9 @@ void ServiceWorkerGlobalScope::DidEvaluateScript() {
   DCHECK(!did_evaluate_script_);
   did_evaluate_script_ = true;
 
+  base::UmaHistogramCounts1000(
+      "ServiceWorker.NumberOfRegisteredFetchHandlers",
+      NumberOfEventListeners(event_type_names::kFetch));
   event_queue_->Start();
 }
 
