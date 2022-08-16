122d5d92be5476ed94d99d496bb0fd5879946443
David Dorwin <ddorwin@chromium.org>
2022-08-16 15:24:21 +0000

    [chrome][pdf] Add ENABLE_PDF around PDF-specific test
    This fixes a build error when enable_pdf=false.
    
    Bug: 1352700
    Change-Id: I347874a980f4a4fdf2a8f5b103fadc55b5c8b29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832504
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035520}

--- chrome/browser/chrome_back_forward_cache_browsertest.cc
+++ chrome/browser/chrome_back_forward_cache_browsertest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_utils.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "pdf/buildflags.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 #include "third_party/blink/public/mojom/webshare/webshare.mojom.h"
@@ -761,6 +762,7 @@ IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
       FROM_HERE);
 }
 
+#if BUILDFLAG(ENABLE_PDF)
 IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
                        DoesNotCachePageWithEmbeddedPdf) {
   const auto tag = GetParam();
@@ -789,6 +791,7 @@ IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
   // the blocklisted feature kContainsPlugins.
   ExpectNotRestoredReasonHaveInnerContents(FROM_HERE);
 }
+#endif  // BUILDFLAG(ENABLE_PDF)
 
 IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
                        DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded) {
