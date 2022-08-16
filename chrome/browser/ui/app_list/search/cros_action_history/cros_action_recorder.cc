d2a217fd76b28c6ce873ec0694f294dd81a49e53
Jong Ahn <jongahn@google.com>
2022-08-16 00:23:56 +0000

    [Cleanup] Delete old-gen and rename file to "structured_events".
    Bug: 1249222
    Change-Id: I5a48cd57db05c13a137825a1e036a8fd4bee6e97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827385
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035291}

--- chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder.cc
+++ chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/app_list/search/cros_action_history/cros_action.pb.h"
-#include "components/metrics/structured/structured_mojo_events.h"
+#include "components/metrics/structured/structured_events.h"
 
 namespace app_list {
 namespace {
