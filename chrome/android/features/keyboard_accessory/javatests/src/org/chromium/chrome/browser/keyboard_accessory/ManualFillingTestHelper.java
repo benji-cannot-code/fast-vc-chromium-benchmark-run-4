0456b6769f529b120d650521ca7e8a2b5e9be0b5
Ivana Žužić <izuzic@google.com>
2022-08-16 17:05:19 +0000

    [PMIntegrationTests] Fix and enable PasswordGenerationIntegrationTest
    The tests were disabled long ago because it was failing on bots.
    The mistake in testManualGenerationUsePassword and
    testAutomaticGenerationUsePassword was that the tests were not waiting
    for the success page to load before checking if the message about the
    password being saved showed up.
    TestManualGenerationUsePassword is still flaky so it's still disabled.
    
    Bug: 1329840
    Change-Id: I3843f8b45c7b195466000672095539012fee1c95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791345
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035582}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
@@ -93,6 +93,15 @@ public class ManualFillingTestHelper {
         mActivityTestRule = activityTestRule;
     }
 
+    public EmbeddedTestServer getOrCreateTestServer() {
+        if (mEmbeddedTestServer == null) {
+            mEmbeddedTestServer = EmbeddedTestServer.createAndStartHTTPSServer(
+                    InstrumentationRegistry.getInstrumentation().getContext(),
+                    ServerCertificate.CERT_OK);
+        }
+        return mEmbeddedTestServer;
+    }
+
     public void loadTestPage(boolean isRtl) {
         loadTestPage("/chrome/test/data/password/password_form.html", isRtl);
     }
@@ -103,9 +112,7 @@ public class ManualFillingTestHelper {
 
     public void loadTestPage(String url, boolean isRtl, boolean waitForNode,
             ChromeWindow.KeyboardVisibilityDelegateFactory keyboardDelegate) {
-        mEmbeddedTestServer = EmbeddedTestServer.createAndStartHTTPSServer(
-                InstrumentationRegistry.getInstrumentation().getContext(),
-                ServerCertificate.CERT_OK);
+        getOrCreateTestServer();
         ChromeWindow.setKeyboardVisibilityDelegateFactory(keyboardDelegate);
         if (mActivityTestRule.getActivity() == null) {
             mActivityTestRule.startMainActivityWithURL(mEmbeddedTestServer.getURL(url));
