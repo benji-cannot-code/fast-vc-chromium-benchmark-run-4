d4b273e410e6adc2a57763d35fab64ade9c4c3a6
David Dorwin <ddorwin@chromium.org>
2022-08-16 15:27:25 +0000

    [chrome] Fix enable_hidpi=false build on non-Windows platforms
    Add missing guards in resources_integrity.cc.
    Windows-specific code is not changed because there is no known use case.
    
    Bug: 1352700
    Change-Id: I46ad9f56b111ae6de7ffaa8292ead32c9a4b2d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832915
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035521}

--- chrome/browser/resources_integrity.cc
+++ chrome/browser/resources_integrity.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "chrome/common/chrome_paths.h"
 #include "crypto/secure_hash.h"
+#include "ui/base/buildflags.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/app/chrome_exe_main_win.h"
@@ -110,8 +111,10 @@ void CheckPakFileIntegrity() {
       kSha256_resources_pak;
   base::span<const uint8_t, crypto::kSHA256Length> chrome_100_hash =
       kSha256_chrome_100_percent_pak;
+#if BUILDFLAG(ENABLE_HIDPI)
   base::span<const uint8_t, crypto::kSHA256Length> chrome_200_hash =
       kSha256_chrome_200_percent_pak;
+#endif
 #endif  // BUILDFLAG(IS_WIN)
 
   scoped_refptr<base::SequencedTaskRunner> task_runner =
@@ -127,9 +130,11 @@ void CheckPakFileIntegrity() {
       chrome_100_hash, task_runner,
       base::BindOnce(&ReportPakIntegrity,
                      "SafeBrowsing.PakIntegrity.Chrome100"));
+#if BUILDFLAG(ENABLE_HIDPI)
   CheckResourceIntegrity(
       resources_pack_path.DirName().AppendASCII("chrome_200_percent.pak"),
       chrome_200_hash, task_runner,
       base::BindOnce(&ReportPakIntegrity,
                      "SafeBrowsing.PakIntegrity.Chrome200"));
+#endif
 }
