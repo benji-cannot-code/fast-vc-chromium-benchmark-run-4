d3aee4a57f76e3a589295c68b4a103382ca25a92
Rune Lillesveen <futhark@chromium.org>
2022-08-16 21:04:10 +0000

    [@container] No !important in style() queries
    !important should not be part of a valid style() query. Add tests for
    custom property matching.
    
    Bug: 1302630
    Change-Id: I001d2d5784c2618cb57a30db61973f3647a27068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832231
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035692}

--- third_party/blink/renderer/core/css/media_query_exp.cc
+++ third_party/blink/renderer/core/css/media_query_exp.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_math_function_value.h"
 #include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_context.h"
+#include "third_party/blink/renderer/core/css/parser/css_parser_impl.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token_range.h"
 #include "third_party/blink/renderer/core/css/parser/css_tokenized_value.h"
 #include "third_party/blink/renderer/core/css/parser/css_variable_parser.h"
@@ -325,8 +326,12 @@ absl::optional<MediaQueryExpValue> MediaQueryExpValue::Consume(
   CSSParserContext::ParserModeOverridingScope scope(context, kHTMLStandardMode);
 
   if (CSSVariableParser::IsValidVariableName(media_feature)) {
-    if (const CSSValue* value =
-            CSSVariableParser::ParseDeclarationValue({range}, false, context)) {
+    CSSTokenizedValue tokenized_value{range};
+    if (CSSParserImpl::RemoveImportantAnnotationIfPresent(tokenized_value)) {
+      return absl::nullopt;
+    }
+    if (const CSSValue* value = CSSVariableParser::ParseDeclarationValue(
+            tokenized_value, false, context)) {
       while (!range.AtEnd())
         range.Consume();
       return MediaQueryExpValue(*value);
