d25ca9348acf4635b04573e4482093da05cc83b4
Jong Ahn <jongahn@google.com>
2022-08-16 20:17:35 +0000

    [CrOSEvents] Add events processor to modify events.
    CrOSEvents will need to post-process events after they are recorded.
    
    Bug: 1350322
    Change-Id: I3ad749f6de99522ff47c6a545fe6bd6fcb7a089c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812390
    Reviewed-by: Hirthanan Subenderan <hirthanan@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035664}

--- components/metrics/structured/structured_metrics_client.h
+++ components/metrics/structured/structured_metrics_client.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/no_destructor.h"
 
+#include "components/metrics/structured/delegating_events_processor.h"
 #include "components/metrics/structured/event.h"
+#include "components/metrics/structured/events_processor_interface.h"
 
 namespace metrics {
 namespace structured {
@@ -42,6 +44,11 @@ class StructuredMetricsClient {
   // Forwards to |delegate_|. If no delegate has been set, then no-op.
   void Record(Event&& event);
 
+  // Adds |events_processor| to further add metadata to recorded events or
+  // listen to recorded events.
+  void AddEventsProcessor(
+      std::unique_ptr<EventsProcessorInterface> events_processor);
+
   // Sets the delegate for the client's recording logic. Should be called before
   // anything else. |this| does not take ownership of |delegate| and assumes
   // that the caller will properly manage the lifetime of delegate and call
@@ -54,6 +61,8 @@ class StructuredMetricsClient {
 
   StructuredMetricsClient();
   ~StructuredMetricsClient();
+
+  DelegatingEventsProcessor delegating_events_processor_;
 
   // Not owned. Assumes that the delegate's lifetime will exceed |this|.
   raw_ptr<RecordingDelegate> delegate_ = nullptr;
