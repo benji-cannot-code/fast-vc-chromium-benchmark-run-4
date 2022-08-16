d5744f34d617da233456a4874e9b918a7b1cef49
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-08-16 23:10:51 +0000

    [grid] Small fixes & remove complex baseline logic.
    Previously we had complicated "truth tables" to determine the correct
    baseline. These were un-intuitive, and complex.
    
    This patch introduces the concept of a writing-mode to read a baseline
    for a particular grid-item.
    
    This is what the spec(s) use, and should be easier to keep consistent
    with the spec.
    
    The two tests updated were using the wrong edge to synthesize their
    respective baselines.
    
    Change-Id: I576b79444e27e03583c709eb15e9e13d400e8b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813642
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035762}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class NGBoxFragment;
+
 // This enum corresponds to each step used to accommodate grid items across
 // intrinsic tracks according to their min and max track sizing functions, as
 // defined in https://drafts.csswg.org/css-grid-2/#algo-spanning-items.
@@ -74,6 +76,8 @@ class CORE_EXPORT NGGridLayoutAlgorithm
                            LayoutUnit* intrinsic_block_size);
 
   LayoutUnit ComputeIntrinsicBlockSizeIgnoringChildren() const;
+
+  LayoutUnit GetLogicalBaseline(const NGBoxFragment&, BaselineGroup) const;
 
   // Returns the size that a grid item will distribute across the tracks with an
   // intrinsic sizing function it spans in the relevant track direction.
