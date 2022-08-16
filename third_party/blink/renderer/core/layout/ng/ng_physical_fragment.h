7dd904aa3815b518363a332ff96508c41f8ffc8c
Kent Tamura <tkent@chromium.org>
2022-08-16 23:58:43 +0000

    FrameSet NG: Copy FrameSetLayoutData in the simplified layout algorithm
    This CL has no behavior changes due to a disabled runtime flag.
    
    Bug: 1346221
    Change-Id: I0eb01889dbf3260b9d8af7b07e9936238170d00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828772
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035787}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -177,6 +177,9 @@ class CORE_EXPORT NGPhysicalFragment
   bool IsAnonymousBlock() const {
     return IsCSSBox() && layout_object_->IsAnonymousBlock();
   }
+  bool IsFrameSet() const {
+    return IsCSSBox() && layout_object_->IsLayoutNGFrameSet();
+  }
   bool IsListMarker() const {
     return IsCSSBox() && layout_object_->IsLayoutNGOutsideListMarker();
   }
