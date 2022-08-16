28a8a68d4b13d274f479f22457010cffb25459bd
Jason Zhang <jiajunz@google.com>
2022-08-16 16:59:47 +0000

    [CrOS Hotspot] Introduce hotspot mojom and HotspotStateHandler class
    This CL introduces the new cros_hotspot_config.mojom which provides
    mojo enum for hotspot tethering. A following CL will add interfaces that
    allow configuration and setup of tethering hotspots.
    This CL also introduce a HotspotStateHandler class which caches hotspot
    related status, e.g: state and active client count, and implements
    methods to get the current state, active client count. A followup CL
    will add the implementation to get capabilities, configurations and
    configure hotspot.
    
    Bug: b/239477916
    Change-Id: If777d633336305a8a42dac941127a7f0c0116cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824937
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035575}

--- chromeos/ash/components/network/network_handler.h
+++ chromeos/ash/components/network/network_handler.h
@@ -29,6 +29,7 @@ class ConnectionInfoMetricsLogger;
 class ESimPolicyLoginMetricsLogger;
 class GeolocationHandler;
 class HiddenNetworkHandler;
+class HotspotStateHandler;
 class ManagedCellularPrefHandler;
 class ManagedNetworkConfigurationHandler;
 class ManagedNetworkConfigurationHandlerImpl;
@@ -106,6 +107,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkHandler {
   CellularInhibitor* cellular_inhibitor();
   CellularPolicyHandler* cellular_policy_handler();
   HiddenNetworkHandler* hidden_network_handler();
+  HotspotStateHandler* hotspot_state_handler();
   NetworkStateHandler* network_state_handler();
   NetworkDeviceHandler* network_device_handler();
   NetworkProfileHandler* network_profile_handler();
@@ -150,6 +152,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkHandler {
   std::unique_ptr<CellularMetricsLogger> cellular_metrics_logger_;
   std::unique_ptr<ConnectionInfoMetricsLogger> connection_info_metrics_logger_;
   std::unique_ptr<HiddenNetworkHandler> hidden_network_handler_;
+  std::unique_ptr<HotspotStateHandler> hotspot_state_handler_;
   std::unique_ptr<ESimPolicyLoginMetricsLogger>
       esim_policy_login_metrics_logger_;
   std::unique_ptr<VpnNetworkMetricsHelper> vpn_network_metrics_helper_;
