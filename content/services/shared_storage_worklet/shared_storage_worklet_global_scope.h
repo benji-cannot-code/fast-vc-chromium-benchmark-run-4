791b06b80571b649f99a987bd49ef56e3f48f50e
Alex Turner <alexmt@chromium.org>
2022-08-16 20:22:42 +0000

    Integrating Private Aggregation with Shared Storage
    Exposes a new privateAggregation.sendHistogramReport() call to Shared
    Storage worklets and adds the mojo plumbing to forward these requests to
    the private_aggregation/ component (if it's enabled) in the browser.
    
    Bug: 1346108
    Change-Id: Iea4db1c14fc1c0dda805a639bee917f4f9dfb2ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814272
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035668}

--- content/services/shared_storage_worklet/shared_storage_worklet_global_scope.h
+++ content/services/shared_storage_worklet/shared_storage_worklet_global_scope.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_SERVICES_SHARED_STORAGE_WORKLET_SHARED_STORAGE_WORKLET_GLOBAL_SCOPE_H_
 
 #include "content/common/content_export.h"
+#include "content/common/private_aggregation_host.mojom-forward.h"
 #include "content/common/shared_storage_worklet_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
@@ -23,6 +24,7 @@ namespace shared_storage_worklet {
 class UrlSelectionOperationHandler;
 class UnnamedOperationHandler;
 class Console;
+class PrivateAggregation;
 class SharedStorage;
 class ModuleScriptDownloader;
 
@@ -39,11 +41,13 @@ class CONTENT_EXPORT SharedStorageWorkletGlobalScope {
       mojo::PendingRemote<network::mojom::URLLoaderFactory>
           pending_url_loader_factory,
       mojom::SharedStorageWorkletServiceClient* client,
+      content::mojom::PrivateAggregationHost* private_aggregation_host,
       const GURL& script_source_url,
       mojom::SharedStorageWorkletService::AddModuleCallback callback);
 
   void OnModuleScriptDownloaded(
       mojom::SharedStorageWorkletServiceClient* client,
+      content::mojom::PrivateAggregationHost* private_aggregation_host,
       const GURL& script_source_url,
       mojom::SharedStorageWorkletService::AddModuleCallback callback,
       std::unique_ptr<std::string> response_body,
@@ -76,6 +80,7 @@ class CONTENT_EXPORT SharedStorageWorkletGlobalScope {
   v8::Global<v8::Context> global_context_;
 
   std::unique_ptr<Console> console_;
+  std::unique_ptr<PrivateAggregation> private_aggregation_;
   std::unique_ptr<SharedStorage> shared_storage_;
 
   std::unique_ptr<UrlSelectionOperationHandler>
