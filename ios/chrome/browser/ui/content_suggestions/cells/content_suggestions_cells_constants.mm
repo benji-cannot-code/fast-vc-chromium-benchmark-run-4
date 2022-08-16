ece82f2156035a54f40b74ac0e5c8597abaf676b
Jeevan Shikaram <jshikaram@chromium.org>
2022-08-16 01:37:35 +0000

    Revert "[ios] Fix tests for kTrendingQueriesModule"
    This reverts commit 8eea7a0bd9b8839ad75284147e153d3ed68a5a93.
    
    Reason for revert: Suspected CL for failing tests on ios-simulator-noncq.
    
    Example build: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/27943/overview
    
    Original change's description:
    > [ios] Fix tests for kTrendingQueriesModule
    >
    > Forces a particular experiment arm that doesn't hide Shortcuts. Also,
    > a test is added to validate the Trending Queries module.
    >
    > Bug: 1350826, 1352158
    > Change-Id: Ie44481ae884018361e3916d793fe0b444068c269
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817208
    > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035246}
    
    Bug: 1350826, 1352158
    Change-Id: Ib93f50a1ebd6c21749f737dabc23780e6c67cd8b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828215
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Owners-Override: Jeevan Shikaram <jshikaram@chromium.org>
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1035322}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.mm
@@ -13,6 +13,3 @@ const CGSize kReturnToRecentTabSize = {/*width=*/343, /*height=*/72};
 
 NSString* const kContentSuggestionsWhatsNewIdentifier =
     @"ContentSuggestionsWhatsNewIdentifier";
-
-NSString* const kQuerySuggestionViewA11yIdentifierPrefix =
-    @"QuerySuggestionViewA11yIdentifierPrefix";
