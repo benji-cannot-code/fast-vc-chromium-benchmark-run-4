da24619eaed5ec274240152073892dddd0e9cfeb
Maxim Kolosovskiy <kolos@chromium.org>
2022-08-16 14:54:37 +0000

    Revert "OOBE: Include ConsolidatedConsent screen in OOBE metrics"
    This reverts commit ee54fadeb7edb490620931e72f13620901d39297.
    
    Reason for revert: I suspect this CL causes failures 
    TrayAccessibilityLoginTestInstance/TrayAccessibilityLoginTest.ShowMenuWithShowOnLoginScreen/1
    SystemTrayClientEnterpriseAccountTest.TrayEnterpriseManagedAccount
    TrayAccessibilityLoginTestInstance/TrayAccessibilityLoginTest.ShowMenuWithShowOnLoginScreen/0
    SystemTrayClientEnterpriseAccountTest.TrayEnterpriseUnmanagedAccount
    ...2 more failure(s) (6 total)...
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/29843/overview
    
    Original change's description:
    > OOBE: Include ConsolidatedConsent screen in OOBE metrics
    >
    > Include Consolidated Consent Screen in the following UMA histograms:
    >  - OOBE.StepShownStatus
    >  - OOBE.StepCompletionTime
    >  - OOBE.StepCompletionTimeByExitReason
    >
    > Bug: b/242679991
    > Change-Id: Ia4bc7aa08be9561341c8df63e716614806273d3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832661
    > Reviewed-by: Roman Sorokin <rsorokin@google.com>
    > Commit-Queue: Osama Fathy <osamafathy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1035452}
    
    Bug: b/242679991
    Change-Id: Ib1e1d767d23d6cfee23268dc3cc7cf6e03e40d3f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834243
    Owners-Override: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035508}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time_to_iso8601.h"
 #include "base/timer/timer.h"
@@ -281,7 +280,6 @@ class DemoSetupTestBase : public OobeBaseTest {
 
  protected:
   test::EnrollmentHelperMixin enrollment_helper_{&mixin_host_};
-  base::HistogramTester histogram_tester_;
 
  private:
   // TODO(agawronska): Maybe create a separate test fixture for offline setup.
@@ -507,17 +505,6 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   test::OobeJS().ClickOnPath(kDemoPreferencesNext);
 
   AcceptTermsAndExpectDemoSetupProgress();
-
-  if (chromeos::features::IsOobeConsolidatedConsentEnabled()) {
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepCompletionTime.Consolidated-consent", 1);
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepShownStatus.Consolidated-consent", 1);
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepCompletionTimeByExitReason.Consolidated-consent."
-        "AcceptedDemo",
-        1);
-  }
 
   // Verify the email corresponds to US.
   EXPECT_EQ("admin-us@cros-demo-mode.com",
@@ -768,14 +755,6 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest, DISABLED_BackOnTermsScreen) {
     test::OobeJS().ClickOnPath(kDemoPreferencesNext);
     test::WaitForConsolidatedConsentScreen();
     test::OobeJS().ClickOnPath(kCCBackButton);
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepCompletionTime.Consolidated-consent", 1);
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepShownStatus.Consolidated-consent", 1);
-    histogram_tester_.ExpectTotalCount(
-        "OOBE.StepCompletionTimeByExitReason.Consolidated-consent."
-        "BackDemo",
-        1);
   } else {
     // User cannot go to ARC ToS screen without accepting eula - simulate that.
     StartupUtils::MarkEulaAccepted();
