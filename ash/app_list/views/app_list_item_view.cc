3f5fa660191e4a9f23d4c487db47d70b6668c2ea
Elliot Tuck <etuck@chromium.org>
2022-08-16 22:26:58 +0000

    launcher: Show dragging mouse pointer when performing app drag
    Fixed: 1251586
    Test: Manually, added unit tests.
    Change-Id: I71413b802890ab7640fec654bb66233663b88451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811458
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035733}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/focus/focus_manager.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 
@@ -415,13 +416,17 @@ void AppListItemView::SetUIState(UIState ui_state) {
   switch (ui_state) {
     case UI_STATE_NORMAL:
       title_->SetVisible(true);
-      if (ui_state_ == UI_STATE_DRAGGING)
+      if (ui_state_ == UI_STATE_DRAGGING) {
+        GetWidget()->SetCursor(ui::mojom::CursorType::kNull);
         ScaleAppIcon(false);
+      }
       break;
     case UI_STATE_DRAGGING:
       title_->SetVisible(false);
-      if (ui_state_ == UI_STATE_NORMAL && !in_cardified_grid_)
+      if (ui_state_ == UI_STATE_NORMAL && !in_cardified_grid_) {
+        GetWidget()->SetCursor(ui::mojom::CursorType::kGrabbing);
         ScaleAppIcon(true);
+      }
       break;
     case UI_STATE_DROPPING_IN_FOLDER:
       break;
