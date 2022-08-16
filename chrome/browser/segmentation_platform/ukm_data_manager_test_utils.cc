ff0cf1676e4408b789c942e17645b3f66aead52e
Siddhartha S <ssid@chromium.org>
2022-08-16 18:41:01 +0000

    Move model provider factory to embedder to share with iOS
    The model provider factory has code that is common for iOS and other
    platforms. This object depends on optimization_guide and cannot be
    inside the platform internals code. So, move it to embedder dir where
    this code can be shared.
    Ensure the default models are passed the same way in chrome and ios.
    
    BUG=1346389
    
    Change-Id: I3d12297f92b09635c2c7166fad9e05a9fcd5d5aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830821
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035609}

--- chrome/browser/segmentation_platform/ukm_data_manager_test_utils.cc
+++ chrome/browser/segmentation_platform/ukm_data_manager_test_utils.cc
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/segmentation_platform/ukm_data_manager_test_utils.h"
 
 #include "base/run_loop.h"
-#include "chrome/browser/segmentation_platform/model_provider_factory_impl.h"
 #include "chrome/browser/segmentation_platform/segmentation_platform_config.h"
 #include "chrome/browser/segmentation_platform/ukm_database_client.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/segmentation_platform/embedder/model_provider_factory_impl.h"
 #include "components/segmentation_platform/internal/database/ukm_database.h"
 #include "components/segmentation_platform/internal/execution/mock_model_provider.h"
 #include "components/segmentation_platform/internal/execution/model_execution_manager_impl.h"
