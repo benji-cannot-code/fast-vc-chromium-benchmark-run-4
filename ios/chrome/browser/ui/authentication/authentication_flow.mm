46c18de0072dbace53036834bcac354582468099
Jérôme Lebel <jlebel@chromium.org>
2022-08-16 20:20:46 +0000

    [iOS] Cleanup AuthenticationFlow
    Removes _shouldSignIn that is always sets to YES.
    Removes _shouldCommitSync, and replaced with
    _postSignInAction == POST_SIGNIN_ACTION_COMMIT_SYNC
    
    To understand all the steps, I created:
    https://graphviz.corp.google.com/#a881b36e57b502d447fe27493d93afc9
    
    Change-Id: Ifa622c0e0582eb1fa14b914d71c027a30df007ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829278
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035666}

--- ios/chrome/browser/ui/authentication/authentication_flow.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow.mm
@@ -99,12 +99,10 @@ enum AuthenticationState {
   AuthenticationState _state;
   BOOL _didSignIn;
   BOOL _failedOrCancelled;
-  BOOL _shouldSignIn;
   BOOL _shouldSignOut;
   // YES if the signed in account is a managed account and the sign-in flow
   // includes sync.
   BOOL _shouldShowManagedConfirmation;
-  BOOL _shouldCommitSync;
   // YES if user policies have to be fetched.
   BOOL _shouldFetchUserPolicy;
 
@@ -215,10 +213,7 @@ enum AuthenticationState {
     case BEGIN:
       return CHECK_SIGNIN_STEPS;
     case CHECK_SIGNIN_STEPS:
-      if (_shouldSignIn)
-        return FETCH_MANAGED_STATUS;
-      else
-        return CHECK_MERGE_CASE;
+      return FETCH_MANAGED_STATUS;
     case FETCH_MANAGED_STATUS:
       return CHECK_MERGE_CASE;
     case CHECK_MERGE_CASE:
@@ -232,19 +227,15 @@ enum AuthenticationState {
         return SIGN_OUT_IF_NEEDED;
       else if (self.localDataClearingStrategy == SHOULD_CLEAR_DATA_CLEAR_DATA)
         return CLEAR_DATA;
-      else if (_shouldSignIn)
-        return SIGN_IN;
       else
-        return COMPLETE_WITH_SUCCESS;
+        return SIGN_IN;
     case SHOW_MANAGED_CONFIRMATION:
       if (_shouldSignOut)
         return SIGN_OUT_IF_NEEDED;
       else if (self.localDataClearingStrategy == SHOULD_CLEAR_DATA_CLEAR_DATA)
         return CLEAR_DATA;
-      else if (_shouldSignIn)
-        return SIGN_IN;
       else
-        return COMPLETE_WITH_SUCCESS;
+        return SIGN_IN;
     case SIGN_OUT_IF_NEEDED:
       return self.localDataClearingStrategy == SHOULD_CLEAR_DATA_CLEAR_DATA
                  ? CLEAR_DATA
@@ -252,10 +243,12 @@ enum AuthenticationState {
     case CLEAR_DATA:
       return SIGN_IN;
     case SIGN_IN:
-      if (_shouldCommitSync)
-        return COMMIT_SYNC;
-      else
-        return COMPLETE_WITH_SUCCESS;
+      switch (_postSignInAction) {
+        case POST_SIGNIN_ACTION_COMMIT_SYNC:
+          return COMMIT_SYNC;
+        case POST_SIGNIN_ACTION_NONE:
+          return COMPLETE_WITH_SUCCESS;
+      }
     case COMMIT_SYNC:
       if (policy::IsUserPolicyEnabled() && _shouldFetchUserPolicy)
         return REGISTER_FOR_USER_POLICY;
@@ -417,8 +410,6 @@ enum AuthenticationState {
     // sign-out is required.
     _shouldSignOut = YES;
   }
-  _shouldSignIn = YES;
-  _shouldCommitSync = _postSignInAction == POST_SIGNIN_ACTION_COMMIT_SYNC;
 }
 
 - (void)signInIdentity:(ChromeIdentity*)identity {
@@ -457,7 +448,7 @@ enum AuthenticationState {
     bool isManagedAccount = _identityToSignInHostedDomain.length > 0;
     signin_metrics::RecordSigninAccountType(signin::ConsentLevel::kSignin,
                                             isManagedAccount);
-    if (_shouldCommitSync)
+    if (_postSignInAction == POST_SIGNIN_ACTION_COMMIT_SYNC)
       signin_metrics::RecordSigninAccountType(signin::ConsentLevel::kSync,
                                               isManagedAccount);
   }
