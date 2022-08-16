d231474aca32cbcba9fffef53eca4edf20b7150e
Kevin Marshall <kmarshall@chromium.org>
2022-08-16 23:22:15 +0000

    [blink] Use Ahem font to improve Blink unit tests' hermeticity.
    Some Blink unit tests sourced fonts from the platform, which broke
    test isolation. This CL switches the tests to use the test font "Ahem"
    and updates the test expectations accordingly.
    
    List of broken tests:
     - WebFrameTest.DivScrollIntoEditablePreservePageScaleTest
     - All/SelectionBoundsRecorderTest.SelectMultiline/LegacyLayout
     - All/SelectionBoundsRecorderTest.SelectMultiline/LayoutNG
     - WebFrameTest.DivScrollIntoEditableTest
    
    Bug: 1352956
    Change-Id: Ie695bdd236fd3c63152e94b0284d9a4fe91686ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832293
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035769}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -4166,6 +4166,7 @@ TEST_F(WebFrameTest, DontZoomInOnFocusedInTouchAction) {
 }
 
 TEST_F(WebFrameTest, DivScrollIntoEditableTest) {
+  RegisterMockedHttpURLLoad("Ahem.ttf");
   RegisterMockedHttpURLLoad("get_scale_for_zoom_into_editable_test.html");
 
   const bool kAutoZoomToLegibleScale = true;
