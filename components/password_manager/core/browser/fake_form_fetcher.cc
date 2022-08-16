31077f4ee530b3fcb2eb80272e8f6748bf600961
Jan Keitel <jkeitel@google.com>
2022-08-16 19:23:11 +0000

    Create separate copies of PasswordForms instead of keeping raw pointers.
    This CL avoids maintaining a set of raw pointers to PasswordForms and instead keeps separate copies. While this may be slightly less
    performant, it reduces the risk of those pointers going stale during
    future refactorings.
    
    Bug: 1351969
    Change-Id: Iabcde87cf022bcb61c2dfcd690e99cd108602571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826135
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035634}

--- components/password_manager/core/browser/fake_form_fetcher.cc
+++ components/password_manager/core/browser/fake_form_fetcher.cc
@@ -39,8 +39,8 @@ const std::vector<InteractionsStats>& FakeFormFetcher::GetInteractionsStats()
   return stats_;
 }
 
-const std::vector<const PasswordForm*>&
-FakeFormFetcher::GetInsecureCredentials() const {
+std::vector<const PasswordForm*> FakeFormFetcher::GetInsecureCredentials()
+    const {
   return insecure_credentials_;
 }
 
--- components/password_manager/core/browser/form_fetcher_impl_unittest.cc
+++ components/password_manager/core/browser/form_fetcher_impl_unittest.cc
@@ -897,6 +897,8 @@ TEST_P(FormFetcherImplTest, Clone_NonEmptyResults) {
   Fetch();
   PasswordForm non_federated = CreateNonFederated();
   PasswordForm federated = CreateFederated();
+  federated.password_issues.insert(
+      {InsecureType::kLeaked, InsecurityMetadata()});
   PasswordForm android_federated = CreateAndroidFederated();
   std::vector<std::unique_ptr<PasswordForm>> results;
   results.push_back(std::make_unique<PasswordForm>(non_federated));
@@ -910,6 +912,8 @@ TEST_P(FormFetcherImplTest, Clone_NonEmptyResults) {
   EXPECT_THAT(
       form_fetcher_->GetFederatedMatches(),
       UnorderedElementsAre(Pointee(federated), Pointee(android_federated)));
+  EXPECT_THAT(form_fetcher_->GetInsecureCredentials(),
+              UnorderedElementsAre(Pointee(federated)));
   EXPECT_FALSE(form_fetcher_->IsBlocklisted());
 
   ASSERT_TRUE(
@@ -930,6 +934,8 @@ TEST_P(FormFetcherImplTest, Clone_NonEmptyResults) {
   EXPECT_THAT(
       clone->GetFederatedMatches(),
       UnorderedElementsAre(Pointee(federated), Pointee(android_federated)));
+  EXPECT_THAT(clone->GetInsecureCredentials(),
+              UnorderedElementsAre(Pointee(federated)));
   MockConsumer consumer;
   EXPECT_CALL(consumer, OnFetchCompleted);
   clone->AddConsumer(&consumer);
