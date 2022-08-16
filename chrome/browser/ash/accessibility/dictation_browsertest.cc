b51c6541737dee68e921b7d9194b8d64b5689c60
Akihiro Ota <akihiroota@chromium.org>
2022-08-16 16:56:14 +0000

    Dictation: Add commands test in Japanese
    AX-Relnotes: N/A
    Bug: 1353368
    Change-Id: I9a6c7ff7db80a311c8d0129247db0915805edbbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832640
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035569}

--- chrome/browser/ash/accessibility/dictation_browsertest.cc
+++ chrome/browser/ash/accessibility/dictation_browsertest.cc
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/accessibility/accessibility_test_utils.h"
 #include "chrome/browser/ash/accessibility/dictation_bubble_test_helper.h"
 #include "chrome/browser/ash/accessibility/speech_monitor.h"
+#include "chrome/browser/ash/base/locale_util.h"
 #include "chrome/browser/ash/input_method/textinput_test_helper.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/speech/speech_recognition_constants.h"
 #include "chrome/browser/speech/speech_recognition_test_helper.h"
@@ -759,30 +761,34 @@ IN_PROC_BROWSER_TEST_P(DictationTest, SmartCapitalizationWithComma) {
   WaitForRecognitionStopped();
 }
 
-// Tests the behavior of Dictation in other languages.
-class DictationI18NTest : public DictationTestBase {
+// Tests the behavior of Dictation in Japanese.
+class DictationJaTest : public DictationTestBase {
  public:
-  DictationI18NTest() = default;
-  ~DictationI18NTest() override = default;
-  DictationI18NTest(const DictationI18NTest&) = delete;
-  DictationI18NTest& operator=(const DictationI18NTest&) = delete;
+  DictationJaTest() = default;
+  ~DictationJaTest() override = default;
+  DictationJaTest(const DictationJaTest&) = delete;
+  DictationJaTest& operator=(const DictationJaTest&) = delete;
 
  protected:
   void SetUpOnMainThread() override {
-    GetActiveUserPrefs()->SetString(prefs::kAccessibilityDictationLocale,
-                                    "ja-JP");
+    locale_util::SwitchLanguage("ja", /*enable_locale_keyboard_layouts=*/true,
+                                /*login_layouts_only*/ false, base::DoNothing(),
+                                browser()->profile());
+    g_browser_process->SetApplicationLocale("ja");
+    GetActiveUserPrefs()->SetString(prefs::kAccessibilityDictationLocale, "ja");
+
     DictationTestBase::SetUpOnMainThread();
   }
 };
 
 // On-device speech recognition is currently limited to en-US, so
-// DictationI18NTest should use network speech recognition only.
+// DictationJaTest should use network speech recognition only.
 INSTANTIATE_TEST_SUITE_P(
     Network,
-    DictationI18NTest,
+    DictationJaTest,
     ::testing::Values(speech::SpeechRecognitionType::kNetwork));
 
-IN_PROC_BROWSER_TEST_P(DictationI18NTest, NoSmartSpacingOrCapitalization) {
+IN_PROC_BROWSER_TEST_P(DictationJaTest, NoSmartSpacingOrCapitalization) {
   ToggleDictationWithKeystroke();
   WaitForRecognitionStarted();
   SendFinalResultAndWaitForTextAreaValue("this", "this");
@@ -792,10 +798,21 @@ IN_PROC_BROWSER_TEST_P(DictationI18NTest, NoSmartSpacingOrCapitalization) {
   WaitForRecognitionStopped();
 }
 
-IN_PROC_BROWSER_TEST_P(DictationI18NTest, DictatesNonAsciiCharacters) {
+IN_PROC_BROWSER_TEST_P(DictationJaTest, CanDictate) {
   ToggleDictationWithKeystroke();
   WaitForRecognitionStarted();
   SendFinalResultAndWaitForTextAreaValue("テニス", "テニス");
+  ToggleDictationWithKeystroke();
+  WaitForRecognitionStopped();
+}
+
+IN_PROC_BROWSER_TEST_P(DictationJaTest, DeleteCharacter) {
+  ToggleDictationWithKeystroke();
+  WaitForRecognitionStarted();
+  // Dictate something.
+  SendFinalResultAndWaitForTextAreaValue("テニス", "テニス");
+  // Perform the 'delete' command.
+  SendFinalResultAndWaitForTextAreaValue("削除", "テニ");
   ToggleDictationWithKeystroke();
   WaitForRecognitionStopped();
 }
