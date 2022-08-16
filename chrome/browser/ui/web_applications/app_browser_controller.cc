1e6e672b86d9757b42e94871274b55203f2b06b6
Alan Cutter <alancutter@chromium.org>
2022-08-16 01:49:30 +0000

    Fix info bar background color in web app windows
    This CL makes info bars in web app windows use the same color scheme
    as the CCT bar (CustomTabBarView). This uses the OS dark mode colors
    rather than using the app's theme/background color.
    
    Screenshots:
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=565655&signed_aid=igpBAXOBi3xPWaP58V0ZlA==&inline=1
    After (light mode): https://bugs.chromium.org/p/chromium/issues/attachment?aid=565654&signed_aid=tQ1l_k6hFgnAdILPXWlwyA==&inline=1
    After (dark mode): https://bugs.chromium.org/p/chromium/issues/attachment?aid=565653&signed_aid=6bbimCNfRS_2bmmqom94Xw==&inline=1
    
    Bug: 1351610
    Change-Id: I9be3ac0cca0693b2afae1e22fe1f7ee76a4a903c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829026
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035326}

--- chrome/browser/ui/web_applications/app_browser_controller.cc
+++ chrome/browser/ui/web_applications/app_browser_controller.cc
@@ -490,6 +490,9 @@ void AppBrowserController::AddColorMixers(
       ui::SetAlpha(kColorPwaToolbarButtonIcon, gfx::kDisabledControlAlpha);
   if (bg_color)
     mixer[kColorWebContentsBackground] = {kColorPwaBackground};
+
+  mixer[kColorInfoBarBackground] = {kColorPwaToolbarBackground};
+  mixer[kColorInfoBarForeground] = {kColorPwaToolbarButtonIcon};
 }
 
 void AppBrowserController::OnReceivedInitialURL() {
