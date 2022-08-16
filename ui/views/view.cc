69a988dbf595c64dbeceeb6214235a52bc2685a0
Emily Shack <emshack@chromium.org>
2022-08-16 19:42:26 +0000

    Revert "Hide tooltip when view becomes hidden"
    This reverts commit b8c159f60c033f4bf8e09a88c48efd972de11705.
    
    Reason for revert: Likely causing builder failure https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/58940/overview
    
    Original change's description:
    > Hide tooltip when view becomes hidden
    >
    > Bug: 926847
    > Change-Id: I49ba5aa0d39b7b6ed343dcf73d45979b02cb73b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828370
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Fr <beaufort.francois@gmail.com>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035574}
    
    Bug: 926847
    Change-Id: I1b7c573bf823d83e772f786b756c9e7e5a91a12e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Owners-Override: Emily Shack <emshack@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833525
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1035641}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -608,9 +608,6 @@ void View::SetVisible(bool visible) {
 
     // Notify all other subscriptions of the change.
     OnPropertyChanged(&visible_, kPropertyEffectsPaint);
-
-    if (was_visible)
-      UpdateTooltip();
   }
 
   if (parent_) {
