0c085c01dc5da43c895b7aa19daa7c1e26bac8e7
Viktor Semeniuk <vsemeniuk@google.com>
2022-08-16 14:00:19 +0000

    [Affiliation service] Using KeepPrefetchForFacets instead of N Prefetches
    KeepPrefetchForFacets was implemented for OnLoginsRetained, but can be
    used here as well.
    
    Bug: 1352457
    Change-Id: I2f61d8b90efd38aed3ba872a09c158f633963962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828903
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035486}

--- components/password_manager/core/browser/android_affiliation/affiliated_match_helper_unittest.cc
+++ components/password_manager/core/browser/android_affiliation/affiliated_match_helper_unittest.cc
@@ -263,6 +263,28 @@ class AffiliatedMatchHelperTest : public testing::Test,
     }
   }
 
+  void ExpectKeepPrefetchForTestLogins() {
+    std::vector<FacetURI> expected_facets;
+    expected_facets.push_back(
+        FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3));
+    expected_facets.push_back(
+        FacetURI::FromCanonicalSpec(kTestAndroidFacetURIBeta2));
+    expected_facets.push_back(
+        FacetURI::FromCanonicalSpec(kTestAndroidFacetURIGamma));
+    expected_facets.push_back(
+        FacetURI::FromCanonicalSpec(kTestAndroidFacetURIBeta3));
+
+    if (base::FeatureList::IsEnabled(
+            features::kFillingAcrossAffiliatedWebsites)) {
+      expected_facets.push_back(
+          FacetURI::FromCanonicalSpec(kTestWebFacetURIAlpha1));
+      expected_facets.push_back(
+          FacetURI::FromCanonicalSpec(kTestWebFacetURIAlpha2));
+    }
+
+    mock_affiliation_service()->ExpectKeepPrefetchForFacets(expected_facets);
+  }
+
   void ExpectCancelPrefetchForTestLogins() {
     mock_affiliation_service()->ExpectCallToCancelPrefetch(
         kTestAndroidFacetURIAlpha3);
@@ -443,7 +465,7 @@ TEST_P(
   AddAndroidAndNonAndroidTestLogins();
   RunUntilIdle();
 
-  ExpectPrefetchForTestLogins();
+  ExpectKeepPrefetchForTestLogins();
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
 }
 
@@ -458,7 +480,7 @@ TEST_P(AffiliatedMatchHelperTest,
 
   AddAndroidAndNonAndroidTestLogins();
 
-  ExpectPrefetchForTestLogins();
+  ExpectKeepPrefetchForTestLogins();
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
 }
 
@@ -466,6 +488,7 @@ TEST_P(AffiliatedMatchHelperTest,
 // Verifies that corresponding affiliation information gets prefetched.
 TEST_P(AffiliatedMatchHelperTest,
        PrefetchAffiliationsForAndroidCredentialsAddedAfterInitialization) {
+  mock_affiliation_service()->ExpectKeepPrefetchForFacets({});
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
 
   ExpectPrefetchForTestLogins();
@@ -476,7 +499,7 @@ TEST_P(AffiliatedMatchHelperTest,
        CancelPrefetchingAffiliationsAndBrandingForRemovedAndroidCredentials) {
   AddAndroidAndNonAndroidTestLogins();
 
-  ExpectPrefetchForTestLogins();
+  ExpectKeepPrefetchForTestLogins();
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
 
   ExpectCancelPrefetchForTestLogins();
@@ -493,7 +516,7 @@ TEST_P(AffiliatedMatchHelperTest,
 TEST_P(AffiliatedMatchHelperTest, PrefetchBeforeTrimForPrimaryKeyUpdates) {
   AddAndroidAndNonAndroidTestLogins();
 
-  ExpectPrefetchForTestLogins();
+  ExpectKeepPrefetchForTestLogins();
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
 
   mock_affiliation_service()->ExpectCallToCancelPrefetch(
@@ -517,10 +540,10 @@ TEST_P(AffiliatedMatchHelperTest, PrefetchBeforeTrimForPrimaryKeyUpdates) {
 // expects that Prefetch() and CancelPrefetch() will each be called four times.
 TEST_P(AffiliatedMatchHelperTest,
        DuplicateCredentialsArePrefetchWithMultiplicity) {
-  EXPECT_CALL(*mock_affiliation_service(),
-              Prefetch(FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3),
-                       base::Time::Max()))
-      .Times(4);
+  mock_affiliation_service()->ExpectKeepPrefetchForFacets(
+      {FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3),
+       FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3),
+       FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3)});
 
   PasswordForm android_form(GetTestAndroidCredentials(kTestAndroidRealmAlpha3));
   password_store()->AddLogin(android_form);
@@ -540,6 +563,11 @@ TEST_P(AffiliatedMatchHelperTest,
   password_store()->AddLogin(android_form3);
 
   ASSERT_NO_FATAL_FAILURE(RunDeferredInitialization());
+
+  EXPECT_CALL(*mock_affiliation_service(),
+              Prefetch(FacetURI::FromCanonicalSpec(kTestAndroidFacetURIAlpha3),
+                       base::Time::Max()))
+      .Times(1);
 
   // Store one credential after deferred initialization.
   PasswordForm android_form4(android_form);
