183f693747face6ce9ced7335eda229e1d1db732
Roland Bock <rbock@google.com>
2022-08-16 18:56:38 +0000

    Refactor browser/speech to use PrefService::GetValueDict
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341996
    Test: Existing unit tests
    Change-Id: I28b3421f0828075a6f28ab57fb467249973576d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830019
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035624}

--- chrome/browser/speech/tts_controller_delegate_impl.h
+++ chrome/browser/speech/tts_controller_delegate_impl.h
@@ -8,13 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/memory/singleton.h"
+#include "base/values.h"
 #include "content/public/browser/tts_controller_delegate.h"
 
 class PrefService;
-
-namespace base {
-class Value;
-}
 
 // Singleton class that manages Chrome side logic for TTS and TTS engine
 // extension APIs. This is only used on ChromeOS.
@@ -45,7 +42,7 @@ class TtsControllerDelegateImpl : public content::TtsControllerDelegate {
 
   virtual const PrefService* GetPrefService(content::TtsUtterance* utterance);
 
-  const base::Value* GetLangToVoicePref(content::TtsUtterance* utterance);
+  const base::Value::Dict* GetLangToVoicePref(content::TtsUtterance* utterance);
 
   friend struct base::DefaultSingletonTraits<TtsControllerDelegateImpl>;
 };
