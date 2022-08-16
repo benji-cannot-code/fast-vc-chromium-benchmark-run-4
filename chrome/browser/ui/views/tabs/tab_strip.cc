80b5e6b81ee45ff5eab73014ab471421c44cd23c
David Pennington <dpenning@chromium.org>
2022-08-16 01:16:03 +0000

    move TabActivationTypes to tab_enums
    Change-Id: I2a5a364c4e27811f5314b25030d96b1073ec9e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810983
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035313}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1404,7 +1404,7 @@ void TabStrip::SelectTab(Tab* tab, const ui::Event& event) {
       base::UmaHistogramSparse("Tabs.DesktopTabOffsetFromRightOfSwitch",
                                GetModelCount() - model_index - 1);
       base::UmaHistogramEnumeration("TabStrip.Tab.Views.ActivationAction",
-                                    TabStripModel::TabActivationTypes::kTab);
+                                    TabActivationTypes::kTab);
 
       if (tab->group().has_value()) {
         base::RecordAction(
