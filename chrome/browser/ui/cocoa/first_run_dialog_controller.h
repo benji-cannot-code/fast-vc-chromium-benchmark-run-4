f00c21cb7336163587eea67f393a58d2fd420955
Elly Fong-Jones <ellyjones@chromium.org>
2022-08-16 21:01:56 +0000

    first-run: allow setting canary as default browser
    This has been requested for a long time, but we couldn't previously
    allow it because of lack of side-by-side install support. This change
    both allows setting canary as default, and removes the code paths from
    the first run dialog for hiding the first run prompt.
    
    Fixed: 1350264
    Change-Id: Icc0a3e61c2f48cb79dc99fcac091c9434a0af878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829489
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035691}

--- chrome/browser/ui/cocoa/first_run_dialog_controller.h
+++ chrome/browser/ui/cocoa/first_run_dialog_controller.h
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // dialog's content view.
 @interface FirstRunDialogViewController : NSViewController
 
-- (instancetype)initWithStatsCheckboxInitiallyChecked:(BOOL)checked
-                        defaultBrowserCheckboxVisible:(BOOL)visible;
+- (instancetype)initWithStatsCheckboxInitiallyChecked:(BOOL)checked;
 
 - (NSString*)windowTitle;
 
