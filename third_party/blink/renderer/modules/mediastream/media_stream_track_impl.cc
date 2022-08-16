96e0deb98a2ff1aaac187a4a202b5267c7dcbb7c
Tony Herre <toprice@chromium.org>
2022-08-16 12:16:11 +0000

    Have MSC sync its own enabled state with its platform_track
    Remove the odd DidSetMediaStreamTrackEnabled() MSTImpl method, instead
    making the MediaStreamComponent do its own syncing of enabled state with
    its platform track.
    
    Bug: 1302689, 1288839
    Change-Id: Ib8384739fbe13da0442d9dd38a84b3371c6dafcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832216
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035462}

--- third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track_impl.cc
@@ -186,13 +186,6 @@ std::unique_ptr<MediaStreamVideoTrack> CloneNativeVideoMediaStreamTrack(
       MediaStreamVideoSource::ConstraintsOnceCallback(), original->Enabled());
 }
 
-void DidSetMediaStreamTrackEnabled(MediaStreamComponent* component) {
-  auto* native_track =
-      MediaStreamTrackPlatform::GetTrack(WebMediaStreamTrack(component));
-  if (native_track)
-    native_track->SetEnabled(component->Enabled());
-}
-
 void DidCloneMediaStreamTrack(MediaStreamComponent* clone) {
   DCHECK(clone);
   DCHECK(clone->Source());
@@ -342,11 +335,6 @@ void MediaStreamTrackImpl::setEnabled(bool enabled) {
 
   SendLogMessage(
       String::Format("%s({enabled=%s})", __func__, enabled ? "true" : "false"));
-
-  if (Ended())
-    return;
-
-  DidSetMediaStreamTrackEnabled(component_.Get());
 }
 
 bool MediaStreamTrackImpl::muted() const {
