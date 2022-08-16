ae056dcb963d9cd8a68ed02c8410d51cf6ccbfc3
Adem Derinel <derinel@google.com>
2022-08-16 14:14:24 +0000

    Settings: Password View page uses requestCredentialDetails
    This CL introduces requestCredentialDetails API to password view page. The API is used to retrieve the full credential when:
    - the user clicks to a password list item in chrome://settings/passwords
    - opens the view page with URL chrome://settings/passwords/view?id=xy
    
    MergePasswordsStoreCopiesMixin is not required in the page anymore.
    
    Bug: 1345899
    Change-Id: I253497da6399e34abacf631a787df73dc854a015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792013
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035492}

--- chrome/test/data/webui/settings/test_password_manager_proxy.ts
+++ chrome/test/data/webui/settings/test_password_manager_proxy.ts
@@ -93,6 +93,8 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
   private getUrlCollectionResponse_: chrome.passwordsPrivate.UrlCollection|
       null = null;
   private changeSavedPasswordResponse_: number|null = null;
+  private requestCredentialDetailsResponse_:
+      chrome.passwordsPrivate.PasswordUiEntry|null = null;
 
   constructor() {
     super([
@@ -117,6 +119,7 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
       'removeSavedPassword',
       'requestExportProgressStatus',
       'requestPlaintextPassword',
+      'requestCredentialDetails',
       'startAutomatedPasswordChange',
       'startBulkPasswordCheck',
       'stopBulkPasswordCheck',
@@ -207,6 +210,19 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
 
   setPlaintextPassword(plaintextPassword: string) {
     this.plaintextPassword_ = plaintextPassword;
+  }
+
+  requestCredentialDetails(id: number) {
+    this.methodCalled('requestCredentialDetails', {id});
+    if (!this.requestCredentialDetailsResponse_) {
+      return Promise.reject(new Error('Could not obtain credential details'));
+    }
+    return Promise.resolve(this.requestCredentialDetailsResponse_);
+  }
+
+  setRequestCredentialDetailsResponse(
+      credential: chrome.passwordsPrivate.PasswordUiEntry) {
+    this.requestCredentialDetailsResponse_ = credential;
   }
 
   // Sets the return value of isOptedInForAccountStorage calls and notifies
