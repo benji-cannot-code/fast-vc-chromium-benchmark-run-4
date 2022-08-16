901af7ab6b515cb8006174bd6d8cc73d27956002
Roland Bock <rbock@google.com>
2022-08-16 21:05:19 +0000

    Refactor TopSitesImpl to use PrefService::GetValueDict
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341996
    Test: Existing unit tests
    Change-Id: I431147b502613f40106c8c769199c6129ea287e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829232
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035694}

--- components/history/core/browser/top_sites_impl.cc
+++ components/history/core/browser/top_sites_impl.cc
@@ -186,9 +186,7 @@ void TopSitesImpl::SyncWithHistory() {
 }
 
 bool TopSitesImpl::HasBlockedUrls() const {
-  const base::Value* blocked_urls =
-      pref_service_->GetDictionary(kBlockedUrlsPrefsKey);
-  return blocked_urls && !blocked_urls->DictEmpty();
+  return !pref_service_->GetValueDict(kBlockedUrlsPrefsKey).empty();
 }
 
 void TopSitesImpl::AddBlockedUrl(const GURL& url) {
@@ -217,9 +215,8 @@ void TopSitesImpl::RemoveBlockedUrl(const GURL& url) {
 
 bool TopSitesImpl::IsBlocked(const GURL& url) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  const base::Value* blocked_urls =
-      pref_service_->GetDictionary(kBlockedUrlsPrefsKey);
-  return blocked_urls && blocked_urls->FindKey(GetURLHash(url));
+  return pref_service_->GetValueDict(kBlockedUrlsPrefsKey)
+      .contains(GetURLHash(url));
 }
 
 void TopSitesImpl::ClearBlockedUrls() {
@@ -518,9 +515,8 @@ void TopSitesImpl::SetTopSites(MostVisitedURLList top_sites,
 int TopSitesImpl::num_results_to_request_from_history() const {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  const base::Value* blocked_urls =
-      pref_service_->GetDictionary(kBlockedUrlsPrefsKey);
-  return kTopSitesNumber + (blocked_urls ? blocked_urls->DictSize() : 0);
+  return kTopSitesNumber +
+         pref_service_->GetValueDict(kBlockedUrlsPrefsKey).size();
 }
 
 void TopSitesImpl::MoveStateToLoaded() {
