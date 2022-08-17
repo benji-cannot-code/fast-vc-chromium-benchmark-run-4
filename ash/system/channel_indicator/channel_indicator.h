fdd59f02dfe044442f0074abc2bb9bd0142f5756
Roger Tinkoff <rtinkoff@google.com>
2022-08-17 22:20:42 +0000

    ash: Fix channel indicator for side-shelf
    ChannelIndicatorView's border is now set according to shelf alignment.
    
    Enable sections of ChannelIndicatorView unit test that verify the
    correct image_view() size in both bottom-shelf and side-shelf.
    
    Bug: 1351496
    Change-Id: I02b41f2bf44498e6f4e54250566ffa6dbeead6f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826800
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036316}

--- ash/system/channel_indicator/channel_indicator.h
+++ ash/system/channel_indicator/channel_indicator.h
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_CHANNEL_INDICATOR_CHANNEL_INDICATOR_H_
 
 #include <string>
+
 #include "ash/public/cpp/session/session_observer.h"
+#include "ash/shell.h"
+#include "ash/shell_observer.h"
 #include "ash/system/tray/tray_item_view.h"
 #include "base/memory/weak_ptr.h"
 #include "components/session_manager/session_manager_types.h"
@@ -18,7 +21,8 @@ namespace ash {
 // A view that resides in the system tray, to make it obvious to the user when a
 // device is running on a release track other than "stable."
 class ASH_EXPORT ChannelIndicatorView : public TrayItemView,
-                                        public SessionObserver {
+                                        public SessionObserver,
+                                        public ShellObserver {
  public:
   ChannelIndicatorView(Shelf* shelf, version_info::Channel channel);
   ChannelIndicatorView(const ChannelIndicatorView&) = delete;
@@ -38,6 +42,10 @@ class ASH_EXPORT ChannelIndicatorView : public TrayItemView,
 
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
+
+  // ShellObserver:
+  void OnShelfAlignmentChanged(aura::Window* root_window,
+                               ShelfAlignment old_alignment) override;
 
   // Introspection methods for testing.
   bool IsLabelVisibleForTesting();
@@ -62,6 +70,12 @@ class ASH_EXPORT ChannelIndicatorView : public TrayItemView,
   const version_info::Channel channel_;
 
   ScopedSessionObserver session_observer_;
+
+  base::ScopedObservation<Shell,
+                          ShellObserver,
+                          &Shell::AddShellObserver,
+                          &Shell::RemoveShellObserver>
+      shell_observer_{this};
 
   base::WeakPtrFactory<ChannelIndicatorView> weak_factory_{this};
 };
--- ash/system/channel_indicator/channel_indicator_unittest.cc
+++ ash/system/channel_indicator/channel_indicator_unittest.cc
@@ -66,8 +66,6 @@ INSTANTIATE_TEST_SUITE_P(ChannelValues,
                                            version_info::Channel::DEV,
                                            version_info::Channel::CANARY));
 
-#define SHELF_ALIGNMENT_TESTS_ENABLED (0)
-
 TEST_P(ChannelIndicatorViewTest, Visible) {
   // Local ref.
   ShellDelegate* shell_delegate = Shell::Get()->shell_delegate();
@@ -102,7 +100,6 @@ TEST_P(ChannelIndicatorViewTest, Visible) {
   EXPECT_FALSE(channel_indicator_view->IsLabelVisibleForTesting());
   EXPECT_TRUE(channel_indicator_view->IsImageViewVisibleForTesting());
 
-#if SHELF_ALIGNMENT_TESTS_ENABLED
   // Two shelf alignments to test.
   auto shelf_alignments = {ShelfAlignment::kBottom, ShelfAlignment::kRight};
 
@@ -124,7 +121,6 @@ TEST_P(ChannelIndicatorViewTest, Visible) {
     EXPECT_GE(image_view_bounds.height(),
               kUnifiedTrayChannelIndicatorDimension);
   }
-#endif  // SHELF_ALIGNMENT_TESTS_ENABLED
 
   // User locks the session, view should display text, no image.
   SetSessionState(session_manager::SessionState::LOCKED);
@@ -136,7 +132,6 @@ TEST_P(ChannelIndicatorViewTest, Visible) {
   EXPECT_FALSE(channel_indicator_view->IsLabelVisibleForTesting());
   EXPECT_TRUE(channel_indicator_view->IsImageViewVisibleForTesting());
 
-#if SHELF_ALIGNMENT_TESTS_ENABLED
   // Image is the right size in both alignments.
   for (const auto& alignment : shelf_alignments) {
     // Initiates the shelf alignment change.
@@ -155,7 +150,6 @@ TEST_P(ChannelIndicatorViewTest, Visible) {
     EXPECT_GE(image_view_bounds.height(),
               kUnifiedTrayChannelIndicatorDimension);
   }
-#endif  // SHELF_ALIGNMENT_TESTS_ENABLED
 }
 
 }  // namespace ash
