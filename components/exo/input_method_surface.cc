5b38516a63133a32a2d391d68c08e803f62cc9de
Melissa Zhang <melzhang@chromium.org>
2022-08-17 03:22:20 +0000

    Revert "[exo] Apply min/max constraints to the requested buffer size."
    This reverts commit 9effea89a1edb74d98e82d5381347eda2cd717a5.
    
    Reason for revert: breaks https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/45163/overview
    
    Original change's description:
    > [exo] Apply min/max constraints to the requested buffer size.
    >
    > Currently exo's xdg-shell impl doesn't apply the min/max size
    > constraints when the buffer size is updated. This spec isn't clearly
    > specified, but looks like some X11 apps depends on X11's WM manager
    > oo change its size. This CL updates the size so that it will satisfy
    > the condition.
    >
    > Bug: 1330028, b/207430591
    > Test: Covered by unittests. Also tested using win.c in 133028.
    >
    > Change-Id: I87abe970c54d036d0382f7d6d5a6eacc2bb8d6b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827939
    > Reviewed-by: Chloe Pelling <cpelling@google.com>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035766}
    
    Bug: 1330028, b/207430591
    Change-Id: I68d0225ec7d25b5fe89823e146201ba7b1218ae1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832607
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Owners-Override: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1035874}

--- components/exo/input_method_surface.cc
+++ components/exo/input_method_surface.cc
@@ -87,8 +87,7 @@ void InputMethodSurface::OnSurfaceCommit() {
   }
 }
 
-void InputMethodSurface::SetWidgetBounds(const gfx::Rect& bounds,
-                                         bool adjusted_by_server) {
+void InputMethodSurface::SetWidgetBounds(const gfx::Rect& bounds) {
   if (bounds == widget_->GetWindowBoundsInScreen())
     return;
 
