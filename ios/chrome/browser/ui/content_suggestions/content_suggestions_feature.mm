c9b7c279e7b2cdd734b39b088e8522b8c443259f
Chris Lu <thegreenfrog@chromium.org>
2022-08-17 20:53:12 +0000

    [ios] Only Enable kTrendingQueriesModule if preceding flag is enabled
    kTrendingQueriesModule cannot be enabled without
    kContentSuggestionsUIModuleRefresh also enabled. Thus,
    kTrendingQueriesModule feature code should be executed only when
    kContentSuggestionsUIModuleRefresh is enabled.
    
    Bug: 1348495
    Change-Id: I842cded80283be3a515c119cad1ef393d553eb59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826827
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036267}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
@@ -44,7 +44,8 @@ bool IsContentSuggestionsUIModuleRefreshEnabled() {
 }
 
 bool IsTrendingQueriesModuleEnabled() {
-  return base::FeatureList::IsEnabled(kTrendingQueriesModule) &&
+  return base::FeatureList::IsEnabled(kContentSuggestionsUIModuleRefresh) &&
+         base::FeatureList::IsEnabled(kTrendingQueriesModule) &&
          !ShouldNeverShowTrendingQueriesModule();
 }
 
