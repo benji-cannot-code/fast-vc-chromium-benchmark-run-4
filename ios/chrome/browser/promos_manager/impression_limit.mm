5aa5f7ba5e00147fad1175e793428df7a5b2fc4c
Benjamin Williams <bwwilliams@google.com>
2022-08-17 16:33:13 +0000

    Create Impression Limit class.
    This change creates an Impression Limit class, enabling a standard
    representation for impression limits.
    
    This change is mostly class scaffolding, as the class will grow in
    subsequent CLs.
    
    Bug: 1353123
    Change-Id: Ic4f8c822084df95ff9dba52dfd506082359a4a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806554
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036117}

---
+++ ios/chrome/browser/promos_manager/impression_limit.mm
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/promos_manager/impression_limit.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation ImpressionLimit
+
+- (instancetype)initWithLimit:(NSInteger)numImpressions
+                   forNumDays:(NSInteger)numDays {
+  if (self = [super init]) {
+    _numImpressions = numImpressions;
+    _numDays = numDays;
+  }
+
+  return self;
+}
+
+@end
