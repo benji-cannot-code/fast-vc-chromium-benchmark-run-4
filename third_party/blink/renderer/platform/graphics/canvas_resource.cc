8e357cd9727527f752445c6dad018eec388b374a
Justin Novosad <junov@chromium.org>
2022-08-17 19:19:01 +0000

    Remove unnecessary DCHECK in ExternalCanvasResource dtor
    DCHECK was failing flakily due to non-deterministic object destruction
    order. Upon navigation, the objects of the previous page all get torn
    down.  In such situations, it is possible that a CanvasResource may
    not be able to be destryed on its thread of origin if the thread has
    already been torn down. Ex: A worker.
    
    Bug: 1348616
    Change-Id: I3de2739b8c847ad8876597a9192160e7101d9e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835450
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036222}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -1094,8 +1094,6 @@ scoped_refptr<ExternalCanvasResource> ExternalCanvasResource::Create(
 }
 
 ExternalCanvasResource::~ExternalCanvasResource() {
-  // Should always be destroyed on thread of origin.
-  DCHECK(!is_cross_thread());
   OnDestroy();
 }
 
