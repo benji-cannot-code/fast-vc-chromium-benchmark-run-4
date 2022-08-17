4a8bfaafdf774f85131c96124ccfe4897bd63429
Gauthier Ambard <gambard@chromium.org>
2022-08-17 14:38:02 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/device_sharing
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Bug: 1311955
    Change-Id: Id2402eef49960c33209baec482db6db0701deb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829280
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036053}

--- ios/chrome/browser/device_sharing/handoff_manager_egtest.mm
+++ ios/chrome/browser/device_sharing/handoff_manager_egtest.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Checks that Handoff will report the specified |gurl|.
+// Checks that Handoff will report the specified `gurl`.
 void AssertHandoffURL(const GURL& gurl) {
   NSURL* handoffURL =
       [HandoffManagerAppInterface currentUserActivityWebPageURL];
