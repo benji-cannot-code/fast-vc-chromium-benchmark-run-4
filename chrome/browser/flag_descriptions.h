43c192599d0b2ab20954a36be7d6b5365f4aae01
Gabriel Oliveira <gabolvr@google.com>
2022-08-17 17:31:41 +0000

    [Signin] Add Signin Intercept feature flags to chrome://flags
    Bug: 1282157
    Change-Id: I6a098cfb3d34a3ab4250ae263097123304cabf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834176
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036146}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3674,6 +3674,12 @@ extern const char kUIDebugToolsDescription[];
 
 extern const char kDesktopDetailedLanguageSettingsName[];
 extern const char kDesktopDetailedLanguageSettingsDescription[];
+
+extern const char kSyncPromoAfterSigninInterceptName[];
+extern const char kSyncPromoAfterSigninInterceptDescription[];
+
+extern const char kSigninInterceptBubbleV2Name[];
+extern const char kSigninInterceptBubbleV2Description[];
 #endif
 
 #if defined(WEBRTC_USE_PIPEWIRE)
