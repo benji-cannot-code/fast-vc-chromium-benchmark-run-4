3ecfb16dc7755b425197f8bb53dd9212d18acd33
Jonah Ryan-Davis <jonahr@google.com>
2022-08-17 18:57:41 +0000

    Add webview flag for IncreasedCmdBufferParseSlice experiment.
    Bug: chromium:1336981
    Change-Id: I98bc27db31223f4ade5a3878a6515b143e1417f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832713
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036208}

--- gpu/command_buffer/service/command_buffer_service.cc
+++ gpu/command_buffer/service/command_buffer_service.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/cmd_buffer_common.h"
 #include "gpu/command_buffer/common/command_buffer_shared.h"
 #include "gpu/command_buffer/service/transfer_buffer_manager.h"
-#include "ui/gl/gl_switches.h"
+#include "gpu/config/gpu_finch_features.h"
 
 #if BUILDFLAG(IS_MAC)
 #include <mach/mach_vm.h>
