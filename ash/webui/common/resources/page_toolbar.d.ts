93d9921a4bbbed8db760dea05217c7fc1220ccdc
Michael Checo <michaelcheco@google.com>
2022-08-17 20:40:11 +0000

    Check in .d.ts files for shared navigation polymer elements
    - This is in preparation for migrating shortcut customization to
      TypeScript
    - Add copy() target to ash/webui/common/resources/BUILD.gn so that
      other projects can depend on the newly added definitions
    
    Bug: b/242549183
    Change-Id: I5120069b376ac512c75154fdf11b0640ba2db7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833543
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036262}

---
+++ ash/webui/common/resources/page_toolbar.d.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+interface PageToolbarElement extends LegacyElementMixin, HTMLElement {
+  title: string;
+  isNarrow: boolean;
+  onMenuTap_(): void;
+}
+
+export {PageToolbarElement};
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'page-toolbar': PageToolbarElement;
+  }
+}
