a5ae9a92b3a35387170a5b56203c5b23575e6f5c
Tushar Agarwal <agarwaltushar@google.com>
2022-08-17 13:42:21 +0000

    Pass new unwinders to TracingSamplerProfiler in the GPU compositor thread.
    We pass in CreateCoreUnwindersFactory here since it lives in the
    chrome/ layer while TracingSamplerProfiler is outside of chrome/.
    
    Also, move the initialization of TracingSamplerProfiler from
    components/viz to chrome/gpu. This aligns better with how the
    profiler is used in other child processes.
    
    Change-Id: Iaa494b8f030390c92636693bf6c7297f7eec96a2
    Bug: b/231934478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804589
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036025}

--- components/viz/service/main/viz_compositor_thread_runner_impl.cc
+++ components/viz/service/main/viz_compositor_thread_runner_impl.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_switches.h"
 #include "gpu/ipc/service/gpu_memory_buffer_factory.h"
-#include "services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.h"
 #include "ui/gfx/switches.h"
 
 #if defined(USE_OZONE)
@@ -81,11 +80,6 @@ std::unique_ptr<VizCompositorThreadType> CreateAndStartCompositorThread() {
 #endif  // !BUILDFLAG(IS_APPLE)
 
   CHECK(thread->StartWithOptions(std::move(thread_options)));
-
-  // Setup tracing sampler profiler as early as possible.
-  thread->task_runner()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&tracing::TracingSamplerProfiler::CreateOnChildThread));
 
   return thread;
 #endif  // !BUILDFLAG(IS_ANDROID)
