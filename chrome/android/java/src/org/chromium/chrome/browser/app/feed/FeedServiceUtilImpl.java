b5f96e27599354c27bc67c6b8d506a51e0cdf205
Jian Li <jianli@chromium.org>
2022-08-17 19:47:21 +0000

    No tab group for start surface.
    Bug: 1350655
    Change-Id: Icb4bde9f9a5f4605442d3a02e1a7a84aafe59063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834591
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036239}

--- chrome/android/java/src/org/chromium/chrome/browser/app/feed/FeedServiceUtilImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/feed/FeedServiceUtilImpl.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.app.feed;
 
+import android.content.Context;
+
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.feed.FeedServiceUtil;
 import org.chromium.chrome.browser.feed.TabGroupEnabledState;
+import org.chromium.chrome.browser.tasks.ReturnToChromeUtil;
 import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
 
 /**
@@ -16,7 +19,11 @@ import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
 public class FeedServiceUtilImpl implements FeedServiceUtil {
     @Override
     public @TabGroupEnabledState int getTabGroupEnabledState() {
-        if (TabUiFeatureUtilities.isTabGroupsAndroidEnabled(ContextUtils.getApplicationContext())) {
+        Context context = ContextUtils.getApplicationContext();
+        if (ReturnToChromeUtil.isStartSurfaceEnabled(context)) {
+            return TabGroupEnabledState.NONE;
+        }
+        if (TabUiFeatureUtilities.isTabGroupsAndroidEnabled(context)) {
             if (TabUiFeatureUtilities.ENABLE_TAB_GROUP_AUTO_CREATION.getValue()) {
                 return TabGroupEnabledState.REPLACED;
             } else {
