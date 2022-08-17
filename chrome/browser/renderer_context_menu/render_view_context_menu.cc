d4e21aa7ce3156ecc261cc62ddfb15574b66c9cc
Vidhan <vidhanj@google.com>
2022-08-17 09:52:50 +0000

    [Fallbacks] Hide Autofill popup if it overlaps with the context menu
    Bug: 1325811
    Change-Id: Ifef87469031fa202801d58335719f0973f24fe27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762492
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1035989}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -724,7 +724,8 @@ RenderViewContextMenu::RenderViewContextMenu(
               GetProfile()->GetOriginalProfile()),
           this,
           &menu_model_,
-          GetBrowser()) {
+          GetBrowser(),
+          GetRenderFrameHost()) {
   if (!g_custom_id_ranges_initialized) {
     g_custom_id_ranges_initialized = true;
     SetContentCustomCommandIdRange(IDC_CONTENT_CONTEXT_CUSTOM_FIRST,
@@ -2576,12 +2577,8 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
 
   if (autofill::AutofillContextMenuManager::IsAutofillCustomCommandId(
           autofill::AutofillContextMenuManager::CommandId(id))) {
-    RenderFrameHost* render_frame_host = GetRenderFrameHost();
-    if (render_frame_host) {
-      autofill_context_menu_manager_.ExecuteCommand(
-          autofill::AutofillContextMenuManager::CommandId(id),
-          render_frame_host, params_);
-    }
+    autofill_context_menu_manager_.ExecuteCommand(
+        autofill::AutofillContextMenuManager::CommandId(id), params_);
     return;
   }
 
