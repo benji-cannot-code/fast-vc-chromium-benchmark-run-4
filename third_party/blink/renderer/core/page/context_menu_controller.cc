9681c1849cb8ce8cefdeb5472c19a995e3faded0
Vidhan <vidhanj@google.com>
2022-08-17 10:48:40 +0000

    [Fallbacks] Fill in textarea fields using the context menu
    Prior to this CL, the textarea fields were not filled using the
    autofill context menu due to couple of reasons:
    1. The field renderer id is not set for the textarea fields so the
    context menu would not fill them.
    2. There were checks for WebInputElement in the renderer in
    FillFieldWithValue that returns null for the textarea fields.
    
    This CL fixes the above alongwith some cleanups.
    
    Bug: 1344455, 1325811
    Change-Id: I3c5c80738221a711db3b3deae045df50b174718a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804639
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036000}

--- third_party/blink/renderer/core/page/context_menu_controller.cc
+++ third_party/blink/renderer/core/page/context_menu_controller.cc
@@ -136,8 +136,9 @@ uint32_t EnumToBitmask(enumType outcome) {
 }
 
 absl::optional<uint64_t> GetRendererId(HitTestResult& result) {
-  if (auto* input = DynamicTo<HTMLInputElement>(result.InnerNode()))
-    return input->UniqueRendererFormControlId();
+  if (auto* text_element = DynamicTo<TextControlElement>(result.InnerNode())) {
+    return text_element->UniqueRendererFormControlId();
+  }
   return absl::nullopt;
 }
 
@@ -371,6 +372,8 @@ static mojom::blink::ContextMenuDataInputFieldType ComputeInputFieldType(
     if (input->IsTextField())
       return mojom::blink::ContextMenuDataInputFieldType::kPlainText;
     return mojom::blink::ContextMenuDataInputFieldType::kOther;
+  } else if (IsA<HTMLTextAreaElement>(result.InnerNode())) {
+    return mojom::blink::ContextMenuDataInputFieldType::kPlainText;
   }
   return mojom::blink::ContextMenuDataInputFieldType::kNone;
 }
