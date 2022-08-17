ffc732b5f1c866d544c0e389dde96c507f692ba9
Hidehiko Abe <hidehiko@chromium.org>
2022-08-17 01:55:56 +0000

    Remove Account::AdFromObjGuid.
    The API is used in known_user, but it was effectively no-op
    (FindStringPath returns false always, because account is considered
    invalid in KnownUser::FindPrefs), so removed.
    
    Other uses are only for testing, so replaced by similar function
    which initializes AccountId as "valid" state.
    
    BUG=1325210
    TEST=Ran tryjob.
    
    Change-Id: I5499f9021ce1cffc1dbeec5df28fdf4b4b6d3127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812952
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035838}

--- chrome/browser/ash/arc/arc_util_unittest.cc
+++ chrome/browser/ash/arc/arc_util_unittest.cc
@@ -286,7 +286,8 @@ TEST_F(ChromeArcUtilTest, IsArcAllowedForProfile_ActiveDirectoryEnabled) {
       {"", "--arc-availability=officially-supported"});
   ScopedLogIn login(
       GetFakeUserManager(),
-      AccountId::AdFromObjGuid("f04557de-5da2-40ce-ae9d-b8874d8da96e"),
+      AccountId::AdFromUserEmailObjGuid("testing_profile@test",
+                                        "f04557de-5da2-40ce-ae9d-b8874d8da96e"),
       user_manager::USER_TYPE_ACTIVE_DIRECTORY);
   EXPECT_FALSE(
       ash::ProfileHelper::Get()->GetUserByProfile(profile())->HasGaiaAccount());
@@ -297,7 +298,8 @@ TEST_F(ChromeArcUtilTest, IsArcAllowedForProfile_ActiveDirectoryDisabled) {
   base::CommandLine::ForCurrentProcess()->InitFromArgv({""});
   ScopedLogIn login(
       GetFakeUserManager(),
-      AccountId::AdFromObjGuid("f04557de-5da2-40ce-ae9d-b8874d8da96e"),
+      AccountId::AdFromUserEmailObjGuid("testing_profile@test",
+                                        "f04557de-5da2-40ce-ae9d-b8874d8da96e"),
       user_manager::USER_TYPE_ACTIVE_DIRECTORY);
   EXPECT_FALSE(
       ash::ProfileHelper::Get()->GetUserByProfile(profile())->HasGaiaAccount());
