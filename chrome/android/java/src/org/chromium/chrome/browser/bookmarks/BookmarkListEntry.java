3d140eff85675ee94f605b086262d0f8c557fb83
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-08-17 21:37:52 +0000

    Remove unused description element from bookmark_section_header.xml
    No behavior is changed. Was added in 2020 but never actually displayed.
    
    Bug: None
    Change-Id: Ib38012962002f61baa01179d9d5f3f2dffcb3506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793233
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036289}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkListEntry.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkListEntry.java
@@ -49,13 +49,10 @@ public final class BookmarkListEntry {
      */
     static final class SectionHeaderData {
         public final CharSequence headerTitle;
-        public final CharSequence headerDescription;
         public final int topPadding;
 
-        SectionHeaderData(
-                @Nullable CharSequence title, @Nullable CharSequence description, int topPadding) {
+        SectionHeaderData(@Nullable CharSequence title, int topPadding) {
             headerTitle = title;
-            headerDescription = description;
             this.topPadding = topPadding;
         }
     }
@@ -127,14 +124,13 @@ public final class BookmarkListEntry {
     /**
      * Create an entry representing the reading list read/unread section header.
      * @param title The title of the section header.
-     * @param description The description of the section header.
      * @param topPadding The top padding of the section header. Only impacts the padding when
      *         greater than 0.
      * @param context The context to use.
      */
     static BookmarkListEntry createSectionHeader(
-            CharSequence title, CharSequence description, int topPadding, Context context) {
-        SectionHeaderData sectionHeaderData = new SectionHeaderData(title, description, topPadding);
+            CharSequence title, int topPadding, Context context) {
+        SectionHeaderData sectionHeaderData = new SectionHeaderData(title, topPadding);
         return new BookmarkListEntry(ViewType.SECTION_HEADER, null, sectionHeaderData);
     }
 
@@ -158,12 +154,6 @@ public final class BookmarkListEntry {
     @Nullable
     CharSequence getHeaderTitle() {
         return mSectionHeaderData.headerTitle;
-    }
-
-    /** @return The description text to be shown if it is a section header. */
-    @Nullable
-    CharSequence getHeaderDescription() {
-        return mSectionHeaderData.headerDescription;
     }
 
     /**
