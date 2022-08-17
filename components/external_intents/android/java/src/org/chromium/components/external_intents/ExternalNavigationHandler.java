a52afdc5584fd1da6e42e29524752d7af10b17fe
Michael Thiessen <mthiesse@chromium.org>
2022-08-17 17:32:40 +0000

    Call AsyncActionTakenInMainFrameCallback for External Nav Message
    I missed calling this callback when implementing the Message for
    blocked external navigations. This will align behavior with what
    happens when the Message isn't shown in terms of when non-committed
    pages are closed and in terms of metrics.
    
    Bug: 1333890
    Change-Id: I244452e0dadd06b18db25aa1e219b6729758efbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831250
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036147}

--- components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
@@ -1725,7 +1725,7 @@ public class ExternalNavigationHandler {
 
         if (requiresPromptForExternalIntent) {
             return maybeAskToLaunchApp(isExternalProtocol, targetIntent, resolvingInfos,
-                    resolveActivity, browserFallbackUrl);
+                    resolveActivity, browserFallbackUrl, params);
         }
 
         return startActivity(targetIntent, shouldProxyForInstantApps, requiresIntentChooser,
@@ -2235,7 +2235,8 @@ public class ExternalNavigationHandler {
 
     protected OverrideUrlLoadingResult maybeAskToLaunchApp(boolean isExternalProtocol,
             Intent targetIntent, QueryIntentActivitiesSupplier resolvingInfos,
-            ResolveActivitySupplier resolveActivity, GURL browserFallbackUrl) {
+            ResolveActivitySupplier resolveActivity, GURL browserFallbackUrl,
+            ExternalNavigationParams params) {
         // For URLs the browser supports, we shouldn't have reached here.
         assert isExternalProtocol;
 
@@ -2304,6 +2305,11 @@ public class ExternalNavigationHandler {
                         .with(MessageBannerProperties.ON_PRIMARY_ACTION,
                                 () -> {
                                     startActivity(targetIntent, false);
+                                    if (params.getAsyncActionTakenInMainFrameCallback() != null) {
+                                        params.getAsyncActionTakenInMainFrameCallback().onResult(
+                                                new ExternalNavigationParams.AsyncActionTakenParams(
+                                                        true, false, params));
+                                    }
                                     return PrimaryActionClickBehavior.DISMISS_IMMEDIATELY;
                                 })
                         .build();
