6f583362c503557cc18806379958379e507feabe
Daniel Andersson <dandersson@chromium.org>
2022-08-17 00:10:04 +0000

    saved_desks: Remove callback from CreateNewDeskFromTemplate.
    This function was taking a callback and thus giving people the idea
    of it being async. However, the callback was always invoked directly
    from the function.
    
    To simplify things, the callback has been removed and the function
    will now return the newly created desk.
    
    Additionally, the function will now require that callers check
    whether a new desk can be created (using `CanCreateDesks()`) first.
    This makes the function consistent with `DesksController::NewDesk`.
    
    Change-Id: I9508332f7494e1d0adb20ff9316e0988467f8697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826449
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035789}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -1050,16 +1050,10 @@ void DesksController::CaptureActiveDeskAsTemplate(
       base::UTF16ToUTF8(active_desk_->name()), root_window_to_show);
 }
 
-void DesksController::CreateNewDeskForTemplate(
+const Desk* DesksController::CreateNewDeskForTemplate(
     bool activate_desk,
-    base::OnceCallback<void(const Desk*)> callback,
     const std::u16string& customized_desk_name) {
-  DCHECK(!callback.is_null());
-
-  if (!CanCreateDesks()) {
-    std::move(callback).Run(nullptr);
-    return;
-  }
+  DCHECK(CanCreateDesks());
 
   // If there is an ongoing animation, we should stop it before creating and
   // activating the new desk, which triggers its own animation.
@@ -1125,7 +1119,7 @@ void DesksController::CreateNewDeskForTemplate(
     DCHECK(!animation_);
   }
 
-  std::move(callback).Run(desk);
+  return desk;
 }
 
 bool DesksController::OnSingleInstanceAppLaunchingFromTemplate(
