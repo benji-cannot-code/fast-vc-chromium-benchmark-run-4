f64d09fabfdb11a4ca9827d946faaf88135c7c18
Josh Simmons <jds@google.com>
2022-08-17 05:21:19 +0000

    Make ContextualSearchContext inherit from SupportsWeakPtr.
    ContextualSearchDeleate requires a WeakPtr<ContextualSearchContext>, so
    it indeed winds up being simpler just having the base class enforce the
    weak requirement rather than the child classes.
    
    Change-Id: I86943f5a3d222ad6ca55542833c82c98d6a479b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832321
    Commit-Queue: Josh Simmons <jds@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035911}

--- components/contextual_search/core/browser/contextual_search_context.h
+++ components/contextual_search/core/browser/contextual_search_context.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/weak_ptr.h"
 #include "url/gurl.h"
 
 // Encapsulates key parts of a Contextual Search Context, including surrounding
 // text.
-struct ContextualSearchContext {
+struct ContextualSearchContext
+    : public base::SupportsWeakPtr<ContextualSearchContext> {
  public:
   // Languages used for translation.
   struct TranslationLanguages {
@@ -38,12 +40,6 @@ struct ContextualSearchContext {
   // Sets the properties needed to resolve a context.
   void SetResolveProperties(const std::string& home_country,
                             bool may_send_base_page_url);
-
-  // Sets the surrounding text to the given string and the selection to the
-  // given start/end range.
-  void SetSurroundingsAndSelection(const std::string& surrounding_text,
-                                   int selection_start,
-                                   int selection_end);
 
   // Adjust the current selection offsets by the given signed amounts.
   void AdjustSelection(int start_adjust, int end_adjust);
