edfa6661ef5377c9835ab2832a3bdf8123c1938c
Ed Chin <edchin@google.com>
2022-08-17 18:40:02 +0000

    [ios][feed] Remove bg timestamp & default false following feed bg refresh
    Change-Id: If5a2f09c5bf7aed2e140bad0c712c8ae6e899dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835267
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036201}

--- ios/chrome/app/feed_app_agent.mm
+++ ios/chrome/app/feed_app_agent.mm
@@ -219,7 +219,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     title = @"Feed Bg Refresh Failure";
   }
   [self maybeRequestNotification:title];
-  SetFeedLastBackgroundRefreshTimestamp([NSDate now]);
 }
 
 @end
