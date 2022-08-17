9e3f45d43ae36062e9c51f1efa5a9ca6c01b8b76
Mahmoud Rashad <mmrashad@google.com>
2022-08-17 18:54:31 +0000

    [Sync] Suppress NTP promo after the user chooses to not signin in FRE
    This CL supresses NTP promo for 24h after the user chooses to not signin
    in the FRE.
    
    Bug: 1353777
    Change-Id: I2398b7c9d8a3d89efb8ee44ab4fa8b7ce83aa535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3836434
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036207}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
@@ -23,6 +23,7 @@ import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.ProfileDataCache;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.signin.services.SigninManager.SignInCallback;
+import org.chromium.chrome.browser.signin.services.SigninPreferencesManager;
 import org.chromium.chrome.browser.ui.signin.R;
 import org.chromium.chrome.browser.ui.signin.account_picker.AccountPickerCoordinator;
 import org.chromium.chrome.browser.ui.signin.account_picker.AccountPickerDialogCoordinator;
@@ -355,6 +356,7 @@ public class SigninFirstRunMediator
 
         mDelegate.recordFreProgressHistogram(MobileFreProgress.WELCOME_DISMISS);
         mDelegate.acceptTermsOfService(mAllowCrashUpload);
+        SigninPreferencesManager.getInstance().temporarilySuppressNewTabPagePromos();
         if (IdentityServicesProvider.get()
                         .getIdentityManager(Profile.getLastUsedRegularProfile())
                         .hasPrimaryAccount(ConsentLevel.SIGNIN)) {
