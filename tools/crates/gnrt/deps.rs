d5f80b1d778c6c36c9f27a80e4bb9ab4586f39cf
Collin Baker <collinbaker@chromium.org>
2022-08-17 19:44:22 +0000

    Finish gnrt tool
    Translates dependency resolution data to build rules and outputs
    BUILD.gn files accordingly.
    
    A follow-up CL will update the build files checked in, finishing the
    transition to gnrt from crates.py for build file generation.
    
    Bug: 1291994
    Change-Id: I29076f67e0cbe07710ac783ff318e3b70ee8c4d3
    
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-dbg,linux-rust-x64-rel,android-rust-arm-rel,android-rust-arm-dbg
    Change-Id: I29076f67e0cbe07710ac783ff318e3b70ee8c4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829810
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036235}

--- tools/crates/gnrt/deps.rs
+++ tools/crates/gnrt/deps.rs
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 //! Utilities to process `cargo metadata` dependency graph.
 
-use crate::crates::{Epoch, NormalizedName};
+use crate::crates::{self, Epoch, NormalizedName};
 use crate::platforms::{self, Platform, PlatformSet};
 
 use std::collections::{hash_map::Entry, HashMap, HashSet};
@@ -54,6 +54,12 @@ pub struct ThirdPartyDep {
     /// valid packages. Returning `false` for a dependency allows better error
     /// messages later.
     pub is_local: bool,
+}
+
+impl ThirdPartyDep {
+    pub fn crate_id(&self) -> crates::ThirdPartyCrate {
+        crates::ThirdPartyCrate { name: self.package_name.clone(), epoch: self.epoch }
+    }
 }
 
 /// A dependency of a `ThirdPartyDep`. Cross-references another `ThirdPartyDep`
