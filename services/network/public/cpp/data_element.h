316f2fa5c90e4c66afd747862ff200abe02c0dbd
Yutaka Hirano <yhirano@chromium.org>
2022-08-17 08:20:58 +0000

    More strict request body handling in PendingBeacon
    content::PendingBeaconService cannot handle request body with
    multiple data elements (which is used when a FormData is given),
    given a SimpleURLLoader restriction. Let's be strict and reject such
    a request body in PendingBeaconHost::SetRequestData.
    
    This allows us to store an optional<DataElement> rather than
    vector<DataElement> in PendingBeaconHost, and that simplifies the
    request body handling in PendingBeaconService::SendBeacons.
    
    This change also adds DataElement::Clone() which is callable only
    when the data element is copyable.
    
    Bug: 1293679
    Change-Id: I69bdddb146d16aefb928e11f477c92c51890ecea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826326
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035970}

--- services/network/public/cpp/data_element.h
+++ services/network/public/cpp/data_element.h
@@ -48,6 +48,8 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElementBytes final {
                              bytes_.size());
   }
 
+  DataElementBytes Clone() const;
+
  private:
   std::vector<uint8_t> bytes_;
 };
@@ -69,6 +71,8 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElementDataPipe final {
 
   mojo::PendingRemote<mojom::DataPipeGetter> ReleaseDataPipeGetter();
   mojo::PendingRemote<mojom::DataPipeGetter> CloneDataPipeGetter() const;
+
+  DataElementDataPipe Clone() const;
 
  private:
   mojo::PendingRemote<mojom::DataPipeGetter> data_pipe_getter_;
@@ -155,6 +159,10 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) DataElement {
   DataElement(DataElement&& other);
   DataElement& operator=(DataElement&& other);
   ~DataElement();
+
+  // Returns a cloned element. This is callable only when the type is not
+  // `kChunkedDataPipe`.
+  DataElement Clone() const;
 
   Tag type() const {
     switch (variant_.index()) {
