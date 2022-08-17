ef02c4a1ac6bf79839df19b2a880bd2a4b04c791
K. Moon <kmoon@chromium.org>
2022-08-17 18:37:56 +0000

    Fix IWYU in ipc_fuzzer
    Fixes missing ipc_platform_file.h include in fuzzer.cc, which is
    included indirectly via content/common/all_messages.h, but only when
    NaCl or PPAPI are enabled.
    
    Bug: 1306610
    Change-Id: I5beb4f053514f4a36f54c218d895798caee6c485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834413
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036198}

--- tools/ipc_fuzzer/fuzzer/fuzzer.cc
+++ tools/ipc_fuzzer/fuzzer/fuzzer.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/gpu_param_traits_macros.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_utils.h"
+#include "ipc/ipc_platform_file.h"
 #include "ipc/ipc_sync_channel.h"
 #include "ipc/ipc_sync_message.h"
 #include "media/gpu/ipc/common/media_param_traits.h"
