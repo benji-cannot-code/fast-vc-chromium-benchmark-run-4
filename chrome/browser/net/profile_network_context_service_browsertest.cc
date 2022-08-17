17a82160e0dd6619833fd5acd25a15689bf66bc3
Matt Mueller <mattm@chromium.org>
2022-08-17 16:20:43 +0000

    Replace accidental absl::StrCat use with base::StrCat
    Snuck in https://chromium-review.googlesource.com/c/chromium/src/+/3759632
    
    Change-Id: Id8e8e882f746ee899b46cf09e651b3e558e19b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831245
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036113}

--- chrome/browser/net/profile_network_context_service_browsertest.cc
+++ chrome/browser/net/profile_network_context_service_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/raw_ptr.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -648,11 +649,11 @@ INSTANTIATE_TEST_SUITE_P(
     [](const testing::TestParamInfo<
         ProfileNetworkContextServiceCertVerifierBuiltinPermissionsPolicyTest::
             ParamType>& info) {
-      return absl::StrCat(
-          std::get<0>(info.param) ? "FeatureTrue" : "FeatureFalse",
-          std::get<1>(info.param).has_value()
-              ? (*std::get<1>(info.param) ? "PolicyTrue" : "PolicyFalse")
-              : "PolicyNotSet");
+      return base::StrCat(
+          {std::get<0>(info.param) ? "FeatureTrue" : "FeatureFalse",
+           std::get<1>(info.param).has_value()
+               ? (*std::get<1>(info.param) ? "PolicyTrue" : "PolicyFalse")
+               : "PolicyNotSet"});
     });
 #endif  // BUILDFLAG(BUILTIN_CERT_VERIFIER_FEATURE_SUPPORTED)
 
@@ -748,11 +749,11 @@ INSTANTIATE_TEST_SUITE_P(
     [](const testing::TestParamInfo<
         ProfileNetworkContextServiceChromeRootStorePermissionsPolicyTest::
             ParamType>& info) {
-      return absl::StrCat(
-          std::get<0>(info.param) ? "FeatureTrue" : "FeatureFalse",
-          std::get<1>(info.param).has_value()
-              ? (*std::get<1>(info.param) ? "PolicyTrue" : "PolicyFalse")
-              : "PolicyNotSet");
+      return base::StrCat(
+          {std::get<0>(info.param) ? "FeatureTrue" : "FeatureFalse",
+           std::get<1>(info.param).has_value()
+               ? (*std::get<1>(info.param) ? "PolicyTrue" : "PolicyFalse")
+               : "PolicyNotSet"});
     });
 #endif  // BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
 
