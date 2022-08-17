fa1346a66a4eedb3ab285a1adf3760823ad0703c
Ali Hijazi <ahijazi@chromium.org>
2022-08-17 20:10:41 +0000

    DanglingPtr: fix dangling ptrs in xproto_internal
    Use unique_ptr to handle owned memory.
    
    Bug: 1291138
    Change-Id: Ic7398f11466afc92e1551c50966885f91bc5780f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834482
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036251}

--- ui/gfx/x/xproto_internal.cc
+++ ui/gfx/x/xproto_internal.cc
@@ -15,7 +15,7 @@ MallocedRefCountedMemory::MallocedRefCountedMemory(void* data)
     : data_(reinterpret_cast<uint8_t*>(data)) {}
 
 const uint8_t* MallocedRefCountedMemory::front() const {
-  return data_;
+  return data_.get();
 }
 
 size_t MallocedRefCountedMemory::size() const {
@@ -24,9 +24,7 @@ size_t MallocedRefCountedMemory::size() const {
   return 0;
 }
 
-MallocedRefCountedMemory::~MallocedRefCountedMemory() {
-  free(data_);
-}
+MallocedRefCountedMemory::~MallocedRefCountedMemory() = default;
 
 OffsetRefCountedMemory::OffsetRefCountedMemory(
     scoped_refptr<base::RefCountedMemory> memory,
