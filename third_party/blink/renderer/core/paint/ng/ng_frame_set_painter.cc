6fd6003da1fe3617456c68c6e86b3b96dd02ba3f
Kent Tamura <tkent@chromium.org>
2022-08-17 07:55:49 +0000

    FrameSet NG: Implement NGFrameSetPainter::PaintObject()
    This CL ports FrameSetPainter::Paint() to NGFrameSetPainter.
    This CL has no behaivor changes due to a disabled runtime flag.
    
    Bug: 1346221
    Change-Id: Iff54a81dc24a3575b31e5af8654c1ce933b212e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835491
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035960}

--- third_party/blink/renderer/core/paint/ng/ng_frame_set_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_frame_set_painter.cc
@@ -5,9 +5,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/ng/ng_frame_set_painter.h"
 
+#include "third_party/blink/renderer/core/paint/paint_info.h"
+
 namespace blink {
 
 void NGFrameSetPainter::PaintObject(const PaintInfo& paint_info,
-                                    const PhysicalOffset& paint_offset) {}
+                                    const PhysicalOffset& paint_offset) {
+  if (paint_info.phase != PaintPhase::kForeground)
+    return;
+
+  if (box_fragment_.Children().size() == 0)
+    return;
+
+  if (box_fragment_.Style().Visibility() != EVisibility::kVisible)
+    return;
+
+  PaintInfo paint_info_for_descendants = paint_info.ForDescendants();
+  paint_info_for_descendants.SetIsInFragmentTraversal();
+  PaintChildren(paint_info_for_descendants);
+
+  PaintBorders(paint_info, paint_offset);
+}
+
+void NGFrameSetPainter::PaintChildren(const PaintInfo& paint_info) {}
+
+void NGFrameSetPainter::PaintBorders(const PaintInfo& paint_info,
+                                     const PhysicalOffset& paint_offset) {}
 
 }  // namespace blink
