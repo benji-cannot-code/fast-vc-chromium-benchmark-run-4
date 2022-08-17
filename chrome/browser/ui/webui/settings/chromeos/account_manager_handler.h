69969111fe4a6aa85f0dacd280cfd6cd40cad852
Matt Menke <mmenke@chromium.org>
2022-08-17 00:14:45 +0000

    Update list and dict Value usage in account_manager_handler.cc
    Only Value::List and Value::Dict should be used to access and update
    list and dict Values - other APIs are all deprecated.
    
    Bug: 1187001, 1338341
    Change-Id: Ib4c295562b033f9573fc97a324409e1f9537eb70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828428
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035792}

--- chrome/browser/ui/webui/settings/chromeos/account_manager_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/account_manager_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/values.h"
 #include "chrome/browser/ash/account_manager/account_apps_availability.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "components/account_id/account_id.h"
@@ -110,13 +111,13 @@ class AccountManagerUIHandler
   // If user (device account) is child - |is_child_user| should be set to true,
   // in this case "unmigrated" property will be always false for secondary
   // accounts.
-  base::ListValue GetSecondaryGaiaAccounts(
+  base::Value::List GetSecondaryGaiaAccounts(
       const std::vector<std::pair<::account_manager::Account, bool>>&
           account_dummy_token_list,
       const base::flat_set<account_manager::Account>& arc_accounts,
       const AccountId device_account_id,
       const bool is_child_user,
-      base::DictionaryValue* device_account);
+      base::Value::Dict* device_account);
 
   // Refreshes the UI.
   void RefreshUI();
