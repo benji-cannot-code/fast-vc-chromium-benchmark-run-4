32c480b3b0e6e083967c087917465199f3225352
Christopher Cameron <ccameron@chromium.org>
2022-08-18 05:42:25 +0000

    blink::Color: Remove helper functions
    Remove the functions MakeRGB, MakeRGBA, MakeRGBA32FromFloats,
    MakeRGBAFromHSLA, MakeRGBAFromHWBA, and several of their helper
    functions. Change all callers to use methods on the Color class.
    
    Un-inline several functions in blink::Color, and minimize the number
    of places where the blink::Color::color_ member is used.
    
    The next patch in this sequence will change the internals of the
    Color class to no longer be an RGBA32.
    
    Bug: 1351544
    Change-Id: I8270d462443e75d9a3bb64d92ee959a39119e15c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834250
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036457}

--- third_party/blink/renderer/platform/graphics/graphics_context_test.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context_test.cc
@@ -83,7 +83,7 @@ TEST(GraphicsContextTest, Recording) {
   auto paint_controller = std::make_unique<PaintController>();
   GraphicsContext context(*paint_controller);
 
-  Color opaque(1.0f, 0.0f, 0.0f, 1.0f);
+  Color opaque = Color::FromRGBA(255, 0, 0, 255);
   gfx::RectF bounds(0, 0, 100, 100);
 
   context.BeginRecording(bounds);
@@ -109,8 +109,8 @@ TEST(GraphicsContextTest, UnboundedDrawsAreClipped) {
   bitmap.eraseColor(0);
   SkiaPaintCanvas canvas(bitmap);
 
-  Color opaque(1.0f, 0.0f, 0.0f, 1.0f);
-  Color transparent(0.0f, 0.0f, 0.0f, 0.0f);
+  Color opaque = Color::FromRGBA(255, 0, 0, 255);
+  Color transparent = Color::kTransparent;
   gfx::RectF bounds(0, 0, 100, 100);
 
   auto paint_controller = std::make_unique<PaintController>();
