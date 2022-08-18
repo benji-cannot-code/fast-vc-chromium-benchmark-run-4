a1cdd2ca8b5b74cfd5f4ab4aa2e40fbaa30cad22
Benjamin Williams <bwwilliams@google.com>
2022-08-18 21:19:33 +0000

    Reland "Add first Promo class unit test."
    This is a reland of commit e5e95ff0b49ef0ba973e0ddaf9afcab217847db1
    
    Change: Update Promo* initializers to the latest version.
    
    Original change's description:
    > Add first Promo class unit test.
    >
    > Change-Id: Iaead9b53af28bec227cb04dcae86524df4587092
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832702
    > Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    > Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1036635}
    
    Change-Id: I7d27eaa91579137fe9200ddbd2d45003f201f2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838393
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036785}

--- ios/chrome/browser/promos_manager/promos_manager_unittest.mm
+++ ios/chrome/browser/promos_manager/promos_manager_unittest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/promos_manager/constants.h"
 #import "ios/chrome/browser/promos_manager/features.h"
 #import "ios/chrome/browser/promos_manager/impression_limit.h"
+#import "ios/chrome/browser/promos_manager/promo.h"
 #import "ios/chrome/browser/promos_manager/promos_manager.h"
 #import "testing/platform_test.h"
 
@@ -39,6 +40,15 @@ NSArray<ImpressionLimit*>* PromosManagerTest::TestImpressionLimits() {
   });
 
   return limits;
+}
+
+Promo* PromosManagerTest::TestPromo() {
+  return [[Promo alloc] initWithIdentifier:promos_manager::Promo::Test];
+}
+
+Promo* PromosManagerTest::TestPromoWithImpressionLimits() {
+  return [[Promo alloc] initWithIdentifier:promos_manager::Promo::Test
+                       andImpressionLimits:TestImpressionLimits()];
 }
 
 void PromosManagerTest::CreatePromosManager() {
@@ -81,6 +91,23 @@ TEST_F(PromosManagerTest, ReturnsNameForTestPromo) {
 TEST_F(PromosManagerTest, ReturnsTestPromoForName) {
   EXPECT_EQ(promos_manager::PromoForName("promos_manager::Promo::Test"),
             promos_manager::Promo::Test);
+}
+
+// Tests PromosManagerTest::TestPromo() correctly creates one mock promo.
+TEST_F(PromosManagerTest, CreatesPromo) {
+  Promo* promo = TestPromo();
+
+  EXPECT_NE(promo, nil);
+  EXPECT_EQ((int)promo.impressionLimits.count, 0);
+}
+
+// Tests PromosManagerTest::TestPromoWithImpressionLimits() correctly creates
+// one mock promo with mock impression limits.
+TEST_F(PromosManagerTest, CreatesPromoWithImpressionLimits) {
+  Promo* promoWithImpressionLimits = TestPromoWithImpressionLimits();
+
+  EXPECT_NE(promoWithImpressionLimits, nil);
+  EXPECT_EQ((int)promoWithImpressionLimits.impressionLimits.count, 2);
 }
 
 // Tests PromosManagerTest::TestImpressionLimits() correctly creates two mock
