1f53429c36e84dd4792a233af3285e2e0a4122a4
Artsiom Mitrokhin <amitrokhin@google.com>
2022-08-18 20:57:31 +0000

    glanceables: Rename welcome_label related files
    Bug: 1353488
    Test: no changes in logic
    Change-Id: Ic0bfdab303afae65b64fc952fd506040db2c1bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840388
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036772}

--- ash/glanceables/glanceables_view_unittest.cc
+++ ash/glanceables/glanceables_view_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/glanceables/glanceables_controller.h"
-#include "ash/glanceables/welcome_label.h"
+#include "ash/glanceables/glanceables_welcome_label.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/test/scoped_feature_list.h"
--- ash/glanceables/glanceables_welcome_label.h
+++ ash/glanceables/glanceables_welcome_label.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_GLANCEABLES_WELCOME_LABEL_H_
-#define ASH_GLANCEABLES_WELCOME_LABEL_H_
+#ifndef ASH_GLANCEABLES_GLANCEABLES_WELCOME_LABEL_H_
+#define ASH_GLANCEABLES_GLANCEABLES_WELCOME_LABEL_H_
 
 #include <string>
 
@@ -34,4 +34,4 @@ class ASH_EXPORT GlanceablesWelcomeLabel : public views::Label {
 
 }  // namespace ash
 
-#endif  // ASH_GLANCEABLES_WELCOME_LABEL_H_
+#endif  // ASH_GLANCEABLES_GLANCEABLES_WELCOME_LABEL_H_
