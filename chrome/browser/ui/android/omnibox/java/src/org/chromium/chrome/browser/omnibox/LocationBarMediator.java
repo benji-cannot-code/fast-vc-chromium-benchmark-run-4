26571d6af7a695ac892c2cf908951c137c46fb15
Ender <ender@google.com>
2022-08-18 02:11:05 +0000

    Remove Mic and Lens buttons in clobber mode.
    The change ensures the Lens and Voice icons are removed from toolbar
    when Omnibox contains user input to release some space for the input
    content.
    
    This seems to have recently regressed as a result of multiple new
    features launching.
    
    Before: http://screen/aKXnqSgE4GDm7CF.png
    After:  http://screen/3g8NHGTkiiwW2ir.png
    ZPS:    http://screen/7H2vhbqXXpzqz7j.png
    
    Change-Id: I8db60813fd46ccd7bc523e34627e5012954158a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832324
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036410}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -1048,6 +1048,7 @@ class LocationBarMediator
     }
 
     private boolean shouldShowMicButton() {
+        if (shouldShowDeleteButton()) return false;
         if (!mNativeInitialized || mVoiceRecognitionHandler == null
                 || !mVoiceRecognitionHandler.isVoiceSearchEnabled()) {
             return false;
@@ -1056,9 +1057,8 @@ class LocationBarMediator
         if (mIsTablet && mShouldShowButtonsWhenUnfocused) {
             return !isToolbarMicEnabled && (mUrlHasFocus || mIsUrlFocusChangeInProgress);
         } else {
-            boolean deleteButtonVisible = shouldShowDeleteButton();
             boolean canShowMicButton = !mIsTablet || !isToolbarMicEnabled;
-            return canShowMicButton && !deleteButtonVisible
+            return canShowMicButton
                     && (mUrlHasFocus || mIsUrlFocusChangeInProgress
                             || mIsLocationBarFocusedFromNtpScroll
                             || mShouldShowMicButtonWhenUnfocused);
@@ -1066,6 +1066,8 @@ class LocationBarMediator
     }
 
     private boolean shouldShowLensButton() {
+        if (shouldShowDeleteButton()) return false;
+
         // When this method is called on UI inflation, return false as the native is not ready.
         if (!mNativeInitialized) {
             return false;
@@ -1086,9 +1088,8 @@ class LocationBarMediator
             return (mUrlHasFocus || mIsUrlFocusChangeInProgress) && isLensOnOmniboxEnabled();
         }
 
-        return !shouldShowDeleteButton()
-                && (mUrlHasFocus || mIsUrlFocusChangeInProgress
-                        || mIsLocationBarFocusedFromNtpScroll || mShouldShowLensButtonWhenUnfocused)
+        return (mUrlHasFocus || mIsUrlFocusChangeInProgress || mIsLocationBarFocusedFromNtpScroll
+                       || mShouldShowLensButtonWhenUnfocused)
                 && isLensOnOmniboxEnabled();
     }
 
