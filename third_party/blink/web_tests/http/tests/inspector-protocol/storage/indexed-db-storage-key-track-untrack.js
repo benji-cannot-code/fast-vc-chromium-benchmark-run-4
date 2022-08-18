407d0a8f51e06cad17ec80bc0a4d6e35dba9d3ef
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-08-18 09:51:54 +0000

    [IndexedDB] Add clean-up after a test
    Bug: 1296584
    Change-Id: I5c1c071f2edd4bab20ec5e7accceb60d5f2ec2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832663
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036531}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
@@ -63,5 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   testRunner.log(oneMoreValue);
 
+  // Clean up
+  await dp.IndexedDB.deleteDatabase({storageKey, databaseName: "test-database"});
+
   testRunner.completeTest();
 })
