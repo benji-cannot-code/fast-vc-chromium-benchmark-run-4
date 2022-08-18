7fd726d9e3d3f27f845484a38a96d46c4c87195f
Kent Tamura <tkent@chromium.org>
2022-08-18 04:59:54 +0000

    FrameSet NG: Set up and transfer FrameSetLayoutData
    This CL has no behavior changes due to a disabled runtime flag.
    
    Bug: 1346221
    Change-Id: I903ecf3239108dae0e062dda6493f4a23e613243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833527
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036447}

--- third_party/blink/renderer/core/layout/ng/ng_frame_set_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_frame_set_layout_algorithm.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/ng_frame_set_layout_algorithm.h"
 
+#include "third_party/blink/renderer/core/html/html_frame_set_element.h"
+#include "third_party/blink/renderer/core/layout/ng/frame_set_layout_data.h"
+
 namespace blink {
 
 NGFrameSetLayoutAlgorithm::NGFrameSetLayoutAlgorithm(
@@ -13,6 +16,19 @@ NGFrameSetLayoutAlgorithm::NGFrameSetLayoutAlgorithm(
           params) {}
 
 const NGLayoutResult* NGFrameSetLayoutAlgorithm::Layout() {
+  auto& frame_set = *To<HTMLFrameSetElement>(Node().GetDOMNode());
+  auto layout_data = std::make_unique<FrameSetLayoutData>();
+  layout_data->border_thickness = frame_set.Border(Style());
+  layout_data->has_border_color = frame_set.HasBorderColor();
+  layout_data->row_allow_border = frame_set.AllowBorderRows();
+  layout_data->col_allow_border = frame_set.AllowBorderColumns();
+
+  // TODO(crbug.com/1346221): Fill layout_data->col_sizes and
+  // layout_data->row_sizes.
+
+  // TODO(crbug.com/1346221): Layout children.
+
+  container_builder_.TransferFrameSetLayoutData(std::move(layout_data));
   return container_builder_.ToBoxFragment();
 }
 
