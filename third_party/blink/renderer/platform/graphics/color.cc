ad77f1ba8a2746acc6923f5efd268acda76535a1
Christopher Cameron <ccameron@chromium.org>
2022-08-18 19:23:48 +0000

    blink::Color: Store 4 floating-point values instead of an RGBA32
    Prior to this change, a blink::Color was internally just an RGBA32, or
    uint32_t. Now, blink::Color stores 4 floats.
    
    Internally, change Color::Rgb() to call Color::toSkColor4f(), rather
    than vice-versa.
    
    This increases the size of a blink::Color from 4 bytes to 16 bytes. It
    also removes the ability to treat a blink::Color interchangeably with
    a uint32_t.
    
    Binary-Size: Size increase is unavoidable (see above).
    Bug: 1351544
    Change-Id: I34f09a7e2dfd149cde1760d50bd00afc0615b293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834252
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036734}

--- third_party/blink/renderer/platform/graphics/color.cc
+++ third_party/blink/renderer/platform/graphics/color.cc
@@ -261,9 +261,7 @@ Color Color::FromSkColor4f(SkColor4f fc) {
 }
 
 SkColor4f Color::toSkColor4f() const {
-  float r, g, b, a;
-  GetRGBA(r, g, b, a);
-  return SkColor4f{r, g, b, a};
+  return SkColor4f{param0_, param1_, param2_, alpha_};
 }
 
 bool Color::HasAlpha() const {
@@ -284,7 +282,7 @@ int Color::Alpha() const {
 }
 
 RGBA32 Color::Rgb() const {
-  return color_;
+  return toSkColor4f().toSkColor();
 }
 
 bool Color::ParseHexColor(const LChar* name, unsigned length, Color& color) {
