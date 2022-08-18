b051376662e6611877ef79921a41ea5d90d974c1
Kalvin Lee <kdlee@chromium.org>
2022-08-18 08:29:26 +0000

    [PA] MTECheckedPtr: Support direct maps
    The present incarnation of MTECheckedPtr has hitherto only supported
    normal buckets. This CL adds support for direct maps.
    
    Fixed: 1307514
    Change-Id: If9aaea647cbfec9dc07c527eb53c1daaad8389e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3815338
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036510}

--- base/memory/raw_ptr.h
+++ base/memory/raw_ptr.h
@@ -162,12 +162,7 @@ struct MTECheckedPtrImplPartitionAllocSupport {
     // Disambiguation: UntagPtr removes the hardware MTE tag, whereas this class
     // is responsible for handling the software MTE tag.
     auto addr = partition_alloc::UntagPtr(ptr);
-    // MTECheckedPtr algorithms work only when memory is
-    // allocated by PartitionAlloc, from normal buckets pool.
-    //
-    // TODO(crbug.com/1307514): Allow direct-map buckets.
-    return partition_alloc::IsManagedByPartitionAlloc(addr) &&
-           partition_alloc::internal::IsManagedByNormalBuckets(addr);
+    return partition_alloc::IsManagedByPartitionAlloc(addr);
   }
 
   // Returns pointer to the tag that protects are pointed by |addr|.
@@ -204,6 +199,7 @@ struct MTECheckedPtrImpl {
     static_assert(sizeof(partition_alloc::PartitionTag) * 8 <= kTagBits, "");
     uintptr_t tag = *(static_cast<volatile partition_alloc::PartitionTag*>(
         PartitionAllocSupport::TagPointer(addr)));
+    DCHECK(tag);
 
     tag <<= kValidAddressBits;
     addr |= tag;
