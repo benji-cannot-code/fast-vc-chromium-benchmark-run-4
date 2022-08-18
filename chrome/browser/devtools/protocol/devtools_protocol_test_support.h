6090e148ee1c21baeca6d66a3a20eaeb985eec56
Peter Kvitek <kvitekp@chromium.org>
2022-08-18 21:16:43 +0000

    [DevTools] Eliminate usage of deprecated send command wrapper in tests.
    This CL removes last occurrences of the deprecated SendCommand() wrapper
    that uses obsolete base::DictionaryValue type and the wrapper itself,
    
    Change-Id: I0924116ede2d9fad156122a1ddaed88488d78c64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840468
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036782}

--- chrome/browser/devtools/protocol/devtools_protocol_test_support.h
+++ chrome/browser/devtools/protocol/devtools_protocol_test_support.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/test_devtools_protocol_client.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class DevToolsProtocolTestBase : public InProcessBrowserTest,
                                  public content::TestDevToolsProtocolClient {
