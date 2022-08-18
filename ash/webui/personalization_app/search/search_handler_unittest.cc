2fd8bcdce90b66b7da9c0f649a84f7a7f269b6ff
Jason Thai <jasontt@chromium.org>
2022-08-18 00:50:57 +0000

    rgbkbd: add search terms
    Add "keyboard backlight" as the main term with other alternatives:
    - "keyboard light color"
    - "keyboard color"
    - "key color"
    - "backlight color"
    - "backlit color"
    
    BUG=b/240980782
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I3c9bc5e6b42b2ae0c47e5719d46574e7fc941536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834764
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036382}

--- ash/webui/personalization_app/search/search_handler_unittest.cc
+++ ash/webui/personalization_app/search/search_handler_unittest.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/ambient/ambient_prefs.h"
 #include "ash/public/cpp/personalization_app/enterprise_policy_delegate.h"
+#include "ash/test/ash_test_base.h"
+#include "ash/test/ash_test_suite.h"
 #include "ash/webui/personalization_app/personalization_app_url_constants.h"
 #include "ash/webui/personalization_app/search/search.mojom-shared.h"
 #include "ash/webui/personalization_app/search/search.mojom-test-utils.h"
@@ -25,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
-#include "base/test/task_environment.h"
 #include "chromeos/ash/components/local_search_service/public/cpp/local_search_service_proxy.h"
 #include "chromeos/ash/components/local_search_service/public/mojom/index.mojom-test-utils.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
 
 namespace ash {
 namespace personalization_app {
@@ -145,7 +147,7 @@ class TestEnterprisePolicyDelegate : public EnterprisePolicyDelegate {
 
 }  // namespace
 
-class PersonalizationAppSearchHandlerTest : public testing::Test {
+class PersonalizationAppSearchHandlerTest : public AshTestBase {
  protected:
   PersonalizationAppSearchHandlerTest() {
     scoped_feature_list_.InitWithFeatures(
@@ -157,7 +159,12 @@ class PersonalizationAppSearchHandlerTest : public testing::Test {
 
   ~PersonalizationAppSearchHandlerTest() override = default;
 
+  // ash::AshTestBase:
   void SetUp() override {
+    ui::ResourceBundle::CleanupSharedInstance();
+    AshTestSuite::LoadTestResources();
+    AshTestBase::SetUp();
+
     local_search_service_proxy_ =
         std::make_unique<::ash::local_search_service::LocalSearchServiceProxy>(
             /*for_testing=*/true);
@@ -231,7 +238,6 @@ class PersonalizationAppSearchHandlerTest : public testing::Test {
   }
 
  private:
-  base::test::TaskEnvironment task_environment_;
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<::chromeos::local_search_service::LocalSearchServiceProxy>
       local_search_service_proxy_;
