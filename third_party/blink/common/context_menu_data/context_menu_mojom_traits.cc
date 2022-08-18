f1e01d17907a20407fca98af4cefe39b78a1ae99
Vidhan <vidhanj@google.com>
2022-08-18 18:59:16 +0000

    [Fallbacks] Add form renderer id to params when the context menu is triggered in a form
    Add a new context menu param 'form_renderer_id', this param will
    store the form's renderer id if the context menu is opened via
    right-clicking on an input field or a textarea field in a form.
    Similar to `field_renderer_id` param.
    
    Bug: 1325811
    Change-Id: I2f0e256abbd1ba0508d80bd8a581871193e73bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820109
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036724}

--- third_party/blink/common/context_menu_data/context_menu_mojom_traits.cc
+++ third_party/blink/common/context_menu_data/context_menu_mojom_traits.cc
@@ -11,6 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 // static
+bool StructTraits<blink::mojom::FormRendererIdDataView, uint64_t>::Read(
+    blink::mojom::FormRendererIdDataView data,
+    uint64_t* out) {
+  *out = data.id();
+  return true;
+}
+
+// static
 bool StructTraits<blink::mojom::FieldRendererIdDataView, uint64_t>::Read(
     blink::mojom::FieldRendererIdDataView data,
     uint64_t* out) {
@@ -42,6 +50,7 @@ bool StructTraits<blink::mojom::UntrustworthyContextMenuParamsDataView,
       !data.ReadSourceType(&out->source_type) ||
       !data.ReadInputFieldType(&out->input_field_type) ||
       !data.ReadSelectionRect(&out->selection_rect) ||
+      !data.ReadFormRendererId(&out->form_renderer_id) ||
       !data.ReadFieldRendererId(&out->field_renderer_id))
     return false;
 
