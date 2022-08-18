206f511017b742b22ff32c9a96506d0cef7d0edc
Qihui Zhao <qihuizhao@google.com>
2022-08-18 05:41:47 +0000

    Reland "[IBAN Filling] Introduce IBAN Manager class which handles IBAN related functionality including retrieving IBAN data, managing IBAN suggestions, filling IBAN field."
    This is a reland of commit bb575920eca2cfeb09026b415aabaa5538b0f648
    
    Original change's description:
    > [IBAN Filling] Introduce IBAN Manager class which handles IBAN related functionality
    > including retrieving IBAN data, managing IBAN suggestions, filling IBAN field.
    >
    > Implementation: https://docs.google.com/document/d/1JlAfURVa3ALm-U5HC4p41fx8PFF-mtRSgN2czfVcb-w/edit?resourcekey=0-t-Xsicco20apeY2EFZGGMw#heading=h.e5uc3qpefa5q
    >
    > Next step:
    > Create IBANManagerFactory which creates IBANManager instance, get IBANMnager instance in the client (chrome/browser/ui/autofill/chrome_autofill_client.cc), and creates SingleFieldFillerRounter(https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/autofill_client.cc;drc=36c59c53be1d8852f5087008c45a95797bac9730;l=49) with the IBANManager.
    >
    > Bug: 1344590
    > Change-Id: I36bed55de6d518db5d92f412b7a16e54c76f52a6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3794042
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1036327}
    
    Bug: 1344590
    Change-Id: I6c3c7518dcd954e3b75e9828140bab62c4a8f5bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835154
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036456}

--- components/autofill/core/browser/autofill_suggestion_generator.h
+++ components/autofill/core/browser/autofill_suggestion_generator.h
@@ -27,6 +27,7 @@ class AutofillType;
 class CreditCard;
 struct FormFieldData;
 class FormStructure;
+class IBAN;
 class PersonalDataManager;
 struct Suggestion;
 
@@ -57,6 +58,10 @@ class AutofillSuggestionGenerator {
       const AutofillType& type,
       const std::string& app_locale,
       bool* should_display_gpay_logo);
+
+  // Generates suggestions for all available IBANs.
+  static std::vector<Suggestion> GetSuggestionsForIBANs(
+      const std::vector<IBAN*>& ibans);
 
   // Converts the vector of promo code offers that is passed in to a vector of
   // suggestions that can be displayed to the user for a promo code field.
