b4bfb54800d3f9bf41ef4d0dd53b5d4feda5ae5b
Benjamin Williams <bwwilliams@google.com>
2022-08-18 20:10:15 +0000

    Revert "Add first Promo class unit test."
    This reverts commit e5e95ff0b49ef0ba973e0ddaf9afcab217847db1.
    
    Reason for revert: Need to use proper inits for Promo*
    
    Original change's description:
    > Add first Promo class unit test.
    >
    > Change-Id: Iaead9b53af28bec227cb04dcae86524df4587092
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832702
    > Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    > Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1036635}
    
    Change-Id: I76ef845263d248a3e982573639cc385560c4ac02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838371
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036746}

--- ios/chrome/browser/promos_manager/promos_manager_unittest.h
+++ ios/chrome/browser/promos_manager/promos_manager_unittest.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/scoped_feature_list.h"
 #import "components/prefs/testing_pref_service.h"
 #import "ios/chrome/browser/promos_manager/impression_limit.h"
-#import "ios/chrome/browser/promos_manager/promo.h"
 #import "ios/chrome/browser/promos_manager/promos_manager.h"
 #import "testing/platform_test.h"
 
@@ -19,12 +18,6 @@ class PromosManagerTest : public PlatformTest {
  public:
   PromosManagerTest();
   ~PromosManagerTest() override;
-
-  // Creates a mock promo without impression limits.
-  Promo* TestPromo();
-
-  // Creates a mock promo with impression limits.
-  Promo* TestPromoWithImpressionLimits();
 
   // Creates mock impression limits.
   NSArray<ImpressionLimit*>* TestImpressionLimits();
