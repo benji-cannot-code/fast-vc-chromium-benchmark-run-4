efbc2e72eb0144e45772bbf6fda652d61f29ed3d
Benjamin Williams <bwwilliams@google.com>
2022-08-18 18:45:26 +0000

    Add unique identifier to Promo initializer.
    Change-Id: I4857da6f9b9507001a2582a18950e5eea1b15398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3836259
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036714}

--- ios/chrome/browser/promos_manager/promo.mm
+++ ios/chrome/browser/promos_manager/promo.mm
@@ -7,20 +7,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "ios/chrome/browser/promos_manager/constants.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @implementation Promo
 
-- (instancetype)init {
-  return [self initWithImpressionLimits:nil];
+- (instancetype)initWithIdentifier:(promos_manager::Promo)identifier {
+  return [self initWithIdentifier:identifier andImpressionLimits:nil];
 }
 
-- (instancetype)initWithImpressionLimits:
-    (NSArray<ImpressionLimit*>*)impressionLimits {
-  if (self = [super init])
+- (instancetype)initWithIdentifier:(promos_manager::Promo)identifier
+               andImpressionLimits:
+                   (NSArray<ImpressionLimit*>*)impressionLimits {
+  if (self = [super init]) {
+    _identifier = identifier;
     _impressionLimits = impressionLimits;
+  }
 
   return self;
 }
