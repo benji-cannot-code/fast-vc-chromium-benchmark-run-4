e6e3b51f953ca232cc25087a8cf6c07bf1ffd07c
Ken Rockot <rockot@google.com>
2022-08-18 05:54:15 +0000

    mojo-ipcz: Implement driver object serialization
    Implements the Serialize() and Deserialize() APIs.
    
    The generic object serialize and deserialize implementation lives
    in Transport, since a Transport may be needed for context (e.g. on
    Windows). Individual driver objects must implement a basic
    interface to be supported there, and this CL implements it trivially
    for TransmissibleHandle.
    
    On most platforms, the PlatformHandles emitted by each object's
    Serialize() method are transmissible directly out-of-band by the
    Transport's underlying Channel. On Windows, Transport serializes
    them into inline message data instead.
    
    A test is added to cover usage across all platforms, essentially
    transmitting one Transport over another, to another process, and
    then verifying end-to-end operation of the transmitted Transport.
    
    Bug: 1299283
    Change-Id: I8068df58b0e1ecb0c232d039c9f5a36aef2519f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811994
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036463}

--- mojo/core/ipcz_driver/driver.cc
+++ mojo/core/ipcz_driver/driver.cc
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/core/ipcz_driver/driver.h"
 
+#include <cstddef>
+#include <cstdint>
+#include <tuple>
+
+#include "base/containers/span.h"
+#include "base/containers/stack_container.h"
 #include "base/rand_util.h"
 #include "mojo/core/ipcz_driver/object.h"
 #include "third_party/ipcz/include/ipcz/ipcz.h"
