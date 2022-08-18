81d088d897c72d65aec61d3ce54531ff22706b6e
Rebekah Potter <rbpotter@chromium.org>
2022-08-18 18:51:19 +0000

    WebUI: Migrate cr_radio_button/* to class-based syntax
    Bug: 1206112
    Change-Id: I60c886e2006c1bd80b04d027a5011f08cc957267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826435
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036718}

--- chrome/test/data/webui/cr_elements/cr_radio_button_test.ts
+++ chrome/test/data/webui/cr_elements/cr_radio_button_test.ts
@@ -23,8 +23,8 @@ suite('cr-radio-button', function() {
     assertTrue(radioButton.hasAttribute('checked'));
     assertEquals('true', radioButton.$.button.getAttribute('aria-checked'));
     assertTrue(
-        getComputedStyle(radioButton.$$('.disc')!).backgroundColor !==
-        'rgba(0, 0, 0, 0)');
+        getComputedStyle(radioButton.shadowRoot!.querySelector('.disc')!)
+            .backgroundColor !== 'rgba(0, 0, 0, 0)');
   }
 
   function assertNotChecked() {
@@ -32,7 +32,8 @@ suite('cr-radio-button', function() {
     assertEquals('false', radioButton.$.button.getAttribute('aria-checked'));
     assertEquals(
         'rgba(0, 0, 0, 0)',
-        getComputedStyle(radioButton.$$('.disc')!).backgroundColor);
+        getComputedStyle(radioButton.shadowRoot!.querySelector('.disc')!)
+            .backgroundColor);
   }
 
   function assertDisabled() {
@@ -69,10 +70,12 @@ suite('cr-radio-button', function() {
 
   test('Ripple', function() {
     assertFalse(!!radioButton.shadowRoot!.querySelector('paper-ripple'));
-    radioButton.fire('focus');
+    radioButton.dispatchEvent(
+        new CustomEvent('focus', {bubbles: true, composed: true}));
     assertTrue(!!radioButton.shadowRoot!.querySelector('paper-ripple'));
     assertTrue(radioButton.shadowRoot!.querySelector('paper-ripple')!.holdDown);
-    radioButton.fire('up');
+    radioButton.dispatchEvent(
+        new CustomEvent('up', {bubbles: true, composed: true}));
     assertFalse(
         radioButton.shadowRoot!.querySelector('paper-ripple')!.holdDown);
   });
--- ui/webui/resources/cr_elements/cr_radio_button/cr_radio_button.d.ts
+++ ui/webui/resources/cr_elements/cr_radio_button/cr_radio_button.d.ts
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {CrRadioButtonBehavior} from './cr_radio_button_behavior.js';
 
-interface CrRadioButtonElement extends LegacyElementMixin,
-                                       CrRadioButtonBehavior, HTMLElement {
+interface CrRadioButtonElement extends CrRadioButtonBehavior, PolymerElement {
   $: {
     button: HTMLElement,
   };
