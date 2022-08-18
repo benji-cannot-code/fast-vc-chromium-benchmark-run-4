f1d65c0b481b4d84e35ed7a787e3e1ac421cabaf
Nihar Majmudar <niharm@google.com>
2022-08-18 09:07:34 +0000

    Browsertests for ntp customize chrome button
    Browser tests for accessing customize chrome side panel through ntp.
    
    Change-Id: I9b9e0d0d1b12d94ace2fc54d812c786cf7dd07d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823498
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036518}

--- chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.js
+++ chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.js
@@ -55,6 +55,10 @@ TEST_F('NewTabPageAppTest', 'CustomizeUrl', function() {
   runMochaSuite('NewTabPageAppTest customize URL');
 });
 
+TEST_F('NewTabPageAppTest', 'CustomizeChromeSidePanel', function() {
+  runMochaSuite('NewTabPageAppTest customize chrome side panel');
+});
+
 var NewTabPageCustomizeDialogTest = class extends NewTabPageBrowserTest {
   /** @override */
   get browsePreload() {
