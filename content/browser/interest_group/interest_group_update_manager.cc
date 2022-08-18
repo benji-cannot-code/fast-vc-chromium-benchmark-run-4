a039bfbade77c8e17f89e7285914a98471d0cefb
Maks Orlovich <morlovich@chromium.org>
2022-08-18 16:05:45 +0000

    FLEDGE: Fix update to handle groupByOrigin mode
    Bug: 1330341
    Change-Id: I82c5050513c00e93ad12afbe13e4c9b3ae35e224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830753
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036626}

--- content/browser/interest_group/interest_group_update_manager.cc
+++ content/browser/interest_group/interest_group_update_manager.cc
@@ -109,6 +109,9 @@ constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
   if (*maybe_execution_mode == "compatibility") {
     interest_group_update.execution_mode =
         blink::InterestGroup::ExecutionMode::kCompatibilityMode;
+  } else if (*maybe_execution_mode == "groupByOrigin") {
+    interest_group_update.execution_mode =
+        blink::InterestGroup::ExecutionMode::kGroupedByOriginMode;
   } else {
     return false;
   }
