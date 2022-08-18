deb6f6fbc5b6afbcb71ee622e1d0c699226cf9cc
François Degros <fdegros@chromium.org>
2022-08-18 05:50:14 +0000

    [Files app] Make MountCondition a class enum
    With proper output operator for debugging and logging.
    
    BUG=chromium:1313346
    TEST=autoninja -C out/Default unit_tests browser_tests components_unittests ash_components_unittests chrome
    
    Change-Id: I562bdfc3a27e430f3cf2113daa2f30022f465e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835224
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036461}

--- ash/components/disks/disk_mount_manager.cc
+++ ash/components/disks/disk_mount_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -460,13 +461,13 @@ class DiskMountManagerImpl : public DiskMountManager,
       return;
     }
 
-    MountCondition mount_condition = MOUNT_CONDITION_NONE;
+    MountCondition mount_condition = MountCondition::kNone;
     if (entry.mount_type == MountType::kDevice) {
       if (entry.error_code == MountError::kUnknownFilesystem) {
-        mount_condition = MOUNT_CONDITION_UNKNOWN_FILESYSTEM;
+        mount_condition = MountCondition::kUnknownFilesystem;
       }
       if (entry.error_code == MountError::kUnsupportedFilesystem) {
-        mount_condition = MOUNT_CONDITION_UNSUPPORTED_FILESYSTEM;
+        mount_condition = MountCondition::kUnsupportedFilesystem;
       }
     }
 
@@ -476,12 +477,13 @@ class DiskMountManagerImpl : public DiskMountManager,
     // If the device is corrupted but it's still possible to format it, it will
     // be fake mounted.
     if (entry.error_code == MountError::kNone ||
-        mount_condition != MOUNT_CONDITION_NONE) {
+        mount_condition != MountCondition::kNone) {
       mount_points_.insert(mount_info);
     }
 
     Disk* disk = nullptr;
-    if ((entry.error_code == MountError::kNone || mount_info.mount_condition) &&
+    if ((entry.error_code == MountError::kNone ||
+         mount_info.mount_condition != MountCondition::kNone) &&
         mount_info.mount_type == MountType::kDevice &&
         !mount_info.source_path.empty() && !mount_info.mount_path.empty()) {
       Disks::iterator disk_map_iter = disks_.find(mount_info.source_path);
@@ -1058,6 +1060,20 @@ class DiskMountManagerImpl : public DiskMountManager,
 };
 
 }  // namespace
+
+std::ostream& operator<<(std::ostream& out, MountCondition condition) {
+  switch (condition) {
+#define PRINT(s)          \
+  case MountCondition::s: \
+    return out << #s;
+    PRINT(kNone)
+    PRINT(kUnknownFilesystem)
+    PRINT(kUnsupportedFilesystem)
+#undef PRINT
+  }
+
+  return out << static_cast<std::underlying_type_t<MountCondition>>(condition);
+}
 
 DiskMountManager::Observer::~Observer() {
   DCHECK(!IsInObserverList());
