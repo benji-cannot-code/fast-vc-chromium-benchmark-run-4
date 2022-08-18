8bd91bd845cbd6514c37ae610107aa50abbeedd6
Sammie Quon <sammiequon@chromium.org>
2022-08-18 17:44:59 +0000

    float: Hide multitask menu when entering tablet mode.
    It still shows up if we click/hover the size button in tablet mode, but
    I think instead of preventing it, we should hide the entire button. Will
    do that in a follow up.
    
    Also prevents the hover event from firing if the widget is not active.
    
    Bug: 1347978
    Test: ash_unittests MultitaskMenuTest.MultitaskMenuClosesOnTabletMode
    Change-Id: Ie2ef62cb0578386b459b0eab34256d26e38935a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805943
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036680}

--- chromeos/ui/frame/caption_buttons/frame_size_button.cc
+++ chromeos/ui/frame/caption_buttons/frame_size_button.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/user_metrics.h"
 #include "chromeos/ui/base/display_util.h"
+#include "chromeos/ui/base/tablet_state.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "chromeos/ui/frame/caption_buttons/snap_controller.h"
 #include "chromeos/ui/wm/features.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/display/screen.h"
+#include "ui/display/tablet_state.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/canvas.h"
@@ -227,7 +229,9 @@ FrameSizeButton::FrameSizeButton(PressedCallback callback,
                                 HTMAXBUTTON),
       delegate_(delegate),
       set_buttons_to_snap_mode_delay_ms_(kSetButtonsToSnapModeDelayMs),
-      in_snap_mode_(false) {}
+      in_snap_mode_(false) {
+  display_observer_.emplace(this);
+}
 
 FrameSizeButton::~FrameSizeButton() = default;
 
@@ -337,7 +341,7 @@ void FrameSizeButton::StateChanged(views::Button::ButtonState old_state) {
   if (!chromeos::wm::features::IsFloatWindowEnabled())
     return;
 
-  if (GetState() == views::Button::STATE_HOVERED) {
+  if (GetState() == views::Button::STATE_HOVERED && GetWidget()->IsActive()) {
     base::OnceClosure cancel_animation = base::BindOnce(
         &FrameSizeButton::DestroyPieAnimation, base::Unretained(this));
     base::OnceClosure show_multitask_menu = base::BindOnce(
@@ -356,6 +360,15 @@ void FrameSizeButton::PaintButtonContents(gfx::Canvas* canvas) {
     pie_animation_->Paint(canvas);
 
   views::FrameCaptionButton::PaintButtonContents(canvas);
+}
+
+void FrameSizeButton::OnDisplayTabletStateChanged(display::TabletState state) {
+  if (state == display::TabletState::kEnteringTabletMode) {
+    pie_animation_.reset();
+    set_buttons_to_snap_mode_timer_.Stop();
+    if (multitask_menu_)
+      multitask_menu_->HideBubble();
+  }
 }
 
 const raw_ptr<MultitaskMenu> FrameSizeButton::GetMultitaskMenuForTesting() {
