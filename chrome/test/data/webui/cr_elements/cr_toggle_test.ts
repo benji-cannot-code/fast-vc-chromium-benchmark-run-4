6d59a7f070f4815f2a5eec25f4e78076cec65f97
Rebekah Potter <rbpotter@chromium.org>
2022-08-19 07:50:58 +0000

    WebUI: Migrate cr-toggle to TypeScript
    Bug: 1316438
    Change-Id: I068739e157f6281d76101d66374b6546739f6538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3837313
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036971}

--- chrome/test/data/webui/cr_elements/cr_toggle_test.ts
+++ chrome/test/data/webui/cr_elements/cr_toggle_test.ts
@@ -26,8 +26,7 @@ suite('cr-toggle', function() {
     assertTrue(toggle.hasAttribute('checked'));
     assertEquals('true', toggle.getAttribute('aria-pressed'));
     // Asserting that the toggle button has actually moved.
-    assertTrue(
-        getComputedStyle(toggle.$$('#knob')!).transform.includes('matrix'));
+    assertTrue(getComputedStyle(toggle.$.knob).transform.includes('matrix'));
   }
 
   function assertNotChecked() {
@@ -35,7 +34,7 @@ suite('cr-toggle', function() {
     assertEquals(null, toggle.getAttribute('checked'));
     assertEquals('false', toggle.getAttribute('aria-pressed'));
     // Asserting that the toggle button has not moved.
-    assertEquals('none', getComputedStyle(toggle.$$('#knob')!).transform);
+    assertEquals('none', getComputedStyle(toggle.$.knob).transform);
   }
 
   function assertDisabled() {
