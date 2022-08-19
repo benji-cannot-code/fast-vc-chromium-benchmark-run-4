9823947b3e903b62e4eb666fd82d573190c34c36
Steven Bennetts <stevenjb@chromium.org>
2022-08-19 00:35:36 +0000

    Cros: Update captive portal histograms
    1. Moves the Chrome OS network captive portal histograms to
       network/histograms.xml since they are strongly tied to the
       chromeos networking code and not directlyr elated to the
       chrome captive portal code or metrics. This requires an
       additional entry to _NAMESPACES_IN_MULTIPLE_FILES in
       tools/metrics/histograms/validate_format.py.
    2. Updates the expiration date of the moved CaptivePortal metrics.
    3. Converts calls to use functions instead of macros.
    
    BUG=b:242618993
    
    Change-Id: I0ed7edbb33c3f33318f7b8752ca27fc32dbf9a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830083
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Quick-Run: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036888}

--- chrome/browser/ash/net/network_portal_detector_impl.cc
+++ chrome/browser/ash/net/network_portal_detector_impl.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
@@ -367,14 +367,13 @@ void NetworkPortalDetectorImpl::OnAttemptCompleted(
                  << ", status=" << status
                  << ", response_code=" << response_code;
 
-  UMA_HISTOGRAM_ENUMERATION("CaptivePortal.NetworkPortalDetectorResult",
-                            status);
+  base::UmaHistogramEnumeration("Network.NetworkPortalDetectorResult", status);
   NetworkState::NetworkTechnologyType type =
       NetworkState::NetworkTechnologyType::kUnknown;
   if (status == CAPTIVE_PORTAL_STATUS_PORTAL) {
     if (network)
       type = network->GetNetworkTechnologyType();
-    UMA_HISTOGRAM_ENUMERATION("CaptivePortal.NetworkPortalDetectorType", type);
+    base::UmaHistogramEnumeration("Network.NetworkPortalDetectorType", type);
   }
 
   if (last_detection_status_ != status) {
--- chromeos/ash/components/network/network_state.cc
+++ chromeos/ash/components/network/network_state.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -668,13 +667,13 @@ void NetworkState::UpdateCaptivePortalState(const base::Value& properties) {
     shill_portal_state_ = PortalState::kUnknown;
   }
 
-  UMA_HISTOGRAM_ENUMERATION("CaptivePortal.NetworkStateResult",
-                            shill_portal_state_);
+  base::UmaHistogramEnumeration("Network.CaptivePortalResult",
+                                shill_portal_state_);
   if (shill_portal_state_ != PortalState::kOnline) {
     NET_LOG(EVENT) << "Shill captive portal state for: " << NetworkId(this)
                    << " = " << shill_portal_state_
                    << " ,status_code=" << status_code;
-    base::UmaHistogramSparse("CaptivePortal.NetworkStateStatusCode",
+    base::UmaHistogramSparse("Network.CaptivePortalStatusCode",
                              std::abs(status_code));
   }
 }
