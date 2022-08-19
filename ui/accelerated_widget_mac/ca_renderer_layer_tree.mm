cd3aae72adaa2f10cb49fddb3be439e26f6a0d33
Maggie Chen <magchen@chromium.org>
2022-08-19 07:49:15 +0000

    Enable CALayerTreeOptimization by default on Mac
    Bug: 1294936
    Change-Id: Ib48b5320cf3e6348c7ec93540710db2fca7c92b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840403
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036968}

--- ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
+++ ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
@@ -39,7 +39,7 @@ constexpr bool g_print_ca_layers = false;
 constexpr int kOutputLevel = 4;
 
 base::Feature kCALayerTreeOptimization{"CALayerTreeOptimization",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
 
 void RecordIOSurfaceHistograms(
     int changed_io_surfaces_during_commit,
