b9c5d1e92e7c370831ea0ee895d54406e74032c8
Cathy Li <chili@chromium.org>
2022-08-19 07:53:06 +0000

    Add nullcheck before getting daily visits count from history db.
    Bug: 1354342
    Change-Id: I83bb66026aef920e8980a542a83a15ab63a3bf82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838848
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036974}

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -1591,6 +1591,9 @@ HistoryLastVisitResult HistoryBackend::GetLastVisitToURL(const GURL& url,
 DailyVisitsResult HistoryBackend::GetDailyVisitsToHost(const GURL& host,
                                                        base::Time begin_time,
                                                        base::Time end_time) {
+  if (!db_) {
+    return {};
+  }
   return db_->GetDailyVisitsToHost(host, begin_time, end_time);
 }
 
