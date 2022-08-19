7abcf00b3acfda67989da253f6f8ca4236745438
Viktoriia Kovalova <vkovalova@google.com>
2022-08-19 14:23:50 +0000

    [Kiosk] Add UMA to continuously monitor memory usage stats
    This CL adds the following metrics:
     * Kiosk.DiskUsagePercentage
     * Kiosk.RamUsagePercentage
     * Kiosk.SwapUsagePercentage
    Those monitor the percentage of available disk space, RAM and swap
    memory, respectively. The values are logged once per hour.
    
    BUG=b:238176782
    TEST=unit_tests --gtest_filter="AppSessionTest*"
    
    Change-Id: I1d9578369d7ea6ce65b0824505c7fbe7d6b0e471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757740
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Viktoriia Kovalova <vkovalova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037094}

--- chrome/browser/chromeos/app_mode/app_session_metrics_service.h
+++ chrome/browser/chromeos/app_mode/app_session_metrics_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "components/prefs/pref_service.h"
 
 namespace chromeos {
@@ -20,10 +21,16 @@ extern const char kKioskSessionDurationNormalHistogram[];
 extern const char kKioskSessionDurationInDaysNormalHistogram[];
 extern const char kKioskSessionDurationCrashedHistogram[];
 extern const char kKioskSessionDurationInDaysCrashedHistogram[];
+extern const char kKioskRamUsagePercentageHistogram[];
+extern const char kKioskSwapUsagePercentageHistogram[];
+extern const char kKioskDiskUsagePercentageHistogram[];
 extern const char kKioskSessionLastDayList[];
 extern const char kKioskSessionStartTime[];
 
 extern const base::TimeDelta kKioskSessionDurationHistogramLimit;
+extern const base::TimeDelta kPeriodicMetricsInterval;
+
+class DiskSpaceCalculator;
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -47,7 +54,7 @@ class AppSessionMetricsService {
   explicit AppSessionMetricsService(PrefService* prefs);
   AppSessionMetricsService(AppSessionMetricsService&) = delete;
   AppSessionMetricsService& operator=(const AppSessionMetricsService&) = delete;
-  ~AppSessionMetricsService() = default;
+  ~AppSessionMetricsService();
 
   void RecordKioskSessionStarted();
   void RecordKioskSessionWebStarted();
@@ -60,6 +67,16 @@ class AppSessionMetricsService {
   bool IsKioskSessionRunning() const;
 
   void RecordKioskSessionStarted(KioskSessionState started_state);
+
+  void StartMetricsTimer();
+
+  void RecordPeriodicMetrics();
+
+  void RecordRamUsage() const;
+
+  void RecordSwapUsage() const;
+
+  void RecordDiskSpaceUsage() const;
 
   void RecordKioskSessionState(KioskSessionState state) const;
 
@@ -82,6 +99,12 @@ class AppSessionMetricsService {
   // either the session is successfully finished or crashed or on the next
   // session startup.
   base::Time start_time_;
+
+  // Invokes callback to record continuously monitored metrics. Starts when
+  // the kiosk session is started.
+  base::RepeatingTimer metrics_timer_;
+
+  const std::unique_ptr<DiskSpaceCalculator> disk_space_calculator_;
 };
 
 }  // namespace chromeos
