b012e3a4a3c73e7eaf1320df9d203499515a1acb
Roland Bock <rbock@google.com>
2022-08-19 12:02:50 +0000

    Prevent FakePrivateMembershipRlweClient outside of tests
    Use CHECK_IS_TEST to ensure that policy::FakePrivateMembershipRlweClient
    is not used in production.
    The real solution will be to fully remove this branch from production binaries.
    
    Bug: b/243111738
    Test: Untestable.
    Change-Id: I4231aa91c3ee2f2c680801966a912d579a72b346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840369
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037035}

--- chrome/browser/ash/login/enrollment/auto_enrollment_controller.cc
+++ chrome/browser/ash/login/enrollment/auto_enrollment_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/check_is_test.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -180,6 +181,7 @@ AutoEnrollmentController::AutoEnrollmentController() {
   // Create the PSM (private set membership) RLWE client factory depending on
   // whether switches::kEnterpriseUseFakePsmRlweClient is set.
   if (ShouldUseFakePsmRlweClient()) {
+    CHECK_IS_TEST();
     psm_rlwe_client_factory_ = std::make_unique<
         policy::FakePrivateMembershipRlweClient::FactoryImpl>();
   } else {
@@ -286,6 +288,7 @@ AutoEnrollmentController::RegisterProgressCallback(
 
 void AutoEnrollmentController::SetAutoEnrollmentClientFactoryForTesting(
     policy::AutoEnrollmentClient::Factory* auto_enrollment_client_factory) {
+  CHECK_IS_TEST();
   testing_auto_enrollment_client_factory_ = auto_enrollment_client_factory;
 }
 
