a1ffaa7d03a561313ec498cbdaf64071d206ab93
Matt Menke <mmenke@chromium.org>
2022-08-19 16:16:13 +0000

    [FLEDGE] Prioritization signals CL 3: InterestGroupStorage support.
    This CL adds support for storing / retrieving / updating the 3 new
    InterestGroup fields added in
    https://chromium-review.googlesource.com/c/chromium/src/+/3747284
    
    `prioritySignalsOverrides` has a new behavior in that updates merge the
    values from the update's JSON dictionary with the old values of the
    InterestGroup. Other fields are completely overwritten.
    
    Also adds the join time to the database, for age calculations.
    
    The fields still have no actual effect when running auctions.
    
    github PR: https://github.com/WICG/turtledove/pull/329
    
    Bug: 1343389
    Change-Id: I5185e0066f6518c9bd56734cd4066ca8d6f21564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755604
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037134}

--- content/browser/interest_group/interest_group_update.h
+++ content/browser/interest_group/interest_group_update.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_map.h"
 #include "content/common/content_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/interest_group/interest_group.h"
@@ -29,6 +30,12 @@ struct CONTENT_EXPORT InterestGroupUpdate {
   ~InterestGroupUpdate();
 
   absl::optional<double> priority;
+  absl::optional<bool> enable_bidding_signals_prioritization;
+  absl::optional<base::flat_map<std::string, double>> priority_vector;
+  // Unlike other fields, this is merged with the previous value, so can keep
+  // old overrides around. Keys mapped to nullopt are deleted.
+  absl::optional<base::flat_map<std::string, absl::optional<double>>>
+      priority_signals_overrides;
   absl::optional<blink::InterestGroup::ExecutionMode> execution_mode;
   absl::optional<GURL> bidding_url;
   absl::optional<GURL> bidding_wasm_helper_url;
