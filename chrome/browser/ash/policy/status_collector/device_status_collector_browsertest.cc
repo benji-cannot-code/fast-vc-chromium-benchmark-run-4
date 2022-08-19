1ea786920ea8edf9f9b2863fa1d25c91b254321b
Ahmed Nasr <anasr@google.com>
2022-08-19 15:11:41 +0000

    Populate total storage size from Spaced in device status collector.
    Size is rounded using RoundByteSize used for the UI.
    
    Bug: b:235136592, b:238441744
    Change-Id: I3bcf588a9dc0eb1ed4125f0c1795e2086d5890a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840686
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037109}

--- chrome/browser/ash/policy/status_collector/device_status_collector_browsertest.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector_browsertest.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/shill/shill_ipconfig_client.h"
 #include "chromeos/ash/components/dbus/shill/shill_profile_client.h"
 #include "chromeos/ash/components/dbus/shill/shill_service_client.h"
+#include "chromeos/ash/components/dbus/spaced/fake_spaced_client.h"
 #include "chromeos/ash/components/dbus/update_engine/fake_update_engine_client.h"
 #include "chromeos/ash/components/dbus/update_engine/update_engine_client.h"
 #include "chromeos/ash/components/dbus/userdataauth/userdataauth_client.h"
@@ -893,6 +894,7 @@ class DeviceStatusCollectorTest : public testing::Test {
     chromeos::TpmManagerClient::InitializeFake();
     chromeos::LoginState::Initialize();
     ash::cros_healthd::FakeCrosHealthd::Initialize();
+    ash::FakeSpacedClient::InitializeFake();
 
     ash::CiceroneClient::InitializeFake();
     ash::ConciergeClient::InitializeFake();
@@ -918,6 +920,7 @@ class DeviceStatusCollectorTest : public testing::Test {
     ash::UpdateEngineClient::Shutdown();
     ash::KioskAppManager::Shutdown();
     ash::cros_healthd::FakeCrosHealthd::Shutdown();
+    ash::FakeSpacedClient::Shutdown();
     TestingBrowserProcess::GetGlobal()->SetLocalState(nullptr);
 
     // Finish pending tasks.
@@ -2942,6 +2945,24 @@ TEST_F(DeviceStatusCollectorTest, TestStatefulPartitionInfo) {
             device_status_.stateful_partition_info().mount_source());
   EXPECT_EQ(fakeStatefulPartitionInfo.filesystem(),
             device_status_.stateful_partition_info().filesystem());
+}
+
+TEST_F(DeviceStatusCollectorTest, TestRootDeviceStorage) {
+  DisableDefaultSettings();
+  static constexpr int64_t kRootDeviceRoundedSize = 128LL * 1024 * 1024 * 1024;
+  static constexpr int64_t kRootDeviceSize = kRootDeviceRoundedSize - 85;
+  ash::FakeSpacedClient::Get()->set_root_device_size(kRootDeviceSize);
+
+  auto options = CreateEmptyDeviceStatusCollectorOptions();
+  RestartStatusCollector(std::move(options));
+  scoped_testing_cros_settings_.device_settings()->SetBoolean(
+      ash::kReportDeviceStorageStatus, true);
+
+  GetStatus();
+
+  ASSERT_TRUE(device_status_.has_root_device_total_storage_bytes());
+  EXPECT_EQ(device_status_.root_device_total_storage_bytes(),
+            kRootDeviceRoundedSize);
 }
 
 TEST_F(DeviceStatusCollectorTest, TestGraphicsStatus) {
