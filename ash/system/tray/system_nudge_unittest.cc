dddf4bc57d33a91d3f3e9b123b4b9e550b18d18f
Kevin Radtke <kradtke@chromium.org>
2022-08-19 02:58:25 +0000

    Nudge Shown Count Metric
    Implement `Ash.NotifierFramework.Nudge.ShownCount` metric to track how many times a specific SystemNudge is shown using its catalog name.
    Added `NudgeCatalogName` enum to enums.xml.
    
    Bug: 1353540
    Change-Id: I75a2eecb65aac24c02f3e1cb67274a045bbab0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840755
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036930}

--- ash/system/tray/system_nudge_unittest.cc
+++ ash/system/tray/system_nudge_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/system/tray/system_nudge_label.h"
 #include "ash/test/ash_test_base.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "ui/gfx/vector_icon_types.h"
 
 namespace ash {
@@ -27,9 +28,11 @@ gfx::VectorIcon kEmptyIcon;
 
 class TestSystemNudge : public SystemNudge {
  public:
-  explicit TestSystemNudge(bool anchor_status_area)
+  explicit TestSystemNudge(
+      bool anchor_status_area,
+      NudgeCatalogName catalog_name = NudgeCatalogName::kTestCatalogName)
       : SystemNudge(kNudgeName,
-                    NudgeCatalogName::kTestCatalogName,
+                    catalog_name,
                     kIconSize,
                     kIconLabelSpacing,
                     kNudgePadding,
@@ -50,6 +53,9 @@ class TestSystemNudge : public SystemNudge {
     return std::u16string();
   }
 };
+
+constexpr char kNudgeShownCountHistogramName[] =
+    "Ash.NotifierFramework.Nudge.ShownCount";
 
 }  // namespace
 
@@ -121,6 +127,24 @@ TEST_F(SystemNudgeTest, NudgeAnchorStatusArea) {
   nudge_bounds.Outset(kNudgeMargin);
   EXPECT_EQ(nudge_bounds.x(), display_bounds.x() + shelf_size);
   EXPECT_EQ(nudge_bounds.bottom(), display_bounds.bottom());
+}
+
+TEST_F(SystemNudgeTest, ShownCountMetric) {
+  base::HistogramTester histogram_tester;
+
+  const NudgeCatalogName catalog_name_1 = static_cast<NudgeCatalogName>(1);
+  const NudgeCatalogName catalog_name_2 = static_cast<NudgeCatalogName>(2);
+  TestSystemNudge nudge_1(/*anchor_status_area=*/true, catalog_name_1);
+  TestSystemNudge nudge_2(/*anchor_status_area=*/true, catalog_name_2);
+
+  nudge_1.Show();
+  histogram_tester.ExpectBucketCount(kNudgeShownCountHistogramName,
+                                     catalog_name_1, 1);
+
+  nudge_2.Show();
+  nudge_2.Show();
+  histogram_tester.ExpectBucketCount(kNudgeShownCountHistogramName,
+                                     catalog_name_2, 2);
 }
 
 }  // namespace ash
