c8b35742b98b34080af5fb59571b6cb724103d90
S. Ganesh <ganesh@chromium.org>
2022-08-19 22:08:51 +0000

    Run DM registration and policy fetch before all installs and updates
    This CL introduces a `GetDMPolicies` that is run to refresh the DM
    registration and policies before installs and updates. After
    `GetDMPolicies` runs, it calls on the corresponding `Internal` method to
    do the actual install or update.
    
    Bug: 1345407
    Change-Id: I11f875444c643c079915ed260c3171f710088e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841072
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037280}

--- chrome/updater/update_service_impl.h
+++ chrome/updater/update_service_impl.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
+class FilePath;
 class SequencedTaskRunner;
 class Version;
 }  // namespace base
@@ -84,6 +85,32 @@ class UpdateServiceImpl : public UpdateService {
 
   // Installs applications in the wake task based on the ForceInstalls policy.
   void ForceInstall(StateChangeCallback state_update, Callback callback);
+
+  // `GetDMPolicies` refreshes the DM registration and policies, and is called
+  // before all installs and updates.
+  void GetDMPolicies(base::OnceClosure callback);
+
+  // After `GetDMPolicies` completes, it calls on these `Internal` method to do
+  // the actual install or update.
+  void UpdateAllInternal(StateChangeCallback state_update, Callback callback);
+  void UpdateInternal(const std::string& app_id,
+                      const std::string& install_data_index,
+                      Priority priority,
+                      PolicySameVersionUpdate policy_same_version_update,
+                      StateChangeCallback state_update,
+                      Callback callback);
+  void InstallInternal(const RegistrationRequest& registration,
+                       const std::string& install_data_index,
+                       Priority priority,
+                       StateChangeCallback state_update,
+                       Callback callback);
+  void RunInstallerInternal(const std::string& app_id,
+                            const base::FilePath& installer_path,
+                            const std::string& install_args,
+                            const std::string& install_data,
+                            const std::string& install_settings,
+                            StateChangeCallback state_update,
+                            Callback callback);
 
   bool IsUpdateDisabledByPolicy(const std::string& app_id,
                                 Priority priority,
