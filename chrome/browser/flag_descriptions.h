be54ed14ff0c0521bd82242ea00caebc0614d6c0
Mohamed Amir Yosef <mamir@chromium.org>
2022-08-19 17:24:40 +0000

    [Passwords] Clean-up UPM Desktop MVP feature and about: flags
    The feature has been fully rolled out, and code has been cleaned-up.
    
    Bug: 1310270
    Change-Id: I6277e8c642d42d2f92a627d7663999f25c9ab947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835601
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037153}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1618,9 +1618,6 @@ extern const char kUnifiedSidePanelDescription[];
 extern const char kUnifiedPasswordManagerAndroidName[];
 extern const char kUnifiedPasswordManagerAndroidDescription[];
 
-extern const char kUnifiedPasswordManagerDesktopName[];
-extern const char kUnifiedPasswordManagerDesktopDescription[];
-
 extern const char kUnsafeWebGPUName[];
 extern const char kUnsafeWebGPUDescription[];
 
