f33abd268cd3f35efc46ce6000bf37271fcd1268
Artsiom Mitrokhin <amitrokhin@google.com>
2022-08-19 17:53:43 +0000

    glanceables: Add "up next" section with calendar events
    This CL adds initial layout/views for the widget. Integration
    with the calendar API will be done in a follow-up CL.
    
    Bug: 1353495
    Test: added to ash_unittests ("GlanceablesUpNextViewTest.*")
    Change-Id: Ibb4f1c89ab7a37fd73fbb7db1c630d039b14396c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838783
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037184}

--- ash/glanceables/glanceables_view.cc
+++ ash/glanceables/glanceables_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/glanceables/glanceables_restore_view.h"
+#include "ash/glanceables/glanceables_up_next_view.h"
 #include "ash/glanceables/glanceables_weather_view.h"
 #include "ash/glanceables/glanceables_welcome_label.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -62,8 +63,8 @@ GlanceablesView::GlanceablesView() {
   up_next_label_ = left_column->AddChildView(std::make_unique<views::Label>());
   SetupSectionLabel(up_next_label_);
   up_next_label_->SetText(l10n_util::GetStringUTF16(IDS_GLANCEABLES_UP_NEXT));
-
-  // TODO(crbug.com/1353119): Add calendar events.
+  up_next_view_ =
+      left_column->AddChildView(std::make_unique<GlanceablesUpNextView>());
 
   // Container for the views on the right.
   auto* right_column = container->AddChildView(std::make_unique<views::View>());
--- ash/glanceables/glanceables_view.h
+++ ash/glanceables/glanceables_view.h
@@ -17,6 +17,7 @@ class Label;
 namespace ash {
 
 class GlanceablesRestoreView;
+class GlanceablesUpNextView;
 class GlanceablesWeatherView;
 class GlanceablesWelcomeLabel;
 
@@ -34,6 +35,7 @@ class ASH_EXPORT GlanceablesView : public views::View {
 
   GlanceablesWelcomeLabel* welcome_label_for_test() { return welcome_label_; }
   GlanceablesWeatherView* weather_view_for_test() { return weather_view_; }
+  GlanceablesUpNextView* up_next_view_for_test() { return up_next_view_; }
 
  private:
   friend class GlanceablesTest;
@@ -42,6 +44,7 @@ class ASH_EXPORT GlanceablesView : public views::View {
   GlanceablesWelcomeLabel* welcome_label_ = nullptr;
   GlanceablesWeatherView* weather_view_ = nullptr;
   views::Label* up_next_label_ = nullptr;
+  GlanceablesUpNextView* up_next_view_ = nullptr;
   views::Label* restore_session_label_ = nullptr;
   GlanceablesRestoreView* restore_view_ = nullptr;
 };
