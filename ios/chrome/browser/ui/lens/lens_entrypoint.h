502705c85782b17b1cddbea85fb5ee3e6eac77d5
Jason Hu <hujasonx@google.com>
2022-08-19 20:56:22 +0000

    Add Lens camera entrypoint flags and histogram enum
    We are adding 3 flags, one for each of the desired entrypoints; the enum will be logged downstream, but needs to be submitted now so that it can be used in the api that downstream code implements (This cl will be submitted first, followed by a skeleton downstream implementation, followed by the necessary header change upstream)
    
    Bug: 1353430
    Change-Id: I204f4ba57a3cd50097062e89604f5219af6cf094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832405
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037257}

---
+++ ios/chrome/browser/ui/lens/lens_entrypoint.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_LENS_LENS_ENTRYPOINT_H_
+#define IOS_CHROME_BROWSER_UI_LENS_LENS_ENTRYPOINT_H_
+
+// Enum representing the possible Lens entrypoints on iOS.
+// Current values should not be renumbered. Please keep in sync with
+// "IOSLensEntrypoint" in src/tools/metrics/histograms/enums.xml.
+enum class LensEntrypoint {
+  ContextMenu = 0,
+  HomeScreenWidget = 1,
+  NewTabPage = 2,
+  Keyboard = 3,
+  kMaxValue = Keyboard,
+};
+
+extern const char kIOSLensEntrypoint[];
+
+#endif  // IOS_CHROME_BROWSER_UI_LENS_LENS_ENTRYPOINT_H_
