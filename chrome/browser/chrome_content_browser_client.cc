33e792848230d588de28d6132ef7f66f7f06a43e
Frank Liberato <liberato@chromium.org>
2022-08-19 18:41:22 +0000

    Check the window disposition before popup blocking
    ChromeContentBrowserClient didn't check `ConsiderForPopupBlocking`
    before trying to block the popup via `MaybeBlockPopup`.  Other call
    sites tend to do both.
    
    This was causing Picture in Picture windows to be subject to popup
    blocking, which is unexpected.  While we may choose to expand popup
    blocking to include PiP windows in some cases, it's unclear right
    now exactly what the restrictions will be.
    
    Also, it turns out that RenderFrameImpl was sending the window open
    disposition properly for PiP windows, so even fixing the above was
    not sufficient.
    
    Bug: 1351855
    Change-Id: Ifd4469551bb9d764a211a8f99bf4f55ac751d808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3837232
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037210}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3477,7 +3477,8 @@ bool ChromeContentBrowserClient::CanCreateWindow(
       frame_name, disposition, features, user_gesture, opener_suppressed);
   NavigateParams nav_params =
       blocked_params.CreateNavigateParams(opener->GetProcess(), web_contents);
-  return blocked_content::MaybeBlockPopup(
+  return !blocked_content::ConsiderForPopupBlocking(disposition) ||
+         blocked_content::MaybeBlockPopup(
              web_contents, &opener_top_level_frame_url,
              std::make_unique<ChromePopupNavigationDelegate>(
                  std::move(nav_params)),
