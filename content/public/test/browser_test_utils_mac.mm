29cabea2e4262edb99ca338f087a50ac2121ae2a
Tom Burgin <bur@chromium.org>
2022-08-19 20:56:32 +0000

    browser_tests: grant an NSApplicationActivationPolicy
    To fix NativeAppWindowCocoaBrowserTest.FrameColor. This test needs
    to activate windows. In order to do so an appropriate
    NSApplicationActivationPolicy must be set.
    
    This CL sets a process wide NSApplicationActivationPolicy of
    NSApplicationActivationPolicyAccessory. Do this early on before any
    tests are run. This will make seen any potential negative interactions
    in a reproducible manner.
    
    Bug: 1322741
    Change-Id: Ie749ccd45b5da9af59905df14cd0276227280370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838824
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Tom Burgin <bur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037258}

---
+++ content/public/test/browser_test_utils_mac.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/test/browser_test_utils.h"
+
+#import <AppKit/AppKit.h>
+
+namespace content {
+
+bool EnableNativeWindowActivation() {
+  // Do not downgrade the activation policy.
+  if (NSApp.activationPolicy > NSApplicationActivationPolicyProhibited) {
+    return true;
+  }
+
+  // NSApplicationActivationPolicyAccessory is the least permissive policy that
+  // still allows for programmatic activation.
+  return [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory]
+             ? true
+             : false;
+}
+
+}  // namespace content
