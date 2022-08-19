e01da01012a272690a67aee2802cd66032aeb0f0
Chung-Sheng Wu <chungsheng@google.com>
2022-08-19 07:56:49 +0000

    Register healthd data collector to service manager
    Register Healthd data collector service to chromeos service manager.
    With this, Healthd don't need to wait for chrome to set up the
    connection. Healthd can obtain the connection anytime from the service
    manager.
    
    Bug: b:230064284
    Change-Id: I957b06fa9a10baf0ac4f42392918e5dde63b2adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812976
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036981}

--- chromeos/ash/services/cros_healthd/private/cpp/data_collector.h
+++ chromeos/ash/services/cros_healthd/private/cpp/data_collector.h
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_SERVICES_CROS_HEALTHD_PRIVATE_CPP_DATA_COLLECTOR_H_
 
 #include "chromeos/ash/services/cros_healthd/private/mojom/cros_healthd_internal.mojom.h"
+#include "chromeos/components/mojo_service_manager/mojom/mojo_service_manager.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 
 namespace chromeos {
 namespace cros_healthd {
 namespace internal {
 
-class DataCollector : public mojom::ChromiumDataCollector {
+class DataCollector : public mojom::ChromiumDataCollector,
+                      public mojo_service_manager::mojom::ServiceProvider {
  public:
   // Delegate class to be replaced for testing.
   class Delegate {
@@ -39,10 +42,17 @@ class DataCollector : public mojom::ChromiumDataCollector {
   void GetTouchscreenDevices(GetTouchscreenDevicesCallback callback) override;
   void GetTouchpadLibraryName(GetTouchpadLibraryNameCallback callback) override;
 
+  // chromeos::mojo_service_manager::mojom::ServiceProvider overrides.
+  void Request(mojo_service_manager::mojom::ProcessIdentityPtr identity,
+               mojo::ScopedMessagePipeHandle receiver) override;
+
   // Pointer to the delegate.
   Delegate* const delegate_;
-  // The mojo receiver.
-  mojo::Receiver<mojom::ChromiumDataCollector> receiver_{this};
+  // The mojo receiver of service provider.
+  mojo::Receiver<chromeos::mojo_service_manager::mojom::ServiceProvider>
+      provider_receiver_{this};
+  // The mojo receiver set of data collector.
+  mojo::ReceiverSet<mojom::ChromiumDataCollector> receiver_set_;
 };
 
 }  // namespace internal
