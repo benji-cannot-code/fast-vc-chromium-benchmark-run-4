e434b2a0171461029936b90f664e1edca0cdef54
Abhijeet Kandalkar <abhijeet@igalia.com>
2022-08-19 17:23:11 +0000

    [mparch] Refactor share_service_impl.h/.cc
    During the development of CL https://crev.com/c/3826497, it was
    discovered that OnSafeBrowsingResultReceived could be marked as a
    private function.
    
    Bug: 1351726
    Change-Id: I4fa42cec4387c6ba2999f5381a4559946ee5b253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828180
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1037150}

--- chrome/browser/webshare/share_service_impl.h
+++ chrome/browser/webshare/share_service_impl.h
@@ -56,6 +56,7 @@ class ShareServiceImpl
              std::vector<blink::mojom::SharedFilePtr> files,
              ShareCallback callback) override;
 
+ private:
   void OnSafeBrowsingResultReceived(
       const std::string& title,
       const std::string& text,
@@ -64,7 +65,6 @@ class ShareServiceImpl
       ShareCallback callback,
       bool is_safe);
 
- private:
   ShareServiceImpl(content::RenderFrameHost& render_frame_host,
                    mojo::PendingReceiver<blink::mojom::ShareService> receiver);
   ~ShareServiceImpl() override;
