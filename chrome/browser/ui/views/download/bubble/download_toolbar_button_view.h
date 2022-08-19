7605efada7eef0d8c57c61c93e4ca2de1614a813
Xinghui Lu <xinghuilu@chromium.org>
2022-08-19 18:49:31 +0000

    [DownloadBubble] Adjust toolbar active color based on theme.
    before: http://screen/QrAPdAVytvJu6Ec
    after: http://screen/B3ffSyAh7JMgkHW
    
    Bug: 1354650
    Change-Id: I04ba7edfdfdd2f470cd241183dbcd35e42165adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842692
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037212}

--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h
@@ -58,6 +58,7 @@ class DownloadToolbarButtonView : public ToolbarButton,
 
   // ToolbarButton:
   void UpdateIcon() override;
+  void OnThemeChanged() override;
 
   // DownloadBubbleNavigationHandler:
   void OpenPrimaryDialog() override;
