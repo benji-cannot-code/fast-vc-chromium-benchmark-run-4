d3387534df81a7794d14057c9e2dfbcb06b49332
Mohamed Amir Yosef <mamir@chromium.org>
2022-08-19 12:45:49 +0000

    [Passwords] Clean up UPM from Signin WebUI
    In addition, this CL remove all obsolete resources (images and strings)
    
    Bug: 1310270
    Change-Id: I049c74c6329fba49ddd59034747862e5979f524c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829689
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037051}

--- chrome/browser/ui/webui/signin/signin_reauth_ui.cc
+++ chrome/browser/ui/webui/signin/signin_reauth_ui.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/signin_resources.h"
-#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/identity_manager/account_info.h"
@@ -99,12 +98,7 @@ SigninReauthUI::SigninReauthUI(content::WebUI* web_ui)
   webui::SetJSModuleDefaults(source);
   source->SetDefaultResource(IDR_SIGNIN_SIGNIN_REAUTH_SIGNIN_REAUTH_HTML);
 
-  // TODO(crbug.com/1310270): Switch kResources back to consexpr after launching
-  // this feature.
-  bool upm_enabled = base::FeatureList::IsEnabled(
-      password_manager::features::kUnifiedPasswordManagerDesktop);
-
-  static webui::ResourcePath kResources[] = {
+  static constexpr webui::ResourcePath kResources[] = {
       {"signin_reauth_app.js", IDR_SIGNIN_SIGNIN_REAUTH_SIGNIN_REAUTH_APP_JS},
       {"signin_reauth_app.html.js",
        IDR_SIGNIN_SIGNIN_REAUTH_SIGNIN_REAUTH_APP_HTML_JS},
@@ -114,13 +108,9 @@ SigninReauthUI::SigninReauthUI(content::WebUI* web_ui)
       {"signin_vars.css.js", IDR_SIGNIN_SIGNIN_VARS_CSS_JS},
       // Resources for the account passwords reauth.
       {"images/signin_reauth_illustration.svg",
-       upm_enabled
-           ? IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_V2_SVG
-           : IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_SVG},
+       IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_SVG},
       {"images/signin_reauth_illustration_dark.svg",
-       upm_enabled
-           ? IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_DARK_V2_SVG
-           : IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_DARK_SVG},
+       IDR_SIGNIN_SIGNIN_REAUTH_IMAGES_ACCOUNT_PASSWORDS_REAUTH_ILLUSTRATION_DARK_SVG},
   };
   source->AddResourcePaths(kResources);
 
