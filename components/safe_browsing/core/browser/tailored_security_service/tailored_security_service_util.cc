dbf19f3449c5e0eb611540c366077b84f371a1f7
Joemer Ramos <joemerramos@chromium.org>
2022-08-19 14:34:00 +0000

    [iOS] Move MaybeNotifySyncUser impl into TailoredSecurityService
    Made MaybeNotifySyncUser a virtual function instead of a pure virtual
    function. The function was being used cross platform and was duplicating
    code. To avoid duplication, we move the base implementation into
    TailoredSecurityService to be shared throughout both subclasses and
    future subclasses. If new subclasses want custom functionality, they
    can always override the function since it's still virtual.
    
    Bug: 1353367
    Change-Id: Id227ec89af4129c5dbd83002e39e2cc46bb09e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833510
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037098}

---
+++ components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_util.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_util.h"
+
+#include "base/metrics/histogram_functions.h"
+#include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_notification_result.h"
+
+namespace safe_browsing {
+
+void RecordEnabledNotificationResult(
+    TailoredSecurityNotificationResult result) {
+  base::UmaHistogramEnumeration(
+      "SafeBrowsing.TailoredSecurity.SyncPromptEnabledNotificationResult2",
+      result);
+}
+
+}  // namespace safe_browsing
--- ios/chrome/browser/safe_browsing/tailored_security/chrome_tailored_security_service.mm
+++ ios/chrome/browser/safe_browsing/tailored_security/chrome_tailored_security_service.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/histogram_functions.h"
 #import "components/prefs/pref_service.h"
 #import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_notification_result.h"
+#import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_util.h"
 #import "components/safe_browsing/core/common/features.h"
 #import "components/safe_browsing/core/common/safe_browsing_policy_handler.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
@@ -23,19 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-namespace {
-
-// Records an UMA Histogram value to count the result of trying to notify a sync
-// user about enhanced protection for the enable case.
-void RecordEnabledNotificationResult(
-    TailoredSecurityNotificationResult result) {
-  base::UmaHistogramEnumeration(
-      "SafeBrowsing.TailoredSecurity.SyncPromptEnabledNotificationResult2",
-      result);
-}
-
-}  // namespace
-
 ChromeTailoredSecurityService::ChromeTailoredSecurityService(
     ChromeBrowserState* browser_state)
     : TailoredSecurityService(
@@ -44,45 +32,6 @@ ChromeTailoredSecurityService::ChromeTailoredSecurityService(
       browser_state_(browser_state) {}
 
 ChromeTailoredSecurityService::~ChromeTailoredSecurityService() = default;
-
-void ChromeTailoredSecurityService::MaybeNotifySyncUser(
-    bool is_enabled,
-    base::Time previous_update) {
-  if (!base::FeatureList::IsEnabled(kTailoredSecurityIntegration))
-    return;
-
-  if (!identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync)) {
-    if (is_enabled) {
-      RecordEnabledNotificationResult(
-          TailoredSecurityNotificationResult::kAccountNotConsented);
-    }
-    return;
-  }
-
-  if (SafeBrowsingPolicyHandler::IsSafeBrowsingProtectionLevelSetByPolicy(
-          browser_state_->GetPrefs())) {
-    if (is_enabled) {
-      RecordEnabledNotificationResult(
-          TailoredSecurityNotificationResult::kSafeBrowsingControlledByPolicy);
-    }
-    return;
-  }
-
-  if (is_enabled && IsEnhancedProtectionEnabled(*prefs())) {
-    RecordEnabledNotificationResult(
-        TailoredSecurityNotificationResult::kEnhancedProtectionAlreadyEnabled);
-  }
-
-  if (is_enabled && !IsEnhancedProtectionEnabled(*prefs())) {
-    ShowSyncNotification(true);
-  }
-
-  if (!is_enabled && IsEnhancedProtectionEnabled(*prefs()) &&
-      prefs()->GetBoolean(
-          prefs::kEnhancedProtectionEnabledViaTailoredSecurity)) {
-    ShowSyncNotification(false);
-  }
-}
 
 void ChromeTailoredSecurityService::ShowSyncNotification(bool is_enabled) {
   std::unique_ptr<web::WebState> web_state =
