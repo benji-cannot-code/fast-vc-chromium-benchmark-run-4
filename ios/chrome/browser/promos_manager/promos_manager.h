5288dc144550924e5fd2f11729fa3fd0d7c8fc0b
Benjamin Williams <bwwilliams@google.com>
2022-08-20 00:58:18 +0000

    Unit test LastSeenDay().
    This change introduces a way to unit test private methods of
    PromosManager via FRIEND_TEST_ALL_PREFIXES.
    
    These tests ensure the correct, most recently seen day is returned;
    ensures the sentinel value is returned when the given `promo` is not
    found in the impressions list.
    
    Change-Id: I5930dea6a8582d88630d5296a6550eab6ebbabb3
    Bug: 1354246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816864
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037336}

--- ios/chrome/browser/promos_manager/promos_manager.h
+++ ios/chrome/browser/promos_manager/promos_manager.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/promos_manager/constants.h"
 #import "ios/chrome/browser/promos_manager/impression_limit.h"
 
+class PromosManagerTest;
+
 // Centralized promos manager for coordinating and scheduling the display of
 // app-wide promos. Feature teams interested in displaying promos should
 // leverage this manager.
@@ -55,6 +57,12 @@ class PromosManager {
   int LastSeenDay(
       promos_manager::Promo promo,
       std::vector<promos_manager::Impression>& sorted_impressions) const;
+
+  // Allow unit tests to access private methods.
+  friend class PromosManagerTest;
+  FRIEND_TEST_ALL_PREFIXES(PromosManagerTest, ReturnsLastSeenDayForPromo);
+  FRIEND_TEST_ALL_PREFIXES(PromosManagerTest,
+                           ReturnsSentinelForNonExistentPromo);
 };
 
 #endif  // IOS_CHROME_BROWSER_PROMOS_MANAGER_PROMOS_MANAGER_H_
