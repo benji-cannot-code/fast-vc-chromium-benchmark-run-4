fb6e72efa010a8eab42b70817d442c319de9f01b
rudranshd <rudranshd@google.com>
2022-08-20 01:22:29 +0000

    [CrOS WiFi] Implement HiddenNetworkHandler::CleanHiddenNetworks
    Implement HiddenNetworkHandler::CleanHiddenNetworks to find and remove
    wrongly hidden networks.
    
    removal with mock WiFi networks.
    
    Bug: b/236148589
    Test: [1] Tests written to verify functionality of hidden network
    Change-Id: I01e24ba2dbaf0e4686f112dca5e0ef970ace6fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787020
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Rudransh Dikshit <rudranshd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037341}

--- chromeos/ash/components/network/hidden_network_handler.h
+++ chromeos/ash/components/network/hidden_network_handler.h
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_NETWORK_HIDDEN_NETWORK_HANDLER_H_
 
 #include "base/component_export.h"
+#include "base/timer/timer.h"
 
 namespace ash {
 
 class NetworkStateHandler;
 class NetworkConfigurationHandler;
+class NetworkMetadataStore;
 
 // This class is responsible for removing wrongly hidden networks by
 // performing network updates daily using a timer. Networks are
@@ -28,10 +30,20 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HiddenNetworkHandler {
 
   void Init(NetworkStateHandler* network_state_handler,
             NetworkConfigurationHandler* network_configuration_handler);
+  // This method will update the NetworkMetadataStore used when querying for
+  // metadata about the network, and will result in immediately checking
+  // for any hidden and wrongly configured networks.
+  void SetNetworkMetadataStore(NetworkMetadataStore* network_metadata_store);
 
  private:
-  NetworkStateHandler* network_state_handler_;
-  NetworkConfigurationHandler* network_configuration_handler_;
+  void CleanHiddenNetworks();
+
+  // Timer ensures that wrongly configured networks are searched for on
+  // a daily basis.
+  base::RepeatingTimer daily_event_timer_;
+  NetworkStateHandler* network_state_handler_ = nullptr;
+  NetworkConfigurationHandler* network_configuration_handler_ = nullptr;
+  NetworkMetadataStore* network_metadata_store_ = nullptr;
 };
 
 }  // namespace ash
--- chromeos/ash/components/network/network_metadata_store.cc
+++ chromeos/ash/components/network/network_metadata_store.cc
@@ -405,7 +405,7 @@ base::Time NetworkMetadataStore::UpdateAndRetrieveWiFiTimestamp(
 
   if (!network || network->GetNetworkTechnologyType() !=
                       NetworkState::NetworkTechnologyType::kWiFi) {
-    return base::Time::UnixEpoch();
+    return base::Time::Now().UTCMidnight();
   }
 
   const base::Value* creation_timestamp =
