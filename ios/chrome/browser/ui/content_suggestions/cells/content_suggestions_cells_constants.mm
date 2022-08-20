67fc1978a7b07736d4b1bfe7a5341fb2430e8eee
Chris Lu <thegreenfrog@chromium.org>
2022-08-20 01:12:48 +0000

    Reland "[ios] Fix tests for kTrendingQueriesModule"
    This is a reland of commit 8eea7a0bd9b8839ad75284147e153d3ed68a5a93
    
    Original change's description:
    > [ios] Fix tests for kTrendingQueriesModule
    >
    > Forces a particular experiment arm that doesn't hide Shortcuts. Also,
    > a test is added to validate the Trending Queries module.
    >
    > Bug: 1350826, 1352158
    > Change-Id: Ie44481ae884018361e3916d793fe0b444068c269
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817208
    > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035246}
    
    Bug: 1350826, 1352158
    Change-Id: Ia8dcc0940b410c268521b8ff892138cf5adea314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832193
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037338}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.mm
@@ -13,3 +13,6 @@ const CGSize kReturnToRecentTabSize = {/*width=*/343, /*height=*/72};
 
 NSString* const kContentSuggestionsWhatsNewIdentifier =
     @"ContentSuggestionsWhatsNewIdentifier";
+
+NSString* const kQuerySuggestionViewA11yIdentifierPrefix =
+    @"QuerySuggestionViewA11yIdentifierPrefix";
