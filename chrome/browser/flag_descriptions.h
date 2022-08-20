9792edb2b3e8557a7a5967f3bb30e7fb68d6276a
Martin Kreichgauer <martinkr@google.com>
2022-08-20 00:39:59 +0000

    extensions: expose kLoadCryptoTokenExtension feature in chrome:flags
    This might be a useful escape hatch in case `chrome.runtime` becoming
    undefined as a side effect of CryptoToken removal turns out to break
    users.
    
    Fixed: 1354318
    Change-Id: I4799f5e857877b69c2a9439cfcc6d7b1b829d292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840728
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037330}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -157,6 +157,9 @@ extern const char kU2FPermissionPromptDescription[];
 extern const char kU2FSecurityKeyAPIName[];
 extern const char kU2FSecurityKeyAPIDescription[];
 
+extern const char kLoadCryptoTokenExtensionName[];
+extern const char kLoadCryptoTokenExtensionDescription[];
+
 extern const char kUpcomingSharingFeaturesName[];
 extern const char kUpcomingSharingFeaturesDescription[];
 
