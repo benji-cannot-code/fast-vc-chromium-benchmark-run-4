c1e2ceddc74963abc8092c3349ed7364f2ca9202
Norge Vizcay <vizcay@google.com>
2022-08-21 15:15:07 +0000

    [FastCheckout] Introduce bottom sheet home page layout.
    Bug: 1334642
    Change-Id: I8c7de5d3a748b5df4add64919c5bc0251b0ffa89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810560
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037491}

--- chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutSheetContent.java
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutSheetContent.java
@@ -24,14 +24,6 @@ public class FastCheckoutSheetContent implements BottomSheetContent {
         mContentView = contentView;
     }
 
-    /**
-     * Sets the screen to show on the bottom sheet.
-     * @param screenType A {@link ScreenType} specifying the screen to show.
-     */
-    void updateCurrentScreen(int screenType) {
-        // TODO(crbug.com/1334642): Implement.
-    }
-
     @Override
     public View getContentView() {
         return mContentView;
@@ -55,7 +47,7 @@ public class FastCheckoutSheetContent implements BottomSheetContent {
 
     @Override
     public int getPriority() {
-        return BottomSheetContent.ContentPriority.HIGH;
+        return ContentPriority.HIGH;
     }
 
     @Override
@@ -70,19 +62,19 @@ public class FastCheckoutSheetContent implements BottomSheetContent {
 
     @Override
     public int getPeekHeight() {
-        return BottomSheetContent.HeightMode.DISABLED;
+        return HeightMode.DISABLED;
     }
 
     @Override
     public float getFullHeightRatio() {
         // TODO(crbug.com/1334642): Implement.
-        return HeightMode.DEFAULT;
+        return HeightMode.WRAP_CONTENT;
     }
 
     @Override
     public float getHalfHeightRatio() {
         // TODO(crbug.com/1334642): Implement.
-        return BottomSheetContent.HeightMode.DEFAULT;
+        return HeightMode.WRAP_CONTENT;
     }
 
     @Override
