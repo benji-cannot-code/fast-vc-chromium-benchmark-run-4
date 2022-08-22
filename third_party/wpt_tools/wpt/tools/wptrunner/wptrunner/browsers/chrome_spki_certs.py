84a83b8542839bbb0d6f423ad9def3511c20de69
Jie Sheng <jiesheng@google.com>
2022-08-22 19:43:32 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: e238ab918eac3ef48132d5c1cf9e161428f11b11
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,android-weblayer-pie-x86-wpt-smoketest,linux-blink-rel
    Bug: 1351820
    Change-Id: I5577ab7d3278b150608e9c5f19c973c0cd9c6b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838727
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037890}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/chrome_spki_certs.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/chrome_spki_certs.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # DO NOT EDIT MANUALLY.
 
 # tools/certs/web-platform.test.pem
-WPT_FINGERPRINT = 'KGfpbZyVqJ6ztPLyXNAGcQwsl5OT7ecOpTKsgd01bNM='
+WPT_FINGERPRINT = 'XreVR++++c9QamuUZu0YWHyqsL3PJarhG/0h87zEimI='
 
 # signed-exchange/resources/127.0.0.1.sxg.pem
 SXG_WPT_FINGERPRINT = '0Rt4mT6SJXojEMHTnKnlJ/hBKMBcI4kteBlhR1eTTdk='
