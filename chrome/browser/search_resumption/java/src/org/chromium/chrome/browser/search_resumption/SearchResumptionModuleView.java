7d73935e69af320f02defe73b38372749f3ece52
Xi Han <hanxi@google.com>
2022-08-22 15:10:11 +0000

    [SearchResumption] Fix header isn't reading properly in talkback.
    The bug was caused by connecting two strings together without a space
    between them. It leads to two words read as one in talkback.
    
    Bug: 1355218
    Change-Id: Idb6eb19b2b2277bcdefd10cfc90f3ead6dc2f16e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845186
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037732}

--- chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleView.java
+++ chrome/browser/search_resumption/java/src/org/chromium/chrome/browser/search_resumption/SearchResumptionModuleView.java
@@ -70,7 +70,7 @@ public class SearchResumptionModuleView extends LinearLayout {
                             : R.string.accessibility_collapsed);
             String description = getContext().getResources().getString(
                     R.string.search_resumption_module_subtitle);
-            mHeaderView.setContentDescription(description + collapseOrExpandedText);
+            mHeaderView.setContentDescription(description + " " + collapseOrExpandedText);
         }
 
         if (mTileContainerView.isExpanded() == shouldExpand) return;
