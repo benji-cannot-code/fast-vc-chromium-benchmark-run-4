5bdccd32575de90616e02fa7d07f3e3694b60ce9
Sorin Jianu <sorin@chromium.org>
2022-08-22 19:37:19 +0000

    Fix sequencing of PolicyStatusImpl COM calls.
    This CL makes the PolicyService calls in the main sequence instead of
    using the OS RPC thread.
    
    Bug: 1353927
    Change-Id: I6d0f78f00dbb09ec2bcebfc4912ac5bd8a859f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841116
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037886}

--- chrome/updater/configurator.h
+++ chrome/updater/configurator.h
@@ -40,6 +40,8 @@ class ExternalConstants;
 class PolicyService;
 class UpdaterPrefs;
 
+// This class is free-threaded. Its instance is shared by multiple sequences and
+// it can't be mutated.
 class Configurator : public update_client::Configurator {
  public:
   Configurator(scoped_refptr<UpdaterPrefs> prefs,
@@ -83,6 +85,8 @@ class Configurator : public update_client::Configurator {
   crx_file::VerifierFormat GetCrxVerifierFormat() const;
 
   // This reloads the policy managers.
+  // TODO(crbug.com/1353925) - eliminate this function because the instance
+  // of the `Configurator` can't be mutated.
   void ResetPolicyService();
 
  private:
