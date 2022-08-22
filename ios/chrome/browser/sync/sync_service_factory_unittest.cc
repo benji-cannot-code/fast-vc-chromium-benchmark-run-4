773f9c02444bd7ef4c5ee7afb06364a58d04cce8
Florian Leimgruber <fleimgruber@google.com>
2022-08-22 09:08:45 +0000

    Add ContactInfo to model types.
    This CL corresponds to steps 7-11 of the sync integration checklist [1].
    
    The data type won't be encrypted and will use the Autofill Address
    settings toggle for now.
    
    [1] https://www.chromium.org/developers/design-documents/sync/model-api/#sync-integration-checklist
    
    Bug: 1348294
    Change-Id: Id5e8352720b6b065400b515eaea8b06a2c34ad5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829146
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037620}

--- ios/chrome/browser/sync/sync_service_factory_unittest.cc
+++ ios/chrome/browser/sync/sync_service_factory_unittest.cc
@@ -54,7 +54,7 @@ class SyncServiceFactoryTest : public PlatformTest {
  protected:
   // Returns the collection of default datatypes.
   syncer::ModelTypeSet DefaultDatatypes() {
-    static_assert(40 == syncer::GetNumModelTypes(),
+    static_assert(41 == syncer::GetNumModelTypes(),
                   "When adding a new type, you probably want to add it here as "
                   "well (assuming it is already enabled).");
 
@@ -68,6 +68,7 @@ class SyncServiceFactoryTest : public PlatformTest {
     datatypes.Put(syncer::AUTOFILL_WALLET_METADATA);
     datatypes.Put(syncer::AUTOFILL_WALLET_OFFER);
     datatypes.Put(syncer::BOOKMARKS);
+    // TODO(crbug.com/1348294): Add CONTACT_INFO once it has a controller.
     datatypes.Put(syncer::DEVICE_INFO);
     if (base::FeatureList::IsEnabled(syncer::kSyncEnableHistoryDataType)) {
       datatypes.Put(syncer::HISTORY);
