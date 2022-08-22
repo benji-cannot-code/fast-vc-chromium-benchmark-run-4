5bc002f16f30e30d764c030eccfeb8aa424a84d6
Alan Cutter <alancutter@chromium.org>
2022-08-22 16:13:39 +0000

    Rename PreinstalledAppsUninstalledByUserConfigs in chrome://web-app-internals
    This CL renames PreinstalledAppsUninstalledByUserConfigs section to
    UserUninstalledPreinstalledWebAppPrefs in chrome://web-app-internals.
    
    The new name matches the name of the
    kUserUninstalledPreinstalledWebAppPref pref it is associated with.
    
    Change-Id: I1d8d1f6afb874891a2c6565f7e6fdc30ca20f48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3800568
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037776}

--- chrome/browser/ui/webui/web_app_internals/web_app_internals_source.cc
+++ chrome/browser/ui/webui/web_app_internals/web_app_internals_source.cc
@@ -37,8 +37,8 @@ namespace {
 // New fields must be added to BuildIndexJson().
 constexpr char kInstalledWebApps[] = "InstalledWebApps";
 constexpr char kPreinstalledWebAppConfigs[] = "PreinstalledWebAppConfigs";
-constexpr char kPreinstalledAppsUninstalledByUserConfigs[] =
-    "PreinstalledAppsUninstalledByUserConfigs";
+constexpr char kUserUninstalledPreinstalledWebAppPrefs[] =
+    "UserUninstalledPreinstalledWebAppPrefs";
 constexpr char kExternallyManagedWebAppPrefs[] = "ExternallyManagedWebAppPrefs";
 constexpr char kCommandManager[] = "CommandManager";
 constexpr char kIconErrorLog[] = "IconErrorLog";
@@ -66,7 +66,7 @@ base::Value BuildIndexJson() {
 
   index.Append(kInstalledWebApps);
   index.Append(kPreinstalledWebAppConfigs);
-  index.Append(kPreinstalledAppsUninstalledByUserConfigs);
+  index.Append(kUserUninstalledPreinstalledWebAppPrefs);
   index.Append(kExternallyManagedWebAppPrefs);
   index.Append(kCommandManager);
   index.Append(kIconErrorLog);
@@ -195,9 +195,9 @@ base::Value BuildExternallyManagedWebAppPrefsJson(Profile* profile) {
   return root;
 }
 
-base::Value BuildPreinstalledAppsUninstalledByUserJson(Profile* profile) {
+base::Value BuildUserUninstalledPreinstalledWebAppPrefsJson(Profile* profile) {
   base::Value::Dict root;
-  root.Set(kPreinstalledAppsUninstalledByUserConfigs,
+  root.Set(kUserUninstalledPreinstalledWebAppPrefs,
            profile->GetPrefs()
                ->GetValueDict(prefs::kUserUninstalledPreinstalledWebAppPref)
                .Clone());
@@ -328,8 +328,8 @@ void WebAppInternalsSource::BuildWebAppInternalsJson(
   root.Append(BuildIndexJson());
   root.Append(BuildInstalledWebAppsJson(*provider));
   root.Append(BuildPreinstalledWebAppConfigsJson(*provider));
+  root.Append(BuildUserUninstalledPreinstalledWebAppPrefsJson(profile));
   root.Append(BuildExternallyManagedWebAppPrefsJson(profile));
-  root.Append(BuildPreinstalledAppsUninstalledByUserJson(profile));
   root.Append(BuildCommandManagerJson(*provider));
   root.Append(BuildIconErrorLogJson(*provider));
   root.Append(BuildInstallProcessErrorLogJson(*provider));
