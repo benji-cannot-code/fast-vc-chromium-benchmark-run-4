03e2d0409c1a9729d96fea3096f05b90a77571ed
Marc Treib <treib@chromium.org>
2022-08-22 11:15:54 +0000

    History Sync: Plumb page_language into the history DB
    The DB column already existed (in the content_annotations table), but
    wasn't populated yet.
    This CL makes HistoryTabHelper a LanguageDetectionObserver so it gets
    notified when the language is determined, and plumbs it into the DB.
    
    Bug: 1347012
    Change-Id: Iffa7a890c065bb1cf31bb0f4fe5b9e22f9b54848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3836338
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037658}

--- chrome/browser/history/history_tab_helper.cc
+++ chrome/browser/history/history_tab_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_host/chrome_navigation_ui_data.h"
+#include "chrome/browser/translate/chrome_translate_client.h"
 #include "components/history/content/browser/history_context_helper.h"
 #include "components/history/core/browser/history_constants.h"
 #include "components/history/core/browser/history_service.h"
@@ -148,7 +149,13 @@ history::VisitContextAnnotations::BrowserType GetBrowserType(
 
 HistoryTabHelper::HistoryTabHelper(WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
-      content::WebContentsUserData<HistoryTabHelper>(*web_contents) {}
+      content::WebContentsUserData<HistoryTabHelper>(*web_contents) {
+  // A translate client is not always attached to web contents (e.g. tests).
+  if (ChromeTranslateClient* translate_client =
+          ChromeTranslateClient::FromWebContents(web_contents)) {
+    translate_observation_.Observe(translate_client->GetTranslateDriver());
+  }
+}
 
 HistoryTabHelper::~HistoryTabHelper() = default;
 
@@ -386,6 +393,20 @@ void HistoryTabHelper::DidOpenRequestedURL(
   new_history_tab_helper->opener_web_contents_ = web_contents()->GetWeakPtr();
 }
 
+void HistoryTabHelper::OnLanguageDetermined(
+    const translate::LanguageDetectionDetails& details) {
+  if (history::HistoryService* hs = GetHistoryService()) {
+    NavigationEntry* entry =
+        web_contents()->GetController().GetLastCommittedEntry();
+    if (entry) {
+      hs->SetPageLanguageForVisit(
+          history::ContextIDForWebContents(web_contents()),
+          entry->GetUniqueID(), web_contents()->GetLastCommittedURL(),
+          details.adopted_language);
+    }
+  }
+}
+
 void HistoryTabHelper::TitleWasSet(NavigationEntry* entry) {
   if (!entry)
     return;
@@ -419,6 +440,8 @@ history::HistoryService* HistoryTabHelper::GetHistoryService() {
 }
 
 void HistoryTabHelper::WebContentsDestroyed() {
+  translate_observation_.Reset();
+
   // We update the history for this URL.
   WebContents* tab = web_contents();
   Profile* profile = Profile::FromBrowserContext(tab->GetBrowserContext());
