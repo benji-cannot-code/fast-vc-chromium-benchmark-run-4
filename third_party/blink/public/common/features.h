8934d9832aec68fe5757e29b2ccf029f14726e69
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-08-22 11:23:34 +0000

    AnonymousIframe: update origin trial name.
    The previous name was gating the RuntimeEnabledFlag. It shipped early in
    M104 before the implementation was finished:
    https://chromiumdash.appspot.com/commit/a093730be618d5f21256e5919b8c05a3528ec9ae
    
    The problem is that OriginTrial do not depend on the version of Chrome.
    So opening the OriginTrial today would apply retroactively to past
    versions.
    
    The next time, we should use a second flag to prevent the OT from being
    used before the code is ready.
    
    This patch:
    - Rename "AnonymousIframe" into "AnonymousIframeOriginTrial".
    - Update the origin trial tests tokens
    - For extra safety, add a second flag to control the feature.
    
    Bug: 1318041
    Change-Id: Iec1b2ed37cb02b48ff3fd427d671a3bda02384c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834551
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037660}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace features {
 
+BLINK_COMMON_EXPORT extern const base::Feature kAnonymousIframeOriginTrial;
 BLINK_COMMON_EXPORT extern const base::Feature kAutomaticLazyFrameLoadingToAds;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<int>
     kTimeoutMillisForLazyAds;
