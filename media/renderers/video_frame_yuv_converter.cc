636257c176b02a21b5c1c8005ce18bcd8fc063d3
Christopher Cameron <ccameron@chromium.org>
2022-08-22 22:19:18 +0000

    VideoFrameYUVConverter: Disable color conversion
    Explicitly require that VideoFrameYUVConverter perform only YUV to RGB
    conversion, and no other color space conversion.
    
    Prior to this change, we explicitly forced the VideoFrame's color
    space to be sRGB. This behavior was consistent with the non-accelerated
    paths for Rec601 and Rec709 (although it was not necessarily correct),
    but undesirable (broken) for HDR formats. We explicitly disable color
    conversion here, so that we can correctly handle HDR formats.
    
    It is likely that we will want to change this behavior to automatically
    perform a specific color space conversion (e.g, for uploading to WebGL,
    while preserving the Rec601 and Rec709 behaviors) in the future. This
    can wait until the non-OOP-R canvas path is removed.
    
    In the non-OOP-R canvas path, color space conversion happened to be
    disabled prior to this change because the same color space was used for
    the YUV SkImage in VideoFrameYUVMailboxesHolder::VideoFrameToSkImage
    as was used for the SkSurface in VideoFrameYUVConverter::
    ConvertFromVideoFrameYUVWithGrContext. Make this more explicit.
    
    In the OOP-R canvas path, color space conversion was automatically
    performed by RasterInterface::ConvertYUVAMailboxesToRGB prior to this
    change. Change the color space parameter to nullptr to disable this.
    
    Bug: 1348566
    Change-Id: I1d33b63f2410963db62b16a1706323d914d2994d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816927
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037970}

--- media/renderers/video_frame_yuv_converter.cc
+++ media/renderers/video_frame_yuv_converter.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <GLES3/gl3.h>
 
+#include "base/logging.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "gpu/command_buffer/client/raster_interface.h"
@@ -68,7 +69,7 @@ GrGLenum GetSurfaceColorFormat(GrGLenum format, GrGLenum type) {
   return format;
 }
 
-bool DrawYUVImageToSkSurface(const VideoFrame* video_frame,
+void DrawYUVImageToSkSurface(const VideoFrame* video_frame,
                              sk_sp<SkImage> image,
                              sk_sp<SkSurface> surface,
                              bool use_visible_rect) {
@@ -89,7 +90,6 @@ bool DrawYUVImageToSkSurface(const VideoFrame* video_frame,
   }
 
   surface->flushAndSubmit();
-  return true;
 }
 
 }  // namespace
@@ -147,10 +147,10 @@ bool VideoFrameYUVConverter::ConvertYUVVideoFrame(
   gpu::Mailbox mailboxes[SkYUVAInfo::kMaxPlanes]{};
   holder_->VideoFrameToMailboxes(video_frame, raster_context_provider,
                                  mailboxes);
-  ri->ConvertYUVAMailboxesToRGB(
-      dest_mailbox_holder.mailbox, holder_->yuva_info().yuvColorSpace(),
-      SkColorSpace::MakeSRGB().get(), holder_->yuva_info().planeConfig(),
-      holder_->yuva_info().subsampling(), mailboxes);
+  ri->ConvertYUVAMailboxesToRGB(dest_mailbox_holder.mailbox,
+                                holder_->yuva_info().yuvColorSpace(), nullptr,
+                                holder_->yuva_info().planeConfig(),
+                                holder_->yuva_info().subsampling(), mailboxes);
   return true;
 }
 
@@ -197,22 +197,31 @@ bool VideoFrameYUVConverter::ConvertFromVideoFrameYUVWithGrContext(
   GrBackendTexture result_texture(result_width, result_height, GrMipMapped::kNo,
                                   result_gl_texture_info);
 
+  // Use the same SkColorSpace for the surface and image, so that no color space
+  // conversion is performed.
+  auto source_and_dest_color_space = SkColorSpace::MakeSRGB();
+
   // Use dst texture as SkSurface back resource.
   auto surface = SkSurface::MakeFromBackendTexture(
       gr_context, result_texture,
       gr_params.flip_y ? kBottomLeft_GrSurfaceOrigin : kTopLeft_GrSurfaceOrigin,
       1, GetCompatibleSurfaceColorType(result_gl_texture_info.fFormat),
-      SkColorSpace::MakeSRGB(), nullptr);
+      source_and_dest_color_space, nullptr);
 
   // Terminate if surface cannot be created.
-  bool result = true;
+  bool result = false;
   if (surface) {
-    auto image =
-        holder_->VideoFrameToSkImage(video_frame, raster_context_provider);
-    result = DrawYUVImageToSkSurface(video_frame, image, surface,
-                                     gr_params.use_visible_rect);
+    auto image = holder_->VideoFrameToSkImage(
+        video_frame, raster_context_provider, source_and_dest_color_space);
+    if (image) {
+      result = true;
+      DrawYUVImageToSkSurface(video_frame, image, surface,
+                              gr_params.use_visible_rect);
+    } else {
+      DLOG(ERROR) << "Failed to create YUV SkImage";
+    }
   } else {
-    result = false;
+    DLOG(ERROR) << "Failed to create SkSurface";
   }
 
   // Release textures to guarantee |holder_| doesn't hold read access on
