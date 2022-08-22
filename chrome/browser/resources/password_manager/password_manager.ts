6e26de0f5d28d65a7da2656872adcbe89ed79fa7
Viktor Semeniuk <vsemeniuk@google.com>
2022-08-22 20:50:33 +0000

    [Password Manager] Adding side bar with icons and strings
    Implementation: https://screenshot.googleplex.com/BpAPKciS4JToGyh
    Mocks: https://docs.google.com/presentation/d/1uuFSa-AeuV5yHzY22-DfPwfUVC7xPPQFR1YZtW-Dldo/edit?resourcekey=0-xHwhkZTV2t5Cqxwj7NxHkA#slide=id.g1198acd1012_0_218
    
    Bug: 1350947
    Change-Id: I7dc60f9660f46b1ae7e0125f02e539fa34f435f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825871
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037931}

--- chrome/browser/resources/password_manager/password_manager.ts
+++ chrome/browser/resources/password_manager/password_manager.ts
@@ -6,4 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './password_manager_app.js';
 
 export {PasswordManagerAppElement} from './password_manager_app.js';
+export {PasswordManagerSideBarElement} from './side_bar.js';
 export {PasswordManagerToolbarElement} from './toolbar.js';
