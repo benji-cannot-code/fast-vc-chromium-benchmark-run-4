b592034e1d77a6c432b7926fbca522b6abf6e3e8
Mustaq Ahmed <mustaq@google.com>
2022-08-22 20:44:14 +0000

    [CodeHealth] Get rid of redundant DragImage::ResolutionScale.
    Also cleanup 4 callers of DragController::DoSystemDrag into one.
    
    Bug: 1302094
    Change-Id: If714dfb46e8140d1e00689821596e1d4365b11bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828514
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037924}

--- third_party/blink/renderer/core/clipboard/data_transfer.cc
+++ third_party/blink/renderer/core/clipboard/data_transfer.cc
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_image.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
-#include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/drag_image.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/cull_rect_updater.h"
@@ -62,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/network/mime/mime_type_registry.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom-blink.h"
-#include "ui/display/screen_info.h"
 
 namespace blink {
 
@@ -371,6 +369,7 @@ void DataTransfer::SetDragImageElement(Node* node, const gfx::Point& loc) {
   setDragImage(nullptr, node, loc);
 }
 
+// static
 gfx::RectF DataTransfer::ClipByVisualViewport(const gfx::RectF& absolute_rect,
                                               const LocalFrame& frame) {
   gfx::Rect viewport_in_root_frame =
@@ -380,9 +379,10 @@ gfx::RectF DataTransfer::ClipByVisualViewport(const gfx::RectF& absolute_rect,
   return IntersectRects(absolute_viewport, absolute_rect);
 }
 
-// static
 // Returns a DragImage whose bitmap contains |contents|, positioned and scaled
 // in device space.
+//
+// static
 std::unique_ptr<DragImage> DataTransfer::CreateDragImageForFrame(
     LocalFrame& frame,
     float opacity,
@@ -413,16 +413,12 @@ std::unique_ptr<DragImage> DataTransfer::CreateDragImageForFrame(
 
   scoped_refptr<Image> image =
       UnacceleratedStaticBitmapImage::Create(surface->makeImageSnapshot());
-  ChromeClient& chrome_client = frame.GetPage()->GetChromeClient();
-  float screen_device_scale_factor =
-      chrome_client.GetScreenInfo(frame).device_scale_factor;
 
   // There is no orientation information in the image, so pass
   // kDoNotRespectImageOrientation in order to avoid wasted work looking
   // at orientation.
   return DragImage::Create(image.get(), kDoNotRespectImageOrientation,
-                           screen_device_scale_factor, kInterpolationDefault,
-                           opacity);
+                           kInterpolationDefault, opacity);
 }
 
 // static
@@ -434,15 +430,20 @@ std::unique_ptr<DragImage> DataTransfer::NodeImage(LocalFrame& frame,
 
 std::unique_ptr<DragImage> DataTransfer::CreateDragImage(
     gfx::Point& loc,
+    float device_scale_factor,
     LocalFrame* frame) const {
   if (drag_image_element_) {
     loc = drag_loc_;
-
+    // The image below is already rendered, so shouldn't be scaled (using
+    // device_scale_factor).
     return NodeImage(*frame, *drag_image_element_);
   }
   if (drag_image_) {
     loc = drag_loc_;
-    return DragImage::Create(drag_image_->GetImage());
+    std::unique_ptr<DragImage> drag_image =
+        DragImage::Create(drag_image_->GetImage());
+    drag_image.get()->Scale(device_scale_factor, device_scale_factor);
+    return drag_image;
   }
   return nullptr;
 }
