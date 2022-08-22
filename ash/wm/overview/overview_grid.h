078a064deb801e28a81a15437afdd539e0b6490e
Yongshun Liu <yongshun@chromium.org>
2022-08-22 20:56:42 +0000

    saved_desks: Update save desk buttons after launching a template
    Reset the cached number of incognito or unsupported window when
    launching a desk template. This fixes the bug when launching a desk
    template, since overview is still live, the save desk buttons might
    have the wrong status(enable/disable and tooltip). The cached number
    should be sync'ed whenever there is a modification for  `window_list_`.
    
    This also addresses some of the warnings.
    
    Test: Manual
    Bug: 1353818
    Change-Id: Ia369b434f2ca8fbbc6e977b403c7983ac5acb082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843132
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037934}

--- ash/wm/overview/overview_grid.h
+++ ash/wm/overview/overview_grid.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_drag_indicators.h"
 #include "ash/wm/splitview/split_view_observer.h"
-#include "ash/wm/window_state.h"
 #include "base/containers/flat_set.h"
 #include "ui/aura/window.h"
 #include "ui/gfx/geometry/rect.h"
