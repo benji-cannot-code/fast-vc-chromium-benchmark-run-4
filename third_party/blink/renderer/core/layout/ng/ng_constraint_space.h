d40d27c7bc0d9fa11c5dea0c2d784084d048b1c2
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-08-22 15:05:43 +0000

    [TableFragmention] Avoid breaking within rowspan-cells.
    Rowspan-cells are... complicated. There is a cyclic dependency between
    their block-size - and the block-size of the rows they span.
    For example:
      1. The rowspan cell may expand due to fragmentation.
      2. This extra block-size needs to be distributed into a row (likely
         the last row).
      3. The row may then expand due to fragmentation.
    (a similar example can happen if an arbitrary row that the rowspan
     cell spans expands first).
    
    To get this correct - complicated machinery would be needed with the
    section layout algorithm to get this all correct.
    
    To avoid this, we artificially make the engine avoid breaks within
    rowspan cells. This is slightly better than inserting forced breaks
    within the section layout algorithm, as it might push the entire table
    into the next fragmentainer for example.
    
    Bug: 1335877
    Change-Id: I48761f6b0c7f6f2c03b9db3b7f45ffbb2f408cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842919
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037730}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -332,6 +332,11 @@ class CORE_EXPORT NGConstraintSpace final {
                          : false;
   }
 
+  bool IsTableCellWithEffectiveRowspan() const {
+    return HasRareData() ? rare_data_->IsTableCellWithEffectiveRowspan()
+                         : false;
+  }
+
   const NGTableConstraintSpaceData* TableData() const {
     return HasRareData() ? rare_data_->TableData() : nullptr;
   }
@@ -1158,6 +1163,15 @@ class CORE_EXPORT NGConstraintSpace final {
       EnsureTableCellData()->has_collapsed_borders = has_collapsed_borders;
     }
 
+    bool IsTableCellWithEffectiveRowspan() const {
+      return GetDataUnionType() == DataUnionType::kTableCellData &&
+             table_cell_data_.has_effective_rowspan;
+    }
+
+    void SetIsTableCellWithEffectiveRowspan(bool has_effective_rowspan) {
+      EnsureTableCellData()->has_effective_rowspan = has_effective_rowspan;
+    }
+
     void SetTableRowData(
         scoped_refptr<const NGTableConstraintSpaceData> table_data,
         wtf_size_t row_index) {
@@ -1316,13 +1330,14 @@ class CORE_EXPORT NGConstraintSpace final {
         return table_cell_borders == other.table_cell_borders &&
                table_cell_column_index == other.table_cell_column_index &&
                is_hidden_for_paint == other.is_hidden_for_paint &&
-               has_collapsed_borders == other.has_collapsed_borders;
+               has_collapsed_borders == other.has_collapsed_borders &&
+               has_effective_rowspan == other.has_effective_rowspan;
       }
 
       bool IsInitialForMaySkipLayout() const {
         return table_cell_borders == NGBoxStrut() &&
                table_cell_column_index == kNotFound && !is_hidden_for_paint &&
-               !has_collapsed_borders;
+               !has_collapsed_borders && !has_effective_rowspan;
       }
 
       NGBoxStrut table_cell_borders;
@@ -1330,6 +1345,7 @@ class CORE_EXPORT NGConstraintSpace final {
       absl::optional<LayoutUnit> table_cell_alignment_baseline;
       bool is_hidden_for_paint = false;
       bool has_collapsed_borders = false;
+      bool has_effective_rowspan = false;
     };
 
     struct TableRowData {
