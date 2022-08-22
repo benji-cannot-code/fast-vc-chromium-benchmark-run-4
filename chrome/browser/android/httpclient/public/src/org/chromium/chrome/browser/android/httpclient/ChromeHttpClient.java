b4c281bd43d281c218c611eb6e6c07227e9516d3
Mohamed Heikal <mheikal@chromium.org>
2022-08-22 17:38:28 +0000

    Rename HttpClient to ChromeHttpClient to avoid collisions
    The HttpClient interface is meant for use by other codebases, HttpClient
    is a very common name and it is annoying to use colliding names in the
    same java file. This cl renames the interface to add a Chrome prefix to
    prevent collisions.
    
    Change-Id: I466f20d293c2d43d8c8da88936a77b722b4efa29
    Bug: 1322186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842852
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037831}

--- chrome/browser/android/httpclient/public/src/org/chromium/chrome/browser/android/httpclient/ChromeHttpClient.java
+++ chrome/browser/android/httpclient/public/src/org/chromium/chrome/browser/android/httpclient/ChromeHttpClient.java
@@ -8,7 +8,7 @@ package org.chromium.chrome.browser.android.httpclient;
 import java.util.Map;
 
 /** A simple HttpClient interface. */
-public interface HttpClient {
+public interface ChromeHttpClient {
     void send(String url, String requestType, byte[] body, Map<String, String> headers,
             HttpResponseCallback responseCallback);
 }
