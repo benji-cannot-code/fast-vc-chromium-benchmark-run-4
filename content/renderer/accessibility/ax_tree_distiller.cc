bee54d6a88e1c689b0a9ce8e7067c765a9f0189d
Abigail Klein <abigailbklein@google.com>
2022-08-22 14:54:20 +0000

    [Read Anything] Don't set max node count when getting AXTree snapshot.
    Note: we may need to introduce a timeout instead to handle pages that
    are extremely long and complex. We will do this in a follow-up after
    some experimentation.
    
    Bug: 1266555
    Change-Id: I87f7a6cbbf1201a2e5e14890ec3fa053fd489985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843757
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037723}

--- content/renderer/accessibility/ax_tree_distiller.cc
+++ content/renderer/accessibility/ax_tree_distiller.cc
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-static constexpr int kMaxNodes = 5000;
-
 // TODO: Consider moving this to AXNodeProperties.
 static const ax::mojom::Role kContentRoles[]{
     ax::mojom::Role::kHeading,
@@ -137,8 +135,11 @@ void AXTreeDistiller::SnapshotAXTree() {
   ui::AXMode ax_mode =
       ui::AXMode::kWebContents | ui::AXMode::kHTML | ui::AXMode::kScreenReader;
   AXTreeSnapshotterImpl snapshotter(render_frame_, ax_mode);
+  // Setting max_node_count = 0 means there is no max.
+  // TODO(crbug.com/1266555): Set a timeout to ensure that huge pages do not
+  // cause the snapshotter to hang.
   snapshotter.Snapshot(
-      /* exclude_offscreen= */ false, kMaxNodes,
+      /* exclude_offscreen= */ false, /* max_node_count= */ 0,
       /* timeout= */ {}, snapshot_.get());
 }
 
