236e170dd4378f37ec52634921c67ba1fa707d11
Ender <ender@google.com>
2022-08-22 18:52:15 +0000

    Reland "Remove Mic and Lens buttons in clobber mode."
    This is a reland of commit cd87f330655fffbc2aa33b3cb157c925fb86591f
    
    Original change's description:
    > Remove Mic and Lens buttons in clobber mode.
    >
    > The change ensures the Lens and Voice icons are removed from toolbar
    > when Omnibox contains user input to release some space for the input
    > content.
    >
    > This seems to have recently regressed as a result of multiple new
    > features launching.
    >
    > Before: http://screen/aKXnqSgE4GDm7CF.png
    > After:  http://screen/3g8NHGTkiiwW2ir.png
    > ZPS:    http://screen/7H2vhbqXXpzqz7j.png
    >
    > Change-Id: I8db60813fd46ccd7bc523e34627e5012954158a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832324
    > Reviewed-by: Gang Wu <gangwu@chromium.org>
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    > Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1036410}
    
    Change-Id: I19a21680916196848a1c20e314b0d12b2a7a20cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846110
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037853}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -1053,8 +1053,9 @@ public class LocationBarMediatorTest {
 
     @Test
     public void testLensButtonVisibility_lensEnabledWithInputText_tablet() {
+        // Do not show lens when the omnibox already has input.
         doReturn(true).when(mLensController).isLensEnabled(any());
-        verifyLensButtonVisibilityWhenFocusChanges(true, "text");
+        verifyLensButtonVisibilityWhenFocusChanges(false, "text");
     }
 
     private void verifyLensButtonVisibilityWhenFocusChanges(
