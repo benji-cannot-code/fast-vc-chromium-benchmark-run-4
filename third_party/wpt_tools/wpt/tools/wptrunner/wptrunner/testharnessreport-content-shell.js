4e060239e54d240727e5314f352effc49f537e37
Thorben Troebst <troebst@google.com>
2022-08-22 23:48:29 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: a0ece3bbdbfdf1bd0f66f841b06d54c1d927af0a
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,android-weblayer-pie-x86-wpt-smoketest,linux-blink-rel
    Change-Id: Ife4f938bc6aeaa52d81d6c33ac5d819b0f6794b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848704
    Commit-Queue: Thorben Troebst <troebst@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038019}

---
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/testharnessreport-content-shell.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var props = {output:%(output)d, debug: %(debug)s};
+var start_loc = document.createElement('a');
+start_loc.href = location.href;
+setup(props);
+
+testRunner.dumpAsText();
+testRunner.waitUntilDone();
+testRunner.setPopupBlockingEnabled(false);
+testRunner.setDumpJavaScriptDialogs(false);
+
+add_completion_callback(function (tests, harness_status) {
+    var id = decodeURIComponent(start_loc.pathname) + decodeURIComponent(start_loc.search) + decodeURIComponent(start_loc.hash);
+    var result_string = JSON.stringify([
+        id,
+        harness_status.status,
+        harness_status.message,
+        harness_status.stack,
+        tests.map(function(t) {
+            return [t.name, t.status, t.message, t.stack]
+        }),
+    ]);
+
+    testRunner.setCustomTextOutput(result_string);
+    testRunner.notifyDone();
+});
