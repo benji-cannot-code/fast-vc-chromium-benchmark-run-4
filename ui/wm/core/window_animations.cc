b14a521a233ee858a41488aef36842ff5f81eda2
Avery Musbach <amusbach@chromium.org>
2022-08-22 18:27:20 +0000

    autotest: Report window hiding animations
    getAppWindowList will return data where isAnimating is true for windows
    in hiding animations such as the minimize animation.
    
    Fixed: b/242616647
    Test: CL:3836801
    Change-Id: I5d40825fd18a1088a769ae967b128fd75bef83aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3839072
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037849}

--- ui/wm/core/window_animations.cc
+++ ui/wm/core/window_animations.cc
@@ -59,6 +59,9 @@ class HidingWindowAnimationObserverBase : public aura::WindowObserver {
  public:
   explicit HidingWindowAnimationObserverBase(aura::Window* window)
       : window_(window) {
+    window_->SetProperty(
+        kWindowHidingAnimationCountKey,
+        window_->GetProperty(kWindowHidingAnimationCountKey) + 1);
     window_->AddObserver(this);
   }
 
@@ -68,8 +71,13 @@ class HidingWindowAnimationObserverBase : public aura::WindowObserver {
       const HidingWindowAnimationObserverBase&) = delete;
 
   ~HidingWindowAnimationObserverBase() override {
-    if (window_)
-      window_->RemoveObserver(this);
+    if (!window_)
+      return;
+    window_->RemoveObserver(this);
+    window_->SetProperty(
+        kWindowHidingAnimationCountKey,
+        window_->GetProperty(kWindowHidingAnimationCountKey) - 1);
+    DCHECK_GE(window_->GetProperty(kWindowHidingAnimationCountKey), 0);
   }
 
   // aura::WindowObserver:
