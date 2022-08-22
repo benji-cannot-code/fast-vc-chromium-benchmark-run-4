571e2f1c194ca7d16316afcd8431c7646819c63d
Kalvin Lee <kdlee@chromium.org>
2022-08-22 10:32:23 +0000

    [PA] Support MSVC in component_export
    MSVC behaves a bit differently WRT __VA_ARGS__ than our current
    toolchains. Looking at historical copies of this file, the workaround
    was to wrap outer expressions inside an additional macro; this CL
    replicates the old workaround.
    
    Bug: 1351310
    Change-Id: Ibd33c20513aa8612424fcc1e7f466bcf77483e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833690
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037647}

--- base/allocator/partition_allocator/partition_alloc_base/component_export.h
+++ base/allocator/partition_allocator/partition_alloc_base/component_export.h
@@ -57,6 +57,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_(                              \
       PA_COMPONENT_MACRO_CONDITIONAL_COMMA_(condition), consequent, alternate)
 
+// MSVC workaround for __VA_ARGS__ expanding into one expression.
+#define PA_MSVC_EXPAND_ARG(arg) arg
+
 // Expands to a comma (,) iff its first argument expands to |1|. Used in
 // conjunction with |PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_()|, as the
 // presence or absense of an extra comma can be used to conditionally shift
@@ -71,7 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // |PA_COMPONENT_MACRO_CONDITIONAL_COMMA_()| above to implement conditional
 // macro expansion.
 #define PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_(...) \
-  PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_IMPL_(__VA_ARGS__)
+  PA_MSVC_EXPAND_ARG(                                  \
+      PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_IMPL_(__VA_ARGS__))
 #define PA_COMPONENT_MACRO_SELECT_THIRD_ARGUMENT_IMPL_(a, b, c, ...) c
 
 #endif  // BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_BASE_COMPONENT_EXPORT_H_
