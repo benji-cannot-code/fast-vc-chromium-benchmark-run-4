c2d00c6b4d6acc7449863f11d627c57f69e7c538
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-08-22 13:47:19 +0000

    Disable AnonymousIframe origin trial on WebLayer.
    A simple patch and the associated tests are missing on WebLayer:
    https://chromium-review.googlesource.com/c/chromium/src/+/3804565
    
    We would normally worked on the patch above, but given this
    announcement:
    https://groups.google.com/a/google.com/g/chromium-dev-internal/c/rwKIZb55TwE/m/_ejiYfMbCwAJ
    We decided not to invest time to do it and remove WebLayer from the
    supported list of platforms.
    
    This patch disable the feature on WebLayer.
    
    1. The RuntimeEnabledFlag is turned off. This doesn't prevent the
    OriginTrial from turning it on. It is only useful for when the feature
    will be enabled by default.
    2. It is disabled from the base::FeatureFlag for the OriginTrial
    specifically.
    
    Bug: 1338402
    Change-Id: I13ea10abf764a0081ac89963a619d55721b45284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833549
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037696}

--- third_party/blink/public/platform/web_runtime_features.h
+++ third_party/blink/public/platform/web_runtime_features.h
@@ -80,6 +80,7 @@ class WebRuntimeFeatures {
   BLINK_PLATFORM_EXPORT static void
   EnableAccessibilityUseAXPositionForDocumentMarkers(bool);
   BLINK_PLATFORM_EXPORT static void EnableAdTagging(bool);
+  BLINK_PLATFORM_EXPORT static void EnableAnonymousIframe(bool);
   BLINK_PLATFORM_EXPORT static void EnableBackgroundFetch(bool);
   BLINK_PLATFORM_EXPORT static void EnableBatchFetchRequests(bool);
   BLINK_PLATFORM_EXPORT static void EnableBrowserVerifiedUserActivationKeyboard(
