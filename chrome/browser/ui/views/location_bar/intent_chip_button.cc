52c90352361afe48ce57d3eed39b4eb1e351deef
Tim Sergeant <tsergeant@chromium.org>
2022-08-22 20:49:40 +0000

    Intents: Do not initialize IntentChipButton with a null browser
    In LocationBarView, `browser_` can be null when used in non-browser
    windows. In this case, we should not initialize IntentChipButton, which
    needs a `Browser` to function. This matches the behavior of many other
    location bar children.
    
    Bug: 1354628
    Change-Id: I68113476790163b2fd5d301f735243cf2f4f9c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840381
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037930}

--- chrome/browser/ui/views/location_bar/intent_chip_button.cc
+++ chrome/browser/ui/views/location_bar/intent_chip_button.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/check.h"
 #include "build/build_config.h"
 #include "chrome/browser/apps/intent_helper/intent_picker_helpers.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
@@ -36,6 +37,7 @@ IntentChipButton::IntentChipButton(Browser* browser,
                                             base::Unretained(this))),
       browser_(browser),
       delegate_(delegate) {
+  DCHECK(browser);
   SetText(l10n_util::GetStringUTF16(IDS_INTENT_CHIP_OPEN_IN_APP));
   SetFocusBehavior(views::PlatformStyle::kDefaultFocusBehavior);
   SetTooltipText(l10n_util::GetStringUTF16(IDS_INTENT_CHIP_OPEN_IN_APP));
