3ff20fffc208c3a21635a3b02b3316f2963c185e
Mike Dougherty <michaeldo@chromium.org>
2022-08-22 22:35:13 +0000

    Disable AccountsTableTestCase potentially causing flake
    One of the AccountsTableTestCase tests are causing the others to be
    flaky. (The failure happens at the start of the test and appears as a
    GridCell animation failure.)
    
    This problem can be seen in
    https://chrome-swarming.appspot.com/task?id=5cddd8c00b76a011&w=true
    where the only two tests ran before the flake were
    testDismissSignOutConfirmationTwice and
    testInterruptDuringSignOutConfirmation. While those tests passed
    successfully, it is believed one of them is causing flake.
    
    The tests are disabled in this CL to verify if the flake clears up.
    
    Bug: 1352968
    Change-Id: If49870bb0915bde12158e8313a4566218cb9fa92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846292
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Owners-Override: edchin <edchin@google.com>
    Reviewed-by: edchin <edchin@google.com>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037993}

--- ios/chrome/browser/ui/settings/google_services/accounts_table_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/accounts_table_egtest.mm
@@ -448,7 +448,8 @@ const NSTimeInterval kSyncOperationTimeout = 10.0;
 
 // Tests to open the sign-out confirmation dialog, and then open an external
 // URL.
-- (void)testInterruptDuringSignOutConfirmation {
+// TODO(crbug.com/1352968): Re-enable this test after defalking.
+- (void)DISABLED_testInterruptDuringSignOutConfirmation {
   FakeChromeIdentity* fakeIdentity = [FakeChromeIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
@@ -472,7 +473,8 @@ const NSTimeInterval kSyncOperationTimeout = 10.0;
 
 // Tests that opening and closing the sign-out confirmation dialog does
 // not affect the user's sign-in state.
-- (void)testDismissSignOutConfirmationTwice {
+// TODO(crbug.com/1352968): Re-enable this test after defalking.
+- (void)DISABLED_testDismissSignOutConfirmationTwice {
   FakeChromeIdentity* fakeIdentity = [FakeChromeIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
