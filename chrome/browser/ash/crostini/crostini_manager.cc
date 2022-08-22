1e11ffcf4754927e0d31d34a90c562e160963b11
Timothy Loh <timloh@chromium.org>
2022-08-22 08:02:14 +0000

    Fix using invalidator iterator in CrostiniRestarter::ExtractRequests
    std::vector::erase(iterator) invalidates the iterator so we should use
    the return value to advance instead of the now-invalid iterator. This is
    covered by CrostiniManagerRestartTest.StartVmOnlyThenFullRestart, which
    already passes as in practice the iterator is a pointer which would
    point to the next element following the deletion.
    
    Bug: 1354175
    Change-Id: I1df33f557dc4d920a3342313c4a6a5810051704b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3844581
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037591}

--- chrome/browser/ash/crostini/crostini_manager.cc
+++ chrome/browser/ash/crostini/crostini_manager.cc
@@ -625,7 +625,7 @@ base::OnceClosure CrostiniManager::CrostiniRestarter::ExtractRequests(
     if (it->observer)
       observer_list_.RemoveObserver(it->observer);
     callbacks.push_back(std::move(it->callback));
-    requests_.erase(it);
+    it = requests_.erase(it);
   }
 
   return base::BindOnce(
