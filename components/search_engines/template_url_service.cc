54ce64104b8993c966021f76cb6129c914de6a81
Angela Yoeurng <yoangela@chromium.org>
2022-08-23 19:59:46 +0000

    [omnibox][search_engines] Add activated and deactivated variants to Omnibox.KeywordModeUsageByEngineType histogram
    This CL adds histograms for when a keyword mode search engine is
    activated and deactivated, enumerated by the type of search engine
    (i.e. whether it's prepopulated, starter pack, or added/not-builtin),
    as well as for how many search engines are active or inactive on
    startup.
    
    Bug: 1287313, 1354714
    Change-Id: Ic1504a5d06b55cc88bd9815faf7c78718bb00167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832326
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038396}

--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/crash_logging.h"
 #include "base/format_macros.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/observer_list.h"
 #include "base/rand_util.h"
@@ -55,6 +56,10 @@ namespace {
 
 const char kDeleteSyncedEngineHistogramName[] =
     "Search.DeleteSyncedSearchEngine";
+// TODO(yoangela): Consider sharing this const with
+//  "Omnibox.KeywordModeUsageByEngineType.Accepted" in omnibox_edit_model.cc.
+const char kKeywordModeUsageByEngineTypeHistogramName[] =
+    "Omnibox.KeywordModeUsageByEngineType";
 
 // Values for an enumerated histogram used to track whenever an ACTION_DELETE is
 // sent to the server for search engines. These are persisted. Do not re-number.
@@ -671,10 +676,20 @@ void TemplateURLService::SetIsActiveTemplateURL(TemplateURL* url,
   DCHECK(url);
 
   TemplateURLData data(url->data());
-  data.is_active = is_active ? TemplateURLData::ActiveStatus::kTrue
-                             : TemplateURLData::ActiveStatus::kFalse;
+  std::string histogram_name = kKeywordModeUsageByEngineTypeHistogramName;
+  if (is_active) {
+    data.is_active = TemplateURLData::ActiveStatus::kTrue;
+    histogram_name.append(".Activated");
+  } else {
+    data.is_active = TemplateURLData::ActiveStatus::kFalse;
+    histogram_name.append(".Deactivated");
+  }
 
   Update(url, TemplateURL(data));
+
+  base::UmaHistogramEnumeration(
+      histogram_name, url->GetBuiltinEngineType(),
+      BuiltinEngineType::KEYWORD_MODE_ENGINE_TYPE_MAX);
 }
 
 TemplateURL* TemplateURLService::CreatePlayAPISearchEngine(
@@ -955,6 +970,22 @@ base::CallbackListSubscription TemplateURLService::RegisterOnLoadedCallback(
                  : on_loaded_callbacks_.Add(std::move(callback));
 }
 
+void TemplateURLService::EmitTemplateURLActiveOnStartupHistogram(
+    OwnedTemplateURLVector* template_urls) {
+  DCHECK(template_urls);
+
+  for (auto& turl : *template_urls) {
+    std::string histogram_name = kKeywordModeUsageByEngineTypeHistogramName;
+    histogram_name.append(
+        (turl->is_active() == TemplateURLData::ActiveStatus::kTrue)
+            ? ".ActiveOnStartup"
+            : ".InactiveOnStartup");
+    base::UmaHistogramEnumeration(
+        histogram_name, turl->GetBuiltinEngineType(),
+        BuiltinEngineType::KEYWORD_MODE_ENGINE_TYPE_MAX);
+  }
+}
+
 void TemplateURLService::OnWebDataServiceRequestDone(
     KeywordWebDataService::Handle h,
     std::unique_ptr<WDTypedResult> result) {
@@ -990,6 +1021,7 @@ void TemplateURLService::OnWebDataServiceRequestDone(
   {
     PatchMissingSyncGUIDs(template_urls.get());
     MaybeSetIsActiveSearchEngines(template_urls.get());
+    EmitTemplateURLActiveOnStartupHistogram(template_urls.get());
     SetTemplateURLs(std::move(template_urls));
 
     // This initializes provider_map_ which should be done before
