7edcf44acdb167dd354a3bcef2d020c6060acead
Curt Clemens <cclem@google.com>
2022-08-23 17:44:56 +0000

    [QuickStart] Put FakeBluetoothAdvertisement in anonymous namespace
    FakeBluetoothAdvertisement was defined in both the FastPairAdvertiser
    and the FastInitiationAdvertiser unit tests. While attempting to move
    FastPairAdvertiser to a new build target (crrev.com/c/3834191), this was
    leading to bizarre runtime errors where the FastPairAdvertiser tests
    would unwittingly create FastInitiationAdvertiser's version of
    FakeBluetoothAdvertisement instead of its own.
    
    TEST=Unit tests still pass after this minor refactor.
    
    Bug: b/234655072
    Change-Id: Id45a4cf9a8a07c83ff54a41c4720cd097640bab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843068
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038327}

--- chrome/browser/nearby_sharing/fast_initiation/fast_initiation_advertiser_unittest.cc
+++ chrome/browser/nearby_sharing/fast_initiation/fast_initiation_advertiser_unittest.cc
@@ -18,11 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/test/mock_bluetooth_advertisement.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace {
+
 using ::testing::_;
 using testing::NiceMock;
 using testing::Return;
 
-namespace {
 constexpr const char kNearbySharingFastInitiationServiceUuid[] =
     "0000fe2c-0000-1000-8000-00805f9b34fb";
 const uint8_t kNearbySharingFastPairId[] = {0xfc, 0x12, 0x8e};
@@ -34,8 +35,6 @@ const uint8_t kFastInitMetadataTypeNotify[] = {0x00, 0x42};
 // Metadata bytes translate to 0b00000100 and 0b10111110, indicating "version
 // 0", "type 1 (silent)", and "transmission power of 66".
 const uint8_t kFastInitMetadataTypeSilent[] = {0x04, 0x42};
-
-}  // namespace
 
 struct RegisterAdvertisementArgs {
   RegisterAdvertisementArgs(
@@ -95,6 +94,8 @@ class FakeBluetoothAdvertisement : public device::BluetoothAdvertisement {
  protected:
   ~FakeBluetoothAdvertisement() override = default;
 };
+
+}  // namespace
 
 class NearbySharingFastInitiationAdvertiserTest : public testing::Test {
  public:
