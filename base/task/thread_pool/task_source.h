e6d8b8f112c200151838b1160187c380007fab7e
Abdias Dagbekpo <adagbekpo@google.com>
2022-08-23 17:36:39 +0000

    [Task] Create delayed priority queue for threadgroup
    This priority queue is going to be used for non-ready task sources.
    
    Bug: 1127430
    Change-Id: I4d18a1d38eeb86964fa1657691d909ae3d45d87a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827947
    Commit-Queue: Abdias Dagbekpo <adagbekpo@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038322}

--- base/task/thread_pool/task_source.h
+++ base/task/thread_pool/task_source.h
@@ -192,6 +192,8 @@ class BASE_EXPORT TaskSource : public RefCountedThreadSafe<TaskSource> {
 
   TaskSourceExecutionMode execution_mode() const { return execution_mode_; }
 
+  void ClearForTesting();
+
  protected:
   virtual ~TaskSource();
 
