e6a58a473151d46189a0622475eaa5bea1a67f75
Maxim Anufriev <maxan@google.com>
2022-08-23 17:03:30 +0000

    [UPM][Metrics] Report uptime on specific GMS 1P API errors
    Chrome uptime (since password store backend initialisation) will be
    reported for the following GMS 1P API errors which are possibly related
    to the GMS Core update installation:
    
    - API_NOT_CONNECTED
    - CONNECTTION_SUSPENDED_DURING_CALL
    - RECONNECTION_TIMED_OUT
    
    Bug: 1355880, 1349276
    Change-Id: I0693bcd1115320c83d3273f2e2d1e10c3d12e9d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846093
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038303}

--- chrome/browser/password_manager/android/password_store_android_backend.h
+++ chrome/browser/password_manager/android/password_store_android_backend.h
@@ -247,6 +247,8 @@ class PasswordStoreAndroidBackend
 
   raw_ptr<PrefService> prefs_ = nullptr;
 
+  base::Time initialized_at_ = base::Time::Now();
+
   base::WeakPtrFactory<PasswordStoreAndroidBackend> weak_ptr_factory_{this};
 };
 
