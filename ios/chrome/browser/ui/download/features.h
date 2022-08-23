ab4f099dab6c83642028f908f36053f74f78d55a
Ewann <ewannpv@google.com>
2022-08-23 11:30:46 +0000

    [iOS] Launch iCal Support
    This CL launches iCal support and adds a kill switch to allow its
    deactivation at any time.
    
    Fixed: 1355763
    Change-Id: Iadadba609ad926060118c052caad4245c89ce946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849469
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038186}

--- ios/chrome/browser/ui/download/features.h
+++ ios/chrome/browser/ui/download/features.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 
-// Feature to download Calendar files.
-extern const base::Feature kDownloadCalendar;
+// Kill switch for Calendar support.
+extern const base::Feature kCalendarKillSwitch;
 
 // Kill switch for Vcard support.
 extern const base::Feature kVCardKillSwitch;
--- ios/chrome/browser/ui/download/features.mm
+++ ios/chrome/browser/ui/download/features.mm
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-const base::Feature kDownloadCalendar{"DownloadCalendar",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kCalendarKillSwitch{"DownloadKillSwitchCalendar",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kARKillSwitch{"DownloadKillSwitchAR",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
