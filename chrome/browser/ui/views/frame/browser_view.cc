4efc0c70281211ab6cd126c477ad47de9a6dc87c
Fangzhen Song <songfangzhen@bytedance.com>
2022-08-23 15:54:04 +0000

    Window Placement: Notify the client asynchronously
    On Mac platforms, exiting fullscreen won't finish until the frame has
    been restored to its original position. We should notify the client
    after the restoration has been completed.
    
    Bug: 1302857
    Change-Id: I0be9881e5be2fcbf0d3c150b89c530b1d87de9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817671
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038259}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1877,6 +1877,8 @@ void BrowserView::FullscreenStateChanged() {
     UpdateWindowControlsOverlayEnabled();
 #endif  // BUILDFLAG(IS_MAC)
 
+  browser_->WindowFullscreenStateChanged();
+
   GetExclusiveAccessManager()
       ->fullscreen_controller()
       ->FullscreenTransititionCompleted();
@@ -4194,7 +4196,7 @@ void BrowserView::ProcessFullscreen(bool fullscreen,
   bool swapping_screens_during_fullscreen = false;
 #if BUILDFLAG(IS_MAC)
   frame_->SetFullscreen(fullscreen, display_id);
-#else   // BUILDFLAG(IS_MAC)
+#else
   if (fullscreen && display_id != display::kInvalidDisplayId) {
     display::Screen* screen = display::Screen::GetScreen();
     display::Display display;
@@ -4271,7 +4273,6 @@ void BrowserView::ProcessFullscreen(bool fullscreen,
     immersive_mode_controller_->SetEnabled(fullscreen);
   }
 
-  browser_->WindowFullscreenStateChanged();
 #if !BUILDFLAG(IS_MAC)
   // On Mac platforms, FullscreenStateChanged() is invoked from
   // BrowserFrameMac::OnWindowFullscreenTransitionComplete when the asynchronous
