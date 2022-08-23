fc852244eae138f871a45ea6ef37b92b6823b7a9
Jaeyong Bae <jdragon.bae@gmail.com>
2022-08-23 02:04:02 +0000

    [CSS] Exclude `layer` keyword in media query parser
    This patch excludes `layer` keyword in media query parser. According to
    recent spec, the <media-type> production does not include the keywords
    only, not, and, or, and layer.
    
    Note: The exclusion of layer is because it would otherwise be ambiguous
    when used in the @import url(…) layer; syntax for the sake of cascade
    layers.
    
    Bug: 1343366
    Change-Id: Ib5421315ccd6bdbf4d3d2e389948791f96ea709c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3839085
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Romederful <jdragon.bae@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1038073}

--- third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
@@ -520,7 +520,7 @@ TEST(CSSParserImplTest, LayeredImportRulesMultipleLayers) {
     ASSERT_TRUE(parsed->IsLayered());
     ASSERT_EQ(1u, parsed->GetLayerName().size());
     EXPECT_EQ(g_empty_atom, parsed->GetLayerName()[0]);
-    EXPECT_EQ("layer", parsed->MediaQueries()->MediaText());
+    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
   }
 
   {
@@ -540,7 +540,7 @@ TEST(CSSParserImplTest, LayeredImportRulesMultipleLayers) {
     ASSERT_TRUE(parsed->IsLayered());
     ASSERT_EQ(1u, parsed->GetLayerName().size());
     EXPECT_EQ("bar", parsed->GetLayerName()[0]);
-    EXPECT_EQ("layer", parsed->MediaQueries()->MediaText());
+    EXPECT_EQ("not all", parsed->MediaQueries()->MediaText());
   }
 }
 
