de8c24f0e8698414acee3aa8406625b3f96a9b19
Katie Dektar <katie@chromium.org>
2022-08-23 17:54:40 +0000

    Add feature flag for Accessibility Service work.
    Design at go/chromeos-atp.
    
    Bug: 1355633
    Change-Id: I4ae774fcfbd726dec1a7e9710737083861808f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3851081
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038334}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -600,6 +600,9 @@ extern const char kEnhancedNetworkVoicesDescription[];
 extern const char kAccessibilityOSSettingsVisibilityName[];
 extern const char kAccessibilityOSSettingsVisibilityDescription[];
 
+extern const char kAccessibilityServiceName[];
+extern const char kAccessibilityServiceDescription[];
+
 extern const char kPostQuantumCECPQ2Name[];
 extern const char kPostQuantumCECPQ2Description[];
 
