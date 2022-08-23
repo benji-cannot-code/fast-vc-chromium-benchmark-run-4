fff9d51a94088e17f026678f68e155b5e68a2af3
Changhao Han <changhaohan@chromium.org>
2022-08-23 06:23:22 +0000

    Handle !important property values when parsing shorthand
    Bug: 471601
    Change-Id: I3e20b5671b3257626d2b6ffe5255b8468cc22eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846309
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038126}

--- third_party/blink/web_tests/inspector-protocol/css/css-get-styles-for-node.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-get-styles-for-node.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           <div id='inspected' style='padding-top: 55px; margin-top: 33px !important;'></div>
           <div id='child-div'></div>
       </div>
-      <div id='shorthand-div' style='margin: 0; margin-top: 5px; padding: var(--x); border: 1px solid black;'></div>`,
+      <div id='shorthand-div' style='margin: 0; margin-top: 5px; padding: var(--x); border: 1px solid black; flex: 1 !important;'></div>`,
       'The test verifies functionality of protocol method CSS.getMatchedStylesForNode and CSS.getInlineStylesForNode.');
 
   await dp.DOM.enable();
