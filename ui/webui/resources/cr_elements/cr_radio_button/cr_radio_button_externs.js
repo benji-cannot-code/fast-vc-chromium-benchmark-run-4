26c3099c5c4c52c253c0c9bea397806d8f83f07d
Rebekah Potter <rbpotter@chromium.org>
2022-08-23 02:59:33 +0000

    WebUI: Migrate cr_radio_button/* to TypeScript
    - Update cr_radio_button_behavior to a mixin. Note that TypeScript
      doesn't interact well with legacy behaviors (no such examples in the
      code so far), so this is done to facilitate the migration.
    - Since mixins can't extend legacy behaviors, this means elements
      using the mixin need to separately extend PaperRippleBehavior; this
      CL moves the portions of code explicitly overriding
      PaperRippleBehavior methods/properties into individual elements from
      the CrRadioButtonMixin.
    - Add a minimal externs file, for the remaining JS users.
    
    Bug: 1316438
    Change-Id: I55f9ecf5baa452a7e61b28bebda79dd9eda100c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840935
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038085}

---
+++ ui/webui/resources/cr_elements/cr_radio_button/cr_radio_button_externs.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/* Minimal externs file provided for places in the code that
+ * still use JavaScript instead of TypeScript.
+ * @externs
+ */
+
+/** @interface */
+class CrRadioButtonMixinInterface {
+  constructor() {
+    /** @type {boolean} */
+    this.checked;
+
+    /** @type {boolean} */
+    this.disabled;
+
+    /** @type {boolean} */
+    this.focusabled;
+
+    /** @type {string} */
+    this.name;
+  }
+
+  /** @return {HTMLElement} */
+  getPaperRipple() {}
+}
+
+/**
+ * @constructor
+ * @extends {HTMLElement}
+ * @implements {CrRadioButtonMixinInterface}
+ */
+function CrRadioButtonElement() {}
+
+/**
+ * @constructor
+ * @extends {HTMLElement}
+ * @implements {CrRadioButtonMixinInterface}
+ */
+function CrCardRadioButtonElement() {}
