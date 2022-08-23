3ed8d4971f3cdbf4f50e32c784963ea2d140f369
Justin Cohen <justincohen@chromium.org>
2022-08-23 15:48:58 +0000

    Revert "ios: Workaround crash in iOS16 FormInputAccessoryCoordinator."
    This reverts commit 0b942f1a940a8c810804dda67a6352108d690232.
    
    Reason for revert: Seems fixed in iOS 16 beta 6.
    
    Original change's description:
    > ios: Workaround crash in iOS16 FormInputAccessoryCoordinator.
    >
    > This call is still crashy on iOS16 beta 5.
    >
    > Bug: 1334530
    > Change-Id: Iaeaac2c85a56106c8700ab7e82ac2cfbf06a1266
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819882
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Commit-Queue: Justin Cohen <justincohen@chromium.org>
    > Reviewed-by: Ali Juma <ajuma@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1033201}
    
    Bug: 1334530
    Change-Id: I82fc4563e5e3a69b8166bbb31d801d9b6076bed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846933
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1038256}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
@@ -240,16 +240,7 @@ BubbleViewType BubbleTypeFromFeature() {
   [self.formInputAccessoryViewController reset];
 
   self.formInputViewController = nil;
-  if (@available(iOS 16, *)) {
-    @try {
-      [GetFirstResponder() reloadInputViews];
-    } @catch (NSException* e) {
-      // TODO(crbug.com/1334530) iOS 16 beta 5 is still throwing an
-      // NSInternalInconsistencyException.
-    }
-  } else {
-    [GetFirstResponder() reloadInputViews];
-  }
+  [GetFirstResponder() reloadInputViews];
 }
 
 #pragma mark - Presenting Children
