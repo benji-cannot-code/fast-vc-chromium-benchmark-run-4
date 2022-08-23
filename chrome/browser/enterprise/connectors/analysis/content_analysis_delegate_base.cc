c2af75ed89a0d2c9f91ed73b7877764c3e86d930
Sneha Nagpaul <snehanagpaul@google.com>
2022-08-23 12:52:07 +0000

    Bypass justification rollout to TOT
    Bug: b/239464563
    Change-Id: Ida721fa5d29b1d3cbc5686a55ce2ee89f873ac86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846022
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038207}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_base.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate_base.cc
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_connectors {
 
 const base::Feature kBypassJustificationEnabled{
-    "kBypassJustificationEnabled", base::FEATURE_DISABLED_BY_DEFAULT};
+    "kBypassJustificationEnabled", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace enterprise_connectors
