3b75977606c3de60f959dce9a743c5e645acf87b
Jonathan Ross <jonross@chromium.org>
2022-08-23 16:47:00 +0000

    Update Rendering Benchmark Metrics and Contacts
    Updating rendering.py to no longer use
    Graphics.Smoothness.PercentDroppedFrames2. We've updated these to v3.
    Alerting has now shifted over as well. So let's no longer record them.
    
    Also update the contact emails to reflect members no longer on the
    project. Adding the team email list as the alternative.
    
    Change-Id: I4d547f0096fbf9be1f5d7c2b9e5f85fc7747251b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846298
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038291}

--- tools/perf/benchmarks/rendering.py
+++ tools/perf/benchmarks/rendering.py
@@ -35,9 +35,6 @@ RENDERING_BENCHMARK_UMA = [
     'Graphics.Smoothness.Jank.AllAnimations',
     'Graphics.Smoothness.Jank.AllInteractions',
     'Graphics.Smoothness.Jank.AllSequences',
-    'Graphics.Smoothness.PercentDroppedFrames2.AllAnimations',
-    'Graphics.Smoothness.PercentDroppedFrames2.AllInteractions',
-    'Graphics.Smoothness.PercentDroppedFrames2.AllSequences',
     'Graphics.Smoothness.PercentDroppedFrames3.AllAnimations',
     'Graphics.Smoothness.PercentDroppedFrames3.AllInteractions',
     'Graphics.Smoothness.PercentDroppedFrames3.AllSequences',
@@ -109,10 +106,10 @@ class _RenderingBenchmark(perf_benchmark.PerfBenchmark):
     return options
 
 
-@benchmark.Info(emails=['behdadb@chromium.org', 'jonross@chromium.org',
-                        'sadrul@chromium.org'],
-                documentation_url='https://bit.ly/rendering-benchmarks',
-                component='Internals>GPU>Metrics')
+@benchmark.Info(
+    emails=['jonross@chromium.org', 'chrome-gpu-metrics@google.com'],
+    documentation_url='https://bit.ly/rendering-benchmarks',
+    component='Internals>GPU>Metrics')
 class RenderingDesktop(_RenderingBenchmark):
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
@@ -137,11 +134,10 @@ class RenderingDesktop(_RenderingBenchmark):
           '--use-gpu-high-thread-priority-for-perf-tests')
 
 
-@benchmark.Info(emails=[
-    'behdadb@chromium.org', 'jonross@chromium.org', 'sadrul@chromium.org'
-],
-                documentation_url='https://bit.ly/rendering-benchmarks',
-                component='Internals>GPU>Metrics')
+@benchmark.Info(
+    emails=['jonross@chromium.org', 'chrome-gpu-metrics@google.com'],
+    documentation_url='https://bit.ly/rendering-benchmarks',
+    component='Internals>GPU>Metrics')
 class RenderingDesktopNoTracing(RenderingDesktop):
   @classmethod
   def Name(cls):
@@ -158,10 +154,10 @@ class RenderingDesktopNoTracing(RenderingDesktop):
     return options
 
 
-@benchmark.Info(emails=['behdadb@chromium.org', 'jonross@chromium.org',
-                        'sadrul@chromium.org'],
-                documentation_url='https://bit.ly/rendering-benchmarks',
-                component='Internals>GPU>Metrics')
+@benchmark.Info(
+    emails=['jonross@chromium.org', 'chrome-gpu-metrics@google.com'],
+    documentation_url='https://bit.ly/rendering-benchmarks',
+    component='Internals>GPU>Metrics')
 class RenderingMobile(_RenderingBenchmark):
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
@@ -199,11 +195,10 @@ class RenderingMobile(_RenderingBenchmark):
     return options
 
 
-@benchmark.Info(emails=[
-    'behdadb@chromium.org', 'jonross@chromium.org', 'sadrul@chromium.org'
-],
-                documentation_url='https://bit.ly/rendering-benchmarks',
-                component='Internals>GPU>Metrics')
+@benchmark.Info(
+    emails=['jonross@chromium.org', 'chrome-gpu-metrics@google.com'],
+    documentation_url='https://bit.ly/rendering-benchmarks',
+    component='Internals>GPU>Metrics')
 class RenderingMobileNoTracing(RenderingMobile):
   @classmethod
   def Name(cls):
