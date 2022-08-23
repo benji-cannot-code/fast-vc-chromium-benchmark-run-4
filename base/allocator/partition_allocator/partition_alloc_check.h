51fb21a62130ff0ad220eda510a8fc766b8fa2a4
Peter Boström <pbos@chromium.org>
2022-08-23 01:24:45 +0000

    Sync PA_CHECK with CHECK_WILL_STREAM
    This makes sure PA_CHECK streams when CHECK_WILL_STREAM is on, which is
    required for EXPECT_CHECK_DEATH() to correctly expect streaming.
    
    Bug: 1355547
    Cq-Include-Trybots: luci.chromium.try:android-official,fuchsia-official,linux-official,mac-official,win-official,win32-official
    Change-Id: I82f1b68366ae3b127321f64651e0eb82505d57e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846118
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038060}

--- base/allocator/partition_allocator/partition_alloc_check.h
+++ base/allocator/partition_allocator/partition_alloc_check.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // - Otherwise, crash immediately. This provides worse error messages though.
 #if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 // For official build discard log strings to reduce binary bloat.
-#if defined(OFFICIAL_BUILD) && defined(NDEBUG)
+#if !CHECK_WILL_STREAM()
 // See base/check.h for implementation details.
 #define PA_CHECK(condition)                        \
   PA_UNLIKELY(!(condition)) ? PA_IMMEDIATE_CRASH() \
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ? ::partition_alloc::internal::logging::RawCheck(                        \
         __FILE__ "(" PA_STRINGIFY(__LINE__) ") Check failed: " #condition) \
   : PA_EAT_CHECK_STREAM_PARAMS()
-#endif  // defined(OFFICIAL_BUILD) && defined(NDEBUG)
+#endif  // !CHECK_WILL_STREAM()
 
 #if BUILDFLAG(PA_DCHECK_IS_ON)
 #define PA_DCHECK(condition) PA_CHECK(condition)
