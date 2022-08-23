43d8b26fdf9eea0a16d4d738658869b5ce9d646b
Takashi Toyoshima <toyoshim@chromium.org>
2022-08-23 10:02:58 +0000

    PageLoadMetrics: Prerender2 support for SchemePLMO
    This CL makes SchemePLMO explicitly stop monitoring metrics
    for prerendered pages.
    
    SchemePLMO is intended to compare performance among several
    schemes, and including prerendering cases will be a noise.
    
    Bug: 1317494
    Change-Id: I1f5c32c0d6fb2f8982c676e970a3454a6750c0ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832613
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038168}

--- chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h
@@ -26,6 +26,8 @@ class SchemePageLoadMetricsObserver
   ObservePolicy OnFencedFramesStart(
       content::NavigationHandle* navigation_handle,
       const GURL& currently_committed_url) override;
+  ObservePolicy OnPrerenderStart(content::NavigationHandle* navigation_handle,
+                                 const GURL& currently_committed_url) override;
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle) override;
   ObservePolicy OnHidden(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
