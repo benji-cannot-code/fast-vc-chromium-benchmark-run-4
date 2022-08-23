700e8e00def74d44317f60ff08788d3beb79c018
Michele Fan <michelefan@chromium.org>
2022-08-23 19:40:30 +0000

    wm: Fix UAF for wm fling handler
    The `WmFlingHandler` can be destroyed in the middle of a call to `WmFlingHandler::OnAnimationStep()` leading to a UAF situation.
    
    Fixed:1350558
    Test: Manually and added a test
    
    Change-Id: Ic4f98c67fddb46d4023add518d1f7ee4bb596015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846389
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038387}

--- ash/wm/gestures/wm_fling_handler.h
+++ ash/wm/gestures/wm_fling_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/compositor/compositor_animation_observer.h"
 #include "ui/gfx/geometry/vector2d_f.h"
@@ -64,6 +65,8 @@ class WmFlingHandler : public ui::CompositorAnimationObserver {
 
   StepCallback on_step_callback_;
   base::RepeatingClosure on_end_callback_;
+
+  base::WeakPtrFactory<WmFlingHandler> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
