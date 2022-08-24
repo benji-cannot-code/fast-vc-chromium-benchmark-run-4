74a618f51c80427285f34c7367fc38d2429b0833
Martin Rodriguez <rodmartin@google.com>
2022-08-24 11:07:57 +0000

    crOS: Make DeviceTrustService part of the login screen
    Make DeviceTrustService part of the ChromeOS login screen to be able
    to handle the inline flow during login.
    Go to go/cas-cros for more information.
    
    Bug: b/242036672
    Change-Id: I187934b70ddcdbb2d62d2f7861dffdaf389e80f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835663
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038705}

--- chrome/browser/ash/login/saml/fake_saml_idp_mixin.h
+++ chrome/browser/ash/login/saml/fake_saml_idp_mixin.h
@@ -42,6 +42,7 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
   void SetCookieValue(const std::string& cookie_value);
   void SetRequireHttpBasicAuth(bool require_http_basic_auth);
   void SetSamlResponseFile(const std::string& xml_file);
+  bool DeviceTrustHeaderRecieved() const;
   bool IsLastChallengeResponseExists() const;
   void AssertChallengeResponseMatchesTpmResponse() const;
 
@@ -50,6 +51,7 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
   GURL GetSamlPageUrl() const;
   GURL GetHttpSamlPageUrl() const;
   GURL GetSamlWithDeviceAttestationUrl() const;
+  GURL GetSamlWithDeviceTrustUrl() const;
 
  private:
   GURL GetSamlAuthPageUrl() const;
@@ -64,7 +66,8 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
     kLogin,
     kLoginAuth,
     kLoginWithDeviceAttestation,
-    kLoginCheckDeviceAnswer
+    kLoginCheckDeviceAnswer,
+    kLoginWithDeviceTrust
   };
 
   // Returns the RequestType that corresponds to `url`, or RequestType::Unknown
@@ -81,6 +84,10 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
   BuildResponseForLoginWithDeviceAttestation(
       const net::test_server::HttpRequest& request,
       const GURL& request_url) const;
+  std::unique_ptr<net::test_server::HttpResponse>
+  BuildResponseForLoginWithDeviceTrust(
+      const net::test_server::HttpRequest& request,
+      const GURL& request_url);
   std::unique_ptr<net::test_server::HttpResponse>
   BuildResponseForCheckDeviceAnswer(
       const net::test_server::HttpRequest& request,
@@ -108,6 +115,7 @@ class FakeSamlIdpMixin final : public InProcessBrowserTestMixin {
 
   bool require_http_basic_auth_ = false;
 
+  bool device_trust_header_recieved_ = false;
   absl::optional<std::string> challenge_response_;
 };
 
