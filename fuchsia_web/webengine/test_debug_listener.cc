4bf23c5064c829d830f7cfc0189a8f1ba5395a0f
Greg Thompson <grt@chromium.org>
2022-08-24 14:55:43 +0000

    [fuchsia] Fail fast in WebEngineDebugIntegrationTest on timeout
    Use fatal failures in TestDebugListener's RunUntilNumberOfPortsIs and
    wrap calls to it in ASSERT_NO_FATAL_FAILURE so that tests fail fast when
    there is a timeout waiting for the right number of ports. As a result,
    tests fail at that spot rather than generating more irrelevant
    diagnostics that distracts the reader from the reason a test has failed.
    
    Bug: 1280703
    Change-Id: Idf31122b50fe1567d86b1bc6e5d90fde34cb7caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845177
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038776}

--- fuchsia_web/webengine/test_debug_listener.cc
+++ fuchsia_web/webengine/test_debug_listener.cc
@@ -42,7 +42,7 @@ void TestDebugListener::RunUntilNumberOfPortsIs(size_t size) {
           run_loop.Quit();
       }));
   run_loop.Run();
-  EXPECT_EQ(debug_ports_.size(), size);
+  ASSERT_EQ(debug_ports_.size(), size);
 }
 
 TestDebugListener::TestPerContextListener::TestPerContextListener(
