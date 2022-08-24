52caf7ee13c48f22eda79b367cc0b36c06c21b14
Egor Pasko <pasko@chromium.org>
2022-08-24 16:24:21 +0000

    webview: disable LoadUrlTest#testProgress
    Reason: flaky on "Marshmallow Tablet Tester"
    
    Bug: 1356194
    Change-Id: I4561a4d0efb8bb13ddbefb44d68e149cc20da6cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852396
    Owners-Override: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038803}

--- android_webview/javatests/src/org/chromium/android_webview/test/LoadUrlTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/LoadUrlTest.java
@@ -28,6 +28,7 @@ import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.android_webview.test.util.JSUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.util.HistoryUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -162,6 +163,7 @@ public class LoadUrlTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "http://crbug.com/1356194")
     public void testProgress() throws Throwable {
         final OnProgressChangedClient contentsClient = new OnProgressChangedClient();
         final AwTestContainerView testContainerView =
