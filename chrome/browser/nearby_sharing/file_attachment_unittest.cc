ae891b4509a8efdd3ed2bff64128aa73549242af
Brando <brandosocarras@google.com>
2022-08-24 02:36:22 +0000

    [Nearby Share] Add unit tests for FileAttachment class
    This adds unit tests for FileAttachment, which has been a gap for some
    time. Unit tests for methods overridden from Attachment class to come in follow up CL.
    
    TEST=this
    
    Bug: b/232970430
    Change-Id: I593d466d0e78ee59fd360cca72639db54d329073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846308
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038549}

---
+++ chrome/browser/nearby_sharing/file_attachment_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/nearby_sharing/file_attachment.h"
+
+#include "base/files/file_path.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+const int64_t kFileSize = 2048;
+const char kFileName[] = "secure_file_name.html";
+base::FilePath kFilePath = base::FilePath();
+const char kMimeType[] = "text/html";
+FileAttachment::Type kMetadataType =
+    sharing::mojom::FileMetadata::Type::kUnknown;
+}  // namespace
+
+TEST(FileAttachmentTest, FileAttachmentTest) {
+  FileAttachment attachment(/*id=*/0, kFileSize, kFileName, kMimeType,
+                            kMetadataType);
+
+  EXPECT_EQ(attachment.size(), kFileSize);
+  EXPECT_EQ(attachment.file_name(), kFileName);
+  EXPECT_EQ(attachment.mime_type(), kMimeType);
+  EXPECT_EQ(attachment.type(), kMetadataType);
+
+  attachment.set_file_path(kFilePath);
+  EXPECT_EQ(attachment.file_path(), kFilePath);
+}
