0e4b7127fda09b4309216f9aa2c52852eec96166
Dmitrii Kuragin <kuragin@chromium.org>
2022-08-24 04:46:51 +0000

    Rename `IsUserInitiatedInstallSource` into `ShouldInstallOverwriteUserDisplayMode`.
    Follow up with refactoring mentioned in http://crrev.com/c/3827881
    
    Bug: 1333966
    Change-Id: I46472407b88f384f592dbf7c39e0897b10404dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848709
    Commit-Queue: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038576}

--- chrome/browser/web_applications/web_app_install_finalizer.cc
+++ chrome/browser/web_applications/web_app_install_finalizer.cc
@@ -55,6 +55,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace web_app {
+namespace {
+
+// Overwrite the user display mode if the install source indicates a
+// user-initiated installation
+bool ShouldInstallOverwriteUserDisplayMode(
+    webapps::WebappInstallSource source) {
+  using InstallSource = webapps::WebappInstallSource;
+  switch (source) {
+    case InstallSource::MENU_BROWSER_TAB:
+    case InstallSource::MENU_CUSTOM_TAB:
+    case InstallSource::AUTOMATIC_PROMPT_BROWSER_TAB:
+    case InstallSource::AUTOMATIC_PROMPT_CUSTOM_TAB:
+    case InstallSource::API_BROWSER_TAB:
+    case InstallSource::API_CUSTOM_TAB:
+    case InstallSource::AMBIENT_BADGE_BROWSER_TAB:
+    case InstallSource::AMBIENT_BADGE_CUSTOM_TAB:
+    case InstallSource::RICH_INSTALL_UI_WEBLAYER:
+    case InstallSource::ARC:
+    case InstallSource::CHROME_SERVICE:
+    case InstallSource::OMNIBOX_INSTALL_ICON:
+    case InstallSource::MENU_CREATE_SHORTCUT:
+      return true;
+    case InstallSource::DEVTOOLS:
+    case InstallSource::MANAGEMENT_API:
+    case InstallSource::INTERNAL_DEFAULT:
+    case InstallSource::ISOLATED_APP_DEV_INSTALL:
+    case InstallSource::EXTERNAL_DEFAULT:
+    case InstallSource::EXTERNAL_POLICY:
+    case InstallSource::SYSTEM_DEFAULT:
+    case InstallSource::SYNC:
+    case InstallSource::SUB_APP:
+    case InstallSource::KIOSK:
+      return false;
+    case InstallSource::COUNT:
+      NOTREACHED();
+      return false;
+  }
+}
+
+}  // namespace
 
 WebAppInstallFinalizer::FinalizeOptions::FinalizeOptions(
     webapps::WebappInstallSource install_surface)
@@ -139,8 +179,7 @@ void WebAppInstallFinalizer::FinalizeInstall(
 
   // Set |user_display_mode| and any user-controllable fields here if this
   // install is user initiated or it's a new app.
-  if (webapps::InstallableMetrics::IsUserInitiatedInstallSource(
-          options.install_surface) ||
+  if (ShouldInstallOverwriteUserDisplayMode(options.install_surface) ||
       !existing_web_app) {
     DCHECK(web_app_info.user_display_mode.has_value());
     web_app->SetUserDisplayMode(*web_app_info.user_display_mode);
