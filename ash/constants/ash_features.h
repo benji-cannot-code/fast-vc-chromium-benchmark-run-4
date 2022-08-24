62bbb6e60c5cb1aee958db0a1b2b1511e4c8ecfc
Michael Rygiel <michaelrygiel@google.com>
2022-08-24 16:38:22 +0000

    CrOS: Add flag for Captive Portal UI 2022 Improvements
    This change introduces the |kCaptivePortalUI2022| feature flag to
    control whether the changes to the captive portal ui are enabled.
    
    BUG=b:243178226
    TEST=autoninja -C out/Default chrome
    
    Change-Id: I62b207d41bfc7bff2f42c94e38fa42686cbe7f86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848745
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Michael Rygiel <michaelrygiel@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038812}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -134,6 +134,8 @@ extern const base::Feature kCameraAppDocScanDlc;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCameraPrivacySwitchNotifications;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kCaptivePortalUI2022;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCaptureModeDemoTools;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCellularBypassESimInstallationConnectivityCheck;
@@ -721,6 +723,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsBluetoothQualityReportEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsBluetoothRevampEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCalendarViewEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCalendarModelDebugModeEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsCaptivePortalUI2022Enabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsCheckPasswordsAgainstCryptohomeHelperEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherItemColorSyncEnabled();
