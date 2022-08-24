7435c14f94b8f4f9b5c6ccb0a2e61b25c604dd96
Xi Han <hanxi@google.com>
2022-08-24 18:18:59 +0000

    [Start] Check showing of overview page in ChromeTabbedActivityTestRule
    Most of the integration tests launches Chrome without any Tab, thus a
    NTP is created as an initial Tab. However, when Start surface is
    enabled, the Start surface will be created instead of a NTP when calling ChromeTabbedActivityTestRule#startMainActivityFromLauncher(). In this case, ChromeTabbedActivityTestRule should wait for overview page is showing rather than loading the first Tab.
    In this CL:
    1. Add overview page showing check in ChromeTabbedActivityTestRule;
    2. For any test which expects to load a NTP and navigates, we update
       the call from startMainActivityFromLauncher() to
       startMainActivityWithURL(UrlConstants.NTP_URL).
    
    Bug: 1356180
    Change-Id: Ibdb949b1a1c73fca6ea156e0a2f1407e78c6b291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854327
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038867}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2844,7 +2844,9 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
 
     @Override
     public boolean isInOverviewMode() {
-        return mLayoutManager != null && mLayoutManager.isLayoutVisible(LayoutType.TAB_SWITCHER);
+        return mLayoutManager != null
+                && (mLayoutManager.isLayoutVisible(LayoutType.TAB_SWITCHER)
+                        || mLayoutManager.isLayoutVisible(LayoutType.START_SURFACE));
     }
 
     @Override
--- chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
@@ -47,6 +47,7 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
 import org.chromium.chrome.test.util.browser.TabLoadObserver;
+import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
@@ -82,7 +83,7 @@ public class NavigateTest {
 
     @Before
     public void setUp() {
-        mActivityTestRule.startMainActivityFromLauncher();
+        mActivityTestRule.startMainActivityWithURL(UrlConstants.NTP_URL);
         mTestServer = EmbeddedTestServer.createAndStartHTTPSServer(
                 InstrumentationRegistry.getContext(), ServerCertificate.CERT_OK);
         mOmnibox = new OmniboxTestUtils(mActivityTestRule.getActivity());
