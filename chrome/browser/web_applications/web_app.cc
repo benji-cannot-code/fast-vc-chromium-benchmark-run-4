ad7e2b065548848ae647a84c283452077d902679
Joe Mason <joenotcharles@google.com>
2022-08-24 19:49:22 +0000

    Clean-up: use base::Overloaded with variants in web_applications
    This puts functions to be called for each branch of a variant inline
    in lambdas, instead of using external functor structs.
    
    Change-Id: I9c6706963208b5a9d45024d0c299729ac475929e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846299
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038909}

--- chrome/browser/web_applications/web_app.cc
+++ chrome/browser/web_applications/web_app.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/containers/contains.h"
+#include "base/functional/overloaded.h"
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_split.h"
@@ -556,31 +557,29 @@ bool WebApp::IsolationData::DevModeProxy::operator!=(
   return !(*this == other);
 }
 base::Value WebApp::IsolationData::AsDebugValue() const {
-  struct ContentVisitor {
-    base::Value::Dict operator()(
-        const WebApp::IsolationData::InstalledBundle& bundle) {
-      base::Value::Dict content;
-      content.SetByDottedPath("installed_bundle.path", bundle.path);
-      return content;
-    }
-
-    base::Value::Dict operator()(
-        const WebApp::IsolationData::DevModeBundle& bundle) {
-      base::Value::Dict content;
-      content.SetByDottedPath("dev_mode_bundle.path", bundle.path);
-      return content;
-    }
-
-    base::Value::Dict operator()(
-        const WebApp::IsolationData::DevModeProxy& proxy) {
-      base::Value::Dict content;
-      content.SetByDottedPath("dev_mode_proxy.proxy_url", proxy.proxy_url);
-      return content;
-    }
-  };
-
   base::Value::Dict value;
-  value.Set("content", absl::visit(ContentVisitor(), content));
+  value.Set(
+      "content",
+      absl::visit(
+          base::Overloaded{
+              [](const WebApp::IsolationData::InstalledBundle& bundle) {
+                base::Value::Dict content;
+                content.SetByDottedPath("installed_bundle.path", bundle.path);
+                return content;
+              },
+              [](const WebApp::IsolationData::DevModeBundle& bundle) {
+                base::Value::Dict content;
+                content.SetByDottedPath("dev_mode_bundle.path", bundle.path);
+                return content;
+              },
+              [](const WebApp::IsolationData::DevModeProxy& proxy) {
+                base::Value::Dict content;
+                content.SetByDottedPath("dev_mode_proxy.proxy_url",
+                                        proxy.proxy_url);
+                return content;
+              },
+          },
+          content));
   return base::Value(std::move(value));
 }
 
