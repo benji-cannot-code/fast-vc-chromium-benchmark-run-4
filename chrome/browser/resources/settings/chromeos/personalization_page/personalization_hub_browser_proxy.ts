a628b63edb3dcf3a6bc28c772b254844be3e4e1e
Jason Thai <jasontt@chromium.org>
2022-08-24 19:50:45 +0000

    CrOS Settings: Convert personalization page to TS
    Also cleaned up unused vars and irrelevant methods.
    
    BUG=chromium:1315757
    TEST=browser_tests
    --gtest_filter="*PersonalizationPageWithPersonalizationHub*.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ie0f47997a4ee790f8f318d20a73b9741602bb9f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3847040
    Quick-Run: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038911}

---
+++ chrome/browser/resources/settings/chromeos/personalization_page/personalization_hub_browser_proxy.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export interface PersonalizationHubBrowserProxy {
+  openPersonalizationHub(): void;
+}
+
+let instance: PersonalizationHubBrowserProxy|null = null;
+
+export class PersonalizationHubBrowserProxyImpl implements
+    PersonalizationHubBrowserProxy {
+  static getInstance(): PersonalizationHubBrowserProxy {
+    return instance || (instance = new PersonalizationHubBrowserProxyImpl());
+  }
+
+  static setInstanceForTesting(obj: PersonalizationHubBrowserProxy) {
+    instance = obj;
+  }
+
+  openPersonalizationHub() {
+    chrome.send('openPersonalizationHub');
+  }
+}
