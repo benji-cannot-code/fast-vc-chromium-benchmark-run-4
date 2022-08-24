d6c7f01ed303bcdfc36f682e904afbd4d2dafbf6
mingxuanshi <mingxuanshi@google.com>
2022-08-24 09:27:02 +0000

    Collect the number of user selection when the user selects an audiodevice as the activated device.
    The collected metric StatusArea_Audio_SwitchInputDevice and StatusArea_Audio_SwitchOutputDevice are not used in anywhere before. They were added back in 2014, check out: https://chromium.googlesource.com/chromium/src/+/2f92dec0177daf6c7d7be1bb44e98fb1cf501f62
    
    BUG: b:201715431
    Change-Id: I79c47fa5e91e98d99f32f7d894121284b8e28f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810765
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Ming-Xuan Shi <mingxuanshi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038671}

--- chromeos/ash/components/audio/cras_audio_handler.h
+++ chromeos/ash/components/audio/cras_audio_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/user_metrics.h"
 #include "base/observer_list.h"
 #include "base/timer/timer.h"
 #include "chromeos/ash/components/audio/audio_device.h"
