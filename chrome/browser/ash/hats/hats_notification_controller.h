f97c1498f0e815a634dca976ee5753f1164fd18a
Steven Bennetts <stevenjb@chromium.org>
2022-08-24 00:22:27 +0000

    CrOS Network: Replace NetworkPortalDetector::Observer with NSHO
    NetworkPortalDetector::Observer only notifies when Chrome portal
    detection detects a change. Generally we want to know about any
    change in portal state, including Shill detection. This will be
    important once Shill detection becomes primary and Chrome detection
    is only used when a proxy is configured.
    
    This switches to NetworkStateHandlerObserver for:
    DriveIntegrationService
    HatsNotificationController
    
    BUG=b:243405151
    TEST=unit_tests, browser_tests
    
    Change-Id: I799ef86031caf4791c73f2726996689de5e2fa59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846604
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038494}

--- chrome/browser/ash/hats/hats_notification_controller.h
+++ chrome/browser/ash/hats/hats_notification_controller.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
-#include "chromeos/ash/components/network/portal_detector/network_portal_detector.h"
+#include "chromeos/ash/components/network/network_state_handler_observer.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
 
@@ -28,7 +28,7 @@ class HatsDialog;
 // managing the HaTS notification that is displayed to the user.
 // This class lives on the UI thread.
 class HatsNotificationController : public message_center::NotificationDelegate,
-                                   public NetworkPortalDetector::Observer {
+                                   public NetworkStateHandlerObserver {
  public:
   static const char kNotificationId[];
 
@@ -80,16 +80,17 @@ class HatsNotificationController : public message_center::NotificationDelegate,
     kMaxValue = kNotificationClicked
   };
 
-  // NotificationDelegate overrides:
   void Initialize(bool is_new_device);
+
+  // NotificationDelegate overrides:
   void Close(bool by_user) override;
   void Click(const absl::optional<int>& button_index,
              const absl::optional<std::u16string>& reply) override;
 
-  // NetworkPortalDetector::Observer override:
-  void OnPortalDetectionCompleted(
-      const NetworkState* network,
-      const NetworkPortalDetector::CaptivePortalStatus status) override;
+  // NetworkStateHandlerObserver override:
+  void PortalStateChanged(const ash::NetworkState* default_network,
+                          ash::NetworkState::PortalState portal_state) override;
+  void OnShuttingDown() override;
 
   void UpdateLastInteractionTime();
 
