4accc17d7ed3005226b26181f0cf2c432015bf55
Alan Cutter <alancutter@chromium.org>
2022-08-24 01:31:51 +0000

    Remove false const qualifiers from WebAppIconManager methods
    This CL removes the const qualifier from WebAppIconManager methods that
    call LogErrorsCallCallback() which appends to the error_log of a mutable
    this.
    
    This const violation was possible via WeakPtrFactory::GetWeakPtr() const
    returning a mutable handle to T.
    
    This CL also exposes const accurate GetWeakPtr() methods on
    WebAppIconManager in preparation for icon health checks.
    
    Bug: 1353659
    Change-Id: I6d5cd6347f96b0c3fc2bb1a079e7fe228d5042bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849834
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038521}

--- chrome/browser/web_applications/test/web_app_icon_test_utils.cc
+++ chrome/browser/web_applications/test/web_app_icon_test_utils.cc
@@ -297,7 +297,7 @@ void IconManagerStartAndAwaitFaviconMonochrome(WebAppIconManager& icon_manager,
   run_loop.Run();
 }
 
-SkColor IconManagerReadAppIconPixel(const WebAppIconManager& icon_manager,
+SkColor IconManagerReadAppIconPixel(WebAppIconManager& icon_manager,
                                     const AppId& app_id,
                                     SquareSizePx size_px,
                                     int x,
