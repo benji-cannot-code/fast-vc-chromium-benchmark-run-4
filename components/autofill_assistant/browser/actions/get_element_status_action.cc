31ff8fd1db8ee5515d3a9742427363ef0e134eb0
Peter Kasting <pkasting@chromium.org>
2022-08-24 17:17:51 +0000

    Remove IsUnicodeWhitespace<(sizeof(Char) == 1)>.
    This function is unsafe.  Callers either should use IsAsciiWhitespace()
    or should convert to a proper Unicode codepoint before calling.
    
    This also makes IsAsciiWhitespace() consistent with kWhitespaceASCII.
    
    Adds a test that would have failed before this patch.
    
    Bug: 1353959
    Change-Id: If14f8b81f3e2918c8cb6c4e592e090389fa89116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840750
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Florian Gauger <fga@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038831}

--- components/autofill_assistant/browser/actions/get_element_status_action.cc
+++ components/autofill_assistant/browser/actions/get_element_status_action.cc
@@ -29,7 +29,7 @@ struct MaybeRe2 {
 
 std::string RemoveWhitespace(const std::string& value) {
   std::string copy = value;
-  base::EraseIf(copy, base::IsUnicodeWhitespace<char>);
+  base::EraseIf(copy, base::IsAsciiWhitespace<char>);
   return copy;
 }
 
