8563deaf23fa024e9cd3211bdf5bf2d12a5c8baf
Jeroen Dhollander <jeroendh@google.com>
2022-08-24 18:03:29 +0000

    Remove forward declaration of view::Widget from permission_prompt_bubble_view.h
    This actually should have been |view*s*::Widget| (not |view::Widget|),
    which confused the auto-add-include feature of clangd.
    Also it's not used in this header so it can be safely removed.
    
    I also removed the other unused forward declaration
    (|permission::RequestType|).
    
    Test: compiled
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia57f1cab3ef2f1b0695b21271e1b7ba504cb8e68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845274
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038853}

--- chrome/browser/ui/views/permissions/permission_prompt_bubble_view.h
+++ chrome/browser/ui/views/permissions/permission_prompt_bubble_view.h
@@ -16,17 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 namespace permissions {
-enum class RequestType;
 enum class PermissionAction;
 }  // namespace permissions
-
-namespace view {
-class Widget;
-}
-
-namespace view {
-class Widget;
-}
 
 class Browser;
 
