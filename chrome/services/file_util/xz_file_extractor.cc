5af9afb7f7c5c19f7b4e29154f96a6c9dee0d7b6
Ken Rockot <rockot@google.com>
2022-08-24 18:04:31 +0000

    Mojo: Prepare data pipe usage for mojo-ipcz
    This tweaks the Mojo data pipe API to specify that MojoBeginWriteData()
    takes an input size as a hint for the intended transfer size. The
    current Mojo implementation ignores this hint, but mojo-ipcz will use it
    to inform allocation decisions since data pipes will no longer be backed
    by dedicated shared memory regions.
    
    This also adapts the small number of invocations that previously
    provided an uninitialized input size to MojoBeginWriteData(), ensuring
    that all callers either provide a meaningful hint, or provide an input
    size of zero to opt into default allocation behavior.
    
    Bug: 1299283
    Change-Id: Ia6afb48ca1e11c66d3862d1a84f7c853bf6e1b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846122
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038854}

--- chrome/services/file_util/xz_file_extractor.cc
+++ chrome/services/file_util/xz_file_extractor.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/numerics/safe_conversions.h"
 #include "third_party/lzma_sdk/C/7zCrc.h"
 #include "third_party/lzma_sdk/C/Xz.h"
 #include "third_party/lzma_sdk/C/XzCrc64.h"
@@ -87,7 +88,7 @@ class XzState {
     // CODER_STATUS_NOT_FINISHED.
     while (status == CODER_STATUS_NOT_FINISHED) {
       uint8_t* data = nullptr;
-      uint32_t size = 0;
+      uint32_t size = base::checked_cast<uint32_t>(buffer_size_);
       result = producer_->BeginWriteData(reinterpret_cast<void**>(&data), &size,
                                          MOJO_WRITE_DATA_FLAG_NONE);
       if (result == MOJO_RESULT_SHOULD_WAIT) {
--- mojo/core/dispatcher.h
+++ mojo/core/dispatcher.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/core/handle_signals_state.h"
 #include "mojo/core/system_impl_export.h"
 #include "mojo/core/watch.h"
+#include "mojo/public/c/system/data_pipe.h"
 #include "mojo/public/c/system/quota.h"
 #include "mojo/public/c/system/trap.h"
 #include "mojo/public/c/system/types.h"
@@ -195,7 +196,9 @@ class MOJO_SYSTEM_IMPL_EXPORT Dispatcher
   // Supports the the |MojoBeginWriteData()| API if implemented by this
   // Dispatcher. Arguments correspond to the ones given to the original API
   // call. See |MojoBeginWriteData()| documentation.
-  virtual MojoResult BeginWriteData(void** buffer, uint32_t* buffer_num_bytes);
+  virtual MojoResult BeginWriteData(void** buffer,
+                                    uint32_t* buffer_num_bytes,
+                                    MojoBeginWriteDataFlags flags);
 
   // Supports the the |MojoEndWriteData()| API if implemented by this
   // Dispatcher. Arguments correspond to the ones given to the original API
