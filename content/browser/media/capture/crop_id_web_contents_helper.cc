4e915cf6dcf58f53df79ca9f7006a620d49c2300
Elad Alon <eladalon@chromium.org>
2022-08-25 21:05:56 +0000

    Reassign remaining non-specific Region Capture TODOs to cleanup
    With the main work on Region Capture now done, and the feature shipped
    as of m104, it is now time to close the tracking bug. Remaining TODOs
    are minor cleanup, and are therefore assigned to the cleanup bug.
    
    Bug: 1264849
    Change-Id: Ie58f6d8532db9700d3aef934f02f775e26ec7ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854088
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039434}

--- content/browser/media/capture/crop_id_web_contents_helper.cc
+++ content/browser/media/capture/crop_id_web_contents_helper.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// TODO(crbug.com/1247761): Remove this protected static function.
+// TODO(crbug.com/1264849): Remove this protected static function.
 // See header for more details.
 base::Token CropIdWebContentsHelper::GUIDToToken(const base::GUID& guid) {
   std::string lowercase = guid.AsLowercaseString();
