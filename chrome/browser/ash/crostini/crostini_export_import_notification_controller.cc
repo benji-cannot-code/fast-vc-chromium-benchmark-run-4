e616d8cee405f021d9528c85efc0ac32921c423d
James Ye <jamesye@google.com>
2022-08-25 02:39:13 +0000

    Move Crostini notification icon to ash/resources/
    Move the Crostini notification icon from //chrome/app/vector_icons to
    //ash/resources/vector_icons. This makes it accessible to
    //components/exo.
    
    Bug: b:242660232
    Test: compile
    Change-Id: I42ee60e041abd45d71491811cb9724d552411518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3853821
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: James Ye <jamesye@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039046}

--- chrome/browser/ash/crostini/crostini_export_import_notification_controller.cc
+++ chrome/browser/ash/crostini/crostini_export_import_notification_controller.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/constants/url_constants.h"
 #include "ash/public/cpp/notification_utils.h"
-#include "chrome/app/vector_icons/vector_icons.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "chrome/browser/ash/crostini/crostini_export_import.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/platform_util.h"
@@ -60,7 +60,7 @@ CrostiniExportImportNotificationController::
       weak_ptr_factory_.GetWeakPtr()));
 
   message_center::RichNotificationData rich_notification_data;
-  rich_notification_data.vector_small_image = &kNotificationLinuxIcon;
+  rich_notification_data.vector_small_image = &ash::kNotificationLinuxIcon;
   rich_notification_data.accent_color = ash::kSystemNotificationColorNormal;
 
   notification_ = std::make_unique<message_center::Notification>(
--- chrome/browser/ash/crostini/crostini_package_notification.cc
+++ chrome/browser/ash/crostini/crostini_package_notification.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/notification_utils.h"
-#include "chrome/app/vector_icons/vector_icons.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "chrome/browser/ash/crostini/crostini_package_service.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service_factory.h"
@@ -67,7 +67,7 @@ CrostiniPackageNotification::CrostiniPackageNotification(
         ->AddObserver(this);
   }
   message_center::RichNotificationData rich_notification_data;
-  rich_notification_data.vector_small_image = &kNotificationLinuxIcon;
+  rich_notification_data.vector_small_image = &ash::kNotificationLinuxIcon;
   rich_notification_data.never_timeout = true;
   rich_notification_data.accent_color = ash::kSystemNotificationColorNormal;
 
