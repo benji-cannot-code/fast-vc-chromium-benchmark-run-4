249a51e3931381c7a154ecd68395897b66828438
Peter Kvitek <kvitekp@chromium.org>
2022-08-25 05:04:55 +0000

    [DevTools] Added 'printing-in-progress' error code.
    This CL replaces a CHECK with an error code that is returned by the
    PrintRenderFrame::PrintWithParams() mojo method. We actually used
    to have similar error code, but it was generated at much higher
    level which prevented simultaneous printing of unrelated pages.
    
    Change-Id: I0e98f3e99b762af4545a9ea485b374f37fbf6010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848842
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039085}

--- components/printing/browser/print_to_pdf/pdf_print_result.cc
+++ components/printing/browser/print_to_pdf/pdf_print_result.cc
@@ -13,8 +13,6 @@ std::string PdfPrintResultToString(PdfPrintResult result) {
       return std::string();  // no error message
     case PdfPrintResult::kPrintFailure:
       return "Printing failed";
-    case PdfPrintResult::kInvalidPrinterSettings:
-      return "Show invalid printer settings error";
     case PdfPrintResult::kInvalidSharedMemoryRegion:
       return "Invalid shared memory region";
     case PdfPrintResult::kInvalidSharedMemoryMapping:
@@ -25,6 +23,8 @@ std::string PdfPrintResultToString(PdfPrintResult result) {
       return "Page range is invalid (start > end)";
     case PdfPrintResult::kPageCountExceeded:
       return "Page range exceeds page count";
+    case PdfPrintResult::kPrintingInProgress:
+      return "Page is already being printed";
   }
 }
 
