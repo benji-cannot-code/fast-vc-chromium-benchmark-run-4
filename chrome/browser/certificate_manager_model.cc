8f3a08eb5026106502141d91502f6e4914ef21d1
Fabian Sommer <fabiansommer@chromium.org>
2022-08-25 14:26:22 +0000

    Delete unnecessary includes of crypto/nss_util.h
    nss_util.h was included in multiple files that did not actually use any
    of its declarations.
    This is problematic because soon, nss_util.h will include a buildflags
    header that will only be present if nss_util is actually built, which
    does not happen on all platforms.
    
    While I am at it, adjust the affected files to include the correct
    headers from crypto, deleting two other unused headers, and adding one
    which does get used.
    
    Bug: 1339021
    Change-Id: I42400e57a4d9446b4c48c8f80cefa2163c1026d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854026
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039238}

--- chrome/browser/certificate_manager_model.cc
+++ chrome/browser/certificate_manager_model.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/resource_context.h"
-#include "crypto/nss_util.h"
+#include "crypto/scoped_nss_types.h"
 #include "net/base/net_errors.h"
 #include "net/cert/cert_database.h"
 #include "net/cert/x509_certificate.h"
--- chrome/common/net/x509_certificate_model_nss.cc
+++ chrome/common/net/x509_certificate_model_nss.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/third_party/mozilla_security_manager/nsNSSCertHelper.h"
 #include "chrome/third_party/mozilla_security_manager/nsNSSCertificate.h"
-#include "crypto/nss_util.h"
-#include "crypto/scoped_nss_types.h"
 #include "net/cert/x509_util_nss.h"
 
 namespace psm = mozilla_security_manager;
