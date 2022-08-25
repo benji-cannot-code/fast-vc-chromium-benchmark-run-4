54aebe9cde45f33382937afff20195c8be685123
Ari Chivukula <arichiv@chromium.org>
2022-08-25 11:53:59 +0000

    [ALPS] Don't kill sessions on kAcceptChMalformed
    We should skip errors noted when parsing ACCEPT_CH frame payloads.
    Taking the error causes the session to be terminated, which caused
    issues in clients before bad parsing code fixed in crrev.com/c/3838726.
    
    Bug: 1355247
    Change-Id: Ibc48704f1d8e0c99bbd6d4b0df0f6a49baf2521d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846099
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039194}

--- net/spdy/alps_decoder.cc
+++ net/spdy/alps_decoder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/alps_decoder.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "net/base/features.h"
 
 namespace net {
@@ -145,8 +146,18 @@ void AlpsDecoder::AcceptChParser::OnFramePayload(const char* data, size_t len) {
     base::StringPiece value;
     if (!ReadUint16PrefixedStringPiece(&payload, &origin) ||
         !ReadUint16PrefixedStringPiece(&payload, &value)) {
-      error_ = Error::kAcceptChMalformed;
-      return;
+      if (base::FeatureList::IsEnabled(
+              features::kShouldKillSessionOnAcceptChMalformed)) {
+        // This causes a session termination.
+        error_ = Error::kAcceptChMalformed;
+        return;
+      } else {
+        // This logs that a session termination was bypassed.
+        base::UmaHistogramEnumeration(
+            "Net.SpdySession.AlpsDecoderStatus.Bypassed",
+            Error::kAcceptChMalformed);
+        return;
+      }
     }
     accept_ch_.push_back(
         spdy::AcceptChOriginValuePair{std::string(origin), std::string(value)});
--- net/spdy/alps_decoder_test.cc
+++ net/spdy/alps_decoder_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/alps_decoder.h"
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "net/base/features.h"
 #include "net/base/hex_utils.h"
@@ -317,7 +318,23 @@ TEST(AlpsDecoderTest, UnknownFrame) {
   EXPECT_EQ(0, decoder.settings_frame_count());
 }
 
-TEST(AlpsDecoderTest, MalformedAcceptChFrame) {
+class AlpsDecoderTestWithFeature : public ::testing::TestWithParam<bool> {
+ public:
+  bool ShouldKillSessionOnAcceptChMalformed() { return GetParam(); }
+
+ private:
+  void SetUp() override {
+    feature_list_.InitWithFeatureState(
+        features::kShouldKillSessionOnAcceptChMalformed,
+        ShouldKillSessionOnAcceptChMalformed());
+  }
+
+  base::test::ScopedFeatureList feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(All, AlpsDecoderTestWithFeature, testing::Bool());
+
+TEST_P(AlpsDecoderTestWithFeature, MalformedAcceptChFrame) {
   // Correct, complete payload.
   std::string payload = HexDecode(
       "0017"  // origin length
@@ -328,6 +345,7 @@ TEST(AlpsDecoderTest, MalformedAcceptChFrame) {
 
   for (uint8_t payload_length = 1; payload_length < payload.length();
        payload_length++) {
+    base::HistogramTester histogram_tester;
     // First two bytes of length.
     std::string frame = HexDecode("0000");
     // Last byte of length.
@@ -342,8 +360,16 @@ TEST(AlpsDecoderTest, MalformedAcceptChFrame) {
 
     AlpsDecoder decoder;
     AlpsDecoder::Error error = decoder.Decode(frame);
-
-    EXPECT_EQ(AlpsDecoder::Error::kAcceptChMalformed, error);
+    if (ShouldKillSessionOnAcceptChMalformed()) {
+      EXPECT_EQ(AlpsDecoder::Error::kAcceptChMalformed, error);
+      histogram_tester.ExpectTotalCount(
+          "Net.SpdySession.AlpsDecoderStatus.Bypassed", 0);
+    } else {
+      EXPECT_EQ(AlpsDecoder::Error::kNoError, error);
+      histogram_tester.ExpectUniqueSample(
+          "Net.SpdySession.AlpsDecoderStatus.Bypassed",
+          static_cast<int>(AlpsDecoder::Error::kAcceptChMalformed), 1);
+    }
   }
 }
 
