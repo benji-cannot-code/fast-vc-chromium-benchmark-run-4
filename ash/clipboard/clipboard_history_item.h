1e9f983e1cf227a73f22f4774c0e9cd6bafd2fc5
Colin Kincaid <ckincaid@chromium.org>
2022-08-25 00:44:16 +0000

    multipaste: Update history items' timestamps during reorder-on-copy
    When a user copies data that is already in clipboard history, the
    original clipboard history item is moved to the top of the list.
    Because the item is reused, its timestamp does not change, so if it was
    originally copied more than two minutes ago, it is not shown as a
    suggested paste item despite the user having just copied it.
    
    This CL fixes the above behavior by updating a clipboard history item's
    "time copied" field when a duplicate copy is detected.
    
    Bug: 1356383
    Change-Id: I67705abda841f24108a71928961a9f3015a95d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855258
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039009}

--- ash/clipboard/clipboard_history_item.h
+++ ash/clipboard/clipboard_history_item.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_CLIPBOARD_CLIPBOARD_HISTORY_ITEM_H_
 
 #include "ash/ash_export.h"
-#include "base/i18n/time_formatting.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "ui/base/clipboard/clipboard_data.h"
@@ -44,8 +42,7 @@ class ASH_EXPORT ClipboardHistoryItem {
   // Underlying data for an item in the clipboard history menu.
   ui::ClipboardData data_;
 
-  // Recorded when data is committed to the clipboard history list for the first
-  // time.
+  // Time when the item's current data was set.
   base::Time time_copied_;
 };
 
