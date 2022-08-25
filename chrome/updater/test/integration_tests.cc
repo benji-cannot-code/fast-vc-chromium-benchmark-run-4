ae13f59a834188bec1c0a0d85d9bfa586ef5e5fb
Sorin Jianu <sorin@chromium.org>
2022-08-25 16:52:40 +0000

    Remove redundant ExpectClean() in updater tests.
    Fixed: 1356646
    
    Change-Id: I8d2a236f1df3b1194a052081b4311424c0956857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855638
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039294}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -634,7 +634,6 @@ TEST_F(IntegrationTest, UninstallCmdLine) {
   // Uninstall the idle updater.
   RunUninstallCmdLine();
   WaitForUpdaterExit();
-  ExpectClean();
 }
 #endif  // BUILDFLAG(IS_WIN)
 
@@ -666,7 +665,6 @@ TEST_F(IntegrationTest, UninstallIfMaxServerWakesBeforeRegistrationExceeded) {
   SetServerStarts(24);
   RunWake(0);
   WaitForUpdaterExit();
-  ExpectClean();
 }
 
 TEST_F(IntegrationTest, UninstallUpdaterWhenAllAppsUninstalled) {
@@ -684,7 +682,6 @@ TEST_F(IntegrationTest, UninstallUpdaterWhenAllAppsUninstalled) {
   UninstallApp("test1");
   RunWake(0);
   WaitForUpdaterExit();
-  ExpectClean();
 }
 
 // Windows does not currently have a concept of app ownership, so this
