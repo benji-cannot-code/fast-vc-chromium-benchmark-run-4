f9b555c4d9275209aaddbfd3690078fd0ad5e077
Brandon Fong <bjfong@google.com>
2022-08-25 18:39:52 +0000

    [Code Health] Replace JNI calls with native logic on restoring frozen tabs
    The goal is to remove additional JNI calls made from the native side on restoration of frozen tabs and perform native side logic instead.
    
    Change-Id: I86bc0c44b4a88722f8ee166901b7603358804459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817844
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039352}

--- chrome/browser/android/historical_tab_saver.h
+++ chrome/browser/android/historical_tab_saver.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/pickle.h"
 
 class TabAndroid;
 namespace content {
@@ -16,6 +17,22 @@ class WebContents;
 }  // namespace content
 
 namespace historical_tab_saver {
+
+// A struct to store the WebContentsState passed down from the JNI to be
+// potentially used in restoring a frozen tab, as a byte buffer.
+struct WebContentsStateByteBuffer {
+  WebContentsStateByteBuffer();
+  ~WebContentsStateByteBuffer();
+  /**
+   * @param data WebContentsState data stored as a byte buffer.
+   * @param size Byte buffer size.
+   * @param saved_state_version Saved state version of the WebContentsState.
+   */
+  WebContentsStateByteBuffer(void* data, int size, int saved_state_version);
+
+  base::Pickle byte_buffer;
+  int state_version;
+};
 
 // A wrapper to manage a web contents of a possibly frozen tab.
 class ScopedWebContents {
@@ -25,20 +42,24 @@ class ScopedWebContents {
   // temporary one will be created from the frozen tab's WebContentsState. If
   // the WebContents was created from a frozen tab it will be destroyed with the
   // returned object.
-  static std::unique_ptr<ScopedWebContents> CreateForTab(TabAndroid* tab);
+  static std::unique_ptr<ScopedWebContents> CreateForTab(
+      TabAndroid* tab,
+      const WebContentsStateByteBuffer* webContentsStateByteBuffer);
 
   ~ScopedWebContents();
 
   ScopedWebContents(const ScopedWebContents&) = delete;
   ScopedWebContents& operator=(const ScopedWebContents&) = delete;
 
-  content::WebContents* web_contents() const { return web_contents_; }
+  explicit ScopedWebContents(content::WebContents* unowned_web_contents);
+  explicit ScopedWebContents(
+      std::unique_ptr<content::WebContents> owned_web_contents);
+
+  content::WebContents* web_contents() const;
 
  private:
-  ScopedWebContents(content::WebContents* web_contents, bool was_frozen);
-
-  raw_ptr<content::WebContents> web_contents_;
-  bool was_frozen_;
+  raw_ptr<content::WebContents> unowned_web_contents_;
+  std::unique_ptr<content::WebContents> owned_web_contents_;
 };
 
 }  // namespace historical_tab_saver
