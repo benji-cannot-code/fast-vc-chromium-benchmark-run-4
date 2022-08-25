02f6f16a84c27dc8fa17275e8774dc2345bcaa59
Phillis Tang <phillis@google.com>
2022-08-25 19:25:14 +0000

    Use style::STYLE_SECONDARY for subtitle
    Make the subtitle for DialogModel::Builder use secondary style to
    differentiate from main title.
    
    See example: https://screenshot.googleplex.com/5XJkar6AoEVaptz
    
    Bug: 1356307
    Change-Id: I19bb93d11fa1d40a7d3ab7a59e457ff1b728a854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855153
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039370}

--- ui/views/bubble/bubble_frame_view.cc
+++ ui/views/bubble/bubble_frame_view.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/paint_info.h"
 #include "ui/views/resources/grit/views_resources.h"
+#include "ui/views/style/typography.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
@@ -109,7 +110,9 @@ BubbleFrameView::BubbleFrameView(const gfx::Insets& title_margins,
       main_image_(AddChildView(std::make_unique<ImageView>())),
       default_title_(AddChildView(CreateDefaultTitleLabel(std::u16string()))),
       subtitle_(AddChildView(
-          CreateLabelWithContext(std::u16string(), style::CONTEXT_LABEL))) {
+          CreateLabelWithContextAndStyle(std::u16string(),
+                                         style::CONTEXT_LABEL,
+                                         style::STYLE_SECONDARY))) {
   default_title_->SetVisible(false);
   main_image_->SetVisible(false);
   subtitle_->SetVisible(false);
@@ -160,7 +163,8 @@ BubbleFrameView::~BubbleFrameView() = default;
 // static
 std::unique_ptr<Label> BubbleFrameView::CreateDefaultTitleLabel(
     const std::u16string& title_text) {
-  return CreateLabelWithContext(title_text, style::CONTEXT_DIALOG_TITLE);
+  return CreateLabelWithContextAndStyle(title_text, style::CONTEXT_DIALOG_TITLE,
+                                        style::STYLE_PRIMARY);
 }
 
 // static
@@ -1063,10 +1067,11 @@ int BubbleFrameView::GetMainImageLeftInsets() const {
 }
 
 // static
-std::unique_ptr<Label> BubbleFrameView::CreateLabelWithContext(
+std::unique_ptr<Label> BubbleFrameView::CreateLabelWithContextAndStyle(
     const std::u16string& label_text,
-    style::TextContext text_context) {
-  auto label = std::make_unique<Label>(label_text, text_context);
+    style::TextContext text_context,
+    style::TextStyle text_style) {
+  auto label = std::make_unique<Label>(label_text, text_context, text_style);
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   label->SetCollapseWhenHidden(true);
   label->SetMultiLine(true);
--- ui/views/bubble/bubble_frame_view.h
+++ ui/views/bubble/bubble_frame_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/progress_bar.h"
 #include "ui/views/input_event_activation_protector.h"
+#include "ui/views/style/typography.h"
 #include "ui/views/window/non_client_view.h"
 
 namespace gfx {
@@ -260,9 +261,10 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView {
   int GetMainImageLeftInsets() const;
 
   // Helper method to create a label with text style
-  static std::unique_ptr<Label> CreateLabelWithContext(
+  static std::unique_ptr<Label> CreateLabelWithContextAndStyle(
       const std::u16string& label_text,
-      style::TextContext text_context);
+      style::TextContext text_context,
+      style::TextStyle text_style);
 
   // The bubble border.
   raw_ptr<BubbleBorder> bubble_border_ = nullptr;
