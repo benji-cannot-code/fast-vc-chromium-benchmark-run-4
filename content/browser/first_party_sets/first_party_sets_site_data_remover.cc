1e96c66831a0deb111c388f11cd91fd6604ac0b1
Shuran Huang <shuuran@chromium.org>
2022-08-25 23:21:20 +0000

    Rename method in FPSDataRemover to be simpler and clearer.
    Change-Id: Ib6065bbba40ad89883def5ada7b1eef14e2fb3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857988
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Auto-Submit: Shuran Huang <shuuran@chromium.org>
    Quick-Run: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039496}

--- content/browser/first_party_sets/first_party_sets_site_data_remover.cc
+++ content/browser/first_party_sets/first_party_sets_site_data_remover.cc
@@ -93,7 +93,7 @@ class ClearingTask : public BrowsingDataRemover::Observer {
 }  // namespace
 
 // static
-void FirstPartySetsSiteDataRemover::HandleRemovingSiteData(
+void FirstPartySetsSiteDataRemover::RemoveSiteData(
     BrowsingDataRemover* remover,
     std::vector<net::SchemefulSite> sites,
     base::OnceCallback<void(uint64_t)> callback) {
