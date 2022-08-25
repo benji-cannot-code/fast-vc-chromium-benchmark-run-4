875a6a882e61019fe6ebb113a88f6870b43b9139
Ming-Ying Chung <mych@chromium.org>
2022-08-25 07:35:36 +0000

    [beacon-api] Implement general `timeout`
    The `timeout` property works independently from `backgroundTimeout` [1].
    Its timer starts immediately right after the value is set or updated to
    >= 0.
    
    Due to this reason, it's difficult to bundle several beacons together.
    The implementation in this CL adds makes `PendingBeacon` itself handle the timer.
    
    [1]: https://github.com/WICG/unload-beacon#properties
    
    Bug: 1293679
    Change-Id: I195abebe625143df687724b311ff60d7e74b9c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835447
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039129}

--- third_party/blink/renderer/core/frame/pending_beacon.h
+++ third_party/blink/renderer/core/frame/pending_beacon.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/pending_beacon_dispatcher.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
+#include "third_party/blink/renderer/platform/timer.h"
 
 namespace blink {
 
@@ -21,6 +22,12 @@ class ExecutionContext;
 
 // Implementation of the PendingBeacon API.
 // https://github.com/WICG/unload-beacon/blob/main/README.md
+// Note that the lifetime of a PendingBeacon instance is not the same as the JS
+// scope where the instance is created. Rather, it stays alive until
+//   - roughly when `sendNow()` or `deactivate()` is called (may still be alive
+//     for a while after this point).
+//   - when the document where it was created is destroyed.
+// See `PendingBeaconDispatcher` for more details.
 class CORE_EXPORT PendingBeacon
     : public ScriptWrappable,
       public PendingBeaconDispatcher::PendingBeacon {
@@ -65,6 +72,11 @@ class CORE_EXPORT PendingBeacon
                        ExceptionState& exception_state);
 
  private:
+  // A convenient method to return a TaskRunner which is able to keep working
+  // even if the JS context is frozen.
+  scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner();
+  void TimeoutTimerFired(TimerBase*);
+
   Member<ExecutionContext> ec_;
   HeapMojoRemote<mojom::blink::PendingBeacon> remote_;
   String url_;
@@ -72,6 +84,9 @@ class CORE_EXPORT PendingBeacon
   base::TimeDelta background_timeout_;
   base::TimeDelta timeout_;
   bool pending_ = true;
+
+  // A timer to handle `setTimeout()`.
+  HeapTaskRunnerTimer<PendingBeacon> timeout_timer_;
 };
 
 }  // namespace blink
