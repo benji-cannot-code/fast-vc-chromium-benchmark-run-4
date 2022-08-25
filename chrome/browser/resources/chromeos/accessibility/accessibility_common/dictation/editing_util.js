4b4e820048e885746be539043e1d8ae8344c0548
Akihiro Ota <akihiroota@chromium.org>
2022-08-25 20:51:53 +0000

    Dictation: Support smart_delete_phrase in Japanese
    Adds the logic necessary to support the smart_delete_phrase macro
    in Japanese. Specifically, this means:
    1. changing the speech parser so that it removes spaces from command
    regexs (since Japanese strings will not contain strings).
    2. changing editing regexs so that they don't use word boundaries,
    since \b in regexs isn't supported for non-ascii characters [1]
    3. Adding a test to confirm expected behavior
    
    [1] https://mathiasbynens.be/notes/es6-unicode-regex
    
    languages that don't use spaces).
    
    AX-Relnotes: Fixes smart commands in Japanese (and potentially other
    Bug: 1353368
    Change-Id: If2080b83e62b3e4f266ee467eb1b452246150a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833544
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039425}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/editing_util.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/editing_util.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {LocaleInfo} from './locale_info.js';
+
 /**
  * EditingUtil provides utility and helper methods for editing-related
  * operations.
@@ -30,10 +32,15 @@ export class EditingUtil {
     deletePhrase = deletePhrase.trim();
     insertPhrase = insertPhrase.trim();
 
-    // Find the right-most occurrence of `deletePhrase`. Require `deletePhrase`
-    // to be separated by word boundaries. If we're deleting text, prefer
-    // the RegExps that include a leading/trailing space to preserve spacing.
-    const re = EditingUtil.getPhraseRegex_(deletePhrase);
+    // Find the right-most occurrence of `deletePhrase`. If we're deleting text,
+    // prefer the RegExps that include a leading/trailing space to preserve
+    // spacing.
+    let re;
+    if (LocaleInfo.considerSpaces()) {
+      re = EditingUtil.getPhraseRegex_(deletePhrase);
+    } else {
+      re = EditingUtil.getPhraseRegexNoWordBoundaries_(deletePhrase);
+    }
     const reWithLeadingSpace =
         EditingUtil.getPhraseRegexLeadingSpace_(deletePhrase);
     const reWithTrailingSpace =
@@ -280,6 +287,7 @@ export class EditingUtil {
   }
 
   /**
+   * TODO(akihiroota): Break regex construction into smaller chunks.
    * Returns a RegExp that matches on the right-most occurrence of a phrase.
    * The returned RegExp is case insensitive and requires that `phrase` is
    * separated by word boundaries.
@@ -289,6 +297,17 @@ export class EditingUtil {
    */
   static getPhraseRegex_(phrase) {
     return new RegExp(`(\\b${phrase}\\b)(?!.*\\b\\1\\b)`, 'i');
+  }
+
+  /**
+   * Similar to above, but doesn't include word boundaries. This is useful for
+   * languages that don't use spaces e.g. Japanese.
+   * @param {string} phrase
+   * @return {!RegExp}
+   * @private
+   */
+  static getPhraseRegexNoWordBoundaries_(phrase) {
+    return new RegExp(`(${phrase})(?!.*\\1)`, 'i');
   }
 
   /**
