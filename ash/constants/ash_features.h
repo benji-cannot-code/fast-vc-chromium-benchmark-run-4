d1741a4220637d5223fddb1be6d2c5b0c52679e5
Nicholas Hollingum <hollingum@google.com>
2022-08-25 04:23:16 +0000

    borealis: Add a flag to force 2x zoom in the client
    Steam's scaling behaviour on 2k screens is problematic, since it seems
    to only scale itself (by 2x) on 4k displays, leaving 2k displays
    visually small.
    
    Ideally we will await improvements by valve to the scaling logic, until
    then we have this flag which users can employ to double the size of the
    steam client as needed.
    
    Bug: b/171935238
    Change-Id: I669439f5ce3e9658040aaa54021890c7225ea7d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784487
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039075}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -120,6 +120,8 @@ extern const base::Feature kBorealisDiskManagement;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kBorealisForceBetaClient;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kBorealisForceDoubleScale;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kBorealisLinuxMode;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kBorealisPermitted;
