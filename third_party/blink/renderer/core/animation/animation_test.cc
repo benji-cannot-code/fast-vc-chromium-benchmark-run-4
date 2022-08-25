6807e1ce5a7e88a080fc1177bad58d7693d4ddf9
Xianzhu Wang <wangxianzhu@chromium.org>
2022-08-25 21:24:46 +0000

    Remove virtual/composite-relative-keyframes and the blink feature
    Bug: 1354826, 389359
    Change-Id: Id8b4e956ea1e86a88aa22a82bf1ac5dca7d76f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843274
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039442}

--- third_party/blink/renderer/core/animation/animation_test.cc
+++ third_party/blink/renderer/core/animation/animation_test.cc
@@ -1543,9 +1543,6 @@ TEST_P(AnimationAnimationTestCompositing, InfiniteDurationAnimation) {
 // of the animation if running.
 TEST_P(AnimationAnimationTestCompositing,
        RestartCompositedAnimationOnSizeChange) {
-  // TODO(crbug.com/389359): Remove forced feature enabling once on by
-  // default.
-  ScopedCompositeRelativeKeyframesForTest composite_relative_keyframes(true);
   SetBodyInnerHTML(R"HTML(
     <div id="target" style="width: 100px; height: 200px; background: blue;
                             will-change: transform">
@@ -1596,9 +1593,6 @@ TEST_P(AnimationAnimationTestCompositing,
 // depends on width and a change to the height does not trigger a restart.
 TEST_P(AnimationAnimationTestCompositing,
        RestartCompositedAnimationOnWidthChange) {
-  // TODO(crbug.com/389359): Remove forced feature enabling once on by
-  // default.
-  ScopedCompositeRelativeKeyframesForTest composite_relative_keyframes(true);
   SetBodyInnerHTML(R"HTML(
     <div id="target" style="width: 100px; height: 200px; background: blue;
                             will-change: transform">
@@ -1639,9 +1633,6 @@ TEST_P(AnimationAnimationTestCompositing,
 // affects height and a change to the width does not trigger a restart.
 TEST_P(AnimationAnimationTestCompositing,
        RestartCompositedAnimationOnHeightChange) {
-  // TODO(crbug.com/389359): Remove forced feature enabling once on by
-  // default.
-  ScopedCompositeRelativeKeyframesForTest composite_relative_keyframes(true);
   SetBodyInnerHTML(R"HTML(
     <div id="target" style="width: 100px; height: 200px; background: blue;
                             will-change: transform">
