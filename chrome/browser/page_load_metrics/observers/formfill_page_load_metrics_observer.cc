7d9b1d12e7a5458cb566659ba18dfd1149f9d372
Johann <johannkoenig@google.com>
2022-08-25 08:48:25 +0000

    PageLoadMetrics: FormfillPageLoadMetricsObserver for Prerender2
    Ensure metrics are only recorded after Prerender pages have
    been activated.
    
    Bug: 1317494
    Change-Id: Iafae8d1c2ba274ff0ffdbed0c4dc96e2f5fbd2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3769363
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039147}

--- chrome/browser/page_load_metrics/observers/formfill_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/formfill_page_load_metrics_observer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
+#include "components/page_load_metrics/browser/page_load_metrics_observer_delegate.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 
@@ -37,34 +38,32 @@ FormfillPageLoadMetricsObserver::OnFencedFramesStart(
 }
 
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
+FormfillPageLoadMetricsObserver::OnPrerenderStart(
+    content::NavigationHandle* navigation_handle,
+    const GURL& currently_committed_url) {
+  return CONTINUE_OBSERVING;
+}
+
+page_load_metrics::PageLoadMetricsObserver::ObservePolicy
 FormfillPageLoadMetricsObserver::OnCommit(
     content::NavigationHandle* navigation_handle) {
-  HostContentSettingsMap* settings_map =
-      HostContentSettingsMapFactory::GetForProfile(Profile::FromBrowserContext(
-          GetDelegate().GetWebContents()->GetBrowserContext()));
-  DCHECK(settings_map);
-
-  const url::Origin& origin =
-      navigation_handle->GetRenderFrameHost()->GetLastCommittedOrigin();
-
-  base::Value formfill_metadata = settings_map->GetWebsiteSetting(
-      origin.GetURL(), origin.GetURL(), ContentSettingsType::FORMFILL_METADATA,
-      nullptr);
-
-  // User data field was detected on this site before.
-  if (formfill_metadata.is_dict() &&
-      formfill_metadata.FindBoolKey(kUserDataFieldFilledKey)) {
-    page_load_metrics::MetricsWebContentsObserver::RecordFeatureUsage(
-        navigation_handle->GetRenderFrameHost(),
-        blink::mojom::WebFeature::kUserDataFieldFilledPreviously);
-  }
+  if (!GetDelegate().IsInPrerenderingBeforeActivationStart())
+    MaybeRecordPriorUsageOfUserData(navigation_handle);
 
   return CONTINUE_OBSERVING;
+}
+
+void FormfillPageLoadMetricsObserver::DidActivatePrerenderedPage(
+    content::NavigationHandle* navigation_handle) {
+  MaybeRecordPriorUsageOfUserData(navigation_handle);
 }
 
 void FormfillPageLoadMetricsObserver::OnFeaturesUsageObserved(
     content::RenderFrameHost* rfh,
     const std::vector<blink::UseCounterFeature>& features) {
+  if (GetDelegate().IsInPrerenderingBeforeActivationStart())
+    return;
+
   if (user_data_field_detected_)
     return;
 
@@ -104,5 +103,30 @@ void FormfillPageLoadMetricsObserver::OnFeaturesUsageObserved(
     settings_map->SetWebsiteSettingDefaultScope(
         origin.GetURL(), origin.GetURL(),
         ContentSettingsType::FORMFILL_METADATA, std::move(formfill_metadata));
+  }
+}
+
+// Check if |kUserDataFieldFilledKey| has been previously set for the associated
+// URL.
+void FormfillPageLoadMetricsObserver::MaybeRecordPriorUsageOfUserData(
+    content::NavigationHandle* navigation_handle) {
+  HostContentSettingsMap* settings_map =
+      HostContentSettingsMapFactory::GetForProfile(Profile::FromBrowserContext(
+          GetDelegate().GetWebContents()->GetBrowserContext()));
+  DCHECK(settings_map);
+
+  const url::Origin& origin =
+      navigation_handle->GetRenderFrameHost()->GetLastCommittedOrigin();
+
+  base::Value formfill_metadata = settings_map->GetWebsiteSetting(
+      origin.GetURL(), origin.GetURL(), ContentSettingsType::FORMFILL_METADATA,
+      nullptr);
+
+  // User data field was detected on this site before.
+  if (formfill_metadata.is_dict() &&
+      formfill_metadata.FindBoolKey(kUserDataFieldFilledKey)) {
+    page_load_metrics::MetricsWebContentsObserver::RecordFeatureUsage(
+        navigation_handle->GetRenderFrameHost(),
+        blink::mojom::WebFeature::kUserDataFieldFilledPreviously);
   }
 }
