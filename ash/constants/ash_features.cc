b94b6fcbc9de4f6016c951cbabff7c5d57631b7a
whalechang <whalechang@google.com>
2022-08-26 01:13:11 +0000

    accelerator_controller: Use new way to change volume
    Use IncreaseOutputVolumeByOneStep in HandleVolumeUp event instead of increase 4% volume everytime.
    
    Use DecreaseOutputVolumeByOneStep in HandleVolumeDown event instead of decrease 4% volume everytime.
    
    Add feature flag to protect volume granularity feature
    
    BUG=b:215297837
    TEST=./out/Default/ash_unittests --gtest_filter="*AcceleratorControllerTest*"
    TEST=./out/Default/unit_tests --gtest_filter=AboutFlagsHistogramTest.CheckHistograms
    TEST=./out/Default/unit_tests --gtest_filter=AboutFlagsTest.EveryFlagHasMetadata
    
    Change-Id: I82a63b52008d914e46699e7a073388b468bc3b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3802344
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039538}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -194,6 +194,10 @@ const base::Feature kAssistPersonalInfoPhoneNumber{
 const base::Feature kAssistantNativeIcons{"AssistantNativeIcons",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
+// Enables Peripheral volume change by hardware reported steps
+const base::Feature kAudioPeripheralVolumeGranularity{
+    "AudioPeripheralVolumeGranularity", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables the Audio Settings Page in System Settings, which allows
 // audio configuration. crbug.com/1092970.
 const base::Feature kAudioSettingsPage{"AudioSettingsPage",
@@ -1987,6 +1991,10 @@ bool IsDriveFsMirroringEnabled() {
 
 bool IsEapGtcWifiAuthenticationEnabled() {
   return base::FeatureList::IsEnabled(kEapGtcWifiAuthentication);
+}
+
+bool IsAudioPeripheralVolumeGranularityEnabled() {
+  return base::FeatureList::IsEnabled(kAudioPeripheralVolumeGranularity);
 }
 
 bool IsEcheSWAEnabled() {
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2991,6 +2991,9 @@ extern const char kQuickAnswersForMoreLocalesDescription[];
 extern const char kEapGtcWifiAuthenticationName[];
 extern const char kEapGtcWifiAuthenticationDescription[];
 
+extern const char kAudioPeripheralVolumeGranularityName[];
+extern const char kAudioPeripheralVolumeGranularityDescription[];
+
 extern const char kEcheSWAName[];
 extern const char kEcheSWADescription[];
 
