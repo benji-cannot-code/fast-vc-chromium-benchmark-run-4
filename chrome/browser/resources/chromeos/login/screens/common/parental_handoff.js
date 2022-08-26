cb1f95d7282b400cb1b6df7513cc2e1c80f734ee
Brahim Chikhaoui <bchikhaoui@google.com>
2022-08-26 17:24:25 +0000

    OOBE - Polymer3 - Check-in Parental Handoff
    Check-in the modularised version of OOBE's Parental Handoff screen.
    
    Bug: 1184731, b/243383662
    Change-Id: If5647c9b49b75da696d687fcb28a08448b2d289d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3851942
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039851}

--- chrome/browser/resources/chromeos/login/screens/common/parental_handoff.js
+++ chrome/browser/resources/chromeos/login/screens/common/parental_handoff.js
@@ -7,7 +7,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for Parental Handoff screen.
  */
 
-/* #js_imports_placeholder */
+import '//resources/cr_elements/shared_vars_css.m.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+
+import {afterNextRender, dom, flush, html, mixinBehaviors, Polymer, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OobeNextButton} from '../../components/buttons/oobe_next_button.m.js';
+
 
 /**
  * @constructor
@@ -15,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {OobeI18nBehaviorInterface}
  * @implements {LoginScreenBehaviorInterface}
  */
-const ParentalHandoffElementBase = Polymer.mixinBehaviors(
+const ParentalHandoffElementBase = mixinBehaviors(
     [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    Polymer.Element);
+    PolymerElement);
 
 /**
  * @typedef {{
@@ -31,7 +44,9 @@ class ParentalHandoff extends ParentalHandoffElementBase {
     return 'parental-handoff-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
