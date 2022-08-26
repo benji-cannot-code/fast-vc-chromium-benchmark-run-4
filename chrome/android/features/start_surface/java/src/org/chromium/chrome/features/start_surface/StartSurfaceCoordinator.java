16863e72e2a092ccb3d5abef55c18eb089cd61fe
Xi Han <hanxi@google.com>
2022-08-26 16:54:13 +0000

    [Start] Set FeedSwipeRefreshLayout Gone when overview page isn't showing.
    In this CL, we set the visibility of the FeedSwipeRefreshLayout as follows:
    1. The visibility of the FeedSwipeRefreshLayout is set GONE when it is created;
    2. The FeedSwipeRefreshLayout is only added to the compositor_view_holder when an overview page is showing, and it becomes visible.
    3. The FeedSwipeRefreshLayout becomes GONE again if the overview page
    is no longer shown.
    
    Bug: 1356730
    Change-Id: I6f98379fe427b072ac548f4a42d445728b5295ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856733
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039826}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
@@ -696,10 +696,11 @@ public class StartSurfaceCoordinator implements StartSurface {
         initializeOffsetChangedListener();
         addHeaderOffsetChangeListener(mOffsetChangedListenerToGenerateScrollEvents);
 
-        mTasksSurfacePropertyModelChangeProcessor = PropertyModelChangeProcessor.create(
-                mPropertyModel,
-                new TasksSurfaceViewBinder.ViewHolder(mContainerView, mTasksSurface.getView()),
-                TasksSurfaceViewBinder::bind);
+        mTasksSurfacePropertyModelChangeProcessor =
+                PropertyModelChangeProcessor.create(mPropertyModel,
+                        new TasksSurfaceViewBinder.ViewHolder(
+                                mContainerView, mTasksSurface.getView(), mSwipeRefreshLayout),
+                        TasksSurfaceViewBinder::bind);
     }
 
     private TabSwitcher.Controller initializeSecondaryTasksSurface() {
@@ -727,7 +728,7 @@ public class StartSurfaceCoordinator implements StartSurface {
         mSecondaryTasksSurfacePropertyModelChangeProcessor =
                 PropertyModelChangeProcessor.create(mPropertyModel,
                         new TasksSurfaceViewBinder.ViewHolder(
-                                mContainerView, mSecondaryTasksSurface.getView()),
+                                mContainerView, mSecondaryTasksSurface.getView(), null),
                         SecondaryTasksSurfaceViewBinder::bind);
         if (mOnTabSelectingListener != null) {
             mSecondaryTasksSurface.setOnTabSelectingListener(mOnTabSelectingListener);
@@ -773,6 +774,7 @@ public class StartSurfaceCoordinator implements StartSurface {
         mContainerView.addView(mSwipeRefreshLayout);
         FrameLayout directChildHolder = new FrameLayout(mActivity);
         mSwipeRefreshLayout.addView(directChildHolder);
+        mSwipeRefreshLayout.setVisibility(View.GONE);
         mContainerView = directChildHolder;
     }
 
@@ -836,5 +838,10 @@ public class StartSurfaceCoordinator implements StartSurface {
     @VisibleForTesting
     boolean isSecondaryTasksSurfaceEmptyForTesting() {
         return mSecondaryTasksSurface == null;
+    }
+
+    @VisibleForTesting
+    FeedSwipeRefreshLayout getFeedSwipeRefreshLayoutForTesting() {
+        return mSwipeRefreshLayout;
     }
 }
