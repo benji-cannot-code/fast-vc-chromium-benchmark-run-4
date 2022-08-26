ddcef14a569c6826812c5fcb46c0198b43c1c931
Anton Swifton <swifton@google.com>
2022-08-26 22:13:19 +0000

    Shimless: Use localized link for the RSU page
    Using localized-link makes the link ChromeVox compliant.
    
    https://screenshot.googleplex.com/BhQHmtke3CsubnZ
    
    Bug: 1198187
    Test: browser_tests
    Change-Id: Iab2ba00ee535567eea3635ee2a6d52197c12a30e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858030
    Commit-Queue: Anton Swifton <swifton@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039995}

--- chrome/test/data/webui/chromeos/shimless_rma/onboarding_enter_rsu_wp_disable_code_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/onboarding_enter_rsu_wp_disable_code_page_test.js
@@ -123,7 +123,9 @@ export function onboardingEnterRsuWpDisableCodePageTest() {
   test('EnterRsuWpDisableCodePageOpenChallengeDialog', async () => {
     await initializeEnterRsuWpDisableCodePage('', '');
 
-    component.shadowRoot.querySelector('#rsuCodeDialogLink').click();
+    component.shadowRoot.querySelector('#rsuCodeDialogLink')
+        .shadowRoot.querySelector('a')
+        .click();
     assertTrue(component.shadowRoot.querySelector('#rsuChallengeDialog').open);
   });
 
@@ -140,7 +142,9 @@ export function onboardingEnterRsuWpDisableCodePageTest() {
     await initializeEnterRsuWpDisableCodePage('', '');
 
     component.allButtonsDisabled = true;
-    component.shadowRoot.querySelector('#rsuCodeDialogLink').click();
+    component.shadowRoot.querySelector('#rsuCodeDialogLink')
+        .shadowRoot.querySelector('a')
+        .click();
     assertFalse(component.shadowRoot.querySelector('#rsuChallengeDialog').open);
   });
 
