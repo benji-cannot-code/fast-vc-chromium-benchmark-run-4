9ebc99cd6aadd5e3cb6a7942a3d75f61a9614128
Maksim Moskvitin <mmoskvitin@google.com>
2022-08-26 12:57:46 +0000

    [Passwords] Fix DCHECK failure caused by ActOnPasswordStoreChanges()
    ModelErrors in SyncMetadataStoreChangeList are supposed to be taken and
    reported to the processor when occur. ActOnPasswordsStoreChanges()
    didn't do this, causing DCHECK failure in SyncMetadataStoreChangeList
    dtor. This CL fixes that.
    
    Bug: 1356729
    Change-Id: Ie6a0b0a3bb6cb5094698b1c139ba63eea926d4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856530
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039722}

--- components/password_manager/core/browser/sync/password_sync_bridge.cc
+++ components/password_manager/core/browser/sync/password_sync_bridge.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/in_memory_metadata_change_list.h"
 #include "components/sync/model/metadata_batch.h"
 #include "components/sync/model/metadata_change_list.h"
+#include "components/sync/model/model_error.h"
 #include "components/sync/model/model_type_change_processor.h"
 #include "components/sync/model/mutable_data_batch.h"
 #include "components/sync/model/sync_metadata_store_change_list.h"
@@ -343,6 +344,11 @@ void PasswordSyncBridge::ActOnPasswordStoreChanges(
         break;
       }
     }
+  }
+
+  if (absl::optional<syncer::ModelError> error =
+          metadata_change_list.TakeError()) {
+    change_processor()->ReportError(*error);
   }
 }
 
