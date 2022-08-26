d2b1a37e799e4002ec25d5811aca72e04c637091
Danila Kuzmin <dkuzmin@google.com>
2022-08-26 19:15:57 +0000

    [OOBE][Polymer3]: Check-in managed-terms-of-service element
    Check-in the modularised version of TermsOfService screen.
    
    Bug: b:243383406
    Change-Id: Ie96c7c8f6d1a4756ce3061e755fd4f00893886db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857313
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039912}

--- chrome/browser/resources/chromeos/login/screens/common/managed_terms_of_service.js
+++ chrome/browser/resources/chromeos/login/screens/common/managed_terms_of_service.js
@@ -8,7 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * screen.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/buttons/oobe_back_button.m.js';
+import '../../components/buttons/oobe_text_button.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+import '../../components/dialogs/oobe_loading_dialog.m.js';
+
+import {I18nBehavior} from '//resources/js/i18n_behavior.m.js';
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+
 
 // Enum that describes the current state of the Terms Of Service screen
 const ManagedTermsState = {
@@ -24,9 +40,14 @@ const ManagedTermsState = {
  * @implements {MultiStepBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
  */
- const ManagedTermsOfServiceBase = Polymer.mixinBehaviors([OobeI18nBehavior,
-  OobeDialogHostBehavior, LoginScreenBehavior, MultiStepBehavior],
-  Polymer.Element);
+const ManagedTermsOfServiceBase = mixinBehaviors(
+    [
+      OobeI18nBehavior,
+      OobeDialogHostBehavior,
+      LoginScreenBehavior,
+      MultiStepBehavior,
+    ],
+    PolymerElement);
 
 /**
  * @typedef {{
@@ -45,7 +66,9 @@ class ManagedTermsOfService extends ManagedTermsOfServiceBase {
     return 'managed-terms-of-service-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
