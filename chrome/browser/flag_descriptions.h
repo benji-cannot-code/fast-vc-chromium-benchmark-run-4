ed2c9b38460819733860b402f424af1e58a38a4c
Ender <ender@google.com>
2022-08-26 20:34:33 +0000

    Add explicit chrome://flags entry for MV Tiles Dynamic Spacing
    Change-Id: Ie228c2600d06aad730edfc2b5dbc1098da7dc26c
    Fixed: 1346120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858607
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039959}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1106,6 +1106,9 @@ extern const char kOmniboxModernizeVisualUpdateDescription[];
 extern const char kOmniboxMostVisitedTilesName[];
 extern const char kOmniboxMostVisitedTilesDescription[];
 
+extern const char kOmniboxMostVisitedTilesDynamicSpacingName[];
+extern const char kOmniboxMostVisitedTilesDynamicSpacingDescription[];
+
 extern const char kOmniboxMostVisitedTilesTitleWrapAroundName[];
 extern const char kOmniboxMostVisitedTilesTitleWrapAroundDescription[];
 
