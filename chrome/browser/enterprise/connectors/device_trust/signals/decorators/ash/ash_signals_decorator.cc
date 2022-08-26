e536e81a50518d5257643cadfec28c90da06c5e7
Martin Rodriguez <rodmartin@google.com>
2022-08-26 15:10:38 +0000

    crOS: Add AllowScreenLock signal to ChromeOS ash
    Add AllowScreenLock signal to be shared in the Device Trust handshake
    in-session.
    
    Change-Id: I7d6618af45f7375497634545435055a5cad7acf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845670
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039789}

--- chrome/browser/enterprise/connectors/device_trust/signals/decorators/ash/ash_signals_decorator.cc
+++ chrome/browser/enterprise/connectors/device_trust/signals/decorators/ash/ash_signals_decorator.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/device_trust/signals/decorators/ash/ash_signals_decorator.h"
 
+#include "ash/constants/ash_pref_names.h"
 #include "base/check.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -16,9 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/connectors/device_trust/signals/decorators/common/metrics_utils.h"
 #include "chrome/browser/enterprise/connectors/device_trust/signals/decorators/common/signals_decorator.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/components/network/device_state.h"
 #include "components/device_signals/core/common/signals_constants.h"
 #include "components/policy/proto/device_management_backend.pb.h"
+#include "components/prefs/pref_service.h"
 
 namespace enterprise_connectors {
 
@@ -68,6 +71,8 @@ void AshSignalsDecorator::Decorate(base::Value::Dict& signals,
               browser_policy_connector_->GetObfuscatedCustomerID());
   signals.Set(device_signals::names::kEnrollmentDomain,
               browser_policy_connector_->GetEnterpriseDomainManager());
+  signals.Set(device_signals::names::kAllowScreenLock,
+              profile_->GetPrefs()->GetBoolean(ash::prefs::kAllowScreenLock));
 
   const ash::DeviceState* device_state =
       GetCurrentlyActiveDeviceState(profile_);
