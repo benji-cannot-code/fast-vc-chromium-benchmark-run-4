f2fed2359d9a2aed55c1389252a47dac270d36a9
David Benjamin <davidben@chromium.org>
2022-08-26 02:33:35 +0000

    Improve TransportConnectJob logging
    The NetLogs in https://crbug.com/1350312 were a bit more tedious to look
    through than they needed to be. This is because
    https://crrev.com/c/3617441 exacerbated some nuisances in CONNECT_JOB
    and SOCKET logging.
    
    This CL does the following:
    
    1. It restores the link (as documented) from CONNECT_JOB to SOCKET on
       CONNECT_JOB_SET_SOCKET. This was dropped in
       https://chromium-review.googlesource.com/c/chromium/src/+/1478259/,
       I'm guessing because nested ConnectJobs share an event source now and
       we get multiple CONNECT_JOB_SET_SOCKET calls. But removing the link
       means it's impossible to go from CONNECT_JOB to SOCKET at all.
    
    2. It adds an event when TransportConnectJob's IPv6 timer fires.
    
    3. When TransportConnectJob starts a connection attempt, it adds a link
       to the SOCKET. Ideally the event would begin and end with the
       socket's Connect() call, but that'll return weird whenever we launch
       sockets in parallel, so I've left them as singleton events for now.
    
    Bug: 669700
    Change-Id: I57f34071e48d016adf732780f24e769eb9fde9c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857083
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039555}

--- net/socket/transport_connect_job.cc
+++ net/socket/transport_connect_job.cc
@@ -10,17 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check_op.h"
-#include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
-#include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
-#include "base/values.h"
 #include "net/base/features.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/ip_endpoint.h"
@@ -29,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/host_resolver_results.h"
 #include "net/dns/public/secure_dns_policy.h"
 #include "net/log/net_log_event_type.h"
-#include "net/log/net_log_source.h"
 #include "net/socket/socket_tag.h"
 #include "net/socket/transport_connect_sub_job.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
@@ -492,6 +488,7 @@ void TransportConnectJob::OnSubJobComplete(int result,
 
 void TransportConnectJob::StartIPv4JobAsync() {
   DCHECK(ipv4_job_);
+  net_log().AddEvent(NetLogEventType::TRANSPORT_CONNECT_JOB_IPV6_FALLBACK);
   int result = ipv4_job_->Start();
   if (result != ERR_IO_PENDING)
     OnSubJobComplete(result, ipv4_job_.get());
