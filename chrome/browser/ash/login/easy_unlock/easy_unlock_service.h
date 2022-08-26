b3ea36571cd4c7ad0005b57aa2c639c806ab6bb5
Roland Bock <rbock@google.com>
2022-08-26 14:05:46 +0000

    Refactor ash/login to use PrefService::GetValue*
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    This CL also replaces a number of deprecated base::ListValue instances
    with base::Value::List as a nice side effect.
    
    Bug: 1341998, 1341996, 1187061
    Test: Existing unit tests
    Change-Id: I9f8bb437ef27771b016ad8c810266aa08ea3f4c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798165
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039754}

--- chrome/browser/ash/login/easy_unlock/easy_unlock_service.h
+++ chrome/browser/ash/login/easy_unlock/easy_unlock_service.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AccountId;
 
-namespace base {
-class ListValue;
-}  // namespace base
-
 namespace user_manager {
 class User;
 }
@@ -88,7 +84,7 @@ class EasyUnlockService : public KeyedService,
   // Retrieve the stored remote devices list:
   //   * If in regular context, device list is retrieved from prefs.
   //   * If in sign-in context, device list is retrieved from TPM.
-  virtual const base::ListValue* GetRemoteDevices() const = 0;
+  virtual const base::Value::List* GetRemoteDevices() const = 0;
 
   // Gets the challenge bytes for the user currently associated with the
   // service.
