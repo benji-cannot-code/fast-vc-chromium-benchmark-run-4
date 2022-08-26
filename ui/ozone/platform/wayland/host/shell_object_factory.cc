bf348a986fcc5139422ee402b2c8ed533119661d
Alexander Dunaev <adunaev@igalia.com>
2022-08-26 10:30:31 +0000

    [linux/wayland] Added the Linux.Wayland.BaseShellUsed UMA.
    We need to decide when the XDG shell V6 can be deprecated.  For that, a
    new UMA is introduced to gather data about the actually used shells.
    
    Bug: 1355936
    Change-Id: Ibd7ea41687e306bba5b55c32f737827882acf38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855812
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039682}

--- ui/ozone/platform/wayland/host/shell_object_factory.cc
+++ ui/ozone/platform/wayland/host/shell_object_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/shell_object_factory.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/xdg_popup_wrapper_impl.h"
 #include "ui/ozone/platform/wayland/host/xdg_surface_wrapper_impl.h"
@@ -16,6 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+namespace {
+
+void ReportBaseShellUMA(UMALinuxWaylandShell shell) {
+  static bool used_shell_reported = false;
+  if (used_shell_reported)
+    return;
+  base::UmaHistogramEnumeration("Linux.Wayland.BaseShellUsed", shell);
+  used_shell_reported = true;
+}
+
+}  // namespace
+
 ShellObjectFactory::ShellObjectFactory() = default;
 ShellObjectFactory::~ShellObjectFactory() = default;
 
@@ -23,6 +36,8 @@ std::unique_ptr<ShellToplevelWrapper>
 ShellObjectFactory::CreateShellToplevelWrapper(WaylandConnection* connection,
                                                WaylandWindow* wayland_window) {
   if (connection->shell()) {
+    ReportBaseShellUMA(UMALinuxWaylandShell::kXdgWmBase);
+
     auto surface =
         std::make_unique<XDGSurfaceWrapperImpl>(wayland_window, connection);
     if (!surface->Initialize())
@@ -32,6 +47,8 @@ ShellObjectFactory::CreateShellToplevelWrapper(WaylandConnection* connection,
         std::move(surface), wayland_window, connection);
     return toplevel->Initialize() ? std::move(toplevel) : nullptr;
   } else if (connection->shell_v6()) {
+    ReportBaseShellUMA(UMALinuxWaylandShell::kXdgShellV6);
+
     auto surface =
         std::make_unique<ZXDGSurfaceV6WrapperImpl>(wayland_window, connection);
     if (!surface->Initialize())
