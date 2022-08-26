adc67fcb04f2f27295e4db79ba541666b5bab452
xiangdong kong <xiangdongkong@google.com>
2022-08-26 18:46:37 +0000

    Update content tracing manager to zip trace data async
    The current code crashes since the ZipString function used is a blocking
    call.
    
     --gtest_filter=ContentTracingManagerBrowserTest.*
    
    Bug: b:243673662
    Test: components_browsertests \
    Change-Id: I1fe37c8bc1d17fb84c78248df357828dc9b1e31c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854710
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039897}

--- components/feedback/content/content_tracing_manager.h
+++ components/feedback/content/content_tracing_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/memory/ref_counted_memory.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 
@@ -51,6 +52,7 @@ class ContentTracingManager : public TracingManager {
 
   void StartTracing();
   void OnTraceDataCollected(std::unique_ptr<std::string> data);
+  void OnTraceDataCompressed(scoped_refptr<base::RefCountedString> data);
 
   // ID of the trace that is being collected.
   int current_trace_id_ = 0;
