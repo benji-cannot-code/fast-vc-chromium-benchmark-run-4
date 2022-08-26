5dbd1bc9a3c9694e3dd80ff429e6075f7a32dfd0
Yaron Friedman <yfriedman@chromium.org>
2022-08-26 21:46:07 +0000

    Clean up Lollipop code in AsyncInitActivity.
    Discovered while scanning ViewTreeObservers in the codebase.
    
    Change-Id: I4e487aa2218c19e07082198b246371af0c587cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857609
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039985}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -19,8 +19,6 @@ import android.provider.Settings.SettingNotFoundException;
 import android.view.Display;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.view.ViewTreeObserver.OnPreDrawListener;
 import android.view.WindowManager;
 
 import androidx.annotation.CallSuper;
@@ -188,7 +186,6 @@ public abstract class AsyncInitializationActivity
         if (!enableInstantStart) {
             triggerLayoutInflation();
         }
-        if (mLaunchBehindWorkaround != null) mLaunchBehindWorkaround.onSetContentView();
     }
 
     /** Controls the parameter of {@link NativeInitializationController#startBackgroundTasks}.*/
@@ -513,7 +510,6 @@ public abstract class AsyncInitializationActivity
         mFirstResumePending = false;
 
         mNativeInitializationController.onResume();
-        if (mLaunchBehindWorkaround != null) mLaunchBehindWorkaround.onResume();
     }
 
     @CallSuper
@@ -521,7 +517,6 @@ public abstract class AsyncInitializationActivity
     public void onPause() {
         mNativeInitializationController.onPause();
         super.onPause();
-        if (mLaunchBehindWorkaround != null) mLaunchBehindWorkaround.onPause();
     }
 
     @CallSuper
@@ -859,65 +854,6 @@ public abstract class AsyncInitializationActivity
      */
     public MultiWindowModeStateDispatcher getMultiWindowModeStateDispatcher() {
         return mMultiWindowModeStateDispatcher;
-    }
-
-    /**
-     * Lollipop (pre-MR1) makeTaskLaunchBehind() workaround.
-     *
-     * Our activity's surface is destroyed at the end of the new activity animation
-     * when ActivityOptions.makeTaskLaunchBehind() is used, which causes a crash.
-     * Making everything invisible when paused prevents the crash, since view changes
-     * will not trigger draws to the missing surface. However, we need to wait until
-     * after the first draw to make everything invisible, as the activity launch
-     * animation needs a full frame (or it will delay the animation excessively).
-     */
-    private final LaunchBehindWorkaround mLaunchBehindWorkaround =
-            (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP)
-                    ? new LaunchBehindWorkaround()
-                    : null;
-
-    private class LaunchBehindWorkaround {
-        private boolean mPaused;
-
-        private View getDecorView() {
-            return getWindow().getDecorView();
-        }
-
-        private ViewTreeObserver getViewTreeObserver() {
-            return getDecorView().getViewTreeObserver();
-        }
-
-        private void onPause() {
-            mPaused = true;
-        }
-
-        public void onResume() {
-            mPaused = false;
-            getDecorView().setVisibility(View.VISIBLE);
-        }
-
-        public void onSetContentView() {
-            getViewTreeObserver().addOnPreDrawListener(mPreDrawListener);
-        }
-
-        // Note, we probably want onDrawListener here, but it isn't being called
-        // when I add this to the decorView. However, it should be the same for
-        // this purpose as long as no other pre-draw listener returns false.
-        private final OnPreDrawListener mPreDrawListener = new OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (mPaused) {
-                            getDecorView().setVisibility(View.GONE);
-                        }
-                        getViewTreeObserver().removeOnPreDrawListener(mPreDrawListener);
-                    }
-                });
-                return true;
-            }
-        };
     }
 
     @Override
