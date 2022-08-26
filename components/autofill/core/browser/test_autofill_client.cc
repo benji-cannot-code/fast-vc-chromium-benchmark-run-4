d6084c5dbb655646ce6404ee12c449798a42db7c
Dominic Battre <battre@chromium.org>
2022-08-26 14:01:06 +0000

    Enable logging of autofill-internals for autofill unittests
    By passing --show-autofill-internals to unittests that rely on the
    TestAutofillClient it is now possible to dump the contents of
    chrome://autofill-internals to LOG(INFO).
    
    Bug: 928595
    Change-Id: If2f76d7a68441b9dc1e4e6928716ee5b97984ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3853651
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039750}

--- components/autofill/core/browser/test_autofill_client.cc
+++ components/autofill/core/browser/test_autofill_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/test_autofill_client.h"
 
+#include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
@@ -24,7 +25,12 @@ TestAutofillClient::TestAutofillClient(
     : test_personal_data_manager_(
           pdm ? std::move(pdm) : std::make_unique<TestPersonalDataManager>()),
       form_origin_(GURL("https://example.test")),
-      last_committed_url_(GURL("https://example.test")) {}
+      last_committed_url_(GURL("https://example.test")),
+      log_manager_(LogManager::Create(&log_router_, base::NullCallback())) {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch("show-autofill-internals"))
+    scoped_logging_subscription_.Observe(&log_router_);
+}
 
 TestAutofillClient::~TestAutofillClient() = default;
 
@@ -338,6 +344,10 @@ bool TestAutofillClient::AreServerCardsSupported() const {
 void TestAutofillClient::ExecuteCommand(int id) {}
 
 void TestAutofillClient::OpenPromoCodeOfferDetailsURL(const GURL& url) {}
+
+LogManager* TestAutofillClient::GetLogManager() const {
+  return log_manager_.get();
+}
 
 void TestAutofillClient::LoadRiskData(
     base::OnceCallback<void(const std::string&)> callback) {
