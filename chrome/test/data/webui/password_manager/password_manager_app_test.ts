7b379bb674775543ea5d0706af49f7a18e9cb2bd
Viktor Semeniuk <vsemeniuk@google.com>
2022-08-26 13:15:44 +0000

    [Password Manager] Tests setup for the new Password Manager page
    Bug: 1350947
    Change-Id: I899f9319ca7dc171fba0d4dded0289f9e8221c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3853337
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039729}

---
+++ chrome/test/data/webui/password_manager/password_manager_app_test.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://password-manager/password_manager.js';
+
+import {PasswordManagerAppElement} from 'chrome://password-manager/password_manager.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {isVisible} from 'chrome://webui-test/test_util.js';
+
+suite('PasswordManagerAppTest', function() {
+  let app: PasswordManagerAppElement;
+
+  setup(function() {
+    document.body.innerHTML = '';
+    app = document.createElement('password-manager-app');
+    document.body.appendChild(app);
+  });
+
+  test('check layout', function() {
+    assertTrue(isVisible(app));
+    assertTrue(isVisible(app.$.sidebar));
+    assertTrue(isVisible(app.$.toolbar));
+  });
+});
