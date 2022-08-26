947658832b756f54c02c4e86de9eb099057a2290
Li Lin <llin@chromium.org>
2022-08-26 01:59:35 +0000

    Return error response body in XHR sender response if available.
    Drive request will failed with response body that contains details
    information to help handling the error.
    
    Follow up CL on Google3 will parse and handle the error response body.
    
    Bug: b/243187246
    Change-Id: I8cc04922352f903c1430da4cfa20c842a2847220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843077
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039547}

--- ash/webui/projector_app/test/projector_message_handler_unittest.cc
+++ ash/webui/projector_app/test/projector_message_handler_unittest.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
 #include "content/public/test/test_web_ui.h"
+#include "net/http/http_status_code.h"
+#include "services/network/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -270,7 +272,7 @@ TEST_F(ProjectorMessageHandlerUnitTest, SendXhr) {
   ASSERT_TRUE(call_data.arg3()->is_dict());
 
   // Verify that it is success.
-  EXPECT_TRUE(call_data.arg3()->FindBoolPath(kXhrResponseSuccessPath));
+  EXPECT_TRUE(*call_data.arg3()->FindBoolPath(kXhrResponseSuccessPath));
 
   // Verify the response.
   const std::string* response =
@@ -281,6 +283,57 @@ TEST_F(ProjectorMessageHandlerUnitTest, SendXhr) {
   const std::string* error =
       call_data.arg3()->FindStringPath(kXhrResponseErrorPath);
   EXPECT_TRUE(error->empty());
+}
+
+TEST_F(ProjectorMessageHandlerUnitTest, SendXhrFailed) {
+  const std::string& test_error_response_body = "error";
+
+  base::Value::List list_args;
+  list_args.Append(kSendXhrCallback);
+  base::ListValue args;
+  args.Append(kTestXhrUrl);
+  args.Append(kTestXhrMethod);
+  args.Append(kTestXhrRequestBody);
+  // Add useCredentials.
+  args.Append(true);
+  // Add additional headers.
+  base::Value::Dict dict;
+  dict.Set(kTestXhrHeaderKey, kTestXhrHeaderValue);
+  args.Append(std::move(dict));
+  list_args.Append(std::move(args));
+
+  mock_app_client().test_url_loader_factory().AddResponse(
+      /*url=*/kTestXhrUrl,
+      /*content=*/test_error_response_body,
+      /*status=*/net::HttpStatusCode::HTTP_NOT_FOUND);
+
+  base::RunLoop run_loop;
+  message_handler()->SetXhrRequestRunLoopQuitClosure(run_loop.QuitClosure());
+  web_ui().HandleReceivedMessage("sendXhr", list_args);
+  run_loop.Run();
+
+  EXPECT_EQ(web_ui().call_data().size(), 1u);
+
+  const content::TestWebUI::CallData& call_data = FetchCallData(0);
+  EXPECT_EQ(call_data.function_name(), kWebUIResponse);
+  EXPECT_EQ(call_data.arg1()->GetString(), kSendXhrCallback);
+
+  // Whether the callback was rejected.
+  EXPECT_TRUE(call_data.arg2()->GetBool());
+  ASSERT_TRUE(call_data.arg3()->is_dict());
+
+  // Verify that request failed.
+  EXPECT_FALSE(*call_data.arg3()->FindBoolPath(kXhrResponseSuccessPath));
+
+  // Verify the response.
+  const std::string* response =
+      call_data.arg3()->FindStringPath(kXhrResponseStringPath);
+  EXPECT_EQ(test_error_response_body, *response);
+
+  // Verify error is empty.
+  const std::string* error =
+      call_data.arg3()->FindStringPath(kXhrResponseErrorPath);
+  EXPECT_EQ("XHR_FETCH_FAILURE", *error);
 }
 
 TEST_F(ProjectorMessageHandlerUnitTest, SendXhrWithUnSupportedUrl) {
@@ -321,7 +374,7 @@ TEST_F(ProjectorMessageHandlerUnitTest, SendXhrWithUnSupportedUrl) {
       call_data.arg3()->FindStringPath(kXhrResponseStringPath);
   EXPECT_TRUE(response->empty());
 
-  // Verify error is empty.
+  // Verify error is UNSUPPORTED_URL.
   const std::string* error =
       call_data.arg3()->FindStringPath(kXhrResponseErrorPath);
   EXPECT_EQ("UNSUPPORTED_URL", *error);
