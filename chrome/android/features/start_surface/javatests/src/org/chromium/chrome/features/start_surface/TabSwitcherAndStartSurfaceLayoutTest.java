b504ae8f25b0b3a0f1a4c4e0b3fa1a49c3499f30
Xi Han <hanxi@google.com>
2022-08-26 20:18:55 +0000

    [Start] Fix tab switcher tests when Start is enabled.
    When Start surface is disabled, the parent view of the GTS is
    compositor_view_holder, while becomes tasks_surface_body when Start
    surface is enabled. Update TabUiTestHelper#getTabSwitcherParentId() and
    add StartSurfaceTestUtils#getTabSwitcherParentId() to allow GTS related
    instrument tests can be run in both Start surface enabled/disabled cases.
    
    Bug: 1356180
    Change-Id: Icb9cbbf62595a8e13e6aa47cf414fd0952ecfd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854008
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039953}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/TabSwitcherAndStartSurfaceLayoutTest.java
@@ -145,6 +145,8 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 
 // clang-format off
 /** Tests for the {@link TabSwitcherAndStartSurfaceLayout} */
@@ -197,7 +199,7 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws ExecutionException {
         AccessibilityChecks.enable();
         mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
         // After setUp, Chrome is launched and has one NTP.
@@ -209,8 +211,7 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
         mUrl = mTestServer.getURL("/chrome/test/data/android/navigate/simple.html");
         mRepeat = 1;
 
-        mTabListDelegate = mTabSwitcherAndStartSurfaceLayout.getStartSurfaceForTesting()
-                                   .getGridTabListDelegate();
+        mTabListDelegate = getTabListDelegateFromUIThread();
         mTabListDelegate.setBitmapCallbackForTesting(mBitmapListener);
         assertEquals(0, mTabListDelegate.getBitmapFetchCountForTesting());
 
@@ -990,7 +991,7 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
         CriteriaHelper.pollUiThread(TabSwitcherCoordinator::hasAppendedMessagesForTesting);
         onView(withId(R.id.tab_grid_message_item)).check(matches(isDisplayed()));
 
-        closeFirstTabInTabSwitcher();
+        closeFirstTabInTabSwitcher(mActivityTestRule.getActivity());
 
         CriteriaHelper.pollUiThread(
                 () -> !TabSuggestionMessageService.isSuggestionAvailableForTesting());
@@ -1735,7 +1736,7 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
         enterTabSwitcher(cta);
         verifyTabSwitcherCardCount(cta, 3);
         assertNull(snackbarManager.getCurrentSnackbarForTesting());
-        closeFirstTabInTabSwitcher();
+        closeFirstTabInTabSwitcher(cta);
         assertTrue(snackbarManager.getCurrentSnackbarForTesting().getController()
                            instanceof UndoBarController);
         verifyTabSwitcherCardCount(cta, 2);
@@ -1758,7 +1759,7 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
 
         // Verify TabModelObserver is correctly setup by checking if tab switcher changes with tab
         // closure.
-        closeFirstTabInTabSwitcher();
+        closeFirstTabInTabSwitcher(cta);
         verifyTabSwitcherCardCount(cta, 0);
 
         // Verify TabGroupModelFilter is correctly setup by checking if tab switcher changes with
@@ -1785,12 +1786,22 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
         assertNull(snackbarManager.getCurrentSnackbarForTesting());
 
         // Verify close this tab group and undo in tab switcher.
-        closeFirstTabInTabSwitcher();
+        closeFirstTabInTabSwitcher(cta);
         assertTrue(snackbarManager.getCurrentSnackbarForTesting().getController()
                            instanceof UndoBarController);
         verifyTabSwitcherCardCount(cta, 0);
         CriteriaHelper.pollInstrumentationThread(TabUiTestHelper::verifyUndoBarShowingAndClickUndo);
         verifyTabSwitcherCardCount(cta, 1);
+    }
+
+    private TabSwitcher.TabListDelegate getTabListDelegateFromUIThread() {
+        AtomicReference<TabSwitcher.TabListDelegate> tabListDelegate = new AtomicReference<>();
+        TestThreadUtils.runOnUiThreadBlocking(
+                ()
+                        -> tabListDelegate.set(
+                                mTabSwitcherAndStartSurfaceLayout.getStartSurfaceForTesting()
+                                        .getGridTabListDelegate()));
+        return tabListDelegate.get();
     }
 
     private void enterTabGroupManualSelection(ChromeTabbedActivity cta) {
@@ -1995,6 +2006,8 @@ public class TabSwitcherAndStartSurfaceLayoutTest {
     }
 
     private Matcher<View> tabSwitcherViewMatcher() {
-        return allOf(withParent(withId(R.id.compositor_view_holder)), withId(R.id.tab_list_view));
+        return allOf(withParent(withId(TabUiTestHelper.getTabSwitcherParentId(
+                             mActivityTestRule.getActivity()))),
+                withId(R.id.tab_list_view));
     }
 }
