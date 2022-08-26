00b0d011ddbe911073cb7bf28f20a98a9cc32e64
Gauthier Ambard <gambard@chromium.org>
2022-08-26 14:11:49 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/promos_manager
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=bwwilliams@google.com
    
    Bug: 1311955
    Change-Id: I5ed8c4f50d71506a9b27097ff98b765a6c42d8de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858626
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1039758}

--- ios/chrome/browser/promos_manager/impression_limit.h
+++ ios/chrome/browser/promos_manager/impression_limit.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface ImpressionLimit : NSObject
 
-// The max number of impressions allowed within |numDays|.
+// The max number of impressions allowed within `numDays`.
 @property(nonatomic, readonly) NSInteger numImpressions;
 
-// The number of days the limit |numImpressions| is restricted to.
+// The number of days the limit `numImpressions` is restricted to.
 @property(nonatomic, readonly) NSInteger numDays;
 
 // Designated initializer. Initializes with an impression count limit for a
