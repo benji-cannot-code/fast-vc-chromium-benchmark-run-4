15f7b012596326a8fa729186565b9ab3f0978060
Andrey Kosyakov <caseq@chromium.org>
2022-08-27 01:17:21 +0000

    Only assign devtools_request_id when at least one NetworkHandler is enabled
    Many network events relevant for DevTools are reported from network service
    conditionally on devtools_request_id being present in ResourceRequest, so as
    to minimize overhead when DevTools protocol clients are not attached.
    Therefore, devtools_request_id itself should only be set when at least one
    of the network agents is enabled.
    
    Change-Id: I0e7a1a656f14a42ec8367a1d3157097ef367b63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856615
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040050}

--- content/browser/devtools/devtools_instrumentation.h
+++ content/browser/devtools/devtools_instrumentation.h
@@ -329,6 +329,13 @@ void OnServiceWorkerMainScriptRequestWillBeSent(
     int64_t version_id,
     network::ResourceRequest& request);
 
+// Fires `Network.onRequestWillBeSent` event for a dedicated worker and shared
+// worker main script. Used for PlzDedicatedWorker/PlzSharedWorker.
+void OnWorkerMainScriptRequestWillBeSent(
+    FrameTreeNode* ftn,
+    const base::UnguessableToken& worker_token,
+    network::ResourceRequest& request);
+
 // Fires `Network.onLoadingFailed` event for a dedicated worker main script.
 // Used for PlzDedicatedWorker.
 void OnWorkerMainScriptLoadingFailed(
@@ -344,13 +351,6 @@ void OnWorkerMainScriptLoadingFinished(
     FrameTreeNode* ftn,
     const base::UnguessableToken& worker_token,
     const network::URLLoaderCompletionStatus& status);
-
-// Fires `Network.onRequestWillBeSent` event for a dedicated worker and shared
-// worker main script. Used for PlzDedicatedWorker/PlzSharedWorker.
-void OnWorkerMainScriptRequestWillBeSent(
-    FrameTreeNode* ftn,
-    const base::UnguessableToken& worker_token,
-    const network::ResourceRequest& request);
 
 // Adds a message from a worklet to the devtools console. This is specific to
 // FLEDGE auction worklet and shared storage worklet where the message may
