11f2730c65e215dfc0c018a8123283888231b2af
David Baron <dbaron@chromium.org>
2022-08-27 01:01:10 +0000

    Revert "lacros: replicate USE_LIBV4L2 ash buildflag in lacros"
    This reverts commit d779e562ed66c33d21694e9ef1adb19d52484f19.
    
    Reason for revert: compilation failure seen in https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-kevin-rel/44412/overview :
    
    media/gpu/v4l2/v4l2_device.cc:144:10: error: 'return' will never be executed [-Werror,-Wunreachable-code-return]
    return false;
    
    Original change's description:
    > lacros: replicate USE_LIBV4L2 ash buildflag in lacros
    >
    > The USE_LIBV4L2 buildflag is not available in lacros. In lacros, this CL
    > checks if the library file is in the file system to replicate the
    > USE_LIBV4L2 buildflag behaviour in ash. This works because the library
    > will get installed only if the USE_LIBV4L2 buildflag is present in ash.
    >
    > BUG=b:240881905
    > TEST=manual test on trogdor and kevin
    >
    > Change-Id: Id322e386a462cf18802e112635108a902f3fd27c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814276
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    > Commit-Queue: Pilar Molina Lopez <pmolinalopez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1040020}
    
    Bug: b:240881905
    Change-Id: I390c02cefeb64dc4a081810efbd272614e696ce3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860155
    Commit-Queue: David Baron <dbaron@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040039}

--- media/gpu/v4l2/v4l2_device.cc
+++ media/gpu/v4l2/v4l2_device.cc
@@ -5,12 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/v4l2_device.h"
 
-#include <errno.h>
 #include <fcntl.h>
-#include <linux/media.h>
 #include <poll.h>
+#include <linux/media.h>
 #include <sys/ioctl.h>
-#include <unistd.h>
 
 #include <algorithm>
 #include <set>
@@ -131,18 +129,6 @@ void V4L2ProcessingTrace(const struct v4l2_buffer* v4l2_buffer, bool start) {
                                     TRACE_ID_LOCAL(timestamp), "timestamp",
                                     timestamp);
   }
-}
-
-bool LibV4L2Exists() {
-#if BUILDFLAG(USE_LIBV4L2)
-  return true;
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  if (access(V4L2Device::kLibV4l2Path, F_OK) == 0)
-    return true;
-  PLOG_IF(FATAL, errno != ENOENT)
-      << "access() failed for a reason other than ENOENT";
-#endif
-  return false;
 }
 
 }  // namespace
@@ -1976,12 +1962,6 @@ size_t V4L2Device::GetNumPlanesOfV4L2PixFmt(uint32_t pix_fmt) {
     return VideoFrame::NumPlanes(fourcc->ToVideoPixelFormat());
   }
   return 1u;
-}
-
-// static
-bool V4L2Device::UseLibV4L2() {
-  static const bool use_libv4l2 = LibV4L2Exists();
-  return use_libv4l2;
 }
 
 void V4L2Device::GetSupportedResolution(uint32_t pixelformat,
