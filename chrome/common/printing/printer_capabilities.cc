08b3ac702a80ea30d5ff17676c56020b815e9f98
Daniel Cheng <dcheng@chromium.org>
2022-08-28 04:26:16 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //chrome/common/printing.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=awscreen@chromium.org
    
    Bug: 1356184
    Change-Id: I3a42cff9a80bb68aea0bb494b00a4870e0bc1402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860376
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040166}

--- chrome/common/printing/printer_capabilities.cc
+++ chrome/common/printing/printer_capabilities.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
 #include "base/threading/scoped_blocking_call.h"
+#include "base/types/optional_util.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
@@ -212,7 +213,7 @@ base::Value::Dict GetSettingsOnBlockingTaskRunner(
 
   return AssemblePrinterSettings(device_name, basic_info, user_defined_papers,
                                  has_secure_protocol,
-                                 base::OptionalOrNullptr(caps));
+                                 base::OptionalToPtr(caps));
 }
 
 }  // namespace printing
