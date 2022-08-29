22e2eb73fcacc7ee511db1652a6ce4a5b78e0db6
François Doray <fdoray@chromium.org>
2022-08-29 19:20:42 +0000

    [sheriff] Disable PolicyUITest.WritePoliciesToJSONFile on Linux CFI.
    Bug: 1338642
    Change-Id: Iaa867b9defcfc0c724fd27bfa9346ad51eb1bee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861699
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040551}

--- chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
+++ chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/cfi_buildflags.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -416,8 +417,12 @@ void PolicyUITest::VerifyExportingPolicies(
   EXPECT_EQ(expected, *value_ptr);
 }
 
-#if !defined(NDEBUG)
-// Slow and hangs often in debug builds. https://crbug.com/1338642
+#if !defined(NDEBUG) ||                                          \
+    (BUILDFLAG(IS_LINUX) &&                                      \
+     (BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
+      BUILDFLAG(CFI_ENFORCEMENT_TRAP) ||                         \
+      BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC)))
+// Slow in debug and CFI builds crbug.com/1338642
 #define MAYBE_WritePoliciesToJSONFile DISABLED_WritePoliciesToJSONFile
 #else
 #define MAYBE_WritePoliciesToJSONFile WritePoliciesToJSONFile
