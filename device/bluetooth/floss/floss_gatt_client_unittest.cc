6e750c6ffc846f3d9daf202524cc491b23d6f752
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2022-08-29 18:32:15 +0000

    [floss] Partially implement Gatt DBus client
    Add a partial implementation of a GATT DBus client. This CL focuses on
    getting partial wiring and DBus serialization/deserialization compiling.
    
    BUG=b:193686094
    TEST=tools/autotest.py -C out/default --run_all ./device/bluetooth/floss
    
    Change-Id: I5fcd69e2f2baf1c91311a85f9511096b44801bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859781
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040523}

---
+++ device/bluetooth/floss/floss_gatt_client_unittest.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/floss/floss_gatt_client.h"
+
+#include "base/memory/raw_ptr.h"
+#include "base/memory/ref_counted.h"
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "base/test/task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace floss {
+
+class FlossGattClientTest : public testing::Test {
+ public:
+  void SetUp() override { client_ = FlossGattClient::Create(); }
+
+  void TearDown() override { client_.reset(); }
+
+  std::unique_ptr<FlossGattClient> client_;
+
+  base::test::TaskEnvironment task_environment_;
+  base::WeakPtrFactory<FlossGattClientTest> weak_ptr_factory_{this};
+};
+
+TEST_F(FlossGattClientTest, BasicTest) {}
+
+}  // namespace floss
