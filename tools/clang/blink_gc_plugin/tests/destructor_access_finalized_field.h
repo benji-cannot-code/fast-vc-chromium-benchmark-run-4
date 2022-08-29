67f173585c7be48e360dfb2ae80c48c334bedc38
Nico Weber <thakis@chromium.org>
2022-08-29 16:52:35 +0000

    Revert "Issue warning for default malloc used in blink"
    This reverts commit fa3b24999ef2ba988906737a5433626c0abdb6d7.
    
    Reason for revert: Prerequisite for reverting
    https://chromium-review.googlesource.com/c/chromium/src/+/3859879
    
    Original change's description:
    > Issue warning for default malloc used in blink
    >
    > The default malloc is discouraged in blink code. This CL adds a
    > checker in blink_gc_plugin to check for classes that use the default
    > malloc.
    >
    > The checker is not enabled by default yet. We'll enable it for blink
    > subdirectories incrementally and fix the issues found.
    >
    > Bug: 1357410
    > Change-Id: I50906a89d767b955a72a1f6bfce4ad5a2ef4159a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860855
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1040240}
    
    Bug: 1357410
    Change-Id: I1907a86cb35498056d8e4fec35e4a5785bfd2ccc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3862234
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040455}

--- tools/clang/blink_gc_plugin/tests/destructor_access_finalized_field.h
+++ tools/clang/blink_gc_plugin/tests/destructor_access_finalized_field.h
@@ -11,10 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Other : public RefCounted<Other> {
-  USING_FAST_MALLOC();
-
- public:
-  bool foo() { return true; }
+public:
+    bool foo() { return true; }
 };
 
 class HeapObject;
--- tools/clang/blink_gc_plugin/tests/left_most_gc_base.h
+++ tools/clang/blink_gc_plugin/tests/left_most_gc_base.h
@@ -10,15 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class A {
- protected:
-  A() = default;
-};
-
-class B {
- protected:
-  B() = default;
-};
+class A { };
+class B { };
 
 class Right : public A, public B, public GarbageCollected<Right> { };  // Error
 class Left : public GarbageCollected<Left>, public B, public A { };
