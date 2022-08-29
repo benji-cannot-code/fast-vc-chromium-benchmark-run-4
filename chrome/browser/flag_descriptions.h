3354e58569e58205724b67a78ed90e43100909ce
François Doray <fdoray@chromium.org>
2022-08-29 15:28:32 +0000

    [blink scheduler] Remove throttle-foreground-timers flag.
    This feature is subsumed by the AlignWakeUps feature.
    
    Bug: 1201248
    Change-Id: I4896226153b0310799fd796addcdccebbc49a694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859927
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040438}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1589,9 +1589,6 @@ extern const char kTopChromeTouchUiDescription[];
 extern const char kThreadedScrollingName[];
 extern const char kThreadedScrollingDescription[];
 
-extern const char kThrottleForegroundTimersName[];
-extern const char kThrottleForegroundTimersDescription[];
-
 extern const char
     kThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframesName[];
 extern const char
