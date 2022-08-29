ceca307bbcb7c6debc995c02c977dfaf15d9607f
Eric Willigers <ericwilligers@chromium.org>
2022-08-29 03:05:18 +0000

    WebAppNavigationBrowserTest: Retire IsServiceAvailable
    As only recent Ash versions are used in skew testing,
    we don't check to check the Ash version in these tests.
    
    This is a partial revert of
    crrev.com/c/3823372
    
    Bug: 1288199
    Change-Id: Id07a059ebe2f005e8a6eb0fead166427b2f76e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859144
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040233}

--- chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
@@ -189,9 +189,6 @@ class WebAppLinkCapturingBrowserTest : public WebAppNavigationBrowserTest {
 // app windows.
 IN_PROC_BROWSER_TEST_F(WebAppLinkCapturingBrowserTest,
                        NavigateExistingClientFromBrowser) {
-  if (!IsServiceAvailable())
-    GTEST_SKIP();
-
   InstallTestApp(
       "/web_apps/get_manifest.html?"
       "launch_handler_client_mode_navigate_existing.json");
@@ -224,9 +221,6 @@ IN_PROC_BROWSER_TEST_F(WebAppLinkCapturingBrowserTest,
 // Link captures from about:blank cleans up the about:blank page.
 IN_PROC_BROWSER_TEST_F(WebAppLinkCapturingBrowserTest,
                        AboutBlankNavigationCleanUp) {
-  if (!IsServiceAvailable())
-    GTEST_SKIP();
-
   InstallTestApp("/web_apps/basic.html");
   TurnOnLinkCapturing();
 
@@ -248,9 +242,6 @@ IN_PROC_BROWSER_TEST_F(WebAppLinkCapturingBrowserTest,
 // page.
 IN_PROC_BROWSER_TEST_F(WebAppLinkCapturingBrowserTest,
                        JavascriptAboutBlankNavigationCleanUp) {
-  if (!IsServiceAvailable())
-    GTEST_SKIP();
-
   InstallTestApp("/web_apps/basic.html");
   TurnOnLinkCapturing();
 
@@ -295,9 +286,6 @@ class WebAppTabStripLinkCapturingBrowserTest
 // the app window.
 IN_PROC_BROWSER_TEST_F(WebAppTabStripLinkCapturingBrowserTest,
                        InScopeNavigationsCaptured) {
-  if (!IsServiceAvailable())
-    GTEST_SKIP();
-
   InstallTestTabbedApp();
   TurnOnLinkCapturing();
 
