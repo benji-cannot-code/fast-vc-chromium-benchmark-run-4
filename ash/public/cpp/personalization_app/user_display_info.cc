39b7db432c737146de5892e5df3ca790cbdeba6a
Jason Thai <jasontt@chromium.org>
2022-08-29 21:51:44 +0000

    personalization: update guest display name
    Show a localized guest name instead of guest user's internal name.
    
    Screenshot: https://screenshot.googleplex.com/8vTMAQnwAkKEQN2
    
    BUG=b/243542261
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I0297099f94c754267512a688a13b9b07e25224f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857197
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040623}

--- ash/public/cpp/personalization_app/user_display_info.cc
+++ ash/public/cpp/personalization_app/user_display_info.cc
@@ -5,21 +5,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/personalization_app/user_display_info.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/user_manager/user.h"
+#include "components/user_manager/user_names.h"
+#include "ui/base/l10n/l10n_util.h"
 
-namespace ash {
-namespace personalization_app {
+namespace ash::personalization_app {
 
 UserDisplayInfo::UserDisplayInfo() = default;
 
 UserDisplayInfo::UserDisplayInfo(const user_manager::User& user)
-    : email(user.GetDisplayEmail()),
-      name(base::UTF16ToUTF8(user.GetDisplayName())) {}
+    : email(user.GetDisplayEmail()) {
+  if (user.GetAccountId() == user_manager::GuestAccountId()) {
+    name = base::UTF16ToUTF8(
+        l10n_util::GetStringUTF16(IDS_PERSONALIZATION_APP_GUEST_NAME));
+  } else {
+    name = base::UTF16ToUTF8(user.GetDisplayName());
+  }
+}
 
 UserDisplayInfo::UserDisplayInfo(UserDisplayInfo&&) = default;
 UserDisplayInfo& UserDisplayInfo::operator=(UserDisplayInfo&&) = default;
 
 UserDisplayInfo::~UserDisplayInfo() = default;
 
-}  // namespace personalization_app
-}  // namespace ash
+}  // namespace ash::personalization_app
