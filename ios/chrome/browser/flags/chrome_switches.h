6bf6b62a37447a65affaeccc01c1fcaa81ba475a
Gauthier Ambard <gambard@chromium.org>
2022-08-29 11:52:29 +0000

    [iOS] Move flags-related files out of ios/chrome/browser
    Move them to the flags/ folder as they are related to flags.
    
    Bug: 1354614
    Change-Id: Idfe93bbd4f540c6245f94cbf5ae7d3705063d3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857307
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040363}

--- ios/chrome/browser/flags/chrome_switches.h
+++ ios/chrome/browser/flags/chrome_switches.h
@@ -6,6 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_FLAGS_CHROME_SWITCHES_H_
 #define IOS_CHROME_BROWSER_FLAGS_CHROME_SWITCHES_H_
 
-#import "ios/chrome/browser/chrome_switches.h"
+// Defines all the command-line switches used by iOS Chrome.
+
+namespace switches {
+
+extern const char kDisableIOSPasswordSuggestions[];
+extern const char kDisableThirdPartyKeyboardWorkaround[];
+
+extern const char kEnableIOSHandoffToOtherDevices[];
+extern const char kEnableSpotlightActions[];
+extern const char kEnableThirdPartyKeyboardWorkaround[];
+extern const char kEnableDiscoverFeed[];
+extern const char kEnableUpgradeSigninPromo[];
+
+extern const char kUserAgent[];
+
+}  // namespace switches
 
 #endif  // IOS_CHROME_BROWSER_FLAGS_CHROME_SWITCHES_H_
--- ios/chrome/browser/flags/ios_chrome_field_trials.mm
+++ ios/chrome/browser/flags/ios_chrome_field_trials.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ios_chrome_field_trials.h"
+#import "ios/chrome/browser/flags/ios_chrome_field_trials.h"
 
 #import "base/check.h"
 #import "base/path_service.h"
