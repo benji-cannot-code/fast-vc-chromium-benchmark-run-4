891484a47d37f4c3d434fc18e4203728b9e1e808
Emilia Paz <emiliapaz@chromium.org>
2022-08-29 22:14:27 +0000

    Add support for ScrollView in DialogModel contents
    Content section can be large, thus we need to support scroll view for
    based on a maximum height. Note that content section doesn't include
    title and buttons row.
    
    Screencast: https://drive.google.com/file/d/1BC4vZvJ7B0EJK6sHoP9f3kfJtHOLcGrl/view
    
    Bug: 1348165
    Change-Id: I32ad2f6dc51518eaedf464c5f47850d0ddf78af8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813818
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040633}

--- ui/views/bubble/bubble_dialog_model_host.h
+++ ui/views/bubble/bubble_dialog_model_host.h
@@ -34,6 +34,8 @@ class VIEWS_EXPORT BubbleDialogModelHost : public BubbleDialogDelegate,
  public:
   enum class FieldType { kText, kControl, kMenuItem };
 
+  class ContentsView;
+
   class VIEWS_EXPORT CustomView : public ui::DialogModelCustomField::Field {
    public:
     CustomView(std::unique_ptr<View> view, FieldType field_type);
@@ -79,12 +81,13 @@ class VIEWS_EXPORT BubbleDialogModelHost : public BubbleDialogDelegate,
   View* GetInitiallyFocusedView() override;
   void OnWidgetInitialized() override;
 
+  View* GetContentsViewForTesting();
+
   // ui::DialogModelHost:
   void Close() override;
   void OnFieldAdded(ui::DialogModelField* field) override;
 
  private:
-  class ContentsView;
   // TODO(pbos): Consider externalizing this functionality into a different
   // format that could feasibly be adopted by LayoutManagers. This is used for
   // BoxLayouts (but could be others) to agree on columns' preferred width as a
