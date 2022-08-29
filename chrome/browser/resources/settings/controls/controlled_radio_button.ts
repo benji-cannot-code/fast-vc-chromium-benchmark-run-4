2705380200d38f8a7f099d446df6d533c3396719
dpapad <dpapad@chromium.org>
2022-08-29 21:33:39 +0000

    WebUI: Remove obsolete references to iron-a11y-keys-behavior.js
    These references seem to be unnecessary since the file is imported but
    the IronA11yKeysBehavior class is not.
    
    Bug: None
    Change-Id: Icf1ef1aa71f777854ca05a1c473876b1210e3300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858367
    Commit-Queue: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040614}

--- chrome/browser/resources/settings/controls/controlled_radio_button.ts
+++ chrome/browser/resources/settings/controls/controlled_radio_button.ts
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '//resources/cr_elements/cr_radio_button/cr_radio_button_style.css.js';
 import '//resources/cr_elements/policy/cr_policy_pref_indicator.js';
-import '//resources/polymer/v3_0/iron-a11y-keys-behavior/iron-a11y-keys-behavior.js';
 import '../settings_shared.css.js';
 
 import {CrRadioButtonMixin, CrRadioButtonMixinInterface} from '//resources/cr_elements/cr_radio_button/cr_radio_button_mixin.js';
