c87467b29970705103ae5b018540bfdb5969d568
Jeevan Shikaram <jshikaram@chromium.org>
2022-08-29 08:34:06 +0000

    [APS] Create a new service called the App Preload Service.
    This CL adds a skeleton implementation of a service.
    
    Bug: b:243329494
    Change-Id: I2cef5ac914acda6fe183967aaae1706703545013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858246
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040310}

---
+++ chrome/browser/apps/app_preload_service/app_preload_service.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_SERVICE_H_
+#define CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+class Profile;
+
+namespace apps {
+
+class AppPreloadService : public KeyedService {
+ public:
+  explicit AppPreloadService(Profile* profile);
+  AppPreloadService(const AppPreloadService&) = delete;
+  AppPreloadService& operator=(const AppPreloadService&) = delete;
+  ~AppPreloadService() override;
+};
+
+}  // namespace apps
+
+#endif  // CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_SERVICE_H_
