f56223d199fbbbdb4e484ac2882f53aa250ca802
Zofia Salata <zsalata@google.com>
2022-08-29 08:16:16 +0000

    [PageInfo] Add correct icon and real strings
    Add the correct icon for blocking 3pc button and
    add new strings for tooltips and title of cookies
    dialog button, all in cookies subpage.
    
    Bug: 1346305
    Change-Id: I18b4f2f10feffaa92c2f2f1449a566f13efe358d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845302
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Zofia Salata <zsalata@google.com>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040303}

--- chrome/browser/ui/views/page_info/page_info_view_factory.cc
+++ chrome/browser/ui/views/page_info/page_info_view_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/page_info/chrome_page_info_ui_delegate.h"
 #include "chrome/browser/ui/view_ids.h"
@@ -465,5 +466,11 @@ const ui::ImageModel PageInfoViewFactory::GetManagedPermissionIcon(
           ? vector_icons::kExtensionIcon
           : vector_icons::kBusinessIcon;
   return ui::ImageModel::FromVectorIcon(managed_vector_icon, ui::kColorIcon,
+                                        GetIconSize());
+}
+
+// static
+const ui::ImageModel PageInfoViewFactory::GetBlockingThirdPartyCookiesIcon() {
+  return ui::ImageModel::FromVectorIcon(kEyeCrossedIcon, ui::kColorIcon,
                                         GetIconSize());
 }
