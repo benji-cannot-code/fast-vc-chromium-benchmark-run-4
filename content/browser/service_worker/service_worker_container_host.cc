4c463cf5d64c01a0bdf3a8a567097bc21c9f5304
Ali Hijazi <ahijazi@chromium.org>
2022-08-29 17:20:56 +0000

    DanglingPtr: fix dangling ptr in ServiceWorkerObjectHost
    Use base::WeakPtr instead of raw_ptr to avoid holding a dangling ptr.
    
    Bug: 1291138
    Change-Id: Ia862ee8d7a8cfbf6ecdfa8d587984aadcfc4ba12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858235
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040471}

--- content/browser/service_worker/service_worker_container_host.cc
+++ content/browser/service_worker/service_worker_container_host.cc
@@ -721,7 +721,7 @@ ServiceWorkerContainerHost::CreateServiceWorkerObjectInfoToSend(
     return existing_object_host->second->CreateCompleteObjectInfoToSend();
   }
   service_worker_object_hosts_[version_id] =
-      std::make_unique<ServiceWorkerObjectHost>(context_, this,
+      std::make_unique<ServiceWorkerObjectHost>(context_, GetWeakPtr(),
                                                 std::move(version));
   return service_worker_object_hosts_[version_id]
       ->CreateCompleteObjectInfoToSend();
@@ -740,7 +740,7 @@ ServiceWorkerContainerHost::GetOrCreateServiceWorkerObjectHost(
     return existing_object_host->second->AsWeakPtr();
 
   service_worker_object_hosts_[version_id] =
-      std::make_unique<ServiceWorkerObjectHost>(context_, this,
+      std::make_unique<ServiceWorkerObjectHost>(context_, GetWeakPtr(),
                                                 std::move(version));
   return service_worker_object_hosts_[version_id]->AsWeakPtr();
 }
