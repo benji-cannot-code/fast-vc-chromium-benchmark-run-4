a90301db4a991405eefe2948a1f281fd3b8df9fb
Aliona DANGLA <alionadangla@google.com>
2022-08-29 14:01:49 +0000

    [iOS] Add flag for open in download
    Add flag with 2 variations:
    * Download with WKDownload API
    * Download V2
    
    Change-Id: I88898d8224a80c47a3d9c55e7317100016dbbeb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858298
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040403}

---
+++ ios/chrome/browser/ui/open_in/features.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_OPEN_IN_FEATURES_H_
+#define IOS_CHROME_BROWSER_UI_OPEN_IN_FEATURES_H_
+
+#import "base/feature_list.h"
+
+// Feature flag that enables Open In download.
+extern const base::Feature kEnableOpenInDownload;
+
+// Indicates which Open In download variant to use.
+extern const char kOpenInDownloadWithWKDownloadParam[];
+extern const char kOpenInDownloadWithV2Param[];
+
+// Convenience method for determining when Open In download with WKDownload is
+// enabled.
+bool IsOpenInDownloadWithWKDownload();
+
+// Convenience method for determining when Open In download with V2 is enabled.
+bool IsOpenInDownloadWithV2();
+
+// Convenience method for determining when Open In download experiment is
+// enabled.
+bool IsOpenInDownloadEnabled();
+
+#endif  // IOS_CHROME_BROWSER_UI_OPEN_IN_FEATURES_H_
