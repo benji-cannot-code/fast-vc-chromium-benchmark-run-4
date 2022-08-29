f4dc3498a1474f9963eeca1168dbe729301cf942
Saho Kobayashi <sahok@google.com>
2022-08-29 08:21:17 +0000

    Reland "Fix standard actions and custom actions from panel"
    This is a reland of commit 425483c2f3e9e02c4b6f5db321a74c31f67875a6
    
    Original change's description:
    > Fix standard actions and custom actions from panel
    >
    > clearSavedNode and pendingCallback is Promise so that
    > PanelBackground.savedNode_ might be deleted before the action is
    > performed.
    > Standard actions and custom actions are saved in the
    > PanelBackground.savedNode_, so it fails to perform the action when
    > savedNode_ is cleared.
    > This CL adds a change to wait for the pendingCallback before
    > clearSavedNode.
    >
    > Bug: none
    > Test: ChromeVoxPanelTest.PerformDoDefaultAction
    > Change-Id: I713c6da64a3aa960e77dfc6158abf0bcfced3a2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749865
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Commit-Queue: Saho Kobayashi <sahok@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1031371}
    
    Bug: none
    Change-Id: I4f25b011747828638eeea4ee2637be1520bf36f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833260
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Saho Kobayashi <sahok@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040305}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel_menu_item.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel_menu_item.js
@@ -15,7 +15,8 @@ export class PanelMenuItem {
    * @param {string} menuItemShortcut The keystrokes to select this item.
    * @param {string} menuItemBraille The braille keystrokes to select this item.
    * @param {string} gesture The gesture to select this item.
-   * @param {Function} callback The function to call if this item is selected.
+   * @param {function() : !Promise} callback The function to call if this item
+   *     is selected.
    * @param {string=} opt_id An optional id for the menu item element.
    */
   constructor(
@@ -29,7 +30,7 @@ export class PanelMenuItem {
     this.menuItemBraille = menuItemBraille;
     /** @type {string} */
     this.gesture = gesture;
-    /** @type {Function} */
+    /** @type {function() : !Promise} */
     this.callback = callback;
 
     /** @type {Element} */
