82f141075c8c4dd7dc6b1f6ccb75cc25883a8b6c
Xianzhu Wang <wangxianzhu@chromium.org>
2022-08-29 02:27:13 +0000

    Update blink_gc_plugin rules about DISALLOW_NEW() and GarbageCollected
    Blink merged DISALLOW_NEW() and DISALLOW_NEW_EXCEPT_PLACEMENT_NEW()
    in 2018 (crrev.com/592753). Now DISALLOW_NEW() disallows allocation
    new only (i.e. the same as the previous
    DISALLOW_NEW_EXCEPT_PLACEMENT_NEW()). Now update blink_gc_plugin
    to reflect the change.
    
    Blink's GarbageCollected also has deleted new operators, which
    affects the plugin rules. Now update stubs.h to make the stubs closer
    to the actual definitions.
    
    Also improve the rule when DISALLOW_NEW() is inherited and reduce
    duplicated warnings when STACK_ALLOCATED() and GarbageCollected is
    used in the same class.
    
    Change-Id: Ie44c286575c665e0d2f103ad058f9103acf14b25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859879
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040228}

--- tools/clang/blink_gc_plugin/tests/heap/stubs.h
+++ tools/clang/blink_gc_plugin/tests/heap/stubs.h
@@ -6,15 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HEAP_STUBS_H_
 #define HEAP_STUBS_H_
 
-#include "stddef.h"
-
-#define WTF_MAKE_FAST_ALLOCATED                 \
-    public:                                     \
-    void* operator new(size_t, void* p);        \
-    void* operator new[](size_t, void* p);      \
-    void* operator new(size_t size);            \
-    private:                                    \
-    typedef int __thisIsHereToForceASemicolonAfterThisMacro
+#include <cstddef>
+#include <new>
+#include <utility>
 
 namespace WTF {
 
@@ -222,7 +216,16 @@ class BasicCrossThreadPersistent : public PersistentBase {
 }  // namespace internal
 
 template <typename T>
-class GarbageCollected {};
+class GarbageCollected {
+  void* operator new(size_t) = delete;
+  void* operator new[](size_t) = delete;
+};
+
+template <typename T, typename... Args>
+T* MakeGarbageCollected(int, Args&&... args) {
+  void* memory = reinterpret_cast<void*>(0x87654321);
+  return ::new (memory) T(std::forward<Args>(args)...);
+}
 
 class GarbageCollectedMixin {
  public:
@@ -262,6 +265,10 @@ using Visitor = cppgc::Visitor;
 
 template <typename T>
 using GarbageCollected = cppgc::GarbageCollected<T>;
+template <typename T, typename... Args>
+T* MakeGarbageCollected(Args&&... args) {
+  return cppgc::MakeGarbageCollected<T>(0, std::forward<Args>(args)...);
+}
 
 using GarbageCollectedMixin = cppgc::GarbageCollectedMixin;
 
@@ -291,7 +298,17 @@ class Visitor {
   void Trace(const T&);
 };
 
-template<typename T> class GarbageCollected { };
+template <typename T>
+class GarbageCollected {
+  void* operator new(size_t) = delete;
+  void* operator new[](size_t) = delete;
+};
+
+template <typename T, typename... Args>
+T* MakeGarbageCollected(Args&&... args) {
+  void* memory = reinterpret_cast<void*>(0x87654321);
+  return ::new (memory) T(std::forward<Args>(args)...);
+}
 
 class GarbageCollectedMixin {
  public:
@@ -346,10 +363,12 @@ public:
 
 using namespace WTF;
 
-#define DISALLOW_NEW()                 \
- private:                              \
-  void* operator new(size_t) = delete; \
-  void* operator new(size_t, void*) = delete;
+#define DISALLOW_NEW()                                            \
+ public:                                                          \
+  void* operator new(size_t, void* location) { return location; } \
+                                                                  \
+ private:                                                         \
+  void* operator new(size_t) = delete
 
 #define STACK_ALLOCATED()                                  \
  public:                                                   \
@@ -357,14 +376,7 @@ using namespace WTF;
                                                            \
  private:                                                  \
   void* operator new(size_t) = delete;                     \
-  void* operator new(size_t, void*) = delete;
-
-#define DISALLOW_NEW_EXCEPT_PLACEMENT_NEW() \
- public:                                    \
-  void* operator new(size_t, void*);        \
-                                            \
- private:                                   \
-  void* operator new(size_t) = delete;
+  void* operator new(size_t, void*) = delete
 
 #define GC_PLUGIN_IGNORE(bug) \
   __attribute__((annotate("blink_gc_plugin_ignore")))
