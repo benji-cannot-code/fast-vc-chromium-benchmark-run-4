8b43ebd664138e9e02bb3c47fe57c527c6583c35
Sorin Jianu <sorin@chromium.org>
2022-08-29 06:26:29 +0000

    Fix registry permission in integration tests `Clean()`
    Clean() can't delete REGSTR_PATH_RUN, and it makes dependent tests
    fail.
    
    Fixed: 1357256
    Change-Id: If45c9b8203c40a0e6e93122849b829a8b252f37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860610
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040273}

--- chrome/updater/test/integration_tests_win.cc
+++ chrome/updater/test/integration_tests_win.cc
@@ -512,7 +512,7 @@ void Clean(UpdaterScope scope) {
   }
 
   if (scope == UpdaterScope::kUser) {
-    base::win::RegKey(root, REGSTR_PATH_RUN, KEY_READ)
+    base::win::RegKey(root, REGSTR_PATH_RUN, KEY_WRITE)
         .DeleteValue(GetTaskNamePrefix(scope).c_str());
   }
 
