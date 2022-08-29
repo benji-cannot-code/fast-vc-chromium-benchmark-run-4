ba62b12af07089fd5d2703a9cdba281f3d6941a4
Jeffrey Young <cowmoo@google.com>
2022-08-29 18:01:44 +0000

    personalization: null safety |OnUserImageIsEnterpriseManagedChanged|
    In some cases, there is no user associated with a given AccountId
    that was used to remove policy controlled avatar image data. Be more
    careful about null safety to prevent a crash in that instance.
    Only notify user image enterprise state observers of the change
    if there is a valid user.
    
    BUG=b:242644179
    
    Change-Id: I31ffb0260d4e12de53a35897c57a05874ffa277b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840856
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040497}

--- chrome/browser/ash/web_applications/personalization_app/enterprise_policy_delegate_impl.cc
+++ chrome/browser/ash/web_applications/personalization_app/enterprise_policy_delegate_impl.cc
@@ -43,7 +43,7 @@ bool EnterprisePolicyDelegateImpl::IsUserImageEnterpriseManaged() const {
 
 bool EnterprisePolicyDelegateImpl::IsWallpaperEnterpriseManaged() const {
   return WallpaperController::Get()->IsWallpaperControlledByPolicy(
-      GetUser(profile_)->GetAccountId());
+      GetAccountId(profile_));
 }
 
 void EnterprisePolicyDelegateImpl::AddObserver(
@@ -60,7 +60,7 @@ void EnterprisePolicyDelegateImpl::OnUserImageIsEnterpriseManagedChanged(
     const user_manager::User& user,
     bool is_enterprise_managed) {
   // Filter out updates from other users.
-  if (user.GetAccountId() != GetUser(profile_)->GetAccountId()) {
+  if (user.GetAccountId() != GetAccountId(profile_)) {
     return;
   }
 
