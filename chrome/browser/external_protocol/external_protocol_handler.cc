b42fcb4190f9932793a9dee5ba997f68126474f7
Daniel Cheng <dcheng@chromium.org>
2022-08-29 01:22:27 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //chrome/browser/external_protocol.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=benwells@chromium.org
    
    Bug: 1356184
    Change-Id: I30113ec1cc5c082d9adb95974dcdabd8b10159ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860726
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040225}

--- chrome/browser/external_protocol/external_protocol_handler.cc
+++ chrome/browser/external_protocol/external_protocol_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
 
 #include <stddef.h>
+
 #include <utility>
 
 #include "base/bind.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/escape.h"
 #include "base/strings/string_util.h"
+#include "base/types/optional_util.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/external_protocol/auto_launch_protocols_policy_handler.h"
@@ -440,7 +442,7 @@ void ExternalProtocolHandler::LaunchUrl(
   if (web_contents)  // Maybe NULL during testing.
     profile = Profile::FromBrowserContext(web_contents->GetBrowserContext());
   BlockState block_state = GetBlockStateWithDelegate(
-      escaped_url.scheme(), base::OptionalOrNullptr(initiating_origin),
+      escaped_url.scheme(), base::OptionalToPtr(initiating_origin),
       g_external_protocol_handler_delegate, profile);
   if (block_state == BLOCK) {
     AddMessageToConsole(
