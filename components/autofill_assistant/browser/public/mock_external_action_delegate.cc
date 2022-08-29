b3a263b8fdfd8e1f6a384d8b9557878310602f49
Luca Hunkeler <hluca@google.com>
2022-08-29 15:37:31 +0000

    Add tests for ExternalActions
    Adding some tests to the headless unit tests to cover a few basic usages of ExternalAction
    
    
    Bug: b/201964911
    Change-Id: Ic12251496b17fde4b104393db921bc3bb0f30bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812168
    Reviewed-by: Florian Gauger <fga@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040440}

---
+++ components/autofill_assistant/browser/public/mock_external_action_delegate.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/public/mock_external_action_delegate.h"
+
+namespace autofill_assistant {
+MockExternalActionDelegate::MockExternalActionDelegate() = default;
+MockExternalActionDelegate::~MockExternalActionDelegate() = default;
+}  // namespace autofill_assistant
