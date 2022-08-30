139a0f21a02c06711be96f865e595dc8f0a08fd6
Mahmoud Rashad <mmrashad@google.com>
2022-08-30 11:14:57 +0000

    [Sync] Suppress NTP promo after X impressions behind a feature flag
    This CL adds the feature flag `SyncAndroidLimitNTPPromoImpressions` and
    adds `SyncAndroidNTPPromoImpressionsLimit` as a feature parameter, then
    it suppresses the NTP promo after `SyncAndroidNTPPromoImpressionsLimit`
    impressions when the flag is enabled, currently the feature flag is
    disabled by default.
    
    Bug: 1353777
    Change-Id: I1d476c159b3fcdb23550f47e6c1c5da017032445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845066
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040890}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2238,6 +2238,9 @@ extern const char kStrictSiteIsolationDescription[];
 extern const char kStylusWritingToInputName[];
 extern const char kStylusWritingToInputDescription[];
 
+extern const char kSyncAndroidLimitNTPPromoImpressionsName[];
+extern const char kSyncAndroidLimitNTPPromoImpressionsDescription[];
+
 extern const char kSyncAndroidPromosWithAlternativeTitleName[];
 extern const char kSyncAndroidPromosWithAlternativeTitleDescription[];
 
