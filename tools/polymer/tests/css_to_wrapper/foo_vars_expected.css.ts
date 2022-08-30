43da3370b32d4ef5645752be7f4677523ae52e27
Rebekah Potter <rbpotter@chromium.org>
2022-08-30 23:51:57 +0000

    Revert "WebUI: Produce scheme-relative //resources URL in css_to_wrapper()."
    This reverts commit 2229b060c9a24954d2ea316d9d48bae9547aef07.
    
    Reason for revert: Likely responsible for breaking tests for the 
    PDF viewer, due to incorrectly introducing scheme-relative
    imports for chrome-extension://
    
    See https://bugs.chromium.org/p/chromium/issues/detail?id=1358255
    
    Original change's description:
    > WebUI: Produce scheme-relative //resources URL in css_to_wrapper().
    >
    > This is necessary since some files (for example [1]) are used from
    > chrome-untrusted:// contexts.
    >
    > [1] ui/webui/resources/cr_elements/shared_style_css.m.js
    >
    > Bug: 1322682
    > Change-Id: Ife78b1824bb14a8780077e3668b05fc83eec38b1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863002
    > Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1041021}
    
    Bug: 1322682
    Change-Id: If031720c4a339c7fe362e128074896130e614307
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866442
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041262}

--- tools/polymer/tests/css_to_wrapper/foo_vars_expected.css.ts
+++ tools/polymer/tests/css_to_wrapper/foo_vars_expected.css.ts
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import './other1.css.js';
 import './other2.css.js';
 
