04affa47164ad32496dd26fdd300bee9d26d117b
dpapad <dpapad@chromium.org>
2022-08-30 16:27:18 +0000

    WebUI: Produce scheme-relative //resources URL in css_to_wrapper().
    This is necessary since some files (for example [1]) are used from
    chrome-untrusted:// contexts.
    
    [1] ui/webui/resources/cr_elements/shared_style_css.m.js
    
    Bug: 1322682
    Change-Id: Ife78b1824bb14a8780077e3668b05fc83eec38b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863002
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041021}

--- tools/polymer/tests/css_to_wrapper/foo_no_includes_style_expected.css.ts
+++ tools/polymer/tests/css_to_wrapper/foo_no_includes_style_expected.css.ts
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import './other_vars.css.js';
 
 const styleMod = document.createElement('dom-module');
