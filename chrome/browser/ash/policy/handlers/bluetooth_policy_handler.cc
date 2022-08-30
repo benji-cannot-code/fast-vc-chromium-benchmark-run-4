b784ede0c57a21e60efdaa1f8f42d431b86f4a42
Andrey Davydov <andreydav@google.com>
2022-08-30 03:06:07 +0000

    [CodeHealth] Removal of ListValue and DictionaryValue from CrosSettings
    This cl migrates CrosSettings::GetList and CrosSettings::GetDictionary
    to base::Value::List and base::Value::Dict respectively.
    
    Bug: 1187061
    Bug: 1187062
    Change-Id: Ia380f027e681995d27bdbf99ed5489b42de4faa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858223
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040740}

--- chrome/browser/ash/policy/handlers/bluetooth_policy_handler.cc
+++ chrome/browser/ash/policy/handlers/bluetooth_policy_handler.cc
@@ -59,7 +59,7 @@ void BluetoothPolicyHandler::SetBluetoothPolicy(
     scoped_refptr<device::BluetoothAdapter> adapter) {
   // Get the updated policy.
   bool allow_bluetooth = true;
-  const base::ListValue* allowed_services_list = nullptr;
+  const base::Value::List* allowed_services_list = nullptr;
   std::vector<device::BluetoothUUID> allowed_services;
 
   cros_settings_->GetBoolean(ash::kAllowBluetooth, &allow_bluetooth);
@@ -75,7 +75,7 @@ void BluetoothPolicyHandler::SetBluetoothPolicy(
   // returns an empty list even if the policy did not set.
   if (cros_settings_->GetList(ash::kDeviceAllowedBluetoothServices,
                               &allowed_services_list)) {
-    for (const auto& list_value : allowed_services_list->GetListDeprecated()) {
+    for (const auto& list_value : *allowed_services_list) {
       if (!list_value.is_string())
         continue;
 
