28d1085ad2c6bf5a29269bc2553a18f412da6043
Aykut Bulut <aykutb@google.com>
2022-08-30 17:19:22 +0000

    Implement redemption time limit
    Prevent frequent token redemption with refresh policy. When a third redemption request operation with refresh policy is within a certain amount of time since the last redemption, return resource exhausted status.
    
    Fixed: 1346290
    Change-Id: Ide6ac71d14e207ec32601be001941428e5063a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3779722
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Commit-Queue: Aykut Bulut <aykutb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041047}

--- services/network/trust_tokens/trust_token_persister_unittest.cc
+++ services/network/trust_tokens/trust_token_persister_unittest.cc
@@ -194,6 +194,7 @@ TYPED_TEST(TrustTokenPersisterTest, StoresIssuerToplevelPairConfigs) {
                        // time to initialize.
 
   TrustTokenIssuerToplevelPairConfig config;
+  config.set_penultimate_redemption("four o'clock");
   config.set_last_redemption("five o'clock");
 
   auto config_to_store =
@@ -223,6 +224,7 @@ TYPED_TEST(TrustTokenPersisterTest, DeletesIssuerToplevelKeyedData) {
                        // time to initialize.
 
   TrustTokenIssuerToplevelPairConfig pair_config;
+  pair_config.set_penultimate_redemption("four o'clock");
   pair_config.set_last_redemption("five o'clock");
 
   auto toplevel = *SuitableTrustTokenOrigin::Create(GURL("https://a.com/"));
