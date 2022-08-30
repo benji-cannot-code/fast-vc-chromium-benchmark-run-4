18e72ab7e85f90898776f603be486805677038c3
Wenyu Fu <wenyufu@google.com>
2022-08-30 16:48:29 +0000

    [HaTS] Do not show survey if UMA consent changed
    We check user's UMA consent before start downloads, but didn't handle the case if user disable the UMA upload between the survey download starts and survey being shown.
    
    Bug: 1355919
    Change-Id: I01d191e9da93787da10be00fe99cf0b0cbdc4049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852427
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041034}

--- chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeSurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeSurveyController.java
@@ -193,7 +193,9 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
      */
     @VisibleForTesting
     void onSurveyAvailable(String siteId) {
-        if (attemptToShowOnTab(mTabModelSelector.getCurrentTab(), siteId)) return;
+        if (!isUMAEnabled() || attemptToShowOnTab(mTabModelSelector.getCurrentTab(), siteId)) {
+            return;
+        }
 
         mTabModelObserver = new TabModelSelectorObserver() {
             @Override
@@ -214,6 +216,13 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
      * @return If the survey prompt was successfully shown.
      */
     private boolean attemptToShowOnTab(Tab tab, String siteId) {
+        if (!isUMAEnabled()) {
+            if (mTabModelObserver != null) {
+                mTabModelSelector.removeObserver(mTabModelObserver);
+                mTabModelObserver = null;
+            }
+            return false;
+        }
         if (!isValidTabForSurvey(tab)) return false;
 
         if (tab.isLoading() || !tab.isUserInteractable()) {
@@ -292,13 +301,16 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
             // Dismiss an enqueued message when the survey has expired so that it does not get shown
             // subsequently.
             message.set(MessageBannerProperties.ON_STARTED_SHOWING, () -> {
-                if (!SurveyController.getInstance().isSurveyExpired(siteId)) {
+                boolean surveyExpired = SurveyController.getInstance().isSurveyExpired(siteId);
+                if (!surveyExpired && isUMAEnabled()) {
                     return true;
                 }
-                Log.w(TAG,
-                        "The survey message prompt was dismissed because the survey "
-                                + "with ID %s has expired.",
-                        siteId);
+                if (surveyExpired) {
+                    Log.w(TAG,
+                            "The survey message prompt was dismissed because the survey "
+                                    + "with ID %s has expired.",
+                            siteId);
+                }
                 new Handler(ThreadUtils.getUiThreadLooper())
                         .post(()
                                         -> mMessageDispatcher.dismissMessage(
@@ -436,8 +448,9 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
     @VisibleForTesting
     void showInfoBarIfApplicable(Tab tab, String siteId, TabObserver observer) {
         if (!tab.isUserInteractable() || tab.isLoading()) return;
-
-        showSurveyPrompt(tab, siteId);
+        if (isUMAEnabled()) {
+            showSurveyPrompt(tab, siteId);
+        }
         tab.removeObserver(observer);
     }
 
