7a4a8abd5161c15b8b32572f1b78f6bdeba10371
Leonard Grey <lgrey@chromium.org>
2022-08-30 16:08:24 +0000

    Mac: re-enable some SampleBufferTransformer tests
    These are passing locally for me. SampleBufferTransformerMjpegTest is
    not so that's staying disabled. New bug: https://crbug.com/1354691
    
    Bug: 1154345
    Change-Id: Ide5ed8c89f87c6369aa57ae3b8dceb54f3674f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843031
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041013}

--- media/capture/video/mac/sample_buffer_transformer_mac_unittest.mm
+++ media/capture/video/mac/sample_buffer_transformer_mac_unittest.mm
@@ -397,14 +397,7 @@ TEST_P(SampleBufferTransformerPixelTransferTest, CanConvertFullScale) {
       PixelBufferIsSingleColor(output_pixel_buffer, kColorR, kColorG, kColorB));
 }
 
-#if defined(ARCH_CPU_ARM64)
-// Bulk-disabled for arm64 bot stabilization: https://crbug.com/1154345
-#define MAYBE_CanConvertAndScaleDown DISABLED_CanConvertAndScaleDown
-#else
-#define MAYBE_CanConvertAndScaleDown CanConvertAndScaleDown
-#endif
-
-TEST_P(SampleBufferTransformerPixelTransferTest, MAYBE_CanConvertAndScaleDown) {
+TEST_P(SampleBufferTransformerPixelTransferTest, CanConvertAndScaleDown) {
   auto [input_pixel_format, output_pixel_format] = GetParam();
 
   base::ScopedCFTypeRef<CMSampleBufferRef> input_sample_buffer =
@@ -535,7 +528,7 @@ TEST_P(SampleBufferTransformerLibyuvTest, CanConvertFullScale) {
       PixelBufferIsSingleColor(output_pixel_buffer, kColorR, kColorG, kColorB));
 }
 
-TEST_P(SampleBufferTransformerLibyuvTest, MAYBE_CanConvertAndScaleDown) {
+TEST_P(SampleBufferTransformerLibyuvTest, CanConvertAndScaleDown) {
   auto [input_pixel_format, output_pixel_format] = GetParam();
 
   base::ScopedCFTypeRef<CMSampleBufferRef> input_sample_buffer =
@@ -657,6 +650,12 @@ TEST_P(SampleBufferTransformerMjpegTest, CanConvertFullScale) {
       PixelBufferIsSingleColor(output_pixel_buffer, kColorR, kColorG, kColorB));
 }
 
+#if defined(ARCH_CPU_ARM64)
+// Disabled, see https://crbug.com/1354691
+#define MAYBE_CanConvertAndScaleDown DISABLED_CanConvertAndScaleDown
+#else
+#define MAYBE_CanConvertAndScaleDown CanConvertAndScaleDown
+#endif
 TEST_P(SampleBufferTransformerMjpegTest, MAYBE_CanConvertAndScaleDown) {
   OSType output_pixel_format = GetParam();
 
