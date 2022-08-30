c72ca930c9fa10b28e5dcd803704a0ea1dfe5f76
Wenyu Fu <wenyufu@google.com>
2022-08-30 16:55:32 +0000

    [CT][EngagementSignals] Do not report signals when UMA disabled
    Do not attach the GestureStateListener when UMA upload is disabled. Changes to UMA upload settings will not affect live CCT tabs.
    
    Adding APIs to reflect whether engagement signal can be reported is not included in this CL.
    
    Bug: 1353547
    Change-Id: I7edb62f2841357c3297ef445e4c7c8f54410d628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864344
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041037}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityTabController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityTabController.java
@@ -46,6 +46,7 @@ import org.chromium.chrome.browser.dependency_injection.ActivityScope;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.InflationObserver;
+import org.chromium.chrome.browser.privacy.settings.PrivacyPreferencesManagerImpl;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.RedirectHandlerTabHelper;
@@ -521,7 +522,10 @@ public class CustomTabActivityTabController implements InflationObserver {
      */
     private void maybeStartSendingRealTimeEngagementSignals(Tab tab) {
         assert tab.getWebContents() != null;
-
+        // Do not report engagement signals if user does not consent to report usage.
+        if (!PrivacyPreferencesManagerImpl.getInstance().isUsageAndCrashReportingPermitted()) {
+            return;
+        }
         if (mScrollState == null) mScrollState = new ScrollState();
         if (mGestureStateListener == null) {
             mGestureStateListener = new GestureStateListener() {
