a76c5bc53c6eaf9e01f188d3d1e786cbbf539317
Joel Hockey <joelhockey@chromium.org>
2022-08-30 04:33:02 +0000

    CrOS PlatformMimeUtil::GetPlatformMimeTypeFromExtension()
    http://go/cros-mime-cache
    
    Implement for CrOS by using freedesktop.org.xml.in source of the
    mime.cache database.
    
    Normally linux systems get this information by parsing the binary
    /usr/share/mime/mime.cache file which is created from the same XML
    file we are using.
    
    By generating this static list we avoid any security issues of
    parsing a file that we don't totally control and trust.
    We can also ensure that tests run the same regardless of which bots
    or machines they are on.
    
    Bug: 1015353
    Change-Id: I6b33c00d2686e48dab9e6b621b2eeeaced15dbc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848953
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040776}

--- net/base/platform_mime_util_linux.cc
+++ net/base/platform_mime_util_linux.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "net/android/network_library.h"
+#elif BUILDFLAG(IS_CHROMEOS_ASH)
+#include "third_party/xdg_shared_mime_info/mime_cache.h"
 #else
 #include "base/nix/mime_util_xdg.h"
 #endif
@@ -28,7 +30,7 @@ bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
 bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
     const base::FilePath::StringType& ext,
     std::string* result) const {
-  return false;
+  return xdg_shared_mime_info::GetMimeCacheTypeFromExtension(ext, result);
 }
 #else
 bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
