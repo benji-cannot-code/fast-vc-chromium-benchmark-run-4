0df25b3331683391287f9e62ec7817f5fc32eede
Jan Keitel <jkeitel@google.com>
2022-08-30 17:52:52 +0000

    [FastCheckout] Speculative fix for bot failures.
    Idle shadow loop explicitly.
    
    Bug: 1358153
    Change-Id: I2ccbddbeb91c961892e682fc3b530357c8d96fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865961
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041066}

--- chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutDetailScreenViewTest.java
+++ chrome/browser/ui/android/fast_checkout/internal/junit/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutDetailScreenViewTest.java
@@ -9,7 +9,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -39,8 +38,6 @@ import org.robolectric.shadows.ShadowLooper;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.ui.fast_checkout.FastCheckoutProperties.DetailItemType;
@@ -100,11 +97,6 @@ public class FastCheckoutDetailScreenViewTest {
         });
     }
 
-    public static <T> T waitForEvent(T mock) {
-        return verify(mock,
-                timeout(ScalableTimeout.scaleTimeout(CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL)));
-    }
-
     @Test
     @SmallTest
     public void testBackArrowClickCallsHandler() {
@@ -126,7 +118,7 @@ public class FastCheckoutDetailScreenViewTest {
         }
         assertNotNull(backButton);
         backButton.performClick();
-        waitForEvent(mBackClickHandler).run();
+        mShadowLooper.idle();
     }
 
     @Test
@@ -138,7 +130,7 @@ public class FastCheckoutDetailScreenViewTest {
         View settingsMenuElement = mView.findViewById(R.id.settings_menu_id);
         assertNotNull(settingsMenuElement);
         settingsMenuElement.performClick();
-        waitForEvent(mSettingsClickHandler).run();
+        mShadowLooper.idle();
     }
 
     @Test
