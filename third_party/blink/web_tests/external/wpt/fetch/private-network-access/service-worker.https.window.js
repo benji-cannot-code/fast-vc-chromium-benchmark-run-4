091fe08b81b74b887abdfc507bff495306dad706
Jonathan Hao <phao@chromium.org>
2022-08-30 16:49:18 +0000

    [Private Network Access] Fix service worker new script wpt test
    When browser requests service worker scripts, it sends a header "Service-Worker".  If the preflight response doesn't safelist it with
    "Access-Control-Allow-Headers", the request will fail, which is why the
    2 cases of private-network-access/service-worker.https.window.js fails.
    This CL fixes it.
    
    Bug: 1239551
    Change-Id: Iba1fee1f7384a39c1ce418382dc0efb069853ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858436
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041035}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/service-worker.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/service-worker.https.window.js
@@ -75,7 +75,7 @@ promise_test(t => makeTest(t, {
   },
   target: {
     server: Server.HTTPS_LOCAL,
-    behavior: { preflight: PreflightBehavior.success(token()) },
+    behavior: { preflight: PreflightBehavior.serviceWorkerSuccess(token()) },
   },
   expected: TestResult.SUCCESS,
 }), "treat-as-public to local: success.");
@@ -96,7 +96,7 @@ promise_test(t => makeTest(t, {
   },
   target: {
     server: Server.HTTPS_PRIVATE,
-    behavior: { preflight: PreflightBehavior.success(token()) },
+    behavior: { preflight: PreflightBehavior.serviceWorkerSuccess(token()) },
   },
   expected: TestResult.SUCCESS,
 }), "treat-as-public to private: success.");
