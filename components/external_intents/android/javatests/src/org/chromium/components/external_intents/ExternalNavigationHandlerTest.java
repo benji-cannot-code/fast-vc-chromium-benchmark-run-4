f7fcd805dfa7ed6c88e15e764ca3f33e337c17b3
Michael Thiessen <mthiesse@chromium.org>
2022-08-30 20:33:24 +0000

    Ignore navigation chain timeout for trusted CCT calling apps
    CCTs rely on bypassing the user gesture requirement for things like
    payment apps that require the user to manually switch to the payment
    app, verify a transaction, then manually switch back to Chrome and get
    redirected to an app. This app switching can exceed the 15s navigation
    chain timeout, but given we bypass the gesture requirement already, we
    should also bypass the 15s timeout when the caller is trusted.
    
    Bug: 1358198
    Change-Id: Ib5c7df9ba262808f13a9aeaadd662bc4ee6c2bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864355
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041164}

--- components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
+++ components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
@@ -2693,6 +2693,12 @@ public class ExternalNavigationHandlerTest {
         checkUrl(YOUTUBE_MOBILE_URL)
                 .withRedirectHandler(redirectHandler)
                 .expecting(OverrideUrlLoadingResultType.NO_OVERRIDE, IGNORE);
+
+        mDelegate.setIsCallingAppTrusted(true);
+        checkUrl(YOUTUBE_MOBILE_URL)
+                .withRedirectHandler(redirectHandler)
+                .expecting(OverrideUrlLoadingResultType.OVERRIDE_WITH_EXTERNAL_INTENT,
+                        START_OTHER_ACTIVITY);
     }
 
     @Test
