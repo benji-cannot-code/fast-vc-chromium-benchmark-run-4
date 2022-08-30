c85d6667eead5cf0c2159bf0f4c762aba556f84a
dpapad <dpapad@chromium.org>
2022-08-30 16:21:48 +0000

    WebUI: Remove Polymer2 from third_party/polymer/.
    Bug: 1184053,965770
    Change-Id: Idaa20f65f57f710f753ee06ad5788e35d60d8d80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858365
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041018}

--- third_party/polymer/v1_0/components-chromium/iron-dropdown/iron-dropdown-scroll-manager-extracted.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-(function() {
-    'use strict';
-    /**
-     * IronDropdownScrollManager is deprecated, use IronScrollManager instead.
-     */
-    Polymer.IronDropdownScrollManager = Polymer.IronScrollManager;
-  })();
