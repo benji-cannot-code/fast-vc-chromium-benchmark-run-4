ad02403cc8843dbec9d883508da534628a0296ad
Lijin Shen <lazzzis@google.com>
2022-08-30 00:34:14 +0000

    Sync infobar scrolling behavior with top browser control.
    This CL makes infobar always show/hide with top browser control.
    
    One exception is full screen mode: because top browser control is not
    visible, the infobar is enforced to be shown until user dismisses it
    or fullscreen mode is exited.
    
    Another exception is in CCT mode, infobar did not start hiding until
    bottom control became fully invisible; now, infobar hides along
    with bottom control.
    
    See demo: https://drive.google.com/drive/folders/1CbR2esg2kkuqTa4EjWbLoh8jbe53MMNE?resourcekey=0-pVjMCdg79jKPxt6AOydNxA&usp=sharing
    
    Bug: 1344189
    Change-Id: I8237e5a313c550ca3ea3da0493e3ee58c6198989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817909
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040693}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -248,6 +248,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kImmersiveUiMode,
     &kIncognitoReauthenticationForAndroid,
     &kIncognitoScreenshot,
+    &kInfobarScrollOptimization,
     &kInstanceSwitcher,
     &kInstantStart,
     &kIsVoiceSearchEnabledCache,
@@ -703,6 +704,9 @@ const base::Feature kIncognitoReauthenticationForAndroid{
 
 const base::Feature kIncognitoScreenshot{"IncognitoScreenshot",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kInfobarScrollOptimization{
+    "InfobarScrollOptimization", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kInstantStart{"InstantStart",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
