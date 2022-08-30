8d2648c0d64efe537865027bbb24e0b38928f203
YuhanYang <yyhyyh@google.com>
2022-08-30 19:09:23 +0000

    diagnostics: Use if check for IsNetworkingInDiagnosticsAppEnabled flag
    The IsNetworkingInDiagnosticsAppEnabled flag in DiagnosticsUI is still on. Will remove this if check together with IsNetworkingInDiagnosticsAppEnabled flag.
    
    Bug: b/242911118
    Change-Id: I39864e858771dadfd2558c0fdf5069de9d8312c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859671
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041110}

--- ash/webui/diagnostics_ui/diagnostics_ui.cc
+++ ash/webui/diagnostics_ui/diagnostics_ui.cc
@@ -443,11 +443,12 @@ DiagnosticsDialogUI::~DiagnosticsDialogUI() {
 
 void DiagnosticsDialogUI::BindInterface(
     mojo::PendingReceiver<diagnostics::mojom::NetworkHealthProvider> receiver) {
-  DCHECK(features::IsNetworkingInDiagnosticsAppEnabled());
-  diagnostics::NetworkHealthProvider* network_health_provider =
-      diagnostics_manager_->GetNetworkHealthProvider();
-  if (network_health_provider) {
-    network_health_provider->BindInterface(std::move(receiver));
+  if (features::IsNetworkingInDiagnosticsAppEnabled()) {
+    diagnostics::NetworkHealthProvider* network_health_provider =
+        diagnostics_manager_->GetNetworkHealthProvider();
+    if (network_health_provider) {
+      network_health_provider->BindInterface(std::move(receiver));
+    }
   }
 }
 
