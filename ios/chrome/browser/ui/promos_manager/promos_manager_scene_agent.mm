cc22f1887cee8a890f76e6267d6764975681de6a
Benjamin Williams <bwwilliams@google.com>
2022-08-30 20:57:53 +0000

    Move PromosManagerSceneAgent to the ui/ layer.
    In https://crrev.com/c/3804910, PromosManagerSceneAgent was introduced,
    but created outside the ui/ layer.
    
    This change moves PromosManagerSceneAgent to a better codebase location
    in the ui/ layer, to avoid to model-ui dependency crossover.
    
    Change-Id: I4ce59b822b964557f61cd31e292c8d17d565149a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3862823
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041170}

--- ios/chrome/browser/ui/promos_manager/promos_manager_scene_agent.mm
+++ ios/chrome/browser/ui/promos_manager/promos_manager_scene_agent.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/promos_manager/promos_manager_scene_agent.h"
+#import "ios/chrome/browser/ui/promos_manager/promos_manager_scene_agent.h"
 
 #import "base/time/time.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
