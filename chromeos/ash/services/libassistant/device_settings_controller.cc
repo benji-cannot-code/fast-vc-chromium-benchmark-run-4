f56f92a9001061ab025a54f403c954cfe3f41383
Peter Kasting <pkasting@chromium.org>
2022-08-30 22:53:45 +0000

    Change calls to std::{all,any,none}_of() to base:: functions: chromeos/
    This generally allows for slightly shorter, simpler calls, and in some
    cases more clearly expresses the intent of the underlying code.
    
    Bug: none
    Change-Id: I68b253cd32b413fd0ac0b86601478152e58f986b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863642
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041231}

--- chromeos/ash/services/libassistant/device_settings_controller.cc
+++ chromeos/ash/services/libassistant/device_settings_controller.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/services/libassistant/device_settings_controller.h"
 
-#include <algorithm>
 #include <memory>
 #include <utility>
 
+#include "base/containers/contains.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/ash/services/libassistant/grpc/assistant_client.h"
@@ -350,10 +350,7 @@ DeviceSettingsController::GetSupportedDeviceSettings(
 
 bool DeviceSettingsController::IsSettingSupported(
     const std::string& setting_id) const {
-  return std::any_of(settings_.begin(), settings_.end(),
-                     [&setting_id](const auto& setting) {
-                       return setting->setting_id() == setting_id;
-                     });
+  return base::Contains(settings_, setting_id, &Setting::setting_id);
 }
 
 void DeviceSettingsController::AddSetting(std::unique_ptr<Setting> setting) {
--- chromeos/printing/ppd_provider.cc
+++ chromeos/printing/ppd_provider.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/printing/ppd_provider.h"
 
-#include <algorithm>
 #include <string>
 #include <utility>
 #include <vector>
@@ -15,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -64,9 +64,8 @@ bool PpdReferenceIsWellFormed(const Printer::PpdReference& reference) {
   // All effective-make-and-model strings should be lowercased, since v2.
   // Since make-and-model strings could include non-Latin chars, only checking
   // that it excludes all upper-case chars A-Z.
-  if (!std::all_of(reference.effective_make_and_model.begin(),
-                   reference.effective_make_and_model.end(),
-                   [](char c) -> bool { return !base::IsAsciiUpper(c); })) {
+  if (!base::ranges::all_of(reference.effective_make_and_model,
+                            [](char c) { return !base::IsAsciiUpper(c); })) {
     return false;
   }
   // Should have exactly one non-empty field.
