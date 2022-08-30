7767108c141f3dd21e75b7d3ca6ccddf4b0fb3c0
Vivian Pao <vpao@google.com>
2022-08-30 04:54:36 +0000

    [Intent Filters] Register PluginVm file intent filters in App Service
    Create and register file intent filters for PluginVm apps in App
    Service. File intent filters for PluginVm will only have an action
    type of "view" and conditions for extension types. These file intent
    filters will ultimately allow Files App to query App Service to find
    and launch PluginVm apps.
    
    Bug: 1340095
    Change-Id: I449c0af7623c9320b5a1b9704ee8279e6921cbfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858922
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040780}

--- chrome/browser/ash/plugin_vm/plugin_vm_test_helper.h
+++ chrome/browser/ash/plugin_vm/plugin_vm_test_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "chromeos/ash/components/dbus/concierge/fake_concierge_client.h"
+#include "chromeos/ash/components/dbus/vm_applications/apps.pb.h"
 
 class TestingProfile;
 
@@ -58,8 +59,18 @@ class PluginVmTestHelper {
   void OpenShelfItem();
   void CloseShelfItem();
 
+  // Adds an app in the default container. Replaces an existing app with the
+  // same app name if one exists.
+  void AddApp(const vm_tools::apps::App& app);
+
+  // Returns the app id that the registry would use for the given app name.
+  static std::string GenerateAppId(const std::string& app_name);
+
  private:
+  void UpdateRegistry();
+
   TestingProfile* testing_profile_;
+  vm_tools::apps::ApplicationList current_apps_;
   std::unique_ptr<user_manager::ScopedUserManager> scoped_user_manager_;
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<base::test::ScopedRunningOnChromeOS> running_on_chromeos_;
