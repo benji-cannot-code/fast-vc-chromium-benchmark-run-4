885ae596af7416d3121d9852431ce644d2e61717
Hongyu Long <hongyulong@chromium.org>
2022-08-30 23:36:31 +0000

    saved_desks: update ChromeVox when entering saved desks library
    This CL updates the ChromeVox announcement when entering the saved desks
    library view. Before this fix, it always refers to "templates". After
    the fix, it would use correct name depending on the feature set. E.g.
    "saved desks and templates" with feature `DesksTemplates` enabled, and
    "saved desks" without it.
    
    Test: Manual & Unittest
    Change-Id: Icd36574338c10fdad4bdb7dba1d3058900484ba7
    Fixed: 1349847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857765
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041247}

--- chrome/browser/ui/ash/accessibility/accessibility_controller_client.cc
+++ chrome/browser/ui/ash/accessibility/accessibility_controller_client.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/accessibility_controller.h"
 #include "ash/public/cpp/accessibility_controller_enums.h"
+#include "ash/wm/desks/templates/saved_desk_util.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/aura/accessibility/automation_manager_aura.h"
@@ -78,8 +79,10 @@ void AccessibilityControllerClient::TriggerAccessibilityAlert(
     case ash::AccessibilityAlert::WORKSPACE_FULLSCREEN_STATE_EXITED:
       msg = IDS_A11Y_ALERT_WORKSPACE_FULLSCREEN_STATE_EXITED;
       break;
-    case ash::AccessibilityAlert::DESK_TEMPLATES_MODE_ENTERED:
-      msg = IDS_A11Y_ALERT_DESK_TEMPLATES_MODE_ENTERED;
+    case ash::AccessibilityAlert::SAVED_DESKS_MODE_ENTERED:
+      msg = ash::saved_desk_util::AreDesksTemplatesEnabled()
+                ? IDS_A11Y_ALERT_SAVED_DESKS_LIBRARY_MODE_ENTERED
+                : IDS_A11Y_ALERT_SAVED_DESKS_SAVED_FOR_LATER_MODE_ENTERED;
       break;
     case ash::AccessibilityAlert::NONE:
       msg = 0;
