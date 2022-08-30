bb974c956aee43b411452264323e26ac61fab92b
Michael Checo <michaelcheco@google.com>
2022-08-30 01:23:49 +0000

    shortcuts: Migrate tests Part 1
    - Move tests from chrome://test/ to the chrome://webui-test/.
    - Add "composite=true" to :build_ts so that other ts_library() targets
      can depend on it.
    - Pass all test files to ts_library() and generate a grdp file.
    
    No files are converted to TS yet, will happen in follow-up CLs.
    
    Bug: b/242549183
    Change-Id: I442efc798a31c871e423509dee6ed5778237afde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848962
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040711}

--- chrome/test/data/webui/chromeos/shortcut_customization/accelerator_subsection_test.js
+++ chrome/test/data/webui/chromeos/shortcut_customization/accelerator_subsection_test.js
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://shortcut-customization/accelerator_subsection.js';
+import 'chrome://webui-test/mojo_webui_test_support.js';
 
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {AcceleratorLookupManager} from 'chrome://shortcut-customization/accelerator_lookup_manager.js';
 import {AcceleratorSubsectionElement} from 'chrome://shortcut-customization/accelerator_subsection.js';
 import {fakeAcceleratorConfig, fakeLayoutInfo} from 'chrome://shortcut-customization/fake_data.js';
 import {Modifier} from 'chrome://shortcut-customization/shortcut_types.js';
-
-import {assertEquals} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.js';
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/test_util.js';
 
 import {CreateUserAccelerator} from './shortcut_customization_test_util.js';
 
