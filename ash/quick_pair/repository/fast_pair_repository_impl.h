c0c425f8da0895e752c00a532ced4b0bd26debd6
Daniel Classon <dclasson@google.com>
2022-08-30 17:01:16 +0000

    [Fast Pair] Save offline Footprints deletes to be retried
    Uses the PendingWriteStore to save Footprints deletes to prefs so that
    they can be retried once the network changes and we are back online.
    
    eventually be removed from Footprints once an Internet connection is
    re-established. Added unit tests to cover additional edge cases.
    
    Test: Tested manually that a headset forgotten when offline will
    Change-Id: I4b46dee0ea3512d5f661aba4c0e07321037cb28c
    Bug: b/221126805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854338
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041038}

--- ash/quick_pair/repository/fast_pair_repository_impl.h
+++ ash/quick_pair/repository/fast_pair_repository_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/time/time.h"
+#include "chromeos/ash/components/network/network_state_handler_observer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace chromeos {
@@ -40,11 +41,13 @@ class DeviceImageStore;
 class DeviceMetadataFetcher;
 class FastPairImageDecoder;
 class FootprintsFetcher;
+class PendingWriteStore;
 class SavedDeviceRegistry;
 
 // The entry point for the Repository component in the Quick Pair system,
 // responsible for connecting to back-end services.
-class FastPairRepositoryImpl : public FastPairRepository {
+class FastPairRepositoryImpl : public FastPairRepository,
+                               public NetworkStateHandlerObserver {
  public:
   FastPairRepositoryImpl();
   FastPairRepositoryImpl(
@@ -54,7 +57,8 @@ class FastPairRepositoryImpl : public FastPairRepository {
       std::unique_ptr<FastPairImageDecoder> image_decoder,
       std::unique_ptr<DeviceIdMap> device_id_map,
       std::unique_ptr<DeviceImageStore> device_image_store,
-      std::unique_ptr<SavedDeviceRegistry> saved_device_registry);
+      std::unique_ptr<SavedDeviceRegistry> saved_device_registry,
+      std::unique_ptr<PendingWriteStore> pending_write_store);
   FastPairRepositoryImpl(const FastPairRepositoryImpl&) = delete;
   FastPairRepositoryImpl& operator=(const FastPairRepositoryImpl&) = delete;
   ~FastPairRepositoryImpl() override;
@@ -85,6 +89,9 @@ class FastPairRepositoryImpl : public FastPairRepository {
   void GetSavedDevices(GetSavedDevicesCallback callback) override;
   void IsDeviceSavedToAccount(const std::string& mac_address,
                               IsDeviceSavedToAccountCallback callback) override;
+
+  // NetworkStateHandlerObserver:
+  void DefaultNetworkChanged(const NetworkState* network) override;
 
  private:
   void CheckAccountKeysImpl(const AccountKeyFilter& account_key_filter,
@@ -134,6 +141,9 @@ class FastPairRepositoryImpl : public FastPairRepository {
       const std::vector<uint8_t>& account_key,
       DeleteAssociatedDeviceByAccountKeyCallback callback,
       bool footprints_removal_success);
+  void RetryPendingDeletes(
+      nearby::fastpair::OptInStatus status,
+      std::vector<nearby::fastpair::FastPairDevice> devices);
   void OnGetSavedDevices(
       GetSavedDevicesCallback callback,
       absl::optional<nearby::fastpair::UserReadDevicesResponse> user_devices);
@@ -158,10 +168,12 @@ class FastPairRepositoryImpl : public FastPairRepository {
   std::unique_ptr<DeviceIdMap> device_id_map_;
   std::unique_ptr<DeviceImageStore> device_image_store_;
   std::unique_ptr<SavedDeviceRegistry> saved_device_registry_;
+  std::unique_ptr<PendingWriteStore> pending_write_store_;
 
   base::flat_map<std::string, std::unique_ptr<DeviceMetadata>> metadata_cache_;
   nearby::fastpair::UserReadDevicesResponse user_devices_cache_;
   base::Time footprints_last_updated_;
+  base::Time retry_write_last_attempted_;
 
   base::WeakPtrFactory<FastPairRepositoryImpl> weak_ptr_factory_{this};
 };
