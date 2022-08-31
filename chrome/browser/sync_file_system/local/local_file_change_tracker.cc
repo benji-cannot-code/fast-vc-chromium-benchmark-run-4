c35ee3ab2b946c308a5be2ba4e4d3926f9a69637
Austin Sullivan <asully@chromium.org>
2022-08-31 19:33:08 +0000

    syncfs: Use make_unique in LocalFileChangeTracker
    Change-Id: I357dfaf89928c135bc7c8df2e325bfcbf7f8a0e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866385
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041670}

--- chrome/browser/sync_file_system/local/local_file_change_tracker.cc
+++ chrome/browser/sync_file_system/local/local_file_change_tracker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/local/local_file_change_tracker.h"
 
 #include <stddef.h>
+#include <memory>
 #include <utility>
 
 #include "base/containers/circular_deque.h"
@@ -82,10 +83,9 @@ LocalFileChangeTracker::LocalFileChangeTracker(
     base::SequencedTaskRunner* file_task_runner)
     : initialized_(false),
       file_task_runner_(file_task_runner),
-      tracker_db_(new TrackerDB(base_path, env_override)),
+      tracker_db_(std::make_unique<TrackerDB>(base_path, env_override)),
       current_change_seq_number_(0),
-      num_changes_(0) {
-}
+      num_changes_(0) {}
 
 LocalFileChangeTracker::~LocalFileChangeTracker() {
   DCHECK(file_task_runner_->RunsTasksInCurrentSequence());
@@ -280,7 +280,7 @@ SyncStatusCode LocalFileChangeTracker::Initialize(
 void LocalFileChangeTracker::ResetForFileSystem(const GURL& origin,
                                                 storage::FileSystemType type) {
   DCHECK(file_task_runner_->RunsTasksInCurrentSequence());
-  std::unique_ptr<leveldb::WriteBatch> batch(new leveldb::WriteBatch);
+  auto batch = std::make_unique<leveldb::WriteBatch>();
   for (auto iter = changes_.begin(); iter != changes_.end();) {
     storage::FileSystemURL url = iter->first;
     int change_seq = iter->second.change_seq;
