c1c0212774f7d6df869295191de1e5952e43164b
Vidhan <vidhanj@google.com>
2022-08-31 16:39:43 +0000

    Stick to overriding by default for the credit card prefilled fields
    With the introduction of kAutofillPreventOverridingPrefilledValues
    feature, the fields that contained a value were not overridden.
    However, if those values are placeholder values, they should be
    overridden by Autofill.
    
    This CL attempts to sanitize the field values and detect if they are
    placeholders.
    We also stick with the legacy behaviour to override the field values
    if they are credit card fields.
    
    Bug: 1275649
    Change-Id: Iadb9fe371899e6878590629e4400a4ee98c36681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834257
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041583}

--- components/autofill/core/browser/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/browser_autofill_manager_unittest.cc
@@ -9373,6 +9373,8 @@ TEST_F(BrowserAutofillManagerTest, PreventOverridingOfPrefilledValues) {
   test::CreateTestFormField("Phone Number", "phonenumber", "12345678901", "tel",
                             &field);
   form.fields.push_back(field);
+  test::CreateTestFormField("Zip Code", "zipcode", "_____", "text", &field);
+  form.fields.push_back(field);
   std::vector<FormData> forms(1, form);
   FormsSeen(forms);
 
@@ -9387,12 +9389,13 @@ TEST_F(BrowserAutofillManagerTest, PreventOverridingOfPrefilledValues) {
   EXPECT_EQ(response_data.fields[2].value, u"Tennessee");
   EXPECT_EQ(response_data.fields[3].value, u"Test Country");
   EXPECT_EQ(response_data.fields[4].value, u"12345678901");
+  EXPECT_EQ(response_data.fields[5].value, u"38116");
 
   {
     FormStructure* form_structure;
     AutofillField* autofill_field;
-    std::vector<std::string> expected_values = {"", "Memphis", "",
-                                                "United States", ""};
+    std::vector<std::string> expected_values = {
+        "", "Memphis", "", "United States", "", ""};
     bool found = browser_autofill_manager_->GetCachedFormAndField(
         form, form.fields[0], &form_structure, &autofill_field);
     ASSERT_TRUE(found);
@@ -9412,11 +9415,14 @@ TEST_F(BrowserAutofillManagerTest, PreventOverridingOfPrefilledValues) {
     EXPECT_TRUE(form_structure->field(0)->is_autofilled);  // No prefilled value
     EXPECT_TRUE(form_structure->field(2)->is_autofilled);  // Selection field.
 
-    // Prefilled value is same as the value to be autofilled so
-    // |value_not_autofilled_over_existing_value_hash| is not set for the field.
+    // Prefilled value is same as the value to be autofilled so the field is
+    // overridden.
     EXPECT_FALSE(form_structure->field(4)->is_autofilled);
     EXPECT_FALSE(form_structure->field(4)
                      ->value_not_autofilled_over_existing_value_hash());
+
+    // Field contained placeholder "______" value.
+    EXPECT_TRUE(form_structure->field(5)->is_autofilled);
   }
 
   features.Reset();
@@ -9431,6 +9437,7 @@ TEST_F(BrowserAutofillManagerTest, PreventOverridingOfPrefilledValues) {
   EXPECT_EQ(response_data.fields[2].value, u"Tennessee");
   EXPECT_EQ(response_data.fields[3].value, u"United States");
   EXPECT_EQ(response_data.fields[4].value, u"12345678901");
+  EXPECT_EQ(response_data.fields[5].value, u"38116");
 }
 
 // Tests that the Autofill does override the prefilled field value since the
@@ -9440,6 +9447,7 @@ TEST_F(BrowserAutofillManagerTest, AutofillOverridePrefilledValue) {
   base::test::ScopedFeatureList features;
   features.InitAndEnableFeature(
       autofill::features::kAutofillPreventOverridingPrefilledValues);
+
   // Set up our form data.
   FormData form;
   form.name = u"MyForm";
