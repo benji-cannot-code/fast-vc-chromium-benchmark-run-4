741d854630bb556669ecbbb02ed1abf7868bd2a2
Aashna Sheth <aashnas@google.com>
2022-08-31 18:01:24 +0000

    [TextBasedAD] Add OnExit() to handle the completion of AD vocalization
    In this CL, we create an OnExit() event which pauses the video if the
    audio description text is not finished speaking at the given exit
    timestamp.
    
    Change-Id: I99e92aa40c1cd1fa00640783f3f8f70ed9c07974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857114
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041616}

--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -4112,6 +4112,8 @@ SpeechSynthesisBase* HTMLMediaElement::SpeechSynthesis() {
   if (!speech_synthesis_) {
     speech_synthesis_ =
         SpeechSynthesisBase::Create(*(GetDocument().domWindow()));
+    speech_synthesis_->SetOnSpeakingCompletedCallback(WTF::BindRepeating(
+        &HTMLMediaElement::OnSpeakingCompleted, WrapWeakPersistent(this)));
   }
   return speech_synthesis_;
 }
@@ -4336,6 +4338,11 @@ void HTMLMediaElement::BindMediaPlayerReceiver(
   media_player_receiver_set_->Value().Add(
       std::move(receiver),
       GetDocument().GetTaskRunner(TaskType::kInternalMedia));
+}
+
+void HTMLMediaElement::OnSpeakingCompleted() {
+  if (paused())
+    Play();
 }
 
 void HTMLMediaElement::Trace(Visitor* visitor) const {
