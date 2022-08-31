55738db1d164e0794dece1406f83ee1e05374650
Norge Vizcay <vizcay@google.com>
2022-08-31 14:50:52 +0000

    [FastCheckout] Fix: Force hiding the sheet on destroy.
    If `Stop` is called on the client and the bottom sheet is showing, the bridge will get destroyed, but the bottom sheet will stay opened.
    
    Bug: 1334642
    Change-Id: I012c99020fc536a88e445b162e41fe3dfd4dfb8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3844886
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041524}

--- chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutBridge.java
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutBridge.java
@@ -71,6 +71,7 @@ class FastCheckoutBridge implements FastCheckoutComponent.Delegate {
     @CalledByNative
     private void destroy() {
         mNativeFastCheckoutBridge = 0;
+        mFastCheckoutComponent.destroy();
     }
 
     @Override
--- chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutCoordinator.java
+++ chrome/browser/ui/android/fast_checkout/internal/java/src/org/chromium/chrome/browser/ui/fast_checkout/FastCheckoutCoordinator.java
@@ -22,11 +22,13 @@ class FastCheckoutCoordinator implements FastCheckoutComponent {
     private FastCheckoutMediator mMediator = new FastCheckoutMediator();
     private PropertyModel mModel = FastCheckoutProperties.createDefaultModel();
     private FastCheckoutSheetContent mContent;
+    private BottomSheetController mBottomSheetController;
 
     @Override
     public void initialize(Context context, BottomSheetController sheetController,
             FastCheckoutComponent.Delegate delegate) {
-        mMediator.initialize(delegate, mModel, sheetController);
+        mBottomSheetController = sheetController;
+        mMediator.initialize(delegate, mModel, mBottomSheetController);
 
         ViewFlipper rootView = (ViewFlipper) LayoutInflater.from(context).inflate(
                 R.layout.fast_checkout_bottom_sheet, null);
@@ -79,5 +81,10 @@ class FastCheckoutCoordinator implements FastCheckoutComponent {
     public void showOptions(
             FastCheckoutAutofillProfile[] profiles, FastCheckoutCreditCard[] creditCards) {
         mMediator.showOptions(profiles, creditCards);
+    }
+
+    @Override
+    public void destroy() {
+        mMediator.destroy();
     }
 }
