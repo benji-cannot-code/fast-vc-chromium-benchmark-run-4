0fd2cb7942d649439fd0c14f0b7e0a18bfb33743
Tommy C. Li <tommycli@chromium.org>
2022-08-31 18:00:02 +0000

    [history] Add enum safety to reading VisitSource from the SQL db
    Bug: 1256979
    Change-Id: Iecb0449e2f8fdae4f755a67bf0bb234efb9124c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864871
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041615}

--- components/history/core/browser/visit_database.cc
+++ components/history/core/browser/visit_database.cc
@@ -97,6 +97,22 @@ bool TransitionIsVisible(int32_t transition) {
                                        ui::PAGE_TRANSITION_KEYWORD_GENERATED);
 }
 
+VisitSource VisitSourceFromInt(int value) {
+  auto converted = static_cast<VisitSource>(value);
+  // Verify that `converted` is actually a valid enum value.
+  switch (converted) {
+    case SOURCE_SYNCED:
+    case SOURCE_BROWSED:
+    case SOURCE_EXTENSION:
+    case SOURCE_FIREFOX_IMPORTED:
+    case SOURCE_IE_IMPORTED:
+    case SOURCE_SAFARI_IMPORTED:
+      return converted;
+  }
+  // In cases of database corruption, SOURCE_BROWSED is a safe default value.
+  return SOURCE_BROWSED;
+}
+
 }  // namespace
 
 VisitDatabase::VisitDatabase() = default;
@@ -958,8 +974,7 @@ void VisitDatabase::GetVisitsSource(const VisitVector& visits,
     // Get the source entries out of the query result.
     while (statement.Step()) {
       std::pair<VisitID, VisitSource> source_entry(
-          statement.ColumnInt64(0),
-          static_cast<VisitSource>(statement.ColumnInt(1)));
+          statement.ColumnInt64(0), VisitSourceFromInt(statement.ColumnInt(1)));
       sources->insert(source_entry);
     }
   }
@@ -971,7 +986,7 @@ VisitSource VisitDatabase::GetVisitSource(const VisitID visit_id) {
   statement.BindInt64(0, visit_id);
   if (!statement.Step())
     return VisitSource::SOURCE_BROWSED;
-  return static_cast<VisitSource>(statement.ColumnInt(0));
+  return VisitSourceFromInt(statement.ColumnInt(0));
 }
 
 std::vector<DomainVisit>
