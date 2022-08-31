f77c2a6d145872fa8934152d91b322869422c4a7
Nancy Wang <nancylingwang@chromium.org>
2022-08-31 19:09:06 +0000

    Enable the kAppServiceGetMenuWithoutMojom flag.
    BUG=1253250
    
    Change-Id: Ia876a5499a212c0de7c9b02a243f3d3a10d35512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865349
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041655}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -23,6 +23,6 @@ const base::Feature kAppServiceWithoutMojom{"AppServiceWithoutMojom",
                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAppServiceGetMenuWithoutMojom{
-    "AppServiceGetMenuWithoutMojom", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AppServiceGetMenuWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace apps
