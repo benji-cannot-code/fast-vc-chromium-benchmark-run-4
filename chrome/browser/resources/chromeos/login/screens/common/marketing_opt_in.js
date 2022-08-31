61166d2e98db6d6293bfcafa6a5f168c79dd46a8
Brahim Chikhaoui <bchikhaoui@google.com>
2022-08-31 10:07:27 +0000

    OOBE - Polymer3 - Check-in marketing opt in
    Check-in the modularised version of OOBE's Marketing_Opt_In screen.
    
    Bug: 1184731, b/243383416
    Change-Id: I79e091b1cee4fdd2a895209b4866ca4ccdedc1fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849255
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041451}

--- chrome/browser/resources/chromeos/login/screens/common/marketing_opt_in.js
+++ chrome/browser/resources/chromeos/login/screens/common/marketing_opt_in.js
@@ -8,7 +8,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * opt-in screen.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/iron-iconset-svg/iron-iconset-svg.js';
+import '//resources/cr_elements/cr_toggle/cr_toggle.js';
+import '../../components/oobe_cr_lottie.m.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OobeBackButton} from '../../components/buttons/oobe_back_button.m.js';
+import {OobeIconButton} from '../../components/buttons/oobe_icon_button.m.js';
+import {OobeTextButton} from '../../components/buttons/oobe_text_button.m.js';
+import {OOBE_UI_STATE, SCREEN_GAIA_SIGNIN} from '../../components/display_manager_types.m.js';
+import {OobeA11yOption} from '../../components/oobe_a11y_option.m.js';
+
 
 /**
  * @constructor
@@ -16,9 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {LoginScreenBehaviorInterface}
  * @implements {MultiStepBehaviorInterface}
  */
-const MarketingScreenElementBase = Polymer.mixinBehaviors(
-    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior],
-    Polymer.Element);
+const MarketingScreenElementBase = mixinBehaviors(
+    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior], PolymerElement);
 
 /**
  * Enum to represent each page in the marketing opt in screen.
@@ -58,7 +76,9 @@ class MarketingOptIn extends MarketingScreenElementBase {
     return 'marketing-opt-in-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
