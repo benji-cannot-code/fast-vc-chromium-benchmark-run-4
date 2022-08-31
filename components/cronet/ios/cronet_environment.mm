b4e3c61b50ea4da9e90942ad30bb8fbafb226378
Paul Semel <paulsemel@chromium.org>
2022-08-31 07:19:42 +0000

    DanglingPtr: fix dangling ptr in URLRequestContext
    Removed indirections to build URLRequestContext. It seems that those
    indirection were needed before, but some recent clean-ups made those
    less useful than they used to be. Having URLRequestContext directly own
    the context avoids memory management issues.
    URLRequestContext is not derived anymore.
    
    Bug: 1291138, 1282361
    Change-Id: I0199c83b3e20702ca72aa905a83e75aa22ef73e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856719
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041395}

--- components/cronet/ios/cronet_environment.mm
+++ components/cronet/ios/cronet_environment.mm
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quiche/src/quiche/quic/core/quic_versions.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
-#include "net/url_request/url_request_context_storage.h"
 #include "url/scheme_host_port.h"
 #include "url/url_util.h"
 
--- net/url_request/url_request_test_util.h
+++ net/url_request/url_request_test_util.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
-#include "net/url_request/url_request_context_storage.h"
 #include "net/url_request/url_request_interceptor.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/url_util.h"
