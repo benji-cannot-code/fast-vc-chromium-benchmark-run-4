9e6ddda8688c586f1e615657aab89301ac557759
Nancy Wang <nancylingwang@chromium.org>
2022-08-31 18:07:20 +0000

    Fix the broken browser tests when enable the non mojom flag.
    When enable the flag kAppServiceGetMenuWithoutMojom, AppService gets
    the app menu items sync, so we don't need to wait for menu items.
    Otherwise, it may break some browser tests when enable the flag.
    https://ci.chromium.org/p/chromium/builders/try/linux-chromeos-rel/1296008?
    
    BUG=1253250
    
    Change-Id: I38d22eda441d42532feb4df9734ed70f94f3adba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864297
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041620}

--- ash/app_list/app_list_test_api.cc
+++ ash/app_list/app_list_test_api.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/layer_animation_stopped_waiter.h"
 #include "base/callback.h"
 #include "base/run_loop.h"
+#include "components/services/app_service/public/cpp/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/layer.h"
@@ -187,7 +188,10 @@ views::MenuItemView* ShowRootMenuAndReturn(
       if (is_folder_item) {
         root_menu = item_view->context_menu_for_folder()->root_menu_item_view();
       } else {
-        WaitUntilItemMenuShown(item_view);
+        if (!base::FeatureList::IsEnabled(
+                apps::kAppServiceGetMenuWithoutMojom)) {
+          WaitUntilItemMenuShown(item_view);
+        }
         ash::AppListMenuModelAdapter* menu_model_adapter =
             item_view->item_menu_model_adapter();
         root_menu = menu_model_adapter->root_for_testing();
