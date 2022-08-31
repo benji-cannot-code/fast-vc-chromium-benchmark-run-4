3b9f67f60914f8e8da6e86b862d83daed817318b
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-08-31 21:57:14 +0000

    Add enterprise policy for tab discard exceptions
    This policy-controlled pref works together with the user-controlled preference to allow admins to specify a list of URL patterns that should be excluded from tab discarding. Users can add to that list via the user-controlled preference, but they can not modify/remove URL patterns from the admin specified list.
    
    Bug: 1302372,1355674
    Change-Id: Ide93df8238e46cc64dd0ae3341ef7d2e793262ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852365
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041754}

--- components/performance_manager/user_tuning/prefs.cc
+++ components/performance_manager/user_tuning/prefs.cc
@@ -20,6 +20,9 @@ const char kBatterySaverModeState[] =
 const char kTabDiscardingExceptions[] =
     "performance_tuning.tab_discarding.exceptions";
 
+const char kManagedTabDiscardingExceptions[] =
+    "performance_tuning.tab_discarding.exceptions.managed";
+
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kHighEfficiencyModeEnabled, false);
   registry->RegisterIntegerPref(
@@ -29,6 +32,7 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterListPref(kTabDiscardingExceptions);
+  registry->RegisterListPref(kManagedTabDiscardingExceptions);
 }
 
 BatterySaverModeState GetCurrentBatterySaverModeState(
