4c55712e5cc2410e0db58fbfee179f16c3d5a1eb
Peter Kvitek <kvitekp@chromium.org>
2022-08-31 19:29:37 +0000

    [headless] Add ShutDownInProcessRenderer() call for CDP browser close.
    RenderProcessHost::ShutDownInProcessRenderer() is required when
    closing the browser when --single-process mode is in effect. This
    was not the case when handling browser.close() DevTools command.
    
    Bug: 1356885
    Change-Id: I395e4607169fd5c05f8b3d6a4768eedae45af590
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863897
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041666}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/app/content_main.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_switches.h"
 #include "headless/app/headless_shell.h"
 #include "headless/app/headless_shell_switches.h"
@@ -333,8 +332,6 @@ void HeadlessShell::ShutdownSoon() {
 
 void HeadlessShell::Shutdown() {
   DCHECK(!web_contents_);
-  if (content::RenderProcessHost::run_renderer_in_process())
-    content::RenderProcessHost::ShutDownInProcessRenderer();
   browser_->Shutdown();
 }
 
