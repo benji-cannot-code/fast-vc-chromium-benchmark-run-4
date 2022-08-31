58b79c16e61f9d76d40d8cf2b1c05b6bf7210383
CJ Huang <chenjih@google.com>
2022-08-31 02:09:23 +0000

    [string match] add diacritic stripping
    add diacritic stripping to fuzzy tokenized string match with flag to
    enable/disable. Additional unittest is added to ensure it works as expected.
    
    Bug: 1347334
    Change-Id: Ie2c7e7b153d0fdd1441b8b2f9c1a15fe6893823a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863076
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Auto-Submit: CJ Huang <chenjih@google.com>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041331}

--- chromeos/ash/components/string_matching/fuzzy_tokenized_string_match.cc
+++ chromeos/ash/components/string_matching/fuzzy_tokenized_string_match.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/ash/components/string_matching/diacritic_utils.h"
 #include "chromeos/ash/components/string_matching/prefix_matcher.h"
 #include "chromeos/ash/components/string_matching/sequence_matcher.h"
 
@@ -212,11 +213,24 @@ double FuzzyTokenizedStringMatch::PrefixMatcher(const TokenizedString& query,
   return 1.0 - std::pow(0.5, match.relevance());
 }
 
-double FuzzyTokenizedStringMatch::Relevance(const TokenizedString& query,
-                                            const TokenizedString& text,
-                                            bool use_weighted_ratio) {
-  // If there is an exact match, relevance will be 1.0 and there is only 1 hit
-  // that is the entire text/query.
+double FuzzyTokenizedStringMatch::Relevance(const TokenizedString& query_input,
+                                            const TokenizedString& text_input,
+                                            bool use_weighted_ratio,
+                                            bool strip_diacritics) {
+  absl::optional<TokenizedString> stripped_query;
+  absl::optional<TokenizedString> stripped_text;
+  if (strip_diacritics) {
+    stripped_query.emplace(RemoveDiacritics(query_input.text()));
+    stripped_text.emplace(RemoveDiacritics(text_input.text()));
+  }
+
+  const TokenizedString& query =
+      strip_diacritics ? stripped_query.value() : query_input;
+  const TokenizedString& text =
+      strip_diacritics ? stripped_text.value() : text_input;
+
+  // If there is an exact match, relevance will be 1.0 and there is only 1
+  // hit that is the entire text/query.
   const auto& query_text = query.text();
   const auto& text_text = text.text();
   const auto query_size = query_text.size();
