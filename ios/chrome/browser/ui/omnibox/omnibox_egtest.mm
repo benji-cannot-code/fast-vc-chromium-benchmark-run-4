66a10c4466bbe9ebbdc4f73b8210f07be9c4d599
Stepan Khapugin <stkhapugin@chromium.org>
2022-08-31 16:03:15 +0000

    [iOS] Remove extra tests for SwiftUI popup.
    Remove the extraneous tests that ran with SwiftUI popup flag on.
    Keep the UIKit variation 2 (the only different one) for omnibox' own
    steady state test.
    
    Bug: none
    Change-Id: I3195a60f9d99b1914e446f60c731f5d3b3c0fc28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864548
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041568}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -546,7 +546,7 @@ id<GREYMatcher> SearchCopiedTextButton() {
 @implementation NewOmniboxPopupLocationBarSteadyStateVariant1TestCase
 
 - (void)setUp {
-  _variant = std::string(kIOSOmniboxUpdatedPopupUIVariation1);
+  _variant = std::string(kIOSOmniboxUpdatedPopupUIVariation1UIKit);
 
   // `appConfigurationForTestCase` is called during [super setUp], and
   // depends on _variant.
@@ -568,7 +568,7 @@ id<GREYMatcher> SearchCopiedTextButton() {
 @implementation NewOmniboxPopupLocationBarSteadyStateVariant2TestCase
 
 - (void)setUp {
-  _variant = std::string(kIOSOmniboxUpdatedPopupUIVariation2);
+  _variant = std::string(kIOSOmniboxUpdatedPopupUIVariation2UIKit);
 
   // `appConfigurationForTestCase` is called during [super setUp], and
   // depends on _variant.
