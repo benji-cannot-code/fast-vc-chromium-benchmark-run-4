6da78052fa61fed15489cb1b5f3641eb05ae58ad
dpapad <dpapad@chromium.org>
2022-08-31 21:13:50 +0000

    WebUI: Allow producing scheme-relative URLs in css_to_wrapper().
    This is necessary since some files (for example [1]) are used from
    chrome-untrusted:// contexts.
    
    Unfortunately can't always produce scheme-relative URLs because in other
    cases the scheme must be "chrome://", while the enclosing context is
    "chrome-extension://". This was the reason the original attempt at [2]
    was reverted.
    
    [1] ui/webui/resources/cr_elements/shared_style_css.m.js
    [2] https://crrev.com/c/3863002
    
    Bug: 1322682
    Change-Id: I46aef5dccf0b375ed75d094d6d3fa200e43da276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866974
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041729}

--- tools/polymer/css_to_wrapper.py
+++ tools/polymer/css_to_wrapper.py
@@ -28,9 +28,10 @@ _METADATA_START_REGEX = '#css_wrapper_metadata_start'
 _METADATA_END_REGEX = '#css_wrapper_metadata_end'
 _IMPORT_REGEX = '#import='
 _INCLUDE_REGEX = '#include='
+_SCHEME_REGEX = '#scheme='
 _TYPE_REGEX = '#type='
 
-_STYLE_TEMPLATE = """import \'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js\';
+_STYLE_TEMPLATE = """import \'%(scheme)s//resources/polymer/v3_0/polymer/polymer_bundled.min.js\';
 %(imports)s
 
 const styleMod = document.createElement(\'dom-module\');
@@ -43,7 +44,7 @@ styleMod.innerHTML = `
 `;
 styleMod.register(\'%(id)s\');"""
 
-_VARS_TEMPLATE = """import \'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js\';
+_VARS_TEMPLATE = """import \'%(scheme)s//resources/polymer/v3_0/polymer/polymer_bundled.min.js\';
 %(imports)s
 
 const $_documentContainer = document.createElement('template');
@@ -91,7 +92,7 @@ def _extract_metadata(css_file):
   metadata_start_line = -1
   metadata_end_line = -1
 
-  metadata = {'type': None}
+  metadata = {'type': None, 'scheme': 'default'}
 
   with io.open(css_file, encoding='utf-8', mode='r') as f:
     lines = f.read().splitlines()
@@ -117,12 +118,14 @@ def _extract_metadata(css_file):
                   'imports': [],
                   'include': None,
                   'metadata_end_line': -1,
+                  'scheme': metadata['scheme'],
                   'type': type,
               }
             elif type == 'vars':
               metadata = {
                   'imports': [],
                   'metadata_end_line': -1,
+                  'scheme': metadata['scheme'],
                   'type': type,
               }
 
@@ -130,6 +133,13 @@ def _extract_metadata(css_file):
           _parse_style_line(line, metadata)
         elif metadata['type'] == 'vars':
           _parse_vars_line(line, metadata)
+
+        if metadata['scheme'] == 'default':
+          scheme_match = re.search(_SCHEME_REGEX, line)
+          if scheme_match:
+            scheme = line[scheme_match.end():]
+            assert scheme in ['chrome', 'relative']
+            metadata['scheme'] = scheme
 
         if _METADATA_END_REGEX in line:
           assert metadata_start_line > -1
@@ -195,6 +205,13 @@ def main(argv):
     content = _extract_content(path.join(wrapper_in_folder, in_file), metadata,
                                args.minify)
 
+    # Extract the URL scheme that should be used for absolute URL imports.
+    scheme = None
+    if metadata['scheme'] in ['default', 'chrome']:
+      scheme = 'chrome:'
+    elif metadata['scheme'] == 'relative':
+      scheme = ''
+
     wrapper = None
     if metadata['type'] == 'style':
       include = ''
@@ -207,11 +224,13 @@ def main(argv):
           'content': content,
           'include': include,
           'id': metadata['id'],
+          'scheme': scheme,
       }
     elif metadata['type'] == 'vars':
       wrapper = _VARS_TEMPLATE % {
           'imports': _urls_to_imports(metadata['imports']),
           'content': content,
+          'scheme': scheme,
       }
 
     assert wrapper
---
+++ tools/polymer/tests/css_to_wrapper/foo_relative_style_expected.css.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import './other1.css.js';
+import './other2.css.js';
+
+const styleMod = document.createElement('dom-module');
+styleMod.innerHTML = `
+  <template>
+    <style include="other1 other2">
+div {
+  font-size: 2rem;
+  --foo-bar: calc(var(--foo-bar1)
+      - var(--foo-bar2)
+      - 3 * var(--foo-bar3));
+}
+    </style>
+  </template>
+`;
+styleMod.register('foo-relative-style');
