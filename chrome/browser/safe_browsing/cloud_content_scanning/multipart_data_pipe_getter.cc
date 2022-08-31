0ecd7927804ba956a4a059170446aa1cd2732de8
Steffen Seckler <sseckler@google.com>
2022-08-31 08:19:38 +0000

    Fix uploads not working on arc directories
    MemoryMappedFile (posix) always uses MAP_SHARED.
    Using mmap in shared mode is not supported on some file systems (e.g.,
    fuse-io in direct-io mode which is, e.g., used by ARC on ChromeOS):
    https://www.kernel.org/doc/Documentation/filesystems/fuse-io.txt
    For READ-ONLY access the MAP_PRIVATE and MAP_SHARED mostly behave
    similarly, s.t., we can retry access with MAP_PRIVATE if MAP_SHARED
    fails.
    So on posix systems, we create our own MemoryMappedFile that can execute
    mmap calls using MAP_PRIVATE.
    
    Fixed: 1347488
    Change-Id: I3957eb776dfa8f0857a4831ef21c023ee3de2ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865950
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Steffen Seckler <sseckler@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041411}

--- chrome/browser/safe_browsing/cloud_content_scanning/multipart_data_pipe_getter.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/multipart_data_pipe_getter.cc
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/c/system/data_pipe.h"
 #include "net/base/net_errors.h"
 
+#if BUILDFLAG(IS_POSIX)
+#include <sys/mman.h>
+
+#include "base/threading/scoped_blocking_call.h"
+#endif
+
 namespace safe_browsing {
 
 namespace {
@@ -25,6 +31,71 @@ constexpr int64_t kMaxSize = 32 * 1024;
 
 }  // namespace
 
+#if BUILDFLAG(IS_POSIX)
+bool MultipartDataPipeGetter::InternalMemoryMappedFile::Initialize(
+    base::File file) {
+  if (IsValid())
+    return false;
+
+  file_ = std::move(file);
+
+  if (!DoInitialize()) {
+    CloseHandles();
+    return false;
+  }
+
+  return true;
+}
+
+bool MultipartDataPipeGetter::InternalMemoryMappedFile::DoInitialize() {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+
+  int64_t file_len = file_.GetLength();
+  if (file_len < 0) {
+    DPLOG(ERROR) << "fstat " << file_.GetPlatformFile();
+    return false;
+  }
+  if (!base::IsValueInRangeForNumericType<size_t>(file_len))
+    return false;
+  length_ = static_cast<size_t>(file_len);
+
+  data_ = static_cast<uint8_t*>(mmap(nullptr, length_, PROT_READ, MAP_SHARED,
+                                     file_.GetPlatformFile(), 0));
+  if (data_ == MAP_FAILED) {
+    // Retry with MAP_PRIVATE mode.
+    // Some file systems do not support MAP_SHARED. Here, it is acceptable to
+    // use MAP_PRIVATE instead. Note: For MAP_PRIVATE, it is unspecified whether
+    // changes to the underlying file are carried through to the mapped region
+    // after the mmap call.
+    data_ = static_cast<uint8_t*>(mmap(nullptr, length_, PROT_READ, MAP_PRIVATE,
+                                       file_.GetPlatformFile(), 0));
+  }
+
+  if (data_ == MAP_FAILED) {
+    DPLOG(ERROR) << "Upload failure: The creation of a memory mapped file "
+                    "failed for file "
+                 << file_.GetPlatformFile();
+    return false;
+  }
+
+  return true;
+}
+
+void MultipartDataPipeGetter::InternalMemoryMappedFile::CloseHandles() {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+
+  if (data_ != nullptr)
+    munmap(data_, length_);
+  file_.Close();
+
+  data_ = nullptr;
+  length_ = 0;
+}
+
+#endif  // BUILDFLAG(IS_POSIX)
+
 // static
 std::unique_ptr<MultipartDataPipeGetter> MultipartDataPipeGetter::Create(
     const std::string& boundary,
@@ -33,7 +104,7 @@ std::unique_ptr<MultipartDataPipeGetter> MultipartDataPipeGetter::Create(
   if (!file.IsValid())
     return nullptr;
 
-  auto mm_file = std::make_unique<base::MemoryMappedFile>();
+  auto mm_file = std::make_unique<InternalMemoryMappedFile>();
   if (!mm_file->Initialize(std::move(file)))
     return nullptr;
 
@@ -60,7 +131,7 @@ std::unique_ptr<MultipartDataPipeGetter> MultipartDataPipeGetter::Create(
 MultipartDataPipeGetter::MultipartDataPipeGetter(
     const std::string& boundary,
     const std::string& metadata,
-    std::unique_ptr<base::MemoryMappedFile> file)
+    std::unique_ptr<InternalMemoryMappedFile> file)
     : MultipartDataPipeGetter(boundary,
                               metadata,
                               std::move(file),
@@ -81,7 +152,7 @@ MultipartDataPipeGetter::MultipartDataPipeGetter(
 MultipartDataPipeGetter::MultipartDataPipeGetter(
     const std::string& boundary,
     const std::string& metadata,
-    std::unique_ptr<base::MemoryMappedFile> file,
+    std::unique_ptr<InternalMemoryMappedFile> file,
     base::ReadOnlySharedMemoryMapping page)
     : file_(std::move(file)), page_(std::move(page)) {
   metadata_ = base::StrCat({"--", boundary, "\r\n", kDataContentType,
@@ -121,7 +192,8 @@ void MultipartDataPipeGetter::Reset() {
   write_position_ = 0;
 }
 
-std::unique_ptr<base::MemoryMappedFile> MultipartDataPipeGetter::ReleaseFile() {
+std::unique_ptr<MultipartDataPipeGetter::InternalMemoryMappedFile>
+MultipartDataPipeGetter::ReleaseFile() {
   return std::move(file_);
 }
 
