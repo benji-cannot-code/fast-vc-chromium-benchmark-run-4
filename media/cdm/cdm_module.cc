8308805e71abd71914f4c7ea9322707df3c28d6b
Xiaohan Wang <xhwang@chromium.org>
2022-08-31 00:27:33 +0000

    media: Report CDM version to trace event
    This is useful to understand which CDM is actually loaded, e.g. in case
    there are multiple CDMs and we cannot fully trust the manifest file in
    testing.
    
    Bug: b/242110958
    Test: Manually tested and verified the trace
    Change-Id: If6a3c3eaa5756320da7b030c99bdac8b1162e4e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860612
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041285}

--- media/cdm/cdm_module.cc
+++ media/cdm/cdm_module.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/crash/core/common/crash_key.h"
 
@@ -173,6 +174,7 @@ bool CdmModule::Initialize(const base::FilePath& cdm_path) {
   // In case of crashes, provide CDM version to facilitate investigation.
   std::string cdm_version = get_cdm_version_func_();
   DVLOG(2) << __func__ << ": cdm_version = " << cdm_version;
+  TRACE_EVENT1("media", "CdmModule::Initialize", "cdm_version", cdm_version);
 
   static crash_reporter::CrashKeyString<32> cdm_version_key("cdm-version");
   cdm_version_key.Set(cdm_version);
