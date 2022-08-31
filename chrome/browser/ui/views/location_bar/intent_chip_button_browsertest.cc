77fc1c4dcaed1c5d91e7a98e0343cf26e4301b6c
Eric Willigers <ericwilligers@chromium.org>
2022-08-31 08:30:36 +0000

    IntentChipButtonBrowserTest: Wait for registration
    InstallOverlappingApp() now waits for the web app to be registered
    with the App Service. This avoids flakiness on Lacros.
    
    Bug: 1358200
    Change-Id: Idc6025a7965d038c449711b30373e691989294d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863077
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041414}

--- chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
+++ chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/intent_chip_button.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
+#include "chrome/browser/web_applications/test/app_registration_waiter.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
 #include "chrome/browser/web_applications/web_app_id.h"
@@ -131,6 +132,8 @@ class IntentChipButtonBrowserTest
 
     overlapping_app_id_ =
         web_app::test::InstallWebApp(profile(), std::move(web_app_info));
+    DCHECK(!overlapping_app_id_.empty());
+    web_app::AppRegistrationWaiter(profile(), overlapping_app_id_).Await();
   }
 
 #if BUILDFLAG(IS_CHROMEOS)
