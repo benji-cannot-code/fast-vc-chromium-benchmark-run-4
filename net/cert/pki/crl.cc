6daac68f62ecd97b6fb267717a7277a4fcf213f3
Daniel Cheng <dcheng@chromium.org>
2022-08-31 15:13:02 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //net/cert.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=mattm@chromium.org
    
    Bug: 1356184
    Change-Id: I33f8409bd02ca84aa0d07715ed7e97fcd2406e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860809
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041535}

--- net/cert/pki/crl.cc
+++ net/cert/pki/crl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/pki/crl.h"
 
 #include "base/stl_util.h"
+#include "base/types/optional_util.h"
 #include "net/cert/pki/cert_errors.h"
 #include "net/cert/pki/revocation_util.h"
 #include "net/cert/pki/signature_algorithm.h"
@@ -423,10 +424,9 @@ CRLRevocationStatus CheckCRL(base::StringPiece raw_crl,
 
   // Check CRL dates. Roughly corresponds to 6.3.3 (a) (1) but does not attempt
   // to update the CRL if it is out of date.
-  if (!CheckRevocationDateValid(
-          tbs_cert_list.this_update,
-          base::OptionalOrNullptr(tbs_cert_list.next_update), verify_time,
-          max_age)) {
+  if (!CheckRevocationDateValid(tbs_cert_list.this_update,
+                                base::OptionalToPtr(tbs_cert_list.next_update),
+                                verify_time, max_age)) {
     return CRLRevocationStatus::UNKNOWN;
   }
 
