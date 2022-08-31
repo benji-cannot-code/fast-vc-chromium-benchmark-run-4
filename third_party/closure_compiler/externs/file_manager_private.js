04a5979c72eb76316ea1e479b765d3a930ef49c0
Ben Reich <benreich@chromium.org>
2022-08-31 23:01:47 +0000

    [FilesTrash] Plumb through show_notification to startIOTask API layer
    As of CL:3859042 IOTasks can be silenced by providing a
    `show_notification` boolean in the constructor. This plumbs through the
    boolean from the fileManagerPrivate layer to enable tasks started via
    startIOTask to not show their notifications.
    
    Bug: b:243879378
    Test: CL:3867014
    Change-Id: I72e74d4abe793c630aee86b21ddfb95747b29333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867013
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041788}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -729,6 +729,7 @@ chrome.fileManagerPrivate.GetVolumeRootOptions;
  * @typedef {{
  *   destinationFolder: (DirectoryEntry|undefined),
  *   password: (string|undefined),
+ *   showNotification: (boolean|undefined),
  * }}
  */
 chrome.fileManagerPrivate.IOTaskParams;
