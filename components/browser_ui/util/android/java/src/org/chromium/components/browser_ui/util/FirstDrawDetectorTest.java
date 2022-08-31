b68b4cd3bc4dd0d6cecaf916e83fc2f99e3a5279
Sky Malice <skym@chromium.org>
2022-08-31 15:34:08 +0000

    UrlBar uses global draw to reset focusable.
    When Chrome starts up onto the NTP, the toolbar will be hidden. This
    is problematic for the UrlBar because it needs an initial draw to
    fix its focusable state. Lower end phones seemed to be losing this
    race and never got a draw for the UrlBar. This change switches the
    draw observation mechanism to work even when UrlBar is not visible.
    
    Bug: 1344819
    Change-Id: Id1c167af05d06deaf5709b8faabc7fa0a268049f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860104
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041550}

--- components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/FirstDrawDetectorTest.java
+++ components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/FirstDrawDetectorTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.init;
+package org.chromium.components.browser_ui.util;
 
 import android.view.View;
 import android.widget.FrameLayout;
@@ -15,14 +15,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseActivityTestRule;
-import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.test.util.BlankUiTestActivity;
 
 import java.util.concurrent.CountDownLatch;
 
 /** Tests for {@link FirstDrawDetector}. */
-@RunWith(ChromeJUnit4ClassRunner.class)
+@RunWith(BaseJUnit4ClassRunner.class)
 public class FirstDrawDetectorTest {
     @Rule
     public BaseActivityTestRule<BlankUiTestActivity> mActivityTestRule =
