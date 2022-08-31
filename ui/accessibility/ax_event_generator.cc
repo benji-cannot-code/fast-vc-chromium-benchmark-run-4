3cf8e48c17c96a6265dbadf7f45968e7e6a59d7e
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2022-08-31 14:01:49 +0000

    Do not clear events for a node that's going to be reparented.
    When a node is going to be reparented, we used to clear any events
    associated with it. This was to address https://crrev.com/c/746144.
    
    But, since the reparented object is not recreated, we are still
    expected to report state changes on the reparented object, to keep it
    in sync with the corresponding accessible object in the system.
    
    That was the cause of this bug: due to a reparenting operation
    that should have been transparent to the user, the state change
    events for the node with the aria-expanded attribute were cleared
    and the system was left with the previous expanded/collapsed state.
    
    The root cause of https://crrev.com/c/746144 was access to an invalid
    pointer to an AXNode stored inside an event object, but this is not
    possible any more because events now store IDs and not pointers. We
    can now revert the code that clears any events, it was probably an
    overkill and, in the same way it caused this problem, it could
    potentially cause others like related with missing value change events.
    
    We finally reenable the regression test for https://crrev.com/c/746144
    and turn it into a crash test with no expectations, hopefully
    addressing its flakiness.
    
    Bug: 1342694
    Change-Id: If24c90d8c317db694a0920e5f6a8bc1d96f8b988
    AX-relnotes: Fix accordion expanded state sometimes not being updated.
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856270
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041496}

--- ui/accessibility/ax_event_generator.cc
+++ ui/accessibility/ax_event_generator.cc
@@ -788,7 +788,6 @@ void AXEventGenerator::OnSubtreeWillBeDeleted(AXTree* tree, AXNode* node) {
 
 void AXEventGenerator::OnNodeWillBeReparented(AXTree* tree, AXNode* node) {
   DCHECK_EQ(tree_, tree);
-  tree_events_.erase(node->id());
 }
 
 void AXEventGenerator::OnSubtreeWillBeReparented(AXTree* tree, AXNode* node) {
