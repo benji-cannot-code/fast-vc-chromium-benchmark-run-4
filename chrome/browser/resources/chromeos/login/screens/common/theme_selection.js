5f723a3ecdd47517e5666101e6d4c3f84b8c7570
Jakub Flis <jaflis@google.com>
2022-08-31 10:20:16 +0000

    OOBE - Polymer3 - Check-in theme selection
    Check-in the modularised version of OOBE's Theme Selection screen.
    
    Bug: 1184731, b/242724200
    Change-Id: Iadd6f524be07d052747f276b66ab735ff6bc1251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858659
    Commit-Queue: Jakub Flis <jaflis@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041454}

--- chrome/browser/resources/chromeos/login/screens/common/theme_selection.js
+++ chrome/browser/resources/chromeos/login/screens/common/theme_selection.js
@@ -5,17 +5,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @fileoverview Polymer element for theme selection screen.
  */
-/* #js_imports_placeholder */
+import '//resources/cr_elements/cr_radio_button/cr_radio_button.js';
+import '//resources/cr_elements/cr_radio_group/cr_radio_group.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/iron-iconset-svg/iron-iconset-svg.js';
+import '../../components/buttons/oobe_next_button.m.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OOBE_UI_STATE} from '../../components/display_manager_types.m.js';
+
 
 /**
  * @constructor
  * @extends {PolymerElement}
  * @implements {LoginScreenBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
+ * @implements {MultiStepBehaviorInterface}
  */
-const ThemeSelectionScreenElementBase = Polymer.mixinBehaviors(
-    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior],
-    Polymer.Element);
+const ThemeSelectionScreenElementBase = mixinBehaviors(
+    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior], PolymerElement);
 
 /**
  * Enum to represent steps on the theme selection screen.
@@ -56,7 +72,9 @@ class ThemeSelectionScreen extends ThemeSelectionScreenElementBase {
     return 'theme-selection-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
@@ -77,20 +95,12 @@ class ThemeSelectionScreen extends ThemeSelectionScreenElementBase {
     };
   }
 
-  constructor() {
-    super();
-  }
-
   get UI_STEPS() {
     return ThemeSelectionStep;
   }
 
   defaultUIStep() {
     return ThemeSelectionStep.OVERVIEW;
-  }
-
-  get EXTERNAL_API() {
-    return [];
   }
 
   /**
@@ -105,11 +115,12 @@ class ThemeSelectionScreen extends ThemeSelectionScreenElementBase {
   ready() {
     super.ready();
     this.initializeLoginScreen('ThemeSelectionScreen');
-    this.selectedTheme = 'auto';
   }
 
   onBeforeShow(data) {
-    this.selectedTheme = 'selectedTheme' in data && data.selectedTheme;
+    if ('selectedTheme' in data) {
+      this.selectedTheme = data.selectedTheme;
+    }
   }
 
   getOobeUIInitialState() {
