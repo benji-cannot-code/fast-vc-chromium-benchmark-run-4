07a5ba9b8d760cb3c57fa06955747f3c89e5d581
Boris Sazonov <bsazonov@chromium.org>
2022-09-01 09:15:18 +0000

    [Android] Don't crash if FirstRunActivity is destroyed
    Adds several mDestroyed checks into SigninFirstRunMediator in places
    where interactions with mDelegate go through an asynchronous callback.
    mDelegate is backed up by FirstRunActivity that might be destroyed
    before the asynchronous callback is invoked, so these checks prevents
    crashes like the one in the linked bug.
    
    Bug: 1358494
    Change-Id: If8fa1fe2bea74cab7244c54e8f7c80bedb976160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867520
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041961}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
@@ -23,6 +23,7 @@ import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.ProfileDataCache;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.signin.services.SigninManager.SignInCallback;
+import org.chromium.chrome.browser.signin.services.SigninManager.SignOutCallback;
 import org.chromium.chrome.browser.signin.services.SigninPreferencesManager;
 import org.chromium.chrome.browser.ui.signin.R;
 import org.chromium.chrome.browser.ui.signin.account_picker.AccountPickerCoordinator;
@@ -289,6 +290,11 @@ public class SigninFirstRunMediator
     }
 
     private void handleContinueWithNative() {
+        if (mDestroyed) {
+            // FirstRunActivity was destroyed while we were waiting for native.
+            return;
+        }
+
         assert mDelegate.getNativeInitializationPromise().isFulfilled();
 
         // This is needed to get metrics/crash reports from the sign-in flow itself.
@@ -320,7 +326,10 @@ public class SigninFirstRunMediator
                 AccountUtils.createAccountFromName(mSelectedAccountName), new SignInCallback() {
                     @Override
                     public void onSignInComplete() {
-                        // Wait for sign-in to be complete before advancing to the next page.
+                        if (mDestroyed) {
+                            // FirstRunActivity was destroyed while we were waiting for sign-in.
+                            return;
+                        }
                         mDelegate.advanceToNextPage();
                     }
 
@@ -353,6 +362,11 @@ public class SigninFirstRunMediator
     }
 
     private void handleDismissWithNative() {
+        if (mDestroyed) {
+            // FirstRunActivity was destroyed while we were waiting for native.
+            return;
+        }
+
         assert mDelegate.getNativeInitializationPromise().isFulfilled();
 
         mDelegate.recordFreProgressHistogram(MobileFreProgress.WELCOME_DISMISS);
@@ -362,11 +376,17 @@ public class SigninFirstRunMediator
                         .getIdentityManager(Profile.getLastUsedRegularProfile())
                         .hasPrimaryAccount(ConsentLevel.SIGNIN)) {
             mModel.set(SigninFirstRunProperties.SHOW_SIGNIN_PROGRESS_SPINNER, true);
+            SignOutCallback signOutCallback = () -> {
+                if (mDestroyed) {
+                    // FirstRunActivity was destroyed while we were waiting for the sign-out.
+                    return;
+                }
+
+                mDelegate.advanceToNextPage();
+            };
             IdentityServicesProvider.get()
                     .getSigninManager(Profile.getLastUsedRegularProfile())
-                    .signOut(SignoutReason.ABORT_SIGNIN,
-                            ()
-                                    -> { mDelegate.advanceToNextPage(); },
+                    .signOut(SignoutReason.ABORT_SIGNIN, signOutCallback,
                             /* forceWipeUserData= */ false);
         } else {
             mDelegate.advanceToNextPage();
