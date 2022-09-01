1051bfa582907614d6ba538117dfbcafb87762d9
Emilia Paz <emiliapaz@chromium.org>
2022-09-01 16:10:06 +0000

    Reset browser shutdown global state in between chrome unit tests
    Make sure shutdown global state is cleared between tests that share a
    single process. This was the flakiness source of multiple tests, which
    can be now re-enabled.
    
    Bug: 1339370
    Change-Id: I38211d4b1bb188fa4a566f543976a857592c1c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865167
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042132}

--- chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
@@ -172,7 +172,7 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
 
 // TODO(crbug.com/1321562) Disabled for flakiness.
 TEST_F(ExtensionsToolbarControlsUnitTest,
-       DISABLED_RequestAccessButtonVisibility_NavigationBetweenPages) {
+       RequestAccessButtonVisibility_NavigationBetweenPages) {
   content::WebContentsTester* web_contents_tester =
       AddWebContentsAndGetTester();
   const GURL url_a("http://www.a.com");
@@ -248,9 +248,8 @@ TEST_F(
   }
 }
 
-// TODO(crbug.com/1321562) Disabled for flakiness.
 TEST_F(ExtensionsToolbarControlsUnitTest,
-       DISABLED_RequestAccessButtonVisibility_MultipleExtensions) {
+       RequestAccessButtonVisibility_MultipleExtensions) {
   content::WebContentsTester* web_contents_tester =
       AddWebContentsAndGetTester();
   const GURL url_a("http://www.a.com");
@@ -303,10 +302,8 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
 // Tests that extensions with activeTab and requested url with withheld access
 // are taken into account for the request access button visibility, but not the
 // ones with just activeTab.
-// TODO(crbug.com/1339370): Withholding host permissions is flaky when the test
-// is run multiple times.
 TEST_F(ExtensionsToolbarControlsUnitTest,
-       DISABLED_RequestAccessButtonVisibility_ActiveTabExtensions) {
+       RequestAccessButtonVisibility_ActiveTabExtensions) {
   content::WebContentsTester* web_contents_tester =
       AddWebContentsAndGetTester();
   const GURL requested_url("http://www.requested-url.com");
@@ -384,10 +381,8 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
   }
 }
 
-// TODO(crbug.com/1339370): Withholding host permissions is flaky when the test
-// is run multiple times.
 TEST_F(ExtensionsToolbarControlsUnitTest,
-       DISABLED_RequestAccessButton_OnPressedExecuteAction) {
+       RequestAccessButton_OnPressedExecuteAction) {
   content::WebContentsTester* web_contents_tester =
       AddWebContentsAndGetTester();
 
--- chrome/test/base/chrome_unit_test_suite.cc
+++ chrome/test/base/chrome_unit_test_suite.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/profiles/profile_shortcut_manager.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "chrome/browser/update_client/chrome_update_query_params_delegate.h"
@@ -90,6 +91,7 @@ class ChromeUnitTestSuiteInitializer : public testing::EmptyTestEventListener {
     arc::ClearArcAllowedCheckForTesting();
     crypto::ResetTokenManagerForTesting();
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+    browser_shutdown::ResetShutdownGlobalsForTesting();
   }
 };
 
