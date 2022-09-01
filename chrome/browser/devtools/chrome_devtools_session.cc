b5bf29141ae5489ba5da576a5f6de73a5fb48b3d
Jiewei Qian <qjw@chromium.org>
2022-09-01 04:28:59 +0000

    devtools: block Target.createTarget to chrome-untrusted:// WebUI origins
    This CL adds a check to chrome's devtools TargetHandler to block
    createTarget calls to chrome-untrusted:// WebUI origins.
    
    This prevents extensions with debugger permissions to open
    chrome-untrusted:// WebUIs that performs high-priviledge actions on
    load.
    
    Bug: 1343104
    Change-Id: I80c63b8d209b21afe89f38b217e1bca3c001be95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855974
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041904}

--- chrome/browser/devtools/chrome_devtools_session.cc
+++ chrome/browser/devtools/chrome_devtools_session.cc
@@ -67,7 +67,8 @@ ChromeDevToolsSession::ChromeDevToolsSession(
   }
   if (IsDomainAvailableToUntrustedClient<TargetHandler>() ||
       channel->GetClient()->IsTrusted()) {
-    target_handler_ = std::make_unique<TargetHandler>(&dispatcher_);
+    target_handler_ = std::make_unique<TargetHandler>(
+        &dispatcher_, channel->GetClient()->IsTrusted());
   }
   if (IsDomainAvailableToUntrustedClient<BrowserHandler>() ||
       channel->GetClient()->IsTrusted()) {
