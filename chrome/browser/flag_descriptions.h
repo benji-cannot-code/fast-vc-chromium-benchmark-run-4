d6b8d7397e2e3a9b79d7cc8754730d6147ba266d
Roman Sorokin <rsorokin@chromium.org>
2022-09-01 09:25:05 +0000

    [flags] Remove sync-settings-categorization flag from the UI
    The feature is enabled for a while now. The feature flag will be removed
    in follow-ups
    
    Bug: 1227417
    Change-Id: Ifc0763927a182e2aabb73f8f49327d149ddae8d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865522
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041978}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3379,9 +3379,6 @@ extern const char kSnoopingProtectionDescription[];
 extern const char kSpectreVariant2MitigationName[];
 extern const char kSpectreVariant2MitigationDescription[];
 
-extern const char kSyncSettingsCategorizationName[];
-extern const char kSyncSettingsCategorizationDescription[];
-
 extern const char kSystemChinesePhysicalTypingName[];
 extern const char kSystemChinesePhysicalTypingDescription[];
 
