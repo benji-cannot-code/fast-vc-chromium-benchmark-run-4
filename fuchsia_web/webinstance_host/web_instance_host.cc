0a46646f471818b0226f0e7c5aff2ffd372654c2
David Dorwin <ddorwin@chromium.org>
2022-09-01 13:28:10 +0000

    [webengine] Remove unused config_reader.h include
    Uses were removed in https://crrev.com/c/3656666.
    
    Bug: 1212191
    Change-Id: I47758aa6895aee80240f1ca35e8ef984dbc81a3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866444
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042071}

--- fuchsia_web/webinstance_host/web_instance_host.cc
+++ fuchsia_web/webinstance_host/web_instance_host.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
-#include "components/fuchsia_component_support/config_reader.h"
 #include "components/fuchsia_component_support/feedback_registration.h"
 #include "fuchsia_web/common/string_util.h"
 #include "fuchsia_web/webengine/features.h"
