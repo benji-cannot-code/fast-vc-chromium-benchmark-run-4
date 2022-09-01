fd076b76489d351ef9b88785631ca61d5ecc855b
Ian Clelland <iclelland@chromium.org>
2022-09-01 15:54:28 +0000

    Revert "[Password Manager] Title and description for Passwords page"
    This reverts commit 7cdc014ff4cdbd7bf06f204986d4c4c15412108a.
    
    Reason for revert: Failing tests on two bots:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28Wayland%29 (Builds 20077 and 20078 so far)
    https://ci.chromium.org/p/chromium/builders/ci/linux-lacros-tester-rel (Builds 30824-30826)
    
    I'm not sure whether this caused existing tests to fail, or if the new test cases are failing; the test output is totally opaque. It coincides with this change landing, though.
    
    Original change's description:
    > [Password Manager] Title and description for Passwords page
    >
    > Mocks: https://docs.google.com/presentation/d/1uuFSa-AeuV5yHzY22-DfPwfUVC7xPPQFR1YZtW-Dldo/edit?resourcekey=0-xHwhkZTV2t5Cqxwj7NxHkA#slide=id.g12fd6483012_0_225
    > Impl: https://screenshot.googleplex.com/8GhURe673mf4YZF
    >
    > Bug: 1350947
    > Change-Id: I753e0985adc9daacab27f74f2c078bb4919ebd5c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861972
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1042074}
    
    Bug: 1350947
    Change-Id: I8e18e389ceb2dbacd836051b0bd30afd9c21a96c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863860
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1042122}

--- chrome/browser/resources/password_manager/password_manager_app.ts
+++ chrome/browser/resources/password_manager/password_manager_app.ts
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
 import './side_bar.js';
 import './toolbar.js';
-import './passwords_section.js';
 
 import {IronPagesElement} from 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
