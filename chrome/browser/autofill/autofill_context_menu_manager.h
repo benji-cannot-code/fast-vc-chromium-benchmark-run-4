2446b89654d14e76b00161f7066852bf9d175d02
Vidhan <vidhanj@google.com>
2022-09-01 16:27:28 +0000

    [Fallbacks] Send params from constructor to AutofillContextMenuManager
    This CL refactors AutofillContextMenuManager such that:
    1. RenderViewContextMenuBase is passed as a delegate to AutofillContextMenuManager.
    2. ContentAutofillDriver is initialized in the constructor of
    AutofillContextMenuManager.
    
    Aa a result, the second implementation of
    AutofillContextMenuManager::ExecuteCommand is not required anymore.
    
    This is done in preparation of the next change to log pre-feature
    metrics.
    
    Bug: 1325811
    Change-Id: I508b011f4be8805f3cb70275a383c9f876d87f21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856272
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042140}

--- chrome/browser/autofill/autofill_context_menu_manager.h
+++ chrome/browser/autofill/autofill_context_menu_manager.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/types/strong_alias.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/renderer_context_menu/render_view_context_menu_base.h"
 #include "content/public/browser/context_menu_params.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "ui/base/models/simple_menu_model.h"
 
 class Browser;
 
@@ -75,7 +75,7 @@ class AutofillContextMenuManager {
   static bool IsAutofillCustomCommandId(CommandId command_id);
 
   AutofillContextMenuManager(PersonalDataManager* personal_data_manager,
-                             ui::SimpleMenuModel::Delegate* delegate,
+                             RenderViewContextMenuBase* delegate,
                              ui::SimpleMenuModel* menu_model,
                              Browser* browser,
                              content::RenderFrameHost* render_frame_host);
@@ -92,22 +92,25 @@ class AutofillContextMenuManager {
   bool IsCommandIdChecked(CommandId command_id) const;
   bool IsCommandIdVisible(CommandId command_id) const;
   bool IsCommandIdEnabled(CommandId command_id) const;
-  // TODO(crbug.com/1325811): Add tests for the method.
-  void ExecuteCommand(CommandId command_id,
-                      const content::ContextMenuParams& params);
-  void ExecuteCommand(CommandId command_id,
-                      ContentAutofillDriver* driver,
-                      const content::ContextMenuParams& params,
-                      const blink::LocalFrameToken local_frame_token);
+  void ExecuteCommand(CommandId command_id);
 
-#if defined(UNIT_TEST)
   // Getter for `command_id_to_menu_item_value_mapper_` used for testing
   // purposes.
   const base::flat_map<CommandId, ContextMenuItem>&
   command_id_to_menu_item_value_mapper_for_testing() const {
     return command_id_to_menu_item_value_mapper_;
   }
-#endif
+
+  // Setter for `params_` used for testing purposes.
+  void set_params_for_testing(content::ContextMenuParams params) {
+    params_ = params;
+  }
+
+  // Setter for `content_autofill_driver_` used for testing purposes.
+  void set_content_autofill_driver_for_testing(
+      ContentAutofillDriver* content_autofill_driver) {
+    content_autofill_driver_ = content_autofill_driver;
+  }
 
  private:
   // Adds address items to the context menu.
@@ -138,9 +141,11 @@ class AutofillContextMenuManager {
 
   raw_ptr<PersonalDataManager> personal_data_manager_;
   raw_ptr<ui::SimpleMenuModel> menu_model_;
-  raw_ptr<ui::SimpleMenuModel::Delegate> delegate_;
+  raw_ptr<RenderViewContextMenuBase> delegate_;
   raw_ptr<Browser> browser_;
   raw_ptr<content::RenderFrameHost> render_frame_host_;
+  raw_ptr<ContentAutofillDriver> content_autofill_driver_;
+  content::ContextMenuParams params_;
 
   // Stores the count of items added to the context menu from Autofill.
   int count_of_items_added_to_menu_model_ = 0;
