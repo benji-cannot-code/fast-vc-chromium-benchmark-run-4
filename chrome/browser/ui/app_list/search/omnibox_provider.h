e6f89957b49f72ef83b5f29547df1a3361c59490
CJ Huang <chenjih@google.com>
2022-09-01 04:33:20 +0000

    [launcher] Mock AutoCompleteController in unittest
    The AutoCompleteController in omnibox provider is mocked, which might be the cause of flaky tests.
    
    Bug: 1357483
    Change-Id: I6ed9a5467828664b85cc019f9dd16bde2b1c0d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865185
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041911}

--- chrome/browser/ui/app_list/search/omnibox_provider.h
+++ chrome/browser/ui/app_list/search/omnibox_provider.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_OMNIBOX_PROVIDER_H_
 #define CHROME_BROWSER_UI_APP_LIST_SEARCH_OMNIBOX_PROVIDER_H_
 
+#include <algorithm>
 #include <memory>
 
 #include "base/time/time.h"
@@ -42,11 +43,10 @@ class OmniboxProvider : public SearchProvider,
   // Populates result list from AutocompleteResult.
   void PopulateFromACResult(const AutocompleteResult& result);
 
-  // Change the query_finished_ flag for testing purpose.
-  // TODO(crbug.com/1356409): Replace this function with formal testing
-  // procedures.
-  void set_query_finished_for_test(bool query_finished) {
-    query_finished_ = query_finished;
+  // Change the controller_ for testing purpose.
+  void set_controller_for_test(
+      std::unique_ptr<AutocompleteController> controller) {
+    controller_ = std::move(controller);
   }
 
  private:
