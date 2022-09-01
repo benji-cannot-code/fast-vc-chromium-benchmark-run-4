24ec9c0ee705e3e3ef7d23326df136000a648505
William Liu <liuwilliam@chromium.org>
2022-09-01 22:16:58 +0000

    [MPArch] Avoid WebContents access for MPArch GuestView in SSLManager.
    We fetch the StoragePartition from:
    NaviController -> FrameTree -> MainFrame -> StoragePartition.
    
    Bug: 1353781, 1261928
    Change-Id: Id7d2537f58bedf1e0c508a7461e57a274cb28997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863011
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042338}

--- content/browser/ssl/ssl_manager.cc
+++ content/browser/ssl/ssl_manager.cc
@@ -55,6 +55,7 @@ enum class MixedContentType {
 };
 
 void OnAllowCertificate(SSLErrorHandler* handler,
+                        StoragePartition* storage_partition,
                         SSLHostStateDelegate* state_delegate,
                         bool record_decision,
                         CertificateRequestResultType decision) {
@@ -74,10 +75,7 @@ void OnAllowCertificate(SSLErrorHandler* handler,
       if (record_decision && state_delegate) {
         state_delegate->AllowCert(handler->request_url().host(),
                                   *handler->ssl_info().cert.get(),
-                                  handler->cert_error(),
-                                  handler->web_contents()
-                                      ->GetPrimaryMainFrame()
-                                      ->GetStoragePartition());
+                                  handler->cert_error(), storage_partition);
       }
       handler->ContinueRequest();
       return;
@@ -348,9 +346,7 @@ void SSLManager::OnCertError(std::unique_ptr<SSLErrorHandler> handler) {
     judgment = ssl_host_state_delegate_->QueryPolicy(
         handler->request_url().host(), *handler->ssl_info().cert.get(),
         handler->cert_error(),
-        // TODO(crbug/1353781): Avoid WebContents for MPArch GuestView. Get
-        // StoragePartition from navi controller's frame tree instead.
-        handler->web_contents()->GetPrimaryMainFrame()->GetStoragePartition());
+        controller_->frame_tree().GetMainFrame()->GetStoragePartition());
   } else {
     judgment = SSLHostStateDelegate::DENIED;
   }
@@ -377,11 +373,8 @@ void SSLManager::DidStartResourceResponse(
   // any previous decisions that have occurred.
   if (!ssl_host_state_delegate_ ||
       !ssl_host_state_delegate_->HasAllowException(
-          // TODO(crbug/1353781): Avoid WebContents for MPArch GuestView. Get
-          // StoragePartition from navi controller's frame tree instead.
-          host, controller_->DeprecatedGetWebContents()
-                    ->GetPrimaryMainFrame()
-                    ->GetStoragePartition())) {
+          host,
+          controller_->frame_tree().GetMainFrame()->GetStoragePartition())) {
     return;
   }
 
@@ -401,9 +394,10 @@ void SSLManager::OnCertErrorInternal(std::unique_ptr<SSLErrorHandler> handler) {
   bool fatal = handler->fatal();
 
   base::RepeatingCallback<void(bool, content::CertificateRequestResultType)>
-      callback = base::BindRepeating(&OnAllowCertificate,
-                                     base::Owned(handler.release()),
-                                     ssl_host_state_delegate_);
+      callback = base::BindRepeating(
+          &OnAllowCertificate, base::Owned(handler.release()),
+          controller_->frame_tree().GetMainFrame()->GetStoragePartition(),
+          ssl_host_state_delegate_);
 
   if (devtools_instrumentation::HandleCertificateError(
           web_contents, cert_error, request_url,
