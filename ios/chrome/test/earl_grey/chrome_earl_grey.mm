a25b971113c7d53a83f8c614f353b839067d4b3e
Mike Dougherty <michaeldo@chromium.org>
2022-09-01 19:44:55 +0000

    Add stack trace logging when EG test helper fails to find element
    Prints the call stack trace to help debug the endlessly spinning
    GridCell MDCActivityIndicator [ChromeEarlGrey
    waitForUIElementToAppearWithMatcher:timeout:]
    
    Bug: 1352968, 1352970
    Change-Id: I75d22cc280270828461b6d853698abc673bbde40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863344
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042258}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -428,7 +428,7 @@ UIWindow* GetAnyKeyWindow() {
   };
 
   bool matched = WaitUntilConditionOrTimeout(timeout, condition);
-  GREYAssert(matched, errorDescription);
+  EG_TEST_HELPER_ASSERT_TRUE(matched, errorDescription);
 }
 
 - (void)waitForUIElementToDisappearWithMatcher:(id<GREYMatcher>)matcher {
@@ -450,7 +450,7 @@ UIWindow* GetAnyKeyWindow() {
   };
 
   bool matched = WaitUntilConditionOrTimeout(timeout, condition);
-  GREYAssert(matched, errorDescription);
+  EG_TEST_HELPER_ASSERT_TRUE(matched, errorDescription);
 }
 
 - (NSString*)currentTabTitle {
--- ios/testing/earl_grey/base_eg_test_helper_impl.mm
+++ ios/testing/earl_grey/base_eg_test_helper_impl.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/testing/earl_grey/base_eg_test_helper_impl.h"
 
+#import "base/debug/stack_trace.h"
+#import "base/logging.h"
 #include "ios/testing/earl_grey/earl_grey_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -48,6 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     description:(NSString*)description {
   if (!fail)
     return;
+
+  DLOG(WARNING) << "\n" << base::debug::StackTrace(/*count=*/15).ToString();
 
   NSString* reason =
       [NSString stringWithFormat:@"%@ is false: %@", expression, description];
