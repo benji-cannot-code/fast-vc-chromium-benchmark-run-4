4d61b833d7068e8799969fda981f285f25941e5a
Daniel Cheng <dcheng@chromium.org>
2022-09-01 12:08:25 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //components/policy.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=pmarko@chromium.org
    
    Bug: 1356184
    Change-Id: I3472d1d693974b288a858822e34cd14812b66ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859822
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042041}

--- components/policy/core/common/policy_map.cc
+++ components/policy/core/common/policy_map.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/types/optional_util.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -156,11 +157,11 @@ base::Value* PolicyMap::Entry::value(base::Value::Type value_type) {
 }
 
 const base::Value* PolicyMap::Entry::value_unsafe() const {
-  return base::OptionalOrNullptr(value_);
+  return base::OptionalToPtr(value_);
 }
 
 base::Value* PolicyMap::Entry::value_unsafe() {
-  return base::OptionalOrNullptr(value_);
+  return base::OptionalToPtr(value_);
 }
 
 void PolicyMap::Entry::set_value(absl::optional<base::Value> val) {
