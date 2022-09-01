8d4f1ce8f61d363e0f2a4cce936c42c43a7edf99
Ahmed Nasr <anasr@google.com>
2022-09-01 16:02:48 +0000

    Add new class SamplerConfiguration to hold sampler and its setting.
    This will help connecting samplers to their enable/disable setting
    so no telemetry data will be collected for event driven telemetry
    if the setting is disabled.
    
    Bug: b:237802510
    Change-Id: I10f06161c7d8df611d44c705cfe0b59e90292e1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849355
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042130}

---
+++ components/reporting/metrics/configured_sampler_unittest.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/reporting/metrics/configured_sampler.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "base/strings/string_piece.h"
+#include "components/reporting/metrics/fake_sampler.h"
+#include "components/reporting/metrics/sampler.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace reporting {
+namespace {
+
+TEST(ConfiguredSamplerTest, Default) {
+  static constexpr char enable_setting_path[] = "path";
+  std::unique_ptr<Sampler> sampler = std::make_unique<test::FakeSampler>();
+  auto* const sampler_ptr = sampler.get();
+
+  ConfiguredSampler configured_sampler(std::move(sampler), enable_setting_path,
+                                       /*setting_enabled_default_value=*/true);
+
+  EXPECT_THAT(configured_sampler.GetSampler(), testing::Eq(sampler_ptr));
+  EXPECT_THAT(configured_sampler.GetEnableSettingPath(),
+              testing::StrEq(enable_setting_path));
+  EXPECT_TRUE(configured_sampler.GetSettingEnabledDefaultValue());
+}
+
+}  // namespace
+}  // namespace reporting
