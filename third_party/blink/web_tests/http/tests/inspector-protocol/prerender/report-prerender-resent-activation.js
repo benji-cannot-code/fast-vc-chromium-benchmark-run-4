63e677c7283ba6694d76561ef30cfbfaa4485f14
Robert Lin <robertlin@chromium.org>
2022-09-01 01:00:25 +0000

    prerender: Resending prerender activation for DevTools.
    This CL stores prerender activation in WebContents and resend it
    once DevTools is reopened as a workaround to the current unsupported
    prerender activation in DevTools.
    
    Design doc: https://docs.google.com/document/d/1l5S3DCMVsFfF_Thg95BT9292sDxas1wzKALz8J9FrB8/edit?usp=sharing
    
    Change-Id: Ib2997325e73a7fbf9a0bfde0228006624d931c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805129
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041841}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/prerender/report-prerender-resent-activation.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Test that prerender navigations report the resent activation.`);
+  await dp.Page.enable();
+
+  // Navigate to speculation rules Prerender Page.
+  await page.navigate('resources/simple-prerender.html');
+  session.evaluate(`document.getElementById('link').click()`);
+  const statusReport = await dp.Page.oncePrerenderAttemptCompleted();
+  testRunner.log(statusReport, '', ['initiatingFrameId', 'sessionId']);
+  await dp.Page.disable();
+  dp.Page.enable();
+  const resentStatusReport = await dp.Page.oncePrerenderAttemptCompleted();
+  testRunner.log(resentStatusReport, '', ['initiatingFrameId', 'sessionId']);
+
+  testRunner.completeTest();
+});
