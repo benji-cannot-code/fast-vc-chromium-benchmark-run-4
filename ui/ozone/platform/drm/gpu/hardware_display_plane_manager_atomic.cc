e83f4856122b0e2a17268cb92254b14c1211f988
Peter McNeeley <petermcneeley@chromium.org>
2022-09-01 23:53:04 +0000

    Allow overlay rotations when testing with original buffers
    Rotated overlays are supported for tiled buffers. However the tiling
    information about buffers is found in the modifiers. We need the
    original buffer that has the original modifiers in order to do the
    correct DRM test.
    
    Most of the time we have the original buffer with the original
    modifiers however in 'GetBufferForPageFlipTest' we can create a fake
    non original testing buffer with made up default modifiers. Therefore,
    we flag these buffers as being non original.
    
    Bug: 1353067
    Change-Id: Ifcf998b5f3630b4f9d4159f5cfa571b1d9affb2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852707
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042370}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
@@ -217,7 +217,7 @@ void HardwareDisplayPlaneManagerAtomic::SetAtomicPropsForCommit(
           static_cast<HardwareDisplayPlaneAtomic*>(plane);
       atomic_plane->AssignPlaneProps(
           0, 0, gfx::Rect(), gfx::Rect(), gfx::OVERLAY_TRANSFORM_NONE,
-          base::kInvalidPlatformFile, DRM_FORMAT_INVALID);
+          base::kInvalidPlatformFile, DRM_FORMAT_INVALID, false);
       atomic_plane->SetPlaneProps(atomic_request);
     }
   }
@@ -326,7 +326,7 @@ bool HardwareDisplayPlaneManagerAtomic::DisableOverlayPlanes(
           static_cast<HardwareDisplayPlaneAtomic*>(plane);
       atomic_plane->AssignPlaneProps(
           0, 0, gfx::Rect(), gfx::Rect(), gfx::OVERLAY_TRANSFORM_NONE,
-          base::kInvalidPlatformFile, DRM_FORMAT_INVALID);
+          base::kInvalidPlatformFile, DRM_FORMAT_INVALID, false);
       atomic_plane->SetPlaneProps(plane_list->atomic_property_set.get());
     }
     ret = drm_->CommitProperties(plane_list->atomic_property_set.get(),
@@ -399,7 +399,8 @@ bool HardwareDisplayPlaneManagerAtomic::SetPlaneData(
   if (!atomic_plane->AssignPlaneProps(
           crtc_id, framebuffer_id, overlay.display_bounds, src_rect,
           overlay.plane_transform, fence_fd,
-          overlay.buffer->framebuffer_pixel_format())) {
+          overlay.buffer->framebuffer_pixel_format(),
+          overlay.buffer->is_original_buffer())) {
     return false;
   }
   return true;
