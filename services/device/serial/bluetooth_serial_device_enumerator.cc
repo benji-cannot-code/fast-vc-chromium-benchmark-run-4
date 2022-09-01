3eaa23ade3753e48c8ee39e532995ec84ccfceeb
Jack Hsieh <chengweih@chromium.org>
2022-09-01 00:56:23 +0000

    [serial] Ensure RemoveObserver during BluetoothAdapter destruction
    There is an UAF issue due to
    BluetoothSerialDeviceEnumerator::AdapterHelper not remove itself from
    observers of BluetoothAdapter and it ends up when
    BluetoothSerialDeviceEnumerator::AdapterHelper is destroyed, the device
    added/removed notified by BluetoothAdapter to its observers might lead
    to UAF. This is fixed by using base::ScopedObservation so that
    RemoveObserver can be called when
    BluetoothSerialDeviceEnumerator::AdapterHelper destructs.
    
    Bug: 1344878
    Change-Id: Ib80ad45257caf0d9dedb0949e9de9cd39aee87ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866838
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041839}

--- services/device/serial/bluetooth_serial_device_enumerator.cc
+++ services/device/serial/bluetooth_serial_device_enumerator.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/containers/contains.h"
+#include "base/scoped_observation.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/unguessable_token.h"
@@ -36,6 +37,16 @@ class BluetoothSerialDeviceEnumerator::AdapterHelper
   // The enumerator that owns this instance.
   base::WeakPtr<BluetoothSerialDeviceEnumerator> enumerator_;
   scoped_refptr<base::SequencedTaskRunner> enumerator_runner_;
+
+  // scoped_refptr<BluetoothAdapter> is required to ensure that this object
+  // actually has a reference to the BluetoothAdapter when the call to
+  // RemoveObserver() happens.
+  scoped_refptr<BluetoothAdapter> adapter_;
+
+  // |observation_| needs to be after |adapter_| to ensure it is reset before
+  // |adapter_|'s reset during destruction.
+  base::ScopedObservation<BluetoothAdapter, BluetoothAdapter::Observer>
+      observation_{this};
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<AdapterHelper> weak_ptr_factory_{this};
 };
@@ -54,16 +65,17 @@ void BluetoothSerialDeviceEnumerator::AdapterHelper::OnGotClassicAdapter(
     scoped_refptr<device::BluetoothAdapter> adapter) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(adapter);
+  adapter_ = std::move(adapter);
 
-  BluetoothAdapter::DeviceList devices = adapter->GetDevices();
+  BluetoothAdapter::DeviceList devices = adapter_->GetDevices();
   for (auto* device : devices) {
-    DeviceAdded(adapter.get(), device);
+    DeviceAdded(adapter_.get(), device);
   }
-  adapter->AddObserver(this);
+  observation_.Observe(adapter_.get());
   enumerator_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&BluetoothSerialDeviceEnumerator::SetClassicAdapter,
-                     enumerator_, std::move(adapter)));
+                     enumerator_, adapter_));
 }
 
 void BluetoothSerialDeviceEnumerator::AdapterHelper::DeviceAdded(
