c69d6dd50d836e9381a58d57f49fa89bfc1d75b9
Brandon Jones <bajones@chromium.org>
2022-09-01 02:59:19 +0000

    Implement maxBindingsPerBindGroup limit
    Bug: dawn:1523
    Change-Id: Ifc2ce4840b36332ef15a21f68b7edb9ed4e514af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864042
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041885}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(maxTextureDimension3D)                     \
   X(maxTextureArrayLayers)                     \
   X(maxBindGroups)                             \
+  X(maxBindingsPerBindGroup)                   \
   X(maxDynamicUniformBuffersPerPipelineLayout) \
   X(maxDynamicStorageBuffersPerPipelineLayout) \
   X(maxSampledTexturesPerShaderStage)          \
