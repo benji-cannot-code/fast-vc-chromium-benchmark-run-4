89ed1134613a505b246ff85c6adc9ab0d5846e92
Benjamin Williams <bwwilliams@google.com>
2022-09-01 13:43:49 +0000

    Create PromosManagerCommands.
    PromosManagerMediator needs a way to communicate with
    PromosManagerCoordinator.
    
    PromosManagerCommands should be created to achieve this.
    
    Change-Id: If5fb754c86d0fc9816f5aa167ac5b2456bb89dcd
    Bug: 1358534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863565
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042075}

---
+++ ios/chrome/browser/ui/commands/promos_manager_commands.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_COMMANDS_PROMOS_MANAGER_COMMANDS_H_
+#define IOS_CHROME_BROWSER_UI_COMMANDS_PROMOS_MANAGER_COMMANDS_H_
+
+#import "ios/chrome/browser/promos_manager/constants.h"
+
+// Commands to show app-wide promos.
+@protocol PromosManagerCommands <NSObject>
+
+// Displays the promo, `promo`.
+- (void)displayPromo:(promos_manager::Promo)promo;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_COMMANDS_PROMOS_MANAGER_COMMANDS_H_
