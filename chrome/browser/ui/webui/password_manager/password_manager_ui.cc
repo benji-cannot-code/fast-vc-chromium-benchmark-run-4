0b3277fa5ca21920801ad926542aa697401b261a
Viktor Semeniuk <vsemeniuk@google.com>
2022-09-01 13:43:37 +0000

    [Password Manager] Title and description for Passwords page
    Mocks: https://docs.google.com/presentation/d/1uuFSa-AeuV5yHzY22-DfPwfUVC7xPPQFR1YZtW-Dldo/edit?resourcekey=0-xHwhkZTV2t5Cqxwj7NxHkA#slide=id.g12fd6483012_0_225
    Impl: https://screenshot.googleplex.com/8GhURe673mf4YZF
    
    Bug: 1350947
    Change-Id: I753e0985adc9daacab27f74f2c078bb4919ebd5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861972
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042074}

--- chrome/browser/ui/webui/password_manager/password_manager_ui.cc
+++ chrome/browser/ui/webui/password_manager/password_manager_ui.cc
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/password_manager/password_manager_ui.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/password_manager_resources.h"
 #include "chrome/grit/password_manager_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/webui/web_ui_util.h"
 
 namespace {
@@ -27,6 +30,7 @@ content::WebUIDataSource* CreatePasswordsUIHTMLSource(Profile* profile) {
       IDR_PASSWORD_MANAGER_PASSWORD_MANAGER_HTML);
 
   static constexpr webui::LocalizedString kStrings[] = {
+      {"addPassword", IDS_PASSWORD_MANAGER_UI_ADD_PASSWORD_BUTTON},
       {"checkup", IDS_PASSWORD_MANAGER_UI_CHECKUP},
       {"clearSearch", IDS_CLEAR_SEARCH},
       {"passwords", IDS_PASSWORD_MANAGER_UI_PASSWORDS},
@@ -35,6 +39,12 @@ content::WebUIDataSource* CreatePasswordsUIHTMLSource(Profile* profile) {
       {"title", IDS_PASSWORD_MANAGER_UI_TITLE},
   };
   source->AddLocalizedStrings(kStrings);
+
+  source->AddString(
+      "passwordsSectionDescription",
+      l10n_util::GetStringFUTF16(
+          IDS_PASSWORD_MANAGER_UI_PASSWORDS_DESCRIPTION,
+          base::ASCIIToUTF16(chrome::kPasswordManagerLearnMoreURL)));
 
   return source;
 }
