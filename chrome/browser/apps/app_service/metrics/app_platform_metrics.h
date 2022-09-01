7cb40b9de23ee6a5a19a914629ff1abd0fcd01ea
Nancy Wang <nancylingwang@chromium.org>
2022-09-01 06:01:39 +0000

    Modify AppPlatformMetrics code to remove the dependency on mojom.
    BUG=1253250
    
    Change-Id: Ib45e6ee5092fac4c0b4130fc20283b8ec91f8b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867016
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041932}

--- chrome/browser/apps/app_service/metrics/app_platform_metrics.h
+++ chrome/browser/apps/app_service/metrics/app_platform_metrics.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
-#include "components/services/app_service/public/mojom/types.mojom.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
 class Profile;
