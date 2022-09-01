f51a469b8532cd1a0e05052d746251d8239a6962
Alan Screen <awscreen@chromium.org>
2022-09-01 21:00:10 +0000

    Add process initialization for printing in Print Backend Service
    Relocate one of the calls to initialize the printing subsystem for the
    process into a support function.  This will contain the necessary
    process setup logic that is needed for any process that is interacting
    with the printing subsystem.  This is distinct from the setup logic
    that is only needed by the browser process.
    
    Update the Print Backend Service utility to use of this initialization
    function.  This is needed to ensure printer name handling is done the
    same between the browser process and out-of-process printing on
    Windows.
    
    Bug: 1347255
    Change-Id: I21c66fd9f0c838e33e0d5cda7053cd2ceb2ed702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791808
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042295}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -119,6 +119,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/media/media_resource_provider.h"
 #include "chrome/common/net/net_resource_provider.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/printing/printing_init.h"
 #include "chrome/common/profiler/thread_profiler.h"
 #include "chrome/common/profiler/thread_profiler_configuration.h"
 #include "chrome/common/profiler/unwind_util.h"
@@ -316,11 +317,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW) && !defined(OFFICIAL_BUILD)
 #include "printing/printed_document.h"
-#endif
-
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW) && BUILDFLAG(IS_WIN)
-#include "chrome/common/printing/printer_capabilities.h"
-#include "printing/backend/win_helper.h"
 #endif
 
 #if BUILDFLAG(ENABLE_RLZ)
@@ -1701,9 +1697,7 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
   }
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW) && !defined(OFFICIAL_BUILD)
 
-#if BUILDFLAG(ENABLE_PRINT_PREVIEW) && BUILDFLAG(IS_WIN)
-  printing::SetGetDisplayNameFunction(&printing::GetUserFriendlyName);
-#endif
+  printing::InitializeProcessForPrinting();
 
   HandleTestParameters(*base::CommandLine::ForCurrentProcess());
 
