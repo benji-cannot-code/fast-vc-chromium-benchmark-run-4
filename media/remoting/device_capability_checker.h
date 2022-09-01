d39655b661a03c973a17383c49ef0a08dd1acdef
Muyao Xu <muyaoxu@google.com>
2022-09-01 18:38:49 +0000

    [RemotePlayback] Support sink discovery for RemotePlayback MediaSource
    This CL adds hardcoded Chromecast devices' audio/video capabilities
    and uses it to determine if a sink is compatible with the RemotePlayback
    MediaSource in Cast MRP.
    
    Bug: 1355608
    Change-Id: I4236e1e697bbd9b0850dfbb03397e7db5fcd7167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828315
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042218}

---
+++ media/remoting/device_capability_checker.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_REMOTING_DEVICE_CAPABILITY_CHECKER_H_
+#define MEDIA_REMOTING_DEVICE_CAPABILITY_CHECKER_H_
+
+#include <string>
+
+namespace media {
+enum class AudioCodec;
+enum class VideoCodec;
+
+namespace remoting {
+// Return true if the device is a Chromecast device according to its
+// `model_name`.
+bool IsChromecast(const std::string& model_name);
+
+// Return true if the device is compatible to render `video_codec`.
+bool IsVideoCodecCompatible(const std::string& model_name,
+                            media::VideoCodec video_codec);
+
+// Return true if the device is compatible to render `audio_codec`.
+bool IsAudioCodecCompatible(const std::string& model_name,
+                            media::AudioCodec audio_codec);
+}  // namespace remoting
+}  // namespace media
+
+#endif  // MEDIA_REMOTING_DEVICE_CAPABILITY_CHECKER_H_
