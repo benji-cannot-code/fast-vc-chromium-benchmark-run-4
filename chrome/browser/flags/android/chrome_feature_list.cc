bd563277affada5dbc44db445071b06d2ac2bfb1
Travis Skare <skare@chromium.org>
2022-09-02 20:36:55 +0000

    [Crow] Expose kCrowLaunchTab feature to Java.
    Bug: 1351782
    Change-Id: I91b9fa29b8d4a9dbe3f3ea699c7a0a4193b644dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871978
    Auto-Submit: Travis Skare <skare@chromium.org>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042761}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -396,6 +396,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &send_tab_to_self::kSendTabToSelfSigninPromo,
     &send_tab_to_self::kSendTabToSelfV2,
     &share::kCormorant,
+    &share::kCrowLaunchTab,
     &share::kPersistShareHubOnAppSwitch,
     &share::kScreenshotsForAndroidV2,
     &share::kUpcomingSharingFeatures,
