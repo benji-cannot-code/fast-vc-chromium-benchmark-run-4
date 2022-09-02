a56e9ea72b0eae2080eb5b02f6e5b577316e276f
Anna Tsvirchkova <atsvirchkova@google.com>
2022-09-02 12:55:02 +0000

    [PwdMessageDialog] Improving save/update password dialog UX
    Only UI changes:
    - Changes the mode in which the dialog is being shown to APP mode. It
    allows the dialog to take all the space of the screen instead of just
    the tab space.
    - Introduces a scroll view for the dialog custom view to be sure that
    everything works well for small screens.
    
    See the screenshots
    Before these changes: https://screenshot.googleplex.com/7JA8VE9Rxo3ZKzF
    After: https://screenshot.googleplex.com/4hEhYMhBtJfhZy9
    
    Bug: 1315916
    Change-Id: I6f27e5826af6fa088580434be3d2cf283cbf8fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867819
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042544}

--- chrome/browser/password_edit_dialog/android/java/src/org/chromium/chrome/browser/password_edit_dialog/PasswordEditDialogCoordinator.java
+++ chrome/browser/password_edit_dialog/android/java/src/org/chromium/chrome/browser/password_edit_dialog/PasswordEditDialogCoordinator.java
@@ -128,7 +128,7 @@ class PasswordEditDialogCoordinator {
         PropertyModelChangeProcessor.create(
                 mDialogViewModel, mDialogView, PasswordEditDialogViewBinder::bind);
 
-        mModalDialogManager.showDialog(mDialogModel, ModalDialogManager.ModalDialogType.TAB);
+        mModalDialogManager.showDialog(mDialogModel, ModalDialogManager.ModalDialogType.APP);
     }
 
     /**
@@ -161,7 +161,9 @@ class PasswordEditDialogCoordinator {
         PropertyModelChangeProcessor.create(
                 mDialogViewModel, mDialogView, PasswordEditDialogViewBinder::bind);
 
-        mModalDialogManager.showDialog(mDialogModel, ModalDialogManager.ModalDialogType.TAB);
+        mModalDialogManager.showDialog(mDialogModel,
+                mIsDialogWithDetailsFeatureEnabled ? ModalDialogManager.ModalDialogType.APP
+                                                   : ModalDialogManager.ModalDialogType.TAB);
     }
 
     private PropertyModel createDialogViewModel(
