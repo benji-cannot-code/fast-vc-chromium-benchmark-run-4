862c47b1795b333e0cb0681c7730b841a6fa4984
Vadym Kargin <vadym.kargin@accenture.com>
2022-09-02 17:29:42 +0000

    Replace the old ToV8 with new ToV8Traits interface in V8 WASM Response.
    This update is part of work on replacing the old ToV8 interface with new ToV8Traits<IDLType>::ToV8 interface.
    
    Issue: Replace ToV8 with ToV8Traits<IDLType>::ToV8
    Bug: 1199627
    Change-Id: Iab9571dc8779422e4031e0ffb5087b5e024f176c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3358880
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042641}

--- third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_response.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -282,8 +283,8 @@ class FetchDataLoaderForWasmStreaming final : public FetchDataLoader,
     {
       ScriptForbiddenScope::AllowUserAgentScript allow_script;
       v8::Local<v8::Value> v8_exception =
-          ToV8(exception, script_state_->GetContext()->Global(),
-               script_state_->GetIsolate());
+          ToV8Traits<DOMException>::ToV8(script_state_, exception)
+              .ToLocalChecked();
       streaming_->Abort(v8_exception);
     }
   }
