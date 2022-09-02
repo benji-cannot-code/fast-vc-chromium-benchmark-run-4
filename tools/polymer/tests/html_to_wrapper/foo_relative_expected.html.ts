a11c79a4adb814cb5abf3956b7289d4a024fb4a0
dpapad <dpapad@chromium.org>
2022-09-02 01:07:54 +0000

    WebUI: Allow producing scheme-relative URLs in html_to_wrapper().
    This is necessary since some files (for example [1]) are used from
    chrome-untrusted:// contexts (for example from CrOS'
    chrome-untrusted://os-feedback at [2])
    
    Unfortunately can't always produce scheme-relative URLs because in other
    cases the scheme must be "chrome://", while the enclosing context is
    "chrome-extension://".
    
    [1] ui/webui/resources/cr_elements/policy/cr_tooltip_icon.{ts/html}
    [2] https://source.chromium.org/chromium/chromium/src/+/main:ash/webui/os_feedback_ui/resources/help_content.js;l=12;drc=617b916c454a06cff1f3eb111b6c92f4407e63f3
    
    Bug: 1292025
    Change-Id: I144bacb87289ee73fb18b629136a625d81415c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866617
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042406}

---
+++ tools/polymer/tests/html_to_wrapper/foo_relative_expected.html.ts
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+export function getTemplate() {
+  return html`<!--_html_template_start_--><style>
+  div {
+    font-size: 2rem;
+  }
+</style>
+<div>Hello world</div>
+<!--_html_template_end_-->`;
+}
