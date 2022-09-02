740851aba025ef6e88b5b3a91aecc1d17d9674e9
Dibyajyoti Pal <dibyapal@chromium.org>
2022-09-02 22:59:51 +0000

    [dPWA] Retire FakeWebAppRegistryController
    This CL completely removes FakeWebAppRegistryController, along with
    its dependencies and adds some changes to the current testing doc.
    
    Bug: b/241948046
    Change-Id: I410ae2b386ebe937e309db7c749576a4e6aaff7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852526
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042819}

--- chrome/browser/apps/app_service/app_icon/app_icon_factory_unittest.cc
+++ chrome/browser/apps/app_service/app_icon/app_icon_factory_unittest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/pixel_test_utils.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
 #include "chrome/browser/extensions/chrome_app_icon.h"
-#include "chrome/browser/web_applications/test/fake_web_app_registry_controller.h"
 #include "chrome/browser/web_applications/test/test_file_utils.h"
 #include "chrome/browser/web_applications/test/web_app_icon_test_utils.h"
 #include "chrome/browser/web_applications/test/web_app_test_utils.h"
