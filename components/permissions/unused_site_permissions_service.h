311749bfa4fe7b295df73e2d35a9d183a5616218
Side Yilmaz <sideyilmaz@chromium.org>
2022-09-02 15:11:48 +0000

    [SafetyCheck] Add UnusedSitePermissionsService and its factory.
    This CL adds UnusedSitePermissionsService that will be responsible of
    running background job to revoke permissions of unused sites. By this
    CL, a dummy service is added without any functionality, as the
    implementation is going on.
    
    Bug: 1345920
    Change-Id: I90be0f901f40255052161e22e776e17f49b1b519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813714
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042580}

---
+++ components/permissions/unused_site_permissions_service.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PERMISSIONS_UNUSED_SITE_PERMISSIONS_SERVICE_H_
+#define COMPONENTS_PERMISSIONS_UNUSED_SITE_PERMISSIONS_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace permissions {
+
+// Add explanation
+class UnusedSitePermissionsService : public KeyedService {
+ public:
+  explicit UnusedSitePermissionsService(content::BrowserContext* context);
+
+  UnusedSitePermissionsService(const UnusedSitePermissionsService&) = delete;
+  UnusedSitePermissionsService& operator=(const UnusedSitePermissionsService&) =
+      delete;
+
+  ~UnusedSitePermissionsService() override;
+
+  // KeyedService implementation.
+  void Shutdown() override;
+};
+
+}  // namespace permissions
+
+#endif  // COMPONENTS_PERMISSIONS_UNUSED_SITE_PERMISSIONS_SERVICE_H_
