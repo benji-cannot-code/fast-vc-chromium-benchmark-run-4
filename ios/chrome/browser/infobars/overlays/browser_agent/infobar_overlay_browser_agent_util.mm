866903b36dfdb356ad3501700b97bb8b06e89d54
Joemer Ramos <joemerramos@chromium.org>
2022-09-02 20:23:14 +0000

    [iOS] Create Interaction Handler for TailoredSecurity Infobar
    The new tailored security infobar needs to have an interaction
    handler to properly work with requested functionality. Good to note
    that the modal parameter is null here since a tailored security infobar
    doesn't need a modal overlay.
    
    LOW_COVERAGE_REASON=Util file isn't unit tested
    
    Bug: 1357605
    Change-Id: Id046448d97b05501c0ba76e16b020724841abc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871095
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042756}

--- ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.h"
 
 #include "base/feature_list.h"
+#import "components/safe_browsing/core/common/features.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/confirm/confirm_infobar_interaction_handler.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/update_password_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/permissions/permissions_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/save_card/save_card_infobar_interaction_handler.h"
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/tailored_security/tailored_security_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/translate/translate_infobar_interaction_handler.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -37,4 +39,9 @@ void AttachInfobarOverlayBrowserAgent(Browser* browser) {
       std::make_unique<SaveAddressProfileInfobarInteractionHandler>());
   browser_agent->AddInfobarInteractionHandler(
       std::make_unique<PermissionsInfobarInteractionHandler>());
+  if (base::FeatureList::IsEnabled(
+          safe_browsing::kTailoredSecurityIntegration)) {
+    browser_agent->AddInfobarInteractionHandler(
+        std::make_unique<TailoredSecurityInfobarInteractionHandler>());
+  }
 }
---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/tailored_security/tailored_security_infobar_interaction_handler.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TAILORED_SECURITY_TAILORED_SECURITY_INFOBAR_INTERACTION_HANDLER_H_
+#define IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TAILORED_SECURITY_TAILORED_SECURITY_INFOBAR_INTERACTION_HANDLER_H_
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/infobar_interaction_handler.h"
+
+// An InfobarInteractionHandler that updates the model layer for interaction
+// events with the UI for tailored security infobars.
+class TailoredSecurityInfobarInteractionHandler
+    : public InfobarInteractionHandler {
+ public:
+  TailoredSecurityInfobarInteractionHandler();
+  ~TailoredSecurityInfobarInteractionHandler() override;
+};
+
+#endif  // IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TAILORED_SECURITY_TAILORED_SECURITY_INFOBAR_INTERACTION_HANDLER_H_
