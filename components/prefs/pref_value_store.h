d32a88c26ab9de9652199e280aabd5d83d4bdf03
Roland Bock <rbock@google.com>
2022-09-02 19:49:45 +0000

    Remove unused PrefValueStore::Delegate
    Currently PrefValueStore::Delegate is unused.
    This CL therefore removes it.
    
    Bug: None
    Change-Id: Ibf1e01b08d0ad4ad8877cdf69cfb612e46ba14c9
    Tests: Existing unit tests for the PrefValueStore
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849359
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042737}

--- components/prefs/pref_value_store.h
+++ components/prefs/pref_value_store.h
@@ -20,9 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_store.h"
 #include "components/prefs/prefs_export.h"
 
-class PersistentPrefStore;
 class PrefNotifier;
-class PrefRegistry;
 class PrefStore;
 
 // The PrefValueStore manages various sources of values for Preferences
@@ -35,35 +33,6 @@ class PrefStore;
 class COMPONENTS_PREFS_EXPORT PrefValueStore {
  public:
   using PrefChangedCallback = base::RepeatingCallback<void(const std::string&)>;
-
-  // Delegate used to observe certain events in the |PrefValueStore|'s lifetime.
-  class Delegate {
-   public:
-    virtual ~Delegate() {}
-
-    // Called by the PrefValueStore constructor with the PrefStores passed to
-    // it.
-    virtual void Init(PrefStore* managed_prefs,
-                      PrefStore* supervised_user_prefs,
-                      PrefStore* extension_prefs,
-                      PrefStore* standalone_browser_prefs,
-                      PrefStore* command_line_prefs,
-                      PrefStore* user_prefs,
-                      PrefStore* recommended_prefs,
-                      PrefStore* default_prefs,
-                      PrefNotifier* pref_notifier) = 0;
-
-    virtual void InitIncognitoUserPrefs(
-        scoped_refptr<PersistentPrefStore> incognito_user_prefs_overlay,
-        scoped_refptr<PersistentPrefStore> incognito_user_prefs_underlay,
-        const std::vector<const char*>& overlay_pref_names) = 0;
-
-    virtual void InitPrefRegistry(PrefRegistry* pref_registry) = 0;
-
-    // Called whenever PrefValueStore::UpdateCommandLinePrefStore is called,
-    // with the same argument.
-    virtual void UpdateCommandLinePrefStore(PrefStore* command_line_prefs) = 0;
-  };
 
   // PrefStores must be listed here in order from highest to lowest priority.
   //   MANAGED contains all managed preferences that are provided by
@@ -117,8 +86,7 @@ class COMPONENTS_PREFS_EXPORT PrefValueStore {
                  PrefStore* user_prefs,
                  PrefStore* recommended_prefs,
                  PrefStore* default_prefs,
-                 PrefNotifier* pref_notifier,
-                 std::unique_ptr<Delegate> delegate = nullptr);
+                 PrefNotifier* pref_notifier);
 
   PrefValueStore(const PrefValueStore&) = delete;
   PrefValueStore& operator=(const PrefValueStore&) = delete;
@@ -138,8 +106,7 @@ class COMPONENTS_PREFS_EXPORT PrefValueStore {
       PrefStore* user_prefs,
       PrefStore* recommended_prefs,
       PrefStore* default_prefs,
-      PrefNotifier* pref_notifier,
-      std::unique_ptr<Delegate> delegate = nullptr);
+      PrefNotifier* pref_notifier);
 
   // A PrefValueStore can have exactly one callback that is directly
   // notified of preferences changing in the store. This does not
@@ -319,9 +286,6 @@ class COMPONENTS_PREFS_EXPORT PrefValueStore {
 
   // True if not all of the PrefStores were initialized successfully.
   bool initialization_failed_;
-
-  // Might be null.
-  std::unique_ptr<Delegate> delegate_;
 };
 
 namespace std {
