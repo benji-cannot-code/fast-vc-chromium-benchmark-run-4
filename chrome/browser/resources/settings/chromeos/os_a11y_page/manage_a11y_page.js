61f658ab684c79f30187b8af9a61f578b837b8a3
Josiah Krutz <josiahk@google.com>
2022-09-02 01:38:47 +0000

    [A11y Settings Revamp] Stay on Manage Accessibility page in Kiosk mode
    Stay on Manage Accessibility page in Kiosk mode, instead of redirecting.
    This is to maintain the current experience, and keep kiosk users from
    accessing other Chrome OS settings.
    
    Bug: 1292142
    Change-Id: I369f95669de50f5f304fcf16afee5153e7a508b3
    AX-Relnotes: Updates to Chrome OS Accessibility Settings to make accessibility options simpler / easier to use.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868695
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042417}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/manage_a11y_page.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/manage_a11y_page.js
@@ -415,7 +415,8 @@ class SettingsManageA11YPageElement extends SettingsManageA11YPageElementBase {
     /** @private {!DevicePageBrowserProxy} */
     this.deviceBrowserProxy_ = DevicePageBrowserProxyImpl.getInstance();
 
-    if (loadTimeData.getBoolean('isAccessibilityOSSettingsVisibilityEnabled')) {
+    if (loadTimeData.getBoolean('isAccessibilityOSSettingsVisibilityEnabled') &&
+        !this.isKioskModeActive_) {
       this.redirectToNewA11ySettings();
     }
   }
