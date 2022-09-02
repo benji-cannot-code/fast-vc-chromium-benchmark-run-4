abd68daf735a96c51ef6376b42f99ee34900956f
Jose Dapena Paz <jdapena@igalia.com>
2022-09-02 08:38:49 +0000

    GCC: move template specialization in audio parameter out of class.
    Fix build error in GCC as there is a template specialization in
    anonymous namespace inside a class.
    
    Bug: 819294
    Change-Id: I0f989895f8f6e9490aa53a1961136e14f844251d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864598
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042485}

--- media/base/audio_parameters.h
+++ media/base/audio_parameters.h
@@ -128,11 +128,6 @@ class MEDIA_SHMEM_EXPORT ChannelLayoutConfig {
     return ChannelLayoutConfig(layout, ChannelLayoutToChannelCount(layout));
   }
 
-  // For |CHANNEL_LAYOUT_DISCRETE|, we have to explicitly set the number of
-  // channels, so we need to use the normal constructor.
-  template <>
-  ChannelLayoutConfig FromLayout<CHANNEL_LAYOUT_DISCRETE>() = delete;
-
   static ChannelLayoutConfig Mono();
 
   static ChannelLayoutConfig Stereo();
@@ -147,6 +142,12 @@ class MEDIA_SHMEM_EXPORT ChannelLayoutConfig {
   ChannelLayout channel_layout_;  // Order of surround sound channels.
   int channels_;                  // Number of channels.
 };
+
+// For |CHANNEL_LAYOUT_DISCRETE|, we have to explicitly set the number of
+// channels, so we need to use the normal constructor.
+template <>
+ChannelLayoutConfig ChannelLayoutConfig::FromLayout<CHANNEL_LAYOUT_DISCRETE>() =
+    delete;
 
 class MEDIA_SHMEM_EXPORT AudioParameters {
  public:
