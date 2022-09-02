664fe9806255875f60b02f3ff82ccd6cbd975298
Kush Sinha <sinhak@chromium.org>
2022-09-02 13:48:04 +0000

    Code cleanup: Fix docs in `SigninErrorController`
    Fix `SigninErrorController`'s documentation. It mentions the usage of
    `AuthStatusProvider` to report auth errors. This is not true anymore.
    This class does not wait to get notified of errors anymore. Instead it
    discovers them proactively by listening to `IdentityManager`. Fix the
    documentation to reflect the current implementation.
    
    Change-Id: I4a6558af7cedd78372b482271edacf45f26c7c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3870404
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042549}

--- components/signin/core/browser/signin_error_controller.h
+++ components/signin/core/browser/signin_error_controller.h
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 
-// Keep track of auth errors and expose them to observers in the UI. Services
-// that wish to expose auth errors to the user should register an
-// AuthStatusProvider to report their current authentication state, and should
-// invoke AuthStatusChanged() when their authentication state may have changed.
+// Keeps track of auth errors and exposes them to observers in the UI.
+// Errors are discovered through `IdentityManager`, by tracking the error states
+// of either the Primary Account only, or all accounts - depending on the value
+// of `AccountMode`.
 class SigninErrorController : public KeyedService,
                               public signin::IdentityManager::Observer {
  public:
@@ -34,7 +34,7 @@ class SigninErrorController : public KeyedService,
   // observers when an error arises or changes.
   class Observer {
    public:
-    virtual ~Observer() {}
+    virtual ~Observer() = default;
     virtual void OnErrorChanged() = 0;
   };
 
@@ -94,8 +94,7 @@ class SigninErrorController : public KeyedService,
   // The account that generated the last auth error.
   CoreAccountId error_account_id_;
 
-  // The auth error detected the last time AuthStatusChanged() was invoked (or
-  // NONE if AuthStatusChanged() has never been invoked).
+  // The last detected auth error.
   GoogleServiceAuthError auth_error_;
 
   base::ObserverList<Observer, false>::Unchecked observer_list_;
