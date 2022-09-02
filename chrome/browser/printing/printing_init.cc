e59fb82ca978885e7724c126cab858ce134630bd
Alan Screen <awscreen@chromium.org>
2022-09-02 00:24:01 +0000

    Clarify naming of printing initialization
    With the addition of InitializeProcessForPrinting() there are now
    different types of printing initialization required depending on
    the process and scenario.
    
    Rename the legacy function InitializePrinting() to have a more
    specific name that identifies what kind of initialization it is for.
    
    Also do a little header cleanup for style.
    
    Bug: 1347255
    Change-Id: I7bdbcb18b83e28768ccd9f421a59231c3ff480e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866655
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042381}

--- chrome/browser/printing/printing_init.cc
+++ chrome/browser/printing/printing_init.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace printing {
 
-void InitializePrinting(content::WebContents* web_contents) {
+void InitializePrintingForWebContents(content::WebContents* web_contents) {
   // Native Headless mode uses a minimalistic Print Manager implementation that
   // shortcuts most of the callbacks providing only print to PDF functionality.
   if (headless::IsChromeNativeHeadless()) {
--- chrome/browser/printing/printing_init.h
+++ chrome/browser/printing/printing_init.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PRINTING_PRINTING_INIT_H__
-#define CHROME_BROWSER_PRINTING_PRINTING_INIT_H__
+#ifndef CHROME_BROWSER_PRINTING_PRINTING_INIT_H_
+#define CHROME_BROWSER_PRINTING_PRINTING_INIT_H_
 
 namespace content {
 class WebContents;
@@ -12,9 +12,9 @@ class WebContents;
 
 namespace printing {
 
-// Initialize printing related classes for web contents.
-void InitializePrinting(content::WebContents* web_contents);
+// Initialize printing related classes for a WebContents.
+void InitializePrintingForWebContents(content::WebContents* web_contents);
 
 }  // namespace printing
 
-#endif  // CHROME_BROWSER_PRINTING_PRINTING_INIT_H__
+#endif  // CHROME_BROWSER_PRINTING_PRINTING_INIT_H_
