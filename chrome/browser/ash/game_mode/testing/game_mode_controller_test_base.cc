4c766ffa5b55aa3a00d5ddc4f9506807bd3e398e
Matthew DeVore <matvore@chromium.org>
2022-09-02 06:47:33 +0000

    Split GameModeControllerTest according to mode type
    The split is not perfect. There is still a reference to Borealis game
    mode in the ARC test, for testing when switching directly between the
    two modes. The ARC test needs to use the HistogramTester so I put it in
    the parent class.
    
    Bug: b:244123832
    Test: GameModeControllerForArcTest+GameModeControllerForBorealisTest
    Change-Id: I8009e3e550e24034525e7e1c5a90b91f3bb0a82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867306
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042469}

---
+++ chrome/browser/ash/game_mode/testing/game_mode_controller_test_base.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include "chrome/browser/ash/game_mode/testing/game_mode_controller_test_base.h"
+
+namespace game_mode {
+
+void GameModeControllerTestBase::SetUp() {
+  ChromeAshTestBase::SetUp();
+  fake_resourced_client_ = new ash::FakeResourcedClient();
+  profile_ = std::make_unique<TestingProfile>();
+  game_mode_controller_ = std::make_unique<GameModeController>();
+  histogram_tester_ = std::make_unique<base::HistogramTester>();
+}
+
+void GameModeControllerTestBase::TearDown() {
+  game_mode_controller_.reset();
+  histogram_tester_.reset();
+  ash::ResourcedClient::Shutdown();
+  ChromeAshTestBase::TearDown();
+}
+
+}  // namespace game_mode
