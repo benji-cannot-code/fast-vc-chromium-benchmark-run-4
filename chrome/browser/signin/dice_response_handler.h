010b7b3878c6ea43f56fdd77eb2007ac02c4f026
David Roger <droger@chromium.org>
2022-09-02 18:47:27 +0000

    [signin] Delete kSupportOAuthOutageInDice
    This feature has been enabled for multiple years. The old codepath can
    be deleted.
    
    Change-Id: I2a493e5482556c90f667850a610d1d967c1b27a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3870484
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042699}

--- chrome/browser/signin/dice_response_handler.h
+++ chrome/browser/signin/dice_response_handler.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/cancelable_callback.h"
-#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
@@ -37,7 +36,6 @@ class IdentityManager;
 extern const int kDiceTokenFetchTimeoutSeconds;
 // Exposed for testing.
 extern const int kLockAccountReconcilorTimeoutHours;
-extern const base::Feature kSupportOAuthOutageInDice;
 
 // Delegate interface for processing a dice request.
 class ProcessDiceHeaderDelegate {
--- chrome/browser/signin/dice_response_handler_unittest.cc
+++ chrome/browser/signin/dice_response_handler_unittest.cc
@@ -8,14 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/bind.h"
 #include "base/check.h"
-#include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/ref_counted.h"
 #include "base/notreached.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "chrome/test/base/testing_profile.h"
@@ -29,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/test/browser_task_environment.h"
-#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -265,8 +260,6 @@ TEST_F(DiceResponseHandlerTest, Signin) {
 // Checks that the account reconcilor is blocked when where was OAuth
 // outage in Dice, and unblocked after the timeout.
 TEST_F(DiceResponseHandlerTest, SupportOAuthOutageInDice) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(kSupportOAuthOutageInDice);
   DiceResponseParams dice_params = MakeDiceParams(DiceAction::SIGNIN);
   dice_params.signin_info->authorization_code.clear();
   dice_params.signin_info->no_authorization_code = true;
@@ -286,8 +279,6 @@ TEST_F(DiceResponseHandlerTest, SupportOAuthOutageInDice) {
 // timeout still restarts.
 TEST_F(DiceResponseHandlerTest, CheckTimersDuringOutageinDice) {
   ASSERT_GT(kLockAccountReconcilorTimeoutHours, 3);
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(kSupportOAuthOutageInDice);
   // Create params for the first header with no authorization code.
   DiceResponseParams dice_params_1 = MakeDiceParams(DiceAction::SIGNIN);
   dice_params_1.signin_info->authorization_code.clear();
@@ -322,8 +313,6 @@ TEST_F(DiceResponseHandlerTest, CheckTimersDuringOutageinDice) {
 // Check that signin works normally (the token is fetched and added to chrome)
 // on valid headers after getting a no_authorization_code header.
 TEST_F(DiceResponseHandlerTest, CheckSigninAfterOutageInDice) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(kSupportOAuthOutageInDice);
   // Create params for the header with no authorization code.
   DiceResponseParams dice_params_1 = MakeDiceParams(DiceAction::SIGNIN);
   dice_params_1.signin_info->authorization_code.clear();
