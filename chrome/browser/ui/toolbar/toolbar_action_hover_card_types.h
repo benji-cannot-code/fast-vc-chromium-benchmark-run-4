57b0e2df0b8aea367d3f902d612debfc2b7e2133
Emilia Paz <emiliapaz@chromium.org>
2022-09-03 00:27:59 +0000

    [Extensions c2s] Add hover card content based on extensions information
    Add hover card content based on the extensions name and current site
    interaction.
    
    Screencast: https://drive.google.com/file/d/1QtvJcaN3-0VZ6bIEGZGBCy2rxpp4I81-/view?usp=sharing
    
    Bug: 1351778
    Change-Id: Idd175d344f372ec4ce403027dd472a5e90dabeba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846241
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042857}

--- chrome/browser/ui/toolbar/toolbar_action_hover_card_types.h
+++ chrome/browser/ui/toolbar/toolbar_action_hover_card_types.h
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // once the base hover card controller class once it's implemented.
 enum class ToolbarActionHoverCardUpdateType {
   kHover,
-  kToolbarActionRemoved,
-  kEvent
+  kEvent,
+  kToolbarActionUpdated,
+  kToolbarActionRemoved
 };
 
 #endif  // CHROME_BROWSER_UI_TOOLBAR_TOOLBAR_ACTION_HOVER_CARD_TYPES_H_
--- chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_ACTION_HOVER_CARD_BUBBLE_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_ACTION_HOVER_CARD_BUBBLE_VIEW_H_
 
+#include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_action_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 class ToolbarActionView;
+class WebContents;
 
 // Dialog that displays a hover card with extensions information.
 class ToolbarActionHoverCardBubbleView
@@ -24,14 +26,21 @@ class ToolbarActionHoverCardBubbleView
       const ToolbarActionHoverCardBubbleView&) = delete;
   ~ToolbarActionHoverCardBubbleView() override;
 
-  // Updates the hover card content.
-  // TODO(crbug.com/1351778): Update content based on a given `action_view`.
-  void UpdateCardContent();
+  // Updates the hover card content for `action_controller` in `web_contents`.
+  void UpdateCardContent(const ToolbarActionViewController* action_controller,
+                         content::WebContents* web_contents);
 
   // Update the text fade to the given percent, which should be between 0 and 1.
   void SetTextFade(double percent);
 
+  // Accessors used by tests.
+  std::u16string GetTitleTextForTesting() const;
+  std::u16string GetFootnoteTitleTextForTesting() const;
+  std::u16string GetFootnoteDescriptionTextForTesting() const;
+
  private:
+  friend class ToolbarActionHoverCardBubbleViewUITest;
+
   class FadeLabel;
   class FootnoteView;
 
@@ -42,6 +51,9 @@ class ToolbarActionHoverCardBubbleView
 
   raw_ptr<FadeLabel> title_label_ = nullptr;
   raw_ptr<FootnoteView> footnote_view_ = nullptr;
+
+  ToolbarActionViewController::HoverCardState state_ =
+      ToolbarActionViewController::HoverCardState::kExtensionDoesNotWantAccess;
 
   absl::optional<int> corner_radius_;
 };
