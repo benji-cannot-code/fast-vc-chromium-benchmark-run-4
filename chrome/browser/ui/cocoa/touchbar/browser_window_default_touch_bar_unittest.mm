d3496101f6a1cc5b9cb94b2875b4c666cc7d6241
Roland Bock <rbock@google.com>
2022-09-03 12:48:57 +0000

    Remove unused function ChangePrefValueStore from PrefService
    PrefService::ChangePrefValueStore and
    PrefServiceFactory::ChangePrefValueStore are unused.
    This CL removes them.
    
    Bug: None
    Change-Id: I690cf48c39f7c44cd8685a647fd87484ce2a0a1e
    Tests: Removed unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855565
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042914}

--- chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar_unittest.mm
+++ chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar_unittest.mm
@@ -62,8 +62,6 @@ class BrowserWindowDefaultTouchBarUnitTest : public BrowserWithTestWindowTest {
 
   void SetShowHomeButton(bool flag) {
     browser()->profile()->GetPrefs()->SetBoolean(prefs::kShowHomeButton, flag);
-    browser()->profile()->GetPrefs()->ChangePrefValueStore(nullptr, nullptr,
-                                                           nullptr, nullptr);
   }
 
   void TearDown() override {
