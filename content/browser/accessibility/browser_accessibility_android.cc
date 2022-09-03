d0f884c51477edf96c5abe62dd7d89863bd843b5
Mark Schillaci <mschillaci@google.com>
2022-09-03 00:07:58 +0000

    Do not report headings as collection items in accessibility Android
    This bug is a small fix to the nodes that receive the
    CollectionItemInfo object on Android. The object should be included
    for any collection items or table headers, however we were
    erroneously including it for all headings as well. This did not
    create a user impact because downstream AT ignored the conflicting/
    confusing data. We will now only pass the information for proper
    table headers.
    
    AX-Relnotes: N/A
    Bug: N/A
    Change-Id: I115d0bd9a24df9a384d20cc1cc40c54b746f8b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866551
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042850}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -285,7 +285,7 @@ bool BrowserAccessibilityAndroid::IsHeading() const {
   if (parent && parent->IsHeading())
     return true;
 
-  return ui::IsHeadingOrTableHeader(GetRole());
+  return ui::IsHeading(GetRole());
 }
 
 bool BrowserAccessibilityAndroid::IsHierarchical() const {
@@ -329,6 +329,10 @@ bool BrowserAccessibilityAndroid::IsSelected() const {
 
 bool BrowserAccessibilityAndroid::IsSlider() const {
   return GetRole() == ax::mojom::Role::kSlider;
+}
+
+bool BrowserAccessibilityAndroid::IsTableHeader() const {
+  return ui::IsTableHeader(GetRole());
 }
 
 bool BrowserAccessibilityAndroid::IsVisibleToUser() const {
