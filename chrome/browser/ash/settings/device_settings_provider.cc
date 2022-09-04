02a791b6198d07a62b95831a76cc297db0a65fec
Hong Xu <xuhong@chromium.org>
2022-09-04 21:11:56 +0000

    Remove kReportDeviceHardwareStatus
    `kReportDeviceHardwareStatus` is not being read anywhere.
    
    Bug: b:243960946
    Change-Id: I879bdd5756808721f02556e131986c7ae3438a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859376
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043028}

--- chrome/browser/ash/settings/device_settings_provider.cc
+++ chrome/browser/ash/settings/device_settings_provider.cc
@@ -134,7 +134,6 @@ const char* const kKnownSettings[] = {
     kReportDeviceCrashReportInfo,
     kReportDeviceCpuInfo,
     kReportDeviceFanInfo,
-    kReportDeviceHardwareStatus,
     kReportDeviceLocation,
     kReportDevicePeripherals,
     kReportDevicePowerStatus,
@@ -694,10 +693,6 @@ void DecodeReportingPolicies(const em::ChromeDeviceSettingsProto& policy,
     if (reporting_policy.has_report_users()) {
       new_values_cache->SetBoolean(kReportDeviceUsers,
                                    reporting_policy.report_users());
-    }
-    if (reporting_policy.has_report_hardware_status()) {
-      new_values_cache->SetBoolean(kReportDeviceHardwareStatus,
-                                   reporting_policy.report_hardware_status());
     }
     if (reporting_policy.has_report_session_status()) {
       new_values_cache->SetBoolean(kReportDeviceSessionStatus,
--- chrome/browser/ash/settings/device_settings_provider_unittest.cc
+++ chrome/browser/ash/settings/device_settings_provider_unittest.cc
@@ -102,7 +102,6 @@ class DeviceSettingsProviderTest : public DeviceSettingsTestBase {
     proto->set_report_network_configuration(enable_reporting);
     proto->set_report_network_status(enable_reporting);
     proto->set_report_users(enable_reporting);
-    proto->set_report_hardware_status(enable_reporting);
     proto->set_report_session_status(enable_reporting);
     proto->set_report_graphics_status(enable_reporting);
     proto->set_report_crash_report_info(enable_reporting);
@@ -192,7 +191,6 @@ class DeviceSettingsProviderTest : public DeviceSettingsTestBase {
         kReportDeviceNetworkConfiguration,
         kReportDeviceNetworkStatus,
         kReportDeviceUsers,
-        kReportDeviceHardwareStatus,
         kReportDevicePeripherals,
         kReportDevicePowerStatus,
         kReportDeviceStorageStatus,
