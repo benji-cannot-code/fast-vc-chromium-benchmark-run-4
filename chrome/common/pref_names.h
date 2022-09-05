bd6e35fd2a3ac034f1c841c1d777b63ab308ad20
Matt Mueller <mattm@chromium.org>
2022-09-05 20:43:39 +0000

    remove code for BuiltinCertificateVerifierEnabled enterprise policy
    Bug: 1358693
    Change-Id: I5adae3b0b61268cdcca9e429069982ead05edffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864462
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043275}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -1145,10 +1145,6 @@ extern const char kClientCertificateManagementAllowed[];
 extern const char kCACertificateManagementAllowed[];
 #endif
 
-#if BUILDFLAG(BUILTIN_CERT_VERIFIER_POLICY_SUPPORTED)
-extern const char kBuiltinCertificateVerifierEnabled[];
-#endif
-
 #if BUILDFLAG(CHROME_ROOT_STORE_POLICY_SUPPORTED)
 extern const char kChromeRootStoreEnabled[];
 #endif
