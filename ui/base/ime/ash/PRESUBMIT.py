eb39e168537c2b4ab5201d4263d34620b50d49c7
John Palmer <jopalmer@chromium.org>
2022-09-05 23:34:17 +0000

    Suggest that users add the tast trybot to ash ime changes
    If you don't add "MAGIC_TAG=luci.chrome.try:chromeos-betty-pi-arc-chrome" to your CL description, it prints a warning like
    
    Changes in this directory are high risk for breaking ChromeOS inputs, please include the tast trybot by adding: MAGIC_TAG=luci.chrome.try:chromeos-betty-pi-arc-chrome to your CL description
    
    MAGIC_TAG replaced with CQ_INCLUDE_TRYBOTS. Not including here to avoid triggering the trybot this time.
    
    Example warning: https://screenshot.googleplex.com/BC8qThJoxBY38Gy
    
    Change-Id: Iec99363b54443d8398d32f4e9d866a0c863822b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874218
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043285}

---
+++ ui/base/ime/ash/PRESUBMIT.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+def CheckTastIsRequested(input_api, output_api):
+  """Checks that the user did add the tast trybot to the description
+  """
+  keyword = 'CQ_INCLUDE_TRYBOTS=luci.chrome.try:chromeos-betty-pi-arc-chrome'
+  if not(keyword in input_api.change.DescriptionText()):
+    return [output_api.PresubmitPromptWarning(
+        'Changes in this directory are high risk for breaking ChromeOS inputs,'
+        + ' please include the tast trybot by adding: '
+        + keyword
+        + ' to your CL description')]
+
+  return []
+
+
+def CheckChange(input_api, output_api):
+    results = []
+    results += CheckTastIsRequested(input_api,output_api)
+    return results
+def CheckChangeOnUpload(input_api, output_api):
+    return CheckChange(input_api, output_api)
+def CheckChangeOnCommit(input_api, output_api):
+    return CheckChange(input_api, output_api)
