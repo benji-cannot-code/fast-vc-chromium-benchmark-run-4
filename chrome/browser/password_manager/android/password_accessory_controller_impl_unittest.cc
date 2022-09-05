28ae1263ce7de2d4231a64340185d3c51aeb08b0
Adem Derinel <derinel@google.com>
2022-09-05 10:46:29 +0000

    Passwords: Mfill: Always enable the non-empty usernames for filling
    Bug: 1247386
    Change-Id: I848e677187a85ace841cd4809e65d5fa40684724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864553
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043158}

--- chrome/browser/password_manager/android/password_accessory_controller_impl_unittest.cc
+++ chrome/browser/password_manager/android/password_accessory_controller_impl_unittest.cc
@@ -408,7 +408,8 @@ TEST_F(PasswordAccessoryControllerTest, ProvidesEmptySuggestionsMessage) {
 TEST_F(PasswordAccessoryControllerTest, PasswordFieldChangesSuggestionType) {
   CreateSheetController();
   cache()->SaveCredentialsAndBlocklistedForOrigin(
-      {CreateEntry("Ben", "S3cur3", GURL(kExampleSite), false, false).get()},
+      {CreateEntry("Ben", "S3cur3", GURL(kExampleSite), false, false).get(),
+       CreateEntry("", "p455w0rd", GURL(kExampleSite), false, false).get()},
       CredentialCache::IsOriginBlocklisted(false),
       url::Origin::Create(GURL(kExampleSite)));
   // Pretend a username field was focused. This should result in non-interactive
@@ -418,6 +419,10 @@ TEST_F(PasswordAccessoryControllerTest, PasswordFieldChangesSuggestionType) {
       RefreshSuggestions(
           PasswordAccessorySheetDataBuilder(passwords_title_str(kExampleDomain))
               .AddUserInfo(kExampleSite)
+              .AppendField(u"No username", u"No username", false, false)
+              .AppendField(u"p455w0rd", password_for_str(u"No username"), true,
+                           false)
+              .AddUserInfo(kExampleSite)
               .AppendField(u"Ben", u"Ben", false, true)
               .AppendField(u"S3cur3", password_for_str(u"Ben"), true, false)
               .Build()));
@@ -426,13 +431,18 @@ TEST_F(PasswordAccessoryControllerTest, PasswordFieldChangesSuggestionType) {
       /*is_manual_generation_available=*/false);
 
   // Pretend that we focus a password field now: By triggering a refresh with
-  // |is_password_field| set to true, all suggestions should become interactive.
+  // |is_password_field| set to true, all suggestions other than the empty
+  // username should become interactive.
   EXPECT_CALL(
       mock_manual_filling_controller_,
       RefreshSuggestions(
           PasswordAccessorySheetDataBuilder(passwords_title_str(kExampleDomain))
               .AddUserInfo(kExampleSite)
-              .AppendField(u"Ben", u"Ben", false, false)
+              .AppendField(u"No username", u"No username", false, false)
+              .AppendField(u"p455w0rd", password_for_str(u"No username"), true,
+                           true)
+              .AddUserInfo(kExampleSite)
+              .AppendField(u"Ben", u"Ben", false, true)
               .AppendField(u"S3cur3", password_for_str(u"Ben"), true, true)
               .Build()));
   controller()->RefreshSuggestionsForField(
@@ -890,7 +900,10 @@ TEST_F(PasswordAccessoryControllerTest, FillsUsername) {
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
 
-  AccessorySheetField selected_field(u"Ben", u"Ben", false, true);
+  AccessorySheetField selected_field(
+      /*display_text=*/u"Ben", /*text_to_fill=*/u"Ben",
+      /*a11y_description=*/u"Ben", /*id=*/"", /*is_obfuscated=*/false,
+      /*selectable=*/true);
   EXPECT_CALL(*driver(),
               FillIntoFocusedField(selected_field.is_obfuscated(),
                                    Eq(selected_field.display_text())));
@@ -915,7 +928,10 @@ TEST_F(PasswordAccessoryControllerTest, FillsPasswordIfNoAuthAvailable) {
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
 
-  AccessorySheetField selected_field(u"S3cur3", u"S3cur3", true, true);
+  AccessorySheetField selected_field(
+      /*display_text=*/u"S3cur3", /*text_to_fill=*/u"S3cur3",
+      /*a11y_description=*/u"S3cur3", /*id=*/"", /*is_obfuscated=*/true,
+      /*selectable=*/true);
   EXPECT_CALL(*password_client(), GetBiometricAuthenticator)
       .WillOnce(Return(mock_authenticator_));
   EXPECT_CALL(*mock_authenticator_.get(), CanAuthenticate)
@@ -944,7 +960,10 @@ TEST_F(PasswordAccessoryControllerTest, FillsPasswordIfAuthSuccessful) {
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
 
-  AccessorySheetField selected_field(u"S3cur3", u"S3cur3", true, true);
+  AccessorySheetField selected_field(
+      /*display_text=*/u"S3cur3", /*text_to_fill=*/u"S3cur3",
+      /*a11y_description=*/u"S3cur3", /*id=*/"", /*is_obfuscated=*/true,
+      /*selectable=*/true);
   ON_CALL(*password_client(), GetBiometricAuthenticator)
       .WillByDefault(Return(mock_authenticator_));
   EXPECT_CALL(*mock_authenticator_.get(), CanAuthenticate)
@@ -977,7 +996,10 @@ TEST_F(PasswordAccessoryControllerTest, DoesntFillPasswordIfAuthFails) {
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
 
-  AccessorySheetField selected_field(u"S3cur3", u"S3cur3", true, true);
+  AccessorySheetField selected_field(
+      /*display_text=*/u"S3cur3", /*text_to_fill=*/u"S3cur3",
+      /*a11y_description=*/u"S3cur3", /*id=*/"", /*is_obfuscated=*/true,
+      /*selectable=*/true);
   ON_CALL(*password_client(), GetBiometricAuthenticator)
       .WillByDefault(Return(mock_authenticator_));
   EXPECT_CALL(*mock_authenticator_.get(), CanAuthenticate)
@@ -1011,7 +1033,10 @@ TEST_F(PasswordAccessoryControllerTest, CancelsOngoingAuthIfDestroyed) {
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
 
-  AccessorySheetField selected_field(u"S3cur3", u"S3cur3", true, true);
+  AccessorySheetField selected_field(
+      /*display_text=*/u"S3cur3", /*text_to_fill=*/u"S3cur3",
+      /*a11y_description=*/u"S3cur3", /*id=*/"", /*is_obfuscated=*/true,
+      /*selectable=*/true);
   ON_CALL(*password_client(), GetBiometricAuthenticator)
       .WillByDefault(Return(mock_authenticator_));
   EXPECT_CALL(*mock_authenticator_.get(), CanAuthenticate)
--- components/autofill/core/browser/ui/accessory_sheet_data.cc
+++ components/autofill/core/browser/ui/accessory_sheet_data.cc
@@ -12,21 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// TODO(crbug.com/1224179) Delete this constructor.
-AccessorySheetField::AccessorySheetField(std::u16string display_text,
-                                         std::u16string a11y_description,
-                                         bool is_obfuscated,
-                                         bool selectable)
-    : display_text_(display_text),
-      text_to_fill_(std::move(display_text)),
-      a11y_description_(std::move(a11y_description)),
-      is_obfuscated_(is_obfuscated),
-      selectable_(selectable),
-      estimated_memory_use_by_strings_(
-          base::trace_event::EstimateMemoryUsage(display_text_) +
-          base::trace_event::EstimateMemoryUsage(text_to_fill_) +
-          base::trace_event::EstimateMemoryUsage(a11y_description_)) {}
-
 AccessorySheetField::AccessorySheetField(std::u16string display_text,
                                          std::u16string text_to_fill,
                                          std::u16string a11y_description,
