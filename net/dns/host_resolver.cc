6b00be847987db5c2e85936dd56cf93609752994
Yoichiro Hibara <hibarayoichiro@google.com>
2022-09-05 23:56:47 +0000

    Format to put net/dns/public/host_resolver_results.h in alphabetical order.
    This is a preparation for https://chromium-review.googlesource.com/c/chromium/src/+/3864601/.
    
    This CL helps make that big CL smaller.
    
    Change-Id: I483125923420c1ad0cddd1d2350c410bf1eaa57f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874724
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichiro Hibara <hibarayoichiro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043287}

--- net/dns/host_resolver.cc
+++ net/dns/host_resolver.cc
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/dns_util.h"
 #include "net/dns/host_cache.h"
 #include "net/dns/host_resolver_manager.h"
-#include "net/dns/public/host_resolver_results.h"
 #include "net/dns/mapped_host_resolver.h"
+#include "net/dns/public/host_resolver_results.h"
 #include "net/dns/resolve_context.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
--- net/dns/public/host_resolver_results.h
+++ net/dns/public/host_resolver_results.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_DNS_HOST_RESOLVER_RESULTS_H_
-#define NET_DNS_HOST_RESOLVER_RESULTS_H_
+#ifndef NET_DNS_PUBLIC_HOST_RESOLVER_RESULTS_H_
+#define NET_DNS_PUBLIC_HOST_RESOLVER_RESULTS_H_
 
 #include <string>
 #include <tuple>
@@ -46,4 +46,4 @@ struct NET_EXPORT_PRIVATE HostResolverEndpointResult {
 
 }  // namespace net
 
-#endif  // NET_DNS_HOST_RESOLVER_RESULTS_H_
+#endif  // NET_DNS_PUBLIC_HOST_RESOLVER_RESULTS_H_
