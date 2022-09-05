f67076c81b629b19eb4fd30232e81c6d8341c65c
Kush Sinha <sinhak@chromium.org>
2022-09-05 09:56:45 +0000

    Signin: Add << operator for `MultiloginParameters`
    `MultiloginParameters` is a non-trivial struct and is hard to debug from
    gTest error messages for AccountReconcilor test failures.
    
    Add a << operator for `MultiloginParameters` - which will be used by
    gTest to print meaningful object dumps on test failures.
    
    LOW_COVERAGE_REASON=Manually tested by checking output in unit tests
    
    Change-Id: Id007c89c1ddcc14606b92810957c5495ee28f878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3870813
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043144}

--- components/signin/public/base/multilogin_parameters.h
+++ components/signin/public/base/multilogin_parameters.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SIGNIN_PUBLIC_BASE_MULTILOGIN_PARAMETERS_H_
 #define COMPONENTS_SIGNIN_PUBLIC_BASE_MULTILOGIN_PARAMETERS_H_
 
+#include <ostream>
+#include <string>
 #include <vector>
 
 #include "google_apis/gaia/core_account_id.h"
@@ -22,6 +24,8 @@ struct MultiloginParameters {
   MultiloginParameters& operator=(const MultiloginParameters& other);
   ~MultiloginParameters();
 
+  std::string ToString() const;
+
   bool operator==(const MultiloginParameters& other) const {
     return mode == other.mode && accounts_to_send == other.accounts_to_send;
   }
@@ -34,6 +38,9 @@ struct MultiloginParameters {
       gaia::MultiloginMode::MULTILOGIN_UPDATE_COOKIE_ACCOUNTS_ORDER;
   std::vector<CoreAccountId> accounts_to_send;
 };
+
+std::ostream& operator<<(std::ostream& out, const MultiloginParameters& p);
+
 }  // namespace signin
 
 #endif  // COMPONENTS_SIGNIN_PUBLIC_BASE_MULTILOGIN_PARAMETERS_H_
