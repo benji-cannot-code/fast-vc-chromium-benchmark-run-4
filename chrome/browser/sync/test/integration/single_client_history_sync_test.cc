270e1199a9b123e9d2e43c1cef659f0682ca2b16
Marc Treib <treib@chromium.org>
2022-09-05 13:18:35 +0000

    Fix and re-enable SingleClientHistorySyncTest.*
    The issue was the classic `DCHECK(ThingWithSideEffects())`, which
    (apparently still) passes the CQ just fine, but then fails on the
    waterfall.
    
    LOW_COVERAGE_REASON=False positive, these *are* tests.
    
    Fixed: 1359319
    Change-Id: I517120e4c0a7f4603ec1acd8cfcc71687d5bd5c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3870530
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043191}

--- chrome/browser/sync/test/integration/single_client_history_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_history_sync_test.cc
@@ -168,8 +168,10 @@ class SingleClientHistorySyncTest : public SyncTest {
     // profiles/browsers it creates. Create an "empty" tab here, so that
     // NavigateToURL() will have a non-null WebContents to navigate in.
     for (int i = 0; i < num_clients(); ++i) {
-      DCHECK(AddTabAtIndexToBrowser(GetBrowser(0), 0, GURL("about:blank"),
-                                    ui::PAGE_TRANSITION_AUTO_TOPLEVEL));
+      if (!AddTabAtIndexToBrowser(GetBrowser(0), 0, GURL("about:blank"),
+                                  ui::PAGE_TRANSITION_AUTO_TOPLEVEL)) {
+        return false;
+      }
     }
 #endif
 
@@ -215,7 +217,7 @@ class SingleClientHistorySyncTest : public SyncTest {
 };
 
 IN_PROC_BROWSER_TEST_F(SingleClientHistorySyncTest,
-                       DISABLED_DoesNotUploadRetroactively) {
+                       DoesNotUploadRetroactively) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   // Navigate somewhere before Sync is turned on.
@@ -251,7 +253,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientHistorySyncTest,
                                                   UrlIs(synced_url2.spec()))));
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientHistorySyncTest, DISABLED_UploadsAllFields) {
+IN_PROC_BROWSER_TEST_F(SingleClientHistorySyncTest, UploadsAllFields) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   // Navigate to some URL, and make sure it shows up on the server.
