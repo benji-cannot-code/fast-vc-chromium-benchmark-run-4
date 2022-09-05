46d51ba62c5119c313697ef40c940a5127f48951
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-09-05 04:35:21 +0000

    system-extensions: Migrate tests that open a new window
    Moves creating new windows to the test itself rather than creating
    it before the test starts.
    
    Adds a new OpenBrowserWindow() method to the test helper.
    
    Bug: b:242264794
    Change-Id: Iab1d7ee795869f17520a4e605e95750285b07ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867305
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043069}

---
+++ chrome/browser/ash/system_extensions/api/window_management/test/cros_window_close.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+importScripts('test_support.js', 'cros_window_test_utils.js');
+
+promise_test(async (t) => {
+  // Open a browser window that takes focus.
+  const {windowId} = await testHelper.openBrowserWindow();
+
+  let windows = await chromeos.windowManagement.getWindows();
+  assert_equals(windows.length, 2);
+
+  let windowToClose = windows.find(window => window.id === windowId);
+  assert_not_equals(undefined, windowToClose,
+      `Could not find window with id: ${windowId};`);
+
+  const eventWatcher =
+    new EventWatcher(t, chromeos.windowManagement, ['windowclosed']);
+
+  windowToClose.close();
+
+  const event = await eventWatcher.wait_for(['windowclosed']);
+  assert_equals(event.window.id, windowId);
+
+  windows = await chromeos.windowManagement.getWindows();
+  assert_equals(windows.length, 1);
+});
+
+done();
