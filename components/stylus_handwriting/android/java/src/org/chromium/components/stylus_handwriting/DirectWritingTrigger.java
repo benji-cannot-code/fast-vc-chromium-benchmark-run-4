c66c8873c56d4067950b869d712c02f3a7f21bd7
Mahesh Machavolu <mahesh.ma@samsung.com>
2022-09-05 11:49:57 +0000

    Fix NPE during stylus handwriting
    NPE occurs during Action Up when the writing
    happens over an input field which can remove
    the focus or change the focus to another
    element during stylus writing. This is to
    protect from Browser crash.
    
    Bug: 1359992
    Change-Id: If53b07e1266faeea8acb4c948f8ba9c882942c81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3873453
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043168}

--- components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/DirectWritingTrigger.java
+++ components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/DirectWritingTrigger.java
@@ -343,6 +343,7 @@ class DirectWritingTrigger
                 } else {
                     // Handle ACTION_UP in editable field, to show DW Toolbar.
                     if (mEditableNodeBounds != null && !mEditableNodeBounds.isEmpty()
+                            && mCurrentStylusDownEvent != null
                             && mEditableNodeBounds.contains((int) mCurrentStylusDownEvent.getX(),
                                     (int) mCurrentStylusDownEvent.getY())) {
                         onStopRecognition(me, mEditableNodeBounds);
