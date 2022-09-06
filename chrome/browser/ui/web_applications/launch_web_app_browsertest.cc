91fb9e1f567ae91bd8d6ad50638b5ef1851a5955
Eric Willigers <ericwilligers@chromium.org>
2022-09-06 06:26:08 +0000

    LaunchWebAppBrowserTest: Uninstall web app
    We uninstall the web app at the end of the test.
    
    Change-Id: I17c26f3831524ac49dfb2960590ac541172da2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875069
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043331}

--- chrome/browser/ui/web_applications/launch_web_app_browsertest.cc
+++ chrome/browser/ui/web_applications/launch_web_app_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/web_app_controller_browsertest.h"
 #include "chrome/browser/web_applications/test/app_registration_waiter.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/browser/notification_service.h"
@@ -53,7 +54,7 @@ IN_PROC_BROWSER_TEST_F(LaunchWebAppBrowserTest, OpenLinkInWebApp) {
   if (!IsServiceAvailable())
     return;
 
-  const GURL start_url("https://app.site.test/");
+  const GURL start_url("https://app.site.test/example/index");
   const AppId app_id = InstallPWA(start_url);
   AppRegistrationWaiter(profile(), app_id).Await();
 
@@ -87,6 +88,11 @@ IN_PROC_BROWSER_TEST_F(LaunchWebAppBrowserTest, OpenLinkInWebApp) {
   EXPECT_EQ(start_url, app_browser->tab_strip_model()
                            ->GetActiveWebContents()
                            ->GetLastCommittedURL());
+
+  UninstallWebApp(app_id);
+  web_app::AppRegistrationWaiter(profile(), app_id,
+                                 apps::Readiness::kUninstalledByUser)
+      .Await();
 }
 
 }  // namespace web_app
