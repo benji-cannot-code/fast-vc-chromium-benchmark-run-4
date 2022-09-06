0d971df2fec3d9f22582b61e1ed65177a662f5ee
Mason Freed <masonf@chromium.org>
2022-09-06 16:07:51 +0000

    Pass in the ExecutionContext when constructing TextTrack
    The previous code was trying to grab the EC from its owning
    MediaElement. Rather than do that, just pass it in on construction.
    See the discussions at [1] and [2].
    
    This should be tested by this test:
    
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/dom/events/event_target_test.cc;l=91;drc=be3d84bb8de1681489e229937105e3f075a7c824
    
    [1] https://groups.google.com/a/chromium.org/g/dom-dev/c/oIb1oR17mJc
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/3858841
    
    Bug: 1357453
    Change-Id: Ib415982eaab081c7913487eb166a10f66d51861a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866453
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1043467}

--- third_party/blink/renderer/core/html/track/text_track.h
+++ third_party/blink/renderer/core/html/track/text_track.h
@@ -41,7 +41,9 @@ namespace blink {
 
 class CueTimeline;
 class ExceptionState;
+class ExecutionContext;
 class HTMLMediaElement;
+class HTMLElement;
 class TextTrack;
 class TextTrackCue;
 class TextTrackCueList;
@@ -59,6 +61,7 @@ class CORE_EXPORT TextTrack : public EventTargetWithInlineData,
   TextTrack(const AtomicString& kind,
             const AtomicString& label,
             const AtomicString& language,
+            HTMLElement* source_element,
             const AtomicString& id = g_empty_atom,
             TextTrackType = kAddTrack);
   ~TextTrack() override;
@@ -149,6 +152,7 @@ class CORE_EXPORT TextTrack : public EventTargetWithInlineData,
   HeapVector<Member<CSSStyleSheet>> style_sheets_;
 
   Member<TextTrackList> track_list_;
+  Member<HTMLElement> source_element_;
   TextTrackMode mode_ = TextTrackMode::kDisabled;
   TextTrackType track_type_;
   ReadinessState readiness_state_;
