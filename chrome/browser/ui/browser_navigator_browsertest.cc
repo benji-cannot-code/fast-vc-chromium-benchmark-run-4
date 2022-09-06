b54260827a7cdb69d65e796eba9e8164cfe014ad
Eric Willigers <ericwilligers@chromium.org>
2022-09-06 21:25:18 +0000

    BrowserNavigatorTest: Remove unused includes
    No web apps are installed in browser_navigator_browsertest.cc
    
    Change-Id: Ie4f53c9123f0975e4dfcb9f1641f849e72542922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875067
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043646}

--- chrome/browser/ui/browser_navigator_browsertest.cc
+++ chrome/browser/ui/browser_navigator_browsertest.cc
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/web_applications/web_app_helpers.h"
-#include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
