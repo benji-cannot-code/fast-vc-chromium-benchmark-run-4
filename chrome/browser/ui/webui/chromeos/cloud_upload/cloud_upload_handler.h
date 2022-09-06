ff6d291afb5a6289b02d37df1db6420e8ae90635
Jérémie Boulic <jboulic@chromium.org>
2022-09-06 00:10:24 +0000

    [Office upload] Add progress notification
    Add office upload notification combining the move and syncing progress.
    
    Bug: b:242685213
    Change-Id: Ibb51e8e6982f5483bdb309ff358d8ad04044ec93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868639
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043292}

--- chrome/browser/ui/webui/chromeos/cloud_upload/cloud_upload_handler.h
+++ chrome/browser/ui/webui/chromeos/cloud_upload/cloud_upload_handler.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/io_task_controller.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/chromeos/extensions/file_manager/scoped_suppress_drive_notifications_for_path.h"
+#include "chrome/browser/ui/webui/chromeos/cloud_upload/cloud_upload_notification_manager.h"
 #include "chromeos/ash/components/drivefs/drivefs_host_observer.h"
 #include "chromeos/ash/components/drivefs/mojom/drivefs.mojom.h"
 #include "storage/browser/file_system/file_system_context.h"
@@ -57,6 +58,9 @@ class CloudUploadHandler
   // Starts the upload workflow. Initiated by the `UploadToCloud` static method.
   void Run(UploadCallback callback);
 
+  // Updates the progress notification for the upload workflow (move + syncing).
+  void UpdateProgressNotification();
+
   // Ends upload and runs Upload callback.
   void OnEndUpload(GURL hosted_url);
 
@@ -84,11 +88,14 @@ class CloudUploadHandler
   scoped_refptr<storage::FileSystemContext> file_system_context_;
   file_manager::io_task::IOTaskController* io_task_controller_;
   drive::DriveIntegrationService* const drive_integration_service_;
+  CloudUploadNotificationManager notification_manager_;
   const storage::FileSystemURL source_url_;
   file_manager::io_task::IOTaskId observed_task_id_;
   base::FilePath observed_relative_drive_path_;
   bool error_found_;
   bool upload_done_;
+  int move_progress_ = 0;
+  int sync_progress_ = 0;
   UploadCallback callback_;
   std::unique_ptr<file_manager::ScopedSuppressDriveNotificationsForPath>
       scoped_suppress_drive_notifications_for_path_ = nullptr;
