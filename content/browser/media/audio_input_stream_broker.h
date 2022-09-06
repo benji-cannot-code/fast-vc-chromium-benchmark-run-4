a33f0f1b99263bff38785d09155727b3d4ccd442
Sergey Ulanov <sergeyu@chromium.org>
2022-09-06 23:46:43 +0000

    Allow overriding AudioStreamBrokerFactory
    1. Moved AudioStreamBrokerFactory interfaces to //content/public
    2. Added WebContents::SetAudioStreamBrokerFactory to make it possible to
       override the default implementation.
    
    WebEngine on Fuchsia will use a new AudioStreamBrokerFactory
    implementation in order to set renderer usage in AudioParamters for
    outgoing streams.
    
    Bug: 1253010
    Change-Id: I35708b75c7ac2395a19b13d8d8f04d736f4537d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863320
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043717}

--- content/browser/media/audio_input_stream_broker.h
+++ content/browser/media/audio_input_stream_broker.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "content/browser/media/audio_stream_broker.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/audio_stream_broker.h"
 #include "media/base/audio_parameters.h"
 #include "media/mojo/mojom/audio_data_pipe.mojom.h"
 #include "media/mojo/mojom/audio_input_stream.mojom.h"
--- content/public/browser/audio_stream_broker.h
+++ content/public/browser/audio_stream_broker.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_MEDIA_AUDIO_STREAM_BROKER_H_
-#define CONTENT_BROWSER_MEDIA_AUDIO_STREAM_BROKER_H_
+#ifndef CONTENT_PUBLIC_BROWSER_AUDIO_STREAM_BROKER_H_
+#define CONTENT_PUBLIC_BROWSER_AUDIO_STREAM_BROKER_H_
 
 #include <cstdint>
 #include <memory>
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "content/common/content_export.h"
-#include "media/mojo/mojom/audio_input_stream.mojom.h"
 #include "media/mojo/mojom/audio_output_stream.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/mojom/media/renderer_audio_input_stream_factory.mojom.h"
@@ -27,7 +26,7 @@ class UserInputMonitorBase;
 namespace mojom {
 class AudioStreamFactory;
 }
-}
+}  // namespace media
 
 namespace content {
 
@@ -138,4 +137,4 @@ class CONTENT_EXPORT AudioStreamBrokerFactory {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_MEDIA_AUDIO_STREAM_BROKER_H_
+#endif  // CONTENT_PUBLIC_BROWSER_AUDIO_STREAM_BROKER_H_
