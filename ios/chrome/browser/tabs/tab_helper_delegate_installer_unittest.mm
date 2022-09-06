705c130a5b7d1ba2817f1004f3f4f02b7e058f4d
Gauthier Ambard <gambard@chromium.org>
2022-09-06 18:38:16 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/tabs
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1311955
    Change-Id: I51684ba90451ba91eae8a0af178c6719774a4a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865951
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043542}

--- ios/chrome/browser/tabs/tab_helper_delegate_installer_unittest.mm
+++ ios/chrome/browser/tabs/tab_helper_delegate_installer_unittest.mm
@@ -179,8 +179,8 @@ TEST_F(TabHelperDelegateInstallerTest, InstallDelegatesForReplacedWebStates) {
   FakeTabHelper* replacement_tab_helper = FakeTabHelper::FromWebState(
       browser_->GetWebStateList()->GetWebStateAt(0));
 
-  // Verify that the delegates were uninstalled from |tab_helper| and installed
-  // for |replacement_tab_helper|.
+  // Verify that the delegates were uninstalled from `tab_helper` and installed
+  // for `replacement_tab_helper`.
   EXPECT_FALSE(tab_helper->GetDelegate());
   EXPECT_FALSE(tab_helper->GetSecondDelegate());
   EXPECT_EQ(replacement_tab_helper->GetDelegate(), &delegate_);
@@ -208,7 +208,7 @@ TEST_F(TabHelperDelegateInstallerTest,
   std::unique_ptr<web::WebState> detached_web_state =
       browser_->GetWebStateList()->DetachWebStateAt(0);
 
-  // Verify that the delegates were uninstalled from |tab_helper|.
+  // Verify that the delegates were uninstalled from `tab_helper`.
   EXPECT_FALSE(tab_helper->GetDelegate());
   EXPECT_FALSE(tab_helper->GetSecondDelegate());
 }
@@ -249,7 +249,7 @@ TEST_F(TabHelperDelegateInstallerTest,
     browser_.reset();
   }
 
-  // Verify that the delegates were uninstalled from |tab_helper|.
+  // Verify that the delegates were uninstalled from `tab_helper`.
   EXPECT_FALSE(set_delegate);
   EXPECT_FALSE(set_second_delegate);
 }
@@ -277,7 +277,7 @@ TEST_F(TabHelperDelegateInstallerTest,
   installer = nullptr;
   second_installer = nullptr;
 
-  // Verify that the delegates were uninstalled from |tab_helper|.
+  // Verify that the delegates were uninstalled from `tab_helper`.
   EXPECT_FALSE(tab_helper->GetDelegate());
   EXPECT_FALSE(tab_helper->GetSecondDelegate());
 }
