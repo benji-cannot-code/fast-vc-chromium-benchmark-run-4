040985742f2b39cff5eb4d68c44dc01f2f1072f7
Georgi Stephanov <gstephanov@google.com>
2022-09-06 11:43:17 +0000

    Change the server URL for GetTriggerScriptsByHashPrefix
    Follow up change to the previous CL[0] which introduced the usage of
    a backend RPC that allows querying for available Assistant Autofill
    trigger scripts in a privacy sensitive way.
    
    This change replaces the URL so that the correct endpoint is hit.
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/3823166
    
    Bug: b/242039152
    Change-Id: I94e235f4c45e55484c7cb707cdee93ef1c53cf0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865014
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Georgi Stephanov <gstephanov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043387}

--- components/autofill_assistant/browser/service/server_url_fetcher.cc
+++ components/autofill_assistant/browser/service/server_url_fetcher.cc
@@ -17,6 +17,7 @@ const char kDefaultAutofillAssistantServerUrl[] =
 const char kScriptEndpoint[] = "/v1/supportsSite2";
 const char kActionEndpoint[] = "/v1/actions2";
 const char kTriggersEndpoint[] = "/v1/triggers";
+const char kTriggersByHashPrefixEndpoint[] = "/v1/triggerScriptsByHashPrefix";
 const char kCapabilitiesByHashEndpoint[] = "/v1/capabilitiesByHashPrefix2";
 const char kUserDataEndpoint[] = "/v1/userData";
 const char kProgressEndpoint[] = "/v1/reportProgress";
@@ -60,6 +61,12 @@ GURL ServerUrlFetcher::GetNextActionsEndpoint() const {
 GURL ServerUrlFetcher::GetTriggerScriptsEndpoint() const {
   GURL::Replacements trigger_replacements;
   trigger_replacements.SetPathStr(kTriggersEndpoint);
+  return server_url_.ReplaceComponents(trigger_replacements);
+}
+
+GURL ServerUrlFetcher::GetTriggerScriptsByHashPrefixEndpoint() const {
+  GURL::Replacements trigger_replacements;
+  trigger_replacements.SetPathStr(kTriggersByHashPrefixEndpoint);
   return server_url_.ReplaceComponents(trigger_replacements);
 }
 
