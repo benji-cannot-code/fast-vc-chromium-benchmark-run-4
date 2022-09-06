8fce5499ab18a984e83eb89d2f1c0be02b0f6ff1
Brahim Chikhaoui <bchikhaoui@google.com>
2022-09-06 12:56:52 +0000

    OOBE - Polymer3 - Check-in Wrong_HWID_Screen
    Check-in the modularised version of OOBE's Wrong_HWID_Screen.
    
    Bug: 1184731, b/243382991
    Change-Id: Ida9d8a9416740c463f62de5da9e3abd99db68c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3873287
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043397}

--- chrome/browser/resources/chromeos/login/screens/common/wrong_hwid.js
+++ chrome/browser/resources/chromeos/login/screens/common/wrong_hwid.js
@@ -7,7 +7,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview wrong HWID screen implementation.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+import '../../components/buttons/oobe_text_button.m.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OOBE_UI_STATE, SCREEN_GAIA_SIGNIN} from '../../components/display_manager_types.m.js';
+
 
 /**
  * @constructor
@@ -15,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {LoginScreenBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
  */
-const WrongHWIDBase = Polymer.mixinBehaviors(
+const WrongHWIDBase = mixinBehaviors(
     [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    Polymer.Element);
+    PolymerElement);
 
 /**
  * @polymer
@@ -27,7 +40,11 @@ class WrongHWID extends WrongHWIDBase {
     return 'wrong-hwid-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+
 
   static get properties() {
     return {};
