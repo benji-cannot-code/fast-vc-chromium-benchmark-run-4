63108b680936b0a4040d5e8f35e88ec65d9c6d01
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-09-06 20:29:26 +0000

    Make PaintOpBuffer::Iterator::operator* return by reference.
    C++ dereference operators are supposed to return a reference, not a
    pointer:
    https://en.cppreference.com/w/cpp/language/operator_member_access
    
    Returning a pointer meant that callers had to double-dereference
    iterators, defeating the purpose of the `operator*`:
    `if (**left_iter != **right_iter)`
    vs.
    `if (*left_iter != *right_iter)`
    
    Moreover, the fact that dereferencing the iterators produced a pointer
    had trickling effects on surrounding code, as pointers were used
    everywhere for non-optional variables and parameters. Google's C++ style
    guide recommends references for non-optional function parameters:
    https://google.github.io/styleguide/cppguide.html#Inputs_and_Outputs
    
    Pointers should always be looked at from a place of skepticism: "is it
    safe to dereference this pointer here?". None of the `PaintOp*` where
    checked before dereferencing. Moving dereferences to choke points and
    using references everywhere else makes the code safer, with pointer
    safety being easier to reason about.
    
    Change-Id: Ibde05c3a9080e1fa1118ca256bba8456e1f692c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864052
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043605}

--- cc/paint/paint_op_buffer_serializer.h
+++ cc/paint/paint_op_buffer_serializer.h
@@ -21,7 +21,7 @@ namespace cc {
 class CC_PAINT_EXPORT PaintOpBufferSerializer {
  public:
   using SerializeCallback =
-      base::RepeatingCallback<size_t(const PaintOp*,
+      base::RepeatingCallback<size_t(const PaintOp&,
                                      const PaintOp::SerializeOptions&,
                                      const PaintFlags*,
                                      const SkM44&,
@@ -92,16 +92,16 @@ class CC_PAINT_EXPORT PaintOpBufferSerializer {
                                  const std::vector<size_t>* offsets);
   // Returns whether searilization of |op| succeeded and we need to serialize
   // the next PaintOp in the PaintOpBuffer.
-  bool WillSerializeNextOp(const PaintOp* op,
+  bool WillSerializeNextOp(const PaintOp& op,
                            SkCanvas* canvas,
                            PlaybackParams params,
                            uint8_t alpha);
   bool SerializeOpWithFlags(SkCanvas* canvas,
-                            const PaintOpWithFlags* flags_op,
+                            const PaintOpWithFlags& flags_op,
                             const PlaybackParams& params,
                             uint8_t alpha);
   bool SerializeOp(SkCanvas* canvas,
-                   const PaintOp* op,
+                   const PaintOp& op,
                    const PaintFlags* flags_to_serialize,
                    const PlaybackParams& params);
   void Save(SkCanvas* canvas, const PlaybackParams& params);
@@ -112,7 +112,7 @@ class CC_PAINT_EXPORT PaintOpBufferSerializer {
                             const Preamble& preamble,
                             const PlaybackParams& params);
   void PlaybackOnAnalysisCanvas(SkCanvas* canvas,
-                                const PaintOp* op,
+                                const PaintOp& op,
                                 const PaintFlags* flags_to_serialize,
                                 const PlaybackParams& params);
 
@@ -133,7 +133,7 @@ class CC_PAINT_EXPORT SimpleBufferSerializer : public PaintOpBufferSerializer {
   size_t written() const { return written_; }
 
  private:
-  size_t SerializeToMemory(const PaintOp* op,
+  size_t SerializeToMemory(const PaintOp& op,
                            const PaintOp::SerializeOptions& options,
                            const PaintFlags* flags_to_serialize,
                            const SkM44& current_ctm,
