535f3c2e770c71b27ab8ce06dce6386e68087ad5
Thanh Nguyen <thanhdng@chromium.org>
2022-09-06 05:47:49 +0000

    [Adaptive Charging] Fix a typo in variable name
    Fix a typo
    
    BUG=b:240481357
    
    Change-Id: I219739fe9b1fc2fc21a84279cbc5794203a25a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874635
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043329}

--- chrome/browser/ash/power/smart_charging/smart_charging_manager.cc
+++ chrome/browser/ash/power/smart_charging/smart_charging_manager.cc
@@ -217,7 +217,7 @@ std::unique_ptr<SmartChargingManager> SmartChargingManager::CreateInstance() {
   DCHECK(detector);
 
   mojo::PendingRemote<viz::mojom::VideoDetectorObserver> video_observer;
-  std::unique_ptr<SmartChargingManager> screen_brightness_manager =
+  std::unique_ptr<SmartChargingManager> smart_charging_manager =
       std::make_unique<SmartChargingManager>(
           detector, video_observer.InitWithNewPipeAndPassReceiver(),
           session_manager::SessionManager::Get(),
@@ -228,7 +228,7 @@ std::unique_ptr<SmartChargingManager> SmartChargingManager::CreateInstance() {
       ->GetHostFrameSinkManager()
       ->AddVideoDetectorObserver(std::move(video_observer));
 
-  return screen_brightness_manager;
+  return smart_charging_manager;
 }
 
 void SmartChargingManager::OnUserActivity(const ui::Event* event) {
