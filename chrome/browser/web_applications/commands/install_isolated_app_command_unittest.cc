b6693cde171321b61be667e8baa857408092dfe0
Robbie McElrath <rmcelrath@chromium.org>
2022-09-06 21:18:27 +0000

    Set WebApp::IsolationData during IWA installation
    Bug: 1359087
    Change-Id: I82ee54cc95a6be35a2196fb74dd91b61b40066b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871520
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043641}

--- chrome/browser/web_applications/commands/install_isolated_app_command_unittest.cc
+++ chrome/browser/web_applications/commands/install_isolated_app_command_unittest.cc
@@ -413,7 +413,7 @@ TEST_F(InstallIsolatedAppCommandTest,
 }
 
 TEST_F(InstallIsolatedAppCommandTest,
-       InstallationFinalizedWithIsolatedAppDebInstallInstallSource) {
+       InstallationFinalizedWithIsolatedAppDevInstallInstallSource) {
   SetPrepareForLoadResultLoaded();
 
   ExpectLoadedForURL("http://test-url-example.com");
@@ -493,6 +493,21 @@ TEST_F(InstallIsolatedAppCommandTest,
   EXPECT_THAT(ExecuteCommand("http://test-url-example.com",
                              std::move(fake_data_retriever)),
               IsInstallationError(HasSubstr("Manifest is null")));
+}
+
+TEST_F(InstallIsolatedAppCommandTest, IsolationDataSentToFinalizer) {
+  SetPrepareForLoadResultLoaded();
+
+  std::string url("http://test-url-example.com/");
+  ExpectLoadedForURL(url);
+
+  EXPECT_THAT(ExecuteCommand(url), IsInstallationOk());
+
+  EXPECT_THAT(
+      install_finalizer().finalize_options_list(),
+      ElementsAre(Field(
+          &WebAppInstallFinalizer::FinalizeOptions::isolation_data,
+          Eq(IsolationData(IsolationData::DevModeProxy{.proxy_url = url})))));
 }
 
 using InstallIsolatedAppCommandManifestTest = InstallIsolatedAppCommandTest;
