e205fc3115ad79b7be401dc44fd564bb80619c37
Wenyu Fu <wenyufu@google.com>
2022-09-06 21:43:32 +0000

    [CT][EngagementSignals] Add unit tests for GestureListenerManagerImpl
    Bug: 1358717
    Change-Id: I4fb449520a02944e5d6d7beabcc3ab0e9cc560a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868407
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043656}

--- content/public/android/java/src/org/chromium/content/browser/GestureListenerManagerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/GestureListenerManagerImpl.java
@@ -236,7 +236,8 @@ public class GestureListenerManagerImpl
     }
 
     @CalledByNative
-    private void onFlingEnd() {
+    @VisibleForTesting
+    void onFlingEnd() {
         mHasActiveFlingScroll = false;
         for (mIterator.rewind(); mIterator.hasNext();) {
             mIterator.next().onFlingEndGesture(verticalScrollOffset(), verticalScrollExtent());
@@ -244,7 +245,8 @@ public class GestureListenerManagerImpl
     }
 
     @CalledByNative
-    private void onEventAck(int event, boolean consumed, float scrollOffsetX, float scrollOffsetY) {
+    @VisibleForTesting
+    void onEventAck(int event, boolean consumed, float scrollOffsetX, float scrollOffsetY) {
         switch (event) {
             case EventType.GESTURE_FLING_START:
                 // If we're here, then |consumed| is false as otherwise #onFlingStart() would have
@@ -310,7 +312,8 @@ public class GestureListenerManagerImpl
      * Called when a gesture event ack happens for |EventType.GESTURE_SCROLL_BEGIN|.
      */
     @CalledByNative
-    private void onScrollBegin(boolean isDirectionUp) {
+    @VisibleForTesting
+    void onScrollBegin(boolean isDirectionUp) {
         setGestureScrollInProgress(true);
         for (mIterator.rewind(); mIterator.hasNext();) {
             mIterator.next().onScrollStarted(
@@ -322,7 +325,8 @@ public class GestureListenerManagerImpl
      * Called when a gesture event ack happens for |EventType.GESTURE_FLING_START|.
      */
     @CalledByNative
-    private void onFlingStart(boolean isDirectionUp) {
+    @VisibleForTesting
+    void onFlingStart(boolean isDirectionUp) {
         // The view expects the fling velocity in pixels/s.
         mHasActiveFlingScroll = true;
         for (mIterator.rewind(); mIterator.hasNext();) {
@@ -337,7 +341,8 @@ public class GestureListenerManagerImpl
     }
 
     @CalledByNative
-    private void resetPopupsAndInput(boolean renderProcessGone) {
+    @VisibleForTesting
+    void resetPopupsAndInput(boolean renderProcessGone) {
         PopupController.hidePopupsAndClearSelection(mWebContents);
         resetScrollInProgress();
         if (renderProcessGone) {
