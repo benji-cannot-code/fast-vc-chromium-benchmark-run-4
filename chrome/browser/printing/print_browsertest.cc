95af7a317ee95242c7620741453fe702b569f9ea
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-09-06 17:33:37 +0000

    Add missing DidGetPrintedPagesCount call to SnapshotForContentAnalysis
    This missing call fixes crbug.com/1358766 when running browser_tests on
    Windows with --gtest_also_run_disabled_tests.
    
    Bug: 1358766
    Change-Id: I64e516271441feb07ec52bd9e4d95fcca42362ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871114
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043501}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -3468,8 +3468,7 @@ IN_PROC_BROWSER_TEST_P(SystemAccessProcessServicePrintBrowserTest,
 
 #endif  // !BUILDFLAG(IS_CHROMEOS)
 
-// TODO(crbug.com/1256506): Re-enable test on Windows.  Must also wait until
-// crbug.com/1358766 is resolved.
+// TODO(crbug.com/1256506): Re-enable test on Windows.
 #if BUILDFLAG(IS_WIN)
 #define MAYBE_ContentAnalysisPrintBrowserTest \
   DISABLED_ContentAnalysisPrintBrowserTest
@@ -3534,8 +3533,7 @@ class MAYBE_ContentAnalysisPrintBrowserTest
   base::test::ScopedFeatureList feature_list_;
 };
 
-// TODO(crbug.com/1256506): Re-enable test on Windows.  Must also wait until
-// crbug.com/1358766 is resolved.
+// TODO(crbug.com/1256506): Re-enable test on Windows.
 #if BUILDFLAG(IS_WIN)
 #define MAYBE_ContentAnalysisScriptedPreviewlessPrintBrowserTest \
   DISABLED_ContentAnalysisScriptedPreviewlessPrintBrowserTest
@@ -3661,14 +3659,12 @@ IN_PROC_BROWSER_TEST_P(MAYBE_ContentAnalysisPrintBrowserTest,
 }
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-// TODO(crbug.com/1256506): Re-enable test on Windows.  Must also wait until
-// crbug.com/1358766 is resolved.
+// TODO(crbug.com/1256506): Re-enable test on Windows.
 INSTANTIATE_TEST_SUITE_P(All,
                          MAYBE_ContentAnalysisPrintBrowserTest,
                          testing::Bool());
 
-// TODO(crbug.com/1256506): Re-enable test on Windows.  Must also wait until
-// crbug.com/1358766 is resolved.
+// TODO(crbug.com/1256506): Re-enable test on Windows.
 // This test suite doesn't run on CrOS since it doesn't support non-print
 // preview scripted printing.
 #if !BUILDFLAG(IS_CHROMEOS)
