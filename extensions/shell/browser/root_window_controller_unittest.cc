5c42eaa91757bc05dd9f68f743a8f7c192124014
Qi Tiezheng <qitiezheng@360.cn>
2022-09-06 18:43:56 +0000

    [extensions] Fix some cpplint errors, part1
    extensions/browser/updater/safe_manifest_parser_unittest.cc:11:
      "extensions/browser/updater/safe_manifest_parser.h" already included at extensions/browser/updater/safe_manifest_parser_unittest.cc:5  [build/include] [4]
    extensions/shell/browser/root_window_controller_unittest.cc:16:
      "extensions/shell/browser/root_window_controller.h" already included at extensions/shell/browser/root_window_controller_unittest.cc:5  [build/include] [4]
    
    Bug: None
    Change-Id: Ic2461809042c37f86832fa7e88d4249eadd7d9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859145
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Qi Tiezheng <qitiezheng@360.cn>
    Cr-Commit-Position: refs/heads/main@{#1043547}

--- extensions/shell/browser/root_window_controller_unittest.cc
+++ extensions/shell/browser/root_window_controller_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/native_app_window.h"
 #include "extensions/common/extension_builder.h"
-#include "extensions/shell/browser/root_window_controller.h"
 #include "extensions/shell/browser/shell_app_window_client.h"
 #include "extensions/shell/browser/shell_native_app_window_aura.h"
 #include "extensions/shell/test/shell_test_base_aura.h"
