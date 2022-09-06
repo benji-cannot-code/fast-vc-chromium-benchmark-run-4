48afd2088e846371d384ad9c19aa8e0b05e533cf
Luca Accorsi <accorsi@google.com>
2022-09-06 09:40:11 +0000

    Add inode field to FileDaemonInfo
    Bug: 1359240
    Test: tools/autotest.py -C out/Default --run_all ./chrome/browser/ash/policy/dlp/
    Change-Id: I6b2356dd64586df4c16a7fa3bdf808d5ec2c6fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869267
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043360}

--- chrome/browser/ash/policy/dlp/dlp_files_controller.cc
+++ chrome/browser/ash/policy/dlp/dlp_files_controller.cc
@@ -126,9 +126,10 @@ DlpFilesController::DlpFileRestrictionDetails::~DlpFileRestrictionDetails() =
     default;
 
 DlpFilesController::FileDaemonInfo::FileDaemonInfo(
+    ino_t inode,
     const base::FilePath& path,
     const std::string& source_url)
-    : path(path), source_url(source_url) {}
+    : inode(inode), path(path), source_url(source_url) {}
 
 DlpFilesController::DlpFileDestination::DlpFileDestination(
     const std::string& url)
@@ -288,7 +289,7 @@ void DlpFilesController::CheckIfDownloadAllowed(
     return;
   }
 
-  FileDaemonInfo file_info(base::FilePath(""), download_url.spec());
+  FileDaemonInfo file_info({}, base::FilePath(""), download_url.spec());
   IsFilesTransferRestricted(
       {std::move(file_info)}, DlpFileDestination(file_path.value()),
       DlpWarnDialog::FilesAction::kDownload,
