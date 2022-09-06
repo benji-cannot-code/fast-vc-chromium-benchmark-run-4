9318ee89681500f647d6aaacd6e33bb088fb5965
Daoyuan Li <daoyuanli@microsoft.com>
2022-09-06 17:50:38 +0000

    Reland "[MediaFoundation] Request next frame during onPlaying event"
    This is a reland of commit 395b2b1c164f38d84dfd07aef5820bae35cdb61a
    
    Original change's description:
    > [MediaFoundation] Request next frame during onPlaying event
    >
    > OS: Win10, Win11
    >
    > During OnPlaying event, a frame request should be called to get
    > the first frame to output at the earliest possible time. This
    > is the earliest time when a frame is available. Current
    > implementation waits for render to be called, while
    > StartPlayingFrom's call for RequestNextFrameBetweenTimestamps
    > may not output a result if the media engine is not yet ready
    > to output a frame.
    >
    > Bug: 1355520
    > Change-Id: Ice60ac41ca4b8cae9b0687626e93017d0a4406f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852409
    > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > Commit-Queue: Daoyuan Li <daoyuanli@microsoft.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1041097}
    
    Bug: 1355520
    Change-Id: Ib8f579de1cb02debd2e05a2e88fb63ef66f3d5d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869709
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daoyuan Li <daoyuanli@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1043519}

--- media/renderers/win/media_foundation_renderer.cc
+++ media/renderers/win/media_foundation_renderer.cc
@@ -180,6 +180,11 @@ void MediaFoundationRenderer::Initialize(MediaResource* media_resource,
     }
   }
 
+  // debug, force mode to dcomp
+  if (force_dcomp_mode_for_testing_) {
+    rendering_mode_ = MediaFoundationRenderingMode::DirectComposition;
+  }
+
   MEDIA_LOG(INFO, media_log_)
       << "Starting MediaFoundationRenderingMode: " << rendering_mode_;
 
@@ -499,6 +504,9 @@ void MediaFoundationRenderer::SetMediaFoundationRenderingMode(
 
   if (mf_media_engine_->HasVideo()) {
     if (render_mode == MediaFoundationRenderingMode::FrameServer) {
+      // cannot change to frameserver if force_dcomp_mode_for_testing_ is true
+      DCHECK(!force_dcomp_mode_for_testing_);
+
       // Make sure we reinitialize the texture pool
       hr = InitializeTexturePool(native_video_size_);
     } else if (render_mode == MediaFoundationRenderingMode::DirectComposition) {
@@ -834,6 +842,10 @@ void MediaFoundationRenderer::OnPlaying() {
   OnBufferingStateChange(
       BufferingState::BUFFERING_HAVE_ENOUGH,
       BufferingStateChangeReason::BUFFERING_CHANGE_REASON_UNKNOWN);
+
+  // Earliest time to request first frame to screen
+  RequestNextFrame();
+
   // The OnPlaying callback from MediaEngineNotifyImpl lets us know that an
   // MF_MEDIA_ENGINE_EVENT_PLAYING message has been received. At this point we
   // can safely start sending Statistics as any asynchronous Flush action in
@@ -969,9 +981,7 @@ void MediaFoundationRenderer::OnError(PipelineStatus status,
     renderer_client_->OnError(new_status);
 }
 
-void MediaFoundationRenderer::RequestNextFrameBetweenTimestamps(
-    base::TimeTicks deadline_min,
-    base::TimeTicks deadline_max) {
+void MediaFoundationRenderer::RequestNextFrame() {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
   if (rendering_mode_ != MediaFoundationRenderingMode::FrameServer) {
     return;
