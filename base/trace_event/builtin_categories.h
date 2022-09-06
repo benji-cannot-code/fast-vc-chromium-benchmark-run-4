b09d7ab475a759cc9805c216eb19046a1eda8b61
Sean Maher <spvw@chromium.org>
2022-09-06 20:12:45 +0000

    Add trace event category for Catan investigations
    This trace event category will be a catch-all which the catan team can
    use as the default when we add trace events for investigations.
    
    This allows differentiation between our trace events and trace
    events added by the owners of the code, as well as to grep the codebase
    to prune trace events which no longer need to be collected, and finally
    to ensure that these trace events are always included in traces,
    regardless of finch configs stating which events are to be collected
    (the problem we have when using team-specific categories).
    
    Change-Id: I15077048b307d3f257379b0c1ea901b875144899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868503
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043590}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("ime")                                                               \
   X("IndexedDB")                                                         \
   X("input")                                                             \
+  X("catan_investigation")                                               \
   X("io")                                                                \
   X("ipc")                                                               \
   X("Java")                                                              \
