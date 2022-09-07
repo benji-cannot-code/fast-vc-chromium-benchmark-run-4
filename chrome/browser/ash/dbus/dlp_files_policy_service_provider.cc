7c550e0c0514d2d2cb65c0d0b3b27fa0ff0ab4ba
Luca Accorsi <accorsi@google.com>
2022-09-07 07:26:24 +0000

    Change IsDlpPolicyMatched to use FileDaemonInfo
    Bug: 1360009
    Test: tools/autotest.py -C out/Default --run_all ./chrome/browser/ash/policy/dlp/
    Change-Id: I54c726a67ba679faf4762618f57227e299cbd982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874685
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043854}

--- chrome/browser/ash/dbus/dlp_files_policy_service_provider.cc
+++ chrome/browser/ash/dbus/dlp_files_policy_service_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
 #include "chrome/browser/ash/policy/dlp/dlp_files_controller.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
@@ -98,9 +99,13 @@ void DlpFilesPolicyServiceProvider::IsDlpPolicyMatched(
   policy::DlpFilesController* files_controller =
       rules_manager->GetDlpFilesController();
 
+  // TODO(crbug.com/1360005): Add actual file path.
   bool restricted =
       files_controller
-          ? files_controller->IsDlpPolicyMatched(request.source_url())
+          ? files_controller->IsDlpPolicyMatched(
+                policy::DlpFilesController::FileDaemonInfo(
+                    request.file_metadata().inode(), base::FilePath(),
+                    request.file_metadata().source_url()))
           : false;
 
   dlp::IsDlpPolicyMatchedResponse response_proto;
