1c8866d4afd0d95acf38397f76b5ab8a109d0b6f
Eric Willigers <ericwilligers@chromium.org>
2022-09-07 09:33:45 +0000

    LacrosWebAppsControllerBrowserTest: change base class
    We now use WebAppNavigationBrowserTest, which uninstalls web apps
    during tear down.
    
    Bug: 1358200
    Change-Id: Icfabbe812cda28fd25613431006d00df7d39efa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875149
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043886}

--- chrome/browser/web_applications/app_service/lacros_web_apps_controller_lacros_browsertest.cc
+++ chrome/browser/web_applications/app_service/lacros_web_apps_controller_lacros_browsertest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/page_info/chrome_page_info_delegate.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/ui/web_applications/web_app_controller_browsertest.h"
+#include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
 #include "chrome/browser/web_applications/app_service/lacros_web_apps_controller.h"
 #include "chrome/browser/web_applications/test/app_registration_waiter.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
@@ -23,13 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/lacros/lacros_service.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "content/public/test/browser_test.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
 namespace web_app {
 
-class LacrosWebAppsControllerBrowserTest : public WebAppControllerBrowserTest {
+class LacrosWebAppsControllerBrowserTest : public WebAppNavigationBrowserTest {
  public:
   LacrosWebAppsControllerBrowserTest() = default;
   ~LacrosWebAppsControllerBrowserTest() override = default;
@@ -52,14 +51,13 @@ IN_PROC_BROWSER_TEST_F(LacrosWebAppsControllerBrowserTest, DefaultContextMenu) {
   if (!IsServiceAvailable())
     GTEST_SKIP() << "Unsupported ash version.";
 
-  const AppId app_id =
-      InstallPWA(https_server()->GetURL("/web_apps/basic.html"));
-  AppRegistrationWaiter(profile(), app_id).Await();
+  InstallTestWebApp();
+  const AppId app_id = test_web_app_id();
 
   // No item should exist in the shelf before the web app is launched.
   browser_test_util::WaitForShelfItem(app_id, /*exists=*/false);
 
-  LaunchWebAppBrowser(app_id);
+  OpenTestWebApp();
 
   // Wait for item to exist in shelf.
   browser_test_util::WaitForShelfItem(app_id, /*exists=*/true);
@@ -98,12 +96,11 @@ IN_PROC_BROWSER_TEST_F(LacrosWebAppsControllerBrowserTest, AppManagement) {
     GTEST_SKIP() << "Unsupported ash version.";
   }
 
-  const GURL app_url = https_server()->GetURL("/web_apps/basic.html");
-  const AppId app_id = InstallPWA(app_url);
-  AppRegistrationWaiter(profile(), app_id).Await();
+  InstallTestWebApp();
+  const AppId app_id = test_web_app_id();
   AppRegistrationWaiter(profile(), kOsSettingsAppId).Await();
 
-  Browser* browser = LaunchWebAppBrowser(app_id);
+  Browser* browser = OpenTestWebApp();
   content::WebContents* web_contents =
       browser->tab_strip_model()->GetActiveWebContents();
   ChromePageInfoDelegate delegate(web_contents);
@@ -114,7 +111,7 @@ IN_PROC_BROWSER_TEST_F(LacrosWebAppsControllerBrowserTest, AppManagement) {
   // Settings should not yet exist in the shelf.
   browser_test_util::WaitForShelfItem(kOsSettingsAppId, /*exists=*/false);
 
-  delegate.ShowSiteSettings(app_url);
+  delegate.ShowSiteSettings(web_contents->GetVisibleURL());
 
   // Settings should now exist in the shelf.
   browser_test_util::WaitForShelfItem(kOsSettingsAppId, /*exists=*/true);
