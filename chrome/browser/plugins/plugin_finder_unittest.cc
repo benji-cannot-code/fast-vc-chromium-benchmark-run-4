fc7c4a52f26a28ebd1719b99a0333f651418b69f
K. Moon <kmoon@chromium.org>
2022-09-07 22:56:35 +0000

    Remove unused PluginMetadata fields
    Removes unused PluginMetadata fields, and simplifies the handling of the
    security status field.
    
    Note that the only security statuses still in use are "fully_trusted"
    (used by the PDF plugins) and "requires_authorization" (the default for
    plugins not described in plugin_metadata.json).
    
    Also removes PluginMetadataTest, as the PluginMetadata class has lost
    all non-trivial functionality, and eventually will be deleted anyway.
    
    Bug: 1064647
    Change-Id: I38e6bbabebbe74331352e5e3b2cc0f5d5c309944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880166
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044234}

--- chrome/browser/plugins/plugin_finder_unittest.cc
+++ chrome/browser/plugins/plugin_finder_unittest.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/files/file_path.h"
 #include "chrome/browser/plugins/plugin_metadata.h"
-#include "content/public/common/webplugininfo.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(PluginFinderTest, FindPluginWithIdentifier) {
@@ -19,8 +17,6 @@ TEST(PluginFinderTest, FindPluginWithIdentifier) {
       "google-chrome-pdf", nullptr, &pdf));
   ASSERT_TRUE(pdf);
 
-  content::WebPluginInfo fake_pdf(u"Chrome PDF Viewer", base::FilePath(), u"0",
-                                  std::u16string());
   EXPECT_EQ(PluginMetadata::SECURITY_STATUS_FULLY_TRUSTED,
-            pdf->GetSecurityStatus(fake_pdf));
+            pdf->security_status());
 }
