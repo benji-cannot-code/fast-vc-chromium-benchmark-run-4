3afb2826b551b48bb95bf0373d61917432e09a05
Vinny Persky <vinnypersky@google.com>
2022-09-07 23:03:56 +0000

    Merchant opt out error dialog parsing in unmask card response
    This CL adds the logic to parse the merchant opt out error dialog fields in an unmask response, and unit tests to ensure this parsing works
    correctly.
    
    Bug: 1355044
    Change-Id: I2d2f7f7d36f95105f4e13c6fde1dcee450567d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843276
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044240}

---
+++ components/autofill/core/browser/payments/autofill_error_dialog_context.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/payments/autofill_error_dialog_context.h"
+
+namespace autofill {
+
+AutofillErrorDialogContext::AutofillErrorDialogContext() = default;
+
+AutofillErrorDialogContext::AutofillErrorDialogContext(
+    const AutofillErrorDialogContext& other) = default;
+
+AutofillErrorDialogContext& AutofillErrorDialogContext::operator=(
+    const AutofillErrorDialogContext&) = default;
+
+AutofillErrorDialogContext::~AutofillErrorDialogContext() = default;
+
+}  // namespace autofill
