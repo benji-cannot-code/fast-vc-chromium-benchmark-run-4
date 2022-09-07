8a46dedac1db004ec3fc7b9904b1157219328528
Jack Shira <jackshira@google.com>
2022-09-07 17:46:17 +0000

    [CrOS HaTS] Fix UAF issue with HatsDialog::Show
    The following scenario produces a UAF:
    - The user logs in and is selected for a survey
    - The user locks their screen
    - The user unlocks their screen and selects the survey notification
    - The user kills Chrome while the survey dialog is being displayed
    
    The underlying cause of this is because the HatsDialog instance is
    deleted before the ash::Shell delete call, which tries to close the
    dialog that is now invalidated.
    
    The solution resolves this issue by allowing the HatsDialog class to
    manage its own lifecycle. This is taken from [the documentation for WebUI](https://source.chromium.org/chromium/chromium/src/+/main:docs/webui_in_chrome.md).
    
    TEST=manually verified that the the workflow above no longer produces UAF
    
    Change-Id: I26e626bd679841968d0a401aad60a8a2a82aed2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878226
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044060}

--- chrome/browser/ash/hats/hats_dialog.h
+++ chrome/browser/ash/hats/hats_dialog.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 
-class Profile;
-
 namespace ash {
 
 // Happiness tracking survey dialog. Sometimes appears after login to ask the
@@ -21,20 +19,23 @@ namespace ash {
 // This class lives on the UI thread.
 class HatsDialog : public ui::WebDialogDelegate {
  public:
-  HatsDialog(const std::string& trigger_id,
-             Profile* user_profile,
-             const std::string& histogram_name);
-
   HatsDialog(const HatsDialog&) = delete;
   HatsDialog& operator=(const HatsDialog&) = delete;
 
-  ~HatsDialog() override;
-
-  void Show(const std::string& site_context);
+  static void Show(const std::string& trigger_id,
+                   const std::string& histogram_name,
+                   const std::string& site_context);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(HatsDialogTest, GetFormattedSiteContext);
   FRIEND_TEST_ALL_PREFIXES(HatsDialogTest, HandleClientTriggeredAction);
+
+  // This class must be allocated on the heap, and general care should be taken
+  // regarding its lifetime, due to its self-managing use of delete in the
+  // `OnDialogClosed` method.
+  HatsDialog(const std::string& trigger_id,
+             const std::string& histogram_name,
+             const std::string& site_context);
 
   // Based on the supplied |action|, returns true if the client should be
   // closed. Handling the action could imply logging or incrementing a survey
@@ -62,7 +63,6 @@ class HatsDialog : public ui::WebDialogDelegate {
 
   const std::string trigger_id_;
   std::string url_;
-  Profile* user_profile_;
   const std::string histogram_name_;
 };
 
