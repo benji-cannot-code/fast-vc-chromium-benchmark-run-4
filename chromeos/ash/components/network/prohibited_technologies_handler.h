256086d812678bc395f31f5736108d5ca8bf968a
Chad Duffin <chadduffin@chromium.org>
2022-09-07 16:07:15 +0000

    [CrOS Network] Handle no prohibited technology case.
    This change updates the handling of device policy to properly set the
    prohibited technologies if there are no prohibited technologies. This
    fixes the case where a technology is prohibited and remains prohibited
    even after the administrator allows all technologies.
    
    Video: https://drive.google.com/file/d/1-xKf0gstPEjbzhLa2mmqQqPKRV3sVhau/view?usp=sharing
    
    Bug: b/242897132
    Test: Added a unit test and verified on a device.
    Change-Id: I4cdabfff77ad31783fddf10c9bf3e6fd2f895604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830315
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043995}

--- chromeos/ash/components/network/prohibited_technologies_handler.h
+++ chromeos/ash/components/network/prohibited_technologies_handler.h
@@ -45,6 +45,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) ProhibitedTechnologiesHandler
   std::vector<std::string> GetCurrentlyProhibitedTechnologies();
 
  private:
+  friend class ManagedNetworkConfigurationHandlerTest;
   friend class NetworkHandler;
   friend class ProhibitedTechnologiesHandlerTest;
 
