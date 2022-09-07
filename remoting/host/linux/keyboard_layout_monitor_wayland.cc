21e431141a9de1045419b295049597d51ac36847
Salman Malik <salmanmalik@chromium.org>
2022-09-07 21:21:15 +0000

    remoting: Stub for keyboard layout monitor for Wayland
    Current Wayland stub causes the binary crash. This CL
    properly stubs out the module for Wayland while we figure
    out how to get the keyboard modifier state from the protocol
    (which may be required to populate correct actions for each
    key).
    
    Bug: chromium:1291247
    Change-Id: Icc82c2ab18eb8fb862f76df8f52dc8e92a134111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872002
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Salman Malik <salmanmalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044185}

--- remoting/host/linux/keyboard_layout_monitor_wayland.cc
+++ remoting/host/linux/keyboard_layout_monitor_wayland.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/keyboard_layout_monitor.h"
 
+#include <memory>
+
 #include "base/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
@@ -13,11 +15,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
+namespace {
+
+class KeyboardLayoutMonitorWayland : public KeyboardLayoutMonitor {
+ public:
+  explicit KeyboardLayoutMonitorWayland(
+      base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback);
+
+  ~KeyboardLayoutMonitorWayland() override;
+
+  void Start() override;
+
+ private:
+  base::RepeatingCallback<void(const protocol::KeyboardLayout&)>
+      layout_changed_callback_;
+};
+
+KeyboardLayoutMonitorWayland::KeyboardLayoutMonitorWayland(
+    base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback)
+    : layout_changed_callback_(std::move(callback)) {}
+
+KeyboardLayoutMonitorWayland::~KeyboardLayoutMonitorWayland() = default;
+
+void KeyboardLayoutMonitorWayland::Start() {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace
+
 std::unique_ptr<KeyboardLayoutMonitor> KeyboardLayoutMonitor::Create(
     base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback,
     scoped_refptr<base::SingleThreadTaskRunner> input_task_runner) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return std::make_unique<KeyboardLayoutMonitorWayland>(std::move(callback));
 }
 
 }  // namespace remoting
