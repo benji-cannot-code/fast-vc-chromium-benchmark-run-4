ec293431c3fabb011b1401835971dd1a9b68e039
Risan <risan@google.com>
2022-09-07 06:16:15 +0000

    Allow GuestOS Play files to share with Linux
    This CL allows share with Linux feature on Play files when virtio-blk is
    active.
    
    Bug: 215255080
    Test: Manual.
    Change-Id: Icfe5d6065d5d160ae7aa2b73f51aaa1c0565c8bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3873134
    Commit-Queue: Risan <risan@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043840}

--- chrome/browser/ash/guest_os/guest_os_share_path.cc
+++ chrome/browser/ash/guest_os/guest_os_share_path.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/guest_os/guest_os_share_path.h"
 
+#include "ash/components/arc/arc_features.h"
 #include "ash/components/arc/arc_util.h"
 #include "base/atomic_ref_count.h"
 #include "base/bind.h"
@@ -249,7 +250,7 @@ void GuestOsSharePath::CallSeneschalSharePath(const std::string& vm_name,
   bool allowed_path = false;
   base::FilePath my_files =
       file_manager::util::GetMyFilesFolderForProfile(profile_);
-  base::FilePath android_files(file_manager::util::kAndroidFilesPath);
+  base::FilePath android_files(file_manager::util::GetAndroidFilesPath());
   base::FilePath removable_media(file_manager::util::kRemovableMediaPath);
   base::FilePath linux_files =
       file_manager::util::GetCrostiniMountDirectory(profile_);
@@ -319,7 +320,9 @@ void GuestOsSharePath::CallSeneschalSharePath(const std::string& vm_name,
     // Allow Android files and subdirs.
     allowed_path = true;
     request.set_storage_location(
-        vm_tools::seneschal::SharePathRequest::PLAY_FILES);
+        base::FeatureList::IsEnabled(arc::kEnableVirtioBlkForData)
+            ? vm_tools::seneschal::SharePathRequest::PLAY_FILES_GUEST_OS
+            : vm_tools::seneschal::SharePathRequest::PLAY_FILES);
   } else if (removable_media.AppendRelativePath(path, &relative_path)) {
     // Allow subdirs of /media/removable.
     allowed_path = true;
