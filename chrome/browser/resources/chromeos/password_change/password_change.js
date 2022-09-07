bccd1af730f8419c1dc6defb00db4cef5b409f81
dpapad <dpapad@chromium.org>
2022-09-07 00:15:04 +0000

    CrOS password-change: Import strings.m.js instead of strings.js.
    Since this code is using JS module (see [1]), it should be using the
    module version of strings.js instead. This also allows formatting
    imports by clang-format without triggering test errors (unlike when
    using the non module version).
    
    [1] crrev.com/c/2742719
    
    Bug: None
    Change-Id: I351b095960e2e8dcd6b27b2fe8d9e24a3c9b5d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877567
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043730}

--- chrome/browser/resources/chromeos/password_change/password_change.js
+++ chrome/browser/resources/chromeos/password_change/password_change.js
@@ -8,11 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * with cryptohome password.
  */
 
-import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
-import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import 'chrome://password-change/strings.js';
+import 'chrome://password-change/strings.m.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/icons.m.js';
+
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 Polymer({
   is: 'password-change',
