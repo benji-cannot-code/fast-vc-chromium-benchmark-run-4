57ec23157979aa271aadd8b470061ad09f7c96c7
Ahmed Nasr <anasr@google.com>
2022-09-07 23:05:33 +0000

    Use EventDrivenTelemetryPool in PeriodicEventCollector.
    Instead of a fixed set of samplers, use EventDrivenTelemetrySamplerPool
    so the samplers can be controlled on each event separately based on
    policy, session state (login) etc..
    
    Bug: b:237802510
    Change-Id: Ie568fcd7f022ac9b17dec424b7533a6b44894091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879764
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044241}

--- components/reporting/metrics/metric_data_collector.h
+++ components/reporting/metrics/metric_data_collector.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "components/reporting/client/report_queue.h"
+#include "components/reporting/metrics/event_driven_telemetry_sampler_pool.h"
 #include "components/reporting/metrics/sampler.h"
 #include "components/reporting/proto/synced/metric_data.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -130,41 +131,13 @@ class EventDetector {
       const MetricData& current_metric_data) = 0;
 };
 
-class AdditionalSamplersCollector {
- public:
-  explicit AdditionalSamplersCollector(std::vector<Sampler*> samplers);
-
-  AdditionalSamplersCollector(const AdditionalSamplersCollector& other) =
-      delete;
-  AdditionalSamplersCollector& operator=(
-      const AdditionalSamplersCollector& other) = delete;
-
-  ~AdditionalSamplersCollector();
-
-  void CollectAll(OptionalMetricCallback on_all_collected_cb,
-                  MetricData metric_data) const;
-
- private:
-  void CollectAdditionalMetricData(
-      uint64_t sampler_index,
-      OptionalMetricCallback on_all_collected_cb,
-      MetricData metric_data,
-      absl::optional<MetricData> new_metric_data) const;
-
-  const std::vector<Sampler*> samplers_;
-
-  SEQUENCE_CHECKER(sequence_checker_);
-
-  base::WeakPtrFactory<AdditionalSamplersCollector> weak_ptr_factory_{this};
-};
-
 // Class to collect metric data periodically, check the collected data for
 // events, and report metric and event data if an event is detecetd.
 class PeriodicEventCollector : public PeriodicCollector {
  public:
   PeriodicEventCollector(Sampler* sampler,
                          std::unique_ptr<EventDetector> event_detector,
-                         std::vector<Sampler*> additional_samplers,
+                         EventDrivenTelemetrySamplerPool* sampler_pool,
                          MetricReportQueue* metric_report_queue,
                          ReportingSettings* reporting_settings,
                          const std::string& enable_setting_path,
@@ -183,14 +156,16 @@ class PeriodicEventCollector : public PeriodicCollector {
   void OnMetricDataCollected(absl::optional<MetricData> metric_data) override;
 
  private:
-  void OnAdditionalMetricDataCollected(absl::optional<MetricData> metric_data);
+  void MergeAndReport(MetricData event_metric_data,
+                      absl::optional<MetricData> telemetry_metric_data);
 
   const std::unique_ptr<EventDetector> event_detector_;
 
-  const std::unique_ptr<AdditionalSamplersCollector>
-      additional_samplers_collector_;
+  raw_ptr<EventDrivenTelemetrySamplerPool> sampler_pool_;
 
   MetricData last_collected_data_;
+
+  base::WeakPtrFactory<PeriodicEventCollector> event_weak_ptr_factory_{this};
 };
 }  // namespace reporting
 
