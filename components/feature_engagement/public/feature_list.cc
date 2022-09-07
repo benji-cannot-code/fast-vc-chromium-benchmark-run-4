592710ec6a4a7148a9692534c53eaf6c4e01d2bd
Thomas Lukaszewicz <tluk@chromium.org>
2022-09-07 04:52:15 +0000

    [side search] Update the icon label to leverage feature engagement
    This CL updates the SideSearchIconView's label animation trigger to
    leverage the feature engagement framework.
    
    Previously the label animation was configured to appear at least
    once for each new browser session. The frequency and type (tab,
    window, profile) of the trigger were all configurable.
    
    This new CL gates the animation behind the newly added IPH flag.
    This is disabled by default and configurable in the same way as
    any other Chrome IPH (see doc below for more details)
    https://source.chromium.org/chromium/chromium/src/+/main:components/feature_engagement/README.md
    
    To test enable IPH_SideSearchPageActionLabel as the
    in-product-help-demo-mode-choice in chrome://flags and use the
    following on the command line:
    
    --enable-features=SideSearch,SideSearchDSESupport,UnifiedSidePanel
    
    LOW_COVERAGE_REASON=Covered by browser tests
    
    Bug: 1358713
    Change-Id: Ia1c95609e5a6fb66c1484f1809e6b5729ac1a837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868181
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043818}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -132,6 +132,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHReopenTabFeature,
     &kIPHSideSearchAutoTriggeringFeature,
     &kIPHSideSearchFeature,
+    &kIPHSideSearchPageActionLabelFeature,
     &kIPHTabSearchFeature,
     &kIPHWebUITabStripFeature,
     &kIPHDesktopPwaInstallFeature,
