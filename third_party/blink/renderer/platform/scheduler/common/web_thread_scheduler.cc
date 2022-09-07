0ffdb1bfd381911a5cbf6ff9dba1cb97dcf870e4
Ivan Murashov <ivan.murashov@lge.com>
2022-09-07 13:38:20 +0000

    libstdc++: Fix incomplete type of blink::MainThread
    Destructor of std::unique_ptr in libstdc++ uses sizeof() which
    requires full definition of blink::MainThread for return type of
    WebThreadScheduler::CreateMainThread().
    Added required include to fix the issue.
    
    Bug: 957519
    Change-Id: I7ae59d082e2a83661c64b9fd166f9502d97f6770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878344
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043930}

--- third_party/blink/renderer/platform/scheduler/common/web_thread_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/common/web_thread_scheduler.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/common/features.h"
 #include "third_party/blink/renderer/platform/scheduler/common/tracing_helper.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h"
+#include "third_party/blink/renderer/platform/scheduler/public/main_thread.h"
 
 namespace blink {
 namespace scheduler {
