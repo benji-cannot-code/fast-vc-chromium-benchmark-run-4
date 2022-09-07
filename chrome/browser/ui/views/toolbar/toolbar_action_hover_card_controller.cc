fe7bde62048f244be902306c579f6176386ad01c
Emilia Paz <emiliapaz@chromium.org>
2022-09-07 22:55:01 +0000

    [Extensions c2s] Toolbar action hover card doesn't highlight anchor view
    The same toolbar action hover card can be displayed for multiple
    toolbar action views. Thus, the hover card dialog should not highlight
    the dialog when shown and leave the highlight functionality to the
    toolbar action view itself.
    
    Screencast: https://drive.google.com/file/d/1r6rj9IHFjo_Do9rX945T0m_uTtdQqFpw/view?usp=sharing
    
    Bug: 1351778
    Change-Id: I9b4bc420173edbe603829fee011b4c4d1f3f5f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876945
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044233}

--- chrome/browser/ui/views/toolbar/toolbar_action_hover_card_controller.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_hover_card_controller.cc
@@ -145,7 +145,6 @@ void ToolbarActionHoverCardController::UpdateHoverCard(
     HideHoverCard();
 }
 
-// TODO(crbug.com/1351778): Fix inkdrop when hovering over `action_view`.
 void ToolbarActionHoverCardController::UpdateOrShowHoverCard(
     ToolbarActionView* action_view,
     ToolbarActionHoverCardUpdateType update_type) {
