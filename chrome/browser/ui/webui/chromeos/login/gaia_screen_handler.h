dbba205c309c7d9fe40baedb3f0575bbe01cb0da
Steven Bennetts <stevenjb@chromium.org>
2022-09-07 18:11:53 +0000

    login: Replace NetworkPortalDetector::Observer with NSHO
    NetworkPortalDetector::Observer only notifies when Chrome portal
    detection detects a change. Generally we want to know about any
    change in portal state, including Shill detection. This will be
    important once Shill detection becomes primary and Chrome detection
    is only used when a proxy is configured.
    
    This migrates the following to NetworkStateHandlerObserver:
        AutoEnrollmentCheckScreen
        GaiaScreenHandler
    
    BUG=b:243405151,b:1133203
    TEST=unit_tests, browser_tests
    
    Change-Id: Ic4e055708fd1297fcffc79f1e8f2fedc5e26cf46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859251
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044083}

--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
@@ -72,8 +72,6 @@ class GaiaView : public base::SupportsWeakPtr<GaiaView> {
 
   virtual ~GaiaView() = default;
 
-  virtual void DisableRestrictiveProxyCheckForTest() = 0;
-
   // Loads Gaia into the webview. Depending on internal state, the Gaia will
   // either be loaded immediately or after an asynchronous clean-up process that
   // cleans DNS cache and cookies. If available, `account_id` is used for
@@ -103,7 +101,6 @@ class GaiaView : public base::SupportsWeakPtr<GaiaView> {
 // A class that handles WebUI hooks in Gaia screen.
 class GaiaScreenHandler : public BaseScreenHandler,
                           public GaiaView,
-                          public NetworkPortalDetector::Observer,
                           public SecurityTokenPinDialogHost {
  public:
   using TView = GaiaView;
@@ -133,7 +130,6 @@ class GaiaScreenHandler : public BaseScreenHandler,
   ~GaiaScreenHandler() override;
 
   // GaiaView:
-  void DisableRestrictiveProxyCheckForTest() override;
   void LoadGaiaAsync(const AccountId& account_id) override;
   void Show() override;
   void Hide() override;
@@ -198,11 +194,6 @@ class GaiaScreenHandler : public BaseScreenHandler,
 
   // WebUIMessageHandler implementation:
   void RegisterMessages() override;
-
-  // NetworkPortalDetector::Observer implementation.
-  void OnPortalDetectionCompleted(
-      const NetworkState* network,
-      const NetworkPortalDetector::CaptivePortalStatus status) override;
 
   // WebUI message handlers.
   void HandleWebviewLoadAborted(int error_code);
@@ -295,9 +286,6 @@ class GaiaScreenHandler : public BaseScreenHandler,
     signin_screen_handler_ = handler;
   }
 
-  // Are we on a restrictive proxy?
-  bool IsRestrictiveProxy() const;
-
   // Returns temporary unused device Id.
   std::string GetTemporaryDeviceId();
 
@@ -368,12 +356,6 @@ class GaiaScreenHandler : public BaseScreenHandler,
   // Test result of userInfo.
   std::string test_services_;
   bool test_expects_complete_login_ = false;
-
-  // True if proxy doesn't allow access to google.com/generate_204.
-  NetworkPortalDetector::CaptivePortalStatus captive_portal_status_ =
-      NetworkPortalDetector::CAPTIVE_PORTAL_STATUS_ONLINE;
-
-  bool disable_restrictive_proxy_check_for_test_ = false;
 
   // Non-owning ptr to SigninScreenHandler instance. Should not be used
   // in dtor.
