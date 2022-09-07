0ba51a54ce734fcbd04e53a4031428170f2880f1
Khushal Sagar <khushalsagar@chromium.org>
2022-09-07 20:01:22 +0000

    blink: Switch default value for CSSOverflowForReplacedElements
    While this feature flag is only applied if there is a finch config
    override, the overrides can use the flag value in client code instead of
    an explicit value provided by the server.
    
    Conservatively use a value which disables the feature.
    
    R=pdr@chromium.org
    
    Bug: 1360573
    Change-Id: Ia0344d6f4aa64c8061cf13e2a40f17772b109b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876632
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044140}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1633,7 +1633,7 @@ const base::Feature kTimedHTMLParserBudget{"TimedHTMLParserBudget",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCSSOverflowForReplacedElements{
-    "CSSOverflowForReplacedElements", base::FEATURE_ENABLED_BY_DEFAULT};
+    "CSSOverflowForReplacedElements", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kScrollUpdateOptimizations{
     "ScrollUpdateOptimizations", base::FEATURE_ENABLED_BY_DEFAULT};
