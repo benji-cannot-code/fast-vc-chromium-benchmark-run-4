50058ce59dc711b13dc1d0549b946da86376b759
Brandon Jones <bajones@chromium.org>
2022-09-07 23:16:31 +0000

    Add the WebGPU "internal" error type
    Allows pushErrorScope to accept "internal" as the filter and
    both popErrorScope and uncaptured errors to return GPUInternalErrors.
    
    Bug: dawn:1531
    Change-Id: Ie3316839c722109e4582ddf4626cfbccb68d0290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878288
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044244}

--- third_party/blink/renderer/modules/webgpu/gpu_device.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/gpu_compute_pipeline.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_external_texture.h"
+#include "third_party/blink/renderer/modules/webgpu/gpu_internal_error.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_out_of_memory_error.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_pipeline_layout.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_query_set.h"
@@ -260,6 +261,9 @@ void GPUDevice::OnUncapturedError(WGPUErrorType errorType,
         StringFromASCIIAndUTF8(message)));
   } else if (errorType == WGPUErrorType_OutOfMemory) {
     init->setError(MakeGarbageCollected<GPUOutOfMemoryError>(
+        StringFromASCIIAndUTF8(message)));
+  } else if (errorType == WGPUErrorType_Internal) {
+    init->setError(MakeGarbageCollected<GPUInternalError>(
         StringFromASCIIAndUTF8(message)));
   } else {
     return;
@@ -574,6 +578,10 @@ void GPUDevice::OnPopErrorScopeCallback(ScriptPromiseResolver* resolver,
       break;
     case WGPUErrorType_Validation:
       resolver->Resolve(MakeGarbageCollected<GPUValidationError>(
+          StringFromASCIIAndUTF8(message)));
+      break;
+    case WGPUErrorType_Internal:
+      resolver->Resolve(MakeGarbageCollected<GPUInternalError>(
           StringFromASCIIAndUTF8(message)));
       break;
     case WGPUErrorType_Unknown:
