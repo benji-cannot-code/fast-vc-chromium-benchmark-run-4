e9175a7cc6b9eec8c19215d4b3ef9942717c6829
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-09-07 04:06:01 +0000

    Fix clang-tidy error about `operator bool` being implicit.
    'operator bool' must be marked explicit to avoid unintentional
    implicit conversions (google-explicit-constructor)
    
    This was hiding a scary bug, where C++ was implicitly casting
    `Iterator` objects to `bool` before comparing them since no
    `operator==` was defined. For instance, when doing `iter1 == iter2`,
    the code really was doing:
    `static_cast<bool>(iter1) == static_cast<bool>(iter2)`
    
    See here for an example of this in action:
    https://godbolt.org/z/vsMWo9a3v
    
    Change-Id: Ifa3bb33834335664b80cb5c4d14b44ac343ed7a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869623
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043812}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -1262,11 +1262,12 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       return Iterator(buffer_, buffer_->data_.get() + buffer_->used_,
                       buffer_->used_);
     }
-    bool operator!=(const Iterator& other) {
+    bool operator==(const Iterator& other) const {
       // Not valid to compare iterators on different buffers.
       DCHECK_EQ(other.buffer_, buffer_);
-      return other.op_offset_ != op_offset_;
+      return other.op_offset_ == op_offset_;
     }
+    bool operator!=(const Iterator& other) const { return !(*this == other); }
     Iterator& operator++() {
       DCHECK(*this);
       const PaintOp& op = **this;
@@ -1276,7 +1277,7 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       CHECK_LE(op_offset_, buffer_->used_);
       return *this;
     }
-    operator bool() const { return op_offset_ < buffer_->used_; }
+    explicit operator bool() const { return op_offset_ < buffer_->used_; }
 
    private:
     Iterator(const PaintOpBuffer* buffer, char* ptr, size_t op_offset)
@@ -1316,10 +1317,13 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       return OffsetIterator(buffer_, buffer_->data_.get() + buffer_->used_,
                             buffer_->used_, offsets_);
     }
-    bool operator!=(const OffsetIterator& other) {
+    bool operator==(const OffsetIterator& other) const {
       // Not valid to compare iterators on different buffers.
       DCHECK_EQ(other.buffer_, buffer_);
-      return other.op_offset_ != op_offset_;
+      return other.op_offset_ == op_offset_;
+    }
+    bool operator!=(const OffsetIterator& other) const {
+      return !(*this == other);
     }
     OffsetIterator& operator++() {
       if (++offsets_index_ >= offsets_->size()) {
@@ -1343,7 +1347,7 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       return *this;
     }
 
-    operator bool() const { return op_offset_ < buffer_->used_; }
+    explicit operator bool() const { return op_offset_ < buffer_->used_; }
 
    private:
     OffsetIterator(const PaintOpBuffer* buffer,
@@ -1387,13 +1391,13 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       return using_offsets_ ? CompositeIterator(offset_iter_->end())
                             : CompositeIterator(iter_->end());
     }
-    bool operator==(const CompositeIterator& other) {
+    bool operator==(const CompositeIterator& other) const {
       if (using_offsets_ != other.using_offsets_)
         return false;
       return using_offsets_ ? (*offset_iter_ == *other.offset_iter_)
                             : (*iter_ == *other.iter_);
     }
-    bool operator!=(const CompositeIterator& other) {
+    bool operator!=(const CompositeIterator& other) const {
       return !(*this == other);
     }
     CompositeIterator& operator++() {
@@ -1403,7 +1407,7 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
         ++*iter_;
       return *this;
     }
-    operator bool() const {
+    explicit operator bool() const {
       return using_offsets_ ? !!*offset_iter_ : !!*iter_;
     }
 
@@ -1432,7 +1436,7 @@ class CC_PAINT_EXPORT PaintOpBuffer : public SkRefCnt {
       return *this;
     }
 
-    operator bool() const { return !!current_op_; }
+    explicit operator bool() const { return !!current_op_; }
 
     // Guaranteed to be 255 for all ops without flags.
     uint8_t alpha() const {
