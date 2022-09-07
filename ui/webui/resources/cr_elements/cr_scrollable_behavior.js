304af719ee1148dac23b69d29c8836347436e1ad
dpapad <dpapad@chromium.org>
2022-09-07 07:36:36 +0000

    WebUI: Add CrScrollableMixin to replace CrScrollableBehavior.
     - Include cr_scrollable_behavior.js on CrOS-only.
     - Make ui/webui/resources/cr_elements:closure_compile CrOS-only.
     - Add equivalent CrScrollableMixin and tests.
     - Deploy CrScrollableMixin in all places that are using TypeScript.
    
    Bug: 1316438
    Change-Id: Idf3a9c73a9ac38dc5d32bf17222ce86e45c11a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868087
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043858}

--- ui/webui/resources/cr_elements/cr_scrollable_behavior.js
+++ ui/webui/resources/cr_elements/cr_scrollable_behavior.js
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * NOTE: If 'container' is not fixed size, it is important to call
  * updateScrollableContents() when [[items]] changes, otherwise the container
  * will not be sized correctly.
+ *
+ * NOTE: This file is deprecated in favor of cr_scrollable_mixin.ts. Don't use
+ * it in new code.
  */
 
 // clang-format off
