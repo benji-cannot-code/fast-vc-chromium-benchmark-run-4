8dcee8fb4e55e437f432cdc46c57110460c55dc8
Mahmoud Rashad <mmrashad@google.com>
2022-09-07 12:53:41 +0000

    [Sync] Implement `OnDegradedRecoverabilityChanged` and calling it
    This CL implements `OnDegradedRecoverabilityChanged` and calling it when
    the degraded recoverability state changes.
    
    Bug: 1247990
    Change-Id: I7607c31cb44df582a93c0a00c38816976988dcef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871107
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1043923}

--- components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler.cc
+++ components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler.cc
@@ -109,6 +109,7 @@ void TrustedVaultDegradedRecoverabilityHandler::Refresh() {
 void TrustedVaultDegradedRecoverabilityHandler::
     OnRecoverabilityIsDegradedDownloaded(
         TrustedVaultRecoverabilityStatus status) {
+  bool old_is_recoverability_degraded = is_recoverability_degraded_;
   switch (status) {
     case TrustedVaultRecoverabilityStatus::kDegraded:
       is_recoverability_degraded_ = true;
@@ -119,6 +120,9 @@ void TrustedVaultDegradedRecoverabilityHandler::
     case TrustedVaultRecoverabilityStatus::kError:
       // TODO(crbug.com/1247990): To be handled.
       break;
+  }
+  if (is_recoverability_degraded_ != old_is_recoverability_degraded) {
+    delegate_->OnDegradedRecoverabilityChanged();
   }
   last_refresh_time_ = base::TimeTicks::Now();
   delegate_->WriteDegradedRecoverabilityState(MakeDegradedRecoverabilityState(
--- components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler_unittest.cc
+++ components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler_unittest.cc
@@ -85,7 +85,7 @@ class MockDelegate
               WriteDegradedRecoverabilityState,
               (const sync_pb::LocalTrustedVaultDegradedRecoverabilityState&),
               (override));
-  MOCK_METHOD(void, OnDegradedRecoverabilityChanged, (bool), (override));
+  MOCK_METHOD(void, OnDegradedRecoverabilityChanged, (), (override));
 };
 
 class TrustedVaultDegradedRecoverabilityHandlerTest : public ::testing::Test {
@@ -208,6 +208,7 @@ TEST_F(TrustedVaultDegradedRecoverabilityHandlerTest,
         std::move(callback).Run(TrustedVaultRecoverabilityStatus::kDegraded);
         return std::make_unique<TrustedVaultConnection::Request>();
       });
+  EXPECT_CALL(delegate_, OnDegradedRecoverabilityChanged).Times(0);
   EXPECT_CALL(delegate_,
               WriteDegradedRecoverabilityState(DegradedRecoverabilityStateEq(
                   degraded_recoverability_state)));
@@ -232,6 +233,7 @@ TEST_F(
         std::move(callback).Run(TrustedVaultRecoverabilityStatus::kNotDegraded);
         return std::make_unique<TrustedVaultConnection::Request>();
       });
+  EXPECT_CALL(delegate_, OnDegradedRecoverabilityChanged);
   EXPECT_CALL(delegate_,
               WriteDegradedRecoverabilityState(DegradedRecoverabilityStateEq(
                   degraded_recoverability_state)));
