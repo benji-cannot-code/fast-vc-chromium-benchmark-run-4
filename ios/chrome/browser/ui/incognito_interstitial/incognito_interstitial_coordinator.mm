511b76fb9140b779de4074c442cdc3158d45d58a
David Jean <djean@chromium.org>
2022-09-07 10:41:25 +0000

    [ios] Present incognito interstitial as sheet
    Mainly solves showing the interstitial fullscreen on iPads.
    Bug is in buganizer: b/244705368
    
    Change-Id: I90330bd59cf220c8e3f7be2ce854914f9dcf461e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871031
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043900}

--- ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_coordinator.mm
+++ ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_coordinator.mm
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.incognitoInterstitialViewController.URLLoaderDelegate = self;
   self.incognitoInterstitialViewController.URLText =
       base::SysUTF8ToNSString(self.urlLoadParams.web_params.url.spec());
+  self.incognitoInterstitialViewController.modalPresentationStyle =
+      UIModalPresentationFormSheet;
 
   [self.baseViewController
       presentViewController:self.incognitoInterstitialViewController
