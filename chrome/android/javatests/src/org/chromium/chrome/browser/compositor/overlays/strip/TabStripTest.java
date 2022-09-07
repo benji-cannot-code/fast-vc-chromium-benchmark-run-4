1097264d99360d4dad11d6add9f4a132cd378400
Alex Ilin <alexilin@chromium.org>
2022-09-07 13:47:39 +0000

    [sheriff] Disable failing TabStripTests
    This CL disables the following tests that are failing on
    Marshmallow Tablet Tester:
    - TabStripTest#testSwitchStripStackersWithFirstTabSelected
    - TabStripTest#testSwitchStripStackersWithIncognito
    
    Bug: 1360472
    Change-Id: I321c6dd1f5426e9350ee0ac5e29a4044121cd961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876371
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043934}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
@@ -539,6 +539,7 @@ public class TabStripTest {
     @LargeTest
     @Restriction(UiRestriction.RESTRICTION_TYPE_TABLET)
     @Feature({"TabStrip"})
+    @DisabledTest(message = "https://crbug.com/1360472")
     public void testSwitchStripStackersWithIncognito() throws Exception {
         // Open an incognito tab to switch to the incognito model.
         sActivityTestRule.newIncognitoTabFromMenu();
@@ -614,6 +615,7 @@ public class TabStripTest {
     @LargeTest
     @Restriction(UiRestriction.RESTRICTION_TYPE_TABLET)
     @Feature({"TabStrip"})
+    @DisabledTest(message = "https://crbug.com/1360472")
     public void testSwitchStripStackersWithFirstTabSelected() throws Exception {
         // Open enough regular tabs to cause the tabs to cascade or the strip to scroll depending
         // on which stacker is being used.
