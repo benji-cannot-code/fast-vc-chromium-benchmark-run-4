9ffdc0101835dfcb6699e0b3a50d09ebf7c580d1
Ryan Sultanem <rsult@google.com>
2022-09-07 15:54:46 +0000

    Extension factories to be part of kSystemProfileSelectionDefaultNone
    Since KeyedServiceFactory under extensions/ cannot inherit from
    `ProfileKeyedServiceFactory` to benefit from the default values for
    System and Guest profiles, this will be done through the
    `ExtensionsBrowserClient` by adding the equivalent of ProfileSelections
    experimental builders methods which will provide a check on the
    experiment (follows the same logic as ProfileKeyedServiceFactory, using
    ProfileSelections experimental builder).
    Also these methods will take a parameter to allow to force the creation
    even within the experiment, in case certain Services still need to be
    created for the System Profile.
    
    Adapted the main extensions/ base factory to use these new methods
    `BrowserContextKeyedAPI`, other independent factories can still use the
    new methods from the ExtensionsBrowserClient.
    
    Bug: 1284664
    Change-Id: I287253ed5176ffd17904ab37b6ec6fb38be11e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854601
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043990}

--- chrome/browser/profiles/profile_selections.cc
+++ chrome/browser/profiles/profile_selections.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/profile_metrics/browser_profile_type.h"
 
 const base::Feature kSystemProfileSelectionDefaultNone{
-    "SystemProfileSlectionDefaultNone",
+    "SystemProfileSelectionDefaultNone",
     base::FeatureState::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kGuestProfileSelectionDefaultNone{
