60d6f71480e62e7a31b35dda30aa22b6a88ec707
Yuki Shiino <yukishiino@chromium.org>
2022-09-07 06:29:25 +0000

    PA: Remove dependency to base/immediate_crash.h from allocator_shim
    Bug: 1151236
    Change-Id: I9adda3275bb827490181f80b26746219c5161081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875842
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043843}

--- base/allocator/partition_allocator/shim/malloc_zone_functions_mac.h
+++ base/allocator/partition_allocator/shim/malloc_zone_functions_mac.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <malloc/malloc.h>
 #include <stddef.h>
 
+#include "base/allocator/partition_allocator/partition_alloc_base/immediate_crash.h"
 #include "base/base_export.h"
-#include "base/immediate_crash.h"
 #include "third_party/apple_apsl/malloc.h"
 
 namespace allocator_shim {
@@ -94,7 +94,7 @@ inline MallocZoneFunctions& GetFunctionsForZone(void* zone) {
     if (g_malloc_zones[i].context == zone)
       return g_malloc_zones[i];
   }
-  IMMEDIATE_CRASH();
+  PA_IMMEDIATE_CRASH();
 }
 
 }  // namespace allocator_shim
