8fe06a4a9d96d5cdea06276f65a7e0164b5afe9b
Lauren Commeignes <laurencom@chromium.org>
2022-09-07 05:46:18 +0000

    Update Omnibox result for rich text to include context that it is a search result
    Bug: 1353173
    Change-Id: I3884f1d9ad0f8164a0dbbcc3afca8eedd99821f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876768
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043835}

--- chrome/browser/ui/app_list/search/omnibox_result.cc
+++ chrome/browser/ui/app_list/search/omnibox_result.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/vector_icons/vector_icons.h"
 #include "ash/public/cpp/style/dark_light_mode_controller.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_util.h"
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher.h"
 #include "chrome/browser/chromeos/launcher_search/search_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -27,10 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/image_util.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "url/gurl.h"
-#include "url/url_canon.h"
 
 using CrosApiSearchResult = crosapi::mojom::SearchResult;
 
@@ -225,11 +221,17 @@ void OmniboxResult::UpdateTitleAndDetails() {
                                           search_result_->contents_type));
 
     if (IsRichEntity()) {
-      SetDetails(description_);
+      // Append the search engine to the description.
+      const std::u16string description_with_search_context =
+          l10n_util::GetStringFUTF16(
+              IDS_APP_LIST_QUERY_SEARCH_DESCRIPTION, description_,
+              GetDefaultSearchEngineName(
+                  TemplateURLServiceFactory::GetForProfile(profile_)));
+      SetDetails(description_with_search_context);
       SetDetailsTags(TagsForTextWithMatchTags(
           query_, description_, search_result_->description_type));
 
-      // Append the search engine to the accessible name only.
+      // Append the search engine to the accessible name.
       const std::u16string accessible_name =
           details().empty() ? title()
                             : base::StrCat({title(), u", ", details()});
