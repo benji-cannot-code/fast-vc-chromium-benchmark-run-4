fb8871e0ea39854cfc68d0b7c26247550a2ea7b9
Antonio Gomes <tonikitoo@igalia.com>
2022-09-07 16:22:13 +0000

    [exo] Mitigate possible race condition in single-tab dragging
    There is a race condiction in exo's ExtendedDragSource logic that
    take place when DragDropOperation::StartDragDropOperation() -
    scheduled by DragDropOperation::ScheduleStartDragDropOperation() -
    gets executed prior to the associated call to
    ExtendedDragSource::Drag().
    Both methods are triggered by the client (eg Lacros) with calls to
    wl_data_device_start_drag() and zcr_extended_drag_source_v1_drag(),
    respective.
    
    This CL mitigates the race, when it happens.
    
    BUG=1351653
    R=oshima@chromium.org
    
    Change-Id: I8016b32eeb7758437c96188828f65d85c5bd50e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871580
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1044007}

--- components/exo/extended_drag_source.cc
+++ components/exo/extended_drag_source.cc
@@ -206,7 +206,14 @@ void ExtendedDragSource::Drag(Surface* dragged_surface,
   dragged_window_holder_ =
       std::make_unique<DraggedWindowHolder>(dragged_surface, drag_offset, this);
 
-  // Drag process will be started once OnDragStarted gets called.
+  // Drag process will be started once OnDragStarted gets called, unless a
+  // OnToplevelWindowDragStarted() got called for a visible window being dragged
+  // prior to this call.
+  if (pending_drag_start_ &&
+      dragged_window_holder_->toplevel_window() == drag_source_window_) {
+    StartDrag(dragged_window_holder_->toplevel_window());
+    pending_drag_start_ = false;
+  }
 }
 
 bool ExtendedDragSource::IsActive() const {
@@ -227,6 +234,8 @@ void ExtendedDragSource::OnToplevelWindowDragStarted(
   if (dragged_window_holder_ && dragged_window_holder_->toplevel_window() &&
       dragged_window_holder_->toplevel_window()->IsVisible()) {
     StartDrag(dragged_window_holder_->toplevel_window());
+  } else {
+    pending_drag_start_ = true;
   }
 }
 
@@ -400,6 +409,7 @@ void ExtendedDragSource::Cleanup() {
   if (drag_source_window_)
     drag_source_window_->RemoveObserver(this);
   drag_source_window_ = nullptr;
+  pending_drag_start_ = false;
   UnlockCursor();
 }
 
