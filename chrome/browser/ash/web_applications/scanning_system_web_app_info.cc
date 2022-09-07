4929e9827e73b28d9b13d0a60e83f9856dd36885
Michael Checo <michaelcheco@google.com>
2022-09-07 15:08:16 +0000

    scanning: Add app icon to launch grid
    Bug: b/241425476
    Change-Id: I214e7cc403b5fb292c3c0d7865ad478065914ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871686
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043968}

--- chrome/browser/ash/web_applications/scanning_system_web_app_info.cc
+++ chrome/browser/ash/web_applications/scanning_system_web_app_info.cc
@@ -59,7 +59,7 @@ std::unique_ptr<WebAppInstallInfo> ScanningSystemAppDelegate::GetWebAppInfo()
 }
 
 bool ScanningSystemAppDelegate::ShouldShowInLauncher() const {
-  return false;
+  return true;
 }
 
 bool ScanningSystemAppDelegate::ShouldCaptureNavigations() const {
