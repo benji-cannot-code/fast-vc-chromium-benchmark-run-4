23990e5d7e396b2b7d92b5f6f0f3b3e3fa7c58ae
Martin Kreichgauer <martinkr@google.com>
2022-09-08 18:12:21 +0000

    webauthn: remove HoverListModel::Observer and references to `ItemType`
    The former looks unimplemented. The latter has been obsolete ever since
    we stopped showing throbber icons for caBLEv2 authenticators, I think.
    
    Bug: 1358719
    Change-Id: I4e7e2a5574825a650569c581cd6cddaf95f15a3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880173
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044664}

--- chrome/browser/ui/webauthn/hover_list_model.h
+++ chrome/browser/ui/webauthn/hover_list_model.h
@@ -19,33 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UI view component.
 class HoverListModel {
  public:
-  enum class ListItemChangeType {
-    kAddToViewComponent,
-    kRemoveFromViewComponent,
-  };
-
-  class Observer {
-   public:
-    virtual void OnListItemAdded(int item_tag) = 0;
-    virtual void OnListItemRemoved(int removed_list_item_tag) = 0;
-    virtual void OnListItemChanged(int changed_list_item_tag,
-                                   ListItemChangeType type) = 0;
-  };
-
   HoverListModel() = default;
-
   HoverListModel(const HoverListModel&) = delete;
   HoverListModel& operator=(const HoverListModel&) = delete;
-
   virtual ~HoverListModel() = default;
 
-  virtual bool ShouldShowPlaceholderForEmptyList() const = 0;
-  virtual std::u16string GetPlaceholderText() const = 0;
-  // GetPlaceholderIcon may return nullptr to indicate that no icon should be
-  // added. This is distinct from using an empty icon as the latter will still
-  // take up as much space as any other icon.
-  virtual const gfx::VectorIcon* GetPlaceholderIcon() const = 0;
-  virtual std::vector<int> GetThrobberTags() const = 0;
   virtual std::vector<int> GetButtonTags() const = 0;
   virtual std::u16string GetItemText(int item_tag) const = 0;
   virtual std::u16string GetDescriptionText(int item_tag) const = 0;
@@ -61,19 +39,6 @@ class HoverListModel {
   // necessary so that all items, including those with descriptions, are the
   // same height.
   virtual bool StyleForTwoLines() const = 0;
-
-  void SetObserver(Observer* observer) {
-    DCHECK(!observer_);
-    observer_ = observer;
-  }
-
-  void RemoveObserver() { observer_ = nullptr; }
-
- protected:
-  Observer* observer() { return observer_; }
-
- private:
-  Observer* observer_ = nullptr;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBAUTHN_HOVER_LIST_MODEL_H_
