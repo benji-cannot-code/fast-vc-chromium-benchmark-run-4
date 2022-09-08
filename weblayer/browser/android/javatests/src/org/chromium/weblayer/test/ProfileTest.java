a84c468191d416d9693b2a865cad9d3b6937da0c
Brandon Wylie <wylieb@chromium.org>
2022-09-08 19:21:58 +0000

    [Sheriff] Disable flaky ProfileTests
    Bug: 1361479
    Change-Id: I4e4be702e882e495d4dc67775d590c603a81faaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884278
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044709}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ProfileTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ProfileTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.Callback;
 import org.chromium.weblayer.CookieManager;
@@ -145,6 +146,7 @@ public class ProfileTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "see crbug.com/1359894")
     public void testEnumerateAllProfileNames() throws Exception {
         final String profileName = "TestEnumerateAllProfileNames";
         final InstrumentationActivity activity = mActivityTestRule.launchWithProfile(profileName);
@@ -183,6 +185,7 @@ public class ProfileTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "see crbug.com/1359894")
     public void testReuseProfile() throws Exception {
         final String profileName = "ReusedProfile";
         final Uri uri = Uri.parse("https://foo.bar");
