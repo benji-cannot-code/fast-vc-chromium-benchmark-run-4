f381012377881c11bdf8085fe28f79bb5c797b74
Christian Flach <cmfcmf@chromium.org>
2022-09-08 23:40:58 +0000

    web-bundles: Simplify SharedFile and fix potential UAF
    `SharedFile` had multiple issues, which this CL fixes:
    
    1. The comment in `DuplicateFile` was wrong/outdated. `DuplicateFile`
       could indeed be called multiple times, not just once. The use of the
       `duplicate_callback_` instance variable was completely superfluous,
       given that the callback was moved out of it immediately as part of
       the synchronous call to `SetFile`.
    2. The private `SetFile` method isn't really needed, and only made the
       class more complex to understand, given that it was called only from
       the constructor (in which case `duplicate_callback_` is `null`
       anyways) and synchronously from `DuplicateFile`.
    3. An outside user of the class had no way to know when the file was
       opened successfully and thus when it was safe to access it via
       `operator->`. This is because the file was opened asynchronously from
       the constructor. Similarly, calls to `DuplicateFile` were just
       silently dropped while the file was still being opened, which could
       have led to surprising issues for users of this class. Existing code
       only worked because `SharedFile` instances weren't immediately used
       after creating them. Instead of opening the file inside the
       constructor, this class now requires its users to provide it an
       already opened file.
    4. The task posted to the `ThreadPool` to duplicate the file does so
       using a pointer to the `base::File` instance held in the class. This
       could cause a UAF if the destructor of the class was called and its
       task to delete the file ran before the task to duplicate the file.
       This is now fixed by running both tasks on a
       `base::SequencedTaskRunner`, which guarantees that the file will
       still be valid when the task to duplicate it runs, even if the
       `SharedFile`'s destructor has run in the meantime. For deleting the
       file on a blocking sequence, the class now uses the more idiomatic
       `DeleteSoon`. Use of the `base::SequencedTaskRunner` no longer allows
       us to run the task to delete the file with `BEST_EFFORT` priority,
       but given that deleting the file is not a huge task, this trade-off
       seems worthwhile, given that this a avoids a potential UAF.
    
    LOW_COVERAGE_REASON=Additional tests are added in followup CLs.
    
    Bug: 1315947
    Change-Id: Ic296e9c15f28f6e394858ec25a8fe89138c5e6e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3873630
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044850}

--- components/web_package/shared_file.cc
+++ components/web_package/shared_file.cc
@@ -14,23 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_package {
 
-SharedFile::SharedFile(
-    base::OnceCallback<std::unique_ptr<base::File>()> open_file_callback) {
-  base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock()}, std::move(open_file_callback),
-      base::BindOnce(&SharedFile::SetFile, base::RetainedRef(this)));
-}
+SharedFile::SharedFile(std::unique_ptr<base::File> file)
+    : task_runner_(
+          base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()})),
+      file_(std::move(file)) {}
 
 void SharedFile::DuplicateFile(base::OnceCallback<void(base::File)> callback) {
-  // Basically this interface expects this method is called at most once. Have
-  // a DCHECK for the case that does not work for a clear reason, just in case.
-  // The call site also have another DCHECK for external callers not to cause
-  // such problematic cases.
-  DCHECK(duplicate_callback_.is_null());
-  duplicate_callback_ = std::move(callback);
-
-  if (file_)
-    SetFile(std::move(file_));
+  DCHECK(file_);
+  task_runner_->PostTaskAndReplyWithResult(
+      FROM_HERE,
+      base::BindOnce(
+          [](base::File* file) -> base::File { return file->Duplicate(); },
+          file_.get()),
+      std::move(callback));
 }
 
 base::File* SharedFile::operator->() {
@@ -39,26 +35,13 @@ base::File* SharedFile::operator->() {
 }
 
 SharedFile::~SharedFile() {
-  // Move the last reference to |file_| that leads an internal blocking call
+  // Move the last reference to `file_` whose destructor leads to blocking call
   // that is not permitted here.
-  base::ThreadPool::PostTask(
-      FROM_HERE, {base::TaskPriority::BEST_EFFORT, base::MayBlock()},
-      base::BindOnce([](std::unique_ptr<base::File> file) {},
-                     std::move(file_)));
-}
-
-void SharedFile::SetFile(std::unique_ptr<base::File> file) {
-  file_ = std::move(file);
-
-  if (duplicate_callback_.is_null())
-    return;
-
-  base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock()},
-      base::BindOnce(
-          [](base::File* file) -> base::File { return file->Duplicate(); },
-          file_.get()),
-      std::move(duplicate_callback_));
+  // It is important that this runs on the same `base::SequencedTaskRunner` that
+  // the callback scheduled in `DuplicateFile` runs on. Otherwise deleting the
+  // file might lead to a UAF inside the file duplication callback if that one
+  // happened to run after the deletion of the file.
+  task_runner_->DeleteSoon(FROM_HERE, std::move(file_));
 }
 
 std::unique_ptr<SharedFile::SharedFileDataSource> SharedFile::CreateDataSource(
