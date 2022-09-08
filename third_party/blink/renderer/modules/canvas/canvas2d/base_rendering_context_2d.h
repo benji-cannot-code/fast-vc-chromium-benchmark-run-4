899747f5c7d534c15c3a239deddc7298e1a84127
Justin Novosad <junov@chromium.org>
2022-09-08 22:16:56 +0000

    Use AffineTransfrom in CanvasPath::GetTransform
    This is a follow-up to CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3873899
    
    The previous CL left us in a state where isInvertible was being
    called on AffineTransform in one place and on TransformMatrix in another
    place. This caused a DCHECK to fail due to a lack of consistency in
    the results of the two IsInvertible() calls.  They were yielding
    different results due to machine precision issues.
    
    By using AffinTransform everywhere, we have consistency once again.
    
    Bug: 1361266
    Change-Id: Ic6870ee2283d9406d273b21bbd22caa4b29cb731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884058
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044805}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -514,7 +514,7 @@ class MODULES_EXPORT BaseRenderingContext2D : public CanvasPath {
 
   void SetTransform(const AffineTransform&);
 
-  TransformationMatrix GetTransform() const override;
+  AffineTransform GetTransform() const override;
 
   bool StateHasFilter();
 
