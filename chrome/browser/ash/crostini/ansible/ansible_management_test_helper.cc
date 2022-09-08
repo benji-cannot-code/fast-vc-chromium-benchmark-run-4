ee9c3d8117c0399fe43010500a17751f0115cdda
Justin Huang <justinhuang@google.com>
2022-09-08 02:12:03 +0000

    Creating a mock of AnsibleManagementService and splitting out tests
    + Created MockAnsibleManagementService
    * Modified all non-AnsibleManagementService unittests to use the mock
      instead of running through the real AnsibleManagement service since it
      doesn't really make sense for CrostiniManager to test
      AnsibleManagementService internals.
    - Removed CrostiniManager unittests that test AnsibleManagementService
      internals
    
    Bug: b:245020120
    Test: Unittests run
    Change-Id: I8e32c10ac29f7093b290c0a128475708851cd1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874110
    Commit-Queue: Justin Huang <justinhuang@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044313}

--- chrome/browser/ash/crostini/ansible/ansible_management_test_helper.cc
+++ chrome/browser/ash/crostini/ansible/ansible_management_test_helper.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/ansible/ansible_management_test_helper.h"
 
 #include "base/files/file_util.h"
+#include "chrome/browser/ash/crostini/ansible/ansible_management_service_factory.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -13,6 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 
 namespace crostini {
+
+// static
+MockAnsibleManagementService*
+AnsibleManagementTestHelper::SetUpMockAnsibleManagementService(
+    Profile* profile) {
+  return static_cast<MockAnsibleManagementService*>(
+      AnsibleManagementServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+          profile, base::BindRepeating(
+                       [](Profile* profile, content::BrowserContext* context) {
+                         return MockAnsibleManagementService::Build(profile);
+                       },
+                       profile)));
+}
 
 AnsibleManagementTestHelper::AnsibleManagementTestHelper(Profile* profile)
     : profile_(profile) {
--- chrome/browser/ash/crostini/ansible/ansible_management_test_helper.h
+++ chrome/browser/ash/crostini/ansible/ansible_management_test_helper.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_CROSTINI_ANSIBLE_ANSIBLE_MANAGEMENT_TEST_HELPER_H_
 
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ash/crostini/ansible/mock_ansible_management_service.h"
 #include "chromeos/ash/components/dbus/cicerone/cicerone_service.pb.h"
 #include "chromeos/ash/components/dbus/cicerone/fake_cicerone_client.h"
 
@@ -20,6 +21,10 @@ namespace crostini {
 class AnsibleManagementTestHelper {
  public:
   explicit AnsibleManagementTestHelper(Profile* profile);
+
+  // Sets up and returns a mock instance of AnsibleManagementService.
+  static MockAnsibleManagementService* SetUpMockAnsibleManagementService(
+      Profile* profile);
 
   void SetUpAnsiblePlaybookPreference();
   void SetUpAnsibleInfra();
