113fb278726961f5abd83e514f3b84490d8873f0
kylechar <kylechar@chromium.org>
2022-09-08 03:50:53 +0000

    Reland "Make compound backing work with GLTextureImageBacking"
    This is a reland of commit 8d6fa86174dda68bdfa7b42e19369e3a083284c2.
    It adds code to produce a memory backed overlay representation which is
    used on Windows.
    
    Original change's description:
    > Make compound backing work with GLTextureImageBacking
    >
    > Make GLTextureImageBacking work with compound image backing and have it
    > replace GLImageBacking for shared memory GMBs.
    > GLTextureImageBackingFactory ignores scanout usage for shared memory
    > GMBs to match existing behaviour in GLImageBackingFactory.
    >
    > This also removes the GLImageBackingFactory functionality to work with
    > shared memory GMBs and having two instances of the factory.
    > Unfortunately GLTextureImageBackingFactory needs two instances, one for
    > regular GL textures and one for shared memory GMBs, to ensure we pick
    > the right backing type on Windows. This should be a temporary state
    > until usage flags to specify when D3D backing, eg. supporting
    > CopyToGpuMemoryBuffer() and ProduceMemory() for overlay path, are needed
    > get added.
    >
    > Bug: 1293509
    > Change-Id: I6eb071c8b27f92a2cbcafbb6145f4c05ce04c000
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3774213
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035129}
    
    Bug: 1293509
    Change-Id: Ie08ee7d29e0cefa44d4b8fb2fbc852cda41a7d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832922
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044355}

--- gpu/command_buffer/service/shared_image/shared_image_factory.cc
+++ gpu/command_buffer/service/shared_image/shared_image_factory.cc
@@ -322,7 +322,8 @@ SharedImageFactory::SharedImageFactory(
         std::make_unique<GLTextureImageBackingFactory>(
             gpu_preferences, workarounds, feature_info.get(),
             shared_context_state_ ? shared_context_state_->progress_reporter()
-                                  : nullptr);
+                                  : nullptr,
+            /*for_cpu_upload_usage=*/false);
     factories_.push_back(std::move(gl_texture_backing_factory));
   }
 
@@ -339,12 +340,13 @@ SharedImageFactory::SharedImageFactory(
 
 #if !BUILDFLAG(IS_ANDROID)
   if (use_gl) {
-    auto gl_image_backing_factory = std::make_unique<GLImageBackingFactory>(
-        gpu_preferences, workarounds, feature_info.get(), image_factory,
-        shared_context_state_ ? shared_context_state_->progress_reporter()
-                              : nullptr,
-        /*for_shared_memory_gmbs=*/true);
-    factories_.push_back(std::move(gl_image_backing_factory));
+    auto gl_texture_backing_factory =
+        std::make_unique<GLTextureImageBackingFactory>(
+            gpu_preferences, workarounds, feature_info.get(),
+            shared_context_state_ ? shared_context_state_->progress_reporter()
+                                  : nullptr,
+            /*for_cpu_upload_usage=*/true);
+    factories_.push_back(std::move(gl_texture_backing_factory));
   }
 #endif
 
@@ -433,8 +435,7 @@ SharedImageFactory::SharedImageFactory(
     auto gl_image_backing_factory = std::make_unique<GLImageBackingFactory>(
         gpu_preferences, workarounds, feature_info.get(), image_factory,
         shared_context_state_ ? shared_context_state_->progress_reporter()
-                              : nullptr,
-        /*for_shared_memory_gmbs=*/false);
+                              : nullptr);
     factories_.push_back(std::move(gl_image_backing_factory));
   }
 #endif
@@ -531,9 +532,14 @@ bool SharedImageFactory::CreateSharedImage(const Mailbox& mailbox,
 
   std::unique_ptr<SharedImageBacking> backing;
   if (use_compound) {
+#if BUILDFLAG(IS_WIN)
+    constexpr bool allow_shm_overlays = true;
+#else
+    constexpr bool allow_shm_overlays = false;
+#endif
     backing = CompoundImageBacking::CreateSharedMemory(
-        factory, mailbox, std::move(handle), format, plane, surface_handle,
-        size, color_space, surface_origin, alpha_type, usage);
+        factory, allow_shm_overlays, mailbox, std::move(handle), format, plane,
+        surface_handle, size, color_space, surface_origin, alpha_type, usage);
   } else {
     backing = factory->CreateSharedImage(
         mailbox, client_id, std::move(handle), format, plane, surface_handle,
