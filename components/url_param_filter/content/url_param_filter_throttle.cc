65627a00e87b18ed3fe490ed5d3487c061d7fdac
Matt Reichhoff <mreichhoff@chromium.org>
2022-09-08 16:20:20 +0000

    [UrlParamFilter] Do not strip nested URLs on non-iOS
    Experimentation shows stripping of such parameters occasionally
    results in interstitials. It was necessary for a prior version of
    the experiment, but with redirects protected, it is less important.
    The prior version is still in use on iOS, however.
    
    Bug: 1360989
    Change-Id: I682f82b2c9b016ac22809e0325d62ea14197fbb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879903
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Brian Begnoche <bcb@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044590}

--- components/url_param_filter/content/url_param_filter_throttle.cc
+++ components/url_param_filter/content/url_param_filter_throttle.cc
@@ -82,7 +82,8 @@ void UrlParamFilterThrottle::WillStartRequest(network::ResourceRequest* request,
                                               bool* defer) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  FilterResult result = FilterUrl(last_hop_initiator_, request->url);
+  FilterResult result = FilterUrl(last_hop_initiator_, request->url,
+                                  NestedFilterOption::kNoFilterNested);
 
   if (should_filter_) {
     request->url = result.filtered_url;
@@ -103,7 +104,8 @@ void UrlParamFilterThrottle::WillRedirectRequest(
     net::HttpRequestHeaders* modified_request_headers,
     net::HttpRequestHeaders* modified_cors_exempt_request_headers) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  FilterResult result = FilterUrl(last_hop_initiator_, redirect_info->new_url);
+  FilterResult result = FilterUrl(last_hop_initiator_, redirect_info->new_url,
+                                  NestedFilterOption::kNoFilterNested);
 
   if (should_filter_) {
     redirect_info->new_url = result.filtered_url;
