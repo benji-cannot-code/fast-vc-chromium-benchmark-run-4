19a4776ed26e793dff06d31521bd357703c8cc0b
Matteo Ruello <mruel@google.com>
2022-09-08 14:22:38 +0000

    Adjusting comments
    Bug: b/245710837
    Change-Id: Ib6327e956b5d2ddc840569d86bb37db4ee78e44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879751
    Auto-Submit: Matteo Ruello <mruel@google.com>
    Commit-Queue: Matteo Ruello <mruel@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044528}

--- components/autofill_assistant/browser/service/no_round_trip_service_unittest.cc
+++ components/autofill_assistant/browser/service/no_round_trip_service_unittest.cc
@@ -44,9 +44,9 @@ GetNoRoundTripScriptsByHashPrefixResponseProto::MatchInfo CreateExampleMatch(
   script->set_path(path);
   match.set_domain(domain);
   auto* routine_script = match.add_routine_scripts();
-  auto* autobot_response = routine_script->mutable_autobot_response();
+  auto* action_response = routine_script->mutable_action_response();
   routine_script->set_script_path(path);
-  autobot_response->add_actions()->set_action_delay_ms(action_delay);
+  action_response->add_actions()->set_action_delay_ms(action_delay);
 
   return match;
 }
@@ -122,9 +122,9 @@ TEST_F(NoRoundTripServiceTest, CallsEndpointAndPopulatesStore) {
   EXPECT_EQ(local_script_store->GetDomain(), kFakeUrl);
   ASSERT_EQ(local_script_store->GetRoutines().size(), 1ul);
   auto routine_script = local_script_store->GetRoutines()[0];
-  auto autobot_response = routine_script.autobot_response();
-  ASSERT_EQ(autobot_response.actions().size(), 1);
-  auto action = autobot_response.actions()[0];
+  auto action_response = routine_script.action_response();
+  ASSERT_EQ(action_response.actions().size(), 1);
+  auto action = action_response.actions()[0];
   EXPECT_EQ(action.action_delay_ms(), 37);
   EXPECT_EQ(routine_script.script_path(), path);
   ASSERT_EQ(local_script_store->GetSupportsSiteResponse().scripts().size(), 1);
@@ -189,9 +189,9 @@ TEST_F(NoRoundTripServiceTest, SelectsRightMatchWhenMultipleAreReturned) {
   EXPECT_EQ(local_script_store->GetDomain(), kFakeUrl);
   ASSERT_THAT(local_script_store->GetRoutines(), Not(IsEmpty()));
   auto routine_script = local_script_store->GetRoutines()[0];
-  auto autobot_response = routine_script.autobot_response();
-  ASSERT_THAT(autobot_response.actions(), Not(IsEmpty()));
-  auto action = autobot_response.actions()[0];
+  auto action_response = routine_script.action_response();
+  ASSERT_THAT(action_response.actions(), Not(IsEmpty()));
+  auto action = action_response.actions()[0];
   EXPECT_EQ(action.action_delay_ms(), 42);
   EXPECT_EQ(routine_script.script_path(), script_path);
   ASSERT_THAT(local_script_store->GetSupportsSiteResponse().scripts(),
@@ -298,12 +298,12 @@ TEST_F(NoRoundTripServiceTest, WithExistingPathGetActionsSucceeds) {
   GetNoRoundTripScriptsByHashPrefixResponseProto_MatchInfo_RoutineScript
       routine;
   routine.set_script_path("script_path");
-  routine.mutable_autobot_response()->set_run_id(0);
+  routine.mutable_action_response()->set_run_id(0);
   routines_.push_back(routine);
 
   EXPECT_CALL(
       mock_response_callback_,
-      Run(net::HTTP_OK, routine.autobot_response().SerializeAsString(), _));
+      Run(net::HTTP_OK, routine.action_response().SerializeAsString(), _));
 
   GetService().GetActions("script_path", GURL(kFakeUrl), TriggerContext(), "",
                           "", mock_response_callback_.Get());
@@ -314,7 +314,7 @@ TEST_F(NoRoundTripServiceTest, GetActionsFailsGivenWrongPath) {
   GetNoRoundTripScriptsByHashPrefixResponseProto_MatchInfo_RoutineScript
       routine;
   routine.set_script_path("not_relevant_for_test");
-  routine.mutable_autobot_response()->set_run_id(0);
+  routine.mutable_action_response()->set_run_id(0);
   routines_.push_back(routine);
 
   EXPECT_CALL(mock_response_callback_,
