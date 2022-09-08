0791e42d3896d04df0ab7cbd667c45c68fe0d29b
Rebekah Potter <rbpotter@chromium.org>
2022-09-08 01:08:54 +0000

    Revert "Update copyright headers in build/, build_overrides/, and buildtools/"
    This reverts commit 8996d5a5ca84fc9faeade4676b2105803e21cca4.
    
    Reason for revert: Breaks android-binary-size bot, see
    https://bugs.chromium.org/p/chromium/issues/detail?id=1361092
    
    Original change's description:
    > Update copyright headers in build/, build_overrides/, and buildtools/
    >
    > The methodology used to generate this CL is documented in
    > https://crbug.com/1098010#c21.
    >
    > No-Try: true
    > Bug: 1098010
    > Change-Id: I0323d0734081fc1aaf3b9ec3b8184411fe1a368a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879808
    > Auto-Submit: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Owners-Override: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044227}
    
    Bug: 1098010
    Change-Id: Ie1b5a07d859e98588c1497220aac54834ea5d7f0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880816
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044284}

--- build/android/unused_resources/UnusedResources.java
+++ build/android/unused_resources/UnusedResources.java
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // Modifications are owned by the Chromium Authors.
-// Copyright 2021 The Chromium Authors
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
--- build/sanitize-win-build-log.sh
+++ build/sanitize-win-build-log.sh
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/sh
-# Copyright 2010 The Chromium Authors
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 sed -r -f `dirname "${0}"`/`basename "${0}" sh`sed
