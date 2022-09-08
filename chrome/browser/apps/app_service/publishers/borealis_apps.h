de5a03113a3795663897dbd3063e0dc2659f9098
Nicholas Hollingum <hollingum@google.com>
2022-09-08 02:41:13 +0000

    borealis: Show borealis' installer in apps search.
    Borealis' installer will be shown in the apps service on compatible
    devices when a user searches for "steam". Currently the app shows as
    "Steam" and selecting it brings up the borealis installer.
    
    If borealis is already installed, the app is not shown. The app is made
    visible/invisible as-needed based on changes to borealis' installation
    state.
    
    We also take the opportunity to clean up the way borealis' apps are
    initialized, simplifying the code.
    
    Bug: b/243733660
    Change-Id: I0d15a385749fe9e24640787285c29979dfd50c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865142
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044327}

--- chrome/browser/apps/app_service/publishers/borealis_apps.h
+++ chrome/browser/apps/app_service/publishers/borealis_apps.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/apps/app_service/app_icon/icon_key_util.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_forward.h"
@@ -59,6 +60,16 @@ class BorealisApps
  private:
   friend class PublisherHost;
 
+  // Helper method for dispatching to the provided |callback| once we
+  // have queried whether borealis is allowed not installed.
+  void CallWithBorealisAllowed(base::OnceCallback<void(bool)> callback);
+
+  // Called after determining whether borealis is |allowed| and |enabled|, this
+  // method sets up the "special" (i.e. non-vm, non-anonymous) apps used by
+  // borealis, such as its installer.
+  void SetUpSpecialApps(bool allowed);
+  void SetUpSpecialAppsMojom(bool allowed);
+
   // Helper method to get the registry used by this profile
   guest_os::GuestOsRegistryService* Registry();
 
@@ -73,6 +84,14 @@ class BorealisApps
       bool new_icon_key);
 
   void Initialize();
+
+  // Called by the pref registry when one of borealis' global permissions (mic,
+  // camera, etc) change.
+  void OnPermissionChanged();
+
+  // Called by the pref registry when borealis' installation pref changes (i.e
+  // for install and uninstall.
+  void OnInstallChanged();
 
   // apps::AppPublisher overrides.
   void LoadIcon(const std::string& app_id,
@@ -123,7 +142,6 @@ class BorealisApps
       const std::vector<std::string>& updated_apps,
       const std::vector<std::string>& removed_apps,
       const std::vector<std::string>& inserted_apps) override;
-  void OnPermissionChanged();
 
   // borealis::BorealisWindowManager::AnonymousAppObserver overrides.
   void OnAnonymousAppAdded(const std::string& shelf_app_id,
@@ -143,6 +161,8 @@ class BorealisApps
       anonymous_app_observation_{this};
 
   PrefChangeRegistrar pref_registrar_;
+
+  base::WeakPtrFactory<BorealisApps> weak_factory_{this};
 };
 
 }  // namespace apps
