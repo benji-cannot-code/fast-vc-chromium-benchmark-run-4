4129221718830db2de90f1d1a6fd1ea1510e7701
Joemer Ramos <joemerramos@chromium.org>
2022-09-08 19:57:28 +0000

    [iOS] Create InfobarBannerOverlayMediator for Tailored Security
    Created a mediator for tailored security service infobar banner. This
    CL is creating the mediator files and relevant test coverage. It also
    links the mediator to the coordinator which shouldn't cause any issues
    since nothing calls a TailoredSecurityInfobarBannerMediator yet.
    
    Bug: 1357605
    Change-Id: Ide545c0400768cfac56dc8e9095e2078bddc26f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876322
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044726}

--- ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_overlay_coordinator.mm
+++ ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_overlay_coordinator.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/overlays/infobar_banner/permissions/permissions_infobar_banner_overlay_mediator.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/reading_list/reading_list_infobar_banner_overlay_mediator.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/save_card/save_card_infobar_banner_overlay_mediator.h"
+#import "ios/chrome/browser/ui/overlays/infobar_banner/tailored_security/tailored_security_infobar_banner_overlay_mediator.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/translate/translate_infobar_banner_overlay_mediator.h"
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator+subclassing.h"
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator_delegate.h"
@@ -58,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [SaveAddressProfileInfobarBannerOverlayMediator class],
     [AddToReadingListInfobarBannerOverlayMediator class],
     [PermissionsBannerOverlayMediator class],
+    [TailoredSecurityInfobarBannerOverlayMediator class],
   ];
 }
 
