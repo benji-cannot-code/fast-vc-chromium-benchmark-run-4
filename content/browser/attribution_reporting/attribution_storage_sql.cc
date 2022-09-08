d53a4794f0abea6b449109754c318ab530c648a5
Andrew Paseltiner <apaseltiner@chromium.org>
2022-09-08 15:29:40 +0000

    Remove unnecessary column name from SQL count
    The column name is meaningless here and is equivalent to *, which is
    clearer and slightly reduces binary size.
    
    Change-Id: If585cf7c129fcbaff35081afcf702cd0f7a0b7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880104
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044563}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -1812,7 +1812,7 @@ bool AttributionStorageSql::HasCapacityForStoringSource(
     const std::string& serialized_origin) {
   static constexpr char kCountSourcesSql[] =
       // clang-format off
-      "SELECT COUNT(source_origin)FROM sources "
+      "SELECT COUNT(*)FROM sources "
       DCHECK_SQL_INDEXED_BY("sources_by_origin")
       "WHERE source_origin=?";  // clang-format on
 
