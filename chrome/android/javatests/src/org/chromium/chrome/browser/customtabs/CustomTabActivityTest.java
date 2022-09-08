f18d33bda3a8fea732b3c605b36117ef9d4ed1f8
Brandon Wylie <wylieb@chromium.org>
2022-09-08 22:28:57 +0000

    [Sheriff] Disable failing CustomTabActivityTest
    Bug: 1361534
    Change-Id: Icfc6245ba0a5716142c6d795bb3dd8be7ea11c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885535
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: bttk - <bttk@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: bttk - <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044811}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -1540,6 +1540,7 @@ public class CustomTabActivityTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "see crbug.com/1361534")
     public void testLaunchIncognitoURL() throws Exception {
         Intent intent = createMinimalCustomTabIntent();
         mCustomTabActivityTestRule.startCustomTabActivityWithIntent(intent);
