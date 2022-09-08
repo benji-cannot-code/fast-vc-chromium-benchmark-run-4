f9b9275b476ed2b6fb698f9ffc393bbe2f689370
Patrick Noland <pnoland@google.com>
2022-09-08 17:07:39 +0000

    [Journeys] Add delay to fetch when search text changes
    This should be a bit more performant and feel less jerky since it will
    let users type fast before the UI changes. This change also vertically centers
    the spinner when it's displayed.
    
    Bug: 1355266
    Change-Id: Ib004b5ff321aa077a0825680a235579e5e68323e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866094
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044621}

--- chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersCoordinator.java
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersCoordinator.java
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.history_clusters;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -98,7 +101,7 @@ public class HistoryClustersCoordinator extends RecyclerView.OnScrollListener
                 mSelectionDelegate, mMetricsLogger, accessibilityUtil, (message) -> {
                     if (mRecyclerView == null) return;
                     mRecyclerView.announceForAccessibility(message);
-                });
+                }, new Handler());
     }
 
     /**
@@ -225,8 +228,13 @@ public class HistoryClustersCoordinator extends RecyclerView.OnScrollListener
     }
 
     private View buildMoreProgressView(ViewGroup parent) {
-        return LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.more_progress_button, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.more_progress_button, parent, false);
+        View progressSpinner = view.findViewById(R.id.progress_spinner);
+        if (progressSpinner != null) {
+            ((LayoutParams) progressSpinner.getLayoutParams()).gravity = Gravity.CENTER;
+        }
+        return view;
     }
 
     private View buildClusterView(ViewGroup parent) {
