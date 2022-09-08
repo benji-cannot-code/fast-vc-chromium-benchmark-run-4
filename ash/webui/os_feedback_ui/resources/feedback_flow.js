8249414b02b775a8b9d9af8c3581079d832281e3
Danny Wang <wangdanny@google.com>
2022-09-08 22:37:52 +0000

    Feedback: Include category_tag in report
    Bug: b:185624798
    Change-Id: Id637ad902e663c6403775f6a6920d9e2eb159aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864226
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044817}

--- ash/webui/os_feedback_ui/resources/feedback_flow.js
+++ ash/webui/os_feedback_ui/resources/feedback_flow.js
@@ -45,6 +45,7 @@ export const FeedbackFlowState = {
 export const AdditionalContextQueryParam = {
   DESCRIPTION_TEMPLATE: 'description_template',
   EXTRA_DIAGNOSTICS: 'extra_diagnostics',
+  CATEGORY_TAG: 'category_tag',
 };
 
 /**
@@ -307,6 +308,9 @@ export class FeedbackFlowElement extends PolymerElement {
         descriptionTemplate && descriptionTemplate.length > 0 ?
         decodeURIComponent(descriptionTemplate) :
         '';
+    const categoryTag = params.get(AdditionalContextQueryParam.CATEGORY_TAG);
+    this.feedbackContext_.categoryTag =
+        categoryTag ? decodeURIComponent(categoryTag) : '';
   }
 
   /**
