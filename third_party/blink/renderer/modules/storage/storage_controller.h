1b9b4b40d25dbcf962635dfaa1e3d1a5e080eb36
Dave Tapuska <dtapuska@chromium.org>
2022-09-08 17:48:30 +0000

    Store a task runner on the StorageNamespace.
    When StorageNamespace is session storage it is associated with a set of
    Pages. These pages will be in the same AgentSchedulingGroup so hold a
    reference to the default task runner for binding the mojo channels to.
    
    BUG=624696
    
    Change-Id: I9f47f603abc15638ce75c2b1865348e1f8f85026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872588
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044648}

--- third_party/blink/renderer/modules/storage/storage_controller.h
+++ third_party/blink/renderer/modules/storage/storage_controller.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}
-
 namespace blink {
 
 class CachedStorageArea;
@@ -59,14 +55,13 @@ class MODULES_EXPORT StorageController : public mojom::blink::DomStorageClient {
     mojo::Remote<mojom::blink::DomStorage> dom_storage_remote;
     mojo::PendingReceiver<mojom::blink::DomStorageClient> client_receiver;
   };
-  StorageController(DomStorageConnection connection,
-                    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
-                    size_t total_cache_limit);
+  StorageController(DomStorageConnection connection, size_t total_cache_limit);
 
   // Creates a MakeGarbageCollected<StorageNamespace> for Session storage, and
   // holds a weak reference for accounting & clearing. If there is already a
   // StorageNamespace created for the given id, it is returned.
-  StorageNamespace* CreateSessionStorageNamespace(const String& namespace_id);
+  StorageNamespace* CreateSessionStorageNamespace(Page& page,
+                                                  const String& namespace_id);
 
   // Returns the total size of all cached areas in namespaces this controller
   // knows of.
@@ -88,17 +83,12 @@ class MODULES_EXPORT StorageController : public mojom::blink::DomStorageClient {
     return dom_storage_remote_.get();
   }
 
-  scoped_refptr<base::SingleThreadTaskRunner> TaskRunner() {
-    return task_runner_;
-  }
-
  private:
   void EnsureLocalStorageNamespaceCreated();
 
   // mojom::blink::DomStorageClient:
   void ResetStorageAreaAndNamespaceConnections() override;
 
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   Persistent<HeapHashMap<String, WeakMember<StorageNamespace>>> namespaces_;
   Persistent<StorageNamespace> local_storage_namespace_;
   size_t total_cache_limit_;
--- third_party/blink/renderer/modules/storage/storage_namespace.cc
+++ third_party/blink/renderer/modules/storage/storage_namespace.cc
@@ -49,11 +49,13 @@ const char StorageNamespace::kSupplementName[] = "SessionStorageNamespace";
 
 StorageNamespace::StorageNamespace(StorageController* controller)
     : Supplement(nullptr), controller_(controller) {}
-StorageNamespace::StorageNamespace(StorageController* controller,
+StorageNamespace::StorageNamespace(Page& page,
+                                   StorageController* controller,
                                    const String& namespace_id)
-    : Supplement(nullptr),
+    : Supplement(page),
       controller_(controller),
-      namespace_id_(namespace_id) {}
+      namespace_id_(namespace_id),
+      task_runner_(page.GetAgentGroupScheduler().DefaultTaskRunner()) {}
 
 // static
 void StorageNamespace::ProvideSessionStorageNamespaceTo(
@@ -63,7 +65,7 @@ void StorageNamespace::ProvideSessionStorageNamespaceTo(
     return;
   auto* ss_namespace =
       StorageController::GetInstance()->CreateSessionStorageNamespace(
-          String(namespace_id.data(), namespace_id.length()));
+          page, String(namespace_id.data(), namespace_id.length()));
   if (!ss_namespace)
     return;
   ProvideTo(page, ss_namespace);
@@ -241,8 +243,7 @@ void StorageNamespace::EnsureConnected() {
   if (namespace_.is_bound())
     return;
   controller_->dom_storage()->BindSessionStorageNamespace(
-      namespace_id_,
-      namespace_.BindNewPipeAndPassReceiver(controller_->TaskRunner()));
+      namespace_id_, namespace_.BindNewPipeAndPassReceiver(task_runner_));
 }
 
 }  // namespace blink
