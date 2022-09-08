681668471cf7c5676322b0ad07fc5740c2018479
dpapad <dpapad@chromium.org>
2022-09-08 08:33:34 +0000

    WebUI: Check-in Polymer3 version of icons.html, shared_vars.html
    Also adding temporary aliases so that the following URL pairs serve
    the same file each:
    
    Pair1:
      - cr_elements/shared_vars_css.m.js (old name)
      - cr_elements/cr_shared_vars.css.js (new name)
    
    Pair2:
      - cr_elements/icons.m.js (old name)
      - cr_elements/icons.html.js (new name)
    
    to make it easier to check-in the Polymer3 version of the code as there
    are 300+ references to it. Remaining references to the old names will be
    updated in follow-up CLs.
    
    Bug: 1184053
    Change-Id: I7ed29fbcd2b8834d17af1fbe81e8081c7a490ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877429
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044442}

--- ui/webui/resources/cr_elements/cr_checkbox/cr_checkbox.ts
+++ ui/webui/resources/cr_elements/cr_checkbox/cr_checkbox.ts
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *  --cr-checkbox-unchecked-box-color
  */
 import '//resources/polymer/v3_0/paper-styles/color.js';
-import '../shared_vars_css.m.js';
+import '../cr_shared_vars.css.js';
 
 import {PaperRippleBehavior} from '//resources/polymer/v3_0/paper-behaviors/paper-ripple-behavior.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
