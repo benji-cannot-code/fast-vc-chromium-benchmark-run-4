18a64290237f4b942c1dd9c8243e50408612b358
L. David Baron <dbaron@chromium.org>
2022-09-08 13:35:44 +0000

    Use ANGLE + SwiftShader when running under rr debugger.
    As discussed in https://github.com/rr-debugger/rr/issues/3341 , some
    configurations of running Chromium or ChromeOS can end up with graphics
    drivers that write directly to the process memory.  Using ANGLE +
    SwiftShader appears to be a good way to avoid this.
    
    This should make running Chromium under rr a more reliable experience
    out-of-the-box, although it does mean that it's more clearly not usable
    for parts of the graphics stack that, at least in many cases, aren't
    usable under rr anyway.
    
    Bug: 1359355
    Change-Id: I7d779cf0383f9fe9ea2bebff8543909c282a68ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787592
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044516}

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <cstdlib>
 #include <sstream>
 #include <string>
 
@@ -285,7 +286,19 @@ GetRequestedGLImplementationFromCommandLine(
     const base::CommandLine* command_line,
     bool* fallback_to_software_gl) {
   *fallback_to_software_gl = false;
-  if (command_line->HasSwitch(switches::kOverrideUseSoftwareGLForTests)) {
+  bool overrideUseSoftwareGL =
+      command_line->HasSwitch(switches::kOverrideUseSoftwareGLForTests);
+#if BUILDFLAG(IS_LINUX) || \
+    (BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_CHROMEOS_DEVICE))
+  if (std::getenv("RUNNING_UNDER_RR")) {
+    // https://rr-project.org/ is a Linux-only record-and-replay debugger that
+    // is unhappy when things like GPU drivers write directly into the
+    // process's address space.  Using swiftshader helps ensure that doesn't
+    // happen and keeps Chrome and linux-chromeos usable with rr.
+    overrideUseSoftwareGL = true;
+  }
+#endif
+  if (overrideUseSoftwareGL) {
     return GetSoftwareGLImplementation();
   }
 
