a44ebd185cb0de8293bbab04119141bc71e7c093
Renato Silva <rrsilva@google.com>
2022-09-08 15:04:50 +0000

    [gaia_auth_host] Modularize password change authenticator
    Create an initial test infrastructure for the gaia_auth_host directory
    that properly supports JS modules. Modularize the password change
    authenticator and move its test to the new structure. Also add
    closure compilation targets for it.
    
    Bug: 1184731, 1350059, b/245282934
    Change-Id: I8a433ea3448277c19e2882c7ea20f332b33a3911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874811
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044552}

---
+++ chrome/test/data/webui/gaia_auth_host/gaia_auth_host_browsertest.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Test fixture for c/b/r/gaia_auth_host.
+ */
+
+GEN('#include "content/public/test/browser_test.h"');
+
+var GaiaAuthHostBrowserTest = class extends testing.Test {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://dummyurl';
+  }
+
+  /** @override */
+  get isAsync() {
+    return true;
+  }
+
+  /** @override */
+  get webuiHost() {
+    return 'dummyurl';
+  }
+};
+
+[['PasswordChangeAuthenticator', 'password_change_authenticator_test.js'],
+].forEach(test => registerTest(...test));
+
+// Common WebUI pattern for test registration.
+function registerTest(testName, module, caseName) {
+  const className = `GaiaAuthHost${testName}Test`;
+  this[className] = class extends GaiaAuthHostBrowserTest {
+    /** @override */
+    get browsePreload() {
+      return `chrome://test/test_loader.html?module=gaia_auth_host/${module}`;
+    }
+  };
+
+  TEST_F(className, caseName || 'All', () => mocha.run());
+}
