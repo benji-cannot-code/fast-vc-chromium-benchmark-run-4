cd5fc8a78614cff379a5fae9d6a54ef16a346f74
Theodor-Stefan Cristea <theocristea@google.com>
2022-09-08 14:35:24 +0000

    Create a driver if one does not exist in GetRetaibanleDriver.
    In some cases, after submitting a password form and then going back to the previous change, GetRetainableDriver was called for a frame before the driver for that frame had the chance to be created.
    
    Now, if a driver does not exist for the repsective frame, GetRetainableDriver creates the driver.
    
    Bug: 1344776
    Change-Id: Idcd14356531fe46483e6dd74f158965119d5705d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879850
    Commit-Queue: Theodor-Stefan Cristea <theocristea@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044537}

--- components/password_manager/ios/ios_password_manager_driver_factory.mm
+++ components/password_manager/ios/ios_password_manager_driver_factory.mm
@@ -52,8 +52,13 @@ IOSPasswordManagerDriverFactory::~IOSPasswordManagerDriverFactory() = default;
 
 // static
 scoped_refptr<IOSPasswordManagerDriver>
-IOSPasswordManagerDriverFactory::GetRetainableDriver(web::WebFrame* frame) {
-  return IOSPasswordManagerWebFrameDriverHelper::FromWebFrame(frame)
+IOSPasswordManagerDriverFactory::GetRetainableDriver(web::WebState* web_state,
+                                                     web::WebFrame* web_frame) {
+  if (!IOSPasswordManagerWebFrameDriverHelper::FromWebFrame(web_frame)) {
+    IOSPasswordManagerDriverFactory::FromWebStateAndWebFrame(web_state,
+                                                             web_frame);
+  }
+  return IOSPasswordManagerWebFrameDriverHelper::FromWebFrame(web_frame)
       ->RetainableDriver();
 }
 
