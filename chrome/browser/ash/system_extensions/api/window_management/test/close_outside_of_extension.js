638c0a2cc71c4ff2ae6e33350eb495a16ebe6941
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-09-08 06:49:34 +0000

    system-extensions: Migrate CloseEvent to new test framework
    Migrates the CloseEvent test to close_outside_of_extension.js.
    
    Bug: b:242264794
    Change-Id: I3650d9d4d5fca2534c0c050d4f17c4c11c4298ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877068
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044407}

---
+++ chrome/browser/ash/system_extensions/api/window_management/test/close_outside_of_extension.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+importScripts('test_support.js', 'cros_window_test_utils.js');
+
+promise_test(async (t) => {
+  const {windowId} = await testHelper.openBrowserWindow();
+
+  const eventWatcher =
+      new EventWatcher(t, chromeos.windowManagement, ['windowclosed']);
+
+  await testHelper.closeBrowserWindow(windowId);
+
+  const event = await eventWatcher.wait_for(['windowclosed']);
+  assert_equals(event.window.id, windowId);
+
+  const windows = await chromeos.windowManagement.getWindows();
+  assert_equals(windows.length, 1);
+});
+
+done();
