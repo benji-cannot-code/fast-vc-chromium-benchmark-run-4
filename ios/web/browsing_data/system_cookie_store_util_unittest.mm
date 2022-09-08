250d03ff454ca6bea13f095b835740e38a9ce9da
Gauthier Ambard <gambard@chromium.org>
2022-09-08 19:17:16 +0000

    [iOS] Change pipes to backticks in /ios/web/browsing_data
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=ajuma@chromium.org
    
    Bug: 1311955
    Change-Id: Ib13cb6c9d543e97b77cbfe13d077ef520cbb33bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883676
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044707}

--- ios/web/browsing_data/system_cookie_store_util_unittest.mm
+++ ios/web/browsing_data/system_cookie_store_util_unittest.mm
@@ -29,7 +29,7 @@ namespace web {
 
 namespace {
 
-// Checks if |system_cookie| was set in WKHTTPCookieStore |cookie_store|.
+// Checks if `system_cookie` was set in WKHTTPCookieStore `cookie_store`.
 bool IsCookieSetInWKCookieStore(NSHTTPCookie* system_cookie,
                                 NSURL* url,
                                 WKHTTPCookieStore* cookie_store) {
@@ -54,7 +54,7 @@ bool IsCookieSetInWKCookieStore(NSHTTPCookie* system_cookie,
   return is_set;
 }
 
-// Sets |cookie| in SystemCookieStore |store| , and wait until set callback
+// Sets `cookie` in SystemCookieStore `store` , and wait until set callback
 // is finished.
 bool SetCookieInCookieStore(NSHTTPCookie* cookie,
                             net::SystemCookieStore* store) {
