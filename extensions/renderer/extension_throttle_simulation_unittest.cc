ef71e90b9468bb4e7bd4056ee4e7ef6919576fff
Hans Wennborg <hans@chromium.org>
2022-09-08 12:31:27 +0000

    Add missing include to unblock libc++ roll
    Bug: none
    Change-Id: I7a8c94b7fd952e777e684d130200c8ee4f4f7076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3882295
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044499}

--- extensions/renderer/extension_throttle_simulation_unittest.cc
+++ extensions/renderer/extension_throttle_simulation_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    anti-DDoS throttling for various different actual downtimes is what
 //    we expect it to be.
 
+#include <stdarg.h>
 #include <stddef.h>
 
 #include <cmath>
