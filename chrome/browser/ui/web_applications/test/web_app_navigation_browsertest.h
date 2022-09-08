b5c9c287fffb343efcd129451d206af16243aa32
Nidhi Jaju <nidhijaju@chromium.org>
2022-09-08 03:50:13 +0000

    Revert "WebAppNavigationBrowserTest: Uninstall apps during tear down"
    This reverts commit daf896f2798ab938a55a22619de7091770fec280.
    
    Reason for revert: Causes build failure i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Builder/41759/overview
    
    Original change's description:
    > WebAppNavigationBrowserTest: Uninstall apps during tear down
    >
    > We uninstall any web apps that were installed by the test.
    >
    > Bug: 1288199, 1358200
    > Change-Id: I0f8221dd508791dc171d75f7dc5e6b5cec323f97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875065
    > Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044349}
    
    Bug: 1288199, 1358200
    Change-Id: Id2bad6ddb44c738e3fd7cc62907f409e28de9f35
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881590
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044354}

--- chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h
+++ chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback_forward.h"
-#include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -91,7 +90,6 @@ class WebAppNavigationBrowserTest : public InProcessBrowserTest {
   void TearDownInProcessBrowserTestFixture() override;
   void SetUpCommandLine(base::CommandLine* command_line) override;
   void SetUpOnMainThread() override;
-  void TearDownOnMainThread() override;
 
   Profile* profile();
 
@@ -126,7 +124,6 @@ class WebAppNavigationBrowserTest : public InProcessBrowserTest {
   // Similar to net::MockCertVerifier, but also updates the CertVerifier
   // used by the NetworkService.
   content::ContentMockCertVerifier cert_verifier_;
-  raw_ptr<Profile> profile_ = nullptr;
   AppId test_web_app_;
   base::HistogramTester histogram_tester_;
 };
