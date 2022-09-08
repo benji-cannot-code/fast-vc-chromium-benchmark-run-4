714242deb1baa31ee9e6685ce728c46332166059
Viktor Semeniuk <vsemeniuk@google.com>
2022-09-08 18:41:49 +0000

    [Password Manager] Updating search bar to look alike Settings page
    This change update search bar position, as well as layout of the main
    page to make it very similar to the Settings page.
    
    Impl: https://screenshot.googleplex.com/96NmWWGqSXmv3Yp
    Mock: https://screenshot.googleplex.com/3aG7TW3a7wCUBKF
    
    Bug: 1350947
    Change-Id: Ie90d1c8a9a54627f1bde5828d5771a357c656292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3882335
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044683}

--- chrome/browser/resources/password_manager/password_manager_app.ts
+++ chrome/browser/resources/password_manager/password_manager_app.ts
@@ -2,7 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_page_host_style.css.js';
+import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
+import './shared_style.css.js';
 import './side_bar.js';
 import './toolbar.js';
 
