1be1233d8e27a7879d99dfb2fae931d8f390982e
Mason Freed <masonf@chromium.org>
2022-09-08 18:41:28 +0000

    Properly process content in kAfterBodyMode
    The previous code changed mode back to kInBodyMode when any
    content was found after the </body> end tag. However, the spec [1]
    says that whitespace should be processed as InBody, but the mode
    should not actually be changed. With this CL, that is the behavior.
    
    WebKit and Gecko appear to already pass the new test I added
    to webkit01.dat.
    
    [1] https://html.spec.whatwg.org/multipage/parsing.html#parsing-main-afterbody
    
    Fixed: 115338
    Change-Id: I979bdabf407a1a3f47c38e0ddde76df3fe72247f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872431
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044682}

--- third_party/blink/renderer/core/html/parser/html_tree_builder.cc
+++ third_party/blink/renderer/core/html/parser/html_tree_builder.cc
@@ -2520,6 +2520,15 @@ ReprocessBuffer:
     case kAfterBodyMode:
     case kAfterAfterBodyMode: {
       // FIXME: parse error
+      StringView leading_whitespace = buffer.TakeLeadingWhitespace();
+      if (!leading_whitespace.IsEmpty()) {
+        InsertionMode mode = GetInsertionMode();
+        SetInsertionMode(kInBodyMode);
+        tree_.InsertTextNode(leading_whitespace, kAllWhitespace);
+        SetInsertionMode(mode);
+      }
+      if (buffer.IsEmpty())
+        return;
       SetInsertionMode(kInBodyMode);
       goto ReprocessBuffer;
     }
