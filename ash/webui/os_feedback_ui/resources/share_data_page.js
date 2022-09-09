ce41684a0eb0b1ce961cdde665984d520be6bb22
Jack Hsieh <chengweih@chromium.org>
2022-09-09 23:47:27 +0000

    Revert "Feedback: Update Preview Screenshot Dialog to new spec"
    This reverts commit d926f05e4e69b174e04a4e37f959cd93031ff7ae.
    
    Reason for revert: Suspecting this causes the regression on https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-rel/67653/overview
    
    Original change's description:
    > Feedback: Update Preview Screenshot Dialog to new spec
    >
    > Adopted new preview screenshot dialog layout instead of using cr-dialog element.
    >
    > Spec doc: https://www.figma.com/proto/1B20ftJpezdcaIL2wOi6KJ/Square---K?page-id=30%3A4066&node-id=1210%3A31291&viewport=-2439%2C185%2C0.29&scaling=min-zoom&starting-point-node-id=1210%3A31291
    >
    > Before: https://screenshot.googleplex.com/9DMMNNp7FLGZqdh
    > After:
    > - light mode: https://screenshot.googleplex.com/dVHn5c3vBbDSiwC
    > - dark mode: https://screenshot.googleplex.com/9Gaz9VbhbJsEsYs
    >
    > Bug: b:185624798
    > Change-Id: Ie3eb7c0c3ed47e8b2180287999d9d0a6fac61560
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880984
    > Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    > Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1045302}
    
    Bug: b:185624798
    Change-Id: I70762bb4e783b2f2e92d0c7d8a3bc9e3bd11fa3a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888046
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045424}

--- ash/webui/os_feedback_ui/resources/share_data_page.js
+++ ash/webui/os_feedback_ui/resources/share_data_page.js
@@ -131,6 +131,7 @@ export class ShareDataPageElement extends ShareDataPageElementBase {
   /** @protected */
   handleScreenshotClick_() {
     this.$.screenshotDialog.showModal();
+    this.$.closeDialogButton.focus();
     this.feedbackServiceProvider_.recordPreSubmitAction(
         FeedbackAppPreSubmitAction.kViewedScreenshot);
   }
