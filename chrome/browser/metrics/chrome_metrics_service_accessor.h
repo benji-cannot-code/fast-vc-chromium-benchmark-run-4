dcd2a04e4bff66d90ce4c8c5a3130933f171dcb0
Alex Chen <alexwchen@chromium.org>
2022-09-09 17:25:39 +0000

    Remove FileManagerPrivateIsUMAEnabledFunction as friend class
    The FileManagerPrivateISUMAEnabledFunction was deprecated already (crrev.com/c/1527946). Removing it as an unused reference.
    
    Bug: 1361021
    Change-Id: I5e5a7700526e367ef6f336636696a73caf21c4ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884221
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045196}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -46,7 +46,6 @@ class DomainReliabilityServiceFactory;
 namespace extensions {
 class ChromeGuestViewManagerDelegate;
 class ChromeMetricsPrivateDelegate;
-class FileManagerPrivateIsUMAEnabledFunction;
 }
 
 namespace first_run {
@@ -131,7 +130,6 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class domain_reliability::DomainReliabilityServiceFactory;
   friend class extensions::ChromeGuestViewManagerDelegate;
   friend class extensions::ChromeMetricsPrivateDelegate;
-  friend class extensions::FileManagerPrivateIsUMAEnabledFunction;
   friend void ChangeMetricsReportingStateWithReply(
       bool,
       OnMetricsReportingCallbackType,
