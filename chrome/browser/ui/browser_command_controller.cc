1090de4a06d372b26794a4996c846beffe8e0222
Louise Brett <loubrett@google.com>
2022-09-09 15:57:25 +0000

    Disable 'Open in Chrome' menu item for the pinned home tab of web apps
    This option causes the tab to close, which we don't want to happen for
    the pinned home tab.
    
    Bug: 897314
    Change-Id: I43b86ead14e37801f44c61de8e24f0333f858d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874298
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045138}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/web_app_dialog_utils.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
+#include "chrome/browser/ui/web_applications/web_app_tabbed_utils.h"
 #include "chrome/browser/ui/webui/inspect_ui.h"
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/common/chrome_features.h"
@@ -1350,8 +1351,13 @@ void BrowserCommandController::UpdateCommandsForTabState() {
   bool is_isolated_app = current_web_contents->GetPrimaryMainFrame()
                              ->GetWebExposedIsolationLevel() >=
                          WebExposedIsolationLevel::kMaybeIsolatedApplication;
+  bool is_pinned_home_tab =
+      web_app::AppBrowserController::IsWebApp(browser_) &&
+      web_app::IsPinnedHomeTab(browser_->tab_strip_model(),
+                               browser_->tab_strip_model()->active_index());
   command_updater_.UpdateCommandEnabled(
-      IDC_OPEN_IN_CHROME, IsWebAppOrCustomTab() && !is_isolated_app);
+      IDC_OPEN_IN_CHROME,
+      IsWebAppOrCustomTab() && !is_isolated_app && !is_pinned_home_tab);
 
   command_updater_.UpdateCommandEnabled(
       IDC_TOGGLE_REQUEST_TABLET_SITE,
