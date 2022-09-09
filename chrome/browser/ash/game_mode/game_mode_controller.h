8cfe6fbb2b122a9eef9e05b9be591a1156c568d8
Matthew DeVore <matvore@chromium.org>
2022-09-09 09:54:41 +0000

    GameModeController: run clang-format
    Fix style errors that have already been merged.
    
    Test: compile
    Change-Id: I92ec7f2ae950bd6e85b70fb6041955efc4ad3e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881530
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Auto-Submit: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045011}

--- chrome/browser/ash/game_mode/game_mode_controller.h
+++ chrome/browser/ash/game_mode/game_mode_controller.h
@@ -63,8 +63,7 @@ class GameModeController : public aura::client::FocusChangeObserver {
  public:
   GameModeController();
   GameModeController(const GameModeController&) = delete;
-  GameModeController& operator=(const GameModeController&) =
-      delete;
+  GameModeController& operator=(const GameModeController&) = delete;
   ~GameModeController() override;
 
   // Overridden from FocusChangeObserver
@@ -90,8 +89,8 @@ class GameModeController : public aura::client::FocusChangeObserver {
     GameMode mode() const override;
 
    private:
-    static void OnSetGameMode(
-        absl::optional<GameMode> refresh_of, absl::optional<GameMode> previous);
+    static void OnSetGameMode(absl::optional<GameMode> refresh_of,
+                              absl::optional<GameMode> previous);
     void RefreshGameMode();
 
     // Used to determine if it's the first instance of game mode failing.
