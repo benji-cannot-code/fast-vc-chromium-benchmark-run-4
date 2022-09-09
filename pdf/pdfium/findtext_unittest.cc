12e34baa11b0899fd9efd82ca8d068c3af3152c8
Tommy Nyquist <nyquist@chromium.org>
2022-09-09 23:16:08 +0000

    Revert "Fix PDF viewer jumping immediately after the user presses 'ctrl+f'"
    This reverts commit 143885f9e50d7cf320343d433880a59c91ac5ce2.
    
    Reason for revert: Speculative revert for flaky test.
    See https://crbug.com/1362116 for details.
    
    Original change's description:
    > Fix PDF viewer jumping immediately after the user presses 'ctrl+f'
    >
    > This behavior is disruptive and inconsistent with the search behavior
    > of non-PDF sites. After this commit, pressing 'ctrl+f' will not start
    > a search until the user presses additional keys (add characters /
    > press enter).
    >
    > This bug is a side effect of fixing another bug "Chrome PDF Search is
    > not executed in background" (crbug.com/764635). The fix reverts the
    > changes done by the previous bug fix (pdfium_engine.cc) and provides an
    > alternative solution (find_request_manager.cc) which requires to update
    > some tests (pdf_find_request_manager_browsertest.cc,
    > find_request_manager_browsertest.cc, findtext_unittest.cc).
    >
    > Lastly this is my first bug so I added my name to AUTHORS.
    >
    > Bug: 1290824
    > Change-Id: I8e152087fb2ac019124e477b4d04d73d7b8f2f21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455153
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Joey Arhar <jarhar@chromium.org>
    > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1043764}
    
    Bug: 1290824, 1362116
    Change-Id: I43366129df4a9e44b5413852a029c3a121f8590e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888066
    Owners-Override: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1045406}

--- pdf/pdfium/findtext_unittest.cc
+++ pdf/pdfium/findtext_unittest.cc
@@ -74,6 +74,8 @@ void ExpectInitialSearchResults(FindTextTestClient& client, int count) {
 
   EXPECT_CALL(client,
               NotifyNumberOfFindResultsChanged(1, /*final_result=*/false));
+  EXPECT_CALL(client,
+              NotifySelectedFindResultChanged(0, /*final_result=*/false));
   for (int i = 2; i < count + 1; ++i) {
     EXPECT_CALL(client,
                 NotifyNumberOfFindResultsChanged(i, /*final_result=*/false));
@@ -183,7 +185,6 @@ TEST_F(FindTextTest, SelectFindResult) {
 
   ExpectInitialSearchResults(client, 4);
   engine->StartFind("world", /*case_sensitive=*/true);
-  ASSERT_TRUE(engine->SelectFindResult(/*forward=*/true));
 
   EXPECT_CALL(client, NotifyNumberOfFindResultsChanged(_, _)).Times(0);
   EXPECT_CALL(client,
@@ -213,9 +214,9 @@ TEST_F(FindTextTest, SelectFindResultAndSwitchToTwoUpView) {
     InSequence sequence;
 
     EXPECT_CALL(client,
-                NotifySelectedFindResultChanged(0, /*final_result=*/true));
-    EXPECT_CALL(client,
                 NotifySelectedFindResultChanged(1, /*final_result=*/true));
+    EXPECT_CALL(client,
+                NotifySelectedFindResultChanged(2, /*final_result=*/true));
   }
   ASSERT_TRUE(engine->SelectFindResult(/*forward=*/true));
   ASSERT_TRUE(engine->SelectFindResult(/*forward=*/true));
@@ -236,7 +237,7 @@ TEST_F(FindTextTest, SelectFindResultAndSwitchToTwoUpView) {
     InSequence sequence;
 
     EXPECT_CALL(client,
-                NotifySelectedFindResultChanged(1, /*final_result=*/true));
+                NotifySelectedFindResultChanged(2, /*final_result=*/true));
   }
   ASSERT_TRUE(engine->SelectFindResult(/*forward=*/true));
 }
