a32d57c0e6da2526e4a3978484985ec3caadfa40
dpapad <dpapad@chromium.org>
2022-09-09 07:09:00 +0000

    WebUI: Update references to old cr_elements URLs in ash/webui/
    The old URLs had been kept as aliases to make it possible to update the
    codebase to the new URLs incrementally.
    
    Bug: 1184053
    Change-Id: I6096bd278558080c218ba736bb71a1fca22cf6a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881153
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044952}

--- ash/webui/common/resources/cr_picture/cr_picture_list.js
+++ ash/webui/common/resources/cr_picture/cr_picture_list.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * profile image.
  */
 
-import '//resources/cr_elements/icons.m.js';
+import '//resources/cr_elements/icons.html.js';
 import '//resources/cr_elements/cr_shared_style.css.js';
 import '//resources/polymer/v3_0/iron-a11y-keys/iron-a11y-keys.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
--- ash/webui/personalization_app/resources/js/ambient/animation_theme_item_element.ts
+++ ash/webui/personalization_app/resources/js/ambient/animation_theme_item_element.ts
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../../css/common.css.js';
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
-import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 
 import {assertNotReached} from 'chrome://resources/js/assert_ts.js';
 
