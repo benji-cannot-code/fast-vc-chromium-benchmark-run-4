3e0e972e12b5ba38249fabc01e6bcd7ea9b40fce
David Munro <davidmunro@google.com>
2022-09-09 10:10:12 +0000

    guest_os: Can share GUEST_OS volumes over 9p
    GUEST_OS volumes are now supported by Seneschal so allow sharing them.
    Currently they can only be shared into the default Crostini container,
    but supporting more target VMs is to come.
    
    Bug: b:240491232
    Test: CQ, deploy, share, check files in penguin and mount in settings
    
    Change-Id: I10c7cb44346de9e34ded58011e036525409fe565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874307
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045017}

--- chrome/browser/ash/guest_os/guest_os_share_path.cc
+++ chrome/browser/ash/guest_os/guest_os_share_path.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/arc_features.h"
 #include "ash/components/arc/arc_util.h"
-#include "base/atomic_ref_count.h"
 #include "base/bind.h"
 #include "base/containers/contains.h"
 #include "base/files/file_util.h"
@@ -245,6 +244,8 @@ void GuestOsSharePath::CallSeneschalSharePath(const std::string& vm_name,
   ash::smb_client::SmbFsShare* smb_share = nullptr;
   base::FilePath smbfs_mount_point_path;
   base::FilePath smbfs_mount_name;
+  auto* vmgr = file_manager::VolumeManager::Get(profile_);
+  base::WeakPtr<file_manager::Volume> volume;
 
   // Allow MyFiles directory and subdirs.
   bool allowed_path = false;
@@ -354,6 +355,17 @@ void GuestOsSharePath::CallSeneschalSharePath(const std::string& vm_name,
     allowed_path = true;
     request.set_storage_location(vm_tools::seneschal::SharePathRequest::SMBFS);
     request.set_smbfs_mount_name(smbfs_mount_name.value());
+  } else if (vmgr && (volume = vmgr->FindVolumeFromPath(path)) &&
+             volume->type() == file_manager::VOLUME_TYPE_GUEST_OS &&
+             AppendRelativePath(volume->mount_path(), path, &relative_path)) {
+    allowed_path = true;
+    // Allow GuestOs files and subdirs.
+    base::FilePath mount_name;
+    fuse_fs_root_path.AppendRelativePath(volume->mount_path(), &mount_name);
+    request.set_storage_location(
+        vm_tools::seneschal::SharePathRequest::GUEST_OS_FILES);
+    request.set_owner_id(crostini::CryptohomeIdForProfile(profile_));
+    request.set_guest_os_mount_name(mount_name.value());
   }
 
   if (!allowed_path) {
