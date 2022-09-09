3c99cf2d318d690c81e909b8a5aeb3c1ecef8d37
Artsiom Mitrokhin <amitrokhin@google.com>
2022-09-09 14:22:09 +0000

    glanceables: Prevent accessibility crash if there is no event title
    [accessibility_paint_checks.cc(90)]
    Check failed: node_data.GetNameFrom() ==
    ax::mojom::NameFrom::kAttributeExplicitlyEmpty (kAttribute vs.
    kAttributeExplicitlyEmpty)View is focusable but has no accessible name
    or placeholder, and is not explicitly marked as empty.
    
    Bug: 1361335
    Test: added to ash_unittests
    Change-Id: I68ae7a9f3ce9384ca3aa5a1953051009b20029a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884231
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045095}

--- ash/glanceables/glanceables_unittests.cc
+++ ash/glanceables/glanceables_unittests.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "ash/ambient/ambient_controller.h"
@@ -334,6 +335,14 @@ TEST_F(GlanceablesTest, UpNextViewOpensCalendarEvent) {
   GetEventItemViews()[1]->AcceleratorPressed(
       ui::Accelerator(ui::KeyboardCode::VKEY_SPACE, 0));
   EXPECT_EQ(GetSystemTrayClient()->show_calendar_event_count(), 1);
+}
+
+TEST_F(GlanceablesTest, UpNextEventItemViewRendersCorrectlyWithoutEventTitle) {
+  google_apis::calendar::CalendarEvent event;
+  GlanceablesUpNextEventItemView view(event);
+
+  EXPECT_EQ(view.GetAccessibleName(), u"(No title)");
+  EXPECT_EQ(view.event_title_label_for_test()->GetText(), u"(No title)");
 }
 
 TEST_F(GlanceablesTest, RestoreViewRendersScreenshot) {
