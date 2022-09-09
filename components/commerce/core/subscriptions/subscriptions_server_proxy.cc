9554f5dfd347330a24e4225d270b86844da78502
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-09-09 22:55:49 +0000

    [PriceTracking] Pass the endpoint_fetcher to the response handler
    We need to pass the endpoint_fetcher pointer to the response handler to
    ensure the fetcher's lifetime extends to the callback and is not
    destroyed prematurely.
    
    Bug: 1361650
    Change-Id: I392f9b1395899d489e203b791f374fd1b68af768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888260
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045392}

--- components/commerce/core/subscriptions/subscriptions_server_proxy.cc
+++ components/commerce/core/subscriptions/subscriptions_server_proxy.cc
@@ -135,7 +135,7 @@ void SubscriptionsServerProxy::Create(
   auto* const fetcher_ptr = fetcher.get();
   fetcher_ptr->Fetch(base::BindOnce(
       &SubscriptionsServerProxy::HandleManageSubscriptionsResponses,
-      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+      weak_ptr_factory_.GetWeakPtr(), std::move(callback), std::move(fetcher)));
 }
 
 void SubscriptionsServerProxy::Delete(
@@ -200,7 +200,7 @@ void SubscriptionsServerProxy::Delete(
   auto* const fetcher_ptr = fetcher.get();
   fetcher_ptr->Fetch(base::BindOnce(
       &SubscriptionsServerProxy::HandleManageSubscriptionsResponses,
-      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+      weak_ptr_factory_.GetWeakPtr(), std::move(callback), std::move(fetcher)));
 }
 
 void SubscriptionsServerProxy::Get(SubscriptionType type,
@@ -252,9 +252,9 @@ void SubscriptionsServerProxy::Get(SubscriptionType type,
   auto fetcher = CreateEndpointFetcher(GURL(service_url), kGetHttpMethod,
                                        kEmptyPostData, traffic_annotation);
   auto* const fetcher_ptr = fetcher.get();
-  fetcher_ptr->Fetch(
-      base::BindOnce(&SubscriptionsServerProxy::HandleGetSubscriptionsResponses,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  fetcher_ptr->Fetch(base::BindOnce(
+      &SubscriptionsServerProxy::HandleGetSubscriptionsResponses,
+      weak_ptr_factory_.GetWeakPtr(), std::move(callback), std::move(fetcher)));
 }
 
 std::unique_ptr<EndpointFetcher>
@@ -271,6 +271,7 @@ SubscriptionsServerProxy::CreateEndpointFetcher(
 
 void SubscriptionsServerProxy::HandleManageSubscriptionsResponses(
     ManageSubscriptionsFetcherCallback callback,
+    std::unique_ptr<EndpointFetcher> endpoint_fetcher,
     std::unique_ptr<EndpointResponse> responses) {
   data_decoder::DataDecoder::ParseJsonIsolated(
       responses->response,
@@ -297,6 +298,7 @@ void SubscriptionsServerProxy::OnManageSubscriptionsJsonParsed(
 
 void SubscriptionsServerProxy::HandleGetSubscriptionsResponses(
     GetSubscriptionsFetcherCallback callback,
+    std::unique_ptr<EndpointFetcher> endpoint_fetcher,
     std::unique_ptr<EndpointResponse> responses) {
   data_decoder::DataDecoder::ParseJsonIsolated(
       responses->response,
