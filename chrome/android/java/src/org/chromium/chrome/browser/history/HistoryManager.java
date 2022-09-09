6e7937268c440e29499489086d0f825ec712f99b
Patrick Noland <pnoland@google.com>
2022-09-09 18:01:43 +0000

    [Journeys] Refresh UI when history is deleted externally
    Bug: 1346691
    Change-Id: Iad85d68b5c20167c63bff014a32be71ede4b51ce
    Binary-Size: diff is spurious https://crbug.com/1361952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878874
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045221}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
@@ -449,6 +449,8 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
                         R.string.multiple_history_items_deleted, numItemsRemoved));
             }
 
+            notifyHistoryClustersCoordinatorOfDeletion();
+
             return true;
         } else if (item.getItemId() == R.id.search_menu_id) {
             mContentManager.removeHeader();
@@ -638,6 +640,11 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
         }
     }
 
+    private void notifyHistoryClustersCoordinatorOfDeletion() {
+        if (mHistoryClustersCoordinator == null) return;
+        mHistoryClustersCoordinator.onHistoryDeletedExternally();
+    }
+
     /**
      * @param action The user action string to record.
      */
@@ -745,6 +752,8 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
         if (mSelectionDelegate.isItemSelected(item)) {
             mSelectionDelegate.toggleSelectionForItem(item);
         }
+
+        notifyHistoryClustersCoordinatorOfDeletion();
     }
 
     // HistoryContentManager.Observer
@@ -770,6 +779,12 @@ public class HistoryManager implements OnMenuItemClickListener, SelectionObserve
     public void onUserAccountStateChanged() {
         mToolbar.onSignInStateChange();
         mShouldShowClearBrowsingDataSupplier.set(mContentManager.getShouldShowClearData());
+    }
+
+    // HistoryContentManager.Observer
+    @Override
+    public void onHistoryDeletedExternally() {
+        notifyHistoryClustersCoordinatorOfDeletion();
     }
 
     @VisibleForTesting
