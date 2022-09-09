3a7cb86bd1819908dfa908464df7ccc495b97d3c
Mark Schillaci <mschillaci@google.com>
2022-09-09 16:24:16 +0000

    Add feature flag for async Android accessibility experiments
    This CL adds a new flag that will be used in experimental work to
    proactively generate Java-side accessibility trees in a "push" model,
    rather than waiting for the framework to request them.
    
    AX-Relnotes: N/A
    Bug: 1361985
    Change-Id: I02d93b04d16281864f8a0856f7b2bc4fc90ff05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884235
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045151}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -316,6 +316,7 @@ CONTENT_EXPORT extern const base::Feature kWebXr;
 CONTENT_EXPORT extern const base::Feature kWebXrArModule;
 
 #if BUILDFLAG(IS_ANDROID)
+CONTENT_EXPORT extern const base::Feature kAccessibilityAsyncTreeConstruction;
 CONTENT_EXPORT extern const base::Feature kAccessibilityPageZoom;
 CONTENT_EXPORT extern const base::Feature
     kBackgroundMediaRendererHasModerateBinding;
