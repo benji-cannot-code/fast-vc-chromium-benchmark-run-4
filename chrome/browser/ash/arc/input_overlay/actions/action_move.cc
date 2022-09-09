0aaf74548fb24ec0f4551fa72dff1df6f132e9dd
Peter Kasting <pkasting@chromium.org>
2022-09-09 19:40:00 +0000

    Convert base::apply to std::apply.
    Bug: none
    Change-Id: I98fcee00c8bad29ac8c3d049a4ef4c8af483ee51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887940
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045292}

--- chrome/browser/ash/arc/input_overlay/actions/action_move.cc
+++ chrome/browser/ash/arc/input_overlay/actions/action_move.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/input_overlay/actions/action_move.h"
 
+#include "base/cxx17_backports.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/arc/input_overlay/actions/action.h"
 #include "chrome/browser/ash/arc/input_overlay/touch_id_manager.h"
--- ui/views/metadata/view_factory_internal.h
+++ ui/views/metadata/view_factory_internal.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/cxx17_backports.h"
 #include "base/memory/raw_ptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/class_property.h"
@@ -140,7 +139,7 @@ class ClassMethodCaller : public PropertySetterBase {
   ~ClassMethodCaller() override = default;
 
   void SetProperty(View* obj) override {
-    base::apply(
+    std::apply(
         Set, std::tuple_cat(std::make_tuple(static_cast<TClass*>(obj)), args_));
   }
 
