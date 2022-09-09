f2cd6060ce18a7e06e1c872e98cfe55ffaf09871
Stephan Hartmann <stha09@googlemail.com>
2022-09-09 05:26:52 +0000

    libstdc++: fix incomplete type of content::AudioStreamBrokerFactory
    Destructor of std::unique_ptr in libstdc++ uses sizeof() which
    requires full definition of content::AudioStreamBrokerFactory for
    return type of
    content::WebContentsDelegate::CreateAudioStreamBrokerFactory().
    
    Bug: 957519
    Change-Id: Ic5ac2863546742e137c7d6524d96bd7a2e164917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883115
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1044934}

--- content/public/browser/web_contents_delegate.cc
+++ content/public/browser/web_contents_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/singleton.h"
 #include "build/build_config.h"
+#include "content/public/browser/audio_stream_broker.h"
 #include "content/public/browser/color_chooser.h"
 #include "content/public/browser/file_select_listener.h"
 #include "content/public/browser/keyboard_event_processing_result.h"
