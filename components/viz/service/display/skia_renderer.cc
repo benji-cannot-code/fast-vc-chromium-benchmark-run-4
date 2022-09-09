b084e1008a652d59523a27a681e88374a5db3989
Bo Liu <boliu@chromium.org>
2022-09-09 17:19:38 +0000

    Add some debugging to SkiaRenderer::BuffersPresented
    Bug: 1357789
    Change-Id: I82f020ea34df4fb3ae815f68cde351fd2e378c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3886906
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045191}

--- components/viz/service/display/skia_renderer.cc
+++ components/viz/service/display/skia_renderer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_macros.h"
@@ -1025,7 +1026,11 @@ void SkiaRenderer::SwapBuffersComplete(gfx::GpuFenceHandle release_fence) {
 }
 
 void SkiaRenderer::BuffersPresented() {
-  DCHECK(!read_lock_release_fence_overlay_locks_.empty());
+  if (read_lock_release_fence_overlay_locks_.empty()) {
+    // Debug crbug.com/1357789.
+    base::debug::DumpWithoutCrashing();
+    return;
+  }
   read_lock_release_fence_overlay_locks_.pop_front();
 }
 
