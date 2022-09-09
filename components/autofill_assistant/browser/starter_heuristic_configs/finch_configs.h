c3f5837249d5eb384a03db50139bef5dac9f638e
Clemens Arbesser <arbesser@google.com>
2022-09-09 12:20:31 +0000

    Move finch heuristic configs and share instances between tabs.
    This is a refactoring only.
    
    Change-Id: I57dadbc4797c92ce4631419089d205478a2f2368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879362
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045047}

---
+++ components/autofill_assistant/browser/starter_heuristic_configs/finch_configs.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_STARTER_HEURISTIC_CONFIGS_FINCH_CONFIGS_H_
+#define COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_STARTER_HEURISTIC_CONFIGS_FINCH_CONFIGS_H_
+
+namespace autofill_assistant {
+
+class LegacyStarterHeuristicConfig;
+class FinchStarterHeuristicConfig;
+
+namespace finch_configs {
+
+// Starter heuristic instances from finch, to be shared between tabs.
+const LegacyStarterHeuristicConfig* GetOrCreateLegacyConfig();
+const FinchStarterHeuristicConfig* GetOrCreateUrlHeuristic1();
+const FinchStarterHeuristicConfig* GetOrCreateUrlHeuristic2();
+const FinchStarterHeuristicConfig* GetOrCreateUrlHeuristic3();
+const FinchStarterHeuristicConfig* GetOrCreateUrlHeuristic4();
+const FinchStarterHeuristicConfig* GetOrCreateUrlHeuristic5();
+
+}  // namespace finch_configs
+}  // namespace autofill_assistant
+
+#endif  // COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_STARTER_HEURISTIC_CONFIGS_FINCH_CONFIGS_H_
