6292b2282f1a5ae1636910e89f4c1b1a9355c510
Yao Xiao <yaoxia@chromium.org>
2022-09-09 17:11:20 +0000

    [Topics] measure how long it takes to run browsingTopics()
    Bug: 1361548
    Change-Id: I2f9d3f45a313f5b5017e166542623eeac5f1390b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884227
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045181}

--- third_party/blink/renderer/modules/browsing_topics/browsing_topics_document_supplement.cc
+++ third_party/blink/renderer/modules/browsing_topics/browsing_topics_document_supplement.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/browsing_topics/browsing_topics_document_supplement.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/mojom/permissions_policy/document_policy_feature.mojom-blink.h"
@@ -134,6 +135,7 @@ ScriptPromise BrowsingTopicsDocumentSupplement::GetBrowsingTopics(
   document_host_->GetBrowsingTopics(WTF::Bind(
       [](ScriptPromiseResolver* resolver,
          BrowsingTopicsDocumentSupplement* supplement,
+         base::TimeTicks start_time,
          mojom::blink::GetBrowsingTopicsResultPtr result) {
         DCHECK(resolver);
         DCHECK(supplement);
@@ -161,9 +163,13 @@ ScriptPromise BrowsingTopicsDocumentSupplement::GetBrowsingTopics(
           result_array.push_back(result_topic);
         }
 
+        base::TimeDelta time_to_resolve = base::TimeTicks::Now() - start_time;
+        base::UmaHistogramTimes("BrowsingTopics.JavaScriptAPI.TimeToResolve",
+                                time_to_resolve);
+
         resolver->Resolve(result_array);
       },
-      WrapPersistent(resolver), WrapPersistent(this)));
+      WrapPersistent(resolver), WrapPersistent(this), base::TimeTicks::Now()));
 
   return promise;
 }
