5202b30bffe60bcb5db67b5a11b64b8f68fef80d
David Bienvenu <davidbienvenu@chromium.org>
2022-09-09 19:37:18 +0000

    win: Remove metric for file/dir delete retry count
    The stat showed that adding more retries would have diminishing
    returns. It also showed that the retries for the recursive delete
    are less useful, after the first retry, but since the retry code is
    shared between single file delete and recursive delete, and multiple retries for single file delete is useful, both cases will retry
    10 times.
    
    See https://bugs.chromium.org/p/chromium/issues/detail?id=1360138#c3
    for more info.
    
    Bug: 1360138
    Change-Id: I833b8eb309d24784e05eea5266d8768902877516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876844
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045290}

--- base/files/file_util_win.cc
+++ base/files/file_util_win.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/process/process_handle.h"
 #include "base/rand_util.h"
@@ -314,12 +313,6 @@ bool DeleteFileOrSetLastError(const FilePath& path, bool recursive) {
 
 constexpr int kMaxDeleteAttempts = 9;
 
-void LogFileDeleteRetryCount(bool recursive, int attempt) {
-  UmaHistogramExactLinear(recursive ? "Windows.PathRecursivelyDeleteRetryCount"
-                                    : "Windows.FileDeleteRetryCount",
-                          attempt, kMaxDeleteAttempts);
-}
-
 void DeleteFileWithRetry(const FilePath& path,
                          bool recursive,
                          int attempt,
@@ -329,9 +322,6 @@ void DeleteFileWithRetry(const FilePath& path,
   static constexpr TimeDelta kDeleteFileRetryDelay = Milliseconds(250);
 
   if (DeleteFileOrSetLastError(path, recursive)) {
-    // Log how many times we had to retry the RetryDeleteFile operation before
-    // it succeeded. This will be from 0 to kMaxDeleteAttempts - 1.
-    LogFileDeleteRetryCount(recursive, attempt);
     // Consider introducing further retries until the item has been removed from
     // the filesystem and its name is ready for reuse; see the comments in
     // chrome/installer/mini_installer/delete_with_retry.cc for details.
@@ -343,8 +333,6 @@ void DeleteFileWithRetry(const FilePath& path,
   ++attempt;
   DCHECK_LE(attempt, kMaxDeleteAttempts);
   if (attempt == kMaxDeleteAttempts) {
-    // Log kMaxDeleteAttempts to indicate failure after exhausting all attempts.
-    LogFileDeleteRetryCount(recursive, attempt);
     if (!reply_callback.is_null())
       std::move(reply_callback).Run(false);
     return;
