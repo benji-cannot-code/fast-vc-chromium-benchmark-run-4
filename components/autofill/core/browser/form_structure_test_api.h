775693e41ed011c7b92641ebeb6ba3b4551cca46
Christoph Schwering <schwering@google.com>
2022-09-09 19:49:47 +0000

    [Autofill] Update and extend test of FormStructure::ShouldBeParsed().
    The unit test for FormStructure::ShouldBeParsed() was outdated and
    referred to old versions of the constants.
    
    This CL splits the single ShouldBeParsed() into several tests and
    adds one for autocomplete types.
    
    Bug: 1153539, 1007974
    Change-Id: I42468a8c5e9e2ec55c7d0286350b5c71398db0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864663
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045300}

--- components/autofill/core/browser/form_structure_test_api.h
+++ components/autofill/core/browser/form_structure_test_api.h
@@ -18,6 +18,8 @@ namespace autofill {
 // Exposes some testing operations for FormStructure.
 class FormStructureTestApi {
  public:
+  using ShouldBeParsedParams = FormStructure::ShouldBeParsedParams;
+
   static void ParseApiQueryResponse(
       base::StringPiece payload,
       const std::vector<FormStructure*>& forms,
@@ -42,6 +44,11 @@ class FormStructureTestApi {
   explicit FormStructureTestApi(FormStructure* form_structure)
       : form_structure_(form_structure) {
     DCHECK(form_structure_);
+  }
+
+  [[nodiscard]] bool ShouldBeParsed(ShouldBeParsedParams params = {},
+                                    LogManager* log_manager = nullptr) {
+    return form_structure_->ShouldBeParsed(params, log_manager);
   }
 
   // Set the heuristic and server types for each field. The `heuristic_types`
