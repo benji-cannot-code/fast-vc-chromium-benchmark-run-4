8a1692dc7b29827d62827d4a67b9abccd73023ed
Clemens Arbesser <arbesser@google.com>
2022-09-09 12:13:17 +0000

    Introduce launched starter configs.
    These will be used to default-enable features while keeping the ability
    to disable them when needed.
    
    Change-Id: Ib45071ebfac5dbf522fd8be6b69630b79654c333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876831
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045044}

--- components/autofill_assistant/browser/starter_heuristic_configs/finch_starter_heuristic_config.cc
+++ components/autofill_assistant/browser/starter_heuristic_configs/finch_starter_heuristic_config.cc
@@ -14,6 +14,7 @@ FinchStarterHeuristicConfig::FinchStarterHeuristicConfig(
   InitFromTrialParams(trial_parameter);
 }
 
+FinchStarterHeuristicConfig::FinchStarterHeuristicConfig() = default;
 FinchStarterHeuristicConfig::~FinchStarterHeuristicConfig() = default;
 
 const std::string& FinchStarterHeuristicConfig::GetIntent() const {
@@ -95,6 +96,11 @@ void FinchStarterHeuristicConfig::InitFromTrialParams(
     VLOG(2) << "Field trial parameter not set";
     return;
   }
+  InitFromString(parameters);
+}
+
+void FinchStarterHeuristicConfig::InitFromString(
+    const std::string& parameters) {
   auto dict = base::JSONReader::ReadAndReturnValueWithError(parameters);
   if (!dict.has_value() || !dict->is_dict()) {
     VLOG(1) << "Failed to parse field trial params as JSON object: "
