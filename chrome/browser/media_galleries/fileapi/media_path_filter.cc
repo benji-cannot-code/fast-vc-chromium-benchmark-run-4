a16511246bfccc88b3270b45fe8484fa486a8cdf
Christian Flach <cmfcmf@chromium.org>
2022-09-09 17:14:36 +0000

    Use `SequenceChecker` macros: /chrome/browser/media_galleries/fileapi
    Using the macros instead of directly using `SequenceChecker` results in
    smaller binaries and decreased memory usage. Most code already uses the
    macros. This CL is part of a series of CLs that fixes remaining usages.
    
    This CL was uploaded by git cl split.
    
    R=mek@chromium.org
    
    Bug: none
    Change-Id: I71c3ed33f63c957864e785a1de120df2795281df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883682
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045187}

--- chrome/browser/media_galleries/fileapi/media_path_filter.cc
+++ chrome/browser/media_galleries/fileapi/media_path_filter.cc
@@ -138,7 +138,7 @@ bool MediaPathFilter::ShouldSkip(const base::FilePath& path) {
 
 MediaPathFilter::MediaPathFilter()
     : initialized_(false) {
-  sequence_checker_.DetachFromSequence();
+  DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
 MediaPathFilter::~MediaPathFilter() {
@@ -158,7 +158,7 @@ MediaGalleryFileType MediaPathFilter::GetType(const base::FilePath& path) {
 }
 
 void MediaPathFilter::EnsureInitialized() {
-  DCHECK(sequence_checker_.CalledOnValidSequence());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (initialized_)
     return;
 
