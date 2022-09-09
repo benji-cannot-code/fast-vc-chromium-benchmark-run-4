a33b1f728ef0d7474ec7ea4988196aa7bf0a2c3c
Ivan Murashov <ivan.murashov@lge.com>
2022-09-09 06:31:15 +0000

    IWYU: Add <string.h> for strlen and memset in dwarf_line_no.cc
    Bug: 957519
    Change-Id: Iddbc7070ebbf5cf8e5b9601b1846a8fc0d0e9b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880356
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1044945}

--- base/debug/dwarf_line_no.cc
+++ base/debug/dwarf_line_no.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <limits>
 
+#include <string.h>
 #include <unistd.h>
 
 #include "base/debug/buffered_dwarf_reader.h"
