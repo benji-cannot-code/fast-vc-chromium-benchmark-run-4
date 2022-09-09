732de9d69d28035955108a7b92ecdcb97900b853
Sylvain Defresne <sdefresne@chromium.org>
2022-09-09 12:28:53 +0000

    [apple] Fix thread-safety issue in GetDisplayNameForLocale()
    The function GetDisplayNameForLocale() is called from multiple
    threads concurrently but has unprotected access to a global
    value that is mutated in the function, which results in KABOOM.
    
    Remove the glocal cache to make the code thread-safe. The cache
    may be needed if NSLocale construction is expensive, but is hard
    to keep around as this code is called on the main thread and so
    cannot use a base::Lock to simply protect the cache.
    
    Once the thread-safety issue is resolved, it will be possible
    to investigate whether a cache is indeed required, and if so,
    it will be written with thread-safety in mind (probably using
    so TLS storage).
    
    Move the function (and its tests) to files that are only build
    on iOS.
    
    Fixed: 1349345
    Change-Id: Ib9ea4620280533bbff77aab234cc6940d46ccf7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879562
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045050}

--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -42,6 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_android.h"
 #endif
 
+#if BUILDFLAG(IS_IOS)
+#include "ui/base/l10n/l10n_util_ios.h"
+#endif
+
 #if defined(USE_GLIB)
 #include <glib.h>
 #endif
