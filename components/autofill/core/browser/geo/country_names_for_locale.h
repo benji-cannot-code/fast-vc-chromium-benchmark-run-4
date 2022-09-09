11dea0d28508351c8c23135e719fbdda66eaad22
Dominic Battre <battre@chromium.org>
2022-09-09 18:35:45 +0000

    New way of recognizing country names
    If you are an Austrian user who
    1) uses Chrome in en_US locale
    2) visits an Austrian shopping website whose language is not recognized,
    your address cannot be saved by Chrome.
    
    Chrome currently uses the following ways to recognize the country name
    ("Österreich"):
    
    1) Default locale (which is en_US)
    2) Locale of your OS (which would be en_US)
    3) Language of the website (which would be "und" for undetermined)
    
    We should explore the impact of using country names in the local
    languages.
    
    For example, my GLinux workstation ships 3 different locales for
    Italy:
    - ca-IT (Catalan)
    - de-IT (German)
    - it-IT (Italian)
    
    The term "Italy" in these the languages is
    - Itàlia
    - Italien
    - Italia
    respectively.
    
    This is not an exhaustive list (there is a population in Italy that
    speaks french) but it's much better than what we have, because none
    of these terms match Italy in en_US.
    
    This CL proposes that
    
    For each country c:
      We look at all locales l associated with c:
        Look a the translation t of c in local l and add a mapping of t
        to c to our country list.
    
    Bug: 1360502
    Change-Id: Ic663afcac3df6cfba6c41ad5d8794e761898a1da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876426
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045249}

--- components/autofill/core/browser/geo/country_names_for_locale.h
+++ components/autofill/core/browser/geo/country_names_for_locale.h
@@ -10,9 +10,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/containers/span.h"
+#include "third_party/icu/source/common/unicode/locid.h"
 #include "third_party/icu/source/i18n/unicode/coll.h"
 
 namespace autofill {
+
+// This is a pseudo locale for the "Country name" -> "Country code" mapping.
+// When using this locale, we map the name of each country in the languages
+// spoken in the country to the country code. This is not exhaustive but
+// based on registered locales. For example, for Italy, there may be ca-IT,
+// de-IT and it-IT, but no fr-IT even though Italy has a French community.
+// This is not perfect but a start.
+constexpr const char kPseudoLocaleOfNativeTranslations[] = "native";
+
+// TODO(crbug.com/1360502) Remove this after finishing the experiment. The
+// purpose of this locale is just to provide a value for a default/control
+// experiment group in which we don't have native translations of countries.
+constexpr const char kPseudoLocaleOfNativeTranslationsDisabled[] =
+    "native-disabled";
+
+// Returns the locales installed on this computer.
+base::span<const icu::Locale> GetAvailableLocales();
 
 class CountryNamesForLocale {
  public:
