b5322fb43bca035a3a081edd44bf98743ba67ea4
Viktor Semeniuk <vsemeniuk@google.com>
2022-09-09 21:22:28 +0000

    [Password Manager] Adding PasswordManagerProxy
    This CL adds PasswordManagerProxy and TestPasswordManagerProxy to the
    new Password Manager page.
    
    This change also allowlists chrome://password-manager/ for
    passwordsPrivate api usage.
    
    Bug: 1350947
    Change-Id: Ie4c41752d124906783869a52747eafee0a75c44c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876152
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045355}

---
+++ chrome/test/data/webui/password_manager/test_util.ts
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export function makePasswordCheckStatus(
+    state?: chrome.passwordsPrivate.PasswordCheckState, checked?: number,
+    remaining?: number,
+    lastCheck?: string): chrome.passwordsPrivate.PasswordCheckStatus {
+  return {
+    state: state || chrome.passwordsPrivate.PasswordCheckState.IDLE,
+    alreadyProcessed: checked,
+    remainingInQueue: remaining,
+    elapsedTimeSinceLastCheck: lastCheck,
+  };
+}
