546bda34b86aaa77b8771ea60d0df8b020dce380
Avi Drissman <avi@chromium.org>
2022-09-09 19:35:54 +0000

    Update copyright headers in chrome
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c34, except for the sweep for "All Rights
    Reserved" as that generates a lot of exceptions and will be a follow-
    up.
    
    No-Try: true
    No-Presubmit: true
    Bug: 1098010
    Change-Id: I5caf6b21db9c12bea71258223f59c1ac4313a738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888258
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045289}

--- chrome/browser/resources/chromeos/accessibility/switch_access/saatlite/compiler/compiler.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/saatlite/compiler/compiler.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const parse = require('./parser').parse;
 const fs = require('fs');
 
-const preamble = `// Copyright 2021 The Chromium Authors. All rights reserved.
+const preamble = `// Copyright 2021 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
