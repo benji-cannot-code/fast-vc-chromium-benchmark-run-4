1c9f3a0bde8f88d8d26272d14b21de0b8305f3a6
Tommy Nyquist <nyquist@chromium.org>
2022-09-09 15:48:04 +0000

    Revert "[WebAuthn] Conditional UI cancellation on Android"
    This reverts commit d7ee8f18c53b4be7a1bcc43e8ec026a0f58f2d9d.
    
    Reason for revert: Breaks multiple Android-bots
    
    Original change's description:
    > [WebAuthn] Conditional UI cancellation on Android
    >
    > AbortController doesn't work for WebAuthn requests on Android because
    > there is no way for the browser to cancel the Android UI once a request
    > has been initiated.
    >
    > However, a pending Conditional UI request can be cancelled before the
    > point when a user selects a credential.
    >
    > This implements that cancellation.
    >
    > Fixed: 1359130
    > Change-Id: I6ac1b0504e005f4a549a3a1cf24625620add7a9b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876623
    > Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044766}
    
    Bug: 1359130, 1361968
    Change-Id: Ic77f0884c80182419eab0561266a92166510615c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887158
    Auto-Submit: Tommy Nyquist <nyquist@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045130}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/AuthenticatorImpl.java
@@ -67,7 +67,6 @@ public final class AuthenticatorImpl implements Authenticator {
     // situation does not matter because all pending requests will return the same value.
     private Queue<org.chromium.mojo.bindings.Callbacks.Callback1<Boolean>>
             mIsUserVerifyingPlatformAuthenticatorAvailableCallbackQueue = new LinkedList<>();
-    private Fido2CredentialRequest mPendingFido2CredentialRequest;
 
     private static Fido2CredentialRequest sFido2CredentialRequestOverrideForTesting;
 
@@ -143,9 +142,7 @@ public final class AuthenticatorImpl implements Authenticator {
             return;
         }
 
-        mPendingFido2CredentialRequest = getFido2CredentialRequest();
-        mPendingFido2CredentialRequest.handleMakeCredentialRequest(options, mRenderFrameHost,
-                mOrigin,
+        getFido2CredentialRequest().handleMakeCredentialRequest(options, mRenderFrameHost, mOrigin,
                 (status, response)
                         -> onRegisterResponse(status, response),
                 status -> onError(status));
@@ -167,8 +164,7 @@ public final class AuthenticatorImpl implements Authenticator {
             return;
         }
 
-        mPendingFido2CredentialRequest = getFido2CredentialRequest();
-        mPendingFido2CredentialRequest.handleGetAssertionRequest(options, mRenderFrameHost, mOrigin,
+        getFido2CredentialRequest().handleGetAssertionRequest(options, mRenderFrameHost, mOrigin,
                 mPayment,
                 (status, response) -> onSignResponse(status, response), status -> onError(status));
     }
@@ -215,13 +211,8 @@ public final class AuthenticatorImpl implements Authenticator {
 
     @Override
     public void cancel() {
-        // This is not implemented for anything other than Conditional UI getAssertion requests,
-        // since there is no way to cancel a request that has already triggered gmscore UI.
-        if (!mIsOperationPending || mGetAssertionCallback == null) {
-            return;
-        }
-
-        mPendingFido2CredentialRequest.cancelConditionalGetAssertion(mRenderFrameHost);
+        // Not implemented, ignored because request sent to gmscore fido cannot be cancelled.
+        return;
     }
 
     /**
@@ -271,7 +262,6 @@ public final class AuthenticatorImpl implements Authenticator {
         mIsOperationPending = false;
         mMakeCredentialCallback = null;
         mGetAssertionCallback = null;
-        mPendingFido2CredentialRequest = null;
     }
 
     @Override
