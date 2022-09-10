241e01bdc3d3cfcbe4a57eb868edc1a29ab28140
Erica Lee <ericamlee@google.com>
2022-09-10 01:01:56 +0000

    [JellyBean] Debug shortcut for dynamic color
    Added a debug shortcut that toggles between two dynamic colors.
    
    TEST=Ran locally with both Jelly on and off. The debug shortcut does toggle colors throughout ChromeOS.
    BUG=b/245617317
    
    Change-Id: I237d7fdd910bb41d06df3e1894ab1ede04e66299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880890
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045458}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -1665,6 +1665,7 @@ bool AcceleratorControllerImpl::CanPerformAction(
     case DEBUG_PRINT_WINDOW_HIERARCHY:
     case DEBUG_SHOW_TOAST:
     case DEBUG_TOGGLE_DARK_MODE:
+    case DEBUG_TOGGLE_DYNAMIC_COLOR:
     case DEBUG_TOGGLE_GLANCEABLES:
     case DEBUG_TOGGLE_SHOW_DEBUG_BORDERS:
     case DEBUG_TOGGLE_SHOW_FPS_COUNTER:
@@ -1914,6 +1915,7 @@ void AcceleratorControllerImpl::PerformAction(
     case DEBUG_PRINT_WINDOW_HIERARCHY:
     case DEBUG_SHOW_TOAST:
     case DEBUG_TOGGLE_DARK_MODE:
+    case DEBUG_TOGGLE_DYNAMIC_COLOR:
     case DEBUG_TOGGLE_GLANCEABLES:
       debug::PerformDebugActionIfEnabled(action);
       break;
