5d5b4987972d8223d85a363ed64243e98706cf02
Daniel Cheng <dcheng@chromium.org>
2022-09-10 02:44:49 +0000

    Revert "[JellyBean] Dynamic Colors in Sample System App"
    This reverts commit 28807e53937731cb438f6284c930dd612bc89fcc.
    
    Reason for revert:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20Full is broken.
    
    Example broken build:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20Full/224634/steps
    
    Error:
    ../../ash/webui/sample_system_web_app_ui/resources/trusted/page_handler.ts:39:39 - error TS2792: Cannot find module 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js'. Did you mean to set the 'moduleResolution' option to 'node', or to add aliases to the 'paths' option?
    
    39 import {startColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
                                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    
    Found 1 error in ../../ash/webui/sample_system_web_app_ui/resources/trusted/page_handler.ts:39
    
    Original change's description:
    > [JellyBean] Dynamic Colors in Sample System App
    >
    > Added Dynamic Colors to the Sample System Web App to be used as an example for other web apps.
    >
    > BUG=b/244353669
    > TEST=locally saw the theme/colors instead of the cros_styles colors.
    >
    > Change-Id: I7087acdb09bd7c46198f11e9461e472b6e12a26f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877349
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    > Commit-Queue: Erica Lee <ericamlee@google.com>
    > Reviewed-by: Zain Afzal <zafzal@google.com>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1045303}
    
    Bug: b/244353669
    Change-Id: Ibbb9d3bc807b2865fb312f9da9789676b4338a38
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887564
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045477}

--- ash/webui/sample_system_web_app_ui/resources/trusted/page_handler.ts
+++ ash/webui/sample_system_web_app_ui/resources/trusted/page_handler.ts
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote} from '/ash/webui/sample_system_web_app_ui/mojom/sample_system_web_app_ui.mojom-webui.js';
-import {startColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 
 // Used to make calls on the remote PageHandler interface. Singleton that client
 // modules can use directly.
@@ -52,5 +51,3 @@ const factoryRemote = PageHandlerFactory.getRemote();
 factoryRemote.createPageHandler(
     pageHandler.$.bindNewPipeAndPassReceiver(),
     callbackRouter.$.bindNewPipeAndPassRemote());
-
-startColorChangeUpdater();
