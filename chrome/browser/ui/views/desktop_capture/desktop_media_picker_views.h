0066f8466fe0ee0dd5355a69adfa4ad1e9d2a770
Elad Alon <eladalon@chromium.org>
2022-09-10 15:49:04 +0000

    Warn before muting system audio
    When screen-capture is requested by an application or an extension,
    users are presented with a media picker, allowing them to choose
    which surface to share, and whether to share audio.
    
    If the user chooses to share audio as well, it is possible for
    an application to to suppress local audio playback, meaning the
    audio is captured into a MediaStreamTrack, but is no longer played
    out over the user's local speakers. If an application intends to
    use this, it is important to warn users, especially if they are
    using a screen reader.
    
    We therefore display a warning, that if the user chooses to
    share audio, then local audio playback will be muted.
    
    A feature flag, kWarnUserOfSystemWideLocalAudioSuppression,
    acts as a killswitch for all new logic introduced to this effect.
    
    Bug: 1354189
    Change-Id: I29aa3c19601f47b984dd3768165ccd63c2337c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887799
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045533}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
@@ -67,6 +67,7 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView,
   bool Accept() override;
   bool Cancel() override;
   bool ShouldShowCloseButton() const override;
+  void OnWidgetInitialized() override;
 
  private:
   friend class DesktopMediaPickerViewsTestApi;
@@ -92,6 +93,8 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView,
 
   void SetAudioCheckboxAt(int index);
 
+  void MaybeSetAudioCheckboxMaxSize();
+
   void OnSourceTypeSwitched(int index);
 
   int GetSelectedTabIndex() const;
@@ -102,6 +105,7 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView,
   DesktopMediaList::Type GetSelectedSourceListType() const;
 
   const bool audio_requested_;
+  const bool suppress_local_audio_playback_;  // Effective only if audio shared.
   const content::GlobalRenderFrameHostId capturer_global_id_;
 
   raw_ptr<DesktopMediaPickerViews> parent_;
