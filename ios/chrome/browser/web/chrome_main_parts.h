0e828649fdda656cba512c1fb441ccf733ca9e0f
Gauthier Ambard <gambard@chromium.org>
2022-09-12 16:47:28 +0000

    [iOS] Move main parts to ios/chrome/browser/web
    It is the only place that is using it.
    
    Bug: 1354614
    Change-Id: I232c8001df04348f9c3fecf565f200a7d86f178a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891016
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045869}

--- ios/chrome/browser/web/chrome_main_parts.h
+++ ios/chrome/browser/web/chrome_main_parts.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_IOS_CHROME_MAIN_PARTS_H_
-#define IOS_CHROME_BROWSER_IOS_CHROME_MAIN_PARTS_H_
+#ifndef IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
+#define IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
 
 #include <memory>
 
@@ -73,4 +73,4 @@ class IOSChromeMainParts : public web::WebMainParts {
 #endif
 };
 
-#endif  // IOS_CHROME_BROWSER_IOS_CHROME_MAIN_PARTS_H_
+#endif  // IOS_CHROME_BROWSER_WEB_CHROME_MAIN_PARTS_H_
