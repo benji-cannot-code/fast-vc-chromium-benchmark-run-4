e26a0e026d8cf966e56afeea567e221e02307643
Alex Ilin <alexilin@chromium.org>
2022-09-12 11:55:47 +0000

    [profile] Fix AvatarMenuBrowserTest.EditProfile*
    The failing tests need to go into a state when no browser windows are
    open. In order to prevent the browser process shutdown, these tests were
    creating a profile picker instance. However, this caused some test
    flakiness on Mac due to a race condition during shutdown.
    
    This CL simplifies the test by instantiating ScopedKeepAlive directly in
    test to prevent the browser process shutdown. This technique is used in
    other browser tests and proved to be more reliable.
    
    Fixed: 1358380
    Change-Id: I451109d860a5efea7b1d83726d745ce00d0ef779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883934
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045769}

--- chrome/browser/profiles/avatar_menu_browsertest.cc
+++ chrome/browser/profiles/avatar_menu_browsertest.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/keep_alive_registry/keep_alive_types.h"
+#include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -81,19 +83,12 @@ IN_PROC_BROWSER_TEST_F(AvatarMenuBrowserTest, EditProfile) {
             chrome::GetSettingsUrl(chrome::kManageProfileSubPage));
 }
 
-// TODO(crbug.com/1358380): Re-enable this test.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_EditProfile_NoBrowser DISABLED_EditProfile_NoBrowser
-#else
-#define MAYBE_EditProfile_NoBrowser EditProfile_NoBrowser
-#endif
 // Click on "Edit" will open a new browser if none exists for a profile.
-IN_PROC_BROWSER_TEST_F(AvatarMenuBrowserTest, MAYBE_EditProfile_NoBrowser) {
+IN_PROC_BROWSER_TEST_F(AvatarMenuBrowserTest, EditProfile_NoBrowser) {
+  // Keep the browser process running while browsers are closed.
+  ScopedKeepAlive keep_alive(KeepAliveOrigin::BROWSER,
+                             KeepAliveRestartOption::DISABLED);
   Profile* profile = browser()->profile();
-  // Open the profile picker before closing all browser windows to keep the
-  // browser process alive.
-  ProfilePicker::Show(ProfilePicker::Params::FromEntryPoint(
-      ProfilePicker::EntryPoint::kProfileMenuManageProfiles));
   BrowserList::CloseAllBrowsersWithProfile(profile);
   ui_test_utils::WaitForBrowserToClose(browser());
   EXPECT_EQ(chrome::GetBrowserCount(profile), 0U);
@@ -126,10 +121,9 @@ IN_PROC_BROWSER_TEST_F(AvatarMenuBrowserTest, EditProfile_SigninRequired) {
           .GetProfileAttributesWithPath(profile->GetPath());
   ASSERT_NE(entry, nullptr);
   entry->LockForceSigninProfile(true);
-  // Open the profile picker before closing all browser windows to keep the
-  // browser process alive.
-  ProfilePicker::Show(ProfilePicker::Params::FromEntryPoint(
-      ProfilePicker::EntryPoint::kProfileMenuManageProfiles));
+  // Keep the browser process running while browsers are closed.
+  ScopedKeepAlive keep_alive(KeepAliveOrigin::BROWSER,
+                             KeepAliveRestartOption::DISABLED);
   BrowserList::CloseAllBrowsersWithProfile(profile);
   ui_test_utils::WaitForBrowserToClose(browser());
   EXPECT_EQ(chrome::GetBrowserCount(profile), 0U);
@@ -143,6 +137,12 @@ IN_PROC_BROWSER_TEST_F(AvatarMenuBrowserTest, EditProfile_SigninRequired) {
 
   // Browser shouldn't be opened since `profile` is locked.
   EXPECT_EQ(chrome::GetBrowserCount(profile), 0U);
+
+  // The browser test doesn't shut down correctly if `keep_alive` is released
+  // while there are no browser windows. Create browser to work around this
+  // problem.
+  entry->LockForceSigninProfile(false);
+  CreateBrowser(profile);
 }
 
 // Sets up multiple profiles so that the profile picker is shown on next
