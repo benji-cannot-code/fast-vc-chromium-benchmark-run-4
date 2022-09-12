d5f8392c308633f5114b1b52bf17a63845d0ca8b
Keren Zhu <kerenzhu@chromium.org>
2022-09-12 18:13:52 +0000

    Fix widget layering feature guard in omnibox popup
    It should keep the old code path if the widget layering is NOT enabled.
    
    Bug: 1355904
    Change-Id: I1cf8f47b3f753e26c55e997a38ea1c658533b3b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891450
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045915}

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -322,7 +322,7 @@ void OmniboxPopupContentsView::UpdatePopupAppearance() {
     popup_->SetPopupContentsView(this);
     popup_->AddObserver(this);
 
-    if (base::FeatureList::IsEnabled(views::features::kWidgetLayering)) {
+    if (!base::FeatureList::IsEnabled(views::features::kWidgetLayering)) {
       popup_->StackAbove(omnibox_view_->GetRelativeWindowForPopup());
       // For some IMEs GetRelativeWindowForPopup triggers the omnibox to lose
       // focus, thereby closing (and destroying) the popup. TODO(sky): this
