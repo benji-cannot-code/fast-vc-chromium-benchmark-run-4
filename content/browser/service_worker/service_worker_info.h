8f1c6d9e4a08834311456c0f4179118a01fea020
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2022-09-12 05:04:34 +0000

    Make fetch_handler_type visible in serviceworker-internals.
    Like fetch_handler_existence, let me make the fetch handler type visible
    in serviceworker-internals page.
    It can be helpful for people to recognize its behavior.
    
    Bug: 1347319
    Change-Id: I60537c46f0d48dd9d2b9f8a8f44665e3a0a9963c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3886300
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045673}

--- content/browser/service_worker/service_worker_info.h
+++ content/browser/service_worker/service_worker_info.h
@@ -37,7 +37,7 @@ struct CONTENT_EXPORT ServiceWorkerVersionInfo
   ServiceWorkerVersionInfo(
       EmbeddedWorkerStatus running_status,
       ServiceWorkerVersion::Status status,
-      ServiceWorkerVersion::FetchHandlerExistence fetch_handler_existence,
+      absl::optional<ServiceWorkerVersion::FetchHandlerType> fetch_handler_type,
       const GURL& script_url,
       const GURL& scope,
       const blink::StorageKey& storage_key,
@@ -53,7 +53,7 @@ struct CONTENT_EXPORT ServiceWorkerVersionInfo
 
   EmbeddedWorkerStatus running_status;
   ServiceWorkerVersion::Status status;
-  ServiceWorkerVersion::FetchHandlerExistence fetch_handler_existence;
+  absl::optional<ServiceWorkerVersion::FetchHandlerType> fetch_handler_type;
   blink::mojom::NavigationPreloadState navigation_preload_state;
   GURL script_url;
   int thread_id;
--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -1219,11 +1219,8 @@ void ServiceWorkerRegistry::DidGetAllRegistrations(
       info.active_version.registration_id = registration_data->registration_id;
       info.active_version.script_response_time =
           registration_data->script_response_time;
-      info.active_version.fetch_handler_existence =
-          (registration_data->fetch_handler_type ==
-           blink::mojom::ServiceWorkerFetchHandlerType::kNoHandler)
-              ? ServiceWorkerVersion::FetchHandlerExistence::DOES_NOT_EXIST
-              : ServiceWorkerVersion::FetchHandlerExistence::EXISTS;
+      info.active_version.fetch_handler_type =
+          registration_data->fetch_handler_type;
       info.active_version.navigation_preload_state.enabled =
           registration_data->navigation_preload_state->enabled;
       info.active_version.navigation_preload_state.header =
@@ -1235,11 +1232,8 @@ void ServiceWorkerRegistry::DidGetAllRegistrations(
       info.waiting_version.registration_id = registration_data->registration_id;
       info.waiting_version.script_response_time =
           registration_data->script_response_time;
-      info.waiting_version.fetch_handler_existence =
-          (registration_data->fetch_handler_type ==
-           blink::mojom::ServiceWorkerFetchHandlerType::kNoHandler)
-              ? ServiceWorkerVersion::FetchHandlerExistence::DOES_NOT_EXIST
-              : ServiceWorkerVersion::FetchHandlerExistence::EXISTS;
+      info.waiting_version.fetch_handler_type =
+          registration_data->fetch_handler_type;
       info.waiting_version.navigation_preload_state.enabled =
           registration_data->navigation_preload_state->enabled;
       info.waiting_version.navigation_preload_state.header =
