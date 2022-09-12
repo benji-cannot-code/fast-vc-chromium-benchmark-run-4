e8d75354d2c10563c804103f75b1f5e31638ed0f
Javier Contreras <javiercon@microsoft.com>
2022-09-12 18:52:31 +0000

    Revert "[ViewsAX] Moving RetargetForEvents to base class AXTreeManager"
    This reverts commit 379628162464364a4a76e5985c1329ee1aad8058.
    
    Reason for revert: Bug regression (issues 1362217 and 1362321)
    likely caused by this CL. Reverting to give us time to investigate
    
    Original change's description:
    > [ViewsAX] Moving RetargetForEvents to base class AXTreeManager
    >
    > In this CL we've started moving event-related logic from
    > BrowserAccessibilityManager up to AXTreeManager as part of the
    > refactoring. This way, ViewsAXTreeManager will be able to
    > use the same event-related logic.
    >
    > Bug: 1049261
    > Change-Id: Ia8d7447f49baeb775c028315a4ad42f571cd5d94
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880809
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    > Quick-Run: Javier Contreras <javiercon@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1045407}
    
    Bug: 1049261
    Change-Id: I314fe99552716f4cad463ecf043e7187dd9e0a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891991
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1045934}

--- content/browser/accessibility/browser_accessibility_android_unittest.cc
+++ content/browser/accessibility/browser_accessibility_android_unittest.cc
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-using RetargetEventType = ui::AXTreeManager::RetargetEventType;
-
 class MockContentClient : public TestContentClient {
  public:
   std::u16string GetLocalizedString(int message_id) override {
@@ -116,9 +114,9 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetTextOnly) {
   BrowserAccessibility* text_obj = manager->GetFromID(111);
   EXPECT_TRUE(text_obj->IsLeaf());
   EXPECT_FALSE(text_obj->CanFireEvents());
-  BrowserAccessibility* updated =
-      manager->GetFromAXNode(manager->RetargetForEvents(
-          text_obj->node(), RetargetEventType::RetargetEventTypeBlinkHover));
+  BrowserAccessibility* updated = manager->RetargetForEvents(
+      text_obj, BrowserAccessibilityManager::RetargetEventType::
+                    RetargetEventTypeBlinkHover);
   // |updated| should be the paragraph.
   EXPECT_EQ(11, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
@@ -155,9 +153,9 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetHeading) {
   BrowserAccessibility* text_obj = manager->GetFromID(111);
   EXPECT_TRUE(text_obj->IsLeaf());
   EXPECT_FALSE(text_obj->CanFireEvents());
-  BrowserAccessibility* updated =
-      manager->GetFromAXNode(manager->RetargetForEvents(
-          text_obj->node(), RetargetEventType::RetargetEventTypeBlinkHover));
+  BrowserAccessibility* updated = manager->RetargetForEvents(
+      text_obj, BrowserAccessibilityManager::RetargetEventType::
+                    RetargetEventTypeBlinkHover);
   // |updated| should be the heading.
   EXPECT_EQ(11, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
@@ -195,9 +193,9 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetFocusable) {
   BrowserAccessibility* text_obj = manager->GetFromID(111);
   EXPECT_TRUE(text_obj->IsLeaf());
   EXPECT_FALSE(text_obj->CanFireEvents());
-  BrowserAccessibility* updated =
-      manager->GetFromAXNode(manager->RetargetForEvents(
-          text_obj->node(), RetargetEventType::RetargetEventTypeBlinkHover));
+  BrowserAccessibility* updated = manager->RetargetForEvents(
+      text_obj, BrowserAccessibilityManager::RetargetEventType::
+                    RetargetEventTypeBlinkHover);
   // |updated| should be the paragraph.
   EXPECT_EQ(11, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
@@ -284,10 +282,9 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetInputControl) {
   BrowserAccessibility* label_text_obj = manager->GetFromID(label_text.id);
   EXPECT_TRUE(label_text_obj->IsLeaf());
   EXPECT_FALSE(label_text_obj->CanFireEvents());
-  BrowserAccessibility* updated =
-      manager->GetFromAXNode(manager->RetargetForEvents(
-          label_text_obj->node(),
-          RetargetEventType::RetargetEventTypeBlinkHover));
+  BrowserAccessibility* updated = manager->RetargetForEvents(
+      label_text_obj, BrowserAccessibilityManager::RetargetEventType::
+                          RetargetEventTypeBlinkHover);
   EXPECT_EQ(label.id, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
 
@@ -298,16 +295,17 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetInputControl) {
       manager->GetFromID(input_container.id);
   EXPECT_TRUE(input_time_container_obj->IsLeaf());
   EXPECT_FALSE(input_time_container_obj->CanFireEvents());
-  updated = manager->GetFromAXNode(manager->RetargetForEvents(
-      input_time_container_obj->node(),
-      RetargetEventType::RetargetEventTypeBlinkHover));
+  updated = manager->RetargetForEvents(
+      input_time_container_obj, BrowserAccessibilityManager::RetargetEventType::
+                                    RetargetEventTypeBlinkHover);
   EXPECT_EQ(input_time.id, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
   BrowserAccessibility* input_text_obj = manager->GetFromID(input_text.id);
   EXPECT_TRUE(input_text_obj->IsLeaf());
   EXPECT_FALSE(input_text_obj->CanFireEvents());
-  updated = manager->GetFromAXNode(manager->RetargetForEvents(
-      input_text_obj->node(), RetargetEventType::RetargetEventTypeBlinkHover));
+  updated = manager->RetargetForEvents(
+      input_text_obj, BrowserAccessibilityManager::RetargetEventType::
+                          RetargetEventTypeBlinkHover);
   EXPECT_EQ(input_time.id, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
 
@@ -317,8 +315,9 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetInputControl) {
   BrowserAccessibility* button_text_obj = manager->GetFromID(button_text.id);
   EXPECT_TRUE(button_text_obj->IsLeaf());
   EXPECT_FALSE(button_text_obj->CanFireEvents());
-  updated = manager->GetFromAXNode(manager->RetargetForEvents(
-      button_text_obj->node(), RetargetEventType::RetargetEventTypeBlinkHover));
+  updated = manager->RetargetForEvents(
+      button_text_obj, BrowserAccessibilityManager::RetargetEventType::
+                           RetargetEventTypeBlinkHover);
   EXPECT_EQ(button.id, updated->GetId());
   EXPECT_TRUE(updated->CanFireEvents());
   manager.reset();
