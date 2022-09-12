b176eb794afc1729c15e3713293adefac3fd8382
Dominik Röttsches <drott@chromium.org>
2022-09-12 19:08:43 +0000

    Factor out supported font formats to css_parsing_utils
    Preparation for @supports(font-tech()) support.
    No functional change.
    
    Bug: 1255685
    Change-Id: I92dd0cb71ef879776a60563df4439349fcd78c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883933
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045944}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.h
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/parser/css_parser_mode.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_token_range.h"
+#include "third_party/blink/renderer/core/css/parser/css_property_parser.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/frame/web_feature_forward.h"
 #include "third_party/blink/renderer/core/style/grid_area.h"
@@ -425,6 +426,11 @@ CSSValue* ConsumeFontFeatureSettings(CSSParserTokenRange&,
 cssvalue::CSSFontFeatureValue* ConsumeFontFeatureTag(CSSParserTokenRange&,
                                                      const CSSParserContext&);
 CSSIdentifierValue* ConsumeFontVariantCSS21(CSSParserTokenRange&);
+CSSIdentifierValue* ConsumeFontTechIdent(CSSParserTokenRange&);
+CSSIdentifierValue* ConsumeFontFormatIdent(CSSParserTokenRange&);
+CSSValueID FontFormatToId(String);
+bool IsSupportedKeywordTech(CSSValueID keyword);
+bool IsSupportedKeywordFormat(CSSValueID keyword);
 
 CSSValue* ConsumeGridLine(CSSParserTokenRange&, const CSSParserContext&);
 CSSValue* ConsumeGridTrackList(CSSParserTokenRange&,
