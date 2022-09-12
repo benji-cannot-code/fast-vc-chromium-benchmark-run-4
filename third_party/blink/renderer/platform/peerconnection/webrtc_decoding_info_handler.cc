13d6f52bd33ed0b0890807b5176d55306cb154d9
Johannes Kron <kron@chromium.org>
2022-09-12 13:07:46 +0000

    Fix cross thread access to static handler instance
    A call to the MediaCapabilities APIs encodingInfo/decodingInfo
    may crash if the calls are made on a worker thread with type webrtc.
    
    This CL fixes the initialization of the static variable that caused
    the crash.
    
    Bug: chromium:1362493
    Change-Id: I2acff9c937288656c3792b7b52d25fb733b2b397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890973
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045783}

--- third_party/blink/renderer/platform/peerconnection/webrtc_decoding_info_handler.cc
+++ third_party/blink/renderer/platform/peerconnection/webrtc_decoding_info_handler.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 WebrtcDecodingInfoHandler* WebrtcDecodingInfoHandler::Instance() {
-  DEFINE_STATIC_LOCAL(WebrtcDecodingInfoHandler, instance, ());
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(WebrtcDecodingInfoHandler, instance, ());
   return &instance;
 }
 
--- third_party/blink/renderer/platform/peerconnection/webrtc_encoding_info_handler.cc
+++ third_party/blink/renderer/platform/peerconnection/webrtc_encoding_info_handler.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 WebrtcEncodingInfoHandler* WebrtcEncodingInfoHandler::Instance() {
-  DEFINE_STATIC_LOCAL(WebrtcEncodingInfoHandler, instance, ());
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(WebrtcEncodingInfoHandler, instance, ());
   return &instance;
 }
 
