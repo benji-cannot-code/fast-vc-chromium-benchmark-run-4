d37909be58801b9ee5409807079ade6c9a31a256
Christoph Schwering <schwering@google.com>
2022-09-12 16:52:41 +0000

    [Autofill] Fix asynchronous parsing in AutofillManagerTest.
    This CL is a speculative fix of flaky unit AutofillManagerTests when
    asynchronous form parsing is enabled.
    
    These tests parse a large number of forms. The parsing thread runs at
    low priority. This CL simply increases the timeout of the run loop that
    wait for the parsing.
    
    Bug: 1309848, 1345089, 1360013
    Change-Id: I0301b66970d56c58ee026198193725b532d9a2ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888166
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045874}

--- components/autofill/core/browser/test_autofill_manager_waiter.h
+++ components/autofill/core/browser/test_autofill_manager_waiter.h
@@ -81,7 +81,9 @@ class TestAutofillManagerWaiter : public AutofillManager::Observer {
   // Equivalent to re-initialization.
   void Reset();
 
-  // The timeout of the RunLoop.
+  // The timeout of the RunLoop. Since asynchronous-parsing thread in
+  // AutofillManager runs at relatively low priority, a high timeout may be
+  // necessary on slow bots.
   base::TimeDelta timeout() const { return timeout_; }
   void set_timeout(base::TimeDelta timeout) { timeout_ = timeout; }
 
@@ -154,7 +156,7 @@ class TestAutofillManagerWaiter : public AutofillManager::Observer {
 
   std::vector<AfterEvent> relevant_events_;
   std::unique_ptr<State> state_ = std::make_unique<State>();
-  base::TimeDelta timeout_ = base::Seconds(5);
+  base::TimeDelta timeout_ = base::Seconds(30);
   base::ScopedObservation<AutofillManager, AutofillManager::Observer>
       observation_{this};
 };
