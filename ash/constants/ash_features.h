09b2b7cf745d787e2224ebeb03eb60b0a92cec14
Li Lin <llin@chromium.org>
2022-09-12 22:03:52 +0000

    Use API key for translation requests.
    We plan to merge this CL in M106 so keeping the change minimum to
    make merging easier.
    
    Follow up CL will update the interface to take use_api_key as input.
    
    This CL also added a feature flag as kill switch on the safe side.
    
    Bug: b/244190982
    Change-Id: I1e6759389b5c00b070f1637125252c740ef97610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3862987
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046051}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -552,6 +552,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kProjectorBleedingEdgeExperience;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kProjectorWebReportCrash;
+extern const base::Feature kProjectorUseApiKeyForTranslation;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kQsRevamp;
 COMPONENT_EXPORT(ASH_CONSTANTS)
@@ -887,6 +888,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorUpdateIndexableTextEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsProjectorUseOAuthForGetVideoInfoEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorWebReportCrashEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS)
+bool IsProjectorUseApiKeyForTranslationEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsQsRevampEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsQuickDimEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
--- ash/webui/projector_app/test/projector_xhr_sender_unittest.cc
+++ ash/webui/projector_app/test/projector_xhr_sender_unittest.cc
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/run_loop.h"
-#include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "google_apis/google_api_keys.h"
+#include "net/base/url_util.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -20,9 +21,14 @@ namespace {
 
 const char kTestUserEmail[] = "testuser1@gmail.com";
 const base::TimeDelta kExpiryTimeFromNow = base::Minutes(10);
-constexpr char kValidUrl[] = "https://www.googleapis.com/drive/v3/files/fileID";
-constexpr char kValidUrl2[] =
+constexpr char kTestDriveRequestUrl[] =
+    "https://www.googleapis.com/drive/v3/files/fileID";
+constexpr char kTestTranslationRequestUrl[] =
     "https://translation.googleapis.com/language/translate/v2";
+
+GURL GetUrlWithApiKey(const GURL& url) {
+  return net::AppendQueryParameter(url, "key", google_apis::GetAPIKey());
+}
 
 }  // namespace
 
@@ -55,7 +61,8 @@ TEST_F(ProjectorXhrSenderTest, Success) {
 
   const std::string& test_response_body = "{}";
   sender()->Send(
-      GURL(kValidUrl), "GET", /*request_body=*/"", /*useCredential=*/false,
+      GURL(kTestDriveRequestUrl), "GET", /*request_body=*/"",
+      /*use_credentials=*/false,
       base::BindOnce(
           [](const std::string& expected_response_body,
              base::RepeatingClosure quit_closure, bool success,
@@ -67,7 +74,7 @@ TEST_F(ProjectorXhrSenderTest, Success) {
           },
           test_response_body, run_loop.QuitClosure()));
 
-  mock_app_client().test_url_loader_factory().AddResponse(kValidUrl,
+  mock_app_client().test_url_loader_factory().AddResponse(kTestDriveRequestUrl,
                                                           test_response_body);
 
   mock_app_client().GrantOAuthTokenFor(
@@ -80,7 +87,8 @@ TEST_F(ProjectorXhrSenderTest, TwoRequests) {
   base::RunLoop run_loop;
   const std::string& test_response_body = "{}";
   sender()->Send(
-      GURL(kValidUrl), "GET", /*request_body=*/"", /*useCredential=*/false,
+      GURL(kTestDriveRequestUrl), "GET", /*request_body=*/"",
+      /*use_credentials=*/false,
       base::BindOnce(
           [](const std::string& expected_response_body,
              base::RepeatingClosure quit_closure, bool success,
@@ -94,8 +102,10 @@ TEST_F(ProjectorXhrSenderTest, TwoRequests) {
 
   base::RunLoop run_loop2;
   const std::string& test_response_body2 = "{data: {}}";
+  auto translation_url = GURL(kTestTranslationRequestUrl);
   sender()->Send(
-      GURL(kValidUrl2), "GET", /*request_body=*/"", /*useCredential=*/false,
+      translation_url, "GET", /*request_body=*/"",
+      /*use_credentials=*/false,
       base::BindOnce(
           [](const std::string& expected_response_body,
              base::RepeatingClosure quit_closure, bool success,
@@ -107,11 +117,11 @@ TEST_F(ProjectorXhrSenderTest, TwoRequests) {
           },
           test_response_body2, run_loop2.QuitClosure()));
 
-  mock_app_client().test_url_loader_factory().AddResponse(kValidUrl,
+  mock_app_client().test_url_loader_factory().AddResponse(kTestDriveRequestUrl,
                                                           test_response_body);
 
-  mock_app_client().test_url_loader_factory().AddResponse(kValidUrl2,
-                                                          test_response_body2);
+  mock_app_client().test_url_loader_factory().AddResponse(
+      GetUrlWithApiKey(translation_url).spec(), test_response_body2);
 
   mock_app_client().GrantOAuthTokenFor(
       kTestUserEmail,
@@ -125,7 +135,8 @@ TEST_F(ProjectorXhrSenderTest, UseCredentials) {
 
   const std::string& test_response_body = "{}";
   sender()->Send(
-      GURL(kValidUrl), "GET", /*request_body=*/"", /*useCredential=*/true,
+      GURL(kTestDriveRequestUrl), "GET", /*request_body=*/"",
+      /*use_credentials=*/true,
       base::BindOnce(
           [](const std::string& expected_response_body,
              base::RepeatingClosure quit_closure, bool success,
@@ -138,8 +149,33 @@ TEST_F(ProjectorXhrSenderTest, UseCredentials) {
           test_response_body, run_loop.QuitClosure()));
 
   // Verify that http request is sent without granting OAuth token.
-  mock_app_client().test_url_loader_factory().AddResponse(kValidUrl,
+  mock_app_client().test_url_loader_factory().AddResponse(kTestDriveRequestUrl,
                                                           test_response_body);
+
+  run_loop.Run();
+}
+
+TEST_F(ProjectorXhrSenderTest, UseApiKey) {
+  base::RunLoop run_loop;
+
+  auto url = GURL(kTestTranslationRequestUrl);
+  const std::string& test_response_body = "{}";
+  sender()->Send(
+      url, "GET", /*request_body=*/"", /*use_credentials=*/false,
+      base::BindOnce(
+          [](const std::string& expected_response_body,
+             base::RepeatingClosure quit_closure, bool success,
+             const std::string& response_body, const std::string& error) {
+            EXPECT_TRUE(success);
+            EXPECT_EQ(expected_response_body, response_body);
+            EXPECT_EQ("", error);
+            quit_closure.Run();
+          },
+          test_response_body, run_loop.QuitClosure()));
+
+  // Verify that http request is sent with API key.
+  mock_app_client().test_url_loader_factory().AddResponse(
+      GetUrlWithApiKey(url).spec(), test_response_body);
 
   run_loop.Run();
 }
@@ -148,7 +184,7 @@ TEST_F(ProjectorXhrSenderTest, NetworkError) {
   base::RunLoop run_loop;
 
   sender()->Send(
-      GURL(kValidUrl), /*method=*/"GET", /*request_body=*/"",
+      GURL(kTestDriveRequestUrl), /*method=*/"GET", /*request_body=*/"",
       /*use_credentials=*/false,
       base::BindOnce(
           [](base::RepeatingClosure quit_closure, bool success,
@@ -161,8 +197,8 @@ TEST_F(ProjectorXhrSenderTest, NetworkError) {
           run_loop.QuitClosure()));
 
   mock_app_client().test_url_loader_factory().AddResponse(
-      GURL(kValidUrl), network::mojom::URLResponseHead::New(), std::string(),
-      network::URLLoaderCompletionStatus(net::HTTP_NOT_FOUND));
+      GURL(kTestDriveRequestUrl), network::mojom::URLResponseHead::New(),
+      std::string(), network::URLLoaderCompletionStatus(net::HTTP_NOT_FOUND));
 
   mock_app_client().GrantOAuthTokenFor(
       kTestUserEmail,
