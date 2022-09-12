68f121d49577b7d3fab43e3f413198b43f7ff219
kenkangxgwe <kenkangxgwe@google.com>
2022-09-12 19:41:29 +0000

    Add Unit Test to ScopedAlsaMixer
    Bug: None
    Test: cast_media_unittests --gtest_filter=*ScopedAlsaMixer*
    Change-Id: Iac7553de548ec9e243c5085229459f24fbe9b9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3886176
    Commit-Queue: Mingyu Kang <kenkangxgwe@google.com>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045963}

--- chromecast/media/cma/backend/alsa/alsa_volume_control.cc
+++ chromecast/media/cma/backend/alsa/alsa_volume_control.cc
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/bind.h"
-#include "base/callback_helpers.h"
 #include "base/check.h"
 #include "base/command_line.h"
+#include "base/cxx17_backports.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/numerics/ranges.h"
 #include "base/strings/string_split.h"
 #include "base/task/current_thread.h"
 #include "chromecast/base/chromecast_switches.h"
@@ -231,7 +229,7 @@ float AlsaVolumeControl::GetRoundtripVolume(float volume) {
   }
 
   long level = 0;  // NOLINT(runtime/int)
-  level = std::round((base::ClampToRange(volume, 0.0f, 1.0f) *
+  level = std::round((base::clamp(volume, 0.0f, 1.0f) *
                       (volume_range_max_ - volume_range_min_)) +
                      volume_range_min_);
   return static_cast<float>(level - volume_range_min_) /
