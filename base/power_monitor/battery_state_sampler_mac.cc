2759cce10f198287349073077f7877225b188fb2
Patrick Monette <pmonette@chromium.org>
2022-09-12 22:34:37 +0000

    Add BatteryStateSampler
    Currently, if a component needs to query the state of the battery, an additional BatteryLevelProvider instance is needed which works by
    manually requesting the current state from the OS. This can be wasteful because the battery level does not change too often so its value can be
    cached.
    
    The new BatteryStateSampler is a singleton that is meant to be the only
    class responsible for querying the battery state from the OS and then
    notifying all the observers of the latest sample.
    
    One disavantage of this solution is that observers do not decide when
    the samples are taken. This is mitigated by the fact that on platforms where it is possible, a sample is only taken when the OS notifies us
    that the value has possibly changed (MacOS only for now). So requesting
    an additional sample would not return a different value anyways.
    
    Bug: 1248057
    Change-Id: I5f80f02c6c6b446acdb0280250f97177168bef56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820130
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046074}

---
+++ base/power_monitor/battery_state_sampler_mac.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/power_monitor/battery_state_sampler.h"
+
+#include "base/power_monitor/iopm_power_source_sampling_event_source.h"
+
+namespace base {
+
+// static
+std::unique_ptr<SamplingEventSource>
+BatteryStateSampler::CreateSamplingEventSource() {
+  return std::make_unique<IOPMPowerSourceSamplingEventSource>();
+}
+
+}  // namespace base
