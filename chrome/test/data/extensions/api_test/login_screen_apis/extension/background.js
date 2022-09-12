29592aeaa504168e93682bef99b3b67d43743d97
anina koehler <aninak@google.com>
2022-09-12 11:13:56 +0000

    LoginApitest.UnlockManagedGuestSession is flaky
    As a result of the chrome.login.unlockManagedGuestSession() API call, the locked session will be unlocked and the test extension running on the lock screen will be removed. The test timed out as there is no guarantee that test.succeed() gets called before the extension is stopped. Instead of awaiting success it should verify the session state.
    
    Bug: 1351338
    Change-Id: I31fc4a3de4ee1906880bb51deb11a7b023a41c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3873640
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Commit-Queue: Anina Koehler <aninak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045761}

--- chrome/test/data/extensions/api_test/login_screen_apis/extension/background.js
+++ chrome/test/data/extensions/api_test/login_screen_apis/extension/background.js
@@ -124,7 +124,7 @@ const tests = {
   'LoginExitCurrentSession': () => {
     chrome.test.getConfig(config => {
       chrome.login.exitCurrentSession(config.customArg);
-      // No check for success as browser process exists.
+      // No check for success as browser process exits.
     });
   },
   'LoginFetchDataForNextLoginAttempt': () => {
@@ -144,10 +144,8 @@ const tests = {
   },
   'LoginUnlockManagedGuestSession': () => {
     chrome.test.getConfig(config => {
-      chrome.login.unlockManagedGuestSession(config.customArg, () => {
-        chrome.test.assertNoLastError();
-        chrome.test.succeed();
-      });
+      chrome.login.unlockManagedGuestSession(config.customArg);
+      // No check for success as browser process exits.
     });
   },
   'LoginUnlockManagedGuestSessionWrongPassword': () => {
