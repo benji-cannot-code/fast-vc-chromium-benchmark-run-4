c79f6ae185a2c03d46913c84fdbf7dcdeaf553b4
Max Ihlenfeldt <mihlenfeldt@igalia.com>
2022-09-12 10:58:52 +0000

    Let TabStripRegionView forward drag events to TabDragController
    This is a simplification refactor for fallback tab dragging. The old
    implementation was needlessly complicated, we now just use the standard
    drag and drop methods, e.g. CanDrop().
    
    Letting TabStripRegionView handle everything and disabling TabStrip for
    DnD also fixes a bug where an extra DragLeave event was emitted because
    the drop target switched from TabStripRegionView to TabStrip.
    
    Bug: 1361940
    Change-Id: I07c8e0abee2d630c737e245eed86279cfc9c1ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883929
    Commit-Queue: Max Ihlenfeldt <mihlenfeldt@igalia.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045759}

--- chrome/browser/ui/views/frame/browser_root_view.cc
+++ chrome/browser/ui/views/frame/browser_root_view.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/buildflags/buildflags.h"
 #include "third_party/blink/public/common/mime_util/mime_util.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
+#include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/drop_target_event.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
@@ -156,11 +157,16 @@ bool BrowserRootView::CanDrop(const ui::OSExchangeData& data) {
   if (!tabstrip()->GetVisible() && !toolbar()->GetVisible())
     return false;
 
-  // Return false and let TabStripRegionView forward drag events to TabStrip.
-  // This is necessary because we don't want to return true if
-  // tabstrip()->WantsToReceiveAllDragEvents() is true but the mouse is not over
-  // the tab strip region, and we don't know the current mouse location.
-  if (tabstrip()->WantsToReceiveAllDragEvents())
+  // If this is for a fallback window dragging session, return false and let
+  // TabStripRegionView forward drag events to TabDragController. This is
+  // necessary because we don't want to return true if the custom MIME type is
+  // there but the mouse is not over the tab strip region, and we don't know the
+  // current mouse location.
+  // TODO(crbug.com/1307594): This is a smoking gun code smell;
+  // TabStripRegionView and Toolbar have different affordances, so they should
+  // separately override the drag&drop methods.
+  if (data.HasCustomFormat(
+          ui::ClipboardFormatType::GetType(ui::kMimeTypeWindowDrag)))
     return false;
 
   // If there is a URL, we'll allow the drop.
