7528e51fcc8e48e4ecbb5d172fb9a51558c8992d
yixie <yixie@chromium.org>
2022-09-12 09:40:35 +0000

    Fix flaky WebKioskAppServiceLauncher tests
    Web app system is not immediately ready when web Kiosk is being started.
    We need to wait for web app to be initialized before we start observing
    web app updates for Kiosk apps.
    
    The Kiosk web app is not always ready inside App Service by the time the
    Kiosk launcher tries to launch it. Therefore we cannot assume readiness
    recorded in UMA is kReady in unit tests.
    
    Bug: 1360965, 1361012, 1361035, 1361045, b:240493670
    Test: Run unit tests WebKioskAppServiceLauncher.*
    Change-Id: Ibb5f9d4634f5810ff39f7e901e65aa2e0206ca99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881811
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Yi Xie <yixie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045738}

--- chrome/browser/ash/app_mode/web_app/web_kiosk_app_service_launcher_unittest.cc
+++ chrome/browser/ash/app_mode/web_app/web_kiosk_app_service_launcher_unittest.cc
@@ -303,8 +303,7 @@ class WebKioskAppServiceLauncherTest : public BrowserWithTestWindowTest {
   bool install_placeholder_ = false;
 };
 
-// TODO(crbug.com/1361035): Fix flaky test
-TEST_F(WebKioskAppServiceLauncherTest, DISABLED_NormalFlowNotInstalled) {
+TEST_F(WebKioskAppServiceLauncherTest, NormalFlowNotInstalled) {
   base::HistogramTester histogram;
 
   SetupAppData(/*installed=*/false);
@@ -321,15 +320,16 @@ TEST_F(WebKioskAppServiceLauncherTest, DISABLED_NormalFlowNotInstalled) {
   EXPECT_EQ(app_data()->status(), WebKioskAppData::Status::kInstalled);
   EXPECT_EQ(app_data()->launch_url(), kAppLaunchUrl);
 
-  histogram.ExpectUniqueSample(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
-                               apps::Readiness::kReady, 1);
+  // App isn't always ready by the time it's being launched. Therefore we check
+  // the total count of kLaunchAppReadinessUMA instead of individual cases.
+  histogram.ExpectTotalCount(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
+                             1);
   histogram.ExpectUniqueSample(
       WebKioskAppServiceLauncher::kWebAppInstallResultUMA,
       webapps::InstallResultCode::kSuccessNewInstall, 1);
 }
 
-// TODO(crbug.com/1360965): Fix flaky test
-TEST_F(WebKioskAppServiceLauncherTest, DISABLED_NormalFlowAlreadyInstalled) {
+TEST_F(WebKioskAppServiceLauncherTest, NormalFlowAlreadyInstalled) {
   base::HistogramTester histogram;
 
   SetupAppData(/*installed=*/true);
@@ -339,8 +339,10 @@ TEST_F(WebKioskAppServiceLauncherTest, DISABLED_NormalFlowAlreadyInstalled) {
 
   EXEC_AND_WAIT_FOR_CALL(launcher()->LaunchApp(), *delegate(), OnAppLaunched());
 
-  histogram.ExpectUniqueSample(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
-                               apps::Readiness::kReady, 1);
+  // App isn't always ready by the time it's being launched. Therefore we check
+  // the total count of kLaunchAppReadinessUMA instead of individual cases.
+  histogram.ExpectTotalCount(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
+                             1);
   histogram.ExpectTotalCount(
       WebKioskAppServiceLauncher::kWebAppInstallResultUMA, 0);
 }
@@ -391,8 +393,10 @@ TEST_F(WebKioskAppServiceLauncherTest, PlaceholderReplaced) {
   EXPECT_FALSE(web_app_provider()->registrar().LookupPlaceholderAppId(
       GURL(kAppInstallUrl), web_app::WebAppManagement::Type::kKiosk));
 
-  histogram.ExpectUniqueSample(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
-                               apps::Readiness::kReady, 1);
+  // App isn't always ready by the time it's being launched. Therefore we check
+  // the total count of kLaunchAppReadinessUMA instead of individual cases.
+  histogram.ExpectTotalCount(KioskAppServiceLauncher::kLaunchAppReadinessUMA,
+                             1);
   histogram.ExpectUniqueSample(
       WebKioskAppServiceLauncher::kWebAppInstallResultUMA,
       webapps::InstallResultCode::kSuccessNewInstall, 1);
