9e5ccdf30c7dea4a9a5c19b5e548936b1254d06a
Joel Hockey <joelhockey@chromium.org>
2022-09-12 21:20:35 +0000

    Update lacros to use static mime cache
    When lacros looks up mime type from file extension, it is currently
    reading from /usr/share/mime/mime.cache. We have recently added an
    equivalent static map which is used by ash which is also available for
    lacros to use. This approach has the performance and security advantage
    that there is no need to read a system file.
    
    Bug: 1015353
    Change-Id: I625342d2f68386df6d8cc78a389b61b7354aa3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890051
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046023}

--- net/base/platform_mime_util_linux.cc
+++ net/base/platform_mime_util_linux.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "net/android/network_library.h"
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(IS_CHROMEOS)
 #include "third_party/xdg_shared_mime_info/mime_cache.h"
 #else
 #include "base/nix/mime_util_xdg.h"
@@ -26,7 +26,7 @@ bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
     std::string* result) const {
   return android::GetMimeTypeFromExtension(ext, result);
 }
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(IS_CHROMEOS)
 bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
     const base::FilePath::StringType& ext,
     std::string* result) const {
