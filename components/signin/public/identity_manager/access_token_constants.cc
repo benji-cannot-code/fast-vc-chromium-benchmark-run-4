03f491e1552236659d45011b64128262b269475e
Russ Hamilton <behamilton@google.com>
2022-09-12 14:39:33 +0000

    Add k-anonymity trust token getter
    Bug: 1342255
    Change-Id: I7bdcfadaca12dd9d3a78b35a3bda0982736943b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830314
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045819}

--- components/signin/public/identity_manager/access_token_constants.cc
+++ components/signin/public/identity_manager/access_token_constants.cc
@@ -70,6 +70,9 @@ const std::set<std::string> GetUnconsentedOAuth2Scopes() {
       // improve suggestions.
       GaiaConstants::kFeedOAuth2Scope,
 
+      // Required by k-Anonymity Server (FLEDGE)
+      GaiaConstants::kKAnonymityServiceOAuth2Scope,
+
     // Required by ChromeOS only.
 #if BUILDFLAG(IS_CHROMEOS_ASH)
       GaiaConstants::kAccountsReauthOAuth2Scope,
