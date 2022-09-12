2fa9ac608ef86e1d78a635b5e70a729475d1c58e
Saeid Eid <saeideid@chromium.org>
2022-09-12 15:41:52 +0000

    [Identifiability Study] Add SequenceChecker DCHECKs to PrivacyBudgetReidScoreEstimator
    The PrivacyBudgetReidScoreEstimator contains some state which needs to be accessed in a thread-safe manner. This CL adds SequenceChecker DCHECKs to the methods.
    
    Bug: 1330244
    Change-Id: Ie02f6d0cc784bec890f97e97d8155690f4d12316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890950
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Saeid Eid <saeideid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045844}

--- chrome/browser/privacy_budget/privacy_budget_reid_score_estimator.h
+++ chrome/browser/privacy_budget/privacy_budget_reid_score_estimator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 
 #include "base/containers/flat_map.h"
+#include "base/sequence_checker.h"
 #include "chrome/browser/privacy_budget/identifiability_study_group_settings.h"
 #include "chrome/common/privacy_budget/types.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
@@ -117,19 +118,24 @@ class PrivacyBudgetReidScoreEstimator {
     blink::IdentifiableSurface reid_surface_key_;
   };
 
-  void WriteReportedReidBlocksToPrefs() const;
+  void WriteReportedReidBlocksToPrefs() const
+      VALID_CONTEXT_REQUIRED(sequence_checker_);
 
   // Keeps track of the blocks for which we want to compute the Reid score.
-  std::list<ReidBlockStorage> surface_blocks_;
+  std::list<ReidBlockStorage> surface_blocks_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   // Keeps track of the Reid blocks which where already reported.
-  IdentifiableSurfaceList already_reported_reid_blocks_;
+  IdentifiableSurfaceList already_reported_reid_blocks_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   // `settings_` pointee must outlive `this`.
   raw_ptr<const IdentifiabilityStudyGroupSettings> settings_;
 
   // `pref_service_` pointee must outlive `this`. Used for persistent state.
   raw_ptr<PrefService> pref_service_ = nullptr;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 #endif  // CHROME_BROWSER_PRIVACY_BUDGET_PRIVACY_BUDGET_REID_SCORE_ESTIMATOR_H_
