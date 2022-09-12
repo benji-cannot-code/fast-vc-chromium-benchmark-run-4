cb30bab89345a34052ed750725fe333d3ec829ed
Jana Grill <janagrill@google.com>
2022-09-12 15:51:24 +0000

    Add migration logic
    Add the steps of backward migration including:
    1. Merge items that were split between Ash and Lacros and put them into
       the temporary directory
    2. Move Lacros-only items back into the Ash profile directory
    3. Move the temporary directory into the Ash profile directory
    4. Delete the Lacros profile directory
    
    The steps are currently empty and each simply calls the next one.
    Subsequent CLs will implement individual steps.
    
    In OOBE, read the value of kBrowserDataBackwardMigrationForUse, which
    contains the hash of the user being back migrated to Ash and use this
    value to access the correct profile directory.
    
    This flag is currently not set by session manager, but for testing it
    can be set in /etc/chrome_dev.conf by passing
    --browser-data-backward-migration-for-user=<hash-without-leading-u>.
    
    Bug: b/244573664
    Change-Id: I9cb4e823a5f0f413be6b9c8b32778e30f4764dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865011
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Jana Grill <janagrill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045847}

--- chrome/browser/ash/login/screens/lacros_data_backward_migration_screen.cc
+++ chrome/browser/ash/login/screens/lacros_data_backward_migration_screen.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/screens/lacros_data_backward_migration_screen.h"
 
+#include "ash/constants/ash_switches.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/path_service.h"
 #include "chrome/browser/ash/crosapi/browser_data_back_migrator.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/common/chrome_paths.h"
 
 namespace ash {
@@ -29,14 +32,31 @@ void LacrosDataBackwardMigrationScreen::ShowImpl() {
     return;
 
   if (!migrator_) {
+    const std::string user_id_hash =
+        base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+            switches::kBrowserDataBackwardMigrationForUser);
+
+    DCHECK(!user_id_hash.empty()) << "user_id_hash should not be empty.";
+    if (user_id_hash.empty()) {
+      LOG(ERROR) << "Could not retrieve user_id_hash from switch "
+                 << switches::kBrowserDataBackwardMigrationForUser
+                 << ". Aborting migration.";
+      // TODO(b/245053119): Attempt restart.
+      return;
+    }
+
     base::FilePath user_data_dir;
     if (!base::PathService::Get(chrome::DIR_USER_DATA, &user_data_dir)) {
       LOG(ERROR) << "Could not get the original user data dir path. Aborting "
                     "migration.";
+      // TODO(b/245053119): Attempt restart.
       return;
     }
 
-    migrator_ = std::make_unique<BrowserDataBackMigrator>(user_data_dir);
+    const base::FilePath profile_data_dir =
+        user_data_dir.Append(ProfileHelper::GetUserProfileDir(user_id_hash));
+
+    migrator_ = std::make_unique<BrowserDataBackMigrator>(profile_data_dir);
   }
 
   migrator_->Migrate(
