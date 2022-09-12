3349502b069eb1995b2870ca4b0e682ae8863bbc
Tom Anderson <thomasanderson@chromium.org>
2022-09-12 23:59:50 +0000

    Fix -Wextra-semi errors (Android)
    R=thakis
    
    Bug: 1355871
    Change-Id: I03a38ffc80a9630bc1965399d89420605a96d1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891834
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046114}

--- components/safe_browsing/android/safe_browsing_api_handler_bridge.h
+++ components/safe_browsing/android/safe_browsing_api_handler_bridge.h
@@ -62,7 +62,7 @@ class SafeBrowsingApiHandlerBridge {
 // SafeBrowsingApiHandlerBridge. Intended for use only in tests.
 class UrlCheckInterceptor {
  public:
-  virtual ~UrlCheckInterceptor(){};
+  virtual ~UrlCheckInterceptor() {}
   virtual void Check(
       std::unique_ptr<SafeBrowsingApiHandlerBridge::ResponseCallback> callback,
       const GURL& url) const = 0;
--- device/gamepad/haptic_gamepad_android.cc
+++ device/gamepad/haptic_gamepad_android.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 HapticGamepadAndroid::HapticGamepadAndroid(int device_index)
-    : device_index_(device_index){};
+    : device_index_(device_index) {}
 
 HapticGamepadAndroid::~HapticGamepadAndroid() = default;
 
