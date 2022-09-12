5083612bff399c7ac18c9771480842ce1a5ba6b9
Etienne Pierre-doray <etiennep@chromium.org>
2022-09-12 16:22:42 +0000

    [task] Less precise dom timer
    This CL makes the AlignWakeUps process opt-out (usually due to active
    WebRTC connection) a bit more agressive. Only dom timers with an
    interval smaller than 32ms are opt-out (considered 'precise').
    
    Bigger intervals are already considerer 'low-resolution' on windows.
    This should reduce the impact of dom timers on time spent in high
    resolution in ExplicitHighResolutionTimerWin experiment.
    
    Bug: 1153139
    Change-Id: Iecc853960ff0a866ea10ae3fa2a803ae6cceb91f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879883
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045861}

--- third_party/blink/renderer/core/frame/dom_timer.cc
+++ third_party/blink/renderer/core/frame/dom_timer.cc
@@ -49,6 +49,7 @@ namespace {
 // greater than 5.
 constexpr int kMaxTimerNestingLevel = 5;
 constexpr base::TimeDelta kMinimumInterval = base::Milliseconds(4);
+constexpr base::TimeDelta kMaxHighResolutionInterval = base::Milliseconds(32);
 
 }  // namespace
 
@@ -103,7 +104,8 @@ DOMTimer::DOMTimer(ExecutionContext* context,
   // MaxUnthrottledTimeoutNestingLevel and SetTimeoutZeroWithoutClamping.
   // TODO(1153139) Remove this logic one experiments have shipped.
   bool precise = (timeout < kMinimumInterval) ||
-                 scheduler::IsAlignWakeUpsDisabledForProcess();
+                 (scheduler::IsAlignWakeUpsDisabledForProcess() &&
+                  timeout < kMaxHighResolutionInterval);
 
   if (nesting_level_ >= max_nesting_level && timeout < kMinimumInterval)
     timeout = kMinimumInterval;
