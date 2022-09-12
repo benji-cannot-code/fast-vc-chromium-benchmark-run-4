5a43f2e68797d168738977080ecfe32f9227c8ec
jhtin <jhtin@chromium.org>
2022-09-12 02:57:44 +0000

    assistive suggester filter: add crosh and ssh app to diacritics denylist
    __SIMPLE_REVIEW__
    
    Fixed: b/242636405
    Change-Id: I568777f06afd6feccb17c4b272fbcb3b819ec7d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881562
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045653}

--- chrome/browser/ash/input_method/assistive_suggester_client_filter_unittest.cc
+++ chrome/browser/ash/input_method/assistive_suggester_client_filter_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -279,6 +279,22 @@ INSTANTIATE_TEST_SUITE_P(
         {"ChromeSSHApp",
          /* url=*/"chrome-extension://iodihamcpbpeioajjeobimgagajmlibd",
          /* app_id=*/"iodihamcpbpeioajjeobimgagajmlibd",
+         /* arc_package_name=*/"",
+         EnabledSuggestions{.emoji_suggestions = false,
+                            .multi_word_suggestions = false,
+                            .personal_info_suggestions = false,
+                            .diacritic_suggestions = false}},
+        {"CroshApp",
+         /* url=*/"chrome-extension://cgfnfgkafmcdkdgilmojlnaadileaach",
+         /* app_id=*/"cgfnfgkafmcdkdgilmojlnaadileaach",
+         /* arc_package_name=*/"",
+         EnabledSuggestions{.emoji_suggestions = false,
+                            .multi_word_suggestions = false,
+                            .personal_info_suggestions = false,
+                            .diacritic_suggestions = false}},
+        {"ChromeTerminalApp",
+         /* url=*/"chrome-extension://fhicihalidkgcimdmhpohldehjmcabcf",
+         /* app_id=*/"fhicihalidkgcimdmhpohldehjmcabcf",
          /* arc_package_name=*/"",
          EnabledSuggestions{.emoji_suggestions = false,
                             .multi_word_suggestions = false,
