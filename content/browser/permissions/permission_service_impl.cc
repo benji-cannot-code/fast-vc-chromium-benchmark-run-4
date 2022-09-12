7a18c718c5836c771cd5247a685a8b2e815673c0
Matt Reichhoff <mreichhoff@chromium.org>
2022-09-12 20:36:00 +0000

    [rsafor] Connect renderer to permission service
    As of now, the grant logic will be shared between rsaFor and rsa.
    The next CL will change that, and include additional validation
    of the new mojo message. Note that the current view is that
    genericizing the domain override concept is not desirable, so the
    implementation is a bit of a one-off.
    
    Bug: 1351540
    Change-Id: Ie5c9ebd726bb9c9d25ffb13ebd7794b688548c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857020
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045997}

--- content/browser/permissions/permission_service_impl.cc
+++ content/browser/permissions/permission_service_impl.cc
@@ -18,8 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/permission_result.h"
 #include "content/public/browser/render_frame_host.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-shared.h"
+#include "url/origin.h"
 
 using blink::mojom::PermissionDescriptorPtr;
 using blink::mojom::PermissionName;
@@ -36,6 +38,23 @@ void PermissionRequestResponseCallbackWrapper(
     const std::vector<PermissionStatus>& vector) {
   DCHECK_EQ(vector.size(), 1ul);
   std::move(callback).Run(vector[0]);
+}
+
+bool IsDomainOverride(const PermissionDescriptorPtr& descriptor) {
+  return descriptor->extension && descriptor->extension->is_storage_access();
+}
+
+url::Origin ExtractDomainOverride(const PermissionDescriptorPtr& descriptor) {
+  const blink::mojom::StorageAccessPermissionDescriptorPtr&
+      override_descriptor = descriptor->extension->get_storage_access();
+  return override_descriptor->siteOverride;
+}
+
+bool IsDomainOverrideEnabled() {
+  // This code path is currently available only when
+  // requestStorageAccessForSite is enabled.
+  return base::FeatureList::IsEnabled(
+      blink::features::kStorageAccessAPIForSiteExtension);
 }
 
 }  // anonymous namespace
@@ -126,11 +145,31 @@ void PermissionServiceImpl::RequestPermissions(
       std::make_unique<PendingRequest>(types, std::move(callback));
 
   int pending_request_id = pending_requests_.Add(std::move(pending_request));
-  PermissionControllerImpl::FromBrowserContext(browser_context)
-      ->RequestPermissionsFromCurrentDocument(
-          types, context_->render_frame_host(), user_gesture,
-          base::BindOnce(&PermissionServiceImpl::OnRequestPermissionsResponse,
-                         weak_factory_.GetWeakPtr(), pending_request_id));
+
+  if (!permissions.empty() && IsDomainOverride(permissions[0])) {
+    if (!IsDomainOverrideEnabled()) {
+      ReceivedBadMessage();
+      return;
+    }
+    if (types.size() > 1) {
+      // Requests with domain overrides must be requested individually.
+      ReceivedBadMessage();
+      return;
+    }
+    url::Origin requested_origin = ExtractDomainOverride(permissions[0]);
+    PermissionControllerImpl::FromBrowserContext(browser_context)
+        ->RequestPermissions(
+            types, context_->render_frame_host(), requested_origin,
+            user_gesture,
+            base::BindOnce(&PermissionServiceImpl::OnRequestPermissionsResponse,
+                           weak_factory_.GetWeakPtr(), pending_request_id));
+  } else {
+    PermissionControllerImpl::FromBrowserContext(browser_context)
+        ->RequestPermissionsFromCurrentDocument(
+            types, context_->render_frame_host(), user_gesture,
+            base::BindOnce(&PermissionServiceImpl::OnRequestPermissionsResponse,
+                           weak_factory_.GetWeakPtr(), pending_request_id));
+  }
 }
 
 void PermissionServiceImpl::OnRequestPermissionsResponse(
