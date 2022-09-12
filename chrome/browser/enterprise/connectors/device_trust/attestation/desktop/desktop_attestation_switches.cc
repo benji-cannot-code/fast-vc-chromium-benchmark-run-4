d3434fb3c1ec64b1048d503491e2cf48f11662b9
Philippe Leblanc <pleblanc@google.com>
2022-09-12 22:49:02 +0000

    Add command line switch to use Verified Access server DEV keys.
    Change-Id: I2ae62bf551238b898c7b73d00f09dd4c9f507b6c
    Fixed: 1345986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780303
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Philippe Leblanc <pleblanc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046085}

---
+++ chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_switches.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_switches.h"
+
+namespace enterprise_connectors::switches {
+
+const char kUseVaDevKeys[] = "use-va-dev-keys";
+
+}  // namespace enterprise_connectors::switches
