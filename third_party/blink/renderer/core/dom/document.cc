c0bca861f9572a417e58fc9408e80a51e280af8c
Chris Fredrickson <cfredric@chromium.org>
2022-09-13 18:55:48 +0000

    Consume user activation when explicitly rejecting requestStorageAccess prompt.
    This corresponds to step 10.1 of the "determine storage access policy"
    procedure defined in https://privacycg.github.io/storage-access/#ua-policy.
    
    Bug: 989663
    Change-Id: I4c022731553c0c0de4f668f807133b575c410a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891174
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046484}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -6283,6 +6283,8 @@ ScriptPromise Document::requestStorageAccess(ScriptState* script_state) {
                     resolver->Resolve();
                     break;
                   case mojom::blink::PermissionStatus::DENIED:
+                    LocalFrame::ConsumeTransientUserActivation(
+                        document->GetFrame());
                     document->expressly_denied_storage_access_ = true;
                     [[fallthrough]];
                   case mojom::blink::PermissionStatus::ASK:
