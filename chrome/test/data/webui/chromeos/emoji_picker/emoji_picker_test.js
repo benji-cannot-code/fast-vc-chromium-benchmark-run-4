1300595a9066f98653fa0c90e1c2f82c5c287f34
Zac Partridge <zacpartridge@google.com>
2022-09-13 01:29:47 +0000

    emoji-picker: no longer crashes when searchResults is undefined
    getNumSearchResults now checks if searchResults is defined before using.
    Also, tests are fixed so that they would catch this error.
    
    Bug: b:245825822
    Change-Id: I48e0f4d7b3990f7bf1cc551650ba349431500629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890431
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046157}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test.js
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -373,12 +373,12 @@ suite('<emoji-picker>', () => {
   });
 
   suite('<emoji-search>', () => {
-    test('works when there are no results', async () => {
+    test('works when there are no results', () => {
       // This test just ensures that no errors are thrown.
       const enterEvent = new KeyboardEvent(
           'keydown', {cancelable: true, key: 'Enter', keyCode: 13});
       const search = findInEmojiPicker('emoji-search');
-      search.onKeyDown(enterEvent);
+      search.onSearchKeyDown(enterEvent);
     });
     test('finds results in the second group', async () => {
       const search = findInEmojiPicker('emoji-search');
