1867f0aa7aa3f3e61f8d98b87fca7af7347a5174
Charlie Harrison <csharrison@chromium.org>
2022-09-13 00:59:26 +0000

    Add ParsedPermissionsPolicyDeclaration::Contains
    Follow-up refactor from comments in crrev.com/c/3877268.
    This CL has no intended behavior changes.
    
    Bug: None
    Change-Id: I2f659a5c32d01eb0f1cdf58d1bf0de8af862d1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888042
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046147}

--- third_party/blink/common/permissions_policy/permissions_policy_declaration.cc
+++ third_party/blink/common/permissions_policy/permissions_policy_declaration.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 #include <vector>
 
+#include "base/containers/contains.h"
 #include "url/origin.h"
 
 namespace blink {
@@ -35,6 +36,12 @@ ParsedPermissionsPolicyDeclaration::ParsedPermissionsPolicyDeclaration(
 ParsedPermissionsPolicyDeclaration&
 ParsedPermissionsPolicyDeclaration::operator=(
     const ParsedPermissionsPolicyDeclaration& rhs) = default;
+
+bool ParsedPermissionsPolicyDeclaration::Contains(
+    const url::Origin& origin) const {
+  return matches_all_origins || (matches_opaque_src && origin.opaque()) ||
+         base::Contains(allowed_origins, origin);
+}
 
 ParsedPermissionsPolicyDeclaration::~ParsedPermissionsPolicyDeclaration() =
     default;
