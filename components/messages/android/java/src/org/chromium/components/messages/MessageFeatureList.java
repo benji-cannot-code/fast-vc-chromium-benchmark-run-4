a26739f62b7f48b1fd424ae2a37efde6a5156ecb
Lijin Shen <lazzzis@google.com>
2022-09-13 17:44:00 +0000

    [Messages] Support multiple views on message container
    1. Add a feature list api for querying status of flags
    2. when stacking animation is enabled, at most 2 views can be added
    on message container.
    3. add elevation property to ensure front message is shown above
    the back one.
    
    Bug: 1200974
    Change-Id: Ib87d9821922f1e6f2a36ddbc1d9da44c15b57609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869179
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046442}

---
+++ components/messages/android/java/src/org/chromium/components/messages/MessageFeatureList.java
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.messages;
+
+import org.chromium.base.FeatureList;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.MainDex;
+import org.chromium.base.annotations.NativeMethods;
+
+/**
+ * Provides an API for querying the status of Message features.
+ */
+@JNINamespace("messages")
+@MainDex
+public class MessageFeatureList {
+    public static final String MESSAGES_FOR_ANDROID_STACKING_ANIMATION =
+            "MessagesForAndroidStackingAnimation";
+
+    private MessageFeatureList() {}
+
+    public static boolean isEnabled(String featureName) {
+        Boolean testValue = FeatureList.getTestValueForFeature(featureName);
+        if (testValue != null) return testValue;
+        assert FeatureList.isNativeInitialized();
+        return MessageFeatureListJni.get().isEnabled(featureName);
+    }
+
+    public static boolean isStackAnimationEnabled() {
+        return isEnabled(MESSAGES_FOR_ANDROID_STACKING_ANIMATION);
+    }
+
+    @NativeMethods
+    interface Natives {
+        boolean isEnabled(String featureName);
+    }
+}
--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -5,9 +5,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/messages/android/messages_feature.h"
 
+#include "base/android/jni_string.h"
+#include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "components/messages/android/jni_headers/MessageFeatureList_jni.h"
+
+using base::android::ConvertJavaStringToUTF8;
+using base::android::JavaParamRef;
 
 namespace messages {
+
+namespace {
+
+const base::Feature* kFeaturesExposedToJava[] = {
+    &kMessagesForAndroidStackingAnimation,
+};
+
+const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
+  for (const base::Feature* feature : kFeaturesExposedToJava) {
+    if (feature->name == feature_name)
+      return feature;
+  }
+  NOTREACHED() << "Queried feature not found in MessageFeatureList: "
+               << feature_name;
+  return nullptr;
+}
+
+}  // namespace
 
 const base::Feature kMessagesForAndroidAdsBlocked{
     "MessagesForAndroidAdsBlocked", base::FEATURE_ENABLED_BY_DEFAULT};
@@ -153,6 +177,14 @@ bool UseGPayIconForSaveCardMessage() {
 
 bool UseDialogV2ForSaveCardMessage() {
   return kMessagesForAndroidSaveCard_UseDialogV2.Get();
+}
+
+static jboolean JNI_MessageFeatureList_IsEnabled(
+    JNIEnv* env,
+    const JavaParamRef<jstring>& jfeature_name) {
+  const base::Feature* feature =
+      FindFeatureExposedToJava(ConvertJavaStringToUTF8(env, jfeature_name));
+  return base::FeatureList::IsEnabled(*feature);
 }
 
 }  // namespace messages
