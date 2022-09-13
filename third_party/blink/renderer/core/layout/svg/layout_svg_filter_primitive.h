4018ee13c020b9bc20dd77129ec03c758496c3dd
Fredrik Söderquist <fs@opera.com>
2022-09-13 18:46:17 +0000

    Invalidate filter chain when detaching filter primitive LayoutObject
    If the LayoutObject for a filter primitive was detached (by for example
    setting 'display' to 'none' or 'contents'), and then said primitive was
    mutated while still holding a, supposed valid, FilterEffect, the
    mutation could cause a crash while accessing its ComputedStyle.
    
    Invalidate the filter chain when a filter primitive is detached from the
    layout tree to make sure the cached chain state is consistent.
    
    Fixed: 1274176
    Change-Id: Iad3b36afbd11397e7fa0c2297fb575b506c4486a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894111
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046479}

--- third_party/blink/renderer/core/layout/svg/layout_svg_filter_primitive.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_filter_primitive.h
@@ -44,6 +44,7 @@ class LayoutSVGFilterPrimitive final : public LayoutObject {
     return false;
   }
 
+  void WillBeDestroyed() override;
   void StyleDidChange(StyleDifference, const ComputedStyle*) override;
   void UpdateLayout() override;
 
