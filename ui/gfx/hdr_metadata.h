67791a8db3ac2806af7ab11d9ea9d50720359acb
Christopher Cameron <ccameron@chromium.org>
2022-09-13 19:58:46 +0000

    gfx::HDRMetadata: Add ToString method
    This is often helpful for debugging.
    
    Change-Id: I7213f7fe22b73d6989ba89485b5b7e69f56d2b02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894110
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046522}

--- ui/gfx/hdr_metadata.h
+++ ui/gfx/hdr_metadata.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_HDR_METADATA_H_
 #define UI_GFX_HDR_METADATA_H_
 
+#include <string>
+
 #include "ui/gfx/color_space_export.h"
 #include "ui/gfx/geometry/point_f.h"
 
@@ -24,6 +26,8 @@ struct COLOR_SPACE_EXPORT ColorVolumeMetadata {
   ColorVolumeMetadata();
   ColorVolumeMetadata(const ColorVolumeMetadata& rhs);
   ColorVolumeMetadata& operator=(const ColorVolumeMetadata& rhs);
+
+  std::string ToString() const;
 
   bool operator==(const ColorVolumeMetadata& rhs) const {
     return ((primary_r == rhs.primary_r) && (primary_g == rhs.primary_g) &&
@@ -52,6 +56,8 @@ struct COLOR_SPACE_EXPORT HDRMetadata {
              (max_frame_average_light_level == 0) &&
              (color_volume_metadata == ColorVolumeMetadata()));
   }
+
+  std::string ToString() const;
 
   bool operator==(const HDRMetadata& rhs) const {
     return (
