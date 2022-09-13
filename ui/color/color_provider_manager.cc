69527cc7f7f5383122d96f544ec95d1d0dc227f1
Sean Kau <skau@chromium.org>
2022-09-13 19:36:09 +0000

    Add a histogram for ColorProviderManager cache size
    To determine how big the cache should be, add a histogram. For lack of
    a better idea, histogram is under the new category Views.*
    
    Bug: 1351845
    Change-Id: I66e73d0fb148175e87e74933a9062617dbadf385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824045
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046509}

--- ui/color/color_provider_manager.cc
+++ ui/color/color_provider_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "build/build_config.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -148,6 +149,8 @@ ColorProvider* ColorProviderManager::GetColorProviderFor(Key key) {
 
     provider->GenerateColorMap();
     iter = color_providers_.Put(key, std::move(provider));
+    base::UmaHistogramExactLinear("Views.ColorProviderCacheSize",
+                                  color_providers_.size(), kCacheSize);
   }
   ColorProvider* provider = iter->second.get();
   DCHECK(provider);
