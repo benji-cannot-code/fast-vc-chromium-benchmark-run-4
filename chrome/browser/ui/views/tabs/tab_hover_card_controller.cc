c73515b594f09352c5465a451eb7c674ed953543
Keren Zhu <kerenzhu@chromium.org>
2022-09-13 21:46:47 +0000

    Revert "Use widget sublevel for permission and tab preview bubbles"
    This reverts commit a5133d121e3e5f5759d57eb477bff6954c0b85c1.
    
    Reason for revert: this is suspected to cause the crash
    crbug.com/1362267.
    
    Original change's description:
    > Use widget sublevel for permission and tab preview bubbles
    >
    > * Add ChromeWidgetSublevel and three semantic z-order sublevels.
    > * Replace explicit StackAbove() and StackAtTop() calls with semantic
    > widget sublevels for permission and tab preview bubbles.
    >
    > This is not be a behavior change.
    > The code is gated under views::features::kWidgetLayering.
    >
    > Bug: 1355904
    > Change-Id: I12aa17ea178632c96ea4024e09d42155eb368320
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876324
    > Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044628}
    
    Bug: 1355904, 1362267
    Change-Id: I7905364bf5415d55572c4d79a9cda0e563f5f5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891085
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046589}

--- chrome/browser/ui/views/tabs/tab_hover_card_controller.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_controller.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/tab_count_metrics.h"
 #include "chrome/browser/ui/tabs/tab_style.h"
 #include "chrome/browser/ui/ui_features.h"
-#include "chrome/browser/ui/views/chrome_widget_sublevel.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h"
@@ -36,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/types/event_type.h"
 #include "ui/views/event_monitor.h"
 #include "ui/views/view.h"
-#include "ui/views/views_features.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
 
@@ -75,11 +73,6 @@ GetMemoryPressureOverride() {
 // Fetches the Omnibox drop-down widget, or returns null if the drop-down is
 // not visible.
 void FixWidgetStackOrder(views::Widget* widget, const Browser* browser) {
-  if (base::FeatureList::IsEnabled(views::features::kWidgetLayering)) {
-    widget->SetZOrderSublevel(ChromeWidgetSublevel::kSublevelHoverable);
-    return;
-  }
-
 #if BUILDFLAG(IS_LINUX)
   // Ensure the hover card Widget assumes the highest z-order to avoid occlusion
   // by other secondary UI Widgets (such as the omnibox Widget, see
