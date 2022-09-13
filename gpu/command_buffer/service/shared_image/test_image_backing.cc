4573ba76abf977864751bbb236dd3af3ed117773
Joel Hockey <joelhockey@chromium.org>
2022-09-13 05:29:35 +0000

    Revert "Make compound backing work with D3DImageBacking"
    This reverts commit 153b4796e9c2aa05a377d655d4e0d4af2723a993.
    
    Reason for revert: crbug.com/1362930
    This is causing corrupted lacros window on ash-chrome +linux environment, which is very important for development.
    
    Original change's description:
    > Make compound backing work with D3DImageBacking
    >
    > Removes the shared memory segment support from D3DImageBacking and use
    > compound backing instead. D3DImageBacking supports readback from texture
    > to memory so add SharedImageBacking::ReadbackToMemory() for compound
    > backing to use. The CopyToGpuMemoryBuffer() implementation is moved to
    > compound backing as well.
    >
    > Bug: 1293509
    > Change-Id: Iaed0df60be5f629a54c62a36ce952f926d9c65d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852364
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044702}
    
    Bug: 1293509
    Change-Id: I8e8916c260914a0b9b159b43a5491999ba4c59c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892290
    Owners-Override: Fergus Dall <sidereal@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046214}

--- gpu/command_buffer/service/shared_image/test_image_backing.cc
+++ gpu/command_buffer/service/shared_image/test_image_backing.cc
@@ -225,10 +225,6 @@ bool TestImageBacking::GetUploadFromMemoryCalledAndReset() {
   return std::exchange(upload_from_memory_called_, false);
 }
 
-bool TestImageBacking::GetReadbackToMemoryCalledAndReset() {
-  return std::exchange(readback_to_memory_called_, false);
-}
-
 SharedImageBackingType TestImageBacking::GetType() const {
   return SharedImageBackingType::kTest;
 }
@@ -243,11 +239,6 @@ void TestImageBacking::SetClearedRect(const gfx::Rect& cleared_rect) {
 
 bool TestImageBacking::UploadFromMemory(const SkPixmap& pixmap) {
   upload_from_memory_called_ = true;
-  return true;
-}
-
-bool TestImageBacking::ReadbackToMemory(SkPixmap& pixmap) {
-  readback_to_memory_called_ = true;
   return true;
 }
 
