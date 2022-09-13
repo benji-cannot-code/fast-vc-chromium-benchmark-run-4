111adf7ac277c0ed0d0e01d197e66a47fbd224a2
jhtin <jhtin@chromium.org>
2022-09-13 10:36:52 +0000

    longpress diacritics: Denylist systemtextapp
    __SIMPLE_REVIEW__
    
    Bug: b/246207633
    Change-Id: I55a4fc7d8f30d2ed27a14f8dc5e85aa7afb1f7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891524
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046299}

--- chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
+++ chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
@@ -147,6 +147,7 @@ const char* kDeniedAppsForDiacritics[] = {
     "iodihamcpbpeioajjeobimgagajmlibd",  // SSH app
     "cgfnfgkafmcdkdgilmojlnaadileaach",  // Crosh app
     "fhicihalidkgcimdmhpohldehjmcabcf",  // Terminal app
+    "mmfbcljfglbokpmkimbfghdkjmjhdgbg",  // System text
 };
 
 bool IsTestUrl(GURL url) {
