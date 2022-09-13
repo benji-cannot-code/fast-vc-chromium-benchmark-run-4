27edeea27db59eb757eedf836152fe05df5eaa47
Aaron Krajeski <aaronhk@chromium.org>
2022-09-13 20:28:53 +0000

    Parse and serialize color spaces for CSS gradients
    go/css-color-4-interpolation
    https://www.w3.org/TR/css-color-4/#interpolation
    
    CSS Gradients can have their color spaces defined directly using the "in" syntax.
    
    "OKLab" is not serialized for new color inputs. "sRGB" is not serialized for legacy colors.
    
    I'll do the <hue-interpolation-method>s next. This should gradient-interpolation-method tests totally passing.
    
    Bug: 1362022
    Change-Id: I77a2c4929df3f9a4c6c655d73715481c34fa1510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888083
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046541}

--- third_party/blink/renderer/core/css/css_gradient_value.h
+++ third_party/blink/renderer/core/css/css_gradient_value.h
@@ -122,6 +122,11 @@ class CSSGradientValue : public CSSImageGeneratorValue {
 
   void TraceAfterDispatch(blink::Visitor*) const;
 
+  void SetColorInterpolationSpace(Color::ColorInterpolationSpace cs) {
+    color_interpolation_space_ = cs;
+  }
+  bool ShouldSerializeColorSpace() const;
+
   struct GradientDesc;
 
  protected:
@@ -153,6 +158,8 @@ class CSSGradientValue : public CSSImageGeneratorValue {
   CSSGradientType gradient_type_;
   bool repeating_ : 1;
   bool is_cacheable_ : 1;
+  Color::ColorInterpolationSpace color_interpolation_space_ =
+      Color::ColorInterpolationSpace::kNone;
 };
 
 class CSSLinearGradientValue final : public CSSGradientValue {
