215d24edba44fc91d8253cb7bcc32dd780188619
Yue Li <updowndota@google.com>
2022-09-13 18:16:44 +0000

    Avatars: Cleanup dead code in default_user_images
    Bug: b/244369996
    Test: Run existing tests
    Change-Id: I82aafa7b5f0d48ade25ddc648221dc6f3ab32a55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892852
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046462}

--- chrome/browser/ash/login/users/default_user_image/default_user_images_unittest.cc
+++ chrome/browser/ash/login/users/default_user_image/default_user_images_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/users/default_user_image/default_user_images.h"
 
+#include "ash/public/cpp/default_user_image.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -12,21 +13,17 @@ namespace ash {
 namespace default_user_image {
 
 TEST(DefaultUserImagesTest, CurrentImageSetShouldBeEligible) {
-  base::Value::List current_default_images =
-      default_user_image::GetCurrentImageSetAsListValue();
+  std::vector<DefaultUserImage> current_default_images =
+      default_user_image::GetCurrentImageSet();
 
-  for (auto& image_data : current_default_images) {
-    const auto index = image_data.FindIntPath("index");
-    EXPECT_TRUE(index.has_value());
-    EXPECT_TRUE(IsValidIndex(index.value()));
-    EXPECT_TRUE(IsInCurrentImageSet(index.value()));
+  for (auto& image : current_default_images) {
+    const auto index = image.index;
+    EXPECT_TRUE(IsValidIndex(index));
+    EXPECT_TRUE(IsInCurrentImageSet(index));
 
-    const auto* url = image_data.FindStringPath("url");
-    EXPECT_TRUE(url);
-    EXPECT_EQ(GetDefaultImageUrl(index.value()).spec(), url->c_str());
-
-    const auto* title = image_data.FindStringPath("title");
-    EXPECT_TRUE(title);
+    const auto default_user_image = GetDefaultUserImage(index);
+    EXPECT_EQ(default_user_image.url, image.url);
+    EXPECT_EQ(default_user_image.title, image.title);
   }
 }
 
