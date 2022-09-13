291a9087b8114f76ebc70e69107bc1b1aadc19f8
Changhao Han <changhaohan@chromium.org>
2022-09-13 16:10:47 +0000

    Check local iframes for top layer elements
    Fixed: 1359976
    Change-Id: I5256601706be8acfa84cae21271cc1c59d241044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890922
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Auto-Submit: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046401}

--- third_party/blink/web_tests/inspector-protocol/dom/dom-getTopLayerElements.js
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-getTopLayerElements.js
@@ -1,17 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-    const {page, session, dp} = await testRunner.startHTML(`
-    <button id="dialogButton1" onclick="window.dialog1.showModal();">Open Dialog</button>
-    <dialog id="dialog1">
-        <button id="dialogButton2" onclick="window.dialog2.showModal();">Open Dialog</button>
-    </dialog>
-    <dialog id="dialog2"></dialog>
-  `, 'Tests DOM.getTopLayerElements command.');
+    const {page, session, dp} = await testRunner.startURL('resources/dom-get-top-layer-elements.html', 'Tests DOM.getTopLayerElements command.');
 
   await dp.DOM.enable();
   await dp.DOM.getDocument();
   await dp.Runtime.evaluate({ expression: `document.getElementById('dialogButton1').click()`});
   await dp.Runtime.evaluate({ expression: `document.getElementById('dialogButton2').click()`});
+  await dp.Runtime.evaluate({ expression: `
+    const iframe = document.querySelector('iframe');
+    iframe.contentWindow.iframeDialog.showModal();
+  `});
 
   const nodesResponse = await dp.DOM.getTopLayerElements();
 
