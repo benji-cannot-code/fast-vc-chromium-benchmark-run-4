9bdaf56f94311a01381d8094cb2613ee6c3cabed
Yongshun Liu <yongshun@chromium.org>
2022-09-13 18:24:14 +0000

    saved_desks: Rework of the app icon sorting code for default icons
    This CL optimizes the sorting part of the app icon container. Sorting
    code is moved to the container level instead of any icon messes up with
    its siblings. When an icon is loaded, it notifies the container to
    re-layout to reflect the correct order and visibility. In addition, the
    new sorting code is designed in a way that preserves the original order
    for non-default icons, i.e. the order that each application is added
    when saving a desk. In addition, it now prioritizes non-default icons,
    i.e. if there is not enough width and it will now try to hide default
    icons first. This is done by loading all icons instead of just loading
    `kMaxIcons` icons and then refusing to process the rest.
    
    `CalculatePreferredSize()` has been fixed for overflow icon so that the
    container has the right width to calculate.
    
    It also fixes the crash when the app is not available on device. This is
    because `SavedDeskIconView::MoveIconViewToBack()` might be called at
    initialization time, and therefore has not been tied to a parent yet.
    
    LOW_COVERAGE_REASON=Fix the crash asap and consolidate/add more tests later.
    
    Test: Manual && SavedDeskTest.*
    Bug: 1361657
    Change-Id: If0590c7b7fd5750012e7276d4209520171d00fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885230
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046468}

--- ash/wm/desks/templates/saved_desk_icon_container.h
+++ ash/wm/desks/templates/saved_desk_icon_container.h
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_DESKS_TEMPLATES_SAVED_DESK_ICON_CONTAINER_H_
 #define ASH_WM_DESKS_TEMPLATES_SAVED_DESK_ICON_CONTAINER_H_
 
+#include <cstddef>
 #include <string>
 #include <utility>
 #include <vector>
 
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/box_layout_view.h"
+#include "ui/views/view.h"
 
 namespace ui {
 class ColorProvider;
@@ -73,15 +75,36 @@ class SavedDeskIconContainer : public views::BoxLayoutView {
   void Layout() override;
 
  private:
+  // Sort icons to the expected order. When it's necessary or `force_update` is
+  // true, update the overflow icon.
+  void SortIconsAndUpdateOverflowIcon();
+
+  // Move all default icons to the back but before the overflow icon. Sorting is
+  // stable for non-default icons, i.e. it preserves the original order. Return
+  // true if any icons are moved to the end; if no change, return false.
+  void MoveDefaultIconsToBack();
+
+  // Update icon visibility and the overflow icon depending on the available
+  // space.
+  void UpdateOverflowIcon();
+
   // Given a sorted vector of pairs of icon identifier and icon info, create
   // views for them.
   void CreateIconViewsFromIconIdentifiers(
       const std::vector<IconIdentifierAndIconInfo>&
           icon_identifier_to_icon_info);
 
+  // Pointer of the overflow icon view.
+  views::View* overflow_icon_view_ = nullptr;
+
+  // Number of apps that are not shown as icons in the container.
+  int uncreated_app_count_ = 0;
+
   // If `this` is created with an incognito window, store the ui::ColorProvider
   // of one of the incognito windows to retrieve its icon's color.
   const ui::ColorProvider* incognito_window_color_provider_ = nullptr;
+
+  base::WeakPtrFactory<SavedDeskIconContainer> weak_ptr_factory_{this};
 };
 
 BEGIN_VIEW_BUILDER(/* no export */,
