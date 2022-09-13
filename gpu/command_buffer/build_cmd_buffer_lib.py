3c8695c502c1058d46ff4fcefd51b5825b037d32
Avi Drissman <avi@chromium.org>
2022-09-13 21:25:34 +0000

    Update copyright headers in gpu/ (and generated ppapi/ files)
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c34.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I30614b78d270d4fc3e347b925efa26033eacd472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891044
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046576}

--- gpu/command_buffer/build_cmd_buffer_lib.py
+++ gpu/command_buffer/build_cmd_buffer_lib.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2018 The Chromium Authors. All rights reserved.
+# Copyright 2018 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Common code generator for command buffers."""
@@ -16,7 +16,7 @@ _SIZE_OF_UINT32 = 4
 _SIZE_OF_COMMAND_HEADER = 4
 _FIRST_SPECIFIC_COMMAND_ID = 256
 
-_LICENSE = """// Copyright %s The Chromium Authors. All rights reserved.
+_LICENSE = """// Copyright %s The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
--- gpu/config/build_workaround_header.py
+++ gpu/config/build_workaround_header.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python3
-# Copyright (c) 2018 The Chromium Authors. All rights reserved.
+# Copyright 2018 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """code generator for gpu workaround definitions"""
@@ -10,7 +10,7 @@ import os
 import sys
 import typing
 
-_LICENSE = """// Copyright 2018 The Chromium Authors. All rights reserved.
+_LICENSE = """// Copyright 2018 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
