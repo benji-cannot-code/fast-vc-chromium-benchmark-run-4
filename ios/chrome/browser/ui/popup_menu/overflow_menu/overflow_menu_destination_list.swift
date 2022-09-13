c895601a7bfb898ebcf8706d3acf49e1a7138baa
Robbie Gibson <rkgibson@google.com>
2022-09-13 07:36:38 +0000

    [iOS] Prevent extraneous animations in overflow menu
    SwiftUI's `.animation()` modifier applies the animation to the entire
    view, which was causing unwanted animations for things like scrolling.
    Instead, adding an `.animation(nil)` modifier before the property we
    want to animate causes only that property to animate. Any earlier
    properties and views are stopped by the nil animation.
    
    Bug: 1362551
    Change-Id: I31b274a93f70baa8d93e3f23d0a40f1f3a8825bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891175
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046247}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_list.swift
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_list.swift
@@ -102,6 +102,7 @@ struct OverflowMenuDestinationList: View {
             }
           }
         }
+        .animation(nil)
         .background(uiConfiguration.highlightDestinationsRow ? Color.blueHalo : Color.clear)
         .animation(.linear(duration: kMaterialDuration3))
         .coordinateSpace(name: Constants.coordinateSpaceName)
