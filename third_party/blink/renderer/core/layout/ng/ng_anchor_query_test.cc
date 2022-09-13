0e1cb9a67d6409151857e22b23b60c3e65eeeeee
Koji Ishii <kojii@chromium.org>
2022-09-13 06:35:57 +0000

    [anchor-position] Exit tests if |!LayoutNGEnabled|
    Somehow |ScopedLayoutNGForTest| doesn't seem to work on the
    `linux_layout_tests_layout_ng_disabled` bots.
    
    This patch disables the anchor positioning tests if LayoutNG
    is not enabled.
    
    Fixed: 1362388
    Change-Id: I83b750e781cda10abdfd60153a40037d64022ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891523
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046229}

--- third_party/blink/renderer/core/layout/ng/ng_anchor_query_test.cc
+++ third_party/blink/renderer/core/layout/ng/ng_anchor_query_test.cc
@@ -74,6 +74,8 @@ std::ostream& operator<<(std::ostream& os, const AnchorTestData& value) {
 }
 
 TEST_F(NGAnchorQueryTest, AnchorNameAdd) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -107,6 +109,8 @@ TEST_F(NGAnchorQueryTest, AnchorNameAdd) {
 }
 
 TEST_F(NGAnchorQueryTest, AnchorNameChange) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -144,6 +148,8 @@ TEST_F(NGAnchorQueryTest, AnchorNameChange) {
 }
 
 TEST_F(NGAnchorQueryTest, AnchorNameRemove) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -179,6 +185,8 @@ TEST_F(NGAnchorQueryTest, AnchorNameRemove) {
 
 // https://tabatkins.github.io/specs/css-anchor-position/#determining
 TEST_F(NGAnchorQueryTest, AnchorNameValid) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <div id="container" style="position: relative">
       <div id="static1">
@@ -220,6 +228,8 @@ TEST_F(NGAnchorQueryTest, AnchorNameValid) {
 }
 
 TEST_F(NGAnchorQueryTest, BlockFlow) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -264,6 +274,8 @@ TEST_F(NGAnchorQueryTest, BlockFlow) {
 }
 
 TEST_F(NGAnchorQueryTest, Inline) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   LoadAhem();
   SetBodyInnerHTML(R"HTML(
     <style>
@@ -323,6 +335,8 @@ TEST_F(NGAnchorQueryTest, Inline) {
 }
 
 TEST_F(NGAnchorQueryTest, OutOfFlow) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -350,6 +364,8 @@ TEST_F(NGAnchorQueryTest, OutOfFlow) {
 
 // Relative-positioning should shift the rectangles.
 TEST_F(NGAnchorQueryTest, Relative) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -371,6 +387,8 @@ TEST_F(NGAnchorQueryTest, Relative) {
 
 // CSS Transform should not shift the rectangles.
 TEST_F(NGAnchorQueryTest, Transform) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -392,6 +410,8 @@ TEST_F(NGAnchorQueryTest, Transform) {
 
 // Scroll positions should not shift the rectangles.
 TEST_F(NGAnchorQueryTest, Scroll) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
@@ -416,6 +436,8 @@ TEST_F(NGAnchorQueryTest, Scroll) {
 }
 
 TEST_F(NGAnchorQueryTest, FragmentedContainingBlock) {
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   SetBodyInnerHTML(R"HTML(
     <style>
     html, body {
