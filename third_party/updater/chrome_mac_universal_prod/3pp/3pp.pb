fdbca9f25f2892416967d587719dfb5ee4cfed6e
Joshua Pawlicki <waffles@chromium.org>
2022-09-13 17:54:59 +0000

    Updater: Stage GoogleUpdater zip to CIPD instead of UpdaterSetup.
    UpdaterSetup has some notarization problems, so embedding the app bundle
    is preferable.
    
    Bug: 1336630
    Change-Id: I12f2eeb9d914b037f557e1b4cfd55a2d9db9598c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891892
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046450}

--- third_party/updater/chrome_mac_universal_prod/3pp/3pp.pb
+++ third_party/updater/chrome_mac_universal_prod/3pp/3pp.pb
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 create {
   source {
     script { name: "fetch.py" }
+    unpack_archive: true
+    no_archive_prune: true
   }
   build {
   }
--- third_party/updater/chrome_mac_universal_prod/3pp/fetch.py
+++ third_party/updater/chrome_mac_universal_prod/3pp/fetch.py
@@ -12,7 +12,7 @@ import sys
 import urllib.request
 
 # TODO(crbug.com/1336630): This is compared lexically. Remove it before M1000.
-MIN_VERSION = '106.0.5243.0'
+MIN_VERSION = '107.0.5298.0'
 
 def fetch():
     """
@@ -43,10 +43,10 @@ def get_url():
         json.dumps({
             'url': [
                 'https://edgedl.me.gvt1.com/edgedl/release2/182l0/latest/'
-                '%s_UpdaterSetup' % os.environ['_3PP_VERSION']
+                'GoogleUpdater-%s.zip' % os.environ['_3PP_VERSION']
             ],
-            'ext': '',
-            'name': ['UpdaterSetup']
+            'ext': '.zip',
+            'name': ['GoogleUpdater-%s.zip' % os.environ['_3PP_VERSION']]
         }))
 
 
