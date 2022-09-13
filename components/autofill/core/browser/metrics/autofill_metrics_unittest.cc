f61f6fc8c1d759dff09cc4626aada2ea2f48d6d6
Arthur Hemery <ahemery@chromium.org>
2022-09-13 07:40:12 +0000

    Revert "Reland: "[Autofill] Enable AutofillParseAsync in some tests.""
    This reverts commit 478833e0f3dfdbffd01f0587e8a84e03e9d85aeb.
    
    Reason for revert: test failures, see https://crbug.com/1363051
    
    Original change's description:
    > Reland: "[Autofill] Enable AutofillParseAsync in some tests."
    >
    > This is a reland of commit 9c6e7a6c7b469cca490868424ff287403f47b972.
    >
    > The CL broke tests in autofill_browsertest.cc and
    > autofill_manager_unittest.cc. Fixes of these tests landed in
    > crrev.com/c/3888165, crrev.com/c/3888166.
    >
    > Original change's description:
    > > This CL adds a fieldtrial testing config for asynchronous form parsing
    > > and enables the feature in several unit tests.
    > >
    > > Bug: 1309848, 1345089
    > > Change-Id: I1c2efe9aff6ab61d05b2316e3673e574aaf2f9ae
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3798546
    > > Commit-Queue: Christoph Schwering <schwering@google.com>
    > > Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1042851}
    >
    > Bug: 1309848, 1345089
    > Change-Id: I70ba79c3b0a88d56513f6d4362f5d308ceecfe91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3870617
    > Commit-Queue: Christoph Schwering <schwering@google.com>
    > Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1046040}
    
    Bug: 1309848, 1345089
    Change-Id: I1849c10784963e63b50fb63e839f1c430de1f0e1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890983
    Owners-Override: Arthur Hemery <ahemery@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046248}

--- components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
@@ -471,8 +471,7 @@ TEST_F(AutofillMetricsTest, NumberOfAutofilledFieldsAtSubmission) {
 // autocomplete attribute at submission time.
 TEST_F(AutofillMetricsTest,
        NumberOfAutofilledFieldsWithAutocompleteUnrecognizedAtSubmission) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
+  scoped_feature_list_.InitAndEnableFeature(
       features::kAutofillFillAndImportFromMoreFields);
   // Set up our form data with two autofilled fields.
   FormData form =
@@ -4433,8 +4432,7 @@ TEST_P(AutofillMetricsIFrameTest,
 
 // Test that we log preflight calls for credit card unmasking.
 TEST_F(AutofillMetricsTest, CreditCardUnmaskingPreflightCall) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
+  scoped_feature_list_.InitAndEnableFeature(
       features::kAutofillCreditCardAuthentication);
   std::string preflight_call_metric =
       "Autofill.BetterAuth.CardUnmaskPreflightCalled";
@@ -7157,8 +7155,7 @@ TEST_F(AutofillMetricsTest, DaysSinceLastUse_Profile) {
 
 // Test that we log the verification status of name tokens.
 TEST_F(AutofillMetricsTest, LogVerificationStatusesOfNameTokens) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
+  scoped_feature_list_.InitAndEnableFeature(
       features::kAutofillEnableSupportForMoreStructureInNames);
 
   base::HistogramTester histogram_tester;
@@ -7202,8 +7199,7 @@ TEST_F(AutofillMetricsTest, LogVerificationStatusesOfNameTokens) {
 
 // Test that we log the verification status of address tokens..
 TEST_F(AutofillMetricsTest, LogVerificationStatusesOfAddressTokens) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
+  scoped_feature_list_.InitAndEnableFeature(
       features::kAutofillEnableSupportForMoreStructureInAddresses);
 
   base::HistogramTester histogram_tester;
