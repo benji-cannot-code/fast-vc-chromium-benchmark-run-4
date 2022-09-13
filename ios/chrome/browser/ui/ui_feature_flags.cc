2fe94bfbfa1828684a49db8104672522eefb6884
Benjamin Williams <bwwilliams@google.com>
2022-09-13 20:01:26 +0000

    Refactor Restore Shorty feature flag.
    Moves flag kIOSNewPostRestoreExperience from
    ios/chrome/browser/ui/ui_feature_flags.h to
    ios/chrome/browser/ui/post_restore_signin/features.h.
    
    Introduces helper method, PostRestoreSignInType(), to determine which
    Post Restore Sign-in Promo to show.
    
    PRD: go/restore-shorty-prd
    Design: go/restore-shorty-dd
    
    Bug: 1362029
    Change-Id: I6b688eb2b9e6a8a87f6efe4576d8c283c101e7a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894197
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046525}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -95,8 +95,3 @@ const base::Feature kMapsExperienceKit{"MapsExperienceKit",
 
 const base::Feature kEnableMiniMap{"EnableMiniMap",
                                    base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kIOSNewPostRestoreExperience{
-    "IOSNewPostRestoreExperience", base::FEATURE_DISABLED_BY_DEFAULT};
-const char kIOSNewPostRestoreExperienceParam[] =
-    "ios-new-post-restore-experience";
