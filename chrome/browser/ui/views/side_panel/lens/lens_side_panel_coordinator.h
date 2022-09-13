180ba922cdba1dfa740e6ab5fd2dcbff622b05af
Anudeep Palanki <apalanki@google.com>
2022-09-13 20:03:09 +0000

    Reland "Unified Side Panel: Add image search key/value to json struct."
    This reverts commit eb9de038b9d60fd365329bb1480cd2b2e115fae0.
    
    Reason for revert: Fixing the broken UT's
    
    Original change's description:
    > Revert "Unified Side Panel: Add image search key/value to json struct."
    >
    > This reverts commit dec15ed8f48265ad21bbd4ec4463a5e4c3372345.
    >
    > Reason for revert: breaks on linux-chromeos-chrome, see https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/24737/overview
    >
    > Original change's description:
    > > Unified Side Panel: Add image search key/value to json struct.
    > >
    > > Added a new image search param to the json file and will use that to direct image search and region search requests to the unified side panel for search engines.
    > >
    > > Design Doc: go/unified-side-panel-non-google-dse
    > >
    > > - Direct 3P DSE requests to side panel only if the image search param exists and the unified side panel is turned on.
    > > - If the unified side panel flag is turned off, the 3P DSE requests are redirected to a new tab.
    > > - For lens requests, standardize the way rendering env variable query param gets added.
    > > - Use default vector image search icon for the 3P DSE's.
    > > - Changing Search Engine when side panel is opened closes side panel and  de-registers lens.
    > >
    > > For testing purposes, enabled side panel for Bing search engine and here are the demos:
    > >
    > > Region Search: https://drive.google.com/file/d/1wKde02oZL6EeFCJYwdPLfKo_p9qST-mE/view
    > >
    > > Image Search: https://drive.google.com/file/d/116hIj5fWda_hKZ_2VHa-L50gNWMsjAg9/view
    > >
    > > Changing Search Engine: https://drive.google.com/file/d/1lgO7yUV-pA6jpSXH6gZdFVT7-vGfqXh3/view
    > >
    > > NOTE: The existing browser tests for the lens unified side panel are broken, so did not add new browser tests for 3P DSE's, we will add them when fixing the browser tests in b/242909164.
    > >
    > > Bug: b/226186680
    > > Change-Id: I268eb7d673e02d6a0a77838280f1c7014367f314
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866097
    > > Commit-Queue: Anudeep Palanki <apalanki@google.com>
    > > Reviewed-by: Ali Stanfield <stanfield@google.com>
    > > Reviewed-by: Evan Stade <estade@chromium.org>
    > > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1045182}
    >
    > Bug: b/226186680
    > Change-Id: I2bb922204f51bdb012eb6c0be120f76e1029e8c2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887281
    > Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Dominique Fauteux-Chapleau <domfc@google.com>
    > Auto-Submit: Dominique Fauteux-Chapleau <domfc@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1045241}
    
    Bug: b/226186680
    Change-Id: I06106a90ce6fe289c6b87c004acea1c44f6fcf15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887228
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Anudeep Palanki <apalanki@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046528}

--- chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/lens/lens_unified_side_panel_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_view_state_observer.h"
+#include "components/search_engines/template_url_service_observer.h"
 
 class Browser;
 
@@ -22,7 +23,8 @@ class Browser;
 class LensSidePanelCoordinator
     : public BrowserUserData<LensSidePanelCoordinator>,
       public SidePanelViewStateObserver,
-      public SidePanelEntryObserver {
+      public SidePanelEntryObserver,
+      public TemplateURLServiceObserver {
  public:
   explicit LensSidePanelCoordinator(Browser* browser);
   LensSidePanelCoordinator(const LensSidePanelCoordinator&) = delete;
@@ -46,7 +48,19 @@ class LensSidePanelCoordinator
   void OnEntryShown(SidePanelEntry* entry) override;
   void OnEntryHidden(SidePanelEntry* entry) override;
 
+  bool IsDefaultSearchProviderGoogle();
+
+  std::u16string GetComboboxLabel();
+
+  const gfx::VectorIcon& GetComboboxIcon();
+
   BrowserView* GetBrowserView();
+
+  // Removes the lens entry from the side panel.
+  void DeregisterLensFromSidePanel();
+
+  // TemplateURLServiceObserver
+  void OnTemplateURLServiceChanged() override;
 
   // SidePanelViewStateObserver
   void OnSidePanelDidClose() override;
@@ -54,6 +68,7 @@ class LensSidePanelCoordinator
   std::unique_ptr<views::View> CreateLensWebView(
       const content::OpenURLParams& params);
 
+  raw_ptr<TemplateURLService> template_url_service_;
   base::WeakPtr<lens::LensUnifiedSidePanelView> lens_side_panel_view_;
 
   BROWSER_USER_DATA_KEY_DECL();
--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -165,6 +165,10 @@ bool IsLensSidePanelEnabled() {
          kEnableSidePanelForLens.Get();
 }
 
+bool IsLensSidePanelEnabledForRegionSearch() {
+  return IsLensSidePanelEnabled() && !IsLensFullscreenSearchEnabled();
+}
+
 bool GetSendImagesAsPng() {
   return base::FeatureList::IsEnabled(kLensStandalone) &&
          base::FeatureList::IsEnabled(kLensTransparentImagesFix);
