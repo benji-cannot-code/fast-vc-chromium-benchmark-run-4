9c59b38b9bf8d6cebdd70ca191edf7c203dd1808
Matteo Ruello <mruel@google.com>
2022-09-14 09:42:58 +0000

    Fix JITT not triggering on JS navigation
    Bug: b/243897243
    Change-Id: I9e4438d65cc65687ca99dc98bc49e30ad6612c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867608
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Matteo Ruello <mruel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046818}

--- components/autofill_assistant/browser/starter.h
+++ components/autofill_assistant/browser/starter.h
@@ -75,6 +75,10 @@ class Starter : public content::WebContentsObserver,
           preconditions_checked_callback);
 
   // content::WebContentsObserver:
+  // Only one function will execute, the other will early return based on the
+  // AutofillAssistantUseDidFinishNavigation feature.
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override;
   void PrimaryPageChanged(content::Page& page) override;
 
   // Invoked when the tab interactability has changed.
@@ -173,8 +177,7 @@ class Starter : public content::WebContentsObserver,
   // successful.
   void ReportPreconditionsChecked(bool start_script);
 
-  void RecordNavigatedAwayMetrics(content::Page& page,
-                                  ukm::SourceId source_id,
+  void RecordNavigatedAwayMetrics(ukm::SourceId source_id,
                                   bool is_error_document) const;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
