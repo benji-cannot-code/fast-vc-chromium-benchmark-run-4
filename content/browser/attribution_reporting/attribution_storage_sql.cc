928c08429af27289d1e91db7f163a51d268a4477
Andrew Paseltiner <apaseltiner@chromium.org>
2022-09-14 18:25:26 +0000

    Remove misleading sequence-detachments from storage classes
    base::SequenceBound constructs the underlying AttributionStorageSql on
    the given sequence, so it is not necessary for the latter's constructor
    to detach.
    
    AttributionStorageSql itself constructs the RateLimitTable on that same
    sequence, so it is not necessary for the latter to detach either.
    
    Change-Id: I53a92693e0b1e7eb3fbf8f8e46a774c799f044fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893337
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047020}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -441,7 +441,6 @@ AttributionStorageSql::AttributionStorageSql(
       delegate_(std::move(delegate)),
       rate_limit_table_(delegate_.get()) {
   DCHECK(delegate_);
-  DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
 AttributionStorageSql::~AttributionStorageSql() {
