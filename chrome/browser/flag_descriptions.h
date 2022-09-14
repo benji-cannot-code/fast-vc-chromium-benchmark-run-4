a779afacfb7bcf3e4733d246d5d0356641389648
Marcello Salomao <msalomao@google.com>
2022-09-14 00:28:47 +0000

    [Files app] Add Inline Sync Status feature flag
    Bug: b:246474388
    Test: None
    Change-Id: I02c3182efa117f9a2f68aa18ba54f0abcd3fd43b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893609
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046664}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3095,6 +3095,9 @@ extern const char kFilesAppExperimentalDescription[];
 extern const char kFilesExtractArchiveName[];
 extern const char kFilesExtractArchiveDescription[];
 
+extern const char kFilesInlineSyncStatusName[];
+extern const char kFilesInlineSyncStatusDescription[];
+
 extern const char kFilesSinglePartitionFormatName[];
 extern const char kFilesSinglePartitionFormatDescription[];
 
