a334b5b61e7fa2eb38866162e6b587897600082e
Joemer Ramos <joemerramos@chromium.org>
2022-09-14 15:52:21 +0000

    [iOS] Create Price Notifications settings UI mediator
    Creates mediator file for new price tracking settings UI. These files
    were pulled from this prototype crrev.com/c/3876495 so there may be
    some files or strings that aren't used yet, but will be used in follow
    up CLs. Rather than add these strings and delegate links when
    introducing other parts of the MVC structure, I thought it would be
    easier to have the files completely fleshed out so the files
    "just work together."
    
    Bug: 1360477
    Change-Id: Ib55050da91812c2fbc3bbea2a1c5d70bf7032027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892422
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046945}

---
+++ ios/chrome/browser/ui/settings/price_notifications/price_notifications_navigation_commands.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_NAVIGATION_COMMANDS_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_NAVIGATION_COMMANDS_H_
+
+// Protocol for navigating to different setting pages from Price Notifications
+// setting.
+@protocol PriceNotificationsNavigationCommands <NSObject>
+
+// Shows tracking price screen.
+- (void)showTrackingPrice;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_NAVIGATION_COMMANDS_H_
