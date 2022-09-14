29cc619844b62c26399c383dc442d4c3550fe3ba
Jérémie Boulic <jboulic@chromium.org>
2022-09-14 07:39:16 +0000

    [Office] Fix open-in-office action ID
    For tasks of type TASK_TYPE_WEB_APP, the action ID should be a full
    valid URL.
    
    Bug: b:243864957
    Change-Id: I737bc424018a73d0f454f8343ad578230bea15b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894212
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046782}

--- chrome/browser/ash/file_manager/file_tasks.cc
+++ chrome/browser/ash/file_manager/file_tasks.cc
@@ -365,7 +365,8 @@ void PostProcessFoundTasks(
         });
     if (it != result_list->end()) {
       FullTaskDescriptor office_task(*it);
-      office_task.task_descriptor.action_id = kActionIdOpenInOffice;
+      office_task.task_descriptor.action_id =
+          base::StrCat({kChromeUIFileManagerURL, "?", kActionIdOpenInOffice});
       result_list->push_back(office_task);
     }
   }
