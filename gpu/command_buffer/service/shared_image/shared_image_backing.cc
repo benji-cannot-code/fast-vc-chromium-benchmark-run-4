3bd92d18cd5ea4db2bee2e8f7082d074bf66e618
kylechar <kylechar@chromium.org>
2022-09-14 19:33:15 +0000

    Add shared memory overlays to compound backing
    This CL extracts shared memory overlay code from reverted
    https://crrev.com/c/3832922 that is needed for both
    GLTextureImageBacking and D3DImageBacking. This is to avoid requiring a
    chain of reverts in the future. SharedMemoryImageBacking and
    CompoundImageBacking can now ProduceOverlay() with a GLImageMemory. This
    should be a no-op since nothing uses it yet.
    
    Bug: 1293509
    Change-Id: Iabc917335b89e235f38219c9900a86496d796628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893770
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047061}

--- gpu/command_buffer/service/shared_image/shared_image_backing.cc
+++ gpu/command_buffer/service/shared_image/shared_image_backing.cc
@@ -189,7 +189,7 @@ SharedImageBacking::ProduceLegacyOverlay(SharedImageManager* manager,
 }
 #endif
 
-void SharedImageBacking::SetNotReferencedCounted() {
+void SharedImageBacking::SetNotRefCounted() {
   DCHECK(!HasAnyRefs());
   is_ref_counted_ = false;
 }
