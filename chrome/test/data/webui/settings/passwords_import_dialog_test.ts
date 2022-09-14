861a885e09c3af63bb8140b8d12bec8f2312e3a4
Elias <eliaskh@chromium.org>
2022-09-14 16:23:04 +0000

    [Passwords][Import] Add link to help center article in case of format issue.
    Mock: https://screenshot.googleplex.com/9mdp4ZCPjkn2KXp
    Implementation: https://screenshot.googleplex.com/9BX5tTMGd5avux7
    
    Bug: 1325290
    Change-Id: I7e622c8d5e458fe88a0f19424845f1f456a48690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885138
    Auto-Submit: Elias Khsheibun <eliaskh@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046955}

--- chrome/test/data/webui/settings/passwords_import_dialog_test.ts
+++ chrome/test/data/webui/settings/passwords_import_dialog_test.ts
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/lazy_load.js';
 
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {ImportDialogState, PasswordsImportDialogElement} from 'chrome://settings/lazy_load.js';
+import {IMPORT_HELP_LANDING_PAGE, ImportDialogState, PasswordsImportDialogElement} from 'chrome://settings/lazy_load.js';
 import {PasswordManagerImpl, SettingsPluralStringProxyImpl, CrButtonElement} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {TestPluralStringProxy} from 'chrome://webui-test/test_plural_string_proxy.js';
@@ -81,7 +81,7 @@ async function assertErrorStateAndClose(
   assertEquals(ImportDialogState.ERROR, importDialog.dialogState);
 
   assertEquals(
-      expectedDescription, importDialog.$.descriptionText.textContent!.trim());
+      expectedDescription, importDialog.$.descriptionText.innerHTML!.trim());
 
   assertFalse(isVisible(
       importDialog.shadowRoot!.querySelector<HTMLElement>('#tipBox')));
@@ -192,8 +192,12 @@ suite('PasswordsImportDialog', function() {
 
     await assertErrorStateAndClose(
         importDialog, passwordManager,
-        importDialog.i18nAdvanced('importPasswordsBadFormatError')
-            .replace('<b></b>', 'test.csv'));
+        importDialog
+            .i18nAdvanced(
+                'importPasswordsBadFormatError',
+                {substitutions: [IMPORT_HELP_LANDING_PAGE]},
+                )
+            .replace('<b></b>', '<b>test.csv</b>'));
   });
 
   test('hasCorrectUnknownErrorState', async function() {
