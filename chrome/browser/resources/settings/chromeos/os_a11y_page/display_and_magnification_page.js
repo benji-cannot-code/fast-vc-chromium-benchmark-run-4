072c51a7f9fdd2d6eede14d0fded17bef9a1818f
Katie Dektar <katie@chromium.org>
2022-09-14 16:31:07 +0000

    Adds simple color filtering settings behind a flag.
    Allows internal experimentation until a full feature
    is designed.
    
    These filters can be used with the new OS settings
    page and the experimental flag:
    --enable-features=AccessibilityOSSettingsVisibility,ExperimentalAccessibilityColorEnhancementSettings
    
    The settings page looks like this:
    https://screenshot.googleplex.com/A6CEqiX46XoQovE.png
    
    Some screenshots of how the tray icons look with different
    settings: go/chromeos-color-enhancement-screenshots.
    
    AX-Relnotes: N/A (behind flag)
    Bug: 1031959
    Change-Id: Ia42629f085d6cdd1e6f00c81d02fe89001553797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860132
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046962}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/display_and_magnification_page.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/display_and_magnification_page.js
@@ -108,6 +108,15 @@ class SettingsDisplayAndMagnificationElement extends
         },
       },
 
+      /** @protected */
+      experimentalColorEnhancementSettingsEnabled_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean(
+              'areExperimentalAccessibilityColorEnhancementSettingsEnabled');
+        },
+      },
+
       /**
        * Whether the user is in kiosk mode.
        * @protected
