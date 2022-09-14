369b1d9b1bddda1fa7b87cfa1b1f6553cc099f1e
Russ Hamilton <behamilton@google.com>
2022-09-14 12:54:06 +0000

    Make FLEDGE call k-anonymity service
    BUG=1234419,1342255
    
    Change-Id: I41432a08c1021bfe0f527f341e52fa9b615fda6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843039
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046872}

--- content/browser/interest_group/storage_interest_group.h
+++ content/browser/interest_group/storage_interest_group.h
@@ -43,7 +43,7 @@ struct CONTENT_EXPORT StorageInterestGroup {
 
     // Unique identifier associated with the data being anonymized, usually a
     // URL.
-    GURL key;
+    std::string key;
     // Whether the `key` was k-anonymous during the last update.
     bool is_k_anonymous;
     // The last time the unique user count was updated.
