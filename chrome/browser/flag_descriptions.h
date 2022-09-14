35daa0f66910522b3e28a660c5e214e7a9921e9c
Jungshik Shin <jshin@chromium.org>
2022-09-14 05:35:26 +0000

    Set up a feature flag for 'Hindi Inscript Layout' on chromeOS
    Bug: 1036187,b/233908778
    Change-Id: Id74aff6597bb88dea20f8d88c1f1fd1345ac3287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881058
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046756}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3186,6 +3186,9 @@ extern const char kVirtualKeyboardNewHeaderDescription[];
 extern const char kCrosLanguageSettingsImeOptionsInSettingsName[];
 extern const char kCrosLanguageSettingsImeOptionsInSettingsDescription[];
 
+extern const char kHindiInscriptLayoutName[];
+extern const char kHindiInscriptLayoutDescription[];
+
 extern const char kImeSystemEmojiPickerName[];
 extern const char kImeSystemEmojiPickerDescription[];
 
