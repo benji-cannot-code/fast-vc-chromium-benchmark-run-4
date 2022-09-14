8fb350e1734892f975b1555fccca3b8f8f17352b
Angus L. M. McLean IV <angusmclean@chromium.org>
2022-09-14 23:45:08 +0000

    holding space exp: Always show pinned files placeholder
    This will, when the suggestions feature flag is enabled, always show
    the pinned files placeholder when the holding space tray is shown,
    assuming the user has no pinned files, to keep the appearance of
    holding space more consistent.
    
    Bug: 1352511
    Change-Id: I2ae3aee6e0aefb6065f5e2b679f54ddc5a8d6fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865600
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047203}

--- ash/system/holding_space/pinned_files_section.h
+++ ash/system/holding_space/pinned_files_section.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/holding_space/holding_space_item_views_section.h"
 
-class PrefService;
-
 namespace ash {
 
 // Section for pinned files in the `PinnedFilesBubble`.
@@ -21,9 +19,6 @@ class PinnedFilesSection : public HoldingSpaceItemViewsSection {
   PinnedFilesSection(const PinnedFilesSection& other) = delete;
   PinnedFilesSection& operator=(const PinnedFilesSection& other) = delete;
   ~PinnedFilesSection() override;
-
-  // Returns whether or not the pinned files section should show a placeholder.
-  static bool ShouldShowPlaceholder(PrefService* prefs);
 
  private:
   // HoldingSpaceItemViewsSection:
