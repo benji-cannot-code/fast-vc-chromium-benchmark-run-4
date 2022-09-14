733ea809f6bd46157f9bf5e9839af1667159d1f3
Austin Sullivan <asully@chromium.org>
2022-09-14 00:50:27 +0000

    Use CreateBitmap in tests needing a dummy bitmap
    Reduce boilerplate by migrating existing code to use this helper.
    
    This CL was uploaded by git cl split.
    
    R=shuchen@chromium.org
    
    Bug: N/A
    Change-Id: Ia6f45e627d0c23dea69a7e28eb4fe3f34d2ef69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299646
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046674}

--- chrome/browser/extensions/api/virtual_keyboard_private/virtual_keyboard_private_apitest.cc
+++ chrome/browser/extensions/api/virtual_keyboard_private/virtual_keyboard_private_apitest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
+#include "ui/gfx/image/image_unittest_util.h"
 
 void CopyTextItem() {
   {
@@ -24,9 +25,7 @@ void CopyTextItem() {
 void CopyBitmapItem() {
   {
     ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste);
-    SkBitmap input_bitmap;
-    input_bitmap.allocN32Pixels(3, 2);
-    input_bitmap.eraseARGB(255, 0, 255, 0);
+    SkBitmap input_bitmap = gfx::test::CreateBitmap(3, 2);
     scw.WriteImage(input_bitmap);
   }
   base::RunLoop().RunUntilIdle();
