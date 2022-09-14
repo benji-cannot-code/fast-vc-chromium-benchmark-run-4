d494598ac1b2289aeac1104de213f23dccabbe00
Nikolaos Papaspyrou <nikolaos@chromium.org>
2022-09-14 08:23:37 +0000

    blink: Fix building with cppgc_enable_object_names
    After crrev.com/c/3858507, building with
    `cppgc_enable_object_names=true` results in a compiler error
    (incomplete type of `ScopedPauseRendering`). This CL fixes this by
    ensuring that `ScopedPauseRendering` is not a forward declaration at
    the place where its size is needed.
    
    Change-Id: I7c58f504dc45662a243c5007d0f006df84e8db0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892390
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046799}

--- third_party/blink/renderer/core/document_transition/document_transition_supplement.cc
+++ third_party/blink/renderer/core/document_transition/document_transition_supplement.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/document_transition/document_transition_supplement.h"
 
 #include "cc/document_transition/document_transition_request.h"
+#include "cc/trees/layer_tree_host.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_document_transition_callback.h"
 #include "third_party/blink/renderer/core/document_transition/document_transition.h"
 #include "third_party/blink/renderer/core/dom/document.h"
