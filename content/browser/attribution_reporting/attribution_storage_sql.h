576e0e24f8aa2b980255b1bd7abbde4c63fdb950
Andrew Paseltiner <apaseltiner@chromium.org>
2022-09-14 18:45:27 +0000

    Replace weak ptr with base::Unretained for DB error callback
    A weak pointer is unnecessary because sql::Database does not invoke the
    error callback after the instance has been destroyed, and the instance
    is outlived by the AttributionStorageSql instance that owns it.
    
    This aligns the behavior with that of the DBs used by the Aggregation
    Service, Shared Storage, and Interest Groups, among others.
    
    Change-Id: Iecccccc858ef12c5998fa1a9afbd972f8550b897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891702
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047030}

--- content/browser/attribution_reporting/attribution_storage_sql.h
+++ content/browser/attribution_reporting/attribution_storage_sql.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
 #include "content/browser/attribution_reporting/attribution_report.h"
@@ -399,7 +398,6 @@ class CONTENT_EXPORT AttributionStorageSql : public AttributionStorage {
       GUARDED_BY_CONTEXT(sequence_checker_);
 
   SEQUENCE_CHECKER(sequence_checker_);
-  base::WeakPtrFactory<AttributionStorageSql> weak_factory_{this};
 };
 
 }  // namespace content
