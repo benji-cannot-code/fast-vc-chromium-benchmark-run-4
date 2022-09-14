ea6042a138e0b1595868f2fdcf1a0e97080203d7
Tim Sergeant <tsergeant@chromium.org>
2022-09-14 07:56:26 +0000

    [Cros WebAPKs] Remove unused Arc install result metric
    This removes the "ArcInstallResult" WebAPK histogram, which is close to
    expiring and has not been used recently. The general "InstallResult"
    histogram is still useful for measuring success rate, and should not
    expire.
    
    Bug: 1360216
    Change-Id: I521bed26aa82a76d288cba1e4fa9248d91dcd668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892084
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046788}

--- chrome/browser/apps/app_service/webapk/webapk_install_task_unittest.cc
+++ chrome/browser/apps/app_service/webapk/webapk_install_task_unittest.cc
@@ -219,8 +219,6 @@ TEST_F(WebApkInstallTaskTest, SuccessfulInstall) {
             "org.chromium.webapk.some_package");
   histograms.ExpectBucketCount(apps::kWebApkInstallResultHistogram,
                                apps::WebApkInstallStatus::kSuccess, 1);
-  histograms.ExpectBucketCount(apps::kWebApkArcInstallResultHistogram,
-                               arc::mojom::WebApkInstallResult::kSuccess, 1);
 }
 
 TEST_F(WebApkInstallTaskTest, ShareTarget) {
@@ -305,9 +303,6 @@ TEST_F(WebApkInstallTaskTest, FailedArcInstall) {
   ASSERT_EQ(apps::webapk_prefs::GetWebApkAppIds(profile()).size(), 0u);
   histograms.ExpectBucketCount(apps::kWebApkInstallResultHistogram,
                                apps::WebApkInstallStatus::kGooglePlayError, 1);
-  histograms.ExpectBucketCount(
-      apps::kWebApkArcInstallResultHistogram,
-      arc::mojom::WebApkInstallResult::kErrorResolveNetworkError, 1);
 }
 
 TEST_F(WebApkInstallTaskTest, MinterTimeout) {
@@ -509,8 +504,6 @@ TEST_F(WebApkInstallTaskTest, SuccessfulUpdateMultipleChanges) {
               testing::IsEmpty());
   histograms.ExpectBucketCount(apps::kWebApkUpdateResultHistogram,
                                apps::WebApkInstallStatus::kSuccess, 1);
-  histograms.ExpectBucketCount(apps::kWebApkArcUpdateResultHistogram,
-                               arc::mojom::WebApkInstallResult::kSuccess, 1);
 }
 
 TEST_F(WebApkInstallTaskTest, AbandonedUpdateNoChanges) {
