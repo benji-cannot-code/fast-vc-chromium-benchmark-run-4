3729e8b59c9aab2633e97123a680c0818828f45c
Alan Zhao <ayzhao@google.com>
2022-09-14 21:09:33 +0000

    Reenable the libc++ nondebug assertion handler on Windows
    The clang roll https://crrev.com/c/3889980 includes the lld patch
    https://reviews.llvm.org/D133165. With this patch, lld now supports weak
    symbols in LLVM bitcode input files when outputting COFF files. This
    allows us to use the nondebug assertion handler when building Windows
    with ThinLTO enabled.
    
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:win7-rel
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome
    Cq-Include-Trybots: chrome/try:win32-pgo,win64-pgo
    Bug: 1335422
    Change-Id: I139f62c83acec3778628884d135f572e4c4f5ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894313
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047103}

--- base/nodebug_assertion.cc
+++ base/nodebug_assertion.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <__config>
-#include <__verbose_abort>
 
 #include "base/base_export.h"
 #include "base/immediate_crash.h"
