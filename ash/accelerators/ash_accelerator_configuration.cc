5277c1014d3e7c2242ccaa0e1d1bbced3988c19c
Jimmy <jimmyxgong@chromium.org>
2022-09-14 02:52:40 +0000

    shortcuts: Implement InitializeAcceleratorMapping
    InitializeAcceleratorMapping is a public api that populates the
    accelerator mappings for AshAcceleratorConfiguration.
    
    Bug: b:216049298
    Test: ash_unittests
    Change-Id: I38b8d37dad4d468ffae3d7d985d8b22910e82905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872255
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046721}

--- ash/accelerators/ash_accelerator_configuration.cc
+++ ash/accelerators/ash_accelerator_configuration.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ash/constants/ash_features.h"
+#include "ash/public/cpp/accelerators.h"
 #include "ash/public/mojom/accelerator_info.mojom.h"
+#include "base/containers/contains.h"
+#include "base/containers/span.h"
 #include "ui/base/accelerators/accelerator.h"
 
 namespace ash {
@@ -19,7 +23,9 @@ AshAcceleratorConfiguration::~AshAcceleratorConfiguration() = default;
 // TODO(jimmyxgong): Implement all functions below as these are only stubs.
 const std::vector<AcceleratorInfo>&
 AshAcceleratorConfiguration::GetConfigForAction(AcceleratorActionId action_id) {
-  return accelerator_infos_;
+  DCHECK(base::Contains(id_to_accelerator_infos_, action_id));
+
+  return id_to_accelerator_infos_[action_id];
 }
 
 bool AshAcceleratorConfiguration::IsMutable() const {
@@ -52,6 +58,25 @@ AcceleratorConfigResult AshAcceleratorConfiguration::RestoreDefault(
 
 AcceleratorConfigResult AshAcceleratorConfiguration::RestoreAllDefaults() {
   return AcceleratorConfigResult::kActionLocked;
+}
+
+void AshAcceleratorConfiguration::InitializeAcceleratorMapping(
+    base::span<const AcceleratorData> accelerators) {
+  accelerator_infos_.clear();
+  id_to_accelerator_infos_.clear();
+
+  for (const auto& data : accelerators) {
+    ui::Accelerator accelerator(data.keycode, data.modifiers);
+    // TODO(jimmyxgong): Ash accelerators should not be locked when
+    // customization is allowed.
+    AcceleratorInfo info(ash::mojom::AcceleratorType::kDefault, accelerator,
+                         /**locked=*/true);
+
+    const uint32_t action_id = static_cast<uint32_t>(data.action);
+
+    accelerator_infos_.push_back(info);
+    id_to_accelerator_infos_[action_id].push_back(info);
+  }
 }
 
 }  // namespace ash
