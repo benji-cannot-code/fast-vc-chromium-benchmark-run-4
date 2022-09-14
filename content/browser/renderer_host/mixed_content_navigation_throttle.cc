dbe646fe80564fadb1431512cc50a7d588b44d0d
Carlos IL <carlosil@chromium.org>
2022-09-14 18:35:54 +0000

    Use GetTupleOrPrecursorTupleIfOpaque instead of origin when making mixed content decisions about frames
    Previously we used the origin, but it is not set for sandboxed frames,
    this CL changes the logic so GetTupleOrPrecursorTupleIfOpaque is now used.
    
    Test: A Web Platform Test covering this case is ready, and will be added
    once this lands (see bug).
    
    Bug: 956979
    Change-Id: Iba125573e81360d1d9b2023065e27d3c5b27cf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881204
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047024}

--- content/browser/renderer_host/mixed_content_navigation_throttle.cc
+++ content/browser/renderer_host/mixed_content_navigation_throttle.cc
@@ -55,7 +55,8 @@ bool IsUrlPotentiallySecure(const GURL& url) {
 // This method should return the same results as
 // SchemeRegistry::shouldTreatURLSchemeAsRestrictingMixedContent.
 bool DoesOriginSchemeRestrictMixedContent(const url::Origin& origin) {
-  return origin.scheme() == url::kHttpsScheme;
+  return origin.GetTupleOrPrecursorTupleIfOpaque().scheme() ==
+         url::kHttpsScheme;
 }
 
 void UpdateRendererOnMixedContentFound(NavigationRequest* navigation_request,
