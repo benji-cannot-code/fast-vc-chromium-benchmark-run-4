6831058273f10961392d5ea9a1dc9ec70689d16b
Xianzhu Wang <wangxianzhu@chromium.org>
2022-09-14 22:29:48 +0000

    Remove gfx::Transform::matrix(), Step 2 (cc/)
    gfx::Transform::matrix() exposes gfx::Matrix44 API to callers. Now
    let users of gfx::Transform use gfx::Transform API only so that we
    can modify the implementation more freely prepare for the merge of
    gfx::Transform and blink::TransformationMatrix.
    
    Bug: 1359528
    Change-Id: Id58401648bf478efaa8a62270bf59ef016372c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892442
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047154}

--- cc/trees/damage_tracker.cc
+++ cc/trees/damage_tracker.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/effect_node.h"
 #include "cc/trees/layer_tree_impl.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/geometry/skia_conversions.h"
 
 namespace cc {
 
@@ -217,7 +218,8 @@ void DamageTracker::ComputeSurfaceDamage(RenderSurfaceImpl* render_surface) {
     bool is_rect_valid = damage_for_this_update_.GetAsRect(&damage_rect);
     if (is_rect_valid && !damage_rect.IsEmpty()) {
       damage_rect = render_surface->Filters().MapRect(
-          damage_rect, render_surface->SurfaceScale().matrix().asM33());
+          damage_rect,
+          gfx::TransformToFlattenedSkMatrix(render_surface->SurfaceScale()));
       damage_for_this_update_ = DamageAccumulator();
       damage_for_this_update_.Union(damage_rect);
     }
