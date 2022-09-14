cf952b96fb730d098af8b5ec8f53f212c9e7abb6
Emily Stark <estark@google.com>
2022-09-14 04:45:01 +0000

    Disable Expect-CT by default and adapt tests
    The Expect-CT feature is deprecated and thus the base::Feature is set
    to disabled by default in M107. I'll clean up the code in M108 after
    it's clear that the removal has stuck.
    
    Bug: 1232560
    Change-Id: I214928cf26ad8a3451e7d5f37a92be64857b1f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891964
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046750}

--- chrome/browser/ui/webui/net_internals/net_internals_ui_browsertest.cc
+++ chrome/browser/ui/webui/net_internals/net_internals_ui_browsertest.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/dns/public/host_resolver_source.h"
 #include "net/dns/public/resolve_error_info.h"
+#include "net/http/transport_security_state.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/request_handler_util.h"
 #include "services/network/test/test_network_context.h"
@@ -348,6 +349,8 @@ void NetInternalsTest::MessageHandler::ResetNetworkContextForTesting(
 NetInternalsTest::NetInternalsTest()
     : test_server_started_(false) {
   message_handler_ = std::make_unique<MessageHandler>(this);
+  scoped_feature_list_.InitAndEnableFeature(
+      net::TransportSecurityState::kDynamicExpectCTFeature);
 }
 
 NetInternalsTest::~NetInternalsTest() {
