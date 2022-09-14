2a9f11bd0f4827e87beffc5b45046efbbdaf2f00
Jeevan Shikaram <jshikaram@chromium.org>
2022-09-14 03:40:02 +0000

    [APS] Add User Type to DeviceInfoManager.
    This CL adds a method to the DeviceInfoManager that returns the
    profile's user type.
    
    Bug: b/243089000
    Change-Id: I80ae83c7851fc87331c5319aba379942effff6ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892083
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046734}

--- chrome/browser/apps/app_preload_service/device_info_manager.h
+++ chrome/browser/apps/app_preload_service/device_info_manager.h
@@ -9,24 +9,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <string>
 
+#include "base/memory/raw_ptr.h"
+
+class Profile;
+
 namespace apps {
 // This class is a helper interface to get info about the device the code is
 // currently running on.
 class DeviceInfoManager {
  public:
-  DeviceInfoManager();
+  explicit DeviceInfoManager(Profile* profile);
   DeviceInfoManager(const DeviceInfoManager&) = delete;
   DeviceInfoManager& operator=(const DeviceInfoManager&) = delete;
   ~DeviceInfoManager();
 
-  // Returns the board family of the device. e.g. brya.
+  // Returns the board family of the device. e.g. "brya"
   std::string GetBoard() const;
 
-  // Returns the Chrome browser version of the device. e.g. (107.0.5296.0)
+  // Returns the Chrome browser version of the device. e.g. "107.0.5296.0"
   std::string GetChromeVersion() const;
 
-  // Returns the ChromeOS platform version of the device. e.g. (15088.0.0)
+  // Returns the ChromeOS platform version of the device. e.g. "15088.0.0"
   std::string GetChromeOsPlatformVersion() const;
+
+  // Returns the User Type of the profile currently running. e.g. "unmanaged"
+  std::string GetUserType() const;
+
+ private:
+  base::raw_ptr<Profile> profile_;
 };
 
 std::ostream& operator<<(std::ostream& os,
