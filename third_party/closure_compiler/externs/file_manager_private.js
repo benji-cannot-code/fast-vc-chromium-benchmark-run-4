5b87a6855b9078520555a72775457ee35882152e
Aida Zolic <aidazolic@chromium.org>
2022-09-14 10:08:21 +0000

    DLP: Add restrictionDetails dialog
    Adds a new DlpRestrictionDetailsDialog widget that can be shown from the
    context menu, when the user selects a managed file.
    
    Bug: 1346254
    Change-Id: I44c29f9dcbaa7f7e171fe229aa107d17fca02ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825948
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046828}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -765,6 +765,8 @@ chrome.fileManagerPrivate.DlpMetadata;
 
 /** @enum {string} */
 chrome.fileManagerPrivate.DlpLevel = {
+  REPORT: 'report',
+  WARN: 'warn',
   BLOCK: 'block',
   ALLOW: 'allow',
 };
--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
@@ -5,15 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assertInstanceof} from 'chrome://resources/js/assert.m.js';
 import {decorate, define as crUiDefine} from 'chrome://resources/js/cr/ui.js';
-import {contextMenuHandler} from './context_menu_handler.js';
-import {Menu} from './menu.js';
-import {MenuItem} from './menu_item.js';
 
 import {DialogType} from '../../../common/js/dialog_type.js';
 import {str, strf, util} from '../../../common/js/util.js';
 import {AllowedPaths} from '../../../common/js/volume_manager_types.js';
 import {BreadcrumbContainer} from '../../../containers/breadcrumb_container.js';
 import {VolumeManager} from '../../../externs/volume_manager.js';
+import {XfDlpRestrictionDetailsDialog} from '../../../widgets/xf_dlp_restriction_details_dialog.js';
 import {FilesPasswordDialog} from '../../elements/files_password_dialog.js';
 import {FilesToast} from '../../elements/files_toast.js';
 import {FilesTooltip} from '../../elements/files_tooltip.js';
@@ -25,6 +23,7 @@ import {A11yAnnounce} from './a11y_announce.js';
 import {ActionModelUI} from './action_model_ui.js';
 import {ActionsSubmenu} from './actions_submenu.js';
 import {ComboButton} from './combobutton.js';
+import {contextMenuHandler} from './context_menu_handler.js';
 import {DefaultTaskDialog} from './default_task_dialog.js';
 import {DialogFooter} from './dialog_footer.js';
 import {BaseDialog} from './dialogs.js';
@@ -38,6 +37,8 @@ import {GearMenu} from './gear_menu.js';
 import {ImportCrostiniImageDialog} from './import_crostini_image_dialog.js';
 import {InstallLinuxPackageDialog} from './install_linux_package_dialog.js';
 import {ListContainer} from './list_container.js';
+import {Menu} from './menu.js';
+import {MenuItem} from './menu_item.js';
 import {MultiMenu} from './multi_menu.js';
 import {MultiMenuButton} from './multi_menu_button.js';
 import {ProgressCenterPanel} from './progress_center_panel.js';
@@ -149,6 +150,12 @@ export class FileManagerUI {
      * @type {?FilesPasswordDialog}
      */
     this.passwordDialog_ = null;
+
+    /**
+     * Dialog for DLP (Data Leak Prevention) restriction details.
+     * @type {?XfDlpRestrictionDetailsDialog}
+     */
+    this.dlpRestrictionDetailsDialog_ = null;
 
     /**
      * The container element of the dialog.
@@ -435,6 +442,24 @@ export class FileManagerUI {
         document.createElement('files-password-dialog'));
     this.element.appendChild(this.passwordDialog_);
     return this.passwordDialog_;
+  }
+
+  /**
+   * Gets the DlpRestrictionDetails dialog.
+   * @return {?XfDlpRestrictionDetailsDialog}
+   */
+  get dlpRestrictionDetailsDialog() {
+    if (!util.isDlpEnabled()) {
+      return null;
+    }
+    if (this.dlpRestrictionDetailsDialog_) {
+      return this.dlpRestrictionDetailsDialog_;
+    }
+    this.dlpRestrictionDetailsDialog_ =
+        /** @type {!XfDlpRestrictionDetailsDialog} */ (
+            document.createElement('xf-dlp-restriction-details-dialog'));
+    this.element.appendChild(this.dlpRestrictionDetailsDialog_);
+    return this.dlpRestrictionDetailsDialog_;
   }
 
   /**
