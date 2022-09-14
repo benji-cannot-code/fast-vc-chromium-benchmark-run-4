29732687667a517b42742f6dcddf28b27d327e30
kylechar <kylechar@chromium.org>
2022-09-14 20:19:48 +0000

    Reland: Make compound backing work with D3DImageBacking
    https://crrev.com/c/3852364 was reverted as it depended on a CL that
    needed to be reverted. Reland without the dependency.
    
    Removes the shared memory segment support from D3DImageBacking and use
    compound backing instead. D3DImageBacking supports readback from texture
    to memory so add SharedImageBacking::ReadbackToMemory() for compound
    backing to use. The CopyToGpuMemoryBuffer() implementation is moved to
    compound backing as well.
    
    Bug: 1293509
    Change-Id: I05c13f3cf4e20fad3102bd61de5a030c5a411bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894561
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047082}

--- gpu/command_buffer/service/shared_image/shared_image_backing.cc
+++ gpu/command_buffer/service/shared_image/shared_image_backing.cc
@@ -103,6 +103,11 @@ bool SharedImageBacking::UploadFromMemory(const SkPixmap& pixmap) {
   return false;
 }
 
+bool SharedImageBacking::ReadbackToMemory(SkPixmap& pixmap) {
+  NOTREACHED();
+  return false;
+}
+
 bool SharedImageBacking::PresentSwapChain() {
   return false;
 }
