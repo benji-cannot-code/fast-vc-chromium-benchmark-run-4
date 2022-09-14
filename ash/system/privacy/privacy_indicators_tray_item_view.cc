a16ce35304883153736d03dd0d19822e6aba87d5
Andre Le <leandre@chromium.org>
2022-09-14 18:40:29 +0000

    PrivacyIndicators: Use solid color layer instead of views::Background
    This is necessary for adding animation to privacy indicators view since
    views::Background don't adjust accordingly when animation progresses. We
    will now use a solid color layer and adjust the clip rect accordingly
    when bounds change.
    
    Bug: 1363383
    Change-Id: I56c52c0b865b7f79d3d988d612c1538829e55897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891540
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047026}

--- ash/system/privacy/privacy_indicators_tray_item_view.cc
+++ ash/system/privacy/privacy_indicators_tray_item_view.cc
@@ -14,12 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/color/color_id.h"
+#include "ui/color/color_provider.h"
+#include "ui/compositor/layer.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/paint_vector_icon.h"
-#include "ui/views/background.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 
@@ -46,10 +49,21 @@ PrivacyIndicatorsTrayItemView::PrivacyIndicatorsTrayItemView(Shelf* shelf)
   layout_manager_->set_main_axis_alignment(
       views::BoxLayout::MainAxisAlignment::kCenter);
 
+  // Set up a solid color layer to paint the background color, then add a layer
+  // to each child so that they are visible and can perform layer animation.
+  SetPaintToLayer(ui::LAYER_SOLID_COLOR);
+  layer()->SetFillsBoundsOpaquely(false);
+  layer()->SetRoundedCornerRadius(
+      gfx::RoundedCornersF{kPrivacyIndicatorsViewHeight / 2});
+
   auto camera_icon = std::make_unique<views::ImageView>();
+  camera_icon->SetPaintToLayer();
+  camera_icon->layer()->SetFillsBoundsOpaquely(false);
   camera_icon_ = container_view->AddChildView(std::move(camera_icon));
 
   auto microphone_icon = std::make_unique<views::ImageView>();
+  microphone_icon->SetPaintToLayer();
+  microphone_icon->layer()->SetFillsBoundsOpaquely(false);
   microphone_icon_ = container_view->AddChildView(std::move(microphone_icon));
 
   AddChildView(std::move(container_view));
@@ -82,6 +96,7 @@ void PrivacyIndicatorsTrayItemView::UpdateAlignmentForShelf(Shelf* shelf) {
   layout_manager_->SetOrientation(
       shelf->PrimaryAxisValue(views::BoxLayout::Orientation::kHorizontal,
                               views::BoxLayout::Orientation::kVertical));
+  UpdateBoundsInset();
 }
 
 void PrivacyIndicatorsTrayItemView::HandleLocaleChange() {
@@ -100,14 +115,15 @@ gfx::Size PrivacyIndicatorsTrayItemView::CalculatePreferredSize() const {
 
 void PrivacyIndicatorsTrayItemView::OnThemeChanged() {
   views::View::OnThemeChanged();
-
-  // We round the corners and set the border thickness based on the shorter side
-  // of the view (the shorter size changes based on shelf alignment).
-  int shorter_side = std::min(width(), height());
-  SetBackground(views::CreateThemedRoundedRectBackground(
-      ui::kColorAshPrivacyIndicatorsBackground, shorter_side / 2,
-      shorter_side - kPrivacyIndicatorsViewHeight));
   UpdateIcons();
+
+  layer()->SetColor(
+      GetColorProvider()->GetColor(ui::kColorAshPrivacyIndicatorsBackground));
+}
+
+void PrivacyIndicatorsTrayItemView::OnBoundsChanged(
+    const gfx::Rect& previous_bounds) {
+  UpdateBoundsInset();
 }
 
 std::u16string PrivacyIndicatorsTrayItemView::GetTooltipText(
@@ -144,6 +160,21 @@ void PrivacyIndicatorsTrayItemView::UpdateIcons() {
   microphone_icon_->SetImage(
       gfx::CreateVectorIcon(kPrivacyIndicatorsMicrophoneIcon,
                             kPrivacyIndicatorsIconSize, icon_color));
+}
+
+void PrivacyIndicatorsTrayItemView::UpdateBoundsInset() {
+  gfx::Rect bounds = GetLocalBounds();
+  auto* shelf = Shelf::ForWindow(GetWidget()->GetNativeWindow());
+
+  // We set the bounds inset based on the shorter side of the view (the shorter
+  // size changes based on shelf alignment).
+  int shorter_side_inset = shelf->PrimaryAxisValue(height(), width()) -
+                           shelf->PrimaryAxisValue(GetPreferredSize().height(),
+                                                   GetPreferredSize().width());
+  bounds.Inset(
+      shelf->PrimaryAxisValue(gfx::Insets::VH(shorter_side_inset / 2, 0),
+                              gfx::Insets::VH(0, shorter_side_inset / 2)));
+  layer()->SetClipRect(bounds);
 }
 
 }  // namespace ash
