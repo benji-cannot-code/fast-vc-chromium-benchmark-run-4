548ed973f1bbc5e7989ee6dd0e287f5b07d51705
Liam Brady <lbrady@google.com>
2022-09-14 20:03:58 +0000

    Fenced frame: enable shared storage for opaque-ads
    Opaque-ads fenced frames need to have shared storage enabled to be
    able to support FLEDGE. Until the opaque URN attributes feature is in
    place, this is a temporary solution to allow a fenced frame to have
    shared storage.
    
    This builds off of the work that was done for attribution reporting:
    https://chromium-review.googlesource.com/c/chromium/src/+/3779724
    
    - An opaque-ads fenced frame will not be allowed to load if shared
    storage is explicitly disabled in its embedder.
    - If the embedder doesn't specify any shared storage policy in its
    header, the opaque-ads fenced frame will act as if it was enabled
    via allow (*) and allow itself to load.
    - If an opaque-ads fenced frame loads, it will turn on shared storage
    in its permissions policy.
    
    Note: I did not add any WPTs to test shared storage because the
    infrastructure is already being tested as part of the attribution
    reporting WPTs.
    
    Change-Id: I147af464034e04a4517d1adc30de72d3303a3f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879804
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047075}

--- third_party/blink/web_tests/wpt_internal/fenced_frame/resources/default-enabled-features-helper.js
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/default-enabled-features-helper.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <script src="resources/utils.js"></script>
 // <script src="/common/get-host-info.sub.js"></script>
 
-async function runAttributionReportingTest(t, should_load, fenced_origin) {
+async function runDefaultEnabledFeaturesTest(t, should_load, fenced_origin) {
   const fencedframe = attachFencedFrameContext({
       attributes: [["mode", "opaque-ads"]],
       origin: fenced_origin});
@@ -27,6 +27,10 @@ async function runAttributionReportingTest(t, should_load, fenced_origin) {
     assert_true(
         document.featurePolicy.allowsFeature('attribution-reporting'),
         "Attribution reporting should be allowed if the fenced " +
-        " frame loaded.");
+        "frame loaded.");
+    assert_true(
+          document.featurePolicy.allowsFeature('shared-storage'),
+          "Shared storage should be allowed if the fenced " +
+          "frame loaded.");
   });
 }
