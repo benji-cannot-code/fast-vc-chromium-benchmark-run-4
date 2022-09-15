9313836b7e42e2645b74de6fb972a2467481da58
Mikel Astiz <mastiz@chromium.org>
2022-09-15 14:12:56 +0000

    [sync] Fork trusted vault metric specific for V1 registration
    The metric is expected to behave better for V1 registrations, which are
    more reliable than V0 registrations. Clients should migrate from V0 to
    V1 with re-registration, but meanwhile a dedicated metric makes it
    easier to accurately analyze the behavior.
    
    Change-Id: I1e47407c27a0b26345c7ec781ed3ff9d9f625172
    Bug: 1334939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899251
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047460}

--- components/sync/trusted_vault/standalone_trusted_vault_backend_unittest.cc
+++ components/sync/trusted_vault/standalone_trusted_vault_backend_unittest.cc
@@ -1614,6 +1614,10 @@ TEST_F(StandaloneTrustedVaultBackendTest, ShouldVerifyRegistration) {
       "Sync.TrustedVaultVerifyDeviceRegistrationState",
       /*sample=*/TrustedVaultDownloadKeysStatusForUMA::kNoNewKeys,
       /*expected_bucket_count=*/1);
+  histogram_tester.ExpectUniqueSample(
+      "Sync.TrustedVaultVerifyDeviceRegistrationStateV1",
+      /*sample=*/TrustedVaultDownloadKeysStatusForUMA::kNoNewKeys,
+      /*expected_bucket_count=*/1);
 }
 
 }  // namespace
