34435773414fda1aee34c319bb7e1b8a1a0154db
Sylvain Defresne <sdefresne@chromium.org>
2022-09-15 10:09:47 +0000

    [ios] Use thread-safe cache for GetDisplayNameForLocale()
    Add a cache for the NSLocale in GetDisplayNameForLocale() as the
    creation of the NSLocale object can be expensive. As the function
    can be called from any thread, use thread-local storage to keep
    a cache per thread and thus make the function thread-safe.
    
    Also, move the code in a separate target to allow compiling it
    with ARC enabled (this makes the code slightly easier to read
    and write).
    
    Remove unnecessary #include from l10n_util_ios.*.
    
    Bug: 1349345
    Change-Id: I4cb8e05e624e20e458d83a872885a0209e0c3e43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883688
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047389}

--- ui/base/l10n/l10n_util_ios.h
+++ ui/base/l10n/l10n_util_ios.h
@@ -10,12 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 
-#ifdef __OBJC__
-@class NSString;
-#else
-class NSString;
-#endif
-
 namespace l10n_util {
 
 // Get localized language name using NSLocale Foundation API. If the system
