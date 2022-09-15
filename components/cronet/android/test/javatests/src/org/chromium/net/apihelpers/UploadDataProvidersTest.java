765a2526e05086180fdc874f1468f2b2010ce0c3
Dan Stahr <danstahr@google.com>
2022-09-15 08:42:27 +0000

    Create a new helpers subpackage for QoL classes.
    Move UploadDataProviders to the new package.
    
    Change-Id: Id46fb5972da3613e7940a4a8a34f7d83fbbb7147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854006
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Dan Stahr <danstahr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047363}

--- components/cronet/android/test/javatests/src/org/chromium/net/apihelpers/UploadDataProvidersTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/apihelpers/UploadDataProvidersTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.net;
+package org.chromium.net.apihelpers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -25,8 +25,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Feature;
+import org.chromium.net.CallbackException;
+import org.chromium.net.CronetTestRule;
 import org.chromium.net.CronetTestRule.CronetTestFramework;
 import org.chromium.net.CronetTestRule.OnlyRunNativeCronet;
+import org.chromium.net.NativeTestServer;
+import org.chromium.net.TestUrlRequestCallback;
+import org.chromium.net.UploadDataProvider;
+import org.chromium.net.UploadDataSink;
+import org.chromium.net.UrlRequest;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -150,6 +157,7 @@ public class UploadDataProvidersTest {
         builder.addHeader("Content-Type", "useless/string");
         builder.setUploadDataProvider(new UploadDataProvider() {
             private static final String CONTENT = "hello";
+
             @Override
             public long getLength() throws IOException {
                 return CONTENT.length();
