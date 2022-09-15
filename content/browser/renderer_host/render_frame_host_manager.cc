14d5da05da8ca8b70b04267ccbff0b08eff3d1a7
W. James MacLean <wjmaclean@chromium.org>
2022-09-15 18:58:58 +0000

    Fix site-origin mismatch for sandboxed srcdoc iframes in per-origin mode.
    In per-origin mode for process-isolated sandboxed iframes, at present
    a sandboxed srcdoc iframe will attempt to clone its parent's
    SiteInstance, with the only difference being that the sandbox flag is
    set. But the parent's SiteInstance will be using a site-based site-url,
    and this will cause the child to attempt to load itself in a process
    with a site-based, and not origin-based, site_url.
    
    This CL revises the cloning process to take into account the parent's
    origin (which the srcdoc frame will use), and creating a consistent
    SiteInstance by recreating the SiteInfo via SiteInfo::Create. This will
    use the same pathways for creation of the SiteInfo as if it had been
    created for a navigation.
    
    Bug: 1345491
    Change-Id: I2870d01f3e916f28784b3d5adb749d9c58edacc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854030
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047598}

--- content/browser/renderer_host/render_frame_host_manager.cc
+++ content/browser/renderer_host/render_frame_host_manager.cc
@@ -3212,7 +3212,7 @@ RenderFrameHostManager::GetSiteInstanceForNavigationRequest(
       // iframes are normally considered to have the same origin as their
       // parents, so this seems reasonable.
       return parent->GetSiteInstance()->GetCompatibleSandboxedSiteInstance(
-          request->GetNavigationId());
+          url_info, parent->GetLastCommittedOrigin());
     }
     AppendReason(reason,
                  "GetSiteInstanceForNavigationRequest => parent-instance"
