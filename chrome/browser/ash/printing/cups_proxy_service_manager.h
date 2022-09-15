eb17ea753adc65b12d8cde8f3f71536bf77ed905
Henrique Ferreiro <hferreiro@igalia.com>
2022-09-15 17:32:49 +0000

    Move sources behind GN conditions to //chrome/browser/ash
    Move sources guarded by is_cfm, !is_official_build and use_cups in
    //chrome/browser/chromeos to //chrome/browser/ash.
    
    This CL is part of the ChromeOS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: Id648434eaa21b590d092dd29b9ef3537d47422cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841813
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1047562}

--- chrome/browser/ash/printing/cups_proxy_service_manager.h
+++ chrome/browser/ash/printing/cups_proxy_service_manager.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/services/cups_proxy/cups_proxy_service.h"
-#include "chrome/services/cups_proxy/public/mojom/proxy.mojom-forward.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
