29f44550c967353d71c0356ff4c53ef00180019e
Nidhi Jaju <nidhijaju@chromium.org>
2022-09-15 08:59:06 +0000

    Remove unused ScopedFeatureList from NetworkContextConfigurationBrowserTest
    This CL removes the ScopedFeatureList declaration in
    NetworkContextConfigurationBrowserTest as it is not used in the tests.
    
    Bug: 1344573
    Change-Id: Iba643e4768ed580bc6f8954438ccffcbefa7a2bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3897711
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047371}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -723,7 +722,6 @@ class NetworkContextConfigurationBrowserTest
   }
 
   raw_ptr<Browser> incognito_ = nullptr;
-  base::test::ScopedFeatureList feature_list_;
 
   net::EmbeddedTestServer https_server_;
   std::unique_ptr<net::test_server::ControllableHttpResponse>
