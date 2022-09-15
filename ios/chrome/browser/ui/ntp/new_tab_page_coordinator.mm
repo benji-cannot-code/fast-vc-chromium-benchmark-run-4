ec7b368c1d0dad7165f7132d398e442a1c57de26
Sergio Collazos <sczs@chromium.org>
2022-09-15 22:44:35 +0000

    [ios] Adds flag to gate Follow blue dot.
    Fixed: 1364297
    Change-Id: I58c8e3fd096833e85bff331c24d13c18151b4a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901092
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047716}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/ntp/new_tab_page_coordinator.h"
 
+#import "base/feature_list.h"
 #import "base/metrics/field_trial_params.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
@@ -95,7 +96,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-
+// Flag to enable the checking of new content for the Follow Feed.
+const base::Feature kEnableCheckForNewFollowContent{
+    "EnableCheckForNewFollowContent", base::FEATURE_DISABLED_BY_DEFAULT};
 }  // namespace
 
 @interface NewTabPageCoordinator () <AppStateObserver,
@@ -618,7 +621,8 @@ namespace {
 }
 
 - (void)updateFollowingFeedHasUnseenContent:(BOOL)hasUnseenContent {
-  if (![self isFollowingFeedAvailable]) {
+  if (![self isFollowingFeedAvailable] ||
+      !base::FeatureList::IsEnabled(kEnableCheckForNewFollowContent)) {
     return;
   }
   if ([self doesFollowingFeedHaveContent]) {
@@ -1355,11 +1359,15 @@ namespace {
 - (FeedHeaderViewController*)feedHeaderViewController {
   DCHECK(!self.browser->GetBrowserState()->IsOffTheRecord());
   if (!_feedHeaderViewController) {
-    // Only show the dot if the user follows available publishers.
-    BOOL followingSegmentDotVisible =
-        [self doesFollowingFeedHaveContent] &&
-        self.discoverFeedService->GetFollowingFeedHasUnseenContent() &&
-        self.selectedFeed != FeedTypeFollowing;
+    BOOL followingSegmentDotVisible = NO;
+    if (base::FeatureList::IsEnabled(kEnableCheckForNewFollowContent) &&
+        IsWebChannelsEnabled()) {
+      // Only show the dot if the user follows available publishers.
+      followingSegmentDotVisible =
+          [self doesFollowingFeedHaveContent] &&
+          self.discoverFeedService->GetFollowingFeedHasUnseenContent() &&
+          self.selectedFeed != FeedTypeFollowing;
+    }
     _feedHeaderViewController = [[FeedHeaderViewController alloc]
         initWithFollowingFeedSortType:self.followingFeedSortType
            followingSegmentDotVisible:followingSegmentDotVisible];
