57a5d16f407af0fb26801d8c4b29ceb8fba49921
Mikel Astiz <mastiz@chromium.org>
2022-09-15 14:25:55 +0000

    [sync] Add metric to record passphrase type upon NOT_MY_BIRTHDAY
    Equivalent insights are not available using metric filters because,
    by the time Sync.PassphraseType2 gets computed (attached to all metric
    records), the passphrase type has already been cleared (so the
    passphrase type *before* the clearing is not logged).
    
    Change-Id: I6523783eb8dadc4a554de70f597ef821b3a3d645
    Fixed: 1364027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3898991
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047468}

--- components/sync/driver/sync_service_impl_unittest.cc
+++ components/sync/driver/sync_service_impl_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/features.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/base/pref_names.h"
+#include "components/sync/base/stop_source.h"
 #include "components/sync/base/sync_util.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/driver/configure_context.h"
@@ -1043,6 +1045,68 @@ TEST_F(SyncServiceImplTest, DisableSyncOnClient) {
 
   EXPECT_FALSE(service()->IsSyncFeatureEnabled());
   EXPECT_FALSE(service()->IsSyncFeatureActive());
+}
+
+TEST_F(SyncServiceImplTest,
+       DisableSyncOnClientLogsPassphraseTypeForNotMyBirthday) {
+  const PassphraseType kPassphraseType = PassphraseType::kKeystorePassphrase;
+
+  SignIn();
+  CreateService(SyncServiceImpl::MANUAL_START);
+  InitializeForNthSync();
+
+  service()->GetEncryptionObserverForTest()->OnPassphraseTypeChanged(
+      kPassphraseType, /*passphrase_time=*/base::Time());
+
+  ASSERT_EQ(SyncService::TransportState::ACTIVE,
+            service()->GetTransportState());
+  ASSERT_TRUE(service()->IsSyncFeatureEnabled());
+  ASSERT_EQ(kPassphraseType, service()->GetUserSettings()->GetPassphraseType());
+
+  SyncProtocolError client_cmd;
+  client_cmd.action = DISABLE_SYNC_ON_CLIENT;
+  client_cmd.error_type = NOT_MY_BIRTHDAY;
+
+  base::HistogramTester histogram_tester;
+  service()->OnActionableError(client_cmd);
+
+  ASSERT_FALSE(service()->IsSyncFeatureEnabled());
+
+  histogram_tester.ExpectUniqueSample(
+      "Sync.PassphraseTypeUponNotMyBirthdayOrEncryptionObsolete",
+      /*sample=*/kPassphraseType,
+      /*expected_bucket_count=*/1);
+}
+
+TEST_F(SyncServiceImplTest,
+       DisableSyncOnClientLogsPassphraseTypeForEncryptionObsolete) {
+  const PassphraseType kPassphraseType = PassphraseType::kKeystorePassphrase;
+
+  SignIn();
+  CreateService(SyncServiceImpl::MANUAL_START);
+  InitializeForNthSync();
+
+  service()->GetEncryptionObserverForTest()->OnPassphraseTypeChanged(
+      kPassphraseType, /*passphrase_time=*/base::Time());
+
+  ASSERT_EQ(SyncService::TransportState::ACTIVE,
+            service()->GetTransportState());
+  ASSERT_TRUE(service()->IsSyncFeatureEnabled());
+  ASSERT_EQ(kPassphraseType, service()->GetUserSettings()->GetPassphraseType());
+
+  SyncProtocolError client_cmd;
+  client_cmd.action = DISABLE_SYNC_ON_CLIENT;
+  client_cmd.error_type = ENCRYPTION_OBSOLETE;
+
+  base::HistogramTester histogram_tester;
+  service()->OnActionableError(client_cmd);
+
+  ASSERT_FALSE(service()->IsSyncFeatureEnabled());
+
+  histogram_tester.ExpectUniqueSample(
+      "Sync.PassphraseTypeUponNotMyBirthdayOrEncryptionObsolete",
+      /*sample=*/kPassphraseType,
+      /*expected_bucket_count=*/1);
 }
 
 // Verify a that local sync mode isn't impacted by sync being disabled.
