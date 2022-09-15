3ea4e183d32eab08b5079554cb90d2be9ef74374
Steven Bennetts <stevenjb@chromium.org>
2022-09-15 22:29:41 +0000

    Introduce NetworkPortalSigninController
    This extracts handing of the signin UI responsibilities to a separate
    controller in preparation for changes to the captive portal UI.
    
    BUG=b:245578628
    TEST=unit_tests --gtest_filter=NetworkPortalDetectorImplTest* && \
         browser_tests --gtest_filter=NetworkPortalDetectorImplBrowserTest*
    
    
    Cleanup NetworkPortalNotificationController
    
    This CL does the following cleanup:
    1. Removes NetworkPortalNotificationController from the chromeos namespace
    2. Removes the NetworkPortalWebDialog co-dependency, using a Delegate
       interface instead.
    3. Uses a private internal Delegate to avoid exposing Show/CloseDialog as
       public methods.
    
    These changes have no behavioral change but will facilitate additional
    refactoring where NetworkPortalNotificationController supports multiple
    delegates.
    
    Change-Id: Id8911d6876de7bd5385a436bd81ee2007aca5d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894657
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047708}

--- chrome/browser/ui/ash/network/network_portal_notification_controller.h
+++ chrome/browser/ui/ash/network/network_portal_notification_controller.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_NETWORK_NETWORK_PORTAL_NOTIFICATION_CONTROLLER_H_
 #define CHROME_BROWSER_UI_ASH_NETWORK_NETWORK_PORTAL_NOTIFICATION_CONTROLLER_H_
 
+#include <memory>
 #include <string>
 
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/ash/net/network_portal_web_dialog.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler_observer.h"
 #include "components/session_manager/core/session_manager_observer.h"
@@ -18,13 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 class NetworkPortalNotificationControllerTest;
+class NetworkPortalSigninController;
 
 // Shows a message center notification when the networking stack detects a
 // captive portal.
 class NetworkPortalNotificationController
     : public NetworkStateHandlerObserver,
-      public session_manager::SessionManagerObserver,
-      public NetworkPortalWebDialog::Delegate {
+      public session_manager::SessionManagerObserver {
  public:
   // The values of these metrics are being used for UMA gathering, so it is
   // important that they don't change between releases.
@@ -46,23 +46,13 @@ class NetworkPortalNotificationController
 
   ~NetworkPortalNotificationController() override;
 
-  // NetworkPortalWebDialog::Delegate
-  void OnDialogDestroyed(const NetworkPortalWebDialog* dialog) override;
-
   // Ignores "No network" errors in browser tests.
   void SetIgnoreNoNetworkForTesting();
 
   bool IsDialogShownForTesting() const;
 
  private:
-  class Delegate;
   friend NetworkPortalNotificationControllerTest;
-
-  // Creates NetworkPortalWebDialog.
-  void ShowDialog();
-
-  // Destroys NetworkPortalWebDialog.
-  void CloseDialog();
 
   // Creates the default notification informing the user that a captive portal
   // has been detected. On click the captive portal login page is opened in the
@@ -83,13 +73,12 @@ class NetworkPortalNotificationController
   // Last network guid for which notification was displayed.
   std::string last_network_guid_;
 
-  // Currently displayed authorization dialog, or NULL if none.
-  NetworkPortalWebDialog* dialog_ = nullptr;
+  // The signin controller is responsible for displaying the captive portal
+  // signin UI.
+  std::unique_ptr<NetworkPortalSigninController> signin_controller_;
 
   // Do not close Portal Login dialog on "No network" error in browser tests.
   bool ignore_no_network_for_testing_ = false;
-
-  base::WeakPtrFactory<NetworkPortalNotificationController> weak_factory_{this};
 };
 
 }  // namespace ash
