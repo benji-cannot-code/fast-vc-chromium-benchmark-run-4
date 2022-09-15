a041291cc727c739566a6490784106dd477c6fa0
Andrii Natiahlyi <natiahlyi@google.com>
2022-09-15 07:51:54 +0000

    [Passwords] Use <h1> for dialog titles to aid navigation using screen readers
    Fixed: 1363572
    Change-Id: Ifc8c7b020416e90b936071f2d949be560328862d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894415
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047348}

--- chrome/browser/resources/settings/autofill_page/password_remove_confirmation_dialog.ts
+++ chrome/browser/resources/settings/autofill_page/password_remove_confirmation_dialog.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import '../i18n_setup.js';
+import './passwords_shared.css.js';
 
 import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
