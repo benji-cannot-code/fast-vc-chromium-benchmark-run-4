934ae6c3fb5916cd050e3b9d027ec44631f95ea4
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-09-15 20:57:32 +0000

    [last-baseline] Add RuntimeEnabledFlag and re-enable parsing.
    This adds a RuntimeEnabledFeatureFlag for last-baseline support, and
    re-enables the parsing.
    
    This isn't a straight revert of:
    https://chromium-review.googlesource.com/c/chromium/src/+/2300086
    
    As we also supported "align-content: last baseline" previously. We'll
    do the align-content feature separately.
    
    Removes fast/alignment/parse-* as these seem covered by WPT tests.
    
    Adds basic support for flex abspos static position as this was easier
    to mark passing than rebaseline these tests.
    
    Bug: 885175
    Change-Id: I827e85d4e261a0617f6cc3dd742b49f544b6218b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894691
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047668}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -155,7 +155,8 @@ AxisEdge CrossAxisStaticPositionEdge(const ComputedStyle& style,
     return AxisEdge::kEnd;
   }
 
-  if (alignment == ItemPosition::kFlexEnd)
+  if (alignment == ItemPosition::kFlexEnd ||
+      alignment == ItemPosition::kLastBaseline)
     return AxisEdge::kEnd;
 
   if (alignment == ItemPosition::kCenter)
