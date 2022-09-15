7f07623091907e1cfaacd59c5f25c473e15aae3c
Alexander Tekle <alexandertekle@google.com>
2022-09-15 22:44:02 +0000

    Add Usage Data to Autofill Wallet Model Type Controller
    Bug: 1341387
    Change-Id: I086ae814c82d068f20fa54d1029f5dc0ef24e20c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896785
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047715}

--- components/autofill/core/browser/payments/autofill_wallet_model_type_controller.cc
+++ components/autofill/core/browser/payments/autofill_wallet_model_type_controller.cc
@@ -32,7 +32,8 @@ AutofillWalletModelTypeController::AutofillWalletModelTypeController(
       sync_service_(sync_service) {
   DCHECK(type == syncer::AUTOFILL_WALLET_DATA ||
          type == syncer::AUTOFILL_WALLET_METADATA ||
-         type == syncer::AUTOFILL_WALLET_OFFER);
+         type == syncer::AUTOFILL_WALLET_OFFER ||
+         type == syncer::AUTOFILL_WALLET_USAGE);
   SubscribeToPrefChanges();
   sync_service_->AddObserver(this);
 }
@@ -52,7 +53,8 @@ AutofillWalletModelTypeController::AutofillWalletModelTypeController(
       sync_service_(sync_service) {
   DCHECK(type == syncer::AUTOFILL_WALLET_DATA ||
          type == syncer::AUTOFILL_WALLET_METADATA ||
-         type == syncer::AUTOFILL_WALLET_OFFER);
+         type == syncer::AUTOFILL_WALLET_OFFER ||
+         type == syncer::AUTOFILL_WALLET_USAGE);
   SubscribeToPrefChanges();
   sync_service_->AddObserver(this);
 }
