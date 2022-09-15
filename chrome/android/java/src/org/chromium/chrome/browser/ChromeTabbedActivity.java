966ece97f477fdb5fe673d92344195b9b9da2e5b
Lijin Shen <lazzzis@google.com>
2022-09-15 19:46:19 +0000

    Disable ReturnToChromeBackPressHandler when StartSurface is disabled
    StartSurface is disabled on tablet.
    
    Bug: 1362590
    Change-Id: I59109fff1fba00decf1bf051ead431abaf209ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894589
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047626}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2287,7 +2287,8 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
         }
 
         // If we aren't in the overview mode, we handle the Tab that is opened from Start Surface.
-        if (!isInOverviewMode() && ReturnToChromeUtil.isTabFromStartSurface(currentTab)) {
+        if (!isInOverviewMode() && !isTablet()
+                && ReturnToChromeUtil.isTabFromStartSurface(currentTab)) {
             returnToOverviewModeOnBackPressed();
             BackPressManager.record(BackPressHandler.Type.TAB_RETURN_TO_CHROME_START_SURFACE);
             return true;
@@ -2341,7 +2342,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
     }
 
     private void initializeBackPressHandlers() {
-        if (mReturnToChromeBackPressHandler == null) {
+        if (mReturnToChromeBackPressHandler == null && !isTablet()) {
             mReturnToChromeBackPressHandler = new ReturnToChromeBackPressHandler(
                     getActivityTabProvider(), this::returnToOverviewModeOnBackPressed);
             mBackPressManager.addHandler(mReturnToChromeBackPressHandler,
