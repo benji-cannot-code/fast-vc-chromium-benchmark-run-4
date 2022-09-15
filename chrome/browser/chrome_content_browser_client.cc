faa747f478e57e4ad1d1e2e66c862227cacd6a8f
Youssef Esmat <youssefesmat@chromium.org>
2022-09-15 21:21:23 +0000

    Do not enable core scheduling for ash renderers when lacros only is used.
    When lacros is the only browser, ash renderers will not run untrusted
    code. In which case, we do not need to enable core scheduler cookies for
    ash renderers allowing them to run on HTs.
    
    Bug: b:218686113
    Change-Id: I00dd250416517fc6b03cc8223dd6f7cc24fa7c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3824677
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Youssef Esmat <youssefesmat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047677}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/accessibility/accessibility_labels_service.h"
 #include "chrome/browser/accessibility/accessibility_labels_service_factory.h"
 #include "chrome/browser/after_startup_task_utils.h"
+#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/bluetooth/chrome_bluetooth_delegate_impl_client.h"
 #include "chrome/browser/browser_about_handler.h"
 #include "chrome/browser/browser_features.h"
@@ -2455,6 +2456,10 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
     if (!login_profile.empty())
       command_line->AppendSwitchASCII(ash::switches::kLoginProfile,
                                       login_profile);
+
+    if (!crosapi::browser_util::IsAshWebBrowserEnabled()) {
+      command_line->AppendSwitch(switches::kAshWebBrowserDisabled);
+    }
 #endif
 
     MaybeCopyDisableWebRtcEncryptionSwitch(command_line, browser_command_line,
