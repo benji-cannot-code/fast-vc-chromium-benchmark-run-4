9d921dbc2f388cd59ff601cdc2513bd5099f4c87
Benjamin Williams <bwwilliams@google.com>
2022-09-15 22:52:43 +0000

    Create StandardPromoAlertHandler & StandardPromoAlertProvider.
    This change introduces a new type of promo—a native iOS alert-styled
    promo—enabling the Restore Shorty multi-arm rollout to be possible.
    
    Video:
    https://drive.google.com/file/d/1Sk3JgipbQCv8ULV3hscKSL5BGMImV9Ew/view?usp=sharing&resourcekey=0-v4_kAneU6rTVXf7Dyjuzkw
    
    Change-Id: I5c9fac3014c5beae69e4aa39209bb038519adb7c
    Bug: 1363957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3898315
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047723}

--- ios/chrome/browser/ui/post_restore_signin/post_restore_signin_provider.h
+++ ios/chrome/browser/ui/post_restore_signin/post_restore_signin_provider.h
@@ -7,13 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_POST_RESTORE_SIGNIN_POST_RESTORE_SIGNIN_PROVIDER_H_
 
 #import "ios/chrome/browser/ui/promos_manager/bannered_promo_view_provider.h"
+#import "ios/chrome/browser/ui/promos_manager/standard_promo_alert_provider.h"
 
 // Provider for displaying the Post Restore Sign-in Promo.
 //
 // The Post Restore Sign-in promo comes in two variations: (1) A fullscreen,
 // FRE-like promo, and (2) a native iOS alert promo. This handler provides the
 // necessary data and functionality to power both variations of this promo.
-@interface PostRestoreSignInProvider : NSObject <BanneredPromoViewProvider>
+@interface PostRestoreSignInProvider
+    : NSObject <BanneredPromoViewProvider, StandardPromoAlertProvider>
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_POST_RESTORE_SIGNIN_POST_RESTORE_SIGNIN_PROVIDER_H_
---
+++ ios/chrome/browser/ui/promos_manager/standard_promo_alert_handler.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ALERT_HANDLER_H_
+#define IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ALERT_HANDLER_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol StandardPromoAlertHandler <NSObject>
+
+@required
+
+// The "Default Action" was touched.
+- (void)standardPromoAlertDefaultAction;
+
+@optional
+
+// The "Cancel Action" button was touched.
+- (void)standardPromoAlertCancelAction;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ALERT_HANDLER_H_
