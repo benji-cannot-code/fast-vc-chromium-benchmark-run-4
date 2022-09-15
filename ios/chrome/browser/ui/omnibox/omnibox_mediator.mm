74bde1d31d0fed681b2bedc43a811bb2fe5e3ccb
Christian Xu <christianxu@google.com>
2022-09-15 09:40:16 +0000

    [iOS][Omnibox] Fix omnibox favicon
    The omnibox is setting the first suggestion's favicon as the leading
     image of the text field.
    On NTP when the first suggestion doesn't have an URL (ex: clipboard
     suggestion), it shows the last cached icon, which can be any icon.
    This is also happening when highlighting (pressing arrow keys on a hardware keyboard) those kind of suggestions.
    
    This CL:
    - Sets the default icon to the default browser when the omnibox is
      empty.
    - Sets the same icon as in the popup when the suggestion doesn't have
      a valid url. (ex: Clipboard suggestions)
    
    Bug: 1363321
    Change-Id: I4ebaa7123f5a2d003dccacaa65eb8fb8afe4e064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894421
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047381}

--- ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/main/default_browser_scene_agent.h"
 #import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_consumer.h"
+#import "ios/chrome/browser/ui/omnibox/omnibox_suggestion_icon_util.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_util.h"
 #import "ios/chrome/browser/ui/omnibox/popup/autocomplete_suggestion.h"
 #import "ios/chrome/browser/ui/ui_feature_flags.h"
@@ -132,12 +133,19 @@ using base::UserMetricsAction;
     [self loadDefaultSearchEngineFaviconWithCompletion:^(UIImage* image) {
       [weakSelf.consumer updateAutocompleteIcon:image];
     }];
-  } else {
-    // Show favicon.
+  } else if (suggestion.destinationUrl.gurl.is_valid()) {
+    // Show url favicon when it's valid.
     [self loadFaviconByPageURL:suggestion.destinationUrl.gurl
                     completion:^(UIImage* image) {
                       [weakSelf.consumer updateAutocompleteIcon:image];
                     }];
+  } else if (isFirstUpdate) {
+    // When no suggestion is highlighted (aka. isFirstUpdate) show the default
+    // browser icon.
+    [self setDefaultLeftImage];
+  } else {
+    // When a suggestion is highlighted, show the same icon as in the popup.
+    [self.consumer updateAutocompleteIcon:suggestion.matchTypeIcon];
   }
 }
 
@@ -162,7 +170,7 @@ using base::UserMetricsAction;
                   completion:(void (^)(UIImage* image))completion {
   // Can't load favicons without a favicon loader.
   DCHECK(self.faviconLoader);
-
+  DCHECK(pageURL.is_valid());
   // Remember which favicon is loaded in case we start loading a new one
   // before this one completes.
   self.latestFaviconURL = pageURL;
