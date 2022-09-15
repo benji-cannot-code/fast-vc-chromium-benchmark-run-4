b45b6e1688101411895c51060fa170d396d816f2
Chung-Sheng Wu <chungsheng@google.com>
2022-09-15 06:53:05 +0000

    mojo: Log error when trying to add known client
    Change the DLOG to LOG when the broker trying to add a known client.
    This should be a kind of error we want to catch not only in debug build.
    
    Bug: b:242961863
    Change-Id: I43b1b37a8f8cffeba131878e05227e69a3702ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3895457
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047325}

--- mojo/core/node_controller.cc
+++ mojo/core/node_controller.cc
@@ -942,7 +942,7 @@ void NodeController::OnAddBrokerClient(const ports::NodeName& from_node,
   }
 
   if (GetPeerChannel(client_name)) {
-    DLOG(ERROR) << "Ignoring AddBrokerClient for known client.";
+    LOG(ERROR) << "Ignoring AddBrokerClient for known client.";
     DropPeer(from_node, nullptr);
     return;
   }
