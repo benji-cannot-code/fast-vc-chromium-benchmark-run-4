e3837569a1f1c42f6aa966b7cc376dd53ba74586
Kush Sinha <sinhak@chromium.org>
2022-09-15 13:38:55 +0000

    Fix dangling ptrs in account_manager_facade_impl_unittest
    Some tests in account_manager_facade_impl_unittest add an observer to
    `AccountManagerFacade` but do not remove it at the end of the test. Fix
    this by using `base::ScopedObservation` to automatically de-register the
    observer at the end of the test.
    
    Change-Id: I6150b0617b4c5409e4306c2e196b20942ac6089c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896815
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047447}

--- components/account_manager_core/account_manager_facade_impl_unittest.cc
+++ components/account_manager_core/account_manager_facade_impl_unittest.cc
@@ -334,7 +334,9 @@ TEST_F(AccountManagerFacadeImplTest, OnTokenUpsertedIsPropagatedToObservers) {
   std::unique_ptr<AccountManagerFacadeImpl> account_manager_facade =
       CreateFacade();
   testing::StrictMock<MockAccountManagerFacadeObserver> observer;
-  account_manager_facade->AddObserver(&observer);
+  base::ScopedObservation<AccountManagerFacade, AccountManagerFacade::Observer>
+      observation{&observer};
+  observation.Observe(account_manager_facade.get());
 
   Account account = CreateTestGaiaAccount(kTestAccountEmail);
   base::RunLoop run_loop;
@@ -348,7 +350,9 @@ TEST_F(AccountManagerFacadeImplTest, OnAccountRemovedIsPropagatedToObservers) {
   std::unique_ptr<AccountManagerFacadeImpl> account_manager_facade =
       CreateFacade();
   testing::StrictMock<MockAccountManagerFacadeObserver> observer;
-  account_manager_facade->AddObserver(&observer);
+  base::ScopedObservation<AccountManagerFacade, AccountManagerFacade::Observer>
+      observation{&observer};
+  observation.Observe(account_manager_facade.get());
 
   Account account = CreateTestGaiaAccount(kTestAccountEmail);
   base::RunLoop run_loop;
@@ -888,7 +892,9 @@ TEST_F(AccountManagerFacadeImplTest, ReportAuthError) {
   std::unique_ptr<AccountManagerFacadeImpl> account_manager_facade =
       CreateFacade();
   testing::StrictMock<MockAccountManagerFacadeObserver> observer;
-  account_manager_facade->AddObserver(&observer);
+  base::ScopedObservation<AccountManagerFacade, AccountManagerFacade::Observer>
+      observation{&observer};
+  observation.Observe(account_manager_facade.get());
 
   Account account = CreateTestGaiaAccount(kTestAccountEmail);
   GoogleServiceAuthError error =
