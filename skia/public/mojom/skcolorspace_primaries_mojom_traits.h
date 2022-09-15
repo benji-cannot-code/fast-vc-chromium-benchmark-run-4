db9929c84d53f6f3495ebb752bda9af34db9039c
Christopher Cameron <ccameron@chromium.org>
2022-09-15 09:05:57 +0000

    Plumb SkColorSpacePrimaries from DXGI to DisplayColorSpaces
    This is needed to provide accurate gamut queries when in advanced
    color mode (currently we lie and say that all displays are P3).
    
    Bug: 1274220
    Change-Id: I7facbd5233f2239b1b10c0b18ce4e6bfd1e627ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884040
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047373}

--- skia/public/mojom/skcolorspace_primaries_mojom_traits.h
+++ skia/public/mojom/skcolorspace_primaries_mojom_traits.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SKIA_PUBLIC_MOJOM_SKCOLORSPACE_PRIMARIES_MOJOM_TRAITS_H_
 #define SKIA_PUBLIC_MOJOM_SKCOLORSPACE_PRIMARIES_MOJOM_TRAITS_H_
 
+#include "skia/ext/skcolorspace_primaries.h"
 #include "skia/public/mojom/skcolorspace_primaries.mojom.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 
