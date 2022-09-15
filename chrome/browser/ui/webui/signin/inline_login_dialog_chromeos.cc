542c7750d809a5d93d6ec2c73ea47af085452c20
Kush Sinha <sinhak@chromium.org>
2022-09-15 13:04:32 +0000

    Use delegating ctor in `InlineLoginDialogChromeOS`
    There are 2 constructors in `InlineLoginDialogChromeOS` which can use
    delegation but have duplicate bodies instead. Fix them by delegating one
    of them to the other.
    
    Change-Id: I558bf5a0868d07278e2256eec7aafc617c72f635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899191
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047431}

--- chrome/browser/ui/webui/signin/inline_login_dialog_chromeos.cc
+++ chrome/browser/ui/webui/signin/inline_login_dialog_chromeos.cc
@@ -167,12 +167,7 @@ InlineLoginDialogChromeOS::InlineLoginDialogChromeOS()
     : InlineLoginDialogChromeOS(GetInlineLoginUrl(std::string())) {}
 
 InlineLoginDialogChromeOS::InlineLoginDialogChromeOS(const GURL& url)
-    : SystemWebDialogDelegate(url, std::u16string() /* title */),
-      delegate_(this),
-      url_(url) {
-  DCHECK(!dialog);
-  dialog = this;
-}
+    : InlineLoginDialogChromeOS(url, absl::nullopt, base::DoNothing()) {}
 
 InlineLoginDialogChromeOS::InlineLoginDialogChromeOS(
     const GURL& url,
