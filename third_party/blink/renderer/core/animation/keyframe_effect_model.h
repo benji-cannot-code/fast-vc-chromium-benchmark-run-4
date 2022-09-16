415a4ed60f6202a7fb598d102fdd2cdfb724cd75
Daniel Cheng <dcheng@chromium.org>
2022-09-16 16:24:36 +0000

    Use base::FunctionRef in blink::KeyframeEffectModelBase.
    std::function<Sig> is banned in Chrome; since the callables are policy
    checks that are synchronusly invoked, migrate them to base::FunctionRef.
    
    base::FunctionRef's underlying implementation is backed by
    absl::FunctionRef, which does not support mutable lambdas; however, the
    lambda in question does not need to be marked mutable so its mutable
    qualification has been removed.
    
    Bug: 1364584
    Change-Id: I19ef051d72b1b1e54fbf64972102f28b5bd64592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900919
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048050}

--- third_party/blink/renderer/core/animation/keyframe_effect_model.h
+++ third_party/blink/renderer/core/animation/keyframe_effect_model.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/functional/function_ref.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/animation/animation_effect.h"
 #include "third_party/blink/renderer/core/animation/effect_model.h"
@@ -190,25 +191,25 @@ class CORE_EXPORT KeyframeEffectModelBase : public EffectModel {
   // Clears the various bits of cached data that this class has.
   void ClearCachedData();
 
-  using ShouldSnapshotPropertyCallback =
-      std::function<bool(const PropertyHandle&)>;
-  using ShouldSnapshotKeyframeCallback =
-      std::function<bool(const PropertySpecificKeyframe&)>;
+  using ShouldSnapshotPropertyFunction =
+      base::FunctionRef<bool(const PropertyHandle&)>;
+  using ShouldSnapshotKeyframeFunction =
+      base::FunctionRef<bool(const PropertySpecificKeyframe&)>;
 
   bool SnapshotCompositableProperties(
       Element& element,
       const ComputedStyle& computed_style,
       const ComputedStyle* parent_style,
-      ShouldSnapshotPropertyCallback should_process_property_callback,
-      ShouldSnapshotKeyframeCallback should_process_keyframe_callback) const;
+      ShouldSnapshotPropertyFunction should_process_property,
+      ShouldSnapshotKeyframeFunction should_process_keyframe) const;
 
   bool SnapshotCompositorKeyFrames(
       const PropertyHandle& property,
       Element& element,
       const ComputedStyle& computed_style,
       const ComputedStyle* parent_style,
-      ShouldSnapshotPropertyCallback should_process_property_callback,
-      ShouldSnapshotKeyframeCallback should_process_keyframe_callback) const;
+      ShouldSnapshotPropertyFunction should_process_property,
+      ShouldSnapshotKeyframeFunction should_process_keyframe) const;
 
   KeyframeVector keyframes_;
   // The spec describes filtering the normalized keyframes at sampling time
