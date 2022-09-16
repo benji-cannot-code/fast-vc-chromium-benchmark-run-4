9803ed40edf53f338042d7cd1113f7801fc36515
Thomas Lukaszewicz <tluk@chromium.org>
2022-09-16 20:11:08 +0000

    [color] Add sys color ids and color mixer to //ui/color
    This CL adds color ids and a mixer for GM3 system colors.
    
    The 4 scheme color tokens for each of the primary, secondary,
    tertiary and error colors have been added. This also adds surface
    and sys outline tokens.
    
    For specifics on mappings to ref tokens see the spreadsheet below.
    go/chrome-desktop-tokens
    
    Bug: 1364625
    Change-Id: I90cfbb818a917be0ac9fb638b847a94bdad4794d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900662
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048182}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -93,6 +93,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRefNeutralVariant99) \
   E_CPONLY(kColorRefNeutralVariant100) \
   \
+  /* UI system color tokens */ \
+  E_CPONLY(kColorSysPrimary) \
+  E_CPONLY(kColorSysOnPrimary) \
+  E_CPONLY(kColorSysPrimaryContainer) \
+  E_CPONLY(kColorSysOnPrimaryContainer) \
+  E_CPONLY(kColorSysSecondary) \
+  E_CPONLY(kColorSysOnSecondary) \
+  E_CPONLY(kColorSysSecondaryContainer) \
+  E_CPONLY(kColorSysOnSecondaryContainer) \
+  E_CPONLY(kColorSysTertiary) \
+  E_CPONLY(kColorSysOnTertiary) \
+  E_CPONLY(kColorSysTertiaryContainer) \
+  E_CPONLY(kColorSysOnTertiaryContainer) \
+  E_CPONLY(kColorSysError) \
+  E_CPONLY(kColorSysOnError) \
+  E_CPONLY(kColorSysErrorContainer) \
+  E_CPONLY(kColorSysOnErrorContainer) \
+  E_CPONLY(kColorSysSurfaceVariant) \
+  E_CPONLY(kColorSysOnSurfaceVariant) \
+  E_CPONLY(kColorSysOutline) \
   /* Core color concepts */ \
   /* The kColorAccent color id is used in color_provider_css_colors_test.ts. \
   If changing the variable name, the variable name in the test needs to be \
