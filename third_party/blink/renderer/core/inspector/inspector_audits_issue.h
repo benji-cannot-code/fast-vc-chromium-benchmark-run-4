08a5dac14e1e96f17dd0761f29c9eee74b94d86a
Henrik Boström <hbos@chromium.org>
2022-09-16 07:49:08 +0000

    Unship non-standard deprecated feature googIPv6.
    This API has had a deprecation warning for the past 6 months and was
    approved for removal in M108:
    https://groups.google.com/u/1/a/chromium.org/g/blink-dev/c/kyFuSzObC-Y
    
    The usage has been stable around 0.0000477% but nobody has said a word
    for all this time and I seriously doubt people will even notice this
    being silently ignored.
    
    Bug: 1315576
    Change-Id: I8a7c2f717822bbdf2136de4aeada597ce495f168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892914
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047873}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.h
@@ -59,7 +59,6 @@ enum class DeprecationIssueType {
   kHostCandidateAttributeGetter,
   kIdentityInCanMakePaymentEvent,
   kInsecurePrivateNetworkSubresourceRequest,
-  kLegacyConstraintGoogIPv6,
   kLocalCSSFileExtensionRejected,
   kMediaSourceAbortRemove,
   kMediaSourceDurationTruncatingBuffered,
