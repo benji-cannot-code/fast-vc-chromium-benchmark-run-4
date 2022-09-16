4be0a1b0f94a84a0c9b7764032e66a3bfb3704dd
Rune Lillesveen <futhark@chromium.org>
2022-09-16 14:56:37 +0000

    Corrected DCHECK for allowing marking style dirty
    As long as we have legacy layout in combination with container queries,
    we may end up doing a resume of a skipped style recalc during layout
    tree rebuild. Additionally, we have a scope allowing marking style dirty
    from style recalc for a couple of exceptional cases. When these happen
    at the same time we may mark style dirty from within layout tree
    rebuild.
    
    Adjust DCHECK to account for that.
    
    Bug: 1362391
    Change-Id: I30c507607938040a4fe8948e731bb71275b479c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901994
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048013}

--- third_party/blink/renderer/core/css/style_engine.h
+++ third_party/blink/renderer/core/css/style_engine.h
@@ -361,6 +361,7 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   AncestorAnalysis AnalyzeAncestors(const Node&);
 
   bool MarkReattachAllowed() const;
+  bool MarkStyleDirtyAllowed() const;
 
   CSSFontSelector* GetFontSelector() { return font_selector_; }
 
