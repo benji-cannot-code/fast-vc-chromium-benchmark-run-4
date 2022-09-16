c21a4e5adde1c0feb885163ed5919b89f0348864
Phillip Pearson <philpearson@google.com>
2022-09-16 16:34:00 +0000

    Don't block Borealis startup on SyncBorealisDisk failure.
    Bug: b:245839776
    Change-Id: Ie9fd6d8b395dfc288000831c9eaf8ba1d38e7f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901193
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Phillip Pearson <philpearson@google.com>
    Auto-Submit: Phillip Pearson <philpearson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048056}

--- chrome/browser/ash/borealis/borealis_task_unittest.cc
+++ chrome/browser/ash/borealis/borealis_task_unittest.cc
@@ -226,7 +226,7 @@ TEST_F(BorealisTasksTest,
   task_environment_.RunUntilIdle();
 }
 
-TEST_F(BorealisTasksTest, SyncBorealisDiskFails) {
+TEST_F(BorealisTasksTest, SyncBorealisDiskFailureIgnored) {
   auto disk_mock = std::make_unique<DiskManagerMock>();
   EXPECT_CALL(*disk_mock, SyncDiskSize(_))
       .WillOnce(testing::Invoke(
@@ -242,8 +242,7 @@ TEST_F(BorealisTasksTest, SyncBorealisDiskFails) {
           }));
 
   CallbackFactory callback_factory;
-  EXPECT_CALL(callback_factory, Call(BorealisStartupResult::kSyncDiskFailed,
-                                     "Failed to sync disk: error message"));
+  EXPECT_CALL(callback_factory, Call(BorealisStartupResult::kSuccess, ""));
   context_->SetDiskManagerForTesting(std::move(disk_mock));
   SyncBorealisDisk task;
   task.Run(context_.get(), callback_factory.BindOnce());
