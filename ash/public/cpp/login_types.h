a888b2c1957cdd635cae989f53fa7b5495612edc
Danila Kuzmin <dkuzmin@google.com>
2022-09-16 13:48:35 +0000

    [OOBE][ErrorScreen]: Remove deprecated supervised user creation error
    Bug: b:239914175
    Change-Id: Ia1b572aeca8b3a8ca4486bfea70fea303186084a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892921
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047977}

--- ash/public/cpp/login_types.h
+++ ash/public/cpp/login_types.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/smartlock_state.h"
 #include "base/callback.h"
 #include "base/time/time.h"
-#include "base/token.h"
 #include "chromeos/components/security_token_pin/constants.h"
 #include "components/account_id/account_id.h"
 
@@ -36,7 +35,7 @@ enum class OobeDialogState {
   WRONG_HWID_WARNING = 3,
 
   // Showing supervised user creation screen.
-  SUPERVISED_USER_CREATION_FLOW = 4,
+  DEPRECATED_SUPERVISED_USER_CREATION_FLOW = 4,
 
   // Showing SAML password confirmation screen.
   SAML_PASSWORD_CONFIRM = 5,
