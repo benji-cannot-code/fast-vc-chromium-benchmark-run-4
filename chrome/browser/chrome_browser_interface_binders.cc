2057d9432d8d0588aedebd3a0be66b551a37a8a4
Andrey Kosyakov <caseq@chromium.org>
2022-09-16 21:52:45 +0000

    Revert "[User Education] Instrument security page for help bubble promo."
    This reverts commit 7a1a941ee8f355b675ac9878a598a0c6e999e00a.
    
    Reason for revert: suspected for breaking mac-archive-rel build (https://ci.chromium.org/ui/p/chromium/builders/ci/mac-archive-rel/37288/overview)
    
    Original change's description:
    > [User Education] Instrument security page for help bubble promo.
    >
    > This allows a help bubble to appear on the "Enhanced Protection" item of
    > the Chrome security settings page.
    >
    > Change-Id: I4663bdd5e89d9089dfb2650eb2f805cf4130cb28
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871526
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1048188}
    
    Change-Id: I71cf84bce5531e0f7f035355a46deafc62c72721
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901060
    Owners-Override: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048214}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -945,8 +945,7 @@ void PopulateChromeWebUIFrameBinders(
       >(map);
 
   RegisterWebUIControllerInterfaceBinder<
-      help_bubble::mojom::HelpBubbleHandlerFactory, InternalsUI,
-      settings::SettingsUI>(map);
+      help_bubble::mojom::HelpBubbleHandlerFactory, InternalsUI>(map);
 
 #if !defined(OFFICIAL_BUILD)
   RegisterWebUIControllerInterfaceBinder<foo::mojom::FooHandler, NewTabPageUI>(
