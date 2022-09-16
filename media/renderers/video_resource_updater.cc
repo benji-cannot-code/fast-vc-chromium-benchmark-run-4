7d308b6325e1c7db24aaee1a58327defebb6f196
Andres Calderon Jaramillo <andrescj@chromium.org>
2022-09-16 17:12:21 +0000

    Reland: "OverlayProcessorOzone: Reject BT.2020 and full-range videos."
    Original change (CL:3662252) description:
    
    This CL extends the AllowColorSpaceCombination() function to prevent
    promoting YVU_420 and NV12 quads to overlays if they use full range YUV
    samples or BT.2020 primaries.
    
    The reason is that since CL:2336347, we always tell the DRM/KMS driver
    to use BT.601 with limited range. This would be okay if we also did the
    same for compositing, but since CL:3662321, we tell the EGL driver to
    use the right range.
    
    ---
    
    Changes since the original CL (CL:3662252):
    
    The original CL was reverted due to a couple of issues. I describe them
    below together with the corresponding fixes:
    
    1) b:241067642: at least one HDR YouTube video was being rendered
       incorrectly. The culprit was actually CL:3749351 which has been
       reverted. However, without that CL, CL:3662252 would prevent all
       videos from being promoted to overlays. That's why CL:3662252 also
       needed to be reverted. To fix this HDR problem, this new CL takes a
       different approach for obtaining the YUV color space for videos: we
       essentially split the |color_space| field in
       viz::TransferableResource into two fields (credit to ccameron@ for
       the suggestion): |color_space| which is to be the color space that
       retains YUV-to-RGB conversion information and
       |color_space_when_sampled| which contains the color space that a GPU
       sampler would get the samples in (if different from |color_space|).
       That way, for a video, |surface_iterator->color_space| corresponds to
       the first color space and the OverlayProcessorOzone can use that
       information appropriately to decide if an NV12 resource should be
       promoted to overlay. The compositor can use the second color space to
       know if a color space conversion is needed (from what the sampler
       sees to some other color space).
    
    2) b:243423557 and b:243150091: video overlays got broken for LaCrOS.
       LaCrOS doesn't seem to be plumbing the correct color space for (at
       least) NV12 resources. For example, when running
       graphics.HTMLVideoRoundedCornersUnderlay.lacros, ash-chrome sees
       {primaries:BT709, transfer:SRGB, matrix:RGB, range:FULL} even though
       the VideoResourceUpdater in lacros-chrome sends
       {primaries:SMPTE170M, transfer:SMPTE170M, matrix:SMPTE170M,
       range:LIMITED}. So, to work around this until b:243150091 is fixed,
       I've added an IsYUVColorSpace() check to make sure we don't reject
       NV12 resources for overlay promotion unless the color space actually
       corresponds to a YUV resource.
    
    Besides these changes, the logic in ui/gl/gl_image_native_pixmap.cc is
    made consistent with what we now do in the OverlayProcessorOzone.
    
    Bug: b:241067642, b:233667677, b:220336463, b:227956160, b:243150091
    Test: play a full-range video and make sure it's not promoted to overlay
    Test: graphics.HTMLVideoRoundedCornersUnderlay.lacros on volteer
    Change-Id: Ica2fdf2cd8fe5bc617f5ba32929fec41c5d0d995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855381
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048078}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -862,10 +862,12 @@ VideoFrameExternalResources VideoResourceUpdater::CreateForHardwarePlanes(
                 << VideoPixelFormatToString(video_frame->format());
     return external_resources;
   }
+  absl::optional<gfx::ColorSpace> resource_color_space_when_sampled;
   if (external_resources.type == VideoFrameResourceType::RGB ||
       external_resources.type == VideoFrameResourceType::RGBA ||
       external_resources.type == VideoFrameResourceType::RGBA_PREMULTIPLIED) {
-    resource_color_space = resource_color_space.GetAsFullRangeRGB();
+    resource_color_space_when_sampled =
+        resource_color_space.GetAsFullRangeRGB();
   }
 
   const size_t num_textures = video_frame->NumTextures();
@@ -875,8 +877,10 @@ VideoFrameExternalResources VideoResourceUpdater::CreateForHardwarePlanes(
       break;
 
     if (copy_required) {
-      CopyHardwarePlane(video_frame.get(), resource_color_space, mailbox_holder,
-                        &external_resources);
+      CopyHardwarePlane(
+          video_frame.get(),
+          resource_color_space_when_sampled.value_or(resource_color_space),
+          mailbox_holder, &external_resources);
     } else {
       const size_t width = video_frame->columns(i);
       const size_t height = video_frame->rows(i);
@@ -887,6 +891,8 @@ VideoFrameExternalResources VideoResourceUpdater::CreateForHardwarePlanes(
           viz::GetResourceFormat(buffer_formats[i]),
           video_frame->metadata().allow_overlay);
       transfer_resource.color_space = resource_color_space;
+      transfer_resource.color_space_when_sampled =
+          resource_color_space_when_sampled;
       transfer_resource.hdr_metadata = video_frame->hdr_metadata();
       if (video_frame->metadata().read_lock_fences_enabled) {
         transfer_resource.synchronization_type = viz::TransferableResource::
