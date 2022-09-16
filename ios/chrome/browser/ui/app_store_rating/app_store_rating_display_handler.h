e8946fa4ac1bf0a8ac41f56d55f6224dd3188ed2
Hira Mahmood <hiramahmood@google.com>
2022-09-16 22:02:55 +0000

    Create blank AppStoreRatingDisplayHandler class.
    This change creates a blank AppStoreRatingDisplayHandler class to
    integrate the Promos Manager into App Store Rating.
    
    Bug: 1359065
    Change-Id: Ie69c2c71cfd67af1658c4f9e962a1d1ca1805c7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899142
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048215}

---
+++ ios/chrome/browser/ui/app_store_rating/app_store_rating_display_handler.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_APP_STORE_RATING_APP_STORE_RATING_DISPLAY_HANDLER_H_
+#define IOS_CHROME_BROWSER_UI_APP_STORE_RATING_APP_STORE_RATING_DISPLAY_HANDLER_H_
+
+#import "ios/chrome/browser/ui/promos_manager/standard_promo_display_handler.h"
+
+// Handler for displaying the App Store Rating Promo.
+//
+// This handler is called by the Promos Manager and presents the App Store
+// Rating promo to eligible users. Users are considered eligible if they
+// 1) have used Chrome for at least 15 total days,
+// 2) have used Chrome for at least 3 days in the past 7 days,
+// 3) have enabled CPE, and
+// 4) have Chrome set as their default browser.
+@interface AppStoreRatingDisplayHandler : NSObject <StandardPromoDisplayHandler>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_APP_STORE_RATING_APP_STORE_RATING_DISPLAY_HANDLER_H_
---
+++ ios/chrome/browser/ui/app_store_rating/app_store_rating_display_handler.mm
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/app_store_rating/app_store_rating_display_handler.h"
+
+#import "ios/chrome/browser/promos_manager/constants.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation AppStoreRatingDisplayHandler
+
+#pragma mark - StandardPromoDisplayHandler
+
+- (void)handleDisplay {
+  // TODO(crbug.com/1364669): Call to Apple's SKStoreReviewController to request
+  // the display of the App Store Rating promo.
+}
+
+#pragma mark - PromoProtocol
+
+- (promos_manager::Promo)identifier {
+  return promos_manager::Promo::AppStoreRating;
+}
+
+@end
