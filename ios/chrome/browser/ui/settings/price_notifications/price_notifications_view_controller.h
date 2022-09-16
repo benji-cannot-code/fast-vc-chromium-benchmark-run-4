fc6e87d7fcede9baeceb65665dd84cd8003e2dc6
Joemer Ramos <joemerramos@chromium.org>
2022-09-16 18:27:56 +0000

    [iOS] Add comment to Price Notifications View Controller
    Forgot to add a comment to the price notifications view controller.
    
    Bug: 1360477
    Change-Id: I52c93b9641ea3d62d1906f9586206872c30275d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902421
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048118}

--- ios/chrome/browser/ui/settings/price_notifications/price_notifications_view_controller.h
+++ ios/chrome/browser/ui/settings/price_notifications/price_notifications_view_controller.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
+// View controller for Price Notifications setting.
 @interface PriceNotificationsViewController
     : SettingsRootTableViewController <PriceNotificationsConsumer,
                                        SettingsControllerProtocol>
