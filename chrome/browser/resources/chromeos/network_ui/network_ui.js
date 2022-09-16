9b4c17919cd7c0fb5ba0665ee7a3e0d2f7efb9bc
Fergus Dall <sidereal@google.com>
2022-09-16 08:38:31 +0000

    Revert "WebUI: Move traffic_counters/ to ash/webui/common/resources/"
    This reverts commit 95df929d471b5e98fedf6de03a132424e47d9704.
    
    Reason for revert: Required to revert CL:3889260
    
    Original change's description:
    > WebUI: Move traffic_counters/ to ash/webui/common/resources/
    >
    > As part of the move also migrating traffic_counters to import Polymer
    > HTML templates at runtime, since ash/webui/common/resources/BUILD.gn
    > is already setup with the appropriate GN rules (this is easier than
    > trying to do this separately).
    >
    > Bug: 1347740,1292025
    > Change-Id: I9234a16974e3d481c0216ba690a482ccfea82262
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896083
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1047868}
    
    Bug: 1347740,1292025,b:246956397
    Change-Id: I58353a4c46e8b5164acc0be6141e1a78e6567f70
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899700
    Auto-Submit: Fergus Dall <sidereal@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fergus Dall <sidereal@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1047885}

--- chrome/browser/resources/chromeos/network_ui/network_ui.js
+++ chrome/browser/resources/chromeos/network_ui/network_ui.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_components/chromeos/network/network_select.js';
 import 'chrome://resources/cr_components/chromeos/network_health/network_diagnostics.js';
 import 'chrome://resources/cr_components/chromeos/network_health/network_health_summary.js';
-import 'chrome://resources/ash/common/traffic_counters/traffic_counters.js';
+import 'chrome://resources/cr_components/chromeos/traffic_counters/traffic_counters.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/cr_elements/cr_tabs/cr_tabs.js';
