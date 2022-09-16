04c6f00ff7b1503a320cc057c9473ab6043f36a6
Jeeva Murugan <jeevam@google.com>
2022-09-16 05:11:02 +0000

    Added feature flag for more accessibility dictation commands
    I made this change so that when we go to add more accessibility
    dictation commands we can develop under a flag.
    
    Bug: 1362842
    Change-Id: I1a17e7fb38cc4d2665ab80f0f089574d4791b5d0
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892281
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Jeeva Murugan <jeevam@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047827}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3092,6 +3092,9 @@ extern const char kExoLockNotificationDescription[];
 extern const char kExperimentalAccessibilityDictationWithPumpkinName[];
 extern const char kExperimentalAccessibilityDictationWithPumpkinDescription[];
 
+extern const char kExperimentalAccessibilityDictationMoreCommandsName[];
+extern const char kExperimentalAccessibilityDictationMoreCommandsDescription[];
+
 extern const char kExperimentalAccessibilityGoogleTtsLanguagePacksName[];
 extern const char kExperimentalAccessibilityGoogleTtsLanguagePacksDescription[];
 
--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -128,6 +128,16 @@ bool IsExperimentalAccessibilityDictationWithPumpkinEnabled() {
       ::features::kExperimentalAccessibilityDictationWithPumpkin);
 }
 
+const base::Feature kExperimentalAccessibilityDictationMoreCommands{
+    "ExperimentalAccessibilityDictationMoreCommands",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
+
+bool IsExperimentalAccessibilityDictationMoreCommandsEnabled() {
+  return base::FeatureList::IsEnabled(
+      ::features::kExperimentalAccessibilityDictationMoreCommands);
+}
+
 const base::Feature kExperimentalAccessibilityGoogleTtsLanguagePacks{
     "ExperimentalAccessibilityGoogleTtsLanguagePacks",
     base::FEATURE_DISABLED_BY_DEFAULT};
