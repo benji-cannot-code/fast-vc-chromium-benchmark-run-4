599f700f7dac67b871d1166f5febaf620bcc4e21
Mark Pearson <mpearson@chromium.org>
2022-09-16 17:23:48 +0000

    Skip Creating Accessibility Label Service Sometimes
    It doesn't need to be created for System profiles or unusual ChromeOS profiles such as the lock screen profile or sign-in screen profile.
    
    It should continue being created for regular/incognito profiles and guest profiles.
    
    Rename one metric that was previously emitted when the instance was created for these unusual profile types.  (Deprecate the metric and replace it with a version 2.)  There are two other metrics emitted in the same function.  These do not have to be renamed because they're only emitted on Android.  Android doesn't use these unusual profile types so these Android metrics were never emitted at inappropriate times.
    
    Not tested interactively.
    
    AX-Relnotes: n/a.  This change should not be user-visible.
    
    Bug: 1284659
    Change-Id: I8912839ad3a203348d590c5e4235d2668efde19a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861308
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048087}

--- chrome/browser/accessibility/accessibility_labels_service_factory.cc
+++ chrome/browser/accessibility/accessibility_labels_service_factory.cc
@@ -5,8 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/accessibility/accessibility_labels_service_factory.h"
 
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/accessibility/accessibility_labels_service.h"
 #include "chrome/browser/profiles/profile.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/profiles/profile_helper.h"
+#endif
 
 // static
 AccessibilityLabelsService* AccessibilityLabelsServiceFactory::GetForProfile(
@@ -28,20 +33,30 @@ AccessibilityLabelsServiceFactory::GetInstance() {
   return base::Singleton<AccessibilityLabelsServiceFactory>::get();
 }
 
-// static
-KeyedService* AccessibilityLabelsServiceFactory::BuildInstanceFor(
-    Profile* profile) {
-  return new AccessibilityLabelsService(profile);
-}
-
 AccessibilityLabelsServiceFactory::AccessibilityLabelsServiceFactory()
     : ProfileKeyedServiceFactory(
           "AccessibilityLabelsService",
-          ProfileSelections::BuildRedirectedInIncognito()) {}
+          ProfileSelections::Builder()
+              .WithRegular(ProfileSelection::kRedirectedToOriginal)
+              // Use OTR profile for Guest Session.
+              .WithGuest(ProfileSelection::kOffTheRecordOnly)
+              // No service for system profile.
+              .WithSystem(ProfileSelection::kNone)
+              .Build()) {}
 
 AccessibilityLabelsServiceFactory::~AccessibilityLabelsServiceFactory() {}
 
 KeyedService* AccessibilityLabelsServiceFactory::BuildServiceInstanceFor(
-    content::BrowserContext* profile) const {
-  return BuildInstanceFor(static_cast<Profile*>(profile));
+    content::BrowserContext* context) const {
+  Profile* profile = Profile::FromBrowserContext(context);
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // ChromeOS creates various profiles (login, lock screen...) that do
+  // not display web content and thus do not need the accessibility labels
+  // service.
+  if (!chromeos::ProfileHelper::IsRegularProfile(profile))
+    return nullptr;
+#endif
+
+  return new AccessibilityLabelsService(profile);
 }
--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -254,6 +254,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_param_filter/content/url_param_filter_throttle.h"
 #include "components/variations/variations_associated_data.h"
 #include "components/variations/variations_switches.h"
+#include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/browser_child_process_host.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_main_parts.h"
@@ -6277,7 +6278,15 @@ bool ChromeContentBrowserClient::ShouldBlockRendererDebugURL(
 ui::AXMode ChromeContentBrowserClient::GetAXModeForBrowserContext(
     content::BrowserContext* browser_context) {
   Profile* profile = Profile::FromBrowserContext(browser_context);
-  return AccessibilityLabelsServiceFactory::GetForProfile(profile)->GetAXMode();
+  auto* service = AccessibilityLabelsServiceFactory::GetForProfile(profile);
+  if (service)
+    return service->GetAXMode();
+  // No AccessibilityLabelsService.  Return the current accessibility mode.
+  ui::AXMode ax_mode =
+      content::BrowserAccessibilityState::GetInstance()->GetAccessibilityMode();
+  // kLabelImages should only be set if there's an AccessibilityLabelsService.
+  DCHECK(!ax_mode.has_mode(ui::AXMode::kLabelImages));
+  return ax_mode;
 }
 
 #if BUILDFLAG(IS_ANDROID)
