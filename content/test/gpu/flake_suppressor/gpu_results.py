61c5c1f572606b19ef6c38ba66df6c38a95d0be7
Preethi Mohan <preethim@google.com>
2022-09-16 18:52:54 +0000

    flake_suppressor tool: Generalize the Results module.
    Bug: 1358733
    
    Change-Id: Iddbcde7884540c7549d1bf01dab8472a52e68c72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893117
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048133}

---
+++ content/test/gpu/flake_suppressor/gpu_results.py
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from typing import Tuple
+
+from flake_suppressor_common import results as results_module
+
+
+class GpuResultProcessor(results_module.ResultProcessor):
+  def GetTestSuiteAndNameFromResultDbName(self, result_db_name: str
+                                          ) -> Tuple[str, str]:
+    _, suite, __, test_name = result_db_name.split('.', 3)
+    return suite, test_name
