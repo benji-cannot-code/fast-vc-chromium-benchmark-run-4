b35de0774802f760131e1e236f89182b98b7ea19
Shuran Huang <shuuran@chromium.org>
2022-09-16 18:13:09 +0000

    Keep track of the last FPS public sets used by a profile
    Create a new DB table `browser_context_sets_version` to record the
    version of the public sets for a given browser_context_id.
    
    Bug: 1363707, 1219656
    Change-Id: Idaa8a7102d083d3e7d85b8aae78e032fda7aedc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899136
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048113}

--- content/browser/first_party_sets/first_party_sets_handler_impl.h
+++ content/browser/first_party_sets/first_party_sets_handler_impl.h
@@ -111,6 +111,7 @@ class CONTENT_EXPORT FirstPartySetsHandlerImpl : public FirstPartySetsHandler {
   }
 
   void GetPersistedPublicSetsForTesting(
+      const std::string& browser_context_id,
       base::OnceCallback<void(
           absl::optional<FirstPartySetsHandlerImpl::FlattenedSets>)> callback);
 
--- content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
@@ -86,12 +86,12 @@ net::PublicSets GetSetsAndWait() {
 
 // TODO(shuuran): Return `net::PublicSets` type instead.
 absl::optional<FirstPartySetsHandlerImpl::FlattenedSets>
-GetPersistedPublicSetsAndWait() {
+GetPersistedPublicSetsAndWait(const std::string& browser_context_id) {
   base::test::TestFuture<
       absl::optional<FirstPartySetsHandlerImpl::FlattenedSets>>
       future;
   FirstPartySetsHandlerImpl::GetInstance()->GetPersistedPublicSetsForTesting(
-      future.GetCallback());
+      browser_context_id, future.GetCallback());
   return future.Get();
 }
 
@@ -253,6 +253,7 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()
       ->SetEmbedderWillProvidePublicSetsForTesting(true);
+  const std::string browser_context_id = "profile";
   const std::string input =
       R"({"primary": "https://foo.test", )"
       R"("associatedSites": ["https://associatedsite.test"]})";
@@ -272,12 +273,12 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()
       ->ClearSiteDataOnChangedSetsForContext(
-          base::BindRepeating(&FakeBrowserContextGetter), "profile",
+          base::BindRepeating(&FakeBrowserContextGetter), browser_context_id,
           /*context_config=*/nullptr, base::DoNothing());
 
   env().RunUntilIdle();
 
-  EXPECT_THAT(GetPersistedPublicSetsAndWait(),
+  EXPECT_THAT(GetPersistedPublicSetsAndWait(browser_context_id),
               Optional(UnorderedElementsAre(
                   Pair(foo, net::FirstPartySetEntry(
                                 foo, net::SiteType::kPrimary, absl::nullopt)),
@@ -293,6 +294,7 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()
       ->SetEmbedderWillProvidePublicSetsForTesting(true);
+  const std::string browser_context_id = "profile";
   const std::string input =
       R"({"primary": "https://foo.test", )"
       R"("associatedSites": ["https://associatedsite.test"]})";
@@ -312,18 +314,19 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()
       ->ClearSiteDataOnChangedSetsForContext(
-          base::BindRepeating(&FakeBrowserContextGetter), "profile",
+          base::BindRepeating(&FakeBrowserContextGetter), browser_context_id,
           /*context_config=*/nullptr, base::DoNothing());
 
   env().RunUntilIdle();
 
-  EXPECT_THAT(GetPersistedPublicSetsAndWait(), absl::nullopt);
+  EXPECT_THAT(GetPersistedPublicSetsAndWait(browser_context_id), absl::nullopt);
 }
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest,
        ClearSiteDataOnChangedSetsForContext_InvalidPublicSetsVersion) {
   FirstPartySetsHandlerImpl::GetInstance()
       ->SetEmbedderWillProvidePublicSetsForTesting(true);
+  const std::string browser_context_id = "profile";
   const base::Version invalid_version = base::Version();
   DCHECK(!invalid_version.IsValid());
 
@@ -337,11 +340,12 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()
       ->ClearSiteDataOnChangedSetsForContext(
-          base::BindRepeating(&FakeBrowserContextGetter), "profile",
+          base::BindRepeating(&FakeBrowserContextGetter), browser_context_id,
           /*context_config=*/nullptr, base::DoNothing());
 
   // Public sets with invalid version was not persisted.
-  EXPECT_THAT(GetPersistedPublicSetsAndWait(), Optional(IsEmpty()));
+  EXPECT_THAT(GetPersistedPublicSetsAndWait(browser_context_id),
+              Optional(IsEmpty()));
 }
 
 TEST_F(FirstPartySetsHandlerImplEnabledTest,
