2409b0d880983b5bd7e313c7997155a52aad5a07
Sylvain Defresne <sdefresne@chromium.org>
2022-09-16 17:32:41 +0000

    [ios] Use templated CreateForWebState() method
    As WebStateUserData::CreateForWebState() now supports an arbitrary
    number of parameter, custom CreateForWebState() overloads are not
    needed anymore.
    
    Convert //ios/chrome/browser/https_upgrades to use the templated
    method instead of custom overloads. Change client to pass the
    required services to the CreateForWebState() method.
    
    Fix some style issues.
    
    Bug: 949840
    Change-Id: Ifa5b4c419f4cfe839b7c5e4e92271cb3e12bdc1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902305
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048091}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/history/history_tab_helper.h"
 #import "ios/chrome/browser/history/top_sites_factory.h"
 #import "ios/chrome/browser/https_upgrades/https_only_mode_upgrade_tab_helper.h"
+#import "ios/chrome/browser/https_upgrades/https_upgrade_service_factory.h"
 #import "ios/chrome/browser/https_upgrades/typed_navigation_upgrade_tab_helper.h"
 #import "ios/chrome/browser/infobars/infobar_badge_tab_helper.h"
 #import "ios/chrome/browser/infobars/infobar_manager_impl.h"
@@ -64,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/password_tab_helper.h"
 #import "ios/chrome/browser/passwords/well_known_change_password_tab_helper.h"
 #import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_tab_helper.h"
+#import "ios/chrome/browser/prerender/prerender_service_factory.h"
 #import "ios/chrome/browser/reading_list/offline_page_tab_helper.h"
 #import "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #import "ios/chrome/browser/reading_list/reading_list_web_state_observer.h"
@@ -247,13 +249,17 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   if (base::FeatureList::IsEnabled(
           security_interstitials::features::kHttpsOnlyMode)) {
-    HttpsOnlyModeUpgradeTabHelper::CreateForWebState(web_state,
-                                                     browser_state->GetPrefs());
+    HttpsOnlyModeUpgradeTabHelper::CreateForWebState(
+        web_state, browser_state->GetPrefs(),
+        PrerenderServiceFactory::GetForBrowserState(browser_state),
+        HttpsUpgradeServiceFactory::GetForBrowserState(browser_state));
     HttpsOnlyModeContainer::CreateForWebState(web_state);
   }
 
   if (base::FeatureList::IsEnabled(omnibox::kDefaultTypedNavigationsToHttps)) {
-    TypedNavigationUpgradeTabHelper::CreateForWebState(web_state);
+    TypedNavigationUpgradeTabHelper::CreateForWebState(
+        web_state, PrerenderServiceFactory::GetForBrowserState(browser_state),
+        HttpsUpgradeServiceFactory::GetForBrowserState(browser_state));
   }
 
   if (IsWebChannelsEnabled() && !is_off_the_record) {
