963122c3208c9528fa5da793868b61b5e11ee307
Daniel Andersson <dandersson@chromium.org>
2022-09-16 23:43:35 +0000

    saved_desks: Unify some functionality in SavedDesk{Grid,Library}View.
    This is a refactoring that changes how the saved desk grid is
    initialized. We had two functions (`AddOrUpdateTemplates` and
    `PopulateGridUI`) that did very similar things. The latter has now been
    removed.
    
    `AddOrUpdateTemplates` is now known as `AddOrUpdateEntries`, ditto for
    `DeleteTemplates`.
    
    Parameters were renamed to be more indicative of what they do, rather
    than how they are currently used.
    
    Additionally, SavedDeskPresenter's constructor will no longer do
    `GetAllEntries`. The result of that call was never used since it
    happened before the overview grid had been created.
    
    Bug: 1363382
    Change-Id: I87f6238f2a2c778e0a5e5394929d2f67b54316ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894971
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048296}

--- ash/wm/desks/templates/saved_desk_presenter.cc
+++ ash/wm/desks/templates/saved_desk_presenter.cc
@@ -316,8 +316,6 @@ SavedDeskPresenter::SavedDeskPresenter(OverviewSession* overview_session)
 
   auto* desk_model = GetDeskModel();
   desk_model_observation_.Observe(desk_model);
-  GetAllEntries(base::GUID(), /*saved_desk_name=*/u"",
-                Shell::GetPrimaryRootWindow());
 
   should_show_templates_ui_ =
       !Shell::Get()->tablet_mode_controller()->InTabletMode() &&
@@ -509,9 +507,9 @@ void SavedDeskPresenter::GetAllEntries(const base::GUID& item_to_focus,
     // Populate `SavedDeskLibraryView` with the desk template entries.
     if (SavedDeskLibraryView* library_view =
             overview_grid->GetSavedDeskLibraryView()) {
-      library_view->PopulateGridUI(result.entries,
-                                   overview_grid->GetGridEffectiveBounds(),
-                                   /*last_saved_desk_uuid=*/item_to_focus);
+      library_view->AddOrUpdateEntries(result.entries, item_to_focus,
+                                       /*animate=*/false);
+
       SavedDeskItemView* item_view =
           library_view->GetItemForUUID(item_to_focus);
       if (!item_view)
@@ -722,9 +720,8 @@ void SavedDeskPresenter::AddOrUpdateUIEntries(
 
   for (auto& overview_grid : overview_session_->grid_list()) {
     if (auto* library_view = overview_grid->GetSavedDeskLibraryView()) {
-      library_view->AddOrUpdateTemplates(
-          new_entries, /*initializing_grid_view=*/false,
-          /*last_saved_template_uuid=*/base::GUID());
+      library_view->AddOrUpdateEntries(new_entries, /*order_first_uuid=*/{},
+                                       /*animate=*/true);
     }
   }
 
@@ -742,7 +739,7 @@ void SavedDeskPresenter::RemoveUIEntries(const std::vector<base::GUID>& uuids) {
   for (auto& overview_grid : overview_session_->grid_list()) {
     // Remove the entries from `SavedDeskLibraryView`.
     if (auto* library_view = overview_grid->GetSavedDeskLibraryView())
-      library_view->DeleteTemplates(uuids, /*delete_animation=*/true);
+      library_view->DeleteEntries(uuids, /*delete_animation=*/true);
   }
 
   if (on_update_ui_closure_for_testing_)
