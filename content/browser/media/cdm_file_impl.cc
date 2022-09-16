ea1cf150eda0fe6cc2eb9bf2ef22672c2f5a1dae
Xiaohan Wang <xhwang@chromium.org>
2022-09-16 17:43:44 +0000

    media: Add UMA on CdmFileIO operation results
    Right now we have UMA on the time to complete a CdmFileIO operation, but
    we don't have UMA on the actual result of it. This CL adds those UMAs.
    
    Here's a list of what's added:
    Media.EME.CdmFileIO.{Read|Write|Delete}File
    Media.EME.CdmFileIO.{Read|Write|Delete}File.Normal
    Media.EME.CdmFileIO.{Read|Write|Delete}File.Incognito
    
    Manually tested the CL by doing a playback on a player using Widevine
    with "persistent state" required, so the CDM will try to read CDM
    storage. Then observed these in about://histograms/Cdm:
    
    ```
    - Histogram: Media.EME.CdmFileIO.FileSizeKBOnFirstRead recorded 1 samples, mean = 0.0 (flags = 0x41) [#]
    0  -O                                                                        (1 = 100.0%)
    1  ...
    
    - Histogram: Media.EME.CdmFileIO.ReadFile recorded 2 samples, mean = 1.0 (flags = 0x41) [#]
    0  O                                                                         (0 = 0.0%)
    1  --O                                                                       (2 = 100.0%) {0.0%}
    2  O                                                                         (0 = 0.0%) {100.0%}
    
    - Histogram: Media.EME.CdmFileIO.ReadFile.Normal recorded 2 samples, mean = 1.0 (flags = 0x41) [#]
    0  O                                                                         (0 = 0.0%)
    1  --O                                                                       (2 = 100.0%) {0.0%}
    2  O                                                                         (0 = 0.0%) {100.0%}
    
    - Histogram: Media.EME.CdmFileIO.TimeTo.ReadFile recorded 2 samples, mean = 0.0 (flags = 0x41) [#]
    0  --O                                                                       (2 = 100.0%)
    1  ...
    
    - Histogram: Media.EME.CdmFileIO.TimeTo.ReadFile.Normal recorded 2 samples, mean = 0.0 (flags = 0x41) [#]
    0  --O                                                                       (2 = 100.0%)
    1  ...
    ```
    
    Bug: b/246376520
    Test: See above
    Change-Id: I537f671d034ce91fd3d4f4e57114937635861af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896581
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048094}

--- content/browser/media/cdm_file_impl.cc
+++ content/browser/media/cdm_file_impl.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "content/browser/media/media_license_storage_host.h"
 #include "media/cdm/cdm_type.h"
@@ -37,9 +36,10 @@ const int64_t kMaxFileSizeBytes = 512 * 1024;
 // Maximum length of a file name.
 const size_t kFileNameMaxLength = 256;
 
-const char kReadTimeUmaName[] = "Media.EME.CdmFileIO.TimeTo.ReadFile";
-const char kWriteTimeUmaName[] = "Media.EME.CdmFileIO.TimeTo.WriteFile";
-const char kDeleteTimeUmaName[] = "Media.EME.CdmFileIO.TimeTo.DeleteFile";
+// UMA suffices for CDM File IO operations.
+const char kReadFile[] = "ReadFile";
+const char kWriteFile[] = "WriteFile";
+const char kDeleteFile[] = "DeleteFile";
 
 }  // namespace
 
@@ -116,14 +116,15 @@ void CdmFileImpl::DidRead(absl::optional<std::vector<uint8_t>> data) {
   DCHECK(read_callback_);
   DCHECK(host_);
 
-  if (!data.has_value()) {
+  bool success = data.has_value();
+  ReportFileOperationUMA(success, kReadFile);
+
+  if (!success) {
     // Unable to read the contents of the file.
     std::move(read_callback_).Run(Status::kFailure, {});
     return;
   }
 
-  // Only report reading time for successful reads.
-  ReportFileOperationTimeUMA(kReadTimeUmaName);
   std::move(read_callback_).Run(Status::kSuccess, std::move(data.value()));
 }
 
@@ -164,22 +165,33 @@ void CdmFileImpl::Write(const std::vector<uint8_t>& data,
       base::BindOnce(&CdmFileImpl::DidWrite, weak_factory_.GetWeakPtr()));
 }
 
-void CdmFileImpl::ReportFileOperationTimeUMA(const std::string& uma_name) {
+void CdmFileImpl::ReportFileOperationUMA(bool success,
+                                         const std::string& operation) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(host_);
 
-  static const char kIncognito[] = ".Incognito";
-  static const char kNormal[] = ".Normal";
+  // Strings for UMA names.
+  static const char kUmaPrefix[] = "Media.EME.CdmFileIO";
+  static const char kTimeTo[] = "TimeTo";
+  const std::string mode_suffix = host_->in_memory() ? "Incognito" : "Normal";
 
-  bool is_incognito = host_->in_memory();
+  // Records the result to the base histogram as well as splitting it out by
+  // incognito or normal mode.
+  auto result_uma_name = base::JoinString({kUmaPrefix, operation}, ".");
+  base::UmaHistogramBoolean(result_uma_name, success);
+  base::UmaHistogramBoolean(
+      base::JoinString({result_uma_name, mode_suffix}, "."), success);
 
-  // This records the time taken to the base histogram as well as splitting it
-  // out by incognito or normal mode.
-  auto time_taken = base::TimeTicks::Now() - start_time_;
-  base::UmaHistogramTimes(uma_name, time_taken);
-  base::UmaHistogramTimes(
-      base::StrCat({uma_name, is_incognito ? kIncognito : kNormal}),
-      time_taken);
+  // Records the time taken to the base histogram as well as splitting it out by
+  // incognito or normal mode. Only reported for successful operation.
+  if (success) {
+    auto time_taken = base::TimeTicks::Now() - start_time_;
+    auto time_taken_uma_name =
+        base::JoinString({kUmaPrefix, kTimeTo, operation}, ".");
+    base::UmaHistogramTimes(time_taken_uma_name, time_taken);
+    base::UmaHistogramTimes(
+        base::JoinString({time_taken_uma_name, mode_suffix}, "."), time_taken);
+  }
 }
 
 void CdmFileImpl::DidWrite(bool success) {
@@ -188,14 +200,14 @@ void CdmFileImpl::DidWrite(bool success) {
   DCHECK(write_callback_);
   DCHECK(host_);
 
+  ReportFileOperationUMA(success, kWriteFile);
+
   if (!success) {
     DLOG(WARNING) << "Unable to write to file " << file_name_;
     std::move(write_callback_).Run(Status::kFailure);
     return;
   }
 
-  // Only report writing time for successful writes.
-  ReportFileOperationTimeUMA(kWriteTimeUmaName);
   std::move(write_callback_).Run(Status::kSuccess);
 }
 
@@ -218,14 +230,14 @@ void CdmFileImpl::DidDeleteFile(bool success) {
   DCHECK(write_callback_);
   DCHECK(host_);
 
+  ReportFileOperationUMA(success, kDeleteFile);
+
   if (!success) {
     DLOG(WARNING) << "Unable to delete file " << file_name_;
     std::move(write_callback_).Run(Status::kFailure);
     return;
   }
 
-  // Only report writing time for successful deletions.
-  ReportFileOperationTimeUMA(kDeleteTimeUmaName);
   std::move(write_callback_).Run(Status::kSuccess);
 }
 
