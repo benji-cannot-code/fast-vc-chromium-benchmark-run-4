2a429dc2add7c1965c6568ca2dfce5fb5878653a
Juan Mojica <juanmojica@google.com>
2022-09-16 22:44:04 +0000

    Fix highlight hover shape on close button in instruction chip.
    Screenshot: https://screenshot.googleplex.com/Bsmhcs6KwztLMxS
    
    Bug: b:246585497
    Change-Id: Id074d3ac3232d00733682dba62f31528aeb2c520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902725
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048245}

--- chrome/browser/ui/views/lens/lens_region_search_instructions_view.cc
+++ chrome/browser/ui/views/lens/lens_region_search_instructions_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/image_button_factory.h"
+#include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/flex_layout.h"
@@ -170,6 +171,9 @@ void LensRegionSearchInstructionsView::Init() {
   close_button_->SetImageHorizontalAlignment(views::ImageButton::ALIGN_CENTER);
   close_button_->SetProperty(
       views::kMarginsKey, gfx::Insets::TLBR(0, kCloseButtonExtraMargin, 0, 0));
+  // Make sure the hover background behind the button is a circle, rather than a
+  // rounded square.
+  views::InstallCircleHighlightPathGenerator(close_button_.get());
   constructed_close_button_ = AddChildView(std::move(close_button_));
 }
 
