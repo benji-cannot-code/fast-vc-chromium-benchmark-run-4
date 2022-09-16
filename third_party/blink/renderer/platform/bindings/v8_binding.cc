6ee71a172766f703405a1b3f0ef47bd0bd6bb0a9
Dave Tapuska <dtapuska@chromium.org>
2022-09-16 13:58:49 +0000

    Pass v8::Isolate into current script APIs.
    The GetCurrentScriptUrls should take the Isolate to avoid a TLS
    lookup.
    
    BUG=263412
    
    Change-Id: I5a7a4a920c3261e85ecd7273e340c8b1b9a7e2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899613
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047982}

--- third_party/blink/renderer/platform/bindings/v8_binding.cc
+++ third_party/blink/renderer/platform/bindings/v8_binding.cc
@@ -52,8 +52,7 @@ v8::Local<v8::Value> FreezeV8Object(v8::Local<v8::Value> value,
   return value;
 }
 
-String GetCurrentScriptUrl() {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+String GetCurrentScriptUrl(v8::Isolate* isolate) {
   DCHECK(isolate);
   if (!isolate->InContext())
     return String();
@@ -63,10 +62,10 @@ String GetCurrentScriptUrl() {
   return ToCoreStringWithNullCheck(script_name);
 }
 
-Vector<String> GetScriptUrlsFromCurrentStack(wtf_size_t unique_url_count) {
+Vector<String> GetScriptUrlsFromCurrentStack(v8::Isolate* isolate,
+                                             wtf_size_t unique_url_count) {
   Vector<String> unique_urls;
 
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
   DCHECK(isolate);
   if (!isolate->InContext())
     return unique_urls;
