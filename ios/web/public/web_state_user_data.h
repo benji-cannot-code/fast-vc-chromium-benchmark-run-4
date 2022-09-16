ce9aa723744aead6a3e26955daad9f64c1bc01ac
Sylvain Defresne <sdefresne@chromium.org>
2022-09-16 11:36:37 +0000

    [ios] Add templated WebStateUserData::CreateForWebState()
    Change CreateForWebState() method to take an arbitrary number of
    arguments (and forward them to the sub-class constructor). This
    will allow the default implementation to be used by sub-classes.
    
    Bug: 949840
    Change-Id: Ied9d30ab54db5f5453b302adc6ed79130b7e7e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901816
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047938}

--- ios/web/public/web_state_user_data.h
+++ ios/web/public/web_state_user_data.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_PUBLIC_WEB_STATE_USER_DATA_H_
 #define IOS_WEB_PUBLIC_WEB_STATE_USER_DATA_H_
 
+#include "base/check.h"
 #include "base/memory/ptr_util.h"
 #include "base/supports_user_data.h"
 #import "ios/web/public/web_state.h"
@@ -44,9 +45,14 @@ class WebStateUserData : public base::SupportsUserData::Data {
  public:
   // Creates an object of type T, and attaches it to the specified WebState.
   // If an instance is already attached, does nothing.
-  static void CreateForWebState(WebState* web_state) {
-    if (!FromWebState(web_state))
-      web_state->SetUserData(UserDataKey(), base::WrapUnique(new T(web_state)));
+  template <typename... Args>
+  static void CreateForWebState(WebState* web_state, Args&&... args) {
+    DCHECK(web_state);
+    if (!FromWebState(web_state)) {
+      web_state->SetUserData(
+          UserDataKey(),
+          base::WrapUnique(new T(web_state, std::forward<Args>(args)...)));
+    }
   }
 
   // Retrieves the instance of type T that was attached to the specified
