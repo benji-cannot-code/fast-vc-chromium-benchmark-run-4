95900032ccdad35fbc1a855660d899f35ba05aeb
Karol Sygiet <sygiet@google.com>
2022-09-16 14:02:43 +0000

    Add controller for the BiometricAuthForFilling promo
    Alongside it adds graphics and strings used in the promo bubble.
    Introduces a new way of authenticating user using biometrics
    from the bubble.
    Controller isn't used anywhere yet, but will be used in a followup to
    create a promo to enable BiometricAuthenticationForFilling feature.
    
    Bug: 1322079
    Change-Id: Icb690cdb1b81df658e33016872a2da00041fd8e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880070
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047985}

--- chrome/browser/ui/passwords/passwords_model_delegate.h
+++ chrome/browser/ui/passwords/passwords_model_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "components/password_manager/core/browser/manage_passwords_referrer.h"
 #include "components/password_manager/core/browser/ui/password_check_referrer.h"
@@ -35,6 +36,8 @@ struct AccountInfo;
 // and notify about user actions.
 class PasswordsModelDelegate {
  public:
+  using AvailabilityCallback = base::OnceCallback<void(bool)>;
+
   // Returns WebContents* the model is attached to.
   virtual content::WebContents* GetWebContents() const = 0;
 
@@ -158,6 +161,13 @@ class PasswordsModelDelegate {
   // method returns false. The password in the reopened bubble will be revealed
   // if the authentication was successful.
   virtual bool AuthenticateUser() = 0;
+
+  // Called from the BiometricAuthenticationForFilling bubble controller when OS
+  // re-auth is needed to enable feature. Runs callback with true parameter
+  // immediately if user authentication is not available for the given platform.
+  // Otherwise, the method schedules a task to show an authentication dialog.
+  virtual void AuthenticateUserWithMessage(const std::u16string& message,
+                                           AvailabilityCallback callback) = 0;
 
   // Called from the Save/Update bubble controller when gaia re-auth is needed
   // to save passwords. This method triggers the reauth flow. Upon successful
