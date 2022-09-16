cc5802fa7f4f8f93057535668ac03f3ef393ba96
Darren Shen <shend@chromium.org>
2022-09-16 00:18:13 +0000

    Revert "[site data] Display partitioned site state"
    This reverts commit 20109ef19955dfa7b0eb1f0bc025c33874a8b0e7.
    
    Reason for revert: Modified tests causing failures crbug.com/1364372
    
    Original change's description:
    > [site data] Display partitioned site state
    >
    > If the site has accessed only partitioned cookies, mark it as "Only accessing partitioned storage" (strings will be updated separately).
    >
    > To get the effective setting for partitioned access, get the content
    > setting from the HostContentSettingMap instead of CookieSettings because
    > partitioned access isn't affected by 3PC blocking.
    >
    > If there both partitioned and unpartitioned cookies:
    > * if both are allowed, the state is "Allowed"
    > * if both are blocked, the state is "Blocked"
    > * if partitioned cookies are allowed (or session only) and regular cookies are blocked (because of 3PC blocking), display as "Only accessing partitioned storage". Only one entry is shown per host.
    >
    > Bug: 1344787
    > Change-Id: I06ad711a3437e6dae7b91c7a4a1f085d8f159141
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891290
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1047489}
    
    Bug: 1344787
    Change-Id: Ia03ba6e54c339ca60685071cebecef5d5a6120fb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901551
    Owners-Override: Darren Shen <shend@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047757}

--- chrome/browser/ui/views/site_data/site_data_row_view.h
+++ chrome/browser/ui/views/site_data/site_data_row_view.h
@@ -19,13 +19,7 @@ class Image;
 namespace views {
 class ImageView;
 class Label;
-class ImageButton;
-class MenuRunner;
 }  // namespace views
-
-namespace ui {
-class MenuModel;
-}  // namespace ui
 
 namespace url {
 class Origin;
@@ -41,7 +35,6 @@ class SiteDataRowView : public views::View {
   SiteDataRowView(
       const url::Origin& origin,
       ContentSetting setting,
-      bool is_fully_partitioned,
       FaviconCache* favicon_cache,
       base::RepeatingCallback<void(const url::Origin&)> delete_callback,
       base::RepeatingCallback<void(const url::Origin&, ContentSetting)>
@@ -50,7 +43,6 @@ class SiteDataRowView : public views::View {
   ~SiteDataRowView() override;
 
   views::Label* state_label_for_testing() { return state_label_; }
-  views::ImageButton* menu_button_for_testing() { return menu_button_; }
 
  private:
   friend class PageSpecificSiteDataDialogBrowserTest;
@@ -58,7 +50,6 @@ class SiteDataRowView : public views::View {
   void SetFaviconImage(const gfx::Image& image);
 
   void OnMenuIconClicked();
-  void OnMenuClosed();
 
   void OnDeleteMenuItemClicked(int event_flags);
   void OnBlockMenuItemClicked(int event_flags);
@@ -72,17 +63,12 @@ class SiteDataRowView : public views::View {
 
   url::Origin origin_;
   ContentSetting setting_;
-  bool is_fully_partitioned_;
   base::RepeatingCallback<void(const url::Origin&)> delete_callback_;
   base::RepeatingCallback<void(const url::Origin&, ContentSetting)>
       create_exception_callback_;
 
   raw_ptr<views::Label> state_label_ = nullptr;
   raw_ptr<views::ImageView> favicon_image_ = nullptr;
-  raw_ptr<views::ImageButton> menu_button_ = nullptr;
-
-  std::unique_ptr<ui::MenuModel> dialog_model_;
-  std::unique_ptr<views::MenuRunner> menu_runner_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_SITE_DATA_SITE_DATA_ROW_VIEW_H_
