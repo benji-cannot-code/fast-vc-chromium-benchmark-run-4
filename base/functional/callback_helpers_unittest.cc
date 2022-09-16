ba42b7a98b11c6f8be2a53444c2dfd1cada20271
Daniel Cheng <dcheng@chromium.org>
2022-09-16 17:16:24 +0000

    Move bind.h, callback{,_forward,_helpers}.h into //base/functional
    Forwarding headers remain in the old locations to ease migration.
    Include paths for files in //base/functional/ are also fixed up to the
    new canonical path; remaining fixups are deferred until followups to
    minimize the risk of conflicts.
    
    Bug: 1364441
    Change-Id: I955e738928dcd7aeba5afb1ddae13e68d61eb697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900088
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048084}

--- base/functional/callback_helpers_unittest.cc
+++ base/functional/callback_helpers_unittest.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/callback_helpers.h"
+#include "base/functional/callback_helpers.h"
 
 #include <functional>
 #include <type_traits>
 
-#include "base/bind.h"
-#include "base/callback.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
 #include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
