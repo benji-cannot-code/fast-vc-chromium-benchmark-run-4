1a88075adf68b51c214604be8345ea8fbf70a939
Chris Fredrickson <cfredric@chromium.org>
2022-09-16 16:45:50 +0000

    Move customization logic into PublicSets
    This CL does not change any behavior; it just moves the bulk of
    the `ComputeEnterpriseCustomizations` into `net::PublicSets`, and
    adjusts plumbing accordingly.
    
    Bug: 1363785
    Change-Id: I3eb15033813dc7922e2ae9d9eefb5b243ceb4a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899178
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048064}

--- net/first_party_sets/public_sets.h
+++ net/first_party_sets/public_sets.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_export.h"
 #include "net/base/schemeful_site.h"
 #include "net/first_party_sets/first_party_set_entry.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace net {
 
@@ -43,6 +44,20 @@ class NET_EXPORT PublicSets {
 
   // Creates a clone of this instance.
   PublicSets Clone() const;
+
+  // Returns a FirstPartySetsContextConfig suitable for passing into
+  // FindEntries, in order to respect the overrides given by `replacement_sets`
+  // and `normalized_additions`.
+  //
+  // Preconditions: sets defined by `replacement_sets` and
+  // `normalized_additions` must be disjoint. `normalized_additions` must be
+  // preprocessed such that no two addition sets intersect with the same public
+  // set (i.e. they must be pre-unioned using a disjoint-set data structure).
+  FirstPartySetsContextConfig ComputeConfig(
+      const std::vector<base::flat_map<SchemefulSite, FirstPartySetEntry>>&
+          replacement_sets,
+      const std::vector<base::flat_map<SchemefulSite, FirstPartySetEntry>>&
+          normalized_additions) const;
 
   // Returns the entry corresponding to the given `site`, if one exists.
   // Respects any customization/overlay specified by `config`. This is
