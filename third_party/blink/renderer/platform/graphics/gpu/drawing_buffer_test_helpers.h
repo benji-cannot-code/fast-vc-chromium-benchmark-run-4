5994742e87d373ed3e75fbe49e0c03bbca77be5d
Saifuddin Hitawala <hitawala@chromium.org>
2022-09-16 22:45:12 +0000

    Reland - Set low latency via desynchronized for WebGL
    Changes from original:
    None, reland change separately from SharedImage changes as they were
    culprit for revert and unrelated.
    
    Original CL Description[1]:
    DrawingBuffer::CreateColorBuffer creates a shared image passing
    SHARED_IMAGE_USAGE_CONCURRENT_READ_WRITE usage if low latency is
    enabled. This flag was not enabled properly by client as the support
    was added recently for OzoneBacking to ChromeOS and would work earlier
    with GLImageBacking. This change sets the low latency enabled flag
    based on the desynchronized flag being passed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3876745
    
    Bug: b/243039477, 1359404, 1363435
    Change-Id: I1185247693b251146375533305ad490f8ab2e3f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899135
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048247}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test_helpers.h
@@ -429,6 +429,7 @@ class DrawingBufferForTests : public DrawingBuffer {
             std::move(context_provider),
             graphics_info,
             false /* usingSwapChain */,
+            false /* desynchronized */,
             std::move(extensions_util),
             client,
             false /* discardFramebufferSupported */,
