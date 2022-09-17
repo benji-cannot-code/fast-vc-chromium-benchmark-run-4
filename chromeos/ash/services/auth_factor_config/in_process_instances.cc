29a5281406cf1e32fb685f04ba4d2e335d2b8d5c
Martin Bidlingmaier <mbid@google.com>
2022-09-17 13:05:29 +0000

    Add browser test for recovery toggle in os settings
    Bug: b/243696986
    Bug: b/239416325
    Change-Id: Ibe97628cad18231b59f4da568f8f4cd53347a45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3853252
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048384}

--- chromeos/ash/services/auth_factor_config/in_process_instances.cc
+++ chromeos/ash/services/auth_factor_config/in_process_instances.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include "base/no_destructor.h"
 #include "chromeos/ash/services/auth_factor_config/auth_factor_config.h"
+#include "chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom-test-utils.h"
 #include "chromeos/ash/services/auth_factor_config/recovery_factor_editor.h"
 
 namespace ash::auth {
@@ -28,9 +29,17 @@ void BindToAuthFactorConfig(
   auth_factor_config->BindReceiver(std::move(receiver));
 }
 
+mojom::AuthFactorConfigAsyncWaiter GetAuthFactorConfigForTesting() {
+  return mojom::AuthFactorConfigAsyncWaiter(auth_factor_config.get());
+}
+
 void BindToRecoveryFactorEditor(
     mojo::PendingReceiver<mojom::RecoveryFactorEditor> receiver) {
   recovery_factor_editor->BindReceiver(std::move(receiver));
+}
+
+mojom::RecoveryFactorEditorAsyncWaiter GetRecoveryFactorEditorForTesting() {
+  return mojom::RecoveryFactorEditorAsyncWaiter(recovery_factor_editor.get());
 }
 
 }  // namespace ash::auth
--- chromeos/ash/services/auth_factor_config/in_process_instances.h
+++ chromeos/ash/services/auth_factor_config/in_process_instances.h
@@ -15,11 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::auth {
 
+namespace mojom {
+class AuthFactorConfigAsyncWaiter;
+class RecoveryFactorEditorAsyncWaiter;
+}  // namespace mojom
+
 void BindToAuthFactorConfig(
     mojo::PendingReceiver<mojom::AuthFactorConfig> receiver);
+mojom::AuthFactorConfigAsyncWaiter GetAuthFactorConfigForTesting();
 
 void BindToRecoveryFactorEditor(
     mojo::PendingReceiver<mojom::RecoveryFactorEditor> receiver);
+mojom::RecoveryFactorEditorAsyncWaiter GetRecoveryFactorEditorForTesting();
 
 }  // namespace ash::auth
 
