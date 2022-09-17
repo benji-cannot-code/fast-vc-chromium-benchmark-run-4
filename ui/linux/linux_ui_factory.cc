c177b8b1a6e9232d7783e23971c7a98bf1ad242a
Tom Anderson <thomasanderson@chromium.org>
2022-09-17 20:52:11 +0000

    Add command-line flag to control the global LinuxUi
    The LinuxUiTheme is controlled per-profile via chrome://settings, but
    there was no way to control the default LinuxUi instance.  This CL adds
    a command-line flag to be used for testing and possibly also by users.
    
    R=sky
    
    Bug: 1317782
    Change-Id: Id7ed6e75fc8feec11c6d192a2f802ede27f29894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902253
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048416}

--- ui/linux/linux_ui_factory.cc
+++ ui/linux/linux_ui_factory.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/command_line.h"
 #include "base/environment.h"
 #include "base/nix/xdg_util.h"
+#include "base/strings/string_util.h"
 #include "ui/base/buildflags.h"
 #include "ui/color/system_theme.h"
 #include "ui/linux/linux_ui.h"
@@ -25,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 namespace {
+
+const char kUiToolkitFlag[] = "ui-toolkit";
 
 std::unique_ptr<LinuxUiAndTheme> CreateGtkUi() {
 #if BUILDFLAG(USE_GTK)
@@ -63,8 +67,18 @@ LinuxUiAndTheme* GetQtUi() {
 }
 
 LinuxUiAndTheme* GetDefaultLinuxUiAndTheme() {
-  std::unique_ptr<base::Environment> env = base::Environment::Create();
+  auto* cmd_line = base::CommandLine::ForCurrentProcess();
+  std::string ui_toolkit =
+      base::ToLowerASCII(cmd_line->GetSwitchValueASCII(kUiToolkitFlag));
+  if (ui_toolkit == "gtk") {
+    if (auto* gtk_ui = GetGtkUi())
+      return gtk_ui;
+  } else if (ui_toolkit == "qt") {
+    if (auto* qt_ui = GetQtUi())
+      return qt_ui;
+  }
 
+  std::unique_ptr<base::Environment> env = base::Environment::Create();
   switch (GetDefaultSystemTheme()) {
     case SystemTheme::kQt:
       if (auto* qt_ui = GetQtUi())
