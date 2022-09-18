3d549adde22b9fbb5de505c3fc1a7ba128aafcaa
Tao Wu <wutao@chromium.org>
2022-09-18 23:29:32 +0000

    libassistant-v2: Load downloadable content (DLC) for sandbox
    This patch loads the libassistant DLC before the sandbox initializes.
    Using the LibAssistantV2 feature, we could switch between V1 and V2 now.
    
    Bug: b/201673839
    Test: gLinux and zork
    Change-Id: Ice2bc9db69d23f1e2d5e0d14ad5020be9831931c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3897783
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048481}

--- chromeos/ash/services/libassistant/constants.cc
+++ chromeos/ash/services/libassistant/constants.cc
@@ -6,17 +6,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/libassistant/constants.h"
 
 #include "base/files/file_util.h"
+#include "build/chromeos_buildflags.h"
 
 #define ASSISTANT_DIR_STRING "google-assistant-library"
+#define ASSISTANT_TEMP_DIR "/tmp/libassistant/"
+#define LIBASSISTANT_DLC_DIR "opt/google/chrome/"
+#define LIBASSISTANT_V1_NAME "libassistant.so"
+#define LIBASSISTANT_V2_NAME "libassistant_v2.so"
 
 namespace chromeos {
 namespace libassistant {
 
+#if BUILDFLAG(IS_CHROMEOS_DEVICE)
 const base::FilePath::CharType kAssistantBaseDirPath[] =
     FILE_PATH_LITERAL("/home/chronos/user/" ASSISTANT_DIR_STRING);
 
-const base::FilePath::CharType kAssistantTempBaseDirPath[] =
-    FILE_PATH_LITERAL("/tmp/" ASSISTANT_DIR_STRING);
+const char kLibAssistantDlcRootPath[] =
+    "/run/imageloader/assistant-dlc/package/root";
+
+const base::FilePath::CharType kLibAssistantV1DlcPath[] =
+    FILE_PATH_LITERAL(LIBASSISTANT_DLC_DIR LIBASSISTANT_V1_NAME);
+
+const base::FilePath::CharType kLibAssistantV2DlcPath[] =
+    FILE_PATH_LITERAL(LIBASSISTANT_DLC_DIR LIBASSISTANT_V2_NAME);
+#else
+// Directory and files used in gLinux simulation.
+const base::FilePath::CharType kAssistantBaseDirPath[] =
+    FILE_PATH_LITERAL(ASSISTANT_TEMP_DIR ASSISTANT_DIR_STRING);
+
+const char kLibAssistantDlcRootPath[] = "";
+
+const base::FilePath::CharType kLibAssistantV1DlcPath[] =
+    FILE_PATH_LITERAL(LIBASSISTANT_V1_NAME);
+
+const base::FilePath::CharType kLibAssistantV2DlcPath[] =
+    FILE_PATH_LITERAL(LIBASSISTANT_V2_NAME);
+#endif
 
 }  // namespace libassistant
 }  // namespace chromeos
