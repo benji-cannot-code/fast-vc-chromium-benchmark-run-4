2bac717bada9fe10dd12588358d95b815b488702
Nancy Wang <nancylingwang@chromium.org>
2022-09-19 23:23:45 +0000

    Modify browser tests to use the non mojom app launch interfaces.
    This CL is used to prepare for CL:3903912 to enable the StopMojomAppService flag.
    
    BUG=1253250
    
    Change-Id: I5384ff658511ecac60a5e5e3f815d0b9ad17064a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901904
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048853}

--- chrome/browser/apps/app_service/notifications_browsertest.cc
+++ chrome/browser/apps/app_service/notifications_browsertest.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
 
-using apps::mojom::OptionalBool;
 using extensions::Extension;
 using extensions::ExtensionNotificationDisplayHelper;
 using extensions::ExtensionNotificationDisplayHelperFactory;
@@ -155,8 +154,7 @@ class AppNotificationsExtensionApiTest : public extensions::ExtensionApiTest {
     ExtensionTestMessageListener launched_listener("launched",
                                                    ReplyBehavior::kWillReply);
     apps::AppServiceProxyFactory::GetForProfile(profile())->Launch(
-        extension->id(), ui::EF_SHIFT_DOWN,
-        apps::mojom::LaunchSource::kFromTest);
+        extension->id(), ui::EF_SHIFT_DOWN, apps::LaunchSource::kFromTest);
     EXPECT_TRUE(launched_listener.WaitUntilSatisfied());
     launched_listener.Reply(create_window_options);
 
