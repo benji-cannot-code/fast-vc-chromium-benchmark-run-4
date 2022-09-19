14196041ff1486b1de1ff0e7ae460d7f7ec701d1
Aliona DANGLA <alionadangla@google.com>
2022-09-19 14:23:55 +0000

    [iOS] Add file operation verification in egtest
    EnterpriseLoadingScreenTestCase have been really flaky lately. The issue
    was because the policy test server was returning a 410 HTTP error code,
    which causes the app to crash. The crash is because the DM token was
    deleted and initialized again when there is a 410 error, but the test
    server was still sending a request too fast for the dm token to be
    completely reinitialized, which causes the crash.
    
    Why the test server was sending a 410 error? The enterprise loading
    screen is shown only on fresh installation. To simulate this
    environment, before and after each test all related files on the disk
    are deleted. The problem was disk operation was not terminated that the
    test started, so the flow was the following one: App started -> Start
    removing files -> The App do not send registration request to the server
    as there is already registration file on the disk -> Removing file end
    -> Send a policy request to the server => Server send 410 error as the
    device that sent the request is not registered.
    
    This CL ensure that we are waiting for file operation to be finished.
    
    Change-Id: I6c5a59cc2bd865cfbf813208e6ead97ffc9bb9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894292
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048597}

--- ios/chrome/browser/policy/policy_app_interface.mm
+++ ios/chrome/browser/policy/policy_app_interface.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/path_service.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/task/thread_pool.h"
+#import "base/test/ios/wait_util.h"
 #import "base/threading/scoped_blocking_call.h"
 #import "base/values.h"
 #import "components/policy/core/browser/browser_policy_connector.h"
@@ -128,7 +129,7 @@ absl::optional<base::Value> DeserializeValue(NSString* json_value) {
   GetTestPlatformPolicyProvider()->UpdateChromePolicy(values);
 }
 
-+ (void)clearAllPoliciesInMemory {
++ (void)clearAllPoliciesInNSUserDefault {
   [[NSUserDefaults standardUserDefaults]
       removeObjectForKey:kPolicyLoaderIOSConfigurationKey];
 }
@@ -170,16 +171,25 @@ absl::optional<base::Value> DeserializeValue(NSString* json_value) {
   store->set_policy_data_for_testing(std::move(policy_data));
 }
 
-+ (void)clearDMTokenDirectory {
-  base::FilePath app_data_dir_path;
-  base::PathService::Get(base::DIR_APP_DATA, &app_data_dir_path);
-  base::FilePath dm_token_dir_path = app_data_dir_path.Append(kDmTokenBaseDir);
++ (BOOL)clearDMTokenDirectory {
+  base::FilePath appDataDirPath;
+  base::PathService::Get(base::DIR_APP_DATA, &appDataDirPath);
+  base::FilePath dmTokenDirPath = appDataDirPath.Append(kDmTokenBaseDir);
 
-  base::ThreadPool::PostTask(
+  __block BOOL didComplete = NO;
+  base::ThreadPool::PostTaskAndReply(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(^{
-        base::DeletePathRecursively(dm_token_dir_path);
+        base::DeletePathRecursively(dmTokenDirPath);
+      }),
+      base::BindOnce(^{
+        didComplete = YES;
       }));
+
+  return base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForFileOperationTimeout, ^{
+        return didComplete;
+      });
 }
 
 + (BOOL)isCloudPolicyClientRegistered {
@@ -191,16 +201,25 @@ absl::optional<base::Value> DeserializeValue(NSString* json_value) {
       ->is_registered();
 }
 
-+ (void)clearCloudPolicyDirectory {
-  base::FilePath user_data_dir;
-  base::PathService::Get(ios::DIR_USER_DATA, &user_data_dir);
-  base::FilePath policy_dir = user_data_dir.Append(kPolicyDir);
++ (BOOL)clearCloudPolicyDirectory {
+  base::FilePath userDataDir;
+  base::PathService::Get(ios::DIR_USER_DATA, &userDataDir);
+  base::FilePath policyDir = userDataDir.Append(kPolicyDir);
 
-  base::ThreadPool::PostTask(
+  __block BOOL didComplete = NO;
+  base::ThreadPool::PostTaskAndReply(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(^{
-        base::DeletePathRecursively(policy_dir);
+        base::DeletePathRecursively(policyDir);
+      }),
+      base::BindOnce(^{
+        didComplete = YES;
       }));
+
+  return base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForFileOperationTimeout, ^{
+        return didComplete;
+      });
 }
 
 + (BOOL)hasUserPolicyDataInCurrentBrowserState {
