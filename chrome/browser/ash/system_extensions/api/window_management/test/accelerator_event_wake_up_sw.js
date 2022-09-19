89266893aaae6052f654f59ab5d4b6f6fb844f4f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-09-19 03:32:52 +0000

    system-extensions: Introcude multi-run tests
    Multi-run tests are useful when the test requires the service worker
    to be stopped and started during a test. On the C++ side, these tests
    consist of a `InitMultiRunTest()` call and `WaitForRun()` calls for
    each additional run. On the JavaScript side, these tests consist of
    calls `test_run()` for each test code that needs to run when the
    service worker runs again.
    
    For example:
    ```C++
    IN_PROC_BROWSER_TEST_F(FooBrowserTest, Foo) {
      // Setups the required infra to run a multi-run test.
      InitMultiRunTest("foo.js");
    
      // Wait for the service worker to wake up and run "first run".
      WaitForRun("first run");
    
      StopServiceWorkers();
      // Perform an action that would wake up the service worker.
      ui::test::EventGenerator generator;
      generator.PressKey(ui::KeyboardCode::VKEY_A, ui::EF_ALT_DOWN);
    
      // Wait for the service worker to wake up and run the "test properties"
      // run.
      WaitForRun("test properties");
    }
    ```
    
    ```js
    importScripts('test_support.js');
    
    const fooPromise = new Promise(resolve => {
      chromeos.windowManagement.addEventListener('foo', resolve);
    });
    
    // Run named `'first_run'`.
    test_run(() => {}, 'first run');
    
    // Run named `'test properties'`.
    test_run(async (t) => {
     let event = await fooPromise;
     assert_equals(event.type, 'foo');
    }, 'test properties');
    ```
    
    Bug: b:242264794
    Change-Id: Ia617e9afe128124a3b3b731767357a44818bb654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3881809
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048496}

---
+++ chrome/browser/ash/system_extensions/api/window_management/test/accelerator_event_wake_up_sw.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+importScripts('test_support.js', 'cros_window_test_utils.js');
+
+// Nothing to do in the first run.
+test_run(() => {}, 'First run');
+
+const acceleratorDownPromise = new Promise(resolve => {
+  chromeos.windowManagement.addEventListener('acceleratordown', resolve);
+});
+
+test_run(async (t) => {
+  let event = await acceleratorDownPromise;
+  // Test some basic properties to make sure it's the event we expect.
+  assert_equals(event.type, 'acceleratordown');
+  assert_equals(event.acceleratorName, 'Control Alt KeyA');
+}, 'Test event properties');
