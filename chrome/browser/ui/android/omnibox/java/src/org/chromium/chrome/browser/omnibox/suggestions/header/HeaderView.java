c832feebb6e0bd39b235a6f8950f57f52258006d
Rong Tan <rongtan@google.com>
2022-09-19 19:47:07 +0000

    Fixed issue: Tapping on transparency headers shows a pressed state, VoiceOver announces transparency headers are in an expanded state
    Bug: 1362159, 1362162
    Change-Id: Ia8008eac3e45940b0f696894fcc73f7f245ca71a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892789
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Rong Tan <rongtan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048745}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/header/HeaderView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/header/HeaderView.java
@@ -48,7 +48,6 @@ public class HeaderView extends SimpleHorizontalLayoutView {
         TypedValue themeRes = new TypedValue();
         getContext().getTheme().resolveAttribute(R.attr.selectableItemBackground, themeRes, true);
         setBackgroundResource(themeRes.resourceId);
-        setClickable(true);
         setFocusable(true);
 
         mHeaderText = new TextView(context);
@@ -68,29 +67,6 @@ public class HeaderView extends SimpleHorizontalLayoutView {
                 getResources().getDimensionPixelSize(R.dimen.omnibox_suggestion_action_icon_width),
                 LayoutParams.MATCH_PARENT));
         addView(mHeaderIcon);
-        ViewCompat.setAccessibilityDelegate(this, new AccessibilityDelegateCompat() {
-            @Override
-            public void onInitializeAccessibilityNodeInfo(
-                    View host, AccessibilityNodeInfoCompat info) {
-                super.onInitializeAccessibilityNodeInfo(host, info);
-                AccessibilityActionCompat action = mIsCollapsed
-                        ? AccessibilityActionCompat.ACTION_EXPAND
-                        : AccessibilityActionCompat.ACTION_COLLAPSE;
-
-                info.addAction(new AccessibilityActionCompat(
-                        AccessibilityEvent.TYPE_VIEW_CLICKED, action.getLabel()));
-                info.addAction(action);
-            }
-
-            @Override
-            public boolean performAccessibilityAction(View host, int action, Bundle arguments) {
-                if (action == AccessibilityNodeInfoCompat.ACTION_EXPAND
-                        || action == AccessibilityNodeInfoCompat.ACTION_COLLAPSE) {
-                    return performClick();
-                }
-                return super.performAccessibilityAction(host, action, arguments);
-            }
-        });
     }
 
     /** Return ImageView used to present group header chevron. */
@@ -135,6 +111,34 @@ public class HeaderView extends SimpleHorizontalLayoutView {
      */
     void setShouldRemoveSuggestionHeaderChevron(boolean shouldRemoveSuggestionHeaderChevron) {
         mHeaderIcon.setVisibility(shouldRemoveSuggestionHeaderChevron ? GONE : VISIBLE);
+        setClickable(!shouldRemoveSuggestionHeaderChevron);
+
+        // Remove extra accessibility announcement for expanded state, and remove click action.
+        if (!shouldRemoveSuggestionHeaderChevron) {
+            ViewCompat.setAccessibilityDelegate(this, new AccessibilityDelegateCompat() {
+                @Override
+                public void onInitializeAccessibilityNodeInfo(
+                        View host, AccessibilityNodeInfoCompat info) {
+                    super.onInitializeAccessibilityNodeInfo(host, info);
+                    AccessibilityActionCompat action = mIsCollapsed
+                            ? AccessibilityActionCompat.ACTION_EXPAND
+                            : AccessibilityActionCompat.ACTION_COLLAPSE;
+
+                    info.addAction(new AccessibilityActionCompat(
+                            AccessibilityEvent.TYPE_VIEW_CLICKED, action.getLabel()));
+                    info.addAction(action);
+                }
+
+                @Override
+                public boolean performAccessibilityAction(View host, int action, Bundle arguments) {
+                    if (action == AccessibilityNodeInfoCompat.ACTION_EXPAND
+                            || action == AccessibilityNodeInfoCompat.ACTION_COLLAPSE) {
+                        return performClick();
+                    }
+                    return super.performAccessibilityAction(host, action, arguments);
+                }
+            });
+        }
     }
 
     /**
