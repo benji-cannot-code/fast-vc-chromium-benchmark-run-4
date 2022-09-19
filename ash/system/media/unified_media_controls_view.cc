36ba3b23ec16f04351b773677ab9fc880dbaac32
Joanmarie Diggs <jdiggs@igalia.com>
2022-09-19 11:26:11 +0000

    Re-enable accessibility paint checks for UnifiedMediaControlsView
    The paint checks were disabled because UnifiedMediaControlsView is
    focusable but lacks an accessible name. When SetTitle is called, the
    accessible name is set and includes the name of the track being played.
    This commit sets an initial accessible name in the constructor to the
    "Media controls" localized string which does not including details of
    the currently-playing track. Now that UnifiedMediaControlsView has an
    accessible name upon paint, the accessibility paint checks can be
    re-enabled.
    
    Bug: 1348081, 1218186
    Change-Id: I99ee3739158fe8347197fe306ce2241c430a0eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888163
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048564}

--- ash/system/media/unified_media_controls_view.cc
+++ ash/system/media/unified_media_controls_view.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/color/color_id.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 #include "ui/gfx/paint_vector_icon.h"
-#include "ui/views/accessibility/accessibility_paint_checks.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/highlight_path_generator.h"
@@ -146,10 +145,6 @@ UnifiedMediaControlsView::UnifiedMediaControlsView(
           },
           this)),
       controller_(controller) {
-  // TODO(crbug.com/1218186): Remove this, this is in place temporarily to be
-  // able to submit accessibility checks. This crashes if fetching a11y node
-  // data during paint because message_view_ is null.
-  SetProperty(views::kSkipAccessibilityPaintChecks, true);
   SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
   SetBackground(views::CreateRoundedRectBackground(GetBackgroundColor(),
                                                    kMediaControlsCornerRadius));
@@ -223,6 +218,15 @@ UnifiedMediaControlsView::UnifiedMediaControlsView(
       IDS_ASH_MEDIA_NOTIFICATION_ACTION_NEXT_TRACK));
 
   button_row_ = AddChildView(std::move(button_row));
+
+  // Focusable views must have an accessible name when shown/painted so that
+  // the screen reader knows what to present to the user. SetTitle sets the
+  // accessible name using a string which includes the title of the song being
+  // played. That seems like the wrong string to use upon creation if nothing
+  // is playing. Therefore setting the name to a string which lacks the "now
+  // playing" information.
+  SetAccessibleName(l10n_util::GetStringUTF16(
+      IDS_ASH_QUICK_SETTINGS_BUBBLE_MEDIA_CONTROLS_SUB_MENU_ACCESSIBLE_DESCRIPTION));
 }
 
 void UnifiedMediaControlsView::SetIsPlaying(bool playing) {
