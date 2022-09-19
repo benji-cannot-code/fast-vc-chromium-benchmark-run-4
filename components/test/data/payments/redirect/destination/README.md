ca0d48967048ab01ef5ac0aac12afec21ac9674d
Rouslan Solomakhin <rouslan@chromium.org>
2022-09-19 15:31:13 +0000

    [Web Payment] Add a redirect test case
    This patch adds a browser test that verifies that a <domain> can
    redirect to a valid payment app at subdomain.<domain>. A similar test
    will be needed for testing Content Security Policy (CSP) control of
    redirects.
    
    Bug: 1349091
    Change-Id: Iadc17ba6510dc98e01f8d065bef785c41e236bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887673
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048622}

---
+++ components/test/data/payments/redirect/destination/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Just-in-time installable payment handler with payment method identifier
+"https://<hostname>/redirect/destination/pay". This payment handler always can
+make payments and responds to a payment request with {"status": "success"}.
---
+++ components/test/data/payments/redirect/destination/app.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright 2022 The Chromium Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+self.addEventListener('canmakepayment', (event) => {
+  event.respondWith(true);
+});
+
+self.addEventListener('paymentrequest', (event) => {
+  event.respondWith({
+    methodName: event.methodData[0].supportedMethods,
+    details: {status: 'success'},
+  });
+});
