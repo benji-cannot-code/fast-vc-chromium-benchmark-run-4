dc6b197543eb17f1bb7926c0c6be6d8ae000a06c
Morten Stenshorne <mstensho@chromium.org>
2022-09-19 21:54:34 +0000

    Can use IsResumingLayout() on outgoing break tokens, too.
    We've been using this utility function for incoming break tokens since
    the beginning of time, but we can also do it for the outgoing ones,
    instead of checking break_token && !break_token->IsRepeated(). This
    might make the code slightly easier to read.
    
    Discussed in
    https://chromium-review.googlesource.com/c/chromium/src/+/3838771/2/third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc#875
    
    Change-Id: I3f0ecd76616846ae094dbcf29eaecd6b8d3c0601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900383
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048808}

--- third_party/blink/renderer/core/paint/ng/ng_table_painters.cc
+++ third_party/blink/renderer/core/paint/ng/ng_table_painters.cc
@@ -491,9 +491,8 @@ bool IsStartRowFragmented(const NGPhysicalBoxFragment& section) {
     if (!child->IsTableNGRow())
       continue;
 
-    const auto* prev_break_token =
-        FindPreviousBreakToken(To<NGPhysicalBoxFragment>(*child));
-    return prev_break_token && !prev_break_token->IsRepeated();
+    return IsResumingLayout(
+        FindPreviousBreakToken(To<NGPhysicalBoxFragment>(*child)));
   }
 
   return false;
@@ -505,9 +504,8 @@ bool IsEndRowFragmented(const NGPhysicalBoxFragment& section) {
     const auto& child = *it;
     if (!child->IsTableNGRow())
       continue;
-    return child->BreakToken() &&
-           !To<NGBlockBreakToken>(child->BreakToken())->IsAtBlockEnd() &&
-           !To<NGBlockBreakToken>(child->BreakToken())->IsRepeated();
+    const auto* break_token = To<NGBlockBreakToken>(child->BreakToken());
+    return IsResumingLayout(break_token) && !break_token->IsAtBlockEnd();
   }
   return false;
 }
