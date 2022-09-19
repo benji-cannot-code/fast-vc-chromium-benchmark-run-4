3c438d14d08f5c0fb02ee0271134e528383b32da
Michael Tang <tangm@microsoft.com>
2022-09-19 23:57:17 +0000

    Reland "Reland "gpu: Support arbitrary transforms in OverlayCandidate""
    This is a reland of commit 329443961a4d934457364046d0c72e709a04eff5
    
    The previous reland was failing in configurations without `DCHECK_ALWAYS_ON`, causing the death tests to fail. In this CL, we've wrapped the death tests with `DCHECK_IS_ON() && defined(GTEST_HAS_DEATH_TEST)` to avoid running the tests when they are not supported.
    
    Original change's description:
    > Reland "gpu: Support arbitrary transforms in OverlayCandidate"
    >
    > This is a reland of commit 684e430112694f0fea318e1ca941a3154f34d4e0
    >
    > Original change's description:
    > > gpu: Support arbitrary transforms in OverlayCandidate
    > >
    > > Allow for an OverlayCandidate to store a gfx::Transform instead of
    > > attempting to bake it into its |display_rect| and extract a
    > > gfx::OverlayTransform. This allows platforms that support delegated
    > > compositing for quads with arbitrary transforms such as rotation,
    > > scaling, etc using OverlayCandidate.
    > >
    > > |display_rect| is now documented to be explicitly in content space,
    > > where |transform| is the content to target space transformation.
    > > |transform| can now be either a gfx::OverlayTransform or a
    > > gfx::Transform. When it is a gfx::OverlayTransform, it only affects the
    > > orientation of the buffer within |display_rect|'s bounds.
    > >
    > > Some platforms do not support clip rects, so OverlayCandidateFactory
    > > bakes the clip and visible rects into the display and uv rects. This
    > > would not be possible if the overlay's transform does not preserve 2d
    > > axis alignment. There are no platforms expected to support arbitrary
    > > transforms but not clip rects, so we've decided to simply DCHECK in
    > > these cases rather than attempt to handle them.
    > >
    > > Change-Id: I1ecb993664031b4e36157357bb23d4f36139c6fc
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3851154
    > > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > > Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    > > Commit-Queue: Michael Tang <tangm@microsoft.com>
    > > Cr-Commit-Position: refs/heads/main@{#1047582}
    >
    > Change-Id: Ic903ba8dc1746b36227860bf4464a23ee8f30fa9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900358
    > Commit-Queue: Michael Tang <tangm@microsoft.com>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1047678}
    
    Change-Id: If78d9a314eac6d329beb1c6a0fd0197f6ccdb117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901220
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1048869}

--- components/viz/service/display/overlay_unittest.cc
+++ components/viz/service/display/overlay_unittest.cc
@@ -1916,7 +1916,7 @@ TEST_F(UnderlayTest, AllowVerticalFlip) {
       &damage_rect_, &content_bounds_);
   ASSERT_EQ(1U, candidate_list.size());
   EXPECT_EQ(gfx::OVERLAY_TRANSFORM_FLIP_VERTICAL,
-            candidate_list.back().transform);
+            absl::get<gfx::OverlayTransform>(candidate_list.back().transform));
 }
 
 TEST_F(UnderlayTest, AllowHorizontalFlip) {
@@ -1944,7 +1944,7 @@ TEST_F(UnderlayTest, AllowHorizontalFlip) {
       &damage_rect_, &content_bounds_);
   ASSERT_EQ(1U, candidate_list.size());
   EXPECT_EQ(gfx::OVERLAY_TRANSFORM_FLIP_HORIZONTAL,
-            candidate_list.back().transform);
+            absl::get<gfx::OverlayTransform>(candidate_list.back().transform));
 }
 
 TEST_F(SingleOverlayOnTopTest, AllowPositiveScaleTransform) {
@@ -2068,7 +2068,8 @@ TEST_F(UnderlayTest, Allow90DegreeRotation) {
       std::move(surface_damage_rect_list), nullptr, &candidate_list,
       &damage_rect_, &content_bounds_);
   ASSERT_EQ(1U, candidate_list.size());
-  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_90, candidate_list.back().transform);
+  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_90,
+            absl::get<gfx::OverlayTransform>(candidate_list.back().transform));
 }
 
 TEST_F(UnderlayTest, Allow180DegreeRotation) {
@@ -2094,7 +2095,8 @@ TEST_F(UnderlayTest, Allow180DegreeRotation) {
       std::move(surface_damage_rect_list), nullptr, &candidate_list,
       &damage_rect_, &content_bounds_);
   ASSERT_EQ(1U, candidate_list.size());
-  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_180, candidate_list.back().transform);
+  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_180,
+            absl::get<gfx::OverlayTransform>(candidate_list.back().transform));
 }
 
 TEST_F(UnderlayTest, Allow270DegreeRotation) {
@@ -2120,7 +2122,8 @@ TEST_F(UnderlayTest, Allow270DegreeRotation) {
       std::move(surface_damage_rect_list), nullptr, &candidate_list,
       &damage_rect_, &content_bounds_);
   ASSERT_EQ(1U, candidate_list.size());
-  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_270, candidate_list.back().transform);
+  EXPECT_EQ(gfx::OVERLAY_TRANSFORM_ROTATE_270,
+            absl::get<gfx::OverlayTransform>(candidate_list.back().transform));
 }
 
 TEST_F(UnderlayTest, AllowsOpaqueCandidates) {
