eb9ecf112cdc5d45673077b0603b0fddf4f23cff
Steinar H. Gunderson <sesse@chromium.org>
2022-09-19 09:45:27 +0000

    Make CSSVariableData::backing_string_ an AtomicString.
    Several CSSParserTokens may share the same contents (and be more
    than 8 bytes long, so not covered by SSO). Using AtomicString
    instead of String deduplicates them, saving RAM:
    
      ECommerce       1378 →  1378 kB
      Encyclopedia    6353 →  6352 kB (  -1 kB)
      Extension       6139 →  6139 kB
      News            3888 →  3870 kB ( -18 kB)
      Search          1845 →  1831 kB ( -14 kB)
      Social1         5206 →  5139 kB ( -67 kB)
      Social2          472 →   472 kB
      Sports          3546 →  3546 kB
      Video          11812 → 11209 kB (-603 kB)
    
    Because most strings that we get in from the outside are not
    AtomicString but just String, we can no longer have our CreateResolved()
    fast path that keeps the existing backing strings. Taking this out
    removes a fair bit of complexity, at the risk of using more memory
    again if there are many CSS variables referring to other variables
    (assuming, of course, that those tokens are so long that they are
    cheaper than allocating space for another backing string pointer).
    
    Performance is generally neutral. (An earlier version of the patch
    showed a slight positive effect for the custom property-heavy
    style perftests.)
    
    Change-Id: I56278a8791f0cba5ec474bb7727121d25afa8443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899072
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048540}

--- third_party/blink/renderer/core/css/style_environment_variables_test.cc
+++ third_party/blink/renderer/core/css/style_environment_variables_test.cc
@@ -92,9 +92,7 @@ class StyleEnvironmentVariablesTest : public PageTestBase {
                 name, /*feature_context=*/nullptr),
             {});
     EXPECT_NE(nullptr, data);
-    Vector<String> backing_strings;
-    data->AppendBackingStrings(backing_strings);
-    return backing_strings[0];
+    return data->Serialize();
   }
 
   void SetVariableOnRoot(const AtomicString& name, const String& value) {
