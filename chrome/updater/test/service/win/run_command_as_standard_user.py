78ff08f01fb5a2cbb24384a9e78c5a86121d4de4
Fabrice de Gans <fdegans@chromium.org>
2022-09-19 18:07:44 +0000

    [code-health] Remove references to vpython in /chrome/updater
    This CL was uploaded by git cl split.
    
    Bug: 941669
    Change-Id: I8b66d75a70470069da7aed0c50c47ef5acb71a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900501
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048685}

--- chrome/updater/test/service/win/run_command_as_standard_user.py
+++ chrome/updater/test/service/win/run_command_as_standard_user.py
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 All arguments provided to this program will be used to reconstruct the command
 line for the child process. For example,
-    vpython run_command_as_standard_user.py --command notepad "hello world.txt"
+    vpython3 run_command_as_standard_user.py --command notepad "hello world.txt"
 will launch a process with command line:
     notepad "hello world.txt"
 
