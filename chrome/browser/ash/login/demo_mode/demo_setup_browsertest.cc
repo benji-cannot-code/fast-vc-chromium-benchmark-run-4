47e75588975151486cb0ab888fa01d6beb20eb72
Jackson Tadie <jacksontadie@google.com>
2022-09-19 17:58:02 +0000

    Reland "Show MGS login UI instead of gaia sign-in UI after DemoMode OOBE completion"
    This is a reland of commit 4cf244aced061d8f802f0f52f6ab2c44a74a46b8
    
    The original CL was reverted due to failing tests in
    demo_setup_browsertests.cc. This CL fixes those tests
    by locking the install attributes (similar to what we
    do in wizard_controller_browsertests.cc) prior to proceeding
    through the setup screen, to simulate enrollment success.
    
    Many tests in demo_setup_browsertests are disabled for
    flakiness, but I re-enabled and ran them locally to guarantee that
    this CL does not break them further.
    
    Original change's description:
    > Show MGS login UI instead of gaia sign-in UI after DemoMode OOBE completion
    >
    > Something about the LoginDisplayHostMojo::StartSigninScreen
    > correctly shows the login UI instead of the
    > Gaia sign-in UI that LoginDisplayHostWebUI shows. While I'm
    > not sure exactly what the issue is, either way the proper thing
    > to do is probably to switch to the Mojo display, so that's
    > what this CL does.
    >
    > Additionally, after ~5 seconds on the login screen, the
    > "Loading Demo..." splash screen is shown. This wasn't
    > happening previously, as DemoSession::ShowSplashScreen was
    > called on the LOGIN_PRIMARY SessionState, which was skipped
    > with this Gaia sign-in screen issue. This CL also fixes
    > a reintroduced DCHECK bug caused by checking on the UI
    > thread whether the splash screen image path exists.
    >
    > Ideally the splash screen would be shown right
    > as OOBE completes, fully hiding the login screen. Unfortunately
    > DemoSession isn't initialized early enough to do that, so further
    > changes are needed to refactor the splash screen behavior out
    > into a different object with an earlier lifecycle. This is
    > an improvement for now though on the current (unintended) behavior.
    >
    > and verifying that login pods screen is shown instead
    > of gaia sign-in screen.
    >
    > Test: Manually test by enrolling device into Demo Mode
    > Bug: b/192259053
    > Change-Id: I78c837ce9a266fe2d71908cfd77d9bd7ae6216a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782972
    > Reviewed-by: Renato Silva <rrsilva@google.com>
    > Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1034068}
    
    Bug: b/192259053
    Change-Id: I74629a2718e25a001bbc5fdb35f464a6a4c50213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834293
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048678}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/startup_utils.h"
 #include "chrome/browser/ash/login/test/enrollment_helper_mixin.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
+#include "chrome/browser/ash/login/test/login_or_lock_screen_visible_waiter.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/ash/login/test/oobe_screens_utils.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/test_predicate_waiter.h"
 #include "chrome/browser/ash/login/ui/login_display_host.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/policy/enrollment/enrollment_status.h"
 #include "chrome/browser/component_updater/cros_component_installer_chromeos.h"
 #include "chrome/browser/ui/webui/chromeos/login/demo_preferences_screen_handler.h"
@@ -61,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/system/fake_statistics_provider.h"
 #include "chromeos/system/statistics_provider.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_store.h"
+#include "content/public/test/browser_task_environment.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -335,12 +338,13 @@ class DemoSetupArcSupportedTest : public DemoSetupTestBase {
   }
 
   void AcceptTermsAndExpectDemoSetupProgress() {
+    test::LockDemoDeviceInstallAttributes();
+    // TODO(b/246012796): If possible, re-enable waiting on the setup screen to
+    // be shown
     if (chromeos::features::IsOobeConsolidatedConsentEnabled()) {
       WaitForConsolidatedConsentScreen();
 
-      OobeScreenWaiter setup_progress_waiter(DemoSetupScreenView::kScreenId);
       test::TapConsolidatedConsentAccept();
-      setup_progress_waiter.Wait();
     } else {
       test::WaitForEulaScreen();
       test::TapEulaAccept();
@@ -349,11 +353,7 @@ class DemoSetupArcSupportedTest : public DemoSetupTestBase {
 
       test::OobeJS().ExpectVisiblePath(kArcTosDemoAppsNotice);
 
-      // As setup screen shows only progress indicator and disappears afterwards
-      // we need to set up waiter before the action that triggers the screen.
-      OobeScreenWaiter setup_progress_waiter(DemoSetupScreenView::kScreenId);
       AcceptArcTos();
-      setup_progress_waiter.Wait();
     }
   }
 
@@ -429,7 +429,9 @@ class DemoSetupArcSupportedTest : public DemoSetupTestBase {
     EXPECT_EQ("admin-fr@cros-demo-mode.com",
               DemoSetupController::GetSubOrganizationEmail());
 
-    OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+    // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+    // it's shown again when Demo setup completes
+    LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
     EXPECT_TRUE(StartupUtils::IsOobeCompleted());
     EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
@@ -525,7 +527,9 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   EXPECT_EQ("admin-us@cros-demo-mode.com",
             DemoSetupController::GetSubOrganizationEmail());
 
-  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
@@ -587,7 +591,9 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
 
   EXPECT_EQ("admin-us@cros-demo-mode.com",
             DemoSetupController::GetSubOrganizationEmail());
-  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_EQ("ABC", g_browser_process->local_state()->GetString(
                        prefs::kDemoModeRetailerId));
@@ -814,6 +820,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest, RetryOnErrorScreen) {
   test::OobeJS().ClickOnPath(kDemoPreferencesNext);
 
   AcceptTermsAndExpectDemoSetupFailure();
+  test::LockDemoDeviceInstallAttributes();
 
   // We need to create another mock after showing error dialog.
   enrollment_helper_.ResetMock();
@@ -824,7 +831,9 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest, RetryOnErrorScreen) {
 
   test::OobeJS().ClickOnPath(kDemoSetupErrorDialogRetry);
 
-  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
@@ -971,7 +980,9 @@ IN_PROC_BROWSER_TEST_F(DemoSetupVariantCountryCodeRegionTest,
   EXPECT_EQ("admin-ca@cros-demo-mode.com",
             DemoSetupController::GetSubOrganizationEmail());
 
-  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
@@ -1094,7 +1105,9 @@ IN_PROC_BROWSER_TEST_F(DemoSetupBlazeyDeviceTest,
   EXPECT_EQ("admin-us-blazey@cros-demo-mode.com",
             DemoSetupController::GetSubOrganizationEmail());
 
-  OobeScreenWaiter(GetFirstSigninScreen()).Wait();
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
