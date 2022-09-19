d3b6b52d282c2ac01d0490dfe0875c358ecdfcef
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-09-19 16:50:18 +0000

    Fix KeyedService access on PermissionManager destruction.
    PermissionContextBase contexts are not actual KeyedServices, which
    means they should be destroyed manually. The proper way to do this
    is in PermissionManager::Shutdown() rather than in ~PermissionManager().
    
    Bug: 1365317
    Change-Id: I36715ad4a5c2e41dc0ad3c45740e32d7282f203e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904221
    Auto-Submit: Aleksey Khoroshilov <akhoroshilov@brave.com>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048646}

--- components/permissions/permission_context_base.h
+++ components/permissions/permission_context_base.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
-#include "components/keyed_service/core/keyed_service.h"
 #include "components/permissions/permission_request.h"
 #include "components/permissions/permission_result.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-forward.h"
@@ -68,8 +67,7 @@ using BrowserPermissionCallback = base::OnceCallback<void(ContentSetting)>;
 // See midi_permission_context.h/cc or push_permission_context.cc/h for some
 // examples.
 
-class PermissionContextBase : public KeyedService,
-                              public content_settings::Observer {
+class PermissionContextBase : public content_settings::Observer {
  public:
   PermissionContextBase(
       content::BrowserContext* browser_context,
--- components/permissions/permission_manager.cc
+++ components/permissions/permission_manager.cc
@@ -188,6 +188,7 @@ void PermissionManager::Shutdown() {
     }
     subscription_type_counts_.clear();
   }
+  permission_contexts_.clear();
 }
 
 PermissionContextBase* PermissionManager::GetPermissionContextForTesting(
