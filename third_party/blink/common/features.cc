fae82698042296374823128a3f66217904b250f7
Evan Shrubsole <eshr@google.com>
2022-09-20 14:27:24 +0000

    Disable WebRtcMetronome feature by default
    Bug: 1365817
    Change-Id: I19db7d5978c37b840de6cc78a267fbddc0d7c580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904546
    Auto-Submit: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049088}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1569,7 +1569,7 @@ BASE_FEATURE(kThrottleIntersectionObserverUMA,
 
 BASE_FEATURE(kWebRtcMetronome,
              "WebRtcMetronome",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncAccessHandleAllSyncSurface,
              "SyncAccessHandleAllSyncSurface",
