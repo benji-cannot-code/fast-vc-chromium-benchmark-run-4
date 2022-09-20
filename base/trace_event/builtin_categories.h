a81ddcccb2a4c0b41684d20b3042e4b044d03900
Sean Maher <spvw@chromium.org>
2022-09-20 17:52:35 +0000

    Fix alphabetic order of category list
    Change-Id: I01b75d833e96b28666be2952b02ea20ed1211a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899181
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049204}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("cast.mdns")                                                         \
   X("cast.mdns.socket")                                                  \
   X("cast.stream")                                                       \
+  X("catan_investigation")                                               \
   X("cc")                                                                \
   X("cc.debug")                                                          \
   X("cdp.perf")                                                          \
@@ -102,7 +103,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("ime")                                                               \
   X("IndexedDB")                                                         \
   X("input")                                                             \
-  X("catan_investigation")                                               \
   X("io")                                                                \
   X("ipc")                                                               \
   X("Java")                                                              \
