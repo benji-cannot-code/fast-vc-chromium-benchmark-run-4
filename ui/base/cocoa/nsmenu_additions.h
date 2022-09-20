31a829279c36fc70f99db05ff7a5d50fe521ef75
Jayson Adams <shrike@chromium.org>
2022-09-20 19:13:11 +0000

    [Mac Keyboard shortcuts] Flash menu titles for all command shortcuts
    Chrome bypasses the normal Cocoa keyboard shortcut processing
    machinery. As a result, some commands such as Cmd-N for a new window,
    Cmd-T for a new tab, and others, do not flash the corresponding
    menu title in the menu bar. This cl adds code that creates a menu bar
    flash for these items. It also causes VoiceOver to speak these commands,
    which wasn't the case before.
    
    Bug: 1280769
    
    Change-Id: I24c7b240a130e4fb323e70d8e8d990ffb7e66254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894852
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049266}

--- ui/base/cocoa/nsmenu_additions.h
+++ ui/base/cocoa/nsmenu_additions.h
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // menu item exists.
 - (NSMenuItem*)cr_menuItemForKeyEquivalentEvent:(NSEvent*)event;
 
+// Locates the menu item corresponding to `chromeCommand` and flashes
+// its owning menu title in the menu bar (without executing the command).
+// Also causes VoiceOver to speak the menu item. Returns NO if the
+// command wasn't found in the menus.
++ (BOOL)flashMenuForChromeCommand:(int)chromeCommand;
+
 @end
 
 #endif  // UI_BASE_COCOA_NSMENU_ADDITIONS_H_
