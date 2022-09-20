195ce1a3cdee41583a08cab9ec677ed1f75693b3
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-09-20 01:30:51 +0000

    [last-baseline] Support last baseline alignment within grid/flex.
    This adds support for last baseline alignment within grid/flex with
    block layout as children (or rather anything which exports a "correct"
    last baseline).
    
    If something has last baseline alignment it will be placed in the
    opposite baseline sharing group that it would naturally be.
    
    The small additional code in the flex/grid layout algorithm is to
    read the baseline from the correct edge.
    
    Bug: 885175
    Change-Id: Ibf5d6dff79e1b611a459257647652506280932b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892666
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048893}

--- third_party/blink/renderer/core/layout/flexible_box_algorithm.h
+++ third_party/blink/renderer/core/layout/flexible_box_algorithm.h
@@ -176,7 +176,7 @@ class FlexItem {
   LayoutUnit MainAxisMarginExtent() const;
   LayoutUnit CrossAxisMarginExtent() const;
 
-  LayoutUnit MarginBoxAscent(bool is_wrap_reverse) const;
+  LayoutUnit MarginBoxAscent(bool is_last_baseline, bool is_wrap_reverse) const;
 
   LayoutUnit AvailableAlignmentSpace() const;
 
