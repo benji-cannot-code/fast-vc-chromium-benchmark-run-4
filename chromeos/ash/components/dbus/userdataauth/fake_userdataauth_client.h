6c2e88894803bf8aac5a0fdfa168a3a14c4a86e9
Martin Bidlingmaier <mbid@google.com>
2022-09-20 09:47:26 +0000

    Add ListAuthFactors method to user data auth client
    Bug: b:241259026
    Change-Id: Ie9fe21b67e2986b42e0c05957ac35878c66851c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906042
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049015}

--- chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
+++ chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h
@@ -250,7 +250,7 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeUserDataAuthClient
       const ::user_data_auth::RemoveAuthFactorRequest& request,
       RemoveAuthFactorCallback callback) override;
   void ListAuthFactors(const ::user_data_auth::ListAuthFactorsRequest& request,
-                       ListAuthFactorsCallback callback);
+                       ListAuthFactorsCallback callback) override;
   void GetRecoveryRequest(
       const ::user_data_auth::GetRecoveryRequestRequest& request,
       GetRecoveryRequestCallback callback) override;
