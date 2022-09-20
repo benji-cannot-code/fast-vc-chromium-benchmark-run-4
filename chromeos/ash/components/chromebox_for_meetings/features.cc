aa848cd9be353d7fe87359b29abb248699b336f3
Jazbel Wang <jazbel@google.com>
2022-09-20 16:55:14 +0000

    chromebox_for_meetings: flag for Meet XU controls.
    BUG=b:246548350
    TEST=n/a
    
    Change-Id: Id37a4d89717bf9f08cadb455a14e0da3e051f8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904981
    Commit-Queue: Jazbel Wang <jazbel@chromium.org>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049169}

--- chromeos/ash/components/chromebox_for_meetings/features.cc
+++ chromeos/ash/components/chromebox_for_meetings/features.cc
@@ -13,4 +13,7 @@ const base::Feature kCloudLogger{"MeetDevicesCloudLogger",
 const base::Feature kMojoServices{"MeetDevicesMojoServices",
                                   base::FEATURE_ENABLED_BY_DEFAULT};
 
+const base::Feature kXuControls{"MeetDevicesXuControls",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace ash::cfm::features
