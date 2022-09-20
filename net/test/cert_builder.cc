d02eaecec8b53467b981d9bc72f9a4b8cf1588f0
Nick Harper <nharper@chromium.org>
2022-09-20 19:59:21 +0000

    Make SignatureAlgorithm optional in ParsedCertificate
    Bug: 1321688
    Change-Id: I0f7232e53a32cb910209e1a83066ae0b4402e20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872527
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049291}

--- net/test/cert_builder.cc
+++ net/test/cert_builder.cc
@@ -692,6 +692,12 @@ void CertBuilder::SetSignatureAlgorithm(
   Invalidate();
 }
 
+void CertBuilder::SetSignatureAlgorithmTLV(
+    base::StringPiece signature_algorithm_tlv) {
+  signature_algorithm_tlv_ = std::string(signature_algorithm_tlv);
+  Invalidate();
+}
+
 void CertBuilder::SetRandomSerialNumber() {
   serial_number_ = base::RandUint64();
   Invalidate();
@@ -984,8 +990,10 @@ void CertBuilder::GenerateCertificate() {
     signature_algorithm = DefaultSignatureAlgorithmForKey(issuer_->GetKey());
   ASSERT_TRUE(signature_algorithm.has_value());
 
-  std::string signature_algorithm_tlv =
-      SignatureAlgorithmToDer(*signature_algorithm);
+  std::string signature_algorithm_tlv = signature_algorithm_tlv_;
+  if (signature_algorithm_tlv.empty()) {
+    signature_algorithm_tlv = SignatureAlgorithmToDer(*signature_algorithm);
+  }
   ASSERT_FALSE(signature_algorithm_tlv.empty());
 
   std::string tbs_cert;
