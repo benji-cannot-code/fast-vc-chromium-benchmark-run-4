b497183399761ee6179997aca5fc1842a02ac571
manukh <manukh@chromium.org>
2022-09-20 16:12:47 +0000

    [memories][persist-clusters] Debug output visit times.
    Currently, the visit WebUI debug shows visit id, score, duration. With
    this CL, also shows visit time.
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I0f5c9a324b823a8e4eca559d47459042e3706a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905468
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049138}

--- chrome/browser/ui/webui/history_clusters/history_clusters_handler.cc
+++ chrome/browser/ui/webui/history_clusters/history_clusters_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "base/time/time_to_iso8601.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history_clusters/history_clusters_metrics_logger.h"
@@ -177,6 +178,8 @@ mojom::URLVisitPtr VisitToMojom(Profile* profile,
     visit_mojom->debug_info["visit_id"] =
         base::NumberToString(annotated_visit.visit_row.visit_id);
     visit_mojom->debug_info["score"] = base::NumberToString(visit.score);
+    visit_mojom->debug_info["visit_time"] =
+        base::TimeToISO8601(visit.annotated_visit.visit_row.visit_time);
     visit_mojom->debug_info["visit_duration"] = base::NumberToString(
         annotated_visit.visit_row.visit_duration.InSecondsF());
   }
