2741d2c4cceee7f0090fa4e19e341bf74f9920a9
Martin Kreichgauer <martinkr@google.com>
2022-09-20 19:57:36 +0000

    webauthn: update request dialog QR sheet
    Remove the header illustration and update strings according to UX mocks.
    
    Updated screenshot: https://screenshot.googleplex.com/7tkU3HqkbrghEA6.png
    
    All new behavior is flag guarded by
    `device::kWebAuthnNewDiscoverableCredentialsUi` (default off). No behavior changes if the flag is disabled.
    
    Bug: 1358719
    Change-Id: I1f136aba8078f43e2c5161275c0b49254d1b3546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902508
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049289}

--- chrome/browser/ui/webauthn/sheet_models.cc
+++ chrome/browser/ui/webauthn/sheet_models.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/fido_types.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/font_list.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/text_utils.h"
 #include "url/gurl.h"
 
@@ -1446,15 +1447,43 @@ AuthenticatorQRSheetModel::~AuthenticatorQRSheetModel() = default;
 
 const gfx::VectorIcon& AuthenticatorQRSheetModel::GetStepIllustration(
     ImageColorScheme color_scheme) const {
+  if (base::FeatureList::IsEnabled(
+          device::kWebAuthnNewDiscoverableCredentialsUi)) {
+    return gfx::kNoneIcon;
+  }
   return color_scheme == ImageColorScheme::kDark ? kWebauthnPhoneDarkIcon
                                                  : kWebauthnPhoneIcon;
 }
 
 std::u16string AuthenticatorQRSheetModel::GetStepTitle() const {
+  if (base::FeatureList::IsEnabled(
+          device::kWebAuthnNewDiscoverableCredentialsUi)) {
+    switch (dialog_model()->transport_availability()->request_type) {
+      case device::FidoRequestType::kMakeCredential:
+        // TODO(1358719): i18n
+        return u"Create a passkey on another device";
+      case device::FidoRequestType::kGetAssertion:
+        // TODO(1358719): i18n
+        return u"Use a passkey from another device";
+    }
+  }
   return l10n_util::GetStringUTF16(IDS_WEBAUTHN_CABLEV2_ADD_PHONE);
 }
 
 std::u16string AuthenticatorQRSheetModel::GetStepDescription() const {
+  if (base::FeatureList::IsEnabled(
+          device::kWebAuthnNewDiscoverableCredentialsUi)) {
+    switch (dialog_model()->transport_availability()->request_type) {
+      case device::FidoRequestType::kMakeCredential:
+        // TODO(1358719): i18n
+        return u"Scan this QR code with your phone to create a passkey for " +
+               GetRelyingPartyIdString(dialog_model());
+      case device::FidoRequestType::kGetAssertion:
+        // TODO(1358719): i18n
+        return u"Scan this QR code to use a passkey for " +
+               GetRelyingPartyIdString(dialog_model()) + u" from your phone";
+    }
+  }
   return l10n_util::GetStringUTF16(IDS_BROWSER_SHARING_QR_CODE_DIALOG_TOOLTIP);
 }
 
