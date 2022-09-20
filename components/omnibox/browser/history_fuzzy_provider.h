3145599739862ace398fbc326485d721378e29c4
Orin Jaworski <orinj@google.com>
2022-09-20 23:27:30 +0000

    [omnibox] Implement fuzzy TRANSPOSE edit operation with distance one
    This CL improves the fuzzy trie search algorithm with the addition of
    TRANSPOSE edit operations that swap two adjacent characters. This was
    previously supported at edit distance 2 using an edit operation pair:
      - REPLACE + REPLACE
      - INSERT + DELETE
      - DELETE + INSERT
    But now with TRANSPOSE, the swap is achieved directly with edit
    distance 1. Since this is a common typo, direct transposition support
    catches more typos with the same tolerance. The cost is one step of
    lookahead, first in the input string and then in the trie, but this is
    balanced by the potential for shorter searches as the input index
    advances faster and the edit pair paths need not be explored.
    
    Bug: 1286859
    Change-Id: Ibdbfd03a90078f2ad2183a4defb11d8a75a40e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908192
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049382}

--- components/omnibox/browser/history_fuzzy_provider.h
+++ components/omnibox/browser/history_fuzzy_provider.h
@@ -36,6 +36,7 @@ struct Edit {
     DELETE,
     INSERT,
     REPLACE,
+    TRANSPOSE,
   };
 
   Edit(Kind kind, size_t at, char16_t new_char);
@@ -46,7 +47,8 @@ struct Edit {
   // The edit operation, the kind of change to make to text.
   Kind kind;
 
-  // Character data; relevant only for REPLACE and INSERT.
+  // Character data; relevant for REPLACE and INSERT, and also for
+  // TRANSPOSE (as a minor optimization, first char is stored here).
   char16_t new_char;
 
   // Text index at which to apply change.
