b70e4350eac30b3275a738993429b34112387812
Takumi Fujimoto <takumif@chromium.org>
2022-09-20 04:29:18 +0000

    Revert "Fix apparent bug in touch device handling."
    This reverts commit 2905efcd639bb446cc9ee611f20d30ed2fdf4052.
    
    Reason for revert: Suspected (with low confidence) to be the culprit for inputs.VirtualKeyboard* failures
    
    First failure: https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-cfi-thin-lto-chrome/39421/overview
    
    Original change's description:
    > Fix apparent bug in touch device handling.
    >
    > Based on the comment in this function, the existing behavior was
    > backwards. It removed devices that _are_ connected instead of devices
    > that _aren't_.
    >
    > Simplify the implementation to make the behavior clearer.
    >
    > Bug: none
    > Change-Id: Ie30cacb40ee11dfd7df4fcd817d43d28e442eb91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3903503
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1048739}
    
    Bug: none
    Change-Id: Ib445621f59487df5a8bf12acdbaa0229fc0e2ce6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907162
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1048942}

--- ui/events/devices/device_data_manager.cc
+++ ui/events/devices/device_data_manager.cc
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/devices/device_data_manager.h"
 
+#include <algorithm>
+
 #include "base/at_exit.h"
 #include "base/bind.h"
 #include "base/check_op.h"
-#include "base/containers/contains.h"
 #include "base/observer_list.h"
 #include "ui/display/types/display_constants.h"
 #include "ui/events/devices/input_device_event_observer.h"
@@ -108,10 +109,17 @@ void DeviceDataManager::UpdateTouchMap() {
   auto last_iter = std::remove_if(
       touch_map_.begin(), touch_map_.end(),
       [this](const std::pair<int, TouchDeviceTransform>& map_entry) {
+        // Check if the given |map_entry| is present in the current list of
+        // connected devices.
+        auto iter = std::find_if(
+            touchscreen_devices_.begin(), touchscreen_devices_.end(),
+            [&map_entry](const TouchscreenDevice& touch_device) {
+              return touch_device.id == map_entry.second.device_id;
+            });
+
         // Remove the device identified by |map_entry| from |touch_map_| if it
         // is not present in the list of currently connected devices.
-        return !base::Contains(touchscreen_devices_, map_entry.second.device_id,
-                               &TouchscreenDevice::id);
+        return iter != touchscreen_devices_.end();
       });
   touch_map_.erase(last_iter, touch_map_.end());
 }
