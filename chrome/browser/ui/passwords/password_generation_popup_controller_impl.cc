2cdb2059e3e770d556a932e888881ebb56ad6479
Maria Timbur <mtimbur@google.com>
2022-09-20 14:30:34 +0000

    Add IsVisible() method in PasswordGenerationPopupControllerImpl
    LOW_COVERAGE_REASON=Unrelated to this change as the added lines are tested in the browsertest below.
    
    Change-Id: I6765da0f150bac34097bc4a79fb1a6347f980c94
    Bug: 1345766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883780
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maria Timbur <mtimbur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049091}

--- chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
+++ chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
@@ -189,6 +189,10 @@ bool PasswordGenerationPopupControllerImpl::HandleKeyPressEvent(
   }
 }
 
+bool PasswordGenerationPopupControllerImpl::IsVisible() const {
+  return view_;
+}
+
 bool PasswordGenerationPopupControllerImpl::PossiblyAcceptPassword() {
   if (password_selected_) {
     PasswordAccepted();  // This will delete |this|.
@@ -223,7 +227,7 @@ void PasswordGenerationPopupControllerImpl::PasswordAccepted() {
     weak_this->HideImpl();
 }
 
-bool PasswordGenerationPopupControllerImpl::Show(GenerationUIState state) {
+void PasswordGenerationPopupControllerImpl::Show(GenerationUIState state) {
   // When switching from editing to generation state, regenerate the password.
   if (state == kOfferGeneration &&
       (state_ != state || current_generated_password_.empty())) {
@@ -234,13 +238,16 @@ bool PasswordGenerationPopupControllerImpl::Show(GenerationUIState state) {
   }
   state_ = state;
 
+  // TODO(crbug.com/1345766): Call the password strength calculation from the
+  // utility process. Store the strength and use it accordingly in the
+  // PasswordGenerationPopupViewViews.
   if (!view_) {
     view_ = PasswordGenerationPopupView::Create(GetWeakPtr());
 
     // Treat popup as being hidden if creation fails.
     if (!view_) {
       HideImpl();
-      return false;
+      return;
     }
     key_press_handler_manager_->RegisterKeyPressHandler(base::BindRepeating(
         [](base::WeakPtr<PasswordGenerationPopupControllerImpl> weak_this,
@@ -250,20 +257,18 @@ bool PasswordGenerationPopupControllerImpl::Show(GenerationUIState state) {
         GetWeakPtr()));
     if (!view_->Show()) {
       // The instance is deleted after this point.
-      return false;
+      return;
     }
   } else {
     view_->UpdateState();
     if (!view_->UpdateBoundsAndRedrawPopup()) {
       // The instance is deleted after this point.
-      return false;
+      return;
     }
   }
 
   if (observer_)
     observer_->OnPopupShown(state_);
-
-  return true;
 }
 
 void PasswordGenerationPopupControllerImpl::UpdateTypedPassword(
