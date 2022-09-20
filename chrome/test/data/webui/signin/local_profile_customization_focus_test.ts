c04b3c09c4c3edfdfdafcadb7f0a19e126a86712
Gabriel Oliveira <gabolvr@google.com>
2022-09-20 15:32:57 +0000

    [Signin] Add SyncPromoAfterSigninInterceptStudy in field trial testing config
    Bug: 1282157
    Change-Id: I3e05b0b4c6a17cc02f36f374335f5525bf5a4b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899896
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049120}

--- chrome/test/data/webui/signin/local_profile_customization_focus_test.ts
+++ chrome/test/data/webui/signin/local_profile_customization_focus_test.ts
@@ -27,6 +27,9 @@ suite('LocalProfileCustomizationFocusTest', function() {
   }
 
   setup(function() {
+    loadTimeData.overrideValues({
+      isLocalProfileCreationDialogEnabled: false,
+    });
     browserProxy = new TestManageProfilesBrowserProxy();
     ManageProfilesBrowserProxyImpl.setInstance(browserProxy);
     return resetTestElement(Routes.MAIN);
@@ -70,7 +73,7 @@ suite('LocalProfileCustomizationFocusTest', function() {
 
   test('ProfileCreationFlowWithSigninPromo', async function() {
     assertTrue(loadTimeData.getValue('isBrowserSigninAllowed'));
-    navigateTo(Routes.NEW_PROFILE);
+    await resetTestElement(Routes.NEW_PROFILE);
     await setupProfileCreation();
     const choice = testElement.shadowRoot!.querySelector('profile-type-choice');
     assertTrue(!!choice);
