4a9d95594bba5d2814bfe63ca461281d9915f8b8
Yuzhu Shen <yzshen@chromium.org>
2022-09-20 18:41:14 +0000

    Exo: introduce WaylandServerTest gtest fixture and related classes.
    WaylandServerTest is used for testing Wayland server-side logic and exercise the
    Wayland protocol, running a test client on a secondary thread.
    
    This CL also updates security_delegate_binding_test.cc to use this new gtest
    fixture. The improvements include:
    - Reduces uninteresting client-side setup code. GlobalBindings is removed as a
      result. GlobalBindings actually had a bug of using a stack object after destruction so this CL fixed that.
    - Avoids directly accessing exo server state from a different thread without
      synchronization.
    
    LOW_COVERAGE_REASON=test_wayland_client_thread.cc is a testing utility class. In this CL the testing coverage is 79%. There are some parts (such as OnFileCanReadWithoutBlocking) are expected to be exercised in future test cases.
    
    Bug: 1251503
    Change-Id: Ife414be926d82dbc53ecdf3e72170fb9d1a6ede7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902952
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049233}

--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -494,7 +494,7 @@ void Server::StartWithDefaultPath(StartCallback callback) {
   Finalize(std::move(callback), /*success=*/true);
 }
 
-bool Server::AddSocket(const std::string name) {
+bool Server::AddSocket(const std::string& name) {
   DCHECK(!name.empty());
   return !wl_display_add_socket(wl_display_.get(), name.c_str());
 }
