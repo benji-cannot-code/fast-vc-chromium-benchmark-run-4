021aadc7f76c3ba1ea84638c1f3d6e1cb60b0fd5
Matt Menke <mmenke@chromium.org>
2022-09-20 16:35:17 +0000

    Migrate components/embedder_support to new pref update classes.
    ListPrefUpdate and DictPrefUpdate are now deprecated, in favor of
    ScopedListPrefUpdate and ScopedDictPrefUpdate, which only expose
    Value::List and Value::Dict, respectively, to discourage use of other
    APIs to manipulate lists and dicts, which are all deprecated.
    
    Bug: 1362719
    Change-Id: I18b0a11cd24a164b32a4af40d7ee762196f0a3fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906881
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049155}

--- components/embedder_support/origin_trials/component_updater_utils.cc
+++ components/embedder_support/origin_trials/component_updater_utils.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/embedder_support/origin_trials/pref_names.h"
 #include "components/prefs/pref_service.h"
-#include "components/prefs/scoped_user_pref_update.h"
 
 namespace {
 
@@ -29,30 +28,31 @@ void ReadOriginTrialsConfigAndPopulateLocalState(PrefService* local_state,
                                                  base::Value manifest) {
   DCHECK(local_state);
 
+  base::Value::Dict& manifest_dict = manifest.GetDict();
+
   if (std::string* override_public_key =
-          manifest.FindStringPath(kManifestPublicKeyPath)) {
+          manifest_dict.FindStringByDottedPath(kManifestPublicKeyPath)) {
     local_state->Set(prefs::kOriginTrialPublicKey,
                      base::Value(*override_public_key));
   } else {
     local_state->ClearPref(prefs::kOriginTrialPublicKey);
   }
 
-  base::Value* override_disabled_feature_list =
-      manifest.FindListPath(kManifestDisabledFeaturesPath);
+  base::Value::List* override_disabled_feature_list =
+      manifest_dict.FindListByDottedPath(kManifestDisabledFeaturesPath);
   if (override_disabled_feature_list &&
-      !override_disabled_feature_list->GetListDeprecated().empty()) {
-    ListPrefUpdate update(local_state, prefs::kOriginTrialDisabledFeatures);
-    *update = std::move(*override_disabled_feature_list);
+      !override_disabled_feature_list->empty()) {
+    local_state->SetList(prefs::kOriginTrialDisabledFeatures,
+                         std::move(*override_disabled_feature_list));
   } else {
     local_state->ClearPref(prefs::kOriginTrialDisabledFeatures);
   }
 
-  base::Value* disabled_tokens_list =
-      manifest.FindListPath(kManifestDisabledTokenSignaturesPath);
-  if (disabled_tokens_list &&
-      !disabled_tokens_list->GetListDeprecated().empty()) {
-    ListPrefUpdate update(local_state, prefs::kOriginTrialDisabledTokens);
-    *update = std::move(*disabled_tokens_list);
+  base::Value::List* disabled_tokens_list =
+      manifest_dict.FindListByDottedPath(kManifestDisabledTokenSignaturesPath);
+  if (disabled_tokens_list && !disabled_tokens_list->empty()) {
+    local_state->SetList(prefs::kOriginTrialDisabledTokens,
+                         std::move(*disabled_tokens_list));
   } else {
     local_state->ClearPref(prefs::kOriginTrialDisabledTokens);
   }
