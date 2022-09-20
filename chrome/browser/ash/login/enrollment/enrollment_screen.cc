4932122c0c58e678bdcab4b05a11c1b8821a070e
Brahim Chikhaoui <bchikhaoui@google.com>
2022-09-20 12:19:38 +0000

    [OOBE] Add skip enrollment dialogue for LP devices
    Update the shelfButton logic to show new dialogue if device is
    License packaged device.
    Implement new browser tests for this new dialogue.
    
    BUG=b:247067982
    
    Change-Id: I046ce1baeee50dbca9c234be1b5d0f6b42a7e5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904061
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049048}

--- chrome/browser/ash/login/enrollment/enrollment_screen.cc
+++ chrome/browser/ash/login/enrollment/enrollment_screen.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/callback_helpers.h"
+#include "base/check_is_test.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -97,6 +98,7 @@ std::string GetEnterpriseDomainManager() {
 }
 
 constexpr char kUserActionCancelTPMCheck[] = "cancel-tpm-check";
+constexpr char kUserActionSkipDialogConfirmation[] = "skip-confirmation";
 
 // Max number of retries to check install attributes state.
 constexpr int kMaxInstallAttributesStateCheckRetries = 60;
@@ -246,6 +248,13 @@ void EnrollmentScreen::ClearAuth(base::OnceClosure callback) {
 void EnrollmentScreen::OnAuthCleared(base::OnceClosure callback) {
   enrollment_helper_ = nullptr;
   std::move(callback).Run();
+}
+
+void EnrollmentScreen::ShowSkipEnrollmentDialogue() {
+  DCHECK(config_.is_license_packaged_with_device);
+  if (view_) {
+    view_->ShowSkipConfirmationDialog();
+  }
 }
 
 bool EnrollmentScreen::MaybeSkip(WizardContext& context) {
@@ -476,8 +485,13 @@ void EnrollmentScreen::ProcessRetry() {
 
 bool EnrollmentScreen::HandleAccelerator(LoginAcceleratorAction action) {
   if (action == LoginAcceleratorAction::kCancelScreenAction) {
-    OnCancel();
-    return true;
+    if (config_.is_license_packaged_with_device) {
+      ShowSkipEnrollmentDialogue();
+      return true;
+    } else {
+      OnCancel();
+      return true;
+    }
   }
   return false;
 }
@@ -799,9 +813,13 @@ void EnrollmentScreen::OnActiveDirectoryJoined(
 void EnrollmentScreen::OnUserActionDeprecated(const std::string& action_id) {
   if (action_id == kUserActionCancelTPMCheck) {
     OnCancel();
-  } else {
-    BaseScreen::OnUserActionDeprecated(action_id);
+    return;
   }
+  if (action_id == kUserActionSkipDialogConfirmation) {
+    OnCancel();
+    return;
+  }
+  BaseScreen::OnUserActionDeprecated(action_id);
 }
 
 void EnrollmentScreen::UpdateChromadMigrationOobeFlow(bool exists) {
