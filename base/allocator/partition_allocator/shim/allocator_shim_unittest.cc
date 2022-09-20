840fa61c8ef137a8de98e7efe7ecf84f1bb10bb5
Yuki Shiino <yukishiino@chromium.org>
2022-09-20 10:14:02 +0000

    PA: Mirror ios_util.h and mac_util.h
    Mirrors iso_util.h, mac_util.h, and sys_info.h to PA library.
    This patch contains the change in https://crrev.com/c/3876779 .
    
    Bug: 1151236, 1337681
    Change-Id: I3937be946538e1432c933760699323ce28c36c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878964
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049022}

--- base/allocator/partition_allocator/shim/allocator_shim_unittest.cc
+++ base/allocator/partition_allocator/shim/allocator_shim_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif BUILDFLAG(IS_APPLE)
 #include <malloc/malloc.h>
 #include "base/allocator/partition_allocator/shim/allocator_interception_mac.h"
-#include "base/mac/mac_util.h"
 #include "third_party/apple_apsl/malloc.h"
 #else
 #include <malloc.h>
