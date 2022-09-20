118e9102a8067a7f50e9191fec82958a552378e0
Rune Lillesveen <futhark@chromium.org>
2022-09-20 09:57:31 +0000

    Cycle detection registered custom properties with lh units
    Like for font-relative properties, using lh units in registered custom
    properties referenced in the line-height property may cause cycles.
    Detect and correctly handle such cycles.
    
    Bug: 937104
    Change-Id: Id2197400addb2f67750b1e2563410ca6cf5319d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904621
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049018}

--- third_party/blink/renderer/core/css/css_variable_data.cc
+++ third_party/blink/renderer/core/css/css_variable_data.cc
@@ -56,6 +56,11 @@ static bool IsRootFontUnitToken(CSSParserToken token) {
          token.GetUnitType() == CSSPrimitiveValue::UnitType::kRems;
 }
 
+static bool IsLineHeightUnitToken(CSSParserToken token) {
+  return token.GetType() == kDimensionToken &&
+         token.GetUnitType() == CSSPrimitiveValue::UnitType::kLhs;
+}
+
 String CSSVariableData::Serialize() const {
   if (original_text_) {
     if (original_text_.EndsWith('\\')) {
@@ -107,6 +112,7 @@ void CSSVariableData::ConsumeAndUpdateTokens(const CSSParserTokenRange& range) {
       string_builder.Append(token.Value());
     has_font_units_ |= IsFontUnitToken(token);
     has_root_font_units_ |= IsRootFontUnitToken(token);
+    has_line_height_units_ |= IsLineHeightUnitToken(token);
     ++num_tokens_;
   }
   backing_string_ = string_builder.ToAtomicString();
--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -736,6 +736,7 @@ StyleCascade::TokenSequence::TokenSequence(const CSSVariableData* data)
     : is_animation_tainted_(data->IsAnimationTainted()),
       has_font_units_(data->HasFontUnits()),
       has_root_font_units_(data->HasRootFontUnits()),
+      has_line_height_units_(data->HasLineHeightUnits()),
       base_url_(data->BaseURL()),
       charset_(data->Charset()) {}
 
@@ -756,6 +757,7 @@ bool StyleCascade::TokenSequence::Append(const TokenSequence& sequence,
   is_animation_tainted_ |= sequence.is_animation_tainted_;
   has_font_units_ |= sequence.has_font_units_;
   has_root_font_units_ |= sequence.has_root_font_units_;
+  has_line_height_units_ |= sequence.has_line_height_units_;
   return true;
 }
 
@@ -766,6 +768,7 @@ bool StyleCascade::TokenSequence::Append(CSSVariableData* data,
   is_animation_tainted_ |= data->IsAnimationTainted();
   has_font_units_ |= data->HasFontUnits();
   has_root_font_units_ |= data->HasRootFontUnits();
+  has_line_height_units_ |= data->HasLineHeightUnits();
   return true;
 }
 
@@ -823,6 +826,9 @@ const CSSValue* StyleCascade::ResolveCustomProperty(
 
   if (HasFontSizeDependency(To<CustomProperty>(property), data.get()))
     resolver.DetectCycle(GetCSSPropertyFontSize());
+
+  if (HasLineHeightDependency(To<CustomProperty>(property), data.get()))
+    resolver.DetectCycle(GetCSSPropertyLineHeight());
 
   state_.Style()->SetHasVariableDeclaration();
 
@@ -1132,6 +1138,15 @@ bool StyleCascade::HasFontSizeDependency(const CustomProperty& property,
   if (data->HasFontUnits())
     return true;
   if (data->HasRootFontUnits() && IsRootElement())
+    return true;
+  return false;
+}
+
+bool StyleCascade::HasLineHeightDependency(const CustomProperty& property,
+                                           CSSVariableData* data) const {
+  if (!property.IsRegistered() || !data)
+    return false;
+  if (data->HasLineHeightUnits())
     return true;
   return false;
 }
