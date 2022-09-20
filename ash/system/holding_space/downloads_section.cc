239e644dfda21417771e2d3f75feed60818fcf73
Colin Kincaid <ckincaid@chromium.org>
2022-09-20 22:58:31 +0000

    Disambiguate ash chevrons by internal padding
    Today, the chevron icons in ash have inconsistent padding and thus
    appear as different sizes. This CL replaces the chevron-right icon,
    which appeared smaller than the chevron-up and chevron-down icons,
    with a chevron-small-right icon. This CL also adds chevron-small-up
    and chevron-small-down icons to use directly instead of rotating the
    smaller chevron-right icon.
    
    This CL is meant to make icon naming clearer and get rid of an
    icon-rotating workaround; there is no expected visual change.
    
    Launcher up before: https://screenshot.googleplex.com/BMMrAp5QZsKB7ZP
    Launcher up after: https://screenshot.googleplex.com/9ExrUwuN7EHxfnP
    Launcher down before: https://screenshot.googleplex.com/7PfsJepxdzeUBuT
    Launcher down after: https://screenshot.googleplex.com/4XuEctxpLgKbHsu
    Tote up before: https://screenshot.googleplex.com/EH3ZKFHBdjuDq3u
    Tote up after: https://screenshot.googleplex.com/7r3LFcq732JxMaX
    Tote down before: https://screenshot.googleplex.com/t4LKk5UFBfLkQgj
    Tote down after: https://screenshot.googleplex.com/kkwryvy4gDmgsgw
    Tote right before: https://screenshot.googleplex.com/A2T4JTKrMev29Nw
    Tote right after: https://screenshot.googleplex.com/5ZjkGHiKRSiUkAm
    
    Future CL(s) will update other chevrons in System UI that use non-ash
    vector icons.
    
    Bug: 1358642
    Change-Id: I972754aafff93ca323bed95c93dded1d7865be3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902389
    Auto-Submit: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049374}

--- ash/system/holding_space/downloads_section.cc
+++ ash/system/holding_space/downloads_section.cc
@@ -90,7 +90,7 @@ class Header : public views::Button {
 
     // Chevron.
     chevron_->SetImage(gfx::CreateVectorIcon(
-        kChevronRightIcon, kHoldingSpaceSectionChevronIconSize,
+        kChevronRightSmallIcon, kHoldingSpaceSectionChevronIconSize,
         ash_color_provider->GetContentLayerColor(
             AshColorProvider::ContentLayerType::kIconColorPrimary)));
   }
