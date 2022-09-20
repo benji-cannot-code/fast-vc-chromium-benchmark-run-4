6ae6e209ccbb8a87545aa27114d9d97dc3857017
Gang Wu <gangwu@chromium.org>
2022-09-20 01:46:33 +0000

    [Omnibox] Calling setTint instead of setColorFilter in ToolbarPhone
    We use #setColorFilter when create the background[1]. For our new
    experiment, we also update background color by #setTint[2]. But Using
    #setColorFilter will disable tint[3], ex, Omnibox in
    https://screenshot.googleplex.com/9dyYPGbwfw2sLYy.
    Since http://g/chromium-dev-internal/HIJYVwLVdOQ/4xFXaCVZAQAJ?hl=en,
    we can remove setColorFilter now, so using #setTint all the place will
    make the color of the Omnibox as our expected.
    
    Before: https://screenshot.googleplex.com/9dyYPGbwfw2sLYy
    After : https://screenshot.googleplex.com/8ypyx5pDSVxP6eU
    
    [1]:
    https://crsrc.org/c/chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java;l=357?q=createModernLocationBarBackground&sq=&ss=chromium%2Fchromium%2Fsrc
    [2]:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java;l=50;drc=4c33ea7ce9a4064087f939723c0e9713193cf54f;bpv=1;bpt=1
    [3]:
    https://developer.android.com/reference/android/graphics/drawable/Drawable#setColorFilter(int,%20android.graphics.PorterDuff.Mode)
    
    Bug: 1365658, 1239289
    Change-Id: I64a4effeff4ec6904d599316d2bed4cfb2e9f3f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905754
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048900}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhoneTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhoneTest.java
@@ -14,13 +14,11 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import android.graphics.Canvas;
-import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -244,9 +242,8 @@ public class ToolbarPhoneTest {
                             false /*isIncognito*/)));
         });
         verify(mLocationbarBackgroundDrawable)
-                .setColorFilter(
-                        locationBarCoordinator.getSuggestionBackgroundColor(false /*isIncognito*/),
-                        PorterDuff.Mode.SRC_IN);
+                .setTint(
+                        locationBarCoordinator.getSuggestionBackgroundColor(false /*isIncognito*/));
         verify(mLocationbarBackgroundDrawable, atLeastOnce()).setCornerRadius(focusedRadius);
 
         // Clear focus on the Omnibox
@@ -258,8 +255,7 @@ public class ToolbarPhoneTest {
                     Matchers.not(locationBarCoordinator.getDropdownBackgroundColor(
                             false /*isIncognito*/)));
         });
-        verify(mLocationbarBackgroundDrawable, atLeastOnce())
-                .setColorFilter(anyInt(), eq(PorterDuff.Mode.SRC_IN));
+        verify(mLocationbarBackgroundDrawable, atLeastOnce()).setTint(anyInt());
         verify(mLocationbarBackgroundDrawable, atLeastOnce()).setCornerRadius(nonFocusedRadius);
     }
 
@@ -287,9 +283,7 @@ public class ToolbarPhoneTest {
                             false /*isIncognito*/)));
         });
         verify(mLocationbarBackgroundDrawable)
-                .setColorFilter(
-                        locationBarCoordinator.getDropdownBackgroundColor(false /*isIncognito*/),
-                        PorterDuff.Mode.SRC_IN);
+                .setTint(locationBarCoordinator.getDropdownBackgroundColor(false /*isIncognito*/));
         verify(mLocationbarBackgroundDrawable, never()).setCornerRadius(anyInt());
 
         // Clear focus on the Omnibox
@@ -301,8 +295,7 @@ public class ToolbarPhoneTest {
                     Matchers.not(locationBarCoordinator.getDropdownBackgroundColor(
                             false /*isIncognito*/)));
         });
-        verify(mLocationbarBackgroundDrawable, atLeastOnce())
-                .setColorFilter(anyInt(), eq(PorterDuff.Mode.SRC_IN));
+        verify(mLocationbarBackgroundDrawable, atLeastOnce()).setTint(anyInt());
         verify(mLocationbarBackgroundDrawable, never()).setCornerRadius(anyInt());
     }
 
