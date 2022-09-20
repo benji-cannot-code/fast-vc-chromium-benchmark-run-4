4085e0c0c3787cb2c55cec1b5ee4e06e3385a2aa
Gauthier Ambard <gambard@chromium.org>
2022-09-20 12:34:48 +0000

    [iOS] Prepare the URL files to be moved
    Add temporary files for downstream compatibility.
    
    Bug: 1354614
    Change-Id: I77a8f3acbbbcdea7f6ba6a6c915b2a609c7b517b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904403
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049052}

---
+++ ios/chrome/browser/url/chrome_url_constants.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_URL_CHROME_URL_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_URL_CHROME_URL_CONSTANTS_H_
+
+// Added for compatibility.
+#import "ios/chrome/browser/chrome_url_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_URL_CHROME_URL_CONSTANTS_H_
