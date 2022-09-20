f313ff01ec71c15b45ea6f09bc5a0662304b4d34
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-09-20 18:46:18 +0000

    [last-baseline] Propagate last-baseline from tables.
    For more details see:
    https://github.com/w3c/csswg-drafts/issues/7655#issuecomment-1248528627
    
    Effectively we should propagate the last-baseline from the last
    table-row (we ignore caption baselines for example).
    
    If the row is baseline aligned (using vertical-align: baseline) this
    becomes the first & last baseline (until we support
    "align-content: last baseline"), otherwise we use the lowest
    content-edge.
    
    Bug: 885175
    Change-Id: Id9de2f8baff5511fd4da2cef3f056c1bac0fc112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907838
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049240}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
@@ -1035,7 +1035,8 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
       border_padding.inline_start + border_spacing.inline_size;
 
   absl::optional<TableBoxExtent> table_box_extent;
-  absl::optional<LayoutUnit> table_baseline;
+  absl::optional<LayoutUnit> first_baseline;
+  absl::optional<LayoutUnit> last_baseline;
 
   bool has_repeated_header = false;
   absl::optional<LayoutUnit> pending_repeated_footer_block_size;
@@ -1277,9 +1278,13 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
     const auto& physical_fragment =
         To<NGPhysicalBoxFragment>(child_result->PhysicalFragment());
     NGBoxFragment fragment(table_writing_direction, physical_fragment);
-    if (child.IsTableSection() && !table_baseline) {
-      if (const auto& section_baseline = fragment.FirstBaseline())
-        table_baseline = *section_baseline + child_block_offset;
+    if (child.IsTableSection()) {
+      if (!first_baseline) {
+        if (const auto& section_first_baseline = fragment.FirstBaseline())
+          first_baseline = child_block_offset + *section_first_baseline;
+      }
+      if (const auto& section_last_baseline = fragment.LastBaseline())
+        last_baseline = child_block_offset + *section_last_baseline;
     }
 
     container_builder_.AddResult(
@@ -1444,8 +1449,11 @@ const NGLayoutResult* NGTableLayoutAlgorithm::GenerateFragment(
       Node().GetDOMNode()->HasTagName(mathml_names::kMtableTag)) {
     container_builder_.SetBaselines(
         MathTableBaseline(Style(), child_block_offset));
-  } else if (table_baseline) {
-    container_builder_.SetFirstBaseline(*table_baseline);
+  } else {
+    if (first_baseline)
+      container_builder_.SetFirstBaseline(*first_baseline);
+    if (last_baseline)
+      container_builder_.SetLastBaseline(*last_baseline);
   }
 
   container_builder_.SetIsTableNGPart();
