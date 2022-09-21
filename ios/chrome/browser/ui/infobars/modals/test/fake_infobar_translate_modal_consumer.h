dcbebc6c14109d1110ac7268f2efe59eb0794541
Sylvain Defresne <sdefresne@chromium.org>
2022-09-21 17:57:12 +0000

    [ios] Fix method reference in comment
    Objective-C method are referenced as -translateWithNewLanguages
    not translateWithNewLanguages().
    
    Bug: none
    Change-Id: Ie92d5280b8913c40c86fa6aca5bca1074232e175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3909496
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049736}

--- ios/chrome/browser/ui/infobars/modals/test/fake_infobar_translate_modal_consumer.h
+++ ios/chrome/browser/ui/infobars/modals/test/fake_infobar_translate_modal_consumer.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign) BOOL shouldDisplayNeverTranslateSiteButton;
 
 // YES if the pref is set to configure the Translate button to trigger
-// translateWithNewLanguages().
+// -translateWithNewLanguages.
 @property(nonatomic, assign) BOOL updateLanguageBeforeTranslate;
 
 // YES if the pref is set to always translate for the source language.
