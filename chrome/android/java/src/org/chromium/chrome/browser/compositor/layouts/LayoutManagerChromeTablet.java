b31f3554ec79b6a94ce6cd8ae0ce1aa9d7b05951
Neil Coronado <nemco@google.com>
2022-09-21 23:33:54 +0000

    [TabStrip] Prevent favicon cache from clearing unnecessarily
    Prevent favicon cache from clearing unnecessarily.
    -Remove #allTabClosureCommitted impl in StripLayoutHelperManager, since
     when this is called, not all tabs are removed. Instead, only the tabs
     that are pending closure are removed. These are cleared from the cache
     in #tabClosureCommitted.
    -Remove #emptyCachesExcept impl in LayoutManagerChromeTablet, since this
     is only called when we are returning to the static layout. The static
     layout is the only one where the tab strip is visible, meaning we don't
     want to clear the cached titles when returning to it.
    
    This prevents us from having to re-request favicons when they're already
    available. Additionally, this prevents the favicons of previously
    unloaded pages from 1. "flickering" during reorder or 2. disappearing
    when exiting the GTS.
    
    Before: https://drive.google.com/file/d/1KSpM8O7G5z1FoAqdzj98OQJ43SGSaldE/view?usp=sharing
    After: https://drive.google.com/file/d/1qEk6VDjyprrOHOyqX8Mw_Afwo6ou3nmN/view?usp=sharing
    
    Bug: 1329293
    Change-Id: I641f3be21a29f2cc6aab2a70378fcf34cfd7e179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869198
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049936}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChromeTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChromeTablet.java
@@ -49,6 +49,9 @@ public class LayoutManagerChromeTablet extends LayoutManagerChrome {
 
     // Internal State
     /** A {@link TitleCache} instance that stores all title/favicon bitmaps as CC resources. */
+    // This cache should not be cleared in LayoutManagerImpl#emptyCachesExcept(), since that method
+    // is currently called when returning to the static layout, which is when these titles will be
+    // visible. See https://crbug.com/1329293.
     protected LayerTitleCache mLayerTitleCache;
 
     private final Supplier<StartSurface> mStartSurfaceSupplier;
@@ -171,12 +174,6 @@ public class LayoutManagerChromeTablet extends LayoutManagerChrome {
             }
         }
         super.showLayout(layoutType, animate);
-    }
-
-    @Override
-    protected void emptyCachesExcept(int tabId) {
-        super.emptyCachesExcept(tabId);
-        if (mLayerTitleCache != null) mLayerTitleCache.clearExcept(tabId);
     }
 
     @Override
