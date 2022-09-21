e60366d264d8eee9f884a9ab8fc8fda0efb87bb1
Lingqi Chi <lingqi@chromium.org>
2022-09-21 02:58:51 +0000

    Prerender: Record kStartFailed when Prerender fails to start
    PrerenderHost::kDestroyed is used in many places, covering various of
    cancellation reasons, which makes it difficult to understand these
    prerender failures.
    
    This CL converts one of its usage to kStartFailed.
    
    Bug: 1363550
    Change-Id: Id33801eb0c19374e98aeb9e89c69d28c00e2f716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899231
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049424}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -1479,6 +1479,8 @@ Page::PrerenderFinalStatus PrerenderFinalStatusToProtocol(
       return Page::PrerenderFinalStatusEnum::ActivatedBeforeStarted;
     case PrerenderHost::FinalStatus::kInactivePageRestriction:
       return Page::PrerenderFinalStatusEnum::InactivePageRestriction;
+    case PrerenderHost::FinalStatus::kStartFailed:
+      return Page::PrerenderFinalStatusEnum::StartFailed;
   }
 }
 
