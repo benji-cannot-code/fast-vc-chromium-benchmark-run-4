7de9a7ccca242ae1579fc707d813dee4945d814f
Gauthier Ambard <gambard@chromium.org>
2022-09-21 13:53:49 +0000

    [iOS] Fix Sync screen symbols
    Fix the weight of the accessories and the symbol of the sync error.
    
    Flag disabled:
    https://screenshot.googleplex.com/BbQUresR7mm8cTg
    https://screenshot.googleplex.com/3ENcS5LPwuruPJz
    Flag enabled:
    https://screenshot.googleplex.com/9s3jgytPszN96Lo
    https://screenshot.googleplex.com/AgoJrHMdxW4tpdA
    
    Bug: 1315544
    Change-Id: Ida78c5d50f6bd5c8c7d50be2ce9b639b0daee0e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904624
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049619}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sync/sync_setup_service.h"
 #import "ios/chrome/browser/ui/icons/chrome_symbol.h"
 #import "ios/chrome/browser/ui/icons/item_icon.h"
+#import "ios/chrome/browser/ui/icons/settings_icon.h"
 #import "ios/chrome/browser/ui/list_model/list_model.h"
-#import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_item.h"
 #import "ios/chrome/browser/ui/settings/cells/sync_switch_item.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_constants.h"
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/sync_error_settings_command_handler.h"
 #import "ios/chrome/browser/ui/settings/sync/utils/sync_util.h"
 #import "ios/chrome/browser/ui/settings/utils/pref_backed_boolean.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_image_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_info_button_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_item.h"
@@ -49,6 +50,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using l10n_util::GetNSString;
 
 namespace {
+
+// Returns the configuration to be used for the accessory.
+UIImageConfiguration* AccessoryConfiguration() {
+  return [UIImageSymbolConfiguration
+      configurationWithPointSize:kSymbolAccessoryPointSize
+                          weight:UIImageSymbolWeightRegular
+                           scale:UIImageSymbolScaleMedium];
+}
 
 // Enterprise icon.
 NSString* const kGoogleServicesEnterpriseImage = @"google_services_enterprise";
@@ -294,8 +303,8 @@ const std::map<SyncSetupService::SyncableDatatype, const char*>
       SyncSetupService::kSyncServiceNeedsTrustedVaultKey;
   if (hasDisclosureIndicator) {
     self.encryptionItem.accessoryView = [[UIImageView alloc]
-        initWithImage:DefaultSymbolTemplateWithPointSize(
-                          kChevronForwardSymbol, kSymbolAccessoryPointSize)];
+        initWithImage:DefaultSymbolWithConfiguration(kChevronForwardSymbol,
+                                                     AccessoryConfiguration())];
     self.encryptionItem.accessoryView.tintColor =
         [UIColor colorNamed:kTextQuaternaryColor];
   } else {
@@ -310,8 +319,8 @@ const std::map<SyncSetupService::SyncableDatatype, const char*>
   TableViewImageItem* googleActivityControlsItem =
       [[TableViewImageItem alloc] initWithType:GoogleActivityControlsItemType];
   googleActivityControlsItem.accessoryView = [[UIImageView alloc]
-      initWithImage:DefaultSymbolTemplateWithPointSize(
-                        kExternalLinkSmbol, kSymbolAccessoryPointSize)];
+      initWithImage:DefaultSymbolWithConfiguration(kExternalLinkSmbol,
+                                                   AccessoryConfiguration())];
   googleActivityControlsItem.accessoryView.tintColor =
       [UIColor colorNamed:kTextQuaternaryColor];
   googleActivityControlsItem.title =
@@ -326,8 +335,8 @@ const std::map<SyncSetupService::SyncableDatatype, const char*>
   TableViewImageItem* dataFromChromeSyncItem =
       [[TableViewImageItem alloc] initWithType:DataFromChromeSync];
   dataFromChromeSyncItem.accessoryView = [[UIImageView alloc]
-      initWithImage:DefaultSymbolTemplateWithPointSize(
-                        kExternalLinkSmbol, kSymbolAccessoryPointSize)];
+      initWithImage:DefaultSymbolWithConfiguration(kExternalLinkSmbol,
+                                                   AccessoryConfiguration())];
   dataFromChromeSyncItem.accessoryView.tintColor =
       [UIColor colorNamed:kTextQuaternaryColor];
   dataFromChromeSyncItem.title =
@@ -703,8 +712,9 @@ const std::map<SyncSetupService::SyncableDatatype, const char*>
          (itemType == SyncNeedsTrustedVaultKeyErrorItemType) ||
          (itemType == SyncTrustedVaultRecoverabilityDegradedErrorItemType))
       << "itemType: " << itemType;
-  SettingsImageDetailTextItem* syncErrorItem =
-      [[SettingsImageDetailTextItem alloc] initWithType:itemType];
+  TableViewDetailIconItem* syncErrorItem =
+      [[TableViewDetailIconItem alloc] initWithType:itemType];
+  syncErrorItem.textLayoutConstraintAxis = UILayoutConstraintAxisVertical;
   syncErrorItem.text = GetNSString(IDS_IOS_SYNC_ERROR_TITLE);
   syncErrorItem.detailText =
       GetSyncErrorDescriptionForSyncSetupService(self.syncSetupService);
@@ -737,7 +747,15 @@ const std::map<SyncSetupService::SyncableDatatype, const char*>
       syncErrorItem.text = GetNSString(IDS_SYNC_NEEDS_VERIFICATION_TITLE);
       break;
   }
-  syncErrorItem.image = [UIImage imageNamed:kGoogleServicesSyncErrorImage];
+  if (UseSymbols()) {
+    syncErrorItem.iconImage = DefaultSettingsRootSymbol(kSyncErrorSymbol);
+    syncErrorItem.iconBackgroundColor = [UIColor colorNamed:kRed500Color];
+    syncErrorItem.iconTintColor = UIColor.whiteColor;
+    syncErrorItem.iconCornerRadius = kColorfulBackgroundSymbolCornerRadius;
+  } else {
+    syncErrorItem.iconImage =
+        [UIImage imageNamed:kGoogleServicesSyncErrorImage];
+  }
   return syncErrorItem;
 }
 
