a363e5cd2a7ecea2e942dcf317a451caa29e81f3
Jun Kokatsu <jkokatsu@google.com>
2022-09-21 16:08:19 +0000

    Add UseCounter for Data URL in SVG use element
    Measure usage of Data URL in SVG use element for potential deprecation.
    
    Bug: 1300195
    Change-Id: I7a1b6a474d14ec19e96457a53c55ff03c6329fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908236
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Jun Kokatsu <jkokatsu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049668}

--- third_party/blink/renderer/core/svg/svg_use_element.cc
+++ third_party/blink/renderer/core/svg/svg_use_element.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/xlink_names.h"
 #include "third_party/blink/renderer/core/xml/parser/xml_document_parser.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_loader_options.h"
@@ -206,6 +207,9 @@ void SVGUseElement::UpdateTargetReference() {
                                 element_url_, document_content_->Url()))) {
     return;
   }
+
+  if (element_url_.ProtocolIsData())
+    UseCounter::Count(GetDocument(), WebFeature::kDataUrlInSvgUse);
 
   auto* context_document = &GetDocument();
   ExecutionContext* execution_context = context_document->GetExecutionContext();
