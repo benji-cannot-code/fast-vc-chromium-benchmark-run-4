e5d6cf6dfed148d7d165dbafe504c013f75a7fc4
Nick Harper <nharper@chromium.org>
2022-09-21 21:40:25 +0000

    Update root_store_tool to handle merged data source
    The contents of ev_roots have been merged into chrome_root_store, so
    the ev_roots files should be removed and references updated.
    
    Bug: b/243954880
    Change-Id: I6a2f2ea8f8e1fece4bbc4d81bad9c420314e4a99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907688
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049882}

--- net/cert/internal/system_trust_store_unittest.cc
+++ net/cert/internal/system_trust_store_unittest.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 #if BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
-#include "net/data/ssl/chrome_root_store/chrome-root-store-test-data-inc.cc"
+#include "net/data/ssl/chrome_root_store/chrome-root-store-test-data-inc.cc"  // nogncheck
 
 TEST(SystemTrustStoreChrome, SystemDistrustOverridesChromeTrust) {
   CertificateList certs = CreateCertificateListFromFile(
