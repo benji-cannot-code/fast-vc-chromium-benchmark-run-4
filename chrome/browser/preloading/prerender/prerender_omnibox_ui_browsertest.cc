d4c7a7fd750e7cc19b43e21272ce39280c3d1bc6
Simon Pelchat <spelchat@chromium.org>
2022-09-21 23:55:34 +0000

    Namespace failure reason enums based on //content vs //chrome.
    Prerender DSE's failure reason uses PrerenderHost::FinalStatus for
    //content code but PrerenderPredictionStatus for //chrome code. To
    distinguish these two enums, we use a different offset for //content
    enums vs //chrome enums.
    
    Note that this changes the value of logged FailureReason enum. We are
    the only users of those logs for now so we can easily deal with the
    change of enum values.
    
    Bug: 1366049
    Change-Id: Ic46b91b8153dd982ab227f9fcf5b9002816927b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906499
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049946}

--- chrome/browser/preloading/prerender/prerender_omnibox_ui_browsertest.cc
+++ chrome/browser/preloading/prerender/prerender_omnibox_ui_browsertest.cc
@@ -68,8 +68,8 @@ content::PreloadingFailureReason ToPreloadingFailureReason(
     PrerenderPredictionStatus status) {
   return static_cast<content::PreloadingFailureReason>(
       static_cast<int>(status) +
-      static_cast<int>(
-          content::PreloadingFailureReason::kPreloadingFailureReasonCommonEnd));
+      static_cast<int>(content::PreloadingFailureReason::
+                           kPreloadingFailureReasonContentEnd));
 }
 
 class AutocompleteActionPredictorObserverImpl
