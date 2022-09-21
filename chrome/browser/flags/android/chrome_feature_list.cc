bd4ebbcad781bc6b5902f64deb0745591769b8f4
Matthias Körber <koerber@google.com>
2022-09-21 15:53:28 +0000

    Remove the feature flag for structured addresses
    This CL removes the feature flag and cleans up code that is only
    relevant in the disabled branch of the feature.
    
    Fixed: 1130194
    Change-Id: I37bed7983632e6da5388c3c06aab2c66ce587e63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907734
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049666}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -95,7 +95,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillManualFallbackAndroid,
     &autofill::features::kAutofillRefreshStyleAndroid,
     &autofill::features::kAutofillEnableSupportForHonorificPrefixes,
-    &autofill::features::kAutofillEnableSupportForMoreStructureInAddresses,
     &autofill::features::kAutofillEnableUpdateVirtualCardEnrollment,
     &autofill::features::kAutofillEnableVirtualCardMetadata,
     &blink::features::kForceWebContentsDarkMode,
