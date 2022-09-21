b2fb5d1c28dc48df67191c3951a33e08939dc177
Kent Tamura <tkent@chromium.org>
2022-09-21 09:34:36 +0000

    LayoutNG: Fix scroll offset clamping in vertical-rl and RTL
    When a vertical scrollbar in vertical-rl writing-mode or RTL direction
    disappeared by updating layout, scroll offset could be wrong by scroll
    offset clamping.  This CL fixes it.
    
    A LayoutNG block should delay ClampScrollOffsetAfterOverflowChange()
    calls until it determines the final state of scrollbars existence. So
    we add a DelayScrollOffsetClampScope to NGBlockNode::Layout().
    
    We should not delay ClampScrollOffsetAfterOverflowChange() called
    by functions other than PaintLayerScrollableArea::UpdateAfterLayout().
    This CL removes the delay capability of ClampScrollOffsetAfterOverflowChange(), and UpdateAfterLayout() calls
    another delay-capable version of ClampScrollOffsetAfterOverflowChange().
    
    The new behavior is guarded by a runtime flag, and it works as a kill
    switch just in case.
    
    This CL fixes PrintBrowserTest.NoScrollingVerticalRl in browser_tests
    with LayoutNGView and LayoutNGPrinting flags enabled.
    
    Bug: 1294155, 1366268
    Change-Id: I9bb8ba80d09adea11dd95c538233720dd06a05e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891818
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049546}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h
@@ -247,7 +247,7 @@ class CORE_EXPORT PaintLayerScrollableArea final
     absl::optional<FreezeScrollbarsScope> freezer_;
   };
 
-  // If a DelayScrollOffsetClampScope object is alive, updateAfterLayout() will
+  // If a DelayScrollOffsetClampScope object is alive, UpdateAfterLayout() will
   // not clamp scroll offsets to ensure they are in the valid range.  When the
   // last DelayScrollOffsetClampScope object is destructed, all
   // PaintLayerScrollableArea's that delayed clamping their offsets will
@@ -382,6 +382,8 @@ class CORE_EXPORT PaintLayerScrollableArea final
   // Currently, they run at the end of box()'es layout (or after all flexbox
   // layout has finished) but while document layout is still happening.
   void UpdateAfterLayout();
+
+  // This function is not affected by DelayScrollOffsetClampScope.
   void ClampScrollOffsetAfterOverflowChange();
 
   void DidChangeGlobalRootScroller() override;
@@ -491,7 +493,7 @@ class CORE_EXPORT PaintLayerScrollableArea final
   scoped_refptr<base::SingleThreadTaskRunner> GetTimerTaskRunner() const final;
 
   // Did DelayScrollOffsetClampScope prevent us from running
-  // clampScrollOffsetsAfterLayout() in updateAfterLayout()?
+  // DelayableClampScrollOffsetsAfterLayout() in UpdateAfterLayout()?
   bool NeedsScrollOffsetClamp() const { return needs_scroll_offset_clamp_; }
   void SetNeedsScrollOffsetClamp(bool val) { needs_scroll_offset_clamp_ = val; }
 
@@ -715,6 +717,9 @@ class CORE_EXPORT PaintLayerScrollableArea final
       bool& previously_was_overlay,
       bool& previously_was_directly_composited,
       gfx::Rect& visual_rect);
+
+  void DelayableClampScrollOffsetAfterOverflowChange();
+  void ClampScrollOffsetAfterOverflowChangeInternal();
 
   // PaintLayer is destructed before PaintLayerScrollable area, during this
   // time before PaintLayerScrollableArea has been collected layer_ will
