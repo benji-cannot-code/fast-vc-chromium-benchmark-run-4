ba26d87f201c819e9fad2a91413f5f209b8939c9
David Bertoni <dbertoni@chromium.org>
2022-09-21 21:47:21 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /components/cdm/common.
    This CL also converts to the new "for" loop syntax in a few files.
    
    This CL was uploaded by git cl split.
    
    R=jrummell@chromium.org
    
    Bug: 1303949
    Change-Id: I5015bec1113beadb541f71ae98dbb2d0fbb49874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906500
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049885}

--- components/cdm/common/cdm_manifest.cc
+++ components/cdm/common/cdm_manifest.cc
@@ -149,7 +149,7 @@ bool GetEncryptionSchemes(
   }
 
   base::flat_set<media::EncryptionScheme> result;
-  for (const auto& item : value->GetListDeprecated()) {
+  for (const auto& item : value->GetList()) {
     if (!item.is_string()) {
       DLOG(ERROR) << "Unrecognized item type in CDM manifest entry "
                   << kCdmSupportedEncryptionSchemesName;
