71c0fffbdb8e23cdf795c4cad0b82be0fc713096
Clark DuVall <cduvall@chromium.org>
2022-09-21 23:15:08 +0000

    Add feature to use mmap for language detection models on posix
    While looking at some heap dumps, I saw the tflite model for language
    detection was taking ~400kb per renderer on Android. This CL switches
    from reading the file into a std::string to using mmap for the model
    file, and will hopefully save memory. This change is behind a
    base::Feature so we can measure using Finch.
    
    This rolls the third_party/tflite_support lib and removes one of the
    chromium specific patches that removed the mmap code now that the
    upstream version builds on windows.
    
    Bug: 1366096
    Change-Id: I07ef0edd6974133bef905dbf83d52f170fe292c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907043
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049926}

--- components/translate/core/common/translate_util.h
+++ components/translate/core/common/translate_util.h
@@ -35,6 +35,11 @@ extern const base::FeatureParam<int>
 // is shown.
 extern const base::FeatureParam<int> kDesktopPartialTranslateBubbleShowDelayMs;
 
+#if !BUILDFLAG(IS_WIN)
+// Controls whether mmap is used to load the language detection model.
+extern const base::Feature kMmapLanguageDetectionModel;
+#endif
+
 // Isolated world sets following security-origin by default.
 extern const char kSecurityOrigin[];
 
