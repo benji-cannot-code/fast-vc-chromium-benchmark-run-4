4784424b522c642f22175fb974d5648c27a7dc3d
Roman Arora <romanarora@chromium.org>
2022-09-21 14:31:32 +0000

    [NTP] Use color pipeline for NTP attribution and MV elements
    Introduce color mixer/recipes for colors used in the NTP for the
    attribution and MV tile elements. Currently these colors will be
    overridden by the new_tab_page_handler in the case a custom background
    is present. However, in the near future (see crbug.com/1365972), we will
    move such override logic to the new_tab_page_color_mixer.
    
    Bug: 1346392
    Change-Id: If4bf4d308e95f25e8e25b2633d89d51f9b432906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906535
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049633}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -136,6 +136,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageActionButtonBorder) \
   E_CPONLY(kColorNewTabPageActionButtonBorderHovered) \
   E_CPONLY(kColorNewTabPageActiveBackground) \
+  E_CPONLY(kColorNewTabPageAttributionForeground) \
   E_CPONLY(kColorNewTabPageBackground) \
   E_CPONLY(kColorNewTabPageBackgroundOverride) \
   E_CPONLY(kColorNewTabPageBorder) \
@@ -153,6 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageLogoUnthemedLight) \
   E_CPONLY(kColorNewTabPageMicBorderColor) \
   E_CPONLY(kColorNewTabPageMicIconColor) \
+  E_CPONLY(kColorNewTabPageMostVisitedForeground) \
   E_CPONLY(kColorNewTabPageMostVisitedTileBackground) \
   E_CPONLY(kColorNewTabPageMostVisitedTileBackgroundUnthemed) \
   E_CPONLY(kColorNewTabPageModuleIconContainerBackground) \
--- chrome/browser/ui/color/new_tab_page_color_mixer.cc
+++ chrome/browser/ui/color/new_tab_page_color_mixer.cc
@@ -54,6 +54,8 @@ void AddNewTabPageColorMixer(ui::ColorProvider* provider,
   mixer[kColorNewTabPageLogoUnthemedDark] = {gfx::kGoogleGrey700};
   mixer[kColorNewTabPageLogoUnthemedLight] = {SkColorSetRGB(0xEE, 0xEE, 0xEE)};
 
+  mixer[kColorNewTabPageMostVisitedForeground] = {
+      dark_mode ? SK_ColorWHITE : gfx::kGoogleGrey800};
   if (dark_mode) {
     mixer[kColorNewTabPageMostVisitedTileBackground] = {gfx::kGoogleGrey900};
   } else {
@@ -153,6 +155,8 @@ void AddNewTabPageColorMixer(ui::ColorProvider* provider,
 }
 
 void AddWebThemeNewTabPageColors(ui::ColorMixer& mixer, bool dark_mode) {
+  mixer[kColorNewTabPageAttributionForeground] = {
+      dark_mode ? SK_ColorWHITE : gfx::kGoogleGrey800};
   mixer[kColorNewTabPageActionButtonBorder] = {dark_mode ? gfx::kGoogleGrey700
                                                          : gfx::kGoogleGrey300};
   mixer[kColorNewTabPageActionButtonBorderHovered] = {
