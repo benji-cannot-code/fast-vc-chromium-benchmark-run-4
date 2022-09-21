2b218eae631bc1077f421177297ec47cabf75e6e
Eva Herencsarova <evih@google.com>
2022-09-21 13:57:10 +0000

    [Autofill] Introduce new sectioning algorithm
    This CL introduces a basic sectioning algorithm that assigns sections to the fields in a form in the following way:
      1. Every field with an autocomplete attribute section S is in section S.
      2. All credit card fields are in one, distinct section, unless they have a valid autocomplete attribute section.
      3. All other fields that are focusable or <select> fields are partitioned into intervals, each of which is a section.
      4. All remaining fields are in one, distinct section..
    
    Bug: 1153539
    Change-Id: I360d697776cc58893db02a5287b1e79ac990c76a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902306
    Commit-Queue: Eva Herencsarova <evih@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049621}

--- components/autofill/core/browser/autofill_merge_unittest.cc
+++ components/autofill/core/browser/autofill_merge_unittest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_profile_comparator.h"
 #include "components/autofill/core/browser/form_data_importer.h"
 #include "components/autofill/core/browser/form_structure.h"
-#include "components/autofill/core/browser/form_structure_test_api.h"
 #include "components/autofill/core/browser/geo/country_names.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/test_autofill_client.h"
@@ -163,10 +162,6 @@ std::vector<AutofillProfile*> PersonalDataManagerMock::GetProfiles() const {
   return result;
 }
 
-FormStructureTestApi test_api(FormStructure* form_structure) {
-  return FormStructureTestApi(form_structure);
-}
-
 }  // namespace
 
 // A data-driven test for verifying merging of Autofill profiles. Each input is
@@ -291,7 +286,6 @@ void AutofillMergeTest::MergeProfiles(const std::string& profiles,
             StringToFieldType(base::UTF16ToUTF8(field->name));
         field->set_heuristic_type(GetActivePatternSource(), type);
       }
-      test_api(&form_structure).IdentifySections(false);
 
       // Import the profile.
       FormDataImporter::ImportFormDataResult imported_data;
--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_processing/label_processing_util.h"
 #include "components/autofill/core/browser/form_processing/name_processing_util.h"
 #include "components/autofill/core/browser/form_structure_rationalizer.h"
+#include "components/autofill/core/browser/form_structure_sectioning_util.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/shadow_prediction_metrics.h"
@@ -1602,6 +1603,12 @@ void FormStructure::IdentifySections(bool ignore_autocomplete) {
 
   if (base::FeatureList::IsEnabled(features::kAutofillUseNewSectioningMethod)) {
     IdentifySectionsWithNewMethod();
+    return;
+  }
+
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillUseParameterizedSectioning)) {
+    AssignSections(fields_);
     return;
   }
 
