1282804316d9580128a6e081e3e8a771e29568e3
Han Gao <hangaoy@google.com>
2022-09-21 21:30:01 +0000

    Add keypress Event Timing Test
    Added two test files to cover the keyup and keydown event, and also
    added a handler to test utils to send a key in response to
    having keyup and keydown events.
    
    Bug: 1255095
    Change-Id: I2a1cbf036c782973d52a651743f095af2fd8222c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891891
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Han Gao <hangaoy@google.com>
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049874}

--- third_party/blink/web_tests/external/wpt/event-timing/resources/event-timing-test-utils.js
+++ third_party/blink/web_tests/external/wpt/event-timing/resources/event-timing-test-utils.js
@@ -169,6 +169,13 @@ function applyAction(eventType, target) {
       || eventType === 'pointerleave' || eventType === 'pointerout') {
     actions.pointerMove(0, 0, {origin: target})
     .pointerMove(0, 0);
+  } else if (eventType === 'keyup' || eventType === 'keydown') {
+    // Any key here as an input should work.
+    // TODO: Switch this to use test_driver.Actions.key{up,down}
+    // when test driver supports it.
+    // Please check crbug.com/893480.
+    const key = 'k';
+    return test_driver.send_keys(target, key);
   } else {
     assert_unreached('The event type ' + eventType + ' is not supported.');
   }
@@ -183,7 +190,9 @@ function requiresListener(eventType) {
           'pointerleave',
           'pointerout',
           'pointerover',
-          'pointerup'
+          'pointerup',
+          'keyup',
+          'keydown'
         ].includes(eventType);
 }
 
