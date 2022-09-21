14997487ed5174b9cdbbc0ef58a6bc5904b1601f
Joe Mason <joenotcharles@google.com>
2022-09-21 12:33:28 +0000

    [iOS] Dump without crashing when failing to get memory footprint
    This validates the theory that a memory regression detected by heap
    profiling but not visible in Memory.Browser.MemoryFootprint is because
    the task_info is returning an error so the MemoryFootprint is never
    logged.
    
    R=olivierrobin
    
    Bug: 1365392
    Change-Id: I9b886df27901893d04d1f06f5fe54842a24cc124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905125
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049596}

--- ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
+++ ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/callback.h"
 #import "base/check.h"
 #import "base/command_line.h"
+#import "base/debug/dump_without_crashing.h"
 #import "base/files/file_path.h"
 #import "base/files/file_util.h"
 #import "base/metrics/histogram_functions.h"
@@ -25,8 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/path_service.h"
 #import "base/process/process_metrics.h"
 #import "base/rand_util.h"
+#import "base/strings/safe_sprintf.h"
 #import "base/task/thread_pool.h"
 #import "base/threading/platform_thread.h"
+#import "components/crash/core/common/crash_key.h"
 #import "components/crash/core/common/crash_keys.h"
 #import "components/history/core/browser/history_service.h"
 #import "components/keyed_service/core/service_access_type.h"
@@ -412,6 +415,17 @@ void IOSChromeMetricsServiceClient::CollectFinalHistograms() {
             "Memory.Browser.MemoryFootprint.Background", footprint_mb);
         break;
     }
+  } else {
+    // Max kern_return_t is 0x100 = 256, plus trailing null.
+    // (https://opensource.apple.com/source/xnu/xnu-792.25.20/osfmk/mach/kern_return.h)
+    // TODO(crbug.com/1365392): Remove this when done debugging the uncaught
+    // memory regression.
+    static crash_reporter::CrashKeyString<4> task_info_kern_return(
+        "task-info-kern-return");
+    char kr_buf[4];
+    base::strings::SafeSPrintf(kr_buf, "%d", kr);
+    task_info_kern_return.Set(kr_buf);
+    base::debug::DumpWithoutCrashing();
   }
 
   std::vector<ChromeBrowserState*> loaded_browser_states =
