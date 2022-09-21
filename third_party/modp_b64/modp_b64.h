da5b3197370f516f4dea5986f2ce414ae50d59c1
David Benjamin <davidben@chromium.org>
2022-09-21 19:39:12 +0000

    Handle overflow in Base64Encode
    Base64Encode makes inputs larger, so it possible that the output length
    does not fit in size_t. Guard against this and CHECK fail if the size is
    too large. (This is probably impossible on 64-bit because the largest
    possible allocation is well below 2^64, but 32-bit's size_t is much
    tighter.)
    
    Bug: none
    Change-Id: Ia3e292bec9a438824b1ab2b75e5de4ec976ffc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3909830
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049798}

--- third_party/modp_b64/modp_b64.h
+++ third_party/modp_b64/modp_b64.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MODP_B64
 #define MODP_B64
 
+#include <limits.h>
 #include <stddef.h>
 
 #ifdef __cplusplus
@@ -82,6 +83,19 @@ size_t modp_b64_encode(char* dest, const char* str, size_t len);
 size_t modp_b64_decode(char* dest, const char* src, size_t len);
 
 /**
+ * The maximum input that can be passed into modp_b64_encode. Lengths beyond
+ * this will overflow modp_b64_encode_len.
+ *
+ * This works because modp_b64_encode_len(A) computes:
+ *     ceiling[max_len / 3] * 4 + 1
+ *   = ceiling[floor[(SIZE_MAX-1)/4]*3 / 3] * 4 + 1
+ *   = floor[(SIZE_MAX-1)/4] * 4 + 1
+ *  <= SIZE_MAX-1 + 1
+ *   = SIZE_MAX
+ */
+#define MODP_B64_MAX_INPUT_LEN ((SIZE_MAX - 1) / 4 * 3)
+
+/**
  * Given a source string of length len, this returns the amount of
  * memory the destination string should have.
  *
@@ -90,6 +104,9 @@ size_t modp_b64_decode(char* dest, const char* src, size_t len);
  * ceiling[len / 3] * 4 + 1
  *
  * +1 is for any extra null.
+ *
+ * WARNING: This expression will overflow if the A is above
+ * MODP_B64_MAX_INPUT_LEN. The caller must check this bound first.
  */
 #define modp_b64_encode_len(A) ((A+2)/3 * 4 + 1)
 
