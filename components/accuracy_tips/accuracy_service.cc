53f5ec5bd7116f3d783a73feccfb07c0b33b0aed
David Bertoni <dbertoni@chromium.org>
2022-09-21 10:01:26 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /components/accuracy_tips.
    This CL also converts to the new "for" loop syntax in a few files.
    
    This CL was uploaded by git cl split.
    
    R=olesiamarukhno@google.com
    
    Bug: 1303949
    Change-Id: Iea9ac22f877aa95b495b1bcca3a06617fa8611c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908795
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049555}

--- components/accuracy_tips/accuracy_service.cc
+++ components/accuracy_tips/accuracy_service.cc
@@ -244,7 +244,7 @@ void AccuracyService::OnAccuracyTipClosed(base::TimeTicks time_opened,
   base::UmaHistogramEnumeration("Privacy.AccuracyTip.AccuracyTipInteraction",
                                 interaction);
   base::UmaHistogramCounts100("Privacy.AccuracyTip.NumDialogsShown",
-                              interaction_list->GetListDeprecated().size());
+                              interaction_list->GetList().size());
   ukm::builders::AccuracyTipDialog ukm_builder(ukm_source_id);
   ukm_builder.SetInteraction(static_cast<int>(interaction));
 
@@ -257,7 +257,7 @@ void AccuracyService::OnAccuracyTipClosed(base::TimeTicks time_opened,
 
     const std::string suffix = GetHistogramSuffix(interaction);
     base::UmaHistogramCounts100("Privacy.AccuracyTip.NumDialogsShown." + suffix,
-                                interaction_list->GetListDeprecated().size());
+                                interaction_list->GetList().size());
     base::UmaHistogramMediumTimes(
         "Privacy.AccuracyTip.AccuracyTipTimeOpen." + suffix, time_open);
   }
