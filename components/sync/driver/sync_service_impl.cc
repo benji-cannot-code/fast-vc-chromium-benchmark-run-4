5be20ab6e1bd76bd45e98aeb31f366e0c859c876
Shabdan Batyrkulov <shabdan@google.com>
2022-09-21 12:54:21 +0000

    Stop handling invalidations if sync is stopped or browser is shutdown.
    Added StopListening() method
    
    Bug: 1357603
    Change-Id: Ibe2932a29eb862f607bafa1f410e6c3b9da28673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906051
    Commit-Queue: Shabdan Batyrkulov <shabdan@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049602}

--- components/sync/driver/sync_service_impl.cc
+++ components/sync/driver/sync_service_impl.cc
@@ -512,13 +512,16 @@ void SyncServiceImpl::ResetEngine(ShutdownReason shutdown_reason,
   }
 
   base::UmaHistogramEnumeration("Sync.ResetEngineReason", reset_reason);
+  SyncInvalidationsService* sync_invalidations_service =
+      sync_client_->GetSyncInvalidationsService();
   switch (shutdown_reason) {
     case ShutdownReason::STOP_SYNC_AND_KEEP_DATA:
+      if (sync_invalidations_service) {
+        sync_invalidations_service->StopListening();
+      }
       RemoveClientFromServer();
       break;
     case ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA: {
-      SyncInvalidationsService* sync_invalidations_service =
-          sync_client_->GetSyncInvalidationsService();
       if (sync_invalidations_service) {
         sync_invalidations_service->StopListeningPermanently();
       }
@@ -526,6 +529,9 @@ void SyncServiceImpl::ResetEngine(ShutdownReason shutdown_reason,
       break;
     }
     case ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA:
+      if (sync_invalidations_service) {
+        sync_invalidations_service->StopListening();
+      }
       break;
   }
 
