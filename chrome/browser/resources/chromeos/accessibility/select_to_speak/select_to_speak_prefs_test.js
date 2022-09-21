abc56e5b3edc7af0b4f21274e83f10ebf42f1a68
Rebekah Potter <rbpotter@chromium.org>
2022-09-21 18:03:36 +0000

    WebUI: Move cr.js to ash
    cr.js is only used by a few ash UIs. Removing WebUI communication
    methods, as these aren't used. Also deleting
    webui_resource_async_browsertest.js which tested these methods, and
    moving cr_test for the other methods under chromeos/ash_common test
    folder.
    
    Fixing an issue with the initial land of this change by exporting
    PropertyKind, which is used in arc_support.
    
    Bug: 1347740
    Change-Id: I81f8f2bd546fbb3f19f3c510a744980bdac4204f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902724
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049742}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_prefs_test.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_prefs_test.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 GEN_INCLUDE(['select_to_speak_e2e_test_base.js']);
-GEN_INCLUDE(['../../../../../../ui/webui/resources/js/cr.js']);
 GEN_INCLUDE(['fake_chrome_event.js']);
 GEN_INCLUDE(['fake_settings_private.js']);
 GEN_INCLUDE(['../common/testing/mock_storage.js']);
@@ -21,7 +20,7 @@ SelectToSpeakPrefsTest = class extends SelectToSpeakE2ETest {
 
     const enhancedNetworkVoicesAllowedKey =
         'settings.a11y.enhanced_network_voices_in_select_to_speak_allowed';
-    this.mockSettingsPrivate_ = new settings.FakeSettingsPrivate([
+    this.mockSettingsPrivate_ = new FakeSettingsPrivate([
       {type: 'number', key: 'settings.tts.speech_rate', value: 1.0},
       {type: 'number', key: 'settings.tts.speech_pitch', value: 1.0},
       {type: 'boolean', key: enhancedNetworkVoicesAllowedKey, value: true},
