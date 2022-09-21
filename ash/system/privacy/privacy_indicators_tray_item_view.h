f37f4ea090a0bfb1bbdf18aaac7221e836bd701e
Andre Le <leandre@chromium.org>
2022-09-21 19:55:55 +0000

    PrivacyIndicators: Add privacy indicators view animation
    Add a 3-part animation for the privacy indicators view.
    
    Design doc: go/cros-privacy-indicators-design
    
    Bug: 1363383
    
    Change-Id: Ife11a8e4cb2dd1c75f66747d38f8556fd3c5a672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900633
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049815}

--- ash/system/privacy/privacy_indicators_tray_item_view.h
+++ ash/system/privacy/privacy_indicators_tray_item_view.h
@@ -8,6 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/system/tray/tray_item_view.h"
+#include "base/timer/timer.h"
+#include "ui/compositor/throughput_tracker.h"
+
+namespace gfx {
+class LinearAnimation;
+}
 
 namespace views {
 class BoxLayout;
@@ -20,6 +26,28 @@ class Shelf;
 // is currently accessing camera/microphone.
 class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
  public:
+  enum AnimationState {
+    // No animation is running.
+    kIdle,
+
+    // `expand_animation_` is running.
+    kExpand,
+
+    // `expand_animation_` finishes but the the shrink animation hasn't started
+    // yet. The view will dwell at its expanded size.
+    kDwellInExpand,
+
+    // Happens when `longer_side_shrink_animation_` already started but
+    // `shorter_side_shrink_animation_` hasn't started yet.
+    kOnlyLongerSideShrink,
+
+    // Happens when both the 2 shrink animations are animating. Note that
+    // `longer_side_shrink_animation_` ended before
+    // `shorter_side_shrink_animation_`, and this state ends when
+    // `shorter_side_shrink_animation_` ends.
+    kBothSideShrink,
+  };
+
   explicit PrivacyIndicatorsTrayItemView(Shelf* shelf);
 
   PrivacyIndicatorsTrayItemView(const PrivacyIndicatorsTrayItemView&) = delete;
@@ -38,6 +66,7 @@ class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
   friend class PrivacyIndicatorsTrayItemViewTest;
 
   // TrayItemView:
+  void PerformVisibilityAnimation(bool visible) override;
   void HandleLocaleChange() override;
   gfx::Size CalculatePreferredSize() const override;
   void OnThemeChanged() override;
@@ -45,12 +74,23 @@ class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
   std::u16string GetTooltipText(const gfx::Point& p) const override;
   views::View* GetTooltipHandlerForPoint(const gfx::Point& point) override;
   const char* GetClassName() const override;
+  void AnimationProgressed(const gfx::Animation* animation) override;
+  void AnimationEnded(const gfx::Animation* animation) override;
+  void AnimationCanceled(const gfx::Animation* animation) override;
 
   // Update the icons for the children views.
   void UpdateIcons();
 
   // Update the bounds insets based on shelf alignment.
   void UpdateBoundsInset();
+
+  // Calculate the size of the view during shrink animation. We are calculating
+  // for the longer side if `for_longer_side` is true, otherwise it is for
+  // shorter side.
+  int CalculateSizeDuringShrinkAnimation(bool for_longer_side) const;
+
+  // End all 3 animations contained in this class.
+  void EndAllAnimations();
 
   views::BoxLayout* layout_manager_ = nullptr;
 
@@ -60,6 +100,22 @@ class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
 
   bool camera_is_used_ = false;
   bool microphone_is_used_ = false;
+
+  // Keep track the current animation state during the multi-part animation.
+  AnimationState animation_state_ = kIdle;
+
+  // Animations for showing/expanding the view, then shrink it to be a dot.
+  std::unique_ptr<gfx::LinearAnimation> expand_animation_;
+  std::unique_ptr<gfx::LinearAnimation> longer_side_shrink_animation_;
+  std::unique_ptr<gfx::LinearAnimation> shorter_side_shrink_animation_;
+
+  // Timers for delaying shrink animations after `expand_animation_` is
+  // completed.
+  base::OneShotTimer longer_side_shrink_delay_timer_;
+  base::OneShotTimer shorter_side_shrink_delay_timer_;
+
+  // Measure animation smoothness metrics for all the animations.
+  absl::optional<ui::ThroughputTracker> throughput_tracker_;
 };
 
 }  // namespace ash
