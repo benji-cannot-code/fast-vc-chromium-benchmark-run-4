100bcff768bd40470a7310e89da66da6ce0dc917
Diego Guapo <diegoguapo@google.com>
2022-09-22 00:20:46 +0000

    Update device attributes after Zero Touch Enrollment.
    Allow to use DM token as authorization for devices enrolled with ZTE in update device attributes requests. New device policy will be added, but client changes need to be done before rolling it out.
    
    LOW_COVERAGE_REASON=<Added unit tests for cloud policy client>
    
    Change-Id: I47952602c3217d2adf9bd2b478e21a7a33df8a96
    Bug: b/233085976
    Test: Sent request to DM Server and verify in Analog logs that the server parsed correctly the DM token and responded with status 200 with result as ATTRIBUTE_UPDATE_DISALLOWED since the new setting is not rolled out yet.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445167
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Diego Guapo <diegoguapo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049961}

--- chrome/browser/ash/login/enrollment/enterprise_enrollment_helper_impl.h
+++ chrome/browser/ash/login/enrollment/enterprise_enrollment_helper_impl.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/enrollment/enterprise_enrollment_helper.h"
+#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/policy/enrollment/enrollment_config.h"
+#include "components/policy/core/common/cloud/cloud_policy_client.h"
 #include "components/policy/core/common/cloud/dm_auth.h"
 #include "components/policy/core/common/cloud/enterprise_metrics.h"
 #include "google_apis/gaia/google_service_auth_error.h"
@@ -81,6 +83,11 @@ class EnterpriseEnrollmentHelperImpl : public EnterpriseEnrollmentHelper {
   // Called by ProfileHelper when a signin profile clearance has finished.
   // `callback` is a callback, that was passed to ClearAuth() before.
   void OnSigninProfileCleared(base::OnceClosure callback);
+
+  // Returns either OAuth token or DM token needed for the device attribute
+  // update permission request.
+  absl::optional<policy::DMAuth> GetDMAuthForDeviceAttributeUpdate(
+      policy::CloudPolicyClient* device_cloud_policy_client);
 
   policy::EnrollmentConfig enrollment_config_;
   std::string enrolling_user_domain_;
--- components/policy/core/common/cloud/cloud_policy_client.cc
+++ components/policy/core/common/cloud/cloud_policy_client.cc
@@ -833,11 +833,7 @@ void CloudPolicyClient::GetDeviceAttributeUpdatePermission(
     CloudPolicyClient::StatusCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   CHECK(is_registered());
-  // This request only works with an OAuth token identifying a user, because
-  // DMServer will resolve that user and check if they have permissions to
-  // update the device's attributes.
-  DCHECK(auth.has_oauth_token());
-
+  DCHECK(auth.has_oauth_token() || auth.has_dm_token());
   const bool has_oauth_token = auth.has_oauth_token();
   const std::string oauth_token =
       has_oauth_token ? auth.oauth_token() : std::string();
@@ -847,7 +843,7 @@ void CloudPolicyClient::GetDeviceAttributeUpdatePermission(
               TYPE_ATTRIBUTE_UPDATE_PERMISSION,
           this,
           /*critical=*/false,
-          !has_oauth_token ? std::move(auth) : DMAuth::NoAuth(), oauth_token,
+          !has_oauth_token ? auth.Clone() : DMAuth::NoAuth(), oauth_token,
           base::BindOnce(
               &CloudPolicyClient::OnDeviceAttributeUpdatePermissionCompleted,
               weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
@@ -864,7 +860,7 @@ void CloudPolicyClient::UpdateDeviceAttributes(
     CloudPolicyClient::StatusCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   CHECK(is_registered());
-  DCHECK(auth.has_oauth_token() || auth.has_enrollment_token());
+  DCHECK(auth.has_oauth_token() || auth.has_dm_token());
 
   const bool has_oauth_token = auth.has_oauth_token();
   const std::string oauth_token =
@@ -874,7 +870,8 @@ void CloudPolicyClient::UpdateDeviceAttributes(
           DeviceManagementService::JobConfiguration::TYPE_ATTRIBUTE_UPDATE,
           this,
           /*critical=*/false,
-          !has_oauth_token ? std::move(auth) : DMAuth::NoAuth(), oauth_token,
+          !has_oauth_token ? auth.Clone() : DMAuth::NoAuth(),
+          oauth_token,
           base::BindOnce(&CloudPolicyClient::OnDeviceAttributeUpdated,
                          weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 
@@ -1355,7 +1352,6 @@ void CloudPolicyClient::OnDeviceAttributeUpdatePermissionCompleted(
               ATTRIBUTE_UPDATE_ALLOWED) {
     success = true;
   }
-
   std::move(callback).Run(success);
   RemoveJob(result.job);
 }
