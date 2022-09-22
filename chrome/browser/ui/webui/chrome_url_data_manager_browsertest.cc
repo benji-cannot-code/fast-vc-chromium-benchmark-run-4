89c02322ed55af4c80d27d9be1df4e0e07996813
Toby Huang <tobyhuang@chromium.org>
2022-09-22 02:13:48 +0000

    Projector: Add test for trusted type violations in chrome://projector
    After re-enabling trusted types in crrev.com/c/3810659, this CL adds a
    browser test to detect trusted type violations. It only checks
    ChromeOS Ash because the chrome://projector URL only works on that
    platform. Notably, it will block the nightly Google3 autoroll from
    landing if someone introduces a change that breaks trusted types.
    
    Bug: b:248077680
    Change-Id: If65a85bf925110ee348e9fbba420a21d992e7668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910702
    Auto-Submit: Toby Huang <tobyhuang@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050011}

--- chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
@@ -319,6 +319,7 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://os-credits",
     "chrome://os-settings",
     "chrome://power",
+    "chrome://projector",
     "chrome://proximity-auth/proximity_auth.html",
     "chrome://set-time",
     "chrome://slow",
