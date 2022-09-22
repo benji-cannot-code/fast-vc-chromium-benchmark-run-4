abfdd27e636f16975604871605551977957fa421
Rouslan Solomakhin <rouslan@chromium.org>
2022-09-22 21:25:24 +0000

    [Web Payment] Mojom constant for URL length.
    Bug: 1349091
    Change-Id: I32dec8aaed0828d05182defa56a84f07e9770c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900576
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050396}

--- url/mojom/url_gurl_mojom_traits_unittest.cc
+++ url/mojom/url_gurl_mojom_traits_unittest.cc
@@ -201,4 +201,10 @@ TEST_F(MojoGURLStructTraitsTest, OriginSerialization) {
   EXPECT_FALSE(output.opaque());
 }
 
+// Test that the "kMaxURLChars" values are the same in url.mojom and
+// url_constants.cc.
+TEST_F(MojoGURLStructTraitsTest, TestMaxURLChars) {
+  EXPECT_EQ(kMaxURLChars, mojom::kMaxURLChars);
+}
+
 }  // namespace url
