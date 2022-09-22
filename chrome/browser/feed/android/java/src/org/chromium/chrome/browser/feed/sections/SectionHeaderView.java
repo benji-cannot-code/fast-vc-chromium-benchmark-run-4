0bf8b31c1c05db80cb8237acd0d769cbb93b8418
Cathy Li <chili@chromium.org>
2022-09-22 00:11:14 +0000

    Hook up new -> dot animation to flag.
    Also update the flag parameter so that it's usable, as spaces in field trial params are not supported.
    
    Bug: b/246627786
    Change-Id: Id923bc4ae03b21b5b540a6874dce3c9978d03288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900453
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049954}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderView.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderView.java
@@ -111,6 +111,8 @@ public class SectionHeaderView extends LinearLayout {
         // Null when unread indicator isn't shown.
         @Nullable
         public UnreadIndicator unreadIndicator;
+        // The text to show on the unreadIndicator, if any.
+        public String unreadIndicatorText;
         // The tab's displayed text.
         public String text = "";
     }
@@ -219,12 +221,12 @@ public class SectionHeaderView extends LinearLayout {
      * Set the properties for the header tab at a particular index to text.
      *
      * Does nothing if index is invalid. Make sure to call addTab() beforehand.
-     *
      * @param text Text to set the tab to.
      * @param hasUnreadContent Whether there is unread content.
+     * @param unreadContentText
      * @param index Index of the tab to set.
      */
-    void setHeaderAt(String text, boolean hasUnreadContent, int index) {
+    void setHeaderAt(String text, boolean hasUnreadContent, String unreadContentText, int index) {
         TabLayout.Tab tab = getTabAt(index);
         if (tab == null) {
             return;
@@ -233,6 +235,7 @@ public class SectionHeaderView extends LinearLayout {
 
         state.text = text;
         state.hasUnreadContent = hasUnreadContent;
+        state.unreadIndicatorText = unreadContentText;
         applyTabState(tab);
     }
 
@@ -506,6 +509,7 @@ public class SectionHeaderView extends LinearLayout {
                             new UnreadIndicator(tab.view.findViewById(android.R.id.text1));
                 }
             }
+            state.unreadIndicator.mNewBadge.setText(state.unreadIndicatorText);
         } else {
             if (state.unreadIndicator != null) {
                 state.unreadIndicator.destroy();
