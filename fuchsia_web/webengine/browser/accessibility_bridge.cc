77ba65bb901a92312d4b893b2e62595e82eea232
Peter Kasting <pkasting@chromium.org>
2022-09-22 18:24:10 +0000

    Change std::find_if*() to use base:: functions: fuchsia_web/
    Goal: Slightly simpler and/or more semantically-accurate code.
    
    Bug: none
    Change-Id: Id9fb119305baa271199dfd036534c65845962bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911718
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050296}

--- fuchsia_web/webengine/browser/accessibility_bridge.cc
+++ fuchsia_web/webengine/browser/accessibility_bridge.cc
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/sys/inspect/cpp/component.h>
 #include <lib/ui/scenic/cpp/view_ref_pair.h>
 
-#include <algorithm>
-
 #include "base/callback.h"
+#include "base/containers/adapters.h"
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/process_context.h"
 #include "base/logging.h"
+#include "base/ranges/algorithm.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "fuchsia_web/webengine/browser/frame_window_tree_host.h"
@@ -757,11 +757,8 @@ AccessibilityBridge::GetNodeIfChangingInUpdate(const ui::AXTreeID& tree_id,
 
   for (auto& update_batch : to_update_) {
     auto result =
-        std::find_if(update_batch.rbegin(), update_batch.rend(),
-                     [&fuchsia_node_id](
-                         const fuchsia::accessibility::semantics::Node& node) {
-                       return node.node_id() == fuchsia_node_id;
-                     });
+        base::ranges::find(base::Reversed(update_batch), fuchsia_node_id,
+                           &fuchsia::accessibility::semantics::Node::node_id);
     if (result != update_batch.rend())
       return &(*result);
   }
