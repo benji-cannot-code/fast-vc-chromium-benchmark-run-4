c0f89999becc31f0329c5ed1e68b1793b81309de
Steinar H. Gunderson <sesse@chromium.org>
2022-09-22 12:45:30 +0000

    More comment adjustments in RuleFeatureSet.
    Change-Id: Ia0f1348642dbbd60ef581c80ba7ce4caea14c869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3912059
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050146}

--- third_party/blink/renderer/core/css/rule_feature_set.h
+++ third_party/blink/renderer/core/css/rule_feature_set.h
@@ -45,6 +45,14 @@ class StyleScope;
 // invalidation sets from them and makes them available via several
 // CollectInvalidationSetForFoo methods which use the indices to quickly gather
 // the relevant InvalidationSets for a particular DOM mutation.
+//
+// The name may be somewhat confusing and is for historical reasons.
+// The original “features” extracted from the selectors were the ones
+// in FeatureMetadata (e.g. “does any selector use ::first-line”);
+// invalidation sets were added later. So even though 90% of the code
+// in the class is about invalidation sets, and they are the primary
+// “features” being extracted from the selector now, the file does not
+// live in css/invalidation/. Perhaps this should be changed.
 class CORE_EXPORT RuleFeatureSet {
   DISALLOW_NEW();
 
@@ -56,7 +64,7 @@ class CORE_EXPORT RuleFeatureSet {
   bool operator==(const RuleFeatureSet&) const;
   bool operator!=(const RuleFeatureSet& o) const { return !(*this == o); }
 
-  // Methods for updating the data in this object.
+  // Merge the given RuleFeatureSet (which remains unchanged) into this one.
   void Merge(const RuleFeatureSet&);
   void Clear();
 
@@ -68,7 +76,7 @@ class CORE_EXPORT RuleFeatureSet {
   SelectorPreMatch CollectFeaturesFromSelector(const CSSSelector&,
                                                const StyleScope*);
 
-  // Methods for accessing the data in this object.
+  // Member functions for accessing non-invalidation-set related features.
   bool UsesFirstLineRules() const { return metadata_.uses_first_line_rules; }
   bool UsesWindowInactiveSelector() const {
     return metadata_.uses_window_inactive_selector;
@@ -76,15 +84,12 @@ class CORE_EXPORT RuleFeatureSet {
   bool NeedsFullRecalcForRuleSetInvalidation() const {
     return metadata_.needs_full_recalc_for_rule_set_invalidation;
   }
-
   unsigned MaxDirectAdjacentSelectors() const {
     return metadata_.max_direct_adjacent_selectors;
   }
-
   bool HasSelectorForId(const AtomicString& id_value) const {
     return id_invalidation_sets_.Contains(id_value);
   }
-
   MediaQueryResultFlags& MutableMediaQueryResultFlags() {
     return media_query_result_flags_;
   }
@@ -95,7 +100,12 @@ class CORE_EXPORT RuleFeatureSet {
   bool HasViewportDependentMediaQueries() const;
   bool HasDynamicViewportDependentMediaQueries() const;
 
-  // Collect descendant and sibling invalidation sets.
+  // Collect descendant and sibling invalidation sets, for a given type of
+  // change (e.g. “if this element added or removed the given class, what other
+  // types of elements need to change?”). This is called during DOM mutations.
+  // CollectInvalidationSets* govern self-invalidation and descendant
+  // invalidations, while CollectSiblingInvalidationSets* govern sibling
+  // invalidations.
   void CollectInvalidationSetsForClass(InvalidationLists&,
                                        Element&,
                                        const AtomicString& class_name) const;
@@ -124,6 +134,8 @@ class CORE_EXPORT RuleFeatureSet {
       Element&,
       const QualifiedName& attribute_name,
       unsigned min_direct_adjacent) const;
+
+  // TODO: Document.
   void CollectUniversalSiblingInvalidationSet(
       InvalidationLists&,
       unsigned min_direct_adjacent) const;
@@ -131,6 +143,7 @@ class CORE_EXPORT RuleFeatureSet {
   void CollectPartInvalidationSet(InvalidationLists&) const;
   void CollectTypeRuleInvalidationSet(InvalidationLists&, ContainerNode&) const;
 
+  // Quick tests for whether we need to consider :has() invalidation.
   bool NeedsHasInvalidationForClass(const AtomicString& class_name) const;
   bool NeedsHasInvalidationForAttribute(
       const QualifiedName& attribute_name) const;
