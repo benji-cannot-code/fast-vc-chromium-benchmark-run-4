a43e4320902a74dc4b4242378ed72505edbd724f
Andrew Paseltiner <apaseltiner@chromium.org>
2022-09-22 22:47:44 +0000

    Revert in-memory AttributionStorageSql DB setting on test completion
    Otherwise, the global setting unintentionally alters the behavior of
    subsequent tests.
    
    We also use the empty path to indicate to AttributionStorageSql to use
    an in-memory DB, which is simpler and uses less memory.
    
    This change only affects test behavior, not real browser usage, as
    real usage always creates on-disk DBs.
    
    Bug: 1366478, 1080764
    Change-Id: I7e9384d99f57846a835d61a1961249581cc75d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3909832
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050429}

--- content/browser/attribution_reporting/trigger_registration_browsertest.cc
+++ content/browser/attribution_reporting/trigger_registration_browsertest.cc
@@ -37,9 +37,7 @@ using ::testing::Pointee;
 
 class AttributionTriggerRegistrationBrowserTest : public ContentBrowserTest {
  public:
-  AttributionTriggerRegistrationBrowserTest() {
-    AttributionManagerImpl::RunInMemoryForTesting();
-  }
+  AttributionTriggerRegistrationBrowserTest() = default;
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     // Sets up the blink runtime feature for ConversionMeasurement.
@@ -73,6 +71,8 @@ class AttributionTriggerRegistrationBrowserTest : public ContentBrowserTest {
   }
 
  private:
+  AttributionManagerImpl::ScopedUseInMemoryStorageForTesting
+      attribution_manager_in_memory_setting_;
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
   base::raw_ptr<MockAttributionHost, DanglingUntriaged> mock_attribution_host_;
 };
