eaa30f5002c13fd5af9d75c572bebb2791fdc012
Nikhil Nayunigari <nikhilcn@google.com>
2022-09-22 21:24:16 +0000

    [CrOS Cellular] Emit shill errors as metrics while enabling technologies
    We are seeing an error rate of ~40% in our metrics while enabling
    cellular technology. Having a break down of errors received from
    platform will help in debugging these failures.
    
    Bug: b/244451383
    Change-Id: I27571396e1f23ad1eab2ad4579e798ef726a7e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905741
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050395}

--- chromeos/ash/components/network/metrics/network_metrics_helper.cc
+++ chromeos/ash/components/network/metrics/network_metrics_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
 #include "chromeos/ash/components/network/metrics/connection_results.h"
+#include "chromeos/ash/components/network/network_event_log.h"
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
@@ -31,7 +32,11 @@ const char kDisconnectionsWithoutUserActionSuffix[] =
     ".DisconnectionsWithoutUserAction";
 
 const char kEnableTechnologyResultSuffix[] = ".EnabledState.Enable.Result";
+const char kEnableTechnologyResultCodeSuffix[] =
+    ".EnabledState.Enable.ResultCode";
 const char kDisableTechnologyResultSuffix[] = ".EnabledState.Disable.Result";
+const char kDisableTechnologyResultCodeSuffix[] =
+    ".EnabledState.Disable.ResultCode";
 
 const char kCellular[] = "Cellular";
 const char kCellularESim[] = "Cellular.ESim";
@@ -232,12 +237,32 @@ void NetworkMetricsHelper::LogConnectionStateResult(const std::string& guid,
 
 void NetworkMetricsHelper::LogEnableTechnologyResult(
     const std::string& technology,
-    bool success) {
+    bool success,
+    const absl::optional<std::string>& shill_error) {
   absl::optional<const std::string> suffix =
       GetTechnologyTypeSuffix(technology);
 
   if (!suffix)
     return;
+
+  if (success == shill_error.has_value()) {
+    if (shill_error.has_value()) {
+      NET_LOG(ERROR) << "Error code: " << *shill_error
+                     << " for successful enable operation on: " << technology;
+    } else {
+      NET_LOG(ERROR)
+          << "Missing error code for unsuccessful enable operation on: "
+          << technology;
+    }
+  }
+
+  ShillConnectResult result = shill_error
+                                  ? ShillErrorToConnectResult(*shill_error)
+                                  : ShillConnectResult::kSuccess;
+  base::UmaHistogramEnumeration(
+      base::StrCat(
+          {kNetworkMetricsPrefix, *suffix, kEnableTechnologyResultCodeSuffix}),
+      result);
 
   base::UmaHistogramBoolean(base::StrCat({kNetworkMetricsPrefix, *suffix,
                                           kEnableTechnologyResultSuffix}),
@@ -247,12 +272,32 @@ void NetworkMetricsHelper::LogEnableTechnologyResult(
 // static
 void NetworkMetricsHelper::LogDisableTechnologyResult(
     const std::string& technology,
-    bool success) {
+    bool success,
+    const absl::optional<std::string>& shill_error) {
   absl::optional<const std::string> suffix =
       GetTechnologyTypeSuffix(technology);
 
   if (!suffix)
     return;
+
+  if (success == shill_error.has_value()) {
+    if (shill_error.has_value()) {
+      NET_LOG(ERROR) << "Error code: " << *shill_error
+                     << " for successful disable operation on: " << technology;
+    } else {
+      NET_LOG(ERROR)
+          << "Missing error code for unsuccessful disable operation on: "
+          << technology;
+    }
+  }
+
+  ShillConnectResult result = shill_error
+                                  ? ShillErrorToConnectResult(*shill_error)
+                                  : ShillConnectResult::kSuccess;
+  base::UmaHistogramEnumeration(
+      base::StrCat(
+          {kNetworkMetricsPrefix, *suffix, kDisableTechnologyResultCodeSuffix}),
+      result);
 
   base::UmaHistogramBoolean(base::StrCat({kNetworkMetricsPrefix, *suffix,
                                           kDisableTechnologyResultSuffix}),
--- chromeos/ash/components/network/shill_property_handler.cc
+++ chromeos/ash/components/network/shill_property_handler.cc
@@ -175,7 +175,7 @@ void ShillPropertyHandler::SetTechnologyEnabled(
     shill_manager_->EnableTechnology(
         technology,
         base::BindOnce(&NetworkMetricsHelper::LogEnableTechnologyResult,
-                       technology, /*success=*/true),
+                       technology, /*success=*/true, absl::nullopt),
         base::BindOnce(&ShillPropertyHandler::EnableTechnologyFailed,
                        AsWeakPtr(), technology, std::move(error_callback)));
   } else {
@@ -185,7 +185,7 @@ void ShillPropertyHandler::SetTechnologyEnabled(
     shill_manager_->DisableTechnology(
         technology,
         base::BindOnce(&NetworkMetricsHelper::LogDisableTechnologyResult,
-                       technology, /*success=*/true),
+                       technology, /*success=*/true, absl::nullopt),
         base::BindOnce(&ShillPropertyHandler::DisableTechnologyFailed,
                        AsWeakPtr(), technology, std::move(error_callback)));
   }
@@ -576,7 +576,8 @@ void ShillPropertyHandler::EnableTechnologyFailed(
     const std::string& dbus_error_name,
     const std::string& dbus_error_message) {
   NetworkMetricsHelper::LogEnableTechnologyResult(technology,
-                                                  /*success=*/false);
+                                                  /*success=*/false,
+                                                  dbus_error_name);
   enabling_technologies_.erase(technology);
   network_handler::ShillErrorCallbackFunction(
       "EnableTechnology Failed", technology, std::move(error_callback),
@@ -590,7 +591,8 @@ void ShillPropertyHandler::DisableTechnologyFailed(
     const std::string& dbus_error_name,
     const std::string& dbus_error_message) {
   NetworkMetricsHelper::LogDisableTechnologyResult(technology,
-                                                   /*success=*/false);
+                                                   /*success=*/false,
+                                                   dbus_error_name);
   disabling_technologies_.erase(technology);
   network_handler::ShillErrorCallbackFunction(
       "DisableTechnology Failed", technology, std::move(error_callback),
