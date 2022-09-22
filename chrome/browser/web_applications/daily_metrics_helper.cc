d0d71161a8801965507ba3f1c5421e46dbf8f3b9
Matt Menke <mmenke@chromium.org>
2022-09-22 00:13:33 +0000

    Migrate chrome/browser/web_applications to new pref update classes.
    ListPrefUpdate and DictPrefUpdate are now deprecated, in favor of
    ScopedListPrefUpdate and ScopedDictPrefUpdate, which only expose
    Value::List and Value::Dict, respectively, to discourage use of other
    APIs to manipulate lists and dicts, which are all deprecated.
    
    Bug: 1362719
    Change-Id: Id1ead96bf49c3e493c7c2c82c0544c2aab62219a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906226
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049955}

--- chrome/browser/web_applications/daily_metrics_helper.cc
+++ chrome/browser/web_applications/daily_metrics_helper.cc
@@ -173,8 +173,7 @@ void EmitRecords(Profile* profile) {
 }
 
 void RemoveRecords(PrefService* prefs) {
-  DictionaryPrefUpdate update(prefs, prefs::kWebAppsDailyMetrics);
-  update->DictClear();
+  prefs->SetDict(prefs::kWebAppsDailyMetrics, base::Value::Dict());
 }
 
 void UpdateRecord(DailyInteraction& record, PrefService* prefs) {
@@ -195,9 +194,9 @@ void UpdateRecord(DailyInteraction& record, PrefService* prefs) {
   }
 
   std::unique_ptr<DictionaryValue> record_dict = RecordToDict(record);
-  DictionaryPrefUpdate update(prefs, prefs::kWebAppsDailyMetrics);
+  ScopedDictPrefUpdate update(prefs, prefs::kWebAppsDailyMetrics);
 
-  update->SetKey(url, std::move(*record_dict));
+  update->Set(url, std::move(*record_dict));
 }
 
 }  // namespace
--- chrome/browser/web_applications/preinstalled_web_app_utils.cc
+++ chrome/browser/web_applications/preinstalled_web_app_utils.cc
@@ -665,9 +665,9 @@ bool WasAppMigratedToWebApp(Profile* profile, const std::string& app_id) {
 void MarkAppAsMigratedToWebApp(Profile* profile,
                                const std::string& app_id,
                                bool was_migrated) {
-  ListPrefUpdate update(profile->GetPrefs(),
-                        webapps::kWebAppsMigratedPreinstalledApps);
-  base::Value::List& update_list = update->GetList();
+  ScopedListPrefUpdate update(profile->GetPrefs(),
+                              webapps::kWebAppsMigratedPreinstalledApps);
+  base::Value::List& update_list = update.Get();
   if (was_migrated)
     EnsureContains(update_list, app_id);
   else
@@ -689,9 +689,9 @@ bool WasMigrationRun(Profile* profile, base::StringPiece feature_name) {
 void SetMigrationRun(Profile* profile,
                      base::StringPiece feature_name,
                      bool was_migrated) {
-  ListPrefUpdate update(profile->GetPrefs(),
-                        prefs::kWebAppsDidMigrateDefaultChromeApps);
-  base::Value::List& update_list = update->GetList();
+  ScopedListPrefUpdate update(profile->GetPrefs(),
+                              prefs::kWebAppsDidMigrateDefaultChromeApps);
+  base::Value::List& update_list = update.Get();
   if (was_migrated)
     EnsureContains(update_list, feature_name);
   else
@@ -715,8 +715,8 @@ void MarkPreinstalledAppAsUninstalled(Profile* profile,
                                       const std::string& app_id) {
   if (WasPreinstalledAppUninstalled(profile, app_id))
     return;
-  ListPrefUpdate update(profile->GetPrefs(),
-                        prefs::kWebAppsUninstalledDefaultChromeApps);
-  EnsureContains(update->GetList(), app_id);
+  ScopedListPrefUpdate update(profile->GetPrefs(),
+                              prefs::kWebAppsUninstalledDefaultChromeApps);
+  EnsureContains(update.Get(), app_id);
 }
 }  // namespace web_app
