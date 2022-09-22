093579ca681c9b2a6765a24f62d627a83293c0da
Sergio Vila Prado <sergiovila@google.com>
2022-09-22 05:41:01 +0000

    Match button styles and progress bar with dynamic colors
    Bug: b/243731658
    Change-Id: I64e8a75862c2553cc36b13bab47e74e868d01869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896816
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sergio Vila Prado <sergiovila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050054}

--- components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/header/AssistantStepProgressBar.java
+++ components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/header/AssistantStepProgressBar.java
@@ -42,7 +42,7 @@ public class AssistantStepProgressBar {
     // animation starts scaling and after a while the alpha changes.
     private static final int PULSING_ALPHA_CHANGE_DELAY_MS = 300;
     private static final int PULSING_RESTART_DELAY_MS = 2_000;
-    private static final int COLOR_LIST = R.color.blue_when_enabled_list;
+    private static final int COLOR_LIST = R.color.default_text_color_accent1_tint_list;
     private static final int ERROR_COLOR_LIST = R.color.default_red;
 
     private static class IconViewHolder {
@@ -303,6 +303,7 @@ public class AssistantStepProgressBar {
 
             line.setBackground(ApiCompatibilityUtils.getDrawable(context.getResources(),
                     R.drawable.autofill_assistant_rounded_corner_background));
+            line.setBackgroundTintList(ContextCompat.getColorStateList(context, COLOR_LIST));
             line.setEnabled(false);
         }
 
@@ -314,6 +315,7 @@ public class AssistantStepProgressBar {
                     R.drawable.autofill_assistant_rounded_corner_background));
 
             line.setTag(AssistantTagsForTesting.PROGRESSBAR_LINE_FOREGROUND_TAG);
+            line.setBackgroundTintList(ContextCompat.getColorStateList(context, COLOR_LIST));
             line.setEnabled(true);
 
             line.setScaleX(0f);
