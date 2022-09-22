ca6e0e94f1043a59e820ba5af9ae787a72716059
Shu Yang <shuyng@google.com>
2022-09-22 16:10:49 +0000

    [RDSD] Record UserActions for RDS domain and subdomain settings
    - DesktopSiteContentSetting.SettingsPage.Entered
    User entered the SingleCategoryPreferences page for Desktop site. It is
    emitted whenever this page is shown regardless how it is triggered.
    
    - DesktopSiteContentSetting.SettingsPage.SiteException.Opened
    User opened the site exception edit dialog by clicking on a site in the
    SingleCategoryPreferences page for Desktop site.
    
    Bug: 1365771
    Change-Id: I347ebf7d366bf4c4d06a2965422a6c434227adff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910373
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050220}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
@@ -394,6 +394,9 @@ public class SingleCategorySettings extends SiteSettingsPreferenceFragment
                 : null;
 
         configureGlobalToggles();
+        if (mCategory.getType() == SiteSettingsCategory.Type.REQUEST_DESKTOP_SITE) {
+            RecordUserAction.record("DesktopSiteContentSetting.SettingsPage.Entered");
+        }
 
         setHasOptionsMenu(true);
 
@@ -476,6 +479,10 @@ public class SingleCategorySettings extends SiteSettingsPreferenceFragment
 
             } else {
                 buildPreferenceDialog(website_pref.site()).show();
+                if (mCategory.getType() == SiteSettingsCategory.Type.REQUEST_DESKTOP_SITE) {
+                    RecordUserAction.record(
+                            "DesktopSiteContentSetting.SettingsPage.SiteException.Opened");
+                }
             }
         }
 
