0f236b41801f9acc26ce5b1ea863bd09faa971a6
Kirubel Aklilu <kaklilu@chromium.org>
2022-09-22 19:36:26 +0000

    Update PrivacySandboxSettings::Observer to track the FPS enabled pref.
    This CL introduces this logic to the observer for later use in updating
    all consumers of the FPS enabled pref to be responsive to dynamic
    updates.
    
    Bug: 1366918
    Change-Id: Ic8790f4cc227727c50e214ce514df5768fcae429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910963
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050335}

--- components/privacy_sandbox/privacy_sandbox_settings.cc
+++ components/privacy_sandbox/privacy_sandbox_settings.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
+#include "content/public/common/content_features.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/cookies/site_for_cookies.h"
 #include "url/gurl.h"
@@ -85,6 +86,11 @@ PrivacySandboxSettings::PrivacySandboxSettings(
       prefs::kPrivacySandboxApisEnabledV2,
       base::BindRepeating(&PrivacySandboxSettings::OnPrivacySandboxPrefChanged,
                           base::Unretained(this)));
+  pref_change_registrar_.Add(
+      prefs::kPrivacySandboxFirstPartySetsEnabled,
+      base::BindRepeating(
+          &PrivacySandboxSettings::OnFirstPartySetsEnabledPrefChanged,
+          base::Unretained(this)));
 }
 
 PrivacySandboxSettings::~PrivacySandboxSettings() = default;
@@ -396,6 +402,16 @@ void PrivacySandboxSettings::OnPrivacySandboxPrefChanged() {
 
   for (auto& observer : observers_)
     observer.OnTrustTokenBlockingChanged(!IsTrustTokensAllowed());
+}
+
+void PrivacySandboxSettings::OnFirstPartySetsEnabledPrefChanged() {
+  if (!base::FeatureList::IsEnabled(features::kFirstPartySets))
+    return;
+
+  for (auto& observer : observers_) {
+    observer.OnFirstPartySetsEnabledChanged(
+        pref_service_->GetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled));
+  }
 }
 
 void PrivacySandboxSettings::AddObserver(Observer* observer) {
--- components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
+++ components/privacy_sandbox/privacy_sandbox_settings_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "components/privacy_sandbox/privacy_sandbox_test_util.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
@@ -805,6 +806,30 @@ TEST_P(PrivacySandboxSettingsTest, TrustTokensAllowed) {
 
   privacy_sandbox_settings()->SetPrivacySandboxEnabled(true);
   EXPECT_TRUE(privacy_sandbox_settings()->IsTrustTokensAllowed());
+}
+
+TEST_P(PrivacySandboxSettingsTest, OnFirstPartySetsEnabledChanged) {
+  // OnFirstPartySetsEnabledChanged() should only call observers when the
+  // base::Feature is enabled and the pref changes.
+  base::test::ScopedFeatureList feature_list_;
+  feature_list_.InitWithFeatures({features::kFirstPartySets}, {});
+  privacy_sandbox_test_util::MockPrivacySandboxObserver observer;
+  privacy_sandbox_settings()->AddObserver(&observer);
+  EXPECT_CALL(observer, OnFirstPartySetsEnabledChanged(/*enabled=*/true));
+
+  prefs()->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled, true);
+  testing::Mock::VerifyAndClearExpectations(&observer);
+
+  EXPECT_CALL(observer, OnFirstPartySetsEnabledChanged(/*enabled=*/false));
+  prefs()->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled, false);
+  testing::Mock::VerifyAndClearExpectations(&observer);
+
+  feature_list_.Reset();
+  feature_list_.InitAndDisableFeature(features::kFirstPartySets);
+  EXPECT_CALL(observer, OnFirstPartySetsEnabledChanged(testing::_)).Times(0);
+
+  prefs()->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled, true);
+  prefs()->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled, false);
 }
 
 TEST_P(PrivacySandboxSettingsTest, IsTopicAllowed) {
