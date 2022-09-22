d0085313111993bacc5cd6830d0414dc34a1b906
David Black <dmblack@google.com>
2022-09-22 00:41:46 +0000

    Reland "Unify modeling logic for holding space sections."
    This is a reland of commit 89bf6045d56e8b33cc41d4b788d16c901a55e7b9
    
    Fix to reland is to move DCHECK_IS_ON checks inside `IsValid()` body.
    
    Original change's description:
    > Unify modeling logic for holding space sections.
    >
    > Currently holding space sections have a concept of:
    > * supported holding space item types
    > * max item counts
    > * max visible item counts
    >
    > These parameters are inconsistently spread across the code base. This
    > CL unifies all holding space section parameters in:
    >
    > //ash/public/cpp/holding_space/holding_space_sections
    >
    > Special casing for screen captures and downloads sections in the model
    > has also been removed.
    >
    > Bug: 1363825
    > Change-Id: Ic1d828a365fdf694cd36545757938ec1e1ae1234
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3897454
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: David Black <dmblack@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1049704}
    
    Bug: 1363825
    Change-Id: Id61e6a958e14121b8d1f910f26665e9a871307ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911276
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049975}

--- ash/system/holding_space/screen_captures_section.cc
+++ ash/system/holding_space/screen_captures_section.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/bubble/bubble_utils.h"
 #include "ash/public/cpp/holding_space/holding_space_constants.h"
 #include "ash/public/cpp/holding_space/holding_space_item.h"
+#include "ash/public/cpp/holding_space/holding_space_section.h"
 #include "ash/public/cpp/holding_space/holding_space_util.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/holding_space/holding_space_item_screen_capture_view.h"
@@ -21,10 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 ScreenCapturesSection::ScreenCapturesSection(HoldingSpaceViewDelegate* delegate)
-    : HoldingSpaceItemViewsSection(
-          delegate,
-          holding_space_util::ScreenCaptureSupportedTypes(),
-          /*max_count=*/kMaxScreenCaptures) {}
+    : HoldingSpaceItemViewsSection(delegate,
+                                   HoldingSpaceSectionId::kScreenCaptures) {}
 
 ScreenCapturesSection::~ScreenCapturesSection() = default;
 
