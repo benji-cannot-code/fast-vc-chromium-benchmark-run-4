78abbd91ae752819472463d1dfbb1726f3ab91a0
Arnaud Mandy <arnaud.mandy@intel.com>
2022-09-22 11:58:45 +0000

    compute pressure: Filter records in PressureObserver
    This patch implements the filtering of records in the PressureObserver
    and not anymore in the platform collector, for the reasons
    explained below.
    As described in [1]: All observers should be updated at their
    samplerate unless the previous record is similar to the new record.
    As discussed in [2], it shouldn't be implemented in the platform
    collector.
    
    [1]: https://github.com/WICG/compute-pressure/commit/fe277d00b98423e9b8344d666958cfda2d1a637d
    [2]: https://github.com/WICG/compute-pressure/issues/121
    
    Bug: 1359196
    Change-Id: I7b926f391ac119a0d33ec4cf91738540faa039e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3868958
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1050140}

---
+++ third_party/blink/web_tests/external/wpt/compute-pressure/compute_pressure_duplicate_updates.tentative.https.window.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/test-only-api.js
+// META: script=resources/pressure-helpers.js
+
+'use strict';
+
+pressure_test(async (t, mockPressureService) => {
+  const pressureUpdates = await new Promise(async resolve => {
+    const updates = [];
+    let n = 0;
+    const observer = new PressureObserver(update => {
+      updates.push(update);
+      if (++n === 2)
+        resolve(updates);
+    }, {sampleRate: 1.0});
+    await observer.observe('cpu');
+
+    mockPressureService.setPressureUpdate('critical');
+    mockPressureService.sendUpdate();
+    mockPressureService.setPressureUpdate('critical');
+    mockPressureService.sendUpdate();
+    mockPressureService.setPressureUpdate('nominal');
+    mockPressureService.sendUpdate();
+  });
+  assert_equals(pressureUpdates.length, 2);
+  assert_equals(pressureUpdates[0].state, 'critical');
+  assert_equals(pressureUpdates[1].state, 'nominal');
+}, 'Updates that fail the "has change in data" test are discarded.');
