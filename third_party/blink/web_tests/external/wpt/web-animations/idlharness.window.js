c26d882ef536b05e8c323d1adcd4522a479d24bf
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-09-22 12:53:19 +0000

    Import wpt@f99ccfb29fcd28e3679eb272e6f65f4394ae69d6
    Using wpt-import in Chromium df490b702cb33d609571e706334307cd345975f2.
    With Chromium commits locally applied on WPT:
    87e3777092 "Add keypress Event Timing Test"
    cb60e1a33e "Speculative deflake WPTs for non-passive wheel events"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ie937b77e268035496e2e952a00cf12b19376a442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911640
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1050148}

--- third_party/blink/web_tests/external/wpt/web-animations/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/web-animations/idlharness.window.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['web-animations', 'web-animations-2'],
-  ['dom', 'html'],
+  ['dom', 'html', 'scroll-animations'],
   idl_array => {
     idl_array.add_objects({
       Animation: ['new Animation()'],
