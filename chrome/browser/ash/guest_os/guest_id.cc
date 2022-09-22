0bbd28ee4c5ffe03f94aeff2af785ad8f348ea97
Peter Kasting <pkasting@chromium.org>
2022-09-22 00:23:17 +0000

    Change std::find_if*() to use base:: functions: chrome/browser/
    Goal: Slightly simpler and/or more semantically-accurate code.
    
    Bug: none
    Change-Id: Ia65d2ec7e40773568973a21f5e0203f05cb96f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911049
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049962}

--- chrome/browser/ash/guest_os/guest_id.cc
+++ chrome/browser/ash/guest_os/guest_id.cc
@@ -150,9 +150,9 @@ void RemoveContainerFromPrefs(Profile* profile, const GuestId& container_id) {
   auto* pref_service = profile->GetPrefs();
   ScopedListPrefUpdate updater(pref_service, prefs::kGuestOsContainers);
   base::Value::List& update_list = updater.Get();
-  auto it = std::find_if(
-      update_list.begin(), update_list.end(),
-      [&](const auto& dict) { return MatchContainerDict(dict, container_id); });
+  auto it = base::ranges::find_if(update_list, [&](const auto& dict) {
+    return MatchContainerDict(dict, container_id);
+  });
   if (it != update_list.end())
     update_list.erase(it);
 }
@@ -161,9 +161,7 @@ void RemoveVmFromPrefs(Profile* profile, VmType vm_type) {
   auto* pref_service = profile->GetPrefs();
   ScopedListPrefUpdate updater(pref_service, prefs::kGuestOsContainers);
   base::Value::List& update_list = updater.Get();
-  auto it = std::find_if(
-      update_list.begin(), update_list.end(),
-      [&](const auto& dict) { return VmTypeFromPref(dict) == vm_type; });
+  auto it = base::ranges::find(update_list, vm_type, &VmTypeFromPref);
   if (it != update_list.end())
     update_list.erase(it);
 }
@@ -185,9 +183,9 @@ void UpdateContainerPref(Profile* profile,
                          const std::string& key,
                          base::Value value) {
   ScopedListPrefUpdate updater(profile->GetPrefs(), prefs::kGuestOsContainers);
-  auto it = std::find_if(
-      updater->begin(), updater->end(),
-      [&](const auto& dict) { return MatchContainerDict(dict, container_id); });
+  auto it = base::ranges::find_if(*updater, [&](const auto& dict) {
+    return MatchContainerDict(dict, container_id);
+  });
   if (it != updater->end()) {
     if (base::Contains(*kPropertiesAllowList, key)) {
       it->SetKey(key, std::move(value));
--- chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.cc
+++ chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.h"
 
-#include <algorithm>
 #include <memory>
 
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "chrome/browser/enterprise/connectors/analysis/analysis_settings.h"
@@ -338,9 +338,9 @@ void LocalBinaryUploadService::FinishRequest(RequestKey key,
     DVLOG(1) << "FinishRequest key=" << key << " not active";
   }
 
-  auto it2 = std::find_if(
-      pending_requests_.begin(), pending_requests_.end(),
-      [key](const RequestInfo& info) { return key == info.request.get(); });
+  auto it2 = base::ranges::find(
+      pending_requests_, key,
+      [](const RequestInfo& info) { return info.request.get(); });
   if (it2 != pending_requests_.end())
     pending_requests_.erase(it2);
 }
