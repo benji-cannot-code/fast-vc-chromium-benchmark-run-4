0713ce51fb315ce08cbc6a74a3f57e2109e28dcc
Peter Kasting <pkasting@chromium.org>
2022-09-22 00:57:17 +0000

    Change std::find_if*() to use base:: functions: android_webview/
    Goal: Slightly simpler and/or more semantically-accurate code.
    
    Bug: none
    Change-Id: I9993fdd3f61695c01c8dffae22d81b929bf1cde2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911757
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049984}

--- android_webview/browser/gfx/root_frame_sink.cc
+++ android_webview/browser/gfx/root_frame_sink.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/gfx/child_frame.h"
 #include "android_webview/browser/gfx/display_scheduler_webview.h"
 #include "android_webview/browser/gfx/viz_compositor_thread_runner_webview.h"
+#include "base/containers/contains.h"
 #include "base/memory/raw_ptr.h"
 #include "base/trace_event/trace_event.h"
 #include "components/viz/common/features.h"
@@ -434,12 +435,10 @@ void RootFrameSink::EvictChildSurface(const viz::SurfaceId& surface_id) {
 }
 
 void RootFrameSink::OnCaptureStarted(const viz::FrameSinkId& frame_sink_id) {
-  auto it = std::find_if(contained_surfaces_.begin(), contained_surfaces_.end(),
-                         [frame_sink_id](const viz::SurfaceId& surface_id) {
-                           return surface_id.frame_sink_id() == frame_sink_id;
-                         });
-  if (it == contained_surfaces_.end())
+  if (!base::Contains(contained_surfaces_, frame_sink_id,
+                      &viz::SurfaceId::frame_sink_id)) {
     return;
+  }
   // When a capture is started we need to force an invalidate.
   if (client_)
     client_->Invalidate();
