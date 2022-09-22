8490323a241fa0689919ed208f5c56e344c84482
Xinyi Ji <xinyiji@google.com>
2022-09-22 02:31:25 +0000

    [Start] Clean up CHECK_SYNC_BEFORE_SHOW_START_AT_STARTUP related code
    Since the experiment of checking sync status before showing the Start surface on Startup does not help in DAU contradicting with the idea in https://chromium-review.googlesource.com/c/chromium/src/+/3152562, we clean up any code added for this experiment.
    
    Bug: 1359349
    Change-Id: Ib1bed894e035ff34e5fb7e64fbb2c9b02955d8f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910578
    Commit-Queue: Xinyi Ji <xinyiji@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050018}

--- chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinatorUnitTestRule.java
+++ chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinatorUnitTestRule.java
@@ -171,14 +171,6 @@ public class StartSurfaceCoordinatorUnitTestRule implements TestRule {
         return mCoordinator;
     }
 
-    public TabModelSelector getTabModelSelector() {
-        return mTabModelSelector;
-    }
-
-    public Activity getActivity() {
-        return mActivity;
-    }
-
     private void initJniMocks() {
         Profile profile = Mockito.mock(Profile.class);
         PrefService prefService = Mockito.mock(PrefService.class);
