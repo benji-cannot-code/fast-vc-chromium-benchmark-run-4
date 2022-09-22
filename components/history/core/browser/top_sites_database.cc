d7770bb5e9098c52e401c409074fd1040a1e359f
Andrew Paseltiner <apaseltiner@chromium.org>
2022-09-22 13:09:49 +0000

    Ensure that TopSitesDatabase writes are atomic
    By returning early if sql::Transaction::Begin fails.
    
    Change-Id: I8e3c9f75d1a612666234c94d564c8ea07bad9ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911836
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050151}

--- components/history/core/browser/top_sites_database.cc
+++ components/history/core/browser/top_sites_database.cc
@@ -340,8 +340,8 @@ void TopSitesDatabase::ApplyDelta(const TopSitesDelta& delta) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   sql::Transaction transaction(db_.get());
-  // TODO: consider returning early if `Begin()` returns false.
-  std::ignore = transaction.Begin();
+  if (!transaction.Begin())
+    return;
 
   for (const auto& deleted : delta.deleted) {
     if (!RemoveURLNoTransaction(deleted))
@@ -381,6 +381,8 @@ void TopSitesDatabase::GetSites(MostVisitedURLList* urls) {
 
 void TopSitesDatabase::SetSiteNoTransaction(const MostVisitedURL& url,
                                             int new_rank) {
+  DCHECK(db_->HasActiveTransactions());
+
   int rank = GetURLRank(url);
   if (rank == kRankOfNonExistingURL) {
     AddSite(url, new_rank);
@@ -391,6 +393,8 @@ void TopSitesDatabase::SetSiteNoTransaction(const MostVisitedURL& url,
 }
 
 void TopSitesDatabase::AddSite(const MostVisitedURL& url, int new_rank) {
+  DCHECK(db_->HasActiveTransactions());
+
   sql::Statement statement(
       db_->GetCachedStatement(SQL_FROM_HERE,
                               "INSERT OR REPLACE INTO top_sites "
@@ -433,7 +437,7 @@ int TopSitesDatabase::GetURLRank(const MostVisitedURL& url) {
 
 void TopSitesDatabase::UpdateSiteRankNoTransaction(const MostVisitedURL& url,
                                                    int new_rank) {
-  DCHECK_GT(db_->transaction_nesting(), 0);
+  DCHECK(db_->HasActiveTransactions());
 
   int prev_rank = GetURLRank(url);
   if (prev_rank == kRankOfNonExistingURL) {
@@ -493,6 +497,8 @@ void TopSitesDatabase::UpdateSiteRankNoTransaction(const MostVisitedURL& url,
 }
 
 bool TopSitesDatabase::RemoveURLNoTransaction(const MostVisitedURL& url) {
+  DCHECK(db_->HasActiveTransactions());
+
   int old_rank = GetURLRank(url);
   if (old_rank == kRankOfNonExistingURL)
     return true;
