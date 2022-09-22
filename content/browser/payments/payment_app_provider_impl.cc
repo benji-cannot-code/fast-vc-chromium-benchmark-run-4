eb8b8d73219a6081c4fe7c36f9c980b2b3226429
David Jacobo <djacobo@google.com>
2022-09-22 18:40:45 +0000

    Style one liner for code consistency
    Remove unnecessary braces for one line if to match surrounding code.
    
    Bug: None
    Test: build
    Change-Id: I4ccbc858421154418ad8b638f046efe919af3eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911283
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050314}

--- content/browser/payments/payment_app_provider_impl.cc
+++ content/browser/payments/payment_app_provider_impl.cc
@@ -297,9 +297,8 @@ void PaymentAppProviderImpl::SetOpenedWindow(
 void PaymentAppProviderImpl::CloseOpenedWindow() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (payment_handler_window_) {
+  if (payment_handler_window_)
     payment_handler_window_->Close();
-  }
 
   payment_handler_window_.reset();
 }
