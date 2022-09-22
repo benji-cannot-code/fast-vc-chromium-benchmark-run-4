0b8bfcd2d14186c232510f72902c665de6e1d334
Yue Zhang <yuezhanggg@chromium.org>
2022-09-22 02:14:25 +0000

    [PriceTracking] Show price tracking list when it's no longer empty
    This CL adds logics in bookmark list to listen to product tracking, so
    that the price tracking list will show after it's detected to be
    no longer empty.
    
    Bug: 1366035, 1346624
    Change-Id: I5526f18ecff6167283be7998bbc2fab9ec8c648b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907185
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050012}

--- chrome/browser/resources/side_panel/bookmarks/bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/bookmarks_list.ts
@@ -71,6 +71,7 @@ export class BookmarksListElement extends PolymerElement {
   private productInfos_: BookmarkProductInfo[];
   hoverVisible: boolean;
   private openFolders_: string[];
+  private shoppingListenerIds_: number[] = [];
 
   override ready() {
     super.ready();
@@ -132,6 +133,12 @@ export class BookmarksListElement extends PolymerElement {
             'Commerce.PriceTracking.SidePanel.TrackedProductsShown');
       }
     });
+    const callbackRouter = this.shoppingListApi_.getCallbackRouter();
+    this.shoppingListenerIds_.push(
+        callbackRouter.priceTrackedForBookmark.addListener(
+            (product: BookmarkProductInfo) =>
+                this.onBookmarkPriceTracked(product)),
+    );
   }
 
   override disconnectedCallback() {
@@ -139,6 +146,8 @@ export class BookmarksListElement extends PolymerElement {
       this.bookmarksApi_.callbackRouter[eventName]!.removeListener(callback);
     }
     this.bookmarksDragManager_.stopObserving();
+    this.shoppingListenerIds_.forEach(
+        id => this.shoppingListApi_.getCallbackRouter().removeListener(id));
   }
 
   /** BookmarksDragDelegate */
@@ -413,6 +422,18 @@ export class BookmarksListElement extends PolymerElement {
     if (bookmarkElement) {
       bookmarkElement.focus();
     }
+  }
+
+  private onBookmarkPriceTracked(product: BookmarkProductInfo) {
+    // Here we only control the visibility of ShoppingListElement. The same
+    // signal will also be handled in ShoppingListElement to update shopping
+    // list.
+    if (this.productInfos_.length > 0) {
+      return;
+    }
+    this.push('productInfos_', product);
+    chrome.metricsPrivate.recordUserAction(
+        'Commerce.PriceTracking.SidePanel.TrackedProductsShown');
   }
 }
 
