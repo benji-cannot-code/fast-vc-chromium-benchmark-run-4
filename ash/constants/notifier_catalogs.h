b96b1f00bccd04e0a3fb4881fc400089f21c604b
Jan Lanik <janlanik@google.com>
2022-09-22 09:22:10 +0000

    [Privacy Hub] Camera Notifications
    Adding a notification that will show up if an app accesses the camera
    that is disabled in PrivacyHub. The notification also contains a button
    that allows to ennable the camera directly without having to go to the
    settings.
    
    Bug: b/246255899
    Change-Id: I88a575c17a8a3691e49bb52e036780a9a974eac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841812
    Auto-Submit: Jan Láník <janlanik@google.com>
    Commit-Queue: Jan Láník <janlanik@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050102}

--- ash/constants/notifier_catalogs.h
+++ ash/constants/notifier_catalogs.h
@@ -167,7 +167,8 @@ enum class NotificationCatalogName {
   kArcLowDiskSpacePreStop = 151,
   kArcLowDiskSpacePostStop = 152,
   kMultiCapture = 153,
-  kMaxValue = kMultiCapture
+  kPrivacyHubCamera = 154,
+  kMaxValue = kPrivacyHubCamera
 };
 
 // A living catalog that registers system nudges.
--- chrome/browser/ui/ash/media_client_impl.cc
+++ chrome/browser/ui/ash/media_client_impl.cc
@@ -8,11 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/media_controller.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/public/cpp/system/toast_data.h"
 #include "ash/public/cpp/system/toast_manager.h"
+#include "ash/session/session_controller_impl.h"
+#include "ash/shell.h"
+#include "ash/system/privacy_hub/camera_privacy_switch_controller.h"
+#include "ash/system/privacy_hub/privacy_hub_controller.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/check_op.h"
@@ -457,9 +462,18 @@ void MediaClientImpl::OnActiveClientChange(cros::mojom::CameraClientType type,
                                            bool is_active) {
   is_camera_active_ = is_active;
 
-  if (is_active && camera_privacy_switch_state_ ==
-                       cros::mojom::CameraPrivacySwitchState::ON) {
-    ShowCameraOffNotification();
+  if (is_active) {
+    if (camera_privacy_switch_state_ ==
+        cros::mojom::CameraPrivacySwitchState::ON) {
+      ShowCameraOffNotification();
+    }
+    if (!ProfileManager::GetActiveUserProfile()->GetPrefs()->GetBoolean(
+            ash::prefs::kUserCameraAllowed)) {
+      ash::Shell::Get()
+          ->privacy_hub_controller()
+          ->camera_controller()
+          .ShowNotification(GetNameOfAppAccessingCameraInternal());
+    }
   }
 }
 
