2ed1340965dc1c6c72f36c81135cd010c3cb6ff4
Wenyu Fu <wenyufu@google.com>
2022-09-22 21:13:14 +0000

    [GMNext] Clean up tab_ui theme overlay #3: remove applyThemeOverlay
    This is a multiple step work of removing all theme overlap added for tab
    switcher theme. The goal is to inline all the styles into
    tab_grid_card_item.xml
    
    Step 3: Remove the empty theme and #applyThemeOverlay APIs
    
    
    Bug: 1251194
    Change-Id: I79455101949fc2240bc6bfe146386c805a8a4efd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885307
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050389}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewTest.java
@@ -60,7 +60,6 @@ public class TabGridDialogViewTest extends BlankUiTestActivityTestCase {
     @Override
     public void setUpTest() throws Exception {
         super.setUpTest();
-        TabUiTestHelper.applyThemeOverlays(getActivity());
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mDummyParent = new FrameLayout(getActivity());
             getActivity().setContentView(mDummyParent);
