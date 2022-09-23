c98713849d21c2fb9ea02faa65529fa8884a42fb
Vasiliy Telezhnikov <vasilyt@chromium.org>
2022-09-23 17:29:44 +0000

    Add DMSAA code path for tiles
    This CL adds DMSAA code path for tiles under UseDMSAAForTiles feature
    flag.
    
    Bug: 1366068
    Change-Id: I2cd3f5c941472d30f3cc3698e6e223dd60e3c6f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913529
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050707}

--- cc/raster/gpu_raster_buffer_provider.cc
+++ cc/raster/gpu_raster_buffer_provider.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_event.h"
 #include "build/chromeos_buildflags.h"
+#include "cc/base/features.h"
 #include "cc/base/histograms.h"
 #include "cc/paint/display_item_list.h"
 #include "cc/paint/paint_canvas.h"
@@ -152,7 +153,9 @@ GpuRasterBufferProvider::GpuRasterBufferProvider(
       max_tile_size_(max_tile_size),
       pending_raster_queries_(pending_raster_queries),
       raster_metric_probability_(raster_metric_probability),
-      is_using_raw_draw_(features::IsUsingRawDraw()) {
+      is_using_raw_draw_(features::IsUsingRawDraw()),
+      is_using_dmsaa_(
+          base::FeatureList::IsEnabled(features::kUseDMSAAForTiles)) {
   DCHECK(pending_raster_queries);
   DCHECK(compositor_context_provider);
   DCHECK(worker_context_provider);
@@ -372,9 +375,11 @@ void GpuRasterBufferProvider::RasterBufferImpl::RasterizeSource(
   }
 
   // Assume legacy MSAA if sample count is positive.
-  gpu::raster::MsaaMode msaa_mode = playback_settings.msaa_sample_count > 0
-                                        ? gpu::raster::kMSAA
-                                        : gpu::raster::kNoMSAA;
+  gpu::raster::MsaaMode msaa_mode =
+      playback_settings.msaa_sample_count > 0
+          ? (client_->is_using_dmsaa_ ? gpu::raster::kDMSAA
+                                      : gpu::raster::kMSAA)
+          : gpu::raster::kNoMSAA;
   // msaa_sample_count should be 1, 2, 4, 8, 16, 32, 64,
   // and log2(msaa_sample_count) should be [0,6].
   // If playback_settings.msaa_sample_count <= 0, the MSAA is not used. It is
