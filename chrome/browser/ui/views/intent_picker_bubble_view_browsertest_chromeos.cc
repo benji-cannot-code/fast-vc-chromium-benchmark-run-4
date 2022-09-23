eaec4b0915da588e279c62c92290437e1171fb75
Darryl James <dljames@chromium.org>
2022-09-23 18:47:16 +0000

    [Sheriff] Disable slow tests under ASAN.
    Bug: 1367375
    Change-Id: I97908b220b0951958efcae349d9a6dfade62dc21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916355
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Owners-Override: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050743}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest_chromeos.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest_chromeos.cc
@@ -1168,7 +1168,8 @@ IN_PROC_BROWSER_TEST_F(IntentPickerBubbleViewBrowserTestChromeOS,
 // Test that remember by choice checkbox works for open PWA option.
 //
 // TODO(https://crbug.com/1361934): Fix timeouts under MSAN.
-#if defined(MEMORY_SANITIZER)
+// TODO(https://crbug.com/1367375): Fix timeouts under ASAN.
+#if defined(MEMORY_SANITIZER) || (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_RememberOpenPWA DISABLED_RememberOpenPWA
 #else
 #define MAYBE_RememberOpenPWA RememberOpenPWA
@@ -1256,7 +1257,8 @@ class IntentPickerBubbleViewPrerenderingBrowserTestChromeOS
 // and the app shouldn't be opened.
 //
 // TODO(https://crbug.com/1361934): Fix timeouts under MSAN.
-#if defined(MEMORY_SANITIZER)
+// TODO(https://crbug.com/1367375): Fix timeouts under ASAN.
+#if defined(MEMORY_SANITIZER) || (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
 #define MAYBE_AppLaunchURLCancelsPrerendering \
   DISABLED_AppLaunchURLCancelsPrerendering
 #else
