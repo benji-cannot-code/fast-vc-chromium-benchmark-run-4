b01c7387b953c86cbe321a6f33da47b0d61fb61f
Darryl James <dljames@chromium.org>
2022-09-23 19:02:39 +0000

    Revert "Partial Reland: Make compound backing work with GLTextureImageBacking"
    This reverts commit d5699d5f8731659a4c9038d68f48e4b9e2090ba8.
    
    Reason for revert: Consistent failures on Linuz ASan/LSan builder
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/106378/test-results
    
    Original change's description:
    > Partial Reland: Make compound backing work with GLTextureImageBacking
    >
    > This is a partial reland of https://crrev.com/c/3832922 as some of the
    > CL was landed separately.
    >
    > Make GLTextureImageBacking work with compound image backing and have it
    > replace GLImageBacking for shared memory GMBs.
    > GLTextureImageBackingFactory ignores scanout usage for shared memory
    > GMBs to match existing behaviour in GLImageBackingFactory.
    >
    > This also removes the GLImageBackingFactory functionality to work with
    > shared memory GMBs and having two instances of the factory.
    > Unfortunately GLTextureImageBackingFactory needs two instances, one for
    > regular GL textures and one for shared memory GMBs, to ensure we pick
    > the right backing type on Windows.
    >
    > This also includes fixes for pixel upload. GLES2 doesn't support
    > conversion on upload so BGRX/RGBX must be repacked to RGB before upload.
    > Also repack if stride of the source data is longer than GL expects and
    > GL_UNPACK_ROW_LENGTH isn't supported. This mostly matches existing
    > GLImageMemory behaviour for upload. There is one major exception,
    > GLTextureImageBacking doesn't support BGR_565 textures while
    > GLImageMemory does, but I don't see any evidence of shared memory GMB
    > usage with BGR_565 textures.
    >
    > Bug: 1293509
    > Change-Id: I342deb475fb2dc1d636f6e6df5a15da989cfd325
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892753
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1050643}
    
    Bug: 1293509
    Change-Id: Ie68aece3a17470a624574157c4993c64143f5784
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916336
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Darryl James <dljames@chromium.org>
    Owners-Override: Darryl James <dljames@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1050751}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing.h
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_GL_TEXTURE_IMAGE_BACKING_H_
 #define GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_GL_TEXTURE_IMAGE_BACKING_H_
 
-#include "components/viz/common/resources/resource_format.h"
 #include "gpu/command_buffer/service/shared_image/gl_texture_image_backing_helper.h"
 
 namespace gl {
@@ -19,8 +18,6 @@ namespace gpu {
 // backed by a GLImage.
 class GLTextureImageBacking : public ClearTrackingSharedImageBacking {
  public:
-  static bool SupportsPixelUploadWithFormat(viz::ResourceFormat format);
-
   GLTextureImageBacking(const Mailbox& mailbox,
                         viz::ResourceFormat format,
                         const gfx::Size& size,
@@ -68,15 +65,12 @@ class GLTextureImageBacking : public ClearTrackingSharedImageBacking {
       MemoryTypeTracker* tracker,
       scoped_refptr<SharedContextState> context_state) override;
   void Update(std::unique_ptr<gfx::GpuFence> in_fence) override;
-  bool UploadFromMemory(const SkPixmap& pixmap) override;
 
   bool IsPassthrough() const { return is_passthrough_; }
 
   const bool is_passthrough_;
   gles2::Texture* texture_ = nullptr;
   scoped_refptr<gles2::TexturePassthrough> passthrough_texture_;
-
-  GLTextureImageBackingHelper::InitializeGLTextureParams texture_params_;
 
   sk_sp<SkPromiseImageTexture> cached_promise_texture_;
   scoped_refptr<gl::GLImageEGL> image_egl_;
