9e9d7eeff67ba160ae1e6f64a8d45eb8d93fbb62
Luciano Pacheco <lucmult@chromium.org>
2022-09-23 02:39:37 +0000

    Files app: Fix remove watcher for unmounted volumes
    For archives we can add and remove file watchers, since CL:3833754
    removing watcher from an unmounted archive was failing.
    
    Fixes: 1365730
    Change-Id: If91f8e90cb5dbd9e00f101e9a739a303813e65ad
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907435
    Reviewed-by: François Degros <fdegros@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050494}

--- chrome/browser/chromeos/extensions/file_manager/private_api_file_system.h
+++ chrome/browser/chromeos/extensions/file_manager/private_api_file_system.h
@@ -114,6 +114,9 @@ class FileWatchFunctionBase : public LoggedExtensionFunction {
  protected:
   ~FileWatchFunctionBase() override = default;
 
+  // A virtual method to tell the base class if the function is addFileWatch().
+  virtual bool IsAddWatch() = 0;
+
   // Performs a file watch operation (ex. adds or removes a file watch) on
   // the IO thread with storage::WatcherManager.
   virtual void PerformFileWatchOperationOnIOThread(
@@ -159,6 +162,7 @@ class FileManagerPrivateInternalAddFileWatchFunction
   void PerformFallbackFileWatchOperationOnUIThread(
       const storage::FileSystemURL& file_system_url,
       base::WeakPtr<file_manager::EventRouter> event_router) override;
+  bool IsAddWatch() override;
 };
 
 // Implements the chrome.fileManagerPrivate.removeFileWatch method.
@@ -181,6 +185,7 @@ class FileManagerPrivateInternalRemoveFileWatchFunction
   void PerformFallbackFileWatchOperationOnUIThread(
       const storage::FileSystemURL& file_system_url,
       base::WeakPtr<file_manager::EventRouter> event_router) override;
+  bool IsAddWatch() override;
 };
 
 // Implements the chrome.fileManagerPrivate.getSizeStats method.
