c09bf01b4947be666f4bcf9540976f8e51faa17d
James Lee <ljjlee@google.com>
2022-09-23 12:07:26 +0000

    Multi-arm experiment for local approval button preference
    This adds feature parameters to allow either the local or remote flow
    to be displayed as the more prominent option in the interstitial
    screen.
    
    Usage is one of the two options below (leaving out the part after the
    colon defaults to local):
    "LocalWebApprovals:preferred_button/remote"
    "LocalWebApprovals:preferred_button/local"
    
    Screenshots:
    * Prefer local, light mode: https://screenshot.googleplex.com/4dk23yUrpvtDJTs
    * Prefer local, dark mode:
    https://screenshot.googleplex.com/9omk63nTPFCVjex
    * Prefer remote, light mode:
    https://screenshot.googleplex.com/5BashudjYPW8yEF
    * Prefer remote, dark mode:
    https://screenshot.googleplex.com/3b2RbDP6oKuniYY
    
    
    Bug: b:243915048
    Change-Id: Ic37a03322bbde0b65e362cbb24c35ed6984831f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904362
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050584}

--- chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
@@ -410,7 +410,8 @@ class SupervisedUserIframeFilterTest
     : public SupervisedUserNavigationThrottleTest,
       public testing::WithParamInterface<
           std::tuple</* web_filter_interstitial_refresh_enabled */ bool,
-                     /* local_web_approvals_enabled */ bool>> {
+                     /* local_web_approvals_enabled */ bool,
+                     /* local_web_approvals_preferred */ bool>> {
  protected:
   SupervisedUserIframeFilterTest() { InitFeatures(); }
 
@@ -424,12 +425,14 @@ class SupervisedUserIframeFilterTest
   bool IsInterstitialBeingShownInFrame(int frame_id);
   bool IsRemoteApprovalsButtonBeingShown(int frame_id);
   bool IsLocalApprovalsButtonBeingShown(int frame_id);
+  void CheckPreferredApprovalButton(int frame_id);
   bool IsLocalApprovalsInsteadButtonBeingShown(int frame_id);
   void SendCommandToFrame(const std::string& command_name, int frame_id);
   void WaitForNavigationFinished(int frame_id, const GURL& url);
   void InitFeatures();
   bool IsWebFilterInterstitialRefreshEnabled() const;
   bool IsLocalWebApprovalsEnabled() const;
+  bool IsLocalWebApprovalsPreferred() const;
 
   PermissionRequestCreatorMock* permission_creator() {
     return permission_creator_;
@@ -520,6 +523,35 @@ bool SupervisedUserIframeFilterTest::IsLocalApprovalsButtonBeingShown(
   return RunCommandAndGetBooleanFromFrame(frame_id, command);
 }
 
+void SupervisedUserIframeFilterTest::CheckPreferredApprovalButton(
+    int frame_id) {
+  if (supervised_users::IsLocalWebApprovalThePreferredButton()) {
+    std::string command =
+        "domAutomationController.send("
+        "(document.getElementById('local-approvals-button').classList.contains("
+        "'primary-button') &&"
+        " !document.getElementById('local-approvals-button').classList."
+        "contains('secondary-button') &&"
+        " document.getElementById('remote-approvals-button').classList."
+        "contains('secondary-button') &&"
+        " !document.getElementById('remote-approvals-button').classList."
+        "contains('primary-button')));";
+    ASSERT_TRUE(RunCommandAndGetBooleanFromFrame(frame_id, command));
+  } else {
+    std::string command =
+        "domAutomationController.send("
+        "(document.getElementById('remote-approvals-button').classList."
+        "contains('primary-button') &&"
+        " !document.getElementById('remote-approvals-button').classList."
+        "contains('secondary-button') &&"
+        " document.getElementById('local-approvals-button').classList.contains("
+        "'secondary-button') &&"
+        " !document.getElementById('local-approvals-button').classList."
+        "contains('primary-button')));";
+    ASSERT_TRUE(RunCommandAndGetBooleanFromFrame(frame_id, command));
+  }
+}
+
 bool SupervisedUserIframeFilterTest::IsLocalApprovalsInsteadButtonBeingShown(
     int frame_id) {
   std::string command =
@@ -573,19 +605,30 @@ bool SupervisedUserIframeFilterTest::RunCommandAndGetBooleanFromFrame(
 }
 
 void SupervisedUserIframeFilterTest::InitFeatures() {
-  std::vector<base::Feature> enabled_features, disabled_features;
+  std::vector<base::test::ScopedFeatureList::FeatureAndParams>
+      enabled_features_and_params;
+  std::vector<base::Feature> disabled_features;
   if (IsWebFilterInterstitialRefreshEnabled()) {
-    enabled_features.push_back(supervised_users::kWebFilterInterstitialRefresh);
+    enabled_features_and_params.emplace_back(
+        supervised_users::kWebFilterInterstitialRefresh,
+        base::FieldTrialParams());
   } else {
     disabled_features.push_back(
         supervised_users::kWebFilterInterstitialRefresh);
   }
   if (IsLocalWebApprovalsEnabled()) {
-    enabled_features.push_back(supervised_users::kLocalWebApprovals);
+    base::FieldTrialParams params;
+    params["preferred_button"] =
+        IsLocalWebApprovalsPreferred()
+            ? supervised_users::kLocalWebApprovalsPreferredButtonLocal
+            : supervised_users::kLocalWebApprovalsPreferredButtonRemote;
+    enabled_features_and_params.emplace_back(
+        supervised_users::kLocalWebApprovals, params);
   } else {
     disabled_features.push_back(supervised_users::kLocalWebApprovals);
   }
-  scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
+  scoped_feature_list_.InitWithFeaturesAndParameters(
+      enabled_features_and_params, disabled_features);
 }
 
 bool SupervisedUserIframeFilterTest::IsWebFilterInterstitialRefreshEnabled()
@@ -597,6 +640,10 @@ bool SupervisedUserIframeFilterTest::IsLocalWebApprovalsEnabled() const {
   return std::get<1>(GetParam());
 }
 
+bool SupervisedUserIframeFilterTest::IsLocalWebApprovalsPreferred() const {
+  return std::get<2>(GetParam());
+}
+
 // Only test the valid configurations of the two flags. LocalWebApprovals will
 // not be enabled without the WebFilterInterstitialRefresh flag.
 INSTANTIATE_TEST_SUITE_P(
@@ -604,11 +651,17 @@ INSTANTIATE_TEST_SUITE_P(
     SupervisedUserIframeFilterTest,
     testing::Values(
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
-                        /* local_web_approvals_enabled */ true),
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ true),
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
-                        /* local_web_approvals_enabled */ false),
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ false),
+        std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
+                        /* local_web_approvals_enabled */ false,
+                        /* local_web_approvals_preferred */ false),
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ false,
-                        /* local_web_approvals_enabled */ false)));
+                        /* local_web_approvals_enabled */ false,
+                        /* local_web_approvals_preferred */ false)));
 
 IN_PROC_BROWSER_TEST_P(SupervisedUserIframeFilterTest, BlockSubFrame) {
   BlockHost(kIframeHost2);
@@ -886,11 +939,17 @@ INSTANTIATE_TEST_SUITE_P(
     SupervisedUserNarrowWidthIframeFilterTest,
     testing::Values(
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
-                        /* local_web_approvals_enabled */ true),
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ true),
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
-                        /* local_web_approvals_enabled */ false),
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ false),
+        std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
+                        /* local_web_approvals_enabled */ false,
+                        /* local_web_approvals_preferred */ false),
         std::make_tuple(/* web_filter_interstitial_refresh_enabled */ false,
-                        /* local_web_approvals_enabled */ false)));
+                        /* local_web_approvals_enabled */ false,
+                        /* local_web_approvals_preferred */ false)));
 
 IN_PROC_BROWSER_TEST_P(SupervisedUserNarrowWidthIframeFilterTest,
                        NarrowWidthWindow) {
@@ -963,11 +1022,16 @@ IN_PROC_BROWSER_TEST_P(SupervisedUserNarrowWidthIframeFilterTest,
 using ChromeOSLocalWebApprovalsTest = SupervisedUserIframeFilterTest;
 
 // Only test for the local web approvals feature enabled.
-INSTANTIATE_TEST_SUITE_P(,
-                         ChromeOSLocalWebApprovalsTest,
-                         testing::Values(std::make_tuple(
-                             /* web_filter_interstitial_refresh_enabled */ true,
-                             /* local_web_approvals_enabled */ true)));
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    ChromeOSLocalWebApprovalsTest,
+    testing::Values(
+        std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ true),
+        std::make_tuple(/* web_filter_interstitial_refresh_enabled */ true,
+                        /* local_web_approvals_enabled */ true,
+                        /* local_web_approvals_preferred */ false)));
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
                        StartLocalWebApprovalsFromMainFrame) {
@@ -983,6 +1047,7 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   const int blocked_frame = blocked_frames[0];
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
+  CheckPreferredApprovalButton(blocked_frame);
 
   // Trigger local approval flow - native dialog should appear.
   SendCommandToFrame(kLocalUrlAccessCommand, blocked_frame);
@@ -996,6 +1061,7 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   EXPECT_TRUE(IsInterstitialBeingShownInMainFrame(browser()));
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
+  CheckPreferredApprovalButton(blocked_frame);
 }
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
@@ -1014,6 +1080,7 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   EXPECT_TRUE(IsInterstitialBeingShownInFrame(blocked_frame));
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
+  CheckPreferredApprovalButton(blocked_frame);
 
   // Trigger local approval flow - native dialog should appear.
   SendCommandToFrame(kLocalUrlAccessCommand, blocked_frame);
@@ -1028,6 +1095,7 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   EXPECT_TRUE(IsInterstitialBeingShownInFrame(blocked_frame));
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
+  CheckPreferredApprovalButton(blocked_frame);
 }
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
