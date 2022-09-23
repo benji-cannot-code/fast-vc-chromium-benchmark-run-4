95b23c58feaea443b7e385a615d2aaa9dc2e2017
Roland Bock <rbock@google.com>
2022-09-23 22:57:49 +0000

    Fallback to the default value in PrefService::GetPreferenceValue
    pref_registry_->defaults() is a better fallback than nullptr.
    
    Note that this could only ever have an effect in case
    
    pref_registry_->defaults()
       != pref_value_store_->GetStore(DEFAULT_STORE)
    
    So in the long run, we might want to add a guarantee for equality.
    
    Bug: 1364529
    Change-Id: I63f380fab0839afc1e1931629a9c804f2fed687c
    Tests: Existing unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899758
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050840}

--- components/prefs/pref_service.cc
+++ components/prefs/pref_service.cc
@@ -679,7 +679,7 @@ const base::Value* PrefService::GetPreferenceValue(
   if (!pref_value_store_->GetValue(path, default_type, &found_value)) {
     // Every registered preference has at least a default value.
     NOTREACHED() << "no valid value found for registered pref " << path;
-    return nullptr;
+    return default_value;
   }
 
   DCHECK_EQ(found_value->type(), default_type);
