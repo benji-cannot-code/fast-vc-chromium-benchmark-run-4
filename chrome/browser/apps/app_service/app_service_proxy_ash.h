03fa027f27205a68b4906c7026861353b0c2b359
Nancy Wang <nancylingwang@chromium.org>
2022-09-23 07:12:18 +0000

    Fix the PWA window opened in the background issue when launch with url.
    With the non mojom app launch interfaces, the app is launched
    synchronously. However, that might be some other tasks which are
    expected to be run before app launching, and the synchronous app launching might cause the schedule issue.
    
    When launch the Meet PWA by clicking the button in calender window(W0),
    if the link capture is enabled, LaunchAppWithUrl is called
    synchronously, and the Meet window(W1) is opened and activated synchronously. And then the follow up task activated the calender
    window(W0). This cause the Meet PWA window opened in background, not
    activated and focus status:
    LaunchAppWithUrl
      --> Create W1
        --> Activated W1 (Meet window)
    Activated W0 (Calender window)
    
    Modify CommonAppsNavigationThrottle to call LaunchAppWithUrl async to
    let other follow up tasks (e.g. Activated W0) to run first, and then
    launch the PWA apps to activate the PWA window, and keep the consistent
    behavior as the mojom launch interface.
    
    BUG=1365790, b:244472502
    
    Change-Id: I9deda09a68cd8cf264163dac25aabd6f59ff26ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906686
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050534}

--- chrome/browser/apps/app_service/app_service_proxy_ash.h
+++ chrome/browser/apps/app_service/app_service_proxy_ash.h
@@ -134,6 +134,9 @@ class AppServiceProxyAsh : public AppServiceProxyBase,
       apps::mojom::LaunchSource launch_source,
       apps::mojom::WindowInfoPtr window_info,
       apps::mojom::Publisher::LaunchAppWithIntentCallback callback) override;
+
+  base::WeakPtr<AppServiceProxy> GetWeakPtr();
+
   void FlushMojoCallsForTesting() override;
 
   void ReInitializeCrostiniForTesting();
--- chrome/browser/apps/intent_helper/common_apps_navigation_throttle.cc
+++ chrome/browser/apps/intent_helper/common_apps_navigation_throttle.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/projector_app/public/cpp/projector_app_constants.h"
 #include "base/containers/contains.h"
 #include "base/memory/values_equivalent.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/default_tick_clock.h"
 #include "base/time/tick_clock.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -248,26 +249,6 @@ bool CommonAppsNavigationThrottle::ShouldCancelNavigation(
   if (handle->IsInPrerenderedMainFrame())
     return true;
 
-  auto launch_source = navigate_from_link() ? LaunchSource::kFromLink
-                                            : LaunchSource::kFromOmnibox;
-  GURL redirected_url =
-      RedirectUrlIfSwa(profile, preferred_app_id.value(), url, clock_);
-  if (base::FeatureList::IsEnabled(apps::kAppServiceLaunchWithoutMojom)) {
-    proxy->LaunchAppWithUrl(
-        preferred_app_id.value(),
-        GetEventFlags(WindowOpenDisposition::NEW_WINDOW,
-                      /*prefer_container=*/true),
-        redirected_url, launch_source,
-        std::make_unique<WindowInfo>(display::kDefaultDisplayId));
-  } else {
-    proxy->LaunchAppWithUrl(
-        preferred_app_id.value(),
-        GetEventFlags(WindowOpenDisposition::NEW_WINDOW,
-                      /*prefer_container=*/true),
-        redirected_url, ConvertLaunchSourceToMojomLaunchSource(launch_source),
-        apps::MakeWindowInfo(display::kDefaultDisplayId));
-  }
-
   const GURL& last_committed_url = web_contents->GetLastCommittedURL();
   if (!last_committed_url.is_valid() || last_committed_url.IsAboutBlank() ||
       // After clicking a link in various apps (eg gchat), a blank redirect page
@@ -275,6 +256,33 @@ bool CommonAppsNavigationThrottle::ShouldCancelNavigation(
       // returns false for links clicked from apps.
       !handle->WasInitiatedByLinkClick()) {
     web_contents->ClosePage();
+  }
+
+  auto launch_source = navigate_from_link() ? LaunchSource::kFromLink
+                                            : LaunchSource::kFromOmnibox;
+  GURL redirected_url =
+      RedirectUrlIfSwa(profile, preferred_app_id.value(), url, clock_);
+  if (base::FeatureList::IsEnabled(apps::kAppServiceLaunchWithoutMojom)) {
+    // The tab may have been closed, which runs async and causes the browser
+    // window to be refocused. Post a task to launch the app to ensure launching
+    // happens after the tab closed, otherwise the opened app window might be
+    // inactivated.
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &AppServiceProxy::LaunchAppWithUrlForBind, proxy->GetWeakPtr(),
+            preferred_app_id.value(),
+            GetEventFlags(WindowOpenDisposition::NEW_WINDOW,
+                          /*prefer_container=*/true),
+            redirected_url, launch_source,
+            std::make_unique<WindowInfo>(display::kDefaultDisplayId)));
+  } else {
+    proxy->LaunchAppWithUrl(
+        preferred_app_id.value(),
+        GetEventFlags(WindowOpenDisposition::NEW_WINDOW,
+                      /*prefer_container=*/true),
+        redirected_url, ConvertLaunchSourceToMojomLaunchSource(launch_source),
+        apps::MakeWindowInfo(display::kDefaultDisplayId));
   }
 
   IntentHandlingMetrics::RecordPreferredAppLinkClickMetrics(
