a9fb0c83251d36f96312edfcc32dae1e6a52a340
Peter Kasting <pkasting@chromium.org>
2022-09-23 10:59:21 +0000

    Change std::find_if*() to use base:: functions: weblayer/
    Goal: Slightly simpler and/or more semantically-accurate code.
    
    Bug: none
    Change-Id: I042c55f981d921abc5c3879b20984db10b688655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911279
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050570}

--- weblayer/browser/browser_impl.cc
+++ weblayer/browser/browser_impl.cc
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "weblayer/browser/browser_impl.h"
 
-#include <algorithm>
-
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
+#include "base/ranges/algorithm.h"
 #include "build/build_config.h"
 #include "components/base32/base32.h"
 #include "components/prefs/pref_service.h"
@@ -432,8 +431,7 @@ std::unique_ptr<Tab> BrowserImpl::RemoveTab(Tab* tab) {
   TabImpl* tab_impl = static_cast<TabImpl*>(tab);
   DCHECK_EQ(this, tab_impl->browser());
   static_cast<TabImpl*>(tab)->set_browser(nullptr);
-  auto iter =
-      std::find_if(tabs_.begin(), tabs_.end(), base::MatchesUniquePtr(tab));
+  auto iter = base::ranges::find_if(tabs_, base::MatchesUniquePtr(tab));
   DCHECK(iter != tabs_.end());
   std::unique_ptr<Tab> owned_tab = std::move(*iter);
   tabs_.erase(iter);
