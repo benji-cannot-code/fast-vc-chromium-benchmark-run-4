9a26922fe63d6181c08053134b7f38a03e07b2dc
Longbo Wei <longbowei@google.com>
2022-09-23 16:45:47 +0000

    shortcuts: Accelerator command migration
    Move these commands to accelerator_commands.cc
      LOCK_RELEASED
      MOVE_ACTIVE_WINDOW_BETWEEN_DISPLAYS
      POWER_RELEASED
    
    Bug: b:207089719
    Test: ash_unittests && DUT.
    
    Change-Id: I66aacccb9d85806f7cd79b75bca42e2e997b5506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907957
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050690}

--- ash/accelerators/accelerator_commands.cc
+++ ash/accelerators/accelerator_commands.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/display/display_configuration_controller.h"
+#include "ash/display/display_move_window_util.h"
 #include "ash/display/privacy_screen_controller.h"
 #include "ash/display/screen_orientation_controller.h"
 #include "ash/focus_cycler.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/keyboard_brightness_control_delegate.h"
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/palette/palette_tray.h"
+#include "ash/system/power/power_button_controller.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/time/calendar_metrics.h"
 #include "ash/system/time/calendar_model.h"
@@ -481,6 +483,11 @@ void LaunchLastApp() {
   Shelf::LaunchShelfItem(-1);
 }
 
+void LockPressed(bool pressed) {
+  Shell::Get()->power_button_controller()->OnLockButtonEvent(pressed,
+                                                             base::TimeTicks());
+}
+
 void LockScreen() {
   Shell::Get()->session_controller()->LockScreen();
 }
@@ -596,6 +603,10 @@ void MoveActiveItem(bool going_left) {
   }
 }
 
+void MoveActiveWindowBetweenDisplays() {
+  display_move_window_util::HandleMoveActiveWindowBetweenDisplays();
+}
+
 void NewDesk() {
   auto* desks_controller = DesksController::Get();
   if (!desks_controller->CanCreateDesks()) {
@@ -653,6 +664,11 @@ void OpenFileManager() {
 
 void OpenHelp() {
   NewWindowDelegate::GetInstance()->OpenGetHelp();
+}
+
+void PowerPressed(bool pressed) {
+  Shell::Get()->power_button_controller()->OnPowerButtonEvent(
+      pressed, base::TimeTicks());
 }
 
 void RemoveCurrentDesk() {
