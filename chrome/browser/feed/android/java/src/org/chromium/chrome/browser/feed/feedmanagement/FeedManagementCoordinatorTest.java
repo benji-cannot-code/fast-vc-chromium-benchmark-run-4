0649456504a00b5757d477c15090cc45e27092bd
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-09-23 09:34:49 +0000

    Revert "Use setDisplayHomeAsUp for backarrow implementation rather than custom navigation set up"
    This reverts commit 2e2c78fe3d711e1fa1b7e95d2d7504e89d8ae93f.
    
    Reason for revert: https://crbug.com/1367200
    
    Original change's description:
    > Use setDisplayHomeAsUp for backarrow implementation rather than custom navigation set up
    >
    > Bug: b/248118670
    > Change-Id: Ib0f71d64821b9736c0b02ab4ffbed103fa31a6bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3912116
    > Reviewed-by: Peter Williamson <petewil@chromium.org>
    > Commit-Queue: Cathy Li <chili@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1050063}
    
    Bug: 1367200,b/248118670
    Change-Id: Ia91653363a6caec07c8e58fd9408577aa12cb550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916005
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050560}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementCoordinatorTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementCoordinatorTest.java
@@ -8,6 +8,10 @@ package org.chromium.chrome.browser.feed.feedmanagement;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Before;
@@ -22,6 +26,7 @@ import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.feed.FeedServiceBridge;
 import org.chromium.chrome.browser.feed.FeedServiceBridgeJni;
 import org.chromium.chrome.browser.feed.StreamKind;
+import org.chromium.chrome.browser.feed.test.R;
 import org.chromium.ui.base.TestActivity;
 
 /**
@@ -57,5 +62,25 @@ public class FeedManagementCoordinatorTest {
     @Test
     public void testConstruction() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testBackArrow() {
+        View outerView = mFeedManagementCoordinator.getView();
+        // Send a click to the back arrow.
+        // Note that finding the back arrow view is ugly because it doesn't
+        // have an ID.
+        boolean clicked = false;
+        ViewGroup actionBar = outerView.findViewById(R.id.action_bar);
+        for (int i = 0; i < actionBar.getChildCount(); i++) {
+            try {
+                AppCompatImageButton button = (AppCompatImageButton) actionBar.getChildAt(i);
+                button.performClick();
+                clicked = true;
+            } catch (ClassCastException e) {
+            }
+        }
+
+        assertTrue(clicked);
     }
 }
--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementCoordinatorTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementCoordinatorTest.java
@@ -9,6 +9,10 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.verify;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.After;
@@ -21,6 +25,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.chrome.browser.feed.test.R;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedBridge;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedBridgeJni;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -75,5 +80,25 @@ public class FollowManagementCoordinatorTest {
     @Test
     public void testConstruction() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testBackArrow() {
+        View outerView = mFollowManagementCoordinator.getView();
+        // Send a click to the back arrow.
+        // Note that finding the back arrow view is ugly because it doesn't
+        // have an ID.
+        boolean clicked = false;
+        ViewGroup actionBar = (ViewGroup) outerView.findViewById(R.id.action_bar);
+        for (int i = 0; i < actionBar.getChildCount(); i++) {
+            try {
+                AppCompatImageButton button = (AppCompatImageButton) actionBar.getChildAt(i);
+                button.performClick();
+                clicked = true;
+            } catch (ClassCastException e) {
+            }
+        }
+
+        assertTrue(clicked);
     }
 }
