b83353faa3b577f4d28b435ecf250449aa626c39
Xiaoling Bao <xiaolingbao@chromium.org>
2022-09-23 01:55:05 +0000

    Add test for offline install from legacy updater.
    Bug: 1286584
    Change-Id: I03cdb6d3dc445a5ebb08a9484a7d1a733e5b676f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913860
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050487}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -323,8 +323,8 @@ class IntegrationTest : public ::testing::Test {
 
   void ExpectLastStarted() { test_commands_->ExpectLastStarted(); }
 
-  void RunOfflineInstall(bool is_silent_install) {
-    test_commands_->RunOfflineInstall(is_silent_install);
+  void RunOfflineInstall(bool is_legacy_install, bool is_silent_install) {
+    test_commands_->RunOfflineInstall(is_legacy_install, is_silent_install);
   }
 
   scoped_refptr<IntegrationTestCommands> test_commands_;
@@ -905,14 +905,21 @@ TEST_F(IntegrationTest, RecoveryNoUpdater) {
 TEST_F(IntegrationTest, OfflineInstall) {
   Install();
   ExpectInstalled();
-  RunOfflineInstall(/*is_silent_install=*/false);
+  RunOfflineInstall(/*is_legacy_install=*/false, /*is_silent_install=*/false);
   Uninstall();
 }
 
 TEST_F(IntegrationTest, SilentOfflineInstall) {
   Install();
   ExpectInstalled();
-  RunOfflineInstall(/*is_silent_install=*/true);
+  RunOfflineInstall(/*is_legacy_install=*/false, /*is_silent_install=*/true);
+  Uninstall();
+}
+
+TEST_F(IntegrationTest, LegacySilentOfflineInstall) {
+  Install();
+  ExpectInstalled();
+  RunOfflineInstall(/*is_legacy_install=*/true, /*is_silent_install=*/true);
   Uninstall();
 }
 
