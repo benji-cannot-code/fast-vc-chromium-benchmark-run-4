b96b8f90440c8d0d162ea4ed8a6bea5f31004ad4
Hira Mahmood <hiramahmood@google.com>
2022-09-23 17:55:27 +0000

    Create initial AppStoreRatingSceneAgentTest class.
    This change creates the initial unit test class for
    AppStoreRatingSceneAgent. The tests are not yet created and will come in
    a later CL.
    
    Bug: 1359065
    Change-Id: I409eab6fef8f140af72122577ccdfc6ee255bd2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915064
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050722}

---
+++ ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent_unittest.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Test fixture for testing AppStoreRatingSceneAgent class.
+class AppStoreRatingSceneAgentTest : public PlatformTest {
+ protected:
+  AppStoreRatingSceneAgentTest() {
+    test_scene_agent_ = [[AppStoreRatingSceneAgent alloc] init];
+  }
+
+  ~AppStoreRatingSceneAgentTest() override {}
+
+  AppStoreRatingSceneAgent* test_scene_agent_;
+};
