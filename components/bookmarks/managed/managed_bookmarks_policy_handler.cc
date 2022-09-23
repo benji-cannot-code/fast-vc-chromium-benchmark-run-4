b4745ead0e60b746a45e00814e28cfb0de01e1dd
Maksim Ivanov <emaxx@chromium.org>
2022-09-23 13:36:51 +0000

    Use Value::Take...() in /components/bookmarks
    Refactor to use the new base::Value
    TakeString/TakeDict/TakeList methods that transfer ownership
    in a clearer way and allow clang-tidy catch misuses.
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1360570
    Change-Id: Ie220df937465f0f34f9594412c8862f83dc9919d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913060
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050609}

--- components/bookmarks/managed/managed_bookmarks_policy_handler.cc
+++ components/bookmarks/managed/managed_bookmarks_policy_handler.cc
@@ -42,7 +42,7 @@ void ManagedBookmarksPolicyHandler::ApplyPolicySettings(
 
   prefs->SetString(prefs::kManagedBookmarksFolderName,
                    GetFolderName(value->GetList()));
-  base::Value::List filtered(FilterBookmarks(std::move(value->GetList())));
+  base::Value::List filtered(FilterBookmarks(std::move(*value).TakeList()));
   prefs->SetValue(prefs::kManagedBookmarks, base::Value(std::move(filtered)));
 }
 
