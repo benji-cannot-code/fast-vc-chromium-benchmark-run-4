0c56b8e8ae644cde04b90a5c9c5e27eb2e8d403b
Mikel Astiz <mastiz@chromium.org>
2022-09-23 14:12:28 +0000

    [favicon] Update unit-tests to exercise desired case
    No behavioral changes, just unit-tests are updated.
    
    As a result of the argument renaming in https://crrev.com/c/3696447,
    these unit-tests didn't test the scenarios they intend to test. This
    patch fixes the naming issue and adds one additional test to
    FaviconSourceTestWithLegacyFormat.
    
    Ideally the tests themselves should assert this better, but doing so
    requires introducing significant test-only complexity and APIs.
    
    Change-Id: I1b70a6f615d3e59bd82177e4f9884891d12a130c
    Bug: 1354197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914217
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050627}

--- chrome/browser/ui/webui/favicon_source_unittest.cc
+++ chrome/browser/ui/webui/favicon_source_unittest.cc
@@ -211,6 +211,20 @@ TEST_F(FaviconSourceTestWithLegacyFormat,
                            extensions::Manifest::TYPE_EXTENSION, 0);
 }
 
+TEST_F(FaviconSourceTestWithLegacyFormat, ShouldNotQueryIfDesiredSizeTooLarge) {
+  EXPECT_CALL(*mock_history_ui_favicon_request_handler_,
+              GetRawFaviconForPageURL)
+      .Times(0);
+  EXPECT_CALL(*mock_favicon_service_, GetRawFavicon).Times(0);
+  EXPECT_CALL(*mock_favicon_service_, GetRawFaviconForPageURL).Times(0);
+
+  // 1000x scale factor runs into the max cap.
+  source()->StartDataRequest(
+      GURL(
+          base::StrCat({kDummyPrefix, "size/16@1000x/https://www.google.com"})),
+      test_web_contents_getter_, base::DoNothing());
+}
+
 TEST_F(FaviconSourceTestWithLegacyFormat,
        ShouldRecordFaviconResourceHistogram_ExtensionOrigin) {
   scoped_refptr<const extensions::Extension> extension =
@@ -265,7 +279,7 @@ TEST_F(FaviconSourceTestWithFavicon2Format,
   source()->StartDataRequest(
       GURL(base::StrCat(
           {kDummyPrefix,
-           "?size=16&scale_factor=1x&pageUrl=https%3A%2F%2Fwww.google."
+           "?size=16&scaleFactor=1x&pageUrl=https%3A%2F%2Fwww.google."
            "com&allowGoogleServerFallback=0"})),
       test_web_contents_getter_, base::DoNothing());
 }
@@ -282,7 +296,7 @@ TEST_F(FaviconSourceTestWithFavicon2Format,
   source()->StartDataRequest(
       GURL(base::StrCat(
           {kDummyPrefix,
-           "?size=16&scale_factor=1x&pageUrl=https%3A%2F%2Fwww.google."
+           "?size=16&scaleFactor=1x&pageUrl=https%3A%2F%2Fwww.google."
            "com&allowGoogleServerFallback=1"})),
       test_web_contents_getter_, base::DoNothing());
 }
@@ -300,7 +314,7 @@ TEST_F(
   source()->StartDataRequest(
       GURL(base::StrCat(
           {kDummyPrefix,
-           "?size=16&scale_factor=1x&pageUrl=https%3A%2F%2Fwww.google."
+           "?size=16&scaleFactor=1x&pageUrl=https%3A%2F%2Fwww.google."
            "com&allowGoogleServerFallback=1"})),
       test_web_contents_getter_, base::DoNothing());
 }
@@ -313,8 +327,10 @@ TEST_F(FaviconSourceTestWithFavicon2Format,
   EXPECT_CALL(*mock_favicon_service_, GetRawFavicon).Times(0);
   EXPECT_CALL(*mock_favicon_service_, GetRawFaviconForPageURL).Times(0);
 
-  // 100x scale factor runs into the max cap.
+  // 1000x scale factor runs into the max cap.
   source()->StartDataRequest(
-      GURL(base::StrCat({kDummyPrefix, "size/16@100x/https://www.google.com"})),
+      GURL(base::StrCat(
+          {kDummyPrefix,
+           "?size=16&scaleFactor=1000x&pageUrl=https%3A%2F%2Fwww.google.com"})),
       test_web_contents_getter_, base::DoNothing());
 }
