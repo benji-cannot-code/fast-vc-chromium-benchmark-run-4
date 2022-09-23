826e6b45305180421da7175eb98048c3e305ad20
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-09-23 00:23:03 +0000

    Revert "Remove temporary IsEmpty() of WTF classes"
    This reverts commit b1599c0060749d88e1dfe2451894563183284881.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision b1599c0060749d88e1dfe2451894563183284881 as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8802277144329236977
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fb1599c0060749d88e1dfe2451894563183284881&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+b1599c0060749d88e1dfe2451894563183284881
    
    Original change's description:
    > Remove temporary IsEmpty() of WTF classes
    > 
    > Change-Id: I88d8bb59a0eafe733376e81a495889083888da5b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914134
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1050460}
    
    
    Change-Id: I4a73d343a8a6e898540baa8585a0b97b98098546
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915368
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Owners-Override: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050464}

--- third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
+++ third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
@@ -139,7 +139,7 @@ void PressureObserver::ReportToCallback(ExecutionContext* execution_context) {
     return;
 
   // Cleared by takeRecords, for example.
-  if (records_.empty())
+  if (records_.IsEmpty())
     return;
 
   HeapVector<Member<PressureRecord>, kMaxQueuedRecords> records;
