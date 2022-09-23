cdc5db661a95271d21a6fdcf27c146a7703fff13
S. Ganesh <ganesh@chromium.org>
2022-09-23 17:27:19 +0000

    Stop all GoogleUpdate instances
    This CL adds logic to stop all GoogleUpdate.exe processes in case they
    do not respond to the shutdown event implemented in crrev/1050262.
    
    Bug: 1290496
    Change-Id: I4b4e0b58ef2713d9f1e2afc3a7f3b7e496c58d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914242
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050705}

--- chrome/updater/win/win_util.cc
+++ chrome/updater/win/win_util.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/free_deleter.h"
 #include "base/path_service.h"
+#include "base/process/kill.h"
 #include "base/process/process.h"
 #include "base/process/process_iterator.h"
 #include "base/ranges/algorithm.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/time/time.h"
 #include "base/win/atl.h"
 #include "base/win/registry.h"
 #include "base/win/scoped_handle.h"
@@ -724,7 +726,7 @@ absl::optional<base::FilePath> GetGoogleUpdateExePath(UpdaterScope scope) {
     return absl::nullopt;
   }
 
-  return goopdate_dir.AppendASCII("GoogleUpdate.exe");
+  return goopdate_dir.AppendASCII(base::WideToASCII(kLegacyExeName));
 }
 
 HRESULT DisableCOMExceptionHandling() {
@@ -908,6 +910,57 @@ base::ScopedClosureRunner SignalShutdownEvent(UpdaterScope scope) {
   shutdown_event->Signal();
   return base::ScopedClosureRunner(
       base::BindOnce(&base::WaitableEvent::Reset, std::move(shutdown_event)));
+}
+
+bool IsShutdownEventSignaled(UpdaterScope scope) {
+  NamedObjectAttributes attr;
+  GetNamedObjectAttributes(kShutdownEvent, scope, &attr);
+
+  base::win::ScopedHandle event_handle(
+      ::OpenEvent(EVENT_ALL_ACCESS, false, attr.name.c_str()));
+  if (!event_handle.IsValid())
+    return false;
+
+  base::WaitableEvent event(std::move(event_handle));
+  return event.IsSignaled();
+}
+
+bool StopGoogleUpdateProcesses(UpdaterScope scope) {
+  // Filters processes running under `path_prefix`.
+  class PathPrefixProcessFilter : public base::ProcessFilter {
+   public:
+    explicit PathPrefixProcessFilter(const base::FilePath& path_prefix)
+        : path_prefix_(path_prefix) {}
+
+    bool Includes(const base::ProcessEntry& entry) const override {
+      base::Process process(::OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,
+                                          false, entry.th32ProcessID));
+      if (!process.IsValid())
+        return false;
+
+      DWORD path_len = MAX_PATH;
+      wchar_t path_string[MAX_PATH];
+      if (!::QueryFullProcessImageName(process.Handle(), 0, path_string,
+                                       &path_len)) {
+        return false;
+      }
+
+      return path_prefix_.IsParent(base::FilePath(path_string));
+    }
+
+   private:
+    const base::FilePath path_prefix_;
+  };
+
+  constexpr base::TimeDelta kShutdownWaitSeconds = base::Seconds(45);
+
+  absl::optional<base::FilePath> target = GetGoogleUpdateExePath(scope);
+  if (!target)
+    return false;
+
+  PathPrefixProcessFilter path_prefix_filter(target->DirName());
+  return base::CleanupProcesses(kLegacyExeName, kShutdownWaitSeconds, -1,
+                                &path_prefix_filter);
 }
 
 }  // namespace updater
