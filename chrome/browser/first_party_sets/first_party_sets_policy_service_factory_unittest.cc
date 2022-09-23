e8bd518074c366ad4b841c06ad6d3f8b7c02e5df
Maksim Ivanov <emaxx@chromium.org>
2022-09-23 13:47:54 +0000

    Use Value::Take...() in /chrome/browser/first_party_sets
    Refactor to use the new base::Value
    TakeString/TakeDict/TakeList methods that transfer ownership
    in a clearer way and allow clang-tidy catch misuses.
    
    This CL was uploaded by git cl split.
    
    R=shuuran@chromium.org
    
    Bug: 1360570
    Change-Id: I6bb8d2477b909b9fff7a536365c711b2a5c0084b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914407
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050615}

--- chrome/browser/first_party_sets/first_party_sets_policy_service_factory_unittest.cc
+++ chrome/browser/first_party_sets/first_party_sets_policy_service_factory_unittest.cc
@@ -71,7 +71,7 @@ TEST_F(FirstPartySetsPolicyServiceFactoryTest, EnabledWithPolicy) {
   profile.GetPrefs()->SetBoolean(prefs::kPrivacySandboxFirstPartySetsEnabled,
                                  true);
   profile.GetPrefs()->SetDict(first_party_sets::kFirstPartySetsOverrides,
-                              std::move(empty_lists.GetDict()));
+                              std::move(empty_lists).TakeDict());
 
   const base::Value::Dict* policy =
       FirstPartySetsPolicyServiceFactory::GetPolicyIfEnabled(profile);
