872c6c309ce04156a399eb4af7ecc133b8e667ac
Angus L. M. McLean IV <angusmclean@chromium.org>
2022-09-23 22:34:33 +0000

    holding space exp: Add new pinned files placeholder strings
    Bug: 1363339
    Change-Id: Ia680c0c518969b52d94778a2932bed80cee89c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911013
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050798}

--- chrome/browser/ui/ash/holding_space/holding_space_client_impl.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_client_impl.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/holding_space/holding_space_keyed_service.h"
 #include "chrome/browser/ui/ash/holding_space/holding_space_keyed_service_factory.h"
 #include "chrome/browser/ui/ash/holding_space/holding_space_util.h"
+#include "components/drive/drive_pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "net/base/mime_util.h"
 #include "storage/browser/file_system/file_system_context.h"
 
@@ -139,6 +141,10 @@ base::FilePath HoldingSpaceClientImpl::CrackFileSystemUrl(
   return file_manager::util::GetFileManagerFileSystemContext(profile_)
       ->CrackURLInFirstPartyContext(file_system_url)
       .path();
+}
+
+bool HoldingSpaceClientImpl::IsDriveDisabled() const {
+  return profile_->GetPrefs()->GetBoolean(drive::prefs::kDisableDrive);
 }
 
 void HoldingSpaceClientImpl::OpenDownloads(SuccessCallback callback) {
