70f378824cc77c62afb51276ac55b373a3353c9d
Patrick Noland <pnoland@google.com>
2022-09-23 01:38:40 +0000

    [Toolbar] Fix crash on post-destroy search engine change
    Bug: 1366664
    Change-Id: I8e2e4742a2f48a82f123ad89804758dd11b00494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915145
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050482}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -335,6 +335,11 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
     @Override
     void destroy() {
         cancelAnimations();
+        Handler handler = getHandler();
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
+
         super.destroy();
     }
 
@@ -2158,12 +2163,11 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         // TODO(tedchoc): Move away from updating based on the search engine change and instead
         //                add the toolbar as a listener to the NewTabPage and udpate only when
         //                it notifies the listeners that it has changed its state.
-        post(new Runnable() {
-            @Override
-            public void run() {
-                updateVisualsForLocationBarState();
-                updateNtpAnimationState();
-            }
+        Handler handler = getHandler();
+        if (handler == null) return;
+        handler.post(() -> {
+            updateVisualsForLocationBarState();
+            updateNtpAnimationState();
         });
     }
 
