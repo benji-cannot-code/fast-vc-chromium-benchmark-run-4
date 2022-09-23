9817f2dc5b0e51036bd92c54262209c8909d5c02
Sean Maher <spvw@chromium.org>
2022-09-23 22:43:13 +0000

    Task Posting v3: migrate (Sequenced|Thread)TaskRunnerHandle
    To migrate to v3 task posting API, this CL moves moves
    (Sequenced|Thread)TaskRunnerHandle::Get() to
    (Sequenced|SingleThread)TaskRunner::GetCurrentDefault()
    and related classes. For more detail on this migration, refer to
    https://docs.google.com/document/d/1tssusPykvx3g0gvbvU4HxGyn3MjJlIylnsH13-Tv6s4/edit#bookmark=id.1umyn7v5a4p
    
    The blink codebase's presubmits were substantially changed. This was
    done because the presubmit did not parse in-class identifiers, and so
    there was no way of banning inner classes/methods/identifiers of a
    class which is allowed.
    
    The reason this was needed for this CL is that
    base::(Sequenced|SingleThread)TaskRunners are permitted in blink, but
    we need to ban (Sequenced|SingleThread)TaskRunner::GetCurrentDefault()
    and [...]::CurrentDefaultHandle to ensure that tasks in blink remain
    properly labeled.
    
    To this end, a new type of identifier (in-class identifiers, such as
    nested::namespace::ClassName::EnumClassName) is parsed by the
    presubmit, and a new kind of rule can be added to the _CONFIG file to
    allow/deny them. They are parsed very similarly to the existing
    identifiers, but crucially, they are allowed by default, whereas
    ClassNames are denied by default.
    
    In addition, the tests for the presubmit were mostly rewritten to
    properly test whether the identifiers in the test are actually
    allowed/disallowed. Instead of simply testing to see whether an
    identifier is present in _CONFIG in the presubmit file, the
    presubmit's functions are called in the same way as happens during the
    presubmit to make sure the code being run actually works.
    
    Finally, it also tests that the identifiers tested are fully parsed
    (a handful of them weren't because they were either preceded by '::'
    or in-class) by the presubmit.
    
    The task runner handle classes still exist in the codebase, and no
    existing Chromium code has been modified, as semantics have been
    preserved despite the implementation change.
    
    The implementation migration will then take place in future CLs.
    Because the two APIs expose the same functionality, the codebase
    can be incrementally migrated.
    
    Bug: 1026641
    Change-Id: I7ea7bb36f30ecd3cdefedcffd8bae16118b0f3d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804911
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050813}

--- base/task/sequenced_task_runner.cc
+++ base/task/sequenced_task_runner.cc
@@ -8,10 +8,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/no_destructor.h"
 #include "base/task/default_delayed_task_handle_delegate.h"
+#include "base/threading/thread_local.h"
 #include "base/time/time.h"
 
 namespace base {
+
+namespace {
+
+ThreadLocalPointer<SequencedTaskRunner::CurrentDefaultHandle>&
+CurrentDefaultHandleTls() {
+  static NoDestructor<
+      ThreadLocalPointer<SequencedTaskRunner::CurrentDefaultHandle>>
+      instance;
+  return *instance;
+}
+
+}  // namespace
 
 bool SequencedTaskRunner::PostNonNestableTask(const Location& from_here,
                                               OnceClosure task) {
@@ -71,6 +85,36 @@ bool SequencedTaskRunner::PostDelayedTaskAt(
                          delayed_run_time.is_null()
                              ? base::TimeDelta()
                              : delayed_run_time - TimeTicks::Now());
+}
+
+// static
+const scoped_refptr<SequencedTaskRunner>&
+SequencedTaskRunner::GetCurrentDefault() {
+  const CurrentDefaultHandle* current_default = CurrentDefaultHandleTls().Get();
+  CHECK(current_default)
+      << "Error: This caller requires a sequenced context (i.e. the current "
+         "task needs to run from a SequencedTaskRunner). If you're in a test "
+         "refer to //docs/threading_and_tasks_testing.md.";
+  return current_default->task_runner_;
+}
+
+// static
+bool SequencedTaskRunner::HasCurrentDefault() {
+  return !!CurrentDefaultHandleTls().Get();
+}
+
+SequencedTaskRunner::CurrentDefaultHandle::CurrentDefaultHandle(
+    scoped_refptr<SequencedTaskRunner> task_runner)
+    : task_runner_(std::move(task_runner)) {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
+  DCHECK(!SequencedTaskRunner::HasCurrentDefault());
+  CurrentDefaultHandleTls().Set(this);
+}
+
+SequencedTaskRunner::CurrentDefaultHandle::~CurrentDefaultHandle() {
+  DCHECK(task_runner_->RunsTasksInCurrentSequence());
+  DCHECK_EQ(CurrentDefaultHandleTls().Get(), this);
+  CurrentDefaultHandleTls().Set(nullptr);
 }
 
 bool SequencedTaskRunner::DeleteOrReleaseSoonInternal(
