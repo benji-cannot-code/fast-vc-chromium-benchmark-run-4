d3e3d6352f8190332d33545c6d7d8fdc7e2b929b
Ernesto Izquierdo Clua <eic@google.com>
2022-09-23 19:07:21 +0000

    [CVC] Fix Autofill CVC Prompt Confirm Button Style
    This patch corrects the style of the Confirm button in the new CVC
    prompt.
    
    Fixed: 1366818
    Change-Id: I189dd303796dc7855eec442e78d45cdcadcd67cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913445
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050752}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
@@ -316,7 +316,7 @@ const char kFooterDummyLinkTarget[] = "about:blank";
       base::SysUTF16ToNSString(_bridge->GetController()->GetOkButtonLabel());
   UIBarButtonItem* confirmButton =
       [[UIBarButtonItem alloc] initWithTitle:confirmButtonText
-                                       style:UIBarButtonItemStylePlain
+                                       style:UIBarButtonItemStyleDone
                                       target:self
                                       action:@selector(onVerifyTapped)];
   [confirmButton setTitleTextAttributes:@{
