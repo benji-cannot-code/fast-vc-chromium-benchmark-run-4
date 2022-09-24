869b4d76faf35d562e92c2b3628d0571c06e2afd
Daniel Cheng <dcheng@chromium.org>
2022-09-24 03:16:03 +0000

    Use helper macros to define base::Features in //components/subresource_filter
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=csharrison@chromium.org
    
    Bug: 1364289
    Change-Id: I3d53f996d25d7aa6c36dc22fee4fb37ef9dc5e1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917009
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050913}

--- components/subresource_filter/core/common/common_features.cc
+++ components/subresource_filter/core/common/common_features.cc
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace subresource_filter {
 
-const base::Feature kAdTagging{"AdTagging", base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kAdTagging, "AdTagging", base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace subresource_filter
