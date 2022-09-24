600f4d6674f51022e5301458acfd47dd32953b07
Daniel Cheng <dcheng@chromium.org>
2022-09-24 19:29:21 +0000

    Use helper macros to define base::Features in //cc
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    Bug: 1364289
    Change-Id: I1af9f031d2e14c41b6ab00bf5b9a8a5ad66224fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916933
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050967}

--- cc/raster/one_copy_raster_buffer_provider.cc
+++ cc/raster/one_copy_raster_buffer_provider.cc
@@ -48,9 +48,9 @@ const int kMaxBytesPerCopyOperation = 1024 * 1024 * 4;
 // at normal thread priority.
 // TODO(crbug.com/1072756): Cleanup the feature when the Stable experiment is
 // complete, on November 25, 2020.
-const base::Feature kOneCopyRasterBufferPlaybackNormalThreadPriority{
-    "OneCopyRasterBufferPlaybackNormalThreadPriority",
-    base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kOneCopyRasterBufferPlaybackNormalThreadPriority,
+             "OneCopyRasterBufferPlaybackNormalThreadPriority",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace
 
