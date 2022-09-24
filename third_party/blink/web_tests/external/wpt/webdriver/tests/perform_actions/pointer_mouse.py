02cdee59a1ab21fb570e37632a5d0bf823946b75
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-09-24 18:04:51 +0000

    Import wpt@f6a4c7e5efbd59c2c1da796abaf8bd3be3bef466
    Using wpt-import in Chromium c4be4c7e0a43171d0470b17481f6da6b76de6725.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    futhark@chromium.org, andruud@chromium.org, ericwilligers@chromium.org, shend@chromium.org:
      external/wpt/css/css-conditional
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I38c3b3eedb7bfe1fd592c1551c7297b6315202f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915252
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050961}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/pointer_mouse.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/pointer_mouse.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import pytest
 
-from webdriver.error import NoSuchWindowException, StaleElementReferenceException
+from webdriver.error import InvalidArgumentException, NoSuchWindowException, StaleElementReferenceException
 
 from tests.perform_actions.support.mouse import get_inview_center, get_viewport_rect
 from tests.perform_actions.support.refine import get_events
@@ -184,3 +184,12 @@ def test_drag_and_drop_with_draggable_element(session_new_window,
     assert "dragenter" in drag_events_captured
     assert "dragleave" in drag_events_captured
     assert "drop" in drag_events_captured
+
+
+@pytest.mark.parametrize("missing", ["x", "y"])
+def test_missing_coordinates(session, test_actions_page, mouse_chain, missing):
+    outer = session.find.css("#outer", all=False)
+    actions = mouse_chain.pointer_move(x=0, y=0, origin=outer)
+    del actions._actions[-1][missing]
+    with pytest.raises(InvalidArgumentException):
+        actions.perform()
--- third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/wheel.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/wheel.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
 
-from webdriver.error import NoSuchWindowException
+from webdriver.error import InvalidArgumentException, NoSuchWindowException
 
 from tests.perform_actions.support.refine import get_events
 from tests.support.asserts import assert_move_to_coordinates
@@ -63,3 +63,14 @@ def test_wheel_scroll_iframe(session, test_actions_scroll_page, wheel_chain):
     assert events[0]["deltaY"] >= 10
     assert events[0]["deltaZ"] == 0
     assert events[0]["target"] == "iframeContent"
+
+
+@pytest.mark.parametrize("missing", ["x", "y", "deltaX", "deltaY"])
+def test_wheel_missing_prop(session, test_actions_scroll_page, wheel_chain, missing):
+    session.execute_script("document.scrollingElement.scrollTop = 0")
+
+    outer = session.find.css("#outer", all=False)
+    actions = wheel_chain.scroll(0, 0, 5, 10, origin=outer)
+    del actions._actions[-1][missing]
+    with pytest.raises(InvalidArgumentException):
+        actions.perform()
