cf55d7e7da84bad4b7bd32d06f2dc587001449b7
Daniel Cheng <dcheng@chromium.org>
2022-09-24 03:27:02 +0000

    Use helper macros to define base::Features in //components/assist_ranker
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=charleszhao@chromium.org
    
    Bug: 1364289
    Change-Id: Ic2da534ec825b7f34e259b30f36c7cd73d3b768c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914638
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050916}

--- components/assist_ranker/predictor_config_definitions.cc
+++ components/assist_ranker/predictor_config_definitions.cc
@@ -12,8 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace assist_ranker {
 
 #if BUILDFLAG(IS_ANDROID)
-const base::Feature kContextualSearchRankerQuery{
-    "ContextualSearchRankerQuery", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kContextualSearchRankerQuery,
+             "ContextualSearchRankerQuery",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
 
--- components/assist_ranker/predictor_config_definitions.h
+++ components/assist_ranker/predictor_config_definitions.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace assist_ranker {
 
 #if BUILDFLAG(IS_ANDROID)
-extern const base::Feature kContextualSearchRankerQuery;
+BASE_DECLARE_FEATURE(kContextualSearchRankerQuery);
 const PredictorConfig GetContextualSearchPredictorConfig();
 #endif  // BUILDFLAG(IS_ANDROID)
 
