833eb578197ee2a264f8771deb8b871f5d33bf95
Daniel Cheng <dcheng@chromium.org>
2022-09-24 02:58:42 +0000

    Use helper macros to define base::Features in //components/site_isolation
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=alexmos@chromium.org
    
    Bug: 1364289
    Change-Id: I7edd445c068f0cf9f7bb7e210e3d851ff0dc0273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916736
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050910}

--- components/site_isolation/features.h
+++ components/site_isolation/features.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace site_isolation {
 namespace features {
 
-extern const base::Feature kCacheSiteIsolationMemoryThreshold;
-extern const base::Feature kSiteIsolationForPasswordSites;
-extern const base::Feature kSiteIsolationForOAuthSites;
-extern const base::Feature kSiteIsolationMemoryThresholds;
+BASE_DECLARE_FEATURE(kCacheSiteIsolationMemoryThreshold);
+BASE_DECLARE_FEATURE(kSiteIsolationForPasswordSites);
+BASE_DECLARE_FEATURE(kSiteIsolationForOAuthSites);
+BASE_DECLARE_FEATURE(kSiteIsolationMemoryThresholds);
 extern const char kStrictSiteIsolationMemoryThresholdParamName[];
 extern const char kPartialSiteIsolationMemoryThresholdParamName[];
 
