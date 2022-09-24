568651a6152ca950718f41ced8bacbb6af3f1687
Daniel Cheng <dcheng@chromium.org>
2022-09-24 02:12:42 +0000

    Use helper macros to define base::Features in //components/discardable_memory
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=penghuang@chromium.org
    
    Bug: 1364289
    Change-Id: Idc6616a86efc86536e8030450a3ac4750c66859d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917031
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050901}

--- components/discardable_memory/client/client_discardable_shared_memory_manager.cc
+++ components/discardable_memory/client/client_discardable_shared_memory_manager.cc
@@ -30,8 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace discardable_memory {
 namespace {
 
-const base::Feature kShorterPeriodicPurge{"ShorterPeriodicPurge",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kShorterPeriodicPurge,
+             "ShorterPeriodicPurge",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Global atomic to generate unique discardable shared memory IDs.
 base::AtomicSequenceNumber g_next_discardable_shared_memory_id;
--- components/discardable_memory/common/discardable_shared_memory_heap.cc
+++ components/discardable_memory/common/discardable_shared_memory_heap.cc
@@ -23,8 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace discardable_memory {
 
-const base::Feature kReleaseDiscardableFreeListPages{
-    "ReleaseDiscardableFreeListPages", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kReleaseDiscardableFreeListPages,
+             "ReleaseDiscardableFreeListPages",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
 
