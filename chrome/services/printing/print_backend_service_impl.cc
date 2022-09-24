eb8b174c1b14ac51ab3a70c790e323ae7a15b75c
Alan Screen <awscreen@chromium.org>
2022-09-24 02:29:22 +0000

    Ensure Linux UI is initialized for OOP printing
    PrintingContextLinux::UpdatePrinterSettings() exits early for updating
    settings when there is no LinuxUI instance present.  This reports
    success, even though some print settings do not get updated further by
    PrintDialogGtk.
    
    Since a full LinuxUI is not supported for Linux services at this time,
    utilize a stub version for the UI.  This enables printing to proceed
    farther without skipping to fill in some information and unblocks the
    rest of the OOP print pipeline.
    
    This only unblocks the OOP portions of the print pipeline which do not
    need to display a system UI dialog.
    
    Bug: 809738, 1361982
    Change-Id: I188b4b8a819618dd5ff35e06a5f7150d028b3655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906648
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050907}

--- chrome/services/printing/print_backend_service_impl.cc
+++ chrome/services/printing/print_backend_service_impl.cc
@@ -42,6 +42,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/backend/cups_connection_pool.h"
 #endif
 
+#if BUILDFLAG(IS_LINUX)
+#include "base/no_destructor.h"
+#include "ui/linux/linux_ui.h"
+#include "ui/linux/linux_ui_delegate_stub.h"
+#include "ui/linux/linux_ui_factory.h"
+#endif
+
 #if BUILDFLAG(IS_WIN)
 #include "base/containers/queue.h"
 #include "base/win/win_util.h"
@@ -55,6 +62,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace printing {
 
 namespace {
+
+#if BUILDFLAG(IS_LINUX)
+void InstantiateLinuxUiDelegate() {
+  // TODO(crbug.com/809738)  Until a real UI can be used in a utility process,
+  // need to use the stub version.
+  static base::NoDestructor<ui::LinuxUiDelegateStub> linux_ui_delegate;
+}
+#endif
 
 scoped_refptr<base::SequencedTaskRunner> GetPrintingTaskRunner() {
   static constexpr base::TaskTraits kTraits = {
@@ -408,6 +423,13 @@ void PrintBackendServiceImpl::Init(const std::string& locale) {
   // `InitCommon()`.
   InitializeProcessForPrinting();
   print_backend_ = PrintBackend::CreateInstance(locale);
+#if BUILDFLAG(IS_LINUX)
+  // Test framework already initializes the UI, so this should not go in
+  // `InitCommon()`.  Additionally, low-level Linux UI is not needed when tests
+  // are using `TestPrintingContext`.
+  InstantiateLinuxUiDelegate();
+  ui::LinuxUi::SetInstance(ui::GetDefaultLinuxUi());
+#endif
 
   InitCommon(locale);
 }
