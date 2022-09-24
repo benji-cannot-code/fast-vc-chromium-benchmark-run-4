48231bc55a2038b0d1d5991244623196598e6cd9
Jimmy <jimmyxgong@chromium.org>
2022-09-24 00:31:52 +0000

    shortcuts: Instantiate AshAcceleratorConfiguration in shell
    By having Shell own AshAcceleratorConfiguration, mojo services in
    ash/webui will be able to retrieve and interact with ash accelerators.
    
    Bug: b:216049298
    Test: ash_unittests
    Change-Id: I962d4d7414d0d08782320cd258ea08f7708032c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906476
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050882}

--- ash/accelerators/accelerator_controller_impl.h
+++ ash/accelerators/accelerator_controller_impl.h
@@ -107,7 +107,7 @@ class ASH_EXPORT AcceleratorControllerImpl
     AcceleratorControllerImpl* controller_;  // Not owned.
   };
 
-  AcceleratorControllerImpl();
+  explicit AcceleratorControllerImpl(AshAcceleratorConfiguration* config);
   AcceleratorControllerImpl(const AcceleratorControllerImpl&) = delete;
   AcceleratorControllerImpl& operator=(const AcceleratorControllerImpl&) =
       delete;
@@ -194,7 +194,7 @@ class ASH_EXPORT AcceleratorControllerImpl
   bool ShouldPreventProcessingAccelerators() const;
 
   AshAcceleratorConfiguration* accelerator_configuration() {
-    return accelerator_configuration_.get();
+    return accelerator_configuration_;
   }
 
  private:
@@ -248,7 +248,7 @@ class ASH_EXPORT AcceleratorControllerImpl
   std::unique_ptr<AcceleratorHistoryImpl> accelerator_history_;
 
   // Manages all accelerator mappings.
-  std::unique_ptr<AshAcceleratorConfiguration> accelerator_configuration_;
+  AshAcceleratorConfiguration* accelerator_configuration_;
 
   // Handles the exit accelerator which requires a double press to exit and
   // shows a popup with an explanation.
--- ash/shell.cc
+++ ash/shell.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/accelerators/accelerator_controller_impl.h"
+#include "ash/accelerators/ash_accelerator_configuration.h"
 #include "ash/accelerators/ash_focus_manager_factory.h"
 #include "ash/accelerators/magnifier_key_scroller.h"
 #include "ash/accelerators/pre_target_accelerator_handler.h"
@@ -1218,7 +1219,12 @@ void Shell::Init(
   cursor_manager_->SetDisplay(
       display::Screen::GetScreen()->GetPrimaryDisplay());
 
-  accelerator_controller_ = std::make_unique<AcceleratorControllerImpl>();
+  ash_accelerator_configuration_ =
+      std::make_unique<AshAcceleratorConfiguration>();
+  ash_accelerator_configuration_->Initialize();
+  accelerator_controller_ = std::make_unique<AcceleratorControllerImpl>(
+      ash_accelerator_configuration_.get());
+
   clipboard_history_controller_ =
       std::make_unique<ClipboardHistoryControllerImpl>();
 
