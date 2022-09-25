321a345b299ce26239bf04268d831955305501ba
Arthur Milchior <arthurmilchior@google.com>
2022-09-25 17:25:23 +0000

    [iOS] moves consent_auditor_factory.h to its folder
    This is now consistent with the non-ios chrome
    
    Fixed: 850428
    Change-Id: I02f6cd93300582f4a602f5748d47a76c1669da05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913207
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051011}

--- ios/chrome/browser/consent_auditor/consent_auditor_factory.mm
+++ ios/chrome/browser/consent_auditor/consent_auditor_factory.mm
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(crbug.com/850428): Move this and .h back to
-// ios/chrome/browser/consent_auditor.
-
-#import "ios/chrome/browser/sync/consent_auditor_factory.h"
+#import "ios/chrome/browser/consent_auditor/consent_auditor_factory.h"
 
 #import <memory>
 #import <utility>
