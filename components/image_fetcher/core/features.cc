e4d03b5349ea233d0bc9240ddeff912ef3f8d600
Daniel Cheng <dcheng@chromium.org>
2022-09-26 22:54:01 +0000

    Use helper macros to define base::Features in //components/image_fetcher
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=skym@chromium.org
    
    Bug: 1364289
    Change-Id: If6f672bf2ca797d105880037e76e5e13c16ef2ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916977
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051459}

--- components/image_fetcher/core/features.cc
+++ components/image_fetcher/core/features.cc
@@ -10,8 +10,9 @@ namespace features {
 
 #if BUILDFLAG(IS_ANDROID)
 // Enables batching decoding of related images in a single process.
-const base::Feature kBatchImageDecoding{"BatchImageDecoding",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kBatchImageDecoding,
+             "BatchImageDecoding",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 }  // namespace features
