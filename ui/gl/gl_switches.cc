569ca93594db960507c58a27705c963e86ff73b0
Dominik Röttsches <drott@chromium.org>
2022-09-26 11:31:22 +0000

    Revert system info based vulkan activation
    This reverts commit 256ea9ac6239cda3f1dbd08558dc07613e271798 and bc354479436757ab0f55f1d1f7d72af0d3237fb9
    
    Reason for revert: Suspected to have caused breakage on Linux CFI, compare https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20CFI/23538/overview
    
    [ RUN      ] All/GLHelperPixelReadbackTest.ScaleTest/0
    ../../third_party/angle/src/gpu_info_util/SystemInfo_vulkan.cpp:59:13: runtime error: control flow integrity check for type 'VkResult (unsigned int *)' failed during indirect function call
    (/b/s/w/ir/out/Release/./libvulkan.so.1+0x78150): note: (unknown) defined here
    ../../third_party/angle/src/gpu_info_util/SystemInfo_vulkan.cpp:59:13: note: check failed in /b/s/w/ir/out/Release/gl_tests, destination function located in /b/s/w/ir/out/Release/./libvulkan.so.1
    
    Original change's description:
    > Disable Vulkan for AMDVLK driver
    >
    > AMDVLK driver is buggy, so disable Chrome vulkan feature when
    > AMDVLK is used on Linux.
    >
    > LOW_COVERAGE_REASON=gl_switches.cc doesn't contains complex logic
    >
    > Bug: 1340081
    > Change-Id: I93db242a4438e29245e2c26a4234e90e1e1a4f1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907884
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Reviewed-by: Geoff Lang <geofflang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1050655}
    
    Bug: 1340081
    Change-Id: Ib9940b72c4481fb5c17438c6d338fd9e8fd28485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916479
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Quick-Run: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051147}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #endif
 
-#if BUILDFLAG(IS_LINUX)
-#include <vulkan/vulkan_core.h>
-#include "third_party/angle/src/gpu_info_util/SystemInfo.h"  // nogncheck
-#endif
-
 namespace gl {
 
 const char kGLImplementationDesktopName[] = "desktop";
@@ -290,27 +285,6 @@ bool IsDefaultANGLEVulkan() {
       base::android::SDK_VERSION_Q)
     return false;
 #endif  // BUILDFLAG(IS_ANDROID)
-#if BUILDFLAG(IS_LINUX)
-  angle::SystemInfo system_info;
-  if (!angle::GetSystemInfoVulkan(&system_info))
-    return false;
-
-  if (static_cast<size_t>(system_info.activeGPUIndex) >=
-      system_info.gpus.size()) {
-    return false;
-  }
-
-  const auto& active_gpu = system_info.gpus[system_info.activeGPUIndex];
-
-  // Vulkan 1.1 is required.
-  if (active_gpu.driverApiVersion < VK_VERSION_1_1)
-    return false;
-
-  // AMDVLK driver is buggy, so disable Vulkan with AMDVLK for now.
-  // crbug.com/1340081
-  if (active_gpu.driverId == VK_DRIVER_ID_AMD_OPEN_SOURCE)
-    return false;
-#endif
   return base::FeatureList::IsEnabled(kDefaultANGLEVulkan);
 }
 
