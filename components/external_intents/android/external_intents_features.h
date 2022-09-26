7e5a5336290e268324db541316a0c29ed5b0775a
Daniel Cheng <dcheng@chromium.org>
2022-09-26 20:46:44 +0000

    Use helper macros to define base::Features in //components/external_intents
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=rsesek@chromium.org
    
    Bug: 1364289
    Change-Id: Iafd19ea96dac3213ce97c5d8b78318b8bb381797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917001
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051384}

--- components/external_intents/android/external_intents_features.h
+++ components/external_intents/android/external_intents_features.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace external_intents {
 
 // Alphabetical:
-extern const base::Feature kAutofillAssistantGoogleInitiatorOriginCheck;
-extern const base::Feature kBlockExternalFormSubmitWithoutGesture;
-extern const base::Feature kExternalNavigationDebugLogs;
+BASE_DECLARE_FEATURE(kAutofillAssistantGoogleInitiatorOriginCheck);
+BASE_DECLARE_FEATURE(kBlockExternalFormSubmitWithoutGesture);
+BASE_DECLARE_FEATURE(kExternalNavigationDebugLogs);
 
 }  // namespace external_intents
 
