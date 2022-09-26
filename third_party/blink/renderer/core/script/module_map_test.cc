875d2292cd27354e180ea0b52845d38559bba04d
Dave Tapuska <dtapuska@chromium.org>
2022-09-26 14:10:38 +0000

    Use the kNetworking task runner for ModuleMapTest
    Load the task runner from the execution context. This makes it
    incompatible with the TestingPlatformSupportWithMockScheduler but it
    isn't clear why that is needed anyways.
    
    BUG=624696
    
    Change-Id: Icbc22986fc3785cfd9224d9d545f4bf5045b1b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914070
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051198}

--- third_party/blink/renderer/core/script/module_map_test.cc
+++ third_party/blink/renderer/core/script/module_map_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_source_location_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/loader/modulescript/module_script_fetch_request.h"
 #include "third_party/blink/renderer/core/loader/modulescript/module_script_fetcher.h"
 #include "third_party/blink/renderer/core/loader/modulescript/module_script_loader_client.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_client_settings_object_snapshot.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support_with_mock_scheduler.h"
+#include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 
 namespace blink {
@@ -140,7 +142,7 @@ class ModuleMapTestModulator final : public DummyModulator {
   }
 
   base::SingleThreadTaskRunner* TaskRunner() override {
-    return Thread::Current()->GetDeprecatedTaskRunner().get();
+    return task_runner_.get();
   }
 
   struct TestRequest final : public GarbageCollected<TestRequest> {
@@ -161,11 +163,14 @@ class ModuleMapTestModulator final : public DummyModulator {
   HeapVector<Member<TestRequest>> test_requests_;
 
   Member<ScriptState> script_state_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   Member<TestModuleRecordResolver> resolver_;
 };
 
 ModuleMapTestModulator::ModuleMapTestModulator(ScriptState* script_state)
     : script_state_(script_state),
+      task_runner_(ExecutionContext::From(script_state_)
+                       ->GetTaskRunner(TaskType::kNetworking)),
       resolver_(MakeGarbageCollected<TestModuleRecordResolver>()) {}
 
 void ModuleMapTestModulator::Trace(Visitor* visitor) const {
@@ -212,10 +217,6 @@ void ModuleMapTest::TearDown() {
 }
 
 TEST_F(ModuleMapTest, sequentialRequests) {
-  ScopedTestingPlatformSupport<TestingPlatformSupportWithMockScheduler>
-      platform;
-  platform->AdvanceClockSeconds(1.);  // For non-zero DocumentParserTimings
-
   KURL url(NullURL(), "https://example.com/foo.js");
 
   // First request
@@ -228,7 +229,7 @@ TEST_F(ModuleMapTest, sequentialRequests) {
   Modulator()->ResolveFetches();
   EXPECT_FALSE(client->WasNotifyFinished())
       << "fetchSingleModuleScript shouldn't complete synchronously";
-  platform->RunUntilIdle();
+  test::RunPendingTasks();
 
   EXPECT_EQ(Modulator()
                 ->GetTestModuleRecordResolver()
@@ -247,7 +248,7 @@ TEST_F(ModuleMapTest, sequentialRequests) {
   Modulator()->ResolveFetches();
   EXPECT_FALSE(client2->WasNotifyFinished())
       << "fetchSingleModuleScript shouldn't complete synchronously";
-  platform->RunUntilIdle();
+  test::RunPendingTasks();
 
   EXPECT_EQ(Modulator()
                 ->GetTestModuleRecordResolver()
@@ -259,10 +260,6 @@ TEST_F(ModuleMapTest, sequentialRequests) {
 }
 
 TEST_F(ModuleMapTest, concurrentRequestsShouldJoin) {
-  ScopedTestingPlatformSupport<TestingPlatformSupportWithMockScheduler>
-      platform;
-  platform->AdvanceClockSeconds(1.);  // For non-zero DocumentParserTimings
-
   KURL url(NullURL(), "https://example.com/foo.js");
 
   // First request
@@ -286,7 +283,7 @@ TEST_F(ModuleMapTest, concurrentRequestsShouldJoin) {
       << "fetchSingleModuleScript shouldn't complete synchronously";
   EXPECT_FALSE(client2->WasNotifyFinished())
       << "fetchSingleModuleScript shouldn't complete synchronously";
-  platform->RunUntilIdle();
+  test::RunPendingTasks();
 
   EXPECT_EQ(Modulator()
                 ->GetTestModuleRecordResolver()
