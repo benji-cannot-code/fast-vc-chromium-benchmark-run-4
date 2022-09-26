2d928cc459c9c5a6f54021b487a2484e63de602d
Chris Harrelson <chrishtr@chromium.org>
2022-09-26 23:25:50 +0000

    [a11y-during-render] Move dirty_object list into Blink
    Bug: 1068668, 1342801
    
    Change-Id: Ie88eee26ba09431364758dd4509a476575befa93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911618
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051477}

--- third_party/blink/public/web/web_ax_object.h
+++ third_party/blink/public/web/web_ax_object.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/public/web/web_ax_enums.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
+#include "ui/accessibility/ax_event_intent.h"
 #include "ui/accessibility/ax_mode.h"
 
 namespace gfx {
@@ -333,10 +334,12 @@ class BLINK_EXPORT WebAXObject {
                          gfx::Transform& container_transform,
                          bool* clips_children = nullptr) const;
 
-  // Retrieves a vector of all WebAXObjects in this document whose
-  // bounding boxes may have changed since the last query. Sends that vector
-  // via mojo to the browser process.
-  void SerializeLocationChanges() const;
+  // Marks ths object as dirty (needing serialization). If subtree is true,
+  // the entire AX subtree should be invalidated as well.
+  void MarkDirty(bool subtree,
+                 ax::mojom::EventFrom event_from,
+                 ax::mojom::Action event_from_action,
+                 std::vector<ui::AXEventIntent> event_intents) const;
 
   // Exchanges a WebAXObject with another.
   void Swap(WebAXObject& other);
