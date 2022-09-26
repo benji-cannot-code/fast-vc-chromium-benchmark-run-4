a6ff65f38ee17980df1519c397050b4a9b847ba4
Daniel Cheng <dcheng@chromium.org>
2022-09-26 16:10:14 +0000

    Use helper macros to define base::Features in //components/paint_preview
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=fredmello@chromium.org
    
    Bug: 1364289
    Change-Id: I79de0844152fbf2210b16e547c16513b80a77333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917075
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051245}

--- components/paint_preview/features/features.cc
+++ components/paint_preview/features/features.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace paint_preview {
 
-const base::Feature kPaintPreviewDemo{"PaintPreviewDemo",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kPaintPreviewDemo,
+             "PaintPreviewDemo",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
-const base::Feature kPaintPreviewShowOnStartup{
-    "PaintPreviewShowOnStartup", base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kPaintPreviewShowOnStartup,
+             "PaintPreviewShowOnStartup",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace paint_preview
