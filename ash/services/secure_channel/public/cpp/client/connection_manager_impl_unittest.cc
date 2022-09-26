f31828bff69da3cb69438c322b0c522c6d65c22c
Crisrael Lucero <crisrael@google.com>
2022-09-26 18:14:11 +0000

    [Phone Hub] Extend connection latency metric
    The PhoneHub.Connectivity.Latency metric was not able to accurately log
    the duration it took for the phone to connect because the histogram used
    only supported up to 10 seconds, whereas we allow up to 60 seconds to
    connect. This change deprecates the old metric and introduces
    PhoneHub.Connection.Latency, which supports medium histograms (up to 3
    minutes) and has a more intuitive name.
    
    Test: Updated unit tests and manually verified using chrome://histograms
    Fixed: b/247596895
    Change-Id: I458a8dc3d74b602ad6ee066eaefe6949449ccf3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908237
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051301}

--- ash/services/secure_channel/public/cpp/client/connection_manager_impl_unittest.cc
+++ ash/services/secure_channel/public/cpp/client/connection_manager_impl_unittest.cc
@@ -37,7 +37,7 @@ const char kConnectionResultMetricName[] = "PhoneHub.Connection.Result";
 const char kConnectionFailureReasonMetricName[] =
     "PhoneHub.Connection.Result.FailureReason";
 const char kConnectionDurationMetricName[] = "PhoneHub.Connection.Duration";
-const char kConnectionLatencyMetricName[] = "PhoneHub.Connectivity.Latency";
+const char kConnectionLatencyMetricName[] = "PhoneHub.Connection.Latency";
 
 constexpr base::TimeDelta kFakeConnectionLatencyTime(base::Seconds(3u));
 constexpr base::TimeDelta kFakeConnectionDurationTime(base::Seconds(10u));
@@ -80,7 +80,7 @@ class TestMetricsRecorder : public NearbyMetricsRecorder {
     base::UmaHistogramEnumeration(kConnectionFailureReasonMetricName, reason);
   }
   void RecordConnectionLatency(const base::TimeDelta latency) override {
-    base::UmaHistogramTimes(kConnectionLatencyMetricName, latency);
+    base::UmaHistogramMediumTimes(kConnectionLatencyMetricName, latency);
   }
   void RecordConnectionDuration(const base::TimeDelta duration) override {
     base::UmaHistogramLongTimes100(kConnectionDurationMetricName, duration);
