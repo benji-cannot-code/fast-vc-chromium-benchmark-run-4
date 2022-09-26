e4b637fd62672bf68a3050a003e23583c737fefd
Matt Menke <mmenke@chromium.org>
2022-09-26 15:45:52 +0000

    Migrate DictionaryPrefUpdate usage in update code.
    DictionaryPrefUpdate is deprecated. ScopedDictPrefUpdate only allows
    access via the newer Value::Dict API, and returns references rather
    than pointers (The pref system itself has CHECKs/DCHECKs that the
    value is registered and of the right type, so consumers do not need to
    check that).
    
    Also fix some consumers that shouldn't be using ScopedDictPrefUpdate.
    Consumers that only read the value should not use it, to avoid sending
    a bogus pref update notification, and consumers that completely
    overwrite the value should use be using PrefService::SetDict(), to
    reduce code size (and potentially slightly improve perf).
    
    Bug: 1362719
    Change-Id: Ic6044adc361215cc8a7f94a8e5ed1e8c4d195f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915492
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051210}

--- chrome/updater/persisted_data.h
+++ chrome/updater/persisted_data.h
@@ -120,7 +120,8 @@ class PersistedData : public base::RefCountedThreadSafe<PersistedData> {
   const base::Value::Dict* GetAppKey(const std::string& id) const;
 
   // Returns an existing or newly created app key under a root pref.
-  base::Value* GetOrCreateAppKey(const std::string& id, base::Value* root);
+  base::Value::Dict* GetOrCreateAppKey(const std::string& id,
+                                       base::Value::Dict& root);
 
   std::string GetString(const std::string& id, const std::string& key) const;
   void SetString(const std::string& id,
