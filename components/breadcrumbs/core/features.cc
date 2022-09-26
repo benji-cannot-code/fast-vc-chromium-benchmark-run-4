a005dd068c122ebebabb03737ad5ab7385e69732
Daniel Cheng <dcheng@chromium.org>
2022-09-26 19:30:48 +0000

    Use helper macros to define base::Features in //components/breadcrumbs
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=jessemckenna@google.com
    
    Bug: 1364289
    Change-Id: I12ff0770aa40a49acd257177d8033862e27b9e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916951
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051346}

--- components/breadcrumbs/core/features.cc
+++ components/breadcrumbs/core/features.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace breadcrumbs {
 
-const base::Feature kLogBreadcrumbs{"LogBreadcrumbs",
-                                    base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kLogBreadcrumbs,
+             "LogBreadcrumbs",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace breadcrumbs
