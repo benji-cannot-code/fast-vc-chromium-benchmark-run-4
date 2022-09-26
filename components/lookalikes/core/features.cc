d64ba7c01a8e82d2ed6f64a9c68a8badfdbafb2a
Daniel Cheng <dcheng@chromium.org>
2022-09-26 19:33:05 +0000

    Use helper macros to define base::Features in //components/lookalikes
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=meacer@chromium.org
    
    Bug: 1364289
    Change-Id: Ia9e7469ef515ddd9c08d29da9fb3e1bf5fb6defb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917050
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051349}

--- components/lookalikes/core/features.cc
+++ components/lookalikes/core/features.cc
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace lookalikes {
 namespace features {
 
-const base::Feature kLookalikeDigitalAssetLinks{
-    "LookalikeDigitalAssetLinks", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kLookalikeDigitalAssetLinks,
+             "LookalikeDigitalAssetLinks",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const char kLookalikeDigitalAssetLinksTimeoutParameter[] = "timeout";
 
-const base::Feature kDetectComboSquattingLookalikes{
-    "ComboSquattingLookalikes", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kDetectComboSquattingLookalikes,
+             "ComboSquattingLookalikes",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace features
 }  // namespace lookalikes
