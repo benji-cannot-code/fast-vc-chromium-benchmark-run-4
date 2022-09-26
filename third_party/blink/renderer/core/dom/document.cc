a690b7c1aa78dcac1e711b14fa45a0ad0b71b65d
Dominic Farolino <dom@chromium.org>
2022-09-26 17:46:12 +0000

    Remove TreeScope::ResetTreeScope
    This CL removes TreeScope::ResetTreeScope, and stops referencing it
    from the blink::Document::open. It essentially undoes the work from
    https://codereview.chromium.org/2727563002, as mentioned in
    https://crbug.com/941913#c3 to make our implementation
    of document.open() spec- and test-compliant again.
    
    Bug: 941913
    Change-Id: I4661a836a9939e59d947d1ae8cc6890b5eba6095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081640
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051286}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3337,10 +3337,6 @@ void Document::open() {
   // Window object as well.
   RemoveAllEventListenersRecursively();
 
-  ResetTreeScope();
-  if (GetFrame())
-    GetFrame()->Selection().Clear();
-
   // Create a new HTML parser and associate it with |document|.
   //
   // Set the current document readiness of |document| to "loading".
--- third_party/blink/renderer/core/dom/tree_scope.cc
+++ third_party/blink/renderer/core/dom/tree_scope.cc
@@ -99,10 +99,6 @@ TreeScope::TreeScope(Document& document,
 
 TreeScope::~TreeScope() = default;
 
-void TreeScope::ResetTreeScope() {
-  selection_ = nullptr;
-}
-
 bool TreeScope::IsInclusiveAncestorTreeScopeOf(const TreeScope& scope) const {
   for (const TreeScope* current = &scope; current;
        current = current->ParentTreeScope()) {
