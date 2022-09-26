72643814892183fe901ba67e58f737cb00d59979
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-09-26 17:49:33 +0000

    Update Sign in detection in Sync E2E tests
    When the "DelayConsentLevelUpgrade" feature is enabled, the consent
    level changes to `kSignIn` while the Sync opt-in is offered, instead
    of `kSync` previously. This change was not taken into account and
    was making E2E tests fail.
    
    Bug: 1353733
    Change-Id: I630ba1abe02520a9e5ea0b20a2c081fe9c20ff31
    Fixed: 1362728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913193
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051288}

--- chrome/browser/ui/webui/signin/login_ui_test_utils.cc
+++ chrome/browser/ui/webui/signin/login_ui_test_utils.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/signin/signin_promo.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/chrome_pages.h"
@@ -87,7 +88,10 @@ class SignInObserver : public signin::IdentityManager::Observer {
 
   void OnPrimaryAccountChanged(
       const signin::PrimaryAccountChangeEvent& event) override {
-    if (event.GetEventTypeFor(signin::ConsentLevel::kSync) !=
+    if (event.GetEventTypeFor(
+            base::FeatureList::IsEnabled(kDelayConsentLevelUpgrade)
+                ? signin::ConsentLevel::kSignin
+                : signin::ConsentLevel::kSync) !=
         signin::PrimaryAccountChangeEvent::Type::kSet) {
       return;
     }
