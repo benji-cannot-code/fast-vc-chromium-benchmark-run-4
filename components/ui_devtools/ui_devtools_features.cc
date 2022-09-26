ae013f0c2574554240880efb20a3f16524ac7fa1
Daniel Cheng <dcheng@chromium.org>
2022-09-26 06:34:31 +0000

    Use helper macros to define base::Features in //components/ui_devtools
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=kerenzhu@chromium.org
    
    Bug: 1364289
    Change-Id: I545fa1cbf21e5f7e1769ab80bc45c90442ef885e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917080
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051081}

--- components/ui_devtools/ui_devtools_features.cc
+++ components/ui_devtools/ui_devtools_features.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui_devtools {
 
-const base::Feature kUIDebugToolsEnableSyntheticEvents{
-    "ui-debug-tools-enable-synthetic-events",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kUIDebugToolsEnableSyntheticEvents,
+             "ui-debug-tools-enable-synthetic-events",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace ui_devtools
