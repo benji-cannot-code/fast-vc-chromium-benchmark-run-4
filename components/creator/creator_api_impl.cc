6b45c15c4fe9f6c1627dc19491f6adb3eca15ed5
Jeffrey Cohen <jeffreycohen@google.com>
2022-09-26 21:30:53 +0000

    [Cormorant] add creator API stub
    Bug: 1362726
    Change-Id: I9bac53db815accc580f180e9d71f88e16221f0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905848
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051410}

---
+++ components/creator/creator_api_impl.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/creator/creator_api_impl.h"
+
+#include "components/creator/public/creator_api.h"
+#include "url/gurl.h"
+
+namespace creator {
+Creator CreatorApiImpl::GetCreator(int channel_id) {
+  // TODO(crbug.com/1365645) query actual data for given channel id.
+  return Creator{/*url=*/GURL("example.com"), /*title=*/u"Example Domain"};
+}
+}  // namespace creator
