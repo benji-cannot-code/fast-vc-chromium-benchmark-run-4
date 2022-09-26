483a2c8a981ae5522624a38e3407127423df9c00
Calder Kitagawa <ckitagawa@chromium.org>
2022-09-26 20:22:25 +0000

    Most Recent Tab on Background Close Cleanup Flag
    This feature is now launched. Cleanup the flag as 1 milestone has passed
    since enabling on ToT.
    
    Fixed: 1319286
    Change-Id: I9a6a37888e4e56d5e1911cdc1b38a15c54487957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916514
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051371}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -253,7 +253,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kKitKatSupported,
     &kLensCameraAssistedSearch,
     &kLensOnQuickActionSearchWidget,
-    &kMostRecentTabOnBackgroundCloseTab,
     &kNewInstanceFromDraggedLink,
     &kNewTabPageTilesTitleWrapAround,
     &kNewWindowAppMenu,
@@ -812,10 +811,6 @@ BASE_FEATURE(kSearchEnginePromoNewDevice,
 
 BASE_FEATURE(kSearchEnginePromoNewDeviceV2,
              "SearchEnginePromo.NewDeviceVer2",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kMostRecentTabOnBackgroundCloseTab,
-             "MostRecentTabOnBackgroundCloseTab",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNewInstanceFromDraggedLink,
