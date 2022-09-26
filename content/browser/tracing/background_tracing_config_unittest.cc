e213f097dc49e17a8c6f837eba01d505232261a5
Maksim Ivanov <emaxx@chromium.org>
2022-09-26 09:09:23 +0000

    Use Value::Take...() in /content/browser/tracing
    Refactor to use the new base::Value
    TakeString/TakeDict/TakeList methods that transfer ownership
    in a clearer way and allow clang-tidy catch misuses.
    
    This CL was uploaded by git cl split.
    
    R=altimin@chromium.org
    
    Bug: 1360570
    Change-Id: I4f9ce73f08f5c8df369c2003ec2b9f37bce9b891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913396
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051107}

--- content/browser/tracing/background_tracing_config_unittest.cc
+++ content/browser/tracing/background_tracing_config_unittest.cc
@@ -49,7 +49,7 @@ std::unique_ptr<BackgroundTracingConfigImpl> ReadFromJSONString(
 
   std::unique_ptr<BackgroundTracingConfigImpl> config(
       static_cast<BackgroundTracingConfigImpl*>(
-          BackgroundTracingConfig::FromDict(std::move(json_value->GetDict()))
+          BackgroundTracingConfig::FromDict(std::move(*json_value).TakeDict())
               .release()));
   return config;
 }
