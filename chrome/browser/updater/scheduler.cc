ace4e87907ab0009ab92d898310036dd6326a142
Joshua Pawlicki <waffles@chromium.org>
2022-09-26 16:14:06 +0000

    Add MayBlock to backup updater task traits.
    Fixed: 1367560
    Change-Id: Id2494b113bca0f8cef596f9466d0828120421da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915498
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051247}

--- chrome/browser/updater/scheduler.cc
+++ chrome/browser/updater/scheduler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/updater/scheduler.h"
 
 #include "base/bind.h"
+#include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
 
@@ -17,7 +18,8 @@ void RunAndReschedule() {
   DoPeriodicTasks(base::BindOnce([]() {
     base::ThreadPool::PostDelayedTask(
         FROM_HERE,
-        {base::TaskPriority::BEST_EFFORT, base::WithBaseSyncPrimitives(),
+        {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
+         base::WithBaseSyncPrimitives(),
          base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
         base::BindOnce(&RunAndReschedule), base::Hours(5));
   }));
@@ -28,7 +30,8 @@ void RunAndReschedule() {
 void SchedulePeriodicTasks() {
   base::ThreadPool::PostDelayedTask(
       FROM_HERE,
-      {base::TaskPriority::BEST_EFFORT, base::WithBaseSyncPrimitives(),
+      {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
+       base::WithBaseSyncPrimitives(),
        base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
       base::BindOnce(&RunAndReschedule), base::Minutes(5));
 }
