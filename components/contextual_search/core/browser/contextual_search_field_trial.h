986a43b5ec30bde05e4c14025f5819002f96edde
Daniel Cheng <dcheng@chromium.org>
2022-09-26 22:25:24 +0000

    Use helper macros to define base::Features in //components/contextual_search
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=donnd@chromium.org
    
    Bug: 1364289
    Change-Id: I671f22f1b93065d0b2b895f0872cd42faaa69c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914477
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051442}

--- components/contextual_search/core/browser/contextual_search_field_trial.h
+++ components/contextual_search/core/browser/contextual_search_field_trial.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 
-extern const base::Feature kContextualSearchDebug;
+BASE_DECLARE_FEATURE(kContextualSearchDebug);
 
 // Manages the Contextual Search field trials for native classes.
 class ContextualSearchFieldTrial {
