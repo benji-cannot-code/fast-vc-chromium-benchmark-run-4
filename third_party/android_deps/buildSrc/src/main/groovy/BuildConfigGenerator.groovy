b55084bc7f82d3ea03d505dd9f2f9215e1910889
Avi Drissman <avi@chromium.org>
2022-09-26 20:47:15 +0000

    Update copyright headers in third_party/android_deps[_autorolled]/
    These files are local files generated to manage Android deps.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: If39e0d34a77139d7ad190d02233533212222558d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918834
    Owners-Override: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051385}

--- third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -102,7 +102,7 @@ class BuildConfigGenerator extends DefaultTask {
     static final String AUTOROLLED_REPO_PATH = 'third_party/android_deps_autorolled'
 
     static final String COPYRIGHT_HEADER = '''\
-        # Copyright 2021 The Chromium Authors. All rights reserved.
+        # Copyright 2021 The Chromium Authors
         # Use of this source code is governed by a BSD-style license that can be
         # found in the LICENSE file.
     '''.stripIndent()
@@ -212,7 +212,7 @@ class BuildConfigGenerator extends DefaultTask {
         // NOTE: Keep the copyright year 2018 until this generated code is updated, avoiding annual churn of all
         //       cipd.yaml files.
         return """\
-            # Copyright 2018 The Chromium Authors. All rights reserved.
+            # Copyright 2018 The Chromium Authors
             # Use of this source code is governed by a BSD-style license that can be
             # found in the LICENSE file.
 
--- third_party/android_deps_autorolled/fetch_all_autorolled.py
+++ third_party/android_deps_autorolled/fetch_all_autorolled.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python3
 
-# Copyright 2021 The Chromium Authors. All rights reserved.
+# Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """A script to generate build.gradle from template and run fetch_all.py
