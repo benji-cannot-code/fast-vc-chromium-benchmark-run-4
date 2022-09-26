6f84d5b3c79d958050bba3077830ae4c623be6cf
Simon Ziegltrum <ziegltrum@google.com>
2022-09-26 15:47:58 +0000

    OOBE: Oobe_network modularized screen
    Fix: b:243383121
    Change-Id: Ie04e26132f458b900a93cb31a7ee7bd2073b6d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916477
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051213}

--- chrome/browser/resources/chromeos/login/screens/oobe/oobe_network.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/oobe_network.js
@@ -7,7 +7,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for displaying network selection OOBE dialog.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/paper-styles/color.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/ash/common/network/network_list.js';
+import '../../components/buttons/oobe_back_button.m.js';
+import '../../components/buttons/oobe_next_button.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+
+import {NetworkList} from '//resources/ash/common/network/network_list_types.js';
+import {I18nBehavior} from '//resources/cr_elements/i18n_behavior.js';
+import {assert} from '//resources/js/assert.js';
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {NetworkSelectLogin} from '../../components/network_select_login.m.js';
+
 
 /**
  * @constructor
@@ -15,9 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {LoginScreenBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
  */
-const NetworkScreenBase = Polymer.mixinBehaviors(
+const NetworkScreenBase = mixinBehaviors(
     [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    Polymer.Element);
+    PolymerElement);
 /**
  * @typedef {{
  *   networkSelectLogin:  NetworkSelectLogin,
@@ -35,7 +53,9 @@ class NetworkScreen extends NetworkScreenBase {
     return 'oobe-network-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
