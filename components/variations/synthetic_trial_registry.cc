b991e90919e882948e5f8ebaabca6d6b47421a7b
Daniel Cheng <dcheng@chromium.org>
2022-09-26 23:28:05 +0000

    Use helper macros to define base::Features in //components/variations
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=rkaplow@chromium.org
    
    Bug: 1364289
    Change-Id: Ib580dbd1cb34c320b6a887658f63b0e2e6687035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916737
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051480}

--- components/variations/synthetic_trial_registry.cc
+++ components/variations/synthetic_trial_registry.cc
@@ -17,8 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace variations {
 namespace internal {
 
-const base::Feature kExternalExperimentAllowlist{
-    "ExternalExperimentAllowlist", base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kExternalExperimentAllowlist,
+             "ExternalExperimentAllowlist",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace internal
 
