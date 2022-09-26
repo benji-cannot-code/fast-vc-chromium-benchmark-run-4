21ec8d98cbb533b528e955e2449b9bc81b2a40d7
Scott Violet <sky@chromium.org>
2022-09-26 19:02:20 +0000

    blink: stops using background html tokenizer for empty docs
    Empty docs generally have no data, so that using a background
    tokenizer for them is overkill. This patch prevents the token
    producer from using the background tokenizer for empty docs.
    My understanding is empty docs may be written to (via
    document.write()), but that already stops background tokenizer,
    so no loss there.
    
    BUG=1345267
    
    Change-Id: I642fd6307cc0c733c990becde088e2981f2609d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914507
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051331}

--- third_party/blink/renderer/core/html/parser/html_document_parser.h
+++ third_party/blink/renderer/core/html/parser/html_document_parser.h
@@ -113,6 +113,8 @@ class CORE_EXPORT HTMLDocumentParser : public ScriptableDocumentParser,
 
   bool DidPumpTokenizerForTesting() const { return did_pump_tokenizer_; }
 
+  HTMLTokenProducer* TokenProducerForTesting() { return token_producer_.get(); }
+
   unsigned GetChunkCountForTesting() const;
 
   TextPosition GetTextPosition() const final;
