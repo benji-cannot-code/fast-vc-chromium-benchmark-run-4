2b1affdb85f3e671abf941df522583001280b3a3
Anders Hartvoll Ruud <andruud@chromium.org>
2022-09-26 10:16:54 +0000

    [@viewport] Remove support for unused features
    Our @viewport rules only use the following: min-width, max-width,
    min-zoom, and max-zoom. Remove support for everything else to avoid
    new dependencies on @viewport features.
    
    Bug: 1365873
    Change-Id: I70518d3357f3261e08a2e59abd3addc64af0a173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907743
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051132}

--- third_party/blink/renderer/core/css/resolver/viewport_style_resolver.h
+++ third_party/blink/renderer/core/css/resolver/viewport_style_resolver.h
@@ -69,7 +69,6 @@ class CORE_EXPORT ViewportStyleResolver final
 
   float ViewportArgumentValue(CSSPropertyID) const;
   Length ViewportLengthValue(CSSPropertyID);
-  mojom::ViewportFit ViewportFitValue() const;
 
   Member<Document> document_;
   Member<MutableCSSPropertyValueSet> property_set_;
