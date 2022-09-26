c87d682198020ec7ac62bd56b5a5c9834927de22
Daniel Cheng <dcheng@chromium.org>
2022-09-26 16:01:59 +0000

    Use helper macros to define base::Features in //components/url_param_filter
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=bcl@chromium.org
    
    Bug: 1364289
    Change-Id: I7cb1f586ea5a1fc780044d22d2347cd259371b3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917090
    Quick-Run: Brian Lefler <bcl@chromium.org>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051237}

--- components/url_param_filter/core/features.cc
+++ components/url_param_filter/core/features.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url_param_filter::features {
 
-const base::Feature kIncognitoParamFilterEnabled{
-    "IncognitoParamFilterEnabled", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kIncognitoParamFilterEnabled,
+             "IncognitoParamFilterEnabled",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace url_param_filter::features
--- components/url_param_filter/core/features.h
+++ components/url_param_filter/core/features.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url_param_filter::features {
 
-extern const base::Feature kIncognitoParamFilterEnabled;
+BASE_DECLARE_FEATURE(kIncognitoParamFilterEnabled);
 
 }  // namespace url_param_filter::features
 
