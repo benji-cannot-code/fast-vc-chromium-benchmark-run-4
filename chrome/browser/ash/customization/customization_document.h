bf38c24ed6278dbd2f13edddd1c192ce45d21810
Roman Sorokin <rsorokin@chromium.org>
2022-09-26 22:32:18 +0000

    [Code Health] Replace DictionaryValue with base::Value::Dict in
    chrome/browser/ash/customization/customization_document*
    
    Bug: 1366865, 1187061
    Change-Id: Ia00b486043a137ed37182496976774a4f03dea85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913925
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051446}

--- chrome/browser/ash/customization/customization_document.h
+++ chrome/browser/ash/customization/customization_document.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
 class PrefRegistrySimple;
@@ -182,7 +183,7 @@ class ServicesCustomizationDocument : public CustomizationDocument {
   bool GetDefaultWallpaperUrl(GURL* out_url) const;
 
   // Returns list of default apps.
-  std::unique_ptr<base::DictionaryValue> GetDefaultApps() const;
+  absl::optional<base::Value::Dict> GetDefaultApps() const;
 
   // Creates an extensions::ExternalLoader that will provide OEM default apps.
   // Cache of OEM default apps stored in profile preferences.
@@ -251,7 +252,7 @@ class ServicesCustomizationDocument : public CustomizationDocument {
   void OnManifestLoaded();
 
   // Returns list of default apps in ExternalProvider format.
-  static std::unique_ptr<base::DictionaryValue> GetDefaultAppsInProviderFormat(
+  static base::Value::Dict GetDefaultAppsInProviderFormat(
       const base::Value::Dict& root);
 
   // Update cached manifest for |profile|.
