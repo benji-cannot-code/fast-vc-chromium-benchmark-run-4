00b96dbaae650f278f8a1606c33000e8f5249fc9
sophiewen <sophiewen@chromium.org>
2022-09-27 23:48:47 +0000

    float: Implement tuck handle to untuck a window.
    This adds an exception to BackGestureEventHandler to prevent it from
    handling tuck handle events.
    
    Test: added
    Bug: 1358307
    Change-Id: I01bdb6423926a456b980371fb071d07196b3a432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915914
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052119}

--- ash/wm/gestures/back_gesture/back_gesture_event_handler.cc
+++ ash/wm/gestures/back_gesture/back_gesture_event_handler.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_delegate.h"
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/model/virtual_keyboard_model.h"
+#include "ash/wm/desks/desks_controller.h"
+#include "ash/wm/float/float_controller.h"
 #include "ash/wm/gestures/back_gesture/back_gesture_affordance.h"
 #include "ash/wm/gestures/back_gesture/back_gesture_contextual_nudge_controller_impl.h"
 #include "ash/wm/overview/overview_controller.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/metrics/user_metrics.h"
 #include "chromeos/ui/base/window_properties.h"
+#include "chromeos/ui/wm/features.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/wm/core/coordinate_conversion.h"
@@ -449,6 +452,24 @@ bool BackGestureEventHandler::CanStartGoingBack(
     return false;
   }
 
+  // Do not enable back gesture if `screen_location` is inside the tuck handle,
+  // let `FloatController` handle the event instead.
+  if (chromeos::wm::features::IsFloatWindowEnabled()) {
+    auto* float_controller = Shell::Get()->float_controller();
+    auto* floated_window = float_controller->FindFloatedWindowOfDesk(
+        DesksController::Get()->GetTargetActiveDesk());
+    if (floated_window &&
+        float_controller->IsFloatedWindowTuckedForTablet(floated_window)) {
+      auto* tuck_handle_widget =
+          float_controller->GetTuckHandleWidget(floated_window);
+      if (tuck_handle_widget &&
+          tuck_handle_widget->GetWindowBoundsInScreen().Contains(
+              screen_location)) {
+        return false;
+      }
+    }
+  }
+
   gfx::Rect hit_bounds_in_screen(
       display::Screen::GetScreen()
           ->GetDisplayNearestWindow(
@@ -458,6 +479,7 @@ bool BackGestureEventHandler::CanStartGoingBack(
     hit_bounds_in_screen.set_x(hit_bounds_in_screen.right() -
                                kStartGoingBackLeftEdgeInset);
   }
+
   hit_bounds_in_screen.set_width(kStartGoingBackLeftEdgeInset);
   const bool hit_in_limited_area =
       hit_bounds_in_screen.Contains(screen_location);
