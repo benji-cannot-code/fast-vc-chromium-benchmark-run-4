f2da4e63f101d370a86f86b77ec843d3f2fc3a05
Sammie Quon <sammiequon@chromium.org>
2022-09-27 00:48:45 +0000

    float: Long touch to show multitask menu.
    Fixed: 1367376
    Test: manual, added test
    Change-Id: I87e42c9d16c2b87c0461b32e0b4e63678ff2a069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916130
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Judy Wang <shidi@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051510}

--- chromeos/ui/frame/caption_buttons/frame_size_button.cc
+++ chromeos/ui/frame/caption_buttons/frame_size_button.cc
@@ -245,15 +245,8 @@ bool FrameSizeButton::OnMousePressed(const ui::MouseEvent& event) {
 
   if (IsTriggerableEvent(event)) {
     // Add a visual indicator of when snap mode will get triggered.
-    if (chromeos::wm::features::IsFloatWindowEnabled()) {
-      base::OnceClosure cancel_animation = base::BindOnce(
-          &FrameSizeButton::DestroyPieAnimation, base::Unretained(this));
-      base::OnceClosure show_multitask_menu = base::BindOnce(
-          &FrameSizeButton::OnPieAnimationCompleted, base::Unretained(this));
-      pie_animation_ = std::make_unique<PieAnimation>(
-          kPieAnimationPressDuration, std::move(cancel_animation),
-          std::move(show_multitask_menu), this);
-    }
+    StartPieAnimation(kPieAnimationPressDuration);
+
     // The minimize and close buttons are set to snap left and right when
     // snapping is enabled. Do not enable snapping if the minimize button is not
     // visible. The close button is always visible.
@@ -308,15 +301,7 @@ void FrameSizeButton::OnGestureEvent(ui::GestureEvent* event) {
     // Add a visual indicator of when snap mode will get triggered. Note that
     // order matters as the subclasses will call `StateChanged()` and we want
     // the changes there to run first.
-    if (chromeos::wm::features::IsFloatWindowEnabled()) {
-      std::pair<base::OnceClosure, base::OnceClosure> split =
-          base::SplitOnceCallback(base::BindOnce(
-              &FrameSizeButton::DestroyPieAnimation, base::Unretained(this)));
-      pie_animation_ = std::make_unique<PieAnimation>(
-          kPieAnimationPressDuration, std::move(split.first),
-          std::move(split.second), this);
-    }
-
+    StartPieAnimation(kPieAnimationPressDuration);
     return;
   }
 
@@ -345,14 +330,8 @@ void FrameSizeButton::StateChanged(views::Button::ButtonState old_state) {
     return;
 
   if (GetState() == views::Button::STATE_HOVERED && GetWidget()->IsActive()) {
-    base::OnceClosure cancel_animation = base::BindOnce(
-        &FrameSizeButton::DestroyPieAnimation, base::Unretained(this));
-    base::OnceClosure show_multitask_menu = base::BindOnce(
-        &FrameSizeButton::OnPieAnimationCompleted, base::Unretained(this));
-    pie_animation_ = std::make_unique<PieAnimation>(
-        kPieAnimationHoverDuration, std::move(cancel_animation),
-        std::move(show_multitask_menu), this);
     // On animation end we should show the multitask menu.
+    StartPieAnimation(kPieAnimationHoverDuration);
   } else if (old_state == views::Button::STATE_HOVERED) {
     pie_animation_.reset();
   }
@@ -382,6 +361,19 @@ void FrameSizeButton::StartSetButtonsToSnapModeTimer(
         FROM_HERE, base::Milliseconds(set_buttons_to_snap_mode_delay_ms_), this,
         &FrameSizeButton::AnimateButtonsToSnapMode);
   }
+}
+
+void FrameSizeButton::StartPieAnimation(base::TimeDelta duration) {
+  if (!chromeos::wm::features::IsFloatWindowEnabled())
+    return;
+
+  base::OnceClosure cancel_animation = base::BindOnce(
+      &FrameSizeButton::DestroyPieAnimation, base::Unretained(this));
+  base::OnceClosure show_multitask_menu = base::BindOnce(
+      &FrameSizeButton::OnPieAnimationCompleted, base::Unretained(this));
+  pie_animation_ =
+      std::make_unique<PieAnimation>(duration, std::move(cancel_animation),
+                                     std::move(show_multitask_menu), this);
 }
 
 void FrameSizeButton::AnimateButtonsToSnapMode() {
