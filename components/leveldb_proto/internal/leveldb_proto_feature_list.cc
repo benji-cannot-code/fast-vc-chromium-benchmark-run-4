800fc42ae204600f4eada03eaec2932049ae0f10
Daniel Cheng <dcheng@chromium.org>
2022-09-27 00:35:52 +0000

    Use helper macros to define base::Features in //components/leveldb_proto
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=ssid@chromium.org
    
    Bug: 1364289
    Change-Id: Ic0aea477a6e1df09a126f40b3f3f2556bada84a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916979
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051504}

--- components/leveldb_proto/internal/leveldb_proto_feature_list.cc
+++ components/leveldb_proto/internal/leveldb_proto_feature_list.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace leveldb_proto {
 
-const base::Feature kProtoDBSharedMigration{"ProtoDBSharedMigration",
-                                            base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kProtoDBSharedMigration,
+             "ProtoDBSharedMigration",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace leveldb_proto
