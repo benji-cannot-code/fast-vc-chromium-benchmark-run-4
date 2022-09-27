374d3618f57b3b30a04190577a9e24d98fcba46b
Yanzhu Du <yzd@google.com>
2022-09-27 20:49:00 +0000

    Add TypeScript definition for Chrome extension APIs
    Bug: b:243584674
    Change-Id: I615739547b45c4b515912939bc55b1e4e33bf63e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915920
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052041}

---
+++ tools/typescript/definitions/notifications.d.ts
@@ -0,0 +1,59 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Minimum definitions for chrome.notifications API */
+// TODO(crbug.com/1203307): Auto-generate this file.
+
+import {ChromeEvent} from './chrome_event.js';
+
+declare global {
+  export namespace chrome {
+    export namespace notifications {
+      export type TemplateType = "basic" | "image" | "list" | "progress";
+
+      export interface ButtonOptions {
+        title: string;
+        iconUrl?: string;
+      }
+
+      export interface ItemOptions {
+        title: string;
+        message: string;
+      }
+
+      export type NotificationOptions<T extends boolean = false> = {
+        contextMessage?: string;
+        priority?: number;
+        eventTime?: number;
+        buttons?: ButtonOptions[];
+        items?: ItemOptions[];
+        progress?: number;
+        isClickable?: boolean;
+        appIconMaskUrl?: string;
+        imageUrl?: string;
+        requireInteraction?: boolean;
+        silent?: boolean;
+      }
+
+      export interface NotificationButtonClickedEvent
+        extends ChromeEvent<(
+          notificationId: string,
+          buttonIndex: number) => void> { }
+
+      export var onButtonClicked: NotificationButtonClickedEvent;
+      export function create(
+        notificationId: string,
+        options: NotificationOptions<true>,
+        callback?: (notificationId: string) => void,
+      ): void;
+      export function create(
+        options: NotificationOptions<true>,
+        callback?: (notificationId: string) => void): void;
+      export function clear(
+        notificationId: string,
+        callback?: (wasCleared: boolean) => void): void;
+    }
+  }
+}
--- tools/typescript/definitions/runtime.d.ts
+++ tools/typescript/definitions/runtime.d.ts
@@ -6,10 +6,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @fileoverview Definitions for chrome.runtime API */
 // TODO(crbug.com/1203307): Auto-generate this file.
 
-declare namespace chrome {
-  export namespace runtime {
-    export let lastError: {
-      message?: string,
-    } | undefined;
+import {ChromeEvent} from './chrome_event.js';
+
+declare global {
+  export namespace chrome {
+    export namespace runtime {
+      export let lastError: {
+        message?: string,
+      } | undefined;
+
+      export interface MessageSender {
+        id?: string;
+        tab?: chrome.tabs.Tab;
+        nativeApplication?: string;
+        frameId?: number;
+        url?: string;
+        tlsChannelId?: string;
+        origin?: string;
+      }
+
+      export interface ExtensionMessageEvent
+        extends ChromeEvent<(
+          message: any,
+          sender: MessageSender,
+          sendResponse: (response?: any) => void) => void> { }
+      export var onMessageExternal: ExtensionMessageEvent;
+    }
   }
 }
