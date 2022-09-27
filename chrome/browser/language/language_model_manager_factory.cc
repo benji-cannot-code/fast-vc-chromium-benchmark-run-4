c4ec0f6129a8881c0c671f3287a2e28a68d3c3f0
Trevor Perrier <perrier@chromium.org>
2022-09-27 22:14:23 +0000

    [ULP] Add page language metrics
    This CL adds three metrics to compare the page language histogram to
    the ULP languages. The histograms are:
    
     - LanguageUsage.ULP.Initiation.AcceptLanguagesPageLanguageOverlap.Base
     - LanguageUsage.ULP.Initiation.PageLanguagesMissingFromULP
     - LanguageUsage.ULP.Initiation.PageLanguagesMissingFromULP.Count
    
    Bug: 1363833
    Change-Id: I8c4ee634135dee0098a74b57e7e044c0fd1af94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922739
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052076}

--- chrome/browser/language/language_model_manager_factory.cc
+++ chrome/browser/language/language_model_manager_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/language/url_language_histogram_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language/core/browser/locale_util.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/language/core/browser/ulp_metrics_logger.h"
+#include "components/language/core/browser/url_language_histogram.h"
 #include "components/language/core/common/language_experiments.h"
 #include "components/language/core/language_model/fluent_language_model.h"
 #include "components/language/core/language_model/ulp_language_model.h"
@@ -40,8 +42,10 @@ namespace {
 
 #if BUILDFLAG(IS_ANDROID)
 // Records per-initialization ULP-related metrics.
-void RecordULPInitMetrics(PrefService* pref_service,
-                          const std::vector<std::string>& ulp_languages) {
+void RecordULPInitMetrics(
+    PrefService* pref_service,
+    const language::UrlLanguageHistogram& page_language_histogram,
+    const std::vector<std::string>& ulp_languages) {
   language::ULPMetricsLogger logger;
 
   logger.RecordInitiationLanguageCount(ulp_languages.size());
@@ -69,8 +73,7 @@ void RecordULPInitMetrics(PrefService* pref_service,
   logger.RecordInitiationTopAcceptLanguageInULP(accept_language_status);
 
   logger.RecordInitiationAcceptLanguagesULPOverlap(
-      ULPMetricsLogger::ULPLanguagesInAcceptLanguagesRatio(accept_languages,
-                                                           ulp_languages));
+      ULPMetricsLogger::LanguagesOverlapRatio(accept_languages, ulp_languages));
 
   std::vector<std::string> never_languages_not_in_ulp =
       ULPMetricsLogger::RemoveULPLanguages(
@@ -80,12 +83,27 @@ void RecordULPInitMetrics(PrefService* pref_service,
       never_languages_not_in_ulp);
   logger.RecordInitiationNeverLanguagesMissingFromULPCount(
       never_languages_not_in_ulp.size());
+
+  std::vector<std::string> page_languages;
+  for (const language::UrlLanguageHistogram::LanguageInfo& language_info :
+       page_language_histogram.GetTopLanguages()) {
+    page_languages.emplace_back(language_info.language_code);
+  }
+  logger.RecordInitiationAcceptLanguagesPageLanguageOverlap(
+      ULPMetricsLogger::LanguagesOverlapRatio(page_languages, ulp_languages));
+  std::vector<std::string> page_languages_not_in_ulp =
+      ULPMetricsLogger::RemoveULPLanguages(page_languages, ulp_languages);
+  logger.RecordInitiationPageLanguagesMissingFromULP(page_languages_not_in_ulp);
+  logger.RecordInitiationPageLanguagesMissingFromULPCount(
+      page_languages_not_in_ulp.size());
 }
 
 void CreateAndAddULPLanguageModel(Profile* profile,
                                   std::vector<std::string> languages) {
   PrefService* pref_service = profile->GetPrefs();
-  RecordULPInitMetrics(pref_service, languages);
+  language::UrlLanguageHistogram* page_languages =
+      UrlLanguageHistogramFactory::GetForBrowserContext(profile);
+  RecordULPInitMetrics(pref_service, *page_languages, languages);
   language::LanguagePrefs(pref_service).SetULPLanguages(languages);
 
   std::unique_ptr<language::ULPLanguageModel> ulp_model =
