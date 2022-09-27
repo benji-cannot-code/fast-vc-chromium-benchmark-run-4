e4e6c3b9b9bf603002596f8832d487c6fce5654d
Antoni Dikov <adikov@google.com>
2022-09-27 22:23:49 +0000

    [CrOS Cellular] Update key where custom APNs are stored
    Store Custom APNs using different key when kApnRevamp flag is on.
    
    TEST=NetworkMetadataStoreTest and on device
    
    Bug: b:162365553
    Change-Id: I89e6f185848a212d60dab9011e232e072bb960a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3909591
    Commit-Queue: Antoni Dikov <adikov@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052078}

--- chromeos/ash/components/network/network_metadata_store.cc
+++ chromeos/ash/components/network/network_metadata_store.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/check.h"
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
+#include "components/onc/onc_constants.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
@@ -36,6 +38,7 @@ const char kOwner[] = "owner";
 const char kExternalModifications[] = "external_modifications";
 const char kBadPassword[] = "bad_password";
 const char kCustomApnList[] = "custom_apn_list";
+const char kCustomApnListV2[] = "custom_apn_list_v2";
 const char kHasFixedHiddenNetworks[] =
     "metadata_store.has_fixed_hidden_networks";
 const char kEnableTrafficCountersAutoReset[] =
@@ -62,6 +65,21 @@ bool ListContains(const base::Value::List* list, const std::string& value) {
   if (!list)
     return false;
   return std::find(list->begin(), list->end(), value) != list->end();
+}
+
+bool IsApnListValid(const base::Value& list) {
+  if (!list.is_list())
+    return false;
+
+  for (const base::Value& apn : list.GetList()) {
+    if (!apn.is_dict())
+      return false;
+
+    if (!apn.GetDict().Find(::onc::cellular_apn::kAccessPointName))
+      return false;
+  }
+
+  return true;
 }
 
 }  // namespace
@@ -478,11 +496,25 @@ bool NetworkMetadataStore::GetHasBadPassword(const std::string& network_guid) {
 
 void NetworkMetadataStore::SetCustomAPNList(const std::string& network_guid,
                                             base::Value list) {
+  if (ash::features::IsApnRevampEnabled()) {
+    if (!IsApnListValid(list)) {
+      NET_LOG(ERROR) << "network_guid: " << network_guid << std::endl
+                     << "Invalid list passed to SetCustomAPNList():" << list;
+      return;
+    }
+
+    SetPref(network_guid, kCustomApnListV2, std::move(list));
+    return;
+  }
+
   SetPref(network_guid, kCustomApnList, std::move(list));
 }
 
 const base::Value* NetworkMetadataStore::GetCustomAPNList(
     const std::string& network_guid) {
+  if (ash::features::IsApnRevampEnabled())
+    return GetPref(network_guid, kCustomApnListV2);
+
   return GetPref(network_guid, kCustomApnList);
 }
 
