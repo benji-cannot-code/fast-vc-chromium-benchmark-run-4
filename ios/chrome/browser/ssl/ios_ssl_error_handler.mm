e657ef97d40be2a49cc911ed16359f40490a4252
Sylvain Defresne <sdefresne@chromium.org>
2022-09-27 14:38:25 +0000

    [ios] Convert time duration constants to base::TimeDelta
    Using base::TimeDelta allow to specify the time unit when defining
    the constant without resorting to putting the unit in the variable
    name.
    
    This also helps converting between different units with the helper
    methods (.InSecondsF(), .InNanosecondsF(), ...).
    
    This convert ios/chrome/browser/ssl while keeping the old constant
    kSSLInterstitialDelayInSeconds available until the internal repo
    has been converted to use the new one.
    
    Bug: none
    Change-Id: I7ccef5e9a6f11872e58dc4c27a30896ed2048932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921581
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051797}

--- ios/chrome/browser/ssl/ios_ssl_error_handler.mm
+++ ios/chrome/browser/ssl/ios_ssl_error_handler.mm
@@ -30,7 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-const int64_t kSSLInterstitialDelayInSeconds = 3;
+// Keep this variable as it is used in the internal repository. Remove it
+// once the internal code has been converted to use kSSLInterstitialDelay
+// instead.
+const int64_t kSSLInterstitialDelayInSeconds =
+    kSSLInterstitialDelay.InSeconds();
 
 using captive_portal::CaptivePortalDetector;
 using security_interstitials::IOSBlockingPageTabHelper;
@@ -124,7 +128,7 @@ void IOSSSLErrorHandler::StartHandlingError() {
 
   // Default to presenting the SSL interstitial if Captive Portal detection
   // takes too long.
-  timer_.Start(FROM_HERE, base::Seconds(kSSLInterstitialDelayInSeconds), this,
+  timer_.Start(FROM_HERE, kSSLInterstitialDelay, this,
                &IOSSSLErrorHandler::ShowSSLInterstitial);
 }
 
