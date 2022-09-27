fa4da2e187c1d3df4c3a836aae7d322e560d054d
Weizhong Xia <weizhong@google.com>
2022-09-27 20:21:23 +0000

    Roll wpt javascript
    This rolls wpt javascript to latest commit at
    https://github.com/web-platform-tests/wpt.
    
    Change-Id: I3dc482f69dc1fe1da41fb87ebd3a586491ad3380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923021
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052022}

--- third_party/blink/web_tests/resources/testharness.js
+++ third_party/blink/web_tests/resources/testharness.js
@@ -1128,7 +1128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      *
      * Typically this function is called implicitly on page load; it's
      * only necessary for users to call this when either the
-     * ``explict_done`` or ``single_page`` properties have been set
+     * ``explicit_done`` or ``single_page`` properties have been set
      * via the :js:func:`setup` function.
      *
      * For single page tests this marks the test as complete and sets its status.
@@ -2720,19 +2720,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * to reduce intermittents without compromising test execution
      * speed when the condition is quickly met.
      *
-     * @example
-     * async_test(t => {
-     *  const popup = window.open("resources/coop-coep.py?coop=same-origin&coep=&navigate=about:blank");
-     *  t.add_cleanup(() => popup.close());
-     *  assert_equals(window, popup.opener);
-     *
-     *  popup.onload = t.step_func(() => {
-     *    assert_true(popup.location.href.endsWith("&navigate=about:blank"));
-     *    // Use step_wait_func_done as about:blank cannot message back.
-     *    t.step_wait_func_done(() => popup.location.href === "about:blank");
-     *  });
-     * }, "Navigating a popup to about:blank");
-     *
      * @param {Function} cond A function taking no arguments and
      *                        returning a boolean. The callback is called
      *                        when this function returns true.
@@ -2774,6 +2761,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * whenever possible since it allows the timeout to be longer
      * to reduce intermittents without compromising test execution speed
      * when the condition is quickly met.
+     *
+     * @example
+     * async_test(t => {
+     *  const popup = window.open("resources/coop-coep.py?coop=same-origin&coep=&navigate=about:blank");
+     *  t.add_cleanup(() => popup.close());
+     *  assert_equals(window, popup.opener);
+     *
+     *  popup.onload = t.step_func(() => {
+     *    assert_true(popup.location.href.endsWith("&navigate=about:blank"));
+     *    // Use step_wait_func_done as about:blank cannot message back.
+     *    t.step_wait_func_done(() => popup.location.href === "about:blank");
+     *  });
+     * }, "Navigating a popup to about:blank");
      *
      * @param {Function} cond A function taking no arguments and
      *                        returning a boolean. The callback is called
@@ -3884,7 +3884,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     /**
      * Timeout the tests.
      *
-     * This only has an effect when ``explict_timeout`` has been set
+     * This only has an effect when ``explicit_timeout`` has been set
      * in :js:func:`setup`. In other cases any call is a no-op.
      *
      */
