5d078cf97af6df999e505e976d3b132a8396609f
Dave Tapuska <dtapuska@chromium.org>
2022-09-27 21:48:19 +0000

    Allowlist users of MainThread::GetTaskRunner
    Mark a few more classes as having permission to use
    MainThread::GetTaskRunner.
    
    BUG=624696
    
    Change-Id: I6cc967e224251871b3bca9f2cb1ef0dddb0deaba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914367
    Reviewed-by: Carlos Caballero Grolimund <carlscab@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052066}

--- third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
@@ -75,10 +75,13 @@ const AtomicString& FontCache::LegacySystemFontFamily() {
   return font_family_names::kBlinkMacSystemFont;
 }
 
-static void InvalidateFontCache() {
+// static
+void FontCache::InvalidateFromAnyThread() {
   if (!IsMainThread()) {
-    Thread::MainThread()->GetDeprecatedTaskRunner()->PostTask(
-        FROM_HERE, WTF::BindOnce(&InvalidateFontCache));
+    Thread::MainThread()
+        ->GetTaskRunner(MainThreadTaskRunnerRestricted())
+        ->PostTask(FROM_HERE,
+                   WTF::BindOnce(&FontCache::InvalidateFromAnyThread));
     return;
   }
   FontCache::Get().Invalidate();
@@ -92,7 +95,7 @@ static void FontCacheRegisteredFontsChangedNotificationCallback(
     CFDictionaryRef) {
   DCHECK_EQ(observer, &FontCache::Get());
   DCHECK(CFEqual(name, kCTFontManagerRegisteredFontsChangedNotification));
-  InvalidateFontCache();
+  FontCache::InvalidateFromAnyThread();
 }
 
 static bool UseHinting() {
