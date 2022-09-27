020101b9fd9afe01bf057e1ec63e68e5f50d26c5
Akihiro Ota <akihiroota@chromium.org>
2022-09-27 20:23:50 +0000

    Dictation: Move Pumpkin install request to PumpkinParseStrategy
    This change moves pumpkin installation logic to PumpkinParseStrategy.
    This will help simplify plumbing when we spin up a
    SandboxedPumpkinTagger from PumpkinData.
    
    This change also unflakes the DictationPumpkinInstallTest.WaitForInstall
    test. The reason why the test was flaking is because the ScopedTempDirs
    were being destroyed before the files could be read. The fix is to
    ensure that they stay alive until the test fixture tears down.
    
    AX-Relnotes: N/A
    Bug: 1258190, 1367156
    Change-Id: I404bf651998ae1704537a5a356559aaa740f4f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914314
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052025}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
@@ -20,7 +20,6 @@ const StopEvent = chrome.speechRecognitionPrivate.SpeechRecognitionStopEvent;
 const SpeechRecognitionType =
     chrome.speechRecognitionPrivate.SpeechRecognitionType;
 const PrefObject = chrome.settingsPrivate.PrefObject;
-const PumpkinData = chrome.accessibilityPrivate.PumpkinData;
 
 /** Main class for the Chrome OS dictation feature. */
 export class Dictation {
@@ -66,9 +65,6 @@ export class Dictation {
 
     /** @private {?MetricsUtils} */
     this.metricsUtils_ = null;
-
-    /** @private {boolean} */
-    this.isPumpkinEnabled_ = false;
 
     /** @private {?FocusHandler} */
     this.focusHandler_ = null;
@@ -137,8 +133,6 @@ export class Dictation {
     // Browser process.
     chrome.accessibilityPrivate.onToggleDictation.addListener(
         this.onToggleDictationListener_);
-
-    this.maybeInstallPumpkin_();
   }
 
   /**
@@ -167,20 +161,6 @@ export class Dictation {
     if (this.inputController_) {
       this.inputController_.removeListeners();
     }
-  }
-
-  /** @private */
-  maybeInstallPumpkin_() {
-    const pumpkinFeature = chrome.accessibilityPrivate.AccessibilityFeature
-                               .DICTATION_PUMPKIN_PARSING;
-    chrome.accessibilityPrivate.isFeatureEnabled(pumpkinFeature, enabled => {
-      this.isPumpkinEnabled_ = enabled;
-      if (enabled) {
-        chrome.accessibilityPrivate.installPumpkinForDictation(data => {
-          this.onPumpkinInstalled_(data);
-        });
-      }
-    });
   }
 
   /**
@@ -530,19 +510,6 @@ export class Dictation {
   static removeAsInputMethod() {
     chrome.languageSettingsPrivate.removeInputMethod(
         InputController.IME_ENGINE_ID);
-  }
-
-  /**
-   * @param {PumpkinData} data
-   * @private
-   */
-  onPumpkinInstalled_(data) {
-    if (!this.isPumpkinEnabled_ || !data) {
-      return;
-    }
-
-    // TODO(akihiroota): Either instantiate a new Web Worker or sandboxed
-    // iframe to execute Pumpkin code.
   }
 
   /**
