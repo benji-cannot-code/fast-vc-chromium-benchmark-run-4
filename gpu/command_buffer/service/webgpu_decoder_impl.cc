5fba7bbc5e761a18d54dc44bb4a77d26b3afd105
Takahiro <hogehoge@gachapin.jp>
2022-09-27 10:08:39 +0000

    Support rg11b10ufloat-renderable feature
    Bug: 1366919
    Change-Id: I7456b18a1edfd7b4d1e1d172b969c3734876a412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916877
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051709}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1123,6 +1123,7 @@ bool WebGPUDecoderImpl::IsFeatureExposed(WGPUFeatureName feature) const {
     case WGPUFeatureName_TextureCompressionETC2:
     case WGPUFeatureName_TextureCompressionASTC:
     case WGPUFeatureName_IndirectFirstInstance:
+    case WGPUFeatureName_RG11B10UfloatRenderable:
       return true;
     default:
       return false;
