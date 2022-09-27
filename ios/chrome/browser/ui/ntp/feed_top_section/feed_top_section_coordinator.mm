7184576de960eca3eabaa34b6c673dd3c20bde5d
Sylvain Defresne <sdefresne@chromium.org>
2022-09-27 11:59:06 +0000

    [ios] Properly shutdown FeedTopSectionMediator
    FeedTopSectionMediator registers an IdentityManager observer
    which needs to be disconnected when the mediator is no longer
    in use by calling the -shutdown method.
    
    Previously this was left to be called when the object is deallocated
    which can be arbitrarily delayed by animation and the autorelease
    pool. Add an explicit call to -shutdown when the coordinator is
    stopped (as it is the one that creates the mediator) to ensure the
    unregistration happens at a known point in time.
    
    Fixes a shutdown crash where the observer was not unregistered
    by the time the IdentityManager was destroyed.
    
    Fixed: 1364209
    Change-Id: I83c7a8fcb265e4c7edfb662199a82f303986de3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921755
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051737}

--- ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_coordinator.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_coordinator.mm
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   _viewController = nil;
+  [self.feedTopSectionMediator shutdown];
   self.feedTopSectionMediator = nil;
   self.feedTopSectionViewController = nil;
 }
