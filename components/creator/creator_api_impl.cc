c226ca98bf887b3ea59f08fd76f1134623644ca8
Jeffrey Cohen <jeffreycohen@google.com>
2022-09-27 19:45:09 +0000

    [Cormorant] web channel ids are strings
    Bug: 1362726
    Change-Id: I2ed180f9e1531c25c80a5aebcc4ddb35bebdf62f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3919710
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051992}

--- components/creator/creator_api_impl.cc
+++ components/creator/creator_api_impl.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/creator/creator_api_impl.h"
 
+#include <string>
+
 #include "components/creator/public/creator_api.h"
 #include "url/gurl.h"
 
 namespace creator {
-Creator CreatorApiImpl::GetCreator(int channel_id) {
+Creator CreatorApiImpl::GetCreator(std::string channel_id) {
   // TODO(crbug.com/1365645) query actual data for given channel id.
   return Creator{/*url=*/GURL("example.com"), /*title=*/u"Example Domain"};
 }
