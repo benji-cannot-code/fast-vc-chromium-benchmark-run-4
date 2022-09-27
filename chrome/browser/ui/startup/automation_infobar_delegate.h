ab49be9b9aea776831aba3866cd1fa7e09c3c1a7
Thiago Perrotta <tperrotta@chromium.org>
2022-09-27 20:24:47 +0000

    automation infobar: remove unused import
    Bug: None
    Change-Id: Ibf4a7dbb7bbffdde14872e06a1b71b033c26185a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921816
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052026}

--- chrome/browser/ui/startup/automation_infobar_delegate.h
+++ chrome/browser/ui/startup/automation_infobar_delegate.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
-#include "url/gurl.h"
 
 // An infobar to inform users if their browser is being controlled by an
 // automated test.
