0249f16857a2e2565c10c15192627f9730784cbc
Avi Drissman <avi@chromium.org>
2022-09-27 19:16:57 +0000

    Update copyright headers in third_party/zlib/
    This is forked and this repo is authoritative for the fork.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I4698084a28dda86210e9dba6ddac76c13377e687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921823
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051972}

--- third_party/zlib/contrib/optimizations/chunkcopy.h
+++ third_party/zlib/contrib/optimizations/chunkcopy.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* chunkcopy.h -- fast chunk copy and set operations
  * Copyright (C) 2017 ARM, Inc.
- * Copyright 2017 The Chromium Authors. All rights reserved.
+ * Copyright 2017 The Chromium Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the Chromium source repository LICENSE file.
  */
--- third_party/zlib/slide_hash_simd.h
+++ third_party/zlib/slide_hash_simd.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* slide_hash_simd.h
  *
- * Copyright 2022 The Chromium Authors. All rights reserved.
+ * Copyright 2022 The Chromium Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the Chromium source repository LICENSE file.
  */
