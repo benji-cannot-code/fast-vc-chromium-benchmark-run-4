c000d22e0a91be0fe668614bdef1fbec457ad426
Koji Ishii <kojii@chromium.org>
2022-09-27 23:13:18 +0000

    [anchor-position] Fix anchor query scopes in multicol
    When the containing block of querying element doesn't have
    anchor queries, |CalculateOffset| should use an empty
    |NGLogicalAnchorQuery|.
    
    However, |LayoutFragmentainerDescendants| passed |nullptr|,
    which means to use |container_builder_->AnchorQuery()|. In
    multicol, this means to use the anchor query for the multicol
    container.
    
    This patch fixes the situation by passing an empty
    |NGLogicalAnchorQuery|.
    
    Bug: 1309178
    Change-Id: Ibb64d3f7932eb677f70063cee303b2e62ca5b918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922615
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1052100}

--- third_party/blink/renderer/core/layout/ng/ng_anchor_query.h
+++ third_party/blink/renderer/core/layout/ng/ng_anchor_query.h
@@ -95,6 +95,9 @@ struct CORE_EXPORT NGLogicalAnchorReference
 class CORE_EXPORT NGLogicalAnchorQuery
     : public GarbageCollected<NGLogicalAnchorQuery> {
  public:
+  // Returns an empty instance.
+  static const NGLogicalAnchorQuery& Empty();
+
   bool IsEmpty() const { return anchor_references_.empty(); }
 
   const NGLogicalAnchorReference* AnchorReference(
@@ -149,8 +152,9 @@ class CORE_EXPORT NGLogicalAnchorQueryForFragmentation {
   }
 
   // Get |NGLogicalAnchorQuery| in the stitched coordinate system for the given
-  // containing block.
-  const NGLogicalAnchorQuery* StitchedAnchorQuery(
+  // containing block. If there is no anchor query for the containing block,
+  // returns an empty instance.
+  const NGLogicalAnchorQuery& StitchedAnchorQuery(
       const LayoutObject& containing_block) const;
 
   // Update the internal map of anchor queries for containing blocks from the
