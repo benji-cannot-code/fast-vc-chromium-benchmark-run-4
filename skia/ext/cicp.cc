98c75690f58ddd272cba7e919c38d4156909e41a
Christopher Cameron <ccameron@chromium.org>
2022-09-27 18:36:50 +0000

    ImageDecoder: Use CICP data embedded in ICC profile, if present
    Add the function skia::CICPGetSkColorSpace to consolidate the
    conversion from CICP data to SkColorSpace in a single place.
    
    Add additional logic to ensure that ImageDecoder::ColorTransform not
    create a transform if we are using a CICP-based profile.
    
    Bug: 1366315
    Change-Id: I1ee162148ea7d2a3c646f8833948261695442e62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913326
    Reviewed-by: Urvang Joshi <urvang@chromium.org>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051924}

--- skia/ext/cicp.cc
+++ skia/ext/cicp.cc
@@ -63,7 +63,7 @@ bool CICPGetPrimaries(uint8_t primaries, SkColorSpacePrimaries& sk_primaries) {
 }
 
 bool CICPGetTransferFn(uint8_t transfer_characteristics,
-                       bool prefer_srgb,
+                       bool prefer_srgb_trfn,
                        skcms_TransferFunction& trfn) {
   // Rec. ITU-T H.273, Table 3.
   switch (transfer_characteristics) {
@@ -71,8 +71,8 @@ bool CICPGetTransferFn(uint8_t transfer_characteristics,
       // Reserved.
       break;
     case 1:
-      trfn = prefer_srgb ? SkNamedTransferFnExt::kSRGB
-                         : SkNamedTransferFnExt::kRec709;
+      trfn = prefer_srgb_trfn ? SkNamedTransferFnExt::kSRGB
+                              : SkNamedTransferFnExt::kRec709;
       return true;
     case 2:
       // Unspecified.
@@ -87,8 +87,8 @@ bool CICPGetTransferFn(uint8_t transfer_characteristics,
       trfn = SkNamedTransferFnExt::kRec470SystemBG;
       return true;
     case 6:
-      trfn = prefer_srgb ? SkNamedTransferFnExt::kSRGB
-                         : SkNamedTransferFnExt::kRec601;
+      trfn = prefer_srgb_trfn ? SkNamedTransferFnExt::kSRGB
+                              : SkNamedTransferFnExt::kRec601;
       return true;
     case 7:
       trfn = SkNamedTransferFnExt::kSMPTE_ST_240;
@@ -103,8 +103,8 @@ bool CICPGetTransferFn(uint8_t transfer_characteristics,
       // Logarithmic transfer characteristic (100 * Sqrt( 10 ) : 1 range).
       break;
     case 11:
-      trfn = prefer_srgb ? SkNamedTransferFnExt::kSRGB
-                         : SkNamedTransferFnExt::kIEC61966_2_4;
+      trfn = prefer_srgb_trfn ? SkNamedTransferFnExt::kSRGB
+                              : SkNamedTransferFnExt::kIEC61966_2_4;
       break;
     case 12:
       // Rec. ITU-R BT.1361-0 extended colour gamut system (historical).
@@ -136,6 +136,32 @@ bool CICPGetTransferFn(uint8_t transfer_characteristics,
 
   trfn = SkNamedTransferFnExt::kInvalid;
   return false;
+}
+
+sk_sp<SkColorSpace> CICPGetSkColorSpace(uint8_t color_primaries,
+                                        uint8_t transfer_characteristics,
+                                        uint8_t matrix_coefficients,
+                                        uint8_t full_range_flag,
+                                        bool prefer_srgb_trfn) {
+  if (matrix_coefficients != 0)
+    return nullptr;
+
+  if (full_range_flag != 1)
+    return nullptr;
+
+  skcms_TransferFunction trfn;
+  if (!CICPGetTransferFn(transfer_characteristics, prefer_srgb_trfn, trfn))
+    return nullptr;
+
+  SkColorSpacePrimaries primaries;
+  if (!CICPGetPrimaries(color_primaries, primaries))
+    return nullptr;
+
+  skcms_Matrix3x3 primaries_matrix;
+  if (!primaries.toXYZD50(&primaries_matrix))
+    return nullptr;
+
+  return SkColorSpace::MakeRGB(trfn, primaries_matrix);
 }
 
 bool CICPGetSkYUVColorSpace(uint8_t matrix_coefficients,
