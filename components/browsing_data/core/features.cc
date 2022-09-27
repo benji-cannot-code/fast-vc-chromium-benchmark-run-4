63ac45f1b7d60279ffc8fe22a2093797196e5ad7
Daniel Cheng <dcheng@chromium.org>
2022-09-27 04:23:16 +0000

    Use helper macros to define base::Features in //components/browsing_data/core
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=jsbell@chromium.org
    
    Bug: 1364289
    Change-Id: I0b3ae642c7645db6b5fba020a1ccdfc43714f858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916997
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051603}

--- components/browsing_data/core/features.cc
+++ components/browsing_data/core/features.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browsing_data {
 namespace features {
 
-const base::Feature kEnableRemovingAllThirdPartyCookies{
-    "EnableRemovingAllThirdPartyCookies", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kEnableRemovingAllThirdPartyCookies,
+             "EnableRemovingAllThirdPartyCookies",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
-const base::Feature kEnableBrowsingDataLifetimeManager{
-    "BrowsingDataLifetimeManager", base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kEnableBrowsingDataLifetimeManager,
+             "BrowsingDataLifetimeManager",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 }  // namespace features
 }  // namespace browsing_data
