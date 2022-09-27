af9b7767518cf42e343592e4010be7d35bc67bea
Lijin Shen <lazzzis@google.com>
2022-09-27 19:20:29 +0000

    Move onBackPressed of FindToolabr and UrlBar to Activity
    Intercept and consume back press for FindToolbar and UrlBar when
    back press refactor is disabled on T+.
    
    Migration plan: https://crbug.com/1368354#c1
    
    Bug: 1368354
    Change-Id: Id30d5f9e368fd40f084466b90a2fa8f6b12af373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3920153
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051978}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -39,6 +39,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.BuildInfo;
 import org.chromium.base.BundleUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.CommandLine;
@@ -2270,6 +2271,15 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (mRootUiCoordinator.getBottomSheetController() != null
                 && mRootUiCoordinator.getBottomSheetController().handleBackPress()) {
             BackPressManager.record(BackPressHandler.Type.BOTTOM_SHEET);
+            return;
+        }
+
+        // This only intercepts back press when back press refactor is disabled on T+. Otherwise,
+        // back press is intercepted in FindToolbarManager internally.
+        if (BuildInfo.isAtLeastT() && mRootUiCoordinator.getFindToolbarManager() != null
+                && mRootUiCoordinator.getFindToolbarManager().isShowing()) {
+            BackPressManager.record(BackPressHandler.Type.FIND_TOOLBAR);
+            mRootUiCoordinator.getFindToolbarManager().hideToolbar();
             return;
         }
 
