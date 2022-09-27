8daee634082e95219bc6e280e15dad51f953e487
Takashi Sakamoto <tasak@google.com>
2022-09-27 09:08:30 +0000

    Fix CertNetFetcherURLLoaderTest's flaky failure.
    CertNetFetcherURLLoaderTest causes to create CacheTransparencySettings.
    CacheTransparencySettings is a singleton and has a sequence checker.
    So we need to reset CacheTransparencySettings at the test's TearDown().
    
    Sample test failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/30172/test-results?q=CertNetFetcherURLLoaderTest
    
    Bug: 1363985
    Change-Id: I0c6130a7ae14dbda828261865a217618c46594a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904985
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051690}

--- services/cert_verifier/cert_net_url_loader/cert_net_fetcher_url_loader_unittest.cc
+++ services/cert_verifier/cert_net_url_loader/cert_net_fetcher_url_loader_unittest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/cert_verifier/cert_net_url_loader/cert_net_fetcher_test.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
+#include "services/network/url_loader.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
@@ -186,6 +187,8 @@ class CertNetFetcherURLLoaderTest : public PlatformTest {
   }
 
   void TeardownOnNetworkThread() {
+    network::URLLoader::ResetPervasivePayloadsListForTesting();
+
     if (!test_util_)
       return;
     test_util_->fetcher()->Shutdown();
