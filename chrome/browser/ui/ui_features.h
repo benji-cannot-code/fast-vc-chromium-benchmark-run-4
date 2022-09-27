138390c88cabfe52bb37422d2e1d772c4f8ad71b
David Roger <droger@chromium.org>
2022-09-27 21:09:34 +0000

    [signin] Delete kForceSigninReauth feature
    This feature has been enabled by default for multiple releases. The flag
    can now be removed.
    
    Change-Id: I31e5e45d9236900c5c7c9781d95c75e8443b4a19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921778
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052046}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -56,8 +56,6 @@ BASE_DECLARE_FEATURE(kEvDetailsInPageInfo);
 BASE_DECLARE_FEATURE(kLightweightExtensionOverrideConfirmations);
 #endif
 
-BASE_DECLARE_FEATURE(kForceSignInReauth);
-
 BASE_DECLARE_FEATURE(kPowerBookmarksSidePanel);
 
 BASE_DECLARE_FEATURE(kProminentDarkModeActiveTabTitle);
