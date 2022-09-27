c7bee060b3c3173158a1eee357c34801a5d16d92
Daniel Cheng <dcheng@chromium.org>
2022-09-27 16:55:01 +0000

    Use helper macros to define base::Features in //components/browser_watcher
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=chrisha@chromium.org
    
    Bug: 1364289
    Change-Id: I734424633e3e3d8d482b0c7d5be0c9345c0df10b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914841
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051875}

--- components/browser_watcher/features.cc
+++ components/browser_watcher/features.cc
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_watcher {
 
-const base::Feature kExtendedCrashReportingFeature{
-    "ExtendedCrashReporting", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kExtendedCrashReportingFeature,
+             "ExtendedCrashReporting",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 const char kInMemoryOnlyParam[] = "in_memory_only";
 
