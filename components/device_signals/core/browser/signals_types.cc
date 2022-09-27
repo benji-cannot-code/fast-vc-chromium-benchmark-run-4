83eee45f007bc1e65c350d3444fce623aef96197
Zonghan Xu <xzonghan@google.com>
2022-09-27 22:04:13 +0000

    Create Settings Signals Collector Boilerplate
    This will be used to collect settings signals (currently only registry presence/value on windows and plist presence/value on mac). It's part of an additional endpoint verification signal in go/ev-additional-signals-prd
    
    This collector runs on browser process.
    
    Fixed: b:237526915
    Change-Id: I6b1a2490e573a0854a24458a046b94d8aba850e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884016
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052074}

--- components/device_signals/core/browser/signals_types.cc
+++ components/device_signals/core/browser/signals_types.cc
@@ -50,8 +50,6 @@ HotfixSignalResponse& HotfixSignalResponse::operator=(
 HotfixSignalResponse::~HotfixSignalResponse() = default;
 #endif  // BUILDFLAG(IS_WIN)
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
-
 GetSettingsOptions::GetSettingsOptions() = default;
 GetSettingsOptions::GetSettingsOptions(const GetSettingsOptions&) = default;
 
@@ -59,6 +57,11 @@ GetSettingsOptions& GetSettingsOptions::operator=(const GetSettingsOptions&) =
     default;
 
 GetSettingsOptions::~GetSettingsOptions() = default;
+
+bool GetSettingsOptions::operator==(const GetSettingsOptions& other) const {
+  return path == other.path && key == other.key &&
+         get_value == other.get_value && hive == other.hive;
+}
 
 SettingsItem::SettingsItem() = default;
 
@@ -85,6 +88,11 @@ SettingsItem& SettingsItem::operator=(const SettingsItem& other) {
 
 SettingsItem::~SettingsItem() = default;
 
+bool SettingsItem::operator==(const SettingsItem& other) const {
+  return path == other.path && presence == other.presence && key == other.key &&
+         hive == other.hive && setting_value == other.setting_value;
+}
+
 SettingsResponse::SettingsResponse() = default;
 SettingsResponse::SettingsResponse(const SettingsResponse&) = default;
 
@@ -92,8 +100,6 @@ SettingsResponse& SettingsResponse::operator=(const SettingsResponse&) =
     default;
 
 SettingsResponse::~SettingsResponse() = default;
-
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 
 FileSystemInfoResponse::FileSystemInfoResponse() = default;
 FileSystemInfoResponse::FileSystemInfoResponse(const FileSystemInfoResponse&) =
