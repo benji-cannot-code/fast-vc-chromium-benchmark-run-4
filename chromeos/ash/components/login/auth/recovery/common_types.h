78a99b39eb8afbb19e4177eb7bd1637c5e50865d
Yunke Zhou <yunkez@google.com>
2022-09-27 14:04:01 +0000

    Add Cryptohome recovery client that interacts with the server
    Bug: b:239420386
    Change-Id: I1a16d80ce37e97c920f9e8116c74d9fa0390815e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865340
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051773}

---
+++ chromeos/ash/components/login/auth/recovery/common_types.h
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_LOGIN_AUTH_RECOVERY_COMMON_TYPES_H_
+#define CHROMEOS_ASH_COMPONENTS_LOGIN_AUTH_RECOVERY_COMMON_TYPES_H_
+
+#include <stdint.h>
+
+#include <string>
+#include <vector>
+
+#include "base/types/strong_alias.h"
+
+namespace ash {
+
+// Gaia access token that is used to call Recovery service APIs.
+using GaiaAccessToken =
+    base::StrongAlias<class GaiaAccessTokenTag, std::string>;
+
+// Gaia reauth proof token that is used as a proof that the user has passed
+// online signin.
+using GaiaReauthProofToken =
+    base::StrongAlias<class GaiaReauthProofTokenTag, std::string>;
+
+// Serialized proto response for FetchEpoch request.
+using CryptohomeRecoveryEpochResponse =
+    base::StrongAlias<class CryptohomeRecoveryEpochResponseTag,
+                      std::vector<uint8_t>>;
+
+// Serialized proto response for FetchRecoveryResponse request.
+using CryptohomeRecoveryResponse =
+    base::StrongAlias<class CryptohomeRecoveryResponseTag,
+                      std::vector<uint8_t>>;
+
+enum class CryptohomeRecoveryServerStatusCode {
+  // The request was completed successfully.
+  kSuccess,
+  // Failed with network error.
+  kNetworkError,
+  // Failed with HTTP 401 (Unauthorized) error.
+  kAuthError,
+  // Failed with HTTP 500-599 error (Server error).
+  kServerError,
+  // Failed with other errors, that are not retriable.
+  kFatalError,
+};
+
+}  // namespace ash
+
+#endif  // CHROMEOS_ASH_COMPONENTS_LOGIN_AUTH_RECOVERY_COMMON_TYPES_H_
