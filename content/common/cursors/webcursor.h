8ad5a6f8815a1d17fc99a9999ccdfd8df066f942
Qi Tiezheng <qitiezheng@360.cn>
2022-09-27 08:25:49 +0000

    [content] Fix some cpplint errors, part5
    content/browser/site_per_process_browsertest.cc:181:
      "content/public/common/content_client.h" already included at content/browser/site_per_process_browsertest.cc:94  [build/include] [4]
    content/browser/site_per_process_browsertest.cc:183:
      "content/test/test_content_browser_client.h" already included at content/browser/site_per_process_browsertest.cc:119  [build/include] [4]
    content/browser/xr/service/browser_xr_runtime_impl.cc:24:
      "build/build_config.h" already included at content/browser/xr/service/browser_xr_runtime_impl.cc:13  [build/include] [4]
    content/common/cursors/webcursor.h:18:
      "ui/base/cursor/cursor.h" already included at content/common/cursors/webcursor.h:10  [build/include] [4]
    content/public/browser/service_process_host.h:29:
      "base/callback.h" already included at content/public/browser/service_process_host.h:13  [build/include] [4]
    
    and reorder platform-specific headers to follow the Chromium C++ Code Style.
    
    Bug: None
    Change-Id: Ica32e6179feb6e563efdcb90f21f97a56533a988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845494
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Qi Tiezheng <qitiezheng@360.cn>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051672}

--- content/common/cursors/webcursor.h
+++ content/common/cursors/webcursor.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_AURA)
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "ui/base/cursor/cursor.h"
 #endif
 
 namespace content {
