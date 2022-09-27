04a9e2669555ef5223146cc6f7ba4043ecc2b266
Daniel Cheng <dcheng@chromium.org>
2022-09-27 00:08:24 +0000

    Use helper macros to define base::Features in //components/certificate_transparency
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=agl@chromium.org
    
    Bug: 1364289
    Change-Id: I9d6c7798f5e7c235dea38ee3c650209038931d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914640
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051496}

--- components/certificate_transparency/ct_features.cc
+++ components/certificate_transparency/ct_features.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace certificate_transparency {
 namespace features {
 
-const base::Feature kCertificateTransparencyComponentUpdater{
-    "CertificateTransparencyComponentUpdater",
-    base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kCertificateTransparencyComponentUpdater,
+             "CertificateTransparencyComponentUpdater",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
 }  // namespace certificate_transparency
