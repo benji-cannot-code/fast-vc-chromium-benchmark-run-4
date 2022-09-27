582e61da679b2a0548923201175aa9956d27e404
Maria Kazinova <kazinova@google.com>
2022-09-27 10:34:19 +0000

    [UPM]  Added a flag to enable kUnifiedPasswordManagerReenrollment.
    Bug: 1368095
    Change-Id: Ife43221ee0f311174d40da933e523ded41ea89df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918330
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051717}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8473,6 +8473,13 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAndroid,
      FEATURE_VALUE_TYPE(
          password_manager::features::kUnifiedPasswordManagerErrorMessages)},
+
+    {"google-mobile-services-passwords-reenrollment",
+     flag_descriptions::kUnifiedPasswordManagerAndroidReenrollmentName,
+     flag_descriptions::kUnifiedPasswordManagerAndroidReenrollmentDescription,
+     kOsAndroid,
+     FEATURE_VALUE_TYPE(
+         password_manager::features::kUnifiedPasswordManagerReenrollment)},
 #endif
 
     {"extension-workflow-justification",
