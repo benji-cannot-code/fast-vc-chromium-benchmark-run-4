3279707e1ccfb3e63b1d185481db430d6c411701
Rebekah Potter <rbpotter@chromium.org>
2022-09-28 04:40:23 +0000

    WebUI: Move webview_manager.js to only user
    As part of this change, also moving the webview_manager_test to
    parent_access_browsertest, and fixing it so that the asserts in the
    callbacks actually run (previously, these were skipped and the test
    was passing accidentally).
    
    Also updating incorrect closure annotations, as the original target
    was not hooked up correctly.
    
    Bug: 1364309
    Change-Id: I78afbde1cd19709f72c426f78a05198a6123a42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914719
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052216}

--- chrome/browser/resources/chromeos/parent_access/parent_access_ui.js
+++ chrome/browser/resources/chromeos/parent_access/parent_access_ui.js
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './strings.m.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {WebviewManager} from 'chrome://resources/js/webview_manager.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {ParentAccessController} from './parent_access_controller.js';
 import {GetOAuthTokenStatus, ParentAccessServerMessageType, ParentAccessUIHandler} from './parent_access_ui.mojom-webui.js';
+import {WebviewManager} from './webview_manager.js';
 
 const parentAccessUIHandler = ParentAccessUIHandler.getRemote();
 
--- chrome/browser/resources/chromeos/parent_access/webview_manager.js
+++ chrome/browser/resources/chromeos/parent_access/webview_manager.js
@@ -12,12 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * standardize some common configurations of webview.
  */
 export class WebviewManager {
-  /**
-   * @param @type {!WebView} The webview to manage.
-   */
+  /** @param {!WebView} webview The webview to manage. */
   constructor(webview) {
     /**
-     * @private @const  @type {!WebView} the webview element that this class
+     * @private {!WebView} the webview element that this class
      *     will manage.
      */
     this.webview_ = webview;
@@ -25,14 +23,14 @@ export class WebviewManager {
     /**
      * Tracks the current function used to filter destinations
      * to which we send access tokens.
-     * @private {!function(string):boolean}
+     * @private {?function(string):boolean}
      */
     this.shouldSendTokenToUrlFn_ = null;
 
     /**
      * Tracks the current function used to filter destinations
      * to which we send allow requests.
-     * @private {!function(string):boolean}
+     * @private {?function(string):boolean}
      */
     this.allowedRequestFn_ = null;
   }
