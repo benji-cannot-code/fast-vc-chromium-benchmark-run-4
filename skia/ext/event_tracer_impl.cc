bc425bf6d1e57fc34d2dac10ed7b99133821c596
Daniel Cheng <dcheng@chromium.org>
2022-09-28 01:13:02 +0000

    Use helper macros to define base::Features in //skia
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=fmalita@chromium.org
    
    Bug: 1364289
    Change-Id: I355f9bd5a63c178d967cbf0c4fad3cf7da3af8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922762
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052145}

--- skia/ext/event_tracer_impl.cc
+++ skia/ext/event_tracer_impl.cc
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // Experiment with not deleting the Skia event tracer at process exit
 // to measure the improvement in performance. See crbug.com/1329594
-const base::Feature kLeakSkiaEventTracerAtExit{
-    "LeakSkiaEventTracerAtExit", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kLeakSkiaEventTracerAtExit,
+             "LeakSkiaEventTracerAtExit",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace
 
 namespace skia {
