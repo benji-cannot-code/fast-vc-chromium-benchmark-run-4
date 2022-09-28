807ae0eec4c34878e5eda7a092b7fb53d24a927d
Dan Stahr <danstahr@google.com>
2022-09-28 08:33:48 +0000

    Add the rest of API helpers for Cronet.
    Change-Id: I2182f4b84709333f5d2075a3ca57c3e8d36fff94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899200
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Dan Stahr <danstahr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052287}

---
+++ components/cronet/android/api/src/org/chromium/net/apihelpers/ByteArrayCronetCallback.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.apihelpers;
+
+import org.chromium.net.UrlResponseInfo;
+
+/**
+ * A specialization of {@link InMemoryTransformCronetCallback} which returns the body bytes
+ * verbatim without any interpretation.
+ */
+public abstract class ByteArrayCronetCallback extends InMemoryTransformCronetCallback<byte[]> {
+    @Override // Override to return the subtype
+    public ByteArrayCronetCallback addCompletionListener(
+            CronetRequestCompletionListener<? super byte[]> listener) {
+        super.addCompletionListener(listener);
+        return this;
+    }
+
+    @Override
+    protected final byte[] transformBodyBytes(UrlResponseInfo info, byte[] bodyBytes) {
+        return bodyBytes;
+    }
+}
