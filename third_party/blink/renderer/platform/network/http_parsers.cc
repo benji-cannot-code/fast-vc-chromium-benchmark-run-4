abf60274f824fee3f41f19413910fe25437be968
Robert Lin <robertlin@chromium.org>
2022-09-28 05:51:53 +0000

    prerender: Refactors prerender same site cross origin opt-in header
    This CL refactors same site cross origin opt-in header to make it
    use enum type to address support loading mode more generally, and moves
    supports_loading_mode.mojom to the same build rule as
    parsed_headers.mojom so that parsed_headers.mojom can import supports_loading_mode.mojom.
    
    Bug: 1356449
    Change-Id: I640fcd78fa864f4668eaa2e354cc6bac21563d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890510
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052240}

--- third_party/blink/renderer/platform/network/http_parsers.cc
+++ third_party/blink/renderer/platform/network/http_parsers.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/parsed_headers.h"
 #include "services/network/public/cpp/timing_allow_origin_parser.h"
 #include "services/network/public/mojom/parsed_headers.mojom-blink.h"
+#include "services/network/public/mojom/supports_loading_mode.mojom-blink.h"
 #include "services/network/public/mojom/timing_allow_origin.mojom-blink.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/platform/web_string.h"
@@ -94,6 +95,10 @@ blink::CSPDirectiveName ConvertToBlink(CSPDirectiveName name) {
 // by both the non-Blink Mojo variant and the Blink Mojo variant.
 blink::WebClientHintsType ConvertToBlink(WebClientHintsType in) {
   return in;
+}
+
+blink::LoadingMode ConvertToBlink(LoadingMode in) {
+  return static_cast<blink::LoadingMode>(in);
 }
 
 // ===== Converters for other basic Blink types =====
@@ -258,7 +263,8 @@ blink::ParsedHeadersPtr ConvertToBlink(const ParsedHeadersPtr& in) {
           ? absl::make_optional(ConvertToBlink(in->critical_ch.value()))
           : absl::nullopt,
       in->xfo, ConvertToBlink(in->link_headers),
-      ConvertToBlink(in->timing_allow_origin), in->is_credentialed_prerender,
+      ConvertToBlink(in->timing_allow_origin),
+      ConvertToBlink(in->supports_loading_mode),
       in->reporting_endpoints.has_value()
           ? absl::make_optional(ConvertToBlink(in->reporting_endpoints.value()))
           : absl::nullopt,
