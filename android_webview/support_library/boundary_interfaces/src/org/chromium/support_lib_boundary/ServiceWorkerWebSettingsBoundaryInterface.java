cba37702ac2cf2f85c9c783e744c04f106784d8f
Peter Birk Pakkenberg <pbirk@chromium.org>
2022-09-28 21:49:07 +0000

    [aw] Remove unused boundary interface methods
    These methods were never unhidden, and are no longer in use by the
    androidx.webkit library. The feature constant that guarded their use has
    also been removed from the array of supported features.
    
    Bug: 1295213
    Change-Id: If4deabc81bda0a6d244832be21163edfbe79a8b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921798
    Reviewed-by: Richard Coles <torne@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052655}

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/ServiceWorkerWebSettingsBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/ServiceWorkerWebSettingsBoundaryInterface.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.support_lib_boundary;
 
-import org.chromium.support_lib_boundary.WebSettingsBoundaryInterface.RequestedWithHeaderMode;
-
 import java.util.Set;
 
 /**
@@ -28,12 +26,6 @@ public interface ServiceWorkerWebSettingsBoundaryInterface {
     void setBlockNetworkLoads(boolean flag);
 
     boolean getBlockNetworkLoads();
-
-    @Deprecated
-    void setRequestedWithHeaderMode(@RequestedWithHeaderMode int mode);
-    @Deprecated
-    @RequestedWithHeaderMode
-    int getRequestedWithHeaderMode();
 
     void setRequestedWithHeaderOriginAllowList(Set<String> allowedOriginRules);
 
