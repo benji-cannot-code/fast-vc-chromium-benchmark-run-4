e5042061d83cfe619c39d1a4eee0e9b0388fdb7b
Hirokazu Honda <hiroh@chromium.org>
2022-09-28 05:27:23 +0000

    media/gpu/V4L2VD: Remove unnecessary kDpbOutputBufferExtraCount
    kDpbOutputBufferExtraCount is limits::kMaxVideoFrames + 1. It is
    the number of video frames in pipeline. The output buffers are
    allocated kDpbOutputBufferExtraCount + |num_output_frames|. It is
    AcceleratedVideoDecoder::GetRequiredNumOfPictures() in v4l2 stateless
    video decoder case. The function takes into account the video frames
    in pipeline, so adding kDpbOutputBufferExtraCount is unnecessary.
    
    Bug: b:249184286
    Test: Google Meet call on asurada and trogdor
    Test: video_decode_accelerator_tests on asurada and trogdor
    Change-Id: I38a6eee1b7c16db5e90d9622caf852ea5f3610e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3920440
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052230}

--- media/gpu/v4l2/v4l2_video_decoder_backend_stateful.cc
+++ media/gpu/v4l2/v4l2_video_decoder_backend_stateful.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
+#include "media/base/limits.h"
 #include "media/base/media_switches.h"
 #include "media/base/video_codecs.h"
 #include "media/gpu/chromeos/dmabuf_video_frame_pool.h"
@@ -617,9 +618,10 @@ void V4L2StatefulVideoDecoderBackend::ChangeResolution() {
   }
 
   auto ctrl = device_->GetCtrl(V4L2_CID_MIN_BUFFERS_FOR_CAPTURE);
-  constexpr size_t DEFAULT_NUM_OUTPUT_BUFFERS = 7;
+  constexpr size_t kDefaultNumOutputBuffers = 7;
+  constexpr size_t kPicsInPipeline = limits::kMaxVideoFrames + 1;
   const size_t num_output_buffers =
-      ctrl ? ctrl->value : DEFAULT_NUM_OUTPUT_BUFFERS;
+      (ctrl ? ctrl->value : kDefaultNumOutputBuffers) + kPicsInPipeline;
   if (!ctrl)
     VLOGF(1) << "Using default minimum number of CAPTURE buffers";
 
