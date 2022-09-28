9f2bf753cb988ab5f3d6a4e3d14e8f0fbb7452e8
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-09-28 16:58:22 +0000

    [Profiles] Don't create keepalives for OTR profiles
    We are getting crash reports for these 2 call-sites of
    ScopedProfileKeepAlive:
    
    - ProfileNotification
    - ManifestUpdateTask
    
    ScopedProfileKeepAlive doesn't support OTR profiles. These two
    code paths can trigger on an OTR profile though, most likely via the
    DevTools protocol. Add guards to prevent creating a keepalive for an
    OTR profile.
    
    Bug: 1362771
    Change-Id: Icde1898daa8694637ef94bb4eab02d602e3459a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906262
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052468}

--- chrome/browser/notifications/profile_notification.cc
+++ chrome/browser/notifications/profile_notification.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/keep_alive/profile_keep_alive_types.h"
 #include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/account_id/account_id.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
@@ -66,7 +67,10 @@ ProfileNotification::ProfileNotification(
   // the process or delete the Profile.
   keep_alive_ = std::make_unique<ScopedKeepAlive>(
       KeepAliveOrigin::NOTIFICATION, KeepAliveRestartOption::DISABLED);
-  if (profile_) {
+  if (profile_ && !profile_->IsOffTheRecord()) {
+    // No need to create a keepalive for Incognito profiles. Incognito
+    // notifications are cleaned up in
+    // NotificationUIManagerImpl::OnProfileWillBeDestroyed().
     profile_keep_alive_ = std::make_unique<ScopedProfileKeepAlive>(
         profile, ProfileKeepAliveOrigin::kNotification);
   }
