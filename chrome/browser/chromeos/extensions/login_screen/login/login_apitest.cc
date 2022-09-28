a49c39d85f3a8db710a6597d5f0baf514f76204e
Jinho Bang <zino@chromium.org>
2022-09-28 16:32:50 +0000

    Convert NOTIFICATION_APP_TERMINATING to use callback in c/b/chromeos
    We are in the process of eliminating NOTIFICATION_APP_TERMINATING. This
    CL converts listeners in //c/b/chromeos/extensions/login_screen to
    instead use browser_shutdown::AddOnAppTerminatingCallback().
    
    Bug: 1174781
    Change-Id: I031b7295abdf1958fd2437521a590eb94296f26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3920436
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052454}

--- chrome/browser/chromeos/extensions/login_screen/login/login_apitest.cc
+++ chrome/browser/chromeos/extensions/login_screen/login/login_apitest.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login_screen_apitest_base.h"
+#include "chrome/browser/lifetime/termination_notification.h"
 #include "chrome/browser/policy/extension_force_install_mixin.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/cryptohome/cryptohome_parameters.h"
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_type.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_main_parts.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/api/test/test_api.h"
@@ -231,13 +229,13 @@ IN_PROC_BROWSER_TEST_F(LoginApitest, LaunchManagedGuestSessionNoAccounts) {
 IN_PROC_BROWSER_TEST_F(LoginApitest, ExitCurrentSession) {
   SetUpDeviceLocalAccountPolicy();
   SetTestCustomArg(kData);
-  content::WindowedNotificationObserver termination_waiter(
-      chrome::NOTIFICATION_APP_TERMINATING,
-      content::NotificationService::AllSources());
 
+  base::RunLoop exit_waiter;
+  auto subscription =
+      browser_shutdown::AddAppTerminatingCallback(exit_waiter.QuitClosure());
   SetUpLoginScreenExtensionAndRunTest(kExitCurrentSession,
                                       /*assert_test_succeed=*/false);
-  termination_waiter.Wait();
+  exit_waiter.Run();
 
   PrefService* local_state = g_browser_process->local_state();
   EXPECT_EQ(kData, local_state->GetString(
