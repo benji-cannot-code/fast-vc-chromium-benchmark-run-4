ca7df6a1774ab0f9aec571b97a21b3b0cec151e5
Andrew Bregger <andrewbregger@google.com>
2022-09-28 23:23:37 +0000

    Added and Integrated SystemProfileProvider
    Integrated SystemProfileProvider with ChromeOSMetricsProvider and
    StructuredMetricsProvider. Introduced ChromeOSSystemProfileProvider
    unittest, which is a rename of ChromeOSMetricsProvider unit test. Since
    the functionality tests was moved to from ChromeOSMetricsProvider to
    ChromeOSSystemProfileProvider the test could be removed.
    
    Unused code is removed from ChromeOSMetricsProvider during refactor.
    
    Design Doc: https://docs.google.com/document/d/1MkrjPRBJ5mYhOoR0o1wytwAT_p2S0Rb-wB_B7jBSb4k/edit?usp=sharing&resourcekey=0-mNl_Nohea7Pz02WbTk_TEA
    
    Bug: 1355983
    Change-Id: Ia5ea9d688d429bd9e07e22debc80111b7b6f82de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3872257
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052702}

--- chrome/browser/ash/multidevice_setup/multidevice_setup_client_factory.h
+++ chrome/browser/ash/multidevice_setup/multidevice_setup_client_factory.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 
 class ChromeOSMetricsProviderTest;
+class ChromeOSSystemProfileProviderTest;
+class ChromeMetricsServiceClientTest;
 class Profile;
 
 namespace ash {
@@ -32,6 +34,8 @@ class MultiDeviceSetupClientFactory : public ProfileKeyedServiceFactory {
  private:
   friend struct base::DefaultSingletonTraits<MultiDeviceSetupClientFactory>;
   friend class ::ChromeOSMetricsProviderTest;
+  friend class ::ChromeOSSystemProfileProviderTest;
+  friend class ::ChromeMetricsServiceClientTest;
 
   MultiDeviceSetupClientFactory();
   ~MultiDeviceSetupClientFactory() override;
