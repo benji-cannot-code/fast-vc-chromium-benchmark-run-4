c6a8d02077faed10d6505be57e4355d9c19d98f4
Daniel Cheng <dcheng@chromium.org>
2022-09-28 12:39:09 +0000

    Use helper macros to define base::Features in //weblayer/browser
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=blundell@chromium.org
    
    Bug: 1364289
    Change-Id: Icc541e2d1d86464fcf327895560ef7c74c9b95c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922952
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052348}

--- weblayer/browser/weblayer_features.h
+++ weblayer/browser/weblayer_features.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace weblayer {
 
 #if BUILDFLAG(IS_ANDROID)
-extern const base::Feature kImmediatelyHideBrowserControlsForTest;
+BASE_DECLARE_FEATURE(kImmediatelyHideBrowserControlsForTest);
 #endif
 
 }  // namespace weblayer
