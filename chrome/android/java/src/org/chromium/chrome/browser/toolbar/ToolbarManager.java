f6c69307ec700461eae6f64531f89e5b7ebad044
Patrick Noland <pnoland@google.com>
2022-09-28 15:40:31 +0000

    [Toolbar] Optimize isLocationBarShown
    This method is surprisingly slow and is called 3 times every frame
    during omnibox focus/unfocus. We can optimize it in the common case by:
    * Short circuiting when not actively showing the start surface
    * Memoizing the homepage url in ToolbarManager
    
    For a bit of color on the slowness: the cost is dominated by fetching
    the Homepage url and checking if it's an NTP url. Fortunately we don't
    usually need to perform that work. When we do, we can use a memoized
    value since ToolbarManager is notified when the homepage state changes.
    
    Bug: 1368334
    Change-Id: Ib3107fbc67d431c53af4dab8600dc8674f88b54c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916849
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052424}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -252,6 +252,7 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
     private MenuButtonCoordinator mMenuButtonCoordinator;
     private MenuButtonCoordinator mOverviewModeMenuButtonCoordinator;
     private HomepageManager.HomepageStateListener mHomepageStateListener;
+    private String mHomepageUrl;
     private StatusBarColorController mStatusBarColorController;
     private final Supplier<ShareDelegate> mShareDelegateSupplier;
     private final ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
@@ -1122,6 +1123,7 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
                 !ReturnToChromeUtil.shouldImproveStartWhenFeedIsDisabled(mActivity));
         // clang-format on
         mHomepageStateListener = () -> {
+            mHomepageUrl = HomepageManager.getHomepageUri();
             Boolean wasHomepageEnabled = mHomepageEnabledSupplier.get();
             boolean isHomepageEnabled = HomepageManager.isHomepageEnabled();
             mHomepageEnabledSupplier.set(isHomepageEnabled);
@@ -1197,8 +1199,8 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
             if (mIsStartSurfaceRefactorEnabled && mLayoutStateProvider != null
                     && mLayoutStateProvider.getActiveLayoutType() == LayoutType.START_SURFACE) {
                 return false;
-            } else if (ReturnToChromeUtil.isStartSurfaceEnabled(mActivity)
-                    && mStartSurfaceState == StartSurfaceState.SHOWN_HOMEPAGE) {
+            } else if (mStartSurfaceState == StartSurfaceState.SHOWN_HOMEPAGE
+                    && ReturnToChromeUtil.isStartSurfaceEnabled(mActivity, mHomepageUrl)) {
                 return false;
             }
 
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -994,6 +994,7 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         TraceEvent.begin("ToolbarPhone.updateLocationBarLayoutForExpansionAnimation");
         if (mOptimizationsEnabled && isInTabSwitcherMode()) return;
 
+        boolean isLocationBarShownInNTP = isLocationBarShownInNTP();
         FrameLayout.LayoutParams locationBarLayoutParams =
                 mLocationBar.getPhoneCoordinator().getFrameLayoutParams();
         int currentLeftMargin = locationBarLayoutParams.leftMargin;
@@ -1043,7 +1044,7 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         if (currentTab != null) {
             getToolbarDataProvider().getNewTabPageDelegate().setUrlFocusChangeAnimationPercent(
                     mUrlFocusChangeFraction);
-            if (isLocationBarShownInNTP()
+            if (isLocationBarShownInNTP
                     && !getToolbarDataProvider().isInOverviewAndShowingOmnibox()) {
                 updateNtpTransitionAnimation();
             } else {
@@ -1082,7 +1083,7 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
             // But for the mShouldShowModernizeVisualUpdate, toolbar and locationbar will have
             // different color when they are focused than in NTP, so the color need to be updated in
             // this case.
-            if ((mShouldShowModernizeVisualUpdate || !isLocationBarShownInNTP())
+            if ((mShouldShowModernizeVisualUpdate || !isLocationBarShownInNTP)
                     && mTabSwitcherState == STATIC_TAB) {
                 int defaultColor = getToolbarDefaultColor();
                 int defaultLocationBarColor =
