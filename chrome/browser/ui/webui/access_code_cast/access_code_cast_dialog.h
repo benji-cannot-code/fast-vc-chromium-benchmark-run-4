d78feaf186e641de09cedca8bb4174a74ae3b8c7
Luke Cao <caott@google.com>
2022-09-28 20:14:09 +0000

    Close the first dialog when launching the second dialog from system tray. Add a browsertest to validate the change.
    This change fixes the crash on chromeOS when one tries to cast from the system tray with a cast dialog already opened in the browser.
    
    Bug: b/229890637
    Change-Id: Ia28cae31eab70f37cc7231730f4bf62885c980f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848751
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: Luke Cao <caott@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052598}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.h
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_ACCESS_CODE_CAST_ACCESS_CODE_CAST_DIALOG_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/media_router/media_cast_mode.h"
@@ -55,8 +56,12 @@ class AccessCodeCastDialog : public ui::WebDialogDelegate,
   static void ShowForDesktopMirroring(
       AccessCodeCastDialogOpenLocation open_location);
 
+  void CloseDialogWidget();
+
   // views::WidgetObserver:
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
+
+  base::WeakPtr<AccessCodeCastDialog> GetWeakPtr();
 
  protected:
   // Creates default params for showing AccessCodeCastDialog
@@ -111,6 +116,8 @@ class AccessCodeCastDialog : public ui::WebDialogDelegate,
   const raw_ptr<Profile> context_;
   base::Time dialog_creation_timestamp_;
   bool closing_dialog_ = false;
+
+  base::WeakPtrFactory<AccessCodeCastDialog> weak_ptr_factory_{this};
 };
 
 }  // namespace media_router
