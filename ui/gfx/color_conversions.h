6050b1562600aac7efbe3fb08a3c52e3d2b57f8e
Juanmihd <juanmihd@google.com>
2022-09-28 16:43:51 +0000

    Adding OKLAB, OKLCH, AdobeRGB and Rec2020 to SkColor4f conversion
    As part of the support of CSS Color 4, we need to support color spaces
    other than sRGB for inputing colors. For that we need to add support
    to some other color spaces.
    
    This CL particularly adds support to convert from AdobeRGB to sRGB and
    from Rec2020 to sRGB. This CL also adds OKLAB and OKLCH.
    
    There have been added extensive unittests for all those color
    transformations, and changing the expectation of some tests that now
    should be passing.
    
    Bug: 1358565
    Change-Id: I1c44023a2291a9ab619bf263e9cc7a7bb992a95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922121
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052457}

--- ui/gfx/color_conversions.h
+++ ui/gfx/color_conversions.h
@@ -20,6 +20,11 @@ GFX_EXPORT std::tuple<float, float, float> LabToXYZD50(float l,
                                                        float b);
 
 // Method exposed for testing purposes.
+GFX_EXPORT std::tuple<float, float, float> OKLabToXYZD65(float l,
+                                                         float a,
+                                                         float b);
+
+// Method exposed for testing purposes.
 GFX_EXPORT std::tuple<float, float, float> DisplayP3ToXYZD50(float r,
                                                              float g,
                                                              float b);
@@ -28,6 +33,16 @@ GFX_EXPORT std::tuple<float, float, float> DisplayP3ToXYZD50(float r,
 GFX_EXPORT std::tuple<float, float, float> ProPhotoToXYZD50(float r,
                                                             float g,
                                                             float b);
+
+// Method exposed for testing purposes.
+GFX_EXPORT std::tuple<float, float, float> AdobeRGBToXYZD50(float r,
+                                                            float g,
+                                                            float b);
+
+// Method exposed for testing purposes.
+GFX_EXPORT std::tuple<float, float, float> Rec2020ToXYZD50(float r,
+                                                           float g,
+                                                           float b);
 
 // Method exposed for testing purposes.
 GFX_EXPORT std::tuple<float, float, float> XYZD50toD65(float x,
@@ -49,6 +64,11 @@ GFX_EXPORT std::tuple<float, float, float> LchToLab(float l,
                                                     float c,
                                                     absl::optional<float> h);
 
+// Method exposed for testing purposes.
+GFX_EXPORT std::tuple<float, float, float> OKLchToLab(float l,
+                                                      float c,
+                                                      absl::optional<float> h);
+
 // Method exposed for blink::color conversions.
 GFX_EXPORT SkColor4f XYZD50ToSkColor4f(float x, float y, float z, float alpha);
 
@@ -57,6 +77,9 @@ GFX_EXPORT SkColor4f XYZD65ToSkColor4f(float x, float y, float z, float alpha);
 
 // Method exposed for blink::color conversions.
 GFX_EXPORT SkColor4f LabToSkColor4f(float l, float a, float b, float alpha);
+
+// Method exposed for blink::color conversions.
+GFX_EXPORT SkColor4f OKLabToSkColor4f(float l, float a, float b, float alpha);
 
 // Method exposed for blink::color conversions.
 GFX_EXPORT SkColor4f SRGBLinearToSkColor4f(float r,
@@ -77,10 +100,25 @@ GFX_EXPORT SkColor4f DisplayP3ToSkColor4f(float r,
                                           float alpha);
 
 // Method exposed for blink::color conversions.
+GFX_EXPORT SkColor4f AdobeRGBToSkColor4f(float r,
+                                         float g,
+                                         float b,
+                                         float alpha);
+
+// Method exposed for blink::color conversions.
+GFX_EXPORT SkColor4f Rec2020ToSkColor4f(float r, float g, float b, float alpha);
+
+// Method exposed for blink::color conversions.
 GFX_EXPORT SkColor4f LchToSkColor4f(float l,
                                     float a,
                                     absl::optional<float> b,
                                     float alpha);
+
+// Method exposed for blink::color conversions.
+GFX_EXPORT SkColor4f OKLchToSkColor4f(float l,
+                                      float a,
+                                      absl::optional<float> b,
+                                      float alpha);
 
 }  // namespace gfx
 
