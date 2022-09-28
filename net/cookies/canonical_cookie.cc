8a74285d6935f9186acac1cb206c47656a912249
sbingler <bingler@chromium.org>
2022-09-28 15:13:52 +0000

    Reject nameless cookies which impersonate valueless prefixed cookies
    This CL rejects nameless cookies whose values attempt to impersonate a
    prefixed cookie without a value.
    E.x.: "=__Secure-foo" impersonates the cookie "__Secure-foo=" and will
    be rejected.
    
    We already reject cookies attempting to impersonate a prefixed cookie
    with a value, e.x.: "=__Secure-foo=bar". This CL expands that.
    
    Since this check is used as part of IsCanonical(), any pre-existing
    cookies meeting this requirement will be deleted from the cookie jar.
    
    Bug: 1356760, 1354090
    Change-Id: I38933f4701cbaf18ed0c7c2c2958cbd08184007a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923735
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052410}

--- net/cookies/canonical_cookie.cc
+++ net/cookies/canonical_cookie.cc
@@ -1656,14 +1656,8 @@ bool CanonicalCookie::HasHiddenPrefixName(
   // Compare the value to the host_prefix.
   if (base::StartsWith(value_without_BWS, host_prefix,
                        base::CompareCase::INSENSITIVE_ASCII)) {
-    // The prefix matches, now check if the value string contains a subsequent
-    // '='.
-    if (value_without_BWS.find_first_of('=', host_prefix.size()) !=
-        base::StringPiece::npos) {
-      // This value contains a hidden prefix name.
-      return true;
-    }
-    return false;
+    // This value contains a hidden prefix name.
+    return true;
   }
 
   // Do a similar check for the secure prefix
@@ -1671,10 +1665,7 @@ bool CanonicalCookie::HasHiddenPrefixName(
 
   if (base::StartsWith(value_without_BWS, secure_prefix,
                        base::CompareCase::INSENSITIVE_ASCII)) {
-    if (value_without_BWS.find_first_of('=', secure_prefix.size()) !=
-        base::StringPiece::npos) {
-      return true;
-    }
+    return true;
   }
 
   return false;
