1785014d54e583539e44720b74dfae65c30fe8d4
Meyron Levanon <meyron@google.com>
2022-09-28 19:46:58 +0000

    Added e2e test for pubsub reporting connector
    Added an E2E test that verifies events are being sent to a Pub Sub instance
    via the reporting connector feature
    
    Bug: b:241008575
    Change-Id: I110734f331e3d219493427200d21507a7b974c58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835052
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Commit-Queue: Meyron Levanon <meyron@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052581}

--- chrome/test/enterprise/e2e/connector/__init__.py
+++ chrome/test/enterprise/e2e/connector/__init__.py
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2021 The Chromium Authors
+# Copyright 2022 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 from .realtime_reporting_bce.realtime_reporting_bce_test import *
+from .reporting_connector_pubsub.reporting_connector_pubsub_test import *
 from .reporting_connector_splunk.reporting_connector_splunk_test import *
