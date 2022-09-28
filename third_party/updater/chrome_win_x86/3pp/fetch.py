8385767ad7ff71738e9c303caf6e04e543c970c6
Avi Drissman <avi@chromium.org>
2022-09-28 16:46:09 +0000

    Update copyright headers in third_party/u*/
    These files are used to import new versions of third-party code, to
    integrate that code into Chromium, or are otherwise not from upstream.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I28b2fe4f16d7304d31784ab72f82458194f25034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924483
    Owners-Override: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052460}

--- third_party/updater/chrome_win_x86/3pp/fetch.py
+++ third_party/updater/chrome_win_x86/3pp/fetch.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python3
-# Copyright 2022 The Chromium Authors. All rights reserved.
+# Copyright 2022 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
