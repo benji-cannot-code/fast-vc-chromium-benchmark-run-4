65d6718c1294415939741b4686611b1e36dc4667
Matt Menke <mmenke@chromium.org>
2022-09-28 20:13:58 +0000

    Migrate DictionaryPrefUpdate usage in components/optimization_guide/core
    DictionaryPrefUpdate is deprecated. ScopedDictPrefUpdate only allows
    access via the newer Value::Dict API, and returns references rather
    than pointers (The pref system itself has CHECKs/DCHECKs that the
    value is registered and of the right type, so consumers do not need to
    check that).
    
    Also fix some consumers that shouldn't be using ScopedDictPrefUpdate.
    Consumers that only read the value should not use it, to avoid sending
    a bogus pref update notification, and consumers that completely
    overwrite the value should be using PrefService::SetDict(), to
    reduce code size (and potentially slightly improve perf).
    
    Bug: 1362719
    Change-Id: Ie4a55b8af67068be10f2d7d2defd589d3e6d1cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915079
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052597}

--- components/optimization_guide/core/optimization_guide_store_unittest.cc
+++ components/optimization_guide/core/optimization_guide_store_unittest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/hint_cache.pb.h"
 #include "components/optimization_guide/proto/models.pb.h"
 #include "components/prefs/pref_registry_simple.h"
-#include "components/prefs/scoped_user_pref_update.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -418,9 +417,9 @@ class OptimizationGuideStoreTest : public testing::Test {
   }
 
   bool IsStoreFilesToDeletePrefEmpty() {
-    DictionaryPrefUpdate pref_update(pref_service_.get(),
-                                     prefs::kStoreFilePathsToDelete);
-    return pref_update.Get()->DictEmpty();
+    const base::Value::Dict& pref_dict =
+        pref_service_->GetDict(prefs::kStoreFilePathsToDelete);
+    return pref_dict.empty();
   }
 
   void RunUntilIdle() { task_environment_.RunUntilIdle(); }
