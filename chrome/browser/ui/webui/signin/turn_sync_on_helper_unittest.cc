7921206ea2f6dd6b3836798b946628f186eec2b6
Mikel Astiz <mastiz@chromium.org>
2022-09-28 10:39:29 +0000

    Simply or fix handling of temp dirs in unit-tests
    The general idea is to leverage the built-in functionality in
    TestingProfile to handle a temporary profile directory. This is simpler
    and, because it leans on base::CreateUniqueTempDirectoryScopedToTest(),
    it is more robust against leaking files across tests (see linked bug).
    
    This patch tackles specifically tests under
    /chrome/browser/ui/webui/signin
    
    This CL was uploaded by git cl split.
    
    R=bsazonov@chromium.org
    
    Bug: 546640
    Change-Id: Ica7bb3078db3a626a3aa27130a01efc6cd4643ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921736
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052316}

--- chrome/browser/ui/webui/signin/turn_sync_on_helper_unittest.cc
+++ chrome/browser/ui/webui/signin/turn_sync_on_helper_unittest.cc
@@ -15,11 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/files/scoped_temp_dir.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_file_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
@@ -343,14 +343,15 @@ class TurnSyncOnHelperTest : public testing::Test {
   TurnSyncOnHelperTest() : local_state_(TestingBrowserProcess::GetGlobal()) {}
 
   void SetUp() override {
-    EXPECT_TRUE(temp_dir_.CreateUniqueTempDir());
+    const base::FilePath temp_user_data_dir =
+        base::CreateUniqueTempDirectoryScopedToTest();
     TestingBrowserProcess::GetGlobal()->SetProfileManager(
         std::make_unique<UnittestProfileManager>(
-            temp_dir_.GetPath(),
+            temp_user_data_dir,
             base::BindRepeating(&TurnSyncOnHelperTest::BuildTestingProfile,
                                 base::Unretained(this))));
     auto testing_profile = BuildTestingProfile(
-        temp_dir_.GetPath().Append(FILE_PATH_LITERAL("profile")),
+        temp_user_data_dir.Append(FILE_PATH_LITERAL("profile")),
         /*delegate=*/nullptr);
     profile_ = testing_profile.get();
 
@@ -364,7 +365,7 @@ class TurnSyncOnHelperTest : public testing::Test {
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
     // Lacros expects that the main profile always exists.
     auto main_profile = BuildTestingProfile(
-        temp_dir_.GetPath().Append(FILE_PATH_LITERAL("Default")),
+        temp_user_data_dir.Append(FILE_PATH_LITERAL("Default")),
         /*delegate=*/nullptr);
     profile_manager()->RegisterTestingProfile(std::move(main_profile),
                                               /*add_to_storage=*/true);
@@ -758,7 +759,6 @@ class TurnSyncOnHelperTest : public testing::Test {
  private:
   content::BrowserTaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
-  base::ScopedTempDir temp_dir_;
   ScopedTestingLocalState local_state_;
   CoreAccountId account_id_;
   raw_ptr<TestingProfile> profile_;
