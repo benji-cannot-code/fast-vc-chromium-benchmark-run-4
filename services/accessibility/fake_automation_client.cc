6519082a5c41fcee6b2ae1d4b113def4a40f98c2
Katie Dektar <katie@chromium.org>
2022-09-28 21:22:39 +0000

    Adds skeleton tests for AccessibilityService.
    Testing framework includes fake service clients/callers
    and allows for binding. Tests work on Linux and Chrome OS.
    
    Bug: 1355633
    Change-Id: I45c6132273678cffa594592399ce1d45b35ff46b
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916684
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052635}

---
+++ services/accessibility/fake_automation_client.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/accessibility/fake_automation_client.h"
+
+namespace ax {
+FakeAutomationClient::FakeAutomationClient(mojom::AccessibilityService* service)
+    : service_(service) {}
+
+FakeAutomationClient::~FakeAutomationClient() = default;
+
+void FakeAutomationClient::BindToAutomation() {
+  service_->BindAutomation(
+      automation_client_receiver_.BindNewPipeAndPassRemote(),
+      automation_.BindNewPipeAndPassReceiver());
+}
+
+bool FakeAutomationClient::IsBound() {
+  return automation_.is_bound() && automation_client_receiver_.is_bound();
+}
+
+}  // namespace ax
