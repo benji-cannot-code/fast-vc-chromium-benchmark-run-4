df3f81d8c0c0fafefc5862ed71ba27e41cd46830
David Bienvenu <davidbienvenu@chromium.org>
2022-09-28 23:09:44 +0000

    win: use IPinnedList3 to unpin shortcuts, fallback to shellexecute
    ~2% of the utility processes to unpin shortcuts from the taskbar fail
    with a process error, possibly because the shellexecute is crashing.
    This CL first tries IPinnedList3::Modify to unpin the shortcut, and
    if that fails, falls back to shellexecute of taskbarunpin. I will see
    if that reduces the Windows.UnpinShortcut.ProcessError metric.
    
    TESTED=manual
    
    Change-Id: Ib57700a22342f2f709be38d57ccc28cfd3646450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922768
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052689}

--- chrome/services/util_win/util_win_impl.cc
+++ chrome/services/util_win/util_win_impl.cc
@@ -241,6 +241,7 @@ void UtilWinImpl::IsPinnedToTaskbar(IsPinnedToTaskbarCallback callback) {
 void UtilWinImpl::UnpinShortcuts(
     const std::vector<base::FilePath>& shortcut_paths,
     UnpinShortcutsCallback callback) {
+  base::win::ScopedCOMInitializer scoped_com_initializer;
   for (const auto& shortcut_path : shortcut_paths)
     UnpinShortcutFromTaskbar(shortcut_path);
 
