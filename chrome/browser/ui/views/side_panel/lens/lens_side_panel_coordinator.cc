9f28b45122ca24466d626639e137c66f44fe59f7
Duncan Mercer <mercerd@google.com>
2022-09-28 17:25:48 +0000

    #Lens Use image search branding label for unified side panel dropdown
    This change allows for Google Lens and 3P DSE to set the image search
    branding that goes in the dropdown menu in a consistent way.
    
    Bug: b:243957954
    Change-Id: I7d65fc84eb5c44a66251b5b393133f4f5eba733c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914009
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052485}

--- chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/lens/lens_side_panel_coordinator.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
-#include "chrome/grit/generated_resources.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/omnibox/browser/favicon_cache.h"
 #include "components/omnibox/browser/vector_icons.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/util.h"
 #include "components/vector_icons/vector_icons.h"
-#include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/image/image.h"
 #include "ui/views/vector_icons.h"
 
@@ -135,11 +133,10 @@ bool LensSidePanelCoordinator::IsDefaultSearchProviderGoogle() {
 }
 
 std::u16string LensSidePanelCoordinator::GetComboboxLabel() {
-  if (IsDefaultSearchProviderGoogle())
-    return l10n_util::GetStringUTF16(
-        IDS_SIDE_PANEL_COMBO_BOX_GOOGLE_LENS_LABEL);
-
-  return GetDefaultSearchEngineName(template_url_service_);
+  // Assuming not nullptr because side panel can't be opened if default search
+  // provider is not initialized
+  DCHECK(current_default_search_provider_);
+  return current_default_search_provider_->image_search_branding_label();
 }
 
 const ui::ImageModel LensSidePanelCoordinator::GetFaviconImage() {
