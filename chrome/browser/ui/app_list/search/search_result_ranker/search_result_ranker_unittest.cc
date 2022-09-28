c2188386eaad67efd9d556a881aaf858ed9328fd
Mikel Astiz <mastiz@chromium.org>
2022-09-28 02:28:07 +0000

    Simply or fix handling of temp dirs in unit-tests
    The general idea is to leverage the built-in functionality in
    TestingProfile to handle a temporary profile directory. This is simpler
    and, because it leans on base::CreateUniqueTempDirectoryScopedToTest(),
    it is more robust against leaking files across tests (see linked bug).
    
    This patch tackles specifically tests under
    /chrome/browser/ui/app_list/search/search_result_ranker
    
    This CL was uploaded by git cl split.
    
    R=tby@chromium.org
    
    Bug: 546640
    Change-Id: Ie997bf89cb06c1dc970712676243ce59248b1ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921735
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052169}

--- chrome/browser/ui/app_list/search/search_result_ranker/search_result_ranker_unittest.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/search_result_ranker_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "base/files/file_util.h"
-#include "base/files/scoped_temp_dir.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -48,7 +47,6 @@ namespace {
 
 using ResultType = ash::AppListSearchResultType;
 
-using base::ScopedTempDir;
 using base::test::ScopedFeatureList;
 using testing::ElementsAre;
 using testing::StrEq;
@@ -111,10 +109,8 @@ class SearchResultRankerTest : public testing::Test {
 
   // testing::Test overrides:
   void SetUp() override {
-    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     TestingProfile::Builder profile_builder;
     profile_builder.SetProfileName("testuser@gmail.com");
-    profile_builder.SetPath(temp_dir_.GetPath().AppendASCII("TestProfile"));
     profile_builder.AddTestingFactory(
         HistoryServiceFactory::GetInstance(),
         base::BindRepeating(&BuildHistoryService));
@@ -160,7 +156,6 @@ class SearchResultRankerTest : public testing::Test {
   // to.
   std::list<TestResult> test_search_results_;
 
-  ScopedTempDir temp_dir_;
   content::BrowserTaskEnvironment task_environment_;
   data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
 
