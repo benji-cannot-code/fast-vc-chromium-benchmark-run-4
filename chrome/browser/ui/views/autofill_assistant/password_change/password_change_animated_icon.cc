1128fdf035c2ecdd5052bbc8836c522803bcaad1
Jan Keitel <jkeitel@google.com>
2022-09-28 15:48:03 +0000

    [APC Desktop] Allow icons animations to be paused and resumed.
    This CL fixes a bug in which icons were not properly paused if
    the currently animated icon was not the one corresponding to the
    current progress step.
    In addition to that, it adds test coverage for the entire
    PasswordChangeRunProgress class.
    
    Bug: 1366424
    Change-Id: If5c5e653b9c5560d7dff83420c90b1a32d4f4c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922194
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052429}

--- chrome/browser/ui/views/autofill_assistant/password_change/password_change_animated_icon.cc
+++ chrome/browser/ui/views/autofill_assistant/password_change/password_change_animated_icon.cc
@@ -15,25 +15,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/views/controls/image_view.h"
 
+namespace gfx {
+class AnimationContainer;
+}  // namespace gfx
+
 namespace {
 
 constexpr int kIconSize = 16;
 
 const gfx::VectorIcon& ProgressStepToIcon(
     autofill_assistant::password_change::ProgressStep progress_step) {
+  using autofill_assistant::password_change::ProgressStep;
   switch (progress_step) {
-    case autofill_assistant::password_change::ProgressStep::
-        PROGRESS_STEP_UNSPECIFIED:
-    case autofill_assistant::password_change::ProgressStep::PROGRESS_STEP_START:
+    case ProgressStep::PROGRESS_STEP_UNSPECIFIED:
+    case ProgressStep::PROGRESS_STEP_START:
       return autofill_assistant::password_change::
           kPasswordChangeProgressStartIcon;
-    case autofill_assistant::password_change::ProgressStep::
-        PROGRESS_STEP_CHANGE_PASSWORD:
+    case ProgressStep::PROGRESS_STEP_CHANGE_PASSWORD:
       return vector_icons::kSettingsIcon;
-    case autofill_assistant::password_change::ProgressStep::
-        PROGRESS_STEP_SAVE_PASSWORD:
+    case ProgressStep::PROGRESS_STEP_SAVE_PASSWORD:
       return kKeyIcon;
-    case autofill_assistant::password_change::ProgressStep::PROGRESS_STEP_END:
+    case ProgressStep::PROGRESS_STEP_END:
       return vector_icons::kCheckCircleIcon;
   }
 }
@@ -42,8 +44,11 @@ const gfx::VectorIcon& ProgressStepToIcon(
 
 PasswordChangeAnimatedIcon::PasswordChangeAnimatedIcon(
     int id,
-    autofill_assistant::password_change::ProgressStep progress_step)
-    : gfx::LinearAnimation(this), progress_step_(progress_step) {
+    autofill_assistant::password_change::ProgressStep progress_step,
+    Delegate* delegate)
+    : gfx::LinearAnimation(this),
+      progress_step_(progress_step),
+      delegate_(delegate) {
   SetID(id);
   SetHorizontalAlignment(views::ImageView::Alignment::kLeading);
   SetImage(ui::ImageModel::FromVectorIcon(ProgressStepToIcon(progress_step_),
@@ -51,11 +56,6 @@ PasswordChangeAnimatedIcon::PasswordChangeAnimatedIcon(
 }
 
 PasswordChangeAnimatedIcon::~PasswordChangeAnimatedIcon() = default;
-
-void PasswordChangeAnimatedIcon::SetAnimationEndedCallback(
-    base::OnceClosure callback) {
-  animation_ended_callback_ = std::move(callback);
-}
 
 void PasswordChangeAnimatedIcon::StartPulsingAnimation(bool pulse_once) {
   bool is_already_pulsing = IsPulsing();
@@ -93,10 +93,13 @@ void PasswordChangeAnimatedIcon::AnimationEnded(
     Start();
   } else {
     animation_ended_ = true;
-    if (animation_ended_callback_) {
-      std::move(animation_ended_callback_).Run();
-    }
+    delegate_->OnAnimationEnded(this);
   }
+}
+
+void PasswordChangeAnimatedIcon::AnimationContainerWasSet(
+    gfx::AnimationContainer* container) {
+  delegate_->OnAnimationContainerWasSet(this, container);
 }
 
 bool PasswordChangeAnimatedIcon::IsPulsing() const {
