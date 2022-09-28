0b9fab2bf61785c982005465b637c4b261087a79
Jérôme Lebel <jlebel@chromium.org>
2022-09-28 21:06:01 +0000

    [iOS] Prevent recording FRE metrics in none FRE case for SyncScreen
    RecordFirstRunScrollButtonVisibilityMetrics() should not be called
    if the screen is not used for FRE.
    
    Change-Id: I3a193e136ea3854315eadb69ebfec51a858ccbd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925623
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052620}

--- ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
@@ -217,6 +217,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)logScrollButtonVisible:(BOOL)scrollButtonVisible {
+  if (!self.firstRun) {
+    return;
+  }
   RecordFirstRunScrollButtonVisibilityMetrics(
       first_run::FirstRunScreenType::kSyncScreenWithoutIdentityPicker,
       scrollButtonVisible);
