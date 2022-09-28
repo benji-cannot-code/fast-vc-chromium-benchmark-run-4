4c6fa59ff7dc159d9a2a8b4fd9142d317ae5ac4c
Peter Boström <pbos@chromium.org>
2022-09-28 23:15:56 +0000

    Add a prefer-dcheck os://flags entry for lacros
    This flag allows sending an opt-in / opt-out key (or unset/default) to
    the update server which'll be set up to put you in the DCHECK cohort if
    you're running a channel which has a DCHECK build (currently only
    Canary).
    
    Bug: 1369212
    Change-Id: I6c21e244a69a9faf42f3283b88847c069c3f6020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3926196
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052695}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3347,6 +3347,9 @@ extern const char kPhoneHubCameraRollDescription[];
 extern const char kPhoneHubFeatureSetupErrorHandlingName[];
 extern const char kPhoneHubFeatureSetupErrorHandlingDescription[];
 
+extern const char kPreferDcheckName[];
+extern const char kPreferDcheckDescription[];
+
 extern const char kProductivityLauncherName[];
 extern const char kProductivityLauncherDescription[];
 
