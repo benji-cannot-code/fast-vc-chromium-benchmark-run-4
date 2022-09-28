429fdc4d739d419298ecfafa481dcdebd82f624c
Andres Salomon <dilinger@queued.net>
2022-09-28 03:41:16 +0000

    Fix missing cmath header build failure in renderer extension
    Building 106.0.5249.51 on a Debian machine, I hit the following
    build error:
    
    FAILED: obj/extensions/renderer/renderer/argument_spec.o
    clang++ [...] -o obj/extensions/renderer/renderer/argument_spec.o
    ../../extensions/renderer/bindings/argument_spec.cc:410:16: error: no member named 'isnan' in namespace 'std'
          if (std::isnan(double_val) || std::isinf(double_val)) {
              ~~~~~^
    ../../extensions/renderer/bindings/argument_spec.cc:410:42: error: no member named 'isinf' in namespace 'std'
          if (std::isnan(double_val) || std::isinf(double_val)) {
                                        ~~~~~^
    2 errors generated.
    
    isnan and isinf are both found in the cmath header, so simply
    including it fixes that build error.
    
    R=rdevlin.cronin@chromium.org, tjudkins@chromium.org
    
    Change-Id: I95c49a8f90a5a734b62b8988dc3138e3afa3c6f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913616
    Commit-Queue: Andres Salomon <dilinger@queued.net>
    Auto-Submit: Andres Salomon <dilinger@queued.net>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052196}

--- extensions/renderer/bindings/argument_spec.cc
+++ extensions/renderer/bindings/argument_spec.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/renderer/bindings/argument_spec.h"
 
+#include <cmath>
+
 #include "base/check.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
