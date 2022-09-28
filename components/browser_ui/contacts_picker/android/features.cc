d34676bf659f11a137806d7f042030f67f8485ce
Daniel Cheng <dcheng@chromium.org>
2022-09-28 14:28:26 +0000

    Use helper macros to define base::Features in //components/browser_ui/contacts_picker/android
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=rayankans@chromium.org
    
    Bug: 1364289
    Change-Id: Ibc09a5f059a67c4e49eb86e7e8d59c20f72b4b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916973
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052384}

--- components/browser_ui/contacts_picker/android/features.cc
+++ components/browser_ui/contacts_picker/android/features.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_ui {
 
-const base::Feature kContactsPickerSelectAll{"ContactsPickerSelectAll",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kContactsPickerSelectAll,
+             "ContactsPickerSelectAll",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace browser_ui
--- components/browser_ui/contacts_picker/android/features.h
+++ components/browser_ui/contacts_picker/android/features.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_ui {
 
-extern const base::Feature kContactsPickerSelectAll;
+BASE_DECLARE_FEATURE(kContactsPickerSelectAll);
 
 }  // namespace browser_ui
 
