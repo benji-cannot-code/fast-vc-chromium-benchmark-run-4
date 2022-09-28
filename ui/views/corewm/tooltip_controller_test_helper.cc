f579a6d97cd538e53896aa0ee4bdc393f2f64c4c
Eriko Kurimoto <elkurin@chromium.org>
2022-09-28 02:29:12 +0000

    Tooltip: Remove unused methods to manipulate TooltipTimer
    This CL removes two unused methods. WillShowTooltipTimer() and
    WillHideTooltipTimer().
    
    WillShowTooltipTimer() is never called now.
    WillHideTooltipTimer() is called twice, but both of them are not needed
    since TooltipStateManager::Show calls TooltipStateManager::HideAndRest
    at the beginning and HideAndReset stops will_hide_tooltip_timer.
    
    This CL doesn't affect the behavior.
    
    Change-Id: I14877d9be1a9fcf4b55f6cc8e047dad954b1a4b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918037
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052171}

--- ui/views/corewm/tooltip_controller_test_helper.cc
+++ ui/views/corewm/tooltip_controller_test_helper.cc
@@ -45,7 +45,6 @@ void TooltipControllerTestHelper::UpdateIfRequired(TooltipTrigger trigger) {
 }
 
 void TooltipControllerTestHelper::FireHideTooltipTimer() {
-  controller_->state_manager_->StopWillHideTooltipTimer();
   controller_->state_manager_->HideAndReset();
 }
 
