673420370f7db121500d4980a4f8372aea353713
Ben Reich <benreich@chromium.org>
2022-09-28 09:20:38 +0000

    Update SWA notification to Files app icon
    The current notification is the Chrome icon, this updates it to use the
    Skia vector icon for Files app.
    
    Bug: b:194636708, b:211519500
    Test: Manual
    Change-Id: Ie26b98305c2b127bc4612fb6914c43e947b1a540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907068
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052297}

--- chrome/browser/chromeos/extensions/file_manager/system_notification_manager.cc
+++ chrome/browser/chromeos/extensions/file_manager/system_notification_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/extensions/file_manager/system_notification_manager.h"
 
 #include "ash/components/arc/arc_prefs.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/webui/file_manager/file_manager_ui.h"
 #include "base/bind.h"
 #include "base/memory/ref_counted.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom-forward.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/prefs/pref_service.h"
-#include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -149,8 +149,7 @@ SystemNotificationManager::CreateNotification(
   return ash::CreateSystemNotification(
       message_center::NOTIFICATION_TYPE_SIMPLE, notification_id, title, message,
       app_name_, GURL(), message_center::NotifierId(),
-      message_center::RichNotificationData(), delegate,
-      vector_icons::kProductIcon,
+      message_center::RichNotificationData(), delegate, ash::kFilesAppIcon,
       message_center::SystemNotificationWarningLevel::NORMAL);
 }
 
@@ -230,7 +229,7 @@ SystemNotificationManager::CreateProgressNotification(
       base::MakeRefCounted<message_center::HandleNotificationClickDelegate>(
           base::BindRepeating(&SystemNotificationManager::HandleProgressClick,
                               weak_ptr_factory_.GetWeakPtr(), notification_id)),
-      vector_icons::kProductIcon,
+      ash::kFilesAppIcon,
       message_center::SystemNotificationWarningLevel::NORMAL);
 }
 
@@ -252,7 +251,7 @@ SystemNotificationManager::CreateIOTaskProgressNotification(
           base::BindRepeating(&SystemNotificationManager::CancelTaskId,
                               weak_ptr_factory_.GetWeakPtr(), task_id,
                               notification_id)),
-      vector_icons::kProductIcon,
+      ash::kFilesAppIcon,
       message_center::SystemNotificationWarningLevel::NORMAL);
 
   // Add the cancel button:
