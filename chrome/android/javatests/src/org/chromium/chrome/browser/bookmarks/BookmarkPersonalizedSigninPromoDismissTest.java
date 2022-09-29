d7ab3196c66ab43981e114dc51ee94ebac42c77c
Mahmoud Rashad <mmrashad@google.com>
2022-09-29 12:34:31 +0000

    [Sync] Rename signin_promo_close_button to sync_promo_close_button
    This CL automatically renames signin_promo_close_button to
    sync_promo_close_button using VSCode Find and replace, no
    behavioral changes.
    
    Bug: 1319351
    Change-Id: I56a92a2475a4f9530d293f5e0e9d5014a1239c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924077
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052889}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoDismissTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoDismissTest.java
@@ -97,7 +97,7 @@ public class BookmarkPersonalizedSigninPromoDismissTest {
     public void testPromoNotShownAfterBeingDismissed() {
         mBookmarkTestRule.showBookmarkManager(mSyncTestRule.getActivity());
         onViewWaiting(allOf(withId(R.id.signin_promo_view_container), isDisplayed()));
-        onView(withId(R.id.signin_promo_close_button)).perform(click());
+        onView(withId(R.id.sync_promo_close_button)).perform(click());
         onView(withId(R.id.signin_promo_view_container)).check(doesNotExist());
 
         closeBookmarkManager();
@@ -117,7 +117,7 @@ public class BookmarkPersonalizedSigninPromoDismissTest {
 
         mBookmarkTestRule.showBookmarkManager(mSyncTestRule.getActivity());
         onViewWaiting(allOf(withId(R.id.signin_promo_view_container), isDisplayed()));
-        onView(withId(R.id.signin_promo_close_button)).perform(click());
+        onView(withId(R.id.sync_promo_close_button)).perform(click());
         onView(withId(R.id.signin_promo_view_container)).check(doesNotExist());
 
         closeBookmarkManager();
