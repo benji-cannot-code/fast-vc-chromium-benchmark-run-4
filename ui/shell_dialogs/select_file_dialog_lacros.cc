dfce8dd6762c576d0b8933a9f04952603357d72c
Aida Zolic <aidazolic@chromium.org>
2022-09-29 14:47:38 +0000

    Add caller parameter in select_file.mojom
    Adds a caller parameter to pass information about the caller's URL from
    `SelectFileDialog` to `SelectFileDialogLacros` and then to ash.
    
    Bug: 1364565
    Change-Id: Iea4c8f51de753ed7563ddeadfdbfe6025c5e2caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918172
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052975}

--- ui/shell_dialogs/select_file_dialog_lacros.cc
+++ ui/shell_dialogs/select_file_dialog_lacros.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
+#include "chromeos/crosapi/mojom/select_file.mojom-shared.h"
 #include "chromeos/crosapi/mojom/select_file.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/shell_dialogs/select_file_dialog.h"
 #include "ui/shell_dialogs/select_file_policy.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 #include "url/gurl.h"
@@ -144,6 +146,9 @@ void SelectFileDialogLacros::SelectFileImpl(
   if (owning_window) {
     owning_shell_window_id_ = GetShellWindowUniqueId(owning_window);
     options->owning_shell_window_id = owning_shell_window_id_;
+  }
+  if (caller && caller->is_valid()) {
+    options->caller = *caller;
   }
 
   // Send request to ash-chrome.
