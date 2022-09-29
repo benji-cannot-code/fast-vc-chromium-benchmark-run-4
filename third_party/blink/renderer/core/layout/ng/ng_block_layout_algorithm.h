390702ffa69ce1b17e9cb2d77644cf9071c51b61
Morten Stenshorne <mstensho@chromium.org>
2022-09-29 20:34:41 +0000

    Add constraint space flag for being pushed by floats.
    When clearance has already been applied in a previous layout pass (along
    with resolving the BFC block-offset) that aborted, or if we apply
    clearance before entering child layout at all, we won't be able to
    detect this during child layout, since the BFC block-offset has already
    been pushed down in the constraint space. We therefore need a flag. This
    allows us to correctly identify class C breakpoints [1].
    
    [1] https://www.w3.org/TR/css-break-3/#possible-breaks
    
    Set this flag before entering layout when we have pre-applied clearance.
    There are two different locations that do this. Added one test for each.
    
    Bug: 1366263
    Change-Id: I84e4c01276fe035720cdc7e6d626fcfaf247f414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921635
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053208}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
@@ -47,6 +47,7 @@ struct NGInflowChildData {
   NGBoxStrut margins;
   bool margins_fully_resolved;
   bool allow_discard_start_margin;
+  bool is_pushed_by_floats = false;
 };
 
 // A class for general block layout (e.g. a <div> with no special style).
--- third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -730,6 +730,12 @@ class CORE_EXPORT NGConstraintSpace final {
     return HasRareData() ? rare_data_->ClearanceOffset() : LayoutUnit::Min();
   }
 
+  // Return true if the BFC block-offset has been increased by the presence of
+  // floats (e.g. clearance).
+  bool IsPushedByFloats() const {
+    return HasRareData() && rare_data_->is_pushed_by_floats;
+  }
+
   // Return true if this is participating within a -webkit-line-clamp context.
   bool IsLineClampContext() const {
     return HasRareData() && rare_data_->is_line_clamp_context;
@@ -869,6 +875,7 @@ class CORE_EXPORT NGConstraintSpace final {
         : bfc_offset(bfc_offset),
           data_union_type(static_cast<unsigned>(DataUnionType::kNone)),
           is_line_clamp_context(false),
+          is_pushed_by_floats(false),
           is_restricted_block_size_table_cell(false),
           hide_table_cell_if_empty(false),
           block_direction_fragmentation_type(
@@ -896,6 +903,7 @@ class CORE_EXPORT NGConstraintSpace final {
           fragmentainer_offset_at_bfc(other.fragmentainer_offset_at_bfc),
           data_union_type(other.data_union_type),
           is_line_clamp_context(other.is_line_clamp_context),
+          is_pushed_by_floats(other.is_pushed_by_floats),
           is_restricted_block_size_table_cell(
               other.is_restricted_block_size_table_cell),
           hide_table_cell_if_empty(other.hide_table_cell_if_empty),
@@ -977,6 +985,7 @@ class CORE_EXPORT NGConstraintSpace final {
     bool MaySkipLayout(const RareData& other) const {
       if (data_union_type != other.data_union_type ||
           is_line_clamp_context != other.is_line_clamp_context ||
+          is_pushed_by_floats != other.is_pushed_by_floats ||
           is_restricted_block_size_table_cell !=
               other.is_restricted_block_size_table_cell ||
           hide_table_cell_if_empty != other.hide_table_cell_if_empty ||
@@ -1019,7 +1028,8 @@ class CORE_EXPORT NGConstraintSpace final {
     bool IsInitialForMaySkipLayout() const {
       if (page_name || fragmentainer_block_size != kIndefiniteSize ||
           fragmentainer_offset_at_bfc || is_line_clamp_context ||
-          is_restricted_block_size_table_cell || hide_table_cell_if_empty ||
+          is_pushed_by_floats || is_restricted_block_size_table_cell ||
+          hide_table_cell_if_empty ||
           block_direction_fragmentation_type != kFragmentNone ||
           requires_content_before_breaking || is_inside_balanced_columns ||
           should_ignore_forced_breaks || is_in_column_bfc ||
@@ -1302,6 +1312,7 @@ class CORE_EXPORT NGConstraintSpace final {
     unsigned data_union_type : 3;
 
     unsigned is_line_clamp_context : 1;
+    unsigned is_pushed_by_floats : 1;
 
     unsigned is_restricted_block_size_table_cell : 1;
     unsigned hide_table_cell_if_empty : 1;
