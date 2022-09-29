d62d92fa809ea5cb1b75b73f1ca07913b910207c
Nicholas Hollingum <hollingum@google.com>
2022-09-29 02:28:38 +0000

    borealis: Add a dialog which shows when borealis is disallowed
    This dialog will be shown to users at the start of the installation if
    borealis is not allowed on their device.
    
    Bug: b/233297090
    Change-Id: I85724d465df97788a25321fa9d3bcb22d3bbf582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917633
    Reviewed-by: Lucy Qu <lqu@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052783}

---
+++ chrome/browser/ui/views/borealis/borealis_installer_disallowed_dialog.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_INSTALLER_DISALLOWED_DIALOG_H_
+#define CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_INSTALLER_DISALLOWED_DIALOG_H_
+
+#include "chrome/browser/ash/borealis/borealis_features.h"
+
+namespace views::borealis {
+
+void ShowInstallerDisallowedDialog(
+    ::borealis::BorealisFeatures::AllowStatus status);
+
+}
+
+#endif  // CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_INSTALLER_DISALLOWED_DIALOG_H_
