ed1a051717acc54452139c6a1f54ed3570c9aacc
Dave Tapuska <dtapuska@chromium.org>
2022-09-29 20:22:21 +0000

    Use MainThreadScheduler's task runner for ScriptRunnerTest
    Access the MainThreadSchedulerImpl's default task runner instead of
    going through the deprecated task runner.
    
    BUG=624696
    
    Change-Id: Ic6b364e67164376eed56605728f1ae6cf03e11be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923254
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053200}

--- third_party/blink/renderer/core/script/script_runner_test.cc
+++ third_party/blink/renderer/core/script/script_runner_test.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/runtime_call_stats.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support_with_mock_scheduler.h"
 
 using testing::InvokeWithoutArgs;
@@ -93,7 +94,7 @@ class ScriptRunnerTest : public testing::Test {
     // Give ScriptRunner a task runner that platform_ will pump in
     // RunUntilIdle()/RunSingleTask().
     script_runner_->SetTaskRunnerForTesting(
-        Thread::Current()->GetDeprecatedTaskRunner().get());
+        platform_->GetMainThreadScheduler()->DefaultTaskRunner().get());
     RuntimeCallStats::SetRuntimeCallStatsForTesting();
   }
   void TearDown() override {
