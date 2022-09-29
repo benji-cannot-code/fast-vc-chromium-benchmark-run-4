c231274c6b71e39ff81e6d562ee7662cad1be150
Filip Gorski <fgorski@chromium.org>
2022-09-29 19:06:27 +0000

    [Fixit][Bookmarks] Extract BookmarkItem to components
    This is almost a pure move.
    One of the methods used ReadingList related functionality and was
    moved to BookmarkUtils.
    
    Bug: 1150129
    Change-Id: Iff873e5c462c89229441bfd88fbbf12d403ef02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928325
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053114}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSaveFlowMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSaveFlowMediator.java
@@ -15,7 +15,6 @@ import org.chromium.base.Callback;
 import org.chromium.base.CallbackController;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkModelObserver;
 import org.chromium.chrome.browser.bookmarks.PowerBookmarkMetrics.PriceTrackingState;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
@@ -23,6 +22,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.subscriptions.CommerceSubscription;
 import org.chromium.chrome.browser.subscriptions.SubscriptionsManager;
 import org.chromium.components.bookmarks.BookmarkId;
+import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.feature_engagement.EventConstants;
 import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -119,7 +119,8 @@ public class BookmarkSaveFlowMediator
                                 : R.string.bookmark_save_flow_title));
         mPropertyModel.set(BookmarkSaveFlowProperties.FOLDER_SELECT_ICON,
                 BookmarkUtils.getFolderIcon(mContext, bookmarkId.getType()));
-        mPropertyModel.set(BookmarkSaveFlowProperties.FOLDER_SELECT_ICON_ENABLED, item.isMovable());
+        mPropertyModel.set(BookmarkSaveFlowProperties.FOLDER_SELECT_ICON_ENABLED,
+                BookmarkUtils.isMovable(item));
         mPropertyModel.set(BookmarkSaveFlowProperties.SUBTITLE_TEXT,
                 mContext.getResources().getString(wasBookmarkMoved
                                 ? R.string.bookmark_page_moved_location
--- chrome/android/java/src/org/chromium/chrome/browser/read_later/ReadingListUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/read_later/ReadingListUtils.java
@@ -12,7 +12,6 @@ import androidx.annotation.Nullable;
 
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge;
-import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.bookmarks.BookmarkUndoController;
 import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
@@ -20,6 +19,7 @@ import org.chromium.chrome.browser.bookmarks.ReadingListFeatures;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.components.bookmarks.BookmarkId;
+import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.bookmarks.BookmarkType;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.embedder_support.util.UrlUtilities;
@@ -52,7 +52,7 @@ public final class ReadingListUtils {
         BookmarkUndoController.createOneshotBookmarkUndoController(
                 activity, bookmarkModel, snackbarManager);
         bookmarkModel.finishLoadingBookmarkModel(() -> {
-            BookmarkBridge.BookmarkItem bookmarkItem =
+            BookmarkItem bookmarkItem =
                     bookmarkModel.getReadingListItem(currentTab.getOriginalUrl());
             bookmarkModel.deleteBookmarks(bookmarkItem.getId());
         });
