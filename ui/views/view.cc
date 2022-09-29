6bd1186f4eb9775adadff399644a906ebd2dd653
Xianzhu Wang <wangxianzhu@chromium.org>
2022-09-29 03:39:11 +0000

    Let gfx::Transfrorm::TransformPoint(PointF*) support perspectives
    Previously only the Point3F version supported perspective. When we
    mapped PointF with a full transform that potentially had perspective,
    we had to convert the PointF to Point3F and use the Point3F version
    of TransformPoint(), which was fragile.
    
    Now let the PointF version also support perspectives. This keeps
    consistence with blink::TransformationMatrix::MapPoint(), and
    prepares for combination of the classes.
    
    Also changed zero comparison of the w component to std::isnormal()
    (which is used in blink::TransformationMatrix to check if a value
    has a valid reciprocal in various functions) to handle cases like
    NaN and near-zero subnormal numbers.
    
    Bug: 1359528
    Change-Id: Ic438a67da33f486d012b60ef5684836b83a13d20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923860
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052802}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -2956,9 +2956,9 @@ bool View::ConvertPointForAncestor(const View* ancestor,
   gfx::Transform trans;
   // TODO(sad): Have some way of caching the transformation results.
   bool result = GetTransformRelativeTo(ancestor, &trans);
-  auto p = gfx::Point3F(gfx::PointF(*point));
+  auto p = gfx::PointF(*point);
   trans.TransformPoint(&p);
-  *point = gfx::ToFlooredPoint(p.AsPointF());
+  *point = gfx::ToFlooredPoint(p);
   return result;
 }
 
