2c57c17183614f3709ed60633fc251eae4edab41
Anastasia Helfinstein <anastasi@google.com>
2022-09-29 01:46:28 +0000

    [ChromeVox] Assign ChromeVoxState.instance before calling init()
    This prevents potential failures in upcoming refactors due to trying to access ChromeVoxState.instance during initialization.
    
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: I6df4e022713152b6e78ea598327e3789c3cacbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911984
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052767}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
@@ -81,12 +81,10 @@ export class Background extends ChromeVoxState {
 
     /** @private {boolean} */
     this.talkBackEnabled_ = false;
-
-    this.init_();
   }
 
-  /** @private */
-  init_() {
+  /** @override */
+  init() {
     // Initialize legacy background page first.
     ChromeVoxBackground.init(this);
 
@@ -511,3 +509,4 @@ export class Background extends ChromeVoxState {
 
 InstanceChecker.closeExtraInstances();
 ChromeVoxState.instance = new Background();
+ChromeVoxState.instance.init();
