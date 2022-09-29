a3bb72de36dd60db8998100b24f1baa795b5baca
Morten Stenshorne <mstensho@chromium.org>
2022-09-29 21:41:22 +0000

    Store actual fragmentainer offset in constraint space.
    The constraint space used to store the block-offset of the nearest block
    formatting context root (within the fragmentainer), rather than the
    actual block-offset of the element being laid out. Since this offset was
    part of the cache key, it could cause false cache hits, as the test
    included demonstrates. The actual block-offset of a fragment may change,
    even if the block formatting context stays where it was.
    
    Storing the block-offset within the fragmentainer on the constraint
    space means that we need to always [*] go back and relayout if the BFC
    block-offset resolution results in anything different from the estimated
    offset.
    
    [*] Only when block fragmentation is involved, though.
    
    This issue was discovered while working on caching fragmented nodes (an
    upcoming CL).
    
    Change-Id: Ica69133189a042b6b066453cc6920e5b1e07c35c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923597
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053256}

--- third_party/blink/renderer/core/layout/layout_box_hot.cc
+++ third_party/blink/renderer/core/layout/layout_box_hot.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_fragment_geometry.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_disable_side_effects_scope.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
@@ -336,8 +337,8 @@ const NGLayoutResult* LayoutBox::CachedLayoutResult(
         // additional checks.)
       } else if (new_space.FragmentainerBlockSize() !=
                      old_space.FragmentainerBlockSize() ||
-                 new_space.FragmentainerOffsetAtBfc() !=
-                     old_space.FragmentainerOffsetAtBfc()) {
+                 new_space.FragmentainerOffset() !=
+                     old_space.FragmentainerOffset()) {
         // If the fragment was forced to stay in a fragmentainer (even if it
         // overflowed), BlockSizeForFragmentation() cannot be used for cache
         // testing.
@@ -371,7 +372,7 @@ const NGLayoutResult* LayoutBox::CachedLayoutResult(
               cached_layout_result->ExclusionSpace();
           if (result_exclusion_space != old_space.ExclusionSpace()) {
             LayoutUnit block_end_offset =
-                new_space.FragmentainerOffsetAtBfc() +
+                FragmentainerOffsetAtBfc(new_space) +
                 result_exclusion_space.ClearanceOffset(EClear::kBoth);
             if (block_end_offset > new_space.FragmentainerBlockSize())
               return true;
@@ -413,7 +414,7 @@ const NGLayoutResult* LayoutBox::CachedLayoutResult(
               cached_layout_result->BlockSizeForFragmentation();
 
           LayoutUnit block_end_offset =
-              new_space.FragmentainerOffsetAtBfc() +
+              FragmentainerOffsetAtBfc(new_space) +
               bfc_block_offset.value_or(LayoutUnit()) +
               block_size_for_fragmentation;
           if (block_end_offset > new_space.FragmentainerBlockSize())
--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -1097,7 +1097,7 @@ const NGLayoutResult* NGFlexLayoutAlgorithm::LayoutInternal() {
   if (UNLIKELY(InvolvedInBlockFragmentation(container_builder_))) {
     NGBreakStatus break_status = FinishFragmentation(
         Node(), ConstraintSpace(), BorderPadding().block_end,
-        FragmentainerSpaceAtBfcStart(ConstraintSpace()), &container_builder_);
+        FragmentainerSpaceLeft(ConstraintSpace()), &container_builder_);
     if (break_status != NGBreakStatus::kContinue) {
       if (break_status == NGBreakStatus::kNeedsEarlierBreak)
         return container_builder_.Abort(NGLayoutResult::kNeedsEarlierBreak);
@@ -1476,8 +1476,7 @@ NGFlexLayoutAlgorithm::GiveItemsFinalPositionAndSizeForFragmentation(
                                                   false);
   bool needs_earlier_break_in_column = false;
   NGLayoutResult::EStatus status = NGLayoutResult::kSuccess;
-  LayoutUnit fragmentainer_space =
-      FragmentainerSpaceAtBfcStart(ConstraintSpace());
+  LayoutUnit fragmentainer_space = FragmentainerSpaceLeft(ConstraintSpace());
 
   HeapVector<NGFlexColumnBreakInfo> column_break_info;
   if (is_column_) {
@@ -1709,7 +1708,7 @@ NGFlexLayoutAlgorithm::GiveItemsFinalPositionAndSizeForFragmentation(
       }
       break_status = BreakBeforeChildIfNeeded(
           ConstraintSpace(), flex_item->ng_input_node, *layout_result,
-          ConstraintSpace().FragmentainerOffsetAtBfc() + offset.block_offset,
+          ConstraintSpace().FragmentainerOffset() + offset.block_offset,
           has_container_separation, &container_builder_, !is_column_,
           current_column_break_info);
     }
@@ -2443,7 +2442,7 @@ MinMaxSizesResult NGFlexLayoutAlgorithm::ComputeMinMaxSizes(
 
 LayoutUnit NGFlexLayoutAlgorithm::FragmentainerSpaceAvailable(
     LayoutUnit block_offset) const {
-  return (FragmentainerSpaceAtBfcStart(ConstraintSpace()) - block_offset)
+  return (FragmentainerSpaceLeft(ConstraintSpace()) - block_offset)
       .ClampNegativeToZero();
 }
 
@@ -2483,7 +2482,7 @@ NGBreakStatus NGFlexLayoutAlgorithm::BreakBeforeRowIfNeeded(
   DCHECK(InvolvedInBlockFragmentation(container_builder_));
 
   LayoutUnit fragmentainer_block_offset =
-      ConstraintSpace().FragmentainerOffsetAtBfc() + row.cross_axis_offset;
+      ConstraintSpace().FragmentainerOffset() + row.cross_axis_offset;
   if (BreakToken())
     fragmentainer_block_offset -= BreakToken()->ConsumedBlockSize();
 
