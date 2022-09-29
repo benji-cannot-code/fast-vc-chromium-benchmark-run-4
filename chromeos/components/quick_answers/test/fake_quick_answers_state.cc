b735eb7fa2457d811bd1265b6a8d4f258860553b
Yue Li <updowndota@google.com>
2022-09-29 00:04:10 +0000

    Quick Answers: Add unit tests for SpellChecker
    Bug: b/237627977
    Test: Run SpellCheckerTest.* under chromeos_components_unittests
    Change-Id: I6f9ff236c7972f909b58e05b58416e1dd4cc7d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3920576
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052716}

--- chromeos/components/quick_answers/test/fake_quick_answers_state.cc
+++ chromeos/components/quick_answers/test/fake_quick_answers_state.cc
@@ -8,3 +8,61 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 FakeQuickAnswersState::FakeQuickAnswersState() = default;
 
 FakeQuickAnswersState::~FakeQuickAnswersState() = default;
+
+void FakeQuickAnswersState::SetSettingsEnabled(bool settings_enabled) {
+  if (settings_enabled_ == settings_enabled) {
+    return;
+  }
+  settings_enabled_ = settings_enabled;
+
+  for (auto& observer : observers_) {
+    observer.OnSettingsEnabled(settings_enabled_);
+  }
+}
+
+void FakeQuickAnswersState::SetConsentStatus(
+    quick_answers::prefs::ConsentStatus consent_status) {
+  if (consent_status_ == consent_status) {
+    return;
+  }
+  consent_status_ = consent_status;
+
+  for (auto& observer : observers_) {
+    observer.OnConsentStatusUpdated(consent_status_);
+  }
+}
+
+void FakeQuickAnswersState::SetApplicationLocale(const std::string& locale) {
+  if (resolved_application_locale_ == locale) {
+    return;
+  }
+  resolved_application_locale_ = locale;
+
+  for (auto& observer : observers_) {
+    observer.OnApplicationLocaleReady(locale);
+  }
+
+  UpdateEligibility();
+}
+
+void FakeQuickAnswersState::SetPreferredLanguages(
+    const std::string& preferred_languages) {
+  if (preferred_languages_ == preferred_languages) {
+    return;
+  }
+  preferred_languages_ = preferred_languages;
+
+  for (auto& observer : observers_) {
+    observer.OnPreferredLanguagesChanged(preferred_languages);
+  }
+}
+
+void FakeQuickAnswersState::OnPrefsInitialized() {
+  prefs_initialized_ = true;
+
+  for (auto& observer : observers_) {
+    observer.OnPrefsInitialized();
+  }
+
+  UpdateEligibility();
+}
