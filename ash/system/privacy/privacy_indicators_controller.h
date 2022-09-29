79c1bf7d060901caca537e8291481429edeb8d0f
Andre Le <leandre@chromium.org>
2022-09-29 01:18:45 +0000

    PrivacyIndicators: Update guestOS indicators according to the new design
    We want to re-use privacy indicators for guestOS. This includes changing
    the style of the notification and showing a green panel in the status
    area when guestOS camera/mic is being accessed.
    
    Design doc: go/cros-privacy-indicators-design
    
    Fixed: 1368256
    Change-Id: I7db050ee11fa76efa9b0716ff8ce67c9d8a0f21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918752
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052752}

--- ash/system/privacy/privacy_indicators_controller.h
+++ ash/system/privacy/privacy_indicators_controller.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
 
+namespace message_center {
+class Notification;
+}  // namespace message_center
+
 namespace ash {
 
 using AppActionClosure = base::RepeatingCallback<void(void)>;
@@ -41,17 +45,30 @@ class ASH_EXPORT PrivacyIndicatorsNotificationDelegate
   const AppActionClosure launch_settings_;
 };
 
-// Add, update, or remove the privacy notification associated with the given
-// `app_id`.
+// Create a notification with the customized metadata for privacy indicators.
 // The given scoped_refptr for `delegate` will be passed as a parameter for
 // CreateSystemNotification() in case of adding/updating the notification, can
 // be provided as a nullptr if irrelevant.
+std::unique_ptr<message_center::Notification> ASH_EXPORT
+CreatePrivacyIndicatorsNotification(
+    const std::string& app_id,
+    absl::optional<std::u16string> app_name,
+    bool is_camera_used,
+    bool is_microphone_used,
+    scoped_refptr<message_center::NotificationDelegate> delegate);
+
+// Add, update, or remove the privacy notification associated with the given
+// `app_id`.
 void ASH_EXPORT ModifyPrivacyIndicatorsNotification(
     const std::string& app_id,
     absl::optional<std::u16string> app_name,
-    bool camera_is_used,
-    bool microphone_is_used,
+    bool is_camera_used,
+    bool is_microphone_used,
     scoped_refptr<PrivacyIndicatorsNotificationDelegate> delegate);
+
+// Update the `PrivacyIndicatorsTrayItemView` across all status area widgets.
+void ASH_EXPORT UpdatePrivacyIndicatorsView(bool is_camera_used,
+                                            bool is_microphone_used);
 
 }  // namespace ash
 
