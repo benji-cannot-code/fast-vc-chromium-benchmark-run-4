d6dbdc264ba9b5f98bd9440871013bc4168cb7e1
Wez <wez@chromium.org>
2022-09-29 12:22:01 +0000

    [mojo] Deprecate process_handle from AddBrokerClientData
    The process_handle field is not actually used for anything, so can be
    safely replaced with a valid "null" process handle. For ABI
    compatibility senders now send the null handle, and receivers ignore
    any supplied handle.
    
    Bug: 1368140
    Change-Id: I75208926f67a66e92a3f1235ede3c5e6718079ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922198
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052866}

--- mojo/core/node_channel.cc
+++ mojo/core/node_channel.cc
@@ -90,7 +90,7 @@ using AcceptPeerData = AcceptPeerDataV0;
 struct alignas(8) AddBrokerClientDataV0 {
   ports::NodeName client_name;
 #if !BUILDFLAG(IS_WIN)
-  uint32_t process_handle = 0;
+  uint32_t process_handle;
 #endif
 };
 
@@ -387,7 +387,9 @@ void NodeChannel::AddBrokerClient(const ports::NodeName& client_name,
   message->SetHandles(std::move(handles));
   data->client_name = client_name;
 #if !BUILDFLAG(IS_WIN)
-  data->process_handle = process_handle.Handle();
+  // Older clients treat this as a real process handle, but don't actually need
+  // it, so send a valid null handle.
+  data->process_handle = base::kNullProcessHandle;
 #endif
   WriteChannelMessage(std::move(message));
 }
@@ -625,7 +627,7 @@ void NodeChannel::OnChannelMessage(const void* payload,
           break;
         }
         delegate_->OnAddBrokerClient(remote_node_name_, data.client_name,
-                                     data.process_handle);
+                                     base::kNullProcessHandle);
 #endif
         return;
       }
