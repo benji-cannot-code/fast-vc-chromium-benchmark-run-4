abe97d58d02321644d3563def5faf0415d3ccc0f
Daniel d'Andrada <dandrader@google.com>
2022-09-29 14:09:20 +0000

    [dPWA Testing] Add CheckAppNavigation action
    Bug: 1216457
    Change-Id: I945dabdb7df44457ef53aa41c592a9aaa170d7b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921797
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052961}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h"
 #include "content/public/test/browser_test.h"
+#include "testing/gtest/include/gtest/gtest-spi.h"
 
 namespace web_app::integration_tests {
 namespace {
@@ -140,6 +141,17 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration,
 IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckAppInListIconCorrect) {
   helper_.InstallMenuOption(InstallableSite::kStandalone);
   helper_.CheckAppInListIconCorrect(Site::kStandalone);
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckAppNavigation) {
+  helper_.InstallMenuOption(InstallableSite::kStandalone);
+  helper_.CheckAppNavigation(Site::kStandalone);
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckAppNavigationFails) {
+  helper_.InstallMenuOption(InstallableSite::kStandalone);
+  EXPECT_NONFATAL_FAILURE(helper_.CheckAppNavigation(Site::kStandaloneNestedB),
+                          "webapps_integration/standalone/basic.html");
 }
 
 // Generated tests:
--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -1951,6 +1951,16 @@ void WebAppIntegrationTestDriver::CheckAppInListWindowed(Site site) {
   AfterStateCheckAction();
 }
 
+void WebAppIntegrationTestDriver::CheckAppNavigation(Site site) {
+  if (!BeforeStateCheckAction(__FUNCTION__))
+    return;
+  ASSERT_TRUE(app_browser());
+  GURL url =
+      app_browser()->tab_strip_model()->GetActiveWebContents()->GetVisibleURL();
+  EXPECT_EQ(GetUrlForSite(site), url);
+  AfterStateCheckAction();
+}
+
 void WebAppIntegrationTestDriver::CheckAppNavigationIsStartUrl() {
   if (!BeforeStateCheckAction(__FUNCTION__))
     return;
