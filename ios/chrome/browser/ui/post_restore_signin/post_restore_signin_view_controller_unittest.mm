9e2e45322f9ae88df375bf495529a40f23b051ef
Scott Yoder <scottyoder@google.com>
2022-09-29 15:31:20 +0000

    [ios] Update strings used for Restore Shorty & add default avatar
    This change corrects a few places where the strings were updated, or
    incorrectly entered originally. It also adds a default user avatar in
    cases where we do not have the user's actual avatar.
    
    Bug: 1362029
    Change-Id: I3bba9b6dc56714806284e1ab89562c8bca5d058b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925875
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053007}

--- ios/chrome/browser/ui/post_restore_signin/post_restore_signin_view_controller_unittest.mm
+++ ios/chrome/browser/ui/post_restore_signin/post_restore_signin_view_controller_unittest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
+#import "ui/base/device_form_factor.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -27,11 +28,22 @@ class PostRestoreSignInViewControllerTest : public PlatformTest {
         initWithAccountInfo:FakeAccountInfo()];
   }
 
+  void ClearUserName() {
+    view_controller_ = [[PostRestoreSignInViewController alloc]
+        initWithAccountInfo:FakeAccountInfoWithoutName()];
+  }
+
   AccountInfo FakeAccountInfo() {
     AccountInfo accountInfo;
     accountInfo.email = std::string(kFakePreRestoreAccountEmail);
     accountInfo.given_name = std::string(kFakePreRestoreAccountGivenName);
     accountInfo.full_name = std::string(kFakePreRestoreAccountFullName);
+    return accountInfo;
+  }
+
+  AccountInfo FakeAccountInfoWithoutName() {
+    AccountInfo accountInfo;
+    accountInfo.email = std::string(kFakePreRestoreAccountEmail);
     return accountInfo;
   }
 
@@ -41,9 +53,28 @@ class PostRestoreSignInViewControllerTest : public PlatformTest {
 TEST_F(PostRestoreSignInViewControllerTest, uiStrings) {
   [view_controller_ loadView];
   EXPECT_TRUE(
-      [view_controller_.titleText isEqualToString:@"Welcome back, Given"]);
+      [view_controller_.titleText isEqualToString:@"Welcome Back, Given"]);
   EXPECT_TRUE([view_controller_.primaryActionString
       isEqualToString:@"Continue as Given"]);
   EXPECT_TRUE([view_controller_.secondaryActionString
       isEqualToString:@"Don't Sign In"]);
+
+  NSString* expectedDisclaimer;
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    expectedDisclaimer = @"You were signed out as part of your iPad reset. Tap "
+                         @"continue below to sign in.";
+  } else {
+    expectedDisclaimer = @"You were signed out as part of your iPhone reset. "
+                         @"Tap continue below to sign in.";
+  }
+  EXPECT_TRUE(
+      [view_controller_.disclaimerText isEqualToString:expectedDisclaimer]);
+}
+
+TEST_F(PostRestoreSignInViewControllerTest, uiStringsWithoutName) {
+  ClearUserName();
+  [view_controller_ loadView];
+  EXPECT_TRUE([view_controller_.titleText isEqualToString:@"Welcome Back"]);
+  EXPECT_TRUE(
+      [view_controller_.primaryActionString isEqualToString:@"Continue"]);
 }
