db484def1e739b9b3b3e8ee1b95bc532d6d2268e
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-09-29 20:04:53 +0000

    Add the PreDiscardResourceUsage to be used to display memory savings
    This CL only adds the SupportsUserData::Data derived class so that
    the UI that depends on it can be developed in parallel to the functionality.
    
    Bug: 1348590
    Change-Id: If49f38cea5db5727e9466cc1f7b365f05c563b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922104
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053192}

--- chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h
+++ chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "chrome/browser/performance_manager/user_tuning/user_performance_tuning_notifier.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "content/public/browser/web_contents_user_data.h"
 
 class ChromeBrowserMainExtraPartsPerformanceManager;
 class PerformanceManagerMetricsProviderTest;
@@ -84,6 +85,24 @@ class UserPerformanceTuningManager
     // Raised when the count of janky intervals reaches X.
     // Can be used by the UI to show a promo
     virtual void OnJankThresholdReached() {}
+  };
+
+  class PreDiscardResourceUsage
+      : public content::WebContentsUserData<PreDiscardResourceUsage> {
+   public:
+    PreDiscardResourceUsage(content::WebContents* contents,
+                            uint64_t resident_set_size_estimate);
+    ~PreDiscardResourceUsage() override;
+
+    uint64_t resident_set_size_estimate() const {
+      return resident_set_size_estimate_;
+    }
+
+   private:
+    friend WebContentsUserData;
+    WEB_CONTENTS_USER_DATA_KEY_DECL();
+
+    uint64_t resident_set_size_estimate_ = 0;
   };
 
   static UserPerformanceTuningManager* GetInstance();
