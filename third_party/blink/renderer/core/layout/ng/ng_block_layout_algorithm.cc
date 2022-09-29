707580fe8c92ef07b6e1b5b228ab174b0971c9bd
Morten Stenshorne <mstensho@chromium.org>
2022-09-29 21:27:20 +0000

    Avoid invalid class C break when a first inflow child fits beside float.
    If a container was cleared by a preceding float, and said container has
    a float and a BFC root child, and the BFC root fits beside the float,
    we'd incorrectly detect container separation before the BFC root, even
    though there was no gap between it and its container.
    
    We don't really need to let child_margin_got_separated be part of the
    container separation check, since we're already comparing block-offsets.
    That said, the whole child_margin_got_separated thing is a bit sketchy,
    in that there may be false positives. Added a TODO.
    
    Note that, among the tests included, only the -004 one would fail
    without this fix. The two others are just regression tests, to make sure
    that we still detect valid class C breakpoints in very similar
    situations.
    
    Bug: 1368518
    Change-Id: I78ff2f5081e4a6878d4556ddbd859286da12e7d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921636
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053246}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1333,6 +1333,10 @@ NGLayoutResult::EStatus NGBlockLayoutAlgorithm::HandleNewFormattingContext(
              child_bfc_offset_estimate == ConstraintSpace().ClearanceOffset());
       // Figure out if the child margin has already got separated from the
       // margin strut or not.
+      //
+      // TODO(mstensho): We get false positives here, if the container was
+      // cleared by floats (but the child wasn't). See
+      // wpt/css/css-break/class-c-breakpoint-after-float-004.html
       child_margin_got_separated =
           child_bfc_offset_estimate != adjoining_bfc_offset_estimate;
     }
@@ -1428,7 +1432,7 @@ NGLayoutResult::EStatus NGBlockLayoutAlgorithm::HandleNewFormattingContext(
 
   if (ConstraintSpace().HasBlockFragmentation()) {
     bool has_container_separation =
-        has_processed_first_child_ || child_margin_got_separated ||
+        has_processed_first_child_ ||
         child_bfc_offset.block_offset > child_bfc_offset_estimate ||
         layout_result->IsPushedByFloats();
     NGBreakStatus break_status = BreakBeforeChildIfNeeded(
