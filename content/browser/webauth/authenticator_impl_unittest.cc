aa369dfff7ae02f6f1730fcfb6c0f3b1da0cf42b
Adam Langley <agl@chromium.org>
2022-09-29 01:12:30 +0000

    Revert "webauthn: drop pre-M99 caBLEv2 compatibility."
    This reverts commit 39215f300e71e5c4f0294e7ffa768f277fc96bc0. This is
    breaking synced phones: evidently it's not just pre-M99 Clank that's
    depending on this code.
    
    Bug: 1369299
    
    Original change's description:
    > webauthn: drop pre-M99 caBLEv2 compatibility.
    >
    > We've been auto-detecting M98 (and prior) caBLE peers and switching to
    > older versions of the protocol in a couple of places:
    >
    > 1. AEAD nonces are now big-endian, but a decryption failure would
    >    trigger an attempt to use little-endian nonces.
    > 2. The padding length is now two bytes, but a parse error would
    >    trigger an attempt to parse with a single byte length.
    >
    > This change removes these accommodations. caBLE peers now need to be at least
    > M99.
    >
    > Change-Id: Ifd04981d89158e575705fbe8e8480a254aa0fb46
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875104
    > Auto-Submit: Adam Langley <agl@chromium.org>
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1045203}
    
    Change-Id: I074dd8bbb76faaa3c68ed4f32c006625ad62177e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3927747
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052747}

--- content/browser/webauth/authenticator_impl_unittest.cc
+++ content/browser/webauth/authenticator_impl_unittest.cc
@@ -9066,7 +9066,8 @@ TEST_P(AuthenticatorCableV2Test, QRBasedWithNoPairing) {
               /*observer=*/nullptr),
           network_context_.get(), root_secret_, "Test Authenticator",
           zero_qr_secret_, peer_identity_x962_,
-          /*contact_id=*/absl::nullopt);
+          /*contact_id=*/absl::nullopt,
+          /*use_new_crypter_construction=*/false);
 
   EXPECT_EQ(AuthenticatorMakeCredential().status, AuthenticatorStatus::SUCCESS);
   EXPECT_EQ(pairings_.size(), 0u);
@@ -9095,7 +9096,8 @@ TEST_P(AuthenticatorCableV2Test, QRBasedNewCrypterConstruction) {
               /*observer=*/nullptr),
           network_context_.get(), root_secret_, "Test Authenticator",
           zero_qr_secret_, peer_identity_x962_,
-          /*contact_id=*/absl::nullopt);
+          /*contact_id=*/absl::nullopt,
+          /*use_new_crypter_construction=*/true);
 
   EXPECT_EQ(AuthenticatorMakeCredential().status, AuthenticatorStatus::SUCCESS);
   EXPECT_EQ(pairings_.size(), 0u);
@@ -9123,7 +9125,8 @@ TEST_P(AuthenticatorCableV2Test, PairingBased) {
               std::move(ble_advert_callback_), &virtual_device_,
               /*observer=*/nullptr),
           network_context_.get(), root_secret_, "Test Authenticator",
-          zero_qr_secret_, peer_identity_x962_, contact_id);
+          zero_qr_secret_, peer_identity_x962_, contact_id,
+          /*use_new_crypter_construction=*/false);
 
   EXPECT_EQ(AuthenticatorMakeCredential().status, AuthenticatorStatus::SUCCESS);
   EXPECT_EQ(pairings_.size(), 1u);
@@ -9309,7 +9312,8 @@ TEST_P(AuthenticatorCableV2Test, ServerLink) {
               /*observer=*/nullptr),
           network_context_.get(), root_secret_, "Test Authenticator",
           server_link.secret, server_link.peer_identity,
-          /*contact_id=*/absl::nullopt);
+          /*contact_id=*/absl::nullopt,
+          /*use_new_crypter_construction=*/false);
 
   EXPECT_EQ(AuthenticatorMakeCredential().status, AuthenticatorStatus::SUCCESS);
   EXPECT_EQ(pairings_.size(), 0u);
@@ -9386,7 +9390,8 @@ class AuthenticatorCableV2AuthenticatorTest
             std::move(ble_advert_callback_), &virtual_device_, this),
         network_context_.get(), root_secret_, "Test Authenticator",
         zero_qr_secret_, peer_identity_x962_,
-        /*contact_id=*/absl::nullopt);
+        /*contact_id=*/absl::nullopt,
+        /*use_new_crypter_construction=*/false);
   }
 
  protected:
