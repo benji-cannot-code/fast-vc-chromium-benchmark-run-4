17577a1e1f9ee934cc5b019c6ac158010a77a6b4
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2022-09-29 21:15:24 +0000

    [GridNG] Basic named line support for subgrid
    This change gets basic named line support working for subgrids. This is
    accomplished by moving the line resolver (now unique per sugrid) to
    NGGridPlacementData. This allows for each subgrid to run their own
    independent auto placement, sharing the parent grid's line names
    with their own distinct line names.
    
    All of the subgrid web_tests related to line names will need updates to
    GetComputedStyle in order to pass. I've prototyped this as a follow-up
    to this change and the two combined will pass a few tests.
    
    This change also adds some minor adjustments to use `IsSubgriddedAxis`
    for code cleanliness.
    
    Bug: 618969
    Change-Id: I296ced1c4e74fe1d8ea95dc58179397e9be48a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866556
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053240}

--- third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.cc
+++ third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.h"
 
-#include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_line_resolver.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 
 namespace blink {
