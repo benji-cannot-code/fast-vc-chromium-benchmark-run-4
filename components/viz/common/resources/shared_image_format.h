d0051bfa33520df9929c724acabfc3b237545611
Saifuddin Hitawala <hitawala@chromium.org>
2022-09-29 00:24:37 +0000

    Add PlaneType::Unknown to SharedImageFormat
    SharedImageFormat can be either single or multi plane created using
    the SinglePlane() and MultiPlane() calls. We would like the format
    to be created only using these calls and not using the default
    constructor which defaults to single plane right now to make it
    stricter. This CL changes is_single_plane_ bool to PlaneType enum
    adding an Unknown type set through the default constructor making it
    strict.
    
    Bug: 1366473
    Change-Id: I02833fe78cbfbe004f544b1f6a038755297e8823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913428
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052728}

--- components/viz/common/resources/shared_image_format.h
+++ components/viz/common/resources/shared_image_format.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/check.h"
+#include "base/check_op.h"
 #include "components/viz/common/resources/resource_format.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "mojo/public/cpp/bindings/union_traits.h"
@@ -68,8 +69,7 @@ class SharedImageFormat {
    */
   enum class ChannelFormat : uint8_t { k8, k10, k16, k16F };
 
-  SharedImageFormat() : format_(RGBA_8888) {}
-
+  SharedImageFormat() = default;
   static constexpr SharedImageFormat SinglePlane(
       ResourceFormat resource_format) {
     return SharedImageFormat(resource_format);
@@ -80,36 +80,54 @@ class SharedImageFormat {
     return SharedImageFormat(plane_config, subsampling, channel_format);
   }
 
-  bool is_single_plane() const { return is_single_plane_; }
   ResourceFormat resource_format() const {
     DCHECK(is_single_plane());
     return format_.resource_format;
   }
   PlaneConfig plane_config() const {
-    DCHECK(!is_single_plane());
+    DCHECK(is_multi_plane());
     return format_.multiplanar_format.plane_config;
   }
   Subsampling subsampling() const {
-    DCHECK(!is_single_plane());
+    DCHECK(is_multi_plane());
     return format_.multiplanar_format.subsampling;
   }
   ChannelFormat channel_format() const {
-    DCHECK(!is_single_plane());
+    DCHECK(is_multi_plane());
     return format_.multiplanar_format.channel_format;
   }
+
+  bool is_single_plane() const {
+    return plane_type_ == PlaneType::kSinglePlane;
+  }
+  bool is_multi_plane() const { return plane_type_ == PlaneType::kMultiPlane; }
 
   // Returns whether the resource format can be used as a software bitmap for
   // export to the display compositor.
   bool IsBitmapFormatSupported() const;
 
   bool operator==(const SharedImageFormat& o) const {
-    return is_single_plane_ == o.is_single_plane() &&
-           (is_single_plane_ ? resource_format() == o.resource_format()
-                             : multiplanar_format() == o.multiplanar_format());
+    if (plane_type_ != o.plane_type())
+      return false;
+
+    switch (plane_type_) {
+      case PlaneType::kUnknown:
+        return true;
+      case PlaneType::kSinglePlane:
+        return resource_format() == o.resource_format();
+      case PlaneType::kMultiPlane:
+        return multiplanar_format() == o.multiplanar_format();
+    }
   }
   bool operator!=(const SharedImageFormat& o) const { return !operator==(o); }
 
  private:
+  enum class PlaneType : uint8_t {
+    kUnknown,
+    kSinglePlane,
+    kMultiPlane,
+  };
+
   union SharedImageFormatUnion {
     // A struct for multiplanar format that is defined by the PlaneConfig,
     // Subsampling and ChannelFormat it holds.
@@ -124,6 +142,7 @@ class SharedImageFormat {
       }
     };
 
+    SharedImageFormatUnion() = default;
     explicit constexpr SharedImageFormatUnion(ResourceFormat resource_format)
         : resource_format(resource_format) {}
     constexpr SharedImageFormatUnion(PlaneConfig plane_config,
@@ -141,19 +160,20 @@ class SharedImageFormat {
                                    SharedImageFormatUnion::MultiplanarFormat>;
 
   explicit constexpr SharedImageFormat(ResourceFormat resource_format)
-      : format_(resource_format) {}
+      : plane_type_(PlaneType::kSinglePlane), format_(resource_format) {}
   constexpr SharedImageFormat(PlaneConfig plane_config,
                               Subsampling subsampling,
                               ChannelFormat channel_format)
-      : is_single_plane_(false),
+      : plane_type_(PlaneType::kMultiPlane),
         format_(plane_config, subsampling, channel_format) {}
 
+  PlaneType plane_type() const { return plane_type_; }
   SharedImageFormatUnion::MultiplanarFormat multiplanar_format() const {
-    DCHECK(!is_single_plane());
+    DCHECK(is_multi_plane());
     return format_.multiplanar_format;
   }
 
-  bool is_single_plane_ = true;
+  PlaneType plane_type_ = PlaneType::kUnknown;
   // `format_` can only be ResourceFormat (for single plane, eg. RGBA) or
   // MultiplanarFormat at any given time.
   SharedImageFormatUnion format_;
--- services/viz/public/cpp/compositing/mojom_traits_unittest.cc
+++ services/viz/public/cpp/compositing/mojom_traits_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
+#include "base/test/gtest_util.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
@@ -1221,6 +1222,14 @@ TEST_F(StructTraitsTest, SharedImageFormatWithMultiPlane) {
   mojo::test::SerializeAndDeserialize<mojom::SharedImageFormat>(input, output);
 
   EXPECT_EQ(input, output);
+}
+
+TEST_F(StructTraitsTest, SharedImageFormatWithUnknownPlane) {
+  SharedImageFormat input = SharedImageFormat();
+  SharedImageFormat output;
+  EXPECT_CHECK_DEATH(
+      mojo::test::SerializeAndDeserialize<mojom::SharedImageFormat>(input,
+                                                                    output));
 }
 
 TEST_F(StructTraitsTest, YUVDrawQuad) {
