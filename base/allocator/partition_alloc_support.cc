f0f355d3639d1ec3245f649ab948d52814119803
Byoungchan Lee <daniel.l@hpcnt.com>
2022-09-29 12:30:00 +0000

    Add missing <cinttypes> include
    PRIxPTR requires <cinttypes> or <inttypes.h> but it was missing.
    There was no problem with the chromium build, since
    <cinttypes> is included in
    third_party/perfetto/include/perfetto/protozero/proto_utils.h.
    
    Bug: None
    Change-Id: I67b7e94a7eaf6dca710e048f3c1d3c5ced883a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924431
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052881}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_alloc_support.h"
 
 #include <array>
+#include <cinttypes>
 #include <cstdint>
 #include <map>
 #include <string>
