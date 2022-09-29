1e6b4bebd9c31cec54bca38f9037d56c5ee28210
David Bertoni <dbertoni@chromium.org>
2022-09-29 20:51:32 +0000

    [Extensions] Convert EXPECT_TRUE to EXPECT_EQ for better error output.
    Some message bubble unit tests should use EXPECT_EQ so the expected
    value is reported when a test fails.
    
    Bug: 883570
    Change-Id: Ib6827b4c493af4c460d049120fceb2be195315fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925226
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053220}

--- chrome/browser/extensions/extension_message_bubble_controller_unittest.cc
+++ chrome/browser/extensions/extension_message_bubble_controller_unittest.cc
@@ -530,7 +530,7 @@ TEST_F(ExtensionMessageBubbleTest, WipeoutControllerTest) {
   EXPECT_TRUE(controller->ShouldShow());
   suspicious_extensions = controller->GetExtensionList();
   ASSERT_EQ(1U, suspicious_extensions.size());
-  EXPECT_TRUE(u"Extension 1" == suspicious_extensions[0]);
+  EXPECT_EQ(u"Extension 1", suspicious_extensions[0]);
   bubble.set_controller(controller.get());
   bubble.Show();  // Simulate showing the bubble.
   EXPECT_EQ(0U, controller->link_click_count());
@@ -554,8 +554,8 @@ TEST_F(ExtensionMessageBubbleTest, WipeoutControllerTest) {
   EXPECT_TRUE(controller->ShouldShow());
   suspicious_extensions = controller->GetExtensionList();
   ASSERT_EQ(2U, suspicious_extensions.size());
-  EXPECT_TRUE(u"Extension 1" == suspicious_extensions[1]);
-  EXPECT_TRUE(u"Extension 2" == suspicious_extensions[0]);
+  EXPECT_EQ(u"Extension 1", suspicious_extensions[1]);
+  EXPECT_EQ(u"Extension 2", suspicious_extensions[0]);
   bubble.set_controller(controller.get());
   bubble.Show();  // Simulate showing the bubble.
   EXPECT_EQ(1U, controller->link_click_count());
@@ -588,8 +588,8 @@ TEST_F(ExtensionMessageBubbleTest, DevModeControllerTest) {
   std::vector<std::u16string> dev_mode_extensions =
       controller->GetExtensionList();
   ASSERT_EQ(2U, dev_mode_extensions.size());
-  EXPECT_TRUE(u"Extension 2" == dev_mode_extensions[0]);
-  EXPECT_TRUE(u"Extension 1" == dev_mode_extensions[1]);
+  EXPECT_EQ(u"Extension 2", dev_mode_extensions[0]);
+  EXPECT_EQ(u"Extension 1", dev_mode_extensions[1]);
   EXPECT_EQ(0U, controller->link_click_count());
   EXPECT_EQ(0U, controller->dismiss_click_count());
   EXPECT_EQ(0U, controller->action_click_count());
@@ -776,7 +776,7 @@ TEST_F(ExtensionMessageBubbleTest, SettingsApiControllerTest) {
     std::vector<std::u16string> override_extensions =
         controller->GetExtensionList();
     ASSERT_EQ(1U, override_extensions.size());
-    EXPECT_TRUE(u"Extension 2" == override_extensions[0]);
+    EXPECT_EQ(u"Extension 2", override_extensions[0]);
     EXPECT_EQ(0U, controller->link_click_count());
     EXPECT_EQ(0U, controller->dismiss_click_count());
     EXPECT_EQ(0U, controller->action_click_count());
@@ -936,7 +936,7 @@ TEST_F(ExtensionMessageBubbleTest,
   EXPECT_TRUE(controller->ShouldShow());
   suspicious_extensions = controller->GetExtensionList();
   ASSERT_EQ(1U, suspicious_extensions.size());
-  EXPECT_TRUE(u"Extension 1" == suspicious_extensions[0]);
+  EXPECT_EQ(u"Extension 1", suspicious_extensions[0]);
   bubble.set_controller(controller.get());
   bubble.set_action_on_show(FakeExtensionMessageBubble::BUBBLE_ACTION_IGNORE);
   bubble.Show();  // Simulate showing the bubble.
