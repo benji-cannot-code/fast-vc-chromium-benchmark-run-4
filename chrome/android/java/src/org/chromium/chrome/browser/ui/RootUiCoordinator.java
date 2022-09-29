04fc42e7b3f28ca146724cc0f6b08fa615405e40
Jinsuk Kim <jinsukkim@chromium.org>
2022-09-29 22:19:40 +0000

    [PCCT] Update toolbar color on find-in-page
    Update the drag bar portion of the toolbar so its color matches that
    of find-in-page UI.
    
    Bug: 1360418
    Change-Id: I1677f950d43d7b83d87983b5d9f16fd612d0fabe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925276
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053275}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1360,7 +1360,9 @@ public class RootUiCoordinator
             }
 
             @Override
-            public void onFindToolbarHidden() {}
+            public void onFindToolbarHidden() {
+                RootUiCoordinator.this.onFindToolbarHidden();
+            }
         };
 
         mFindToolbarManager.addObserver(mFindToolbarObserver);
@@ -1376,6 +1378,12 @@ public class RootUiCoordinator
                     OverlayPanel.StateChangeReason.UNKNOWN);
         }
     }
+
+    /**
+     * Called when the find in page toolbar is shown. Sub-classes may override to manage
+     * cross-feature interaction, e.g. hide other features when this feature is shown.
+     */
+    protected void onFindToolbarHidden() {}
 
     /**
      * @return Whether the "update available" badge should be displayed on menu button(s) in the
--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/PartialCustomTabHeightStrategyTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/PartialCustomTabHeightStrategyTest.java
@@ -29,6 +29,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Insets;
 import android.graphics.Rect;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -109,6 +110,9 @@ public class PartialCustomTabHeightStrategyTest {
     private static final int MULTIWINDOW_HEIGHT = FULL_HEIGHT / 2;
     private static final int STATUS_BAR_HEIGHT = 68;
 
+    private static final int FIND_TOOLBAR_COLOR = 3755;
+    private static final int PCCT_TOOLBAR_COLOR = 12111;
+
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {{true}, {false}});
@@ -168,6 +172,8 @@ public class PartialCustomTabHeightStrategyTest {
     private CommandLine mCommandLine;
     @Mock
     private FullscreenManager mFullscreenManager;
+    @Mock
+    private GradientDrawable mDragBarBackground;
 
     private Context mContext;
     private List<WindowManager.LayoutParams> mAttributeResults;
@@ -253,9 +259,9 @@ public class PartialCustomTabHeightStrategyTest {
     }
 
     private PartialCustomTabHeightStrategy createPcctAtHeight(int heightPx, boolean isFixedHeight) {
-        PartialCustomTabHeightStrategy pcct =
-                new PartialCustomTabHeightStrategy(mActivity, heightPx, null, null, isFixedHeight,
-                        mOnResizedCallback, mActivityLifecycleDispatcher, mFullscreenManager);
+        PartialCustomTabHeightStrategy pcct = new PartialCustomTabHeightStrategy(mActivity,
+                heightPx, null, null, isFixedHeight, mOnResizedCallback,
+                mActivityLifecycleDispatcher, mFullscreenManager, false);
         pcct.setWindowAboveNavbarForTesting(mWindowAboveNavbar);
         pcct.setMockViewForTesting(
                 mNavbar, mSpinnerView, mSpinner, mToolbarView, mToolbarCoordinator);
@@ -982,6 +988,22 @@ public class PartialCustomTabHeightStrategyTest {
         // Drag tab slightly but actionDown and actionUp will be performed at the same Y.
         // The tab should remain open.
         assertTabIsAtInitialPos(dragTab(handleStrategy, 1500, 1450, 1500));
+    }
+
+    @Test
+    public void dragBarMatchesFindToolbarInColor() {
+        PartialCustomTabHeightStrategy strategy = createPcctAtHeight(500);
+        strategy.setToolbarColorForTesting(PCCT_TOOLBAR_COLOR);
+        doReturn(FIND_TOOLBAR_COLOR)
+                .when(mResources)
+                .getColor(eq(R.color.find_in_page_background_color));
+        doReturn(mDragBarBackground).when(mDragBar).getBackground();
+
+        strategy.onFindToolbarShown();
+        verify(mDragBarBackground).setColor(FIND_TOOLBAR_COLOR);
+
+        strategy.onFindToolbarHidden();
+        verify(mDragBarBackground).setColor(PCCT_TOOLBAR_COLOR);
     }
 
     private boolean isFullscreen() {
