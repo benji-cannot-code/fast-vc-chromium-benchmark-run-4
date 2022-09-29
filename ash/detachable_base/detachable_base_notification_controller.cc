f1275d5c278eb4deba5a18f3ace7b4aa5230558c
Maria Timbur <mtimbur@google.com>
2022-09-29 20:23:26 +0000

    Move chromeos/ui/vector_icons/notification_warning.icon to components/vector_icons
    The notification_warning.icon will be used in the password strength indicator feature. This would imply adding a ChromeOS dependency in chrome/browser/ui/BUILD.gn, thus, the icon should be moved to a common place.
    
    This is a purely mechanical change and doesn't introduce behavioral changes.
    
    Change-Id: Id563a9831941d38c50f53bb22162c5e366c435c4
    Bug: 1345766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925528
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Maria Timbur <mtimbur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053202}

--- ash/detachable_base/detachable_base_notification_controller.cc
+++ ash/detachable_base/detachable_base_notification_controller.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
-#include "chromeos/ui/vector_icons/vector_icons.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
@@ -74,7 +74,7 @@ void DetachableBaseNotificationController::
               kDetachableBaseNotifierId,
               NotificationCatalogName::kDetachableBaseRequiresUpdate),
           message_center::RichNotificationData(), nullptr,
-          chromeos::kNotificationWarningIcon,
+          vector_icons::kNotificationWarningIcon,
           message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
   // Set system priority so the notification gets shown when the user session is
   // blocked.
@@ -149,7 +149,7 @@ void DetachableBaseNotificationController::ShowPairingNotificationIfNeeded() {
               message_center::NotifierType::SYSTEM_COMPONENT,
               kDetachableBaseNotifierId,
               NotificationCatalogName::kDetachableBasePairingNotification),
-          options, nullptr, chromeos::kNotificationWarningIcon,
+          options, nullptr, vector_icons::kNotificationWarningIcon,
           message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
 
   message_center::MessageCenter::Get()->AddNotification(
--- chrome/browser/ash/login/signin/signin_error_notifier.cc
+++ chrome/browser/ash/login/signin/signin_error_notifier.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "chromeos/ash/components/account_manager/account_manager_factory.h"
-#include "chromeos/ui/vector_icons/vector_icons.h"
 #include "components/account_id/account_id.h"
 #include "components/account_manager_core/account.h"
 #include "components/account_manager_core/chromeos/account_manager.h"
@@ -122,7 +121,7 @@ CreateDeviceAccountErrorNotification(
           GURL(device_account_notification_id), notifier_id, data,
           new message_center::HandleNotificationClickDelegate(
               base::BindRepeating(&HandleDeviceAccountReauthNotificationClick)),
-          chromeos::kNotificationWarningIcon,
+          vector_icons::kNotificationWarningIcon,
           message_center::SystemNotificationWarningLevel::WARNING);
   notification->SetSystemPriority();
 
