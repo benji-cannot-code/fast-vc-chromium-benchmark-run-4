4b9b6537c282a985574c0e0a58fae7cb80a670d8
Katie Dektar <katie@chromium.org>
2022-09-30 22:58:01 +0000

    Moves all remaining Automation V8 routes to ui/.
    Continuing with refactoring AutomationInternalCustomBindings
    to ui/accessibility/platform/automation, this change moves
    all remaining Automation V8 routes into AutomationV8Bindings.
    
    LOW_COVERAGE_REASON=Pure refactor, no new or changed functionality
    
    Bug: 1357889
    Change-Id: I7a5e0072e3b6c4d5a5407e386d02db7e595ddaa6
    AX-Relnotes: N/A
    Text: Existing
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924806
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053814}

--- ui/accessibility/platform/automation/automation_tree_manager_owner.cc
+++ ui/accessibility/platform/automation/automation_tree_manager_owner.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ui/accessibility/platform/automation/automation_tree_manager_owner.h"
+#include <set>
 #include "base/containers/cxx20_erase.h"
 #include "base/containers/flat_tree.h"
 #include "ui/accessibility/ax_event.h"
@@ -709,6 +710,35 @@ void AutomationTreeManagerOwner::DestroyAccessibilityTree(
 
   RemoveAutomationTreeWrapperFromCache(tree_id);
   trees_with_event_listeners_.erase(tree_id);
+}
+
+bool AutomationTreeManagerOwner::CalculateNodeState(const ui::AXTreeID& tree_id,
+                                                    int node_id,
+                                                    uint32_t* node_state,
+                                                    bool* offscreen,
+                                                    bool* focused) const {
+  ui::AutomationAXTreeWrapper* tree_wrapper =
+      GetAutomationAXTreeWrapperFromTreeID(tree_id);
+  if (!tree_wrapper)
+    return false;
+
+  ui::AXNode* node =
+      tree_wrapper->GetNodeFromTree(tree_wrapper->GetTreeID(), node_id);
+  if (!node)
+    return false;
+
+  *node_state = node->data().state;
+
+  ui::AutomationAXTreeWrapper* top_tree_wrapper = nullptr;
+  ui::AutomationAXTreeWrapper* walker = tree_wrapper;
+  while (walker && walker != top_tree_wrapper) {
+    top_tree_wrapper = walker;
+    GetParent(walker->ax_tree()->root(), &walker);
+  }
+
+  *focused = tree_wrapper->IsInFocusChain(node->id());
+  ComputeGlobalNodeBounds(tree_wrapper, node, gfx::RectF(), offscreen);
+  return true;
 }
 
 void AutomationTreeManagerOwner::CacheAutomationTreeWrapperForTreeID(
