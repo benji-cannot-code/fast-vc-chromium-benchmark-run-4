9e7caabc726d9785875d6b1cd0a427e5a157091e
Miyoung Shin <myid.shin@igalia.com>
2022-09-30 04:32:35 +0000

    Reland "[MPArch] Migrate Guest View Test in identity_apitest.cc"
    This is a reland of commit f520c148544fde31f9b03d3fa0a8452d07b512e9
    that fixes the test failure on only MAC buildbots by changing
    to wait until the embedder WebContents is ready.
    
    LOW_COVERAGE_REASON=Changed only test codes.
    
    Original change's description:
    > [MPArch] Migrate Guest View Test in identity_apitest.cc
    >
    > This CL uses TestGuestView* APIs and WaitForSingleGuestViewCreated
    > to get the guest view, and removes the WaitForGURLAndCloseWindow
    > class in LaunchWebAuthFlowFunctionTest.UserCloseWindow.
    >
    > Bug: 1261928
    > Change-Id: If6987620b847edcda03c5cbd91be8f427b7bb7f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3892535
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1048634}
    
    Bug: 1261928
    Change-Id: I85782d3a8a72bdd68fde21c7c359848b2586a3df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908038
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1053403}

--- components/guest_view/browser/test_guest_view_manager.cc
+++ components/guest_view/browser/test_guest_view_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/test/test_timeouts.h"
 #include "components/guest_view/browser/guest_view_base.h"
 #include "components/guest_view/browser/guest_view_manager_delegate.h"
 #include "content/public/browser/render_frame_host.h"
@@ -145,6 +146,16 @@ void TestGuestViewManager::WaitUntilAttached(GuestViewBase* guest_view) {
 
   attached_run_loop_ = std::make_unique<base::RunLoop>();
   attached_run_loop_->Run();
+
+  // Completion of the attachment process may be delayed despite AttachGuest
+  // having been called. We need to wait until the attachment is no longer
+  // considered in progress.
+  while (!guest_view->attached()) {
+    base::RunLoop run_loop;
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, run_loop.QuitClosure(), TestTimeouts::tiny_timeout());
+    run_loop.Run();
+  }
 }
 
 void TestGuestViewManager::WaitForViewGarbageCollected() {
