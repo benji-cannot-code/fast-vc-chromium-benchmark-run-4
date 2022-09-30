949478f27fde391a108a6181b67fc115ca272b8b
Li Lin <llin@chromium.org>
2022-09-30 01:19:48 +0000

    Notify user when recording is aborted due to audio capture disabled.
    Bug: b/249355530
    Change-Id: I582edd1393dd512edaac0f02d51210e359945154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923874
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053298}

--- ash/projector/projector_ui_controller.cc
+++ ash/projector/projector_ui_controller.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
-#include "ash/strings/grit/ash_strings.h"
 #include "ash/system/toast/toast_manager_impl.h"
 #include "base/callback_helpers.h"
 #include "components/live_caption/views/caption_bubble.h"
@@ -123,11 +122,11 @@ void ShowNotification(
 }  // namespace
 
 // static
-void ProjectorUiController::ShowFailureNotification(int message_id) {
+void ProjectorUiController::ShowFailureNotification(int message_id,
+                                                    int title_id) {
   RecordCreationFlowError(message_id);
   ShowNotification(
-      kProjectorErrorNotificationId, IDS_ASH_PROJECTOR_FAILURE_TITLE,
-      message_id,
+      kProjectorErrorNotificationId, title_id, message_id,
       message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
 }
 
--- ash/projector/projector_ui_controller.h
+++ ash/projector/projector_ui_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/projector/projector_metrics.h"
 #include "ash/public/cpp/projector/projector_session.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/scoped_observation.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -27,7 +28,9 @@ class ASH_EXPORT ProjectorUiController : public ProjectorSessionObserver {
  public:
   // Shows a notification informing the user that a Projector error has
   // occurred.
-  static void ShowFailureNotification(int message_id);
+  static void ShowFailureNotification(
+      int message_id,
+      int title_id = IDS_ASH_PROJECTOR_FAILURE_TITLE);
 
   // Shows a notification informing the user that a Projector save error has
   // occurred.
