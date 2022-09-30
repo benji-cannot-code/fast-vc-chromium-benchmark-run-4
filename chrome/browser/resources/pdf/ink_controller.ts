21df3e049c82556ed9f9df071c618e1f725b99b8
Rebekah Potter <rbpotter@chromium.org>
2022-09-30 01:32:53 +0000

    PDF: Remove unnecessary use of deprecated event_target.js
    event_target.js is a workaround for the closure compiler. It should
    not be used by any TypeScript files.
    
    Change-Id: Ie36087a73235b6a6abc366c1804eb2880941c6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929580
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053320}

--- chrome/browser/resources/pdf/ink_controller.ts
+++ chrome/browser/resources/pdf/ink_controller.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assertNotReached} from 'chrome://resources/js/assert_ts.js';
-import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.js';
 
 import {AnnotationTool} from './annotation_tool.js';
 import {SaveRequestType} from './constants.js';
--- chrome/test/data/pdf/viewport_scroller_test.ts
+++ chrome/test/data/pdf/viewport_scroller_test.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {Point, ViewportInterface, ViewportScroller} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
-import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.js';
 
 class FakePlugin extends EventTarget {
   get offsetWidth(): number {
