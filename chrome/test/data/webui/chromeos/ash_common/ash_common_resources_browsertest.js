585dbe49870d81fe6385e7e3fa41bb419a617610
Rebekah Potter <rbpotter@chromium.org>
2022-09-30 17:57:43 +0000

    WebUI: Move list_property_update_behavior to ash/
    The file is only used in OS settings and the ash network component.
    
    Bug: 1347740, 1364309
    Change-Id: I09586054391072cec3f51293f349a47a40f0aab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928472
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053659}

--- chrome/test/data/webui/chromeos/ash_common/ash_common_resources_browsertest.js
+++ chrome/test/data/webui/chromeos/ash_common/ash_common_resources_browsertest.js
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Polymer BrowserTest fixture.
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
-GEN('#include "build/chromeos_buildflags.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
-var WebUIResourcesBrowserTest = class extends PolymerTest {
+var AshCommonResourcesBrowserTest = class extends PolymerTest {
   /** @override */
   get browsePreload() {
     throw new Error('this is abstract and should be overridden by subclasses');
@@ -23,16 +22,14 @@ var WebUIResourcesBrowserTest = class extends PolymerTest {
   }
 };
 
-GEN('#if BUILDFLAG(IS_CHROMEOS_ASH)');
-var WebUIResourcesListPropertyUpdateBehaviorTest =
-    class extends WebUIResourcesBrowserTest {
+var AshCommonResourcesListPropertyUpdateBehaviorTest =
+    class extends AshCommonResourcesBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://webui-test/test_loader.html?module=resources/list_property_update_behavior_tests.js';
+    return 'chrome://webui-test/test_loader.html?module=chromeos/ash_common/list_property_update_behavior_tests.js';
   }
 };
 
-TEST_F('WebUIResourcesListPropertyUpdateBehaviorTest', 'All', function() {
+TEST_F('AshCommonResourcesListPropertyUpdateBehaviorTest', 'All', function() {
   mocha.run();
 });
-GEN('#endif');
