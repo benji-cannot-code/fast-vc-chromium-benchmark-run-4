4e7e22e58458f5035c7b8659a8953ed95c1cdc2e
Mei Liang <meiliang@chromium.org>
2022-09-30 01:13:34 +0000

    Revert "[iOS][Omnibox] Fix crash in adaptive suggestion"
    This reverts commit 44f4f7539f45420840681ce8d97452d0a54a1ffb.
    
    Reason for revert: This breaks the tree. https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator/75678/overview
    
    Original change's description:
    > [iOS][Omnibox] Fix crash in adaptive suggestion
    >
    > Visible suggestions computation is broken with iOS 16.
    > It can be negative.
    >
    > Replace variable to unsigned, so visible suggestions will always be
    > maxed to the number of suggestions.
    >
    > Bug: 1366143,1369538
    > Change-Id: Ie8efda6082102df3783cfdbd09dbaf275dce2410
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925532
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Auto-Submit: Christian Xu <christianxu@chromium.org>
    > Commit-Queue: Christian Xu <christianxu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1052958}
    
    Bug: 1366143,1369538
    Change-Id: I1b5bc9828c443359b527768bde10c41374a87bbf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929463
    Owners-Override: Yoshifumi Inoue <yosin@google.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053288}

--- ios/chrome/browser/ui/omnibox/popup/autocomplete_result_consumer.h
+++ ios/chrome/browser/ui/omnibox/popup/autocomplete_result_consumer.h
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // user doesn't have to scroll or hide the keyboard to see those `n` first
 // suggestions.
 - (void)requestResultsWithVisibleSuggestionCount:
-    (NSUInteger)visibleSuggestionCount
+    (NSInteger)visibleSuggestionCount
     __attribute__((swift_name("requestResults(visibleSuggestionCount:)")));
 ;
 
