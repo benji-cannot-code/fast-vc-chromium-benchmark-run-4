05eed6af8b2b3b73911624787b2dfd3e4e18f030
Matt Reichhoff <mreichhoff@chromium.org>
2022-09-30 14:59:24 +0000

    [rsafor] Instrument rsaFor calls
    This change begins writing similar metrics for
    requestStorageAccessForOrigin as are written for standard
    requestStorageAccess calls.
    
    Bug: 1351540
    Change-Id: Ib86412d59338027778ed5665e00c4d5e8e6789c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924183
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Brandon Maslen <brandm@microsoft.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053573}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -378,11 +378,17 @@ enum class RequestStorageResult {
   REJECTED_EXISTING_DENIAL = 5,
   REJECTED_SANDBOXED = 6,
   REJECTED_GRANT_DENIED = 7,
-  kMaxValue = REJECTED_GRANT_DENIED,
+  REJECTED_INCORRECT_FRAME = 8,
+  kMaxValue = REJECTED_INCORRECT_FRAME,
 };
 void FireRequestStorageAccessHistogram(RequestStorageResult result) {
   base::UmaHistogramEnumeration("API.StorageAccess.RequestStorageAccess",
                                 result);
+}
+
+void FireRequestStorageAccessForOriginHistogram(RequestStorageResult result) {
+  base::UmaHistogramEnumeration(
+      "API.StorageAccess.RequestStorageAccessForOrigin", result);
 }
 
 class IntrinsicSizeResizeObserverDelegate : public ResizeObserver::Delegate {
@@ -6063,6 +6069,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
     ScriptState* script_state,
     const AtomicString& origin) {
   if (!GetFrame()) {
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::REJECTED_NO_ORIGIN);
     // Note that in detached frames, resolvers are not able to return a promise.
     return ScriptPromise::RejectWithDOMException(
         script_state,
@@ -6088,6 +6096,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
         "requestStorageAccessForOrigin: Must be handling a user gesture to "
         "use."));
 
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::REJECTED_NO_USER_GESTURE);
     resolver->Reject();
     return promise;
   }
@@ -6098,6 +6108,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
         mojom::blink::ConsoleMessageLevel::kError,
         "requestStorageAccessForOrigin: Only supported in primary top-level "
         "browsing contexts."));
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::REJECTED_INCORRECT_FRAME);
     resolver->Reject();
     return promise;
   }
@@ -6108,6 +6120,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
         mojom::blink::ConsoleMessageLevel::kError,
         "requestStorageAccessForOrigin: Cannot be used by opaque origins."));
 
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::REJECTED_OPAQUE_ORIGIN);
     resolver->Reject();
     return promise;
   }
@@ -6120,6 +6134,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
         mojom::blink::ConsoleMessageSource::kSecurity,
         mojom::blink::ConsoleMessageLevel::kError,
         "requestStorageAccessForOrigin: Invalid origin parameter."));
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::REJECTED_OPAQUE_ORIGIN);
     resolver->Reject();
     return promise;
   }
@@ -6127,6 +6143,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
   if (dom_window_->GetSecurityOrigin()->IsSameSiteWith(supplied_origin.get())) {
     // Access is not actually disabled, so accept the request.
     resolver->Resolve();
+    FireRequestStorageAccessForOriginHistogram(
+        RequestStorageResult::APPROVED_EXISTING_ACCESS);
     return promise;
   }
 
@@ -6151,6 +6169,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
                 switch (status) {
                   case mojom::blink::PermissionStatus::GRANTED:
                     document->expressly_denied_storage_access_ = false;
+                    FireRequestStorageAccessForOriginHistogram(
+                        RequestStorageResult::APPROVED_NEW_GRANT);
                     resolver->Resolve();
                     break;
                   case mojom::blink::PermissionStatus::DENIED:
@@ -6160,6 +6180,8 @@ ScriptPromise Document::requestStorageAccessForOrigin(
                     [[fallthrough]];
                   case mojom::blink::PermissionStatus::ASK:
                   default:
+                    FireRequestStorageAccessForOriginHistogram(
+                        RequestStorageResult::REJECTED_GRANT_DENIED);
                     resolver->Reject();
                 }
               },
