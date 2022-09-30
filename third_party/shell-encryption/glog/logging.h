50c2db1f8b7e419f28d4ddd2fc72c11c4465f27b
Avi Drissman <avi@chromium.org>
2022-09-30 17:25:59 +0000

    Fix checkdeps loophole
    It used to be that having a missing space between #include and
    the quoted file path would remove the include from consideration
    by checkdeps. This CL makes that no longer be so.
    
    Fixed: 1369334
    Change-Id: I4447b791376d226198bef15d9b2ba042de91b9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3927862
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053640}

--- third_party/shell-encryption/glog/logging.h
+++ third_party/shell-encryption/glog/logging.h
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright 2020 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef SHELL_ENCRYPTION_GLOG_H_
 #define SHELL_ENCRYPTION_GLOG_H_
 
-#include"base/logging.h"
+#include "base/logging.h"
 
-#endif	// SHELL_ENCRYPTION_GLOG_H_
+#endif  // SHELL_ENCRYPTION_GLOG_H_
