4f70358607f6e621d4997edec0b5c147f305d344
Rebekah Potter <rbpotter@chromium.org>
2022-09-30 21:06:46 +0000

    WebUI: Remove non-CrOS/iOS use of util_deprecated
    Bug: 1364309
    Change-Id: Ic78a199427dda37a91e5b343dc69d72b026caa1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928003
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053763}

--- components/management/resources/management.js
+++ components/management/resources/management.js
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 document.addEventListener('DOMContentLoaded', function() {
   if (isManaged()) {
-    $('managed-info').classList.remove('hidden');
+    document.body.querySelector('#managed-info').classList.remove('hidden');
   } else {
-    $('unmanaged-info').classList.remove('hidden');
+    document.body.querySelector('#unmanaged-info').classList.remove('hidden');
   }
 });
