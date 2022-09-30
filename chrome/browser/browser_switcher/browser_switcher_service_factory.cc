b7b70b1328c1acd8da0f16a7b1eefdd83fdf9386
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-09-30 16:22:53 +0000

    [BrowserSwitcher] Exclude from LaCrOS & Fuchsia builds
    Legacy Browser Support was implemented before we added LaCrOS and
    Fuchsia to Chromium's supported platforms. It is compiled on those
    platforms, but this is accidental rather than intentional.
    
    Legacy Browser Support doesn't do anything meaningful or useful on
    Fuchsia or LaCrOS, and we don't support it. Exclude these files from the
    build.
    
    Change-Id: Ie85c8c83dd171090dd1c7c839fd3e7bfa31756e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3926265
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053608}

--- chrome/browser/browser_switcher/browser_switcher_service_factory.cc
+++ chrome/browser/browser_switcher/browser_switcher_service_factory.cc
@@ -16,6 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_switcher/browser_switcher_service_win.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS)
+#error BrowserSwitcher is not supported on ChromeOS. Neither Ash nor LaCrOS.
+#endif
+
+#if BUILDFLAG(IS_FUCHSIA)
+#error BrowserSwitcher is not support on Fuchsia.
+#endif
+
 namespace browser_switcher {
 
 namespace {
