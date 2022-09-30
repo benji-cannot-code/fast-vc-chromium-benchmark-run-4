a0ed527b708859ebd46d66d03c690d82b4f0ccb0
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-09-30 15:51:39 +0000

    [BrowserSwitcher] Don't create Service for Guest/System profiles
    Currently, BrowserSwitcherServiceFactory doesn't distinguish between
    regular and Guest/System profiles, or even primary and Incognito.
    
    In practice, this was not a problem. BrowserSwitcherService is created
    via a NavigationThrottle that only triggers for regular, non-OTR
    profiles. This behaviour is only correct by accident, and it's not
    explicitly stated in the code anywhere.
    
    Tweak this Factory so it only creates a Service for non-OTR, regular
    profiles. This extra safeguard makes the intended behaviour obvious.
    
    Change-Id: Ib0deeffb6c31efb74a68c9078967dfdec45505e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921945
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053593}

--- chrome/browser/browser_switcher/browser_switcher_navigation_throttle.cc
+++ chrome/browser/browser_switcher/browser_switcher_navigation_throttle.cc
@@ -50,6 +50,9 @@ bool MaybeLaunchAlternativeBrowser(
   BrowserSwitcherService* service =
       BrowserSwitcherServiceFactory::GetForBrowserContext(
           navigation_handle->GetWebContents()->GetBrowserContext());
+  if (!service)
+    return false;
+
   const GURL& url = navigation_handle->GetURL();
   bool should_switch = service->sitelist()->ShouldSwitch(url);
 
@@ -90,8 +93,9 @@ BrowserSwitcherNavigationThrottle::MaybeCreateThrottleFor(
 
   content::BrowserContext* browser_context =
       navigation->GetWebContents()->GetBrowserContext();
+  Profile* profile = Profile::FromBrowserContext(browser_context);
 
-  if (browser_context->IsOffTheRecord())
+  if (!profile->IsRegularProfile())
     return nullptr;
 
   if (!navigation->IsInPrimaryMainFrame())
