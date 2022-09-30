424fc6f31f728302df4682440546099ca9b09a1f
dpapad <dpapad@chromium.org>
2022-09-30 23:00:15 +0000

    InlineLogin: Migrate tests to TypeScript.
    Bug: 1296509
    Change-Id: I615d0ea1dd6765ec166a8ad854a4c4393c50e456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3927815
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053818}

--- chrome/browser/resources/inline_login/inline_login_app.ts
+++ chrome/browser/resources/inline_login/inline_login_app.ts
@@ -22,6 +22,7 @@ import {CrViewManagerElement} from 'chrome://resources/cr_elements/cr_view_manag
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {WebUIListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
 import {isRTL} from 'chrome://resources/js/util.js';
+import {PaperSpinnerLiteElement} from 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
@@ -73,6 +74,7 @@ interface SigninErrorPageData {
 export interface InlineLoginAppElement {
   $: {
     signinFrame: WebViewElement,
+    spinner: PaperSpinnerLiteElement,
     viewManager: CrViewManagerElement,
   };
 }
@@ -542,7 +544,7 @@ export class InlineLoginAppElement extends InlineLoginAppElementBase {
   }
   // </if>
 
-  private setAuthExtHostForTest(authExtHost: Authenticator) {
+  setAuthExtHostForTest(authExtHost: Authenticator) {
     this.authExtHost_ = authExtHost;
     this.addAuthExtHostListeners_();
   }
