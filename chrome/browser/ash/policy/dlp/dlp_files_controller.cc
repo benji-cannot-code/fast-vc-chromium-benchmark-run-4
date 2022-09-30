b6f4f4d59a17cbfcf2928d441976c813a159b483
Anna Ibatullina <annaib@google.com>
2022-09-30 10:20:50 +0000

    DLP: Add class with info about confidential file
    Create a DlpConfidentialFile class for passing icon and file name to
    the dialog.
    This class is an alternative to DlpConfidentialContent for files. The
    difference between them should be in comparison methods:
    DlpConfidentialContent compares elements by source urls while
    DlpConfidentialFile should compare elements by file paths.
    
    Bug: 1368651
    Change-Id: Icb5f8b2d33da8302ef9586b1f248a501b1f028fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924803
    Commit-Queue: Anna Ibatullina <annaib@google.com>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053486}

--- chrome/browser/ash/policy/dlp/dlp_files_controller.cc
+++ chrome/browser/ash/policy/dlp/dlp_files_controller.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_confidential_contents.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_confidential_file.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_histogram_helper.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_reporting_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chromeos/dbus/dlp/dlp_client.h"
 #include "chromeos/dbus/dlp/dlp_service.pb.h"
-#include "chromeos/ui/base/file_icon_util.h"
 #include "storage/browser/file_system/file_system_url.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/mojom/choosers/file_chooser.mojom.h"
@@ -354,7 +353,7 @@ void DlpFilesController::IsFilesTransferRestricted(
 
   std::vector<FileDaemonInfo> restricted_files;
   std::vector<FileDaemonInfo> warned_files;
-  DlpConfidentialContents dialog_files;
+  std::vector<DlpConfidentialFile> dialog_files;
   std::string destination_pattern;
   std::vector<std::string> warned_source_patterns;
   for (const auto& file : transferred_files) {
@@ -385,10 +384,7 @@ void DlpFilesController::IsFilesTransferRestricted(
       warned_files.push_back(file);
       warned_source_patterns.emplace_back(source_pattern);
       if (files_action != FileAction::kDownload) {
-        dialog_files.Add(
-            // TODO(crbug.com/1357593): Pass the proper |dark_background|
-            chromeos::GetIconForPath(file.path, /*dark_background=*/false),
-            file.path.BaseName().LossyDisplayName(), GURL(file.source_url));
+        dialog_files.emplace_back(file.path);
       }
       DlpHistogramEnumeration(dlp::kFileActionWarnedUMA, files_action);
     }
