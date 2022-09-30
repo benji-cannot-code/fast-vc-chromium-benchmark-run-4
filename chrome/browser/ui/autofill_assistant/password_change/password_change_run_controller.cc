b28f529c4bc8d992a54af3b39f589c2d89595368
Bruno Braga <brunobraga@google.com>
2022-09-30 14:00:30 +0000

    [APC Desktop] Introduce accessibility string
    Setup so that once we have texts being announced we can easily plug and play.
    
    Bug: 1329179
    Change-Id: I87d8e2d1549479aba6b793729bf0b54d0a8d6ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928269
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053543}

---
+++ chrome/browser/ui/autofill_assistant/password_change/password_change_run_controller.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill_assistant/password_change/password_change_run_controller.h"
+
+PasswordChangeRunController::Model::Model() = default;
+PasswordChangeRunController::Model::Model(Model&& other) = default;
+PasswordChangeRunController::Model::Model(Model& other) = default;
+PasswordChangeRunController::Model&
+PasswordChangeRunController::Model::operator=(
+    const PasswordChangeRunController::Model& other) = default;
+PasswordChangeRunController::Model&
+PasswordChangeRunController::Model::operator=(
+    PasswordChangeRunController::Model&& other) = default;
+
+PasswordChangeRunController::Model::~Model() = default;
