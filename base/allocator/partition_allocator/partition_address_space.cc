86a0922bb2805541ec8c4540b242089441f7dbe5
Kalvin Lee <kdlee@chromium.org>
2022-09-30 07:03:36 +0000

    [PA] Fix stray `#define`
    The `#define` for PartitionAlloc's shadow metadata is
    `PA_ENABLE_SHADOW_METADATA`, not `PA_ENABLE_SHADOW_GIGACAGE`.
    
    Bug: 1362969
    Change-Id: I4483f1e71508e5e62a528ed2653f89452d8ba9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924989
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053439}

--- base/allocator/partition_allocator/partition_address_space.cc
+++ base/allocator/partition_allocator/partition_address_space.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif  // BUILDFLAG(IS_WIN)
 
-#if defined(PA_ENABLE_SHADOW_GIGACAGE)
+#if defined(PA_ENABLE_SHADOW_METADATA)
 #include <sys/mman.h>
 #endif
 
@@ -169,7 +169,7 @@ void PartitionAddressSpace::Init() {
     return;
 
   size_t regular_pool_size = RegularPoolSize();
-#if defined(PA_ENABLE_SHADOW_GIGACAGE)
+#if defined(PA_ENABLE_SHADOW_METADATA)
   int regular_pool_fd = memfd_create("/regular_pool", MFD_CLOEXEC);
 #else
   int regular_pool_fd = -1;
@@ -195,7 +195,7 @@ void PartitionAddressSpace::Init() {
       !IsInRegularPool(setup_.regular_pool_base_address_ + regular_pool_size));
 
   size_t brp_pool_size = BRPPoolSize();
-#if defined(PA_ENABLE_SHADOW_GIGACAGE)
+#if defined(PA_ENABLE_SHADOW_METADATA)
   int brp_pool_fd = memfd_create("/brp_pool", MFD_CLOEXEC);
 #else
   int brp_pool_fd = -1;
