109b93f6ff8afa59477d335b9b28630c57a695f1
shrekshao <shrekshao@google.com>
2022-09-30 19:45:00 +0000

    Add maxBufferSize WebGPU Limit
    Dependent on https://dawn-review.googlesource.com/c/dawn/+/103660
    
    Bug: dawn:1525
    Change-Id: Ib5f6e09449724c996c4c8ca0a4e08bc51ea9d322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928415
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053719}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(minUniformBufferOffsetAlignment)           \
   X(minStorageBufferOffsetAlignment)           \
   X(maxVertexBuffers)                          \
+  X(maxBufferSize)                             \
   X(maxVertexAttributes)                       \
   X(maxVertexBufferArrayStride)                \
   X(maxInterStageShaderComponents)             \
--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.h
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.h
@@ -47,6 +47,7 @@ class GPUSupportedLimits final : public ScriptWrappable {
   unsigned minUniformBufferOffsetAlignment() const;
   unsigned minStorageBufferOffsetAlignment() const;
   unsigned maxVertexBuffers() const;
+  uint64_t maxBufferSize() const;
   unsigned maxVertexAttributes() const;
   unsigned maxVertexBufferArrayStride() const;
   unsigned maxInterStageShaderComponents() const;
