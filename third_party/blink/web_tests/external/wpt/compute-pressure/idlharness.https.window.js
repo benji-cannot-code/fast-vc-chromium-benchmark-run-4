cac3aadc822a9a07896dd2ba3242aa14c042d953
Arnaud Mandy <arnaud.mandy@intel.com>
2022-09-30 11:01:33 +0000

    compute pressure: Adding WebIDL interface test
    Introducing testing for WebIDL interface.
    
    The method requestPermission() is not yet implemented, therefore
    we need to add an "expected" file, because testing this method will
    fail.
    
    Bug: 1368452
    Change-Id: Ib834996916a0d66cc98f5216b5eeca3dc261040a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3919091
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1053497}

---
+++ third_party/blink/web_tests/external/wpt/compute-pressure/idlharness.https.window.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+// https://wicg.github.io/compute-pressure/
+
+'use strict';
+
+idl_test(['compute-pressure'], ['dom', 'html'], async idl_array => {
+  idl_array.add_objects({
+    PressureObserver: ['observer'],
+  });
+
+  self.observer = new PressureObserver(() => {}, {sampleRate: 1.0});
+});
