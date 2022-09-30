a8b17a777a68f53d6580c1b4c5e3d7115f0dec9b
Ben Wagner <benjaminwagner@google.com>
2022-09-30 13:00:16 +0000

    support creating BrowserCaptureMediaStreamTrack in FromTransferredState
    Add fields to MediaStreamTrack::TransferredValues to support
    serializing subtypes of MediaStreamTrack. Currently the only new bit of
    data is the crop_version for BrowserCaptureMediaStreamTrack. No new
    data is needed for FocusableMediaStreamTrack (its parent class) and
    other subtypes are not yet supported. Correct handling of crop_version
    is left for a future CL -- this functionality is still behind a
    command-line flag.
    
    Ensure that a transferred FocusableMediaStreamTrack can't be focused,
    since the window of opportunity is considered to have ended.
    
    Return the actual implementation track from
    MediaStreamTrack::FromTransferredState so that methods are available
    to JS.
    
    LOW_COVERAGE_REASON=https://crbug.com/843209
    
    Bug: 1288839
    Change-Id: I8065e599a54d311066154de7536181e2053a5ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928726
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053529}

--- third_party/blink/renderer/modules/mediastream/media_stream_track.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_track.h
@@ -59,6 +59,7 @@ class MODULES_EXPORT MediaStreamTrack
 
   // For carrying data to the FromTransferredState method.
   struct TransferredValues {
+    const WrapperTypeInfo* track_impl_subtype;
     base::UnguessableToken session_id;
     base::UnguessableToken transfer_id;
     String kind;
@@ -68,6 +69,9 @@ class MODULES_EXPORT MediaStreamTrack
     bool muted;
     WebMediaStreamTrack::ContentHintType content_hint;
     MediaStreamSource::ReadyState ready_state;
+    // Set only if
+    // track_impl_subtype->IsSubclass(BrowserCaptureMediaStreamTrack::GetStaticWrapperTypeInfo())
+    absl::optional<uint32_t> crop_version;
   };
 
   // See SetFromTransferredStateImplForTesting in ./test/transfer_test_utils.h.
