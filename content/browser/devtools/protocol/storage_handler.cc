5625ae20bffda82fbb3e3b21621a1d2a6a3c1af0
Danil Somsikov <dsv@chromium.org>
2022-09-30 09:24:59 +0000

    Disable Storage.getCookies for untrusted clients. It doesn't seem to be used by extensions right now and is exposing information that is browser wide and might be inappropriate for extensions.
    Bug: 1344647
    Change-Id: I37e3fcdfed312342d100b489ed523425bd2b0a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929023
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053471}

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -270,8 +270,9 @@ class StorageHandler::IndexedDBObserver
   mojo::Receiver<storage::mojom::IndexedDBObserver> receiver_;
 };
 
-StorageHandler::StorageHandler()
-    : DevToolsDomainHandler(Storage::Metainfo::domainName) {}
+StorageHandler::StorageHandler(bool client_is_trusted)
+    : DevToolsDomainHandler(Storage::Metainfo::domainName),
+      client_is_trusted_(client_is_trusted) {}
 
 StorageHandler::~StorageHandler() {
   DCHECK(!cache_storage_observer_);
@@ -300,6 +301,8 @@ Response StorageHandler::Disable() {
 
 void StorageHandler::GetCookies(Maybe<std::string> browser_context_id,
                                 std::unique_ptr<GetCookiesCallback> callback) {
+  if (!client_is_trusted_)
+    callback->sendFailure(Response::ServerError("Permission denied"));
   StoragePartition* storage_partition = nullptr;
   Response response = StorageHandler::FindStoragePartition(browser_context_id,
                                                            &storage_partition);
