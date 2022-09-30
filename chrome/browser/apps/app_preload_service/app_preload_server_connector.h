c6597344c3c0c1d0e5cf0d647ca73d616cb64a4d
Jeevan Shikaram <jshikaram@chromium.org>
2022-09-30 07:11:39 +0000

    [APS] Add an APS server connector class.
    This CL adds an APS server connector class that allows the APS to make
    calls to the backend server API. This CL begins the skeleton
    implementation of the class.
    
    Change-Id: Ibb396198433e205793c240889094f46f1ec7c04c
    Bug: b:243088845, b:243089046, b:244647696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867072
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053442}

--- chrome/browser/apps/app_preload_service/app_preload_server_connector.h
+++ chrome/browser/apps/app_preload_service/app_preload_server_connector.h
@@ -6,7 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_SERVER_CONNECTOR_H_
 #define CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_SERVER_CONNECTOR_H_
 
+#include <memory>
+
 #include "base/callback_forward.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+
+namespace network {
+class SimpleURLLoader;
+}  // namespace network
 
 namespace apps {
 
@@ -25,8 +34,20 @@ class AppPreloadServerConnector {
       delete;
   ~AppPreloadServerConnector();
 
-  void GetAppsForFirstLogin(const DeviceInfo device_info,
-                            GetInitialAppsCallback callback);
+  void GetAppsForFirstLogin(
+      const DeviceInfo& device_info,
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+      GetInitialAppsCallback callback);
+
+ private:
+  void OnGetAppsForFirstLoginResponse(
+      GetInitialAppsCallback callback,
+      std::unique_ptr<std::string> response_body);
+
+  std::unique_ptr<network::SimpleURLLoader> loader_;
+
+  // Weak Factory should go last.
+  base::WeakPtrFactory<AppPreloadServerConnector> weak_ptr_factory_{this};
 };
 
 }  // namespace apps
--- chrome/browser/apps/app_preload_service/app_preload_service_unittest.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_unittest.cc
@@ -4,11 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/apps/app_preload_service/app_preload_service.h"
+
 #include <algorithm>
 #include <memory>
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/apps/app_preload_service/app_preload_service_factory.h"
@@ -16,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_task_environment.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -26,6 +30,9 @@ static constexpr char kFirstLoginFlowCompletedKey[] =
 
 static constexpr char kApsStateManager[] =
     "apps.app_preload_service.state_manager";
+
+static constexpr char kServerUrl[] =
+    "http://localhost:9876/v1/app_provisioning/apps";
 
 const base::Value::Dict& GetStateManager(Profile* profile) {
   return profile->GetPrefs()->GetDict(kApsStateManager);
@@ -56,10 +63,16 @@ class AppPreloadServiceTest : public testing::Test {
   void SetUp() override {
     testing::Test::SetUp();
 
-    profile_ = std::make_unique<TestingProfile>();
+    TestingProfile::Builder profile_builder;
+    profile_builder.SetSharedURLLoaderFactory(
+        base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+            &url_loader_factory_));
+    profile_ = profile_builder.Build();
   }
 
   Profile* GetProfile() { return profile_.get(); }
+
+  network::TestURLLoaderFactory url_loader_factory_;
 
  private:
   content::BrowserTaskEnvironment task_environment_;
@@ -99,6 +112,9 @@ TEST_F(AppPreloadServiceTest, ServiceAccessPerProfile) {
 }
 
 TEST_F(AppPreloadServiceTest, FirstLoginPrefSet) {
+  // TODO: flesh out the response as response processing code is added.
+  url_loader_factory_.AddResponse(kServerUrl, "empty content");
+
   auto flow_completed =
       GetStateManager(GetProfile()).FindBool(kFirstLoginFlowCompletedKey);
   // Since we're creating a new profile with no saved state, we expect the
