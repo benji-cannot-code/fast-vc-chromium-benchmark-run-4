fe38d3577208c883106f5baa2861012d09beb023
Thomas Lukaszewicz <tluk@chromium.org>
2022-09-30 05:21:32 +0000

    Reland "[full restore] Restore the OS URL Handler SWA's override URL"
    This is a reland of commit 9014f908dda8ba9ebb7536a87e8eeeb203cd7bd8
    
    Test was failing on bots due to the async nature of testing for the
    committed URL. The reland instead waits for the new browser to commit.
    
    See diff from patchset 1.
    
    Original change's description:
    > [full restore] Restore the OS URL Handler SWA's override URL
    >
    > This CL updates Full Restore code to account for the optional
    > override URL AppLaunchInfo param when launching SWAs. Previously SWAs
    > were restored with their default app URL. This behavior has been made
    > opt-in.
    >
    > This was causing issues with apps such as the OS URL Handler SWA which
    > sets a meaningless placeholder URL (chrome://internal) as its app URL,
    > instead relying on the override URL to navigate to the right system
    > page. When this SWA was restored it would ignore the override URL and
    > instead navigate to the empty chrome://internal page.
    >
    > Follow ups should investigate the possibility of having all SWAs be
    > restored with their override URL (if specified at launch).
    >
    > LOW_COVERAGE_REASON=Covered by browser tests
    >
    > Bug: 1279938, 1368285
    > Change-Id: Iaf92a061cee06e809b121a6d40f9f1f74098a45a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918980
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1052750}
    
    Bug: 1279938, 1368285
    Change-Id: I9f6a2b74fff595999c01382b61d1ff5395732388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928549
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053416}

--- chrome/browser/ash/system_web_apps/types/system_web_app_delegate.cc
+++ chrome/browser/ash/system_web_apps/types/system_web_app_delegate.cc
@@ -127,6 +127,10 @@ bool SystemWebAppDelegate::ShouldShowTabContextMenuShortcut(
   return true;
 }
 
+bool SystemWebAppDelegate::ShouldRestoreOverrideUrl() const {
+  return false;
+}
+
 bool SystemWebAppDelegate::IsUrlInSystemAppScope(const GURL& url) const {
   return false;
 }
