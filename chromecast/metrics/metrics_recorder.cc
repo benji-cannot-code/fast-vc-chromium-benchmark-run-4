24a1ea74b52642f0ff03ebdec211072cfbe9c2d9
Matt Swartwout <mwswartwout@google.com>
2022-09-30 18:52:25 +0000

    Add new standard_agent_id field to Cast metrics
    Bug: b/249806511
    Test: None
    Change-Id: Ia56c1c934b4e148315695d5162ea583ecbbee644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929441
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Matt Swartwout <mwswartwout@google.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053693}

--- chromecast/metrics/metrics_recorder.cc
+++ chromecast/metrics/metrics_recorder.cc
@@ -112,6 +112,11 @@ class DummyEventBuilder : public CastEventBuilder {
     return *this;
   }
 
+  CastEventBuilder& SetAoghStandardAgentId(
+      const std::string& agent_id) override {
+    return *this;
+  }
+
   CastEventBuilder& SetUiVersion(const std::string& ui_version) override {
     return *this;
   }
