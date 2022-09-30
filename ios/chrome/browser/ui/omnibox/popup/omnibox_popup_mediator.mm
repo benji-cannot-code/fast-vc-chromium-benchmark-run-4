b792d0a5bfd8b50c9e4ea950a73c9a2aab277af1
Christian Xu <christianxu@google.com>
2022-09-30 17:07:08 +0000

    [iOS][Omnibox] Fix crash in adaptive suggestion
    Visible suggestions computation is broken with iOS 16.
    It can be negative.
    
    Replace variable to unsigned, so visible suggestions will always be
    maxed to the number of suggestions.
    
    Bug: 1366143,1369538
    Change-Id: I373405ad27d026805d155d2e5c302f192267c42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3926065
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053629}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
@@ -116,9 +116,9 @@ const CGFloat kOmniboxIconSize = 16;
 #pragma mark - AutocompleteResultDataSource
 
 - (void)requestResultsWithVisibleSuggestionCount:
-    (NSInteger)visibleSuggestionCount {
-  size_t visibleSuggestions =
-      MIN(visibleSuggestionCount, (NSInteger)_currentResult.size());
+    (NSUInteger)visibleSuggestionCount {
+  NSUInteger visibleSuggestions =
+      MIN(visibleSuggestionCount, _currentResult.size());
   if (visibleSuggestions > 0) {
     // Groups visible suggestions by search vs url. Skip the first suggestion
     // because it's the omnibox content.
@@ -403,6 +403,8 @@ const CGFloat kOmniboxIconSize = 16;
 }
 
 - (void)groupCurrentSuggestionsFrom:(NSUInteger)begin to:(NSUInteger)end {
+  DCHECK(begin <= _currentResult.size());
+  DCHECK(end <= _currentResult.size());
   AutocompleteResult::GroupSuggestionsBySearchVsURL(
       std::next(_currentResult.begin(), begin),
       std::next(_currentResult.begin(), end));
