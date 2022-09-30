7bf81755675cb1efcc1de7a44757404418769885
Robert Ogden <robertogden@chromium.org>
2022-09-30 17:20:28 +0000

    Pull Tensorflow Models repo into chromium //third_party
    The whole repo itself is rather large, so only the relevant files are
    added.
    
    Currently the library is not being built into Chrome, that will come in
    a future change, waiting on golden testdata.
    
    Bug: b/245564409
    Change-Id: I057fc4f2dc567298385b276bb2830d249a32a60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917401
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053638}

---
+++ third_party/tensorflow_models/shims/libutf/utf.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_TENSORFLOW_MODELS_LIBUTF_UTF_H_
+#define THIRD_PARTY_TENSORFLOW_MODELS_LIBUTF_UTF_H_
+
+// This file exists so that //third_party/tensorflow_models/src can use
+// #include "libutf/utf.h"
+// to include Chromium's libutf which lives at
+// //third_party/utf/src/include/utf.h without needing local patches.
+
+#include "third_party/utf/src/include/utf.h"
+
+#endif  // THIRD_PARTY_TENSORFLOW_MODELS_LIBUTF_UTF_H_
