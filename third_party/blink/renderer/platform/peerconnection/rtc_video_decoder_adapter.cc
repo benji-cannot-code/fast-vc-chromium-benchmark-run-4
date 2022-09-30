bec67c5dc68b6423fd4289e64d1c5d73c433eb5d
Hirokazu Honda <hiroh@chromium.org>
2022-09-30 16:06:39 +0000

    RTCVDAdapter: Replace |has_error_| with |status_|
    This CL introduces RTCVideoDecoderAdapter::Status. It replaces
    |has_error_| and |key_frame_required_|.
    
    Bug: b:241349739
    Test: webrtc playground
    Test: Meet call
    Change-Id: Ide6f190ceca84b09e192ada937976fa7c6f82674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3844855
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053602}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
@@ -275,7 +275,8 @@ RTCVideoDecoderAdapter::EnqueueBuffer(
     return *fallback_reason;
   }
 
-  if (key_frame_required_) {
+  base::AutoLock auto_lock(lock_);
+  if (status_ == Status::kNeedKeyFrame) {
     // We discarded previous frame because we have too many pending buffers (see
     // logic) below. Now we need to wait for the key frame and discard
     // everything else.
@@ -285,14 +286,12 @@ RTCVideoDecoderAdapter::EnqueueBuffer(
     }
     DVLOG(2) << "Key frame received, resume decoding";
     // ok, we got key frame and can continue decoding
-    key_frame_required_ = false;
+    ChangeStatus(Status::kOk);
   }
 
   // Queue for decoding.
-  base::AutoLock auto_lock(lock_);
-  if (has_error_) {
+  if (status_ == Status::kError)
     return RTCVideoDecoderFallbackReason::kPreviousErrorOnDecode;
-  }
 
   if (HasSoftwareFallback(config_.codec()) &&
       pending_buffers_.size() >= kMaxPendingBuffers) {
@@ -302,11 +301,12 @@ RTCVideoDecoderAdapter::EnqueueBuffer(
     pending_buffers_.clear();
     // Actually we just discarded a frame. We must wait for the key frame and
     // drop any other non-key frame.
-    key_frame_required_ = true;
     if (++consecutive_error_count_ > kMaxConsecutiveErrors) {
       decode_timestamps_.clear();
+      ChangeStatus(Status::kError);
       return RTCVideoDecoderFallbackReason::kConsecutivePendingBufferOverflow;
     }
+    ChangeStatus(Status::kNeedKeyFrame);
     return DecodeResult::kErrorRequestKeyFrame;
   }
 
@@ -382,7 +382,7 @@ void RTCVideoDecoderAdapter::OnDecodeDone(media::DecoderStatus status) {
                              static_cast<int>(status.code()));
 
     base::AutoLock auto_lock(lock_);
-    has_error_ = true;
+    ChangeStatus(Status::kError);
     pending_buffers_.clear();
     decode_timestamps_.clear();
     return;
@@ -535,6 +535,9 @@ bool RTCVideoDecoderAdapter::InitializeSync(
   }
 
   decoder_info_.implementation_name = "ExternalDecoder (" + decoder_name + ")";
+
+  base::AutoLock auto_lock(lock_);
+  ChangeStatus(Status::kNeedKeyFrame);
   return result;
 }
 
@@ -552,13 +555,15 @@ bool RTCVideoDecoderAdapter::Configure(const Settings& settings) {
       static_cast<int32_t>(settings.max_render_resolution().Width()) *
       settings.max_render_resolution().Height();
 
+  const bool init_success = status_ != Status::kError;
   base::UmaHistogramBoolean("Media.RTCVideoDecoderInitDecodeSuccess",
-                            !has_error_);
-  if (!has_error_) {
+                            init_success);
+
+  if (init_success) {
     UMA_HISTOGRAM_ENUMERATION("Media.RTCVideoDecoderProfile", config_.profile(),
                               media::VIDEO_CODEC_PROFILE_MAX + 1);
   }
-  return !has_error_;
+  return init_success;
 }
 
 int32_t RTCVideoDecoderAdapter::Decode(const webrtc::EncodedImage& input_image,
@@ -619,13 +624,13 @@ int32_t RTCVideoDecoderAdapter::RegisterDecodeCompleteCallback(
 
   base::AutoLock auto_lock(lock_);
   decode_complete_callback_ = callback;
-  if (has_error_) {
+  if (status_ == Status::kError) {
     RecordRTCVideoDecoderFallbackReason(
         config_.codec(),
         RTCVideoDecoderFallbackReason::kPreviousErrorOnRegisterCallback);
+    return WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE;
   }
-  return has_error_ ? WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE
-                    : WEBRTC_VIDEO_CODEC_OK;
+  return WEBRTC_VIDEO_CODEC_OK;
 }
 
 int32_t RTCVideoDecoderAdapter::Release() {
@@ -634,8 +639,8 @@ int32_t RTCVideoDecoderAdapter::Release() {
   base::AutoLock auto_lock(lock_);
   pending_buffers_.clear();
   decode_timestamps_.clear();
-  return has_error_ ? WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE
-                    : WEBRTC_VIDEO_CODEC_OK;
+  return status_ == Status::kError ? WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE
+                                   : WEBRTC_VIDEO_CODEC_OK;
 }
 
 bool RTCVideoDecoderAdapter::ShouldReinitializeForSettingHDRColorSpace(
@@ -684,6 +689,12 @@ bool RTCVideoDecoderAdapter::ReinitializeSync(
     RecordReinitializationLatency(base::TimeTicks::Now() - start_time);
   }
   return result;
+}
+
+void RTCVideoDecoderAdapter::ChangeStatus(Status new_status) {
+  // It is impossible to recover once status becomes kError.
+  if (status_ != Status::kError)
+    status_ = new_status;
 }
 
 // static
