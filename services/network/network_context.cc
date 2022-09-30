4387064be736db28ab1e0efaecffbceec6085f4d
Matt Mueller <mattm@chromium.org>
2022-09-30 01:58:48 +0000

    Clean up dead cert_net_fetcher_ code in NetworkContext.
    NetworkContext always uses CertNetFetcherURLLoader since
    https://chromium-review.googlesource.com/c/chromium/src/+/2674530
    but a bit of code for using CertNetFetcherURLRequest was orphaned.
    
    Bug: 1111472
    Change-Id: Iefcb7219bd489bd79e3e11d1949bcea248136550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3927793
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053359}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/caching_cert_verifier.h"
 #include "net/cert/cert_verifier.h"
 #include "net/cert/coalescing_cert_verifier.h"
-#include "net/cert_net/cert_net_fetcher_url_request.h"
 #include "net/cookies/cookie_access_delegate.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/dns/host_cache.h"
@@ -627,9 +626,6 @@ NetworkContext::~NetworkContext() {
   // May be nullptr in tests.
   if (network_service_)
     network_service_->DeregisterNetworkContext(this);
-
-  if (cert_net_fetcher_)
-    cert_net_fetcher_->Shutdown();
 
   if (domain_reliability_monitor_)
     domain_reliability_monitor_->Shutdown();
@@ -2733,9 +2729,6 @@ URLRequestContextOwner NetworkContext::MakeURLRequestContext(
     proxy_delegate_->SetProxyResolutionService(
         result.url_request_context->proxy_resolution_service());
   }
-
-  if (cert_net_fetcher_)
-    cert_net_fetcher_->SetURLRequestContext(result.url_request_context.get());
 
   return result;
 }
