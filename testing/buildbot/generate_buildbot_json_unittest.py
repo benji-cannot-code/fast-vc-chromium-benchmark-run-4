2989bfc6a9568d681c630f06a5573d358d58590c
Brian Sheedy <bsheedy@chromium.org>
2022-09-30 23:32:17 +0000

    Move isolate name as Skylab test
    Moves 'isolate_name' to 'test' when generating GPU Skylab tests, as
    that is what Skylab bots use when determining which targets to build.
    
    Bug: 1363802
    Change-Id: I536db3372d97e81cf2ae650a395909da7fc7f7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930877
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Reviewed-by: Xinan Lin <linxinan@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053830}

--- testing/buildbot/generate_buildbot_json_unittest.py
+++ testing/buildbot/generate_buildbot_json_unittest.py
@@ -1893,7 +1893,6 @@ GPU_TELEMETRY_TEST_OUTPUT_SKYLAB = """\
           "-v",
           "--extra-browser-args=--log-level=0 --js-flags=--expose-gc"
         ],
-        "isolate_name": "telemetry_gpu_integration_test",
         "merge": {
           "args": [],
           "script": "//testing/merge_scripts/standard_isolated_script_merge.py"
@@ -1903,6 +1902,7 @@ GPU_TELEMETRY_TEST_OUTPUT_SKYLAB = """\
         "swarming": {
           "can_use_on_swarming_builders": false
         },
+        "test": "telemetry_gpu_integration_test",
         "test_id_prefix": "ninja://chrome/test:telemetry_gpu_integration_test/"
       }
     ]
