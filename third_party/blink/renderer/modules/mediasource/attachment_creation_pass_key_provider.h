fbb92dcbe2e442c1a987a1c7d668aca428d17826
Matt Wolenetz <wolenetz@chromium.org>
2022-10-01 01:00:48 +0000

    MSE-in-Workers: Relaunch with flag guards and handle visibility restricted
    This undoes the previous launch revert commit 887a8c4e3d5203b0528e7c911b24be3aa67d2b4e, and includes multiple fixes.
    
    Switches the two RuntimeEnabled features that enable MSE-in-Workers
    from experimental to stable status:
      MediaSourceInWorkers: Basic support, still using legacy object
        URLs for attachment
      MediaSourceInWorkersUsingHandle: Upgraded to match updated MSE spec, final PR linked below.
    
    Updates the stable webexposed expectations for both the Main/Window
    context and the DedicatedWorker context.
    
    MSE spec PRs with handle usage refinements:
      * https://github.com/w3c/media-source/pull/306
      * this relaunch also includes implementation of spec fix from
        https://github.com/w3c/media-source/pull/317
    
    Intent-to-ship=https://groups.google.com/a/chromium.org/g/blink-dev/c/FRY3F1v6Two
    
    Versus the original launch, this relaunch:
    * adds base::Feature (aka flag-guards) for each of the two
      RuntimeEnabledFeatures, above (with same feature name strings).
      This is to comply with mandatory flag-guarding PSA process update,
      and is meant to mitigate possible binary respins in the event this
      feature pair yet again needs to be disabled.
    * updates the MediaSource.handle attribute getter to fix the
      regression responsible for the previous revert, complying with
      MSE spec that was fixed to prevent that regression (see PR #317
      linked above):
      * Removes NotSupportedError exception throwing logic from the
        handle attribute getter.
      * Restricts visibility of the handle attribute to only dedicated
        worker contexts (removes visibility of it from the main/Window
        context versus previous launch attempt), along with corresponding
        webexposed stable web_tests expectations matching this change.
      * Updates mediasource-worker-handle.html web_test to no longer
        expect NotSupportedError exception, nor even ability to access the
        handle attribute of a MediaSource object on the main/window
        context.
    
    BUG=878133
    
    Change-Id: Id34a07254b9b98e79c495429f8ed79555b0c4580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910706
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053854}

--- third_party/blink/renderer/modules/mediasource/attachment_creation_pass_key_provider.h
+++ third_party/blink/renderer/modules/mediasource/attachment_creation_pass_key_provider.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ExceptionState;
 class MediaSource;
 class MediaSourceHandleImpl;
 class ScriptState;
@@ -28,7 +27,7 @@ class AttachmentCreationPassKeyProvider {
   // create a MediaSourceAttachmentSupplement.
   static PassKey GetPassKey() { return PassKey(); }
   friend String URLMediaSource::createObjectURL(ScriptState*, MediaSource*);
-  friend MediaSourceHandleImpl* MediaSource::handle(ExceptionState&);
+  friend MediaSourceHandleImpl* MediaSource::handle();
 };
 
 }  // namespace blink
--- third_party/blink/renderer/modules/mediasource/media_source.h
+++ third_party/blink/renderer/modules/mediasource/media_source.h
@@ -99,8 +99,7 @@ class MediaSource final : public EventTargetWithInlineData,
   void clearLiveSeekableRange(ExceptionState&)
       LOCKS_EXCLUDED(attachment_link_lock_);
 
-  MediaSourceHandleImpl* handle(ExceptionState&)
-      LOCKS_EXCLUDED(attachment_link_lock_);
+  MediaSourceHandleImpl* handle() LOCKS_EXCLUDED(attachment_link_lock_);
 
   static bool isTypeSupported(ExecutionContext* context, const String& type);
 
