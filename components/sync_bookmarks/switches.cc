5dcdbf7807cc1ff777cd06c1371e4148acdb4245
Daniel Cheng <dcheng@chromium.org>
2022-10-01 05:23:05 +0000

    Use helper macros to define base::Features in //components, part 3
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This is a manual cleanup pass to fix up features that were defined as
    inline constexpr in headers.
    
    Bug: 1364289
    Change-Id: I608ef537205904ced7690da77c975f6cce6445ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931274
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053898}

--- components/sync_bookmarks/switches.cc
+++ components/sync_bookmarks/switches.cc
@@ -1,21 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_SYNC_BOOKMARKS_SWITCHES_H_
-#define COMPONENTS_SYNC_BOOKMARKS_SWITCHES_H_
+#include "components/sync_bookmarks/switches.h"
 
 #include "base/feature_list.h"
 
 namespace switches {
 
-BASE_DECLARE_FEATURE(kSyncOmitLargeBookmarkFaviconUrl);
+BASE_FEATURE(kSyncOmitLargeBookmarkFaviconUrl,
+             "SyncOmitLargeBookmarkFaviconUrl",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // TODO(crbug.com/1232951): remove the feature toggle once most of bookmarks
 // have been reuploaded.
-BASE_DECLARE_FEATURE(kSyncReuploadBookmarks);
+BASE_FEATURE(kSyncReuploadBookmarks,
+             "SyncReuploadBookmarks",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace switches
-
-#endif  // COMPONENTS_SYNC_BOOKMARKS_SWITCHES_H_
