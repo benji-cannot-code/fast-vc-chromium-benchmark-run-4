41f65453321ce218732e57e9250c2636957c20fd
Daniel Cheng <dcheng@chromium.org>
2022-10-01 03:45:33 +0000

    Use helper macros to define base::Features in //extensions–//ui
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This is an automated cleanup pass to fix up new stragglers before
    enabling stricter enforcement. There are some additional CLs with manual
    cleanups in this range of directories.
    
    Bug: 1364289
    Change-Id: I0d100788924cbf865986bfed600d8e9001c1b72c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931407
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053887}

--- headless/lib/headless_content_main_delegate.cc
+++ headless/lib/headless_content_main_delegate.cc
@@ -65,8 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace headless {
 
 namespace features {
-const base::Feature kVirtualTime{"VirtualTime",
-                                 base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kVirtualTime, "VirtualTime", base::FEATURE_DISABLED_BY_DEFAULT);
 }
 
 const base::FilePath::CharType kDefaultProfileName[] =
