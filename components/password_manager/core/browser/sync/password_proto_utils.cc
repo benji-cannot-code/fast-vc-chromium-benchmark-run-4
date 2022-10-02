592e023e0db45b6e3e5475d28bc6fc40f151b296
Mohamed Amir Yosef <mamir@chromium.org>
2022-10-02 21:43:06 +0000

    [Passwords][Sync] Sync password notes only when notes feature enabled
    Before this CL:
    Chrome will unconditionally sync password notes to the sync server.
    When the notes features is disabled, the client doesn't have a reliable
    value of the note, and hence shouldn't send it to the server.
    
    After this CL:
    Chrome will only sync password notes to the sync server when the
    notes feature is enabled.
    
    This CL is also adding the proper testing that confirms that proto
    conversion works as expected with and without the notes feature.
    
    Since the code that unconditionally writes notes has been rolled out
    to public clients already, this CL is deprecating the proto fields
    that used to carry notes and notes backup and introduces new fields
    instead.
    
    Although the notes backup field is gated behind a feature toggle, it
    is decided to also deprecate it for future simplicity and ease
    of debugging.
    
    The currently implemented server workaround will *not* be effective
    until the server picks-up the proto changes.
    
    LOW_COVERAGE_REASON=predates this patch, and integration test will be added in a follow up.
    
    Bug: 1368988
    Change-Id: Ic3d320c277a71ff31279d38feb06aa0d6e2745aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925585
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054013}

--- components/password_manager/core/browser/sync/password_proto_utils.cc
+++ components/password_manager/core/browser/sync/password_proto_utils.cc
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/sync/password_proto_utils.h"
 
 #include "base/containers/flat_map.h"
+#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/password_manager/core/browser/password_form.h"
+#include "components/password_manager/core/common/password_manager_features.h"
+#include "components/sync/base/features.h"
 #include "components/sync/protocol/password_specifics.pb.h"
 
 using autofill::FormData;
@@ -257,8 +260,13 @@ sync_pb::PasswordSpecificsData SpecificsDataFromPassword(
           : password_form.federation_origin.Serialize());
   *password_data.mutable_password_issues() =
       PasswordIssuesMapToProto(password_form.password_issues);
-  *password_data.mutable_notes() =
-      PasswordNotesToProto(password_form.notes, base_password_data.notes());
+  // TODO(crbug.com/1369638): Force downloading all passwords from the sync
+  // server when the feature is enabled in order to avoid local copy missing
+  // notes overriding the server copy.
+  if (base::FeatureList::IsEnabled(syncer::kPasswordNotesWithBackup)) {
+    *password_data.mutable_notes() =
+        PasswordNotesToProto(password_form.notes, base_password_data.notes());
+  }
   return password_data;
 }
 
@@ -297,7 +305,9 @@ PasswordForm PasswordFromSpecifics(
   password.federation_origin =
       url::Origin::Create(GURL(password_data.federation_url()));
   password.password_issues = PasswordIssuesMapFromProto(password_data);
-  password.notes = PasswordNotesFromProto(password_data.notes());
+  if (base::FeatureList::IsEnabled(syncer::kPasswordNotesWithBackup)) {
+    password.notes = PasswordNotesFromProto(password_data.notes());
+  }
   return password;
 }
 
