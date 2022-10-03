210a9d4b05420c61052881ea1abfadb7d6971256
Tom Anderson <thomasanderson@chromium.org>
2022-10-03 23:25:54 +0000

    Remove unnecessary comment and build guard in xdg_util.h
    The comment is no longer applicable.  "nix" is not defined anywhere and
    "asdf" is not a helpful error message.
    
    R=thestig
    
    Bug: None
    Change-Id: Ide6681eeacaa4d7054804d877cf75ce82af1b1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925488
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054411}

--- base/nix/xdg_util.h
+++ base/nix/xdg_util.h
@@ -8,16 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // XDG refers to http://en.wikipedia.org/wiki/Freedesktop.org .
 // This file contains utilities found across free desktop environments.
-//
-// TODO(brettw) this file should be in app/x11, but is currently used by
-// net. We should have a net API to allow the embedder to specify the behavior
-// that it uses XDG for, and then move this file.
 
 #include "base/base_export.h"
-
-#ifdef nix
-#error asdf
-#endif
 
 namespace base {
 
