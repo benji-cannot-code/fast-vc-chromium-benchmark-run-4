62d78d21aac29f80a951ab48ae8dc026b0e21dd1
Benjamin Williams <bwwilliams@google.com>
2022-10-03 18:46:05 +0000

    Create feature kFullscreenPromosManagerSkipInternalLimits.
    This change creates a new feature flag which enables the Promos
    Manager's internal Impression Limits to be skipped.
    
    This is helpful for local development, when feature teams are creating a
    new promo to be managed by the Promos Manager, but want to avoid getting
    blocked/restricted by the Promos Manager's internal Impression Limits.
    
    Change-Id: I3591f8cb944435d76c8e6152c7a4d7bde73a8b0a
    Bug: 1370105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931125
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054243}

--- ios/chrome/browser/promos_manager/features.h
+++ ios/chrome/browser/promos_manager/features.h
@@ -9,11 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/feature_list.h"
 
 // Feature flag to enable the Fullscreen Promos Manager.
-// For more information, please see here:
-// go/bling-fullscreen-promos-manager-design-doc.
 BASE_DECLARE_FEATURE(kFullscreenPromosManager);
+
+// Feature flag to enable skipping evaluation of the Fullscreen Promos Manager's
+// internal Impression Limits.
+BASE_DECLARE_FEATURE(kFullscreenPromosManagerSkipInternalLimits);
 
 // Returns true if the Fullscreen Promos Manager is enabled.
 bool IsFullscreenPromosManagerEnabled();
+
+// Returns true if the Fullscreen Promos Manager should skip evaluation of its
+// internal Impression Limits when deciding whether or not to display a promo.
+bool IsSkippingInternalImpressionLimitsEnabled();
 
 #endif  // IOS_CHROME_BROWSER_PROMOS_MANAGER_FEATURES_H_
