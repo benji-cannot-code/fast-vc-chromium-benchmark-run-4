f53d2aa59833f9e683e0e80f2b638574fe831226
Anastasia Helfinstein <anastasi@google.com>
2022-10-03 21:01:20 +0000

    [ChromeVox] Remove unused code from injected/
    Cleanup unused code from previous versions of ChromeVox.
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: Ica418edf86fb9c4fc25856f579d77eada50e1768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928461
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054326}

--- chrome/browser/resources/chromeos/accessibility/chromevox/injected/loader.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/injected/loader.js
@@ -5,11 +5,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 goog.require('ApiImplementation');
 
-/**
- * Initializes minimal content script.
- */
-function initMin() {
-  ApiImplementation.init();
-}
-
-initMin();
+ApiImplementation.init();
