44d74defb59bdcd2589ed6c22d9b9ccc88d5d151
Nick Diego Yamane <nickdiego@igalia.com>
2022-10-03 16:48:01 +0000

    wayland: fix frame decoration when in tiled/ssd mode
    SwayWM implements a tiling window manager and, by default uses
    server-side decorations to draw, for example, simple pixel borders
    around the toplevel windows. In such case, Chrome shouldn't include
    decoration insets (with drop shadows, etc) when 'use system title bar'
    setting setting is off, which would mean client-side decoration mode.
    That's because Sway enfores ssd, even if clients request to use csd,
    and that's spec-compliant [1], as per:
    
    > The compositor can decide not to use the client's mode and enforce a
    > different mode instead.
    
    When xdg-shell reports tiled modes ozone/wayland already reports it up
    to its delegate, which in case of Chrome already sets the decoration
    inset to (0,0,0,0), though the was still a missing bit in order to get
    it correctly rendered, that's what this CL adds as well as unit tests to
    exercise such code path.
    
    R=adunaev@igalia.com
    
    [1] https://wayland.app/protocols/xdg-decoration-unstable-v1#zxdg_toplevel_decoration_v1:request:set_mode
    
    Bug: 1369621, 1294191
    Test: ozone_unittests and manually with Chrome under SwayWM
    Change-Id: I164fead8df936a075db40ea251fd2384d997d19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924221
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1054200}

--- ui/ozone/platform/wayland/host/xdg_toplevel_wrapper_impl.cc
+++ ui/ozone/platform/wayland/host/xdg_toplevel_wrapper_impl.cc
@@ -89,8 +89,7 @@ XDGToplevelWrapperImpl::XDGToplevelWrapperImpl(
     WaylandConnection* connection)
     : xdg_surface_wrapper_(std::move(surface)),
       wayland_window_(wayland_window),
-      connection_(connection),
-      decoration_mode_(DecorationMode::kNone) {}
+      connection_(connection) {}
 
 XDGToplevelWrapperImpl::~XDGToplevelWrapperImpl() = default;
 
@@ -357,7 +356,7 @@ void XDGToplevelWrapperImpl::WmCapabilities(void* data,
 
 void XDGToplevelWrapperImpl::SetTopLevelDecorationMode(
     DecorationMode requested_mode) {
-  if (!zxdg_toplevel_decoration_ || requested_mode == decoration_mode_)
+  if (!zxdg_toplevel_decoration_ || requested_mode == decoration_mode())
     return;
 
   zxdg_toplevel_decoration_v1_set_mode(zxdg_toplevel_decoration_.get(),
@@ -371,7 +370,7 @@ void XDGToplevelWrapperImpl::ConfigureDecoration(
     uint32_t mode) {
   auto* surface = static_cast<XDGToplevelWrapperImpl*>(data);
   DCHECK(surface);
-  surface->decoration_mode_ = ToDecorationMode(mode);
+  surface->set_decoration_mode(ToDecorationMode(mode));
 }
 
 void XDGToplevelWrapperImpl::InitializeXdgDecoration() {
