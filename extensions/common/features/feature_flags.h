423db4a53290ebe0ea0e40225aa47721b513fd78
Daniel Cheng <dcheng@chromium.org>
2022-10-03 21:29:33 +0000

    Fix extension SimpleFeature tests to use base::Feature macros.
    Rework the test overrides to not require copying base::Features.
    
    Bug: 1364289
    Change-Id: Ib40dbe46aeb61c8ed684d0d9966e461914567af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931513
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054340}

--- extensions/common/features/feature_flags.h
+++ extensions/common/features/feature_flags.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_COMMON_FEATURES_FEATURE_FLAGS_H_
 
 #include <string>
-#include <vector>
 
 #include "base/auto_reset.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 
 namespace extensions {
@@ -19,13 +19,14 @@ namespace extensions {
 // same name.
 bool IsFeatureFlagEnabled(const std::string& feature_flag);
 
-// Used to override the set of base::Feature flags while the returned value is
-// in scope. Clients must ensure that |features| remains alive (non-dangling)
-// while the returned value is in scope.
+// Used to override the set of base::Feature flags while the returned scoper is
+// alive. Clients must ensure that pointers in |features| remain valid
+// (non-dangling) while the returned scoper is alive (note that features are
+// generally global variables, so this should always be trivially true...).
 using ScopedFeatureFlagsOverride =
-    base::AutoReset<const std::vector<base::Feature>*>;
+    base::AutoReset<base::span<const base::Feature*>>;
 ScopedFeatureFlagsOverride CreateScopedFeatureFlagsOverrideForTesting(
-    const std::vector<base::Feature>* features);
+    base::span<const base::Feature*> features);
 
 }  // namespace extensions
 
