d4e5ca3a3a17ce592bda2652cef290fad771601f
Daniel Cheng <dcheng@chromium.org>
2022-10-03 00:13:38 +0000

    Fix a few more base::Feature macro stragglers.
    These instances were not picked up by the previous passes, due to being
    new introductions or not matching the regexes (typically, because the
    feature name was an identifier, not a string literal).
    
    Bug: 1364289
    Change-Id: I95276a886f075c539ef9769a8ca7901807cea6b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931275
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054020}

--- components/reporting/compression/compression_module.cc
+++ components/reporting/compression/compression_module.cc
@@ -21,13 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace reporting {
 
-const base::Feature kCompressReportingPipeline{
-    CompressionModule::kCompressReportingFeature,
-    base::FEATURE_ENABLED_BY_DEFAULT};
-
-// static
-const char CompressionModule::kCompressReportingFeature[] =
-    "CompressReportingPipeline";
+BASE_FEATURE(kCompressReportingPipeline,
+             "CompressReportingPipeline",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
 
