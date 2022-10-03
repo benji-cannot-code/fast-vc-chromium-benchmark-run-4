04bcbecb85172ce1b1a56883d283d0950485d2e3
Thomas Lukaszewicz <tluk@chromium.org>
2022-10-03 18:44:59 +0000

    [ash] Prevent startup URLs from being restored for incognito windows
    Currently on ash we call ash::ProfileHelper::IsRegularProfile to
    ensure we only restore for regular profiles when restoring URLs.
    
    However this only checks the profile is not a lockscreen or sign-in
    profile, it does not check that the profile is of type kRegular.
    CanRestoreUrlsForProfile() has now been updated to check and ensure
    we correctly test the profile type type.
    
    A follow up should address the implementation of
    ash::ProfileHelper::IsRegularProfile or update the naming to avoid
    confusion.
    
    Bug: 1370160
    Change-Id: Ia1fd08d3fadb54802a90e5f6829b6e6b3ec013ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931226
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054241}

--- chrome/test/base/browser_process_platform_part_test_api_chromeos.h
+++ chrome/test/base/browser_process_platform_part_test_api_chromeos.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/cros_component_manager.h"
 
 class BrowserProcessPlatformPart;
+class Profile;
 
 // Used to override parts of BrowserProcessPlatformParts in tests.
 class BrowserProcessPlatformPartTestApi {
@@ -30,6 +31,8 @@ class BrowserProcessPlatformPartTestApi {
   // Shuts down the cros component manager set by
   // InitializeCrosComponentManager().
   void ShutdownCrosComponentManager();
+
+  bool CanRestoreUrlsForProfile(const Profile* profile);
 
  private:
   BrowserProcessPlatformPart* const platform_part_;
