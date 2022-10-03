7261a5684ec62d72f3ec33992ba75e709a6417fa
Yoshiki Tanioka <tanioka@google.com>
2022-10-03 05:06:19 +0000

    Prerender: Precog API triggering outcome test on sequential prerendering
    This CL adds test to make sure the pending prerender host has
    PreloadingTriggeringOutcome::kUnspecified.
    
    In the current implementation, prerender requests are triggered right
    after the PrerenderHostRegistry receives the requests. But if the
    sequential prerendering feature is enabled, some prerender requests can
    be pending.
    The test in this CL makes sure that those pending prerender hosts have
    PreloadingTriggeringOutcome::kUnspecified as the post-triggering
    outcome.
    
    Bug: 1355151
    Change-Id: I63214900244fb40b39cd2f9be8b1b51b21887eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3915642
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Yoshiki Tanioka <tanioka@google.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054051}

--- content/browser/preloading/prerender/prerender_host.h
+++ content/browser/preloading/prerender/prerender_host.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/stored_page.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
+#include "content/public/browser/preloading_data.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -273,6 +274,8 @@ class CONTENT_EXPORT PrerenderHost : public WebContentsObserver {
   const std::string& embedder_histogram_suffix() const {
     return attributes_.embedder_histogram_suffix;
   }
+
+  base::WeakPtr<PreloadingAttempt> preloading_attempt() { return attempt_; }
 
  private:
   // Records the status to UMA and UKM. `initiator_ukm_id` represents the page
