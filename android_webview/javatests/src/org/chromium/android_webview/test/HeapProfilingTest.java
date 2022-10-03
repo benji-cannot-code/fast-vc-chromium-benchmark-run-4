09fccf3cc4d797edbbaaa19b4de7e8b3d4597376
Xi Han <hanxi@google.com>
2022-10-03 21:30:31 +0000

    [Sheriff] Disable two HeapPoriflingTest.
    Disable:
    - org.chromium.android_webview.test.HeapProfilingTest#testModeBrowserDynamicPseudoSamplePartial__multiprocess_mode and
    - org.chromium.android_webview.test.HeapProfilingTest#testModeBrowserDynamicPseudo__multiprocess_mode
    
    Bug: 1370781
    Change-Id: I2e458ee9b01f4724eb6c9d51df54d32dfd22d36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933127
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Owners-Override: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054341}

--- android_webview/javatests/src/org/chromium/android_webview/test/HeapProfilingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/HeapProfilingTest.java
@@ -45,6 +45,7 @@ public class HeapProfilingTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1370781")
     public void testModeBrowserDynamicPseudo() {
         HeapProfilingTestShim shim = new HeapProfilingTestShim();
         Assert.assertTrue(shim.runTestForMode("browser", true, "pseudo", false, false));
@@ -60,6 +61,7 @@ public class HeapProfilingTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1370781")
     public void testModeBrowserDynamicPseudoSamplePartial() {
         HeapProfilingTestShim shim = new HeapProfilingTestShim();
         Assert.assertTrue(shim.runTestForMode("browser", true, "pseudo", true, false));
