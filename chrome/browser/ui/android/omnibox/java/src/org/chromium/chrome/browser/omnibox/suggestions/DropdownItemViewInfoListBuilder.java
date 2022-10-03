b64f1f7ff02009ae7ba0d33409f3ee200f8e1773
Ender <ender@google.com>
2022-10-03 21:31:31 +0000

    Adaptive suggestions: consistent keyboard handling
    The change removes RecyclerViews scroll listener, replacing it
    entirely with LayoutManager as a dedicated scroll listener.
    
    The updated scroll listener avoids re-emitting keyboard show or hide
    events, remembering the previously emitted state until the list is
    dismissed and opened again.
    
    Bug: 1370178
    Change-Id: If5aead5da6f73f5db040e1efc9c66805a27f5bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931191
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054342}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
@@ -253,12 +253,10 @@ class DropdownItemViewInfoListBuilder {
                 autocompleteResult.groupSuggestionsBySearchVsURL(
                         1, Math.min(numVisibleSuggestions, firstSuggestionWithHeader));
             }
+            mBuiltListHasFullyConcealedElements = (numVisibleSuggestions < suggestionsCount);
             if (numVisibleSuggestions < firstSuggestionWithHeader) {
-                mBuiltListHasFullyConcealedElements = true;
                 autocompleteResult.groupSuggestionsBySearchVsURL(
                         numVisibleSuggestions, firstSuggestionWithHeader);
-            } else {
-                mBuiltListHasFullyConcealedElements = false;
             }
         }
 
