e12d532036a0b93608b40260a4924f76b5bd8d7b
Ella Ge <eirage@chromium.org>
2022-10-04 03:46:24 +0000

    Remove histogram TrustedWebActivity.LocationUpdateErrorCode
    This is rarely used. The no error one is >99.9%, all other three error types have less than 0.1% records in total.
    
    Bug: 1362630
    Change-Id: I9174a3405fba01776ab23d3f180f1ca1ffb5e02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917352
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054509}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClient.java
@@ -38,7 +38,6 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeApplicationImpl;
 import org.chromium.chrome.browser.browserservices.TrustedWebActivityClientWrappers.Connection;
 import org.chromium.chrome.browser.browserservices.TrustedWebActivityClientWrappers.ConnectionPool;
-import org.chromium.chrome.browser.browserservices.constants.LocationUpdateError;
 import org.chromium.chrome.browser.browserservices.metrics.TrustedWebActivityUmaRecorder;
 import org.chromium.chrome.browser.browserservices.metrics.TrustedWebActivityUmaRecorder.DelegatedNotificationSmallIconFallback;
 import org.chromium.chrome.browser.browserservices.permissiondelegation.InstalledWebappPermissionManager;
@@ -499,6 +498,5 @@ public class TrustedWebActivityClient {
         Bundle error = new Bundle();
         error.putString("message", message);
         callback.onExtraCallback(EXTRA_NEW_LOCATION_ERROR_CALLBACK, error);
-        mRecorder.recordLocationUpdateError(LocationUpdateError.NO_TWA);
     }
 }
