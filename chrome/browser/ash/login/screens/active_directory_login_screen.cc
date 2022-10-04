e5b3e78775faf4db6203f202b5ac7492a3bc0cb4
Roman Sorokin <rsorokin@chromium.org>
2022-10-04 12:38:18 +0000

    [known_user] Remove deprecated known_user::GetAccountId
    Migrate to their KnownUser variant in
    /chrome/browser/ash/login/screens
    
    This CL was uploaded by git cl split.
    
    R=antrim@chromium.org
    
    Bug: 1150434
    Change-Id: I655eb7f11a728e756444fe9c720c969394b452d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930585
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054643}

--- chrome/browser/ash/login/screens/active_directory_login_screen.cc
+++ chrome/browser/ash/login/screens/active_directory_login_screen.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/ui/login_display_host.h"
 #include "chrome/browser/ash/login/ui/signin_ui.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/webui/chromeos/login/active_directory_login_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/error_screen_handler.h"
@@ -139,7 +140,8 @@ void ActiveDirectoryLoginScreen::OnAdAuthResult(
       DCHECK(account_info.has_account_id() &&
              !account_info.account_id().empty() &&
              LoginDisplayHost::default_host());
-      const AccountId account_id(user_manager::known_user::GetAccountId(
+      user_manager::KnownUser known_user(g_browser_process->local_state());
+      const AccountId account_id(known_user.GetAccountId(
           username, account_info.account_id(), AccountType::ACTIVE_DIRECTORY));
       LoginDisplayHost::default_host()->SetDisplayAndGivenName(
           account_info.display_name(), account_info.given_name());
--- chrome/browser/ash/login/screens/active_directory_password_change_screen.cc
+++ chrome/browser/ash/login/screens/active_directory_password_change_screen.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/ui/login_display_host.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/webui/chromeos/login/active_directory_password_change_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
 #include "chrome/grit/generated_resources.h"
@@ -115,7 +116,8 @@ void ActiveDirectoryPasswordChangeScreen::OnAuthFinished(
     case authpolicy::ERROR_NONE: {
       DCHECK(account_info.has_account_id() &&
              !account_info.account_id().empty());
-      const AccountId account_id = user_manager::known_user::GetAccountId(
+      user_manager::KnownUser known_user(g_browser_process->local_state());
+      const AccountId account_id = known_user.GetAccountId(
           username, account_info.account_id(), AccountType::ACTIVE_DIRECTORY);
       DCHECK(LoginDisplayHost::default_host());
       LoginDisplayHost::default_host()->SetDisplayAndGivenName(
