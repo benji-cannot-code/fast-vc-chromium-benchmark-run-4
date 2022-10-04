123e38c9f379eae3758c600883b1e2312ff0ccd0
Aliona DANGLA <alionadangla@google.com>
2022-10-04 16:58:50 +0000

    [iOS] Add variation in open in flags
    As the Open In experiment will also include UI changes, this CL add a
    new variation to the existing flag.
    
    Bug: 1358007
    Change-Id: Id4ebe15b25fe085e243d22fbe900e6f0a8c39a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928770
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054793}

--- ios/chrome/browser/ui/open_in/features.h
+++ ios/chrome/browser/ui/open_in/features.h
@@ -12,18 +12,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BASE_DECLARE_FEATURE(kEnableOpenInDownload);
 
 // Indicates which Open In download variant to use.
+extern const char kOpenInDownloadInShareButtonParam[];
 extern const char kOpenInDownloadWithWKDownloadParam[];
 extern const char kOpenInDownloadWithV2Param[];
 
-// Convenience method for determining when Open In download with WKDownload is
-// enabled.
+// Convenience method for determining when Open In with legacy download in share
+// button is enabled.
+bool IsOpenInDownloadInShareButton();
+
+// Convenience method for determining when Open In download with WKDownload in
+// share button is enabled.
 bool IsOpenInDownloadWithWKDownload();
 
-// Convenience method for determining when Open In download with V2 is enabled.
+// Convenience method for determining when Open In download with V2 in share
+// button is enabled.
 bool IsOpenInDownloadWithV2();
 
-// Convenience method for determining when Open In download experiment is
+// Convenience method for determining when new download experiment is
 // enabled.
-bool IsOpenInDownloadEnabled();
+bool IsOpenInNewDownloadEnabled();
+
+// Convenience method for determining if Open In activities are moved in the
+// share button.
+bool IsOpenInActivitiesInShareButtonEnabled();
 
 #endif  // IOS_CHROME_BROWSER_UI_OPEN_IN_FEATURES_H_
