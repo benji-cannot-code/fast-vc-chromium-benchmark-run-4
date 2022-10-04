c9e5ff7dbe3e8af4c34515c9f5c3100f4b816001
Daniel Cheng <dcheng@chromium.org>
2022-10-04 08:04:31 +0000

    Use base::test::FeatureRef for feature vectors in //third_party.
    A base::Feature should not be copyable; a feature is intended to have a
    single global instance, as it caches internal mutable state.
    
    Feature vectors in conjunction with `base::test::ScopedFeatureList` are
    by far the most common use of the base::Feature copy constructor. This
    is a mostly automated replacement of `std::vector<base::Feature>` with
    `std::vector<base::test::FeatureRef>` to remove that dependency.
    
    This CL was uploaded by git cl split.
    
    Bug: 1370572
    Change-Id: I8a8fa892115f55271295d6c69844ed0c5dc449af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934114
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054569}

--- third_party/blink/renderer/core/content_capture/content_capture_test.cc
+++ third_party/blink/renderer/core/content_capture/content_capture_test.cc
@@ -153,14 +153,15 @@ class ContentCaptureLocalFrameClientHelper : public EmptyLocalFrameClient {
   WebContentCaptureClient& client_;
 };
 
-class ContentCaptureTest
-    : public PageTestBase,
-      public ::testing::WithParamInterface<std::vector<base::Feature>> {
+class ContentCaptureTest : public PageTestBase,
+                           public ::testing::WithParamInterface<
+                               std::vector<base::test::FeatureRef>> {
  public:
   ContentCaptureTest() {
     EnablePlatform();
     feature_list_.InitWithFeatures(
-        GetParam(), /*disabled_features=*/std::vector<base::Feature>());
+        GetParam(),
+        /*disabled_features=*/std::vector<base::test::FeatureRef>());
   }
 
   void SetUp() override {
@@ -367,8 +368,8 @@ class ContentCaptureTest
 INSTANTIATE_TEST_SUITE_P(
     ,
     ContentCaptureTest,
-    testing::Values(std::vector<base::Feature>{},
-                    std::vector<base::Feature>{
+    testing::Values(std::vector<base::test::FeatureRef>{},
+                    std::vector<base::test::FeatureRef>{
                         features::kContentCaptureConstantStreaming}));
 
 TEST_P(ContentCaptureTest, Basic) {
