fac7fca3ae2fcecea14b99bb5ea71339dd8ea824
Juanmihd <juanmihd@google.com>
2022-10-04 11:01:25 +0000

    Adding color conversion from Lab to LCH
    This CL is a part of a series of CL of color conversions needd for
    CSS Color 5 implementation of color-mix.
    
    This CL adds the implementation and tests of Lab to LCH conversion.
    
    This CL also fixes the LCH to Lab conversion, to avoid using angle
    conversions that already are implemented in gfx. It also fixes some
    typos in the variable names in the LCH to Lab test.
    
    Bug: 1092638
    Change-Id: I706d9aafc26c63e7952355175476c4e463ec947d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933392
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054631}

--- ui/gfx/color_conversions.cc
+++ ui/gfx/color_conversions.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/modules/skcms/skcms.h"
 #include "ui/gfx/color_space.h"
+#include "ui/gfx/geometry/angle_conversions.h"
 
 namespace gfx {
 
@@ -127,8 +128,12 @@ std::tuple<float, float, float> LchToLab(float l,
   if (!h.has_value())
     return std::make_tuple(l, 0, 0);
 
-  return std::make_tuple(l, c * std::cos(h.value() * 3.141592f / 180.0f),
-                         c * std::sin(h.value() * 3.141592f / 180.0f));
+  return std::make_tuple(l, c * std::cos(gfx::DegToRad(h.value())),
+                         c * std::sin(gfx::DegToRad(h.value())));
+}
+std::tuple<float, float, float> LabToLCH(float l, float a, float b) {
+  return std::make_tuple(l, std::sqrt(a * a + b * b),
+                         gfx::RadToDeg(std::atan2f(b, a)));
 }
 
 std::tuple<float, float, float> LabToXYZD50(float l, float a, float b) {
