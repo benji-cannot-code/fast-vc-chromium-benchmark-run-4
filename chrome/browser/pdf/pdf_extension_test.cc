4c6e39592774c11555588b0b003455f169813bf3
Daniel Cheng <dcheng@chromium.org>
2022-10-04 07:59:30 +0000

    Use base::test::FeatureRef for feature vectors in //chrome/browser/pdf.
    A base::Feature should not be copyable; a feature is intended to have a
    single global instance, as it caches internal mutable state.
    
    Feature vectors in conjunction with `base::test::ScopedFeatureList` are
    by far the most common use of the base::Feature copy constructor. This
    is a mostly automated replacement of `std::vector<base::Feature>` with
    `std::vector<base::test::FeatureRef>` to remove that dependency.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1370572
    Change-Id: I181b8d4565ed59e86ac6350fb275384317b7e5e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932881
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054567}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -389,9 +389,13 @@ class PDFExtensionTest : public extensions::ExtensionApiTest {
   }
 
   // Hooks to set up feature flags.
-  virtual std::vector<base::Feature> GetEnabledFeatures() const { return {}; }
+  virtual std::vector<base::test::FeatureRef> GetEnabledFeatures() const {
+    return {};
+  }
 
-  virtual std::vector<base::Feature> GetDisabledFeatures() const { return {}; }
+  virtual std::vector<base::test::FeatureRef> GetDisabledFeatures() const {
+    return {};
+  }
 
  private:
   base::test::ScopedFeatureList feature_list_;
@@ -400,7 +404,7 @@ class PDFExtensionTest : public extensions::ExtensionApiTest {
 
 class PDFExtensionTestWithPartialLoading : public PDFExtensionTest {
  protected:
-  std::vector<base::Feature> GetEnabledFeatures() const override {
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     auto enabled = PDFExtensionTest::GetEnabledFeatures();
     enabled.push_back(chrome_pdf::features::kPdfIncrementalLoading);
     enabled.push_back(chrome_pdf::features::kPdfPartialLoading);
@@ -1361,7 +1365,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionContentSettingJSTest, DISABLED_NoBeepCsp) {
 
 class PDFExtensionWebUICodeCacheJSTest : public PDFExtensionJSTest {
  protected:
-  std::vector<base::Feature> GetEnabledFeatures() const override {
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     auto enabled = PDFExtensionJSTest::GetEnabledFeatures();
     enabled.push_back(features::kWebUICodeCache);
     return enabled;
@@ -2108,7 +2112,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTest, PrintButton) {
 
 class PDFExtensionRegionSearchTest : public PDFExtensionTest {
  protected:
-  std::vector<base::Feature> GetEnabledFeatures() const override {
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     auto enabled = PDFExtensionTest::GetEnabledFeatures();
     enabled.push_back(lens::features::kLensStandalone);
     return enabled;
@@ -3930,7 +3934,7 @@ class PDFExtensionAccessibilityTextExtractionTest : public PDFExtensionTest {
   }
 
  protected:
-  std::vector<base::Feature> GetEnabledFeatures() const override {
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     auto enabled = PDFExtensionTest::GetEnabledFeatures();
     enabled.push_back(chrome_pdf::features::kAccessiblePDFForm);
     return enabled;
@@ -4143,7 +4147,7 @@ class PDFExtensionAccessibilityTreeDumpTest
   }
 
  protected:
-  std::vector<base::Feature> GetEnabledFeatures() const override {
+  std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     auto enabled = PDFExtensionTest::GetEnabledFeatures();
     enabled.push_back(chrome_pdf::features::kAccessiblePDFForm);
     return enabled;
