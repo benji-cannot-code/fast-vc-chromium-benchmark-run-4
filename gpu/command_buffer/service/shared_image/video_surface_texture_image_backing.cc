de98d35e70586ebbe49baaf997f048d8ed08c9fa
Colin Blundell <blundell@chromium.org>
2022-10-04 13:23:17 +0000

    [VideoSurfaceTextureImageBacking] Pass null GLImage when binding texture
    We are eliminating StreamTextureSharedImageInterface needing to subclass
    GLImage. The only blocker is that VideoSurfaceTextureImageBacking passes
    in StreamTextureSII as a GLImage instance to
    AbstractTexture::BindStreamTextureImage(). We are highly confident that
    the only side-effect of passing this instance (as opposed to null) is
    that the GLES2 command decoders then perform lazy binding on the
    StreamTextureSII, and we have determined that this lazy binding is
    unnecessary (see linked bug).
    
    Hence, we should be able to pass nullptr to BindStreamTextureImage()
    [note: the method itself still needs to be called for correctness, and
    it is valid to pass nullptr]. We can then eliminate StreamTextureSII
    being a GLImage subclass. However, the codepaths are subtle enough that
    it is challenging to be absolutely sure that there will be no unintended
    side-effects from passing null here. We especially want to be cautious
    as this code is executed on Android WebView. This CL thus implements
    passing null with an enabled-by-default base::Feature that can serve as
    a killswitch.
    
    Bug: 1310020
    Change-Id: I22669b76c37d7ed8d3829d6433bf7289c5a3433d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933003
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054677}

--- gpu/command_buffer/service/shared_image/video_surface_texture_image_backing.cc
+++ gpu/command_buffer/service/shared_image/video_surface_texture_image_backing.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/resources/resource_sizes.h"
@@ -25,6 +26,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_utils.h"
 
 namespace gpu {
+
+namespace {
+
+// If enabled, then nullptr is passed for the GLImage instance when invoking
+// BindStreamTextureImage(). Rolling this change out is the last blocker to
+// eliminating StreamTextureSharedImageInterface being a subclass of GLImage.
+// TODO(crbug.com/1310020): Remove this flag once the change has rolled out
+// safely.
+BASE_FEATURE(kPassNullForGLImageWhenBindingTexture,
+             "kPassNullForGLImageWhenBindingTexture",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// Returns either |nullptr| or |sii|.
+gl::GLImage* GetGLImageToUseWhenBindingTexture(
+    StreamTextureSharedImageInterface* sii) {
+  return (base::FeatureList::IsEnabled(kPassNullForGLImageWhenBindingTexture))
+             ? nullptr
+             : sii;
+}
+
+}  // namespace
 
 VideoSurfaceTextureImageBacking::VideoSurfaceTextureImageBacking(
     const Mailbox& mailbox,
@@ -194,7 +216,7 @@ VideoSurfaceTextureImageBacking::ProduceGLTexture(SharedImageManager* manager,
   // texture_id in abstract texture via BindStreamTextureImage().
   DCHECK(stream_texture_sii_->TextureOwnerBindsTextureOnUpdate());
   texture->BindStreamTextureImage(
-      stream_texture_sii_.get(),
+      GetGLImageToUseWhenBindingTexture(stream_texture_sii_.get()),
       stream_texture_sii_->GetTextureBase()->service_id());
 
   return std::make_unique<GLTextureVideoImageRepresentation>(
@@ -223,7 +245,7 @@ VideoSurfaceTextureImageBacking::ProduceGLTexturePassthrough(
   // texture_id in abstract texture via BindStreamTextureImage().
   DCHECK(stream_texture_sii_->TextureOwnerBindsTextureOnUpdate());
   texture->BindStreamTextureImage(
-      stream_texture_sii_.get(),
+      GetGLImageToUseWhenBindingTexture(stream_texture_sii_.get()),
       stream_texture_sii_->GetTextureBase()->service_id());
 
   return std::make_unique<GLTexturePassthroughVideoImageRepresentation>(
@@ -264,7 +286,7 @@ VideoSurfaceTextureImageBacking::ProduceSkia(
   // texture_id in abstract texture via BindStreamTextureImage().
   DCHECK(stream_texture_sii_->TextureOwnerBindsTextureOnUpdate());
   texture->BindStreamTextureImage(
-      stream_texture_sii_.get(),
+      GetGLImageToUseWhenBindingTexture(stream_texture_sii_.get()),
       stream_texture_sii_->GetTextureBase()->service_id());
 
   std::unique_ptr<gpu::GLTextureImageRepresentationBase> gl_representation;
