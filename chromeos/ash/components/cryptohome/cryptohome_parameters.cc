389ed6c6dcdab4ffa28b9cf948300cd8a499f43e
Roman Sorokin <rsorokin@chromium.org>
2022-10-04 22:29:31 +0000

    [known_user] Remove deprecated known_user::GetAccountId
    Migrate to their KnownUser variant in
    /components/user_manager
    
    This CL was uploaded by git cl split.
    
    Bug: 1150434
    Change-Id: I0cd7e64f1ea6646c84b9a4eaee3f41e5e062f96b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930206
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054978}

--- chromeos/ash/components/cryptohome/cryptohome_parameters.cc
+++ chromeos/ash/components/cryptohome/cryptohome_parameters.cc
@@ -42,6 +42,9 @@ const std::string GetCryptohomeId(const AccountId& account_id) {
 }
 
 AccountId LookupUserByCryptohomeId(const std::string& cryptohome_id) {
+  if (cryptohome_id.empty())
+    return EmptyAccountId();
+
   const std::vector<AccountId> known_account_ids =
       user_manager::known_user::GetKnownAccountIds();
 
@@ -60,8 +63,7 @@ AccountId LookupUserByCryptohomeId(const std::string& cryptohome_id) {
     }
   }
 
-  return user_manager::known_user::GetAccountId(
-      cryptohome_id, std::string() /* id */, AccountType::UNKNOWN);
+  return user_manager::known_user::GetPlatformKnownAccountId(cryptohome_id);
 }
 
 }  //  anonymous namespace
