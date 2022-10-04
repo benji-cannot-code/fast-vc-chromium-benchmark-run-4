c58ac4932d78b781127a707f391b15e610411f04
Martin Bidlingmaier <mbid@google.com>
2022-10-04 01:46:27 +0000

    Move state from userdataauth testapi to fake client itself
    There was a rather convoluted circular interdependency there, where the
    test api would manipulate state in the fake client, and the fake client
    would read data in the test api object. Now all the state that was
    previously in the test api object lives in the fake client, which
    simplifies things. The test api itself is an empty struct.
    
    The commit also exposes the test api methods via CryptohomeMixin by
    inheriting from it.
    
    Bug: b:239430274
    Change-Id: I34156965ea8a467d66d5121af5326379938e2e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916216
    Auto-Submit: Martin Bidlingmaier <mbid@google.com>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054438}

--- chrome/browser/ash/login/test/cryptohome_mixin.h
+++ chrome/browser/ash/login/test/cryptohome_mixin.h
@@ -11,13 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chromeos/ash/components/dbus/cryptohome/rpc.pb.h"
+#include "chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h"
 #include "components/account_id/account_id.h"
 
 namespace ash {
 
 // Mixin that acts as a broker between tests
 // and FakeUserDataAuthClient, handling all interactions and transformations
-class CryptohomeMixin : public InProcessBrowserTestMixin {
+class CryptohomeMixin : public InProcessBrowserTestMixin,
+                        public ash::FakeUserDataAuthClient::TestApi {
  public:
   explicit CryptohomeMixin(InProcessBrowserTestMixinHost* host);
   CryptohomeMixin(const CryptohomeMixin&) = delete;
