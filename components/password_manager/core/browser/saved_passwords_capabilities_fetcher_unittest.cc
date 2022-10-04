e9c8cee1aae49918ee5023ae390c223d8bef1c17
Jan Keitel <jkeitel@google.com>
2022-10-04 12:39:24 +0000

    Make SavedPasswordsCapabilitiesFetcher compatible with account stores.
    This CL changes SavedPasswordsCapabilitiesFetcher to take a
    SavedPasswordsPresenter instead of a (profile) password store
    and passes both the account and the profile store in the
    factory method.
    
    This is in preparation for enabling APC on Desktop for account store
    users on Android.
    
    Bug: 1368639
    Change-Id: I18e8578a82f9f11101063c1aac5e84f627d72428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928272
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054645}

--- components/password_manager/core/browser/saved_passwords_capabilities_fetcher_unittest.cc
+++ components/password_manager/core/browser/saved_passwords_capabilities_fetcher_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/capabilities_service.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/test_password_store.h"
+#include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -112,12 +113,14 @@ class SavedPasswordsCapabilitiesFetcherTest : public ::testing::Test {
  public:
   SavedPasswordsCapabilitiesFetcherTest() {
     store_->Init(/*prefs=*/nullptr, /*affiliated_match_helper=*/nullptr);
+    FillPasswordStore();
     auto capabilities_service =
         std::make_unique<NiceMock<MockCapabilitiesService>>();
     mock_capabilities_service_ = capabilities_service.get();
     fetcher_ = std::make_unique<SavedPasswordsCapabilitiesFetcher>(
-        std::move(capabilities_service), store_);
-    FillPasswordStore();
+        std::move(capabilities_service),
+        std::make_unique<SavedPasswordsPresenter>(store_));
+    RunUntilIdle();
   }
 
   ~SavedPasswordsCapabilitiesFetcherTest() override {
