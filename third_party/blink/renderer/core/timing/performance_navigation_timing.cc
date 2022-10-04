cfe114f7f88e5fe52e1bbaab18fda28a751699ee
Emily Shack <emshack@chromium.org>
2022-10-04 23:28:49 +0000

    Revert "[bfcache] Add notRestoredReasons field in PerformanceNavigationTiming"
    This reverts commit e1af9facf232a11871b0c668b90ec17c5a5d5e16.
    
    Reason for revert: Likely cause for multiple builder failures. For example, see https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20Tests%20(sandboxed)/93643/overview
    
    Original change's description:
    > [bfcache] Add notRestoredReasons field in PerformanceNavigationTiming
    >
    > This CL adds back/forward cache not restored reasons in Performance
    > NavigationTiming API.
    > It's proposed here:
    > https://github.com/w3c/navigation-timing/issues/171
    > Explainer: https://github.com/rubberyuzu/bfcache-not-retored-reason/blob/main/NotRestoredReason.md
    >
    > This CL does two things:
    > - exposes not restored reasons to PerformanceNavigationTiming API
    > - adds WPT
    >
    > The WPT have to run on all platforms because this API is intended to
    > collect bfcache metrics from the wild, regardless of the platforms.
    >
    > LOW_COVERAGE_REASON=Adding coverage for new field. Just missing the tests for the existing fields
    >
    > Bug: 1349228
    > Change-Id: I8dd96a60188bdff94a21c4e4e34cacf181a823db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3835534
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Weizhong Xia <weizhong@google.com>
    > Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1054800}
    
    Bug: 1349228
    Change-Id: Iab0fed375560bb0b09bf4e8ea18b80c5d88b1375
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935464
    Owners-Override: Emily Shack <emshack@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055015}

--- third_party/blink/renderer/core/timing/performance_navigation_timing.cc
+++ third_party/blink/renderer/core/timing/performance_navigation_timing.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_timing.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
-#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/loader/document_load_timing.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/performance_entry_names.h"
@@ -312,54 +311,6 @@ DOMHighResTimeStamp PerformanceNavigationTiming::duration() const {
   return loadEventEnd();
 }
 
-ScriptValue PerformanceNavigationTiming::notRestoredReasons(
-    ScriptState* script_state) const {
-  DocumentLoader* loader = GetDocumentLoader();
-  if (!loader || !loader->GetFrame()->IsOutermostMainFrame())
-    return ScriptValue::CreateNull(script_state->GetIsolate());
-
-  // TODO(crbug.com/1370954): Save NotRestoredReasons in Document instead of
-  // Frame.
-  return NotRestoredReasonsBuilder(script_state,
-                                   loader->GetFrame()->GetNotRestoredReasons());
-}
-
-ScriptValue PerformanceNavigationTiming::NotRestoredReasonsBuilder(
-    ScriptState* script_state,
-    const mojom::blink::BackForwardCacheNotRestoredReasonsPtr& reasons) const {
-  if (!reasons)
-    return ScriptValue::CreateNull(script_state->GetIsolate());
-  V8ObjectBuilder builder(script_state);
-  builder.AddBoolean("blocked", reasons->blocked);
-  builder.AddString("url", AtomicString(reasons->same_origin_details
-                                            ? reasons->same_origin_details->url
-                                            : ""));
-  builder.AddString("src", AtomicString(reasons->same_origin_details
-                                            ? reasons->same_origin_details->src
-                                            : ""));
-  builder.AddString("id", AtomicString(reasons->same_origin_details
-                                           ? reasons->same_origin_details->id
-                                           : ""));
-  builder.AddString("name",
-                    AtomicString(reasons->same_origin_details
-                                     ? reasons->same_origin_details->name
-                                     : ""));
-  Vector<AtomicString> reason_strings;
-  Vector<v8::Local<v8::Value>> children_result;
-  if (reasons->same_origin_details) {
-    for (const auto& reason : reasons->same_origin_details->reasons) {
-      reason_strings.push_back(reason);
-    }
-    for (const auto& child : reasons->same_origin_details->children) {
-      children_result.push_back(
-          NotRestoredReasonsBuilder(script_state, child).V8Value());
-    }
-  }
-  builder.Add("reasons", reason_strings);
-  builder.Add("children", children_result);
-  return builder.GetScriptValue();
-}
-
 void PerformanceNavigationTiming::BuildJSONValue(
     V8ObjectBuilder& builder) const {
   PerformanceResourceTiming::BuildJSONValue(builder);
@@ -379,12 +330,6 @@ void PerformanceNavigationTiming::BuildJSONValue(
     builder.AddNumber(
         "activationStart",
         PerformanceNavigationTimingActivationStart::activationStart(*this));
-  }
-
-  if (RuntimeEnabledFeatures::BackForwardCacheNotRestoredReasonsEnabled(
-          ExecutionContext::From(builder.GetScriptState()))) {
-    builder.Add("notRestoredReasons",
-                notRestoredReasons(builder.GetScriptState()));
   }
 }
 }  // namespace blink
