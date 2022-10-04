13cf098a821c8b7f0c442379f655489c02595c9d
David Bertoni <dbertoni@chromium.org>
2022-10-04 19:53:01 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ui/webui.
    This CL was uploaded by git cl split.
    
    R=ortuno@chromium.org
    
    Bug: 1303949
    Change-Id: I402753492bdd5b3bb52b31919734a4aeb190509e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929798
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054890}

--- chrome/browser/ui/webui/certificate_provisioning_ui_handler_unittest.cc
+++ chrome/browser/ui/webui/certificate_provisioning_ui_handler_unittest.cc
@@ -95,7 +95,7 @@ void FormatDictRecurse(base::Value* value,
     return;
   }
   if (value->is_list()) {
-    for (base::Value& child : value->GetListDeprecated())
+    for (base::Value& child : value->GetList())
       FormatDictRecurse(&child, messages);
     return;
   }
@@ -123,7 +123,7 @@ base::Value FormatJsonDict(const base::StringPiece input,
 // |profile_id|.
 base::Value GetByProfileId(const base::Value& all_processes,
                            const std::string& profile_id) {
-  for (const base::Value& process : all_processes.GetListDeprecated()) {
+  for (const base::Value& process : all_processes.GetList()) {
     if (profile_id == *process.GetDict().FindString("certProfileId"))
       return process.Clone();
   }
@@ -184,7 +184,7 @@ class CertificateProvisioningUiHandlerTest : public ::testing::Test {
     if (!out_profile_ids)
       return;
     out_profile_ids->clear();
-    for (const base::Value& process : out_all_processes->GetListDeprecated()) {
+    for (const base::Value& process : out_all_processes->GetList()) {
       const std::string* profile_id =
           process.GetDict().FindString("certProfileId");
       ASSERT_TRUE(profile_id);
@@ -228,7 +228,7 @@ TEST_F(CertificateProvisioningUiHandlerTest, NoProcesses) {
   base::Value all_processes;
   ASSERT_NO_FATAL_FAILURE(RefreshCertProvisioningProcesses(
       &all_processes, /*out_profile_ids=*/nullptr));
-  EXPECT_TRUE(all_processes.GetListDeprecated().empty());
+  EXPECT_TRUE(all_processes.GetList().empty());
 }
 
 TEST_F(CertificateProvisioningUiHandlerTest, HasOneProcess) {
