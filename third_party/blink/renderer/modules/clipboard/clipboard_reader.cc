0aa836ff7f67558e7170bb03fe10be8a937b12f3
Michael Lippautz <mlippautz@chromium.org>
2022-10-04 12:37:03 +0000

    CrossThreadHandle: Use in ClipBoard{Reader,Write}
    Rewrite safe usage of CrossThreadPersistent with CrossThreadHandle.
    
    Drive-by: Rewrite usages of CTP for DOMArrayBuffer with using
    ArrayBufferContents which is a thread-safe smart pointer around the
    backing store.
    
    Bug: 1370013
    Change-Id: I8fb144901949d5d18c88ae76bc01249650ca8d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921741
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054641}

--- third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/clipboard/clipboard.h"
 #include "third_party/blink/renderer/modules/clipboard/clipboard_promise.h"
 #include "third_party/blink/renderer/modules/clipboard/clipboard_writer.h"
+#include "third_party/blink/renderer/platform/heap/cross_thread_handle.h"
 #include "third_party/blink/renderer/platform/image-encoders/image_encoder.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
@@ -84,15 +85,15 @@ class ClipboardTextReader final : public ClipboardReader {
     }
 
     worker_pool::PostTask(
-        FROM_HERE, CrossThreadBindOnce(
-                       &ClipboardTextReader::EncodeOnBackgroundThread,
-                       std::move(plain_text), WrapCrossThreadPersistent(this),
-                       std::move(clipboard_task_runner_)));
+        FROM_HERE,
+        CrossThreadBindOnce(&ClipboardTextReader::EncodeOnBackgroundThread,
+                            std::move(plain_text), MakeCrossThreadHandle(this),
+                            std::move(clipboard_task_runner_)));
   }
 
   static void EncodeOnBackgroundThread(
       String plain_text,
-      ClipboardTextReader* reader,
+      CrossThreadHandle<ClipboardTextReader> reader,
       scoped_refptr<base::SingleThreadTaskRunner> clipboard_task_runner) {
     DCHECK(!IsMainThread());
 
@@ -102,10 +103,13 @@ class ClipboardTextReader final : public ClipboardReader {
     utf8_bytes.ReserveInitialCapacity(utf8_text.size());
     utf8_bytes.Append(utf8_text.data(), utf8_text.size());
 
-    PostCrossThreadTask(*clipboard_task_runner, FROM_HERE,
-                        CrossThreadBindOnce(&ClipboardTextReader::NextRead,
-                                            WrapCrossThreadPersistent(reader),
-                                            std::move(utf8_bytes)));
+    PostCrossThreadTask(
+        *clipboard_task_runner, FROM_HERE,
+        CrossThreadBindOnce(
+            &ClipboardTextReader::NextRead,
+            MakeUnwrappingCrossThreadHandle<ClipboardTextReader>(
+                std::move(reader)),
+            std::move(utf8_bytes)));
   }
 
   void NextRead(Vector<uint8_t> utf8_bytes) override {
@@ -161,16 +165,15 @@ class ClipboardHtmlReader final : public ClipboardReader {
       return;
     }
     worker_pool::PostTask(
-        FROM_HERE,
-        CrossThreadBindOnce(&ClipboardHtmlReader::EncodeOnBackgroundThread,
-                            std::move(sanitized_html),
-                            WrapCrossThreadPersistent(this),
-                            std::move(clipboard_task_runner_)));
+        FROM_HERE, CrossThreadBindOnce(
+                       &ClipboardHtmlReader::EncodeOnBackgroundThread,
+                       std::move(sanitized_html), MakeCrossThreadHandle(this),
+                       std::move(clipboard_task_runner_)));
   }
 
   static void EncodeOnBackgroundThread(
       String plain_text,
-      ClipboardHtmlReader* reader,
+      CrossThreadHandle<ClipboardHtmlReader> reader,
       scoped_refptr<base::SingleThreadTaskRunner> clipboard_task_runner) {
     DCHECK(!IsMainThread());
 
@@ -180,10 +183,11 @@ class ClipboardHtmlReader final : public ClipboardReader {
     utf8_bytes.ReserveInitialCapacity(utf8_text.size());
     utf8_bytes.Append(utf8_text.data(), utf8_text.size());
 
-    PostCrossThreadTask(*clipboard_task_runner, FROM_HERE,
-                        CrossThreadBindOnce(&ClipboardHtmlReader::NextRead,
-                                            WrapCrossThreadPersistent(reader),
-                                            std::move(utf8_bytes)));
+    PostCrossThreadTask(
+        *clipboard_task_runner, FROM_HERE,
+        CrossThreadBindOnce(&ClipboardHtmlReader::NextRead,
+                            MakeUnwrappingCrossThreadHandle(std::move(reader)),
+                            std::move(utf8_bytes)));
   }
 
   void NextRead(Vector<uint8_t> utf8_bytes) override {
@@ -235,16 +239,15 @@ class ClipboardSvgReader final : public ClipboardReader {
       return;
     }
     worker_pool::PostTask(
-        FROM_HERE,
-        CrossThreadBindOnce(&ClipboardSvgReader::EncodeOnBackgroundThread,
-                            std::move(sanitized_svg),
-                            WrapCrossThreadPersistent(this),
-                            std::move(clipboard_task_runner_)));
+        FROM_HERE, CrossThreadBindOnce(
+                       &ClipboardSvgReader::EncodeOnBackgroundThread,
+                       std::move(sanitized_svg), MakeCrossThreadHandle(this),
+                       std::move(clipboard_task_runner_)));
   }
 
   static void EncodeOnBackgroundThread(
       String plain_text,
-      ClipboardSvgReader* reader,
+      CrossThreadHandle<ClipboardSvgReader> reader,
       scoped_refptr<base::SingleThreadTaskRunner> clipboard_task_runner) {
     DCHECK(!IsMainThread());
 
@@ -254,10 +257,11 @@ class ClipboardSvgReader final : public ClipboardReader {
     utf8_bytes.ReserveInitialCapacity(utf8_text.size());
     utf8_bytes.Append(utf8_text.data(), utf8_text.size());
 
-    PostCrossThreadTask(*clipboard_task_runner, FROM_HERE,
-                        CrossThreadBindOnce(&ClipboardSvgReader::NextRead,
-                                            WrapCrossThreadPersistent(reader),
-                                            std::move(utf8_bytes)));
+    PostCrossThreadTask(
+        *clipboard_task_runner, FROM_HERE,
+        CrossThreadBindOnce(&ClipboardSvgReader::NextRead,
+                            MakeUnwrappingCrossThreadHandle(std::move(reader)),
+                            std::move(utf8_bytes)));
   }
 
   void NextRead(Vector<uint8_t> utf8_bytes) override {
