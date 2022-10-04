850e96caa194d48d591e5e001b7ef1b3a8728b28
Daniel Cheng <dcheng@chromium.org>
2022-10-04 07:02:01 +0000

    Use base::test::FeatureRef for feature vectors in //chrome/browser/preloading.
    A base::Feature should not be copyable; a feature is intended to have a
    single global instance, as it caches internal mutable state.
    
    Feature vectors in conjunction with `base::test::ScopedFeatureList` are
    by far the most common use of the base::Feature copy constructor. This
    is a mostly automated replacement of `std::vector<base::Feature>` with
    `std::vector<base::test::FeatureRef>` to remove that dependency.
    
    This CL was uploaded by git cl split.
    
    R=lingqi@chromium.org
    
    Bug: 1370572
    Change-Id: Icf407a8652313c92a9bd9dfcbe9015d6b05576a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933734
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054547}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
@@ -406,7 +406,7 @@ class SearchPrefetchServiceEnabledBrowserTest
                              {{"max_attempts_per_caching_duration", "3"},
                               {"cache_size", "1"},
                               {"device_memory_threshold_MB", "0"}}}};
-    std::vector<base::Feature> disabled_features = {};
+    std::vector<base::test::FeatureRef> disabled_features = {};
     if (BlockOnHeadersEnabled()) {
       enabled_features.push_back({kSearchPrefetchBlockBeforeHeaders, {}});
     } else {
@@ -3383,7 +3383,7 @@ class SearchPrefetchServiceNavigationPrefetchBrowserTest
                               {"cache_size", "1"},
                               {"device_memory_threshold_MB", "0"}}},
                             {kSearchNavigationPrefetch, {}}};
-    std::vector<base::Feature> disabled_features = {};
+    std::vector<base::test::FeatureRef> disabled_features = {};
 
     feature_list_.InitWithFeaturesAndParameters(enabled_features,
                                                 disabled_features);
@@ -3678,7 +3678,7 @@ class SearchNavigationPrefetchHoldbackBrowserTest
                               {"device_memory_threshold_MB", "0"},
                               {"prefetch_holdback", "true"}}},
                             {kSearchNavigationPrefetch, {{}}}};
-    std::vector<base::Feature> disabled_features = {};
+    std::vector<base::test::FeatureRef> disabled_features = {};
 
     feature_list_.InitWithFeaturesAndParameters(enabled_features,
                                                 disabled_features);
@@ -3781,7 +3781,7 @@ class SearchNavigationPrefetchNoCancelBrowserTest
                               {"device_memory_threshold_MB", "0"}}},
                             {kSearchPrefetchSkipsCancel, {}},
                             {kSearchNavigationPrefetch, {{}}}};
-    std::vector<base::Feature> disabled_features = {};
+    std::vector<base::test::FeatureRef> disabled_features = {};
 
     feature_list_.InitWithFeaturesAndParameters(enabled_features,
                                                 disabled_features);
