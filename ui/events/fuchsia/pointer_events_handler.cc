a9231e28f363dd9b82e88d465131f17939ceb0a2
Jaeheon Yi <jaeheon@google.com>
2022-10-04 01:53:12 +0000

    [fuchsia] Convert GFX client to modern input APIs
    This patch converts the legacy GFX client to use
    the newer pointer and focus APIs. It reduces the
    delta for the Flatland migration, since the
    Flatland client also uses these newer APIs.
    
    Tested with ozone_unittests, events_unittests,
    and a manual run on the production device.
    
    Bug: fuchsia:64382
    Change-Id: I9e6e51947a6d8605aae25e627fe4442915d653c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928365
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Jaeheon Yi <jaeheon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054450}

--- ui/events/fuchsia/pointer_events_handler.cc
+++ ui/events/fuchsia/pointer_events_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <memory>
 
+#include "base/fuchsia/fuchsia_logging.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
@@ -179,10 +180,11 @@ TouchEvent CreateTouchEventDraft(const fup::TouchEvent& event,
                                 view_parameters.viewport_to_view_transform);
   // TODO(fxbug.dev/88580): Consider setting hover via
   // ui::TouchEvent::set_hovering().
-  return TouchEvent(event_type, gfx::PointF(logical[0], logical[1]),
-                    gfx::PointF(sample.position_in_viewport()[0],
-                                sample.position_in_viewport()[1]),
-                    timestamp, pointer_details);
+  gfx::PointF location(logical[0], logical[1]);
+  gfx::PointF root_location(sample.position_in_viewport()[0],
+                            sample.position_in_viewport()[1]);
+  return TouchEvent(event_type, location, root_location, timestamp,
+                    pointer_details);
 }
 
 // It returns a "draft" because the coordinates are logical. Later,
@@ -283,7 +285,14 @@ std::unique_ptr<MouseEvent> CreateMouseEventDraft(
 
 PointerEventsHandler::PointerEventsHandler(fup::TouchSourceHandle touch_source,
                                            fup::MouseSourceHandle mouse_source)
-    : touch_source_(touch_source.Bind()), mouse_source_(mouse_source.Bind()) {}
+    : touch_source_(touch_source.Bind()), mouse_source_(mouse_source.Bind()) {
+  touch_source_.set_error_handler([](zx_status_t status) {
+    ZX_LOG(ERROR, status) << "fuchsia.ui.pointer.TouchSource disconnected.";
+  });
+  mouse_source_.set_error_handler([](zx_status_t status) {
+    ZX_LOG(ERROR, status) << "fuchsia.ui.pointer.MouseSource disconnected.";
+  });
+}
 
 PointerEventsHandler::~PointerEventsHandler() = default;
 
