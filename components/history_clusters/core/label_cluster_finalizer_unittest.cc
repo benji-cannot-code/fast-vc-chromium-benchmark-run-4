6a8f7d30c2ea264e3a968b613329264137bc462b
Sophie Chang <sophiechang@chromium.org>
2022-10-04 16:45:48 +0000

    Fix entity labels to use human-readable name instead of entity id
    Followup after https://chromium-review.googlesource.com/c/chromium/src/+/3915476
    
    Bug: b/244505276
    Change-Id: I2ef9618af936c6995c36955ea0f38919c2d0dbd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933093
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054780}

--- components/history_clusters/core/label_cluster_finalizer_unittest.cc
+++ components/history_clusters/core/label_cluster_finalizer_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/clustering_test_utils.h"
 #include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
+#include "components/optimization_guide/core/entity_metadata.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -20,7 +21,20 @@ using ::testing::UnorderedElementsAre;
 class LabelClusterFinalizerTest : public ::testing::Test {
  public:
   void SetUp() override {
-    cluster_finalizer_ = std::make_unique<LabelClusterFinalizer>();
+    base::flat_map<std::string, optimization_guide::EntityMetadata>
+        entity_metadata_map;
+    optimization_guide::EntityMetadata md1;
+    md1.human_readable_name = "doesntmatter";
+    entity_metadata_map["someotherentity"] = md1;
+    optimization_guide::EntityMetadata md2;
+    md2.human_readable_name = "doesntmatter";
+    entity_metadata_map["highscoringentitybutlowvisitscore"] = md2;
+    optimization_guide::EntityMetadata label_md;
+    label_md.human_readable_name = "chosenlabel";
+    entity_metadata_map["baz"] = label_md;
+
+    cluster_finalizer_ =
+        std::make_unique<LabelClusterFinalizer>(entity_metadata_map);
   }
 
   void TearDown() override { cluster_finalizer_.reset(); }
@@ -39,13 +53,13 @@ TEST_F(LabelClusterFinalizerTest, ClusterWithNoSearchTerms) {
       testing::CreateDefaultAnnotatedVisit(1, GURL("https://foo.com/")));
   visit.score = 0.8;
   visit.annotated_visit.content_annotations.model_annotations.entities = {
-      {"chosenlabel", 50}};
+      {"baz", 50}};
 
   history::ClusterVisit visit2 = testing::CreateClusterVisit(
       testing::CreateDefaultAnnotatedVisit(2, GURL("https://bar.com/")));
   visit2.score = 0.25;
   visit2.annotated_visit.content_annotations.model_annotations.entities = {
-      {"chosenlabel", 50}, {"highscoringentitybutlowvisitscore", 100}};
+      {"baz", 50}, {"highscoringentitybutlowvisitscore", 100}};
 
   history::ClusterVisit visit3 = testing::CreateClusterVisit(
       testing::CreateDefaultAnnotatedVisit(3, GURL("https://baz.com/")));
@@ -53,7 +67,7 @@ TEST_F(LabelClusterFinalizerTest, ClusterWithNoSearchTerms) {
       testing::ClusterVisitToDuplicateClusterVisit(visit));
   visit3.score = 0.8;
   visit3.annotated_visit.content_annotations.model_annotations.entities = {
-      {"chosenlabel", 25}, {"someotherentity", 10}};
+      {"baz", 25}, {"someotherentity", 10}};
 
   {
     // With only search term labelling active, there should be no label.
@@ -144,7 +158,7 @@ TEST_F(LabelClusterFinalizerTest, TakesHighestScoringSearchTermIfAvailable) {
       testing::CreateDefaultAnnotatedVisit(2, GURL("https://baz.com/")));
   visit3.score = 0.8;
   visit3.annotated_visit.content_annotations.model_annotations.entities = {
-      {"github", 100}, {"otherentity", 100}};
+      {"github", 100}, {"someotherentity", 100}};
   visit3.annotated_visit.content_annotations.search_terms = u"searchtermlabel";
 
   history::Cluster cluster;
