990603f3212276e76c202c9b27b06d2ae6afa5ec
Carlos IL <carlosil@chromium.org>
2022-10-04 18:28:46 +0000

    Set default value for mixed content autoupgrades to true if no settings are availabile
    Setting this to false was resulting in about:blank iframes being
    allowed to load mixed content.
    
    Change-Id: I4dc93ffefb16fbd9def66591bf679f48d5eb1b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931767
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054847}

--- components/content_settings/renderer/content_settings_agent_impl.cc
+++ components/content_settings/renderer/content_settings_agent_impl.cc
@@ -410,7 +410,7 @@ bool ContentSettingsAgentImpl::ShouldAutoupgradeMixedContent() {
         content_setting_rules_->mixed_content_rules, GURL());
     return setting != CONTENT_SETTING_ALLOW;
   }
-  return false;
+  return true;
 }
 
 RendererContentSettingRules*
