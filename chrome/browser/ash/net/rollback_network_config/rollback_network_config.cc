73c2fa788658227c5ebfba1f0ae90f611441f826
David Bertoni <dbertoni@chromium.org>
2022-10-04 18:14:59 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ash/net/rollback_network_config.
    This CL was uploaded by git cl split.
    
    R=mpolzer@google.com
    
    Bug: 1303949
    Change-Id: I28ce4b3c5cf081fb2b30e98f3d68490d2a52d52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932941
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054833}

--- chrome/browser/ash/net/rollback_network_config/rollback_network_config.cc
+++ chrome/browser/ash/net/rollback_network_config/rollback_network_config.cc
@@ -383,7 +383,7 @@ void RollbackNetworkConfig::Importer::Import(const std::string& network_config,
   }
 
   auto barrier_closure = base::BarrierClosure(
-      network_list->GetListDeprecated().size(),
+      network_list->GetList().size(),
       base::BindOnce(&RollbackNetworkConfig::Importer::AllNetworksConfigured,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
 
@@ -393,7 +393,7 @@ void RollbackNetworkConfig::Importer::Import(const std::string& network_config,
 
   bool ownership_taken = IsOwnershipTaken();
 
-  for (base::Value& network : network_list->GetListDeprecated()) {
+  for (base::Value& network : network_list->GetList()) {
     if (!ownership_taken) {
       ManagedOncConfigureActivePartAsDeviceWide(network.Clone(),
                                                 finished_a_network);
