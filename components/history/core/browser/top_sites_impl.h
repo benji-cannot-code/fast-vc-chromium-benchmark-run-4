3f1fa9da7c039cfd8863569d7b39797a1f4a6b56
Moe Ahmadi <mahmadi@chromium.org>
2022-10-04 17:39:02 +0000

    [RQ] Adds utility function in TURLService to generate SRP URL for DSE
    Bug: 1317829
    Change-Id: Ie7cc0f0cec7a6623dd498a83fa5053246aaf255c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933216
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054804}

--- components/history/core/browser/top_sites_impl.h
+++ components/history/core/browser/top_sites_impl.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefRegistrySimple;
 class PrefService;
-class SearchTermsData;
-class TemplateURL;
 class TemplateURLService;
 
 namespace base {
@@ -42,14 +40,6 @@ class TopSitesImplTest;
 
 // How many top sites to store in the cache.
 static constexpr size_t kTopSitesNumber = 10;
-
-// Returns true if it can set |url| to a valid canonical search results page
-// URL for |default_provider| given the search terms.
-bool GetSearchResultsPageForDefaultSearchProvider(
-    const TemplateURL& default_provider,
-    const SearchTermsData& search_terms_data,
-    const std::u16string& search_terms,
-    GURL* url);
 
 // This class allows requests for most visited urls on any thread. All other
 // methods must be invoked on the UI thread. All mutations to internal state
