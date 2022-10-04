b69944f72ffa4b3107d32246e5c71d365b5d48ef
Aida Zolic <aidazolic@chromium.org>
2022-10-04 08:32:25 +0000

    [DLP] Pass caller URL in DownloadFilePicker
    Passes the download item's URL to `SelectFile()` which is needed for
    Data Leak Prevention checks in the picker dialog.
    
    Bug: 1364565
    Change-Id: Ic5e5c0fee3d0d2d94c6ba3cd4892ab14fb8c6283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929052
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054581}

--- chrome/browser/download/download_file_picker.cc
+++ chrome/browser/download/download_file_picker.cc
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/dlp/dlp_files_controller.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_warn_dialog.h"
-#include "chrome/browser/profiles/profile.h"
 #endif
 
 using download::DownloadItem;
@@ -97,10 +95,17 @@ DownloadFilePicker::DownloadFilePicker(download::DownloadItem* item,
   }
 #endif
 
+  const GURL* caller =
+#if BUILDFLAG(IS_CHROMEOS)
+      &download_item_->GetURL();
+#else
+      nullptr;
+#endif
+
   select_file_dialog_->SelectFile(
       ui::SelectFileDialog::SELECT_SAVEAS_FILE, std::u16string(),
       suggested_path_, &file_type_info, 0, base::FilePath::StringType(),
-      owning_window, nullptr);
+      owning_window, /*params=*/nullptr, caller);
 }
 
 DownloadFilePicker::~DownloadFilePicker() {
