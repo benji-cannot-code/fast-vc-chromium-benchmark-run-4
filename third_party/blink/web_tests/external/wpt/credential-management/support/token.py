d37c62b48f91a773768f44fb8ddab70105feb69f
Christian Biesinger <cbiesinger@chromium.org>
2022-10-04 21:32:51 +0000

    [FedCM] Set the destination to "webidentity"
    R=yigu@chromium.org
    
    Bug: 1368382
    Change-Id: I48458aa84429ea66a9823b9d6f493b0730c6bb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930655
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054946}

--- third_party/blink/web_tests/external/wpt/credential-management/support/token.py
+++ third_party/blink/web_tests/external/wpt/credential-management/support/token.py
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
+  if not b"sec-fetch-dest" in request.headers or request.headers[b"sec-fetch-dest"] != b"webidentity":
+    return (500, [], "Missing Sec-Fetch-Dest header")
   if not b"cookie" in request.cookies or request.cookies[b"cookie"].value != b"1":
     return (500, [], "Missing cookie")
   return "{\"token\": \"token\"}"
