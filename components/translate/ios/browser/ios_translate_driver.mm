22781e996575d7653918bf7800c348b1b37464b4
Mike Dougherty <michaeldo@chromium.org>
2022-10-04 06:54:10 +0000

    Remove TranslateController's usage of WebState::AddScriptCommandCallback
    This CL replaces TranslateController's usage of
    WebState::AddScriptCommandCallback with a JavaScriptFeature to listen
    for translate messages.
    
    Additionally, update translate_ios.js and convert TranslateController to
    a WebStateUserData instance in order to retrieve it from
    TranslateJavaScriptFeature.
    
    Bug:1175922
    
    Change-Id: I9baa13b8d66641b9645f93f62c9da1be1e7cbb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930560
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054534}

--- components/translate/ios/browser/ios_translate_driver.mm
+++ components/translate/ios/browser/ios_translate_driver.mm
@@ -76,10 +76,9 @@ IOSTranslateDriver::IOSTranslateDriver(
           web_state, language_detection_model,
           translate_manager_->translate_client()->GetPrefs());
 
-  translate_controller_ = std::make_unique<TranslateController>(
+  TranslateController::CreateForWebState(
       web_state, JSTranslateWebFrameManagerFactory::GetInstance());
-
-  translate_controller_->set_observer(this);
+  TranslateController::FromWebState(web_state)->set_observer(this);
 }
 
 IOSTranslateDriver::~IOSTranslateDriver() {
@@ -164,13 +163,14 @@ void IOSTranslateDriver::TranslatePage(int page_seq_no,
   source_language_ = source_lang;
   target_language_ = target_lang;
   pending_page_seq_no_ = page_seq_no;
-  translate_controller_->InjectTranslateScript(translate_script);
+  TranslateController::FromWebState(web_state_)
+      ->InjectTranslateScript(translate_script);
 }
 
 void IOSTranslateDriver::RevertTranslation(int page_seq_no) {
   if (page_seq_no != page_seq_no_)
     return;  // The user navigated away.
-  translate_controller_->RevertTranslation();
+  TranslateController::FromWebState(web_state_)->RevertTranslation();
 }
 
 bool IOSTranslateDriver::IsIncognito() {
@@ -258,7 +258,8 @@ void IOSTranslateDriver::OnTranslateScriptReady(TranslateErrors error_type,
   std::string source = (source_language_ != kUnknownLanguageCode)
                            ? source_language_
                            : kAutoDetectionLanguage;
-  translate_controller_->StartTranslation(source_language_, target_language_);
+  TranslateController::FromWebState(web_state_)
+      ->StartTranslation(source_language_, target_language_);
 }
 
 void IOSTranslateDriver::OnTranslateComplete(TranslateErrors error_type,
