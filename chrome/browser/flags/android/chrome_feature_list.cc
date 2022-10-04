899ad69ae5137c2d787c8ef3998e2fccf94792fe
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-10-04 16:54:19 +0000

    Remove EnableSyncImmediatelyInFRE flag
    The feature is enabled by default since M106.
    
    Fixed: 795292
    Change-Id: I1686d1f1b72c804d81c1ce8df66ca33ab79f896a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865502
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054789}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -417,7 +417,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &switches::kForceDisableExtendedSyncPromos,
     &switches::kGaiaIdInAMF,
     &switches::kTangibleSync,
-    &syncer::kEnableSyncImmediatelyInFRE,
     &syncer::kSyncTrustedVaultPassphraseRecovery,
     &syncer::kSyncAndroidLimitNTPPromoImpressions,
     &syncer::kSyncAndroidPromosWithAlternativeTitle,
