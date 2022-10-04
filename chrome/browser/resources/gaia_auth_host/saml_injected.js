205bcc46d5bd5fae893df1e1a01eee19fea93ac8
Anastasiia N <anastasiian@chromium.org>
2022-10-04 16:02:59 +0000

    Use JS modules in gaia_auth_host/: format
    Format files as a follow-up to https://crrev.com/c/3838710.
    
    No functional changes in this CL.
    
    Bug: 1350059
    Change-Id: I93b52402014ec3053ea84e92ef35ea590c12a187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913749
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054734}

--- chrome/browser/resources/gaia_auth_host/saml_injected.js
+++ chrome/browser/resources/gaia_auth_host/saml_injected.js
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {Channel} from './channel.js';
-import {WebviewScrollShadowsHelper, WebviewScrollShadowsHelperConstructor} from './scroll_helper_injected.js';
 import {PostMessageChannel} from './post_message_channel.js';
+import {WebviewScrollShadowsHelper, WebviewScrollShadowsHelperConstructor} from './scroll_helper_injected.js';
 
 /**
  * @fileoverview
--- chrome/browser/resources/gaia_auth_host/saml_username_autofill.js
+++ chrome/browser/resources/gaia_auth_host/saml_username_autofill.js
@@ -5,34 +5,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {appendParam} from 'chrome://resources/js/util.js';
 
-  /**
-   * Try to autofill email on login page for supported identity providers
-   * @param {string} url Url of IdP login page
-   * @param {?string} urlParameterNameToAutofillUsername Url parameter name
-   *     which can be used to autofill the username field
-   * @param {?string} email User's email which is to be used as a username on
-   *     IdP login page
-   * @return {?string} Modified url which can autofill the username field, or
-   *     null.
-   */
-  export function maybeAutofillUsername(
-      url, urlParameterNameToAutofillUsername, email) {
-    if (!urlParameterNameToAutofillUsername ||
-        urlParameterNameToAutofillUsername.length === 0) {
-      return null;
-    }
-    if (!url.startsWith('https')) {
-      return null;
-    }
-    if (!email) {
-      return null;
-    }
-    // Don't do anything if url already contains
-    // `urlParameterNameToAutofillUsername`.
-    if (url.match(urlParameterNameToAutofillUsername)) {
-      return null;
-    }
-
-    url = appendParam(url, urlParameterNameToAutofillUsername, email);
-    return url;
+/**
+ * Try to autofill email on login page for supported identity providers
+ * @param {string} url Url of IdP login page
+ * @param {?string} urlParameterNameToAutofillUsername Url parameter name
+ *     which can be used to autofill the username field
+ * @param {?string} email User's email which is to be used as a username on
+ *     IdP login page
+ * @return {?string} Modified url which can autofill the username field, or
+ *     null.
+ */
+export function maybeAutofillUsername(
+    url, urlParameterNameToAutofillUsername, email) {
+  if (!urlParameterNameToAutofillUsername ||
+      urlParameterNameToAutofillUsername.length === 0) {
+    return null;
   }
+  if (!url.startsWith('https')) {
+    return null;
+  }
+  if (!email) {
+    return null;
+  }
+  // Don't do anything if url already contains
+  // `urlParameterNameToAutofillUsername`.
+  if (url.match(urlParameterNameToAutofillUsername)) {
+    return null;
+  }
+
+  url = appendParam(url, urlParameterNameToAutofillUsername, email);
+  return url;
+}
