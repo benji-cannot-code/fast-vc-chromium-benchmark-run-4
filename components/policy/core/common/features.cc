1d5aded306d4deb1a2233ae4b3314b9144bba280
Igor Ruvinov <igorruvinov@chromium.org>
2022-10-04 22:12:14 +0000

    Enable DMToken deletion feature by default.
    The feature is approved and ready to launch via Finch.
    
    Bug: 1370536
    Change-Id: I39ee7bfc61fe33bcadfabdbdd2fc8b748c5d5bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931863
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054965}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -33,7 +33,7 @@ BASE_FEATURE(kActivateMetricsReportingEnabledPolicyAndroid,
 
 BASE_FEATURE(kDmTokenDeletion,
              "DmTokenDeletion",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
 
