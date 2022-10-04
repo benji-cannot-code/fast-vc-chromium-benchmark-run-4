ca31ba164e3e7e24bf27e4cddb913d323f8cff26
Daniel Cheng <dcheng@chromium.org>
2022-10-04 02:11:39 +0000

    Add overloads to avoid copying base::Feature with ScopedFeatureList.
    A base::Feature should not be copyable; a feature is intended to have a
    single global instance, as it caches internal mutable state.
    
    One major reason features are copied is to specify lists of features to
    enable or disable in tests. This use case is addressed by the addition
    of a base::test::FeatureRef helper.
    
    To aid incremental migration, this CL just adds overloads but makes no
    actual migration. Followups will change `std::vector<base::Feature>`
    into `std::vector<base::test::FeatureRef>`, and the compatibility
    overloads will be removed after that.
    
    A few small test fixes are required to disambiguate callers that
    accidentally wrapped lists of features with extra {}'s: with the new
    overloads, it becomes ambiguous which std::vector overload to choose.
    Simply remove the extra {}'s so that the std::initializer_list
    overload is chosen instead.
    
    Bug: 1370572
    Change-Id: Ie826ffab87b890588131bfb28a52dd7e0e54a137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932204
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054480}

--- chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
@@ -124,7 +124,8 @@ class PwaInstallViewBrowserTest : public extensions::ExtensionBrowserTest {
          {feature_engagement::kIPHDesktopPwaInstallFeature, {}}},
 #if BUILDFLAG(IS_CHROMEOS_ASH)
         {
-          {features::kWebAppsCrosapi, chromeos::features::kLacrosPrimary}, {}
+            features::kWebAppsCrosapi,
+            chromeos::features::kLacrosPrimary,
         }
 #else
         {}
