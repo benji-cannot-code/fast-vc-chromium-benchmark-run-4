413d2b97334583679b495beeaee63b459899aefe
Avi Drissman <avi@chromium.org>
2022-10-05 23:19:30 +0000

    Revert "Make confirmation chip independent of request chip"
    This reverts commit b1217816d842b67faf08cba1d25837399a47fa2a.
    
    Reason for revert: breaks asan
    
    see https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/106685/test-results
    
    =================================================================
    ==7990==ERROR: AddressSanitizer: heap-use-after-free on address 0x61500014a3c8 at pc 0x55bcdbc9090d bp 0x7ffe8c468ab0 sp 0x7ffe8c468aa8
    READ of size 8 at 0x61500014a3c8 thread T0 (browser_tests)
        #0 0x55bcdbc9090c in begin buildtools/third_party/libc++/trunk/include/vector:1374:33
        #1 0x55bcdbc9090c in begin<std::Cr::vector<base::internal::UncheckedObserverAdapter, std::Cr::allocator<base::internal::UncheckedObserverAdapter> > &> base/ranges/ranges.h:44:37
        #2 0x55bcdbc9090c in begin<std::Cr::vector<base::internal::UncheckedObserverAdapter, std::Cr::allocator<base::internal::UncheckedObserverAdapter> > &> base/ranges/ranges.h:105:10
        #3 0x55bcdbc9090c in find_if<std::Cr::vector<base::internal::UncheckedObserverAdapter, std::Cr::allocator<base::internal::UncheckedObserverAdapter> > &, (lambda at ../../base/observer_list.h:287:21), base::identity, std::Cr::random_access_iterator_tag> base/ranges/algorithm.h:483:26
        #4 0x55bcdbc9090c in base::ObserverList<permissions::PermissionRequestManager::Observer, false, true, base::internal::UncheckedObserverAdapter>::RemoveObserver(permissions::PermissionRequestManager::Observer const*) base/observer_list.h:286:21
        #5 0x55bcdf91c380 in Reset base/scoped_observation.h:86:7
        #6 0x55bcdf91c380 in ~ScopedObservation base/scoped_observation.h:70:26
        #7 0x55bcdf91c380 in permissions::PermissionRequestObserver::~PermissionRequestObserver() components/permissions/test/permission_request_observer.cc:14:55
        #8 0x55bccbcd2629 in PermissionRequestChipGestureSensitiveBrowserTest_ShouldUpdateActiverPRMAndObservations_Test::RunTestOnMainThread() chrome/browser/ui/views/permissions/permission_request_chip_browsertest.cc:294:1
        #9 0x55bce299402f in content::BrowserTestBase::ProxyRunTestOnMainThreadLoop() content/public/test/browser_test_base.cc:883:7
        #10 0x55bce299accf in Invoke<void (content::BrowserTestBase::*)(), content::BrowserTestBase *> base/functional/bind_internal.h:643:12
        #11 0x55bce299accf in MakeItSo<void (content::BrowserTestBase::*)(), std::Cr::tuple<base::internal::UnretainedWrapper<content::BrowserTestBase, base::RawPtrBanDanglingIfSupported> > > base/functional/bind_internal.h:822:12
        #12 0x55bce299accf in RunImpl<void (content::BrowserTestBase::*)(), std::Cr::tuple<base::internal::UnretainedWrapper<content::BrowserTestBase, base::RawPtrBanDanglingIfSupported> >, 0UL> base/functional/bind_internal.h:916:12
        #13 0x55bce299accf in base::internal::Invoker<base::internal::BindState<void (content::BrowserTestBase::*)(), base::internal::UnretainedWrapper<content::BrowserTestBase, base::RawPtrBanDanglingIfSupported>>, void ()>::RunOnce(base::internal::BindStateBase*) base/functional/bind_internal.h:867:12
    
    
    Original change's description:
    > Make confirmation chip independent of request chip
    >
    > See design doc: go/confirmation-chip
    >
    > Bug: 1364276
    > Change-Id: Iab08a5bb96271d45101ba68c0134751b921b7667
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921717
    > Commit-Queue: Florian Jacky <fjacky@chromium.org>
    > Reviewed-by: Illia Klimov <elklm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1055397}
    
    Bug: 1364276
    Change-Id: Iecf56cfafaf15a2ab8329138f7508de8afd0a341
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937182
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055548}

--- components/permissions/request_type.cc
+++ components/permissions/request_type.cc
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/notreached.h"
-#include "base/ranges/algorithm.h"
 #include "build/build_config.h"
 #include "components/content_settings/core/common/content_settings_types.h"
-#include "components/permissions/permission_request.h"
 #include "components/permissions/permissions_client.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -254,17 +252,6 @@ absl::optional<ContentSettingsType> RequestTypeToContentSettingsType(
       // Not associated with a ContentSettingsType.
       return absl::nullopt;
   }
-}
-
-// Returns whether confirmation chips can be displayed
-bool IsConfirmationChipSupported(RequestType for_request_type) {
-  return base::ranges::any_of(
-      std::vector<RequestType>{
-          RequestType::kNotifications, RequestType::kGeolocation,
-          RequestType::kCameraStream, RequestType::kMicStream},
-      [for_request_type](permissions::RequestType request_type) {
-        return request_type == for_request_type;
-      });
 }
 
 IconId GetIconId(RequestType type) {
