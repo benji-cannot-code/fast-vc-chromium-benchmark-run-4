4e3a3b4dc36994ed106bddb931d91d62add37b7b
Sylvain Defresne <sdefresne@chromium.org>
2022-10-05 18:50:26 +0000

    [ios] Convert time duration constants to base::TimeDelta
    Using base::TimeDelta allow to specify the time unit when defining
    the constant without resorting to putting the unit in the variable
    name.
    
    This also helps converting between different units with the helper
    methods (.InSecondsF(), .InNanosecondsF(), ...).
    
    This CL converts //ios/chrome/browser/link_to_text.
    
    Remove unused constants from link_to_text_constants.h at the same
    time as the rest of the refactor since this allow deleting the
    implementation file.
    
    Bug: none
    Change-Id: I39fe07759eedabe46690601e4ebb98120ea6420d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934829
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055415}

--- ios/chrome/browser/link_to_text/link_to_text_utils.mm
+++ ios/chrome/browser/link_to_text/link_to_text_utils.mm
@@ -51,7 +51,7 @@ shared_highlighting::LinkGenerationError OutcomeToError(
 }
 
 BOOL IsLinkGenerationTimeout(base::TimeDelta latency) {
-  return latency.InMilliseconds() >= kLinkGenerationTimeoutInMs;
+  return latency >= kLinkGenerationTimeout;
 }
 
 }  // namespace link_to_text
