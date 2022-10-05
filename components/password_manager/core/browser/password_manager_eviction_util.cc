d72b18294b4021656127f95177792ce5b7a42eec
Maxim Anufriev <maxan@google.com>
2022-10-05 16:07:32 +0000

    [UPM] Use eviction utils from PwM-internals
    This moves chrome://password-manager-internals/ from directly updating
    the eviction status pref to using the eviction utils. This is required
    as eviction util also properly resets the state before re-enrolling
    the user.
    
    Password manager eviction utils are moved to the components.
    
    Bug: 1369145,1362464
    Change-Id: Ibb909ffd176274d5fb8d5b57529aca8234f64723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924353
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055320}

--- components/password_manager/core/browser/password_manager_eviction_util.cc
+++ components/password_manager/core/browser/password_manager_eviction_util.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/password_manager/android/password_manager_eviction_util.h"
+#include "components/password_manager/core/browser/password_manager_eviction_util.h"
 
 #include "base/containers/flat_set.h"
 #include "base/logging.h"
