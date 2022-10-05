d6679964d671fc77b6f5b97813477d40b6d5a4ab
Nancy Wang <nancylingwang@chromium.org>
2022-10-05 07:15:44 +0000

    Remove PreferredApp related mojom implementation.
    This CL is used to prepare for CL:3932640 to remove the
    kAppServicePreferredAppsWithoutMojom flag.
    
    BUG=1253250
    
    Change-Id: I19982dcafc0f3902508df6330c6bf6126fa9ead2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934662
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055156}

--- components/services/app_service/app_service_mojom_impl.h
+++ components/services/app_service/app_service_mojom_impl.h
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
-class PreferredAppsList;
-
 // The implementation of the apps::mojom::AppService Mojo interface.
 //
 // See components/services/app_service/README.md.
@@ -96,19 +94,6 @@ class AppServiceMojomImpl : public apps::mojom::AppService,
                                  int64_t display_id) override;
   void OpenNativeSettings(apps::mojom::AppType app_type,
                           const std::string& app_id) override;
-  void AddPreferredApp(apps::mojom::AppType app_type,
-                       const std::string& app_id,
-                       apps::mojom::IntentFilterPtr intent_filter,
-                       apps::mojom::IntentPtr intent,
-                       bool from_publisher) override;
-  void RemovePreferredApp(apps::mojom::AppType app_type,
-                          const std::string& app_id) override;
-  void SetSupportedLinksPreference(
-      apps::mojom::AppType app_type,
-      const std::string& app_id,
-      std::vector<apps::mojom::IntentFilterPtr> all_link_filters) override;
-  void RemoveSupportedLinksPreference(apps::mojom::AppType app_type,
-                                      const std::string& app_id) override;
   void SetResizeLocked(apps::mojom::AppType app_type,
                        const std::string& app_id,
                        apps::mojom::OptionalBool locked) override;
@@ -142,9 +127,6 @@ class AppServiceMojomImpl : public apps::mojom::AppService,
   // false.
   bool HasPublisher(AppType app_type) override;
 
-  // Retern the preferred_apps_list_ for testing.
-  PreferredAppsList& GetPreferredAppsListForTesting();
-
  private:
   void OnPublisherDisconnected(apps::mojom::AppType app_type);
 
@@ -157,8 +139,6 @@ class AppServiceMojomImpl : public apps::mojom::AppService,
   // Must come after the publisher and subscriber maps to ensure it is
   // destroyed first, closing the connection to avoid dangling callbacks.
   mojo::ReceiverSet<apps::mojom::AppService> receivers_;
-
-  std::unique_ptr<PreferredAppsImpl> preferred_apps_impl_;
 };
 
 }  // namespace apps
