f7eff8bfb9c77971773c7ba3b8e385de584bd812
Cici Ruan <cuicuiruan@google.com>
2022-10-05 18:36:24 +0000

    arc/gio: Implement the feature of adding new actions
    This CL adds the feature of adding new action for GIO beta. Two
    temporary buttons are added to add new actions for ActionTap and
    ActionMove.
    
    /restore the actions.
    
    Bug: b:241966781
    Test: unit_tests TouchInjectorTest
    Test: manual test for editing feature like adding/saving/cancel
    Change-Id: I6fd8b316307301d0de26080cf2e834a4692d9f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925660
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055411}

--- chrome/browser/ash/arc/input_overlay/actions/action.h
+++ chrome/browser/ash/arc/input_overlay/actions/action.h
@@ -67,6 +67,9 @@ class Action {
   virtual ~Action();
 
   virtual bool ParseFromJson(const base::Value& value);
+  // Used to create an action from UI.
+  virtual bool InitFromEditor();
+  bool ParseFromProto(const ActionProto& proto);
   void OverwriteFromProto(const ActionProto& proto);
   // 1. Return true & non-empty touch_events:
   //    Call SendEventFinally to send simulated touch event.
@@ -111,7 +114,7 @@ class Action {
   const Position& GetCurrentDisplayedPosition();
 
   // Return the proto object if the action is customized.
-  std::unique_ptr<ActionProto> ConvertToProtoIfCustomized();
+  virtual std::unique_ptr<ActionProto> ConvertToProtoIfCustomized() const;
   // Update |touch_down_positions_| after reposition, rotation change or window
   // bounds change.
   void UpdateTouchDownPositions();
