58c6b65819ef45c2f2daa50e341b7554a7099a04
Nancy Wang <nancylingwang@chromium.org>
2022-10-05 07:24:17 +0000

    Remove the kAppServicePreferredAppsWithoutMojom flag.
    The kAppServicePreferredAppsWithoutMojom flag is enabled in M105. So it
    can be removed now.
    
    BUG=1253250
    
    Change-Id: Ibf22754996909efea57ea3be7ac29690bdd9c6f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932640
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055158}

--- chrome/browser/apps/app_service/app_service_proxy_ash.cc
+++ chrome/browser/apps/app_service/app_service_proxy_ash.cc
@@ -474,10 +474,6 @@ void AppServiceProxyAsh::OnUninstallDialogClosed(
 }
 
 void AppServiceProxyAsh::InitializePreferredAppsForAllSubscribers() {
-  if (!base::FeatureList::IsEnabled(kAppServicePreferredAppsWithoutMojom)) {
-    return;
-  }
-
   AppServiceProxyBase::InitializePreferredAppsForAllSubscribers();
   if (crosapi_subscriber_ && preferred_apps_impl_) {
     crosapi_subscriber_->InitializePreferredApps(
@@ -487,10 +483,6 @@ void AppServiceProxyAsh::InitializePreferredAppsForAllSubscribers() {
 
 void AppServiceProxyAsh::OnPreferredAppsChanged(
     PreferredAppChangesPtr changes) {
-  if (!base::FeatureList::IsEnabled(kAppServicePreferredAppsWithoutMojom)) {
-    return;
-  }
-
   if (!crosapi_subscriber_) {
     AppServiceProxyBase::OnPreferredAppsChanged(std::move(changes));
     return;
--- components/services/app_service/public/cpp/features.h
+++ components/services/app_service/public/cpp/features.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
-COMPONENT_EXPORT(APP_TYPES)
-BASE_DECLARE_FEATURE(kAppServicePreferredAppsWithoutMojom);
 COMPONENT_EXPORT(APP_TYPES) BASE_DECLARE_FEATURE(kAppServiceLaunchWithoutMojom);
 COMPONENT_EXPORT(APP_TYPES)
 BASE_DECLARE_FEATURE(kAppServiceSetPermissionWithoutMojom);
