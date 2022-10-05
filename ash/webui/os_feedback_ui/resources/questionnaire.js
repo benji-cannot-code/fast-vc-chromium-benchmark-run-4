3db475512d29e6858b345665995263b51f39ed70
Bao-Duy Tran <tranbaoduy@chromium.org>
2022-10-05 01:55:05 +0000

    Avoid "type" in WiFi feedback templates.
    CrOS Essential Inputs team uses keyword search to filter user
    feedback reports for triage, and "type" (in the sense of typing
    on a keyboard) is an unavoidable keyword for us.
    
    Therefore unfortunately we're getting many unrelated "wifi" reports
    just because the word "type" appears in the template thus in all
    of those reports (in the sense of "a kind of something").
    
    drive-by: Add full stops at the end of copyright messages to conform
    to the requirements, as notified by presubmit.
    
    __SIMPLE_REVIEW__
    
    Bug: None
    Change-Id: If620c0e9bb6c932149eed0d4f735b0e6603aa2e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928456
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055077}

--- ash/webui/os_feedback_ui/resources/questionnaire.js
+++ ash/webui/os_feedback_ui/resources/questionnaire.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2022 The Chromium Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,7 +27,7 @@ const questionBluetoothOtherDevices = '[Bluetooth] Do other computer devices ' +
     '(such as non-Chrome OS devices or other Chromebooks) ' +
     'work well with this Bluetooth peripheral (such as headset or mouse)? ';
 
-const questionWifiTypeOfIssue = '[WiFi] What type of issue is this? ' +
+const questionWifiTypeOfIssue = '[WiFi] What kind of issue is this? ' +
     'Please select one or more from the below: \n' +
     '   * Failure to connect to Wi-Fi \n' +
     '   * Internet connectivity \n' +
@@ -46,7 +46,7 @@ const questionWifiNetworkWorkingBefore =
 const questionWifiOtherDevices = '[WiFi] Do other computer devices ' +
     '(such as non-Chrome OS devices or other Chromebooks) ' +
     'have the same issue using the same Wi-Fi network? ' +
-    'If so, please specify the device type. ';
+    'If so, please specify the kind of device. ';
 
 const questionCellularSim = '[Cellular] Who is your SIM card carrier? ' +
     'For example: Verizon, T-Mobile, AT&T. ';
