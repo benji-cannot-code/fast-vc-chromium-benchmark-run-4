c3b2573391c399076dd4b1d8be15cf7210828804
Jonathan Hao <phao@chromium.org>
2022-10-05 13:58:20 +0000

    [Private Network Access] Support Service Workers initiated fetches
    Project plan: https://docs.google.com/document/d/1fFSY8bExYZvKTDBBS0flry6E6Ihn63HOr0JhD2fB7ko/edit?hl=en&forcehl=1#heading=h.7nki9mck5t64
    
    The EmbeddedWorkerInstance gets its client security state from the owner
    version [1] and eventually uses it to create the loader factory, so this
    CL sets the necessary fields in the client security state in the
    ServiceWorkerVersion so that the PNA checker would have the right state.
    This fixes all test cases in service-worker-fetch.https.window.js
    
    Also, to not break the test sw-navigate-useragent.js, this CL has to
    make sure that the ip address space is stored in
    ServiceWorkerRegistrationData, so we have this information when a
    service worker is restored from DB. It was achieved by adding
    ip_address_space back to PolicyContainer mojom definition, which was
    used by ServiceWorkerRegistrationData.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:content/browser/service_worker/embedded_worker_instance.cc;l=321;drc=d3d83ab314bea231f21e36a54f2fe976dc12fadb
    
    Bug: 1239551
    Change-Id: Ibe2e76792011d055a42356cc40cf08344b69443c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885147
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055251}

--- third_party/blink/public/platform/web_policy_container.h
+++ third_party/blink/public/platform/web_policy_container.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_POLICY_CONTAINER_H_
 
 #include "services/network/public/mojom/cross_origin_embedder_policy.mojom-shared.h"
+#include "services/network/public/mojom/ip_address_space.mojom-shared.h"
 #include "services/network/public/mojom/referrer_policy.mojom-shared.h"
 #include "services/network/public/mojom/web_sandbox_flags.mojom-shared.h"
 #include "third_party/blink/public/mojom/frame/policy_container.mojom-shared.h"
@@ -27,6 +28,8 @@ struct WebPolicyContainerPolicies {
   bool is_anonymous = false;
   network::mojom::WebSandboxFlags sandbox_flags =
       network::mojom::WebSandboxFlags::kNone;
+  network::mojom::IPAddressSpace ip_address_space =
+      network::mojom::IPAddressSpace::kUnknown;
 };
 
 // TODO(antoniosartori): Remove this when CommitNavigation IPC will be handled
--- third_party/blink/renderer/core/frame/policy_container_test.cc
+++ third_party/blink/renderer/core/frame/policy_container_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/mojom/content_security_policy.mojom-blink-forward.h"
 #include "services/network/public/mojom/cross_origin_embedder_policy.mojom-blink-forward.h"
+#include "services/network/public/mojom/ip_address_space.mojom-blink-forward.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/testing/mock_policy_container_host.h"
 #include "third_party/blink/renderer/platform/network/http_parsers.h"
@@ -19,7 +20,8 @@ TEST(PolicyContainerTest, MembersAreSetDuringConstruction) {
       network::mojom::blink::CrossOriginEmbedderPolicyValue::kNone,
       network::mojom::blink::ReferrerPolicy::kNever,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>(),
-      /*anonymous=*/false, network::mojom::WebSandboxFlags::kNone);
+      /*anonymous=*/false, network::mojom::WebSandboxFlags::kNone,
+      network::mojom::blink::IPAddressSpace::kUnknown);
   PolicyContainer policy_container(host.BindNewEndpointAndPassDedicatedRemote(),
                                    std::move(policies));
 
@@ -33,7 +35,8 @@ TEST(PolicyContainerTest, UpdateReferrerPolicyIsPropagated) {
       network::mojom::blink::CrossOriginEmbedderPolicyValue::kNone,
       network::mojom::blink::ReferrerPolicy::kAlways,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>(),
-      /*anonymous=*/false, network::mojom::WebSandboxFlags::kNone);
+      /*anonymous=*/false, network::mojom::WebSandboxFlags::kNone,
+      network::mojom::blink::IPAddressSpace::kUnknown);
   PolicyContainer policy_container(host.BindNewEndpointAndPassDedicatedRemote(),
                                    std::move(policies));
 
