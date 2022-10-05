5f3c8241caa9ec9c7d52c2abe0ea82f45e4b8de1
Saeid Eid <saeideid@chromium.org>
2022-10-05 16:36:23 +0000

    [Identifiability Study] Add Support for Workers in Identifiability Study
    Workers don’t currently report surfaces of type kWebfeature of the identifiability study. This CL adds code to allow all three types of workers (dedicated, shared, and service workers) to report these surfaces.
    
    Bug: 1350408
    Change-Id: If0120bd403c6d0cc260a5cab4e61e7cfd5668180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904622
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Saeid Eid <saeideid@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055336}

--- third_party/blink/renderer/core/workers/worker_thread_test_helper.h
+++ third_party/blink/renderer/core/workers/worker_thread_test_helper.h
@@ -41,6 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "v8/include/v8.h"
 
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "third_party/blink/renderer/platform/testing/scoped_fake_ukm_recorder.h"
+
 namespace blink {
 
 class FakeWorkerGlobalScope : public WorkerGlobalScope {
@@ -53,9 +57,20 @@ class FakeWorkerGlobalScope : public WorkerGlobalScope {
                           base::TimeTicks::Now(),
                           false) {
     ReadyToRunWorkerScript();
+    GetBrowserInterfaceBroker().SetBinderForTesting(
+        ukm::mojom::UkmRecorderInterface::Name_,
+        WTF::BindRepeating(
+            [](ScopedFakeUkmRecorder* interface,
+               mojo::ScopedMessagePipeHandle handle) {
+              interface->SetHandle(std::move(handle));
+            },
+            WTF::Unretained(&scoped_fake_ukm_recorder_)));
   }
 
-  ~FakeWorkerGlobalScope() override = default;
+  ~FakeWorkerGlobalScope() override {
+    GetBrowserInterfaceBroker().SetBinderForTesting(
+        ukm::mojom::UkmRecorderInterface::Name_, {});
+  }
 
   // EventTarget
   const AtomicString& InterfaceName() const override {
@@ -112,6 +127,8 @@ class FakeWorkerGlobalScope : public WorkerGlobalScope {
   ExecutionContextToken GetExecutionContextToken() const final {
     return token_;
   }
+
+  ScopedFakeUkmRecorder scoped_fake_ukm_recorder_;
 
  private:
   SharedWorkerToken token_;
