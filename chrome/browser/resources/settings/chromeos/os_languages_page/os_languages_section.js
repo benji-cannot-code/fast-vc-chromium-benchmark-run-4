07df1cbf48b98ed27c08d04b0441f2e49e64395f
Keith Lee <keithlee@chromium.org>
2022-10-05 07:28:20 +0000

    Move the import to the correct place and sort the imports.
    This was in the wrong place in
    https://chromium-review.googlesource.com/c/chromium/src/+/3917843.
    
    As this is used in os_languages_section.html, it should be in os_languages_section.js.
    
    I'm doing this separately to simplify review and prevent invalidating the previous +1s.
    
    R=mlcui
    
    BUG=b:234790486
    
    Change-Id: If4b61a53f88551576228b242072ef62896d15ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929996
    Auto-Submit: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055161}

--- chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_section.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_section.js
@@ -11,11 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
+import './input_method_options_page.js';
 import './input_page.js';
+import './languages.js';
+import './os_japanese_manage_user_dictionary_page.js';
 import './os_languages_page_v2.js';
 import './smart_inputs_page.js';
-import './input_method_options_page.js';
-import './languages.js';
 import '../../settings_page/settings_animated_pages.js';
 import '../../settings_page/settings_subpage.js';
 import '../../settings_shared.css.js';
