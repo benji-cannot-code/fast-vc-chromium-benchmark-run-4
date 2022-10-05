0e0d17af2688fd0531043686628eb786ed8fa8c7
Tommy Martino <tmartino@google.com>
2022-10-05 23:00:26 +0000

    [PWM UI Split] Add Font Provider Api
    This patch adds a new Font Provider Api giving access to fonts shipped
    in the ChromeInternal framework. Initially this provider will be used
    for providing a custom navigation item title for Bling's Password
    Manager using the ProductSans-Regular font. Subsequent CL's will add
    the internal provider implementation and the custom title view for the
    Password Manager.
    
    The histogram added in this CL is used from the internal implementation
    of the provider to monitor for possible failures while loading the
    ProductSans-Regular font from the ChromeInternal framework.
    
    Bug: 1335156
    Change-Id: Id9328d5b7b428591ffbacb6da990ccb505e4988e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930200
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055535}

---
+++ ios/public/provider/chrome/browser/font/font_api.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_FONT_FONT_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_FONT_FONT_API_H_
+
+#import <UIKit/UIKit.h>
+
+namespace ios {
+namespace provider {
+
+// Returns a regular-weight font for use when displaying branded product names,
+// or a system default where these fonts aren't bundled.
+UIFont* GetBrandedProductRegularFont(CGFloat size);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_FONT_FONT_API_H_
