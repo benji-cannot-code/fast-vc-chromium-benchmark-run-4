7361db387436f6cfac97c0a58c49034db3d07c9a
James Cook <jamescook@chromium.org>
2022-10-05 02:47:55 +0000

    app_list: Clean up "bubble" app list browsertests
    The feature Productivity Launcher has been on-by-default since M-104.
    Remove the ScopedFeatureList that forces it on in 2 browser tests.
    
    Also rename the tests to take "bubble" out of the name, since the
    "bubble" launcher is now the default in clamshell mode.
    
    Bug: 1359022
    Change-Id: I0cc0e0a7999bf53b616b74ff3e0403c8f98c9664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931899
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055100}

--- chrome/browser/ui/ash/app_list/apps_grid_drag_browsertest.cc
+++ chrome/browser/ui/ash/app_list/apps_grid_drag_browsertest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/app_list/views/apps_grid_view_test_api.h"
-#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/accelerators.h"
 #include "ash/public/cpp/test/app_list_test_api.h"
 #include "ash/root_window_controller.h"
@@ -17,16 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "ui/events/test/event_generator.h"
 
-class BubbleAppsGridDragBrowserTest : public InProcessBrowserTest {
+class AppsGridDragBrowserTest : public InProcessBrowserTest {
  public:
-  BubbleAppsGridDragBrowserTest() {
-    scoped_feature_list_.InitAndEnableFeature(
-        ash::features::kProductivityLauncher);
-  }
-  BubbleAppsGridDragBrowserTest(const BubbleAppsGridDragBrowserTest&) = delete;
-  BubbleAppsGridDragBrowserTest& operator=(
-      const BubbleAppsGridDragBrowserTest&) = delete;
-  ~BubbleAppsGridDragBrowserTest() override = default;
+  AppsGridDragBrowserTest() = default;
+  AppsGridDragBrowserTest(const AppsGridDragBrowserTest&) = delete;
+  AppsGridDragBrowserTest& operator=(const AppsGridDragBrowserTest&) = delete;
+  ~AppsGridDragBrowserTest() override = default;
 
   // InProcessBrowserTest:
   void SetUpOnMainThread() override {
@@ -91,12 +86,11 @@ class BubbleAppsGridDragBrowserTest : public InProcessBrowserTest {
 
  private:
   ash::AppListTestApi app_list_test_api_;
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Verifies that reordering app list items by mouse drag works as expected on
 // the bubble apps grid.
-IN_PROC_BROWSER_TEST_F(BubbleAppsGridDragBrowserTest, ItemReorderByMouseDrag) {
+IN_PROC_BROWSER_TEST_F(AppsGridDragBrowserTest, ItemReorderByMouseDrag) {
   // Get the top level item ids before any operations.
   const std::vector<std::string> default_top_level_ids =
       app_list_test_api()->GetTopLevelViewIdList();
@@ -123,7 +117,7 @@ IN_PROC_BROWSER_TEST_F(BubbleAppsGridDragBrowserTest, ItemReorderByMouseDrag) {
 
 // Verifies that merging two items into a folder and moving an item out of a
 // folder work as expected on the bubble apps grid.
-IN_PROC_BROWSER_TEST_F(BubbleAppsGridDragBrowserTest, ItemMerge) {
+IN_PROC_BROWSER_TEST_F(AppsGridDragBrowserTest, ItemMerge) {
   // Record the item count before any operations.
   const size_t default_top_level_item_count =
       app_list_test_api()->GetTopLevelViewIdList().size();
