cf2fd5b20bfe73f1a4dc595bbc086fc3072df7a8
dpapad <dpapad@chromium.org>
2022-10-05 07:56:55 +0000

    Mojo bindings: Remove obsolete 'js_bindings_mode' flag.
    This flag controlled whether just "old" or both "old" and "new" (aka
    "Lite") JS bindings would be generated. It seems that the value of "old"
    is no longer used anywhere and therefore the flag is unnecessary.
    
    Will look separately into whether the "old" bindings are still used
    anywhere or whether they are unnecessarily generated.
    
    This is just part of some initial cleanups before attempting to add
    proper TypeScript support to Mojo.
    
    Bug: 1002798
    Change-Id: Iaccf23522ac51672b14f808ed8b2e1551039d993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933894
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055166}

--- mojo/public/tools/mojom/mojom/generate/generator.py
+++ mojo/public/tools/mojom/mojom/generate/generator.py
@@ -244,7 +244,6 @@ class Generator:
                variant=None,
                bytecode_path=None,
                for_blink=False,
-               js_bindings_mode="new",
                js_generate_struct_deserializers=False,
                export_attribute=None,
                export_header=None,
@@ -263,7 +262,6 @@ class Generator:
     self.variant = variant
     self.bytecode_path = bytecode_path
     self.for_blink = for_blink
-    self.js_bindings_mode = js_bindings_mode
     self.js_generate_struct_deserializers = js_generate_struct_deserializers
     self.export_attribute = export_attribute
     self.export_header = export_header
