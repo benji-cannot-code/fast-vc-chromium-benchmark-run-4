dbff821f5af8e7e6b7f252296c23cdfb7d2e8eb5
Abhijith Nair <abhijithnair@google.com>
2022-10-05 15:58:50 +0000

    Add framework to set embedder readable metadata to WebView manifest
    This CL adds a disabled service to add metadata in the webview manifest.
    A dummy service is provided to adhere with
    go/androidx-api-guidelines#resources-manifest-metadata.
    
    This can be used for feature checking. https://r.android.com/2232128 is a corresponding CL on the AndroidX side.
    
    Bug: 1355297
    Change-Id: I7326c76b09cd826638ae8dbc0fc08d38c5078301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921574
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055313}

---
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/StartupFeatureMetadataHolder.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * A placeholder service to avoid adding application-level metadata. The service
+ * is only used to expose metadata defined in the library's manifest. It is
+ * never invoked.
+ */
+public final class StartupFeatureMetadataHolder extends Service {
+    public StartupFeatureMetadataHolder() {}
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException();
+    }
+}
