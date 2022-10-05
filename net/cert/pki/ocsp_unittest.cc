6311592d799361322132b8e009a8afdc08bc9bb5
Bob Beck <bbe@google.com>
2022-10-05 21:08:04 +0000

    Convert most of pki to use std::string_view instead of base::StringPiece
    This isn't complete yet, as there are a few decisions
    yet to be made - net/der (which may move into pki) has
    been changed to support both for now, and the asn1
    utilities need to be considered yet.
    
    Continuing from 3781899 which was accidentally merged and
    reverted
    
    Bug: 1322914
    Change-Id: I6ae18a4e801b9b6002d25e8d9dbc6e6a5bcbab9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3887361
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055473}

--- net/cert/pki/ocsp_unittest.cc
+++ net/cert/pki/ocsp_unittest.cc
@@ -24,7 +24,7 @@ std::string GetFilePath(const std::string& file_name) {
   return std::string("net/data/ocsp_unittest/") + file_name;
 }
 
-scoped_refptr<ParsedCertificate> ParseCertificate(base::StringPiece data) {
+scoped_refptr<ParsedCertificate> ParseCertificate(std::string_view data) {
   CertErrors errors;
   return ParsedCertificate::Create(
       bssl::UniquePtr<CRYPTO_BUFFER>(CRYPTO_BUFFER_new(
@@ -125,7 +125,7 @@ const TestParams kTestParams[] = {
 // Parameterised test name generator for tests depending on RenderTextBackend.
 struct PrintTestName {
   std::string operator()(const testing::TestParamInfo<TestParams>& info) const {
-    base::StringPiece name(info.param.file_name);
+    std::string_view name(info.param.file_name);
     // Strip ".pem" from the end as GTest names cannot contain period.
     name.remove_suffix(4);
     return std::string(name);
@@ -179,7 +179,7 @@ TEST_P(CheckOCSPTest, FromFile) {
             der::Input(&request_data));
 }
 
-base::StringPiece kGetURLTestParams[] = {
+std::string_view kGetURLTestParams[] = {
     "http://www.example.com/",
     "http://www.example.com/path/",
     "http://www.example.com/path",
@@ -187,8 +187,8 @@ base::StringPiece kGetURLTestParams[] = {
     "http://user:pass@www.example.com/path?query",
 };
 
-class CreateOCSPGetURLTest
-    : public ::testing::TestWithParam<base::StringPiece> {};
+class CreateOCSPGetURLTest : public ::testing::TestWithParam<std::string_view> {
+};
 
 INSTANTIATE_TEST_SUITE_P(All,
                          CreateOCSPGetURLTest,
