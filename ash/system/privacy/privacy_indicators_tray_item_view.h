23d9a34e3b145afd2f371ae695b3ad530d2e4c84
Andre Le <leandre@chromium.org>
2022-10-05 19:55:15 +0000

    PrivacyIndicators: Add privacy indicators to status area a11y name
    Also make PrivacyIndicatorsTrayItemView::GetTooltipText public to make
    this possible.
    
    Change-Id: I2e673af89cb484a53c2d9e91ec92791ef678fd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923745
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055451}

--- ash/system/privacy/privacy_indicators_tray_item_view.h
+++ ash/system/privacy/privacy_indicators_tray_item_view.h
@@ -62,6 +62,9 @@ class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
   // Update the view according to the shelf alignment.
   void UpdateAlignmentForShelf(Shelf* shelf);
 
+  // TrayItemView:
+  std::u16string GetTooltipText(const gfx::Point& point) const override;
+
  private:
   friend class PrivacyIndicatorsTrayItemViewTest;
 
@@ -71,7 +74,6 @@ class ASH_EXPORT PrivacyIndicatorsTrayItemView : public TrayItemView {
   gfx::Size CalculatePreferredSize() const override;
   void OnThemeChanged() override;
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
-  std::u16string GetTooltipText(const gfx::Point& p) const override;
   views::View* GetTooltipHandlerForPoint(const gfx::Point& point) override;
   const char* GetClassName() const override;
   void AnimationProgressed(const gfx::Animation* animation) override;
