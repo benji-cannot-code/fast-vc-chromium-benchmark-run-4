671b4efc4ee29b77fe9c275c66e77b6ab5220cf1
Hira Mahmood <hiramahmood@google.com>
2022-10-05 21:04:30 +0000

    Create feature flag for App Store Rating promo testing.
    This change creates a feature flag that will be used to manually test
    the App Store Rating promo. The flag will be used to set the conditions
    that will make the user eligible to be shown the promo (to be implemented
    in a future CL).
    
    Bug: 1359065
    Change-Id: I3c02594b3849616cfbfb60a47726f2b4fc262990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936502
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055471}

--- ios/chrome/browser/ui/app_store_rating/features.h
+++ ios/chrome/browser/ui/app_store_rating/features.h
@@ -11,7 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Feature flag to enable the App Store Rating feature.
 BASE_DECLARE_FEATURE(kAppStoreRating);
 
+// Feature flag to override the trigger requirements for the App Store Rating
+// promo. This feature is enabled only during testing.
+BASE_DECLARE_FEATURE(kAppStoreRatingIgnoreEligibilityCheckTest);
+
 // Returns true if App Store Rating feature is enabled.
 bool IsAppStoreRatingEnabled();
+
+// Returns true if App Store Rating Test Engaged User feature is enabled.
+bool IsAppStoreRatingTestEngagedUserEnabled();
 
 #endif  // IOS_CHROME_BROWSER_UI_APP_STORE_RATING_FEATURES_H_
