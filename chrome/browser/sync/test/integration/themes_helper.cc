1da98c150c8905552cf789cdc7c1cde468f01409
Marc Treib <treib@chromium.org>
2022-10-05 14:08:43 +0000

    Sync: Replace THEMES integration tests
    The existing (single-client) integration tests for THEMES were quite
    poor - they just committed something and check that the local state
    remains unchanged.
    This CL adds a basic set of integration tests for both uploads and
    downloads.
    
    As a side benefit, this gets rid of UpdatedProgressMarkerChecker in
    these tests.
    
    Bug: 1170798, 1170798
    Change-Id: I7cb83f2869eedc413a97fb9f6c1f82832c80e285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929188
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055260}

--- chrome/browser/sync/test/integration/themes_helper.cc
+++ chrome/browser/sync/test/integration/themes_helper.cc
@@ -7,18 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check.h"
-#include "base/check_op.h"
 #include "base/strings/string_number_conversions.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/updater/extension_updater.h"
-#include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_extension_helper.h"
 #include "chrome/browser/themes/theme_helper.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "components/crx_file/id_util.h"
-#include "content/public/browser/notification_source.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/manifest.h"
 
@@ -33,9 +29,25 @@ ThemeService* GetThemeService(Profile* profile) {
   return ThemeServiceFactory::GetForProfile(profile);
 }
 
+bool UsingSystemThemeFunc(ThemeService* theme_service) {
+  return theme_service->UsingSystemTheme();
+}
+
+bool UsingDefaultThemeFunc(ThemeService* theme_service) {
+  return theme_service->UsingDefaultTheme();
+}
+
+bool UsingCustomThemeFunc(ThemeService* theme_service) {
+  return theme_service->GetThemeID() != ThemeHelper::kDefaultThemeID;
+}
+
 }  // namespace
 
 namespace themes_helper {
+
+bool IsSystemThemeDistinctFromDefaultTheme(Profile* profile) {
+  return GetThemeService(profile)->IsSystemThemeDistinctFromDefaultTheme();
+}
 
 std::string GetCustomTheme(int index) {
   return crx_file::id_util::GenerateId(MakeName(index));
@@ -46,15 +58,15 @@ std::string GetThemeID(Profile* profile) {
 }
 
 bool UsingCustomTheme(Profile* profile) {
-  return GetThemeID(profile) != ThemeHelper::kDefaultThemeID;
+  return UsingCustomThemeFunc(GetThemeService(profile));
 }
 
 bool UsingDefaultTheme(Profile* profile) {
-  return GetThemeService(profile)->UsingDefaultTheme();
+  return UsingDefaultThemeFunc(GetThemeService(profile));
 }
 
 bool UsingSystemTheme(Profile* profile) {
-  return GetThemeService(profile)->UsingSystemTheme();
+  return UsingSystemThemeFunc(GetThemeService(profile));
 }
 
 bool ThemeIsPendingInstall(Profile* profile, const std::string& id) {
@@ -73,16 +85,6 @@ void UseDefaultTheme(Profile* profile) {
 
 void UseSystemTheme(Profile* profile) {
   GetThemeService(profile)->UseSystemTheme();
-}
-
-// Helper function to let us bind this functionality into a callback.
-bool UsingSystemThemeFunc(ThemeService* theme_service) {
-  return theme_service->UsingSystemTheme();
-}
-
-// Helper function to let us bind this functionality into a callback.
-bool UsingDefaultThemeFunc(ThemeService* theme_service) {
-  return theme_service->UsingDefaultTheme();
 }
 
 }  // namespace themes_helper
@@ -132,13 +134,16 @@ void ThemeConditionChecker::OnThemeChanged() {
 }
 
 SystemThemeChecker::SystemThemeChecker(Profile* profile)
-    : ThemeConditionChecker(
-          profile,
-          "Waiting until profile is using system theme",
-          base::BindRepeating(&themes_helper::UsingSystemThemeFunc)) {}
+    : ThemeConditionChecker(profile,
+                            "Waiting until profile is using system theme",
+                            base::BindRepeating(&UsingSystemThemeFunc)) {}
 
 DefaultThemeChecker::DefaultThemeChecker(Profile* profile)
-    : ThemeConditionChecker(
-          profile,
-          "Waiting until profile is using default theme",
-          base::BindRepeating(&themes_helper::UsingDefaultThemeFunc)) {}
+    : ThemeConditionChecker(profile,
+                            "Waiting until profile is using default theme",
+                            base::BindRepeating(&UsingDefaultThemeFunc)) {}
+
+CustomThemeChecker::CustomThemeChecker(Profile* profile)
+    : ThemeConditionChecker(profile,
+                            "Waiting until profile is using a custom theme",
+                            base::BindRepeating(&UsingCustomThemeFunc)) {}
--- chrome/browser/sync/test/integration/themes_helper.h
+++ chrome/browser/sync/test/integration/themes_helper.h
@@ -8,17 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/sync/test/integration/status_change_checker.h"
-#include "chrome/browser/sync/test/integration/sync_test.h"
 #include "chrome/browser/themes/theme_service_observer.h"
 
 class Profile;
 class ThemeService;
 
 namespace themes_helper {
+
+bool IsSystemThemeDistinctFromDefaultTheme(Profile* profile);
 
 // Gets the unique ID of the custom theme with the given index.
 [[nodiscard]] std::string GetCustomTheme(int index);
@@ -112,6 +113,13 @@ class SystemThemeChecker : public ThemeConditionChecker {
 class DefaultThemeChecker : public ThemeConditionChecker {
  public:
   explicit DefaultThemeChecker(Profile* profile);
+};
+
+// Waits until |profile| is using a custom theme.
+// Returns false in case of timeout.
+class CustomThemeChecker : public ThemeConditionChecker {
+ public:
+  explicit CustomThemeChecker(Profile* profile);
 };
 
 #endif  // CHROME_BROWSER_SYNC_TEST_INTEGRATION_THEMES_HELPER_H_
