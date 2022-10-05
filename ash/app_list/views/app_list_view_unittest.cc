b5b9629ad42fb179bd7b062dcd039bf0d2578384
Ana Salazar <anasalazar@chromium.org>
2022-10-05 22:55:39 +0000

    Remove AppListViewTabletTest suite
    Previously, the test suite used to verify the behaviour for tablet mode
    launcher with and without productivity launcher enabled.
    
    After M104, Legacy launcher code is set to be removed so verifying this
    behaviour with legacy launcher is pointless.
    
    Remove AppListViewTabletTest suite and move related tests into AppListViewTest.
    
    Bug: 1360501
    Change-Id: Ib1bcf72e4915d9f61265350c0db9ef47ce367f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935531
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055530}

--- ash/app_list/views/app_list_view_unittest.cc
+++ ash/app_list/views/app_list_view_unittest.cc
@@ -539,23 +539,6 @@ class AppListViewPeekingTest : public AppListViewTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// Tests for tablet mode. Parameterized by ProductivityLauncher.
-class AppListViewTabletTest : public AppListViewTest,
-                              public testing::WithParamInterface<bool> {
- public:
-  AppListViewTabletTest() {
-    const bool enable_productivity_launcher = GetParam();
-    feature_list_.InitWithFeatureState(features::kProductivityLauncher,
-                                       enable_productivity_launcher);
-  }
-
-  base::test::ScopedFeatureList feature_list_;
-};
-
-INSTANTIATE_TEST_SUITE_P(ProductivityLauncher,
-                         AppListViewTabletTest,
-                         testing::Bool());
-
 // Tests app list view layout for different screen sizes.
 class AppListViewScalableLayoutTest : public AppListViewTest {
  public:
@@ -1618,7 +1601,7 @@ TEST_F(AppListViewPeekingTest, EscapeKeySideShelfFullscreenToClosed) {
 }
 
 // Tests that pressing escape when in tablet mode keeps app list in fullscreen.
-TEST_P(AppListViewTabletTest, EscapeKeyTabletModeStayFullscreen) {
+TEST_F(AppListViewTest, EscapeKeyTabletModeStayFullscreen) {
   // Put into fullscreen by using tablet mode.
   Initialize(/*is_tablet_mode=*/true);
 
@@ -1735,7 +1718,7 @@ TEST_F(AppListViewPeekingTest, DisplayTest) {
 }
 
 // As above above, but tests tablet mode with and without ProductivityLauncher.
-TEST_P(AppListViewTabletTest, DisplayTest) {
+TEST_F(AppListViewTest, DisplayTest) {
   Initialize(/*is_tablet_mode=*/true);
   EXPECT_EQ(-1, GetPaginationModel()->total_pages());
   delegate_->GetTestModel()->PopulateApps(kInitialItems);
@@ -1863,7 +1846,7 @@ TEST_F(AppListViewTest, DISABLED_SearchResultsTest) {
 }
 
 // Tests that a context menu can be shown between app icons in tablet mode.
-TEST_P(AppListViewTabletTest, ShowContextMenuBetweenAppsInTabletMode) {
+TEST_F(AppListViewTest, ShowContextMenuBetweenAppsInTabletMode) {
   Initialize(true /*is_tablet_mode*/);
   delegate_->GetTestModel()->PopulateApps(kInitialItems);
   Show();
@@ -2022,7 +2005,7 @@ TEST_F(AppListViewTest, InitialPageResetClamshellModeTest) {
 
 // Tests that, in tablet mode, the current app list page doesn't immediately
 // reset to the initial page when app list is closed and re-opened.
-TEST_P(AppListViewTabletTest, PagePersistanceTabletModeTest) {
+TEST_F(AppListViewTest, PagePersistanceTabletModeTest) {
   Initialize(true /*is_tablet_mode*/);
 
   AppListTestModel* model = delegate_->GetTestModel();
