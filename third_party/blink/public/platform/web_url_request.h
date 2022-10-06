c1235953c89d85126803cb9e86658b6ce4e96d4b
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-10-06 21:46:01 +0000

    Expose WebURLRequest::ConvertToNetPriority
    To use the conversion outside Blink in
    https://chromium-review.googlesource.com/c/chromium/src/+/3935183.
    
    This CL also
    
    - Merges multiple almost identical
      ConvertWebKitPriorityToNetPriority() implementations,
    - Renamed them as Blink is no longer WebKit, and
    - Updated the comments.
    
    Bug: 1369823
    Change-Id: Ibf6859ad1e69cf749d79dffcbb26f49e386c2bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936409
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056030}

--- third_party/blink/public/platform/web_url_request.h
+++ third_party/blink/public/platform/web_url_request.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
+#include "net/base/request_priority.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "ui/base/page_transition_types.h"
@@ -89,6 +90,7 @@ class BLINK_PLATFORM_EXPORT WebURLRequest {
     kLowest = kVeryLow,
     kHighest = kVeryHigh,
   };
+  static net::RequestPriority ConvertToNetPriority(WebURLRequest::Priority);
 
   ~WebURLRequest();
   WebURLRequest();
