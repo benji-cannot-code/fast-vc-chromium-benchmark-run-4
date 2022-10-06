e7444267b62902f4ba21cae5bb005f052d48b15d
Tibor Goldschwendt <tiborg@chromium.org>
2022-10-06 18:48:37 +0000

    Revert "WebUI: Support sanitizing animated images"
    This reverts commit e45665930163066cbb77122a82fb501c2112f436.
    
    Reason for revert: breaks promo image (crbug/1371657)
    
    Original change's description:
    > WebUI: Support sanitizing animated images
    >
    > This change adds support to serve sanitized animated images in ChromeOS
    > WebUI.
    >
    > gfx API cl: https://chromium-review.googlesource.com/c/chromium/src/+/3874501
    > skia API cl: https://skia-review.googlesource.com/c/skia/+/572935
    >
    > For detailed background: go/avatars-from-cloud
    >
    > Bug: b/243200556
    > Test: Run existing tests
    > Change-Id: Id59e2510789f66707017275fd45ad9efcfb1651a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885348
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: Yue Li <updowndota@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1046588}
    
    Bug: b/243200556
    Bug: 1371657
    Change-Id: I646cf524604566b8a743e629824083a4e738bd0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935279
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055930}

--- chrome/browser/ui/webui/sanitized_image_source.h
+++ chrome/browser/ui/webui/sanitized_image_source.h
@@ -12,12 +12,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "content/public/browser/url_data_source.h"
-#include "services/data_decoder/public/cpp/data_decoder.h"
-#include "services/data_decoder/public/cpp/decode_image.h"
-#include "services/data_decoder/public/mojom/image_decoder.mojom.h"
 
 class Profile;
-class SkBitmap;
+
+namespace gfx {
+class Image;
+}  // namespace gfx
+
+namespace image_fetcher {
+class ImageDecoder;
+}  // namespace image_fetcher
 
 namespace network {
 class SharedURLLoaderFactory;
@@ -45,31 +49,12 @@ class IdentityManager;
 //   chrome://image?url=<external image URL>&isGooglePhotos=true
 class SanitizedImageSource : public content::URLDataSource {
  public:
-  using DecodeAnimationCallback =
-      data_decoder::mojom::ImageDecoder::DecodeAnimationCallback;
-
-  // A delegate class that is faked out for testing purposes.
-  class DataDecoderDelegate {
-   public:
-    DataDecoderDelegate() = default;
-    virtual ~DataDecoderDelegate() = default;
-
-    virtual void DecodeAnimation(const std::string& data,
-                                 DecodeAnimationCallback callback);
-
-   private:
-    // The instance of the Data Decoder used by this DataDecoderDelegate to
-    // perform any image decoding operations. The underlying service instance is
-    // started lazily when needed and torn down when not in use.
-    data_decoder::DataDecoder data_decoder_;
-  };
-
   explicit SanitizedImageSource(Profile* profile);
   // This constructor lets us pass mock dependencies for testing.
   SanitizedImageSource(
       Profile* profile,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      std::unique_ptr<DataDecoderDelegate> delegate);
+      std::unique_ptr<image_fetcher::ImageDecoder> image_decoder);
   SanitizedImageSource(const SanitizedImageSource&) = delete;
   SanitizedImageSource& operator=(const SanitizedImageSource&) = delete;
   ~SanitizedImageSource() override;
@@ -96,23 +81,15 @@ class SanitizedImageSource : public content::URLDataSource {
   void OnImageLoaded(std::unique_ptr<network::SimpleURLLoader> loader,
                      content::URLDataSource::GotDataCallback callback,
                      std::unique_ptr<std::string> body);
-  void OnAnimationDecoded(
-      content::URLDataSource::GotDataCallback callback,
-      std::vector<data_decoder::mojom::AnimationFramePtr> mojo_frames);
-
-  void EncodeAndReplyStaticImage(
-      content::URLDataSource::GotDataCallback callback,
-      const SkBitmap& bitmap);
-  void EncodeAndReplyAnimatedImage(
-      content::URLDataSource::GotDataCallback callback,
-      std::vector<data_decoder::mojom::AnimationFramePtr> mojo_frames);
+  void OnImageDecoded(content::URLDataSource::GotDataCallback callback,
+                      const gfx::Image& image);
 
   // Owned by `IdentityManagerFactory` or `IdentityTestEnvironment`.
   raw_ptr<signin::IdentityManager> identity_manager_;
 
   const scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
 
-  std::unique_ptr<DataDecoderDelegate> data_decoder_delegate_;
+  std::unique_ptr<image_fetcher::ImageDecoder> image_decoder_;
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<SanitizedImageSource> weak_ptr_factory_{this};
