d228101032c9cace6f961b868e6ed21e1e0117ca
Alan Cutter <alancutter@chromium.org>
2022-10-06 18:08:02 +0000

    Replace GetWeakPtr() with GetMutableWeakPtr() in /chrome/browser/ash/eche_app
    This CL replaces uses of GetWeakPtr() with GetMutableWeakPtr() in places
    that expect a WeakPtr<T> while in a const T context. This helps make the
    const -> non-const transition more explicit.
    
    This CL was uploaded by git cl split.
    
    R=jonmann@chromium.org
    
    Bug: 1366168
    Change-Id: Icaad641b4bb5949c0e22a158adcdbf2b018dda57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934656
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055893}

--- chrome/browser/ash/eche_app/eche_app_manager_factory.cc
+++ chrome/browser/ash/eche_app/eche_app_manager_factory.cc
@@ -267,9 +267,9 @@ KeyedService* EcheAppManagerFactory::BuildServiceInstanceFor(
       std::move(presence_monitor_client),
       base::BindRepeating(&EcheAppManagerFactory::LaunchEcheApp, profile),
       base::BindRepeating(&EcheAppManagerFactory::ShowNotification,
-                          weak_ptr_factory_.GetWeakPtr(), profile),
+                          weak_ptr_factory_.GetMutableWeakPtr(), profile),
       base::BindRepeating(&EcheAppManagerFactory::CloseNotification,
-                          weak_ptr_factory_.GetWeakPtr(), profile));
+                          weak_ptr_factory_.GetMutableWeakPtr(), profile));
 }
 
 std::unique_ptr<SystemInfo> EcheAppManagerFactory::GetSystemInfo(
