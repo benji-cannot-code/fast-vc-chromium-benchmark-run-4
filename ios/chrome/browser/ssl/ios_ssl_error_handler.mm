c085856cd6150ae7e71d16f2f6723c06fa9a09fe
Sylvain Defresne <sdefresne@chromium.org>
2022-10-06 09:50:20 +0000

    [ios] Remove unused deprecated constant
    The internal code has been updated to not use the deprecated constant
    kSSLInterstitialDelayInSeconds so it can safely be deleted.
    
    Bug: none
    Change-Id: I7f9d71b790625c83aa736f102c1e583688913a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936152
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055690}

--- ios/chrome/browser/ssl/ios_ssl_error_handler.mm
+++ ios/chrome/browser/ssl/ios_ssl_error_handler.mm
@@ -30,12 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-// Keep this variable as it is used in the internal repository. Remove it
-// once the internal code has been converted to use kSSLInterstitialDelay
-// instead.
-const int64_t kSSLInterstitialDelayInSeconds =
-    kSSLInterstitialDelay.InSeconds();
-
 using captive_portal::CaptivePortalDetector;
 using security_interstitials::IOSBlockingPageTabHelper;
 
