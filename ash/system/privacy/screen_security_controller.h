63e7d8bb2c695365b46603d417c9d4d374a779bc
Andre Le <leandre@chromium.org>
2022-10-06 18:00:02 +0000

    PrivacyIndicators: Add screen share icon to privacy indicators view
    Add a screen share indicator to PrivacyIndicatorsTrayItemView, as well
    as update it accordingly when screen sharing status changes.
    
    We also change ScreenSecurityNotificationController to be
    ScreenSecurityController since this class now also handles
    non-notification related logic.
    
    Bug: 1357853
    Change-Id: Iab7c402a14db68c8d3310c1e2324057c907d2ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933380
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055885}

--- ash/system/privacy/screen_security_controller.h
+++ ash/system/privacy/screen_security_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_NOTIFICATION_CONTROLLER_H_
-#define ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_NOTIFICATION_CONTROLLER_H_
+#ifndef ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_CONTROLLER_H_
+#define ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_CONTROLLER_H_
 
 #include <string>
 #include <vector>
@@ -22,19 +22,16 @@ extern ASH_EXPORT const char kNotifierScreenCapture[];
 extern ASH_EXPORT const char kNotifierScreenShare[];
 
 // Controller class to manage screen security notifications.
-class ASH_EXPORT ScreenSecurityNotificationController
-    : public ScreenCaptureObserver,
-      public ScreenShareObserver,
-      public ShellObserver {
+class ASH_EXPORT ScreenSecurityController : public ScreenCaptureObserver,
+                                            public ScreenShareObserver,
+                                            public ShellObserver {
  public:
-  ScreenSecurityNotificationController();
+  ScreenSecurityController();
 
-  ScreenSecurityNotificationController(
-      const ScreenSecurityNotificationController&) = delete;
-  ScreenSecurityNotificationController& operator=(
-      const ScreenSecurityNotificationController&) = delete;
+  ScreenSecurityController(const ScreenSecurityController&) = delete;
+  ScreenSecurityController& operator=(const ScreenSecurityController&) = delete;
 
-  ~ScreenSecurityNotificationController() override;
+  ~ScreenSecurityController() override;
 
  private:
   void CreateNotification(const std::u16string& message, bool is_capture);
@@ -70,10 +67,9 @@ class ASH_EXPORT ScreenSecurityNotificationController
   std::vector<base::OnceClosure> share_stop_callbacks_;
   base::RepeatingClosure change_source_callback_;
 
-  base::WeakPtrFactory<ScreenSecurityNotificationController> weak_ptr_factory_{
-      this};
+  base::WeakPtrFactory<ScreenSecurityController> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_NOTIFICATION_CONTROLLER_H_
+#endif  // ASH_SYSTEM_PRIVACY_SCREEN_SECURITY_CONTROLLER_H_
--- ash/system/system_notification_controller.cc
+++ ash/system/system_notification_controller.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/network/managed_sim_lock_notifier.h"
 #include "ash/system/network/wifi_toggle_notification_controller.h"
 #include "ash/system/power/power_notification_controller.h"
-#include "ash/system/privacy/screen_security_notification_controller.h"
+#include "ash/system/privacy/screen_security_controller.h"
 #include "ash/system/session/session_limit_notification_controller.h"
 #include "ash/system/tracing_notification_controller.h"
 #include "ash/system/update/update_notification_controller.h"
@@ -32,8 +32,7 @@ SystemNotificationController::SystemNotificationController()
           std::make_unique<GestureEducationNotificationController>()),
       power_(std::make_unique<PowerNotificationController>(
           message_center::MessageCenter::Get())),
-      screen_security_(
-          std::make_unique<ScreenSecurityNotificationController>()),
+      screen_security_(std::make_unique<ScreenSecurityController>()),
       session_limit_(std::make_unique<SessionLimitNotificationController>()),
       tracing_(std::make_unique<TracingNotificationController>()),
       update_(std::make_unique<UpdateNotificationController>()),
