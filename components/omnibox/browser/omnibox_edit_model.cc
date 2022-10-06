686db86b7f05d193f42d670c42b1414d1f63cc9b
Orin Jaworski <orinj@google.com>
2022-10-06 20:31:39 +0000

    [omnibox] Fix stale focus indicators on popup selection change
    This CL updates popup appearance after every popup selection change
    so that it always appears up to date with new selection state.
    Previously it was possible to change selection without popup appearance
    change, and in such cases the UI would look stale and show incorrect
    focus states. See fixed bug for details.
    
    Fixed: 1369229
    Change-Id: Ica83e2480b9df52c84d9b776d617a9bb1ee2c7dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934123
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055984}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -1985,6 +1985,8 @@ void OmniboxEditModel::SetPopupSelection(OmniboxPopupSelection new_selection,
                          std::u16string());
     }
   }
+  // Without this, focus indicators may appear stale (see crbug.com/1369229).
+  popup_view_->UpdatePopupAppearance();
 }
 
 OmniboxPopupSelection OmniboxEditModel::StepPopupSelection(
