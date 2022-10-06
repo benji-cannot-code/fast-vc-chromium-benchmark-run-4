02a5289d2e8a54c4a736fcb69f772efac026aef4
Alan Cutter <alancutter@chromium.org>
2022-10-06 08:56:48 +0000

    Remove const qualifier on GetWeakPtr() helper in /components/web_package
    This CL removes an unnecessary const -> non-const transition for
    WebBundleURLLoaderFactory.
    
    This CL was uploaded by git cl split.
    
    R=ksakamoto@chromium.org
    
    Bug: 1366168
    Change-Id: I8c48b999102a7280364fd9066010bfbabafa3118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933928
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055672}

--- components/web_package/web_bundle_url_loader_factory.h
+++ components/web_package/web_bundle_url_loader_factory.h
@@ -55,7 +55,7 @@ class WebBundleURLLoaderFactory {
   WebBundleURLLoaderFactory& operator=(const WebBundleURLLoaderFactory&) =
       delete;
 
-  base::WeakPtr<WebBundleURLLoaderFactory> GetWeakPtr() const;
+  base::WeakPtr<WebBundleURLLoaderFactory> GetWeakPtr();
 
   void SetBundleStream(mojo::ScopedDataPipeConsumerHandle body);
   void ReportErrorAndCancelPendingLoaders(
