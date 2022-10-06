7dd247ca789c8145eecac13a930b925d3b97e111
Alexander Bolodurin <alexbn@chromium.org>
2022-10-06 04:15:15 +0000

    fileSystemProvider: port copy_entry test to service workers
    Bug: b:235414571
    Change-Id: I96288890e0dd9cc3a83794ebb9928144744b0ded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934665
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055599}

---
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/copy_entry/sw.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {TestFileSystemProvider} from '/_test_resources/api_test/file_system_provider/service_worker/provider.js';
+
+const provider =
+    new TestFileSystemProvider(TestFileSystemProvider.FILESYSTEM_ID);
+
+provider.setUpProviderListeners();
+provider.setUpCommandListener(self);
