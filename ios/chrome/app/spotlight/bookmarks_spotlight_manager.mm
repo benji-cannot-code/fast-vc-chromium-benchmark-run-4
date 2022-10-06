8b058de5f6490aaa2cd005687df71094c356fac1
Sylvain Defresne <sdefresne@chromium.org>
2022-10-06 10:32:55 +0000

    [ios] Convert time duration constants to base::TimeDelta
    Using base::TimeDelta allow to specify the time unit when defining
    the constant without resorting to putting the unit in the variable
    name.
    
    This also helps converting between different units with the helper
    methods (.InSecondsF(), .InNanosecondsF(), ...).
    
    This CL converts //ios/chrome/app/spotlight.
    
    Bug: none
    Change-Id: Ie12cfd9d5abe401553f4071e03a258a14728f6e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934735
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Éric Noyau <noyau@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055702}

--- ios/chrome/app/spotlight/bookmarks_spotlight_manager.mm
+++ ios/chrome/app/spotlight/bookmarks_spotlight_manager.mm
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <CoreSpotlight/CoreSpotlight.h>
 
+#import "base/mac/foundation_util.h"
 #import "base/metrics/histogram_macros.h"
 #import "base/strings/sys_string_conversions.h"
+#import "base/time/time.h"
 #import "base/version.h"
 #import "components/bookmarks/browser/base_bookmark_model_observer.h"
 #import "components/bookmarks/browser/bookmark_model.h"
@@ -27,7 +29,7 @@ namespace {
 const int kMaxInitialIndexSize = 1000;
 
 // Minimum delay between two global indexing of bookmarks.
-const int kDelayBetweenTwoIndexingInSeconds = 7 * 86400;  // One week.
+const base::TimeDelta kDelayBetweenTwoIndexing = base::Days(7);
 
 }  // namespace
 
@@ -231,23 +233,25 @@ initWithLargeIconService:(favicon::LargeIconService*)largeIconService
 }
 
 - (BOOL)shouldReindex {
-  NSDate* date = [[NSUserDefaults standardUserDefaults]
-      objectForKey:@(spotlight::kSpotlightLastIndexingDateKey)];
+  NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+
+  NSDate* date = base::mac::ObjCCast<NSDate>(
+      [userDefaults objectForKey:@(spotlight::kSpotlightLastIndexingDateKey)]);
   if (!date) {
     return YES;
   }
-  NSDate* expirationDate =
-      [date dateByAddingTimeInterval:kDelayBetweenTwoIndexingInSeconds];
-  if ([expirationDate compare:[NSDate date]] == NSOrderedAscending) {
+  const base::TimeDelta timeSinceLastIndexing =
+      base::Time::Now() - base::Time::FromNSDate(date);
+  if (timeSinceLastIndexing >= kDelayBetweenTwoIndexing) {
     return YES;
   }
-  NSNumber* lastIndexedVersionString = [[NSUserDefaults standardUserDefaults]
-      objectForKey:@(spotlight::kSpotlightLastIndexingVersionKey)];
-  if (!lastIndexedVersionString) {
+  NSNumber* lastIndexedVersion = base::mac::ObjCCast<NSNumber>([userDefaults
+      objectForKey:@(spotlight::kSpotlightLastIndexingVersionKey)]);
+  if (!lastIndexedVersion) {
     return YES;
   }
 
-  if ([lastIndexedVersionString integerValue] <
+  if ([lastIndexedVersion integerValue] <
       spotlight::kCurrentSpotlightIndexVersion) {
     return YES;
   }
@@ -329,18 +333,18 @@ initWithLargeIconService:(favicon::LargeIconService*)largeIconService
 }
 
 - (void)completedClearAllSpotlightItems {
-  NSDate* startOfReindexing = [NSDate date];
+  const base::Time startOfReindexing = base::Time::Now();
   _nodesIndexed = 0;
   [self refreshNodeInIndex:_bookmarkModel->root_node() initial:YES];
-  NSDate* endOfReindexing = [NSDate date];
-  NSTimeInterval indexingDuration =
-      [endOfReindexing timeIntervalSinceDate:startOfReindexing];
+  const base::Time endOfReindexing = base::Time::Now();
+
   UMA_HISTOGRAM_TIMES("IOS.Spotlight.BookmarksIndexingDuration",
-                      base::Milliseconds(1000 * indexingDuration));
+                      endOfReindexing - startOfReindexing);
   UMA_HISTOGRAM_COUNTS_1000("IOS.Spotlight.BookmarksInitialIndexSize",
                             [self pendingLargeIconTasksCount]);
+
   [[NSUserDefaults standardUserDefaults]
-      setObject:endOfReindexing
+      setObject:endOfReindexing.ToNSDate()
          forKey:@(spotlight::kSpotlightLastIndexingDateKey)];
 
   [[NSUserDefaults standardUserDefaults]
