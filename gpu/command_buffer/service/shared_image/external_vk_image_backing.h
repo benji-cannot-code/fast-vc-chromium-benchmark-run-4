e23201a1c6e8f6269732b96d6a6e0ac08bcfbd27
Saifuddin Hitawala <hitawala@chromium.org>
2022-10-06 01:27:27 +0000

    Use base::flat_map for ExternalVkImageBacking image_usage_cache
    Use base::flat_map mapping from VkFormat to VkImageUsageFlags for the image_usage_cache_ in ExternalVkImageBacking. Earlier it was an
    array of ResourceFormat enum size mapping ResourceFormat to the usage
    but VkFormat should be used. Also remove the VulkanImageUsageCache
    struct wrapper.
    
    Bug: 1366477
    Change-Id: Ia9258e26d9b2aede8e7ad271e2031278d11172d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933840
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055585}

--- gpu/command_buffer/service/shared_image/external_vk_image_backing.h
+++ gpu/command_buffer/service/shared_image/external_vk_image_backing.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/types/pass_key.h"
@@ -28,11 +29,6 @@ namespace gpu {
 class VulkanCommandPool;
 class VulkanImage;
 
-struct VulkanImageUsageCache {
-  // Maximal usage flags for VK_IMAGE_TILING_OPTIMAL each ResourceFormat.
-  VkImageUsageFlags optimal_tiling_usage[viz::RESOURCE_FORMAT_MAX + 1];
-};
-
 class ExternalVkImageBacking final : public ClearTrackingSharedImageBacking {
  public:
   static std::unique_ptr<ExternalVkImageBacking> Create(
@@ -45,7 +41,7 @@ class ExternalVkImageBacking final : public ClearTrackingSharedImageBacking {
       GrSurfaceOrigin surface_origin,
       SkAlphaType alpha_type,
       uint32_t usage,
-      const VulkanImageUsageCache* image_usage_cache,
+      const base::flat_map<VkFormat, VkImageUsageFlags>& image_usage_cache,
       base::span<const uint8_t> pixel_data,
       bool using_gmb = false);
 
@@ -59,8 +55,7 @@ class ExternalVkImageBacking final : public ClearTrackingSharedImageBacking {
       const gfx::ColorSpace& color_space,
       GrSurfaceOrigin surface_origin,
       SkAlphaType alpha_type,
-      uint32_t usage,
-      const VulkanImageUsageCache* image_usage_cache);
+      uint32_t usage);
 
   ExternalVkImageBacking(base::PassKey<ExternalVkImageBacking>,
                          const Mailbox& mailbox,
