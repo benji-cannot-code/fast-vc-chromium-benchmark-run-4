031024595913ccbed4aad67fc7a56a54e1fb2a41
Noah Rose Ledesma <noahrose@google.com>
2022-10-06 01:16:05 +0000

    Implement file utility functions for Linux
    Linux updater data is stored in /opt/ for system installs and ~/.local
    for user installs. Additionally, the POSIX-compatible Mac implementation
    of `PathOwnedByUser` was moved to a location shared by Linux code paths.
    
    Fixed: 1371496
    Change-Id: Ia12e2b91b6527ca56a7e606bf248e3f01d2e8463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934420
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055583}

---
+++ chrome/updater/linux/linux_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_LINUX_LINUX_UTIL_H_
+#define CHROME_UPDATER_LINUX_LINUX_UTIL_H_
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace base {
+class FilePath;
+}  // namespace base
+
+namespace updater {
+enum class UpdaterScope;
+
+// For user installations returns a path to the "~/.local" for the logged in
+// user. For system installations returns "/opt/".
+absl::optional<base::FilePath> GetApplicationDataDirectory(UpdaterScope scope);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_LINUX_LINUX_UTIL_H_
