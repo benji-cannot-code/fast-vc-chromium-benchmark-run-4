92d0b350987bd720b0ae3ecfab89d7a114f0ace5
dpapad <dpapad@chromium.org>
2022-10-06 19:54:41 +0000

    WebUI: Remove web_ui_test.mojom.js, not actually used.
    There are two types of Mojo WebUI tests
    
     1) mojo_webui: Uses web_ui_test.mojom-webui.js
     2) mojo_lite_webui: Uses web_ui_test.mojom-lite.js
    
    web_ui_test.mojom.js is a left-over from pre-"lite" JS bindings, and
    is not actually used anywhere.
    
    Also removing test_api.js codeptah referring to the pre-"lite" Mojo
    bindings.
    
    Bug: 1179821
    Change-Id: Ib7deefbee97fe50544e05d73a27ccffbb5a4f95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933895
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055970}

--- chrome/test/base/mojo_web_ui_browser_test.h
+++ chrome/test/base/mojo_web_ui_browser_test.h
@@ -17,7 +17,6 @@ class MojoWebUIBrowserTest : public BaseWebUIBrowserTest {
   ~MojoWebUIBrowserTest() override;
 
   void set_use_mojo_modules() { use_mojo_modules_ = true; }
-  void set_use_mojo_lite_bindings() { use_mojo_lite_bindings_ = true; }
 
   // WebUIBrowserTest:
   void BrowsePreload(const GURL& browse_to) override;
@@ -29,7 +28,6 @@ class MojoWebUIBrowserTest : public BaseWebUIBrowserTest {
   std::unique_ptr<WebUITestContentBrowserClient> test_content_browser_client_;
 
   bool use_mojo_modules_ = false;
-  bool use_mojo_lite_bindings_ = false;
 };
 
 #endif  // CHROME_TEST_BASE_MOJO_WEB_UI_BROWSER_TEST_H_
