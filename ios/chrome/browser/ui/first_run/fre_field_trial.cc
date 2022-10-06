0d0258283c948c0a60e7d25b5784b2e194679a8f
ginnnnnnny <ginnyhuang@chromium.org>
2022-10-06 08:11:37 +0000

    [iOS] Launch FRE default browser - short delay
    This CL launches the FRE experiment arm "short delay", and the code
    changes include
    
    1) Enable the FRE default browser feature by default
    2) Change the default variant (arm) to the short delay instead of the
    default delay.
    
    Users can still control this feature and arm through chrome://flags.
    
    A follow up CL would be created to remove all FRE default browser
    feature and experiment code, including the chrome://flags toggle.
    
    Bug: 1371706
    Change-Id: I7112544d710c5b2e5a3ace76a404bd8760f341a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936211
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055653}

--- ios/chrome/browser/ui/first_run/fre_field_trial.cc
+++ ios/chrome/browser/ui/first_run/fre_field_trial.cc
@@ -65,7 +65,7 @@ constexpr base::FeatureParam<NewDefaultBrowserPromoFRE>::Option
 constexpr base::FeatureParam<NewDefaultBrowserPromoFRE>
     kNewDefaultBrowserPromoFREParam{&kEnableFREDefaultBrowserPromoScreen,
                                     kFREDefaultBrowserPromoParam,
-                                    NewDefaultBrowserPromoFRE::kDefaultDelay,
+                                    NewDefaultBrowserPromoFRE::kShortDelay,
                                     &kNewDefaultBrowserPromoFREOptions};
 
 // Options for kkNewMobileIdentityConsistencyFREParam.
