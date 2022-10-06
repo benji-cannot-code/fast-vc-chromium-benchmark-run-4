feae952ad52e664310e163ada6dacac921181109
Brahim Chikhaoui <bchikhaoui@google.com>
2022-10-06 07:49:37 +0000

    [OOBE] Enrollment attribute-prompt-step ShelfButton issue
    Hide the shelfBUtton “Use as personal device” for the Enrollment
    Screen in the step attribute-prompt for optional Asset identifier
    and Device Location.
    
    BUG= b:249016433
    
    Change-Id: I6deed7443eb17507395c34e6baf9a893d8b1b848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933012
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055641}

--- ash/public/cpp/login_types.h
+++ ash/public/cpp/login_types.h
@@ -44,7 +44,7 @@ enum class OobeDialogState {
   PASSWORD_CHANGED = 6,
 
   // Showing device enrollment screen.
-  ENROLLMENT = 7,
+  ENROLLMENT_CANCEL_DISABLED = 7,
 
   // Showing error screen.
   ERROR = 8,
@@ -81,6 +81,7 @@ enum class OobeDialogState {
 
   // Showing marketing opt-in screen.
   MARKETING_OPT_IN = 19,
+
 };
 
 // Modes of the managed device, which is used to update the visibility of
