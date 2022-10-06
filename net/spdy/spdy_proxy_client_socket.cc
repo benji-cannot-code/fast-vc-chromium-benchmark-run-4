b85ef4fd2962bd0877436a9477f2a54f69136c71
Alan Cutter <alancutter@chromium.org>
2022-10-06 20:46:34 +0000

    Replace GetWeakPtr() with GetMutableWeakPtr() in /net/spdy
    This CL replaces uses of GetWeakPtr() with GetMutableWeakPtr() in places
    that expect a WeakPtr<T> while in a const T context. This helps make the
    const -> non-const transition more explicit.
    
    This CL was uploaded by git cl split.
    
    R=rch@chromium.org
    
    Bug: 1366168
    Change-Id: If0da439471ce983686673a2e2d1f09aa8ad04448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936136
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055989}

--- net/spdy/spdy_proxy_client_socket.cc
+++ net/spdy/spdy_proxy_client_socket.cc
@@ -286,7 +286,7 @@ void SpdyProxyClientSocket::RunWriteCallback(CompletionOnceCallback callback,
   if (end_stream_state_ == EndStreamState::kEndStreamReceived) {
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::BindOnce(&SpdyProxyClientSocket::MaybeSendEndStream,
-                                  weak_factory_.GetWeakPtr()));
+                                  weak_factory_.GetMutableWeakPtr()));
   }
 }
 
