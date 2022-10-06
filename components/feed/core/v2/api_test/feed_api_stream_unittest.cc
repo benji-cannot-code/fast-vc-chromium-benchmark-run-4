78c2267196468847205c8043d9ba57321f710da6
Daniel Cheng <dcheng@chromium.org>
2022-10-06 16:03:39 +0000

    Use base::test::FeatureRef for feature vectors in //components/feed.
    A base::Feature should not be copyable; a feature is intended to have a
    single global instance, as it caches internal mutable state.
    
    Feature vectors in conjunction with `base::test::ScopedFeatureList` are
    by far the most common use of the base::Feature copy constructor. This
    is a mostly automated replacement of `std::vector<base::Feature>` with
    `std::vector<base::test::FeatureRef>` to remove that dependency.
    
    This CL was uploaded by git cl split.
    
    R=harringtond@chromium.org
    
    Bug: 1370572
    Change-Id: I2d38f8ea30b27c2eef19d34ad2042ff1c21971a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931925
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055818}

--- components/feed/core/v2/api_test/feed_api_stream_unittest.cc
+++ components/feed/core/v2/api_test/feed_api_stream_unittest.cc
@@ -355,8 +355,9 @@ TEST_P(FeedStreamTestForAllStreamTypes, UseFeedQueryOverride) {
 TEST_F(FeedApiTest, OnboardingFetchAfterStartup) {
   // Enable WebFeed and WebFeedOnboarding flags.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeed, kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeed,
+                                                          kWebFeedOnboarding},
+                                      disabled_features = {};
   response_translator_.InjectResponse(MakeTypicalInitialModelState());
   features.InitWithFeatures(enabled_features, disabled_features);
 
@@ -378,8 +379,8 @@ TEST_F(FeedApiTest, WebFeedLoadWithNoSubscriptions) {
 TEST_F(FeedApiTest, WebFeedLoadWithNoSubscriptionsAndOnboarding) {
   // Turn on the onboarding feature.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeedOnboarding},
+                                      disabled_features = {};
   response_translator_.InjectResponse(MakeTypicalInitialModelState());
   features.InitWithFeatures(enabled_features, disabled_features);
 
@@ -415,8 +416,8 @@ TEST_F(FeedApiTest, WebFeedLoadWithNoSubscriptionsAndOnboarding) {
 TEST_F(FeedApiTest, WebFeedContentExprirationWithNoSubscriptionsAndOnboarding) {
   // Turn on the onboarding feature.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeedOnboarding},
+                                      disabled_features = {};
   response_translator_.InjectResponse(MakeTypicalInitialModelState());
   features.InitWithFeatures(enabled_features, disabled_features);
 
@@ -474,8 +475,8 @@ TEST_P(FeedNetworkEndpointTest, TestAllNetworkEndpointConfigs) {
   // Enable WebFeed and subscribe to a page, so that we can check if the WebFeed
   // is refreshed by ForceRefreshForDebugging.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeed},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeed},
+                                      disabled_features = {};
   if (GetDiscoFeedEnabled()) {
     enabled_features.push_back(kDiscoFeedEndpoint);
   } else {
@@ -3282,8 +3283,9 @@ TEST_F(FeedApiTest, InvalidateFeedCache_UnknownDoesNotForceRefreshAnyFeeds) {
   // Enable WebFeed and WebFeedOnboarding flags to force WebFeed to fetch
   // without subscriptions.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeed, kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeed,
+                                                          kWebFeedOnboarding},
+                                      disabled_features = {};
   features.InitWithFeatures(enabled_features, disabled_features);
   {
     // Load both feeds and allow them to fetch network contents.
@@ -3322,8 +3324,9 @@ TEST_F(FeedApiTest, InvalidateFeedCache_DoesNotRefreshOtherFeed) {
   // Enable WebFeed and WebFeedOnboarding flags to force WebFeed to fetch
   // without subscriptions.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeed, kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeed,
+                                                          kWebFeedOnboarding},
+                                      disabled_features = {};
   features.InitWithFeatures(enabled_features, disabled_features);
   {
     // Load both feeds and allow them to fetch network contents.
--- components/feed/core/v2/scheduling_unittest.cc
+++ components/feed/core/v2/scheduling_unittest.cc
@@ -219,8 +219,8 @@ TEST_F(ContentLifetimeTest, ShouldWaitForNewContent_ServerThreshold_Valid) {
 TEST_F(ContentLifetimeTest, ShouldWaitForNewContent_WithNoSubscriptions) {
   // Enable WebFeed and WebFeedOnboarding flags.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeedOnboarding},
+                                      disabled_features = {};
   features.InitWithFeatures(enabled_features, disabled_features);
   EXPECT_FALSE(ShouldWaitForNewContent(
       metadata_, StreamType(StreamKind::kFollowing), base::Days(6),
@@ -320,8 +320,8 @@ TEST_F(ContentLifetimeTest, ContentInvalidFromAge_ServerThreshold_Invalid) {
 TEST_F(ContentLifetimeTest, ContentInvalidFromAge_SubscriptionlessThreshold) {
   // Enable WebFeed and WebFeedOnboarding flags.
   base::test::ScopedFeatureList features;
-  std::vector<base::Feature> enabled_features = {kWebFeedOnboarding},
-                             disabled_features = {};
+  std::vector<base::test::FeatureRef> enabled_features = {kWebFeedOnboarding},
+                                      disabled_features = {};
   features.InitWithFeatures(enabled_features, disabled_features);
   EXPECT_FALSE(ContentInvalidFromAge(metadata_,
                                      StreamType(StreamKind::kFollowing),
