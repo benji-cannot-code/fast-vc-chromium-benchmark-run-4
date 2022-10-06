0010a8b3e04177e543a392e910ad994e22d7c560
Cam Bickel <cambickel@google.com>
2022-10-06 16:59:09 +0000

    shortcuts: Add flag called "enable-shortcut-customization"
    This flag will gate the ability to customize shortcuts in the new shortcuts app.
    
    Bug: b/216049298
    Test: browser_tests --gtest_filter=ShortcutCustomizationApp*
    Change-Id: I9212c4db8ce3139c8d94f4034e2b72c2503225a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3907839
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055851}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1140,6 +1140,11 @@ const char kEnableShortcutCustomizationAppDescription[] =
     "Enable the shortcut customization SWA, allowing users to customize system "
     "shortcuts.";
 
+extern const char kEnableShortcutCustomizationName[] =
+    "Enable customization in new shortcuts app";
+extern const char kEnableShortcutCustomizationDescription[] =
+    "Enable customization of shortcuts in the new shortcuts app.";
+
 const char kExperimentalRgbKeyboardPatternsName[] =
     "Enable experimental RGB Keyboard patterns support";
 const char kExperimentalRgbKeyboardPatternsDescription[] =
