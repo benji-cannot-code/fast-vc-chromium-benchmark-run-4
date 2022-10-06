40b2884e1ee2195ff652a705b3c33c270fe645e0
wenyu zhang <zhangwenyu@google.com>
2022-10-06 21:08:32 +0000

    diagnostics: Close tester when exit fullscreen
    When user presses "ecs" key, the tester will only exit the fullscreen
    mode. However, we want the tester to close when user has exited the
    fullscreen mode. Add a event listener to listen to the
    "fullscreenchange" event to handle this case.
    
    Bug: 1207678
    Test: JS browser test --gtest_filter=DiagnosticsAppWithInput.*
    Change-Id: I6f9c91b5fc3392677a09d5a79ee02a47e01cccfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935128
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056002}

--- chrome/test/data/webui/chromeos/diagnostics/input_list_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/input_list_test.js
@@ -180,7 +180,7 @@ export function inputListTestSuite() {
     assertEquals(fakeTouchDevices[1].name, touchscreenCard.devices[0].name);
   });
 
-  test('TouchscreenTesterShow', async () => {
+  test('TouchscreenTesterShowAndClose', async () => {
     await initializeInputList([], [fakeTouchDevices[1]]);
 
     const resolver = new PromiseResolver();
@@ -204,6 +204,9 @@ export function inputListTestSuite() {
     await flushTasks();
     assertEquals(1, requestFullscreenCalled);
     assertTrue(introDialog.open);
+
+    touchscreenTester.shadowRoot.dispatchEvent(new Event('fullscreenchange'));
+    assertFalse(introDialog.open);
   });
 
   test('EmptySectionsHidden', async () => {
