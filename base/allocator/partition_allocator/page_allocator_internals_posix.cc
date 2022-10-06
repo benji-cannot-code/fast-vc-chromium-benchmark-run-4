e16e667cc04220bd8f6ee2a8cb0da18259e906bb
Stephen Roettger <sroettger@google.com>
2022-10-06 11:10:36 +0000

    Pass a pkey in PageAccessibilityConfiguration
    In preparation to support a pkey pool, we'll need to pass the pkey to
    use through to the posix APIs that mprotect the memory.
    Since this is part of the page accessibility, I added it to the
    existing PageAccessibilityConfiguration by turning it into a struct.
    
    This also introduces a ENABLE_PKEYS build flag.
    
    Change-Id: Iaaab4de760feeee9dd485852ea1c149fb46d0d3a
    Bug: v8:13355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934900
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Stephen Röttger <sroettger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055711}

--- base/allocator/partition_allocator/page_allocator_internals_posix.cc
+++ base/allocator/partition_allocator/page_allocator_internals_posix.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace partition_alloc::internal {
 
 int GetAccessFlags(PageAccessibilityConfiguration accessibility) {
-  switch (accessibility) {
+  switch (accessibility.permissions) {
     case PageAccessibilityConfiguration::kRead:
       return PROT_READ;
     case PageAccessibilityConfiguration::kReadWriteTagged:
