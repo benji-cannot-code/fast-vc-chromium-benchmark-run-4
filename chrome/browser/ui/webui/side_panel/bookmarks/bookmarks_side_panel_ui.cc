6189016959ca1530bbd65a81121f6cf3e3e3fc3b
Emily Shack <emshack@chromium.org>
2022-10-06 21:10:12 +0000

    Display folder child count and abbreviated URL
    Screenshot of compact state:
    https://screenshot.googleplex.com/AijxDQRgZj8xW3w
    
    Screenshot of expanded state:
    https://screenshot.googleplex.com/4SJAiWNY2e4CHDY
    
    Bug: 1365606
    Change-Id: I1e2f5e7f946263c3178fc1966516c8e59fbe6be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3919635
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056005}

--- chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_side_panel_ui.cc
+++ chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_side_panel_ui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
+#include "chrome/browser/ui/webui/plural_string_handler.h"
 #include "chrome/browser/ui/webui/sanitized_image_source.h"
 #include "chrome/browser/ui/webui/side_panel/bookmarks/bookmarks_page_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
@@ -36,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/style/platform_style.h"
 
 BookmarksSidePanelUI::BookmarksSidePanelUI(content::WebUI* web_ui)
-    : ui::MojoBubbleWebUIController(web_ui) {
+    : ui::MojoBubbleWebUIController(web_ui, true) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUIBookmarksSidePanelHost);
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
@@ -80,6 +81,13 @@ BookmarksSidePanelUI::BookmarksSidePanelUI(content::WebUI* web_ui)
   webui::SetupWebUIDataSource(
       source, base::make_span(kSidePanelResources, kSidePanelResourcesSize),
       resource);
+
+  // Add a handler to provide pluralized strings.
+  auto plural_string_handler = std::make_unique<PluralStringHandler>();
+  plural_string_handler->AddLocalizedString("bookmarkFolderChildCount",
+                                            IDS_BOOKMARK_FOLDER_CHILD_COUNT);
+  web_ui->AddMessageHandler(std::move(plural_string_handler));
+
   content::WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(),
                                 source);
   content::URLDataSource::Add(profile,
