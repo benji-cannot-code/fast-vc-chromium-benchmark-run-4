2567aedc6a77878e35a77f823f9f74a0df1ef6cd
Simon Ziegltrum <ziegltrum@google.com>
2022-10-06 10:00:09 +0000

    [OOBE]: Demo Setup screen modularized
    Fix: b:243382391
    Change-Id: I7dfb6f475261dc69d930a1332a4b4be1419149e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930920
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055692}

--- chrome/browser/resources/chromeos/login/screens/oobe/demo_setup.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/demo_setup.js
@@ -8,7 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * screen.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/buttons/oobe_back_button.m.js';
+import '../../components/buttons/oobe_text_button.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+import '../../components/progress_list_item/progress_list_item.m.js';
+
+import {loadTimeData} from '//resources/js/load_time_data.m.js';
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+
 
 /**
  * UI mode for the dialog.
@@ -26,14 +42,14 @@ const DemoSetupUIState = {
  * @implements {MultiStepBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
  */
-const DemoSetupScreenBase = Polymer.mixinBehaviors(
+const DemoSetupScreenBase = mixinBehaviors(
     [
       OobeI18nBehavior,
       OobeDialogHostBehavior,
       LoginScreenBehavior,
       MultiStepBehavior,
     ],
-    Polymer.Element);
+    PolymerElement);
 
 /**
  * @polymer
@@ -43,7 +59,9 @@ class DemoSetupScreen extends DemoSetupScreenBase {
     return 'demo-setup-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
