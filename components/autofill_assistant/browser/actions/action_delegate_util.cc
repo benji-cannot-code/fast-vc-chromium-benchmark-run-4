a7a287b5e085c8b968ad54336be1026b562e9ed9
Alan Cutter <alancutter@chromium.org>
2022-10-06 13:49:22 +0000

    Remove const qualifier on GetWeakPtr() helper in /components/autofill_assistant
    This CL removes an unnecessary const -> non-const transition for
    ActionDelegate.
    
    This CL was uploaded by git cl split.
    
    R=fga@google.com
    
    Bug: 1366168
    Change-Id: Ic3ade27442e88020c6818fe26d0127b2627a120c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937070
    Reviewed-by: Florian Gauger <fga@google.com>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1055751}

--- components/autofill_assistant/browser/actions/action_delegate_util.cc
+++ components/autofill_assistant/browser/actions/action_delegate_util.cc
@@ -91,7 +91,7 @@ void SkipIfFail(element_action_util::ElementActionCallback action,
 }
 
 // Adds a sequence of actions that execute a click.
-void AddClickOrTapSequence(const ActionDelegate* delegate,
+void AddClickOrTapSequence(ActionDelegate* delegate,
                            ClickType click_type,
                            element_action_util::ElementActionVector* actions) {
   AddStepIgnoreTiming(
@@ -225,7 +225,7 @@ void FindElementAndPerform(const ActionDelegate* delegate,
 }
 
 void PerformClickOrTapElement(
-    const ActionDelegate* delegate,
+    ActionDelegate* delegate,
     ClickType click_type,
     const ElementFinderResult& element,
     base::OnceCallback<void(const ClientStatus&)> done) {
@@ -236,7 +236,7 @@ void PerformClickOrTapElement(
   element_action_util::PerformAll(std::move(actions), element, std::move(done));
 }
 
-void PerformSetFieldValue(const ActionDelegate* delegate,
+void PerformSetFieldValue(ActionDelegate* delegate,
                           const std::string& value,
                           KeyboardValueFillStrategy fill_strategy,
                           int key_press_delay_in_millisecond,
