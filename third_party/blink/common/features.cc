045169a3b218e03460b51faa45398491c3125e95
Di Zhang <dizhangg@chromium.org>
2022-10-07 21:00:37 +0000

    Add Finch experiment config for feature DocumentEventNodePathCaching
    Change-Id: Ic405b86aa0f5ab6a93f6dee3f2de325f7b15833d
    Bug: 1359407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933509
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056505}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1645,7 +1645,7 @@ BASE_FEATURE(kDocumentEventNodePathCaching,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kDocumentMaxEventNodePathCachedEntries{
-    &kDocumentEventNodePathCaching, "max-cache-entries", 100};
+    &kDocumentEventNodePathCaching, "max-cache-entries", 10};
 
 BASE_FEATURE(kPostMessageDifferentPartitionSameOriginBlocked,
              "PostMessageDifferentPartitionSameOriginBlocked",
