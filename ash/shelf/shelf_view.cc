782991625e2c5d08381dc4c5b34ed540eef5ec96
Avery Musbach <amusbach@chromium.org>
2022-10-07 20:37:30 +0000

    ash: Fix crash on drag from launcher to empty shelf
    In Shelf Party mode, there may be no items on the shelf. This CL fixes a
    DCHECK failure, or a crash if DCHECKs are disabled, when you drag an
    item from the launcher to the empty shelf. Now that workflow will
    actually work, meaning the dragged item will be pinned to the shelf.
    
    Fixed: 1329374
    Test: ash_unittests All/AppsGridViewDragTest.DragAndPinItemToEmptyShelf/*
    Change-Id: Ia2406fd01f8f3d24be7079dc4956490c63c2a611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938349
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056485}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -1572,6 +1572,18 @@ void ShelfView::ContinueDrag(const ui::LocatedEvent& event) {
 }
 
 void ShelfView::MoveDragViewTo(int primary_axis_coordinate) {
+  if (visible_views_indices_.empty()) {
+    DCHECK(model_->in_shelf_party());
+    if (shelf_->IsHorizontalAlignment()) {
+      if (drag_view_->x() != app_icons_layout_offset_)
+        drag_view_->SetX(app_icons_layout_offset_);
+    } else {
+      if (drag_view_->y() != app_icons_layout_offset_)
+        drag_view_->SetY(app_icons_layout_offset_);
+    }
+    return;
+  }
+
   const size_t current_item_index =
       view_model_->GetIndexOfView(drag_view_).value();
   const std::pair<size_t, size_t> indices(GetDragRange(current_item_index));
@@ -1844,6 +1856,11 @@ std::pair<size_t, size_t> ShelfView::GetDragRange(size_t index) {
 bool ShelfView::ShouldUpdateDraggedViewPinStatus(size_t dragged_view_index) {
   if (!features::IsDragUnpinnedAppToPinEnabled())
     return false;
+
+  if (visible_views_indices_.empty()) {
+    DCHECK(model_->in_shelf_party());
+    return false;
+  }
 
   DCHECK(base::Contains(visible_views_indices_, dragged_view_index));
   bool is_moved_item_pinned =
