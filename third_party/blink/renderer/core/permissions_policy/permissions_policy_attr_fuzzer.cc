e22d246d5e026658969d17b3bcd6868017a69277
Ari Chivukula <arichiv@chromium.org>
2022-10-07 18:29:46 +0000

    [Permissions Policy] Update Fuzzer corpus
    Didn't end up doing this earlier, but it's important.
    
    Bug: 1345994
    Change-Id: I8b457dba95185259cd5251a6ac9e7249d6419c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938117
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056412}

--- third_party/blink/renderer/core/permissions_policy/permissions_policy_attr_fuzzer.cc
+++ third_party/blink/renderer/core/permissions_policy/permissions_policy_attr_fuzzer.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  // TODO(crbug.com/1345994): Add seed corpus w/ wildcards.
   static blink::BlinkFuzzerTestSupport test_support =
       blink::BlinkFuzzerTestSupport();
   blink::PolicyParserMessageBuffer logger;
