47aa423fe0f04a30fde3cedc86bce7dae7b2f283
Hamda <hmare@google.com>
2022-10-07 19:31:49 +0000

    [KeyManagementBugBash] Fix load key error in mac key rotation
    This change handles the error when the connectors internal fails
    to load the persisted signing key. It also handles the error of the
    key rotation callback not being invoked.
    
    Screencast: https://screencast.googleplex.com/cast/NTU0MTc4NjUyNjE1NDc1Mnw0MDlmMDI3MS1jOQ
    
    Bug: b/249525428
    Change-Id: I21c963b8d464e5c239c5d9589689bcc401f29d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938539
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056453}

--- chrome/browser/enterprise/connectors/device_trust/key_management/browser/key_rotation_launcher_impl.h
+++ chrome/browser/enterprise/connectors/device_trust/key_management/browser/key_rotation_launcher_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/key_rotation_command.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/key_rotation_launcher.h"
 
 class PrefService;
@@ -44,6 +45,7 @@ class KeyRotationLauncherImpl : public KeyRotationLauncher {
   raw_ptr<policy::DeviceManagementService> device_management_service_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   base::raw_ptr<PrefService> local_prefs_;
+  std::unique_ptr<KeyRotationCommand> command_;
 };
 
 }  // namespace enterprise_connectors
