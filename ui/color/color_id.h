eb8d07966f63c1934b11348411c81861fd9bbf02
Jocelyn Tran <jocelyntran@google.com>
2022-10-07 04:32:24 +0000

    [Read Anything] Add color ids for theme
    In this CL, we add foreground color ids for the 3 existing themes in
    Read Anything. We already have skcolors for these colors but we will
    need color ids to color the separators in the toolbar.
    
    Bug: 1266555
    Change-Id: I842c4f407a0c6e0007ef2d6aa0cbd300196d60ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3920031
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056108}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -238,6 +238,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorOverlayScrollbarStrokeHoveredLight) \
   E_CPONLY(kColorProgressBar) \
   E_CPONLY(kColorProgressBarPaused) \
+  E_CPONLY(kColorReadAnythingForegroundDark) \
+  E_CPONLY(kColorReadAnythingForegroundLight) \
+  E_CPONLY(kColorReadAnythingForegroundYellow) \
   E_CPONLY(kColorSeparator) \
   E_CPONLY(kColorShadowBase) \
   E_CPONLY(kColorShadowValueAmbientShadowElevationSixteen) \
