93c3c5838fe3c312969268d79b72c04140143f8f
Ender <ender@google.com>
2022-10-07 21:22:08 +0000

    [omnibox][grouping] Plumb GroupConfig proto to Java
    The change replaces Omnibox AutocompleteResult#GroupDetails with
    GroupsProto#GroupConfig on Java side, plumbing the entire content
    of the GroupConfig over to Java.
    
    Note: this change aims at doing absolute minimum amount of changes
    required to make things work to reduce the review difficulty.
    
    Follow up work includes
    - plumb groups data via protobuf map (single protobuf string rather
      than series of distinct strings),
    - migrating CachedZeroSuggestManager to record groups protobuf.
    
    Binary size note: Omnibox team is dealing with ever increasing
    complexity of the suggest data and has begun migration to the Protobuf
    format. The migration permits us to
    1) simplify and properly structure the data we currently exchange
       with GWS (json with a ton of extensions, parsed manually),
    2) reduce a big volume of JNI calls to a simple handoff of serialized
       protobuf struct,
    3) remove all the code that currently facilitates data exchange
       between C++ and Java.
    There's a lot of code that does that currently, and all of that code
    also has to come with appropriate coverage.
    As we migrate our logic to use proto objects, we will be deleting
    production code we no longer need.
    
    Change-Id: Ifbf22a1de46a4f3ce3d45b49e393418c66b52343
    Binary-Size: Size increase is unavoidable and transient (see above).
    Related-Change-Id: I439f4e3f379d3e662b301dd940d1ad979b9f9bbd
    Bug: 1343512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938880
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056515}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManagerUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManagerUnitTest.java
@@ -32,7 +32,7 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
-import org.chromium.components.omnibox.AutocompleteResult.GroupDetails;
+import org.chromium.components.omnibox.GroupsProto.GroupConfig;
 import org.chromium.ui.modelutil.ListObservable.ListObserver;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -105,6 +105,21 @@ public class DropdownItemViewInfoListManagerUnitTest {
         }
     }
 
+    /**
+     * Create a simple GroupConfig instance with supplied text and visibility.
+     *
+     * @param headerText The header text to apply to group config.
+     * @param isHidden Whether the newly built group is default-collapsed.
+     * @return Newly constructed GroupConfig.
+     */
+    private GroupConfig buildGroupConfig(String headerText, boolean isHidden) {
+        return GroupConfig.newBuilder()
+                .setHeaderText(headerText)
+                .setVisibility(isHidden ? GroupConfig.Visibility.HIDDEN
+                                        : GroupConfig.Visibility.DEFAULT_VISIBLE)
+                .build();
+    }
+
     @Test
     @SmallTest
     public void modelUpdates_visibilityChangesOnlyUpdateTheModel() {
@@ -120,7 +135,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2),
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2));
 
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
 
         // Monitor updates moving forward.
@@ -157,7 +172,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2),
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2));
 
-        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupConfig>());
         verifyModelEquals(listWithBothGroupsExpanded);
 
         // Toggle group 1.
@@ -204,10 +219,10 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2));
 
         // Receive suggestions list with group 1 default-collapsed.
-        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupDetails>() {
+        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupConfig>() {
             {
-                put(1, new GroupDetails("Collapsed", true));
-                put(2, new GroupDetails("Expanded", false));
+                put(1, buildGroupConfig("Collapsed", true));
+                put(2, buildGroupConfig("Expanded", false));
             }
         });
 
@@ -221,10 +236,10 @@ public class DropdownItemViewInfoListManagerUnitTest {
         verifyModelEquals(listWithBothGroupsExpanded);
 
         // Receive suggestions list with group 2 default-collapsed.
-        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupDetails>() {
+        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupConfig>() {
             {
-                put(1, new GroupDetails("Expanded", false));
-                put(2, new GroupDetails("Collapsed", true));
+                put(1, buildGroupConfig("Expanded", false));
+                put(2, buildGroupConfig("Collapsed", true));
             }
         });
         final List<DropdownItemViewInfo> listWithGroup2Collapsed =
@@ -235,10 +250,10 @@ public class DropdownItemViewInfoListManagerUnitTest {
         verifyModelEquals(listWithBothGroupsExpanded);
 
         // Receive suggestions list with both groups default-collapsed.
-        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupDetails>() {
+        mManager.setSourceViewInfoList(listWithBothGroupsExpanded, new SparseArray<GroupConfig>() {
             {
-                put(1, new GroupDetails("Collapsed", true));
-                put(2, new GroupDetails("Collapsed", true));
+                put(1, buildGroupConfig("Collapsed", true));
+                put(2, buildGroupConfig("Collapsed", true));
             }
         });
         final List<DropdownItemViewInfo> listWithBothGroupsCollapsed =
@@ -263,7 +278,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2),
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2));
 
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
 
         // Expand group 1.
@@ -288,7 +303,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2),
                 new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 2));
 
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
 
         // Collapse group 1.
@@ -329,12 +344,12 @@ public class DropdownItemViewInfoListManagerUnitTest {
                         new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 1),
                         new DropdownItemViewInfo(mBasicSuggestionProcessor, mModel, 1));
 
-        mManager.setSourceViewInfoList(list1, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list1, new SparseArray<GroupConfig>());
         verifyModelEquals(list1);
 
         mManager.clear();
 
-        mManager.setSourceViewInfoList(list2, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list2, new SparseArray<GroupConfig>());
         verifyModelEquals(list2);
     }
 
@@ -349,7 +364,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                         new DropdownItemViewInfo(mBasicSuggestionProcessor,
                                 new PropertyModel(SuggestionCommonProperties.ALL_KEYS), 1));
 
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
         verifyPropertyValues(View.LAYOUT_DIRECTION_INHERIT, BrandedColorScheme.LIGHT_BRANDED_THEME);
 
@@ -371,7 +386,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                         new PropertyModel(SuggestionCommonProperties.ALL_KEYS), 2),
                 new DropdownItemViewInfo(mBasicSuggestionProcessor,
                         new PropertyModel(SuggestionCommonProperties.ALL_KEYS), 2));
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
         verifyPropertyValues(View.LAYOUT_DIRECTION_RTL, BrandedColorScheme.INCOGNITO);
     }
@@ -393,7 +408,7 @@ public class DropdownItemViewInfoListManagerUnitTest {
                 new DropdownItemViewInfo(mBasicSuggestionProcessor,
                         new PropertyModel(SuggestionCommonProperties.ALL_KEYS), groupId));
 
-        mManager.setSourceViewInfoList(list, new SparseArray<GroupDetails>());
+        mManager.setSourceViewInfoList(list, new SparseArray<GroupConfig>());
         verifyModelEquals(list);
 
         //
--- components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteResultUnitTest.java
+++ components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteResultUnitTest.java
@@ -14,7 +14,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
-import org.chromium.components.omnibox.AutocompleteResult.GroupDetails;
+import org.chromium.components.omnibox.GroupsProto.GroupConfig;
 import org.chromium.url.ShadowGURL;
 
 import java.util.ArrayList;
@@ -31,6 +31,14 @@ public class AutocompleteResultUnitTest {
         return AutocompleteMatchBuilder.searchWithType(OmniboxSuggestionType.SEARCH_SUGGEST)
                 .setDisplayText("Dummy Suggestion " + index)
                 .setDescription("Dummy Description " + index)
+                .build();
+    }
+
+    private GroupConfig buildGroupConfig(String headerText, boolean isHidden) {
+        return GroupConfig.newBuilder()
+                .setHeaderText(headerText)
+                .setVisibility(isHidden ? GroupConfig.Visibility.HIDDEN
+                                        : GroupConfig.Visibility.DEFAULT_VISIBLE)
                 .build();
     }
 
@@ -52,14 +60,14 @@ public class AutocompleteResultUnitTest {
         list1.get(2).getSubtypes().add(4);
         list2.get(2).getSubtypes().add(4);
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", false));
-        groupsDetails1.put(20, new GroupDetails("Test", true));
+        groupsDetails1.put(10, buildGroupConfig("Hello", false));
+        groupsDetails1.put(20, buildGroupConfig("Test", true));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", false));
-        groupsDetails2.put(20, new GroupDetails("Test", true));
+        groupsDetails2.put(10, buildGroupConfig("Hello", false));
+        groupsDetails2.put(20, buildGroupConfig("Test", true));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list1, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list2, groupsDetails2);
@@ -75,14 +83,14 @@ public class AutocompleteResultUnitTest {
         List<AutocompleteMatch> list2 = Arrays.asList(
                 buildSuggestionForIndex(2), buildSuggestionForIndex(1), buildSuggestionForIndex(3));
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", false));
-        groupsDetails1.put(20, new GroupDetails("Test", true));
+        groupsDetails1.put(10, buildGroupConfig("Hello", false));
+        groupsDetails1.put(20, buildGroupConfig("Test", true));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", false));
-        groupsDetails2.put(20, new GroupDetails("Test", true));
+        groupsDetails2.put(10, buildGroupConfig("Hello", false));
+        groupsDetails2.put(20, buildGroupConfig("Test", true));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list1, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list2, groupsDetails2);
@@ -98,13 +106,13 @@ public class AutocompleteResultUnitTest {
         List<AutocompleteMatch> list2 = Arrays.asList(
                 buildSuggestionForIndex(1), buildSuggestionForIndex(2), buildSuggestionForIndex(3));
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", true));
-        groupsDetails1.put(20, new GroupDetails("Test", false));
+        groupsDetails1.put(10, buildGroupConfig("Hello", true));
+        groupsDetails1.put(20, buildGroupConfig("Test", false));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", true));
+        groupsDetails2.put(10, buildGroupConfig("Hello", true));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list1, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list2, groupsDetails2);
@@ -120,14 +128,14 @@ public class AutocompleteResultUnitTest {
         List<AutocompleteMatch> list2 = Arrays.asList(
                 buildSuggestionForIndex(1), buildSuggestionForIndex(2), buildSuggestionForIndex(3));
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", false));
-        groupsDetails1.put(20, new GroupDetails("Test", true));
+        groupsDetails1.put(10, buildGroupConfig("Hello", false));
+        groupsDetails1.put(20, buildGroupConfig("Test", true));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", false));
-        groupsDetails2.put(20, new GroupDetails("Test", false));
+        groupsDetails2.put(10, buildGroupConfig("Hello", false));
+        groupsDetails2.put(20, buildGroupConfig("Test", false));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list1, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list2, groupsDetails2);
@@ -143,15 +151,15 @@ public class AutocompleteResultUnitTest {
         List<AutocompleteMatch> list2 = Arrays.asList(
                 buildSuggestionForIndex(1), buildSuggestionForIndex(2), buildSuggestionForIndex(3));
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", false));
-        groupsDetails1.put(20, new GroupDetails("Test", false));
+        groupsDetails1.put(10, buildGroupConfig("Hello", false));
+        groupsDetails1.put(20, buildGroupConfig("Test", false));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", false));
-        groupsDetails2.put(20, new GroupDetails("Test", false));
-        groupsDetails2.put(30, new GroupDetails("Yikes", false));
+        groupsDetails2.put(10, buildGroupConfig("Hello", false));
+        groupsDetails2.put(20, buildGroupConfig("Test", false));
+        groupsDetails2.put(30, buildGroupConfig("Yikes", false));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list1, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list2, groupsDetails2);
@@ -179,18 +187,18 @@ public class AutocompleteResultUnitTest {
         List<AutocompleteMatch> list = Arrays.asList(
                 buildSuggestionForIndex(1), buildSuggestionForIndex(2), buildSuggestionForIndex(3));
 
-        SparseArray<GroupDetails> groupsDetails1 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails2 = new SparseArray<>();
-        SparseArray<GroupDetails> groupsDetails3 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails1 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails2 = new SparseArray<>();
+        SparseArray<GroupConfig> groupsDetails3 = new SparseArray<>();
 
-        groupsDetails1.put(10, new GroupDetails("Hello", false));
-        groupsDetails1.put(20, new GroupDetails("Test", false));
+        groupsDetails1.put(10, buildGroupConfig("Hello", false));
+        groupsDetails1.put(20, buildGroupConfig("Test", false));
 
-        groupsDetails2.put(10, new GroupDetails("Hello", false));
-        groupsDetails2.put(15, new GroupDetails("Test", false));
+        groupsDetails2.put(10, buildGroupConfig("Hello", false));
+        groupsDetails2.put(15, buildGroupConfig("Test", false));
 
-        groupsDetails3.put(10, new GroupDetails("Hello", false));
-        groupsDetails3.put(20, new GroupDetails("Test 2", false));
+        groupsDetails3.put(10, buildGroupConfig("Hello", false));
+        groupsDetails3.put(20, buildGroupConfig("Test 2", false));
 
         AutocompleteResult res1 = AutocompleteResult.fromCache(list, groupsDetails1);
         AutocompleteResult res2 = AutocompleteResult.fromCache(list, groupsDetails2);
@@ -288,8 +296,7 @@ public class AutocompleteResultUnitTest {
         res.notifyNativeDestroyed();
         Assert.assertFalse(res.isFromCachedResult());
 
-        res = AutocompleteResult.fromNative(
-                0xfedcba98, new AutocompleteMatch[0], new int[0], new String[0], new boolean[0]);
+        res = AutocompleteResult.fromNative(0xfedcba98, new AutocompleteMatch[0], new byte[0]);
         Assert.assertFalse(res.isFromCachedResult());
         res.notifyNativeDestroyed();
         Assert.assertFalse(res.isFromCachedResult());
