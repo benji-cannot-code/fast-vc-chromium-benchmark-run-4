9ba7064f750754b130f7ec6e62086e183a97f82e
Jong Ahn <jongahn@google.com>
2022-10-07 22:58:53 +0000

    [Structured Metrics] Remove EventBase.
    Final CL of a refactor removing EventBase.
    
    Bug: 1350322
    Change-Id: I4eb888bdcc142517e634fc57541429d39a025259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933136
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056556}

--- chrome/browser/metrics/structured/lacros_structured_metrics_recorder.cc
+++ chrome/browser/metrics/structured/lacros_structured_metrics_recorder.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/structured_metrics_service.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
 #include "components/metrics/structured/event.h"
-#include "components/metrics/structured/event_base.h"
 #include "components/metrics/structured/structured_metrics_client.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
--- chrome/browser/metrics/structured/lacros_structured_metrics_recorder.h
+++ chrome/browser/metrics/structured/lacros_structured_metrics_recorder.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_types.h"
 #include "base/task/sequenced_task_runner.h"
 #include "components/metrics/structured/event.h"
-#include "components/metrics/structured/event_base.h"
 #include "components/metrics/structured/structured_metrics_client.h"
 
 namespace metrics {
