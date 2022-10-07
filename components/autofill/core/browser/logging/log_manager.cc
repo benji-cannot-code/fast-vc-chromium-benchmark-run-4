2155c03d3eae9436b39d49ba2f323ff84bd75605
Christoph Schwering <schwering@google.com>
2022-10-07 13:18:18 +0000

    [Autofill] Use BufferingLogManager in asynchronous form parsing.
    This CL introduces a BufferingLogManager whose messages can be flushed
    to a RoutingLogManager.
    
    Moreover, this CL uses this BufferingLogManager in asynchronous form
    parsing so that chrome://autofill-internals generates the expected
    output if AutofillParseAsync is enabled.
    
    Bug: 1309848, 1345089, 928595
    Change-Id: I6d4670aeb4d2f20b22987d6d1c4caddecd36d18b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935043
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056262}

--- components/autofill/core/browser/logging/log_manager.cc
+++ components/autofill/core/browser/logging/log_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/logging/log_manager.h"
 
+#include <utility>
+
 #include "base/memory/raw_ptr.h"
 #include "components/autofill/core/browser/logging/log_router.h"
 
@@ -94,6 +96,49 @@ void RoutingLogManagerImpl::ProcessLog(base::Value::Dict node,
   log_router_->ProcessLog(std::move(node));
 }
 
+class BufferingLogManagerImpl : public BufferingLogManager {
+ public:
+  BufferingLogManagerImpl() = default;
+
+  BufferingLogManagerImpl(const BufferingLogManagerImpl&) = delete;
+  BufferingLogManagerImpl& operator=(const BufferingLogManagerImpl&) = delete;
+
+  ~BufferingLogManagerImpl() override = default;
+
+  // BufferingLogManager
+  void Flush(LogManager& destination) override;
+  // LogManager
+  bool IsLoggingActive() const override;
+  LogBufferSubmitter Log() override;
+  void ProcessLog(base::Value::Dict node,
+                  base::PassKey<LogBufferSubmitter>) override;
+
+ private:
+  std::vector<base::Value::Dict> nodes_;
+  absl::optional<base::PassKey<LogBufferSubmitter>> pass_key_;
+};
+
+void BufferingLogManagerImpl::Flush(LogManager& destination) {
+  auto nodes = std::exchange(nodes_, {});
+  for (auto& node : nodes)
+    destination.ProcessLog(std::move(node), *pass_key_);
+}
+
+bool BufferingLogManagerImpl::IsLoggingActive() const {
+  return true;
+}
+
+LogBufferSubmitter BufferingLogManagerImpl::Log() {
+  return LogBufferSubmitter(this);
+}
+
+void BufferingLogManagerImpl::ProcessLog(
+    base::Value::Dict node,
+    base::PassKey<LogBufferSubmitter> pass_key) {
+  nodes_.push_back(std::move(node));
+  pass_key_ = pass_key;
+}
+
 }  // namespace
 
 // static
@@ -102,6 +147,11 @@ std::unique_ptr<RoutingLogManager> LogManager::Create(
     base::RepeatingClosure notification_callback) {
   return std::make_unique<RoutingLogManagerImpl>(
       log_router, std::move(notification_callback));
+}
+
+// static
+std::unique_ptr<BufferingLogManager> LogManager::CreateBuffering() {
+  return std::make_unique<BufferingLogManagerImpl>();
 }
 
 }  // namespace autofill
