494dfcf8df745ff0ab3f90c3f68f22471df95dd0
Yoav Weiss <yoavweiss@chromium.org>
2022-10-07 13:22:59 +0000

    Avoid spamming FCP observers for soft navigations
    As a followup to [1], this CL avoids calling unrelated FCP observers for
    soft navigation FCPs.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3928699
    
    Bug: 1370495
    Change-Id: I909da1d3aa836bcf7ce43e023a1a44e16ff3d294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937954
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056265}

--- third_party/blink/renderer/core/paint/paint_timing.cc
+++ third_party/blink/renderer/core/paint/paint_timing.cc
@@ -337,9 +337,12 @@ void PaintTiming::SetFirstContentfulPaintPresentation(base::TimeTicks stamp) {
                                       "GlobalFirstContentfulPaint",
                                       TRACE_EVENT_SCOPE_GLOBAL, stamp);
   first_contentful_paint_presentation_ = stamp;
+  bool is_soft_navigation_fcp = false;
   if (first_contentful_paint_presentation_ignoring_soft_navigations_
           .is_null()) {
     first_contentful_paint_presentation_ignoring_soft_navigations_ = stamp;
+  } else {
+    is_soft_navigation_fcp = true;
   }
   probe::PaintTiming(
       GetSupplementable(), "firstContentfulPaint",
@@ -348,6 +351,11 @@ void PaintTiming::SetFirstContentfulPaintPresentation(base::TimeTicks stamp) {
   if (performance) {
     performance->AddFirstContentfulPaintTiming(
         first_contentful_paint_presentation_);
+  }
+  // For soft navigations, we just want to report a performance entry, but not
+  // trigger any of the other FCP observers.
+  if (is_soft_navigation_fcp) {
+    return;
   }
   if (GetFrame())
     GetFrame()->Loader().Progress().DidFirstContentfulPaint();
