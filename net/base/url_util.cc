aa07dfdad9ac0bbf61a4e0960a5fe258e0b4a5c7
Meredith Lane <meredithl@chromium.org>
2022-10-07 05:49:54 +0000

    Revert "#Lens Add new RemoveKeyInQuery function and use that for lens"
    This reverts commit 4657a8b962900a086225055a7147fada992a6514.
    
    Reason for revert: Suspected culprit of failing browser_tests on Linux MSan. Example failed build: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/35356/overview
    
    Original change's description:
    > #Lens Add new RemoveKeyInQuery function and use that for lens
    >
    > Added a new function to remove the query param instead of just replacing it with empty string.
    >
    > Bug: b/245753529
    > Change-Id: Ic0bf8ddd64cee2d5072b36a8ddcd036c4eca1d2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934545
    > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > Commit-Queue: Anudeep Palanki <apalanki@google.com>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1056015}
    
    Bug: b/245753529
    Change-Id: Iacb315b591a5f3112307227a064ef6d3ce18d897
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939164
    Owners-Override: Meredith Lane <meredithl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056144}

--- net/base/url_util.cc
+++ net/base/url_util.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "net/base/ip_address.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 #include "url/scheme_host_port.h"
 #include "url/url_canon.h"
@@ -79,14 +78,10 @@ GURL AppendQueryParameter(const GURL& url,
 
 GURL AppendOrReplaceQueryParameter(const GURL& url,
                                    const std::string& name,
-                                   absl::optional<base::StringPiece> value) {
+                                   const std::string& value) {
   bool replaced = false;
   std::string param_name = base::EscapeQueryParamValue(name, true);
-  bool should_keep_param = value.has_value();
-
-  std::string param_value;
-  if (should_keep_param)
-    param_value = base::EscapeQueryParamValue(value.value(), true);
+  std::string param_value = base::EscapeQueryParamValue(value, true);
 
   const std::string input = url.query();
   url::Component cursor(0, input.size());
@@ -100,11 +95,7 @@ GURL AppendOrReplaceQueryParameter(const GURL& url,
     // Check |replaced| as only the first pair should be replaced.
     if (!replaced && key == param_name) {
       replaced = true;
-      if (!should_keep_param)
-        continue;
-
       key_value_pair = (param_name + "=" + param_value);
-
     } else {
       key_value_pair.assign(input, key_range.begin,
                             value_range.end() - key_range.begin);
@@ -114,7 +105,7 @@ GURL AppendOrReplaceQueryParameter(const GURL& url,
 
     output += key_value_pair;
   }
-  if (!replaced && should_keep_param) {
+  if (!replaced) {
     if (!output.empty())
       output += "&";
 
