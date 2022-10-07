c025ff6c5d1c00727b3c6cf7aa84eabadae89ec0
Mikel Astiz <mastiz@chromium.org>
2022-10-07 08:54:16 +0000

    [sync] Decouple trusted vault notion of staleness
    Prior to this patch, the proto field `keys_are_stale` conflated two
    different concepts that strictly speaking are independent (although they
    in practice correlate strongly):
    
    1) The notion that higher layers (consumers of the keys, in this case
       the Nigori logic) reported that the keys are insufficient, which is
       a strong signal that new ones are needed and if possible the server
       should be contacted to download new ones.
    
    2) The notion that it is not possible to register the local device (join
       the security domain) with the current local state (specifically, it
       means the currently-known keys are insufficient and retrieval is
       required).
    
    There are corner cases were the two may diverge: for example the fact
    that 1) above is true (i.e.  MarkLocalKeysAsStale() was invoked) doesn't
    necessarily guarantee that the client shouldn't attempt to register
    itself. In normal circumnstances such attempt would indeed fail, but it
    is also possible in advanced cases that, although the client doesn't
    know, it is already registered (e.g. previous attempt's response wasn't
    received) depending on how the server behaves in this case (the current
    server would reject additional requests, though).
    
    Besides corner cases, decoupling these two is just simpler to reason
    about and debug. It essentially means that the device registration logic
    is completely independent from how upper layers behave.
    
    Change-Id: I2b481af10becab202443fc0c4cf16fe89baf761e
    Bug: 1081643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3898590
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056196}

--- components/sync/trusted_vault/standalone_trusted_vault_backend.h
+++ components/sync/trusted_vault/standalone_trusted_vault_backend.h
@@ -130,6 +130,8 @@ class StandaloneTrustedVaultBackend
 
   void SetDeviceRegisteredVersionForTesting(const std::string& gaia_id,
                                             int version);
+  void SetLastRegistrationReturnedLocalDataObsoleteForTesting(
+      const std::string& gaia_id);
 
   void SetClockForTesting(base::Clock* clock);
 
