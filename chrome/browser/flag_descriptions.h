8b1bf19b3a391e486525538b9f9a0531527ca671
Elliot Tuck <etuck@chromium.org>
2022-10-07 15:27:57 +0000

    CrOS Shelf: Add flag to guard focus order change
    Add a flag that will be used to guard a change to the focus ordering of
    shelf items.
    
    Bug: 1349427
    Change-Id: I28330beea85e49751b728810f4e53316af73e504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933180
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056316}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3427,6 +3427,9 @@ extern const char kSecondaryGoogleAccountUsageDescription[];
 extern const char kShelfAutoHideSeparationName[];
 extern const char kShelfAutoHideSeparationDescription[];
 
+extern const char kShelfFocusOrderV1Name[];
+extern const char kShelfFocusOrderV1Description[];
+
 extern const char kShimlessRMAFlowName[];
 extern const char kShimlessRMAFlowDescription[];
 
