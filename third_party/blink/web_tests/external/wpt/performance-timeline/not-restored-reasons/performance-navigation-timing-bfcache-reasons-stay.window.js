a50cecbf108796c3dd990aed753210e01ce6eba7
Yuzu Saijo <yuzus@chromium.org>
2022-10-07 09:39:07 +0000

    [bfcache] Do not reset notRestoredReasons upon successful bfcache navigation
    This CL makes PerformanceNavigationEntry.notRestoredReasons not reset
    after successful bfcache navigation. After this CL, when the page is
    successfully restored from bfcache, the reasons from the previous
    navigation stay.
    
    Now, the only time we reset the reasons is when we get a new set of
    reasons.
    
    This was discussed on the email thread here:
    https://groups.google.com/a/chromium.org/g/bfcache-dev/c/fGPWPVTbyJA
    
    Bug:1363438
    
    Change-Id: I5214f56e8424356cad8190c2a0144e4d1c017746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906513
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056205}

--- third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-bfcache-reasons-stay.window.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-bfcache-reasons-stay.window.js
@@ -9,59 +9,49 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-// Ensure that cross-origin subtree's reasons are not exposed to
-// notRestoredReasons.
+// Ensure that notRestoredReasons are only updated after non BFCache navigation.
 promise_test(async t => {
   const rcHelper = new RemoteContextHelper();
   // Open a window with noopener so that BFCache will work.
   const rc1 = await rcHelper.addWindow(
       /*config=*/ null, /*options=*/ {features: 'noopener'});
+  // Use WebSocket to block BFCache.
+  await useWebSocket(rc1);
   const rc1_url = await rc1.executeScript(() => {
     return location.href;
   });
-  // Add a cross-origin iframe and use BroadcastChannel.
-  const rc1_child = await rc1.addIframe(
-      /*extraConfig=*/ {
-        origin: 'HTTP_REMOTE_ORIGIN',
-        scripts: [],
-        headers: [],
-      },
-      /*attributes=*/ {id: 'test-id'},
-  );
-  // Use WebSocket to block BFCache.
-  await useWebSocket(rc1_child);
-
-  const rc1_child_url = await rc1_child.executeScript(() => {
-    return location.href;
-  });
-  // Add a child to the iframe.
-  const rc1_grand_child = await rc1_child.addIframe();
-  const rc1_grand_child_url = await rc1_grand_child.executeScript(() => {
-    return location.href;
-  });
   prepareForBFCache(rc1);
+
   // Navigate away.
   const rc2 = await rc1.navigateToNew();
-
   // Navigate back.
   await rc2.historyBack();
   assert_not_bfcached(rc1);
+
   // Check the reported reasons.
   await assertNotRestoredReasonsEquals(
       rc1,
-      /*blocked=*/ false,
+      /*blocked=*/ true,
       /*url=*/ rc1_url,
       /*src=*/ '',
       /*id=*/ '',
       /*name=*/ '',
-      /*reasons=*/[],
-      /*children=*/[{
-        'blocked': true,
-        'url': '',
-        'src': '',
-        'id': '',
-        'name': '',
-        'reasons': [],
-        'children': []
-      }]);
+      /*reasons=*/['WebSocket'],
+      /*children=*/[]);
+  prepareForBFCache(rc1);
+
+  await rc1.historyForward();
+  await rc2.historyBack();
+  // This time no blocking feature is used, so the page is restored
+  // from BFCache. Ensure that the previous reasons stay there.
+  assert_implements_bfcache(rc1);
+  await assertNotRestoredReasonsEquals(
+      rc1,
+      /*blocked=*/ true,
+      /*url=*/ rc1_url,
+      /*src=*/ '',
+      /*id=*/ '',
+      /*name=*/ '',
+      /*reasons=*/['WebSocket'],
+      /*children=*/[]);
 });
--- third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-not-bfcached.window.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-not-bfcached.window.js
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=RemoteContextHelper navigation using BFCache
+// META: script=./test-helper.js
 // META: script=/common/dispatcher/dispatcher.js
 // META: script=/common/get-host-info.sub.js
 // META: script=/common/utils.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
-// META: script=/html/browsers/browsing-the-web/remote-context-helper-tests/resources/test-helper.js
 // META: script=/websockets/constants.sub.js
 
 'use strict';
@@ -15,29 +15,28 @@ promise_test(async t => {
   // Open a window with noopener so that BFCache will work.
   const rc1 = await rcHelper.addWindow(
       /*config=*/ null, /*options=*/ {features: 'noopener'});
-
-  const domainPort = SCHEME_DOMAIN_PORT;
-  await rc1.executeScript((domain) => {
-    var ws = new WebSocket(domain + '/echo');
-  }, [domainPort]);
+  // Use WebSocket to block BFCache.
+  await useWebSocket(rc1);
 
   const rc1_url = await rc1.executeScript(() => {
     return location.href;
   });
+  prepareForBFCache(rc1);
 
   // Navigate away.
   const rc2 = await rc1.navigateToNew();
 
   // Navigate back.
   await rc2.historyBack();
+  assert_not_bfcached(rc1);
   // Check the reported reasons.
   await assertNotRestoredReasonsEquals(
-    rc1,
-    /*blocked=*/true,
-    /*url=*/rc1_url,
-    /*src=*/ "",
-    /*id=*/"",
-    /*name=*/"",
-    /*reasons=*/["WebSocket"],
-    /*children=*/[]);
+      rc1,
+      /*blocked=*/ true,
+      /*url=*/ rc1_url,
+      /*src=*/ '',
+      /*id=*/ '',
+      /*name=*/ '',
+      /*reasons=*/['WebSocket'],
+      /*children=*/[]);
 });
