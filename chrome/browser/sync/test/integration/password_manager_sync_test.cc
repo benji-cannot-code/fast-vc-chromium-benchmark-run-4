2bdcd547295653b9bfb25761ba59867f562b150a
Mikel Astiz <mastiz@chromium.org>
2022-10-07 13:59:44 +0000

    [Passwords] Add more tests about sync-paused and auth errors
    This is a continuation of crrev.com/c/3929140 as part of a broader effort
    to document with tests the current behavior for the sync-paused state
    and auth errors, ahead of behavioral changes.
    
    Change-Id: Iea2c2b4492dea4ccf75373c486e6f12deac7cbd7
    Bug: 1156584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938069
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056281}

--- chrome/browser/sync/test/integration/password_manager_sync_test.cc
+++ chrome/browser/sync/test/integration/password_manager_sync_test.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_manager_features_util.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/browser/password_sync_util.h"
 #include "components/password_manager/core/common/password_manager_features.h"
@@ -908,10 +909,14 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest, SyncUtilApis) {
       password_manager::sync_util::IsPasswordSyncEnabled(GetSyncService(0)));
   EXPECT_TRUE(
       password_manager::sync_util::IsPasswordSyncActive(GetSyncService(0)));
+  EXPECT_NE(absl::nullopt,
+            password_manager::sync_util::GetSyncingAccount(GetSyncService(0)));
   EXPECT_EQ(password_manager::sync_util::GetSyncUsernameIfSyncingPasswords(
                 GetSyncService(0),
                 IdentityManagerFactory::GetForProfile(GetProfile(0))),
             kExpectedUsername);
+  EXPECT_EQ(password_manager_util::GetPasswordSyncState(GetSyncService(0)),
+            password_manager::SyncState::kSyncingNormalEncryption);
 
   // Enter a persistent auth error state (web signout).
   GetClient(0)->EnterSyncPausedStateForPrimaryAccount();
@@ -926,11 +931,15 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest, SyncUtilApis) {
   // all persistent auth errors.
   EXPECT_FALSE(
       password_manager::sync_util::IsPasswordSyncActive(GetSyncService(0)));
+  EXPECT_EQ(password_manager_util::GetPasswordSyncState(GetSyncService(0)),
+            password_manager::SyncState::kNotSyncing);
 
-  // In the current implementation, these predicates return true even if sync is
-  // paused.
+  // In the current implementation, the APIs below treat sync as enabled/active
+  // even while paused.
   EXPECT_TRUE(
       password_manager::sync_util::IsPasswordSyncEnabled(GetSyncService(0)));
+  EXPECT_NE(absl::nullopt,
+            password_manager::sync_util::GetSyncingAccount(GetSyncService(0)));
   EXPECT_EQ(password_manager::sync_util::GetSyncUsernameIfSyncingPasswords(
                 GetSyncService(0),
                 IdentityManagerFactory::GetForProfile(GetProfile(0))),
