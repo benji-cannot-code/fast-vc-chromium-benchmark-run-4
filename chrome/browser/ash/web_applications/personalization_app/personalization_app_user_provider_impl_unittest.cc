70bc1f31e4ef86db216013d1c4013757f8fa7d51
Marc Treib <treib@chromium.org>
2022-10-07 11:50:09 +0000

    Revert "Avatars: Support animated images for UserImageLoader"
    This reverts commit 833346a87e26b29dd93747f2e2ecaa65a8007b8c.
    
    Reason for revert: Introduced memleaks, see crbug.com/1372380
    
    Original change's description:
    > Avatars: Support animated images for UserImageLoader
    >
    > This change adds animated image support for UserImageLoader, which is
    > required to cache the bytes data of default user images from Cloud.
    >
    > Bug: b/250810109
    > Test: Local build
    > Change-Id: Ib9a6b4013abe981b46a1abc27fa70d17f443656b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935527
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Yue Li <updowndota@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1056172}
    
    Bug: b/250810109, 1372380
    Change-Id: I607ba7952df08aa6b5c93aee8b83840369113794
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934627
    Owners-Override: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056238}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_user_provider_impl_unittest.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_user_provider_impl_unittest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
-#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -184,9 +183,6 @@ class PersonalizationAppUserProviderImplTest : public testing::Test {
 
     user_provider_->BindInterface(
         user_provider_remote_.BindNewPipeAndPassReceiver());
-
-    in_process_data_decoder_ =
-        std::make_unique<data_decoder::test::InProcessDataDecoder>();
   }
 
   TestingProfile* profile() { return profile_; }
@@ -247,8 +243,6 @@ class PersonalizationAppUserProviderImplTest : public testing::Test {
 
   const base::HistogramTester& histogram_tester() { return histogram_tester_; }
 
-  void RunUntilIdle() { task_environment_.RunUntilIdle(); }
-
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
   content::BrowserTaskEnvironment task_environment_;
@@ -262,8 +256,6 @@ class PersonalizationAppUserProviderImplTest : public testing::Test {
       user_provider_remote_;
   std::unique_ptr<PersonalizationAppUserProviderImpl> user_provider_;
   base::HistogramTester histogram_tester_;
-  std::unique_ptr<data_decoder::test::InProcessDataDecoder>
-      in_process_data_decoder_;
 };
 
 TEST_F(PersonalizationAppUserProviderImplTest, GetsUserInfo) {
@@ -292,7 +284,6 @@ TEST_F(PersonalizationAppUserProviderImplTest, ObservesUserAvatarImage) {
   // Select a default image.
   int image_index = ash::default_user_image::GetRandomDefaultImageIndex();
   user_image_manager()->SaveUserDefaultImageIndex(image_index);
-  RunUntilIdle();
 
   // Observer received the updated image information. Because it is a default
   // image, receives a default image with the right index.
@@ -307,7 +298,6 @@ TEST_F(PersonalizationAppUserProviderImplTest, SelectDefaultImage) {
   int image_index = ash::default_user_image::GetRandomDefaultImageIndex();
   user_provider_remote()->get()->SelectDefaultImage(image_index);
   user_provider_remote()->FlushForTesting();
-  RunUntilIdle();
 
   // Observer received the updated user image of type default with the right
   // index.
@@ -381,7 +371,6 @@ TEST_F(PersonalizationAppUserProviderImplTest,
 
   user_provider_remote()->get()->SelectDefaultImage(image_index);
   user_provider_remote()->FlushForTesting();
-  RunUntilIdle();
 
   // Bucket count is incremented after selecting this default image.
   histogram_tester().ExpectBucketCount(
@@ -391,7 +380,6 @@ TEST_F(PersonalizationAppUserProviderImplTest,
   // Select the same image again.
   user_provider_remote()->get()->SelectDefaultImage(image_index);
   user_provider_remote()->FlushForTesting();
-  RunUntilIdle();
 
   // Bucket count is not incremented.
   histogram_tester().ExpectBucketCount(
@@ -436,7 +424,6 @@ TEST_F(PersonalizationAppUserProviderImplTest,
   // Select a default image first to make sure profile is not selected.
   user_provider_remote()->get()->SelectDefaultImage(
       ash::default_user_image::GetRandomDefaultImageIndex());
-  RunUntilIdle();
   // Now select profile.
   user_provider_remote()->get()->SelectProfileImage();
   user_provider_remote()->FlushForTesting();
@@ -471,7 +458,6 @@ TEST_F(PersonalizationAppUserProviderImplTest,
   user_provider_remote()->get()->SelectDefaultImage(
       ash::default_user_image::GetRandomDefaultImageIndex());
   user_provider_remote()->FlushForTesting();
-  RunUntilIdle();
 
   histogram_tester().ExpectBucketCount(
       ash::UserImageManager::kUserImageChangedHistogramName,
@@ -514,7 +500,6 @@ TEST_F(PersonalizationAppUserProviderImplTest,
   user_image_manager()->SaveUserDefaultImageIndex(
       kDeprecatedImageWithSourceInfoIndex);
   SetUserImageObserver();
-  RunUntilIdle();
 
   absl::optional<default_user_image::DeprecatedSourceInfo>
       expected_source_info =
