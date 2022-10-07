6c8e3a8bd0eb79a0d21735b06e230b573bd07c79
Sebastien Lalancette <seblalancette@chromium.org>
2022-10-07 14:34:33 +0000

    [DTC] Add Signals Contract Browser Test
    Adds a browser test which validates the format of the device signals
    payload (signal names, values' types, presence per platform, etc).
    
    Fixed: b:250012680
    Change-Id: I77b7cda64feefd821ae06e40c6dfce3220722380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925793
    Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056297}

--- chrome/browser/enterprise/connectors/device_trust/signals/signals_service.h
+++ chrome/browser/enterprise/connectors/device_trust/signals/signals_service.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_SIGNALS_SIGNALS_SERVICE_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_SIGNALS_SIGNALS_SERVICE_H_
 
-#include <memory>
-
 #include "base/callback.h"
 #include "base/values.h"
 
@@ -16,8 +14,7 @@ namespace enterprise_connectors {
 // Service in charge of retrieving context-aware signals for its consumers.
 class SignalsService {
  public:
-  using CollectSignalsCallback =
-      base::OnceCallback<void(const base::Value::Dict)>;
+  using CollectSignalsCallback = base::OnceCallback<void(base::Value::Dict)>;
 
   virtual ~SignalsService() = default;
 
