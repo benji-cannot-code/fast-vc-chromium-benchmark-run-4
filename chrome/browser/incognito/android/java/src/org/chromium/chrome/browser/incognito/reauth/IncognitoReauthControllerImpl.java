4ea05678873dab4e9d487d3fb5082c30de8c1b87
Rohit Agarwal <roagarwal@chromium.org>
2022-10-07 11:59:45 +0000

    [Android][IncognitoReauth] Add logic to handle layout state changes.
    This CL adds a LayoutStateObserver from IncognitoReauthControllerImpl
    which allows the controller to observer changes like when tab switcher
    layout is hidden or when we are showing a browsing layout.
    
    Listening to tab switcher layout hidden change allows to:
     a) Hide any re-auth screen which might be shown.
     b) And hiding the re-auth screen results in deleting the underlying
        coordinator which needs to be null when showing a re-auth screen
        again.
     c) crbug.com/1371918 arises because we were not destroying the
        coordinator and therefore unable to show the fullscreen re-auth.
    
    Listening to browsing layout changes allows to:
     a) Show the fullscreen dialog when we transit from TabSwitcher layout to directly opening a Tab.
    
    Demo: https://screencast.googleplex.com/cast/NjE4NDQyNzMxNjA1MTk2OHxjZWYwY2FiZC1kMA
    
    Bug: 1227656, 1371918
    Change-Id: I2a39ca0182abd7ed0425ade8abd30fee89b60bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934854
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056244}

--- chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerImpl.java
+++ chrome/browser/incognito/android/java/src/org/chromium/chrome/browser/incognito/reauth/IncognitoReauthControllerImpl.java
@@ -109,6 +109,23 @@ public class IncognitoReauthControllerImpl
                 }
             };
 
+    private final LayoutStateProvider.LayoutStateObserver mLayoutStateObserver =
+            new LayoutStateProvider.LayoutStateObserver() {
+                @Override
+                public void onStartedShowing(int layoutType, boolean showToolbar) {
+                    if (layoutType == LayoutType.BROWSING) {
+                        showDialogIfRequired();
+                    }
+                }
+
+                @Override
+                public void onFinishedHiding(int layoutType) {
+                    if (layoutType == LayoutType.TAB_SWITCHER) {
+                        hideDialogIfShowing(DialogDismissalCause.DIALOG_INTERACTION_DEFERRED);
+                    }
+                }
+            };
+
     // The {@link TabModelSelectorProfileSupplier} passed to the constructor may not have a {@link
     // Profile} set if the Tab state is not initialized yet. We make use of the {@link Profile} when
     // accessing {@link UserPrefs} in showDialogIfRequired. A null {@link Profile} would result in a
@@ -190,6 +207,7 @@ public class IncognitoReauthControllerImpl
         layoutStateProviderOneshotSupplier.onAvailable(
                 mLayoutStateProviderCallbackController.makeCancelable(layoutStateProvider -> {
                     mLayoutStateProvider = layoutStateProvider;
+                    mLayoutStateProvider.addObserver(mLayoutStateObserver);
                     showDialogIfRequired();
                 }));
 
@@ -222,6 +240,10 @@ public class IncognitoReauthControllerImpl
         mLayoutStateProviderCallbackController.destroy();
         mIncognitoReauthCoordinatorFactory.destroy();
         mOnBackPressedInFullScreenReauthCallback.setEnabled(false);
+
+        if (mLayoutStateProvider != null) {
+            mLayoutStateProvider.removeObserver(mLayoutStateObserver);
+        }
         hideDialogIfShowing(DialogDismissalCause.ACTIVITY_DESTROYED);
     }
 
