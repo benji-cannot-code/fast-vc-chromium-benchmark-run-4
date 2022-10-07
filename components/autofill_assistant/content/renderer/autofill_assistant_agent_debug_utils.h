afb2101b5f54adb6301d60e24dfb232a33c38468
Mathias Carlen <mcarlen@chromium.org>
2022-10-07 16:27:50 +0000

    [AutofillAssistant] Add override used field.
    Before this change there was no good way to know whether a semantic
    element find was using an override or not.
    
    This change adds and plumbs a field from the renderer back to the
    browser that indicates for what semantic lookups an override was used.
    
    BUG=b/247742768
    
    Change-Id: I57e1913ec0c5adad4a5f47bd5afd392cb43df9f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934762
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056338}

--- components/autofill_assistant/content/renderer/autofill_assistant_agent_debug_utils.h
+++ components/autofill_assistant/content/renderer/autofill_assistant_agent_debug_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/flat_map.h"
+#include "components/autofill_assistant/content/renderer/autofill_assistant_model_executor.h"
 #include "third_party/blink/public/web/modules/autofill_assistant/node_signals.h"
 
 namespace autofill_assistant {
@@ -34,7 +35,7 @@ SemanticLabelsPair DecodeSemanticPredictionLabelsJson(std::string encodedJson);
 std::u16string SemanticPredictionResultToDebugString(
     SemanticPredictionLabelMap roles,
     SemanticPredictionLabelMap objectives,
-    const std::pair<int, int>& result,
+    const ModelExecutorResult& result,
     bool ignore_objective);
 
 }  // namespace autofill_assistant
