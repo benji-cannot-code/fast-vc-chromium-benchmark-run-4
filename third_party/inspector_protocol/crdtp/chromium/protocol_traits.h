7f981e1df262bb12d5042f808b967a07f8d77713
Andrey Kosyakov <caseq@chromium.org>
2022-10-08 18:09:45 +0000

    Roll inspector_protocol with updated license boilerplate
    Also update copyright headers for local files.
    
    Bug: 1098010
    Change-Id: I68b6409544469a79278718b1e6776b6068dd0a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939497
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056697}

--- third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
+++ third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef CRDTP_CHROMIUM_PROTOCOL_TYPE_TRAITS_H_
 #define CRDTP_CHROMIUM_PROTOCOL_TYPE_TRAITS_H_
 
