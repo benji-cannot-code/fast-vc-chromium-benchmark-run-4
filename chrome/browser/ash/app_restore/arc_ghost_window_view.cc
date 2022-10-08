e8b6f9ba22e160acc1d014ef284c9d41cbacd200
Shengsong Tan <sstan@google.com>
2022-10-08 10:05:12 +0000

    arc: Add uma metrics for ARC ghost window view type
    This CL add UMA for ghost window view type.
    
    BUG=None
    TEST=None
    
    Change-Id: I05973a9a2a044389358022226d71ca5877a68fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933039
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056658}

--- chrome/browser/ash/app_restore/arc_ghost_window_view.cc
+++ chrome/browser/ash/app_restore/arc_ghost_window_view.cc
@@ -27,6 +27,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+constexpr char kGhostWindowTypeHistogram[] = "Arc.GhostWindowViewType";
+
+// Ghost window view type enumeration; Used for UMA counter.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class GhostWindowType {
+  kIconSpinning = 0,
+  kIconSpinningWithFixupText = 1,
+  kMaxValue = kIconSpinningWithFixupText,
+};
+
 class Throbber : public views::View {
  public:
   explicit Throbber(uint32_t color) : color_(color) {
@@ -127,12 +138,19 @@ void ArcGhostWindowView::SetType(arc::GhostWindowType type) {
       message_label_ = label.get();
       AddChildView(std::move(label));
       Layout();
+
+      base::UmaHistogramEnumeration(
+          kGhostWindowTypeHistogram,
+          GhostWindowType::kIconSpinningWithFixupText);
     }
   } else {
     if (message_label_) {
       RemoveChildView(message_label_);
       message_label_ = nullptr;
       Layout();
+
+      base::UmaHistogramEnumeration(kGhostWindowTypeHistogram,
+                                    GhostWindowType::kIconSpinning);
     }
   }
 }
