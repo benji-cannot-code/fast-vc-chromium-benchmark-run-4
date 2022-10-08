16f8fe38e57d815738e23e3dfd303bc998f2bcbe
Jonah Ryan-Davis <jonahr@google.com>
2022-10-08 00:48:53 +0000

    Forward context loss to all SkiaImageRepresentations.
    SkiaOutputSurfaceImplOnGpu should call OnContextLoss for each of
    the SkiaImageRepresentations. This was causing some backings to
    call the destructor assuming a context was current when it was
    really lost.
    
    Bug: chromium:1370730
    Change-Id: I2fac3bf953b90722fbf04ac441eae4d1f351719f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933066
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056596}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -2131,6 +2131,10 @@ void SkiaOutputSurfaceImplOnGpu::MarkContextLost(ContextLostReason reason) {
   // Release all ongoing AsyncReadResults.
   ReleaseAsyncReadResultHelpers();
 
+  for (auto& entry : skia_representations_) {
+    entry.second->OnContextLost();
+  }
+
   context_state_->MarkContextLost();
   if (context_lost_callback_) {
     PostTaskToClientThread(std::move(context_lost_callback_));
