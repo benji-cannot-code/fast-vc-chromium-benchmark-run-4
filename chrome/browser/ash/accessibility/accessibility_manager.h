9c158a59b8f000ecb655028af548a05377437a84
Katie Dektar <katie@chromium.org>
2022-10-08 00:26:08 +0000

    [ATP] Adds an AccessibilityServiceClient in Ash.
    The AccessibilityServiceClient can communicates to the new service
    via mojom APIs. It passes accessibility feature state changes
    and accessibility events to the service. The AccessibilityManager
    creates an AccessibilityServiceClient and informs it when
    features are toggled.
    
    Methods which will be added to mojom are built out as stubs
    in the client.
    
    This code is behind the flag --enable-features=AccessibilityService,
    and the service itself is behind a gn flag enable_accessibility_service.
    
    Googlers, see go/chromeos-atp-service-process.
    
    AX-Relnotes: N/A
    Bug: 1355633
    Test: New browsertests added.
    Change-Id: Id51b49b0c6267d571d8fd4aa2bc1bd6ddb8add11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936096
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056585}

--- chrome/browser/ash/accessibility/accessibility_manager.h
+++ chrome/browser/ash/accessibility/accessibility_manager.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/accessibility/chromevox_panel.h"
+#include "chrome/browser/ash/accessibility/service/accessibility_service_client.h"
 #include "chrome/browser/extensions/api/braille_display_private/braille_controller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_observer.h"
@@ -55,6 +56,7 @@ enum class SelectToSpeakState;
 enum class SelectToSpeakPanelAction;
 enum class Sound;
 struct AccessibilityFocusRingInfo;
+class AccessibilityServiceClient;
 
 enum class AccessibilityNotificationType {
   kManagerShutdown,
@@ -554,6 +556,8 @@ class AccessibilityManager
   std::set<std::string> accessibility_common_enabled_features_;
 
   AccessibilityStatusCallbackList callback_list_;
+
+  std::unique_ptr<AccessibilityServiceClient> accessibility_service_client_;
 
   bool braille_display_connected_ = false;
   base::ScopedObservation<
