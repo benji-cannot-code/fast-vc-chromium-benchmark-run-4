97397521b7c4cb13987aca98d64210040540e754
Loko Kung <lokokung@google.com>
2022-10-08 20:04:44 +0000

    Plumbs DocumentToken support through for WebGPU caching.
    Bug: dawn:549
    Change-Id: I09ade8db8e340dc8b18d4542d3f6324d4a1ecd4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937023
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056705}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/gpu_disk_cache_factory.h"
 #include "content/browser/gpu/gpu_main_thread_factory.h"
 #include "content/browser/gpu/gpu_memory_buffer_manager_singleton.h"
+#include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/storage_partition_impl.h"
 #include "content/browser/worker_host/dedicated_worker_host.h"
 #include "content/browser/worker_host/dedicated_worker_service_impl.h"
@@ -1077,8 +1078,18 @@ std::string GpuProcessHost::GetIsolationKey(
     int32_t process_id,
     const blink::WebGPUExecutionContextToken& token) {
   if (token.Is<blink::DocumentToken>()) {
-    // TODO(dawn:549) Add DocumentToken support when it is ready.
-    return "";
+    // Return an empty isolation key if the frame host is gone. This could
+    // happen if the frame is destroyed (or being destroyed) in between when we
+    // are trying to get the isolation key.
+    RenderFrameHostImpl* frame_host = RenderFrameHostImpl::FromDocumentToken(
+        process_id, token.GetAs<blink::DocumentToken>());
+    if (!frame_host) {
+      return "";
+    }
+
+    auto isolation_key =
+        frame_host->GetNetworkIsolationKey().ToCacheKeyString();
+    return isolation_key ? *isolation_key : "";
   } else if (token.Is<blink::DedicatedWorkerToken>()) {
     // Return an empty isolation key if the process host or the worker host is
     // gone. This may happen if the worker is destroyed (or being destroyed) in
