639d9dc8f14abad613ec3c57b6b6e56fb9e65b1a
Andrew Pantera <andp@chromium.org>
2022-10-08 00:20:34 +0000

    Same App Window Cycle: Add flag.
    Add feature flag for alt + backtick window cycling.
    
    Bug: b/250706997
    Change-Id: Iff8b4fbcec956e761a68739ac70d343c238f714f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936585
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Reviewed-by: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056583}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1101,6 +1101,11 @@ BASE_FEATURE(kHomeButtonWithText,
 // the Chromebook to share its cellular internet connection to other devices.
 BASE_FEATURE(kHotspot, "Hotspot", base::FEATURE_DISABLED_BY_DEFAULT);
 
+// If enabled, allows the user to cycle between windows of an app using Alt + `.
+BASE_FEATURE(kSameAppWindowCycle,
+             "SameAppWindowCycle",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Controls whether the snooping protection prototype is enabled.
 BASE_FEATURE(kSnoopingProtection,
              "SnoopingProtection",
@@ -2868,6 +2873,10 @@ bool IsReverseScrollGesturesEnabled() {
 
 bool IsRgbKeyboardEnabled() {
   return base::FeatureList::IsEnabled(kRgbKeyboard);
+}
+
+bool IsSameAppWindowCycleEnabled() {
+  return base::FeatureList::IsEnabled(kSameAppWindowCycle);
 }
 
 bool IsSamlNotificationOnPasswordChangeSuccessEnabled() {
