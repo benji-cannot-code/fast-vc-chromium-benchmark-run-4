9d5595ce67dba1dc124276976ccd3cc63ef979b5
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-09 03:53:43 +0000

    Import wpt@cfb1e6bb5b0e86254362270919712ad603151446
    Using wpt-import in Chromium 3b00ce7893bd080dcec8338606d83ba221d86094.
    With Chromium commits locally applied on WPT:
    ff4c2a0b44 "[webdriver] Add Get All/Named Cookie Command"
    279fa6d966 "[bfcache] Do not reset notRestoredReasons upon successful bfcache navigation"
    920765787f "[Fixit] Clean up redundant executor HTML files."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    asully@chromium.org, stevebe@microsoft.com:
      external/wpt/file-system-access
    mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    rego@igalia.com:
      external/wpt/css/css-contain
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Iacf9bd83311f73c54c184cadc1aa2c73e060ada7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940068
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056731}

---
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/resources/dedicated-worker-supporting-revalidation.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+
+
+def main(request, response):
+    headers = []
+    if request.headers.get(b'if-none-match', None):
+        status = 304, u"Not Modified"
+        return status, headers, u""
+    else:
+        headers.append((b"Content-Type", b"text/javascript"))
+        headers.append((b"Cross-Origin-Embedder-Policy", b"require-corp"))
+        headers.append((b"Cache-Control", b"private, max-age=0, must-revalidate"))
+        headers.append((b"ETag", b"abcdef"))
+        status = 200, u"OK"
+        return status, headers, u"self.onmessage = (e) => { self.postMessage('LOADED'); };"
--- third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/key_shortcuts.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/key_shortcuts.py
@@ -1,48 +1,48 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-from tests.perform_actions.support.keys import Keys, MODIFIER_KEY
+from tests.perform_actions.support.keys import Keys
 from tests.perform_actions.support.refine import get_keys
 
 
-def test_mod_a_and_backspace_deletes_all_text(session, key_reporter, key_chain):
+def test_mod_a_and_backspace_deletes_all_text(session, key_reporter, key_chain, modifier_key):
     key_chain.send_keys("abc d") \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("a") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("a") \
              .key_down(Keys.BACKSPACE) \
              .perform()
     assert get_keys(key_reporter) == ""
 
 
-def test_mod_a_mod_c_right_mod_v_pastes_text(session, key_reporter, key_chain):
+def test_mod_a_mod_c_right_mod_v_pastes_text(session, key_reporter, key_chain, modifier_key):
     initial = "abc d"
     key_chain.send_keys(initial) \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("a") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("a") \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("c") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("c") \
              .send_keys([Keys.RIGHT]) \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("v") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("v") \
              .perform()
     assert get_keys(key_reporter) == initial * 2
 
 
-def test_mod_a_mod_x_deletes_all_text(session, key_reporter, key_chain):
+def test_mod_a_mod_x_deletes_all_text(session, key_reporter, key_chain, modifier_key):
     key_chain.send_keys("abc d") \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("a") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("a") \
-             .key_down(MODIFIER_KEY) \
+             .key_down(modifier_key) \
              .key_down("x") \
-             .key_up(MODIFIER_KEY) \
+             .key_up(modifier_key) \
              .key_up("x") \
              .perform()
     assert get_keys(key_reporter) == ""
