a29bb09ecc1ae92390f61b6b9b1a03c225748ed0
Daniel Cheng <dcheng@chromium.org>
2022-10-09 00:14:58 +0000

    Delete base::Feature's copy constructor and assignment operators.
    Bug: 1370572
    Change-Id: I6480299da0c2c4f27b5a8390c2d915776d1771b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940401
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056719}

--- base/test/scoped_feature_list.h
+++ base/test/scoped_feature_list.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/types/pass_key.h"
-#include "build/build_config.h"
 #include "third_party/abseil-cpp/absl/base/attributes.h"
 
 namespace base {
@@ -146,14 +145,6 @@ class ScopedFeatureList final {
   // interactions.
   void InitWithFeatures(const std::vector<FeatureRef>& enabled_features,
                         const std::vector<FeatureRef>& disabled_features);
-#if !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && \
-    !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_APPLE)
-  // TODO(https://crbug.com/1370572): Remove these temporary forwarding helpers.
-  void InitWithFeatures(const std::vector<Feature>& enabled_features,
-                        const std::vector<Feature>& disabled_features);
-  void InitWithFeatures(std::initializer_list<Feature> enabled_features,
-                        std::initializer_list<Feature> disabled_features);
-#endif
 
   // Initializes and registers a FeatureList instance based on the current
   // FeatureList and overridden with single enabled feature.
@@ -176,16 +167,6 @@ class ScopedFeatureList final {
   void InitWithFeaturesAndParameters(
       const std::vector<FeatureAndParams>& enabled_features,
       const std::vector<FeatureRef>& disabled_features);
-#if !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_ANDROID) && \
-    !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_APPLE)
-  // TODO(https://crbug.com/1370572): Remove these temporary forwarding helpers.
-  void InitWithFeaturesAndParameters(
-      const std::vector<FeatureAndParams>& enabled_features,
-      const std::vector<Feature>& disabled_features);
-  void InitWithFeaturesAndParameters(
-      const std::vector<FeatureAndParams>& enabled_features,
-      std::initializer_list<Feature> disabled_features);
-#endif
 
   // Initializes and registers a FeatureList instance based on the current
   // FeatureList and overridden with single disabled feature.
