41415634a8e12b57a6b2c27ec30e3d17b2254d31
Tony Herre <toprice@chromium.org>
2022-10-10 14:30:47 +0000

    Remove unused arg in RTCEncodedAudioStreamTransformerDelegate ctr
    Change-Id: Ia3316fa8af2ce3c11cfa7491b086b75b81171b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942224
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056943}

--- third_party/blink/renderer/platform/peerconnection/rtc_encoded_audio_stream_transformer.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_encoded_audio_stream_transformer.cc
@@ -33,7 +33,6 @@ class RTCEncodedAudioStreamTransformerDelegate
     : public webrtc::FrameTransformerInterface {
  public:
   RTCEncodedAudioStreamTransformerDelegate(
-      RTCEncodedAudioStreamTransformer* transformer,
       scoped_refptr<base::SingleThreadTaskRunner> realm_task_runner,
       scoped_refptr<RTCEncodedAudioStreamTransformer::Broker>
           transformer_broker)
@@ -152,7 +151,6 @@ RTCEncodedAudioStreamTransformer::RTCEncodedAudioStreamTransformer(
     : broker_(base::AdoptRef(new Broker(this))),
       delegate_(
           new rtc::RefCountedObject<RTCEncodedAudioStreamTransformerDelegate>(
-              this,
               std::move(realm_task_runner),
               broker_)) {}
 
