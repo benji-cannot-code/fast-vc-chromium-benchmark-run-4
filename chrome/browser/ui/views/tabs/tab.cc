eda68c7364af7df3461d458da351362d0582bc8e
Jeffrey Young <cowmoo@google.com>
2022-10-10 20:29:44 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=pkasting@chromium.org
    
    Change-Id: I0852c5e654f9069257b5d6a3d25eb77c1769a3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940831
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057127}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/compositor.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/geometry/rect_conversions.h"
--- chrome/browser/ui/views/tabs/tab_icon.cc
+++ chrome/browser/ui/views/tabs/tab_icon.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/favicon_size.h"
