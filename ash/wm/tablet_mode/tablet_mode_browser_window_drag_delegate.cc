806441a98f4b003f52ab0e7c6579827600e1b3fd
Jeffrey Young <cowmoo@google.com>
2022-10-10 17:08:46 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=xdai@chromium.org
    
    Change-Id: I6d638ebfba37eec32ffefa96e2cba60074f6110c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940280
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057006}

--- ash/wm/tablet_mode/tablet_mode_browser_window_drag_delegate.cc
+++ ash/wm/tablet_mode/tablet_mode_browser_window_drag_delegate.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/display/screen.h"
-#include "ui/gfx/color_analysis.h"
-#include "ui/gfx/color_utils.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/coordinate_conversion.h"
 
