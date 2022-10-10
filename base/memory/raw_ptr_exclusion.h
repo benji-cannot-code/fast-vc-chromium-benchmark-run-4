a61ea3e00968f97d31ab2e34a8b5e1a3dfbed4df
Stephan Hartmann <stha09@googlemail.com>
2022-10-10 17:25:56 +0000

    GCC: disable unsupported annotate attribute
    Creates a lot of noise in non-official builds.
    
    Bug: 819294
    Change-Id: Iede1efa84877c8a725271818b0d4177c6e3175c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941629
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057012}

--- base/memory/raw_ptr_exclusion.h
+++ base/memory/raw_ptr_exclusion.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_MEMORY_RAW_PTR_EXCLUSION_H_
 
 #include "base/allocator/buildflags.h"
+#include "base/compiler_specific.h"
 #include "build/build_config.h"
 
+#if HAS_ATTRIBUTE(annotate)
 #if defined(OFFICIAL_BUILD) && !BUILDFLAG(FORCE_ENABLE_RAW_PTR_EXCLUSION)
 // The annotation changed compiler output and increased binary size so disable
 // for official builds.
@@ -18,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Marks a field as excluded from the raw_ptr usage enforcement clang plugin.
 // Example: RAW_PTR_EXCLUSION Foo* foo_;
 #define RAW_PTR_EXCLUSION __attribute__((annotate("raw_ptr_exclusion")))
+#endif
+#else
+#define RAW_PTR_EXCLUSION
 #endif
 
 #endif  // BASE_MEMORY_RAW_PTR_EXCLUSION_H_
