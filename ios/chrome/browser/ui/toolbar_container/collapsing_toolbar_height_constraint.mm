f864c4d7bbb586431e51d77c7cc96a501a33d1be
Jérôme Lebel <jlebel@chromium.org>
2022-10-10 09:29:03 +0000

    [iOS] Switch to ios/chrome/common/ui/util/ui_util.h
    This patch is part of a series of patches to switch import from:
      ios/chrome/browser/ui/util/ui_util.h
    to:
      ios/chrome/common/ui/util/ui_util.h
    
    This is related to:
       crrev.com/c/3936525 Moving methods to the new location
       crrev.com/c/3936523 Cleanup old location
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Change-Id: I3996c4fc890942d1681c4775f55035f19e4aa2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940186
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056859}

--- ios/chrome/browser/ui/toolbar_container/collapsing_toolbar_height_constraint.mm
+++ ios/chrome/browser/ui/toolbar_container/collapsing_toolbar_height_constraint.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/cxx17_backports.h"
 #import "ios/chrome/browser/ui/toolbar_container/collapsing_toolbar_height_constraint_delegate.h"
 #import "ios/chrome/browser/ui/toolbar_container/toolbar_collapsing.h"
-#import "ios/chrome/browser/ui/util/ui_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
