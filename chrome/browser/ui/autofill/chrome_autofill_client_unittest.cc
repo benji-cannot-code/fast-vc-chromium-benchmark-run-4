278be9c33170038633a848725fabcbb5c6ae2878
Norge Vizcay <vizcay@google.com>
2022-10-10 14:40:43 +0000

    [FastCheckout] Support only users with MSBB enabled.
    Bug: 1334642
    Change-Id: I7095bbcd9f171a3e9061ec83467b11e271b7f88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937942
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056949}

--- chrome/browser/ui/autofill/chrome_autofill_client_unittest.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/features.h"
 #include "components/autofill_assistant/browser/public/prefs.h"
 #include "components/prefs/pref_service.h"
+#include "components/unified_consent/pref_names.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::test::ScopedFeatureList;
@@ -52,6 +53,11 @@ class ChromeAutofillClientTest : public ChromeRenderViewHostTestHarness {
 
     personal_data_manager_->SetAutofillProfileEnabled(true);
     personal_data_manager_->SetAutofillCreditCardEnabled(true);
+
+    // Enable MSBB by default. If MSBB has been explicitly turned off, Fast
+    // Checkout is not supported.
+    profile()->GetPrefs()->SetBoolean(
+        unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled, true);
   }
 
   raw_ptr<ChromeAutofillClient> chrome_autofill_client_ = nullptr;
@@ -89,6 +95,20 @@ TEST_F(ChromeAutofillClientTest,
 
   personal_data_manager()->SetAutofillCreditCardEnabled(true);
   personal_data_manager()->SetAutofillProfileEnabled(false);
+  EXPECT_FALSE(client()->IsFastCheckoutSupported());
+}
+
+TEST_F(ChromeAutofillClientTest, NoFastCheckoutSupportWithDisabledMSBB) {
+  ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      {::features::kFastCheckout,
+       autofill_assistant::features::kAutofillAssistant},
+      {});
+
+  // If MSBB has been explicitly turned off, Fast Checkout is not supported.
+  profile()->GetPrefs()->SetBoolean(
+      unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled, false);
+
   EXPECT_FALSE(client()->IsFastCheckoutSupported());
 }
 
