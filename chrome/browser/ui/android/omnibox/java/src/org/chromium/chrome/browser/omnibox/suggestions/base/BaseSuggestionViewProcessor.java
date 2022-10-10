2b03e895224fadfa968855d7554b1af58cfbfab4
Ender <ender@google.com>
2022-10-10 22:47:31 +0000

    [omnibox][grouping] Use Group Header IDs for zone rounding
    The change initiates migration towards implicit suggestion zoning.
    The group header ids are used to define boundaries of rounded
    sections.
    
    Bug: 1343512
    Change-Id: I495d4cb059f68b5a0e3c600824cf4d968f9b53e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933849
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057168}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProcessor.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProcessor.java
@@ -15,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.chromium.chrome.browser.omnibox.MatchClassificationStyle;
-import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.R;
 import org.chromium.chrome.browser.omnibox.suggestions.FaviconFetcher;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionHost;
@@ -39,7 +38,6 @@ public abstract class BaseSuggestionViewProcessor implements SuggestionProcessor
     private final int mDesiredFaviconWidthPx;
     private final int mDecorationImageSizePx;
     private final int mSuggestionSizePx;
-    private boolean mDropdownItemRoundingEnabled;
 
     /**
      * @param context Current context.
@@ -71,11 +69,6 @@ public abstract class BaseSuggestionViewProcessor implements SuggestionProcessor
      */
     protected int getDecorationImageSize() {
         return mDecorationImageSizePx;
-    }
-
-    @Override
-    public void onNativeInitialized() {
-        mDropdownItemRoundingEnabled = OmniboxFeatures.shouldShowModernizeVisualUpdate(mContext);
     }
 
     @Override
@@ -225,13 +218,5 @@ public abstract class BaseSuggestionViewProcessor implements SuggestionProcessor
      */
     protected Context getContext() {
         return mContext;
-    }
-
-    @Override
-    public void onUrlFocusChange(boolean hasFocus) {}
-
-    @Override
-    public boolean allowBackgroundRounding() {
-        return mDropdownItemRoundingEnabled;
     }
 }
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/header/HeaderProcessor.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/header/HeaderProcessor.java
@@ -87,9 +87,6 @@ public class HeaderProcessor implements DropdownItemProcessor {
         }
     }
 
-    @Override
-    public void onUrlFocusChange(boolean hasFocus) {}
-
     /**
      * Signals that native initialization has completed.
      * And cache the feature flag value from the flag.
@@ -109,10 +106,5 @@ public class HeaderProcessor implements DropdownItemProcessor {
 
         mUseUpdatedHeaderPadding =
                 ChromeFeatureList.isEnabled(ChromeFeatureList.OMNIBOX_HEADER_PADDING_UPDATE);
-    }
-
-    @Override
-    public boolean allowBackgroundRounding() {
-        return false;
     }
 }
