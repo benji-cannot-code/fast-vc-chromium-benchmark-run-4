f81abf4ca480893e51ed67ae982bb7607a0f52b8
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-10-10 14:01:42 +0000

    Add Signin.SigninAbortedAccessPoint metric on Android
    Records the event of sign-in being aborted per entry point. Only
    recorded on Android.
    
    Fixed: 1371130
    Change-Id: I07b9fb65e7e3e292ac04e74fb65c6133977047ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934856
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056930}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
@@ -559,6 +559,9 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager {
         mSignInState = null;
         notifyCallbacksWaitingForOperation();
 
+        RecordHistogram.recordEnumeratedHistogram("Signin.SigninAbortedAccessPoint",
+                signInState.getAccessPoint(), SigninAccessPoint.MAX);
+
         if (signInState.mCallback != null) {
             signInState.mCallback.onSignInAborted();
         }
