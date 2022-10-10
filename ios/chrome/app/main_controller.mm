54e130ec1ce734aca2cc396500522ed00fedb3fc
Olivier ROBIN <olivierrobin@google.com>
2022-10-10 14:28:22 +0000

    Call saveFieldTrialValuesForGroupApp
    saveFieldTrialValuesForGroupApp will save data accessible by 1st party
    applications to know Chrome capabilities.
    
    Method was implemented but never called in previous implementation.
    
    Bug: 1371910
    Change-Id: I4a5272756ef3663522de287f3fe8624f816f972a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938223
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056942}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -1037,13 +1037,15 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
                   }];
 }
 
-// Schedule a call to `saveFieldTrialValuesForExtensions` for deferred
-// execution.
-- (void)scheduleSaveFieldTrialValuesForExtensions {
+// Schedule a call to `scheduleSaveFieldTrialValuesForExternals` for deferred
+// execution. Externals can be extensions or 1st party apps.
+- (void)scheduleSaveFieldTrialValuesForExternals {
+  __weak __typeof(self) weakSelf = self;
   [[DeferredInitializationRunner sharedInstance]
       enqueueBlockNamed:kSaveFieldTrialValues
                   block:^{
-                    [self saveFieldTrialValuesForExtensions];
+                    [weakSelf saveFieldTrialValuesForExtensions];
+                    [weakSelf saveFieldTrialValuesForGroupApp];
                   }];
 }
 
@@ -1165,7 +1167,7 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   [self startFreeMemoryMonitoring];
   [self scheduleAppDistributionPings];
   [self initializeMailtoHandling];
-  [self scheduleSaveFieldTrialValuesForExtensions];
+  [self scheduleSaveFieldTrialValuesForExternals];
   [self scheduleEnterpriseManagedDeviceCheck];
   [self scheduleFaviconsCleanup];
 }
