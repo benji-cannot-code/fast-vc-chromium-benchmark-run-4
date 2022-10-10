a3810ddde8fc7572a1f81d84c8cc40584615f78d
Roman Sorokin <rsorokin@chromium.org>
2022-10-10 04:24:53 +0000

    [Code Health] Replace DictionaryValue in ...
    ... c/b/apps and c/b/extensions/external*loader*
    
    Bug: 1366865, 1187061
    Change-Id: I084fe550741876be92b2dd567c51c08a937c5b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936290
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056810}

--- chrome/browser/apps/user_type_filter.h
+++ chrome/browser/apps/user_type_filter.h
@@ -7,13 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_APPS_USER_TYPE_FILTER_H_
 
 #include <string>
+#include "base/values.h"
 
 class Profile;
-
-namespace base {
-class Value;
-class ListValue;
-}  // namespace base
 
 namespace apps {
 
@@ -42,8 +38,8 @@ std::string DetermineUserType(Profile* profile);
 // Safe to call on non-UI thread.
 bool UserTypeMatchesJsonUserType(const std::string& user_type,
                                  const std::string& app_id,
-                                 const base::Value* json_root,
-                                 const base::ListValue* default_user_types);
+                                 const base::Value::Dict& json_root,
+                                 const base::Value::List* default_user_types);
 
 }  // namespace apps
 
