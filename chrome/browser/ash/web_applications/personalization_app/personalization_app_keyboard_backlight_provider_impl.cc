2eaedf7d53ff31dc531ab5c3be206665c4ea2330
Jeffrey Young <cowmoo@google.com>
2022-10-10 18:26:59 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=wutao@chromium.org
    
    Change-Id: I65e1370e9c13011b7d9bd8cb58089d2548da73f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940809
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057059}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_keyboard_backlight_provider_impl.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_keyboard_backlight_provider_impl.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"
 #include "third_party/skia/include/core/SkColor.h"
-#include "ui/gfx/color_analysis.h"
 
 namespace ash::personalization_app {
 
