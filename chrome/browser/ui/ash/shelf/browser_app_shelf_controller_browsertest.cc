d200fe6ef0179721178d081476dcae4518f89654
Eric Willigers <ericwilligers@chromium.org>
2022-10-10 03:02:01 +0000

    BrowserAppShelfControllerBrowserTest: activate
    BrowserAppShelfControllerBrowserTest.ActivateAndMinimizeWindows
    now explicitly activates an app window at the start of the test.
    
    WaitForCondition() accepts a Location, for improved failure reporting.
    
    Bug: 1371757
    Change-Id: I6ae8ff078b9ded50e1d3547d1f6e2a5e13b289ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940650
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056799}

--- chrome/browser/ui/ash/shelf/browser_app_shelf_controller_browsertest.cc
+++ chrome/browser/ui/ash/shelf/browser_app_shelf_controller_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/window_properties.h"
 #include "base/callback.h"
+#include "base/location.h"
 #include "base/run_loop.h"
 #include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
@@ -123,10 +124,10 @@ class TestConditionWaiter : public apps::BrowserAppInstanceObserver,
     OnAnyEvent();
   }
 
-  void Wait(const std::string& message) {
+  void Wait(const base::Location& from_here, const std::string& message) {
     if (!condition_.Run()) {
       base::test::ScopedRunLoopTimeout timeout(
-          FROM_HERE, TestTimeouts::action_timeout(),
+          from_here, TestTimeouts::action_timeout(),
           base::BindLambdaForTesting(
               [&]() { return "Waiting for: " + message; }));
       run_loop_.Run();
@@ -151,7 +152,8 @@ class TestConditionWaiter : public apps::BrowserAppInstanceObserver,
 };
 
 #define WAIT_FOR(condition)                                                   \
-  WaitForCondition(base::BindLambdaForTesting([&]() { return (condition); }), \
+  WaitForCondition(FROM_HERE,                                                 \
+                   base::BindLambdaForTesting([&]() { return (condition); }), \
                    #condition)
 
 struct ExpectedAppMenuItem {
@@ -205,13 +207,14 @@ class BrowserAppShelfControllerBrowserTest
     return apps::AppServiceProxyFactory::GetForProfile(browser()->profile());
   }
 
-  void WaitForCondition(TestConditionWaiter::Condition condition,
+  void WaitForCondition(const base::Location& from_here,
+                        TestConditionWaiter::Condition condition,
                         const std::string& message) {
     auto* proxy =
         apps::AppServiceProxyFactory::GetForProfile(browser()->profile());
     TestConditionWaiter(*registry_, proxy->AppRegistryCache(),
                         std::move(condition))
-        .Wait(message);
+        .Wait(from_here, message);
   }
 
   std::string InstallWebApp(const std::string& start_url,
@@ -626,7 +629,10 @@ IN_PROC_BROWSER_TEST_F(BrowserAppShelfControllerBrowserTest,
   // Both are pinned.
   EXPECT_EQ(ShelfStatus(kAppId_A), ash::STATUS_RUNNING);
   EXPECT_EQ(ShelfStatus(kAppId_B), ash::STATUS_RUNNING);
-  // App B is active.
+
+  // App B window is activated.
+  ASSERT_EQ(SelectShelfItem(kAppId_B),
+            (SelectResult{ash::SHELF_ACTION_WINDOW_ACTIVATED, {}}));
   WAIT_FOR(!IsAppActive(kAppId_A) && IsAppActive(kAppId_B));
 
   // App A window is activated.
