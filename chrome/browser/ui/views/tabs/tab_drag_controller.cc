eda234a02e326951c20fc3b42a48721b901817ac
Marijn Kruisselbrink <mek@chromium.org>
2022-10-10 19:35:18 +0000

    Disable detachable tabs in PWAs on Mac.
    Out of process remote_cocoa windows don't currently work with the tab
    dragging logic for detachable tabs, so disable detachable tabs for
    tabs within such windows.
    
    Bug: 1076777
    Change-Id: Iaac200eb947b37ddfe8162ccdec5bbab653601e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937126
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057097}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -75,6 +75,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_properties.h"
 #endif
 
+#if BUILDFLAG(IS_MAC)
+#include "components/remote_cocoa/browser/window.h"
+#endif
+
 #if defined(USE_AURA)
 #include "ui/aura/env.h"                            // nogncheck
 #include "ui/aura/window.h"                         // nogncheck
@@ -493,6 +497,16 @@ void TabDragController::Init(TabDragContext* source_context,
   mouse_offset_ = mouse_offset;
   last_point_in_screen_ = start_point_in_screen_;
   last_move_screen_loc_ = start_point_in_screen_.x();
+  // Detachable tabs are not supported on Mac if the window is an out-of-process
+  // (remote_cocoa) window, i.e. a PWA window.
+  // TODO(https://crbug.com/1076777): Make detachable tabs work in PWAs on Mac.
+#if BUILDFLAG(IS_MAC)
+  if (source_context_->GetWidget() &&
+      remote_cocoa::IsWindowRemote(
+          source_context_->GetWidget()->GetNativeWindow())) {
+    detach_behavior_ = NOT_DETACHABLE;
+  }
+#endif
 
   source_context_emptiness_tracker_ =
       std::make_unique<SourceTabStripEmptinessTracker>(
