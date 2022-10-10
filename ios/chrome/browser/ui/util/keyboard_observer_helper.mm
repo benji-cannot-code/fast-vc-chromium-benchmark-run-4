8493b51b407caa446dd45c0ee7b95a05604074f8
Jérôme Lebel <jlebel@chromium.org>
2022-10-10 13:32:42 +0000

    [iOS] Switch to ios/chrome/common/ui/util/ui_util.h
    This patch is part of a series of patches to switch import from:
      ios/chrome/browser/ui/util/ui_util.h
    to:
      ios/chrome/common/ui/util/ui_util.h
    
    This is related to:
       crrev.com/c/3936525 Moving methods to the new location
       crrev.com/c/3936523 Cleanup old location
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Change-Id: I463b9a73584e41aeef78920ff305a470f657ed05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939477
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056916}

--- ios/chrome/browser/ui/util/keyboard_observer_helper.mm
+++ ios/chrome/browser/ui/util/keyboard_observer_helper.mm
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/check_op.h"
-#import "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ui/base/device_form_factor.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
--- ios/chrome/browser/ui/util/rtl_geometry.mm
+++ ios/chrome/browser/ui/util/rtl_geometry.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <limits>
 
 #import "base/check_op.h"
-#import "ios/chrome/browser/ui/util/ui_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
