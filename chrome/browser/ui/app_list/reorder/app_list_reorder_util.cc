7ad23db9945e7765801d465aab2721fe32bb4d55
Jeffrey Young <cowmoo@google.com>
2022-10-10 12:17:25 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=nancylingwang@chromium.org
    
    Change-Id: I7a7c36bc65ce50a12aaa873b78f2215fe8d2a257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940830
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056899}

--- chrome/browser/ui/app_list/reorder/app_list_reorder_util.cc
+++ chrome/browser/ui/app_list/reorder/app_list_reorder_util.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColorType.h"
 #include "third_party/skia/include/core/SkScalar.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/gfx/color_analysis.h"
 
 namespace app_list {
 namespace reorder {
