1074d69602449e00d870f969ee9a3ebf77d748f9
Yo Wakita <yowakita@google.com>
2022-10-10 21:24:31 +0000

    Changes imports/exports to lazy load Lens upload dialog.
    Bug: 1373150
    Change-Id: I801dd4a9d3eab1eddc9a0428bc9223f09db0a633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3943006
    Commit-Queue: Yo Wakita <yowakita@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057145}

--- chrome/test/data/webui/new_tab_page/lens_upload_dialog_test.ts
+++ chrome/test/data/webui/new_tab_page/lens_upload_dialog_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://new-tab-page/new_tab_page.js';
 
-import {LensUploadDialogElement} from 'chrome://new-tab-page/new_tab_page.js';
+import {LensUploadDialogElement} from 'chrome://new-tab-page/lazy_load.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
