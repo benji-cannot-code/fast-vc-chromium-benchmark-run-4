68cb507594fae62f1a2dee98ab5a3f8929a8902d
Rakina Zata Amni <rakina@chromium.org>
2022-10-10 16:49:57 +0000

    Remove code for BFCache A/B experiment header
    The origin trial that wanted to use this never ran. Delete all code
    related to this as it's no longer needed.
    
    Bug: 1179313
    Change-Id: I6bdbffbba33e3536c859a62f4b439ef7430ee2de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937793
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056998}

--- third_party/blink/web_tests/http/tests/origin_trials/webexposed/resources/echo-request-sec-bfcache-experiment.php
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-header('Content-Type: text/plain');
-if (isset($_SERVER['HTTP_SEC_BFCACHE_EXPERIMENT']))
-    echo 'Sec-bfcache-experiment: ' . $_SERVER['HTTP_SEC_BFCACHE_EXPERIMENT'];
-else
-    echo 'Sec-bfcache-experiment: <missing>';
-?>
