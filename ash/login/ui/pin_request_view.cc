d2ae5bef61e06ba06d3b5d9bf64c2788c5a8c016
Jeffrey Young <cowmoo@google.com>
2022-10-10 15:09:40 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=rrsilva@google.com
    
    Change-Id: I258c7f45b723834e3da8abdc3df07caf6c52937d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940808
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056965}

--- ash/login/ui/pin_request_view.cc
+++ ash/login/ui/pin_request_view.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/color_analysis.h"
-#include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/label_button.h"
