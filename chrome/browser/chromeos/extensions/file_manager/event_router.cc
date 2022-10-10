9f21d9691707c28198938479e1bb7cc842fcf75c
David Munro <davidmunro@google.com>
2022-10-10 06:15:13 +0000

    guest_os: Remove an unused include, add a missing one
    guest_os_share_path.h no longer needs crostini headers so remove it.
    This breaks event_router.cc due to transitive dependency so add a
    missing include there too.
    
    Bug: b:240491232
    Test: CQ
    
    Change-Id: If68391f4c48db246563c0e112607db6cf33b848e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924613
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056825}

--- chrome/browser/chromeos/extensions/file_manager/event_router.cc
+++ chrome/browser/chromeos/extensions/file_manager/event_router.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/arc/arc_util.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
+#include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/drive/file_system_util.h"
 #include "chrome/browser/ash/file_manager/app_id.h"
