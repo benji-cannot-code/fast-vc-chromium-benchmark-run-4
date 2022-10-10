6e0432cff3e2e8610ffe96a9ccab7118bcaa04fd
Rune Lillesveen <futhark@chromium.org>
2022-10-10 09:17:14 +0000

    Disabling flaky StopOnThreadPool* tests
    Bug: 1372387
    Change-Id: I9f5da85c592ee55dc068bc6a798f3b2d2b89da4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940886
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Owners-Override: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056856}

--- content/browser/tracing/startup_tracing_browsertest.cc
+++ content/browser/tracing/startup_tracing_browsertest.cc
@@ -364,7 +364,8 @@ IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, MAYBE_StopOnUIThread) {
   CheckOutput(GetExpectedPath(), GetOutputType());
 }
 
-IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, StopOnThreadPool) {
+// Flaky: crbug.com/1372387
+IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, DISABLED_StopOnThreadPool) {
   EXPECT_TRUE(NavigateToURL(shell(), GetTestUrl("", "title1.html")));
 
   auto expected_path = GetExpectedPath();
@@ -381,7 +382,8 @@ IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, StopOnThreadPool) {
   run_loop.Run();
 }
 
-IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, StopOnThreadPoolTwice) {
+// Flaky: crbug.com/1372387
+IN_PROC_BROWSER_TEST_P(EmergencyStopTracingTest, DISABLED_StopOnThreadPoolTwice) {
   EXPECT_TRUE(NavigateToURL(shell(), GetTestUrl("", "title1.html")));
 
   auto expected_path = GetExpectedPath();
