3200f3c4d4dad9d512a3a432a899805fbbb51f9a
Joemer Ramos <joemerramos@chromium.org>
2022-10-10 13:30:16 +0000

    [iOS] Enable kEnhancedProtectionPhase2IOS by default
    Before launching the feature, feature should be enabled by default.
    
    Bug: 1327383
    Change-Id: I40adefe57587f2a2965b81432663a67ced9e44c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936340
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056915}

--- ios/chrome/browser/ui/settings/safety_check/safety_check_mediator_unittest.mm
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_mediator_unittest.mm
@@ -331,7 +331,7 @@ TEST_F(SafetyCheckMediatorTest, SafeBrowsingSafeUI) {
     EXPECT_NSEQ(
         mediator_.safeBrowsingCheckItem.detailText,
         GetNSString(
-            IDS_IOS_SETTINGS_SAFETY_CHECK_SAFE_BROWSING_STANDARD_PROTECTION_ENABLED_DESC));
+            IDS_IOS_SETTINGS_SAFETY_CHECK_SAFE_BROWSING_STANDARD_PROTECTION_ENABLED_DESC_WITH_ENHANCED_PROTECTION));
   } else {
     EXPECT_NSEQ(
         mediator_.safeBrowsingCheckItem.detailText,
@@ -363,7 +363,7 @@ TEST_F(SafetyCheckMediatorTest,
   EXPECT_NSEQ(
       mediator_.safeBrowsingCheckItem.detailText,
       GetNSString(
-          IDS_IOS_SETTINGS_SAFETY_CHECK_SAFE_BROWSING_STANDARD_PROTECTION_ENABLED_DESC));
+          IDS_IOS_SETTINGS_SAFETY_CHECK_SAFE_BROWSING_STANDARD_PROTECTION_ENABLED_DESC_WITH_ENHANCED_PROTECTION));
   EXPECT_EQ(mediator_.safeBrowsingCheckItem.trailingImage,
             [[UIImage imageNamed:@"settings_safe_state"]
                 imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]);
@@ -432,7 +432,7 @@ TEST_F(SafetyCheckMediatorTest, SafeBrowsingUnSafeUI) {
   EXPECT_NSEQ(
       mediator_.safeBrowsingCheckItem.detailText,
       GetNSString(IDS_IOS_SETTINGS_SAFETY_CHECK_SAFE_BROWSING_DISABLED_DESC));
-  EXPECT_FALSE(mediator_.safeBrowsingCheckItem.infoButtonHidden);
+  EXPECT_TRUE(mediator_.safeBrowsingCheckItem.infoButtonHidden);
 }
 
 TEST_F(SafetyCheckMediatorTest, SafeBrowsingManagedUI) {
