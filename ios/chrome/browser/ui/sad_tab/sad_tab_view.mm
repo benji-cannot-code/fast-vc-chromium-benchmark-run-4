34b566b3a539a92793d0f6c2f6f1cbc65fde359f
Jérôme Lebel <jlebel@chromium.org>
2022-10-10 09:42:39 +0000

    [iOS] Switch to ios/chrome/common/ui/util/ui_util.h
    This patch is part of a series of patches to switch import from:
      ios/chrome/browser/ui/util/ui_util.h
    to:
      ios/chrome/common/ui/util/ui_util.h
    
    This is related to:
       crrev.com/c/3936525 Moving methods to the new location
       crrev.com/c/3936523 Cleanup old location
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Change-Id: Ie2b669d4cc43861713cad3610733670622f161e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933563
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056866}

--- ios/chrome/browser/ui/sad_tab/sad_tab_view.mm
+++ ios/chrome/browser/ui/sad_tab/sad_tab_view.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 #import "ios/chrome/common/ui/util/text_view_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ios/web/public/browser_state.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "net/base/mac/url_conversions.h"
