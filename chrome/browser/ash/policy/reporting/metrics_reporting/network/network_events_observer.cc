2a8b5ed86ca7c3aec71ed99c69580db6951ff64d
Ahmed Nasr <anasr@google.com>
2022-10-10 18:09:05 +0000

    Migrate NetworkEventsObserver to use NetworkHealthService.
    Use NetworkHealthService instead of CrosHealthd to observe network
    events to avoid call to CrosHealthd while the network events are
    already observed in ash.
    
    Bug: b:244763433
    Change-Id: I1de2b9c8e7a8ec7f54507fbb8fc289bbf39c5c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940051
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057050}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/task/bind_post_task.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "chrome/browser/ash/net/network_health/network_health_manager.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/network/wifi_signal_strength_rssi_fetcher.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
 #include "chromeos/ash/components/network/network_type_pattern.h"
-#include "chromeos/ash/services/cros_healthd/public/cpp/service_connection.h"
 #include "components/reporting/proto/synced/metric_data.pb.h"
 
 namespace reporting {
@@ -104,7 +104,7 @@ void NetworkEventsObserver::OnSignalStrengthChanged(
 }
 
 void NetworkEventsObserver::AddObserver() {
-  ash::cros_healthd::ServiceConnection::GetInstance()->AddNetworkObserver(
+  ash::network_health::NetworkHealthManager::GetInstance()->AddObserver(
       BindNewPipeAndPassRemote());
 }
 
--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_unittest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_unittest.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.h"
 #include "chromeos/ash/components/network/network_handler_test_helper.h"
-#include "chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h"
 #include "chromeos/login/login_state/login_state.h"
 #include "components/reporting/proto/synced/metric_data.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -59,7 +59,7 @@ class NetworkEventsObserverTest
   ~NetworkEventsObserverTest() override = default;
 
   void SetUp() override {
-    ::ash::cros_healthd::FakeCrosHealthd::Initialize();
+    ash::DebugDaemonClient::InitializeFake();
 
     ::chromeos::LoginState::Initialize();
     ::chromeos::LoginState::Get()->SetLoggedInStateAndPrimaryUser(
@@ -89,7 +89,7 @@ class NetworkEventsObserverTest
 
   void TearDown() override {
     ::chromeos::LoginState::Shutdown();
-    ::ash::cros_healthd::FakeCrosHealthd::Shutdown();
+    ash::DebugDaemonClient::Shutdown();
   }
 
   base::test::TaskEnvironment task_environment_;
