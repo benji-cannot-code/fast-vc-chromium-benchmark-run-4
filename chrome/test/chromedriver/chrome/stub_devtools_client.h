6f040bfb91bd18c368b8b014738d568307e28e14
Vladimir Nechaev <nechaev@chromium.org>
2022-10-10 22:52:50 +0000

    DevToolsClient supports CDP traffic tunneling
    DevToolsClient is able to tunnel its CDP traffic via a BiDi protocol.
    
    
    Bug: chromedriver:4181
    Change-Id: Ibf259090d5ba246017fc810d3d50642fd849e088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913128
    Commit-Queue: Vladimir Nechaev <nechaev@chromium.org>
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057174}

--- chrome/test/chromedriver/chrome/stub_devtools_client.h
+++ chrome/test/chromedriver/chrome/stub_devtools_client.h
@@ -23,6 +23,8 @@ class StubDevToolsClient : public DevToolsClient {
   // Overridden from DevToolsClient:
   const std::string& GetId() override;
   const std::string& SessionId() const override;
+  const std::string& TunnelSessionId() const override;
+  void SetTunnelSessionId(const std::string& session_id) override;
   bool IsNull() const override;
   bool WasCrashed() override;
   Status ConnectIfNecessary() override;
@@ -60,6 +62,7 @@ class StubDevToolsClient : public DevToolsClient {
  protected:
   const std::string id_;
   std::string session_id_;
+  std::string tunnel_session_id_;
   std::list<DevToolsEventListener*> listeners_;
 };
 
