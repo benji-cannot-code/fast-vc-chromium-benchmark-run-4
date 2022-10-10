17f1e7a882ae2f565dd616114deb1227292d5807
Jérôme Lebel <jlebel@chromium.org>
2022-10-10 13:39:13 +0000

    [iOS] Switch to ios/chrome/common/ui/util/ui_util.h
    This patch is part of a series of patches to switch import from:
      ios/chrome/browser/ui/util/ui_util.h
    to:
      ios/chrome/common/ui/util/ui_util.h
    
    This is related to:
       crrev.com/c/3936525 Moving methods to the new location
       crrev.com/c/3936523 Cleanup old location
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Change-Id: I60a074428d64f9a7935bcbd8c4ddbb83838fe25b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938075
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056920}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/main/browser_interface_provider.h"
 #import "ios/chrome/browser/ui/main/scene_delegate.h"
-#import "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/web_state_list/session_metrics.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_metrics_browser_agent.h"
 #import "ios/net/cookies/cookie_store_ios.h"
