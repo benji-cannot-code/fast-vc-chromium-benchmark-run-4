caa3c24c2dfd57a4da0bada41580804624fd10d6
Jérôme Lebel <jlebel@chromium.org>
2022-10-10 12:14:10 +0000

    [iOS] Switch to ios/chrome/common/ui/util/ui_util.h
    This patch is part of a series of patches to switch import from:
      ios/chrome/browser/ui/util/ui_util.h
    to:
      ios/chrome/common/ui/util/ui_util.h
    
    This is related to:
       crrev.com/c/3936525 Moving methods to the new location
       crrev.com/c/3936523 Cleanup old location
    
    This CL was uploaded by git cl split.
    
    R=msarda@chromium.org
    
    Change-Id: Ia9ca3ae638d86943f43e8af7049313ce37acfcac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938515
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056898}

--- ios/chrome/browser/signin/chrome_account_manager_service.mm
+++ ios/chrome/browser/signin/chrome_account_manager_service.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/base/signin_pref_names.h"
 #import "ios/chrome/browser/application_context/application_context.h"
 #import "ios/chrome/browser/signin/resized_avatar_cache.h"
-#import "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
