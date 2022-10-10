71a7f9e51fc62ad69966f1183c690b9251bd9e1d
揚帆起航 <uioptt24@gmail.com>
2022-10-10 13:16:38 +0000

    Use helper macros to define base::Features in //components/domain_reliability/features.*
    Change-Id: Ic3f88cc80c4a54bbdf519c45465559052d6ebc85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928424
    Auto-Submit: 揚帆起航 <uioptt24@gmail.com>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056910}

--- components/domain_reliability/features.cc
+++ components/domain_reliability/features.cc
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace domain_reliability {
 namespace features {
 
-DOMAIN_RELIABILITY_EXPORT extern const base::Feature
-    kPartitionDomainReliabilityByNetworkIsolationKey{
-        "PartitionDomainReliabilityByNetworkIsolationKey",
-        base::FEATURE_ENABLED_BY_DEFAULT};
+DOMAIN_RELIABILITY_EXPORT BASE_FEATURE(kPartitionDomainReliabilityByNetworkIsolationKey,
+                                       "PartitionDomainReliabilityByNetworkIsolationKey",
+                                       base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
 }  // namespace domain_reliability
