c439ae68ded7f7a7ec7b39ac3be2a9bf9c932889
sangheon77.kim <sangheon77.kim@samsung.com>
2022-10-11 02:18:16 +0000

    Don't reset CompositorSurfaceManager once a surface is created
    https://chromium-review.googlesource.com/c/chromium/src/+/3760059
    fixed the screen flickering bug when screen is off and on, but the
    patch used a contidion to check if the destroyed surface match the
    created surface.
    
    However, the condition can't cover all problem cases. e.g.,
    1. turn lcd off while text is selected
    2. The opaque surface is destroyed as lcd is off.
    3. A translucent surface is requested because the state of selection
       handle is changed to false as lcd is off.
    4. ScreenStateReceiverWorkaround get the event of lcd off from FW,
       so its mNeedsReset is set to true.
    5. turn lcd on
    6. A translucent surface is created as lcd is on.
    7. ScrrenStateReceiverWorkaround try to clear mNeedsReset because
       surface is created, but the detroyed surface in step2 is different
       from the created surface in step6.
    
    The ScreenStateReceiverWorkaround was required because surface is not
    created when lcd is on. Therefore, this patch clear mNeedsReset if
    surface is created sucessfully when lcd is on, instead of checking if
    the destroyed surface match the created surface.
    
    Bug: 1344407
    Change-Id: I21c09ab85b1fa3ab2064b958d929f23fad757db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937706
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Sangheon Kim <sangheon77.kim@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1057272}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
@@ -118,15 +118,8 @@ public class CompositorView
             }
         }
 
-        public void surfaceDestroyed(Surface surface) {
-            mLastDestroyedSurface = surface;
-        }
-
-        public void surfaceCreated(Surface surface) {
-            // If surface is created successfully when screen is on again, don't
-            // reset CompositorSurfaceManager.
-            mNeedsReset = mNeedsReset && (mLastDestroyedSurface != surface);
-            mLastDestroyedSurface = null;
+        public void clearNeedsReset() {
+            mNeedsReset = false;
         }
     }
 
@@ -451,7 +444,9 @@ public class CompositorView
     public void surfaceCreated(Surface surface) {
         if (mNativeCompositorView == 0) return;
 
-        if (mScreenStateReceiver != null) mScreenStateReceiver.surfaceCreated(surface);
+        // if a requested surface is created successfully, CompositorSurfaceManager doesn't need to
+        // be reset.
+        if (mScreenStateReceiver != null) mScreenStateReceiver.clearNeedsReset();
         mFramesUntilHideBackground = 2;
         mHaveSwappedFramesSinceSurfaceCreated = false;
         updateNeedsDidSwapBuffersCallback();
@@ -474,7 +469,6 @@ public class CompositorView
         CompositorViewJni.get().surfaceDestroyed(mNativeCompositorView, CompositorView.this);
 
         if (mScreenStateReceiver != null) {
-            mScreenStateReceiver.surfaceDestroyed(surface);
             mScreenStateReceiver.maybeResetCompositorSurfaceManager();
         }
     }
