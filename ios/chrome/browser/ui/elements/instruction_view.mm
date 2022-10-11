9ba450f246d964e799be2a15da50018d2d4ea847
Arthur Milchior <arthurmilchior@chromium.org>
2022-10-11 11:12:40 +0000

    [iOS] change InstructionView color
    Change the InstructionView background color and step label color to
    unify all colors between different screens.
    
    Screen (as of patch 11) are on https://drive.google.com/file/d/1OkB2LcfdFJIhj20eq17EthgtxcFBBcsf/view?usp=sharing and https://drive.google.com/file/d/14pJMefuKM_9xeWnXeHDjDAdKqKS01g4W/view?usp=sharing
    
    Fixed: 1371106
    Change-Id: If420ae2499618217115d473ce71802b975c85d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932992
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057394}

--- ios/chrome/browser/ui/elements/instruction_view.mm
+++ ios/chrome/browser/ui/elements/instruction_view.mm
@@ -81,7 +81,7 @@ constexpr CGFloat kIconLabelWidth = 30;
             [UIColor colorNamed:kGroupedSecondaryBackgroundColor];
         break;
       case InstructionViewStyleDefault:
-        self.backgroundColor = [UIColor colorNamed:kGrey100Color];
+        self.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
         break;
     }
     self.layer.cornerRadius = kCornerRadius;
