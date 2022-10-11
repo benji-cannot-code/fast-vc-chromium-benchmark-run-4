24e660f1beee2eb30bcc1c067bf0c027913be8de
Daniel White <danieltwhite@google.com>
2022-10-11 00:22:50 +0000

    Added Authorization Check to PushNotificationUtil function
    In this CL, PushNotificationUtil::RegisterDeviceWithAPNS function
    was extended to check that chrome has push notification permission
    before attempting to register with APNS.
    
    Before this change, it was the responsibility of the caller of
    PushNotificationUtil to check if the application had permission.
    
    Bug: 1373108
    Change-Id: I421cb3b16c25f52508ba1a27e0cd5c457b4f3da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942107
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057241}

--- ios/chrome/browser/push_notification/push_notification_util.mm
+++ ios/chrome/browser/push_notification/push_notification_util.mm
@@ -15,11 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation PushNotificationUtil
 
 + (void)registerDeviceWithAPNS {
-  // iOS instructs that registering the device with APNS must be done on the
-  // main thread. Otherwise, a runtime warning is generated.
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [[UIApplication sharedApplication] registerForRemoteNotifications];
-  });
+  [PushNotificationUtil
+      getPermissionSettings:^(UNNotificationSettings* settings) {
+        if (settings.authorizationStatus == UNAuthorizationStatusAuthorized) {
+          // iOS instructs that registering the device with APNS must be done on
+          // the main thread. Otherwise, a runtime warning is generated.
+          dispatch_async(dispatch_get_main_queue(), ^{
+            [[UIApplication sharedApplication] registerForRemoteNotifications];
+          });
+        }
+      }];
 }
 
 + (void)registerActionableNotifications:
