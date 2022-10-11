0a498675987a4affa22b51bbdd8d126dfe68b02b
David Bertoni <dbertoni@chromium.org>
2022-10-11 22:09:55 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /content/browser/webrtc.
    This CL was uploaded by git cl split.
    
    R=guidou@chromium.org
    
    Bug: 1303949
    Change-Id: Ib9b7656336620cee98786182b48caea7633968dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946505
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057729}

--- content/browser/webrtc/webrtc_internals_unittest.cc
+++ content/browser/webrtc/webrtc_internals_unittest.cc
@@ -250,8 +250,8 @@ TEST_F(WebRtcInternalsTest, EnsureNoLogWhenNoObserver) {
   EXPECT_EQ("update-all-peer-connections", observer.event_name());
 
   ASSERT_TRUE(observer.event_data()->is_list());
-  EXPECT_EQ(1U, observer.event_data()->GetListDeprecated().size());
-  base::Value& dict = observer.event_data()->GetListDeprecated()[0];
+  EXPECT_EQ(1U, observer.event_data()->GetList().size());
+  base::Value& dict = observer.event_data()->GetList()[0];
   ASSERT_TRUE(dict.is_dict());
   ASSERT_FALSE(dict.FindPath("log"));
 
@@ -277,8 +277,8 @@ TEST_F(WebRtcInternalsTest, EnsureLogIsRemovedWhenObserverIsRemoved) {
   EXPECT_EQ("update-all-peer-connections", observer.event_name());
 
   ASSERT_TRUE(observer.event_data()->is_list());
-  EXPECT_EQ(1U, observer.event_data()->GetListDeprecated().size());
-  base::Value& dict = observer.event_data()->GetListDeprecated()[0];
+  EXPECT_EQ(1U, observer.event_data()->GetList().size());
+  base::Value& dict = observer.event_data()->GetList()[0];
   ASSERT_TRUE(dict.is_dict());
   ASSERT_TRUE(dict.FindPath("log")->is_list());
 
@@ -288,8 +288,8 @@ TEST_F(WebRtcInternalsTest, EnsureLogIsRemovedWhenObserverIsRemoved) {
   EXPECT_EQ("update-all-peer-connections", observer.event_name());
 
   ASSERT_TRUE(observer.event_data()->is_list());
-  EXPECT_EQ(1U, observer.event_data()->GetListDeprecated().size());
-  base::Value& updated_dict = observer.event_data()->GetListDeprecated()[0];
+  EXPECT_EQ(1U, observer.event_data()->GetList().size());
+  base::Value& updated_dict = observer.event_data()->GetList()[0];
   ASSERT_TRUE(updated_dict.is_dict());
   ASSERT_FALSE(updated_dict.FindPath("log"));
 
@@ -493,7 +493,7 @@ TEST_F(WebRtcInternalsTest, SendAllUpdatesWithPeerConnectionUpdate) {
   ASSERT_TRUE(observer.event_data());
 
   ASSERT_TRUE(observer.event_data()->is_list());
-  base::Value::ConstListView list = observer.event_data()->GetListDeprecated();
+  const base::Value::List& list = observer.event_data()->GetList();
   EXPECT_EQ(1U, list.size());
 
   ASSERT_TRUE(list.begin()->is_dict());
