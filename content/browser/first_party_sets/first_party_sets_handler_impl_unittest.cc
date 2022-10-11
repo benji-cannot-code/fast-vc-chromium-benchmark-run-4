f56d21c3012c0c8d7501664ab3c2db0657d2c1c0
Shuran Huang <shuuran@chromium.org>
2022-10-11 20:53:45 +0000

    Call FPS DB to get sites to clear and update clear status once done
    This change is the reland of the second half of the
    I406be199255b08b962f423b21ca5ca2a4e17bf4c
    "Implement ClearSiteDataOnChangedSetsForContext method for FPS" with
    a bit more test coverage. The first half is submitted in
    crrev.com/c/3938199.
    
    This change is safe to reland with either of the following being true:
    1. FPS transition clearing is currently disabled by default; and
    2. incompatible databases will be deleted with crrev.com/c/3941965.
    
    Bug: 1219656, 1372445
    Change-Id: I89bc97e39d8f806da0ece4e60faca166c1f32ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945839
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057676}

--- content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ::testing::Eq;
 using ::testing::IsEmpty;
+using ::testing::Optional;
 using ::testing::Pair;
 using ::testing::SizeIs;
 using ::testing::UnorderedElementsAre;
@@ -67,6 +68,14 @@ absl::optional<net::GlobalFirstPartySets> GetPersistedGlobalSetsAndWait(
     const std::string& browser_context_id) {
   base::test::TestFuture<absl::optional<net::GlobalFirstPartySets>> future;
   FirstPartySetsHandlerImpl::GetInstance()->GetPersistedGlobalSetsForTesting(
+      browser_context_id, future.GetCallback());
+  return future.Take();
+}
+
+absl::optional<bool> HasEntryInBrowserContextsClearedAndWait(
+    const std::string& browser_context_id) {
+  base::test::TestFuture<absl::optional<bool>> future;
+  FirstPartySetsHandlerImpl::GetInstance()->HasBrowserContextClearedForTesting(
       browser_context_id, future.GetCallback());
   return future.Take();
 }
@@ -290,6 +299,10 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
 
   FirstPartySetsHandlerImpl::GetInstance()->Init(scoped_dir_.GetPath(),
                                                  LocalSetDeclaration());
+
+  EXPECT_THAT(HasEntryInBrowserContextsClearedAndWait(browser_context_id),
+              Optional(false));
+
   ASSERT_THAT(GetSetsAndWait().FindEntries({foo, associated},
                                            net::FirstPartySetsContextConfig()),
               UnorderedElementsAre(
@@ -318,6 +331,8 @@ TEST_F(FirstPartySetsHandlerImplEnabledTest,
           Pair(associated,
                net::FirstPartySetEntry(foo, net::SiteType::kAssociated,
                                        absl::nullopt))));
+  EXPECT_THAT(HasEntryInBrowserContextsClearedAndWait(browser_context_id),
+              Optional(true));
 
   histogram.ExpectUniqueSample(
       kFirstPartySetsClearSiteDataOutcomeHistogram,
