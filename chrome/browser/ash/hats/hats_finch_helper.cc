601e75826a8850e285f69538200dddf708a4e971
Jack Shira <jackshira@google.com>
2022-10-11 00:17:39 +0000

    [CrOS HaTS] Rework HaTS/UMA integration
    Moving forward, we will allow surveys to opt-in to the sending
    of all survey questions to both Lisntr and UMA. This CL provides
    the infrastructure for a survey to flag on UMA metrics for their
    survey by providing a `histogram_name` parameter to their survey
    feature. Since it is important for the UMA metrics to be tied to
    the HaTS survey ID, it requires both of those values to be provided
    in a way that can be updated out-of-band of normal ChromeOS releases.
    
    Testing this new change involved running ChromeOS on Linux with specific
    survey feature parameters to enable the survey for testing purposes. The
    histograms produced were confirmed via chrome://histograms. The flags
    used are as follows:
    
    --force-happiness-tracking-system
    --enable-features=HappinessTrackingSystem:prob/1.0/trigger_id/test/histogram_name/ChromeOS.HaTS.TestSurvey
    
    BUG=b:208685719
    TEST=ran unit tests
    TEST=manually verified histograms via steps above
    
    Change-Id: Ic3ce453c147d5f5f49d70559be0f5adf5e6101fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923592
    Commit-Queue: Jack Shira <jackshira@google.com>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057239}

--- chrome/browser/ash/hats/hats_finch_helper.cc
+++ chrome/browser/ash/hats/hats_finch_helper.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/field_trial_params.h"
 #include "base/rand_util.h"
+#include "base/strings/string_util.h"
 #include "chrome/browser/ash/hats/hats_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_features.h"
@@ -31,10 +32,37 @@ const char HatsFinchHelper::kSurveyStartDateMsParam[] = "survey_start_date_ms";
 // static
 const char HatsFinchHelper::kTriggerIdParam[] = "trigger_id";
 
+const char HatsFinchHelper::kHistogramNameParam[] = "histogram_name";
+
 std::string HatsFinchHelper::GetTriggerID(const HatsConfig& hats_config) {
   DCHECK(base::FeatureList::IsEnabled(hats_config.feature));
   return base::GetFieldTrialParamValueByFeature(hats_config.feature,
                                                 kTriggerIdParam);
+}
+
+// To enable UMA collection for a specific survey, the Finch configuration
+// file for this survey should be updated to include a `histogram_name`
+// parameter along side the `trigger_id` parameter. Without this
+// `histogram_name` parameter specified, no survey-specific UMA data will be
+// collected.
+std::string HatsFinchHelper::GetHistogramName(const HatsConfig& hats_config) {
+  DCHECK(base::FeatureList::IsEnabled(hats_config.feature));
+  // Fetch the histogram name from the feature parameters, if it is assigned.
+  // An empty string will be returned if the parameter is not set in Finch.
+  // This value should be a valid histogram that has been registered in the
+  // histograms.xml file, otherwise it will not be ingested by UMA.
+  std::string histogram_name = base::GetFieldTrialParamValueByFeature(
+      hats_config.feature, kHistogramNameParam);
+
+  // Valid histogram names for HaTS/UMA integration must start with the
+  // prefix "ChromeOS.HaTS.". This corresponds to the histogram definition
+  // in the histograms.xml file.
+  if (!base::StartsWith(histogram_name, "ChromeOS.HaTS.")) {
+    LOG(ERROR) << "Invalid HaTS histogram name: " << histogram_name;
+    return std::string();
+  }
+
+  return histogram_name;
 }
 
 std::string HatsFinchHelper::GetCustomClientDataAsString(
@@ -64,7 +92,7 @@ HatsFinchHelper::HatsFinchHelper(Profile* profile,
   CheckForDeviceSelection();
 }
 
-HatsFinchHelper::~HatsFinchHelper() {}
+HatsFinchHelper::~HatsFinchHelper() = default;
 
 void HatsFinchHelper::LoadFinchParamValues(const HatsConfig& hats_config) {
   if (!base::FeatureList::IsEnabled(hats_config.feature))
