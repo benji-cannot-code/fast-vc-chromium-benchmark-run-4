83fe2511d06f20a71d36d295382999202006d5c3
Ken Buchanan <kenrb@chromium.org>
2022-10-11 18:50:24 +0000

    [WebAuthn] Make passkeys display above passwords on TTF sheet
    When using WebAuthn Conditional UI on Android, passwords currently
    display above passkeys on the Touch-To-Fill bottom sheet. This change
    reverses that.
    
    This is covered by a test in a subsequent CL: See
    TouchToFillControllerTest.testShowCredentialsWithWebAuthnAndPasswordEntries in
    https://chromium-review.googlesource.com/c/chromium/src/+/3928137.
    
    Bug: 1371186
    Change-Id: I8f7307e7363002a517e92d2ace88502bbf7d17b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935162
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057577}

--- chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
+++ chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
@@ -125,8 +125,8 @@ public class TouchToFillIntegrationTest {
         });
         BottomSheetTestSupport.waitForOpen(mBottomSheetController);
 
-        pollUiThread(() -> getCredentials().getChildAt(2) != null);
-        TouchCommon.singleClickView(getCredentials().getChildAt(2));
+        pollUiThread(() -> getCredentials().getChildAt(1) != null);
+        TouchCommon.singleClickView(getCredentials().getChildAt(1));
 
         waitForEvent(mMockBridge).onWebAuthnCredentialSelected(sCam);
         verify(mMockBridge, never()).onDismissed();
