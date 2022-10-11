71b77f5d38447edcc25b20fe59fe3ea51d5bb4ca
Ramin Halavati <rhalavati@chromium.org>
2022-10-11 08:10:58 +0000

    Enable ProtoConvertor tests for MainContentExtraction.
    MainContentExtraction tests are update based on recent update in
    ProtoConvertor and tests are updated.
    
    Bug: 1278249
    Change-Id: Icea383587e97d4d58916a881f8bfd20d20441296
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941687
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057354}

--- ui/accessibility/test_ax_tree_update_json_reader.cc
+++ ui/accessibility/test_ax_tree_update_json_reader.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/accessibility/test_ax_tree_update_json_reader.h"
 
+#include <algorithm>
+
 #include "base/containers/contains.h"
 #include "base/containers/flat_set.h"
 #include "base/numerics/clamped_math.h"
@@ -336,6 +338,10 @@ AXTreeUpdate AXTreeUpdateFromJSON(const base::Value& json,
 
   tree_update.root_id =
       AddNode(tree_update, json.GetList().front(), role_conversions);
+
+  // |AddNode| adds child nodes before parent nodes, while AXTree deserializer
+  // expects parents first.
+  std::reverse(tree_update.nodes.begin(), tree_update.nodes.end());
 
   return tree_update;
 }
