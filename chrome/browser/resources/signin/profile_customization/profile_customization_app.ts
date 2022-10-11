d1548a1523aeec6bca75860d3b3a0547d2a70f53
Gabriel Oliveira <gabolvr@google.com>
2022-10-11 14:30:43 +0000

    [Profiles] Validate name input on focus out in Profile Customization
    When creating a new local profile, the name input is empty and invalid,
    thus the "Done" button is disabled, but no error message is displayed
    yet if the user tries to click on it.
    
    This CL displays an error message in case the user hasn't interacted
    with the name input when the input loses focus (by clicking on the Done
    button, for instance).
    
    Preview: https://screencast.googleplex.com/cast/NjU1NzQ4NDM2Njk1NDQ5NnxmYTRmNzg4ZS05MQ
    
    Fixed: 1371154
    Change-Id: I54cfad837123d89acd0dadb280400435f38819b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941692
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057448}

--- chrome/browser/resources/signin/profile_customization/profile_customization_app.ts
+++ chrome/browser/resources/signin/profile_customization/profile_customization_app.ts
@@ -217,6 +217,10 @@ export class ProfileCustomizationAppElement extends
   private closeSelectAvatar_() {
     this.$.viewManager.switchView('customizeDialog', 'fade-in', 'fade-out');
   }
+
+  private validateInputOnBlur_() {
+    this.$.nameInput.validate();
+  }
 }
 
 declare global {
