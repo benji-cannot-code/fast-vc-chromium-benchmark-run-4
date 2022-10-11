5e1b536e29665a3254b4ac66a419c2f6e4de0697
Daniel Murphy <dmurph@google.com>
2022-10-11 18:45:19 +0000

    [dPWA] Make sure locks are acquired async
    This prevents possibly flaky behavior being hidden in tests that only
    shows up in production (when locks have contention).
    
    R=phillis@chromium.org
    
    Bug: 1354312
    Change-Id: I420363a31da0d1ad5024c8b02bc4f853c336caab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936599
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057576}

--- chrome/browser/web_applications/commands/fetch_manifest_and_install_command_browsertest.cc
+++ chrome/browser/web_applications/commands/fetch_manifest_and_install_command_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/web_app_controller_browsertest.h"
 #include "chrome/browser/web_applications/test/web_app_test_utils.h"
@@ -156,6 +157,9 @@ IN_PROC_BROWSER_TEST_F(FetchManifestAndInstallCommandTest,
           }),
       /*use_fallback=*/false);
 
+  // Create a new tab to ensure that the browser isn't destroyed with the web
+  // contents closing.
+  chrome::NewTab(browser());
   web_contents->Close();
 
   loop.Run();
