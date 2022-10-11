2a34473ae9dd3008aeb79280647b670bfcd52449
Courtney Wong <courtneywong@chromium.org>
2022-10-11 19:49:09 +0000

    Create a JS interface for the ParentAccessUIHandler
    The ParentAccessUiHandler caches the handler object, which can be utilized by the parent access widget.
    
    
    Bug: b/247151955
    Change-Id: Ie7746ee39ea2676cbc81efcbcd5ab5a49bce3957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902394
    Reviewed-by: Dan S <danan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057616}

---
+++ chrome/browser/resources/chromeos/parent_access/parent_access_ui_handler.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ParentAccessUIHandler, ParentAccessUIHandlerRemote} from './parent_access_ui.mojom-webui.js';
+
+/** @type {?ParentAccessUIHandlerRemote} */
+let parentAccessUIHandler = null;
+
+/** @return  {!ParentAccessUIHandlerRemote} */
+export function getParentAccessUIHandler() {
+  if (!parentAccessUIHandler) {
+    parentAccessUIHandler = ParentAccessUIHandler.getRemote();
+  }
+  return parentAccessUIHandler;
+}
