b2de17db919c122fb99968b581a1b64807745111
Shuran Huang <shuuran@chromium.org>
2022-10-11 15:08:20 +0000

    Update FPS db to handle `Service` site type
    Make free function DeserializeSiteType a static method of FPSEntry, and
    fail on unknown site type for debug build.
    
    Bug: 1219656, 1349781
    Change-Id: I8daff53b312275c400c359aa5f3a37dff6a5d7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939752
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057465}

--- content/browser/first_party_sets/database/first_party_sets_database_unittest.cc
+++ content/browser/first_party_sets/database/first_party_sets_database_unittest.cc
@@ -1001,9 +1001,11 @@ TEST_F(FirstPartySetsDatabaseTest, PersistSets_FormatCheck) {
   const std::string browser_context_id = "b";
   const net::SchemefulSite primary(GURL("https://aaa.test"));
   const net::SchemefulSite associated_site(GURL("https://bbb.test"));
+  const net::SchemefulSite service_site(GURL("https://ccc.test"));
 
-  const net::SchemefulSite manual_primary(GURL("https://ccc.test"));
-  const net::SchemefulSite manual_associated_site(GURL("https://ddd.test"));
+  const net::SchemefulSite manual_primary(GURL("https://ddd.test"));
+  const net::SchemefulSite manual_associated_site(GURL("https://eee.test"));
+  const net::SchemefulSite manual_service_site(GURL("https://fff.test"));
 
   const net::SchemefulSite config_primary_site(GURL("https://example.test"));
   const net::SchemefulSite config_site_member1(GURL("https://member1.test"));
@@ -1013,6 +1015,9 @@ TEST_F(FirstPartySetsDatabaseTest, PersistSets_FormatCheck) {
       /*entries=*/{{associated_site,
                     net::FirstPartySetEntry(primary, net::SiteType::kAssociated,
                                             absl::nullopt)},
+                   {service_site,
+                    net::FirstPartySetEntry(primary, net::SiteType::kService,
+                                            absl::nullopt)},
                    {primary,
                     net::FirstPartySetEntry(primary, net::SiteType::kPrimary,
                                             absl::nullopt)}},
@@ -1020,6 +1025,9 @@ TEST_F(FirstPartySetsDatabaseTest, PersistSets_FormatCheck) {
   base::flat_map<net::SchemefulSite, net::FirstPartySetEntry> manual_sets = {
       {manual_associated_site,
        net::FirstPartySetEntry(manual_primary, net::SiteType::kAssociated,
+                               absl::nullopt)},
+      {manual_service_site,
+       net::FirstPartySetEntry(manual_primary, net::SiteType::kService,
                                absl::nullopt)},
       {manual_primary,
        net::FirstPartySetEntry(manual_primary, net::SiteType::kPrimary,
