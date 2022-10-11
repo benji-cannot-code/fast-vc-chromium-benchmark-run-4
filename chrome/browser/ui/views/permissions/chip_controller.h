65122d09b23c1d1d48cafe3b8e5fd21c833f6ff8
Andy Paicu <andypaicu@chromium.org>
2022-10-11 13:36:46 +0000

    Use base::DoNothing() instead of having a dummy class function
    Bug: 1364276
    Change-Id: I277024adc11de865309e7c7b543f1d3f33db2e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945050
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057428}

--- chrome/browser/ui/views/permissions/chip_controller.h
+++ chrome/browser/ui/views/permissions/chip_controller.h
@@ -165,9 +165,6 @@ class ChipController : public permissions::PermissionRequestManager::Observer,
   // Updates chip icon, text and theme with model.
   void SyncChipWithModel();
 
-  // NOP method used for callbacks.
-  void DoNothing() {}
-
   // Opens the Page Info Dialog.
   void ShowPageInfoDialog();
 
