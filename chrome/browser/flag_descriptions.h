fb8bd99037717f564ca7332f6dc88ac19605edfe
Marlon Facey <mfacey@chromium.org>
2022-10-11 02:34:09 +0000

    [journeys] Add flag for side panel journeys queryless view
    Bug: 1310280
    Change-Id: I7dae484ad77c1cc91cbcd014374a24c92df5be82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930693
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057278}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1486,6 +1486,10 @@ extern const char kSidePanelJourneysFlagId[];
 extern const char kSidePanelJourneysName[];
 extern const char kSidePanelJourneysDescription[];
 
+extern const char kSidePanelJourneysQuerylessFlagId[];
+extern const char kSidePanelJourneysQuerylessName[];
+extern const char kSidePanelJourneysQuerylessDescription[];
+
 extern const char kSidePanelWebViewName[];
 extern const char kSidePanelWebViewDescription[];
 
