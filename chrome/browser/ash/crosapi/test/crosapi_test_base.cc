14b2cc27020c6c21062a7eb6ee81049502db2e93
Anna Sato <annasato@google.com>
2022-10-11 12:38:50 +0000

    Introduce template class to bind crosapi
    In each crosapi test, we need to bind the crosapi we use.
    The function |BindCrosapiInterface()| provides an easy way to bind it.
    
    It will be used only in ash crosapi test.
    
    Bug: 1357441
    Change-Id: Icc91f77ed4ccec77c3e91c395fc2d3c969dcbb68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924660
    Commit-Queue: Anna Sato <annasato@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057411}

--- chrome/browser/ash/crosapi/test/crosapi_test_base.cc
+++ chrome/browser/ash/crosapi/test/crosapi_test_base.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,4 @@ void CrosapiTestBase::SetUp() {
   ASSERT_TRUE(AshCrosapiTestEnv::GetInstance()->IsValid());
 }
 
-mojo::Remote<mojom::Crosapi>& CrosapiTestBase::GetCrosapiRemote() {
-  return AshCrosapiTestEnv::GetInstance()->GetCrosapiRemote();
-}
 }  // namespace crosapi
