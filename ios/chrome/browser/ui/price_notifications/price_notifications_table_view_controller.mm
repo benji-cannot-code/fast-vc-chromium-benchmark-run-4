667a6b1bba90ef6aac5d6aac569daad12edbe3fb
Daniel White <danieltwhite@google.com>
2022-10-11 20:23:41 +0000

    Implemented PriceNotificationsTableViewItem
    In this CL, the new PriceNotificationsTableViewItem was defined and the
    PriceNotificationsTableViewController was adapted to initialize and
    display the new TableViewItem.
    
    In a future CL, the PriceNotificationsPriceChipView will be implemented
    and the TableViewItem will adapted to use the new price chips.
    
    Bug: 1368700
    Change-Id: Iacd8a15efcb2a91ca2d9169f305c627575f5ab23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939753
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057644}

--- ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.mm
+++ ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.h"
 
 #import "base/mac/foundation_util.h"
+#import "ios/chrome/browser/ui/price_notifications/cells/price_notifications_table_view_item.h"
 #import "ios/chrome/browser/ui/price_notifications/price_notifications_constants.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_link_header_footer_item.h"
@@ -89,8 +90,10 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
 
 // Loads the TableViewItems into self.tableViewModel.
 - (void)loadItems {
-  NSMutableArray<TableViewItem*>* trackableArray = [NSMutableArray array];
-  NSMutableArray<TableViewItem*>* trackedArray = [NSMutableArray array];
+  NSMutableArray<PriceNotificationsTableViewItem*>* trackableArray =
+      [NSMutableArray array];
+  NSMutableArray<PriceNotificationsTableViewItem*>* trackedArray =
+      [NSMutableArray array];
   // TODO(crbug.com/1373071) Once the PriceNotificationsMediator has been
   // created, the mediator will be called to populate the `trackableArray` and
   // `trackedArray` objects.
@@ -102,7 +105,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
 
 // Adds `items` to self.tableViewModel for the section designated by
 // `sectionID`.
-- (void)loadItemsFromArray:(NSArray<TableViewItem*>*)items
+- (void)loadItemsFromArray:(NSArray<PriceNotificationsTableViewItem*>*)items
                  toSection:(SectionIdentifier)sectionID {
   TableViewModel* model = self.tableViewModel;
   [model addSectionWithIdentifier:sectionID];
@@ -115,7 +118,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
 
   [model setHeader:[self createHeaderForSectionIndex:sectionID isEmpty:NO]
       forSectionWithIdentifier:sectionID];
-  for (TableViewItem* item in items) {
+  for (PriceNotificationsTableViewItem* item in items) {
     [model addItem:item toSectionWithIdentifier:sectionID];
   }
 }
