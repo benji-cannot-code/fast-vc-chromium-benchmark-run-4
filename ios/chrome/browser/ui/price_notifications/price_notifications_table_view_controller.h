17eb73b9ac918320a90882f2db8577a9cf63ad9d
Daniel White <danieltwhite@google.com>
2022-10-11 17:46:15 +0000

    Implemented PriceNotificationsTableViewController
    In this CL, the new PriceNotificationsTableViewController was defined
    and the PriceNotificationsCoordinator was adapted to initialize and the
    tableViewController.
    
    In a future CL, the PriceNotificationsTableCells will be implemented and
    the tableview will be adapted to use these new cells.
    
    Bug: 1362349
    Bug: 1362348
    Bug: 1362347
    Change-Id: I9cc6a84d51023bb5de63e63126df64adea090cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938491
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057543}

---
+++ ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_TABLE_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller.h"
+
+// View controller that displays PriceNotifications list items in a table view.
+@interface PriceNotificationsTableViewController : ChromeTableViewController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_PRICE_NOTIFICATIONS_PRICE_NOTIFICATIONS_TABLE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/ui/price_notifications/price_notifications_view_coordinator.mm
+++ ios/chrome/browser/ui/price_notifications/price_notifications_view_coordinator.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
+#import "ios/chrome/browser/ui/price_notifications/price_notifications_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller.h"
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller_constants.h"
@@ -23,7 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong)
     TableViewNavigationController* navigationController;
 // The view controller used to display price notifications.
-@property(nonatomic, strong) ChromeTableViewController* tableViewController;
+@property(nonatomic, strong)
+    PriceNotificationsTableViewController* tableViewController;
 
 @end
 
@@ -32,11 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator
 
 - (void)start {
-  // TODO(crbug.com/1368700) Once the PriceNotificationTableViewController is
-  // created, add the code that instantiates it and sets it to coordinator's
-  // `tableViewController` property.
-  self.tableViewController =
-      [[ChromeTableViewController alloc] initWithStyle:ChromeTableViewStyle()];
+  self.tableViewController = [[PriceNotificationsTableViewController alloc]
+      initWithStyle:ChromeTableViewStyle()];
 
   // Add the "Done" button and hook it up to stop.
   UIBarButtonItem* dismissButton = [[UIBarButtonItem alloc]
