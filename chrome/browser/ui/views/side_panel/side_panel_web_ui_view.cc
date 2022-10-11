5743e8d0a277fc6a75c4f8b6c6d84b008285e414
Emily Shack <emshack@chromium.org>
2022-10-11 23:21:01 +0000

    Don't request focus when showing side panel
    Fix for a launch-blocking a11y bug, where the combobox lost focus when
    using the keyboard to open a new side panel.
    
    Bug: 1373497
    Change-Id: I7ff30e448d424d19bd7cc2df6bf1ba980cfe9e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945943
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057770}

--- chrome/browser/ui/views/side_panel/side_panel_web_ui_view.cc
+++ chrome/browser/ui/views/side_panel/side_panel_web_ui_view.cc
@@ -71,7 +71,8 @@ void SidePanelWebUIView::ViewHierarchyChanged(
 void SidePanelWebUIView::ShowUI() {
   SetVisible(true);
   SidePanelUtil::GetSidePanelContentProxy(this)->SetAvailable(true);
-  RequestFocus();
+  if (!base::FeatureList::IsEnabled(features::kUnifiedSidePanel))
+    RequestFocus();
   if (on_show_cb_)
     on_show_cb_.Run();
 }
