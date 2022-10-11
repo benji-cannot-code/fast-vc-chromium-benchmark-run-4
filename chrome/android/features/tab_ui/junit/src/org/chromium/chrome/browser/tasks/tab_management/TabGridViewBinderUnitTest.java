b52925a54e59366b33468c85e37402438598de38
Sirisha Kavuluru <skavuluru@google.com>
2022-10-11 21:24:04 +0000

    [GTS] Enable GTS on ToT Launch approval: https://launch.corp.google.com/launch/4201098
    Bug: 1293881
    Change-Id: I68008310b096f45f78fce8f8ad1f81d13bd8972f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942667
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057698}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinderUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinderUnitTest.java
@@ -103,6 +103,7 @@ public final class TabGridViewBinderUnitTest {
 
     @Test
     public void bindClosableTabWithCardWidth_updateCardAndThumbnail() {
+        TabUiFeatureUtilities.setTabletGridTabSwitcherPolishEnabledForTesting(false);
         // Update width.
         // updatedBitmapWidth = updatedCardWidth - margins = 200 - 40 = 160.
         // updatedBitmapHeight = INIT_HEIGHT - margins = 200 - 40 - 160.
@@ -127,6 +128,7 @@ public final class TabGridViewBinderUnitTest {
 
     @Test
     public void bindClosableTabWithCardWidth_updateCardAndThumbnail_notSelected() {
+        TabUiFeatureUtilities.setTabletGridTabSwitcherPolishEnabledForTesting(false);
         // Update width.
         // updatedBitmapWidth = updatedCardWidth - margins = 200 - 40 = 160.
         // updatedBitmapHeight = INIT_HEIGHT - margins = 200 - 40 - 160.
