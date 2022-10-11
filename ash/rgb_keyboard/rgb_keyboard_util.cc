de137d80b60666e6ccc2043ea1fa22f359bd72ab
Jeffrey Young <cowmoo@google.com>
2022-10-11 22:16:41 +0000

    color_analysis: rm unused import
    This CL was uploaded by git cl split.
    
    R=zentaro@chromium.org
    
    Change-Id: Iea43fcb93ac066e18a9b634cc77d027cc07e793d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941136
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Auto-Submit: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057738}

--- ash/rgb_keyboard/rgb_keyboard_util.cc
+++ ash/rgb_keyboard/rgb_keyboard_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom.h"
-#include "ui/gfx/color_analysis.h"
 
 namespace ash {
 
