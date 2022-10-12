a926749f0da838a8bd6d3171d5cf75ca7658086b
Brandon Mendez <brandonmendez@google.com>
2022-10-12 23:16:35 +0000

    Added command line switch for custom swa bundle location to ease dev testing of chrome demo mode
    Change-Id: If15c744bd1bcf57377ae81bda4b73c5f254059a1
    Fixed: 245991285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946606
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Brandon Mendez <brandonmendez@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058353}

--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -102,6 +102,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDefaultWallpaperLarge[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDefaultWallpaperSmall[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDemoModeHighlightsApp[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDemoModeScreensaverApp[];
+COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const char kDemoModeSwaContentDirectory[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDerelictDetectionTimeout[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDerelictIdleTimeout[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDisableArcCpuRestriction[];
