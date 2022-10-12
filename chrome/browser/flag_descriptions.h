fc95f36d6aa7652f062eccfa2855fb90a0f81498
DongJun Kim <deejay.kim@navercorp.com>
2022-10-12 01:09:51 +0000

    Remove PageInfoDiscoverability flag
    Discoverability is fully launched so remove the related features and
    their use.
    - PageInfoDiscoverability
    - PageInfoDiscoverabilityTimeOut
    
    Bug: 1227423
    Change-Id: I24332edc4daddea3a69b8174efcd27c62a1a47fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932429
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: DongJun Kim <deejay@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057820}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2198,9 +2198,6 @@ extern const char kNotificationPermissionRationaleDescription[];
 extern const char kOfflinePagesLivePageSharingName[];
 extern const char kOfflinePagesLivePageSharingDescription[];
 
-extern const char kPageInfoDiscoverabilityTimeoutsName[];
-extern const char kPageInfoDiscoverabilityTimeoutsDescription[];
-
 extern const char kPageInfoHistoryName[];
 extern const char kPageInfoHistoryDescription[];
 
