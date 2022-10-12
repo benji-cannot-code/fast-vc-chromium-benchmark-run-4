b4761bb01be4c9f83a3ecb26cb7a8fd22bfb4c08
Yue Li <updowndota@google.com>
2022-10-12 15:03:13 +0000

    Login: Fix a potential memleak in LoginAuthUserView
    Bug: 1372380
    Test: Run existing tests
    Change-Id: Icaeb02d3b326e320ce7525f25f58703fe9f9e74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945924
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058044}

--- ash/login/ui/login_password_view.h
+++ ash/login/ui/login_password_view.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_palette.h"
 #include "ash/public/cpp/session/user_info.h"
 #include "ui/base/ime/ash/ime_keyboard.h"
+#include "ui/compositor/layer_animation_observer.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 #include "ui/views/view.h"
@@ -55,7 +56,8 @@ enum class EasyUnlockIconState;
 //  ------------------
 class ASH_EXPORT LoginPasswordView : public views::View,
                                      public views::TextfieldController,
-                                     public ImeControllerImpl::Observer {
+                                     public ImeControllerImpl::Observer,
+                                     public ui::ImplicitAnimationObserver {
  public:
   // TestApi is used for tests to get internal implementation details.
   class ASH_EXPORT TestApi {
@@ -161,6 +163,9 @@ class ASH_EXPORT LoginPasswordView : public views::View,
   // ImeControllerImpl::Observer:
   void OnCapsLockChanged(bool enabled) override;
   void OnKeyboardLayoutNameChanged(const std::string&) override {}
+
+  // ui::ImplicitAnimationObserver:
+  void OnImplicitAnimationsCompleted() override;
 
   void HandleLeftIconsVisibilities(bool handling_capslock);
 
