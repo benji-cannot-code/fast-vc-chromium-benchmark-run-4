ea0a1966291ab0c2db1b2d7fc9ff5d20f8962680
Colin Blundell <blundell@chromium.org>
2022-10-12 07:24:21 +0000

    [android_image_reader_utils] Remove dead code
    This CL removes dead code from android_image_reader_utils.*. A followup
    CL will move it to //ui/gl, as the remaining functions in it are
    Android-specific functions for interacting with EGL fences that are
    used both by SharedImage internals and WebView's vulkan_gl_interop.cc.
    
    Bug: 1370478
    Change-Id: Ia3eabbff66c6067cc0c2b2e125d51ef8a2848de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945028
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057914}

--- gpu/ipc/common/android/android_image_reader_utils.h
+++ gpu/ipc/common/android/android_image_reader_utils.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_IPC_COMMON_ANDROID_ANDROID_IMAGE_READER_UTILS_H_
 #define GPU_IPC_COMMON_ANDROID_ANDROID_IMAGE_READER_UTILS_H_
 
-#include <android/hardware_buffer.h>
-
-#include "base/android/android_image_reader_compat.h"
 #include "base/files/scoped_file.h"
 #include "gpu/gpu_export.h"
 
@@ -16,10 +13,6 @@ namespace gpu {
 
 // Create and inserts an egl fence and exports a ScopedFD from it.
 GPU_EXPORT base::ScopedFD CreateEglFenceAndExportFd();
-
-// Delete the AImage asynchronously by inserting an android native fence sync.
-bool DeleteAImageAsync(AImage* image,
-                       base::android::AndroidImageReader* loader);
 
 // Create and insert an EGL fence and imports the provided fence fd.
 GPU_EXPORT bool InsertEglFenceAndWait(base::ScopedFD acquire_fence_fd);
