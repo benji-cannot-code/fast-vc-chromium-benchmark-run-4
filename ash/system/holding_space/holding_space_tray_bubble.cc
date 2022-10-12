4c7380d83707d60149d4fdd6d69af49f27cb29c1
David Black <dmblack@google.com>
2022-10-12 01:04:11 +0000

    Separators in holding space should be filled, not outlined.
    Previously the separator was outlined, not filled. This was difficult
    to tell on glinux builds but more obvious on DUTs.
    
    Bug: 1352503
    Change-Id: I1922e154e702e965179e71b64bcb63a7c6ad087a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946185
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057812}

--- ash/system/holding_space/holding_space_tray_bubble.cc
+++ ash/system/holding_space/holding_space_tray_bubble.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/views/animation/animation_delegate_views.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/proposed_layout.h"
@@ -301,7 +302,8 @@ class HoldingSpaceTrayBubble::ChildBubbleContainer
       if (last_visible_child) {
         float y = gfx::Tween::FloatValueBetween(
             0.5f, last_visible_child->bounds().bottom(), child->bounds().y());
-        canvas->DrawRect(gfx::RectF(x, y, width, height), color);
+        canvas->FillRect(gfx::ToRoundedRect(gfx::RectF(x, y, width, height)),
+                         color);
       }
 
       last_visible_child = child;
