307530fc1bb50d98ebe071dfe45cb683a7562779
Abigail Klein <abigailbklein@google.com>
2022-10-12 19:04:19 +0000

    Reland of "[Read Anything] Do not use ScopedObservation for SidePanelEntry"
    This reverts commit 485e1c0e23a6846096b537a665869f33a9798f89.
    
    Reason for revert: Fix test failures
    
    Original change's description:
    > [Build Sheriff] Revert "[Read Anything] Do not use ScopedObservation for SidePanelEntry"
    >
    > This reverts commit a1cf2dac62547361d560cbe39970d0c39b081418.
    >
    > Reason for revert: Causing a number of test failures (see bug)
    > Bug: 1373964
    >
    > Original change's description:
    > > [Read Anything] Do not use ScopedObservation for SidePanelEntry
    > > observation.
    > >
    > > This was causing a UAF. When the browser window closed, both the
    > > SidePanelEntry and the ReadAnythingCoordinator were destroyed at the
    > > same time as they are SupportsUserData and BrowserUserData,
    > > respectively. When the ScopedObservation attempted to remove observer,
    > > the pointer had already been freed. This change adds
    > > ReadAnythingCoordinator as an observer to SidePanelEntry and removes
    > > it as an observer via deregistering it from SidePanelRegistry.
    > >
    > > Bug: 1266555, 1344756, 1349795, 1352138, 1365832
    > > Change-Id: Ib63faa9d385f577f1f96a0510759f07efa4b115a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820321
    > > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > > Commit-Queue: Abigail Klein <abigailbklein@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1058076}
    >
    > Bug: 1266555, 1344756, 1349795, 1352138, 1365832
    > Change-Id: I8ba94a103171f191883c3b156c9666eb5768a8af
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949476
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Dana Fried <dfried@google.com>
    > Reviewed-by: Abigail Klein <abigailbklein@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1058130}
    
    Bug: 1373964
    Bug: 1266555, 1344756, 1349795, 1352138, 1365832
    Change-Id: Ibee52fc389fc03e0e1a7fa24bed412a9d90a009e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950072
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058202}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view_browsertest.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/side_panel/read_anything/read_anything_toolbar_view.h"
 
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
@@ -54,7 +56,9 @@ class MockReadAnythingCoordinator : public ReadAnythingCoordinator {
 
 class ReadAnythingToolbarViewTest : public InProcessBrowserTest {
  public:
-  ReadAnythingToolbarViewTest() = default;
+  ReadAnythingToolbarViewTest() {
+    scoped_feature_list_.InitWithFeatures({features::kUnifiedSidePanel}, {});
+  }
   ~ReadAnythingToolbarViewTest() override = default;
 
   // InProcessBrowserTest:
@@ -93,6 +97,7 @@ class ReadAnythingToolbarViewTest : public InProcessBrowserTest {
   MockReadAnythingFontComboboxDelegate font_combobox_delegate_;
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<ReadAnythingToolbarView> toolbar_view_;
   std::unique_ptr<MockReadAnythingCoordinator> coordinator_;
 };
