5207aaa4c71b73e7d916668f848a08ae9f652cf9
Bob Beck <bbe@google.com>
2022-10-12 17:47:48 +0000

    Remove use of base/base64.h from net/cert/pki
    Bug: 1322914
    Change-Id: I39b636596e19cb3c9feb9a4eb770d2d62e34c989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945508
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058131}

--- net/cert/pki/ocsp.cc
+++ net/cert/pki/ocsp.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/pki/ocsp.h"
 
-#include "base/base64.h"
 #include "base/containers/contains.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
