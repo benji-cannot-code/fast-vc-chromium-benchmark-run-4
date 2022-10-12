9f47303e6c0cade52b1793717b8dc062df99e466
Yo Wakita <yowakita@google.com>
2022-10-12 17:27:07 +0000

    Adds flag for Lens Desktop NTP entrypoint.
    Screen of flags UI: https://screenshot.googleplex.com/5YHhq7gcSNbwrq7
    
    Bug: 1373572
    Change-Id: I2caa664c71dda6292aff03d41e9b088aeb2c9223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947462
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Yo Wakita <yowakita@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058123}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2494,6 +2494,9 @@ extern const char kNtpCacheOneGoogleBarDescription[];
 extern const char kNtpChromeCartModuleName[];
 extern const char kNtpChromeCartModuleDescription[];
 
+extern const char kNtpDesktopLensName[];
+extern const char kNtpDesktopLensDescription[];
+
 extern const char kNtpDriveModuleName[];
 extern const char kNtpDriveModuleDescription[];
 
