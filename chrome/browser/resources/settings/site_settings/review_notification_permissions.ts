10d9471812d4a8df4227c76ede54ff92f1e62eea
Eduard Hez <ehez@chromium.org>
2022-10-12 12:19:59 +0000

    [SafetyCheck] Completion and empty state for notification permissions
    The notification permission review in settings/content/notifications
    will now show a completion message upon finishing the review. Also, if
    there is nothing to do for the user the entire module is hidden.
    
    Bug: 1345920
    Change-Id: Ief0b899c078085f50d7d8e5c72ada535c9931616
    Screenshots: https://bugs.chromium.org/p/chromium/issues/detail?id=1345920#c26, https://bugs.chromium.org/p/chromium/issues/detail?id=1345920#c36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925443
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Eduard Hez <ehez@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057998}

--- chrome/browser/resources/settings/site_settings/review_notification_permissions.ts
+++ chrome/browser/resources/settings/site_settings/review_notification_permissions.ts
@@ -10,13 +10,10 @@ import 'chrome://resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
 import '../settings_shared.css.js';
 import '../i18n_setup.js';
 
-import {CrActionMenuElement} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
-import {CrLazyRenderElement} from 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.js';
 import {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {WebUIListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
-import {assertNotReached} from 'chrome://resources/js/assert_ts.js';
-import {PaperTooltipElement} from 'chrome://resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
+import {assert, assertNotReached} from 'chrome://resources/js/assert_ts.js';
 import {DomRepeatEvent, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BaseMixin} from '../base_mixin.js';
@@ -27,8 +24,6 @@ import {NotificationPermission, SiteSettingsPrefsBrowserProxy, SiteSettingsPrefs
 
 export interface SettingsReviewNotificationPermissionsElement {
   $: {
-    tooltip: PaperTooltipElement,
-    actionMenu: CrLazyRenderElement<CrActionMenuElement>,
     undoToast: CrToastElement,
     undoNotification: HTMLElement,
   };
@@ -62,7 +57,7 @@ export class SettingsReviewNotificationPermissionsElement extends
       /* List of domains that sends a lot of notifications. */
       notificationPermissionReviewList_: {
         type: Array,
-        value: () => [],
+        value: null,
       },
 
       /* The last origin that the user interacted with. */
@@ -73,10 +68,21 @@ export class SettingsReviewNotificationPermissionsElement extends
         type: Object,
         value: Actions,
       },
+
+      /**
+       * Indicates whether to show completion info after user has finished the
+       * review process.
+       */
+      shouldShowCompletionInfo_: {
+        type: Boolean,
+        computed: 'computeShouldShowCompletionInfo_' +
+            '(notificationPermissionReviewList_.*)',
+      },
     };
   }
 
-  private notificationPermissionReviewList_: NotificationPermission[];
+  private notificationPermissionReviewList_: NotificationPermission[]|null;
+  private shouldShowCompletionInfo_: boolean;
   private browserProxy_: SiteSettingsPrefsBrowserProxy =
       SiteSettingsPrefsBrowserProxyImpl.getInstance();
   private lastOrigin_: string;
@@ -113,7 +119,9 @@ export class SettingsReviewNotificationPermissionsElement extends
   /* Show action menu when clicked to three dot menu. */
   private onShowActionMenuClick_(e: DomRepeatEvent<NotificationPermission>) {
     this.lastOrigin_ = e.model.item.origin;
-    this.$.actionMenu.get().showAt(e.target as HTMLElement);
+    const actionMenu = this.shadowRoot!.querySelector('cr-action-menu');
+    assert(actionMenu);
+    actionMenu.showAt(e.target as HTMLElement);
   }
 
   private onBlockNotificationPermissionClick_(
@@ -131,7 +139,7 @@ export class SettingsReviewNotificationPermissionsElement extends
     this.browserProxy_.ignoreNotificationPermissionForOrigin(this.lastOrigin_);
     this.lastUserAction_ = Actions.IGNORE;
     this.showUndoToast_();
-    this.$.actionMenu.get().close();
+    this.shadowRoot!.querySelector('cr-action-menu')!.close();
   }
 
   private onResetClick_(e: DomRepeatEvent<NotificationPermission>) {
@@ -139,7 +147,7 @@ export class SettingsReviewNotificationPermissionsElement extends
     this.browserProxy_.resetNotificationPermissionForOrigin(this.lastOrigin_);
     this.lastUserAction_ = Actions.RESET;
     this.showUndoToast_();
-    this.$.actionMenu.get().close();
+    this.shadowRoot!.querySelector('cr-action-menu')!.close();
   }
 
   /* Repopulate the list when notification permission list is updated. */
@@ -151,7 +159,8 @@ export class SettingsReviewNotificationPermissionsElement extends
   private onShowTooltip_(e: Event) {
     e.stopPropagation();
     const target = e.target!;
-    const tooltip = this.$.tooltip;
+    const tooltip = this.shadowRoot!.querySelector('paper-tooltip');
+    assert(tooltip);
     tooltip.target = target;
     tooltip.updatePosition();
     const hide = () => {
@@ -258,6 +267,12 @@ export class SettingsReviewNotificationPermissionsElement extends
     return this.i18n(
         'safetyCheckNotificationPermissionReviewMoreActionsAriaLabel',
         this.lastOrigin_);
+  }
+
+  /** Show info that review is completed when there are no permissions left. */
+  private computeShouldShowCompletionInfo_(): boolean {
+    return !!this.notificationPermissionReviewList_ &&
+        this.notificationPermissionReviewList_.length === 0;
   }
 }
 
