3ff2be965715b6ede719d1c689a2b6c0c59ace14
Max Li <maxqli@google.com>
2022-10-12 17:31:49 +0000

    Add Primes providers to Chromium.
    Primes is a Google internal library for generating performance metrics.
    
    This CL is part of a multi-CL change:
    => https://crrev.com/c/3947359
     - https://crrev.com/i/5029613
     - https://crrev.com/c/3949729
    
    Change-Id: I9c7c7776975d1bbbeedc9e263a2f1fe3ba9306fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947359
    Commit-Queue: Max Li <maxqli@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058124}

---
+++ ios/chrome/test/providers/primes/test_primes.mm
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/notreached.h"
+#import "ios/public/provider/chrome/browser/primes/primes_api.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ios {
+namespace provider {
+
+bool IsPrimesSupported() {
+  // Primes is not supported for tests.
+  return false;
+}
+
+void PrimesStartLogging() {
+  // Primes is not supported for tests.
+  NOTREACHED();
+}
+
+void PrimesStopLogging() {
+  // Primes is not supported for tests.
+  NOTREACHED();
+}
+
+}  // namespace provider
+}  // namespace ios
