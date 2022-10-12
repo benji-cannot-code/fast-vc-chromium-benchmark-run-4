56e7aff87abf29739076e6c3cb288fc73ecaf6b5
Ian Clelland <iclelland@chromium.org>
2022-10-12 16:24:26 +0000

    Report the time of a video's first displayed frame for LCP
    This CL plumbs the information needed to capture LCP from videos
    into Video timing information for LCP into the HTMLVideoElement, from
    where it is sent to the PaintTimingDetector when the initial frame is
    displayed.
    
    Video byte size is taken from the WebMediaPlayer's PipelineStatistics,
    while the result of the Timing-Allow-Origin check goes through URLIndex,
    in the same manner as CORS checks.
    
    Bug: 1289664
    Change-Id: I9e8ed63a2913d5dc70b0f1b2bf9f928db4f1b908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814106
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058088}

--- chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/page_load_metrics_test_waiter.h"
 #include "components/paint_preview/buildflags/buildflags.h"
 #include "content/public/browser/render_widget_host_view.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/hit_test_region_observer.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
@@ -264,6 +265,10 @@ IN_PROC_BROWSER_TEST_F(PageViewportInLCPTest, FullSizeImageInIframe) {
 
 class IsAnimatedLCPTest : public MetricIntegrationTest {
  public:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
+                                    "LCPAnimatedImagesWebExposed");
+  }
   void test_is_animated(const char* html_name,
                         blink::LargestContentfulPaintType flag_set,
                         bool expected,
@@ -308,6 +313,19 @@ IN_PROC_BROWSER_TEST_F(
   test_is_animated("/animated_image_with_larger_text_first.html",
                    blink::LargestContentfulPaintType::kAnimatedImage,
                    /*expected=*/false);
+}
+
+// crbug.com/1373885: This test seems to be unreliable on Lacros
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_LargestContentfulPaint_IsVideo \
+  DISABLED_LargestContentfulPaint_IsVideo
+#else
+#define MAYBE_LargestContentfulPaint_IsVideo LargestContentfulPaint_IsVideo
+#endif
+IN_PROC_BROWSER_TEST_F(IsAnimatedLCPTest,
+                       MAYBE_LargestContentfulPaint_IsVideo) {
+  test_is_animated("/is_video.html", blink::LargestContentfulPaintType::kVideo,
+                   /*expected=*/true);
 }
 
 class MouseoverLCPTest : public MetricIntegrationTest {
--- third_party/blink/public/platform/web_url_response.h
+++ third_party/blink/public/platform/web_url_response.h
@@ -143,6 +143,7 @@ class BLINK_PLATFORM_EXPORT WebURLResponse {
   void SetIsLegacyTLSVersion(bool);
   void SetHasRangeRequested(bool);
   void SetTimingAllowPassed(bool);
+  bool TimingAllowPassed() const;
 
   void SetSecurityStyle(SecurityStyle);
 
