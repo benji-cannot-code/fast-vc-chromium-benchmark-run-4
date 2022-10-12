ebb79e65c501e6e5bbae2a5f0f75258980554409
Sylvain Defresne <sdefresne@chromium.org>
2022-10-12 10:52:48 +0000

    [ios] Convert client code to use SystemIdentity
    Remove uses of ChromeIdentity* and replace them with
    id<SystemIdentity> in client code. Eventually, this
    will allow removing the ChromeIdentity type.
    
    This CL converts FakeAccountDetailsViewController.
    
    Bug: 1201182
    Change-Id: I2025ab94139190d34a39d8450c5a7729b249daaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3944992
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057970}

--- ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.mm
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.mm
@@ -60,7 +60,7 @@ void SetCachedAvatarForIdentity(id<SystemIdentity> identity, UIImage* avatar) {
 }  // anonymous namespace
 
 @interface FakeAccountDetailsViewController : UIViewController {
-  __weak ChromeIdentity* _identity;
+  __weak id<SystemIdentity> _identity;
   UIButton* _removeAccountButton;
   UIButton* _closeAccountDetailsButton;
 }
@@ -68,7 +68,7 @@ void SetCachedAvatarForIdentity(id<SystemIdentity> identity, UIImage* avatar) {
 
 @implementation FakeAccountDetailsViewController
 
-- (instancetype)initWithIdentity:(ChromeIdentity*)identity {
+- (instancetype)initWithIdentity:(id<SystemIdentity>)identity {
   self = [super initWithNibName:nil bundle:nil];
   if (self) {
     _identity = identity;
