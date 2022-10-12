b5be22665db4d7b8cb2a92a0630bf2c3e8733911
Nicholas Hollingum <hollingum@google.com>
2022-10-12 10:04:14 +0000

    borealis: Allow borealis to run on the beta branch
    When borealis enters the beta development phase we will allow compatible
    chromebooks on the beta channel to install it.
    
    Bug: b/243858898
    Change-Id: Ia3cec5c6f6553807809f404eaad3180054ee4e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906682
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Auto-Submit: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057954}

--- chrome/browser/ash/borealis/borealis_features.h
+++ chrome/browser/ash/borealis/borealis_features.h
@@ -30,7 +30,7 @@ class BorealisFeatures {
     kBlockedOnChildAccount,
     kVmPolicyBlocked,
     kUserPrefBlocked,
-    kBlockedOnBetaStable,
+    kBlockedOnStable,
     kBlockedByFlag,
     kUnsupportedModel,
     kHardwareChecksFailed,
