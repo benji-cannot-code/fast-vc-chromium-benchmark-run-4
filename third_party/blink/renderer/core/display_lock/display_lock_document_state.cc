114f2424f27bb86e3949536e536d3481000a5a79
Vladimir Levin <vmpstr@chromium.org>
2022-10-12 17:24:15 +0000

    CV: Add a runtime flag to warn on render access in c-v locked subtrees
    This patch adds a warning that will fire on any content-visibity forced
    access. Note this flag is not meant to be enabled as stable, just used
    as a tool to temporarily check where the pessimizations of
    content-visibility may be.
    
    R=chrishtr@chromium.org, pdr@chromium.org
    
    Change-Id: I8abe052ede330b0619466afab1a61e9c5511d1a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946540
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058120}

--- third_party/blink/renderer/core/display_lock/display_lock_document_state.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_document_state.cc
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kForcedRendering[] =
-    "Rendering was performed in a subtree hidden by content-visibility:hidden.";
+    "Rendering was performed in a subtree hidden by content-visibility.";
 const char kForcedRenderingMax[] =
-    "Rendering was performed in a subtree hidden by content-visibility:hidden. "
+    "Rendering was performed in a subtree hidden by content-visibility. "
     "Further messages will be suppressed.";
 constexpr unsigned kMaxConsoleMessages = 500;
 
@@ -453,9 +453,12 @@ void DisplayLockDocumentState::IssueForcedRenderWarning(Element* element) {
   // accessing content-visibility: hidden subtrees intentionally.
   if (forced_render_warnings_ < kMaxConsoleMessages) {
     forced_render_warnings_++;
+    auto level =
+        RuntimeEnabledFeatures::WarnOnContentVisibilityRenderAccessEnabled()
+            ? mojom::blink::ConsoleMessageLevel::kWarning
+            : mojom::blink::ConsoleMessageLevel::kVerbose;
     auto* console_message = MakeGarbageCollected<ConsoleMessage>(
-        mojom::blink::ConsoleMessageSource::kJavaScript,
-        mojom::blink::ConsoleMessageLevel::kVerbose,
+        mojom::blink::ConsoleMessageSource::kJavaScript, level,
         forced_render_warnings_ == kMaxConsoleMessages ? kForcedRenderingMax
                                                        : kForcedRendering);
     console_message->SetNodes(document_->GetFrame(),
