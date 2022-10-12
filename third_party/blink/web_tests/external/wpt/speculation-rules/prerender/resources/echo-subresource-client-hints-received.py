da98ca8d8c5fd202723fcee9e6a9ace68cdd9394
Lingqi Chi <lingqi@chromium.org>
2022-10-12 11:00:06 +0000

    Prerender: add webtest for client hints
    This behavior is specified by
    https://github.com/WICG/nav-speculation/pull/182.
    
    Primary --------- Server --- Prerender
      | --> initial-->|
      |     client    |
      |     hint(ICH) |
      |               |
      | <--add spr----|
      |               |
      | ----initiate---------------->|
      |               |              |
      .               |<- ICH--------|
      .               |              |
      .               |              |
      .               |              |
                      |--new ------ >|
                      |  Accept-CH:  |
                      |   somefield  |
                      |              |
                      |<-SUBResources|
                      |   ICH U      |
     .                |   {somefield:|
     .                     values}.  |
     |                               |
     |<<<<<<Ready Signal<<<<<<<<<<<<<|
     |
     ===== Activation ================
     |                               |
     |---> ICH ----------------------|
                                (ACTIVATED)
                                (TEST PASS
                                 SIGNAL)
    
    
    Bug: 1348882
    Change-Id: I0f8b0b600670ff6bfa278ef2a4795216e4b06176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929984
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057978}

---
+++ third_party/blink/web_tests/external/wpt/speculation-rules/prerender/resources/echo-subresource-client-hints-received.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+""" Handle the sub-resource requests and attach the received client info to
+the response.
+"""
+
+
+def main(request, response):
+    response.status = 200
+
+    # Echo the received CH headers.
+    response.headers.set(
+        b"server_received_bitness",
+        "true" if b"sec-ch-ua-bitness" in request.headers else "false")
+    response.headers.set(
+        b"server_received_full_version_list", "true"
+        if b"sec-ch-ua-full-version-list" in request.headers else "false")
