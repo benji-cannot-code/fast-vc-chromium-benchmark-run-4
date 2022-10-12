49f0fe0da894aad8b6be23fbcf92557447a424f9
Eshwar Stalin <estalin@chromium.org>
2022-10-12 17:57:26 +0000

    Adding command line switch for device has battery flag in performance manager
    The battery saver mode settings are shown only when the device has a battery. In order to allow us to test this setting on devices without a battery adding a command line switch to override the flag.
    
    Bug: None
    Change-Id: I073c4406f5f3d4d1e8780500c427986c1c856dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940305
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058141}

--- chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
+++ chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h"
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/power_monitor/power_monitor.h"
 #include "base/values.h"
@@ -57,6 +58,9 @@ class HighEfficiencyModeToggleDelegateImpl
 }  // namespace
 
 const uint64_t UserPerformanceTuningManager::kLowBatteryThresholdPercent = 20;
+
+const char UserPerformanceTuningManager::kForceDeviceHasBattery[] =
+    "force-device-has-battery";
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(
     UserPerformanceTuningManager::PreDiscardResourceUsage);
@@ -197,6 +201,12 @@ void UserPerformanceTuningManager::Start() {
 
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kBatterySaverModeAvailable)) {
+    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(kForceDeviceHasBattery)) {
+      force_has_battery_ = true;
+      has_battery_ = true;
+    }
+
     pref_change_registrar_.Add(
         performance_manager::user_tuning::prefs::kBatterySaverModeState,
         base::BindRepeating(
@@ -308,7 +318,8 @@ void UserPerformanceTuningManager::OnBatteryStateSampled(
     return;
 
   bool had_battery = has_battery_;
-  has_battery_ = battery_state->battery_count > 0;
+  has_battery_ = force_has_battery_ || battery_state->battery_count > 0;
+
   // If the "has battery" state changed, notify observers.
   if (had_battery != has_battery_) {
     for (auto& obs : observers_) {
