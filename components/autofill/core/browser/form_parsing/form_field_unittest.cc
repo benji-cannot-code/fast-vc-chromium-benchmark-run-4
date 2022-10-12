9d77da8c2fc3a788671f7307dd67b338f26d8dcd
Matthias Körber <koerber@google.com>
2022-10-12 22:27:03 +0000

    Introduce NUMERIC_TYPE and local heuristics
    Introduces a heuristic type for numeric fields that should obviously not
    be filled and grants precedence to those fields.
    The purpose is to ignore false positive server classification for
    numeric types that are prone to false-positive votes.
    
    
    Change-Id: I74b0d0cbc268ec6c3a55967e64ebe160348004a7
    Bug: 807748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549078
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058323}

--- components/autofill/core/browser/form_parsing/form_field_unittest.cc
+++ components/autofill/core/browser/form_parsing/form_field_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
-#include "components/autofill/core/browser/form_parsing/buildflags.h"
 #include "components/autofill/core/browser/form_parsing/form_field.h"
 #include "components/autofill/core/browser/form_parsing/parsing_test_utils.h"
 #include "components/autofill/core/browser/form_structure.h"
