3cedb38bd2650d4f0fba72149a29df43df11a69b
Sylvain Defresne <sdefresne@chromium.org>
2022-10-12 10:55:54 +0000

    [ios] Convert client code to use SystemIdentity
    Remove uses of ChromeIdentity* and replace them with
    id<SystemIdentity> in client code. Eventually, this
    will allow removing the ChromeIdentity type.
    
    This CL converts //ios/chrome/browser/ui/ntp.
    
    Bug: 1201182
    Change-Id: I830a7efcc0712389111995331a002f7a89a820e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946083
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057975}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -1339,7 +1339,7 @@ BASE_FEATURE(kEnableCheckForNewFollowContent,
 
   ios::ChromeIdentityService* identity_service =
       ios::GetChromeBrowserProvider().GetChromeIdentityService();
-  ChromeIdentity* identity =
+  id<SystemIdentity> identity =
       self.authService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
   if (!identity) {
     [self updateFeedWithIsSupervisedUser:NO];
