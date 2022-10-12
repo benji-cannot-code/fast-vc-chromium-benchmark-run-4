ef82e6e61105fbd0062cee3976fcc1865c085018
Daniel Brinkers <brinky@chromium.org>
2022-10-12 07:57:35 +0000

    storage: Add CopyOrMoveHookDelegateComposite
    Enables multiple implementations of CopyOrHookDelegate to be used.
    
    Bug: 1361878
    Change-Id: I734845b3b309807d70810b7a88042a970cc1158c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883786
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Daniel Brinkers <brinky@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1057482}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945078
    Cr-Commit-Position: refs/heads/main@{#1057922}

--- storage/browser/file_system/copy_or_move_hook_delegate.cc
+++ storage/browser/file_system/copy_or_move_hook_delegate.cc
@@ -12,9 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace storage {
 
-CopyOrMoveHookDelegate::CopyOrMoveHookDelegate() {
+CopyOrMoveHookDelegate::CopyOrMoveHookDelegate(bool is_composite)
+    : is_composite_(is_composite) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
+
+CopyOrMoveHookDelegate::~CopyOrMoveHookDelegate() = default;
 
 void CopyOrMoveHookDelegate::OnBeginProcessFile(
     const FileSystemURL& source_url,
