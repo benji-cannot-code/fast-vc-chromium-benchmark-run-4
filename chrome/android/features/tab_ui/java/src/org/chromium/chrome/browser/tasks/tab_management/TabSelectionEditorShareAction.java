0fa37609738852142d23b0ecb2d91f8b6432d538
Brandon Fong <bjfong@google.com>
2022-10-12 15:22:47 +0000

    [Android Bulk Tab Edit] Add param for share action in TabSelectionEditorV2.
    Adding a param to enable/disable the share action in TabSelectionEditorV2. Also default the selection editor to exit upon finishing the share action.
    
    Bug: crbug.com/1373563
    Change-Id: I157da82821f1e1c7154d5ac14060bcd5851a734f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947442
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058053}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorShareAction.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorShareAction.java
@@ -96,7 +96,7 @@ public class TabSelectionEditorShareAction extends TabSelectionEditorAction {
 
     @Override
     public boolean shouldHideEditorAfterAction() {
-        return false;
+        return true;
     }
 
     private String getTabListStringForSharing(List<Tab> tabs) {
