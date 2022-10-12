b4fdb3a7656f2c1ec21cf4731e5a99f51d3aa087
Rushan Suleymanov <rushans@google.com>
2022-10-12 10:32:13 +0000

    Revert "[VC]: set background blur state on mojo startup"
    This reverts commit 1d38b071b2534238ce38cb9cd89343f805a0484d.
    
    Reason for revert: flaky tests on https://ci.chromium.org/p/chromium/builders/ci/chromeos-amd64-generic-rel?limit=200
    
    Original change's description:
    > [VC]: set background blur state on mojo startup
    >
    > BUG=b:245841220
    > TEST=deploy to DUT, toggle flag on and off, see mojo call in logs
    >
    > Change-Id: I25071c9df6dd0f30c20196353bc0ca56054911d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935763
    > Commit-Queue: Jim Pollock <jmpollock@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Ricky Liang <jcliang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1057910}
    
    Bug: b:245841220
    Bug: 1373815
    Change-Id: Iaff224c6671dab710c2d838fd3507cb410ca6b86
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3948647
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057963}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1650,16 +1650,6 @@ BASE_FEATURE(kVCBackgroundBlur,
              "VCBackgroundBlur",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Controls whether the vc background replace is enabled.
-BASE_FEATURE(kVCBackgroundReplace,
-             "VCBackgroundReplace",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// Controls whether the vc portrait relighting is enabled.
-BASE_FEATURE(kVCPortraitRelighting,
-             "VCPortraitRelighting",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Enables or disables the Quick Settings Network revamp, which updates Network
 // Quick Settings UI and related infrastructure. See https://crbug.com/1169479.
 BASE_FEATURE(kQuickSettingsNetworkRevamp,
@@ -3018,14 +3008,6 @@ bool IsUseStorkSmdsServerAddressEnabled() {
 
 bool IsVCBackgroundBlurEnabled() {
   return base::FeatureList::IsEnabled(kVCBackgroundBlur);
-}
-
-bool IsVCBackgroundReplaceEnabled() {
-  return base::FeatureList::IsEnabled(kVCBackgroundReplace);
-}
-
-bool IsVCPortraitRelightingEnabled() {
-  return base::FeatureList::IsEnabled(kVCPortraitRelighting);
 }
 
 bool IsVcControlsUiEnabled() {
--- media/capture/video/chromeos/camera_hal_dispatcher_impl_unittest.cc
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/chromeos/mojom/camera_common.mojom.h"
 #include "media/capture/video/chromeos/mojom/cros_camera_client.mojom.h"
 #include "media/capture/video/chromeos/mojom/cros_camera_service.mojom.h"
-#include "media/capture/video/chromeos/mojom/effects_pipeline.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -60,11 +59,6 @@ class MockCameraHalServer : public cros::mojom::CameraHalServer {
                void(cros::mojom::CameraPrivacySwitchState state));
   MOCK_METHOD1(GetAutoFramingSupported,
                void(GetAutoFramingSupportedCallback callback));
-
-  void SetCameraEffect(::cros::mojom::EffectsConfigPtr config,
-                       SetCameraEffectCallback callback) override {
-    std::move(callback).Run(::cros::mojom::SetEffectResult::kOk);
-  }
 
   mojo::PendingRemote<cros::mojom::CameraHalServer> GetPendingRemote() {
     return receiver_.BindNewPipeAndPassRemote();
