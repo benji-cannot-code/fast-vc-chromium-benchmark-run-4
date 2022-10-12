3bc1a55916b5e2e197f6bff656db0a006a0585e5
Renato Silva <rrsilva@google.com>
2022-10-12 16:13:58 +0000

    OOBE - Polymer3 - Check-in gaia-button
    Check-in the modularised version of gaia-button.
    
    Bug: 1184731, b/243382353
    Change-Id: I8795ee65e79fa6c384c626ec1da70cddee2fdebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3948713
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058083}

--- chrome/browser/resources/chromeos/login/components/gaia_button.js
+++ chrome/browser/resources/chromeos/login/components/gaia_button.js
@@ -7,13 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element wrapping gaia styled button for login/oobe.
  */
 
-/* #js_imports_placeholder */
+import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/polymer/v3_0/paper-styles/color.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 
 /**
  * @constructor
  * @extends {PolymerElement}
  */
-const GaiaButtonBase = Polymer.mixinBehaviors([], Polymer.Element);
+const GaiaButtonBase = mixinBehaviors([], PolymerElement);
 
 /**
  * @typedef {{
@@ -22,12 +26,15 @@ const GaiaButtonBase = Polymer.mixinBehaviors([], Polymer.Element);
  */
 GaiaButtonBase.$;
 
+/** @polymer */
 class GaiaButton extends GaiaButtonBase {
   static get is() {
     return 'gaia-button';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
@@ -44,10 +51,6 @@ class GaiaButton extends GaiaButtonBase {
         value: false,
       },
     };
-  }
-
-  constructor() {
-    super();
   }
 
   focus() {
