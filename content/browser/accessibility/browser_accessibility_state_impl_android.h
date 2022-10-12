cd57bff185708b5ba011760378a3ad85566b7cac
Mark Schillaci <mschillaci@google.com>
2022-10-12 23:02:09 +0000

    Update UMA for Android accessibility to be split per enabled service type
    This CL expands the histograms of Android accessibility to be recorded
    on a per-accessibility service variation.
    
    Before this CL, we track the requested events, flags, feedback, and
    capabilities of all enabled accessibility service for Android users.
    We record the union for all enabled services during construction in
    the "Accessibility.AndroidServiceInfo" enum histogram.
    
    With this CL, we add variants to this histogram for assistive tech,
    password managers, and other. We record the same information, but now
    in a series of variants so we can correlate the data between the
    types of enabled accessibility services (e.g. what capabilities do the
    password managers request). We record all possible subsets of these
    three variants (e.g. "password manager with assistive tech" is different
    than both "password manager" or "assistive tech"). We record this in a
    new histogram, "Accessibility.AndroidServiceInfo.{RunningApps}", and
    we deprecate the existing histogram.
    
    AX-Relnotes: N/A
    Bug: 1197608
    Change-Id: I82bae99d305d1ef734e0343cd85e912ec79bae18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945884
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058341}

--- content/browser/accessibility/browser_accessibility_state_impl_android.h
+++ content/browser/accessibility/browser_accessibility_state_impl_android.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/accessibility/browser_accessibility_state_impl.h"
 
-#include "base/android/jni_android.h"
-#include "base/android/jni_array.h"
-
 namespace content {
 
 class BrowserContext;
@@ -19,9 +16,14 @@ class BrowserAccessibilityStateImplAndroid
     : public BrowserAccessibilityStateImpl {
  public:
   BrowserAccessibilityStateImplAndroid();
-  ~BrowserAccessibilityStateImplAndroid() override {}
+  ~BrowserAccessibilityStateImplAndroid() override = default;
 
   void CollectAccessibilityServiceStats();
+  void RecordAccessibilityServiceStatsHistogram(int event_type_mask,
+                                                int feedback_type_mask,
+                                                int flags_mask,
+                                                int capabilities_mask,
+                                                std::string histogram);
 
  protected:
   void UpdateHistogramsOnOtherThread() override;
