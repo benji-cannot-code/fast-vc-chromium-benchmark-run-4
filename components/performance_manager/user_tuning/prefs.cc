b73630c23d8e4142d06b253f910b06581e1e1ea9
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-10-12 20:37:33 +0000

    Make the tab discarding exceptions list pref syncable
    Bug: 1370699
    Change-Id: Ib1a3577de6f9c3592da3181b3d41627f8d5f2f06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949693
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058246}

--- components/performance_manager/user_tuning/prefs.cc
+++ components/performance_manager/user_tuning/prefs.cc
@@ -31,7 +31,8 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 }
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterListPref(kTabDiscardingExceptions);
+  registry->RegisterListPref(kTabDiscardingExceptions,
+                             user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterListPref(kManagedTabDiscardingExceptions);
 }
 
