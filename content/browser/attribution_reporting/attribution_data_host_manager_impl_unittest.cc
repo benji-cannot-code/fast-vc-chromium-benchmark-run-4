02ce14de18e11270ead6d44b9cb48863c95b2e26
Andrew Paseltiner <apaseltiner@chromium.org>
2022-10-12 13:26:57 +0000

    Report destination-mismatch errors in Attribution Internals UI
    Bug: 1351843
    Change-Id: Ie30839ff662ad91a074e68a60ce3bc0da55c9632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933073
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058017}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl_unittest.cc
@@ -44,6 +44,8 @@ namespace content {
 
 namespace {
 
+using ::attribution_reporting::mojom::SourceRegistrationError;
+
 using ::testing::_;
 using ::testing::AllOf;
 using ::testing::ElementsAre;
@@ -1301,6 +1303,11 @@ TEST_F(AttributionDataHostManagerImplTest,
   auto reporter = url::Origin::Create(GURL("https://report.test"));
   auto source_site = url::Origin::Create(GURL("https://source.test"));
 
+  EXPECT_CALL(mock_manager_,
+              NotifyFailedSourceRegistration(
+                  kRegisterSourceJson, reporter,
+                  SourceRegistrationError::kDestinationMismatched));
+
   const blink::AttributionSrcToken attribution_src_token;
   data_host_manager_.NotifyNavigationRedirectRegistration(
       attribution_src_token, kRegisterSourceJson, reporter, source_site);
@@ -1383,11 +1390,9 @@ TEST_F(AttributionDataHostManagerImplTest,
   auto reporter = url::Origin::Create(GURL("https://report.test"));
   auto source_site = url::Origin::Create(GURL("https://source.test"));
 
-  EXPECT_CALL(
-      mock_manager_,
-      NotifyFailedSourceRegistration(
-          "!!!invalid json", reporter,
-          attribution_reporting::mojom::SourceRegistrationError::kInvalidJson));
+  EXPECT_CALL(mock_manager_, NotifyFailedSourceRegistration(
+                                 "!!!invalid json", reporter,
+                                 SourceRegistrationError::kInvalidJson));
 
   const blink::AttributionSrcToken attribution_src_token;
   data_host_manager_.NotifyNavigationRedirectRegistration(
