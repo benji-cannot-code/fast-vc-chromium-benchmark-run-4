e67ecb1a7c8ddeae673284347653ae8064f940e9
Alan Cutter <alancutter@chromium.org>
2022-10-12 07:55:06 +0000

    Add icon file based WebAppIconHealthChecks
    This CL adds WebAppIconHealthChecks for icon files being empty or
    missing.
    
    Bug: 1353659
    Change-Id: I8439b57779e1c6c74d966dcd9143cc2a5a2af7fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861256
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057920}

--- chrome/browser/web_applications/file_utils_wrapper.h
+++ chrome/browser/web_applications/file_utils_wrapper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/files/file.h"
 #include "base/memory/ref_counted.h"
 #include "build/build_config.h"
 
@@ -42,6 +43,8 @@ class FileUtilsWrapper : public base::RefCountedThreadSafe<FileUtilsWrapper> {
   bool DirectoryExists(const base::FilePath& path);
 
   bool CreateDirectory(const base::FilePath& full_path);
+
+  bool GetFileInfo(const base::FilePath& file_path, base::File::Info* info);
 
   int ReadFile(const base::FilePath& filename, char* data, int max_size);
 
