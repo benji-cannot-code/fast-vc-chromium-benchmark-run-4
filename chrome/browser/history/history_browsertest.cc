232ae4369213c46bdddfa96a36648b0f7f5b05a4
Marc Treib <treib@chromium.org>
2022-10-13 14:10:25 +0000

    History: Prevent chrome-untrusted:// URLs from being persisted in the DB
    chrome:// URLs (and a bunch of other schemes) are already excluded.
    There's no reason why chrome-untrusted:// should be treated differently
    from chrome:// here, so this CL adds it to the exclusion list.
    
    (Adding these URLs to the history DB could leak to them being synced,
    which is explicitly undesired in some cases - see bug.)
    
    Bug: 1357496
    Change-Id: Id1595d8111c90a1c2246397bd1cdfe98928e4a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949276
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058680}

--- chrome/browser/history/history_browsertest.cc
+++ chrome/browser/history/history_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/webui_config_map.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/fenced_frame_test_util.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_frame_navigation_observer.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "ui/webui/untrusted_web_ui_browsertest_util.h"
 #include "url/gurl.h"
 
 using content::BrowserThread;
@@ -359,14 +361,20 @@ IN_PROC_BROWSER_TEST_F(HistoryBrowserTest, InvalidURLNoHistory) {
 
 // URLs with special schemes should not go in history.
 IN_PROC_BROWSER_TEST_F(HistoryBrowserTest, InvalidSchemeNoHistory) {
-  GURL about_blank("about:blank");
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), about_blank));
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL("about:blank")));
   ExpectEmptyHistory();
-  GURL view_source("view-source:about:blank");
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), view_source));
+
+  ASSERT_TRUE(
+      ui_test_utils::NavigateToURL(browser(), GURL("view-source:about:blank")));
   ExpectEmptyHistory();
-  GURL chrome("chrome://about");
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), chrome));
+
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL("chrome://about")));
+  ExpectEmptyHistory();
+
+  content::WebUIConfigMap::GetInstance().AddUntrustedWebUIConfig(
+      std::make_unique<ui::TestUntrustedWebUIConfig>("test-host"));
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), GURL("chrome-untrusted://test-host/title1.html")));
   ExpectEmptyHistory();
 }
 
