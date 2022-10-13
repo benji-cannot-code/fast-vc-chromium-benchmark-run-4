ca85724fa055524036bbd7a456a4d6404291ec50
Bryan Bonnet <bbonnet@google.com>
2022-10-13 21:40:48 +0000

    [ntp] Implement basic file upload functionality for the Lens Upload dialog.
    Screencast: https://drive.google.com/file/d/1G9nxD77czruviPDrI6KZWQ8YTAZmrgDc/view?usp=sharing&resourcekey=0-tA64LoCn3wZ_RU6lGTCzng
    
    Bug: 1369767
    Change-Id: I6765258fa8e983c0a72e56f23bce5911ae2accd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945694
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Bryan Bonnet <bbonnet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058930}

--- chrome/browser/resources/new_tab_page/lens_upload_dialog.ts
+++ chrome/browser/resources/new_tab_page/lens_upload_dialog.ts
@@ -9,6 +9,7 @@ import '//resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 
 import {afterNextRender, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {LensErrorType, LensFormElement} from './lens_form.js';
 import {getTemplate} from './lens_upload_dialog.html.js';
 import {WindowProxy} from './window_proxy.js';
 
@@ -30,6 +31,7 @@ enum DialogState {
 export interface LensUploadDialogElement {
   $: {
     dialog: HTMLDivElement,
+    lensForm: LensFormElement,
   };
 }
 
@@ -162,6 +164,18 @@ export class LensUploadDialogElement extends PolymerElement {
 
   private onOfflineRetryButtonClick_() {
     this.setOnlineState_();
+  }
+
+  private onUploadFileClick_() {
+    this.$.lensForm.openSystemFilePicker();
+  }
+
+  private handleFormLoading_() {
+    this.dialogState_ = DialogState.LOADING;
+  }
+
+  private handleFormError_(_event: CustomEvent<LensErrorType>) {
+    // TODO(crbug.com/1367506): Implement error state.
   }
 }
 declare global {
