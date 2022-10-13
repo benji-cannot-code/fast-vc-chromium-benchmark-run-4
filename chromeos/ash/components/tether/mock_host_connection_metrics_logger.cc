41db8ef8f8a71c3d7c47e28e09061caf8e6e3a17
Henrique Ferreiro <hferreiro@igalia.com>
2022-10-13 21:11:39 +0000

    Move {ash => chromeos/ash}/components/tether/
    ChromeOS components and services are meant to live in chromeos/,
    separate from Ash shell code. Fix a previous migration that used the
    wrong directory.
    
    Also, replace banned function std::to_string with base::NumberToString.
    
    This CL is part of the ChromeOS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I2f84ab367176e55dcc6872e413b55e8ae865dc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929187
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058919}

---
+++ chromeos/ash/components/tether/mock_host_connection_metrics_logger.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/tether/mock_host_connection_metrics_logger.h"
+
+#include "chromeos/ash/components/tether/active_host.h"
+
+namespace ash {
+
+namespace tether {
+
+MockHostConnectionMetricsLogger::MockHostConnectionMetricsLogger(
+    ActiveHost* active_host)
+    : HostConnectionMetricsLogger(active_host) {}
+
+MockHostConnectionMetricsLogger::~MockHostConnectionMetricsLogger() = default;
+
+}  // namespace tether
+
+}  // namespace ash
