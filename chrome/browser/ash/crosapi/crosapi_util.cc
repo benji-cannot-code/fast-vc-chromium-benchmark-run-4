41d3e7b9d7db39cf2647c258e8e175d3b7e78695
Danan S <danan@chromium.org>
2022-10-13 04:34:42 +0000

    [Parent Access] Add ParentAccess to the crosapi mojom interface
    This will allow it to be invoked from LaCros.
    
    Bug: b:200587178
    Change-Id: Ib5553e81a873af78ec689934476f41c52462bd11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950353
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058514}

--- chrome/browser/ash/crosapi/crosapi_util.cc
+++ chrome/browser/ash/crosapi/crosapi_util.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/network_settings_service.mojom.h"
 #include "chromeos/crosapi/mojom/networking_attributes.mojom.h"
 #include "chromeos/crosapi/mojom/networking_private.mojom.h"
+#include "chromeos/crosapi/mojom/parent_access.mojom.h"
 #include "chromeos/crosapi/mojom/policy_service.mojom.h"
 #include "chromeos/crosapi/mojom/power.mojom.h"
 #include "chromeos/crosapi/mojom/prefs.mojom.h"
@@ -246,7 +247,7 @@ constexpr InterfaceVersionEntry MakeInterfaceVersionEntry() {
   return {T::Uuid_, T::Version_};
 }
 
-static_assert(crosapi::mojom::Crosapi::Version_ == 96,
+static_assert(crosapi::mojom::Crosapi::Version_ == 97,
               "If you add a new crosapi, please add it to "
               "kInterfaceVersionEntries below.");
 
@@ -351,6 +352,7 @@ constexpr InterfaceVersionEntry kInterfaceVersionEntries[] = {
     MakeInterfaceVersionEntry<media_session::mojom::MediaControllerManager>(),
     MakeInterfaceVersionEntry<media_session::mojom::AudioFocusManager>(),
     MakeInterfaceVersionEntry<media_session::mojom::AudioFocusManagerDebug>(),
+    MakeInterfaceVersionEntry<crosapi::mojom::ParentAccess>(),
 };
 
 constexpr bool HasDuplicatedUuid() {
