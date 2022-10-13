cd9ffdfd45afecd34245fb51ebf259e2e9c9c7fd
Peter Kasting <pkasting@chromium.org>
2022-10-13 22:12:58 +0000

    Change std::find() to use base:: functions: tools/
    Simplifies code slightly.
    
    Bug: 1368812
    Change-Id: I2c4ef496d15e2fc6f883b84512471891b0cc9917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953898
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058955}

--- tools/accessibility/inspect/ax_dump_tree.cc
+++ tools/accessibility/inspect/ax_dump_tree.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/containers/contains.h"
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "content/public/browser/ax_inspect_factory.h"
@@ -98,7 +99,7 @@ int main(int argc, char** argv) {
       LOG(ERROR) << "Unknown API type: " << api_str;
       return 1;
     }
-    if (std::find(apis.begin(), apis.end(), api) == apis.end()) {
+    if (!base::Contains(apis, api)) {
       LOG(ERROR) << "Unsupported API for this platform: "
                  << static_cast<std::string>(api);
       return 1;
