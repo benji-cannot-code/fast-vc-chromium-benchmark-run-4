51da4de9b2a5ce1ca237b1940d0244200f084db4
Bob Beck <bbe@google.com>
2022-10-13 19:52:09 +0000

    Remove base/check_op.h usage in cert/pki
    Bug: 1322914
    Change-Id: Iac6244584b24c6e78ba1859062a00d9862b597e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950178
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058889}

--- net/cert/pki/parse_certificate_fuzzer.cc
+++ net/cert/pki/parse_certificate_fuzzer.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-#include "base/check_op.h"
 #include "net/cert/pki/cert_errors.h"
 #include "net/cert/pki/parsed_certificate.h"
 #include "net/cert/x509_util.h"
--- net/cert/pki/parse_name.cc
+++ net/cert/pki/parse_name.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/pki/parse_name.h"
 
-#include "base/check_op.h"
 #include "base/strings/string_number_conversions.h"
 #include "net/der/parse_values.h"
 #include "third_party/boringssl/src/include/openssl/bytestring.h"
