6cfa5506746ddf8b95de67bb16da3e9a90e7a95a
Sangseok Jang <sangseok.jang@navercorp.com>
2022-10-13 15:54:42 +0000

    [Clean up] Remove duplicated header on cc file
    Bug: N/A
    Change-Id: Ic37f59e9aa4801b755bcf66c3b840b04be1f1d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949152
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058744}

--- chrome/browser/ui/cocoa/tab_menu_bridge.mm
+++ chrome/browser/ui/cocoa/tab_menu_bridge.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/tab_menu_bridge.h"
 
 #import <Cocoa/Cocoa.h>
-#include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
 
 #include "base/callback.h"
 #include "base/mac/scoped_nsobject.h"
