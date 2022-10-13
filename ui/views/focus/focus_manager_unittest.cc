1eb2e88e9e19abfa7fac0c3a0259b15955effd55
Joanmarie Diggs <jdiggs@igalia.com>
2022-10-13 17:06:50 +0000

    Remove kSkipAccessibilityPaintChecks from FocusManager unit test
    The paint checks were skipped because a test created focusable views
    which lacked a valid role and accessible name. Give the views being
    tested a role and name so we can remove kSkipAccessibilityPaintChecks.
    
    Bug: 1348081, 1218186
    Change-Id: I6496455c16bd22bb63ab13a54e315ad119cf220d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952679
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058780}

--- ui/views/focus/focus_manager_unittest.cc
+++ ui/views/focus/focus_manager_unittest.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/accelerators/test_accelerator_target.h"
 #include "ui/events/keycodes/keyboard_codes.h"
-#include "ui/views/accessibility/accessibility_paint_checks.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/accessible_pane_view.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/buildflags.h"
@@ -1147,12 +1147,18 @@ TEST_F(DesktopWidgetFocusManagerTest, AnchoredDialogInDesktopNativeWidgetAura) {
   child->SetFocusBehavior(View::FocusBehavior::ALWAYS);
   bubble_widget->GetRootView()->AddChildView(child);
 
-  // TODO(crbug.com/1218186): Remove this, this is in place temporarily to be
-  // able to submit accessibility checks, but this focusable View needs to
-  // add a name so that the screen reader knows what to announce.
-  parent1->SetProperty(views::kSkipAccessibilityPaintChecks, true);
-  parent2->SetProperty(views::kSkipAccessibilityPaintChecks, true);
-  child->SetProperty(views::kSkipAccessibilityPaintChecks, true);
+  // In order to pass the accessibility paint checks, focusable views must have
+  // a valid role.
+  parent1->GetViewAccessibility().OverrideRole(ax::mojom::Role::kGroup);
+  parent2->GetViewAccessibility().OverrideRole(ax::mojom::Role::kGroup);
+  child->GetViewAccessibility().OverrideRole(ax::mojom::Role::kButton);
+
+  // In order to pass the accessibility paint checks, focusable views must have
+  // a non-empty accessible name, or have their name set to explicitly empty.
+  parent1->GetViewAccessibility().OverrideName(u"Parent 1");
+  parent2->GetViewAccessibility().OverrideName(
+      u"", ax::mojom::NameFrom::kAttributeExplicitlyEmpty);
+  child->GetViewAccessibility().OverrideName("uChild");
 
   widget->Activate();
   parent1->RequestFocus();
