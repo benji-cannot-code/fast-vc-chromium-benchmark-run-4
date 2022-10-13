65d12c3f0ac5c6cdb1786533d19d0043072837bf
Osama Fathy <osamafathy@google.com>
2022-10-13 12:51:48 +0000

    [OOBE][CHOOBE] Add ThemeSelectionScreen::ShouldBeSkipped() method
    Bug: b:253209155
    Change-Id: I7d0aa2b750edda2168760a219521fe952bd48449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3948568
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058649}

--- chrome/browser/ash/login/screens/theme_selection_screen.cc
+++ chrome/browser/ash/login/screens/theme_selection_screen.cc
@@ -55,11 +55,9 @@ ThemeSelectionScreen::ThemeSelectionScreen(
 
 ThemeSelectionScreen::~ThemeSelectionScreen() = default;
 
-bool ThemeSelectionScreen::MaybeSkip(WizardContext& context) {
-  if (context.skip_post_login_screens_for_tests) {
-    exit_callback_.Run(Result::kNotApplicable);
+bool ThemeSelectionScreen::ShouldBeSkipped(const WizardContext& context) const {
+  if (context.skip_post_login_screens_for_tests)
     return true;
-  }
 
   const PrefService::Preference* pref =
       ProfileManager::GetActiveUserProfile()->GetPrefs()->FindPreference(
@@ -67,11 +65,18 @@ bool ThemeSelectionScreen::MaybeSkip(WizardContext& context) {
   if (pref->IsManaged() || pref->IsRecommended() ||
       !chromeos::features::IsOobeThemeSelectionEnabled() ||
       !features::IsDarkLightModeEnabled()) {
-    exit_callback_.Run(Result::kNotApplicable);
     return true;
   }
 
   return false;
+}
+
+bool ThemeSelectionScreen::MaybeSkip(WizardContext& context) {
+  if (!ShouldBeSkipped(context))
+    return false;
+
+  exit_callback_.Run(Result::kNotApplicable);
+  return true;
 }
 
 void ThemeSelectionScreen::ShowImpl() {
