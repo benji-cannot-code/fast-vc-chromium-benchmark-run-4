d52947d5e4ecb2f334da8c03024ff1196b3d1c3e
Alex Ilin <alexilin@chromium.org>
2022-10-13 14:12:40 +0000

    [a11y] Fix color picker's a11y in cr-customized-themes
    A custom color picker in cr-customize-themes wasn't accessible because
    cr-customize-themes used an <input type="color"> element in an unusual
    way: an element itself was hidden and the color picker was invoked by
    calling a `click()` method from JavaScript. This makes a11y labels
    behave weirdly in the native color picker.
    
    To work around this issue, this CL makes the <input> visible but fully
    transparent and also calls `focus()` before `click()`. It seems like
    only the combination of both methods gives the desired result.
    
    Fixed: 1373479
    Change-Id: I3e8d3de99bad96ceb13b5c80801369087c7c67a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3948872
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058684}

--- ui/webui/resources/cr_components/customize_themes/customize_themes.ts
+++ ui/webui/resources/cr_components/customize_themes/customize_themes.ts
@@ -133,6 +133,7 @@ export class CustomizeThemesElement extends CustomizeThemesElementBase {
       this.showManagedThemeDialog_ = true;
       return;
     }
+    this.$.colorPicker.focus();
     this.$.colorPicker.click();
   }
 
