0ae29f11b5f864e508a507ce8c0df5f0bea0f28e
Brian Ho <hob@chromium.org>
2022-10-13 15:56:53 +0000

    viz: Selectively reenable overlays on legacy VD
    Previously, we disabled overlays on boards running the legacy video
    decoder [1] because SkiaRenderer requires all overlays to be backed
    by SharedImages, and the legacy video decoder doesn't decode into
    SharedImages.
    
    This adversely affected use cases like fast ink which requires an
    overlay. Disabling overlays uniformly was too heavy-handed as it's
    really only the video buffers that weren't allocated via
    SharedImageManager- canvases and ARC++ apps should still be promoted
    just fine. This CL selectively re-enables overlays on legacy video
    decoder platforms by checking to see if the resource is actually
    associated with a SharedImage.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3514562
    
    Bug: b:240461248
    Change-Id: If12a0cf989ea34c19ebd215c04516fd79fe2dde5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949358
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Brian Ho <hob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058747}

--- components/viz/service/display/overlay_candidate.h
+++ components/viz/service/display/overlay_candidate.h
@@ -56,6 +56,7 @@ class VIZ_SERVICE_EXPORT OverlayCandidate {
     kFailBufferFormat,
     kFailNearFilter,
     kFailPriority,
+    kFailNotSharedImage,
   };
   using TrackingId = uint32_t;
   static constexpr TrackingId kDefaultTrackingId{0};
