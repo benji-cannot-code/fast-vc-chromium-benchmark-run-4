4b719952266a1b5430ccb92aa208ab7f460ac31d
Nicola Tommasi <tommasin@chromium.org>
2022-10-13 12:11:54 +0000

    [FPS][Android] Updating First Party Sets strings
    This CL updates First Party Sets Related strings.
    
    Bug: 1349370
    Change-Id: Icec1b37649a3ba3d94e5f90d5c844faa1dd8ab56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3951990
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058633}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesPreference.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesPreference.java
@@ -42,6 +42,7 @@ public class PageInfoCookiesPreference extends SiteSettingsPreferenceFragment {
     private boolean mDeleteDisabled;
     private boolean mDataUsed;
     private CharSequence mHostName;
+    private FPSCookieInfo mFPSInfo;
 
     /**  Parameters to configure the cookie controls view. */
     public static class PageInfoCookiesViewParams {
@@ -120,8 +121,12 @@ public class PageInfoCookiesPreference extends SiteSettingsPreferenceFragment {
                 new AlertDialog.Builder(getContext(), R.style.ThemeOverlay_BrowserUI_AlertDialog)
                         .setTitle(R.string.page_info_cookies_clear)
                         .setMessage(R.string.page_info_cookies_clear_confirmation)
-                        .setMessage(
-                                getString(R.string.page_info_cookies_clear_confirmation, mHostName))
+                        .setMessage(mFPSInfo != null
+                                        ? getContext().getString(
+                                                R.string.page_info_cookies_clear_fps_confirmation,
+                                                mHostName, mFPSInfo.getOwner())
+                                        : getString(R.string.page_info_cookies_clear_confirmation,
+                                                mHostName))
                         .setPositiveButton(R.string.page_info_cookies_clear_confirmation_button,
                                 (dialog, which) -> mOnClearCallback.run())
                         .setNegativeButton(
@@ -172,6 +177,7 @@ public class PageInfoCookiesPreference extends SiteSettingsPreferenceFragment {
      * @return a boolean indicating if the FPS info has been shown or not.
      */
     public boolean maybeShowFPSInfo(FPSCookieInfo fpsInfo, String currentOrigin) {
+        mFPSInfo = fpsInfo;
         if (fpsInfo == null) {
             return false;
         }
