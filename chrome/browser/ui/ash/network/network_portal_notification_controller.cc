9e91c676e404f59f96f120b0342a174f33d1dfa3
Steven Bennetts <stevenjb@chromium.org>
2022-10-13 14:55:02 +0000

    CrOS captive portal: Add additional metrics
    Ths CL adds two new metrics:
      * Network.NetworkPortalDetectorHasProxy - to measure how many networks
        running Chrome portal detection have a proxy configured.
      * Network.NetworkPortalNotificationState - to measure how of the a
        portal detection notification is shown and which type of portal was found.
    
    BUG=b:242618993
    TEST=manual
    
    Change-Id: I70a97fbace4b7963536441b4404cb6bd1ad56ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950050
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058706}

--- chrome/browser/ui/ash/network/network_portal_notification_controller.cc
+++ chrome/browser/ui/ash/network/network_portal_notification_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ash/mobile/mobile_activator.h"
@@ -203,6 +204,9 @@ void NetworkPortalNotificationController::PortalStateChanged(
   }
   last_network_guid_ = network->guid();
   last_portal_state_ = portal_state;
+
+  base::UmaHistogramEnumeration("Network.NetworkPortalNotificationState",
+                                portal_state);
 
   std::unique_ptr<message_center::Notification> notification =
       CreateDefaultCaptivePortalNotification(network, portal_state);
