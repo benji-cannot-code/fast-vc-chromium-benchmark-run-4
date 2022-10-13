4c66dc0465378130f191e19cab5983c5ed6bfa2b
Matthias Körber <koerber@google.com>
2022-10-13 12:31:44 +0000

    Add autofill metrics utils
    Both the form_structure.* as well as the autofill_metrics.* contains a
    lot of code. This file aims to be a home for utility functions that are
    used in the context of metric acquisition that should not directly live
    in autofill_metrics.
    
    This CL only adds the file with one small function but it will be
    populated in subsequent CLs.
    
    Bug: 1373972
    Change-Id: I3e3d390d60c26739014ad5141c71dafa7ef47899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949239
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058641}

---
+++ components/autofill/core/browser/metrics/autofill_metrics_utils.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
+
+#include "base/check.h"
+#include "components/autofill/core/browser/autofill_field.h"
+
+namespace autofill {
+
+bool FieldHasMeaningfulFieldTypes(const AutofillField& field) {
+  // This function should only be invoked when the possible types have been
+  // determined.
+  DCHECK(!field.possible_types().empty());
+
+  if (field.possible_types().contains_any({UNKNOWN_TYPE, EMPTY_TYPE})) {
+    // If either UNKNOWN_TYPE or EMPTY_TYPE is present, there should be no other
+    // type
+    DCHECK_EQ(field.possible_types().size(), 1u);
+    return false;
+  }
+  return true;
+}
+
+}  // namespace autofill
