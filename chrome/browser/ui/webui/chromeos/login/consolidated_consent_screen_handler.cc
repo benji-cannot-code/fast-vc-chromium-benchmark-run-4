51464aa2c8fb8382d3394fe7cefec063ee43a6fa
Osama Fathy <osamafathy@google.com>
2022-10-13 13:56:40 +0000

    OOBE: Remove unused ScreenConfig from ConsolidatedConsentScreenView
    Since https://crrev.com/c/3851102 was landed, ScreenConfig is not
    used anymore.
    
    Bug: b:253419267
    Change-Id: I43d5e9fb08beac688b62a5a837c91e850b5e347c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952195
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058671}

--- chrome/browser/ui/webui/chromeos/login/consolidated_consent_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/consolidated_consent_screen_handler.cc
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-ConsolidatedConsentScreenView::ScreenConfig::ScreenConfig() = default;
-
-ConsolidatedConsentScreenView::ScreenConfig::~ScreenConfig() = default;
-
 ConsolidatedConsentScreenHandler::ConsolidatedConsentScreenHandler()
     : BaseScreenHandler(kScreenId) {}
 
