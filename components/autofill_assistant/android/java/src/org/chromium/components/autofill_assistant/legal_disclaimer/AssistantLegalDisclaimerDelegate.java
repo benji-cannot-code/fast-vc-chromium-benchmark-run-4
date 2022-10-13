2450bb70f71a5a291a1be84fee6d3cf44cc11ee1
Lakshmi Lokesh <lakshmilokesh@google.com>
2022-10-13 05:34:53 +0000

    [Java Component] Extend the showForm with LegalDisclaimer
    Changes Overview:
    1. New UI component for handling LegalDisclaimer scenarios for prompt-like actions.
    2. Unit tests for UI component.
    
    Note: Integration tests are added in the related CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3919930
    Tested E2E with server side changes prototype.
    
    Bug: b/247724509
    Change-Id: I7838586378f1c82b64f1e2782191ab2c802ffb26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928954
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Shashank Jain <jainshashank@google.com>
    Commit-Queue: Lakshmi Lokesh <lakshmilokesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058529}

---
+++ components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/legal_disclaimer/AssistantLegalDisclaimerDelegate.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.autofill_assistant.legal_disclaimer;
+
+/**
+ * Common interface for autofill assistant legal disclaimer delegates.
+ */
+public interface AssistantLegalDisclaimerDelegate {
+    /**
+     * Called when a text link of the legal disclaimer message <link0>text</link0> is clicked.
+     */
+    void onLinkClicked(int link);
+}
