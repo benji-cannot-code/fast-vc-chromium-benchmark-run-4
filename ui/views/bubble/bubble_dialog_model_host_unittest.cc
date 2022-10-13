7cfbdea18a0f64ce54c278ceaecf703d9ad1408a
Austin Sullivan <asully@chromium.org>
2022-10-13 00:57:22 +0000

    ui: Support setting a default button in DialogModel
    Currently, only defaulting to the accept/OK button is supported.
    
    This is required to migrate File System Access prompts to use this
    class.
    
    Fixed: 1358351
    Change-Id: I0f8ba26c1fde0e8cb9d55a25230c8b2fc15e9e40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866893
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058419}

--- ui/views/bubble/bubble_dialog_model_host_unittest.cc
+++ ui/views/bubble/bubble_dialog_model_host_unittest.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_tracker.h"
+#include "ui/views/controls/button/label_button.h"
 #include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/test/widget_test.h"
+#include "ui/views/view_class_properties.h"
 
 namespace views {
 
@@ -114,6 +116,48 @@ TEST_F(BubbleDialogModelHostTest, ElementIDsReportedCorrectly) {
   EXPECT_NE(nullptr, ui::ElementTracker::GetElementTracker()->GetUniqueElement(
                          kExtraButtonId, context));
   bubble_widget->CloseNow();
+}
+
+TEST_F(BubbleDialogModelHostTest, OverrideDefaultButton) {
+  auto host = std::make_unique<BubbleDialogModelHost>(
+      ui::DialogModel::Builder()
+          .AddCancelButton(base::OnceClosure())
+          .OverrideDefaultButton(ui::DialogButton::DIALOG_BUTTON_CANCEL)
+          .Build(),
+      /*anchor_view=*/nullptr, BubbleBorder::Arrow::TOP_RIGHT);
+  EXPECT_EQ(host->GetDefaultDialogButton(),
+            ui::DialogButton::DIALOG_BUTTON_CANCEL);
+}
+
+TEST_F(BubbleDialogModelHostTest, OverrideDefaultButtonDeathTest) {
+  EXPECT_DEATH_IF_SUPPORTED(
+      std::make_unique<BubbleDialogModelHost>(
+          ui::DialogModel::Builder()
+              .AddCancelButton(base::OnceClosure())
+              .OverrideDefaultButton(ui::DialogButton::DIALOG_BUTTON_OK)
+              .Build(),
+          /*anchor_view=*/nullptr, BubbleBorder::Arrow::TOP_RIGHT),
+      "")
+      << "Cannot override the default button with a button which does not "
+         "exist.";
+}
+
+TEST_F(BubbleDialogModelHostTest,
+       SetInitiallyFocusedViewOverridesDefaultButtonFocus) {
+  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFocusedField);
+
+  auto host = std::make_unique<BubbleDialogModelHost>(
+      ui::DialogModel::Builder()
+          .AddCancelButton(base::OnceClosure())
+          .OverrideDefaultButton(ui::DialogButton::DIALOG_BUTTON_CANCEL)
+          .AddTextfield(kFocusedField, u"label", u"text")
+          .SetInitiallyFocusedField(kFocusedField)
+          .Build(),
+      /*anchor_view=*/nullptr, BubbleBorder::Arrow::TOP_RIGHT);
+  EXPECT_EQ(host->GetDefaultDialogButton(),
+            ui::DialogButton::DIALOG_BUTTON_CANCEL);
+  EXPECT_EQ(host->GetInitiallyFocusedView()->GetProperty(kElementIdentifierKey),
+            kFocusedField);
 }
 
 }  // namespace views
