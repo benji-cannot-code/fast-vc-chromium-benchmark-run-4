c064304acd2e4ffea74a433dadc3502af3291390
Avi Drissman <avi@chromium.org>
2022-10-13 16:42:43 +0000

    Fix compile error with the macOS 13 SDK
    The macOS 13 SDK changes forward includes, and the use of
    CVPixelBufferRef now causes an error. Include the correct header
    in which it is defined.
    
    Bug: 1334739
    Change-Id: Id057f8eab4d971138a19c50da517d407aea32e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953592
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058770}

--- ui/accelerated_widget_mac/ca_renderer_layer_tree.h
+++ ui/accelerated_widget_mac/ca_renderer_layer_tree.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCELERATED_WIDGET_MAC_CA_RENDERER_LAYER_TREE_H_
 #define UI_ACCELERATED_WIDGET_MAC_CA_RENDERER_LAYER_TREE_H_
 
+#include <CoreVideo/CoreVideo.h>
 #include <IOSurface/IOSurface.h>
 #include <QuartzCore/QuartzCore.h>
 
