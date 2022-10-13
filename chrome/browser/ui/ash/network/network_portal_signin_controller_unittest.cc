4764168c3b5c26a515f649440572e090604d1749
Steven Bennetts <stevenjb@chromium.org>
2022-10-13 22:01:56 +0000

    CrOS captive portal: Use the dialog when not logged in or in kiosk mode.
    Currently we open the captive portal signin dialog if there is no user
    profile, however we do have a "user" profile in the login page so we
    need to check the login state explicitly.
    
    This also adds a check for kiosk mode in case there is a way to open
    the signin dialog once logged in as a kisok app (or one is added).
    
    BUG=b:245578628
    TEST=unit_tests --gtest_filter=NetworkPortalSigninControllerTest*
    
    Change-Id: I4e4c5a332a574e6206717a0b95906f2d2f6720aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952981
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058951}

--- chrome/browser/ui/ash/network/network_portal_signin_controller_unittest.cc
+++ chrome/browser/ui/ash/network/network_portal_signin_controller_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/proxy_config/proxy_config_dictionary.h"
 #include "components/proxy_config/proxy_config_pref_names.h"
 #include "components/proxy_config/proxy_prefs.h"
+#include "components/user_manager/fake_user_manager.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -167,6 +168,16 @@ class NetworkPortalSigninControllerTest : public testing::Test {
 };
 
 TEST_F(NetworkPortalSigninControllerTest, LoginScreen) {
+  controller_->ShowSignin();
+  EXPECT_FALSE(controller_->dialog_url().empty());
+}
+
+TEST_F(NetworkPortalSigninControllerTest, KioskMode) {
+  SimulateLogin();
+  const user_manager::User* user = user_manager_->AddKioskAppUser(
+      AccountId::FromUserEmail("fake_user@test"));
+  user_manager_->LoginUser(user->GetAccountId());
+
   controller_->ShowSignin();
   EXPECT_FALSE(controller_->dialog_url().empty());
 }
