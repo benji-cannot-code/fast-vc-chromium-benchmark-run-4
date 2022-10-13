ef9c563b2e1e140c8109738dc174fd1d638a9966
Ana Salazar <anasalazar@chromium.org>
2022-10-13 00:05:44 +0000

    Remove scroll gesture from shelf to hide app list
    The fullscreen peeking launcher could toggle visibility with a
    mousewheel or scroll gesture over the shelf. The same gesture was useful
    to navigate through app list pages; depending on the direction of the
    gesture, one could advance or retract the active page by one.
    
    After M104, Productivity launcher was default. Although the old gesture
    is being active behind a flag, it is only able to show the bubble
    launcher.
    
    Since a lot of the code for handling app list scroll from the shelf will
    now be deprecated, remove traces of the old gesture.
    
    Bug: 1372225
    Change-Id: Ib0a1d2ac08e7dbb4a351e9b16926cf0896bde8f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950570
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058383}

--- ash/app_list/app_list_controller_impl.h
+++ ash/app_list/app_list_controller_impl.h
@@ -45,10 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefRegistrySimple;
 
-namespace ui {
-class MouseWheelEvent;
-}  // namespace ui
-
 namespace ash {
 
 class AppListBadgeController;
@@ -125,8 +121,6 @@ class ASH_EXPORT AppListControllerImpl
   void Show(int64_t display_id,
             absl::optional<AppListShowSource> show_source,
             base::TimeTicks event_time_stamp);
-  void ProcessMouseWheelEvent(const ui::MouseWheelEvent& event);
-  void ProcessScrollEvent(const ui::ScrollEvent& event);
   void UpdateAppListWithNewTemporarySortOrder(
       const absl::optional<AppListSortOrder>& new_order,
       bool animate,
@@ -196,7 +190,6 @@ class ASH_EXPORT AppListControllerImpl
   void OnStateTransitionAnimationCompleted(
       AppListViewState state,
       bool was_animation_interrupted) override;
-  int AdjustAppListViewScrollOffset(int offset, ui::EventType type) override;
   void LoadIcon(const std::string& app_id) override;
   bool HasValidProfile() const override;
   bool ShouldHideContinueSection() const override;
