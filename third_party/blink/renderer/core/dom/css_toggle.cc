371ce64b7aea17e7ce82f97b48a050b146636173
L. David Baron <dbaron@chromium.org>
2022-10-13 16:43:47 +0000

    Implement toggle-visibility CSS property.
    See specification:
    https://tabatkins.github.io/css-toggle/#toggle-visibility-property
    and clarifying discussion in:
    https://github.com/tabatkins/css-toggle/issues/38
    
    (Support for toggles is behind the CSSToggles flag, which is currently
    off.)
    
    Bug: 1250716
    Change-Id: Ib29d879b7ba81dcfe95cb59fe6e6b66f0e24e528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922815
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058772}

--- third_party/blink/renderer/core/dom/css_toggle.cc
+++ third_party/blink/renderer/core/dom/css_toggle.cc
@@ -298,7 +298,7 @@ void CSSToggle::SetValueAndCheckGroup(const State& value) {
     if (specifier)
       states = &specifier->StateSet();
 
-    if (!ValueMatches(State(0u), states)) {
+    if (ValueIsActive(states)) {
       MakeRestOfToggleGroupZero();
     }
   }
@@ -554,7 +554,7 @@ void CSSToggle::ChangeToggle(const ToggleTrigger& action,
 
   // If t’s value does not match 0, and group is true, then set the value of
   // all other toggles in the same toggle group as t to 0.
-  if (is_group && !ValueMatches(State(0u), &states))
+  if (is_group && ValueIsActive(&states))
     MakeRestOfToggleGroupZero();
 }
 
--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2092,7 +2092,8 @@ class ComputedStyle : public ComputedStyleBase,
   }
 
   bool IsContentVisibilityVisible() const {
-    return ContentVisibility() == EContentVisibility::kVisible;
+    return ContentVisibility() == EContentVisibility::kVisible &&
+           ToggleVisibility().IsNull();
   }
 
   // Interleaving roots are elements that may require layout to fully update
