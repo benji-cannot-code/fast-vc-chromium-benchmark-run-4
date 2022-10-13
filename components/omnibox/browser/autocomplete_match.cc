0bef776abcb6e7cc42d05f5fab40f004abbcf4aa
manukh <manukh@chromium.org>
2022-10-13 01:30:27 +0000

    [omnibox][post-ac] Don't preserve non-default-able suggestions.
    Currently, the AC update tries to preserve the previous default
    suggestion. Often, the previous default is no longer default-able.
    
    **E.g.**
    
    User types 'xy', the default suggestion is 'xyz.com/xyw'. The user then
    types 'w', the previous suggestion, though still matching, is no longer
    default-able and should not be preserved.
    
    **The problem**
    
    Incorrectly preserving it produces a perceivable moment where
    'xyw[z.com/]' is inline autocompleted. Preserving it also means if the
    user presses enter before the incorrect default is replaced, they'll
    navigate to the wrong site.
    
    This problem is exacerbated by the `kPreserveDefault` feature, which
    makes it possible for preserving a non-defaultable match to kick the
    user out of keyword mode.
    
    **Reproducing**
    
    This reproduces most easily for HUP suggestions where the old and new
    input match the same suggestion (though also occurs in other cases too).
    
    Sync providers don't typically reproduce this, as either a) their old
    matches are not transferred if they have new matches, or b) their old
    matches are demoted during transferring so that they won't be default.
    Though there were cases where this reproduced with the bk provider.
    
    Non-HUP async providers don't typically reproduce this, as the doc and
    search providers both have synchronous cached suggestions which
    prevents their old matches from transferring.
    
    HUP suggestions when the new input no longer matches the suggestion
    don't typically reproduce this, as transferred matches are demoted so
    that they won't be default.
    
    On the other hand, HUP suggestions when the new input still matches the
    suggestion are problematic because the transferred match is merged with
    the still-matching duplicate HQP (since HUP only prefix matches)
    suggestion preventing its score from being demoted.
    
    **Fix**
    
    The CL addresses the issue by never preserving either transferred or
    non-defaultable suggestions. The 1st check is necessary as in the above
    case, the suggestion is marked defaultable and marked transferred.
    
    Bug: 1340291, 1332854, 1358961
    Change-Id: I51070845d50bff3e8e9d9fd8c788feaabf8ff294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942924
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058435}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -1426,6 +1426,8 @@ void AutocompleteMatch::UpgradeMatchWithPropertiesFrom(
     relevance = duplicate_match.relevance;
   }
 
+  from_previous = from_previous && duplicate_match.from_previous;
+
   // Take the |action|, if any, so that it will be presented instead of buried.
   if (!action && duplicate_match.action &&
       AutocompleteMatch::IsActionCompatibleType(type)) {
