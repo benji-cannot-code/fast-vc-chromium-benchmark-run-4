39274af18e4fc311894356a8cf91fcb4c13f76f1
Cathy Li <chili@chromium.org>
2022-10-13 17:44:43 +0000

    Reland "Use setDisplayHomeAsUp for backarrow implementation rather than custom navigation set up"
    The original commit was reverted due to flakey skia goldens
    
    This is a reland of commit 2e2c78fe3d711e1fa1b7e95d2d7504e89d8ae93f
    
    Original change's description:
    > Use setDisplayHomeAsUp for backarrow implementation rather than custom navigation set up
    >
    > Bug: b/248118670
    > Change-Id: Ib0f71d64821b9736c0b02ab4ffbed103fa31a6bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3912116
    > Reviewed-by: Peter Williamson <petewil@chromium.org>
    > Commit-Queue: Cathy Li <chili@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1050063}
    
    Bug: b/248118670
    Change-Id: I3d5b79b46cacbf65fc448ceb60a4eeae958ad9f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3951415
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058803}

--- chrome/android/java/src/org/chromium/chrome/browser/app/feed/followmanagement/FollowManagementActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/feed/followmanagement/FollowManagementActivity.java
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.app.feed.followmanagement;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.SnackbarActivity;
@@ -24,5 +27,21 @@ public class FollowManagementActivity extends SnackbarActivity {
 
         FollowManagementCoordinator coordinator = new FollowManagementCoordinator(this);
         setContentView(coordinator.getView());
+
+        // Set up the toolbar and back button.
+        Toolbar toolbar = (Toolbar) findViewById(org.chromium.chrome.browser.feed.R.id.action_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
