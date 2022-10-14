83f73b85012447e78ef31142da8e7bcf280e9b85
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-10-14 14:09:17 +0000

    [ReArchPPV] Make the Lacros FRE SignedIn Ctrl internal
    Moves it to the Flow Controller's anonymous namespace, since it's never
    used anywhere else, and to reduce confusion around the two categories
    of "flow controller"-named classes.
    
    Bug: 1358843
    Change-Id: Iadbc8145ac2e5af433e2c2bac85b13f0cc6eb11e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3955442
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059241}

--- chrome/browser/ui/views/profiles/first_run_flow_controller_lacros.cc
+++ chrome/browser/ui/views/profiles/first_run_flow_controller_lacros.cc
@@ -5,9 +5,127 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/profiles/first_run_flow_controller_lacros.h"
 
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/views/profiles/lacros_first_run_signed_in_flow_controller.h"
+#include "base/logging.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/views/profiles/profile_management_step_controller.h"
+#include "chrome/browser/ui/views/profiles/profile_management_utils.h"
+#include "chrome/browser/ui/views/profiles/profile_picker_signed_in_flow_controller.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+
+namespace {
+// Registers a new `Observer` that will invoke `callback_` when `manager`
+// notifies it via `OnRefreshTokensLoaded()`.
+class OnRefreshTokensLoadedObserver : public signin::IdentityManager::Observer {
+ public:
+  OnRefreshTokensLoadedObserver(signin::IdentityManager* manager,
+                                base::OnceClosure callback)
+      : callback_(std::move(callback)) {
+    DCHECK(callback_);
+    identity_manager_observation_.Observe(manager);
+  }
+
+  // signin::IdentityManager::Observer
+  void OnRefreshTokensLoaded() override {
+    identity_manager_observation_.Reset();
+
+    if (callback_)
+      std::move(callback_).Run();
+  }
+
+ private:
+  base::OnceClosure callback_;
+
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
+};
+
+class LacrosFirstRunSignedInFlowController
+    : public ProfilePickerSignedInFlowController {
+ public:
+  // `finish_flow_callback` will be called when the user completes the FRE, but
+  // might not be executed, for example if this object is destroyed before the
+  // flow is completed.
+  LacrosFirstRunSignedInFlowController(
+      ProfilePickerWebContentsHost* host,
+      Profile* profile,
+      std::unique_ptr<content::WebContents> contents,
+      base::OnceClosure sync_confirmation_seen_callback,
+      FinishFlowCallback finish_flow_callback)
+      : ProfilePickerSignedInFlowController(host,
+                                            profile,
+                                            std::move(contents),
+                                            absl::optional<SkColor>()),
+        sync_confirmation_seen_callback_(
+            std::move(sync_confirmation_seen_callback)),
+        finish_flow_callback_(std::move(finish_flow_callback)) {}
+
+  ~LacrosFirstRunSignedInFlowController() override = default;
+
+  // ProfilePickerSignedInFlowController:
+  void Init() override {
+    signin::IdentityManager* identity_manager =
+        IdentityManagerFactory::GetForProfile(profile());
+
+    if (can_retry_init_observer_)
+      can_retry_init_observer_.reset();
+
+    LOG(WARNING) << "Init running "
+                 << (identity_manager->AreRefreshTokensLoaded() ? "with"
+                                                                : "without")
+                 << " refresh tokens.";
+
+    if (!identity_manager->AreRefreshTokensLoaded()) {
+      // We can't proceed with the init yet, as the tokens will be needed to
+      // obtain extended account info and turn on sync. Register this method to
+      // be called again when they become available.
+      can_retry_init_observer_ =
+          std::make_unique<OnRefreshTokensLoadedObserver>(
+              identity_manager,
+              base::BindOnce(
+                  &LacrosFirstRunSignedInFlowController::Init,
+                  // Unretained ok: `this` owns the observer and outlives it.
+                  base::Unretained(this)));
+      return;
+    }
+
+    ProfilePickerSignedInFlowController::Init();
+
+    LOG(WARNING)
+        << "Init completed and initiative handed off to TurnSyncOnHelper.";
+  }
+
+  void FinishAndOpenBrowser(PostHostClearedCallback callback) override {
+    if (finish_flow_callback_.value())
+      std::move(finish_flow_callback_.value()).Run(std::move(callback));
+  }
+
+  void SwitchToSyncConfirmation() override {
+    DCHECK(sync_confirmation_seen_callback_);  // Should be called only once.
+    std::move(sync_confirmation_seen_callback_).Run();
+
+    ProfilePickerSignedInFlowController::SwitchToSyncConfirmation();
+  }
+
+ protected:
+  void PreShowScreenForDebug() override {
+    LOG(WARNING) << "Calling ShowScreen()";
+  }
+
+ private:
+  // Callback that gets called when the user gets to the last step of the FRE.
+  base::OnceClosure sync_confirmation_seen_callback_;
+
+  // Callback that will be called when the user completes all the steps in the
+  // flow, to finalize and close it.
+  FinishFlowCallback finish_flow_callback_;
+
+  std::unique_ptr<signin::IdentityManager::Observer> can_retry_init_observer_;
+};
+
+}  // namespace
 
 FirstRunFlowControllerLacros::FirstRunFlowControllerLacros(
     ProfilePickerWebContentsHost* host,
@@ -20,6 +138,11 @@ FirstRunFlowControllerLacros::FirstRunFlowControllerLacros(
       first_run_exited_callback_(std::move(first_run_exited_callback)) {
   DCHECK(first_run_exited_callback_);
 
+  auto mark_sync_confirmation_seen_callback =
+      base::BindOnce(&FirstRunFlowControllerLacros::MarkSyncConfirmationSeen,
+                     // Unretained ok: the callback is passed to a step that
+                     // the `this` will own and outlive.
+                     base::Unretained(this));
   auto finish_flow_callback = FinishFlowCallback(
       base::BindOnce(&FirstRunFlowControllerLacros::ExitFlowAndRun,
                      // Unretained ok: the callback is passed to a step that
@@ -28,12 +151,11 @@ FirstRunFlowControllerLacros::FirstRunFlowControllerLacros(
                      // Unretained ok: `signed_in_flow` will register a profile
                      // keep alive.
                      base::Unretained(profile)));
-
   auto signed_in_flow = std::make_unique<LacrosFirstRunSignedInFlowController>(
       host, profile,
       content::WebContents::Create(content::WebContents::CreateParams(profile)),
+      std::move(mark_sync_confirmation_seen_callback),
       std::move(finish_flow_callback));
-  signed_in_flow_ = signed_in_flow->GetWeakPtr();
 
   RegisterStep(initial_step(),
                ProfileManagementStepController::CreateForPostSignInFlow(
@@ -43,9 +165,9 @@ FirstRunFlowControllerLacros::FirstRunFlowControllerLacros(
 FirstRunFlowControllerLacros::~FirstRunFlowControllerLacros() {
   // Call the callback if not called yet. This happens when the user exits the
   // flow by closing the window, or for intent overrides.
-  if (first_run_exited_callback_ && signed_in_flow_) {
+  if (first_run_exited_callback_) {
     std::move(first_run_exited_callback_)
-        .Run(signed_in_flow_->sync_confirmation_seen()
+        .Run(sync_confirmation_seen_
                  ? ProfilePicker::FirstRunExitStatus::kQuitAtEnd
                  : ProfilePicker::FirstRunExitStatus::kQuitEarly,
              ProfilePicker::FirstRunExitSource::kControllerDestructor,
@@ -75,4 +197,8 @@ void FirstRunFlowControllerLacros::ExitFlowAndRun(
       .Run(ProfilePicker::FirstRunExitStatus::kCompleted,
            ProfilePicker::FirstRunExitSource::kFlowFinished,
            std::move(finish_flow_callback));
+}
+
+void FirstRunFlowControllerLacros::MarkSyncConfirmationSeen() {
+  sync_confirmation_seen_ = true;
 }
