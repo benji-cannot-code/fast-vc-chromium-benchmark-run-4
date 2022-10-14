bb19425fdece31a8f42a4678da2f88b0410182bd
David Bokan <bokan@chromium.org>
2022-10-14 21:35:50 +0000

    SET: Snapshots account for scrollbars
    https://crrev.com/c/3863091 made it so that snapshots are positioned
    with respect to a stable "snapshot viewport", whose position and size
    are unchanged in response to the URL bar. This CL expands this to work
    for scrollbars.
    
    Scrollbars are somewhat different from the URL bar and keyboard. They
    are part of the LayoutView's box and paint is already offset by the
    scrollbar (left-side vertical scrollbars, on an RTL page).
    
    Two additional changes are need to ensure snapshot sizing is correct in
    the presence of scrollbars:
    
      * ::page-transition now uses 100vw/vh to make it the full
        scrollbar-including viewport size.
    
      * Expand the overflow clip to include the scrollbar region.
    
    Bug: 1357156
    Change-Id: I9bf60f63b4204b6334e3d9f0246834983ec322fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938845
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059471}

--- third_party/blink/renderer/core/document_transition/document_transition_style_builder.h
+++ third_party/blink/renderer/core/document_transition/document_transition_style_builder.h
@@ -38,7 +38,7 @@ class DocumentTransitionStyleBuilder {
                           const ContainerProperties& properties,
                           WritingMode writing_mode);
 
-  void AddRootStyles(const String& rules);
+  void AddRootStyles(const gfx::RectF& snapshot_viewport_rect_css);
 
   String Build();
 
