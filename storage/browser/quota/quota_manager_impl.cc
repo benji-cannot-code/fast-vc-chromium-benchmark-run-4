96d1101becdb01a95a96d97d34c3a73c4719d58c
Evan Stade <estade@chromium.org>
2022-10-14 17:43:42 +0000

    Change name of QuotaManager::GetBucket to clarify its usage
    `GetBucket` becomes `GetBucketForTesting` because it is only used in
    tests and because production code should always identify buckets by
    their ID (and therefore should generally not need to get a bucket by
    its name).
    
    Bug: 1202167
    Change-Id: I107f861715e900a65df7d87592711f5c40cf8e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953286
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059347}

--- storage/browser/quota/quota_manager_impl.cc
+++ storage/browser/quota/quota_manager_impl.cc
@@ -1129,7 +1129,7 @@ void QuotaManagerImpl::CreateBucketForTesting(
                      weak_factory_.GetWeakPtr(), std::move(callback)));
 }
 
-void QuotaManagerImpl::GetBucket(
+void QuotaManagerImpl::GetBucketForTesting(
     const StorageKey& storage_key,
     const std::string& bucket_name,
     blink::mojom::StorageType type,
