f57965f877ccce6ef27350343fcd0b4c00f44e24
Wes Okuhara <wesokuhara@google.com>
2022-10-14 23:26:50 +0000

    CrOS Settings: Convert device page (display) to TS
    Additionally loads HTML templates for custom elements at runtime.
    
    multiple displays, etc.
    
    Bug: chromium:1315757, chromium:1292025
    Test: browser_tests --gtest-filter="OSSettingsDevice*"
    Test: Manually test Display subpage, orientation, primary display,
    Change-Id: I8c0ba8f5e67491b01207135f4892841be8c6a40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935157
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059527}

--- chrome/test/data/webui/settings/chromeos/device_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/device_page_tests.js
@@ -1270,9 +1270,22 @@ suite('SettingsDevicePage', function() {
             displayDiv.click();
             assertEquals(
                 displayPage.displays[1].id, displayPage.selectedDisplay.id);
+            flush();
 
-            displayPage.updatePrimaryDisplay_({target: {value: '0'}});
-            displayPage.onOrientationChange_({target: {value: '90'}});
+            const primaryDisplaySelect =
+                displayPage.shadowRoot.getElementById('primaryDisplaySelect');
+            assertTrue(!!primaryDisplaySelect);
+            primaryDisplaySelect.value = '0';
+            primaryDisplaySelect.dispatchEvent(new CustomEvent('change'));
+            flush();
+
+            const orientationSelect =
+                displayPage.shadowRoot.getElementById('orientationSelect');
+            assertTrue(!!orientationSelect);
+            orientationSelect.value = '90';
+            orientationSelect.dispatchEvent(new CustomEvent('change'));
+            flush();
+
             fakeSystemDisplay.onDisplayChanged.callListeners();
 
             return Promise.all([
@@ -1295,7 +1308,12 @@ suite('SettingsDevicePage', function() {
             assertEquals(90, displayPage.displays[1].rotation);
 
             // Mirror the displays.
-            displayPage.onMirroredTap_({target: {blur: function() {}}});
+            const displayMirrorCheckbox =
+                displayPage.shadowRoot.getElementById('displayMirrorCheckbox');
+            assertTrue(!!displayMirrorCheckbox);
+            displayMirrorCheckbox.click();
+            flush();
+
             fakeSystemDisplay.onDisplayChanged.callListeners();
 
             return Promise.all([
