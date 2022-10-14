8b1ef151c75068e0a9cb1b2ff65f0113571a5cc2
Dibyajyoti Pal <dibyapal@chromium.org>
2022-10-14 22:02:45 +0000

    [dPWA] Moving reusable functions from manifest update task to utils
    This CL introduces a manifest_update_utils file that stores some
    enums and utility functions that will be used by the ManifestUpdateManager,
    ManifestUpdateTask and the 2 commands. This is broken from
    crrev.com/c/3934704 which will be rebased on top of this.
    
    This leads to some duplicate functions created inside ManifestUpdateTask
    which will be removed once the task is deleted.
    
    Bug: 1298130, b/243699195
    Change-Id: Ib7126e146615b1f7a9d89594620dba18b5623170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953580
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059491}

--- chrome/browser/web_applications/manifest_update_manager.h
+++ chrome/browser/web_applications/manifest_update_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/system_web_apps/types/system_web_app_delegate_map.h"
 #include "chrome/browser/web_applications/app_registrar_observer.h"
 #include "chrome/browser/web_applications/manifest_update_task.h"
+#include "chrome/browser/web_applications/manifest_update_utils.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_install_manager.h"
 #include "chrome/browser/web_applications/web_app_install_manager_observer.h"
