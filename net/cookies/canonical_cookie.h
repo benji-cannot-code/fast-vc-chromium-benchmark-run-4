7f801c7fd71c758070bdb52aaf634385689b5894
Chris Fredrickson <cfredric@chromium.org>
2022-10-14 13:55:13 +0000

    Make CanonicalCookie's unsanitizing ctor public to avoid WrapUnique.
    This avoids having to use raw `new`, without making it possible for
    anything but CanonicalCookie to call this ctor.
    
    Change-Id: I2b93c5ca94cb3c270ecda5884e82b7dec988337b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952420
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1059239}

--- net/cookies/canonical_cookie.h
+++ net/cookies/canonical_cookie.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
+#include "base/types/pass_key.h"
 #include "net/base/features.h"
 #include "net/base/net_export.h"
 #include "net/cookies/cookie_access_result.h"
@@ -71,6 +72,30 @@ class NET_EXPORT CanonicalCookie {
   CanonicalCookie& operator=(const CanonicalCookie& other);
   CanonicalCookie& operator=(CanonicalCookie&& other);
   ~CanonicalCookie();
+
+  // This constructor does not validate or canonicalize their inputs;
+  // the resulting CanonicalCookies should not be relied on to be canonical
+  // unless the caller has done appropriate validation and canonicalization
+  // themselves.
+  // NOTE: Prefer using CreateSanitizedCookie() over directly using this
+  // constructor.
+  CanonicalCookie(base::PassKey<CanonicalCookie>,
+                  std::string name,
+                  std::string value,
+                  std::string domain,
+                  std::string path,
+                  base::Time creation,
+                  base::Time expiration,
+                  base::Time last_access,
+                  base::Time last_update,
+                  bool secure,
+                  bool httponly,
+                  CookieSameSite same_site,
+                  CookiePriority priority,
+                  bool same_party,
+                  absl::optional<CookiePartitionKey> partition_key,
+                  CookieSourceScheme scheme_secure = CookieSourceScheme::kUnset,
+                  int source_port = url::PORT_UNSPECIFIED);
 
   // Creates a new |CanonicalCookie| from the |cookie_line| and the
   // |creation_time|.  Canonicalizes inputs.  May return nullptr if
@@ -440,29 +465,6 @@ class NET_EXPORT CanonicalCookie {
  private:
   FRIEND_TEST_ALL_PREFIXES(CanonicalCookieTest, TestPrefixHistograms);
   FRIEND_TEST_ALL_PREFIXES(CanonicalCookieTest, TestHasHiddenPrefixName);
-
-  // This constructor does not validate or canonicalize their inputs;
-  // the resulting CanonicalCookies should not be relied on to be canonical
-  // unless the caller has done appropriate validation and canonicalization
-  // themselves.
-  // NOTE: Prefer using CreateSanitizedCookie() over directly using this
-  // constructor.
-  CanonicalCookie(std::string name,
-                  std::string value,
-                  std::string domain,
-                  std::string path,
-                  base::Time creation,
-                  base::Time expiration,
-                  base::Time last_access,
-                  base::Time last_update,
-                  bool secure,
-                  bool httponly,
-                  CookieSameSite same_site,
-                  CookiePriority priority,
-                  bool same_party,
-                  absl::optional<CookiePartitionKey> partition_key,
-                  CookieSourceScheme scheme_secure = CookieSourceScheme::kUnset,
-                  int source_port = url::PORT_UNSPECIFIED);
 
   // The special cookie prefixes as defined in
   // https://tools.ietf.org/html/draft-west-cookie-prefixes
