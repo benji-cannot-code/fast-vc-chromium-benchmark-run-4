4062f7fd2aa4a602845ac346d0b8d2bb8d2e79cb
Antonio Sartori <antoniosartori@chromium.org>
2022-10-14 07:34:50 +0000

    [Identifiability Study] Fix tracing of surfaces
    Tracing of IdentifiableSurfaces was introduced by
    https://crrev.com/c/3842351. However, that required the
    IdentifiabilityStudy to be enabled. This CL fixes this so that tracing
    can be enabled independently.
    
    Moreover, we make the trace events instant, since they do not have a
    meaningful scope.
    
    Bug: 1355226
    Change-Id: I2eceac68dcb6f1dd842f3a15f95353762ee00a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952091
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059136}

--- third_party/blink/renderer/platform/fonts/font_matching_metrics.cc
+++ third_party/blink/renderer/platform/fonts/font_matching_metrics.cc
@@ -34,6 +34,22 @@ HashSet<T> SetIntersection(const HashSet<T>& a, const HashSet<T>& b) {
 
 namespace blink {
 
+namespace {
+
+bool IdentifiabilityStudyShouldSampleFonts() {
+  return IdentifiabilityStudySettings::Get()->ShouldSampleAnyType({
+      IdentifiableSurface::Type::kLocalFontLookupByUniqueOrFamilyName,
+      IdentifiableSurface::Type::kLocalFontLookupByUniqueNameOnly,
+      IdentifiableSurface::Type::kLocalFontLookupByFallbackCharacter,
+      IdentifiableSurface::Type::kLocalFontLookupAsLastResort,
+      IdentifiableSurface::Type::kGenericFontLookup,
+      IdentifiableSurface::Type::kLocalFontLoadPostScriptName,
+      IdentifiableSurface::Type::kLocalFontExistenceByUniqueNameOnly,
+  });
+}
+
+}  // namespace
+
 FontMatchingMetrics::FontMatchingMetrics(
     bool top_level,
     ukm::UkmRecorder* ukm_recorder,
@@ -117,7 +133,7 @@ void FontMatchingMetrics::ReportLocalFontExistenceByUniqueNameOnly(
 void FontMatchingMetrics::InsertFontHashIntoMap(IdentifiableTokenKey input_key,
                                                 SimpleFontData* font_data,
                                                 TokenToTokenHashMap& hash_map) {
-  DCHECK(IdentifiabilityStudySettings::Get()->IsActive());
+  DCHECK(IdentifiabilityStudyShouldSampleFonts());
   if (hash_map.Contains(input_key))
     return;
   IdentifiableToken output_token(GetHashForFontData(font_data));
@@ -270,7 +286,7 @@ void FontMatchingMetrics::ReportEmojiSegmentGlyphCoverage(
 }
 
 void FontMatchingMetrics::PublishIdentifiabilityMetrics() {
-  if (!IdentifiabilityStudySettings::Get()->IsActive())
+  if (!IdentifiabilityStudyShouldSampleFonts())
     return;
 
   IdentifiabilityMetricBuilder builder(source_id_);
@@ -350,7 +366,7 @@ void FontMatchingMetrics::PublishEmojiGlyphMetrics() {
 }
 
 void FontMatchingMetrics::OnFontLookup() {
-  DCHECK(IdentifiabilityStudySettings::Get()->IsActive());
+  DCHECK(IdentifiabilityStudyShouldSampleFonts());
   if (!identifiability_metrics_timer_.IsActive()) {
     identifiability_metrics_timer_.StartOneShot(base::Minutes(1), FROM_HERE);
   }
