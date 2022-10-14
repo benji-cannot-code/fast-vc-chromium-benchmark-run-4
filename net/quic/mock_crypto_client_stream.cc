db5652481e9cb7fbdbbcbf0844c3fc4e210cb228
Tsuyoshi Horo <horo@chromium.org>
2022-10-14 08:11:35 +0000

    Roll src/net/third_party/quiche/src/ a338ea827..44da7b642 (3 commits)
    https://quiche.googlesource.com/quiche.git/+log/a338ea827764..44da7b642b96
    
    $ git log a338ea827..44da7b642 --date=short --no-merges --format='%ad %ae %s'
    2022-10-13 fayang Internal change
    2022-10-12 fayang Remove unreachable branch in QuicUtils::IsConnectionIdLengthValidForVersion.
    2022-10-12 martinduke Eliminate enum QuicConnectionIdLength.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Chromium-side changes:
      https://quiche.googlesource.com/quiche.git/+/a12ff5d removed
      quic::QuicConnectionIdLength. So this CL defines k0ByteConnectionId
      and k8ByteConnectionId in .cc files.
    
    Change-Id: I2a8facce2456dfe70fbd7923c33999d987402389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952776
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059140}

--- net/quic/mock_crypto_client_stream.cc
+++ net/quic/mock_crypto_client_stream.cc
@@ -35,7 +35,6 @@ using quic::kDefaultMaxStreamsPerConnection;
 using quic::kQBIC;
 using quic::NullDecrypter;
 using quic::NullEncrypter;
-using quic::PACKET_8BYTE_CONNECTION_ID;
 using quic::Perspective;
 using quic::ProofVerifyContext;
 using quic::QUIC_CRYPTO_MESSAGE_AFTER_HANDSHAKE_COMPLETE;
@@ -54,6 +53,11 @@ using quic::TransportParameters;
 using std::string;
 
 namespace net {
+namespace {
+
+static constexpr int k8ByteConnectionId = 8;
+
+}  // namespace
 
 MockCryptoClientStream::MockCryptoClientStream(
     const QuicServerId& server_id,
@@ -366,7 +370,7 @@ void MockCryptoClientStream::SetConfigNegotiated() {
 #endif
   cgst.push_back(kQBIC);
   QuicConfig config(config_);
-  config.SetBytesForConnectionIdToSend(PACKET_8BYTE_CONNECTION_ID);
+  config.SetBytesForConnectionIdToSend(k8ByteConnectionId);
   config.SetMaxBidirectionalStreamsToSend(kDefaultMaxStreamsPerConnection / 2);
   config.SetMaxUnidirectionalStreamsToSend(kDefaultMaxStreamsPerConnection / 2);
   config.SetInitialMaxStreamDataBytesIncomingBidirectionalToSend(
