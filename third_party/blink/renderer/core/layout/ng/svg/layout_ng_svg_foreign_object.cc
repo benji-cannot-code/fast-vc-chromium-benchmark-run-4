b32fa4c019815d2bcf22fe1d5870f8ce6c97929c
Kent Tamura <tkent@chromium.org>
2022-10-14 01:46:54 +0000

    Avoid SetLocation() in LayoutNGSVGForeignObject
    Some parents of LayoutNGSVGForeignObject don't have NGPhysicalFragments
    and NGLinks.  So, LayoutNGSVGForeignObject needs to store location information itself.
    
    This CL should not have user-visible behavior changes.
    
    Bug: 1353190
    Change-Id: Ib28263e0ac2a4668bde7f79c002eacdc1371cc22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3954582
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059064}

--- third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.cc
+++ third_party/blink/renderer/core/layout/ng/svg/layout_ng_svg_foreign_object.cc
@@ -69,6 +69,11 @@ AffineTransform LayoutNGSVGForeignObject::LocalToSVGParentTransform() const {
   return transform;
 }
 
+LayoutPoint LayoutNGSVGForeignObject::Location() const {
+  NOT_DESTROYED();
+  return overridden_location_;
+}
+
 PaintLayerType LayoutNGSVGForeignObject::LayerTypeRequired() const {
   NOT_DESTROYED();
   // Skip LayoutSVGBlock's override.
@@ -137,7 +142,7 @@ void LayoutNGSVGForeignObject::UpdateBlockLayout(bool relayout_children) {
   // would pull this information from ComputedStyle - in SVG those properties
   // are ignored for non <svg> elements, so we mimic what happens when
   // specifying them through CSS.
-  SetLocation(LayoutPoint(zoomed_location));
+  overridden_location_ = LayoutPoint(zoomed_location);
 
   UpdateNGBlockLayout();
   DCHECK(!NeedsLayout());
