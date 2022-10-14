7890e04657bcc3f7313ce9d965bd6b1523ad07a5
Alan Screen <awscreen@chromium.org>
2022-10-14 00:21:57 +0000

    Relocate enable_basic_print_dialog build flag to //printing
    Move the existing enable_basic_print_dialog build flag from
    //chrome/common to //printing.  This allows it to be used even for
    low-level printing build control.
    
    Bug: 1374181
    Change-Id: I147870cccbe25204e0cfb3ae009dba4d7a5a042f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950942
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058998}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/common/buildflags.h"
 #include "components/prefs/pref_service.h"
 #include "components/printing/common/print.mojom.h"
 #include "content/public/browser/web_ui_message_handler.h"
