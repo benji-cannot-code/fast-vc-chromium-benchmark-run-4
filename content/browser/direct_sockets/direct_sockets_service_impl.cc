63ff0db2b65cd9a704ad5a764e95c130c6aa53f5
Andrew Rayskiy <greengrape@google.com>
2022-10-14 21:57:28 +0000

    [DirectSockets] Remove IsolatedFrame() checks from OpenTcp/OpenUdp
    Change-Id: Ie59b81a9f414cbeb591ba37f2d7104c0b2bf33ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952097
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059487}

--- content/browser/direct_sockets/direct_sockets_service_impl.cc
+++ content/browser/direct_sockets/direct_sockets_service_impl.cc
@@ -56,11 +56,13 @@ network::mojom::NetworkContext*& GetNetworkContextForTesting() {
 }
 
 bool IsFrameSufficientlyIsolated(content::RenderFrameHost* frame) {
-  return frame->GetWebExposedIsolationLevel() >=
-             content::RenderFrameHost::WebExposedIsolationLevel::
-                 kMaybeIsolatedApplication &&
-         frame->IsFeatureEnabled(
-             blink::mojom::PermissionsPolicyFeature::kDirectSockets);
+  if (frame->GetWebExposedIsolationLevel() >=
+      content::RenderFrameHost::WebExposedIsolationLevel::
+          kMaybeIsolatedApplication) {
+    return true;
+  }
+
+  return false;
 }
 
 network::mojom::TCPConnectedSocketOptionsPtr CreateTCPConnectedSocketOptions(
@@ -123,7 +125,14 @@ void DirectSocketsServiceImpl::CreateForFrame(
     RenderFrameHost* render_frame_host,
     mojo::PendingReceiver<blink::mojom::DirectSocketsService> receiver) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (!render_frame_host->IsFeatureEnabled(
+          blink::mojom::PermissionsPolicyFeature::kDirectSockets)) {
+    mojo::ReportBadMessage(
+        "Permissions policy blocks access to Direct Sockets.");
+  }
   if (!IsFrameSufficientlyIsolated(render_frame_host)) {
+    mojo::ReportBadMessage(
+        "Frame is not sufficiently isolated to use Direct Sockets.");
     return;
   }
   new DirectSocketsServiceImpl(render_frame_host, std::move(receiver));
@@ -138,16 +147,6 @@ void DirectSocketsServiceImpl::OpenTcpSocket(
     mojo::PendingReceiver<network::mojom::TCPConnectedSocket> receiver,
     mojo::PendingRemote<network::mojom::SocketObserver> observer,
     OpenTcpSocketCallback callback) {
-  if (!IsFrameSufficientlyIsolated(&render_frame_host())) {
-    mojo::ReportBadMessage("Insufficient isolation to open socket");
-    return;
-  }
-
-  if (!GetNetworkContext()) {
-    mojo::ReportBadMessage("Invalid request to open socket");
-    return;
-  }
-
   const std::string remote_host = options->remote_hostname;
   const uint16_t remote_port = options->remote_port;
 
@@ -175,16 +174,6 @@ void DirectSocketsServiceImpl::OpenUdpSocket(
     mojo::PendingReceiver<blink::mojom::DirectUDPSocket> receiver,
     mojo::PendingRemote<network::mojom::UDPSocketListener> listener,
     OpenUdpSocketCallback callback) {
-  if (!IsFrameSufficientlyIsolated(&render_frame_host())) {
-    mojo::ReportBadMessage("Insufficient isolation to open socket");
-    return;
-  }
-
-  if (!GetNetworkContext()) {
-    mojo::ReportBadMessage("Invalid request to open socket");
-    return;
-  }
-
   const std::string remote_host = options->remote_hostname;
   const uint16_t remote_port = options->remote_port;
 
