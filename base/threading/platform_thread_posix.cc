36501189749afdf4a5e56c698846d5e6b081e7ea
Rakina Zata Amni <rakina@chromium.org>
2022-10-14 01:09:29 +0000

    Revert "[Fuchsia] Use Kernel IDs to identify threads"
    This reverts commit 361a7b9e584be47ce38c41dda905832d57c45d3c.
    
    Reason for revert: Suspecting this is causing build failure https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-x64-dbg/36916/overview
    
    Original change's description:
    > [Fuchsia] Use Kernel IDs to identify threads
    >
    > Fix PlatformThread::CurrentId() to return the kernel Id for the
    > thread, rather than its thread-specific handle.
    >
    > This provides consistency with e.g. Ids of enumerated threads
    > for a process, as seen in debugging tools, etc.
    >
    > Change-Id: I5c17a6425af3978513574abaacf2edbe8e43e5c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942962
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1059028}
    
    Change-Id: I63f790c6da8b7001333dee59908e2235308599f8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952777
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1059044}

--- base/threading/platform_thread_posix.cc
+++ base/threading/platform_thread_posix.cc
@@ -41,8 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_FUCHSIA)
-#include <lib/zx/thread.h>
-#include "base/fuchsia/koid.h"
+#include <zircon/process.h>
 #else
 #include <sys/resource.h>
 #endif
@@ -258,10 +257,7 @@ PlatformThreadId PlatformThread::CurrentId() {
   //   thread control block of pthread). See gettid.c in bionic.
   return gettid();
 #elif BUILDFLAG(IS_FUCHSIA)
-  thread_local static zx_koid_t id =
-      GetKoid(*zx::unowned_thread(zx::thread::self()))
-          .value_or(ZX_KOID_INVALID);
-  return id;
+  return zx_thread_self();
 #elif BUILDFLAG(IS_SOLARIS) || BUILDFLAG(IS_QNX)
   return pthread_self();
 #elif BUILDFLAG(IS_NACL) && defined(__GLIBC__)
