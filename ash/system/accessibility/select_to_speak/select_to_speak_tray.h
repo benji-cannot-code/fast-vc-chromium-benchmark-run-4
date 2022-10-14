d2ae2bb2151ab46425657b79ce5bbfe0fc05493c
Alex Newcomer <newcomer@google.com>
2022-10-14 23:34:36 +0000

    refactor select_to_speak_tray
     - Using SetPressedCallback()
     - Some slight modernizations
    
    Change-Id: Iedbeb967c909beaf41fb43b71c0daab40d01d2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933235
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059533}

--- ash/system/accessibility/select_to_speak/select_to_speak_tray.h
+++ ash/system/accessibility/select_to_speak/select_to_speak_tray.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/tray_background_view_catalog.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/system/tray/tray_background_view.h"
-#include "ui/views/controls/image_view.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 
 namespace views {
 class ImageView;
@@ -19,24 +19,24 @@ class ImageView;
 
 namespace ash {
 
+class Shelf;
+
 // A button in the tray that lets users start/stop Select-to-Speak.
 class ASH_EXPORT SelectToSpeakTray : public TrayBackgroundView,
                                      public AccessibilityObserver,
                                      public SessionObserver {
  public:
-  SelectToSpeakTray(Shelf* shelf, TrayBackgroundViewCatalogName catalog_name);
+  METADATA_HEADER(SelectToSpeakTray);
 
+  SelectToSpeakTray(Shelf* shelf, TrayBackgroundViewCatalogName catalog_name);
   SelectToSpeakTray(const SelectToSpeakTray&) = delete;
   SelectToSpeakTray& operator=(const SelectToSpeakTray&) = delete;
-
   ~SelectToSpeakTray() override;
 
   // TrayBackgroundView:
   void Initialize() override;
   std::u16string GetAccessibleNameForTray() override;
   void HandleLocaleChange() override;
-  const char* GetClassName() const override;
-  bool PerformAction(const ui::Event& event) override;
   void OnThemeChanged() override;
   // The SelectToSpeakTray does not have a bubble, so these functions are
   // no-ops.
@@ -60,8 +60,8 @@ class ASH_EXPORT SelectToSpeakTray : public TrayBackgroundView,
   // Updates icon if the color of the icon changes.
   void UpdateIconOnColorChanges();
 
-  // Weak pointer, will be parented by TrayContainer for its lifetime.
-  views::ImageView* icon_;
+  // Owned by TrayContainer for its lifetime.
+  views::ImageView* icon_ = nullptr;
 
   ScopedSessionObserver session_observer_{this};
 };
--- ash/system/accessibility/select_to_speak/select_to_speak_tray_unittest.cc
+++ ash/system/accessibility/select_to_speak/select_to_speak_tray_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/gfx/image/image_unittest_util.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 
 namespace ash {
