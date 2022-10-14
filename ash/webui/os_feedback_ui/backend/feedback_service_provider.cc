c8f589cc69fb94aac6798e8b4ba78d2f687da770
xiangdong kong <xiangdongkong@google.com>
2022-10-14 18:14:29 +0000

    feedback: reset receiver before binding mojom interface to avoid crash
    When trying to open the feedback tool while one instant is running, the
    chrome will crash due to the mojom interface was bound already.
    
    Bug: b:185624798
    Test: tools/autotest.py --run_all ash/webui/os_feedback_ui/backend/;
    Change-Id: I2e58c2ad4b1747e1cf0fec7ceb0ba40c00fbb5c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950960
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059368}

--- ash/webui/os_feedback_ui/backend/feedback_service_provider.cc
+++ ash/webui/os_feedback_ui/backend/feedback_service_provider.cc
@@ -150,6 +150,7 @@ void FeedbackServiceProvider::RecordHelpContentSearchResultCount(int count) {
 void FeedbackServiceProvider::BindInterface(
     mojo::PendingReceiver<os_feedback_ui::mojom::FeedbackServiceProvider>
         receiver) {
+  receiver_.reset();
   receiver_.Bind(std::move(receiver));
 }
 
--- ash/webui/os_feedback_ui/backend/help_content_provider.cc
+++ ash/webui/os_feedback_ui/backend/help_content_provider.cc
@@ -255,6 +255,7 @@ void HelpContentProvider::GetHelpContents(
 void HelpContentProvider::BindInterface(
     mojo::PendingReceiver<os_feedback_ui::mojom::HelpContentProvider>
         receiver) {
+  receiver_.reset();
   receiver_.Bind(std::move(receiver));
 }
 
