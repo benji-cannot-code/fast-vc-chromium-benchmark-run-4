ceeb4c368ec36b0fb7207851198bf53524a76ef5
Jérôme Lebel <jlebel@chromium.org>
2022-10-14 09:01:52 +0000

    [iOS] Fixing accessibility for TangibleSync
    Fix type in method names:
     -[TangibleSyncViewController
    setPrimaryIdentityAvatarAccessibilityLabel:]
     -[PromoStyleViewController setAvatarAccessibilityLabel:]
    
    Issue created by crrev.com/c/3934722.
    
    This patch fix accessibility for TangibleSync.
    
    Fixed: 1374418
    Change-Id: Ibb9c9d1511c0862d4ea724567e54a54146df380d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952354
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059159}

--- ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
+++ ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
@@ -532,7 +532,7 @@ constexpr CGFloat kFullAvatarImageSize = 100;
   }
 }
 
-- (void)setavatarAccessibilityLabel:(NSString*)avatarAccessibilityLabel {
+- (void)setAvatarAccessibilityLabel:(NSString*)avatarAccessibilityLabel {
   _avatarAccessibilityLabel = avatarAccessibilityLabel;
   if (self.hasAvatarImage) {
     self.avatarImageView.accessibilityLabel = avatarAccessibilityLabel;
