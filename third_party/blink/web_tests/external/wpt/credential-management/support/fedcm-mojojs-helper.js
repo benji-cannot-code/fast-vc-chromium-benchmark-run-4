7358acbdd49e0c724732b1843b8858deaeefdcb4
Peter Kotwicz <pkotwicz@chromium.org>
2022-10-15 00:08:51 +0000

    [FedcM][Testing] Clarify name of fedcm_test() test function
    This CL renames the fedcm_test() test function to fedcm_mojo_mock_test()
    to make it clearer that the test function mocks out the mojo_
    communication. This CL will avoid confusion with other functions called
    fedcm_test() - notably one added in
    https://chromium-review.googlesource.com/c/chromium/src/+/3957897 -
    which do not mock out the mojo communication.
    
    BUG=None
    
    Change-Id: Ie04b25b47387830af29e3904495ee8ad03a5d374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957558
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059547}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-mojojs-helper.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-mojojs-helper.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import { MockFederatedAuthRequest } from './fedcm-mock.js';
 
-export function fedcm_test(test_func, name, exception, properties) {
+export function fedcm_mojo_mock_test(test_func, name, exception, properties) {
   promise_test(async (t) => {
     assert_implements(navigator.credentials, 'missing navigator.credentials');
     const mock = new MockFederatedAuthRequest();
