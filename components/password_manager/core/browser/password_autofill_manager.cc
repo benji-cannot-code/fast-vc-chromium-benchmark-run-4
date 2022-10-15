53a32eba11e9222de843760605a33184c65ccafc
Viktor Semeniuk <vsemeniuk@google.com>
2022-10-15 09:40:19 +0000

    Cancel ongoing authentication in PasswordAutofillManager
    This change cancels last authentication when the new authentication is
    requested.
    
    Bug: 1322079
    Fixed: 1375024
    Change-Id: Id5b11e986ffb3d2bc6322441c2942fd2860b8d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932608
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1059637}

--- components/password_manager/core/browser/password_autofill_manager.cc
+++ components/password_manager/core/browser/password_autofill_manager.cc
@@ -509,6 +509,7 @@ void PasswordAutofillManager::DidAcceptSuggestion(
         PasswordDropdownSelectedOption::kPassword,
         password_client_->IsIncognito());
 
+    CancelBiometricReauthIfOngoing();
     scoped_refptr<device_reauth::BiometricAuthenticator> authenticator =
         password_client_->GetBiometricAuthenticator();
     // Note: this is currently only implemented on Android, Mac and Windows. For
