2b6e3b929030923044a632e97a5bd4d2847c1d8c
Sean Kau <skau@chromium.org>
2022-10-15 01:28:10 +0000

    Revert to flat_map cache in ColorProviderManager
    PWAs are each requesting a new ColorProvider so if we have enough PWAs
    open, a ColorProvider could be generated when a new tab is created.
    To mitigate this, revert to the old strategy that caches all
    ColorProviders until a theme change.
    
    Bug: 1360558
    Change-Id: I330872e53b2758bf14ffe2d64202faa2a2b5a853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953897
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059582}

--- ui/color/color_provider_manager.cc
+++ ui/color/color_provider_manager.cc
@@ -24,13 +24,12 @@ namespace ui {
 
 namespace {
 
-// Cache at most 5 ColorProviders to prevent unbounded storage from user_color.
-constexpr size_t kCacheSize = 5;
+// Estimated upper limit of what we should record for cache size.
+constexpr int kCacheHistogramMax = 100;
 
 class GlobalManager : public ColorProviderManager {
  public:
-  explicit GlobalManager(size_t cache_size = kCacheSize)
-      : ColorProviderManager(cache_size) {}
+  GlobalManager() = default;
   GlobalManager(const GlobalManager&) = delete;
   GlobalManager& operator=(const GlobalManager&) = delete;
   ~GlobalManager() override = default;
@@ -85,8 +84,7 @@ ColorProviderManager::Key& ColorProviderManager::Key::operator=(const Key&) =
 
 ColorProviderManager::Key::~Key() = default;
 
-ColorProviderManager::ColorProviderManager(size_t cache_size)
-    : color_providers_(cache_size) {
+ColorProviderManager::ColorProviderManager() {
   ResetColorProviderInitializerList();
 }
 
@@ -107,10 +105,10 @@ ColorProviderManager& ColorProviderManager::Get() {
 }
 
 // static
-ColorProviderManager& ColorProviderManager::GetForTesting(size_t cache_size) {
+ColorProviderManager& ColorProviderManager::GetForTesting() {
   absl::optional<GlobalManager>& manager = GetGlobalManager();
   if (!manager.has_value())
-    manager.emplace(cache_size);
+    manager.emplace();
   return manager.value();
 }
 
@@ -127,7 +125,7 @@ void ColorProviderManager::ResetColorProviderInitializerList() {
 
 void ColorProviderManager::ResetColorProviderCache() {
   if (!color_providers_.empty())
-    color_providers_.Clear();
+    color_providers_.clear();
 }
 
 void ColorProviderManager::AppendColorProviderInitializer(
@@ -140,7 +138,7 @@ void ColorProviderManager::AppendColorProviderInitializer(
 }
 
 ColorProvider* ColorProviderManager::GetColorProviderFor(Key key) {
-  auto iter = color_providers_.Get(key);
+  auto iter = color_providers_.find(key);
   if (iter == color_providers_.end()) {
     auto provider = std::make_unique<ColorProvider>();
     DCHECK(initializer_list_);
@@ -148,9 +146,9 @@ ColorProvider* ColorProviderManager::GetColorProviderFor(Key key) {
       initializer_list_->Notify(provider.get(), key);
 
     provider->GenerateColorMap();
-    iter = color_providers_.Put(key, std::move(provider));
+    iter = color_providers_.emplace(key, std::move(provider)).first;
     base::UmaHistogramExactLinear("Views.ColorProviderCacheSize",
-                                  color_providers_.size(), kCacheSize);
+                                  color_providers_.size(), kCacheHistogramMax);
   }
   ColorProvider* provider = iter->second.get();
   DCHECK(provider);
