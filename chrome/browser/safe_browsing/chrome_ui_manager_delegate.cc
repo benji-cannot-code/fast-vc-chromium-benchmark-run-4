f8dc06e0e228c5b679464005ca57ad082f5f8f09
JunHo Seo <junho0924.seo@lge.com>
2022-10-15 02:31:37 +0000

    Change ProcessManager::GetExtensionHostForRenderFrameHost() to be more clear
    1. Changed the name to GetBackgroundHostForRenderFrameHost
    Because the method only checks existing background hosts.
    
    2. Limited to serve only for primary main frame to avoid
    getting background host from non extension frames.
    
    Bug: 1340001
    Change-Id: Ibab2d4950ab853bb690571d4396a09155063fb16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935664
    Commit-Queue: JunHo Seo <junho0924.seo@lge.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059594}

--- chrome/browser/safe_browsing/chrome_ui_manager_delegate.cc
+++ chrome/browser/safe_browsing/chrome_ui_manager_delegate.cc
@@ -68,7 +68,7 @@ bool ChromeSafeBrowsingUIManagerDelegate::IsHostingExtension(
     return false;
 
   extensions::ExtensionHost* extension_host =
-      extension_manager->GetExtensionHostForRenderFrameHost(
+      extension_manager->GetBackgroundHostForRenderFrameHost(
           web_contents->GetPrimaryMainFrame());
   return extension_host != nullptr;
 #else
