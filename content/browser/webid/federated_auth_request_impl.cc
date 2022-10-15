d3c25ca9664a28d28230910cd26b90ed0594130a
Peter Kotwicz <pkotwicz@chromium.org>
2022-10-15 03:11:20 +0000

    [FedCM] Compute embedding and iframe origin in consistent way
    This CL changes how `rp_url_for_display` and
    `rp_iframe_url_for_display` are computed to be consistent with how
    the origins are computed for FedCM permissions
    
    BUG=None
    
    Change-Id: Id55789f5ccbf1e0f78f4f45f9000d8fca0a1a2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953413
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059600}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -301,6 +301,10 @@ std::string FormatUrlForDisplay(const GURL& url) {
       url_formatter::SchemeDisplay::OMIT_HTTP_AND_HTTPS));
 }
 
+std::string FormatOriginForDisplay(const url::Origin& origin) {
+  return FormatUrlForDisplay(origin.GetURL());
+}
+
 bool ShouldFailIfNotSignedInWithIdp(
     const GURL& idp_url,
     FederatedIdentitySharingPermissionContextDelegate*
@@ -838,8 +842,7 @@ void FederatedAuthRequestImpl::MaybeShowAccountsDialog(
   if (!pending_idps_.empty())
     return;
 
-  std::string rp_url_for_display =
-      FormatUrlForDisplay(rp_web_contents->GetLastCommittedURL());
+  std::string rp_url_for_display = FormatOriginForDisplay(GetEmbeddingOrigin());
 
   std::vector<IdentityProviderData> idp_data_for_display;
   for (const auto& idp : idp_order_) {
@@ -849,8 +852,7 @@ void FederatedAuthRequestImpl::MaybeShowAccountsDialog(
 
   absl::optional<std::string> iframe_url_for_display = absl::nullopt;
   if (IsFedCmIframeSupportEnabled() && show_iframe_requester_) {
-    iframe_url_for_display =
-        FormatUrlForDisplay(render_frame_host().GetLastCommittedURL());
+    iframe_url_for_display = FormatOriginForDisplay(origin());
   }
 
   request_dialog_controller_->ShowAccountsDialog(
@@ -893,10 +895,8 @@ void FederatedAuthRequestImpl::HandleAccountsFetchFailure(
   DCHECK(render_frame_host().GetMainFrame()->IsInPrimaryMainFrame());
 
   request_dialog_controller_->ShowFailureDialog(
-      rp_web_contents,
-      FormatUrlForDisplay(rp_web_contents->GetLastCommittedURL()),
-      FormatUrlForDisplay(idp_url),
-      FormatUrlForDisplay(render_frame_host().GetLastCommittedURL()),
+      rp_web_contents, FormatOriginForDisplay(GetEmbeddingOrigin()),
+      FormatUrlForDisplay(idp_url), FormatOriginForDisplay(origin()),
       base::BindOnce(
           &FederatedAuthRequestImpl::OnDismissFailureDialog,
           weak_ptr_factory_.GetWeakPtr(), FederatedAuthRequestResult::kError,
