654cdd27691bad21f7a1d8b442ac3da80b4686dd
Loko Kung <lokokung@google.com>
2022-10-16 19:16:29 +0000

    Add error messages and logging to WebGPU caching trace tests.
    Bug: dawn:549
    Change-Id: Ic1ebab5ab9230921a22197f0c0de830b67736bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950475
    Commit-Queue: Loko Kung <lokokung@google.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059744}

--- content/test/data/gpu/webgpu-unittest-worker.js
+++ content/test/data/gpu/webgpu-unittest-worker.js
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import { webGpuUnitTests } from './webgpu-unittest-utils.js';
 
 onmessage = async (e) => {
-  postMessage({ result: await webGpuUnitTests.runTest(e.data.testId) });
+  postMessage(await webGpuUnitTests.runTest(e.data.testId));
 }
