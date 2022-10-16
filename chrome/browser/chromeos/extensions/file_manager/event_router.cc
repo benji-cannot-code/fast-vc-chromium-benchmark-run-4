8d90ba1a2eb84c99809eca01470ee6309df0a8d5
Claudio DeSouza <cdesouza@igalia.com>
2022-10-16 18:43:34 +0000

    Apply modernize-loop-convert on //chrome extensions code I
    This CL uses clang-tidy modernize-loop-convert to convert index based
    loops into range-based for loops, for code under //chrome associated
    with extensions.
    
    Bug: 776257, 895746
    Change-Id: Ie3276acd6c6d964b21abf7e01fe6d5e992938b33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3889652
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059742}

--- chrome/browser/chromeos/extensions/file_manager/event_router.cc
+++ chrome/browser/chromeos/extensions/file_manager/event_router.cc
@@ -114,8 +114,7 @@ bool IsRecoveryToolRunning(Profile* profile) {
       "jndclpdbaamdhonoechobihbbiimdgai"   // Recovery tool prod
   };
 
-  for (size_t i = 0; i < std::size(kRecoveryToolIds); ++i) {
-    const std::string extension_id = kRecoveryToolIds[i];
+  for (auto extension_id : kRecoveryToolIds) {
     if (extension_prefs->IsExtensionRunning(extension_id))
       return true;
   }
