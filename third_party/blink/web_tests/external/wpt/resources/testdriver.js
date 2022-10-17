e8df0b9db49dd02e8feee5088ad46cee199a217c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-17 23:24:29 +0000

    Import wpt@96c883ecc0d911875b4c7e0238a20744a63218be
    Using wpt-import in Chromium add3f4073a7f2652bf07bf891233f3693df1be7f.
    With Chromium commits locally applied on WPT:
    4c9aa435ea "[Mixed Content] WPT tests for sandboxed documents."
    a8a7c7e514 "Fix HTML map element not matching image using id attribute"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    antoniosartori@chromium.org, arthursonzogni@chromium.org:
      external/wpt/content-security-policy
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    japhet@chromium.org:
      external/wpt/fetch
    jsbell@chromium.org:
      external/wpt/resources
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/selectors
    yosin@chromium.org, pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I830d09d95741e154c3f8c27b530e768505e9de35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961831
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1060201}

--- third_party/blink/web_tests/external/wpt/resources/testdriver.js
+++ third_party/blink/web_tests/external/wpt/resources/testdriver.js
@@ -678,7 +678,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             return Promise.reject(new Error("unimplemented"));
         },
 
-        delete_named_cookie: function(name, context=null) {
+        get_named_cookie: function(name, context=null) {
             return Promise.reject(new Error("unimplemented"));
         },
 
