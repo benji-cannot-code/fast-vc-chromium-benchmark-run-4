0d05c555c29b759416eefcd02aa797363c9e8e93
Mike Dougherty <michaeldo@chromium.org>
2022-10-17 22:26:47 +0000

    Convert include to imports in ios/web_view/internal/translate/*
    Bug: None
    Change-Id: I02b146ae2eb53721abfd16d99228c5441c022bd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962012
    Reviewed-by: John Wu <jzw@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060175}

--- ios/web_view/internal/translate/cwv_translation_policy_unittest.mm
+++ ios/web_view/internal/translate/cwv_translation_policy_unittest.mm
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#include "base/strings/sys_string_conversions.h"
+#import "base/strings/sys_string_conversions.h"
 #import "ios/web_view/internal/translate/cwv_translation_language_internal.h"
-#include "testing/gtest/include/gtest/gtest.h"
+#import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
-#include "testing/platform_test.h"
+#import "testing/platform_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
--- ios/web_view/internal/translate/web_view_translate_service.mm
+++ ios/web_view/internal/translate/web_view_translate_service.mm
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web_view/internal/translate/web_view_translate_service.h"
+#import "ios/web_view/internal/translate/web_view_translate_service.h"
 
-#include "base/bind.h"
-#include "base/memory/ptr_util.h"
-#include "base/no_destructor.h"
-#include "components/translate/core/browser/translate_download_manager.h"
-#include "ios/web_view/internal/app/application_context.h"
+#import "base/bind.h"
+#import "base/memory/ptr_util.h"
+#import "base/no_destructor.h"
+#import "components/translate/core/browser/translate_download_manager.h"
+#import "ios/web_view/internal/app/application_context.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
