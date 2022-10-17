14aa21c778d912e444566578b91260b50a5469f0
Jonathan Freed <freedjm@chromium.org>
2022-10-17 20:46:29 +0000

    Adding client support for experiment tagging using only an experiment ID.
    Bug: 1374046
    Change-Id: I3b57aebca9d26416a447661077e4e0e2316d649c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3950057
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Jonathan Freed <freedjm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060130}

--- components/feed/core/v2/public/types.h
+++ components/feed/core/v2/public/types.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iosfwd>
 #include <map>
 #include <string>
+#include <vector>
 
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
@@ -73,9 +74,9 @@ using EphemeralChangeId = base::IdTypeU32<class EphemeralChangeIdClass>;
 using SurfaceId = base::IdTypeU32<class SurfaceIdClass>;
 using ImageFetchId = base::IdTypeU32<class ImageFetchIdClass>;
 
-// A map of trial names (key) to group names (value) that is
+// A map of trial names (key) and list of group names/IDs (value)
 // sent from the server.
-typedef std::map<std::string, std::string> Experiments;
+typedef std::map<std::string, std::vector<std::string>> Experiments;
 
 struct NetworkResponseInfo {
   NetworkResponseInfo();
