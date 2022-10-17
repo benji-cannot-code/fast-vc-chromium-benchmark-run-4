f4210c9ccd5763f589d3c01ff08cd4feeb0529ac
Kalvin Lee <kdlee@chromium.org>
2022-10-17 09:27:30 +0000

    [PA] ios: IWYU
    To use `DCHECK()`, one must include `//base/check.h`.
    
    Bug: 1371729
    Change-Id: Ieec114a0c639bcb1f0dc812c66a7c5e167be51d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3954830
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059850}

--- ios/chrome/browser/ui/settings/password/password_settings/scoped_password_settings_reauth_module_override.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/scoped_password_settings_reauth_module_override.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/password/password_settings/scoped_password_settings_reauth_module_override.h"
 
+#import "base/check.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_module.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
