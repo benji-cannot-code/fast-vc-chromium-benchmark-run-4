cfdd281401c8ea8d70f5a327cf3a05cb8eacda9a
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2022-10-17 22:28:04 +0000

    [floss] Implement remote gatt characteristic
    Implement the BluetoothRemoteGattCharacteristicFloss class which handles
    reads and writes to a characteristic.
    
    BUG=b:193685841
    TEST=tools/autotest.py -C out/msan --run_all ./device/bluetooth/floss/
    
    Change-Id: Iaddd6da5b0dd353d23ab161134568f744f8aef00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3951410
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060177}

--- device/bluetooth/floss/bluetooth_gatt_service_floss.h
+++ device/bluetooth/floss/bluetooth_gatt_service_floss.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_BLUETOOTH_FLOSS_BLUETOOTH_GATT_SERVICE_FLOSS_H_
 #define DEVICE_BLUETOOTH_FLOSS_BLUETOOTH_GATT_SERVICE_FLOSS_H_
 
+#include <map>
+
 #include "device/bluetooth/bluetooth_gatt_service.h"
 #include "device/bluetooth/floss/floss_gatt_client.h"
 
@@ -14,7 +16,8 @@ class BluetoothAdapterFloss;
 
 // Subclass of |BluetoothGattService| for platforms that use Floss.
 class DEVICE_BLUETOOTH_EXPORT BluetoothGattServiceFloss
-    : public device::BluetoothGattService {
+    : public device::BluetoothGattService,
+      public FlossGattClientObserver {
  public:
   BluetoothGattServiceFloss(const BluetoothGattServiceFloss&) = delete;
   BluetoothGattServiceFloss& operator=(const BluetoothGattServiceFloss&) =
@@ -27,9 +30,40 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothGattServiceFloss
   static device::BluetoothGattService::GattErrorCode GattStatusToServiceError(
       const GattStatus status);
 
+  // Adds an observer for a specific handle. This observer will only get
+  // callbacks invoked for that specific handle.
+  void AddObserverForHandle(int32_t handle, FlossGattClientObserver* observer);
+
+  // Removes the observer for a specific handle.
+  void RemoveObserverForHandle(int32_t handle);
+
+  // FlossGattClientObserver overrides.
+  void GattCharacteristicRead(std::string address,
+                              GattStatus status,
+                              int32_t handle,
+                              const std::vector<uint8_t>& data) override;
+  void GattCharacteristicWrite(std::string address,
+                               GattStatus status,
+                               int32_t handle) override;
+  void GattDescriptorRead(std::string address,
+                          GattStatus status,
+                          int32_t handle,
+                          const std::vector<uint8_t>& data) override;
+  void GattDescriptorWrite(std::string address,
+                           GattStatus status,
+                           int32_t handle) override;
+  void GattNotify(std::string address,
+                  int32_t handle,
+                  const std::vector<uint8_t>& data) override;
+
  protected:
   explicit BluetoothGattServiceFloss(BluetoothAdapterFloss* adapter);
   ~BluetoothGattServiceFloss() override;
+
+  // Cache of observers tied to a specific handle. When callbacks are observed
+  // for a specific handle within this GATT service, it is dispatched here to
+  // that specific observer.
+  std::map<int32_t, raw_ptr<FlossGattClientObserver>> observer_by_handle_;
 
  private:
   // The adapter associated with (and which indirectly owns) this service.
