8e853af7f927b29def7e068650be9fc39ce668e5
Michael Ershov <miersh@google.com>
2022-10-17 16:54:44 +0000

    [Code Health] Delete SyncProtocolError::ToValue()
    The SyncProtocolError::ToValue() method returns the deprecated
    type base::DictionaryValue and there's no reason to refactor
    it because it's never used.
    
    Bug: 1187061
    Test: Compilation
    Change-Id: I32d6954a8151488a5e62fefc86fd17e32876b288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939470
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060003}

--- components/sync/protocol/sync_protocol_error.h
+++ components/sync/protocol/sync_protocol_error.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/values.h"
 #include "components/sync/base/model_type.h"
 
 namespace syncer {
@@ -79,7 +78,6 @@ struct SyncProtocolError {
   SyncProtocolError();
   SyncProtocolError(const SyncProtocolError& other);
   ~SyncProtocolError();
-  std::unique_ptr<base::DictionaryValue> ToValue() const;
 };
 
 const char* GetSyncErrorTypeString(SyncProtocolErrorType type);
