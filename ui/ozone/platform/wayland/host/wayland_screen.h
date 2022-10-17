1c617e79723d7c9de96b53d8cc3b0846bfc886d5
Erik Chen <erikchen@chromium.org>
2022-10-17 08:47:41 +0000

    Revert "Use same display id in lacros"
    This reverts commit 6d30f144fb6667e622a273865e3aba39afd8c7f5.
    
    Reason for revert:
    lacros does not launch on ToT:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1375158
    
    Original change's description:
    > Use same display id in lacros
    >
    > Lacros uses mojo to create windows, hence can't use output object there (also violates layer dependency).
    > This will also make it easy match the display information on ash-chrome and lacros, which will be helpful when troubleshoot/disagnoze display related issues.
    >
    > Bug: 1342798
    > Test: WaylandZauraOutputTest.DisplayIdConversions , and updated existing test to cover output id != display id scenario.
    > Change-Id: I4bf29f346fe1efd712809932cd19ca1912c42974
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884722
    > Reviewed-by: Maksim Sisov <msisov@igalia.com>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1059294}
    
    Bug: 1342798, 1375083
    Change-Id: I1419724a466c18b80d326a8010d5b50668ce6792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3956817
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1059831}

--- ui/ozone/platform/wayland/host/wayland_screen.h
+++ ui/ozone/platform/wayland/host/wayland_screen.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
-#include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
-#include "ui/ozone/platform/wayland/host/wayland_output.h"
 #include "ui/ozone/public/platform_screen.h"
 
 namespace gfx {
@@ -45,7 +43,6 @@ class WaylandScreen : public PlatformScreen {
   ~WaylandScreen() override;
 
   void OnOutputAddedOrUpdated(uint32_t output_id,
-                              int64_t display_id,
                               const gfx::Point& origin,
                               const gfx::Size& logical_size,
                               const gfx::Size& physical_size,
@@ -55,8 +52,6 @@ class WaylandScreen : public PlatformScreen {
                               int32_t logical_transform,
                               const std::string& label);
   void OnOutputRemoved(uint32_t output_id);
-
-  uint32_t GetOutputIdForDisplayId(int64_t display_id);
 
   void OnTabletStateChanged(display::TabletState tablet_state);
 
@@ -119,7 +114,6 @@ class WaylandScreen : public PlatformScreen {
   // All parameters are in DIP screen coordinates/units except |physical_size|,
   // which is in physical pixels.
   void AddOrUpdateDisplay(uint32_t output_id,
-                          int64_t display_id,
                           const gfx::Point& origin,
                           const gfx::Size& logical_size,
                           const gfx::Size& physical_size,
@@ -131,7 +125,6 @@ class WaylandScreen : public PlatformScreen {
 
   raw_ptr<WaylandConnection> connection_ = nullptr;
 
-  base::flat_map<WaylandOutput::Id, int64_t> display_id_map_;
   display::DisplayList display_list_;
 
   base::ObserverList<display::DisplayObserver> observers_;
--- ui/ozone/platform/wayland/host/wayland_zaura_output_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_zaura_output_unittest.cc
@@ -22,12 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/test_wayland_server_thread.h"
 #include "ui/ozone/platform/wayland/test/test_zaura_output.h"
 
-#include "base/logging.h"
-
 namespace ui {
+namespace {
 
 using ::testing::Values;
-namespace {
+
 class WaylandZAuraOutputTest : public ::testing::Test {
  public:
   WaylandZAuraOutputTest()
@@ -78,12 +77,10 @@ class WaylandZAuraOutputTest : public ::testing::Test {
   std::unique_ptr<WaylandScreen> platform_screen_;
 };
 
-}  // namespace
-
 TEST_F(WaylandZAuraOutputTest, HandleInsets) {
   WaylandOutput* wayland_output = output_manager_->GetPrimaryOutput();
   ASSERT_TRUE(wayland_output);
-  EXPECT_TRUE(wayland_output->IsReady());
+  EXPECT_TRUE(wayland_output->is_ready());
   EXPECT_EQ(wayland_output->physical_size(), gfx::Size(800, 600));
   EXPECT_TRUE(wayland_output->insets().IsEmpty());
   EXPECT_TRUE(wayland_output->get_zaura_output());
@@ -104,7 +101,7 @@ TEST_F(WaylandZAuraOutputTest, HandleInsets) {
   server_.Pause();
 
   // Verify that insets is updated.
-  EXPECT_TRUE(wayland_output->IsReady());
+  EXPECT_TRUE(wayland_output->is_ready());
   EXPECT_EQ(wayland_output->physical_size(), gfx::Size(800, 600));
   EXPECT_EQ(wayland_output->insets(), sent_insets);
 }
@@ -112,7 +109,7 @@ TEST_F(WaylandZAuraOutputTest, HandleInsets) {
 TEST_F(WaylandZAuraOutputTest, HandleLogicalTransform) {
   WaylandOutput* wayland_output = output_manager_->GetPrimaryOutput();
   ASSERT_TRUE(wayland_output);
-  EXPECT_TRUE(wayland_output->IsReady());
+  EXPECT_TRUE(wayland_output->is_ready());
   EXPECT_FALSE(wayland_output->logical_transform());
   EXPECT_TRUE(wayland_output->get_zaura_output());
 
@@ -127,33 +124,9 @@ TEST_F(WaylandZAuraOutputTest, HandleLogicalTransform) {
   base::RunLoop().RunUntilIdle();
   server_.Pause();
 
-  EXPECT_TRUE(wayland_output->IsReady());
+  EXPECT_TRUE(wayland_output->is_ready());
   EXPECT_EQ(wayland_output->logical_transform(), WL_OUTPUT_TRANSFORM_270);
 }
 
-// Test edge case display ids are converted correctly.
-TEST_F(WaylandZAuraOutputTest, DisplayIdConversions) {
-  const int64_t kTestIds[] = {
-      std::numeric_limits<int64_t>::min(),
-      std::numeric_limits<int64_t>::min() + 1,
-      static_cast<int64_t>(std::numeric_limits<int32_t>::min()) - 1,
-      std::numeric_limits<int32_t>::min(),
-      std::numeric_limits<int32_t>::min() + 1,
-      0,
-      std::numeric_limits<int32_t>::max() - 1,
-      std::numeric_limits<int32_t>::max(),
-      static_cast<int64_t>(std::numeric_limits<int32_t>::max()) + 1,
-      std::numeric_limits<int64_t>::max() - 1,
-      std::numeric_limits<int64_t>::max()};
-
-  for (int64_t id : kTestIds) {
-    uint32_t display_id_hi = static_cast<uint32_t>(id >> 32);
-    uint32_t display_id_lo = static_cast<uint32_t>(id);
-    WaylandZAuraOutput aura_output;
-    WaylandZAuraOutput::OnDisplayId(&aura_output, nullptr, display_id_hi,
-                                    display_id_lo);
-    EXPECT_EQ(id, aura_output.display_id().value());
-  }
-}
-
+}  // namespace
 }  // namespace ui
