4a80d3fcb244d6eec3cd069ebafdd6a044b53473
Xianzhu Wang <wangxianzhu@chromium.org>
2022-10-17 23:26:35 +0000

    Use double in gfx::Transform API and 4x4 matrix
    The component type of Matrix44 and the most API of gfx::Transform are
    changed to double to prepare for combination of gfx::Transform and
    blink::TransformationMarix (which uses double components).
    
    We still use float precision in gfx::Transform operations when we
    use AxisTransform2d, for simplicity, and to ensure less differences
    between the transformed path and offset (e.g. with Vector2dF) path
    for layers etc. For example, cc::Layer::offset_to_transform_parent
    and scroll offsets are Vector2dF, and it can be baked into screen
    transform etc., and we may use them in different code paths.
    
    Bug: 1359528
    Change-Id: Id4e643d170ff12f04270333749ea855d75725ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914178
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Owners-Override: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060202}

--- chrome/browser/vr/ui_unittest.cc
+++ chrome/browser/vr/ui_unittest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/geometry/test/geometry_util.h"
 
 namespace vr {
 
@@ -1258,7 +1259,7 @@ TEST_F(UiTest, ResetRepositioner) {
   model_->mutable_primary_controller().recentered = true;
 
   AdvanceFrame();
-  EXPECT_EQ(original, repositioner->world_space_transform());
+  EXPECT_TRANSFORM_NEAR(original, repositioner->world_space_transform(), 1e-20);
 }
 
 // No element in the controller root's subtree should be hit testable.
