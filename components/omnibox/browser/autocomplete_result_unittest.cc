5c0edf6acae6d279cf02571b8ba553a6fccf18f1
Tommy C. Li <tommycli@chromium.org>
2022-10-17 20:22:09 +0000

    [omnibox] Preserve entity MID in DiscourageTopMatchFromBeingSearchEntity
    In the omnibox, if the top match is an entity suggestion, we will find
    the 'plain match' duplicate, and un-deduplicate it to promote on top
    in place of the entity.
    
    This would severely reduce the amount of entity MIDs logged, because
    this 'plain match' would by default lack the MID.
    
    But this CL works around that problem by propagating the entity MID
    onto the plain match too. And this adds a test for that.
    
    It also updates the commentary in `AutocompleteMatch` to clarify that
    this `entity_id` field is used internally instead of for UI display.
    
    This should work around the plain match issue we discussed.
    
    This work is based off of mahmadi@'s proof of concept here:
    https://chromium-review.googlesource.com/c/chromium/src/+/3914069
    
    Bug: b/244507194, 1367485, 1368129
    Change-Id: I4d00dc2fd7ef14890833002c5dc7bf4c24068084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3956199
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060122}

--- components/omnibox/browser/autocomplete_result_unittest.cc
+++ components/omnibox/browser/autocomplete_result_unittest.cc
@@ -1552,6 +1552,9 @@ TEST_F(AutocompleteResultTest,
   matches[0].duplicate_matches.push_back(matches.back());
   matches.pop_back();
 
+  // Simulate the top match having an `entity_id`.
+  matches[0].entity_id = "/m/012abc";
+
   AutocompleteInput input(u"f", metrics::OmniboxEventProto::OTHER,
                           TestSchemeClassifier());
   AutocompleteResult result;
@@ -1567,6 +1570,8 @@ TEST_F(AutocompleteResultTest,
   EXPECT_EQ(match->relevance, 900);
   EXPECT_TRUE(match->allowed_to_be_default_match);
   EXPECT_EQ(match->stripped_destination_url.spec(), "http://search/?q=foo");
+  // Expect that the Entity's ID was merged over to the plain match equivalent.
+  EXPECT_EQ(match->entity_id, "/m/012abc");
 
   match = result.match_at(1);
   // The search entity suggestion should be ranked higher than the higher
@@ -1577,12 +1582,14 @@ TEST_F(AutocompleteResultTest,
   EXPECT_EQ(match->relevance, 1000);
   EXPECT_TRUE(match->allowed_to_be_default_match);
   EXPECT_EQ(match->stripped_destination_url.spec(), "http://search/?q=foo");
+  EXPECT_EQ(match->entity_id, "/m/012abc");
 
   match = result.match_at(2);
   EXPECT_EQ(match->type, AutocompleteMatchType::SEARCH_SUGGEST);
   EXPECT_EQ(match->relevance, 1200);
   EXPECT_FALSE(match->allowed_to_be_default_match);
   EXPECT_EQ(match->stripped_destination_url.spec(), "http://search/?q=foo2");
+  EXPECT_TRUE(match->entity_id.empty());
 }
 
 TEST_F(AutocompleteResultTest,
