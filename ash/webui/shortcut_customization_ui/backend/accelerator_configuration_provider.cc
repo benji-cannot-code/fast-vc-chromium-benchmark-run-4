acf94e8db29465f52669fba23651eb4eafb36e61
Jimmy <jimmyxgong@chromium.org>
2022-10-17 18:16:39 +0000

    shortcuts: Reset mojo receiver on BindInterface
    Prevents a crash in which attempting to open multiple
    instances of the app will crash Chrome.
    
    Bug: b:216049298
    Test: ash_webui_unittest
    Change-Id: I2fb13918e03087def909ae82902563c023bcc83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957271
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060058}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -88,6 +88,7 @@ void AcceleratorConfigurationProvider::BindInterface(
     mojo::PendingReceiver<
         shortcut_customization::mojom::AcceleratorConfigurationProvider>
         receiver) {
+  receiver_.reset();
   receiver_.Bind(std::move(receiver));
 }
 
