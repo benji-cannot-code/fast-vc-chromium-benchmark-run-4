cd387a9cdf70fee0cfef186ad1c23c5350ad37ea
Jérôme Lebel <jlebel@chromium.org>
2022-10-17 14:04:25 +0000

    [iOS] Update the separator height InstructionView
    Use AlignValueToPixel() to make sure the separator height is aligned
    with physical pixels.
    
    Before:
    https://screenshot.googleplex.com/AQfJiAaxAS7FqoV
    After:
    https://screenshot.googleplex.com/AuktyukGdK6ehfW
    
    Fixed: 1373484
    Change-Id: Iacf5f56082d022e08bb6989f54a7e1a34803bc18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3959578
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059923}

--- ios/chrome/browser/ui/elements/instruction_view.mm
+++ ios/chrome/browser/ui/elements/instruction_view.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/dynamic_type_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -126,7 +127,8 @@ constexpr CGFloat kIconLabelWidth = 30;
     [separator.trailingAnchor constraintEqualToAnchor:liner.trailingAnchor],
     [separator.topAnchor constraintEqualToAnchor:liner.topAnchor],
     [separator.bottomAnchor constraintEqualToAnchor:liner.bottomAnchor],
-    [liner.heightAnchor constraintEqualToConstant:kSeparatorHeight]
+    [liner.heightAnchor
+        constraintEqualToConstant:AlignValueToPixel(kSeparatorHeight)],
   ]];
 
   return liner;
