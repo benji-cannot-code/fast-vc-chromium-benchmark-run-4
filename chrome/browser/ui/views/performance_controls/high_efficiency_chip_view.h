af8481b6ef95cd37309c673e0131d5073fabb680
Steven Luong <stluong@chromium.org>
2022-10-17 18:33:23 +0000

    Fixed High Efficiency Chip Not Collapsing when Navigating Tabs
    Fixed bug where the page action chip incorrectly stays open after
    navigating to another tab also with a chip. Page action chip now
    shows in the collapsed state after navigating to another tab and coming
    back.
    
    Bug also mentions the issue where the text of the chip is not appearing
    that is believed to be caused by navigating to another tab while the
    chip is expanded. I was unable to reliably reproduce that issue and thus
    unable to determine the cause of that bug; unsure if this CL is also
    able to fix that issue.
    
    Bug: 1373168
    Change-Id: Ibb1abc3558153ca89f1d0e45e2939a8600058fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949481
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060075}

--- chrome/browser/ui/views/performance_controls/high_efficiency_chip_view.h
+++ chrome/browser/ui/views/performance_controls/high_efficiency_chip_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/performance_controls/high_efficiency_bubble_observer.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -16,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Represents the high efficiency page action chip that appears on previously
 // discarded tabs.
 class HighEfficiencyChipView : public PageActionIconView,
-                               public HighEfficiencyBubbleObserver {
+                               public HighEfficiencyBubbleObserver,
+                               public TabStripModelObserver {
  public:
   METADATA_HEADER(HighEfficiencyChipView);
   HighEfficiencyChipView(
@@ -31,6 +33,12 @@ class HighEfficiencyChipView : public PageActionIconView,
   // HighEfficiencyBubbleObserver:
   void OnBubbleShown() override;
   void OnBubbleHidden() override;
+
+  // TabStripModelObserver:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
 
  protected:
   // PageActionIconView:
