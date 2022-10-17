47723fa990cc5f31da202e3f8f245c20c26cd6cf
Henrique Nakashima <hnakashima@google.com>
2022-10-17 19:38:55 +0000

    [Android] Remove @FlakyTest count attribute from test health scripts
    This potentially impacts go/clank- since the structure of the
    exported json is changed.
    
    Bug: 1304448
    Change-Id: I7bb144a6d2da51e1dac5b07c3961d7f022703ec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3956759
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060101}

--- tools/android/test_health/test_health_extractor_unittest.py
+++ tools/android/test_health/test_health_extractor_unittest.py
@@ -51,8 +51,7 @@ class GetRepoTestHealth(unittest.TestCase):
             java_test_health=java_test_utils.JavaTestHealth(
                 java_package=_JAVA_PACKAGE_HEALTHY_TESTS,
                 disabled_tests_count=0,
-                disable_if_tests_count=0,
-                flaky_tests_count=0),
+                disable_if_tests_count=0),
             git_repo_info=_CHROMIUM_REPO_INFO)
 
         test_health_infos = test_health_extractor.get_repo_test_health(
@@ -74,8 +73,7 @@ class GetRepoTestHealth(unittest.TestCase):
             java_test_health=java_test_utils.JavaTestHealth(
                 java_package=_JAVA_PACKAGE_UNHEALTHY_TESTS,
                 disabled_tests_count=1,
-                disable_if_tests_count=1,
-                flaky_tests_count=0),
+                disable_if_tests_count=1),
             git_repo_info=_CHROMIUM_REPO_INFO)
 
         test_health_infos = test_health_extractor.get_repo_test_health(
