e738a858bcd9cb2f03fa20a8d1d90d14be66b89a
Jood Hajeer <jood@google.com>
2022-10-17 11:15:35 +0000

    Update Sharing Utils to Use new DeviceInfo Enums.
    (Third patch for Bug 1358151)
    
    Update sharing service and utils to use the new device_info enums.
    
    Update in Java as well.
    
    There should be no behavioral changes.
    
    Bug: 1358151
    Change-Id: I48827d97fe877ed4ed23a96a3b6fda094f0693fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945049
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1059886}

--- chrome/browser/sharing/sharing_device_source_sync.cc
+++ chrome/browser/sharing/sharing_device_source_sync.cc
@@ -27,26 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using sync_pb::SharingSpecificFields;
 
 namespace {
-bool IsDesktop(sync_pb::SyncEnums::DeviceType type) {
-  switch (type) {
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_CROS:
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_LINUX:
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_MAC:
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_WIN:
-      return true;
-    case sync_pb::SyncEnums_DeviceType_TYPE_PHONE:
-    case sync_pb::SyncEnums_DeviceType_TYPE_TABLET:
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_UNSET:
-    case sync_pb::SyncEnums::DeviceType::SyncEnums_DeviceType_TYPE_OTHER:
-      return false;
-  }
-}
 
 bool IsStale(const syncer::DeviceInfo& device) {
   if (base::FeatureList::IsEnabled(kSharingMatchPulseInterval)) {
     base::TimeDelta pulse_delta = base::Hours(
-        IsDesktop(device.device_type()) ? kSharingPulseDeltaDesktopHours.Get()
-                                        : kSharingPulseDeltaAndroidHours.Get());
+        device.form_factor() == syncer::DeviceInfo::FormFactor::kDesktop
+            ? kSharingPulseDeltaDesktopHours.Get()
+            : kSharingPulseDeltaAndroidHours.Get());
     base::Time min_updated_time =
         base::Time::Now() - device.pulse_interval() - pulse_delta;
     return device.last_updated_timestamp() < min_updated_time;
