1f6bc47c586013579b7c8f63c3aef3adae194c41
Adam Psarouthakis <psarouthakis@google.com>
2022-10-17 14:20:45 +0000

    [Code Health] Remove calls to base::Value::GetListDeprecated.
    Bug: 1303949
    Change-Id: I90b21a3d6437575d8f5b99a25f0bc177d3fdaba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957134
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1059931}

--- services/data_decoder/public/cpp/safe_xml_parser.cc
+++ services/data_decoder/public/cpp/safe_xml_parser.cc
@@ -61,7 +61,7 @@ bool GetXmlElementText(const base::Value& element, std::string* text) {
     return false;
 
   const base::Value* text_node = nullptr;
-  for (const base::Value& value : children->GetListDeprecated()) {
+  for (const base::Value& value : children->GetList()) {
     if (IsXmlElementOfType(value, mojom::XmlParser::kTextNodeType) ||
         IsXmlElementOfType(value, mojom::XmlParser::kCDataNodeType)) {
       text_node = &value;
@@ -104,7 +104,7 @@ int GetXmlElementChildrenCount(const base::Value& element,
   if (!children)
     return 0;
   int child_count = 0;
-  for (const base::Value& value : children->GetListDeprecated()) {
+  for (const base::Value& value : children->GetList()) {
     DCHECK(value.is_dict());
     std::string tag_name;
     bool success = GetXmlElementTagName(value, &tag_name);
@@ -119,7 +119,7 @@ const base::Value* GetXmlElementChildWithType(const base::Value& element,
   const base::Value* children = GetXmlElementChildren(element);
   if (!children)
     return nullptr;
-  for (const base::Value& value : children->GetListDeprecated()) {
+  for (const base::Value& value : children->GetList()) {
     DCHECK(value.is_dict());
     if (IsXmlElementOfType(value, type)) {
       return &value;
@@ -133,7 +133,7 @@ const base::Value* GetXmlElementChildWithTag(const base::Value& element,
   const base::Value* children = GetXmlElementChildren(element);
   if (!children)
     return nullptr;
-  for (const base::Value& value : children->GetListDeprecated()) {
+  for (const base::Value& value : children->GetList()) {
     DCHECK(value.is_dict());
     if (IsXmlElementNamed(value, tag))
       return &value;
@@ -149,7 +149,7 @@ bool GetAllXmlElementChildrenWithTag(
   if (!children)
     return false;
   bool found = false;
-  for (const base::Value& child : children->GetListDeprecated()) {
+  for (const base::Value& child : children->GetList()) {
     DCHECK(child.is_dict());
     if (IsXmlElementNamed(child, tag)) {
       found = true;
