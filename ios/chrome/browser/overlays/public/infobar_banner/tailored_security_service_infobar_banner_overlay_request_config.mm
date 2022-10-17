bf5edaf2b34640711139d15a22190d0724d35a3f
Joemer Ramos <joemerramos@chromium.org>
2022-10-17 17:51:16 +0000

    [iOS] Link InfoBar to TabHelper for Tailored Security Service
    TailoredSecurityServiceTabHelper needs to create message prompts and
    handle the interaction with the infobars created. This CL links
    infobars created in the past to the tab helper. It also allows the
    tab helper to be created in tab_helper_util.mm if the flag is enabled.
    
    Other things fixed in this CL:
    - Method that uses WebState was moved into an if condition to avoid
    calling the method when WebState is null
    - Changing the message prompt icon image name to a proper substitute
    so code doesn't crash looking for an invalid string
    - Add line to set the preference value for
    kAccountTailoredSecurityShownNotification when
    OnTailoredSecurityBitChanged is called
    - Add line to set the preference value for
    kAccountTailoredSecurityShownNotification when
    OnTailoredSecurityBitChanged is called
    
    change is behind a feature flag guard
    
    Low-Coverage-Reason: This file shouldn't have coverage as the line
    Bug: 1353363, 1358965
    Change-Id: I1461f7300eef4b71be45c8c3e6f08d89838575dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3943788
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060040}

--- ios/chrome/browser/overlays/public/infobar_banner/tailored_security_service_infobar_banner_overlay_request_config.mm
+++ ios/chrome/browser/overlays/public/infobar_banner/tailored_security_service_infobar_banner_overlay_request_config.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // TODO(crbug.com/1358296): Replace placeholder when icon png is added.
 // The name of the icon image for the tailored security service banner.
-NSString* const kIconImageName = @"icon_image";
+NSString* const kIconImageName = @"legacy_password_key";
 }  // namespace
 
 namespace tailored_security_service_infobar_overlays {
@@ -37,7 +37,6 @@ TailoredSecurityServiceBannerRequestConfig::
   button_label_text_ = delegate->GetMessageActionText();
   description_ = delegate->GetDescription();
   icon_image_name_ = kIconImageName;
-  has_badge_ = delegate->IsConsented();
 }
 
 TailoredSecurityServiceBannerRequestConfig::
--- ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper.h
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper.h
@@ -6,18 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_TAB_HELPER_H_
 #define IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_TAB_HELPER_H_
 
+#import "base/scoped_observation.h"
+#import "components/infobars/core/infobar_manager.h"
 #import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_observer.h"
 #import "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
 namespace safe_browsing {
 class TailoredSecurityService;
-}
+enum class TailoredSecurityServiceMessageState;
+}  // namespace safe_browsing
 
 // A tab helper that uses Tailored Security Service for promoting users to
 // enable better levels of Safe Browsing.
 class TailoredSecurityTabHelper
-    : public safe_browsing::TailoredSecurityServiceObserver,
+    : public infobars::InfoBarManager::Observer,
+      public safe_browsing::TailoredSecurityServiceObserver,
       public web::WebStateObserver,
       public web::WebStateUserData<TailoredSecurityTabHelper> {
  public:
@@ -41,10 +45,16 @@ class TailoredSecurityTabHelper
   void OnTailoredSecurityServiceDestroyed() override;
   void OnSyncNotificationMessageRequest(bool is_enabled) override;
 
+  // infobars::InfoBarManager::Observer implementation.
+  void OnInfoBarRemoved(infobars::InfoBar* infobar, bool animate) override;
+
  private:
   friend class web::WebStateUserData<TailoredSecurityTabHelper>;
 
   void UpdateFocusAndURL(bool focused, const GURL& url);
+
+  void ShowInfoBar(
+      safe_browsing::TailoredSecurityServiceMessageState message_state);
 
   WEB_STATE_USER_DATA_KEY_DECL();
 
@@ -61,7 +71,15 @@ class TailoredSecurityTabHelper
   bool has_query_request_ = false;
 
   // Associated WebState.
-  web::WebState* web_state_;
+  web::WebState* web_state_ = nullptr;
+
+  // The currently displayed infobar.
+  infobars::InfoBar* infobar_ = nullptr;
+
+  // Scoped observer that facilitates observing the infobar manager.
+  base::ScopedObservation<infobars::InfoBarManager,
+                          infobars::InfoBarManager::Observer>
+      infobar_manager_scoped_observation_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_TAB_HELPER_H_
