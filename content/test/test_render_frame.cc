377d6209dff006643bc4bb99647bf16480b16c4c
W. James MacLean <wjmaclean@chromium.org>
2022-10-18 19:41:49 +0000

    Remove test-only srcdoc pathway in FrameLoader.
    This CL removes a test-only pathway in frame_loader.cc
    (FillStaticResponseIfNeeded) by moving that pathway into
    test-specific code in test_web_frame_owner.h and
    content/test/test_render_frame.cc. The latter requires
    the interface code added in renderer/core/exported/test_web_frame_owner.cc which, while
    not great, still seems better than leaving the test-only
    code in frame_loader.cc.
    
    
    Bug: 1290435
    Change-Id: I200a334ee070190a297f004b60202df5b4422ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841835
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060630}

--- content/test/test_render_frame.cc
+++ content/test/test_render_frame.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/navigation/navigation_params.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/controller_service_worker.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_container.mojom.h"
+#include "third_party/blink/public/test/test_web_frame_helper.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_navigation_control.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -367,6 +368,11 @@ void TestRenderFrame::BeginNavigation(
 
     navigation_params->policy_container->policies.sandbox_flags =
         navigation_params->frame_policy->sandbox_flags;
+
+    if (url.IsAboutSrcdoc()) {
+      blink::TestWebFrameHelper::FillStaticResponseForSrcdocNavigation(
+          GetWebFrame(), navigation_params.get());
+    }
 
     frame_->CommitNavigation(std::move(navigation_params),
                              nullptr /* extra_data */);
