853d0b17710d729e2a2d6bc36c20655791104d18
Alan Screen <awscreen@chromium.org>
2022-10-18 18:43:47 +0000

    [cleanup] Replace enable_basic_printing with enable_printing
    Mechanical change to replace build flag enable_basic_printing with
    enable_printing.  The source code define was already being set as
    ENABLE_PRINTING, so this makes the build and source names match.
    
    Change-Id: Ie1be57ce1f17655f9910cecc48f602b9a986c5de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957357
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060594}

--- third_party/blink/renderer/core/frame/remote_frame_view.cc
+++ third_party/blink/renderer/core/frame/remote_frame_view.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_PRINTING)
 // nogncheck because dependency on //printing is conditional upon
-// enable_basic_printing flags.
+// enable_printing flags.
 #include "printing/metafile_skia.h"  // nogncheck
 #endif
 
