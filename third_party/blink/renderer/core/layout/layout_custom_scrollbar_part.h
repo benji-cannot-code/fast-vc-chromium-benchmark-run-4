fc85d16951af05ae8c5cf04517736365b14d1447
Kent Tamura <tkent@chromium.org>
2022-10-18 04:44:06 +0000

    Avoid SetFrameRect for LayoutCustomScrollbarPart
    For the stop-copying-back project, we should not call |LayoutBox::
    SetFrameRect()| from code outside of CopyFragmentDataToLayoutBox().
    
    This CL should not have user-visible behavior changes.
    
    Bug: 1353190
    Change-Id: I7d497fc0054dd2c0120cdacf6459c4c531062aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960310
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060302}

--- third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.h
+++ third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.h
@@ -69,6 +69,13 @@ class CORE_EXPORT LayoutCustomScrollbarPart final : public LayoutReplaced {
   // available.
   int ComputeLength() const;
 
+  // Update the overridden location and size.
+  void SetOverriddenFrameRect(const LayoutRect& rect);
+  // Rerturn the overridden location set by SetOverriddenFrameRect();
+  LayoutPoint Location() const override;
+  // Rerturn the overridden size set by SetOverriddenFrameRect();
+  LayoutSize Size() const override;
+
   LayoutUnit MarginTop() const override;
   LayoutUnit MarginBottom() const override;
   LayoutUnit MarginLeft() const override;
@@ -141,7 +148,7 @@ class CORE_EXPORT LayoutCustomScrollbarPart final : public LayoutReplaced {
 
   Member<ScrollableArea> scrollable_area_;
   Member<CustomScrollbar> scrollbar_;
-
+  LayoutRect overridden_rect_;
   ScrollbarPart part_;
 };
 
