5bd35d2330cd9a698a8fdf8077df105915a65142
Mason Freed <masonf@chromium.org>
2022-10-18 00:49:01 +0000

    Add meta timeout=long to charset-parameter test
    It gets the occasional timeout failure, so this will give it more
    time to run.
    
    Fixed: 1374068
    Change-Id: Id8291eb4f8797ecb84071e32657b075048c690bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961551
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060232}

--- third_party/blink/web_tests/external/wpt/mimesniff/mime-types/charset-parameter.window.js
+++ third_party/blink/web_tests/external/wpt/mimesniff/mime-types/charset-parameter.window.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
+
 promise_test(() => {
   // Don't load generated-mime-types.json as none of them are navigable
   return fetch("resources/mime-types.json").then(res => res.json().then(runTests));
