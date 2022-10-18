9b231afa6243ca6ccdde59b8866dfdfcfac8949c
Dave Tapuska <dtapuska@chromium.org>
2022-10-18 14:16:44 +0000

    Revert "Protect against the lists mutating during microtask execution."
    This reverts commit 9286c59ee99aece02b13d24a914af1289a326e71.
    
    Reason for revert: v8 heap regressions
    
    BUG=1375968,1375959,1376021
    
    
    Original change's description:
    > Protect against the lists mutating during microtask execution.
    >
    > The agents_ and agent_group_scheduler lists could mutate during execution of microtasks leading to a DCHECK failure.
    >
    > BUG=961186
    >
    > Change-Id: I1a030f9b94315325ccb3b38ec1f3490534657a89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957874
    > Reviewed-by: Dominic Farolino <dom@chromium.org>
    > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1060161}
    
    Bug: 961186
    Change-Id: I68c68658292b6ee9e12590cafe5bbdfd56c34588
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963349
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060451}

--- third_party/blink/renderer/platform/scheduler/main_thread/agent_group_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/agent_group_scheduler_impl.cc
@@ -129,8 +129,7 @@ void AgentGroupSchedulerImpl::RemoveAgent(Agent* agent) {
 }
 
 void AgentGroupSchedulerImpl::PerformMicrotaskCheckpoint() {
-  HeapHashSet<WeakMember<Agent>> agents(*agents_);
-  for (Agent* agent : agents) {
+  for (Agent* agent : *agents_) {
     agent->PerformMicrotaskCheckpoint();
   }
 }
