40e240a03e608585fcafddebd6ddd63f1bbb068f
Ari Chivukula <arichiv@chromium.org>
2022-10-18 23:59:37 +0000

    [CORS Cleanup] (1) Re-order code and comments
    This list has expanded organicly overtime, and it's time to impose some
    order on it. There should be no functional difference, and the function
    should now be more readable.
    
    This CL is part of a series:
    (1) Re-order code and comments
    (2) Add rtt, downlink, and ect
    (3) Add fuzzer corpus
    
    Bug: 1375854
    Change-Id: I7d34e58eb9e90ee5d936d1186b56a3ef42909da3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963449
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060757}

--- services/network/public/cpp/cors/cors_fuzzer.cc
+++ services/network/public/cpp/cors/cors_fuzzer.cc
@@ -8,10 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // TODO(crbug.com/1375854) Add corpus so generated data is higher quality.
   const std::string test_data(reinterpret_cast<const char*>(data), size);
-  network::cors::IsCorsSafelistedHeader("device-memory", test_data);
-  network::cors::IsCorsSafelistedHeader("width", test_data);
+  network::cors::IsCorsSafelistedHeader("accept", test_data);
+  network::cors::IsCorsSafelistedHeader("accept-language", test_data);
+  network::cors::IsCorsSafelistedHeader("content-language", test_data);
   network::cors::IsCorsSafelistedHeader("content-type", test_data);
+  network::cors::IsCorsSafelistedHeader("range", test_data);
+  network::cors::IsCorsSafelistedHeader("device-memory", test_data);
+  network::cors::IsCorsSafelistedHeader("dpr", test_data);
+  network::cors::IsCorsSafelistedHeader("width", test_data);
+  network::cors::IsCorsSafelistedHeader("viewport-width", test_data);
+  network::cors::IsCorsSafelistedHeader("save-data", test_data);
   network::cors::IsCorsSafelistedHeader(test_data, test_data);
   return 0;
 }
