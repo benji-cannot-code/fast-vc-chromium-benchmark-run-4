9fc37e343e748316b18ce1c2d882c0ea04ecb204
Rouslan Solomakhin <rouslan@chromium.org>
2022-10-18 18:38:21 +0000

    [Web Payment][CanMakePayment ID] Return undefined field values.
    Before this patch, enabling the
    chrome://flags/#clear-identity-in-can-make-payment flag would make
    topOrigin, paymentRequestOrigin, methodData, and modifiers fields in
    "canmakepayment" event be empty, but still defined. This prevented
    feature detection via `if (event.topOrigin === undefined)`.
    
    This patch returns an `undefined` value from the field accessors when
    the feature flag is enabled.
    
    After this patch, enabling the
    chrome://flags/#clear-identity-in-can-make-payment flag makes
    `if (event.topOrigin === undefined)` result "true", so it can be used
    for feature detection.
    
    Bug: 1290492
    Change-Id: I7d11064fdead88effd30750eeac75a86ab17145a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3955818
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060586}

--- components/test/data/payments/can_make_payment_event_fields/app/app.js
+++ components/test/data/payments/can_make_payment_event_fields/app/app.js
@@ -16,8 +16,9 @@ self.addEventListener('canmakepayment', (event) => {
   details.ifModifiers = (!!event.modifiers);
 
   // Comparisons to empty values.
-  details.emptyTopOrigin = (event.topOrigin === '');
-  details.emptyPaymentRequestOrigin = (event.paymentRequestOrigin === '');
+  details.emptyTopOrigin = (!event.topOrigin || event.topOrigin === '');
+  details.emptyPaymentRequestOrigin =
+      (!event.paymentRequestOrigin || event.paymentRequestOrigin === '');
   details.emptyMethodData = (!event.methodData || event.methodData.length == 0);
   details.emptyModifiers = (!event.modifiers || event.modifiers.length == 0);
 
@@ -27,6 +28,12 @@ self.addEventListener('canmakepayment', (event) => {
       (event.paymentRequestOrigin !== undefined);
   details.definedMethodData = (event.methodData !== undefined);
   details.definedModifiers = (event.modifiers !== undefined);
+
+  // "in" condition checks.
+  details.inTopOrigin = ('topOrigin' in event);
+  details.inPaymentRequestOrigin = ('paymentRequestOrigin' in event);
+  details.inMethodData = ('methodData' in event);
+  details.inModifiers = ('modifiers' in event);
 
   event.respondWith(true);
 });
