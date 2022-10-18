7631175b763c2aba663fdae45c8b6224a8c24b92
Tanmoy Mollik <triploblastic@chromium.org>
2022-10-18 18:32:18 +0000

    [Test] Enable ProfileOAuth2TokenServiceDelegateTest.testHasOAuth2RefreshTokenWhenAccountIsOnDevice
    Unable to produce locally. This test was flaky likely due to
    PersistedTabDataTest running before this test. For context:
    https://crbug.com/1291110#c2
    https://crbug.com/1287572
    
    Bug: 1291110
    Change-Id: Iba9aee82b70422a3e0b1eddc2942eda51f568051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952552
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060577}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegateTest.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegateTest.java
@@ -30,7 +30,6 @@ import org.chromium.base.Promise;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.components.signin.AccessTokenData;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
@@ -144,7 +143,6 @@ public class ProfileOAuth2TokenServiceDelegateTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "Test is flaky, see crbug.com/1291110")
     public void testHasOAuth2RefreshTokenWhenAccountIsOnDevice() {
         mAccountManagerFacade.addAccount(ACCOUNT);
         ThreadUtils.runOnUiThreadBlocking(
