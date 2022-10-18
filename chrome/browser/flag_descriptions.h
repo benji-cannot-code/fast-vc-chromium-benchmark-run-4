7d9fd71095dcee48147780da31c503dfff9251cd
Nicholas Hollingum <hollingum@google.com>
2022-10-18 04:12:19 +0000

    exo: Clean up exo-lock-notification
    This feature has been on by default for a while. in this cl we clean up
    the flag code and remove usages of it in chrome.
    
    Bug: b/254135851
    Change-Id: I05fb73c76fd251a0e7ddae01714ee9f11db835b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962409
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060293}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3087,9 +3087,6 @@ extern const char kExoGamepadVibrationDescription[];
 extern const char kExoOrdinalMotionName[];
 extern const char kExoOrdinalMotionDescription[];
 
-extern const char kExoLockNotificationName[];
-extern const char kExoLockNotificationDescription[];
-
 extern const char kExperimentalAccessibilityDictationWithPumpkinName[];
 extern const char kExperimentalAccessibilityDictationWithPumpkinDescription[];
 
