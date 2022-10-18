a756ac3f6fb7921dd60b3f0db0837760eca7725c
James Lee <ljjlee@google.com>
2022-10-18 00:07:26 +0000

    Add metric histogram tests
    Follow-up to crrev.com/c/3949316 adding tests for the
    ManagedMode.BlockingInterstitialCommand and
    ManagedUsers.RequestPermissionSource histograms.
    
    Bug: b/245118839
    Change-Id: I06fc646070184a811d82da319c3634a48560c2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3955402
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060221}

--- chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
@@ -294,7 +294,6 @@ void SupervisedUserNavigationThrottleTest::SetUpOnMainThread() {
 #endif
 IN_PROC_BROWSER_TEST_F(SupervisedUserNavigationThrottleTest,
                        MAYBE_DisallowPrerendering) {
-  base::HistogramTester histogram_tester;
   const GURL initial_url = embedded_test_server()->GetURL("/simple.html");
   const GURL allowed_url =
       embedded_test_server()->GetURL("/supervised_user/simple.html");
@@ -663,6 +662,8 @@ INSTANTIATE_TEST_SUITE_P(
                         /* local_web_approvals_preferred */ false)));
 
 IN_PROC_BROWSER_TEST_P(SupervisedUserIframeFilterTest, BlockSubFrame) {
+  base::HistogramTester histogram_tester;
+
   BlockHost(kIframeHost2);
   GURL allowed_url_with_iframes = embedded_test_server()->GetURL(
       kExampleHost, "/supervised_user/with_iframes.html");
@@ -690,6 +691,13 @@ IN_PROC_BROWSER_TEST_P(SupervisedUserIframeFilterTest, BlockSubFrame) {
                             permission_creator()->url_requests()[0]);
 
   EXPECT_FALSE(IsInterstitialBeingShownInFrame(blocked_frame_id));
+
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialCommandHistogramName,
+      SupervisedUserInterstitial::Commands::REMOTE_ACCESS_REQUEST, 1);
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialPermissionSourceHistogramName,
+      SupervisedUserInterstitial::RequestPermissionSource::SUB_FRAME, 1);
 }
 
 IN_PROC_BROWSER_TEST_P(SupervisedUserIframeFilterTest, BlockMultipleSubFrames) {
@@ -1034,6 +1042,7 @@ INSTANTIATE_TEST_SUITE_P(
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
                        StartLocalWebApprovalsFromMainFrame) {
+  base::HistogramTester histogram_tester;
   BlockHost(kExampleHost);
 
   GURL blocked_url = embedded_test_server()->GetURL(
@@ -1061,10 +1070,18 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
   CheckPreferredApprovalButton(blocked_frame);
+
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialCommandHistogramName,
+      SupervisedUserInterstitial::Commands::LOCAL_ACCESS_REQUEST, 1);
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialPermissionSourceHistogramName,
+      SupervisedUserInterstitial::RequestPermissionSource::MAIN_FRAME, 1);
 }
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
                        StartLocalWebApprovalsFromIframe) {
+  base::HistogramTester histogram_tester;
   BlockHost(kIframeHost1);
 
   const GURL allowed_url_with_iframes = embedded_test_server()->GetURL(
@@ -1095,6 +1112,13 @@ IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
   EXPECT_TRUE(IsLocalApprovalsButtonBeingShown(blocked_frame));
   EXPECT_TRUE(IsRemoteApprovalsButtonBeingShown(blocked_frame));
   CheckPreferredApprovalButton(blocked_frame);
+
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialCommandHistogramName,
+      SupervisedUserInterstitial::Commands::LOCAL_ACCESS_REQUEST, 1);
+  histogram_tester.ExpectUniqueSample(
+      SupervisedUserInterstitial::kInterstitialPermissionSourceHistogramName,
+      SupervisedUserInterstitial::RequestPermissionSource::SUB_FRAME, 1);
 }
 
 IN_PROC_BROWSER_TEST_P(ChromeOSLocalWebApprovalsTest,
