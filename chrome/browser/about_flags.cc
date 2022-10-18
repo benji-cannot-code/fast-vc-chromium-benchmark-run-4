1019e4553401e2ab358aebf7501603d7522c493c
Sophie Chang <sophiechang@chromium.org>
2022-10-18 16:10:17 +0000

    Add about flag for easy access to showing all clusters on Journeys zero state
    Bug: b/244505276
    Change-Id: I8b7e51afe0c26a42b9f86a6dc7d761949b66b6ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961727
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060510}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -106,6 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/flags_ui/flags_ui_switches.h"
 #include "components/heavy_ad_intervention/heavy_ad_features.h"
 #include "components/history/core/browser/features.h"
+#include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/features.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "components/invalidation/impl/invalidation_switches.h"
@@ -5855,6 +5856,13 @@ const FeatureEntry kFeatureEntries[] = {
          history_clusters::features::kOnDeviceClusteringKeywordFiltering,
          kJourneysOnDeviceClusteringKeywordFilteringVariations,
          "HistoryJourneysKeywordFiltering")},
+
+    {"history-journeys-show-all-clusters",
+     flag_descriptions::kJourneysShowAllClustersName,
+     flag_descriptions::kJourneysShowAllClustersDescription,
+     kOsDesktop | kOsAndroid,
+     SINGLE_VALUE_TYPE(history_clusters::switches::
+                           kShouldShowAllClustersOnProminentUiSurfaces)},
 
     {"page-content-annotations", flag_descriptions::kPageContentAnnotationsName,
      flag_descriptions::kPageContentAnnotationsDescription,
