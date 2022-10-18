fc07e9b4f383b8c8c735012de420dfc72b6e2454
Louis Romero <lpromero@google.com>
2022-10-18 14:48:34 +0000

    Use NSThread.isMainThread in UIKit utils
    Change-Id: I094c43e6587694568da66f36a0f4c2e30487cdc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963270
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060458}

--- ios/chrome/browser/ui/util/uikit_ui_util.mm
+++ ios/chrome/browser/ui/util/uikit_ui_util.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <stdint.h>
 #import <cmath>
 
+#import "base/check.h"
 #import "base/check_op.h"
 #import "base/ios/ios_util.h"
 #import "base/mac/foundation_util.h"
@@ -23,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/dynamic_type_util.h"
 #import "ios/chrome/browser/ui/util/rtl_geometry.h"
 #import "ios/chrome/common/ui/util/ui_util.h"
-#import "ios/web/public/thread/web_thread.h"
 #import "ui/base/l10n/l10n_util.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 #import "ui/base/resource/resource_bundle.h"
@@ -282,7 +282,7 @@ UIView* GetFirstResponderSubview(UIView* view) {
 }
 
 UIResponder* GetFirstResponder() {
-  DCHECK_CURRENTLY_ON(web::WebThread::UI);
+  DCHECK(NSThread.isMainThread);
   return GetFirstResponderSubview(GetAnyKeyWindow());
 }
 
