9d520336d76644afef935fba53a6f632944ae445
Vidhan <vidhanj@google.com>
2022-10-18 14:11:32 +0000

    [Fallbacks] Obfuscate Credit card number when shown via the context menu
    Screenshot: https://screenshot.googleplex.com/5QcdS7HKiML9dpn.png
    Bug: 1325811
    Change-Id: If90c5bf20a6eb962584b7bcbe67d3d096d01af31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963171
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1060446}

--- chrome/browser/autofill/autofill_context_menu_manager.cc
+++ chrome/browser/autofill/autofill_context_menu_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/functional/overloaded.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/browser.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/common/autofill_features.h"
 
@@ -369,9 +371,14 @@ bool AutofillContextMenuManager::CreateSubMenuWithData(
     }
 
     std::u16string value = absl::visit(
-        [type](const auto& alternative) {
-          return alternative->GetRawInfo(type);
-        },
+        base::Overloaded{[type](const CreditCard* card) {
+                           if (type == CREDIT_CARD_NUMBER)
+                             return card->ObfuscatedLastFourDigits();
+                           return card->GetRawInfo(type);
+                         },
+                         [type](const AutofillProfile* profile) {
+                           return profile->GetRawInfo(type);
+                         }},
         profile_or_credit_card);
 
     if (value.empty())
