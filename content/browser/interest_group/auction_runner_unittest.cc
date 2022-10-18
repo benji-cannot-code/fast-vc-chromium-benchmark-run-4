8a988eb496afc1765d1a13beb9bcb60eec0c6ba5
Matt Menke <mmenke@chromium.org>
2022-10-18 19:20:10 +0000

    [FLEDGE] Don't abort reporting phase on bad data from seller.
    Instead, ignore the bad data. We're moving the reporting phase after
    auction complete, which means reporting errors will no longer be able
    to fail auctions. We don't want scripts that compromise reporting
    processes to be able to cause the reporting phase to fail after the
    auction itself has succeeded.
    
    Bug: 1370882
    Change-Id: I551f33866adc9d31afc63189667dea7ee5802727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957161
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060619}

--- content/browser/interest_group/auction_runner_unittest.cc
+++ content/browser/interest_group/auction_runner_unittest.cc
@@ -7171,8 +7171,8 @@ TEST_F(AuctionRunnerTest, BadBid) {
 
 // Test cases where a bad report URL is received over Mojo from the seller
 // worklet. Bad report URLs should be rejected in the Mojo process, so this
-// results in reporting a bad Mojo message - the reporting is aborted, though
-// the auction is considered a success.
+// results in reporting a bad Mojo message, though the reporting phase is
+// allowed to continue.
 TEST_F(AuctionRunnerTest, BadSellerReportUrl) {
   StartStandardAuctionWithMockService();
 
@@ -7206,10 +7206,16 @@ TEST_F(AuctionRunnerTest, BadSellerReportUrl) {
           /*debug_win_report_url=*/absl::nullopt, /*pa_requests=*/{},
           /*errors=*/{});
 
-  // Bidder1 never gets to report anything, since the seller providing a bad
-  // report URL aborts the auction.
+  // The seller provides a bad report URL.
   seller_worklet->WaitForReportResult();
   seller_worklet->InvokeReportResultCallback(GURL("http://not.https.test/"));
+
+  // The winning bidder still gets a chance to provide a report URL.
+  mock_auction_process_manager_->WaitForWinningBidderReload();
+  bidder1_worklet =
+      mock_auction_process_manager_->TakeBidderWorklet(kBidder1Url);
+  bidder1_worklet->WaitForReportWin();
+  bidder1_worklet->InvokeReportWinCallback(GURL("https://bidder.report.test/"));
   auction_run_loop_->Run();
 
   EXPECT_EQ("Invalid seller report URL", TakeBadMessage());
@@ -7218,7 +7224,8 @@ TEST_F(AuctionRunnerTest, BadSellerReportUrl) {
   EXPECT_EQ(InterestGroupKey(kBidder1, kBidder1Name), result_.winning_group_id);
   EXPECT_EQ(GURL("https://ad1.com/"), result_.ad_url);
   EXPECT_TRUE(result_.ad_component_urls.empty());
-  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre());
+  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre(
+                                       GURL("https://bidder.report.test/")));
   EXPECT_TRUE(result_.ad_beacon_map.metadata.empty());
   EXPECT_TRUE(result_.private_aggregation_requests.empty());
   EXPECT_EQ(6, result_.bidder1_bid_count);
@@ -7232,8 +7239,8 @@ TEST_F(AuctionRunnerTest, BadSellerReportUrl) {
 
 // Test cases where a bad report URL is received over Mojo from the seller
 // worklet. Bad report URLs should be rejected in the Mojo process, so this
-// results in reporting a bad Mojo message - the reporting is aborted, though
-// the auction is considered a success.
+// results in reporting a bad Mojo message, though the reporting phase is
+// allowed to continue.
 TEST_F(AuctionRunnerTest, BadSellerBeaconUrl) {
   StartStandardAuctionWithMockService();
 
@@ -7267,12 +7274,18 @@ TEST_F(AuctionRunnerTest, BadSellerBeaconUrl) {
           /*debug_win_report_url=*/absl::nullopt, /*pa_requests=*/{},
           /*errors=*/{});
 
-  // Bidder1 never gets to report anything, since the seller providing a bad
-  // report URL aborts the auction.
+  // The seller provides a bad beacon map.
   seller_worklet->WaitForReportResult();
   seller_worklet->InvokeReportResultCallback(
-      /*report_url=*/absl::nullopt,
+      GURL("https://seller.report.test/"),
       {{"click", GURL("http://not.https.test/")}});
+
+  // The winning bidder still gets a chance to provide a report URL.
+  mock_auction_process_manager_->WaitForWinningBidderReload();
+  bidder1_worklet =
+      mock_auction_process_manager_->TakeBidderWorklet(kBidder1Url);
+  bidder1_worklet->WaitForReportWin();
+  bidder1_worklet->InvokeReportWinCallback(GURL("https://bidder.report.test/"));
   auction_run_loop_->Run();
 
   EXPECT_EQ("Invalid seller beacon URL for 'click'", TakeBadMessage());
@@ -7281,7 +7294,9 @@ TEST_F(AuctionRunnerTest, BadSellerBeaconUrl) {
   EXPECT_EQ(InterestGroupKey(kBidder1, kBidder1Name), result_.winning_group_id);
   EXPECT_EQ(GURL("https://ad1.com/"), result_.ad_url);
   EXPECT_TRUE(result_.ad_component_urls.empty());
-  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre());
+  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre(
+                                       GURL("https://seller.report.test/"),
+                                       GURL("https://bidder.report.test/")));
   EXPECT_TRUE(result_.ad_beacon_map.metadata.empty());
   EXPECT_TRUE(result_.private_aggregation_requests.empty());
   EXPECT_EQ(6, result_.bidder1_bid_count);
@@ -7295,8 +7310,8 @@ TEST_F(AuctionRunnerTest, BadSellerBeaconUrl) {
 
 // Test cases where a bad report URL is received over Mojo from the winning
 // component seller worklet. Bad report URLs should be rejected in the Mojo
-// process, so this results in reporting a bad Mojo message - the reporting is
-// aborted, though the auction is considered a success.
+// process, so this results in reporting a bad Mojo message, though the
+// reporting phase is allowed to continue.
 TEST_F(AuctionRunnerTest, BadComponentSellerReportUrl) {
   this->SetUpComponentAuctionAndResponses(
       /*bidder1_seller=*/kComponentSeller1,
@@ -7360,7 +7375,8 @@ TEST_F(AuctionRunnerTest, BadComponentSellerReportUrl) {
 
   // Top-level seller worklet returns a valid HTTPS report URL.
   seller_worklet->WaitForReportResult();
-  seller_worklet->InvokeReportResultCallback(GURL("https://ok.test/"));
+  seller_worklet->InvokeReportResultCallback(
+      GURL("https://seller.report.test/"));
 
   mock_auction_process_manager_->WaitForWinningSellerReload();
   component_seller_worklet =
@@ -7370,8 +7386,12 @@ TEST_F(AuctionRunnerTest, BadComponentSellerReportUrl) {
   component_seller_worklet->WaitForReportResult();
   component_seller_worklet->InvokeReportResultCallback(GURL("Invalid URL"));
 
-  // Bidder1 never gets to report anything, since the seller providing a bad
-  // report URL aborts the auction.
+  // The winning bidder still gets a chance to provide a report URL.
+  mock_auction_process_manager_->WaitForWinningBidderReload();
+  bidder1_worklet =
+      mock_auction_process_manager_->TakeBidderWorklet(kBidder1Url);
+  bidder1_worklet->WaitForReportWin();
+  bidder1_worklet->InvokeReportWinCallback(GURL("https://bidder.report.test/"));
   auction_run_loop_->Run();
 
   EXPECT_EQ("Invalid seller report URL", TakeBadMessage());
@@ -7380,7 +7400,9 @@ TEST_F(AuctionRunnerTest, BadComponentSellerReportUrl) {
   EXPECT_EQ(InterestGroupKey(kBidder1, kBidder1Name), result_.winning_group_id);
   EXPECT_EQ(GURL("https://ad1.com/"), result_.ad_url);
   EXPECT_TRUE(result_.ad_component_urls.empty());
-  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre());
+  EXPECT_THAT(result_.report_urls, testing::UnorderedElementsAre(
+                                       GURL("https://seller.report.test/"),
+                                       GURL("https://bidder.report.test/")));
   EXPECT_TRUE(result_.ad_beacon_map.metadata.empty());
   EXPECT_TRUE(result_.private_aggregation_requests.empty());
   EXPECT_EQ(6, result_.bidder1_bid_count);
