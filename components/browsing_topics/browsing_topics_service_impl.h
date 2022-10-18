270e21748d8a124dafea076573e9e0ed883909da
Yao Xiao <yaoxia@chromium.org>
2022-10-18 18:17:56 +0000

    [Topics] Remove GetTopicsForSiteForDisplay()
    This method was intended for direct use, but later it turned out to be
    undesirable, but is still used to filter out random&padded topics after
    intercepting the API result. This is not a straightforward approach.
    This CL removes that method, and switch to directly notify
    PageSpecificContentSettings on true top topics inside
    BrowsingTopicsServiceImpl::GetBrowsingTopicsForJsApi.
    
    Also, fix an untight expectation in tests: PSCS::GetAccessedTopics()
    should always return sorted topics, instead of in arbitrary order.
    
    Bug: 1374334
    Change-Id: Iae9d8b7afd3aa43d91e0482427f8e0909f7188d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953735
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060552}

--- components/browsing_topics/browsing_topics_service_impl.h
+++ components/browsing_topics/browsing_topics_service_impl.h
@@ -54,9 +54,6 @@ class BrowsingTopicsServiceImpl
       bool calculate_now,
       mojom::PageHandler::GetBrowsingTopicsStateCallback callback) override;
 
-  std::vector<privacy_sandbox::CanonicalTopic> GetTopicsForSiteForDisplay(
-      const url::Origin& top_origin) const override;
-
   std::vector<privacy_sandbox::CanonicalTopic> GetTopTopicsForDisplay()
       const override;
 
--- components/browsing_topics/epoch_topics_unittest.cc
+++ components/browsing_topics/epoch_topics_unittest.cc
@@ -58,8 +58,15 @@ TEST_F(EpochTopicsTest, TopicForSite_InvalidIndividualTopics) {
 
   std::string top_site = "foo.com";
 
-  EXPECT_EQ(epoch_topics.TopicForSiteForDisplay(top_site, kTestKey),
-            absl::nullopt);
+  bool output_is_true_topic = false;
+  bool candidate_topic_filtered = false;
+
+  EXPECT_EQ(
+      epoch_topics.TopicForSite(top_site, HashedDomain(1), kTestKey,
+                                output_is_true_topic, candidate_topic_filtered),
+      absl::nullopt);
+  EXPECT_FALSE(output_is_true_topic);
+  EXPECT_FALSE(candidate_topic_filtered);
 }
 
 TEST_F(EpochTopicsTest, TopicForSite) {
@@ -119,9 +126,6 @@ TEST_F(EpochTopicsTest, TopicForSite) {
       EXPECT_FALSE(output_is_true_topic);
       EXPECT_TRUE(candidate_topic_filtered);
     }
-
-    EXPECT_EQ(epoch_topics.TopicForSiteForDisplay(top_site, kTestKey),
-              Topic(2));
   }
 
   {
@@ -173,10 +177,6 @@ TEST_F(EpochTopicsTest, TopicForSite) {
       EXPECT_FALSE(output_is_true_topic);
       EXPECT_FALSE(candidate_topic_filtered);
     }
-
-    // Topic(3) is a padded topic. Thus it's not returned.
-    EXPECT_EQ(epoch_topics.TopicForSiteForDisplay(top_site, kTestKey),
-              absl::nullopt);
   }
 
   {
@@ -216,9 +216,6 @@ TEST_F(EpochTopicsTest, TopicForSite) {
               Topic(186));
     EXPECT_FALSE(output_is_true_topic);
     EXPECT_FALSE(candidate_topic_filtered);
-
-    EXPECT_EQ(epoch_topics.TopicForSiteForDisplay(top_site, kTestKey),
-              absl::nullopt);
   }
 }
 
