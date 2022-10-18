f61a3f8ca11af4516deef4afcfa4e55c8324576b
Joanmarie Diggs <jdiggs@igalia.com>
2022-10-18 14:14:19 +0000

    Remove kSkipAccessibilityPaintChecks from Ash WindowCycleItemView
    A test was failing because WindowMiniView lacked an accessible name.
    WindowMiniView gets its accessible name from the window's title, but
    the TestWindowBuilder does not create windows with titles. Change the
    TestWindowBuilder so that the windows it creates have a title in
    order to re-enable the accessibility paint checks.
    
    Bug: 1348081, 1218186
    Change-Id: Ie2110d68d394ac8e3b43f9ccee31c615186b931e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957014
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060448}

--- ash/test/ash_test_base.cc
+++ ash/test/ash_test_base.cc
@@ -308,6 +308,7 @@ aura::Window* AshTestBase::CreateTestWindowInShellWithDelegateAndType(
       .SetDelegate(delegate)
       .SetWindowType(type)
       .SetWindowId(id)
+      .SetWindowTitle(u"Window " + base::NumberToString16(id))
       .AllowAllWindowStates()
       .Build()
       .release();
