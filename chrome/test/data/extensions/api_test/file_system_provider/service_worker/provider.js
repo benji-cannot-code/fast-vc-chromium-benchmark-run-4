e8bedf60830c76d6e6f5019c3e80eeedcda7a0e7
Luciano Pacheco <lucmult@chromium.org>
2022-10-18 11:20:12 +0000

    fileSystemProvider: port remove watcher test to service workers
    Bug: b:235414571
    Change-Id: I238fed5bfa7cc33f896bd3902b2e1f6c0cb77723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960199
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060401}

--- chrome/test/data/extensions/api_test/file_system_provider/service_worker/provider.js
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/provider.js
@@ -880,6 +880,12 @@ export class TestFileSystemProvider {
       return;
     }
 
+    const error = this.testConfig['onRemoveWatcherRequestedError'];
+    if (error) {
+      onError(error);
+      return;
+    }
+
     if (!this.findEntryByPath(options.entryPath)) {
       onSuccess();
       return;
