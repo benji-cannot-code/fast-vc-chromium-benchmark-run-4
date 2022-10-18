291e537fd994faec006db661f722136ad451ddd3
Phillis Tang <phillis@google.com>
2022-10-18 20:35:12 +0000

    DPWA: improve scroll button style
    Use the default ink drop effect of icon button for the scroll button,
    to address color contrast issue with drawing it on top of developer
    provided images, use a solid backgounrd. Also apply shadow on the
    button to further contrast.
    
    screenshot: https://screenshot.googleplex.com/AbPN9cpCLa3xzvn
    
    Bug: 1371168
    Change-Id: I7bf90ed0a11d676c6c36a404ae14a04daab81a5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3956203
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060658}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -247,7 +247,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorPwaToolbarBackground) \
   E_CPONLY(kColorPwaToolbarButtonIcon) \
   E_CPONLY(kColorPwaToolbarButtonIconDisabled) \
-  E_CPONLY(kColorPwaScrollButtonBackground) \
   /* QR code colors. */ \
   E_CPONLY(kColorQrCodeBackground) \
   E_CPONLY(kColorQrCodeBorder) \
