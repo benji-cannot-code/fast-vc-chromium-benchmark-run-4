a7d60c2acd214734bcf96aad145b766e3fce77d0
Zonghan Xu <xzonghan@chromium.org>
2022-10-18 18:52:41 +0000

    [New EV] Create Registry Signals Client
    Create the windows version of settings signals collector, which gather registry presence and values, and add the settings collector to reportingPrivate API.
    
    Bug: b:245525199, 1376090
    Change-Id: Ie496b6570dc8f2d2341ad815046bc29a404e48f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3926256
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060600}

--- components/device_signals/core/browser/mac/plist_settings_client_unittest.mm
+++ components/device_signals/core/browser/mac/plist_settings_client_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/files/file_path.h"
+#include "base/strings/stringprintf.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "components/device_signals/test/test_constants.h"
@@ -33,7 +34,7 @@ class PlistSettingsClientTest : public testing::Test {
                                  const std::string& setting_value) {
     SettingsItem item;
     if (!setting_value.empty())
-      item.setting_value = base::Value(setting_value);
+      item.setting_json_value = "\"" + setting_value + "\"";
     return FinishSettingItemSetup(item, key_path, value);
   }
 
@@ -41,7 +42,7 @@ class PlistSettingsClientTest : public testing::Test {
                                  PresenceValue value,
                                  const int setting_value) {
     SettingsItem item;
-    item.setting_value = base::Value(setting_value);
+    item.setting_json_value = base::StringPrintf("%d", setting_value);
     return FinishSettingItemSetup(item, key_path, value);
   }
 
@@ -49,7 +50,7 @@ class PlistSettingsClientTest : public testing::Test {
                                  PresenceValue value,
                                  const double setting_value) {
     SettingsItem item;
-    item.setting_value = base::Value(setting_value);
+    item.setting_json_value = base::StringPrintf("%f", setting_value);
     return FinishSettingItemSetup(item, key_path, value);
   }
 
