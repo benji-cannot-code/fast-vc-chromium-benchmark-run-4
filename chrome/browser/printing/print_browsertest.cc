c028d6ee6415be59da2142535a8e18c4fbc4e804
Lei Zhang <thestig@chromium.org>
2022-10-18 17:51:09 +0000

    Fix build when enable_oop_printing=false.
    https://crrev.com/1050907 changed print_dialog_gtk.cc to start checking
    `kEnableOopPrintDriversJobPrint`, assuming it is unconditionally
    defined.
    
    Similarly, various CLs like https://crrev.com/992136 changed
    print_browsertest.cc without the proper conditionals.
    
    Change-Id: Ie7efe976bba4b7583be104fad37984bea07f8773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962532
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060540}

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -2734,6 +2734,7 @@ class SystemAccessProcessPrintBrowserTestBase : public PrintBrowserTest,
 
   void PrimeAsRepeatingErrorGenerator() { reset_errors_after_check_ = false; }
 
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
   void PrimeForSpoolingSharedMemoryErrors() {
     simulate_spooling_memory_errors_ = true;
   }
@@ -2785,6 +2786,7 @@ class SystemAccessProcessPrintBrowserTestBase : public PrintBrowserTest,
   bool print_backend_service_use_detected() const {
     return print_backend_service_use_detected_;
   }
+#endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
 
   mojom::ResultCode use_default_settings_result() const {
     return use_default_settings_result_;
@@ -2839,7 +2841,6 @@ class SystemAccessProcessPrintBrowserTestBase : public PrintBrowserTest,
     return std::make_unique<TestPrintJobWorker>(
         rfh_id, &test_print_job_worker_callbacks_);
   }
-#endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
 
   void OnUseDefaultSettings() {
     did_use_default_settings_ = true;
@@ -2862,6 +2863,7 @@ class SystemAccessProcessPrintBrowserTestBase : public PrintBrowserTest,
       print_backend_service_use_detected_ = true;
     }
   }
+#endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
 
   void ErrorCheck(mojom::ResultCode result) {
     // Interested to reset any trigger for causing access-denied errors, so
