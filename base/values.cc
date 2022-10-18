b056d331f8fe949360247c3ff172569b0ae0c89a
Lei Zhang <thestig@chromium.org>
2022-10-18 23:55:00 +0000

    Delete base::(Dictionary)Value::CreateDeepCopy().
    These 2 CreateDeepCopy() methods have no callers left, so delete them.
    
    Bug: 1187059
    Change-Id: Icbf44e59f7084d99fe245012cc4ce37e422e71af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964870
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060750}

--- base/values.cc
+++ base/values.cc
@@ -1394,10 +1394,6 @@ bool Value::GetAsDictionary(const DictionaryValue** out_value) const {
   return is_dict();
 }
 
-std::unique_ptr<Value> Value::CreateDeepCopy() const {
-  return std::make_unique<Value>(Clone());
-}
-
 bool operator==(const Value& lhs, const Value& rhs) {
   return lhs.data_ == rhs.data_;
 }
@@ -1780,10 +1776,6 @@ bool DictionaryValue::GetList(StringPiece path, ListValue** out_value) {
 void DictionaryValue::Swap(DictionaryValue* other) {
   CHECK(other->is_dict());
   dict().swap(other->dict());
-}
-
-std::unique_ptr<DictionaryValue> DictionaryValue::CreateDeepCopy() const {
-  return std::make_unique<DictionaryValue>(dict());
 }
 
 ///////////////////// ListValue ////////////////////
