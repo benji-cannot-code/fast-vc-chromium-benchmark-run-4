8a8e8f9493b33fce4e9a11b90f8076ab3a71d60c
Alexander Alekseev <alemate@chromium.org>
2022-10-18 23:08:42 +0000

    Reland "[chromeos] Implement Ash.LoginSessionRestore.* histograms."
    This is a reland of commit bda85d6f898b533eacc59fc627d7b5cf58941fb0
    
    Original commit was reverted in https://crrev.com/c/3956639 because
    of TotalAnimationThroughputReporterTest.SingleAnimation failures.
    This Cl replaces timed waiting by observer in test.
    
    Original change's description:
    > [chromeos] Implement Ash.LoginSessionRestore.* histograms.
    >
    > See go/ash-lacros-login-performance for details.
    >
    > BUG=b:237400719,b:218559327
    > Low-Coverage-Reason:chrome/browser/ash/app_restore/full_restore_app_launch_handler.cc does not have a unit test and I only added a couple of lines there.
    >
    > Change-Id: I32b231224bd4bf270f284752f1b89e682600e46f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778122
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1059058}
    
    Bug: b:237400719,b:218559327
    Change-Id: I866335739c00c7729471b8ebb9196f4509c79fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957425
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060728}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_constants.h"
 #include "ash/display/screen_orientation_controller.h"
 #include "ash/frame/non_client_frame_view_ash.h"
+#include "ash/metrics/login_unlock_throughput_recorder.h"
 #include "ash/public/cpp/ash_constants.h"
 #include "ash/public/cpp/rounded_corner_utils.h"
 #include "ash/public/cpp/shelf_types.h"
@@ -659,6 +660,9 @@ void ShellSurfaceBase::SetRestoreInfo(int32_t restore_session_id,
   DCHECK(!widget_);
   restore_session_id_.emplace(restore_session_id);
   restore_window_id_.emplace(restore_window_id);
+  ash::LoginUnlockThroughputRecorder* throughput_recorder =
+      ash::Shell::Get()->login_unlock_throughput_recorder();
+  throughput_recorder->OnRestoredWindowCreated(restore_window_id);
 }
 
 void ShellSurfaceBase::SetRestoreInfoWithWindowIdSource(
@@ -1813,6 +1817,18 @@ void ShellSurfaceBase::CommitWidget() {
     if (needs_layout_on_show_) {
       widget_->CenterWindow(GetWidgetBoundsFromVisibleBounds().size());
       needs_layout_on_show_ = false;
+    }
+
+    if (restore_window_id_.has_value()) {
+      ash::LoginUnlockThroughputRecorder* throughput_recorder =
+          ash::Shell::Get()->login_unlock_throughput_recorder();
+
+      aura::Window* root_window = host_window()->GetRootWindow();
+      if (root_window) {
+        ui::Compositor* compositor = root_window->layer()->GetCompositor();
+        throughput_recorder->OnBeforeRestoredWindowShown(
+            restore_window_id_.value(), compositor);
+      }
     }
 
     if (initially_activated_) {
