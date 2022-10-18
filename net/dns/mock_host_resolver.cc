b4d76afb62642e9ca90a2a2940a02daabbfe6807
Hans-Filip Elo <helo@vewd.com>
2022-10-18 08:09:28 +0000

    libstdc++: Fix template deduction error for std::set
    This patch fixes a template deduction error for std::set by explicitly
    naming template parameter for the key when using libstdc++.
    
    Bug: 957519
    Change-Id: I219a4a6f9b29eb4fb1d3d764490733cc050e4886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961009
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Hans-Filip Elo <helo@vewd.com>
    Cr-Commit-Position: refs/heads/main@{#1060345}

--- net/dns/mock_host_resolver.cc
+++ net/dns/mock_host_resolver.cc
@@ -642,8 +642,9 @@ void MockHostResolverBase::RuleResolver::AddIPLiteralRuleWithDnsAliases(
   endpoints.emplace_back();
   CHECK_EQ(ParseAddressList(ip_literal, &endpoints[0].ip_endpoints), OK);
   AddRule(hostname_pattern,
-          RuleResult(std::move(endpoints),
-                     std::set(dns_aliases.begin(), dns_aliases.end())));
+          RuleResult(
+              std::move(endpoints),
+              std::set<std::string>(dns_aliases.begin(), dns_aliases.end())));
 }
 
 void MockHostResolverBase::RuleResolver::AddIPLiteralRuleWithDnsAliases(
@@ -675,9 +676,9 @@ void MockHostResolverBase::RuleResolver::AddRuleWithFlags(
   std::vector<HostResolverEndpointResult> endpoints;
   endpoints.emplace_back();
   CHECK_EQ(ParseAddressList(ip_literal, &endpoints[0].ip_endpoints), OK);
-  AddRule(host_pattern,
-          RuleResult(std::move(endpoints),
-                     std::set(dns_aliases.begin(), dns_aliases.end())));
+  AddRule(host_pattern, RuleResult(std::move(endpoints),
+                                   std::set<std::string>(dns_aliases.begin(),
+                                                         dns_aliases.end())));
 }
 
 MockHostResolverBase::State::State() = default;
