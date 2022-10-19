e9995b5f6300db780c77a7168c0ba357a7e6d8af
Jongmok Kim <jongmok.kim@navercorp.com>
2022-10-19 09:17:59 +0000

    Remove RefCountedString::TakeString from chrome/*, chromos/*, content/*
    Bug: 1374216
    Change-Id: I96ae14e445c8c665f1bc5ea25bf37230d5cd3458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962237
    Auto-Submit: jongmok kim <jongmok.kim@navercorp.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060915}

--- chrome/common/net/net_resource_provider.cc
+++ chrome/common/net/net_resource_provider.cc
@@ -43,7 +43,7 @@ struct LazyDirectoryListerCacher {
             IDR_DIR_HEADER_HTML),
         value);
 
-    html_data = base::RefCountedString::TakeString(&str);
+    html_data = base::MakeRefCounted<base::RefCountedString>(std::move(str));
   }
 
   scoped_refptr<base::RefCountedMemory> html_data;
