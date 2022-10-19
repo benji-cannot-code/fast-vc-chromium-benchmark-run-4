ae54a7b77b1480ccc0189b444ad6e22d98f47c9e
Ana Salazar <anasalazar@chromium.org>
2022-10-19 20:39:54 +0000

    Remove parameters from PopulatedAppListTest
    The test suite was previously designed to test on Legacy and
    Productivity launcher.
    
    We stopped supporting legacy launcher on M104, so remove the parameter
    used in these tests to toggle the feature kProductivityLauncher.
    
    TappingAppsGridClosesVirtualKeyboard test was modified to work with the
    fullscreen launcher. The fix includes using EventGenerator.
    
    Bug: 1360501
    Change-Id: I606695fe9c93af2ae5ce8dea179a606b3c673570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964677
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061221}

--- ash/app_list/views/folder_header_view.h
+++ ash/app_list/views/folder_header_view.h
@@ -50,7 +50,7 @@ class ASH_EXPORT FolderHeaderView : public views::View,
  private:
   class FolderNameView;
   friend class FolderHeaderViewTest;
-  friend class PopulatedAppListTestBase;
+  friend class PopulatedAppListTest;
 
   // Updates UI.
   void Update();
