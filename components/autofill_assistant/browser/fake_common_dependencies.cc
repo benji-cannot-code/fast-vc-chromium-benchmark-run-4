3bc2181f70cc3760efea25d274889ce2e7f8fe4f
Stephan Hartmann <stha09@googlemail.com>
2022-10-19 15:42:38 +0000

    libstdc++: fix incomplete type of autofill_assistant::AssistantFieldTrialUtil
    Destructor of std::unique_ptr in libstdc++ uses sizeof() which
    requires full definition of autofill_assistant::AssistantFieldTrialUtil
    for return type of FakeCommonDependencies::CreateFieldTrialUtil().
    
    Bug: 957519
    Change-Id: I63fdc2da2c3dda9d34a384594ed63f68d02b54a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963053
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1061049}

--- components/autofill_assistant/browser/fake_common_dependencies.cc
+++ components/autofill_assistant/browser/fake_common_dependencies.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/autofill_assistant/browser/fake_common_dependencies.h"
+#include "components/autofill_assistant/browser/assistant_field_trial_util.h"
 
 namespace autofill_assistant {
 
