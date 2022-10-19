ed2a50f254100a03955a7d9c225ff51e9cbc0634
Hubert Chao <hchao@chromium.org>
2022-10-19 18:18:52 +0000

    Add chrome://flags entry for ChromeRootStoreEnabled
    Folloed instructions at go/finch-feature-api#chrome-feature-flag-in-chromeflags
    
    Change-Id: I3aa5c7b3c2e815b39d508f6964db9aa6435730a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965139
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061144}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -81,6 +81,9 @@ extern const char kBrokerFileOperationsOnDiskCacheInNetworkServiceDescription[];
 
 extern const char kCOLRV1FontsDescription[];
 
+extern const char kChromeRootStoreEnabledName[];
+extern const char kChromeRootStoreEnabledDescription[];
+
 extern const char kClipboardUnsanitizedContentName[];
 extern const char kClipboardUnsanitizedContentDescription[];
 
