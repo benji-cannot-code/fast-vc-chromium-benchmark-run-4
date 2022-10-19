bbb6148fb5b28747d05a833ece62beb461d04a99
Alex Ilin <alexilin@chromium.org>
2022-10-19 14:39:35 +0000

    [signin] Remove MergeSession call from InlineSigninHelper
    AccountsCookieMutator::AddAccountToCookie() uses a MergeSession call
    which is now deprecated.
    
    This CL removes the `AddAccountToCookie()` call from InlineSigninHelper
    since it was added purely as a performance improvement that didn't
    really help. See the comment thread [1] for context.
    
    It's OK to just remove this call because the account reconcilor will
    add an account to cookies automatically, it will just take more time.
    
    As described in a TODO comment [2], the best user experience can be
    achieved by waiting until cookies are set before opening a browser
    window.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2879027/comment/7eedf2d3_af3f1f5c/
    [2] https://crsrc.org/c/chrome/browser/ui/webui/signin/inline_login_handler_impl.cc;drc=1f98247a185695455858064fd00c5da26d375db5;l=331
    
    Bug: 1252805
    Change-Id: I843824321ae60083d5ac4426ff8b4a8c3974e0cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963043
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061018}

--- chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_impl.cc
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/identity_manager/account_info.h"
-#include "components/signin/public/identity_manager/accounts_cookie_mutator.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
@@ -400,10 +399,6 @@ void InlineSigninHelper::OnClientOAuthSuccessAndBrowserOpened(
         result.is_under_advanced_protection,
         signin_metrics::SourceForRefreshTokenOperation::
             kInlineLoginHandler_Signin);
-
-    identity_manager->GetAccountsCookieMutator()->AddAccountToCookie(
-        identity_manager->GetPrimaryAccountId(signin::ConsentLevel::kSignin),
-        gaia::GaiaSource::kPrimaryAccountManager, {});
 
     signin_metrics::LogSigninReason(signin_metrics::Reason::kReauthentication);
   } else {
