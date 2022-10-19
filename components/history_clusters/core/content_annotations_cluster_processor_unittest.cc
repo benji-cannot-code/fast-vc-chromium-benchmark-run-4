8c44e8792ed3a4c2d812d4c2794a48a9238a31f7
Sophie Chang <sophiechang@chromium.org>
2022-10-19 23:38:53 +0000

    Remove ability to do so many copies in clusterprocessor
    Bug: b/244505276
    Change-Id: If3d489764c1fae257015acfd71d69897b2bbe276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965679
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061322}

--- components/history_clusters/core/content_annotations_cluster_processor_unittest.cc
+++ components/history_clusters/core/content_annotations_cluster_processor_unittest.cc
@@ -56,9 +56,8 @@ class ContentAnnotationsClusterProcessorTest : public ::testing::Test {
 
   void TearDown() override { cluster_processor_.reset(); }
 
-  std::vector<history::Cluster> ProcessClusters(
-      const std::vector<history::Cluster>& clusters) {
-    return cluster_processor_->ProcessClusters(clusters);
+  void ProcessClusters(std::vector<history::Cluster>* clusters) {
+    cluster_processor_->ProcessClusters(clusters);
   }
 
  private:
@@ -98,13 +97,13 @@ TEST_F(ContentAnnotationsClusterProcessorTest, AboveThreshold) {
   cluster2.visits = {testing::CreateClusterVisit(visit5)};
   clusters.push_back(cluster2);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
+  ProcessClusters(&clusters);
   EXPECT_THAT(
-      testing::ToVisitResults(result_clusters),
+      testing::ToVisitResults(clusters),
       ElementsAre(ElementsAre(
           testing::VisitResult(1, 1.0), testing::VisitResult(2, 1.0),
           testing::VisitResult(4, 1.0), testing::VisitResult(10, 1.0))));
-  ASSERT_EQ(result_clusters.size(), 1u);
+  ASSERT_EQ(clusters.size(), 1u);
 }
 
 TEST_F(ContentAnnotationsClusterProcessorTest, BelowThreshold) {
@@ -148,8 +147,8 @@ TEST_F(ContentAnnotationsClusterProcessorTest, BelowThreshold) {
   cluster5.visits = {testing::CreateClusterVisit(visit7)};
   clusters.push_back(cluster5);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
-  EXPECT_THAT(testing::ToVisitResults(result_clusters),
+  ProcessClusters(&clusters);
+  EXPECT_THAT(testing::ToVisitResults(clusters),
               ElementsAre(ElementsAre(testing::VisitResult(1, 1.0),
                                       testing::VisitResult(2, 1.0),
                                       testing::VisitResult(4, 1.0)),
@@ -205,13 +204,12 @@ TEST_F(ContentAnnotationsClusterProcessorCosineSimilarityTest, AboveThreshold) {
   cluster2.visits = {testing::CreateClusterVisit(visit5)};
   clusters.push_back(cluster2);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
+  ProcessClusters(&clusters);
   EXPECT_THAT(
-      testing::ToVisitResults(result_clusters),
+      testing::ToVisitResults(clusters),
       ElementsAre(ElementsAre(
           testing::VisitResult(1, 1.0), testing::VisitResult(2, 1.0),
           testing::VisitResult(4, 1.0), testing::VisitResult(10, 1.0))));
-  ASSERT_EQ(result_clusters.size(), 1u);
 }
 
 TEST_F(ContentAnnotationsClusterProcessorCosineSimilarityTest, BelowThreshold) {
@@ -255,8 +253,8 @@ TEST_F(ContentAnnotationsClusterProcessorCosineSimilarityTest, BelowThreshold) {
   cluster5.visits = {testing::CreateClusterVisit(visit7)};
   clusters.push_back(cluster5);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
-  EXPECT_THAT(testing::ToVisitResults(result_clusters),
+  ProcessClusters(&clusters);
+  EXPECT_THAT(testing::ToVisitResults(clusters),
               ElementsAre(ElementsAre(testing::VisitResult(1, 1.0),
                                       testing::VisitResult(2, 1.0),
                                       testing::VisitResult(4, 1.0)),
@@ -316,13 +314,12 @@ TEST_F(ContentAnnotationsClusterProcessorIntersectionMetricTest,
                      testing::CreateClusterVisit(visit6)};
   clusters.push_back(cluster2);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
-  EXPECT_THAT(testing::ToVisitResults(result_clusters),
+  ProcessClusters(&clusters);
+  EXPECT_THAT(testing::ToVisitResults(clusters),
               ElementsAre(ElementsAre(
                   testing::VisitResult(1, 1.0), testing::VisitResult(2, 1.0),
                   testing::VisitResult(4, 1.0), testing::VisitResult(10, 1.0),
                   testing::VisitResult(11, 1.0))));
-  ASSERT_EQ(result_clusters.size(), 1u);
 }
 
 TEST_F(ContentAnnotationsClusterProcessorIntersectionMetricTest,
@@ -357,8 +354,8 @@ TEST_F(ContentAnnotationsClusterProcessorIntersectionMetricTest,
   cluster2.visits = {testing::CreateClusterVisit(visit5)};
   clusters.push_back(cluster2);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
-  ASSERT_EQ(result_clusters.size(), 2u);
+  ProcessClusters(&clusters);
+  ASSERT_EQ(clusters.size(), 2u);
 }
 
 class ContentAnnotationsSimilarityVariousCollectionsFilteringTest
@@ -408,8 +405,8 @@ TEST_F(ContentAnnotationsSimilarityVariousCollectionsFilteringTest,
   cluster2.visits = {testing::CreateClusterVisit(visit5)};
   clusters.push_back(cluster2);
 
-  std::vector<history::Cluster> result_clusters = ProcessClusters(clusters);
-  EXPECT_THAT(testing::ToVisitResults(result_clusters),
+  ProcessClusters(&clusters);
+  EXPECT_THAT(testing::ToVisitResults(clusters),
               ElementsAre(ElementsAre(testing::VisitResult(1, 1.0),
                                       testing::VisitResult(2, 1.0),
                                       testing::VisitResult(4, 1.0)),
