1582b97e515a78324b274c685268115ed0163f30
Koji Ishii <kojii@chromium.org>
2022-10-19 09:19:05 +0000

    [anchor-position] Rename to |NGLogicalAnchorQueryMap|
    This patch renames |NGLogicalAnchorQueryForFragmentation| to
    |NGLogicalAnchorQueryMap|, as it turned out that the same
    logic is also useful for the inline containing block case.
    
    This patch has no behavior changes.
    
    Bug: 1309178
    Change-Id: I84a74f0b75991a9f33104e17d0345d1bf8e4f9f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966009
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060917}

--- third_party/blink/renderer/core/layout/ng/ng_anchor_query.cc
+++ third_party/blink/renderer/core/layout/ng/ng_anchor_query.cc
@@ -504,8 +504,7 @@ void NGLogicalAnchorQuery::SetFromPhysical(
   }
 }
 
-const NGLogicalAnchorQuery&
-NGLogicalAnchorQueryForFragmentation::StitchedAnchorQuery(
+const NGLogicalAnchorQuery& NGLogicalAnchorQueryMap::StitchedAnchorQuery(
     const LayoutObject& containing_block) const {
   DCHECK(&containing_block);
   DCHECK(containing_block.CanContainAbsolutePositionObjects() ||
@@ -516,7 +515,7 @@ NGLogicalAnchorQueryForFragmentation::StitchedAnchorQuery(
   return NGLogicalAnchorQuery::Empty();
 }
 
-void NGLogicalAnchorQueryForFragmentation::Update(
+void NGLogicalAnchorQueryMap::Update(
     const base::span<const NGLogicalLink>& children,
     const base::span<const NGLogicalOOFNodeForFragmentation>& oof_nodes,
     const LayoutBox& root,
