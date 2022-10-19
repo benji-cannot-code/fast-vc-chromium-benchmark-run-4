ae0030e6c92953f84ebfc30572a95ded401c7272
Steven Holte <holte@chromium.org>
2022-10-19 00:30:09 +0000

    Validate slot range bounds.
    Avoids issues with unreasonable slots_in_member calculation.
    
    Bug: 1375935
    Change-Id: I00b5c1b00a47120fed9b87ad59073e4d88c2462a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960927
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060770}

--- components/variations/variations_layers.cc
+++ components/variations/variations_layers.cc
@@ -24,7 +24,8 @@ enum class InvalidLayerReason {
   kNoMembers = 2,
   kInvalidEntropyMode = 3,
   kSlotsDoNotDivideLowEntropyDomain = 4,
-  kMaxValue = kSlotsDoNotDivideLowEntropyDomain,
+  kInvalidSlotBounds = 5,
+  kMaxValue = kInvalidSlotBounds,
 };
 
 void LogInvalidLayerReason(InvalidLayerReason reason) {
@@ -140,6 +141,21 @@ NormalizedMurmurHashEntropyProvider ComputeRemainderEntropy(
   return NormalizedMurmurHashEntropyProvider(remainder.value, remainder.range);
 }
 
+bool ValidSlotBounds(const Layer& layer_proto) {
+  for (const auto& member : layer_proto.members()) {
+    for (const auto& range : member.slots()) {
+      // start and end are both unsigned (uint32_t) so no need to check that
+      // they are non-negative.
+      if (range.end() >= layer_proto.num_slots())
+        return false;
+      if (range.start() > range.end()) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 }  // namespace
 
 VariationsLayers::VariationsLayers(const VariationsSeed& seed,
@@ -173,6 +189,11 @@ void VariationsLayers::ConstructLayer(const EntropyProviders& entropy_providers,
   if (layer_proto.entropy_mode() != Layer::LOW &&
       layer_proto.entropy_mode() != Layer::DEFAULT) {
     LogInvalidLayerReason(InvalidLayerReason::kInvalidEntropyMode);
+    return;
+  }
+
+  if (!ValidSlotBounds(layer_proto)) {
+    LogInvalidLayerReason(InvalidLayerReason::kInvalidSlotBounds);
     return;
   }
 
