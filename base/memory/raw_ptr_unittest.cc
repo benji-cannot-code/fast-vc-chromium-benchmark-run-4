9e05f81ecff27d3cee6f9bd39e6790f4d709957f
James Lee <ljjlee@google.com>
2022-10-19 09:00:11 +0000

    Revert "Make use_mte_checked_ptr no longer a gn arg"
    This reverts commit 94d2dfa6436d939c4abbe70d1da969160a9ce024.
    
    Reason for revert: crrev.com/c/3960115 need to be reverted as it
    breaks gn gen, but won't revert cleanly without first reverting
    this CL.
    
    Example breakage:
    https://ci.chromium.org/ui/p/chrome/builders/ci/clang-release-builder/27528/overview
    
    Original change's description:
    > Make use_mte_checked_ptr no longer a gn arg
    >
    > It intended to duplicate the functionality of the enable_mte_checked_ptr_support gn arg, and may have been confusing to users as two why set both.
    >
    > As a matter of fact, use_mte_checked_ptr was accidentally unused, and
    > this CL fixes that.
    >
    > Change-Id: I87772cc54d620fe409957b9a6beec6d5ce711a57
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935984
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    > Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1060856}
    
    Change-Id: If1a2ce78ad7922792d9058e1b0e8d9a4d33a1316
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963326
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: James Lee <ljjlee@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1060911}

--- base/memory/raw_ptr_unittest.cc
+++ base/memory/raw_ptr_unittest.cc
@@ -39,9 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/include/perfetto/test/traced_value_test_support.h"  // no-presubmit-check nogncheck
 #endif  // BUILDFLAG(ENABLE_BASE_TRACING) && BUILDFLAG(PA_USE_BASE_TRACING)
 
-#if defined(RAW_PTR_USE_MTE_CHECKED_PTR)
+#if defined(PA_ENABLE_MTE_CHECKED_PTR_SUPPORT_WITH_64_BITS_POINTERS)
 #include "base/allocator/partition_allocator/partition_tag_types.h"
-#endif
+#endif  // defined(PA_ENABLE_MTE_CHECKED_PTR_SUPPORT_WITH_64_BITS_POINTERS)
 
 #if BUILDFLAG(USE_ASAN_BACKUP_REF_PTR)
 #include <sanitizer/asan_interface.h>
@@ -2208,7 +2208,7 @@ TEST_F(AsanBackupRefPtrTest, BoundReferences) {
 
 #endif  // BUILDFLAG(USE_ASAN_BACKUP_REF_PTR)
 
-#if defined(RAW_PTR_USE_MTE_CHECKED_PTR)
+#if defined(PA_ENABLE_MTE_CHECKED_PTR_SUPPORT_WITH_64_BITS_POINTERS)
 
 static constexpr size_t kTagOffsetForTest = 2;
 
@@ -2386,7 +2386,7 @@ TEST(MTECheckedPtrImpl, AdvancedPointerShiftedAppropriately) {
 #endif  // !defined(MEMORY_TOOL_REPLACES_ALLOCATOR) &&
         // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
-#endif  // defined(RAW_PTR_USE_MTE_CHECKED_PTR)
+#endif  // defined(PA_ENABLE_MTE_CHECKED_PTR_SUPPORT_WITH_64_BITS_POINTERS)
 
 }  // namespace internal
 }  // namespace base
