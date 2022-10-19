62136b934bc7d97bbe224dad22cbd27024309876
Xianzhu Wang <wangxianzhu@chromium.org>
2022-10-19 01:52:38 +0000

    Clean up and optimize gfx::Matrix44
    - Use Chromium C++ style and remove dependency to Skia.
    - Change the 16-parameter constructor to col-major order.
    - Use Double4 SIMD optimizations.
    - Remove branches when isIdentity(), isTranslate() etc. is true,
      because they are rarely true after crrev.com/1059685, and the cost
      of the branches is no longer worth it.
    - Remove the type mask because it would have little value with the
      above change, and recomputeTypeMask() after each mutation had
      non-trivial cost [1].
    - The is*() functions are kept just for gfx::Transform API. Now they
      check matrix components.
    - Remove unused IsScale().
    
    Some of the code comes from blink::TransformationMatrix (e.g.,
    GetInverse) and there's an eventual goal to merge them.
    
    [1] https://uma.googleplex.com/p/chrome/callstacks?sid=32e1e72082c77473db82e946349ff7c1
    
    Bug: 1359528
    Change-Id: I4389ea323481463f7e5d4dc5be83262030e1f436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3958365
    Reviewed-by: David Baron <dbaron@chromium.org>
    Owners-Override: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060809}

--- ui/gfx/geometry/transform_util.h
+++ ui/gfx/geometry/transform_util.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GFX_GEOMETRY_TRANSFORM_UTIL_H_
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
+// TODO(crbug.com/1359528): Remove dependency to Skia.
+#include "third_party/skia/include/core/SkScalar.h"
 #include "ui/gfx/geometry/axis_transform2d.h"
 #include "ui/gfx/geometry/geometry_skia_export.h"
 #include "ui/gfx/geometry/point.h"
