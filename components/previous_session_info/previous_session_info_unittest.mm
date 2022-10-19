082199cbb16d9b3a384359fbfe42e02f81fbaa9b
Justin Cohen <justincohen@chromium.org>
2022-10-19 18:04:10 +0000

    Revert "ios: Speculative fix for PreviousSessionInfo report params crash."
    This reverts commit 2aa1b6a61d6f22ef3add3da3295daa0d0b2b62d1.
    
    Reason for revert: Needs more synchronization crbug.com/1376562
    
    Original change's description:
    > ios: Speculative fix for PreviousSessionInfo report params crash.
    >
    > This has a similar root cause to crrev.com/c/3341393, attaching the
    > same description here:
    >
    > There appears to be a crash related to writing to NSUserDefaults off
    > the main thread at the same time a WebProcess is terminated.  Since
    > Chromium does a lot of NSUserDefaults on foreground and
    > backgrounding, and WebProcess terminatation also tends to happen
    > during foreground/backgrounding, the likelihood that these two things
    > happen at the same time is high.
    >
    > Bug: 1360033
    > Change-Id: I8c5907da244843145aed511e4385ff0d0c6a2527
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3959958
    > Commit-Queue: Justin Cohen <justincohen@chromium.org>
    > Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Auto-Submit: Justin Cohen <justincohen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1060607}
    
    Bug: 1360033
    Change-Id: Ifeefe8ad590822309356bbd5ac6d9a2799501c95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965172
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061128}

--- components/previous_session_info/previous_session_info_unittest.mm
+++ components/previous_session_info/previous_session_info_unittest.mm
@@ -517,6 +517,8 @@ TEST_F(PreviousSessionInfoTest, ReportParameters) {
       dictionaryForKey:previous_session_info_constants::
                            kPreviousSessionInfoParams];
   EXPECT_NSEQ(@{kKey0 : @"https://example2.test/"}, URLs);
+  [PreviousSessionInfo resetSharedInstanceForTesting];
+  EXPECT_NSEQ(URLs, [[PreviousSessionInfo sharedInstance] reportParameters]);
 
   // Add second URL.
   NSString* const kKey1 = @"url1";
@@ -531,9 +533,13 @@ TEST_F(PreviousSessionInfoTest, ReportParameters) {
     kKey1 : @"https://example3.test/",
   };
   EXPECT_NSEQ(expected, URLs);
+  [PreviousSessionInfo resetSharedInstanceForTesting];
+  EXPECT_NSEQ(URLs, [[PreviousSessionInfo sharedInstance] reportParameters]);
 
   // Removing non-existing key does not crash.
   [[PreviousSessionInfo sharedInstance] removeReportParameterForKey:@"url2"];
+  [PreviousSessionInfo resetSharedInstanceForTesting];
+  EXPECT_NSEQ(URLs, [[PreviousSessionInfo sharedInstance] reportParameters]);
 
   // Remove first URL.
   [[PreviousSessionInfo sharedInstance] removeReportParameterForKey:kKey0];
@@ -541,6 +547,8 @@ TEST_F(PreviousSessionInfoTest, ReportParameters) {
       dictionaryForKey:previous_session_info_constants::
                            kPreviousSessionInfoParams];
   EXPECT_NSEQ(@{kKey1 : @"https://example3.test/"}, URLs);
+  [PreviousSessionInfo resetSharedInstanceForTesting];
+  EXPECT_NSEQ(URLs, [[PreviousSessionInfo sharedInstance] reportParameters]);
 
   // Remove second URL.
   [[PreviousSessionInfo sharedInstance] removeReportParameterForKey:kKey1];
