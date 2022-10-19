616514483241a0bceb4e461004d2acc822c54eec
Joseph Pang <pjlee@google.com>
2022-10-19 22:32:50 +0000

    arc/gio: Add menu reposition
    Add the functionality of moving the menu with a mouse or touch.
    
    Bug: b:250900717
    Test: manual test
    Test: out/Default/unit_tests --gtest_filter="MenuEntryViewTest.*"
    
    Change-Id: I6b033e6d329944d370449cdccf959017eba316a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3943127
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061283}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/background.h"
+#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/widget/widget.h"
@@ -192,7 +193,7 @@ void DisplayOverlayController::AddMenuEntryView(views::Widget* overlay_widget) {
       vector_icons::kVideogameAssetOutlineIcon, SK_ColorBLACK);
 
   // Create and position entry point for |InputMenuView|.
-  auto menu_entry = std::make_unique<views::ImageButton>(base::BindRepeating(
+  auto menu_entry = std::make_unique<MenuEntryView>(base::BindRepeating(
       &DisplayOverlayController::OnMenuEntryPressed, base::Unretained(this)));
   menu_entry->SetImage(views::Button::STATE_NORMAL, game_icon);
   menu_entry->SetBackground(views::CreateRoundedRectBackground(
