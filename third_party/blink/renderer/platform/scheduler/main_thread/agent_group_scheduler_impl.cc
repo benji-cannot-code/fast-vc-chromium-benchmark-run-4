637d3415874e5ea194c9ad53a2cdc1d268bbe1de
Dave Tapuska <dtapuska@chromium.org>
2022-10-19 23:21:50 +0000

    Use inline vector for performing microtask checkpoint.
    We do not wish to allocate any memory while processing a microtask
    so using an inline Vector allows us the speed and correctness.
    
    BUG=1376394
    
    Change-Id: Ib1e272ebd27895d76291fa412f6b512e95cc016d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966131
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061308}

--- third_party/blink/renderer/platform/scheduler/main_thread/agent_group_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/agent_group_scheduler_impl.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/platform/task_type.h"
-#include "third_party/blink/renderer/platform/heap/collection_support/clear_collection_scope.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/public/dummy_schedulers.h"
 
@@ -125,8 +125,12 @@ void AgentGroupSchedulerImpl::AddAgent(Agent* agent) {
 }
 
 void AgentGroupSchedulerImpl::PerformMicrotaskCheckpoint() {
-  HeapHashSet<WeakMember<Agent>> agents(*agents_);
-  ClearCollectionScope<HeapHashSet<WeakMember<Agent>>> clear_scope(&agents);
+  // This code is performance sensitive so we do not wish to allocate
+  // memory, use an inline vector of 10.
+  HeapVector<Member<Agent>, 10> agents;
+  for (Agent* agent : *agents_) {
+    agents.push_back(agent);
+  }
   for (Agent* agent : agents) {
     DCHECK(agents_->Contains(agent));
     agent->PerformMicrotaskCheckpoint();
