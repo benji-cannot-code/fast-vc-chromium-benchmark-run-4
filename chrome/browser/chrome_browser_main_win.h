482e657da2313e319b13ed5d957064315b06df31
David Roger <droger@chromium.org>
2022-10-19 08:45:16 +0000

    [win] Fix crash in ModuleDatabase with the kObserverBasedPostProfileInit experiment
    `PostProfileInit()` is only called after profiles are created, which is
    not always at startup. `SetupModuleDatabase()` is being moved to
    `PreProfileInit()` which is called at startup.
    
    Fixed: 1372608
    Bug: 1150326
    Change-Id: I799aa325cae9381e0d84883eb3b103dc8ef8a7be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3948584
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060905}

--- chrome/browser/chrome_browser_main_win.h
+++ chrome/browser/chrome_browser_main_win.h
@@ -39,6 +39,7 @@ class ChromeBrowserMainPartsWin : public ChromeBrowserMainParts {
 
   // ChromeBrowserMainParts overrides.
   void ShowMissingLocaleMessageBox() override;
+  void PreProfileInit() override;
   void PostProfileInit(Profile* profile, bool is_initial_profile) override;
   void PostBrowserStart() override;
 
