b02331d619ebd6936ece000bedf1299307b22962
Robert Ogden <robertogden@chromium.org>
2022-10-19 23:28:42 +0000

    Add fuzzing tests to libphonenumber
    Adds two fuzzing tests, one for the basic libphonenumber's Parse.
    
    The other fuzzer is for the charntorune function that is actually
    declared in //third_party/utf, but is defined in libphonenumber's
    rune.c.
    
    Bug: 1346675
    Change-Id: Ie9ad7b3ca66c862d027764c904756281d4e2d675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964620
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061311}

---
+++ third_party/libphonenumber/charntorune_fuzzer.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "third_party/libphonenumber/dist/cpp/src/phonenumbers/utf/utf.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  Rune rune;
+  charntorune(&rune, reinterpret_cast<const char*>(data), size);
+  return 0;
+}
