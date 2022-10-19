b43aaf7d8dcb8e60960e1f96098abb1dda9011c7
Matt Menke <mmenke@chromium.org>
2022-10-19 02:30:12 +0000

    [FLEDGE] Pass reporter to AuctionRunner.
    Tiny CL that makes InterestGroupAuction pass the (completed)
    InterestGroupAuctionReporter to AuctionRunner, which retrieves various
    parameters from it.
    
    Bug: 1370882
    Change-Id: I302a11df8b83a03b80d962b2cd5c81c56d769f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3956487
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060828}

--- content/browser/interest_group/auction_runner.cc
+++ content/browser/interest_group/auction_runner.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
+#include "content/browser/interest_group/interest_group_auction_reporter.h"
 #include "content/browser/interest_group/interest_group_manager_impl.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/services/auction_worklet/public/mojom/private_aggregation_request.mojom.h"
@@ -177,14 +178,22 @@ void AuctionRunner::OnReportingPhaseComplete(
 
   UpdateInterestGroupsPostAuction();
 
+  std::unique_ptr<InterestGroupAuctionReporter> reporter =
+      auction_.TakeReporter();
+  DCHECK(reporter);
+
+  auto errors = auction_.TakeErrors();
+  errors.insert(errors.end(), reporter->errors().begin(),
+                reporter->errors().end());
+
   state_ = State::kSucceeded;
   std::move(callback_).Run(
       this, /*manually_aborted=*/false, std::move(winning_group_key),
       auction_.top_bid()->bid->render_url,
-      auction_.top_bid()->bid->ad_components, auction_.TakeReportUrls(),
+      auction_.top_bid()->bid->ad_components, reporter->TakeReportUrls(),
       std::move(debug_loss_report_urls), std::move(debug_win_report_urls),
-      auction_.TakeAdBeaconMap(), auction_.TakePrivateAggregationRequests(),
-      auction_.TakeErrors());
+      reporter->TakeAdBeaconMap(), reporter->TakePrivateAggregationRequests(),
+      std::move(errors));
 }
 
 void AuctionRunner::UpdateInterestGroupsPostAuction() {
