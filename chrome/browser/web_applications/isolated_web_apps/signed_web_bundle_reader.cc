5981385c7f9f553e89c246402198be09d1a4cf29
Christian Flach <cmfcmf@chromium.org>
2022-10-19 12:01:42 +0000

    isolated-apps: Evict SignedWebBundleReaders from cache after timeout
    Since each `SignedWebBundleReader` uses memory and holds a reference to
    a file, we need to make sure to cleanup old readers if they have not
    been used for some time. This CL introduces a time-based mechanism that
    removes `CacheEntry`s that have not been accessed for 10 minutes or
    more.
    
    In line with go/isolated-apps-when-to-verify-signatures, signatures of
    Isolated Web Apps are verified once per browser session on non-ChromeOS
    devices (and only at install-time on ChromeOS). To keep track of the
    files whose signatures were already verified during the session, we
    introduce an additional `base::flat_set` that stores the file paths of
    already verified files.
    
    Public Explainer: https://github.com/reillyeon/isolated-web-apps
    Design Docs: go/isolated-apps-serving, go/isolated-app-scheme
                 go/isolated-apps-when-to-verify-signatures
    
    Bug: 1367177
    
    Change-Id: I3aec008e1208b7fa5907831b6d7e9f88686b3285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755528
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060965}

--- chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_reader.cc
+++ chrome/browser/web_applications/isolated_web_apps/signed_web_bundle_reader.cc
@@ -177,12 +177,10 @@ void SignedWebBundleReader::OnShouldContinueParsingAfterIntegrityBlock(
     case SignatureVerificationAction::Type::kContinueAndVerifySignatures:
       VerifySignatures(std::move(integrity_block), std::move(callback));
       return;
-#if BUILDFLAG(IS_CHROMEOS)
     case SignatureVerificationAction::Type::
         kContinueAndSkipSignatureVerification:
       ReadMetadata(std::move(callback));
       return;
-#endif
   }
 }
 
@@ -502,16 +500,12 @@ SignedWebBundleReader::SignatureVerificationAction SignedWebBundleReader::
                                      absl::nullopt);
 }
 
-#if BUILDFLAG(IS_CHROMEOS)
-
 // static
 SignedWebBundleReader::SignatureVerificationAction SignedWebBundleReader::
     SignatureVerificationAction::ContinueAndSkipSignatureVerification() {
   return SignatureVerificationAction(
       Type::kContinueAndSkipSignatureVerification, absl::nullopt);
 }
-
-#endif
 
 SignedWebBundleReader::SignatureVerificationAction::SignatureVerificationAction(
     Type type,
