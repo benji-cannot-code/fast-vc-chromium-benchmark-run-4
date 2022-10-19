c2625a868047aa18d2c57334e6006396b7797c92
Femi Adegunloye <mrfemi@google.com>
2022-10-19 02:05:41 +0000

    [exo][hdr] Remove last exo buildflag
    Remove enable_color_manager buildflag, associated files, and includes as
    there no other flags being used by the file.
    
    BUG=b:192562912
    
    Change-Id: Ic9decc2681cff60b6dbab8e08eb928aac94876ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939204
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    Reviewed-by: Jim Shargo <jshargo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060816}

--- components/exo/wayland/clients/test/client_version_test.cc
+++ components/exo/wayland/clients/test/client_version_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wayland/clients/test/client_version_test.h"
 
 #include <alpha-compositing-unstable-v1-client-protocol.h>
+#include <chrome-color-management-server-protocol.h>
 #include <content-type-v1-server-protocol.h>
 #include <cursor-shapes-unstable-v1-server-protocol.h>
 #include <extended-drag-unstable-v1-server-protocol.h>
@@ -85,6 +86,7 @@ struct Globals {
   std::unique_ptr<zcr_secure_output_v1> zcr_secure_output_v1;
   std::unique_ptr<zcr_alpha_compositing_v1> zcr_alpha_compositing_v1;
   std::unique_ptr<zcr_stylus_v2> zcr_stylus_v2;
+  std::unique_ptr<zcr_color_manager_v1> zcr_color_manager_v1;
   std::unique_ptr<zcr_cursor_shapes_v1> zcr_cursor_shapes_v1;
   std::unique_ptr<zcr_gaming_input_v2> zcr_gaming_input_v2;
   std::unique_ptr<zcr_text_input_extension_v1> zcr_text_input_extension_v1;
@@ -179,6 +181,7 @@ void RegistryHandler(void* data,
           REGISTRY_CALLBACK(zcr_secure_output_v1, zcr_secure_output_v1),
           REGISTRY_CALLBACK(zcr_alpha_compositing_v1, zcr_alpha_compositing_v1),
           REGISTRY_CALLBACK(zcr_stylus_v2, zcr_stylus_v2),
+          REGISTRY_CALLBACK(zcr_color_manager_v1, zcr_color_manager_v1),
           REGISTRY_CALLBACK(zcr_cursor_shapes_v1, zcr_cursor_shapes_v1),
           REGISTRY_CALLBACK(zcr_gaming_input_v2, zcr_gaming_input_v2),
           REGISTRY_CALLBACK(zcr_keyboard_configuration_v1,
