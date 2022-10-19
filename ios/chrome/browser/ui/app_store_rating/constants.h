df9f5993269b21fbb28412683aa4a84cc29b7d84
Hira Mahmood <hiramahmood@google.com>
2022-10-19 21:50:46 +0000

    Implement unit tests for AppStoreRatingSceneAgent.
    This change implements unit tests for the AppStoreRatingSceneAgent
    class.
    
    Bug: 1359065
    Change-Id: Id7da02bd2e53b314f5c5ca2f8bf475efd495afee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952411
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061256}

---
+++ ios/chrome/browser/ui/app_store_rating/constants.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_APP_STORE_RATING_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_APP_STORE_RATING_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+// Key used to store the total number of unique days that the user has
+// started a session in NSUserDefaults.
+extern NSString* const kAppStoreRatingTotalDaysOnChromeKey;
+
+// Key used to store an array of unique days that the user has started
+// a session in the past 7 days in NSUserDefaults.
+extern NSString* const kAppStoreRatingActiveDaysInPastWeekKey;
+
+#endif  // IOS_CHROME_BROWSER_UI_APP_STORE_RATING_CONSTANTS_H_
