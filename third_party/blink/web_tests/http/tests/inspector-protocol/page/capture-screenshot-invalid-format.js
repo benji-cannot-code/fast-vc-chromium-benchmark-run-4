c04a867e71fd1b3eed8f37682b77fddb091b48e8
Andrey Kosyakov <caseq@chromium.org>
2022-10-19 02:55:51 +0000

    DevTools: refactor screenshot encoding logic
    Rather than carrying textual format + quality through the async
    pipeline of screenshot creation, produce a ScreenshodEncoder closure
    early bound with relevant parameters.
    
    As a drive-by, avoid DCHECK()-ing upon invalid screenshot format.
    
    Bug: 1375011
    Change-Id: Ic1e08449a6b26274bacc84ed19000e0c17510315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964794
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060841}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/capture-screenshot-invalid-format.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      `Tests that calling Page.captureScreenshot with invalid format returns an error`);
+
+  const response = await dp.Page.captureScreenshot({format: "pcx"});
+  testRunner.log(response.error || 'FAIL: error not reported');
+  testRunner.completeTest();
+})
