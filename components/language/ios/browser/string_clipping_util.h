cca1b60f810726f700b6fccf65e66e195c48f859
Mike Dougherty <michaeldo@chromium.org>
2022-10-19 21:09:06 +0000

    Move string_clipping_util to //components/language
    This file is being moved in preparation of merging
    LanguageDetectionController into IOSLanguageDetectionTabHelper. The full
    change is visible in crrev.com/c/3929711, but portions of that CL will
    be landed separately to reduce the size of each change.
    
    Bug: None
    Change-Id: Ie713d54b25a90231bead74373e2e189a24afab9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962110
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061236}

--- components/language/ios/browser/string_clipping_util.h
+++ components/language/ios/browser/string_clipping_util.h
@@ -1,15 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_TRANSLATE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
-#define COMPONENTS_TRANSLATE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
+#ifndef COMPONENTS_LANGUAGE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
+#define COMPONENTS_LANGUAGE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
 
 #include <stddef.h>
 
 #include <string>
-
 
 // Truncates |contents| to |length|.
 // Returns a string terminated at the last space to ensure no words are
@@ -19,4 +18,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 std::u16string GetStringByClippingLastWord(const std::u16string& contents,
                                            size_t length);
 
-#endif  // COMPONENTS_TRANSLATE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
+#endif  // COMPONENTS_LANGUAGE_IOS_BROWSER_STRING_CLIPPING_UTIL_H_
--- components/language/ios/browser/string_clipping_util_unittest.cc
+++ components/language/ios/browser/string_clipping_util_unittest.cc
@@ -1,16 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/translate/ios/browser/string_clipping_util.h"
+#include "components/language/ios/browser/string_clipping_util.h"
 
 #include <stddef.h>
 
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace translate {
+namespace language {
 namespace {
 
 // Tests that a regular sentence is clipped correctly.
@@ -46,4 +46,4 @@ TEST(StringByClippingLastWordTest, ClipLongTextContentNoSpace) {
 }
 
 }  // namespace
-}  // namespace translate
+}  // namespace language
