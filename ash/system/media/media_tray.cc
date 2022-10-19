4b8ddd0d338714eda56ee35a42553c2084e4fc53
Alex Newcomer <newcomer@google.com>
2022-10-19 15:23:53 +0000

    chromeos:small refactor of MediaTray
    The PerformAction override was not needed, just impl
    GetBubbleWidget. Also some tiny modernizations.
    
    Bug=b/250632411
    
    Change-Id: I6cca048a4d6de21318425b1ae1ad521cfe70d9e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957159
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061036}

--- ash/system/media/media_tray.cc
+++ ash/system/media/media_tray.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/style/icon_button.h"
 #include "ash/system/media/media_notification_provider.h"
 #include "ash/system/tray/tray_bubble_view.h"
 #include "ash/system/tray/tray_bubble_wrapper.h"
@@ -100,6 +101,31 @@ enum PinState {
 class GlobalMediaControlsTitleView : public views::View {
  public:
   GlobalMediaControlsTitleView() {
+    auto* box_layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
+        views::BoxLayout::Orientation::kHorizontal, kTitleViewInsets));
+    box_layout->set_minimum_cross_axis_size(kTitleViewHeight);
+    box_layout->set_cross_axis_alignment(
+        views::BoxLayout::CrossAxisAlignment::kCenter);
+
+    title_label_ = AddChildView(std::make_unique<views::Label>());
+    title_label_->SetText(
+        l10n_util::GetStringUTF16(IDS_ASH_GLOBAL_MEDIA_CONTROLS_TITLE));
+    title_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+    title_label_->SetAutoColorReadabilityEnabled(false);
+    title_label_->SetFontList(views::Label::GetDefaultFontList().Derive(
+        kTitleFontSizeIncrease, gfx::Font::NORMAL, gfx::Font::Weight::MEDIUM));
+
+    // Media tray should always be pinned to shelf when we are opening the
+    // dialog.
+    DCHECK(MediaTray::IsPinnedToShelf());
+    pin_button_ = AddChildView(std::make_unique<MediaTray::PinButton>());
+
+    box_layout->SetFlexForView(title_label_, 1);
+  }
+
+  void OnThemeChanged() override {
+    views::View::OnThemeChanged();
     SetBorder(views::CreatePaddedBorder(
         views::CreateSolidSidedBorder(
             gfx::Insets::TLBR(0, 0, kMenuSeparatorWidth, 0),
@@ -108,35 +134,15 @@ class GlobalMediaControlsTitleView : public views::View {
         gfx::Insets::TLBR(kMenuSeparatorVerticalPadding, 0,
                           kMenuSeparatorVerticalPadding - kMenuSeparatorWidth,
                           0)));
-
-    auto* box_layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
-        views::BoxLayout::Orientation::kHorizontal, kTitleViewInsets));
-    box_layout->set_minimum_cross_axis_size(kTitleViewHeight);
-    box_layout->set_cross_axis_alignment(
-        views::BoxLayout::CrossAxisAlignment::kCenter);
-
-    auto* title_label = AddChildView(std::make_unique<views::Label>());
-    title_label->SetText(
-        l10n_util::GetStringUTF16(IDS_ASH_GLOBAL_MEDIA_CONTROLS_TITLE));
-    title_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-    title_label->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
+    title_label_->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
         AshColorProvider::ContentLayerType::kTextColorPrimary));
-    title_label->SetAutoColorReadabilityEnabled(false);
-    title_label->SetFontList(views::Label::GetDefaultFontList().Derive(
-        kTitleFontSizeIncrease, gfx::Font::NORMAL, gfx::Font::Weight::MEDIUM));
-
-    // Media tray should always be pinned to shelf when we are opening the
-    // dialog.
-    DCHECK(MediaTray::IsPinnedToShelf());
-    pin_button_ = AddChildView(std::make_unique<MediaTray::PinButton>());
-
-    box_layout->SetFlexForView(title_label, 1);
   }
 
   views::Button* pin_button() { return pin_button_; }
 
  private:
-  MediaTray::PinButton* pin_button_ = nullptr;
+  views::ImageButton* pin_button_ = nullptr;
+  views::Label* title_label_ = nullptr;
 };
 
 }  // namespace
@@ -251,12 +257,8 @@ void MediaTray::HandleLocaleChange() {
       IDS_ASH_GLOBAL_MEDIA_CONTROLS_BUTTON_TOOLTIP_TEXT));
 }
 
-bool MediaTray::PerformAction(const ui::Event& event) {
-  if (bubble_)
-    CloseBubble();
-  else
-    ShowBubble();
-  return true;
+views::Widget* MediaTray::GetBubbleWidget() const {
+  return bubble_ ? bubble_->GetBubbleWidget() : nullptr;
 }
 
 void MediaTray::ShowBubble() {
