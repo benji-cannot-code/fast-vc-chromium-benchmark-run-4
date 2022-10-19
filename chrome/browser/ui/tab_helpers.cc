ffb4708ce3a20e609255fa723982e80995cf2712
Christian Dullweber <dullweber@chromium.org>
2022-10-19 14:27:45 +0000

    Safety check: Fix crash in off-the-record profiles
    The service is not created in off-the-record profiles so we shouldn't
    try to create a TabHelper either.
    
    Bug: 1376360
    Change-Id: I2890cef0fc87cee8a527c75df0dc010de65722e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964431
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061011}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -525,8 +525,10 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   if (base::FeatureList::IsEnabled(
           permissions::features::kRecordPermissionExpirationTimestamps)) {
     auto* service = UnusedSitePermissionsServiceFactory::GetForProfile(profile);
-    permissions::UnusedSitePermissionsService::TabHelper::CreateForWebContents(
-        web_contents, service);
+    if (service) {
+      permissions::UnusedSitePermissionsService::TabHelper::
+          CreateForWebContents(web_contents, service);
+    }
   }
 #endif
 
