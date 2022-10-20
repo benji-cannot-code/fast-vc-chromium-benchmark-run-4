0026d8689e18f5fb9e138efc7d0752287c5d6579
Daniel Murphy <dmurph@google.com>
2022-10-20 23:27:30 +0000

    [dPWA] Refactoring launches to always be async.
    Context:
    Reading information from the WebAppProvider system is often inherently
    racy (given all of our async data sources). Putting operations behind
    commands guarantees the the data read is not going to change across
    async calls (and that you aren't reading it in the middle of an
    operation like 'install', 'uninstall', etc).
    
    This project protects 'launch' to make sure it doesn't happen during
    and 'uncommitted read' state.
    
    This change was created by using 'git cl split', and is the first part
    in making dPWA launches async. The app service is designed to have
    launches be async (and in lacros they will ALWAYS be async), but
    currently they are "mostly" synchronous. This means that there are a
    variety of tests (and even one instance of production code - the
    intent launcher) which assumes this. This patch fixes all of those
    cases.
    
    Specifically patches split here:
    * Add browser or navigation waiters to wait until launches complete in
      tests.
    * Modifies the method signatures required for making launches async for
      making launches async (but just calls the callback synchronously for
      now.
    * Fixes a future bug for Lacros where the intent navigation closes the
      window before launching the app, causing the browser/profile to start
      shutting down. Keep-alives were added to ensure this does not happen.
    
    There is no functional change in this patch.
    If you want to see a patch with all test changes & functionality
    changes, see:
    https://chromium-review.googlesource.com/c/chromium/src/+/3929017
    
    $description
    
    This CL was uploaded by git cl split.
    
    R=jamescook@chromium.org, sky@chromium.org
    
    Bug: b/242059352, 1377102
    Change-Id: Ib9f8e7a52b74f15bdaee4cf654f8d31f19f22535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969258
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061885}

--- chrome/browser/ui/views/frame/system_menu_model_builder_browsertest_chromeos.cc
+++ chrome/browser/ui/views/frame/system_menu_model_builder_browsertest_chromeos.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/test/browser_test.h"
@@ -57,7 +58,10 @@ IN_PROC_BROWSER_TEST_F(SystemMenuModelBuilderMultiUserTest,
 
   // Open the settings window and record the |settings_browser|.
   auto* manager = SettingsWindowManager::GetInstance();
+  ui_test_utils::BrowserChangeObserver browser_opened(
+      nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
   manager->ShowOSSettings(profile);
+  browser_opened.Wait();
 
   auto* settings_browser = manager->FindBrowserForProfile(profile);
   ASSERT_TRUE(settings_browser);
