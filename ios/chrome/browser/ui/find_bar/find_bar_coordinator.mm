388be53586ebaf8343259150876dcefd7f4bcaf2
Sylvain Defresne <sdefresne@chromium.org>
2022-10-20 15:09:14 +0000

    [ios] Fix crash in TextZoomMediator
    Remove usage of ActiveWebStateObservationForwarder from TextZoomMediator
    as it already observe WebStateList to react to the change of the active
    tab. Move the WebStateObserver registration in TextZoomMediator and call
    RemoveObserver when the WebState is destroyed.
    
    Change BrowserCoordinator to call -stop and to drop its reference to the
    TextZoomCoordinator and FindBarCoordinator after they are closed. Also
    correctly support if they are re-opened.
    
    Use an enum to communicate which Toolbar to open if trying to switch
    from TextZoomCoordinator and FindBarCoordinator. This avoid creating
    a coordinator and then immediately dropping the reference without
    calling -start nor -stop (making it difficult to reason about whether
    the -stop needs to be called on the object if not nil).
    
    Bug: 1315281
    Change-Id: I7452ee191b6317a129aea5b8ad7c6457c064b5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963239
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061602}

--- ios/chrome/browser/ui/find_bar/find_bar_coordinator.mm
+++ ios/chrome/browser/ui/find_bar/find_bar_coordinator.mm
@@ -77,6 +77,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   [self.findBarController findBarViewWillHide];
   [self.presenter dismissAnimated:animated];
+
+  [self.mediator disconnect];
   self.mediator = nil;
 }
 
