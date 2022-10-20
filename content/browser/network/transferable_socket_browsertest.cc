78fb35f0bd058fd6b023c651e7ab8303eb3b5393
Fabrice de Gans <fdegans@chromium.org>
2022-10-20 17:54:09 +0000

    [content] Call ReleaseSocketDescriptorForTesting() on the IO thread
    In TransferableSocketBrowserTest.TransferSocket, the call to
    ReleaseSocketDescriptorForTesting() ends up calling
    StopWatchingFileDescriptor() on a different sequence than the one used
    to start watching the file descriptor.
    
    On Fuchsia, the use of a WeakPtr in the StopWatchingFileDescriptor()
    implementation ensures that the call be made on the same sequence as
    the one used to start watching, making the test fail in dchecks
    builds.
    
    This change fixes the issue by calling
    ReleaseSocketDescriptorForTesting() on the IO thread for every
    non-Windows platforms.
    
    Bug: 1071095, 1374888
    Change-Id: I01216c40c144aee371bae8b2797da0d06daa5ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953263
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061697}

--- content/browser/network/transferable_socket_browsertest.cc
+++ content/browser/network/transferable_socket_browsertest.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "build/build_config.h"
 #include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/service_process_host.h"
 #include "content/public/browser/service_process_info.h"
@@ -141,9 +143,15 @@ IN_PROC_BROWSER_TEST_F(TransferableSocketBrowserTest, TransferSocket) {
   network::TransferableSocket transferable(
       socket.ReleaseSocketDescriptorForTesting(), network_process);
 #else
-  network::TransferableSocket transferable(
-      socket.ReleaseSocketDescriptorForTesting());
+  base::test::TestFuture<net::SocketDescriptor> socket_descriptor;
+  GetIOThreadTaskRunner({})->PostTaskAndReplyWithResult(
+      FROM_HERE, base::BindLambdaForTesting([&]() {
+        return socket.ReleaseSocketDescriptorForTesting();
+      }),
+      socket_descriptor.GetCallback());
+  network::TransferableSocket transferable(socket_descriptor.Get());
 #endif
+
   {
     base::RunLoop network_service_runloop;
     network_service_test()->MakeRequestToServer(
