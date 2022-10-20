ed6895a2b19d92e409993cdebfcfe4913c46ca1d
Victor-Gabriel Savu <vsavu@google.com>
2022-10-20 11:02:28 +0000

    migration: Trigger back migration during kiosk start
    Backward migration (Lacros -> Ash) needs to be triggered in the same
    way as forward migration (Ash -> Lacros). Add a missing call
    in kiosk_launch_controller.cc.
    
    Bug: b:244572632
    Change-Id: I93bc3741a603048cd6749f2ab093b9544d665c23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963236
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Jana Grill <janagrill@google.com>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061509}

--- chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
+++ chrome/browser/ash/login/app_mode/kiosk_launch_controller.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_launcher.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_manager.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_service_launcher.h"
+#include "chrome/browser/ash/crosapi/browser_data_back_migrator.h"
 #include "chrome/browser/ash/crosapi/browser_data_migrator.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/crosapi/crosapi_ash.h"
@@ -292,6 +293,13 @@ void KioskLaunchController::OnProfileLoaded(Profile* profile) {
           user->GetAccountId(), user->username_hash(),
           crosapi::browser_util::PolicyInitState::kAfterInit)) {
     LOG(WARNING) << "Restarting chrome to run profile migration.";
+    return;
+  }
+
+  if (BrowserDataBackMigrator::MaybeRestartToMigrateBack(
+          user->GetAccountId(), user->username_hash(),
+          crosapi::browser_util::PolicyInitState::kAfterInit)) {
+    LOG(WARNING) << "Restarting chrome to run backward profile migration.";
     return;
   }
 
