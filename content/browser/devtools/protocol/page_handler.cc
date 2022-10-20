33e9d9477b0f569390df8adc7e254ccb68c36650
Andrey Kosyakov <caseq@chromium.org>
2022-10-20 23:01:38 +0000

    Add optimizeForSpeed support to HeadlessExperimental.captureScreenshot
    As a drive-by, implement WEBP support for screenshots captured
    by HeadlessExperimental.beginFrame.
    
    Bug: 1375011, b/203675739
    Change-Id: I87bf631408cb7205c0451781f554ddeaed1bbf5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968879
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061870}

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/back_forward_cache/disabled_reason_id.h"
 #include "content/browser/child_process_security_policy_impl.h"
