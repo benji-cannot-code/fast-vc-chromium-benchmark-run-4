f246dec2c1dd018fe81a8979afbb6390c42db9b0
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-10-20 09:29:53 +0000

    [IndexedDB] Prefer storage key over security origin
    Bug: 1347831
    Change-Id: Idc3de1a1964fe3d4c41256a54ae016b18dbf4ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918391
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061487}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-set-items-by-storage-key.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp, session, page} = await testRunner.startBlank(
+      `Tests that setting IndexedDB data by storage key works differently to doing so by origin\n`);
+
+  await dp.DOMStorage.enable();
+  await dp.Page.enable();
+
+  testRunner.log("Set storage item on an iframe of page with one origin:");
+  await page.navigate('http://devtools-origin1.test:8000/inspector-protocol/resources/page-with-frame-indexed-db.html');
+  await session.evaluateAsync(`window.onMessagePromise`);
+  testRunner.log("item set\n");
+
+  testRunner.log("Read item from an iframe of page with other origin: ");
+  await page.navigate('http://devtools-origin2.test:8000/inspector-protocol/resources/page-with-frame-indexed-db.html');
+  testRunner.log(await session.evaluateAsync(`window.onMessagePromise`));
+
+  testRunner.completeTest();
+})
+
