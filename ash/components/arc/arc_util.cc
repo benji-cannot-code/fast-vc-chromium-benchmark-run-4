abb287731fad38c536239de8069d95a385854de3
Yusuke Sato <yusukes@google.com>
2022-10-20 19:02:53 +0000

    arc: Introduce arc::GetArcAndroidSdkVersionAsInt()
    This helper function will make it easier to write Chrome code
    for a specific ARC version e.g. R.
    
    BUG=b:253691786
    
    Change-Id: Id42a14d46aa6d8dbcd1b43e36c9157c261ac14df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966906
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061731}

--- ash/components/arc/arc_util.cc
+++ ash/components/arc/arc_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.h"
 #include "chromeos/ash/components/dbus/upstart/upstart_client.h"
+#include "chromeos/version/version_loader.h"
 #include "components/exo/shell_surface_util.h"
 #include "components/user_manager/user_manager.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -114,6 +115,21 @@ bool IsArcAvailable() {
 bool IsArcVmEnabled() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       ash::switches::kEnableArcVm);
+}
+
+int GetArcAndroidSdkVersionAsInt() {
+  const auto arc_version_str =
+      chromeos::version_loader::GetArcAndroidSdkVersion();
+  if (!arc_version_str) {
+    LOG(ERROR) << "ARC SDK version is unknown";
+    return kMaxArcVersion;
+  }
+  int arc_version;
+  if (!base::StringToInt(*arc_version_str, &arc_version)) {
+    LOG(WARNING) << "ARC SDK version is not a number: " << *arc_version_str;
+    return kMaxArcVersion;
+  }
+  return arc_version;
 }
 
 bool IsArcVmRtVcpuEnabled(uint32_t cpus) {
