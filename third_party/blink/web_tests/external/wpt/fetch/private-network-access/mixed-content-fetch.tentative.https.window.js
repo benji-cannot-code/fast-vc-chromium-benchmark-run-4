b88b6c9764b429f2ae384842d6840a88f0d7ff0b
Titouan Rigoudy <titouan@chromium.org>
2022-10-20 13:08:35 +0000

    [Private Network Access] Fix no-cors tests.
    `mode` was misspelled as `method`, which led the tests to fail when they
    should not have.
    
    Bug: chromium:1338439
    Change-Id: I1e933e4d39a3df3d1c28b28a5a2dd8d58032beca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968034
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061545}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/mixed-content-fetch.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/mixed-content-fetch.tentative.https.window.js
@@ -123,9 +123,9 @@ function makeTests({ source, target }) {
     },
     fetchOptions: {
       targetAddressSpace: correctAddressSpace,
-      method: "no-cors",
+      mode: "no-cors",
     },
-    expected: FetchTestResult.SUCCESS,
+    expected: FetchTestResult.OPAQUE,
   });
 }
 
