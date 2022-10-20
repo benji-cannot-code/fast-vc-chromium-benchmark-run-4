19cb2a66cd9db5e05dd840032aeaaa6891727e4c
Haiyang Pan <hypan@google.com>
2022-10-20 01:21:22 +0000

    Set logdog's project from realm name in LUCI_CONTEXT
    And fallback to "chromium" if realm name does not exist.
    
    logdog_wrapper.py used to hardcode the project.
    A better way is to parse the realm name in LUCI_CONTEXT for the project,
    and set it in the script.
    
    Bug: 1373518
    Change-Id: I5142aa1ba12cf2710a1fcd2e573c47f10ded8aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946542
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061358}

--- build/android/test_wrapper/logdog_wrapper.py
+++ build/android/test_wrapper/logdog_wrapper.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import contextlib
+import json
 import logging
 import os
 import signal
@@ -26,7 +27,6 @@ from devil.utils import signal_handler
 from devil.utils import timeout_retry
 from py_utils import tempfile_ext
 
-PROJECT = 'chromium'
 OUTPUT = 'logdog'
 COORDINATOR_HOST = 'luci-logdog.appspot.com'
 LOGDOG_TERMINATION_TIMEOUT = 30
@@ -75,6 +75,28 @@ def NoLeakingProcesses(popen):
                         str(popen.pid))
 
 
+def GetProjectFromLuciContext():
+  """Return the "project" from LUCI_CONTEXT.
+
+  LUCI_CONTEXT contains a section "realm.name" whose value follows the format
+  "<project>:<realm>". This method parses and return the "project" part.
+
+  Fallback to "chromium" if realm name is None
+  """
+  project = 'chromium'
+  ctx_path = os.environ.get('LUCI_CONTEXT')
+  if ctx_path:
+    try:
+      with open(ctx_path) as f:
+        luci_ctx = json.load(f)
+        realm_name = luci_ctx.get('realm', {}).get('name')
+        if realm_name:
+          project = realm_name.split(':')[0]
+    except (OSError, IOError, ValueError):
+      pass
+  return project
+
+
 def main():
   parser = CommandParser()
   args, extra_cmd_args = parser.parse_known_args(sys.argv[1:])
@@ -103,17 +125,18 @@ def main():
                                                   'butler.sock')
       prefix = os.path.join('android', 'swarming', 'logcats',
                             os.environ.get('SWARMING_TASK_ID'))
+      project = GetProjectFromLuciContext()
 
       logdog_cmd = [
           args.logdog_bin_cmd,
-          '-project', PROJECT,
+          '-project', project,
           '-output', OUTPUT,
           '-prefix', prefix,
           '-coordinator-host', COORDINATOR_HOST,
           'serve',
           '-streamserver-uri', streamserver_uri]
       test_env.update({
-          'LOGDOG_STREAM_PROJECT': PROJECT,
+          'LOGDOG_STREAM_PROJECT': project,
           'LOGDOG_STREAM_PREFIX': prefix,
           'LOGDOG_STREAM_SERVER_PATH': streamserver_uri,
           'LOGDOG_COORDINATOR_HOST': COORDINATOR_HOST,
