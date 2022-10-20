137ac6d0401321efe9ec85e102d291999bc7bc6d
Tushar Agarwal <agarwaltushar@google.com>
2022-10-20 08:05:45 +0000

    Remove "reached code"-related code in tracing, since it is no longer used/needed.
    Bug: b/242847966
    Change-Id: I9435a8dd3d07eea7aa80fe7c8cb1807b2bb68415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966358
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061466}

--- services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
+++ services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
@@ -47,10 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/cpp/stack_sampling/loader_lock_sampling_thread_win.h"
 #endif
 
-#if BUILDFLAG(IS_ANDROID)
-#include "base/android/reached_code_profiler.h"
-#endif  // BUILDFLAG(IS_ANDROID)
-
 using StreamingProfilePacketHandle =
     protozero::MessageHandle<perfetto::protos::pbzero::StreamingProfilePacket>;
 using TracePacketHandle = perfetto::TraceWriter::TracePacketHandle;
@@ -384,17 +380,7 @@ struct FrameDetails {
     ANDROID_ARM64_UNWINDING_SUPPORTED || ANDROID_CFI_UNWINDING_SUPPORTED
 // Returns whether stack sampling is supported on the current platform.
 bool IsStackSamplingSupported() {
-#if BUILDFLAG(IS_ANDROID)
-  // The sampler profiler would conflict with the reached code profiler if they
-  // run at the same time because they use the same signal to suspend threads.
-  if (base::android::IsReachedCodeProfilerEnabled()) {
-    return false;
-  }
-#endif  // BUILDFLAG(IS_ANDROID)
-  if (!base::StackSamplingProfiler::IsSupportedForCurrentPlatform()) {
-    return false;
-  }
-  return true;
+  return base::StackSamplingProfiler::IsSupportedForCurrentPlatform();
 }
 #endif
 
@@ -893,14 +879,6 @@ void TracingSamplerProfiler::StartTracing(
 #endif
     return;
   }
-
-#if BUILDFLAG(IS_ANDROID)
-  // The sampler profiler would conflict with the reached code profiler if they
-  // run at the same time because they use the same signal to suspend threads.
-  if (base::android::IsReachedCodeProfilerEnabled()) {
-    return;
-  }
-#endif  // BUILDFLAG(IS_ANDROID)
 
   if (!base::StackSamplingProfiler::IsSupportedForCurrentPlatform()) {
     return;
--- services/tracing/public/cpp/trace_event_agent.cc
+++ services/tracing/public/cpp/trace_event_agent.cc
@@ -24,10 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/cpp/trace_event_args_allowlist.h"
 #include "services/tracing/public/cpp/tracing_features.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "services/tracing/public/cpp/stack_sampling/reached_code_data_source_android.h"
-#endif
-
 namespace tracing {
 
 // static
@@ -52,9 +48,6 @@ TraceEventAgent::TraceEventAgent() {
 
   PerfettoTracedProcess::Get()->AddDataSource(
       TraceEventDataSource::GetInstance());
-#if BUILDFLAG(IS_ANDROID)
-  PerfettoTracedProcess::Get()->AddDataSource(ReachedCodeDataSource::Get());
-#endif
 }
 
 TraceEventAgent::~TraceEventAgent() = default;
