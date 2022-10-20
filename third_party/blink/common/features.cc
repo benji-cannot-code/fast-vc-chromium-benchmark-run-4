4a44c640b211dd37cc38eda866c63afc4e9d0fa2
Jonah Ryan-Davis <jonahr@google.com>
2022-10-20 15:36:05 +0000

    Add features to enable high performance GPU usage for WebGL.
    This CL fixes two bugs that were present when selecting a GLDisplay on
    the high performance GPU. Since this path is now available after the
    fix, it's only enabled behind the flag EGLDualGPURendering.
    This CL also adds the flag ForceHighPerformanceGPUForWebGL to force
    the powerPreference "high-performance" on all WebGL contexts.
    
    Bug: chromium:1251724
    Change-Id: I2e787852089582b88988c1bf524a041d665d4413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952755
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061616}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1672,5 +1672,9 @@ BASE_FEATURE(kAbortSignalHandleBasedRemoval,
              "AbortSignalHandleBasedRemoval",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kForceHighPerformanceGPUForWebGL,
+             "ForceHighPerformanceGPUForWebGL",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
 }  // namespace blink
