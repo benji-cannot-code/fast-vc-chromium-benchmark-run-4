d350173f81fcb380fca4aee8127d8186b0c44429
Yusuke Sato <yusukes@google.com>
2022-10-20 20:26:43 +0000

    arc: Use GetArcVersionAsInt() to detect ARC P
    The previous code didn't work for ARC R container.
    
    BUG=b:253691786
    
    Change-Id: I256eb39619ca4bb931d3c26ef75234160f468520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965066
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061781}

--- chrome/browser/ash/app_restore/arc_window_utils.cc
+++ chrome/browser/ash/app_restore/arc_window_utils.cc
@@ -90,7 +90,7 @@ apps::WindowInfoPtr HandleArcWindowInfo(apps::WindowInfoPtr window_info) {
   // For ARC P, the window bounds in launch parameters should minus caption
   // height.
   int extra_caption_height = 0;
-  if (!arc::IsArcVmEnabled()) {
+  if (arc::GetArcAndroidSdkVersionAsInt() == arc::kArcVersionP) {
     extra_caption_height =
         views::GetCaptionButtonLayoutSize(
             views::CaptionButtonLayoutSize::kNonBrowserCaption)
