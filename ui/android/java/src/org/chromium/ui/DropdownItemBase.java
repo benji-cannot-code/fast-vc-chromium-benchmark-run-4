ddfc55251994c0eedbeff4aaaf12733b38fca726
Vishwas Uppoor <vishwasuppoor@google.com>
2022-10-20 08:51:42 +0000

    [Metadata][Clank] Split second line of the Autofill dropdown suggestion
    crrev.com/c/3710111 splits the first line of the Autofill dropdown
    suggestion to enable filling the card name and the last 4 digits
    separately. This change makes the same change for the second line of
    the Autofill dropdown suggestion. This is required in payment forms
    when fields other than the card number are focused.
    
    Current implementation (label not too long): https://screenshot.googleplex.com/3LhY738uppfyemF
    Current implementation (label too long): https://screenshot.googleplex.com/8dH6tZ5TY28YBsg
    Updated implementation (label not too long): https://screenshot.googleplex.com/3Myq9GmiruH76qu
    Updated implementation (label too long): https://screenshot.googleplex.com/4azStoSXM7h6KeH
    
    Caveat
    1. Currently the card name and the last 4 digits are separated by 2
    spaces, but as we proceed to create the suggestion as a matrix
    (crrev.com/c/3690212), I'm thinking it's best to add margin between
    the components in a line instead of prefixing 2 spaces to them.
    2. This change only gets the UI infrastructure ready on the Clank
    side to receive the second line as 2 sub-labels. The actual
    suggestion will be populated in the future after crrev.com/c/3880710
    lands.
    
    Bug: 1364155
    Change-Id: I8ec0567694679d6bf5fadb01471c236ed01ce9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899442
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061477}

--- ui/android/java/src/org/chromium/ui/DropdownItemBase.java
+++ ui/android/java/src/org/chromium/ui/DropdownItemBase.java
@@ -31,6 +31,11 @@ public class DropdownItemBase implements DropdownItem {
     }
 
     @Override
+    public String getSecondarySublabel() {
+        return null;
+    }
+
+    @Override
     public String getItemTag() {
         return null;
     }
