a1486dbb69f509734171fae5eee7c14e61b66f2b
Eric Sum <esum@google.com>
2022-10-20 04:55:37 +0000

    Add experiment flag for throttling ambient mode animations.
    Bug: b:249539848
    Change-Id: If75e3d29ca2d32e6352d6d460320c282e564a59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966924
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061419}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2698,6 +2698,9 @@ extern const char kAlwaysEnableHdcpDefault[];
 extern const char kAlwaysEnableHdcpType0[];
 extern const char kAlwaysEnableHdcpType1[];
 
+extern const char kAmbientModeThrottleAnimationName[];
+extern const char kAmbientModeThrottleAnimationDescription[];
+
 extern const char kApnRevampName[];
 extern const char kApnRevampDescription[];
 
