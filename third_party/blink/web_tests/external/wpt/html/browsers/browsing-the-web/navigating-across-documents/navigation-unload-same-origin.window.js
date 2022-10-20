ab887ec445d76f1565fbe5fc686f775458a91efc
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-20 07:20:40 +0000

    Import wpt@8a68e3423cb7b3ebd833b295e70d3d4231cc67b1
    Using wpt-import in Chromium 34acf9452b9871eace653047a5af68fedac80b33.
    With Chromium commits locally applied on WPT:
    c386df4690 "[Private Network Access] Test targetAddressSpace more thoroughly."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I0150515e8786f07833b60848f13f009b76065780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967850
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1061454}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/navigating-across-documents/navigation-unload-same-origin.window.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Same-origin navigation started from unload handler must be ignored
+// META: script=../resources/helpers.js
+
+promise_test(async () => {
+  const iframe = await addIframe();
+
+  iframe.contentWindow.addEventListener("unload", () => {
+    iframe.contentWindow.location.href = "/common/blank.html?fail";
+  });
+
+  iframe.src = "/common/blank.html?pass";
+
+  await waitForIframeLoad(iframe);
+  assert_equals(iframe.contentWindow.location.search, "?pass");
+});
