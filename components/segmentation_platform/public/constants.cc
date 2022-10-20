6e8255c9adcd03692a5bd530ac5757c7465bce03
ritikagup <ritikagup@google.com>
2022-10-20 18:54:38 +0000

    Adding DefaultModelTestLibrary and migrating default model tests to use DefaultModelTestLibrary
    1. Extracting out the boiler plate code used across default model unit
    test for setup to a test library.
    2. Migrating the unit test classes to use the test library and easier to
    write unit tests.
    
    The process of writing unit tests for default models becomes easier as
    the setup work for model is done by library and the model unit test
    class has focus only on the tests.
    
    Bug: 1340681
    Change-Id: If31903a34730218a75188d6ac9bfa1209e618f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939340
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061728}

--- components/segmentation_platform/public/constants.cc
+++ components/segmentation_platform/public/constants.cc
@@ -103,4 +103,8 @@ std::string SegmentIdToHistogramVariant(proto::SegmentId segment_id) {
   }
 }
 
+std::string GetSubsegmentKey(const std::string& segmentation_key) {
+  return segmentation_key + kSubsegmentDiscreteMappingSuffix;
+}
+
 }  // namespace segmentation_platform
