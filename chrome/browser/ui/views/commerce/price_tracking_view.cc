e466b702dbd584cdb6d9f7a4468e75102d81e566
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-10-20 01:02:43 +0000

    [PriceTracking] Use const& for PriceTrackingView constructor arguments
    Use const& to avoid unnecessary copying.
    
    Change-Id: I44796b943a110cc7d46157f141628491b4e9c24a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965926
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061345}

--- chrome/browser/ui/views/commerce/price_tracking_view.cc
+++ chrome/browser/ui/views/commerce/price_tracking_view.cc
@@ -36,8 +36,8 @@ constexpr int kImageBorderRadius = 4;
 }  // namespace
 
 PriceTrackingView::PriceTrackingView(Profile* profile,
-                                     GURL page_url,
-                                     gfx::ImageSkia product_image,
+                                     const GURL& page_url,
+                                     const gfx::ImageSkia& product_image,
                                      bool is_price_track_enabled)
     : profile_(profile), is_price_track_enabled_(is_price_track_enabled) {
   // image column
@@ -120,7 +120,7 @@ std::u16string PriceTrackingView::GetToggleAccessibleName() {
                    : IDS_PRICE_TRACKING_TRACK_PRODUCT_ACCESSIBILITY);
 }
 
-void PriceTrackingView::OnToggleButtonPressed(const GURL url) {
+void PriceTrackingView::OnToggleButtonPressed(const GURL& url) {
   is_price_track_enabled_ = !is_price_track_enabled_;
   if (is_price_track_enabled_) {
     base::RecordAction(base::UserMetricsAction(
