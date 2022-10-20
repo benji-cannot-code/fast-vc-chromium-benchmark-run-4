d3b7eb542dd1633271eaf26018641083672e24c3
James Cook <jamescook@chromium.org>
2022-10-20 21:34:19 +0000

    qs: Fix the filename for class IMEDetailedView
    It should be ime_detailed_view.h/cc, not tray_ime_chromeos.h/cc.
    
    Update the copyright date to 2022 because presubmit thinks these files
    are new and demands the current year.
    
    Bug: b:252870873
    Test: compiles
    Change-Id: Ieb1f7f57612c4331c08ff3e99886deec41d0af9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969337
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061826}

--- ash/system/ime/ime_detailed_view.h
+++ ash/system/ime/ime_detailed_view.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2012 The Chromium Authors
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_IME_TRAY_IME_CHROMEOS_H_
-#define ASH_SYSTEM_IME_TRAY_IME_CHROMEOS_H_
+#ifndef ASH_SYSTEM_IME_IME_DETAILED_VIEW_H_
+#define ASH_SYSTEM_IME_IME_DETAILED_VIEW_H_
 
 #include <stddef.h>
 
@@ -59,4 +59,4 @@ class IMEDetailedView : public ImeListView {
 
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_IME_TRAY_IME_CHROMEOS_H_
+#endif  // ASH_SYSTEM_IME_IME_DETAILED_VIEW_H_
