4f069e99f2a3ca064564b3a50672cf78bc9bd2c4
David Benjamin <davidben@chromium.org>
2022-10-20 22:28:12 +0000

    Make SerializedScriptValue::Create take a span<const uint8_t>
    This fixes a few issues:
    
    First, every caller of the bytes-based
    SerializedScriptValue::Create(const char *, size_t) overload actually
    had a span of uint8_t and needed to cast. We should just fix the type.
    Along the way, migrate it to base::span which is more convenient in most
    cases.
    
    Doing so ran into the String(const LChar*) constructor; by being
    implicit, that constructor actually fires on uint8_t[N] because arrays
    decay to pointers and LChar is the same type as uint8_t. This means
    every String|span<uint8_t> overload set is ambiguous on byte arrays. To
    mitigate this, I've made that constructor explicit. Ideally it'd be
    called String::FromLatin1 or something, but I've left that alone here.
    
    Finally, this fixes the SerializedValue() test helper function to no
    longer require even-length input. This, as I understand, is a remnant of
    when the serializer worked on Blink strings. But now this helper is
    exactly the same as SerializedScriptValue::Create except it is slightly
    shorter and works with initializer lists. I've stopped short of
    completely removing it because the overload set makes an initializer
    list not quite work right. (The language doesn't know the type of the
    values in the initializer list.)
    
    (The original motivation of this change was
    https://chromium-review.googlesource.com/c/chromium/src/+/3930754 ran
    into issues with SerializedValue's DCHECK. Tugging on that revealed a
    chain of issues, so I've tried to chisel through a few of them.)
    
    Bug: none
    Change-Id: Icf571cfe3ec4dce7eaf29b28fb7021e615290129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960433
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061850}

--- third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value_test.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value_test.cc
@@ -35,8 +35,7 @@ TEST(SerializedScriptValueTest, WireFormatRoundTrip) {
       sourceSerializedScriptValue->GetWireData();
 
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(
-          reinterpret_cast<const char*>(wire_data.data()), wire_data.size());
+      SerializedScriptValue::Create(wire_data);
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(scope.GetIsolate());
   EXPECT_TRUE(deserialized->IsTrue());
@@ -47,8 +46,7 @@ TEST(SerializedScriptValueTest, WireFormatVersion17NoByteSwapping) {
 
   const uint8_t data[] = {0xFF, 0x11, 0xFF, 0x0D, 0x54, 0x00};
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(reinterpret_cast<const char*>(data),
-                                    sizeof(data));
+      SerializedScriptValue::Create(data);
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(scope.GetIsolate());
   EXPECT_TRUE(deserialized->IsTrue());
@@ -60,8 +58,7 @@ TEST(SerializedScriptValueTest, WireFormatVersion16ByteSwapping) {
   // Using UChar instead of uint8_t to get ntohs() byte swapping.
   const UChar data[] = {0xFF10, 0xFF0D, 0x5400};
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(reinterpret_cast<const char*>(data),
-                                    sizeof(data));
+      SerializedScriptValue::Create(base::as_bytes(base::make_span(data)));
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(scope.GetIsolate());
   EXPECT_TRUE(deserialized->IsTrue());
@@ -73,8 +70,7 @@ TEST(SerializedScriptValueTest, WireFormatVersion13ByteSwapping) {
   // Using UChar instead of uint8_t to get ntohs() byte swapping.
   const UChar data[] = {0xFF0D, 0x5400};
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(reinterpret_cast<const char*>(data),
-                                    sizeof(data));
+      SerializedScriptValue::Create(base::as_bytes(base::make_span(data)));
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(scope.GetIsolate());
   EXPECT_TRUE(deserialized->IsTrue());
@@ -86,8 +82,7 @@ TEST(SerializedScriptValueTest, WireFormatVersion0ByteSwapping) {
   // Using UChar instead of uint8_t to get ntohs() byte swapping.
   const UChar data[] = {0x5400};
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(reinterpret_cast<const char*>(data),
-                                    sizeof(data));
+      SerializedScriptValue::Create(base::as_bytes(base::make_span(data)));
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(scope.GetIsolate());
   EXPECT_TRUE(deserialized->IsTrue());
@@ -113,8 +108,7 @@ TEST(SerializedScriptValueTest, WireFormatVersion0ImageData) {
   data.resize(257);  // (508 pixel data + 6 header bytes) / 2
 
   scoped_refptr<SerializedScriptValue> serializedScriptValue =
-      SerializedScriptValue::Create(reinterpret_cast<const char*>(data.data()),
-                                    data.size() * sizeof(UChar));
+      SerializedScriptValue::Create(base::as_bytes(base::make_span(data)));
   v8::Local<v8::Value> deserialized =
       serializedScriptValue->Deserialize(isolate);
   ASSERT_TRUE(deserialized->IsObject());
