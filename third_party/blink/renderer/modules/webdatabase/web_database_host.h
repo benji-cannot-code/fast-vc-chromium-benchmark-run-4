0d8181f8f676d502f5155cb64c2c2b334a738098
Dave Tapuska <dtapuska@chromium.org>
2022-10-20 20:16:07 +0000

    Remove usage of Thread::GetDeprecatedTaskRunner in WebDatabaseHost.
    Just use IsMainThread to check if the thread is the main thread or not.
    
    BUG=624696
    
    Change-Id: I5c25abc5939fa2856003570c0ccd169a797e4b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967517
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061769}

--- third_party/blink/renderer/modules/webdatabase/web_database_host.h
+++ third_party/blink/renderer/modules/webdatabase/web_database_host.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}
-
 namespace blink {
 
 namespace mojom {
@@ -77,10 +73,6 @@ class WebDatabaseHost {
 
   // Need a SharedRemote as method calls will happen from the Database thread.
   mojo::SharedRemote<mojom::blink::WebDatabaseHost> shared_remote_;
-
-  // Used to ensure that the database gets opened from the main thread, but that
-  // other database-related event is reported from the database thread instead.
-  scoped_refptr<base::SingleThreadTaskRunner> main_thread_task_runner_;
 };
 
 }  // namespace blink
