3a06fb674e393668fbbd28d78a089d107b47414c
Steven Holte <holte@chromium.org>
2022-10-20 14:24:01 +0000

    De-objectify seed simulation.
    Remove VariationsSeedSimulator and make its public method a function.
    
    Change-Id: I43307da432c5387efb778e1c89fac35c9eb5292a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930685
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061578}

--- components/variations/service/variations_service_unittest.cc
+++ components/variations/service/variations_service_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/proto/study.pb.h"
 #include "components/variations/proto/variations_seed.pb.h"
 #include "components/variations/scoped_variations_ids_provider.h"
+#include "components/variations/variations_seed_simulator.h"
 #include "components/version_info/channel.h"
 #include "components/web_resource/resource_request_allowed_notifier_test_util.h"
 #include "net/base/mock_network_change_notifier.h"
@@ -695,7 +696,7 @@ TEST_F(VariationsServiceTest, Observer) {
     TestVariationsServiceObserver observer;
     service.AddObserver(&observer);
 
-    VariationsSeedSimulator::Result result;
+    SeedSimulationResult result;
     result.normal_group_change_count = cases[i].normal_count;
     result.kill_best_effort_group_change_count = cases[i].best_effort_count;
     result.kill_critical_group_change_count = cases[i].critical_count;
