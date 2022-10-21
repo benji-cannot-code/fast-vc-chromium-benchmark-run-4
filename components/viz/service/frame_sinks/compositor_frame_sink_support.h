8a9c0488894d963dbe9190aef1c5716856c927b0
Khushal Sagar <khushalsagar@chromium.org>
2022-10-21 20:19:44 +0000

    viz: Add GPU side support for cross-document transitions.
    Add |navigation_id| to transition directives to identify the unique
    navigation that the directive is associated with. This allows a
    transition to be started between Documents which are associated with
    distinct CC instances that could potentially be in different renderer
    processes.
    
    Bug: 1372584
    Change-Id: I809c48af331e90192dc4c1da60a7db8f13e5c62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966476
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062324}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.h
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.h
@@ -254,7 +254,8 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   void ProcessCompositorFrameTransitionDirective(
       const CompositorFrameTransitionDirective& directive,
       Surface* surface);
-  void OnCompositorFrameTransitionDirectiveProcessed(uint32_t sequence_id);
+  void OnCompositorFrameTransitionDirectiveProcessed(
+      const CompositorFrameTransitionDirective& directive);
 
   void DidReceiveCompositorFrameAck();
   void DidPresentCompositorFrame(uint32_t frame_token,
@@ -424,6 +425,8 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   // frames may be produced by Surfaces managed by distinct
   // CompositorFrameSinks.
   std::unique_ptr<SurfaceAnimationManager> surface_animation_manager_;
+  // The sequence ID for the save directive pending copy.
+  uint32_t in_flight_save_sequence_id_ = 0;
 
   std::unique_ptr<power_scheduler::PowerModeVoter> power_mode_voter_;
 
