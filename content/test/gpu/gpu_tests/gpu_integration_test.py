48e8245804cca7aa46452ac816c94f58cc598729
Brian Sheedy <bsheedy@chromium.org>
2022-10-21 18:07:06 +0000

    Make NVIDIA tags consistent
    At some point, NVIDIA Android devices started reporting
    "nvidia-corporation" instead of "nvidia". This CL makes it so that
    instances of "nvidia-corporation" are replaced by by "nvidia" to keep
    things consistent between desktop and Android. This functionality can be
    easily applied to other tags in the future if necessary.
    
    Bug: 1377134
    Change-Id: I00bc8102dcfd606232988eed351c8269dc0c1fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968691
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062238}

--- content/test/gpu/gpu_tests/gpu_integration_test.py
+++ content/test/gpu/gpu_tests/gpu_integration_test.py
@@ -847,6 +847,7 @@ class GpuIntegrationTest(
     display_server = gpu_helper.GetDisplayServer(browser.browser_type)
     if display_server:
       tags.append(display_server)
+    tags = gpu_helper.ReplaceTags(tags)
     return tags
 
   @classmethod
