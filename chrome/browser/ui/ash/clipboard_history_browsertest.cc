a30dc3d53e4d61f409379426f37f073fc2dea4aa
Colin Kincaid <ckincaid@chromium.org>
2022-10-21 18:42:04 +0000

    multipaste: Fix delete button hover text
    Prior to https://crrev.com/c/3261713, hovering over a clipboard history
    item's delete button did not show any text. The hover text is now
    "Close", which this CL changes to "Remove" for clarity.
    
    Before: https://screenshot.googleplex.com/7e6pX2CPyYL68T3
    After: https://screenshot.googleplex.com/sE2hGtBgdS5HYYn
    
    Fixed: 1377113
    Change-Id: Ief0738384fa8225dce5ba0c8bb50330da8b79890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969684
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062265}

--- chrome/browser/ui/ash/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard_history_browsertest.cc
@@ -45,7 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "ui/base/data_transfer_policy/data_transfer_policy_controller.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/events/test/event_generator.h"
+#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/controls/menu/menu_item_view.h"
@@ -241,6 +243,10 @@ class ClipboardHistoryBrowserTest : public ash::LoginManagerTest {
 
     GetEventGenerator()->MoveMouseTo(
         delete_button->GetBoundsInScreen().CenterPoint());
+    EXPECT_EQ(
+        delete_button->GetTooltipText(delete_button->bounds().CenterPoint()),
+        l10n_util::GetStringUTF16(
+            IDS_CLIPBOARD_HISTORY_DELETE_BUTTON_HOVER_TEXT));
     GetEventGenerator()->ClickLeftButton();
   }
 
