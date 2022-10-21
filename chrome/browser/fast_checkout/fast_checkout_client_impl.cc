3acbdba049997bc69651080dfd77b517d26f68c7
Wolfgang Billenstein <bwolfgang@google.com>
2022-10-21 11:38:30 +0000

    [Fast Checkout] Add commandline switch for disabling RPC signing
    Bug: 1334642
    Change-Id: Ic32d0fa18a1d52c81a897a23d1be94341b795a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968617
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1062054}

--- chrome/browser/fast_checkout/fast_checkout_client_impl.cc
+++ chrome/browser/fast_checkout/fast_checkout_client_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/fast_checkout/fast_checkout_client_impl.h"
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
@@ -62,21 +63,32 @@ std::vector<autofill::AutofillProfile*> GetValidAddressProfiles(
 base::flat_map<std::string, std::string> CreateScriptParameters(
     bool run_consentless,
     GURL url) {
-  return {{autofill_assistant::public_script_parameters::kIntentParameterName,
-           kIntentValue},
-          {autofill_assistant::public_script_parameters::
-               kOriginalDeeplinkParameterName,
-           url.spec()},
-          {autofill_assistant::public_script_parameters::kEnabledParameterName,
-           kTrue},
-          {autofill_assistant::public_script_parameters::
-               kStartImmediatelyParameterName,
-           kTrue},
-          {autofill_assistant::public_script_parameters::kCallerParameterName,
-           kCaller},
-          {autofill_assistant::public_script_parameters::kSourceParameterName,
-           kSource},
-          {kIsNoRoundTrip, run_consentless ? kTrue : kFalse}};
+  base::flat_map<std::string, std::string> script_parameters = {
+      {autofill_assistant::public_script_parameters::kIntentParameterName,
+       kIntentValue},
+      {autofill_assistant::public_script_parameters::
+           kOriginalDeeplinkParameterName,
+       url.spec()},
+      {autofill_assistant::public_script_parameters::kEnabledParameterName,
+       kTrue},
+      {autofill_assistant::public_script_parameters::
+           kStartImmediatelyParameterName,
+       kTrue},
+      {autofill_assistant::public_script_parameters::kCallerParameterName,
+       kCaller},
+      {autofill_assistant::public_script_parameters::kSourceParameterName,
+       kSource},
+      {kIsNoRoundTrip, run_consentless ? kTrue : kFalse}};
+
+  // TODO(b/251365675): Remove once all endpoints support RPC signing.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          kAutofillAssistantUrl)) {
+    script_parameters.insert({autofill_assistant::public_script_parameters::
+                                  kDisableRpcSigningParameterName,
+                              kTrue});
+  }
+
+  return script_parameters;
 }
 
 }  // namespace
