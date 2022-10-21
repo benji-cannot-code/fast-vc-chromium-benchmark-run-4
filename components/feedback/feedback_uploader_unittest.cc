789e66e93f38afc16cb966d572ee5ac44e7f1f41
Anton Swifton <swifton@google.com>
2022-10-21 21:42:40 +0000

    feedback: don't send reports from tast tests
    Turn off sending feedback reports to Listnr if it's running
    in a Tast test, so that Listnr is not spammed with numerous
    automatically generated reports that don't provide any feedback.
    
    This CL uses the feature kOsFeedbackTastTest to determine whether
    the feedback app is run from a Tast test. The fixtures for feedback
    Tast tests will be updated to ensure that all tests initialize this
    feature.
    
    Bug: b:185624798, b:243338565
    Test: components_unittests
    Change-Id: I3d94ee9ec17b8b51c04d02b703addce0ade4f46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965637
    Commit-Queue: Anton Swifton <swifton@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1062364}

--- components/feedback/feedback_uploader_unittest.cc
+++ components/feedback/feedback_uploader_unittest.cc
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/task_traits.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "build/build_config.h"
+#include "components/feedback/features.h"
 #include "components/feedback/feedback_report.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -140,10 +142,16 @@ class FeedbackUploaderTest : public testing::Test {
 
   MockFeedbackUploader* uploader() const { return uploader_.get(); }
 
+  void SetupTastTestFeature() {
+    scoped_feature_list_.InitWithFeatures(
+        {feedback::features::kSkipSendingFeedbackReportInTastTests}, {});
+  }
+
  private:
   network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory_;
   base::test::TaskEnvironment task_environment_;
+  base::test::ScopedFeatureList scoped_feature_list_;
   base::ScopedTempDir scoped_temp_dir_;
   std::unique_ptr<MockFeedbackUploader> uploader_;
 };
@@ -224,6 +232,13 @@ TEST_F(FeedbackUploaderTest, MAYBE_SimulateOfflineReports) {
   // queue is now empty.
   EXPECT_EQ(uploader()->dispatched_reports().size(), 3u);
   EXPECT_TRUE(uploader()->QueueEmpty());
+}
+
+TEST_F(FeedbackUploaderTest, TastTestsDontSendReports) {
+  SetupTastTestFeature();
+  QueueReport(kReportOne);
+
+  EXPECT_EQ(uploader()->dispatched_reports().size(), 0u);
 }
 
 }  // namespace feedback
