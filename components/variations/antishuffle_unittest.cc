1410aaab6a11655588ab44ca74f4b544e1a67f65
Steven Holte <holte@chromium.org>
2022-10-21 20:32:30 +0000

    Pass ValueInRange to NormalizedMurmurHash
    Change-Id: Ia77e365942ff6746e1e5a831bb1c0755ed6a2b32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968985
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062332}

--- components/variations/antishuffle_unittest.cc
+++ components/variations/antishuffle_unittest.cc
@@ -98,7 +98,7 @@ void ProcessSeed(EntropyProviders&& entropy_providers) {
 // randomization is not supported.
 
 TEST(VariationsAntishuffleTest, HighEntropyNil_LowEntropy0) {
-  ProcessSeed(EntropyProviders("", 0, 8000));
+  ProcessSeed(EntropyProviders("", {0, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group87");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group22");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
@@ -108,7 +108,7 @@ TEST(VariationsAntishuffleTest, HighEntropyNil_LowEntropy0) {
 }
 
 TEST(VariationsAntishuffleTest, HighEntropyId0_LowEntropy0) {
-  ProcessSeed(EntropyProviders("clientid_0", 0, 8000));
+  ProcessSeed(EntropyProviders("clientid_0", {0, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group64");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group15");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
@@ -118,7 +118,7 @@ TEST(VariationsAntishuffleTest, HighEntropyId0_LowEntropy0) {
 }
 
 TEST(VariationsAntishuffleTest, HighEntropyId1_LowEntropy0) {
-  ProcessSeed(EntropyProviders("clientid_1", 0, 8000));
+  ProcessSeed(EntropyProviders("clientid_1", {0, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group02");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group40");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
@@ -128,7 +128,7 @@ TEST(VariationsAntishuffleTest, HighEntropyId1_LowEntropy0) {
 }
 
 TEST(VariationsAntishuffleTest, HighEntropyNil_LowEntropy7999) {
-  ProcessSeed(EntropyProviders("", 7999, 8000));
+  ProcessSeed(EntropyProviders("", {7999, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group43");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group48");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
@@ -138,7 +138,7 @@ TEST(VariationsAntishuffleTest, HighEntropyNil_LowEntropy7999) {
 }
 
 TEST(VariationsAntishuffleTest, HighEntropyId0_LowEntropy7999) {
-  ProcessSeed(EntropyProviders("clientid_0", 7999, 8000));
+  ProcessSeed(EntropyProviders("clientid_0", {7999, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group64");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group15");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
@@ -148,7 +148,7 @@ TEST(VariationsAntishuffleTest, HighEntropyId0_LowEntropy7999) {
 }
 
 TEST(VariationsAntishuffleTest, HighEntropyId1_LowEntropy7999) {
-  ProcessSeed(EntropyProviders("clientid_1", 7999, 8000));
+  ProcessSeed(EntropyProviders("clientid_1", {7999, 8000}));
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt"), "group02");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_WithSalt"), "group40");
   EXPECT_EQ(base::FieldTrialList::FindFullName("Study_NoSalt_LowEntropy"),
