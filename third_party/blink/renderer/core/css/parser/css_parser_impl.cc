2a8256e6b3cbd6089cbdce260a7de80a69facb0d
Steinar H. Gunderson <sesse@chromium.org>
2022-10-21 11:43:00 +0000

    Use move constructors for child rules of StyleRule.
    This was written before C++11, and thus used a hack with non-const
    references plus std::swap() instead of taking in constructors by value
    and using std::move().
    
    Change-Id: I604a4274c83011bdd6568a3fed62cdd53128f0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967606
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062062}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -896,7 +896,7 @@ StyleRuleMedia* CSSParserImpl::ConsumeMediaRule(
   if (observer_)
     observer_->EndRuleBody(stream.Offset());
 
-  return MakeGarbageCollected<StyleRuleMedia>(media, rules);
+  return MakeGarbageCollected<StyleRuleMedia>(media, std::move(rules));
 }
 
 StyleRuleSupports* CSSParserImpl::ConsumeSupportsRule(
@@ -941,7 +941,7 @@ StyleRuleSupports* CSSParserImpl::ConsumeSupportsRule(
 
   return MakeGarbageCollected<StyleRuleSupports>(
       prelude_serialized, supported == CSSSupportsParser::Result::kSupported,
-      rules);
+      std::move(rules));
 }
 
 StyleRuleFontFace* CSSParserImpl::ConsumeFontFaceRule(
@@ -1158,7 +1158,7 @@ StyleRuleBase* CSSParserImpl::ConsumeScopeRule(CSSParserTokenStream& stream) {
   if (observer_)
     observer_->EndRuleBody(stream.Offset());
 
-  return MakeGarbageCollected<StyleRuleScope>(*style_scope, rules);
+  return MakeGarbageCollected<StyleRuleScope>(*style_scope, std::move(rules));
 }
 
 StyleRuleContainer* CSSParserImpl::ConsumeContainerRule(
@@ -1203,7 +1203,8 @@ StyleRuleContainer* CSSParserImpl::ConsumeContainerRule(
   if (observer_)
     observer_->EndRuleBody(stream.Offset());
 
-  return MakeGarbageCollected<StyleRuleContainer>(*container_query, rules);
+  return MakeGarbageCollected<StyleRuleContainer>(*container_query,
+                                                  std::move(rules));
 }
 
 StyleRuleBase* CSSParserImpl::ConsumeLayerRule(CSSParserTokenStream& stream) {
@@ -1269,7 +1270,8 @@ StyleRuleBase* CSSParserImpl::ConsumeLayerRule(CSSParserTokenStream& stream) {
   if (observer_)
     observer_->EndRuleBody(stream.Offset());
 
-  return MakeGarbageCollected<StyleRuleLayerBlock>(std::move(name), rules);
+  return MakeGarbageCollected<StyleRuleLayerBlock>(std::move(name),
+                                                   std::move(rules));
 }
 
 StyleRulePositionFallback* CSSParserImpl::ConsumePositionFallbackRule(
