e49b660c0e94fc625c780516922df59a078f7977
Neil Coronado <nemco@google.com>
2022-10-21 23:21:59 +0000

    [Tests] Fix failing tablet tests
    Fix failing tablet tests
    -TabStripTest
    -TabGridDialogTest
    -TabSwitcherTabletTest
    -Incognito*LeakageTest
    
    Bug: 1374370
    Change-Id: I462d60631c2fd2127c872583f79f8968732be60d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967214
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062400}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiFeatureUtilities.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiFeatureUtilities.java
@@ -130,6 +130,7 @@ public class TabUiFeatureUtilities {
 
     private static Boolean sTabManagementModuleSupportedForTesting;
     private static Boolean sGridTabSwitcherPolishEnabledForTesting;
+    private static Boolean sGridTabSwitcherDelayCreationEnabledForTesting;
 
     /**
      * Set whether the tab management module is supported for testing.
@@ -201,6 +202,13 @@ public class TabUiFeatureUtilities {
     }
 
     /**
+     * Set whether the tablet grid tab switcher polish is enabled for testing.
+     */
+    public static void setGtsDelayCreationEnabledForTesting(@Nullable Boolean enabled) {
+        sGridTabSwitcherDelayCreationEnabledForTesting = enabled;
+    }
+
+    /**
      * @return Whether the tablet Grid Tab Switcher Polish is enabled.
      * @param context The activity context.
      */
@@ -218,6 +226,10 @@ public class TabUiFeatureUtilities {
      *         instead of on startup.
      */
     public static boolean isTabletGridTabSwitcherDelayCreationEnabled() {
+        if (sGridTabSwitcherDelayCreationEnabledForTesting != null) {
+            return sGridTabSwitcherDelayCreationEnabledForTesting;
+        }
+
         return DELAY_GTS_CREATION.getValue();
     }
 
