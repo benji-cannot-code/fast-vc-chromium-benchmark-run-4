90b221e4ff00ae5c3521a2904a7798bb8506dfa7
Dominic Farolino <dom@chromium.org>
2022-10-21 18:10:18 +0000

    Fenced Frames: Implement innerConfig attribute
    Now that https://crrev.com/c/3946658 has landed and we have the
    `FencedFrameInnerConfig` WebIDL object shell implemented, this CL
    introduces the `innerConfig` non-reflected IDL attribute to the
    `HTMLFencedFrameElement` interface, and wires up a trivial setter and
    getter.
    
    The next phase will be wiring this up to the navigation flow, so that
    `fenced_frame.innerConfig = [..object..]` can trigger a navigation to
    the config described by the assigned `FencedFrameInnerConfig` object.
    
    R=gtanzer@chromium.org
    
    Bug: 1123606
    Change-Id: I732f76c3f16dce5ad21d7bc8b04bbb79053ce8f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3970608
    Quick-Run: Xiaochen Zhou <xiaochenzh@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062241}

--- third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.h
+++ third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/fenced_frame/fenced_frame.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/core/html/fenced_frame/fenced_frame_inner_config.h"
 #include "third_party/blink/renderer/core/html/html_frame_owner_element.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
@@ -105,6 +106,8 @@ class CORE_EXPORT HTMLFencedFrameElement : public HTMLFrameOwnerElement {
   // while keeping the inner frame size unchanged.
   HTMLIFrameElement* InnerIFrameElement() const;
 
+  FencedFrameInnerConfig* innerConfig() const { return nullptr; }
+  void setInnerConfig(FencedFrameInnerConfig* config) const {}
   // Web-exposed API that returns whether an opaque-ads fenced frame would be
   // allowed to be created in the current active document of this node.
   // Checks the following criteria:
