0bf4ef5fffea2488222e68674696dd7938ab35b7
Yulun Wu <yulunwu@chromium.org>
2022-10-21 20:55:20 +0000

    Add pixel test for answer card title/details text.
    Bug: 1376738
    Change-Id: Ic6cb0a7056f561dfd313f07b482d5653f7e7da75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966272
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062346}

--- ash/app_list/views/search_result_list_view_unittest.cc
+++ ash/app_list/views/search_result_list_view_unittest.cc
@@ -147,8 +147,6 @@ class SearchResultListViewTest : public views::test::WidgetTest,
         ash::AppListSearchResultType::kAssistantText);
     assistant_result->set_display_type(ash::SearchResultDisplayType::kList);
     assistant_result->SetAccessibleName(u"Accessible Name");
-    assistant_result->SetTitleTextVector(
-        BuildTitleTextVector(u"assistant result"));
     assistant_result->SetTitle(u"assistant result");
     results->Add(std::move(assistant_result));
 
@@ -163,13 +161,12 @@ class SearchResultListViewTest : public views::test::WidgetTest,
       result->set_display_type(ash::SearchResultDisplayType::kList);
       result->SetAccessibleName(
           base::UTF8ToUTF16(base::StringPrintf("Result %d", i)));
-      result->SetTitleTextVector(BuildTitleTextVector(
-          base::UTF8ToUTF16(base::StringPrintf("Result %d", i))));
+      result->SetTitle(base::UTF8ToUTF16(base::StringPrintf("Result %d", i)));
       result->set_best_match(true);
       if (i < 2) {
         result->SetAccessibleName(
             base::UTF8ToUTF16(base::StringPrintf("Result %d, Detail", i)));
-        result->SetDetailsTextVector(BuildDetailsTextVector(u"Detail"));
+        result->SetDetails(u"Detail");
       }
       results->Add(std::move(result));
     }
@@ -201,28 +198,6 @@ class SearchResultListViewTest : public views::test::WidgetTest,
     return keyboard_shortcut_text_vector;
   }
 
-  std::vector<SearchResult::TextItem> BuildTitleTextVector(
-      std::u16string title) {
-    std::vector<SearchResult::TextItem> title_text_vector;
-    SearchResult::TextItem shortcut_text_item_1(
-        ash::SearchResultTextItemType::kString);
-    shortcut_text_item_1.SetText(title);
-    shortcut_text_item_1.SetTextTags({});
-    title_text_vector.push_back(shortcut_text_item_1);
-    return title_text_vector;
-  }
-
-  std::vector<SearchResult::TextItem> BuildDetailsTextVector(
-      std::u16string details) {
-    std::vector<SearchResult::TextItem> details_text_vector;
-    SearchResult::TextItem shortcut_text_item_1(
-        ash::SearchResultTextItemType::kString);
-    shortcut_text_item_1.SetText(details);
-    shortcut_text_item_1.SetTextTags({});
-    details_text_vector.push_back(shortcut_text_item_1);
-    return details_text_vector;
-  }
-
   void SetUpKeyboardShortcutResult() {
     SearchModel::SearchResults* results = GetResults();
 
@@ -230,8 +205,8 @@ class SearchResultListViewTest : public views::test::WidgetTest,
         std::make_unique<TestSearchResult>();
     result->set_display_type(ash::SearchResultDisplayType::kList);
     result->SetAccessibleName(u"Copy and Paste");
-    result->SetTitleTextVector(BuildTitleTextVector(u"Copy and Paste"));
-    result->SetDetailsTextVector(BuildDetailsTextVector(u"Shortcuts"));
+    result->SetTitle(u"Copy and Paste");
+    result->SetDetails(u"Shortcuts");
     result->set_best_match(true);
     result->SetKeyboardShortcutTextVector(BuildKeyboardShortcutTextVector());
     results->Add(std::move(result));
@@ -253,8 +228,8 @@ class SearchResultListViewTest : public views::test::WidgetTest,
                    : u" Copy and Paste ";
 
     result->SetAccessibleName(title);
-    result->SetTitleTextVector(BuildTitleTextVector(title));
-    result->SetDetailsTextVector(BuildDetailsTextVector(u"Shortcuts"));
+    result->SetTitle(title);
+    result->SetDetails(u"Shortcuts");
     result->SetKeyboardShortcutTextVector(BuildKeyboardShortcutTextVector());
     results->Add(std::move(result));
 
@@ -277,8 +252,8 @@ class SearchResultListViewTest : public views::test::WidgetTest,
     result->set_display_type(ash::SearchResultDisplayType::kList);
     result->set_best_match(true);
     result->SetAccessibleName(u"Accessible Name");
-    result->SetTitleTextVector(BuildTitleTextVector(base::UTF8ToUTF16(
-        base::StringPrintf("Added Result %d", GetUnifiedViewResultCount()))));
+    result->SetTitle(base::UTF8ToUTF16(
+        base::StringPrintf("Added Result %d", GetUnifiedViewResultCount())));
     GetResults()->Add(std::move(result));
   }
 
