d11cd357f4812809e93b669db76739872a1e4bcf
Artsiom Mitrokhin <amitrokhin@google.com>
2022-10-21 20:33:01 +0000

    Add better handling for interrupted tablet<->clamshell transition
    This CL also disables "AppListAnimationTest.*" tests with
    `kAnimateScaleOnTabletModeTransition` enabled (these tests verify
    old y-axis animation).
    
    Bug: 1344199
    Change-Id: Iddcedffbf33c397b107e727a1ba73d238f8adc6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971505
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062333}

--- ash/app_list/app_list_controller_impl_unittest.cc
+++ ash/app_list/app_list_controller_impl_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/app_list_controller_impl.h"
 
-#include <set>
 #include <string>
 
 #include "ash/app_list/app_list_badge_controller.h"
@@ -28,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_pref_names.h"
 #include "ash/keyboard/keyboard_controller_impl.h"
 #include "ash/keyboard/ui/test/keyboard_test_util.h"
+#include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/assistant/controller/assistant_ui_controller.h"
 #include "ash/public/cpp/session/session_types.h"
 #include "ash/public/cpp/shelf_config.h"
@@ -52,11 +52,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/number_formatting.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/session_manager/session_manager_types.h"
 #include "ui/compositor/layer.h"
-#include "ui/compositor/presentation_time_recorder.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/compositor/test/layer_animation_stopped_waiter.h"
 #include "ui/events/test/event_generator.h"
@@ -65,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/accessibility_paint_checks.h"
 #include "ui/views/controls/textfield/textfield_test_api.h"
 #include "ui/views/test/views_test_utils.h"
-#include "ui/views/test/widget_animation_waiter.h"
 
 namespace ash {
 
@@ -774,10 +771,15 @@ TEST_F(AppListControllerImplTest, CreatePage) {
 
 // The test parameter indicates whether the shelf should auto-hide. In either
 // case the animation behaviors should be the same.
+// TODO(crbug.com/1344199): Remove after flipping
+// `kAnimateScaleOnTabletModeTransition`.
 class AppListAnimationTest : public AshTestBase,
                              public testing::WithParamInterface<bool> {
  public:
-  AppListAnimationTest() = default;
+  AppListAnimationTest() {
+    scoped_feature_list_.InitAndDisableFeature(
+        app_list_features::kAnimateScaleOnTabletModeTransition);
+  }
 
   AppListAnimationTest(const AppListAnimationTest&) = delete;
   AppListAnimationTest& operator=(const AppListAnimationTest&) = delete;
@@ -836,6 +838,7 @@ class AppListAnimationTest : public AshTestBase,
  private:
   // Set during setup.
   gfx::Rect shown_shelf_bounds_;
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 INSTANTIATE_TEST_SUITE_P(AutoHideShelf, AppListAnimationTest, testing::Bool());
