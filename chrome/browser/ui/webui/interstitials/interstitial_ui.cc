0bb4b8e82701f3ad63338fad8ee98bb19756a365
Sneha Nagpaul <snehanagpaul@google.com>
2022-10-21 23:36:31 +0000

    [URLFiltering]Implement Enterprise Warn interstitial UI
    This interstitial will be shown when URLs are flagged by admin configured rules in enterprise managed chrome browsers. The user can choose to bypass this warning.
    
    Bug: b/251185468
    Change-Id: If34f77e290ccd3fb1dcee698553ee8577f8ae921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938168
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062407}

--- chrome/browser/ui/webui/interstitials/interstitial_ui.cc
+++ chrome/browser/ui/webui/interstitials/interstitial_ui.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/enterprise/connectors/interstitials/enterprise_warn_controller_client.h"
+#include "chrome/browser/enterprise/connectors/interstitials/enterprise_warn_page.h"
 #include "chrome/browser/lookalikes/lookalike_url_blocking_page.h"
 #include "chrome/browser/lookalikes/lookalike_url_controller_client.h"
 #include "chrome/browser/profiles/profile.h"
@@ -332,6 +334,15 @@ CreateSafeBrowsingBlockingPage(content::WebContents* web_contents) {
           ui_manager, web_contents, main_frame_url, {resource}, true));
 }
 
+std::unique_ptr<EnterpriseWarnPage> CreateEnterpriseWarnPage(
+    content::WebContents* web_contents) {
+  GURL request_url("https://url-warned-example.net");
+  return std::make_unique<EnterpriseWarnPage>(
+      web_contents, request_url,
+      std::make_unique<EnterpriseWarnControllerClient>(web_contents,
+                                                       request_url));
+}
+
 std::unique_ptr<TestSafeBrowsingBlockingPageQuiet>
 CreateSafeBrowsingQuietBlockingPage(content::WebContents* web_contents) {
   safe_browsing::SBThreatType threat_type =
@@ -495,6 +506,8 @@ void InterstitialHTMLSource::StartDataRequest(
     interstitial_delegate = CreateBlockedInterceptionBlockingPage(web_contents);
   } else if (path_without_query == "/safebrowsing") {
     interstitial_delegate = CreateSafeBrowsingBlockingPage(web_contents);
+  } else if (path_without_query == "/enterprise-warn") {
+    interstitial_delegate = CreateEnterpriseWarnPage(web_contents);
   } else if (path_without_query == "/clock") {
     interstitial_delegate = CreateBadClockBlockingPage(web_contents);
   } else if (path_without_query == "/lookalike") {
