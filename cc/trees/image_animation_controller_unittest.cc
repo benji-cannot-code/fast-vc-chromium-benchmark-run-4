5e6d3d7d605c1f634a894e2077b214e9c3d49111
Sean Maher <spvw@chromium.org>
2022-10-21 18:20:42 +0000

    include <utility> in imagine_animation_controller_unittest
    This is because the file uses std::move, and that is in <utility>.
    
    Change-Id: Ied32b410f1e69a14f6325051dba3bd056f547cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968739
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Sean Maher <spvw@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062253}

--- cc/trees/image_animation_controller_unittest.cc
+++ cc/trees/image_animation_controller_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/image_animation_controller.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/memory/raw_ptr.h"
