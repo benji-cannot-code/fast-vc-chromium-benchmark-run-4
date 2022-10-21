0a3408b97b7077aec0253b9d93a1ac385e0698ed
Joshua Pawlicki <waffles@chromium.org>
2022-10-21 19:39:02 +0000

    Updater: put backup scheduler on a sequence.
    Fixed: 1376977
    Change-Id: Ic5cbf67d029824ecf85d9146452f30cd882010c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3970600
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062300}

--- chrome/browser/updater/scheduler.cc
+++ chrome/browser/updater/scheduler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/updater/scheduler.h"
 
 #include "base/bind.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
@@ -16,24 +17,20 @@ namespace {
 
 void RunAndReschedule() {
   DoPeriodicTasks(base::BindOnce([]() {
-    base::ThreadPool::PostDelayedTask(
-        FROM_HERE,
-        {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
-         base::WithBaseSyncPrimitives(),
-         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
-        base::BindOnce(&RunAndReschedule), base::Hours(5));
+    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+        FROM_HERE, base::BindOnce(&RunAndReschedule), base::Hours(5));
   }));
 }
 
 }  // namespace
 
 void SchedulePeriodicTasks() {
-  base::ThreadPool::PostDelayedTask(
-      FROM_HERE,
+  base::ThreadPool::CreateSequencedTaskRunner(
       {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
        base::WithBaseSyncPrimitives(),
-       base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
-      base::BindOnce(&RunAndReschedule), base::Minutes(5));
+       base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN})
+      ->PostDelayedTask(FROM_HERE, base::BindOnce(&RunAndReschedule),
+                        base::Minutes(5));
 }
 
 }  // namespace updater
