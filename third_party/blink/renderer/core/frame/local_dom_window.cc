fe814fd5a9331569598b5e6409b5e22ca1b3dc0a
Yoav Weiss <yoavweiss@chromium.org>
2022-10-21 16:40:58 +0000

    Set registering task as parent of rAF and rIC callbacks
    When investigating 1371932, I realized that one of the issues there is
    that TaskAttributionTracker doesn't consider registrers for
    requestAnimationFrame and requestIdleCallback as their parent tasks.
    This CL fixes that.
    
    Bug: 1371932
    Change-Id: Ic342b092e91e758fd4ebff17ad0ac87f39c42e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971327
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062191}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -159,6 +159,24 @@ bool IsRunningMicrotasks(ScriptState* script_state) {
     return microtask_queue->IsRunningMicrotasks();
   return v8::MicrotasksScope::IsRunningMicrotasks(script_state->GetIsolate());
 }
+
+void SetCurrentTaskAsCallbackParent(CallbackFunctionBase* callback) {
+  ScriptState* script_state = callback->CallbackRelevantScriptState();
+  auto* tracker = ThreadScheduler::Current()->GetTaskAttributionTracker();
+  if (tracker && script_state->World().IsMainWorld()) {
+    callback->SetParentTaskId(tracker->RunningTaskAttributionId(script_state));
+  }
+}
+
+int RequestAnimationFrame(Document* document,
+                          V8FrameRequestCallback* callback,
+                          bool legacy) {
+  SetCurrentTaskAsCallbackParent(callback);
+  auto* frame_callback = MakeGarbageCollected<V8FrameCallback>(callback);
+  frame_callback->SetUseLegacyTimeBase(legacy);
+  return document->RequestAnimationFrame(frame_callback);
+}
+
 }  // namespace
 
 class LocalDOMWindow::NetworkStateObserver final
@@ -1865,16 +1883,12 @@ void LocalDOMWindow::resizeTo(int width, int height) const {
 }
 
 int LocalDOMWindow::requestAnimationFrame(V8FrameRequestCallback* callback) {
-  auto* frame_callback = MakeGarbageCollected<V8FrameCallback>(callback);
-  frame_callback->SetUseLegacyTimeBase(false);
-  return document()->RequestAnimationFrame(frame_callback);
+  return RequestAnimationFrame(document(), callback, /*legacy=*/false);
 }
 
 int LocalDOMWindow::webkitRequestAnimationFrame(
     V8FrameRequestCallback* callback) {
-  auto* frame_callback = MakeGarbageCollected<V8FrameCallback>(callback);
-  frame_callback->SetUseLegacyTimeBase(true);
-  return document()->RequestAnimationFrame(frame_callback);
+  return RequestAnimationFrame(document(), callback, /*legacy=*/true);
 }
 
 void LocalDOMWindow::cancelAnimationFrame(int id) {
@@ -1882,11 +1896,7 @@ void LocalDOMWindow::cancelAnimationFrame(int id) {
 }
 
 void LocalDOMWindow::queueMicrotask(V8VoidFunction* callback) {
-  ScriptState* script_state = callback->CallbackRelevantScriptState();
-  auto* tracker = ThreadScheduler::Current()->GetTaskAttributionTracker();
-  if (tracker && script_state->World().IsMainWorld()) {
-    callback->SetParentTaskId(tracker->RunningTaskAttributionId(script_state));
-  }
+  SetCurrentTaskAsCallbackParent(callback);
   GetAgent()->event_loop()->EnqueueMicrotask(
       WTF::BindOnce(&V8VoidFunction::InvokeAndReportException,
                     WrapPersistent(callback), nullptr));
@@ -1898,6 +1908,7 @@ bool LocalDOMWindow::originAgentCluster() const {
 
 int LocalDOMWindow::requestIdleCallback(V8IdleRequestCallback* callback,
                                         const IdleRequestOptions* options) {
+  SetCurrentTaskAsCallbackParent(callback);
   if (!GetFrame())
     return 0;
   return document_->RequestIdleCallback(V8IdleTask::Create(callback), options);
