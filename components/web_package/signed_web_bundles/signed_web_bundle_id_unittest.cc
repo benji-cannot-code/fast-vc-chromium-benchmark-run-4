124540890aec0d12593079c4164101156a4dfc5a
Christian Flach <cmfcmf@chromium.org>
2022-10-21 14:56:33 +0000

    signed-web-bundles: Add missing comparison operators
    Bug: 1365852
    Change-Id: I659bf2cf54f5ca41e9fb4ea8f9dc993d6b2f8d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963031
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Oleksandr Peletskyi <peletskyi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062136}

--- components/web_package/signed_web_bundles/signed_web_bundle_id_unittest.cc
+++ components/web_package/signed_web_bundles/signed_web_bundle_id_unittest.cc
@@ -44,7 +44,7 @@ class SignedWebBundleIdValidTest
     : public ::testing::TestWithParam<
           std::tuple<std::string,
                      SignedWebBundleId::Type,
-                     absl::optional<std::array<uint8_t, 32>>>> {
+                     absl::optional<Ed25519PublicKey>>> {
  public:
   SignedWebBundleIdValidTest()
       : raw_id_(std::get<0>(GetParam())),
@@ -54,7 +54,7 @@ class SignedWebBundleIdValidTest
  protected:
   std::string raw_id_;
   SignedWebBundleId::Type type_;
-  absl::optional<std::array<uint8_t, 32>> public_key_;
+  absl::optional<Ed25519PublicKey> public_key_;
 };
 
 TEST_P(SignedWebBundleIdValidTest, ParseValidIDs) {
@@ -62,7 +62,7 @@ TEST_P(SignedWebBundleIdValidTest, ParseValidIDs) {
   EXPECT_TRUE(parsed_id.has_value());
   EXPECT_EQ(parsed_id->type(), type_);
   if (type_ == SignedWebBundleId::Type::kEd25519PublicKey) {
-    EXPECT_EQ(parsed_id->GetEd25519PublicKey().bytes(), *public_key_);
+    EXPECT_EQ(parsed_id->GetEd25519PublicKey(), *public_key_);
   }
 }
 
@@ -75,9 +75,10 @@ INSTANTIATE_TEST_SUITE_P(
                         SignedWebBundleId::Type::kDevelopment,
                         absl::nullopt),
         // Ed25519 key suffix
-        std::make_tuple(kEd25519SignedWebBundleId,
-                        SignedWebBundleId::Type::kEd25519PublicKey,
-                        kEd25519PublicKeyBytes)),
+        std::make_tuple(
+            kEd25519SignedWebBundleId,
+            SignedWebBundleId::Type::kEd25519PublicKey,
+            Ed25519PublicKey::Create(base::make_span(kEd25519PublicKeyBytes)))),
     [](const testing::TestParamInfo<SignedWebBundleIdValidTest::ParamType>&
            info) { return std::get<0>(info.param); });
 
@@ -122,6 +123,11 @@ TEST(SignedWebBundleIdTest, Comparators) {
   EXPECT_TRUE(a1 == a1);
   EXPECT_TRUE(a1 == a2);
   EXPECT_FALSE(a1 == b);
+
+  EXPECT_FALSE(a1 != a1);
+  EXPECT_FALSE(a1 != a2);
+  EXPECT_TRUE(a1 != b);
+
   EXPECT_TRUE(a1 < b);
   EXPECT_FALSE(b < a2);
 }
@@ -132,7 +138,7 @@ TEST(SignedWebBundleIdTest, CreateForEd25519PublicKey) {
 
   auto id = SignedWebBundleId::CreateForEd25519PublicKey(public_key);
   EXPECT_EQ(id.type(), SignedWebBundleId::Type::kEd25519PublicKey);
-  EXPECT_EQ(id.GetEd25519PublicKey().bytes(), public_key.bytes());
+  EXPECT_EQ(id.GetEd25519PublicKey(), public_key);
   EXPECT_EQ(id.id(), kEd25519SignedWebBundleId);
 }
 
