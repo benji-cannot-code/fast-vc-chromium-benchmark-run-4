7a1e469e91b514c06c795404c044977222ff62e0
Vigen Issahhanjan <vigeni@google.com>
2022-10-21 21:21:14 +0000

    [Cast Embedder] Remove RuntimeService From Non-Grpc Types
    This CL updates the RuntimeApplication and associated files to remove
    all usages of LoadApplicationRequest and LaunchApplicationRequest,
    instead moving all such object usages to the gRPC-specific platform
    implementation types.
    
    Bug: 1357135, 1361521
    Change-Id: Ia9bfe211b792538b652c7c1ace6db3559a4b7d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3888011
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1062355}

--- chromecast/cast_core/runtime/browser/cast_runtime_content_browser_client.cc
+++ chromecast/cast_core/runtime/browser/cast_runtime_content_browser_client.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/cast_core/cast_core_switches.h"
 #include "chromecast/cast_core/runtime/browser/runtime_application.h"
 #include "chromecast/cast_core/runtime/browser/runtime_application_dispatcher.h"
-#include "chromecast/cast_core/runtime/browser/runtime_application_dispatcher_platform.h"
 #include "chromecast/cast_core/runtime/browser/runtime_application_dispatcher_platform_grpc.h"
 #include "chromecast/media/base/video_plane_controller.h"
 #include "components/cast_receiver/browser/public/application_client.h"
@@ -24,21 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 
 namespace {
-
-std::unique_ptr<RuntimeApplicationDispatcherPlatform>
-CreateApplicationDispatcherPlatform(
-    RuntimeApplicationDispatcherPlatform::Client& client,
-    CastWebService* web_service) {
-  auto* command_line = base::CommandLine::ForCurrentProcess();
-  std::string runtime_id =
-      command_line->GetSwitchValueASCII(cast::core::kCastCoreRuntimeIdSwitch);
-  std::string runtime_service_path =
-      command_line->GetSwitchValueASCII(cast::core::kRuntimeServicePathSwitch);
-
-  LOG(INFO) << "gRPC platform created";
-  return std::make_unique<RuntimeApplicationDispatcherPlatformGrpc>(
-      client, web_service, runtime_id, runtime_service_path);
-}
 
 // CastServiceSimple impl for Cast Core that allows correct dispatcher start up
 // and tear down.
@@ -167,8 +151,7 @@ CastRuntimeContentBrowserClient::GetNetworkContextGetter() {
 
 void CastRuntimeContentBrowserClient::InitializeCoreComponents(
     CastWebService* web_service) {
-  app_dispatcher_ = std::make_unique<RuntimeApplicationDispatcher>(
-      base::BindOnce(&CreateApplicationDispatcherPlatform), web_service, *this);
+  app_dispatcher_ = RuntimeApplicationDispatcher::Create(*this, web_service);
 }
 
 }  // namespace chromecast
--- chromecast/cast_core/runtime/browser/runtime_application_platform_grpc.cc
+++ chromecast/cast_core/runtime/browser/runtime_application_platform_grpc.cc
@@ -16,28 +16,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/cast_core/grpc/grpc_status_or.h"
 #include "chromecast/cast_core/runtime/browser/grpc_webui_controller_factory.h"
 #include "chromecast/cast_core/runtime/browser/message_port_service_grpc.h"
+#include "chromecast/cast_core/runtime/browser/runtime_application.h"
 #include "chromecast/cast_core/runtime/browser/url_rewrite/url_request_rewrite_type_converters.h"
 #include "chromecast/common/feature_constants.h"
 
 namespace chromecast {
 
 RuntimeApplicationPlatformGrpc::RuntimeApplicationPlatformGrpc(
-    scoped_refptr<base::SequencedTaskRunner> task_runner,
-    std::string session_id,
-    Client& client)
-    : client_(client),
-      session_id_(std::move(session_id)),
+    std::unique_ptr<RuntimeApplication> runtime_application,
+    scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : runtime_application_(std::move(runtime_application)),
       task_runner_(std::move(task_runner)) {
+  DCHECK(runtime_application_);
   DCHECK(task_runner_);
+
+  runtime_application_->SetDelegate(*this);
 }
 
-RuntimeApplicationPlatformGrpc::~RuntimeApplicationPlatformGrpc() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-}
+RuntimeApplicationPlatformGrpc::~RuntimeApplicationPlatformGrpc() = default;
 
 void RuntimeApplicationPlatformGrpc::Load(
-    cast::runtime::LoadApplicationRequest request,
-    LoadCompleteCB callback) {
+    const cast::runtime::LoadApplicationRequest& request,
+    RuntimeApplication::StatusCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!grpc_server_);
 
@@ -87,24 +87,26 @@ void RuntimeApplicationPlatformGrpc::Load(
   LOG(INFO) << "Runtime application server started: endpoint="
             << request.runtime_application_service_info().grpc_endpoint();
 
-  std::move(callback).Run(true);
-
-  // Set initial URL rewrite rules.
+  // TODO(vigeni): Consider extacting this into RuntimeApplicationBase as a
+  // mojo.
   url_rewrite::mojom::UrlRequestRewriteRulesPtr mojom_rules =
       mojo::ConvertTo<url_rewrite::mojom::UrlRequestRewriteRulesPtr>(
           request.url_rewrite_rules());
-  client_->OnUrlRewriteRulesSet(std::move(mojom_rules));
+  runtime_application_->SetUrlRewriteRules(std::move(mojom_rules));
+
+  runtime_application_->Load(std::move(callback));
 }
 
 void RuntimeApplicationPlatformGrpc::Launch(
-    cast::runtime::LaunchApplicationRequest request,
-    LaunchCompleteCB callback) {
+    const cast::runtime::LaunchApplicationRequest& request,
+    RuntimeApplication::StatusCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (request.core_application_service_info().grpc_endpoint().empty()) {
     std::move(callback).Run(false);
     return;
   }
+
   if (request.cast_media_service_info().grpc_endpoint().empty()) {
     std::move(callback).Run(false);
     return;
@@ -119,11 +121,10 @@ void RuntimeApplicationPlatformGrpc::Launch(
 
   // TODO(b/244455581): Configure multizone.
 
-  client_->OnMediaStateSet(request.media_state());
-  client_->OnVisibilitySet(request.visibility());
-  client_->OnTouchInputSet(request.touch_input());
+  runtime_application_->SetMediaState(request.media_state());
+  runtime_application_->SetVisibility(request.visibility());
+  runtime_application_->SetTouchInput(request.touch_input());
 
-  // Report that Cast application launch is initiated.
   std::move(callback).Run(true);
 }
 
@@ -132,13 +133,14 @@ void RuntimeApplicationPlatformGrpc::HandlePostMessage(
     cast::v2::RuntimeMessagePortApplicationServiceHandler::PostMessage::Reactor*
         reactor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!client_->IsApplicationRunning()) {
+  if (!runtime_application_->IsApplicationRunning()) {
     reactor->Write(grpc::Status(grpc::StatusCode::NOT_FOUND,
                                 "No active cast session for PostMessage"));
     return;
   }
 
-  const auto success = client_->OnMessagePortMessage(std::move(request));
+  const auto success =
+      runtime_application_->OnMessagePortMessage(std::move(request));
   if (success) {
     cast::web::MessagePortStatus message_port_status;
     message_port_status.set_status(cast::web::MessagePortStatus::OK);
@@ -154,7 +156,7 @@ void RuntimeApplicationPlatformGrpc::HandleSetUrlRewriteRules(
     cast::v2::RuntimeApplicationServiceHandler::SetUrlRewriteRules::Reactor*
         reactor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!client_->IsApplicationRunning()) {
+  if (!runtime_application_->IsApplicationRunning()) {
     reactor->Write(
         grpc::Status(grpc::StatusCode::NOT_FOUND,
                      "No active cast session for SetUrlRewriteRules"));
@@ -164,7 +166,7 @@ void RuntimeApplicationPlatformGrpc::HandleSetUrlRewriteRules(
     url_rewrite::mojom::UrlRequestRewriteRulesPtr mojom_rules =
         mojo::ConvertTo<url_rewrite::mojom::UrlRequestRewriteRulesPtr>(
             request.rules());
-    client_->OnUrlRewriteRulesSet(std::move(mojom_rules));
+    runtime_application_->SetUrlRewriteRules(std::move(mojom_rules));
   }
   reactor->Write(cast::v2::SetUrlRewriteRulesResponse());
 }
@@ -174,7 +176,7 @@ void RuntimeApplicationPlatformGrpc::HandleSetMediaState(
     cast::v2::RuntimeApplicationServiceHandler::SetMediaState::Reactor*
         reactor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  client_->OnMediaStateSet(request.media_state());
+  runtime_application_->SetMediaState(request.media_state());
   reactor->Write(cast::v2::SetMediaStateResponse());
 }
 
@@ -183,7 +185,7 @@ void RuntimeApplicationPlatformGrpc::HandleSetVisibility(
     cast::v2::RuntimeApplicationServiceHandler::SetVisibility::Reactor*
         reactor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  client_->OnVisibilitySet(request.visibility());
+  runtime_application_->SetVisibility(request.visibility());
   reactor->Write(cast::v2::SetVisibilityResponse());
 }
 
@@ -192,7 +194,7 @@ void RuntimeApplicationPlatformGrpc::HandleSetTouchInput(
     cast::v2::RuntimeApplicationServiceHandler::SetTouchInput::Reactor*
         reactor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  client_->OnTouchInputSet(request.touch_input());
+  runtime_application_->SetTouchInput(request.touch_input());
   reactor->Write(cast::v2::SetTouchInputResponse());
 }
 
@@ -202,7 +204,7 @@ void RuntimeApplicationPlatformGrpc::NotifyApplicationStarted() {
 
   auto call = core_app_stub_->CreateCall<
       cast::v2::CoreApplicationServiceStub::ApplicationStarted>();
-  call.request().set_cast_session_id(session_id_);
+  call.request().set_cast_session_id(runtime_application_->GetCastSessionId());
   std::move(call).InvokeAsync(base::BindOnce(
       [](cast::utils::GrpcStatusOr<cast::v2::ApplicationStartedResponse>
              response_or) {
@@ -220,7 +222,7 @@ void RuntimeApplicationPlatformGrpc::NotifyApplicationStopped(
 
   auto call = core_app_stub_->CreateCall<
       cast::v2::CoreApplicationServiceStub::ApplicationStopped>();
-  call.request().set_cast_session_id(session_id_);
+  call.request().set_cast_session_id(runtime_application_->GetCastSessionId());
   call.request().set_stop_reason(stop_reason);
   call.request().set_error_code(net_error_code);
   std::move(call).InvokeAsync(base::BindOnce(
@@ -241,7 +243,7 @@ void RuntimeApplicationPlatformGrpc::NotifyMediaPlaybackChanged(bool playing) {
 
   auto call = core_app_stub_->CreateCall<
       cast::v2::CoreApplicationServiceStub::MediaPlaybackChanged>();
-  call.request().set_cast_session_id(session_id_);
+  call.request().set_cast_session_id(runtime_application_->GetCastSessionId());
   call.request().set_media_playback_state(
       playing ? cast::common::MediaPlaybackState::PLAYING
               : cast::common::MediaPlaybackState::STOPPED);
@@ -254,8 +256,8 @@ void RuntimeApplicationPlatformGrpc::NotifyMediaPlaybackChanged(bool playing) {
       }));
 }
 
-void RuntimeApplicationPlatformGrpc::GetAllBindingsAsync(
-    GetAllBindingsCB callback) {
+void RuntimeApplicationPlatformGrpc::GetAllBindings(
+    GetAllBindingsCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(core_message_port_app_stub_);
   auto call = core_message_port_app_stub_->CreateCall<
@@ -284,13 +286,22 @@ RuntimeApplicationPlatformGrpc::CreateWebUIControllerFactory(
 }
 
 void RuntimeApplicationPlatformGrpc::OnAllBindingsReceived(
-    GetAllBindingsCB callback,
+    GetAllBindingsCallback callback,
     cast::utils::GrpcStatusOr<cast::bindings::GetAllResponse> response_or) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!response_or.ok()) {
-    std::move(callback).Run(absl::nullopt);
-  } else {
-    std::move(callback).Run(std::move(response_or).value());
+    std::move(callback).Run(false, std::vector<std::string>());
+    return;
   }
+
+  const cast::bindings::GetAllResponse& response = response_or.value();
+  std::vector<std::string> bindings;
+  bindings.reserve(response.bindings_size());
+  for (int i = 0; i < response.bindings_size(); ++i) {
+    bindings.emplace_back(response.bindings(i).before_load_script());
+  }
+
+  std::move(callback).Run(true, std::move(bindings));
 }
 
 }  // namespace chromecast
