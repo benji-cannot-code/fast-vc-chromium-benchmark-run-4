1fdda21ce6c757f9e09c13197f612b2d9a36bb49
Gauthier Ambard <gambard@chromium.org>
2022-10-21 15:43:59 +0000

    [iOS] Update omnibox trailing button color
    To have it consistent with the color of the badges.
    
    Bug: 1315544
    Change-Id: Ib9c7ae78da4fe547b407b821023c17f65a7a7ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968539
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062156}

--- ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check_op.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/elements/extended_touch_target_button.h"
+#import "ios/chrome/browser/ui/icons/chrome_symbol.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_constants.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
 #import "ios/chrome/browser/ui/util/dynamic_type_util.h"
@@ -97,7 +98,11 @@ const CGFloat kLocationLabelVerticalOffset = -1;
 
   scheme.fontColor = [UIColor colorNamed:kTextPrimaryColor];
   scheme.placeholderColor = [UIColor colorNamed:kTextfieldPlaceholderColor];
-  scheme.trailingButtonColor = [UIColor colorNamed:kGrey500Color];
+  if (UseSymbols()) {
+    scheme.trailingButtonColor = [UIColor colorNamed:kGrey600Color];
+  } else {
+    scheme.trailingButtonColor = [UIColor colorNamed:kGrey500Color];
+  }
 
   return scheme;
 }
