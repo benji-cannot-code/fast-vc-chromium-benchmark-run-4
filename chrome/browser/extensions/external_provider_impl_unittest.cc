bad5f13edf6b1b7293ff2437a541d59155b213d9
Glen Robertson <glenrob@chromium.org>
2022-10-21 12:15:07 +0000

    Disable ExternalProviderImplTest broken on official build
    These tests were broken by changes to the web app testing infrastructure
    (crrev.com/3954727) and it will be easier to fix forward rather than
    rolling back that CL.
    
    Bug: 1377191
    Change-Id: I7e27f2d9039734d3586a919dca212bfbfbc4a208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3970341
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062075}

--- chrome/browser/extensions/external_provider_impl_unittest.cc
+++ chrome/browser/extensions/external_provider_impl_unittest.cc
@@ -252,7 +252,8 @@ class ExternalProviderImplTest : public ExtensionServiceTestBase {
 }  // namespace
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-TEST_F(ExternalProviderImplTest, InAppPayments) {
+// TODO(crbug.com/1377191): Re-enable this test
+TEST_F(ExternalProviderImplTest, DISABLED_InAppPayments) {
   InitServiceWithExternalProviders();
 
   AwaitCheckForExternalUpdates();
@@ -270,7 +271,8 @@ TEST_F(ExternalProviderImplTest, BlockedExternalUserProviders) {
   EXPECT_FALSE(registry()->GetInstalledExtension(kExternalAppId));
 }
 
-TEST_F(ExternalProviderImplTest, NotBlockedExternalUserProviders) {
+// TODO(crbug.com/1377191): Re-enable this test
+TEST_F(ExternalProviderImplTest, DISABLED_NotBlockedExternalUserProviders) {
   OverrideExternalExtensionsPath();
   InitServiceWithExternalProviders(false);
 
