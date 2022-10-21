91d283297bda66a184c12a9f26f2634795426d5d
Marijn Kruisselbrink <mek@chromium.org>
2022-10-21 00:24:52 +0000

    On Mac store file and protocol handler info in AppShimRegistry.
    When creating app shims we need this information for all profiles
    an app is installed in, so store it keyed by profile in AppShimRegistry.
    Follow up CLS will make use of this info.
    
    Bug: 1368650, 1225380, 1377008
    Change-Id: I53d6dd2af0702c01ff98779808608f31d1965ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961869
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061913}

--- chrome/browser/web_applications/app_shim_registry_mac.h
+++ chrome/browser/web_applications/app_shim_registry_mac.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_APP_SHIM_REGISTRY_MAC_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_APP_SHIM_REGISTRY_MAC_H_
 
+#include <map>
 #include <set>
 #include <string>
 
@@ -67,6 +68,42 @@ class AppShimRegistry {
   std::set<std::string> GetInstalledAppsForProfile(
       const base::FilePath& profile) const;
 
+  // Called when the file and/or protocol handlers for an app are updated in a
+  // specific profile. Used to calculate the union of all handlers for a app
+  // when updating the app shim.
+  void SaveFileHandlersForAppAndProfile(
+      const std::string& app_id,
+      const base::FilePath& profile,
+      std::set<std::string> file_handler_extensions,
+      std::set<std::string> file_handler_mime_types);
+  void SaveProtocolHandlersForAppAndProfile(
+      const std::string& app_id,
+      const base::FilePath& profile,
+      std::set<std::string> protocol_handlers);
+
+  struct HandlerInfo {
+    HandlerInfo();
+    ~HandlerInfo();
+    HandlerInfo(HandlerInfo&&);
+    HandlerInfo(const HandlerInfo&);
+    HandlerInfo& operator=(HandlerInfo&&);
+    HandlerInfo& operator=(const HandlerInfo&);
+
+    bool IsEmpty() const {
+      return file_handler_extensions.empty() &&
+             file_handler_mime_types.empty() && protocol_handlers.empty();
+    }
+
+    std::set<std::string> file_handler_extensions;
+    std::set<std::string> file_handler_mime_types;
+    std::set<std::string> protocol_handlers;
+  };
+
+  // Returns all the file and protocol handlers for the given app, keyed by
+  // profile path.
+  std::map<base::FilePath, HandlerInfo> GetHandlersForApp(
+      const std::string& app_id);
+
   // Helper functions for testing.
   void SetPrefServiceAndUserDataDirForTesting(
       PrefService* pref_service,
@@ -96,7 +133,8 @@ class AppShimRegistry {
   // |app_id|.
   void SetAppInfo(const std::string& app_id,
                   const std::set<base::FilePath>* installed_profiles,
-                  const std::set<base::FilePath>* last_active_profiles);
+                  const std::set<base::FilePath>* last_active_profiles,
+                  const std::map<base::FilePath, HandlerInfo>* handlers);
 
   raw_ptr<PrefService> override_pref_service_ = nullptr;
   base::FilePath override_user_data_dir_;
--- chrome/browser/web_applications/os_integration/web_app_file_handler_manager.cc
+++ chrome/browser/web_applications/os_integration/web_app_file_handler_manager.cc
@@ -103,14 +103,6 @@ void WebAppFileHandlerManager::EnableAndRegisterOsFileHandlers(
     return;
   }
 
-#if BUILDFLAG(IS_MAC)
-  std::move(callback).Run(Result::kOk);
-#else
-  // File handler registration is done via shortcuts creation on MacOS,
-  // WebAppShortcutManager::BuildShortcutInfoForWebApp collects file handler
-  // information to shortcut_info->file_handler_extensions, then used by MacOS
-  // implementation of |internals::CreatePlatformShortcuts|. So we avoid
-  // creating shortcuts twice here.
   const apps::FileHandlers* file_handlers = GetEnabledFileHandlers(app_id);
   if (file_handlers) {
     RegisterFileHandlersWithOs(app_id, GetRegistrar()->GetAppShortName(app_id),
@@ -119,7 +111,6 @@ void WebAppFileHandlerManager::EnableAndRegisterOsFileHandlers(
     // No file handlers registered.
     std::move(callback).Run(Result::kOk);
   }
-#endif
 }
 
 void WebAppFileHandlerManager::DisableAndUnregisterOsFileHandlers(
@@ -148,19 +139,7 @@ void WebAppFileHandlerManager::DisableAndUnregisterOsFileHandlers(
     return;
   }
 
-  // File handler information is embedded in the shortcut, when
-  // |DeleteSharedAppShims| is called in
-  // |OsIntegrationManager::UninstallOsHooks|, file handlers are also
-  // unregistered.
-#if BUILDFLAG(IS_MAC)
-  // When updating file handlers, |callback| here triggers the registering of
-  // the new file handlers. It is therefore important that |callback| not be
-  // dropped on the floor.
-  // https://crbug.com/1201993
-  std::move(callback).Run(Result::kOk);
-#else
   UnregisterFileHandlersWithOs(app_id, profile_, std::move(callback));
-#endif
 }
 
 const apps::FileHandlers* WebAppFileHandlerManager::GetEnabledFileHandlers(
