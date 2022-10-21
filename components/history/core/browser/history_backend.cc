56a82569d16dc839fb36aa1fbd879c206842b74c
Tommy C. Li <tommycli@chromium.org>
2022-10-21 23:35:26 +0000

    [history] Add some TRACE_EVENT0 and transaction begin/commit UMAs
    Adds some missing TRACE_EVENT0 statements for some methods around
    transactions that we are interested in.
    
    Also adds two boolean UMAs to track how often transaction begin or
    commit attempts fail in the wild.
    
    We suspect they led to inadverted nested transactions in the past.
    
    This CL is a followup to:
    https://chromium-review.googlesource.com/c/chromium/src/+/3966274
    
    Bug: 1321483, 1367850, 1354173
    Change-Id: Icf947f0f4d68a18fbd77ca44a324f60d1a46b26e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971582
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062406}

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -2663,6 +2663,7 @@ void HistoryBackend::SendFaviconChangedNotificationForIconURL(
 }
 
 void HistoryBackend::Commit() {
+  TRACE_EVENT0("browser", "HistoryBackend::Commit");
   if (!db_)
     return;
 
@@ -2690,6 +2691,7 @@ void HistoryBackend::Commit() {
 }
 
 void HistoryBackend::ScheduleCommit() {
+  TRACE_EVENT0("browser", "HistoryBackend::ScheduleCommit");
   // Non-cancelled means there's an already scheduled commit. Note that
   // CancelableOnceClosure starts cancelled with the default constructor.
   if (!scheduled_commit_.IsCancelled())
@@ -2703,6 +2705,7 @@ void HistoryBackend::ScheduleCommit() {
 }
 
 void HistoryBackend::CancelScheduledCommit() {
+  TRACE_EVENT0("browser", "HistoryBackend::CancelScheduledCommit");
   scheduled_commit_.Cancel();
 }
 
@@ -2741,6 +2744,7 @@ void HistoryBackend::ProcessDBTaskImpl() {
 }
 
 void HistoryBackend::BeginSingletonTransaction() {
+  TRACE_EVENT0("browser", "HistoryBackend::BeginSingletonTransaction");
   // TODO(crbug.com/1321483): Convert to DCHECKs after sussing out all the
   // transaction bugs in History.
   CHECK(!singleton_transaction_);
@@ -2748,7 +2752,9 @@ void HistoryBackend::BeginSingletonTransaction() {
   CHECK_EQ(db_->transaction_nesting(), 0);
   singleton_transaction_ = db_->CreateTransaction();
 
-  if (singleton_transaction_->Begin()) {
+  bool success = singleton_transaction_->Begin();
+  UMA_HISTOGRAM_BOOLEAN("History.Backend.TransactionBeginSuccess", success);
+  if (success) {
     CHECK_EQ(db_->transaction_nesting(), 1);
   } else {
     // Failing to begin the transaction is weird but it's not the end of the
@@ -2765,6 +2771,8 @@ void HistoryBackend::BeginSingletonTransaction() {
 }
 
 void HistoryBackend::CommitSingletonTransactionIfItExists() {
+  TRACE_EVENT0("browser",
+               "HistoryBackend::CommitSingletonTransactionIfItExists");
   // This can happen if the transaction was not successfully started.
   // TODO(crbug.com/1321483): Convert to DCHECKs after sussing out all the
   // transaction bugs in History.
@@ -2776,7 +2784,10 @@ void HistoryBackend::CommitSingletonTransactionIfItExists() {
 
   CHECK_EQ(db_->transaction_nesting(), 1)
       << "Someone opened multiple transactions.";
-  if (singleton_transaction_->Commit()) {
+
+  bool success = singleton_transaction_->Commit();
+  UMA_HISTOGRAM_BOOLEAN("History.Backend.TransactionCommitSuccess", success);
+  if (success) {
     CHECK_EQ(db_->transaction_nesting(), 0)
         << "Someone left a transaction open.";
   } else {
@@ -2993,6 +3004,7 @@ void HistoryBackend::DatabaseErrorCallback(int error, sql::Statement* stmt) {
 }
 
 void HistoryBackend::KillHistoryDatabase() {
+  TRACE_EVENT0("browser", "HistoryBackend::KillHistoryDatabase");
   scheduled_kill_db_ = false;
   if (!db_)
     return;
