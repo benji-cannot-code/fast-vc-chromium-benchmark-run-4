2e657214a3771501ff0f38e8a278711c97cb6601
Sorin Jianu <sorin@chromium.org>
2022-10-22 01:49:52 +0000

    Updater: removed deprecated ThreadTaskRunnerHandle calls.
    Fixed: 1377553
    Change-Id: Ie48c9dbbe580c80fafde7348bd2b1ed53ba079a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971708
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062447}

--- chrome/updater/update_service_impl.cc
+++ chrome/updater/update_service_impl.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/version.h"
 #include "chrome/updater/auto_run_on_os_upgrade_task.h"
--- chrome/updater/win/app_install_controller.cc
+++ chrome/updater/win/app_install_controller.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_checker.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "base/win/atl.h"
@@ -625,7 +624,6 @@ void AppInstallControllerImpl::InstallApp(
     const std::string& app_name,
     base::OnceCallback<void(int)> callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(base::ThreadTaskRunnerHandle::IsSet());
 
   app_id_ = app_id;
   app_name_ = base::UTF8ToUTF16(app_name);
@@ -678,7 +676,6 @@ void AppInstallControllerImpl::InstallAppOffline(
     const std::string& app_name,
     base::OnceCallback<void(int)> callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(base::ThreadTaskRunnerHandle::IsSet());
 
   app_id_ = app_id;
   app_name_ = base::UTF8ToUTF16(app_name);
