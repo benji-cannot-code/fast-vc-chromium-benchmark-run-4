76cd22244e4da7d7fd45226c0eac4af391076fc3
Patrick Noland <pnoland@google.com>
2022-10-22 18:06:20 +0000

    Preload overscroll glow resource
    Loading this resource during a scroll can cause jank, but we can preload
    it asynchronously using existing machinery.
    
    Bug: 1376682
    Change-Id: I383d9f9357991f0daa9922479b2b89ea78cb0a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965067
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062514}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
@@ -28,6 +28,7 @@ import org.chromium.chrome.browser.compositor.layouts.LayoutProvider;
 import org.chromium.chrome.browser.compositor.layouts.LayoutRenderHost;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.compositor.resources.StaticResourcePreloads;
+import org.chromium.chrome.browser.compositor.resources.SystemResourcePreloads;
 import org.chromium.chrome.browser.externalnav.IntentWithRequestMetadataHandler;
 import org.chromium.chrome.browser.layouts.scene_layer.SceneLayer;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
@@ -638,6 +639,9 @@ public class CompositorView
             mResourceManager.preloadResources(AndroidResourceType.STATIC,
                     StaticResourcePreloads.getSynchronousResources(getContext()),
                     StaticResourcePreloads.getAsynchronousResources(getContext()));
+            mResourceManager.preloadResources(AndroidResourceType.SYSTEM,
+                    SystemResourcePreloads.getSynchronousResources(),
+                    SystemResourcePreloads.getAsynchronousResources());
             mPreloadedResources = true;
         }
 
