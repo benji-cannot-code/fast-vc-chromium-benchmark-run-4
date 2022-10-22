59b71c8726dcdc445af4b64bf3ac48f41de85788
Sergey Ulanov <sergeyu@chromium.org>
2022-10-22 00:54:50 +0000

    [Fuchsia] Avoid unnecessary sync tokens in video frames
    FuchsiaVideoDecoder was producing video frames with unverified sync
    tokens in the Mailbox. These tokens were verified before being
    submitted to the compositor, which slows down submission. But they
    are not really necessary, except the for the first frame.
    Updated the decoder to supply SyncToken only when necessary. Also,
    it now uses verified sync tokens, which ensures that the tokens don't
    need o be verified during submission.
    
    Bug: 1174339
    Change-Id: I0a8c0ab3eb42c52f3804fbf26fc6be9c54e61086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949466
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062437}

--- media/fuchsia/video/fuchsia_video_decoder.cc
+++ media/fuchsia/video/fuchsia_video_decoder.cc
@@ -124,6 +124,8 @@ class FuchsiaVideoDecoder::OutputMailbox {
         raster_context_provider_->SharedImageInterface()->CreateSharedImage(
             gmb.get(), nullptr, color_space, kTopLeft_GrSurfaceOrigin,
             kPremul_SkAlphaType, usage);
+    create_sync_token_ = raster_context_provider_->SharedImageInterface()
+                             ->GenVerifiedSyncToken();
   }
 
   OutputMailbox(const OutputMailbox&) = delete;
@@ -131,7 +133,7 @@ class FuchsiaVideoDecoder::OutputMailbox {
 
   ~OutputMailbox() {
     raster_context_provider_->SharedImageInterface()->DestroySharedImage(
-        sync_token_, mailbox_);
+        release_sync_token_, mailbox_);
   }
 
   const gpu::Mailbox& mailbox() { return mailbox_; }
@@ -152,8 +154,11 @@ class FuchsiaVideoDecoder::OutputMailbox {
 
     gpu::MailboxHolder mailboxes[VideoFrame::kMaxPlanes];
     mailboxes[0].mailbox = mailbox_;
-    mailboxes[0].sync_token = raster_context_provider_->SharedImageInterface()
-                                  ->GenUnverifiedSyncToken();
+
+    if (create_sync_token_.HasData()) {
+      mailboxes[0].sync_token = create_sync_token_;
+      create_sync_token_.Clear();
+    }
 
     auto frame = VideoFrame::WrapNativeTextures(
         pixel_format, mailboxes,
@@ -183,7 +188,7 @@ class FuchsiaVideoDecoder::OutputMailbox {
   void OnFrameDestroyed(const gpu::SyncToken& sync_token) {
     DCHECK(is_used_);
     is_used_ = false;
-    sync_token_ = sync_token;
+    release_sync_token_ = sync_token;
 
     if (!reuse_callback_) {
       // If the mailbox cannot be reused then we can just delete it.
@@ -192,13 +197,13 @@ class FuchsiaVideoDecoder::OutputMailbox {
     }
 
     raster_context_provider_->ContextSupport()->SignalSyncToken(
-        sync_token_,
+        release_sync_token_,
         BindToCurrentLoop(base::BindOnce(&OutputMailbox::OnSyncTokenSignaled,
                                          weak_factory_.GetWeakPtr())));
   }
 
   void OnSyncTokenSignaled() {
-    sync_token_.Clear();
+    release_sync_token_.Clear();
     std::move(reuse_callback_).Run();
   }
 
@@ -207,7 +212,9 @@ class FuchsiaVideoDecoder::OutputMailbox {
   gfx::Size size_;
 
   gpu::Mailbox mailbox_;
-  gpu::SyncToken sync_token_;
+
+  gpu::SyncToken create_sync_token_;
+  gpu::SyncToken release_sync_token_;
 
   // Set to true when the mailbox is referenced by a video frame.
   bool is_used_ = false;
