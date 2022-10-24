bd3204c46d263c907a3bab164bd42737ae9336df
Susanne Westphal <swestphal@google.com>
2022-10-24 11:41:31 +0000

    [DAL] Allow localhost for Origin Verification
    Localhost is treated as secure origin, so the digital asset link
    validation should be allowed too.
    
    Bug: 1376958
    Change-Id: Ib87c7b827a2246b23ac37351e10784efd6037dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3973218
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062756}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerOriginVerifierTest.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerOriginVerifierTest.java
@@ -46,6 +46,7 @@ public class WebLayerOriginVerifierTest {
     private int mUid = Process.myUid();
     private Origin mHttpsOrigin = Origin.create("https://www.example.com");
     private Origin mHttpOrigin = Origin.create("http://www.android.com");
+    private Origin mHttpLocalhostOrigin = Origin.create("http://localhost:1234/");
 
     private WebLayerOriginVerifier mHandleAllUrlsVerifier;
 
@@ -124,6 +125,16 @@ public class WebLayerOriginVerifierTest {
                 () -> mHandleAllUrlsVerifier.start(resultListener, null, mHttpOrigin));
         mVerificationResultLatch.await();
         Assert.assertFalse(resultListener.isVerified());
+    }
+
+    @Test
+    public void testHttpLocalhostVerificationAllowed() throws Exception {
+        TestOriginVerificationListener resultListener =
+                new TestOriginVerificationListener(mVerificationResultLatch);
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> mHandleAllUrlsVerifier.start(resultListener, null, mHttpLocalhostOrigin));
+        mVerificationResultLatch.await();
+        Assert.assertTrue(resultListener.isVerified());
     }
 
     @Test
