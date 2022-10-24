ab4bfdd256e0563a6ea4ed0d3389b46aa0a3be0e
Keith Lee <keithlee@chromium.org>
2022-10-24 06:19:07 +0000

    Revert "Add a flag to allow JP settings data to be migrated"
    This reverts commit b62f6800fcfa40b1cf5d523cc5d50fd82f37a238.
    
    Reason for revert: I have changed the design to make this involve a single large flag rather than 3 separate flags. It just seems simpler to do it that way. go/e14s-jp-sys-pk-v2
    
    
    Original change's description:
    > Add a flag to allow JP settings data to be migrated
    >
    > This will just be a logic change, there should be no impact on
    > the user.
    >
    > This just makes it so I can turn on the flag and have the extension read it via "sysemjapanesesettingsmigration".
    >
    > Bug: b/250997017
    > Change-Id: I78ba6a3bd517f332cfd9e9d382df459bb22edc75
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960613
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Keith Lee <keithlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1061384}
    
    Bug: b/250997017
    Change-Id: If52d132ab3dfaa19339f9f1d9605bdcc56aa898a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971643
    Auto-Submit: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062674}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3212,9 +3212,6 @@ extern const char kImeAssistPersonalInfoDescription[];
 extern const char kVirtualKeyboardNewHeaderName[];
 extern const char kVirtualKeyboardNewHeaderDescription[];
 
-extern const char kCrosJpSysPkSetingsMigrateName[];
-extern const char kCrosJpSysPkSetingsMigrateDescription[];
-
 extern const char kHindiInscriptLayoutName[];
 extern const char kHindiInscriptLayoutDescription[];
 
