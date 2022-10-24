3e8f062a21953c84b26507e51bb90ba30eb5b513
andrewxu <andrewxu@chromium.org>
2022-10-24 01:53:42 +0000

    [Pixel Diff Ash] Create a new demo test case
    The current demo test case uses `ComparePrimaryFullScreen()`, a
    discouraged API. This CL creates a new demo test case to use
    `CompareUiComponentsOnPrimaryScreen()` instead.
    
    The new test case verifies the screenshot of solid-colored widgets
    added in test code so it does not depend on any ash UI feature.
    
    Attach the benchmark screenshot to the crbug issue.
    
    Bug: 1351444
    Change-Id: Ic3ddb72dedfc8eb1abce350d9deab03b9667711a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968644
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062630}

--- ash/test/ash_pixel_diff_test_helper.h
+++ ash/test/ash_pixel_diff_test_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/view_skia_gold_pixel_diff.h"
 
 namespace ash {
-class DemoAshPixelDiffTest;
 
 // A helper class that provides utility functions for performing pixel diff
 // tests via the Skia Gold.
@@ -26,15 +25,15 @@ class AshPixelDiffTestHelper {
   AshPixelDiffTestHelper& operator=(const AshPixelDiffTestHelper&) = delete;
   ~AshPixelDiffTestHelper();
 
-  // Similar to `ComparePrimaryFullScreen()` but with the difference that only
-  // the pixels within the screen bounds of `ui_components` are compared. The
-  // function caller has the duty to choose suitable `ui_components` in their
-  // tests to avoid unnecessary pixel comparisons. Otherwise, pixel tests could
-  // be fragile to the changes in production code.
-  // `ui_components` is a variadic argument list, consisting of view pointers,
-  // widget pointers or window pointers. `ui_components` can have the pointers
-  // of different categories.
-  // Example usages:
+  // Takes a full primary screenshot then compares it with the benchmark image
+  // specified by `screenshot_name`. Only the pixels within the screen bounds of
+  // `ui_components` affect the comparison result. Returns the comparison
+  // result. The function caller has the duty to choose suitable `ui_components`
+  // in their tests to avoid unnecessary pixel comparisons. Otherwise, pixel
+  // tests could be fragile to the changes in production code. `ui_components`
+  // is a variadic argument list, consisting of view pointers, widget pointers
+  // or window pointers. `ui_components` can have the pointers of different
+  // categories. Example usages:
   //
   //  views::View* view_ptr = ...;
   //  views::Widget* widget_ptr = ...;
@@ -56,11 +55,8 @@ class AshPixelDiffTestHelper {
   }
 
  private:
-  friend DemoAshPixelDiffTest;
-
-  // Takes a screenshot of the primary fullscreen then uploads it to the Skia
-  // Gold to perform pixel comparison. `screenshot_name` specifies the benchmark
-  // image. Returns the comparison result.
+  // Similar to `CompareUiComponentsOnPrimaryScreen()` but with the difference
+  // that every pixel on the screen is compared.
   // NOTE: use this function only when necessary. Otherwise, a tiny UI change
   // may break many pixel tests.
   bool ComparePrimaryFullScreen(const std::string& screenshot_name);
