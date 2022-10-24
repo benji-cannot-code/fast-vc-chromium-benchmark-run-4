e60470beaa70e227d01e07a33ab9449f8f69f7c4
adamta <adamta@google.com>
2022-10-24 17:49:33 +0000

    [iOS] Set up Following feed sort type experiment
    Bug: 1377422
    Change-Id: I59fe0dde32900809e1a3e943fcb7d1d878ad40f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972240
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062891}

--- ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
@@ -34,6 +34,10 @@ BASE_FEATURE(kEnableDiscoverFeedTopSyncPromo,
              "EnableDiscoverFeedTopSyncPromo",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kFollowingFeedDefaultSortType,
+             "FollowingFeedDefaultSortType",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 const char kDiscoverFeedSRSReconstructedTemplatesEnabled[] =
     "DiscoverFeedSRSReconstructedTemplatesEnabled";
 
@@ -44,9 +48,13 @@ BASE_FEATURE(kNTPViewHierarchyRepair,
              "NTPViewHierarchyRepair",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-const char kDiscoverFeedTopSyncPromoStyleParam[] = "FeedTopPromoStyle";
 const char kDiscoverFeedTopSyncPromoStyleFullWithTitle[] = "fullWithTitle";
 const char kDiscoverFeedTopSyncPromoStyleCompact[] = "compact";
+
+const char kFollowingFeedDefaultSortTypeSortByLatest[] = "SortByLatest";
+const char kFollowingFeedDefaultSortTypeGroupedByPublisher[] =
+    "GroupedByPublisher";
+
 BASE_FEATURE(kEnableFeedAblation,
              "FeedAblationEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -79,6 +87,16 @@ bool IsDiscoverFeedTopSyncPromoCompact() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kEnableDiscoverFeedTopSyncPromo, kDiscoverFeedTopSyncPromoStyleCompact,
       false);
+}
+
+bool IsFollowingFeedDefaultSortTypeEnabled() {
+  return base::FeatureList::IsEnabled(kFollowingFeedDefaultSortType);
+}
+
+bool IsDefaultFollowingFeedSortTypeGroupedByPublisher() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kFollowingFeedDefaultSortType,
+      kFollowingFeedDefaultSortTypeGroupedByPublisher, true);
 }
 
 bool IsFeedAblationEnabled() {
