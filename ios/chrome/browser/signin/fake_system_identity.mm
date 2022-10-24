ccdeec0097c89d3b3ad0ce3418ef57f69e5ff235
Jérôme Lebel <jlebel@chromium.org>
2022-10-24 12:42:11 +0000

    [iOS] Add -[FakeSystemIdentity description]
    Change-Id: I63ea39b5b63a6af06eff974d8c31636e87199c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972417
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062772}

--- ios/chrome/browser/signin/fake_system_identity.mm
+++ ios/chrome/browser/signin/fake_system_identity.mm
@@ -142,6 +142,13 @@ NSString* const kCoderHashedGaiaIDKey = @"HashedGaiaID";
   return self;
 }
 
+- (NSString*)description {
+  return [NSString stringWithFormat:@"<%@: %p, GaiaID: \"%@\", name: \"%@\", "
+                                    @"email: \"%@\">",
+                                    self.class.description, self, self.gaiaID,
+                                    self.userFullName, self.userEmail];
+}
+
 + (BOOL)supportsSecureCoding {
   return YES;
 }
