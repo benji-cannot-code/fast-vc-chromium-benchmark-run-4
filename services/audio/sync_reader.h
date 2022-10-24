9dfe018e8639bfe3e97fb44a31cedcc462c58f87
Fredrik Hernqvist <fhernqvist@google.com>
2022-10-24 11:51:37 +0000

    Sample AudioRendererMissedDeadline stats in samples of 10 seconds.
    It also moves Media.AudioRendererAudioGlitches into
    Media.AudioRendererAudioGlitches2.
    
    This obsoletes these histograms:
    Media.AudioRendererMissedDeadline
    Media.AudioRendererMissedDeadline2.{Mixing/}.{Long/Short}.{Latency}
    Media.AudioRendererAudioGlitches
    
    And adds these histograms:
    Media.AudioRendererMissedDeadline3.{Mixing/}.{Intervals/Short}.{Latency}
    Media.AudioRendererAudioGlitches2
    
    Fixed: 1366713
    Fixed: 1366715
    Change-Id: Ie02bc20e1af2daa1da49873d052fc02a2865065d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942189
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062758}

--- services/audio/sync_reader.h
+++ services/audio/sync_reader.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_bus.h"
 #include "media/base/audio_latency.h"
 #include "services/audio/output_controller.h"
+#include "services/audio/output_glitch_counter.h"
 
 namespace audio {
 
@@ -36,6 +37,12 @@ class SyncReader : public OutputController::SyncReader {
   SyncReader(base::RepeatingCallback<void(const std::string&)> log_callback,
              const media::AudioParameters& params,
              base::CancelableSyncSocket* foreign_socket);
+
+  // Allows us to set a custom OutputGlitchCounter for testing.
+  SyncReader(base::RepeatingCallback<void(const std::string&)> log_callback,
+             const media::AudioParameters& params,
+             base::CancelableSyncSocket* foreign_socket,
+             std::unique_ptr<OutputGlitchCounter> glitch_counter);
 
   SyncReader(const SyncReader&) = delete;
   SyncReader& operator=(const SyncReader&) = delete;
@@ -90,14 +97,8 @@ class SyncReader : public OutputController::SyncReader {
   // Shared memory wrapper used for transferring audio data to Read() callers.
   std::unique_ptr<media::AudioBus> output_bus_;
 
-  // Track the number of times the renderer missed its real-time deadline and
-  // report a UMA stat during destruction.
-  size_t renderer_callback_count_{0};
-  size_t renderer_missed_callback_count_{0};
-  size_t trailing_renderer_missed_callback_count_{0};
-  size_t mixing_renderer_callback_count_{0};
-  size_t mixing_renderer_missed_callback_count_{0};
-  size_t mixing_trailing_renderer_missed_callback_count_{0};
+  // Used for logging.
+  size_t renderer_missed_callback_count_ = 0;
 
   // The maximum amount of time to wait for data from the renderer.  Calculated
   // from the parameters given at construction.
@@ -107,6 +108,8 @@ class SyncReader : public OutputController::SyncReader {
   // The index of the audio buffer we're expecting to be sent from the renderer;
   // used to block with timeout for audio data.
   uint32_t buffer_index_{0};
+
+  std::unique_ptr<OutputGlitchCounter> glitch_counter_;
 };
 
 }  // namespace audio
