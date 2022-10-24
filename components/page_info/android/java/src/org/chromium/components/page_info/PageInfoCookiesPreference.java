13c2463d7802d062c434b48af8ba4e833e45bf4c
Nicola Tommasi <tommasin@chromium.org>
2022-10-24 20:50:01 +0000

    [FPS][Android] Fix NPE in PageInfoCookiesPreference
    This CL fixes the NullPointerException detected in the related bug.
    
    Bug: 1377784
    Change-Id: Idc4052c4e5d46f86e7d0158ee532a5653519e62a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976328
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062991}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesPreference.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesPreference.java
@@ -178,7 +178,7 @@ public class PageInfoCookiesPreference extends SiteSettingsPreferenceFragment {
      */
     public boolean maybeShowFPSInfo(FPSCookieInfo fpsInfo, String currentOrigin) {
         mFPSInfo = fpsInfo;
-        if (fpsInfo == null) {
+        if (fpsInfo == null || mFPSInUse == null) {
             return false;
         }
 
