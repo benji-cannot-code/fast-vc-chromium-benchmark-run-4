67c527542ac1d4389befaf8c161985bd04da7f98
Victor-Gabriel Savu <vsavu@google.com>
2022-10-24 13:17:06 +0000

    Add error page and progress reporting
    Add support for enabling the error page. Allow the progress bar
    value to be updated.
    
    Bug: b:254421029
    Change-Id: I5d1a0803e41bc0e1998bbe2caf6e51cc35f06a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967781
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Jana Grill <janagrill@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1062781}

--- chrome/browser/resources/chromeos/login/screens/login/lacros_data_backward_migration.js
+++ chrome/browser/resources/chromeos/login/screens/login/lacros_data_backward_migration.js
@@ -40,6 +40,7 @@ const LacrosDataBackwardMigrationScreenElementBase = mixinBehaviors(
     ],
     PolymerElement);
 
+/** @polymer */
 class LacrosDataBackwardMigrationScreen extends
     LacrosDataBackwardMigrationScreenElementBase {
   static get is() {
@@ -66,7 +67,30 @@ class LacrosDataBackwardMigrationScreen extends
   get UI_STEPS() {
     return {
       PROGRESS: 'progress',
+      ERROR: 'error',
     };
+  }
+
+  get EXTERNAL_API() {
+    return [
+      'setProgressValue',
+      'setFailureStatus',
+    ];
+  }
+
+  /**
+   * Called when the migration failed.
+  */
+  setFailureStatus() {
+    this.setUIStep('error');
+  }
+
+  /**
+   * Called to update the progress of data migration.
+   * @param {number} progress Percentage of data copied so far.
+   */
+  setProgressValue(progress) {
+    this.progressValue_ = progress;
   }
 
   ready() {
