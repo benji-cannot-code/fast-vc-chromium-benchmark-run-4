9412bdd3770c08f58ef7b086fb37383690a84988
Kevin McNee <mcnee@chromium.org>
2022-10-24 09:27:42 +0000

    Migrate DlpContentTabHelperBrowserTest off of NotificationService
    We convert the use of NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME to the
    test helper CreateAndLoadWebContentsObserver. Technically, this waits
    for the entire WebContents to load instead of just the primary main
    frame, but the distinction does not seem to matter for this test.
    
    Bug: 1174761
    Change-Id: I9c581fda0c31f6f228a075fb3edc754ce22e3d7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966907
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062718}

--- chrome/browser/chromeos/policy/dlp/dlp_content_tab_helper_browsertest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_content_tab_helper_browsertest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
@@ -58,9 +57,7 @@ IN_PROC_BROWSER_TEST_F(DlpContentTabHelperBrowserTest, PlatformApp) {
   ExtensionTestMessageListener launched_listener("Launched");
 
   // Install Platform App
-  content::WindowedNotificationObserver app_loaded_observer(
-      content::NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME,
-      content::NotificationService::AllSources());
+  content::CreateAndLoadWebContentsObserver app_loaded_observer;
   const extensions::Extension* extension = InstallPlatformApp("dlp_test");
   ASSERT_TRUE(extension);
 
