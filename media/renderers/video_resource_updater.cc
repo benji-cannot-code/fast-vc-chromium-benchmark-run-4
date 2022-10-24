72d8205ae0ce68afbaea1647618f859845b26555
Sergey Ulanov <sergeyu@chromium.org>
2022-10-24 18:39:22 +0000

    Avoid adding sync tokens in VideoResourceUpdater::ReturnTexture
    VideoResourceUpdater::ReturnTexture() was calling
    video_frame->UpdateReleaseSyncToken() even when the compositor returned
    the texture without a sync_token. As result a new sync token was produced
    by SyncTokenClientImpl. That new sync token was delaying submittion of
    the new frame to the compositor because VideoFrameSinkBundle always
    returns old frame before submitting next one.
    
    Bug: 1174339
    Change-Id: Id7ef25d26ec72096a79cc7641ae4b6d0385a14d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972181
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062915}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -1343,6 +1343,9 @@ void VideoResourceUpdater::ReturnTexture(scoped_refptr<VideoFrame> video_frame,
   if (lost_resource)
     return;
 
+  if (!sync_token.HasData())
+    return;
+
   // The video frame will insert a wait on the previous release sync token.
   SyncTokenClientImpl client(
       ContextGL(), nullptr /* gpu::SharedImageInterface* */, sync_token);
