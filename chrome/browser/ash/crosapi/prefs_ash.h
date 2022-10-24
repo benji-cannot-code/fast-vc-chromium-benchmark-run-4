a446e18fa7e7359c3ac5b6b3ddaab4049d51f883
David Roger <droger@chromium.org>
2022-10-24 02:01:04 +0000

    [Profiles] Unregister ProfileManagerObserver in /chrome/browser/ash/crosapi
    This is in preparation for adding the `check_empty` parameter to the
    observer list.
    This ensures there is no UaF when observers try to unregister after
    the profile manager was destroyed.
    
    This CL was uploaded by git cl split.
    
    R=erikchen@chromium.org
    
    Bug: 1363063
    Change-Id: I88208b4299608f63f681e2d1bb3d9a51ab39b74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963232
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062632}

--- chrome/browser/ash/crosapi/prefs_ash.h
+++ chrome/browser/ash/crosapi/prefs_ash.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/profiles/profile_observer.h"
 #include "chromeos/crosapi/mojom/prefs.mojom.h"
@@ -29,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefService;
 class PrefChangeRegistrar;
-class ProfileManager;
 
 namespace crosapi {
 
@@ -91,9 +91,6 @@ class PrefsAsh : public mojom::Prefs,
 
   void OnAppTerminating();
 
-  // In production, owned by g_browser_process, which does not outlives this
-  // object.
-  ProfileManager* profile_manager_;
   // In production, owned by g_browser_process, which outlives this object.
   PrefService* const local_state_;
 
@@ -109,6 +106,9 @@ class PrefsAsh : public mojom::Prefs,
 
   // Observe profile destruction to reset prefs observation.
   base::ScopedObservation<Profile, ProfileObserver> profile_observation_{this};
+
+  base::ScopedObservation<ProfileManager, ProfileManagerObserver>
+      profile_manager_observation_{this};
 
   base::CallbackListSubscription on_app_terminating_subscription_;
   // Map of extension pref paths to preference names.
