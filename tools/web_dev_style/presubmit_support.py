94b78f93ca11aee6a04ebc4d1ac128aa44180204
Rebekah Potter <rbpotter@chromium.org>
2022-10-24 18:29:28 +0000

    WebUI: Add a presubmit to catch new JS file additions
    Move logic from chrome/test/data/webui/settings/PRESUBMIT.py to a
    common utility in web_dev_style. Apply it to chrome/browser/resources,
    with necessary exceptions for WebUIs that are currently in JS, and
    use it in chrome/test/data/webui/settings/.
    
    This new check should be expanded to other WebUI folders in future CLs.
    
    Change-Id: I079ea09232d20d8f0f2b48eda53a6770812f174f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969024
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062907}

--- tools/web_dev_style/presubmit_support.py
+++ tools/web_dev_style/presubmit_support.py
@@ -8,6 +8,7 @@ from . import css_checker
 from . import html_checker
 from . import js_checker
 from . import resource_checker
+from . import added_js_files_check
 
 
 def IsResource(f):
@@ -42,3 +43,9 @@ def CheckStyleESLint(input_api, output_api):
 def DisallowIncludes(input_api, output_api, msg):
   return resource_checker.ResourceChecker(
       input_api, output_api, file_filter=IsResource).DisallowIncludes(msg)
+
+
+def DisallowNewJsFiles(input_api, output_api, file_filter=lambda f: True):
+  return added_js_files_check.AddedJsFilesCheck(input_api,
+                                                output_api,
+                                                file_filter=file_filter)
