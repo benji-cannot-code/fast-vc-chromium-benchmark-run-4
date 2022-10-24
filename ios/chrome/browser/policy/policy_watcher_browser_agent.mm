e8617674e7a2da90d5d6497497f9aaf456598e71
Jérôme Lebel <jlebel@chromium.org>
2022-10-24 20:25:32 +0000

    [iOS] Check value of weak_ptr before using it in SignOut callback
    Change-Id: Ia71ee3429709d27b57a31ad1ae43249942b41e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3974948
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062982}

--- ios/chrome/browser/policy/policy_watcher_browser_agent.mm
+++ ios/chrome/browser/policy/policy_watcher_browser_agent.mm
@@ -117,7 +117,9 @@ void PolicyWatcherBrowserAgent::ForceSignOutIfSigninDisabled() {
       auth_service_->SignOut(
           signin_metrics::ProfileSignout::SIGNOUT_PREF_CHANGED,
           /*force_clear_browsing_data=*/false, ^{
-            weak_ptr->OnSignOutComplete();
+            if (weak_ptr) {
+              weak_ptr->OnSignOutComplete();
+            }
           });
     }
 
