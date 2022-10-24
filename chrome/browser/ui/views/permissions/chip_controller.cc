77db9e32ed77f40cbb8894ca3d639d2ba13c3e98
Florian Jacky <fjacky@chromium.org>
2022-10-24 09:59:48 +0000

    Delay showing the request bubble once a confirmation chip is shown
    The previous fix only addressed the request/confirmation overlap when the request chip feature was enabled and used. This CL additionally delays the prompt bubble when request chip is disabled/not used (i.e. no user gesture), which was missed in the last fix.
    
    Bug: 1373596
    Change-Id: I4cace2ca1d7f154ad61b0f9b68fd046ac9880863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971508
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062728}

--- chrome/browser/ui/views/permissions/chip_controller.cc
+++ chrome/browser/ui/views/permissions/chip_controller.cc
@@ -178,13 +178,15 @@ bool ChipController::ShouldWaitForConfirmationToComplete() {
 
 void ChipController::InitializePermissionPrompt(
     content::WebContents* web_contents,
-    permissions::PermissionPrompt::Delegate* delegate) {
+    permissions::PermissionPrompt::Delegate* delegate,
+    base::OnceCallback<void()> callback) {
   DCHECK(delegate);
   if (ShouldWaitForConfirmationToComplete()) {
     base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
         FROM_HERE,
         base::BindOnce(&ChipController::InitializePermissionPrompt,
-                       weak_factory_.GetWeakPtr(), web_contents, delegate),
+                       weak_factory_.GetWeakPtr(), web_contents, delegate,
+                       std::move(callback)),
         collapse_timer_.GetCurrentDelay());
     return;
   }
@@ -206,6 +208,7 @@ void ChipController::InitializePermissionPrompt(
   active_chip_permission_request_manager_ =
       permissions::PermissionRequestManager::FromWebContents(web_contents);
   active_chip_permission_request_manager_.value()->AddObserver(this);
+  std::move(callback).Run();
 }
 
 void ChipController::ShowPermissionPrompt(
@@ -221,7 +224,7 @@ void ChipController::ShowPermissionPrompt(
     return;
   }
 
-  InitializePermissionPrompt(web_contents, delegate);
+  InitializePermissionPrompt(web_contents, delegate, base::DoNothing());
 
   request_chip_shown_time_ = base::TimeTicks::Now();
 
