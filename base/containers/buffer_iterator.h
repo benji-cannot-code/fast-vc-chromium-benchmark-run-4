2836469314e429079184e98d0a6cbee8fe719c0a
Jose Dapena Paz <jdapena@igalia.com>
2022-10-24 16:38:43 +0000

    IWYU: usage of memcpy in buffer_iterator.h requires declaration.
    Fix build issue:
        ../../base/containers/buffer_iterator.h:99:13: error: ‘memcpy’ was not declared in this scope, and no declarations were found by argument-dependent lookup at the point of instantiation [-fpermissive]
           99 |       memcpy(&t.emplace(), remaining_.data(), sizeof(T));
              |       ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Bug: 957519
    Change-Id: I1151e9c67694958cfdcfc0b2c7baae7d3095da07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966223
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1062850}

--- base/containers/buffer_iterator.h
+++ base/containers/buffer_iterator.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CONTAINERS_BUFFER_ITERATOR_H_
 #define BASE_CONTAINERS_BUFFER_ITERATOR_H_
 
+#include <string.h>
+
 #include <type_traits>
 
 #include "base/bit_cast.h"
