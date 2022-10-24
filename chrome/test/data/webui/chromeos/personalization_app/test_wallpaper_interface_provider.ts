cc7a58e842e55c5ec26d5a33ecba85c69009429d
Jason Thai <jasontt@chromium.org>
2022-10-24 17:45:33 +0000

    wallpaper: add option to force refresh for google photos
    Enhance google photos daily refresh by allowing the API
    |SelectGooglePhotosAlbum| to force refreshing to a new wallpaper or
    skip it.
    
    Also added some improvements for the loading UI regarding the Google
    Photos daily refresh.
    
    Demo: http://shortn/_aKSGsyoQJU
    
    BUG=b/247613591
    TEST=ash_unittests
    --gtest_filter="*WallpaperControllerGooglePhotosWallpaperTest.*"
    TEST=unit_tests
    --gtest_filter="*WallpaperProviderImplGooglePhotosTest.*
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia6d5312915b0e6f21cd4787f7e4d7451c0df30b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953266
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062887}

--- chrome/test/data/webui/chromeos/personalization_app/test_wallpaper_interface_provider.ts
+++ chrome/test/data/webui/chromeos/personalization_app/test_wallpaper_interface_provider.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CurrentWallpaper, DefaultImageSymbol, FetchGooglePhotosAlbumsResponse, FetchGooglePhotosPhotosResponse, GooglePhotosAlbum, GooglePhotosEnablementState, GooglePhotosPhoto, kDefaultImageSymbol, OnlineImageType, WallpaperCollection, WallpaperImage, WallpaperLayout, WallpaperObserverInterface, WallpaperObserverRemote, WallpaperProviderInterface, WallpaperType} from 'chrome://personalization/js/personalization_app.js';
+import {CurrentWallpaper, DefaultImageSymbol, FetchGooglePhotosAlbumsResponse, FetchGooglePhotosPhotosResponse, GooglePhotosAlbum, GooglePhotosEnablementState, GooglePhotosPhoto, kDefaultImageSymbol, OnlineImageType, SetDailyRefreshResponse, WallpaperCollection, WallpaperImage, WallpaperLayout, WallpaperObserverInterface, WallpaperObserverRemote, WallpaperProviderInterface, WallpaperType} from 'chrome://personalization/js/personalization_app.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {FilePath} from 'chrome://resources/mojo/mojo/public/mojom/base/file_path.mojom-webui.js';
 import {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
@@ -127,6 +127,10 @@ export class TestWallpaperProvider extends
   currentWallpaper: CurrentWallpaper;
   selectWallpaperResponse = true;
   selectGooglePhotosPhotoResponse = true;
+  selectGooglePhotosAlbumResponse: SetDailyRefreshResponse = {
+    success: true,
+    forceRefresh: true,
+  };
   selectDefaultImageResponse = true;
   selectLocalImageResponse = true;
   updateDailyRefreshWallpaperResponse = true;
@@ -239,7 +243,7 @@ export class TestWallpaperProvider extends
 
   selectGooglePhotosAlbum(id: string) {
     this.methodCalled('selectGooglePhotosAlbum', id);
-    return Promise.resolve({success: false});
+    return Promise.resolve({response: this.selectGooglePhotosAlbumResponse});
   }
 
   getGooglePhotosDailyRefreshAlbumId() {
