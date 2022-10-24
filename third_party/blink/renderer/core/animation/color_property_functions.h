d7bbefe258b1b553cc08bffbda12c8c87949b10b
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-10-24 17:25:05 +0000

    [StyleBuilder] Make color property readonly.
    Bug: 1377295
    Change-Id: I65484d3ee468a5d2e41caa7b9637684170a18ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3973351
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062882}

--- third_party/blink/renderer/core/animation/color_property_functions.h
+++ third_party/blink/renderer/core/animation/color_property_functions.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ComputedStyle;
+class ComputedStyleBuilder;
 class CSSProperty;
 
 struct OptionalStyleColor {
@@ -46,6 +47,7 @@ class ColorPropertyFunctions {
                                             const ComputedStyle&);
   static void SetUnvisitedColor(const CSSProperty&,
                                 ComputedStyle&,
+                                ComputedStyleBuilder&,
                                 const Color&);
   static void SetVisitedColor(const CSSProperty&, ComputedStyle&, const Color&);
 };
