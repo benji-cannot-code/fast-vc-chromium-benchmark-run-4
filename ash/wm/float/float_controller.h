9b5dc8bae51f384166c4c980537b5a6072940b10
sophiewen <sophiewen@chromium.org>
2022-10-24 23:15:50 +0000

    float: Refactor tucking window from FloatController
    Moves ScopedWindowTucker and TuckHandle to their own file.
    
    Fixed: b:254365116
    Test: manual + existing
    Change-Id: Iae1d65db5f3cc0f304814b179b8cf0162d8e3693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971050
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063060}

--- ash/wm/float/float_controller.h
+++ ash/wm/float/float_controller.h
@@ -137,8 +137,6 @@ class ASH_EXPORT FloatController : public TabletModeObserver,
 
  private:
   class FloatedWindowInfo;
-  class ScopedWindowTucker;
-  class TuckHandle;
   friend class DefaultState;
   friend class TabletModeWindowState;
   friend class WindowFloatTest;
