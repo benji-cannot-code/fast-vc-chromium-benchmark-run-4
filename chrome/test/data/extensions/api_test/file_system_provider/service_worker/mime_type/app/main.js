d09b1ca49e9fb8853fd531371450f128731eb2e0
Alexander Bolodurin <alexbn@chromium.org>
2022-10-24 04:00:24 +0000

    fileSystemProvider: port mime_type test to service workers
    There are two differences from the existing test other than the
    foreground-page/service-worker split:
    
    - The file-handling app is a separate Chrome app that sends the
      transformed launch event back to the testing extension as soon as it
      launches.
    - Entries are not converted to external entries, as this is not
      necessary anymore.
    
    Bug: b:235414571
    Change-Id: I03f512f8e401605d82f54d39e9c40deeefb90d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971138
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062651}

---
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/mime_type/app/main.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.app.runtime.onLaunched.addListener((/** @type{!Object} */ e) => {
+  // Send the transformed launch event back to the text extension as soon as
+  // this Chrome app is launched by it.
+  chrome.runtime.sendMessage('pkplfbidichfdicaijlchgnapepdginl', {
+    id: e.id,
+    items: (e.items || []).map(item => ({
+                                 type: item.type,
+                                 entryName: item.entry.name,
+                               })),
+  });
+});
