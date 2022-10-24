577cb2495601d3ab165898a3fc8f0781d095645e
David Bertoni <dbertoni@chromium.org>
2022-10-24 21:34:44 +0000

    [Extensions] Enable more webRequest API tests for service workers.
    This CL parameterizes the ExtensionDevToolsProtocolTest, along with a
    few tests that are trivially convertible.
    
    Bug: 1093066
    Change-Id: I34ce4b6ef742c5e878a09b186dc1d6e210205123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967095
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063008}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -529,7 +529,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
 }
 
 class ExtensionDevToolsProtocolTest
-    : public ExtensionWebRequestApiTest,
+    : public ExtensionWebRequestApiTestWithContextType,
       public content::TestDevToolsProtocolClient {
  protected:
   void Attach() { AttachToWebContents(web_contents()); }
@@ -544,7 +544,15 @@ class ExtensionDevToolsProtocolTest
   }
 };
 
-IN_PROC_BROWSER_TEST_F(ExtensionDevToolsProtocolTest,
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         ExtensionDevToolsProtocolTest,
+                         ::testing::Values(ContextType::kPersistentBackground));
+
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         ExtensionDevToolsProtocolTest,
+                         ::testing::Values(ContextType::kServiceWorker));
+
+IN_PROC_BROWSER_TEST_P(ExtensionDevToolsProtocolTest,
                        HeaderOverriddenByExtension) {
   Attach();
   ASSERT_TRUE(embedded_test_server()->Start());
@@ -603,7 +611,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionDevToolsProtocolTest,
   ASSERT_EQ(*cookie_value, "cookieValue");
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionDevToolsProtocolTest,
+IN_PROC_BROWSER_TEST_P(ExtensionDevToolsProtocolTest,
                        HeaderOverrideViaProtocolAllowedByExtension) {
   Attach();
   ASSERT_TRUE(embedded_test_server()->Start());
@@ -723,7 +731,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest, WebRequestTypes) {
 #else
 #define MAYBE_WebRequestTestOSDD WebRequestTestOSDD
 #endif
-IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiTest, MAYBE_WebRequestTestOSDD) {
+IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
+                       MAYBE_WebRequestTestOSDD) {
   // An OSDD request is only generated when a main frame at is loaded at /, so
   // serve osdd/index.html from the root of the test server:
   embedded_test_server()->ServeFilesFromDirectory(
