8dcfb8459702b5a68415dfed057cfa7a1c10ee3e
Ramin Halavati <rhalavati@chromium.org>
2022-10-24 06:36:34 +0000

    Pass model files content to ChromeScreenAI library.
    ChromeScreenAI library interface is updated as:
     - Having different init functions for visual annotation and main
       content extraction.
     - Passing file content instead of models folder to the library for
       Screen2x. (To be done later for ScreenAI.)
     - Adding a function to fetch library version.
     - Separating the function that sets debug mode.
    
    To support the above, the allow-listed folder in sandbox on Windows
    is removed and file handles are opened in browser and sent to the
    library.
    The Google3 side of this change is in http://cl/481586758.
    
    Bug: 1278249
    Change-Id: Ie5855bebefc04b1c7b4f4c22a17ca956ce34c9bc
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963033
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062678}

--- components/services/screen_ai/public/cpp/screen_ai_install_state.h
+++ components/services/screen_ai/public/cpp/screen_ai_install_state.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/observer_list_types.h"
 
@@ -18,6 +19,18 @@ class ScreenAIComponentInstallerPolicy;
 namespace screen_ai {
 
 class ScreenAIInstallStateTest;
+
+// Manages required files for ScreenAI library initialization.
+class ComponentModelFiles {
+ public:
+  explicit ComponentModelFiles(const base::FilePath& library_folder);
+  ComponentModelFiles(const ComponentModelFiles&) = delete;
+  ComponentModelFiles& operator=(const ComponentModelFiles&) = delete;
+  ~ComponentModelFiles() = default;
+
+  base::File screen2x_model_config_;
+  base::File screen2x_model_;
+};
 
 class ScreenAIInstallState {
  public:
@@ -36,22 +49,33 @@ class ScreenAIInstallState {
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
-  bool is_component_ready();
+  bool is_component_ready() { return component_ready_; }
 
   base::FilePath get_component_binary_path() { return component_binary_path_; }
 
-  void set_component_ready_for_testing(
-      const base::FilePath& component_binary_path) {
-    component_binary_path_ = component_binary_path;
-  }
+  void set_component_ready_for_testing() { component_ready_ = true; }
+
+  ComponentModelFiles* GetComponentModelFiles();
 
  private:
   friend class component_updater::ScreenAIComponentInstallerPolicy;
   friend class ScreenAIInstallStateTest;
 
-  void SetComponentReady(const base::FilePath& component_binary_path);
+  // Notifies this class that the component is downloaded and verified.
+  void ComponentFolderVerified(const base::FilePath& component_folder);
+
+  // Opens component files. The files will be used when the service is
+  // initializing.
+  void OpenComponentFiles();
+
+  void SetComponentModelFiles(std::unique_ptr<ComponentModelFiles> model_files);
+
+  // Marks component ready and informs observers.
+  void SetComponentReady();
 
   base::FilePath component_binary_path_;
+  std::unique_ptr<ComponentModelFiles> component_model_files_;
+  bool component_ready_;
 
   std::vector<Observer*> observers_;
 };
--- components/services/screen_ai/public/cpp/screen_ai_service_router.cc
+++ components/services/screen_ai/public/cpp/screen_ai_service_router.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/screen_ai/public/cpp/screen_ai_service_router.h"
 
+#include <utility>
+
 #include "base/files/file_path.h"
 #include "components/services/screen_ai/public/cpp/screen_ai_install_state.h"
 #include "content/public/browser/service_process_host.h"
@@ -60,8 +62,16 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
           .WithDisplayName("Screen AI Service")
           .Pass());
 
-  if (screen_ai_service_.is_bound())
-    screen_ai_service_->LoadLibrary(library_path);
+  if (screen_ai_service_.is_bound()) {
+    screen_ai::ComponentModelFiles* model_files =
+        ScreenAIInstallState::GetInstance()->GetComponentModelFiles();
+    DCHECK(model_files);
+    DCHECK(model_files->screen2x_model_config_.IsValid());
+    DCHECK(model_files->screen2x_model_.IsValid());
+    screen_ai_service_->LoadLibrary(
+        std::move(model_files->screen2x_model_config_),
+        std::move(model_files->screen2x_model_), library_path);
+  }
 }
 
 }  // namespace screen_ai
