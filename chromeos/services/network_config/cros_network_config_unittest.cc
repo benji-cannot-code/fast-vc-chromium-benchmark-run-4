f9be1b441cdb02e4bdbe2b6d0579b3bb9a207d36
Steven Bennetts <stevenjb@chromium.org>
2022-10-24 16:14:57 +0000

    Add portal_probe_url to cros network config and health mojoms
    This CL:
    * Adds portal_probe_url to NetworkStateProperties in
      cros_network_config.mojom.
    * Adds portal_probe_url to Network in network_health.mojom.
    
    The property is populated with NetworkState::probe_url.
    
    We need the property in network_health.mojom for providing it to
    the render process in Ash now and in Lacros later.
    
    We need the property in cros_network_config.mojom because the
    network_health mojo API uses the cros_network_config.mojom to
    populate a subset of "network health" properties.
    
    BUG=b:250725789
    TEST=chromeos_unittests --gtest_filter=NetworkHealth*:CrosNetwork*
    
    Change-Id: I999de6aae733286c19c04fed0ab699d56a4e545d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934958
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062838}

--- chromeos/services/network_config/cros_network_config_unittest.cc
+++ chromeos/services/network_config/cros_network_config_unittest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/login_state/login_state.h"
 #include "chromeos/services/network_config/public/cpp/cros_network_config_test_observer.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-shared.h"
+#include "components/captive_portal/core/captive_portal_detector.h"
 #include "components/onc/onc_constants.h"
 #include "components/onc/onc_pref_names.h"
 #include "components/prefs/testing_pref_service.h"
@@ -907,6 +908,7 @@ TEST_F(CrosNetworkConfigTest, PortalState) {
   ASSERT_TRUE(network);
   EXPECT_EQ(mojom::ConnectionStateType::kOnline, network->connection_state);
   EXPECT_EQ(mojom::PortalState::kOnline, network->portal_state);
+  EXPECT_FALSE(network->portal_probe_url);
 
   helper()->ConfigureService(
       R"({"GUID": "wifi1_guid", "Type": "wifi", "State": "portal-suspected",
@@ -915,14 +917,19 @@ TEST_F(CrosNetworkConfigTest, PortalState) {
   ASSERT_TRUE(network);
   EXPECT_EQ(mojom::ConnectionStateType::kPortal, network->connection_state);
   EXPECT_EQ(mojom::PortalState::kPortalSuspected, network->portal_state);
+  ASSERT_TRUE(network->portal_probe_url);
+  EXPECT_EQ(captive_portal::CaptivePortalDetector::kDefaultURL,
+            *network->portal_probe_url);
 
   helper()->ConfigureService(
       R"({"GUID": "wifi1_guid", "Type": "wifi", "State": "redirect-found",
-          "Strength": 90, "AutoConnect": true})");
+          "Strength": 90, "AutoConnect": true, "ProbeUrl": "http://foo.com"})");
   network = GetNetworkState("wifi1_guid");
   ASSERT_TRUE(network);
   EXPECT_EQ(mojom::ConnectionStateType::kPortal, network->connection_state);
   EXPECT_EQ(mojom::PortalState::kPortal, network->portal_state);
+  ASSERT_TRUE(network->portal_probe_url);
+  EXPECT_EQ("http://foo.com/", network->portal_probe_url->spec());
 
   helper()->ConfigureService(
       R"({"GUID": "wifi1_guid", "Type": "wifi", "State": "no-connectivity",
@@ -931,6 +938,7 @@ TEST_F(CrosNetworkConfigTest, PortalState) {
   ASSERT_TRUE(network);
   EXPECT_EQ(mojom::ConnectionStateType::kPortal, network->connection_state);
   EXPECT_EQ(mojom::PortalState::kNoInternet, network->portal_state);
+  EXPECT_FALSE(network->portal_probe_url);
 
   helper()->ConfigureService(
       R"({"GUID": "wifi1_guid", "Type": "wifi", "State": "portal-suspected",
