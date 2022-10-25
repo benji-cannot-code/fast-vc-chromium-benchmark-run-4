1b9256ce307fcfd95f55940f18a4d381aae9fddb
Rune Lillesveen <futhark@chromium.org>
2022-10-25 11:32:37 +0000

    [@container] Add tests for getQueryingDescendantsForContainer
    Bug: 1302630
    Change-Id: I159fd1451c05832d90a61418ec76150051f11763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3974614
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063244}

--- third_party/blink/web_tests/inspector-protocol/dom/container-queries/dom-get-querying-descendants-for-container.js
+++ third_party/blink/web_tests/inspector-protocol/dom/container-queries/dom-get-querying-descendants-for-container.js
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   await testQueryingDescendantsResult('#container-unnamed', '.desc-unnamed');
   await testQueryingDescendantsResult('#container-named', '.desc-named');
+  await testQueryingDescendantsResult('#container-unnamed-style', '.desc-unnamed-style');
+  await testQueryingDescendantsResult('#container-named-style', '.desc-named-style');
 
   testRunner.completeTest();
 });
