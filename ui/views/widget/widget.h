ab4388a9c115d7fc8fa8369438a9f75da920de0b
Allen Bauer <kylixrd@chromium.org>
2022-10-25 16:45:15 +0000

    Make WidgetDelegate support a WeakPtr and use this for Widget.
    Bug: 1346369
    Change-Id: I01dfd71bd69821995ddcaa606e94853149dd1ce3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780675
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063359}

--- ui/views/widget/widget.h
+++ ui/views/widget/widget.h
@@ -587,7 +587,7 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   const Widget* GetPrimaryWindowWidget() const;
 
   // Gets/Sets the WidgetDelegate.
-  WidgetDelegate* widget_delegate() const { return widget_delegate_; }
+  WidgetDelegate* widget_delegate() const { return widget_delegate_.get(); }
 
   // Sets the specified view as the contents of this Widget. There can only
   // be one contents view child of this Widget's RootView. This view is sized to
@@ -1189,6 +1189,9 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   friend class TextfieldTest;
   friend class ViewAuraTest;
   friend class ui_devtools::PageAgentViews;
+  // TODO (kylixrd): Remove this after Widget no longer can "own" the
+  // WidgetDelegate.
+  friend class WidgetDelegate;
   friend void DisableActivationChangeHandlingForTests();
 
   // Sets/gets the type of disabling widget activation change handling.
@@ -1241,9 +1244,18 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
 
   base::ObserverList<WidgetRemovalsObserver>::Unchecked removals_observers_;
 
-  // Non-owned pointer to the Widget's delegate. If a NULL delegate is supplied
+  // Weak pointer to the Widget's delegate. If a NULL delegate is supplied
   // to Init() a default WidgetDelegate is created.
-  raw_ptr<WidgetDelegate> widget_delegate_ = nullptr;
+  base::WeakPtr<WidgetDelegate> widget_delegate_;
+
+  // TODO(kylixrd): Rename this once the transition requiring the client to own
+  // the delegate is finished.
+  // [Owned Widget delegate if the DefaultWidgetDelegate is used. This
+  // ties the lifetime of the default delegate to the Widget.]
+  //
+  // This will "own" the delegate when WidgetDelegate::owned_by_widget() is
+  // true.
+  std::unique_ptr<WidgetDelegate> owned_widget_delegate_;
 
   // The parent of this widget. This is the widget that associates with
   // the |params.parent| supplied to Init(). If no parent is given or the native
