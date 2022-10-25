33c49298f024c625af6ff1f5e30ca73d3488595c
mgeraldson <mgeraldson@igalia.com>
2022-10-25 07:38:44 +0000

    ax_test: reduce content dependencies in Fuchsia' AXTreeFormatter (1/4)
    In this commit, we Kill AXTreeFormatter dependency on
    BrowserAccessibility::FromAXPlatformNodeDelegate()
    
    R=asurkov@igalia.com
    
    Change-Id: I09a395ab38242c8e33bb3a225f2d47166ba921c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966234
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Simple Shell <mgeraldson@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1063190}

--- content/browser/accessibility/accessibility_tree_formatter_fuchsia.h
+++ content/browser/accessibility/accessibility_tree_formatter_fuchsia.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ACCESSIBILITY_ACCESSIBILITY_TREE_FORMATTER_FUCHSIA_H_
 #define CONTENT_BROWSER_ACCESSIBILITY_ACCESSIBILITY_TREE_FORMATTER_FUCHSIA_H_
 
-#include "content/browser/accessibility/browser_accessibility.h"
 #include "content/common/content_export.h"
 #include "ui/accessibility/platform/inspect/ax_tree_formatter_base.h"
 
@@ -32,13 +31,13 @@ class CONTENT_EXPORT AccessibilityTreeFormatterFuchsia
       std::vector<AXPropertyFilter>* property_filters) override;
 
  private:
-  void RecursiveBuildTree(const BrowserAccessibility& node,
+  void RecursiveBuildTree(const ui::AXPlatformNodeDelegate& node,
                           base::Value::Dict* dict) const;
 
   std::string ProcessTreeForOutput(
       const base::Value::Dict& node) const override;
 
-  void AddProperties(const BrowserAccessibility& node,
+  void AddProperties(const ui::AXPlatformNodeDelegate& node,
                      base::Value::Dict* dict) const;
 };
 
