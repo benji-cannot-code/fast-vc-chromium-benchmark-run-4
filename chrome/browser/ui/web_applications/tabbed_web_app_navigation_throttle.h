8e19a366c7bacba98a23ba14bf7b1fb2ef2ab0e2
Louise Brett <loubrett@google.com>
2022-10-25 07:26:15 +0000

    Only create tabbed web app navigation throttle for primary main frame
    This fixes an issue with the pinned home tab where subframe
    navigations were opening in a new tab, causing some strange bahviour.
    
    This change also adds WillRedirectRequest(). This doesn't change
    any behaviour but makes it clear what is happening with redirects.
    
    Bug: 897314
    Change-Id: I9af7e791dae976a3148bc38d29a05bcd7ccaac54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963947
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063187}

--- chrome/browser/ui/web_applications/tabbed_web_app_navigation_throttle.h
+++ chrome/browser/ui/web_applications/tabbed_web_app_navigation_throttle.h
@@ -24,6 +24,7 @@ class TabbedWebAppNavigationThrottle : public content::NavigationThrottle {
 
   // content::NavigationThrottle:
   ThrottleCheckResult WillStartRequest() override;
+  ThrottleCheckResult WillRedirectRequest() override;
   const char* GetNameForLogging() override;
 
  private:
