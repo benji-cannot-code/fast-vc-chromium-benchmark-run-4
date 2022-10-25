b662615b8e6d90d29e965648c880c4ec8968ca81
Leonid Baraz <lbaraz@chromium.org>
2022-10-25 01:34:56 +0000

    Revert "Wait for asynchronous activity in the test to finish."
    This reverts commit d50c6b9f1440c0cee8ebdd056152a4a1ea7811bb.
    
    Reason for revert: crrev.com/c/3969566 rolled back and the problem fixed differently, no longer needing this wait.
    
    Original change's description:
    > Wait for asynchronous activity in the test to finish.
    >
    > This became necessary after crrev.com/c/3969566 (fixing a flakiness)
    > introduced additional asynchronous action in ReportQueueProducer, which
    > in turn made it necessary to destruct an object on the thread.
    > The tests need to ensure this destruction finishes.
    >
    > Bug: 1376844
    > Change-Id: Ia3ef98c3bbb1c12d2b69f6742ab051842946e050
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969644
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    > Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1061960}
    
    Bug: 1376844
    Change-Id: I35c4931dbc6d929b28308aff36070818f6a11054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976779
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1063114}

--- chrome/browser/ash/settings/device_settings_test_helper.cc
+++ chrome/browser/ash/settings/device_settings_test_helper.cc
@@ -98,7 +98,6 @@ void DeviceSettingsTestBase::TearDown() {
   device_policy_.reset();
   base::RunLoop().RunUntilIdle();
   profile_.reset();
-  task_environment_.RunUntilIdle();
 }
 
 void DeviceSettingsTestBase::ReloadDevicePolicy() {
