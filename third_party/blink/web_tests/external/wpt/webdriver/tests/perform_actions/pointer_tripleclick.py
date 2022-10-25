5473e19c0e77f39d4696eefc97d13ef36aaa6372
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-25 15:27:38 +0000

    Import wpt@9db869d55c710e6cd9ca1603d082c9c5da85cdb0
    Using wpt-import in Chromium 1818a6010dcef63d8f78da0a27208ea948614f11.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I1f528bb0a45703950e9aa13df3323f85fb3eed5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975160
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063325}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/pointer_tripleclick.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/pointer_tripleclick.py
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import math
+
 from tests.perform_actions.support.refine import get_events
 from tests.support.asserts import assert_move_to_coordinates
 from tests.support.helpers import filter_dict
@@ -20,8 +22,8 @@ def test_tripleclick_at_coordinates(session, mouse_chain, inline):
     div = session.find.css("div", all=False)
     div_rect = div.rect
     div_centre = {
-        "x": div_rect["x"] + int(div_rect["width"]/2),
-        "y": div_rect["y"] + int(div_rect["height"]/2)
+        "x": math.floor(div_rect["x"] + div_rect["width"] / 2),
+        "y": math.floor(div_rect["y"] + div_rect["height"] / 2)
     }
     mouse_chain \
         .pointer_move(div_centre["x"], div_centre["y"]) \
@@ -32,4 +34,4 @@ def test_tripleclick_at_coordinates(session, mouse_chain, inline):
 
     actual_text = session.execute_script("return document.getSelection().toString();")
 
-    assert lots_of_text == actual_text
+    assert actual_text == lots_of_text
