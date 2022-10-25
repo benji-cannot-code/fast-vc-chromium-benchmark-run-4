48055aefdf5f292fcaf426bb4e5f9b3d4fbcf7e3
Yoshifumi Inoue <yosin@chromium.org>
2022-10-25 03:01:29 +0000

    [initial-letter] Change NGExclusionSpace:: InitialLetterClearance() API
    This patch changes `NGExclusionSpace:: InitialLetterClearance()` to
    take `EClear` and get rid of taking `EClear` and `EFloat` to
    make clear about what initial letter clearance used and preparation
    of implementing initial letter layout[1].
    
    API after this patch:
      * InitialLetterClearance(EClear) -> LayoutUnit
      * InitialLetterClearance(EFloat) -> LayoutUnit
    
    API before this patch
      * InitialLetterClearance(EClear, EFloat) -> LayoutUnit
      * InitialLetterClearance() -> LayoutUnit
    
    Note: This patch is a follow-up of the CL[2] which introcued API
    `InitialLetterClearance()`.
    
    
    Usage example:
    
    In FindLayoutOpportunityForFloat()
    ...
      const LayoutUnit clearance_offset =
          std::max({exclusion_space.ClearanceOffset(clear_type),
                    exclusion_space.InitialLetterClearanceOffset(float_type)});
    
      AdjustToClearance(clearance_offset, &adjusted_origin_point);
    
      return exclusion_space.FindLayoutOpportunity(
          adjusted_origin_point, unpositioned_float.available_size.inline_size,
          inline_size + fragment_margins.InlineSum() /* minimum_inline_size */)
    ```
    
    ```
    In NGBlockLayoutAlgorithm::FinishLayout()
    ```
      // If the current layout is a new formatting context, we need to encapsulate
      // all of our floats.
      if (ConstraintSpace().IsNewFormattingContext()) {
        intrinsic_block_size_ = std::max(
            {intrinsic_block_size_, ExclusionSpace().ClearanceOffset(EClear::kBoth),
             ExclusionSpace().InitialLetterClearanceOffset(EClear::kBoth)});
      }
    ```
    
    In NGBlockLayoutAlgorithm::LayoutNewFormattingContext()
    ...
      if (!IsResumingLayout(child_break_token)) {
        // The origin offset is where we should start looking for layout
        // opportunities. It needs to be adjusted by the child's clearance.
        AdjustToClearance(
            std::max(ExclusionSpace().ClearanceOffset(child_style.Clear(Style())),
                     ExclusionSpace().InitialLetterClearanceOffset(EClear::kBoth)),
            &origin_offset);
      }
    ```
    
    
    [1] http://crrev.com/c/3874383 Initial Letter All-In-One
    [2] http://crrev.com/c/3963588  [initial-letter] Make NGExclusionSpace
    to handle initial letter box
    
    Bug: 1276900
    Change-Id: I89314eee2b5851b0c1a472b61ff86c79eab280f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971640
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063136}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space_test.cc
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space_test.cc
@@ -25,8 +25,10 @@ struct ExclusionSpaceForTesting {
   NGExclusionSpace exclusion_space;
   LayoutUnit available_inline_size;
 
-  LayoutUnit InitialLetterClearanceOffset() const {
-    return exclusion_space.InitialLetterClearanceOffset();
+  Vector<LayoutUnit> InitialLetterClearanceOffset() const {
+    return {exclusion_space.InitialLetterClearanceOffset(EClear::kBoth),
+            exclusion_space.InitialLetterClearanceOffset(EClear::kLeft),
+            exclusion_space.InitialLetterClearanceOffset(EClear::kRight)};
   }
 
   void Add(const NGExclusion* exclusion) { exclusion_space.Add(exclusion); }
@@ -386,7 +388,8 @@ TEST(NGExclusionSpaceTest, InitialLetterBasic) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterDirectionRight) {
@@ -440,7 +443,8 @@ TEST(NGExclusionSpaceTest, InitialLetterDirectionRight) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatLeft1) {
@@ -491,7 +495,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatLeft1) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatLeft2) {
@@ -544,7 +549,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatLeft2) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatLeft2ClearLeft) {
@@ -599,7 +605,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatLeft2ClearLeft) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatLeftAndRight) {
@@ -663,7 +670,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatLeftAndRight) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatLeftAfterBreak) {
@@ -719,7 +727,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatLeftAfterBreak) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 124),
               ElementsAre(LayoutOpportunity(9, 124, 309)));
 
-  EXPECT_EQ(LayoutUnit(73), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(73), LayoutUnit(73), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, InitialLetterFloatRight2) {
@@ -778,7 +787,8 @@ TEST(NGExclusionSpaceTest, InitialLetterFloatRight2) {
   EXPECT_THAT(exclusion_space.AllLayoutOpportunities(9, 174),
               ElementsAre(LayoutOpportunity(9, 174, 309)));
 
-  EXPECT_EQ(LayoutUnit(123), exclusion_space.InitialLetterClearanceOffset());
+  EXPECT_THAT(exclusion_space.InitialLetterClearanceOffset(),
+              ElementsAre(LayoutUnit(123), LayoutUnit(123), LayoutUnit::Min()));
 }
 
 TEST(NGExclusionSpaceTest, ZeroInlineSizeOpportunity) {
