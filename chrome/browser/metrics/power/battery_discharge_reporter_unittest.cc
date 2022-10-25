2356a587f571c10a8a18aa7a41cdae0f92ad6a9d
Patrick Monette <pmonette@chromium.org>
2022-10-25 15:53:25 +0000

    Fix the battery discharge in milliwatts metric
    This change increases the max value for the
    BatteryDischargeRateMilliwatts histogram, and fixes its unit label.
    
    Change-Id: I084b676638ad57c3b7069f1b80a4a3ed2dd8bf03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976394
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063337}

--- chrome/browser/metrics/power/battery_discharge_reporter_unittest.cc
+++ chrome/browser/metrics/power/battery_discharge_reporter_unittest.cc
@@ -20,11 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr const char* kBatteryDischargeModeHistogramName =
-    "Power.BatteryDischargeMode4";
+    "Power.BatteryDischargeMode5";
 constexpr const char* kBatteryDischargeRateMilliwattsHistogramName =
-    "Power.BatteryDischargeRateMilliwatts4";
+    "Power.BatteryDischargeRateMilliwatts5";
 constexpr const char* kBatteryDischargeRateRelativeHistogramName =
-    "Power.BatteryDischargeRateRelative4";
+    "Power.BatteryDischargeRateRelative5";
 
 constexpr base::TimeDelta kTolerableDrift = base::Seconds(1);
 constexpr int kFullBatteryChargeLevel = 10000;
