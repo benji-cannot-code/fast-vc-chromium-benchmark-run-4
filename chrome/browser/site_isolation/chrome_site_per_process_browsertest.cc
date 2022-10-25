7a87bd2ba62fe4cddbf625cf76c2095203383a8c
Kevin McNee <mcnee@chromium.org>
2022-10-25 23:36:24 +0000

    Remove TestNotificationTracker
    There is one remaining usage of this which is to assert that no
    RenderProcessHosts are unexpectedly destroyed during a test. We have
    the test calculate this on its own (by taking a set difference of live
    RenderProcessHosts), and then remove TestNotificationTracker.
    
    Bug: 170921, 357627
    Change-Id: I446d76b83d85e26d29b235bf4ceadbe436ac7faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976451
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063568}

--- chrome/browser/site_isolation/chrome_site_per_process_browsertest.cc
+++ chrome/browser/site_isolation/chrome_site_per_process_browsertest.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/hit_test_region_observer.h"
 #include "content/public/test/test_navigation_observer.h"
-#include "content/public/test/test_notification_tracker.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/api/extensions_api_client.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
