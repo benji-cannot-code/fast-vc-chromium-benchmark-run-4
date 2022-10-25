b4fd89be7a7fd579365431d4d63d5a225ef8eb84
Abin K Paul <abin.paul1@gmail.com>
2022-10-25 14:08:57 +0000

    Add content-type to PerformanceResourceTiming
    This CL introduces a contentType field to Performance Resource
    Timing object. This field is behind a Runtime Enabled Flag.
    
    Resource Timing PR : https://github.com/w3c/resource-timing/pull/341
    Fetch PR : https://github.com/whatwg/fetch/pull/1481
    
    Bug: 1366706
    Change-Id: If4c92ef96d74e3ddbb71420ac61dbea4bfa1163b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916841
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Abin Paul <abin.paul1@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1063289}

--- third_party/blink/renderer/core/timing/performance_resource_timing.cc
+++ third_party/blink/renderer/core/timing/performance_resource_timing.cc
@@ -81,6 +81,7 @@ PerformanceResourceTiming::PerformanceResourceTiming(
       alpn_negotiated_protocol_(
           static_cast<String>(info.alpn_negotiated_protocol)),
       connection_info_(static_cast<String>(info.connection_info)),
+      content_type_(static_cast<String>(info.content_type)),
       render_blocking_status_(info.render_blocking_status
                                   ? RenderBlockingStatusType::kBlocking
                                   : RenderBlockingStatusType::kNonBlocking),
@@ -196,6 +197,10 @@ AtomicString PerformanceResourceTiming::renderBlockingStatus() const {
   }
   NOTREACHED();
   return "non-blocking";
+}
+
+AtomicString PerformanceResourceTiming::contentType() const {
+  return content_type_;
 }
 
 uint16_t PerformanceResourceTiming::responseStatus() const {
@@ -479,6 +484,9 @@ void PerformanceResourceTiming::BuildJSONValue(V8ObjectBuilder& builder) const {
   builder.AddString("nextHopProtocol", nextHopProtocol());
   if (RuntimeEnabledFeatures::RenderBlockingStatusEnabled()) {
     builder.AddString("renderBlockingStatus", renderBlockingStatus());
+  }
+  if (RuntimeEnabledFeatures::ResourceTimingContentTypeEnabled()) {
+    builder.AddString("contentType", contentType());
   }
   builder.AddNumber("workerStart", workerStart());
   builder.AddNumber("redirectStart", redirectStart());
