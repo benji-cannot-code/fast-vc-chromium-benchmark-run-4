b9cb515dc6ba6fd845698a600414d3a44d903acd
Mahmoud Rashad <mmrashad@google.com>
2022-10-25 14:37:42 +0000

    [Sync] Start the handler only for users that opted-in to TrustedVault
    Bug: 1247990
    Change-Id: I36a5dab39911720a8a0f2a612e8da994d5fca0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967484
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063301}

--- components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler.cc
+++ components/sync/trusted_vault/trusted_vault_degraded_recoverability_handler.cc
@@ -70,7 +70,6 @@ TrustedVaultDegradedRecoverabilityHandler::
     last_refresh_time_ =
         base::TimeTicks::Now() - (base::Time::Now() - last_refresh_time);
   }
-  Start();
 }
 
 TrustedVaultDegradedRecoverabilityHandler::
