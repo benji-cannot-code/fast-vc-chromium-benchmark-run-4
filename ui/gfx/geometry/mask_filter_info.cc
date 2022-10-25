7537b120f8fe9f2d39693061bc3fbbf58d664498
Xianzhu Wang <wangxianzhu@chromium.org>
2022-10-25 15:40:23 +0000

    Fix gfx::MaskFilterInfo::ApplyTransform() with rotation
    We should call the gfx::Transform version of
    LinearGradient::ApplyTransform() from the gfx::Transform version of
    MaskFilterInfo::ApplyTransform() to ensure the rotation is applied to
    LinearGradient.
    
    Bug: 1376263
    Change-Id: I1793a23438f61f31aee5f6fc1e1d61f66345b915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975755
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063328}

--- ui/gfx/geometry/mask_filter_info.cc
+++ ui/gfx/geometry/mask_filter_info.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/geometry/mask_filter_info.h"
 
-#include <sstream>
-
 #include "ui/gfx/geometry/axis_transform2d.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 #include "ui/gfx/geometry/transform.h"
@@ -38,10 +36,8 @@ bool MaskFilterInfo::ApplyTransform(const Transform& transform) {
     return false;
   rounded_corner_bounds_ = RRectF(new_rect);
 
-  if (gradient_mask_ && !gradient_mask_->IsEmpty()) {
-    gradient_mask_->ApplyTransform(AxisTransform2d::FromScaleAndTranslation(
-        transform.To2dScale(), transform.To2dTranslation()));
-  }
+  if (gradient_mask_ && !gradient_mask_->IsEmpty())
+    gradient_mask_->ApplyTransform(transform);
 
   return true;
 }
@@ -61,7 +57,9 @@ std::string MaskFilterInfo::ToString() const {
   std::string result = "MaskFilterInfo{" + rounded_corner_bounds_.ToString();
 
   if (gradient_mask_)
-    result += ", gradient_mask=" + gradient_mask_->ToString() + "}";
+    result += ", gradient_mask=" + gradient_mask_->ToString();
+
+  result += "}";
 
   return result;
 }
