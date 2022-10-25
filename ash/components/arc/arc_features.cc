c8e6ec1927628db5756f191f399dc0c22074a694
Ryo Hashimoto <hashimoto@chromium.org>
2022-10-25 02:12:16 +0000

    arc: Add ArcActivationNecessityChecker
    The new feature is disabled by default.
    ArcActivationNecessityChecker is used to decide if it's necessary to
    start ARC without the user's action.
    
    BUG=b:247044798
    TEST=unit_tests
    
    Change-Id: I95a5e8e67783ca7d86d028da83827104a228644a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967991
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063125}

--- ash/components/arc/arc_features.cc
+++ ash/components/arc/arc_features.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
+// Controls whether to always start ARC automatically, or wait for the user's
+// action to start it later in an on-demand manner.
+BASE_FEATURE(kArcOnDemandFeature,
+             "ArcOnDemand",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Controls ACTION_BOOT_COMPLETED broadcast for third party applications on ARC.
 // When disabled, third party apps will not receive this broadcast.
 BASE_FEATURE(kBootCompletedBroadcastFeature,
--- chrome/browser/ash/arc/session/arc_session_manager.h
+++ chrome/browser/ash/arc/session/arc_session_manager.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/arc_support_host.h"
 #include "chrome/browser/ash/arc/policy/arc_android_management_checker.h"
 #include "chrome/browser/ash/arc/session/adb_sideloading_availability_delegate_impl.h"
+#include "chrome/browser/ash/arc/session/arc_activation_necessity_checker.h"
 #include "chrome/browser/ash/arc/session/arc_app_id_provider_impl.h"
 #include "chrome/browser/ash/arc/session/arc_requirement_checker.h"
 #include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
@@ -354,6 +355,9 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // Returns true if ARC is started directly.
   bool RequestEnableImpl();
 
+  // Called when activation necessity check is done.
+  void OnActivationNecessityChecked(bool result);
+
   // Negotiates the terms of service to user, if necessary.
   // Otherwise, move to StartAndroidManagementCheck().
   void MaybeStartTermsOfServiceNegotiation();
@@ -464,6 +468,8 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   ArcRequirementChecker::AndroidManagementCheckerFactory
       android_management_checker_factory_;
   std::unique_ptr<ArcRequirementChecker> requirement_checker_;
+
+  std::unique_ptr<ArcActivationNecessityChecker> activation_necessity_checker_;
 
   std::unique_ptr<ScopedOptInFlowTracker> scoped_opt_in_tracker_;
   std::unique_ptr<ArcPaiStarter> pai_starter_;
