f882f0e03e1826564e4368fbf00e4dd7359a6fca
Toni Barzic <tbarzic@chromium.org>
2022-10-25 22:57:26 +0000

    Run zero state search when creating tablet mode app list
    AppsContainer runs zero state search in response to app list target
    visibility change to visible. This gets called from app list controller
    when the calculated target app list visibility changes (which depends on
    tablet mode state, whether app list is occluded by other windows,
    overview mode state, etc.).
    When app list view gets first created, target app list visibility
    (assuming app list was not occluded by other UI) was changing before
    the app list view, and apps container were actually created. As a
    result, AppsContainer was missing initial app list target visibility
    changes, and did not initiate zero state search. This changes
    AppListPresenterImpl to notify AppListController that target app list
    view visibility is changing after app list view has been created.
    
    Add tests to ensure zero state search is run when entering tablet mode
    if app list becomes immediately visible.
    
    BUG=1377498
    
    Change-Id: I042c826dc3a085f96305b3bf6ac9166f86cfbbbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972617
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063549}

--- ash/app_list/app_list_presenter_impl.cc
+++ ash/app_list/app_list_presenter_impl.cc
@@ -232,7 +232,6 @@ void AppListPresenterImpl::Show(AppListViewState preferred_state,
   showing_app_list_ = true;
 
   is_target_visibility_show_ = true;
-  OnVisibilityWillChange(GetTargetVisibility(), display_id);
   RequestPresentationTime(display_id, event_time_stamp);
 
   if (!view_) {
@@ -242,6 +241,7 @@ void AppListPresenterImpl::Show(AppListViewState preferred_state,
     view_->GetWidget()->GetNativeWindow()->TrackOcclusionState();
   }
 
+  OnVisibilityWillChange(GetTargetVisibility(), display_id);
   controller_->UpdateLauncherContainer(display_id);
 
   // App list needs to know the new shelf layout in order to calculate its
--- chrome/browser/ui/ash/app_list/app_list_with_recent_apps_browsertest.cc
+++ chrome/browser/ui/ash/app_list/app_list_with_recent_apps_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/public/cpp/test/app_list_test_api.h"
+#include "ash/public/cpp/test/shell_test_api.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/search/search_controller.h"
@@ -66,4 +67,28 @@ IN_PROC_BROWSER_TEST_F(AppListWithRecentAppBrowserTest, MouseClickAtRecentApp) {
       "Apps.NewUserFirstLauncherAction.ClamshellMode",
       static_cast<int>(ash::AppListLaunchedFrom::kLaunchedFromRecentApps),
       /*expected_bucket_count=*/1);
+}
+
+// Tests that recent apps are shown in tablet mode app list after transition
+// from bubble launcher search to tablet mode.
+IN_PROC_BROWSER_TEST_F(AppListWithRecentAppBrowserTest,
+                       RecentAppsShownInTabletModeAfterClearingSearch) {
+  // Minimize the browser window so tablet mode launcher becomes visible
+  // immediately after transition to tablet mode.
+  browser()->window()->Minimize();
+
+  views::View* recent_app = app_list_test_api_.GetRecentAppAt(0);
+  ASSERT_TRUE(recent_app);
+
+  // Simulate launcher search.
+  app_list_test_api_.SimulateSearch(u"foo");
+
+  // Transition to tablet mode while bubble launcher is showing search UI, to
+  // verify that recent apps are not empty if launcher is shown just after
+  // clearing search.
+  ash::ShellTestApi().SetTabletModeEnabledForTest(true);
+  app_list_test_api_.WaitForAppListShowAnimation(/*is_bubble_window =*/false);
+
+  recent_app = app_list_test_api_.GetRecentAppAt(0);
+  ASSERT_TRUE(recent_app);
 }
