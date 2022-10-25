65ab4ea75f12351fc5a2184c3a5b6b9219c54f4a
Anders Hartvoll Ruud <andruud@chromium.org>
2022-10-25 11:25:42 +0000

    [StyleBuilder] Don't generate superfluous internal functions
    Fields with the "external" template currently generates internal
    (r-value) setter functions  even when a custom setter isn't marked
    in css_properties.json5. This isn't needed, and likely just makes it
    more confusing when internal versions of the setter do and do not
    appear.
    
    This CL fixes the code generation to take custom setter info into
    account, and also reorganizes into decl_[internal_]move_method macros,
    to better match the main pattern.
    
    Note that (now-removed) check on field.is_bit_field had no effect,
    since fields using the external template are never bit fields.
    
    Bug: 1377295
    Change-Id: I7ba8579923a30c6807c77044980bbc47df48afe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3973078
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063242}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -807,7 +807,7 @@ void StyleAdjuster::AdjustForSVGTextElement(ComputedStyle& style,
       ComputedStyleInitialValues::InitialColumnRuleStyle());
   style.SetColumnRuleWidthInternal(
       LayoutUnit(ComputedStyleInitialValues::InitialColumnRuleWidth()));
-  style.SetColumnRuleColorInternal(
+  style.SetColumnRuleColor(
       ComputedStyleInitialValues::InitialColumnRuleColor());
   builder.SetInternalVisitedColumnRuleColorInternal(
       ComputedStyleInitialValues::InitialInternalVisitedColumnRuleColor());
