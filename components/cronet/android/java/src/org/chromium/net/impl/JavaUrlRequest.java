afde21fafb5843c9b30dc6f63ce62beca2ac28da
Mohannad Farrag <aymanm@google.com>
2022-10-25 18:17:54 +0000

    Add mActiveRequestCount to Cronet's Java Engine.
    * This will keep track of the requests in which `start()` has been called on but hasn't reached a final state yet.
      See https://developer.android.com/guide/topics/connectivity/cronet/lifecycle for more information on Cronet's request lifecycle.
    * This is a step in exposing the number of active requests in CronetEngine.
    
    Change-Id: I6b565af59eb44438fc8b81f724ff375d6823fb0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945019
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063409}

--- components/cronet/android/java/src/org/chromium/net/impl/JavaUrlRequest.java
+++ components/cronet/android/java/src/org/chromium/net/impl/JavaUrlRequest.java
@@ -106,6 +106,7 @@ final class JavaUrlRequest extends UrlRequestBase {
     private String mPendingRedirectUrl;
     private HttpURLConnection mCurrentUrlConnection; // Only accessed on mExecutor.
     private OutputStreamDataSink mOutputStreamDataSink; // Only accessed on mExecutor.
+    private final JavaCronetEngine mEngine;
     private final int mCronetEngineId;
     private final CronetLogger mLogger;
 
@@ -224,6 +225,7 @@ final class JavaUrlRequest extends UrlRequestBase {
                 });
             }
         });
+        mEngine = engine;
         mCronetEngineId = engine.getCronetEngineId();
         mLogger = engine.getCronetLogger();
         mCurrentUrl = url;
@@ -400,6 +402,7 @@ final class JavaUrlRequest extends UrlRequestBase {
     @Override
     public void start() {
         mAdditionalStatusDetails = Status.CONNECTING;
+        mEngine.incrementActiveRequestCount();
         transitionStates(State.NOT_STARTED, State.STARTED, new Runnable() {
             @Override
             public void run() {
@@ -977,6 +980,7 @@ final class JavaUrlRequest extends UrlRequestBase {
 
         void onCanceled(final UrlResponseInfo info) {
             closeResponseChannel();
+            mEngine.decrementActiveRequestCount();
             mUserExecutor.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -991,6 +995,7 @@ final class JavaUrlRequest extends UrlRequestBase {
         }
 
         void onSucceeded(final UrlResponseInfo info) {
+            mEngine.decrementActiveRequestCount();
             mUserExecutor.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -1006,6 +1011,7 @@ final class JavaUrlRequest extends UrlRequestBase {
 
         void onFailed(final UrlResponseInfo urlResponseInfo, final CronetException e) {
             closeResponseChannel();
+            mEngine.decrementActiveRequestCount();
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
