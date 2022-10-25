b0ddd572e7a49108ce9f4bd6643745a02e45e6f9
mgeraldson <mgeraldson@igalia.com>
2022-10-25 07:44:12 +0000

    ax_test: reduce content dependencies in Linux AXEventRecorder (1/2)
    R=asurkov@igalia.com
    
    Bug: 1376543
    Change-Id: Idf7019e42ad17ce43f88c266be36dc9299d1fa8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966568
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Simple Shell <mgeraldson@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1063193}

--- content/browser/accessibility/accessibility_event_recorder_auralinux.cc
+++ content/browser/accessibility/accessibility_event_recorder_auralinux.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_handle.h"
 #include "base/strings/stringprintf.h"
 #include "content/browser/accessibility/browser_accessibility_auralinux.h"
-#include "content/browser/accessibility/browser_accessibility_manager.h"
+#include "ui/accessibility/platform/ax_platform_tree_manager.h"
 #include "ui/accessibility/platform/inspect/ax_inspect_utils_auralinux.h"
 
 namespace content {
@@ -60,7 +60,7 @@ bool AccessibilityEventRecorderAuraLinux::ShouldUseATSPI() {
 }
 
 AccessibilityEventRecorderAuraLinux::AccessibilityEventRecorderAuraLinux(
-    BrowserAccessibilityManager* manager,
+    ui::AXPlatformTreeManager* manager,
     base::ProcessId pid,
     const ui::AXTreeSelector& selector)
     : manager_(manager), pid_(pid), selector_(selector) {
@@ -140,7 +140,7 @@ void AccessibilityEventRecorderAuraLinux::ProcessATKEvent(
     unsigned int n_params,
     const GValue* params) {
   // If we don't have a root object, it means the tree is being destroyed.
-  if (!manager_->GetBrowserAccessibilityRoot()) {
+  if (!manager_->RootDelegate()) {
     RemoveATKEventListeners();
     return;
   }
