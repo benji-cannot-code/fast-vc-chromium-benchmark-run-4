4f9e220a214ae786fe6c15e4c93f9f8703bdfc30
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2022-10-25 17:48:01 +0000

    Local Font Access: Remove FontTableMap IDL and implementation.
    All usage was removed in https://crrev.com/c/2354961, but the actual
    implementation remained in the tree.
    
    Change-Id: Ib994a1931461f0a4a15362540f4628a7622a5229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968070
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063391}

--- third_party/blink/renderer/modules/font_access/font_metadata.cc
+++ third_party/blink/renderer/modules/font_access/font_metadata.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/fileapi/blob.h"
-#include "third_party/blink/renderer/modules/font_access/font_table_map.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
 #include "third_party/blink/renderer/platform/fonts/font_global_context.h"
