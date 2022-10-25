9ffce40e17ccbd234d242ab8549f5c998fd46917
Bo Liu <boliu@chromium.org>
2022-10-25 00:49:31 +0000

    Remove unused UI_RESOURCE_INVALID_REQUEST
    Change-Id: I48169b2c7cb85c62c9d570818fc3fae4e82edd1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3974831
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063100}

--- cc/resources/ui_resource_request.h
+++ cc/resources/ui_resource_request.h
@@ -20,7 +20,6 @@ class CC_EXPORT UIResourceRequest {
   enum UIResourceRequestType {
     UI_RESOURCE_CREATE,
     UI_RESOURCE_DELETE,
-    UI_RESOURCE_INVALID_REQUEST
   };
 
   UIResourceRequest(UIResourceRequestType type, UIResourceId id);
