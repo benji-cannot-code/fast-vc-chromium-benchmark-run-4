290345cd3443e0ce4c9f379fefca8822d2c1f33b
Andrew Rayskiy <greengrape@google.com>
2022-10-25 23:05:55 +0000

    Update PinnedLauncherApps and DefaultHandlersForFileExtensions policies
    PinnedLauncherApps now supports System Web Apps;
    DefaultHandlersForFileExtensions now supports Android Apps & System Web Apps.
    
    Bug: 1371300
    
    Change-Id: Id22aca679d01df7e7809261f5bdeeadeb782a709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934845
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063555}

--- chrome/browser/ash/policy/handlers/configuration_policy_handler_ash.h
+++ chrome/browser/ash/policy/handlers/configuration_policy_handler_ash.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/onc/onc_constants.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
 #include "components/policy/core/common/policy_map.h"
-#include "components/prefs/pref_registry_simple.h"
 
 namespace policy {
 
