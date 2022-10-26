f9f5aba846fb6c6db081be0378c86838d87338d6
Kent Tamura <tkent@chromium.org>
2022-10-26 23:59:54 +0000

    TextEncoding: Remove some code from encoding_tables.h
    * Remove unnecessary forward declarations
    * Remove SortByFirst() and StableSortByFirst()
      base::ranges::sort() and base::ranges::stable_sort() are enough.
    
    This CL has no behavior changes.
    
    Change-Id: Ic72a0e963f9b32eaeab13e7100ba2f085d6e9751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3980662
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064086}

--- third_party/blink/renderer/platform/wtf/text/encoding_tables.cc
+++ third_party/blink/renderer/platform/wtf/text/encoding_tables.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unicode/ucnv.h>
 
+#include "base/ranges/algorithm.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_codec_icu.h"
@@ -191,7 +192,7 @@ const Jis0208EncodeIndex& EnsureJis0208EncodeIndexForEncode() {
     auto& index = EnsureJis0208EncodeIndexForDecode();
     for (size_t i = 0; i < index.size(); ++i)
       (*table)[i] = {index[i].second, index[i].first};
-    StableSortByFirst(*table);
+    base::ranges::stable_sort(*table, CompareFirst{});
   });
   return *table;
 }
@@ -294,7 +295,7 @@ const EucKrEncodeIndex& EnsureEucKrEncodeIndexForEncode() {
     auto& index = EnsureEucKrEncodeIndexForDecode();
     for (size_t i = 0; i < index.size(); ++i)
       (*table)[i] = {index[i].second, index[i].first};
-    SortByFirst(*table);
+    base::ranges::sort(*table, CompareFirst{});
     DCHECK(SortedFirstsAreUnique(*table));
   });
   return *table;
@@ -344,7 +345,7 @@ const Gb18030EncodeIndex& EnsureGb18030EncodeIndexForEncode() {
     auto& index = EnsureGb18030EncodeTable();
     for (uint16_t i = 0; i < index.size(); ++i)
       (*table)[i] = {index[i], i};
-    StableSortByFirst(*table);
+    base::ranges::stable_sort(*table, CompareFirst{});
   });
   return *table;
 }
