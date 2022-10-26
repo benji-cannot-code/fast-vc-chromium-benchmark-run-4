36ad91ad68608bf793e34ee7171a31f40b284721
Javier Fernández García-Boente <jfernandez@igalia.com>
2022-10-26 09:50:10 +0000

    Initial implementation of the Ed25519 algorithm
    This CL implements the generateKey and basic import/export operations
    for the Ed25519 algorithm, as well as the sign / verify methods.
    
    Additionally, it adds the new algorithm in the registry and implements its normalization, which is quite simple due to the lack of parameters.
    
    Finally, it adds the logic for the structured clone serialization, together with their proprer unit tests.
    
    The feature is implemented behind the WebCryptoCurve25519 runtime flag for now. See the intent-to-prototype request [1] for details.
    
    [1] https://groups.google.com/a/chromium.org/g/blink-dev/c/n0uKIqfypW0/m/xu5UBbaBAwAJ
    
    Bug: 1370697
    
    Change-Id: Ief074ef458a6760383d42b955560ac701c20b18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930754
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063711}

--- components/webcrypto/algorithm_registry.cc
+++ components/webcrypto/algorithm_registry.cc
@@ -31,7 +31,8 @@ class AlgorithmRegistry {
         ecdsa_(CreateEcdsaImplementation()),
         ecdh_(CreateEcdhImplementation()),
         hkdf_(CreateHkdfImplementation()),
-        pbkdf2_(CreatePbkdf2Implementation()) {
+        pbkdf2_(CreatePbkdf2Implementation()),
+        ed25519_(CreateEd25519Implementation()) {
     crypto::EnsureOpenSSLInit();
   }
 
@@ -67,6 +68,8 @@ class AlgorithmRegistry {
         return hkdf_.get();
       case blink::kWebCryptoAlgorithmIdPbkdf2:
         return pbkdf2_.get();
+      case blink::kWebCryptoAlgorithmIdEd25519:
+        return ed25519_.get();
       default:
         return nullptr;
     }
@@ -86,6 +89,7 @@ class AlgorithmRegistry {
   const std::unique_ptr<AlgorithmImplementation> ecdh_;
   const std::unique_ptr<AlgorithmImplementation> hkdf_;
   const std::unique_ptr<AlgorithmImplementation> pbkdf2_;
+  const std::unique_ptr<AlgorithmImplementation> ed25519_;
 };
 
 }  // namespace
--- third_party/blink/renderer/bindings/modules/v8/serialization/web_crypto_sub_tags.h
+++ third_party/blink/renderer/bindings/modules/v8/serialization/web_crypto_sub_tags.h
@@ -28,6 +28,7 @@ enum CryptoKeyAlgorithmTag : uint32_t {
   kEcdhTag = 15,
   kHkdfTag = 16,
   kPbkdf2Tag = 17,
+  kEd25519Tag = 18,
   // Maximum allowed value is 2^32-1
 };
 
@@ -61,6 +62,7 @@ enum CryptoKeySubTag : uint8_t {
   kRsaHashedKeyTag = 4,
   kEcKeyTag = 5,
   kNoParamsKeyTag = 6,
+  kEd25519KeyTag = 7,
   // Maximum allowed value is 255
 };
 
