d7e715ded0096e830c61669bb97a3737d2c98c3d
Peter Kasting <pkasting@chromium.org>
2022-10-26 15:07:54 +0000

    Replace std::equal() with base::ranges::equal(): cc/
    Bug: 1368812
    Change-Id: Ia04fb9f893ef5ffa6bc5048e7c65d34c52067373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3980554
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063801}

--- cc/paint/paint_filter.cc
+++ cc/paint/paint_filter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/values_equivalent.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "base/types/optional_util.h"
 #include "build/build_config.h"
 #include "cc/paint/draw_image.h"
@@ -839,8 +840,8 @@ sk_sp<PaintFilter> MatrixConvolutionPaintFilter::SnapshotWithImagesInternal(
 bool MatrixConvolutionPaintFilter::operator==(
     const MatrixConvolutionPaintFilter& other) const {
   return kernel_size_ == other.kernel_size_ &&
-         std::equal(kernel_.container().begin(), kernel_.container().end(),
-                    other.kernel_.container().begin(), AreScalarsEqual) &&
+         base::ranges::equal(kernel_.container(), other.kernel_.container(),
+                             AreScalarsEqual) &&
          PaintOp::AreEqualEvenIfNaN(gain_, other.gain_) &&
          PaintOp::AreEqualEvenIfNaN(bias_, other.bias_) &&
          kernel_offset_ == other.kernel_offset_ &&
