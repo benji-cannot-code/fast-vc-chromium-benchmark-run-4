0c0639c2893ced99c7377488ffea2bafeb860303
Cici Ruan <cuicuiruan@google.com>
2022-10-26 01:38:31 +0000

    arc/gio: Send the GIO beta flag to ARC++
    Send the feature flag kArcInputOverlayBeta state to ARC++ on boot time.
    
    Bug: b/241966781
    Test: Manual test and the flag state is received correctly.
    Test: unittests
    Change-Id: Iffe68f84e87235ca3394f29c7e5d631e9c91b46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972060
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063616}

--- chrome/browser/ash/arc/intent_helper/arc_settings_service.cc
+++ chrome/browser/ash/arc/intent_helper/arc_settings_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/mojom/backup_settings.mojom.h"
 #include "ash/components/arc/mojom/pip.mojom.h"
 #include "ash/components/arc/session/arc_bridge_service.h"
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -187,6 +188,7 @@ class ArcSettingsServiceImpl : public TimezoneSettings::Observer,
   void SyncConsumerAutoUpdateToggle() const;
   void SyncDockedMagnifierEnabled() const;
   void SyncFocusHighlightEnabled() const;
+  void SyncGIOBetaEnabled() const;
   void SyncLocale() const;
   void SyncLocationServiceEnabled() const;
   void SyncProxySettings() const;
@@ -461,6 +463,7 @@ void ArcSettingsServiceImpl::SyncBootTimeSettings() const {
   SyncConsumerAutoUpdateToggle();
   SyncDockedMagnifierEnabled();
   SyncFocusHighlightEnabled();
+  SyncGIOBetaEnabled();
   SyncProxySettings();
   SyncReportingConsent(/*initial_sync=*/false);
   SyncPictureInPictureEnabled();
@@ -752,6 +755,12 @@ void ArcSettingsServiceImpl::SyncUse24HourClock() const {
   extras.Set("use24HourClock", use24HourClock);
   SendSettingsBroadcast("org.chromium.arc.intent_helper.SET_USE_24_HOUR_CLOCK",
                         extras);
+}
+
+void ArcSettingsServiceImpl::SyncGIOBetaEnabled() const {
+  SendBoolValueSettingsBroadcast(
+      ash::features::IsArcInputOverlayBetaEnabled(), /*managed=*/false,
+      "org.chromium.arc.intent_helper.ACTION_SET_GIO_BETA_ENABLED");
 }
 
 void ArcSettingsServiceImpl::SyncConsumerAutoUpdateToggle() const {
