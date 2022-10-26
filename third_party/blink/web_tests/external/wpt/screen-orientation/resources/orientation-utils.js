eb63098357d3233e89ce84f9b39d399e3a4aac9d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-26 03:32:39 +0000

    Import wpt@a8246501acb0ec2d34b3c8c369c7b51f63cf1806
    Using wpt-import in Chromium 13fabf1d3420292aa410aa48189abd2ff36ee13e.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I9c57a1222bbc19b1b1dfa4952307e8418ae70838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978813
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1063641}

--- third_party/blink/web_tests/external/wpt/screen-orientation/resources/orientation-utils.js
+++ third_party/blink/web_tests/external/wpt/screen-orientation/resources/orientation-utils.js
@@ -4,14 +4,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {object} options
  * @param {string} options.src - The iframe src
  * @param {Window} options.context - The browsing context in which the iframe will be created
+ * @param {string} options.sandbox - The sandbox attribute for the iframe
  * @returns
  */
 export async function attachIframe(options = {}) {
-  const { src, context } = {
-    ...{ src: "about:blank", context: self },
+  const { src, context, sandbox, allowFullscreen } = {
+    ...{
+      src: "about:blank",
+      context: self,
+      allowFullscreen: true,
+      sandbox: null,
+    },
     ...options,
   };
   const iframe = context.document.createElement("iframe");
+  if (sandbox !== null) iframe.sandbox = sandbox;
+  iframe.allowFullscreen = allowFullscreen;
   await new Promise((resolve) => {
     iframe.onload = resolve;
     iframe.src = src;
