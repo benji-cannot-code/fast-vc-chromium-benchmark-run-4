081bba63889cef1570271b09f40b799740cf7e9d
Benjamin Zielinski <bzielinski@google.com>
2022-10-26 17:30:11 +0000

    [Cast2Class] Cleanup TODOs
    Since code coverage is now provided by browsertests, these TODOs are
    obsolete.
    
    Bug: b/223434114
    Change-Id: Ie55d6be4a62dcae75e60e1596bd0a80d315b19e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3979463
    Reviewed-by: george benz <gbj@google.com>
    Auto-Submit: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063890}

--- chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.cc
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_dialog.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_provider.h"
 #include "url/gurl.h"
 
-// TODO(b/223434114): Add tests for AccessCodeCastDialog
-
 namespace media_router {
 
 namespace {
--- chrome/browser/ui/webui/access_code_cast/access_code_cast_ui.cc
+++ chrome/browser/ui/webui/access_code_cast/access_code_cast_ui.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-// TODO(b/223434114): Add tests for AccessCodeCastUI
-
 namespace media_router {
 
 AccessCodeCastUI::AccessCodeCastUI(content::WebUI* web_ui)
