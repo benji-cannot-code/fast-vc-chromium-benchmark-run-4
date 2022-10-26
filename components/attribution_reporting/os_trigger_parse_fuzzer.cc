0e9048086f67535adee433fcf8b2ab9151d3c840
Thomas Quintanilla <tquintanilla@google.com>
2022-10-26 20:06:15 +0000

    Implements fuzzer for OS-Trigger parsing
    OS-Source parsing fuzzer will follow the same format.
    
    Change-Id: I54a432a35896ab31b779992c5da3c3fb7a3b4c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983492
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063965}

---
+++ components/attribution_reporting/os_trigger_parse_fuzzer.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/command_line.h"
+#include "base/strings/string_piece.h"
+#include "components/attribution_reporting/parse.h"
+
+namespace {
+
+struct Environment {
+  Environment() { base::CommandLine::Init(0, nullptr); }
+};
+
+}  // namespace
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+  attribution_reporting::ParseOsTriggerRegistrationHeader(
+      base::StringPiece(reinterpret_cast<const char*>(data), size));
+  return 0;
+}
