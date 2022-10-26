7712cdb87698da365bfc1f40acca0874cf3ba7a8
Peter Kasting <pkasting@chromium.org>
2022-10-26 14:51:40 +0000

    Replace std::equal() with base::ranges::equal(): mojo/
    Bug: 1368812
    Change-Id: I06f0b8ffdd8bad380d526ca2c640d1b57227c19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3979302
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063794}

--- mojo/core/platform_wrapper_unittest.cc
+++ mojo/core/platform_wrapper_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/memory/unsafe_shared_memory_region.h"
 #include "base/process/process_handle.h"
+#include "base/ranges/algorithm.h"
 #include "build/build_config.h"
 #include "mojo/core/test/mojo_test_base.h"
 #include "mojo/public/c/system/platform_handle.h"
@@ -112,7 +113,7 @@ DEFINE_TEST_CLIENT_TEST_WITH_PIPE(ReadPlatformFile, PlatformWrapperTest, h) {
   std::vector<char> data(message.size());
   EXPECT_EQ(file.ReadAtCurrentPos(data.data(), static_cast<int>(data.size())),
             static_cast<int>(data.size()));
-  EXPECT_TRUE(std::equal(message.begin(), message.end(), data.begin()));
+  EXPECT_TRUE(base::ranges::equal(message, data));
   EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h));
 }
 
