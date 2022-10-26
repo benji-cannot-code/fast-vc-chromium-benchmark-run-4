6b62829a4a0ff9c28ba23649e10d5f1a8f7b8c8e
Thomas Lukaszewicz <tluk@chromium.org>
2022-10-26 00:00:47 +0000

    [ntp] Remove white MV tile pill for CWS themes
    Currently MV tiles are treated differently depending on whether
    a custom background or CWS background is applied. This CL aligns
    CWS background image treatments with that for custom backgrounds.
    
    This effectively sets the text to white and applies background
    protection, see image below.
    
    Before:
    https://screenshot.googleplex.com/u9qpwCcyzbuSurW.png
    
    After:
    https://screenshot.googleplex.com/6ghuf9J7zeG4KxH.png
    
    Bug: 1364767
    Change-Id: I91767efd48dcd8b33229cb6767a58b8615b66e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930706
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063581}

--- chrome/browser/ui/webui/new_tab_page_third_party/new_tab_page_third_party_handler.cc
+++ chrome/browser/ui/webui/new_tab_page_third_party/new_tab_page_third_party_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/new_tab_page_third_party/new_tab_page_third_party_handler.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/themes/theme_service.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/prefs/pref_service.h"
+#include "components/search/ntp_features.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/color/color_provider.h"
 #include "ui/gfx/color_utils.h"
@@ -74,7 +76,8 @@ void NewTabPageThirdPartyHandler::NotifyAboutTheme() {
     theme->has_custom_background =
         theme_provider->HasCustomImage(IDR_THEME_NTP_BACKGROUND);
     theme->id = profile_->GetPrefs()->GetString(prefs::kCurrentThemeID);
-    most_visited->use_title_pill = true;
+    most_visited->use_title_pill =
+        !base::FeatureList::IsEnabled(ntp_features::kNtpRemoveScrim);
   }
   theme->most_visited = std::move(most_visited);
   page_->SetTheme(std::move(theme));
