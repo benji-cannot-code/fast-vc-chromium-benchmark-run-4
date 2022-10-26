85d739e39f20cdf6f821736947ec87d4897cd1e7
Aida Zolic <aidazolic@chromium.org>
2022-10-26 16:16:29 +0000

    DLP: Increase coverage for dlp_drag_drop_notifier.cc
    The OnWidgetDestroying() was not covered by tests. This function just
    calls the base class so the override shouldn't be needed - thus removed
    from dlp_drag_drop_notifier.cc and added a test for the base class, i.e.
    in dlp_data_transfer_notifier_browsertest.cc.
    
    R=ayaelattar@chromium.org
    
    Fixed: 1304202
    Change-Id: Id0ce7466234598db2ee5baf83f64f4a883208b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975681
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063840}

--- chrome/browser/chromeos/policy/dlp/dlp_drag_drop_notifier.h
+++ chrome/browser/chromeos/policy/dlp/dlp_drag_drop_notifier.h
@@ -33,9 +33,6 @@ class DlpDragDropNotifier : public DlpDataTransferNotifier {
   void ProceedPressed(views::Widget* widget);
 
   void CancelPressed(views::Widget* widget);
-
-  // views::WidgetObserver
-  void OnWidgetDestroying(views::Widget* widget) override;
 };
 
 }  // namespace policy
