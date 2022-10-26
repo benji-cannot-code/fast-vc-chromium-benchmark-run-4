b9a224963dd65a196af34641eef43403f12a479c
Curtis McMullan <curtismcmullan@chromium.org>
2022-10-26 23:23:51 +0000

    multiword: shortcircuit candidate generation w pref
    Currently when the user preference for predictive writing is set to
    false we do not show multi word suggestions to the user. However, the
    preference does not prevent candidate generation from occuring in the
    background. This CL fixes this by ensuring the correct preference is
    used when informing the ime sharedlib to generate multi word suggestions
    or not.
    
    BUG=b:255833128
    
    Change-Id: I88e7753660c7f8ae26405924b6eaf88708b7269a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984845
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064072}

--- chrome/browser/ash/input_method/native_input_method_engine_observer.cc
+++ chrome/browser/ash/input_method/native_input_method_engine_observer.cc
@@ -119,8 +119,7 @@ bool IsPhysicalKeyboardAutocorrectEnabled(PrefService* prefs,
 
 bool IsPredictiveWritingEnabled(PrefService* pref_service,
                                 const std::string& engine_id) {
-  return (features::IsAssistiveMultiWordEnabled() &&
-          IsPredictiveWritingPrefEnabled(pref_service, engine_id) &&
+  return (IsPredictiveWritingPrefEnabled(pref_service, engine_id) &&
           IsUsEnglishEngine(engine_id));
 }
 
@@ -463,10 +462,9 @@ InputFieldContext CreateInputFieldContext(
 mojom::TextPredictionMode GetTextPredictionMode(
     const std::string& engine_id,
     const InputFieldContext& context,
-    const PrefService& prefs) {
+    PrefService* prefs) {
   return context.multiword_enabled && context.multiword_allowed &&
-                 prefs.GetBoolean(prefs::kAssistPredictiveWritingEnabled) &&
-                 IsUsEnglishEngine(engine_id)
+                 IsPredictiveWritingEnabled(prefs, engine_id)
              ? mojom::TextPredictionMode::kEnabled
              : mojom::TextPredictionMode::kDisabled;
 }
@@ -496,7 +494,7 @@ mojom::InputFieldInfoPtr CreateInputFieldInfo(
     const std::string& engine_id,
     const ui::TextInputMethod::InputContext& context,
     const InputFieldContext& input_field_context,
-    const PrefService& prefs,
+    PrefService* prefs,
     bool is_normal_screen) {
   // Disable most features on the login screen.
   if (!is_normal_screen) {
@@ -670,9 +668,11 @@ void NativeInputMethodEngineObserver::OnActivate(const std::string& engine_id) {
   // TODO(b/181077907): Always launch the IME service and let IME service decide
   // whether it should shutdown or not.
   if (IsFstEngine(engine_id) && ShouldRouteToNativeMojoEngine(engine_id) &&
-      // The FST Mojo engine is only needed if autocorrect is enabled.
+      // The FST Mojo engine is only needed if autocorrect is enabled ...
       !IsPhysicalKeyboardAutocorrectEnabled(prefs_, engine_id) &&
-      !IsPredictiveWritingEnabled(prefs_, engine_id)) {
+      // ... or if predictive writing is enabled.
+      !(features::IsAssistiveMultiWordEnabled() &&
+        IsPredictiveWritingEnabled(prefs_, engine_id))) {
     connection_factory_.reset();
     remote_manager_.reset();
     input_method_.reset();
@@ -770,7 +770,7 @@ void NativeInputMethodEngineObserver::HandleOnFocusAsyncForNativeMojoEngine(
       InputMethodManager::Get()->GetActiveIMEState()->GetUIStyle() ==
       InputMethodManager::UIStyle::kNormal;
   mojom::InputFieldInfoPtr input_field_info = CreateInputFieldInfo(
-      engine_id, context, input_field_context, *prefs_, is_normal_screen);
+      engine_id, context, input_field_context, prefs_, is_normal_screen);
 
   base::OnceCallback<void(bool)> on_focus_callback =
       base::BindOnce(&NativeInputMethodEngineObserver::ActivateTextClient,
