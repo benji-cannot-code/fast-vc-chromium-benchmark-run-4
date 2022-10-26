88a789308ada6c10b51076b6bf43067ad592d7e6
Kevin Ellis <kevers@google.com>
2022-10-26 18:22:28 +0000

    Make worker raf time relative to worker load time.
    Presently, requestAnimationFrame from a worker reports a frame time
    that is based on a time origin that may be extracted from an external
    source.  Conversely, window.requestAnimationFrame reports a frame
    time that is relative to page load.  For consistency, we should report
    a time relative to worker load when invoked from a web worker.  This
    fixes a privacy exploit where it is possible to determine how long the
    browser has been running and not how long the current page has been
    loaded.
    
    Bug: 1236113
    Change-Id: I0fb42fb2c939059df93f6a8a3f642f5dad6bb047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975149
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063922}

--- third_party/blink/renderer/core/animation_frame/worker_animation_frame_provider.cc
+++ third_party/blink/renderer/core/animation_frame/worker_animation_frame_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/agent.h"
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
 #include "third_party/blink/renderer/core/timing/worker_global_scope_performance.h"
+#include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 #include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 
@@ -64,7 +65,12 @@ void WorkerAnimationFrameProvider::BeginFrame(const viz::BeginFrameArgs& args) {
             }
           }
 
-          double time = (args.frame_time - base::TimeTicks()).InMillisecondsF();
+          auto* global_scope =
+              DynamicTo<WorkerGlobalScope>(provider->context_.Get());
+          DCHECK(global_scope);
+          double time = Performance::ClampTimeResolution(
+              base::TimeTicks::Now() - global_scope->TimeOrigin(),
+              provider->context_->CrossOriginIsolatedCapability());
           provider->callback_collection_.ExecuteFrameCallbacks(time, time);
         }
         provider->begin_frame_provider_->FinishBeginFrame(args);
