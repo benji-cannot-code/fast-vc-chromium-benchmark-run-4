75995893a8ae8117b47f1d9c762d869eec88a451
Pu Shi <pushi@google.com>
2022-10-26 23:13:24 +0000

    [Nearby] Add check to make sure Nearby search concepts are only added when on primary profile.
    TESTED=manually tested and verified behaviors are correct.
    
    Fixed: b/238623564
    Change-Id: I5b812aee280456e7744e16682dad14c941871af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976874
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064057}

--- chrome/browser/ui/webui/settings/ash/multidevice_section.cc
+++ chrome/browser/ui/webui/settings/ash/multidevice_section.cc
@@ -820,6 +820,10 @@ bool MultiDeviceSection::IsFeatureSupported(Feature feature) {
 }
 
 void MultiDeviceSection::RefreshNearbyBackgroundScanningShareSearchConcepts() {
+  if (!NearbySharingServiceFactory::IsNearbyShareSupportedForBrowserContext(
+          profile())) {
+    return;
+  }
   SearchTagRegistry::ScopedTagUpdater updater = registry()->StartUpdate();
   NearbySharingService* nearby_sharing_service =
       NearbySharingServiceFactory::GetForBrowserContext(profile());
@@ -847,6 +851,10 @@ void MultiDeviceSection::RefreshNearbyBackgroundScanningShareSearchConcepts() {
 }
 
 void MultiDeviceSection::OnEnabledChanged(bool enabled) {
+  if (!NearbySharingServiceFactory::IsNearbyShareSupportedForBrowserContext(
+          profile())) {
+    return;
+  }
   SearchTagRegistry::ScopedTagUpdater updater = registry()->StartUpdate();
   if (enabled) {
     updater.RemoveSearchTags(GetNearbyShareOffSearchConcepts());
