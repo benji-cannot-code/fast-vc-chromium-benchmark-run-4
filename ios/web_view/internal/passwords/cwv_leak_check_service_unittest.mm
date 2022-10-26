0d3036c8a14d15e7e87d88a3ded8c77ad4d11571
Peter Kasting <pkasting@chromium.org>
2022-10-26 17:31:59 +0000

    Replace std::equal() with base::ranges::equal(): ios/
    Bug: 1368812
    Change-Id: I0b80c61e198beac2290151f4f6357de4f99c7996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3979098
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063891}

--- ios/web_view/internal/passwords/cwv_leak_check_service_unittest.mm
+++ ios/web_view/internal/passwords/cwv_leak_check_service_unittest.mm
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/ranges/algorithm.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/task_environment.h"
 #import "components/password_manager/core/browser/bulk_leak_check_service.h"
@@ -80,12 +81,11 @@ ACTION_P(CreateFakeBulkLeakCheck, out) {
 }
 
 MATCHER_P(CredentialsAre, credentials, "") {
-  return std::equal(arg.begin(), arg.end(), credentials.get().begin(),
-                    credentials.get().end(),
-                    [](const auto& lhs, const auto& rhs) {
-                      return lhs.username() == rhs.username() &&
-                             lhs.password() == rhs.password();
-                    });
+  return base::ranges::equal(arg, credentials.get(),
+                             [](const auto& lhs, const auto& rhs) {
+                               return lhs.username() == rhs.username() &&
+                                      lhs.password() == rhs.password();
+                             });
 }
 
 class CWVLeakCheckServiceTest : public PlatformTest {
