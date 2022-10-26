3eee4e5111ccc4af09dbbe32052a70925632a69f
jchen10 <jie.a.chen@intel.com>
2022-10-26 00:47:49 +0000

    Re-enable rgb10a2 overlay support
    As Chromium isn't parsing the HDR metadata of some video streams
    correctly, the feature was previously disabled inaccurately. This takes
    it back with the enhanced checks for the presence of HDR stream
    metadata and HDR monitor before choosing rgb10a2 overlay.
    
    Bug: 1362411
    Bug: 1362288
    Bug: skia:13779
    Change-Id: I07b33a8c11ca30db4142eea800eb88b19ad7b0f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917398
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063598}

--- ui/gl/direct_composition_support.cc
+++ ui/gl/direct_composition_support.cc
@@ -101,6 +101,9 @@ gfx::Size g_primary_monitor_size;
 // The number of all visible display monitors on a desktop.
 int g_num_monitors = 0;
 
+// Whether there is a HDR capable display monitor being connected.
+bool g_system_hdr_enabled = false;
+
 // Global direct composition device.
 IDCompositionDevice2* g_dcomp_device = nullptr;
 // Whether swap chain present failed and direct composition should be disabled.
@@ -369,6 +372,10 @@ void UpdateMonitorInfo() {
   } else {
     g_primary_monitor_size = gfx::Size();
   }
+  g_system_hdr_enabled = false;
+  auto dxgi_info = gl::GetDirectCompositionHDRMonitorDXGIInfo();
+  for (const auto& output_desc : dxgi_info->output_descs)
+    g_system_hdr_enabled |= output_desc->hdr_enabled;
 }
 
 }  // namespace
@@ -560,6 +567,12 @@ int GetDirectCompositionNumMonitors() {
   if (g_num_monitors == 0)
     UpdateMonitorInfo();
   return g_num_monitors;
+}
+
+bool DirectCompositionSystemHDREnabled() {
+  if (g_num_monitors == 0)
+    UpdateMonitorInfo();
+  return g_system_hdr_enabled;
 }
 
 DXGI_FORMAT GetDirectCompositionSDROverlayFormat() {
