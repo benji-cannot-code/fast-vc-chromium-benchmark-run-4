d0ff2df2134cbff780438952b1f7e751827f08ee
Oleksandr Peletskyi <peletskyi@google.com>
2022-10-26 10:56:16 +0000

    Declare minimal IWA external install options
    The IwaExternalInstallOptions class will contain all the necessary
    information to install a IWA form the enterprise policy. So far this
    class contains only Web Bundle ID and update manifest URL. In future
    it will include:
     - path to the web bundle
     - URL of the web bundle
     - etc.
    
    The IwaExternalInstallOptions is similar to ExternalInstallOptions but
    overall the installing of the IWA will be more complex. Thus it makes
    sense to use getters rather than having a struct of fieds.
    
    Design doc: go/iwa-mgs
    
    Bug: 1364132
    Change-Id: I7a5e36d9bf226a2c905966c7b8fce03f6bfee817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3970675
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Oleksandr Peletskyi <peletskyi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063727}

---
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_constants.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_constants.h"
+
+namespace web_app {
+
+const char kUpdateManifestUrlKey[] = "update_manifest_url";
+const char kWebBundleIdKey[] = "web_bundle_id";
+
+}  // namespace web_app
---
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_policy_constants.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_POLICY_ISOLATED_WEB_APP_POLICY_CONSTANTS_H_
+#define CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_POLICY_ISOLATED_WEB_APP_POLICY_CONSTANTS_H_
+
+namespace web_app {
+
+// Keys for the IsolatedWebAppInstallForceList preference.
+extern const char kUpdateManifestUrlKey[];
+extern const char kWebBundleIdKey[];
+
+}  // namespace web_app
+
+#endif  // CHROME_BROWSER_WEB_APPLICATIONS_ISOLATED_WEB_APPS_POLICY_ISOLATED_WEB_APP_POLICY_CONSTANTS_H_
