5b46e2bf38388552293ed76cf7a5600adf989a11
James Cook <jamescook@chromium.org>
2022-10-26 22:12:32 +0000

    Clean up several deprecated user metrics actions for status area
    A set of metrics was added in 2016 to inform a redesign of the ChromeOS
    status area. That redesign was finished long ago. Our PM says we no
    longer need these metrics.
    
    In some cases the recording code is already gone, most likely deleted
    in some earlier cleanup.
    
    Bug: b:251738050
    Change-Id: I191a8760a0293519fa7a87e219dc965c2f1c8d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978739
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064029}

--- ash/system/tracing_notification_controller.cc
+++ ash/system/tracing_notification_controller.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
 #include "base/bind.h"
-#include "base/metrics/user_metrics.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
@@ -28,8 +27,6 @@ namespace {
 const char kNotifierId[] = "ash.tracing";
 
 void HandleNotificationClick() {
-  base::RecordAction(
-      base::UserMetricsAction("StatusArea_Tracing_Default_Selected"));
   Shell::Get()->system_tray_model()->client()->ShowChromeSlow();
 }
 
--- ash/system/update/update_notification_controller.cc
+++ ash/system/update/update_notification_controller.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
 #include "chromeos/ash/components/dbus/update_engine/update_engine_client.h"
@@ -294,8 +293,6 @@ void UpdateNotificationController::RestartForUpdate() {
   }
   // System updates require restarting the device.
   Shell::Get()->session_controller()->RequestRestartForUpdate();
-  base::RecordAction(
-      base::UserMetricsAction("StatusArea_OS_Update_Default_Selected"));
 }
 
 void UpdateNotificationController::RestartCancelled() {
