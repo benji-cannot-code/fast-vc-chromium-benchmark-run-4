f7f3933140fa0218fefc5e46c03b3f0cd81c9c93
Bohdan Tyshchenko <bohdanty@google.com>
2022-10-26 09:28:49 +0000

    [OOBE][DeviceDisabledScreen] - Polymer3 Check-In
    Check-In a modularized version of the Device Disabled Screen.
    
    BUG=b:243382555
    
    Change-Id: I0cc794fae3eb741433dd5671ad6e9c0e8d84bc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3974555
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063707}

--- chrome/browser/resources/chromeos/login/screens/common/device_disabled.js
+++ chrome/browser/resources/chromeos/login/screens/common/device_disabled.js
@@ -7,7 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for Device Disabled message screen.
  */
 
-/* #js_imports_placeholder */
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.m.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OOBE_UI_STATE} from '../../components/display_manager_types.m.js';
+
 
 /**
  * @constructor
@@ -15,9 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {OobeI18nBehaviorInterface}
  * @implements {LoginScreenBehaviorInterface}
  */
-const DeviceDisabledElementBase = Polymer.mixinBehaviors(
+const DeviceDisabledElementBase = mixinBehaviors(
     [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    Polymer.Element);
+    PolymerElement);
 
 /**
  * @typedef {{
@@ -31,7 +43,9 @@ class DeviceDisabled extends DeviceDisabledElementBase {
     return 'device-disabled-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
