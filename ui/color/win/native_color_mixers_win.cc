98af2a61cbd9731643a3b9163b5b54807adbc31e
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2022-10-26 16:31:19 +0000

    [Fluent Scrollbars] Implement colors for Fluent scrollbar parts.
    The CL adds colors for Fluent scrollbar parts such as thumb, track,
    corner, button, and arrow.
    
    List of changes:
        1. Moved native_theme_features.cc/h into a separate gn component to
           use in the ui/color/ and avoid dependency cycles. 
        2. Overrode required scrollbar colors in the native win mixer.
        3. Updated baselines for existing tests.
        4. Created additional web test suite to verify dark mode scrollbars.
    
    Bug: 1353574, 1351598
    Change-Id: I0504309977ab094bc7f6eaae0230c2976f215742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935315
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063847}

--- ui/color/win/native_color_mixers_win.cc
+++ ui/color/win/native_color_mixers_win.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/color/win/accent_color_observer.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
+#include "ui/native_theme/native_theme_features.h"
 
 namespace ui {
 
@@ -124,10 +125,32 @@ void AddNativeCoreColorMixer(ColorProvider* provider,
 
 void AddNativeUiColorMixer(ColorProvider* provider,
                            const ColorProviderManager::Key& key) {
-  if (key.contrast_mode == ColorProviderManager::ContrastMode::kNormal)
+  if (key.contrast_mode == ColorProviderManager::ContrastMode::kNormal &&
+      !IsFluentScrollbarEnabled())
     return;
 
   ColorMixer& mixer = provider->AddMixer();
+
+  // Override scrollbar colors for the Fluent scrollbar.
+  // TODO(crbug.com/1378337): Implement high contrast mode for the Fluent
+  // scrollbar. Currently, normal and high contrast modes are the same.
+  if (IsFluentScrollbarEnabled()) {
+    const bool dark_mode =
+        key.color_mode == ColorProviderManager::ColorMode::kDark;
+
+    mixer[kColorScrollbarArrowForeground] = {
+        dark_mode ? SkColorSetA(SK_ColorWHITE, 0x8B)
+                  : SkColorSetA(SK_ColorBLACK, 0x72)};
+    mixer[kColorScrollbarArrowForegroundPressed] = {
+        dark_mode ? SkColorSetA(SK_ColorWHITE, 0xC8)
+                  : SkColorSetA(SK_ColorBLACK, 0x9B)};
+    mixer[kColorScrollbarThumb] = {mixer[kColorScrollbarArrowForeground]};
+    mixer[kColorScrollbarTrack] = {dark_mode ? SkColorSetRGB(0x2C, 0x2C, 0x2C)
+                                             : SkColorSetRGB(0xFC, 0xFC, 0xFC)};
+  }
+
+  if (key.contrast_mode == ColorProviderManager::ContrastMode::kNormal)
+    return;
 
   mixer[kColorButtonForegroundChecked] = {
       key.color_mode == ColorProviderManager::ColorMode::kDark
