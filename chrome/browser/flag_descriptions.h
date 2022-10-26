a50d9497e21bab4c5b9301e4cdb5f2ae1c3bcbf4
Simon Hangl <simonha@google.com>
2022-10-26 15:50:42 +0000

    [MSC] Enable feature flags for getDisplayMediaSet for Lacros.
    Before this change the feature flags for getDisplayMediaSet
    were only available for ChromeOS ash. This change enables
    them also for lacros.
    
    Bug: 1300883
    Change-Id: I924b0733f169d3060e310e14cd7b7307fc22b39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902462
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063822}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2919,12 +2919,6 @@ extern const char kFastPairSavedDevicesDescription[];
 extern const char kFrameSinkDesktopCapturerInCrdName[];
 extern const char kFrameSinkDesktopCapturerInCrdDescription[];
 
-extern const char kGetDisplayMediaSetName[];
-extern const char kGetDisplayMediaSetDescription[];
-
-extern const char kGetDisplayMediaSetAutoSelectAllScreensName[];
-extern const char kGetDisplayMediaSetAutoSelectAllScreensDescription[];
-
 extern const char kUseHDRTransferFunctionName[];
 extern const char kUseHDRTransferFunctionDescription[];
 extern const char kDoubleTapToZoomInTabletModeName[];
@@ -3573,6 +3567,12 @@ extern const char kDisableIdleSocketsCloseOnMemoryPressureDescription[];
 
 extern const char kDisableOfficeEditingComponentAppName[];
 extern const char kDisableOfficeEditingComponentAppDescription[];
+
+extern const char kGetDisplayMediaSetName[];
+extern const char kGetDisplayMediaSetDescription[];
+
+extern const char kGetDisplayMediaSetAutoSelectAllScreensName[];
+extern const char kGetDisplayMediaSetAutoSelectAllScreensDescription[];
 
 extern const char kVaapiAV1DecoderName[];
 extern const char kVaapiAV1DecoderDescription[];
