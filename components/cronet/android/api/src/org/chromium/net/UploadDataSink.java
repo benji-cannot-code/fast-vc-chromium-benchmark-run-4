7018fd00d91da91486c4585f8ce00ee1a615cba6
Dan Stahr <danstahr@google.com>
2022-10-27 12:44:42 +0000

    Promote stable APIs from Experimental classes to non-experimental counterparts.
    See
    https://docs.google.com/document/d/1xUDc8XBdMxbjeTNvQXaNQMQDbXztJllFMYy9OzOG2-U/edit?resourcekey=0-dtH686zVbr2oWyfMKWHs7A
    for more details.
    
    Change-Id: I0f90631dc87dad3ad03a2530f64a73eb05ccc036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975392
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Dan Stahr <danstahr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064283}

--- components/cronet/android/api/src/org/chromium/net/UploadDataSink.java
+++ components/cronet/android/api/src/org/chromium/net/UploadDataSink.java
@@ -6,19 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net;
 
 /**
- * Defines callbacks methods for {@link UploadDataProvider}. All methods
- * may be called synchronously or asynchronously, on any thread.
+ * Defines callbacks methods for {@link UploadDataProvider}. All methods may be called synchronously
+ * or asynchronously, on any thread.
  */
 public abstract class UploadDataSink {
     /**
      * Called by {@link UploadDataProvider} when a read succeeds.
-     * @param finalChunk For chunked uploads, {@code true} if this is the final
-     *     read. It must be {@code false} for non-chunked uploads.
+     *
+     * @param finalChunk For chunked uploads, {@code true} if this is the final read. It must be
+     * {@code false} for non-chunked uploads.
      */
     public abstract void onReadSucceeded(boolean finalChunk);
 
     /**
      * Called by {@link UploadDataProvider} when a read fails.
+     *
      * @param exception Exception passed on to the embedder.
      */
     public abstract void onReadError(Exception exception);
@@ -29,8 +31,9 @@ public abstract class UploadDataSink {
     public abstract void onRewindSucceeded();
 
     /**
-     * Called by {@link UploadDataProvider} when a rewind fails, or if rewinding
-     * uploads is not supported.
+     * Called by {@link UploadDataProvider} when a rewind fails, or if rewinding uploads is not
+     * supported.
+     *
      * @param exception Exception passed on to the embedder.
      */
     public abstract void onRewindError(Exception exception);
--- components/cronet/android/api/src/org/chromium/net/apihelpers/InMemoryTransformCronetCallback.java
+++ components/cronet/android/api/src/org/chromium/net/apihelpers/InMemoryTransformCronetCallback.java
@@ -36,7 +36,9 @@ public abstract class InMemoryTransformCronetCallback<T> extends ImplicitFlowCon
     private ByteArrayOutputStream mResponseBodyStream;
     private WritableByteChannel mResponseBodyChannel;
 
-    /** The set of listeners observing the associated request. */
+    /**
+     * The set of listeners observing the associated request.
+     */
     private final Set<CronetRequestCompletionListener<? super T>> mListeners =
             new LinkedHashSet<>();
 
