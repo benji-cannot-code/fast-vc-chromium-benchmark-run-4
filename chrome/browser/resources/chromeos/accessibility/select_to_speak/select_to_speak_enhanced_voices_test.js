e2127624a81bae6b2f83b0bc0a4eb1f4dcb726a8
Josiah Krutz <josiahk@google.com>
2022-10-27 23:02:29 +0000

    [Select-to-speak] Migrate Prefs from chrome.storage to Chrome User Prefs
    Migrate Select-to-speak preferences out of chrome.storage.sync, to be
    backed instead by Chrome User Prefs. This paves a way for the
    Select-to-speak settings toggles and dropdowns to migrate from the
    Extension Options page, into the main Chrome OS Settings app.
    
    AX-Relnotes: N/A.
    Bug: 1354821
    Change-Id: I73b1dc589964eda0b269fc8c20cf17fea488e779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968799
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064588}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_enhanced_voices_test.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_enhanced_voices_test.js
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 GEN_INCLUDE(['select_to_speak_e2e_test_base.js']);
-GEN_INCLUDE(['../common/testing/fake_chrome_event.js']);
-GEN_INCLUDE(['../common/testing/fake_settings_private.js']);
 GEN_INCLUDE(['../common/testing/mock_tts.js']);
 
 SelectToSpeakEnhancedNetworkTtsVoicesTest = class extends SelectToSpeakE2ETest {
@@ -21,16 +19,6 @@ SelectToSpeakEnhancedNetworkTtsVoicesTest = class extends SelectToSpeakE2ETest {
           this.confirmationDialogShowCount_ += 1;
           callback(this.confirmationDialogResponse_);
         };
-
-    this.enhancedNetworkVoicesPolicyKey_ =
-        'settings.a11y.enhanced_network_voices_in_select_to_speak_allowed';
-    this.mockSettingsPrivate_ = new FakeSettingsPrivate([
-      {type: 'number', key: 'settings.tts.speech_rate', value: 1.0},
-      {type: 'number', key: 'settings.tts.speech_pitch', value: 1.0},
-      {type: 'boolean', key: this.enhancedNetworkVoicesPolicyKey_, value: true},
-    ]);
-    this.mockSettingsPrivate_.allowSetPref();
-    chrome.settingsPrivate = this.mockSettingsPrivate_;
 
     chrome.i18n = {
       getMessage(msgid) {
@@ -48,8 +36,6 @@ SelectToSpeakEnhancedNetworkTtsVoicesTest = class extends SelectToSpeakE2ETest {
         'SelectToSpeakConstants',
         '/select_to_speak/select_to_speak_constants.js');
     await importModule('PrefsManager', '/select_to_speak/prefs_manager.js');
-
-    selectToSpeak.prefsManager_.initPreferences();
   }
 
   /** @override */
@@ -58,10 +44,9 @@ SelectToSpeakEnhancedNetworkTtsVoicesTest = class extends SelectToSpeakE2ETest {
   }
 
   // Sets the policy to allow or disallow the network voices.
-  async setEnhancedNetworkVoicesPolicy(allowed) {
-    await new Promise(
-        resolve => this.mockSettingsPrivate_.setPref(
-            this.enhancedNetworkVoicesPolicyKey_, allowed, '', resolve));
+  setEnhancedNetworkVoicesPolicy(allowed) {
+    chrome.settingsPrivate.setPref(
+        PrefsManager.ENHANCED_VOICES_POLICY_KEY, allowed, '', () => {});
   }
 };
 
@@ -137,7 +122,9 @@ AX_TEST_F(
         assertTrue(selectToSpeak.prefsManager_.enhancedNetworkVoicesEnabled());
 
         // Sets the policy to disallow network voices.
-        await this.setEnhancedNetworkVoicesPolicy(/* allowed= */ false);
+        this.setEnhancedNetworkVoicesPolicy(/* allowed= */ false);
+        // Pref change is slow to propagate so also set directly.
+        selectToSpeak.prefsManager_.enhancedNetworkVoicesAllowed_ = false;
         assertFalse(selectToSpeak.prefsManager_.enhancedNetworkVoicesEnabled());
       })]);
       const textNode = this.findTextNode(root, 'This is some text');
@@ -151,7 +138,7 @@ AX_TEST_F(
 AX_TEST_F(
     'SelectToSpeakEnhancedNetworkTtsVoicesTest',
     'DisablesDialogIfDisallowedByPolicy', async function() {
-      await this.setEnhancedNetworkVoicesPolicy(/* allowed= */ false);
+      this.setEnhancedNetworkVoicesPolicy(/* allowed= */ false);
 
       const root = await this.runWithLoadedTree(
           'data:text/html;charset=utf-8,' +
