de0f146b5ce206a0e79d85b5fb796d5f5240c8dd
Nathan Memmott <memmott@chromium.org>
2022-10-27 23:03:38 +0000

    [LevelDBScopes] Add options argument to AcquireLocks
    Adds the AcquireOptions struct that's passed into AcquireLocks.
    Currently there is only the ensure_async option which when true, does
    not allow for the callback to be run synchronously.
    
    Bug: 1354312
    Change-Id: Ib2907e4322a69419aa37d1b5e37198b8f9988571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983665
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064590}

--- components/services/storage/indexed_db/locks/partitioned_lock_manager.cc
+++ components/services/storage/indexed_db/locks/partitioned_lock_manager.cc
@@ -23,6 +23,8 @@ PartitionedLockManager::PartitionedLockRequest::PartitionedLockRequest(
     LockType type)
     : lock_id(std::move(lock_id)), type(type) {}
 
+PartitionedLockManager::AcquireOptions::AcquireOptions() = default;
+
 PartitionedLockManager::LockRequest::LockRequest() = default;
 PartitionedLockManager::LockRequest::LockRequest(
     LockType type,
@@ -66,7 +68,8 @@ int64_t PartitionedLockManager::RequestsWaitingForTesting() const {
 void PartitionedLockManager::AcquireLocks(
     base::flat_set<PartitionedLockRequest> lock_requests,
     base::WeakPtr<PartitionedLockHolder> locks_holder,
-    LocksAcquiredCallback callback) {
+    LocksAcquiredCallback callback,
+    AcquireOptions acquire_options) {
   if (!locks_holder) {
     std::move(callback).Run();
     return;
@@ -78,7 +81,8 @@ void PartitionedLockManager::AcquireLocks(
   // stack (every task would execute in the stack of its parent task,
   // recursively).
   scoped_refptr<base::RefCountedData<bool>> run_callback_synchonously =
-      base::MakeRefCounted<base::RefCountedData<bool>>(true);
+      base::MakeRefCounted<base::RefCountedData<bool>>(
+          !acquire_options.ensure_async);
 
   locks_holder->locks.reserve(lock_requests.size());
   size_t num_closure_calls = lock_requests.size();
