f258ba08e16278c2ab1699aeae084b01b466d8d1
Sammie Quon <sammiequon@chromium.org>
2022-10-27 00:40:36 +0000

    float: Untuck a window when it is activated
    It can be activated by overview, alt+tab, shelf, applist, etc.
    
    Test: manual, ash_unittests TabletWindowFloatTest.UntuckWindowOnActivation
    Change-Id: I3f174f4b0cb2948f6af4e56f2e0d959b0f356ae4
    Fixed: b/255628917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978712
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064113}

--- ash/wm/float/scoped_window_tucker.cc
+++ ash/wm/float/scoped_window_tucker.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/wm/public/activation_client.h"
 
 namespace ash {
 
@@ -109,10 +110,16 @@ ScopedWindowTucker::ScopedWindowTucker(aura::Window* window, bool left)
   tuck_handle_widget_->Init(std::move(params));
 
   tuck_handle_widget_->SetContentsView(std::make_unique<TuckHandle>(
-      base::BindRepeating(&ScopedWindowTucker::OnButtonPressed,
+      base::BindRepeating(&ScopedWindowTucker::UntuckWindow,
                           base::Unretained(this)),
       left));
   tuck_handle_widget_->Show();
+
+  Shell::Get()->activation_client()->AddObserver(this);
+}
+
+ScopedWindowTucker::~ScopedWindowTucker() {
+  Shell::Get()->activation_client()->RemoveObserver(this);
 }
 
 void ScopedWindowTucker::AnimateTuck(bool left) {
@@ -168,9 +175,15 @@ void ScopedWindowTucker::AnimateTuck(bool left) {
                     gfx::Tween::ACCEL_20_DECEL_100);
 }
 
-void ScopedWindowTucker::OnButtonPressed() {
-  // Untuck the window, which sets the window bounds back onscreen.
-  // Destroys `this`.
+void ScopedWindowTucker::OnWindowActivated(ActivationReason reason,
+                                           aura::Window* gained_active,
+                                           aura::Window* lost_active) {
+  // Note that `UntuckWindow()` destroys `this`.
+  if (gained_active == window_)
+    UntuckWindow();
+}
+
+void ScopedWindowTucker::UntuckWindow() {
   Shell::Get()->float_controller()->MaybeUntuckFloatedWindowForTablet(window_);
 }
 
