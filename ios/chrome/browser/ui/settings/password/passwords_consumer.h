ec315e4a31b16d8da031aae3519b4acec0ada603
Veronique Nguyen <veronguyen@google.com>
2022-10-27 16:04:45 +0000

    Receive AffiliationGroup object from SavedPasswordsPresenter
    - Add setAffiliatedGroups in the Password Consumer and call it in the
    Password Mediator
    - Handle blocked site logic.
    - Add and update unit tests.
    
    Bug: 1355955
    Change-Id: I37d0d2974064f636dcf73242e24f7cc58ef3f02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975369
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064353}

--- ios/chrome/browser/ui/settings/password/passwords_consumer.h
+++ ios/chrome/browser/ui/settings/password/passwords_consumer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 struct CredentialUIEntry;
+class AffiliatedGroup;
 }  // namespace password_manager
 
 // Enum with all possible UI states of password check.
@@ -40,9 +41,20 @@ typedef NS_ENUM(NSInteger, PasswordCheckUIState) {
     unmutedCompromisedPasswordsCount:(NSInteger)count;
 
 // Displays password and blocked forms.
+// TODO(crbug.com/1359392): Remove this.
 - (void)setPasswords:(std::vector<password_manager::CredentialUIEntry>)passwords
         blockedSites:
             (std::vector<password_manager::CredentialUIEntry>)blockedSites;
+
+// Displays affiliated groups for the Password Manager.
+// This method relates to the -setPasswords method above. This will eventually
+// replace it when the feature is done.
+- (void)setAffiliatedGroups:
+            (const std::vector<password_manager::AffiliatedGroup>&)
+                affiliatedGroups
+               blockedSites:
+                   (const std::vector<password_manager::CredentialUIEntry>&)
+                       blockedSites;
 
 // Updates "On/Off" state for Passwords In Other Apps item.
 - (void)updatePasswordsInOtherAppsDetailedText;
