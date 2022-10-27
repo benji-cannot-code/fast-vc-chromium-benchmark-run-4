2e5f86ecb8695d949b470ebce4c413296b2d18d4
Wei4 Wang <wei4.wang@intel.com>
2022-10-27 00:56:52 +0000

    [ComputePressure] Add Permissions Policy integration
    Make ComputePressure controlled by Permissions Policy according to the
    spec [1].
    
    [1] https://wicg.github.io/compute-pressure/#policy-control
    
    Bug: 1351781
    Change-Id: I2bfe16ddc666c497c8d3f82f8c482369490eefb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963324
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064123}

--- third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
+++ third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/compute_pressure/pressure_observer.h"
 
+#include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_observer_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_record.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_source.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/modules/compute_pressure/pressure_observer_manager.h"
 #include "third_party/blink/renderer/modules/compute_pressure/pressure_record.h"
@@ -17,6 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
+
+namespace {
+
+constexpr char kFeaturePolicyBlocked[] =
+    "Access to the feature \"compute pressure\" is disallowed by permissions "
+    "policy.";
+
+}  // namespace
 
 PressureObserver::PressureObserver(V8PressureUpdateCallback* observer_callback,
                                    PressureObserverOptions* options,
@@ -52,6 +62,16 @@ void PressureObserver::observe(ScriptState* script_state,
   if (execution_context->IsContextDestroyed()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kNotSupportedError,
                                       "Execution context is detached.");
+    return;
+  }
+
+  // Checks whether the document is allowed by Permissions Policy to call
+  // Compute Pressure API.
+  if (!execution_context->IsFeatureEnabled(
+          mojom::blink::PermissionsPolicyFeature::kComputePressure,
+          ReportOptions::kReportOnFailure)) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kNotAllowedError,
+                                      kFeaturePolicyBlocked);
     return;
   }
 
