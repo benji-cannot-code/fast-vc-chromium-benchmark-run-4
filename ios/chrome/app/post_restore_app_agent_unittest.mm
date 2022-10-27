447f8b94cf0d1be2eee825b8f9ad4c34de28b22e
Jérôme Lebel <jlebel@chromium.org>
2022-10-27 12:37:35 +0000

    [iOS] Remove unused authentication_service_fake.h import
    This is a followup for crrev.com/c/3978491.
    
    Bug: 1376908
    Change-Id: I2ec69e5cbd59bad7c65fae9d615eba77bd73a139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3982703
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064281}

--- ios/chrome/app/post_restore_app_agent_unittest.mm
+++ ios/chrome/app/post_restore_app_agent_unittest.mm
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_delegate_fake.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
-#import "ios/chrome/browser/signin/authentication_service_fake.h"
 #import "ios/chrome/browser/signin/signin_util.h"
 #import "ios/chrome/browser/ui/post_restore_signin/features.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
