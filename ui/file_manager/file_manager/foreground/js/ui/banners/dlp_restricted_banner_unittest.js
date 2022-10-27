b05e98e4c9095769d30c8ecd452eb10f79a26bc7
Aida Zolic <aidazolic@chromium.org>
2022-10-27 22:13:02 +0000

    DLP: Allow all roots for DlpRestrictedBanner
    Sets the list of allowedVolumes() to all root types, as the DLP banner
    can be shown wherever the user currently navigates to. The type of the
    dialog will disabled some volumes, such as Trash in save-as.
    
    Fixed: 1364014
    Change-Id: Id310b47fbc8debbaed5e80868fe5a40369312e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986467
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064567}

---
+++ ui/file_manager/file_manager/foreground/js/ui/banners/dlp_restricted_banner_unittest.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+
+import {VolumeManagerCommon} from '../../../../common/js/volume_manager_types.js';
+
+import {DlpRestrictedBanner} from './dlp_restricted_banner.js';
+
+/** @type{!DlpRestrictedBanner} */
+let dlpRestrictedBanner;
+
+export function setUp() {
+  const htmlTemplate = `<dlp-restricted-banner>
+          <span slot="text"></span>
+          </dlp-restricted-banner>`;
+  document.body.innerHTML = htmlTemplate;
+  dlpRestrictedBanner = /** @type{!DlpRestrictedBanner} */ (
+      document.body.querySelector('dlp-restricted-banner'));
+}
+
+/**
+ * Test that the banner is shown in all root types.
+ */
+export function testAllowedVolumes() {
+  assertEquals(
+      dlpRestrictedBanner.allowedVolumes().length,
+      Object.values(VolumeManagerCommon.RootType).length);
+}
