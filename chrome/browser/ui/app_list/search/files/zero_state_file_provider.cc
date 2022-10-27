5f4ffeae5a9d34092a461e8bf32faaca0c7bf394
Toni Barzic <tbarzic@chromium.org>
2022-10-27 01:29:20 +0000

    Reland "Add separate app launcher search provider for zero state"
    This relands commit 98ab2f0f400caa4d9695001beefb9789b8bed237.
    
    The commit was reverted because it broke
    HelpAppSearchBrowserTest.ReleaseNotesDecreasesTimesShownOnAppListOpen on
    official build - the zero state callback was getting called before test
    search provider returned results, causing continue section not to
    surface. The fix is to explicitly enable continue section in tests. Real
    continue section providers will return asynchronously, which will give
    test provider enough time to populate test results. A better fix, where
    test provider will have appropriate, zero-state result type is coming
    in CL:3972927.
    
    Original change's description:
    > Add separate app launcher search provider for zero state
    >
    > With this change, all results providers in zero-state have a
    > "zero state" result type, which makes
    > SearchProvider::ShouldBlockZeroState() unnecessary - whether zero state
    > should be blocked can be inferred from the provider result type.
    >
    > BUG=1352586
    >
    > Change-Id: Ica0607af811d39a1d55420436bac6e70044c7e94
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957358
    > Reviewed-by: Rachel Wong <wrong@chromium.org>
    > Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1063649}
    
    Bug: 1352586
    Change-Id: I546443f0175a65803eb91eb379503638d7469b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984753
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064137}

--- chrome/browser/ui/app_list/search/files/zero_state_file_provider.cc
+++ chrome/browser/ui/app_list/search/files/zero_state_file_provider.cc
@@ -79,10 +79,6 @@ ash::AppListSearchResultType ZeroStateFileProvider::ResultType() const {
   return ash::AppListSearchResultType::kZeroStateFile;
 }
 
-bool ZeroStateFileProvider::ShouldBlockZeroState() const {
-  return true;
-}
-
 void ZeroStateFileProvider::StartZeroState() {
   query_start_time_ = base::TimeTicks::Now();
 
