9be8f756f8356f793a56468f6e4f332359723cd5
Joemer Ramos <joemerramos@chromium.org>
2022-10-27 18:16:18 +0000

    [iOS] Exit fullscreen mode when permission prompt shows
    These changes are behind a feature flag. When the feature flag is
    enabled, if the user is in fullscreen mode, the user will exit
    fullscreen mode before the permission prompt is shown. This needs to be
    done since the user can't see the permission prompt or the following
    infobar message in fullscreen mode.
    
    Fixed: 1315263
    Change-Id: Ifcf7006c1fb675915daaee10399249d0e94d0568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984665
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064440}

--- ios/web/web_state/ui/crw_wk_ui_handler.mm
+++ ios/web/web_state/ui/crw_wk_ui_handler.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/logging.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/strings/sys_string_conversions.h"
+#import "ios/web/common/features.h"
 #import "ios/web/navigation/wk_navigation_action_util.h"
 #import "ios/web/navigation/wk_navigation_util.h"
 #import "ios/web/public/ui/context_menu_params.h"
@@ -97,6 +98,21 @@ enum class PermissionRequest {
       break;
   }
   base::UmaHistogramEnumeration(kPermissionRequestsHistogram, request);
+  if (@available(iOS 16.0, *)) {
+    if (base::FeatureList::IsEnabled(web::features::kEnableFullscreenAPI)) {
+      __weak __typeof(self) weakSelf = self;
+      [webView closeAllMediaPresentationsWithCompletionHandler:^{
+        web::WebStateImpl* webStateImpl = weakSelf.webStateImpl;
+        if (webStateImpl) {
+          web::GetWebClient()->WillDisplayMediaCapturePermissionPrompt(
+              webStateImpl);
+        }
+        decisionHandler(WKPermissionDecisionPrompt);
+      }];
+      return;
+    }
+  }
+
   web::GetWebClient()->WillDisplayMediaCapturePermissionPrompt(
       self.webStateImpl);
   decisionHandler(WKPermissionDecisionPrompt);
