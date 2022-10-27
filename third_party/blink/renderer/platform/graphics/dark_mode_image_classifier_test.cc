2000ec229d9f2dd38f6cba82310733a3c3dd15a5
Peter E Conn <peconn@chromium.org>
2022-10-27 09:45:33 +0000

    🌓 Add alternative Dark Mode Image classifier policy.
    Manual testing discovered that a simpler image classification policy
    could be used to achieve better results when determining whether an
    image should be inverted or not.
    
    This change adds an option to use that policy.
    
    Bug: 1361235
    Change-Id: Ib57acd6998a4220a0d5dbd7ea36253f2a19394c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874922
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064241}

--- third_party/blink/renderer/platform/graphics/dark_mode_image_classifier_test.cc
+++ third_party/blink/renderer/platform/graphics/dark_mode_image_classifier_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/graphics/bitmap_image.h"
+#include "third_party/blink/renderer/platform/graphics/dark_mode_settings.h"
 #include "third_party/blink/renderer/platform/graphics/image.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_image.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support_with_mock_scheduler.h"
@@ -23,7 +24,8 @@ const float kEpsilon = 0.00001;
 class DarkModeImageClassifierTest : public testing::Test {
  public:
   DarkModeImageClassifierTest() {
-    dark_mode_image_classifier_ = std::make_unique<DarkModeImageClassifier>();
+    dark_mode_image_classifier_ = std::make_unique<DarkModeImageClassifier>(
+        DarkModeImageClassifierPolicy::kNumColorsWithMlFallback);
   }
 
   // Loads the image from |file_name|.
