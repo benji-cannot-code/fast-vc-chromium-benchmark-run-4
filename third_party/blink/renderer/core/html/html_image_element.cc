38239a6e4a44cb59be705a21a02a512a4b2c3d5f
Yoav Weiss <yoavweiss@chromium.org>
2022-10-27 12:18:44 +0000

    Another use counter for compat impact of sizes=auto related spec changes
    Looks like the use-counter landed previously [1] is not measuring what
    we want it to measure :/ This CL fixes that by adding a new counter,
    that measures lazy loaded images with a 'w' descriptor, and *without*
    a "sizes" attribute.
    
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3868743
    
    Bug: 1359051
    Change-Id: I1ea83b518a8702163af60da671373e10534596a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981433
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064276}

--- third_party/blink/renderer/core/html/html_image_element.cc
+++ third_party/blink/renderer/core/html/html_image_element.cc
@@ -115,7 +115,7 @@ HTMLImageElement::HTMLImageElement(Document& document, bool created_by_parser)
       is_ad_related_(false),
       is_lcp_element_(false),
       is_changed_shortly_after_mouseover_(false),
-      has_sizes_attribute_(false),
+      has_sizes_attribute_in_img_or_sibling_(false),
       is_lazy_loaded_(false),
       referrer_policy_(network::mojom::ReferrerPolicy::kDefault) {
   SetHasCustomStyleCallbacks();
@@ -297,7 +297,6 @@ void HTMLImageElement::SetBestFitURLAndDPRFromImageCandidate(
       listener_ = MakeGarbageCollected<ViewportChangeListener>(this);
 
     GetDocument().GetMediaQueryMatcher().AddViewportListener(listener_);
-    // If we have a listener, that means a viewport dependent image
   } else if (listener_) {
     GetDocument().GetMediaQueryMatcher().RemoveViewportListener(listener_);
   }
@@ -378,11 +377,6 @@ void HTMLImageElement::ParseAttribute(
     }
   } else {
     HTMLElement::ParseAttribute(params);
-  }
-  if (has_sizes_attribute_ && is_lazy_loaded_ && listener_) {
-    UseCounter::Count(
-        GetDocument(),
-        WebFeature::kViewportDependentLazyLoadedImageWithSizesAttribute);
   }
 }
 
@@ -804,7 +798,8 @@ FetchParameters::ResourceWidth HTMLImageElement::GetResourceWidth() const {
 float HTMLImageElement::SourceSize(Element& element) {
   float value;
   // We only care if the sizes attribute exist here for use counter purposes..
-  has_sizes_attribute_ = SourceSizeValue(&element, GetDocument(), value);
+  has_sizes_attribute_in_img_or_sibling_ =
+      SourceSizeValue(&element, GetDocument(), value);
   return value;
 }
 
@@ -890,6 +885,17 @@ void HTMLImageElement::EnsurePrimaryContent() {
 
 bool HTMLImageElement::IsCollapsed() const {
   return layout_disposition_ == LayoutDisposition::kCollapsed;
+}
+
+void HTMLImageElement::SetAutoSizesUsecounter() {
+  if (is_lazy_loaded_ && listener_) {
+    UseCounter::Count(
+        GetDocument(),
+        has_sizes_attribute_in_img_or_sibling_
+            ? WebFeature::kViewportDependentLazyLoadedImageWithSizesAttribute
+            : WebFeature::
+                  kViewportDependentLazyLoadedImageWithoutSizesAttribute);
+  }
 }
 
 void HTMLImageElement::SetLayoutDisposition(
