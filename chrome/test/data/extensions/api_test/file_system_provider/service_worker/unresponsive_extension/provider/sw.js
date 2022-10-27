d15776e56c59a34426906ec644fd20658fe79abe
Alexander Bolodurin <alexbn@chromium.org>
2022-10-27 06:55:39 +0000

    fileSystemProvider: port unresponsive_extension test to service workers
    Bug: b:235414571
    Change-Id: I62c539f50ba7af23c79a1a25c966566bb97a55b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3980876
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064196}

---
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/unresponsive_extension/provider/sw.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {promisifyWithLastError} from '/_test_resources/api_test/file_system_provider/service_worker/helpers.js';
+import {serviceWorkerMain, TestFileSystemProvider} from '/_test_resources/api_test/file_system_provider/service_worker/provider.js';
+
+async function main() {
+  serviceWorkerMain(self);
+  await promisifyWithLastError(chrome.fileSystemProvider.mount, {
+    fileSystemId: TestFileSystemProvider.FILESYSTEM_ID,
+    displayName: 'Test Filesystem',
+    writable: true,
+  });
+}
+
+main();
