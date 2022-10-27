e25233b5bdf6a940c8133234e2ec20ed42858749
David Bertoni <dbertoni@chromium.org>
2022-10-27 00:17:25 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/speech/extension_api.
    This CL was uploaded by git cl split.
    
    R=dtseng@chromium.org
    
    Bug: 1303949
    Change-Id: I9093681c0499ae8cb9acdbd6542640dbe5b1f770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984312
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064094}

--- chrome/browser/speech/extension_api/tts_engine_extension_api.cc
+++ chrome/browser/speech/extension_api/tts_engine_extension_api.cc
@@ -77,7 +77,7 @@ void WarnIfMissingPauseOrResumeListener(Profile* profile,
 
 std::unique_ptr<std::vector<extensions::TtsVoice>>
 ValidateAndConvertToTtsVoiceVector(const extensions::Extension* extension,
-                                   base::Value::ConstListView voices_data,
+                                   const base::Value::List& voices_data,
                                    bool return_after_first_error,
                                    const char** error) {
   auto tts_voices = std::make_unique<std::vector<extensions::TtsVoice>>();
@@ -154,7 +154,7 @@ std::unique_ptr<std::vector<extensions::TtsVoice>> GetVoicesInternal(
                                       &voices_data)) {
     const char* error = nullptr;
     return ValidateAndConvertToTtsVoiceVector(
-        extension, voices_data->GetListDeprecated(),
+        extension, voices_data->GetList(),
         /* return_after_first_error = */ false, &error);
   }
 
@@ -384,7 +384,7 @@ ExtensionTtsEngineUpdateVoicesFunction::Run() {
   // Validate the voices and return an error if there's a problem.
   const char* error = nullptr;
   auto tts_voices = ValidateAndConvertToTtsVoiceVector(
-      extension(), voices_data.GetListDeprecated(),
+      extension(), voices_data.GetList(),
       /* return_after_first_error = */ true, &error);
   if (error)
     return RespondNow(Error(error));
