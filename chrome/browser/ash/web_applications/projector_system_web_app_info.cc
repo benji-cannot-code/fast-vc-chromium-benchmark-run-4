d31626ca7928e10d9b4dc80bfbc087d65755692c
Sean Kau <skau@chromium.org>
2022-10-27 17:50:48 +0000

    Remove semantic colors debug flag in /chrome/browser/ash/web_applications
    Since we've migrated away from cros_styles to using color tokens, the
    flag no longer does anything. Removing it from the codebase.
    
    Note: the remaining uses of cros_styles will be migrated in a future
    CL.
    
    This CL was uploaded by git cl split.
    
    R=qjw@chromium.org
    
    Bug: b:255838401
    Change-Id: I4107b0f7eb189b9215859f053a059abb885337c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983997
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064423}

--- chrome/browser/ash/web_applications/projector_system_web_app_info.cc
+++ chrome/browser/ash/web_applications/projector_system_web_app_info.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/web_applications/projector_system_web_app_info.h"
 
-#include "ash/constants/ash_features.h"
 #include "ash/webui/grit/ash_projector_app_trusted_resources.h"
 #include "ash/webui/projector_app/public/cpp/projector_app_constants.h"
 #include "chrome/browser/ash/web_applications/system_web_app_install_utils.h"
@@ -20,10 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 SkColor GetBgColor(bool use_dark_mode) {
-  return cros_styles::ResolveColor(
-      cros_styles::ColorName::kBgColor, use_dark_mode,
-      base::FeatureList::IsEnabled(
-          ash::features::kSemanticColorsDebugOverride));
+  return cros_styles::ResolveColor(cros_styles::ColorName::kBgColor,
+                                   use_dark_mode);
 }
 
 }  // namespace
