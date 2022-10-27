5888fc3e66d6a163eeb2f1ab141fac339345f530
Viktor Semeniuk <vsemeniuk@google.com>
2022-10-27 10:52:44 +0000

    Preventing Biometric Auth promo dialog from showing after declining
    This change fixes a bug when promo dialog is shown again after user
    clicked "No thanks" button.
    
    Bug: 1378292
    Change-Id: I410691579e79feb460033ccd21d5a2a1cda5e603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978548
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064258}

--- chrome/browser/ui/passwords/passwords_model_delegate.h
+++ chrome/browser/ui/passwords/passwords_model_delegate.h
@@ -197,6 +197,10 @@ class PasswordsModelDelegate {
   // enabled.
   virtual void ShowBiometricActivationConfirmation() = 0;
 
+  // Called when user clicked "No thanks" button on Biometric Authentication
+  // before filling promo dialog.
+  virtual void OnBiometricAuthBeforeFillingDeclined() = 0;
+
  protected:
   virtual ~PasswordsModelDelegate() = default;
 };
--- chrome/browser/ui/passwords/passwords_model_delegate_mock.h
+++ chrome/browser/ui/passwords/passwords_model_delegate_mock.h
@@ -120,6 +120,7 @@ class PasswordsModelDelegateMock
               (),
               (const override));
   MOCK_METHOD(void, ShowBiometricActivationConfirmation, (), (override));
+  MOCK_METHOD(void, OnBiometricAuthBeforeFillingDeclined, (), (override));
 };
 
 #endif  // CHROME_BROWSER_UI_PASSWORDS_PASSWORDS_MODEL_DELEGATE_MOCK_H_
