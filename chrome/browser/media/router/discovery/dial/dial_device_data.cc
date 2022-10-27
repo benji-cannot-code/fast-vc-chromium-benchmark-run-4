591895f464a861e09e96ae902549952cdd9ab45c
mark a. foltz <mfoltz@chromium.org>
2022-10-27 20:47:51 +0000

    [Media Router] Remove DialEnforceUrlIPAddress feature.
    Cleanup code from this launched feature.
    
    Bug: 1270509
    Change-Id: I0b610c1eb1e99f3a77ce62af8e27c00635a57257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988365
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064530}

--- chrome/browser/media/router/discovery/dial/dial_device_data.cc
+++ chrome/browser/media/router/discovery/dial/dial_device_data.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/router/discovery/dial/dial_device_data.h"
 
 #include "base/check.h"
-#include "base/feature_list.h"
-#include "chrome/browser/media/router/media_router_feature.h"
 
 namespace media_router {
 
@@ -49,11 +47,7 @@ bool DialDeviceData::IsValidUrl(const GURL& url) const {
   if (host_address.IsPubliclyRoutable())
     return false;
 
-  if (base::FeatureList::IsEnabled(kDialEnforceUrlIPAddress)) {
-    return host_address == ip_address_;
-  } else {
-    return true;
-  }
+  return host_address == ip_address_;
 }
 
 bool DialDeviceData::UpdateFrom(const DialDeviceData& new_data) {
