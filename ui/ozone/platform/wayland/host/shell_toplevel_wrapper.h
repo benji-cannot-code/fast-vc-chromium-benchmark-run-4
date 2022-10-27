05bdc148d0d51ca7572175dcb789550512c563f7
Nick Yamane <nickdiego@igalia.com>
2022-10-27 07:15:04 +0000

    Revert "wayland: fix frame decoration when in tiled/ssd mode"
    This reverts commit 1144ea71b8e65032abfeff0b1256516419c4807e.
    
    Reason for revert: Caused regression at https://crbug.com/1373443, no trivial fix found, so reverting.
    
    Original change's description:
    > wayland: fix frame decoration when in tiled/ssd mode
    >
    > SwayWM implements a tiling window manager and, by default uses
    > server-side decorations to draw, for example, simple pixel borders
    > around the toplevel windows. In such case, Chrome shouldn't include
    > decoration insets (with drop shadows, etc) when 'use system title bar'
    > setting setting is off, which would mean client-side decoration mode.
    > That's because Sway enfores ssd, even if clients request to use csd,
    > and that's spec-compliant [1], as per:
    >
    > > The compositor can decide not to use the client's mode and enforce a
    > > different mode instead.
    >
    > When xdg-shell reports tiled modes ozone/wayland already reports it up
    > to its delegate, which in case of Chrome already sets the decoration
    > inset to (0,0,0,0), though the was still a missing bit in order to get
    > it correctly rendered, that's what this CL adds as well as unit tests to
    > exercise such code path.
    >
    > R=​adunaev@igalia.com
    >
    > [1] https://wayland.app/protocols/xdg-decoration-unstable-v1#zxdg_toplevel_decoration_v1:request:set_mode
    >
    > Bug: 1369621, 1294191
    > Test: ozone_unittests and manually with Chrome under SwayWM
    > Change-Id: I164fead8df936a075db40ea251fd2384d997d19e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924221
    > Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    > Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1054200}
    
    Bug: 1369621, 1294191, 1373443
    Change-Id: I9f55f0f65d7bafc51774896f3eb88e1c6cf68341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984601
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1064203}

--- ui/ozone/platform/wayland/host/shell_toplevel_wrapper.h
+++ ui/ozone/platform/wayland/host/shell_toplevel_wrapper.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/gtest_prod_util.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 #include "ui/platform_window/extensions/wayland_extension.h"
 
@@ -144,18 +143,6 @@ class ShellToplevelWrapper {
   virtual bool SupportsActivation() = 0;
   virtual void Activate() = 0;
   virtual void Deactivate() = 0;
-
-  DecorationMode decoration_mode() const { return decoration_mode_; }
-
- protected:
-  FRIEND_TEST_ALL_PREFIXES(WaylandWindowTest, CanSetDecorationInsets);
-
-  void set_decoration_mode(DecorationMode mode) { decoration_mode_ = mode; }
-
- private:
-  // On client side, it keeps track of the decoration mode currently in
-  // use if xdg-decoration protocol extension is available.
-  DecorationMode decoration_mode_ = DecorationMode::kNone;
 };
 
 // Look for |value| in |wl_array| in C++ style.
