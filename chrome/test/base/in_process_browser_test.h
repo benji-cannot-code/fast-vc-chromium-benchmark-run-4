d9e8a3ee9fc0033d1f1fe7c281119288ab79064a
Jie Sheng <jiesheng@google.com>
2022-10-27 16:22:37 +0000

    Add an AddTags function in the test fixture
    Bug: 1365899
    Change-Id: I637787cdbc4bcd7a4e81fbeb794a75a8af837262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966667
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064365}

--- chrome/test/base/in_process_browser_test.h
+++ chrome/test/base/in_process_browser_test.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_BASE_IN_PROCESS_BROWSER_TEST_H_
 #define CHROME_TEST_BASE_IN_PROCESS_BROWSER_TEST_H_
 
+#include <map>
 #include <memory>
 #include <string>
 
@@ -179,6 +180,13 @@ class InProcessBrowserTest : public content::BrowserTestBase {
   // instance through browser() but browser creation is delayed until after
   // PreRunTestOnMainThread().
   void SelectFirstBrowser();
+
+  // This function is used to record a set of properties for a test case in
+  // gtest result and that will be used by resultDB. The map's key value pair
+  // are defined by each test case. For use case check this bug:
+  // https://crbug.com/1365899
+  // The final value of the result is the format of key1=value1;key2=value2.
+  void RecordPropertyFromMap(const std::map<std::string, std::string>& tags);
 
  protected:
   // Closes the given browser and waits for it to release all its resources.
