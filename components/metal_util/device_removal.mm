a3ea831700b89fd77de45e7332a1a687aa4b9c6f
Leonard Grey <lgrey@chromium.org>
2022-10-27 13:19:32 +0000

    Mac: fix leak in metal::RegisterGracefulExitOnDeviceRemoval
    MTLCopyAllDevicesWithObserver returns an owned NSArray of MTLDevice.
    Since we only call this for the side effect of setting the handler, we
    drop the returned array and it leaks.
    
    This change releases it immediately.
    
    Bug: 1320449
    Change-Id: I2c1073ffce557c531c685cc9b3c0394dbc17027f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968583
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064293}

--- components/metal_util/device_removal.mm
+++ components/metal_util/device_removal.mm
@@ -13,7 +13,9 @@ namespace metal {
 
 void RegisterGracefulExitOnDeviceRemoval() {
   id<NSObject> deviceObserver = nil;
-  MTLCopyAllDevicesWithObserver(
+  // Immediately release the returned devices since we just care about setting
+  // the handler.
+  [MTLCopyAllDevicesWithObserver(
       &deviceObserver, ^(id<MTLDevice> device, MTLDeviceNotificationName name) {
         if (name == MTLDeviceRemovalRequestedNotification ||
             name == MTLDeviceWasRemovedNotification) {
@@ -25,7 +27,7 @@ void RegisterGracefulExitOnDeviceRemoval() {
           // exit the browser), but we don't support that on macOS anyway.
           base::Process::TerminateCurrentProcessImmediately(0);
         }
-      });
+      }) release];
 }
 
 }  // namespace metal
