b7298bc9c39bc08483a32f5de23ee00302a34a84
Gauthier Ambard <gambard@chromium.org>
2022-10-27 16:56:04 +0000

    [iOS] Use symbol for info table view cells
    Replace the material icon with SF Symbol.
    
    Bug: 1315544
    Change-Id: I7303aeac1467319c9e77734548b368c3f353263c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986487
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064386}

--- ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.h"
 
+#import "ios/chrome/browser/ui/icons/symbols.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_cells_constants.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -22,6 +23,8 @@ namespace {
 // Proportion of `textLayoutGuide` and `statusTextLabel`. This guarantees both
 // of them at least occupies 20% of the cell.
 const CGFloat kCellLabelsWidthProportion = 0.2f;
+
+const CGFloat kInfoSymbolSize = 22;
 
 }  // namespace
 
@@ -107,10 +110,15 @@ const CGFloat kCellLabelsWidthProportion = 0.2f;
     [self.contentView addSubview:_statusTextLabel];
 
     _trailingButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [_trailingButton
-        setImage:[[UIImage imageNamed:@"table_view_cell_info"]
-                     imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]
-        forState:UIControlStateNormal];
+    UIImage* infoImage;
+    if (UseSymbols()) {
+      infoImage = DefaultSymbolTemplateWithPointSize(kInfoCircleSymbol,
+                                                     kInfoSymbolSize);
+    } else {
+      infoImage = [[UIImage imageNamed:@"table_view_cell_info"]
+          imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    }
+    [_trailingButton setImage:infoImage forState:UIControlStateNormal];
     _trailingButton.tintColor = [UIColor colorNamed:kBlueColor];
     _trailingButton.translatesAutoresizingMaskIntoConstraints = NO;
     [_trailingButton
