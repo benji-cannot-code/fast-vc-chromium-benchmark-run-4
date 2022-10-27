2366ab69b47ff8f2eb9a84a704f7f72e01ae6615
Luca Hunkeler <hluca@google.com>
2022-10-27 15:50:09 +0000

    Paint webpage while attempting clicks in baground tabs
    Clicking in background tabs does not always work as the page is not
    painted while the WebContents are considered invisible.
    This CL forces the painting of the background page while we are attempting to click an element in it and resets the state once we are done.
    
    Bug: b/255552791
    Change-Id: Ib98a216dd3fc8c9589838387f54df5dd190e44b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978168
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064343}

--- components/autofill_assistant/browser/web/web_controller.cc
+++ components/autofill_assistant/browser/web/web_controller.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill_assistant/browser/client_status.h"
+#include "components/autofill_assistant/browser/features.h"
 #include "components/autofill_assistant/browser/public/rectf.h"
 #include "components/autofill_assistant/browser/string_conversions_util.h"
 #include "components/autofill_assistant/browser/user_data_util.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
@@ -779,6 +781,19 @@ void WebController::ClickOrTapElement(
     std::move(callback).Run(status);
     return;
   }
+
+  // If the WebContents are not currently visible, we increment the capturer
+  // count while we execute the click. This causes the page to be painted as if
+  // it were visible and allows for the click to be executed properly on
+  // non-visible tabs.
+  base::ScopedClosureRunner capture_handle;
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillAssistantPaintInvisibleTabForClick) &&
+      web_contents_->GetVisibility() != content::Visibility::VISIBLE) {
+    capture_handle = web_contents_->IncrementCapturerCount(
+        gfx::Size(), /* stay_hidden =*/true, /* stay_awake =*/false);
+  }
+
   std::unique_ptr<ClickOrTapWorker> worker =
       std::make_unique<ClickOrTapWorker>(devtools_client_.get());
   auto* ptr = worker.get();
@@ -787,7 +802,7 @@ void WebController::ClickOrTapElement(
       element, click_type,
       base::BindOnce(
           &WebController::OnClickOrTapElement, weak_ptr_factory_.GetWeakPtr(),
-          ptr,
+          ptr, std::move(capture_handle),
           base::BindOnce(&DecorateWebControllerStatus,
                          WebControllerErrorInfoProto::CLICK_OR_TAP_ELEMENT,
                          std::move(callback))));
@@ -795,11 +810,18 @@ void WebController::ClickOrTapElement(
 
 void WebController::OnClickOrTapElement(
     ClickOrTapWorker* getter_to_release,
+    base::ScopedClosureRunner capture_handler,
     base::OnceCallback<void(const ClientStatus&)> callback,
     const ClientStatus& status) {
   base::EraseIf(pending_workers_, [getter_to_release](const auto& worker) {
     return worker.get() == getter_to_release;
   });
+
+  // We reset the state of the WebContents capturer count.
+  if (capture_handler) {
+    capture_handler.RunAndReset();
+  }
+
   std::move(callback).Run(status);
 }
 
