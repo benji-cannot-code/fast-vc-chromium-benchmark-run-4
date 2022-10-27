e7d32fefa732ff9f04c35c7df4f4cbc2926c1255
Yoav Weiss <yoavweiss@chromium.org>
2022-10-27 05:13:28 +0000

    Revert "Unix epoch / monotonic clock should be sync across Performance objects."
    This reverts commit 39021765e14f7046bc72554c03210179d5f584be.
    
    Reason for revert: https://crbug.com/1365808
    
    Original change's description:
    > Unix epoch / monotonic clock should be sync across Performance objects.
    >
    > - Use the statoc TimeTicks::UnixEpoch in Performance instead of calculating it from
    >   scratch
    > - Refactor Performance to rely on real clock, as mock clock does not
    >   support UnixEpoch and it's the only value needed by Performance.
    >   Using the "real" clock (base::TimeTicks) makes the testing more
    >   realistic.
    > - Add a unittest that shows that changing the clock doesn't affect
    >   time origin
    >
    > Bug: 1207386
    > Change-Id: I6547f8534f161b93334891d93a81634404022148
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3768282
    > Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    > Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1025269}
    
    Bug: 1207386, 1365808
    Change-Id: I30bf02ec1df10c843b47bcb37a8b64e644898f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981834
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064182}

--- third_party/blink/renderer/core/timing/performance.cc
+++ third_party/blink/renderer/core/timing/performance.cc
@@ -36,9 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/time/default_clock.h"
 #include "base/time/default_tick_clock.h"
-#include "base/time/tick_clock.h"
-#include "base/time/time.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/mojom/permissions_policy/document_policy_feature.mojom-blink.h"
@@ -110,6 +109,13 @@ const SecurityOrigin* GetSecurityOrigin(ExecutionContext* context) {
 bool IsMeasureOptionsEmpty(const PerformanceMeasureOptions& options) {
   return !options.hasDetail() && !options.hasEnd() && !options.hasStart() &&
          !options.hasDuration();
+}
+
+base::TimeDelta GetUnixAtZeroMonotonic(const base::Clock* clock,
+                                       const base::TickClock* tick_clock) {
+  base::TimeDelta unix_time_now = clock->Now() - base::Time::UnixEpoch();
+  base::TimeDelta time_since_origin = tick_clock->NowTicks().since_origin();
+  return unix_time_now - time_since_origin;
 }
 
 void RecordLongTaskUkm(ExecutionContext* execution_context,
@@ -188,6 +194,8 @@ Performance::Performance(
           task_runner_,
           this,
           &Performance::FireResourceTimingBufferFull) {
+  unix_at_zero_monotonic_ =
+      GetUnixAtZeroMonotonic(base::DefaultClock::GetInstance(), tick_clock_);
   // |context| may be null in tests.
   if (context) {
     background_tracing_helper_ =
@@ -231,8 +239,11 @@ ScriptPromise Performance::measureUserAgentSpecificMemory(
 
 DOMHighResTimeStamp Performance::timeOrigin() const {
   DCHECK(!time_origin_.is_null());
-  return ClampTimeResolution(time_origin_ - base::TimeTicks::UnixEpoch(),
-                             cross_origin_isolated_capability_);
+  base::TimeDelta time_origin_from_zero_monotonic =
+      time_origin_ - base::TimeTicks();
+  return ClampTimeResolution(
+      unix_at_zero_monotonic_ + time_origin_from_zero_monotonic,
+      cross_origin_isolated_capability_);
 }
 
 PerformanceEntryVector Performance::getEntries() {
@@ -1145,8 +1156,11 @@ void Performance::Trace(Visitor* visitor) const {
   EventTargetWithInlineData::Trace(visitor);
 }
 
-void Performance::SetTickClockForTesting(const base::TickClock* tick_clock) {
+void Performance::SetClocksForTesting(const base::Clock* clock,
+                                      const base::TickClock* tick_clock) {
   tick_clock_ = tick_clock;
+  // Recompute |unix_at_zero_monotonic_|.
+  unix_at_zero_monotonic_ = GetUnixAtZeroMonotonic(clock, tick_clock_);
 }
 
 void Performance::ResetTimeOriginForTesting(base::TimeTicks time_origin) {
