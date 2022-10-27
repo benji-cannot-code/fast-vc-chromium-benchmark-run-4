edfebe742345bd86c79514f0ad0627bd8808d4c1
Yiren Wang <yrw@chromium.org>
2022-10-27 00:18:30 +0000

    Toggle PiP 2.0 window top bar view when the window changes active status
    When the PiP 2.0 window is active, the window title and icons other than
    the lock icons should all be white. When the PiP 2.0 window becomes
    inactive, the title and icon colors should dim, and back-to-tab and
    close buttons should hide.
    
    We count the following cases as the PiP window is active:
    - PiP window is hovered with mouse
    - PiP window is in focus with keyboard navigation
    - PiP window is in focus with any other format of activation
    - Dialogs are opened in the PiP window
    
    Bug: 1361499
    Change-Id: Ia02640d6f25b964bcd743d2fa5457947b1f86610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947827
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064096}

--- chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
+++ chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_PICTURE_IN_PICTURE_BROWSER_FRAME_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_FRAME_PICTURE_IN_PICTURE_BROWSER_FRAME_VIEW_H_
 
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h"
 #include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/layout/box_layout_view.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace views {
 class Label;
@@ -29,7 +31,8 @@ class PictureInPictureBrowserFrameView
       public LocationIconView::Delegate,
       public IconLabelBubbleView::Delegate,
       public ContentSettingImageView::Delegate,
-      public device::GeolocationManager::PermissionObserver {
+      public device::GeolocationManager::PermissionObserver,
+      public views::WidgetObserver {
  public:
   METADATA_HEADER(PictureInPictureBrowserFrameView);
 
@@ -59,6 +62,7 @@ class PictureInPictureBrowserFrameView
   gfx::Size GetMinimumSize() const override;
   void OnThemeChanged() override;
   void Layout() override;
+  void AddedToWidget() override;
 
   // ChromeLocationBarModelDelegate:
   content::WebContents* GetActiveWebContents() const override;
@@ -90,6 +94,14 @@ class PictureInPictureBrowserFrameView
   void OnSystemPermissionUpdated(
       device::LocationSystemPermissionStatus new_status) override;
 
+  // views::WidgetObserver:
+  void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+  // ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
+
   // Convert the bounds of a child view of |controls_container_view_| to use
   // the system's coordinate system.
   gfx::Rect ConvertControlViewBounds(views::View* control_view) const;
@@ -104,6 +116,15 @@ class PictureInPictureBrowserFrameView
 
   // Updates the state of the images showing the content settings status.
   void UpdateContentSettingsIcons();
+
+  // Updates the top bar title and icons according to whether user wants to
+  // interact with the window. The top bar should be highlighted in all these
+  // cases:
+  // - PiP window is hovered with mouse
+  // - PiP window is in focus with keyboard navigation
+  // - PiP window is in focus with any other format of activation
+  // - Dialogs are opened in the PiP window
+  void UpdateTopBarView(bool render_active);
 
  private:
   // A model required to use LocationIconView.
@@ -124,6 +145,10 @@ class PictureInPictureBrowserFrameView
 
   raw_ptr<CloseImageButton> close_image_button_ = nullptr;
   raw_ptr<views::View> back_to_tab_button_ = nullptr;
+
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
+  bool mouse_inside_window_ = false;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_FRAME_PICTURE_IN_PICTURE_BROWSER_FRAME_VIEW_H_
