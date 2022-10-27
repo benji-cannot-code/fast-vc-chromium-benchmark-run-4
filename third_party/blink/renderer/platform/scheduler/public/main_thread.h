4d0439be7e0aab3cf8db3e45e5b0f551c73d78d0
Dave Tapuska <dtapuska@chromium.org>
2022-10-27 19:24:39 +0000

    Allow GetDeprecatedTaskRunner call to retrieve GpuMemoryBufferManager.
    MediaStreamVideoTrackUnderlyingSink needs access to the
    GpuMemoryBufferManager. Allow list this code path.
    
    BUG=624696
    
    Change-Id: I9521e41fcaa6c4d0190320dfaad0c04b136071bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988923
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064482}

--- third_party/blink/renderer/platform/scheduler/public/main_thread.h
+++ third_party/blink/renderer/platform/scheduler/public/main_thread.h
@@ -35,6 +35,8 @@ class MainThreadTaskRunnerRestricted {
   friend MainThreadTaskRunnerRestricted AccessMainThreadForGpuFactories();
   friend MainThreadTaskRunnerRestricted
   AccessMainThreadForWebGraphicsContext3DProvider();
+  friend MainThreadTaskRunnerRestricted
+  AccessMainThreadForGpuMemoryBufferManager();
 
   MainThreadTaskRunnerRestricted() = default;
 };
