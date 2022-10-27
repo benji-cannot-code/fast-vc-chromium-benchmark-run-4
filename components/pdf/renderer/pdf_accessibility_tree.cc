4a28c57b845d5a973c11cbf00c0bfb35d7af9d29
Nektarios Paisios <nektar@chromium.org>
2022-10-27 22:49:55 +0000

    Fixes a few bugs with OnOcrDataReceived in PdfAccessibilityTree
    When the OCR Service in PdfAccessibilityTree
    is done producing the tree update,
    RenderAccessibility is requested to unserialize
    the changes that were made to the tree source.
    Unfortunately, calling OnPluginRootUpdated() does not
    always cause all the modifications to the tree
    source to be unserialized, because
    often newly added pages from the OCR Service
    might not be direct children of the root.
    A followup patch will change SetPluginSource to UpdatePluginSource which
    will also handle partial updates to the PDF tree.
    
    Also, I took the opportunity to fix the use of the "erase & remove" idiom
    by switching to the C++ 20 equivalent algorithms.
    https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom#:~:text=The%20erase%E2%80%93remove%20idiom%20is,a%20C%2B%2B%20Standard%20Library%20container.
    std::erase and std::erase_if, insead of std::erase(ranges::remove(vec, value, end)
    
    R=dtseng@chromium.org, rhalavati@chromium.org
    
    AX-RelNotes: n/a.
    Change-Id: I7b88db7f4d398386a2294e3b9bb8a1e9958b3e1d
    Bug: 1278249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3982909
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064584}

--- components/pdf/renderer/pdf_accessibility_tree.cc
+++ components/pdf/renderer/pdf_accessibility_tree.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/containers/cxx20_erase.h"
 #include "base/i18n/break_iterator.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
@@ -1797,18 +1798,18 @@ void PdfAccessibilityTree::OnOcrDataReceived(
   page_node->AddStringAttribute(ax::mojom::StringAttribute::kChildTreeId,
                                 child_tree_id.ToString());
 
-  const auto removed_image_iter = ranges::remove_if(
-      nodes_,
-      [&image_node_id](const auto& node) { return node->id == image_node_id; });
-  DCHECK(removed_image_iter != ranges::end(nodes_));
+  int num_erased = base::EraseIf(nodes_, [&image_node_id](const auto& node) {
+    return node->id == image_node_id;
+  });
+  DCHECK_EQ(num_erased, 1);
   const auto parent_node_iter =
       ranges::find_if(nodes_, [&parent_node_id](const auto& node) {
         return node->id == parent_node_id;
       });
   DCHECK(parent_node_iter != ranges::end(nodes_));
-  const auto removed_child_iter =
-      ranges::remove((*parent_node_iter)->child_ids, image_node_id);
-  DCHECK(removed_child_iter != ranges::end((*parent_node_iter)->child_ids));
+  num_erased = base::Erase((*parent_node_iter)->child_ids, image_node_id);
+  DCHECK_EQ(num_erased, 1);
+  (*parent_node_iter)->child_ids.push_back(page_node->id);
 
   update.node_id_to_clear = image_node_id;
   update.root_id = doc_node_->id;
@@ -1817,7 +1818,7 @@ void PdfAccessibilityTree::OnOcrDataReceived(
 
   if (!tree_.Unserialize(update))
     LOG(FATAL) << tree_.error();
-  render_accessibility->OnPluginRootNodeUpdated();
+  render_accessibility->SetPluginTreeSource(this);
 }
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
 
