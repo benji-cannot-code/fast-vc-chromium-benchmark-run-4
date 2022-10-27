948fc7bbfc900cb6ad12871cbcbf7118277e3a1c
Oleksandr Sarapulov <olsa@google.com>
2022-10-27 16:27:17 +0000

    Reland "Replaced couple of deprecated methods in Keystore services"
    This is a reland of commit 4ae5ba2e9b9ea4c50981a0afba0a1aa68c92e909
    
    Original change's description:
    > Replaced couple of deprecated methods in Keystore services
    >
    > Replaced methods: DEPRECATED_GetKeyStores, DEPRECATED_GetCertificates,
    > DEPRECATED_AddCertificate,DEPRECATED_DidGetCertificates,
    > DEPRECATED_DidImportCertificate. Updated unit tests.
    >
    > IS_CHROMEOS_LACROS, which needs branching on unit tests, and also
    > there are additional browser's_tests.
    >
    > Low-Coverage-Reason: ValidateCrosapi is used only with BUILDFLAG=
    > Bug: 1128022
    > Change-Id: Ia7b47091fe056f931006122fa632e7bf46a06d50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929184
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > Reviewed-by: Roland Bock <rbock@google.com>
    > Commit-Queue: Oleksandr Sarapulov <olsa@google.com>
    > Reviewed-by: Michael Ershov <miersh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1062733}
    
    Bug: 1128022
    Change-Id: Ieda5df2ddde59a78087415b45b6a0b4a72fce3ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975695
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Oleksandr Sarapulov <olsa@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064366}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.h
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.h
@@ -58,8 +58,7 @@ class EnterprisePlatformKeysGetCertificatesFunction : public ExtensionFunction {
   ~EnterprisePlatformKeysGetCertificatesFunction() override = default;
   ResponseAction Run() override;
 
-  void OnGetCertificates(
-      crosapi::mojom::DEPRECATED_GetCertificatesResultPtr result);
+  void OnGetCertificates(crosapi::mojom::GetCertificatesResultPtr result);
   DECLARE_EXTENSION_FUNCTION("enterprise.platformKeys.getCertificates",
                              ENTERPRISE_PLATFORMKEYS_GETCERTIFICATES)
 };
@@ -70,7 +69,8 @@ class EnterprisePlatformKeysImportCertificateFunction
   ~EnterprisePlatformKeysImportCertificateFunction() override = default;
   ResponseAction Run() override;
 
-  void OnAddCertificate(const std::string& error);
+  void OnAddCertificate(bool is_error,
+                        crosapi::mojom::KeystoreError error_code);
   DECLARE_EXTENSION_FUNCTION("enterprise.platformKeys.importCertificate",
                              ENTERPRISE_PLATFORMKEYS_IMPORTCERTIFICATE)
 };
@@ -92,7 +92,7 @@ class EnterprisePlatformKeysInternalGetTokensFunction
   ~EnterprisePlatformKeysInternalGetTokensFunction() override = default;
   ResponseAction Run() override;
 
-  void OnGetKeyStores(crosapi::mojom::DEPRECATED_GetKeyStoresResultPtr result);
+  void OnGetKeyStores(crosapi::mojom::GetKeyStoresResultPtr result);
   DECLARE_EXTENSION_FUNCTION("enterprise.platformKeysInternal.getTokens",
                              ENTERPRISE_PLATFORMKEYSINTERNAL_GETTOKENS)
 };
