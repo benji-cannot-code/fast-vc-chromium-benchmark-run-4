d6136d0863c27d9f2aecbfdd2871fc0e7d94fb89
Thiabaud Engelbrecht <thiabaud@google.com>
2022-10-27 13:30:06 +0000

    [content] Fix lower subframe limits for V8
    This CL is a follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/3936776. This
    CL fixes the previous one, to start the V8 Isolate in lower memory mode,
    and to correctly handle the case where the V8 Isolate is made visible/hidden.
    
    Bug: 1331368
    Change-Id: I9861a1f2e5f798f7b980db142d32cc20e0be3adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953408
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064298}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -905,6 +905,11 @@ void RenderThreadImpl::InitializeWebKit(mojo::BinderMap* binders) {
     isolate->IsolateInBackgroundNotification();
   }
 
+  if (base::FeatureList::IsEnabled(
+          features::kLowerV8MemoryLimitForNonMainRenderers)) {
+    isolate->IsolateInBackgroundNotification();
+  }
+
   // Hook up blink's codecs so skia can call them. Since only the renderer
   // processes should be doing image decoding, this is not done in the common
   // skia initialization code for the GPU.
@@ -1675,11 +1680,7 @@ bool RenderThreadImpl::RendererIsHidden() const {
 }
 
 void RenderThreadImpl::OnRendererHidden() {
-  if (!base::FeatureList::IsEnabled(
-          features::kLowerV8MemoryLimitForNonMainRenderers) ||
-      MainFrameCounter::has_main_frame()) {
-    blink::MainThreadIsolate()->IsolateInBackgroundNotification();
-  }
+  blink::MainThreadIsolate()->IsolateInBackgroundNotification();
   // TODO(rmcilroy): Remove IdleHandler and replace it with an IdleTask
   // scheduled by the RendererScheduler - http://crbug.com/469210.
   if (!GetContentClient()->renderer()->RunIdleHandlerWhenWidgetsHidden())
@@ -1688,7 +1689,11 @@ void RenderThreadImpl::OnRendererHidden() {
 }
 
 void RenderThreadImpl::OnRendererVisible() {
-  blink::MainThreadIsolate()->IsolateInForegroundNotification();
+  if (!base::FeatureList::IsEnabled(
+          features::kLowerV8MemoryLimitForNonMainRenderers) ||
+      MainFrameCounter::has_main_frame()) {
+    blink::MainThreadIsolate()->IsolateInForegroundNotification();
+  }
   if (!GetContentClient()->renderer()->RunIdleHandlerWhenWidgetsHidden())
     return;
   main_thread_scheduler_->SetRendererHidden(false);
