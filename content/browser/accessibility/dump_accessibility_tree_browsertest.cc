455dfa369785507cb797781d95141d132807fbaf
Valerie Young <spectranaut@igalia.com>
2022-10-27 17:58:53 +0000

    AXAPI: Expose aria-description in AXCustomContent
    In macOS 11, the AXAPI attribute "AXCustomContent" was introduced to
    surface information in the `aria-description` string.
    
    This change exposes the `aria-description` string in AXCustomContent
    for all macOS versions, but only exposes AXHelp when
    MAC_OS_X_VERSION_MIN_REQUIRED is less than 11.
    
    There is a follow up to exposed aria-describedby in AXCustom content
    as well: https://bugs.chromium.org/p/chromium/issues/detail?id=1373178
    
    Fixed: 1350145
    Change-Id: I0fcbc44c339c6ab6c1b47364aba4ccb4d5b29665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928073
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1064430}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -29,10 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell.h"
 #include "ui/accessibility/platform/inspect/ax_api_type.h"
 
-#if BUILDFLAG(IS_MAC)
-#include "base/mac/mac_util.h"
-#endif
-
 // TODO(aboxhall): Create expectations on Android for these
 #if BUILDFLAG(IS_ANDROID)
 #define MAYBE(x) DISABLED_##x
