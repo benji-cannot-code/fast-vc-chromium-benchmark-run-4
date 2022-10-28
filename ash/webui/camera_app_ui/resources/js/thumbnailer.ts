7ed057dbcb71dcb9b9babf051a4e66414a55c1e4
Chu-Hsuan Yang <chuhsuan@chromium.org>
2022-10-28 08:30:23 +0000

    CCA: Fix "Open image in new tab" for PDF thumbnail
    Add MIME type `image/jpeg` when making thumbnail images from PDF blobs.
    
    Bug: b:255268260
    Test: Manually
    Change-Id: If42dc1e00af0160d5765320b62fb573bfad89499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989840
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064740}

--- ash/webui/camera_app_ui/resources/js/thumbnailer.ts
+++ ash/webui/camera_app_ui/resources/js/thumbnailer.ts
@@ -217,7 +217,7 @@ async function getImageFromPdf(blob: Blob): Promise<Blob> {
     }
     if (isImage) {
       i += ' stream\n'.length;
-      return new Blob([buf.slice(i, i + length)]);
+      return new Blob([buf.slice(i, i + length)], {type: 'image/jpeg'});
     }
   }
   throw new NoImageInPdfError();
