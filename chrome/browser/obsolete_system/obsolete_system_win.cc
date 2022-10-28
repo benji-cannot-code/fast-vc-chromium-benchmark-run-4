b4470ebfe870d0d979e2f0d189e1887c031a7750
Jesse McKenna <jessemckenna@google.com>
2022-10-28 21:31:04 +0000

    Remove infobar, about:help warnings for Win 7/8.*
    The existing infobar was determined to be too vague (crbug.com/1379272).
    This change removes it while the team rewords the message to explicitly
    state that M109 is the last supported version, i.e., that M110 will
    require Win 10.
    
    Bug: 1377525, 1379272
    Change-Id: I4d0b3485af84a9f8f87e341ff6b47fc508ae2817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990633
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065043}

--- chrome/browser/obsolete_system/obsolete_system_win.cc
+++ chrome/browser/obsolete_system/obsolete_system_win.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool IsObsoleteOsVersion() {
-  return base::win::GetVersion() < base::win::Version::WIN10;
+  return base::win::GetVersion() < base::win::Version::WIN7;
 }
 
 }  // namespace
@@ -28,13 +28,6 @@ bool ObsoleteSystem::IsObsoleteNowOrSoon() {
 
 // static
 std::u16string ObsoleteSystem::LocalizedObsoleteString() {
-  const auto version = base::win::GetVersion();
-  if (version == base::win::Version::WIN7)
-    return l10n_util::GetStringUTF16(IDS_WIN_7_OBSOLETE);
-  if (version == base::win::Version::WIN8)
-    return l10n_util::GetStringUTF16(IDS_WIN_8_OBSOLETE);
-  if (version == base::win::Version::WIN8_1)
-    return l10n_util::GetStringUTF16(IDS_WIN_8_1_OBSOLETE);
   return l10n_util::GetStringUTF16(IDS_WIN_XP_VISTA_OBSOLETE);
 }
 
@@ -45,8 +38,5 @@ bool ObsoleteSystem::IsEndOfTheLine() {
 
 // static
 const char* ObsoleteSystem::GetLinkURL() {
-  const auto version = base::win::GetVersion();
-  if (version < base::win::Version::WIN7)
-    return chrome::kWindowsXPVistaDeprecationURL;
-  return chrome::kWindows78DeprecationURL;
+  return chrome::kWindowsXPVistaDeprecationURL;
 }
