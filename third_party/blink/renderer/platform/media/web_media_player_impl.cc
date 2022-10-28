d4f60804bc01a4648b6d8cca1aeaae8dc2879e98
Ted Meyer <tmathmeyer@chromium.org>
2022-10-28 00:51:17 +0000

    Remove use of chunk_demuxer rawptr in WMPI
    This removes one key instance of |chunk_demuxer_| from WMPI, which
    is critical for making the demuxer specific behavior of WMPI simpler,
    in order to facilitate moving it out.
    
    Bug: 1377053
    Change-Id: Ie5b035f94a6356a644bb9e59693bcb89eee9b69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969347
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064637}

--- third_party/blink/renderer/platform/media/web_media_player_impl.cc
+++ third_party/blink/renderer/platform/media/web_media_player_impl.cc
@@ -1850,9 +1850,11 @@ void WebMediaPlayerImpl::OnPipelineResumed() {
   UpdateBackgroundVideoOptimizationState();
 }
 
-void WebMediaPlayerImpl::OnDemuxerOpened() {
+void WebMediaPlayerImpl::OnChunkDemuxerOpened() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
-  client_->MediaSourceOpened(new WebMediaSourceImpl(chunk_demuxer_));
+  CHECK_EQ(demuxer_->GetDemuxerType(), media::DemuxerType::kChunkDemuxer);
+  client_->MediaSourceOpened(new WebMediaSourceImpl(
+      static_cast<media::ChunkDemuxer*>(demuxer_.get())));
 }
 
 void WebMediaPlayerImpl::OnMemoryPressure(
@@ -2982,8 +2984,8 @@ void WebMediaPlayerImpl::StartPipeline() {
             &WebMediaPlayerImpl::OnEncryptedMediaInitData, weak_this_));
 
     chunk_demuxer_ = new media::ChunkDemuxer(
-        media::BindToCurrentLoop(
-            base::BindOnce(&WebMediaPlayerImpl::OnDemuxerOpened, weak_this_)),
+        media::BindToCurrentLoop(base::BindOnce(
+            &WebMediaPlayerImpl::OnChunkDemuxerOpened, weak_this_)),
         media::BindToCurrentLoop(
             base::BindRepeating(&WebMediaPlayerImpl::OnProgress, weak_this_)),
         encrypted_media_init_data_cb, media_log_.get());
