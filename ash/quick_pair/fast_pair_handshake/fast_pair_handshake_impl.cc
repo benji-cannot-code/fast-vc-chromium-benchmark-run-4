2de9f407a5770fd126a35cdcb073f7daec97f64d
Juliet Levesque <julietlevesque@google.com>
2022-10-28 23:33:46 +0000

    [Fast Pair] Add Handshake funnel metrics
    Adds funnel metrics for the Handshake flow for when we start the
    Handshake, when we initialize the GATT connection successfully, when
    we receive response bytes from the key based pariring GATT
    characteristic, and when the handshake completes successfully.
    
    TEST=Verified on chrome://histograms that these are all logged on
    success, and on failures only buckets up to that point are logged.
    
    Bug: b/254903408
    Change-Id: I1eb9361d9d68890581e1cf6627f85f8016c83b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988765
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065095}

--- ash/quick_pair/fast_pair_handshake/fast_pair_handshake_impl.cc
+++ ash/quick_pair/fast_pair_handshake/fast_pair_handshake_impl.cc
@@ -32,11 +32,14 @@ FastPairHandshakeImpl::FastPairHandshakeImpl(
                         std::move(on_complete),
                         nullptr,
                         nullptr) {
+  RecordHandshakeStep(FastPairHandshakeSteps::kHandshakeStarted, *device_);
+
   device::BluetoothDevice* bluetooth_device =
       adapter_->GetDevice(device_->ble_address);
 
   if (!bluetooth_device) {
-    QP_LOG(INFO) << __func__ << ": Lost device before starting handshake.";
+    QP_LOG(INFO) << __func__
+                 << ": Lost device before starting GATT connection.";
     std::move(on_complete_callback_)
         .Run(device_, PairFailure::kPairingDeviceLost);
     return;
@@ -64,6 +67,9 @@ void FastPairHandshakeImpl::OnGattClientInitializedCallback(
     return;
   }
 
+  QP_LOG(INFO) << __func__
+               << ": Fast Pair GATT service client initialization successful.";
+  RecordHandshakeStep(FastPairHandshakeSteps::kGattInitalized, *device_);
   FastPairDataEncryptorImpl::Factory::CreateAsync(
       device_,
       base::BindOnce(&FastPairHandshakeImpl::OnDataEncryptorCreateAsync,
@@ -88,8 +94,7 @@ void FastPairHandshakeImpl::OnDataEncryptorCreateAsync(
   }
 
   fast_pair_data_encryptor_ = std::move(fast_pair_data_encryptor);
-  QP_LOG(INFO) << __func__
-               << ": Fast Pair GATT service client initialization successful.";
+  QP_LOG(INFO) << __func__ << ": beginning key-based pairing protocol";
   RecordTotalDataEncryptorCreateTime(base::TimeTicks::Now() -
                                      encryptor_create_start_time);
 
@@ -111,8 +116,10 @@ void FastPairHandshakeImpl::OnWriteResponse(
   RecordWriteKeyBasedCharacteristicResult(/*success=*/!failure.has_value());
 
   if (failure) {
-    QP_LOG(WARNING) << __func__
-                    << ": Failed to write request: " << failure.value();
+    QP_LOG(WARNING)
+        << __func__
+        << ": Failed during key-based pairing protocol due to failure: "
+        << failure.value();
     RecordWriteKeyBasedCharacteristicPairFailure(failure.value());
     RecordHandshakeResult(/*success=*/false);
     RecordHandshakeFailureReason(HandshakeFailureReason::kFailedWriteResponse);
@@ -121,6 +128,8 @@ void FastPairHandshakeImpl::OnWriteResponse(
   }
 
   QP_LOG(INFO) << __func__ << ": Successfully wrote response.";
+  RecordHandshakeStep(FastPairHandshakeSteps::kKeyBasedPairingResponseReceived,
+                      *device_);
 
   fast_pair_data_encryptor_->ParseDecryptedResponse(
       response_bytes,
@@ -163,6 +172,7 @@ void FastPairHandshakeImpl::OnParseDecryptedResponse(
 
   completed_successfully_ = true;
   RecordHandshakeResult(/*success=*/true);
+  RecordHandshakeStep(FastPairHandshakeSteps::kHandshakeComplete, *device_);
   std::move(on_complete_callback_).Run(device_, absl::nullopt);
 }
 
