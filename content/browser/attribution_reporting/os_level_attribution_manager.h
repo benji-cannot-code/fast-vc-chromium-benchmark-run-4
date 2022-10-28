09e27b6c709cde99e7aea137160fbe1ca335f301
Andrew Paseltiner <apaseltiner@chromium.org>
2022-10-28 21:47:28 +0000

    Add OsLevelAttributionManager interface
    We will call its methods in a followup CL that also implements the
    corresponding methods on the AttributionDataHost Mojo interface.
    
    Bug: 1378298, 1374035
    Change-Id: I4dbb033ccbf22268f432deca210685ecd0b07ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989109
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065052}

---
+++ content/browser/attribution_reporting/os_level_attribution_manager.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_ATTRIBUTION_REPORTING_OS_LEVEL_ATTRIBUTION_MANAGER_H_
+#define CONTENT_BROWSER_ATTRIBUTION_REPORTING_OS_LEVEL_ATTRIBUTION_MANAGER_H_
+
+namespace attribution_reporting {
+class OsSource;
+class OsTrigger;
+}  // namespace attribution_reporting
+
+namespace content {
+
+// Interface between the browser's Attribution Reporting implementation and the
+// operating system's.
+class OsLevelAttributionManager {
+ public:
+  virtual ~OsLevelAttributionManager() = default;
+
+  // Registers the given source with the OS.
+  virtual void RegisterSource(attribution_reporting::OsSource) = 0;
+
+  // Registers the given trigger with the OS.
+  virtual void RegisterTrigger(attribution_reporting::OsTrigger) = 0;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_ATTRIBUTION_REPORTING_OS_LEVEL_ATTRIBUTION_MANAGER_H_
