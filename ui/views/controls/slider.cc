06ad86fb58d659d1d7128367763c3ba8fd178ef4
Sylvie Liu <sylvieliu@chromium.org>
2022-10-28 18:08:51 +0000

    Modified slider thumb offset for mouse/gesture event to enable click within the thumb circle
    The slider thumb doesn't respond to click/gesture event within its
    circle due to initial_button_offset_ set in PrepareForMove(). However,
    in the QS Revamp project, the thumb is removed and the new value
    indicator has a radius of 16px. So if the click falls in the circle of
    the new indicator and there is no response, it can be very noticeable.
    In this CL, we modified PrepareForMove() in slider.cc so that the value
    indicator can respond to every click on the slider. A virtual function
    GetValueIndicatorRadius() is added to avoid passing in hard-coded value.
    The new QsSlider subclass will override this function to provide the
    radius for the new indicator.
    
    Bug: 254051810
    Change-Id: I8e3a55e88117cc32969a90735159692f4afcb029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960596
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064927}

--- ui/views/controls/slider.cc
+++ ui/views/controls/slider.cc
@@ -46,10 +46,7 @@ constexpr float kSliderRoundedRadius = 2.f;
 // The padding used to hide the slider underneath the thumb.
 constexpr int kSliderPadding = 2;
 
-// The radius of the thumb and the highlighted thumb of the slider,
-// respectively.
-constexpr float kThumbRadius = 4.f;
-constexpr float kThumbWidth = 2 * kThumbRadius;
+// The radius of the highlighted thumb of the slider
 constexpr float kThumbHighlightRadius = 12.f;
 
 float GetNearestAllowedValue(const base::flat_set<float>& allowed_values,
@@ -77,6 +74,7 @@ float GetNearestAllowedValue(const base::flat_set<float>& allowed_values,
 Slider::Slider(SliderListener* listener) : listener_(listener) {
   highlight_animation_.SetSlideDuration(base::Milliseconds(150));
   SetFlipCanvasOnPaintForRTLUI(true);
+
 #if BUILDFLAG(IS_MAC)
   SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
 #else
@@ -94,6 +92,14 @@ float Slider::GetValue() const {
 
 void Slider::SetValue(float value) {
   SetValueInternal(value, SliderChangeReason::kByApi);
+}
+
+float Slider::GetValueIndicatorRadius() const {
+  return value_indicator_radius_;
+}
+
+void Slider::SetValueIndicatorRadius(float radius) {
+  value_indicator_radius_ = radius;
 }
 
 bool Slider::GetEnableAccessibilityEvents() const {
@@ -213,12 +219,13 @@ void Slider::PrepareForMove(const int new_x) {
   gfx::Rect content = GetContentsBounds();
   float value = GetAnimatingValue();
 
-  const int thumb_x = value * (content.width() - kThumbWidth);
+  const int thumb_x = value * (content.width() - 2 * value_indicator_radius_);
   const int candidate_x = GetMirroredXInView(new_x - inset.left()) - thumb_x;
-  if (candidate_x >= 0 && candidate_x < kThumbWidth)
+  if (candidate_x >= value_indicator_radius_ - kThumbRadius &&
+      candidate_x < value_indicator_radius_ + kThumbRadius)
     initial_button_offset_ = candidate_x;
   else
-    initial_button_offset_ = kThumbRadius;
+    initial_button_offset_ = value_indicator_radius_;
 }
 
 void Slider::MoveButtonTo(const gfx::Point& point) {
@@ -227,9 +234,9 @@ void Slider::MoveButtonTo(const gfx::Point& point) {
   int amount = base::i18n::IsRTL()
                    ? width() - inset.left() - point.x() - initial_button_offset_
                    : point.x() - inset.left() - initial_button_offset_;
-  SetValueInternal(
-      static_cast<float>(amount) / (width() - inset.width() - kThumbWidth),
-      SliderChangeReason::kByUser);
+  SetValueInternal(static_cast<float>(amount) /
+                       (width() - inset.width() - 2 * value_indicator_radius_),
+                   SliderChangeReason::kByUser);
 }
 
 void Slider::OnSliderDragStarted() {
@@ -467,6 +474,7 @@ int Slider::GetSliderExtraPadding() const {
 BEGIN_METADATA(Slider, View)
 ADD_PROPERTY_METADATA(float, Value)
 ADD_PROPERTY_METADATA(bool, EnableAccessibilityEvents)
+ADD_PROPERTY_METADATA(float, ValueIndicatorRadius)
 END_METADATA
 
 }  // namespace views
