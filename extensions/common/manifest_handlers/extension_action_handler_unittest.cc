0f05d24c2ad55d4a16d20c6e6701ad97a8431cf5
Justin Lulejian <jlulejian@chromium.org>
2022-10-28 20:29:21 +0000

    [Extensions] Refactor VerifyInstallWarnings() into its own file.
    Also remove the test-specific logic it was testing which is fragile and
    doesn't enhance the validity of the test cases. Prior to this we were
    validating that things like manifest v2 deprecation warnings were being
    added which aren't relevant to the test cases using this method.
    
    Bug: 1353210
    Change-Id: I03b864c1ebf75fc700f0c267d453df53d514ddd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962156
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065011}

--- extensions/common/manifest_handlers/extension_action_handler_unittest.cc
+++ extensions/common/manifest_handlers/extension_action_handler_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_test.h"
+#include "extensions/common/warnings_test_util.h"
 #include "extensions/test/test_extension_dir.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -192,37 +193,6 @@ class ExtensionActionManifestTest
     return extension;
   }
 
-  // Verifies that a specific warning was set on install. Or if not, that the
-  // expected number of warnings were generated.
-  void VerifyInstallWarnings(scoped_refptr<Extension> extension,
-                             int manifest_version,
-                             const std::string& expected_warning_message) {
-    std::vector<const std::string> install_warning_messages;
-    for (const InstallWarning& warning : extension->install_warnings()) {
-      install_warning_messages.push_back(warning.message);
-    }
-    std::vector<const std::string> expected_warning_messages;
-
-    // The test does not register these features, so this warning is
-    // expected.
-    const char* action_key = GetManifestKeyForActionType(GetParam());
-    expected_warning_messages.push_back(ErrorUtils::FormatErrorMessage(
-        manifest_errors::kUnrecognizedManifestKey, action_key));
-    if (manifest_version == 2) {
-      // Manifest v2 always installs a warning that it is deprecated.
-      expected_warning_messages.push_back(
-          manifest_errors::kManifestV2IsDeprecatedWarning);
-    }
-
-    if (!expected_warning_message.empty()) {
-      expected_warning_messages.push_back(expected_warning_message);
-    }
-    EXPECT_THAT(install_warning_messages,
-                testing::ContainerEq(expected_warning_messages));
-    EXPECT_EQ(expected_warning_messages.size(),
-              extension->install_warnings().size());
-  }
-
  private:
   // The "action" key is restricted to trunk.
   ScopedCurrentChannel scoped_channel_{version_info::Channel::UNKNOWN};
@@ -341,7 +311,8 @@ TEST_P(ExtensionActionManifestTest, ValidDefaultPopup) {
   scoped_refptr<Extension> test_extension = LoadExtensionWithDefaultPopup(
       valid_popup_file_name, manifest_version, &test_extension_dir);
   ASSERT_TRUE(test_extension);
-  VerifyInstallWarnings(test_extension, manifest_version, "");
+  EXPECT_FALSE(warnings_test_util::HasInstallWarning(
+      test_extension, manifest_errors::kInvalidExtensionOriginPopup));
 }
 
 // Tests success when default_popup is empty.
@@ -352,7 +323,8 @@ TEST_P(ExtensionActionManifestTest, EmptyDefaultPopup) {
   scoped_refptr<Extension> test_extension = LoadExtensionWithDefaultPopup(
       empty_popup_file_name, manifest_version, &test_extension_dir);
   ASSERT_TRUE(test_extension);
-  VerifyInstallWarnings(test_extension, manifest_version, "");
+  EXPECT_FALSE(warnings_test_util::HasInstallWarning(
+      test_extension, manifest_errors::kInvalidExtensionOriginPopup));
 }
 
 // Tests warning when the default_popup seems to be for another extension.
@@ -365,8 +337,8 @@ TEST_P(ExtensionActionManifestTest, OtherExtensionSpecifiedDefaultPopup) {
       LoadExtensionWithDefaultPopup(other_extension_specified_popup_file_name,
                                     manifest_version, &test_extension_dir);
   ASSERT_TRUE(test_extension);
-  VerifyInstallWarnings(test_extension, manifest_version,
-                        manifest_errors::kInvalidExtensionOriginPopup);
+  EXPECT_TRUE(warnings_test_util::HasInstallWarning(
+      test_extension, manifest_errors::kInvalidExtensionOriginPopup));
 }
 
 // Tests warning when the default_popup doesn't exist on file system.
@@ -377,8 +349,8 @@ TEST_P(ExtensionActionManifestTest, NonexistentDefaultPopup) {
   scoped_refptr<Extension> test_extension = LoadExtensionWithDefaultPopup(
       nonexistent_popup_file_name, manifest_version, &test_extension_dir);
   ASSERT_TRUE(test_extension);
-  VerifyInstallWarnings(test_extension, manifest_version,
-                        manifest_errors::kNonexistentDefaultPopup);
+  EXPECT_TRUE(warnings_test_util::HasInstallWarning(
+      test_extension, manifest_errors::kNonexistentDefaultPopup));
 }
 
 // Test the handling of the default_state key.
---
+++ extensions/common/warnings_test_util.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/common/warnings_test_util.h"
+
+#include "base/memory/scoped_refptr.h"
+#include "extensions/common/extension.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace extensions::warnings_test_util {
+
+bool HasInstallWarning(scoped_refptr<Extension> extension,
+                       const std::string& expected_message) {
+  return base::ranges::count_if(
+             extension->install_warnings(),
+             [expected_message](const InstallWarning& warning) {
+               return warning.message == expected_message;
+             }) == 1;
+}
+
+}  // namespace extensions::warnings_test_util
