ac9560a556670d13002af5182ac4e1497f1f4b1d
Yuki Shiino <yukishiino@chromium.org>
2022-10-28 09:51:59 +0000

    bindings: Implement web_idl.SyncIterator
    Implements web_idl.SyncIterator and stores all instances of
    iterable/maplike/setlike declared in IDL interfaces into the
    web_idl_database.pickle.
    
    web_idl.SyncIterator objects will be used to generate bindings
    code just like web_idl.ObservableArray objects are used to
    generate V8ObservableArrayFooBar.
    
    Bug: 1354546
    Change-Id: I82432c8e10312d13081adc08175e3395feeb65b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3636970
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1064767}

--- third_party/blink/renderer/bindings/scripts/web_idl/user_defined_type.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/user_defined_type.py
@@ -12,6 +12,11 @@ class UserDefinedType(WithIdentifier):
     UserDefinedType is a common base class of spec-author-defined types.
 
     Spec-author-defined types are top-level IDL definitions given an identifier.
+
+    Despite that sync iterators are not top-level IDL definitions nor have an
+    identifier, SyncIterator inherits from UserDefinedType just in order to
+    make bind_gen.interface.generate_class_like work nicely with using
+    is_interface, is_namespace, etc.
     """
 
     def __init__(self, identifier):
@@ -45,6 +50,11 @@ class UserDefinedType(WithIdentifier):
     @property
     def is_namespace(self):
         """Returns True if this is an IDL namespace."""
+        return False
+
+    @property
+    def is_sync_iterator(self):
+        """Returns True if this is a sync iterator."""
         return False
 
 
