4fd716a6b636f6ab3b14ea2dd6c87c39652a0c1a
Sky Malice <skym@chromium.org>
2022-10-28 22:01:33 +0000

    Enable ToolbarControlContainerTest#testIsDirty
    The HardwareDraw path in the ViewResourceAdapter had bugs and did not
    pass this test after other toolbar code was changed. HardwarDraw is no
    longer being experimented with and has been removed from testing config
    in crrev.com/c/3795559. These tests run successfully in the SoftwareDraw
    path.
    
    Bug: 1344612
    Change-Id: Ia4ac1ce82f24ea43f2e9431567ecb635d3e6be8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988638
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065061}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarControlContainerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarControlContainerTest.java
@@ -28,7 +28,6 @@ import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.cc.input.BrowserControlsState;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
@@ -149,7 +148,6 @@ public class ToolbarControlContainerTest {
     }
 
     @Test
-    @DisabledTest(message = "Temporarily disabled due to https://crbug.com/1344612")
     public void testIsDirty() {
         ToolbarViewResourceAdapter adapter = makeAdapter();
         adapter.setOnResourceReadyCallback((resource) -> {});
