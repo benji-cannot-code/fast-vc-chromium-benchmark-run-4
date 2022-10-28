366519287b7d93956683ccc15e66fcf4f041646e
sinafirooz <sinafirooz@google.com>
2022-10-28 18:27:39 +0000

    sensor: Fix race condition on PlatformSensorAndroid destruction
    Validate the pointer to a PlatformSensorAndroid object when passing it
    from Java to C++. This prevents call to NotifyPlatformSensorError after
    the PlatformSensorAndroid has been destroyed.
    
    The C++ PlatformSensorAndroid destructor causes
    mNativePlatformSensorAndroid to be set to 0 on Java side. Trying to call
    a method of PlatformSensorAndroid after will cause a null dereference
    error.
    
    Bug: 1376703
    Change-Id: I1ee517eb2882b10ae828e307ad0998929690d11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988628
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064940}

--- services/device/generic_sensor/android/java/src/org/chromium/device/sensors/PlatformSensor.java
+++ services/device/generic_sensor/android/java/src/org/chromium/device/sensors/PlatformSensor.java
@@ -297,8 +297,10 @@ public class PlatformSensor implements SensorEventListener {
      */
     @GuardedBy("mLock")
     protected void sensorError() {
-        PlatformSensorJni.get().notifyPlatformSensorError(
-                mNativePlatformSensorAndroid, PlatformSensor.this);
+        if (mNativePlatformSensorAndroid != 0) {
+            PlatformSensorJni.get().notifyPlatformSensorError(
+                    mNativePlatformSensorAndroid, PlatformSensor.this);
+        }
     }
 
     /**
