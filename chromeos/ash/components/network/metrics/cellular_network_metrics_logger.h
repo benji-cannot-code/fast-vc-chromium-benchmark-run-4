7def8a94c0d2ca95989bfd7ff1c3d93b9bd67a57
Gordon Seto <gordonseto@google.com>
2022-10-28 20:49:37 +0000

    [CrOS Cellular] Consolidate CellularNetworkMetricsLogger observance
    Remove observance of NetworkStateHandler in CellularNetworkMetricsLogger
    and instead us ConnectionInfoMetricsLogger::Observer to observe
    connection attempt finishes in order to emit
    Network.Ash.Cellular.Apn.CustomApns.Count.
    
    Bug: b:162365553
    Test: DUT and *CellularNetworkMetricsLoggerTest*
    Change-Id: Ib8598dc49e9979df552e397b0f8fbce041de7bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988421
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065025}

--- chromeos/ash/components/network/metrics/cellular_network_metrics_logger.h
+++ chromeos/ash/components/network/metrics/cellular_network_metrics_logger.h
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 
-#include "base/containers/flat_set.h"
 #include "base/scoped_observation.h"
 #include "chromeos/ash/components/network/metrics/connection_info_metrics_logger.h"
-#include "chromeos/ash/components/network/network_state_handler_observer.h"
 
 namespace ash {
 
@@ -19,8 +17,7 @@ class NetworkMetadataStore;
 
 // Provides APIs for logging metrics related to cellular networks.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularNetworkMetricsLogger
-    : public NetworkStateHandlerObserver,
-      public ConnectionInfoMetricsLogger::Observer {
+    : public ConnectionInfoMetricsLogger::Observer {
  public:
   CellularNetworkMetricsLogger(
       NetworkStateHandler* network_state_handler,
@@ -32,20 +29,10 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularNetworkMetricsLogger
   ~CellularNetworkMetricsLogger() override;
 
  private:
-  // NetworkStateHandlerObserver:
-  void NetworkListChanged() override;
-  void NetworkConnectionStateChanged(const NetworkState* network) override;
-
   // ConnectionInfoMetricsLogger::Observer:
   void OnConnectionResult(
       const std::string& guid,
       const absl::optional<std::string>& shill_error) override;
-
-  // Logs the number of custom APNs saved for a network.
-  void AttemptLogCustomApnsCount(const NetworkState* network);
-
-  // GUIDs of cellular networks that are currently connected.
-  base::flat_set<std::string> connected_cellular_network_guids_;
 
   NetworkStateHandler* network_state_handler_ = nullptr;
   NetworkMetadataStore* network_metadata_store_ = nullptr;
