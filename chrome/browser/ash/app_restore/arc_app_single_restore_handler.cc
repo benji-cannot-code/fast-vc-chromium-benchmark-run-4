b7d6276c11732f5a8180f2b2e82e7f5212a0e696
Shengsong Tan <sstan@google.com>
2022-10-28 11:40:46 +0000

    arc: Activate ARC when launch app with ghost window
    With go/arc-on-demand-v1, ARC may not be active even if it's enabled.
    Call ArcSessionManager::AllowActivation() to activate ARC when restoring
    apps
    
    BUG=b:255886216
    TEST=Add --enable-features=ArcOnDemand to /etc/chrome_dev.conf and
    launch Play Store with ghost window
    
    Change-Id: I54e21d526afb1527aba72df2d072f10569989a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986763
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064789}

--- chrome/browser/ash/app_restore/arc_app_single_restore_handler.cc
+++ chrome/browser/ash/app_restore/arc_app_single_restore_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/app_restore/arc_ghost_window_handler.h"
+#include "chrome/browser/ash/arc/session/arc_session_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/app_restore/app_launch_info.h"
 #include "components/app_restore/app_restore_data.h"
@@ -34,6 +35,11 @@ void ArcAppSingleRestoreHandler::LaunchGhostWindowWithApp(
     int event_flags,
     arc::GhostWindowType window_type,
     arc::mojom::WindowInfoPtr window_info) {
+  // Activate ARC in case still not active. ArcSessionManager may null in test
+  // env.
+  if (arc::ArcSessionManager::Get())
+    arc::ArcSessionManager::Get()->AllowActivation();
+
   // The ghost window and corresponding shelf item need to be added after ash
   // shelf ready.
   if (!is_shelf_ready_) {
