279a3a0a075ebae06b21adea503e2674201c42e6
Calder Kitagawa <ckitagawa@chromium.org>
2022-10-28 19:03:40 +0000

    Revert "Reland "[Android Bulk Tab Edit] Longpress for TabSelectionEditor entry""
    This reverts commit 0e7ad51f1737c155c330839da77523e8a2db78a5.
    
    Reason for revert: https://crbug.com/1379222
    
    Causes crashes even when flag is not enabled according to report.
    Possibly need to ensure "cached" state is safely purged.
    
    Original change's description:
    > Reland "[Android Bulk Tab Edit] Longpress for TabSelectionEditor entry"
    >
    > This is a reland of commit 8eb0ac6c83865f05e170582e025361cd60835861.
    >
    > Relanding the original longpress CL since this does not seem to be related to crbug.com/1378276.
    >
    > Revert: https://chromium-review.googlesource.com/c/chromium/src/+/3978272
    >
    > Original change's description:
    > > [Android Bulk Tab Edit] Longpress for TabSelectionEditor entry
    > >
    > > Propsosed implementation of longpress entrypoint to the
    > > TabSelectionEditor.
    > >
    > > Bug: crbug.com/1343374
    > > Change-Id: I69629c5a247adb47a49fbfc98906c81926992d85
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905682
    > > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > > Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    > > Reviewed-by: Xi Han <hanxi@chromium.org>
    > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1062844}
    >
    > Bug: crbug.com/1343374
    > Change-Id: I86a782d2ba9a3e217453095d0c71dedf21904bd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983412
    > Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1063961}
    
    Bug: 1379222, 1343374
    Change-Id: I957dd6e084b0bec6ce9114490f8b481acb625090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990213
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1064955}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
@@ -76,8 +76,7 @@ import java.util.List;
 public class TabSwitcherCoordinator
         implements DestroyObserver, TabSwitcher, TabSwitcher.TabListDelegate,
                    TabSwitcherMediator.ResetHandler, TabSwitcherMediator.MessageItemsController,
-                   TabSwitcherMediator.PriceWelcomeMessageController,
-                   TabGridItemTouchHelperCallback.OnLongPressEventListener {
+                   TabSwitcherMediator.PriceWelcomeMessageController {
     /**
      * Interface to control the IPH dialog.
      */
@@ -186,7 +185,6 @@ public class TabSwitcherCoordinator
                         return true;
                     } else if (id == R.id.menu_select_tabs) {
                         showTabSelectionEditorV2();
-                        RecordUserAction.record("TabMultiSelectV2.OpenFromGrid");
                         RecordUserAction.record("MobileMenuSelectTabs");
                         return true;
                     } else if (id == R.id.track_prices_row_menu_id) {
@@ -259,7 +257,7 @@ public class TabSwitcherCoordinator
             mTabListCoordinator = new TabListCoordinator(mode, activity, tabModelSelector,
                     mMultiThumbnailCardProvider, titleProvider, true, mMediator, null,
                     TabProperties.UiType.CLOSABLE, null, this, container, true, COMPONENT_NAME,
-                    mRootView, null, this);
+                    mRootView, null);
             mContainerViewChangeProcessor = PropertyModelChangeProcessor.create(containerViewModel,
                     mTabListCoordinator.getContainerView(), TabListContainerViewBinder::bind);
 
@@ -554,6 +552,7 @@ public class TabSwitcherCoordinator
             tabs.add(list.getTabAt(i));
         }
         mTabSelectionEditorCoordinator.getController().show(tabs);
+        RecordUserAction.record("TabMultiSelectV2.OpenFromGrid");
     }
 
     private void setUpPriceTracking(Context context, ModalDialogManager modalDialogManager) {
@@ -782,13 +781,6 @@ public class TabSwitcherCoordinator
                                           .getCurrentTabModelFilter()
                                           .index());
         }
-    }
-
-    // OnLongPressEventListener implementation
-    @Override
-    public void onLongPressEvent(int tabId) {
-        showTabSelectionEditorV2();
-        RecordUserAction.record("TabMultiSelectV2.OpenLongPressInGrid");
     }
 
     private void appendMessagesTo(int index) {
