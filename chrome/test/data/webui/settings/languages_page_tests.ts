d4812b2bfabdf0b37f9eac095843e9696705375f
Trevor Perrier <perrier@chromium.org>
2022-10-28 19:32:15 +0000

    [LanguageSettings] Update fieldtrial_testing_config
    This CL enables the DesktopDetailedLanguageSettings feature in
    fieldtrail_testing_config.
    
    It splits CrSettingsLanguagesPageTest.LanguageMenu into two suites:
    - LanguageMenuWithTranslate
    - LanguageMenuMoveLanguages
    
    The first suite tests the old behavior with the translate option
    integrated into the overflow menu and forces detailed language settings
    to false.
    
    The second test only tests moving the language menu items which is
    done on both the old and new language settings.
    
    Bug: 1331833
    Change-Id: I2485f74b9cd3a3c16f6bb9b3e8f70b4c3b91ad00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976873
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064975}

--- chrome/test/data/webui/settings/languages_page_tests.ts
+++ chrome/test/data/webui/settings/languages_page_tests.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-import {isChromeOS, isWindows} from 'chrome://resources/js/cr.m.js';
+import {isWindows} from 'chrome://resources/js/cr.m.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.js';
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -23,7 +23,8 @@ import {TestLanguagesBrowserProxy} from './test_languages_browser_proxy.js';
 const languages_page_tests = {
   TestNames: {
     AddLanguagesDialog: 'add languages dialog',
-    LanguageMenu: 'language menu',
+    LanguageMenuWithTranslate: 'language menu with translate options',
+    LanguageMenuMoveLanguages: 'move language menu items',
   },
 };
 
@@ -34,6 +35,20 @@ suite('languages page', function() {
   let languagesPage: SettingsLanguagesPageElement;
   let actionMenu: CrActionMenuElement;
   let browserProxy: TestLanguagesBrowserProxy;
+
+  /*
+   * Finds, asserts and returns the menu item for the given i18n key.
+   * @param i18nKey Name of the i18n string for the item's text.
+   */
+  function getMenuItem<T extends HTMLElement>(i18nKey: string): T {
+    const i18nString = loadTimeData.getString(i18nKey);
+    assertTrue(!!i18nString);
+    const menuItems = actionMenu.querySelectorAll<T>('.dropdown-item');
+    const menuItem = Array.from(menuItems).find(
+        item => item.textContent!.trim() === i18nString);
+    assertTrue(!!menuItem, 'Menu item "' + i18nKey + '" not found');
+    return menuItem;
+  }
 
   // Initial value of enabled languages pref used in tests.
   const initialLanguages = 'en-US,sw';
@@ -269,36 +284,17 @@ suite('languages page', function() {
     });
   });
 
-  suite(languages_page_tests.TestNames.LanguageMenu, function() {
+  suite(languages_page_tests.TestNames.LanguageMenuWithTranslate, function() {
     /*
-     * Finds, asserts and returns the menu item for the given i18n key.
-     * @param i18nKey Name of the i18n string for the item's text.
+     * When the detailed language settings are not shown the translate options
+     * are integrated with the language list. This suite tests the translate
+     * option in the languages submenu.
      */
-    function getMenuItem<T extends HTMLElement>(i18nKey: string): T {
-      const i18nString = loadTimeData.getString(i18nKey);
-      assertTrue(!!i18nString);
-      const menuItems = actionMenu.querySelectorAll<T>('.dropdown-item');
-      const menuItem = Array.from(menuItems).find(
-          item => item.textContent!.trim() === i18nString);
-      assertTrue(!!menuItem, 'Menu item "' + i18nKey + '" not found');
-      return menuItem;
-    }
-
-    /*
-     * Checks the visibility of each expected menu item button.
-     * @param Dictionary from i18n keys to expected visibility of those menu
-     *     items.
-     */
-    function assertMenuItemButtonsVisible(
-        buttonVisibility: {[key: string]: boolean}) {
-      assertTrue(actionMenu.open);
-      for (const buttonKey of Object.keys(buttonVisibility)) {
-        const buttonItem = getMenuItem(buttonKey);
-        assertEquals(
-            !buttonVisibility[buttonKey], buttonItem.hidden,
-            'Menu item "' + buttonKey + '" hidden');
-      }
-    }
+    suiteSetup(function() {
+      loadTimeData.overrideValues({
+        enableDesktopDetailedLanguageSettings: false,
+      });
+    });
 
     test('structure', function() {
       const languageOptionsDropdownTrigger =
@@ -313,9 +309,9 @@ suite('languages page', function() {
 
       // Disable Translate. On platforms that can't change the UI language,
       // this hides all the checkboxes, so the separator isn't needed.
-      // Chrome OS and Windows still show a checkbox and thus the separator.
+      // Windows still shows a checkbox and thus the separator.
       languagesPage.setPrefValue('translate.enabled', false);
-      assertEquals(isChromeOS || isWindows ? 1 : 0, separator.offsetHeight);
+      assertEquals(isWindows ? 1 : 0, separator.offsetHeight);
     });
 
     test('test translate.enable toggle', function() {
@@ -435,6 +431,25 @@ suite('languages page', function() {
       assertTrue(!!translateOption);
       assertTrue(translateOption.hidden);
     });
+  });
+
+  suite(languages_page_tests.TestNames.LanguageMenuMoveLanguages, function() {
+    /*
+     * Checks the visibility of each expected menu item button.
+     * @param Dictionary from i18n keys to expected visibility of those menu
+     *     items.
+     */
+    function assertMenuItemButtonsVisible(
+        buttonVisibility: {[key: string]: boolean}) {
+      assertTrue(actionMenu.open);
+      for (const buttonKey of Object.keys(buttonVisibility)) {
+        const buttonItem = getMenuItem(buttonKey);
+        assertEquals(
+            !buttonVisibility[buttonKey], buttonItem.hidden,
+            'Menu item "' + buttonKey + '" hidden');
+      }
+    }
+
 
     test('remove language when starting with 3 languages', function() {
       // Enable a language which we can then disable.
@@ -447,8 +462,7 @@ suite('languages page', function() {
       const items =
           languagesPage.shadowRoot!.querySelector('#languagesSection')!
               .querySelectorAll<HTMLElement>('.list-item');
-      const domRepeat =
-          languagesPage.shadowRoot!.querySelector('dom-repeat');
+      const domRepeat = languagesPage.shadowRoot!.querySelector('dom-repeat');
       assertTrue(!!domRepeat);
       const item = Array.from(items).find(function(el) {
         return domRepeat.itemForElement(el) &&
@@ -482,8 +496,7 @@ suite('languages page', function() {
       const items =
           languagesPage.shadowRoot!.querySelector('#languagesSection')!
               .querySelectorAll<HTMLElement>('.list-item');
-      const domRepeat =
-          languagesPage.shadowRoot!.querySelector('dom-repeat');
+      const domRepeat = languagesPage.shadowRoot!.querySelector('dom-repeat');
       assertTrue(!!domRepeat);
       const item = Array.from(items).find(function(el) {
         return domRepeat.itemForElement(el) &&
@@ -504,8 +517,7 @@ suite('languages page', function() {
       const items =
           languagesPage.shadowRoot!.querySelector('#languagesSection')!
               .querySelectorAll<HTMLElement>('.list-item');
-      const domRepeat =
-          languagesPage.shadowRoot!.querySelector('dom-repeat');
+      const domRepeat = languagesPage.shadowRoot!.querySelector('dom-repeat');
       assertTrue(!!domRepeat);
       const item = Array.from(items).find(function(el) {
         return domRepeat.itemForElement(el) &&
