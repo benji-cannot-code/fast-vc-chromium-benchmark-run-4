d09ec9139bb76e400560afd03d4298618958d5e1
Aaron Leventhal <aleventhal@google.com>
2022-10-28 20:05:09 +0000

    Move a11y to post-lifecycle steps (iteration #5)
    This is a reland of CL:3964415 and supporting CLs, which
    were reverted together with a number of supporting CLs,
    in CL:3990067 because of crbug.com/1376991. The problematic code
    is fixed in CL:3988990. Also fixes a flake in week pickers
    reported in bug 1378062.
    
    A11y is moved to the end of the document lifecycle, where it can
    occur in parallel with other tasks.
    
    Parts of the code that used to force layout updates in order to
    update the AX tree now call UpdateAXForAllDocuments(), a new
    method introduced in the previous CL, which first updates the
    lifecycle and then manually updates the AX tree.
    
    Additional code cleanups in AXObjectCacheImpl related to
    popups were also now possible.
    
    Test changes:
    * Changes to css-display-expected-win.txt and content-editable-notifications.html were due to a subtle change in event ordering,
    but are still correct.
    
    Fixed: 1378062,1376991
    NOTRY: true
    Change-Id: I7fa925e12cfb7352cbaf9c2deed4b83e47ae084d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991106
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064998}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2130,6 +2130,7 @@ const AtomicString& Element::computedRole() {
         DocumentUpdateReason::kJavaScript);
   }
   AXContext ax_context(document, ui::kAXModeBasic);
+  ax_context.GetAXObjectCache().ProcessDeferredAccessibilityEvents(document);
   return ax_context.GetAXObjectCache().ComputedRoleForNode(this);
 }
 
@@ -2143,6 +2144,7 @@ String Element::computedName() {
         DocumentUpdateReason::kJavaScript);
   }
   AXContext ax_context(document, ui::kAXModeBasic);
+  ax_context.GetAXObjectCache().ProcessDeferredAccessibilityEvents(document);
   return ax_context.GetAXObjectCache().ComputedNameForNode(this);
 }
 
--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -2647,7 +2647,7 @@ void LocalFrame::DidResume() {
 
   // TODO(yuzus): Figure out where these calls should really belong.
   GetDocument()->DispatchHandleLoadStart();
-  GetDocument()->DispatchHandleLoadOrLayoutComplete();
+  GetDocument()->DispatchHandleLoadComplete();
 }
 
 void LocalFrame::MaybeLogAdClickNavigation() {
