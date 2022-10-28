16bed97abca34e019e3aa7a52cb07028407aaea6
Gordon Seto <gordonseto@google.com>
2022-10-28 18:12:04 +0000

    [CrOS Cellular] Emit EnabledCustomApnsStatus on cellular connect attempt
    Emit Network.Ash.Cellular.NoEnabledCustomApns.ConnectionResult.All or
    Network.Ash.Cellular.HasEnabledCustomApns.ConnectionResult.All when
    a Cellular network connection attempt occurs.
    
    Bug: b:162365553
    Test: DUT and *ConnectionInfoMetricsLoggerTest*
    Change-Id: Iba268b0d0869d99c65facc3d8bed1e6355cd9141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984251
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064931}

--- chromeos/ash/components/network/metrics/cellular_network_metrics_logger.h
+++ chromeos/ash/components/network/metrics/cellular_network_metrics_logger.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 
 #include "base/containers/flat_set.h"
+#include "base/scoped_observation.h"
+#include "chromeos/ash/components/network/metrics/connection_info_metrics_logger.h"
 #include "chromeos/ash/components/network/network_state_handler_observer.h"
 
 namespace ash {
@@ -17,19 +19,27 @@ class NetworkMetadataStore;
 
 // Provides APIs for logging metrics related to cellular networks.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularNetworkMetricsLogger
-    : public NetworkStateHandlerObserver {
+    : public NetworkStateHandlerObserver,
+      public ConnectionInfoMetricsLogger::Observer {
  public:
-  CellularNetworkMetricsLogger(NetworkStateHandler* network_state_handler,
-                               NetworkMetadataStore* network_metadata_store);
+  CellularNetworkMetricsLogger(
+      NetworkStateHandler* network_state_handler,
+      NetworkMetadataStore* network_metadata_store,
+      ConnectionInfoMetricsLogger* connection_info_metrics_logger);
   CellularNetworkMetricsLogger(const CellularNetworkMetricsLogger&) = delete;
   CellularNetworkMetricsLogger& operator=(const CellularNetworkMetricsLogger&) =
       delete;
   ~CellularNetworkMetricsLogger() override;
 
  private:
-  // NetworkStateHandlerObserver::
+  // NetworkStateHandlerObserver:
   void NetworkListChanged() override;
   void NetworkConnectionStateChanged(const NetworkState* network) override;
+
+  // ConnectionInfoMetricsLogger::Observer:
+  void OnConnectionResult(
+      const std::string& guid,
+      const absl::optional<std::string>& shill_error) override;
 
   // Logs the number of custom APNs saved for a network.
   void AttemptLogCustomApnsCount(const NetworkState* network);
@@ -39,6 +49,10 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularNetworkMetricsLogger
 
   NetworkStateHandler* network_state_handler_ = nullptr;
   NetworkMetadataStore* network_metadata_store_ = nullptr;
+
+  base::ScopedObservation<ConnectionInfoMetricsLogger,
+                          ConnectionInfoMetricsLogger::Observer>
+      connection_info_metrics_logger_observation_{this};
 };
 
 }  // namespace ash
