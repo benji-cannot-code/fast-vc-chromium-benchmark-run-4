2576ace4bc9040c8cc577406f28bda39fe150d98
Mike Dougherty <michaeldo@chromium.org>
2022-10-28 14:29:08 +0000

    Move CWTWebDriverAppInterface JS calls to WebFrame::ExecuteJavaScript
    Bug: 1216152
    Change-Id: I66df032f6e2140bb7181ce575a0c7763a223bb92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989483
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064822}

--- ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
+++ ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/chrome/test/wpt/cwt_stderr_logger.h"
 #import "ios/testing/nserror_util.h"
+#import "ios/web/public/js_messaging/web_frame.h"
+#import "ios/web/public/js_messaging/web_frame_util.h"
 #import "ios/web/public/test/navigation_test_util.h"
 #import "ios/web/public/ui/crw_web_view_proxy.h"
 #import "ios/web/public/web_state.h"
@@ -238,7 +240,8 @@ void DispatchSyncOnMainThread(void (^block)(void)) {
       return;
     webStateFound = YES;
     subscription = webState->AddScriptCommandCallback(callback, command);
-    webState->ExecuteJavaScript(
+    web::WebFrame* main_frame = web::GetMainFrame(webState);
+    main_frame->ExecuteJavaScript(
         base::UTF8ToUTF16(scriptFunctionWithCompletionHandler));
   });
 
