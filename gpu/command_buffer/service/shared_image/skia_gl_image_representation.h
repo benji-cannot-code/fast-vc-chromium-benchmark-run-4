927d687b30344122ea9dd07180ccc270f969aaee
Michael Tang <tangm@microsoft.com>
2022-10-28 22:30:21 +0000

    gpu: Add update_rect parameter to BeginScopedWriteAccess
    Add a parameter to BeginScopedWriteAccess that can be used as a hint so
    that SharedImageBackings can potentially optimize their draws. This is
    mainly intended for a new IDCompositionSurface-based backing.
    
    Bug: 1378741
    Change-Id: I1fcba72f3fe80bdb2e9b79a010946e2f2ea4f4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984225
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1065072}

--- gpu/command_buffer/service/shared_image/skia_gl_image_representation.h
+++ gpu/command_buffer/service/shared_image/skia_gl_image_representation.h
@@ -32,6 +32,7 @@ class GPU_GLES2_EXPORT SkiaGLImageRepresentation
   std::vector<sk_sp<SkSurface>> BeginWriteAccess(
       int final_msaa_count,
       const SkSurfaceProps& surface_props,
+      const gfx::Rect& update_rect,
       std::vector<GrBackendSemaphore>* begin_semaphores,
       std::vector<GrBackendSemaphore>* end_semaphores,
       std::unique_ptr<GrBackendSurfaceMutableState>* end_state) override;
