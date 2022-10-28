5676b0dc58095fd674ff1ee0d0b2ba16feffee76
Clement Yan <clamclamyan@google.com>
2022-10-28 04:01:59 +0000

    [WebUI] Migrate add supervision UI
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: Iafd61b17e30dcbb98ec541889aeb0324aaba2222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989805
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064692}

--- chrome/browser/ui/webui/ash/add_supervision/add_supervision_ui.h
+++ chrome/browser/ui/webui/ash/add_supervision/add_supervision_ui.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision.mojom-forward.h"
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision_handler.h"
 #include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
+#include "chrome/common/webui_url_constants.h"
+#include "content/public/browser/webui_config.h"
+#include "content/public/common/url_constants.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/native_widget_types.h"
@@ -61,6 +64,17 @@ class AddSupervisionDialog : public SystemWebDialogDelegate {
 
  private:
   bool should_close_on_escape_ = true;
+};
+
+class AddSupervisionUI;
+
+// WebUIConfig for chrome://add-supervision
+class AddSupervisionUIConfig
+    : public content::DefaultWebUIConfig<AddSupervisionUI> {
+ public:
+  AddSupervisionUIConfig()
+      : DefaultWebUIConfig(content::kChromeUIScheme,
+                           chrome::kChromeUIAddSupervisionHost) {}
 };
 
 // Controller for chrome://add-supervision
--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -1000,8 +1000,6 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<ash::AccountManagerErrorUI>;
   if (url.host_piece() == chrome::kChromeUIAccountMigrationWelcomeHost)
     return &NewWebUI<ash::AccountMigrationWelcomeUI>;
-  if (url.host_piece() == chrome::kChromeUIAddSupervisionHost)
-    return &NewWebUI<ash::AddSupervisionUI>;
   if (url.host_piece() == chrome::kChromeUIParentAccessHost)
     return &NewWebUI<ash::ParentAccessUI>;
   if (url.host_piece() == chrome::kChromeUIAudioHost &&
