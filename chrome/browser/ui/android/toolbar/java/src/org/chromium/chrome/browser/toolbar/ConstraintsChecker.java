737f0f47d41fe3108dceae55578003e1e3f2ddb0
Sky Malice <skym@chromium.org>
2022-10-28 21:45:26 +0000

    Touch suppression cleanup.
    Follow up to comments in merge https://crrev.com/c/3983931.
    
    Bug: 1374427
    Change-Id: I10a0500f7111ce536c948fe052bafe43f0abd415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988534
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065050}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ConstraintsChecker.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ConstraintsChecker.java
@@ -65,7 +65,7 @@ public class ConstraintsChecker implements Callback<Integer> {
     public void onResult(Integer result) {
         if (!areControlsLocked()) {
             mConstraintsSupplier.removeObserver(this);
-            mHandler.post(() -> mViewResourceAdapter.onResourceRequested());
+            mHandler.post(mViewResourceAdapter::onResourceRequested);
         }
     }
 }
