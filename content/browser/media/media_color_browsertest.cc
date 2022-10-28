48acb86a7671504596e55e23e09e889fdf27e3f5
Jagdish Chourasia <jagdish.c@samsung.com>
2022-10-28 21:26:21 +0000

    [media] Code cleanup
    Kitkat is deprecated in chrome, this code is not needed anymore
    
    Change-Id: I74f1a82fd56feae5920b2ff6089f13df52ca04eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990129
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Auto-Submit: Jagdish Chourasia <jagdish.c@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1065042}

--- content/browser/media/media_color_browsertest.cc
+++ content/browser/media/media_color_browsertest.cc
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/test_data_util.h"
 #include "media/media_buildflags.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "base/android/build_info.h"
-#endif
-
 namespace content {
 
 class MediaColorTest : public MediaBrowserTest {
@@ -106,14 +102,6 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv444pVp9) {
 #define MAYBE_Yuv420pH264 Yuv420pH264
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pH264) {
-#if BUILDFLAG(IS_ANDROID)
-  // https://crbug.com/907572
-  if (base::android::BuildInfo::GetInstance()->sdk_int() <=
-      base::android::SDK_VERSION_KITKAT) {
-    DVLOG(0) << "Skipping test - Yuv420pH264 is flaky on KitKat devices.";
-    return;
-  }
-#endif
   RunColorTest("yuv420p.mp4");
 }
 
@@ -137,14 +125,6 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuvj420pH264) {
 #define MAYBE_Yuv420pRec709H264 Yuv420pRec709H264
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pRec709H264) {
-#if BUILDFLAG(IS_ANDROID)
-  // https://crbug.com/907572
-  if (base::android::BuildInfo::GetInstance()->sdk_int() <=
-      base::android::SDK_VERSION_KITKAT) {
-    DVLOG(0) << "Skipping test - Yuv420pRec709H264 is flaky on KitKat devices.";
-    return;
-  }
-#endif
   RunColorTest("yuv420p_rec709.mp4");
 }
 
