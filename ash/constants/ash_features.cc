441a685dda7357e2b00cc025ec5212e54f5ddbce
Jerry Liu <pzliu@google.com>
2022-10-28 01:12:28 +0000

    personalization: feature flag kAmbientSubpageUIChange
    Add a feature and entry in chrome://flags for kAmbientSubpageUIChange.
    It will be used to hide implementations for the new ambient subpage as
    part of PUBv2.
    
    BUG=b:255370406, b:255370980
    TEST=Manually check chrome://flags
    
    Change-Id: If656df2d182679df69e17b27b8f0bc9ed79551f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984256
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064643}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -130,6 +130,10 @@ BASE_FEATURE(kAmbientModeThrottleAnimation,
              "ChromeOSAmbientModeThrottleAnimation",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kAmbientSubpageUIChange,
+             "AmbientSubpageUIChange",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kApnRevamp, "ApnRevamp", base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Controls whether to enable ARC ADB sideloading support.
@@ -2207,6 +2211,10 @@ bool IsAmbientModePhotoPreviewEnabled() {
 
 bool IsAmbientModeThrottleAnimationEnabled() {
   return base::FeatureList::IsEnabled(kAmbientModeThrottleAnimation);
+}
+
+bool IsAmbientSubpageUIChangeEnabled() {
+  return base::FeatureList::IsEnabled(kAmbientSubpageUIChange);
 }
 
 bool IsApnRevampEnabled() {
