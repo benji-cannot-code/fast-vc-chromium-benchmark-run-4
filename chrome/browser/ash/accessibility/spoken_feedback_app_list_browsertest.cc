35d03215926795ef8078074e90c025d046d8ebb1
Amanda Deacon <amandadeacon@chromium.org>
2022-10-28 04:00:59 +0000

    [Launcher health] Remove the classic launcher.
    The search_result_ranker/* directory and several classes are only
    needed by the classic launcher. These are no longer needed, due to the
    complete launch of the productivity launcher.
    
    Remove all of this code at once. It is difficult/impossible to do this in smaller steps, due to cyclic dependencies. See diagram: http://shortn/_53jXvfbFwp
    
    In this CL:
    
    1) Delete SearchControllerImpl class.
    2) From search controller interface, delete AddGroup() and modify
       AddProvider().
    3) Delete Mixer class.
    4) Delete search_result_ranker/ directory.
       N.B. Leave 20190923_example_preprocessor_config.pb for follow-up
       deletion, due to different owners' requirements.
    
    SearchControllerImpl has been succeeded by SearchControllerImplNew. A
    follow-up CL will rename SearchControllerImplNew to SearchControllerImpl. This is not done in this CL for ease of reviewing.
    
    Test: existing; also ran a clean build
    Bug: 1258415
    Change-Id: I411c87db605ad1b3b7e29560e102d19ba23721fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3739903
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Auto-Submit: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064691}

--- chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/chrome_app_list_model_updater.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
-#include "chrome/browser/ui/app_list/search/search_controller_impl.h"
 #include "chrome/browser/ui/app_list/search/search_controller_impl_new.h"
 #include "chrome/browser/ui/app_list/search/search_provider.h"
 #include "chrome/browser/ui/app_list/test/chrome_app_list_test_support.h"
@@ -138,8 +137,7 @@ void InitializeTestSearchProviders(
           ChromeSearchResult::Category::kApps,
           ChromeSearchResult::ResultType::kInstalledApp);
   *apps_provider_ptr = apps_provider.get();
-  size_t apps_group_id = search_controller->AddGroup(10);
-  search_controller->AddProvider(apps_group_id, std::move(apps_provider));
+  search_controller->AddProvider(std::move(apps_provider));
 
   std::unique_ptr<TestSearchProvider> web_provider =
       std::make_unique<TestSearchProvider>(
@@ -147,8 +145,7 @@ void InitializeTestSearchProviders(
           ChromeSearchResult::Category::kWeb,
           ChromeSearchResult::ResultType::kOmnibox);
   *web_provider_ptr = web_provider.get();
-  size_t omnibox_group_id = search_controller->AddGroup(10);
-  search_controller->AddProvider(omnibox_group_id, std::move(web_provider));
+  search_controller->AddProvider(std::move(web_provider));
 }
 
 }  // namespace
--- chrome/browser/ui/app_list/search/ranking/filtering_ranker.cc
+++ chrome/browser/ui/app_list/search/ranking/filtering_ranker.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/ranking/constants.h"
 #include "chrome/browser/ui/app_list/search/ranking/util.h"
-#include "chrome/browser/ui/app_list/search/search_result_ranker/recurrence_ranker.h"
 
 namespace app_list {
 namespace {
