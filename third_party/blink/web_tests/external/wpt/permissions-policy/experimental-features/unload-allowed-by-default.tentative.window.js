6da2c77f520573a419b9787a4552b8c3391f6f1a
Fergal Daly <fergal@chromium.org>
2022-10-28 09:26:31 +0000

    Add cross-origin case to Permissions-Policy: unload WPT.
    Without this, if we update the default to EnabledForSelf, none of
    the WPTs fail.
    
    Bug: 1324111
    Change-Id: I18006429151e1a24dc95fee150edcbf40e50e339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989531
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064762}

--- third_party/blink/web_tests/external/wpt/permissions-policy/experimental-features/unload-allowed-by-default.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/permissions-policy/experimental-features/unload-allowed-by-default.tentative.window.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title='unload' Policy : allowed by default
 // META: script=/common/dispatcher/dispatcher.js
+// META: script=/common/get-host-info.sub.js
 // META: script=/common/utils.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=./resources/unload-helper.js
@@ -14,7 +15,9 @@ promise_test(async t => {
       new RemoteContextHelper({scripts: ['./resources/unload-helper.js']});
   // In the same browsing context group to ensure BFCache is not used.
   const main = await rcHelper.addWindow();
-  const subframe = await main.addIframe();
-  await assertWindowRunsUnload(subframe, 'subframe', {shouldRunUnload: true});
+  const sameOriginSubframe = await main.addIframe();
+  const crossOriginSubframe = await main.addIframe({ origin: 'HTTP_REMOTE_ORIGIN' });
+  await assertWindowRunsUnload(sameOriginSubframe, 'sameOriginSubframe', { shouldRunUnload: true });
+  await assertWindowRunsUnload(crossOriginSubframe, 'crossOriginSubframe', { shouldRunUnload: true });
   await assertWindowRunsUnload(main, 'main', {shouldRunUnload: true});
 });
