d605961539c0ef2f8fd39c9141db8a97eb4c551c
Peter Boström <pbos@chromium.org>
2022-10-28 23:50:15 +0000

    Lazily call stream() for (D)CHECK streaming
    This decreases binary size on a (D)CHECK-enabled official build from
    343744k to 342488k. Most (D)CHECKs don't stream.
    
    Bug: 1379405
    Change-Id: I1c6724772eb3cbb7c38e3e2a2777c0f0d1e75aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991412
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065103}

--- base/debug/crash_logging.cc
+++ base/debug/crash_logging.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/crash_logging.h"
 
+#include <ostream>
+
 #include "base/strings/string_piece.h"
 #include "build/build_config.h"
 
--- base/win/scoped_winrt_initializer.cc
+++ base/win/scoped_winrt_initializer.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <roapi.h>
 #include <windows.h>
 
+#include <ostream>
+
 #include "base/check_op.h"
 #include "base/threading/scoped_thread_priority.h"
 #include "base/win/com_init_util.h"
