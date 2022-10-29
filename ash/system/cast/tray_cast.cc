5eba73a7dc01dc0e37dc5607ae6e39fe33b11526
Jiaming Cheng <jiamingc@chromium.org>
2022-10-29 00:35:33 +0000

    Show cast feature tile in the revamped view
    Also
        -Removed the ash switch and use the revamped view to show the
    cast tile.
        -Added the cast temp button in the revamped qs view.
    
    
    Bug: b:252872586
    Change-Id: Ibda3b3a843df1aa966de2ddadb26758a606f5f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989859
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065116}

--- ash/system/cast/tray_cast.cc
+++ ash/system/cast/tray_cast.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/ash_view_ids.h"
 #include "ash/public/cpp/system_tray_client.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -20,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/hover_highlight_view.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_detailed_view.h"
-#include "base/command_line.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/branding_buildflags.h"
@@ -135,10 +133,9 @@ void CastDetailedView::UpdateReceiverListFromCachedData() {
     view_to_sink_map_[container] = sink.id;
   }
 
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kForceQuickSettingsCast)) {
-    // Don't associate this placeholder item with a sink, since it's not a real
-    // device.
+  if (features::IsQsRevampEnabled()) {
+    // TODO(b/252872586): Implement an empty state of the cast detailed view.
+    // The following line shows a fake entry.
     AddScrollListItem(scroll_content(), kSystemMenuCastGenericIcon, u"Device");
   }
 
@@ -156,7 +153,7 @@ void CastDetailedView::HandleViewClicked(views::View* view) {
   } else if (view == add_access_code_device_) {
     base::RecordAction(base::UserMetricsAction(
         "StatusArea_Cast_Detailed_Launch_AccesCastDialog"));
-    Shell::Get()->system_tray_model()->client()-> ShowAccessCodeCastingDialog(
+    Shell::Get()->system_tray_model()->client()->ShowAccessCodeCastingDialog(
         AccessCodeCastDialogOpenLocation::kSystemTrayCastMenu);
   }
 }
