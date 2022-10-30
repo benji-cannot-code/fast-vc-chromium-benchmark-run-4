14566474a27789b2e7033172b4dc365159ef361c
Guido Urdaneta <guidou@chromium.org>
2022-10-30 07:22:03 +0000

    Revert "getDisplayMedia({video: {frameRate: {max: 0}}}) should fail"
    This reverts commit 236968447f0ebc5440625d41292ca9b2aaffd26f.
    
    Reason for revert: A similar change caused an important regression in the past. Let's test that particular scenario before relanding. See https://crbug.com/725696 and https://crbug.com/729746
    
    Original change's description:
    > getDisplayMedia({video: {frameRate: {max: 0}}}) should fail
    >
    > This CL makes sure getDisplayMedia({video: {frameRate: {max: 0}}})
    > fail with OverconstrainedError by changing the minimum frame rate for
    > screen sharing from 0 to 0.01.
    >
    > Bug: 1379243
    > Spec: https://github.com/w3c/mediacapture-screen-share/issues/253
    > Change-Id: Ia9add67565ab1e31b7732ed80c11daa291b3c107
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986448
    > Commit-Queue: Fr <beaufort.francois@gmail.com>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1065088}
    
    Bug: 1379243
    Change-Id: Ia353c4e9ffc1a6c49c8f5fe7b4792e99fb24d47e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991209
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065267}

--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_content.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_content.cc
@@ -36,7 +36,6 @@ static_assert(kDefaultScreenCastWidth <= kMaxScreenCastDimension,
 static_assert(kDefaultScreenCastHeight <= kMaxScreenCastDimension,
               "Invalid kDefaultScreenCastHeight");
 
-const double kMinScreenCastFrameRate = 0.01;
 const double kMaxScreenCastFrameRate = 120.0;
 const double kDefaultScreenCastFrameRate =
     MediaStreamVideoSource::kDefaultFrameRate;
@@ -71,7 +70,7 @@ class VideoContentCaptureCandidates {
                                     kMaxScreenCastDimension),
         frame_rate_set_(
             DoubleRangeSet::FromConstraint(constraint_set.frame_rate,
-                                           kMinScreenCastFrameRate,
+                                           0.0,
                                            kMaxScreenCastFrameRate)),
         device_id_set_(media_constraints::StringSetFromConstraint(
             constraint_set.device_id)),
