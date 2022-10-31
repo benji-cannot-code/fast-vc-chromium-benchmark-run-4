f8c2684721301ca7c6b5bed5ec2079f7655a706b
Luciano Pacheco <lucmult@chromium.org>
2022-10-31 07:44:49 +0000

    Files F2: Convert file_tasks to TS
    Bug: b:228112698
    Change-Id: I229c1fd9933dbbe7a2f9859743c6488577e25703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965697
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065392}

--- ui/file_manager/file_manager/foreground/js/quick_view_uma.js
+++ ui/file_manager/file_manager/foreground/js/quick_view_uma.js
@@ -11,7 +11,7 @@ import {metrics} from '../../common/js/metrics.js';
 import {VolumeManagerCommon} from '../../common/js/volume_manager_types.js';
 import {VolumeManager} from '../../externs/volume_manager.js';
 
-import {FileTasks} from './file_tasks.js';
+import {UMA_INDEX_KNOWN_EXTENSIONS} from './file_tasks.js';
 
 /**
  * UMA exporter for Quick View.
@@ -48,10 +48,10 @@ export class QuickViewUma {
       extension = 'directory';
     } else if (extension === '') {
       extension = 'no extension';
-    } else if (FileTasks.UMA_INDEX_KNOWN_EXTENSIONS.indexOf(extension) < 0) {
+    } else if (UMA_INDEX_KNOWN_EXTENSIONS.indexOf(extension) < 0) {
       extension = 'unknown extension';
     }
-    metrics.recordEnum(name, extension, FileTasks.UMA_INDEX_KNOWN_EXTENSIONS);
+    metrics.recordEnum(name, extension, UMA_INDEX_KNOWN_EXTENSIONS);
   }
 
   /**
