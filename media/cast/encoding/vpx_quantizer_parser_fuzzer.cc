79691d35f8dc4a0b80507fb02e17f1daa28c5c95
Maksim Ivanov <emaxx@chromium.org>
2022-10-31 20:28:55 +0000

    Fuzz media vpx_quantizer_parser
    Implement fuzzer for checking data parsing logic in
    //media/cast/encoding/vpx_quantizer_parser.cc.
    
    Bug: none
    Change-Id: Ie24b46cbee98d3b8c52de2f83feeb96b28fd708a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990791
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065651}

---
+++ media/cast/encoding/vpx_quantizer_parser_fuzzer.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <tuple>
+
+#include "media/cast/encoding/vpx_quantizer_parser.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::ignore = media::cast::ParseVpxHeaderQuantizer(data, size);
+  return 0;
+}
