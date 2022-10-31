95987a0f2655c6e73e4f17a70776ecd01d238be2
Max Ihlenfeldt <max@igalia.com>
2022-10-31 09:37:44 +0000

    Fallback tab dragging: hide drag image while attached
    This uses the new UpdateDragImage() and its ozone/wayland implementation
    so that the drag image for fallback tab dragging (i.e. tab dragging
    using a regular platform drag and drop session) is only displayed while
    in the kDraggingUsingSystemDragAndDrop state, and hidden while in the
    kDraggingTabs state.
    
    It is not possible to just end the drag session when attaching the tabs
    and start a new one when detaching again, due to Wayland's limitations.
    
    Fixed: 1377802
    Change-Id: I29254c045bb9b23743b58c3570ad08be6918849c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988204
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Max Ihlenfeldt <max@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1065416}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/gestures/gesture_recognizer.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/point_conversions.h"
+#include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/event_monitor.h"
@@ -80,6 +81,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "components/remote_cocoa/browser/window.h"
+#endif
+
+#if BUILDFLAG(IS_LINUX)
+#include "ui/aura/client/drag_drop_client.h"
 #endif
 
 #if defined(USE_AURA)
@@ -246,6 +251,18 @@ void OffsetX(int x_offset, std::vector<gfx::Rect>* rects) {
 bool IsWindowDragUsingSystemDragDropAllowed() {
   return base::FeatureList::IsEnabled(
       features::kAllowWindowDragUsingSystemDragDrop);
+}
+
+void UpdateSystemDnDDragImage(TabDragContext* attached_context,
+                              const gfx::ImageSkia& image) {
+#if BUILDFLAG(IS_LINUX)
+  aura::Window* root_window =
+      attached_context->GetWidget()->GetNativeWindow()->GetRootWindow();
+  if (aura::client::GetDragDropClient(root_window)) {
+    aura::client::GetDragDropClient(root_window)
+        ->UpdateDragImage(image, {-image.height() / 2, -image.width() / 2});
+  }
+#endif  // BUILDFLAG(IS_LINUX)
 }
 
 }  // namespace
@@ -1008,6 +1025,9 @@ TabDragController::Liveness TabDragController::ContinueDragging(
       }
       current_state_ = DragState::kDraggingTabs;
       MoveAttached(point_in_screen, true);
+      // Hide the drag image while attached.
+      UpdateSystemDnDDragImage(attached_context_, {});
+
       // Set |tab_strip_changed| to true so that |attached_context_| is
       // activated later on.
       tab_strip_changed = true;
@@ -1137,24 +1157,7 @@ TabDragController::DragBrowserToNewTabStrip(TabDragContext* target_context,
   return DRAG_BROWSER_RESULT_CONTINUE;
 }
 
-TabDragController::Liveness
-TabDragController::StartSystemDragAndDropSessionIfNecessary(
-    const gfx::Point& point_in_screen) {
-  DCHECK(IsWindowDragUsingSystemDragDropAllowed());
-  DCHECK(ui::ResourceBundle::HasSharedInstance());
-  current_state_ = DragState::kDraggingUsingSystemDragAndDrop;
-
-  if (system_drag_and_drop_session_running_)
-    return Liveness::ALIVE;
-  system_drag_and_drop_session_running_ = true;
-
-  auto data_provider = ui::OSExchangeDataProviderFactory::CreateProvider();
-  // Set data in a format that is accepted by TabStrip so that a drop can
-  // happen.
-  base::Pickle pickle;
-  data_provider->SetPickledData(
-      ui::ClipboardFormatType::GetType(ui::kMimeTypeWindowDrag), pickle);
-
+gfx::ImageSkia TabDragController::GetDragImageForSystemDnD() {
   // The width has the same value, as the logo image is square-shaped.
   auto display = display::Screen::GetScreen()->GetDisplayNearestWindow(
       GetAttachedBrowserWidget()->GetNativeWindow());
@@ -1165,11 +1168,38 @@ TabDragController::StartSystemDragAndDropSessionIfNecessary(
   gfx::ImageSkia drag_image =
       *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
           IDR_PRODUCT_LOGO_256);
-  drag_image = gfx::ImageSkiaOperations::CreateResizedImage(
+  return gfx::ImageSkiaOperations::CreateResizedImage(
       drag_image, skia::ImageOperations::RESIZE_BEST, drag_image_size);
+}
+
+TabDragController::Liveness
+TabDragController::StartSystemDragAndDropSessionIfNecessary(
+    const gfx::Point& point_in_screen) {
+  DCHECK(IsWindowDragUsingSystemDragDropAllowed());
+  DCHECK(ui::ResourceBundle::HasSharedInstance());
+  current_state_ = DragState::kDraggingUsingSystemDragAndDrop;
+
+  if (system_drag_and_drop_session_running_) {
+    // Show the drag image again.
+    gfx::ImageSkia drag_image = GetDragImageForSystemDnD();
+    UpdateSystemDnDDragImage(attached_context_, drag_image);
+
+    return Liveness::ALIVE;
+  }
+
+  system_drag_and_drop_session_running_ = true;
+
+  auto data_provider = ui::OSExchangeDataProviderFactory::CreateProvider();
+  // Set data in a format that is accepted by TabStrip so that a drop can
+  // happen.
+  base::Pickle pickle;
+  data_provider->SetPickledData(
+      ui::ClipboardFormatType::GetType(ui::kMimeTypeWindowDrag), pickle);
+
+  gfx::ImageSkia drag_image = GetDragImageForSystemDnD();
   data_provider->SetDragImage(
       drag_image,
-      gfx::Vector2d(-drag_image_height / 2, -drag_image_height / 2));
+      gfx::Vector2d(-drag_image.height() / 2, -drag_image.width() / 2));
 
   base::WeakPtr<TabDragController> ref(weak_factory_.GetWeakPtr());
   GetAttachedBrowserWidget()->RunShellDrag(
