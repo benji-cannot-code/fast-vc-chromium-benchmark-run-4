cb2cd6cc4fb42cec628e9f70362d79a26ccf4bb8
Danil Somsikov <dsv@chromium.org>
2022-10-31 12:57:25 +0000

    Do not wait for pending dispatch in the extensions-resources.js test.
    This doesn't work with tab target and causes occasional timeouts without
    it. I couldn't reproduce flakiness, but if there's one, we should
    address it in a different way.
    
    Bug: 1370050
    Change-Id: I16484a73df5cead4e856251377086ae71da37c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991507
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065445}

--- third_party/blink/web_tests/http/tests/devtools/extensions/extensions-resources.js
+++ third_party/blink/web_tests/http/tests/devtools/extensions/extensions-resources.js
@@ -111,14 +111,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       extension_runWithResource(/audits-style1\.css$/, function(resource) {
         resource.setContent("div.test { width: 140px; height: 42px; }", false);
       });
-      // The next step is going to produce a console message that will be logged, so synchronize the output now.
-      evaluateOnFrontend("TestRunner.deprecatedRunAfterPendingDispatches(reply)", function() {
-        extension_runWithResource(/abe\.png$/, function(resource) {
-          resource.setContent("", true);
-        });
-        extension_runWithResource(/audits-style1\.css$/, function(resource) {
-          resource.setContent(expected_content, true);
-        });
+      extension_runWithResource(/abe\.png$/, function(resource) {
+        resource.setContent("", true);
+      });
+      extension_runWithResource(/audits-style1\.css$/, function(resource) {
+        resource.setContent(expected_content, true);
       });
 
       function onContentCommitted(resource, content) {
