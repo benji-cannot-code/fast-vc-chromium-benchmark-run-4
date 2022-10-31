a8ccd79cf1fbb5b4903dddb4ed5a7693fdfde3b8
Vaibhav Nagarnaik <vnagarnaik@google.com>
2022-10-31 18:36:29 +0000

    Motherboard: Fill in BIOS type for windows
    Test: Verified that the field is being set correctly.
    Change-Id: I7c044946f0ae966ee0f0220842fe68c89ce417f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964616
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Vaibhav Nagarnaik <vnagarnaik@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065585}

--- components/metrics/motherboard.cc
+++ components/metrics/motherboard.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if BUILDFLAG(IS_WIN)
+#include <windows.h>
+
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "base/win/scoped_bstr.h"
@@ -139,6 +141,19 @@ void ReadWin32Bios(const ComPtr<IWbemServices>& services,
   *bios_version = ReadStringMember(class_object, kVersion);
 }
 
+void ReadFirmwareType(absl::optional<Motherboard::BiosType>* bios_type) {
+  FIRMWARE_TYPE firmware_type = FirmwareTypeUnknown;
+  if (GetFirmwareType(&firmware_type)) {
+    if (firmware_type == FirmwareTypeBios) {
+      *bios_type = Motherboard::BiosType::kLegacy;
+    } else if (firmware_type == FirmwareTypeUefi) {
+      *bios_type = Motherboard::BiosType::kUefi;
+    } else {
+      *bios_type = absl::nullopt;
+    }
+  }
+}
+
 MotherboardDetails ReadMotherboardDetails() {
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
@@ -148,6 +163,7 @@ MotherboardDetails ReadMotherboardDetails() {
     return details;
   ReadWin32BaseBoard(services, &details.manufacturer, &details.model);
   ReadWin32Bios(services, &details.bios_manufacturer, &details.bios_version);
+  ReadFirmwareType(&details.bios_type);
   return details;
 }
 #endif
