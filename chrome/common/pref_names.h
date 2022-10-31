9f08a73d3da656d5030eacb6f3760b782cef1902
Jungshik Shin <jshin@chromium.org>
2022-10-31 16:41:45 +0000

    Reland "Turn Hindi Inscript pref to Local State pref"
    This reverts commit 0d96a0254f51c59aee93b6ee74fb2bb4a6a2dc17.
    
    Reason for revert: a fix for the unit test failure, InputMethodConfigurationTest.TestInitialize on linux-chromeos-chrome
    bot, was applied.
    
    Original change's description:
    > Revert "Turn Hindi Inscript pref to Local State pref"
    >
    > This reverts commit 562cd734d309dfca65d3b6f962e302890fe47b6f.
    >
    > Reason for revert: speculative revert for crbug.com/1377729
    >
    > Original change's description:
    > > Turn Hindi Inscript pref to Local State pref
    > >
    > > Make Hindi Inscript Layout pref a Local state pref instead of
    > > a Profile pref.
    > >
    > > With this change, it becomes accessible in
    > > c/b/a/input_method/component_extension_ime_manager_delegate_impl.cc
    > > and the feature availability control can be moved back there
    > > where the layout availability used to be controlled in
    > > crrev.com/c/3692862 .
    > >
    > > With the move, Hindi Inscript Layout can be properly hidden
    > > instead of just being hidden in the AddInputMethod dialog.
    > > The imperfect hiding mechanism implemented in crrev.com/c/3935574
    > > is fixed.
    > >
    > > Low-Coverage-Reason::small change added to a file that has never had a unit_test
    > >
    > > Bug: b/233908778
    > > Change-Id: If3b455ff9209020109a50b4bd5b771a506a2c20d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3962762
    > > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > > Commit-Queue: Jungshik Shin <jshin@chromium.org>
    > > Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    > > Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1062260}
    >
    > Bug: b/233908778
    > Bug: 1377729
    > Change-Id: Ia9b2e863ad598c0d05d13df615f081e369194ed0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3973594
    > Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1062675}
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: b/233908778
    Bug: 1377729
    Change-Id: I0890ab2f84c2798e371b33d806be65c4a5442992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989499
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065520}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -253,7 +253,6 @@ extern const char kLanguageEnabledImesSyncable[];
 extern const char kLanguageImeMenuActivated[];
 extern const char kLanguageInputMethodSpecificSettings[];
 extern const char kLanguageShouldMergeInputMethods[];
-extern const char kHindiInscriptLayoutEnabled[];
 
 extern const char kLabsAdvancedFilesystemEnabled[];
 extern const char kLabsMediaplayerEnabled[];
@@ -1269,6 +1268,7 @@ extern const char kSCTAuditingHashdanceReportCount[];
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 extern const char kConsumerAutoUpdateToggle[];
+extern const char kHindiInscriptLayoutEnabled[];
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
