51038bb838e6a84ccdba917b5e912d81ea046dc2
Sebastien Lalancette <seblalancette@chromium.org>
2022-10-31 18:47:51 +0000

    [DTC] Add top-level handshake UMA logging
    Log the top-level success/failure outcome in Device Trust handshake
    flows between the browser and servers.
    
    Change-Id: I5605530186ef1b80459ee388d13af68a1bac8219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990215
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065591}

--- chrome/browser/enterprise/connectors/device_trust/navigation_throttle_unittest.cc
+++ chrome/browser/enterprise/connectors/device_trust/navigation_throttle_unittest.cc
@@ -57,6 +57,8 @@ constexpr char kLatencyHistogramName[] =
     "Enterprise.DeviceTrust.Attestation.ResponseLatency.%s";
 constexpr char kFunnelHistogramName[] =
     "Enterprise.DeviceTrust.Attestation.Funnel";
+constexpr char kHandshakeResultHistogram[] =
+    "Enterprise.DeviceTrust.Handshake.Result";
 
 scoped_refptr<net::HttpResponseHeaders> GetHeaderChallenge(
     const std::string& challenge) {
@@ -106,7 +108,8 @@ class DeviceTrustNavigationThrottleTest : public testing::Test {
   }
 
   void TestReplyChallengeResponseAndResume(DeviceTrustResponse response,
-                                           std::string expected_json) {
+                                           std::string expected_json,
+                                           DTHandshakeResult expected_result) {
     content::MockNavigationHandle test_handle(GURL("https://www.example.com/"),
                                               main_frame());
     test_handle.set_response_headers(GetHeaderChallenge(kChallenge));
@@ -135,6 +138,8 @@ class DeviceTrustNavigationThrottleTest : public testing::Test {
                                ? "Failure"
                                : "Success"),
         1);
+    histogram_tester_.ExpectUniqueSample(kHandshakeResultHistogram,
+                                         expected_result, 1);
   }
 
  protected:
@@ -206,8 +211,8 @@ TEST_F(DeviceTrustNavigationThrottleTest, TestReplyValidChallengeResponse) {
   DeviceTrustResponse test_response_valid = {kChallengeResponse, absl::nullopt,
                                              absl::nullopt};
   std::string valid_challenge_json = kChallengeResponse;
-  TestReplyChallengeResponseAndResume(test_response_valid,
-                                      valid_challenge_json);
+  TestReplyChallengeResponseAndResume(test_response_valid, valid_challenge_json,
+                                      DTHandshakeResult::kSuccess);
   histogram_tester_.ExpectBucketCount(
       kFunnelHistogramName, DTAttestationFunnelStep::kChallengeResponseSent, 1);
 
@@ -222,8 +227,8 @@ TEST_F(DeviceTrustNavigationThrottleTest,
   DeviceTrustResponse test_response_unknown = {"", absl::nullopt,
                                                absl::nullopt};
   std::string unknown_error_json = "{\"error\":\"unknown\"}";
-  TestReplyChallengeResponseAndResume(test_response_unknown,
-                                      unknown_error_json);
+  TestReplyChallengeResponseAndResume(test_response_unknown, unknown_error_json,
+                                      DTHandshakeResult::kUnknown);
 }
 
 TEST_F(DeviceTrustNavigationThrottleTest,
@@ -233,7 +238,8 @@ TEST_F(DeviceTrustNavigationThrottleTest,
       DTAttestationResult::kMissingSigningKey};
   std::string timeout_json =
       "{\"code\":\"missing_signing_key\",\"error\":\"timeout\"}";
-  TestReplyChallengeResponseAndResume(test_response_timeout, timeout_json);
+  TestReplyChallengeResponseAndResume(test_response_timeout, timeout_json,
+                                      DTHandshakeResult::kTimeout);
 }
 
 TEST_F(DeviceTrustNavigationThrottleTest, TestChallengeNotFromIdp) {
@@ -298,6 +304,9 @@ TEST_F(DeviceTrustNavigationThrottleTest, TestTimeout) {
       base::StringPrintf(kLatencyHistogramName, "Failure"), 1);
   histogram_tester_.ExpectTotalCount(
       base::StringPrintf(kLatencyHistogramName, "Success"), 0);
+
+  histogram_tester_.ExpectUniqueSample(kHandshakeResultHistogram,
+                                       DTHandshakeResult::kTimeout, 1);
 }
 
 }  // namespace enterprise_connectors
