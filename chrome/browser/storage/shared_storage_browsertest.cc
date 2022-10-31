337ef0b4fae7314452596e8202a50380de92946e
Fergal Daly <fergal@chromium.org>
2022-10-31 18:32:17 +0000

    Update uses of WebContentsConsoleObserver::Wait to check return value.
    The return value is newly-added and will be marked [[nodiscard]] as
    soon as all uses are updated.
    
    Previous CLs update under content/ but nothing outside of content/.
    
    BUG=1379192
    
    This CL was uploaded by git cl split.
    
    R=ayui@chromium.org
    
    Change-Id: Ie071a462db4ee8cf1afd36e3e8f7da7b91204fef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990467
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065579}

--- chrome/browser/storage/shared_storage_browsertest.cc
+++ chrome/browser/storage/shared_storage_browsertest.cc
@@ -263,7 +263,7 @@ class SharedStorageChromeBrowserTest : public PlatformBrowserTest {
         content::JsReplace("sharedStorage.worklet.addModule($1)",
                            module_script_url)));
 
-    add_module_console_observer.Wait();
+    ASSERT_TRUE(add_module_console_observer.Wait());
 
     EXPECT_LE(1u,
               content::GetAttachedSharedStorageWorkletHostsCount(
@@ -303,7 +303,7 @@ class SharedStorageChromeBrowserTest : public PlatformBrowserTest {
         content::JsReplace("sharedStorage.worklet.addModule($1)",
                            module_script_url)));
 
-    add_module_console_observer.Wait();
+    EXPECT_TRUE(add_module_console_observer.Wait());
 
     EXPECT_LE(1u,
               content::GetAttachedSharedStorageWorkletHostsCount(
@@ -325,7 +325,7 @@ class SharedStorageChromeBrowserTest : public PlatformBrowserTest {
         sharedStorage.run('test-operation');
       )");
 
-    script_console_observer.Wait();
+    EXPECT_TRUE(script_console_observer.Wait());
     EXPECT_EQ(1u, script_console_observer.messages().size());
 
     EXPECT_EQ(last_script_message,
@@ -349,7 +349,11 @@ class SharedStorageChromeBrowserTest : public PlatformBrowserTest {
       sharedStorage.run('remaining-budget-operation', {data: {}});
     )"));
 
-    budget_console_observer.Wait();
+    bool observed = budget_console_observer.Wait();
+    EXPECT_TRUE(observed);
+    if (!observed) {
+      return nan("");
+    }
 
     EXPECT_EQ(1u, budget_console_observer.messages().size());
     std::string console_message =
@@ -421,7 +425,7 @@ class SharedStoragePrefBrowserTest
       sharedStorage.worklet.addModule('shared_storage/simple_module.js');
     )"));
 
-    add_module_console_observer.Wait();
+    EXPECT_TRUE(add_module_console_observer.Wait());
 
     // Shared Storage is enabled in order to `addModule()`.
     EXPECT_EQ(1u, add_module_console_observer.messages().size());
@@ -465,7 +469,7 @@ class SharedStoragePrefBrowserTest
         content::JsReplace("sharedStorage.worklet.addModule($1)",
                            module_script_url)));
 
-    add_module_console_observer.Wait();
+    EXPECT_TRUE(add_module_console_observer.Wait());
 
     EXPECT_EQ(1u,
               content::GetAttachedSharedStorageWorkletHostsCount(
@@ -490,7 +494,7 @@ class SharedStoragePrefBrowserTest
         sharedStorage.run('test-operation');
       )");
 
-    script_console_observer.Wait();
+    EXPECT_TRUE(script_console_observer.Wait());
     EXPECT_EQ(1u, script_console_observer.messages().size());
 
     if (SuccessExpected()) {
@@ -543,7 +547,7 @@ IN_PROC_BROWSER_TEST_P(SharedStoragePrefBrowserTest, AddModule) {
     return;
   }
 
-  console_observer.Wait();
+  ASSERT_TRUE(console_observer.Wait());
 
   // Privacy Sandbox is enabled and 3P cookies are allowed, so Shared Storage
   // should be allowed.
@@ -596,7 +600,7 @@ IN_PROC_BROWSER_TEST_P(SharedStoragePrefBrowserTest, RunOperation) {
     return;
   }
 
-  run_op_console_observer.Wait();
+  ASSERT_TRUE(run_op_console_observer.Wait());
 
   // Privacy Sandbox is enabled and 3P cookies are allowed, so Shared Storage
   // should be allowed.
@@ -654,7 +658,7 @@ IN_PROC_BROWSER_TEST_P(SharedStoragePrefBrowserTest, RunURLSelectionOperation) {
     return;
   }
 
-  run_url_op_console_observer.Wait();
+  ASSERT_TRUE(run_url_op_console_observer.Wait());
 
   // Privacy Sandbox is enabled and 3P cookies are allowed, so Shared Storage
   // should be allowed.
@@ -2269,7 +2273,7 @@ class SharedStorageFencedFrameChromeBrowserTest
           {data: {'mockResult': 1}});
     )");
 
-    run_url_op_console_observer.Wait();
+    EXPECT_TRUE(run_url_op_console_observer.Wait());
 
     EXPECT_TRUE(run_url_op_result.error.empty());
     EXPECT_TRUE(
