f074cf7e0c31bc861d7cad82dc0277938da1b88c
Daniel d'Andrada <dandrader@google.com>
2022-10-31 10:21:28 +0000

    [Code Health] Remove notification usage in ExtensionFunctionalTest
    Remove usage of NOTIFICATION_CRX_INSTALLER_DONE.
    
    Part of the larger effort of removing the NotificationService.
    
    Bug: 1174728
    Change-Id: I71a9a803d7f4cd3a386f79d9bc4b70137583cc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3985729
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065421}

--- chrome/browser/extensions/extension_functional_browsertest.cc
+++ chrome/browser/extensions/extension_functional_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/test_future.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -26,10 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
-#include "extensions/browser/notification_types.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using base::test::TestFuture;
 
 namespace extensions {
 
@@ -51,11 +52,14 @@ class ExtensionFunctionalTest : public ExtensionBrowserTest {
     installer->set_off_store_install_allow_reason(
         CrxInstaller::OffStoreInstallAllowedInTest);
 
-    observer_->Watch(NOTIFICATION_CRX_INSTALLER_DONE,
-                     content::Source<CrxInstaller>(installer.get()));
-
+    TestFuture<absl::optional<CrxInstallError>> installer_done_future;
+    installer->AddInstallerCallback(
+        installer_done_future
+            .GetCallback<const absl::optional<CrxInstallError>&>());
     installer->InstallCrx(path);
-    observer_->Wait();
+
+    const absl::optional<CrxInstallError>& error = installer_done_future.Get();
+    EXPECT_FALSE(error);
 
     size_t num_after = registry->enabled_extensions().size();
     EXPECT_EQ(num_before + 1, num_after);
