9df151b665dc4bf977af3da637fce19e9f72fa61
Roger Tinkoff <rtinkoff@google.com>
2022-10-31 18:27:19 +0000

    ash: Add VideoConferenceTrayController
    Add VideoConferenceTrayController, owned by the shell. It doesn't do
    anything just yet, but it's needed for critical VC manager
    integration that will un-block bubble UI development.
    
    Bug: b/255402675
    Change-Id: I789f604dfe13fb6b508d5a56175cfb18544fe290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975189
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065570}

---
+++ ash/system/video_conference/video_conference_tray_controller.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/video_conference/video_conference_tray_controller.h"
+
+namespace ash {
+
+VideoConferenceTrayController::VideoConferenceTrayController() = default;
+
+VideoConferenceTrayController::~VideoConferenceTrayController() = default;
+
+}  // namespace ash
