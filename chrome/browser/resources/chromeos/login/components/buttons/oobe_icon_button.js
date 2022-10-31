1790cbd18783542aa7d5fd3af4f9d855a5a1c062
Renato Silva <rrsilva@google.com>
2022-10-31 19:40:45 +0000

    OOBE - Polymer3 - Check-in oobe-next-button
    Check-in the modularised version of oobe-next-button.
    
    Bug: 1184731, b/250891214
    Change-Id: Idf69ba8720bd6991a4ccb6c65b1feeca094d5612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3992909
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065629}

--- chrome/browser/resources/chromeos/login/components/buttons/oobe_icon_button.js
+++ chrome/browser/resources/chromeos/login/components/buttons/oobe_icon_button.js
@@ -3,19 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
-import '//resources/cr_elements/cr_button/cr_button.js';
-import '//resources/cr_elements/cr_icons.css.js';
-import '//resources/cr_elements/cr_shared_style.css.js';
-import '../common_styles/common_styles.m.js';
-import '../hd_iron_icon.js';
-import '../oobe_vars/oobe_custom_vars_css.m.js';
-
-import {html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-
-import {OobeBaseButton} from './oobe_base_button.js';
-
 /**
+ *   @fileoverview
  *   Material design button that shows an icon and displays text.
  *
  *   Example:
@@ -38,6 +27,17 @@ import {OobeBaseButton} from './oobe_base_button.js';
  *                        label-for-aria value.
  *
  */
+
+import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/cr_icons.css.js';
+import '//resources/cr_elements/cr_shared_style.css.js';
+import '../common_styles/common_styles.m.js';
+import '../hd_iron_icon.js';
+import '../oobe_vars/oobe_custom_vars_css.m.js';
+
+import {html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {OobeBaseButton} from './oobe_base_button.js';
 
 /** @polymer */
 export class OobeIconButton extends OobeBaseButton {
