9ebff03aa02103129ffcf948b1bce063cd101bd6
Artsiom Mitrokhin <amitrokhin@google.com>
2022-10-31 15:07:18 +0000

    Prevent from showing dismiss animation on top of opened apps
    Bug: 1344199
    Change-Id: I05a267dbcc881db348a88ab6b67c47827079a29f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988007
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065483}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -859,7 +859,7 @@ void AppListControllerImpl::OnTabletModeStarted() {
       session_manager::SessionState::ACTIVE) {
     ShowHomeScreen();
   }
-  UpdateLauncherContainer();
+  UpdateFullscreenLauncherContainer();
 
   // If the app list is visible before the transition to tablet mode,
   // AppListPresenter relies on the active window change to detect the app list
@@ -889,7 +889,7 @@ void AppListControllerImpl::OnTabletModeEnded() {
                     ->GetShelfLayoutManager()
                     ->HasVisibleWindow());
   fullscreen_presenter_->OnTabletModeChanged(false);
-  UpdateLauncherContainer();
+  UpdateFullscreenLauncherContainer();
 
   // Dismiss the app list if the tablet mode ends.
   DismissAppList();
@@ -1539,7 +1539,7 @@ void AppListControllerImpl::OnAppListPageChanged(AppListState page) {
   if (!fullscreen_presenter_)
     return;
 
-  UpdateLauncherContainer();
+  UpdateFullscreenLauncherContainer();
 
   if (page == AppListState::kStateEmbeddedAssistant) {
     // ShowUi() will be no-op if the Assistant UI is already visible.
@@ -1870,13 +1870,14 @@ void AppListControllerImpl::UpdateForOverviewModeChange(bool show_home_launcher,
                                        animation_settings_updater);
 }
 
-void AppListControllerImpl::UpdateLauncherContainer(
+void AppListControllerImpl::UpdateFullscreenLauncherContainer(
     absl::optional<int64_t> display_id) {
   aura::Window* window = fullscreen_presenter_->GetWindow();
   if (!window)
     return;
 
-  aura::Window* parent_window = GetContainerForDisplayId(display_id);
+  aura::Window* parent_window =
+      GetFullscreenLauncherContainerForDisplayId(display_id);
   if (parent_window && !parent_window->Contains(window)) {
     parent_window->AddChild(window);
     // Release focus if the launcher is moving behind apps, and there is app
@@ -1890,12 +1891,7 @@ void AppListControllerImpl::UpdateLauncherContainer(
   }
 }
 
-int AppListControllerImpl::GetContainerId() const {
-  return ShouldLauncherShowBehindApps() ? kShellWindowId_HomeScreenContainer
-                                        : kShellWindowId_AppListContainer;
-}
-
-aura::Window* AppListControllerImpl::GetContainerForDisplayId(
+aura::Window* AppListControllerImpl::GetFullscreenLauncherContainerForDisplayId(
     absl::optional<int64_t> display_id) {
   aura::Window* root_window = nullptr;
   if (display_id.has_value()) {
@@ -1904,12 +1900,9 @@ aura::Window* AppListControllerImpl::GetContainerForDisplayId(
     root_window = fullscreen_presenter_->GetWindow()->GetRootWindow();
   }
 
-  return root_window ? root_window->GetChildById(GetContainerId()) : nullptr;
-}
-
-bool AppListControllerImpl::ShouldLauncherShowBehindApps() const {
-  return IsTabletMode() &&
-         app_list_page_ != AppListState::kStateEmbeddedAssistant;
+  return root_window
+             ? root_window->GetChildById(GetFullscreenLauncherContainerId())
+             : nullptr;
 }
 
 void AppListControllerImpl::Shutdown() {
@@ -2006,6 +1999,13 @@ void AppListControllerImpl::OnReporterActivated() {
   FeatureDiscoveryDurationReporter::GetInstance()->MaybeActivateObservation(
       feature_discovery::TrackableFeature::
           kAppListReorderAfterSessionActivation);
+}
+
+int AppListControllerImpl::GetFullscreenLauncherContainerId() const {
+  const bool should_show_behind_apps =
+      app_list_page_ != AppListState::kStateEmbeddedAssistant;
+  return should_show_behind_apps ? kShellWindowId_HomeScreenContainer
+                                 : kShellWindowId_AppListContainer;
 }
 
 }  // namespace ash
