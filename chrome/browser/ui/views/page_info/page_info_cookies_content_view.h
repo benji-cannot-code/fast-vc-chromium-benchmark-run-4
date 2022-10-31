232140fbb6485f1c0dcb4eaaa7466cac62e2af05
Dana Fried <dfried@chromium.org>
2022-10-31 20:42:42 +0000

    [Test Automation] Update site-specific tests to Kombucha 1.5
    This vastly cuts down on the size of the tests as well as illustrates
    best practices for the new 1.5 interface.
    
    This set of tests also demonstrates how to create custom step generators
    and callback generators to use across related tests.
    
    Bug: 1374319
    Change-Id: Ibb047eede53dc4568dfad5bb2e2a70f83ae6c7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934067
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065655}

--- chrome/browser/ui/views/page_info/page_info_cookies_content_view.h
+++ chrome/browser/ui/views/page_info/page_info_cookies_content_view.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/toggle_button.h"
 #include "ui/views/view.h"
 
-DECLARE_CUSTOM_ELEMENT_EVENT_TYPE(kPageInfoCookiesSubpageLoaded);
-
 // The view that is used as a content view of the Cookies subpage in page info.
 // It contains information about cookies (short description, how many sites
 // are allowed).
