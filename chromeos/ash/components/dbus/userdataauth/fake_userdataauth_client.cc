e13983e6e52437041ad53a43e0f47e2bae3b7277
Martin Bidlingmaier <mbid@google.com>
2022-10-31 13:09:25 +0000

    Add integration test for PIN settings
    This test required a more complicated fixture than just an
    OSSettingsMixin. The additional fixture code must be executed before we
    we open the settings page. To accomplish this, this commit moves the
    call that opens the settings page from the SetUpOnMainThread function of
    OSSettingsBrowserTestMixin to the actual browser test fixture.
    
    Bug: b/243696986
    Bug: b/255734669
    Fixed: b/241259026
    Change-Id: Ib2a774b502386b6e69c110ff7edca4dd0fdd1f36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981293
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065446}

--- chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.cc
+++ chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.cc
@@ -497,6 +497,12 @@ bool FakeUserDataAuthClient::TestApi::HasRecoveryFactor(
   return ContainsFakeFactor<RecoveryFactor>(user_state.auth_factors);
 }
 
+bool FakeUserDataAuthClient::TestApi::HasPinFactor(
+    const cryptohome::AccountIdentifier& account_id) {
+  const UserCryptohomeState& user_state = GetUserState(account_id);
+  return ContainsFakeFactor<PinFactor>(user_state.auth_factors);
+}
+
 std::string FakeUserDataAuthClient::TestApi::AddSession(
     const cryptohome::AccountIdentifier& account_id,
     bool authenticated) {
@@ -1121,7 +1127,9 @@ void FakeUserDataAuthClient::ListAuthFactors(
     reply.add_supported_auth_factors(user_data_auth::AUTH_FACTOR_TYPE_KIOSK);
   } else {
     reply.add_supported_auth_factors(user_data_auth::AUTH_FACTOR_TYPE_PASSWORD);
-    reply.add_supported_auth_factors(user_data_auth::AUTH_FACTOR_TYPE_PIN);
+    if (supports_low_entropy_credentials_) {
+      reply.add_supported_auth_factors(user_data_auth::AUTH_FACTOR_TYPE_PIN);
+    }
     reply.add_supported_auth_factors(
         user_data_auth::AUTH_FACTOR_TYPE_CRYPTOHOME_RECOVERY);
   }
