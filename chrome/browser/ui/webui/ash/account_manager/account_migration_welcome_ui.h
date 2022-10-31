c38a0629cc9263b21c6b077c3eef40368d01a188
Clement Yan <clamclamyan@google.com>
2022-10-31 04:22:23 +0000

    [WebUI] Migrate account migration webui to WebUIConfig
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: Ic1759a8902a95cb54d6047405978f6946012af34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989804
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065366}

--- chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.h
+++ chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.h
@@ -7,10 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_ASH_ACCOUNT_MANAGER_ACCOUNT_MIGRATION_WELCOME_UI_H_
 
 #include "base/memory/weak_ptr.h"
+#include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "content/public/browser/webui_config.h"
+#include "content/public/common/url_constants.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 
 namespace ash {
+
+class AccountMigrationWelcomeUI;
+
+// WebUIConfig for chrome:://account-migration-welcome
+class AccountMigrationWelcomeUIConfig
+    : public content::DefaultWebUIConfig<AccountMigrationWelcomeUI> {
+ public:
+  AccountMigrationWelcomeUIConfig()
+      : DefaultWebUIConfig(content::kChromeUIScheme,
+                           chrome::kChromeUIAccountMigrationWelcomeHost) {}
+};
 
 // For chrome:://account-migration-welcome
 class AccountMigrationWelcomeUI : public ui::WebDialogUI {
