5f7e9a50fd6954ec5d9adc4c04f2e477bed7506b
Anastasia Helfinstein <anastasi@google.com>
2022-11-01 20:01:18 +0000

    [ChromeVox] Add types ChromeVoxRole and AbstractRole
    This change is a pure refactor.
    
    R=francisjp
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: I30727aa34bf7d5e4d970b77583791104b6343718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983495
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066091}

---
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/role_type.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Augments chrome.automation.RoleType with abstract types for
+ * ChromeVox.
+ */
+
+/** @enum {string} */
+export const AbstractRole = {
+  CONTAINER: 'abstractContainer',
+  FORM_FIELD_CONTAINER: 'abstractFormFieldContainer',
+  ITEM: 'abstractItem',
+  LIST: 'abstractList',
+  NAME_FROM_CONTENTS: 'abstractNameFromContents',
+  RANGE: 'abstractRange',
+  SPAN: 'abstractSpan',
+};
+
+/** @typedef {!chrome.automation.RoleType|!AbstractRole} */
+export let ChromeVoxRole;
