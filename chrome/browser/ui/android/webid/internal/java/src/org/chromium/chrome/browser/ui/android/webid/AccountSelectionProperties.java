6a7569cf37c7ad5ad4cbfdbecd673d993ad366dd
Peter Kotwicz <pkotwicz@chromium.org>
2022-11-01 16:52:28 +0000

    Record when the FedCM terms-of-service link is clicked.
    This CL adds 2 histograms:
    - Blink.FedCm.SignUp.PrivacyPolicyClicked
    Records when privacy policy link is clicked
    - Blink.FedCm.SignUp.TermsOfServiceClicked
    Records when terms of service link is clicked
    
    BUG=1343552
    
    Change-Id: Ieaa0894bfaf03724ebd7c1c796cebe065ade3f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3994463
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065977}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionProperties.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionProperties.java
@@ -85,6 +85,8 @@ class AccountSelectionProperties {
             public String mIdpForDisplay;
             public GURL mTermsOfServiceUrl;
             public GURL mPrivacyPolicyUrl;
+            public Runnable mTermsOfServiceClickRunnable;
+            public Runnable mPrivacyPolicyClickRunnable;
         }
 
         static final ReadableObjectPropertyKey<Properties> PROPERTIES =
