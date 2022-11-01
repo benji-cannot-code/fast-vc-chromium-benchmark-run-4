4a1183d56f3210b4f12d079ca0c8c16e27f59c2a
Nan Lin <linnan@chromium.org>
2022-11-01 21:25:44 +0000

    Send debug reports when noise is applied to an attribution source
    https://github.com/WICG/attribution-reporting-api/pull/589
    
    This is only sent when attribution debug cookie is set.
    
    Bug: 1379078
    Change-Id: I16b718bf4fe39e3068e85e7816eca214c26f64c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989006
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066152}

--- content/browser/attribution_reporting/attribution_test_utils.cc
+++ content/browser/attribution_reporting/attribution_test_utils.cc
@@ -1301,6 +1301,8 @@ std::ostream& operator<<(std::ostream& out, StorableSource::Result status) {
       return out << "excessiveReportingOrigins";
     case StorableSource::Result::kProhibitedByBrowserPolicy:
       return out << "prohibitedByBrowserPolicy";
+    case StorableSource::Result::kSuccessNoised:
+      return out << "successNoised";
   }
 }
 
