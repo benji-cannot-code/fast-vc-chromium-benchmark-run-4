5b8386463f5150a2effe77f949c5b2d77aff0fa5
Hirokazu Honda <hiroh@chromium.org>
2022-11-01 18:02:16 +0000

    media: Rename VaapiAV1Decoder feature to ChromeOSHWAV1Decoder
    V4L2VideoDecoder will support AV1 hardware video decoding soon. This
    renames the VaapiAV1Decoder feature name to ChromeOSHWAV1Decoder so
    we can use the feature for both ChromeOS video decoder implementation.
    This also removes the flag for the feature. We would rather control
    the feature with --enable|disable-features these days.
    
    Bug: b:255626192
    Test: AV1 hardware video decoder works on brya
    Change-Id: Ibaab39a10dd9a2d037453f1ca394a8b7391597f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995724
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066015}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3603,9 +3603,6 @@ extern const char kGetDisplayMediaSetDescription[];
 extern const char kGetDisplayMediaSetAutoSelectAllScreensName[];
 extern const char kGetDisplayMediaSetAutoSelectAllScreensDescription[];
 
-extern const char kVaapiAV1DecoderName[];
-extern const char kVaapiAV1DecoderDescription[];
-
 extern const char kIntentChipSkipsPickerName[];
 extern const char kIntentChipSkipsPickerDescription[];
 
