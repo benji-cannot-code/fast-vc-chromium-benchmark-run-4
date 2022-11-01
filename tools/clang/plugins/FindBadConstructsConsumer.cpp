a8fad2e9c709b3f1c192d07971149c5ff6e3dada
Keishi Hattori <keishi@chromium.org>
2022-11-01 23:52:31 +0000

    Reland: Add check-bad-raw-ptr-cast to the clang plugin
    change in reland: fix build error
    
    Original description:
    check-bad-raw-ptr-cast checks for raw_ptr<T>* being cast to another type.
    
    This prevents issues like this: https://chromium-review.googlesource.com/c/chromium/src/+/2587397/9/base/memory/checked_ptr.md#230
    
    As discussed in https://chat.google.com/room/AAAAdj-LwK0/QOr9OiIVv5M
    
    Change-Id: I2da17fc4dbcb080f334871dc85be6d581e867ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961908
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066236}

--- tools/clang/plugins/FindBadConstructsConsumer.cpp
+++ tools/clang/plugins/FindBadConstructsConsumer.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FindBadConstructsConsumer.h"
 
+#include "FindBadRawPtrPatterns.h"
 #include "Util.h"
 #include "clang/AST/Attr.h"
 #include "clang/Frontend/CompilerInstance.h"
@@ -231,6 +232,7 @@ void FindBadConstructsConsumer::Traverse(ASTContext& context) {
   RecursiveASTVisitor::TraverseDecl(context.getTranslationUnitDecl());
   if (ipc_visitor_)
     ipc_visitor_->set_context(nullptr);
+  FindBadRawPtrPatterns(options_, context, instance());
 }
 
 bool FindBadConstructsConsumer::TraverseDecl(Decl* decl) {
---
+++ tools/clang/plugins/tests/bad_raw_ptr_cast.cpp
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "bad_raw_ptr_cast.h"
+
+#include <cstring>
+
+void BadRawPtr::Check() {
+  // Explicit cast from raw_ptr<T>* is not allowed.
+  void* bar = reinterpret_cast<void*>(&foo_);
+  bar = static_cast<void*>(&foo_);
+  bar = (void*)&foo_;
+  // Implicit cast from raw_ptr<T>* is not allowed.
+  bar = &foo_;
+  memcpy(&foo_, &foo_, 1);
+}
