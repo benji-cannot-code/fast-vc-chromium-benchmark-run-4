4c4f063f9e401258d9cae474e5d01f2021ec1afd
Ahmed Mehfooz <amehfooz@chromium.org>
2022-11-01 00:15:04 +0000

    Add support for notification icons and counter in notification tray
    This CL retrofits the NotificationIconsController to be usable
    with the new notification_center_tray. The unified_system_tray does
    not instantiate NotificationIconsController anymore if QsRevamp
    is enabled.
    
    Also parameterized unit tests for NotificationCounterView and
    NotificationIconsController to test inside the notification_center_tray.
    
    Screenshot after change: https://screenshot.googleplex.com/9n9Nq5Sxu322x9j.png
    
    Bug: b:252829807
    
    Change-Id: I73eb3d81193acb23b8b35b565d74c6f2e6363c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960541
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065732}

--- ash/system/notification_center/notification_center_tray.cc
+++ ash/system/notification_center/notification_center_tray.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/constants/tray_background_view_catalog.h"
+#include "ash/public/cpp/shelf_config.h"
 #include "ash/shelf/shelf.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "ash/system/tray/tray_bubble_view.h"
+#include "ash/system/tray/tray_container.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/message_center/message_center.h"
@@ -22,11 +24,24 @@ namespace ash {
 NotificationCenterTray::NotificationCenterTray(Shelf* shelf)
     : TrayBackgroundView(shelf,
                          TrayBackgroundViewCatalogName::kNotificationCenter,
-                         RoundedCornerBehavior::kStartRounded) {
+                         RoundedCornerBehavior::kStartRounded),
+      notification_icons_controller_(
+          std::make_unique<NotificationIconsController>(shelf)) {
   SetLayoutManager(std::make_unique<views::FlexLayout>());
   set_use_bounce_in_animation(false);
 
   message_center::MessageCenter::Get()->AddObserver(this);
+
+  tray_container()->SetMargin(
+      /*main_axis_margin=*/kUnifiedTrayContentPadding -
+          ShelfConfig::Get()->status_area_hit_region_padding(),
+      0);
+
+  // TODO(b/255986529): Rewrite the `NotificationIconsController` class so that
+  // we do not have to add icon views that are owned by the
+  // `NotificationCenterTray` from the controller. We should make sure views are
+  // only added by host views.
+  notification_icons_controller_->AddNotificationTrayItems(tray_container());
 }
 
 NotificationCenterTray::~NotificationCenterTray() {
@@ -98,6 +113,9 @@ void NotificationCenterTray::UpdateVisibility() {
     return;
 
   SetVisiblePreferred(new_visibility);
+
+  notification_icons_controller_->UpdateNotificationIcons();
+  notification_icons_controller_->UpdateNotificationIndicators();
 }
 
 BEGIN_METADATA(NotificationCenterTray, TrayBackgroundView)
