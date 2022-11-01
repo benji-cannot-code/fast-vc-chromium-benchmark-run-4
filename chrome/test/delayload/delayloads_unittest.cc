8a9af7e24fb950b835d382af96807748c7193c71
Asami Doi <asamidoi@chromium.org>
2022-11-01 00:03:04 +0000

    Sheriff: disable DelayloadsTest.ChromeDllLoadSanityTest on Windows
    Bug: 1047723
    Change-Id: I076c350b295a3cb7feb79342f7e54cb55f0f7557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3994526
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065722}

--- chrome/test/delayload/delayloads_unittest.cc
+++ chrome/test/delayload/delayloads_unittest.cc
@@ -129,8 +129,8 @@ TEST_F(DelayloadsTest, ChromeDllDelayloadsCheck) {
   }
 }
 
-// Flaking on ASAN: https://crbug.com/1047723
-#if defined(ADDRESS_SANITIZER)
+// https://crbug.com/1047723
+#if defined(ADDRESS_SANITIZER) || BUILDFLAG(IS_WIN)
 #define MAYBE_ChromeDllLoadSanityTest DISABLED_ChromeDllLoadSanityTest
 #else
 #define MAYBE_ChromeDllLoadSanityTest ChromeDllLoadSanityTest
