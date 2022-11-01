c3e47dc42efaf0f02bed659bc0bc7563893b3f23
ginnnnnnny <ginnyhuang@chromium.org>
2022-11-01 21:52:49 +0000

    [iOS][finch-first-run] Expiry mechanism for variations app state agent
    This CL adds the check whether there is a current seed available in the
    variations seed store and whether it has expired. The result  will (in
    later CL) be recorded by metrics. This mechanism is also used to
    determine whether the app is in its first run.
    
    Since the variations service and seed store objects would not be
    available when this check executes, the seed fetch time is retrieved
    from NSUserDefaults, instead of from the seed store. Every time the app
    backgrounds, however, the app would retrieve the fetch time of the last
    seed from variations seed store and puts it into NSUserDefault.
    
    The above logic would be executed every time the app runs and shuts down
    instead of only on the first run, as we previously designed it to be.
    This is because the existing first-run-checker is unavailable when this
    code is executed, and we have to resort to adding a new field in
    NSUserDefaults anyways, so why not make it more useful?
    
    For this purpose, new APIs are added to variations_seed_store.h. Since
    this function imports `components/variations/", the DEP file is updated
    to include it.
    
    Bug: 1372180
    Change-Id: Ice468664c17720cbe67ba40725f946b784b8b6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968540
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066173}

--- components/variations/variations_seed_store.cc
+++ components/variations/variations_seed_store.cc
@@ -298,7 +298,7 @@ bool VariationsSeedStore::StoreSafeSeed(
 }
 
 base::Time VariationsSeedStore::GetLastFetchTime() const {
-  return local_state_->GetTime(prefs::kVariationsLastFetchTime);
+  return GetLastFetchTimeFromPrefService(local_state_);
 }
 
 base::Time VariationsSeedStore::GetSafeSeedFetchTime() const {
@@ -374,6 +374,12 @@ void VariationsSeedStore::RegisterPrefs(PrefRegistrySimple* registry) {
       prefs::kVariationsSafeSeedSessionConsistencyCountry, std::string());
   registry->RegisterStringPref(prefs::kVariationsSafeSeedSignature,
                                std::string());
+}
+
+// static
+base::Time VariationsSeedStore::GetLastFetchTimeFromPrefService(
+    PrefService* prefs) {
+  return prefs->GetTime(prefs::kVariationsLastFetchTime);
 }
 
 // static
