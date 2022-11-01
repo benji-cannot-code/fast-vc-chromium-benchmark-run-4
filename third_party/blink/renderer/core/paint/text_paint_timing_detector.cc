7a60caf0dc06435f06c130833561aec37e1a6b80
Hao Liu <haoliuk@chromium.org>
2022-11-01 15:44:37 +0000

    Make web font styled text re-eligible for lcp.
    This CL makes the following happen.
    1, If a web font gets applied in swap period and if the style text gets
    larger than the existing LCP element size, a new LCP entry is emitted.
    2, If a web fonts get applied in block period, no LCP entry is emitted.
    3, If a web fonts get applied in swap period, but after user input, no
    LCP entry is emitted.
    
    Bug: 1310995
    Change-Id: I1313eda1752fa0b5be48184aa28b7f6016f65307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972847
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065952}

--- third_party/blink/renderer/core/paint/text_paint_timing_detector.cc
+++ third_party/blink/renderer/core/paint/text_paint_timing_detector.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/paint/largest_contentful_paint_calculator.h"
@@ -101,6 +102,7 @@ void TextPaintTimingDetector::OnPaintFinished() {
 void TextPaintTimingDetector::LayoutObjectWillBeDestroyed(
     const LayoutObject& object) {
   recorded_set_.erase(&object);
+  rewalkable_set_.erase(&object);
   texts_queued_for_paint_time_.erase(&object);
 }
 
@@ -145,6 +147,9 @@ bool TextPaintTimingDetector::ShouldWalkObject(
     return true;
   }
 
+  if (rewalkable_set_.Contains(&object))
+    return true;
+
   // This metric defines the size of a text block by its first size, so we
   // should not walk the object if it has been recorded.
   return !recorded_set_.Contains(&object);
@@ -174,6 +179,15 @@ void TextPaintTimingDetector::RecordAggregatedText(
                                                   mapped_visual_rect);
     }
     return;
+  }
+
+  // Web font styled node should be rewalkable so that resizing during swap
+  // would make the node eligible to be LCP candidate again.
+  if (RuntimeEnabledFeatures::WebFontResizeLCPEnabled()) {
+    if (aggregator.GetNode()->GetComputedStyle() &&
+        aggregator.GetNode()->GetComputedStyle()->GetFont().HasCustomFont()) {
+      rewalkable_set_.insert(&aggregator);
+    }
   }
 
   recorded_set_.insert(&aggregator);
@@ -215,6 +229,8 @@ void TextPaintTimingDetector::ReportLargestIgnoredText() {
 void TextPaintTimingDetector::Trace(Visitor* visitor) const {
   visitor->Trace(callback_manager_);
   visitor->Trace(frame_view_);
+  visitor->Trace(text_element_timing_);
+  visitor->Trace(rewalkable_set_);
   visitor->Trace(recorded_set_);
   visitor->Trace(text_element_timing_);
   visitor->Trace(texts_queued_for_paint_time_);
