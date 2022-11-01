3635ec421c0d3bc1ed1b89b407ad45b52f477d5b
Hongchan Choi <hongchan@chromium.org>
2022-11-01 17:24:45 +0000

    Introduce AudioSinkInfo IDL to WebAudio
    This CL introduces AudioSinkInfo IDL, which is needed for the
    implementation of output device selection in Web Audio API.
    
    This change is branched from: https://crrev.com/c/3943082
    
    Bug: 1216187
    Change-Id: Icdd560f4860d2cfc93bf22c27df83e05303ea4c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993271
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065999}

---
+++ third_party/blink/renderer/modules/webaudio/audio_sink_info.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webaudio/audio_sink_info.h"
+
+namespace blink {
+
+AudioSinkInfo* AudioSinkInfo::Create(const String& type) {
+  return MakeGarbageCollected<AudioSinkInfo>(type);
+}
+
+AudioSinkInfo::AudioSinkInfo(const String& type) {}
+
+AudioSinkInfo::~AudioSinkInfo() = default;
+
+String AudioSinkInfo::type() const {
+  // Currently "none" is the only `type` available.
+  return "none";
+}
+
+}  // namespace blink
