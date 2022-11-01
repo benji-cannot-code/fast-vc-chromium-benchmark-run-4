d651c48a0f5775e86f3a23eb4779805e5b7c39bb
Emircan Uysaler <emircan@chromium.org>
2022-11-01 20:27:08 +0000

    [flatland] Recreate Flatland images on resolution change
    This CL changes the overlay behavior such that the Image resources are
    recreated when the pixmap notifies of a size change.
    
    Bug: 1146006
    Change-Id: I0b1b0c560bcadb9809a476f866359420ca6c05dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991274
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066113}

--- ui/ozone/platform/flatland/flatland_surface.h
+++ ui/ozone/platform/flatland/flatland_surface.h
@@ -102,6 +102,7 @@ class FlatlandSurface : public ui::PlatformWindowSurface {
   struct FlatlandIds {
     fuchsia::ui::composition::ContentId image_id;
     fuchsia::ui::composition::TransformId transform_id;
+    gfx::Size image_size;
   };
 
   void OnGetLayout(fuchsia::ui::composition::LayoutInfo info);
