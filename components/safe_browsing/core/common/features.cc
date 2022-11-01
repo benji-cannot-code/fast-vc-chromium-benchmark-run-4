9c90f2d0930d62d6f6822a6fd58c5b8b3b388a16
Xinghui Lu <xinghuilu@chromium.org>
2022-11-01 01:44:27 +0000

    [TailoredWarning] Send CSBRR when discard on download bubble/shelf
    Previously CSBRR report is only sent when the user clicks keep. To
    better measure the impact of the download warnings, send reports
    when the user clicks discard as well. The did_proceed field is set
    to false when discard is clicked.
    
    This is controlled by a feature flag
    kSafeBrowsingCsbrrNewDownloadTrigger that is disabled by default.
    
    Bug: 1363368
    Change-Id: I124c8800587ea1a2099a79d54af2950c314234ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3957259
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065779}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -153,6 +153,10 @@ BASE_FEATURE(kRealTimeUrlLookupForEnterpriseAllowlistBypass,
              "SafeBrowsingRealTimeUrlLookupForEnterpriseAllowlistBypass",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kSafeBrowsingCsbrrNewDownloadTrigger,
+             "SafeBrowsingCsbrrNewDownloadTrigger",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kSafeBrowsingCsbrrWithToken,
              "SafeBrowsingCsbrrWithToken",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -243,6 +247,7 @@ constexpr struct {
       {&kNestedArchives, true},
       {&kOmitNonUserGesturesFromReferrerChain, true},
       {&kRealTimeUrlLookupForEnterpriseAllowlistBypass, true},
+      {&kSafeBrowsingCsbrrNewDownloadTrigger, true},
       {&kSafeBrowsingCsbrrWithToken, true},
       {&kSafeBrowsingDisableConsumerCsdForEnterprise, true},
       {&kSafeBrowsingEnterpriseCsd, true},
