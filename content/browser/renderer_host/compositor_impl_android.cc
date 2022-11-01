a730d881c999854e20b8e5c5feaf443da6df166b
Jonathan Ross <jonross@chromium.org>
2022-11-01 20:32:53 +0000

    CompositorImplAndroid Don't Post Feedback Request with no LayerTreeHost
    There is a surprising crash in
    CompositorImpl::RequestPresentationTimeForNextFrame being called before 
    the LayerTreeHost is created.
    
    It appears to be when CompositorImpl::SetRootWindow is called when
    reusing an existing DelegatedFrameHostAndroid, which has an enqueued
    feedback request.
    
    We speculate that we can create the LayerTreeHost in the ctor, however
    that is too risky to merge back into M107.
    
    This change adds an early exit in
    CompositorImpl::RequestPresentationTimeForNextFrame when there is no
    LayerTreeHost.
    
    It also updates DelegatedFrameHostAndroid::DetachFromCompositor to clear 
    out any enqueued feedback requests when detaching from an active 
    compositor. Having tab change metrics delayed by an unknown amount of
    time while detached does not seem useful/actionable.
    
    Bug: 1379257
    Change-Id: I8c559c8a3d822f7607a78e3b9622ab3382962fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995610
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066120}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -1014,6 +1014,8 @@ void CompositorImpl::PreserveChildSurfaceControls() {
 
 void CompositorImpl::RequestPresentationTimeForNextFrame(
     PresentationTimeCallback callback) {
+  if (!host_)
+    return;
   host_->RequestPresentationTimeForNextFrame(std::move(callback));
 }
 
