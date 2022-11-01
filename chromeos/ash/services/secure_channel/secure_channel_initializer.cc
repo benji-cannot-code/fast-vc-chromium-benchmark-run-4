4d5852bb7b90aca96c560b8ac0a40d2895f56085
Katherine Lai <laikatherine@google.com>
2022-11-01 00:19:38 +0000

    [floss] Update secure channel to support Floss
    Allow secure channel to run with Floss adapter.
    
    BUG=b:235276515
    TEST=Manually verify scanning starts on Floss
    
    Change-Id: I45d5631ff9df79e3630b03fa7a30adca758b8fe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3976848
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065736}

--- chromeos/ash/services/secure_channel/secure_channel_initializer.cc
+++ chromeos/ash/services/secure_channel/secure_channel_initializer.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/secure_channel/secure_channel_impl.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/dbus/bluez_dbus_manager.h"
+#include "device/bluetooth/floss/floss_dbus_manager.h"
+#include "device/bluetooth/floss/floss_features.h"
 
 namespace ash::secure_channel {
 
@@ -56,8 +58,15 @@ SecureChannelInitializer::ConnectionRequestArgs::~ConnectionRequestArgs() =
 
 SecureChannelInitializer::SecureChannelInitializer(
     scoped_refptr<base::TaskRunner> task_runner) {
+  bool is_initialized = false;
+  if (floss::features::IsFlossEnabled()) {
+    is_initialized = floss::FlossDBusManager::IsInitialized();
+  } else {
+    is_initialized = bluez::BluezDBusManager::IsInitialized();
+  }
+
   // May not be initialized in tests.
-  if (!bluez::BluezDBusManager::IsInitialized())
+  if (!is_initialized)
     return;
 
   PA_LOG(VERBOSE) << "SecureChannelInitializer::SecureChannelInitializer(): "
