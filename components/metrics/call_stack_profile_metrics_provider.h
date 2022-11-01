c2e8c5ef7e955b1a7f14a92aa8a45b20c5964e70
Ian Barkley-Yeung <iby@chromium.org>
2022-11-01 02:08:41 +0000

    Add dbus hook to get status of stack profiler
    Add a dbus hook which will allow ChromeOS integration tast tests to
    confirm that the stack profiler is working as intended and not crashing.
    
    See
    https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3969653
    for the ChromeOS-side of this change.
    
    BUG=b:214117401,chromium:1324337
    
    Change-Id: Ic5d1a4c50872c2373ee61fe7d607c0269b926496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3980195
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065790}

--- components/metrics/call_stack_profile_metrics_provider.h
+++ components/metrics/call_stack_profile_metrics_provider.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_CALL_STACK_PROFILE_METRICS_PROVIDER_H_
 #define COMPONENTS_METRICS_CALL_STACK_PROFILE_METRICS_PROVIDER_H_
 
+#include <map>
 #include <string>
 
 #include "base/callback.h"
 #include "base/feature_list.h"
 #include "base/time/time.h"
+#include "build/buildflag.h"
 #include "components/metrics/metrics_provider.h"
+#include "third_party/metrics_proto/execution_context.pb.h"
 #include "third_party/metrics_proto/sampled_profile.pb.h"
 
 namespace metrics {
@@ -28,6 +31,13 @@ class CallStackProfileMetricsProvider : public MetricsProvider {
   // purposes.
   using InterceptorCallback =
       base::RepeatingCallback<void(SampledProfile profile)>;
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // Count of profiles, brokens down by the Process and Thread type of the
+  // profile.
+  using ProcessThreadCount =
+      std::map<::metrics::Process, std::map<::metrics::Thread, int>>;
+#endif
 
   CallStackProfileMetricsProvider();
 
@@ -57,6 +67,15 @@ class CallStackProfileMetricsProvider : public MetricsProvider {
   // starting any profiling since the callback is accessed asynchronously on the
   // profiling thread.
   static void SetCpuInterceptorCallbackForTesting(InterceptorCallback callback);
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // Gets the counts of all successfully collected profiles, broken down by
+  // process type and thread type. "Successfully collected" is defined pretty
+  // minimally (we got a couple of frames). Expensive function; intended only
+  // to be run during ChromeOS tast integration testing, not to be run on end-
+  // user machines.
+  static ProcessThreadCount GetSuccessfullyCollectedCounts();
+#endif
 
   // MetricsProvider:
   void OnRecordingEnabled() override;
