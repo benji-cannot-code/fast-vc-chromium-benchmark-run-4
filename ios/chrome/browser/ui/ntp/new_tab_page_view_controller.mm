d3ffdfe8e9447d7a53d0b64af022b1e3dd2e100f
adamta <adamta@google.com>
2022-11-01 17:31:49 +0000

    [iOS] Don't scroll to top when NTP appears
    Fixes issue where switching from incognito to regular NTP would love the saved scroll position.
    
    Fixed: 1373861
    Change-Id: I5dfc333be26e2f27e340dc27f1355f31e82032ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995348
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066000}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -198,10 +198,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // changed in another tab. This ensures that the sticky elements are correct
   // whenever an NTP reappears.
   [self handleStickyElementsForScrollPosition:[self scrollPosition] force:YES];
-  
-  if (![self isInitialOffsetFromSavedState]) {
-    [self setContentOffsetToTop];
-  }
 
   self.viewDidAppear = YES;
 }
