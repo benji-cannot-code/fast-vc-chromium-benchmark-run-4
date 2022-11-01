d71f2bff78ea24a1e068a9699f30f5021eaa166b
Gang Wu <gangwu@chromium.org>
2022-11-01 23:40:21 +0000

    [RSearches] Remove the flag RelatedSearchesSimplifiedUx
    Related Searches is launched, so there are 5 flags can be cleaned up.
    this CL cleans up the flag RelatedSearchesSimplifiedUx.
    
    Bug:1380445
    
    Change-Id: I5b1f1246bfbc59e453d85424f0d15b919e059c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995426
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066234}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -279,7 +279,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kRelatedSearches,
     &kRelatedSearchesAlternateUx,
     &kRelatedSearchesInBar,
-    &kRelatedSearchesSimplifiedUx,
     &kRelatedSearchesUi,
     &kRequestDesktopSiteDefaults,
     &kRequestDesktopSiteDefaultsControl,
@@ -907,10 +906,6 @@ BASE_FEATURE(kRelatedSearchesAlternateUx,
 BASE_FEATURE(kRelatedSearchesInBar,
              "RelatedSearchesInBar",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kRelatedSearchesSimplifiedUx,
-             "RelatedSearchesSimplifiedUx",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRelatedSearchesUi,
              "RelatedSearchesUi",
