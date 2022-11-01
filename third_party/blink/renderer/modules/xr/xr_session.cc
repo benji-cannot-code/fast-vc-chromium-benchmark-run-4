f4087ed874fa463f223dc11510e5b04d451abe1a
Xianzhu Wang <wangxianzhu@chromium.org>
2022-11-01 03:51:39 +0000

    gfx::Transform::ToString() and ToDecomposedString()
    This prepares or combination of blink::TransformationMatrix and gfx::Transform by making the ToString() API the same.
    
    - Change format of gfx::Transform::ToString() slightly:
      "%+0.4f" to "%lg" to better handle huge and tiny values. This makes
      the output less aligned, but contain more useful information.
    
    - Add gfx::Transform::ToDecomposedTransform() which is basically
      equavalent to blink::TransformationMatrix::ToString(false).
      It uses gfx::DecomposedTransform::ToString() which is also modified
      to use "%lg" instead of "%0.4f".
    
    Bug: 1359528
    Change-Id: I31f36002e851261784c5d822822c404656738314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3992490
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Owners-Override: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065809}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -807,7 +807,7 @@ ScriptPromise XRSession::requestHitTestSource(
   auto entity_types = GetEntityTypesForHitTest(options_init);
 
   DVLOG(3) << __func__
-           << ": native_from_offset = " << native_from_offset.ToString(true);
+           << ": native_from_offset = " << native_from_offset.ToString();
 
   // Transformation from passed in pose to |space|.
 
@@ -817,11 +817,8 @@ ScriptPromise XRSession::requestHitTestSource(
   auto space_from_ray = offsetRay->RawMatrix();
   auto origin_from_ray = native_from_offset * space_from_ray;
 
-  DVLOG(3) << __func__
-           << ": space_from_ray = " << space_from_ray.ToString(true);
-
-  DVLOG(3) << __func__
-           << ": origin_from_ray = " << origin_from_ray.ToString(true);
+  DVLOG(3) << __func__ << ": space_from_ray = " << space_from_ray.ToString();
+  DVLOG(3) << __func__ << ": origin_from_ray = " << origin_from_ray.ToString();
 
   device::mojom::blink::XRRayPtr ray_mojo = device::mojom::blink::XRRay::New();
 
