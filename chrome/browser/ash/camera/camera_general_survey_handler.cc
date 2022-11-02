51b89ce22ffe3e9622746f7a376958dfd5e4df82
Yudhistira Erlandinata <yerlandinata@google.com>
2022-11-02 05:39:23 +0000

    Update general camera HaTS config
    Based on recent discussions in go/cros-camera:dd:general-hats,
    we want to show camera survey to users who open the camera for a
    short time. Therefore, kMinCameraOpenDurationForSurvey value is
    updated to 15 seconds.
    
    BUG=b:247933523, chromium:1367198, http://launch/4210909
    TEST=unit_tests --gtest_filter="*CameraGeneralSurveyHandler*"
    TEST=manual test: go/cros-camera:test-cases:general-hats
    
    Change-Id: If50e461fcf98e39fcd97bc24882ab872a46e7150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995291
    Quick-Run: Yudhistira Erlandinata <yerlandinata@chromium.org>
    Commit-Queue: Yudhistira Erlandinata <yerlandinata@chromium.org>
    Auto-Submit: Yudhistira Erlandinata <yerlandinata@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066342}

--- chrome/browser/ash/camera/camera_general_survey_handler.cc
+++ chrome/browser/ash/camera/camera_general_survey_handler.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-constexpr base::TimeDelta kMinCameraOpenDurationForSurvey = base::Minutes(3);
+constexpr base::TimeDelta kMinCameraOpenDurationForSurvey = base::Seconds(15);
 constexpr base::TimeDelta kCameraSurveyTriggerDelay = base::Seconds(5);
 
 class CameraGeneralSurveyHandlerDelegate
