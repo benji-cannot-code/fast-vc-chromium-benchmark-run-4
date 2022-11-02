1b41fe904b7f6893cf5cafef56363c47ec4b2913
David Yeung <dayeung@chromium.org>
2022-11-02 16:12:57 +0000

    views: NativeWidget will hold WeakPtr to Widget instead of raw_ptr
    Widget/NativeWidget ownership will be changed so they only have weak pointers to each other.
    
    This CL will not change any existing ownership models but will change the raw_ptr to a WeakPtr.
    
    NativeWidget -> WeakPtr -> Widget
    
    Bug: 1377397
    Change-Id: I828b2c571bb63b20113e3cd68758d32d95385d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972159
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066512}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -844,7 +844,9 @@ bool DesktopWindowTreeHostWin::WidgetSizeIsClientSize() const {
 }
 
 bool DesktopWindowTreeHostWin::IsModal() const {
-  return native_widget_delegate_->IsModal();
+  if (native_widget_delegate_)
+    return native_widget_delegate_->IsModal();
+  return false;
 }
 
 int DesktopWindowTreeHostWin::GetInitialShowState() const {
@@ -962,7 +964,8 @@ void DesktopWindowTreeHostWin::HandleCreate() {
 
 void DesktopWindowTreeHostWin::HandleDestroying() {
   drag_drop_client_->OnNativeWidgetDestroying(GetHWND());
-  native_widget_delegate_->OnNativeWidgetDestroying();
+  if (native_widget_delegate_)
+    native_widget_delegate_->OnNativeWidgetDestroying();
 
   // Destroy the compositor before destroying the HWND since shutdown
   // may try to swap to the window.
