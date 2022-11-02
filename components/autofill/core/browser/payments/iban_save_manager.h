645e2b30d1eac553f964598f063991df6c38b766
Qihui Zhao <qihuizhao@google.com>
2022-11-02 20:31:51 +0000

    Introduce IBANSaveStrikeDatabase class.
    The IBANSaveStrikeDatabase will be used for IBAN local save strike check, add strike for ignore/decline IBAN and clear strike for add IBAN.
    
    Bug: 1349109
    Change-Id: I056925b71ccc46546e2ee583359edd1f7170691f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935862
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066658}

--- components/autofill/core/browser/payments/iban_save_manager.h
+++ components/autofill/core/browser/payments/iban_save_manager.h
@@ -14,13 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+class IBANSaveStrikeDatabase;
 class PersonalDataManager;
 
 // Decides whether an IBAN local save should be offered and handles the workflow
 // for local saves.
 class IBANSaveManager {
  public:
-  explicit IBANSaveManager(PersonalDataManager* personal_data_manager);
+  explicit IBANSaveManager(AutofillClient* client);
   IBANSaveManager(const IBANSaveManager&) = delete;
   IBANSaveManager& operator=(const IBANSaveManager&) = delete;
   virtual ~IBANSaveManager();
@@ -35,8 +36,6 @@ class IBANSaveManager {
       const absl::optional<std::u16string>& nickname = absl::nullopt) {
     OnUserDidDecideOnLocalSave(user_decision, nickname);
   }
-
-  IBAN get_iban_save_candidate_for_testing() { return iban_save_candidate_; }
 
  private:
   // Called once the user makes a decision with respect to the local IBAN
@@ -54,6 +53,9 @@ class IBANSaveManager {
   // The IBAN to be saved if local IBAN save is accepted. It will be set if
   // imported IBAN is not empty.
   IBAN iban_save_candidate_;
+
+  // StrikeDatabase used to check whether to offer to save the IBAN or not.
+  std::unique_ptr<IBANSaveStrikeDatabase> iban_save_strike_database_;
 
   base::WeakPtrFactory<IBANSaveManager> weak_ptr_factory_{this};
 };
