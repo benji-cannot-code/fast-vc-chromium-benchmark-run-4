eea998f29d682b9ffe81b5fa2b6868bbf2269b91
Leonid Baraz <lbaraz@chromium.org>
2022-11-02 23:19:02 +0000

    Fix flaky Storage tests
    Wait for Storage/StorageQueue deletion in tests.
    
    Bug: b:256170378
    Change-Id: I5e5b06e4ccb648bbf3e0eb61fa4091f937d2cfb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991991
    Reviewed-by: Hong Xu <xuhong@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066778}

--- components/reporting/storage/storage_queue.h
+++ components/reporting/storage/storage_queue.h
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/callback.h"
+#include "base/callback_list.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/feature_list.h"
@@ -31,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/storage/storage_configuration.h"
 #include "components/reporting/storage/storage_uploader_interface.h"
+#include "components/reporting/util/refcounted_closure_list.h"
 #include "components/reporting/util/status.h"
 #include "components/reporting/util/statusor.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -129,6 +132,11 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
   void AssignDegradationQueues(
       const std::vector<scoped_refptr<StorageQueue>>& degradation_queues);
 
+  // Registers completion notification callback. Thread-safe.
+  // All registered callbacks are called when the queue destruction comes
+  // to its completion.
+  void RegisterCompletionCallback(base::OnceClosure callback);
+
   // Test only: makes specified records fail on specified operation kind.
   void TestInjectErrorsForOperation(
       const test::StorageQueueOperationKind operation_kind,
@@ -166,7 +174,8 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
         const base::FilePath& filename,
         int64_t size,
         scoped_refptr<ResourceInterface> memory_resource,
-        scoped_refptr<ResourceInterface> disk_space_resource);
+        scoped_refptr<ResourceInterface> disk_space_resource,
+        scoped_refptr<RefCountedClosureList> completion_closure_list);
 
     Status Open(bool read_only);  // No-op if already opened.
     void Close();                 // No-op if not opened.
@@ -208,7 +217,14 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
     SingleFile(const base::FilePath& filename,
                int64_t size,
                scoped_refptr<ResourceInterface> memory_resource,
-               scoped_refptr<ResourceInterface> disk_space_resource);
+               scoped_refptr<ResourceInterface> disk_space_resource,
+               scoped_refptr<RefCountedClosureList> completion_closure_list);
+
+    SEQUENCE_CHECKER(sequence_checker_);
+
+    // Completion closure list reference. Dropped last, when `ReadContext` is
+    // destructed.
+    const scoped_refptr<RefCountedClosureList> completion_closure_list_;
 
     // Flag (valid for opened file only): true if file was opened for reading
     // only, false otherwise.
@@ -219,8 +235,8 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
 
     std::unique_ptr<base::File> handle_;  // Set only when opened/created.
 
-    scoped_refptr<ResourceInterface> memory_resource_;
-    scoped_refptr<ResourceInterface> disk_space_resource_;
+    const scoped_refptr<ResourceInterface> memory_resource_;
+    const scoped_refptr<ResourceInterface> disk_space_resource_;
 
     // When reading the file, this is the buffer and data positions.
     // If the data is read sequentially, buffered portions are reused
@@ -388,6 +404,10 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
   const scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner_;
   SEQUENCE_CHECKER(storage_queue_sequence_checker_);
 
+  // Completion closure list reference. Dropped when `StorageQueue` is
+  // destructed.
+  const scoped_refptr<RefCountedClosureList> completion_closure_list_;
+
   // Dedicated sequence task runner for low priority actions (which make
   // no impact on the main activity - e.g., deletion of the outdated metafiles).
   // Serializeing them should reduce their impact.
@@ -438,9 +458,6 @@ class StorageQueue : public base::RefCountedDeleteOnSequence<StorageQueue> {
   // [first_unconfirmed_sequencing_id_, first_sequencing_id_) is a gap
   // that cannot be filled in and is uploaded as such.
   absl::optional<int64_t> first_unconfirmed_sequencing_id_;
-
-  // Latest metafile. May be null.
-  scoped_refptr<SingleFile> meta_file_;
 
   // Ordered map of the files by ascending sequencing id.
   std::map<int64_t, scoped_refptr<SingleFile>> files_;
