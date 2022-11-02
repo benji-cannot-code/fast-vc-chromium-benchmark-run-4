80b2f07e7fc3de91a2c17d09e63f9c33f8454d84
Femi Adegunloye <mrfemi@google.com>
2022-11-02 23:33:01 +0000

    color/hdr: add new transferID to gfx::ColorSpace
    The new transferID will give 510 values to SDR content and reach a max
    brightness of 5x SDR.
    
    BUG=b:NONE
    
    Change-Id: I1d33b8676f85e37c26943313f75eca186697aa30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967277
    Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066788}

--- ui/gfx/color_space.h
+++ ui/gfx/color_space.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "build/build_config.h"
+#include "skia/ext/skcolorspace_trfn.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/color_space_export.h"
@@ -232,6 +233,11 @@ class COLOR_SPACE_EXPORT ColorSpace {
                       RangeID::FULL);
   }
 
+  // An extended sRGB ColorSpace that matches the sRGB EOTF but extends to
+  // 4.99x the headroom of SDR brightness. Designed for a 10 bpc buffer format.
+  // Uses P3 primaries. An HDR ColorSpace suitable for blending and compositing.
+  static ColorSpace CreateExtendedSRGB10Bit();
+
   // Create a piecewise-HDR color space.
   // - If |primaries| is CUSTOM, then |custom_primary_matrix| must be
   //   non-nullptr.
@@ -383,6 +389,10 @@ class COLOR_SPACE_EXPORT ColorSpace {
   // skcms_TransferFunction which is extended to all real values. This is true
   // unless the color space has a non-RGB matrix.
   bool HasExtendedSkTransferFn() const;
+
+  // Returns true if the transfer function values of this color space match
+  // those of the passed in skcms_TransferFunction.
+  bool IsTransferFunctionEqualTo(const skcms_TransferFunction& fn) const;
 
   // Returns true if each color in |other| can be expressed in this color space.
   bool Contains(const ColorSpace& other) const;
