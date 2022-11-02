c50152a15d4c3dbf11dc5aa3f16c04fcb01a0ee4
Fergal Daly <fergal@chromium.org>
2022-11-02 00:58:23 +0000

    Update uses of WebContentsConsoleObserver::Wait to check return value.
    The return value is newly-added and will be marked [[nodiscard]] as
    soon as all uses are updated.
    
    Previous CLs update under content/ but nothing outside of content/.
    
    BUG=1379192
    
    This CL was uploaded by git cl split.
    
    R=benwells@chromium.org
    
    Change-Id: Icd4e9378d789cb4077be2226ebf4661cf22c033f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990155
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066272}

--- chrome/browser/external_protocol/external_protocol_handler_browsertest.cc
+++ chrome/browser/external_protocol/external_protocol_handler_browsertest.cc
@@ -107,7 +107,7 @@ class ExternalProtocolHandlerSandboxBrowserTest
         has_user_gesture ? content::EXECUTE_SCRIPT_DEFAULT_OPTIONS
                          : content::EXECUTE_SCRIPT_NO_USER_GESTURE));
 
-    observer.Wait();
+    EXPECT_TRUE(observer.Wait());
     std::string message = observer.GetMessageAt(0u);
 
     return message == allowed_msg_1 || message == allowed_msg_2;
@@ -199,7 +199,7 @@ IN_PROC_BROWSER_TEST_F(ExternalProtocolHandlerBrowserTest,
   observer.SetPattern("*aunch*'mailto:test@site.test'*");
   ASSERT_TRUE(
       ExecJs(web_content(), "window.open('mailto:test@site.test', '_self');"));
-  observer.Wait();
+  ASSERT_TRUE(observer.Wait());
   ASSERT_EQ(1u, observer.messages().size());
   EXPECT_EQ("Launched external handler for 'mailto:test@site.test'.",
             observer.GetMessageAt(0u));
@@ -214,7 +214,7 @@ IN_PROC_BROWSER_TEST_F(ExternalProtocolHandlerBrowserTest,
   observer.SetPattern("Failed to launch 'does.not.exist:failure'*");
   ASSERT_TRUE(
       ExecJs(web_content(), "window.open('does.not.exist:failure', '_self');"));
-  observer.Wait();
+  ASSERT_TRUE(observer.Wait());
   ASSERT_EQ(1u, observer.messages().size());
 #endif
 }
@@ -337,7 +337,7 @@ IN_PROC_BROWSER_TEST_F(ExternalProtocolHandlerBrowserTest,
 
   ASSERT_TRUE(
       ExecJs(originating_rfh, "location.href = 'willfailtolaunch://foo';"));
-  observer.Wait();
+  ASSERT_TRUE(observer.Wait());
   ASSERT_EQ(1u, observer.messages().size());
   EXPECT_EQ("Not allowed to launch 'willfailtolaunch://foo'.",
             observer.GetMessageAt(0u));
