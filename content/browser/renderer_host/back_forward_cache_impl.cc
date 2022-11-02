a4b804869c7190f639f6a257ed7f621cac36fb82
Fergal Daly <fergal@chromium.org>
2022-11-02 03:07:30 +0000

    Insert missing spaces into logged message.
    This is a follow-up to https://crrev.com/c/3990133
    
    Bug: 1379490
    Change-Id: I5c8e190b7f2f07e086209f15fdc3ff0aae0a2909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995945
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066311}

--- content/browser/renderer_host/back_forward_cache_impl.cc
+++ content/browser/renderer_host/back_forward_cache_impl.cc
@@ -1438,8 +1438,8 @@ bool BackForwardCacheImpl::IsScreenReaderAllowed() {
 void BackForwardCacheImpl::VlogUnexpectedRendererToBrowserMessage(
     const char* interface_name,
     uint32_t message_name) {
-  VLOG(1) << "BackForwardCacheMessageFilter::WillDispatch bad_message"
-          << "interface_name" << interface_name << "message_name"
+  VLOG(1) << "BackForwardCacheMessageFilter::WillDispatch bad_message "
+          << "interface_name " << interface_name << "message_name "
           << message_name;
 }
 
