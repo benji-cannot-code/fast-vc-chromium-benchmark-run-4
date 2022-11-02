8c13292166f629dd8ac1248b50d2c39f82b54a50
Aishwarya Rajesh <aishwaryarj@google.com>
2022-11-02 20:22:04 +0000

    [RDSZ] Add feature flag for desktop site page zoom
    Add a //content feature flag that guards the implementation of desktop site page zoom on Android.
    
    Bug: 1370707
    Change-Id: I66db0562f1ac4eaf3b4486f76fb263c193637f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997212
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066648}

--- content/browser/android/content_feature_list.cc
+++ content/browser/android/content_feature_list.cc
@@ -37,6 +37,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kReduceGpuPriorityOnBackground,
     &features::kRequestDesktopSiteAdditions,
     &features::kRequestDesktopSiteExceptions,
+    &features::kRequestDesktopSiteZoom,
     &features::kTouchDragAndContextMenu,
     &features::kWebAuthConditionalUI,
     &features::kWebBluetoothNewPermissionsBackend,
