7e226736da3c6000e48138fce66e389725a1110e
Aya Elgendy <ayag@chromium.org>
2022-11-02 19:13:42 +0000

    [Code Health Rotation] Replace ListValue with base::Value::List in chrome/browser/ash/arc/tracing/arc_*
    To be continued in other CL(s)...
    
    Bug: 1187062
    Change-Id: I82e8437254ec3a98bfc06d9f85e429c27275ac9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999442
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Aya Elgendy‎ <ayag@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066611}

--- chrome/browser/ash/arc/tracing/arc_system_model.h
+++ chrome/browser/ash/arc/tracing/arc_system_model.h
@@ -54,7 +54,7 @@ class ArcSystemModel {
   void CloseRangeForValueEvents(uint64_t max_timestamp);
 
   void CopyFrom(const ArcSystemModel& other);
-  base::DictionaryValue Serialize() const;
+  base::Value::Dict Serialize() const;
   bool Load(const base::Value* root);
 
   bool operator==(const ArcSystemModel& other) const;
