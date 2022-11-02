2e42d4c2bf7617d7bfd0556f05b8bfa852f48ecd
Joe Mason <joenotcharles@google.com>
2022-11-02 21:26:23 +0000

    Record an estimate of total memory sampled by the heap profiler to UMA
    Bug: 1380062
    Change-Id: I095de65534cdae8b01880945d2dfda293f5530cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3994905
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066704}

--- components/heap_profiling/in_process/heap_profiler_controller.cc
+++ components/heap_profiling/in_process/heap_profiler_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/check.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/numerics/clamped_math.h"
 #include "base/profiler/module_cache.h"
 #include "base/rand_util.h"
 #include "base/sampling_heap_profiler/sampling_heap_profiler.h"
@@ -273,6 +274,7 @@ void HeapProfilerController::RetrieveAndSendSnapshot(
 
   SampleMap merged_samples = MergeSamples(samples);
 
+  base::ClampedNumeric<uint64_t> total_sampled_bytes;
   for (auto& pair : merged_samples) {
     const Sample& sample = pair.first;
     const SampleValue& value = pair.second;
@@ -289,9 +291,17 @@ void HeapProfilerController::RetrieveAndSendSnapshot(
     // do not have a meaningful timestamp.
     profile_builder.OnSampleCompleted(std::move(frames), base::TimeTicks(),
                                       value.total, value.count);
+
+    total_sampled_bytes += value.total;
   }
 
   profile_builder.OnProfileCompleted(base::TimeDelta(), base::TimeDelta());
+
+  constexpr int kBytesPerMB = 1024 * 1024;
+  base::UmaHistogramMemoryLargeMB(
+      ProcessHistogramName("HeapProfiling.InProcess.TotalSampledMemory",
+                           process_type),
+      base::ClampDiv(total_sampled_bytes, kBytesPerMB));
 }
 
 }  // namespace heap_profiling
