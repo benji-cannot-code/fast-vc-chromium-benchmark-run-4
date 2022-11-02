114b5a785dfcf8f697217e269c8aa4839caaa6f1
Elly Fong-Jones <ellyjones@chromium.org>
2022-11-02 15:29:48 +0000

    base: include sys/types.h in symbolize.h
    It is needed for ssize_t. In glibc specifically, ssize_t happens to get
    transitively included by some other header - I'm not sure exactly what -
    but POSIX says ssize_t is in sys/types.h and in other libcs the included
    is needed for the type.
    
    Bug: 1380656
    Change-Id: Ibeef8c80f44595b6056fc1be8a104ab7428aa8bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997878
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066486}

--- base/third_party/symbolize/symbolize.h
+++ base/third_party/symbolize/symbolize.h
@@ -55,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_SYMBOLIZE_H_
 #define BASE_SYMBOLIZE_H_
 
+#include <sys/types.h>  // for ssize_t
+
 #include "utilities.h"
 #include "config.h"
 #include "glog/logging.h"
