173c526483fa051a829b29b787759296a51c5771
Jimmy <jimmyxgong@chromium.org>
2022-11-02 00:01:43 +0000

    shortcuts: Add AcceleratorUpdatedObserver
    AcceleratorUpdatedObserver mojo api lets the backend SWA provider to
    notify the SWA frontend of updated accelerator events. Though not
    all implemented yet, this observer can be triggered via:
    - Keyboard device connected/disconnected
    - Change of accelerator-related prefs
    - Modifications of accelerators
    
    Bug: b:216049298
    Test: ash_webui_unittest
    Change-Id: Ia054aa7d6f88f60c698a31b91e303afdf7a3bcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988357
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066244}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
 #include "base/memory/weak_ptr.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
 #include "ui/events/devices/input_device.h"
@@ -43,6 +44,9 @@ class AcceleratorConfigurationProvider
   void IsMutable(ash::mojom::AcceleratorSource source,
                  IsMutableCallback callback) override;
   void GetAccelerators(GetAcceleratorsCallback callback) override;
+  void AddObserver(mojo::PendingRemote<
+                   shortcut_customization::mojom::AcceleratorsUpdatedObserver>
+                       observer) override;
 
   // ui::InputDeviceEventObserver:
   void OnInputDeviceConfigurationChanged(uint8_t input_device_types) override;
@@ -62,6 +66,10 @@ class AcceleratorConfigurationProvider
 
   void UpdateKeyboards();
 
+  void NotifyAcceleratorsUpdated();
+
+  AcceleratorConfigurationMap CreateConfigurationMap();
+
   std::vector<AcceleratorInfo> accelerator_infos_;
 
   std::map<AcceleratorActionId, std::vector<AcceleratorInfo>>
@@ -75,6 +83,10 @@ class AcceleratorConfigurationProvider
       receiver_{this};
 
   AcceleratorConfiguration* ash_accelerator_configuration_;
+
+  mojo::Remote<shortcut_customization::mojom::AcceleratorsUpdatedObserver>
+      accelerators_updated_observers_;
+
   base::WeakPtrFactory<AcceleratorConfigurationProvider> weak_ptr_factory_{
       this};
 };
