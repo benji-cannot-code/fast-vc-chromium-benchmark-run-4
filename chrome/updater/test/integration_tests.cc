2c829ac805d9de74a35703e225171f3d0be86afb
Sorin Jianu <sorin@chromium.org>
2022-11-02 15:42:09 +0000

    Updater: remove redundant clean up calls.
    Some of these calls might actually hide unclean uninstall issues.
    
    Fixed: 1380659
    Change-Id: Iadd5ff9e61e34de73534b504e996d1153a7aa2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995179
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066490}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -406,7 +406,14 @@ TEST_F(IntegrationTest, SelfUninstallOutdatedUpdater) {
   ExpectVersionNotActive(kUpdaterVersion);
   ExpectVersionNotActive("0.0.0.0");
 
-  Uninstall();
+  // Do not call `Uninstall()` since the outdated updater uninstalled itself.
+  // Additional clean up is needed because of how this test is set up. After
+  // the outdated instance uninstalls, a few files are left in the product
+  // directory: prefs.json, updater.log, and overrides.json. These files are
+  // owned by the active instance of the updater but in this case there is
+  // no active instance left; therefore, explicit clean up is required.
+  PrintLog();
+  CopyLog();
   Clean();
 }
 
@@ -435,7 +442,6 @@ TEST_F(IntegrationTest, QualifyUpdater) {
   ExpectVersionActive(kUpdaterVersion);
 
   Uninstall();
-  Clean();
 }
 
 TEST_F(IntegrationTest, SelfUpdate) {
@@ -451,7 +457,6 @@ TEST_F(IntegrationTest, SelfUpdate) {
   ExpectAppVersion(kUpdaterAppId, next_version);
 
   Uninstall();
-  Clean();
 }
 
 TEST_F(IntegrationTest, ReportsActive) {
@@ -514,7 +519,6 @@ TEST_F(IntegrationTest, UpdateApp) {
   ExpectLastStarted();
 
   Uninstall();
-  Clean();
 }
 
 #if BUILDFLAG(IS_WIN)
@@ -540,7 +544,6 @@ TEST_F(IntegrationTest, ForceInstallApp) {
   ExpectAppVersion(kAppId, v1);
 
   Uninstall();
-  Clean();
 }
 #endif  // BUILDFLAG(IS_WIN)
 
