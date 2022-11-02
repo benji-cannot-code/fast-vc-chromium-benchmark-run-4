148b9de455ca6191a59f2dffed78c2ddf04c41a2
Elly Fong-Jones <ellyjones@chromium.org>
2022-11-02 17:08:06 +0000

    sandbox: include sys/types.h for uid_t, gid_t, pid_t
    In glibc these happen to get transitively included by string (I think?)
    but POSIX says these types live in sys/types.h and in other libcs
    (such as musl) a direct include of sys/types.h is needed.
    
    Bug: 1380656
    Change-Id: Ia5db2c02183c822789cf90fce58a6037e37fcb75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998080
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066548}

--- sandbox/linux/services/credentials.h
+++ sandbox/linux/services/credentials.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SANDBOX_LINUX_SERVICES_CREDENTIALS_H_
 #define SANDBOX_LINUX_SERVICES_CREDENTIALS_H_
 
+#include <sys/types.h>
+
 #include "build/build_config.h"
 // Link errors are tedious to track, raise a compile-time error instead.
 #if BUILDFLAG(IS_ANDROID)
