05c12996f5d6fbe9b0c7cb08ab0324ceb8a5a9c9
William Liu <liuwilliam@chromium.org>
2022-11-02 22:46:29 +0000

    Properly set transferSize, encodedBodySize and decodedBodySize for Prefetch.
    These three performance APIs return zero because
    PrefetchFromStringURLLoader is not aware of the initial prefetch
    request. This CL plumbs the size information to the URLLoader, through
    the PrefetchContainer.
    
    Also see (internal access): https://docs.google.com/document/d/1wMelfFY34jnyCDA6AAy0nm3l16vE0ruTo3y3rnEaso4
    
    Bug: 1380812
    Change-Id: I53abb87fcf837f737b992fe418444e2c75573366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000224
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066762}

--- content/browser/preloading/prefetch/prefetch_from_string_url_loader.cc
+++ content/browser/preloading/prefetch/prefetch_from_string_url_loader.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
+#include "content/browser/preloading/prefetch/prefetch_container.h"
 #include "content/browser/preloading/prefetch/prefetched_mainframe_response_container.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -27,11 +28,13 @@ namespace content {
 
 PrefetchFromStringURLLoader::PrefetchFromStringURLLoader(
     std::unique_ptr<PrefetchedMainframeResponseContainer> response,
+    const absl::optional<PrefetchResponseSizes>& response_sizes,
     const network::ResourceRequest& tentative_resource_request)
     : head_(response->ReleaseHead()),
       body_buffer_(
           base::MakeRefCounted<net::StringIOBuffer>(response->ReleaseBody())),
-      bytes_of_raw_data_to_transfer_(body_buffer_->size()) {}
+      bytes_of_raw_data_to_transfer_(body_buffer_->size()),
+      response_sizes_(response_sizes) {}
 
 PrefetchFromStringURLLoader::~PrefetchFromStringURLLoader() = default;
 
@@ -140,7 +143,13 @@ void PrefetchFromStringURLLoader::OnHandleReady(
 }
 
 void PrefetchFromStringURLLoader::Finish(int error) {
-  client_->OnComplete(network::URLLoaderCompletionStatus(error));
+  network::URLLoaderCompletionStatus status(error);
+  if (response_sizes_) {
+    status.encoded_data_length = response_sizes_->encoded_data_length;
+    status.encoded_body_length = response_sizes_->encoded_body_length;
+    status.decoded_body_length = response_sizes_->decoded_body_length;
+  }
+  client_->OnComplete(status);
   handle_watcher_.reset();
   producer_handle_.reset();
   client_.reset();
