956abd459b7a4f63b34db08804d405caad4ee1a4
Yichen <yichenz@chromium.org>
2022-11-02 00:18:07 +0000

    Report Ash.EventLatency.TotalLatency
    In addition to Ash.EventLatency.{EventType}.TotalLatency,
    report Ash.EventLatency.TotalLatency as the total latency
    of all types of event sent to UI compositor.
    
    BUG=b:249557153
    
    Change-Id: Ieffe4c9d4e972fc630d4f0791d9edb34512a14f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990423
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Yichen Zhou <yichenz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066253}

--- ash/metrics/ui_metrics_recorder.cc
+++ ash/metrics/ui_metrics_recorder.cc
@@ -114,6 +114,9 @@ void UiMetricsRecorder::ReportEventLatency(
                       cc::EventMetrics::GetTypeName(latency.event_type),
                       ".TotalLatency"}),
         latency.total_latency, base::Milliseconds(1), base::Seconds(5), 100);
+    UMA_HISTOGRAM_CUSTOM_TIMES("Ash.EventLatency.TotalLatency",
+                               latency.total_latency, base::Milliseconds(1),
+                               base::Seconds(5), 100);
   }
 }
 }  // namespace ash
