8c8d7b95fefe2562fab1bde452512398da950ad3
Ben Pastene <bpastene@chromium.org>
2022-11-02 16:35:24 +0000

    Persist the magic_cros_vm_cache on CrOS VM test bots
    This magic cache today is supposed to make CrOS VM test tasks prefer to
    run on bots that have already ran similar tasks. This helps avoid
    re-downloading the large VM images in the test input. These images
    are stored in the isolated cache, whose contents the bots don't really
    advertise. So we use a dummy named cache dir to indicate the presence
    of the VM image in the isolated cache.
    
    But a change was made to swarming a while back so that it no longer
    persists empty named caches. And the CrOS VM test tasks never wrote
    anything to their magic named VM cache, so swarming no longer keeps
    the magic caches around, which totally killed the bot affinity that
    the CrOS VM test tasks were counting on.
    
    So this makes it so the test runner will write a dummy text file to
    the magic named cache so that swarming persists it.
    
    The path to the cache is plumbed down to the test runner via the
    'args' field in the corresponding mixin. This appends the arg to
    the end of the test runner invocation, and this would interfere
    with certain types of tests (eg: telemetry tests). These tests
    are invoked like:
    ./build/chromeos/test_runner.py $(args to the test runner) -- $(args to telemetry)
    
    The `--magic-vm-cache` arg would show up after the '--', which would
    have made the test harness unaware of it. So this also reworks how
    args are parsed. Instead of the test command coming after the '--',
    we remove the use of '--' all together and treat the list of unknown
    args as the test command. This seems to work.
    
    Bug: None
    Change-Id: Ide84009e33fbce6a685f9552b85544e63effe759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990219
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066526}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1828,7 +1828,6 @@ class MetaBuildWrapper:
         cmdline += [
             os.path.join('bin', 'cros_test_wrapper'),
             '--logs-dir=${ISOLATED_OUTDIR}',
-            '--',
         ]
       if is_android:
         extra_files.append('../../build/android/test_wrapper/logdog_wrapper.py')
