693ffe4dc17cd382fd23a74b60db154c25e34d80
Nancy Wang <nancylingwang@chromium.org>
2022-11-02 06:42:43 +0000

    Add app icon reader and writer classes.
    There will be follow up CLs to implement app icon reader and writer
    classes to read and write icons to the AppService directory on the disk.
    
    BUG=1380608
    
    Change-Id: I15591166a5893d072eca2d31717e1cc1332201c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995256
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066353}

---
+++ chrome/browser/apps/app_service/app_icon/app_icon_reader.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_service/app_icon/app_icon_reader.h"
+
+namespace apps {
+
+AppIconReader::AppIconReader() = default;
+
+AppIconReader::~AppIconReader() = default;
+
+void AppIconReader::ReadIcons(const std::string& app_id,
+                              int32_t size_hint_in_dip,
+                              IconEffects icon_effects,
+                              LoadIconCallback callback) {
+  // TODO(crbug.com/1380608): Implement the icon reading function.
+}
+
+}  // namespace apps
---
+++ chrome/browser/apps/app_service/app_icon/app_icon_writer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_APP_SERVICE_APP_ICON_APP_ICON_WRITER_H_
+#define CHROME_BROWSER_APPS_APP_SERVICE_APP_ICON_APP_ICON_WRITER_H_
+
+namespace apps {
+
+// AppIconWriter writes app icons to the icon image files in the local disk.
+//
+// TODO(crbug.com/1380608): Implement the icon writing function.
+class AppIconWriter {
+ public:
+  AppIconWriter();
+  AppIconWriter(const AppIconWriter&) = delete;
+  AppIconWriter& operator=(const AppIconWriter&) = delete;
+  ~AppIconWriter();
+};
+
+}  // namespace apps
+
+#endif  // CHROME_BROWSER_APPS_APP_SERVICE_APP_ICON_APP_ICON_WRITER_H_
