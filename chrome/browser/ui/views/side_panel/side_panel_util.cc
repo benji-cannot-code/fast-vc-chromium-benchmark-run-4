ae69ff6bca27ba8f113983046dac1b979e639150
Marlon Facey <mfacey@chromium.org>
2022-11-02 17:16:36 +0000

    [journeys] Add history flag to side panel entry registration
    Add main journeys flag to journeys side panel registration so journeys
    option does not show for nonsupported languages.
    
    Bug: 1376032
    Change-Id: Ice5dce73616795569a934c54d7fecebfa7701b31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984765
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066551}

--- chrome/browser/ui/views/side_panel/side_panel_util.cc
+++ chrome/browser/ui/views/side_panel/side_panel_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/strings/strcat.h"
+#include "chrome/browser/history_clusters/history_clusters_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/side_panel/user_note/user_note_ui_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/webview/webview_side_panel_coordinator.h"
 #include "components/feed/feed_feature_list.h"
+#include "components/history_clusters/core/history_clusters_service.h"
 #include "components/user_notes/user_notes_features.h"
 #include "ui/accessibility/accessibility_features.h"
 
@@ -62,7 +64,11 @@ void SidePanelUtil::PopulateGlobalEntries(Browser* browser,
       ->CreateAndRegisterEntry(global_registry);
 
   // Add history clusters.
+  auto* history_clusters_service =
+      HistoryClustersServiceFactory::GetForBrowserContext(browser->profile());
   if (base::FeatureList::IsEnabled(features::kSidePanelJourneys) &&
+      history_clusters_service &&
+      history_clusters_service->IsJourneysEnabled() &&
       !browser->profile()->IsIncognitoProfile()) {
     HistoryClustersSidePanelCoordinator::GetOrCreateForBrowser(browser)
         ->CreateAndRegisterEntry(global_registry);
