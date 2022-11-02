5df3eaac9ffadeef9ccaf270d4120e7fa406d007
Etienne Noel <etiennenoel@google.com>
2022-11-02 22:42:39 +0000

    Moved the `IsValidBucketName` method to its own file.
    The `IsValidBucketName` method has been moved to its own file. Unit
    tests were also added to confirm the requirements.
    
    Change-Id: I9c1457371857559f7c34fdb69910318764f68a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999044
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Etienne Noël <etiennenoel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066757}

---
+++ content/browser/buckets/bucket_utils_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/buckets/bucket_utils.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace content {
+
+TEST(BucketUtilsTest, IsValidBucketName) {
+  struct {
+    std::string bucket_name;
+    bool expected_validity;
+  } test_cases[] = {
+      {"", false},
+      {"StringThatIsLongerThan64Characters1234567890123456789012345678901",
+       false},
+      {"-ShouldNotStartWithDash", false},
+      {"_ShouldNotStartWithUnderscore", false},
+      {"ShouldNotContainNonAsciiCharacters!", false},
+      {"ShouldNotContainUpperCases", false},
+      {"should-be-valid-123", true},
+  };
+
+  for (auto& testCase : test_cases) {
+    EXPECT_EQ(IsValidBucketName(testCase.bucket_name),
+              testCase.expected_validity);
+  }
+}
+}  // namespace content
