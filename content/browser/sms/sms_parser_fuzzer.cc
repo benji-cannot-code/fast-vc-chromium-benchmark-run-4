d11561506ee06a5a1b38deab494fbdaf0dea763f
Maksim Ivanov <emaxx@chromium.org>
2022-11-03 15:57:45 +0000

    Fuzz content::SmsParser
    Implement fuzzer for checking data parsing logic in
    //content/browser/sms/sms_parser.cc.
    
    Bug: none
    Change-Id: I14acd6a619d6bb80c93f3033e6cc9238630c23cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991088
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067043}

---
+++ content/browser/sms/sms_parser_fuzzer.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <tuple>
+
+#include "base/strings/string_piece.h"
+#include "content/browser/sms/sms_parser.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::ignore = content::SmsParser::Parse(
+      base::StringPiece(reinterpret_cast<const char*>(data), size));
+  return 0;
+}
