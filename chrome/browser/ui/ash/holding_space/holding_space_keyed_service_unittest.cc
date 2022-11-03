19c6bc9a339da98873491f11d64a17b068260b61
Jongmok Kim <johny.kimc@gmail.com>
2022-11-03 14:14:19 +0000

    Remove RefCountedString::TakeString
    Add jongmok kim to AUTHORS.
    
    Bug: 1374216
    Change-Id: I73f61df9641d7b994b2a009541e80c6a70548baf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993482
    Commit-Queue: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066992}

--- chrome/browser/ui/ash/holding_space/holding_space_keyed_service_unittest.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_keyed_service_unittest.cc
@@ -2869,11 +2869,11 @@ class HoldingSpaceKeyedServicePrintToPdfIntegrationTest
     pdf_printer_handler_->SetPdfSavedClosureForTesting(run_loop.QuitClosure());
     pdf_printer_handler_->SetPrintToPdfPathForTesting(file_path);
 
-    std::string data;
-    pdf_printer_handler_->StartPrint(job_title,
-                                     /*settings=*/base::Value::Dict(),
-                                     base::RefCountedString::TakeString(&data),
-                                     /*callback=*/base::DoNothing());
+    pdf_printer_handler_->StartPrint(
+        job_title,
+        /*settings=*/base::Value::Dict(),
+        base::MakeRefCounted<base::RefCountedString>(std::string()),
+        /*callback=*/base::DoNothing());
 
     run_loop.Run();
   }
--- chrome/browser/ui/webui/about_ui.cc
+++ chrome/browser/ui/webui/about_ui.cc
@@ -265,7 +265,7 @@ class ChromeOSTermsHandler
               IDS_TERMS_HTML);
     }
     std::move(callback_).Run(
-        base::RefCountedString::TakeString(std::move(contents_)));
+        base::MakeRefCounted<base::RefCountedString>(std::move(contents_)));
   }
 
   // Path in the URL.
@@ -337,7 +337,7 @@ class ChromeOSCreditsHandler
               IDR_OS_CREDITS_HTML);
     }
     std::move(callback_).Run(
-        base::RefCountedString::TakeString(std::move(contents_)));
+        base::MakeRefCounted<base::RefCountedString>(std::move(contents_)));
   }
 
   // Path in the URL.
@@ -356,7 +356,7 @@ void OnBorealisCreditsLoaded(content::URLDataSource::GotDataCallback callback,
     credits_html = l10n_util::GetStringUTF8(IDS_BOREALIS_CREDITS_PLACEHOLDER);
   }
   std::move(callback).Run(
-      base::RefCountedString::TakeString(std::move(credits_html)));
+      base::MakeRefCounted<base::RefCountedString>(std::move(credits_html)));
 }
 
 void HandleBorealisCredits(Profile* profile,
@@ -441,7 +441,7 @@ class CrostiniCreditsHandler
       contents_ = l10n_util::GetStringUTF8(IDS_CROSTINI_CREDITS_PLACEHOLDER);
     }
     std::move(callback_).Run(
-        base::RefCountedString::TakeString(std::move(contents_)));
+        base::MakeRefCounted<base::RefCountedString>(std::move(contents_)));
   }
 
   // Path in the URL.
@@ -725,9 +725,7 @@ void AboutUIHTMLSource::StartDataRequest(
 void AboutUIHTMLSource::FinishDataRequest(
     const std::string& html,
     content::URLDataSource::GotDataCallback callback) {
-  std::string html_copy(html);
-  std::move(callback).Run(
-      base::RefCountedString::TakeString(std::move(html_copy)));
+  std::move(callback).Run(base::MakeRefCounted<base::RefCountedString>(html));
 }
 
 std::string AboutUIHTMLSource::GetMimeType(const GURL& url) {
