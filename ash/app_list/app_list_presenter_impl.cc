e8777a31a6ddcb3ef9b8cebbe6f9df56e3c18aea
Wen-Chien Wang <wcwang@chromium.org>
2022-11-03 19:03:38 +0000

    Remove rounded corner in app list presenter and app list view
    As the app list presenter and app list view are only used in tablet
    mode, there is no rounded corner needed to be considered in these
    classes. Remove them in this cl.
    
    Bug: 1378658
    Change-Id: I1a5d7b800225335ebd681dd8238ed4d63067b59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004059
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067159}

--- ash/app_list/app_list_presenter_impl.cc
+++ ash/app_list/app_list_presenter_impl.cc
@@ -88,23 +88,6 @@ void DidPresentCompositorFrame(base::TimeTicks event_time_stamp,
   }
 }
 
-// Whether the shelf background type indicates that shelf has rounded corners.
-bool IsShelfBackgroundTypeWithRoundedCorners(
-    ShelfBackgroundType background_type) {
-  switch (background_type) {
-    case ShelfBackgroundType::kDefaultBg:
-    case ShelfBackgroundType::kOverview:
-      return true;
-    case ShelfBackgroundType::kMaximized:
-    case ShelfBackgroundType::kOobe:
-    case ShelfBackgroundType::kHomeLauncher:
-    case ShelfBackgroundType::kLogin:
-    case ShelfBackgroundType::kLoginNonBlurredWallpaper:
-    case ShelfBackgroundType::kInApp:
-      return false;
-  }
-}
-
 // Invokes `complete_callback_` at the end of animation.
 class FullscreenLauncherAnimationObserver
     : public ui::ImplicitAnimationObserver,
@@ -257,7 +240,6 @@ void AppListPresenterImpl::Show(AppListViewState preferred_state,
       Shelf::ForWindow(view_->GetWidget()->GetNativeView()->GetRootWindow());
   shelf->shelf_layout_manager()->UpdateAutoHideState();
 
-  // Observe the shelf for changes to rounded corners.
   // If presenter is observing a shelf instance different than `shelf`, it's
   // because the app list view on the associated display is closing. It's safe
   // to remove this observation (given that shelf background changes should not
@@ -271,8 +253,6 @@ void AppListPresenterImpl::Show(AppListViewState preferred_state,
   // ScrollableShelfView are deleted. https://crbug.com/1163332
   view_->SetDragAndDropHostOfCurrentAppList(
       shelf->shelf_widget()->GetDragAndDropHostForAppList());
-  view_->SetShelfHasRoundedCorners(
-      IsShelfBackgroundTypeWithRoundedCorners(shelf->GetBackgroundType()));
   std::unique_ptr<AppListView::ScopedAccessibilityAnnouncementLock>
       scoped_accessibility_lock;
 
@@ -767,13 +747,6 @@ void AppListPresenterImpl::OnShelfShuttingDown() {
   shelf_observer_.Reset();
   if (view_)
     view_->SetDragAndDropHostOfCurrentAppList(nullptr);
-}
-
-void AppListPresenterImpl::OnBackgroundTypeChanged(
-    ShelfBackgroundType background_type,
-    AnimationChangeType change_type) {
-  view_->SetShelfHasRoundedCorners(
-      IsShelfBackgroundTypeWithRoundedCorners(background_type));
 }
 
 void AppListPresenterImpl::SnapAppListBoundsToDisplayEdge() {
