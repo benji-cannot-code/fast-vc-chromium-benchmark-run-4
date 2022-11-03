bd20dedb69612b6c35d417bddaad3c19766f9958
Duong Dac <ddac@chromium.org>
2022-11-03 13:54:50 +0000

    [sign-in] Reorder account capabilities in alphabetical order
    This is a pure refactoring to bring more order to the
    account capabilities list.
    
    Change-Id: Icaec6dd0abf58bd24509e2c2fefe19e42f56b5b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998654
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066977}

--- components/signin/public/identity_manager/account_capabilities_test_mutator.cc
+++ components/signin/public/identity_manager/account_capabilities_test_mutator.cc
@@ -43,10 +43,8 @@ void AccountCapabilitiesTestMutator::set_can_stop_parental_supervision(
       value;
 }
 
-void AccountCapabilitiesTestMutator::set_is_subject_to_parental_controls(
-    bool value) {
-  capabilities_->capabilities_map_[kIsSubjectToParentalControlsCapabilityName] =
-      value;
+void AccountCapabilitiesTestMutator::set_can_toggle_auto_updates(bool value) {
+  capabilities_->capabilities_map_[kCanToggleAutoUpdatesName] = value;
 }
 
 void AccountCapabilitiesTestMutator::set_is_allowed_for_machine_learning(
@@ -55,8 +53,10 @@ void AccountCapabilitiesTestMutator::set_is_allowed_for_machine_learning(
       value;
 }
 
-void AccountCapabilitiesTestMutator::set_can_toggle_auto_updates(bool value) {
-  capabilities_->capabilities_map_[kCanToggleAutoUpdatesName] = value;
+void AccountCapabilitiesTestMutator::set_is_subject_to_parental_controls(
+    bool value) {
+  capabilities_->capabilities_map_[kIsSubjectToParentalControlsCapabilityName] =
+      value;
 }
 
 void AccountCapabilitiesTestMutator::SetAllSupportedCapabilities(bool value) {
