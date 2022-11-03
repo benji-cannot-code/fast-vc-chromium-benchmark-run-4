d734b7472e87e3a16e0d844a2ca4708b46fb7033
Christian Flach <cmfcmf@chromium.org>
2022-11-03 11:02:25 +0000

    isolated-apps: Derive `WebAppUrlInfo` from `SignedWebBundleId`
    Instead of accepting invalid hostnames in `IsolatedWebAppUrlInfo`, only
    accept hostnames that match the Web Bundle ID format. There is no use
    case for instances of `IsolatedWebAppUrlInfo` with invalid hostnames.
    
    Bug: 1365852
    Change-Id: I792b9a70fa983d8703800a02909d4e49b58bfb02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3985727
    Commit-Queue: Christian Flach <cmfcmf@google.com>
    Reviewed-by: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066937}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/types/expected.h"
 #include "chrome/browser/web_applications/web_app_id.h"
+#include "components/web_package/signed_web_bundles/signed_web_bundle_id.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -17,10 +18,6 @@ namespace content {
 class BrowserContext;
 class StoragePartitionConfig;
 }  // namespace content
-
-namespace web_package {
-class SignedWebBundleId;
-}
 
 namespace web_app {
 
@@ -32,14 +29,12 @@ class IsolatedWebAppUrlInfo {
   // message if the URL isn't valid.
   //
   // Note that this only performs basic URL validation; a non-error value does
-  // not guarantee the URL contains a valid key in its hostname, or that it
-  // corresponds to an existing or installed app.
+  // not guarantee that it corresponds to an existing or installed app.
   static base::expected<IsolatedWebAppUrlInfo, std::string> Create(
       const GURL& url);
 
-  // Wraps Create() but accepts a SignedWebBundleId object.
-  static base::expected<IsolatedWebAppUrlInfo, std::string>
-  CreateFromSignedWebBundleId(
+  // Creates an IsolatedWebAppUrlInfo instance from a SignedWebBundleId object.
+  static IsolatedWebAppUrlInfo CreateFromSignedWebBundleId(
       const web_package::SignedWebBundleId& web_bundle_id);
 
   // Returns the origin of the IWA that this URL refers to.
@@ -49,21 +44,21 @@ class IsolatedWebAppUrlInfo {
   // this URL.
   const AppId& app_id() const;
 
+  // Returns the Web Bundle ID of the IWA that this URL refers to.
+  const web_package::SignedWebBundleId& web_bundle_id() const;
+
   // Returns the StoragePartitionConfig that should be used by the resource
   // hosted at this URL.
   content::StoragePartitionConfig storage_partition_config(
       content::BrowserContext* browser_context) const;
 
-  // Parses a `SignedWebBundleId` from the URL, verifying that it is a valid
-  // isolated-app:// URL. Returns an error message on failure.
-  base::expected<web_package::SignedWebBundleId, std::string>
-  ParseSignedWebBundleId() const;
-
  private:
-  explicit IsolatedWebAppUrlInfo(const url::Origin& url);
+  explicit IsolatedWebAppUrlInfo(
+      const web_package::SignedWebBundleId& web_bundle_id);
 
   url::Origin origin_;
   AppId app_id_;
+  web_package::SignedWebBundleId web_bundle_id_;
 };
 
 }  // namespace web_app
