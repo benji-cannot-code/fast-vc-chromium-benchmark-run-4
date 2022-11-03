49beb24735bda280cd1e7b4455e9ea6cd96a8d3f
Maggie Cai <mxcai@chromium.org>
2022-11-03 05:08:34 +0000

    Revert "[ChromeVox] Untangle common/keyboard_handler.js"
    This reverts commit 3ed2695665fd74c56545adf2e40da38945865ad3.
    
    Reason for revert: This is likely the cause for failure of ChromeVox browser tests, starting from https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/46832
    
    Original change's description:
    > [ChromeVox] Untangle common/keyboard_handler.js
    >
    > This change is a pure refactor.
    >
    > AX-Relnotes: n/a.
    > Bug: none
    > Test: cq
    > Change-Id: I9e39682f9a12cf2dba6fb293012f46e4c95ee4cf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969583
    > Reviewed-by: Josiah Krutz <josiahk@google.com>
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1066843}
    
    Bug: none
    Change-Id: I3c4c4daa3a2a7cd646e893a2c69982e3c293cc1e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999428
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1066878}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-import {AbstractKeyboardHandler} from '../common/abstract_keyboard_handler.js';
 import {BackgroundBridge} from '../common/background_bridge.js';
 import {BrailleCommandData} from '../common/braille/braille_command_data.js';
 import {BrailleKeyCommand, BrailleKeyEvent} from '../common/braille/braille_key_types.js';
@@ -17,6 +16,7 @@ import {Command, CommandStore} from '../common/command_store.js';
 import {GestureCommandData} from '../common/gesture_command_data.js';
 import {KeyMap} from '../common/key_map.js';
 import {KeyUtil} from '../common/key_util.js';
+import {ChromeVoxKbHandler} from '../common/keyboard_handler.js';
 import {Msgs} from '../common/msgs.js';
 import {Spannable} from '../common/spannable.js';
 import {QueueMode, TtsSpeechProperties} from '../common/tts_interface.js';
@@ -25,20 +25,10 @@ import {QueueMode, TtsSpeechProperties} from '../common/tts_interface.js';
  * Class to manage the keyboard explorer.
  */
 export class LearnMode {
-  constructor() {
-    /** @private {!LearnModeKeyboardHandler} */
-    this.keyboardHandler_ = new LearnModeKeyboardHandler();
-  }
-
   /**
    * Initialize keyboard explorer.
    */
   static async init() {
-    if (LearnMode.instance) {
-      throw new Error('Cannot initialize LearnMode when it already exists');
-    }
-    LearnMode.instance = new LearnMode();
-
     // Export global objects from the background page context into this one.
     window.backgroundWindow = chrome.extension.getBackgroundPage();
     window.ChromeVox = window.backgroundWindow['ChromeVox'];
@@ -55,6 +45,8 @@ export class LearnMode {
     chrome.accessibilityPrivate.setKeyboardListener(true, true);
     BackgroundBridge.BrailleCommandHandler.setEnabled(false);
     BackgroundBridge.GestureCommandHandler.setEnabled(false);
+
+    ChromeVoxKbHandler.commandHandler = LearnMode.onCommand;
 
     $('instruction').textContent = Msgs.getMsg('learn_mode_intro');
     LearnMode.shouldFlushSpeech_ = true;
@@ -98,8 +90,7 @@ export class LearnMode {
       BackgroundBridge.UserActionMonitor.onKeyDown(evt).then(
           (shouldPropagate) => {
             if (shouldPropagate) {
-              LearnMode.instance.keyboardHandler_.basicKeyDownActionsListener(
-                  evt);
+              ChromeVoxKbHandler.basicKeyDownActionsListener(evt);
             }
             LearnMode.clearRange();
           });
@@ -376,13 +367,3 @@ document.addEventListener('DOMContentLoaded', function() {
 function $(id) {
   return document.getElementById(id);
 }
-
-class LearnModeKeyboardHandler extends AbstractKeyboardHandler {
-  /** @override */
-  handleCommand(command) {
-    LearnMode.onCommand(command);
-  }
-}
-
-/** @type {LearnMode} */
-LearnMode.instance;
