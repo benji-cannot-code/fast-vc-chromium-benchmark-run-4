cb2967c9f2e3e7f2976f8b7de72c38cb112faf68
Bruno Pitrus <brunopitrus@hotmail.com>
2022-11-03 13:43:09 +0000

    Correct ODR violation
    Types inside anonymous namespaces have internal linkage and cannot match across translation units.
    
    Change-Id: I57182a3788a997070b5656e2177a84abbfca0cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998576
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066970}

--- components/browsing_data/core/counters/passwords_counter.cc
+++ components/browsing_data/core/counters/passwords_counter.cc
@@ -35,6 +35,8 @@ bool IsPasswordSyncEnabled(const syncer::SyncService* sync_service) {
   }
 }
 
+}  // namespace
+
 // PasswordStoreFetcher ----------------------------------
 
 // Fetches passswords and observes a PasswordStoreInterface.
@@ -168,8 +170,6 @@ void PasswordStoreFetcher::CancelAllRequests() {
   cancelable_task_tracker()->TryCancelAll();
   weak_ptr_factory_.InvalidateWeakPtrs();
 }
-
-}  // namespace
 
 // PasswordsCounter::PasswordsResult ----------------------------------
 PasswordsCounter::PasswordsResult::PasswordsResult(
