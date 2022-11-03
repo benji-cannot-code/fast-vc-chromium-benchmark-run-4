005767db1b6de05ad39498d7292a57fc74f7e4ec
Anastasia Helfinstein <anastasi@google.com>
2022-11-03 02:05:09 +0000

    [ChromeVox] Untangle common/keyboard_handler.js
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: I9e39682f9a12cf2dba6fb293012f46e4c95ee4cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969583
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066843}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/learn_mode.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+import {AbstractKeyboardHandler} from '../common/abstract_keyboard_handler.js';
 import {BackgroundBridge} from '../common/background_bridge.js';
 import {BrailleCommandData} from '../common/braille/braille_command_data.js';
 import {BrailleKeyCommand, BrailleKeyEvent} from '../common/braille/braille_key_types.js';
@@ -16,7 +17,6 @@ import {Command, CommandStore} from '../common/command_store.js';
 import {GestureCommandData} from '../common/gesture_command_data.js';
 import {KeyMap} from '../common/key_map.js';
 import {KeyUtil} from '../common/key_util.js';
-import {ChromeVoxKbHandler} from '../common/keyboard_handler.js';
 import {Msgs} from '../common/msgs.js';
 import {Spannable} from '../common/spannable.js';
 import {QueueMode, TtsSpeechProperties} from '../common/tts_interface.js';
@@ -25,10 +25,20 @@ import {QueueMode, TtsSpeechProperties} from '../common/tts_interface.js';
  * Class to manage the keyboard explorer.
  */
 export class LearnMode {
+  constructor() {
+    /** @private {!LearnModeKeyboardHandler} */
+    this.keyboardHandler_ = new LearnModeKeyboardHandler();
+  }
+
   /**
    * Initialize keyboard explorer.
    */
   static async init() {
+    if (LearnMode.instance) {
+      throw new Error('Cannot initialize LearnMode when it already exists');
+    }
+    LearnMode.instance = new LearnMode();
+
     // Export global objects from the background page context into this one.
     window.backgroundWindow = chrome.extension.getBackgroundPage();
     window.ChromeVox = window.backgroundWindow['ChromeVox'];
@@ -45,8 +55,6 @@ export class LearnMode {
     chrome.accessibilityPrivate.setKeyboardListener(true, true);
     BackgroundBridge.BrailleCommandHandler.setEnabled(false);
     BackgroundBridge.GestureCommandHandler.setEnabled(false);
-
-    ChromeVoxKbHandler.commandHandler = LearnMode.onCommand;
 
     $('instruction').textContent = Msgs.getMsg('learn_mode_intro');
     LearnMode.shouldFlushSpeech_ = true;
@@ -90,7 +98,8 @@ export class LearnMode {
       BackgroundBridge.UserActionMonitor.onKeyDown(evt).then(
           (shouldPropagate) => {
             if (shouldPropagate) {
-              ChromeVoxKbHandler.basicKeyDownActionsListener(evt);
+              LearnMode.instance.keyboardHandler_.basicKeyDownActionsListener(
+                  evt);
             }
             LearnMode.clearRange();
           });
@@ -367,3 +376,13 @@ document.addEventListener('DOMContentLoaded', function() {
 function $(id) {
   return document.getElementById(id);
 }
+
+class LearnModeKeyboardHandler extends AbstractKeyboardHandler {
+  /** @override */
+  handleCommand(command) {
+    LearnMode.onCommand(command);
+  }
+}
+
+/** @type {LearnMode} */
+LearnMode.instance;
