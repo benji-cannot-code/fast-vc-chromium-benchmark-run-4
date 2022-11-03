da1d743974eb8d75f6f583e8151af113eb74210e
Hailey Wang <haileywang@google.com>
2022-11-03 22:07:30 +0000

    Disable flaky test in AutofillAssistantCollectUserData
    Bug: 1381245
    Change-Id: I2264b7074a7f94272d6f0b3df2976a5c712387d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003992
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067256}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantCollectUserDataIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantCollectUserDataIntegrationTest.java
@@ -1060,6 +1060,7 @@ public class AutofillAssistantCollectUserDataIntegrationTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1381245")
     public void testCreateAndEnterTransientContact() throws Exception {
         GetUserDataResponseProto userData =
                 GetUserDataResponseProto.newBuilder()
