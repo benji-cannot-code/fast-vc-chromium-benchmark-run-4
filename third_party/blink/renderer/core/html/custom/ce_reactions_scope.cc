ec223f47387319acf0118c31249c1bed0e8cf98f
Dave Tapuska <dtapuska@chromium.org>
2022-11-03 23:59:31 +0000

    Convert the CustomElementReactionStack to be Agent supplement data.
    This implementation now matches the specification text.
    - Removed the stack CEReactionScope static variable and always push
    an element on the stack.
    - Associate CustomElementReactionStack with Supplement<Agent>
    - Remove CustomElementReactionStack member from CustomElementRegistry as
    it seems unused.
    
    This is a reland of crrev.com/4bc40abe but without the dependant
    change of CEReactionsScope looking up the current ExecutionContext.
    Looking up the current ExecutionContext caused a bindings perf
    regression and to avoid that we keep the `top_of_stack_` variable
    around but only associated CustomElementReactionStack when we have
    something to do.
    
    BUG=961186
    
    Change-Id: Id1350d7e38c65f539e18c44e5b3381d0a119c617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4002888
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067316}

--- third_party/blink/renderer/core/html/custom/ce_reactions_scope.cc
+++ third_party/blink/renderer/core/html/custom/ce_reactions_scope.cc
@@ -7,24 +7,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element_reaction_stack.h"
 
 namespace blink {
 
 CEReactionsScope* CEReactionsScope::top_of_stack_ = nullptr;
 
-void CEReactionsScope::EnqueueToCurrentQueue(Element& element,
-                                             CustomElementReaction& reaction) {
-  if (!work_to_do_) {
-    work_to_do_ = true;
-    CustomElementReactionStack::Current().Push();
-  }
-  CustomElementReactionStack::Current().EnqueueToCurrentQueue(element,
-                                                              reaction);
+// static
+CEReactionsScope* CEReactionsScope::Current() {
+  DCHECK(IsMainThread());
+  return top_of_stack_;
 }
 
-void CEReactionsScope::InvokeReactions() {
-  CustomElementReactionStack::Current().PopInvokingReactions();
+CEReactionsScope::CEReactionsScope() : prev_(top_of_stack_) {
+  // For speed of the bindings we use a global variable to determine if
+  // we have a CEReactionScope. We check that this is only on the main thread
+  // otherwise this global variable will have collisions.
+  DCHECK(IsMainThread());
+  top_of_stack_ = this;
+}
+
+CEReactionsScope::~CEReactionsScope() {
+  if (stack_)
+    stack_->PopInvokingReactions();
+  top_of_stack_ = top_of_stack_->prev_;
+}
+
+void CEReactionsScope::EnqueueToCurrentQueue(CustomElementReactionStack& stack,
+                                             Element& element,
+                                             CustomElementReaction& reaction) {
+  if (!stack_)
+    stack.Push();
+  stack_ = &stack;
+  stack.EnqueueToCurrentQueue(element, reaction);
 }
 
 }  // namespace blink
