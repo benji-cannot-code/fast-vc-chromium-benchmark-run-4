33e768d1edfb7d8da360f48045ebd9ecd91f45ba
Andrew Rayskiy <greengrape@google.com>
2022-11-03 17:11:04 +0000

    [PreinstalledWebApps] Add Preinstalled Web Apps support for PinnedLauncherApps policy
    Change-Id: I21c1be7754ce8e00f29ffa80a016622e058cb24e
    Bug: 1371300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945016
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067079}

--- chrome/browser/apps/app_service/policy_util.h
+++ chrome/browser/apps/app_service/policy_util.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    * Arc Apps
 //    * Chrome Apps
 //    * System Web Apps
+//    * Preinstalled Web Apps
 
 #ifndef CHROME_BROWSER_APPS_APP_SERVICE_POLICY_UTIL_H_
 #define CHROME_BROWSER_APPS_APP_SERVICE_POLICY_UTIL_H_
@@ -43,6 +44,9 @@ bool IsWebAppPolicyId(const std::string& policy_id);
 // Checks whether |policy_id| specifies a System Web App.
 bool IsSystemWebAppPolicyId(const std::string& policy_id);
 
+// Checks whether |policy_id| specifies a Preinstalled Web App.
+bool IsPreinstalledWebAppPolicyId(const std::string& policy_id);
+
 // Transforms the provided |raw_policy_id| if necessary.
 // For Web Apps, converts it to GURL and returns the spec().
 // Does nothing for other app types.
@@ -71,8 +75,8 @@ absl::optional<std::string> GetAppIdFromPolicyId(Profile*,
 // GetPolicyIdsFromAppId() with "aaa111" will return {"aaa111"}.
 //
 // System Web App Example:
-// Chrome generates apps ids for all preinstalled System Web Apps -- let's say
-// the id of the Camera app is "hfhhnacclhffhdffklopdkcgdhifgngh". Calling
+// Chrome generates apps ids for all System Web Apps -- let's say the id of the
+// Camera app is "hfhhnacclhffhdffklopdkcgdhifgngh". Calling
 // GetPolicyIdsFromAppId() with "hfhhnacclhffhdffklopdkcgdhifgngh" will return
 // {"camera"}.
 absl::optional<std::vector<std::string>> GetPolicyIdsFromAppId(
