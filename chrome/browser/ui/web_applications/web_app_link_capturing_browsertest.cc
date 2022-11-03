3758621155cd0dc2ba3addc1c4c15d1c1a265116
Dibyajyoti Pal <dibyapal@chromium.org>
2022-11-03 22:08:32 +0000

    [dPWA] Retire ManifestUpdateTask
    This CL retires the ManifestUpdateTask now that the
    ManifestUpdate commands have been merged. Also removes any dependencies to the task and updates the documentation to point to the new code.
    
    Bug: 1298130, b/254486239
    Change-Id: I7ddb1f96f55fbd4871cb5aa7d32006be04d55d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939947
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067257}

--- chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
-#include "chrome/browser/web_applications/manifest_update_manager.h"
-#include "chrome/browser/web_applications/manifest_update_task.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/test/web_app_test_observers.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
--- chrome/browser/web_applications/web_app_install_finalizer.cc
+++ chrome/browser/web_applications/web_app_install_finalizer.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/commands/web_app_uninstall_command.h"
 #include "chrome/browser/web_applications/isolation_prefs_utils.h"
-#include "chrome/browser/web_applications/manifest_update_task.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcuts_menu.h"
 #include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
