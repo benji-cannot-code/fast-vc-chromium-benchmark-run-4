101029f9a6c0cebea2a8b903a5b3f9070bd51121
Hailey Wang <haileywang@google.com>
2022-11-03 00:51:12 +0000

    Segmentation internal changes on stats.cc
    Change model related histogram to directly use segment id.
    
    Add concept of model type for each model return types.
    
    Bug: 1364224
    Change-Id: I1c0f12849a3c8e85ada2290b623434c75bc72c9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928464
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066821}

--- components/segmentation_platform/internal/scheduler/model_execution_scheduler_impl.cc
+++ components/segmentation_platform/internal/scheduler/model_execution_scheduler_impl.cc
@@ -94,7 +94,6 @@ void ModelExecutionSchedulerImpl::OnModelExecutionCompleted(
     segment_result.set_result(result->score);
     segment_result.set_timestamp_us(
         clock_->Now().ToDeltaSinceWindowsEpoch().InMicroseconds());
-    stats::RecordModelScore(segment_id, result->score);
   }
 
   segment_database_->SaveSegmentResult(
