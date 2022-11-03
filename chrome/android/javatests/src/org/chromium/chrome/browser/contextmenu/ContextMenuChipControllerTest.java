bf4356da53df83919f684fcf71e485a08760e035
Juan Mojica <juanmojica@google.com>
2022-11-03 23:57:28 +0000

    Re-enable tests in ContextMenuChipControllerTest.
    Bug: 1381197
    Change-Id: I97cb11e64e837e21c6cee69e780fe4e25281e486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004232
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067315}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuChipControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuChipControllerTest.java
@@ -24,7 +24,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -143,7 +142,6 @@ public class ContextMenuChipControllerTest extends BlankUiTestActivityTestCase {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1381197")
     public void testExpectedChipTextMaxWidthPx() {
         ContextMenuChipController chipController =
                 new ContextMenuChipController(getActivity(), mAnchorView, mMockDismissRunnable);
@@ -154,7 +152,6 @@ public class ContextMenuChipControllerTest extends BlankUiTestActivityTestCase {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1381197")
     public void testExpectedChipTextMaxWidthPx_EndButtonHidden() {
         ContextMenuChipController chipController =
                 new ContextMenuChipController(getActivity(), mAnchorView, mMockDismissRunnable);
