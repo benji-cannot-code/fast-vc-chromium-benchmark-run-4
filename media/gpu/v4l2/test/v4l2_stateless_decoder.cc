e4acc5538aab45f2fa3f8b5f315869d9a8d172df
Hang Nguyen <hnt@chromium.org>
2022-11-03 18:02:30 +0000

    media/gpu/v4l2/test: Add VP8 Decoder for V4L2 Platform Decoding
    This CL adds class initialization to extend VP8 support in v4l2
    stateless, including allocating iotcl requests, OUTPUT and CAPTURE
    queue. It also implements |VP8Decoder::Create|,
    |VP8Decoder::DecodeNextFrame| functions, which correctly parses and
    decodes frames from the bitstream.
    
    BUG=b:246647671
    TEST=./v4l2_stateless_decoder --video=vp80-01-intra-1400_20210719.ivf
    --v=4 --frames=1 on asurada
    
    Change-Id: I57135f12bf6a7a38774a7554bcaa0de7c0b71393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984461
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067112}

--- media/gpu/v4l2/test/v4l2_stateless_decoder.cc
+++ media/gpu/v4l2/test/v4l2_stateless_decoder.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "media/gpu/v4l2/test/h264_decoder.h"
 #include "media/gpu/v4l2/test/video_decoder.h"
+#include "media/gpu/v4l2/test/vp8_decoder.h"
 #include "media/gpu/v4l2/test/vp9_decoder.h"
 
 // AV1 stateless decoding not supported upstream yet
@@ -29,6 +30,7 @@ using media::v4l2_test::Av1Decoder;
 #endif
 using media::v4l2_test::H264Decoder;
 using media::v4l2_test::VideoDecoder;
+using media::v4l2_test::Vp8Decoder;
 using media::v4l2_test::Vp9Decoder;
 
 namespace {
@@ -105,6 +107,9 @@ std::unique_ptr<VideoDecoder> CreateVideoDecoder(
 
   if (!decoder)
     decoder = H264Decoder::Create(stream);
+
+  if (!decoder)
+    decoder = Vp8Decoder::Create(stream);
 
   return decoder;
 }
