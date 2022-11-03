c0df2588165cb80ed407a0b6712ee71f0571444a
Lei Zhang <thestig@chromium.org>
2022-11-03 16:38:47 +0000

    Add some missing check_op.h includes in ash/public.
    Code that use CHECK_EQ() should include check_op.h instead of relying on
    transitive includes.
    
    Change-Id: I09fedb465c67979dff1c49f782fbf94dbe1f3e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4001403
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067060}

--- ash/public/cpp/clipboard_image_model_factory.cc
+++ ash/public/cpp/clipboard_image_model_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/clipboard_image_model_factory.h"
 
+#include "base/check_op.h"
+
 namespace ash {
 
 namespace {
