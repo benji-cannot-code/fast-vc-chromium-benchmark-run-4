1fd21328e8098d43c507cc4298ec0c3d90ff361f
Morten Stenshorne <mstensho@chromium.org>
2022-11-03 10:19:49 +0000

    Don't mark LayoutInline clean if side-effects are disabled.
    Doing that might lead to a bad cache hit later on. This could cause both
    correctness issues and crashes.
    
    Bug: 1379473
    Change-Id: I7e703197c95e674d3966296b0cd6f5892c834874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000663
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066910}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_disable_side_effects_scope.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_floats_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
@@ -118,7 +119,8 @@ NGInlineBoxState* NGInlineLayoutAlgorithm::HandleCloseTag(
   // Just clear |NeedsLayout| flags. Culled inline boxes do not need paint
   // invalidations. If this object produces box fragments,
   // |NGInlineBoxStateStack| takes care of invalidations.
-  item.GetLayoutObject()->ClearNeedsLayoutWithoutPaintInvalidation();
+  if (!NGDisableSideEffectsScope::IsDisabled())
+    item.GetLayoutObject()->ClearNeedsLayoutWithoutPaintInvalidation();
   return box;
 }
 
