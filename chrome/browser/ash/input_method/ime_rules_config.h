86b3dd0ccae139ba219fa08ec1a2cab89d5b4ada
Zac Partridge <zacpartridge@google.com>
2022-11-03 22:10:46 +0000

    autocorrect: disable on urls domain agnostically (not exact match)
    previously the implementation used an exact match for the denylist
    Example, "amazon.com" is in the list but not "amazon.com.au".
    The change now excludes the registry (.com or .com.au or .co.uk or ...)
    when comparing. Relevant tests added.
    
    Bug: b:253512401
    Change-Id: Ib1d38f9085010cddfccaabafb1877fc5323b2001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3954950
    Commit-Queue: Zac Partridge <zacpartridge@google.com>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067261}

--- chrome/browser/ash/input_method/ime_rules_config.h
+++ chrome/browser/ash/input_method/ime_rules_config.h
@@ -33,6 +33,11 @@ class ImeRulesConfig {
   // Runs the rule check against contextual info.
   bool IsAutoCorrectDisabled(const TextFieldContextualInfo& info);
 
+  // Checks if domain is a sub-domain of info
+ private:
+  bool IsSubDomain(const TextFieldContextualInfo& info,
+                   const std::string& domain);
+
  private:
   ImeRulesConfig();
 
@@ -40,6 +45,8 @@ class ImeRulesConfig {
   friend struct base::DefaultSingletonTraits<ImeRulesConfig>;
 
   friend class ImeRulesConfigTest;
+  friend class ImeRulesConfigEnabledTest;
+  friend class ImeRulesConfigDisabledTest;
 
   // Initializes the config from IME rules trial parameters. If there is
   // no trial or parsing fails, the rules will be empty and as such always
@@ -50,20 +57,20 @@ class ImeRulesConfig {
   std::vector<std::string> rule_auto_correct_domain_denylist_;
   // The default denylist of domains that will turn off auto_correct feature.
   std::vector<std::string> default_auto_correct_domain_denylist_{
-      "amazon.com",
-      "b.corp.google.com",
-      "buganizer.corp.google.com",
-      "cider.corp.google.com",
-      "classroom.google.com",
-      "desmos.com",
-      "docs.google.com",
-      "facebook.com",
-      "instagram.com",
-      "outlook.live.com",
-      "outlook.office.com",
-      "quizlet.com",
-      "whatsapp.com",
-      "youtube.com",
+      "amazon",
+      "b.corp.google",
+      "buganizer.corp.google",
+      "cider.corp.google",
+      "classroom.google",
+      "desmos",
+      "docs.google",
+      "facebook",
+      "instagram",
+      "outlook.live",
+      "outlook.office",
+      "quizlet",
+      "whatsapp",
+      "youtube",
   };
 };
 
