eced3ac4289e74ad266fcd17598f2ac0a2b1c7ed
Katie Dektar <katie@chromium.org>
2022-11-04 21:29:53 +0000

    [ATP] Support gin bindings for Automation.
    ATP will re-implement the V8 bindings for Automation and other
    extension APIs used by accessibility feature component extensions.
    This change makes it easier to re-use existing bindings code:
    *Allows ATP to use gin::CreateFunctionTemplate while extensions
    continues to use v8::FunctionTemplate::New
    *Requires a change to add a way to get the original
    V8::FunctionCallbackInfo from a gin::Arguments.
    
    If we don't add the getter to gin::Arguments, all bindings code
    across all extensions used by Accessibility Features would need
    to be updated to take a gin::Bindings, or we'd need to build
    a custom way outside of gin::Arguments to get the
    FunctionCallbackInfo.
    
    For diagrams and high-level overview of the context of this
    change see go/chromeos-atp-v8-design (WIP).
    
    Bug: 1357889
    AX-Relnotes: N/A
    Test: Existing
    Change-Id: If4552a4c08ae8a32a2c0ee9288891dcd8c8a64a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3985889
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067695}

---
+++ ui/accessibility/platform/automation/v8_handler_function_wrapper.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/accessibility/platform/automation/v8_handler_function_wrapper.h"
+
+#include "gin/arguments.h"
+
+namespace ui {
+
+void V8HandlerFunctionWrapper::RunV8(
+    const v8::FunctionCallbackInfo<v8::Value>& args) {
+  gin::Arguments arguments(args);
+  Run(&arguments);
+}
+
+}  // namespace ui
