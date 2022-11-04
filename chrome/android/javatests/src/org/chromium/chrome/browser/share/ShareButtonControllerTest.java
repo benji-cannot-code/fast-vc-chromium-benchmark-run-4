0268d9a330f6e6903bc8aeee998fdd598a750749
Brandon Wylie <wylieb@chromium.org>
2022-11-04 21:38:51 +0000

    [sheriff] Disable failling ShareButtonControllerTest
    Bug: 1381572
    Change-Id: I027ba616f9dbc11635d0f336e92af3bf2d800c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005575
    Reviewed-by: bttk - <bttk@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: bttk - <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067700}

--- chrome/android/javatests/src/org/chromium/chrome/browser/share/ShareButtonControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/share/ShareButtonControllerTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -130,6 +131,7 @@ public final class ShareButtonControllerTest {
     @CommandLineFlags.Add({"force-fieldtrial-params=Study.Group:start_surface_variation/single"})
     @Restriction(
             {UiRestriction.RESTRICTION_TYPE_PHONE, Restriction.RESTRICTION_TYPE_NON_LOW_END_DEVICE})
+    @DisabledTest(message = "crbug.com/1381572")
     public void
     testShareButtonInToolbarNotAffectedByOverview() throws TimeoutException {
         // Sign in.
@@ -160,6 +162,7 @@ public final class ShareButtonControllerTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1381572")
     public void testShareButtonInToolbarIsDisabledOnUpdate() {
         View experimentalButton = mActivityTestRule.getActivity()
                                           .getToolbarManager()
