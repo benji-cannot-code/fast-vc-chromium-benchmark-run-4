442768977205b58f218027e5cd43a7b6106359b1
Jesse McKenna <jessemckenna@google.com>
2022-11-04 00:37:03 +0000

    Revert "Remove infobar, about:help warnings for Win 7/8.*"
    This reverts commit 56f5b6100cfe588d484be948104033031a66b7bc.
    
    Reason for revert: After discussion, the infobar was ultimately 
    determined to be fine and is therefore being re-added.
    
    Original change's description:
    > Remove infobar, about:help warnings for Win 7/8.*
    >
    > The existing infobar was determined to be too vague (crbug.com/1379272).
    > This change removes it while the team rewords the message to explicitly
    > state that M109 is the last supported version, i.e., that M110 will
    > require Win 10.
    >
    > Bug: 1377525, 1379272
    > Change-Id: I4d0b3485af84a9f8f87e341ff6b47fc508ae2817
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990633
    > Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1065043}
    
    Bug: 1377525, 1379272
    Change-Id: I9daa778210d23cc5135825c92a2f8dc4931ae373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004000
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067331}

--- chrome/browser/obsolete_system/obsolete_system_win.cc
+++ chrome/browser/obsolete_system/obsolete_system_win.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool IsObsoleteOsVersion() {
-  return base::win::GetVersion() < base::win::Version::WIN7;
+  return base::win::GetVersion() < base::win::Version::WIN10;
 }
 
 }  // namespace
@@ -28,6 +28,13 @@ bool ObsoleteSystem::IsObsoleteNowOrSoon() {
 
 // static
 std::u16string ObsoleteSystem::LocalizedObsoleteString() {
+  const auto version = base::win::GetVersion();
+  if (version == base::win::Version::WIN7)
+    return l10n_util::GetStringUTF16(IDS_WIN_7_OBSOLETE);
+  if (version == base::win::Version::WIN8)
+    return l10n_util::GetStringUTF16(IDS_WIN_8_OBSOLETE);
+  if (version == base::win::Version::WIN8_1)
+    return l10n_util::GetStringUTF16(IDS_WIN_8_1_OBSOLETE);
   return l10n_util::GetStringUTF16(IDS_WIN_XP_VISTA_OBSOLETE);
 }
 
@@ -38,5 +45,8 @@ bool ObsoleteSystem::IsEndOfTheLine() {
 
 // static
 const char* ObsoleteSystem::GetLinkURL() {
-  return chrome::kWindowsXPVistaDeprecationURL;
+  const auto version = base::win::GetVersion();
+  if (version < base::win::Version::WIN7)
+    return chrome::kWindowsXPVistaDeprecationURL;
+  return chrome::kWindows78DeprecationURL;
 }
