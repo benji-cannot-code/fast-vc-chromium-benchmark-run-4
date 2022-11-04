6e0727e5828d5cd30569c8e8b001f6d8358eb1d4
Alexander Bolodurin <alexbn@chromium.org>
2022-11-04 02:58:39 +0000

    fileSystemProvider: remove unnecessary test file
    Remove FILE_BLOCKS_FOREVER, FILE_BLOCK_IO can always be used instead.
    Also rename FILE_BLOCK_OPEN to be consistent.
    
    Bug: b:235414571
    Change-Id: I4d735134b7771d389874d948613f53182d035524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005403
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067381}

--- chrome/test/data/extensions/api_test/file_system_provider/service_worker/get_all/test.js
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/get_all/test.js
@@ -16,7 +16,7 @@ async function main() {
       const fileSystem = await mountTestFileSystem({openedFilesLimit: 2});
       // Start a read so we have at least one file open.
       const fileEntry = await fileSystem.getFileEntry(
-          TestFileSystemProvider.FILE_BLOCKS_FOREVER, {create: false});
+          TestFileSystemProvider.FILE_BLOCK_IO, {create: false});
       const file = await openFile(fileEntry);
       startReadTextFromBlob(file);
       await remoteProvider.waitForEvent('onOpenFileRequested');
@@ -31,7 +31,7 @@ async function main() {
       chrome.test.assertEq(2, fsInfos[0].openedFilesLimit);
       chrome.test.assertEq(1, fsInfos[0].openedFiles.length);
       chrome.test.assertEq(
-          `/${TestFileSystemProvider.FILE_BLOCKS_FOREVER}`,
+          `/${TestFileSystemProvider.FILE_BLOCK_IO}`,
           fsInfos[0].openedFiles[0].filePath);
       chrome.test.assertEq(
           chrome.fileSystemProvider.OpenFileMode.READ,
@@ -46,7 +46,7 @@ async function main() {
       chrome.test.assertEq(2, fsInfo.openedFilesLimit);
       chrome.test.assertEq(1, fsInfo.openedFiles.length);
       chrome.test.assertEq(
-          `/${TestFileSystemProvider.FILE_BLOCKS_FOREVER}`,
+          `/${TestFileSystemProvider.FILE_BLOCK_IO}`,
           fsInfo.openedFiles[0].filePath);
       chrome.test.assertEq(
           chrome.fileSystemProvider.OpenFileMode.READ,
--- chrome/test/data/extensions/api_test/file_system_provider/service_worker/read_file/test.js
+++ chrome/test/data/extensions/api_test/file_system_provider/service_worker/read_file/test.js
@@ -34,7 +34,7 @@ async function main() {
       const reads = [];
       for (let i = 0; i < N; i++) {
         const fileEntry = await fileSystem.getFileEntry(
-            TestFileSystemProvider.FILE_STALL_READ, {create: false});
+            TestFileSystemProvider.FILE_BLOCK_IO, {create: false});
         const file = await openFile(fileEntry);
         reads.push(readTextFromBlob(file));
       }
@@ -82,7 +82,7 @@ async function main() {
       await remoteProvider.resetState();
 
       const fileEntry = await fileSystem.getFileEntry(
-          TestFileSystemProvider.FILE_BLOCKS_FOREVER,
+          TestFileSystemProvider.FILE_BLOCK_IO,
           {create: false},
       );
       const file = await openFile(fileEntry);
@@ -109,7 +109,7 @@ async function main() {
       await remoteProvider.setHandlerEnabled('onAbortRequested', false);
 
       const fileEntry = await fileSystem.getFileEntry(
-          TestFileSystemProvider.FILE_BLOCKS_FOREVER,
+          TestFileSystemProvider.FILE_BLOCK_IO,
           {create: false, exclusive: false},
       );
       const file = await openFile(fileEntry);
@@ -138,7 +138,7 @@ async function main() {
 
       // Start reading two files, the first read should get stuck on open.
       const fileEntry1 = await fileSystem.getFileEntry(
-          TestFileSystemProvider.FILE_STALL_OPEN,
+          TestFileSystemProvider.FILE_BLOCK_OPEN,
           {create: false, exclusive: false},
       );
       const fileEntry2 = await fileSystem.getFileEntry(
