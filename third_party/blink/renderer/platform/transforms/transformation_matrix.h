17a1942c202b590cae11aa375fb06fbf08d7f1ea
Xianzhu Wang <wangxianzhu@chromium.org>
2022-11-04 22:52:43 +0000

    Remove blink::TransformationMatrix::MakeAffine()
    It was used only in PaintLayer::UpdateTransformationMatrix() when
    compositing is not enabled. This looks unnecessary (or even
    incorrect). This doesn't affect rendering which always uses paint
    properties, and the transform will be flattened when needed.
    
    The callsite of MakeAffine() was added in 2009 in
    https://bugs.webkit.org/show_bug.cgi?id=24396 when WebKit was
    adding 3d transform support. It seems that at that time 3d
    transforms were only supported when compositing was enabled.
    
    Bug: 1359528, 1381553
    Change-Id: I315bcdcfcb09661f23dee8a1d574812bb09c7dcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006815
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067749}

--- third_party/blink/renderer/platform/transforms/transformation_matrix.h
+++ third_party/blink/renderer/platform/transforms/transformation_matrix.h
@@ -320,9 +320,6 @@ class PLATFORM_EXPORT TransformationMatrix {
   bool IsAffine() const { return IsFlat() && !HasPerspective(); }
   bool Is2dTransform() const { return IsAffine(); }
 
-  // Throw away the non-affine parts of the matrix (lossy!)
-  void MakeAffine();
-
   [[nodiscard]] AffineTransform ToAffineTransform() const;
 
   // Same as gfx::Transform::Flatten(); see the docs for that function for
