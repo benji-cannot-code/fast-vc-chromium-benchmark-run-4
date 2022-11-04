a190fdf076a2f61a33c5ee9a381219db66d81160
Brandon Wylie <wylieb@chromium.org>
2022-11-04 18:40:44 +0000

    [sheriff] Disable failing TabbedNavigationBarColorControllerTest
    Bug: 1381509
    Change-Id: Ie5cc0eb0767eb364886bdb77a964b92abe84c835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006180
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067594}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
@@ -25,6 +25,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -108,6 +109,7 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1381509")
     public void testToggleFullscreen() throws TimeoutException {
         assertEquals("Navigation bar should be colorSurface before entering fullscreen mode.",
                 mRegularNavigationColor, mWindow.getNavigationBarColor());
