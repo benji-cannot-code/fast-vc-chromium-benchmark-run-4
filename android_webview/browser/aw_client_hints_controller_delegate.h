4cac489613ee9348665e98a938461f0bb48d55ca
Ari Chivukula <arichiv@chromium.org>
2022-11-04 21:08:02 +0000

    [WebView] (2) Implement AwClientHintsControllerDelegate {PersistClientHints}
    This CL adds the actual persistence needed for client hints to be sent
    on page load and also deletes them if all cookies are deleted as in the
    main browser. HostContentSettingsMap is used in the main browser, but
    this isn't used at all in webview, so PrefService seems the best
    substitute. The amount of data stored here shouldn't be huge, so this
    seems reasonable.
    
    Some assumptions/calculations:
    
    The average origin has an https scheme
    The average origin has no port
    The average length(origin) = 8 + length(domain)
    
    The maximum amount of client hints right now is 27
    (see web_client_hints_types.mojom)
    Most sites use few or none, but the amount can expand
    so let's call the future proof average 50
    The serialized length of a minified list of ints 0-49
    is 141 bytes
    
    The maximum of a dictionary entry for an average origin is thus:
    141 + 1 + 8 + length(domain) = 150 + length(domain)
    A safe likely maximum is 200 bytes per origin visited
    
    If we assume the average webview only visits 5-10 origins
    (only top level counts) before being cleared, then the size
    is then 1-2KB.
    
    This CL is part of a series:
    (1) Add scaffolding for persistent client hints
    (2) Implement AwClientHintsControllerDelegate {...}
    (3) Add integration test
    (4) Enable persistent client hints by default
    
    Design doc:
    https://docs.google.com/document/u/1/d/1r1AKHex1_UKh3wIp4ITkU4-J9-tdNZSOmUDeDVWw_AU/edit
    
    Bug: 921655
    Change-Id: I0a0cfe901522196e9c02ab22a3fcf238e137071e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995353
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067676}

--- android_webview/browser/aw_client_hints_controller_delegate.h
+++ android_webview/browser/aw_client_hints_controller_delegate.h
@@ -31,6 +31,10 @@ class Origin;
 
 namespace android_webview {
 
+namespace prefs {
+extern const char kClientHintsCachedPerOriginMap[];
+}  // namespace prefs
+
 class AwClientHintsControllerDelegate
     : public content::ClientHintsControllerDelegate {
  public:
