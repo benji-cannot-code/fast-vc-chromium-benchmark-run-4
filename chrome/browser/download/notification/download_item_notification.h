930496eecb556a08eae62ee5dba4539233c7205c
Yuki Awano <yawano@google.com>
2022-11-04 19:49:28 +0000

    Add a button to open a pdf file with Gallery app
    - Add a button in Download notification to open a pdf file with the
      Gallery app. See the attached bug for details.
    
          e.g. --enable-features=GalleryAppPdfEditNotification:text/Edit
    
    Bug: b:250018322
    Test: Manually tested with flags
    Change-Id: Ia9d296d122c58aebeb1668c6cbbc6d688b56b3dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004377
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067628}

--- chrome/browser/download/notification/download_item_notification.h
+++ chrome/browser/download/notification/download_item_notification.h
@@ -72,11 +72,7 @@ class DownloadItemNotification : public ImageDecoder::ImageRequest,
 
   enum ImageDecodeStatus { NOT_STARTED, IN_PROGRESS, DONE, FAILED, NOT_IMAGE };
 
-  enum NotificationUpdateType {
-    ADD,
-    UPDATE,
-    UPDATE_AND_POPUP
-  };
+  enum NotificationUpdateType { ADD, UPDATE, UPDATE_AND_POPUP };
 
   std::string GetNotificationId() const;
 
@@ -116,6 +112,8 @@ class DownloadItemNotification : public ImageDecoder::ImageRequest,
 
   bool IsScanning() const;
   bool AllowedToOpenWhileScanning() const;
+
+  bool IsGalleryAppPdfEditNotificationEligible() const;
 
   Browser* GetBrowser() const;
   Profile* profile() const;
