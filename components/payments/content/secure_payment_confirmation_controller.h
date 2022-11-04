f278b2554f7385d6602c19e68eb7c6417738c7ef
Rouslan Solomakhin <rouslan@chromium.org>
2022-11-04 14:11:39 +0000

    [Web Payment] Add comments about when a delegate can be null.
    Before this patch, the DocumentService object PaymentRequest (owned by
    the RenderFrameHost) passed its base::WeakPtr to other classes that it
    owns, but there was no comment about when the WeakPtr could be
    invalidated.
    
    This patch adds comments about when a WeakPtr of PaymentRequest can be
    invalid and removes an unused accessor to this WeakPtr.
    
    Bug: 1380096
    Change-Id: I50c7d4887a2bfe2a6a71f2228a0b05c6e296d311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999984
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067517}

--- components/payments/content/secure_payment_confirmation_controller.h
+++ components/payments/content/secure_payment_confirmation_controller.h
@@ -56,6 +56,7 @@ class SecurePaymentConfirmationController
  private:
   void SetupModelAndShowDialogIfApplicable();
 
+  // Can be null when the webpage closes or the iframe refreshes or navigates.
   base::WeakPtr<PaymentRequest> request_;
 
   SecurePaymentConfirmationModel model_;
