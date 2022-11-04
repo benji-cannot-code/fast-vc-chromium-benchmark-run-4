59c582cccda251dfdd97f1c8375aaf1e89998b32
Jackson Tadie <jacksontadie@google.com>
2022-11-04 22:55:07 +0000

    Block Demo Mode Chrome App installation additionally in Lacros
    When the DemoModeSWA feature is enabled, specific Demo Mode Chrome Apps
    should be blocked from installing from policy (because we're migrating
    to the Demo Mode SWA). This wasn't happening in Lacros due to the code
    being included only in ash. This CL causes the code to be included in both ash and lacros.
    
    Test: With lacros-browser only enabled, verify that only the Demo Mode
    SWA is launched at the beginning of every demo session
    Bug: b/257137556
    
    Change-Id: Iaf397fa19887956a47f3d56b99a569f68c18abbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000757
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067750}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -114,11 +114,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/constants/chromeos_features.h"
+#endif
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "base/system/sys_info.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/extensions/install_limiter.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "storage/browser/file_system/file_system_backend.h"
 #include "storage/browser/file_system/file_system_context.h"
 #endif
@@ -1912,14 +1915,14 @@ bool ExtensionService::OnExternalExtensionFileFound(
     return false;
   }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (chromeos::features::IsDemoModeSWAEnabled()) {
     if (extension_misc::IsDemoModeChromeApp(info.extension_id)) {
       pending_extension_manager()->Remove(info.extension_id);
       return true;
     }
   }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   // no client (silent install)
   scoped_refptr<CrxInstaller> installer(CrxInstaller::CreateSilent(this));
