942e1a8ace1c84325427ba244501fd9e0b00f3ce
Vasilii Sukhanov <vasilii@chromium.org>
2022-11-04 13:46:58 +0000

    Deprecate PasswordManager.CleanedUpPasswords histogram.
    The CL also prolongs PasswordManager.PasswordStoreInitResult.
    
    Fixed: 1377903,1379910
    Change-Id: Iba098bf3c3ba842717c68cb258e05d13f77177c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003358
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067506}

--- components/password_manager/core/browser/login_database.cc
+++ components/password_manager/core/browser/login_database.cc
@@ -1622,8 +1622,6 @@ DatabaseCleanupResult LoginDatabase::DeleteUndecryptableLogins() {
   } else {
     metrics_util::LogDeleteUndecryptableLoginsReturnValue(
         metrics_util::DeleteCorruptedPasswordsResult::kSuccessPasswordsDeleted);
-    UMA_HISTOGRAM_COUNTS_100("PasswordManager.CleanedUpPasswords",
-                             forms_to_be_deleted.size());
   }
 #endif
 
--- components/password_manager/core/browser/login_database_unittest.cc
+++ components/password_manager/core/browser/login_database_unittest.cc
@@ -1227,7 +1227,6 @@ TEST_F(LoginDatabaseTest, UpdateOverlappingCredentials) {
   complete_form.in_store = PasswordForm::Store::kProfileStore;
   incomplete_form.in_store = PasswordForm::Store::kProfileStore;
 
-
   // Both still exist now.
   EXPECT_TRUE(db().GetAutofillableLogins(&result));
   ASSERT_EQ(2U, result.size());
@@ -2121,16 +2120,10 @@ TEST_F(LoginDatabaseUndecryptableLoginsTest, DeleteUndecryptableLoginsTest) {
 
 // Check histograms.
 #if BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CASTOS))
-  histogram_tester.ExpectUniqueSample("PasswordManager.CleanedUpPasswords", 2,
-                                      1);
   histogram_tester.ExpectUniqueSample(
       "PasswordManager.DeleteUndecryptableLoginsReturnValue",
       metrics_util::DeleteCorruptedPasswordsResult::kSuccessPasswordsDeleted,
       1);
-#else
-  EXPECT_TRUE(
-      histogram_tester.GetAllSamples("PasswordManager.CleanedUpPasswords")
-          .empty());
 #endif
 }
 
@@ -2163,9 +2156,6 @@ TEST_F(LoginDatabaseUndecryptableLoginsTest, KeychainLockedTest) {
   EXPECT_EQ(DatabaseCleanupResult::kEncryptionUnavailable,
             db.DeleteUndecryptableLogins());
 
-  EXPECT_TRUE(
-      histogram_tester.GetAllSamples("PasswordManager.CleanedUpPasswords")
-          .empty());
   histogram_tester.ExpectUniqueSample(
       "PasswordManager.DeleteUndecryptableLoginsReturnValue",
       metrics_util::DeleteCorruptedPasswordsResult::kEncryptionUnavailable, 1);
