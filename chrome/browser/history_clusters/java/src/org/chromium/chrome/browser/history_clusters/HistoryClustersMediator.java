5cf8c6c5464e7b9b726090487cd99afe576f184c
Patrick Noland <pnoland@google.com>
2022-11-04 03:21:17 +0000

    [Journeys] Clear selection state when history is deleted externally
    Bug: 1380749
    Change-Id: Id6aec374a3dc8b42049524b11875ac6c27af3c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004070
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067388}

--- chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersMediator.java
+++ chrome/browser/history_clusters/java/src/org/chromium/chrome/browser/history_clusters/HistoryClustersMediator.java
@@ -361,6 +361,7 @@ class HistoryClustersMediator extends RecyclerView.OnScrollListener implements S
 
     void onHistoryDeletedExternally() {
         if (mQueryState == null) return;
+        mSelectionDelegate.clearSelection();
         resetModel();
         startQuery(mQueryState.getQuery());
     }
