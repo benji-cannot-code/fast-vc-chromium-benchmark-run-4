8686c61946080855a49df3518fe2311c2756a13c
Evan Stade <estade@chromium.org>
2022-11-04 00:42:13 +0000

    Storage buckets: clear expired buckets during eviction rounds
    Combine this with eviction in order to share the timing (polling) and
    to make sure zombie expired buckets aren't using up cache and causing
    us to evict non-expired buckets.
    
    Bug: 1315392
    Change-Id: Ib7a4ac2715aecba04fcbb8c119d99e0a7e97cb2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997427
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067334}

--- storage/browser/quota/quota_database_unittest.cc
+++ storage/browser/quota/quota_database_unittest.cc
@@ -1218,6 +1218,10 @@ TEST_P(QuotaDatabaseTest, Expiration) {
   ASSERT_TRUE(result.ok());
   EXPECT_TRUE(result->expiration.is_null());
 
+  QuotaErrorOr<std::set<BucketInfo>> expired_buckets = db.GetExpiredBuckets();
+  ASSERT_TRUE(expired_buckets.ok());
+  EXPECT_EQ(0U, expired_buckets->size());
+
   // Non-default `expiration` value.
   BucketInitParams params2(
       StorageKey::CreateFromStringForTesting("http://example/"),
@@ -1228,10 +1232,24 @@ TEST_P(QuotaDatabaseTest, Expiration) {
   EXPECT_EQ(params2.expiration, result->expiration);
 
   // Update `expiration` value.
-  const base::Time updated_time = base::Time::Now() + base::Days(1);
+  base::Time updated_time = base::Time::Now() + base::Days(1);
   result = db.UpdateBucketExpiration(result->id, updated_time);
   ASSERT_TRUE(result.ok());
   EXPECT_EQ(updated_time, result->expiration);
+
+  expired_buckets = db.GetExpiredBuckets();
+  ASSERT_TRUE(expired_buckets.ok());
+  EXPECT_EQ(0U, expired_buckets->size());
+
+  // Set expiration to the past.
+  updated_time = base::Time::Now() - base::Days(1);
+  result = db.UpdateBucketExpiration(result->id, updated_time);
+  ASSERT_TRUE(result.ok());
+  EXPECT_EQ(updated_time, result->expiration);
+
+  expired_buckets = db.GetExpiredBuckets();
+  ASSERT_TRUE(expired_buckets.ok());
+  EXPECT_EQ(1U, expired_buckets->size());
 }
 
 TEST_P(QuotaDatabaseTest, Persistent) {
