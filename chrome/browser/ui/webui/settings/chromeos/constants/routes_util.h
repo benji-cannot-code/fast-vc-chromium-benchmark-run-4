2be538a988fa4c6acea67d07b5f1014d1fa564cb
Xiaohui Chen <xiaohuic@google.com>
2022-11-04 20:23:50 +0000

    os settings: autogenerated route paths collection
    Previously the route paths collection is manually defined in
    routes_util.cc.  It is not kept up-to-date and error prone. Now we
    genearte the collection based on the routes.mojom parser result
    automatically.
    
    Bug: None
    Test: unit_tests --gtest_filter=RoutesUtilTest.*
    Change-Id: I1124961cf9fa79416dd209ead6ee97bbc54e9b26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993877
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067643}

--- chrome/browser/ui/webui/settings/chromeos/constants/routes_util.h
+++ chrome/browser/ui/webui/settings/chromeos/constants/routes_util.h
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-namespace chromeos {
-namespace settings {
+namespace chromeos::settings {
 
 // TODO(khorimoto): Remove this path, since it's no longer used.
 extern const char kOsSignOutSubPage[];
@@ -17,7 +16,6 @@ extern const char kOsSignOutSubPage[];
 // Returns true if the sub-page is one of the above.
 bool IsOSSettingsSubPage(const std::string& sub_page);
 
-}  // namespace settings
-}  // namespace chromeos
+}  // namespace chromeos::settings
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SETTINGS_CHROMEOS_CONSTANTS_ROUTES_UTIL_H_
---
+++ chrome/browser/ui/webui/settings/chromeos/constants/routes_util_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "chrome/browser/ui/webui/settings/chromeos/constants/routes_util.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos::settings {
+
+namespace {
+
+class RoutesUtilTest : public testing::Test {};
+
+TEST_F(RoutesUtilTest, WellKnownRoutesAreValid) {
+  ASSERT_TRUE(IsOSSettingsSubPage("help"));
+  ASSERT_TRUE(IsOSSettingsSubPage("internet"));
+  ASSERT_TRUE(IsOSSettingsSubPage("power"));
+  ASSERT_TRUE(IsOSSettingsSubPage("networks"));
+  ASSERT_TRUE(IsOSSettingsSubPage("networks?type=Tether"));
+  ASSERT_TRUE(IsOSSettingsSubPage("networkDetail?guid=123456"));
+}
+
+TEST_F(RoutesUtilTest, BadRoutesAreValid) {
+  ASSERT_FALSE(IsOSSettingsSubPage("a_wrong_url"));
+  ASSERT_FALSE(IsOSSettingsSubPage("a_wrong_url?param=bad_param"));
+}
+
+}  // namespace
+
+}  // namespace chromeos::settings
