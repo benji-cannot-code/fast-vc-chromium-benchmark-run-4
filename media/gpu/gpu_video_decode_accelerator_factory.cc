9af6074479c84537485808b99fd352fc1ae935ae
Chen-Yu Tsai <wenst@chromium.org>
2022-11-04 11:28:45 +0000

    media/gpu: Re-enable V4L2 decoders for standard Linux if built
    Previously the code that interfaces with V4L2 decoder classes were
    only guarded by the "USE_V4L2_CODEC" build flag. This implied the
    that as long as the build flag was set, it could be built and run
    on standard Linux.
    
    Recently code was added to guard it to only use it for ChromeOS
    Ash (and not for LaCrOS), which inadvertently disabled the code
    for Linux as well.
    
    As we want to develop the V4L2 decoders on standard Linux in the
    future, fix up the build flag guards so that it works when built
    for Linux with "use_v4l2_codec=true" manually specified.
    
          Build for standard Linux with "use_v4l2_codec=true" GN arg.
          VDA test should be able to find V4L2 decoder and try to use it,
          but likely fail (due to other reasons)
    
    Bug: None
    Test: Extract newer "linux-libc-dev" >= 5.17 package to sysroot, then
    Change-Id: Ie670e0a25ec6567dafc60c080a892905e2384d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3996068
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067480}

--- media/gpu/gpu_video_decode_accelerator_factory.cc
+++ media/gpu/gpu_video_decode_accelerator_factory.cc
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(USE_VAAPI)
 #include "media/gpu/vaapi/vaapi_video_decode_accelerator.h"
 #include "ui/gl/gl_implementation.h"
-#elif BUILDFLAG(USE_V4L2_CODEC) && BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(USE_V4L2_CODEC) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH))
 #include "media/gpu/v4l2/v4l2_device.h"
 #include "media/gpu/v4l2/v4l2_slice_video_decode_accelerator.h"
 #include "media/gpu/v4l2/v4l2_video_decode_accelerator.h"
@@ -61,7 +62,8 @@ gpu::VideoDecodeAcceleratorCapabilities GetDecoderCapabilitiesInternal(
 #if BUILDFLAG(USE_VAAPI)
   capabilities.supported_profiles =
       VaapiVideoDecodeAccelerator::GetSupportedProfiles();
-#elif BUILDFLAG(USE_V4L2_CODEC) && BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(USE_V4L2_CODEC) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH))
   GpuVideoAcceleratorUtil::InsertUniqueDecodeProfiles(
       V4L2VideoDecodeAccelerator::GetSupportedProfiles(),
       &capabilities.supported_profiles);
@@ -100,7 +102,8 @@ GpuVideoDecodeAcceleratorFactory::GetDecoderCapabilities(
   static gpu::VideoDecodeAcceleratorCapabilities capabilities =
       GetDecoderCapabilitiesInternal(gpu_preferences, workarounds);
 
-#if BUILDFLAG(USE_V4L2_CODEC) && BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(USE_V4L2_CODEC) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH))
   // V4L2-only: the decoder devices may not be visible at the time the GPU
   // process is starting. If the capabilities vector is empty, try to query the
   // devices again in the hope that they will have appeared in the meantime.
@@ -145,7 +148,8 @@ GpuVideoDecodeAcceleratorFactory::CreateVDA(
   // both. In those cases prefer the VA creation function.
 #if BUILDFLAG(USE_VAAPI)
     &GpuVideoDecodeAcceleratorFactory::CreateVaapiVDA,
-#elif BUILDFLAG(USE_V4L2_CODEC) && BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(USE_V4L2_CODEC) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH))
     &GpuVideoDecodeAcceleratorFactory::CreateV4L2VDA,
     &GpuVideoDecodeAcceleratorFactory::CreateV4L2SliceVDA,
 #endif
@@ -192,7 +196,8 @@ GpuVideoDecodeAcceleratorFactory::CreateVaapiVDA(
                                                 gl_client_.bind_image));
   return decoder;
 }
-#elif BUILDFLAG(USE_V4L2_CODEC) && BUILDFLAG(IS_CHROMEOS_ASH)
+#elif BUILDFLAG(USE_V4L2_CODEC) && \
+    (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH))
 std::unique_ptr<VideoDecodeAccelerator>
 GpuVideoDecodeAcceleratorFactory::CreateV4L2VDA(
     const gpu::GpuDriverBugWorkarounds& /*workarounds*/,
