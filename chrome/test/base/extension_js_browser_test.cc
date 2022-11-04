5651d294298246b16f82c460e818cf0bdae9ac77
Jacob Francis <francisjp@google.com>
2022-11-04 19:48:43 +0000

    Use BackgroundScriptExecutor::ExecuteScript instead of browsertest_util::ExecuteScriptInBackgroundPage.
    This change is being made to support running
    browser tests in Manifest V3 Service Workers.
    
    AX-RelNotes: N/A
    Change-Id: I449d1176a4f781fa089b501cf4417e137677d6c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997375
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067627}

--- chrome/test/base/extension_js_browser_test.cc
+++ chrome/test/base/extension_js_browser_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
+#include "extensions/browser/background_script_executor.h"
 #include "extensions/browser/browsertest_util.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_host_test_helper.h"
@@ -68,14 +69,16 @@ bool ExtensionJSBrowserTest::RunJavascriptTestF(bool is_async,
 
   std::u16string script_16 = base::JoinString(scripts, u"\n");
   std::string script = base::UTF16ToUTF8(script_16);
+  auto result = extensions::BackgroundScriptExecutor::ExecuteScript(
+      extension_host_browser_context_, extension_id_, script,
+      extensions::BackgroundScriptExecutor::ResultCapture::kSendScriptResult);
 
-  std::string result =
-      extensions::browsertest_util::ExecuteScriptInBackgroundPage(
-          Profile::FromBrowserContext(extension_host_browser_context_),
-          extension_id_, script);
+  if (!result.is_string())
+    return false;
 
+  std::string result_str = result.GetString();
   std::unique_ptr<base::Value> value_result =
-      base::JSONReader::ReadDeprecated(result);
+      base::JSONReader::ReadDeprecated(result_str);
   CHECK_EQ(base::Value::Type::DICTIONARY, value_result->type());
   base::DictionaryValue* dict_value =
       static_cast<base::DictionaryValue*>(value_result.get());
