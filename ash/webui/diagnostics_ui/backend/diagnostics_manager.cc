ca4accfc2a1b829d87963acb829763cb55ca5912
Ashley Prasad <ashleydp@google.com>
2022-11-04 15:38:21 +0000

    refactor: move code into feature directory
    To help manage the growing number of files under diagnostics
    "backend" shifting files into feature specific directories.
    
    - Move code for common code into into the "common" directory
    - Move code for connectivity diagnostics feature into the "connectivity"
     directory
    - Move code for input diagnostics feature into the "input" directory
    - Move code for system diagnostics feature into the "system" directory
    
    Bug: b:253021171
    Test: ash_unittest,ash_webui_unittest, and browser_test --gtest_filter=*Diagnostics*
    Change-Id: I1bc1b9a95eb2371ca6b78ecf233630bb6b170bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000649
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067549}

--- ash/webui/diagnostics_ui/backend/diagnostics_manager.cc
+++ ash/webui/diagnostics_ui/backend/diagnostics_manager.cc
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ui/aura/window.h>
 #include "ash/constants/ash_features.h"
 #include "ash/system/diagnostics/diagnostics_log_controller.h"
-#include "ash/webui/diagnostics_ui/backend/input_data_provider.h"
-#include "ash/webui/diagnostics_ui/backend/network_health_provider.h"
+#include "ash/webui/diagnostics_ui/backend/connectivity/network_health_provider.h"
+#include "ash/webui/diagnostics_ui/backend/input/input_data_provider.h"
 #include "ash/webui/diagnostics_ui/backend/session_log_handler.h"
-#include "ash/webui/diagnostics_ui/backend/system_data_provider.h"
-#include "ash/webui/diagnostics_ui/backend/system_routine_controller.h"
+#include "ash/webui/diagnostics_ui/backend/system/system_data_provider.h"
+#include "ash/webui/diagnostics_ui/backend/system/system_routine_controller.h"
 
 namespace ash {
 namespace diagnostics {
