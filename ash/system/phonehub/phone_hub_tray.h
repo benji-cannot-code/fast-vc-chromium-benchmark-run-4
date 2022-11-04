ace2cf65280c6a11683adce22e394578d62689f6
Jennifer Serrano <jennserrano@google.com>
2022-11-04 21:02:15 +0000

    Phone Hub: Bubble moves correctly when zoom changes.
    This fixes the bug where the Phone Hub bubble does not correctly stay in the same position while changing zoom levels.
    
    -After fix-
    Zooming out with Phone Hub bubble open: https://screenshot.googleplex.com/ea3QE3pps98kuax.png
    Zooming in with Phone Hub bubble open:
    https://screenshot.googleplex.com/6Z5oCyypz2a3K2v.png
    
    TEST = manually verified on zork that the bubble stays in the same position when 1) zooming in 2) zooming out
    
    Change-Id: I8d8bf7c0724920885f494ded43ec97f364c1b639
    Fixed: b/215468889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000689
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067672}

--- ash/system/phonehub/phone_hub_tray.h
+++ ash/system/phonehub/phone_hub_tray.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/phonehub/phone_hub_content_view.h"
 #include "ash/system/phonehub/phone_hub_ui_controller.h"
 #include "ash/system/phonehub/phone_status_view.h"
+#include "ash/system/screen_layout_observer.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "base/bind.h"
@@ -42,7 +43,8 @@ class ASH_EXPORT PhoneHubTray : public TrayBackgroundView,
                                 public OnboardingView::Delegate,
                                 public PhoneStatusView::Delegate,
                                 public PhoneHubUiController::Observer,
-                                public SessionObserver {
+                                public SessionObserver,
+                                public ScreenLayoutObserver {
  public:
   explicit PhoneHubTray(Shelf* shelf);
   PhoneHubTray(const PhoneHubTray&) = delete;
@@ -73,6 +75,9 @@ class ASH_EXPORT PhoneHubTray : public TrayBackgroundView,
 
   // OnboardingView::Delegate:
   void HideStatusHeaderView() override;
+
+  // ScreenLayoutObserver:
+  void OnDisplayConfigurationChanged() override;
 
   // Provides the Eche icon and Eche loading indicator to
   // `EcheTray` in order to let `EcheTray` control the visibiliity
