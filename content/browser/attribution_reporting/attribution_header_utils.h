7eff745ba10812b29117890fb81035d33dee6365
Andrew Paseltiner <apaseltiner@chromium.org>
2022-11-04 14:14:55 +0000

    Parse JSON into intermediate structure
    We will eventually move this into components/attribution_reporting after
    AttributionFilterData and AttributionAggregationKeys are.
    
    Bug: 1379009
    Change-Id: I69fd130e213d287eb6eb1ce4c6e03af49f5ee546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004737
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067520}

--- content/browser/attribution_reporting/attribution_header_utils.h
+++ content/browser/attribution_reporting/attribution_header_utils.h
@@ -6,19 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ATTRIBUTION_REPORTING_ATTRIBUTION_HEADER_UTILS_H_
 #define CONTENT_BROWSER_ATTRIBUTION_REPORTING_ATTRIBUTION_HEADER_UTILS_H_
 
+#include <stdint.h>
+
+#include "base/time/time.h"
 #include "base/types/expected.h"
 #include "base/values.h"
 #include "components/attribution_reporting/source_registration_error.mojom-forward.h"
+#include "content/browser/attribution_reporting/attribution_aggregation_keys.h"
+#include "content/browser/attribution_reporting/attribution_filter_data.h"
 #include "content/browser/attribution_reporting/attribution_source_type.h"
 #include "content/common/content_export.h"
-
-namespace base {
-class Time;
-}  // namespace base
-
-namespace url {
-class Origin;
-}  // namespace url
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "url/origin.h"
 
 namespace content {
 
@@ -33,6 +32,34 @@ ParseSourceRegistration(base::Value::Dict registration,
                         url::Origin source_origin,
                         AttributionSourceType source_type,
                         bool is_within_fenced_frame);
+
+struct SourceRegistration {
+  static base::expected<SourceRegistration,
+                        attribution_reporting::mojom::SourceRegistrationError>
+  Parse(base::Value::Dict, url::Origin reporting_origin);
+
+  SourceRegistration();
+
+  ~SourceRegistration();
+
+  SourceRegistration(const SourceRegistration&);
+  SourceRegistration& operator=(const SourceRegistration&);
+
+  SourceRegistration(SourceRegistration&&);
+  SourceRegistration& operator=(SourceRegistration&&);
+
+  uint64_t source_event_id;
+  url::Origin destination;
+  url::Origin reporting_origin;
+  absl::optional<base::TimeDelta> expiry;
+  absl::optional<base::TimeDelta> event_report_window;
+  absl::optional<base::TimeDelta> aggregatable_report_window;
+  int64_t priority;
+  AttributionFilterData filter_data;
+  absl::optional<uint64_t> debug_key;
+  AttributionAggregationKeys aggregation_keys;
+  bool debug_reporting;
+};
 
 }  // namespace content
 
