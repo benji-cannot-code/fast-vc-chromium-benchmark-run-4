6520a31bdd06f105eb231cfd967b94757f8df361
Md Shahadat Hossain Shahin <shahinmd@google.com>
2022-11-04 10:06:12 +0000

    [Privacy Hub] Add search concepts
    Adds search concept for some contents in the Privacy controls page.
    
    Bug: b/254911300
    Change-Id: I0c628e581b3221940020fd7afc39983d91dabd91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4001582
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067459}

--- chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.ts
+++ chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.ts
@@ -679,6 +679,8 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'cr:person';
       case SearchResultIcon.kBluetooth:
         return 'cr:bluetooth';
+      case SearchResultIcon.kCamera:
+        return 'os-settings:camera';
       case SearchResultIcon.kCellular:
         return 'os-settings:cellular';
       case SearchResultIcon.kChrome:
@@ -707,6 +709,8 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'os-settings:folder-outline';
       case SearchResultIcon.kFullscreenMagnifier:
         return 'os-settings:fullscreen-magnifier';
+      case SearchResultIcon.kGeolocation:
+        return 'os-settings:geolocation';
       case SearchResultIcon.kGlobe:
         return 'os-settings:language';
       case SearchResultIcon.kGooglePlay:
@@ -725,6 +729,8 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'cr:search';
       case SearchResultIcon.kMessages:
         return 'os-settings:multidevice-messages';
+      case SearchResultIcon.kMicrophone:
+        return 'os-settings:microphone';
       case SearchResultIcon.kMouse:
         return 'os-settings:mouse';
       case SearchResultIcon.kNearbyShare:
