e1cec7f57915194d8300294b6282522de4a90940
Peter Kotwicz <pkotwicz@chromium.org>
2022-11-04 21:30:17 +0000

    Record when the FedCM terms-of-service link is clicked part 2/2
    This CL adds recording for the following histograms on desktop:
    - Blink.FedCm.SignUp.PrivacyPolicyClicked
    - Blink.FedCm.SignUp.TermsOfServiceClicked
    
    BUG=1343552
    
    Change-Id: I6b355a5bc5ea23e3d1aa558107a10867d0c7f7a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3996332
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067696}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.h
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.h
@@ -36,6 +36,8 @@ class AccountSelectionBubbleView : public views::BubbleDialogDelegateView,
   // Used to observe changes to the account selection bubble.
   class Observer {
    public:
+    enum class LinkType { PRIVACY_POLICY, TERMS_OF_SERVICE };
+
     // Called when the user either selects the account from the multi-account
     // chooser or clicks the "continue" button.
     // Takes `account` as well as `idp_data` since passing `account_id` is
@@ -46,7 +48,7 @@ class AccountSelectionBubbleView : public views::BubbleDialogDelegateView,
         const IdentityProviderDisplayData& idp_data) = 0;
 
     // Called when the user clicks "privacy policy" or "terms of service" link.
-    virtual void OnLinkClicked(const GURL& url) = 0;
+    virtual void OnLinkClicked(LinkType link_type, const GURL& url) = 0;
 
     // Called when the user clicks "back" button.
     virtual void OnBackButtonClicked() = 0;
