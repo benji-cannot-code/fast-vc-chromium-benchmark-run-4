1aa07074f18678a833def04ac0b00dcf70ba2191
Maxim Anufriev <maxan@google.com>
2022-11-05 09:07:43 +0000

    Reland "[UPM] Send crash dump on GMS Backend DEVELOPER_ERROR"
    This reverts commit 56c4afc7876899f3e9e90bddf4d014ed0e44fb7c.
    
    Reason for revert: Landing crash reporting again to investigate
    current occurrences of these errors.
    
    Original change's description:
    > Revert "[UPM] Send crash dump on GMS Backend DEVELOPER_ERROR"
    >
    > This reverts commit 270179d853cd0c493cdaa121d09eb74e2807f27d.
    >
    > Reason for revert: enough data collected.
    >
    > Original change's description:
    > > [UPM] Send crash dump on GMS Backend DEVELOPER_ERROR
    > >
    > > This CL re-enables crash dumps that were disabled in crrev.com/c/3686453
    > > This is not a clean revert as it collects additional data.
    > >
    > > It is expected to produce crash dumps without crashing the browser and
    > > is needed to collect debug information.
    > >
    > > Bug: 1324588
    > > Change-Id: Ied1f0886da07f480f5c2a2191818a8a51cd5f016
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706600
    > > Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    > > Commit-Queue: Maxim Anufriev <maxan@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1016153}
    >
    > Bug: 1324588
    > Change-Id: I5edfb4db74275b8fbd7533444b919144c3736776
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3731388
    > Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    > Commit-Queue: Maxim Anufriev <maxan@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1018634}
    
    Bug: 1324588
    Change-Id: I1c0e6fb4c67e6dd644dea54ce9fde2cf38f12731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003167
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067849}

--- chrome/browser/password_manager/android/password_store_android_backend.h
+++ chrome/browser/password_manager/android/password_store_android_backend.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/android/password_store_android_backend_api_error_codes.h"
 #include "chrome/browser/password_manager/android/password_store_android_backend_bridge.h"
 #include "chrome/browser/password_manager/android/password_sync_controller_delegate_android.h"
+#include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_store_backend.h"
 #include "components/password_manager/core/browser/password_store_backend_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_store_util.h"
@@ -116,11 +117,13 @@ class PasswordStoreAndroidBackend
     JobReturnHandler(LoginsOrErrorReply callback,
                      PasswordStoreBackendMetricsRecorder metrics_recorder,
                      base::TimeDelta delay,
-                     PasswordStoreOperation operation);
+                     PasswordStoreOperation operation,
+                     base::OnceClosure crash_dump_callback);
     JobReturnHandler(PasswordChangesOrErrorReply callback,
                      PasswordStoreBackendMetricsRecorder metrics_recorder,
                      base::TimeDelta delay,
-                     PasswordStoreOperation operation);
+                     PasswordStoreOperation operation,
+                     base::OnceClosure crash_dump_callback);
     JobReturnHandler(JobReturnHandler&&);
     JobReturnHandler& operator=(JobReturnHandler&&) = delete;
     ~JobReturnHandler();
@@ -137,6 +140,8 @@ class PasswordStoreAndroidBackend
 
     void RecordMetrics(absl::optional<AndroidBackendError> error) const;
     base::TimeDelta GetElapsedTimeSinceStart() const;
+    // TODO(crbug.com/1324588): Remove after disabling crash dumps.
+    void SendCrashDump();
 
     base::TimeDelta GetDelay();
     PasswordStoreOperation GetOperation();
@@ -147,6 +152,7 @@ class PasswordStoreAndroidBackend
     PasswordStoreBackendMetricsRecorder metrics_recorder_;
     base::TimeDelta delay_;
     PasswordStoreOperation operation_;
+    base::OnceClosure crash_dump_callback_;
   };
 
   using JobId = PasswordStoreAndroidBackendBridge::JobId;
@@ -239,6 +245,8 @@ class PasswordStoreAndroidBackend
   void OnError(PasswordStoreAndroidBackendBridge::JobId job_id,
                AndroidBackendError error) override;
 
+  // TODO(crbug.com/1324588): Remove signon_realm and origin after disabling
+  // crash dumps.
   template <typename Callback>
   // Calling this method can be delayed in the case when a retry is scheduled.
   // Since the retry logic implements exponential backoff the duration of the
@@ -249,7 +257,12 @@ class PasswordStoreAndroidBackend
                    Callback callback,
                    MetricInfix metric_infix,
                    PasswordStoreOperation operation,
-                   base::TimeDelta delay);
+                   base::TimeDelta delay,
+                   absl::optional<std::string> signon_realm = absl::nullopt,
+                   absl::optional<std::string> origin = absl::nullopt,
+                   absl::optional<bool> is_username_empty = absl::nullopt,
+                   absl::optional<bool> is_blocklisted = absl::nullopt,
+                   absl::optional<PasswordForm::Scheme> scheme = absl::nullopt);
   absl::optional<JobReturnHandler> GetAndEraseJob(JobId job_id);
 
   // Gets logins matching |form|.
