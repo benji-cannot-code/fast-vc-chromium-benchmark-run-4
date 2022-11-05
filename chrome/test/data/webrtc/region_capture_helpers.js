fe958ed722c1223ba43f3b3618c52e2a83f7c87a
Elad Alon <eladalon@chromium.org>
2022-11-05 22:32:05 +0000

    Correct the default value of selfBrowserSurface
    The new media-picker order (tabs/windows/screens) is being rolled out
    gradually. It is intended to nudge users away from sharing screens,
    and towards sharing tabs, which prevents oversharing and significantly
    improves privacy. More on this in the following articles:
    * https://developer.chrome.com/docs/web-platform/screen-sharing-controls/
    * https://developer.chrome.com/blog/avoiding-oversharing-when-screen-sharing/
    
    However, an unintended side-effect of offering tabs first, is that it
    also increases the rate with which users choose the current tab.
    With very few exceptions, the current tab is the worst thing to share.
    In video conferencing applications, this produces the hall-of-mirrors
    effect, and worse - audio feedback loops leading to "howling".
    Apps that *are* interested in self-capture,
    use {selfBrowserSurface: "include"} and/or {preferCurrentTab: true}
    to signal that to the browser.
    
    We should therefore ensure that,
    if neither {selfBrowserSurface: "include"} nor {preferCurrentTab: true}
    were specified, the current tab should be excluded from the list
    of tabs. This change will be rolled out gradually alongside the gradual
    rollout of the new media-picker order.
    
    Bug: 1381265
    Change-Id: I116c8de7f99d78417851ef1ff609453ea45cfafb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004701
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067885}

--- chrome/test/data/webrtc/region_capture_helpers.js
+++ chrome/test/data/webrtc/region_capture_helpers.js
@@ -87,7 +87,10 @@ async function startCapture() {
   }
 
   try {
-    const stream = await navigator.mediaDevices.getDisplayMedia();
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      selfBrowserSurface: "include"
+    });
     [track] = stream.getVideoTracks();
     window.domAutomationController.send(`${role}-capture-success`);
   } catch (e) {
@@ -112,7 +115,10 @@ async function startSecondCapture(targetFrame) {
   }
 
   try {
-    const stream = await navigator.mediaDevices.getDisplayMedia();
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      selfBrowserSurface: "include"
+    });
     [otherCaptureTrack] = stream.getVideoTracks();
     window.domAutomationController.send(`${role}-second-capture-success`);
   } catch (e) {
