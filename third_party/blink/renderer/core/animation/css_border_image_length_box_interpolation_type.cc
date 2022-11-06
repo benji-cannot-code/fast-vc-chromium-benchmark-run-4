06491241d65a8ede9257e1cde39a00d1f5827af4
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-11-06 23:48:55 +0000

    [StyleBuilder] Mark border-image and mask-box-image as readonly.
    As above. There should be no behaviour change.
    
    Bug: 1377295
    Change-Id: Iace3daad6face5c3ea1e9b418ae8c75713052f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005178
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067937}

--- third_party/blink/renderer/core/animation/css_border_image_length_box_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_border_image_length_box_interpolation_type.cc
@@ -48,20 +48,20 @@ const BorderImageLengthBox& GetBorderImageLengthBox(
 }
 
 void SetBorderImageLengthBox(const CSSProperty& property,
-                             ComputedStyle& style,
+                             ComputedStyleBuilder& builder,
                              const BorderImageLengthBox& box) {
   switch (property.PropertyID()) {
     case CSSPropertyID::kBorderImageOutset:
-      style.SetBorderImageOutset(box);
+      builder.SetBorderImageOutset(box);
       break;
     case CSSPropertyID::kWebkitMaskBoxImageOutset:
-      style.SetMaskBoxImageOutset(box);
+      builder.SetMaskBoxImageOutset(box);
       break;
     case CSSPropertyID::kBorderImageWidth:
-      style.SetBorderImageWidth(box);
+      builder.SetBorderImageWidth(box);
       break;
     case CSSPropertyID::kWebkitMaskBoxImageWidth:
-      style.SetMaskBoxImageWidth(box);
+      builder.SetMaskBoxImageWidth(box);
       break;
     default:
       NOTREACHED();
@@ -409,7 +409,7 @@ void CSSBorderImageLengthBoxInterpolationType::ApplyStandardPropertyValue(
   };
   BorderImageLengthBox box(convert_side(kSideTop), convert_side(kSideRight),
                            convert_side(kSideBottom), convert_side(kSideLeft));
-  SetBorderImageLengthBox(CssProperty(), *state.Style(), box);
+  SetBorderImageLengthBox(CssProperty(), state.StyleBuilder(), box);
 }
 
 }  // namespace blink
