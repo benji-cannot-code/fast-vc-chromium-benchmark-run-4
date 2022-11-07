bd77615299c42045614eaa172851f10822aaebfe
Nicola Tommasi <tommasin@chromium.org>
2022-11-07 15:30:53 +0000

    [Clank] Fix promos running in parallel
    This CL should fix crbug.com/1369342. Since the PrivacySandbox promo logic in NewTabPage was not taken into account in the main TabbedRootUiCoordinator promo logic, we need to store a temporary boolean info which stops triggering other promos when the PS promo has already been shown in the same browser run.
    
    Bug: 1369342
    Change-Id: Ib03028d44403fd86a72b520f14982b731a23ad2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967972
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068108}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
@@ -24,6 +24,9 @@ public class PrivacySandboxDialogController {
     private static WeakReference<Dialog> sDialog;
     private static Boolean sShowNew;
     private static Boolean sDisableAnimations;
+    // TODO(crbug.com/1330704): This variable and its usage can be removed when the PrivacySandbox
+    // promo logic will be decoupled from the NewTabPage.
+    private static boolean sNewNoticeShownInCurrentSession;
 
     /**
      * Launches an appropriate dialog if necessary and returns whether that happened.
@@ -49,6 +52,7 @@ public class PrivacySandboxDialogController {
                     new PrivacySandboxBottomSheetNotice(
                             context, bottomSheetController, settingsLauncher)
                             .showNotice(/*animate = */ sDisableAnimations == null);
+                    sNewNoticeShownInCurrentSession = true;
                 } else if (launchContext == PrivacySandboxDialogLaunchContext.BROWSER_START
                         && !newNotice) {
                     // Invoked at browser start without the new notice; show it.
@@ -77,6 +81,13 @@ public class PrivacySandboxDialogController {
         // TODO(crbug.com/1375230) Remove this code path if the ability to
         // differentiate notice types is no longer required.
         return (sShowNew != null) ? sShowNew : true;
+    }
+
+    /**
+     * Returns true if the new notice has already been shown in the current session.
+     */
+    public static boolean hasNewNoticeBeenShownInCurrentSession() {
+        return sNewNoticeShownInCurrentSession;
     }
 
     @VisibleForTesting
