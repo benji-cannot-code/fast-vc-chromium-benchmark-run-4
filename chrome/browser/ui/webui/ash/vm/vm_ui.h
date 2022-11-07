b084bf320c20605cf6e9c9371bebc87d290dcca0
Clement Yan <clamclamyan@google.com>
2022-11-07 01:58:26 +0000

    [WebUI] Migrate VmUI to WebUIConfig
    Context:
    
    Currently WebUI properties, e.g. CSPs, requesting schemes, host, mojo,
    etc. are stored in WebUIControllers themselves or in separate lists
    like IsWebUIAllowedToMakeNetworkRequests. The lifetime of a
    WebUIController is bound to the frame, which makes it hard to use for
    some use cases where the WebUIController hasn't been created yet. A
    non-dynamically allocated class where clients could query a WebUI's
    properties would be easier to use and audit.
    
    WebUIConfig is a class that stores properties of WebUIs. For now the
    properties are 1. the WebUI's origin, 2. if the WebUI is enabled,
    and 3. GetURLDataSource() method for service worker initialization.
    In the future this class could include information like CSPs, if we
    should enable Mojo, if we should enable chrome.send(), if we should
    allow network requests, URLDataSource, etc.
    
    Bug: 1317510
    Change-Id: I74bb5cec217b89b41cf006f7d9e035494400f17e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4001321
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067952}

--- chrome/browser/ui/webui/ash/vm/vm_ui.h
+++ chrome/browser/ui/webui/ash/vm/vm_ui.h
@@ -9,10 +9,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "chrome/browser/ui/webui/ash/vm/vm.mojom-forward.h"
+#include "chrome/common/webui_url_constants.h"
+#include "content/public/browser/webui_config.h"
+#include "content/public/common/url_constants.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 
 namespace ash {
+
+class VmUI;
+
+// WebUIConfig for chrome://vm
+class VmUIConfig : public content::DefaultWebUIConfig<VmUI> {
+ public:
+  VmUIConfig()
+      : DefaultWebUIConfig(content::kChromeUIScheme, chrome::kChromeUIVmHost) {}
+};
 
 // The WebUI for chrome://vm
 class VmUI : public ui::MojoWebUIController {
--- chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_credentials_dialog.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_share_dialog.h"
 #include "chrome/browser/ui/webui/ash/sys_internals/sys_internals_ui.h"
+#include "chrome/browser/ui/webui/ash/vm/vm_ui.h"
 #include "chrome/browser/ui/webui/settings/ash/os_settings_ui.h"
 #if !defined(OFFICIAL_BUILD)
 #include "ash/webui/sample_system_web_app_ui/sample_system_web_app_ui.h"
@@ -99,6 +100,7 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(
       std::make_unique<ash::smb_dialog::SmbShareDialogUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SysInternalsUIConfig>());
+  map.AddWebUIConfig(std::make_unique<ash::VmUIConfig>());
 #if !defined(OFFICIAL_BUILD)
   map.AddWebUIConfig(std::make_unique<ash::SampleSystemWebAppUIConfig>());
 #endif  // !defined(OFFICIAL_BUILD)
