b204811b55331bcccf37317253f8790bb522d930
Noel Gordon <noel@chromium.org>
2022-11-07 01:16:15 +0000

    filesapp: minor xf_conflict_dialog UX fix and unittest change
    The dialog content should not be user-selectable (appiness): set CSS
    user-select:none on the dialog part to prevent selection.
    
    Remove loadTimeData from the unittest: all files app strings are now
    available in the unittest framework, no need to mock them anymore.
    
    Bug: b/255264604
    Test: browser_tests --gtest_filter="FileManagerJsTest.XfConflictDialog*"
    Change-Id: I648074dea944e26d286ece69be338204631defbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005687
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067948}

--- ui/file_manager/file_manager/widgets/xf_conflict_dialog_unittest.ts
+++ ui/file_manager/file_manager/widgets/xf_conflict_dialog_unittest.ts
@@ -3,26 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {assertEquals, assertFalse, assertNotEquals, assertNotReached, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {waitUntil} from '../common/js/test_error_reporting.js';
 
 import {ConflictResolveType, XfConflictDialog} from './xf_conflict_dialog.js';
-
-/**
- * Mock LoadTimeData strings.
- */
-export function setUpPage() {
-  loadTimeData.getString = (id: string) => {
-    switch (id) {
-      case 'CONFLICT_DIALOG_MESSAGE':
-        return 'File name $1 exists. What would you like to do?';
-      default:
-        return id;
-    }
-  };
-}
 
 /**
  * Creates new <xf-conflict-dialog> for each test.
