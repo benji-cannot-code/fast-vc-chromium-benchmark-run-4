124f396f404f9874b47c74ebb1caa9ae2a1c7870
Calder Kitagawa <ckitagawa@chromium.org>
2022-11-07 22:29:52 +0000

    [Android Bulk Edit Tabs] Hide only if visible
    TabMultiSelectV2.Closed reports incorrectly due to hide() running even
    if the UI surface isn't visible. Fix this by only running hide() if the
    UI surface is visible.
    
    Bug: 1382182
    Change-Id: I9a1ae3900b70421e4028aa97555e159a2aaa9cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008669
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068360}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorMediator.java
@@ -124,7 +124,7 @@ class TabSelectionEditorMediator
 
             @Override
             public void willCloseTab(Tab tab, boolean animate, boolean didCloseAlone) {
-                if (isEditorVisible()) hide();
+                hide();
             }
         };
 
@@ -296,6 +296,8 @@ class TabSelectionEditorMediator
 
     @Override
     public void hide() {
+        if (!isEditorVisible()) return;
+
         if (TabUiFeatureUtilities.isTabSelectionEditorV2Enabled(mContext)) {
             RecordUserAction.record("TabMultiSelectV2.Closed");
         }
