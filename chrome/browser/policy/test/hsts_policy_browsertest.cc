ea7208b00fc8bc0a1d2450a554d650934904cf1f
Fabio Tirelo <ftirelo@chromium.org>
2022-11-07 16:31:36 +0000

    [Clank CBCM] Enable hsts_policy_browsertest on Android
    Bug: 1309996
    Change-Id: Ie1dd16b6cf56c8c91da798d85e8bc7cec8e23ea0
    Fixed: 1309996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550456
    Reviewed-by: Owen Min <zmin@chromium.org>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068136}

--- chrome/browser/policy/test/hsts_policy_browsertest.cc
+++ chrome/browser/policy/test/hsts_policy_browsertest.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "chrome/browser/policy/policy_test_utils.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/base/chrome_test_utils.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
 #include "content/public/browser/network_service_instance.h"
@@ -24,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 class HSTSPolicyTest : public PolicyTest {
+ public:
   void SetUpInProcessBrowserTestFixture() override {
     PolicyTest::SetUpInProcessBrowserTestFixture();
     PolicyMap policies;
@@ -46,9 +45,9 @@ IN_PROC_BROWSER_TEST_F(HSTSPolicyTest, HSTSPolicyBypassList) {
 
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url("http://example/");
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  ASSERT_TRUE(NavigateToUrl(url, this));
   content::WebContents* contents =
-      browser()->tab_strip_model()->GetActiveWebContents();
+      chrome_test_utils::GetActiveWebContents(this);
   // If the policy didn't take effect, the request to http://example would be
   // upgraded to https://example. This checks that the HSTS upgrade to https
   // didn't happen.
