f55543e106be964c646287852fa01b07c453ecce
Russ Hamilton <behamilton@google.com>
2022-11-07 17:00:47 +0000

    Add encryption for Oblivious HTTP implementation
    Bug: 1342255
    Change-Id: I88874663a9c0c0e13eda5559e37bc37a8c1b39b4
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989683
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068152}

--- net/third_party/quiche/overrides/quiche_platform_impl/quiche_logging_impl.h
+++ net/third_party/quiche/overrides/quiche_platform_impl/quiche_logging_impl.h
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QUICHE_CHECK_LT_IMPL(val1, val2) CHECK_LT(val1, val2)
 #define QUICHE_CHECK_GE_IMPL(val1, val2) CHECK_GE(val1, val2)
 #define QUICHE_CHECK_GT_IMPL(val1, val2) CHECK_GT(val1, val2)
+#define QUICHE_CHECK_OK_IMPL(value) CHECK((value).ok())
 
 #define QUICHE_DCHECK_IMPL(condition) DCHECK(condition)
 #define QUICHE_DCHECK_EQ_IMPL(val1, val2) DCHECK_EQ(val1, val2)
