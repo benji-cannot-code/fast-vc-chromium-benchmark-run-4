b7d831db82b39b210940db9797042d824c7fe610
Stephen Roettger <sroettger@google.com>
2022-11-07 14:52:02 +0000

    Support pkey debug mode without pkey 0 access
    While using the pkey pool, we want to ensure that we're aware of all the
    memory reads/writes to pkey 0 memory. For that, we're testing in debug
    mode with pkru register set to DISABLE_ACCESS to pkey 0. This breaks in
    three places:
    1) stack cookies are read from pkey 0 memory
    2) The libc memset accesses a global variable
    3) Some code reads pthread_self() which accesses the TLS
    
    This cl disables stack cookies in debug mode and adds a
    LiftPkeyRestrictionScope class, which temporarily resets the pkru
    register and allows access to all memory.
    
    Bug: v8:13355
    Change-Id: I0be2a992dd8527dfc0ecec8eec8e39c8aca0aed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949284
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Stephen Röttger <sroettger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068089}

--- base/allocator/partition_allocator/partition_lock.h
+++ base/allocator/partition_allocator/partition_lock.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_base/thread_annotations.h"
 #include "base/allocator/partition_allocator/partition_alloc_base/threading/platform_thread.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
+#include "base/allocator/partition_allocator/pkey.h"
 #include "base/allocator/partition_allocator/spinning_mutex.h"
 #include "build/build_config.h"
 
@@ -25,6 +26,10 @@ class PA_LOCKABLE Lock {
   inline constexpr Lock();
   void Acquire() PA_EXCLUSIVE_LOCK_FUNCTION() {
 #if BUILDFLAG(PA_DCHECK_IS_ON)
+#if BUILDFLAG(ENABLE_PKEYS)
+    LiftPkeyRestrictionsScope lift_pkey_restrictions;
+#endif
+
     // When PartitionAlloc is malloc(), it can easily become reentrant. For
     // instance, a DCHECK() triggers in external code (such as
     // base::Lock). DCHECK() error message formatting allocates, which triggers
@@ -70,6 +75,9 @@ class PA_LOCKABLE Lock {
   void AssertAcquired() const PA_ASSERT_EXCLUSIVE_LOCK() {
     lock_.AssertAcquired();
 #if BUILDFLAG(PA_DCHECK_IS_ON)
+#if BUILDFLAG(ENABLE_PKEYS)
+    LiftPkeyRestrictionsScope lift_pkey_restrictions;
+#endif
     PA_DCHECK(owning_thread_ref_.load(std ::memory_order_acquire) ==
               base::PlatformThread::CurrentRef());
 #endif
