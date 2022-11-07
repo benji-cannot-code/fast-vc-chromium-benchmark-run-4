9850865f59642d5e2bb0d0c253deb3183b14a1b7
Alvin Ji <alvinji@chromium.org>
2022-11-07 15:46:18 +0000

    Attach LocalFrameToken to WebAudioSinkDescriptor
    Creating AudioRendererSink requires a frame token for IPC to propagate a
    request to the browser side. Currently we get a token from
    `blink::WebLocalFrame::FrameForCurrentContext()`.
    
    However, the invocation of `AudioContext.setSinkId()` will queue a task
    to fire a callback
    `mojom::blink::MediaDevicesListener::OndeviceChanged`, and this call
    chain invalidates `blink::WebLocalFrame::FrameForCurrentContext()`
    because it's invoked from non-V8 environment.
    
    By directly adding a frame token to WebAudioSinkDescriptor, we can
    guarantee to pass a valid frame token associated with the AudioContext.
    
    Bug: 1216187
    Change-Id: I2e486e3f32bf30288a40150cc0b9984b07367fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007929
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068113}

--- third_party/blink/public/platform/web_audio_sink_descriptor.h
+++ third_party/blink/public/platform/web_audio_sink_descriptor.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/notreached.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/platform/web_string.h"
 
 namespace blink {
@@ -26,19 +27,21 @@ class WebAudioSinkDescriptor {
     kLastValue = kSilent
   };
 
+  WebAudioSinkDescriptor() = default;
+
   // For an "audible" sink with a user-selected identifier. The empty string
   // on |sink_id| means the system's default audio device.
-  explicit WebAudioSinkDescriptor(const WebString& sink_id)
-      : type_(kAudible), sink_id_(sink_id) {}
+  explicit WebAudioSinkDescriptor(const WebString& sink_id,
+                                  const LocalFrameToken& token)
+      : type_(kAudible), sink_id_(sink_id), token_(token) {}
 
   // For a "silent" sink.
-  explicit WebAudioSinkDescriptor() : type_(kSilent) {}
+  explicit WebAudioSinkDescriptor(const LocalFrameToken& token)
+      : type_(kSilent), token_(token) {}
 
+  const LocalFrameToken& Token() const { return token_; }
   AudioSinkType Type() const { return type_; }
-  WebString SinkId() const {
-    DCHECK_EQ(type_, kAudible);
-    return sink_id_;
-  }
+  WebString SinkId() const { return sink_id_; }
 
   bool operator==(const WebAudioSinkDescriptor& rhs) const {
     return this->Type() == rhs.Type() && this->SinkId() == rhs.SinkId();
@@ -47,6 +50,7 @@ class WebAudioSinkDescriptor {
  private:
   AudioSinkType type_;
   WebString sink_id_;
+  LocalFrameToken token_;
 };
 
 }  // namespace blink
