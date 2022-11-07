0956b2f5e02b6fc6f59859fd0a699044d9d3680c
Salma Elmahallawy <esalma@google.com>
2022-11-07 22:35:50 +0000

    [Managed Prefs] Payment method toggle disabled when managed
    One could change the toggle state when AutofillCreditCardEnabled was
    set to false. However, no UI to add credit cards was offered,
    so it was simply UI bug.
    This CL disables the toggle for autofill in the payment methods
    setting page when AutofillCreditCardEnabled policy is set to false.
    
    Bug: 1323030
    Change-Id: Ice2125a72f55ad114846a03faaaca695934b64d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999941
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068366}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragment.java
@@ -104,7 +104,7 @@ public class AutofillPaymentMethodsFragment extends PreferenceFragmentCompat
             @Override
             public boolean isPreferenceClickDisabledByPolicy(Preference preference) {
                 return PersonalDataManager.isAutofillCreditCardManaged()
-                        && !PersonalDataManager.isAutofillProfileEnabled();
+                        && !PersonalDataManager.isAutofillCreditCardEnabled();
             }
         });
         getPreferenceScreen().addPreference(autofillSwitch);
