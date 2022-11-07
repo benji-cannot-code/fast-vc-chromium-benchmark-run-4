2cfb04d507231de50d2331b52ef37b721d47c055
Alessio Bazzica <alessiob@chromium.org>
2022-11-07 16:19:26 +0000

    WASAPIAudioInputStream: remove volume range stats
    Bug: webrtc:7494
    Change-Id: Ibe3dc247c14d3119983788bac87a8c7e2a823f77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008259
    Commit-Queue: Ale Bzk <alessiob@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068129}

--- media/audio/win/audio_low_latency_input_win.cc
+++ media/audio/win/audio_low_latency_input_win.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_features.h"
 #include "media/audio/win/avrt_wrapper_win.h"
 #include "media/audio/win/core_audio_util_win.h"
-#include "media/audio/win/volume_range_util.h"
 #include "media/base/audio_block_fifo.h"
 #include "media/base/audio_bus.h"
 #include "media/base/audio_timestamp_helper.h"
@@ -579,23 +578,6 @@ void WASAPIAudioInputStream::Stop() {
     }
   }
 
-  absl::optional<VolumeRange> volume_range;
-  if (add_uma_histogram && system_audio_volume_ &&
-      !AudioDeviceDescription::IsLoopbackDevice(device_id_)) {
-    VolumeRange range;
-    HRESULT hr = system_audio_volume_->GetVolumeRange(
-        &range.min_volume_db, &range.max_volume_db, &range.volume_step_db);
-    if (FAILED(hr)) {
-      SendLogMessage("%s => (ERROR: IAudioEndpointVolume::GetVolumeRange=[%s])",
-                     __func__, ErrorToString(hr).c_str());
-    } else {
-      SendLogMessage("%s => (IAudioEndpointVolume::GetVolumeRange) %f %f %f",
-                     __func__, range.min_volume_db, range.max_volume_db,
-                     range.volume_step_db);
-      volume_range = range;
-    }
-  }
-
   // Stops periodic AGC microphone measurements.
   StopAgc();
 
@@ -624,7 +606,6 @@ void WASAPIAudioInputStream::Stop() {
     base::UmaHistogramBoolean("Media.Audio.InputVolumeStartsAtZeroWin",
                               audio_session_starts_at_zero_volume_);
     audio_session_starts_at_zero_volume_ = false;
-    LogVolumeRangeUmaHistograms(volume_range);
   }
 
   SendLogMessage(
