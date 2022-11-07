56fe0122a9917182e1dcd65658f15c9190a9fb06
Andrew Paseltiner <apaseltiner@chromium.org>
2022-11-07 20:06:00 +0000

    Remove destination parameters from OsSource
    We no longer intend to use any parameters on these types, per
    https://github.com/WICG/attribution-reporting-api/pull/602.
    
    Bug: 1381499
    Change-Id: Ie24079fa182b99ff2dbf88f18d4ff3300f8bda1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006356
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068273}

--- third_party/blink/public/common/attribution_reporting/mojom_traits.h
+++ third_party/blink/public/common/attribution_reporting/mojom_traits.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <string>
 #include <utility>
 
 #include "components/attribution_reporting/parse.h"
@@ -16,9 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/mojom/conversions/attribution_data_host.mojom-shared.h"
 #include "url/gurl.h"
-#include "url/mojom/origin_mojom_traits.h"
 #include "url/mojom/url_gurl_mojom_traits.h"
-#include "url/origin.h"
 
 namespace mojo {
 
@@ -54,16 +51,6 @@ struct BLINK_COMMON_EXPORT
     return os_source.url();
   }
 
-  static const std::string& os_destination(
-      const attribution_reporting::OsSource& os_source) {
-    return os_source.os_destination();
-  }
-
-  static const url::Origin& web_destination(
-      const attribution_reporting::OsSource& os_source) {
-    return os_source.web_destination();
-  }
-
   // TODO(apaseltiner): Define this in a separate .cc file.
   static bool Read(blink::mojom::AttributionOsSourceDataView data,
                    attribution_reporting::OsSource* out) {
@@ -71,18 +58,8 @@ struct BLINK_COMMON_EXPORT
     if (!data.ReadUrl(&url))
       return false;
 
-    std::string os_destination;
-    if (!data.ReadOsDestination(&os_destination))
-      return false;
-
-    url::Origin web_destination;
-    if (!data.ReadWebDestination(&web_destination))
-      return false;
-
     absl::optional<attribution_reporting::OsSource> os_source =
-        attribution_reporting::OsSource::Create(std::move(url),
-                                                std::move(os_destination),
-                                                std::move(web_destination));
+        attribution_reporting::OsSource::Create(std::move(url));
     if (!os_source.has_value())
       return false;
 
