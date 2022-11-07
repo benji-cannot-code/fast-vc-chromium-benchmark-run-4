4286e2321d29ed1dcee2cc3a8023e495052168d3
Elie Maamari <emaamari@google.com>
2022-11-07 15:37:48 +0000

    Fix use after free for UserContext in OnAuthVerified
    We need to take ownership of the user context before invoking the
    callback to not risk execution flow going back to
    InSessionAuthDialogController with a null `user_context_`
    
    Bug: b/241256423
    Change-Id: I10f069e2ce8af7a2e1444ce21440966563b0d7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993406
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068110}

--- chromeos/ash/components/login/auth/auth_performer.cc
+++ chromeos/ash/components/login/auth/auth_performer.cc
@@ -542,7 +542,7 @@ void AuthPerformer::OnInvalidateAuthSession(
     AuthOperationCallback callback,
     absl::optional<user_data_auth::InvalidateAuthSessionReply> reply) {
   // The auth session is useless even if we failed to invalidate it.
-  context->SetAuthSessionId("");
+  context->ResetAuthSessionId();
 
   auto error = user_data_auth::ReplyToCryptohomeError(reply);
   if (error != user_data_auth::CRYPTOHOME_ERROR_NOT_SET &&
@@ -552,7 +552,6 @@ void AuthPerformer::OnInvalidateAuthSession(
     return;
   }
 
-  context->SetAuthSessionId("");
   std::move(callback).Run(std::move(context), absl::nullopt);
 }
 
