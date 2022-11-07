0e96d8e6668d8271b9ceda579a3fb9267a28e5af
Olga Sharonova <olka@chromium.org>
2022-11-07 09:30:09 +0000

    Reduce verbosity of some Mac audio logs.
    Bug: 0
    Change-Id: Iaea8648ee4d5edc979123fa0953a704f4bea5a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003179
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Fredrik Hernqvist <fhernqvist@google.com>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068018}

--- media/audio/mac/audio_manager_mac.cc
+++ media/audio/mac/audio_manager_mac.cc
@@ -330,7 +330,7 @@ static bool GetDeviceChannels(AudioDeviceID device,
       return false;
     }
 
-    DVLOG(1) << __FUNCTION__ << " Input channels: " << *channels;
+    DVLOG(2) << __FUNCTION__ << " Input channels: " << *channels;
     return true;
   }
 
@@ -438,7 +438,7 @@ static bool GetDeviceChannels(AudioUnit audio_unit,
     }
   }
 
-  DVLOG(1) << __FUNCTION__ << " Output channels: " << *channels;
+  DVLOG(2) << __FUNCTION__ << " Output channels: " << *channels;
   return true;
 }
 
