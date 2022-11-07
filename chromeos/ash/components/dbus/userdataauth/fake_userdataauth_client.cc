da583d678dff3c342056000994f3517da193a0d0
Martin Bidlingmaier <mbid@google.com>
2022-11-07 16:24:00 +0000

    Add fake userdataauth test method to destroy sessions
    Bug: b:245293194
    Change-Id: If9b0b6e700c656e36fb46367004f00ce351b51e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3916475
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068131}

--- chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.cc
+++ chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.cc
@@ -541,6 +541,10 @@ std::string FakeUserDataAuthClient::TestApi::AddSession(
   return auth_session_id;
 }
 
+void FakeUserDataAuthClient::TestApi::DestroySessions() {
+  g_instance->auth_sessions_.clear();
+}
+
 FakeUserDataAuthClient::UserCryptohomeState&
 FakeUserDataAuthClient::TestApi::GetUserState(
     const cryptohome::AccountIdentifier& account_id) {
