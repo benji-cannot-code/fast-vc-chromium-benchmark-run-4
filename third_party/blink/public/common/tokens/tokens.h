8e2b00c949300e266082c748333e07cb71a05bdb
Yuki Shiino <yukishiino@chromium.org>
2022-11-07 09:48:21 +0000

    ShadowRealm: Implement ShadowRealm global object in Blink (1/N)
    This patch implements ShadowRealm[1]'s global objects on
    Blink side. Instances of blink::ShadowRealmGlobalScope will be
    used as global objects just like WorkerOrWorkletGlobalScope.
    
    This patch implements the mostly minimum implementation of
    EventTarget + ExecutionContext as the first step.
    
    [1] https://github.com/tc39/proposal-shadowrealm/blob/main/explainer.md
    
    Bug: 1281880
    Change-Id: If218dce172ca58b51b0698a2fa371884cd1f621a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999460
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068023}

--- third_party/blink/public/common/tokens/tokens.h
+++ third_party/blink/public/common/tokens/tokens.h
@@ -87,6 +87,10 @@ using WorkletToken = MultiToken<AnimationWorkletToken,
                                 PaintWorkletToken>;
 
 ////////////////////////////////////////////////////////////////////////////////
+// SHADOW REALM TOKENS
+using ShadowRealmToken = base::TokenType<class ShadowRealmTokenTypeMarker>;
+
+////////////////////////////////////////////////////////////////////////////////
 // OTHER TOKENS
 //
 // Keep this section last.
@@ -111,7 +115,8 @@ using ExecutionContextToken = MultiToken<LocalFrameToken,
                                          AnimationWorkletToken,
                                          AudioWorkletToken,
                                          LayoutWorkletToken,
-                                         PaintWorkletToken>;
+                                         PaintWorkletToken,
+                                         ShadowRealmToken>;
 
 // Identifies a blink::PortalContents / blink::HTMLPortalElement in the
 // renderer process, and a content::Portal in the browser process.
