c6583d3d4224ee79bd3a21dd362d138c4965ee8a
Steinar H. Gunderson <sesse@chromium.org>
2022-11-07 16:34:22 +0000

    Make image elements compatible with MISU.
    <img> elements are ineligible for incremental style updates on inline
    style (MISU), since they have custom style callbacks. However, in most cases
    (i.e., when the image is not replaced by fallback content), the custom
    style behaves identically to regular style. Thus, only set the custom
    style flag if we actually have custom style, leaving it eligible for
    MISU in all other cases. This helps ~7% on MotionMark's “leaves” benchmark.
    
    All the heavy lifting (profiling and identifying the issue) by vmiura@.
    
    Change-Id: I246b583395801f38fef9a6e6221caa50c82ba82c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008160
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068137}

--- third_party/blink/renderer/core/dom/node.h
+++ third_party/blink/renderer/core/dom/node.h
@@ -1142,6 +1142,9 @@ class CORE_EXPORT Node : public EventTarget {
   void SetHasCustomStyleCallbacks() {
     SetFlag(true, kHasCustomStyleCallbacksFlag);
   }
+  void UnsetHasCustomStyleCallbacks() {
+    SetFlag(false, kHasCustomStyleCallbacksFlag);
+  }
 
   void SetTreeScope(TreeScope* scope) { tree_scope_ = scope; }
   void SetIsFinishedParsingChildren(bool value) {
