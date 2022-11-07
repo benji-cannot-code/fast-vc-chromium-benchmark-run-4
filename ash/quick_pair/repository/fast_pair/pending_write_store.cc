e3157f840db7f0d026dbe8a2b0f1e5aeda8f7e8b
Brando <brandosocarras@google.com>
2022-11-07 21:12:23 +0000

    [Fast Pair] Interchangeable Pending adds and writes changed to writes
    The terms add and write were being used interchangeably to refer
    to the process of writing a PendingWrite to Footprints when
    network connection is unavailable initially and made available
    at a later time.
    
    	- PendingWriteStore and FastPairImpl unit tests
    	ran with no issue.
    	- Initial Pair was tested on DUT without issue.
    	- To test the pending write store, while Pixel Buds Pro were
    	advertising, Connect was clicked on the initial pair
    	notification and wifi was immediately toggled off. Seconds
    	later, it was toggled on again, network connection was
    	reestablished, and Pixel Buds Pro were observed as
    	saved to account.
    
    Test: 
    Fixed: b/257104274
    Change-Id: I96e2008a5b60b259fd22d4f491774f3559fc3232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999712
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068314}

--- ash/quick_pair/repository/fast_pair/pending_write_store.cc
+++ ash/quick_pair/repository/fast_pair/pending_write_store.cc
@@ -44,7 +44,7 @@ PendingWriteStore::PendingDelete::PendingDelete(
 
 PendingWriteStore::PendingDelete::~PendingDelete() = default;
 
-void PendingWriteStore::AddPairedDevice(const std::string& mac_address,
+void PendingWriteStore::WritePairedDevice(const std::string& mac_address,
                                         const std::string& hex_model_id) {
   PrefService* pref_service =
       QuickPairBrowserDelegate::Get()->GetActivePrefService();
@@ -57,7 +57,7 @@ void PendingWriteStore::AddPairedDevice(const std::string& mac_address,
 }
 
 std::vector<PendingWriteStore::PendingWrite>
-PendingWriteStore::GetPendingAdds() {
+PendingWriteStore::GetPendingWrites() {
   std::vector<PendingWriteStore::PendingWrite> list;
   PrefService* pref_service =
       QuickPairBrowserDelegate::Get()->GetActivePrefService();
