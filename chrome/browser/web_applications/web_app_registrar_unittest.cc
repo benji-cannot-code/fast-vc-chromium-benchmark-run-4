a5e9778509ecc2a0bcc031af10af06b91340e903
Robbie McElrath <rmcelrath@chromium.org>
2022-11-07 23:17:39 +0000

    iwa: Check WebApp::isolation_data() in WebAppRegistrar::IsIsolated
    This CL updates WebAppRegistrar::IsIsolated to check for IsolationData
    in the WebApp in addition to the isolated_storage flag. The
    isolated_storage check will be removed once we've completely migrated
    to the new installation flow.
    
    Bug: 1333966
    Change-Id: I2973b821e80bfe6de91827227cde225ef70c43e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005452
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068390}

--- chrome/browser/web_applications/web_app_registrar_unittest.cc
+++ chrome/browser/web_applications/web_app_registrar_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/web_applications/commands/run_on_os_login_command.h"
+#include "chrome/browser/web_applications/isolation_data.h"
 #include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/test/fake_web_app_database_factory.h"
 #include "chrome/browser/web_applications/test/fake_web_app_provider.h"
@@ -979,6 +980,8 @@ TEST_F(WebAppRegistrarTest,
   web_app->SetUserDisplayMode(UserDisplayMode::kBrowser);
   web_app->SetIsLocallyInstalled(true);
   web_app->SetStorageIsolated(true);
+  web_app->SetIsolationData(IsolationData(IsolationData::DevModeProxy{
+      .proxy_url = url::Origin::Create(GURL("http://127.0.0.1:8080"))}));
 
   RegisterApp(std::move(web_app));
 
