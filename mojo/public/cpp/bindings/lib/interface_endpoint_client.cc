fa56c97159941cf1f7c9f63ab8df048a41e395c2
Harkiran Bolaria <hbolaria@google.com>
2022-11-07 11:11:48 +0000

    Long-running task tracking to include mojo IPC metadata.
    In the scheduler.long_tasks tracing category, some long
    tasks will be posted via the mojo interface endpoint
    client. In such cases, we want to include mojo IPC hash,
    location iid, and interface name so that trace events
    are useful. Plumb these values to the LongTaskTracker
    when they are known.
    
    See example trace where mojo IPC metadata is included in
    LongTaskTracker events:
    https://ui.perfetto.dev/#!/?s=2d61ea229532e9f4d56110e9fa3fdcd57483aa97d3bab98ba219415324b3bfd
    
    This trace additionally shows trace logging from
    InterfaceEndpointClient to show that the correct metadata
    is recorded.
    
    Bug: 1375779
    Change-Id: I1b1349b30298ec0de0631b229b8e8a1060e128dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972498
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068043}

--- mojo/public/cpp/bindings/lib/interface_endpoint_client.cc
+++ mojo/public/cpp/bindings/lib/interface_endpoint_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/bind_post_task.h"
+#include "base/task/common/task_annotator.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/thread_local.h"
 #include "base/trace_event/interned_args_helper.h"
@@ -922,6 +923,13 @@ bool InterfaceEndpointClient::HandleValidatedMessage(Message* message) {
               });
 
   DCHECK_EQ(handle_.id(), message->interface_id());
+
+  // Sync messages can be sent and received at arbitrary points in time and we
+  // should not associate them with the top-level scheduler task.
+  if (!message->has_flag(Message::kFlagIsSync)) {
+    const auto method_info = method_info_callback_(*message);
+    base::TaskAnnotator::OnIPCReceived(interface_name_, method_info);
+  }
 
   if (encountered_error_) {
     // This message is received after error has been encountered. For associated
