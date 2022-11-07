2bac40301ba319760ba28e82c2a96345f5ae8c05
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-11-07 22:15:43 +0000

    Import wpt@ffa757e3a82f235b46bc318c4ad51eea0278ae20
    Using wpt-import in Chromium 6da5f95fba6f5b4d5938870e14422c9d7f8a2ea3.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/IndexedDB
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I262cb3d8213b548c9272011b8d9ea742abe75b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010058
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1068348}

--- third_party/blink/web_tests/external/wpt/IndexedDB/idbindex_keyPath.any.js
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbindex_keyPath.any.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: title=IndexedDB: IDBIndex keyPath attribute - same object
+// META: title=IndexedDB: IDBIndex keyPath attribute
 // META: script=resources/support.js
 
 indexeddb_test(
@@ -29,3 +29,47 @@ indexeddb_test(
     t.done();
   },
   `IDBIndex's keyPath attribute returns the same object.`);
+
+  indexeddb_test(
+  (t, db) => {
+    const store = db.createObjectStore('store', {autoIncrement: true});
+    store.createIndex('index', ['a']);
+
+    store.add({a: 1, b: 2, c: 3})
+  },
+  (t, db) => {
+    const tx = db.transaction('store', 'readonly', {durability: 'relaxed'});
+    const store = tx.objectStore('store');
+    const index = store.index('index');
+    const cursorReq = index.openCursor();
+
+    cursorReq.onsuccess = t.step_func_done((e) => {
+      const expectedKeyValue = [1];
+      const actualKeyValue = e.target.result.key;
+
+      assert_array_equals(actualKeyValue, expectedKeyValue, "An array keypath should yield an array key");
+    });
+  },
+  `IDBIndex's keyPath array with a single value`);
+
+  indexeddb_test(
+  (t, db) => {
+    const store = db.createObjectStore('store', {autoIncrement: true});
+    store.createIndex('index', ['a', 'b']);
+
+    store.add({a: 1, b: 2, c: 3})
+  },
+  (t, db) => {
+    const tx = db.transaction('store', 'readonly', {durability: 'relaxed'});
+    const store = tx.objectStore('store');
+    const index = store.index('index');
+    const cursorReq = index.openCursor();
+
+    cursorReq.onsuccess = t.step_func_done((e) => {
+      const expectedKeyValue = [1, 2];
+      const actualKeyValue = e.target.result.key;
+
+      assert_array_equals(actualKeyValue, expectedKeyValue, "An array keypath should yield an array key");
+    });
+  },
+  `IDBIndex's keyPath array with multiple values`);
