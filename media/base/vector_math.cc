eb15bc50506674714965a89625afc80f4cff2523
Stephan Hartmann <stha09@googlemail.com>
2022-11-07 17:13:30 +0000

    IWYU: add cmath for std::pow in vector_math
    Bug: 957519
    Change-Id: I3240f39e6cbbf07d04836ad803fd4281174ad991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008162
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1068157}

--- media/base/vector_math.cc
+++ media/base/vector_math.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/vector_math_testing.h"
 
 #include <algorithm>
+#include <cmath>
 
 #include "base/check_op.h"
 #include "base/cpu.h"
