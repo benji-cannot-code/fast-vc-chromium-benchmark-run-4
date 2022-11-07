0e0af00beadc480b94ab49c4a2267014efa2c361
Shivani Patel <shivpatel@google.com>
2022-11-07 21:21:37 +0000

    Reland "Add downscale and encode latency tracking to chrome region search" to fix build dependency error
    This is a reland of commit 992235407619bb82d1e67a7386a229552dd7da6a
    
    Original change's description:
    > Add downscale and encode latency tracking to chrome region search
    >
    > Bug: b/256891782
    > Change-Id: I014422be622d7cedad688ad76d6085c0e87fd22b
    >
    > Low-Coverage-Reason: This CL does not change behavior and relies on existing tests.
    > Change-Id: I014422be622d7cedad688ad76d6085c0e87fd22b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997911
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Juan Mojica <juanmojica@google.com>
    > Commit-Queue: Shivani Patel <shivpatel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1067877}
    
    Bug: b/256891782
    Change-Id: I0ea30c253b7aca6ecbf271fb55c4c694e66bbe5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008665
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Shivani Patel <shivpatel@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068319}

--- chrome/browser/lens/region_search/lens_region_search_controller.cc
+++ chrome/browser/lens/region_search/lens_region_search_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "components/lens/lens_entrypoints.h"
 #include "components/lens/lens_features.h"
+#include "components/lens/lens_metadata.mojom.h"
 #include "components/lens/lens_rendering_environment.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -154,6 +155,17 @@ void LensRegionSearchController::RecordRegionSizeRelatedMetrics(
       GetAspectRatioFromSize(region_height, region_width));
 }
 
+bool LensRegionSearchController::NeedsDownscale(gfx::Image image) {
+  if (image.Height() * image.Width() < features::GetMaxAreaForRegionSearch()) {
+    return false;
+  }
+  if (image.Width() < features::GetMaxPixelsForRegionSearch() &&
+      image.Height() < features::GetMaxPixelsForRegionSearch()) {
+    return false;
+  }
+  return true;
+}
+
 void LensRegionSearchController::OnCaptureCompleted(
     const image_editor::ScreenshotCaptureResult& result) {
   // Close all open UI overlays and bubbles.
@@ -170,6 +182,8 @@ void LensRegionSearchController::OnCaptureCompleted(
     return;
   }
 
+  std::vector<lens::mojom::LatencyLogPtr> log_data;
+
   const gfx::Image& captured_image = result.image;
   // If image is empty, then record UMA and close.
   if (captured_image.IsEmpty()) {
@@ -181,7 +195,18 @@ void LensRegionSearchController::OnCaptureCompleted(
   // Record region size related UMA histograms according to region and screen.
   RecordRegionSizeRelatedMetrics(result.screen_bounds, captured_image.Size());
 
+  if (NeedsDownscale(captured_image)) {
+    log_data.push_back(lens::mojom::LatencyLog::New(
+        lens::mojom::Phase::DOWNSCALE_START, captured_image.Size(), gfx::Size(),
+        lens::mojom::ImageFormat::ORIGINAL, base::Time::Now()));
+  }
   const gfx::Image& image = ResizeImageIfNecessary(captured_image);
+  if (NeedsDownscale(captured_image)) {
+    log_data.push_back(lens::mojom::LatencyLog::New(
+        lens::mojom::Phase::DOWNSCALE_END, captured_image.Size(), image.Size(),
+        lens::mojom::ImageFormat::ORIGINAL, base::Time::Now()));
+  }
+
   CoreTabHelper* core_tab_helper =
       CoreTabHelper::FromWebContents(web_contents());
   if (!core_tab_helper) {
@@ -201,7 +226,8 @@ void LensRegionSearchController::OnCaptureCompleted(
         image, captured_image.Size(), entry_point,
         /* is_region_search_request= */ true,
         /* is_side_panel_enabled_for_feature= */
-        lens::features::IsLensSidePanelEnabledForRegionSearch());
+        lens::features::IsLensSidePanelEnabledForRegionSearch(),
+        std::move(log_data));
   } else {
     core_tab_helper->SearchByImage(image, captured_image.Size());
   }
