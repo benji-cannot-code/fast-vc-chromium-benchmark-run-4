9b0db3dbc199a55f7969a739f4ff3e555e149e60
Rubin Deliallisi <rubindl@chromium.org>
2022-11-07 10:26:29 +0000

    [Privacy Guide]: Cookie fragment visibility
    Implementation of the conditions for which the Cookies page should or should not be displayed.
    
    Bug: 1238896
    Change-Id: Iab4e9bca3ae8e71cf1b9b40991992dce2ddb593c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005955
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068028}

--- chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImpl.java
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImpl.java
@@ -5,9 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.privacy_guide;
 
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingBridge;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingState;
 import org.chromium.chrome.browser.sync.SyncService;
+import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
+import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.content_settings.CookieControlsMode;
+import org.chromium.components.content_settings.PrefNames;
+import org.chromium.components.user_prefs.UserPrefs;
 
 /**
  * Computes for each privacy guide step whether it should be displayed or not.
@@ -26,7 +32,11 @@ class StepDisplayHandlerImpl implements StepDisplayHandler {
 
     @Override
     public boolean shouldDisplayCookies() {
-        // TODO: Check the settings for the CookiesFragment.
-        return true;
+        boolean allowCookies = WebsitePreferenceBridge.isCategoryEnabled(
+                Profile.getLastUsedRegularProfile(), ContentSettingsType.COOKIES);
+        @CookieControlsMode
+        int cookieControlsMode = UserPrefs.get(Profile.getLastUsedRegularProfile())
+                                         .getInteger(PrefNames.COOKIE_CONTROLS_MODE);
+        return allowCookies && cookieControlsMode != CookieControlsMode.OFF;
     }
 }
