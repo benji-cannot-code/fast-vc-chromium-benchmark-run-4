75ed4927eaf772bbdd62b0968395feb340b22987
Aliona DANGLA <alionadangla@google.com>
2022-11-07 23:53:12 +0000

    [iOS] Conditionally display show previous and next tab shortcut
    This CL display show next tab and show previous tab only when there is
    valid web pages and there is more than 1 tab.
    
    See the following screens. When there is only one tab:
    https://drive.google.com/file/d/1fYIzgQ7Nl5Jnd9tZG7gzHMZ7pN2KLfuL/view?usp=share_link
    
    When there is two tabs:
    https://drive.google.com/file/d/19ADkWnFkmpCtgEAHi8zqxmpXpBsfAw-d/view?usp=share_link
    
    Bug: 1382203
    Change-Id: I2c2c2121dee3c4567fa2cbbbdbaf0f1ddd87883a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010778
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068412}

--- ios/chrome/browser/ui/browser_view/key_commands_provider.mm
+++ ios/chrome/browser/ui/browser_view/key_commands_provider.mm
@@ -180,8 +180,6 @@ using base::UserMetricsAction;
   }
   if (sel_isEqual(action, @selector(keyCommand_openLocation)) ||
       sel_isEqual(action, @selector(keyCommand_closeTab)) ||
-      sel_isEqual(action, @selector(keyCommand_showNextTab)) ||
-      sel_isEqual(action, @selector(keyCommand_showPreviousTab)) ||
       sel_isEqual(action, @selector(keyCommand_showBookmarks)) ||
       sel_isEqual(action, @selector(keyCommand_addToBookmarks)) ||
       sel_isEqual(action, @selector(keyCommand_reload)) ||
@@ -208,6 +206,13 @@ using base::UserMetricsAction;
   }
   if (sel_isEqual(action, @selector(keyCommand_close))) {
     return self.canDismissModals;
+  }
+  if (sel_isEqual(action, @selector(keyCommand_showNextTab)) ||
+      sel_isEqual(action, @selector(keyCommand_showPreviousTab))) {
+    WebStateList* webStateList = self.browser->GetWebStateList();
+    return webStateList &&
+           webStateList->active_index() != WebStateList::kInvalidIndex &&
+           self.tabsCount > 1;
   }
   return [super canPerformAction:action withSender:sender];
 }
