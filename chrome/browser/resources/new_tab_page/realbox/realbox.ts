f976df9254fda26f39c0006c0af05b4636f1914f
Roman Arora <romanarora@chromium.org>
2022-11-07 20:25:57 +0000

    [NTP] Realbox comprehensive icons
    Applies themed voice search icons to the Realbox component when
    comprehensive theming and theme realbox feature flags are enabled.
    
    - Light themes get a dark icon.
    - Dark themes get a light icon.
    - White background themes retain the multi-colored icon.
    
    Screenshots:
    https://drive.google.com/file/d/100qazkFtCryhV8kR9qxJubc7HeDTokiC/view
    https://drive.google.com/file/d/1SwlyNVbOMaJILWB9m_eYtozlMtvEAh0K/view
    https://drive.google.com/file/d/1IX8YLFvSJCFe86QsU2B6h7Eii7KyWHAM/view
    
    Bug: 1378284
    Change-Id: I7fd039938290bf4a8ec782a20af376fca6ab72c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993507
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068287}

--- chrome/browser/resources/new_tab_page/realbox/realbox.ts
+++ chrome/browser/resources/new_tab_page/realbox/realbox.ts
@@ -84,6 +84,12 @@ export class RealboxElement extends PolymerElement {
         reflectToAttribute: true,
       },
 
+      /** Whether to display themed icons for the current background color. */
+      themedIcons: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
+
       //========================================================================
       // Private properties
       //========================================================================
@@ -194,6 +200,7 @@ export class RealboxElement extends PolymerElement {
   matchesAreVisible: boolean;
   matchSearchbox: boolean;
   realboxLensSearchEnabled: boolean;
+  themedIcons: boolean;
   private charTypedTime_: number;
   private isDeletingInput_: boolean;
   private lastIgnoredEnterEvent_: KeyboardEvent|null;
