dcbb7258797f904e4c94412717ce793f3450dcb4
dljames <dljames@google.com>
2022-11-07 23:13:36 +0000

    [ SavedTabGroup ] Add feature flag variation for #tab-groups-save
    Adds a sync_integration variation to the #tab-groups-save feature flag.
    This will be used to test cross device syncing of saved tab groups when
    the feature is ready. Accordingly, the default enabled value for
    #tab-groups-save will now only include local persistence of saved tab
    groups.
    
    Extends the expiration of the feature flag from 110 -> 115.
    
    Change-Id: If7baa312e9065a348ddb230a94e9ab5c3709d3f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006478
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Auto-Submit: Darryl James <dljames@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068388}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1349,6 +1349,14 @@ const FeatureEntry::FeatureVariation kJourneysVisitDedupingVariations[] = {
      std::size(kJourneysVisitDedupingUseHostParams), nullptr},
 };
 
+const FeatureEntry::FeatureParam kTabGroupsSaveWithSyncIntegration[] = {
+    {"sync_integration", "true"},
+};
+const FeatureEntry::FeatureVariation kTabGroupsSaveVariations[] = {
+    {" with sync integration", kTabGroupsSaveWithSyncIntegration,
+     std::size(kTabGroupsSaveWithSyncIntegration), nullptr},
+};
+
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC) || \
     BUILDFLAG(IS_WIN) || BUILDFLAG(IS_FUCHSIA)
 const FeatureEntry::FeatureParam kOmniboxDocumentProviderServerScoring[] = {
@@ -6031,7 +6039,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"tab-groups-save", flag_descriptions::kTabGroupsSaveName,
      flag_descriptions::kTabGroupsSaveDescription, kOsDesktop,
-     FEATURE_VALUE_TYPE(features::kTabGroupsSave)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(features::kTabGroupsSave,
+                                    kTabGroupsSaveVariations,
+                                    "TabGroupsSave")},
 
     {flag_descriptions::kScrollableTabStripFlagId,
      flag_descriptions::kScrollableTabStripName,
