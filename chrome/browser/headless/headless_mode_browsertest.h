228c0880a6700ada2ab0f8c47647b7356f6ac096
Peter Kvitek <kvitekp@chromium.org>
2022-11-07 19:32:43 +0000

    [headless] Remove deprecated NativeHeadlessChrome method.
    Native Headless mode is now officially called new Headless mode and
    the traditional headless mode (an alternate browser implementation
    found in //headless) became the old Headless mode.
    
    This CL removes deprecated headless::NativeHeadlessChrome and
    backward compatible 'chrome' --headless switch value.
    
    It is a follow up to https://crrev.com/c/4000223
    
    Bug: 1380881
    Change-Id: I6d53cebff4a67bd7cec4a8a9c51d394fd548ec4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006816
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068252}

--- chrome/browser/headless/headless_mode_browsertest.h
+++ chrome/browser/headless/headless_mode_browsertest.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class HeadlessModeBrowserTest : public InProcessBrowserTest {
  public:
-  static constexpr char kHeadlessSwitchValue[] = "chrome";
+  static constexpr char kHeadlessSwitchValue[] = "new";
 
   HeadlessModeBrowserTest() = default;
 
