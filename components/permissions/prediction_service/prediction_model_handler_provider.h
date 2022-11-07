f7c582daa232e319b9640ed164a3d1d31639ec90
Ravjit <ravjit@chromium.org>
2022-11-07 13:26:49 +0000

    Fix issue in CPSS due to delayed model creation
    CPSS on-device predictions have been reducing because tflite model is unavailable. This is happening because the Model handler is created too late. Initialize the model handler earlier.
    
    Bug: 1381147
    Change-Id: I12637a54f076f3c3d11f74e17f17bc704305576b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003070
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068066}

--- components/permissions/prediction_service/prediction_model_handler_provider.h
+++ components/permissions/prediction_service/prediction_model_handler_provider.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_MODEL_HANDLER_PROVIDER_H_
 #define COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_MODEL_HANDLER_PROVIDER_H_
 
+#include <memory>
+
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
 #include "components/permissions/prediction_service/prediction_model_handler.h"
@@ -16,7 +18,7 @@ class PredictionModelHandlerProvider : public KeyedService {
  public:
   explicit PredictionModelHandlerProvider(
       optimization_guide::OptimizationGuideModelProvider* optimization_guide);
-  ~PredictionModelHandlerProvider() override = default;
+  ~PredictionModelHandlerProvider() override;
   PredictionModelHandlerProvider(const PredictionModelHandlerProvider&) =
       delete;
   PredictionModelHandlerProvider& operator=(
@@ -25,8 +27,9 @@ class PredictionModelHandlerProvider : public KeyedService {
   PredictionModelHandler* GetPredictionModelHandler(RequestType request_type);
 
  private:
-  PredictionModelHandler* notification_prediction_model_handler_;
-  PredictionModelHandler* geolocation_prediction_model_handler_;
+  std::unique_ptr<PredictionModelHandler>
+      notification_prediction_model_handler_;
+  std::unique_ptr<PredictionModelHandler> geolocation_prediction_model_handler_;
 };
 }  // namespace permissions
 #endif  // COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_MODEL_HANDLER_PROVIDER_H_
