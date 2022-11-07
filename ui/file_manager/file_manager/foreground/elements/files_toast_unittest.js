b81118734385fd45b8635b7b94c79eb3d4560be7
Luciano Pacheco <lucmult@chromium.org>
2022-11-07 05:03:58 +0000

    Files F2: Convert <files-toast> to TS
    This is the first Polymer element converted to TS.
    
    Use externs to be able to Closure compile the JS using the toast,
    because the JS generated from TS isn't fully understood by Closure.
    
    Bug: b:256982613
    Change-Id: I1fe44c0136d6cf5b81b5be0ad21005e9aad1a9ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999364
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067977}

--- ui/file_manager/file_manager/foreground/elements/files_toast_unittest.js
+++ ui/file_manager/file_manager/foreground/elements/files_toast_unittest.js
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import './files_toast.js';
 
-import {FilesToast} from './files_toast.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 export function setUpPage() {
   document.body.innerHTML += '<files-toast></files-toast>';
