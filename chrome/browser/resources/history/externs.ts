a868b19bfb8433928b75ee1a4f75352acc655590
Jood Hajeer <jood@google.com>
2022-11-08 09:53:54 +0000

    Remove DeviceType from ForeignSession TS Code.
    DeviceType in ForeignSession is not used.
    Removing this is needed to simplify DeviceInfo refactoring.
    
    Bug: 1358151
    Change-Id: I6b55501812d5f731fe1bc17d7812f06df2b5985a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003168
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068529}

--- chrome/browser/resources/history/externs.ts
+++ chrome/browser/resources/history/externs.ts
@@ -86,7 +86,6 @@ export interface ForeignSessionWindow {
  */
 export interface ForeignSession {
   collapsed: boolean;
-  deviceType: string;
   name: string;
   modifiedTime: string;
   tag: string;
