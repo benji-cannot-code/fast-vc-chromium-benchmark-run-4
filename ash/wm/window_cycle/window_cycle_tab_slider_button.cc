0ea224fafcfcc504a2c7df76307cc05132ae4cac
Sammie Quon <sammiequon@chromium.org>
2022-11-08 04:34:48 +0000

    wm: Some minor cleanup to WindowCycleTabSlider
      - Remove some headers. Most likely left behind from a file split.
      - Remove some unneeded layers.
      - Updated to use new views stuff BoxLayoutView and AnimationBuilder.
    
    Bug: none
    Test: manual
    Change-Id: If979a435db4fb8f88cd469c2298a80c5ef7cc29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007217
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068496}

--- ash/wm/window_cycle/window_cycle_tab_slider_button.cc
+++ ash/wm/window_cycle/window_cycle_tab_slider_button.cc
@@ -6,12 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_cycle/window_cycle_tab_slider_button.h"
 
 #include "ash/style/ash_color_provider.h"
-#include "base/strings/utf_string_conversions.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/gfx/canvas.h"
-#include "ui/views/background.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/layout/box_layout.h"
 
 namespace ash {
 
