d5fb38c7e131a50c54a9013c784b17f1a4c1ad52
avynn <avynn@google.com>
2022-11-08 20:24:37 +0000

    Saved Desk: Fix parsing error crash
    Fixed a bug wherein reading an un-parsable file resulted in an
    erroneous access to a nullptr.  Isolated bug in a test and verified
    bug fixed with test.
    
    TESTED: LocalDeskDatManagerTest.*
    Bug: b/258029726
    Change-Id: I64a84b829d0142ca3260a642cb83ffa49d4773ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014680
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068751}

--- components/desks_storage/core/local_desk_data_manager.cc
+++ components/desks_storage/core/local_desk_data_manager.cc
@@ -463,6 +463,10 @@ LocalDeskDataManager::LoadCacheOnBackgroundSequence(
     std::unique_ptr<ash::DeskTemplate> entry =
         ReadFileToTemplate(fully_qualified_path);
 
+    // TODO(b/248645596): Record metrics about files that failed to parse.
+    if (entry == nullptr)
+      continue;
+
     // Rename file for saved desk if uuid in file and file name are different.
     std::string entry_uuid_string = entry->uuid().AsLowercaseString();
     entry_uuid_string.append(kFileExtension);
@@ -475,10 +479,7 @@ LocalDeskDataManager::LoadCacheOnBackgroundSequence(
       }
     }
 
-    // TODO(crbug/1359398): Record metrics about files that failed to parse.
-    if (entry) {
-      entries.push_back(std::move(entry));
-    }
+    entries.push_back(std::move(entry));
   }
   return {CacheStatus::kOk, std::move(entries)};
 }
