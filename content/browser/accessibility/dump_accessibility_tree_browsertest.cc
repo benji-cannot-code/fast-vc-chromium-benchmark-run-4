5e3df7d154655a98e4572ffee88c326fb0e9d169
Aaron Leventhal <aleventhal@google.com>
2022-11-08 19:58:52 +0000

    Hypertext not recomputed after object not becomes leaf
    When an object becomes a leaf, it's hypertext should become the
    empty string. However, if the change that causes it to become a leaf
    is too deep in its subtree, then the hypertext is not recomputed.
    The failing test in patchset 1 shows the issue -- it triggers a DCHECK that finds problems with hypertext links not returning an IndexInParent.
    
    The problem occurs because Blink a11y only serializes node that
    actually change from its point of view. In the failing test, that is the node hidden when aria-hidden changes to true, and its included-in-tree parent. However, some included-in-tree parents are ignored on the browser/platform side. It's the first unignored ancestor that needs to compute the hypertext. However, OnAtomicUpdateFinished() does not include the node because it was not part of the serialization.
    
    The fix is to patch the stage in AXTree::Unserialize() where unignored ancestors have their "unignored" values updated, specifically they update their unignored children. For each unignored ancestor that is updated, we now also add the ancestor to the list of nodes changed for onAtomicUpdateFinished(), where hypertext is recomputed.
    
    Note that this CL is a necessary to synchronous a11y in CL:3914885.
    
    Bug: none
    Change-Id: I9a553995752dd252b7bcc88b55da0a4b2da22546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011858
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068733}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -3514,6 +3514,10 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, ReusedMapChangeUsemap) {
   RunRegressionTest(FILE_PATH_LITERAL("reused-map-change-usemap.html"));
 }
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, RootBecomesLeaf) {
+  RunRegressionTest(FILE_PATH_LITERAL("root-becomes-leaf.html"));
+}
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilitySlotCreationCrash) {
   RunRegressionTest(FILE_PATH_LITERAL("slot-creation-crash.html"));
