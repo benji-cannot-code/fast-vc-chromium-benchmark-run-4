f73bab6c0979ec427b5f67c24e696e84fee5d0a2
Aishwarya Rajesh <aishwaryarj@google.com>
2022-11-08 17:40:33 +0000

    [RDSG] Fix crash attributed to reload of destroyed tab
    Currently, a click on the desktop site global setting opt-in message updates the setting and attempts to reload the tab on which the message is shown to use the desktop user agent.
    
    Since this is a window-scoped message, the message could be accepted on a tab that is different from the tab on which it was shown. If the tab on which the message was shown is destroyed at this time, it will cause a crash as seen in the crbug.
    
    The intention is to reload the current activity tab when the message is accepted; this CL uses the ActivityTabProvider to get a reference to the current activity tab that can be reloaded when the message is accepted.
    
    Bug: 1381841
    Change-Id: Ic4e350589645ab652c51b08693b23e8b46f69f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008864
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068605}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/RequestDesktopUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/RequestDesktopUtils.java
@@ -15,6 +15,7 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.SysUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -574,11 +575,13 @@ public class RequestDesktopUtils {
      * @param profile The current {@link Profile}.
      * @param messageDispatcher The {@link MessageDispatcher} to enqueue the message.
      * @param context The current context.
-     * @param tab The {@link Tab} where the message is shown.
+     * @param currentTabSupplier The tab {@link ObservableSupplier} that provides a reference to the
+     *         current activity tab.
      * @return Whether the opt-in message was shown.
      */
     public static boolean maybeShowGlobalSettingOptInMessage(double displaySizeInInches,
-            Profile profile, MessageDispatcher messageDispatcher, Context context, Tab tab) {
+            Profile profile, MessageDispatcher messageDispatcher, Context context,
+            ObservableSupplier<Tab> currentTabSupplier) {
         if (messageDispatcher == null) return false;
 
         if (!shouldShowGlobalSettingOptInMessage(displaySizeInInches, profile)) {
@@ -598,14 +601,7 @@ public class RequestDesktopUtils {
                                 resources.getString(R.string.yes))
                         .with(MessageBannerProperties.ON_PRIMARY_ACTION,
                                 () -> {
-                                    updateDesktopSiteGlobalSettingOnUserRequest(profile, true);
-                                    // TODO(crbug.com/1350274): Remove this explicit load when this
-                                    // bug is addressed.
-                                    if (tab != null) {
-                                        tab.loadIfNeeded(
-                                                LoadIfNeededCaller
-                                                        .MAYBE_SHOW_GLOBAL_SETTING_OPT_IN_MESSAGE);
-                                    }
+                                    onGlobalSettingOptInMessageClicked(profile, currentTabSupplier);
                                     return PrimaryActionClickBehavior.DISMISS_IMMEDIATELY;
                                 })
                         .build();
@@ -707,6 +703,17 @@ public class RequestDesktopUtils {
         } else if (isControlGroup && !ChromeFeatureList.isEnabled(syntheticFeatureName)) {
             UmaSessionStats.registerSyntheticFieldTrial(
                     syntheticFeatureName, baseGroupName + CONTROL_GROUP_SUFFIX);
+        }
+    }
+
+    @VisibleForTesting
+    static void onGlobalSettingOptInMessageClicked(
+            Profile profile, ObservableSupplier<Tab> currentTabSupplier) {
+        updateDesktopSiteGlobalSettingOnUserRequest(profile, true);
+        Tab tab = currentTabSupplier.get();
+        // TODO(crbug.com/1350274): Remove this explicit load when this bug is addressed.
+        if (tab != null && !tab.isDestroyed()) {
+            tab.loadIfNeeded(LoadIfNeededCaller.MAYBE_SHOW_GLOBAL_SETTING_OPT_IN_MESSAGE);
         }
     }
 
