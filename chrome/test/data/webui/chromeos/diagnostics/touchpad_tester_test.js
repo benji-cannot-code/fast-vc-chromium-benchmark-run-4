beb34dd291f93125b04e0418fe594738d0807449
Ashley Prasad <ashleydp@google.com>
2022-11-08 16:22:50 +0000

    diagnostics: add frontend touchpad-tester skeleton (1/2)
    - Add new polymer element for touchpad-tester.
    - Add test for touchpad-tester.
    - Update BUILD.gn to include new files.
    
    Bug: b:253021171
    Test: browser_test --gtest_filter=*DiagnosticsAppWithInput* --gtest_also_run_disabled_tests
    Change-Id: Ib378c09537e4173311f24ee892891f8398cb7a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988935
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068568}

---
+++ chrome/test/data/webui/chromeos/diagnostics/touchpad_tester_test.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+
+import {TouchpadTesterElement} from 'chrome://diagnostics/touchpad_tester.js';
+import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+
+import {assertFalse, assertTrue} from '../../chai_assert.js';
+import {isChildVisible} from '../../test_util.js';
+
+suite('touchpadTesterTestSuite', function() {
+  /** @type {?TouchpadTesterElement} */
+  let touchpadTesterElement = null;
+
+  setup(() => {
+    document.body.innerHTML = '';
+  });
+
+  teardown(() => {
+    touchpadTesterElement.remove();
+    touchpadTesterElement = null;
+  });
+
+  /**
+   * Adds tester to page DOM.
+   * @return {!Promise}
+   */
+  function initializeElement() {
+    touchpadTesterElement = /** @type {!TouchpadTesterElement} */ (
+        document.createElement(TouchpadTesterElement.is));
+    assertTrue(!!touchpadTesterElement);
+    document.body.appendChild(touchpadTesterElement);
+
+    return flushTasks();
+  }
+
+  test('VerifyElementInitalizedCorrectly', async () => {
+    await initializeElement();
+    const titleSlotSelector = '#touchpadTesterDialog div[slot=title]';
+    assertFalse(isChildVisible(touchpadTesterElement, titleSlotSelector));
+  });
+});
