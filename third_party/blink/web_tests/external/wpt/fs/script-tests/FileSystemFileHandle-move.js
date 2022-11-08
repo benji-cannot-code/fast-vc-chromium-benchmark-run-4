76a5643336a221321e98b91c311c9b44fe1e9d59
Austin Sullivan <asully@chromium.org>
2022-11-08 22:37:55 +0000

    FSA: Reject overwriting moves with InvalidModificationError
    Moves which would overwrite an existing file currently reject with
    NoModificationAllowedError, which we would like to keep (mostly)
    reserved for file locking.
    
    This change was proposed here:
    https://github.com/web-platform-tests/wpt/commit/b20e8343ce5ff1eada39dc1487b8b28ed903fff6#r85726061
    
    Adds a new FileSystemAccessStatus to facilitate this.
    
    Bug: 1366652, 1381621
    Change-Id: I32fa3c568b88315bd24889f6341d592c69c44970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006320
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068831}

--- third_party/blink/web_tests/external/wpt/fs/script-tests/FileSystemFileHandle-move.js
+++ third_party/blink/web_tests/external/wpt/fs/script-tests/FileSystemFileHandle-move.js
@@ -98,7 +98,7 @@ directory_test(async (t, root) => {
   // Can't move handle to overwrite an existing file.
   await stream.close();
   await promise_rejects_dom(
-      t, 'NoModificationAllowedError', handle.move('file-after'));
+      t, 'InvalidModificationError', handle.move('file-after'));
   assert_array_equals(
       await getSortedDirectoryEntries(root), ['file-after', 'file-before']);
 }, 'move(name) while the destination file has an open writable fails');
@@ -291,10 +291,10 @@ directory_test(async (t, root) => {
   // Assert the file is still in the source directory.
   assert_array_equals(await getSortedDirectoryEntries(dir_src), ['file']);
 
-  // Can't move handle once the writable is closed.
+  // Can't move handle to overwrite an existing file.
   await stream.close();
   await promise_rejects_dom(
-      t, 'NoModificationAllowedError', file.move(dir_dest));
+      t, 'InvalidModificationError', file.move(dir_dest));
   assert_array_equals(await getSortedDirectoryEntries(dir_src), ['file']);
 }, 'move(dir) while the destination file has an open writable fails');
 
@@ -315,10 +315,10 @@ directory_test(async (t, root) => {
   // Assert the file is still in the source directory.
   assert_array_equals(await getSortedDirectoryEntries(dir_src), ['file-src']);
 
-  // Can't move handle once the writable is closed.
+  // Can't move handle to overwrite an existing file.
   await stream.close();
   await promise_rejects_dom(
-      t, 'NoModificationAllowedError', file.move(dir_dest, 'file-dest'));
+      t, 'InvalidModificationError', file.move(dir_dest, 'file-dest'));
   // Assert the file is still in the source directory.
   assert_array_equals(await getSortedDirectoryEntries(dir_src), ['file-src']);
   assert_equals(await getFileContents(file), 'abc');
