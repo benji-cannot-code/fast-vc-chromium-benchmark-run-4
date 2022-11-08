06bcec6237239b97722f199096d6f06e3f516821
Jian Li <jianli@chromium.org>
2022-11-08 00:36:43 +0000

    Update feed content duplication metrics to exclude unviewed content
    The feed contents that have not been viewed may be included in the
    feed refresh. To better reflect the duplication, we excluded those
    duplicate contents that are not viewed before.
    
    Bug: 1373253
    Change-Id: I9489f1800d14510b34d1b0b253681eb6ed5749e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006804
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068433}

--- components/feed/core/v2/feedstore_util.h
+++ components/feed/core/v2/feedstore_util.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_FEED_CORE_V2_FEEDSTORE_UTIL_H_
 #define COMPONENTS_FEED_CORE_V2_FEEDSTORE_UTIL_H_
 
-#include <deque>
 #include <string>
 #include "base/containers/flat_set.h"
 #include "base/strings/string_piece_forward.h"
@@ -26,7 +25,6 @@ class Metadata;
 const char kForYouStreamKey[] = "i";
 const char kFollowStreamKey[] = "w";
 constexpr base::StringPiece kChannelStreamKeyPrefix = "c";
-const int kMaxMostRecentContentHashes = 50;
 
 std::string StreamKey(const feed::StreamType& stream_type);
 feed::StreamType StreamTypeFromId(base::StringPiece key);
@@ -89,12 +87,6 @@ feed::ContentHashSet GetViewContentIds(const Metadata& metadata,
                                        const feed::StreamType& stream_type);
 int32_t ContentHashFromPrefetchMetadata(
     const feedwire::PrefetchMetadata& prefetch_metadata);
-
-// Appends `new_content_hashes` to `Metadata.most_recent_content_hashes` whose
-// size is capped to kMaxMostRecentContentHashes. The oldest content appears
-// at the beginning of the list.
-void AddMostRecentContentHashes(Metadata& metadata,
-                                std::deque<uint32_t> new_content_hashes);
 
 base::flat_set<uint32_t> GetViewedContentHashes(
     const Metadata& metadata,
