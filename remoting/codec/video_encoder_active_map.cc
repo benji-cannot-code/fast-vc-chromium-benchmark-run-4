9f737ad1ae551aea506f38ce66b7bb3806436385
Stephan Hartmann <stha09@googlemail.com>
2022-11-08 18:15:42 +0000

    IWYU: add string.h for memset in remoting::VideoEncoderActiveMap
    Bug: 957519
    Change-Id: Idd65f817a65dce39dc459cac8394fa2c74bd26d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013547
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068651}

--- remoting/codec/video_encoder_active_map.cc
+++ remoting/codec/video_encoder_active_map.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#include <string.h>
+
 #include "base/check_op.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_region.h"
