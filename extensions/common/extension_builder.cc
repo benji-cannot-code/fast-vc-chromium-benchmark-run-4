3b729582ac92d737717c1f3244aaf019fbbcb411
Justin Lulejian <jlulejian@chromium.org>
2022-11-08 23:15:37 +0000

    [Extensions] Refactor similar usages to GetManifestKeyForActionType.
    Bug: 897040
    Change-Id: I6399fa0a34abd44b07c8685a5fe4e91ebee18543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990376
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068845}

--- extensions/common/extension_builder.cc
+++ extensions/common/extension_builder.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "components/crx_file/id_util.h"
 #include "extensions/common/api/content_scripts.h"
+#include "extensions/common/api/extension_action/action_info.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_constants.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -63,20 +64,7 @@ struct ExtensionBuilder::ManifestData {
     }
 
     if (action) {
-      // TODO(devlin): Update this when action_info_test_util.[h|cc] is moved to
-      // //extensions.
-      const char* action_key = nullptr;
-      switch (*action) {
-        case ActionInfo::TYPE_PAGE:
-          action_key = manifest_keys::kPageAction;
-          break;
-        case ActionInfo::TYPE_BROWSER:
-          action_key = manifest_keys::kBrowserAction;
-          break;
-        case ActionInfo::TYPE_ACTION:
-          action_key = manifest_keys::kAction;
-          break;
-      }
+      const char* action_key = ActionInfo::GetManifestKeyForActionType(*action);
       manifest.Set(action_key, std::make_unique<base::DictionaryValue>());
     }
 
