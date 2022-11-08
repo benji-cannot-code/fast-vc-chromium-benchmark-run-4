2ccdbcf7f1cbceb20ad8f6017bab14a9d7b70d23
kylechar <kylechar@chromium.org>
2022-11-08 14:05:29 +0000

    Remove USE_OZONE workaround from keyboard_unittests
    The tests are for Chrome OS so USE_OZONE is always true. The code looks
    to be a workaround from before Chrome OS was entirely switched over to
    ozone and not relevant anymore so just remove it.
    
    Bug: 1377327
    Change-Id: I345dff1bd0b717f15ab44ad501f70ab996a40da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4009164
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068542}

--- ash/keyboard/ui/keyboard_ui_controller_unittest.cc
+++ ash/keyboard/ui/keyboard_ui_controller_unittest.cc
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/test/event_generator.h"
 #include "ui/gfx/geometry/rect.h"
 
-#if defined(USE_OZONE)
-#include "ui/ozone/public/ozone_platform.h"
-#endif
-
 namespace keyboard {
 namespace {
 
@@ -249,14 +245,7 @@ class KeyboardUIControllerTest : public aura::test::AuraTestBase,
             contents_window->IsVisible() && controller_.IsKeyboardVisible());
   }
 
-  void RunLoop(base::RunLoop* run_loop) {
-#if defined(USE_OZONE)
-    // TODO(crbug/776357): Figure out why the initializer randomly doesn't run
-    // for some tests. In the mean time, prevent flaky Ozone crash.
-    ui::OzonePlatform::InitializeForGPU(ui::OzonePlatform::InitParams());
-#endif
-    run_loop->Run();
-  }
+  void RunLoop(base::RunLoop* run_loop) { run_loop->Run(); }
 
   std::unique_ptr<TestFocusController> focus_controller_;
 
