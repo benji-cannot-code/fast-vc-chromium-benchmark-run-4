073929b7d73880f4c12f70f93de7dbbe7fc6d501
Sylvain Defresne <sdefresne@chromium.org>
2022-11-08 15:18:31 +0000

    [ios] Convert time duration constants to base::TimeDelta
    Using base::TimeDelta allow to specify the time unit when defining
    the constant without resorting to putting the unit in the variable
    name.
    
    This also helps converting between different units with the helper
    methods (.InSecondsF(), .InNanosecondsF(), ...).
    
    This CL converts unit test code calling WaitUntilConditionOrTimeout
    to use the new override taking the timeout as base::TimeDelta in
    //ios/chrome/browser/web/image_fetch.
    
    Bug: none
    Change-Id: Ifb8a0768d23645c5acff463e1da44b9c32eba4d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4009262
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068556}

--- ios/chrome/browser/web/image_fetch/image_fetch_tab_helper_unittest.mm
+++ ios/chrome/browser/web/image_fetch/image_fetch_tab_helper_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/test/metrics/histogram_tester.h"
+#import "base/time/time.h"
 #import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/web/image_fetch/image_fetch_java_script_feature.h"
 #import "ios/web/js_messaging/java_script_feature_manager.h"
@@ -36,7 +37,7 @@ using base::test::ios::WaitUntilConditionOrTimeout;
 
 namespace {
 // Timeout for calling on ImageFetchTabHelper::GetImageData.
-const NSTimeInterval kWaitForGetImageDataTimeout = 1.0;
+constexpr base::TimeDelta kWaitForGetImageDataTimeout = base::Seconds(1);
 
 const char kImageUrl[] = "http://www.chrooooooooooome.com/";
 const char kImageData[] = "abc";
