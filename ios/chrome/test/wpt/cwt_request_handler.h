b5838a5c51eaaa4993523969f14e81438cc5eef6
Sylvain Defresne <sdefresne@chromium.org>
2022-11-08 14:22:15 +0000

    [ios] Convert time duration constants to base::TimeDelta
    Using base::TimeDelta allow to specify the time unit when defining
    the constant without resorting to putting the unit in the variable
    name.
    
    This also helps converting between different units with the helper
    methods (.InSecondsF(), .InNanosecondsF(), ...).
    
    This CL converts unit test code calling WaitUntilConditionOrTimeout
    to use the new override taking the timeout as base::TimeDelta in
    //ios/chrome/test/wpt.
    
    Bug: none
    Change-Id: I707d194348ebb4fd6da39a059a9cc2df877cb260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008642
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068548}

--- ios/chrome/test/wpt/cwt_request_handler.h
+++ ios/chrome/test/wpt/cwt_request_handler.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/ios/block_types.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
@@ -144,8 +145,8 @@ class CWTRequestHandler {
   std::string target_tab_id_;
 
   // Timeouts used when performing browser operations.
-  NSTimeInterval script_timeout_;
-  NSTimeInterval page_load_timeout_;
+  base::TimeDelta script_timeout_;
+  base::TimeDelta page_load_timeout_;
 
   // A server for test files used in crash tests.
   net::EmbeddedTestServer test_case_server_;
