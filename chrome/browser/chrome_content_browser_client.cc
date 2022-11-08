b7530e753f84bf8f3cbd5d47553a038d2700cf44
Thiago Perrotta <tperrotta@chromium.org>
2022-11-08 23:28:01 +0000

    Reland "Introduce the enable_component_updater feature flag."
    This reverts commit 619686adaf70b23bd44ab1bcaf095d7750f10539.
    
    Reason for revert: Reland. The original CL was reverted by mistake, and the actual culprit was https://chromium-review.googlesource.com/c/chromium/src/+/4001540.
    
    Original change's description:
    > Revert "Introduce the enable_component_updater feature flag."
    >
    > This reverts commit 547fb42ef068026116857e74e951947f9586754e.
    >
    > Reason for revert: Breaks the build.
    >
    > Original change's description:
    > > Introduce the enable_component_updater feature flag.
    > >
    > > This CL does not introduce any change to the current behavior of
    > > component updater, it's a no-op refactoring w.r.t. how component updater
    > > behaved before Chrome for Testing was introduced.
    > >
    > > Design document: https://goo.gle/chrome-for-testing#bookmark=id.t21k118g131p
    > >
    > > Bug: 1336611
    > > Change-Id: I11a8db029d4afaace56d7b0a1d5ff5b22b41d730
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921814
    > > Reviewed-by: Greg Thompson <grt@chromium.org>
    > > Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    > > Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1067002}
    >
    > Bug: 1336611
    > Change-Id: I902706b1da9a949abf783e8ac28ce401f6337c10
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4002711
    > Reviewed-by: Florent Castelli <orphis@chromium.org>
    > Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    > Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1067027}
    
    Bug: 1336611
    Change-Id: I4347c22f86bb027b5ef873ea99a8c4645b467c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004745
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068853}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -6932,9 +6932,13 @@ bool ChromeContentBrowserClient::ShouldDisableOriginAgentClusterDefault(
 }
 
 bool ChromeContentBrowserClient::WillProvidePublicFirstPartySets() {
+#if BUILDFLAG(ENABLE_COMPONENT_UPDATER)
   return !base::CommandLine::ForCurrentProcess()->HasSwitch(
              switches::kDisableComponentUpdate) &&
          base::FeatureList::IsEnabled(features::kFirstPartySets);
+#else
+  return false;
+#endif  // BUILDFLAG(ENABLE_COMPONENT_UPDATER)
 }
 
 content::mojom::AlternativeErrorPageOverrideInfoPtr
