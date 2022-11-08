6ef121f60848ecbb0b986e9be115a93601ef0f0d
Stephan Hartmann <stha09@googlemail.com>
2022-11-08 20:05:35 +0000

    libstdc++: remove const in cross_otr_web_contents_observer_unittest
    libstdc++ requires non-const value_type for std::vector.
    
    Bug: 957519
    Change-Id: I7a881d883847481be3bdf246daf4fb7e55dea6ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013282
    Reviewed-by: Brian Begnoche <bcb@chromium.org>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068736}

--- components/url_param_filter/content/cross_otr_web_contents_observer_unittest.cc
+++ components/url_param_filter/content/cross_otr_web_contents_observer_unittest.cc
@@ -95,7 +95,7 @@ TEST_F(CrossOtrWebContentsObserverTest, HandleRedirects) {
 
   ASSERT_NE(observer, nullptr);
 
-  std::vector<const std::string> redirect_sequence{
+  std::vector<std::string> redirect_sequence{
       "HTTP/1.1 302 Moved Temporarily",
       "HTTP/1.1 307 Temporary Redirect",  // 2 'external' 307 redirects
       "HTTP/1.1 307 Temporary Redirect",
@@ -133,7 +133,7 @@ TEST_F(CrossOtrWebContentsObserverTest, HandleRedirectsExperimentalMetrics) {
 
   ASSERT_NE(observer, nullptr);
 
-  std::vector<const std::string> redirect_sequence{
+  std::vector<std::string> redirect_sequence{
       "HTTP/1.1 302 Moved Temporarily",
       "HTTP/1.1 307 Temporary Redirect",  // 2 'external' 307 redirects
       "HTTP/1.1 307 Temporary Redirect",
@@ -185,7 +185,7 @@ TEST_F(CrossOtrWebContentsObserverTest,
 
   ASSERT_NE(observer, nullptr);
 
-  std::vector<const std::string> redirect_sequence{
+  std::vector<std::string> redirect_sequence{
       "HTTP/1.1 302 Moved Temporarily",
       "HTTP/1.1 307 Temporary Redirect",  // 2 'external' 307 redirects
       "HTTP/1.1 307 Temporary Redirect",
