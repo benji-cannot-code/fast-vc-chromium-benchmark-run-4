b3e449cde2350c6023e732c622f910d7e52f4602
Anastasia Helfinstein <anastasi@google.com>
2022-11-08 19:05:12 +0000

    [ChromeVox] Remove LogStore.getInstance()
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: Id8a84bd621a7977f083a0b547703feffc87a8909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999718
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068697}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/logging/event_stream_logger.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/logging/event_stream_logger.js
@@ -50,7 +50,7 @@ export class EventStreamLogger {
    */
   eventStreamLogging(evt) {
     const eventLog = new EventLog(evt);
-    LogStore.getInstance().writeLog(eventLog);
+    LogStore.instance.writeLog(eventLog);
     console.log(eventLog.toString());
   }
 
