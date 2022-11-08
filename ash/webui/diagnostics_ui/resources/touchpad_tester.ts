da7514fd73642f4b6456dc74bbad416759c78d92
Ashley Prasad <ashleydp@google.com>
2022-11-08 17:45:49 +0000

    diagnostics: add frontend touchpad-tester skeleton (2/2)
    - Add localization for title text.
    - Add `show/close` function to manage tester visibility.
    - Add touchpad property to ensure tester is configured for a specific
      device.
    - Test show and close update correctly.
    
    Bug: b:253021171
    Test: browser_test --gtest_filter=*DiagnosticsAppWithInput* --gtest_also_run_disabled_tests
    Change-Id: Ie2b85835905f17b5fff2767cc63ed03c2cc9fbe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990274
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068614}

--- ash/webui/diagnostics_ui/resources/touchpad_tester.ts
+++ ash/webui/diagnostics_ui/resources/touchpad_tester.ts
@@ -6,16 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './diagnostics_shared.css.js';
 
 import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {TouchDeviceInfo} from './input_data_provider.mojom-webui.js';
 import {getTemplate} from './touchpad_tester.html.js';
 
 export interface TouchpadTesterElement {
   $: {touchpadTesterDialog: CrDialogElement};
 }
 
-export class TouchpadTesterElement extends PolymerElement {
+const TouchpadTesterElementBase = I18nMixin(PolymerElement);
+
+export class TouchpadTesterElement extends TouchpadTesterElementBase {
   static get is() {
     return 'touchpad-tester';
   }
@@ -26,6 +31,24 @@ export class TouchpadTesterElement extends PolymerElement {
 
   static get properties(): PolymerElementProperties {
     return {};
+  }
+
+  // Touchpad device being tested.
+  touchpad: TouchDeviceInfo|null = null;
+
+  /**
+   * Resets dialog configuration to default.
+   */
+  close(): void {
+    this.$.touchpadTesterDialog.close();
+    this.touchpad = null;
+  }
+
+  /** Setup display for requested touchpad.*/
+  show(touchpad: TouchDeviceInfo): void {
+    assert(!!touchpad);
+    this.touchpad = touchpad;
+    this.$.touchpadTesterDialog.showModal();
   }
 }
 
