d7d68e9df7c0676540e60ac2e4da03a106ef9639
David Benjamin <davidben@chromium.org>
2022-11-08 17:46:07 +0000

    Restore a number of unit tests on iOS.
    These exclusions were for a number of reasons. The SpawnedTestServer
    tests have since migrated to EmbeddedTestServer, so we no longer need
    to exclude them. Other tests were just missing test data in the
    bundle_data rule. I've updated and rerun the script that generates
    the list.
    
    However, we do need to exclude a few individual tests because of the
    client cert ifdef. I've updated the comment since we ought to just
    remove it now.
    
    iOS trips some flakes in 0-RTT tests more readily. Add some
    RunUntilIdle calls to fix this.
    
    Also remove a stray test file (rsa-pss-sha1-wrong-salt.pem) that's no
    longer used, but I forgot to delete.
    
    Bug: 492672
    Change-Id: I72d349f3138b48f2066d0e9e07a0fa32fd7152e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274595
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068615}

--- net/socket/ssl_client_socket_unittest.cc
+++ net/socket/ssl_client_socket_unittest.cc
@@ -1598,6 +1598,8 @@ TEST_P(SSLClientSocketVersionTest, ConnectBadValidityIgnoreCertErrors) {
   EXPECT_TRUE(sock_->IsConnected());
 }
 
+// Client certificates are disabled on iOS.
+#if !BUILDFLAG(IS_IOS)
 // Attempt to connect to a page which requests a client certificate. It should
 // return an error code on connect.
 TEST_P(SSLClientSocketVersionTest, ConnectClientAuthCertRequested) {
@@ -1640,6 +1642,7 @@ TEST_P(SSLClientSocketVersionTest, ConnectClientAuthSendNullCert) {
   sock_->Disconnect();
   EXPECT_FALSE(sock_->IsConnected());
 }
+#endif  // !IS_IOS
 
 // TODO(wtc): Add unit tests for IsConnectedAndIdle:
 //   - Server closes an SSL connection (with a close_notify alert message).
@@ -2743,6 +2746,8 @@ TEST_P(SSLClientSocketVersionTest, VerifyReturnChainProperlyOrdered) {
   EXPECT_FALSE(sock_->IsConnected());
 }
 
+// Client certificates are disabled on iOS.
+#if !BUILDFLAG(IS_IOS)
 INSTANTIATE_TEST_SUITE_P(TLSVersion,
                          SSLClientSocketCertRequestInfoTest,
                          ValuesIn(GetTLSVersions()));
@@ -2820,6 +2825,7 @@ TEST_P(SSLClientSocketCertRequestInfoTest, CertKeyTypes) {
     EXPECT_EQ(CLIENT_CERT_ECDSA_SIGN, request_info->cert_key_types[1]);
   }
 }
+#endif  // !IS_IOS
 
 // Tests that the Certificate Transparency (RFC 6962) TLS extension is
 // supported.
@@ -3671,6 +3677,8 @@ TEST_F(SSLClientSocketTest, AlpnClientDisabled) {
   EXPECT_EQ(kProtoUnknown, sock_->GetNegotiatedProtocol());
 }
 
+// Client certificates are disabled on iOS.
+#if !BUILDFLAG(IS_IOS)
 // Connect to a server requesting client authentication, do not send
 // any client certificates. It should refuse the connection.
 TEST_P(SSLClientSocketVersionTest, NoCert) {
@@ -3813,6 +3821,7 @@ TEST_F(SSLClientSocketTest, ClearSessionCacheOnClientCertChange) {
   ASSERT_TRUE(CreateAndConnectSSLClientSocket(SSLConfig(), &rv));
   EXPECT_THAT(rv, IsError(ERR_BAD_SSL_CLIENT_AUTH_CERT));
 }
+#endif  // !IS_IOS
 
 HashValueVector MakeHashValueVector(uint8_t value) {
   HashValueVector out;
@@ -4790,7 +4799,11 @@ TEST_F(SSLClientSocketZeroRTTTest, ZeroRTTReject) {
   rv = WriteAndWait(kRequest);
   EXPECT_EQ(ERR_EARLY_DATA_REJECTED, rv);
 
-  // Retrying the connection should succeed.
+  // Run the event loop so the rejection has reached the TLS session cache.
+  base::RunLoop().RunUntilIdle();
+
+  // Now that the session cache has been updated, retrying the connection
+  // should succeed.
   socket = MakeClient(true);
   ASSERT_THAT(Connect(), IsOk());
   ASSERT_THAT(MakeHTTPRequest(ssl_socket()), IsOk());
@@ -4822,7 +4835,11 @@ TEST_F(SSLClientSocketZeroRTTTest, ZeroRTTWrongVersion) {
   rv = WriteAndWait(kRequest);
   EXPECT_EQ(ERR_WRONG_VERSION_ON_EARLY_DATA, rv);
 
-  // Retrying the connection should succeed.
+  // Run the event loop so the rejection has reached the TLS session cache.
+  base::RunLoop().RunUntilIdle();
+
+  // Now that the session cache has been updated, retrying the connection
+  // should succeed.
   socket = MakeClient(true);
   ASSERT_THAT(Connect(), IsOk());
   ASSERT_THAT(MakeHTTPRequest(ssl_socket()), IsOk());
@@ -5598,6 +5615,8 @@ TEST_F(SSLClientSocketZeroRTTTest, EarlyDataReasonNoResume) {
   int rv = ReadAndWait(buf.get(), 4096);
   EXPECT_EQ(ERR_EARLY_DATA_REJECTED, rv);
 
+  // The histogram may be record asynchronously.
+  base::RunLoop().RunUntilIdle();
   histograms.ExpectUniqueSample(kReasonHistogram,
                                 ssl_early_data_session_not_resumed, 1);
 }
