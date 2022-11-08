472b8d9001ec4248d68050d2006ad02b8802eddc
Rebekah Potter <rbpotter@chromium.org>
2022-11-08 18:20:22 +0000

    Reland "WebUI: Add cr.ts, TypeScript version of cr.m.js"
    This reverts commit 4bc8514aad40ad8c6ad9c1927ab42dd3eda52254.
    
    Reason for revert: Will land after https://chromium-review.googlesource.com/c/chromium/src/+/4012946
    
    Original change's description:
    > Revert "WebUI: Add cr.ts, TypeScript version of cr.m.js"
    >
    > This reverts commit 9102d8de2e642993d435a0a2211cf56e554d2469.
    >
    > Reason for revert: This CL is causing failures on Android arm64 builder bots https://ci.chromium.org/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20(dbg)/3413?
    >
    > Original change's description:
    > > WebUI: Add cr.ts, TypeScript version of cr.m.js
    > >
    > > Transform cr.m.js into a wrapper for cr.ts. This is a workaround to
    > > keep closure typechecking working, while not having duplicates of
    > > global variables within mixed UIs (e.g. settings).
    > >
    > > Since cr.js is the runtime file that now contains global variables,
    > > this is what is excluded from the bundle for all optimized UIs that
    > > were previously excluding cr.m.js.
    > >
    > > Also updating all shared ui/webui/resources code to refer to the new
    > > cr.ts directly. Non-closure UIs should be updated in a followup.
    > >
    > > Requires adding chrome.send externs to a number of closure targets
    > > that had previously been relying on getting these externs
    > > transitively.
    > >
    > > Bug: 1316438
    > > Change-Id: I2c71a91e2db22c31163ef1ede88270c548d5d6ef
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003498
    > > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1068564}
    >
    > Bug: 1316438
    > Change-Id: I28bc225a6f7573072c9b60cfcc06dc265d8487fb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013644
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    > Owners-Override: Kirubel Aklilu <kaklilu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1068569}
    
    Bug: 1316438
    Change-Id: Ic22b2e5e95babdeb853e6296814db2b15d06f6c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4012740
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068657}

--- ui/webui/resources/cr_components/certificate_manager/certificates_browser_proxy.ts
+++ ui/webui/resources/cr_components/certificate_manager/certificates_browser_proxy.ts
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * to interact with the browser.
  */
 
-import {sendWithPromise} from '../../js/cr.m.js';
+import {sendWithPromise} from '../../js/cr.js';
 
 /**
  * @see chrome/browser/ui/webui/settings/certificates_handler.cc
