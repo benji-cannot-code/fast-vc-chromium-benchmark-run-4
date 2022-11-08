452cff887f72bd94f20febe1bcd16400ed60b874
Addison Luh <aluh@chromium.org>
2022-11-08 17:48:28 +0000

    Minor refactor of RootWindowSettings.
    Change-Id: I880acf4ec42c3971093356ef88cfe7c7f4031c87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011221
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Addison Luh <aluh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068619}

--- ash/root_window_settings.h
+++ ash/root_window_settings.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_ROOT_WINDOW_SETTINGS_H_
 #define ASH_ROOT_WINDOW_SETTINGS_H_
 
-#include <stdint.h>
+#include <cstdint>
 
 #include "ash/ash_export.h"
 
