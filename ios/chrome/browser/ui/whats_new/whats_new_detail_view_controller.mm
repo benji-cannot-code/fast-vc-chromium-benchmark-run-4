9af5cae8a068228f6de11d7152210fa20f942448
Cheick Cisse <cheickcisse@google.com>
2022-11-08 23:04:13 +0000

    [WhatsNew] Update WhatsNewDetailView font and margin
    Change-Id: Idaf5c3debe3fc76dd7c82f6ec7d52100ded67f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013728
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068842}

--- ios/chrome/browser/ui/whats_new/whats_new_detail_view_controller.mm
+++ ios/chrome/browser/ui/whats_new/whats_new_detail_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/elements/instruction_view.h"
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller.h"
+#import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/whats_new/whats_new_detail_view_action_handler.h"
 #import "ios/chrome/browser/ui/whats_new/whats_new_detail_view_delegate.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -23,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr CGFloat kDefaultMargin = 16;
+constexpr CGFloat kDefaultMargin = 32;
 constexpr CGFloat kActionsBottomMargin = 10;
 constexpr CGFloat kContentWidthMultiplier = 0.8;
 constexpr CGFloat kButtonHorizontalMargin = 4;
@@ -294,10 +295,8 @@ NSString* const kWhatsNewScrollViewAccessibilityIdentifier =
   if (!_titleLabel) {
     _titleLabel = [[UILabel alloc] init];
     _titleLabel.numberOfLines = 0;
-    UIFont* font = [UIFont preferredFontForTextStyle:UIFontTextStyleLargeTitle];
-    UIFontDescriptor* boldFontDescriptor = [font.fontDescriptor
-        fontDescriptorWithSymbolicTraits:UIFontDescriptorTraitBold];
-    _titleLabel.font = [UIFont fontWithDescriptor:boldFontDescriptor size:0];
+    _titleLabel.font =
+        CreateDynamicFont(UIFontTextStyleTitle1, UIFontWeightBold);
     _titleLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
     _titleLabel.text = self.titleText;
     _titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -316,7 +315,7 @@ NSString* const kWhatsNewScrollViewAccessibilityIdentifier =
     _subtitleLabel.font =
         [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
     _subtitleLabel.numberOfLines = 0;
-    _subtitleLabel.textColor = [UIColor colorNamed:kGrey800Color];
+    _subtitleLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
     _subtitleLabel.text = self.subtitleText;
     _subtitleLabel.textAlignment = NSTextAlignmentCenter;
     _subtitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
