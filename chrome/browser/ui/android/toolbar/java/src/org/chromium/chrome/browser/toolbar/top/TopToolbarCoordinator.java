3360bcf9475dbd2e35df574615b5760109ac05ad
Cathy Wei <cathywei@google.com>
2022-11-08 21:59:38 +0000

    [Sticky-Header]Add toolbar hairline control and detect screen-size
    This CL contains:
    1. Add a toolbar hairline control method. We won't show the toolbar hairline when the header stick on the top.
    2. Detect the screen size. When the width < 360 dp or the height < 360 dp, we won't show the sticky header in any case.
    
    before:
    http://shortn/_Ggj3zmx1Gb
    after:
    http://shortn/_5Y32s2xcEq
    
    Bug: b:241271058
    Change-Id: Ie49ef184ac6ec00ba667339af9b985d05e36a16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995392
    Commit-Queue: Cathy Wei <cathywei@google.com>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068809}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
@@ -786,4 +786,9 @@ public class TopToolbarCoordinator implements Toolbar {
     public StartSurfaceToolbarCoordinator getStartSurfaceToolbarForTesting() {
         return mStartSurfaceToolbarCoordinator;
     }
+
+    @Override
+    public void setHairlineVisibility(boolean isVisible) {
+        mToolbarLayout.setHairlineVisibility(isVisible);
+    }
 }
