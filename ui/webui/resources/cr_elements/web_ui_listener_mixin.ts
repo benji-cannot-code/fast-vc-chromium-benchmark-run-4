b6a6e4afdfbd722f7fc9d1d814878dddbe2af933
Kirubel Aklilu <kaklilu@chromium.org>
2022-11-08 16:59:46 +0000

    Revert "WebUI: Add cr.ts, TypeScript version of cr.m.js"
    This reverts commit 9102d8de2e642993d435a0a2211cf56e554d2469.
    
    Reason for revert: This CL is causing failures on Android arm64 builder bots https://ci.chromium.org/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20(dbg)/3413?
    
    Original change's description:
    > WebUI: Add cr.ts, TypeScript version of cr.m.js
    >
    > Transform cr.m.js into a wrapper for cr.ts. This is a workaround to
    > keep closure typechecking working, while not having duplicates of
    > global variables within mixed UIs (e.g. settings).
    >
    > Since cr.js is the runtime file that now contains global variables,
    > this is what is excluded from the bundle for all optimized UIs that
    > were previously excluding cr.m.js.
    >
    > Also updating all shared ui/webui/resources code to refer to the new
    > cr.ts directly. Non-closure UIs should be updated in a followup.
    >
    > Requires adding chrome.send externs to a number of closure targets
    > that had previously been relying on getting these externs
    > transitively.
    >
    > Bug: 1316438
    > Change-Id: I2c71a91e2db22c31163ef1ede88270c548d5d6ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003498
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1068564}
    
    Bug: 1316438
    Change-Id: I28bc225a6f7573072c9b60cfcc06dc265d8487fb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013644
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Owners-Override: Kirubel Aklilu <kaklilu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1068569}

--- ui/webui/resources/cr_elements/web_ui_listener_mixin.ts
+++ ui/webui/resources/cr_elements/web_ui_listener_mixin.ts
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {addWebUiListener, removeWebUiListener, WebUiListener} from '../js/cr.js';
+import {addWebUIListener, removeWebUIListener, WebUIListener} from '../js/cr.m.js';
 
 type Constructor<T> = new (...args: any[]) => T;
 
@@ -23,7 +23,7 @@ export const WebUiListenerMixin = dedupingMixin(
          * Holds WebUI listeners that need to be removed when this element is
          * destroyed.
          */
-        private webUiListeners_: WebUiListener[] = [];
+        private webUIListeners_: WebUIListener[] = [];
 
         /**
          * Adds a WebUI listener and registers it for automatic removal when
@@ -35,13 +35,13 @@ export const WebUiListenerMixin = dedupingMixin(
          * @param callback The callback run when the event is fired.
          */
         addWebUIListener(eventName: string, callback: Function) {
-          this.webUiListeners_.push(addWebUiListener(eventName, callback));
+          this.webUIListeners_.push(addWebUIListener(eventName, callback));
         }
 
         override disconnectedCallback() {
           super.disconnectedCallback();
-          while (this.webUiListeners_.length > 0) {
-            removeWebUiListener(this.webUiListeners_.pop()!);
+          while (this.webUIListeners_.length > 0) {
+            removeWebUIListener(this.webUIListeners_.pop()!);
           }
         }
       }
