f0d244aeab7971e40460d437d059f20124299a18
Clifford Cheng <cliffordcheng@chromium.org>
2022-11-08 00:36:20 +0000

    [dpwa] Update FileOptions from Txt|Png to Foo|Bar.
    The enums and CUJs are also updated to match that.
    
    Change-Id: I853c6428d68be99f62484c66495265be9739cc12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010921
    Auto-Submit: Clifford Cheng <cliffordcheng@chromium.org>
    Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068432}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -100,11 +100,11 @@ enum class AskAgainOptions { kAskAgain, kRemember };
 enum class FileExtension { kFoo, kBar };
 
 enum class FilesOptions {
-  kOneTextFile,
-  kMultipleTextFiles,
-  kOnePngFile,
-  kMultiplePngFiles,
-  kAllTextAndPngFiles
+  kOneFooFile,
+  kMultipleFooFiles,
+  kOneBarFile,
+  kMultipleBarFiles,
+  kAllFooAndBarFiles
 };
 
 enum class UpdateDialogResponse {
