4552190b5c9937d01d46da3a8d5e57573afbb380
Thomas Lukaszewicz <tluk@chromium.org>
2022-11-09 19:11:43 +0000

    [wayland] Propagate minimized state to exo clients
    This CL leverages existing window minimized state, added to exo with
    the aura-shell extension, to notify clients when a given xdg toplevel
    window is minimized in a configure event.
    
    This addresses an issue where Chrome expects to be able to restore
    minimized windows without necessarily propagating an activation
    event.
    
    Bug: b/244356321
    Change-Id: I465f952d50ab76133eea15e55aecf94a59415b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006231
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1069270}

--- ui/ozone/platform/wayland/host/zxdg_toplevel_v6_wrapper_impl.cc
+++ ui/ozone/platform/wayland/host/zxdg_toplevel_v6_wrapper_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/host/zxdg_toplevel_v6_wrapper_impl.h"
 
+#include <aura-shell-client-protocol.h>
 #include <xdg-decoration-unstable-v1-client-protocol.h>
 #include <xdg-shell-client-protocol.h>
 #include <xdg-shell-unstable-v6-client-protocol.h>
@@ -175,6 +176,8 @@ void ZXDGToplevelV6WrapperImpl::ConfigureTopLevel(
               CheckIfWlArrayHasValue(states, ZXDG_TOPLEVEL_V6_STATE_FULLSCREEN),
           .is_activated =
               CheckIfWlArrayHasValue(states, ZXDG_TOPLEVEL_V6_STATE_ACTIVATED),
+          .is_minimized =
+              CheckIfWlArrayHasValue(states, ZAURA_TOPLEVEL_STATE_MINIMIZED),
       });
 }
 
