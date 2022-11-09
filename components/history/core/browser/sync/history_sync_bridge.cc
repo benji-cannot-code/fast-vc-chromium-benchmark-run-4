93d099f4cf863043733cdedeb88d5c79dfbef9ea
Marc Treib <treib@chromium.org>
2022-11-09 13:38:35 +0000

    History Sync: Make sure ApplyStopSyncChanges() calls ApplySyncChanges()
    When ApplyStopSyncChanges gets called with a non-null
    MetadataChangeList, that means all Sync metadata should be cleared.
    Before this CL, the HistorySyncBridge implementation of
    ApplyStopSyncChanges() did not do anything with the passed-in MCL.
    That happened to work correctly, because the particular MCL
    implementation used here anyway writes all changes directly to the
    store, so by the time ApplyStopSyncChanges() gets called, the metadata
    is in fact already cleared.
    But this was quite subtle and potentially fragile. So this CL adds a
    call to the superclass implementation, to ensure the correct behavior
    (even if, in the future, a different MCL implementation were used).
    This CL also adds a test to ensure metadata is cleared correctly
    (which, due to the above, actually passes both with and without this
    CL's changes).
    
    Bug: 1366759
    Change-Id: I0577e865eead4eeaae5e2309fdff4b352528ef43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013284
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069114}

--- components/history/core/browser/sync/history_sync_bridge.cc
+++ components/history/core/browser/sync/history_sync_bridge.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/url_row.h"
 #include "components/sync/base/page_transition_conversion.h"
 #include "components/sync/model/conflict_resolution.h"
+#include "components/sync/model/entity_change.h"
 #include "components/sync/model/metadata_batch.h"
 #include "components/sync/model/metadata_change_list.h"
 #include "components/sync/model/model_type_change_processor.h"
@@ -670,6 +671,9 @@ syncer::ConflictResolution HistorySyncBridge::ResolveConflict(
 
 void HistorySyncBridge::ApplyStopSyncChanges(
     std::unique_ptr<syncer::MetadataChangeList> delete_metadata_change_list) {
+  ModelTypeSyncBridge::ApplyStopSyncChanges(
+      std::move(delete_metadata_change_list));
+
   sync_started_ = false;
 }
 
