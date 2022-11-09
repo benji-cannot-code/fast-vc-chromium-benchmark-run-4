5de2e6a5cef82a72e54885a1e85c43f145c14f22
Lijin Shen <lazzzis@google.com>
2022-11-09 22:26:35 +0000

    Revert "[RCTS] SessionRestoreMessageController"
    This reverts commit 7370246097df2bc1db7f63cd61f01a3d56b8ba94.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1382879#c8
    
    Original change's description:
    > [RCTS] SessionRestoreMessageController
    >
    > - Show message UI indicating Custom Tab session can be restored
    > - CCT-Retaining-State-In-Memory flag is now false by default
    >
    > Bug: 1377047
    > Change-Id: I9c65f82ae3ae4a00e57efcdf654b59e23c0e7acc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3994192
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Zach Katz <katzz@google.com>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1068613}
    
    Bug: 1377047
    Change-Id: I56089e1db465de428c97e64a40c1224da901c31e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018020
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069376}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
@@ -46,7 +46,6 @@ import org.chromium.chrome.browser.customtabs.CustomTabTabPersistencePolicy;
 import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.customtabs.DefaultBrowserProviderImpl;
 import org.chromium.chrome.browser.customtabs.ReparentingTaskProvider;
-import org.chromium.chrome.browser.customtabs.features.sessionrestore.SessionRestoreMessageController;
 import org.chromium.chrome.browser.customtabs.shadows.ShadowExternalNavigationDelegateImpl;
 import org.chromium.chrome.browser.flags.ActivityType;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
@@ -105,7 +104,6 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
     @Mock public ChromeBrowserInitializer browserInitializer;
     @Mock public CustomTabIncognitoManager customTabIncognitoManager;
     @Mock public TabModelInitializer tabModelInitializer;
-    @Mock public SessionRestoreMessageController sessionRestoreMessageController;
     // clang-format on
     public AsyncTabParamsManager realAsyncTabParamsManager =
             AsyncTabParamsManagerFactory.createAsyncTabParamsManager();
@@ -178,7 +176,7 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
                 navigationEventObserver, tabProvider, reparentingTaskProvider,
                 () -> customTabIncognitoManager, () -> realAsyncTabParamsManager,
                 () -> activity.getSavedInstanceState(), activity.getWindowAndroid(),
-                tabModelInitializer, sessionRestoreMessageController);
+                tabModelInitializer);
     }
     // clang-format on
 
