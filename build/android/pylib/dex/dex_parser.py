cecda655c42f3c95f6efdef5fb5cd0f91a6b8c0a
Samuel Huang <huangs@chromium.org>
2022-11-09 21:35:39 +0000

    [Android] DEX Reader: Apply more Python 3 fixes.
    dex_reader.py, despite supporting Python 3, had some problems:
    * Passing DEX file as input leads to UnicodeDecodeError from file
      open(). This can be fixed by passing 'rb' as the mode.
    * _DexReader._DecodeMUtf8() uses unichr(), and on NameError, switches to
      chr(): This mnight have been done for Python 2 compatibility. However,
      since all 3 current callers are all Python 3, this is now redundant.
    
    This CL fixes the above issues, in preparation of usage by SuperSize.
    
    Bug: 1270438
    Change-Id: I3a74ca681d812239a5962826d8175399d03a8081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017715
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069286}

--- build/android/pylib/dex/dex_parser.py
+++ build/android/pylib/dex/dex_parser.py
@@ -12,7 +12,6 @@ is provided, but it does not include error handling or validation.
 """
 
 
-
 import argparse
 import collections
 import errno
@@ -319,11 +318,7 @@ class _DexReader:
         code = ((a & 0x0f) << 12) | ((b & 0x3f) << 6) | (c & 0x3f)
       else:
         raise _MUTf8DecodeError('Bad byte', string_length, offset)
-
-      try:
-        ret += unichr(code)
-      except NameError:
-        ret += chr(code)
+      ret += chr(code)
 
     if self.ReadUByte() != 0x00:
       raise _MUTf8DecodeError('Expected string termination', string_length,
@@ -543,7 +538,7 @@ def main():
         _DumpDexItems(z.read(path), path, args.item)
 
   else:
-    with open(args.input) as f:
+    with open(args.input, 'rb') as f:
       _DumpDexItems(f.read(), args.input, args.item)
 
 
