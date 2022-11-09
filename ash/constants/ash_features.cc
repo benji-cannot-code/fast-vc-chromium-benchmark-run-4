5cab7bc46da4c349bd754896f83167f95020773d
Ahmed Fakhry <afakhry@chromium.org>
2022-11-09 03:21:42 +0000

    gif_recording: Add feature flag
    This CL adds a feature flag `kGifRecording` that will hide
    all the upcoming changes needed for this feature.
    
    Fixed: b:258322762
    Test: None
    Change-Id: I72faab059507fc487cbb22009abeb7ae32e79861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015095
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068951}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1024,6 +1024,10 @@ BASE_FEATURE(kGesturePropertiesDBusService,
              "GesturePropertiesDBusService",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables the ability to record the screen into an animated GIF image from the
+// native screen capture tool.
+BASE_FEATURE(kGifRecording, "GifRecording", base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables editing with handwriting gestures within the virtual keyboard.
 BASE_FEATURE(kHandwritingGestureEditing,
              "HandwritingGestureEditing",
@@ -2562,6 +2566,10 @@ bool IsGaiaReauthEndpointEnabled() {
 
 bool IsGalleryAppPdfEditNotificationEnabled() {
   return base::FeatureList::IsEnabled(kGalleryAppPdfEditNotification);
+}
+
+bool IsGifRecordingEnabled() {
+  return base::FeatureList::IsEnabled(kGifRecording);
 }
 
 bool AreGlanceablesEnabled() {
