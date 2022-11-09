c10992f737d60ba607da8a341b4fb0ecf24ad651
Lukasz Anforowicz <lukasza@chromium.org>
2022-11-09 21:47:44 +0000

    NavigationRequest::GetOriginToCommit for 204s should return nullopt.
    For downloads and 204s, there is no document (of any origin) committed
    in a renderer process.  To accurately reflect that, this CL changes the
    API of GetOriginToCommit so that it returns an `absl::optional<...>`.
    
    Bug: 1362818
    Change-Id: Ic693e2b12ef37d1b30a1d3437a266b3f4a674907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010546
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069307}

--- content/browser/renderer_host/navigation_controller_impl.cc
+++ content/browser/renderer_host/navigation_controller_impl.cc
@@ -4561,7 +4561,7 @@ NavigationControllerImpl::GetNavigationApiHistoryEntryVectors(
     FrameTreeNode* node,
     NavigationRequest* request) {
   url::Origin pending_origin = request
-                                   ? request->GetOriginToCommit()
+                                   ? request->GetOriginToCommit().value()
                                    : url::Origin::Create(node->current_url());
 
   scoped_refptr<SiteInstance> site_instance =
