d71e284de41b6bc06e52438b52e4096ab7c7fc00
Xiaocheng Hu <xiaochengh@chromium.org>
2022-11-09 23:44:49 +0000

    [anchor-position] Resolve tree-scoped anchor references in NGAnchorQuery
    This patch makes NG*AnchorQuery resolve tree-scoped anchor
    references by using ScopedCSSName as the hashmap key. To
    support this, special hash trait for ScopedCSSName is added so
    that the hash maps hash the ScopedCSSName objects themselves
    instead of the wrapper pointers (like Member<>).
    
    See also previous patches:
    - crrev.com/c/3984835: Make anchor names tree-scoped
    - crrev.com/c/3985707: Make anchor() and anchor-size() tree-scoped
    
    Fixed: 1309178
    Change-Id: Ib377c9dd00b42252add374266c1620ed41bb65f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989498
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069439}

--- third_party/blink/renderer/core/layout/ng/ng_anchor_query_map.cc
+++ third_party/blink/renderer/core/layout/ng/ng_anchor_query_map.cc
@@ -86,7 +86,7 @@ struct NGStitchedAnchorQuery : public GarbageCollected<NGStitchedAnchorQuery> {
   NGLogicalAnchorQuery* StitchedAnchorQuery() const {
     auto* anchor_query = MakeGarbageCollected<NGLogicalAnchorQuery>();
     for (const auto& it : references)
-      anchor_query->Set(it.key, it.value->StitchedAnchorReference());
+      anchor_query->Set(*it.key, it.value->StitchedAnchorReference());
     return anchor_query;
   }
 
@@ -105,13 +105,13 @@ struct NGStitchedAnchorQuery : public GarbageCollected<NGStitchedAnchorQuery> {
       return;
     for (const auto& it : *anchor_query) {
       DCHECK(it.value->fragment);
-      AddAnchorReference(it.key, *it.value->fragment,
+      AddAnchorReference(*it.key, *it.value->fragment,
                          it.value->rect + offset_from_fragmentainer,
                          fragmentainer, Conflict::kFirstInCallOrder);
     }
   }
 
-  void AddAnchorReference(const AtomicString& anchor_name,
+  void AddAnchorReference(const ScopedCSSName& anchor_name,
                           const NGPhysicalFragment& fragment,
                           const PhysicalRect& physical_rect_in_fragmentainer,
                           const FragmentainerContext& fragmentainer,
@@ -120,7 +120,7 @@ struct NGStitchedAnchorQuery : public GarbageCollected<NGStitchedAnchorQuery> {
         fragmentainer.converter.ToLogical(physical_rect_in_fragmentainer);
     auto* new_value = MakeGarbageCollected<NGStitchedAnchorReference>(
         fragment, rect_in_fragmentainer, fragmentainer);
-    const auto result = references.insert(anchor_name, new_value);
+    const auto result = references.insert(&anchor_name, new_value);
     if (result.is_new_entry)
       return;
 
@@ -152,7 +152,8 @@ struct NGStitchedAnchorQuery : public GarbageCollected<NGStitchedAnchorQuery> {
 
   void Trace(Visitor* visitor) const { visitor->Trace(references); }
 
-  HeapHashMap<AtomicString, Member<NGStitchedAnchorReference>> references;
+  HeapHashMap<Member<const ScopedCSSName>, Member<NGStitchedAnchorReference>>
+      references;
 };
 
 // This collects |NGStitchedAnchorQuery| for each containing block.
@@ -325,9 +326,8 @@ struct NGStitchedAnchorQueries {
       NGStitchedAnchorQuery& query =
           EnsureStitchedAnchorQuery(*containing_block);
       if (fragment.Style().AnchorName()) {
-        // TODO(xiaochengh): Handle tree-scoped anchor name.
         query.AddAnchorReference(
-            fragment.Style().AnchorName()->GetName(), fragment,
+            *fragment.Style().AnchorName(), fragment,
             {offset_from_fragmentainer, fragment.Size()}, fragmentainer,
             NGStitchedAnchorQuery::Conflict::kOverwriteIfBefore);
       }
