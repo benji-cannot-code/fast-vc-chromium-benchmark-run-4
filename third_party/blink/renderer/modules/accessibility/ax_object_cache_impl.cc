d81dc138d28495bc36d67c3360afea877bead955
Aaron Leventhal <aleventhal@google.com>
2022-11-09 18:56:30 +0000

    Introduce ScheduleAXUpdate()
    AXObjectCacheImpl::ScheduleAXUpdate() replaces ::ScheduleVisualUpdate(),
    and does not require a document to be provided. It schedules a visual
    update for both the main (and if present) popup document.
    
    The documents are always processed together now. It does not make sense
    to send a11y updates to the serializer or inspector when only one of
    the documents is up-to-date. Therefore, ScheduleAXUpdate() requests
    a visual update for the main document. Later, when ProcessDeferredUpdates() is called, if it is for the main document, we proceed but force a layout update on the popup, if it exists.
    
    This is a necessary precursor to CL:3914885, synchronous a11y serialization.
    
    Bug: none
    Change-Id: I6179d2ddc08a69180d7b046e744e06fb2b3113e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013510
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069260}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -1049,7 +1049,7 @@ AXObject* AXObjectCacheImpl::Get(AbstractInlineTextBox* inline_text_box) {
 
 void AXObjectCacheImpl::Invalidate(Document& document, AXID ax_id) {
   if (GetInvalidatedIds(document).insert(ax_id).is_new_entry)
-    ScheduleVisualUpdate(document);
+    ScheduleAXUpdate();
 }
 
 AXID AXObjectCacheImpl::GetAXID(Node* node) {
@@ -1962,7 +1962,7 @@ void AXObjectCacheImpl::DeferTreeUpdateInternal(base::OnceClosure callback,
 
   // These events are fired during RunPostLifecycleTasks(),
   // ensure there is a document lifecycle update scheduled.
-  ScheduleVisualUpdate(*tree_update_document);
+  ScheduleAXUpdate();
 }
 
 void AXObjectCacheImpl::DeferTreeUpdateInternal(base::OnceClosure callback,
@@ -1997,7 +1997,7 @@ void AXObjectCacheImpl::DeferTreeUpdateInternal(base::OnceClosure callback,
 
   // These events are fired during RunPostLifecycleTasks(),
   // ensure there is a document lifecycle update scheduled.
-  ScheduleVisualUpdate(tree_update_document);
+  ScheduleAXUpdate();
 }
 
 void AXObjectCacheImpl::DeferTreeUpdate(
@@ -2498,7 +2498,7 @@ void AXObjectCacheImpl::ProcessDeferredAccessibilityEvents(Document& document) {
     DCHECK_EQ(&document, GetPopupDocumentIfShowing());
     // Since a change occurred in the popup, processing of both documents will
     // be needed. A visual update on the main document will force this.
-    ScheduleVisualUpdate(GetDocument());
+    ScheduleAXUpdate();
     return;
   }
 
@@ -2902,27 +2902,28 @@ void AXObjectCacheImpl::PostNotification(AXObject* object,
 
   // These events are fired during RunPostLifecycleTasks(),
   // ensure there is a visual update scheduled.
-  ScheduleVisualUpdate(document);
+  ScheduleAXUpdate();
 }
 
-void AXObjectCacheImpl::ScheduleVisualUpdate(Document& document) {
+void AXObjectCacheImpl::ScheduleAXUpdate() {
+  // A visual update will force accessibility to be updated as well.
   // Scheduling visual updates before the document is finished loading can
   // interfere with event ordering. In any case, at least one visual update will
   // occur between now and when the document load is complete.
-  if (!document.IsLoadCompleted())
+  if (!GetDocument().IsLoadCompleted())
     return;
 
   // If there was a document change that doesn't trigger a lifecycle update on
   // its own, (e.g. because it doesn't make layout dirty), make sure we run
   // lifecycle phases to update the computed accessibility tree.
-  LocalFrameView* frame_view = document.View();
-  Page* page = document.GetPage();
+  LocalFrameView* frame_view = GetDocument().View();
+  Page* page = GetDocument().GetPage();
   if (!frame_view || !page)
     return;
 
   if (!frame_view->CanThrottleRendering() &&
-      !document.GetPage()->Animator().IsServicingAnimations()) {
-    page->Animator().ScheduleVisualUpdate(document.GetFrame());
+      !GetDocument().GetPage()->Animator().IsServicingAnimations()) {
+    page->Animator().ScheduleVisualUpdate(GetDocument().GetFrame());
   }
 }
 
