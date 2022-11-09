434e271ba5b212b190470b102ee27b701c3f9aa8
Alvin Ji <alvinji@chromium.org>
2022-11-09 22:50:43 +0000

    Implement output device selection feature in AudioContext
    This CL is to implement the followings for output device selection:
    - IDL changes in AudioContext
    - sinkId validation
    - The union type of (AudioSinkInfo or DOMString) for AudioContext.sinkId
    - Handling promise resolvers from AudioContext.setSinkiId() call
    - Attach LocalFrameToken to WebAudioSinkDescriptor for deferred `SetSinkDescriptor` task
    
    Relevant spec:
    - https://webaudio.github.io/web-audio-api/#dom-audiocontext-audiocontext
    - https://webaudio.github.io/web-audio-api/#validating-sink-identifier
    - https://webaudio.github.io/web-audio-api/#AudioSinkOptions
    - https://webaudio.github.io/web-audio-api/#AudioSinkInfo
    Related CL:
    - https://crrev.com/c/3943082
    - https://crrev.com/c/3972947
    
    Bug: 1216187
    Change-Id: Ibe1a9acd003984a4021acae16d913ea48127d41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006903
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069409}

--- third_party/blink/renderer/modules/webaudio/setsinkid_resolver.h
+++ third_party/blink/renderer/modules/webaudio/setsinkid_resolver.h
@@ -7,19 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_SETSINKID_RESOLVER_H_
 
 #include "media/base/output_device_info.h"
+#include "third_party/blink/public/platform/web_audio_sink_descriptor.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 
 namespace blink {
 
 class AudioContext;
 class ScriptPromiseResolver;
+class V8UnionAudioSinkOptionsOrString;
 
 class SetSinkIdResolver : public ScriptPromiseResolver {
  public:
   static SetSinkIdResolver* Create(ScriptState*,
                                    AudioContext&,
-                                   const String& sink_id);
-  SetSinkIdResolver(ScriptState*, AudioContext&, const String& sink_id);
+                                   const V8UnionAudioSinkOptionsOrString&);
+  SetSinkIdResolver(ScriptState*,
+                    AudioContext&,
+                    const V8UnionAudioSinkOptionsOrString&);
   SetSinkIdResolver(const SetSinkIdResolver&) = delete;
   SetSinkIdResolver& operator=(const SetSinkIdResolver&) = delete;
   ~SetSinkIdResolver() override = default;
@@ -38,7 +42,7 @@ class SetSinkIdResolver : public ScriptPromiseResolver {
 
   WeakMember<AudioContext> audio_context_;
 
-  String sink_id_;
+  WebAudioSinkDescriptor sink_descriptor_;
 };
 }  // namespace blink
 
