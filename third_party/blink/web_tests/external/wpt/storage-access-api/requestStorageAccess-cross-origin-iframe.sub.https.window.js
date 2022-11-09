455302e4ca1a520268ebac2ef401037487a2e248
Johann Hofmann <johannhof@chromium.org>
2022-11-09 14:32:27 +0000

    Split up SAA tests, add test for basic permission states.
    This removes the recurive monolithical single SAA test file in favor of
    smaller ones. This helps test grant cases where we need to have both
    user interaction and permission grants, which were extremely unreliable
    with the larger test file.
    
    Bug: 1378969, 1374277
    Change-Id: Iebc9bd48185ff398181bf1a16c35995a2467cb54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986031
    Commit-Queue: Johann Hofmann <johannhof@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069134}

---
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-cross-origin-iframe.sub.https.window.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=helpers.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+'use strict';
+
+(async function() {
+  // Set up storage access rules
+  try {
+    await test_driver.set_storage_access("https://{{domains[www]}}:{{ports[https][0]}}/", "*", "blocked");
+  } catch (e) {
+    // Ignore, can be unimplemented if the platform blocks cross-site cookies
+    // by default. If this failed without default blocking we'll notice it later
+    // in the test.
+  }
+
+  // Create a test with a single-child cross-origin iframe.
+  RunTestsInIFrame('https://{{domains[www]}}:{{ports[https][0]}}/storage-access-api/resources/requestStorageAccess-iframe.https.html?testCase=cross-origin-frame&rootdocument=false');
+})();
---
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-nested-cross-origin-iframe.sub.https.window.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=helpers.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+'use strict';
+
+(async function() {
+  // Set up storage access rules
+  try {
+    await test_driver.set_storage_access("https://{{domains[www]}}:{{ports[https][0]}}/", "*", "blocked");
+  } catch (e) {
+    // Ignore, can be unimplemented if the platform blocks cross-site cookies
+    // by default. If this failed without default blocking we'll notice it later
+    // in the test.
+  }
+
+  // Validate the nested-iframe scenario where the cross-origin frame
+  // containing the tests is not the first child.
+  RunTestsInNestedIFrame('https://{{domains[www]}}:{{ports[https][0]}}/storage-access-api/resources/requestStorageAccess-iframe.https.html?testCase=nested-cross-origin-frame&rootdocument=false');
+})();
