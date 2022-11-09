cdb2c80e1d1eeb8cbce4e344f20a7d2b0a0643ee
Yue Li <updowndota@google.com>
2022-11-09 21:44:24 +0000

    Reland "Avatars: Update rule for RandomDefaultImageIndex"
    This is a reland of commit 424e83e1176595770f0c546734ed6e4cf74a75f6
    
    Fixed the flaky buffer overflow.
    
    Original change's description:
    > Avatars: Update rule for RandomDefaultImageIndex
    >
    > Update the rule to pick based on the actual order of the current
    > avatars.
    >
    > Bug: None
    > Test: Run existing tests
    > Change-Id: I74c0dbbfc1fd9f7027fe70b432c1735f8ba6c944
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3951042
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Yue Li <updowndota@chromium.org>
    > Auto-Submit: Yue Li <updowndota@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1068724}
    
    Bug: None
    Change-Id: I13dcfa2eeb1dd0f10f65b909899564b2204e6ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018075
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069301}

--- chrome/browser/ash/login/users/default_user_image/default_user_images.cc
+++ chrome/browser/ash/login/users/default_user_image/default_user_images.cc
@@ -326,11 +326,6 @@ const int kFirstDefaultImageIndex = 48;
 
 const int kLastLegacyImageIndex = 33;
 
-// Limit random default image index to prevent undesirable UI behavior when
-// selecting an image with a high index. E.g. automatic scrolling of picture
-// list that is used to present default images.
-const int kLastRandomDefaultImageIndex = 62;
-
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
 // The order and the values of these constants are important for histograms
@@ -391,7 +386,8 @@ int GetDefaultImageResourceId(int index) {
 }
 
 int GetRandomDefaultImageIndex() {
-  return base::RandInt(kFirstDefaultImageIndex, kLastRandomDefaultImageIndex);
+  return kCurrentImageIndexes[base::RandInt(
+      0, std::size(kCurrentImageIndexes) - 1)];
 }
 
 bool IsValidIndex(int index) {
