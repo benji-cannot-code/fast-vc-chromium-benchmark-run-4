c6d88b4533f2a3ef75b8324ef7a7f7f4eb4ea147
Tim Judkins <tjudkins@chromium.org>
2022-11-09 23:52:39 +0000

    [extensions] Enable NewWebstoreDomain feature by default on stable
    With all of the protections that have been put in place for the new
    webstore domain, we can now remove the channel restriction and enable
    exposing the required APIs on the new domain by default.
    
    Bug: 1338235
    Change-Id: I41caaa04fbf1b76e06b85913af48cfdd3bfad7b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011200
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069446}

--- chrome/browser/extensions/process_management_browsertest.cc
+++ chrome/browser/extensions/process_management_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "components/version_info/channel.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -38,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/process_map.h"
-#include "extensions/common/extension_features.h"
-#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/manifest_handlers/web_accessible_resources_info.h"
 #include "extensions/common/switches.h"
 #include "net/dns/mock_host_resolver.h"
@@ -88,7 +85,6 @@ class ChromeWebStoreProcessTest
       public testing::WithParamInterface<const char*> {
  public:
   ChromeWebStoreProcessTest() {
-    feature_list_.InitAndEnableFeature(extensions_features::kNewWebstoreDomain);
     // The tests need the https server to resolve the webstore domain being
     // tested and 2 related subdomains with the same eTLD+1. Add certificates
     // for each.
@@ -154,10 +150,6 @@ class ChromeWebStoreProcessTest
     host_resolver()->AddRule("*", "127.0.0.1");
   }
 
-  // The webstore API availability for the new domain is currently behind a
-  // feature and channel restriction.
-  base::test::ScopedFeatureList feature_list_;
-  ScopedCurrentChannel current_channel_override_{version_info::Channel::DEV};
   GURL webstore_url_;
 };
 
