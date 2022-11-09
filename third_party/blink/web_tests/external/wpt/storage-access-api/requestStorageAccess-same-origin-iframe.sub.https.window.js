1014fb01cc1d7be68f6094a3b609e7d83a5c7d8a
Nate Chapin <japhet@chromium.org>
2022-11-09 19:03:32 +0000

    Revert "Split up SAA tests, add test for basic permission states."
    This reverts commit 83372fa176ed0ecafe1e9b41ffa79317e61a1500.
    
    Reason for revert: Failures on mac bots (e.g., https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/12813/overview)
    
    Original change's description:
    > Split up SAA tests, add test for basic permission states.
    >
    > This removes the recurive monolithical single SAA test file in favor of
    > smaller ones. This helps test grant cases where we need to have both
    > user interaction and permission grants, which were extremely unreliable
    > with the larger test file.
    >
    > Bug: 1378969, 1374277
    > Change-Id: Iebc9bd48185ff398181bf1a16c35995a2467cb54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986031
    > Commit-Queue: Johann Hofmann <johannhof@chromium.org>
    > Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1069134}
    
    Bug: 1378969, 1374277
    Change-Id: I8bd132001df0a4b0b42a21fcafa39493cd464a13
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018473
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069267}

--- third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-same-origin-iframe.sub.https.window.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: script=helpers.js
-// META: script=/resources/testdriver.js
-// META: script=/resources/testdriver-vendor.js
-'use strict';
-
-// Create a test with a single-child same-origin iframe.
-RunTestsInIFrame('resources/requestStorageAccess-iframe.html?testCase=same-origin-frame&rootdocument=false');
