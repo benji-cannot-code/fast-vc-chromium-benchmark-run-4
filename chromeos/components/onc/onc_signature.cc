ab9e25b92630f5c84102d7713afe98c017e02fd8
Emmanuel Arias Soto <eariassoto@google.com>
2022-11-09 10:04:08 +0000

    [CrOS Cellular] Add Cellular UserAPNList ONC field
    This field will be used by the Revamp feature to handle the custom user APNs.
    These APNs are filtered when translating the dictionary ONC to a Shill
    dictionary. Only enabled APNs will be sent to Shill to create a data connection
    under the new revamp.
    
    Bug: b:162365553
    Test: chromeos_unittests --gtest_filter=ONCTranslatorOncToShillTest*
    Change-Id: I4b4fb97b76b5aaa46b9c86cff00e5c2e81376257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991032
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069060}

--- chromeos/components/onc/onc_signature.cc
+++ chromeos/components/onc/onc_signature.cc
@@ -321,6 +321,7 @@ const OncFieldSignature cellular_fields[] = {
     {::onc::cellular::kAutoConnect, &kBoolSignature},
     {::onc::cellular::kICCID, &kStringSignature},
     {::onc::cellular::kSMDPAddress, &kStringSignature},
+    {::onc::cellular::kUserAPNList, &kCellularApnListSignature},
     {nullptr}};
 
 const OncFieldSignature cellular_with_state_fields[] = {
