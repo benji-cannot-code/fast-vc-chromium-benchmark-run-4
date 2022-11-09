2ced8dcfcf1eeffc4d13828569c69932b7fa3962
Ben Reich <benreich@chromium.org>
2022-11-09 08:11:37 +0000

    [Files app] Enable directory tree context menu via keyboard navigation
    The directory tree currently only shows the context menu for items if
    the tree is right clicked. This means there is no way to trigger the
    menu via keyboard navigation. The "contextmenu" event is passed to the
    directory tree at the tree level (not at the individual item level).
    
    To enable keyboard navigation to trigger the context menu, re-dispatch
    the event from the #directory-tree down to the individual items. This
    requires also recomputing the PointerEvent to ensure the context menu is
    rendered at the correct location.
    
    Fixed: b:255700728
    Test: browser_tests --gtest_filter=*dirContextMenuKeyboardNavigation*
    Change-Id: Ia259277ddc3ea77bae94caba2256a6b4f0ddf120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015323
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069023}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1168,7 +1168,8 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("dirContextMenuComputers"),
         TestCase("dirContextMenuTrash").EnableTrash(),
         TestCase("dirContextMenuShortcut"),
-        TestCase("dirContextMenuFocus")));
+        TestCase("dirContextMenuFocus"),
+        TestCase("dirContextMenuKeyboardNavigation")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     DriveSpecific, /* drive_specific.js */
