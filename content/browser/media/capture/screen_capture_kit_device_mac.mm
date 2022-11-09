40f4ef0d6c9fa213dfba2804bc4f369ff6d699e9
Johannes Kron <kron@chromium.org>
2022-11-09 15:08:58 +0000

    Add exception for desktop capture using ScreenCaptureKit
    There's a bug in the desktop capture code of ScreenCaptureKit on macOS<13.
    No error is returned but still nothing is captured.
    
    This CL adds an exception so that we use the old API for desktop capture
    if macOS<13, even though the ScreenCaptureKitMac feature is enabled and
    the API is supported (i.e macOS>=12.3).
    
    Bug: chromium:1352441
    Change-Id: Ib13cc8a6be59a14467c6dfd86f3f7ec725c9fecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013379
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069147}

--- content/browser/media/capture/screen_capture_kit_device_mac.mm
+++ content/browser/media/capture/screen_capture_kit_device_mac.mm
@@ -259,12 +259,23 @@ class API_AVAILABLE(macos(12.3)) ScreenCaptureKitDeviceMac
   base::WeakPtrFactory<ScreenCaptureKitDeviceMac> weak_factory_{this};
 };
 
+// Desktop capture is not working in macOS < 13, see https:://crbug.com/1352441.
+bool IsScreenCaptureKitDesktopWorking(void) {
+  if (@available(macOS 13.0, *)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 }  // namespace
 
 std::unique_ptr<media::VideoCaptureDevice> CreateScreenCaptureKitDeviceMac(
     const DesktopMediaID& source) {
   switch (source.type) {
     case DesktopMediaID::TYPE_SCREEN:
+      if (!IsScreenCaptureKitDesktopWorking())
+        return nullptr;
       // ScreenCaptureKitDeviceMac only supports a single display at a time. It
       // will not stitch desktops together.
       // https://crbug.com/1178360
@@ -281,12 +292,14 @@ std::unique_ptr<media::VideoCaptureDevice> CreateScreenCaptureKitDeviceMac(
       return nullptr;
   }
 
-  IncrementDesktopCaptureCounter(SCREEN_CAPTURER_CREATED);
-  IncrementDesktopCaptureCounter(source.audio_share
-                                     ? SCREEN_CAPTURER_CREATED_WITH_AUDIO
-                                     : SCREEN_CAPTURER_CREATED_WITHOUT_AUDIO);
-  if (@available(macos 12.3, *))
+  if (@available(macOS 12.3, *)) {
+    IncrementDesktopCaptureCounter(SCREEN_CAPTURER_CREATED);
+    IncrementDesktopCaptureCounter(source.audio_share
+                                       ? SCREEN_CAPTURER_CREATED_WITH_AUDIO
+                                       : SCREEN_CAPTURER_CREATED_WITHOUT_AUDIO);
+
     return std::make_unique<ScreenCaptureKitDeviceMac>(source);
+  }
   return nullptr;
 }
 
