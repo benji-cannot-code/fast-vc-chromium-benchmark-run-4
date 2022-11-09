bd6850d1adf1cb1ff6c59a0356d59219b29d1087
Xi Han <hanxi@google.com>
2022-11-09 22:44:07 +0000

    [Start] Make Start not scrollable when the content is shorter than screen.
    1. Resets Start surface scroll when Feed is turned off.
    2. Sets tasks surface body's height WRAP_CONTENT and check whether tasks surface view is shorter than the screen size. If it is, makes it not scrollable by setting AppBarLayout#canDrag false.
    
    Video: http://shortn/_u5D8TNdiuN
    Video for small phone with signin promo:
    https://drive.google.com/file/d/1VnUAR4xPbLchgfTVu-6Ti-UCKWpdAQkj/view?usp=share_link
    
    Bug: 1208486
    Change-Id: I13b04871a60ce5e7564874b416906029383aca3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988566
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069407}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceCoordinator.java
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.features.start_surface;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 import android.app.Activity;
 import android.os.SystemClock;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -279,6 +282,7 @@ public class StartSurfaceCoordinator implements StartSurface {
         mTabSwitcherCustomViewManagerSupplier = new OneshotSupplierImpl<>();
         boolean excludeQueryTiles = !mIsStartSurfaceEnabled
                 || !ChromeFeatureList.sQueryTilesOnStart.isEnabled();
+        boolean hasPrimaryTasksSurfaceCreated = false;
         if (!mIsStartSurfaceEnabled) {
             // Create Tab switcher directly to save one layer in the view hierarchy.
             mTabSwitcher = TabManagementModuleProvider.getDelegate().createGridTabSwitcher(activity,
@@ -293,24 +297,38 @@ public class StartSurfaceCoordinator implements StartSurface {
             // createSwipeRefreshLayout has to be called before creating any surface.
             createSwipeRefreshLayout();
             createAndSetStartSurface(excludeQueryTiles);
+            hasPrimaryTasksSurfaceCreated = true;
         }
 
         TabSwitcher.Controller controller =
                 mTabSwitcher != null ? mTabSwitcher.getController() : mTasksSurface.getController();
         Runnable initializeMVTilesRunnable =
-                mTasksSurface == null ? null : mTasksSurface::initializeMVTiles;
-        View logoContainerView = mTasksSurface == null
-                ? null
-                : mTasksSurface.getView().findViewById(R.id.logo_container);
+                hasPrimaryTasksSurfaceCreated ? mTasksSurface::initializeMVTiles : null;
+        View logoContainerView = hasPrimaryTasksSurfaceCreated
+                ? mTasksSurface.getView().findViewById(R.id.logo_container)
+                : null;
         ViewGroup feedPlaceholderParentView =
-                mTasksSurface == null ? null : mTasksSurface.getBodyViewContainer();
+                hasPrimaryTasksSurfaceCreated ? mTasksSurface.getBodyViewContainer() : null;
         mStartSurfaceMediator = new StartSurfaceMediator(controller, containerView,
                 mTabModelSelector, mPropertyModel,
-                mIsStartSurfaceEnabled ? this::initializeSecondaryTasksSurface : null,
+                hasPrimaryTasksSurfaceCreated ? this::initializeSecondaryTasksSurface : null,
                 mIsStartSurfaceEnabled, mActivity, mBrowserControlsManager,
                 this::isActivityFinishingOrDestroyed, excludeQueryTiles,
                 startSurfaceOneshotSupplier, hadWarmStart, jankTracker, initializeMVTilesRunnable,
                 mParentTabSupplier, logoContainerView, backPressManager, feedPlaceholderParentView);
+
+        // Set the height of Start surface homepage's tasks_surface_body as wrap_content. We only
+        // update the tasks_surface_body in mTasksSurface since it's Start surface homepage while we
+        // don't change mSecondaryTasksSurface (grid tab switcher).
+        // If Feed placeholder is shown and we set body's height wrap_content, feed articles aren't
+        // rendered on emulator. Thus we need to add the check of whether the place holder is
+        // showing. See https://crbug.com/1208486.
+        if (hasPrimaryTasksSurfaceCreated && !mStartSurfaceMediator.hasFeedPlaceholderShown()) {
+            FrameLayout layout = (FrameLayout) mTasksSurface.getBodyViewContainer();
+            LayoutParams params = layout.getLayoutParams();
+            params.height = WRAP_CONTENT;
+            layout.setLayoutParams(params);
+        }
 
         startSurfaceOneshotSupplier.set(this);
     }
@@ -706,6 +724,7 @@ public class StartSurfaceCoordinator implements StartSurface {
         mTasksSurface.getView().setId(R.id.primary_tasks_surface_view);
         initializeOffsetChangedListener();
         addHeaderOffsetChangeListener(mOffsetChangedListenerToGenerateScrollEvents);
+        mTasksSurface.initHeaderDragListener();
 
         mTasksSurfacePropertyModelChangeProcessor =
                 PropertyModelChangeProcessor.create(mPropertyModel,
--- chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
+++ chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
@@ -89,6 +89,8 @@ import org.chromium.chrome.browser.omnibox.OmniboxStub;
 import org.chromium.chrome.browser.omnibox.UrlFocusChangeListener;
 import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
+import org.chromium.chrome.browser.preferences.PrefChangeRegistrar;
+import org.chromium.chrome.browser.preferences.PrefChangeRegistrarJni;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
@@ -183,7 +185,9 @@ public class StartSurfaceMediatorUnitTest {
     @Mock
     private LogoView mLogoView;
     @Mock
-    LogoBridge.Natives mLogoBridge;
+    LogoBridge.Natives mLogoBridgeJni;
+    @Mock
+    private PrefChangeRegistrar.Natives mPrefChangeRegistrarJni;
     @Mock
     private Profile mProfile;
     @Mock
@@ -254,8 +258,10 @@ public class StartSurfaceMediatorUnitTest {
         doReturn(mFeedReliabilityLogger)
                 .when(mExploreSurfaceCoordinator)
                 .getFeedReliabilityLogger();
-        mJniMocker.mock(LogoBridgeJni.TEST_HOOKS, mLogoBridge);
+
+        mJniMocker.mock(LogoBridgeJni.TEST_HOOKS, mLogoBridgeJni);
         doReturn(mLogoView).when(mLogoContainerView).findViewById(R.id.search_provider_logo);
+        mJniMocker.mock(PrefChangeRegistrarJni.TEST_HOOKS, mPrefChangeRegistrarJni);
     }
 
     @After
@@ -1426,7 +1432,7 @@ public class StartSurfaceMediatorUnitTest {
         mediator.showOverview(true);
 
         verify(mLogoContainerView).setVisibility(View.VISIBLE);
-        verify(mLogoBridge).getCurrentLogo(anyLong(), any(), any());
+        verify(mLogoBridgeJni).getCurrentLogo(anyLong(), any(), any());
         Assert.assertTrue(mediator.isLogoVisible());
     }
 
