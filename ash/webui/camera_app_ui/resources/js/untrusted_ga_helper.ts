15cfa53d3be5cfcaa97c8e702ec0c4a29ef5308c
Pi-Hsun Shih <pihsun@chromium.org>
2022-11-09 11:01:10 +0000

    CCA: Change non-null assertion to assertExists
    We have eslint rule to warn on non-null assertion. Change them to
    runtime assertion.
    
    Bug: None
    Test: cca.py lint
    Change-Id: I671d8a072e19199a0c17e61952afd0ffe398ee66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015307
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069077}

--- ash/webui/camera_app_ui/resources/js/untrusted_ga_helper.ts
+++ ash/webui/camera_app_ui/resources/js/untrusted_ga_helper.ts
@@ -3,15 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert} from './assert.js';
+import {assert, assertExists} from './assert.js';
 
 /**
  * The GA library URL in trusted type.
  */
 const gaLibraryURL = (() => {
-  const staticUrlPolicy = window.trustedTypes!.createPolicy(
-      'ga-js-static',
-      {createScriptURL: (_url: string) => '../js/lib/analytics.js'});
+  const staticUrlPolicy =
+      assertExists(window.trustedTypes).createPolicy('ga-js-static', {
+        createScriptURL: (_url: string) => '../js/lib/analytics.js',
+      });
   return staticUrlPolicy.createScriptURL('');
 })();
 
--- ash/webui/camera_app_ui/resources/js/untrusted_video_processor_helper.ts
+++ ash/webui/camera_app_ui/resources/js/untrusted_video_processor_helper.ts
@@ -3,17 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertExists} from './assert.js';
 import * as Comlink from './lib/comlink.js';
 
 /**
  * The MP4 video processor URL in trusted type.
  */
 const mp4VideoProcessorURL: TrustedScriptURL = (() => {
-  const staticUrlPolicy =
-      window.trustedTypes!.createPolicy('video-processor-js-static', {
-        createScriptURL: (_url: string) =>
-            '/js/models/ffmpeg/video_processor.js',
-      });
+  const staticUrlPolicy = assertExists(window.trustedTypes)
+                              .createPolicy('video-processor-js-static', {
+                                createScriptURL: (_url: string) =>
+                                    '/js/models/ffmpeg/video_processor.js',
+                              });
   // TODO(crbug.com/980846): Remove the empty string if
   // https://github.com/w3c/webappsec-trusted-types/issues/278 gets fixed.
   return staticUrlPolicy.createScriptURL('');
