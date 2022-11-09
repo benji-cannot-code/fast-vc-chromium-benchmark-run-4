514255792f3510c182166bf3da74dac4b18ede36
Andrew Paseltiner <apaseltiner@chromium.org>
2022-11-09 13:45:35 +0000

    Include all destinations in event-level reports
    For sources with only one destination, the report body's format is
    unchanged. For sources with multiple destinations, the
    attribution_destination field is a list of them.
    
    Bug: 1382389
    Change-Id: Ibf3cbd03d83466536cf02c43cadb83d9c453bc98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013641
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069115}

--- content/browser/attribution_reporting/attribution_test_utils.h
+++ content/browser/attribution_reporting/attribution_test_utils.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
@@ -442,6 +443,8 @@ class SourceBuilder {
 
   SourceBuilder& SetDestinationOrigin(url::Origin origin);
 
+  SourceBuilder& SetDestinationOrigins(base::flat_set<url::Origin>);
+
   SourceBuilder& SetReportingOrigin(url::Origin origin);
 
   SourceBuilder& SetSourceType(AttributionSourceType source_type);
@@ -487,7 +490,7 @@ class SourceBuilder {
   absl::optional<base::TimeDelta> event_report_window_;
   absl::optional<base::TimeDelta> aggregatable_report_window_;
   url::Origin source_origin_;
-  url::Origin destination_origin_;
+  base::flat_set<url::Origin> destination_origins_;
   url::Origin reporting_origin_;
   AttributionSourceType source_type_ = AttributionSourceType::kNavigation;
   int64_t priority_ = 0;
