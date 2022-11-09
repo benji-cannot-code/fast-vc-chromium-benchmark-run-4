7b76ca4be7c9d0009c6b7eec8e3ee0d6e61163e6
Ce Chen <cch@chromium.org>
2022-11-09 16:36:16 +0000

    [omnibox] add downloader for on device tail suggest.
    Tested in MacOS version 12.6.1
    
    Bug: 1372112
    Change-Id: Ide615a6b8b44e399e33ce1183cdbea614a0075ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000125
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: C Ch <cch@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069186}

--- components/omnibox/browser/on_device_model_update_listener.h
+++ components/omnibox/browser/on_device_model_update_listener.h
@@ -8,26 +8,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/flat_set.h"
 #include "base/files/file_path.h"
 #include "base/no_destructor.h"
 #include "base/threading/thread_checker.h"
 
-// This class is used by OnDeviceHeadSuggestComponentInstaller to hold the
-// directory & filename for the on device model downloaded by Component Updater.
+// This class is used by OnDeviceHeadSuggestComponentInstaller or
+// OnDeviceTailModelObserver to hold the filenames for the on device models
+// downloaded by corresponding services.
 class OnDeviceModelUpdateListener {
  public:
 
   static OnDeviceModelUpdateListener* GetInstance();
 
-  // Called by Component Updater when model update is completed to update
-  // |model_dir_| and |model_filename_|.
-  void OnModelUpdate(const base::FilePath& model_dir);
+  // Called by Component Updater when head model update is completed to update
+  // |head_model_dir_| and |head_model_filename_|.
+  void OnHeadModelUpdate(const base::FilePath& model_dir);
 
-  std::string model_filename() const;
+  // Called by on device tail model observer when tail model update is completed
+  // to update |tail_model_filename_| and |vocab_filename_|.
+  void OnTailModelUpdate(
+      const base::FilePath& model_file,
+      const base::flat_set<base::FilePath>& additional_files);
+
+  std::string head_model_filename() const;
+  std::string tail_model_filename() const;
+  std::string vocab_filename() const;
 
  private:
   friend class base::NoDestructor<OnDeviceModelUpdateListener>;
   friend class OnDeviceHeadProviderTest;
+  friend class OnDeviceModelUpdateListenerTest;
 
   void ResetListenerForTest();
 
@@ -37,11 +48,17 @@ class OnDeviceModelUpdateListener {
   OnDeviceModelUpdateListener& operator=(const OnDeviceModelUpdateListener&) =
       delete;
 
-  // The directory where the on device model resides.
-  base::FilePath model_dir_;
+  // The directory where the on device head model resides.
+  base::FilePath head_model_dir_;
 
-  // The filename of the model.
-  std::string model_filename_;
+  // The filename of the head model.
+  std::string head_model_filename_;
+
+  // The filename of the tail model.
+  std::string tail_model_filename_;
+
+  // The filename of the vocabulary file for the tail model.
+  std::string vocab_filename_;
 
   THREAD_CHECKER(thread_checker_);
 };
