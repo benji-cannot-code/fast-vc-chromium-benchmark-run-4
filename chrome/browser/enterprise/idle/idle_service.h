f2d18e8229ed7368a36e095d38216c3cad42afd0
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-11-09 14:48:55 +0000

    [Idle Timeout] Implement ActionRunner
    Since there's no IdleTimeoutActions policy yet, this is effectively a
    refactor. Actions are still hard-coded in ActionRunner, to
    "close_browsers" + "show_profile_picker".
    
    Add ActionRunner, which takes a list of strings and performs the actions
    specified, using a priority_queue<>. This simplifies IdleService
    implementation, and makes it easier to add new actions (like "clear
    browsing data") in the future.
    
    Bug: 1326685, 1316551
    Change-Id: I97a56a9b7f5c8877b1c83aa0f556ae27fed3f7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968079
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069142}

--- chrome/browser/enterprise/idle/idle_service.h
+++ chrome/browser/enterprise/idle/idle_service.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_IDLE_IDLE_SERVICE_H_
 #define CHROME_BROWSER_ENTERPRISE_IDLE_IDLE_SERVICE_H_
 
+#include <memory>
+
 #include "base/callback_list.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
+#include "chrome/browser/enterprise/idle/action_runner.h"
 #include "chrome/browser/enterprise/idle/browser_closer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -47,12 +51,11 @@ class IdleService : public KeyedService,
   void OnCloseFinished(BrowserCloser::CloseResult result);
 
   raw_ptr<Profile> const profile_;
+  std::unique_ptr<ActionRunner> action_runner_;
   PrefChangeRegistrar pref_change_registrar_;
 
   bool is_idle_ = false;
   base::TimeDelta idle_threshold_;
-
-  base::CallbackListSubscription browser_close_subscription_;
 
   base::ScopedObservation<ui::IdlePollingService,
                           ui::IdlePollingService::Observer>
