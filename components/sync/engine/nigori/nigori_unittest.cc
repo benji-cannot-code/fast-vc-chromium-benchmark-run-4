a032fa1b9ee7dc23e4a82f29b9c1d9e69eee9ae0
Maksim Moskvitin <mmoskvitin@google.com>
2022-11-09 10:15:01 +0000

    [Sync] Update Nigori::Encrypt() to indicate it never fails
    Encryption success doesn't depend on input (except extreme corner cases
    when input is way too long - but this would probably cause OOM crash).
    
    Keys themselves are either derived in the sane way or imported and
    verified, so corrupted keys shouldn't be possible.
    
    Besides that, there are more generic reasons to fail, like OOM inside
    BoringSSL. It seems reasonable to crash in such cases and perhaps even
    safer to do so.
    
    This CL updates Encrypt() method to use CHECK() inside and return
    encrypted output directly (instead of using output parameter with
    boolean result).
    
    Bug: 1368018
    Change-Id: I65dba67731ede1c0edd2682245d74768d6cfca4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008281
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069066}

--- components/sync/engine/nigori/nigori_unittest.cc
+++ components/sync/engine/nigori/nigori_unittest.cc
@@ -84,15 +84,8 @@ TEST(SyncNigoriTest, EncryptDifferentIv) {
       KeyDerivationParams::CreateForPbkdf2(), "password");
   ASSERT_THAT(nigori, NotNull());
 
-  std::string plaintext("value");
-
-  std::string encrypted1;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted1));
-
-  std::string encrypted2;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted2));
-
-  EXPECT_NE(encrypted1, encrypted2);
+  const std::string plaintext("value");
+  EXPECT_NE(nigori->Encrypt(plaintext), nigori->Encrypt(plaintext));
 }
 
 TEST(SyncNigoriTest, Decrypt) {
@@ -116,13 +109,10 @@ TEST(SyncNigoriTest, EncryptDecrypt) {
       KeyDerivationParams::CreateForPbkdf2(), "password");
   ASSERT_THAT(nigori, NotNull());
 
-  std::string plaintext("value");
-
-  std::string encrypted;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted));
+  const std::string plaintext("value");
 
   std::string decrypted;
-  EXPECT_TRUE(nigori->Decrypt(encrypted, &decrypted));
+  EXPECT_TRUE(nigori->Decrypt(nigori->Encrypt(plaintext), &decrypted));
 
   EXPECT_EQ(plaintext, decrypted);
 }
@@ -132,13 +122,10 @@ TEST(SyncNigoriTest, EncryptDecryptEmptyString) {
       KeyDerivationParams::CreateForPbkdf2(), "password");
   ASSERT_THAT(nigori, NotNull());
 
-  std::string plaintext;
-
-  std::string encrypted;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted));
+  const std::string plaintext;
 
   std::string decrypted;
-  EXPECT_TRUE(nigori->Decrypt(encrypted, &decrypted));
+  EXPECT_TRUE(nigori->Decrypt(nigori->Encrypt(plaintext), &decrypted));
 
   EXPECT_EQ(plaintext, decrypted);
 }
@@ -148,10 +135,9 @@ TEST(SyncNigoriTest, CorruptedIv) {
       KeyDerivationParams::CreateForPbkdf2(), "password");
   ASSERT_THAT(nigori, NotNull());
 
-  std::string plaintext("test");
+  const std::string plaintext("test");
 
-  std::string encrypted;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted));
+  std::string encrypted = nigori->Encrypt(plaintext);
 
   // Corrupt the IV by changing one of its byte.
   encrypted[0] = (encrypted[0] == 'a' ? 'b' : 'a');
@@ -167,10 +153,9 @@ TEST(SyncNigoriTest, CorruptedCiphertext) {
       KeyDerivationParams::CreateForPbkdf2(), "password");
   ASSERT_THAT(nigori, NotNull());
 
-  std::string plaintext("test");
+  const std::string plaintext("test");
 
-  std::string encrypted;
-  EXPECT_TRUE(nigori->Encrypt(plaintext, &encrypted));
+  std::string encrypted = nigori->Encrypt(plaintext);
 
   // Corrput the ciphertext by changing one of its bytes.
   encrypted[Nigori::kIvSize + 10] =
@@ -200,12 +185,10 @@ TEST(SyncNigoriTest, ExportImport) {
   std::string plaintext;
   std::string ciphertext;
 
-  EXPECT_TRUE(nigori1->Encrypt(original, &ciphertext));
-  EXPECT_TRUE(nigori2->Decrypt(ciphertext, &plaintext));
+  EXPECT_TRUE(nigori2->Decrypt(nigori1->Encrypt(original), &plaintext));
   EXPECT_EQ(original, plaintext);
 
-  EXPECT_TRUE(nigori2->Encrypt(original, &ciphertext));
-  EXPECT_TRUE(nigori1->Decrypt(ciphertext, &plaintext));
+  EXPECT_TRUE(nigori1->Decrypt(nigori2->Encrypt(original), &plaintext));
   EXPECT_EQ(original, plaintext);
 
   std::string permuted1, permuted2;
