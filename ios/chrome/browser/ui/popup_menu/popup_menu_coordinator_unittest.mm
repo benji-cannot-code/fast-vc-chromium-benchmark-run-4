086af80474e7c5ffa4727db173fa6990c8a2a317
Louis Romero <lpromero@google.com>
2022-11-09 13:35:20 +0000

    Migrate new overflow menu to LayoutGuideCenter
    This CL migrates how the new overflow menu anchors itself, by using the
    LayoutGuideCenter instead of NamedGuide.
    
    Bug: 1382054
    Change-Id: I7ce5ff568c7690868efbce71e943ae7b362bdc47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008165
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069112}

--- ios/chrome/browser/ui/popup_menu/popup_menu_coordinator_unittest.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_coordinator_unittest.mm
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/page_info_commands.h"
 #import "ios/chrome/browser/ui/commands/popup_menu_commands.h"
 #import "ios/chrome/browser/ui/commands/qr_scanner_commands.h"
+#import "ios/chrome/browser/ui/main/scene_state.h"
+#import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_presenter_delegate.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_ui_updating.h"
 #import "ios/chrome/browser/ui/util/layout_guide_names.h"
@@ -57,6 +59,10 @@ class PopupMenuCoordinatorTest : public PlatformTest {
   PopupMenuCoordinatorTest() {
     browser_state_ = TestChromeBrowserState::Builder().Build();
     browser_ = std::make_unique<TestBrowser>(browser_state_.get());
+    // PopupMenuCoordinator relies on LayoutGuideCenter, which requires a scene
+    // state browser agent.
+    scene_state_ = [[SceneState alloc] initWithAppState:nil];
+    SceneStateBrowserAgent::CreateForBrowser(browser_.get(), scene_state_);
   }
 
   void SetUp() override {
@@ -91,6 +97,7 @@ class PopupMenuCoordinatorTest : public PlatformTest {
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<TestChromeBrowserState> browser_state_;
   std::unique_ptr<TestBrowser> browser_;
+  SceneState* scene_state_;
   id mock_browser_commands_handler_;
   id mock_browser_coordinator_commands_handler_;
   id mock_bookmarks_commands_handler_;
