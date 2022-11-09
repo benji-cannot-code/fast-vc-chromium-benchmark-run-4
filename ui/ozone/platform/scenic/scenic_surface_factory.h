80edc8492b00fab02b3072d8e16007eed2af28b6
Peter Varga <pvarga@inf.u-szeged.hu>
2022-11-09 00:21:50 +0000

    [Fuchsia] Add assert(enable_vulkan) to Scenic build
    Scenic can't be built without Vulkan. Also remove
    BUILDFLAG(ENABLE_VULKAN) checks.
    
    Bug: none
    Change-Id: I677c9798fcf73108d4d187b5c2bdaaef02bb481f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4001790
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068888}

--- ui/ozone/platform/scenic/scenic_surface_factory.h
+++ ui/ozone/platform/scenic/scenic_surface_factory.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/thread_annotations.h"
 #include "base/threading/thread_checker.h"
-#include "gpu/vulkan/buildflags.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/system/handle.h"
@@ -70,11 +69,9 @@ class ScenicSurfaceFactory : public SurfaceFactoryOzone {
       gfx::Size size,
       gfx::BufferFormat format,
       gfx::NativePixmapHandle handle) override;
-#if BUILDFLAG(ENABLE_VULKAN)
   std::unique_ptr<gpu::VulkanImplementation> CreateVulkanImplementation(
       bool use_swiftshader,
       bool allow_protected_memory) override;
-#endif
 
   // Registers a surface for a |widget|.
   //
