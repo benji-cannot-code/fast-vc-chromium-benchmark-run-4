e4196a66eb8309e9fef33bd4ab4775722e5baba3
Victor Tan <victortan@chromium.org>
2022-11-09 22:41:54 +0000

    Add usecounter for ReduceAcceptLanguage
    Add counter for the ReduceAcceptLanguage feature which is required to
    run an origin trial. We are more interested in the usage for the JS getter navigator.languages.
    
    Bug: 1323776
    Change-Id: Ic2cdf904af33f4c26297969bbc62d30228fe5b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017293
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069403}

--- third_party/blink/renderer/core/frame/navigator_language.cc
+++ third_party/blink/renderer/core/frame/navigator_language.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/navigator_language.h"
 
 #include "services/network/public/cpp/features.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/language.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
@@ -70,6 +71,8 @@ void NavigatorLanguage::EnsureUpdatedLanguage() {
       if (RuntimeEnabledFeatures::ReduceAcceptLanguageEnabled(
               execution_context_)) {
         languages_ = Vector<String>({languages_.front()});
+        UseCounter::Count(execution_context_,
+                          WebFeature::kReduceAcceptLanguage);
       }
     }
 
