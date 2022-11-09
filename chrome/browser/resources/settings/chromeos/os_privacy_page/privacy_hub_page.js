80d4373ec58cb267a9c2465fae40deb337c9aef7
Md Shahadat Hossain Shahin <shahinmd@google.com>
2022-11-09 09:47:26 +0000

    [Privacy Hub] Update mic and camera toggle sublabel
    The sublabel of the camera toggle will be static instead of changing
    depending on the availability of cameras/microphones.
    
    Bug: b/257201701
    Change-Id: Ie452126412793d0cecba54623bbb2f06f97edf77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011341
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069056}

--- chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_page.js
+++ chrome/browser/resources/settings/chromeos/os_privacy_page/privacy_hub_page.js
@@ -142,12 +142,6 @@ class SettingsPrivacyHubPage extends SettingsPrivacyHubPageBase {
         computed: 'computeIsCameraListEmpty_(camerasConnected_)',
       },
 
-      /** @private {string} */
-      cameraToggleSubLabel_: {
-        type: String,
-        computed: 'computeCameraToggleSubLabel_(isCameraListEmpty_)',
-      },
-
       /**
        * The list of connected microphones.
        * @private {Array<string>}
@@ -161,12 +155,6 @@ class SettingsPrivacyHubPage extends SettingsPrivacyHubPageBase {
       isMicListEmpty_: {
         type: Boolean,
         computed: 'computeIsMicListEmpty_(microphonesConnected_)',
-      },
-
-      /** @private {string} */
-      microphoneToggleSubLabel_: {
-        type: String,
-        computed: 'computeMicrophoneToggleSubLabel_(isMicListEmpty_)',
       },
 
       /** @private {boolean} */
@@ -206,34 +194,12 @@ class SettingsPrivacyHubPage extends SettingsPrivacyHubPageBase {
   }
 
   /**
-   * @return {string} The camera toggle sublabel.
-   * @private
-   */
-  computeCameraToggleSubLabel_() {
-    if (this.isCameraListEmpty_) {
-      return '';
-    }
-    return this.i18n('cameraToggleSubtext');
-  }
-
-  /**
    * @return {boolean} Whether the list of microphones displayed in this page is
    *     empty.
    * @private
    */
   computeIsMicListEmpty_() {
     return this.microphonesConnected_.length === 0;
-  }
-
-  /**
-   * @return {string} The microphone toggle sublabel.
-   * @private
-   */
-  computeMicrophoneToggleSubLabel_() {
-    if (this.isMicListEmpty_) {
-      return '';
-    }
-    return this.i18n('microphoneToggleSubtext');
   }
 
   /**
