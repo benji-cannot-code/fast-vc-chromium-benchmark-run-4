5d879e84f0e44fe709e2d4119251b15c2bf6c38d
Sergey Poromov <poromov@chromium.org>
2022-11-09 09:20:14 +0000

    dlp: Fix UAF in DlpScopedFileAccessDelegate.
    This class will be destroyed before DlpClient
    that might call a callback to
    DlpScopedFileAccessDelegate::OnResponse()
    
    Bug: 1381094
    Change-Id: I491d9df71705bd081d9649dec612e82829319b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013548
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069044}

--- chrome/browser/chromeos/policy/dlp/dlp_scoped_file_access_delegate.h
+++ chrome/browser/chromeos/policy/dlp/dlp_scoped_file_access_delegate.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/scoped_file.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chromeos/dbus/dlp/dlp_service.pb.h"
 #include "components/file_access/scoped_file_access.h"
 #include "components/file_access/scoped_file_access_delegate.h"
@@ -66,6 +67,8 @@ class DlpScopedFileAccessDelegate
   // browser instance. It is initialized and destructed at the same time as
   // dbus.
   raw_ptr<chromeos::DlpClient> client_;
+
+  base::WeakPtrFactory<DlpScopedFileAccessDelegate> weak_ptr_factory_;
 };
 
 }  // namespace policy
