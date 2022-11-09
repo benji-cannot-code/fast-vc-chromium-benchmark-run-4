e12db1d02ede44225c25ccb9624622d30134be73
Mohamed Amir Yosef <mamir@chromium.org>
2022-11-09 21:43:18 +0000

    [UI] Move PageSwitcherView  under c/b/ui/views/controls
    gerrit doesn't detect the header file rename because I have added some
    documentation (in addition to other minor changes)
    
    the page_switcher_view.h delta is here
    https://diff.googleplex.com/#key=kDzVUQrwytri
    
    Bug: 1382018
    Change-Id: Id9c2a4f4f580d861ec216ab6a52b627dbfe99145
    Low-Coverage-Reason: This is a mechanical CL without new functionality
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013000
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069299}

--- chrome/browser/ui/views/controls/page_switcher_view.cc
+++ chrome/browser/ui/views/controls/page_switcher_view.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/page_info/page_switcher_view.h"
+#include "chrome/browser/ui/views/controls/page_switcher_view.h"
 
 #include "chrome/browser/ui/views/page_info/page_info_view_factory.h"
 #include "ui/views/layout/fill_layout.h"
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 PageSwitcherView::PageSwitcherView(std::unique_ptr<views::View> initial_page) {
   SetLayoutManager(std::make_unique<views::FillLayout>());
   current_page_ = AddChildView(std::move(initial_page));
-  current_page_->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
 }
 
 PageSwitcherView::~PageSwitcherView() = default;
@@ -20,7 +19,6 @@ void PageSwitcherView::SwitchToPage(std::unique_ptr<views::View> page) {
   if (current_page_)
     RemoveChildViewT(current_page_.get());
   current_page_ = AddChildView(std::move(page));
-  current_page_->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
   PreferredSizeChanged();
 }
 
--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/page_info/page_info_bubble_view.h"
 
+#include <memory>
+
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -13,10 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/page_info/page_info_dialog.h"
 #include "chrome/browser/ui/views/bubble_anchor_util_views.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/controls/page_switcher_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_main_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_security_content_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_view_factory.h"
-#include "chrome/browser/ui/views/page_info/page_switcher_view.h"
 #include "components/dom_distiller/core/url_constants.h"
 #include "components/dom_distiller/core/url_utils.h"
 #include "components/page_info/core/features.h"
@@ -166,8 +168,11 @@ PageInfoBubbleView::PageInfoBubbleView(
 
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical));
-  page_container_ = AddChildView(std::make_unique<PageSwitcherView>(
-      view_factory_->CreateMainPageView(std::move(initialized_callback))));
+  std::unique_ptr<views::View> main_page_view =
+      view_factory_->CreateMainPageView(std::move(initialized_callback));
+  main_page_view->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_ = AddChildView(
+      std::make_unique<PageSwitcherView>(std::move(main_page_view)));
 
   views::BubbleDialogDelegateView::CreateBubble(this);
 }
@@ -199,14 +204,20 @@ views::BubbleDialogDelegateView* PageInfoBubbleView::CreatePageInfoBubble(
 }
 
 void PageInfoBubbleView::OpenMainPage(base::OnceClosure initialized_callback) {
-  page_container_->SwitchToPage(
-      view_factory_->CreateMainPageView(std::move(initialized_callback)));
+  std::unique_ptr<views::View> main_page_view =
+      view_factory_->CreateMainPageView(std::move(initialized_callback));
+  main_page_view->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(main_page_view));
 }
 
 void PageInfoBubbleView::OpenSecurityPage() {
   presenter_->RecordPageInfoAction(
       PageInfo::PageInfoAction::PAGE_INFO_SECURITY_DETAILS_OPENED);
-  page_container_->SwitchToPage(view_factory_->CreateSecurityPageView());
+  std::unique_ptr<views::View> security_page_view =
+      view_factory_->CreateSecurityPageView();
+  security_page_view->SetID(
+      PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(security_page_view));
   AnnouncePageOpened(
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURITY_SUBPAGE_HEADER));
 }
@@ -214,7 +225,11 @@ void PageInfoBubbleView::OpenSecurityPage() {
 void PageInfoBubbleView::OpenPermissionPage(ContentSettingsType type) {
   presenter_->RecordPageInfoAction(
       PageInfo::PageInfoAction::PAGE_INFO_PERMISSION_DIALOG_OPENED);
-  page_container_->SwitchToPage(view_factory_->CreatePermissionPageView(type));
+  std::unique_ptr<views::View> permissions_page_view =
+      view_factory_->CreatePermissionPageView(type);
+  permissions_page_view->SetID(
+      PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(permissions_page_view));
   AnnouncePageOpened(PageInfoUI::PermissionTypeToUIString(type));
 }
 
@@ -222,8 +237,11 @@ void PageInfoBubbleView::OpenAboutThisSitePage(
     const page_info::proto::SiteInfo& info) {
   presenter_->RecordPageInfoAction(
       PageInfo::PageInfoAction::PAGE_INFO_ABOUT_THIS_SITE_PAGE_OPENED);
-  page_container_->SwitchToPage(
-      view_factory_->CreateAboutThisSitePageView(info));
+  std::unique_ptr<views::View> about_this_site_page_view =
+      view_factory_->CreateAboutThisSitePageView(info);
+  about_this_site_page_view->SetID(
+      PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(about_this_site_page_view));
   AnnouncePageOpened(
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_ABOUT_THIS_SITE_HEADER));
 }
@@ -231,8 +249,11 @@ void PageInfoBubbleView::OpenAboutThisSitePage(
 void PageInfoBubbleView::OpenAdPersonalizationPage() {
   presenter_->RecordPageInfoAction(
       PageInfo::PageInfoAction::PAGE_INFO_AD_PERSONALIZATION_PAGE_OPENED);
-  page_container_->SwitchToPage(
-      view_factory_->CreateAdPersonalizationPageView());
+  std::unique_ptr<views::View> ad_personalization_page_view =
+      view_factory_->CreateAdPersonalizationPageView();
+  ad_personalization_page_view->SetID(
+      PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(ad_personalization_page_view));
   AnnouncePageOpened(
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_AD_PERSONALIZATION_HEADER));
 }
@@ -240,7 +261,10 @@ void PageInfoBubbleView::OpenAdPersonalizationPage() {
 void PageInfoBubbleView::OpenCookiesPage() {
   presenter_->RecordPageInfoAction(
       PageInfo::PageInfoAction::PAGE_INFO_COOKIES_PAGE_OPENED);
-  page_container_->SwitchToPage(view_factory_->CreateCookiesPageView());
+  std::unique_ptr<views::View> cookies_page_view =
+      view_factory_->CreateCookiesPageView();
+  cookies_page_view->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_CURRENT_VIEW);
+  page_container_->SwitchToPage(std::move(cookies_page_view));
   AnnouncePageOpened(l10n_util::GetStringUTF16(IDS_PAGE_INFO_COOKIES));
 }
 
