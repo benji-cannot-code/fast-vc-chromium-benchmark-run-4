69a443b970691cf8e4570c802cd4836f4e9ad047
Aaron Leventhal <aleventhal@google.com>
2022-11-09 01:28:05 +0000

    Remove unnecessary conditions protecting lifecycle update
    Bug: none
    Change-Id: Ie737cdac27909df37c7576b7ba317ddc98976c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4012220
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068924}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2124,8 +2124,7 @@ const AtomicString& Element::computedRole() {
   Document& document = GetDocument();
   if (!document.IsActive() || !document.View())
     return g_null_atom;
-  if (document.NeedsLayoutTreeUpdate() || document.View()->NeedsLayout() ||
-      document.Lifecycle().GetState() < DocumentLifecycle::kPrePaintClean) {
+  if (document.Lifecycle().GetState() < DocumentLifecycle::kPrePaintClean) {
     document.View()->UpdateAllLifecyclePhasesExceptPaint(
         DocumentUpdateReason::kJavaScript);
   }
@@ -2138,8 +2137,7 @@ String Element::computedName() {
   Document& document = GetDocument();
   if (!document.IsActive() || !document.View())
     return String();
-  if (document.NeedsLayoutTreeUpdate() || document.View()->NeedsLayout() ||
-      document.Lifecycle().GetState() < DocumentLifecycle::kPrePaintClean) {
+  if (document.Lifecycle().GetState() < DocumentLifecycle::kPrePaintClean) {
     document.View()->UpdateAllLifecyclePhasesExceptPaint(
         DocumentUpdateReason::kJavaScript);
   }
