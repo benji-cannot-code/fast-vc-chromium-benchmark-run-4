e7c95d3780265870a11d4ccd06167e5b3318e09a
Marijn Kruisselbrink <mek@chromium.org>
2022-11-10 23:37:39 +0000

    Refactor window staggering code for PWAs.
    In https://crrev.com/c/3622157 code was added to stagger web app
    windows to make sure they aren't created right on top of each other.
    This CL partially reverts that CL, avoiding duplicating the staggering
    logic in WebAppLaunchProcess, and instead enables the (existing)
    logic to stagger newly created browser windows for apps in
    WindowSizer.
    
    This also moves the test that was added in the original CL (which
    didn't actually test the added logic, since SWAs have their own
    staggering code already) to WebAppBrowserTest to make it run on all
    platforms and actually run using a PWA rather than a SWA.
    
    Finally this also removes now redundant logic for staggering SWA
    windows, as the WindowSizer logic now also takes care of those.
    
    Bug: 1111401
    Include-Ci-Only-Tests: true
    Change-Id: Ic6a08dc8c6e151a3eb16f67f1341355e036bb7d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014363
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070047}

--- chrome/browser/ui/ash/system_web_apps/system_web_app_delegate_ui_impl.cc
+++ chrome/browser/ui/ash/system_web_apps/system_web_app_delegate_ui_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/web_app_launch_queue.h"
@@ -59,13 +58,9 @@ Browser* SystemWebAppDelegate::LaunchAndNavigateSystemWebApp(
         kOmitFromSessionRestore, ShouldAllowResize(), ShouldAllowMaximize());
     started_new_navigation = true;
   } else if (!reuse_existing_window) {
-    gfx::Rect initial_bounds = browser->window()->GetRestoredBounds();
-    const int offset = GetLayoutConstant(WEB_APP_WINDOW_STAGGER_OFFSET);
-    initial_bounds.Offset(offset, offset);
     browser = web_app::CreateWebApplicationWindow(
         profile, params.app_id, params.disposition, params.restore_id,
-        kOmitFromSessionRestore, ShouldAllowResize(), ShouldAllowMaximize(),
-        initial_bounds);
+        kOmitFromSessionRestore, ShouldAllowResize(), ShouldAllowMaximize());
     started_new_navigation = true;
   }
 
