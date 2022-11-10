5bd63e924f2af07f3e2154b7cf22c99e9bc6f28a
Yury Khmel <khmel@chromium.org>
2022-11-10 23:28:46 +0000

    arc: Stabilize logic to detect PAI apps not available.
    Current Play Store implementation may shuffle installation by dropping
    existing and restarting. This makes our legacy logic to detect PAI apps
    not installed fragile. This CL changes this by moving default app not
    available detection logic after the last active installation is done.
    
    BUG=b:258534932
    TEST=Manually on nocturne device where flakiness was detected.
    TEST=out/Default/unit_tests --gtest_filter=*ArcDefaultAppTest*
    
    Change-Id: Ib7ffef0f61ed5180d2bd4fa9f06c355440e2efee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021504
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070045}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
@@ -1387,7 +1387,7 @@ void ArcAppListPrefs::OnConnectionClosed() {
   DisableAllApps();
   installing_packages_count_ = 0;
   apps_installations_.clear();
-  detect_default_app_availability_timeout_.Stop();
+  CancelDefaultAppLoadingTimeout();
   ClearIconRequestRecord();
 
   if (sync_service_) {
@@ -1790,6 +1790,10 @@ void ArcAppListPrefs::DetectDefaultAppAvailability() {
 }
 
 void ArcAppListPrefs::MaybeSetDefaultAppLoadingTimeout() {
+  // Don't check if anything is installing or scheduled right now.
+  if (installing_packages_count_)
+    return;
+
   // Find at least one not installed default app package.
   for (const auto& package : default_apps_->GetActivePackages()) {
     if (!GetPackage(package)) {
@@ -1799,6 +1803,10 @@ void ArcAppListPrefs::MaybeSetDefaultAppLoadingTimeout() {
       break;
     }
   }
+}
+
+void ArcAppListPrefs::CancelDefaultAppLoadingTimeout() {
+  detect_default_app_availability_timeout_.Stop();
 }
 
 void ArcAppListPrefs::AddApp(const arc::mojom::AppInfo& app_info) {
@@ -2273,6 +2281,7 @@ void ArcAppListPrefs::OnIconInstalled(const std::string& app_id,
 void ArcAppListPrefs::OnInstallationStarted(
     const absl::optional<std::string>& package_name) {
   ++installing_packages_count_;
+  CancelDefaultAppLoadingTimeout();
 
   if (!package_name.has_value())
     return;
@@ -2313,6 +2322,7 @@ void ArcAppListPrefs::OnInstallationFinished(
     return;
   }
   --installing_packages_count_;
+  MaybeSetDefaultAppLoadingTimeout();
 }
 
 void ArcAppListPrefs::NotifyAppStatesChanged(const std::string& app_id) {
