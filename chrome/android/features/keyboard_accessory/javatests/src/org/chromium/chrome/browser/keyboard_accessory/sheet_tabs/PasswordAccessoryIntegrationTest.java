03fa94e10cdf84bc3b3db13254bbf3bc8f7647b2
Patrick Noland <pnoland@chromium.org>
2022-11-10 22:46:25 +0000

    Revert "[Android][Mfill] Ensure focused input has sufficient space"
    This reverts commit 68303c8e1b4ca3142397a228d725351f4ddc816c.
    
    Reason for revert: Causing AddressAccessoryIntegrationTest#testFillsSuggestionOnClick to fail on multiple builders, e.g.
    https://ci.chromium.org/p/chrome/builders/ci/android-arm-tests
    
    with error
    androidx.test.espresso.PerformException: Error performing 'performing ViewAction: with tab with matching description. on item matching: holder with view: is assignable from class: class org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryButtonGroupView' on view '(with id: org.chromium.chrome.tests:id/bar_items_view and is displayed on the screen to the user)'.
    
    
    Original change's description:
    > [Android][Mfill] Ensure focused input has sufficient space
    >
    > This change fixes the calculation for sufficient space in the
    > accessory. 80dip need to be left.
    > Including the 48dip bar, 128 dip need to be left.
    > The bottom inset of the soft keyboard is *not* provided by the viewport
    > inset supplier but by the window.
    > The |webcontents height - window inset| must therefore be at least
    > be 128 dip to allow the accessory.
    >
    > The computation for minimal available space is incorrect at the moment:
    > it adds the height of the accessory to the webContents height
    > (presumably because that amounted to the visible height + bar) which is
    > not sufficient since it doesn't include the soft keyboard.
    > Maybe during one of the inset provider changes, it broke?
    > This used to be covered by layout changes.
    >
    > Bug: 1382768
    > Change-Id: I5390f4b542f444ad9d22cd6f1169aced23d6a524
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016832
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1069830}
    
    Bug: 1382768
    Change-Id: Ia856c12e6ecb57e71ee3efa1b98937e083c206f3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020986
    Owners-Override: Patrick Noland <pnoland@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070030}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessoryIntegrationTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessoryIntegrationTest.java
@@ -18,7 +18,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.AllOf.allOf;
 
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingTestHelper.isTransformed;
-import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingTestHelper.scrollToLastElement;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingTestHelper.selectTabAtPosition;
 import static org.chromium.chrome.browser.keyboard_accessory.ManualFillingTestHelper.whenDisplayed;
 import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabTestHelper.isKeyboardAccessoryTabLayout;
@@ -114,7 +113,7 @@ public class PasswordAccessoryIntegrationTest {
                 .perform(selectTabAtPosition(0));
 
         mHelper.waitForKeyboardToDisappear();
-        whenDisplayed(withId(R.id.passwords_sheet)).perform(scrollToLastElement());
+        whenDisplayed(withId(R.id.passwords_sheet));
         onView(withText(containsString("Manage password"))).check(matches(isDisplayed()));
     }
 
