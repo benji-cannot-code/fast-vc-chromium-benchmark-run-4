b3e3067df8bd9b92b806152ffe93caa6eb36a75c
Roman Sorokin <rsorokin@google.com>
2022-11-10 11:58:35 +0000

    [Sheriff] Revert "[Code Health] Remove NOTIFICATION_CRX_INSTALLER_DONE usage in ExtensionBrowserTest"
    This reverts commit 39c6bf7bff307e94692e1054f92b3ab1a2ef0884.
    
    Reason for revert: Looks like the ThemeServiceBrowserTest.GetColorForToolbarButton test became flaky after this CL, See history: https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FThemeServiceBrowserTest.GetColorForToolbarButton?q=VHASH%3A282db19e8ac0a6be
    
    Original change's description:
    > [Code Health] Remove NOTIFICATION_CRX_INSTALLER_DONE usage in ExtensionBrowserTest
    >
    > Part of the larger effort of removing the NotificationService.
    >
    > Bug: 1174728
    > Change-Id: I7c032ea72b9c070129d2bba098dc7378881f5e4d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3987943
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1066387}
    
    Bug: 1174728
    Change-Id: Ifc5dec13c8ace8c1f6208ed933e065d32e432bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020155
    Owners-Override: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Daniel d'Andrada <dandrader@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069700}

--- chrome/browser/extensions/extension_browsertest.cc
+++ chrome/browser/extensions/extension_browsertest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/test_future.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -648,8 +647,6 @@ const Extension* ExtensionBrowserTest::InstallOrUpdateExtension(
   ExtensionRegistry* registry = extension_registry();
   size_t num_before = registry->enabled_extensions().size();
 
-  scoped_refptr<CrxInstaller> installer;
-  absl::optional<CrxInstallError> install_error;
   {
     std::unique_ptr<ScopedTestDialogAutoConfirm> prompt_auto_confirm;
     if (ui_type == INSTALL_UI_TYPE_CANCEL) {
@@ -684,8 +681,8 @@ const Extension* ExtensionBrowserTest::InstallOrUpdateExtension(
       install_ui = std::make_unique<ExtensionInstallPrompt>(
           browser->tab_strip_model()->GetActiveWebContents());
     }
-    installer =
-        CrxInstaller::Create(extension_service(), std::move(install_ui));
+    scoped_refptr<CrxInstaller> installer(
+        CrxInstaller::Create(extension_service(), std::move(install_ui)));
     installer->set_expected_id(id);
     installer->set_creation_flags(creation_flags);
     installer->set_install_source(install_source);
@@ -696,15 +693,12 @@ const Extension* ExtensionBrowserTest::InstallOrUpdateExtension(
           CrxInstaller::OffStoreInstallAllowedInTest);
     }
 
-    base::test::TestFuture<absl::optional<CrxInstallError>>
-        installer_done_future;
-    installer->AddInstallerCallback(
-        installer_done_future
-            .GetCallback<const absl::optional<CrxInstallError>&>());
+    observer_->Watch(NOTIFICATION_CRX_INSTALLER_DONE,
+                     content::Source<CrxInstaller>(installer.get()));
 
     installer->InstallCrx(crx_path);
 
-    install_error = installer_done_future.Get();
+    observer_->Wait();
   }
 
   size_t num_after = registry->enabled_extensions().size();
@@ -729,13 +723,7 @@ const Extension* ExtensionBrowserTest::InstallOrUpdateExtension(
 
   if (!observer_->WaitForExtensionViewsToLoad())
     return nullptr;
-
-  if (install_error)
-    return nullptr;
-
-  // Even though we can already get the Extension from the CrxInstaller,
-  // ensure it's also in the list of enabled extensions.
-  return registry->GetExtensionById(installer->extension()->id(),
+  return registry->GetExtensionById(last_loaded_extension_id(),
                                     ExtensionRegistry::ENABLED);
 }
 
