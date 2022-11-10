46f5c5eac7597bbee51a788384898e43d1573f20
Yue Zhang <yuezhanggg@chromium.org>
2022-11-10 19:41:53 +0000

    [ACD] Support cart extraction for potential partner merchants [2]
    This CL supports discounttiredirect.com.
    
    Bug: 1196078
    Change-Id: I49cd02ff0580e1cdeed12c1b1bcd7bd70cfc8abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000403
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069907}

--- chrome/renderer/resources/cart/cart-product-extraction.js
+++ chrome/renderer/resources/cart/cart-product-extraction.js
@@ -123,6 +123,13 @@ function extractImage(item) {
     }
     return extractImageUrl(image);
   }
+  if (hostname.endsWith("discounttiredirect.com")) {
+    const image = item.querySelector(".cart-item__product-image");
+    if (image == null) {
+      return null;
+    }
+    return extractImageUrl(image);
+  }
   // Sometimes an item contains small icons, which need to be filtered out.
   // TODO: two pass getLargeImages() is probably too slow.
   let images = getLargeImages(item, 40);
@@ -156,9 +163,11 @@ function extractImageUrl(image) {
   if (lazyUrl != null)
     return lazyUrl;
 
-  // Special handling for Google store and America's Tire.
+  // Special handling for Google store, America's Tire and Discount
+  // Tire Direct.
   if (image.className === "bg-img"
-    || image.className.includes("product-image__image-block")) {
+    || image.className.includes("product-image__image-block")
+    || image.className.includes("cart-item__product-image")) {
     if (image.style.backgroundImage == undefined) {
       return null;
     }
@@ -216,7 +225,8 @@ function extractUrl(item) {
   // panel after clicking on each item instead of directing to product page.
   if (document.URL.includes("samsclub.com")
       || document.URL.includes("ae.com")
-      || document.URL.includes("kiehls.com")) {
+      || document.URL.includes("kiehls.com")
+      || document.URL.includes("discounttiredirect.com")) {
     return "";
   }
   let anchors;
@@ -961,6 +971,8 @@ async function extractAllItems(root) {
   } else if (document.URL.includes("americastire.com")
     || document.URL.includes("discounttire.com")) {
     items = root.querySelectorAll("[role=\"listitem\"]");
+  } else if (document.URL.includes("discounttiredirect.com")) {
+    items = root.querySelectorAll(".cart-item");
   } else {
     skipFiltering = false;
     // Generic pattern
