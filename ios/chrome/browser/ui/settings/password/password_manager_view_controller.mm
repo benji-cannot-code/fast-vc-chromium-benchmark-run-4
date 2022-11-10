611ea6aab05a6e4478d8e644c35d5f7387726d6f
Ernesto Izquierdo Clua <eic@google.com>
2022-11-10 19:22:18 +0000

    Show empty state view after deleting item in password manager.
    Fixed: 1383174
    Change-Id: Ieff56a6976f15bff9e066a58986daa0a43bcb866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020965
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069890}

--- ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
@@ -529,8 +529,9 @@ NSInteger kTrailingSymbolSize = 18;
 
   [self showOrHideEmptyView];
 
-  // If we don't have data or settings to show, add an empty state, then stop
-  // so that we don't add anything that overlaps the illustrated background.
+  // If we don't have data or settings to show, add an empty state, then
+  // stop so that we don't add anything that overlaps the illustrated
+  // background.
   if ([self shouldShowEmptyStateView]) {
     return;
   }
@@ -1219,6 +1220,12 @@ NSInteger kTrailingSymbolSize = 18;
 }
 
 - (void)updatePasswordManagerUI {
+  if ([self shouldShowEmptyStateView]) {
+    [self setEditing:NO animated:YES];
+    [self reloadData];
+    return;
+  }
+
   TableViewModel* model = self.tableViewModel;
   NSMutableIndexSet* sectionsToUpdate = [NSMutableIndexSet indexSet];
 
