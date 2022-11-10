801149af709a07b7ed587c95d46f83bf14a88b84
Xi Han <hanxi@chromium.org>
2022-11-10 16:05:09 +0000

    Revert "[Start] Make Start not scrollable when the content is shorter than screen."
    This reverts commit 5de7a31a25b276e9e3258769347fdd54f7401b97.
    
    Reason for revert: crbug.com/1383105
    
    Original change's description:
    > [Start] Make Start not scrollable when the content is shorter than screen.
    >
    > 1. Resets Start surface scroll when Feed is turned off.
    > 2. Sets tasks surface body's height WRAP_CONTENT and check whether tasks surface view is shorter than the screen size. If it is, makes it not scrollable by setting AppBarLayout#canDrag false.
    >
    > Video: http://shortn/_u5D8TNdiuN
    > Video for small phone with signin promo:
    > https://drive.google.com/file/d/1VnUAR4xPbLchgfTVu-6Ti-UCKWpdAQkj/view?usp=share_link
    >
    > Bug: 1208486
    > Change-Id: I13b04871a60ce5e7564874b416906029383aca3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988566
    > Commit-Queue: Xi Han <hanxi@chromium.org>
    > Reviewed-by: Fred Mello <fredmello@chromium.org>
    > Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1069407}
    
    Bug: 1208486
    Change-Id: I32f157b45a2397aacbeca7ee397ae4feb62b8611
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020744
    Auto-Submit: Xi Han <hanxi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1069789}

--- chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
+++ chrome/android/features/start_surface/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
@@ -89,8 +89,6 @@ import org.chromium.chrome.browser.omnibox.OmniboxStub;
 import org.chromium.chrome.browser.omnibox.UrlFocusChangeListener;
 import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
-import org.chromium.chrome.browser.preferences.PrefChangeRegistrar;
-import org.chromium.chrome.browser.preferences.PrefChangeRegistrarJni;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
@@ -185,9 +183,7 @@ public class StartSurfaceMediatorUnitTest {
     @Mock
     private LogoView mLogoView;
     @Mock
-    LogoBridge.Natives mLogoBridgeJni;
-    @Mock
-    private PrefChangeRegistrar.Natives mPrefChangeRegistrarJni;
+    LogoBridge.Natives mLogoBridge;
     @Mock
     private Profile mProfile;
     @Mock
@@ -258,10 +254,8 @@ public class StartSurfaceMediatorUnitTest {
         doReturn(mFeedReliabilityLogger)
                 .when(mExploreSurfaceCoordinator)
                 .getFeedReliabilityLogger();
-
-        mJniMocker.mock(LogoBridgeJni.TEST_HOOKS, mLogoBridgeJni);
+        mJniMocker.mock(LogoBridgeJni.TEST_HOOKS, mLogoBridge);
         doReturn(mLogoView).when(mLogoContainerView).findViewById(R.id.search_provider_logo);
-        mJniMocker.mock(PrefChangeRegistrarJni.TEST_HOOKS, mPrefChangeRegistrarJni);
     }
 
     @After
@@ -1432,7 +1426,7 @@ public class StartSurfaceMediatorUnitTest {
         mediator.showOverview(true);
 
         verify(mLogoContainerView).setVisibility(View.VISIBLE);
-        verify(mLogoBridgeJni).getCurrentLogo(anyLong(), any(), any());
+        verify(mLogoBridge).getCurrentLogo(anyLong(), any(), any());
         Assert.assertTrue(mediator.isLogoVisible());
     }
 
