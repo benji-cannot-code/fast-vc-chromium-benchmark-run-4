ea93d79ee184e17ea2189f4b1289371e3ee5d5e2
David Bokan <bokan@chromium.org>
2022-11-10 04:17:05 +0000

    VT: Fix snapshot rect for a resizing virtual keyboard
    ViewTransitions compute the "snapshot rect" which is a viewport-like
    rect that doesn't change size when UI such as scrollbars, virtual
    keyboard, mobile URL bar are showing or hidden.
    
    When the keyboard is in a resize-content mode (so that innerHeight is
    changed by the keyboard, see:
    https://developer.chrome.com/blog/viewport-resize-behavior/), the
    snapshot rect must account for the keyboard by outsetting the layout
    viewport.
    
    This CL plumbs the virtual-keyboard height into the renderer via
    SynchronizeVisualProperties so that DocumentTransitionStyleTracker can
    use this value to perform the outset. This value includes any keyboard
    accessories such as the autofill bar.
    
    Since the keyboard resize behavior is implemented in the //chrome layer,
    this CL adds end-to-end chrome browser tests for this behavior. This
    requires introducing some hand-rolled pixel testing functionality into
    java tests which isn't common so also requires plumbing the
    InsertVisualStateCallback mechanism into java, allowing tests to wait on
    a Blink main frame to be presented to the screen.
    
    Bug: 1350910
    Change-Id: Ie1b01cb5082e7059a0e03b99d25a361f3f87e189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971061
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069581}

--- content/public/android/java/src/org/chromium/content/browser/framehost/RenderFrameHostImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/framehost/RenderFrameHostImpl.java
@@ -229,6 +229,12 @@ public class RenderFrameHostImpl implements RenderFrameHost {
                 mNativeRenderFrameHostAndroid, RenderFrameHostImpl.this);
     }
 
+    @Override
+    public void forceRedrawAndWaitForPresentation(Runnable callback) {
+        RenderFrameHostImplJni.get().forceRedrawAndWaitForPresentation(
+                mNativeRenderFrameHostAndroid, callback);
+    }
+
     @NativeMethods
     interface Natives {
         GURL getLastCommittedURL(long nativeRenderFrameHostAndroid, RenderFrameHostImpl caller);
@@ -259,5 +265,7 @@ public class RenderFrameHostImpl implements RenderFrameHost {
                 RenderFrameHostImpl caller, String relyingPartyId, Origin effectiveOrigin,
                 boolean isPaymentCredentialCreation);
         int getLifecycleState(long nativeRenderFrameHostAndroid, RenderFrameHostImpl caller);
+        void forceRedrawAndWaitForPresentation(
+                long nativeRenderFrameHostAndroid, Runnable callback);
     }
 }
