d4334b34d9fea4b868b2ad1a012deef2b2d891e4
Mason Freed <masonf@chromium.org>
2022-11-10 15:13:53 +0000

    Replace boolean fullscreen error with more descriptive strings
    This implements a TODO to expand the error code to be more than just
    "fullscreen error". No functional change, other than to issue better
    error messages.
    
    Bug: 1307772
    Change-Id: I6e00871effe999ab3265319422f64a9bc609ca96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018694
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069765}

--- third_party/blink/renderer/core/fullscreen/fullscreen.h
+++ third_party/blink/renderer/core/fullscreen/fullscreen.h
@@ -105,7 +105,7 @@ class CORE_EXPORT Fullscreen final : public GarbageCollected<Fullscreen>,
                                         FullscreenRequestType,
                                         const FullscreenOptions*,
                                         ScriptPromiseResolver* resolver,
-                                        bool error);
+                                        const char* error);
 
   static void ContinueExitFullscreen(Document*,
                                      ScriptPromiseResolver* resolver,
