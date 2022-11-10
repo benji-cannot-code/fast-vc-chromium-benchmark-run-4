c9c676be01f2ef9355f4374d0e5f04adc079de7d
Kyungjun Lee <kyungjunlee@google.com>
2022-11-10 19:04:43 +0000

    Split proto_convertor.cc and its unittest file using two classes.
    proto_convertor.cc has two independent functions that can be separated
    into two classes. The goal is to simply refactor proto_convertor.cc
    and proto_convertor_unittest.cc, using two classes that handle the two
    independent functions, respectively. This refactoring aims to increase
    readability and does not change any behavior.
    
    Bug: 1373459
    Change-Id: Ica1bd648e1172972f986ef65d0a2b870c69298a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006559
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069869}

---
+++ components/services/screen_ai/proto/visual_annotator_proto_convertor.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SERVICES_SCREEN_AI_PROTO_VISUAL_ANNOTATOR_PROTO_CONVERTOR_H_
+#define COMPONENTS_SERVICES_SCREEN_AI_PROTO_VISUAL_ANNOTATOR_PROTO_CONVERTOR_H_
+
+#include <string>
+
+#include "ui/accessibility/ax_tree_update.h"
+
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
+namespace screen_ai {
+
+// Converts serialized VisualAnnotation proto from VisualAnnotator to
+// AXTreeUpdate. The argument `image_rect` is the bounding box of the image
+// from which the visual annotation was created.
+ui::AXTreeUpdate VisualAnnotationToAXTreeUpdate(
+    const std::string& serialized_proto,
+    const gfx::Rect& image_rect);
+
+// Resets the node id generator to start from 1 again.
+void ResetNodeIDForTesting();
+
+}  // namespace screen_ai
+
+#endif  // COMPONENTS_SERVICES_SCREEN_AI_PROTO_VISUAL_ANNOTATOR_PROTO_CONVERTOR_H_
