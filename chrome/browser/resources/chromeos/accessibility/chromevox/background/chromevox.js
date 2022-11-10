4927259af99e23e6c23412dd276e2667d07a053b
Anastasia Helfinstein <anastasi@google.com>
2022-11-10 19:05:35 +0000

    [ChromeVox] Move position to ChromeVoxState
    This change is a pure refactor.
    
    R=akihiroota
    
    AX-Relnotes: n/a.
    Bug: none
    Test: cq
    Change-Id: I85d0fc9ca103cdd70ccb4ac26a96efa8eb4a0f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018918
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069870}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox.js
@@ -55,7 +55,3 @@ ChromeVox.isStickyPrefOn = localStorage['sticky'] === String(true);
  * @type {?boolean}
  */
 ChromeVox.stickyOverride = null;
-/**
- * @type {Object<string, constants.Point>}
- */
-ChromeVox.position = {};
--- chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox_state.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/chromevox_state.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     ChromeVox state, to avoid direct dependencies on the Background
  *     object and to facilitate mocking for tests.
  */
+import {constants} from '../../common/constants.js';
 import {CursorRange} from '../../common/cursors/range.js';
 import {BrailleKeyEvent} from '../common/braille/braille_key_types.js';
 import {NavBraille} from '../common/braille/nav_braille.js';
@@ -142,6 +143,9 @@ ChromeVoxState.instance;
 
 /** @type {!Array<ChromeVoxStateObserver>} */
 ChromeVoxState.observers = [];
+
+/** @type {!Object<string, constants.Point>} */
+ChromeVoxState.position = {};
 
 /** @protected {function()} */
 ChromeVoxState.resolveReadyPromise_;
