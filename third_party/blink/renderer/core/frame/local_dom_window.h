a0916ebe9940af0adab041eaa441ce176b598e6d
Nate Chapin <japhet@chromium.org>
2022-11-10 01:03:14 +0000

    Introduce HistoryUserActivationState
    This is a rename and generalization of CloseWatcher's existing logic for
    tracking user activation so that it can be shared with the Navigation
    API in a future CL.
    
    Change-Id: I121721732c8893e15f63bd2fb2a772244d7d8acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003812
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069490}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/dom_window.h"
+#include "third_party/blink/renderer/core/frame/history_user_activation_state.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/use_counter_impl.h"
 #include "third_party/blink/renderer/core/html/closewatcher/close_watcher.h"
@@ -491,6 +492,10 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
     return closewatcher_stack_;
   }
 
+  HistoryUserActivationState& history_user_activation_state() {
+    return history_user_activation_state_;
+  }
+
   void IncrementNavigationId() { navigation_id_++; }
   uint32_t GetNavigationId() const { return navigation_id_; }
 
@@ -617,6 +622,8 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
   Member<Fence> fence_;
 
   Member<CloseWatcher::WatcherStack> closewatcher_stack_;
+
+  HistoryUserActivationState history_user_activation_state_;
 
   // If set, this window is a Document Picture in Picture window.
   // https://github.com/steimelchrome/document-pip-explainer/blob/main/explainer.md
