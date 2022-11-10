229344b8f90ef9d3a362beec0afc08a43b59b031
Yilkal <yilkal@chromium.org>
2022-11-10 19:58:24 +0000

    Update Projector to use server based recognition.
    This cl updates projector to use server based
    speech recognition for team-fooding.
    
    This cl introduces a feature flag
    that is used to force enabling projector to use server based
    recognition.
    
    Bug: b/245616124
    Change-Id: Ie32ac0be5b304e25d44c41ad0b7f4c4aa9ec4483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010822
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069924}

--- ash/projector/projector_metadata_controller.cc
+++ ash/projector/projector_metadata_controller.cc
@@ -19,11 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "media/mojo/mojom/speech_recognition.mojom.h"
+#include "third_party/icu/source/common/unicode/locid.h"
 
 namespace ash {
 namespace {
-
-constexpr char kEnglishLanguage[] = "en";
 
 // Writes the given |data| in a file with |path|. Returns true if saving
 // succeeded, or false otherwise.
@@ -48,10 +47,8 @@ ProjectorMetadataController::~ProjectorMetadataController() = default;
 
 void ProjectorMetadataController::OnRecordingStarted() {
   metadata_ = std::make_unique<ProjectorMetadata>();
-
-  // TODO(b/200960615) When multi-language support is available for speech
-  // recognition, get the language from the speech recognition service.
-  metadata_->SetCaptionLanguage(kEnglishLanguage);
+  auto locale = icu::Locale::getDefault();
+  metadata_->SetCaptionLanguage(locale.getLanguage());
 }
 
 void ProjectorMetadataController::RecordTranscription(
