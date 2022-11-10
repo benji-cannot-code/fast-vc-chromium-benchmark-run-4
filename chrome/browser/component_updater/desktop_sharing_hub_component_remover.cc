efe5cc4a4224a0ed3b16b9764d907ee71420bbbe
Matt Reichhoff <mreichhoff@chromium.org>
2022-11-10 20:18:25 +0000

    Remove url_param_filter component updater code
    Bug: 1380927
    Change-Id: I6796d483341ab9c5c8463034310b93b736377f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014245
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069944}

--- chrome/browser/component_updater/desktop_sharing_hub_component_remover.cc
+++ chrome/browser/component_updater/desktop_sharing_hub_component_remover.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_path.h"
 #include "base/task/thread_pool.h"
-#include "chrome/browser/component_updater/component_updater_utils.h"
+#include "components/component_updater/component_updater_utils.h"
 
 namespace component_updater {
 
