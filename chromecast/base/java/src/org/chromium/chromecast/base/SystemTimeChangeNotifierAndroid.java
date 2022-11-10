14b65d0b3a861553214861e1a95cce19ac1d22a0
Ashley Newson <ashleynewson@chromium.org>
2022-11-10 15:21:59 +0000

    Explicitly register receivers in chromecast/ for protected broadcasts
    Replaces uses of Context.registerReceiver with the new
    registerProtectedBroadcastReceiver method. Context.registerReceiver is
    now a banned function (when used directly).
    
    Bug: 1374667
    Change-Id: I372cf34ad22bc81a61a25ed1523919b632d61d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013380
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069768}

--- chromecast/base/java/src/org/chromium/chromecast/base/SystemTimeChangeNotifierAndroid.java
+++ chromecast/base/java/src/org/chromium/chromecast/base/SystemTimeChangeNotifierAndroid.java
@@ -41,7 +41,8 @@ public final class SystemTimeChangeNotifierAndroid {
             }
         };
         IntentFilter filter = new IntentFilter(Intent.ACTION_TIME_CHANGED);
-        ContextUtils.getApplicationContext().registerReceiver(mTimeChangeObserver, filter);
+        ContextUtils.registerProtectedBroadcastReceiver(
+                ContextUtils.getApplicationContext(), mTimeChangeObserver, filter);
     }
 
     @CalledByNative private void finalizeFromNative() {
