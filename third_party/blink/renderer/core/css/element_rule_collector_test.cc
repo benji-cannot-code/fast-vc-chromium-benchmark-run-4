59b50c60b2bc670a0868d96940ffe92252e7ded0
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-11-10 16:07:42 +0000

    [StyleBuilder] Plumb more data out of the element rule collector.
    Specifically:
     - FirstLineDependsOnSizeContainerQueries
     - HasNonUniversalHighlightPseudoStyles
     - HasNonUaHighlightPseudoStyles
    
    And also make AffectedBy* readonly.
    
    There should be no behaviour change.
    
    Bug: 1377295
    Change-Id: I145b8357adc6eb1bffbde4d4af35527237d7dbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016916
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069791}

--- third_party/blink/renderer/core/css/element_rule_collector_test.cc
+++ third_party/blink/renderer/core/css/element_rule_collector_test.cc
@@ -329,7 +329,7 @@ TEST_F(ElementRuleCollectorTest, MatchesNonUniversalHighlights) {
       args.Append("{}");
     args.Append(")");
 
-    return style->HasNonUniversalHighlightPseudoStyles();
+    return result.HasNonUniversalHighlightPseudoStyles();
   };
 
   Element& body = *GetDocument().body();
