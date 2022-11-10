063844359348de8155d7df2666b1d05b40438fd5
Jordan Bayles <jophba@chromium.org>
2022-11-10 20:44:26 +0000

    [Cast Streaming] Disable HW VP8 on ChromeOS
    This patch disables hardware VP8 encoding for cast streaming on all
    ChromeOS devices, regardless of what receiver we are connected to.
    
    This is the next step in fixing major artifacting and other issues
    introduced by hardware encoding being enabled.
    
    Bug: 1382591
    Change-Id: Ic6df7baadc7a65cccc0af168c005dbef241407ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014988
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069966}

--- media/cast/encoding/external_video_encoder.cc
+++ media/cast/encoding/external_video_encoder.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS)
 #include "base/cpu.h"                     // nogncheck
 #include "base/no_destructor.h"           // nogncheck
-#include "third_party/re2/src/re2/re2.h"  // nogncheck
 #endif
 
 #include "base/bind.h"
@@ -27,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
@@ -78,39 +76,16 @@ bool IsHardwareVP8EncodingSupported(
     base::StringPiece receiver_model_name,
     const std::vector<media::VideoEncodeAccelerator::SupportedProfile>&
         profiles) {
-#if BUILDFLAG(IS_CHROMEOS)
-  // NOTE: the hardware encoder on some Chrome OS devices does not play well
-  // with Vizio TVs. See https://crbug.com/1238774 for more information.
-  // Vizio uses the TV model string for the receiver model name.
-  const char* kVizioRegex =
-      R"(^(?i)(([DEMPV]|OLED)\d\d\w*-[A-Z]\w*)|(.*Vizio.*)$)";
-
-  if (RE2::FullMatch(re2::StringPiece(receiver_model_name.data(),
-                                      receiver_model_name.size()),
-                     RE2(kVizioRegex))) {
-    return false;
-  }
-#endif
-
-#if (BUILDFLAG(IS_CHROMEOS) && ARCH_CPU_X86_64) || \
-    (BUILDFLAG(IS_CHROMEOS_ASH) && ARCH_CPU_ARM_FAMILY)
-  // The encoder also doesn't work well with some first party Chromecast
-  // devices. See https://crbug.com/1342276 for more information.
-  if (base::StartsWith(receiver_model_name, "Chromecast")) {
-    return false;
-  }
-  // Sabrina devices sometimes advertise as Eureka Dongle
-  if (base::StartsWith(receiver_model_name, "Eureka Dongle")) {
-    return false;
-  }
-#endif
-
+// The hardware encoder on ChromeOS has major issues when connecting to a
+// variety of first and third party devices. See https://crbug.com/1382591.
+#if !BUILDFLAG(IS_CHROMEOS)
   for (const auto& vea_profile : profiles) {
     if (vea_profile.profile >= media::VP8PROFILE_MIN &&
         vea_profile.profile <= media::VP8PROFILE_MAX) {
       return true;
     }
   }
+#endif
 
   return false;
 }  // namespace
