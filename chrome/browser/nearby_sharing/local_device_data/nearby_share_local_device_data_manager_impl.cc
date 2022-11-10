3e932f60ec815eb5efdf268cf7b92c51536614ac
Michael Hansen <hansenmichael@google.com>
2022-11-10 19:28:43 +0000

    [Nearby] Stable device ID switch for tests
    This allows us to provide a Nearby Share device ID via the command line
    rather than having it randomly generated when the feature is first
    enabled.
    
    This is a further mitigation for the issue where we accumulate
    thousands of certificates through automated E2E testing. Previous fixes
    allowed us to set the certificates to expire sooner, but we were still
    building up many thousands of certificates on the test accounts. This
    is now causing certificate sync to fail because the RPC response is too
    long.
    
    With this fix we will set a static device ID for each physical device
    in the lab. Each test run will overwrite the existing certificates for
    that device on the server. This will prevent the build-up in the first
    place.
    
    Test: Manually tested on Atlas with and without the switch.
    
    Bug: b/246861975
    Change-Id: I46b9b46d08afca7cba429e155f9f118edc7c4385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010725
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069896}

--- chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl.cc
+++ chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/notreached.h"
 #include "base/rand_util.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_prefs.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_profile_info_provider.h"
+#include "chrome/browser/nearby_sharing/common/nearby_share_switches.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_device_data_updater.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_device_data_updater_impl.h"
 #include "chrome/browser/nearby_sharing/scheduling/nearby_share_scheduler.h"
@@ -115,8 +117,13 @@ std::string NearbyShareLocalDeviceDataManagerImpl::GetId() {
   if (!id.empty())
     return id;
 
-  for (size_t i = 0; i < kDeviceIdLength; ++i)
-    id += kAlphaNumericChars[base::RandGenerator(kAlphaNumericChars.size())];
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kNearbyShareDeviceID)) {
+    id = command_line->GetSwitchValueASCII(switches::kNearbyShareDeviceID);
+  } else {
+    for (size_t i = 0; i < kDeviceIdLength; ++i)
+      id += kAlphaNumericChars[base::RandGenerator(kAlphaNumericChars.size())];
+  }
 
   pref_service_->SetString(prefs::kNearbySharingDeviceIdPrefName, id);
 
