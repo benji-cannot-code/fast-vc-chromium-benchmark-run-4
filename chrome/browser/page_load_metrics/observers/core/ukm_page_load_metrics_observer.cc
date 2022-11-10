61969e12f512ef5959d5faacbefc47a4843f9474
Hiroki Kumazaki <kumagi@google.com>
2022-11-10 11:42:19 +0000

    Remove MobileFriendliness::bad_tap_targets_ratio
    We can evaluate tap targets friendliness by https://chromium-review.googlesource.com/c/chromium/src/+/3967993
    Then we don't need whole page evaluation of bad-tap-targets and let's remove entire code around this for code simplicity.
    
    Bug: 1194136
    Change-Id: Id759940c3928855a6cb46abae4c6ce51bcdf5478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015628
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069693}

--- chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.cc
@@ -1329,7 +1329,6 @@ void UkmPageLoadMetricsObserver::RecordMobileFriendlinessMetrics() {
       page_load_metrics::GetBucketedViewportHardcodedWidth(*mf));
   builder.SetTextContentOutsideViewportPercentage(
       mf->text_content_outside_viewport_percentage);
-  builder.SetBadTapTargetsRatio(mf->bad_tap_targets_ratio);
 
   // Make sure at least one MF evaluation happen.
   builder.Record(ukm::UkmRecorder::Get());
