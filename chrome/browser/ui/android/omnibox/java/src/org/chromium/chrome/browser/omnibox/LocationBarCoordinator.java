13ec0f26bbffe3f0f4492550f82185a2c9350a62
Ted Choc <tedchoc@chromium.org>
2022-11-10 19:45:34 +0000

    Include the visible omnibox hint text in the toolbar snapshot suppression logic.
    BUG=1063346
    
    Change-Id: I84d0c47b48350a10f9ac7d1793852b7c69eb413f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018054
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069910}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -462,6 +462,11 @@ public class LocationBarCoordinator implements LocationBar, NativeInitObserver,
         updateButtonVisibility();
     }
 
+    /** @see UrlBarCoordinator#getVisibleTextPrefixHint() */
+    public CharSequence getOmniboxVisibleTextPrefixHint() {
+        return mUrlCoordinator.getVisibleTextPrefixHint();
+    }
+
     /**
      * Returns the {@link LocationBarCoordinatorPhone} for this coordinator.
      *
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarSnapshotState.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarSnapshotState.java
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar.top;
 
 import android.content.res.ColorStateList;
+import android.text.TextUtils;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.IntDef;
+import androidx.annotation.Nullable;
 
 import org.chromium.chrome.browser.toolbar.ButtonData;
 import org.chromium.chrome.browser.toolbar.top.ToolbarPhone.VisualState;
@@ -62,6 +64,8 @@ public class ToolbarSnapshotState {
     private final ButtonData mOptionalButtonData;
     private final @VisualState int mVisualState;
     private String mUrlText = "";
+    @Nullable
+    private CharSequence mVisibleTextPrefixHint;
     private final @DrawableRes int mSecurityIcon;
     private final ColorStateList mColorStateList;
     private final boolean mIsShowingUpdateBadgeDuringLastCapture;
@@ -69,7 +73,8 @@ public class ToolbarSnapshotState {
     private final int mUnfocusedLocationBarLayoutWidth;
 
     public ToolbarSnapshotState(@ColorInt int tint, int tabCount, ButtonData optionalButtonData,
-            @VisualState int visualState, String urlText, @DrawableRes int securityIcon,
+            @VisualState int visualState, String urlText,
+            @Nullable CharSequence visibleTextPrefixHint, @DrawableRes int securityIcon,
             ColorStateList colorStateList, boolean isShowingUpdateBadgeDuringLastCapture,
             boolean isPaintPreview, float progress, int unfocusedLocationBarLayoutWidth) {
         mTint = tint;
@@ -77,6 +82,7 @@ public class ToolbarSnapshotState {
         mOptionalButtonData = optionalButtonData;
         mVisualState = visualState;
         mUrlText = urlText;
+        mVisibleTextPrefixHint = visibleTextPrefixHint;
         mSecurityIcon = securityIcon;
         mColorStateList = colorStateList;
         mIsShowingUpdateBadgeDuringLastCapture = isShowingUpdateBadgeDuringLastCapture;
@@ -112,14 +118,25 @@ public class ToolbarSnapshotState {
             return ToolbarSnapshotDifference.PAINT_PREVIEW;
         } else if (mUnfocusedLocationBarLayoutWidth != that.mUnfocusedLocationBarLayoutWidth) {
             return ToolbarSnapshotDifference.LOCATION_BAR_WIDTH;
-        } else if (!Objects.equals(mUrlText, that.mUrlText)) {
+        } else if (!isVisibleUrlTextSame(that)) {
             return ToolbarSnapshotDifference.URL_TEXT;
         } else if (!Objects.equals(mColorStateList, that.mColorStateList)) {
             return ToolbarSnapshotDifference.HOME_BUTTON_COLOR;
-        } else {
-            return ToolbarSnapshotDifference.NONE;
         }
+        return ToolbarSnapshotDifference.NONE;
     }
+
+    private boolean isVisibleUrlTextSame(ToolbarSnapshotState that) {
+        if (mVisibleTextPrefixHint != null
+                && TextUtils.equals(mVisibleTextPrefixHint, that.mVisibleTextPrefixHint)) {
+            if (TextUtils.indexOf(mUrlText, mVisibleTextPrefixHint) >= 0) return true;
+            assert false : "The visible hint, " + mVisibleTextPrefixHint
+                           + ", should always be part of the URL text, "
+                           + mUrlText;
+        }
+        return TextUtils.equals(mUrlText, that.mUrlText);
+    }
+
     @ColorInt
     int getTint() {
         return mTint;
