ab49bd6a4afd28cc63c10e211998cfcb3bc9f5c4
Olesia Marukhno <olesiamarukhno@google.com>
2022-11-10 13:16:12 +0000

    [privacy sandbox] Create components for new dialogs
    Set up structure for new privacy sandbox dialog. Create new static html
    files and new top level components. The new UI is available via:
    chrome://privacy-sandbox-dialog/combined and chrome://privacy-sandbox-dialog/notice
    
    Bug: 1378703
    Change-Id: Id965683fdb54f8abf6eb178aff137930c0020535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020374
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069715}

---
+++ chrome/browser/resources/privacy_sandbox/privacy_sandbox_notice_dialog_app.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './privacy_sandbox_notice_dialog_app.html.js';
+
+export class PrivacySandboxNoticeDialogAppElement extends PolymerElement {
+  static get is() {
+    return 'privacy-sandbox-notice-dialog-app';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {};
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'privacy-sandbox-notice-dialog-app': PrivacySandboxNoticeDialogAppElement;
+  }
+}
+
+customElements.define(
+    PrivacySandboxNoticeDialogAppElement.is,
+    PrivacySandboxNoticeDialogAppElement);
