fbaf7b11bfa09cff3e745b3bac317467e5b51e9e
Ali Juma <ajuma@chromium.org>
2022-11-10 20:41:20 +0000

    [iOS] Fix UaF in QRScannerViewController EG tests
    This fixes a use-after-free caused by using a pointer to a string
    after it goes out of scope. This bug is in test runner code, so does
    not have any security implications for shipping code.
    
    Change-Id: I7a14582931b5cef8fcf20753ac74230b6aa40f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021228
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069961}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
@@ -170,7 +170,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
       std::make_unique<net::test_server::BasicHttpResponse>();
   http_response->set_code(net::HTTP_OK);
 
-  const char* body_content = nullptr;
+  std::string body_content;
   if (base::StartsWith(request.relative_url, kTestURL,
                        base::CompareCase::SENSITIVE)) {
     body_content = kTestURLResponse;
@@ -183,8 +183,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
     std::string query;
     bool found = net::GetValueForKeyInQuery(url, "q", &query);
     if (found) {
-      std::string content = "Query: " + query;
-      body_content = content.c_str();
+      body_content = "Query: " + query;
     } else {
       body_content = "No query";
     }
@@ -192,10 +191,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
     return nullptr;
   }
 
-  if (body_content) {
-    http_response->set_content(
-        base::StringPrintf("<html><body>%s</body></html>", body_content));
-  }
+  http_response->set_content(
+      base::StringPrintf("<html><body>%s</body></html>", body_content.c_str()));
 
   return std::move(http_response);
 }
