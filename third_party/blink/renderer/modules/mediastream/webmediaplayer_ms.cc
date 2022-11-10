5f52af02ed4da3ce1b1a2837cb0789962e324c45
Markus Handell <handellm@google.com>
2022-11-10 12:00:02 +0000

    Revert "Migrate MediaStreamVideoSource's IO thread to sequence under feature."
    This reverts commit 4dc7e935e0588197fc150a0b5efe8241e6ce213f.
    
    Reason for revert: Breaks tests, see crbug.com/1382592
    
    Original change's description:
    > Migrate MediaStreamVideoSource's IO thread to sequence under feature.
    >
    > Data shows migrating MediaStream's IO thread to sequence helps
    > context switches which should eventually help reduce frequency
    > and thereby power.
    >
    > This change introduces default-off feature
    > UseThreadPoolForMediaStreamIOTaskRunner which if enabled backs the IO
    > task runner from the thread pool.
    >
    > Bug: 1379591
    > Change-Id: I4ffe97677c35bd1373056d71eaf1e42efda56667
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990566
    > Commit-Queue: Markus Handell <handellm@google.com>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1067142}
    
    Bug: 1379591
    Change-Id: I14fdda5f85fef8bbe6c5181c86ec1e13235052dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016619
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Markus Handell <handellm@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1069701}

--- third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
+++ third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
@@ -170,7 +169,7 @@ class WebMediaPlayerMS::FrameDeliverer {
         media_task_runner_(media_task_runner),
         worker_task_runner_(worker_task_runner),
         gpu_factories_(gpu_factories) {
-    DETACH_FROM_SEQUENCE(io_sequence_checker_);
+    DETACH_FROM_THREAD(io_thread_checker_);
 
     CreateGpuMemoryBufferPoolIfNecessary();
   }
@@ -179,12 +178,12 @@ class WebMediaPlayerMS::FrameDeliverer {
   FrameDeliverer& operator=(const FrameDeliverer&) = delete;
 
   ~FrameDeliverer() {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
     FreeGpuMemoryBufferPool();
   }
 
   void OnVideoFrame(scoped_refptr<media::VideoFrame> frame) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 
 // On Android, stop passing frames.
 #if BUILDFLAG(IS_ANDROID)
@@ -241,7 +240,7 @@ class WebMediaPlayerMS::FrameDeliverer {
   }
 
   void SetRenderFrameSuspended(bool render_frame_suspended) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
     render_frame_suspended_ = render_frame_suspended;
     if (render_frame_suspended_) {
       // Drop GpuMemoryBuffer pool to free memory.
@@ -272,7 +271,7 @@ class WebMediaPlayerMS::FrameDeliverer {
   }
 
   void FreeGpuMemoryBufferPool() {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 
     if (gpu_memory_buffer_pool_) {
       DropCurrentPoolTasks();
@@ -283,7 +282,7 @@ class WebMediaPlayerMS::FrameDeliverer {
 
   void EnqueueFrame(media::VideoFrame::ID original_frame_id,
                     scoped_refptr<media::VideoFrame> frame) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 
     {
       bool tracing_enabled = false;
@@ -303,7 +302,7 @@ class WebMediaPlayerMS::FrameDeliverer {
   }
 
   void DropCurrentPoolTasks() {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(io_sequence_checker_);
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
     DCHECK(gpu_memory_buffer_pool_);
 
     if (!weak_factory_for_pool_.HasWeakPtrs())
@@ -334,7 +333,7 @@ class WebMediaPlayerMS::FrameDeliverer {
   media::GpuVideoAcceleratorFactories* const gpu_factories_;
 
   // Used for DCHECKs to ensure method calls are executed on the correct thread.
-  SEQUENCE_CHECKER(io_sequence_checker_);
+  THREAD_CHECKER(io_thread_checker_);
 
   base::WeakPtrFactory<FrameDeliverer> weak_factory_for_pool_{this};
   base::WeakPtrFactory<FrameDeliverer> weak_factory_{this};
