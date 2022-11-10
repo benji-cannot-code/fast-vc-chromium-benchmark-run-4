0201408275e9931268391243b7a70080b9e5a552
David Bokan <bokan@chromium.org>
2022-11-10 21:04:29 +0000

    [Reland] VT: Fix snapshot rect for a resizing virtual keyboard
    Reland note: original CL in initial upload. Fix is to avoid using
    the inset method which was added in a newer API level.
    
    ViewTransitions compute the "snapshot rect" which is a viewport-like
    rect that doesn't change size when UI such as scrollbars, virtual
    keyboard, mobile URL bar are showing or hidden.
    
    When the keyboard is in a resize-content mode (so that innerHeight is
    changed by the keyboard, see:
    https://developer.chrome.com/blog/viewport-resize-behavior/), the
    snapshot rect must account for the keyboard by outsetting the layout
    viewport.
    
    This CL plumbs the virtual-keyboard height into the renderer via
    SynchronizeVisualProperties so that DocumentTransitionStyleTracker can
    use this value to perform the outset. This value includes any keyboard
    accessories such as the autofill bar.
    
    Since the keyboard resize behavior is implemented in the //chrome layer,
    this CL adds end-to-end chrome browser tests for this behavior. This
    requires introducing some hand-rolled pixel testing functionality into
    java tests which isn't common so also requires plumbing the
    InsertVisualStateCallback mechanism into java, allowing tests to wait on
    a Blink main frame to be presented to the screen.
    
    Bug: 1350910
    Change-Id: Ib86da86fbc4f9079fc4eafe26d055cb6d2837a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021408
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: David Bokan <bokan@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069983}

--- content/browser/renderer_host/input/autoscroll_browsertest.cc
+++ content/browser/renderer_host/input/autoscroll_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <tuple>
 
+#include "base/callback_helpers.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -133,7 +134,7 @@ class AutoscrollBrowserTest : public ContentBrowserTest {
     RenderFrameSubmissionObserver observer(
         GetWidgetHost()->render_frame_metadata_provider());
     for (int i = 0; i < num_repeat; i++) {
-      GetWidgetHost()->RequestForceRedraw(i);
+      GetWidgetHost()->ForceRedrawAndWaitForPresentation(base::DoNothing());
       observer.WaitForAnyFrameSubmission();
     }
   }
--- content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/Coordinates.java
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/Coordinates.java
@@ -55,4 +55,8 @@ public class Coordinates {
     public int getContentHeightPixInt() {
         return mRenderCoordinates.getContentHeightPixInt();
     }
+
+    public float getContentOffsetYPix() {
+        return mRenderCoordinates.getContentOffsetYPix();
+    }
 }
