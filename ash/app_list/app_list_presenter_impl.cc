3291d94c94d62cf63cefb3df746216d9580afc37
Mohsen Izadi <mohsen@chromium.org>
2022-11-10 18:58:16 +0000

    Fix app list show/hide input latency metrics
    App list uses presentation callbacks to calculate input latency metrics
    for showing/hiding the app list:
     - Apps.AppListShow.InputLatency;
     - Apps.AppListHide.InputLatency.
    
    To determine the time that change was presented to the user, it uses
    presentation time callbacks registered using
    `ui::Compositor::RequestPresentationTimeForNextFrame()`. These callbacks
    are called even if the presentation is not successful, reporting an
    incorrect value. They should instead use presentation time callbacks
    registered using the newly added function
    `ui::compositor->RequestSuccessfulPresentationTimeForNextFrame()` which
    are called on the first successful presentation.
    
    Bug: 1378612
    Change-Id: I16f39bf1777d091bfe33420bb4a321a18379bfc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997024
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069858}

--- ash/app_list/app_list_presenter_impl.cc
+++ ash/app_list/app_list_presenter_impl.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/types/display_constants.h"
 #include "ui/gfx/geometry/transform.h"
 #include "ui/gfx/geometry/transform_util.h"
-#include "ui/gfx/presentation_feedback.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/transient_window_manager.h"
@@ -74,13 +73,13 @@ inline ui::Layer* GetLayer(views::Widget* widget) {
 // record UMA of input latency.
 void DidPresentCompositorFrame(base::TimeTicks event_time_stamp,
                                bool is_showing,
-                               const gfx::PresentationFeedback& feedback) {
-  const base::TimeTicks present_time = feedback.timestamp;
-  if (present_time.is_null() || event_time_stamp.is_null() ||
-      present_time < event_time_stamp) {
+                               base::TimeTicks presentation_timestamp) {
+  if (presentation_timestamp.is_null() || event_time_stamp.is_null() ||
+      presentation_timestamp < event_time_stamp) {
     return;
   }
-  const base::TimeDelta input_latency = present_time - event_time_stamp;
+  const base::TimeDelta input_latency =
+      presentation_timestamp - event_time_stamp;
   if (is_showing) {
     UMA_HISTOGRAM_TIMES("Apps.AppListShow.InputLatency", input_latency);
   } else {
@@ -703,7 +702,7 @@ void AppListPresenterImpl::RequestPresentationTime(
   ui::Compositor* compositor = root_window->layer()->GetCompositor();
   if (!compositor)
     return;
-  compositor->RequestPresentationTimeForNextFrame(
+  compositor->RequestSuccessfulPresentationTimeForNextFrame(
       base::BindOnce(&DidPresentCompositorFrame, event_time_stamp,
                      is_target_visibility_show_));
 }
