90908aadc2448f9ab1301845548d6c81338abe98
Ana Salazar <anasalazar@chromium.org>
2022-11-10 20:48:41 +0000

    Remove deprecated code from ash/app_list
    This cl gets rid of some of the productivity launcher checks on app list
    derived classes to simplify logic.
    
    Comments within the classes are removed or updated as needed.
    
    Bug: 1372217
    Change-Id: Id9a36b7d12b16678e6ef06a3dd6f342b1fd21c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017414
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069972}

--- ash/app_list/views/app_list_menu_model_adapter.cc
+++ ash/app_list/views/app_list_menu_model_adapter.cc
@@ -89,7 +89,6 @@ void AppListMenuModelAdapter::RecordHistogramOnMenuClosed() {
       }
       break;
     case PRODUCTIVITY_LAUNCHER_RECENT_APP:
-      DCHECK(features::IsProductivityLauncherEnabled());
       if (is_tablet_mode()) {
         UMA_HISTOGRAM_ENUMERATION(
             "Apps.ContextMenuShowSourceV2.ProductivityLauncherRecentApp."
@@ -111,7 +110,6 @@ void AppListMenuModelAdapter::RecordHistogramOnMenuClosed() {
       }
       break;
     case PRODUCTIVITY_LAUNCHER_APP_GRID:
-      DCHECK(features::IsProductivityLauncherEnabled());
       if (is_tablet_mode()) {
         UMA_HISTOGRAM_ENUMERATION(
             "Apps.ContextMenuShowSourceV2.ProductivityLauncherAppGrid."
