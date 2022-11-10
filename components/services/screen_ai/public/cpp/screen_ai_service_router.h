8716fc7b5a01c92af5951bbdbac4f3f15fe0f3d5
Ramin Halavati <rhalavati@chromium.org>
2022-11-10 06:45:05 +0000

    Fix possible UAFs on ScreenAI service.
    ScreenAI service router and implementation are updated to remove the
    possibility of UAF when profiles are closing.
    
    Bug: 1382369, 1382690, 1278249
    Change-Id: Ia3d21464daddb81ec93bcef828b3a4f2a15dc118
    Ax-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016407
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069623}

--- components/services/screen_ai/public/cpp/screen_ai_service_router.h
+++ components/services/screen_ai/public/cpp/screen_ai_service_router.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SERVICES_SCREEN_AI_PUBLIC_CPP_SCREEN_AI_SERVICE_ROUTER_H_
 #define COMPONENTS_SERVICES_SCREEN_AI_PUBLIC_CPP_SCREEN_AI_SERVICE_ROUTER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/services/screen_ai/public/mojom/screen_ai_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -34,6 +35,8 @@ class ScreenAIServiceRouter : public KeyedService {
 
  private:
   mojo::Remote<mojom::ScreenAIService> screen_ai_service_;
+
+  base::WeakPtrFactory<ScreenAIServiceRouter> weak_ptr_factory_{this};
 };
 
 }  // namespace screen_ai
