21ead8cf4640c0ab0620fd94e32dc378ecd35923
Prateek R. Patil <tfpc@google.com>
2022-11-10 14:13:47 +0000

    Changes duration reporting for FilesRequestHandler
    Updates the UMA histogram for duration with individual file upload
    times. Done by using a RequestStart callback in
    BinaryUploadService::Request to write an accurate start time for
    multi-file uploads.
    
    Bug: 1354301
    Change-Id: I9412f074909a7d7258c12ffeb4329e29d65ba1cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016410
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Prateek Patil <tfpc@google.com>
    Reviewed-by: Steffen Seckler <sseckler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069738}

--- chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_CLOUD_CONTENT_SCANNING_FILE_ANALYSIS_REQUEST_H_
 
 #include "base/feature_list.h"
+#include "base/functional/callback_helpers.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/enterprise/connectors/service_provider_config.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
@@ -25,7 +26,9 @@ class FileAnalysisRequest : public BinaryUploadService::Request {
       base::FilePath file_name,
       std::string mime_type,
       bool delay_opening_file,
-      BinaryUploadService::ContentAnalysisCallback callback);
+      BinaryUploadService::ContentAnalysisCallback callback,
+      BinaryUploadService::Request::RequestStartCallback start_callback =
+          base::DoNothing());
   FileAnalysisRequest(const FileAnalysisRequest&) = delete;
   FileAnalysisRequest& operator=(const FileAnalysisRequest&) = delete;
   ~FileAnalysisRequest() override;
