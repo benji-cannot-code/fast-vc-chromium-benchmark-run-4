910c817a9492316e9266dde9bc6d1f231b627ce9
Mustafa Emre Acer <meacer@chromium.org>
2022-11-11 23:53:14 +0000

    Implement non-transitional IDNA processing behind a flag
    This CL adds a flag called UseIDNA2008NonTransitional that enables
    non-transitional IDNA processing for URLs. When enabled, hostnames
    that contain deviation characters will no longer be mapped and will
    be usable as is. These deviation characters are:
    - U+00DF LATIN SMALL LETTER SHARP S
    - U+03C2 GREEK SMALL LETTER FINAL SIGMA
    - U+200D ZERO WIDTH JOINER
    - U+200C ZERO WIDTH NON-JOINER
    
    See https://unicode.org/reports/tr46/#Deviations for details.
    
    As an example, http://faß.de is presently mapped to http://fass.de
    with IDNA 2008 transitional processing. When the flag is enabled,
    ß will no longer be mapped to ss and the URL will remain unchanged.
    
    Bug: 694157
    Change-Id: I37df9aea5520374d1fce829b0d6ee22d9bdf32ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018995
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070571}

---
+++ url/url_idna_icu.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef URL_URL_IDNA_ICU_H_
+#define URL_URL_IDNA_ICU_H_
+
+#include "base/component_export.h"
+
+namespace url {
+
+// Closes the currently used global ICU IDNA instance and resets its pointer.
+COMPONENT_EXPORT(URL) void ResetUIDNAForTesting();
+
+}  // namespace url
+
+#endif
