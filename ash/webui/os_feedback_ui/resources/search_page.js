57cb839b0028fd3bb71bf83b8a00029a5698de9b
YuhanYang <yyhyyh@google.com>
2022-11-11 02:26:08 +0000

    Feedback: Add scrolling effect on resizable window
    - Add a shadow shield at the bottom of the content div when it's scrollable and there's more content below.
    - Add a shadow elevation when the content div is scrolling.
    - Toggle the scrolling visual effect regarding the content div should be scrollable or not due to some change (resizing, or several checkboxes becoming visible).
    
    Screenshot:
    - non scrollable:
    https://screenshot.googleplex.com/4Anu8qfXh6zz8SL
    - scrollable (before scrolling): https://screenshot.googleplex.com/8M9rYnGpbDXkYKs
    - scrolling:
    https://screenshot.googleplex.com/AhuJDEoN3aPBn8R
    - scrolling to the bottom:
    https://screenshot.googleplex.com/9XrXTMegrcd733j
    - resized:
    https://screenshot.googleplex.com/AijALXegevCj7f2
    
    Tested on VM.
    
    Bug: b:185624798
    Change-Id: I8099bb643164a07fc6b5832a3bc0de2df11f58fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020861
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070132}

--- ash/webui/os_feedback_ui/resources/search_page.js
+++ ash/webui/os_feedback_ui/resources/search_page.js
@@ -14,6 +14,7 @@ import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v
 
 import {btRegEx, buildWordMatcher, FeedbackFlowState} from './feedback_flow.js';
 import {FeedbackContext, HelpContentList, HelpContentProviderInterface, SearchRequest, SearchResponse, SearchResult} from './feedback_types.js';
+import {showScrollingEffectOnStart, showScrollingEffects} from './feedback_utils.js';
 import {getHelpContentProvider} from './mojo_interface_provider.js';
 import {domainQuestions, questionnaireBegin} from './questionnaire.js';
 
@@ -138,8 +139,12 @@ export class SearchPageElement extends SearchPageElementBase {
       this.resolveIframeLoaded_ = resolve;
     });
 
-    // Set focus on the input field after iframe is loaded.
-    this.iframeLoaded_.then(() => this.focusInputElement());
+    // Set focus on the input field and decide whether to show scrolling effect
+    // after iframe is loaded.
+    this.iframeLoaded_.then(() => {
+      this.focusInputElement();
+      showScrollingEffectOnStart(this);
+    });
 
     /** @private {?HTMLIFrameElement} */
     this.iframe_ = null;
@@ -471,6 +476,14 @@ export class SearchPageElement extends SearchPageElementBase {
     // the end of the appended text, so we need to move the cursor back to where
     // the user was typing before.
     textarea.selectionEnd = savedCursor;
+  }
+
+  /**
+   * @param {!Event} event
+   * @protected
+   */
+  onContainerScroll_(event) {
+    showScrollingEffects(event, this);
   }
 }
 
