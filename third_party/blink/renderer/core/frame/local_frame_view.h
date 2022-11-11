c3c6f27961b6b980ae1e0635044290d1b6d780a5
Khushal Sagar <khushalsagar@chromium.org>
2022-11-11 18:59:45 +0000

    view-transitions: Support transitions for in process iframes.
    Ensure a ViewTransition can run in Documents hosted in in-process
    iframes. This re-enables a WPT which tests this case.
    
    R=vmpstr@chromium.org
    
    Bug: 1376158
    Change-Id: If600e30a4cb25c746084e8dbd758cfe28ef7227a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021588
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070394}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -1018,6 +1018,8 @@ class CORE_EXPORT LocalFrameView final
 
   DarkModeFilter& EnsureDarkModeFilter();
 
+  bool HasViewTransitionThrottlingRendering() const;
+
   LayoutSize size_;
 
   typedef HeapHashSet<Member<LayoutEmbeddedObject>> EmbeddedObjectSet;
