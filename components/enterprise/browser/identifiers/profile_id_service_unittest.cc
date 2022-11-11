172ed306f296f13a3e513d4266ead4282cedfdf9
hmare <hmare@google.com>
2022-11-11 19:01:46 +0000

    [ProfileID] Add metrics for profile identifier creation
    Created metrics that are recorded if the call to get profile id fails. These metrics are important for making debugging easier.
    
    Bug: b/254637702
    Change-Id: I79158919e1f895a4ad948e774a896d86bba3c4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3977349
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070397}

--- components/enterprise/browser/identifiers/profile_id_service_unittest.cc
+++ components/enterprise/browser/identifiers/profile_id_service_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64url.h"
 #include "base/hash/sha1.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "components/enterprise/browser/identifiers/identifiers_prefs.h"
 #include "components/enterprise/browser/identifiers/mock_profile_id_delegate.h"
 #include "components/prefs/testing_pref_service.h"
@@ -22,6 +23,10 @@ namespace {
 
 constexpr char kFakeProfileGUID[] = "fake-guid";
 constexpr char kFakeDeviceId[] = "fake-id";
+constexpr char kProfildeIdErrorHistogramName[] =
+    "Enterprise.ProfileIdentifier.Error";
+constexpr char kProfildeIdStatusHistogramName[] =
+    "Enterprise.ProfileIdentifier.Status";
 
 }  // namespace
 
@@ -40,11 +45,17 @@ class ProfileIdServiceTest : public testing::Test {
   MockProfileIdDelegate* mock_delegate_ = nullptr;
   TestingPrefServiceSimple profile_prefs_;
   std::unique_ptr<ProfileIdService> service_;
+  base::HistogramTester histogram_tester_;
 };
 
 // Tests that no profile ID is retrieved if the profile GUID is empty.
 TEST_F(ProfileIdServiceTest, GetProfileId_Failure_NoProfileGuid) {
   EXPECT_FALSE(service_->GetProfileId());
+  histogram_tester_.ExpectUniqueSample(
+      kProfildeIdErrorHistogramName,
+      ProfileIdService::Error::kGetProfileGUIDFailure, 1);
+  histogram_tester_.ExpectUniqueSample(kProfildeIdStatusHistogramName, false,
+                                       1);
 }
 
 // Tests that no profile ID is retrieved if the device ID is empty.
@@ -52,6 +63,11 @@ TEST_F(ProfileIdServiceTest, GetProfileId_Failure_NoDeviceId) {
   profile_prefs_.SetString(kProfileGUIDPref, kFakeProfileGUID);
   EXPECT_CALL(*mock_delegate_, GetDeviceId()).WillOnce(Return(""));
   EXPECT_FALSE(service_->GetProfileId());
+  histogram_tester_.ExpectUniqueSample(
+      kProfildeIdErrorHistogramName,
+      ProfileIdService::Error::kGetDeviceIdFailure, 1);
+  histogram_tester_.ExpectUniqueSample(kProfildeIdStatusHistogramName, false,
+                                       1);
 }
 
 // Tests a successful profile ID generation.
@@ -65,6 +81,8 @@ TEST_F(ProfileIdServiceTest, GetProfileId_Success) {
                         base::Base64UrlEncodePolicy::OMIT_PADDING,
                         &encoded_string);
   EXPECT_EQ(encoded_string, service_->GetProfileId());
+  histogram_tester_.ExpectTotalCount(kProfildeIdErrorHistogramName, 0);
+  histogram_tester_.ExpectUniqueSample(kProfildeIdStatusHistogramName, true, 1);
 }
 
 }  // namespace enterprise
