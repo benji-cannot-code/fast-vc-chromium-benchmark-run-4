74877084653808bfa08feaa2292c2579bb98d049
lbowen <lbowen@google.com>
2022-11-11 00:13:26 +0000

    Fixed retailer ID and store ID validation regex.
    Use the rule that for three capital lower ASCII letters followed by a hyphen and four Arabic numerals as retailer and store ID respectively.
    
    Bug: b/258277379
    Change-Id: I1ac0112c898705f664a1aa2cba77f309cbb58a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014246
    Commit-Queue: Bowen Li <lbowen@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070062}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -94,6 +94,8 @@ const test::UIPath kDemoPreferencesCountrySelect = {kDemoPrefsId,
                                                     "countrySelect", "select"};
 const test::UIPath kDemoPreferencesRetailerStoreId = {kDemoPrefsId,
                                                       "retailerIdInput"};
+const test::UIPath kDemoPreferencesRetailerStoreIdInputDisplayMessage = {
+    kDemoPrefsId, "retailer-id-input-display-text"};
 const test::UIPath kDemoPreferencesNext = {kDemoPrefsId, "nextButton"};
 
 const test::UIPath kNetworkScreen = {kNetworkId};
@@ -332,6 +334,38 @@ class DemoSetupArcSupportedTest : public DemoSetupTestBase {
     test::OobeJS().ExpectVisiblePath(kCCAcceptButton);
   }
 
+  void ContinueForValidRetailerAndStoreId(
+      const std::string& expected_retailer_store_id) {
+    test::OobeJS().TypeIntoPath(expected_retailer_store_id,
+                                kDemoPreferencesRetailerStoreId);
+    test::OobeJS().ExpectEnabledPath(kDemoPreferencesNext);
+    test::OobeJS().ExpectElementText(
+        l10n_util::GetStringUTF8(
+            IDS_OOBE_DEMO_SETUP_PREFERENCES_RETAILER_ID_INPUT_HELP_TEXT),
+        kDemoPreferencesRetailerStoreIdInputDisplayMessage);
+    test::OobeJS().ClickOnPath(kDemoPreferencesNext);
+
+    EXPECT_EQ(expected_retailer_store_id, WizardController::default_controller()
+                                              ->demo_setup_controller()
+                                              ->get_retailer_store_id_input());
+  }
+
+  void ContinueForInvalidRetailerAndStoreId(
+      const std::string& expected_retailer_store_id) {
+    test::OobeJS().TypeIntoPath(expected_retailer_store_id,
+                                kDemoPreferencesRetailerStoreId);
+    test::OobeJS().ExpectEnabledPath(kDemoPreferencesNext);
+    test::OobeJS().ExpectElementText(
+        l10n_util::GetStringUTF8(
+            IDS_OOBE_DEMO_SETUP_PREFERENCES_RETAILER_ID_INPUT_ERROR_TEXT),
+        kDemoPreferencesRetailerStoreIdInputDisplayMessage);
+    test::OobeJS().ClickOnPath(kDemoPreferencesNext);
+
+    EXPECT_EQ(expected_retailer_store_id, WizardController::default_controller()
+                                              ->demo_setup_controller()
+                                              ->get_retailer_store_id_input());
+  }
+
   void AcceptArcTos() {
     test::OobeJS().CreateVisibilityWaiter(true, kArcTosNextButton)->Wait();
     test::OobeJS().ClickOnPath(kArcTosNextButton);
@@ -432,7 +466,7 @@ class DemoSetupArcSupportedTest : public DemoSetupTestBase {
               DemoSetupController::GetSubOrganizationEmail());
 
     // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-    // it's shown again when Demo setup completes
+    // it's shown again when Demo setup completes.
     LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
     EXPECT_TRUE(StartupUtils::IsOobeCompleted());
@@ -530,7 +564,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
             DemoSetupController::GetSubOrganizationEmail());
 
   // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-  // it's shown again when Demo setup completes
+  // it's shown again when Demo setup completes.
   LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
@@ -576,16 +610,43 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
 
   UseOnlineModeOnNetworkScreen();
 
-  const std::string expectedRetailerStoreId = "ABC-1234";
+  ContinueForValidRetailerAndStoreId("ABC-1234");
 
-  test::OobeJS().TypeIntoPath(expectedRetailerStoreId,
-                              kDemoPreferencesRetailerStoreId);
-  test::OobeJS().ExpectEnabledPath(kDemoPreferencesNext);
-  test::OobeJS().ClickOnPath(kDemoPreferencesNext);
+  AcceptTermsAndExpectDemoSetupProgress();
 
-  EXPECT_EQ(expectedRetailerStoreId, WizardController::default_controller()
-                                         ->demo_setup_controller()
-                                         ->get_retailer_store_id_input());
+  EXPECT_EQ("admin-us@cros-demo-mode.com",
+            DemoSetupController::GetSubOrganizationEmail());
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes.
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
+
+  EXPECT_EQ("ABC", g_browser_process->local_state()->GetString(
+                       prefs::kDemoModeRetailerId));
+  EXPECT_EQ("1234", g_browser_process->local_state()->GetString(
+                        prefs::kDemoModeStoreId));
+
+  EXPECT_TRUE(StartupUtils::IsOobeCompleted());
+  EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
+}
+
+/**
+ * Test the cases of valid retailer and store ID.
+ */
+using DemoSetupValidRetailerAndStoreIdTest = DemoSetupArcSupportedTest;
+
+IN_PROC_BROWSER_TEST_F(DemoSetupValidRetailerAndStoreIdTest,
+                       OnlineSetupFlowSuccessWithValidNullRetailerAndStoreId) {
+  // Simulate successful online setup.
+  enrollment_helper_.ExpectEnrollmentMode(
+      policy::EnrollmentConfig::MODE_ATTESTATION);
+  enrollment_helper_.ExpectAttestationEnrollmentSuccess();
+  SimulateNetworkConnected();
+
+  TriggerDemoModeOnWelcomeScreen();
+
+  UseOnlineModeOnNetworkScreen();
+
+  ContinueForValidRetailerAndStoreId("");
 
   test::OobeJS().ClickOnPath(kDemoPreferencesNext);
 
@@ -594,13 +655,99 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   EXPECT_EQ("admin-us@cros-demo-mode.com",
             DemoSetupController::GetSubOrganizationEmail());
   // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-  // it's shown again when Demo setup completes
+  // it's shown again when Demo setup completes.
   LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
-  EXPECT_EQ("ABC", g_browser_process->local_state()->GetString(
+  EXPECT_EQ("", g_browser_process->local_state()->GetString(
+                    prefs::kDemoModeRetailerId));
+  EXPECT_EQ(
+      "", g_browser_process->local_state()->GetString(prefs::kDemoModeStoreId));
+
+  EXPECT_TRUE(StartupUtils::IsOobeCompleted());
+  EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
+}
+
+IN_PROC_BROWSER_TEST_F(
+    DemoSetupValidRetailerAndStoreIdTest,
+    OnlineSetupFlowSuccessWithValidNonNullRetailerAndStoreId) {
+  // Simulate successful online setup.
+  enrollment_helper_.ExpectEnrollmentMode(
+      policy::EnrollmentConfig::MODE_ATTESTATION);
+  enrollment_helper_.ExpectAttestationEnrollmentSuccess();
+  SimulateNetworkConnected();
+
+  TriggerDemoModeOnWelcomeScreen();
+
+  UseOnlineModeOnNetworkScreen();
+
+  ContinueForValidRetailerAndStoreId("BBY-1234");
+
+  test::OobeJS().ClickOnPath(kDemoPreferencesNext);
+
+  AcceptTermsAndExpectDemoSetupProgress();
+
+  EXPECT_EQ("admin-us@cros-demo-mode.com",
+            DemoSetupController::GetSubOrganizationEmail());
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes.
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
+
+  EXPECT_EQ("BBY", g_browser_process->local_state()->GetString(
                        prefs::kDemoModeRetailerId));
   EXPECT_EQ("1234", g_browser_process->local_state()->GetString(
                         prefs::kDemoModeStoreId));
+
+  EXPECT_TRUE(StartupUtils::IsOobeCompleted());
+  EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
+}
+
+/**
+ * Test the cases of invalid retailer and store ID.
+ */
+class DemoSetupInvalidRetailerAndStoreIdTest
+    : public DemoSetupArcSupportedTest,
+      public ::testing::WithParamInterface<std::string> {};
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    DemoSetupInvalidRetailerAndStoreIdTest,
+    ::testing::Values(
+        "ABCD-1234",  // Retailer ID letters are more than three.
+        "A-1234",     // Retailer ID letters are less than three.
+        "ABC-123",    // Store ID digits are less than four.
+        "ABC-12345",  // Store ID digits are more than four.
+        "abC-1234",   // Retailer ID contains lower case letters.
+        "ABC_1234",   // There is no hyphen between retailer ID and store ID.
+        "ABC1234"     // There is no hyphen between retailer ID and store ID.
+        ));
+
+IN_PROC_BROWSER_TEST_P(DemoSetupInvalidRetailerAndStoreIdTest,
+                       OnlineSetupFlowSuccessWithInvalidRetailerAndStoreId) {
+  // Simulate successful online setup.
+  enrollment_helper_.ExpectEnrollmentMode(
+      policy::EnrollmentConfig::MODE_ATTESTATION);
+  enrollment_helper_.ExpectAttestationEnrollmentSuccess();
+  SimulateNetworkConnected();
+
+  TriggerDemoModeOnWelcomeScreen();
+
+  UseOnlineModeOnNetworkScreen();
+
+  // Display the valid message if no retailer and store ID is provided.
+  test::OobeJS().ExpectElementText(
+      l10n_util::GetStringUTF8(
+          IDS_OOBE_DEMO_SETUP_PREFERENCES_RETAILER_ID_INPUT_HELP_TEXT),
+      kDemoPreferencesRetailerStoreIdInputDisplayMessage);
+
+  ContinueForInvalidRetailerAndStoreId(GetParam());
+
+  test::OobeJS().ClickOnPath(kDemoPreferencesNext);
+
+  AcceptTermsAndExpectDemoSetupProgress();
+
+  // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
+  // it's shown again when Demo setup completes.
+  LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
@@ -834,7 +981,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest, RetryOnErrorScreen) {
   test::OobeJS().ClickOnPath(kDemoSetupErrorDialogRetry);
 
   // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-  // it's shown again when Demo setup completes
+  // it's shown again when Demo setup completes.
   LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
@@ -984,7 +1131,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupVariantCountryCodeRegionTest,
             DemoSetupController::GetSubOrganizationEmail());
 
   // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-  // it's shown again when Demo setup completes
+  // it's shown again when Demo setup completes.
   LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
@@ -1109,7 +1256,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupBlazeyDeviceTest,
             DemoSetupController::GetSubOrganizationEmail());
 
   // LoginOrLockScreen is shown at beginning of OOBE, so we need to wait until
-  // it's shown again when Demo setup completes
+  // it's shown again when Demo setup completes.
   LoginOrLockScreenVisibleWaiter().WaitEvenIfShown();
 
   EXPECT_TRUE(StartupUtils::IsOobeCompleted());
