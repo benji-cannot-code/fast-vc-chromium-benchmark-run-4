2b566c5fb68876a25999a81b8ed66e1fa6cfa7b7
Martin Rodriguez <rodmartin@google.com>
2022-11-11 10:43:51 +0000

    [Sheriff] Disable PowerBookmarkDatabaseImplTest tests
    Disable:
    - PowerBookmarkDatabaseImplTest.GetPowerOverviewsForTypeDeserializingProtoFails
    - PowerBookmarkDatabaseImplTest.GetPowersForURLDeserializingProtoFails
    
    On Linux due consistent failure.
    
    Bug: 1383289
    Change-Id: Iefd09c2148a89a7ecbd44bf63f4335852009ee07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023424
    Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    Reviewed-by: Bastian Kersting <bkersting@google.com>
    Owners-Override: Martin Rodriguez <rodmartin@google.com>
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070220}

--- components/power_bookmarks/storage/power_bookmark_database_impl_unittest.cc
+++ components/power_bookmarks/storage/power_bookmark_database_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
+#include "build/build_config.h"
 #include "components/power_bookmarks/core/proto/power_bookmark_specifics.pb.h"
 #include "sql/database.h"
 #include "sql/meta_table.h"
@@ -272,7 +273,16 @@ TEST_F(PowerBookmarkDatabaseImplTest, GetPowersForURLUnspecifiedType) {
   EXPECT_EQ(PowerType::POWER_TYPE_MOCK, stored_powers[0]->power_type());
 }
 
-TEST_F(PowerBookmarkDatabaseImplTest, GetPowersForURLDeserializingProtoFails) {
+// // TODO(crbug.com/1383289): Re-enable this test.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_GetPowersForURLDeserializingProtoFails \
+  DISABLED_GetPowersForURLDeserializingProtoFails
+#else
+#define MAYBE_GetPowersForURLDeserializingProtoFails \
+  GetPowersForURLDeserializingProtoFails
+#endif
+TEST_F(PowerBookmarkDatabaseImplTest,
+       MAYBE_GetPowersForURLDeserializingProtoFails) {
   ASSERT_FALSE(base::PathExists(db_file_path()));
 
   // Init DB
@@ -329,9 +339,16 @@ TEST_F(PowerBookmarkDatabaseImplTest, GetPowerOverviewsForType) {
   EXPECT_EQ(GURL("https://boogle.com"), power_overviews[1]->power()->url());
   EXPECT_EQ(1u, power_overviews[1]->count());
 }
-
+// // TODO(crbug.com/1383289): Re-enable this test.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_GetPowerOverviewsForTypeDeserializingProtoFails \
+  DISABLED_GetPowerOverviewsForTypeDeserializingProtoFails
+#else
+#define MAYBE_GetPowerOverviewsForTypeDeserializingProtoFails \
+  GetPowerOverviewsForTypeDeserializingProtoFails
+#endif
 TEST_F(PowerBookmarkDatabaseImplTest,
-       GetPowerOverviewsForTypeDeserializingProtoFails) {
+       MAYBE_GetPowerOverviewsForTypeDeserializingProtoFails) {
   ASSERT_FALSE(base::PathExists(db_file_path()));
 
   // Init DB
