fe3910fa0351f8614bd0a8d513654f0654cf455c
Kramer Ge <fangzhoug@chromium.org>
2022-11-11 20:58:40 +0000

    [Ozone/Wayland]Try gbm_create_device during FindDrmRenderNodePath()
    On Mediatek 8173 SoC, 2 render nodes are exposed for gbm device
    creation: renderD128 by PVR and renderD129 by mediatek. renderD128 can
    be opened but gbm_create_device will fail, resulting in no gbm_device
    for Lacros.
    
    Ash works around it by issuing DRM ioctl and finds that card1 by PVR has
    no crtcs.
    
    Before the driver is fixed, drm render node path should be validated by
    attempting gbm_create_device().
    
    Bug: 1341390
    Change-Id: I795dde4cc38235fa2d991923943e78c70a9cba20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021497
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070480}

--- ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.cc
+++ ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.h"
 
 #include <fcntl.h>
+#include <gbm.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "base/strings/stringprintf.h"
+#include "ui/gfx/linux/scoped_gbm_device.h"
 
 namespace ui {
 
@@ -56,6 +58,12 @@ void DrmRenderNodePathFinder::FindDrmRenderNodePath() {
     std::string dri_render_node(base::StringPrintf(kDriRenderNodeTemplate, i));
     base::ScopedFD drm_fd(open(dri_render_node.c_str(), O_RDWR));
     if (drm_fd.get() < 0)
+      continue;
+
+    // In case the first node /dev/dri/renderD128 can be opened but fails to
+    // create gbm device on certain driver (E.g. PowerVR). Skip such paths.
+    ScopedGbmDevice device(gbm_create_device(drm_fd.get()));
+    if (!device)
       continue;
 
     drm_render_node_path_ = base::FilePath(dri_render_node);
