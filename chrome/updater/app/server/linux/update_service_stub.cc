da8c7c24a1f37852f67a26c165ebdc570433cf55
Noah Rose Ledesma <noahrose@google.com>
2022-11-11 21:30:02 +0000

    Add UpdateServiceInternal RPC for Linux
    Create the new interface using the existing RPC pathways.
    
    An RPC interface was added that will mirror the
    UpdateServiceInternal in the same manner as
    crrev.com/c/3946604.
    
    Fixed: 1381585
    Change-Id: Ia795d167d78573af63e7d4b7ee400eed957794b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003626
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070497}

--- chrome/updater/app/server/linux/update_service_stub.cc
+++ chrome/updater/app/server/linux/update_service_stub.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/app/server/linux/mojom/updater_service.mojom-forward.h"
 #include "chrome/updater/linux/ipc_constants.h"
 #include "chrome/updater/registration_data.h"
+#include "chrome/updater/updater_version.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace updater {
@@ -115,7 +116,8 @@ UpdateServiceStub::UpdateServiceStub(scoped_refptr<updater::UpdateService> impl,
 UpdateServiceStub::~UpdateServiceStub() = default;
 
 void UpdateServiceStub::OnClientDisconnected() {
-  VLOG(1) << "Receiver disconnected: " << server_.current_receiver();
+  VLOG(1) << "UpdateService receiver disconnected: "
+          << server_.current_receiver();
 }
 
 void UpdateServiceStub::GetVersion(GetVersionCallback callback) {
@@ -218,6 +220,36 @@ void UpdateServiceStub::RunInstaller(const std::string& app_id,
   impl_->RunInstaller(app_id, installer_path, install_args, install_data,
                       install_settings, std::move(state_change_callback),
                       std::move(on_complete_callback));
+}
+
+UpdateServiceInternalStub::UpdateServiceInternalStub(
+    scoped_refptr<updater::UpdateServiceInternal> impl,
+    UpdaterScope scope)
+    : server_(
+          GetActiveDutyInternalSocketPath(scope, base::Version(kUpdaterVersion))
+              ->MaybeAsASCII(),
+          this,
+          base::BindRepeating(&IsTrustedIPCEndpoint)),
+      impl_(impl) {
+  server_.set_disconnect_handler(
+      base::BindRepeating(&UpdateServiceInternalStub::OnClientDisconnected,
+                          base::Unretained(this)));
+  server_.StartServer();
+}
+
+UpdateServiceInternalStub::~UpdateServiceInternalStub() = default;
+
+void UpdateServiceInternalStub::OnClientDisconnected() {
+  VLOG(1) << "UpdateServiceInternal receiver disconnected: "
+          << server_.current_receiver();
+}
+
+void UpdateServiceInternalStub::Run(RunCallback callback) {
+  impl_->Run(std::move(callback));
+}
+
+void UpdateServiceInternalStub::Hello(HelloCallback callback) {
+  impl_->Hello(std::move(callback));
 }
 
 }  // namespace updater
