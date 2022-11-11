c0550c89b8f820de77284ae0be8e967e5261aa1c
Scott Haseley <shaseley@chromium.org>
2022-11-11 23:03:03 +0000

    [scheduler] Delete web_rail_mode_observer.h
    This contents of this file were moved in 2019 but the file was left.
    This CL deletes it.
    
    Bug: None
    Change-Id: I6a335490799515de2c448c29ed03227b631ea23e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021645
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070535}

--- content/renderer/render_thread_impl.h
+++ content/renderer/render_thread_impl.h
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/public/mojom/conversions/attribution_reporting.mojom.h"
-#include "third_party/blink/public/platform/scheduler/web_rail_mode_observer.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/platform/url_loader_throttle_provider.h"
 #include "third_party/blink/public/platform/web_connection_type.h"
--- third_party/blink/public/platform/scheduler/web_thread_scheduler.h
+++ third_party/blink/public/platform/scheduler/web_thread_scheduler.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
-#include "third_party/blink/public/platform/scheduler/web_rail_mode_observer.h"
 #include "third_party/blink/public/platform/web_common.h"
 
 namespace blink {
