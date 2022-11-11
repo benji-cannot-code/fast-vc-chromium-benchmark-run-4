f3a1b4e22446da761f13ec44147b821ac6543247
Robbie McElrath <rmcelrath@chromium.org>
2022-11-11 19:41:49 +0000

    iwa: Migrate IsolatedWebAppsDeveloperModeAllowedPolicyTest to new install flow
    Bug: 1333966
    Change-Id: I082e7a9d5af30f3e47e34676189c596e26ddc498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021765
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070434}

--- chrome/browser/policy/test/isolated_apps_developer_mode_allowed_browsertest.cc
+++ chrome/browser/policy/test/isolated_apps_developer_mode_allowed_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/test/isolated_web_app_test_utils.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
@@ -26,15 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-constexpr char kIsolatedWebAppHost[] = "isolated-app.com";
-
 using PolicyVerdictPair = std::tuple<policy::PolicyTest::BooleanPolicy, bool>;
 using BooleanPolicy = policy::PolicyTest::BooleanPolicy;
+
+base::FilePath::StringType kSimpleIsolatedWebAppPath =
+    FILE_PATH_LITERAL("web_apps/simple_isolated_app");
 
 class IsolatedWebAppsDeveloperModeAllowedPolicyTest
     : public web_app::IsolatedWebAppBrowserTestHarness,
@@ -42,15 +43,6 @@ class IsolatedWebAppsDeveloperModeAllowedPolicyTest
  public:
   IsolatedWebAppsDeveloperModeAllowedPolicyTest() {
     scoped_feature_list_.InitAndEnableFeature(features::kIsolatedWebApps);
-  }
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    IsolatedWebAppBrowserTestHarness::SetUpCommandLine(command_line);
-
-    const std::string isolated_app_origins =
-        std::string("https://") + kIsolatedWebAppHost;
-    command_line->AppendSwitchASCII(switches::kIsolatedAppOrigins,
-                                    isolated_app_origins);
   }
 
   void SetUpInProcessBrowserTestFixture() override {
@@ -84,33 +76,33 @@ class IsolatedWebAppsDeveloperModeAllowedPolicyTest
 };
 
 IN_PROC_BROWSER_TEST_P(IsolatedWebAppsDeveloperModeAllowedPolicyTest, MockTcp) {
-  auto app_id = InstallIsolatedWebApp(kIsolatedWebAppHost);
+  std::unique_ptr<net::EmbeddedTestServer> isolated_web_app_dev_server =
+      CreateAndStartServer(kSimpleIsolatedWebAppPath);
+  web_app::IsolatedWebAppUrlInfo url_info = InstallDevModeProxyIsolatedWebApp(
+      isolated_web_app_dev_server->GetOrigin());
+  content::RenderFrameHost* app_frame = OpenApp(url_info.app_id());
 
-  content::RenderFrameHost* app_frame = OpenApp(app_id);
-  content::WebContents* app_contents =
-      content::WebContents::FromRenderFrameHost(app_frame);
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      chrome::FindBrowserWithWebContents(app_contents),
-      https_server()->GetURL(kIsolatedWebAppHost,
-                             "/policy/direct_sockets.html")));
+  content::RenderFrameHost* navigated_frame = ui_test_utils::NavigateToURL(
+      GetBrowserFromFrame(app_frame),
+      url_info.origin().GetURL().Resolve("/direct_sockets.html"));
 
   const bool enabled = std::get<1>(GetParam());
-  ASSERT_EQ(enabled, EvalJs(app_contents->GetPrimaryMainFrame(), "mockTcp()"));
+  ASSERT_EQ(enabled, EvalJs(navigated_frame, "mockTcp()"));
 }
 
 IN_PROC_BROWSER_TEST_P(IsolatedWebAppsDeveloperModeAllowedPolicyTest, MockUdp) {
-  auto app_id = InstallIsolatedWebApp(kIsolatedWebAppHost);
+  std::unique_ptr<net::EmbeddedTestServer> isolated_web_app_dev_server =
+      CreateAndStartServer(kSimpleIsolatedWebAppPath);
+  web_app::IsolatedWebAppUrlInfo url_info = InstallDevModeProxyIsolatedWebApp(
+      isolated_web_app_dev_server->GetOrigin());
+  content::RenderFrameHost* app_frame = OpenApp(url_info.app_id());
 
-  content::RenderFrameHost* app_frame = OpenApp(app_id);
-  content::WebContents* app_contents =
-      content::WebContents::FromRenderFrameHost(app_frame);
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      chrome::FindBrowserWithWebContents(app_contents),
-      https_server()->GetURL(kIsolatedWebAppHost,
-                             "/policy/direct_sockets.html")));
+  content::RenderFrameHost* navigated_frame = ui_test_utils::NavigateToURL(
+      GetBrowserFromFrame(app_frame),
+      url_info.origin().GetURL().Resolve("/direct_sockets.html"));
 
   const bool enabled = std::get<1>(GetParam());
-  ASSERT_EQ(enabled, EvalJs(app_contents->GetPrimaryMainFrame(), "mockUdp()"));
+  ASSERT_EQ(enabled, EvalJs(navigated_frame, "mockUdp()"));
 }
 
 INSTANTIATE_TEST_SUITE_P(
