46479b748448036752c2c54d8a2ad84eab607fc2
Matthew Denton <mpdenton@chromium.org>
2022-11-11 23:51:33 +0000

    Don't DiscourageGetaddrinfo() when using OutOfProcessSystemDnsResolution
    When the OutOfProcessSystemDnsResolution feature is enabled, we
    base::DumpWithoutCrashing() whenever getaddrinfo() is called as this
    should not run in the network service when it is sandboxed.
    
    Unfortunately third-party kerberos libraries can use getaddrinfo() in
    the network service (kerberos will eventually be run in the browser
    process when the network service is sandboxed). This causes a
    DumpWithoutCrashing() for all users of kerberos. So, remove the
    call to DiscourageGetaddrinfo(). It can be added back with the sandbox
    when it is launched.
    
    Bug: 1382933
    Change-Id: I34c591b28becaea38eba5c71ea2e4023248b116b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023014
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070569}

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -95,10 +95,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/os_crypt/key_storage_config_linux.h"
 #endif
 
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-#include "sandbox/linux/services/libc_interceptor.h"
-#endif
-
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/application_status_listener.h"
 #include "net/android/http_auth_negotiate_android.h"
@@ -513,14 +509,6 @@ void NetworkService::SetSystemDnsResolver(
   CHECK(
       base::FeatureList::IsEnabled(features::kOutOfProcessSystemDnsResolution));
   CHECK(override_remote);
-
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-  // The in-process system DNS resolver is being overridden with an
-  // out-of-process Mojo implementation. So, generate a crash dump if this
-  // process ever calls getaddrinfo().
-  // TODO(crbug.com/1312224): remove this once network service is sandboxed.
-  sandbox::DiscourageGetaddrinfo();
-#endif
 
   // Using a Remote (as opposed to a SharedRemote) is fine as system host
   // resolver overrides should only be invoked on the main thread.
