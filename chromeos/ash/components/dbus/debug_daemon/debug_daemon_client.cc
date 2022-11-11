bef64f146997e74270142c8abff767bea02d54e0
xiangdong kong <xiangdongkong@google.com>
2022-11-11 22:28:04 +0000

    feedback: track the results of GetFeedbackLogsV2 dbus call
    Switched to use the CallMethodWithErrorResponse to receive the dbus
    error code. This change will give us more insight why sometimes the dbus
    call failed.
    
    Bug: b:238782324
    Change-Id: Iff3d04b09206d8da46a2b8f66394e5a5319c18a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4001003
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070522}

--- chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.cc
+++ chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_config.h"
 #include "chromeos/ash/components/dbus/cryptohome/rpc.pb.h"
 #include "chromeos/ash/components/dbus/debug_daemon/fake_debug_daemon_client.h"
+#include "chromeos/ash/components/dbus/debug_daemon/metrics.h"
 #include "chromeos/dbus/common/pipe_reader.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
@@ -83,6 +84,8 @@ class PipeReaderWrapper : public base::SupportsWeakPtr<PipeReaderWrapper> {
   void OnIOComplete(absl::optional<std::string> result) {
     if (!result.has_value()) {
       VLOG(1) << "Failed to read data.";
+      RecordGetFeedbackLogsV2DbusResult(
+          GetFeedbackLogsV2DbusResult::kErrorReadingData);
       RunCallbackAndDestroy(absl::nullopt);
       return;
     }
@@ -92,6 +95,8 @@ class PipeReaderWrapper : public base::SupportsWeakPtr<PipeReaderWrapper> {
         base::DictionaryValue::From(json_reader.Deserialize(nullptr, nullptr));
     if (!logs.get()) {
       VLOG(1) << "Failed to deserialize the JSON logs.";
+      RecordGetFeedbackLogsV2DbusResult(
+          GetFeedbackLogsV2DbusResult::kErrorDeserializingJSonLogs);
       RunCallbackAndDestroy(absl::nullopt);
       return;
     }
@@ -281,7 +286,7 @@ class DebugDaemonClientImpl : public DebugDaemonClient {
     writer.CloseContainer(&sub_writer);
 
     DVLOG(1) << "Requesting feedback logs";
-    debugdaemon_proxy_->CallMethod(
+    debugdaemon_proxy_->CallMethodWithErrorResponse(
         &method_call, kBigLogsDBusTimeoutMS,
         base::BindOnce(&DebugDaemonClientImpl::OnFeedbackLogsResponse,
                        weak_ptr_factory_.GetWeakPtr(),
@@ -827,7 +832,9 @@ class DebugDaemonClientImpl : public DebugDaemonClient {
   }
 
   void OnFeedbackLogsResponse(base::WeakPtr<PipeReaderWrapper> pipe_reader,
-                              dbus::Response* response) {
+                              dbus::Response* response,
+                              dbus::ErrorResponse* err_response) {
+    RecordGetFeedbackLogsV2DbusError(err_response);
     if (!response && pipe_reader.get()) {
       // We need to terminate the data stream if an error occurred while the
       // pipe reader is still waiting on read.
