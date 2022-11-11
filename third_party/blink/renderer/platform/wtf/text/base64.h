ba4f960f16ae310a7c359df106eb6c5b1f1ec78d
Charlie Harrison <csharrison@chromium.org>
2022-11-11 19:31:35 +0000

    Remove WTF::Base64EncodePolicy
    Looks like this is dead code.
    
    Change-Id: Ice50ce289b7aa51c309631c9845d6c9a490f70c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023845
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070422}

--- third_party/blink/renderer/platform/wtf/text/base64.h
+++ third_party/blink/renderer/platform/wtf/text/base64.h
@@ -34,16 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-enum Base64EncodePolicy { kBase64DoNotInsertLFs, kBase64InsertLFs };
-
 enum Base64DecodePolicy { kBase64DoNotValidatePadding, kBase64ValidatePadding };
 
-WTF_EXPORT void Base64Encode(base::span<const uint8_t>,
-                             Vector<char>&,
-                             Base64EncodePolicy = kBase64DoNotInsertLFs);
-[[nodiscard]] WTF_EXPORT String
-    Base64Encode(base::span<const uint8_t>,
-                 Base64EncodePolicy = kBase64DoNotInsertLFs);
+WTF_EXPORT void Base64Encode(base::span<const uint8_t>, Vector<char>&);
+[[nodiscard]] WTF_EXPORT String Base64Encode(base::span<const uint8_t>);
 
 WTF_EXPORT bool Base64Decode(
     const String&,
@@ -77,15 +71,10 @@ WTF_EXPORT bool Base64UnpaddedURLDecode(
 // encoding in plain base64.
 WTF_EXPORT String NormalizeToBase64(const String&);
 
-WTF_EXPORT String Base64URLEncode(const char*,
-                                  unsigned,
-                                  Base64EncodePolicy = kBase64DoNotInsertLFs);
+WTF_EXPORT String Base64URLEncode(const char*, unsigned);
 
 }  // namespace WTF
 
-using WTF::Base64EncodePolicy;
-using WTF::kBase64DoNotInsertLFs;
-using WTF::kBase64InsertLFs;
 using WTF::Base64DecodePolicy;
 using WTF::kBase64DoNotValidatePadding;
 using WTF::kBase64ValidatePadding;
