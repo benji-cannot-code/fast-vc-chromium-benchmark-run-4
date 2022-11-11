8a9477589db2c871ae4845ccd2144d9ef13c4b0a
S. Ganesh <ganesh@chromium.org>
2022-11-11 03:46:54 +0000

    Updater: OS compatibility support for offline app installs
    For online app installs, the update server does compat checks to make
    sure the app being installed matches with the OS that the install is
    being attempted on.
    
    This CL adds equivalent support for offline installs, where no update
    server is involved.
    
    The `platform`, `arch`, and `min_os_version` attributes in the offline
    update response are used to determine compatibility of the app being
    installed with the host OS.
    
    Bug: 1382653
    Change-Id: If1fc76f9e4197b8384c3f98a455988e10babf699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021533
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070156}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -66,7 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/win/ui/ui_util.h"
 #pragma clang diagnostic pop
 
-#include "components/update_client/utils.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace updater {
@@ -557,7 +556,7 @@ class AppInstallControllerImpl : public AppInstallController,
   void DoInstallAppOffline(const base::FilePath& installer_path,
                            const std::string& install_args,
                            const std::string& install_data);
-  void HandleArchitectureNotCompatible();
+  void HandleOsNotSupported();
   void InstallComplete(UpdateService::Result result);
 
   [[nodiscard]] static ObserverCompletionInfo HandleInstallResult(
@@ -722,10 +721,8 @@ void AppInstallControllerImpl::InstallAppOffline(
                            base::FilePath /*installer_path*/,
                            std::string /*arguments*/,
                            std::string /*install_data*/>& result) {
-                      if (!IsArchitectureCompatible(
-                              std::get<0>(result).system_requirements.arch,
-                              update_client::GetArchitecture())) {
-                        self->HandleArchitectureNotCompatible();
+                      if (!IsOsSupported(std::get<0>(result))) {
+                        self->HandleOsNotSupported();
                         return;
                       }
 
@@ -813,7 +810,7 @@ void AppInstallControllerImpl::DoInstallAppOffline(
               install_data, install_settings)));
 }
 
-void AppInstallControllerImpl::HandleArchitectureNotCompatible() {
+void AppInstallControllerImpl::HandleOsNotSupported() {
   UpdateService::UpdateState update_state;
   update_state.app_id = app_id_;
   update_state.state = UpdateService::UpdateState::State::kUpdateError;
