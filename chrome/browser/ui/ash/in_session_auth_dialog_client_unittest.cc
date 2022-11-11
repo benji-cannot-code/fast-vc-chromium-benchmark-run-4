999bfbaf12fcbd8025825663a8a098b292208664
Denis Kuznetsov <antrim@chromium.org>
2022-11-11 18:29:26 +0000

    Enable AuthSessions for WebAuthN flow
    Bug: b/241256423
    Change-Id: I33539ab6ea978d8932397b7ab2e48418954ee7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020345
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070377}

--- chrome/browser/ui/ash/in_session_auth_dialog_client_unittest.cc
+++ chrome/browser/ui/ash/in_session_auth_dialog_client_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/in_session_auth_dialog_client.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/in_session_auth_dialog_client.h"
 #include "ash/public/cpp/webauthn_dialog_controller.h"
 #include "base/callback.h"
@@ -13,8 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chromeos/ash/components/cryptohome/system_salt_getter.h"
+#include "chromeos/ash/components/dbus/userdataauth/fake_cryptohome_misc_client.h"
+#include "chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h"
 #include "chromeos/ash/components/dbus/userdataauth/userdataauth_client.h"
 #include "chromeos/ash/components/login/auth/fake_extended_authenticator.h"
+#include "chromeos/ash/components/login/auth/public/cryptohome_key_constants.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/browser_task_environment.h"
@@ -61,9 +66,18 @@ class InSessionAuthDialogClientTest : public testing::Test {
  public:
   InSessionAuthDialogClientTest() {
     ash::UserDataAuthClient::InitializeFake();
+    ash::FakeUserDataAuthClient::TestApi::Get()->set_enable_auth_check(true);
+    ash::CryptohomeMiscClient::InitializeFake();
+    ash::SystemSaltGetter::Initialize();
+
     client_ = std::make_unique<InSessionAuthDialogClient>();
   }
-  ~InSessionAuthDialogClientTest() override = default;
+
+  ~InSessionAuthDialogClientTest() override {
+    ash::SystemSaltGetter::Shutdown();
+    ash::CryptohomeMiscClient::Shutdown();
+    ash::UserDataAuthClient::Shutdown();
+  }
 
   void SetupActiveUser() {
     fake_user_manager_->AddUser(kAccountId);
@@ -92,7 +106,33 @@ class InSessionAuthDialogClientTest : public testing::Test {
     return fake_authenticator_->last_unlock_webauthn_secret();
   }
 
- private:
+  void ConfigureExistingUserWithPassword(const AccountId& user,
+                                         const std::string& password) {
+    Key key(Key::KEY_TYPE_PASSWORD_PLAIN, std::string(), password);
+    key.Transform(Key::KEY_TYPE_SALTED_SHA256_TOP_HALF,
+                  ash::SystemSaltGetter::ConvertRawSaltToHexString(
+                      ash::FakeCryptohomeMiscClient::GetStubSystemSalt()));
+
+    cryptohome::Key cryptohome_key;
+    cryptohome_key.mutable_data()->set_label(ash::kCryptohomeGaiaKeyLabel);
+    cryptohome_key.set_secret(key.GetSecret());
+
+    auto* test_api = ash::FakeUserDataAuthClient::TestApi::Get();
+    auto account_id = cryptohome::CreateAccountIdentifierFromAccountId(user);
+    test_api->AddExistingUser(account_id);
+    test_api->AddKey(account_id, cryptohome_key);
+  }
+
+  void StartAuthSessionForActiveUser() {
+    base::RunLoop run_loop;
+    bool result = true;
+    client_->StartAuthSession(base::BindLambdaForTesting(
+        [&result](bool success) { result = success; }));
+    run_loop.RunUntilIdle();
+    EXPECT_TRUE(result);
+  }
+
+ protected:
   // The ExtendedAuthenticator::AuthenticateToCheck task is posted to main (UI)
   // thread.
   const content::BrowserTaskEnvironment task_environment_;
@@ -112,10 +152,14 @@ TEST_F(InSessionAuthDialogClientTest, WrongPassword) {
   const user_manager::User* const user =
       user_manager::UserManager::Get()->GetActiveUser();
   UserContext expected_user_context(*user);
-  expected_user_context.SetKey(
-      Key(Key::KEY_TYPE_PASSWORD_PLAIN, std::string(), kPassword));
+  Key key(Key::KEY_TYPE_PASSWORD_PLAIN, std::string(), kPassword);
+  expected_user_context.SetKey(key);
 
   SetExpectedContext(expected_user_context);
+  if (ash::features::IsUseAuthsessionForWebAuthNEnabled()) {
+    ConfigureExistingUserWithPassword(user->GetAccountId(), kPassword);
+    StartAuthSessionForActiveUser();
+  };
 
   base::RunLoop run_loop;
 
@@ -140,6 +184,11 @@ TEST_F(InSessionAuthDialogClientTest, PasswordAuthSuccess) {
 
   SetExpectedContext(expected_user_context);
 
+  if (ash::features::IsUseAuthsessionForWebAuthNEnabled()) {
+    ConfigureExistingUserWithPassword(user->GetAccountId(), kPassword);
+    StartAuthSessionForActiveUser();
+  };
+
   base::RunLoop run_loop;
 
   bool result = false;
@@ -151,7 +200,9 @@ TEST_F(InSessionAuthDialogClientTest, PasswordAuthSuccess) {
 
   run_loop.RunUntilIdle();
   EXPECT_TRUE(result);
-  EXPECT_TRUE(GetLastUnlockWebAuthnSecret());
+  if (!ash::features::IsUseAuthsessionForWebAuthNEnabled()) {
+    EXPECT_TRUE(GetLastUnlockWebAuthnSecret());
+  }
 }
 
 }  // namespace
