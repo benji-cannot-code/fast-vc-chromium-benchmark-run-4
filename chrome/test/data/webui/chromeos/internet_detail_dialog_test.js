055109e21447225c8cacc0ba029553e6cdd38b36
Gordon Seto <gordonseto@google.com>
2022-11-11 20:14:58 +0000

    [CrOS Cellular] Hide link in <apn-list> when in <internet-detail-dialog>
    Hide "Learn more" link in <apn-list> when displayed in
    <internet-detail-dialog. This is because <internet-detail-dialog> is
    shown in the sign-in screen and in OOBE, two places where links can't
    navigate to the browser.
    
    Screenshot:
    https://screenshot.googleplex.com/3hvu72SXgkbHpVW.png
    
    Bug: b:162365553
    Test: *InternetDetailDialog*, *ApnList*, DUT with flag on/off
    Change-Id: Ie108f2460d666f1d48813a6a3f217ce854e01a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4010924
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070451}

--- chrome/test/data/webui/chromeos/internet_detail_dialog_test.js
+++ chrome/test/data/webui/chromeos/internet_detail_dialog_test.js
@@ -335,7 +335,10 @@ suite('internet-detail-dialog', () => {
             internetDetailDialog.shadowRoot.querySelector('#apnRowSublabel')
                 .textContent.trim();
         assertFalse(!!getApnSectionSublabel());
-        assertTrue(!!internetDetailDialog.shadowRoot.querySelector('apn-list'));
+        const getApnList = () =>
+            internetDetailDialog.shadowRoot.querySelector('apn-list');
+        assertTrue(!!getApnList());
+        assertTrue(getApnList().shouldOmitLinks);
         const isApnListShowing = () =>
             internetDetailDialog.shadowRoot.querySelector('iron-collapse')
                 .opened;
