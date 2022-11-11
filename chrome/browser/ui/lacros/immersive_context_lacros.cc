44033dd24a78e289d113521d592d4d7974867862
Pengchao Cai <pengchaocai@chromium.org>
2022-11-11 19:33:46 +0000

    [Lacros] Fix top-chrome visibility issue in full-screen mode
    Bug: 1367263
    Change-Id: I586b36978b2c61666a62cd48382378310fe30e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006965
    Commit-Queue: pengchao Cai <pengchaocai@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070426}

--- chrome/browser/ui/lacros/immersive_context_lacros.cc
+++ chrome/browser/ui/lacros/immersive_context_lacros.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/platform_window/extensions/wayland_extension.h"
+#include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host_lacros.h"
 #include "ui/views/widget/widget.h"
 
@@ -40,6 +41,5 @@ gfx::Rect ImmersiveContextLacros::GetDisplayBoundsInScreen(
 }
 
 bool ImmersiveContextLacros::DoesAnyWindowHaveCapture() {
-  NOTIMPLEMENTED_LOG_ONCE();
-  return false;
+  return views::MenuController::GetActiveInstance() != nullptr;
 }
