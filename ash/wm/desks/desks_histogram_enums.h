1118918ecb2bfcd739a7d6dde3f19862bbaa290b
Joe Suzuki <joesuzuki@google.com>
2022-11-11 14:17:47 +0000

    Add Floating Workspace Desk Template Type
    The Floating Workspace V2 prototype needs to distinguish between different Desk Templates in storage. Adding a new Desk Template type specifically for Floating Workspace will help during search.
    
    Bug: b/258228780
    Change-Id: Ife84d36aaad5d87dba01fa3a35f3a46b7806ce07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014379
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Joe Suzuki <joesuzuki@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070280}

--- ash/wm/desks/desks_histogram_enums.h
+++ ash/wm/desks/desks_histogram_enums.h
@@ -20,7 +20,8 @@ enum class DesksCreationRemovalSource {
   kSaveAndRecall = 5,
   kApi = 6,
   kEnsureDefaultDesk = 7,
-  kMaxValue = kEnsureDefaultDesk,
+  kFloatingWorkspace = 8,
+  kMaxValue = kFloatingWorkspace,
 };
 
 // These values are logged to UMA. Entries should not be renumbered and
