3b11d40ac893783c21e23de120f6b19c39c63ff5
Shu-yu Guo <syg@chromium.org>
2022-11-11 09:27:56 +0000

    [v8] Add base::feature for change-Array-by-copy
    I2S with 3 LGTMs:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/ptKolvGLGWY/m/_18d7JS9AQAJ
    
    Bug: v8:12764
    Change-Id: I095d557e477c9484d73a1f73c495df10c0dd418c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021848
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070203}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -158,4 +158,9 @@ BASE_FEATURE(kJavaScriptSymbolAsWeakMapKey,
              "JavaScriptSymbolAsWeakMapKey",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables the change-Array-by-copy proposal.
+BASE_FEATURE(kJavaScriptChangeArrayByCopy,
+             "JavaScriptChangeArrayByCopy",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace features
