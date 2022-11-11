f8501d2b6db53c5c6be957424afb50f7ba09c0a0
Solomon Kinard <solomonkinard@chromium.org>
2022-11-11 21:32:04 +0000

    Extensions: FileHandlers: Add accept property to the manifest key
    This change is absolutely essential to have in place before continuing
    to connect native OS file handling with browser extensions. The `accept`
    key is how the OS knows how to route files in the first place.
    
    * Added tests to verify existing behavior.
    * Added tests to verify `accept` sub-key behavior.
    * Allow `object` in .idl without errors
    
    Bug: 1313786
    
    Change-Id: If4557b23ad4f9795441f75ed9fe16822a33fcd79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998032
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070498}

--- extensions/common/features/complex_feature.cc
+++ extensions/common/features/complex_feature.cc
@@ -30,8 +30,7 @@ ComplexFeature::ComplexFeature(std::vector<Feature*>* features) {
 #endif
 }
 
-ComplexFeature::~ComplexFeature() {
-}
+ComplexFeature::~ComplexFeature() = default;
 
 Feature::Availability ComplexFeature::IsAvailableToManifest(
     const HashedExtensionId& hashed_id,
