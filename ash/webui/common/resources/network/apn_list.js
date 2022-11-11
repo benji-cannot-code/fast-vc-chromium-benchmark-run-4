f52e9ce8f660c1f1fa67b40f195ffdfc2f304cc0
Antoni Dikov <adikov@google.com>
2022-11-11 10:30:26 +0000

    [CrOS Cellular] Add APN list item three dot menu
    * Implement apn-list and apn-list-item with dummy values.
    * Add three dot menu to each apn-list-item.
    * Create a separate test file for apn-list and apn-list-item.
    
    https://screenshot.googleplex.com/6yjwKUawoHDkiAi.png
    
    Test: autoninja -C out/Default/ browser_tests && testing/xvfb.py ./out/Default/browser_tests --gtest_filter=*ApnList*
    Bug: b:162365553
    Change-Id: If4f309ef5caf89c4923875156240031b2483d786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003174
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Antoni Dikov <adikov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070214}

--- ash/webui/common/resources/network/apn_list.js
+++ ash/webui/common/resources/network/apn_list.js
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_components/localized_link/localized_link.js';
 import './network_shared.css.js';
-import '//resources/polymer/v3_0/iron-list/iron-list.js';
+import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
+import 'chrome://resources/ash/common/network/apn_list_item.js';
 
 import {I18nBehavior} from '//resources/ash/common/i18n_behavior.js';
 import {Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -22,6 +23,14 @@ Polymer({
   is: 'apn-list',
 
   behaviors: [I18nBehavior],
+
+  properties: {
+    // TODO(b/162365553): Populate with real data and remove dummy values.
+    apns: {
+      type: Array,
+      value: [{name: 'apn1'}, {name: 'apn2'}, {name: 'apn3'}],
+    },
+  },
 
   /**
    * Redirects to "Lean more about APN" page.
