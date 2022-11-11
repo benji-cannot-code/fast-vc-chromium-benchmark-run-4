3269b5191b82de7afcb1cb133440d513d6ddbe9f
Mohamed Amir Yosef <mamir@chromium.org>
2022-11-11 11:46:51 +0000

    [Passwords] Introduce Skeleton of the revamped manage passwords bubble
    This is sharing the same controller as ItemsBubbleController.
    
    Bug: 1382017
    Change-Id: Icd4039ad4f37693bc90155c037856c08b116c80f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020093
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070233}

--- chrome/browser/ui/views/passwords/password_bubble_view_base.cc
+++ chrome/browser/ui/views/passwords/password_bubble_view_base.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/passwords/manage_passwords_icon_views.h"
+#include "chrome/browser/ui/views/passwords/manage_passwords_view.h"
 #include "chrome/browser/ui/views/passwords/move_to_account_store_bubble_view.h"
 #include "chrome/browser/ui/views/passwords/password_auto_sign_in_view.h"
 #include "chrome/browser/ui/views/passwords/password_generation_confirmation_view.h"
@@ -83,7 +84,12 @@ PasswordBubbleViewBase* PasswordBubbleViewBase::CreateBubble(
   password_manager::ui::State model_state =
       PasswordsModelDelegateFromWebContents(web_contents)->GetState();
   if (model_state == password_manager::ui::MANAGE_STATE) {
-    view = new PasswordItemsView(web_contents, anchor_view);
+    if (base::FeatureList::IsEnabled(
+            password_manager::features::kRevampedPasswordManagementBubble)) {
+      view = new ManagePasswordsView(web_contents, anchor_view);
+    } else {
+      view = new PasswordItemsView(web_contents, anchor_view);
+    }
   } else if (model_state == password_manager::ui::AUTO_SIGNIN_STATE) {
     view = new PasswordAutoSignInView(web_contents, anchor_view);
   } else if (model_state == password_manager::ui::CONFIRMATION_STATE) {
