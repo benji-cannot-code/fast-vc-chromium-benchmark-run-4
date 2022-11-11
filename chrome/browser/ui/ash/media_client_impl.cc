7879bb56d2abefcf26b2a7f113b82a2b9171f789
Md Shahadat Hossain Shahin <shahinmd@google.com>
2022-11-11 08:50:38 +0000

    Change the icon of the camera notifications
    Bug: b/253230652
    Change-Id: I0e31ef2b725ccc42c3d172551e2e5e47ac660b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020376
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070195}

--- chrome/browser/ui/ash/media_client_impl.cc
+++ chrome/browser/ui/ash/media_client_impl.cc
@@ -561,7 +561,7 @@ void MediaClientImpl::ShowCameraOffNotification(
           message_center::RichNotificationData(),
           new message_center::HandleNotificationClickDelegate(
               base::DoNothingAs<void()>()),
-          vector_icons::kVideocamOffIcon,
+          vector_icons::kSettingsIcon,
           message_center::SystemNotificationWarningLevel::NORMAL);
   SystemNotificationHelper::GetInstance()->Display(*notification);
 }
