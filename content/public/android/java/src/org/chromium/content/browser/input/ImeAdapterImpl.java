a74890f05a4ee367143a4fb6280a5e7e94b747a0
Peter Conn <peconn@chromium.org>
2022-11-11 18:54:33 +0000

    🖊️ Remove unnecessary null checks from ImeAdapterImpl.
    `mCursorAnchorInfoController` was only null pre-L, which we do not support anymore.
    
    Change-Id: I15311d5e3f837802ff125d1b47670869b7a1c46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020303
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070393}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
@@ -124,7 +124,7 @@ public class ImeAdapterImpl
     // InputMethodManager on appropriate timing, depending on how IME requested the information
     // via InputConnection. The update request is per InputConnection, hence for each time it is
     // re-created, the monitoring status will be reset.
-    private CursorAnchorInfoController mCursorAnchorInfoController;
+    private final CursorAnchorInfoController mCursorAnchorInfoController;
 
     private final List<ImeEventObserver> mEventObservers = new ArrayList<>();
 
@@ -220,7 +220,6 @@ public class ImeAdapterImpl
         // Deep copy newConfig so that we can notice the difference.
         mCurrentConfig = new Configuration(getContainerView().getResources().getConfiguration());
 
-        // CursorAnchroInfo is supported only after L.
         mCursorAnchorInfoController = CursorAnchorInfoController.create(
                 wrapper, new CursorAnchorInfoController.ComposingTextDelegate() {
                     @Override
@@ -1141,7 +1140,6 @@ public class ImeAdapterImpl
             ImeAdapterImplJni.get().requestCursorUpdate(mNativeImeAdapterAndroid,
                     ImeAdapterImpl.this, immediateRequest, monitorRequest);
         }
-        if (mCursorAnchorInfoController == null) return false;
         return mCursorAnchorInfoController.onRequestCursorUpdates(
                 immediateRequest, monitorRequest, getContainerView());
     }
@@ -1163,7 +1161,6 @@ public class ImeAdapterImpl
             boolean hasInsertionMarker, boolean isInsertionMarkerVisible,
             float insertionMarkerHorizontal, float insertionMarkerTop,
             float insertionMarkerBottom) {
-        if (mCursorAnchorInfoController == null) return;
         mCursorAnchorInfoController.onUpdateFrameInfo(scaleFactor, contentOffsetYPix,
                 hasInsertionMarker, isInsertionMarkerVisible, insertionMarkerHorizontal,
                 insertionMarkerTop, insertionMarkerBottom, getContainerView());
@@ -1279,7 +1276,6 @@ public class ImeAdapterImpl
 
     @CalledByNative
     private void setCharacterBounds(float[] characterBounds) {
-        if (mCursorAnchorInfoController == null) return;
         mCursorAnchorInfoController.setCompositionCharacterBounds(
                 characterBounds, getContainerView());
     }
