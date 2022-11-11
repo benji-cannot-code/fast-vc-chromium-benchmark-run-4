90e5ef5baac496a94e0265f99ce72d1aa632df8a
Aya ElAttar <ayaelattar@chromium.org>
2022-11-11 23:30:22 +0000

    DLP: Show notification for blocked launch
    Show desktop notification for blocked file share or open.
    
    Bug: 1382065
    Change-Id: I0c3671d4c9b42dbace76d1fc6048be0df69a6bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013040
    Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070557}

--- chrome/browser/ash/policy/dlp/dlp_files_controller.cc
+++ chrome/browser/ash/policy/dlp/dlp_files_controller.cc
@@ -77,6 +77,7 @@ constexpr size_t kEntriesLimit = 100;
 
 constexpr char kUploadBlockedNotificationId[] = "upload_dlp_blocked";
 constexpr char kDownloadBlockedNotificationId[] = "download_dlp_blocked";
+constexpr char kOpenBlockedNotificationId[] = "open_dlp_blocked";
 
 // FileSystemContext instance set for testing.
 storage::FileSystemContext* g_file_system_context_for_testing = nullptr;
@@ -954,7 +955,12 @@ void DlpFilesController::LaunchIfAllowed(
   }
 
   if (!response.files_paths().empty()) {
-    // TODO(crbug.com/1382065): Show block notification.
+    ShowNotification(
+        kOpenBlockedNotificationId,
+        l10n_util::GetStringUTF16(IDS_POLICY_DLP_FILES_OPEN_BLOCK_TITLE),
+        l10n_util::GetPluralStringFUTF16(
+            IDS_POLICY_DLP_FILES_OPEN_BLOCK_MESSAGE,
+            response.files_paths().size()));
     std::move(result_callback).Run(/*is_allowed=*/false);
     return;
   }
