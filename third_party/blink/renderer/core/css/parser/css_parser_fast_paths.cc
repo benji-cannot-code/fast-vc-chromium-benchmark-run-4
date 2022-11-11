31190d28ffb5199f64565932bf1890c338d39afe
Steinar H. Gunderson <sesse@chromium.org>
2022-11-11 16:56:38 +0000

    Remove some template remnants from CSSParserFastPaths.
    This should have been done in the patch where we removed
    the UChar support.
    
    Change-Id: I0cfa1f50cfc8dce1fc8a5f74808f227d6740076e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020344
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070325}

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -99,8 +99,7 @@ static inline bool IsSimpleLengthPropertyID(CSSPropertyID property_id,
   return properties.Has(property_id);
 }
 
-template <typename CharacterType>
-static inline bool ParseSimpleLength(const CharacterType* characters,
+static inline bool ParseSimpleLength(const LChar* characters,
                                      unsigned length,
                                      CSSPrimitiveValue::UnitType& unit,
                                      double& number) {
@@ -1446,8 +1445,7 @@ bool CSSParserFastPaths::IsValidSystemFont(CSSValueID value_id) {
   return value_id >= CSSValueID::kCaption && value_id <= CSSValueID::kStatusBar;
 }
 
-template <typename CharType>
-static inline CSSValue* ParseCSSWideKeywordValue(const CharType* ptr,
+static inline CSSValue* ParseCSSWideKeywordValue(const LChar* ptr,
                                                  unsigned length) {
   if (length == 7 && MatchesCaseInsensitiveLiteral4(ptr, "init") &&
       MatchesCaseInsensitiveLiteral4(ptr + 3, "tial")) {
@@ -1526,10 +1524,9 @@ static CSSValue* ParseKeywordValue(CSSPropertyID property_id,
   return nullptr;
 }
 
-template <typename CharType>
 static bool ParseTransformTranslateArguments(
-    CharType*& pos,
-    CharType* end,
+    const LChar*& pos,
+    const LChar* end,
     unsigned expected_count,
     CSSFunctionValue* transform_value) {
   while (expected_count) {
@@ -1553,9 +1550,8 @@ static bool ParseTransformTranslateArguments(
   return true;
 }
 
-template <typename CharType>
-static bool ParseTransformRotateArgument(CharType*& pos,
-                                         CharType* end,
+static bool ParseTransformRotateArgument(const LChar*& pos,
+                                         const LChar* end,
                                          CSSFunctionValue* transform_value) {
   wtf_size_t delimiter =
       WTF::Find(pos, static_cast<wtf_size_t>(end - pos), ')');
@@ -1579,9 +1575,8 @@ static bool ParseTransformRotateArgument(CharType*& pos,
   return true;
 }
 
-template <typename CharType>
-static bool ParseTransformNumberArguments(CharType*& pos,
-                                          CharType* end,
+static bool ParseTransformNumberArguments(const LChar*& pos,
+                                          const LChar* end,
                                           unsigned expected_count,
                                           CSSFunctionValue* transform_value) {
   while (expected_count) {
@@ -1605,9 +1600,8 @@ static bool ParseTransformNumberArguments(CharType*& pos,
 
 static const int kShortestValidTransformStringLength = 12;
 
-template <typename CharType>
-static CSSFunctionValue* ParseSimpleTransformValue(CharType*& pos,
-                                                   CharType* end) {
+static CSSFunctionValue* ParseSimpleTransformValue(const LChar*& pos,
+                                                   const LChar* end) {
   if (end - pos < kShortestValidTransformStringLength)
     return nullptr;
 
