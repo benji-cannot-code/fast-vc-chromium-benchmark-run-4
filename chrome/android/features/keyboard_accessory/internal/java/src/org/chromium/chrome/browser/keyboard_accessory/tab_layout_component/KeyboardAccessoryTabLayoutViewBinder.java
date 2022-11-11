7b56648ebbca2a8bc57a458932419ea9f7712992
Tommy Nyquist <nyquist@chromium.org>
2022-11-11 19:03:51 +0000

    Revert "[mfill] Introduce KeyboardAccessoryButtonGroupView and binding for v2"
    This reverts commit 39275678458fd1a046d6b796ca80beb5c80beb9a.
    
    Reason for revert: Consistently fails AddressAccessoryIntegrationTest#testFillsSuggestionOnClick on multiple bots.
    See for details: https://crbug.com/1383532
    
    Original change's description:
    > [mfill] Introduce KeyboardAccessoryButtonGroupView and binding for v2
    >
    > This change is the first CL to replace TabLayout with a custom button
    > group. Future CLs will continue renaming tabs -> buttons after
    > AUTOFILL_KEYBOARD_ACCESSORY is launched
    >
    > The change is required to correctly announce the buttons in the bar.
    > See screenshots with the Talkback output:
    > Before: http://screen/ABmsBhswaSuUB3d
    > After: http://screen/W66BgpPtozA5Ztg
    >
    > Bug: 1247384
    > Change-Id: I35331080a0189ecb8a147bdad1eac2cead85cbad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3970644
    > Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1069720}
    
    Bug: 1247384, 1383532
    Change-Id: Id6266676718f87e0f3199db7eb0b7a10623ccbd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023771
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070400}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/tab_layout_component/KeyboardAccessoryTabLayoutViewBinder.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/tab_layout_component/KeyboardAccessoryTabLayoutViewBinder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.keyboard_accessory.tab_layout_component;
 
 import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutProperties.ACTIVE_TAB;
-import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutProperties.BUTTON_SELECTION_CALLBACKS;
 import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutProperties.TABS;
 import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutProperties.TAB_SELECTION_CALLBACKS;
 
@@ -80,8 +79,6 @@ class KeyboardAccessoryTabLayoutViewBinder
             // Don't add null as listener. It's a valid state but an invalid argument.
             TabLayout.OnTabSelectedListener listener = model.get(TAB_SELECTION_CALLBACKS);
             if (listener != null) view.setTabSelectionAdapter(listener);
-        } else if (propertyKey == BUTTON_SELECTION_CALLBACKS) {
-            // Do nothing.
         } else {
             assert false : "Every possible property update needs to be handled!";
         }
