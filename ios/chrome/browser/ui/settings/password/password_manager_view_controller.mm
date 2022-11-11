de75d5de6ff2553466db72be5eed95df5422b807
Ernesto Izquierdo Clua <eic@google.com>
2022-11-11 14:33:18 +0000

    Fix setAffiliatedGroups.
    This patch fixes setAffiliatedGroups so that the UI is only updated
    after the loading spinner is removed, the same way it is done in
    setPasswords.
    
    Bug: 1335156
    Change-Id: I0ca11598ddd6b176272cb2da14a60009154bc4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021906
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070283}

--- ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_view_controller.mm
@@ -1202,10 +1202,7 @@ NSInteger kTrailingSymbolSize = 18;
   if (!_didReceivePasswords) {
     _blockedSites = blockedSites;
     _affiliatedGroups = affiliatedGroups;
-    _didReceivePasswords = YES;
     [self hideLoadingSpinnerBackground];
-    [self updateUIForEditState];
-    [self reloadData];
   } else {
     if (_affiliatedGroups == affiliatedGroups &&
         _blockedSites == blockedSites) {
