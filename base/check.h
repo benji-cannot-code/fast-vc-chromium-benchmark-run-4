9b2164d07a45575ebae4ff9980de48ee0a3570b0
Brett Brotherton <bbrotherton@google.com>
2022-11-11 18:20:24 +0000

    Prevent -Wunused-parameter on VoidifyStream constructor
    downstream chromeos client packages that have -Wunused-parameter
    compiler flags were failing on the VoidifiStream constructor with an
    unused parameter, remove the parameter name to make the error go away
    and allow client packages to still build with -Wunused-parameter
    
    Bug: b/258709783
    
    Change-Id: Id4334873ef7f4dfadee4e1ef173548e4bb86bab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023199
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Brett Brotherton <bbrotherton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070373}

--- base/check.h
+++ base/check.h
@@ -46,7 +46,7 @@ namespace logging {
 class VoidifyStream {
  public:
   VoidifyStream() = default;
-  explicit VoidifyStream(bool ignored) {}
+  explicit VoidifyStream(bool) {}
 
   // This operator has lower precedence than << but higher than ?:
   void operator&(std::ostream&) {}
