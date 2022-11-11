90b05d92a20ea19b4f418c759f93fe903b59404b
Jongmok Kim <jongmok.kim@navercorp.com>
2022-11-11 23:52:23 +0000

    Add a tooltip to tab search button for windows 10, 11
    On windows 10 and above, the caption button is used for tab search button.
    However, it does not show windows platform tooltip.
    
    Bug: 1383226
    Change-Id: I6f18004afbad5c510f1fa313a3fa754fa25415e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021889
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: jongmok kim <johny.kimc@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1070570}

--- chrome/browser/ui/views/frame/windows_tab_search_caption_button.cc
+++ chrome/browser/ui/views/frame/windows_tab_search_caption_button.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/glass_browser_frame_view.h"
 #include "chrome/browser/ui/views/tab_search_bubble_host.h"
+#include "chrome/grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/view_class_properties.h"
 
@@ -28,6 +30,7 @@ WindowsTabSearchCaptionButton::WindowsTabSearchCaptionButton(
   SetProperty(views::kElementIdentifierKey, kTabSearchButtonElementId);
   views::FocusRing::Get(this)->SetColorId(
       kColorTabSearchCaptionButtonFocusRing);
+  SetTooltipText(l10n_util::GetStringUTF16(IDS_TOOLTIP_TAB_SEARCH));
 }
 
 WindowsTabSearchCaptionButton::~WindowsTabSearchCaptionButton() = default;
