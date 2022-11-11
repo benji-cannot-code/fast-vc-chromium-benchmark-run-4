a463c441b0f85a0f6026969e37b4e219760a3a19
Yaron Friedman <yfriedman@chromium.org>
2022-11-11 12:18:39 +0000

    [Android M Cleanup] Simplify isCharging logic.
    Could have been cleaned up earlier as M is deprecated. Simplify code.
    
    Change-Id: I761b17e5f944d438be81664ad8e930e10ed595b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017877
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070243}

--- base/android/java/src/org/chromium/base/compat/ApiHelperForM.java
+++ base/android/java/src/org/chromium/base/compat/ApiHelperForM.java
@@ -15,7 +15,6 @@ import android.graphics.drawable.Icon;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
-import android.os.BatteryManager;
 import android.os.Build;
 import android.os.PowerManager;
 import android.os.Process;
@@ -168,11 +167,6 @@ public final class ApiHelperForM {
     /** See {@link Display.Mode#getPhysicalHeight() }. */
     public static int getModePhysicalHeight(Display.Mode mode) {
         return mode.getPhysicalHeight();
-    }
-
-    /** See {@link BatteryManager#isCharging() }. */
-    public static boolean isCharging(BatteryManager batteryManager) {
-        return batteryManager.isCharging();
     }
 
     /** See {@link Icon#createWithBitmap(Bitmap) }. */
