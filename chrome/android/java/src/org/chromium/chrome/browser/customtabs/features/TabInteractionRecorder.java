c851cffe1ff15d8d289747d597b60f4adf4cad67
Wenyu Fu <wenyufu@google.com>
2022-11-12 00:09:40 +0000

    [RCTS] Add unit test for TabInteractionRecorderAndroid
    Add basic unit tests that were missing on Android side.
    
    Bug: 1322240
    Change-Id: I5c917a22af4110e51c1bef9b5e2b208924f90608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018733
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070583}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/TabInteractionRecorder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/TabInteractionRecorder.java
@@ -41,6 +41,11 @@ public class TabInteractionRecorder {
         mNativeTabInteractionRecorder = nativePtr;
     }
 
+    @VisibleForTesting
+    TabInteractionRecorder() {
+        this(1L);
+    }
+
     @CalledByNative
     private static @Nullable TabInteractionRecorder create(long nativePtr) {
         if (nativePtr == 0) return null;
