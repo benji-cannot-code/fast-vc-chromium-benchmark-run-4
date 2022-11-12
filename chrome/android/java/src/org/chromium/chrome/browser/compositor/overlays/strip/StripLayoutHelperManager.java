4c28b487b3896e831aa0db7379e9d16e9e12ddcf
Zhe Li <zheliooo@google.com>
2022-11-12 00:01:25 +0000

    [Tab Strip Redesign] Update status bar color
    Folio-light: https://screenshot.googleplex.com/BqMeCUzxFxLTDbH
    Folio-dark: https://screenshot.googleplex.com/5SYn4BBvxvCQHLU
    
    Detached-light: https://screenshot.googleplex.com/463FyKJcrpx3pmY
    Detached-dark: https://screenshot.googleplex.com/5AY3uekMRV8oVxr
    
    Settings-light: https://screenshot.googleplex.com/4RvSk8BE5iST8UA
    Settings-dark: https://screenshot.googleplex.com/AJA4epJSFPkLF99
    
    Bug: 1373631
    Change-Id: I94a1f1a08975d9ed7c892defa99ea47b8b144d42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017176
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070577}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -9,7 +9,6 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.RectF;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -53,7 +52,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
 import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
-import org.chromium.components.browser_ui.styles.ChromeColors;
+import org.chromium.chrome.browser.tasks.tab_management.TabUiThemeProvider;
 import org.chromium.components.browser_ui.widget.animation.Interpolators;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.LocalizationUtils;
@@ -686,23 +685,7 @@ public class StripLayoutHelperManager implements SceneOverlay, PauseResumeWithNa
     }
 
     public @ColorInt int getBackgroundColor() {
-        if (TabUiFeatureUtilities.isTabStripFolioEnabled()) {
-            if (mIsIncognito) {
-                return Color.BLACK;
-            }
-            // @TODO(crbug.com/1373630): May change the color for night theme after finalizing the
-            // spec
-            return ChromeColors.getSurfaceColor(mContext, R.dimen.default_elevation_2);
-        } else if (TabUiFeatureUtilities.isTabStripDetachedEnabled()) {
-            if (mIsIncognito) {
-                // Use a non-dynamic dark background color for incognito, slightly greyer than
-                // Color.BLACK
-                return ChromeColors.getPrimaryBackgroundColor(mContext, mIsIncognito);
-            }
-            return ChromeColors.getSurfaceColor(mContext, R.dimen.default_elevation_0);
-        }
-        // This is the default color for tab strip and fades
-        return Color.BLACK;
+        return TabUiThemeProvider.getTabStripBackgroundColor(mContext, mIsIncognito);
     }
 
     /**
