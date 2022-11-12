c41b61dffea303e6ba5fc9ea4ed975f9604463fc
Daniel Cheng <dcheng@chromium.org>
2022-11-12 00:50:02 +0000

    Mark callback and friends as trivial ABI everywhere except Windows.
    A local official Linux build goes from 209.06 MB to 208.09 MB. For
    whatever reason, clang-cl is not happy with the use of TRIVIAL_ABI in
    these contexts, so for now, this is TRIVIAL_ABI everywhere except
    Windows.
    
    To prevent the workaround macros from leaking, the definitions are
    duplicated in both callback.h and callback_internal.h, since there's a
    few files that include callback_internal.h directly.
    
    In addition, build/build_config.h is already transitively included, so
    removing the hack later should be trivial.
    
    Bug: 1383397
    Change-Id: I755d0b2c4b2e6eec89026ee16a88c572ab7794a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021547
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070593}

--- base/functional/callback_internal.h
+++ base/functional/callback_internal.h
@@ -12,8 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/base_export.h"
+#include "base/compiler_specific.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/ref_counted.h"
+#include "build/build_config.h"
+
+// TODO(https://crbug.com/1383397): Temporary workaround for clang-cl
+// ignoring TRIVIAL_ABI on `CallbackBaseCopyable`, `OnceCallback`, and
+// `RepeatingCallback` because, for some reason, it considers them to have
+// non-trivial abi base classes, despite all the base classes being annotated
+// with TRIVIAL_ABI...
+#if BUILDFLAG(IS_WIN)
+#define TRIVIAL_ABI_EXCEPT_WIN
+#else
+#define TRIVIAL_ABI_EXCEPT_WIN TRIVIAL_ABI
+#endif
 
 namespace base {
 
@@ -111,8 +124,10 @@ class BASE_EXPORT BindStateBase
 // template bloat.
 // CallbackBase<MoveOnly> is a direct base class of MoveOnly callbacks, and
 // CallbackBase<Copyable> uses CallbackBase<MoveOnly> for its implementation.
-class BASE_EXPORT CallbackBase {
+class BASE_EXPORT TRIVIAL_ABI_EXCEPT_WIN CallbackBase {
  public:
+  // Since this type is marked as TRIVIAL_ABI, the move operations must leave
+  // the moved-from object in a trivially destructible state.
   inline CallbackBase(CallbackBase&& c) noexcept;
   CallbackBase& operator=(CallbackBase&& c) noexcept;
 
@@ -166,6 +181,9 @@ class BASE_EXPORT CallbackBase {
   // Force the destructor to be instantiated inside this translation unit so
   // that our subclasses will not get inlined versions.  Avoids more template
   // bloat.
+  //
+  // Since type is marked as TRIVIAL_ABI, the destructor must be a no-op when
+  // `this` is a moved-from object.
   ~CallbackBase();
 
   scoped_refptr<BindStateBase> bind_state_;
@@ -177,11 +195,15 @@ CallbackBase::CallbackBase(BindStateBase* bind_state)
     : bind_state_(AdoptRef(bind_state)) {}
 
 // CallbackBase<Copyable> is a direct base class of Copyable Callbacks.
-class BASE_EXPORT CallbackBaseCopyable : public CallbackBase {
+class BASE_EXPORT TRIVIAL_ABI_EXCEPT_WIN CallbackBaseCopyable
+    : public CallbackBase {
  public:
   CallbackBaseCopyable(const CallbackBaseCopyable& c);
-  CallbackBaseCopyable(CallbackBaseCopyable&& c) noexcept = default;
   CallbackBaseCopyable& operator=(const CallbackBaseCopyable& c);
+
+  // Since this type is marked as TRIVIAL_ABI, the move operations must leave
+  // the moved-from object in a trivially destructible state.
+  CallbackBaseCopyable(CallbackBaseCopyable&& c) noexcept = default;
   CallbackBaseCopyable& operator=(CallbackBaseCopyable&& c) noexcept;
 
  protected:
@@ -247,5 +269,7 @@ struct ThenHelper<OriginalCallback<OriginalR(OriginalArgs...)>,
 
 }  // namespace internal
 }  // namespace base
+
+#undef TRIVIAL_ABI_EXCEPT_WIN
 
 #endif  // BASE_FUNCTIONAL_CALLBACK_INTERNAL_H_
