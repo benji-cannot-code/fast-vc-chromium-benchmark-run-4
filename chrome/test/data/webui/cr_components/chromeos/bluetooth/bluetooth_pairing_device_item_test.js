98c18b0932a0916350a90613e022c3a1f916d4cc
Rebekah Potter <rbpotter@chromium.org>
2022-11-12 01:43:46 +0000

    WebUI: Migrate test_util to TS and fork for ChromeOS closure
    Bug: 1316438
    Change-Id: I8c43a9591f4b7b3e3b7d72c3cc35423b451b67c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021970
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070612}

--- chrome/test/data/webui/cr_components/chromeos/bluetooth/bluetooth_pairing_device_item_test.js
+++ chrome/test/data/webui/cr_components/chromeos/bluetooth/bluetooth_pairing_device_item_test.js
@@ -11,7 +11,7 @@ import {AudioOutputCapability, DeviceConnectionState, DeviceType} from 'chrome:/
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {assertEquals, assertTrue} from '../../../chai_assert.js';
-import {eventToPromise} from '../../../test_util.js';
+import {eventToPromise} from '../../../chromeos/test_util.js';
 
 import {createDefaultBluetoothDevice} from './fake_bluetooth_config.js';
 
