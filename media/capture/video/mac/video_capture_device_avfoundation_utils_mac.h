e78aab3081f156ea05dad5b357a8edc5d9f3a843
Tony Herre <toprice@chromium.org>
2022-11-14 12:45:06 +0000

    Migrate to AVCaptureDeviceDiscoverySession for mac camera discovery
    Stop using the deprecated AVCaptureDevice devices API for listing
    cameras, in a vague hope that this will help with crbug.com/1379392.
    
    All guarded by a couple of features for a safer rollout.
    
    Docs links:
    - Deprecated API: https://developer.apple.com/documentation/avfoundation/avcapturedevice/1386237-devices?language=objc
    - Shiny new goodness: https://developer.apple.com/documentation/avfoundation/avcapturedevicediscoverysession?language=objc
    
    Change-Id: I11cf1f8c7cbf2eb2033a5c0ab8e5949e2a500671
    Bug: 1383511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020199
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070959}

--- media/capture/video/mac/video_capture_device_avfoundation_utils_mac.h
+++ media/capture/video/mac/video_capture_device_avfoundation_utils_mac.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <CoreMedia/CoreMedia.h>
 #import <CoreVideo/CoreVideo.h>
 
+#include "base/feature_list.h"
 #include "base/mac/scoped_nsobject.h"
 #include "media/capture/video/video_capture_device_descriptor.h"
 #include "media/capture/video_capture_types.h"
@@ -33,6 +34,10 @@ base::scoped_nsobject<NSDictionary> GetVideoCaptureDeviceNames();
 
 gfx::Size CAPTURE_EXPORT GetPixelBufferSize(CVPixelBufferRef pixel_buffer);
 gfx::Size CAPTURE_EXPORT GetSampleBufferSize(CMSampleBufferRef sample_buffer);
+
+// When enabled, we use an AVCaptureDeviceDiscoverySession for enumerating
+// cameras, instead of the deprecated [AVDeviceCapture devices].
+CAPTURE_EXPORT BASE_DECLARE_FEATURE(kUseAVCaptureDeviceDiscoverySession);
 
 }  // namespace media
 
