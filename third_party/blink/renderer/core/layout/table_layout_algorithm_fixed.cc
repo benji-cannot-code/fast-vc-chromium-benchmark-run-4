63195d1438b21927a8944b9a44aac9ee0d13c968
Steinar H. Gunderson <sesse@chromium.org>
2022-11-14 21:39:12 +0000

    Make blink::Length always store length as floats.
    blink::Length can store lengths either as ints or as floats,
    seemingly only for legacy reasons related to multiplying them
    when doing table calculations (which, according to Morten,
    is going to go away eventually). This leads to funny situations,
    such as where operator== converts calculation handles to floats and
    compares them (which would give the wrong results if we have
    more than 2^24 of them).
    
    Simplify away nearly all of this, by simply always storing floats
    unless we have a calculation handle. This simplifies operator==
    to the point where it gets inlined in most cases (and we never
    have a call to IsCalculationEqual() for non-kCalculated).
    
    Change-Id: I6f6cdba060cbba5ef13d6669fa73ed51aed32138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023472
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071235}

--- third_party/blink/renderer/core/layout/table_layout_algorithm_fixed.cc
+++ third_party/blink/renderer/core/layout/table_layout_algorithm_fixed.cc
@@ -74,6 +74,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+static Length BrokenLegacyMultiply(const Length& length, float v) {
+  if (length.IsCalculated()) {
+    NOTREACHED();
+    return length;
+  }
+
+  Length ret = length.GetRoundToInt()
+                   ? Length(static_cast<int>(length.GetFloatValue() * v),
+                            length.GetType())
+                   : Length(length.GetFloatValue() * v, length.GetType());
+  ret.SetQuirk(length.Quirk());
+  return ret;
+}
+
 TableLayoutAlgorithmFixed::TableLayoutAlgorithmFixed(LayoutTable* table)
     : TableLayoutAlgorithm(table), recorded_width_difference_(false) {}
 
@@ -128,8 +142,8 @@ int TableLayoutAlgorithmFixed::CalcWidthArray() {
       if ((col_style_logical_width.IsFixed() ||
            col_style_logical_width.IsPercent()) &&
           col_style_logical_width.IsPositive()) {
-        width_[current_effective_column] = col_style_logical_width;
-        width_[current_effective_column] *= span_in_current_effective_column;
+        width_[current_effective_column] = BrokenLegacyMultiply(
+            col_style_logical_width, span_in_current_effective_column);
         used_width += effective_col_width * span_in_current_effective_column;
       }
       span -= span_in_current_effective_column;
@@ -177,8 +191,8 @@ int TableLayoutAlgorithmFixed::CalcWidthArray() {
       float e_span = table_->SpanOfEffectiveColumn(current_column);
       // Only set if no col element has already set it.
       if (width_[current_column].IsAuto() && !logical_width.IsAuto()) {
-        width_[current_column] = logical_width;
-        width_[current_column] *= e_span / span;
+        width_[current_column] =
+            BrokenLegacyMultiply(logical_width, e_span / span);
         used_width += fixed_border_box_logical_width * e_span / span;
       }
       used_span += e_span;
