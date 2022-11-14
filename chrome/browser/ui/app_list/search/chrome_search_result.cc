ca64ad24bb20b2a96bb6e9eef6e4d74f152251ba
CJ Huang <chenjih@google.com>
2022-11-14 07:05:18 +0000

    [test] Add ChromeSearchResult unit test.
    Test the interaction between title and title vector, and between detail and detail vector.
    
    Bug: 1364452
    Change-Id: I345a232484eb03f533b5d5f1609766eb8ebe326d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023130
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070885}

--- chrome/browser/ui/app_list/search/chrome_search_result.cc
+++ chrome/browser/ui/app_list/search/chrome_search_result.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
-#include "base/containers/adapters.h"
 #include "base/logging.h"
 #include "chrome/browser/ui/app_list/app_context_menu.h"
 #include "ui/base/models/image_model.h"
