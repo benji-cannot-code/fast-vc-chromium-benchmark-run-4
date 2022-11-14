98c0789d95a0cb7522ecef257184633a91926ddc
hmare <hmare@google.com>
2022-11-14 20:47:30 +0000

    [ProfileID] Add profile id to secure connect API
    Adds the enterprise profile identifier to the GetContextInfo enterprise
    reporting public API
    
    Design document: go/chrome-profile-identifier-dd
    Screenshot: https://screenshot.googleplex.com/BFWu4KkNUvLp5pi
    
    Bug:1384158
    
    Change-Id: Id142334091ccf1e481a6fea7a500254c29e4c5db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007926
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071200}

--- chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_apitest.cc
+++ chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_apitest.cc
@@ -461,15 +461,15 @@ IN_PROC_BROWSER_TEST_F(EnterpriseReportingPrivateApiTest, GetContextInfo) {
   constexpr char kChromeCleanupEnabledType[] = "boolean";
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   constexpr char kThirdPartyBlockingEnabledType[] = "boolean";
-  constexpr char kCount[] = "18";
+  constexpr char kCount[] = "19";
 #else
   constexpr char kThirdPartyBlockingEnabledType[] = "undefined";
-  constexpr char kCount[] = "17";
+  constexpr char kCount[] = "18";
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #else
   constexpr char kChromeCleanupEnabledType[] = "undefined";
   constexpr char kThirdPartyBlockingEnabledType[] = "undefined";
-  constexpr char kCount[] = "16";
+  constexpr char kCount[] = "17";
 #endif  // BUILDFLAG(IS_WIN)
 
   constexpr char kTest[] = R"(
@@ -500,6 +500,7 @@ IN_PROC_BROWSER_TEST_F(EnterpriseReportingPrivateApiTest, GetContextInfo) {
       chrome.test.assertEq(typeof info.thirdPartyBlockingEnabled,'%s');
       chrome.test.assertEq(typeof info.osFirewall, 'string');
       chrome.test.assertTrue(info.systemDnsServers instanceof Array);
+      chrome.test.assertEq(typeof info.enterpriseProfileId, 'string');
 
       chrome.test.notifyPass();
     });)";
