2d6f37f5291bd6490b05b5dc3ef1034b2d00adba
Kevin McNee <mcnee@chromium.org>
2022-11-14 23:26:06 +0000

    Restore FrameOwnerElementTypeTests
    The CL that removed the fenced frames ShadowDOM implementation [1]
    deleted these tests, however it was only necessary to remove the
    fenced frames parameterization.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3918971
    
    Bug: 1262022
    Change-Id: I8e0648a9f0d7b0daa51e7514e0f4d1d978e29d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022978
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071290}

--- content/common/frame_owner_element_type_mojom_traits.cc
+++ content/common/frame_owner_element_type_mojom_traits.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/common/frame_owner_element_type_mojom_traits.h"
-#include "third_party/blink/public/common/features.h"
 
 namespace mojo {
 
