2ccac46571438c46ad27800b1d37d623fadb4811
Adam Langley <agl@chromium.org>
2022-11-14 17:14:51 +0000

    webauthn: support credProps on Android.
    This change plumbs credProps information from Play Services.
    
    Bug: b/258733829
    Change-Id: I6b519f3f2457fda9acca9636ce4afe6dd50e5d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022565
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071073}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2CredentialRequest.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2CredentialRequest.java
@@ -71,6 +71,7 @@ public class Fido2CredentialRequest implements Callback<Pair<Integer, Intent>> {
     private WebContents mWebContents;
     private boolean mAppIdExtensionUsed;
     private long mStartTimeMs;
+    private boolean mEchoCredProps;
     private WebAuthnBrowserBridge mBrowserBridge;
     private boolean mAttestationAcceptable;
 
@@ -141,6 +142,8 @@ public class Fido2CredentialRequest implements Callback<Pair<Integer, Intent>> {
         // filtered here.
         mAttestationAcceptable =
                 options.authenticatorSelection.residentKey == ResidentKeyRequirement.DISCOURAGED;
+        mEchoCredProps = options.credProps;
+
         Fido2ApiCall call = new Fido2ApiCall(ContextUtils.getApplicationContext(), mSupportLevel);
         Parcel args = call.start();
         Fido2ApiCall.PendingIntentResult result = new Fido2ApiCall.PendingIntentResult(call);
@@ -480,8 +483,16 @@ public class Fido2CredentialRequest implements Callback<Pair<Integer, Intent>> {
             errorCode = convertError(error);
         } else if (mMakeCredentialCallback != null) {
             if (response instanceof org.chromium.blink.mojom.MakeCredentialAuthenticatorResponse) {
-                mMakeCredentialCallback.onRegisterResponse(AuthenticatorStatus.SUCCESS,
-                        (org.chromium.blink.mojom.MakeCredentialAuthenticatorResponse) response);
+                org.chromium.blink.mojom.MakeCredentialAuthenticatorResponse creationResponse =
+                        (org.chromium.blink.mojom.MakeCredentialAuthenticatorResponse) response;
+                if (mEchoCredProps) {
+                    // The other credProps fields will have been set by
+                    // `parseIntentResponse` if Play Services provided credProps
+                    // information.
+                    creationResponse.echoCredProps = true;
+                }
+                mMakeCredentialCallback.onRegisterResponse(
+                        AuthenticatorStatus.SUCCESS, creationResponse);
                 mMakeCredentialCallback = null;
                 return;
             }
