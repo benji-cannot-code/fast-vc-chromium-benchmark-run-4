98dc148f19ff3d8de596825c4743ad3f8d0c6293
Marijn Kruisselbrink <mek@chromium.org>
2022-11-14 21:15:37 +0000

    Fix window control overlay actions.
    A Check action should not be modifying any state, so make sure these
    actions don't launch the app as a side effect. Also add DCHECKs to
    reduce the chance of similar problems in the future.
    
    Finally adjust all the critical user journeys that incorrectly relied on
    these check actions modifying state.
    
    Bug: None
    Change-Id: I284dfdb86579e61202b20c889467b15bbf8de972
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025299
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071220}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -2499,24 +2499,26 @@ void WebAppIntegrationTestDriver::CheckWindowCreated() {
 void WebAppIntegrationTestDriver::CheckWindowControlsOverlayToggle(
     Site site,
     IsShown is_shown) {
-  if (!BeforeStateChangeAction(__FUNCTION__))
+  if (!BeforeStateCheckAction(__FUNCTION__))
     return;
-  if (!app_browser())
-    app_browser_ = GetAppBrowserForSite(site);
   ASSERT_TRUE(app_browser());
+  EXPECT_TRUE(AppBrowserController::IsForWebApp(app_browser(),
+                                                GetAppIdBySiteMode(site)));
   EXPECT_EQ(app_browser()->app_controller()->AppUsesWindowControlsOverlay(),
             is_shown == IsShown::kShown);
-  AfterStateChangeAction();
+  AfterStateCheckAction();
 }
 
 void WebAppIntegrationTestDriver::CheckWindowControlsOverlay(Site site,
                                                              IsOn is_on) {
-  if (!BeforeStateChangeAction(__FUNCTION__))
+  if (!BeforeStateCheckAction(__FUNCTION__))
     return;
   ASSERT_TRUE(app_browser());
+  EXPECT_TRUE(AppBrowserController::IsForWebApp(app_browser(),
+                                                GetAppIdBySiteMode(site)));
   BrowserView* app_view = BrowserView::GetBrowserViewForBrowser(app_browser());
   EXPECT_EQ(app_view->IsWindowControlsOverlayEnabled(), is_on == IsOn::kOn);
-  AfterStateChangeAction();
+  AfterStateCheckAction();
 }
 
 void WebAppIntegrationTestDriver::CheckWindowDisplayMinimal() {
@@ -2587,6 +2589,7 @@ void WebAppIntegrationTestDriver::OnWebAppManifestUpdated(
 
 bool WebAppIntegrationTestDriver::BeforeStateChangeAction(
     const char* function) {
+  DCHECK(!base::StartsWith(function, "Check"));
   if (testing::Test::HasFatalFailure() && !in_tear_down_)
     return false;
   LOG(INFO) << "BeforeStateChangeAction: "
@@ -2650,6 +2653,7 @@ void WebAppIntegrationTestDriver::AfterStateChangeAction() {
 }
 
 bool WebAppIntegrationTestDriver::BeforeStateCheckAction(const char* function) {
+  DCHECK(base::StartsWith(function, "Check"));
   if (testing::Test::HasFatalFailure() && !in_tear_down_)
     return false;
   ++executing_action_level_;
