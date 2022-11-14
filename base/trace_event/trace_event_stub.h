557d148f7f74ffd21831363a74dfdae9804973e4
Aman Verma <amanvr@google.com>
2022-11-14 14:49:04 +0000

    [base] Change CurrentProcess::SetProcessType to use switch case to map process_type to sting name
    Remove for-loop which was mapping process types to string names. Add
    ENABLE_BASE_TRACING flags for compatibility with enable_base_tracing =
    false.
    
    Change-Id: I1ff1922cf0a9b5ab54e749c91c81c2e188f479fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016554
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070996}

--- base/trace_event/trace_event_stub.h
+++ base/trace_event/trace_event_stub.h
@@ -257,9 +257,6 @@ inline const char* QueueName_Name(QueueName value) {
 namespace ChromeProcessDescriptor {
 
 enum ProcessType {};
-inline const char* ProcessType_Name(ProcessType process_type) {
-  return "";
-}
 
 }  // namespace ChromeProcessDescriptor
 
