917d841a43ae98214ae28b38182a1f06ee7b8352
Josiah Krutz <josiahk@google.com>
2022-11-14 22:26:29 +0000

    [Select-to-speak] Enable UI improvements and voice switching
    Default enable Select-to-speak UI improvements and voice switching for
    all users. http://go/select-to-speak-ui-improvements
    
    Launch: http://launch/4212650
    Bug: 1269912, 950391, 1269891
    Change-Id: I4ac920b8f0d7f384b70c2f83e999f0e896815bcd
    AX-Relnotes: Enables automatic voice-switching on text which is tagged by the developer with a language code. Allows users to start Select-to-Speak on selected text using the right-click context menu in web content. Improves usability of tray button by adding new tooltip text, giving instructions to select text and stop speaking depending on the current Select-to-speak mode.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022575
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071256}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -168,7 +168,7 @@ bool IsExperimentalAccessibilityGoogleTtsLanguagePacksEnabled() {
 
 BASE_FEATURE(kExperimentalAccessibilitySelectToSpeakVoiceSwitching,
              "ExperimentalAccessibilitySelectToSpeakVoiceSwitching",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsExperimentalAccessibilitySelectToSpeakVoiceSwitchingEnabled() {
   return base::FeatureList::IsEnabled(
@@ -229,7 +229,7 @@ bool IsAccessibilitySelectToSpeakPrefsMigrationEnabled() {
 
 BASE_FEATURE(kAccessibilitySelectToSpeakContextMenuOption,
              "AccessibilitySelectToSpeakContextMenuOption",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAccessibilitySelectToSpeakContextMenuOptionEnabled() {
   return base::FeatureList::IsEnabled(
@@ -238,7 +238,7 @@ bool IsAccessibilitySelectToSpeakContextMenuOptionEnabled() {
 
 BASE_FEATURE(kAccessibilitySelectToSpeakHoverTextImprovements,
              "AccessibilitySelectToSpeakHoverTextImprovements",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAccessibilitySelectToSpeakHoverTextImprovementsEnabled() {
   return base::FeatureList::IsEnabled(
