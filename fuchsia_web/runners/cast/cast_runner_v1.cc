4ed876bef75c4cf245da4ce4286719b8b8d8122d
Wez <wez@chromium.org>
2022-11-14 11:51:32 +0000

    [fuchsia] Pass-through LogSink to Cast activities
    Bug: 1332972
    Change-Id: Id498cb25b9af87ef4689670cf596c5f6e2dec6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021045
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070949}

--- fuchsia_web/runners/cast/cast_runner_v1.cc
+++ fuchsia_web/runners/cast/cast_runner_v1.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuchsia/component/cpp/fidl.h>
 #include <fuchsia/component/decl/cpp/fidl.h>
+#include <fuchsia/logger/cpp/fidl.h>
 #include <fuchsia/sys/cpp/fidl.h>
 #include <fuchsia/ui/app/cpp/fidl.h>
 #include <lib/fdio/directory.h>
@@ -68,9 +69,12 @@ class CastComponentV1 : public fuchsia::sys::ComponentController {
         SvcForCfv2Dir()->AddEntry(child_id_, std::move(child_svc));
     ZX_CHECK(status == ZX_OK, status);
 
-    // TODO(crbug.com/1332972): Migrate the CFv2 code not to need these routed
+    // TODO(crbug.com/1332972): Migrate the CFv2 code not to need this routed
     // via the Cast activity's incoming services.
     OfferFromStartupContext<chromium::cast::ApplicationConfigManager>();
+
+    // Offer the Cast component its own LogSink.
+    OfferFromStartupContext<fuchsia::logger::LogSink>();
 
     // Offer services from the associated Agent to the CFv2 component.
     OfferFromAgent<chromium::cast::ApiBindings>();
