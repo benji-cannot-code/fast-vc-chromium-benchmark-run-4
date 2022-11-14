9057bb7827ec4eb4f261c0a860e7e5981c8a32b3
Cathy Wei <cathywei@google.com>
2022-11-14 23:45:46 +0000

    [Sticky-Header]Show the sticky header on the start surface
    The sticky header wasn't shown on the start surface, and in this CL, we make it shown below the toolbar.
    Note that the margin should be adjusted to the appropriate number in later CLs.
    
    Before:
    http://shortn/_Ghyt1BJL4b
    After:
    http://shortn/_s09mOQ5apN
    
    Bug: b:241271058, b:257512192
    
    Change-Id: Ia97dca5f323cea96a1184aa8141b3fc9f0324f06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011498
    Commit-Queue: Cathy Wei <cathywei@google.com>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071311}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderView.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderView.java
@@ -573,4 +573,9 @@ public class SectionHeaderView extends LinearLayout {
      * Does nothing otherwise.
      */
     void setStickyHeaderVisible(boolean isVisible) {}
+
+    /**
+     * Adjust the margin of the sticky header.
+     */
+    void updateStickyHeaderMargin(int marginValue) {}
 }
