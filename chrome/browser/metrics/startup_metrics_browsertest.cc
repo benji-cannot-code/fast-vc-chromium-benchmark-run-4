82e42130226e28b10bcdd5e97ed11b4d085f4328
Gabriel Charette <gab@chromium.org>
2022-11-14 14:13:21 +0000

    [Startup] Add missing metrics to StartupMetricsTest
    Change-Id: I670c4200a552cb169eafd17c0051cb398dc5828d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022216
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070985}

--- chrome/browser/metrics/startup_metrics_browsertest.cc
+++ chrome/browser/metrics/startup_metrics_browsertest.cc
@@ -31,6 +31,7 @@ constexpr const char* kStartupMetrics[] = {
 
 // Desktop specific metrics
 #if !BUILDFLAG(IS_ANDROID)
+    "Startup.BrowserMessageLoopStart.To.NonEmptyPaint2",
     "Startup.BrowserWindow.FirstPaint",
     "Startup.BrowserWindowDisplay",
     "Startup.FirstWebContents.MainNavigationFinished",
@@ -40,6 +41,7 @@ constexpr const char* kStartupMetrics[] = {
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_WIN)
+    "Startup.BrowserMessageLoopStartHardFaultCount",
     "Startup.Temperature",
 #endif
 };
