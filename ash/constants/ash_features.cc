f20f36fc9a84dccf4813e65a39532cd8f0cc2be8
Osama Fathy <osamafathy@google.com>
2022-11-14 15:13:05 +0000

    [OOBE][CHOOBE] Introduce CHOOBE feature flag
    Bug: b:258500863
    Change-Id: I97845c2a00b656fb08297647c5642a3f4a858cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016840
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071012}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1483,6 +1483,9 @@ BASE_FEATURE(kOnDeviceSpeechRecognition,
              "OnDeviceSpeechRecognition",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// If enabled, CHOBOE Screen will be shown during the new user onboarding flow.
+BASE_FEATURE(kOobeChoobe, "OobeChoobe", base::FEATURE_DISABLED_BY_DEFAULT);
+
 // If enabled, EULA and ARC Terms of Service screens are skipped and merged
 // into Consolidated Consent Screen.
 BASE_FEATURE(kOobeConsolidatedConsent,
@@ -2841,6 +2844,10 @@ bool IsOobeMaterialNextEnabled() {
 
 bool IsOobeNetworkScreenSkipEnabled() {
   return base::FeatureList::IsEnabled(kEnableOobeNetworkScreenSkip);
+}
+
+bool IsOobeChoobeEnabled() {
+  return base::FeatureList::IsEnabled(kOobeChoobe);
 }
 
 bool IsOobeConsolidatedConsentEnabled() {
