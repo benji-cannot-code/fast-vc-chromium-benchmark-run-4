194951dd2351e11a83d89830730ad7a9c4b97064
Paul Adedeji <pauladedeji@google.com>
2022-11-14 20:02:33 +0000

    [ntp][side-panel] Add testing suite for Customize Chrome side panel
    Change-Id: I57a19fa9eaf8f7cc5e6656c4faf0a1e7027346f8
    Bug: 1352367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021510
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071166}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
