7b6f18dae5c881f80c7b0e47b661033f445a3a26
Ahmed Mehfooz <amehfooz@chromium.org>
2022-11-14 22:32:23 +0000

    Add support to disable popups when the notification center is up
    This CL sets the visibility for the MessageCenter when the
    NotificationCenterBubble is shown so that popups are not shown
    while it is visible.
    
    Bug: b:258685449
    Change-Id: I0c4afe255750f9dc4a3799bcab3e8fbb5a339051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020741
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071260}

--- ash/system/notification_center/notification_center_test_api.cc
+++ ash/system/notification_center/notification_center_test_api.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/notification_center/notification_center_view.h"
 #include "ash/system/notification_center/stacked_notification_bar.h"
 #include "ui/events/test/event_generator.h"
+#include "ui/message_center/message_center.h"
 
 namespace ash {
 
@@ -41,6 +42,10 @@ void NotificationCenterTestApi::RemoveNotification(const std::string& id) {
 
 bool NotificationCenterTestApi::IsBubbleShown() {
   return notification_center_tray_->is_active() && GetWidget()->IsVisible();
+}
+
+bool NotificationCenterTestApi::IsPopupShown(const std::string& id) {
+  return message_center::MessageCenter::Get()->FindPopupNotificationById(id);
 }
 
 bool NotificationCenterTestApi::IsTrayShown() {
