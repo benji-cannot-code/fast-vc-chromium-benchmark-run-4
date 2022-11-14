94e6354059711332b9bed56a8ce45383bef89cfb
Harkiran Bolaria <hbolaria@google.com>
2022-11-14 17:38:56 +0000

    Add mojo message type information to LongTaskTracker.
    In the LongTaskTracker, we don't currently record enough info
    to discern whether the IPC is a message or reply, so record
    the message type directly in the Trace Event. This will allow
    the full task name information to be retrievable from the
    Trace Event.
    
    Bug: 1375779
    Change-Id: I2f290e5ae1abb6e967a1b2e3417e0d17c3d9924d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016362
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071083}

--- base/task/common/task_annotator.cc
+++ base/task/common/task_annotator.cc
@@ -64,14 +64,16 @@ const PendingTask* TaskAnnotator::CurrentTaskForThread() {
 }
 
 void TaskAnnotator::OnIPCReceived(const char* interface_name,
-                                  uint32_t (*method_info)()) {
+                                  uint32_t (*method_info)(),
+                                  bool is_response) {
   base::TaskAnnotator::LongTaskTracker* current_long_task_tracker =
       GetTLSForCurrentLongTaskTracker()->Get();
 
   if (!current_long_task_tracker)
     return;
 
-  current_long_task_tracker->SetIpcDetails(interface_name, method_info);
+  current_long_task_tracker->SetIpcDetails(interface_name, method_info,
+                                           is_response);
 }
 
 TaskAnnotator::TaskAnnotator() = default;
@@ -281,6 +283,7 @@ TaskAnnotator::LongTaskTracker::~LongTaskTracker() {
 
   if (!is_tracing_)
     return;
+
   TimeTicks task_end_time = tick_clock_->NowTicks();
   if ((task_end_time - task_start_time_) >= kMaxTaskDurationTimeDelta) {
     TRACE_EVENT_BEGIN("scheduler.long_tasks", "LongTaskTracker",
@@ -302,8 +305,10 @@ TaskAnnotator::LongTaskTracker::~LongTaskTracker() {
 }
 
 void TaskAnnotator::LongTaskTracker::SetIpcDetails(const char* interface_name,
-                                                   uint32_t (*method_info)()) {
+                                                   uint32_t (*method_info)(),
+                                                   bool is_response) {
   ipc_interface_name_ = interface_name;
+  is_response_ = is_response;
 
   if (!method_info)
     return;
@@ -322,6 +327,7 @@ void TaskAnnotator::LongTaskTracker::EmitReceivedIPCDetails(
   auto* info = ctx.event()->set_chrome_mojo_event_info();
   info->set_mojo_interface_tag(ipc_interface_name_);
   info->set_ipc_hash(ipc_hash_);
+  info->set_is_reply(is_response_);
 
   // The Native client will not build as the relevant implementation of
   // base::ModuleCache::CreateModuleForAddress is not implemented for it.
