a41c7353eb2dcd00318c69c92aaae615a004ff52
Tao Wu <wutao@chromium.org>
2022-11-14 20:54:20 +0000

    assistant: Stop observation before stop service
    We stop observing media/app_list events before we destroy
    `AssistantManagerImpl`, otherwise notification may happen any time after it is destroyed.
    
    We also add an assistant status check before calling on `assistant_` functions. We could not assume the assistant manager service is ready even the `assistant_` is valid pointer.
    
    Bug: 1383557
    Test: could not repro local, speculative fix
    Change-Id: I05fcfe01ec411b80b97c046d68d9708269f487fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024269
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071203}

--- ash/assistant/assistant_controller_impl.cc
+++ ash/assistant/assistant_controller_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/android_intent_helper.h"
+#include "ash/public/cpp/assistant/assistant_state.h"
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/mojom/assistant_volume_control.mojom.h"
 #include "ash/session/session_controller_impl.h"
@@ -91,6 +92,10 @@ void AssistantControllerImpl::SendAssistantFeedback(
     bool assistant_debug_info_allowed,
     const std::string& feedback_description,
     const std::string& screenshot_png) {
+  if (!IsAssistantReady()) {
+    return;
+  }
+
   assistant::AssistantFeedback assistant_feedback;
   assistant_feedback.assistant_debug_info_allowed =
       assistant_debug_info_allowed;
@@ -152,7 +157,7 @@ void AssistantControllerImpl::OpenUrl(const GURL& url,
                                       bool from_server) {
   // app_list search result will be opened by `OpenUrl()`. However, the
   // `assistant_` may not be ready. Show a toast to indicate it.
-  if (!assistant_) {
+  if (!IsAssistantReady()) {
     assistant_ui_controller_.ShowUnboundErrorToast();
     return;
   }
@@ -270,8 +275,9 @@ void AssistantControllerImpl::OnOutputNodeVolumeChanged(uint64_t node,
 }
 
 void AssistantControllerImpl::OnAccessibilityStatusChanged() {
-  if (!assistant_)
+  if (!IsAssistantReady()) {
     return;
+  }
 
   // The Assistant service needs to be informed of changes to accessibility
   // state so that it can turn on/off A11Y features appropriately.
@@ -280,14 +286,24 @@ void AssistantControllerImpl::OnAccessibilityStatusChanged() {
 }
 
 void AssistantControllerImpl::OnColorModeChanged(bool dark_mode_enabled) {
-  if (!assistant_)
+  if (!IsAssistantReady()) {
     return;
+  }
 
   assistant_->OnColorModeChanged(dark_mode_enabled);
 }
 
 bool AssistantControllerImpl::IsAssistantReady() const {
-  return !!assistant_;
+  if (!assistant_) {
+    return false;
+  }
+
+  if (AssistantState::Get()->assistant_status() ==
+      assistant::AssistantStatus::NOT_READY) {
+    return false;
+  }
+
+  return true;
 }
 
 void AssistantControllerImpl::NotifyConstructed() {
