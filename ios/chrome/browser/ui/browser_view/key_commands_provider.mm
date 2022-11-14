9bfdaa9489ea472b34e810b987c430d85e7b1788
Louis Romero <lpromero@google.com>
2022-11-14 15:43:09 +0000

    Remove extraneous early returns
    webStateList is a prerequisite to KeyCommandsProvider already, so no
    need to be so defensive.
    
    Bug: 1379148
    Change-Id: I06686253ad289b8f8cb2b079b0c7e3b3f892fb3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023376
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071025}

--- ios/chrome/browser/ui/browser_view/key_commands_provider.mm
+++ ios/chrome/browser/ui/browser_view/key_commands_provider.mm
@@ -318,9 +318,6 @@ using base::UserMetricsAction;
 - (void)keyCommand_showNextTab {
   RecordAction(UserMetricsAction("MobileKeyCommandShowNextTab"));
   WebStateList* webStateList = self.browser->GetWebStateList();
-  if (!webStateList)
-    return;
-
   int activeIndex = webStateList->active_index();
   if (activeIndex == WebStateList::kInvalidIndex)
     return;
@@ -338,9 +335,6 @@ using base::UserMetricsAction;
 - (void)keyCommand_showPreviousTab {
   RecordAction(UserMetricsAction("MobileKeyCommandShowPreviousTab"));
   WebStateList* webStateList = self.browser->GetWebStateList();
-  if (!webStateList)
-    return;
-
   int activeIndex = webStateList->active_index();
   if (activeIndex == WebStateList::kInvalidIndex)
     return;
