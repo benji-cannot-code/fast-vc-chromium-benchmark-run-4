134f361d86c0994552e82972eab0b4bfff056400
Mustafa Emre Acer <meacer@chromium.org>
2022-11-14 17:42:20 +0000

    Revert "Implement non-transitional IDNA processing behind a flag"
    This reverts commit 60447d27b618ac094873b2c13d9c9e727390c419.
    
    Reason for revert: Thread safety
    
    Original change's description:
    > Implement non-transitional IDNA processing behind a flag
    >
    > This CL adds a flag called UseIDNA2008NonTransitional that enables
    > non-transitional IDNA processing for URLs. When enabled, hostnames
    > that contain deviation characters will no longer be mapped and will
    > be usable as is. These deviation characters are:
    > - U+00DF LATIN SMALL LETTER SHARP S
    > - U+03C2 GREEK SMALL LETTER FINAL SIGMA
    > - U+200D ZERO WIDTH JOINER
    > - U+200C ZERO WIDTH NON-JOINER
    >
    > See https://unicode.org/reports/tr46/#Deviations for details.
    >
    > As an example, http://faß.de is presently mapped to http://fass.de
    > with IDNA 2008 transitional processing. When the flag is enabled,
    > ß will no longer be mapped to ss and the URL will remain unchanged.
    >
    > Bug: 694157
    > Change-Id: I37df9aea5520374d1fce829b0d6ee22d9bdf32ec
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018995
    > Reviewed-by: Emily Stark <estark@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1070571}
    
    Bug: 694157
    Change-Id: I3c509230dc8fad7c1e696007d4ac8f918964b995
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024340
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071085}

--- components/url_formatter/spoof_checks/idn_spoof_checker.cc
+++ components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/icu/source/i18n/unicode/regex.h"
 #include "third_party/icu/source/i18n/unicode/translit.h"
 #include "third_party/icu/source/i18n/unicode/uspoof.h"
-#include "url/url_features.h"
 
 namespace url_formatter {
 
@@ -377,10 +376,8 @@ IDNSpoofChecker::Result IDNSpoofChecker::SafeToDisplayAsUnicode(
   // chosen. On the other hand, 'fu<sharp-s>' typed or copy and pasted
   // as Unicode would be canonicalized to 'fuss' by GURL and is displayed as
   // such. See http://crbug.com/595263 .
-  if (!url::IsUsingIDNA2008NonTransitional() &&
-      deviation_characters_.containsSome(label_string)) {
+  if (deviation_characters_.containsSome(label_string))
     return Result::kDeviationCharacters;
-  }
 
   // Disallow Icelandic confusables for domains outside Iceland's ccTLD (.is).
   if (label_string.length() > 1 && top_level_domain != "is" &&
