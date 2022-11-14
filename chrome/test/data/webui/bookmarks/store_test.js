b426ea0848b2874955fe69ca2e4279a56e3b0c3a
dpapad <dpapad@chromium.org>
2022-11-14 20:01:25 +0000

    Bookmarks: Enable TrustedTypes CSP checks.
    Bug: 1098690
    Change-Id: I59ccafa59f415b18391dc72f2b1bf024f7a934d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024332
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071164}

--- chrome/test/data/webui/bookmarks/store_test.js
+++ chrome/test/data/webui/bookmarks/store_test.js
@@ -100,7 +100,7 @@ suite('bookmarks.StoreClientMixin', function() {
   });
 
   setup(function() {
-    document.body.innerHTML = '';
+    document.body.innerHTML = window.trustedTypes.emptyHTML;
 
     // Reset store instance:
     Store.setInstance(new Store());
