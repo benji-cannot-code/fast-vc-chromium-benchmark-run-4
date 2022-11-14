64849ba483c135576b57ad123972a6343ea68ec3
David Pennington <dpenning@chromium.org>
2022-11-14 17:28:50 +0000

    Add strategy for creating the overflow indicators for TabScrolling.
    Change-Id: I1254674ba2dd2f43168d5bc5f88cb85096208abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000742
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071079}

--- chrome/browser/ui/views/tabs/tab_strip_scroll_container.h
+++ chrome/browser/ui/views/tabs/tab_strip_scroll_container.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_SCROLL_CONTAINER_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_SCROLL_CONTAINER_H_
 
+#include <memory>
+
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/views/tabs/overflow_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -17,6 +19,7 @@ class ImageButton;
 }
 
 class TabStrip;
+class TabStripScrollingOverflowIndicatorStrategy;
 
 // Allows the TabStrip to be scrolled back and forth when there are more tabs
 // than can be displayed at one time. When the TabStrip is scrollable, displays
@@ -73,10 +76,9 @@ class TabStripScrollContainer : public views::View, views::ViewObserver {
   raw_ptr<views::ImageButton> leading_scroll_button_;
   raw_ptr<views::ImageButton> trailing_scroll_button_;
 
-  // The views, owned by |scroll_view_|, that indicate that there are more
-  // tabs overflowing to the left or right.
-  raw_ptr<views::View> left_overflow_indicator_;
-  raw_ptr<views::View> right_overflow_indicator_;
+  // The class handling the overflow indiciators for the scroll view.
+  std::unique_ptr<TabStripScrollingOverflowIndicatorStrategy>
+      overflow_indicator_strategy_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_SCROLL_CONTAINER_H_
