56df0014ecfd65d6cda5eb6debeede947edb11f0
Thomas Nguyen <tungnh@google.com>
2022-11-14 09:43:50 +0000

    Bind dialog input protector to it's anchor widget changed event.
    Bug: 1371215
    
    Change-Id: I39b9ea632447e1e7d4ba1b1d57f67a293c751b62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016874
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1070921}

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/views_features.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
+#include "ui/views/window/dialog_client_view.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "ui/base/win/shell.h"
@@ -799,6 +800,16 @@ void BubbleDialogDelegate::OnAnchorBoundsChanged() {
   // TODO(pbos): Reconsider whether to update the anchor when the view isn't
   // drawn.
   SizeToContents();
+
+  // We will not accept input event a short time after anchored view changed.
+  UpdateInputProtectorsTimeStamp();
+}
+
+void BubbleDialogDelegate::UpdateInputProtectorsTimeStamp() {
+  if (auto* dialog = GetDialogClientView())
+    dialog->UpdateInputProtectorTimeStamp();
+
+  GetBubbleFrameView()->UpdateInputProtectorTimeStamp();
 }
 
 gfx::Rect BubbleDialogDelegate::GetBubbleBounds() {
