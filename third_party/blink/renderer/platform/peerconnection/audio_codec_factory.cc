5ca8405b277bbe941879653074e098a32f08d601
Alessio Bazzica <alessiob@chromium.org>
2022-11-14 10:37:29 +0000

    WebRTC overrides: remove iSAC audio codec dependencies
    Needed to land https://webrtc-review.googlesource.com/c/src/+/282421
    
    Tested:
    - Custom chromium build: this CL and the WebRTC one linked above
    - Verified that iSAC does not show up any longer in the "Offer SDP"
      output on [1]
    - Made a test RTC call
    
    [1] https://webrtc.github.io/samples/src/content/peerconnection/munge-sdp/
    
    Bug: webrtc:14450
    Change-Id: Id41d4288a2e7f1256c8c8979b3ede496ca083efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013319
    Commit-Queue: Ale Bzk <alessiob@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070939}

--- third_party/blink/renderer/platform/peerconnection/audio_codec_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/audio_codec_factory.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/audio_codecs/g711/audio_encoder_g711.h"
 #include "third_party/webrtc/api/audio_codecs/g722/audio_decoder_g722.h"
 #include "third_party/webrtc/api/audio_codecs/g722/audio_encoder_g722.h"
-#include "third_party/webrtc/api/audio_codecs/isac/audio_decoder_isac.h"
-#include "third_party/webrtc/api/audio_codecs/isac/audio_encoder_isac.h"
 #include "third_party/webrtc/api/audio_codecs/opus/audio_decoder_multi_channel_opus.h"
 #include "third_party/webrtc/api/audio_codecs/opus/audio_decoder_opus.h"
 #include "third_party/webrtc/api/audio_codecs/opus/audio_encoder_multi_channel_opus.h"
@@ -74,18 +72,16 @@ struct NotAdvertisedDecoder {
 rtc::scoped_refptr<webrtc::AudioEncoderFactory>
 CreateWebrtcAudioEncoderFactory() {
   return webrtc::CreateAudioEncoderFactory<
-      webrtc::AudioEncoderOpus, webrtc::AudioEncoderIsac,
-      webrtc::AudioEncoderG722, webrtc::AudioEncoderG711,
-      NotAdvertisedEncoder<webrtc::AudioEncoderL16>,
+      webrtc::AudioEncoderOpus, webrtc::AudioEncoderG722,
+      webrtc::AudioEncoderG711, NotAdvertisedEncoder<webrtc::AudioEncoderL16>,
       NotAdvertisedEncoder<webrtc::AudioEncoderMultiChannelOpus>>();
 }
 
 rtc::scoped_refptr<webrtc::AudioDecoderFactory>
 CreateWebrtcAudioDecoderFactory() {
   return webrtc::CreateAudioDecoderFactory<
-      webrtc::AudioDecoderOpus, webrtc::AudioDecoderIsac,
-      webrtc::AudioDecoderG722, webrtc::AudioDecoderG711,
-      NotAdvertisedDecoder<webrtc::AudioDecoderL16>,
+      webrtc::AudioDecoderOpus, webrtc::AudioDecoderG722,
+      webrtc::AudioDecoderG711, NotAdvertisedDecoder<webrtc::AudioDecoderL16>,
       NotAdvertisedDecoder<webrtc::AudioDecoderMultiChannelOpus>>();
 }
 
