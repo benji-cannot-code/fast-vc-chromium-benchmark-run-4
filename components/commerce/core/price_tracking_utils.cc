4b205f811e65701558380e01defdd64f5cd7590b
Matt Jones <mdjones@chromium.org>
2022-11-14 21:35:57 +0000

    Add price tracking util to track or untrack by cluster ID
    This utility will help handle the case where a product is tracked
    via a different bookmark. Behind the scenes, the utility looks up
    an existing bookmark to untrack with.
    
    Bug: 1377115
    Change-Id: I99317bdf6aa3967cf849485992aa081bd65b7a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993870
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071231}

--- components/commerce/core/price_tracking_utils.cc
+++ components/commerce/core/price_tracking_utils.cc
@@ -78,6 +78,23 @@ bool IsProductBookmark(bookmarks::BookmarkModel* model,
   return meta && meta->has_shopping_specifics();
 }
 
+void SetPriceTrackingStateForClusterId(
+    ShoppingService* service,
+    bookmarks::BookmarkModel* model,
+    const uint64_t cluster_id,
+    bool enabled,
+    base::OnceCallback<void(bool)> callback) {
+  if (!service || !model)
+    return;
+
+  std::vector<const bookmarks::BookmarkNode*> product_bookmarks =
+      GetBookmarksWithClusterId(model, cluster_id, 1);
+  if (product_bookmarks.size() > 0) {
+    SetPriceTrackingStateForBookmark(service, model, product_bookmarks[0],
+                                     enabled, std::move(callback));
+  }
+}
+
 void SetPriceTrackingStateForBookmark(ShoppingService* service,
                                       bookmarks::BookmarkModel* model,
                                       const bookmarks::BookmarkNode* node,
@@ -149,7 +166,8 @@ void SetPriceTrackingStateForBookmark(ShoppingService* service,
 
 std::vector<const bookmarks::BookmarkNode*> GetBookmarksWithClusterId(
     bookmarks::BookmarkModel* model,
-    uint64_t cluster_id) {
+    uint64_t cluster_id,
+    size_t max_count) {
   std::vector<const bookmarks::BookmarkNode*> results =
       GetAllShoppingBookmarks(model);
 
@@ -168,6 +186,11 @@ std::vector<const bookmarks::BookmarkNode*> GetBookmarksWithClusterId(
       continue;
 
     bookmarks_with_cluster.push_back(node);
+
+    // If we're only looking for a fixed number of bookmarks, stop if we reach
+    // that number.
+    if (max_count > 0 && bookmarks_with_cluster.size() >= max_count)
+      break;
   }
 
   return bookmarks_with_cluster;
