0d8a00c997f5c07cef1d7bb4e0964ba9f7f6d3bb
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-11-14 10:28:00 +0000

    [Extensions] Separate API and chrome extension system service factories
    Separate out browser context keyed service factory instantiation for
    core chrome extension system services from that of API services at the
    //chrome/browser/extensions layer. This allows us to reduce dependencies
    from the core extensions system on individual APIs.
    
    Bug: 1381562
    Change-Id: I4ec7b0e8127b1837bbe11c278ac38b056ea82270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007014
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070934}

--- chrome/browser/extensions/browser_context_keyed_service_factories.h
+++ chrome/browser/extensions/browser_context_keyed_service_factories.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_extensions {
 
 // Ensures the existence of any BrowserContextKeyedServiceFactory provided by
-// the Chrome extensions code.
+// the Chrome extensions code or its corresponding APIs.
 void EnsureBrowserContextKeyedServiceFactoriesBuilt();
 
 }  // namespace chrome_extensions
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -222,7 +222,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/browser_context_keyed_service_factories.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/extensions/chromeos_browser_context_keyed_service_factories.h"
+#include "chrome/browser/extensions/api/chromeos_api_browser_context_keyed_service_factories.h"
 #endif
 #endif
 
