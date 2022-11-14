6af45cc4b04976c696b93ae854f81534ed453c17
Mohamed Omar <mohamedaomar@google.com>
2022-11-14 15:15:27 +0000

    Added a handler for cert_upload request
    Bug: None
    Change-Id: Ibb6387fbe2462a80a3975299b0ae0278cc2d9486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020229
    Commit-Queue: Mohamed Omar <mohamedaomar@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Mohamed Omar <mohamedaomar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071016}

--- components/policy/test_support/embedded_policy_test_server.cc
+++ components/policy/test_support/embedded_policy_test_server.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/test_support/policy_storage.h"
 #include "components/policy/test_support/request_handler_for_api_authorization.h"
 #include "components/policy/test_support/request_handler_for_auto_enrollment.h"
+#include "components/policy/test_support/request_handler_for_cert_upload.h"
 #include "components/policy/test_support/request_handler_for_check_android_management.h"
 #include "components/policy/test_support/request_handler_for_chrome_desktop_report.h"
 #include "components/policy/test_support/request_handler_for_client_cert_provisioning.h"
@@ -99,6 +100,7 @@ EmbeddedPolicyTestServer::EmbeddedPolicyTestServer()
   ResetServerState();
   RegisterHandler(std::make_unique<RequestHandlerForApiAuthorization>(this));
   RegisterHandler(std::make_unique<RequestHandlerForAutoEnrollment>(this));
+  RegisterHandler(std::make_unique<RequestHandlerForCertUpload>(this));
   RegisterHandler(
       std::make_unique<RequestHandlerForCheckAndroidManagement>(this));
   RegisterHandler(std::make_unique<RequestHandlerForChromeDesktopReport>(this));
