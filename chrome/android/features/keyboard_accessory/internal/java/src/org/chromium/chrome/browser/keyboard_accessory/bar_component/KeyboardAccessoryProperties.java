552623bec70990b948e00a577248bdde474c0895
Adem Derinel <derinel@google.com>
2022-11-14 10:23:37 +0000

    [mfill] Reland Introduce KeyboardAccessoryButtonGroupView and binding for v2
    Updates in test matchers only. See the diff between PS1 and PS2 for
    details.
    
    Original description:
    This change is the first CL to replace TabLayout with a custom button
    group. Future CLs will continue renaming tabs -> buttons after
    AUTOFILL_KEYBOARD_ACCESSORY is launched
    
    The change is required to correctly announce the buttons in the bar.
    See screenshots with the Talkback output:
    Before: http://screen/ABmsBhswaSuUB3d
    After: http://screen/W66BgpPtozA5Ztg
    
    Bug: 1247384, 1383532
    Change-Id: I967c5b6b718428197f346bca6937fc9234edae08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025662
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070932}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryProperties.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryProperties.java
@@ -5,15 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.keyboard_accessory.bar_component;
 
+import android.view.View;
+
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.tabs.TabLayout;
-
 import org.chromium.base.Callback;
 import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData.Action;
-import org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutCoordinator.TabLayoutCallbacks;
+import org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabLayoutCoordinator.SheetOpenerCallbacks;
 import org.chromium.components.autofill.AutofillSuggestion;
 import org.chromium.ui.modelutil.ListModel;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -44,8 +44,8 @@ class KeyboardAccessoryProperties {
             new WritableObjectPropertyKey<>("sheet_title");
     static final WritableBooleanPropertyKey KEYBOARD_TOGGLE_VISIBLE =
             new WritableBooleanPropertyKey("toggle_visible");
-    static final WritableObjectPropertyKey<TabLayoutBarItem> TAB_LAYOUT_ITEM =
-            new WritableObjectPropertyKey<>("tab_layout_item");
+    static final WritableObjectPropertyKey<SheetOpenerBarItem> SHEET_OPENER_ITEM =
+            new WritableObjectPropertyKey<>("sheet_opener_item");
     static final WritableObjectPropertyKey<Runnable> SHOW_KEYBOARD_CALLBACK =
             new WritableObjectPropertyKey<>("keyboard_callback");
     static final ReadableBooleanPropertyKey DISABLE_ANIMATIONS_FOR_TESTING =
@@ -58,7 +58,7 @@ class KeyboardAccessoryProperties {
     static PropertyModel.Builder defaultModelBuilder() {
         return new PropertyModel
                 .Builder(DISABLE_ANIMATIONS_FOR_TESTING, BAR_ITEMS, VISIBLE, SKIP_CLOSING_ANIMATION,
-                        BOTTOM_OFFSET_PX, TAB_LAYOUT_ITEM, KEYBOARD_TOGGLE_VISIBLE, SHEET_TITLE,
+                        BOTTOM_OFFSET_PX, SHEET_OPENER_ITEM, KEYBOARD_TOGGLE_VISIBLE, SHEET_TITLE,
                         SHOW_KEYBOARD_CALLBACK, OBFUSCATED_CHILD_AT_CALLBACK, SHOW_SWIPING_IPH)
                 .with(BAR_ITEMS, new ListModel<>())
                 .with(VISIBLE, false)
@@ -174,24 +174,24 @@ class KeyboardAccessoryProperties {
     }
 
     /**
-     * A tab layout in a {@link RecyclerView} can be destroyed and recreated whenever it is
-     * scrolled out of/into view. This wrapper allows to trigger a callback whenever the view is
-     * recreated so it can be bound to its component.
+     * A tab layout or a button group in a {@link RecyclerView} can be destroyed and recreated
+     * whenever it is scrolled out of/into view. This wrapper allows to trigger a callback whenever
+     * the view is recreated so it can be bound to its component.
      */
-    static final class TabLayoutBarItem extends BarItem {
-        private final TabLayoutCallbacks mTabLayoutCallbacks;
+    static final class SheetOpenerBarItem extends BarItem {
+        private final SheetOpenerCallbacks mSheetOpenerCallbacks;
 
-        TabLayoutBarItem(TabLayoutCallbacks tabLayoutCallbacks) {
+        SheetOpenerBarItem(SheetOpenerCallbacks sheetOpenerCallbacks) {
             super(Type.TAB_LAYOUT, null);
-            mTabLayoutCallbacks = tabLayoutCallbacks;
+            mSheetOpenerCallbacks = sheetOpenerCallbacks;
         }
 
-        void notifyAboutViewCreation(TabLayout tabs) {
-            mTabLayoutCallbacks.onTabLayoutBound(tabs);
+        void notifyAboutViewCreation(View view) {
+            mSheetOpenerCallbacks.onViewBound(view);
         }
 
-        void notifyAboutViewDestruction(TabLayout tabs) {
-            mTabLayoutCallbacks.onTabLayoutUnbound(tabs);
+        void notifyAboutViewDestruction(View view) {
+            mSheetOpenerCallbacks.onViewUnbound(view);
         }
     }
 
