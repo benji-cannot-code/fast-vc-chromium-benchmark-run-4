14287b9c173bddc7b2d633415b3ec2d27aa5437c
Xi Han <hanxi@google.com>
2022-11-15 23:12:57 +0000

    [Start] Reland: Make Start not scrollable when the content is shorter than screen.
    The original CL got reverted because it breaks the scrolling on Start
    when splitting screen happens. This CL fixes the issue by querying the
    displayMatrics() in real time rather than using the cached value. The CL
    also fixes another multiple window issue: when Feeds is disabled, we
    have to change the height of the tasks_surface_body back to
    "match_parent" to prevent the Feeds's header scrolling above the toolbar
    and can't be scrolled back.
    
    The original CL description is:
    1. Resets Start surface scroll when Feed is turned off.
    2. Sets tasks surface body's height WRAP_CONTENT and check whether tasks
       surface view is shorter than the screen size. If it is, makes it not
       scrollable by setting AppBarLayout#canDrag false.
    
    Video: http://shortn/_u5D8TNdiuN
    Video for small phone with signin promo:
    https://drive.google.com/file/d/1VnUAR4xPbLchgfTVu-6Ti-UCKWpdAQkj/view?usp=share_link
    Video for multiple window: https://drive.google.com/file/d/1VDWuXMmKl1BjzaNek8gLA55lQGauleyr/view?usp=share_link
    
    Bug: 1208486, 1383105
    Change-Id: Ie151f834063d1aa82c47081380114f33d9bec935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021417
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071891}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/TasksSurface.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/TasksSurface.java
@@ -131,4 +131,9 @@ public interface TasksSurface {
      */
     @Nullable
     TabSwitcherCustomViewManager getTabSwitcherCustomViewManager();
+
+    /**
+     * Initialize the listener to decide whether the tasks view can be draggable or scrollable.
+     */
+    void initHeaderDragListener();
 }
