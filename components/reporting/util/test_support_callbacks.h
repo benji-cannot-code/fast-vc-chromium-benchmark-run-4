4cb34d73bc00677ab20dc95e2aee3b93ea595be6
Hong Xu <xuhong@chromium.org>
2022-11-15 21:49:11 +0000

    Use remove_reference_t in TestMultiEvent
    Synced back from crrev.com/c/4026013
    
    If `ResType` is a reference, user would be risking referencing to a wild
    object if not using carefully. Removing this reference to prevent this
    hazard.
    
    Bug: b:259210325
    Change-Id: I53a347465cfc955e4d56d4316e2f7afc5cc31fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025629
    Auto-Submit: Hong Xu <xuhong@google.com>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071823}

--- components/reporting/util/test_support_callbacks.h
+++ components/reporting/util/test_support_callbacks.h
@@ -44,7 +44,7 @@ class TestMultiEvent {
   TestMultiEvent(const TestMultiEvent& other) = delete;
   TestMultiEvent& operator=(const TestMultiEvent& other) = delete;
 
-  using TupleType = std::tuple<ResType...>;
+  using TupleType = std::tuple<std::remove_reference_t<ResType>...>;
 
   [[nodiscard]] const TupleType& ref_result() {
     static_assert(
@@ -54,7 +54,6 @@ class TestMultiEvent {
     return result_.value();
   }
 
-  template <typename T = TupleType>
   [[nodiscard]] TupleType result() {
     static_assert(
         IsMovable<TupleType>(),
