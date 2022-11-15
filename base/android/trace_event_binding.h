5935d670baf3136275376cfce779064a504f11ec
Harkiran Bolaria <hbolaria@google.com>
2022-11-15 15:52:18 +0000

    Ensure all Java trace events are recorded with the Java category.
    The new low-overhead trace scenario will not include top-level
    trace events, only Java events. At the moment, we need to analyze
    how many, and which Java events should be recorded; as such,
    start by recording all events. This will extend to EarlyTraceEvent
    trace events as well, to be comprehensive.
    
    Bug: 1375779
    Change-Id: I486baab0b7750d112705bdde181618bda525f26a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027565
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071621}

--- base/android/trace_event_binding.h
+++ base/android/trace_event_binding.h
@@ -11,7 +11,7 @@ namespace android {
 namespace internal {
 
 constexpr const char kJavaTraceCategory[] = "Java";
-constexpr const char kToplevelTraceCategory[] = "toplevel";
+constexpr const char kToplevelTraceCategory[] = "toplevel,Java";
 
 }  // namespace internal
 }  // namespace android
