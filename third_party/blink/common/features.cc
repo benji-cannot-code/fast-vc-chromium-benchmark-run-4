548e07512ebd16fd8f92e57d1a4d9557fce6c795
Ian Clelland <iclelland@chromium.org>
2022-11-15 15:53:17 +0000

    Remove feature flags for two LCP experiments.
    kIncludeBackgroundSVGInLCP and kIncludeInitiallyInvisibleImagesInLCP are
    both enabled by default, and the flags can now be removed.
    
    Change-Id: I97d975ed1de7f681ff339e30311b6ab99127f737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022566
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071622}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1066,14 +1066,6 @@ BASE_FEATURE(kDelayLowPriorityRequestsAccordingToNetworkState,
              "DelayLowPriorityRequestsAccordingToNetworkState",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kIncludeInitiallyInvisibleImagesInLCP,
-             "IncludeInitiallyInvisibleImagesInLCP",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kIncludeBackgroundSVGInLCP,
-             "IncludeBackgroundSVGInLCP",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 const base::FeatureParam<int> kMaxNumOfThrottleableRequestsInTightMode{
     &kDelayLowPriorityRequestsAccordingToNetworkState,
     "MaxNumOfThrottleableRequestsInTightMode", 5};
