b960405250aa1c4c72bb9ad96b8101fcb4513ffd
Andre Le <leandre@chromium.org>
2022-11-15 21:32:58 +0000

    Notification: Fix an issue with update group child notification
    We should copy the grouping metadata from the old notification to the
    new notification when update.
    
    Fixed: 258304230
    Change-Id: Iac4bf46a9908608cad086b5351b556e76af54be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026804
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071817}

--- ui/message_center/views/notification_view_base_unittest.cc
+++ ui/message_center/views/notification_view_base_unittest.cc
@@ -823,6 +823,9 @@ TEST_F(NotificationViewBaseTest, FixedViewMode) {
 }
 
 TEST_F(NotificationViewBaseTest, SnoozeButton) {
+  MessageCenter::Get()->RemoveAllNotifications(/*by_user=*/false,
+                                               MessageCenter::RemoveType::ALL);
+
   // Create notification to replace the current one with itself.
   message_center::RichNotificationData rich_data;
   rich_data.settings_button_handler = SettingsButtonHandler::INLINE;
@@ -1075,8 +1078,11 @@ TEST_F(NotificationViewBaseTest, AppNameExtension) {
 }
 
 TEST_F(NotificationViewBaseTest, AppNameSystemNotification) {
+  MessageCenter::Get()->RemoveAllNotifications(/*by_user=*/false,
+                                               MessageCenter::RemoveType::ALL);
+
   std::u16string app_name = u"system notification";
-  message_center::MessageCenter::Get()->SetSystemNotificationAppName(app_name);
+  MessageCenter::Get()->SetSystemNotificationAppName(app_name);
   RichNotificationData data;
   data.settings_button_handler = SettingsButtonHandler::INLINE;
   auto notification = std::make_unique<Notification>(
@@ -1106,6 +1112,9 @@ TEST_F(NotificationViewBaseTest, AppNameWebNotification) {
 }
 
 TEST_F(NotificationViewBaseTest, AppNameWebAppNotification) {
+  MessageCenter::Get()->RemoveAllNotifications(/*by_user=*/false,
+                                               MessageCenter::RemoveType::ALL);
+
   const GURL web_app_url("http://example.com");
 
   NotifierId notifier_id(web_app_url, /*title=*/u"web app title");
