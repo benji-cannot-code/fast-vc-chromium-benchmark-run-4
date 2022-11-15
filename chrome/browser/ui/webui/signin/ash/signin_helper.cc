1c12efb0315e4ed6b63c3cd37b0a0e40b2924227
Roman Sorokin <rsorokin@chromium.org>
2022-11-15 14:51:37 +0000

    Nuke secondary-google-account-usage flag
    Bug: none
    Change-Id: I66a67212717575deb490fc3856ec3f4154e6b7a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020270
    Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071598}

--- chrome/browser/ui/webui/signin/ash/signin_helper.cc
+++ chrome/browser/ui/webui/signin/ash/signin_helper.cc
@@ -26,11 +26,6 @@ constexpr char kSecondaryGoogleAccountUsageHistogramName[] =
 
 using SigninRestrictionPolicyFetcher = UserCloudSigninRestrictionPolicyFetcher;
 
-// static
-bool SigninHelper::IsSecondaryGoogleAccountUsageEnabled() {
-  return base::FeatureList::IsEnabled(features::kSecondaryGoogleAccountUsage);
-}
-
 SigninHelper::ArcHelper::ArcHelper(
     bool is_available_in_arc,
     bool is_account_addition,
@@ -76,7 +71,7 @@ SigninHelper::SigninHelper(
 
   if (AccountAppsAvailability::IsArcAccountRestrictionsEnabled())
     DCHECK(arc_helper_);
-  if (!IsInitialPrimaryAccount() && IsSecondaryGoogleAccountUsageEnabled()) {
+  if (!IsInitialPrimaryAccount()) {
     DCHECK(show_signin_blocked_error_);
     restriction_fetcher_ =
         std::make_unique<UserCloudSigninRestrictionPolicyFetcher>(
@@ -91,7 +86,7 @@ SigninHelper::~SigninHelper() = default;
 
 void SigninHelper::OnClientOAuthSuccess(const ClientOAuthResult& result) {
   refresh_token_ = result.refresh_token;
-  if (!IsInitialPrimaryAccount() && IsSecondaryGoogleAccountUsageEnabled()) {
+  if (!IsInitialPrimaryAccount()) {
     restriction_fetcher_->GetSecondaryGoogleAccountUsage(
         /*access_token_fetcher=*/GaiaAccessTokenFetcher::
             CreateExchangeRefreshTokenForAccessTokenInstance(
