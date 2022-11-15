c782d4a656eadf9abe22f2c6dfc91d91ff30a9d8
Adem Derinel <derinel@google.com>
2022-11-15 07:48:34 +0000

    [Passwords] Announce keyboard accessory state depending on the bar items
    This change updates the announcement before displaying the keyboard
    accessory.
    If there are suggestion chips, the announcement is kept as is.
    If there are only buttons / tabs, "Alternative filling options
    available above the keyboard" will be announced.
    
    No UI changes are involved. The change is behind the
    AUTOFILL_KEYBOARD_ACCESSORY flag.
    
    Bug: 1314339
    Change-Id: I087eb3506af2a3ca24b80aff103ff0826cc3bfeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013287
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071485}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryMediator.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.keyboard_accessory.bar_component;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.BAR_ITEMS;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.BOTTOM_OFFSET_PX;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.DISABLE_ANIMATIONS_FOR_TESTING;
+import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.HAS_SUGGESTIONS;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.KEYBOARD_TOGGLE_VISIBLE;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.OBFUSCATED_CHILD_AT_CALLBACK;
 import static org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryProperties.SHEET_OPENER_ITEM;
@@ -99,7 +100,17 @@ class KeyboardAccessoryMediator
                 retainedItems.add(retainedItems.size(), mModel.get(SHEET_OPENER_ITEM));
             }
             mModel.get(BAR_ITEMS).set(retainedItems);
+            mModel.set(HAS_SUGGESTIONS, barHasSuggestions());
         };
+    }
+
+    private boolean barHasSuggestions() {
+        for (BarItem barItem : mModel.get(BAR_ITEMS)) {
+            if (barItem.getViewType() == BarItem.Type.SUGGESTION) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -113,6 +124,7 @@ class KeyboardAccessoryMediator
             retainedItems.add(retainedItems.size(), mModel.get(SHEET_OPENER_ITEM));
         }
         mModel.get(BAR_ITEMS).set(retainedItems);
+        mModel.set(HAS_SUGGESTIONS, barHasSuggestions());
         TraceEvent.end("KeyboardAccessoryMediator#onItemAvailable");
     }
 
@@ -263,7 +275,8 @@ class KeyboardAccessoryMediator
                 || propertyKey == SHEET_OPENER_ITEM || propertyKey == SHEET_TITLE
                 || propertyKey == SKIP_CLOSING_ANIMATION
                 || propertyKey == DISABLE_ANIMATIONS_FOR_TESTING
-                || propertyKey == OBFUSCATED_CHILD_AT_CALLBACK || propertyKey == SHOW_SWIPING_IPH) {
+                || propertyKey == OBFUSCATED_CHILD_AT_CALLBACK || propertyKey == SHOW_SWIPING_IPH
+                || propertyKey == HAS_SUGGESTIONS) {
             return;
         }
         assert false : "Every property update needs to be handled explicitly!";
