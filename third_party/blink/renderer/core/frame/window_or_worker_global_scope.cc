ff429de7d13cdf5340c91ebbf10bd4e8c777fd9d
Charlie Harrison <csharrison@chromium.org>
2022-11-15 01:17:45 +0000

    Clean up WTF's Base64Decode API
    - Allowing decode to take a StringView input removes redundant API
      surface.
    - The only callers using CharacterMatchFunctionPtr /
      kBase64ValidatePadding want the behavior of infra's forgiving base64
      algorithm. Just expose that option directly. This simplifies the
      overall API surface considerably.
    - Add comments about the actual behavior of the various options.
    
    This CL has no intended behavior change.
    
    Bug: 1383507
    
    Change-Id: I47e488caeba2f4370be0f9fe47dbd1fad186c144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021434
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071370}

--- third_party/blink/renderer/core/frame/window_or_worker_global_scope.cc
+++ third_party/blink/renderer/core/frame/window_or_worker_global_scope.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/page_dismissal_scope.h"
 #include "third_party/blink/renderer/core/frame/policy_container.h"
-#include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/messaging/message_port.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
@@ -135,8 +134,7 @@ String WindowOrWorkerGlobalScope::atob(EventTarget&,
     return String();
   }
   Vector<char> out;
-  if (!Base64Decode(encoded_string, out, IsHTMLSpace<UChar>,
-                    kBase64ValidatePadding)) {
+  if (!Base64Decode(encoded_string, out, Base64DecodePolicy::kForgiving)) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidCharacterError,
         "The string to be decoded is not correctly encoded.");
