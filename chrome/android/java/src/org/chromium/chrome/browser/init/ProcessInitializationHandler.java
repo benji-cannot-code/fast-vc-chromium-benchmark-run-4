86b0472e0cade647d1dcf3bcaabc059fd9cf72b4
Peter Wen <wnwen@chromium.org>
2022-11-15 23:48:40 +0000

    Android: Clean up some expired histograms
    Removed code emitting Android.TabPersistentStore.MetadataFileSize as it
    was no longer used and expired since M85.
    
    Removed code emitting InputMethod.MatchesSystemLanguage and
    UMA.Debug.EnableCrashUpload.DeferredStartUpAsyncTaskDuration. Both have
    been expired for multiple years.
    
    Bug: 1385002, 1384859
    Change-Id: I1a3dbe66ba402ae12bf3dae8b0d83c1e79bf16b3
    Fixed: 1385002, 1384859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025214
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071914}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -9,7 +9,6 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.SystemClock;
 import android.text.format.DateUtils;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -123,7 +122,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Handles the initialization dependences of the browser process.  This is meant to handle the
@@ -492,14 +490,10 @@ public class ProcessInitializationHandler {
              */
             private static final long LOGCAT_RELEVANCE_THRESHOLD_IN_HOURS = 12;
 
-            private long mAsyncTaskStartTime;
-
             @Override
             protected Void doInBackground() {
                 try {
                     TraceEvent.begin("ChromeBrowserInitializer.onDeferredStartup.doInBackground");
-                    mAsyncTaskStartTime = SystemClock.uptimeMillis();
-
                     initCrashReporting();
 
                     // Initialize the WebappRegistry if it's not already initialized. Must be in
@@ -528,10 +522,6 @@ public class ProcessInitializationHandler {
             protected void onPostExecute(Void params) {
                 // Must be run on the UI thread after the WebappRegistry has been completely warmed.
                 WebappRegistry.getInstance().unregisterOldWebapps(System.currentTimeMillis());
-
-                RecordHistogram.recordLongTimesHistogram(
-                        "UMA.Debug.EnableCrashUpload.DeferredStartUpAsyncTaskDuration",
-                        SystemClock.uptimeMillis() - mAsyncTaskStartTime);
             }
 
             /**
@@ -700,13 +690,5 @@ public class ProcessInitializationHandler {
             }
         }
         RecordHistogram.recordCount1MHistogram("InputMethod.ActiveCount", uniqueLanguages.size());
-
-        InputMethodSubtype currentSubtype = imm.getCurrentInputMethodSubtype();
-        Locale systemLocale = Locale.getDefault();
-        if (currentSubtype != null && currentSubtype.getLocale() != null && systemLocale != null) {
-            String keyboardLanguage = currentSubtype.getLocale().split("_")[0];
-            boolean match = systemLocale.getLanguage().equalsIgnoreCase(keyboardLanguage);
-            RecordHistogram.recordBooleanHistogram("InputMethod.MatchesSystemLanguage", match);
-        }
     }
 }
