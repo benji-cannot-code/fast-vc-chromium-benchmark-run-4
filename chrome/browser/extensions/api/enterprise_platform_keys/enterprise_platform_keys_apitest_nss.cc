c77493b3dd6ddacbc5134e2c8ab4eae71106cd9e
Jie Sheng <jiesheng@google.com>
2022-11-15 00:14:45 +0000

    Add screenplay tag to EnterprisePlatformKeys tests
    The EnterprisePlatformKeys tests cover a ChromeOS CUJ.
    To address the Code Yellow in Product Health in ChromeOS
    we want to track the CUJ coverage with screenplay.
    
    Bug: 1365899
    Change-Id: I3a728364b665a0d1caed651eb5f9e63d6016a70c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993868
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071337}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cryptohi.h>
 #include <stddef.h>
 
+#include <map>
 #include <memory>
 #include <string>
 
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_path.h"
 #include "base/json/json_writer.h"
+#include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/values.h"
@@ -272,6 +274,13 @@ class EnterprisePlatformKeysTest
            user_status() == UserStatus::MANAGED_AFFILIATED_DOMAIN;
   }
 
+  void AddScreenplayTag() {
+    static base::NoDestructor<std::map<std::string, std::string>> tag;
+    tag->insert(std::pair<std::string, std::string>(
+        "feature_id", "screenplay-f9cdeb9c-d567-4d70-a2dc-9ee4203175e6"));
+    RecordPropertyFromMap(*tag);
+  }
+
   ExtensionForceInstallMixin extension_force_install_mixin_{&mixin_host_};
 
  private:
@@ -295,10 +304,12 @@ class EnterprisePlatformKeysTest
 }  // namespace
 
 IN_PROC_BROWSER_TEST_P(EnterprisePlatformKeysTest, PRE_Basic) {
+  AddScreenplayTag();
   RunPreTest();
 }
 
 IN_PROC_BROWSER_TEST_P(EnterprisePlatformKeysTest, Basic) {
+  AddScreenplayTag();
   {
     base::RunLoop loop;
     NssServiceFactory::GetForContext(profile())
@@ -427,6 +438,13 @@ class EnterprisePlatformKeysLoginScreenTest
     return &extension_force_install_mixin_;
   }
 
+  void AddScreenplayTag() {
+    static base::NoDestructor<std::map<std::string, std::string>> tag;
+    tag->insert(std::pair<std::string, std::string>(
+        "feature_id", "screenplay-f9cdeb9c-d567-4d70-a2dc-9ee4203175e6"));
+    RecordPropertyFromMap(*tag);
+  }
+
  private:
   void SetUp() override {
     ash::platform_keys::PlatformKeysServiceFactory::GetInstance()
@@ -464,6 +482,7 @@ class EnterprisePlatformKeysLoginScreenTest
 };
 
 IN_PROC_BROWSER_TEST_P(EnterprisePlatformKeysLoginScreenTest, Basic) {
+  AddScreenplayTag();
   base::DictionaryValue config;
   config.SetStringKey("customArg",
                       BuildCustomArg(/*user_session_test=*/false,
