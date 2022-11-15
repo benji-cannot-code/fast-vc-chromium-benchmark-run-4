e06322a5c8d04c41687a25863fa964a830eb5f62
Antia Puentes <apuentes@igalia.com>
2022-11-15 22:55:16 +0000

    Rewrite GetPrimaryMainFrame comparison in terms of IsInPrimaryMainFrame (4/n)
    This CL replaces the GetPrimaryMainFrame() comparison check in favor
    of a more concise method IsInPrimaryMainFrame() in
    top_controls_slide_controller_chromeos.cc.
    
    Bug: 1268893
    Change-Id: Ibe6f2725a7abd056b12171229b9687c668143d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028106
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Antia Puentes <apuentes@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1071878}

--- chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos.cc
+++ chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos.cc
@@ -212,7 +212,7 @@ class TopControlsSlideTabObserver
                    const GURL& validated_url,
                    int error_code) override {
     if (render_frame_host->IsActive() &&
-        (render_frame_host == web_contents()->GetPrimaryMainFrame())) {
+        render_frame_host->IsInPrimaryMainFrame()) {
       UpdateBrowserControlsStateShown(/*animate=*/true);
     }
   }
