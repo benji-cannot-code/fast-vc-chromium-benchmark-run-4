cc364c4f94dbd4a2924ba58b9fd831123b0e5756
Robert Lin <robertlin@chromium.org>
2022-11-15 04:58:48 +0000

    sheriff: Disable MediaSessionVideoPictureInPictureWindowControllerBrowserTest .PlayPauseHandlersCalled
    This CL disables a failing test.
    
    Bug: 1384370
    Change-Id: I12b91fe12ee7df494574ef43ea07573b77cd9bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027463
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Owners-Override: Robert Lin <robertlin@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071450}

--- chrome/browser/picture_in_picture/video_picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/video_picture_in_picture_window_controller_browsertest.cc
@@ -1790,7 +1790,8 @@ IN_PROC_BROWSER_TEST_F(
 // calls the Media Session actions "play" and "pause" handler functions.
 IN_PROC_BROWSER_TEST_F(
     MediaSessionVideoPictureInPictureWindowControllerBrowserTest,
-    PlayPauseHandlersCalled) {
+    // TODO(crbug.com/1384370): Re-enable this test
+    DISABLED_PlayPauseHandlersCalled) {
   LoadTabAndEnterPictureInPicture(
       browser(), base::FilePath(kPictureInPictureWindowSizePage));
   content::WebContents* active_web_contents =
