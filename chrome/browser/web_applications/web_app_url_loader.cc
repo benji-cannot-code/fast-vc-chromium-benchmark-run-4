cc8ebce9cc9b8d0c03af401972affdaa8bec63d0
Antia Puentes <apuentes@igalia.com>
2022-11-15 18:50:50 +0000

    Rewrite GetPrimaryMainFrame comparison in terms of IsInPrimaryMainFrame (15/n)
    This CL replaces the GetPrimaryMainFrame() comparison check in favor
    of a more concise method IsInPrimaryMainFrame() in
    web_app_url_loader.cc.
    
    Bug: 1268893
    Change-Id: I4320feedcbf723ecaf4264629df5d4c158635168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028086
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Antia Puentes <apuentes@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1071739}

--- chrome/browser/web_applications/web_app_url_loader.cc
+++ chrome/browser/web_applications/web_app_url_loader.cc
@@ -116,7 +116,7 @@ class LoaderTask : public content::WebContentsObserver {
   }
 
   bool IsSubframeLoad(content::RenderFrameHost* render_frame_host) const {
-    return web_contents()->GetPrimaryMainFrame() != render_frame_host;
+    return !render_frame_host->IsInPrimaryMainFrame();
   }
 
   void DidFailLoad(content::RenderFrameHost* render_frame_host,
