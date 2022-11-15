cc40ac1ea83ceb3a5daa31821bcc8a1eb0d890e9
Jack Yammine <jyammine@google.com>
2022-11-15 10:54:44 +0000

    [fyfre]Add splash screen chrome logo animation
    Bug: 1347507
    Change-Id: I29d921f3b1e5de84fcabd5107fb27c6a144848bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020378
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071528}

--- chrome/browser/ui/webui/intro/intro_ui.cc
+++ chrome/browser/ui/webui/intro/intro_ui.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/intro_resources.h"
 #include "chrome/grit/intro_resources_map.h"
+#include "components/grit/components_resources.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -59,6 +60,8 @@ IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
   AddStrings(source);
 
   source->AddResourcePath("product-logo.png", IDR_PRODUCT_LOGO_128);
+  source->AddResourcePath("product-logo-animation.svg",
+                          IDR_FIRST_RUN_PRODUCT_LOGO_ANIMATION);
 
   // Unretained ok: `this` owns the handler.
   web_ui->AddMessageHandler(std::make_unique<IntroHandler>(base::BindRepeating(
