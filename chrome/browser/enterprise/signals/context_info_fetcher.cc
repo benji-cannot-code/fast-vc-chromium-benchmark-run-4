99e747d1371b67d7ebd30cc444644888973b1129
Huanpo Lin <robertlin@chromium.org>
2022-11-15 00:12:47 +0000

    Revert "[ProfileID] Add profile id to secure connect API"
    This reverts commit 2eb32db7c23e306ef2bb0465d8ae83286f8c7cde.
    
    Reason for revert: 
    A consistent failure appears after this CL.
    https://ci.chromium.org/p/chromium/builders/ci/linux-lacros-tester-rel/35045
    The initial CL may be related.
    
    Original change's description:
    > [ProfileID] Add profile id to secure connect API
    >
    > Adds the enterprise profile identifier to the GetContextInfo enterprise
    > reporting public API
    >
    > Design document: go/chrome-profile-identifier-dd
    > Screenshot: https://screenshot.googleplex.com/BFWu4KkNUvLp5pi
    >
    > Bug:1384158
    >
    > Change-Id: Id142334091ccf1e481a6fea7a500254c29e4c5db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007926
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    > Commit-Queue: Hamda Mare <hmare@google.com>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1071200}
    
    Bug: 1384158
    Change-Id: If5041062baf1ca66460845b120466601e91bb9b6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025567
    Owners-Override: Robert Lin <robertlin@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071334}

--- chrome/browser/enterprise/signals/context_info_fetcher.cc
+++ chrome/browser/enterprise/signals/context_info_fetcher.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/connectors/connectors_service.h"
-#include "chrome/browser/enterprise/identifiers/profile_id_service_factory.h"
 #include "chrome/browser/enterprise/signals/signals_utils.h"
 #include "chrome/browser/enterprise/util/affiliation.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/component_updater/pref_names.h"
-#include "components/enterprise/browser/identifiers/profile_id_service.h"
 #include "components/policy/content/policy_blocklist_service.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/site_isolation_policy.h"
@@ -55,14 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_signals {
 
 namespace {
-
-absl::optional<std::string> GetEnterpriseProfileId(Profile* profile) {
-  auto* profile_id_service =
-      enterprise::ProfileIdServiceFactory::GetForProfile(profile);
-  if (profile_id_service)
-    return profile_id_service->GetProfileId();
-  return absl::nullopt;
-}
 
 #if BUILDFLAG(IS_LINUX)
 const char** GetUfwConfigPath() {
@@ -237,7 +227,6 @@ void ContextInfoFetcher::Fetch(ContextInfoCallback callback) {
       utils::GetSafeBrowsingProtectionLevel(profile->GetPrefs());
   info.password_protection_warning_trigger =
       utils::GetPasswordProtectionWarningTrigger(profile->GetPrefs());
-  info.enterprise_profile_id = GetEnterpriseProfileId(profile);
 
 #if BUILDFLAG(IS_WIN)
   base::ThreadPool::CreateCOMSTATaskRunner({base::MayBlock()})
