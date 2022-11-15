3db0852d4dcccffe77ae996956930d60326fb779
Ningxin Hu <ningxin.hu@intel.com>
2022-11-15 01:03:00 +0000

    WebNN: Sort operators topologically for building XNNPACK Subgraph
    XNNPACK Subgraph requires the operators to be defined in topological
    order. It means when the operator 'i' depends on the output of the
    operator 'j' directly or indirectly, the operator 'j' must be defined
    before the operator 'i'.
    
    This CL implements GetOperatorsInTopologicalOrder() method that returns
    the operators in topological order by searching from the named output
    operands. It will be used to define the XNNPACK Subgraph nodes for the
    topologically sorted operators.
    
    This CL also implements the TopoSortOperatorsTest as a unit test for
    GetOperatorsInTopologicalOrder() method.
    
    Bug: 1273291
    Change-Id: I584f4cbf370aea590d04129cecfe3fb00aedb6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3977475
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1071365}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_xnnpack.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_xnnpack.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/cross_thread_persistent.h"
 #include "third_party/xnnpack/src/include/xnnpack.h"
 
@@ -34,6 +35,15 @@ class MODULES_EXPORT MLGraphXnnpack final : public MLGraph {
 
   ~MLGraphXnnpack() override;
 
+  // Return the operators in topological order by searching from the named
+  // output operands. It ensures operator 'j' appears before operator 'i' in the
+  // result, if 'i' depends on 'j'.
+
+  // The sorted operators will be used by CreateXnnSubgraphAndRuntime() that
+  // defines the subgraph Nodes for operators in topological order.
+  static HeapVector<Member<const MLOperator>>* GetOperatorsInTopologicalOrder(
+      const MLNamedOperands& named_outputs);
+
  private:
   // Post the XNNPACK subgraph building to a background thread.
   void BuildAsyncImpl(const MLNamedOperands& named_outputs,
@@ -42,7 +52,8 @@ class MODULES_EXPORT MLGraphXnnpack final : public MLGraph {
   // Build the XNNPACK subgraph off the main thread.
   static void BuildOnBackgroundThread(
       CrossThreadPersistent<MLGraphXnnpack> graph,
-      CrossThreadPersistent<MLNamedOperands> named_outputs,
+      CrossThreadPersistent<HeapVector<Member<const MLOperator>>>
+          toposorted_operators,
       CrossThreadPersistent<ScriptPromiseResolver> resolver,
       scoped_refptr<base::SequencedTaskRunner> resolver_task_runner);
 
