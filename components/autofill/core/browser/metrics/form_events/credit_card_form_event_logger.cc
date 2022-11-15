e9e6f7de51152da36d158baeb936f3e66060436a
Siyu An <siyua@chromium.org>
2022-11-15 01:01:41 +0000

    [Metadata] Add metrics to record the suggestion acceptance latency
    Latency between suggestion shown and suggestion selected.
    Logged when at least one card has metadata.
    Broken down by four sub-histograms
    
    Bug: 1313616, 1384311
    Change-Id: I5fcfaaad7952f786d7918d63353efcddae84c402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997219
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071364}

--- components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.cc
+++ components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/validation.h"
 #include "components/autofill/core/common/autofill_internals/log_message.h"
 #include "components/autofill/core/common/autofill_internals/logging_scope.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill_assistant/core/public/autofill_assistant_intent.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
@@ -42,8 +43,11 @@ CreditCardFormEventLogger::~CreditCardFormEventLogger() = default;
 
 void CreditCardFormEventLogger::OnDidFetchSuggestion(
     const std::vector<Suggestion>& suggestions,
-    bool with_offer) {
+    bool with_offer,
+    const autofill_metrics::CardMetadataLoggingContext&
+        metadata_logging_context) {
   has_eligible_offer_ = with_offer;
+  metadata_logging_context_ = metadata_logging_context;
   suggestions_.clear();
   for (const auto& suggestion : suggestions)
     suggestions_.emplace_back(suggestion);
@@ -61,6 +65,8 @@ void CreditCardFormEventLogger::OnDidShowSuggestions(
   // Also perform the logging actions from the base class:
   FormEventLoggerBase::OnDidShowSuggestions(form, field, form_parsed_timestamp,
                                             sync_state, off_the_record);
+
+  suggestion_shown_timestamp_ = AutofillTickClock::NowTicks();
 }
 
 void CreditCardFormEventLogger::OnDidSelectCardSuggestion(
@@ -98,6 +104,14 @@ void CreditCardFormEventLogger::OnDidSelectCardSuggestion(
         Log(FORM_EVENT_VIRTUAL_CARD_SUGGESTION_SELECTED_ONCE, form);
       }
       break;
+  }
+
+  // Log the latency between suggestion being shown and suggestion being
+  // selected.
+  if (metadata_logging_context_.card_metadata_available) {
+    autofill_metrics::LogCardSuggestionAcceptanceLatencyMetric(
+        AutofillTickClock::NowTicks() - suggestion_shown_timestamp_,
+        metadata_logging_context_);
   }
 }
 
--- components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.h
+++ components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.h
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/form_events/form_event_logger_base.h"
 #include "components/autofill/core/browser/metrics/form_events/form_events.h"
+#include "components/autofill/core/browser/metrics/payments/card_metadata_metrics.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/sync_utils.h"
+#include "components/autofill/core/common/autofill_tick_clock.h"
 #include "components/autofill/core/common/signatures.h"
 
 namespace autofill {
@@ -48,10 +50,14 @@ class CreditCardFormEventLogger : public FormEventLoggerBase {
     is_context_secure_ = is_context_secure;
   }
 
-  // Invoked when |suggestions| are successfully fetched. |with_offer| indicates
+  // Invoked when `suggestions` are successfully fetched. `with_offer` indicates
   // whether an offer is attached to any of the suggestion in the list.
+  // `metadata_logging_context` contains information about whether any card has
+  // a non-empty product description or art image, and whether they are shown.
   void OnDidFetchSuggestion(const std::vector<Suggestion>& suggestions,
-                            bool with_offer);
+                            bool with_offer,
+                            const autofill_metrics::CardMetadataLoggingContext&
+                                metadata_logging_context);
 
   void OnDidShowSuggestions(const FormStructure& form,
                             const AutofillField& field,
@@ -140,6 +146,10 @@ class CreditCardFormEventLogger : public FormEventLoggerBase {
   std::vector<Suggestion> suggestions_;
   bool has_eligible_offer_ = false;
   bool card_selected_has_offer_ = false;
+  autofill_metrics::CardMetadataLoggingContext metadata_logging_context_;
+
+  // Set when a list of suggestion is shown.
+  base::TimeTicks suggestion_shown_timestamp_;
 
   // Weak references.
   raw_ptr<PersonalDataManager> personal_data_manager_;
