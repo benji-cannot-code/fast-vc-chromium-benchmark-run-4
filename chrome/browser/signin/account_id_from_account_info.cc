b30020767091a7a89102c5b7a0ec09ed330d0dca
Roman Sorokin <rsorokin@chromium.org>
2022-11-15 11:52:35 +0000

    Signin: Use GetAccountId from the known_user
    The function could find a missing id: either email or gaia id
    
    This CL should fix a possible regression introduced in
    http://crrev.com/c/3925213 - when the email or gaia is empty in
    account_info
    
    Bug: 1370701, 1150434
    Change-Id: Ifdf8e8ac6edcdcb4c2fe0d5cd5ee0aabcd507401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024447
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071544}

--- chrome/browser/signin/account_id_from_account_info.cc
+++ chrome/browser/signin/account_id_from_account_info.cc
@@ -8,10 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/account_id/account_id.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/browser_process.h"
+#include "components/user_manager/known_user.h"
+#endif
+
 AccountId AccountIdFromAccountInfo(const CoreAccountInfo& account_info) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  return AccountId::FromNonCanonicalEmail(account_info.email, account_info.gaia,
-                                          AccountType::GOOGLE);
+  user_manager::KnownUser known_user(g_browser_process->local_state());
+  return known_user.GetAccountId(account_info.email, account_info.gaia,
+                                 AccountType::GOOGLE);
 #else
   if (account_info.email.empty() || account_info.gaia.empty())
     return EmptyAccountId();
--- chrome/browser/signin/account_id_from_account_info_unittest.cc
+++ chrome/browser/signin/account_id_from_account_info_unittest.cc
@@ -4,14 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/signin/account_id_from_account_info.h"
+#include "chrome/test/base/scoped_testing_local_state.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-class AccountIdFromAccountInfoTest : public testing::Test {};
 
 // Tests that AccountIdFromAccountInfo() passes along a canonicalized email to
 // AccountId.
 TEST(AccountIdFromAccountInfoTest,
      AccountIdFromAccountInfo_CanonicalizesRawEmail) {
+  ScopedTestingLocalState local_state(TestingBrowserProcess::GetGlobal());
   AccountInfo info;
   info.email = "test.email@gmail.com";
   info.gaia = "test_id";
