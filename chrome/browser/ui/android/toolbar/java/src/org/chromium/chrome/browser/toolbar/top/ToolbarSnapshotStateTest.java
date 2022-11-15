63e2a5a499fff8402e968dfb79e75d0c4d671687
Ted Choc <tedchoc@chromium.org>
2022-11-15 23:55:59 +0000

    Harden ToolbarSnapshotState to check for nulls for visual hint checks.
    This also moves the assertion to the constructor to make it easier to see where the errors are being triggered.
    
    BUG=1383684
    
    Change-Id: I7cf0e4ff2c45791c622e519bd461523d824585e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027036
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071924}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarSnapshotStateTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarSnapshotStateTest.java
@@ -12,6 +12,7 @@ import androidx.annotation.ColorInt;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -52,6 +53,11 @@ public class ToolbarSnapshotStateTest {
     public void before() {
         mDefaultColorStateList = ColorStateList.valueOf(DEFAULT_TINT);
         mDefaultToolbarSnapshotState = new ToolbarSnapshotStateBuilder().build();
+    }
+
+    @After
+    public void after() {
+        ToolbarSnapshotState.sSkipVisibleHintAssertion = false;
     }
 
     @Test
@@ -118,11 +124,13 @@ public class ToolbarSnapshotStateTest {
 
     @Test
     public void testSameUrlText_DifferentHintText() {
+        ToolbarSnapshotState.sSkipVisibleHintAssertion = true;
         ToolbarSnapshotState initialToolbarSnapshotState =
                 new ToolbarSnapshotStateBuilder().setVisibleTextPrefixHint("foo").build();
         ToolbarSnapshotState otherToolbarSnapshotState = new ToolbarSnapshotStateBuilder().build();
         Assert.assertEquals(ToolbarSnapshotDifference.NONE,
                 initialToolbarSnapshotState.getAnyDifference(otherToolbarSnapshotState));
+        ToolbarSnapshotState.sSkipVisibleHintAssertion = false;
     }
 
     @Test
