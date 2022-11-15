25bdbb1b736e212c1f3a2ac3cf86b14a8a74c7ec
Angela Czubak <acz@semihalf.com>
2022-11-15 16:24:15 +0000

    [Magnifier] Fix RewriteEvent
    Release all touches also when the event type is ui::ET_TOUCH_CANCELLED.
    It is possible that a touch is recognized as a palm after some time
    and so the touch is not really released but rather marked as cancelled.
    Either way it should be treated as nonexistent.
    Add unittest for cancelled touches to verify that magnifier treats
    the cancelled touches as if they no longer existed.
    
    Bug: b:255573949
    Change-Id: I2cfcee89cb4160803453cfd53d9d57fb450b9939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4009259
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071640}

--- ash/accessibility/magnifier/partial_magnifier_controller.cc
+++ ash/accessibility/magnifier/partial_magnifier_controller.cc
@@ -132,7 +132,8 @@ void PartialMagnifierController::OnLocatedEvent(
     SetActive(true);
   }
 
-  if (event->type() == ui::ET_TOUCH_RELEASED)
+  if (event->type() == ui::ET_TOUCH_RELEASED ||
+      event->type() == ui::ET_TOUCH_CANCELLED)
     SetActive(false);
 
   if (!is_active_)
