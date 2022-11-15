26aa5fb6cdd0321b85e62eb11fde62bf386ba83d
Hirokazu Honda <hiroh@chromium.org>
2022-11-15 11:59:27 +0000

    RTCVideoEncoder: Move VideoEncoderInfo to RTCVideoEncoder
    RTCVideoEncoder acquires webrtc::EncoderInfo in RTCVideoEncoder::Impl
    with acquiring a lock. Ideally it should be moved to RTCVideoEncoder and
    should be accessed without a lock.
    This CL moves the variable, webrtc::EncoderInfo, to RTCVideoEncoder,
    but it is still protected by lock. This is because GetEncoderInfo() is
    called by threads whose belong to different sequences.
    
    Bug: b:241349739
    Test: webrtc video call
    Change-Id: I0d36128c8710f577cc8b774e30af9c935dc6d9f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005860
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071546}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_test.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/task_environment.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -393,6 +394,7 @@ class RTCVideoEncoderTest
   base::Thread encoder_thread_;
 
  private:
+  base::test::TaskEnvironment task_environment_;
   std::unique_ptr<media::MockGpuVideoAcceleratorFactories> mock_gpu_factories_;
   std::unique_ptr<EncodedImageCallbackWrapper> callback_wrapper_;
   base::WaitableEvent idle_waiter_;
