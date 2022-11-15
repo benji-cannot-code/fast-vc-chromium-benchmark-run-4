13c0223f1a846dbe914d03a8d1f6a22478ecf67e
Nancy Wang <nancylingwang@chromium.org>
2022-11-15 23:58:17 +0000

    Remove the kAppServiceLaunchWithoutMojom flag.
    Modify publishers to use the non mojom app launch function, and remove
    the kAppServiceLaunchWithoutMojom flag.
    
    BUG=1253250
    
    Change-Id: If30ff78d1088caa4a2466763494579e4fa1ac33d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023155
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071925}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -7,10 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
-BASE_FEATURE(kAppServiceLaunchWithoutMojom,
-             "AppServiceLaunchWithoutMojom",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kAppServiceWithoutMojom,
              "AppServiceWithoutMojom",
              base::FEATURE_ENABLED_BY_DEFAULT);
