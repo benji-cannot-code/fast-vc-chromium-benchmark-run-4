ebdaa37e64772b4180daf136536aa888d22009f0
David Padlipsky <dpad@google.com>
2022-11-15 21:44:07 +0000

    Diagnostics: Prevent six pack notification when accelerators are blocked
    When accelerators are blocked, do not show the six pack deprecation
    notification as user does not expect key strokes to be processed
    normally.
    
    Bug: b/207089601
    Test: unit_test
    Change-Id: Ifd6c8366f36e62007316239fc86878c6bb83b83a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027025
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071822}

--- chrome/browser/ash/notifications/deprecation_notification_controller.cc
+++ chrome/browser/ash/notifications/deprecation_notification_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/accelerators/accelerator_controller_impl.h"
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -116,7 +117,13 @@ void DeprecationNotificationController::ShowNotification(
 
 bool DeprecationNotificationController::
     ShouldShowSixPackKeyDeprecationNotification(ui::KeyboardCode key_code) {
-  return !shown_key_notifications_.contains(key_code);
+  const auto* accelerator_controller = Shell::Get()->accelerator_controller();
+  DCHECK(accelerator_controller);
+
+  // Six pack key notification should not show if accelerators are being blocked
+  // as the user does not expect these keys to be interpreted as a six pack key.
+  return !accelerator_controller->ShouldPreventProcessingAccelerators() &&
+         !shown_key_notifications_.contains(key_code);
 }
 
 void DeprecationNotificationController::
