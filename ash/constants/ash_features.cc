892cdbe106fa16706792e535168251a7436a7f47
Hardik Goyal <hardikgoyal@google.com>
2022-11-15 22:35:18 +0000

    Add Feature Flag for USS Migration
    BUG=b:258465382
    
    Change-Id: I47d6820cbf3bc5f11019dccf88b74434e2aa71ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027673
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Auto-Submit: Hardik Goyal <hardikgoyal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071858}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -78,6 +78,12 @@ BASE_FEATURE(kAmbientModeFeature,
              "ChromeOSAmbientMode",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Allows Cryptohome to migrate existing VaultKeysets to AuthFactors and
+// UserSecretStash.
+BASE_FEATURE(kMigrateToUserSecretStash,
+             "MigrateToUserSecretStash",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 constexpr base::FeatureParam<bool> kAmbientModeCapturedOnPixelAlbumEnabled{
     &kAmbientModeFeature, "CapturedOnPixelAlbumEnabled", false};
 
@@ -2242,6 +2248,10 @@ bool DoWindowsFollowCursor() {
 
 bool Is16DesksEnabled() {
   return base::FeatureList::IsEnabled(kEnable16Desks);
+}
+
+bool IsMigrateToUserSecretStashEnabled() {
+  return base::FeatureList::IsEnabled(kMigrateToUserSecretStash);
 }
 
 bool IsAdaptiveChargingEnabled() {
--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -403,6 +403,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kEducationEnrollmentOobeFlow);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kMicMuteNotifications);
 COMPONENT_EXPORT(ASH_CONSTANTS)
+BASE_DECLARE_FEATURE(kMigrateToUserSecretStash);
+COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kDisableMessagesCrossDeviceIntegration);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kMinimumChromeVersion);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kMojoDBusRelay);
@@ -751,6 +753,7 @@ bool IsLoginScreenDeviceTrustConnectorFeatureEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsMacAddressRandomizationEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsManagedTermsOfServiceEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsMicMuteNotificationsEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsMigrateToUserSecretStashEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsMinimumChromeVersionEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNearbyKeepAliveFixEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNetworkingInDiagnosticsAppEnabled();
