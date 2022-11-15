c51b3f43ebd2919900fa55a2ade2741b10263557
Keren Zhu <kerenzhu@chromium.org>
2022-11-15 18:03:08 +0000

    Reland "views: skip invisible widgets when enforcing widget sublevels"
    This is a reland of commit 88c32aaf852e1a00f084d11bcf140a6902cc4f5f
    
    * Disable tests on MacOS 10.13 because they are not reliable (we've seen
      the same issue in WidgetTestInteractive.ChildWidgetStackAbove).
    * Add comments about the caveats of IsWindowStackedAbove() when used
      with Aura desktop widgets or on MacOS 10.13.
    
    See patchset 1..2 for changes on top of the original CL. This CL passed
    tryjobs on mac_chromium_10.13_rel_ng bot.
    
    Original change's description:
    > views: skip invisible widgets when enforcing widget sublevels
    >
    > On Mac StackAbove() an invisible widget is no-op. Therefore, skip them
    > in SublevelManager::OrderChildWidget().
    >
    > Bug: 1369180
    > Change-Id: Id56e6300b8f65ae909f5357946b7b0efc3f13a2a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924315
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1069814}
    
    Bug: 1369180
    Change-Id: I3694ed544d7f5d48ea5f9e64996f3460b9504b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021087
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071701}

--- ui/views/widget/sublevel_manager_unittest.cc
+++ ui/views/widget/sublevel_manager_unittest.cc
@@ -12,9 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/test/scoped_feature_list.h"
+#include "build/buildflag.h"
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/views_features.h"
+
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
 
 namespace views {
 
@@ -34,6 +39,11 @@ class SublevelManagerTest : public ViewsTestBase,
     set_native_widget_type(
         std::get<ViewsTestBase::NativeWidgetType>(GetParam()));
     ViewsTestBase::SetUp();
+#if BUILDFLAG(IS_MAC)
+    // MacOS 10.13 does not report window z-ordering reliably.
+    if (base::mac::IsAtMostOS10_13())
+      GTEST_SKIP();
+#endif
   }
 
   std::unique_ptr<Widget> CreateChildWidget(
@@ -56,6 +66,7 @@ class SublevelManagerTest : public ViewsTestBase,
       widget->Show();
     else
       widget->ShowInactive();
+    test::WidgetVisibleWaiter(widget.get()).Wait();
   }
 
   static std::string PrintTestName(
@@ -108,6 +119,8 @@ TEST_P(SublevelManagerTest, EnsureSublevel) {
         std::get<Widget::InitParams::Activatable>(GetParam()));
   }
 
+  ShowWidget(root);
+
   int order[] = {0, 1, 2};
   do {
     for (int i : order)
@@ -143,6 +156,7 @@ TEST_P(SublevelManagerTest, DISABLED_LevelSupersedeSublevel) {
       CreateChildWidget(root.get(), ui::ZOrderLevel::kFloatingWindow, 0,
                         std::get<Widget::InitParams::Activatable>(GetParam()));
 
+  ShowWidget(root);
   ShowWidget(high_level_widget);
   ShowWidget(low_level_widget);
 
@@ -193,6 +207,7 @@ TEST_P(SublevelManagerTest, SetSublevel) {
       CreateChildWidget(root.get(), ui::ZOrderLevel::kNormal, 2,
                         std::get<Widget::InitParams::Activatable>(GetParam()));
 
+  ShowWidget(root);
   ShowWidget(child2);
   ShowWidget(child1);
   EXPECT_TRUE(
@@ -274,6 +289,35 @@ TEST_P(SublevelManagerTest, WidgetReparent) {
   Widget::ReparentNativeView(child->GetNativeView(), nullptr);
   ShowWidget(child);
 #endif
+}
+
+// Invisible widgets should be skipped to work around MacOS where
+// stacking above them is no-op (crbug.com/1369180).
+// When they become invisible, sublevels should be respected.
+TEST_P(SublevelManagerTest, SkipInvisibleWidget) {
+  std::unique_ptr<Widget> root = CreateTestWidget();
+  std::unique_ptr<Widget> children[3];
+
+  ShowWidget(root);
+  for (int i = 0; i < 3; i++) {
+    children[i] = CreateChildWidget(
+        root.get(), ui::ZOrderLevel::kNormal, i,
+        std::get<Widget::InitParams::Activatable>(GetParam()));
+    ShowWidget(children[i]);
+
+    // Hide the second widget.
+    if (i == 1)
+      children[i]->Hide();
+  }
+
+  EXPECT_TRUE(test::WidgetTest::IsWindowStackedAbove(children[2].get(),
+                                                     children[0].get()));
+
+  ShowWidget(children[1]);
+  EXPECT_TRUE(test::WidgetTest::IsWindowStackedAbove(children[1].get(),
+                                                     children[0].get()));
+  EXPECT_TRUE(test::WidgetTest::IsWindowStackedAbove(children[2].get(),
+                                                     children[1].get()));
 }
 
 // TODO(crbug.com/1333445): We should also test NativeWidgetType::kDesktop,
