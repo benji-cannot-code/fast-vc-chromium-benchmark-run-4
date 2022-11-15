93a52c5fb9cd7461276cdaa38ecef11cae9e4648
Stephan Hartmann <stha09@googlemail.com>
2022-11-15 14:37:30 +0000

    GCC: fix use of __has_feature in base tls.h
    Testing for __has_feature and using __has_feature at the same
    time fails if __has_feature is not defined. Use HAS_FEATURE instead.
    
    Bug: 819294
    Change-Id: I01eb1a2ca8fd7bb73e5a9beb4b89d572c52f2e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027007
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071592}

--- base/allocator/dispatcher/tls.h
+++ base/allocator/dispatcher/tls.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <pthread.h>
 
-#if defined(__has_feature) && __has_feature(thread_sanitizer)
+#if HAS_FEATURE(thread_sanitizer)
 #define DISABLE_TSAN_INSTRUMENTATION __attribute__((no_sanitize("thread")))
 #else
 #define DISABLE_TSAN_INSTRUMENTATION
