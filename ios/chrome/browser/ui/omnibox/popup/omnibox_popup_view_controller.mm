cbc46f35f2dc42910ebfda73455d513e6b0f182b
Stepan Khapugin <stkhapugin@chromium.org>
2022-11-15 14:17:01 +0000

    [iOS][Omnibox] Remove the top padding in variation 1.
    Sets the top padding to 0. This prevents headers from sticking to
    the top of the inset content.
    
    Fixed: 1383981
    Change-Id: Ifbeeb8090f39a7b4c81bed832fe41505f745c845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026622
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071581}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const CGFloat kTopAndBottomPadding = 8.0;
-const CGFloat kTopPaddingVariation1 = 8.0;
+const CGFloat kTopPaddingVariation1 = 0.0;
 const CGFloat kTopPaddingVariation2 = 10.0;
 const CGFloat kTopBottomPaddingVariation2Ipad = 16.0;
 const CGFloat kFooterHeightVariation1 = 12.0;
