f3e94d4cecc3b1203ac776c56f1e749709b81797
Tommy Steimel <steimel@chromium.org>
2022-11-15 17:50:04 +0000

    PiP 1.5: Remove PiP WebRTC feature flag
    This CL removes the MediaSessionWebRTC feature flag as it's no longer
    needed as it's on by default everywhere.
    
    Bug: 1383599
    Change-Id: Ie3f02a9147eca9e0e5a015236e0674a42679ea4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024078
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071690}

--- content/browser/media/session/media_session_service_impl_browsertest.cc
+++ content/browser/media/session/media_session_service_impl_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/memory/raw_ptr.h"
-#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/media/session/media_session_impl.h"
 #include "content/browser/media/session/media_session_player_observer.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "media/base/media_content_type.h"
-#include "media/base/media_switches.h"
 #include "services/media_session/public/cpp/test/mock_media_session.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -171,6 +169,25 @@ class MediaSessionServiceImplBrowserTest : public ContentBrowserTest {
     observer.WaitForExpectedActions(expected_actions);
   }
 
+  void ExecuteScriptToSetUpWebRTCMediaSessionSync() {
+    ASSERT_TRUE(ExecJs(shell(), kSetUpWebRTCMediaSessionScript));
+    media_session::test::MockMediaSessionMojoObserver observer(*GetSession());
+
+    std::set<media_session::mojom::MediaSessionAction> expected_actions;
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kPlay);
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kPause);
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kStop);
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kSeekTo);
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kScrubTo);
+    expected_actions.insert(
+        media_session::mojom::MediaSessionAction::kToggleMicrophone);
+    expected_actions.insert(
+        media_session::mojom::MediaSessionAction::kToggleCamera);
+    expected_actions.insert(media_session::mojom::MediaSessionAction::kHangUp);
+
+    observer.WaitForExpectedActions(expected_actions);
+  }
+
  private:
   std::unique_ptr<MockMediaSessionPlayerObserver> player_;
 };
@@ -259,40 +276,7 @@ IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplBrowserTest,
   EXPECT_EQ(1u, GetService()->actions().size());
 }
 
-// Browser tests with the MediaSessionWebRTC feature enabled.
-// TODO(steimel): Merge with above tests when the feature is enabled by default.
-class MediaSessionServiceImplWebRTCBrowserTest
-    : public MediaSessionServiceImplBrowserTest {
- protected:
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    MediaSessionServiceImplBrowserTest::SetUpCommandLine(command_line);
-    feature_list_.InitAndEnableFeature(media::kMediaSessionWebRTC);
-  }
-
-  void ExecuteScriptToSetUpWebRTCMediaSessionSync() {
-    ASSERT_TRUE(ExecJs(shell(), kSetUpWebRTCMediaSessionScript));
-    media_session::test::MockMediaSessionMojoObserver observer(*GetSession());
-
-    std::set<media_session::mojom::MediaSessionAction> expected_actions;
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kPlay);
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kPause);
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kStop);
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kSeekTo);
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kScrubTo);
-    expected_actions.insert(
-        media_session::mojom::MediaSessionAction::kToggleMicrophone);
-    expected_actions.insert(
-        media_session::mojom::MediaSessionAction::kToggleCamera);
-    expected_actions.insert(media_session::mojom::MediaSessionAction::kHangUp);
-
-    observer.WaitForExpectedActions(expected_actions);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplWebRTCBrowserTest,
+IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplBrowserTest,
                        MicrophoneAndCameraStatesInitiallyUnknown) {
   EXPECT_TRUE(NavigateToURL(shell(), GetTestUrl(".", "title1.html")));
   EnsurePlayer();
@@ -305,7 +289,7 @@ IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplWebRTCBrowserTest,
   observer.WaitForCameraState(media_session::mojom::CameraState::kUnknown);
 }
 
-IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplWebRTCBrowserTest,
+IN_PROC_BROWSER_TEST_F(MediaSessionServiceImplBrowserTest,
                        MicrophoneAndCameraStatesCanBeSet) {
   EXPECT_TRUE(NavigateToURL(shell(), GetTestUrl(".", "title1.html")));
   EnsurePlayer();
