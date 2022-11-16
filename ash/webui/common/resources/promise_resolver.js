872a2125d84de9f763beba8b4ab4c83416642fbd
Rebekah Potter <rbpotter@chromium.org>
2022-11-16 17:44:03 +0000

    WebUI: Migrate promise_resolver to TypeScript and fork for CrOS closure
    Bug: 1316438
    Change-Id: I7dce7b527804a2f3fac3026a5a09931025089467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028823
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072298}

--- ash/webui/common/resources/promise_resolver.js
+++ ash/webui/common/resources/promise_resolver.js
@@ -3,7 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertNotReached} from './assert.js';
+// This file is deprecated in favor of
+// ui/webui/resources/js/promise_resolver.ts, and is maintained only for
+// legacy Closure Compiler users.
+
+import {assertNotReached} from 'chrome://resources/js/assert.js';
 
 /**
  * @fileoverview PromiseResolver is a helper class that allows creating a
