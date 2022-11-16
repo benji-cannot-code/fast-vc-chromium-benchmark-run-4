607024f795ecf518061db5bd004bd565de275e57
Jocelyn Tran <jocelyntran@google.com>
2022-11-16 18:01:13 +0000

    [Read Anything] Define default theme from system theme
    In this CL, we define the default theme for Read Anything from the
    system theme. If the user is on dark mode, the default theme option for
    Read Anything will be the Read Anything dark theme. For light mode, the
    default theme will be the Read Anything light theme.
    
    This CL also moves the creation of the Read Anything theme object from
    the model to the page handler. The model initializes the ColorIds for
    the themes and those are passed around as needed. When we reach the page
    handler, we will have access to the color provider (since its a View
    implementation) to convert the theme ColorIds to SKColors to make the
    theme object.
    
    Bug: 1266555
    Change-Id: Ia4a51b056a5ac3ead1a8a8395eced8ea047f5f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3987769
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072314}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_model.h
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_model.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "chrome/common/accessibility/read_anything.mojom.h"
-#include "third_party/skia/include/core/SkColor.h"
 #include "ui/accessibility/ax_node_id_forward.h"
 #include "ui/accessibility/ax_tree_update.h"
 #include "ui/base/models/combobox_model.h"
@@ -78,19 +77,15 @@ class ReadAnythingColorsModel : public ui::ComboboxModel {
     int icon_asset;
 
     // The foreground color, used for text and icon hints.
-    SkColor foreground;
+    ui::ColorId foreground_color_id;
 
     // The background color, used for text background.
-    SkColor background;
-
-    // The foreground color as a ColorId, used for separators.
-    ui::ColorId foreground_color_id;
+    ui::ColorId background_color_id;
   };
 
   bool IsValidColorsIndex(size_t index);
   void SetDefaultColorsIndexFromPref(size_t index);
   ColorInfo& GetColorsAt(size_t index);
-  ui::ColorId GetForegroundColorId(size_t index);
   void SetIconColorId(ui::ColorId color_id);
 
   // Simple pass-through method so Init can set the starting state colors.
@@ -221,7 +216,12 @@ class ReadAnythingModel {
         const ui::AXTreeUpdate& snapshot,
         const std::vector<ui::AXNodeID>& content_node_ids) {}
     virtual void OnReadAnythingThemeChanged(
-        read_anything::mojom::ReadAnythingThemePtr new_theme) = 0;
+        std::string& font_name,
+        double font_scale,
+        ui::ColorId foreground_color_id,
+        ui::ColorId background_color_id,
+        read_anything::mojom::Spacing line_spacing,
+        read_anything::mojom::Spacing letter_spacing) = 0;
   };
 
   ReadAnythingModel();
@@ -245,6 +245,7 @@ class ReadAnythingModel {
   double GetValidFontScale(double font_scale);
   void DecreaseTextSize();
   void IncreaseTextSize();
+  void SetIconColorIds(ui::ColorId color_id);
   void SetSelectedColorsByIndex(size_t new_index);
   void SetSelectedLineSpacingByIndex(size_t new_index);
   void SetSelectedLetterSpacingByIndex(size_t new_index);
@@ -252,7 +253,6 @@ class ReadAnythingModel {
   ReadAnythingFontModel* GetFontModel() { return font_model_.get(); }
   double GetFontScale() { return font_scale_; }
   ReadAnythingColorsModel* GetColorsModel() { return colors_model_.get(); }
-  ui::ColorId GetForegroundColorId();
   ReadAnythingLineSpacingModel* GetLineSpacingModel() {
     return line_spacing_model_.get();
   }
@@ -263,14 +263,13 @@ class ReadAnythingModel {
  private:
   void NotifyAXTreeDistilled();
   void NotifyThemeChanged();
-  void SetIconColorIds(ui::ColorId color_id);
 
   // State:
 
   // Members of read_anything::mojom::ReadAnythingTheme:
   std::string font_name_;
-  SkColor foreground_color_;
-  SkColor background_color_;
+  ui::ColorId foreground_color_id_;
+  ui::ColorId background_color_id_;
 
   // A scale multiplier for font size (internal use only, not shown to user).
   float font_scale_;
