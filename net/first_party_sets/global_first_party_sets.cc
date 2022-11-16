38cd5bc4bf6e33f40cdef4bcb529db4e49e75b05
Chris Fredrickson <cfredric@chromium.org>
2022-11-16 21:18:19 +0000

    Bail out of config computation early for no-ops.
    Change-Id: I5fab81170df0531882c08bcd693bea7702e550c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031388
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072441}

--- net/first_party_sets/global_first_party_sets.cc
+++ net/first_party_sets/global_first_party_sets.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/functional/function_ref.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/ranges/algorithm.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/types/optional_util.h"
 #include "net/base/schemeful_site.h"
@@ -236,6 +237,14 @@ void GlobalFirstPartySets::ApplyManuallySpecifiedSet(
 FirstPartySetsContextConfig GlobalFirstPartySets::ComputeConfig(
     const std::vector<SingleSet>& replacement_sets,
     const std::vector<SingleSet>& addition_sets) const {
+  if (base::ranges::all_of(replacement_sets,
+                           [](const SingleSet& set) { return set.empty(); }) &&
+      base::ranges::all_of(addition_sets,
+                           [](const SingleSet& set) { return set.empty(); })) {
+    // Nothing to do.
+    return FirstPartySetsContextConfig();
+  }
+
   // Maps a site to its new entry if it has one.
   std::vector<std::pair<SchemefulSite, absl::optional<FirstPartySetEntry>>>
       site_to_entry;
@@ -346,6 +355,12 @@ std::vector<base::flat_map<SchemefulSite, FirstPartySetEntry>>
 GlobalFirstPartySets::NormalizeAdditionSets(
     const std::vector<base::flat_map<SchemefulSite, FirstPartySetEntry>>&
         addition_sets) const {
+  if (base::ranges::all_of(addition_sets,
+                           [](const SingleSet& set) { return set.empty(); })) {
+    // Nothing to do.
+    return {};
+  }
+
   // Find all the addition sets that intersect with any given public set.
   base::flat_map<SchemefulSite, base::flat_set<size_t>> addition_set_overlaps;
   for (size_t set_idx = 0; set_idx < addition_sets.size(); set_idx++) {
