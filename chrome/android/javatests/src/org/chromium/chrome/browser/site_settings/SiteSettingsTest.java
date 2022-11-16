177266c2ece9f48bad399ed4565689daa0afb6b0
Christian Dullweber <dullweber@chromium.org>
2022-11-16 10:00:49 +0000

    Privacy Sandbox Settings: Set up 'site data' category page
    Set up site data page. The UI follows the existing content setting
    layout for binary settings. We can look into a radio button layout
    later on.
    
    Screenshot: https://crbug.com/1378703#c19
    Bug: 1378703, b/254415173
    Change-Id: I90e340bbbb9da5e89ac3a784d31d3282bd9f764d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025930
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072130}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -1261,7 +1261,16 @@ public class SiteSettingsTest {
         checkPreferencesForCategory(SiteSettingsCategory.Type.COOKIES, cookie);
     }
 
-    // TODO(b/254415173): Add tests for site data and third-party cookies page.
+    @Test
+    @SmallTest
+    @Feature({"Preferences"})
+    @EnableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)
+    public void testOnlyExpectedPreferencesSiteData() {
+        testExpectedPreferences(SiteSettingsCategory.Type.SITE_DATA, BINARY_TOGGLE_WITH_EXCEPTION,
+                BINARY_TOGGLE_WITH_EXCEPTION);
+    }
+
+    // TODO(b/254415173): Add tests for third-party cookies page.
 
     @Test
     @SmallTest
@@ -2253,11 +2262,12 @@ public class SiteSettingsTest {
                     singleCategorySettings.findPreference(SingleCategorySettings.BINARY_TOGGLE_KEY);
             assert toggle != null;
 
+            var delegate = new ChromeSiteSettingsDelegate(
+                    toggle.getContext(), Profile.getLastUsedRegularProfile());
+
             Assert.assertEquals("Preference title is not set correctly.",
                     singleCategorySettings.getResources().getString(
-                            ContentSettingsResources.getTitle(mContentSettingsType,
-                                    new ChromeSiteSettingsDelegate(toggle.getContext(),
-                                            Profile.getLastUsedRegularProfile()))),
+                            ContentSettingsResources.getTitle(mContentSettingsType, delegate)),
                     toggle.getTitle());
             assertNotNull("Enabled summary text should not be null.", toggle.getSummaryOn());
             assertNotNull("Disabled summary text should not be null.", toggle.getSummaryOff());
@@ -2265,8 +2275,10 @@ public class SiteSettingsTest {
             String summary = mIsCategoryEnabled ? toggle.getSummaryOn().toString()
                                                 : toggle.getSummaryOff().toString();
             String expected = singleCategorySettings.getResources().getString(mIsCategoryEnabled
-                            ? ContentSettingsResources.getEnabledSummary(mContentSettingsType)
-                            : ContentSettingsResources.getDisabledSummary(mContentSettingsType));
+                            ? ContentSettingsResources.getEnabledSummary(
+                                    mContentSettingsType, delegate)
+                            : ContentSettingsResources.getDisabledSummary(
+                                    mContentSettingsType, delegate));
             Assert.assertEquals(
                     "Summary text in state <" + mIsCategoryEnabled + "> does not match.", expected,
                     summary);
