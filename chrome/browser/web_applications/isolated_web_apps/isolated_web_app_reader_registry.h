1fb8a8675a6fe0df8c6a43587ea090fa25952fe3
Christian Flach <cmfcmf@chromium.org>
2022-11-16 09:46:40 +0000

    iwa: Add UMA metric measuring whether SignedWebBundleReaders are cached
    This will help us understand whether our assumption that most requests
    will be handled by a cached `SignedWebBundleReader` actually is true.
    
    Expiry is set to one year from now to allow us to monitor this metric
    over the course of launching Isolated Web Apps next year.
    
    Bug: 1382396
    Change-Id: I002f4334bd8140ef82382c2ba70f7498160c4c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013424
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072125}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_reader_registry.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_reader_registry.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
@@ -128,6 +129,9 @@ class IsolatedWebAppReaderRegistry : public KeyedService {
                     ReadResponseCallback callback);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(IsolatedWebAppReaderRegistryTest,
+                           TestConcurrentRequests);
+
   void OnIntegrityBlockRead(
       const base::FilePath& web_bundle_path,
       const web_package::SignedWebBundleId& web_bundle_id,
@@ -158,6 +162,8 @@ class IsolatedWebAppReaderRegistry : public KeyedService {
       ReadResponseCallback callback,
       base::expected<web_package::mojom::BundleResponsePtr,
                      SignedWebBundleReader::ReadResponseError> response_head);
+
+  enum class ReaderCacheState;
 
   // A thin wrapper around `base::flat_map<base::FilePath, Cache::Entry>` that
   // automatically removes entries from the cache if they have not been accessed
@@ -208,6 +214,15 @@ class IsolatedWebAppReaderRegistry : public KeyedService {
 
       const base::TimeTicks last_access() const { return last_access_; }
 
+      ReaderCacheState AsReaderCacheState() {
+        switch (state) {
+          case State::kPending:
+            return ReaderCacheState::kCachedPending;
+          case State::kReady:
+            return ReaderCacheState::kCachedReady;
+        }
+      }
+
       enum class State { kPending, kReady };
 
       State state = State::kPending;
@@ -231,6 +246,15 @@ class IsolatedWebAppReaderRegistry : public KeyedService {
     base::flat_map<base::FilePath, Entry> cache_;
     base::RepeatingTimer cleanup_timer_;
     SEQUENCE_CHECKER(sequence_checker_);
+  };
+
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  enum class ReaderCacheState {
+    kNotCached = 0,
+    kCachedReady = 1,
+    kCachedPending = 2,
+    kMaxValue = kCachedPending
   };
 
   Cache reader_cache_;
