409baf8114b0a33975169d7dfedce10ca56bc055
Ben Reich <benreich@chromium.org>
2022-11-16 04:58:25 +0000

    [FilesTrash] Remove unused trash implementation
    The previous implemented of trash is not used anymore, so this removes a
    lot of the dead code and unifies all the trash code in the
    common/trash.js file.
    
    Bug: b:259180945
    Test: browser_tests --gtest_filter=*trash*
    Change-Id: I64988b31d9497c539cb0ecb2259c11db5abe2ce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022743
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072061}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -14,7 +14,7 @@ import {DialogType, isModal} from '../../common/js/dialog_type.js';
 import {FileType} from '../../common/js/file_type.js';
 import {EntryList} from '../../common/js/files_app_entry_types.js';
 import {metrics} from '../../common/js/metrics.js';
-import {RestoreFailedType, RestoreFailedTypesUMA, RestoreFailedUMA, TrashEntry} from '../../common/js/trash.js';
+import {RestoreFailedType, RestoreFailedTypesUMA, RestoreFailedUMA, shouldMoveToTrash, TrashEntry} from '../../common/js/trash.js';
 import {str, strf, util} from '../../common/js/util.js';
 import {VolumeManagerCommon} from '../../common/js/volume_manager_types.js';
 import {xfm} from '../../common/js/xfm.js';
@@ -1158,8 +1158,7 @@ CommandHandler.deleteCommand_ = new (class extends FilesCommand {
 
     // Hide 'move-to-trash' if trash will not be used. E.g. drive or removable.
     if (event.command.id === 'move-to-trash' &&
-        (!fileManager.fileOperationManager.willUseTrash(
-             fileManager.volumeManager, entries) ||
+        (!shouldMoveToTrash(entries, fileManager.volumeManager) ||
          !fileManager.trashEnabled)) {
       event.canExecute = false;
       event.command.setHidden(true);
@@ -1185,10 +1184,10 @@ CommandHandler.deleteCommand_ = new (class extends FilesCommand {
       return;
     }
 
-    // We show undo toast rather than dialog for entries which will use trash.
+    // Trashing an item shows an "Undo" visual signal instead of a confirmation
+    // dialog.
     if (!permanentlyDelete &&
-        fileManager.fileOperationManager.willUseTrash(
-            fileManager.volumeManager, entries) &&
+        shouldMoveToTrash(entries, fileManager.volumeManager) &&
         fileManager.trashEnabled) {
       fileManager.ui.nudgeContainer.showNudge(NudgeType['TRASH_NUDGE']);
 
