2518b014d82973e41c18039f89638136de005923
Marijn Kruisselbrink <mek@chromium.org>
2022-11-16 18:25:06 +0000

    Revert "WebUI: Migrate promise_resolver to TypeScript and fork for CrOS closure"
    This reverts commit 95484c70d84be78b1936131903577d3b8ed81239.
    
    Reason for revert: Causing compile failures on https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    
    Original change's description:
    > WebUI: Migrate promise_resolver to TypeScript and fork for CrOS closure
    >
    > Bug: 1316438
    > Change-Id: I7dce7b527804a2f3fac3026a5a09931025089467
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028823
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1072298}
    
    Bug: 1316438
    Change-Id: Ieeb5551a96ed4173b1eee7b2790f897ca5be7e80
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032111
    Owners-Override: Marijn Kruisselbrink <mek@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072335}

--- ui/webui/resources/js/promise_resolver.js
+++ ui/webui/resources/js/promise_resolver.js
@@ -3,11 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file is deprecated in favor of
-// ui/webui/resources/js/promise_resolver.ts, and is maintained only for
-// legacy Closure Compiler users.
-
-import {assertNotReached} from 'chrome://resources/js/assert.js';
+import {assertNotReached} from './assert.js';
 
 /**
  * @fileoverview PromiseResolver is a helper class that allows creating a
