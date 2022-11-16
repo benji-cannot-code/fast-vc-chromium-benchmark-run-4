e92d76130956a230c6c26f92c289f843e331cb63
Marc Treib <treib@chromium.org>
2022-11-16 09:23:45 +0000

    Sync: Don't use primary accounts without a refresh token
    During signout, it can happen that the refresh token for the primary
    account gets removed before the account itself is removed.
    Before this CL, this could lead the Sync metadata not getting cleared
    properly: The missing refresh token was treated as an auth error,
    which caused Sync to go into the "paused" state, i.e. shut down but
    *without* clearing metadata. Then, when the account actually got
    removed an instant later, metadata did *not* actually get cleared due
    to crbug.com/897628.
    This CL fixes the behavior by making Sync ignore the primary account
    if it doesn't have a refresh token. This state is transient and can
    only happen during signout as described, with one exception: During
    startup, the primary account itself may be known before its refresh
    token is loaded. This case is excluded by checking
    AreRefreshTokensLoaded().
    
    For safety, this change is behind an enabled-by-default feature
    "SyncIgnoreAccountWithoutRefreshToken".
    
    Bug: 1383912
    Change-Id: I0fcfd0fde1a7a9f0712467deeefaa2491f50170c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026142
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072119}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/sync/base/features.h"
+
 #include "base/feature_list.h"
 
 namespace syncer {
@@ -143,5 +144,9 @@ BASE_FEATURE(kSyncPauseUponAnyPersistentAuthError,
 BASE_FEATURE(kSyncEnforceBookmarksCountLimit,
              "SyncEnforceBookmarksCountLimit",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kSyncIgnoreAccountWithoutRefreshToken,
+             "SyncIgnoreAccountWithoutRefreshToken",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace syncer
