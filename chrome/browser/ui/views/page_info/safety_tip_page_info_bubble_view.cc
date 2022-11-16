124264377675df818ff6af1cd8c8e8f55aeb6c68
Antia Puentes <apuentes@igalia.com>
2022-11-16 11:22:15 +0000

    Rewrite GetPrimaryMainFrame comparison in terms of IsInPrimaryMainFrame (8/n)
    This CL replaces the GetPrimaryMainFrame() comparison check in favor
    of a more concise method IsInPrimaryMainFrame() in:
     * safe_tip_page_info_bubble_view.cc
     * page_info_bubble_view_base.cc
    
    Bug: 1268893
    Change-Id: I6909eebec4ae0872081b4976275e9460216ea39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028636
    Commit-Queue: Antia Puentes <apuentes@igalia.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072157}

--- chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view.cc
@@ -190,7 +190,7 @@ void SafetyTipPageInfoBubbleView::OpenHelpCenter() {
 
 void SafetyTipPageInfoBubbleView::RenderFrameDeleted(
     content::RenderFrameHost* render_frame_host) {
-  if (render_frame_host != web_contents()->GetPrimaryMainFrame()) {
+  if (!render_frame_host->IsInPrimaryMainFrame()) {
     return;
   }
 
