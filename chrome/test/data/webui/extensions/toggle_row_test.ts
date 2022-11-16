14573be24dbba713f910e2ef22b8ca14b0830e02
dpapad <dpapad@chromium.org>
2022-11-16 01:50:49 +0000

    Extensions: Enable TrustedTypes CSP checks.
    Bug: 1098690
    Change-Id: Idf714972e87a49e655fee73556d1d9bc0ab0a621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021119
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071981}

--- chrome/test/data/webui/extensions/toggle_row_test.ts
+++ chrome/test/data/webui/extensions/toggle_row_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://extensions/extensions.js';
 
 import {ExtensionsToggleRowElement} from 'chrome://extensions/extensions.js';
+import {getTrustedHTML} from 'chrome://resources/js/static_types.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
@@ -13,7 +14,7 @@ suite('extensions-toggle-row', function() {
   let row: ExtensionsToggleRowElement;
 
   setup(function() {
-    document.body.innerHTML = `
+    document.body.innerHTML = getTrustedHTML`
       <extensions-toggle-row id="row">
         <span id="label">Description</span>
       </extensions-toggle-row>
