59edd419d7115feeca7e289a824eb0b7464c22ba
Youkichi Hosoi <youkichihosoi@chromium.org>
2022-11-16 15:43:05 +0000

    arc: Check ArcVmDataMigrationStatus to decide whether to use virtio-blk
    This CL makes ARCVM boot with virtio-blk /data if and only if:
    1) the ArcEnableVirtioBlkForData feature is set, or
    2) the ArcVmDataMigration feature is set and the migration is finished.
    
    Bug: b:258278176
    Test: enable-features=ArcVmDataMigration
    Test: enable-features=ArcEnableVirtioBlkForData
    Test: getprop ro.boot.arcvm_virtio_blk_data
    Test: ash_components_unittests --gtest_filter="ArcSession*"
    Test: ash_components_unittests --gtest_filter="ArcUtilTest.*"
    Test: ash_components_unittests --gtest_filter="ArcVmClientAdapter*"
    
    Change-Id: I5074a011380582597f6229504b40b46c5df54d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015630
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072236}

--- ash/components/arc/arc_util.cc
+++ ash/components/arc/arc_util.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdio>
 
 #include "ash/components/arc/arc_features.h"
+#include "ash/components/arc/arc_prefs.h"
+#include "ash/components/arc/session/arc_vm_data_migration_status.h"
 #include "ash/constants/app_types.h"
 #include "ash/constants/ash_switches.h"
 #include "base/bind.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/upstart/upstart_client.h"
 #include "chromeos/version/version_loader.h"
 #include "components/exo/shell_surface_util.h"
+#include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/aura/client/aura_constants.h"
@@ -426,6 +429,26 @@ void ConfigureUpstartJobs(std::deque<JobDesc> jobs,
       NOTREACHED();
       break;
   }
+}
+
+bool ShouldUseVirtioBlkData(PrefService* profile_prefs) {
+  // If kEnableVirtioBlkForData is set, force using virtio-blk /data regardless
+  // of the migration status.
+  if (base::FeatureList::IsEnabled(kEnableVirtioBlkForData))
+    return true;
+
+  // Just use virtio-fs when ARCVM /data migration is not enabled.
+  if (!base::FeatureList::IsEnabled(kEnableArcVmDataMigration))
+    return false;
+
+  ArcVmDataMigrationStatus status = static_cast<ArcVmDataMigrationStatus>(
+      profile_prefs->GetInteger(prefs::kArcVmDataMigrationStatus));
+  if (status == ArcVmDataMigrationStatus::kFinished) {
+    VLOG(1) << "ARCVM /data migration has finished";
+    return true;
+  }
+  VLOG(1) << "ARCVM /data migration hasn't finished yet. Status=" << status;
+  return false;
 }
 
 }  // namespace arc
--- ash/components/arc/test/fake_arc_session.h
+++ ash/components/arc/test/fake_arc_session.h
@@ -41,6 +41,7 @@ class FakeArcSession : public ArcSession {
       ArcClientAdapter::DemoModeDelegate* delegate) override;
   void TrimVmMemory(TrimVmMemoryCallback callback, int page_limit) override;
   void SetDefaultDeviceScaleFactor(float scale_factor) override;
+  void SetUseVirtioBlkData(bool use_virtio_blk_data) override;
 
   // To emulate unexpected stop, such as crash.
   void StopWithReason(ArcStopReason reason);
