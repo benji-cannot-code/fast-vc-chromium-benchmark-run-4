e7bef11cfd4235f4083664fec1afb5a6e748cabc
Jordan Bayles <jophba@chromium.org>
2022-11-16 04:48:31 +0000

    Manual roll of Open Screen + breaking fix
    This patch is part of an ongoing project to implement trace logging in
    Chrome for Open Screen, to help diagnose some issues with Cast
    Streaming.
    
    Included patches:
    20f3df65 Add argument support to trace logging
    4387e290 Fixes for trace logging in Chrome
    7cbec661 [Open Screen] Fix standalone sender and receiver for libavutil 57.
    6e60fdc3 [Open Screen] Finish adding GN visibility and header checks.
    
    Bug: 1376885
    Change-Id: Idf43cfbe94081b86b1657edda632a09723cb6487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029382
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072058}

--- components/openscreen_platform/trace_logging_platform.cc
+++ components/openscreen_platform/trace_logging_platform.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace openscreen {
 
-bool IsTraceLoggingEnabled(TraceCategory::Value category) {
+bool IsTraceLoggingEnabled(TraceCategory category) {
   return false;
 }
 
