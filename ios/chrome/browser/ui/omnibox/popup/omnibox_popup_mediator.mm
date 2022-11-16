976b001f0757574904c79ce5b892440b4862df2c
Stepan Khapugin <stkhapugin@chromium.org>
2022-11-16 14:00:47 +0000

    [iOS] Add more long-press actions to MV tiles.
    Implements all the long-press actions from the spec:
    - Copy
    - New Tab
    - New Incognito Tab
    - Share
    
    For sharing, introduces a new sharing delegate for the mediator,
    since sharing requires spawning and starting a SharingCoordinator.
    
    Also, renames existing UrlBlacklisted to UrlBlocklisted action.
    
    Bug: 1379333
    Change-Id: Ida400e6354aec91f61df24574f276593feb35375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021277
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072187}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.h"
 
 #import "base/feature_list.h"
+#import "base/ios/ios_util.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
@@ -510,6 +511,40 @@ const NSUInteger kMaxSuggestTileTypePosition = 15;
         NSMutableArray<UIMenuElement*>* menuElements =
             [[NSMutableArray alloc] init];
 
+        [menuElements
+            addObject:[actionFactory actionToOpenInNewTabWithURL:copyURL
+                                                      completion:nil]];
+
+        UIAction* incognitoAction =
+            [actionFactory actionToOpenInNewIncognitoTabWithURL:copyURL
+                                                     completion:nil];
+
+        if (self.allowIncognitoActions) {
+          // Disable the "Open in Incognito" option if the incognito mode is
+          // disabled.
+          incognitoAction.attributes = UIMenuElementAttributesDisabled;
+        }
+
+        [menuElements addObject:incognitoAction];
+
+        if (base::ios::IsMultipleScenesSupported()) {
+          UIAction* newWindowAction = [actionFactory
+              actionToOpenInNewWindowWithURL:copyURL
+                              activityOrigin:
+                                  WindowActivityContentSuggestionsOrigin];
+          [menuElements addObject:newWindowAction];
+        }
+
+        [menuElements addObject:[actionFactory actionToCopyURL:copyURL]];
+
+        [menuElements addObject:[actionFactory actionToShareWithBlock:^{
+                        [weakSelf.sharingDelegate
+                            popupMediator:weakSelf
+                                 shareURL:copyURL
+                                    title:carouselItem.title
+                               originView:view];
+                      }]];
+
         [menuElements addObject:[actionFactory actionToRemoveWithBlock:^{
                         [weakSelf removeMostVisitedForURL:copyURL
                                          withCarouselItem:weakItem];
@@ -541,7 +576,7 @@ const NSUInteger kMaxSuggestTileTypePosition = 15;
     return;
   }
   base::RecordAction(
-      base::UserMetricsAction("MostVisited_UrlBlacklisted_Omnibox"));
+      base::UserMetricsAction("MostVisited_UrlBlocklisted_Omnibox"));
   [self blockMostVisitedURL:URL];
   [self.carouselItemConsumer deleteCarouselItem:carouselItem];
 }
