c99c46b52aa2aab8adf3e15fe8f945da0238755f
K. Moon <kmoon@chromium.org>
2022-11-16 15:15:58 +0000

    Drop workarounds for PDFium's libpng
    Drops workarounds for separating Chromium's and PDFium's copies of
    libpng. PDFium no longer uses its own copy of libpng.
    
    Fixed: 1130821
    Change-Id: Ica77b4109448366b8711c4f649b287b362d3462b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021576
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Commit-Queue: Leon Scroggins <scroggo@google.com>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072225}

--- third_party/libpng/pnglibconf.h
+++ third_party/libpng/pnglibconf.h
@@ -226,13 +226,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PNG_USER_CHUNK_MALLOC_MAX 4000000L
 /* end of chromium settings */
 
-/* chromium prefixing */
-/*
- * This is necessary to build multiple copies of libpng.  We need this while pdfium builds
- * its own copy of libpng.
- */
-#define PNG_PREFIX
-#include "pngprefix.h"
-/* end of chromium prefixing */
-
 #endif /* PNGLCONF_H */
