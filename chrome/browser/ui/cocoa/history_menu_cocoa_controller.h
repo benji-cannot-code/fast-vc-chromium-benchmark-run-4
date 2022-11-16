677dde60eaeae8a06ca66a7b673ddb24c4e52908
Ali Hijazi <ahijazi@chromium.org>
2022-11-16 09:38:52 +0000

    DanglingPtr: Mark all of them as untriaged [9/N]
    This patch is a no-op with all the common build flags.
    
    Add the "DanglingUntriaged" raw_ptr annotation. It indicates a raw_ptr
    becomes dangling, and it should be triaged/fixed. This will also disable
    dangling protection for those pointers, once enabled.
    
    These were identified by running the mac-rel CQ bot with DPD activated
    (both build + runtime here: https://crrev.com/c/3941825)
    
    Bug: 1291138
    Change-Id: I2311840240ccde63b4247324820ffb80e13ea858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028009
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072123}

--- chrome/browser/ui/cocoa/history_menu_cocoa_controller.h
+++ chrome/browser/ui/cocoa/history_menu_cocoa_controller.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // creation and maintenance of the menu happens in the Bridge.
 @interface HistoryMenuCocoaController : NSObject<NSMenuDelegate> {
  @private
-  raw_ptr<HistoryMenuBridge> _bridge;  // weak; owns us
+  raw_ptr<HistoryMenuBridge, DanglingUntriaged> _bridge;  // weak; owns us
 }
 
 - (instancetype)initWithBridge:(HistoryMenuBridge*)bridge;
