ba2eedcba722d299f96dee153cdae0fef3c9d12e
Mohsen Izadi <mohsen@chromium.org>
2022-11-16 01:30:01 +0000

    Fix presentation callback in WidgetBase::ForceRedraw()
    `WidgetBase::ForceRedraw()` forces a redraw and then uses a presentation
    callback to determine when the redraw is completed by being presented on
    the screen. However, the callback it currently uses might be called even
    if the submitted frame fails to present successfully on the screen. This
    CL fixes it by using callbacks that are called after a frame is
    successfully presented on the screen.
    
    Bug: 1378612
    Change-Id: I6b101b76b1299ca79885ce1f97f1e6aefdca8044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025997
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071968}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/screen_info.h"
 #include "ui/gfx/geometry/dip_util.h"
-#include "ui/gfx/presentation_feedback.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "third_party/blink/renderer/platform/widget/compositing/android_webview/synchronous_layer_tree_frame_sink.h"
@@ -86,7 +85,7 @@ static const char kRenderer[] = "Renderer";
 
 void OnDidPresentForceDrawFrame(
     mojom::blink::Widget::ForceRedrawCallback callback,
-    const gfx::PresentationFeedback& feedback) {
+    base::TimeTicks presentation_timestamp) {
   std::move(callback).Run();
 }
 
@@ -335,7 +334,7 @@ scheduler::WidgetScheduler* WidgetBase::WidgetScheduler() {
 
 void WidgetBase::ForceRedraw(
     mojom::blink::Widget::ForceRedrawCallback callback) {
-  LayerTreeHost()->RequestPresentationTimeForNextFrame(
+  LayerTreeHost()->RequestSuccessfulPresentationTimeForNextFrame(
       base::BindOnce(&OnDidPresentForceDrawFrame, std::move(callback)));
   LayerTreeHost()->SetNeedsCommitWithForcedRedraw();
 
