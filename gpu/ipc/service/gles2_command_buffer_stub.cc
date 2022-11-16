20f6464eb5f7ab13fd3896bb2285df4e27dc9308
Vasiliy Telezhnikov <vasilyt@chromium.org>
2022-11-16 20:57:47 +0000

    Add GLES2ExternalFramebuffer functionality
    NaCL plugins draw to default framebuffer, but it has to be offscreen.
    Right now it uses TakeFrontBuffer/ReturnFrontBuffer flow and legacy
    mailboxes. This CL implements GLES2ExternalFramebuffer which allows
    clients to attach shared image to the default framebuffer.
    
    This CL doesn't expose it's as IPC yet.
    
    The implementation guarded by feature flag
    (kPPAPISharedImagesSwapChain) and command line flag
    (kDisablePPAPISharedImagesSwapChain) that can veto the feature
    flag. Extra command line is needed for enterprise policy.
    
    Bug: 1342033
    Change-Id: I11e0075c7aedae54bee43691a56edc111943c5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014159
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072426}

--- gpu/ipc/service/gles2_command_buffer_stub.cc
+++ gpu/ipc/service/gles2_command_buffer_stub.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/gpu_fence.h"
 #include "ui/gfx/gpu_fence_handle.h"
+#include "ui/gfx/switches.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_implementation.h"
@@ -72,7 +73,9 @@ GLES2CommandBufferStub::GLES2CommandBufferStub(
                         sequence_id,
                         stream_id,
                         route_id),
-      gles2_decoder_(nullptr) {}
+      gles2_decoder_(nullptr),
+      use_shared_images_swapchain_for_ppapi_(
+          features::UseSharedImagesSwapChainForPPAPI()) {}
 
 GLES2CommandBufferStub::~GLES2CommandBufferStub() = default;
 
@@ -465,6 +468,20 @@ void GLES2CommandBufferStub::OnReturnFrontBuffer(const Mailbox& mailbox,
                                                  bool is_lost) {
   // No need to pull texture updates.
   gles2_decoder_->ReturnFrontBuffer(mailbox, is_lost);
+}
+
+void GLES2CommandBufferStub::OnSetDefaultFramebufferSharedImage(
+    const Mailbox& mailbox,
+    int samples_count,
+    bool preserve,
+    bool needs_depth,
+    bool needs_stencil) {
+  if (!use_shared_images_swapchain_for_ppapi_)
+    return;
+
+  // No need to pull texture updates.
+  gles2_decoder_->SetDefaultFramebufferSharedImage(
+      mailbox, samples_count, preserve, needs_depth, needs_stencil);
 }
 
 void GLES2CommandBufferStub::CreateGpuFenceFromHandle(
