262376d4e10076326ccb468e10edd3b4c67dea1c
Prateek R. Patil <tfpc@google.com>
2022-11-16 18:29:22 +0000

    Adds localized error status to policy page UI.
    Occurs when the policy is not read on a managed device and will
    display this in the status box for device or user policy.
    
    Bug: b:250846114
    Change-Id: I72730515b22537340c51f0b997cf3673f0dd079c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026583
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Prateek Patil <tfpc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1072340}

--- components/policy/resources/webui/status_box.js
+++ components/policy/resources/webui/status_box.js
@@ -126,6 +126,11 @@ export class StatusBoxElement extends CustomElement {
           status.lastCloudReportSentTimestamp + ' (' +
               status.timeSinceLastCloudReportSent + ')');
     }
+
+    if (status.error) {
+      this.setLabelAndShow_(
+          '.error', loadTimeData.getString('statusErrorManagedNoPolicy'));
+    }
   }
 }
 
