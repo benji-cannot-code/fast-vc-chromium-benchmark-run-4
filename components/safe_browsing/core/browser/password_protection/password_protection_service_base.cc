33fa3ae13f6a7f59a4b3034ea30da6ce5cfee753
Joemer Ramos <joemerramos@chromium.org>
2022-11-16 01:47:59 +0000

    [iOS] Re-enable Password Protection EG tests
    The EG tests were breaking since we weren't taking artificial unsafe
    urls into account when calling CanGetReputationOfURL(). Additionally,
    original flakiness was caused by the test not waiting for the popup so
    we added a small timer. EG tests also no longer look like they break on
    iPad 14.4 and iPhone 14.5+.
    
    Fixed: 1221635, 1213616
    Change-Id: Idb10a20c28a2a64b33934e5dc11907d125a725c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026863
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071980}

--- components/safe_browsing/core/browser/password_protection/password_protection_service_base.cc
+++ components/safe_browsing/core/browser/password_protection/password_protection_service_base.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/browser/db/database_manager.h"
 #include "components/safe_browsing/core/browser/password_protection/password_protection_request.h"
 #include "components/safe_browsing/core/browser/sync/sync_utils.h"
+#include "components/safe_browsing/core/browser/verdict_cache_manager.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/utils.h"
 #include "google_apis/google_api_keys.h"
@@ -100,6 +101,9 @@ PasswordProtectionServiceBase::~PasswordProtectionServiceBase() {
 
 // static
 bool PasswordProtectionServiceBase::CanGetReputationOfURL(const GURL& url) {
+  if (VerdictCacheManager::has_artificial_unsafe_url()) {
+    return true;
+  }
   if (!safe_browsing::CanGetReputationOfUrl(url)) {
     return false;
   }
--- ios/chrome/browser/safe_browsing/password_protection_egtest.mm
+++ ios/chrome/browser/safe_browsing/password_protection_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <string>
 
 #import "base/ios/ios_util.h"
+#import "base/test/ios/wait_util.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "ios/chrome/browser/passwords/password_manager_app_interface.h"
 #import "ios/chrome/browser/ui/passwords/password_constants.h"
@@ -66,13 +67,12 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
   config.additional_args.push_back(
       std::string("--mark_as_allowlisted_for_phish_guard=") +
       _allowlistedURL.spec());
-  // TODO(crbug.com/1221635) testPasswordReuseDetectionWarning is disabled
-  /*
+
   if ([self isRunningTest:@selector(testPasswordReuseDetectionWarning)]) {
     // Use commandline args to save a fake phishing cached verdict.
     config.additional_args.push_back(
         std::string("--mark_as_phish_guard_phishing=") + _phishingURL.spec());
-  }*/
+  }
 
   return config;
 }
@@ -121,23 +121,22 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 
 // Tests that password protection UI is shown when saved password is reused on
 // phishing site.
-// TODO(crbug.com/1221635) This fails on iPad 14.4 and iPhone 14.5+
-- (void)DISABLED_testPasswordReuseDetectionWarning {
+- (void)testPasswordReuseDetectionWarning {
   // PhishGuard is only available on iOS 14.0 or above.
 
   [ChromeEarlGrey loadURL:_phishingURL];
   [ChromeEarlGrey waitForWebStateContainingText:kInputPage];
 
   [self typePasswordIntoWebInput];
-
-  [[EarlGrey selectElementWithMatcher:PasswordProtectionMatcher()]
-      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:PasswordProtectionMatcher()
+                                  timeout:base::test::ios::
+                                              kWaitForUIElementTimeout];
 }
 
 // Tests that password protection UI is not shown when saved password is reused
 // on safe site.
-// TODO(crbug.com/1213616) Test is flaky.
-- (void)DISABLED_testPasswordProtectionNotShownForAllowListedURL {
+- (void)testPasswordProtectionNotShownForAllowListedURL {
   // PhishGuard is only available on iOS 14.0 or above.
 
   [ChromeEarlGrey loadURL:_allowlistedURL];
