5d3b6cccf1c905b506a60c127a64edab63b54a53
Ewann Pelle <ewannpv@chromium.org>
2022-11-16 18:46:30 +0000

    [iOS][3/N] Adds Pinned Tabs UI
    This CL Adds the following elements to the Pinned Tabs view:
    - PinnedTabsLayout: a layout object that organizes item.
    - PinnedCell: a cell to display pinned items.
    
    Bug: 1382015
    Change-Id: I12af9e54ed3a70f1d050739e1895bebc6e7c5a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013304
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Petro Akzhygitov <pakzhygitov@google.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072353}

--- ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_view_controller.mm
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_view_controller.h"
 
+#import "ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_cell.h"
 #import "ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_constants.h"
+#import "ios/chrome/browser/ui/tab_switcher/pinned_tabs/pinned_tabs_layout.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher_item.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 
