006cf1802f483d859f89c25a67fca2dcb014a387
Mohannad Farrag <aymanm@google.com>
2022-11-16 18:40:20 +0000

    Expose mActiveRequestCount in CronetEngine's API
    * This will expose the count of in-flight requests that hasn't reached the `onSucceeded`, `onCancelled` or `onFailed` stage. This would be useful in implementing `idle resource` for testing purposes.
    * all implementations support the count of in-flight requests and will return a non-negative integer when called.
    * Added tests to cover different branches for the Java Engine including `onSucceeded`, `onCancelled` and `onFailed`. In which all of those should affect the number of in-flight requests when a request reach such a state.
    * Verify that mActiveRequestCount is incremented exactly after `start()` has been called and decremented exactly before one of the final state callbacks has been called.
    
    Change-Id: I5d535bb4b54825477743604b44747d98073e2e97
    Bug: 251352880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945035
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1072348}

--- components/cronet/android/java/src/org/chromium/net/impl/JavaCronetEngine.java
+++ components/cronet/android/java/src/org/chromium/net/impl/JavaCronetEngine.java
@@ -199,6 +199,11 @@ public final class JavaCronetEngine extends CronetEngineBase {
     }
 
     @Override
+    public int getActiveRequestCount() {
+        return mActiveRequestCount.get();
+    }
+
+    @Override
     public void bindToNetwork(long networkHandle) {
         throw new UnsupportedOperationException(
                 "The multi-network API is not supported by the Java implementation "
--- components/cronet/android/test/javatests/src/org/chromium/net/TestUrlRequestCallback.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/TestUrlRequestCallback.java
@@ -54,12 +54,19 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
     // Whether to permit calls on the network thread.
     private boolean mAllowDirectExecutor;
 
+    // Whether to stop the executor thread after reaching a terminal method.
+    // Terminal methods are (onSucceeded, onFailed or onCancelled)
+    private boolean mBlockOnTerminalState;
+
     // Conditionally fail on certain steps.
     private FailureType mFailureType = FailureType.NONE;
     private ResponseStep mFailureStep = ResponseStep.NOTHING;
 
     // Signals when request is done either successfully or not.
     private final ConditionVariable mDone = new ConditionVariable();
+
+    // Hangs the calling thread until a terminal method has started executing.
+    private final ConditionVariable mWaitForTerminalToStart = new ConditionVariable();
 
     // Signaled on each step when mAutoAdvance is false.
     private final ConditionVariable mStepBlock = new ConditionVariable();
@@ -141,6 +148,19 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
         fillInExecutorThread();
     }
 
+    /**
+     * This blocks the callback executor thread once it has reached a final state callback.
+     * In order to continue execution, this method must be called again and providing {@code false}
+     * to continue execution.
+     * @param blockOnTerminalState the state to set for the executor thread
+     */
+    public void setBlockOnTerminalState(boolean blockOnTerminalState) {
+        mBlockOnTerminalState = blockOnTerminalState;
+        if (!blockOnTerminalState) {
+            mDone.open();
+        }
+    }
+
     public void setAutoAdvance(boolean autoAdvance) {
         mAutoAdvance = autoAdvance;
     }
@@ -156,6 +176,14 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
 
     public void blockForDone() {
         mDone.block();
+    }
+
+    /**
+     * Blocks the calling thread until one of the final states has been called.
+     * This is called before the callback has finished executed.
+     */
+    public void waitForTerminalToStart() {
+        mWaitForTerminalToStart.block();
     }
 
     public void waitForNextStep() {
@@ -260,6 +288,8 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
 
         mResponseStep = ResponseStep.ON_SUCCEEDED;
         mResponseInfo = info;
+        mWaitForTerminalToStart.open();
+        if (mBlockOnTerminalState) mDone.block();
         openDone();
         maybeThrowCancelOrPause(request);
     }
@@ -290,6 +320,8 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
         mResponseStep = ResponseStep.ON_FAILED;
         mOnErrorCalled = true;
         mError = error;
+        mWaitForTerminalToStart.open();
+        if (mBlockOnTerminalState) mDone.block();
         openDone();
         maybeThrowCancelOrPause(request);
     }
@@ -305,6 +337,8 @@ public class TestUrlRequestCallback extends UrlRequest.Callback {
 
         mResponseStep = ResponseStep.ON_CANCELED;
         mOnCanceledCalled = true;
+        mWaitForTerminalToStart.open();
+        if (mBlockOnTerminalState) mDone.block();
         openDone();
         maybeThrowCancelOrPause(request);
     }
