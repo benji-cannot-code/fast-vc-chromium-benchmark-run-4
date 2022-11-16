a40a655a905707c2c87f7aa99427d4f557040133
Colin Blundell <blundell@chromium.org>
2022-11-16 17:02:48 +0000

    [GPU] Remove stale include from gl_texture_image_backing.cc
    Clarifies which GLImage subclass is actually used in this file.
    
    Bug: 1332554
    Change-Id: I31b17a009f7a4d04e5a7bf97acc0e82fec065948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029145
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072269}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_fence.h"
 #include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_image_native_pixmap.h"
-#include "ui/gl/gl_image_shared_memory.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/scoped_binders.h"
