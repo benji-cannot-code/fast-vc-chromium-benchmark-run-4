ae79028e7fb3c3862259a7ab19732cfedc1e0b44
Rebekah Potter <rbpotter@chromium.org>
2022-11-16 21:02:13 +0000

    WebUI: Migrate promise_resolver to TypeScript and fork for CrOS closure
    Bug: 1316438
    Change-Id: I6b1df29a357e311266965d54cb4ebfdcaf352b40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032328
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072434}

--- chrome/test/data/pdf/annotations_feature_enabled_test.ts
+++ chrome/test/data/pdf/annotations_feature_enabled_test.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {AnnotationTool, SaveRequestType, ViewerInkHostElement} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 
 import {waitFor} from './test_util.js';
@@ -451,6 +452,7 @@ chrome.test.runTests([
   async function testSaveAfterAnnotationMode() {
     const saveData = await viewer.getCurrentControllerForTesting()!.save(
         SaveRequestType.EDITED);
+    assert(saveData);
     chrome.test.assertTrue(saveData.editModeForTesting!);
     chrome.test.succeed();
   },
