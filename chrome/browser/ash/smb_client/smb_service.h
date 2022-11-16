9c7c3b2579a214bf271a148ca3cd8929f0896d37
Josh Simmons <simmonsjosh@google.com>
2022-11-16 03:23:11 +0000

    SMB: Add test for saved and preconfigured URL validity
    Previously, preconfigured (via policy) and persisted (previously saved
    by the user) share URLs were not validated before passing on to smbfs.
    
    Now that they are validated, add unit tests to ensure their correctness
    is validated during MountInternal().
    
    This also cleans up the naming of the MountInternal callbacks to make
    it clearer which entry point they're being invoked from.
    
    Bug: 1260281
    Test: unit_tests --gtest_filter="*SmbService*"
    Change-Id: Ica1bb5a9f5bbb7aa4317874eaf0a5caab3400493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812820
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1072021}

--- chrome/browser/ash/smb_client/smb_service.h
+++ chrome/browser/ash/smb_client/smb_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/files/file.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system_info.h"
@@ -118,16 +119,20 @@ class SmbService : public KeyedService,
       SmbFsShare::MounterCreationCallback callback);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(SmbServiceWithSmbfsTest, MountInvalidSaved);
+  FRIEND_TEST_ALL_PREFIXES(SmbServiceWithSmbfsTest, MountInvalidPreconfigured);
+
   using MountInternalCallback =
       base::OnceCallback<void(SmbMountResult result,
                               const base::FilePath& mount_path)>;
 
-  // Callback passed to MountInternal().
-  void MountInternalDone(MountResponse callback,
-                         const SmbShareInfo& info,
-                         bool should_open_file_manager_after_mount,
-                         SmbMountResult result,
-                         const base::FilePath& mount_path);
+  // Callback passed to MountInternal() when mounts are initiated
+  // (generally by user interaction) via Mount().
+  void OnUserInitiatedMountDone(MountResponse callback,
+                                const SmbShareInfo& info,
+                                bool should_open_file_manager_after_mount,
+                                SmbMountResult result,
+                                const base::FilePath& mount_path);
 
   // Mounts an SMB share with url |share_url| using either smbprovider or smbfs
   // based on feature flags.
@@ -156,8 +161,17 @@ class SmbService : public KeyedService,
       const std::vector<SmbShareInfo>& saved_smbfs_shares,
       const std::vector<SmbUrl>& preconfigured_shares);
 
+  // Sets the callback passed to MountInternal() when a saved or
+  // preconfigured share mount request is made during setup.
+  void SetRestoredShareMountDoneCallbackForTesting(
+      MountInternalCallback callback);
+
   // Mounts a saved (smbfs) SMB share with details |info|.
   void MountSavedSmbfsShare(const SmbShareInfo& info);
+
+  // Handles the response from attempting to mount a previously saved share.
+  void OnMountSavedSmbfsShareDone(SmbMountResult result,
+                                  const base::FilePath& mount_path);
 
   // Mounts a preconfigured (by policy) SMB share with path |share_url|. The
   // share is mounted with empty credentials.
@@ -239,6 +253,7 @@ class SmbService : public KeyedService,
 
   base::OnceClosure setup_complete_callback_;
   SmbFsShare::MounterCreationCallback smbfs_mounter_creation_callback_;
+  MountInternalCallback restored_share_mount_done_callback_;
 };
 
 }  // namespace smb_client
