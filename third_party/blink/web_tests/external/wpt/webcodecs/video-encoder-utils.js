4eca8c41ed016efb55eba673573d06f76cfc76b7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-11-17 21:22:41 +0000

    Import wpt@67eef80ad87e294d3d2378f4353ad004eb0ec6f1
    Using wpt-import in Chromium 0c73021e0bba41179b1bffb470617a241973ee54.
    With Chromium commits locally applied on WPT:
    661771ae92 "URLPattern: Escape pattern baseURL values."
    dbbb2be5a4 "Clip inner border for local attachment background"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ia28c8c4d9486c848771f06e1ddc5b8490e05d81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031649
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1073048}

--- third_party/blink/web_tests/external/wpt/webcodecs/video-encoder-utils.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/video-encoder-utils.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 async function checkEncoderSupport(test, config) {
+  assert_equals("function", typeof VideoEncoder.isConfigSupported);
   let supported = false;
   try {
     const support = await VideoEncoder.isConfigSupported(config);
