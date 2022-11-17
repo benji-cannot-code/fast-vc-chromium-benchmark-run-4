09056ec56fde4468ab3bf736fb3bd7a96387aa4f
Morten Stenshorne <mstensho@chromium.org>
2022-11-17 17:02:59 +0000

    Split NGBoxFragmentBuilder::HasChildBreakInside().
    Distinguish between already having broken inside, and having to to break
    inside before generating a fragment.
    
    HasChildBreakInside() was really only useful at the end of layout. Yet,
    it was called in the middle of layout when handling a column spanner.
    Effectively rename HasChildBreakInside() to ShouldBreakInside() (and
    document that it should only be called at the end of layout). If all the
    caller wants to know is whether we've already broken inside, we should
    call (the new) HasInsertedChildBreak() instead. The spanner handling
    code now does that. This distinction doesn't matter in practice at that
    point, though, since the two condition that we're now skipping -
    last_inline_break_token_ or has_subsequent_children_ - can only happen
    to inline layout or grid layout, respectively. And we cannot encounter
    column spanners in such cases. But it's more correct this way.
    
    This CL shouldn't cause any behavior changes, but is in preparation for
    an upcoming CL, which is about better prevention of parallel flow around
    column spanners.
    
    Change-Id: Ic9b3313306665325d66b993bd5011236fdf77153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031750
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072871}

--- third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
@@ -543,7 +543,7 @@ const NGLayoutResult* NGBoxFragmentBuilder::ToBoxFragment(
     if (!break_token_) {
       if (last_inline_break_token_)
         child_break_tokens_.push_back(std::move(last_inline_break_token_));
-      if (DidBreakSelf() || HasChildBreakInside())
+      if (DidBreakSelf() || ShouldBreakInside())
         break_token_ = NGBlockBreakToken::Create(this);
     }
 
@@ -701,7 +701,7 @@ void NGBoxFragmentBuilder::AdjustFixedposContainingBlockForInnerMulticols() {
 #if DCHECK_IS_ON()
 
 void NGBoxFragmentBuilder::CheckNoBlockFragmentation() const {
-  DCHECK(!HasChildBreakInside());
+  DCHECK(!ShouldBreakInside());
   DCHECK(!HasInflowChildBreakInside());
   DCHECK(!DidBreakSelf());
   DCHECK(!has_forced_break_);
