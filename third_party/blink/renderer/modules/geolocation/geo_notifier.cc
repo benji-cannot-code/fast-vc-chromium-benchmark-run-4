1e8b49988a13991fbe709279b6bb6757d3ffb75f
Reilly Grant <reillyg@chromium.org>
2022-11-17 23:56:37 +0000

    geolocation: Remove expired timeout histograms
    These histograms expired on 2020-02-01.
    
    Bug: 1384844
    Change-Id: I62f24e6de6fa019ec103d7f52b69745dd5fa59ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032696
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073121}

--- third_party/blink/renderer/modules/geolocation/geo_notifier.cc
+++ third_party/blink/renderer/modules/geolocation/geo_notifier.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/geolocation/geo_notifier.h"
 
-#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_position_options.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -29,11 +28,6 @@ GeoNotifier::GeoNotifier(Geolocation* geolocation,
       use_cached_position_(false) {
   DCHECK(geolocation_);
   DCHECK(success_callback_);
-
-  base::UmaHistogramCustomTimes("Geolocation.Timeout",
-                                base::Milliseconds(options_->timeout()),
-                                base::Milliseconds(1), base::Minutes(10),
-                                /* buckets = */ 20);
 }
 
 void GeoNotifier::Trace(Visitor* visitor) const {
@@ -135,11 +129,6 @@ void GeoNotifier::TimerFired(TimerBase*) {
         nullptr, MakeGarbageCollected<GeolocationPositionError>(
                      GeolocationPositionError::kTimeout, "Timeout expired"));
   }
-
-  base::UmaHistogramCustomTimes("Geolocation.TimeoutExpired",
-                                base::Milliseconds(options_->timeout()),
-                                base::Milliseconds(1), base::Minutes(10),
-                                /* buckets = */ 20);
 
   geolocation_->RequestTimedOut(this);
 }
