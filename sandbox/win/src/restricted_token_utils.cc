c87104946c9add84f62eabc1be541a2e6fff0273
James Forshaw <forshaw@chromium.org>
2022-11-17 18:35:40 +0000

    [Windows] Remove old sandbox job handling for Windows 7 and 8.
    This CL removes legacy job functionality in the Windows sandbox used on
    Windows 7 and 8. There's no longer a need to check for nested job
    support and the job can be assigned during process creation. This was
    also an opportunity to clean up the Job class to remove unused features.
    
    Bug: 1385495
    Change-Id: I82a1ffbeacde0a62a4f95cf8a1e26f766f470fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032432
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072931}

--- sandbox/win/src/restricted_token_utils.cc
+++ sandbox/win/src/restricted_token_utils.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/windows_version.h"
-#include "sandbox/win/src/job.h"
 #include "sandbox/win/src/restricted_token.h"
 #include "sandbox/win/src/sandbox_nt_util.h"
 #include "sandbox/win/src/sandbox_utils.h"
