accd0a8d2b73a2033c796c9cddd91b694e218a53
Benedikt Meurer <bmeurer@chromium.org>
2022-11-17 09:09:06 +0000

    [DevTools] Remove expired histograms in DevTools.BackgroundService.
    The following histograms expired with M99:
    - DevTools.BackgroundService.ClearEvents
    - DevTools.BackgroundService.GetEvents
    - DevTools.BackgroundService.LogEvent
    
    We haven't been using and/or monitoring these histograms for the purpose
    of DevTools, so let's just remove them from the codebase.
    
    Bug: chromium:927726
    Fixed: chromium:1384733, chromium:988721, chromium:1268001
    Change-Id: I53a846ef4d6159741e98d7dd55ef125436abe831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030499
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072682}

--- content/browser/devtools/devtools_background_services_context_impl.cc
+++ content/browser/devtools/devtools_background_services_context_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/guid.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/observer_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
@@ -32,16 +31,6 @@ std::string CreateEntryKeyPrefix(devtools::proto::BackgroundService service) {
 
 std::string CreateEntryKey(devtools::proto::BackgroundService service) {
   return CreateEntryKeyPrefix(service) + base::GenerateGUID();
-}
-
-void DidLogServiceEvent(blink::ServiceWorkerStatusCode status) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.LogEvent", status);
-}
-
-void DidClearServiceEvents(blink::ServiceWorkerStatusCode status) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.ClearEvents", status);
 }
 
 constexpr devtools::proto::BackgroundService ServiceToProtoEnum(
@@ -163,8 +152,6 @@ void DevToolsBackgroundServicesContextImpl::DidGetUserData(
     blink::ServiceWorkerStatusCode status) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.GetEvents", status);
-
   std::vector<devtools::proto::BackgroundServiceEvent> events;
 
   if (status != blink::ServiceWorkerStatusCode::kOk) {
@@ -198,7 +185,7 @@ void DevToolsBackgroundServicesContextImpl::ClearLoggedBackgroundServiceEvents(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   service_worker_context_->ClearUserDataForAllRegistrationsByKeyPrefix(
-      CreateEntryKeyPrefix(service), base::BindOnce(&DidClearServiceEvents));
+      CreateEntryKeyPrefix(service), base::DoNothing());
 }
 
 void DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEvent(
@@ -236,7 +223,7 @@ void DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEvent(
   service_worker_context_->StoreRegistrationUserData(
       service_worker_registration_id, blink::StorageKey(origin),
       {{CreateEntryKey(event.background_service()), event.SerializeAsString()}},
-      base::BindOnce(&DidLogServiceEvent));
+      base::DoNothing());
 
   NotifyEventObservers(std::move(event));
 }
