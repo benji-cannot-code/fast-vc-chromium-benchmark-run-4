7141db328ec2ccf7097f10cc69fe6ae3553b46d7
Han Gao <hangaoy@google.com>
2022-11-17 23:12:14 +0000

    Add browser test to verify Cumulative Layout Shift one second gap
    Add cumulative_layout_shift_browsertest to add coverage on CLS
    Normalization one second gap. In the simple-block-movement.html file,
    we perform the first layout shift with 160px movement, and we pause
    the program for one second and then perform another layout shift
    with 340px movement.
    
    Add watcher_entry_record list in ScoreWatcher to record startTime and
    score pair for each Performance Entry, and extract the list in browser
    test to help us verify the gap between two layout shifts are at least
    one second. Based on the rule of CLS Normalization, these two layout
    shifts should be aggregated in different session windows, so we also
    verify that the CLS values in UMA and UKM are the same as the second
    layout shift score.
    
    Bug: 1370548
    Change-Id: If82935c06bb9c4c0655a297d497dcdedb23b2e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3982899
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Han Gao <hangaoy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073098}

--- third_party/blink/web_tests/external/wpt/layout-instability/resources/util.js
+++ third_party/blink/web_tests/external/wpt/layout-instability/resources/util.js
@@ -35,6 +35,9 @@ computeExpectedScore = (impactRegionArea, moveDistance) => {
   return impactFraction * distanceFraction;
 };
 
+// An list to record all the entries with startTime and score.
+let watcher_entry_record = [];
+
 // An object that tracks the document cumulative layout shift score.
 // Usage:
 //
@@ -62,6 +65,7 @@ ScoreWatcher = function() {
     list.getEntries().forEach(entry => {
       this.lastEntry = entry;
       this.score += entry.value;
+      watcher_entry_record.push({startTime: entry.startTime, score: entry.value});
       if (!entry.hadRecentInput)
         this.scoreWithInputExclusion += entry.value;
       this.resolve();
@@ -75,6 +79,10 @@ ScoreWatcher.prototype.checkExpectation = function(expectation) {
     assert_equals(this.score, expectation.score);
   if (expectation.sources)
     check_sources(expectation.sources, this.lastEntry.sources);
+};
+
+ScoreWatcher.prototype.get_entry_record = function() {
+  return watcher_entry_record;
 };
 
 check_sources = (expect_sources, actual_sources) => {
