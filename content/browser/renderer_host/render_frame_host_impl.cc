ecf4e26e1a49ac869c5394c802332ce1c7cb26c0
William Liu <liuwilliam@chromium.org>
2022-11-17 20:42:04 +0000

    Retire BrowserPluginPopupMenuHelper (Mac)
    This CL moves the ShowPopupMenu call from WebContentsImpl to
    WebContentsViewChildFrame so we can retire BrowserPluginPopupMenuHelper
    permanently.
    
    Bug: 533069
    Change-Id: I017250ae77d90b02a66f223c5fe7b5acbd24318d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021635
    Auto-Submit: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073014}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -6800,9 +6800,7 @@ void RenderFrameHostImpl::ShowPopupMenu(
     return;
   }
 
-  if (delegate()->ShowPopupMenu(this, &popup_client, bounds, item_height,
-                                font_size, selected_item, &menu_items,
-                                right_aligned, allow_multiple_selection)) {
+  if (delegate()->ShowPopupMenu(this, bounds)) {
     return;
   }
 
