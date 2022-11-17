beca96b2e8f4d5168296f7565edbceff376382f0
Sorin Jianu <sorin@chromium.org>
2022-11-17 21:17:28 +0000

    Updater: kill processes before starting integration tests.
    An integration test must start with a clean snapshot of processes.
    
    Bug: 1385131
    Change-Id: I4468119350da75bc58bd9c3ec0b490faf2d127b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034788
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073039}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -97,6 +97,7 @@ class IntegrationTest : public ::testing::Test {
                          true,    // enable_thread_id
                          true,    // enable_timestamp
                          false);  // enable_tickcount
+    CleanProcesses();
     EXPECT_TRUE(WaitForUpdaterExit());
     Clean();
     ExpectClean();
