410c927f5eaede70e433e76a48600dc30784abc9
David Bertoni <dbertoni@chromium.org>
2022-11-17 02:32:08 +0000

    [Extensions] Move IsInServiceWorker to common utils handler.
    Bug: None
    Change-Id: I80d2e4da909a22b67f13a60150524808b6ab417c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025360
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072573}

--- extensions/renderer/resources/set_icon.js
+++ extensions/renderer/resources/set_icon.js
@@ -4,9 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 var exceptionHandler = require('uncaught_exception_handler');
-var natives = requireNative('setIcon');
-var SetIconCommon = natives.SetIconCommon;
-var inServiceWorker = natives.IsInServiceWorker();
+var SetIconCommon = requireNative('setIcon').SetIconCommon;
+var inServiceWorker = requireNative('utils').isInServiceWorker();
 
 function loadImagePathForServiceWorker(path, callback, failureCallback) {
   let fetchPromise = fetch(path);
