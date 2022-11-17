0ef27c85eccd696a80c15c7459e4703d15125ea7
Rubin Deliallisi <rubindl@chromium.org>
2022-11-17 13:00:37 +0000

    Privacy Sandbox Settings: Replace Privacy Sandbox V3 with V4 in settings
    Remove the old Privacy Sandbox entry point in the Privacy and Security
    settings.
    Add a row and empty fragment for the replacement page.
    Move the "incognito_lock" preference to the privacy section
    
    Bug: 1378703, b/254414134, b/254411458, b/254412034
    Change-Id: Icc0133b05b381ad4bb8c53222ded418ece98118b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028241
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072748}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.privacy_sandbox;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -39,12 +38,6 @@ public class PrivacySandboxSettingsFragment extends PrivacySandboxSettingsBaseFr
     public static final String FLOC_PREFERENCE = "floc_page";
 
     private @PrivacySandboxReferrer int mPrivacySandboxReferrer;
-
-    public static CharSequence getStatusString(Context context) {
-        return context.getString(PrivacySandboxBridge.isPrivacySandboxEnabled()
-                        ? R.string.privacy_sandbox_status_enabled
-                        : R.string.privacy_sandbox_status_disabled);
-    }
 
     /**
      * Initializes all the objects related to the preferences page.
