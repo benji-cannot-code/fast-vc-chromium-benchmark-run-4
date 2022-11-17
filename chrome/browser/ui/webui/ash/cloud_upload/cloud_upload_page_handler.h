178933a6544d01e973be3fe8b8dfac851f152720
Austin Tankiang <austinct@chromium.org>
2022-11-17 11:01:52 +0000

    [Office Upload] Add Office PWA install page
    This page is only displayed if the PWA has not already been installed,
    so also add a mojo function to query the install status. The actual
    installation of the PWA will be done in a follow-up CL.
    
    Bug: b:251046341
    Change-Id: Ie932805a05967218cf3a95cf79beef97deba8698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025563
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072719}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.h
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.h
@@ -11,10 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload.mojom-shared.h"
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload.mojom.h"
+#include "chrome/browser/web_applications/web_app_id.h"
+#include "components/webapps/browser/install_result_code.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
+
+class Profile;
 
 namespace ash::cloud_upload {
 
@@ -25,6 +29,7 @@ class CloudUploadPageHandler : public mojom::PageHandler {
   using RespondAndCloseCallback =
       base::OnceCallback<void(mojom::UserAction action)>;
   CloudUploadPageHandler(
+      Profile* profile,
       mojom::DialogArgsPtr args,
       mojo::PendingReceiver<mojom::PageHandler> pending_page_handler,
       RespondAndCloseCallback callback);
@@ -36,9 +41,12 @@ class CloudUploadPageHandler : public mojom::PageHandler {
 
   // mojom::PageHandler:
   void GetDialogArgs(GetDialogArgsCallback callback) override;
+  void IsOfficePWAInstalled(IsOfficePWAInstalledCallback callback) override;
   void RespondAndClose(mojom::UserAction action) override;
 
  private:
+  Profile* profile_;
+
   mojom::DialogArgsPtr dialog_args_;
 
   mojo::Receiver<PageHandler> receiver_;
