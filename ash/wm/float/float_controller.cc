3b837f6c25cd156a5928cd8cf56e071f650e3daf
Judy Wang <shidi@chromium.org>
2022-11-17 01:41:00 +0000

    FloatWindow: Add metrics for how many windows are floated per session.
    Add metrics to count the number of floating windows per session.
    This counts how many times any window enters float state.
    if the same window are floated twice in the same session, it will
    get recorded twice.
    
    Test: Manual and Unit test.
    Change-Id: I272ebcfc85be1d97602729663b8e9f303b0d0d17
    Fixed: b/258473416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020910
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072553}

--- ash/wm/float/float_controller.cc
+++ ash/wm/float/float_controller.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace/workspace_event_handler.h"
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/metrics/histogram_functions.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "chromeos/ui/wm/constants.h"
 #include "chromeos/ui/wm/window_util.h"
@@ -193,7 +194,11 @@ FloatController::FloatController() {
     OnRootWindowAdded(root);
 }
 
-FloatController::~FloatController() = default;
+FloatController::~FloatController() {
+  // Record how many windows are floated per session.
+  base::UmaHistogramCounts100(kFloatWindowCountsPerSessionHistogramName,
+                              floated_window_counter_);
+}
 
 // static
 gfx::Rect FloatController::GetPreferredFloatWindowClamshellBounds(
@@ -638,6 +643,11 @@ void FloatController::FloatImpl(aura::Window* window) {
 
   if (!desk->is_active())
     HideFloatedWindow(window);
+
+  // Update floated window counts.
+  // Note that if the same window gets floated 2 times in the same session, it's
+  // counted as 2 floated windows.
+  ++floated_window_counter_;
 
   if (!tablet_mode_observation_.IsObserving())
     tablet_mode_observation_.Observe(Shell::Get()->tablet_mode_controller());
