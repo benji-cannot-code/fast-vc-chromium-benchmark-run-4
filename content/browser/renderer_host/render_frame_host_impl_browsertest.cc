5fbe97bef50ef83c3f183e24da63036fd806df3e
Jesse McKenna <jessemckenna@google.com>
2022-11-17 22:53:19 +0000

    subframe-reuse: remove experiment arms
    The subframe-reuse feature was enabled by default in
    crrev.com/c/3811446, which landed in version 107.0.5292.0.
    
    At least a milestone has passed since the feature was enabled by default
    (per the guidance in go/finch-best-practices), so the experiment code
    can start to be removed.
    
    This change removes the various experiment arms whose behavior was
    not enabled by default (as they did not perform as well as the
    default "constant delay" behavior). The base::Feature itself will
    be cleaned up in a subsequent CL.
    
    Bug: 894253
    Change-Id: If33d1c35d2ed21359bdfd9457b90f63f48a5d66d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007041
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073093}

--- content/browser/renderer_host/render_frame_host_impl_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_impl_browsertest.cc
@@ -5530,10 +5530,7 @@ class RenderFrameHostImplSubframeReuseBrowserTest
     : public RenderFrameHostImplBrowserTest {
  public:
   RenderFrameHostImplSubframeReuseBrowserTest() {
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kSubframeShutdownDelay, {{"type", "constant-long"}});
-    EXPECT_EQ(features::kSubframeShutdownDelayTypeParam.Get(),
-              features::SubframeShutdownDelayType::kConstantLong);
+    scoped_feature_list_.InitAndEnableFeature(features::kSubframeShutdownDelay);
   }
 
  protected:
