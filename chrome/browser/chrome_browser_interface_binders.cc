6afae329deeb767be10e80b2753dff8ca9322cd6
Mike Wasserman <msw@chromium.org>
2022-11-17 21:12:31 +0000

    Revert "Instrument side panel reading list for WebUI help bubbles"
    This reverts commit 4a29a22f306ecb2856cc6b4431dcf54b05e8f473.
    
    Reason for revert: compile failures; eg. https://ci.chromium.org/ui/p/chromium/builders/ci/mac-archive-dbg/20483
    
    Original change's description:
    > Instrument side panel reading list for WebUI help bubbles
    >
    > This instruments the reading list with the HelpBubbleHandlerFactory. In a follow-up CL, we'll be adding a tutorial that requires element identifiers to be passed to the side panel reading list.
    >
    > Bug: 1385113
    > Change-Id: Ibfb9290a57e0b75d95e5eb6aecceb73173bfd41a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025630
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1072945}
    
    Bug: 1385113
    Change-Id: If68f9599e7bcda0ea2ec535ad633ae7a331dccd6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4035807
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Owners-Override: Mike Wasserman <msw@google.com>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073033}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -957,7 +957,7 @@ void PopulateChromeWebUIFrameBinders(
 
   RegisterWebUIControllerInterfaceBinder<
       help_bubble::mojom::HelpBubbleHandlerFactory, InternalsUI,
-      settings::SettingsUI, ReadingListUI>(map);
+      settings::SettingsUI>(map);
 
 #if !defined(OFFICIAL_BUILD)
   RegisterWebUIControllerInterfaceBinder<foo::mojom::FooHandler, NewTabPageUI>(
--- chrome/browser/ui/webui/side_panel/reading_list/reading_list_ui.h
+++ chrome/browser/ui/webui/side_panel/reading_list/reading_list_ui.h
@@ -13,12 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_load_timer.h"
 #include "chrome/common/accessibility/read_anything.mojom.h"
 #include "components/commerce/core/mojom/shopping_list.mojom.h"
-#include "components/user_education/webui/help_bubble_handler.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/webui/mojo_bubble_web_ui_controller.h"
-#include "ui/webui/resources/cr_components/help_bubble/help_bubble.mojom.h"
 
 class BookmarksPageHandler;
 class ReadAnythingPageHandler;
@@ -32,8 +30,7 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
                       public reading_list::mojom::PageHandlerFactory,
                       public side_panel::mojom::BookmarksPageHandlerFactory,
                       public read_anything::mojom::PageHandlerFactory,
-                      public shopping_list::mojom::ShoppingListHandlerFactory,
-                      public help_bubble::mojom::HelpBubbleHandlerFactory {
+                      public shopping_list::mojom::ShoppingListHandlerFactory {
  public:
   explicit ReadingListUI(content::WebUI* web_ui);
   ReadingListUI(const ReadingListUI&) = delete;
@@ -55,10 +52,6 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
   void BindInterface(
       mojo::PendingReceiver<shopping_list::mojom::ShoppingListHandlerFactory>
           receiver);
-
-  void BindInterface(
-      mojo::PendingReceiver<help_bubble::mojom::HelpBubbleHandlerFactory>
-          pending_receiver);
 
   void SetActiveTabURL(const GURL& url);
 
@@ -85,12 +78,6 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
       mojo::PendingReceiver<shopping_list::mojom::ShoppingListHandler> receiver)
       override;
 
-  // help_bubble::mojom::HelpBubbleHandlerFactory:
-  void CreateHelpBubbleHandler(
-      mojo::PendingRemote<help_bubble::mojom::HelpBubbleClient> client,
-      mojo::PendingReceiver<help_bubble::mojom::HelpBubbleHandler> handler)
-      override;
-
   std::unique_ptr<ReadingListPageHandler> page_handler_;
   mojo::Receiver<reading_list::mojom::PageHandlerFactory>
       page_factory_receiver_{this};
@@ -106,10 +93,6 @@ class ReadingListUI : public ui::MojoBubbleWebUIController,
   std::unique_ptr<commerce::ShoppingListHandler> shopping_list_handler_;
   mojo::Receiver<shopping_list::mojom::ShoppingListHandlerFactory>
       shopping_list_factory_receiver_{this};
-
-  std::unique_ptr<user_education::HelpBubbleHandler> help_bubble_handler_;
-  mojo::Receiver<help_bubble::mojom::HelpBubbleHandlerFactory>
-      help_bubble_handler_factory_receiver_{this};
 
   WebuiLoadTimer webui_load_timer_;
 
