303500a62fd015c67162691df21cc8b5a939f362
Joseph Park <josephjoopark@chromium.org>
2022-11-17 21:17:35 +0000

    Remove redundant UpdateImage() call from Checkbox::OnThemeChanged()
    Bug: 1385516
    Change-Id: Iab32d421449dbe9beb6a0d21d26fd6ad551e05da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031714
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073040}

--- ui/views/controls/button/checkbox.cc
+++ ui/views/controls/button/checkbox.cc
@@ -178,7 +178,6 @@ std::unique_ptr<LabelButtonBorder> Checkbox::CreateDefaultBorder() const {
 
 void Checkbox::OnThemeChanged() {
   LabelButton::OnThemeChanged();
-  UpdateImage();
 }
 
 SkPath Checkbox::GetFocusRingPath() const {
