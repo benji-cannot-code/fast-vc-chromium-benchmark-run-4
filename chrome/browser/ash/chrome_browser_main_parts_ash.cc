ff1a36925866e8cef9c0e119f318fdd081486fc4
Erik Chen <erikchen@chromium.org>
2022-11-17 02:10:29 +0000

    Refactor memory metrics collection on ChromeOS.
    This CL is a refactor and is not intended to (noticeably) change any
    user-visible functionality.
    
    Prior to this CL:
      * There were two sets of memory metrics (zram, PSI) that were both
        emitted every 10 seconds, but the metrics were not in sync.
      * The PSI metrics were gated behind an ash feature which was always
        enabled with default collection interval of 10s.
      * The PSI metrics and zram metrics were initialized at different
        points in the startup sequence. The PSI metrics were initialized too
        early, and the comment was inaccurate.
    
    This CL:
      * Merges the timers so that they're collected at the same time. This
        in turn will allow us to emit a new aggregate metric.
      * Removes the ash feature and unconditionally enables the metrics.
      * Moves initialization so it happens in the same place, and the right
        time.
      * Renames PSIMemoryMetrics to MemoryMetrics.
    
    Change-Id: I7fd6be033d6912cfb46c7ab3e68fb8a54f2acd72
    Bug: 1382936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4017533
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072564}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/startup_utils.h"
 #include "chrome/browser/ash/login/users/chrome_user_manager.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/ash/memory_metrics.h"
 #include "chrome/browser/ash/mojo_service_manager/connection_helper.h"
 #include "chrome/browser/ash/net/bluetooth_pref_state_observer.h"
 #include "chrome/browser/ash/net/network_health/network_health_manager.h"
@@ -144,7 +145,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/printing/bulk_printers_calculator_factory.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ash/profiles/signin_profile_handler.h"
-#include "chrome/browser/ash/psi_memory_metrics.h"
 #include "chrome/browser/ash/quick_pair/quick_pair_browser_delegate_impl.h"
 #include "chrome/browser/ash/scheduler_configuration_manager.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
@@ -1264,15 +1264,6 @@ void ChromeBrowserMainPartsAsh::PreBrowserStart() {
   external_metrics_ = new ExternalMetrics;
   external_metrics_->Start();
 
-  // Aiming to collect memory metrics even prior to login, start them
-  // early.
-  if (base::FeatureList::IsEnabled(features::kMemoryPressureMetricsDetail)) {
-    // Start background collection of memory pressure data for Chrome OS.
-    memory_pressure_detail_ = base::MakeRefCounted<PSIMemoryMetrics>(
-        features::kMemoryPressureMetricsDetailLogPeriod.Get());
-    memory_pressure_detail_->Start();
-  }
-
   // -- This used to be in ChromeBrowserMainParts::PreMainMessageLoopRun()
   // -- immediately after ChildProcess::WaitForDebugger().
 
@@ -1381,8 +1372,10 @@ void ChromeBrowserMainPartsAsh::PostBrowserStart() {
         std::make_unique<diagnostics::DiagnosticsBrowserDelegateImpl>());
   }
 
-  zram_detail_ = base::MakeRefCounted<memory::ZramMetrics>();
-  zram_detail_->Start();
+  // Start background collection of memory pressure data for Chrome OS.
+  memory_pressure_detail_ = base::MakeRefCounted<MemoryMetrics>(
+      MemoryMetrics::kDefaultPeriodInSeconds);
+  memory_pressure_detail_->Start();
 
   if (ash::memory::ZramWritebackController::IsSupportedAndEnabled()) {
     zram_writeback_controller_ = ash::memory::ZramWritebackController::Create();
@@ -1403,9 +1396,6 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
   // Do this early to keep logging from taking time during shutdown.
   if (memory_pressure_detail_ != nullptr) {
     memory_pressure_detail_->Stop();
-  }
-  if (zram_detail_ != nullptr) {
-    zram_detail_->Stop();
   }
 
   if (zram_writeback_controller_ != nullptr) {
--- chrome/browser/ash/chrome_browser_main_parts_ash.h
+++ chrome/browser/ash/chrome_browser_main_parts_ash.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/wilco_dtc_supportd/wilco_dtc_supportd_manager.h"
 #include "chrome/browser/chrome_browser_main_linux.h"
 #include "chrome/browser/memory/memory_kills_monitor.h"
-#include "chromeos/ash/components/memory/memory.h"
 #include "chromeos/ash/components/memory/zram_writeback_controller.h"
 
 class AssistantBrowserDelegateImpl;
@@ -81,7 +80,7 @@ class LowDiskNotification;
 class MultiCaptureNotification;
 class NetworkPrefStateObserver;
 class NetworkThrottlingObserver;
-class PSIMemoryMetrics;
+class MemoryMetrics;
 class PowerMetricsReporter;
 class RendererFreezer;
 class SessionTerminationManager;
@@ -197,9 +196,7 @@ class ChromeBrowserMainPartsAsh : public ChromeBrowserMainPartsLinux {
 
   scoped_refptr<ExternalMetrics> external_metrics_;
 
-  scoped_refptr<PSIMemoryMetrics> memory_pressure_detail_;
-
-  scoped_refptr<memory::ZramMetrics> zram_detail_;
+  scoped_refptr<MemoryMetrics> memory_pressure_detail_;
 
   std::unique_ptr<arc::ArcServiceLauncher> arc_service_launcher_;
 
