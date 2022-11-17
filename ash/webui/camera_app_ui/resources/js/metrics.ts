94d2b348b438eb390a6ce415532759e1de2cc34b
Pi-Hsun Shih <pihsun@chromium.org>
2022-11-17 08:38:00 +0000

    CCA: Initialize untrusted_scripts iframes on start
    Consolidates all places that use untrusted_scripts iframes, and
    initialize them on start in init.ts, so tast can start profiling on
    those iframes and collect coverage reports.
    
    The corresponding tast change is on CL:3950271.
    
    Note that this won't break without the tast changes and the tast changes
    won't break without this change, so the two CLs doesn't need to depends
    on each other.
    
    Bug: b:197712141
    Test: manually
    
    Change-Id: Ie0b03f43efb5ec382b72da4e13c321c462aba1ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3951457
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072674}

--- ash/webui/camera_app_ui/resources/js/metrics.ts
+++ ash/webui/camera_app_ui/resources/js/metrics.ts
@@ -22,8 +22,7 @@ import {
   Resolution,
   VideoResolutionLevel,
 } from './type.js';
-import {GAHelper} from './untrusted_ga_helper.js';
-import * as util from './util.js';
+import {getGAHelper} from './untrusted_scripts.js';
 import {WaitableEvent} from './waitable_event.js';
 
 /**
@@ -34,9 +33,6 @@ const GA_ID = 'UA-134822711-1';
 let baseDimen: Map<number, number|string>|null = null;
 
 const ready = new WaitableEvent();
-
-const gaHelper =
-    util.createUntrustedJSModule<GAHelper>('/js/untrusted_ga_helper.js');
 
 /**
  * Send the event to GA backend.
@@ -68,7 +64,7 @@ async function sendEvent(
   const canSendMetrics =
       await ChromeHelper.getInstance().isMetricsAndCrashReportingEnabled();
   if (canSendMetrics) {
-    (await gaHelper).sendGAEvent(event);
+    (await getGAHelper()).sendGAEvent(event);
   }
 }
 
@@ -80,7 +76,7 @@ async function sendEvent(
  */
 export async function setMetricsEnabled(enabled: boolean): Promise<void> {
   await ready.wait();
-  await (await gaHelper).setMetricsEnabled(GA_ID, enabled);
+  await (await getGAHelper()).setMetricsEnabled(GA_ID, enabled);
 }
 
 const SCHEMA_VERSION = 3;
@@ -162,7 +158,8 @@ export async function initMetrics(): Promise<void> {
     localStorage.set(LocalStorageKey.GA_USER_ID, id);
   }
 
-  await (await gaHelper).initGA(GA_ID, clientId, Comlink.proxy(setClientId));
+  await (await getGAHelper())
+      .initGA(GA_ID, clientId, Comlink.proxy(setClientId));
   ready.signal();
 }
 
