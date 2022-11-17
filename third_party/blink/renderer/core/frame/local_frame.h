32223ce63545133981f07b5505f7595e52b6195b
Yuzu Saijo <yuzus@chromium.org>
2022-11-17 09:32:27 +0000

    [bfcache] Select a random cross-origin iframe to report blocking status
    This CL implements randomly selecting cross-origin iframe to report
    whether or not it blocked bfcache for NotRestoredReasons API.
    
    This is to minimize the cross-origin information leak.
    Detailed design: https://docs.google.com/document/d/1NIJJik06bx5MOhRQXkqyWf5nPAp0XmQiGZlyhtZXOUY/edit#heading=h.bzbyb5fq4dud
    
    Along with this, we are removing HasBlockingReasons() check in renderer
    as well, now that we send the partially masked information and the check
    cannot tell us the correct value any more.
    
    Bug: 1384381
    Change-Id: I50f8164e34131a60770cff63f52c009950c6a1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027464
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072690}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -615,8 +615,6 @@ class CORE_EXPORT LocalFrame final
       mojom::blink::BackForwardCacheNotRestoredReasonsPtr);
   const mojom::blink::BackForwardCacheNotRestoredReasonsPtr&
   GetNotRestoredReasons();
-  // Returns if the saved NotRestoredReasons has any blocking reasons.
-  bool HasBlockingReasons();
 
   const AtomicString& GetReducedAcceptLanguage() const {
     return reduced_accept_language_;
@@ -923,10 +921,6 @@ class CORE_EXPORT LocalFrame final
                                     String& clip_text,
                                     String& clip_html,
                                     gfx::Rect& clip_rect);
-
-  // Helper function for |HasBlockingReasons()|.
-  bool HasBlockingReasonsHelper(
-      const mojom::blink::BackForwardCacheNotRestoredReasonsPtr&);
 
 #if !BUILDFLAG(IS_ANDROID)
   void SetTitlebarAreaDocumentStyleEnvironmentVariables() const;
--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -3094,10 +3094,6 @@ void WebLocalFrameImpl::SetNotRestoredReasons(
       ConvertNotRestoredReasons(not_restored_reasons));
 }
 
-bool WebLocalFrameImpl::HasBlockingReasons() {
-  return GetFrame()->HasBlockingReasons();
-}
-
 const mojom::blink::BackForwardCacheNotRestoredReasonsPtr&
 WebLocalFrameImpl::GetNotRestoredReasons() {
   return GetFrame()->GetNotRestoredReasons();
