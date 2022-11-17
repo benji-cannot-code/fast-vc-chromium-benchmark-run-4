e767aa85564be324084f74725263c1f9e026a55b
Oksana Zhuravlova <oksamyt@chromium.org>
2022-11-17 16:35:23 +0000

    [aw][tracing] Compress traces before upload
    This change updates AwBackgroundTracingMetricsProvider to compress the
    serialized trace before it is uploaded. If the compressed trace exceeds
    512KB, it is discarded.
    
    Bug: b/247824653
    Change-Id: I40a7c684fffa3362a7254b393b995d8d6057b734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877575
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072843}

--- android_webview/browser/tracing/aw_background_tracing_metrics_provider.h
+++ android_webview/browser/tracing/aw_background_tracing_metrics_provider.h
@@ -8,7 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/tracing/common/background_tracing_metrics_provider.h"
 
+#include "base/memory/weak_ptr.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
 namespace tracing {
+
+// Only upload traces under 512 KB to decrease the risk of filling up
+// the app's IPC binder buffer (1 MB) and causing crashes.
+constexpr static int kCompressedUploadLimitBytes = 512 * 1024;
 
 class AwBackgroundTracingMetricsProvider
     : public BackgroundTracingMetricsProvider {
@@ -28,8 +35,18 @@ class AwBackgroundTracingMetricsProvider
  private:
   // BackgroundTracingMetricsProvider:
   void ProvideEmbedderMetrics(
-      metrics::ChromeUserMetricsExtension* uma_proto,
-      base::HistogramSnapshotManager* snapshot_manager) override;
+      metrics::ChromeUserMetricsExtension& uma_proto,
+      std::string&& serialized_trace,
+      metrics::TraceLog& log,
+      base::HistogramSnapshotManager* snapshot_manager,
+      base::OnceCallback<void(bool)> done_callback) override;
+
+  void OnTraceCompressed(metrics::ChromeUserMetricsExtension& uma_proto,
+                         metrics::TraceLog& log,
+                         base::OnceCallback<void(bool)> done_callback,
+                         absl::optional<std::string> serialized_trace);
+
+  base::WeakPtrFactory<AwBackgroundTracingMetricsProvider> weak_factory_{this};
 };
 
 }  // namespace tracing
