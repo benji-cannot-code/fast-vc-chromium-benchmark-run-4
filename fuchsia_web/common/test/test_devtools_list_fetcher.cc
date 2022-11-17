a06737d933927baa31b0980bc8e7719198a6d206
Adam Psarouthakis <psarouthakis@google.com>
2022-11-17 14:17:29 +0000

    [Code Health] Remove calls to base::Value::GetListDeprecated.
    Bug: 1303949
    Change-Id: I8c425450e94589f85ce52b6832215abe3a5032f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022222
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1072773}

--- fuchsia_web/common/test/test_devtools_list_fetcher.cc
+++ fuchsia_web/common/test/test_devtools_list_fetcher.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_test_util.h"
 
-base::Value GetDevToolsListFromPort(uint16_t port) {
+base::Value::List GetDevToolsListFromPort(uint16_t port) {
   GURL url(base::StringPrintf("http://127.0.0.1:%d/json/list", port));
   auto request_context = net::CreateTestURLRequestContextBuilder()->Build();
   net::TestDelegate delegate;
@@ -27,14 +27,17 @@ base::Value GetDevToolsListFromPort(uint16_t port) {
   delegate.RunUntilComplete();
 
   if (delegate.request_status() < 0)
-    return base::Value();
+    return base::Value::List();
 
   if (request->response_headers()->response_code() != net::HTTP_OK)
-    return base::Value();
+    return base::Value::List();
 
   const std::string& result = delegate.data_received();
   if (result.empty())
-    return base::Value();
+    return base::Value::List();
 
-  return base::JSONReader::Read(result).value_or(base::Value());
+  return base::JSONReader::Read(result)
+      .value_or(base::Value())
+      .GetList()
+      .Clone();
 }
