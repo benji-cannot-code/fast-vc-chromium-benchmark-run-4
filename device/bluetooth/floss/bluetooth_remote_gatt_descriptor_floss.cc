6032a8a76348858b69cc81d86e80baaaf425480c
Katherine Lai <laikatherine@google.com>
2022-11-17 21:36:23 +0000

    [floss] GATT fixes for smart lock
    * Call ConfigureMTU to request for the max length of an
      ATT data packet to speed up BLE throughput.
    * Only queue full discovery if not already done.
    * Call error CB if GattDescriptorWrite times out.
    
    BUG=b:249549480
    TEST=Manual testing of smart lock
    
    Change-Id: I66b76fff695b8b4d86ac9a2e63feffdd9257ff82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021618
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Auto-Submit: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073057}

--- device/bluetooth/floss/bluetooth_remote_gatt_descriptor_floss.cc
+++ device/bluetooth/floss/bluetooth_remote_gatt_descriptor_floss.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "components/device_event_log/device_event_log.h"
 #include "device/bluetooth/floss/bluetooth_adapter_floss.h"
 #include "device/bluetooth/floss/bluetooth_remote_gatt_characteristic_floss.h"
 #include "device/bluetooth/floss/bluetooth_remote_gatt_service_floss.h"
@@ -16,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/floss/floss_gatt_client.h"
 
 namespace floss {
+
+const int kGattTimeoutMs = 2000;
 
 // static
 std::unique_ptr<BluetoothRemoteGattDescriptorFloss>
@@ -132,6 +136,8 @@ void BluetoothRemoteGattDescriptorFloss::GattDescriptorWrite(
   }
 
   auto [callback, error_callback, data] = std::move(pending_write_callbacks_);
+  DCHECK(callback);
+  DCHECK(error_callback);
 
   if (status == GattStatus::kSuccess) {
     cached_data_ = data;
@@ -185,6 +191,23 @@ void BluetoothRemoteGattDescriptorFloss::OnWriteDescriptor(
 
   pending_write_callbacks_ = std::make_tuple(
       std::move(callback), std::move(error_callback), std::move(data));
+
+  // Ensure callbacks don't get dropped if no |GattDescriptorWrite| received.
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&BluetoothRemoteGattDescriptorFloss::OnWriteTimeout,
+                     weak_ptr_factory_.GetWeakPtr()),
+      base::Milliseconds(kGattTimeoutMs));
+}
+
+void BluetoothRemoteGattDescriptorFloss::OnWriteTimeout() {
+  if (std::get<0>(pending_write_callbacks_)) {
+    BLUETOOTH_LOG(ERROR)
+        << "Timeout waiting for GattDescriptorWrite for Device "
+        << service_->GetDevice()->GetAddress();
+    GattDescriptorWrite(service_->GetDevice()->GetAddress(), GattStatus::kError,
+                        descriptor_->instance_id);
+  }
 }
 
 void BluetoothRemoteGattDescriptorFloss::NotifyValueChanged() {
