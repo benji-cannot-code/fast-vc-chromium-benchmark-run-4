226062f75e82a01526f7cc28fe9bfdec9c044abc
Greg Thompson <grt@chromium.org>
2022-11-17 17:55:32 +0000

    [fuchsia] Enable SystemNetworkApiUnitTest.GetNetworkInterfaces
    The test passes now that extensions_unittests is a v2 component
    
    Fixed: 1255187
    Change-Id: If6a6104067c1096df52d0720bcbf589a9ac7831c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030410
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072906}

--- extensions/browser/api/system_network/system_network_api_unittest.cc
+++ extensions/browser/api/system_network/system_network_api_unittest.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "extensions/browser/api/system_network/system_network_api.h"
-#include "build/build_config.h"
+
 #include "extensions/browser/api_test_utils.h"
 #include "extensions/browser/api_unittest.h"
 #include "extensions/common/extension_builder.h"
@@ -21,13 +21,7 @@ using SystemNetworkApiUnitTest = extensions::ApiUnitTest;
 
 }  // namespace
 
-// TODO(crbug.com/1255187): Fails on Fuchsia running with run-test-component.
-#if BUILDFLAG(IS_FUCHSIA)
-#define MAYBE_GetNetworkInterfaces DISABLED_GetNetworkInterfaces
-#else
-#define MAYBE_GetNetworkInterfaces GetNetworkInterfaces
-#endif  // BUILDFLAG(IS_FUCHSIA)
-TEST_F(SystemNetworkApiUnitTest, MAYBE_GetNetworkInterfaces) {
+TEST_F(SystemNetworkApiUnitTest, GetNetworkInterfaces) {
   scoped_refptr<SystemNetworkGetNetworkInterfacesFunction> socket_function(
       new SystemNetworkGetNetworkInterfacesFunction());
   scoped_refptr<const Extension> empty_extension(
