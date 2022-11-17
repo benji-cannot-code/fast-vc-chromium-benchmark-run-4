765478cb96c91bd2c9585a395f38608b4f97a9a3
François Doray <fdoray@chromium.org>
2022-11-17 16:15:40 +0000

    DumpWithoutCrashing on early feature access.
    Currently, accessing a feature too early will DCHECK. This is great
    to find problematic accesses in developer builds, but doesn't allow
    gathering a lot of reports from the field. This CL adds a
    DumpWithoutCrashing to allow collection of reports from the field, but
    without making Chrome unusable if there are problematic accesses. Once
    we're confident that there are not problematic accesses, we'll remove
    the DumpWithoutCrashing and change the DCHECK to a CHECK.
    
    Bug: 1358639
    Change-Id: I1abf056060415e3215f9fafcae98af09b4918cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031432
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072830}

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
-#include "base/debug/stack_trace.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial.h"
@@ -93,9 +92,9 @@ class EarlyFeatureAccessTracker {
     DCHECK(!feature) << "Accessed feature " << feature->name
                      << " before FeatureList registration.";
     // TODO(crbug.com/1383852): When we believe that all early accesses have
-    // been fixed, add a base::debug::DumpWithoutCrashing(), to get reports from
-    // the field without making Chrome unusable. If we don't get reports, change
-    // the DCHECK above to a CHECK.
+    // been fixed, remove this base::debug::DumpWithoutCrashing() and change the
+    // above DCHECK to a CHECK.
+    base::debug::DumpWithoutCrashing();
 #endif  // !BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_ANDROID) &&
         // !BUILDFLAG(IS_CHROMEOS)
   }
