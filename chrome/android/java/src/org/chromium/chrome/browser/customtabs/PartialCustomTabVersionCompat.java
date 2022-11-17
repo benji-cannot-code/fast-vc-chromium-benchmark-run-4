68aa2211dd3d6a31b285a5a78b4a27bbef9f4f66
Jinsuk Kim <jinsukkim@chromium.org>
2022-11-17 00:54:03 +0000

    Reland "[PCCT] Hook up CustomTabsCallback#onActivityResized"
    Relands the API with an enhancement that handles updates not only in
    height but also in width.
    
    Bug: 1366844
    
    This reverts commit 6c0502a2744cf704ffecfe25910b95ea4aab03a5.
    
    Change-Id: Ie0d66db69df7586f574037b7af6d1e5aa921b2c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022322
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072539}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/PartialCustomTabVersionCompat.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/PartialCustomTabVersionCompat.java
@@ -38,6 +38,9 @@ abstract class PartialCustomTabVersionCompat {
     /** Returns display height */
     abstract @Px int getDisplayHeight();
 
+    /** Returns display width */
+    abstract @Px int getDisplayWidth();
+
     /** Returns the status bar height */
     abstract @Px int getStatusbarHeight();
 
@@ -60,6 +63,12 @@ abstract class PartialCustomTabVersionCompat {
         @Px
         int getDisplayHeight() {
             return mActivity.getWindowManager().getCurrentWindowMetrics().getBounds().height();
+        }
+
+        @Override
+        @Px
+        int getDisplayWidth() {
+            return mActivity.getWindowManager().getCurrentWindowMetrics().getBounds().width();
         }
 
         @Override
@@ -134,6 +143,18 @@ abstract class PartialCustomTabVersionCompat {
                 mActivity.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
             }
             return displayMetrics.heightPixels;
+        }
+
+        @Override
+        @Px
+        int getDisplayWidth() {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            if (MultiWindowUtils.getInstance().isInMultiWindowMode(mActivity)) {
+                mActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            } else {
+                mActivity.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
+            }
+            return displayMetrics.widthPixels;
         }
 
         @Override
