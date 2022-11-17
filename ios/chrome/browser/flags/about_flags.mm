3d4c373ba6aca1e75ccff23a002b437623d447ba
Hira Mahmood <hiramahmood@google.com>
2022-11-17 17:45:41 +0000

    Create feature kCredentialProviderExtensionPromo.
    This change creates the feature flag for the Credential Provider
    Extension promo feature. When enabled, the CPE promo is displayed to
    eligible users.
    
    Bug: None
    Change-Id: I498de67e0d9b157a2570768e0bf66034289b2a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027829
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1072898}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/features.h"
 #import "ios/chrome/browser/ui/bubble/bubble_features.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
+#import "ios/chrome/browser/ui/credential_provider_promo/features.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_utils.h"
 #import "ios/chrome/browser/ui/download/features.h"
 #import "ios/chrome/browser/ui/first_run/field_trial_constants.h"
@@ -1338,7 +1339,10 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"remove-crash-infobar", flag_descriptions::kRemoveCrashInfobarName,
      flag_descriptions::kRemoveCrashInfobarDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kRemoveCrashInfobar)},
-
+    {"credential-provider-extension-promo",
+     flag_descriptions::kCredentialProviderExtensionPromoName,
+     flag_descriptions::kCredentialProviderExtensionPromoDescription,
+     flags_ui::kOsIos, FEATURE_VALUE_TYPE(kCredentialProviderExtensionPromo)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
