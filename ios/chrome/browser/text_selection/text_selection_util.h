3d6e63596d2cd365be2a1df6d4b6ada2079c4069
Raj T <rajendrant@chromium.org>
2022-11-17 16:33:08 +0000

    Add text classifier global feature flag to control all entity types
    Change-Id: Ibb0d287d01a7bdcecb078511bfcd7f959265b4bf
    Bug: 1385567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032289
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072840}

--- ios/chrome/browser/text_selection/text_selection_util.h
+++ ios/chrome/browser/text_selection/text_selection_util.h
@@ -12,4 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // calendar.
 BASE_DECLARE_FEATURE(kEnableExpKitCalendarTextClassifier);
 
+// Feature flag to enable Text Classifier entity detection in experience kit
+// The feature params will control which entity types are enabled for
+// detection.
+BASE_DECLARE_FEATURE(kEnableExpKitTextClassifier);
+
 #endif  // IOS_CHROME_BROWSER_TEXT_SELECTION_TEXT_SELECTION_UTIL_H_
--- ios/chrome/browser/text_selection/text_selection_util.mm
+++ ios/chrome/browser/text_selection/text_selection_util.mm
@@ -12,3 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BASE_FEATURE(kEnableExpKitCalendarTextClassifier,
              "EnableExpKitCalendarTextClassifier",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kEnableExpKitTextClassifier,
+             "EnableExpKitTextClassifier",
+             base::FEATURE_DISABLED_BY_DEFAULT);
