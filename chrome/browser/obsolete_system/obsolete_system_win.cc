efd0f97c02133c644fe1591d56d23be7d5e45713
Jesse McKenna <jessemckenna@google.com>
2022-11-17 21:15:03 +0000

    Use kernel32 version for OS deprecation infobar
    This change makes the code to detect obsolete Windows versions get the
    OS version from `Kernel32Version()` instead of `GetVersion()`.
    
    If Chrome is run in App Compatibility mode [1], `GetVersion()` will
    return the compatibility-mode version rather than the true system
    version. This may be done by Windows automatically in certain cases, or
    by system administrators, anti-virus software, etc.
    
    The code to detect obsolete Windows versions is used to display warnings
    that the OS is no longer supported (e.g., the Windows 7/8 deprecation
    infobar recently added [2]). This means the current code shows warnings
    to users in compatibility mode for unsupported versions even if their
    actual OS is Windows 10, which can confuse users [3].
    
    `Kernel32Version()` returns the correct OS version by checking the
    version of kernel32.dll, and is used in e.g., Crashpad to ensure the
    right version is recorded.
    
    This change was tested by looking for the Windows 7/8 deprecation
    infobar on the following systems:
    * Windows 10 (not shown)
    * Windows 10 in compatibility mode for Windows 8 (not shown)
    * Windows 7 (shown)
    * Windows 8.1 (shown)
    * Windows 8.1 in compatibility mode for Windows 7
      (shown, infobar correctly says the OS version is Windows 8.1)
    
    [1] done by:
      1. right-clicking a taskbar icon
      2. navigating to Properties > Compatibility
      3. checking the box "Run this program in compatibility mode for"
      4. choosing the desired Windows version in the dropdown menu
      5. clicking OK
      6. launching Chrome via the taskbar icon
    [2] crrev.com/c/4004000
    [3] example support thread from the Win XP/Vista deprecation:
        https://support.google.com/chrome/thread/99522885/chrome-thinks-i-m-using-xp-but-i-m-using-windows-10?hl=en
    
    Bug: 1377525
    Change-Id: Ifd413d7efd941e7bb6c7d278ae2414e360147fab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031572
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073035}

--- chrome/browser/obsolete_system/obsolete_system_win.cc
+++ chrome/browser/obsolete_system/obsolete_system_win.cc
@@ -15,8 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// Obsolete-system checks get the system version from kernel32.dll's version, to
+// avoid getting an incorrect version reported by App Compatibility mode. This
+// prevents obsolete-system warnings from appearing when Chrome is run in
+// compatibility mode on modern versions of Windows.
+base::win::Version GetRealOSVersion() {
+  return base::win::OSInfo::GetInstance()->Kernel32Version();
+}
+
 bool IsObsoleteOsVersion() {
-  return base::win::GetVersion() < base::win::Version::WIN10;
+  return GetRealOSVersion() < base::win::Version::WIN10;
 }
 
 }  // namespace
@@ -28,7 +36,7 @@ bool ObsoleteSystem::IsObsoleteNowOrSoon() {
 
 // static
 std::u16string ObsoleteSystem::LocalizedObsoleteString() {
-  const auto version = base::win::GetVersion();
+  const auto version = GetRealOSVersion();
   if (version == base::win::Version::WIN7)
     return l10n_util::GetStringUTF16(IDS_WIN_7_OBSOLETE);
   if (version == base::win::Version::WIN8)
@@ -45,7 +53,7 @@ bool ObsoleteSystem::IsEndOfTheLine() {
 
 // static
 const char* ObsoleteSystem::GetLinkURL() {
-  const auto version = base::win::GetVersion();
+  const auto version = GetRealOSVersion();
   if (version < base::win::Version::WIN7)
     return chrome::kWindowsXPVistaDeprecationURL;
   return chrome::kWindows78DeprecationURL;
