d337fe0e59b7fa38d4e864285673973a02c46186
Cole Horvitz <colehorvitz@chromium.org>
2022-11-18 21:52:58 +0000

    USB Internals: Enable trusted types
    Bug: 1098690
    Change-Id: Iac238fb2bb31da8abbe547693e98dff1a5d16206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034789
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073569}

--- chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
+++ chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/usb_internals_resources.h"
 #include "chrome/grit/usb_internals_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
-#include "services/network/public/mojom/content_security_policy.mojom.h"
 
 UsbInternalsUI::UsbInternalsUI(content::WebUI* web_ui)
     : ui::MojoWebUIController(web_ui) {
@@ -34,12 +33,7 @@ UsbInternalsUI::UsbInternalsUI(content::WebUI* web_ui)
       source,
       base::make_span(kUsbInternalsResources, kUsbInternalsResourcesSize),
       IDR_USB_INTERNALS_USB_INTERNALS_HTML);
-  source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::RequireTrustedTypesFor,
-      "require-trusted-types-for 'script';");
-  source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::TrustedTypes,
-      "trusted-types static-types usb-test-static;");
+  webui::EnableTrustedTypesCSP(source);
 
   content::WebUIDataSource::Add(Profile::FromWebUI(web_ui), source);
 }
