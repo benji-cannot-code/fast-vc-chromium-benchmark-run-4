8a9bfa0e579b7173d87e9d36433c9500ed91bb8f
rgod <rgod@google.com>
2022-11-18 13:55:08 +0000

    Remove TestPasswordGenerationPopupController
    Replaces TestPasswordGenerationPopupController with directly using
    PasswordGenerationPopupControllerImpl::GetOrCreate()
    
    This is just a cleanup of a leftover TODO.
    
    Bug: 1345766
    Change-Id: If9b6fab855f66d634a74a573739b024a669c9200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4036969
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073341}

--- chrome/browser/ui/passwords/password_generation_popup_controller_impl.h
+++ chrome/browser/ui/passwords/password_generation_popup_controller_impl.h
@@ -127,6 +127,10 @@ class PasswordGenerationPopupControllerImpl
       const zoom::ZoomController::ZoomChangedEventData& data) override;
 #endif  // !BUILDFLAG(IS_ANDROID)
 
+#if defined(UNIT_TEST)
+  PasswordGenerationPopupView* view() const { return view_; }
+#endif
+
  protected:
   PasswordGenerationPopupControllerImpl(
       const gfx::RectF& bounds,
@@ -135,9 +139,6 @@ class PasswordGenerationPopupControllerImpl
       PasswordGenerationPopupObserver* observer,
       content::WebContents* web_contents,
       content::RenderFrameHost* frame);
-
-  // Handle to the popup. May be NULL if popup isn't showing.
-  raw_ptr<PasswordGenerationPopupView> view_;
 
  private:
   class KeyPressRegistrator;
@@ -171,6 +172,9 @@ class PasswordGenerationPopupControllerImpl
 
   // Accept password if it's selected.
   bool PossiblyAcceptPassword();
+
+  // Handle to the popup. May be NULL if popup isn't showing.
+  raw_ptr<PasswordGenerationPopupView> view_;
 
   const autofill::FormData form_data_;
 
