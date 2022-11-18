5ab4d8796df97bac99fc35709e6d1ab8665ccb29
Mustafa Emre Acer <meacer@chromium.org>
2022-11-18 18:43:51 +0000

    Add UI flag for IDNA 2008 Non-Transitional mode
    This CL adds a user visible flag at chrome://flags for the
    UseIDNA2008NonTransitional feature.
    
    Bug: 694157
    Change-Id: Iedbd645619658a519b51205cea7f14d86b268e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4036010
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073455}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -217,6 +217,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_switches.h"
 #include "ui/native_theme/native_theme_features.h"
 #include "ui/ui_features.h"
+#include "url/url_features.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "base/allocator/buildflags.h"
@@ -9152,6 +9153,11 @@ const FeatureEntry kFeatureEntries[] = {
     {"omit-cors-client-cert", flag_descriptions::kOmitCorsClientCertName,
      flag_descriptions::kOmitCorsClientCertDescription, kOsAll,
      FEATURE_VALUE_TYPE(network::features::kOmitCorsClientCert)},
+
+    {"use-idna2008-non-transitional",
+     flag_descriptions::kUseIDNA2008NonTransitionalName,
+     flag_descriptions::kUseIDNA2008NonTransitionalDescription, kOsAll,
+     FEATURE_VALUE_TYPE(url::kUseIDNA2008NonTransitional)},
 
 #if BUILDFLAG(IS_CHROMEOS)
     {"link-capturing-infobar", flag_descriptions::kLinkCapturingInfoBarName,
