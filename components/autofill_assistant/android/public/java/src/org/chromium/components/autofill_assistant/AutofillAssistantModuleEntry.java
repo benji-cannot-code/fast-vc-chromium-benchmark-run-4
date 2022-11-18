f719fad94f9c5b5ed0aa92c5130b86813790c22f
Luca Hunkeler <hluca@google.com>
2022-11-18 11:58:53 +0000

    Remove Autofill Assistant direct actions
    Change-Id: I564b69284386f15ab54b431e1a102356eadc1433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034206
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073310}

--- components/autofill_assistant/android/public/java/src/org/chromium/components/autofill_assistant/AutofillAssistantModuleEntry.java
+++ components/autofill_assistant/android/public/java/src/org/chromium/components/autofill_assistant/AutofillAssistantModuleEntry.java
@@ -5,11 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.autofill_assistant;
 
-import android.content.Context;
-import android.view.View;
-
-import org.chromium.base.supplier.Supplier;
-import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.module_installer.builder.ModuleInterface;
 import org.chromium.content_public.browser.WebContents;
 
@@ -26,21 +21,4 @@ public interface AutofillAssistantModuleEntry {
      */
     AssistantOnboardingHelper createOnboardingHelper(
             WebContents webContents, AssistantDependencies dependencies);
-
-    /**
-     * Returns a {@link AutofillAssistantActionHandler} instance tied to the activity owning the
-     * given bottom sheet, and scrim view.
-     *
-     * @param context activity context
-     * @param bottomSheetController bottom sheet controller instance of the activity
-     * @param browserControlsFactory factory for providing browser controls state
-     * @param rootView root view of the activity
-     * @param webContentsSupplier supplier of the current WebContents
-     * @param staticDependencies used to create platform-specific dependencies
-     */
-    AutofillAssistantActionHandler createActionHandler(Context context,
-            BottomSheetController bottomSheetController,
-            AssistantBrowserControlsFactory browserControlsFactory, View rootView,
-            Supplier<WebContents> webContentsSupplier,
-            AssistantStaticDependencies staticDependencies);
 }
