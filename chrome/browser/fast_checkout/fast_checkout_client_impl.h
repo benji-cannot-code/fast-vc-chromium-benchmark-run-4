76fe5a3d3cabe6f41c5071a261f360bd2d069848
Norge Vizcay <vizcay@google.com>
2022-11-18 14:36:10 +0000

    [FastCheckout] Remove autofill_assistant dependencies from FC client.
    Bug: 1385669,1334642
    Change-Id: I0931254000113903c3167e2c1d1f517dac4f67b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030384
    Reviewed-by: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073352}

--- chrome/browser/fast_checkout/fast_checkout_client_impl.h
+++ chrome/browser/fast_checkout/fast_checkout_client_impl.h
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_observation.h"
 #include "chrome/browser/fast_checkout/fast_checkout_client.h"
-#include "chrome/browser/fast_checkout/fast_checkout_prefs.h"
 #include "chrome/browser/ui/fast_checkout/fast_checkout_controller_impl.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
-#include "components/autofill_assistant/browser/public/headless_script_controller.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
 #include "url/gurl.h"
@@ -22,15 +20,8 @@ namespace autofill {
 class LogManager;
 }
 
-namespace autofill_assistant {
-class RuntimeManager;
-}
-
 constexpr char kUmaKeyFastCheckoutRunOutcome[] =
     "Autofill.FastCheckout.RunOutcome";
-
-// The command line switch for specifying a custom server URL.
-constexpr char kAutofillAssistantUrl[] = "autofill-assistant-url";
 
 // Enum defining possible outcomes of a Fast Checkout run. Must be kept in sync
 // with enums.xml.
@@ -75,10 +66,6 @@ class FastCheckoutClientImpl
  protected:
   explicit FastCheckoutClientImpl(content::WebContents* web_contents);
 
-  // Creates the headless script controller.
-  virtual std::unique_ptr<autofill_assistant::HeadlessScriptController>
-  CreateHeadlessScriptController();
-
   // Creates the external action delegate.
   virtual std::unique_ptr<FastCheckoutExternalActionDelegate>
   CreateFastCheckoutExternalActionDelegate();
@@ -86,11 +73,6 @@ class FastCheckoutClientImpl
   // Creates the UI controller.
   virtual std::unique_ptr<FastCheckoutController>
   CreateFastCheckoutController();
-
-  // Gets the RunTimeManager used to disable dialogs and prompts, such as
-  // password manager, translation dialogs and permissions. Protected to allow
-  // for overrides by test classes.
-  virtual autofill_assistant::RuntimeManager* GetRuntimeManager();
 
  private:
   friend class content::WebContentsUserData<FastCheckoutClientImpl>;
@@ -106,8 +88,7 @@ class FastCheckoutClientImpl
   void OnHidden();
 
   // Registers when a run is complete. Used in callbacks.
-  void OnRunComplete(
-      autofill_assistant::HeadlessScriptController::ScriptResult result);
+  void OnRunComplete();
 
   // Displays the bottom sheet UI. If the underlying autofill data is updated,
   // the method is called again to refresh the information displayed in the UI.
@@ -115,10 +96,6 @@ class FastCheckoutClientImpl
 
   // Turns keyboard suppression on and off.
   void SetShouldSuppressKeyboard(bool suppress);
-
-  // Registers when onboarding was completed successfully and the scripts
-  // are ready to run.
-  void OnOnboardingCompletedSuccessfully();
 
   // Returns true if fast checkout should run, e.g. if the feature is enabled.
   bool ShouldRun(bool script_supports_consentless_execution);
@@ -134,12 +111,6 @@ class FastCheckoutClientImpl
   std::unique_ptr<FastCheckoutExternalActionDelegate>
       fast_checkout_external_action_delegate_;
 
-  // Controls a script run triggered by the headless API. This class is
-  // responsible for handling the forwarding of actions to
-  // `apc_external_action_delegate_` and managing the run lifetime.
-  std::unique_ptr<autofill_assistant::HeadlessScriptController>
-      external_script_controller_;
-
   // Fast Checkout UI Controller. Responsible for showing the bottomsheet and
   // handling user selections.
   std::unique_ptr<FastCheckoutController> fast_checkout_controller_;
@@ -153,9 +124,6 @@ class FastCheckoutClientImpl
   base::ScopedObservation<autofill::PersonalDataManager,
                           autofill::PersonalDataManagerObserver>
       personal_data_manager_observation_{this};
-
-  // Handles fast checkout profile prefs, i.e. declining onboarding.
-  FastCheckoutPrefs fast_checkout_prefs_;
 
   // content::WebContentsUserData:
   WEB_CONTENTS_USER_DATA_KEY_DECL();
