12d80e8be4996077e7206a772ec26554999156e6
Ryo Hashimoto <hashimoto@chromium.org>
2022-11-18 05:26:54 +0000

    arc: Add arc::prefs::kArcPackagesIsUpToDate
    - This new pref indicates if the package list held by ArcAppList is
      up to date.
    - Use this pref to implement go/arc-on-demand-v1
    
    BUG=b:255725262
    TEST=unit_tests
    TEST=With --enable-fatures=ArcOnDemand in etc/chrome_dev.conf, confirmed
    that rebooting the device before app install finishes results in ARC
    automatically started.
    
    Change-Id: Ia828a970e09bde144ce846cece2f9bd266d9e47c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029570
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073221}

--- chrome/browser/ash/arc/session/arc_activation_necessity_checker_unittest.cc
+++ chrome/browser/ash/arc/session/arc_activation_necessity_checker_unittest.cc
@@ -72,6 +72,7 @@ class ArcActivationNecessityCheckerTest : public testing::Test {
     profile_ = profile_builder.Build();
     profile_->GetProfilePolicyConnector()->OverrideIsManagedForTesting(true);
     profile_->GetPrefs()->SetBoolean(prefs::kArcEnabled, true);
+    profile_->GetPrefs()->SetBoolean(prefs::kArcPackagesIsUpToDate, true);
 
     const AccountId account_id(AccountId::FromUserEmailGaiaId(
         profile_->GetProfileUserName(), "1234567890"));
@@ -147,6 +148,14 @@ TEST_F(ArcActivationNecessityCheckerTest, UnmanagedUser) {
 
 TEST_F(ArcActivationNecessityCheckerTest, AdbSideloadingIsAvailable) {
   adb_sideloading_availability_delegate_.set_result(true);
+  base::test::TestFuture<bool> future;
+  checker_->Check(future.GetCallback());
+  EXPECT_TRUE(future.Get());
+}
+
+TEST_F(ArcActivationNecessityCheckerTest, PacakgeListIsNotUpToDate) {
+  profile_->GetPrefs()->SetBoolean(prefs::kArcPackagesIsUpToDate, false);
+
   base::test::TestFuture<bool> future;
   checker_->Check(future.GetCallback());
   EXPECT_TRUE(future.Get());
