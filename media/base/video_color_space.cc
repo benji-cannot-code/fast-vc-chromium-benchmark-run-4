7aabb9ca27155c2a7a7c238ff71bde9fe3b78c67
Dale Curtis <dalecurtis@chromium.org>
2022-11-18 18:33:36 +0000

    [WebCodecs] Fix embedded color profile for OpenH264 encoding.
    This also improves the full cycle test to verify the decoded color
    space matches the one provided by the encoder.
    
    Bug: 1377842
    
    Change-Id: If0635649ddec267910e7a307348fd79f6a96c879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032971
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073441}

--- media/base/video_color_space.cc
+++ media/base/video_color_space.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/video_color_space.h"
 
+#include "base/strings/stringprintf.h"
+
 namespace media {
 
 VideoColorSpace::PrimaryID VideoColorSpace::GetPrimaryID(int primary) {
@@ -288,6 +290,13 @@ gfx::ColorSpace VideoColorSpace::ToGfxColorSpace() const {
   }
 
   return gfx::ColorSpace(primary_id, transfer_id, matrix_id, range);
+}
+
+std::string VideoColorSpace::ToString() const {
+  return base::StringPrintf("{primary=%d, transfer=%d, matrix=%d, range=%d}",
+                            static_cast<int>(primaries),
+                            static_cast<int>(transfer),
+                            static_cast<int>(matrix), static_cast<int>(range));
 }
 
 VideoColorSpace VideoColorSpace::REC709() {
