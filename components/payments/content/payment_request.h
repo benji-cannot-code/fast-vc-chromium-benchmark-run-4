b31f34a59f0d833a9f90f2cef78173b813bce626
Nick Burris <nburris@chromium.org>
2022-11-18 18:50:37 +0000

    [SPC] Add Secure Payment Confirmation opt out WPT
    Adds chromedriver support and web platform test for opt-out for SPC. The
    WPT passes locally with SPC opt out enabled (currently disabled behind a
    flag).
    
    There's a small change in the SPC MVC logic, since opt-out can now
    happen by calling SPCController::OnOptOut directly, which resulted in
    the dialog view calling back to OnCancel when the dialog closes since it
    didn't know opt-out was programmatically invoked. This is fixed by
    having the controller set a bit on the model which the view checks.
    
    Spec PR: https://github.com/w3c/secure-payment-confirmation/pull/215
    
    Bug: 1385865
    Change-Id: I777e92b5110785415de68ef9f0497905598e4897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030688
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073462}

--- components/payments/content/payment_request.h
+++ components/payments/content/payment_request.h
@@ -38,6 +38,7 @@ enum class SPCTransactionMode {
   NONE,
   AUTOACCEPT,
   AUTOREJECT,
+  AUTOOPTOUT,
 };
 
 // This class manages the interaction between the renderer (through the
--- third_party/blink/web_tests/external/wpt/resources/testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/testharness.js
@@ -2247,7 +2247,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 ReadOnlyError: 0,
                 VersionError: 0,
                 OperationError: 0,
-                NotAllowedError: 0
+                NotAllowedError: 0,
+                OptOutError: 0
             };
 
             var code_name_map = {};
