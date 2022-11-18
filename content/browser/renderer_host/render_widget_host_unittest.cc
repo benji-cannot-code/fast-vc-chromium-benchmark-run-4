f945fe1d7e73f74b2431ef1af1d3cd32cfe80f9b
Mike Wasserman <msw@chromium.org>
2022-11-18 22:54:42 +0000

    Fix ImageTransportFactory teardown in RenderWidgetHostTest on Mac
    Fixed: 1386239
    Change-Id: Ibc3e81a0edcf054cf00e4649a8e5813f2a8b2372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4038663
    Quick-Run: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073610}

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -625,11 +625,8 @@ class RenderWidgetHostTest : public testing::Test {
     site_instance_group_.reset();
     process_.reset();
     browser_context_.reset();
-
-#if defined(USE_AURA)
-    ImageTransportFactory::Terminate();
-#endif
 #if defined(USE_AURA) || BUILDFLAG(IS_MAC)
+    ImageTransportFactory::Terminate();
     display::Screen::SetScreenInstance(nullptr);
     screen_.reset();
 #endif
@@ -641,8 +638,7 @@ class RenderWidgetHostTest : public testing::Test {
     base::RunLoop().RunUntilIdle();
   }
 
-  virtual void ConfigureView(TestView* view) {
-  }
+  virtual void ConfigureView(TestView* view) {}
 
   void ReinitalizeHost() {
     host_->BindWidgetInterfaces(
