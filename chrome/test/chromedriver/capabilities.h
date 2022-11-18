7164998e938c8878563eaa9b6862b060c11ecee4
Sammie Quon <sammiequon@chromium.org>
2022-11-18 17:52:35 +0000

    CodeHealth: Remove DictionaryValue from chrome/test/chromedriver p1
    Test: existing tests
    Bug: 1187061
    Change-Id: I9150f9c74fb760e81efceefebb9dd5b9c661d384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028841
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073410}

--- chrome/test/chromedriver/capabilities.h
+++ chrome/test/chromedriver/capabilities.h
@@ -169,7 +169,7 @@ struct Capabilities {
   // Time to wait for extension background page to appear. If 0, no waiting.
   base::TimeDelta extension_load_timeout;
 
-  std::unique_ptr<base::DictionaryValue> local_state;
+  std::unique_ptr<base::Value::Dict> local_state;
 
   std::string log_path;
 
@@ -184,7 +184,7 @@ struct Capabilities {
 
   base::Value devtools_events_logging_prefs;
 
-  std::unique_ptr<base::DictionaryValue> prefs;
+  std::unique_ptr<base::Value::Dict> prefs;
 
   Switches switches;
 
