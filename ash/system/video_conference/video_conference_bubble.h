271918ba5a7b013efd160af061c2dc06cfd1495d
Roger Tinkoff <rtinkoff@google.com>
2022-11-18 00:49:32 +0000

    ash: Add no-frills UI for testing camera effects
    Add a toggle button for testing background replace.
    
    Add a radio switch for testing background blur.
    
    Screenshot:
    https://screenshot.googleplex.com/6iK6uEaD9tzCktT
    
    Bug: b/259450086
    Change-Id: I196f397a5cfc609159f89b440cd5990d09851c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030189
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073145}

--- ash/system/video_conference/video_conference_bubble.h
+++ ash/system/video_conference/video_conference_bubble.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/tray/tray_bubble_view.h"
 
+namespace views {
+class ToggleButton;
+}  // namespace views
+
 namespace ash {
 
 // The bubble that contains controls for camera and microphone effects,
@@ -20,6 +24,30 @@ class VideoConferenceBubbleView : public TrayBubbleView {
   VideoConferenceBubbleView& operator=(const VideoConferenceBubbleView&) =
       delete;
   ~VideoConferenceBubbleView() override = default;
+
+ private:
+  // A view that combines a `views::Label` and a `views::ToggleButton`,
+  // for toggling a camera/mic effect from the VC bubble.
+  class LabeledToggleButton : public views::View {
+   public:
+    explicit LabeledToggleButton(views::Button::PressedCallback callback,
+                                 const std::u16string& effect_name);
+
+    LabeledToggleButton(const LabeledToggleButton&) = delete;
+    LabeledToggleButton& operator=(const LabeledToggleButton&) = delete;
+    ~LabeledToggleButton() override = default;
+
+    // Returns `true` if the `ToggleButton` is "on," `false` otherwise.
+    bool GetIsOn() const;
+
+   private:
+    views::ToggleButton* button_;
+  };
+
+  // Invoked when the "background replace" toggle button is pressed.
+  void OnBackgroundReplaceToggleButtonPressed();
+
+  LabeledToggleButton* background_replace_button_;
 };
 
 }  // namespace ash
