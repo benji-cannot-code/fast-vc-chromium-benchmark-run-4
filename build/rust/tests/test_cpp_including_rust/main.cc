e75a03629794a18728dd54a159cebe170f751af4
danakj <danakj@chromium.org>
2022-11-18 20:10:10 +0000

    Remove unused //build/rust/mixed_target
    Mixed target is designed to allow tight interop between Rust and C++
    inside a single GN target, with cyclical dependencies. Since this is
    not needed, we can remove it along with the bonus complexity it adds
    into the rust_target template.
    
    R=adetaylor@chromium.org
    
    Bug: 1381552
    Change-Id: I71763cf0d41ac47fad7b7453b895c1339065c004
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,linux-rust-x64-dbg,android-rust-arm-dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034526
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073514}

--- build/rust/tests/test_cpp_including_rust/main.cc
+++ build/rust/tests/test_cpp_including_rust/main.cc
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/rust/tests/test_mixed_static_library/test_mixed_static_library.h"
 #include "build/rust/tests/test_rust_static_library/src/lib.rs.h"
 
 int main(int argc, char* argv[]) {
   say_hello();
-  add_two_ints_via_rust_then_cpp(3, 4);
+  add_two_ints_via_rust(3, 4);
   return 0;
 }
--- build/rust/tests/test_rust_unittests/main.rs
+++ build/rust/tests/test_rust_unittests/main.rs
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern crate test;
 
 use test::Bencher;
-use test_mixed_static_library::add_two_ints_using_cpp;
+use test_rust_static_library::add_two_ints_via_rust;
 
 #[test]
 fn test_call_into_mixed_static_library() {
-    assert_eq!(add_two_ints_using_cpp(5, 7), 12)
+    assert_eq!(add_two_ints_via_rust(5, 7), 12)
 }
 
 #[allow(soft_unstable)]
