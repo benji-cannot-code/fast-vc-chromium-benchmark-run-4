b2f7e28a5910c687ec1a7527dc25254d47ac27aa
Kent Tamura <tkent@chromium.org>
2022-11-18 07:51:29 +0000

    AvailableLogicalWidth/Height should not be used for SVG <text> and <foreignObject>
    SVG <text> and <foreignObject> don't need AvailableLogicalWidth() and
    AvailableLogicalHeight() because
     - they ignore the available size field of NGConstraintSpace, and
     - they don't support margins.
    Note that LayoutNGMixin::UpdateMargins() calls AvailableLogicalWidth().
    
    Before this CL, these functions referred to LayoutBox::Size() of the
    containing block even though LayoutBox::Size() did not have valid data
    yet.
    
    This CL has no behavior changes.
    
    Bug: 1353190
    Change-Id: I7817fc123efad25a09ed02c3cc7bbb6d2e29ad53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037406
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073250}

--- third_party/blink/renderer/core/layout/ng/layout_ng_block_flow_mixin.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_block_flow_mixin.cc
@@ -277,7 +277,8 @@ void LayoutNGBlockFlowMixin<Base>::UpdateNGBlockLayout() {
   }
 
   LayoutNGMixin<Base>::UpdateInFlowBlockLayout();
-  LayoutNGMixin<Base>::UpdateMargins();
+  if (!Base::IsSVGChild())
+    LayoutNGMixin<Base>::UpdateMargins();
 }
 
 template <typename Base>
