cacfceba1c4234b0e1a5a771b92a53facc92cca9
Mohsen Izadi <mohsen@chromium.org>
2022-11-18 00:03:37 +0000

    Fix Omnibox.CharTypedToRepaintLatency.*ToPresent histograms
    These histograms use presentation time callbacks registered using
    `ui::Compositor::RequestPresentationTimeForNextFrame()` function to
    determine when a character is presented to the user. However, these
    callbacks are called even if the next submitted frame fails to present
    in which case the omnibox code does not report the metrics. With this
    CL, presentation time callbacks are registered using the newly added
    `ui::Compositor::RequestSuccessfulPresentationTimeForNextFrame()`
    function. In this case callbacks are not run until a subsequent frame is
    successfully presented, and then the metrics are reported. This fixes
    the issue of under reporting the metrics.
    
    Bug: 1378612
    Change-Id: I220ac4ea16e8a0c5afdfc8ce7126798450c8a602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999869
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073123}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -96,7 +96,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/image/image.h"
-#include "ui/gfx/presentation_feedback.h"
 #include "ui/gfx/selection_model.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/gfx/text_utils.h"
@@ -484,19 +483,17 @@ void OmniboxViewViews::OnPaint(gfx::Canvas* canvas) {
     UMA_HISTOGRAM_TIMES("Omnibox.CharTypedToRepaintLatency.ToPaint",
                         now - insert_char_time_);
     latency_histogram_state_ = ON_PAINT_CALLED;
-    GetWidget()->GetCompositor()->RequestPresentationTimeForNextFrame(
+    GetWidget()->GetCompositor()->RequestSuccessfulPresentationTimeForNextFrame(
         base::BindOnce(
             [](base::TimeTicks insert_timestamp,
                base::TimeTicks paint_timestamp,
-               const gfx::PresentationFeedback& feedback) {
-              if (feedback.flags & gfx::PresentationFeedback::kFailure)
-                return;
+               base::TimeTicks presentation_timestamp) {
               UMA_HISTOGRAM_TIMES(
                   "Omnibox.CharTypedToRepaintLatency.PaintToPresent",
-                  feedback.timestamp - paint_timestamp);
+                  presentation_timestamp - paint_timestamp);
               UMA_HISTOGRAM_TIMES(
                   "Omnibox.CharTypedToRepaintLatency.InsertToPresent",
-                  feedback.timestamp - insert_timestamp);
+                  presentation_timestamp - insert_timestamp);
             },
             insert_char_time_, now));
   }
