2ec223f29092593fe6574e238c11e0a96eaa06ae
François Doray <fdoray@chromium.org>
2022-11-18 22:04:28 +0000

    Tweak tests to allow running tasks by batches on ChromeOS.
    The "RunTasksByBatches" runs multiple tasks in "batches" before
    checking for system message. Enabling it cause test failures, which
    are addressed in this CL, in preparation for enabling the feature via
    fieldtrial_testing_config.json.
    
    - devtools_interactive_browsertest.cc
      With RunTaskByBatches, multiple tasks can run in a row without
      pumping system messages. As a result, the existing implementation of
      `CheckWaiter` can busy loop. This CL introduces a small delay
      between each invocation of `CheckWaiter::Check()`, to force pumping
      system messages.
    
    - password_bubble_interactive_uitest.cc
      This test checked that the password bubble was closed after
      running pending tasks with `content::RunAllPendingInMessageLoop`.
      This API doesn't provide any guarantee about pumping system
      messages which are required for the bubble to be closed. This CL
      modifies the test to keep running tasks *and* pumping system
      messages until the bubble is closed.
    
    Bug: 1299950
    Change-Id: I0b3e3279861acd5f8d7deef3603bed33669be3af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032779
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073576}

--- chrome/browser/devtools/devtools_interactive_browsertest.cc
+++ chrome/browser/devtools/devtools_interactive_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
+#include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -30,7 +31,7 @@ class CheckWaiter {
   CheckWaiter(base::RepeatingCallback<bool()> callback, bool expected)
       : callback_(callback),
         expected_(expected),
-        timeout_(base::Time::NowFromSystemTime() + base::Seconds(1)) {}
+        timeout_(base::TimeTicks::Now() + base::Seconds(1)) {}
 
   CheckWaiter(const CheckWaiter&) = delete;
   CheckWaiter& operator=(const CheckWaiter&) = delete;
@@ -49,11 +50,16 @@ class CheckWaiter {
 
  private:
   bool Check() {
-    if (callback_.Run() != expected_ &&
-        base::Time::NowFromSystemTime() < timeout_) {
-      base::ThreadTaskRunnerHandle::Get()->PostTask(
-          FROM_HERE, base::BindOnce(base::IgnoreResult(&CheckWaiter::Check),
-                                    base::Unretained(this)));
+    if (callback_.Run() != expected_ && base::TimeTicks::Now() < timeout_) {
+      // Check again after a short timeout. Important: Don't use an immediate
+      // task to check again, because the pump would be allowed to run it
+      // immediately without processing system events (system events are
+      // required for the state to change).
+      base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+          FROM_HERE,
+          base::BindOnce(base::IgnoreResult(&CheckWaiter::Check),
+                         base::Unretained(this)),
+          TestTimeouts::tiny_timeout());
       return false;
     }
 
@@ -65,7 +71,7 @@ class CheckWaiter {
 
   base::RepeatingCallback<bool()> callback_;
   bool expected_;
-  base::Time timeout_;
+  const base::TimeTicks timeout_;
   // The waiter's RunLoop quit closure.
   base::RepeatingClosure quit_;
 };
