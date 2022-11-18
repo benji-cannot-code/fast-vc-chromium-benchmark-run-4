34f264225222f3cdaeb0b6ef9bc471238f114088
Louis Romero <lpromero@google.com>
2022-11-18 09:37:21 +0000

    Support ESC shortcut in Site Info page
    This CL adds support for dismissing the Site info sheet from the
    keyboard with the ESC key.
    
    Bug: 1385763
    Change-Id: I1712fd359fc1eb7252c56453aac51b667687c13d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034326
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073276}

--- ios/chrome/browser/ui/page_info/page_info_view_controller.mm
+++ ios/chrome/browser/ui/page_info/page_info_view_controller.mm
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/page_info/page_info_view_controller.h"
 
 #import "base/mac/foundation_util.h"
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/net/crurl.h"
 #import "ios/chrome/browser/ui/commands/page_info_commands.h"
 #import "ios/chrome/browser/ui/icons/symbols.h"
+#import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
 #import "ios/chrome/browser/ui/page_info/page_info_constants.h"
 #import "ios/chrome/browser/ui/permissions/permission_info.h"
 #import "ios/chrome/browser/ui/permissions/permissions_constants.h"
@@ -255,6 +258,23 @@ float kTitleLabelMinimumScaleFactor = 0.7f;
 
 - (void)presentationControllerDidDismiss:
     (UIPresentationController*)presentationController {
+  [self.pageInfoCommandsHandler hidePageInfo];
+}
+
+#pragma mark - UIResponder
+
+// To always be able to register key commands via -keyCommands, the VC must be
+// able to become first responder.
+- (BOOL)canBecomeFirstResponder {
+  return YES;
+}
+
+- (NSArray*)keyCommands {
+  return @[ UIKeyCommand.cr_close ];
+}
+
+- (void)keyCommand_close {
+  base::RecordAction(base::UserMetricsAction("MobileKeyCommandClose"));
   [self.pageInfoCommandsHandler hidePageInfo];
 }
 
