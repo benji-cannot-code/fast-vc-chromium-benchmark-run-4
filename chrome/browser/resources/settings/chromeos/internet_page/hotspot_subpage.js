a94bfed561be1819fd9fe05e0859493e4a865b0d
Jason Zhang <jiajunz@google.com>
2022-11-18 05:58:47 +0000

    [CrOS Hotspot] Add hotspot subpage
    This CL adds basic functionality to hotspot subpage with a description
    text and learn more link. Additionally, it also implements route to
    subpage when clicking on the hotspot summary row. It also adds
    a browser test to test hotspot subpage and hotspot summary item
    
    Screenshot: https://screenshot.googleplex.com/ML386A3XjPxm8kr
    
    Bug: b/239477916
    Change-Id: I45cb5e4d4812456b2e323acc3b3b9fd2ebef288d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022538
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073226}

---
+++ chrome/browser/resources/settings/chromeos/internet_page/hotspot_subpage.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * Settings subpage for managing Hotspot.
+ */
+
+import './internet_shared_css.js';
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/** @polymer */
+class SettingsHotspotSubpageElement extends PolymerElement {
+  static get is() {
+    return 'settings-hotspot-subpage';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+}
+
+customElements.define(
+    SettingsHotspotSubpageElement.is, SettingsHotspotSubpageElement);
