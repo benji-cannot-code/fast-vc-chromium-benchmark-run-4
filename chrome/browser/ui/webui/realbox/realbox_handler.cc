ce1e37e7169dd16952fc1c1a3069da09ea5f4518
John Lee <johntlee@chromium.org>
2022-11-18 02:58:45 +0000

    Realbox: Use chrome://images for image icons in suggestions
    This CL updates the way images of realbox suggestions are grabbed.
    It now uses SanitizedImageSource and its endpoint chrome://images to
    embed images related to various matches, which eliminates the need
    for callbacks, prevents extra loading of images for suggestions that
    are never shown, and provides for cleaner, simpler code.
    
    This CL also removes some unused code from the previous update of
    using chrome://favicon2 for favicons.
    
    Bug: 1279611
    Change-Id: Ic54a2fe61c6b772d333cc02f72acc95ef9c3b96d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3997604
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073185}

--- chrome/browser/ui/webui/realbox/realbox_handler.cc
+++ chrome/browser/ui/webui/realbox/realbox_handler.cc
@@ -19,10 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
-#include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
-#include "chrome/browser/favicon/favicon_service_factory.h"
-#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h"
@@ -508,22 +505,9 @@ RealboxHandler::RealboxHandler(
     content::WebContents* web_contents)
     : profile_(profile),
       web_contents_(web_contents),
-      bitmap_fetcher_service_(
-          BitmapFetcherServiceFactory::GetForBrowserContext(profile)),
-      favicon_cache_(FaviconServiceFactory::GetForProfile(
-                         profile,
-                         ServiceAccessType::EXPLICIT_ACCESS),
-                     HistoryServiceFactory::GetForProfile(
-                         profile,
-                         ServiceAccessType::EXPLICIT_ACCESS)),
       page_handler_(this, std::move(pending_page_handler)) {}
 
-RealboxHandler::~RealboxHandler() {
-  // Clear pending bitmap requests.
-  for (auto bitmap_request_id : bitmap_request_ids_) {
-    bitmap_fetcher_service_->CancelRequest(bitmap_request_id);
-  }
-}
+RealboxHandler::~RealboxHandler() = default;
 
 void RealboxHandler::SetPage(
     mojo::PendingRemote<realbox::mojom::Page> pending_page) {
@@ -823,34 +807,4 @@ void RealboxHandler::OnResultChanged(AutocompleteController* controller,
           web_contents_, autocomplete_controller_->result());
     }
   }
-
-  // Clear pending bitmap requests before requesting new ones.
-  for (auto bitmap_request_id : bitmap_request_ids_) {
-    bitmap_fetcher_service_->CancelRequest(bitmap_request_id);
-  }
-  bitmap_request_ids_.clear();
-
-  int match_index = -1;
-  for (const auto& match : autocomplete_controller_->result()) {
-    match_index++;
-
-    // Request bitmaps for matche images.
-    if (!match.ImageUrl().is_empty()) {
-      bitmap_request_ids_.push_back(bitmap_fetcher_service_->RequestImage(
-          match.ImageUrl(),
-          base::BindOnce(&RealboxHandler::OnRealboxBitmapFetched,
-                         weak_ptr_factory_.GetWeakPtr(), match_index,
-                         match.ImageUrl())));
-    }
-  }
-}
-
-void RealboxHandler::OnRealboxBitmapFetched(int match_index,
-                                            const GURL& image_url,
-                                            const SkBitmap& bitmap) {
-  auto data = gfx::Image::CreateFrom1xBitmap(bitmap).As1xPNGBytes();
-  std::string data_url =
-      webui::GetPngDataUrl(data->front_as<unsigned char>(), data->size());
-
-  page_->AutocompleteMatchImageAvailable(match_index, image_url, data_url);
 }
