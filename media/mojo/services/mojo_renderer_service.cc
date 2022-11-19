8a0e1acf6871f986f2b02f34358102e2343d2555
Xiaohan Wang <xhwang@chromium.org>
2022-11-19 00:13:09 +0000

    media: Stop media time update after error in MojoRendererService
    After an error, we should stop media time update just in case the
    failing underlying renderer is still updating media time for any
    reason.
    
    Bug: 1385259
    Change-Id: I8f9f83bc34bea0721f5fb66cb84e76fa5a5c3624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4038942
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073648}

--- media/mojo/services/mojo_renderer_service.cc
+++ media/mojo/services/mojo_renderer_service.cc
@@ -161,6 +161,7 @@ void MojoRendererService::SetCdm(
 void MojoRendererService::OnError(PipelineStatus error) {
   DVLOG(1) << __func__ << "(" << error << ")";
   state_ = STATE_ERROR;
+  CancelPeriodicMediaTimeUpdates();
   client_->OnError(std::move(error));
 }
 
