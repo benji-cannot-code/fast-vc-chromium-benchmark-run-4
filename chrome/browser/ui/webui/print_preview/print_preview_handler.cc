db758f0fb675d56c98fae3ab9a1283041d6c35c6
Lei Zhang <thestig@chromium.org>
2022-11-19 01:05:55 +0000

    Expire various expired PrintPreview histograms.
    Many of these are long expired.
    
    - Deleted the entries from histograms.xml files.
    - Remove the code that generate these histograms.
    
    Bug: 1384767,1384936
    Change-Id: I4ed067a45b513881146edfec78275260d1900c1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029053
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073671}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -666,9 +666,6 @@ void PrintPreviewHandler::HandleGetPreview(const base::Value::List& args) {
   settings.Set(kPreviewUIID,
                print_preview_ui()->GetIDForPrintPreviewUI().value());
 
-  // Increment request count.
-  ++regenerate_preview_request_count_;
-
   WebContents* initiator = GetInitiator();
   RenderFrameHost* rfh =
       initiator
@@ -711,8 +708,6 @@ void PrintPreviewHandler::HandleGetPreview(const base::Value::List& args) {
 }
 
 void PrintPreviewHandler::HandlePrint(const base::Value::List& args) {
-  ReportRegeneratePreviewRequestCountBeforePrint(
-      regenerate_preview_request_count_);
   CHECK(args[0].is_string());
   const std::string& callback_id = args[0].GetString();
   CHECK(!callback_id.empty());
@@ -808,9 +803,6 @@ void PrintPreviewHandler::HandleShowSystemDialog(
 void PrintPreviewHandler::HandleClosePreviewDialog(
     const base::Value::List& /*args*/) {
   ReportUserActionHistogram(UserActionBuckets::kCancel);
-
-  ReportRegeneratePreviewRequestCountBeforeCancel(
-      regenerate_preview_request_count_);
 }
 
 void PrintPreviewHandler::GetLocaleInformation(base::Value::Dict* settings) {
