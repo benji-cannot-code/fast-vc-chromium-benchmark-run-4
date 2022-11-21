be3546a077a20247e8b5f929421e74b9daf2ca3e
Justin Lulejian <jlulejian@chromium.org>
2022-11-21 21:39:51 +0000

    [Extensions] Refactor default_popup origin validation to Parse().
    Before this we would store the invalid value in ActionInfo, however
    further discussion showed that we should not store the value if we never
    intend on using it.
    
    Context: https://crbug.com/1353210#c8
    
    Bug: 897040
    Change-Id: Ic47452a842bf9dd3d3645239f99ade6136841244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989276
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074268}

--- extensions/common/manifest_handlers/extension_action_handler.cc
+++ extensions/common/manifest_handlers/extension_action_handler.cc
@@ -17,10 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/image_util.h"
 #include "extensions/common/manifest_constants.h"
 
-// Adds `extensions::InstallWarning`s to an `extensions::Extension` if the
-// `default_popup` value for the action is not same origin, or doesn't
-// exist in the filesystem.
-void SetWarningsForInvalidDefaultPopup(
+// Adds `extensions::InstallWarning`s to `warnings` if the`default_popup` value
+// for the action doesn't exist in the filesystem.
+void SetWarningsForNonExistentDefaultPopup(
     const extensions::ActionInfo* action,
     const char* manifest_key,
     const extensions::Extension* extension,
@@ -36,13 +35,7 @@ void SetWarningsForInvalidDefaultPopup(
   base::FilePath resource_path =
       extension->GetResource(relative_path).GetFilePath();
 
-  // Check popup is only for this extension.
-  if (!extension->origin().IsSameOriginWith(default_popup_url)) {
-    warnings->push_back(extensions::InstallWarning(
-        extensions::manifest_errors::kInvalidExtensionOriginPopup, manifest_key,
-        extensions::manifest_keys::kActionDefaultPopup));
-    // Check that the popup file actually exists on filesystem.
-  } else if (resource_path.empty() || !base::PathExists(resource_path)) {
+  if (resource_path.empty() || !base::PathExists(resource_path)) {
     warnings->push_back(extensions::InstallWarning(
         extensions::manifest_errors::kNonexistentDefaultPopup, manifest_key,
         extensions::manifest_keys::kActionDefaultPopup));
@@ -95,8 +88,10 @@ bool ExtensionActionHandler::Parse(Extension* extension,
       return false;
     }
 
+    std::vector<InstallWarning> install_warnings;
     std::unique_ptr<ActionInfo> action_info =
-        ActionInfo::Load(extension, type, dict, error);
+        ActionInfo::Load(extension, type, dict, &install_warnings, error);
+    extension->AddInstallWarnings(std::move(install_warnings));
     if (!action_info)
       return false;  // Failed to parse extension action definition.
 
@@ -138,7 +133,8 @@ bool ExtensionActionHandler::Validate(
       ActionInfo::GetManifestKeyForActionType(action->type);
   DCHECK(manifest_key);
 
-  SetWarningsForInvalidDefaultPopup(action, manifest_key, extension, warnings);
+  SetWarningsForNonExistentDefaultPopup(action, manifest_key, extension,
+                                        warnings);
 
   // Empty default icon is valid.
   if (action->default_icon.empty())
