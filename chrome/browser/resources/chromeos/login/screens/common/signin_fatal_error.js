7dbbdfa316adb2bce559c01551664259688214a3
Brahim Chikhaoui <bchikhaoui@google.com>
2022-11-21 19:27:09 +0000

    OOBE - Polymer3 - Check-in SigninFatalError Screen
    Check-in the modularised version of OOBE's Signin Fatal
    Error Screen.
    
    Bug: 1184731, b/243383141
    Change-Id: I3dac3af7a0b3bba64026c6658d004a682dc0bdff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871263
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074147}

--- chrome/browser/resources/chromeos/login/screens/common/signin_fatal_error.js
+++ chrome/browser/resources/chromeos/login/screens/common/signin_fatal_error.js
@@ -7,7 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for signin fatal error.
  */
 
-/* #js_imports_placeholder */
+import '//resources/js/action_link.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../../components/oobe_icons.m.js';
+import '../../components/common_styles/common_styles.m.js';
+import '../../components/common_styles/oobe_dialog_host_styles.m.js';
+import '../../components/dialogs/oobe_adaptive_dialog.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.m.js';
+import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
+import {OobeTextButton} from '../../components/buttons/oobe_text_button.js';
+import {OOBE_UI_STATE, SCREEN_GAIA_SIGNIN} from '../../components/display_manager_types.m.js';
+import {OobeTypes} from '../../components/oobe_types.m.js';
+
 
 /**
  * @constructor
@@ -15,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @implements {LoginScreenBehaviorInterface}
  * @implements {OobeI18nBehaviorInterface}
  */
-const SigninFatalErrorBase = Polymer.mixinBehaviors(
+const SigninFatalErrorBase = mixinBehaviors(
     [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    Polymer.Element);
+    PolymerElement);
 
 /**
  * @typedef {{
@@ -34,7 +49,10 @@ class SigninFatalScreen extends SigninFatalErrorBase {
     return 'signin-fatal-error-element';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
 
   static get properties() {
     return {
@@ -53,6 +71,7 @@ class SigninFatalScreen extends SigninFatalErrorBase {
        */
       errorState_: {
         type: Number,
+        value: 0,
       },
 
       /**
@@ -61,6 +80,7 @@ class SigninFatalScreen extends SigninFatalErrorBase {
        */
       params_: {
         type: Object,
+        value: {},
       },
 
       keyboardHint_: {
@@ -77,11 +97,6 @@ class SigninFatalScreen extends SigninFatalErrorBase {
     };
   }
 
-  constructor() {
-    super();
-    this.errorState_ = 0;
-    this.params_ = {};
-  }
 
   ready() {
     super.ready();
