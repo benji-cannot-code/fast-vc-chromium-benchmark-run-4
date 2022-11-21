97c49b1becb84e82b48e9d3602de0f2e75c338b0
Markus Handell <handellm@google.com>
2022-11-21 09:32:24 +0000

    Reland: Migrate MediaStreamVideoSource's IO thread to sequence under feature.
    (Last attempt to land,
    https://chromium-review.googlesource.com/c/chromium/src/+/3990566
    failed due to use of a static variable. This version creates
    a public blink feature which gets used from the blink platform instead.)
    
    Data shows migrating MediaStream's IO thread to sequence helps
    context switches which should eventually help reduce frequency
    and thereby power.
    
    This change introduces default-off feature
    UseThreadPoolForMediaStreamVideoTaskRunner which if enabled backs the IO
    task runner from the thread pool.
    
    Bug: 1379591
    Change-Id: I80bb5850c1425ac4def67ffb8fd92de171ea4cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4023370
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073976}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_video_frame_submitter.h"
 #include "third_party/blink/public/web/blink.h"
 #include "third_party/blink/public/web/modules/media/audio/audio_device_factory.h"
+#include "third_party/blink/public/web/modules/mediastream/media_stream_video_source.h"
 #include "third_party/blink/public/web/modules/mediastream/webmediaplayer_ms.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "url/origin.h"
@@ -792,7 +793,7 @@ blink::WebMediaPlayer* MediaFactory::CreateWebMediaPlayerForMediaStream(
   return new blink::WebMediaPlayerMS(
       frame, client, GetWebMediaPlayerDelegate(), std::move(media_log),
       render_frame_->GetTaskRunner(blink::TaskType::kInternalMedia),
-      render_thread->GetIOTaskRunner(),
+      blink::Platform::Current()->GetMediaStreamVideoSourceVideoTaskRunner(),
       GetOrCreateVideoFrameCompositorTaskRunner(render_frame_),
       render_thread->GetMediaSequencedTaskRunner(),
       std::move(compositor_worker_task_runner),
--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1662,5 +1662,9 @@ BASE_FEATURE(kStartMediaStreamCaptureIndicatorInBrowser,
              "StartMediaStreamCaptureIndicatorInBrowser",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kUseThreadPoolForMediaStreamVideoTaskRunner,
+             "UseThreadPoolForMediaStreamVideoTaskRunner",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
 }  // namespace blink
