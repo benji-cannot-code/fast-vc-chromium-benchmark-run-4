9ecf2bb8ad927e2346c34b111803418aea080406
Jagdish Chourasia <jagdish.c@samsung.com>
2022-11-21 13:05:03 +0000

    [Code Health] Fix include order
    There was violation of include order. Correct the same.
    
    Change-Id: I122dc536917e05eca5c84ea53da1289873793ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4042265
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Jagdish Chourasia <jagdish.c@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1074037}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
-#include "base/memory/raw_ptr.h"
 
+#include "base/memory/raw_ptr.h"
 #include "content/public/browser/document_picture_in_picture_window_controller.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/picture_in_picture_window_controller.h"
