d7df9a94b7b1675c6463714d6941ace654b0625b
Kenichi Ishibashi <bashi@chromium.org>
2022-11-21 07:10:24 +0000

    Make sure CronetURLRequest has valid read buffer
    Bug: 1335423
    Change-Id: Ifa95ea1b79c9ac048cf1937e97a05fe78dee4a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4041767
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073947}

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "net/base/auth.h"
+#include "net/base/io_buffer.h"
 #include "net/base/load_flags.h"
 #include "net/base/load_timing_info.h"
 #include "net/base/net_errors.h"
@@ -777,6 +778,9 @@ int URLRequest::Read(IOBuffer* dest, int dest_size) {
     // Caller is not too bright.  I guess we've done what they asked.
     return OK;
   }
+
+  // Caller should provide a buffer.
+  DCHECK(dest && dest->data());
 
   int rv = job_->Read(dest, dest_size);
   if (rv == ERR_IO_PENDING) {
