abd5989be850a6dfb9cb8fd25660e308cba49fb2
Matthias Körber <koerber@google.com>
2022-11-21 09:54:20 +0000

    Remove AutofillUiImprovements features
    This CL removes 3 feature flags that are associated with an effort
    that was enabled by default and launched on all desktop platforms.
    
    Bug: 1274134
    Change-Id: Ie0047e63fc7e4563a0dc0d38bf4a58dfb0d95b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030556
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073986}

--- components/password_manager/core/browser/password_autofill_manager.cc
+++ components/password_manager/core/browser/password_autofill_manager.cc
@@ -223,9 +223,7 @@ void MaybeAppendManagePasswordsEntry(
   // Add a separator before the manage option unless there are no suggestions
   // yet.
   // TODO(crbug.com/1274134): Clean up once improvements are launched.
-  if (base::FeatureList::IsEnabled(
-          autofill::features::kAutofillVisualImprovementsForSuggestionUi) &&
-      !suggestions->empty()) {
+  if (!suggestions->empty()) {
     suggestions->push_back(
         autofill::Suggestion(autofill::POPUP_ITEM_ID_SEPARATOR));
   }
@@ -282,11 +280,7 @@ autofill::Suggestion CreateEntryToOptInToAccountStorageThenGenerate() {
       l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_GENERATE_PASSWORD));
   suggestion.frontend_id =
       autofill::POPUP_ITEM_ID_PASSWORD_ACCOUNT_STORAGE_OPT_IN_AND_GENERATE;
-  suggestion.icon =
-      base::FeatureList::IsEnabled(
-          autofill::features::kAutofillVisualImprovementsForSuggestionUi)
-          ? "keyIcon"
-          : "google";
+  suggestion.icon = "keyIcon";
   return suggestion;
 }
 
@@ -768,13 +762,6 @@ std::vector<autofill::Suggestion> PasswordAutofillManager::BuildSuggestions(
                               : CreateGenerationEntry());
   }
 
-  // TODO(crbug.com/1274134): Delete once improvements are launched.
-  if (!base::FeatureList::IsEnabled(
-          autofill::features::kAutofillVisualImprovementsForSuggestionUi)) {
-    // Add "Manage all passwords" link to settings.
-    MaybeAppendManagePasswordsEntry(&suggestions);
-  }
-
   // Add button to opt into using the account storage for passwords and then
   // suggest.
   if (show_account_storage_optin)
@@ -784,13 +771,9 @@ std::vector<autofill::Suggestion> PasswordAutofillManager::BuildSuggestions(
   if (show_account_storage_resignin)
     suggestions.push_back(CreateEntryToReSignin());
 
-  // TODO(crbug.com/1274134): Remove feature flag once improvements are
-  // launched.
-  if (base::FeatureList::IsEnabled(
-          autofill::features::kAutofillVisualImprovementsForSuggestionUi)) {
-    // Add "Manage all passwords" link to settings.
-    MaybeAppendManagePasswordsEntry(&suggestions);
-  }
+  // Add "Manage all passwords" link to settings.
+  MaybeAppendManagePasswordsEntry(&suggestions);
+
   return suggestions;
 }
 
