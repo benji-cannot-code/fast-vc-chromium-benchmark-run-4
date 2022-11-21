b8afe2a4454e5c0408ddd89d783f5ce7e0745b9d
Danila Kuzmin <dkuzmin@google.com>
2022-11-21 11:13:15 +0000

    [OOBE][RecommendApps]: Revamp cleanup part 1/3
    Remove dependency from kAppDiscoveryForOobe and kOobeNewRecommendApps
    feature flags in screen, handler and related browser tests.
    
    Feature was enabled by default since M105.
    
    Bug: 1261902
    Change-Id: Ida2cf79b64810db2f30a47f6d85875d07f9c2485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028008
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Auto-Submit: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074003}

--- chrome/browser/ui/webui/ash/login/recommend_apps_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/recommend_apps_screen_handler.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/login/recommend_apps_screen_handler.h"
 
-#include "ash/constants/ash_features.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/values.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/grit/component_extension_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
@@ -43,19 +41,7 @@ RecommendAppsScreenHandler::~RecommendAppsScreenHandler() = default;
 
 void RecommendAppsScreenHandler::DeclareLocalizedValues(
     ::login::LocalizedValuesBuilder* builder) {
-  // TODO(crbug.com/1261902): Clean-up old strings once feature is launched.
   builder->Add("recommendAppsLoading", IDS_LOGIN_RECOMMEND_APPS_SCREEN_LOADING);
-  if (!features::IsOobeNewRecommendAppsEnabled()) {
-    builder->Add("recommendAppsScreenTitle",
-                 IDS_LOGIN_RECOMMEND_APPS_OLD_SCREEN_TITLE);
-    builder->Add("recommendAppsScreenDescription",
-                 IDS_LOGIN_RECOMMEND_APPS_OLD_SCREEN_DESCRIPTION);
-    builder->Add("recommendAppsSkip", IDS_LOGIN_RECOMMEND_APPS_DO_IT_LATER);
-    builder->Add("recommendAppsInstall", IDS_LOGIN_RECOMMEND_APPS_DONE);
-    builder->Add("recommendAppsSelectAll",
-                 IDS_LOGIN_RECOMMEND_APPS_OLD_SELECT_ALL);
-    return;
-  }
   builder->AddF("recommendAppsScreenTitle",
                 IDS_LOGIN_RECOMMEND_APPS_SCREEN_TITLE,
                 ui::GetChromeOSDeviceName());
@@ -74,18 +60,9 @@ void RecommendAppsScreenHandler::DeclareLocalizedValues(
                IDS_LOGIN_RECOMMEND_APPS_SCREEN_DESCRIPTION_EXPAND_BUTTON);
 }
 
-void RecommendAppsScreenHandler::GetAdditionalParameters(
-    base::Value::Dict* dict) {
-  dict->Set("isOobeNewRecommendAppsEnabled",
-            features::IsOobeNewRecommendAppsEnabled());
-  BaseScreenHandler::GetAdditionalParameters(dict);
-}
-
 void RecommendAppsScreenHandler::Show() {
   ShowInWebUI();
 }
-
-void RecommendAppsScreenHandler::Hide() {}
 
 void RecommendAppsScreenHandler::OnLoadSuccess(base::Value app_list) {
   LoadAppListInUI(std::move(app_list));
@@ -97,16 +74,6 @@ void RecommendAppsScreenHandler::OnParseResponseError() {
 
 void RecommendAppsScreenHandler::LoadAppListInUI(base::Value app_list) {
   RecordUmaScreenState(RecommendAppsScreenState::SHOW);
-  // TODO(crbug.com/1261902): Clean-up old implementation once feature is
-  // launched.
-  if (!features::IsOobeNewRecommendAppsEnabled() ||
-      !base::FeatureList::IsEnabled(::features::kAppDiscoveryForOobe)) {
-    const ui::ResourceBundle& resource_bundle =
-        ui::ResourceBundle::GetSharedInstance();
-    std::string app_list_webview = resource_bundle.LoadDataResourceString(
-        IDR_ARC_SUPPORT_RECOMMEND_APP_LIST_VIEW_HTML);
-    CallExternalAPI("setWebview", app_list_webview);
-  }
   CallExternalAPI("loadAppList", std::move(app_list));
 }
 
