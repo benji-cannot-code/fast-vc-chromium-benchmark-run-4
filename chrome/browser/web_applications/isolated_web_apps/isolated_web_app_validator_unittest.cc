619207abf66a34fb9f776de887e91c71b0b2f648
Christian Flach <cmfcmf@chromium.org>
2022-11-21 15:53:14 +0000

    web-bundles: Store missing primary URL as `nullopt` instead of GURL()
    This CL updates the Web Bundle parsing logic to represent a
    non-specified primary URL as `absl::nullopt` instead of an invalid,
    empty `GURL`.
    
    Bug: 1385394
    Change-Id: I15af1b37ef4f1866b5e7276ee29270b4704ce893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030549
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074095}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_validator_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_validator_unittest.cc
@@ -108,7 +108,7 @@ TEST_F(IsolatedWebAppValidatorIntegrityBlockTest, EmptyPublicKeyStack) {
 class IsolatedWebAppValidatorMetadataTest
     : public IsolatedWebAppValidatorTest,
       public ::testing::WithParamInterface<
-          std::tuple<std::string,
+          std::tuple<absl::optional<std::string>,
                      std::vector<std::string>,
                      absl::optional<std::string>>> {
  public:
@@ -121,7 +121,7 @@ class IsolatedWebAppValidatorMetadataTest
   }
 
  protected:
-  GURL primary_url_;
+  absl::optional<GURL> primary_url_;
   std::vector<GURL> entries_;
   absl::optional<std::string> error_message_;
 };
@@ -140,12 +140,14 @@ INSTANTIATE_TEST_SUITE_P(
     All,
     IsolatedWebAppValidatorMetadataTest,
     ::testing::Values(
-        std::make_tuple("", std::vector<std::string>({kUrl}), absl::nullopt),
-        std::make_tuple("",
+        std::make_tuple(absl::nullopt,
+                        std::vector<std::string>({kUrl}),
+                        absl::nullopt),
+        std::make_tuple(absl::nullopt,
                         std::vector<std::string>({kUrl, kUrl + "/foo#bar"}),
                         "The URL of an exchange is invalid: URLs must not have "
                         "a fragment part."),
-        std::make_tuple("",
+        std::make_tuple(absl::nullopt,
                         std::vector<std::string>({kUrl, kUrl + "/foo?bar"}),
                         "The URL of an exchange is invalid: URLs must not have "
                         "a query part."),
@@ -154,12 +156,12 @@ INSTANTIATE_TEST_SUITE_P(
             std::vector<std::string>({kUrl}),
             "Primary URL must not be present, but was isolated-app://"
             "aerugqztij5biqquuk3mfwpsaibuegaqcitgfchwuosuofdjabzqaaic/"),
-        std::make_tuple("",
+        std::make_tuple(absl::nullopt,
                         std::vector<std::string>({kUrl, "https://foo/"}),
                         "The URL of an exchange is invalid: The URL scheme "
                         "must be isolated-app, but was https"),
         std::make_tuple(
-            "",
+            absl::nullopt,
             std::vector<std::string>({kUrl, kUrlFromAnotherIsolatedWebApp}),
             "The URL of an exchange contains the wrong Signed Web Bundle ID: "
             "berugqztij5biqquuk3mfwpsaibuegaqcitgfchwuosuofdjabzqaaic")));
