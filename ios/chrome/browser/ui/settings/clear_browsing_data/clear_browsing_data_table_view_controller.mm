c3ef7e9d316cb9070edbc7cbd6f62c337a4822f5
Arthur Milchior <arthurmilchior@chromium.org>
2022-11-21 13:25:59 +0000

    [iOS] CBD histogram indicates it comes from CBD
    Tested: manually with signing-out from CBD and from the profile settings.
    In both case, it leads to the expected histogram.
    
    Bug: 1341654
    Change-Id: I773c923b9b8864cd28f5cdb61c7422a26d6ab188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034167
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074042}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
@@ -350,9 +350,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)view:(TableViewLinkHeaderFooterView*)view didTapLinkURL:(CrURL*)url {
   if (url.gurl == GURL(kCBDSignOutOfChromeURL)) {
     DCHECK(base::FeatureList::IsEnabled(switches::kEnableCbdSignOut));
-    // TODO(crbug.com/1341654): Log a user action indicating that the user
-    // clicked on the sign out link from the footer. Remove the action
-    // indicating that this came from signin > signout.
     [self showSignOutWithItemView:[view contentView]];
     return;
   }
@@ -555,6 +552,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Offer the user to sign-out near itemView
 // If they sync, they can keep or delete their data.
+// TODO(crbug.com/1385791) Test that correct histogram is registered.
 - (void)showSignOutWithItemView:(UIView*)itemView {
   if (_signoutCoordinator) {
     // An action is already in progress, ignore user's request.
