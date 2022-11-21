4bc612ce5b80431b63f1b722b8fca73785360526
Vikram Pasupathy <vpasupathy@chromium.org>
2022-11-21 16:42:42 +0000

    Clear Site Specific Media License Data on Windows
    This CL now allows users on Windows to clear Media License Data for
    specific sites.
    
    Testing:
    1. Manual repro now clears site specific data on prefs-internals.
    2. Also added a unit test to catch any regressions in the future.
    
    Bug: b/255829707
    Change-Id: I602f40ebe86e8ad117b1f7e3fb1a0bbf3ac361a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032009
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074111}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -94,6 +94,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #endif
 
+#if BUILDFLAG(IS_WIN)
+#include "chrome/browser/media/cdm_document_service_impl.h"
+#endif
+
 using extensions::mojom::APIPermissionID;
 
 namespace settings {
@@ -2225,6 +2229,29 @@ void SiteSettingsHandler::RemoveNonTreeModelData(
   // by the model.
   for (const auto& origin : origins)
     browsing_data_model_->RemoveBrowsingData(origin.host(), base::DoNothing());
+
+#if BUILDFLAG(IS_WIN)
+  // Removes any Media License Data associated with the origin that is not
+  // stored in quota nodes. This should only be on Windows as ChromeOS does
+  // not support removing Media License Data per origin, and
+  // site_settings_handler.cc does not handle Android site specific code.
+  // The code for Android site specific code is located in
+  // components/browser_ui/site_settings/android/website_preference_bridge.cc
+  // TODO(b/248311157) - When CrOS supports the ability to delete platform
+  // keys by domain, implement the CrOS specific logic regarding clearing site
+  // specific media license data.
+  // TODO(b/248311157) - When the migration to BrowsingDataModel is finished,
+  // remove this and integrate the media license data removal steps there.
+  auto filter_builder = content::BrowsingDataFilterBuilder::Create(
+      content::BrowsingDataFilterBuilder::Mode::kDelete);
+
+  for (const auto& origin : origins)
+    filter_builder->AddOrigin(origin);
+
+  CdmDocumentServiceImpl::ClearCdmData(
+      profile_, base::Time::Min(), base::Time::Max(),
+      filter_builder->BuildUrlFilter(), base::DoNothing());
+#endif  // BUILDFLAG(IS_WIN)
 }
 
 void SiteSettingsHandler::SetModelsForTesting(
