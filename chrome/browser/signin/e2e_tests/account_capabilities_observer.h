ff294e52b0280dde104fb82ebc0c6b66b16ba6fb
Madhav Srinivasan <msrini@google.com>
2022-11-21 22:33:50 +0000

    Refactor live_sign_in_test to make Sign In code reusable
    The goal of this is to make the sign in flow reusable so that other
    tests can use them. Also groups some functions into a new class
    SignInFunctions for conciseness.
    
    Change-Id: I3f485b8c1d048b653fcd5d8c4fa9c8c56bd908eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032400
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Madhav Srinivasan <msrini@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074301}

---
+++ chrome/browser/signin/e2e_tests/account_capabilities_observer.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SIGNIN_E2E_TESTS_ACCOUNT_CAPABILITIES_OBSERVER_H_
+#define CHROME_BROWSER_SIGNIN_E2E_TESTS_ACCOUNT_CAPABILITIES_OBSERVER_H_
+
+#include "base/run_loop.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+
+namespace signin::test {
+
+// Observer class allowing to wait for account capabilities to be known.
+class AccountCapabilitiesObserver : public IdentityManager::Observer {
+ public:
+  explicit AccountCapabilitiesObserver(IdentityManager* identity_manager);
+  ~AccountCapabilitiesObserver() override;
+
+  // IdentityManager::Observer:
+  void OnExtendedAccountInfoUpdated(const AccountInfo& info) override;
+
+  // This should be called only once per AccountCapabilitiesObserver instance.
+  void WaitForAllCapabilitiesToBeKnown(CoreAccountId account_id);
+
+ private:
+  raw_ptr<IdentityManager> identity_manager_ = nullptr;
+  CoreAccountId account_id_;
+  base::RunLoop run_loop_;
+  base::ScopedObservation<IdentityManager, IdentityManager::Observer>
+      identity_manager_observation_{this};
+};
+
+}  // namespace signin::test
+
+#endif  // CHROME_BROWSER_SIGNIN_E2E_TESTS_ACCOUNT_CAPABILITIES_OBSERVER_H_
