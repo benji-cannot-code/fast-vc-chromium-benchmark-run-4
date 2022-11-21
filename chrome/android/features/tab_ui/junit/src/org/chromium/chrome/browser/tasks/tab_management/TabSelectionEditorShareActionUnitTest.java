58800e6387347925a28fe1552a97038f2c17e7be
Andrew Grieve <agrieve@chromium.org>
2022-11-21 20:23:28 +0000

    TabSelectionEditorTest: Add missing tearDown() for global state.
    Test was causing SecondaryTasksSurfaceViewBinderUnitTest to fail on
    androidx roller due to not resetting a call to:
    TabSelectionEditorShareAction.setIntentCallbackForTesting();
    
    Also changes a HashMap subclass to a Map.of(), because I could not
    resist.
    
    Bug: 1386262
    Change-Id: Ide2530dcb06fbaec66fbb90054ac621194fa9a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043762
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074233}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorShareActionUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorShareActionUnitTest.java
@@ -16,6 +16,7 @@ import android.graphics.drawable.Drawable;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -48,7 +49,6 @@ import org.chromium.url.GURL;
 import org.chromium.url.JUnitTestGURLs;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -75,15 +75,11 @@ public class TabSelectionEditorShareActionUnitTest {
     private MockTabModel mTabModel;
     private TabSelectionEditorShareAction mAction;
 
-    Map<Integer, GURL> mIdUrlMap = new HashMap<Integer, GURL>() {
-        {
-            put(1, JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_1));
-            put(2, JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_2));
-            put(3, JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_3));
-            put(4, JUnitTestGURLs.getGURL(JUnitTestGURLs.NTP_URL));
-            put(5, JUnitTestGURLs.getGURL(JUnitTestGURLs.ABOUT_BLANK));
-        }
-    };
+    Map<Integer, GURL> mIdUrlMap = Map.of(1, JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_1), 2,
+            JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_2), 3,
+            JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_3), 4,
+            JUnitTestGURLs.getGURL(JUnitTestGURLs.NTP_URL), 5,
+            JUnitTestGURLs.getGURL(JUnitTestGURLs.ABOUT_BLANK));
 
     @Before
     public void setUp() {
@@ -104,6 +100,11 @@ public class TabSelectionEditorShareActionUnitTest {
                 .thenReturn(new TabModelFilterProvider());
         mJniMocker.mock(DomDistillerUrlUtilsJni.TEST_HOOKS, mDomDistillerUrlUtilsJni);
         mAction.configure(mTabModelSelector, mSelectionDelegate, mDelegate, false);
+    }
+
+    @After
+    public void tearDown() {
+        TabSelectionEditorShareAction.setIntentCallbackForTesting(null);
     }
 
     @Test
