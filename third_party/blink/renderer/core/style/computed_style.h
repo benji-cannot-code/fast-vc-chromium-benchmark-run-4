37ab15711530cade5c84ffc3097675484709711a
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-11-21 00:12:13 +0000

    [StyleBuilder] Make font related calls use StyleBuilder instead of Style
    This reduces the number of ComputedStyleBuilder::MutableInternalStyle
    calls, and StyleResolverState::Style calls.
    
    There should be no behaviour change.
    
    Bug: 1377295
    Change-Id: I546f9a9e507154eb9505df18c9f27eb7cfde11c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037700
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073879}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2807,6 +2807,12 @@ class ComputedStyleBuilder final : public ComputedStyleBuilderBase {
   const FontDescription& GetFontDescription() const {
     return GetFont().GetFontDescription();
   }
+  int FontSize() const { return GetFontDescription().ComputedPixelSize(); }
+  LayoutUnit FontHeight() const {
+    if (const SimpleFontData* font_data = GetFont().PrimaryFont())
+      return LayoutUnit(font_data->GetFontMetrics().Height());
+    return LayoutUnit();
+  }
   FontOrientation ComputeFontOrientation() const;
   void UpdateFontOrientation();
 
@@ -2815,6 +2821,12 @@ class ComputedStyleBuilder final : public ComputedStyleBuilderBase {
     FontDescription description(GetFontDescription());
     description.SetLetterSpacing(letter_spacing);
     SetFontDescription(description);
+  }
+
+  // line-height
+  bool HasInitialLineHeight() const {
+    return LineHeightInternal() ==
+           ComputedStyleInitialValues::InitialLineHeight();
   }
 
   // margin-*
