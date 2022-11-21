4302f03fc7296df6f3c682b00de30a82c10ab82a
Kateryna Prokopenko <kprokopenko@chromium.org>
2022-11-21 10:20:26 +0000

    [CacheStorage] Add methods to (un-)trackCacheStorage for storage key
    Bug: 1349331
    Change-Id: I9751c9cbe9a7fb0838b2781d87aaea11cd4dfe91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854640
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073993}

--- content/browser/devtools/protocol/storage_handler.h
+++ content/browser/devtools/protocol/storage_handler.h
@@ -83,7 +83,11 @@ class StorageHandler : public DevToolsDomainHandler,
 
   // Ignores all double calls to track an origin.
   Response TrackCacheStorageForOrigin(const std::string& origin) override;
+  Response TrackCacheStorageForStorageKey(
+      const std::string& storage_key) override;
   Response UntrackCacheStorageForOrigin(const std::string& origin) override;
+  Response UntrackCacheStorageForStorageKey(
+      const std::string& storage_key) override;
   Response TrackIndexedDBForOrigin(const std::string& origin) override;
   Response TrackIndexedDBForStorageKey(const std::string& storage_key) override;
   Response UntrackIndexedDBForOrigin(const std::string& origin) override;
@@ -153,8 +157,8 @@ class StorageHandler : public DevToolsDomainHandler,
       const std::string& owner_origin,
       const SharedStorageEventParams& params);
 
-  void NotifyCacheStorageListChanged(const std::string& origin);
-  void NotifyCacheStorageContentChanged(const std::string& origin,
+  void NotifyCacheStorageListChanged(const blink::StorageKey& storage_key);
+  void NotifyCacheStorageContentChanged(const blink::StorageKey& storage_key,
                                         const std::string& name);
   void NotifyIndexedDBListChanged(const std::string& origin,
                                   const std::string& storage_key);
