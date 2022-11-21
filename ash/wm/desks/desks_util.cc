ea4171421dd518f5a064f968d65ae993fa99a9c6
Yongshun Liu <yongshun@chromium.org>
2022-11-21 19:55:57 +0000

    Ash: Desk preview update with per-desk-z-order for all desk windows
    This makes desk preview sync with per-desk-z-order feature for all desk
    windows. It's done by putting all desk window below the nearest window
    that should be on top in the desk.
    
    Bug: b/255846698
    Change-Id: If06d2e6bcb616d1557fdbb491a1ca50f5907ce55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029182
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074197}

--- ash/wm/desks/desks_util.cc
+++ ash/wm/desks/desks_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_session.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
+#include "base/containers/adapters.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
@@ -193,6 +194,27 @@ bool IsDraggingAnyDesk() {
 bool IsWindowVisibleOnAllWorkspaces(const aura::Window* window) {
   return window->GetProperty(aura::client::kWindowWorkspaceKey) ==
          aura::client::kWindowWorkspaceVisibleOnAllWorkspaces;
+}
+
+bool IsZOrderTracked(aura::Window* window) {
+  return window->GetType() == aura::client::WindowType::WINDOW_TYPE_NORMAL &&
+         window->GetProperty(aura::client::kZOrderingKey) ==
+             ui::ZOrderLevel::kNormal;
+}
+
+absl::optional<size_t> GetWindowZOrder(
+    const std::vector<aura::Window*>& windows,
+    aura::Window* window) {
+  size_t position = 0;
+  for (aura::Window* w : base::Reversed(windows)) {
+    if (IsZOrderTracked(w)) {
+      if (w == window)
+        return position;
+      ++position;
+    }
+  }
+
+  return absl::nullopt;
 }
 
 }  // namespace desks_util
