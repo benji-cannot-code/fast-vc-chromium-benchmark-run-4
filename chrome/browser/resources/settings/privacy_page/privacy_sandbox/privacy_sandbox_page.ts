607a9514d07f7192e13b89e485b42a9a418f4e8f
Gabriel Oliveira <gabolvr@google.com>
2022-11-21 13:20:32 +0000

    [Privacy Sandbox] Create empty privacy sandbox settings page component
    Bug: 1378703, b/254413449
    Change-Id: Idf03a8cea237aba1a1e2d93766daae40460d719c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030443
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074041}

---
+++ chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_page.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './privacy_sandbox_page.html.js';
+
+export class SettingsPrivacySandboxPageElement extends PolymerElement {
+  static get is() {
+    return 'settings-privacy-sandbox-page';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-privacy-sandbox-page': SettingsPrivacySandboxPageElement;
+  }
+}
+
+customElements.define(
+    SettingsPrivacySandboxPageElement.is, SettingsPrivacySandboxPageElement);
