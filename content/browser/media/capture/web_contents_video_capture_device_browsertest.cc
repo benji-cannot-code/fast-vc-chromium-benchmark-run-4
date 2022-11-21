d9e3e98fddf525d91eb0bd3ec83d87870762403f
Jordan Bayles <jophba@chromium.org>
2022-11-21 21:08:00 +0000

    [Content Capture] Add HiDPI fieldtrial config
    This patch adds a fieldtrial config for WebContentsCaptureHiDPI for use
    as a Finch experiment.
    
    Bug: 1324757
    Change-Id: I1c14ff619be35c771fb4935b1f46789995c58be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026608
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Auto-Submit: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074255}

--- content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
+++ content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/shell/browser/shell.h"
+#include "media/base/media_switches.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_types.h"
 #include "media/base/video_util.h"
@@ -81,7 +82,10 @@ class WebContentsVideoCaptureDeviceBrowserTest
     : public ContentCaptureDeviceBrowserTestBase,
       public FrameTestUtil {
  public:
-  WebContentsVideoCaptureDeviceBrowserTest() = default;
+  WebContentsVideoCaptureDeviceBrowserTest() {
+    // TODO(https://crbug.com/1324757): tests should work with HiDPI enabled.
+    scoped_feature_list_.InitAndDisableFeature(media::kWebContentsCaptureHiDpi);
+  }
 
   WebContentsVideoCaptureDeviceBrowserTest(
       const WebContentsVideoCaptureDeviceBrowserTest&) = delete;
@@ -282,6 +286,9 @@ class WebContentsVideoCaptureDeviceBrowserTest
   }
 
   void WaitForFirstFrame() final { WaitForFrameWithColor(SK_ColorBLACK); }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Tests that the device refuses to start if the WebContents target was
