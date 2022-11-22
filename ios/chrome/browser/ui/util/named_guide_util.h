f22948832b30d9afeeffb04a4f7bd29a80752ad6
Louis Romero <lpromero@google.com>
2022-11-22 13:30:19 +0000

    Remove unused method
    Fixed: 1392654
    Change-Id: I56cbf3a8d1680779070406036a430c57be0f311e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4046162
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074599}

--- ios/chrome/browser/ui/util/named_guide_util.h
+++ ios/chrome/browser/ui/util/named_guide_util.h
@@ -13,9 +13,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Creates NamedGuides with the GuideNames in `names` and adds them to `view`.
 void AddNamedGuidesToView(NSArray<GuideName*>* names, UIView* view);
 
-// Sets the constrained views for the NamedGuides indicated by the keys of
-// `views_for_names` to their corresponding values in the `views_for_names`.
-void SetNamedGuideConstrainedViews(
-    NSDictionary<GuideName*, UIView*>* views_for_names);
-
 #endif  // IOS_CHROME_BROWSER_UI_UTIL_NAMED_GUIDE_UTIL_H_
