564d2509b2c2543c2f35f582070f91ef4a631fb7
Stepan Khapugin <stkhapugin@chromium.org>
2022-11-22 14:35:29 +0000

    [iOS] Add omnibox debugging mode toggle.
    Adds an "experimental setting" for omnibox debugging.
    
    Bug: none
    Change-Id: Ib8aedd149a367c2885d45718dc713ec83ef6f161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037273
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074622}

--- ios/chrome/browser/flags/system_flags.mm
+++ ios/chrome/browser/flags/system_flags.mm
@@ -95,6 +95,11 @@ bool IsMemoryDebuggingEnabled() {
 #endif  // BUILDFLAG(CHROMIUM_BRANDING)
 }
 
+bool IsOmniboxDebuggingEnabled() {
+  return [[NSUserDefaults standardUserDefaults]
+      boolForKey:@"EnableOmniboxDebugging"];
+}
+
 bool IsStartupCrashEnabled() {
   return [[NSUserDefaults standardUserDefaults] boolForKey:kEnableStartupCrash];
 }
