20184249c6db89ddc81eedba39def99827ce7921
Meyron Levanon <meyron@google.com>
2022-11-22 00:16:42 +0000

    Added Chronicle reporting connector 2E2 test
    Added an E2E test that verifies events are being sent to a Chronicle instance
    via the reporting connector feature
    
    b: 241008617
    Change-Id: I12c58e73e47793606681df847d7ff940c634b340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998013
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Commit-Queue: Meyron Levanon <meyron@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074359}

--- chrome/test/enterprise/e2e/connector/__init__.py
+++ chrome/test/enterprise/e2e/connector/__init__.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from .realtime_reporting_bce.realtime_reporting_bce_test import *
+from .reporting_connector_chronicle.reporting_connector_chronicle_test import *
 from .reporting_connector_crowdstrike.reporting_connector_crowdstrike_test import *
 from .reporting_connector_pubsub.reporting_connector_pubsub_test import *
 from .reporting_connector_splunk.reporting_connector_splunk_test import *
