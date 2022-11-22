e62778a8b2c6df633421e80bd1a7b7c9ad7f1046
Andrey Kosyakov <caseq@chromium.org>
2022-11-22 17:54:16 +0000

    DevTools: plumb indication that session waits for debugger to v8
    This plumbs the flag indicating that the session has been created
    in the waiting-for-debugger state down to v8, so that v8 would
    only issue `V8InspectorClient::runIfWaitingForDebugger()` once
    all session have resumed. This only matters for workers, as similar
    behavior for frames is already ensured by the navigation throttles.
    
    This requires v8 change: https://chromium-review.googlesource.com/c/v8/v8/+/3976353
    
    Bug: 1352175
    Change-Id: Iaf11c65848dd09f6d34a70a70709472010564ff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3977348
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074731}

--- content/services/auction_worklet/worklet_devtools_debug_test_util.cc
+++ content/services/auction_worklet/worklet_devtools_debug_test_util.cc
@@ -53,7 +53,8 @@ TestDevToolsAgentClient::TestDevToolsAgentClient(
                                 session_.BindNewEndpointAndPassReceiver(),
                                 io_session_.BindNewPipeAndPassReceiver(),
                                 nullptr, use_binary_protocol_,
-                                /*client_is_trusted=*/true, session_id_);
+                                /*client_is_trusted=*/true, session_id_,
+                                /*session_waits_for_debugger=*/false);
 }
 
 TestDevToolsAgentClient::~TestDevToolsAgentClient() = default;
