cd323b9370e31796a639c10e2732b03f55f300c6
Alex Moshchuk <alexmos@chromium.org>
2022-11-22 22:11:57 +0000

    Reset source SiteInstance before scheduling PDF navigations in subframes.
    This CL fixes a SiteInstance/BrowserContext lifetime issue in
    PdfNavigationThrottle::WillStartRequest(), which cancels certain
    subframe PDF navigations and schedules replacement navigations with
    slightly tweaked params via a PostTask.  The PostTask takes in
    OpenURLParams, which contains the source SiteInstance in a
    scoped_refptr.  Unfortunately, issue 1382761 shows that the
    BrowserContext can get destroyed after the task is scheduled but
    before it runs, and even though the task uses a WebContents WeakPtr to
    return early in that case, the task's OpenURLParams would only get
    destroyed and decrement the source SiteInstance's refcount at the time
    of that early return, which is already after the BrowserContext is
    destroyed.  When the (source) SiteInstance destructor runs and tries
    to use the SiteInstance's BrowserContext, things blow up.
    
    As a short-term fix, we can avoid keeping the source SiteInstance
    alive longer than its BrowserContext by not passing it through
    OpenURLParams, but rather setting it directly when the task runs.
    This is possible because in this case the source SiteInstance should
    always be the SiteInstance of the PDF extension loaded in the guest's
    main frame.
    
    Longer-term, we should find a more systematic way to fix these
    problems, for example by not exposing refcounting of SiteInstances
    outside of //content or introducing an API for scheduling navigations
    that is robust against BrowserContext destruction.  See the bug for
    more details and other ideas.
    
    Bug: 1382761
    Change-Id: I88370a1bc0700d2a49553b9d3b1ecbaf5ce62cb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043432
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074889}

--- content/public/test/mock_navigation_handle.h
+++ content/public/test/mock_navigation_handle.h
@@ -51,7 +51,7 @@ class MockNavigationHandle : public NavigationHandle {
     return starting_site_instance_;
   }
   SiteInstance* GetSourceSiteInstance() override {
-    return nullptr;  // Good enough for unit tests...
+    return source_site_instance_;
   }
   bool IsInMainFrame() const override {
     return render_frame_host_ ? !render_frame_host_->GetParent() : true;
@@ -228,6 +228,9 @@ class MockNavigationHandle : public NavigationHandle {
   void set_starting_site_instance(SiteInstance* site_instance) {
     starting_site_instance_ = site_instance;
   }
+  void set_source_site_instance(SiteInstance* site_instance) {
+    source_site_instance_ = site_instance;
+  }
   void set_page_transition(ui::PageTransition page_transition) {
     page_transition_ = page_transition;
   }
@@ -303,6 +306,7 @@ class MockNavigationHandle : public NavigationHandle {
   GURL url_;
   GURL previous_primary_main_frame_url_;
   raw_ptr<SiteInstance> starting_site_instance_ = nullptr;
+  raw_ptr<SiteInstance> source_site_instance_ = nullptr;
   raw_ptr<WebContents> web_contents_ = nullptr;
   GURL base_url_for_data_url_;
   blink::mojom::Referrer referrer_;
