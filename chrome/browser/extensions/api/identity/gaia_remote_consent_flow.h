3d6ae87059ce8d3c0e6b5528eeb032cb6de79a5a
Ryan Sultanem <rsult@google.com>
2022-11-22 16:37:55 +0000

    launchWebAuthFlow to do authentication through tab
    Created the feature flag `kAuthWithWebContentTab` to separate the new behavior.
    For the `launchWebAuthFlow` flow, `WebAuthFlow` now tries to authenticate using a web content displayed in a new tab instead of an app window.
    
    Change-Id: I46d1b6cb8e4a43eed7ea63afef56aee85cc5e0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030540
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074688}

--- chrome/browser/extensions/api/identity/gaia_remote_consent_flow.h
+++ chrome/browser/extensions/api/identity/gaia_remote_consent_flow.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/accounts_cookie_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/set_accounts_in_cookie_result.h"
+#include "content/public/browser/storage_partition.h"
 #include "google_apis/gaia/core_account_id.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_mint_token_flow.h"
@@ -89,6 +90,8 @@ class GaiaRemoteConsentFlow
   void GaiaRemoteConsentFlowFailed(Failure failure);
 
   void DetachWebAuthFlow();
+
+  content::StoragePartition* GetStoragePartition();
 
   raw_ptr<Delegate> delegate_;
   raw_ptr<Profile> profile_;
