c6e55a2522151062cb5208124942b60e91e70ec0
Duong Dac <ddac@chromium.org>
2022-11-22 19:25:09 +0000

    Add feature flag for hiding non-displayable account email
    WIP: crrev.com/c/4040250
    
    Bug: b/250624181
    Change-Id: I32d47e7833e7cb8938dbf4dede43d40f2fc5bc63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047627
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074791}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -248,6 +248,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kFoldableJankFix,
     &kGridTabSwitcherForTablets,
     &kHandleMediaIntents,
+    &kHideNonDisplayableAccountEmail,
     &kImmersiveUiMode,
     &kIncognitoReauthenticationForAndroid,
     &kIncognitoScreenshot,
@@ -776,6 +777,10 @@ BASE_FEATURE(kGridTabSwitcherForTablets,
 
 BASE_FEATURE(kHandleMediaIntents,
              "HandleMediaIntents",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kHideNonDisplayableAccountEmail,
+             "HideNonDisplayableAccountEmail",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kImmersiveUiMode,
