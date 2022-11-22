1e2f2c76b19887d4ada90b9cb74b5947cfea87e3
dpapad <dpapad@chromium.org>
2022-11-22 22:24:20 +0000

    Enable TrustedTypes checks for a few more small WebUIs.
    Specifically for:
     - chrome://reset-password
     - chrome://topics-internals
     - chrome://webuijserror
    
    Bug: 1098690
    Change-Id: Id11922d255aaee0b4aac93240319e757156582a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049561
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074902}

--- chrome/browser/ui/webui/reset_password/reset_password_ui.cc
+++ chrome/browser/ui/webui/reset_password/reset_password_ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/reset_password/reset_password.mojom.h"
+#include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "components/safe_browsing/content/browser/password_protection/password_protection_service.h"
@@ -119,7 +120,7 @@ ResetPasswordUI::ResetPasswordUI(content::WebUI* web_ui)
       password_type_(GetPasswordType(web_ui->GetWebContents())) {
   std::unique_ptr<content::WebUIDataSource> html_source(
       content::WebUIDataSource::Create(chrome::kChromeUIResetPasswordHost));
-  html_source->DisableTrustedTypesCSP();
+  webui::EnableTrustedTypesCSP(html_source.get());
   html_source->AddResourcePath("reset_password.js", IDR_RESET_PASSWORD_JS);
   html_source->AddResourcePath("reset_password.mojom-webui.js",
                                IDR_RESET_PASSWORD_MOJOM_WEBUI_JS);
