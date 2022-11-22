24c6bf0fd131b08fb55525f4b08d938c4e1076ee
Rebekah Potter <rbpotter@chromium.org>
2022-11-22 18:39:23 +0000

    Net Export: Fix undefined function call
    R=dpapad@chromium.org
    
    Fixed: 1392636
    Change-Id: Ie9c152f1c455ff1d9afffaf0fc6fbd3d07d028fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4046469
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074760}

--- components/net_log/resources/net_export.js
+++ components/net_log/resources/net_export.js
@@ -52,7 +52,7 @@ class NetExportView {
   initialize() {
     // Tell NetExportMessageHandler to notify the UI of future state changes
     // from this point on and listen for net-log-info-changed events.
-    addWebUIListener(
+    addWebUiListener(
         'net-log-info-changed', info => this.onExportNetLogInfoChanged_(info));
     chrome.send('enableNotifyUIWithState');
   }
