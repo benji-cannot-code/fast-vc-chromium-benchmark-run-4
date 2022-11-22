f3d69e239737c0d6be37a962c83ce05f647ac08e
Angus L. M. McLean IV <angusmclean@chromium.org>
2022-11-22 00:15:09 +0000

    holding space: Move generic bubble and section tests
    Moved some test on logic for showing and hiding bubbles, as well as
    for item ordering in HoldingSpaceItemViewsSection. This requires
    several methods to be added to`HoldingSpaceAshTestBase` and moving `TestHoldingSpaceItemViewsSection` and
    `TestHoldingSpaceTrayChildBubble` to separate files.
    
    Bug: 1373911
    Change-Id: I408ba01d93ff4c2cc2ff236f347bcc356d1c25dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953853
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074358}

--- ash/system/holding_space/holding_space_ash_test_base.h
+++ ash/system/holding_space/holding_space_ash_test_base.h
@@ -6,16 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_ASH_TEST_BASE_H_
 #define ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_ASH_TEST_BASE_H_
 
+#include "ash/public/cpp/holding_space/holding_space_controller.h"
 #include "ash/public/cpp/holding_space/holding_space_item.h"
 #include "ash/public/cpp/holding_space/holding_space_model.h"
 #include "ash/public/cpp/holding_space/mock_holding_space_client.h"
 #include "ash/test/ash_test_base.h"
+#include "components/account_id/account_id.h"
 
 namespace ash {
 
 // Base class for tests of holding space in ash.
 class HoldingSpaceAshTestBase : public AshTestBase {
  public:
+  static constexpr char kTestUser[] = "user@test";
+
   HoldingSpaceAshTestBase();
   HoldingSpaceAshTestBase(const HoldingSpaceAshTestBase&) = delete;
   HoldingSpaceAshTestBase& operator=(const HoldingSpaceAshTestBase&) = delete;
@@ -23,10 +27,22 @@ class HoldingSpaceAshTestBase : public AshTestBase {
 
   // Adds an item of the specified `type` backed by a file at the specified
   // `file_path` to the model for the currently active user.
-  void AddItem(HoldingSpaceItem::Type type, const base::FilePath& file_path);
+  HoldingSpaceItem* AddItem(HoldingSpaceItem::Type type,
+                            const base::FilePath& file_path);
+
+  // Adds an item of the specified `type` that is not fully initialized to
+  // the model for the currently active user.
+  HoldingSpaceItem* AddPartiallyInitializedItem(HoldingSpaceItem::Type type,
+                                                const base::FilePath& path);
 
   // Removes all items from the model for the currently active user.
   void RemoveAllItems();
+
+  // Mark the time of first add in prefs.
+  void MarkTimeOfFirstAdd(
+      AccountId account_id = AccountId::FromUserEmail(kTestUser));
+
+  HoldingSpaceModel* model() { return HoldingSpaceController::Get()->model(); }
 
  protected:
   // AshTestBase:
