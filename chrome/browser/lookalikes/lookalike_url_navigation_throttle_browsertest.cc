bcf99f98f5d8f824e4f1f5cc1133e1c5d3a19ccd
Mustafa Emre Acer <meacer@chromium.org>
2022-11-22 00:46:10 +0000

    Lookalikes: Rename a lookalike match type enum
    Bug: 1386300
    Change-Id: I79492faa875b9e205355e61c8179c7d643cbe504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043228
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074379}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
@@ -1116,7 +1116,8 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
         NavigationSuggestionEvent::kMatchSiteEngagement);
 
     ukm_urls.push_back(kNavigatedUrl);
-    CheckUkm(ukm_urls, "MatchType", LookalikeUrlMatchType::kSiteEngagement);
+    CheckUkm(ukm_urls, "MatchType",
+             LookalikeUrlMatchType::kSkeletonMatchSiteEngagement);
   }
 }
 
@@ -1219,7 +1220,7 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
       NavigationSuggestionEvent::kMatchSiteEngagement);
 
   CheckUkm({kNavigatedUrl}, "MatchType",
-           LookalikeUrlMatchType::kSiteEngagement);
+           LookalikeUrlMatchType::kSkeletonMatchSiteEngagement);
 }
 
 // Similar to Idn_SiteEngagement_Match, but tests a single domain. Also checks
@@ -1250,7 +1251,8 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
         NavigationSuggestionEvent::kMatchSiteEngagement);
 
     ukm_urls.push_back(kNavigatedUrl);
-    CheckUkm(ukm_urls, "MatchType", LookalikeUrlMatchType::kSiteEngagement);
+    CheckUkm(ukm_urls, "MatchType",
+             LookalikeUrlMatchType::kSkeletonMatchSiteEngagement);
   }
 
   // Incognito shouldn't record metrics because there are no engaged sites.
@@ -1275,7 +1277,8 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
         incognito, histograms, kNavigatedUrl, kEngagedUrl,
         NavigationSuggestionEvent::kMatchSiteEngagement);
     ukm_urls.push_back(kNavigatedUrl);
-    CheckUkm(ukm_urls, "MatchType", LookalikeUrlMatchType::kSiteEngagement);
+    CheckUkm(ukm_urls, "MatchType",
+             LookalikeUrlMatchType::kSkeletonMatchSiteEngagement);
   }
 
   // Main profile shouldn't record metrics because there are no engaged sites.
@@ -1352,7 +1355,7 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
       NavigationSuggestionEvent::kMatchSiteEngagement);
 
   CheckUkm({kNavigatedUrl}, "MatchType",
-           LookalikeUrlMatchType::kSiteEngagement);
+           LookalikeUrlMatchType::kSkeletonMatchSiteEngagement);
 }
 
 // Navigate to lookalike domains that redirect to benign domains and ensure that
