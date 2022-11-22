d46b5bebe444cdac61fb5585422b9233d074bbef
Yuki Shiino <yukishiino@chromium.org>
2022-11-22 07:32:35 +0000

    ShadowRealm: Implement ShadowRealm global object in Blink (2/N)
    As a follow-up of https://crrev.com/c/3999460, adds support of
    mojo IPC support of ShadowRealmToken.
    
    Bug: 1281880
    Change-Id: Ib73bb8d51ecf2e082884ec0f0fca0882722f96fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4036907
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074488}

--- third_party/blink/common/tokens/tokens_mojom_traits.cc
+++ third_party/blink/common/tokens/tokens_mojom_traits.cc
@@ -174,6 +174,12 @@ bool UnionTraits<
       *output = token;
       return ret;
     }
+    case DataView::Tag::kShadowRealmToken: {
+      blink::ShadowRealmToken token;
+      bool ret = input.ReadShadowRealmToken(&token);
+      *output = token;
+      return ret;
+    }
   }
   return false;
 }
