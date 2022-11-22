1b18194575ef52545d0f323c774129a9c3dcb97c
manukh <manukh@chromium.org>
2022-11-22 20:47:20 +0000

    [omnibox][post-ac] Reimplement tail suggest ellipses.
    Tail suggests are indented and display ellipses. E.g.:
    
      (input)    giberish giberish harry pot
      (swyt)     giberish giberish harry pot
      (tail)                   ... harry potter
    
    Unfortunately, the implementation of tail suggest ellipses is a bit
    awkward:
    
    - NTP realbox expects the match contents to be 'harry potter' and will
      prepend it's own '...' in a different `<span>`. This makes it easy to
      align the 'harry' in all 3 suggestions.
    - On the other hand, desktop & android expect the contents to be '...
      harry potter' displayed in a single view; they will measure the width
      of the '...' and unindent the view accordingly.
    - As a result, the match contents is set different depending on surface
      (realbox v desktop/android).
    - Consequently, the content adjustment has to be done in
      surface-speicific flows. In this case, it's done in the UI layout
      flow, which seems misplaced. In contrast, all other annotatins, like
      pedals and keywords, are done in the AC update flow, which seems much
      more appropriate.
    - Lastly, having the contents modified outside the AC update flow is
      prone to bugs (e.g. crbug.com/1383285) caused by other changes
      assuming matches are stable outside of the the AC update flow.
    
    This CL addresses these. It makes the desktop & android views follow
    realbox's example of having a separate view element for the ellipses and
    the contents. Thus, now all surfaces expect the contents to not include
    the ellipses `'...'`, making the matches consistent and allowing the
    tail suggest processing to be done in the shared AC update flow rather
    than the surface-specific view layout flow. This also allows removing
    the logic to prepend `'...'` and the NONE classification to contents.
    
    https://screenshot.googleplex.com/BzQeDWGSBtDh9FY
    
    Bug: 1383285, 1340291
    Change-Id: I11849709bb6617fe22259984f2dc3d85ef341ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048680
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074840}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -890,14 +890,6 @@ void AutocompleteController::SetMatchDestinationURL(
 #endif
 }
 
-void AutocompleteController::SetTailSuggestContentPrefixes() {
-  result_.SetTailSuggestContentPrefixes();
-}
-
-void AutocompleteController::SetTailSuggestCommonPrefixes() {
-  result_.SetTailSuggestCommonPrefixes();
-}
-
 const AutocompleteResult& AutocompleteController::result() const {
   return DebouncingEnabled() ? published_result_ : result_;
 }
@@ -988,6 +980,8 @@ void AutocompleteController::UpdateResult(
   UpdateKeywordDescriptions(&result_);
   UpdateAssociatedKeywords(&result_);
   UpdateAssistedQueryStats(&result_);
+  UpdateTailSuggestPrefix(&result_);
+
   if (search_provider_)
     search_provider_->RegisterDisplayedAnswers(result_);
 
@@ -1212,6 +1206,17 @@ void AutocompleteController::UpdateAssistedQueryStats(
     }
     match->search_terms_args->assisted_query_stats = base::StringPrintf(
         "chrome.%s.%s", selected_index.c_str(), autocompletions.c_str());
+  }
+}
+
+void AutocompleteController::UpdateTailSuggestPrefix(
+    AutocompleteResult* result) {
+  const auto common_prefix = result->GetCommonPrefix();
+  if (!common_prefix.empty()) {
+    for (auto& match : *result) {
+      if (match.type == AutocompleteMatchType::SEARCH_SUGGEST_TAIL)
+        match.tail_suggest_common_prefix = common_prefix;
+    }
   }
 }
 
