b492ebe75a65abe379a917b96fb1b6299a7ab61f
Emmanuel Arias Soto <eariassoto@google.com>
2022-11-22 10:01:35 +0000

    [CrOS Cellular] Pass DeviceState path to CreateNonShillCellularNetwork
    NetworkState::CreateNonShillCellularNetwork only requires the device
    state path. This change allows future unit tests to use this helper
    function without constructing a DeviceState.
    
    chromeos_unittests --gtest_filter=StubCellularNetworksProviderTest.*
    
    Change-Id: Ia86983601022ee7a87ae39d2b8a0935224d48440
    Bug: b:162365553
    Test: chromeos_unittests --gtest_filter=NetworkStateTest.*\
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037765
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074534}

--- chromeos/ash/components/network/network_state_unittest.cc
+++ chromeos/ash/components/network/network_state_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/task_environment.h"
 #include "base/values.h"
+#include "chromeos/ash/components/network/device_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
 #include "chromeos/ash/components/network/network_state_test_helper.h"
 #include "chromeos/ash/components/network/tether_constants.h"
@@ -469,7 +470,7 @@ TEST_F(NetworkStateTest, NonShillCellular) {
   std::unique_ptr<NetworkState> non_shill_cellular =
       NetworkState::CreateNonShillCellularNetwork(
           kTestIccid, kTestEid, kTestGuid, /*is_managed=*/false,
-          GetCellularDevice());
+          GetCellularDevice()->path());
   EXPECT_EQ(kTestIccid, non_shill_cellular->iccid());
   EXPECT_EQ(kTestEid, non_shill_cellular->eid());
   EXPECT_EQ(kTestGuid, non_shill_cellular->guid());
@@ -483,7 +484,7 @@ TEST_F(NetworkStateTest, NonShillCellular) {
 
   non_shill_cellular = NetworkState::CreateNonShillCellularNetwork(
       kTestIccid, kTestEid, kTestGuid, /*is_managed=*/true,
-      GetCellularDevice());
+      GetCellularDevice()->path());
   EXPECT_EQ(kTestIccid, non_shill_cellular->iccid());
   EXPECT_EQ(kTestEid, non_shill_cellular->eid());
   EXPECT_EQ(kTestGuid, non_shill_cellular->guid());
