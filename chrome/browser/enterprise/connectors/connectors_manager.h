968156d2ce2ee77ba633d5d07be3a248cf5439ce
Nasser Al-shawwa <alshawwa@chromium.org>
2022-11-22 14:40:37 +0000

    Remove FileSystemConnector enabled checks/settings fetch
    Bug: b/256182367
    Change-Id: I6c3285d85632beedecea8b8e0baf170545790c1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043209
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074626}

--- chrome/browser/enterprise/connectors/connectors_manager.h
+++ chrome/browser/enterprise/connectors/connectors_manager.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/enterprise/connectors/analysis/analysis_service_settings.h"
 #include "chrome/browser/enterprise/connectors/common.h"
-#include "chrome/browser/enterprise/connectors/file_system/service_settings.h"
 #include "chrome/browser/enterprise/connectors/reporting/extension_install_event_router.h"
 #include "chrome/browser/enterprise/connectors/reporting/reporting_service_settings.h"
 #include "chrome/browser/enterprise/connectors/service_provider_config.h"
@@ -35,8 +34,6 @@ class ConnectorsManager {
       std::map<AnalysisConnector, std::vector<AnalysisServiceSettings>>;
   using ReportingConnectorsSettings =
       std::map<ReportingConnector, std::vector<ReportingServiceSettings>>;
-  using FileSystemConnectorsSettings =
-      std::map<FileSystemConnector, std::vector<FileSystemServiceSettings>>;
 
   ConnectorsManager(
       std::unique_ptr<BrowserCrashEventRouter> browser_crash_event_router,
@@ -66,19 +63,9 @@ class ConnectorsManager {
       AnalysisConnector connector);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
-  // Validates which settings should be applied to a file system connector
-  // against cached policies.
-  absl::optional<FileSystemSettings> GetFileSystemGlobalSettings(
-      FileSystemConnector connector);
-  // In addition to method above; also validates specifically for an URL.
-  absl::optional<FileSystemSettings> GetFileSystemSettings(
-      const GURL& url,
-      FileSystemConnector connector);
-
   // Checks if the corresponding connector is enabled.
   bool IsConnectorEnabled(AnalysisConnector connector) const;
   bool IsConnectorEnabled(ReportingConnector connector) const;
-  bool IsConnectorEnabled(FileSystemConnector connector) const;
 
   bool DelayUntilVerdict(AnalysisConnector connector);
   absl::optional<std::u16string> GetCustomMessage(AnalysisConnector connector,
@@ -101,8 +88,6 @@ class ConnectorsManager {
       const;
   const ReportingConnectorsSettings& GetReportingConnectorsSettingsForTesting()
       const;
-  const FileSystemConnectorsSettings&
-  GetFileSystemConnectorsSettingsForTesting() const;
 
  private:
   // Validates which settings should be applied to an analysis connector event
@@ -115,7 +100,6 @@ class ConnectorsManager {
   // Read and cache the policy corresponding to |connector|.
   void CacheAnalysisConnectorPolicy(AnalysisConnector connector);
   void CacheReportingConnectorPolicy(ReportingConnector connector);
-  void CacheFileSystemConnectorPolicy(FileSystemConnector connector);
 
   // Sets up |pref_change_registrar_|. Used by the constructor and
   // SetUpForTesting.
@@ -129,12 +113,6 @@ class ConnectorsManager {
   absl::optional<ReportingSettings> GetReportingSettingsFromConnectorPolicy(
       ReportingConnector connector);
 
-  // Returns service settings (if there are multiple service providers, only the
-  // first one for now) for |connector|. Cache the policy value the first time
-  // this is called for every different connector.
-  FileSystemServiceSettings* GetFileSystemServiceSettings(
-      FileSystemConnector connector);
-
   // Cached values of available service providers. This information validates
   // the Connector policies have a valid provider.
   raw_ptr<const ServiceProviderConfig> service_provider_config_;
@@ -143,7 +121,6 @@ class ConnectorsManager {
   // used or when a policy is updated.
   AnalysisConnectorsSettings analysis_connector_settings_;
   ReportingConnectorsSettings reporting_connector_settings_;
-  FileSystemConnectorsSettings file_system_connector_settings_;
 
   // Used to track changes of connector policies and propagate them in
   // |connector_settings_|.
--- chrome/browser/enterprise/connectors/file_system/browsertest_helper.h
+++ chrome/browser/enterprise/connectors/file_system/browsertest_helper.h
@@ -186,7 +186,6 @@ class FileSystemConnectorBrowserTestBase : public InProcessBrowserTest {
 
  protected:
   void SetUpOnMainThread() override;
-  void SetUpCommandLine(base::CommandLine* command_line) override;
   void TearDownOnMainThread() override;
 
   void SetCloudFSCPolicy(const std::string& policy_value);
