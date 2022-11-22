ac3a8791c06afd90d62df63c8c91f93f8b2ec223
Emilia Paz <emiliapaz@chromium.org>
2022-11-22 02:11:04 +0000

    [Extensions] Verify extension requests permissions before grant
    Extension can be affected by runtime host permissions if:
      a. Permissions can be withheld for the extension, and
      b. Extension requests host permissions
    
    Checking if the extension has active or withheld permissions is not
    sufficient because it doesn't include optional permissions at initial
    state. A more correct way is to check what permissions (required and optional) the extension requested.
    
    Bug: 1387707
    Change-Id: Ie7bff99b7c025e73aa0b07e5ce5a7d734a7c06d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043437
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074413}

--- extensions/browser/permissions_manager.cc
+++ extensions/browser/permissions_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
@@ -405,16 +406,10 @@ bool PermissionsManager::CanAffectExtension(const Extension& extension) const {
   if (!util::CanWithholdPermissionsFromExtension(extension))
     return false;
 
-  // The extension can be affected if it currently has host permissions, or if
-  // it did and they are actively withheld.
-  return !extension.permissions_data()
-              ->active_permissions()
-              .effective_hosts()
-              .is_empty() ||
-         !extension.permissions_data()
-              ->withheld_permissions()
-              .effective_hosts()
-              .is_empty();
+  // The extension can be affected by runtime host permissions if it requests
+  // host permissions.
+  return !PermissionsParser::GetRequiredPermissions(&extension).IsEmpty() ||
+         !PermissionsParser::GetOptionalPermissions(&extension).IsEmpty();
 }
 
 bool PermissionsManager::HasGrantedHostPermission(const Extension& extension,
