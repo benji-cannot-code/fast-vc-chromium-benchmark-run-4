5741d1a162f2d55dcde242006bdfd669b54037ba
Ari Chivukula <arichiv@chromium.org>
2022-11-22 19:17:14 +0000

    [Expired Histograms] WebRTC.SystemMaxConsecutiveBytesDelayed WebRTC.SystemPercentPacketsDelayed
    This CL is part of a code health project to remove expired histogram
    logging from the codebase. This data has not been recorded since M81.
    
    Bug: 1384274
    Change-Id: I806a294c245ad64aeb149f6e0857c6f0365e69b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039445
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074785}

--- services/network/p2p/socket_tcp.cc
+++ services/network/p2p/socket_tcp.cc
@@ -254,11 +254,8 @@ bool P2PSocketTcpBase::OnPacket(base::span<const uint8_t> data) {
 }
 
 void P2PSocketTcpBase::WriteOrQueue(SendBuffer& send_buffer) {
-  IncrementTotalSentPackets();
   if (write_buffer_.buffer.get()) {
     write_queue_.push(send_buffer);
-    IncrementDelayedPackets();
-    IncrementDelayedBytes(send_buffer.buffer->size());
     return;
   }
 
@@ -313,8 +310,6 @@ bool P2PSocketTcpBase::HandleWriteResult(int result) {
     } else {
       write_buffer_ = write_queue_.front();
       write_queue_.pop();
-      // Update how many bytes are still waiting to be sent.
-      DecrementDelayedBytes(write_buffer_.buffer->size());
     }
   }
   return true;
