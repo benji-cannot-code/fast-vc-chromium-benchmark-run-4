bdbfaa54d97a3bb3ddc9f29f0eed6ec729fbb71b
Moe Ahmadi <mahmadi@chromium.org>
2022-11-22 20:31:07 +0000

    Enables launch features/params for local history revamp on Mobile.
    Finch configs can be found in:
    LocalHistorySuggestRevamp_Android.gcl
    LocalHistorySuggestRevamp_iOS.gcl
    
    Bug: 1119654
    Change-Id: If4f562e1ae554a0264a1577d1767599bc1c9f66d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4045481
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074833}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -778,11 +778,7 @@ bool IsZeroSuggestPrefetchingEnabledInContext(
 const base::FeatureParam<int> kOmniboxLocalZeroSuggestAgeThresholdParam(
     &omnibox::kOmniboxLocalZeroSuggestAgeThreshold,
     "OmniboxLocalZeroSuggestAgeThreshold",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-    60);
-#else
     90);
-#endif
 
 base::Time GetLocalHistoryZeroSuggestAgeThreshold() {
   return (base::Time::Now() -
@@ -796,11 +792,7 @@ const base::FeatureParam<bool> kZeroSuggestIgnoreDuplicateVisits(
 const base::FeatureParam<bool> kPrefixSuggestIgnoreDuplicateVisits(
     &omnibox::kLocalHistorySuggestRevamp,
     "PrefixSuggestIgnoreDuplicateVisits",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-    false);
-#else
     true);
-#endif
 
 // Short bookmarks.
 
