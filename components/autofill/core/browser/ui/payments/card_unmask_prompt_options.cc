a3c89a257063331c0a3d8ebaf55869b0c8e5f099
Alexander Tekle <alexandertekle@google.com>
2022-11-22 17:16:30 +0000

    Cleanup member variables in Card Unmask Prompt Controller
    Store the card_unmask_prompt_options directly and reference it in the Card Unmask Prompt Controller.
    
    Change-Id: I23167eb8b47eec5e5886674970ed4b17fe9dfd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043445
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074712}

--- components/autofill/core/browser/ui/payments/card_unmask_prompt_options.cc
+++ components/autofill/core/browser/ui/payments/card_unmask_prompt_options.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+CardUnmaskPromptOptions::CardUnmaskPromptOptions() = default;
+
 CardUnmaskPromptOptions::CardUnmaskPromptOptions(
     const absl::optional<CardUnmaskChallengeOption>& challenge_option,
     AutofillClient::UnmaskCardReason reason)
