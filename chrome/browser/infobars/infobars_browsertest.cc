8c1543297c238a42fa6eb756b29254248d7485ac
Zain Afzal <zafzal@google.com>
2022-11-22 23:32:48 +0000

    Revert "Disable Keystone Updater infobar on macOS conditionally on GN flag"
    This reverts commit a3541c231eaf539d7ccb6b65b47075d79a6e660c.
    
    Reason for revert: Suspected culprit for  InvokeUi_keystone_promotion mac failures (https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/13207/overview)
    
    Original change's description:
    > Disable Keystone Updater infobar on macOS conditionally on GN flag
    >
    > Make the keystone updater infobar respect the value of the
    > "enable_updater" flag.
    >
    > Design document: https://goo.gle/chrome-for-testing#bookmark=id.txnno6i8zvhj
    >
    > R=​waffles@chromium.org
    >
    > Bug: 1336611
    > Change-Id: I5405db47ccdf88bfc96cddb692dc3569523ed3ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026292
    > Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Mathias Bynens <mathias@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1074786}
    
    Bug: 1336611
    Change-Id: I598cf300625b71c4ab813d6cacd3c18e81479e7a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049261
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Zain Afzal <zafzal@google.com>
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074938}

--- chrome/browser/infobars/infobars_browsertest.cc
+++ chrome/browser/infobars/infobars_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/buildflags.h"
 #include "chrome/browser/devtools/devtools_infobar_delegate.h"
 #include "chrome/browser/extensions/api/debugger/extension_dev_tools_infobar_delegate.h"
 #include "chrome/browser/extensions/api/messaging/incognito_connectability_infobar_delegate.h"
@@ -67,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/default_browser_infobar_delegate.h"
 #endif
 
-#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+#if BUILDFLAG(IS_MAC)
 #include "chrome/browser/ui/cocoa/keystone_infobar_delegate.h"
 #endif
 
@@ -281,7 +280,7 @@ void InfoBarUiTest::ShowUi(const std::string& name) {
       break;
 
     case IBD::KEYSTONE_PROMOTION_INFOBAR_DELEGATE_MAC:
-#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+#if BUILDFLAG(IS_MAC)
       KeystonePromotionInfoBarDelegate::Create(GetWebContents());
 #else
       ADD_FAILURE() << "This infobar is not supported on this OS.";
--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -74,10 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "base/mac/mac_util.h"
-#if BUILDFLAG(ENABLE_UPDATER)
 #include "chrome/browser/ui/cocoa/keystone_infobar_delegate.h"
 #endif
-#endif  // BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "chrome/browser/win/conflicts/incompatible_applications_updater.h"
@@ -207,7 +205,7 @@ void StartupBrowserCreatorImpl::Launch(
 
   web_app::MaybeInstallAppFromCommandLine(*command_line_, *profile);
 
-#if BUILDFLAG(IS_MAC) && BUILDFLAG(ENABLE_UPDATER)
+#if BUILDFLAG(IS_MAC)
   if (process_startup == chrome::startup::IsProcessStartup::kYes) {
     // Check whether the auto-update system needs to be promoted from user
     // to system.
