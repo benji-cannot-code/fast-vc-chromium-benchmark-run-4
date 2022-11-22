d192f50ebd40a3e9688358b6426f82c54f2932dd
Simon Zünd <szuend@chromium.org>
2022-11-22 09:07:26 +0000

    [devtools] Prohibit cookies when requesting DevTools remote resources
    This CL disables sending cookies when we make requests to the
    DevTools GAE instance when loading from devtools://devtools/remote or
    devtools://devtools/custom.
    
    The GAE app doesn't set or use any cookies and AFAICT the same goes
    for the DevTools renderer.
    
    R=caseq@chromium.org, dsv@chromium.org
    
    Bug: b:256900141
    Change-Id: I44a37496197b2f6bc2fd534eb7b87a5d4af414e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040247
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074511}

--- chrome/browser/ui/webui/devtools_ui_data_source.cc
+++ chrome/browser/ui/webui/devtools_ui_data_source.cc
@@ -252,8 +252,7 @@ void DevToolsDataSource::StartRemoteDataRequest(
           destination: GOOGLE_OWNED_SERVICE
         }
         policy {
-          cookies_allowed: YES
-          cookies_store: "user"
+          cookies_allowed: NO
           setting: "This feature cannot be disabled by settings."
           chrome_policy {
             DeveloperToolsAvailability {
@@ -292,8 +291,7 @@ void DevToolsDataSource::StartCustomDataRequest(
           destination: WEBSITE
         }
         policy {
-          cookies_allowed: YES
-          cookies_store: "user"
+          cookies_allowed: NO
           setting: "This feature cannot be disabled by settings."
           chrome_policy {
             DeveloperToolsAvailability {
@@ -315,6 +313,7 @@ void DevToolsDataSource::StartNetworkRequest(
   auto request = std::make_unique<network::ResourceRequest>();
   request->url = url;
   request->load_flags = load_flags;
+  request->credentials_mode = network::mojom::CredentialsMode::kOmit;
 
   auto request_iter = pending_requests_.emplace(pending_requests_.begin());
   request_iter->callback = std::move(callback);
