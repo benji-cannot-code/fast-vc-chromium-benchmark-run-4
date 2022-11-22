394d5ea3ae7bdf1c8471a7de8ca60c4835822257
Ahmed Mehfooz <amehfooz@chromium.org>
2022-11-22 22:41:11 +0000

    Fix Pixel test flakiness
    Get a screenshot when the notification is displayed in
    the message center instead of a popup. Popup screenshots are
    flaky because of legacy animations.
    
    Fixed: b:260111440
    Change-Id: I5afb9a7f5237d468cb1d6335d9e17aef27f5b59b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049720
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074910}

--- ash/system/notification_center/notification_center_test_api.h
+++ ash/system/notification_center/notification_center_test_api.h
@@ -25,6 +25,7 @@ class Widget;
 namespace ash {
 
 class NotificationCenterBubble;
+class NotificationListView;
 class NotificationCenterTray;
 
 // Utility class to facilitate easier testing of the notification center.
@@ -65,6 +66,10 @@ class NotificationCenterTestApi {
   // otherwise.
   bool IsTrayShown();
 
+  // Returns the notification view associated with the provided notification id.
+  // Should be only used when the notifications bubble is open.
+  views::View* GetNotificationViewForId(const std::string& id);
+
   // Returns the popup view associated with the provided notification id,
   // nullptr otherwise.
   views::View* GetPopupViewForId(const std::string& id);
@@ -89,6 +94,8 @@ class NotificationCenterTestApi {
 
  private:
   std::string GenerateNotificationId();
+
+  NotificationListView* GetNotificationListView();
 
   std::unique_ptr<message_center::Notification> CreateNotification(
       const std::string& id,
