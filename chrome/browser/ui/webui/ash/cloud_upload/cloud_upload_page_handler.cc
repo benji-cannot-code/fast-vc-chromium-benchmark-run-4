df72d5626bb846dd7aa2da7944b16761540ad543
Peter Marshall <petermarshall@chromium.org>
2022-11-22 06:33:16 +0000

    [Office upload] Set setup complete on last page of office setup
    Once the user has reached the last page of the setup for office, we
    can go ahead and mark setup complete so if they bail out at this
    point, they don't have to do it again.
    
    Bug: b:260030850
    Change-Id: Ie1c414a8e9cb8f8bff829708e3470c038d6548aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044470
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074471}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_page_handler.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/ash/file_manager/file_tasks.h"
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload.mojom.h"
 #include "chrome/browser/web_applications/commands/install_from_info_command.h"
 #include "chrome/browser/web_applications/web_app_command_manager.h"
@@ -58,6 +59,17 @@ void CloudUploadPageHandler::RespondAndClose(mojom::UserAction action) {
   if (callback_) {
     std::move(callback_).Run(action);
   }
+}
+
+void CloudUploadPageHandler::SetOfficeAsDefaultHandler() {
+  using file_manager::file_tasks::kActionIdOpenInOffice;
+
+  file_manager::file_tasks::SetWordFileHandler(profile_, kActionIdOpenInOffice);
+  file_manager::file_tasks::SetExcelFileHandler(profile_,
+                                                kActionIdOpenInOffice);
+  file_manager::file_tasks::SetPowerPointFileHandler(profile_,
+                                                     kActionIdOpenInOffice);
+  file_manager::file_tasks::SetOfficeSetupComplete(profile_);
 }
 
 }  // namespace ash::cloud_upload
