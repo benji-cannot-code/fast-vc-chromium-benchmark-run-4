ae01240701ab2cef5f447ab1255ea2d972813e58
Yuchen Liu <yucliu@chromium.org>
2022-11-22 21:48:20 +0000

    Callback to notify the surface will be destroyed soon
    Add a callback to WebContents to let the host Activity to send a
    signal before surface destroy.
    
    The secure surface used by MediaCodec may be destroyed before
    SurfaceHolder.Callback2.surfaceDestroyed return. This will fail
    MediaCodec decoding.
    
    We observed this failure in cast for atv.
    
    We can avoid such failures if the activity let MediaCodec know the
    surface will be destroyed in Activity.onUserLeaveHint.
    
    Bug: internal b/242706143
    Test: Background secure playback, no MediaCodec failure observed.
    Change-Id: Ie16bac67090c3bef03ee5c1e51451e5b6c90deae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4011099
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074870}

--- content/public/test/android/javatests/src/org/chromium/content_public/browser/test/mock/MockWebContents.java
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/mock/MockWebContents.java
@@ -320,4 +320,7 @@ public class MockWebContents implements WebContents {
 
     @Override
     public void notifyBrowserControlsHeightChanged() {}
+
+    @Override
+    public void tearDownDialogOverlays() {}
 }
