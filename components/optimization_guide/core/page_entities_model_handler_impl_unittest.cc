075f0d1245199575fed83ed2a920d1137094b04f
Robert Ogden <robertogden@chromium.org>
2022-11-22 23:08:15 +0000

    Rename PCAService Model Executors to Handlers
    These classes live on the UI thread, so they should have been named
    Handler to start with.
    
    Also renames the UMA to keep everything tidy.
    
    Bug: b/259406572
    Change-Id: Ice96b69aeebc8b7bae4ffd0ef44e9c2782506af8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032026
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074921}

--- components/optimization_guide/core/page_entities_model_handler_impl_unittest.cc
+++ components/optimization_guide/core/page_entities_model_handler_impl_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/optimization_guide/core/page_entities_model_executor_impl.h"
+#include "components/optimization_guide/core/page_entities_model_handler_impl.h"
 
 #include "base/observer_list.h"
 #include "base/path_service.h"
@@ -63,24 +63,24 @@ class ModelObserverTracker : public TestOptimizationGuideModelProvider {
   base::ObserverList<OptimizationTargetModelObserver> registered_observers_;
 };
 
-class PageEntitiesModelExecutorImplTest : public testing::Test {
+class PageEntitiesModelHandlerImplTest : public testing::Test {
  public:
-  PageEntitiesModelExecutorImplTest() {
-    PageEntitiesModelExecutorConfig config;
+  PageEntitiesModelHandlerImplTest() {
+    PageEntitiesModelHandlerConfig config;
     // The false variation is tested in a src-internal test.
     config.should_provide_filter_path = true;
-    SetPageEntitiesModelExecutorConfigForTesting(config);
+    SetPageEntitiesModelHandlerConfigForTesting(config);
   }
 
   void SetUp() override {
     model_observer_tracker_ = std::make_unique<ModelObserverTracker>();
 
-    model_executor_ = std::make_unique<PageEntitiesModelExecutorImpl>(
+    model_executor_ = std::make_unique<PageEntitiesModelHandlerImpl>(
         model_observer_tracker_.get(),
         base::ThreadPool::CreateSequencedTaskRunner(
             {base::MayBlock(), base::TaskPriority::BEST_EFFORT}));
 
-    // Wait for PageEntitiesModelExecutor to set everything up.
+    // Wait for PageEntitiesModelHandler to set everything up.
     task_environment_.RunUntilIdle();
   }
 
@@ -88,7 +88,7 @@ class PageEntitiesModelExecutorImplTest : public testing::Test {
     model_executor_.reset();
     model_observer_tracker_.reset();
 
-    // Wait for PageEntitiesModelExecutor to clean everything up.
+    // Wait for PageEntitiesModelHandler to clean everything up.
     task_environment_.RunUntilIdle();
   }
 
@@ -133,7 +133,7 @@ class PageEntitiesModelExecutorImplTest : public testing::Test {
     return entity_metadata;
   }
 
-  PageEntitiesModelExecutor* model_executor() { return model_executor_.get(); }
+  PageEntitiesModelHandler* model_executor() { return model_executor_.get(); }
 
   ModelObserverTracker* model_observer_tracker() const {
     return model_observer_tracker_.get();
@@ -156,10 +156,10 @@ class PageEntitiesModelExecutorImplTest : public testing::Test {
  private:
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<ModelObserverTracker> model_observer_tracker_;
-  std::unique_ptr<PageEntitiesModelExecutorImpl> model_executor_;
+  std::unique_ptr<PageEntitiesModelHandlerImpl> model_executor_;
 };
 
-TEST_F(PageEntitiesModelExecutorImplTest, CreateNoMetadata) {
+TEST_F(PageEntitiesModelHandlerImplTest, CreateNoMetadata) {
   base::HistogramTester histogram_tester;
 
   std::unique_ptr<ModelInfo> model_info = TestModelInfoBuilder().Build();
@@ -171,14 +171,14 @@ TEST_F(PageEntitiesModelExecutorImplTest, CreateNoMetadata) {
   EXPECT_EQ(ExecuteModel("Taylor Swift singer"), absl::nullopt);
 
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreatedSuccessfully", false,
+      "OptimizationGuide.PageEntitiesModelHandler.CreatedSuccessfully", false,
       1);
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreationStatus",
+      "OptimizationGuide.PageEntitiesModelHandler.CreationStatus",
       EntityAnnotatorCreationStatus::kMissingModelMetadata, 1);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, CreateMetadataWrongType) {
+TEST_F(PageEntitiesModelHandlerImplTest, CreateMetadataWrongType) {
   base::HistogramTester histogram_tester;
 
   proto::Any any;
@@ -200,14 +200,14 @@ TEST_F(PageEntitiesModelExecutorImplTest, CreateMetadataWrongType) {
   EXPECT_EQ(ExecuteModel("Taylor Swift singer"), absl::nullopt);
 
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreatedSuccessfully", false,
+      "OptimizationGuide.PageEntitiesModelHandler.CreatedSuccessfully", false,
       1);
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreationStatus",
+      "OptimizationGuide.PageEntitiesModelHandler.CreationStatus",
       EntityAnnotatorCreationStatus::kMissingEntitiesModelMetadata, 1);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, CreateNoSlices) {
+TEST_F(PageEntitiesModelHandlerImplTest, CreateNoSlices) {
   base::HistogramTester histogram_tester;
 
   proto::Any any;
@@ -229,16 +229,16 @@ TEST_F(PageEntitiesModelExecutorImplTest, CreateNoSlices) {
   EXPECT_EQ(ExecuteModel("Taylor Swift singer"), absl::nullopt);
 
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreatedSuccessfully", false,
+      "OptimizationGuide.PageEntitiesModelHandler.CreatedSuccessfully", false,
       1);
   histogram_tester.ExpectUniqueSample(
-      "OptimizationGuide.PageEntitiesModelExecutor.CreationStatus",
+      "OptimizationGuide.PageEntitiesModelHandler.CreationStatus",
       EntityAnnotatorCreationStatus::
           kMissingEntitiesModelMetadataSliceSpecification,
       1);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, ModelInfoUpdated) {
+TEST_F(PageEntitiesModelHandlerImplTest, ModelInfoUpdated) {
   bool callback_run = false;
   model_executor()->AddOnModelUpdatedCallback(
       base::BindOnce([](bool* flag) { *flag = true; }, &callback_run));
@@ -267,7 +267,7 @@ TEST_F(PageEntitiesModelExecutorImplTest, ModelInfoUpdated) {
   EXPECT_TRUE(immediate_callback_run);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, CreateMissingFiles) {
+TEST_F(PageEntitiesModelHandlerImplTest, CreateMissingFiles) {
   proto::Any any;
   proto::PageEntitiesModelMetadata metadata;
   metadata.add_slice("global");
@@ -320,24 +320,24 @@ TEST_F(PageEntitiesModelExecutorImplTest, CreateMissingFiles) {
     EXPECT_EQ(ExecuteModel("Taylor Swift singer"), absl::nullopt);
 
     histogram_tester.ExpectUniqueSample(
-        "OptimizationGuide.PageEntitiesModelExecutor.CreatedSuccessfully",
-        false, 1);
+        "OptimizationGuide.PageEntitiesModelHandler.CreatedSuccessfully", false,
+        1);
 
     histogram_tester.ExpectUniqueSample(
-        "OptimizationGuide.PageEntitiesModelExecutor.CreationStatus",
+        "OptimizationGuide.PageEntitiesModelHandler.CreationStatus",
         missing_file_and_status.second, 1);
   }
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, GetMetadataForEntityIdNoModel) {
+TEST_F(PageEntitiesModelHandlerImplTest, GetMetadataForEntityIdNoModel) {
   EXPECT_EQ(GetMetadataForEntityId("/m/0dl567"), absl::nullopt);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest, ExecuteModelNoModel) {
+TEST_F(PageEntitiesModelHandlerImplTest, ExecuteModelNoModel) {
   EXPECT_EQ(ExecuteModel("Taylor Swift singer"), absl::nullopt);
 }
 
-TEST_F(PageEntitiesModelExecutorImplTest,
+TEST_F(PageEntitiesModelHandlerImplTest,
        SetsUpModelCorrectlyBasedOnFeatureParams) {
   absl::optional<proto::Any> registered_model_metadata;
   EXPECT_TRUE(model_observer_tracker()->DidRegisterForTarget(
--- components/optimization_guide/core/page_topics_model_handler.cc
+++ components/optimization_guide/core/page_topics_model_handler.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/optimization_guide/core/page_topics_model_executor.h"
+#include "components/optimization_guide/core/page_topics_model_handler.h"
 
 #include <ctype.h>
 
@@ -140,7 +140,7 @@ int MeaninglessPrefixLength(const std::string& host) {
 
 }  // namespace
 
-PageTopicsModelExecutor::PageTopicsModelExecutor(
+PageTopicsModelHandler::PageTopicsModelHandler(
     OptimizationGuideModelProvider* model_provider,
     scoped_refptr<base::SequencedTaskRunner> background_task_runner,
     const absl::optional<proto::Any>& model_metadata)
@@ -151,9 +151,9 @@ PageTopicsModelExecutor::PageTopicsModelExecutor(
       background_task_runner_(background_task_runner) {
   SetShouldUnloadModelOnComplete(false);
 }
-PageTopicsModelExecutor::~PageTopicsModelExecutor() = default;
+PageTopicsModelHandler::~PageTopicsModelHandler() = default;
 
-void PageTopicsModelExecutor::ExecuteJob(
+void PageTopicsModelHandler::ExecuteJob(
     base::OnceClosure on_job_complete_callback,
     std::unique_ptr<PageContentAnnotationJob> job) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -164,7 +164,7 @@ void PageTopicsModelExecutor::ExecuteJob(
     background_task_runner_->PostTaskAndReplyWithResult(
         FROM_HERE,
         base::BindOnce(&LoadOverrideListFromFile, *override_list_file_path_),
-        base::BindOnce(&PageTopicsModelExecutor::OnOverrideListLoadAttemptDone,
+        base::BindOnce(&PageTopicsModelHandler::OnOverrideListLoadAttemptDone,
                        weak_ptr_factory_.GetWeakPtr(),
                        std::move(on_job_complete_callback), std::move(job)));
     return;
@@ -174,7 +174,7 @@ void PageTopicsModelExecutor::ExecuteJob(
       std::move(on_job_complete_callback), std::move(job));
 }
 
-std::string PageTopicsModelExecutor::PreprocessHost(
+std::string PageTopicsModelHandler::PreprocessHost(
     const std::string& host) const {
   std::string output = base::ToLowerASCII(host);
 
@@ -200,7 +200,7 @@ std::string PageTopicsModelExecutor::PreprocessHost(
   return output;
 }
 
-void PageTopicsModelExecutor::ExecuteOnSingleInput(
+void PageTopicsModelHandler::ExecuteOnSingleInput(
     AnnotationType annotation_type,
     const std::string& raw_input,
     base::OnceCallback<void(const BatchAnnotationResult&)> callback) {
@@ -228,14 +228,14 @@ void PageTopicsModelExecutor::ExecuteOnSingleInput(
   }
 
   ExecuteModelWithInput(
-      base::BindOnce(&PageTopicsModelExecutor::
-                         PostprocessCategoriesToBatchAnnotationResult,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback),
-                     annotation_type, raw_input),
+      base::BindOnce(
+          &PageTopicsModelHandler::PostprocessCategoriesToBatchAnnotationResult,
+          weak_ptr_factory_.GetWeakPtr(), std::move(callback), annotation_type,
+          raw_input),
       processed_input);
 }
 
-void PageTopicsModelExecutor::OnOverrideListLoadAttemptDone(
+void PageTopicsModelHandler::OnOverrideListLoadAttemptDone(
     base::OnceClosure on_job_complete_callback,
     std::unique_ptr<PageContentAnnotationJob> job,
     absl::optional<
@@ -254,7 +254,7 @@ void PageTopicsModelExecutor::OnOverrideListLoadAttemptDone(
       std::move(on_job_complete_callback), std::move(job));
 }
 
-void PageTopicsModelExecutor::PostprocessCategoriesToBatchAnnotationResult(
+void PageTopicsModelHandler::PostprocessCategoriesToBatchAnnotationResult(
     base::OnceCallback<void(const BatchAnnotationResult&)> callback,
     AnnotationType annotation_type,
     const std::string& raw_input,
@@ -271,7 +271,7 @@ void PageTopicsModelExecutor::PostprocessCategoriesToBatchAnnotationResult(
 }
 
 absl::optional<std::vector<WeightedIdentifier>>
-PageTopicsModelExecutor::ExtractCategoriesFromModelOutput(
+PageTopicsModelHandler::ExtractCategoriesFromModelOutput(
     const std::vector<tflite::task::core::Category>& model_output) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
@@ -395,14 +395,14 @@ PageTopicsModelExecutor::ExtractCategoriesFromModelOutput(
   return final_categories;
 }
 
-void PageTopicsModelExecutor::UnloadModel() {
+void PageTopicsModelHandler::UnloadModel() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   BertModelHandler::UnloadModel();
   override_list_ = absl::nullopt;
 }
 
-void PageTopicsModelExecutor::OnModelUpdated(
+void PageTopicsModelHandler::OnModelUpdated(
     proto::OptimizationTarget optimization_target,
     const ModelInfo& model_info) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
