179ca16988f17e3482a7be15334f8a5f3d8f25c8
David Jean <djean@chromium.org>
2022-11-22 15:30:15 +0000

    [ios] Simplify annotation tab helper
    base::ScopedBlockingCall has been moved to provider.
    
    Bug: 1350974
    Change-Id: I5d91b0e9a47a0bc5f96931a97f7b25ec0c3ba3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4041227
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074648}

--- ios/chrome/browser/web/annotations/annotations_tab_helper.mm
+++ ios/chrome/browser/web/annotations/annotations_tab_helper.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
 #import "base/task/thread_pool.h"
-#import "base/threading/scoped_blocking_call.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/text_selection/text_classifier_model_service.h"
 #import "ios/chrome/browser/text_selection/text_classifier_model_service_factory.h"
@@ -33,25 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static NSString* kDecorationDate = @"DATE";
 static NSString* kDecorationAddress = @"ADDRESS";
 static NSString* kDecorationPhoneNumber = @"PHONE_NUMBER";
-
-namespace {
-
-// Applies text classifier to extract intents in the given text. Returns
-// a `base::Value::List` of annotations (see i/w/a/annotations_utils.h).
-// This runs in the thread pool.
-// TODO(crbug.com/1350974): move scope block to provider, and remove
-// ApplyDataExtractor.
-absl::optional<base::Value> ApplyDataExtractor(
-    const std::string& text,
-    NSTextCheckingType handled_types,
-    const base::FilePath& model_path) {
-  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
-                                                base::BlockingType::WILL_BLOCK);
-  return ios::provider::ExtractDataElementsFromText(text, handled_types,
-                                                    model_path);
-}
-
-}  //  namespace
 
 AnnotationsTabHelper::AnnotationsTabHelper(web::WebState* web_state)
     : web_state_(web_state) {
@@ -99,7 +79,7 @@ void AnnotationsTabHelper::OnTextExtracted(web::WebState* web_state,
       FROM_HERE,
       {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
-      base::BindOnce(&ApplyDataExtractor, text,
+      base::BindOnce(&ios::provider::ExtractDataElementsFromText, text,
                      ios::provider::GetHandledIntentTypes(web_state),
                      std::move(model_path)),
       base::BindOnce(&AnnotationsTabHelper::ApplyDeferredProcessing,
