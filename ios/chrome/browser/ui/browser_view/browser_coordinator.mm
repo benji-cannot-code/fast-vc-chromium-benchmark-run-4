237e3d571a6a344cc9eaaa4bb5d7f1463d66c2aa
Mark Cogan <marq@google.com>
2022-11-22 09:10:05 +0000

    [iOS] fix ordering of StoreKit tab helper init.
    The current ordering of initialization in BrowserCoordinator sets
    TabHelper delegates before coordinators are created. This means that
    for tabs that are present during a cold start, there will be no StoreKit
    or PassKit coordinators when the tab helpers for those features have
    delegates attached.
    
    This CL is a stopgap fix, which adds a second round of webstate delegate
    attachment after coordinators are started, along with DCHECKs to catch
    assigning nil delegates to these tab helpers.
    
    The intent is that this CL can be cherrypicked into M109. A more complete
    cleanup plan is discussed in the TODO bug (crbug.com/1392109) referenced
    in the CL.
    
    Bug: 1380980
    Change-Id: Ic460770d2718dc15a58aaa4ef422160527abb0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043122
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074513}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -445,6 +445,8 @@ enum class ToolbarKind {
   [self startMediators];
   [self installDelegatesForAllWebStates];
   [self startChildCoordinators];
+  // TODO(crbug.com/1392109) remove this special case.
+  [self installPostCoordinatorDelegatesForAllWebStates];
   // Browser delegates can have dependencies on coordinators.
   [self installDelegatesForBrowser];
   [self installDelegatesForBrowserState];
@@ -1783,6 +1785,14 @@ enum class ToolbarKind {
               atIndex:(int)index
            activating:(BOOL)activating {
   [self installDelegatesForWebState:webState];
+  // TODO(crbug.com/1392109): remove these special cases.
+  DCHECK(self.passKitCoordinator);
+  PassKitTabHelper::FromWebState(webState)->SetDelegate(
+      self.passKitCoordinator);
+
+  DCHECK(self.storeKitCoordinator);
+  StoreKitTabHelper::FromWebState(webState)->SetLauncher(
+      self.storeKitCoordinator);
 }
 
 - (void)webStateList:(WebStateList*)webStateList
@@ -1842,6 +1852,24 @@ enum class ToolbarKind {
   for (int i = 0; i < self.browser->GetWebStateList()->count(); i++) {
     web::WebState* webState = self.browser->GetWebStateList()->GetWebStateAt(i);
     [self installDelegatesForWebState:webState];
+  }
+}
+// Temporary fix for crbug.com/1380980. Webstate delegates which depend on
+// coordinators are set up here.
+// TODO(crbug.com/1392109) Remove this workaround and stop having coordinators
+// which are delegates of webstates that start themselves.
+- (void)installPostCoordinatorDelegatesForAllWebStates {
+  for (int i = 0; i < self.browser->GetWebStateList()->count(); i++) {
+    web::WebState* webState = self.browser->GetWebStateList()->GetWebStateAt(i);
+    // Add delegates for webstates where those delegates are other coorindators.
+    // (Please don't add further code here).
+    DCHECK(self.passKitCoordinator);
+    PassKitTabHelper::FromWebState(webState)->SetDelegate(
+        self.passKitCoordinator);
+
+    DCHECK(self.storeKitCoordinator);
+    StoreKitTabHelper::FromWebState(webState)->SetLauncher(
+        self.storeKitCoordinator);
   }
 }
 
@@ -1943,19 +1971,11 @@ enum class ToolbarKind {
         self.viewController);
   }
 
-  PassKitTabHelper::FromWebState(webState)->SetDelegate(
-      self.passKitCoordinator);
-
   if (PrintTabHelper::FromWebState(webState)) {
     PrintTabHelper::FromWebState(webState)->set_printer(self.printController);
   }
 
   RepostFormTabHelper::FromWebState(webState)->SetDelegate(self);
-
-  if (StoreKitTabHelper::FromWebState(webState)) {
-    StoreKitTabHelper::FromWebState(webState)->SetLauncher(
-        self.storeKitCoordinator);
-  }
 
   FollowTabHelper* followTabHelper = FollowTabHelper::FromWebState(webState);
   if (followTabHelper) {
