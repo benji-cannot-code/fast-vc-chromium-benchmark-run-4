95ee1363279995b107ff47218b9ad7e5bdb63a38
Saifuddin Hitawala <hitawala@chromium.org>
2022-11-23 23:07:24 +0000

    Rename viz::ResourceFormatToClosestSkColorType(SIFormat)
    Rename viz::ResourceFormatToClosestSkColorType that takes in a
    SharedImageFormat param to viz::ToClosestSkColorType
    
    Bug: 1366481, 1378707
    Change-Id: Iace2bab875e9e4fbcb3be0682460d927efa85ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039988
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075370}

--- components/viz/common/resources/resource_format_utils_unittest.cc
+++ components/viz/common/resources/resource_format_utils_unittest.cc
@@ -20,8 +20,7 @@ class ResourceFormatUtilTest : public testing::Test {
     for (int plane_index = 0; plane_index < format.NumberOfPlanes();
          plane_index++) {
       EXPECT_EQ(expected_types[plane_index],
-                ResourceFormatToClosestSkColorType(gpu_compositing, format,
-                                                   plane_index));
+                ToClosestSkColorType(gpu_compositing, format, plane_index));
     }
   }
 };
