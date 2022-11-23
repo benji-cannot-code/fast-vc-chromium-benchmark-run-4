672971ad7c4c4caaae502c96f0f77856509f9f31
Jongmok Kim <jongmok.kim@navercorp.com>
2022-11-23 01:11:23 +0000

    Clean up arg that is not needed in AddTab
    Bug: N/A
    Change-Id: Ic3aaca16a2abdf86abcd250bd83df6cfae576f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032895
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: jongmok kim <johny.kimc@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1074989}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -219,7 +219,7 @@ void BrowserTabStripController::InitFromModel(TabStrip* tabstrip) {
   // Walk the model, calling our insertion observer method for each item within
   // it.
   for (int i = 0; i < model_->count(); ++i)
-    AddTab(model_->GetWebContentsAt(i), i, model_->active_index() == i);
+    AddTab(model_->GetWebContentsAt(i), i);
 }
 
 bool BrowserTabStripController::IsCommandEnabledForTab(
@@ -612,8 +612,7 @@ void BrowserTabStripController::OnTabStripModelChanged(
     case TabStripModelChange::kInserted: {
       for (const auto& contents : change.GetInsert()->contents) {
         DCHECK(model_->ContainsIndex(contents.index));
-        AddTab(contents.contents, contents.index,
-               selection.new_contents == contents.contents);
+        AddTab(contents.contents, contents.index);
       }
       break;
     }
@@ -779,9 +778,7 @@ void BrowserTabStripController::SetTabDataAt(content::WebContents* web_contents,
                         TabRendererData::FromTabInModel(model_, model_index));
 }
 
-void BrowserTabStripController::AddTab(WebContents* contents,
-                                       int index,
-                                       bool is_active) {
+void BrowserTabStripController::AddTab(WebContents* contents, int index) {
   // Cancel any pending tab transition.
   hover_tab_selector_.CancelTabTransition();
 
