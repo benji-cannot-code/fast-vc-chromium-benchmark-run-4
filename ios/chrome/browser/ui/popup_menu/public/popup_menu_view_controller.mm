2d6fd2f8c8c9a43b012c800542aa87ed54be2989
Louis Romero <lpromero@google.com>
2022-11-23 14:34:18 +0000

    Support ESC in popup menus
    This CL adds support for the ESC key shortcut to dismiss popup menus.
    
    Fixed: 1392499
    Change-Id: I1c24e5d63667fb93e63214deaab7489d4986d2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043182
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075141}

--- ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.h"
 
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
 #import "ios/chrome/browser/ui/image_util/image_util.h"
+#import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_ui_constants.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/util/accessibility_close_menu_button.h"
@@ -52,12 +55,36 @@ const CGFloat kImageMargin = 196;
   return self;
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  // Let the view controller become first responder to handle the ESC key
+  // command. See `-keyCommands` and `-keyCommand_close`.
+  [self becomeFirstResponder];
+}
+
 - (void)addContent:(UIViewController*)content {
   [self addChildViewController:content];
   content.view.translatesAutoresizingMaskIntoConstraints = NO;
   [self.contentContainer addSubview:content.view];
   AddSameConstraints(self.contentContainer, content.view);
   [content didMoveToParentViewController:self];
+}
+
+#pragma mark - UIResponder
+
+// To always be able to register key commands via -keyCommands, the VC must be
+// able to become first responder.
+- (BOOL)canBecomeFirstResponder {
+  return YES;
+}
+
+- (NSArray*)keyCommands {
+  return @[ UIKeyCommand.cr_close ];
+}
+
+- (void)keyCommand_close {
+  base::RecordAction(base::UserMetricsAction("MobileKeyCommandClose"));
+  [self.delegate popupMenuViewControllerWillDismiss:self];
 }
 
 #pragma mark - Private
