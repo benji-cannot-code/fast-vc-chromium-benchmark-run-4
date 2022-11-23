f44bec1f848602cc72ab1994c6c49710a600f1f5
Di Wu <diwux@google.com>
2022-11-23 01:17:28 +0000

    Clicking Ash shelf icon shows a reduced list of tabs
    When it comes to clicking shelf icons, apps (YouTube, Map,
    YouTube Music, Gmail, ...) behave differently from the Ash browser. In both cases, clicking the shelf icon would reveal a list of tabs of the app that is being clicked. The similarity ends here. For apps, this list should only contain tabs whose web contents belong to their original app. It's possible within an app the user wandered into completely unrelated web contents. For tabs showing those unrelated contents, it should not be present in the aforementioned list of tabs. In the meantime, this "filtering" mechanism shouldn't happen if the shelf icon in question is the Ash browser, for obvious reasons.
    
    This CL makes sure no "filtering the list of tabs" is happening when
    clicking the Ash browser shelf icon, so that the list of tabs being
    rendered is complete.
    
    Bug: 1328064
    Change-Id: Ie325cf8f46d7f7d8e034cbf03675be720cbcb457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040848
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074992}

--- chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.cc
+++ chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
+#include "components/app_constants/constants.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/app_window/native_app_window.h"
@@ -148,6 +149,8 @@ class AppMatcher {
     return extension_ ? WebContentMatchesHostedApp(web_contents, browser)
                       : WebContentMatchesWebApp(web_contents, browser);
   }
+
+  bool IsAshBrowser() const { return app_id_ == app_constants::kChromeAppId; }
 
  private:
   bool WebContentMatchesHostedApp(content::WebContents* web_contents,
@@ -459,8 +462,10 @@ AppShortcutShelfItemController::GetAppWebContents(
     TabStripModel* tab_strip = browser->tab_strip_model();
     for (int index = 0; index < tab_strip->count(); index++) {
       content::WebContents* web_contents = tab_strip->GetWebContentsAt(index);
-      if (matcher.WebContentMatchesApp(web_contents, browser))
+      if (matcher.IsAshBrowser() ||
+          matcher.WebContentMatchesApp(web_contents, browser)) {
         items.push_back(web_contents);
+      }
     }
   }
   return items;
