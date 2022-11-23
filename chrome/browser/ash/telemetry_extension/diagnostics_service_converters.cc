95c8c8db9fb8faed660274e56bbae1de7f811f67
Bastian Kersting <bkersting@google.com>
2022-11-23 17:38:01 +0000

    [Telemetry] Introduce runFingerprintAliveRoutine
    Introduce new chrome.os.diagnostics.runFingerprintAliveRoutine API
    function to check if the fingerprint sensor is alive.
    
    Bug: b/25924980
    Test: unit_tests --gtest_filter="*Convert*"
    Test: browser_tests --gtest_filter="*Telemetry*"
    Test: Follow http://go/lacros-test-linux to run tests locally
    Test: ./testing/xvfb.py build/lacros/test_runner.py \
          test out/lacros/unit_tests --gtest_filter="*TelemetryExtension*" \
          --ash-chrome-path ./out/lacros/ash_clang_x64/test_ash_chrome
    Test: ./testing/xvfb.py build/lacros/test_runner.py \
          test out/lacros/lacros_chrome_browsertests \
          --gtest_filter="*TelemetryExtension*"
          --ash-chrome-path ./out/lacros/ash_clang_x64/test_ash_chrome
    NOPRESUBMIT=True
    
    Change-Id: I9cffc2080f08d6376c5f18b20da2b124512e95bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026988
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075206}

--- chrome/browser/ash/telemetry_extension/diagnostics_service_converters.cc
+++ chrome/browser/ash/telemetry_extension/diagnostics_service_converters.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/string_piece.h"
 #include "chrome/browser/ash/wilco_dtc_supportd/mojo_utils.h"
-#include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom-shared.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
 #include "chromeos/crosapi/mojom/diagnostics_service.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -115,6 +114,8 @@ absl::optional<crosapi::mojom::DiagnosticsRoutineEnum> Convert(
       return crosapi::mojom::DiagnosticsRoutineEnum::kGatewayCanBePinged;
     case cros_healthd::mojom::DiagnosticRoutineEnum::kSensitiveSensor:
       return crosapi::mojom::DiagnosticsRoutineEnum::kSensitiveSensor;
+    case cros_healthd::mojom::DiagnosticRoutineEnum::kFingerprintAlive:
+      return crosapi::mojom::DiagnosticsRoutineEnum::kFingerprintAlive;
     default:
       return absl::nullopt;
   }
--- chrome/browser/chromeos/extensions/telemetry/api/fake_diagnostics_service.h
+++ chrome/browser/chromeos/extensions/telemetry/api/fake_diagnostics_service.h
@@ -65,6 +65,8 @@ class FakeDiagnosticsService : public crosapi::mojom::DiagnosticsService {
   void RunFloatingPointAccuracyRoutine(
       uint32_t length_seconds,
       RunFloatingPointAccuracyRoutineCallback callback) override;
+  void RunFingerprintAliveRoutine(
+      RunFingerprintAliveRoutineCallback callback) override;
   void RunGatewayCanBePingedRoutine(
       RunGatewayCanBePingedRoutineCallback callback) override;
   void RunLanConnectivityRoutine(
