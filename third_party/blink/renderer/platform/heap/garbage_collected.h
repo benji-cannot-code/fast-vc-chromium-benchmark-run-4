a93fe3e4a48cb60d12e918d9006cc8de9a0c0407
Daniel Cheng <dcheng@chromium.org>
2022-11-23 22:28:04 +0000

    Ban GarbageCollected<T> from being Unretained in Chrome callbacks.
    This is not a perfect solution:
    
    - The trait implementation does not force T to be fully-defined. A
      forward-declared type can always be used as Unretained(). This is
      tracked as https://crbug.com/1392872.
    - The Blink traits themselves are reliant on the marker types in v8
      always evaluating to void if present. Attempting to use std::void_t<>
      as part of the specialization causes the compiler complain that the
      template specialization is redefined.
    
    Bug: 1386249
    Change-Id: I2b0c2282a11ccea1e8925e5014901925088e699d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049894
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075352}

--- third_party/blink/renderer/platform/heap/garbage_collected.h
+++ third_party/blink/renderer/platform/heap/garbage_collected.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_GARBAGE_COLLECTED_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_GARBAGE_COLLECTED_H_
 
+#include <type_traits>
+
+#include "base/functional/disallow_unretained.h"
 #include "third_party/blink/renderer/platform/heap/thread_state_storage.h"
 #include "v8/include/cppgc/allocation.h"
 #include "v8/include/cppgc/garbage-collected.h"
@@ -52,5 +55,19 @@ T* MakeGarbageCollected(AdditionalBytes additional_bytes, Args&&... args) {
 }
 
 }  // namespace blink
+
+namespace base::internal {
+
+// Do not copy this code. Chromium code should just use DISALLOW_UNRETAINED()
+// directly. This is needed because v8 lives outside the Chromium repository and
+// does not want to even indirectly rely on //base concepts.
+template <typename T>
+struct TypeSupportsUnretained<
+    T,
+    std::enable_if_t<cppgc::IsGarbageCollectedOrMixinTypeV<T>>> {
+  static constexpr inline bool kValue = false;
+};
+
+}  // namespace base::internal
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_GARBAGE_COLLECTED_H_
