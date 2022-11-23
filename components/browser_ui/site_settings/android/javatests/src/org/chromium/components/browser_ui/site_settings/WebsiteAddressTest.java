38d67273b64001aa13156901256e2a9de11ccb70
Andrey Zaytsev <andzaytsev@google.com>
2022-11-23 14:39:47 +0000

    Reland Site Data Improvements: Get rid of duplicate entries
    Relanding the CL with fixes for wildcard patterns. Tested manually.
    Original CL: https://chromium-review.googlesource.com/c/chromium/src/+/3959840
    
    Before: https://screenshot.googleplex.com/BdCDj5tBZYXeZw3.png
    After: https://screenshot.googleplex.com/3crSYBsZX7Hrnk9.png
    
    Bug: 1342991
    Change-Id: I9c816f743d143d1f9390768edd77359cf9217afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047219
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075146}

--- components/browser_ui/site_settings/android/javatests/src/org/chromium/components/browser_ui/site_settings/WebsiteAddressTest.java
+++ components/browser_ui/site_settings/android/javatests/src/org/chromium/components/browser_ui/site_settings/WebsiteAddressTest.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.browser_ui.site_settings;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
 import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
@@ -113,5 +116,17 @@ public class WebsiteAddressTest {
                 // Note: hash codes could still be the same.
             }
         }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Preferences"})
+    public void testConvertHostToOrigin() {
+        WebsiteAddress first = WebsiteAddress.create("developer.android.com");
+        WebsiteAddress second = WebsiteAddress.create("http://developer.android.com");
+        Assert.assertThat(first, not(equalTo(second)));
+
+        WebsiteAddress converted = WebsiteAddress.create(first.getOrigin());
+        Assert.assertEquals(converted, second);
     }
 }
