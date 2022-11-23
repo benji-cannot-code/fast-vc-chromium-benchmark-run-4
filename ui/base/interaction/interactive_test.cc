c830f42ae11214996b7788c63dd8fa35cb608133
Dana Fried <dfried@chromium.org>
2022-11-23 18:18:36 +0000

    [Tutorials] Interaction sequence steps can inherit context.
    This is useful for both interactive testing and tutorials. Sometimes we
    want to have a series of tutorial steps or test steps that execute in
    the same context, which might be different from the starting context.
    
    An example would be items in a WebContents, which receive their own
    context.
    
    This CL modifies how context works in InteractionSequence so that
    instead of two fields:
     - a context, which is used to set the sequence's context and must
       match it if already set
     - "in_any_context" which specifies whether the sequence should look
       only in the sequence context or in any context
    
    The new system is as follows:
     - a single field, which can be:
       - any explicit context (does not have to match the sequence)
       - one of the following:
         - ContextMode::kAny (replaces the old in_any_context)
         - ContextMode::kInitial (uses the sequence context)
         - ContextMode::kFromPreviousStep (uses the context of the
           previous step)
    
    This lets us do sequences of steps like:
    
      kShown kIdentifier ContextMode::kAny
      kActivated kIdentifier ContextMode::kFromPreviousStep
    
    Whereas before those would have required naming the element during the
    first step because an activated step cannot use "in_any_context" (this
    was and still is a limitation of the ElementTracker which can be
    alleviated in a follow-up CL).
    
    Bug: 1385113
    Change-Id: Ic366060196b909fb2ad3e1110d3c3313ada0cfbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049713
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075236}

--- ui/base/interaction/interactive_test.cc
+++ ui/base/interaction/interactive_test.cc
@@ -227,7 +227,7 @@ InteractiveTestApi::MultiStep InteractiveTestApi::FlushEvents() {
 InteractiveTestApi::MultiStep InteractiveTestApi::InAnyContext(
     MultiStep steps) {
   for (auto& step : steps)
-    step.SetFindElementInAnyContext(true);
+    step.SetContext(InteractionSequence::ContextMode::kAny);
   return steps;
 }
 
