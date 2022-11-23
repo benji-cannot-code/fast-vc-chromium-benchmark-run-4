48baa5b8f2b0ed7e5a57bf10d7377ce703a259b6
Alex Turner <alexmt@chromium.org>
2022-11-23 16:18:23 +0000

    Increase data clearing task priority in Private Aggregation
    Currently, the task runner uses BEST_EFFORT priority. This updates that
    to USER_VISIBLE whenever there's a queued or running data clearing task.
    
    This cl also makes the task runner per-service (i.e. per-storage
    partition) instead of global. If a storage partition is destroyed and
    then immediately re-created, this may add a slight risk of concurrent
    file access. However, this should be rare (if even possible) and aligns
    with the behavior of other API storage.
    
    Bug: 1383515
    Change-Id: Ie43fddb382f42feafd4c0b0603c0f6c6a2142e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026466
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075175}

--- content/browser/private_aggregation/private_aggregation_budgeter.h
+++ content/browser/private_aggregation/private_aggregation_budgeter.h
@@ -10,18 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "content/browser/private_aggregation/private_aggregation_budget_key.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/storage_partition.h"
 
-template <class T>
-class scoped_refptr;
-
 namespace base {
 class FilePath;
-class SequencedTaskRunner;
+class UpdateableSequencedTaskRunner;
 }  // namespace base
 
 namespace content {
@@ -94,7 +92,7 @@ class CONTENT_EXPORT PrivateAggregationBudgeter {
 
   // `db_task_runner` should not be nullptr.
   PrivateAggregationBudgeter(
-      scoped_refptr<base::SequencedTaskRunner> db_task_runner,
+      scoped_refptr<base::UpdateableSequencedTaskRunner> db_task_runner,
       bool exclusively_run_in_memory,
       const base::FilePath& path_to_db_dir);
 
@@ -154,6 +152,7 @@ class CONTENT_EXPORT PrivateAggregationBudgeter {
                      base::Time delete_end,
                      StoragePartition::StorageKeyMatcherFunction filter,
                      base::OnceClosure done);
+  void OnClearDataComplete();
 
   void ProcessAllPendingCalls();
 
@@ -162,6 +161,15 @@ class CONTENT_EXPORT PrivateAggregationBudgeter {
   // initialized. The size is limited to `kMaxPendingCalls` except that
   // `ClearData()` can store additional tasks beyond that limit.
   std::vector<base::OnceClosure> pending_calls_;
+
+  // The task runner for all private aggregation storage operations. Updateable
+  // to allow for priority to be temporarily increased to `USER_VISIBLE` when a
+  // clear data task is queued or running. Otherwise `BEST_EFFORT` is used.
+  scoped_refptr<base::UpdateableSequencedTaskRunner> db_task_runner_;
+
+  // How many clear data storage tasks are queued or running currently, i.e.
+  // have been posted but the reply has not been run.
+  int num_pending_clear_data_tasks_ = 0;
 
   // `nullptr` until initialization is complete or if initialization failed.
   // Otherwise, owned by this class until destruction. Iff present,
