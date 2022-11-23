fce5999c1a7879c9353779495489a69618480d3d
Dibyajyoti Pal <dibyapal@chromium.org>
2022-11-23 20:54:18 +0000

    [dPWA] Remove unregistration logic from OS integration manager
    This CL removes the unregistration logic hardcoded to be called
    from the OS integration manager on app being uninstalled. The
    registration/unregistration would be handled by Execute() going
    forward, and separate functions to differentiate between them
    will be handled separately based on per sub manager design.
    
    Bug: b/254491451
    Change-Id: Icc308f689110c6ba267a821a4e250e3c0148f08a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044743
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075315}

--- chrome/browser/web_applications/os_integration/os_integration_manager.h
+++ chrome/browser/web_applications/os_integration/os_integration_manager.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/strings/string_piece_forward.h"
 #include "chrome/browser/web_applications/app_registrar_observer.h"
-#include "chrome/browser/web_applications/os_integration/os_integration_sub_manager.h"
 #include "chrome/browser/web_applications/os_integration/url_handler_manager.h"
 #include "chrome/browser/web_applications/os_integration/web_app_file_handler_manager.h"
 #include "chrome/browser/web_applications/os_integration/web_app_protocol_handler_manager.h"
@@ -40,6 +39,7 @@ class FakeOsIntegrationManager;
 class WebAppIconManager;
 class WebAppRegistrar;
 class WebAppSyncBridge;
+class OsIntegrationSubManager;
 class WebAppUiManager;
 
 // OsHooksErrors contains the result of all Os hook deployments.
@@ -76,10 +76,6 @@ using UninstallOsHooksCallback =
 using UpdateOsHooksCallback =
     base::OnceCallback<void(OsHooksErrors os_hooks_errors)>;
 
-// Callback made when sub-managers are called
-using SubManagerCompletedCallback =
-    base::OnceCallback<void(OsHooksErrors os_hooks_errors)>;
-
 using BarrierCallback =
     base::RepeatingCallback<void(OsHookType::Type os_hook, bool completed)>;
 
@@ -114,8 +110,9 @@ class OsIntegrationManager : public AppRegistrarObserver {
 
   virtual void Start();
 
-  virtual void Synchronize(const AppId& app_id,
-                           SubManagerCompletedCallback callback);
+  // Start OS Integration synchronization from external points. This should be
+  // the only point of call into OsIntegrationManager from external places.
+  virtual void Synchronize(const AppId& app_id, base::OnceClosure callback);
 
   // Install all needed OS hooks for the web app.
   // If provided |web_app_info| is a nullptr, it will read icons data from disk,
@@ -295,15 +292,12 @@ class OsIntegrationManager : public AppRegistrarObserver {
       const AppId& app_id,
       const proto::WebAppOsIntegrationState& desired_state,
       const absl::optional<proto::WebAppOsIntegrationState>& expected_state,
-      SubManagerCompletedCallback callback);
+      base::OnceClosure callback);
 
   virtual void WriteStateToDB(
       const AppId& app_id,
       const proto::WebAppOsIntegrationState& desired_state,
-      SubManagerCompletedCallback done_callback);
-
-  virtual void OnSynchronizationComplete(
-      SubManagerCompletedCallback done_callback);
+      base::OnceClosure callback);
 
   void OnShortcutsCreated(const AppId& app_id,
                           std::unique_ptr<WebAppInstallInfo> web_app_info,
@@ -337,10 +331,10 @@ class OsIntegrationManager : public AppRegistrarObserver {
   std::unique_ptr<WebAppProtocolHandlerManager> protocol_handler_manager_;
   std::unique_ptr<UrlHandlerManager> url_handler_manager_;
 
+  std::vector<std::unique_ptr<OsIntegrationSubManager>> sub_managers_;
+
   base::ScopedObservation<WebAppRegistrar, AppRegistrarObserver>
       registrar_observation_{this};
-
-  std::vector<std::unique_ptr<OsIntegrationSubManager>> sub_managers_;
 
   base::WeakPtrFactory<OsIntegrationManager> weak_ptr_factory_{this};
 };
