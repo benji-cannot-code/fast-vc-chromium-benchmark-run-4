76bf2f15bbec6d721f32944e1c6b39ec99821317
Ella Ge <eirage@chromium.org>
2022-11-23 20:44:57 +0000

    Delete all launchpad code
    Most of them need a rewrite anyway. It's better to get rid of them
    before I forgot.
    
    Bug: 1178919
    Change-Id: Ia7d262c51df3c7691b520d9cf25ee1986d3ffbae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048068
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075308}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -181,7 +181,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAssistantIntentPageUrl,
     &kAssistantIntentTranslateInfo,
     &kAssistantNonPersonalizedVoiceSearch,
-    &kAppLaunchpad,
     &kAppMenuMobileSiteOption,
     &kAppToWebAttribution,
     &kBackgroundThreadPool,
@@ -514,8 +513,6 @@ BASE_FEATURE(kAssistantIntentTranslateInfo,
 BASE_FEATURE(kAssistantNonPersonalizedVoiceSearch,
              "AssistantNonPersonalizedVoiceSearch",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kAppLaunchpad, "AppLaunchpad", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAppMenuMobileSiteOption,
              "AppMenuMobileSiteOption",
