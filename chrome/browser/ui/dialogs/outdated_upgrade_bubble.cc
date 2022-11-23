1099083912da19e7c3e6f40660a1146069160343
MAD <mad@google.com>
2022-11-23 21:13:36 +0000

    Remove unused UMA logging.
    Bug: 1384919
    Change-Id: Ice135586b6fafe246004df82d2ef9c574e465848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048280
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Marc-André Decoste <mad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075327}

--- chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
+++ chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/dialogs/outdated_upgrade_bubble.h"
 
 #include "base/bind.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/strcat.h"
 #include "base/task/thread_pool.h"
@@ -33,12 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// The maximum number of ignored bubble we track in the NumLaterPerReinstall
-// histogram.
-constexpr int kMaxIgnored = 50;
-// The number of buckets we want the NumLaterPerReinstall histogram to use.
-constexpr int kNumIgnoredBuckets = 5;
-
 // For ChromeOS Lacros, browser updates are done via system services, thus
 // we redirect to the safetyCheck page that interacts with these. On other
 // platforms it may be possible to download an updated browser via a site.
@@ -55,29 +48,15 @@ const char* kUpdateBrowserRedirectUrl =
 
 bool g_upgrade_bubble_is_showing = false;
 
-// The number of times the user ignored the bubble before finally choosing to
-// reinstall.
-int g_num_ignored_bubbles = 0;
-
 void OnWindowClosing() {
   g_upgrade_bubble_is_showing = false;
-
-  // Increment the ignored bubble count (if this bubble wasn't ignored, this
-  // increment is offset by a decrement in OnDialogAccepted()).
-  if (g_num_ignored_bubbles < kMaxIgnored)
-    ++g_num_ignored_bubbles;
 }
 
 void OnDialogAccepted(content::PageNavigator* navigator,
                       bool auto_update_enabled,
                       const char* update_browser_redirect_url) {
-  // Offset the +1 in OnWindowClosing().
-  --g_num_ignored_bubbles;
   if (auto_update_enabled) {
     DCHECK(UpgradeDetector::GetInstance()->is_outdated_install());
-    UMA_HISTOGRAM_CUSTOM_COUNTS("OutdatedUpgradeBubble.NumLaterPerReinstall",
-                                g_num_ignored_bubbles, 1, kMaxIgnored,
-                                kNumIgnoredBuckets);
     base::RecordAction(
         base::UserMetricsAction("OutdatedUpgradeBubble.Reinstall"));
 
@@ -88,9 +67,6 @@ void OnDialogAccepted(content::PageNavigator* navigator,
 #if BUILDFLAG(IS_WIN)
   } else {
     DCHECK(UpgradeDetector::GetInstance()->is_outdated_install_no_au());
-    UMA_HISTOGRAM_CUSTOM_COUNTS("OutdatedUpgradeBubble.NumLaterPerEnableAU",
-                                g_num_ignored_bubbles, 1, kMaxIgnored,
-                                kNumIgnoredBuckets);
     base::RecordAction(
         base::UserMetricsAction("OutdatedUpgradeBubble.EnableAU"));
     // Record that the autoupdate flavour of the dialog has been shown.
