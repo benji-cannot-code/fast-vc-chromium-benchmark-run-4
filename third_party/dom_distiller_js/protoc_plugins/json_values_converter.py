3610b9bd17de29fcfba40ade17230c8b87420c87
Matt Menke <mmenke@chromium.org>
2022-11-23 16:03:56 +0000

    Migrate dom_distiller to GetList().
    It was using GetListDeprecated().
    
    Bug: 1338341
    Change-Id: I53ee6d29001c30ebef08ce4bae18c00937b5edec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048035
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075171}

--- third_party/dom_distiller_js/protoc_plugins/json_values_converter.py
+++ third_party/dom_distiller_js/protoc_plugins/json_values_converter.py
@@ -179,7 +179,7 @@ class CppConverterWriter(writer.CodeWriter):
         'if (!value->is_list()) {{\n'
         '  goto error;\n'
         '}}\n'
-        'for (const auto& element : value->GetListDeprecated()) {{\n'
+        'for (const auto& element : value->GetList()) {{\n'
     )
 
     with self.AddIndent():
