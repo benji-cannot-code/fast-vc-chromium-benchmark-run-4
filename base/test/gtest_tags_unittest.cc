47d8dfc654b8630053e5c058cd2363d236c0e230
Jie Sheng <jiesheng@google.com>
2022-11-23 21:15:59 +0000

    Add test tags to gtest result xml file
    With this, import the gtest tags could
    add tag into gtest result xml and upload tags
    into resultDB.
    
    Bug: 1365899
    Change-Id: Id5234fa84e0b9ccb3ef125bec9cba4939dbddfb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044634
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075329}

---
+++ base/test/gtest_tags_unittest.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/gtest_tags.h"
+
+#include "base/test/gtest_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace base {
+
+TEST(GtestTagsTest, AddInvalidName) {
+  EXPECT_DCHECK_DEATH(AddTagToTestResult("", "value"));
+}
+
+TEST(GtestTagsTest, AddValidTag) {
+  AddTagToTestResult("name", "value");
+}
+
+}  // namespace base
