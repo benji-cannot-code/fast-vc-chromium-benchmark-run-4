cb4970829fc05f7b8f6c4b2063f4fa578c1251e2
Yulun Wu <yulunwu@chromium.org>
2022-11-23 20:40:08 +0000

    reenable search box state api test.
    Bug: 1385365
    Change-Id: Ifb3bca214321edd141b6a1e0e9d1e9920c42629d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032429
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075306}

--- chrome/browser/ui/app_list/search/types.cc
+++ chrome/browser/ui/app_list/search/types.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 double Scoring::FinalScore() const {
-  if (filter)
+  if (filter && !override_filter_for_test)
     return -1.0;
   return ftrl_result_score;
 }
