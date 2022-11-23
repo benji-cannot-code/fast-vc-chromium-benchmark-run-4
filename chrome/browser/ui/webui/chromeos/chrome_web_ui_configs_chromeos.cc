ee8c0a39434f153698cb313d504c4d1081e95847
Longbo Wei <longbowei@google.com>
2022-11-23 23:48:16 +0000

    Revert "shortcuts: Create chrome delegate"
    This reverts commit 1238d90cc46b7defc5159b2880171be1a8fd7811.
    
    Reason for revert:
    shortcut_customization_delegate may not be needed because we can use
    GetLastActiveUserPrefService() to get pref_service
    
    
    Original change's description:
    > shortcuts: Create chrome delegate
    >
    > Create a delegate to expose //chrome browser functionality to the
    > Shortcut Customization UI in //ash.
    >
    > Screenshot(Toggle kSendFunctionKeys in DUT):
    > http://screen/5J6zQ9uRNZ7ZiTD
    >
    > Bug: b:216049298
    > Test: ash_webui_unittest && DUT
    > Change-Id: I239bb88851cfe2fdb0c53b934debf1382a57b6e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024042
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: Longbo Wei <longbowei@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1074163}
    
    Bug: b:216049298
    Change-Id: I0a78974063416ea1edb517725720879eda35498a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4052901
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075392}

--- chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/chromeos/chrome_web_ui_configs_chromeos.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h"
 #include "ash/webui/system_extensions_internals_ui/system_extensions_internals_ui.h"
 #include "chrome/browser/ash/web_applications/camera_app/chrome_camera_app_ui_delegate.h"
-#include "chrome/browser/ash/web_applications/shortcut_customization_ui/chrome_shortcut_customization_delegate.h"
 #include "chrome/browser/ui/webui/ash/account_manager/account_manager_error_ui.h"
 #include "chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.h"
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision_ui.h"
@@ -92,11 +91,6 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(
       MakeComponentConfig<ash::CameraAppUIConfig, ash::CameraAppUI,
                           ChromeCameraAppUIDelegate>());
-  map.AddWebUIConfig(
-      MakeComponentConfig<ash::ShortcutCustomizationAppUIConfig,
-                          ash::ShortcutCustomizationAppUI,
-                          ChromeShortcutCustomizationDelegate>());
-
   map.AddWebUIConfig(std::make_unique<ash::AccountManagerErrorUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::AccountMigrationWelcomeUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::AddSupervisionUIConfig>());
@@ -140,6 +134,7 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<ash::PasswordChangeUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::PowerUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SetTimeUIConfig>());
+  map.AddWebUIConfig(std::make_unique<ash::ShortcutCustomizationAppUIConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SlowTraceControllerConfig>());
   map.AddWebUIConfig(std::make_unique<ash::SlowUIConfig>());
   map.AddWebUIConfig(
