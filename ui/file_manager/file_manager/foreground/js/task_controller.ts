886a3bc5762c17253cd5ade2d6e940b2edc83127
Luciano Pacheco <lucmult@chromium.org>
2022-11-23 07:20:58 +0000

    Files app: Fix hiding defaultTask in context menu
    Changes in CL:3948718 caused the defaultTask to not be hidden when there
    is no tasks.
    
    Test: browser_tests --gtest_filter="ContextMenu/FilesAppBrowserTest.Test/checkDefaultTask"
    Bug: b:260161217
    Change-Id: I41b44140ef258e39d90747f2020615a6a0fe62d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4050006
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075049}

--- ui/file_manager/file_manager/foreground/js/task_controller.ts
+++ ui/file_manager/file_manager/foreground/js/task_controller.ts
@@ -477,9 +477,8 @@ export class TaskController {
   private updateContextMenuTaskItems_(
       openTasks: chrome.fileManagerPrivate.ResultingTasks) {
     const taskCount = openTasks.tasks.length;
+    const defaultTask = FileTasks.getDefaultTask(openTasks, this.taskHistory_);
     if (taskCount > 0) {
-      const defaultTask =
-          FileTasks.getDefaultTask(openTasks, this.taskHistory_);
       if (defaultTask) {
         const menuItem = this.ui_.defaultTaskMenuItem;
         /**
@@ -505,11 +504,10 @@ export class TaskController {
         menuItem.label = defaultTask.title;
         menuItem.descriptor = defaultTask.descriptor;
       }
-
-      this.canExecuteDefaultTask_ = defaultTask != null;
-      this.defaultTaskCommand_.canExecuteChange(this.ui_.listContainer.element);
     }
 
+    this.canExecuteDefaultTask_ = defaultTask != null;
+    this.defaultTaskCommand_.canExecuteChange(this.ui_.listContainer.element);
     this.canExecuteOpenActions_ = taskCount > 1;
     this.openWithCommand_.canExecuteChange(this.ui_.listContainer.element);
 
