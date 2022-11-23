f19d0649f5f528ce2f7fc62861ad1f0bae655104
Sean Maher <spvw@chromium.org>
2022-11-23 20:51:39 +0000

    Record UnnecessaryWakeup UMA histograms in ThreadPool
    In the case where a WorkerThread is waken up without having any work to
    run, this UMA is triggered. This allows us to see whether experiments
    increase or decrease on average the number of unnecessary wakeups.
    
      1) Set up a Meet call with multiple participants (go/bondui)
         Make sure the Meet tab is foreground for step (2)
      2) Record a trace from https://ui.perfetto.dev with "wakeup.flow"
         enabled
      3) Verify the presence of "ThreadPool.UnnecessaryWakeup" trace events
         as expected
      4) Verify UMA counts at
         chrome://histograms/ThreadPool.UnnecessaryWakeup
    
    Test: 
    Bug: 1377480
    Change-Id: I322c1a90e8856b3fe1d8aba540147474f9dde5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039985
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075312}

--- base/task/thread_pool/thread_group_impl.cc
+++ base/task/thread_pool/thread_group_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/numerics/clamped_math.h"
 #include "base/ranges/algorithm.h"
 #include "base/sequence_token.h"
@@ -212,6 +213,7 @@ class ThreadGroupImpl::WorkerThreadDelegateImpl : public WorkerThread::Delegate,
   void DidProcessTask(RegisteredTaskSource task_source) override;
   TimeDelta GetSleepTimeout() override;
   void OnMainExit(WorkerThread* worker) override;
+  void RecordUnnecessaryWakeup() override;
 
   // BlockingObserver:
   void BlockingStarted(BlockingType blocking_type) override;
@@ -335,6 +337,7 @@ ThreadGroupImpl::ThreadGroupImpl(StringPiece histogram_label,
                                  TrackedRef<TaskTracker> task_tracker,
                                  TrackedRef<Delegate> delegate)
     : ThreadGroup(std::move(task_tracker), std::move(delegate)),
+      histogram_label_(histogram_label),
       thread_group_label_(thread_group_label),
       thread_type_hint_(thread_type_hint),
       idle_workers_set_cv_for_testing_(lock_.CreateConditionVariable()),
@@ -774,6 +777,15 @@ void ThreadGroupImpl::WorkerThreadDelegateImpl::OnMainExit(
 #endif
   if (outer_->num_workers_cleaned_up_for_testing_cv_)
     outer_->num_workers_cleaned_up_for_testing_cv_->Signal();
+}
+
+void ThreadGroupImpl::WorkerThreadDelegateImpl::RecordUnnecessaryWakeup() {
+  base::BooleanHistogram::FactoryGet(
+      std::string("ThreadPool.UnnecessaryWakeup.") + outer_->histogram_label_,
+      base::Histogram::kUmaTargetedHistogramFlag)
+      ->Add(true);
+
+  TRACE_EVENT_INSTANT("wakeup.flow", "ThreadPool.UnnecessaryWakeup");
 }
 
 void ThreadGroupImpl::WorkerThreadDelegateImpl::BlockingStarted(
