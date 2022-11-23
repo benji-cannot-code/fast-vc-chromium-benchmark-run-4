a693c6fec741ab8860a9050b0ea09df5111f2b83
Andrew Wolfers <aswolfers@chromium.org>
2022-11-23 17:52:49 +0000

    Revert "crosapi: Remove power ash implementation from crosapi"
    This reverts commit 7d4879e3028c3fbf57bf51a5c7691658693cba96.
    
    Reason for revert: Restore PowerAsh implementation because Lacros
    PowerSaveBlocker needs to re-add its dependency.
    
    Original change's description:
    > crosapi: Remove power ash implementation from crosapi
    >
    > PowerAsh is not used anywhere. This CL deletes it and the corresponding
    > CrosapiAsh::BindPower implementation.
    >
    > Bug: b:193670013
    > Change-Id: Iadad3eab18165f822caa6db550ff2b0a0287b587
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645130
    > Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1042554}
    
    Bug: b:260134089
    Bug: b:193670013
    Change-Id: Ia20c9c5db6067a37435e6f1c3dd56e093c06e6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051660
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075217}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/networking_private_ash.h"
 #include "chrome/browser/ash/crosapi/parent_access_ash.h"
 #include "chrome/browser/ash/crosapi/policy_service_ash.h"
+#include "chrome/browser/ash/crosapi/power_ash.h"
 #include "chrome/browser/ash/crosapi/prefs_ash.h"
 #include "chrome/browser/ash/crosapi/remoting_ash.h"
 #include "chrome/browser/ash/crosapi/resource_manager_ash.h"
@@ -233,6 +234,7 @@ CrosapiAsh::CrosapiAsh(CrosapiDependencyRegistry* registry)
           g_browser_process->local_state())),
       parent_access_ash_(std::make_unique<ParentAccessAsh>()),
       policy_service_ash_(std::make_unique<PolicyServiceAsh>()),
+      power_ash_(std::make_unique<PowerAsh>()),
       prefs_ash_(
           std::make_unique<PrefsAsh>(g_browser_process->profile_manager(),
                                      g_browser_process->local_state())),
@@ -650,7 +652,7 @@ void CrosapiAsh::BindPolicyService(
 }
 
 void CrosapiAsh::BindPower(mojo::PendingReceiver<mojom::Power> receiver) {
-  NOTIMPLEMENTED();
+  power_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindPrefs(mojo::PendingReceiver<mojom::Prefs> receiver) {
--- chrome/browser/ash/crosapi/crosapi_ash.h
+++ chrome/browser/ash/crosapi/crosapi_ash.h
@@ -87,6 +87,7 @@ class NetworkingAttributesAsh;
 class NetworkingPrivateAsh;
 class ParentAccessAsh;
 class PolicyServiceAsh;
+class PowerAsh;
 class PrefsAsh;
 class PrintingMetricsAsh;
 class RemotingAsh;
@@ -528,6 +529,7 @@ class CrosapiAsh : public mojom::Crosapi {
   std::unique_ptr<NetworkSettingsServiceAsh> network_settings_service_ash_;
   std::unique_ptr<ParentAccessAsh> parent_access_ash_;
   std::unique_ptr<PolicyServiceAsh> policy_service_ash_;
+  std::unique_ptr<PowerAsh> power_ash_;
   std::unique_ptr<PrefsAsh> prefs_ash_;
   std::unique_ptr<PrintingMetricsAsh> printing_metrics_ash_;
   std::unique_ptr<ash::ProbeServiceAsh> probe_service_ash_;
