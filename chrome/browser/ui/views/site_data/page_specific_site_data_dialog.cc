6f260c8e2125c70d73325418724ac4a4c2e64ac6
Austin Sullivan <asully@chromium.org>
2022-11-23 00:38:37 +0000

    ui: Move DialogModelButton label to Params
    Currently, the label is passed as a separate parameter when adding a
    button. This solution is arguably more clean, especially for buttons that
    take an ID but do not require a custom label
    
    Bug: N/A
    Change-Id: I0151a1e936e7bcc5e9ff274411a40975b8a08929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964876
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074978}

--- chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
@@ -572,7 +572,8 @@ views::Widget* ShowPageSpecificSiteDataDialog(
           base::BindRepeating(&PageSpecificSiteDataDialogModelDelegate::
                                   OnDialogExplicitlyClosed,
                               base::Unretained(delegate)),
-          l10n_util::GetStringUTF16(IDS_DONE))
+          ui::DialogModelButton::Params().SetLabel(
+              l10n_util::GetStringUTF16(IDS_DONE)))
       .SetCloseActionCallback(base::BindOnce(
           &PageSpecificSiteDataDialogModelDelegate::OnDialogExplicitlyClosed,
           base::Unretained(delegate)));
