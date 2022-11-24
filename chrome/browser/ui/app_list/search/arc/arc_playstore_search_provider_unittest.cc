5dba7abe336d158e2add11410e8c2d4b1716657e
CJ Huang <chenjih@google.com>
2022-11-24 05:29:01 +0000

    [launcher] Deprecate kTile and kChip.
    Deprecate SearchResultDisplayType::kTile and SearchResultDisplayType::kChip as they are not used any more.
    
    Bug: b/259439778, b/259483633
    Change-Id: I636cc73e2958d729477f196ebb9015c293f90ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044849
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Auto-Submit: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075470}

--- chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider_unittest.cc
+++ chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider_unittest.cc
@@ -109,7 +109,7 @@ TEST_F(ArcPlayStoreSearchProviderTest, Basic) {
     SCOPED_TRACE(base::StringPrintf("Testing result %zu", i));
     EXPECT_EQ(results[i]->title(),
               base::StrCat({kQuery, u" ", base::NumberToString16(i)}));
-    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kTile);
+    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kList);
     EXPECT_EQ(base::UTF16ToUTF8(results[i]->formatted_price()),
               base::StringPrintf("$%zu.22", i));
     EXPECT_EQ(results[i]->rating(), i);
@@ -150,7 +150,7 @@ TEST_F(ArcPlayStoreSearchProviderTest, PartiallyFailedQuery) {
     SCOPED_TRACE(base::StringPrintf("Testing result %zu", i));
     EXPECT_EQ(results[i]->title(),
               base::StrCat({kQuery, u" ", base::NumberToString16(i)}));
-    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kTile);
+    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kList);
     EXPECT_EQ(base::UTF16ToUTF8(results[i]->formatted_price()),
               base::StringPrintf("$%zu.22", i));
     EXPECT_EQ(results[i]->rating(), i);
@@ -187,7 +187,7 @@ TEST_F(ArcPlayStoreSearchProviderTest, ResultsWithoutPriceAndRating) {
     SCOPED_TRACE(base::StringPrintf("Testing result %zu", i));
     EXPECT_EQ(results[i]->title(),
               base::StrCat({kQuery, u" ", base::NumberToString16(i)}));
-    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kTile);
+    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kList);
     EXPECT_EQ(base::UTF16ToUTF8(results[i]->formatted_price()), "");
     EXPECT_EQ(results[i]->rating(), -1);
     const bool is_instant_app = i % 2 == 0;
@@ -224,7 +224,7 @@ TEST_F(ArcPlayStoreSearchProviderTest, IgnoreResultsWithoutIcon) {
     SCOPED_TRACE(base::StringPrintf("Testing result %zu", i));
     EXPECT_EQ(results[i]->title(),
               base::StrCat({kQuery, u" ", base::NumberToString16(i)}));
-    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kTile);
+    EXPECT_EQ(results[i]->display_type(), ash::SearchResultDisplayType::kList);
     EXPECT_EQ(base::UTF16ToUTF8(results[i]->formatted_price()),
               base::StringPrintf("$%zu.22", i));
     EXPECT_EQ(results[i]->rating(), i);
