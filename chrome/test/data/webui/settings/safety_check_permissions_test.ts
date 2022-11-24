00222ed9765183fbe7574d65ae0604615bb8dfaf
Tom Van Goethem <tov@chromium.org>
2022-11-24 01:14:21 +0000

    [SafetyCheck] Update text of aria label and header text
    This CL aligns the text for the ARIA label of the entry point button and
    the header text with the UX design. Additionally, it fixes an issue with
    the composition of the CSS flexbox of the header.
    
    Bug: 1345920, 1374908
    Change-Id: Ifd968cf8f0c429b1582ca1d24f95ecc946618ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040134
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075410}

--- chrome/test/data/webui/settings/safety_check_permissions_test.ts
+++ chrome/test/data/webui/settings/safety_check_permissions_test.ts
@@ -138,8 +138,7 @@ suite('SafetyCheckNotificationPermissionsUiTests', function() {
       iconStatus: SafetyCheckIconStatus.NOTIFICATION_PERMISSIONS,
       label: 'Review <b>2 sites</b> that recently sent a lot of notifications',
       buttonLabel: 'Review',
-      buttonAriaLabel:
-          'Review 2 sites that recently sent a lot of notifications',
+      buttonAriaLabel: 'Review notification permissions',
     });
 
     // User clicks review button.
@@ -175,8 +174,7 @@ suite('SafetyCheckNotificationPermissionsUiTests', function() {
       iconStatus: SafetyCheckIconStatus.NOTIFICATION_PERMISSIONS,
       label: 'Review <b>1 site</b> that recently sent a lot of notifications',
       buttonLabel: 'Review',
-      buttonAriaLabel:
-          'Review 1 site that recently sent a lot of notifications',
+      buttonAriaLabel: 'Review notification permissions',
     });
 
     // User clicks review button.
