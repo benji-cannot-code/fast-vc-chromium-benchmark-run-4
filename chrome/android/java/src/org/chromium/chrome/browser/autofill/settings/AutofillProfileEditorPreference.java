0e0515f6effd8c8d806d6b773e55cb40173f44a4
Ivana Žužić <izuzic@google.com>
2022-11-24 21:08:31 +0000

    [TTFCC] Modularizing PersonalDataManager and AutofillTestHelper
    Changes:
    - PersonalDataManager, LegalMessageLine, AutofillUiUtils and
    AutofillEditorBase are moved out of chrome_java and into a separate
    target chrome/browser/autofill/android:java
    - PersonalDataManagerTest is moved to
    chrome/browser/autofill/test:test_java
    - AutofillTestHelper is moved to
    chrome/browser/autofill/test:test_support_java
    
    This change should make it easier to use PersonalDataManager.CreditCard
    without making copies or depending on chrome_java.
    The test helper is modularized for a similar reason.
    
    This CL is necessary for https://crrev.com/c/3963013 to be able to not
    introduce more dependencies on chrome_java.
    
    Bug: 1247698
    Change-Id: I55014e101325e3d5297ef87d352ec81996dcaf48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028643
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075630}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfileEditorPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfileEditorPreference.java
@@ -9,6 +9,8 @@ import android.content.Context;
 
 import androidx.preference.DialogPreference;
 
+import org.chromium.chrome.browser.autofill.AutofillEditorBase;
+
 /**
  * Launches the UI to edit, create or delete an Autofill profile entry.
  */
