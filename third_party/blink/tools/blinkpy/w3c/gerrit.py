87464a4e38d49e5ee88a7eb896b43717058da034
Weizhong Xia <weizhong@google.com>
2022-11-24 01:34:38 +0000

    Clean up exported PRs after chromium CLs are abandoned
    We now run this as a step in wpt exporter. After a chromium CL
    has been abandoned for more than two weeks, the exported PR will
    be closed automatically.
    
    Merged CL with no exportable changes will cause a previously exported
    Pr to be closed immediately.
    
    Bug: 1034348
    Change-Id: I835acb1ef98f5eab0fd9776eb99da13533ff1c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049884
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075423}

--- third_party/blink/tools/blinkpy/w3c/gerrit.py
+++ third_party/blink/tools/blinkpy/w3c/gerrit.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import base64
 import json
 import logging
+from datetime import datetime
 from requests.exceptions import HTTPError
 
 from blinkpy.common.net.network_transaction import NetworkTimeout
@@ -147,6 +148,13 @@ class GerritCL(object):
     @property
     def status(self):
         return self._data['status']
+
+    @property
+    def updated(self):
+        # Timestamps are given in UTC and have the format "'yyyy-mm-dd hh:mm:ss.fffffffff'"
+        # where "'ffffffffff'" represents nanoseconds.
+        return datetime.strptime(self._data['updated'][:-10],
+                                 '%Y-%m-%d %H:%M:%S')
 
     @property
     def messages(self):
