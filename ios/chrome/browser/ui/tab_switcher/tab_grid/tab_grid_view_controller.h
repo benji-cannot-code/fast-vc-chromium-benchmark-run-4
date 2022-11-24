e3c26314d7a9a79e473d76e0d869f6924edf1130
Aliona DANGLA <alionadangla@google.com>
2022-11-24 15:13:27 +0000

    [iOS] Add search tabs shortcut from tab grid
    See video:
    https://drive.google.com/file/d/1zGHB3jSKW4MCDs3hQB3R2hCCWilDCknM/view?usp=share_link
    
    Fixed: 1384656
    Change-Id: I27a76284cacf271adee2a6b76b8e2e05b88ca617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026923
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075568}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.h
@@ -95,7 +95,8 @@ enum class TabGridPageConfiguration {
                         KeyCommandActions,
                         LayoutSwitcherProvider,
                         TabGridPaging,
-                        ThumbStripSupporting>
+                        ThumbStripSupporting,
+                        ViewRevealingAnimatee>
 
 @property(nonatomic, weak) id<ApplicationCommands> handler;
 @property(nonatomic, weak) id<IncognitoReauthCommands> reauthHandler;
