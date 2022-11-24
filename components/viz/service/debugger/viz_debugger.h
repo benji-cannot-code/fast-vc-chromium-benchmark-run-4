7b40e363f2c9adb99ec2ad34210e306f232a5be8
Peter McNeeley <petermcneeley@chromium.org>
2022-11-24 21:00:31 +0000

    Visual debugger software renderer buffer capture
    Bug: 1376294
    Change-Id: Ia31043465e1953449d4d6a076b78abd3874705be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044742
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075627}

--- components/viz/service/debugger/viz_debugger.h
+++ components/viz/service/debugger/viz_debugger.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/viz/privileged/mojom/viz_main.mojom.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/vector2d_f.h"
@@ -85,9 +86,7 @@ class VIZ_SERVICE_EXPORT VizDebugger {
     BufferInfo();
     ~BufferInfo();
     BufferInfo(const BufferInfo& a);
-    int width;
-    int height;
-    std::vector<DrawOption> buffer;
+    SkBitmap bitmap;
   };
 
   struct Buffer {
@@ -391,6 +390,15 @@ class VIZ_SERVICE_EXPORT VizDebugger {
     static VizDebugger g_debugger;
     return &g_debugger;
   }
+
+  // These structures are part of public API and must be included.
+  struct VIZ_SERVICE_EXPORT BufferInfo {
+    BufferInfo();
+    ~BufferInfo();
+    BufferInfo(const BufferInfo& a);
+    SkBitmap bitmap;
+  };
+
   inline void CompleteFrame(uint64_t counter,
                             const gfx::Size& window_pix,
                             base::TimeTicks time_ticks) {}
@@ -399,6 +407,7 @@ class VIZ_SERVICE_EXPORT VizDebugger {
   VizDebugger(const VizDebugger&) = delete;
   VizDebugger& operator=(const VizDebugger&) = delete;
 };
+
 }  // namespace viz
 
 #define DBG_OPT_RED 0
