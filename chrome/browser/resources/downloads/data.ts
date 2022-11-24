279cbfe948e025f31064fad5eb66e0668462873a
Rebekah Potter <rbpotter@chromium.org>
2022-11-24 01:59:51 +0000

    Downloads: Update to TypeScript Mojo Bindings
    Bug: 1002978
    Change-Id: I1e652dc8c551a3120397d88e9f19540267ac8282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4007057
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075441}

--- chrome/browser/resources/downloads/data.ts
+++ chrome/browser/resources/downloads/data.ts
@@ -10,6 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {Data} from './downloads.mojom-webui.js';
 
-export class MojomData extends Data {
+export interface MojomData extends Data {
   hideDate: boolean;
 }
