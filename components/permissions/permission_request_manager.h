3664608b4a70a707d5acdcf2afc933fc6909eea4
Illia Klimov <elklm@google.com>
2022-11-24 21:18:36 +0000

    Fix crash in PermissionRequestManager after navigation.
    Bug: 1393252
    Change-Id: I61b442e7ad7eac61de2c8ee60f5941a7e0f8e868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055565
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075635}

--- components/permissions/permission_request_manager.h
+++ components/permissions/permission_request_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "components/permissions/permission_prompt.h"
 #include "components/permissions/permission_request_queue.h"
 #include "components/permissions/permission_ui_selector.h"
@@ -466,6 +467,10 @@ class PermissionRequestManager
   absl::optional<base::TimeDelta> time_to_decision_for_test_;
 
   absl::optional<bool> enabled_app_level_notification_permission_for_testing_;
+
+  // A timer is used to pre-ignore the permission request if it's been displayed
+  // as a quiet chip.
+  base::OneShotTimer preignore_timer_;
 
   base::WeakPtrFactory<PermissionRequestManager> weak_factory_{this};
   WEB_CONTENTS_USER_DATA_KEY_DECL();
