f642bf59d7e69e4f31ee7d188339c5ad6f5e95ca
Ashley Newson <ashleynewson@chromium.org>
2022-11-24 12:21:36 +0000

    Implement heap OOM management for JavaScriptEngine
    Prevent an OOMing isolate from bringing down the entire sandbox by
    freezing the thread before a crash.
    
    JavaScriptEngine runs on Android and thus uses a single process to host
    all V8 isolates and minimize impact on system health. The code which can
    be run in separate isolates may be totally unrelated and thus should not
    interfere with each other's execution.
    
    V8 is designed primarily for a multi-process architecture, where
    crashing the process would be a contained event. As such, its normal
    response to exceeding the heap limit is to abort the process, which is
    inappropriate for the shared-process architecture of JavaScriptEngine.
    
    Therefore, in order to minimise the impact on other isolates, we enter an idle loop in the misbehaving isolate's thread to avoid the crash.
    
    These changes are specifically for heap-based memory management.
    Memory management for Array Buffers will be addressed separately.
    
    Bug: 1347094
    Change-Id: I6ae1a555809b6791c697530bff7b5fc7a99a46f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047785
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075550}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -133,6 +133,7 @@ bool EnsureBrowserStateDirectoriesCreated(const base::FilePath&,
 namespace android_webview {
 class AwFormDatabaseService;
 class CookieManager;
+class JsSandboxIsolate;
 class ScopedAllowInitGLBindings;
 class VizCompositorThreadRunnerWebView;
 }  // namespace android_webview
@@ -684,6 +685,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
 
   // Allowed usage:
   friend class ::ChromeNSSCryptoModuleDelegate;
+  friend class android_webview::JsSandboxIsolate;
   friend class base::internal::GetAppOutputScopedAllowBaseSyncPrimitives;
   friend class base::SimpleThread;
   friend class blink::CategorizedWorkerPoolImpl;
