f64b3b8f41341bef2947030c13a066669b76f15c
Mikel Astiz <mastiz@chromium.org>
2022-11-24 16:35:59 +0000

    [ReadingList] Fix ReadingListModelLoaded() notified too early
    ReadingListModel should notify observers about the model being loaded
    only after sync metadata is also loaded (ModelReadyToSync() invoked).
    Otherwise, there is a theoretical risk that changes to the model during
    this gap may never sync (in practice, changes shortly after browser
    startup).
    
    The issue is minor and unlikely to reproduce in the real world.
    
    For the behavior to be testable, ordering of invocation needs to be
    enforced in unit-tests. To achieve this, the previous home-grown mock
    is replaced with gMock mocks, which also makes unit-tests more standard.
    
    Change-Id: I71368a88a6d3ec3b793d27bd6c5199b6d9680717
    Bug: 1386158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055260
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075579}

--- components/reading_list/core/reading_list_sync_bridge.h
+++ components/reading_list/core/reading_list_sync_bridge.h
@@ -161,8 +161,10 @@ class ReadingListSyncBridge : public ReadingListModelStorage,
       const absl::optional<syncer::ModelError>& error,
       std::unique_ptr<syncer::ModelTypeStore::RecordList> entries);
   void OnDatabaseSave(const absl::optional<syncer::ModelError>& error);
-  void OnReadAllMetadata(const absl::optional<syncer::ModelError>& error,
-                         std::unique_ptr<syncer::MetadataBatch> metadata_batch);
+  void OnReadAllMetadata(
+      ReadingListSyncBridgeDelegate::ReadingListEntries loaded_entries,
+      const absl::optional<syncer::ModelError>& error,
+      std::unique_ptr<syncer::MetadataBatch> metadata_batch);
 
   void AddEntryToBatch(syncer::MutableDataBatch* batch,
                        const ReadingListEntry& entry);
