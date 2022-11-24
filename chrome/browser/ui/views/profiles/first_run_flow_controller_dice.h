8af7f6f2faf4090632b5a84b93685119327038a1
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-11-24 10:59:36 +0000

    [fyfre] Simplify nested callbacks during flow completion
    In our current use cases, we don't have good ways to chain the
    callback that execute the intercepted user intent and passing some
    outcome information to the callback that will close the picker view.
    
    This CL then stops passing callbacks around to simplify the
    implementation.
    
    Bug: 1383969, 1375277
    Change-Id: Ic460fe7be23ed835aa9e329ed1dd5501b9bbb10f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051026
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075544}

--- chrome/browser/ui/views/profiles/first_run_flow_controller_dice.h
+++ chrome/browser/ui/views/profiles/first_run_flow_controller_dice.h
@@ -34,6 +34,10 @@ class FirstRunFlowControllerDice : public ProfileManagementFlowController {
       ProfilePicker::FirstRunExitedCallback first_run_exited_callback);
   ~FirstRunFlowControllerDice() override;
 
+ protected:
+  // ProfileManagementFlowController:
+  bool PreFinishWithBrowser() override;
+
  private:
   void HandleIntroSigninChoice(bool sign_in);
 
