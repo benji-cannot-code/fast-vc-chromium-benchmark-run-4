1491e3a284804e4e46426f088dafb4c93dfe944c
Adam Langley <agl@chromium.org>
2022-11-24 20:55:13 +0000

    webauthn: CONSTRAINT_ERROR from Play Services is not CREDENTIAL_EXCLUDED.
    The mapping from CONSTRAINT_ERROR to CREDENTIAL_EXCLUDED has existed for
    a long time, but it's not true of current Play Services and I'm not sure
    that it ever was. Pertinently, Play Services has added passkey support,
    which involves new cases that result in a CONSTRAINT_ERROR. But
    CREDENTIAL_EXCLUDED is a meaningful error: it indicates to a site that a
    registration was moot because a credential already exists. We shouldn't
    return it unless that is really the case.
    
    Change-Id: Ia77e0894c91b4aa799a9214d537b04eb4eaa3a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053723
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075623}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
@@ -153,7 +153,7 @@ public class Fido2CredentialRequestTest {
                         .name("notSupportedError"),
                 new ParameterSet()
                         .value(Fido2Api.CONSTRAINT_ERR, FILLER_ERROR_MSG,
-                                Integer.valueOf(AuthenticatorStatus.CREDENTIAL_EXCLUDED))
+                                Integer.valueOf(AuthenticatorStatus.UNKNOWN_ERROR))
                         .name("constraintErrorReRegistration"),
                 new ParameterSet()
                         .value(Fido2Api.INVALID_STATE_ERR, FILLER_ERROR_MSG,
