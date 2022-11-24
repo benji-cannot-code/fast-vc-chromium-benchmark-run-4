f29a8e2147f01fc0428b72d09489748de32e5bd7
Ramy El Garawany <elgarawany@google.com>
2022-11-24 01:39:03 +0000

    Using the new RectF/SizeF InvScale function in ui/ code.
    This change builds on the https://crrev.com/c/3887510 change that
    introduced the InvScale function to gfx::RectF and SizeF.
    
    The new result might be slightly different due to floating point
    differences between multiplying by an inverse and dividing. However, the
    new code is arguably more accurate.
    
    Change-Id: Idc625e18739d4e838da655e8c3f00cb27a115b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960873
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Ramy El Garawany <elgarawany@google.com>
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1075426}

--- ui/ozone/platform/wayland/host/wayland_data_drag_controller_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_data_drag_controller_unittest.cc
@@ -439,7 +439,7 @@ TEST_P(WaylandDataDragControllerTest, ReceiveDragPixelSurface) {
   gfx::Point entered_point{800, 600};
   {
     gfx::PointF expected_position(entered_point);
-    expected_position.Scale(1.0f / kTripleScale);
+    expected_position.InvScale(kTripleScale);
 
     EXPECT_CALL(*drop_handler_,
                 MockDragMotion(PointFNear(expected_position), _, _));
@@ -477,7 +477,7 @@ TEST_P(WaylandDataDragControllerTest, ReceiveDragPixelSurface) {
   gfx::Point center_point{400, 300};
   {
     gfx::PointF expected_position(center_point);
-    expected_position.Scale(1.0f / kTripleScale);
+    expected_position.InvScale(kTripleScale);
 
     EXPECT_CALL(*drop_handler_,
                 MockDragMotion(PointFNear(expected_position), _, _))
