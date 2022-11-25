b1d8846259606751c59b634ddbb9d615340e6e4f
Jinho Bang <zino@chromium.org>
2022-11-25 15:10:58 +0000

    installedapp: Use ExceptionState instead of DOMException ctors
    When throwing an exception `e`, it's important to make a stack trace
    available as `e.stack` as long as possible.  However, DOMException's
    constructor doesn't create this `stack` property, so the direct use of
    DOMException's constructor is highly discouraged. So this patch migrates
    `DOMException` constructor to `ExceptionState::ThrowDOMException`.
    
    Bug: 991544
    Change-Id: I7cf4adcbc05bd9e87089c54d69779a13c43224a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055587
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075796}

--- third_party/blink/renderer/modules/installedapp/navigator_installed_app.cc
+++ third_party/blink/renderer/modules/installedapp/navigator_installed_app.cc
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/callback_promise_adapter.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
-#include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
 #include "third_party/blink/renderer/modules/installedapp/installed_app_controller.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
@@ -23,26 +23,27 @@ namespace blink {
 
 ScriptPromise NavigatorInstalledApp::getInstalledRelatedApps(
     ScriptState* script_state,
-    Navigator& navigator) {
+    Navigator& navigator,
+    ExceptionState& exception_state) {
   // [SecureContext] from the IDL ensures this.
   DCHECK(ExecutionContext::From(script_state)->IsSecureContext());
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
 
   if (!navigator.DomWindow()) {
-    auto* exception = MakeGarbageCollected<DOMException>(
+    exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidStateError,
         "The object is no longer associated to a document.");
-    resolver->Reject(exception);
+    resolver->Reject(exception_state);
     return promise;
   }
 
   if (!navigator.DomWindow()->GetFrame()->IsOutermostMainFrame()) {
-    auto* exception = MakeGarbageCollected<DOMException>(
+    exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidStateError,
         "getInstalledRelatedApps() is only supported in "
         "top-level browsing contexts.");
-    resolver->Reject(exception);
+    resolver->Reject(exception_state);
     return promise;
   }
 
--- third_party/blink/renderer/modules/installedapp/navigator_installed_app.h
+++ third_party/blink/renderer/modules/installedapp/navigator_installed_app.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ExceptionState;
 class Navigator;
 class ScriptPromise;
 class ScriptState;
@@ -18,7 +19,9 @@ class NavigatorInstalledApp final {
   STATIC_ONLY(NavigatorInstalledApp);
 
  public:
-  static ScriptPromise getInstalledRelatedApps(ScriptState*, Navigator&);
+  static ScriptPromise getInstalledRelatedApps(ScriptState*,
+                                               Navigator&,
+                                               ExceptionState& exception_state);
 };
 
 }  // namespace blink
