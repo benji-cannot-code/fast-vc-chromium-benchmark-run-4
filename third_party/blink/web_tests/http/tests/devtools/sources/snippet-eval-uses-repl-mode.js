b69ce0eda47c6263d91f8a4052b042eb95f21570
Simon Zünd <szuend@chromium.org>
2022-11-25 05:57:03 +0000

    [devtools] Import web tests from devtools-frontend repo
    When we moved DevTools to it's own repository we experimented with also
    moving the DevTools related web tests. This turned out to be
    infeasible as we would loose a great amount of coverage.
    
    This CL imports the tests that are found only in devtools-frontend,
    after which we can remove the web tests in DevTools.
    
    Bug: 1393314
    Change-Id: I9c6470ecb580fb3c67ef8d2e94a81513c0b92c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055875
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075702}

---
+++ third_party/blink/web_tests/http/tests/devtools/sources/snippet-eval-uses-repl-mode.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult('Tests that snippet scripts are evaluated in REPL mode\n');
+
+  await TestRunner.loadLegacyModule('snippets');
+  await TestRunner.showPanel('sources');
+
+  TestRunner.addSniffer(TestRunner.RuntimeAgent, 'invoke_evaluate', function(args) {
+    TestRunner.addResult('Called RuntimeAgent.invoke_evaluate');
+    TestRunner.addResult('Value of \'replMode\': ' + args.replMode);
+  });
+
+  const uiSourceCode = await Snippets.ScriptSnippetFileSystem.findSnippetsProject().createFile('', null, '');
+  await uiSourceCode.rename('Snippet1');
+  uiSourceCode.setWorkingCopy('let a = 1; let a = 2;');
+
+  await Snippets.evaluateScriptSnippet(uiSourceCode);
+
+  TestRunner.completeTest();
+})();
