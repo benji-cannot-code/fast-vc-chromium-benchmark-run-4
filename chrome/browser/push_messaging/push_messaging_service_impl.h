b7afefe40a4bbd9690f016071e92319e1dbe7980
Takashi Sakamoto <tasak@google.com>
2022-11-25 01:49:12 +0000

    Revert "Revoke site-level Notifications permission."
    This reverts commit 9a8dfe09ddb8860885c903205d897dae0e0d7ec7.
    
    Reason for revert: suspect causing lint failure on android bots.
    
    Sample build faliure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-archive-rel/29557/overview
    
    Sample log:
    ../../chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java:586: Warning: This method should only be accessed from tests or within private scope [VisibleForTests]
                        NotificationSystemStatusUtil.getAppNotificationStatus()
                                                     ~~~~~~~~~~~~~~~~~~~~~~~~
    0 errors, 1 warnings (506 errors, 235 warnings filtered by baseline lint-baseline.xml)
    
    
    Original change's description:
    > Revoke site-level Notifications permission.
    >
    > This CL revokes site-level Notifications permission if Chrome has no
    > app-level Notifications permission on Android. There is a 3-day grace
    > period that starts as soon as Chrome tries to show a notification but
    > has no Notifications permissions.
    >
    > In this CL I'm moving triggering Notifications permission revocation
    > logic from `PushMessagingServiceImpl` to `NotificationPlatformBridge`.
    > The reason for that is we believe `PushMessagingServiceImpl` is too
    > early in the push message processing pipeline and in case of a cold
    > launch it may have inconsistent results regarding webapps verification.
    >
    > `NotificationPlatformBridge::displayNotificationInternal` is at the end
    > of the pipeline and allows filtering webapps from sites without side
    > effects.
    >
    > In the current iteration, we entirely skip the webapps. After a
    > successful launch, I will follow up by including webapps in
    > permission revocation.
    >
    > Bug: 1378515
    > Change-Id: I786ac8c122b96b2e4539b59728f65db2d44a470e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030307
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Illia Klimov <elklm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1075636}
    
    Bug: 1378515
    Change-Id: I274cde3ab168aee62cd7c3e3398a98827f68e6e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054886
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1075660}

--- chrome/browser/push_messaging/push_messaging_service_impl.h
+++ chrome/browser/push_messaging/push_messaging_service_impl.h
@@ -39,16 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 class PrefRegistrySimple;
+class PrefService;
 class Profile;
 class PushMessagingAppIdentifier;
 class PushMessagingServiceTest;
 class FCMRevocationTest;
 class ScopedKeepAlive;
 class ScopedProfileKeepAlive;
-
-#if BUILDFLAG(IS_ANDROID)
-class PrefService;
-#endif
 
 namespace blink {
 namespace mojom {
@@ -70,14 +67,6 @@ class InstanceIDDriver;
 }  // namespace instance_id
 
 namespace {
-
-enum class FcmTokenRevocation {
-  kResetGracePeriod = 0,
-  kRevokePermission = 1,
-  kGracePeriodIsNotOver = 2,
-  kMaxValue = kGracePeriodIsNotOver,
-};
-
 struct PendingMessage {
   PendingMessage(std::string app_id, gcm::IncomingMessage message);
   PendingMessage(const PendingMessage& other);
@@ -118,11 +107,6 @@ class PushMessagingServiceImpl : public content::PushMessagingService,
 #if BUILDFLAG(IS_ANDROID)
   // Registers Local State prefs used by this class.
   static void RegisterPrefs(PrefRegistrySimple* registry);
-
-  static void RevokePermissionIfPossible(GURL origin,
-                                         bool app_level_notifications_enabled,
-                                         PrefService* prefs,
-                                         Profile* profile);
 #endif
 
   // gcm::GCMAppHandler implementation.
@@ -212,6 +196,12 @@ class PushMessagingServiceImpl : public content::PushMessagingService,
       base::RepeatingClosure callback);
   void SetRemoveExpiredSubscriptionsCallbackForTesting(
       base::OnceClosure closure);
+  void set_enabled_app_level_notification_permission_for_testing(bool enabled) {
+    enabled_app_level_notification_permission_for_testing_ = enabled;
+  }
+  void set_prefs_for_testing(PrefService* prefs_for_testing) {
+    prefs_for_testing_ = prefs_for_testing;
+  }
 
  private:
   friend class PushMessagingBrowserTestBase;
@@ -263,6 +253,17 @@ class PushMessagingServiceImpl : public content::PushMessagingService,
 
   void CheckOriginAndDispatchNextMessage();
 
+#if BUILDFLAG(IS_ANDROID)
+  //  Verifies if Chrome has Android app-level Notifications permission. If
+  //  app-level permission is missing, `message` will be ignored, and site-level
+  //  Notifications permission and FCM will be revoked.
+  //
+  //  Returns true if `message` should be ignored, returns false otherwise.
+  bool CheckAndRevokeNotificationPermissionIfNeeded(
+      const std::string& app_id,
+      const gcm::IncomingMessage& message,
+      const PushMessagingAppIdentifier& app_identifier);
+#endif
   // Subscribe methods ---------------------------------------------------------
 
   void DoSubscribe(PushMessagingAppIdentifier app_identifier,
@@ -498,6 +499,9 @@ class PushMessagingServiceImpl : public content::PushMessagingService,
   bool shutdown_started_ = false;
 
   int render_process_id_ = content::ChildProcessHost::kInvalidUniqueID;
+
+  absl::optional<bool> enabled_app_level_notification_permission_for_testing_;
+  absl::optional<PrefService*> prefs_for_testing_;
 
   base::WeakPtrFactory<PushMessagingServiceImpl> weak_factory_{this};
 };
