8b04ae39478ade3067298f89716d870c8bbdef9d
David Roger <droger@chromium.org>
2022-11-25 11:13:14 +0000

    Restrict GetPrimaryUserProfile() to ChromeOS and Android
    Bug: 1264436
    Change-Id: I59545b80c34a6b803f2a7ab3df3c8a3668b91525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4036534
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075758}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -669,6 +669,8 @@ Profile* ProfileManager::GetLastUsedProfile() {
 // static
 Profile* ProfileManager::GetLastUsedProfileIfLoaded() {
   ProfileManager* profile_manager = g_browser_process->profile_manager();
+  if (!profile_manager)  // Can be null in unit tests.
+    return nullptr;
   return profile_manager->GetProfileByPath(
       profile_manager->GetLastUsedProfileDir());
 }
@@ -731,6 +733,7 @@ std::vector<Profile*> ProfileManager::GetLastOpenedProfiles() {
   return to_return;
 }
 
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
 // static
 Profile* ProfileManager::GetPrimaryUserProfile() {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -763,6 +766,7 @@ Profile* ProfileManager::GetPrimaryUserProfile() {
 
   return profile_manager->GetActiveUserOrOffTheRecordProfile();
 }
+#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
 
 // static
 Profile* ProfileManager::GetActiveUserProfile() {
