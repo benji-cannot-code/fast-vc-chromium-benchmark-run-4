03229ac81ab5ca045f6f71adf9e73f8eb104452d
Mitsuru Oshima <oshima@chromium.org>
2022-11-25 11:51:52 +0000

    Simplified Wayland unit tests
    This is a follow up CL for crrev.com/c/4039745.
    
    Bug: 1355936
    Change-Id: Ia6979f1d4b0cb5a98e21b5ec2d5cdaa20a667aa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054360
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075771}

--- ui/ozone/platform/wayland/host/wayland_screen_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_screen_unittest.cc
@@ -1322,4 +1322,10 @@ INSTANTIATE_TEST_SUITE_P(XdgVersionStableTest,
                          LazilyConfiguredScreenTest,
                          Values(wl::ServerConfig{}));
 
+INSTANTIATE_TEST_SUITE_P(
+    XdgVersionStableTestWithAuraShell,
+    LazilyConfiguredScreenTest,
+    Values(wl::ServerConfig{
+        .enable_aura_shell = wl::EnableAuraShellProtocol::kEnabled}));
+
 }  // namespace ui
