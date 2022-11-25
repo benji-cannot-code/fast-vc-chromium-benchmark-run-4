678d93b68d96321e3164465b49ec27896e99aace
Sebastien Lalancette <seblalancette@chromium.org>
2022-11-25 15:44:38 +0000

    [DTC] Collect CS Agent ID
    Add collection of CS agent and customer IDs on Windows. Those values
    are then forwarded as part of the inline flow's handshake.
    
    Fixed: b:260091069
    Change-Id: I47eefa4188f2d33fabb7062ed7473ce24d3b00ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054088
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075805}

--- components/device_signals/test/signals_contract.cc
+++ components/device_signals/test/signals_contract.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/test/bind.h"
 #include "build/build_config.h"
 #include "components/device_signals/core/common/signals_constants.h"
 
@@ -153,6 +154,29 @@ GetSignalsContract() {
       base::BindRepeating(VerifyOptionalString, names::kWindowsUserDomain);
   contract[names::kSecureBootEnabled] =
       base::BindRepeating(VerifyIsSettingInteger, names::kSecureBootEnabled);
+
+  contract[names::kCrowdStrike] =
+      base::BindLambdaForTesting([](const base::Value::Dict& signals) {
+        // CrowdStrike signals are optional. But if the object is set, then at
+        // least one of the values must be present.
+        auto* cs_value = signals.Find(device_signals::names::kCrowdStrike);
+        if (!cs_value) {
+          return true;
+        }
+
+        if (!cs_value->is_dict()) {
+          return false;
+        }
+
+        const auto& cs_dict = cs_value->GetDict();
+        auto* customer_id =
+            cs_dict.FindString(device_signals::names::kCustomerId);
+        auto* agent_id = cs_dict.FindString(device_signals::names::kAgentId);
+
+        return (customer_id && !customer_id->empty()) ||
+               (agent_id && !agent_id->empty());
+      });
+
 #else
   // Windows-only signals that shouldn't be set on other platforms.
   contract[names::kChromeCleanupEnabled] =
@@ -163,6 +187,8 @@ GetSignalsContract() {
       base::BindRepeating(VerifyUnset, names::kWindowsUserDomain);
   contract[names::kSecureBootEnabled] =
       base::BindRepeating(VerifyUnset, names::kSecureBootEnabled);
+  contract[names::kCrowdStrike] =
+      base::BindRepeating(VerifyUnset, names::kCrowdStrike);
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
