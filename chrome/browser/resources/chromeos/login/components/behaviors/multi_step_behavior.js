056fa86932ef89c90dd2e96c789bf6c375891db6
Renato Silva <rrsilva@google.com>
2022-11-25 11:16:50 +0000

    [OOBE] - Polymer3 - Check-in display_manager.js
    Check-in the modularised version of display_manager.js.
    
    Bug: b/260015687, b/250886207
    Change-Id: I7a165e540c4689f53619e397bb0193c817c068bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055713
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075761}

--- chrome/browser/resources/chromeos/login/components/behaviors/multi_step_behavior.js
+++ chrome/browser/resources/chromeos/login/components/behaviors/multi_step_behavior.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 // #import {dom, Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-// #import {invokePolymerMethod} from '../../display_manager.m.js';
+// #import {invokePolymerMethod} from '../../display_manager.js';
 // clang-format on
 
 /**
