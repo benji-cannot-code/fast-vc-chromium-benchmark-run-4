151ecc95a258fa3a700f965c9ab0843829a5f0c1
Duong Dac <ddac@chromium.org>
2022-11-25 21:57:58 +0000

    [password-manager] Hide email for applicable accounts
    This change concerns accounts that are not capable to have the email
    address displayed.
    
    For fields that typically display the email address,
    - Replace with the user's display name (First Last) if available.
    - Otherwise, leave blank.
    
    In action:
    https://docs.google.com/document/d/1_W3he3EidbBUUmkXqDfBp1HyWwynx3N0jclAyeE5Ies
    
    This CL was uploaded by git cl split.
    
    R=fhorschig@chromium.org
    
    Bug: 1284725, b/250624181, b/240319819
    Change-Id: I072efc56243dab2206c4a7444a0fb72d4f4726f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055706
    Auto-Submit: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075839}

--- chrome/browser/password_manager/android/save_update_password_message_delegate.cc
+++ chrome/browser/password_manager/android/save_update_password_message_delegate.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/android/android_theme_resources.h"
 #include "chrome/browser/android/resource_mapper.h"
+#include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/password_manager/android/password_infobar_utils.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/profiles/profile.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_ui_utils.h"
 #include "components/password_manager/core/common/password_manager_features.h"
+#include "components/signin/public/identity_manager/tribool.h"
 #include "components/url_formatter/elide_url.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -140,8 +142,19 @@ void SaveUpdatePasswordMessageDelegate::DisplaySaveUpdatePasswordPromptInternal(
   } else {
     passwords_state_.OnPendingPassword(std::move(form_to_save));
   }
-  account_email_ =
-      account_info.has_value() ? account_info.value().email : std::string();
+
+  if (account_info.has_value()) {
+    if (base::FeatureList::IsEnabled(
+            chrome::android::kHideNonDisplayableAccountEmail) &&
+        account_info->capabilities.can_have_email_address_displayed() ==
+            signin::Tribool::kFalse) {
+      account_email_ = account_info.value().full_name;
+    } else {
+      account_email_ = account_info.value().email;
+    }
+  } else {
+    account_email_ = std::string();
+  }
 
   CreateMessage(update_password);
   RecordMessageShownMetrics();
