dd43ad26e18ddfe9bea55d2caef9d6778755fccd
Ari Chivukula <arichiv@chromium.org>
2022-11-25 20:20:21 +0000

    [Expired Histograms] Extensions.WebRequest.OnBeforeSendHeadersEventResponse, Extensions.WebRequest.OnHeadersReceivedEventResponse
    This CL is part of a code health project to remove expired histogram
    logging from the codebase. This data has not been recorded since M77.
    
    Bug: 1384837, 827582
    Change-Id: Ia541e0ea4a5e9fba2377740d11906f1205983f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048141
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075833}

--- extensions/browser/api/web_request/web_request_api.cc
+++ extensions/browser/api/web_request/web_request_api.cc
@@ -1278,11 +1278,6 @@ int ExtensionWebRequestEventRouter::OnBeforeSendHeaders(
         browser_context, request, listeners, std::move(event_details));
   }
 
-  UMA_HISTOGRAM_ENUMERATION(
-      "Extensions.WebRequest.OnBeforeSendHeadersEventResponse",
-      initialize_blocked_requests ? WebRequestEventResponse::kObserved
-                                  : WebRequestEventResponse::kIgnored);
-
   if (!initialize_blocked_requests)
     return net::OK;  // Nobody saw a reason for modifying the request.
 
@@ -1363,11 +1358,6 @@ int ExtensionWebRequestEventRouter::OnHeadersReceived(
     initialize_blocked_requests |= DispatchEvent(
         browser_context, request, listeners, std::move(event_details));
   }
-
-  UMA_HISTOGRAM_ENUMERATION(
-      "Extensions.WebRequest.OnHeadersReceivedEventResponse",
-      initialize_blocked_requests ? WebRequestEventResponse::kObserved
-                                  : WebRequestEventResponse::kIgnored);
 
   if (!initialize_blocked_requests)
     return net::OK;  // Nobody saw a reason for modifying the request.
