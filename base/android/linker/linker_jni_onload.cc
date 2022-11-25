d471d6b900fc9ec85e47d1fbb0eb85c35fae2e2e
Egor Pasko <pasko@chromium.org>
2022-11-25 15:00:50 +0000

    Remove third_party/android_crazy_linker
    The libchromium_android_linker.so uses `__android_log_print`.  Add
    `-llog` linker flag to pull the exporting library from the NDK.
    
    The modern_linker_jni.cc depends on C++ operator delete (mangled as
    _ZdlPv). To avoid pulling all libc++ dependencies the crazylinker
    defined its own operators new and delete. Copy them into
    modern_linker_jni verbatim, with tiny changes to remove confusing
    references to 'crazy'. The linker_jni and modern_linker_jni will be
    merged in a followup change.
    
    Bug: 1383210
    Change-Id: Iaffcd4b27b2a4a071dd507217218cb22f5431e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055528
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075793}

--- base/android/linker/linker_jni_onload.cc
+++ base/android/linker/linker_jni_onload.cc
@@ -7,13 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // conflict with JNI_Onload() defined by the test library. The linker tests
 // together with the linker internals are smashed into (=linked with) the test
 // library.
-//
-// This file also helps avoiding LegacyLinkerJNIInit() and its dependencies in
-// base_unittests. There are no plans to unittest LegacyLinker.
 
 #include <jni.h>
 
-#include "base/android/linker/legacy_linker_jni.h"
 #include "base/android/linker/linker_jni.h"
 #include "base/android/linker/modern_linker_jni.h"
 
@@ -42,8 +38,7 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     LOG_ERROR("Could not create JNIEnv");
     return -1;
   }
-  if (!LinkerJNIInit(vm, env) || !LegacyLinkerJNIInit(vm, env) ||
-      !ModernLinkerJNIInit(vm, env)) {
+  if (!LinkerJNIInit(vm, env) || !ModernLinkerJNIInit(vm, env)) {
     return -1;
   }
   LOG_INFO("Done");
