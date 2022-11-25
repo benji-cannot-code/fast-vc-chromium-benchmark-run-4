eaddefcb9d46d46ff57db0a960e84f6d9b95f770
Patrick Meenan <pmeenan@chromium.org>
2022-11-25 16:07:01 +0000

    Remove experiment for batching of fetch requests
    This reverts the changes from
    https://chromium-review.googlesource.com/c/chromium/src/+/3751095
    
    The experiment has completed and showed degraded performance. Details
    in https://crbug.com/1338206 and design doc:
    https://docs.google.com/document/d/1tF_Vgvfn3Wff18uPx9bJnckK8nvyaBNWahEX6MfjMnk/edit?usp=sharing
    
    Bug: 1338206
    Change-Id: I292db91ae716a01d4978c94dd62756e35542635c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030932
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075810}

--- third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/loading_behavior_observer.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher_properties.h"
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/aggregated_metric_reporter.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_status.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
@@ -175,14 +174,7 @@ void ResourceLoadScheduler::Request(ResourceLoadSchedulerClient* client,
 
   // Remember the ClientId since MaybeRun() below may destruct the caller
   // instance and |id| may be inaccessible after the call.
-  // Don't run if we are still batching fetch requests.
-
-  // MaybeRun() will get called directly at the end of the batch if a batch
-  // operation is active.
-  if (0 == pending_batch_operations_ ||
-      !RuntimeEnabledFeatures::BatchFetchRequestsEnabled()) {
-    MaybeRun();
-  }
+  MaybeRun();
 }
 
 void ResourceLoadScheduler::SetPriority(ClientId client_id,
@@ -481,19 +473,6 @@ void ResourceLoadScheduler::SetConnectionInfo(
   }
 
   MaybeRun();
-}
-
-void ResourceLoadScheduler::StartBatch() {
-  pending_batch_operations_++;
-}
-
-void ResourceLoadScheduler::EndBatch() {
-  DCHECK_NE(0U, pending_batch_operations_);
-
-  pending_batch_operations_--;
-  if (0 == pending_batch_operations_) {
-    MaybeRun();
-  }
 }
 
 bool ResourceLoadScheduler::CanRequestForMultiplexedConnectionsInTight() const {
