84cda3370e222d07fec71832b0ba13f1e5bf4b50
Roman Sorokin <rsorokin@chromium.org>
2022-11-25 15:31:49 +0000

    [Sheriff] Disable *PendingRequestsGetProcessed on Mac
    Bug: 1365018
    Change-Id: Ie5e43b122afc062c2904c89741011ca6e860c18f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055727
    Reviewed-by: Michael Ershov <miersh@google.com>
    Owners-Override: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075799}

--- chrome/browser/enterprise/connectors/analysis/local_binary_upload_service_unittest.cc
+++ chrome/browser/enterprise/connectors/analysis/local_binary_upload_service_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/analysis/local_binary_upload_service.h"
 
 #include "base/callback_helpers.h"
+#include "build/build_config.h"
 #include "chrome/browser/enterprise/connectors/analysis/analysis_settings.h"
 #include "chrome/browser/enterprise/connectors/analysis/fake_content_analysis_sdk_manager.h"
 #include "chrome/test/base/testing_profile.h"
@@ -196,7 +197,13 @@ TEST_F(LocalBinaryUploadServiceTest, SomeRequestsArePending) {
   EXPECT_EQ(1u, lbus.GetPendingRequestCountForTesting());
 }
 
-TEST_F(LocalBinaryUploadServiceTest, PendingRequestsGetProcessed) {
+// Flaky on mac, http://crbug.com/1365018
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_PendingRequestsGetProcessed DISABLED_PendingRequestsGetProcessed
+#else
+#define MAYBE_PendingRequestsGetProcessed PendingRequestsGetProcessed
+#endif
+TEST_F(LocalBinaryUploadServiceTest, MAYBE_PendingRequestsGetProcessed) {
   LocalBinaryUploadService lbus;
 
   content_analysis::sdk::ContentAnalysisResponse response;
