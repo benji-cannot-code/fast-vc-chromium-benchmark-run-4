3332e131a390fa634922c23f6ddc528a0bec2ea6
Aliona DANGLA <alionadangla@google.com>
2022-11-25 15:28:40 +0000

    [iOS] Add Close All and Undo shortcut
    See video:
    https://drive.google.com/file/d/1fzTcLFnA1Bkn4SDiEre4g0ex-6mbu4SJ/view?usp=share_link
    
    Fixed: 1385871
    Change-Id: I48ca7f13c6663ff39900d616cdb3ce82c631bf13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027080
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075798}

--- ios/chrome/browser/ui/keyboard/key_command_actions.h
+++ ios/chrome/browser/ui/keyboard/key_command_actions.h
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)keyCommand_showReadingList;
 - (void)keyCommand_goToTabGrid;
 - (void)keyCommand_clearBrowsingData;
+- (void)keyCommand_closeAll;
+- (void)keyCommand_undo;
 
 @end
 
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -164,7 +164,6 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 @property(nonatomic, strong) UIControl* scrimView;
 @property(nonatomic, weak) TabGridTopToolbar* topToolbar;
 @property(nonatomic, weak) TabGridBottomToolbar* bottomToolbar;
-@property(nonatomic, assign) BOOL undoCloseAllAvailable;
 // Bool informing if the confirmation action sheet is displayed.
 @property(nonatomic, assign) BOOL closeAllConfirmationDisplayed;
 @property(nonatomic, assign) TabGridConfiguration configuration;
@@ -2618,6 +2617,29 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   }
 }
 
+#pragma mark - UIResponder Helper
+
+// Returns YES if "close all" can be performed. Conditions are:
+// * Tab grid is currently displayed,
+// * There are tabs to close in the current page,
+// * Not in an undo scenario.
+- (BOOL)canCloseAllTab {
+  return self.currentState == ViewRevealState::Revealed &&
+         ((self.currentPage == TabGridPageIncognitoTabs &&
+           !self.incognitoTabsViewController.gridEmpty) ||
+          (self.currentPage == TabGridPageRegularTabs &&
+           !self.regularTabsViewController.gridEmpty &&
+           !self.undoCloseAllAvailable));
+}
+
+// Returns YES if "undo" the close all action can be performed.
+- (BOOL)canUndoCloseAllTab {
+  return /* Ensure the tab grid is currently displayed. */ self.currentState ==
+             ViewRevealState::Revealed &&
+         self.currentPage == TabGridPageRegularTabs &&
+         self.undoCloseAllAvailable;
+}
+
 #pragma mark - UIResponder
 
 // To always be able to register key commands via -keyCommands, the VC must be
@@ -2631,6 +2653,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
     // Other key commands are already declared in the menu.
     return @[
       UIKeyCommand.cr_openNewRegularTab,
+      UIKeyCommand.cr_undo,
       // TODO(crbug.com/1385469): Move it to the menu builder once we have the
       // strings.
       UIKeyCommand.cr_select2,
@@ -2653,6 +2676,12 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   }
   if (sel_isEqual(action, @selector(keyCommand_find))) {
     return self.currentState == ViewRevealState::Revealed;
+  }
+  if (sel_isEqual(action, @selector(keyCommand_closeAll))) {
+    return [self canCloseAllTab];
+  }
+  if (sel_isEqual(action, @selector(keyCommand_undo))) {
+    return [self canUndoCloseAllTab];
   }
   return [super canPerformAction:action withSender:sender];
 }
@@ -2705,6 +2734,17 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   base::RecordAction(
       base::UserMetricsAction("MobileKeyCommandGoToRemoteTabGrid"));
   [self setCurrentPageAndPageControl:TabGridPageRemoteTabs animated:YES];
+}
+
+- (void)keyCommand_closeAll {
+  base::RecordAction(base::UserMetricsAction("MobileKeyCommandCloseAll"));
+  [self closeAllButtonTapped:nil];
+}
+
+- (void)keyCommand_undo {
+  base::RecordAction(base::UserMetricsAction("MobileKeyCommandUndo"));
+  // This function is also responsible for handling undo.
+  [self closeAllButtonTapped:nil];
 }
 
 @end
