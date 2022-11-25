7ff28e85e171355bd0c5ab96f179539388f622ee
Cassie Wang <cassiewang@google.com>
2022-11-25 15:06:37 +0000

    arc: Pass ArcHostVpn flag value to ARC
    Flag value is only sent to ARC on initial mojo connection.
    
    ARC-side change: http://ag/20426391
    
      toggled flag on/off on DUT
      start a builtin host VPN
      observed ArcHostVpn started/not started in ARC
    
    Bug: 257889534
    Test: autoninja -C out/Default unit_tests
    Test: manual:
    Change-Id: I597348cda8bfa4495ea99b997bb4467d1084fe96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4019075
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Cassie Wang <cassiewang@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075794}

--- ash/components/arc/net/arc_net_host_impl.cc
+++ ash/components/arc/net/arc_net_host_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/components/arc/arc_browser_context_keyed_service_factory_base.h"
+#include "ash/components/arc/arc_features.h"
 #include "ash/components/arc/arc_prefs.h"
 #include "ash/components/arc/net/cert_manager.h"
 #include "ash/components/arc/session/arc_bridge_service.h"
@@ -622,6 +623,18 @@ void ArcNetHostImpl::OnConnectionReady() {
 
   // Listen on network configuration changes.
   ash::PatchPanelClient::Get()->AddObserver(this);
+
+  SetUpFlags();
+}
+
+void ArcNetHostImpl::SetUpFlags() {
+  auto* net_instance =
+      ARC_GET_INSTANCE_FOR_METHOD(arc_bridge_service_->net(), SetUpFlag);
+  if (!net_instance)
+    return;
+
+  net_instance->SetUpFlag(arc::mojom::Flag::ENABLE_ARC_HOST_VPN,
+                          base::FeatureList::IsEnabled(arc::kEnableArcHostVpn));
 }
 
 void ArcNetHostImpl::OnConnectionClosed() {
