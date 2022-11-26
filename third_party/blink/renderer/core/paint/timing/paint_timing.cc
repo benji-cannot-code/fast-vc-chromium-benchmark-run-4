6b16a3507dd5ed9510015cc219b59080a0e0d664
Ari Chivukula <arichiv@chromium.org>
2022-11-26 17:16:50 +0000

    [CrossThreadHandle] Convert third_party/blink/renderer/core/paint/paint_timing.cc
    This is part of a cleanup task to make memory safer when data isn't used
    cross thread, but is instead merely passed to be used back again on the
    originating thread. Design doc:
    https://docs.google.com/document/d/1GIT0ysdQ84sGhIo1r9EscF_fFt93lmNVM_q4vvHj2FQ/edit#
    
    Bug: 1377345
    Change-Id: I18467b519da52ac14aa4adc4eb2bb976665fb7a0
    
    fuzzer infra failure
    
    No-Try: true
    Change-Id: I18467b519da52ac14aa4adc4eb2bb976665fb7a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047900
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075899}

--- third_party/blink/renderer/core/paint/timing/paint_timing.cc
+++ third_party/blink/renderer/core/paint/timing/paint_timing.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/timing/performance_timing_for_reporting.h"
 #include "third_party/blink/renderer/core/timing/window_performance.h"
 #include "third_party/blink/renderer/platform/graphics/paint/ignore_paint_timing_scope.h"
+#include "third_party/blink/renderer/platform/heap/cross_thread_handle.h"
 #include "third_party/blink/renderer/platform/instrumentation/resource_coordinator/document_resource_coordinator.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
@@ -269,7 +270,7 @@ void PaintTiming::SetFirstContentfulPaint(base::TimeTicks stamp) {
 void PaintTiming::RegisterNotifyPresentationTime(PaintEvent event) {
   RegisterNotifyPresentationTime(
       CrossThreadBindOnce(&PaintTiming::ReportPresentationTime,
-                          WrapCrossThreadWeakPersistent(this), event));
+                          MakeUnwrappingCrossThreadWeakHandle(this), event));
 }
 
 void PaintTiming::
@@ -278,7 +279,7 @@ void PaintTiming::
   RegisterNotifyPresentationTime(CrossThreadBindOnce(
       &PaintTiming::
           ReportFirstPaintAfterBackForwardCacheRestorePresentationTime,
-      WrapCrossThreadWeakPersistent(this), index));
+      MakeUnwrappingCrossThreadWeakHandle(this), index));
 }
 
 void PaintTiming::RegisterNotifyPresentationTime(ReportTimeCallback callback) {
