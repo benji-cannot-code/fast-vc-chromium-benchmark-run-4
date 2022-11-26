35a38eae6d76e56c341eb40d34205138b5a3bba5
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-26 09:21:31 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=shaochuan@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Ia2f3574b08298db21bdee07f455c3069f896132b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057286
    Commit-Queue: Shao-Chuan Lee <shaochuan@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Shao-Chuan Lee <shaochuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075878}

--- ash/components/arc/metrics/stability_metrics_manager.cc
+++ ash/components/arc/metrics/stability_metrics_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/metrics/stability_metrics_manager.h"
 
+#include <ostream>
+
 #include "ash/components/arc/arc_prefs.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/prefs/pref_service.h"
