aac71a6503072ec8d0a95f8ecbadabfd9ac270cf
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-26 05:56:04 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=gab@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I5403440cc901dbd663afe917aa212b708642610b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057349
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075867}

--- services/preferences/tracked/dictionary_hash_store_contents.cc
+++ services/preferences/tracked/dictionary_hash_store_contents.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/preferences/tracked/dictionary_hash_store_contents.h"
 
+#include <ostream>
+
 #include "base/callback.h"
 #include "base/check.h"
 #include "base/notreached.h"
