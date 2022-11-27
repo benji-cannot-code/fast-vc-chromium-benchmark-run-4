d161035c78806cc80301da3925c4de4a20e7f3bc
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-27 20:48:10 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=fayang@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Ia06841a7ab2f9874e3655c8cf3b123e6d025353b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057317
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Fan Yang <fayang@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076012}

--- net/base/backoff_entry_serializer.cc
+++ net/base/backoff_entry_serializer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/backoff_entry_serializer.h"
 
 #include <algorithm>
+#include <ostream>
 #include <utility>
 
 #include "base/notreached.h"
--- net/base/net_string_util_icu_alternatives_ios.mm
+++ net/base/net_string_util_icu_alternatives_ios.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <CoreFoundation/CoreFoundation.h>
 
+#include <ostream>
 #include <string>
 
 #include "base/check.h"
