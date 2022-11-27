ec94febb88f45b0e526f61e0bb40e7884200e08d
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-27 20:29:47 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=rkaplow@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I48323d8152f79aa893713d32c1e94c7f25f5ea1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057307
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076007}

--- base/metrics/sample_vector.cc
+++ base/metrics/sample_vector.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/sample_vector.h"
 
+#include <ostream>
+
 #include "base/check_op.h"
 #include "base/lazy_instance.h"
 #include "base/memory/ptr_util.h"
