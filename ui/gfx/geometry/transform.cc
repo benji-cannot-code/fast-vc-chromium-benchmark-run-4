6bf08c5f166e5a43eb2ba0f2fde9528395048576
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-27 20:14:57 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=danakj@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I0bb9381c5cc3c7891c6b04cb9f283ad25a1a0feb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057398
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076003}

--- ui/gfx/geometry/transform.cc
+++ ui/gfx/geometry/transform.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/geometry/transform.h"
 
+#include <ostream>
+
 #include "base/check_op.h"
 #include "base/notreached.h"
 #include "base/strings/stringprintf.h"
