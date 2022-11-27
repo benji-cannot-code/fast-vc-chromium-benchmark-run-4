b5a8216b86c0df3a3a9d5007e2f870cefd3d2da6
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-27 20:11:29 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=etiennep@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Id7a1dbef101d01a3a3dd36368ef83d29257e9728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057547
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076002}

--- tools/mac/power/power_sampler/json_exporter.cc
+++ tools/mac/power/power_sampler/json_exporter.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "tools/mac/power/power_sampler/json_exporter.h"
 
+#include <ostream>
+
 #include "base/files/file.h"
 #include "base/files/file_util.h"
 #include "base/json/json_writer.h"
