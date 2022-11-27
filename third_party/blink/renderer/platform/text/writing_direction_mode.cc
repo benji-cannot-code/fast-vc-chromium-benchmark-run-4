ea207c892ecb8b461efc01596c3d2db740662228
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-27 17:49:18 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=dtapuska@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Idfaca41a61d6adfee56bd7d49f055be118dd12af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057507
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075995}

--- third_party/blink/renderer/platform/text/writing_direction_mode.cc
+++ third_party/blink/renderer/platform/text/writing_direction_mode.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/text/writing_direction_mode.h"
 
+#include <ostream>
+
 namespace blink {
 
 std::ostream& operator<<(std::ostream& ostream,
