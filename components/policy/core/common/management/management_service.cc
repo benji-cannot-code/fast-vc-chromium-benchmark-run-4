e377cb7a9b574555210e7e7e0f53974e5d1923f5
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 09:58:15 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=rsorokin@google.com
    
    Bug: 1270812, 1372522
    Change-Id: I982e7d7f63c14a22e9798081ca7aab7a7aa7b3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057389
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076116}

--- components/policy/core/common/management/management_service.cc
+++ components/policy/core/common/management/management_service.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/policy/core/common/management/management_service.h"
 
+#include <ostream>
 #include <tuple>
 
 #include "base/barrier_closure.h"
