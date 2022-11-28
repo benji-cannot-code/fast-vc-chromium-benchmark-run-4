4a4c14c469716877c471a96e27911a2557031d3d
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 20:21:46 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=seantopping@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Ic29551d01c9aaefa175ebaf5f0cfd8f86d7a7922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057309
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076381}

--- chromecast/common/cast_resource_delegate.cc
+++ chromecast/common/cast_resource_delegate.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/common/cast_resource_delegate.h"
 
+#include <ostream>
+
 #include "base/files/file_path.h"
 #include "base/notreached.h"
 #include "base/path_service.h"
