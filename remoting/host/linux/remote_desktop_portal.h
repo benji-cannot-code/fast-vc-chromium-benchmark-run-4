8cea451d8c0cebb6985723bff802d0afe78dc5f0
Salman <salmanmalik@chromium.org>
2022-11-28 15:53:21 +0000

    remoting: Embed cursor in stream for it2me env
    Without this remote support stream wouldn't show cursor
    when the cursor is moved in the original stream.
    
    Bug: chromium:1291247
    Change-Id: I6cb52a0c9cafba82f177eaa13a10e97722febb88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047944
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Salman Malik <salmanmalik@chromium.org>
    Auto-Submit: Salman Malik <salmanmalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076220}

--- remoting/host/linux/remote_desktop_portal.h
+++ remoting/host/linux/remote_desktop_portal.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "clipboard_portal.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_options.h"
 #include "third_party/webrtc/modules/desktop_capture/linux/wayland/portal_request_response.h"
 #include "third_party/webrtc/modules/desktop_capture/linux/wayland/scoped_glib.h"
 #include "third_party/webrtc/modules/desktop_capture/linux/wayland/screen_capture_portal_interface.h"
@@ -34,8 +35,9 @@ class RemoteDesktopPortal
  public:
   // |notifier| must outlive |RemoteDesktopPortal| instance and will be called
   // into from the capturer thread.
-  explicit RemoteDesktopPortal(
-      webrtc::ScreenCastPortal::PortalNotifier* screencast_notifier);
+  RemoteDesktopPortal(
+      webrtc::ScreenCastPortal::PortalNotifier* screencast_notifier,
+      bool prefer_cursor_embedded);
   RemoteDesktopPortal(const RemoteDesktopPortal&) = delete;
   RemoteDesktopPortal& operator=(const RemoteDesktopPortal&) = delete;
   ~RemoteDesktopPortal() override;
