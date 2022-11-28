34a3b6752b67a4166d629d47bd155b85cb4fd0e5
Anastasia Helfinstein <anastasi@google.com>
2022-11-28 21:17:46 +0000

    [ChromeVox] Remove unused ContentScriptBridge from options/
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: Ic72dcdddf62b725c999e3a7145123f63bfa3cc5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053353
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076413}

--- chrome/browser/resources/chromeos/accessibility/chromevox/options/options.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/options/options.js
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {constants} from '../../common/constants.js';
 import {BackgroundBridge} from '../common/background_bridge.js';
 import {BrailleTable} from '../common/braille/braille_table.js';
-import {ContentScriptBridge} from '../common/content_script_bridge.js';
 import {Msgs} from '../common/msgs.js';
 import {PanelCommand, PanelCommandType} from '../common/panel_command.js';
 import {PunctuationEchoes, TtsSettings} from '../common/tts_types.js';
@@ -32,7 +31,6 @@ export class OptionsPage {
    * @this {OptionsPage}
    */
   static async init() {
-    ContentScriptBridge.init();
     OptionsPage.populateVoicesSelect();
     BrailleTable.getAll(function(tables) {
       /** @type {!Array<BrailleTable.Table>} */
@@ -157,12 +155,6 @@ export class OptionsPage {
       if (event.key === 'speakTextUnderMouse') {
         chrome.accessibilityPrivate.enableMouseEvents(
             event.newValue === String(true));
-      }
-    });
-
-    ContentScriptBridge.addMessageListener(function(message) {
-      if (message['prefs']) {
-        OptionsPage.update();
       }
     });
 
