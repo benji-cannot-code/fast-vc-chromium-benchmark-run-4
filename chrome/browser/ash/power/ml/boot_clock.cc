fb47affed9a5178386593f4d56ba5d19433687b9
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 01:50:49 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=martis@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Iab188917a1fa8f3b44285e0f09ea193f74f39798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057308
    Commit-Queue: Michael Martis <martis@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076039}

--- chrome/browser/ash/power/ml/boot_clock.cc
+++ chrome/browser/ash/power/ml/boot_clock.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/power/ml/boot_clock.h"
 
 #include <time.h>
+#include <ostream>
 
 #include "base/check_op.h"
 #include "base/time/time_override.h"
