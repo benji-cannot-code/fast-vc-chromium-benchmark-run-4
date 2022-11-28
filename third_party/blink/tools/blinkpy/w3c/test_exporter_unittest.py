ad3586b13ea1bd47187395b0c08888cddeb5ca6d
Weizhong Xia <weizhong@google.com>
2022-11-28 19:05:49 +0000

    Reland "Clean up exported PRs after chromium CLs are abandoned"
    This reverts commit 332ffa60287c360198c6b2c7f1f2946867af1159.
    
    Reason for revert: <This is actually a false alarm. When we close upstream PRs, we have checked there is no exportable change in the chromium CL.>
    
    Original change's description:
    > Revert "Clean up exported PRs after chromium CLs are abandoned"
    >
    > This reverts commit f7be15f904f40aff0b39045afbd5354403b49734.
    >
    > Reason for revert: <There is a chance we will always close exported PR instead of merging it when chromium CL is merged. Revert for now.>
    >
    > Original change's description:
    > > Clean up exported PRs after chromium CLs are abandoned
    > >
    > > We now run this as a step in wpt exporter. After a chromium CL
    > > has been abandoned for more than two weeks, the exported PR will
    > > be closed automatically.
    > >
    > > Merged CL with no exportable changes will cause a previously exported
    > > Pr to be closed immediately.
    > >
    > > Bug: 1034348
    > > Change-Id: I835acb1ef98f5eab0fd9776eb99da13533ff1c47
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049884
    > > Auto-Submit: Weizhong Xia <weizhong@google.com>
    > > Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    > > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1075423}
    >
    > Bug: 1034348
    > Change-Id: I1576ab533b6b1507cce02de801a3ce87cb0f3466
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055450
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1075571}
    
    Bug: 1034348
    Change-Id: Ib7e81a68dc3012581e8ca8e77000c58d5e73273f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061867
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076327}

--- third_party/blink/tools/blinkpy/w3c/test_exporter_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/test_exporter_unittest.py
@@ -90,6 +90,7 @@ class TestExporterTest(LoggingTestCase):
             MockChromiumCommit(
                 self.host, position='refs/heads/main@{#3}', change_id='I003', subject='subject 3', body='body 3'),
         ], [])
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -172,6 +173,7 @@ class TestExporterTest(LoggingTestCase):
             MockChromiumCommit(
                 self.host, position='refs/heads/main@{#458479}', change_id='I0147'),
         ], [])
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -257,6 +259,7 @@ class TestExporterTest(LoggingTestCase):
                     self.host, subject='subject', body='body',
                     change_id=None)),
         ]
+        test_exporter.pr_cleaner.run = lambda x, y: None
         test_exporter.main(['--credentials-json', '/tmp/credentials.json'])
 
         self.assertEqual(test_exporter.wpt_github.calls, [
@@ -313,6 +316,7 @@ class TestExporterTest(LoggingTestCase):
                 api=test_exporter.gerrit,
                 chromium_commit=MockChromiumCommit(self.host))
         ]
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -360,6 +364,7 @@ class TestExporterTest(LoggingTestCase):
                 api=test_exporter.gerrit,
                 chromium_commit=MockChromiumCommit(self.host))
         ]
+        test_exporter.pr_cleaner.run = lambda x, y: None
         test_exporter.main(['--credentials-json', '/tmp/credentials.json'])
 
         self.assertEqual(test_exporter.wpt_github.calls, [
@@ -383,6 +388,7 @@ class TestExporterTest(LoggingTestCase):
         test_exporter.get_exportable_commits = lambda: ([
             MockChromiumCommit(self.host, change_id='decafbad'), ], [])
         test_exporter.gerrit = MockGerritAPI()
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -409,6 +415,7 @@ class TestExporterTest(LoggingTestCase):
         test_exporter.get_exportable_commits = lambda: ([
             MockChromiumCommit(self.host, change_id='decafbad'), ], [])
         test_exporter.gerrit = MockGerritAPI()
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -451,6 +458,7 @@ class TestExporterTest(LoggingTestCase):
                 api=test_exporter.gerrit,
                 chromium_commit=MockChromiumCommit(self.host))
         ]
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -468,6 +476,7 @@ class TestExporterTest(LoggingTestCase):
         test_exporter.get_exportable_commits = lambda: ([], [])
         test_exporter.gerrit = MockGerritAPI()
         test_exporter.gerrit.query_exportable_open_cls = raise_gerrit_error
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
@@ -487,6 +496,7 @@ class TestExporterTest(LoggingTestCase):
         test_exporter.get_exportable_commits = lambda: (
             [], ['There was an error with the rutabaga.'])
         test_exporter.gerrit = MockGerritAPI()
+        test_exporter.pr_cleaner.run = lambda x, y: None
         success = test_exporter.main(
             ['--credentials-json', '/tmp/credentials.json'])
 
