e6e0defef2d4846488a6924f5f38d4183ef657c2
Viktor Semeniuk <vsemeniuk@google.com>
2022-11-28 11:40:16 +0000

    [Password Manager] Showing the number of accounts in a group
    This change shows a string next to a group name with number of accounts.
    The string is hidden if there is only 1 account in the group.
    
    Impl: https://screenshot.googleplex.com/7MuL8ruihdjjmQW
    Mock: https://screenshot.googleplex.com/Be29KqZ7oBGRBYK
    
    Bug: 1350947
    Change-Id: I8495b6766914f04339f565742e7b7d49a4066221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030257
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076149}

--- chrome/browser/resources/password_manager/password_manager.ts
+++ chrome/browser/resources/password_manager/password_manager.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './password_manager_app.js';
 
 export {PasswordDetailsSectionElement} from './password_details_section.js';
+export {PasswordListItemElement} from './password_list_item.js';
 export {PasswordManagerAppElement} from './password_manager_app.js';
 export {BlockedSite, BlockedSitesListChangedListener, CredentialsChangedListener, PasswordCheckInteraction, PasswordCheckStatusChangedListener, PasswordManagerImpl, PasswordManagerProxy} from './password_manager_proxy.js';
 export {PasswordsSectionElement} from './passwords_section.js';
