14f7cd206ea3875d423ccd1fae32975b84bdaed8
Camden King <camdenking@google.com>
2022-11-28 20:35:38 +0000

    [dPWA] Adding ResultCallback to UpdateShortcut and UpdateAllShortcuts
    Updating UpdateShortcut and UpdateAllShortcuts to use ResultCallback's.
    
    Bug: b/257070289
    Change-Id: Ia6062ef1f52ee7b8f075a832381d7550d952047d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4014668
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Camden King <camdenking@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076392}

--- chrome/browser/web_applications/os_integration/web_app_shortcut_chromeos.cc
+++ chrome/browser/web_applications/os_integration/web_app_shortcut_chromeos.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/location.h"
 #include "base/task/task_runner.h"
+#include "chrome/browser/web_applications/web_app_constants.h"
 
 namespace web_app {
 
@@ -35,9 +36,11 @@ void DeletePlatformShortcuts(const base::FilePath& web_app_path,
                                                     /*shortcut_deleted=*/true));
 }
 
-void UpdatePlatformShortcuts(const base::FilePath& web_app_path,
-                             const std::u16string& old_app_title,
-                             const ShortcutInfo& shortcut_info) {}
+Result UpdatePlatformShortcuts(const base::FilePath& web_app_path,
+                               const std::u16string& old_app_title,
+                               const ShortcutInfo& shortcut_info) {
+  return Result::kOk;
+}
 
 ShortcutLocations GetAppExistingShortCutLocationImpl(
     const ShortcutInfo& shortcut_info) {
