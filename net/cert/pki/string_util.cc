6e0c103f2f28fc915cf43ee88466ccb68545faca
Bob Beck <bbe@google.com>
2022-11-28 20:11:53 +0000

    Remove base/ranges/algorithm.h dependency from string_util.cc
    Replace with std::equal
    
    Bug: 1322914
    Change-Id: I8f575304f6f1eb183a86446507d5d95d03fe54ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057729
    Commit-Queue: Bob Beck <bbe@google.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076370}

--- net/cert/pki/string_util.cc
+++ net/cert/pki/string_util.cc
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/pki/string_util.h"
 
+#include <algorithm>
 #include <iomanip>
 #include <sstream>
 #include <string>
 
-#include "base/ranges/algorithm.h"
 #include "third_party/boringssl/src/include/openssl/mem.h"
 
 namespace net::string_util {
@@ -24,10 +24,10 @@ bool IsAscii(std::string_view str) {
 }
 
 bool IsEqualNoCase(std::string_view str1, std::string_view str2) {
-  return base::ranges::equal(str1, str2,
-                             [](const unsigned char a, const unsigned char b) {
-                               return OPENSSL_tolower(a) == OPENSSL_tolower(b);
-                             });
+  return std::equal(str1.begin(), str1.end(), str2.begin(), str2.end(),
+                    [](const unsigned char a, const unsigned char b) {
+                      return OPENSSL_tolower(a) == OPENSSL_tolower(b);
+                    });
 }
 
 bool EndsWithNoCase(std::string_view str, std::string_view suffix) {
