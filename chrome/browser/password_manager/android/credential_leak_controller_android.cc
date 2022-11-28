f580d71543228b75258aef31d28fc36948e80aac
Bruno Braga <brunobraga@google.com>
2022-11-28 14:29:36 +0000

    [APC] Remove leak warning entry point from mobile
    Bug: 1386065
    Change-Id: I59cb38032f67b208fb87c14431a0357f3bbfaad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047724
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076193}

--- chrome/browser/password_manager/android/credential_leak_controller_android.cc
+++ chrome/browser/password_manager/android/credential_leak_controller_android.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/android/chrome_jni_headers/PasswordChangeLauncher_jni.h"
 #include "chrome/browser/password_manager/android/password_checkup_launcher_helper.h"
 #include "chrome/browser/ui/android/passwords/credential_leak_dialog_view_android.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
@@ -55,6 +54,7 @@ void CredentialLeakControllerAndroid::OnCancelDialog() {
 
 void CredentialLeakControllerAndroid::OnAcceptDialog() {
   LeakDialogType dialog_type = password_manager::GetLeakDialogType(leak_type_);
+  DCHECK(dialog_type != LeakDialogType::kChangeAutomatically);
   LeakDialogDismissalReason dismissal_reason =
       LeakDialogDismissalReason::kClickedOk;
   switch (dialog_type) {
@@ -66,16 +66,6 @@ void CredentialLeakControllerAndroid::OnAcceptDialog() {
       dismissal_reason = LeakDialogDismissalReason::kClickedCheckPasswords;
       break;
     case LeakDialogType::kChangeAutomatically:
-      dismissal_reason =
-          LeakDialogDismissalReason::kClickedChangePasswordAutomatically;
-      // Register that an automated password change flow was started.
-      // |password_change_success_tracker_| might be null in tests.
-      if (password_change_success_tracker_) {
-        password_change_success_tracker_->OnChangePasswordFlowStarted(
-            origin_, base::UTF16ToUTF8(username_),
-            PasswordChangeSuccessTracker::StartEvent::kAutomatedFlow,
-            PasswordChangeSuccessTracker::EntryPoint::kLeakWarningDialog);
-      }
       break;
   }
 
@@ -91,6 +81,7 @@ void CredentialLeakControllerAndroid::OnAcceptDialog() {
 
   switch (dialog_type) {
     case LeakDialogType::kChange:
+    case LeakDialogType::kChangeAutomatically:
       // No-op.
       break;
     case LeakDialogType::kCheckup:
@@ -98,13 +89,6 @@ void CredentialLeakControllerAndroid::OnAcceptDialog() {
       PasswordCheckupLauncherHelper::LaunchLocalCheckup(
           env, window_android_->GetJavaObject(),
           PasswordCheckReferrerAndroid::kLeakDialog);
-      break;
-    case LeakDialogType::kChangeAutomatically:
-      Java_PasswordChangeLauncher_start(
-          env, window_android_->GetJavaObject(),
-          url::GURLAndroid::FromNativeGURL(env, origin_),
-          base::android::ConvertUTF16ToJavaString(env, username_),
-          /*skip_login=*/true);
       break;
   }
 
