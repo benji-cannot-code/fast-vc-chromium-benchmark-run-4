0998a2245f4dfbed583617a10f02f9c0f626e622
Anders Hartvoll Ruud <andruud@chromium.org>
2022-11-28 14:43:48 +0000

    [scroll-animations] Use shortest serialization of animation-timeline
    Per cssom guidelines, serializations should take the shortest
    form possible without changing the meaning. Since "foo" and foo
    refer to the same timeline name, and foo is a valid <custom-ident>,
    the correct serialization of "foo" is foo.
    
    This matches the behavior of Firefox.
    
    Bug: 1382876
    Change-Id: I41c351da1ba75fc8fb63c31de971d0fcb7ee3fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061351
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076199}

--- third_party/blink/renderer/core/css/properties/computed_style_utils.cc
+++ third_party/blink/renderer/core/css/properties/computed_style_utils.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/cssom/css_unit_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_unparsed_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_unsupported_color.h"
+#include "third_party/blink/renderer/core/css/properties/css_parsing_utils.h"
 #include "third_party/blink/renderer/core/css/style_color.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
@@ -2033,8 +2034,16 @@ CSSValue* ComputedStyleUtils::ValueForAnimationTimeline(
            timeline.GetKeyword() == CSSValueID::kNone);
     return CSSIdentifierValue::Create(timeline.GetKeyword());
   }
-  if (timeline.IsName())
-    return ValueForStyleName(timeline.GetName());
+  if (timeline.IsName()) {
+    const AtomicString& name = timeline.GetName();
+    // Serialize as <string> if the value is not a valid <custom-ident>.
+    if (css_parsing_utils::IsCSSWideKeyword(name) ||
+        EqualIgnoringASCIICase(name, "auto") ||
+        EqualIgnoringASCIICase(name, "none")) {
+      return MakeGarbageCollected<CSSStringValue>(name);
+    }
+    return MakeGarbageCollected<CSSCustomIdentValue>(name);
+  }
   DCHECK(timeline.IsScroll());
   const StyleTimeline::ScrollData& scroll_data = timeline.GetScroll();
   CSSValue* axis = scroll_data.HasDefaultAxis()
