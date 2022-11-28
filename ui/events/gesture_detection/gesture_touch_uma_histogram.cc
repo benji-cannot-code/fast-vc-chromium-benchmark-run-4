e8f6f4b48095a5dc34370bc6c2b5b449049f165b
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 18:06:18 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=flackr@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Ie27220802211218af23244d97e22847c58d8ed3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057350
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076275}

--- ui/events/gesture_detection/gesture_touch_uma_histogram.cc
+++ ui/events/gesture_detection/gesture_touch_uma_histogram.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/gesture_detection/gesture_touch_uma_histogram.h"
 
+#include <ostream>
+
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 
