24451b67fec0555586381c8320d0717b5b31c94a
Jocelyn Tran <jocelyntran@google.com>
2022-11-28 23:30:13 +0000

    Reland "[Read Anything] Define default theme from system theme"
    This is a reland of commit c581bca3d733213bc8d020023fd28d44c497cb5d
    
    The original change was reverted because it triggered uninitialized
    memory usage on MSAN bots. This reland initializes the members of
    ReadAnythingTheme in read_anything_model.h to fix that issue.
    
    Original change's description:
    > [Read Anything] Define default theme from system theme
    >
    > In this CL, we define the default theme for Read Anything from the
    > system theme. If the user is on dark mode, the default theme option for
    > Read Anything will be the Read Anything dark theme. For light mode, the
    > default theme will be the Read Anything light theme.
    >
    > This CL also moves the creation of the Read Anything theme object from
    > the model to the page handler. The model initializes the ColorIds for
    > the themes and those are passed around as needed. When we reach the page
    > handler, we will have access to the color provider (since its a View
    > implementation) to convert the theme ColorIds to SKColors to make the
    > theme object.
    >
    > Bug: 1266555
    > Change-Id: Ia4a51b056a5ac3ead1a8a8395eced8ea047f5f01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3987769
    > Reviewed-by: Abigail Klein <abigailbklein@google.com>
    > Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1072314}
    
    Bug: 1266555
    Change-Id: I157fdb39a72db52b033c19dfe0b287c851c0fd19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034922
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076502}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_model_unittest.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_model_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/accessibility/accessibility_features.h"
 
-using read_anything::mojom::ReadAnythingThemePtr;
 using testing::_;
 using testing::FloatNear;
 
@@ -28,7 +27,12 @@ class MockReadAnythingModelObserver : public ReadAnythingModel::Observer {
               (override));
   MOCK_METHOD(void,
               OnReadAnythingThemeChanged,
-              (ReadAnythingThemePtr new_theme),
+              (std::string & font_name,
+               double font_scale,
+               ui::ColorId foreground_color_id,
+               ui::ColorId background_color_id,
+               read_anything::mojom::Spacing line_spacing,
+               read_anything::mojom::Spacing letter_spacing),
               (override));
 };
 
@@ -64,10 +68,12 @@ TEST_F(ReadAnythingModelTest, AddingModelObserverNotifiesAllObservers) {
   model_->AddObserver(&model_observer_1_);
 
   EXPECT_CALL(model_observer_1_, OnAXTreeDistilled(_, _)).Times(0);
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   EXPECT_CALL(model_observer_2_, OnAXTreeDistilled(_, _)).Times(0);
-  EXPECT_CALL(model_observer_2_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_2_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->AddObserver(&model_observer_2_);
 }
@@ -77,13 +83,16 @@ TEST_F(ReadAnythingModelTest, RemovedModelObserversDoNotReceiveNotifications) {
   model_->AddObserver(&model_observer_2_);
 
   EXPECT_CALL(model_observer_1_, OnAXTreeDistilled(_, _)).Times(0);
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   EXPECT_CALL(model_observer_2_, OnAXTreeDistilled(_, _)).Times(0);
-  EXPECT_CALL(model_observer_2_, OnReadAnythingThemeChanged(_)).Times(0);
+  EXPECT_CALL(model_observer_2_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(0);
 
   EXPECT_CALL(model_observer_3_, OnAXTreeDistilled(_, _)).Times(0);
-  EXPECT_CALL(model_observer_3_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_3_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->RemoveObserver(&model_observer_2_);
   model_->AddObserver(&model_observer_3_);
@@ -92,7 +101,8 @@ TEST_F(ReadAnythingModelTest, RemovedModelObserversDoNotReceiveNotifications) {
 TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedFontIndex) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->SetSelectedFontByIndex(2);
 }
@@ -111,7 +121,8 @@ TEST_F(ReadAnythingModelTest, NotificationsOnSetDistilledAXTree) {
 TEST_F(ReadAnythingModelTest, NotificationsOnDecreasedFontSize) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->DecreaseTextSize();
 
@@ -121,7 +132,8 @@ TEST_F(ReadAnythingModelTest, NotificationsOnDecreasedFontSize) {
 TEST_F(ReadAnythingModelTest, NotificationsOnIncreasedFontSize) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->IncreaseTextSize();
 
@@ -131,7 +143,8 @@ TEST_F(ReadAnythingModelTest, NotificationsOnIncreasedFontSize) {
 TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedColorsIndex) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->SetSelectedColorsByIndex(2);
 }
@@ -139,7 +152,8 @@ TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedColorsIndex) {
 TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedLineSpacingIndex) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->SetSelectedLineSpacingByIndex(2);
 }
@@ -147,7 +161,8 @@ TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedLineSpacingIndex) {
 TEST_F(ReadAnythingModelTest, NotificationsOnSetSelectedLetterSpacingIndex) {
   model_->AddObserver(&model_observer_1_);
 
-  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_)).Times(1);
+  EXPECT_CALL(model_observer_1_, OnReadAnythingThemeChanged(_, _, _, _, _, _))
+      .Times(1);
 
   model_->SetSelectedLetterSpacingByIndex(2);
 }
--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -241,6 +241,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorOverlayScrollbarStrokeHoveredLight) \
   E_CPONLY(kColorProgressBar) \
   E_CPONLY(kColorProgressBarPaused) \
+  E_CPONLY(kColorReadAnythingBackground) \
+  E_CPONLY(kColorReadAnythingBackgroundDark) \
+  E_CPONLY(kColorReadAnythingBackgroundLight) \
+  E_CPONLY(kColorReadAnythingBackgroundYellow) \
+  E_CPONLY(kColorReadAnythingForeground) \
   E_CPONLY(kColorReadAnythingForegroundDark) \
   E_CPONLY(kColorReadAnythingForegroundLight) \
   E_CPONLY(kColorReadAnythingForegroundYellow) \
