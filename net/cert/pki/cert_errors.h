4d1947a6c120ae59b7c4abe698bbd47ae7169b76
Bob Beck <bbe@google.com>
2022-11-28 18:29:38 +0000

    Remove unused base/compiler_specific.h
    Bug: 1322914
    Change-Id: I75d5e5bed55321145346b7b8f47cff91e30a5103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053886
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076305}

--- net/cert/pki/cert_errors.h
+++ net/cert/pki/cert_errors.h
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
-#include "base/compiler_specific.h"
 #include "net/base/net_export.h"
 #include "net/cert/pki/cert_error_id.h"
 #include "net/cert/pki/parsed_certificate.h"
--- net/cert/pki/verify_certificate_chain.h
+++ net/cert/pki/verify_certificate_chain.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
-#include "base/compiler_specific.h"
 #include "net/base/net_export.h"
 #include "net/cert/pki/cert_errors.h"
 #include "net/cert/pki/parsed_certificate.h"
