e61f9199caee787c6633da5232693c0b94d149ad
Maxim Kolosovskiy <kolos@google.com>
2022-11-28 10:04:31 +0000

    [Passwords] Clean up auto-submission feature
    Bug: 1393271
    
    Change-Id: If48458df7afac6cde844e1f6a8018b55a4cb70c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051123
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076118}

--- chrome/browser/touch_to_fill/touch_to_fill_controller.cc
+++ chrome/browser/touch_to_fill/touch_to_fill_controller.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_driver.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
-#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/url_formatter/elide_url.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
@@ -58,16 +57,6 @@ std::vector<UiCredential> SortCredentials(
 // the form's structure (submission_readiness).
 bool ShouldTriggerSubmission(SubmissionReadinessState submission_readiness,
                              bool* ready_for_submission) {
-  bool submission_enabled = base::FeatureList::IsEnabled(
-      password_manager::features::kTouchToFillPasswordSubmission);
-  bool allow_non_conservative_heuristics =
-      submission_enabled &&
-      !base::GetFieldTrialParamByFeatureAsBool(
-          password_manager::features::kTouchToFillPasswordSubmission,
-          password_manager::features::
-              kTouchToFillPasswordSubmissionWithConservativeHeuristics,
-          false);
-
   switch (submission_readiness) {
     case SubmissionReadinessState::kNoInformation:
     case SubmissionReadinessState::kError:
@@ -79,12 +68,9 @@ bool ShouldTriggerSubmission(SubmissionReadinessState submission_readiness,
 
     case SubmissionReadinessState::kEmptyFields:
     case SubmissionReadinessState::kMoreThanTwoFields:
-      *ready_for_submission = true;
-      return allow_non_conservative_heuristics;
-
     case SubmissionReadinessState::kTwoFields:
       *ready_for_submission = true;
-      return submission_enabled;
+      return true;
   }
 }
 
