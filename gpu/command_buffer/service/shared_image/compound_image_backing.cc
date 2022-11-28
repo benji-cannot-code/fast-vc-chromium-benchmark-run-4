ecbb8799d79e56d44fc4aa71cc9bceda578e98e2
Kyle Charbonneau <kylechar@chromium.org>
2022-11-28 18:07:25 +0000

    Revert "Allow shared image backings to update size"
    This reverts commit 51759d769736239a7e7e46743d38e8418b638c63.
    
    Reason for revert: Deadlock in crbug.com/1393539
    
    Original change's description:
    > Allow shared image backings to update size
    >
    > SharedImageBacking estimated_size() was being set on creation. Some
    > backings, eg. CompoundImageBacking or RawDrawImageBacking, may allocate
    > additional memory and change size. Add UpdateEstimatedSize() to change
    > estimated size and propagate that information to the peak GPU memory
    > tracker. Also add tests this works as expected for CompoundImageBacking.
    >
    > Low-Coverage-Reason: Most GPU tests don't count for code coverage.
    >
    > Bug: 1353911
    > Change-Id: I410bb7db7ff63aa279668f937b36f0d4fac11276
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049660
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1075240}
    
    Bug: 1353911, 1393539, 1393613
    Change-Id: Ib395b050fe1ed46c31638d97ce82fc61202f72a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057448
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Auto-Submit: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076277}

--- gpu/command_buffer/service/shared_image/compound_image_backing.cc
+++ gpu/command_buffer/service/shared_image/compound_image_backing.cc
@@ -602,9 +602,6 @@ void CompoundImageBacking::LazyAllocateGpuBacking() {
 
   gpu_backing_->SetNotRefCounted();
   gpu_backing_->SetClearedRect(gfx::Rect(size()));
-
-  // Update peak GPU memory tracking with the new estimated size.
-  UpdateEstimatedSize(EstimatedSizeForMemTracking());
 }
 
 }  // namespace gpu
