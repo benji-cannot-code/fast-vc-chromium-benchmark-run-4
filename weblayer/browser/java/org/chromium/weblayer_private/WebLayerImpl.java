ccbb6b9a9dfbda8a8f0d32386c27cdff67c410ed
Susanne Westphal <swestphal@google.com>
2022-11-28 13:49:55 +0000

    [WebEngine] Remove settings fragment
    The settings fragment requires an activity, which we do not have in WebEngine, so removing it for now.
    
    Tracking bug for possibly readding it is created (1382445).
    
    Bug: 1382445, 1378606
    Change-Id: I0bfe58690e50466bb2883defa86b0753ba41b9a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013348
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076177}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
@@ -88,8 +88,6 @@ import org.chromium.weblayer_private.interfaces.ICrashReporterController;
 import org.chromium.weblayer_private.interfaces.IMediaRouteDialogFragment;
 import org.chromium.weblayer_private.interfaces.IObjectWrapper;
 import org.chromium.weblayer_private.interfaces.IProfile;
-import org.chromium.weblayer_private.interfaces.ISettingsFragment;
-import org.chromium.weblayer_private.interfaces.ISiteSettingsFragment;
 import org.chromium.weblayer_private.interfaces.IWebLayer;
 import org.chromium.weblayer_private.interfaces.IWebLayerClient;
 import org.chromium.weblayer_private.interfaces.ObjectWrapper;
@@ -100,7 +98,6 @@ import org.chromium.weblayer_private.media.MediaSessionManager;
 import org.chromium.weblayer_private.media.MediaStreamManager;
 import org.chromium.weblayer_private.metrics.MetricsServiceClient;
 import org.chromium.weblayer_private.metrics.UmaUtils;
-import org.chromium.weblayer_private.settings.SettingsFragmentImpl;
 
 import java.io.File;
 import java.io.IOException;
@@ -381,20 +378,6 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         Bundle unwrappedArgs = ObjectWrapper.unwrap(fragmentArgs, Bundle.class);
         BrowserFragmentImpl fragment = new BrowserFragmentImpl(mProfileManager, unwrappedArgs);
         return fragment.asIBrowserFragment();
-    }
-
-    @Override
-    public ISettingsFragment createSettingsFragmentImpl(IObjectWrapper fragmentArgs) {
-        StrictModeWorkaround.apply();
-        Bundle unwrappedArgs = ObjectWrapper.unwrap(fragmentArgs, Bundle.class);
-        return new SettingsFragmentImpl(mProfileManager, unwrappedArgs).asISettingsFragment();
-    }
-
-    @Override
-    public ISiteSettingsFragment createSiteSettingsFragmentImpl(IObjectWrapper fragmentArgs) {
-        StrictModeWorkaround.apply();
-        Bundle unwrappedArgs = ObjectWrapper.unwrap(fragmentArgs, Bundle.class);
-        return new SettingsFragmentImpl(mProfileManager, unwrappedArgs).asISiteSettingsFragment();
     }
 
     @Override
