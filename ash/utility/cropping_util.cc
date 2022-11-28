786b9e1db6c0669616f52ccd7e2747508ad944e7
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 18:01:40 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=xdai@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I421480d3d5cd7549b1b711b1533a3a63846aa7ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057207
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076268}

--- ash/utility/cropping_util.cc
+++ ash/utility/cropping_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/utility/cropping_util.h"
 
+#include <ostream>
+
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/notreached.h"
