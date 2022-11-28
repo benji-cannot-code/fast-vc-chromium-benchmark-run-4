633e78a8a11788f8186850d092617383f590f20a
Bob Beck <bbe@google.com>
2022-11-28 18:20:37 +0000

    Remove unused base/strings/string_number_conversions.h
    Bug: 1322914
    Change-Id: I1d657a1168c7ff0fdd6429f892d263438af64848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053867
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076291}

--- net/cert/pki/signature_algorithm_unittest.cc
+++ net/cert/pki/signature_algorithm_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/files/file_util.h"
-#include "base/strings/string_number_conversions.h"
 #include "net/cert/pem.h"
 #include "net/der/input.h"
 #include "net/der/parser.h"
