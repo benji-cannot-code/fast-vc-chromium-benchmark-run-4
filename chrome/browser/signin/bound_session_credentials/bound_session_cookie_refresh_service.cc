5b0f4a95dcff82a014de7905841ce16bf8c94e69
Monica Basta <msalama@chromium.org>
2022-11-28 12:49:34 +0000

    [BSC] Add 'BoundSessionCookieRefreshService'
    This service will be responsible for the following:
    - Receive requests to refresh the SIDTS cookie
    - Request a signature from the token binding service
    - Create a fetcher to do the network refresh request and provide it with
      the signature
    - Runs a callback when the cookie is set in the cookie Jar
    - Monitor cookie changes and update the renderers.
    
    This CL only adds the factory and the service. The implementation will
    be added in future CLs.
    
    Bug: 1392135
    Change-Id: I0ee6b49c917aa970cbe21c36540c095678eb8582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055564
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076159}

---
+++ chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service.h"
+
+BoundSessionCookieRefreshService::BoundSessionCookieRefreshService() = default;
+
+BoundSessionCookieRefreshService::~BoundSessionCookieRefreshService() = default;
