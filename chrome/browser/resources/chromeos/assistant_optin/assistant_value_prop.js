193a049e7c936150e013737c85dd85d687c770d5
Brahim Chikhaoui <bchikhaoui@google.com>
2022-11-28 14:16:54 +0000

    OOBE - Polymer3 - Check-in assistant_value_prop
    Check-in the modularised version of OOBE's
    assistant_value_prop
    
    Bug: b:243383410
    Change-Id: Idd3248c1fda6dbb83e190c3a2d7cc8762e894773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055520
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076186}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
@@ -12,7 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Event 'loaded' will be fired when the page has been successfully loaded.
  */
 
-/* #js_imports_placeholder */
+import '//resources/cr_elements/cr_dialog/cr_dialog.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '../components/dialogs/oobe_adaptive_dialog.js';
+import '../components/buttons/oobe_next_button.js';
+import '../components/buttons/oobe_text_button.js';
+import '../components/common_styles/oobe_dialog_host_styles.m.js';
+import './assistant_common_styles.m.js';
+import './assistant_icon.m.js';
+import './setting_zippy.js';
+
+import {afterNextRender, html, mixinBehaviors, Polymer, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {OobeDialogHostBehavior} from '../components/behaviors/oobe_dialog_host_behavior.m.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../components/behaviors/oobe_i18n_behavior.m.js';
+
+import {BrowserProxyImpl} from './browser_proxy.m.js';
+import {AssistantNativeIconType, HtmlSanitizer, webviewStripLinksContentScript} from './utils.m.js';
+
 
 /**
  * Name of the screen.
@@ -24,8 +41,8 @@ const VALUE_PROP_SCREEN_ID = 'ValuePropScreen';
  * @constructor
  * @extends {PolymerElement}
  */
-const AssistantValuePropBase = Polymer.mixinBehaviors(
-    [OobeI18nBehavior, OobeDialogHostBehavior], Polymer.Element);
+const AssistantValuePropBase =
+    mixinBehaviors([OobeI18nBehavior, OobeDialogHostBehavior], PolymerElement);
 
 /**
  * @polymer
@@ -35,7 +52,9 @@ class AssistantValueProp extends AssistantValuePropBase {
     return `assistant-value-prop`;
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
@@ -166,8 +185,8 @@ class AssistantValueProp extends AssistantValuePropBase {
      */
     this.sanitizer_ = new HtmlSanitizer();
 
-    /** @private {?assistant.BrowserProxy} */
-    this.browserProxy_ = assistant.BrowserProxyImpl.getInstance();
+    /** @private {?BrowserProxy} */
+    this.browserProxy_ = BrowserProxyImpl.getInstance();
   }
 
   setUrlTemplateForTesting(url) {
@@ -488,8 +507,7 @@ class AssistantValueProp extends AssistantValuePropBase {
     this.$['overlay-close-button'].addEventListener(
         'click', () => this.hideOverlay());
 
-    Polymer.RenderStatus.afterNextRender(
-        this, () => this.$['next-button'].focus());
+    afterNextRender(this, () => this.$['next-button'].focus());
 
     if (!this.initialized_) {
       this.valuePropView_ = this.$['value-prop-view'];
