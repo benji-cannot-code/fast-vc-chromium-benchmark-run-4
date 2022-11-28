61c610861fc65a8b3350cdfd6d259de36a9d02c8
Nicolas MacBeth <nicolasmacbeth@google.com>
2022-11-28 19:31:52 +0000

    [iOS][BlueDotDefaultBrowserPromo] Feature and feature-flag
    Added the feature and feature-flag for the blue dot default browser
    promo with enum param support.
    
    Bug: 1381635
    Change-Id: Ifa0d786a9eb93fa4e10b09eed7a2773fee788e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4003627
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076342}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -5,6 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 
+BASE_FEATURE(kDefaultBrowserBlueDotPromo,
+             "DefaultBrowserBlueDotPromo",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+constexpr base::FeatureParam<BlueDotPromoUserGroup>::Option
+    kBlueDotPromoUserGroupOptions[] = {
+        {BlueDotPromoUserGroup::kAllDBPromosDisabled, "all-db-promos-disabled"},
+        {BlueDotPromoUserGroup::kOnlyBlueDotPromoEnabled,
+         "only-blue-dot-promo-enabled"}};
+
+constexpr base::FeatureParam<BlueDotPromoUserGroup> kBlueDotPromoUserGroupParam{
+    &kDefaultBrowserBlueDotPromo, "user-group",
+    BlueDotPromoUserGroup::kOnlyBlueDotPromoEnabled,
+    &kBlueDotPromoUserGroupOptions};
+
 BASE_FEATURE(kExpandedTabStrip,
              "ExpandedTabStrip",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- ios/chrome/browser/ui/ui_feature_flags.h
+++ ios/chrome/browser/ui/ui_feature_flags.h
@@ -8,6 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Availability.h"
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+// Feature flag to enable default browser blue dot promo.
+BASE_DECLARE_FEATURE(kDefaultBrowserBlueDotPromo);
+
+// Enum for blue dot promo user groups (control/experiment) and its param. The
+// reason why we need a custom control group is to disable other independent
+// default browser promos, which are already shipped.
+enum class BlueDotPromoUserGroup {
+  kAllDBPromosDisabled,
+  kOnlyBlueDotPromoEnabled,
+};
+extern const base::FeatureParam<BlueDotPromoUserGroup>
+    kBlueDotPromoUserGroupParam;
 
 // Feature to open tab switcher after sliding down the toolbar.
 BASE_DECLARE_FEATURE(kExpandedTabStrip);
