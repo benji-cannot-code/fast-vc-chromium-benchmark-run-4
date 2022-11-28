d2b38e999396e33480a7495e910bd08d7571c32b
Bob Beck <bbe@google.com>
2022-11-28 19:08:53 +0000

    Remove unused base/logging.h dependency from path_builder.cc
    Bug: 1322914
    Change-Id: Ia3ccfa7d74031239e0b6b81f1aa801aec00296bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057548
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076329}

--- net/cert/pki/path_builder.cc
+++ net/cert/pki/path_builder.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <unordered_set>
 
-#include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "net/base/net_errors.h"
