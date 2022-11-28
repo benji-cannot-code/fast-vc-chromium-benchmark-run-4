283f8129a18d344add62a1559831784841fceb92
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 11:27:24 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=carlscab@google.com
    
    Bug: 1270812, 1372522
    Change-Id: Ibca94df34922ea1abda5fc684bf2a6d40867e22a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054370
    Reviewed-by: Carlos Caballero Grolimund <carlscab@google.com>
    Commit-Queue: Carlos Caballero Grolimund <carlscab@google.com>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076143}

--- base/task/common/checked_lock_impl.cc
+++ base/task/common/checked_lock_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/common/checked_lock_impl.h"
 
+#include <ostream>
 #include <unordered_map>
 #include <vector>
 
