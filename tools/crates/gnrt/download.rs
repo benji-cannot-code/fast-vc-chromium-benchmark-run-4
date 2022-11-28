d421323cdd111d73355a6fcc6c4ff7d5fbff3451
danakj <danakj@chromium.org>
2022-11-28 20:13:49 +0000

    Add git sha1 to README.chromium for downloaded crates
    R=collinbaker@chromium.org
    
    Bug: 1306516
    Change-Id: Iae33ae2d1346ec556a157f7b61a58e48329fe159
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg,linux-rust-x64-dbg,linux-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054601
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076373}

--- tools/crates/gnrt/download.rs
+++ tools/crates/gnrt/download.rs
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::fs;
 use std::io::{Read, Write};
 use std::process::{self, ExitCode};
 
@@ -85,7 +86,26 @@ pub fn download(
         .find(|(allowed_license, _)| &cargo.package.license == *allowed_license)
         .expect("License in downloaded Cargo.toml is not in ALLOWED_LICENSES");
 
-    let readme = gen_readme_chromium_text(&cargo, readme_license, security);
+    let vcs_path = crate_path.join(".cargo_vcs_info.json");
+    let vcs_contents = match fs::read_to_string(&vcs_path) {
+        Ok(s) => serde_json::from_str(&s).unwrap(),
+        Err(_) => None,
+    };
+    let githash: Option<&str> = vcs_contents.as_ref().and_then(|v| {
+        use serde_json::Value::*;
+        match v {
+            Object(map) => match map.get("git") {
+                Some(Object(map)) => match map.get("sha1") {
+                    Some(String(s)) => Some(&s[..]),
+                    _ => None,
+                },
+                _ => None,
+            },
+            _ => None,
+        }
+    });
+
+    let readme = gen_readme_chromium_text(&cargo, readme_license, githash, security);
     std::fs::write(build_path.join("README.chromium"), readme)
         .expect("Failed to write README.chromium");
 
@@ -95,12 +115,15 @@ pub fn download(
 }
 
 /// Generate the contents of the README.chromium file.
-fn gen_readme_chromium_text(manifest: &CargoManifest, license: &str, security: bool) -> String {
-    let security = if security {
-        "yes"
-    } else {
-        "no"
-    };
+fn gen_readme_chromium_text(
+    manifest: &CargoManifest,
+    license: &str,
+    githash: Option<&str>,
+    security: bool,
+) -> String {
+    let security = if security { "yes" } else { "no" };
+
+    let revision = githash.map_or_else(String::new, |s| format!("Revision: {s}\n"));
 
     format!(
         "Name: {crate_name}\n\
@@ -108,7 +131,8 @@ fn gen_readme_chromium_text(manifest: &CargoManifest, license: &str, security: b
          Description: {description}\n\
          Version: {version}\n\
          Security Critical: {security}\n\
-         License: {license}\n",
+         License: {license}\n\
+         {revision}",
         crate_name = manifest.package.name,
         url = format!("{}/{}", CRATES_IO_VIEW_URL, manifest.package.name),
         description = manifest.package.description.as_ref().unwrap_or(&"".to_string()),
