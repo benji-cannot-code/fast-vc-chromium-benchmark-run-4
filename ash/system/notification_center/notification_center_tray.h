807dffc6abf9ee6f0de6dc223f5534580e352f91
Ahmed Mehfooz <amehfooz@chromium.org>
2022-11-28 21:39:13 +0000

    Remove widget observer from notification center tray
    The TrayBubbleWrapper already takes care of updating it's owner
    (TrayBackgroundView) when it's widget is destroyed. Updating the
    NotificationCenterTray to take advantage of this.
    
    Bug: b/259734217
    
    Change-Id: Id5071e912f799e1505500951082186a13f7ec94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037916
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076420}

--- ash/system/notification_center/notification_center_tray.h
+++ ash/system/notification_center/notification_center_tray.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/message_center_observer.h"
 #include "ui/message_center/message_center_types.h"
-#include "ui/views/widget/widget_observer.h"
 
 namespace views {
 class Widget;
@@ -33,8 +32,7 @@ class TrayBubbleView;
 // opens a bubble with a scrollable list of all current notifications.
 class ASH_EXPORT NotificationCenterTray
     : public TrayBackgroundView,
-      public message_center::MessageCenterObserver,
-      public views::WidgetObserver {
+      public message_center::MessageCenterObserver {
  public:
   METADATA_HEADER(NotificationCenterTray);
 
@@ -70,9 +68,6 @@ class ASH_EXPORT NotificationCenterTray
   void OnNotificationRemoved(const std::string& notification_id,
                              bool by_user) override;
   void OnNotificationUpdated(const std::string& notification_id) override;
-
-  // views::WidgetObserver:
-  void OnWidgetDestroying(views::Widget* widget) override;
 
   // Update the visibility of the tray button based on available notifications.
   // If there are no notifications the tray button should be hidden and shown
