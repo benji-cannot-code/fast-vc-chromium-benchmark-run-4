7cac72db6f1e3477382b82ce6b2d66daaf3a3898
Julie Jeongeun Kim <jkim@igalia.com>
2022-11-28 11:34:15 +0000

    [mparch] Move FTN access to navigator.cc on creating WebBundleHandleTracker
    This CL is a part of the effort to split RFH and FTN.
    It introduces MaybeCreateWebBundleHandleTracker helper in
    navigator.cc and moves FTN access to find the RFHI's parent or opener
    from RFHI::MaybeCreateWebBundleHandleTracker() to that helper.
    
    It removes WebBundleHandleTracker parameter from
    Navigator::OnBeginNavigation() and uses
    MaybeCreateWebBundleHandleTracker() to create the tracker.
    
    Bug: 1179502
    Change-Id: Ie21a093fbcb77e6557a3afc17ebdad3595181542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054766
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1076145}

--- content/browser/renderer_host/navigator.h
+++ content/browser/renderer_host/navigator.h
@@ -45,7 +45,6 @@ class NavigationRequest;
 class NavigatorDelegate;
 class PrefetchedSignedExchangeCache;
 class RenderFrameHostImpl;
-class WebBundleHandleTracker;
 struct LoadCommittedDetails;
 struct UrlInfo;
 
@@ -182,7 +181,6 @@ class CONTENT_EXPORT Navigator {
       mojo::PendingAssociatedRemote<mojom::NavigationClient> navigation_client,
       scoped_refptr<PrefetchedSignedExchangeCache>
           prefetched_signed_exchange_cache,
-      std::unique_ptr<WebBundleHandleTracker> web_bundle_handle_tracker,
       mojo::PendingReceiver<mojom::NavigationRendererCancellationListener>
           renderer_cancellation_listener);
 
