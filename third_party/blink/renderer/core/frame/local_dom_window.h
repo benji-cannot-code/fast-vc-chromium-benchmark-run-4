c7845f47d594734d0422bc0c17362f47e264a909
François Beaufort <beaufort.francois@gmail.com>
2022-11-28 09:36:26 +0000

    PiP 2.0: Opening a PiP window from a PiP window is not allowed
    This CL makes sure documentPictureInPicture.requestWindow() rejects
    from a PiP window. Note that it wasn't possible already to open a
    PiP window from a PiP window as a similar check already existed in the
    browser side. The problem was that the promise returned by
    documentPictureInPicture.requestWindow() was not rejected when it was
    the case.
    
    Spec: https://github.com/WICG/document-picture-in-picture/issues/13
    Test: https://doc-pip.glitch.me
    Bug: 1386162
    Change-Id: I108b3b6edba8c595bfe0855141c034d5f9688dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4056896
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076109}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -505,6 +505,9 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   NavigationApi* navigation();
 
+  // Is this a Document Picture in Picture window?
+  bool IsPictureInPictureWindow() const;
+
  protected:
   // EventTarget overrides.
   void AddedEventListener(const AtomicString& event_type,
@@ -529,8 +532,6 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   void DispatchLoadEvent();
 
-  // Is this a Document Picture in Picture window?
-  bool IsPictureInPictureWindow() const;
   void SetIsPictureInPictureWindow();
 
   // Return the viewport size including scrollbars.
