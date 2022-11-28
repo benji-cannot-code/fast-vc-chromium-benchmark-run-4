0f4778f61b138cf04bb78df0590a48d9b74e93f7
Russ Hamilton <behamilton@google.com>
2022-11-28 19:29:05 +0000

    K-anonymity clean-up.
    - Remove unused interest group name and update URL k-anonymity fields.
    - Remove bidding_wasm_helper URL from Ad k-anonymity keys.
    - Move interest group name to the end of the Ad Reporting k-anonymity
      key.
    
    Bug: 1234419
    Change-Id: I004cae5f5e806b268b62a5e527d52ed7fe55b9de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044582
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076340}

--- content/browser/interest_group/interest_group_k_anonymity_manager.cc
+++ content/browser/interest_group/interest_group_k_anonymity_manager.cc
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-std::string KAnonKeyFor(const url::Origin& owner, const std::string& name) {
-  return owner.GetURL().spec() + '\n' + name;
-}
-
 std::string KAnonKeyForAdBid(const blink::InterestGroup& group,
                              const blink::InterestGroup::Ad& ad) {
   DCHECK(group.ads);
@@ -24,7 +20,6 @@ std::string KAnonKeyForAdBid(const blink::InterestGroup& group,
   DCHECK(group.bidding_url);
   return group.owner.GetURL().spec() + '\n' +
          group.bidding_url.value_or(GURL()).spec() + '\n' +
-         group.bidding_wasm_helper_url.value_or(GURL()).spec() + '\n' +
          ad.render_url.spec();
 }
 
@@ -33,10 +28,9 @@ std::string KAnonKeyForAdNameReporting(const blink::InterestGroup& group,
   DCHECK(group.ads);
   DCHECK(base::Contains(*group.ads, ad));
   DCHECK(group.bidding_url);
-  return group.owner.GetURL().spec() + '\n' + group.name + '\n' +
+  return group.owner.GetURL().spec() + '\n' +
          group.bidding_url.value_or(GURL()).spec() + '\n' +
-         group.bidding_wasm_helper_url.value_or(GURL()).spec() + '\n' +
-         ad.render_url.spec();
+         ad.render_url.spec() + '\n' + group.name;
 }
 
 InterestGroupKAnonymityManager::InterestGroupKAnonymityManager(
@@ -56,21 +50,6 @@ void InterestGroupKAnonymityManager::QueryKAnonymityForInterestGroup(
   std::vector<std::string> ids_to_query;
   base::Time check_time = base::Time::Now();
   base::TimeDelta min_wait = k_anonymity_service_->GetQueryInterval();
-
-  if (!storage_group.name_kanon ||
-      storage_group.name_kanon->last_updated < check_time - min_wait) {
-    ids_to_query.push_back(KAnonKeyFor(storage_group.interest_group.owner,
-                                       storage_group.interest_group.name));
-  }
-
-  if (storage_group.interest_group.daily_update_url) {
-    if (!storage_group.daily_update_url_kanon ||
-        storage_group.daily_update_url_kanon->last_updated <
-            check_time - min_wait) {
-      ids_to_query.push_back(
-          storage_group.interest_group.daily_update_url->spec());
-    }
-  }
 
   for (const auto& ad : storage_group.bidding_ads_kanon) {
     if (ad.last_updated < check_time - min_wait) {
@@ -111,13 +90,6 @@ void InterestGroupKAnonymityManager::QuerySetsCallback(
                                                  update_time};
     interest_group_manager_->UpdateKAnonymity(data);
   }
-}
-
-void InterestGroupKAnonymityManager::RegisterInterestGroupAsJoined(
-    const blink::InterestGroup& group) {
-  RegisterIDAsJoined(KAnonKeyFor(group.owner, group.name));
-  if (group.daily_update_url)
-    RegisterIDAsJoined(group.daily_update_url->spec());
 }
 
 void InterestGroupKAnonymityManager::RegisterAdAsWon(
