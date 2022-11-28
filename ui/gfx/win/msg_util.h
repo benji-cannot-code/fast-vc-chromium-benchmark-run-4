46a57145902e1a1bda7b7c8087b68743bf7dae22
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 22:59:36 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I6b3433028dfc131fbe1243322e8f2e2c7aaca7b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057510
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076484}

--- ui/gfx/win/msg_util.h
+++ ui/gfx/win/msg_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_WIN_MSG_UTIL_H_
 #define UI_GFX_WIN_MSG_UTIL_H_
 
+#include <ostream>
+
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
 #include "ui/gfx/geometry/point.h"
