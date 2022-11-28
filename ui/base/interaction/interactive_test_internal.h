9d2519427c2098837887114b170a2723985a1854
Dana Fried <dfried@chromium.org>
2022-11-28 08:26:31 +0000

    [Test Automation] Kombucha: massive improvements to context-switching
    Now multiple steps can be executed in one of the following modes:
     - InSameContext: Carries over the context from the previous step.
     - InContext: Uses the specified context instead of the default context
       for the test sequence.
    
    This is especially useful for tests involving multiple browser windows
    or tests involving e.g. tab-modal windows, tests for WebUI IPH, etc.
    
    This CL also modifies how multi-step actions like ClickMouse,
    EnsureNotPresent, and FlushEvents so that they can carry over an
    existing context for use in InSameContext. For example, the following
    works:
    
      InAnyContext(WaitForShow(kElementId)),
      InSameContext(Steps(
        MoveMouseTo(kElementId),
    	ClickMouse(),
    	WithElement(kElementId(...)))) // retains previous context
    
    Bug: 1375862
    Change-Id: I48755424c81574d124947195add8dfc0b92fe4c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4052903
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076092}

--- ui/base/interaction/interactive_test_internal.h
+++ ui/base/interaction/interactive_test_internal.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_list.h"
 #include "base/logging.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/task/single_thread_task_runner.h"
@@ -44,7 +45,9 @@ class InteractiveTestPrivate {
   void operator=(const InteractiveTestPrivate&) = delete;
 
   InteractionTestUtil& test_util() { return *test_util_; }
-  TrackedElement* pivot_element() { return pivot_element_.get(); }
+
+  // Gets the pivot element for the specified context, which must exist.
+  TrackedElement* GetPivotElement(ElementContext context) const;
 
   // Call this method during test SetUp(), or SetUpOnMainThread() for browser
   // tests.
@@ -80,15 +83,29 @@ class InteractiveTestPrivate {
  private:
   friend class ui::test::InteractiveTestApi;
 
+  // Prepare for a sequence to start.
+  void Init(ElementContext initial_context);
+
+  // Clean up after a sequence.
+  void Cleanup();
+
+  // Note when a new element appears; we may update the context list.
+  void OnElementAdded(TrackedElement* el);
+
+  // Maybe adds a pivot element for the given context.
+  void MaybeAddPivotElement(ElementContext context);
+
   // Tracks whether a sequence succeeded or failed.
   bool success_ = false;
 
   // Used to simulate input to UI elements.
   std::unique_ptr<InteractionTestUtil> test_util_;
 
-  // Always present during a test sequence; used to relay events to trigger
-  // follow-up steps.
-  std::unique_ptr<TrackedElement> pivot_element_;
+  // Used to keep track of valid contexts.
+  base::CallbackListSubscription context_subscription_;
+
+  // Used to relay events to trigger follow-up steps.
+  std::map<ElementContext, std::unique_ptr<TrackedElement>> pivot_elements_;
 
   // Overrides the default test failure behavior to test the API itself.
   InteractionSequence::AbortedCallback aborted_callback_for_testing_;
@@ -145,6 +162,7 @@ InteractiveTestPrivate::MultiStep InteractiveTestPrivate::PostTask(T&& task) {
   result.emplace_back(std::move(
       InteractionSequence::StepBuilder()
           .SetElementID(kInteractiveTestPivotElementId)
+          .SetContext(InteractionSequence::ContextMode::kFromPreviousStep)
           .SetType(InteractionSequence::StepType::kCustomEvent,
                    kInteractiveTestPivotEventType)
           .SetStartCallback(
