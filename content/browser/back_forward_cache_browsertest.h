61d09dcbbfaf40751a033d33fd1c6fecc5588c5a
Fergal Daly <fergal@chromium.org>
2022-11-28 06:32:28 +0000

    Switch same_origin_details from pointer to optional.
    This is a no-op tidy. It means we can inline the data of a tree
    node instead of creating extra variables.
    
    Change-Id: I5d186e0292cb908a3f90f2889f1d0ec589998862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4055214
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Yuzu Saijo <yuzus@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076072}

--- content/browser/back_forward_cache_browsertest.h
+++ content/browser/back_forward_cache_browsertest.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_mock_cert_verifier.h"
 #include "content/test/content_browser_test_utils_internal.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/mojom/back_forward_cache_not_restored_reasons.mojom-blink.h"
 
 namespace content {
@@ -133,7 +134,7 @@ class BackForwardCacheBrowserTest
 
   ReasonsMatcher MatchesNotRestoredReasons(
       const testing::Matcher<blink::mojom::BFCacheBlocked>& blocked,
-      const SameOriginMatcher* same_origin_details);
+      const absl::optional<SameOriginMatcher>& same_origin_details);
   SameOriginMatcher MatchesSameOriginDetails(
       const testing::Matcher<std::string>& id,
       const testing::Matcher<std::string>& name,
