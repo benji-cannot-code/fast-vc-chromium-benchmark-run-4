0fdcf66973554700f6a67e1b31d30d3e2c62e61c
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-28 17:11:06 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=henrika@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I1e5cf1c5f55e4039379cb3b491b2a998434d0db9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057447
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076254}

--- media/audio/audio_device_thread.cc
+++ media/audio/audio_device_thread.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_device_thread.h"
 
 #include <limits>
+#include <ostream>
 
 #include "base/check_op.h"
 #include "base/system/sys_info.h"
