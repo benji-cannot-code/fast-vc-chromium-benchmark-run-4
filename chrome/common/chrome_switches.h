392bccd5e911b388ef56873cea68b4ac7b8d659b
Yann Dago <ydago@chromium.org>
2022-11-28 16:57:41 +0000

    Remove flag auth-server-whitelist
    Bug: 1330537
    Change-Id: Id6cd080b81317bf9e214f77f78d09ae38df63db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053388
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076247}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -53,7 +53,6 @@ extern const char kAppsGalleryDownloadURL[];
 extern const char kAppsGalleryUpdateURL[];
 extern const char kAppsGalleryURL[];
 extern const char kAuthServerAllowlist[];
-extern const char kAuthServerAllowlistDeprecated[];
 extern const char kAutoOpenDevToolsForTabs[];
 extern const char kAutoSelectDesktopCaptureSource[];
 extern const char kAutoSelectTabCaptureSourceByTitle[];
