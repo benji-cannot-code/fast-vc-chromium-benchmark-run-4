e740d3a312acc1691e8a9969d7dd6ac74aa99359
Roman Arora <romanarora@chromium.org>
2022-11-29 03:44:55 +0000

    [NTP] Comprehensive theming hover colors
    Update control hover color applied to the module buttons,  drive list
    items, and customize chrome button to match when an autogenerated theme
    is applied.
    
    Note that the Customize chrome button retains the default blue hover
    colors when the default color is applied.
    
    Screenshots:
    White background:
    https://drive.google.com/file/d/1iXjKwQ5NIiSVibN4zSS1NnoN8EEBeORw/view
    
    Light background:
    https://drive.google.com/file/d/1Iq-7KPi86P-p3TdQvOZE8NyIL_QvB6__/view
    
    Dark background:
    https://drive.google.com/file/d/1EEzEOpWmvf2Xs-Ud669BLAYqNPdDrip5/view
    
    Bug: 1393195
    Change-Id: I813ec24603ecf66a6102762ef64e1eba95c84be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062288
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076633}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -143,6 +143,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageBackgroundOverride) \
   E_CPONLY(kColorNewTabPageBorder) \
   E_CPONLY(kColorNewTabPageButtonBackground) \
+  E_CPONLY(kColorNewTabPageButtonBackgroundHovered) \
   E_CPONLY(kColorNewTabPageButtonForeground) \
   E_CPONLY(kColorNewTabPageCartModuleDiscountChipBackground) \
   E_CPONLY(kColorNewTabPageCartModuleDiscountChipForeground) \
