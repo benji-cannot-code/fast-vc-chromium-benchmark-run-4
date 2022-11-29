ee7f8735eb4beb1cfbef89fbc51f237369732e4f
Dibyajyoti Pal <dibyapal@chromium.org>
2022-11-29 00:39:34 +0000

    [dPWA] Add flags for OS Integration Sub Manager
    As per the design discussion, this sets up 2 flags for the OS
    integration sub manager design, one for writing DB states and one for
    enabling Execute(). This ensures fine grained handling of the new system
    in case things go wrong after release, and can be used by resulting
    implementations of the sub managers.
    
    Bug: 1295044, b/254491451
    Change-Id: I4759ea8ef1f99769b7c515fd102d1425af4ef489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054082
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076543}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -862,6 +862,9 @@ extern const char kContextualPageActionsPriceTrackingDescription[];
 extern const char kContextualPageActionsReaderModeName[];
 extern const char kContextualPageActionsReaderModeDescription[];
 
+extern const char kEnableOsIntegrationSubManagersName[];
+extern const char kEnableOsIntegrationSubManagersDescription[];
+
 extern const char kGpuRasterizationName[];
 extern const char kGpuRasterizationDescription[];
 
