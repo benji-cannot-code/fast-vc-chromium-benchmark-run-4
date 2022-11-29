f849a20bd68d2211a2acdfe5bf815ab22f5a170c
Nan Lin <linnan@chromium.org>
2022-11-29 23:31:41 +0000

    Send debug reports when report window has passed
    A debug report will be sent if debug cookie is set.
    
    Bug: 1379078
    Change-Id: Iebbee791233be3c70e8dbca5ed9357de1ca3122f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030686
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077146}

--- content/browser/attribution_reporting/attribution_debug_report.cc
+++ content/browser/attribution_reporting/attribution_debug_report.cc
@@ -62,7 +62,6 @@ absl::optional<DebugDataType> GetReportDataType(EventLevelResult result,
     case EventLevelResult::kSuccess:
     case EventLevelResult::kProhibitedByBrowserPolicy:
     case EventLevelResult::kSuccessDroppedLowerPriority:
-    case EventLevelResult::kReportWindowPassed:
       return absl::nullopt;
     case EventLevelResult::kInternalError:
       return DataTypeIfCookieSet(DebugDataType::kTriggerUnknownError,
@@ -100,6 +99,9 @@ absl::optional<DebugDataType> GetReportDataType(EventLevelResult result,
     case EventLevelResult::kExcessiveReports:
       return DataTypeIfCookieSet(DebugDataType::kTriggerEventExcessiveReports,
                                  is_debug_cookie_set);
+    case EventLevelResult::kReportWindowPassed:
+      return DataTypeIfCookieSet(DebugDataType::kTriggerEventReportWindowPassed,
+                                 is_debug_cookie_set);
   }
 }
 
@@ -109,7 +111,6 @@ absl::optional<DebugDataType> GetReportDataType(AggregatableResult result,
     case AggregatableResult::kSuccess:
     case AggregatableResult::kNotRegistered:
     case AggregatableResult::kProhibitedByBrowserPolicy:
-    case AggregatableResult::kReportWindowPassed:
       return absl::nullopt;
     case AggregatableResult::kInternalError:
       return DataTypeIfCookieSet(DebugDataType::kTriggerUnknownError,
@@ -139,6 +140,10 @@ absl::optional<DebugDataType> GetReportDataType(AggregatableResult result,
     case AggregatableResult::kInsufficientBudget:
       return DataTypeIfCookieSet(
           DebugDataType::kTriggerAggregateInsufficientBudget,
+          is_debug_cookie_set);
+    case AggregatableResult::kReportWindowPassed:
+      return DataTypeIfCookieSet(
+          DebugDataType::kTriggerAggregateReportWindowPassed,
           is_debug_cookie_set);
   }
 }
@@ -173,6 +178,8 @@ std::string SerializeReportDataType(DebugDataType data_type) {
       return "trigger-event-excessive-reports";
     case DebugDataType::kTriggerEventStorageLimit:
       return "trigger-event-storage-limit";
+    case DebugDataType::kTriggerEventReportWindowPassed:
+      return "trigger-event-report-window-passed";
     case DebugDataType::kTriggerAggregateDeduplicated:
       return "trigger-aggregate-deduplicated";
     case DebugDataType::kTriggerAggregateNoContributions:
@@ -181,6 +188,8 @@ std::string SerializeReportDataType(DebugDataType data_type) {
       return "trigger-aggregate-insufficient-budget";
     case DebugDataType::kTriggerAggregateStorageLimit:
       return "trigger-aggregate-storage-limit";
+    case DebugDataType::kTriggerAggregateReportWindowPassed:
+      return "trigger-aggregate-report-window-passed";
     case DebugDataType::kTriggerUnknownError:
       return "trigger-unknown-error";
   }
@@ -240,10 +249,12 @@ base::Value::Dict GetReportDataBody(
     case DebugDataType::kTriggerEventLowPriority:
     case DebugDataType::kTriggerEventExcessiveReports:
     case DebugDataType::kTriggerEventStorageLimit:
+    case DebugDataType::kTriggerEventReportWindowPassed:
     case DebugDataType::kTriggerAggregateDeduplicated:
     case DebugDataType::kTriggerAggregateNoContributions:
     case DebugDataType::kTriggerAggregateInsufficientBudget:
     case DebugDataType::kTriggerAggregateStorageLimit:
+    case DebugDataType::kTriggerAggregateReportWindowPassed:
     case DebugDataType::kTriggerUnknownError:
       NOTREACHED();
       return base::Value::Dict();
@@ -270,8 +281,10 @@ base::Value::Dict GetReportDataBody(DebugDataType data_type,
     case DebugDataType::kTriggerEventDeduplicated:
     case DebugDataType::kTriggerEventNoMatchingConfigurations:
     case DebugDataType::kTriggerEventNoise:
+    case DebugDataType::kTriggerEventReportWindowPassed:
     case DebugDataType::kTriggerAggregateDeduplicated:
     case DebugDataType::kTriggerAggregateNoContributions:
+    case DebugDataType::kTriggerAggregateReportWindowPassed:
     case DebugDataType::kTriggerUnknownError:
       break;
     case DebugDataType::kTriggerAttributionsPerSourceDestinationLimit:
