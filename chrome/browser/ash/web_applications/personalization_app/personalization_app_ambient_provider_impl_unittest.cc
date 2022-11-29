18719b66421c3f0965e967295841bd60124a51b7
Giovanni Ortuno Urquidi <ortuno@chromium.org>
2022-11-29 06:32:07 +0000

    Revert "Reland "[ambient] Notify the JS side when screen saver is closed""
    This reverts commit bed8604a142870a542fe4643dc51bf48b5d21b51.
    
    Reason for revert: Causing Linux Chromium OS ASan LSan Tests (1)
    to fail.
    
    Original change's description:
    > Reland "[ambient] Notify the JS side when screen saver is closed"
    >
    > This is a reland of commit 90408e4348b4ea4df133b7f521073b24d337f560
    >
    > Original change's description:
    > > [ambient] Notify the JS side when screen saver is closed
    > >
    > > When the screensaver is closed for any reason, we notify the JS side. As a result the UI is updated.
    > >
    > > Bug: b/259716397
    > > Change-Id: I2a78a0cb7be8dbf7e19cc741295ce29e3aebbcfb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4021018
    > > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > > Commit-Queue: Ilkin Safarli <safarli@google.com>
    > > Reviewed-by: Yuki Awano <yawano@google.com>
    > > Reviewed-by: Tao Wu <wutao@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1074824}
    >
    > Bug: b/259716397
    > Change-Id: Iddae022254bf494eab65e7c633eca231b02d57cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053420
    > Reviewed-by: Yuki Awano <yawano@google.com>
    > Commit-Queue: Ilkin Safarli <safarli@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1076375}
    
    Bug: b/259716397
    Change-Id: Ifdeed629d6c9aea6bb6dbb72468fc6dd787fea89
    Fixed: 1394315
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060301
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076686}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_ambient_provider_impl_unittest.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_ambient_provider_impl_unittest.cc
@@ -7,14 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <vector>
-#include "ash/ambient/ambient_controller.h"
+
 #include "ash/ambient/test/ambient_ash_test_helper.h"
 #include "ash/constants/ambient_animation_theme.h"
 #include "ash/public/cpp/ambient/ambient_prefs.h"
 #include "ash/public/cpp/ambient/common/ambient_settings.h"
 #include "ash/public/cpp/ambient/fake_ambient_backend_controller_impl.h"
-#include "ash/shell.h"
-#include "ash/test/ash_test_base.h"
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom.h"
 #include "base/callback_helpers.h"
 #include "base/ranges/algorithm.h"
@@ -71,8 +69,6 @@ class TestAmbientObserver
     previews_ = std::move(previews);
   }
 
-  void OnScreenSaverClosed() override { screen_saver_closed_ = true; }
-
   mojo::PendingRemote<ash::personalization_app::mojom::AmbientObserver>
   pending_remote() {
     if (ambient_observer_receiver_.is_bound()) {
@@ -117,8 +113,6 @@ class TestAmbientObserver
       ambient_observer_receiver_{this};
 
   bool ambient_mode_enabled_ = false;
-  bool screen_saver_closed_ = false;
-
   ash::AmbientAnimationTheme animation_theme_ =
       ash::AmbientAnimationTheme::kSlideshow;
   ash::AmbientModeTopicSource topic_source_ =
@@ -131,13 +125,10 @@ class TestAmbientObserver
 
 }  // namespace
 
-class PersonalizationAppAmbientProviderImplTest : public ash::AshTestBase {
+class PersonalizationAppAmbientProviderImplTest : public testing::Test {
  public:
   PersonalizationAppAmbientProviderImplTest()
-      : ash::AshTestBase(std::unique_ptr<base::test::TaskEnvironment>(
-            std::make_unique<content::BrowserTaskEnvironment>(
-                base::test::TaskEnvironment::TimeSource::MOCK_TIME))),
-        profile_manager_(TestingBrowserProcess::GetGlobal()) {}
+      : profile_manager_(TestingBrowserProcess::GetGlobal()) {}
   PersonalizationAppAmbientProviderImplTest(
       const PersonalizationAppAmbientProviderImplTest&) = delete;
   PersonalizationAppAmbientProviderImplTest& operator=(
@@ -147,8 +138,6 @@ class PersonalizationAppAmbientProviderImplTest : public ash::AshTestBase {
  protected:
   // testing::Test:
   void SetUp() override {
-    ash::AshTestBase::SetUp();
-
     ASSERT_TRUE(profile_manager_.SetUp());
     profile_ = profile_manager_.CreateTestingProfile(kFakeTestEmail);
 
@@ -163,16 +152,11 @@ class PersonalizationAppAmbientProviderImplTest : public ash::AshTestBase {
         ambient_provider_remote_.BindNewPipeAndPassReceiver());
 
     SetEnabledPref(true);
-    GetAmbientAshTestHelper()->ambient_client().SetAutomaticalyIssueToken(true);
-
-    Shell::Get()->ambient_controller()->set_backend_controller_for_testing(
-        nullptr);
-
     fake_backend_controller_ =
         std::make_unique<ash::FakeAmbientBackendControllerImpl>();
+    ambient_ash_test_helper_ = std::make_unique<ash::AmbientAshTestHelper>();
+    ambient_ash_test_helper_->ambient_client().SetAutomaticalyIssueToken(true);
   }
-
-  void TearDown() override { ash::AshTestBase::TearDown(); }
 
   TestingProfile* profile() { return profile_; }
 
@@ -305,7 +289,7 @@ class PersonalizationAppAmbientProviderImplTest : public ash::AshTestBase {
   }
 
   void FastForwardBy(base::TimeDelta time) {
-    task_environment()->FastForwardBy(time);
+    task_environment_.FastForwardBy(time);
   }
 
   bool IsFetchSettingsPendingAtBackend() const {
@@ -328,6 +312,8 @@ class PersonalizationAppAmbientProviderImplTest : public ash::AshTestBase {
   }
 
  private:
+  content::BrowserTaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   TestingProfileManager profile_manager_;
   content::TestWebUI web_ui_;
   std::unique_ptr<content::WebContents> web_contents_;
