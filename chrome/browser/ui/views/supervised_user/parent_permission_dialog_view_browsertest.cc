e9593723da79475e85ecb67afc8cf95b1d9e3192
Michael Ershov <miersh@google.com>
2022-11-29 11:12:55 +0000

    Prepare tests for Chrome side owner key generation
    In the upcoming change Chrome will start establishing ownership on
    ChromeOS, but some tests are not prepared to emulate that properly.
    They would need to make sure that all the relevant fake/mock/stub
    classes generate public owner key, install attributes, local state,
    device policies and other files. Tests with PRE_ steps would need to
    ensure that all the relevant files are persisted on disk in between
    tests.
    
    The current implementation of the tests in this CL is not emulating
    the ownership logic realistically enough and it doesn't seem to
    be essential for the logic under test.
    Instead skip to the state when the consumer ownership is already
    established to ensure that the tests continue working with the
    upcoming ownership changes (without requiring addition setups).
    
    
    Bug: b:244407123
    Test: ParentPermissionDialogViewTest*
    Change-Id: I93afb8956658a906b42891e6ae743344b160841f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061452
    Auto-Submit: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076770}

--- chrome/browser/ui/views/supervised_user/parent_permission_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/supervised_user/parent_permission_dialog_view_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
+#include "chrome/browser/ash/login/test/device_state_mixin.h"
 #include "chrome/browser/ash/login/test/fake_gaia_mixin.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
@@ -260,6 +261,13 @@ class ParentPermissionDialogViewTest
 
   ParentPermissionDialog::Result result_;
 
+  // Emulate consumer ownership (create public owner key file, install
+  // attributes file, etc) so Chrome doesn't need to do it. The current setup is
+  // not sufficient to go through the ownership flow successfully and it's not
+  // essential to the logic under test.
+  ash::DeviceStateMixin device_state_{
+      &mixin_host_,
+      ash::DeviceStateMixin::State::OOBE_COMPLETED_CONSUMER_OWNED};
   ash::LoggedInUserMixin logged_in_user_mixin_{
       &mixin_host_,
       // Simulate Gellerization / Adding Supervision to load extensions.
