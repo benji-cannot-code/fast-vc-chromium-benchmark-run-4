f4da23e5ba0c835bb6dc8db3fe1a6d4973d8a12f
Anders Hartvoll Ruud <andruud@chromium.org>
2022-11-29 10:16:57 +0000

    [scroll-animations] Make scroll-timeline-name tree-scoped
    This is in preparation of tree-scoped timeline lookups.
    
    There is no behavior change.
    
    Bug: 1382876
    Change-Id: Ifbce9b28b2572b18e4fd435eed50b91b0a08123b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061671
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076753}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -195,7 +195,7 @@ bool HasAnimationsOrTransitions(const StyleResolverState& state) {
 }
 
 bool HasTimelines(const StyleResolverState& state) {
-  if (!state.StyleBuilder().ScrollTimelineName().empty())
+  if (state.StyleBuilder().ScrollTimelineName())
     return true;
   if (state.StyleBuilder().ViewTimelineName())
     return true;
