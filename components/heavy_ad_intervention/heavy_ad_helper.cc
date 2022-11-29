afef5e503acb60cd3e9f09b3e10f23c1018c8151
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-29 01:28:50 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=csharrison@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Ie4d2d2b5a1ba024c526eaea45fc356b7ba4abae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054374
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076576}

--- components/heavy_ad_intervention/heavy_ad_helper.cc
+++ components/heavy_ad_intervention/heavy_ad_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/heavy_ad_intervention/heavy_ad_helper.h"
 
+#include <ostream>
+
 #include "base/check.h"
 #include "base/strings/string_piece.h"
 #include "components/grit/components_resources.h"
