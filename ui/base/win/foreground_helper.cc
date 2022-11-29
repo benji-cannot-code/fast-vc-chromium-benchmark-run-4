2501229970b59000c6549e14237253b48bbd27db
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-11-29 00:43:28 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=robliao@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I445706358c1c2ea08ee57a35323dcda7717577f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057396
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076546}

--- ui/base/win/foreground_helper.cc
+++ ui/base/win/foreground_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/win/foreground_helper.h"
 
+#include <ostream>
+
 #include "base/logging.h"
 #include "ui/gfx/win/window_impl.h"
 
