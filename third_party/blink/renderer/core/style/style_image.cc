d3c1ca3aa41fe5706856a2db27eb0e7f27d65c3e
Fredrik Söderquist <fs@opera.com>
2022-11-29 16:49:08 +0000

    Improve HasIntrinsicSize() for StyleFetchedImage{,Set}
    For SVG images, call GetIntrinsicSizingInfo() and then check the fields
    of the returned structure. StyleImage should probably provide a way to
    return the structure, but this will be a later step.
    
    This allows BackgroundImageGeometry (et al) to compute a more correct
    size in some cases.
    
    Move SVGImage pAR 'none' sizing quirk to
    SVGImage::GetIntrinsicSizingInfo. This way we preserve the quirk even
    when not using SVGImage::ConcreteObjectSize.
    
    In the tests:
    
     svg/wicd/test-scalable-background-image1.xhtml
     svg/zoom/page/zoom-svg-as-background-with-relative-size.html
    
    the referenced image does not have intrinsic dimensions, so the snapped
    positioning area will be used when resolving the image size.
    
    Fixed: 935057
    Change-Id: I03680ea2f2694500f09901bba613db275d0e2841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037347
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076899}

--- third_party/blink/renderer/core/style/style_image.cc
+++ third_party/blink/renderer/core/style/style_image.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/style/style_image.h"
 
+#include "third_party/blink/renderer/core/layout/intrinsic_sizing_info.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image.h"
 
 namespace blink {
@@ -33,6 +34,14 @@ gfx::SizeF StyleImage::ImageSizeForSVGImage(
       gfx::ScaleSize(default_object_size, 1 / multiplier);
   return ApplyZoom(svg_image.ConcreteObjectSize(unzoomed_default_object_size),
                    multiplier);
+}
+
+bool StyleImage::HasIntrinsicDimensionsForSVGImage(const SVGImage& svg_image) {
+  IntrinsicSizingInfo intrinsic_sizing_info;
+  if (!svg_image.GetIntrinsicSizingInfo(intrinsic_sizing_info))
+    return false;
+  return intrinsic_sizing_info.has_width || intrinsic_sizing_info.has_height ||
+         !intrinsic_sizing_info.aspect_ratio.IsEmpty();
 }
 
 }  // namespace blink
