f68017376fa66710d3932cc47034cef32ddc200d
Ankush Singh <ankushkush@google.com>
2022-11-29 13:32:51 +0000

    [sync] Set lower synced bookmarks limit for mobile platforms
    Setting a lower limit on the number of synced bookmarks for mobile will
    allow for a smoother experience on mobile devices.
    1. There are not many users of bookmarks on mobiles.
    2. The gain in user experience outweighs any added downsides.
    
    Bug: 1347466
    Change-Id: Ibfbd411bf459b82ee791b5ce87e741f88a70a6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061354
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076813}

--- components/sync_bookmarks/bookmark_model_type_processor.cc
+++ components/sync_bookmarks/bookmark_model_type_processor.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "base/trace_event/trace_event.h"
+#include "build/build_config.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_node.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
@@ -48,6 +49,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sync_bookmarks {
 
 namespace {
+
+#if BUILDFLAG(IS_IOS) or BUILDFLAG(IS_ANDROID)
+// Set a lower limit for mobile platforms.
+// 1. There are not many users of bookmarks on mobiles.
+// 2. Prevents creation of an overly huge sync metadata file to be stored on
+// the disk.
+// 3. Reduced memory consumption and processing, noticeable especially during
+// an initial merge.
+// 4. A lower limit for mobile platforms reflects the lower
+// capacity/processing power of mobile devices.
+//
+// Since the bookmark model thread is the UI thread, a smoother user
+// experience outweighs the resulting downsides.
+constexpr size_t kDefaultMaxBookmarksTillSyncEnabled = 20000;
+#else
+constexpr size_t kDefaultMaxBookmarksTillSyncEnabled = 100000;
+#endif
 
 class ScopedRemoteUpdateBookmarks {
  public:
@@ -124,7 +142,8 @@ size_t CountSyncableBookmarksFromModel(bookmarks::BookmarkModel* model) {
 
 BookmarkModelTypeProcessor::BookmarkModelTypeProcessor(
     BookmarkUndoService* bookmark_undo_service)
-    : bookmark_undo_service_(bookmark_undo_service) {}
+    : bookmark_undo_service_(bookmark_undo_service),
+      max_bookmarks_till_sync_enabled_(kDefaultMaxBookmarksTillSyncEnabled) {}
 
 BookmarkModelTypeProcessor::~BookmarkModelTypeProcessor() {
   if (bookmark_model_ && bookmark_model_observer_) {
