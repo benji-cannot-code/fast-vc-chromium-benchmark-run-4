e650bc66ceae7f96ac15a0c1bec0933c8235af16
Jonathan Lee <jonathanjlee@google.com>
2022-11-29 20:00:37 +0000

    [blinkpy] Handle null `HTTPResponse` correctly in retry
    A `RequestException` may set `response` to `None`, so checking that the
    attribute exists was not sufficient to retrieve and log HTTP debug
    information.
    
    Correct the timeout test case to reflect the fact that
    `requests.exceptions.Timeout` is a `RequestException` but not an
    `HTTPError` because no status code is received. This exercises this
    bug's path.
    
    See Also:
      https://requests.readthedocs.io/en/latest/_modules/requests/exceptions/#RequestException
    
    Fixed: 1394451
    Change-Id: I77a94ee4a2dce26bb7112840ce0aebfe827b684b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063354
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077028}

--- third_party/blink/tools/blinkpy/common/net/network_transaction_unittest.py
+++ third_party/blink/tools/blinkpy/common/net/network_transaction_unittest.py
@@ -27,13 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from unittest import mock
+
 from blinkpy.common.net.network_transaction import NetworkTransaction, NetworkTimeout
 from blinkpy.common.system.log_testing import LoggingTestCase
 
-from requests.exceptions import HTTPError
 from requests import Response
+from requests.exceptions import HTTPError, Timeout
 
 
+@mock.patch('time.sleep', new=mock.Mock())
 class NetworkTransactionTest(LoggingTestCase):
     exception = Exception('Test exception')
 
@@ -78,6 +81,9 @@ class NetworkTransactionTest(LoggingTestCase):
         response.url = 'http://foo.com/'
         raise HTTPError(response=response)
 
+    def _raise_timeout(self):
+        raise Timeout()
+
     def test_retry(self):
         transaction = NetworkTransaction(initial_backoff_seconds=0)
         self.assertEqual(transaction.run(self._raise_500_error), 42)
@@ -96,12 +102,5 @@ class NetworkTransactionTest(LoggingTestCase):
     def test_timeout(self):
         transaction = NetworkTransaction(
             initial_backoff_seconds=60 * 60, timeout_seconds=60)
-        did_process_exception = False
-        did_throw_exception = True
-        try:
-            transaction.run(self._raise_500_error)
-            did_throw_exception = False
-        except NetworkTimeout:
-            did_process_exception = True
-        self.assertTrue(did_throw_exception)
-        self.assertTrue(did_process_exception)
+        with self.assertRaises(NetworkTimeout):
+            transaction.run(self._raise_timeout)
