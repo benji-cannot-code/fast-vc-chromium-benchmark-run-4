10a7fbf468d6c86c9007a547c16a83be359946d3
Sean Maher <spvw@chromium.org>
2022-11-29 06:01:55 +0000

    Emit wakeup.flow events which point directly to "WorkerThread Active"
    In addition, add an API that lets WaitableEvents opt out of wakeup.flow
    so that we don't duplicate these events. This mechanism is very similar
    to declare_only_used_while_idle() logic.
    
    Bug: 1377480
    Change-Id: Ic6d2b151315ae40486e12867d314a536898dfe21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048144
    Auto-Submit: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076676}

--- base/synchronization/waitable_event.cc
+++ base/synchronization/waitable_event.cc
@@ -14,8 +14,10 @@ namespace base {
 void WaitableEvent::Signal() {
   // Must be ordered before SignalImpl() to guarantee it's emitted before the
   // matching TerminatingFlow in TimedWait().
-  TRACE_EVENT_INSTANT("wakeup.flow", "WaitableEvent::Signal",
-                      perfetto::Flow::FromPointer(this));
+  if (emit_wakeup_flow_) {
+    TRACE_EVENT_INSTANT("wakeup.flow", "WaitableEvent::Signal",
+                        perfetto::Flow::FromPointer(this));
+  }
   SignalImpl();
 }
 
@@ -41,7 +43,7 @@ bool WaitableEvent::TimedWait(TimeDelta wait_delta) {
 
   const bool result = TimedWaitImpl(wait_delta);
 
-  if (result) {
+  if (result && emit_wakeup_flow_) {
     TRACE_EVENT_INSTANT("wakeup.flow", "WaitableEvent::Wait Complete",
                         perfetto::TerminatingFlow::FromPointer(this));
   }
