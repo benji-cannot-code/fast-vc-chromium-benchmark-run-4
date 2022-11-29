955cdacbfc356aa1760b4313e1babf7f8e07d22e
Christian Flach <cmfcmf@chromium.org>
2022-11-29 11:58:45 +0000

    web-bundles: Accept GURL in WebBundleBuilder methods
    Accepting GURL reduces boilerplate code. `base::StringPiece` is still
    accepted as well, and can be useful when trying to create Web Bundles
    with relative (or invalid) URLs.
    
    Bug: None
    Change-Id: I08659d9355a1f292873754a4e496f29c730dd296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020673
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076781}

--- content/browser/web_package/web_bundle_browsertest_base.cc
+++ content/browser/web_package/web_bundle_browsertest_base.cc
@@ -292,8 +292,8 @@ FrameTreeNode* GetFirstChild(WebContents* web_contents) {
 
 std::string CreateSimpleWebBundle(const GURL& primary_url) {
   web_package::WebBundleBuilder builder;
-  builder.AddPrimaryURL(primary_url.spec());
-  builder.AddExchange(primary_url.spec(),
+  builder.AddPrimaryURL(primary_url);
+  builder.AddExchange(primary_url,
                       {{":status", "200"}, {"content-type", "text/html"}},
                       "<title>Ready</title>");
   std::vector<uint8_t> bundle = builder.CreateBundle();
@@ -304,7 +304,7 @@ void AddHtmlFile(web_package::WebBundleBuilder* builder,
                  const GURL& base_url,
                  const std::string& path,
                  const std::string& content) {
-  builder->AddExchange(base_url.Resolve(path).spec(),
+  builder->AddExchange(base_url.Resolve(path),
                        {{":status", "200"}, {"content-type", "text/html"}},
                        content);
 }
@@ -314,7 +314,7 @@ void AddScriptFile(web_package::WebBundleBuilder* builder,
                    const std::string& path,
                    const std::string& content) {
   builder->AddExchange(
-      base_url.Resolve(path).spec(),
+      base_url.Resolve(path),
       {{":status", "200"}, {"content-type", "application/javascript"}},
       content);
 }
@@ -322,7 +322,7 @@ void AddScriptFile(web_package::WebBundleBuilder* builder,
 std::string CreatePathTestWebBundle(const GURL& base_url) {
   const std::string primary_url_path = "/web_bundle/path_test/in_scope/";
   web_package::WebBundleBuilder builder;
-  builder.AddPrimaryURL(base_url.Resolve(primary_url_path).spec());
+  builder.AddPrimaryURL(base_url.Resolve(primary_url_path));
   AddHtmlFile(&builder, base_url, primary_url_path, "<title>Ready</title>");
   AddHtmlFile(
       &builder, base_url, "/web_bundle/path_test/in_scope/page.html",
@@ -401,7 +401,7 @@ void SetUpSubPageTest(net::EmbeddedTestServer* primary_server,
   *third_party_origin = third_party_server->GetURL("/");
 
   web_package::WebBundleBuilder builder;
-  builder.AddPrimaryURL(primary_url_origin->Resolve("/top").spec());
+  builder.AddPrimaryURL(primary_url_origin->Resolve("/top"));
   AddHtmlFile(&builder, *primary_url_origin, "/top", R"(
     <script>
     window.addEventListener('message',
@@ -614,7 +614,7 @@ void SetUpSharedNavigationsTest(net::EmbeddedTestServer* server,
                                 std::string* web_bundle_content) {
   SetUpNavigationTestServer(server, url_origin);
   web_package::WebBundleBuilder builder;
-  builder.AddPrimaryURL(url_origin->Resolve("/top-page/").spec());
+  builder.AddPrimaryURL(url_origin->Resolve("/top-page/"));
   for (const auto& path : pathes)
     AddHtmlAndScriptForNavigationTest(&builder, *url_origin, path, "");
 
@@ -982,7 +982,7 @@ void SetUpIframeNavigationTest(net::EmbeddedTestServer* server,
                                std::string* web_bundle_content) {
   SetUpNavigationTestServer(server, url_origin);
   web_package::WebBundleBuilder builder;
-  builder.AddPrimaryURL(url_origin->Resolve("/top-page/").spec());
+  builder.AddPrimaryURL(url_origin->Resolve("/top-page/"));
   const std::vector<std::string> pathes = {"/top-page/", "/1-page/",
                                            "/2-page/"};
   for (const auto& path : pathes)
