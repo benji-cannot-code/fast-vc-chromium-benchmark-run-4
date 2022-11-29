3b8034592aab15892accf7094b51fe373b017625
Brando <brandosocarras@google.com>
2022-11-29 23:29:22 +0000

    [Fast Pair] Retry RFCOMM connection 5 times with cooldowns
    Add 5 retries for connecting to RFCOMM with an  exponential cooldown
    of 2, 4, 8, 16, and 32s.
    
    Test= unit tests in this CL and manual testing with DUT.
        Manual testing was done by adding code to force 4 connection
        failures and a log of each attempt. Retroactive pairing
        was performed and was successful. All 4 logs were observed.
    
    Bug: b/259603442
    Change-Id: Ia9ad4f3561eb8460a3d9a74f8198c0724b764462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037917
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077143}

--- ash/quick_pair/message_stream/message_stream_lookup_impl.h
+++ ash/quick_pair/message_stream/message_stream_lookup_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "device/bluetooth/bluetooth_adapter.h"
 
 namespace device {
@@ -96,6 +97,12 @@ class MessageStreamLookupImpl : public MessageStreamLookup,
   // Internal method called by BluetoothAdapterFactory to provide the adapter
   // object.
   void OnGetAdapter(scoped_refptr<device::BluetoothAdapter> adapter);
+
+  // Maps devices addresses to message stream attempt counts and retry timers,
+  // respectively.
+  base::flat_map<std::string, int> create_message_stream_attempts_;
+  base::flat_map<std::string, std::unique_ptr<base::OneShotTimer>>
+      create_message_stream_retry_timers_;
 
   base::ObserverList<MessageStreamLookup::Observer> observers_;
 
