72babefadb5f6b30e0f5e42602032d2649882a94
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-11-29 02:07:17 +0000

    Import wpt@4572ca03c2eb3516fee3b609b81149882274975b
    Using wpt-import in Chromium 62e4a393b725c75670cfa37589e7e635e8540ab7.
    With Chromium commits locally applied on WPT:
    302c03c06c "[anchor-position] Support logical keywords"
    b7c7d2949f "[anchor-position] Fix an error in `anchor-position-writing-modes-002.html`"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mcasas@chromium.org:
      external/wpt/geolocation-API
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I97c4c11e53a5b9ae58fc6b10bf04efb6c0caee1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060562
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1076592}

--- third_party/blink/web_tests/external/wpt/geolocation-API/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/geolocation-API/idlharness.https.window.js
@@ -7,23 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // https://www.w3.org/TR/geolocation-API/
 
 window.onload = async () => {
-  await test_driver.set_permission({ name: "geolocation" }, "denied");
-  const positionError = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(reject, resolve);
-  });
-
-  await test_driver.set_permission({ name: "geolocation" }, "granted");
-  const position = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-
   idl_test(["geolocation"], ["hr-time", "html"], (idl_array) => {
     idl_array.add_objects({
-      Navigator: ["navigator"],
       Geolocation: ["navigator.geolocation"],
-      GeolocationPositionError: [positionError],
-      GeolocationPosition: [position],
-      GeolocationCoordinates: [position.coords],
     });
   });
 };
