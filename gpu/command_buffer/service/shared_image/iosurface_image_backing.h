e68e8c848fdd456f31245622e4752d2128fd2eb2
Christopher Cameron <ccameron@chromium.org>
2022-11-29 10:15:33 +0000

    IOSurfaceImageBacking: Do not allocate GLImage for anonymous images
    Change IOSurfaceImageBacking to no longer go through the
    GLImageIOSurface to get a the IOSurface and related parameters.
    
    Change IOSurfaceImageBackingFactory::CreateSharedImageInternal to no
    longer allocate a GLImage.
    
    Do not yet update the IOSurfaceImageBackingFactory::CreateSharedImage
    which take a GpuMemoryBufferHandle to do this yet (it has several
    other interactions that need to be cleaned up separately).
    
    Change-Id: Ic840bcb5f9a3b7f213e2a026476683c1566b32a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061651
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076752}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.h
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.h
@@ -156,7 +156,7 @@ class OverlayIOSurfaceRepresentation : public OverlayImageRepresentation {
   OverlayIOSurfaceRepresentation(SharedImageManager* manager,
                                  SharedImageBacking* backing,
                                  MemoryTypeTracker* tracker,
-                                 scoped_refptr<gl::GLImage> gl_image);
+                                 gfx::ScopedIOSurface io_surface);
   ~OverlayIOSurfaceRepresentation() override;
 
  private:
@@ -165,7 +165,7 @@ class OverlayIOSurfaceRepresentation : public OverlayImageRepresentation {
   gfx::ScopedIOSurface GetIOSurface() const override;
   bool IsInUseByWindowServer() const override;
 
-  scoped_refptr<gl::GLImage> gl_image_;
+  gfx::ScopedIOSurface io_surface_;
 };
 
 class MemoryIOSurfaceRepresentation : public MemoryImageRepresentation {
@@ -212,6 +212,10 @@ class GPU_GLES2_EXPORT IOSurfaceImageBacking
       public IOSurfaceBackingEGLState::Client {
  public:
   IOSurfaceImageBacking(
+      gfx::ScopedIOSurface io_surface,
+      uint32_t io_surface_plane,
+      gfx::BufferFormat io_surface_format,
+      gfx::GenericSharedMemoryId io_surface_id,
       scoped_refptr<gl::GLImage> image,
       const Mailbox& mailbox,
       viz::SharedImageFormat format,
@@ -278,6 +282,10 @@ class GPU_GLES2_EXPORT IOSurfaceImageBacking
 
   bool IsPassthrough() const { return true; }
 
+  gfx::ScopedIOSurface io_surface_;
+  const uint32_t io_surface_plane_;
+  const gfx::BufferFormat io_surface_format_;
+  const gfx::GenericSharedMemoryId io_surface_id_;
   scoped_refptr<gl::GLImage> image_;
 
   // Used to determine whether to release the texture in EndAccess() in use
