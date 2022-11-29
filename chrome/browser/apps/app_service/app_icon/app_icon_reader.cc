9a9e0714b819168f4ba309c6f72409e339bb0120
Nancy Wang <nancylingwang@chromium.org>
2022-11-29 05:10:48 +0000

    Modify AppServiceProxy to write the icon data to the local disk.
    This CL modifies AppServiceProxy to call icon_writer_ to write the icon data to the local disk when failed reading the icon data from the local
    disk.
    
    Tests will be added in the follow up CLs.
    
    BUG=1380608
    
    Change-Id: If80120af6fed5bb97f3a3c17c74c0eb1d7945227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4033448
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076655}

--- chrome/browser/apps/app_service/app_icon/app_icon_reader.cc
+++ chrome/browser/apps/app_service/app_icon/app_icon_reader.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/app_icon/app_icon_reader.h"
 
+#include "base/files/file_util.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_decoder.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
--- chrome/browser/apps/app_service/app_icon/app_icon_reader.h
+++ chrome/browser/apps/app_service/app_icon/app_icon_reader.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_util.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
 
