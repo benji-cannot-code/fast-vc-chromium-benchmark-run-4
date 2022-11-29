9caaac11e83ca30db262921dc20629679587b83c
Andrew Grieve <agrieve@chromium.org>
2022-11-29 17:19:07 +0000

    Android: Do not cache ApplicationInfo in BundleUtils
    The caching was added recently to fix the associated bug, but it
    turns out to be unnecessary.
    
    Bug: b/258453540
    Change-Id: I84731a3ee3a45e35836f6808999c05bb88521aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062366
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076910}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngine.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngine.java
@@ -96,7 +96,6 @@ class SplitCompatEngine implements InstallEngine {
 
             if (status == SplitInstallSessionStatus.INSTALLED) {
                 mFacade.updateCrashKeys();
-                mFacade.updateBundleUtils();
             }
 
             for (String moduleName : modules) {
