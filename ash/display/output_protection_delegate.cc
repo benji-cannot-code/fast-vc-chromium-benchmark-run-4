eac1a30e4f14f120fc94cc563ff8dd603b66bc46
Ahmed Fakhry <afakhry@chromium.org>
2022-11-29 17:43:44 +0000

    capture_mode: Handle protected content of inactive tabs
    When a tab is inactive, it becomes a child of the unparented
    container. Recording can then start normally of the browser
    window hosting it, since it's not a descendant of it.
    
    Once that tab gets activated, capture mode should recheck its
    content protection restrictions in case that tab is not allowed
    to be recorded.
    
    Fixed: b:259604614
    Test: Manually, Added a new unit test.
    Change-Id: I104224500081962973b5b098e348feea4198de3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063370
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076918}

--- ash/display/output_protection_delegate.cc
+++ ash/display/output_protection_delegate.cc
@@ -78,12 +78,12 @@ void OutputProtectionDelegate::OnDisplayMetricsChanged(
     return;
   }
 
-  OnWindowMayHaveMovedToAnotherDisplay();
+  OnWindowMayHaveMovedToAnotherDisplayOrWindow();
 }
 
 void OutputProtectionDelegate::OnWindowHierarchyChanged(
     const aura::WindowObserver::HierarchyChangeParams& params) {
-  OnWindowMayHaveMovedToAnotherDisplay();
+  OnWindowMayHaveMovedToAnotherDisplayOrWindow();
 }
 
 void OutputProtectionDelegate::OnWindowDestroying(aura::Window* window) {
@@ -125,11 +125,16 @@ void OutputProtectionDelegate::SetProtection(uint32_t protection_mask,
                                     std::move(callback));
 }
 
-void OutputProtectionDelegate::OnWindowMayHaveMovedToAnotherDisplay() {
+void OutputProtectionDelegate::OnWindowMayHaveMovedToAnotherDisplayOrWindow() {
   DCHECK(window_);
-  int64_t new_display_id =
-      display::Screen::GetScreen()->GetDisplayNearestWindow(window_).id();
 
+  // The window may have moved to a display that is currently being recorded, or
+  // to be hosted by a browser window that is being recorded when a tab becomes
+  // active, so we need to refresh Capture Mode's content protection.
+  CaptureModeController::Get()->RefreshContentProtection();
+
+  const int64_t new_display_id =
+      display::Screen::GetScreen()->GetDisplayNearestWindow(window_).id();
   if (display_id_ == new_display_id)
     return;
 
@@ -140,11 +145,8 @@ void OutputProtectionDelegate::OnWindowMayHaveMovedToAnotherDisplay() {
     manager()->ApplyContentProtection(client_->id, display_id_,
                                       display::CONTENT_PROTECTION_METHOD_NONE,
                                       base::DoNothing());
-
-    // The window may have moved to a display that is currently being recorded,
-    // so we need to refresh Capture Mode's content protection.
-    CaptureModeController::Get()->RefreshContentProtection();
   }
+
   display_id_ = new_display_id;
 }
 
