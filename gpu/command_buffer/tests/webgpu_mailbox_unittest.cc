5b182f9dcd1c0d0d201129d761cb67fe0c2bb235
Austin Eng <enga@chromium.org>
2022-11-29 01:24:37 +0000

    Unsuppress WebGPU unittests on M1
    Fixed: 1271926
    Change-Id: Icdbb1b550971ad619d62d25720669a900907c13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061368
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Auto-Submit: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076569}

--- gpu/command_buffer/tests/webgpu_mailbox_unittest.cc
+++ gpu/command_buffer/tests/webgpu_mailbox_unittest.cc
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/color_space.h"
-
-#if BUILDFLAG(IS_MAC)
-#include "gpu/command_buffer/tests/gl_manager.h"
-#endif  // BUILDFLAG(IS_MAC)
-
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_utils.h"
 #include "ui/gl/init/gl_factory.h"
@@ -133,18 +128,6 @@ class WebGPUMailboxTest
 
  protected:
   void SetUp() override {
-#if BUILDFLAG(IS_MAC)
-    // Crashing on Mac M1. Currently missing stack trace. crbug.com/1271926
-    // This must be checked before WebGPUTest::Initialize otherwise context
-    // switched is locked and we cannot temporarily have this GLContext.
-    GLManager gl_manager;
-    gl_manager.Initialize(GLManager::Options());
-    std::string renderer(gl_manager.context()->GetGLRenderer());
-    if (renderer.find("Apple M1") != std::string::npos)
-      mac_m1_ = true;
-    gl_manager.Destroy();
-#endif
-
     SKIP_TEST_IF(!WebGPUSupported());
     SKIP_TEST_IF(!WebGPUSharedImageSupported());
     WebGPUTest::SetUp();
@@ -247,9 +230,6 @@ class WebGPUMailboxTest
     queue.Submit(1, &commands);
   }
 
-#if BUILDFLAG(IS_MAC)
-  bool mac_m1_ = false;
-#endif
   wgpu::Device device_;
 };
 
@@ -811,11 +791,6 @@ TEST_P(WebGPUMailboxTest, UseA_UseB_DestroyA_DestroyB) {
 // devices tried to create shared images with the same (id, generation) (which
 // is possible because they can be on different Dawn wires) they would conflict.
 TEST_P(WebGPUMailboxTest, AssociateOnTwoDevicesAtTheSameTime) {
-#if BUILDFLAG(IS_MAC)
-  // Crashing on Mac M1. Currently missing stack trace. crbug.com/1271926
-  SKIP_TEST_IF(mac_m1_);
-#endif
-
   // Create a the shared images.
   SharedImageInterface* sii = GetSharedImageInterface();
   Mailbox mailbox_a = sii->CreateSharedImage(
