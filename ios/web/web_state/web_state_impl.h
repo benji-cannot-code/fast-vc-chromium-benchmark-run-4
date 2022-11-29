de37147be8f01b2541b6a25ee8b7a00af45f0f03
ginnnnnnny <ginnyhuang@chromium.org>
2022-11-29 18:15:28 +0000

    [iOS][media-permissions] WebStateImpl & delegate API for custom prompt
    This CL adds all needed ios/web APIs for the permissions overlay tab
    helper to be notified that the media permission is requested and to
    acquire the decision handler needed to process the user's decision to
    grant or deny media permissions.
    
    The new delegate method should return a boolean value which indicates
    whether the delegate supports custom prompting. Currently, the
    WebStateDelegateBrowserAgent is not implemented to support showing a
    custom prompt, so it would return NO in the delegate method so the
    web state would still show the default prompt that asks for permissions.
    
    Bug: 1356768
    Change-Id: I0237dcb1b01efcfe7c8c1daaa7246a0d4feec180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047948
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076943}

--- ios/web/web_state/web_state_impl.h
+++ ios/web/web_state/web_state_impl.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol CRWWebViewProxy;
 @protocol CRWWebViewNavigationProxy;
 @class UIViewController;
+enum WKPermissionDecision : NSInteger;
 
 namespace web {
 
@@ -246,6 +247,13 @@ class WebStateImpl final : public WebState {
 
   // Removes all current web frames.
   void RemoveAllWebFrames();
+
+  // Requests the user's permission to access requested `permissions`.
+  typedef void (^PermissionDecisionHandler)(WKPermissionDecision decision)
+      API_AVAILABLE(ios(15.0));
+  void RequestPermissionsWithDecisionHandler(NSArray<NSNumber*>* permissions,
+                                             PermissionDecisionHandler handler)
+      API_AVAILABLE(ios(15.0));
 
   // WebState:
   WebStateDelegate* GetDelegate() final;
