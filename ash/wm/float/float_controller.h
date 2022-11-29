bb31f889015a0b207104010b9219d08828ae1f28
Sammie Quon <sammiequon@chromium.org>
2022-11-29 23:38:31 +0000

    float: Add metrics to track how long float session lasts
    In most cases, the session is the duration of the `FloatedWindowInfo`
    object. The one exception is when there is a floated window, and we
    change desks; the object persists in this case. We don't want to record
    though, otherwise a floated window on an inactive desk could be there
    indefinitely.
    
    We stop recording a floated window if it is no longer on the active
    desk, and start recording again if we move to a desk with a floated
    window.
    
    Test: ash_unittests WindowFloatMetricsTest.FloatWindowDuration
    Bug: b/258473579
    Change-Id: I9e037e23920d8d7c2cd9453ddba9e3e8da971436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4019123
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077150}

--- ash/wm/float/float_controller.h
+++ ash/wm/float/float_controller.h
@@ -50,10 +50,6 @@ class ASH_EXPORT FloatController : public TabletModeObserver,
     kBottomRight,
   };
 
-  // Public so it can be used by unit tests.
-  constexpr static char kFloatWindowCountsPerSessionHistogramName[] =
-      "Ash.Float.FloatWindowCountsPerSession";
-
   FloatController();
   FloatController(const FloatController&) = delete;
   FloatController& operator=(const FloatController&) = delete;
@@ -144,7 +140,7 @@ class ASH_EXPORT FloatController : public TabletModeObserver,
   friend class DefaultState;
   friend class TabletModeWindowState;
   friend class WindowFloatTest;
-  FRIEND_TEST_ALL_PREFIXES(WindowFloatTest, FloatWindowCountPerSession);
+  FRIEND_TEST_ALL_PREFIXES(WindowFloatMetricsTest, FloatWindowCountPerSession);
 
   // Calls `FloatImpl()` and additionally updates the magnetism if needed.
   void FloatForTablet(aura::Window* window,
