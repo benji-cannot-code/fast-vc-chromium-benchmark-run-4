e228cc223ced5025dc1be26bea997d77af081867
Xiaoling Bao <xiaolingbao@chromium.org>
2022-11-29 20:50:54 +0000

    Add logging module switch when starting updater from wake.
    Bug: 1394469
    Change-Id: Icf8754312d1b4a184de4fb113b682cbc410fb3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063355
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077047}

--- chrome/updater/mac/setup/setup.mm
+++ chrome/updater/mac/setup/setup.mm
@@ -171,7 +171,9 @@ base::ScopedCFTypeRef<CFDictionaryRef> CreateWakeLaunchdPlist(
   [program_arguments addObjectsFromArray:@[
     base::SysUTF8ToNSString(updater_path.value()),
     MakeProgramArgument(kWakeAllSwitch),
-    MakeProgramArgument(kEnableLoggingSwitch)
+    MakeProgramArgument(kEnableLoggingSwitch),
+    MakeProgramArgumentWithValue(kLoggingModuleSwitch,
+                                 kLoggingModuleSwitchValue)
   ]];
   if (scope == UpdaterScope::kSystem)
     [program_arguments addObject:MakeProgramArgument(kSystemSwitch)];
