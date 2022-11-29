d507f59a8bbfefbb567c83cd2c35e9ddb51f8aa9
Tibor Goldschwendt <tiborg@chromium.org>
2022-11-29 18:50:07 +0000

    [cc-side-panel] Use TestBrowserProxy.fromClass for mocking
    This saves code and strenghtens type checks.
    
    Change-Id: Ie041e9fa9cd48f5b6a02e62a4aae62644f9a28bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027039
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076983}

--- chrome/test/data/webui/side_panel/customize_chrome/test_support.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/test_support.ts
@@ -6,33 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 type Constructor<T> = new (...args: any[]) => T;
-
-export function createMock<T extends object>(clazz: Constructor<T>):
-    {mock: T, callTracker: TestBrowserProxy} {
-  const callTracker = new TestBrowserProxy(
-      Object.getOwnPropertyNames(clazz.prototype)
-          .filter(methodName => methodName !== 'constructor'));
-  const handler = {
-    get: function(_target: T, prop: string) {
-      if (clazz.prototype[prop] instanceof Function) {
-        return (...args: any[]) => callTracker.methodCalled(prop, ...args);
-      }
-      if (Object.getOwnPropertyDescriptor(clazz.prototype, prop)!.get) {
-        return callTracker.methodCalled(prop);
-      }
-      return undefined;
-    },
-  };
-  return {mock: new Proxy<T>({} as unknown as T, handler), callTracker};
-}
-
 type Installer<T> = (instance: T) => void;
 
 export function installMock<T extends object>(
-    clazz: Constructor<T>, installer?: Installer<T>): TestBrowserProxy {
+    clazz: Constructor<T>, installer?: Installer<T>): TestBrowserProxy<T> {
   installer = installer ||
       (clazz as unknown as {setInstance: Installer<T>}).setInstance;
-  const {mock, callTracker} = createMock(clazz);
+  const mock = TestBrowserProxy.fromClass(clazz);
   installer!(mock);
-  return callTracker;
+  return mock;
 }
