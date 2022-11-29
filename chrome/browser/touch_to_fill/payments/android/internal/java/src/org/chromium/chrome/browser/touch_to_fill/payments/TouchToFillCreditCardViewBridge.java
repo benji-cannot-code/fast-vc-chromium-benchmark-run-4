b14322532b60fe197d883f7925166fd747b29190
Ivana Žužić <izuzic@google.com>
2022-11-29 16:02:29 +0000

    Reland "[TTFCC] Show credit cards in the bottom sheet"
    This is a reland of commit 116ed391211a3dba4580a5726d0205c79617cee5
    
    In the original CL I've removed the dependency on chrome_java.
    It seemed unnecessary, but this error showed up in the CI:
    Dependency Checks Failed
    Target: //chrome/browser/touch_to_fill/payments/android/internal:javatests
    Direct classpath is incomplete. To fix, add deps on:
     * //chrome/android:chrome_java
         * org.chromium.chrome.browser.ChromeTabbedActivity (needed by org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardViewTest)
         * org.chromium.chrome.browser.ui.RootUiCoordinator (needed by org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardViewTest)
    
    I've reproduced it locally for the configuration of the bot that failed and fixed it by adding a dependency to chrome_java to the suggested target. Since the target is only a test this fix is acceptable.
    
    Original change's description:
    > [TTFCC] Show credit cards in the bottom sheet
    >
    > From the bridge, the cards are passed further to the showSheet
    > method of the TouchToFillCreditCardCoordinator which implements
    > TouchToFillCreditCardComponent.
    >
    > The TouchToFillCreditCardCoordinator calls the showSheet method of the
    > TouchToFillCreditCardMediator, which stores the credit cards as one of
    > the TouchToFillCreditCardProperties.
    >
    > Changes to bind the views are made to TouchToFillCreditCardView and TouchToFillCreditCardViewBinder.
    >
    > The name of the card and the expiration date are already shown and the
    > favicon will be added in a followup.
    >
    > HSV link to inspect the layout:
    > https://hsv.googleplex.com/4905358175764480
    >
    > Screenshot with two cards:
    > https://screenshot.googleplex.com/8BgSEpFi4Kn5M9k
    >
    > Bug: 1247698
    > Change-Id: I4ffc976cae783183628828ef6b2f5d6fe2e967b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963013
    > Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    > Reviewed-by: Matthias Körber <koerber@google.com>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1076217}
    
    Bug: 1247698
    Change-Id: Id46bb7fc0571c09d680abb9ba4c9a0efccb90a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061461
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076880}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewBridge.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewBridge.java
@@ -46,7 +46,7 @@ class TouchToFillCreditCardViewBridge {
     @CalledByNative
     private void showSheet(
             PersonalDataManager.CreditCard[] cards, boolean shouldShowScanCreditCard) {
-        mComponent.showSheet(shouldShowScanCreditCard);
+        mComponent.showSheet(cards, shouldShowScanCreditCard);
     }
 
     @CalledByNative
