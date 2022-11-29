94d9f514f86b492bcbd886ad27e8a1dbccf117f0
George Benz <gbj@google.com>
2022-11-29 22:55:44 +0000

    Adds screenplay tags to all CUJ's
    Bug: b/260139986
    Change-Id: I80ef8902e29397c50b3208d1cc29e760f19201d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060895
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: george benz <gbj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077130}

--- chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
+++ chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.h"
 
+#include <map>
+
 #include "base/auto_reset.h"
 #include "base/memory/ptr_util.h"
+#include "base/no_destructor.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
@@ -568,6 +571,14 @@ raw_ptr<AccessCodeCastPrefUpdater>
 AccessCodeCastIntegrationBrowserTest::GetPrefUpdater() {
   return AccessCodeCastSinkServiceFactory::GetForProfile(browser()->profile())
       ->pref_updater_.get();
+}
+
+void AccessCodeCastIntegrationBrowserTest::AddScreenplayTag(
+    const std::string& screenplay_tag) {
+  static base::NoDestructor<std::map<std::string, std::string>> tag;
+  tag->insert(
+      std::pair<std::string, std::string>("feature_id", screenplay_tag));
+  RecordPropertyFromMap(*tag);
 }
 
 }  // namespace media_router
