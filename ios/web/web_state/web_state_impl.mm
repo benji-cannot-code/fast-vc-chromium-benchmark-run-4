a7bf80038075ea09917110a0cafe2cfaf4542116
ginnnnnnny <ginnyhuang@chromium.org>
2022-11-29 20:22:35 +0000

    Reland "[iOS][media-permissions] WebStateImpl & delegate API for custom prompt"
    This is a reland of commit 415c3349ee1c71bf0163e29df49a6c8a2545d9c4
    
    Reason for failure: Tests only run on simulator, so the variable
    `kWebViewDecisionHandlingTimeout` is not used on device.
    
    Original change's description:
    > [iOS][media-permissions] WebStateImpl & delegate API for custom prompt
    >
    > This CL adds all needed ios/web APIs for the permissions overlay tab
    > helper to be notified that the media permission is requested and to
    > acquire the decision handler needed to process the user's decision to
    > grant or deny media permissions.
    >
    > The new delegate method should return a boolean value which indicates
    > whether the delegate supports custom prompting. Currently, the
    > WebStateDelegateBrowserAgent is not implemented to support showing a
    > custom prompt, so it would return NO in the delegate method so the
    > web state would still show the default prompt that asks for permissions.
    >
    > Bug: 1356768
    > Change-Id: I0237dcb1b01efcfe7c8c1daaa7246a0d4feec180
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047948
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    > Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1076943}
    
    Bug: 1356768
    Change-Id: Ib6ffc15497e328ce82d42ef149cc07f062ef2c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060279
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Owners-Override: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077039}

--- ios/web/web_state/web_state_impl.mm
+++ ios/web/web_state/web_state_impl.mm
@@ -314,6 +314,12 @@ void WebStateImpl::RemoveAllWebFrames() {
   RealizedState()->RemoveAllWebFrames();
 }
 
+void WebStateImpl::RequestPermissionsWithDecisionHandler(
+    NSArray<NSNumber*>* permissions,
+    PermissionDecisionHandler handler) {
+  RealizedState()->RequestPermissionsWithDecisionHandler(permissions, handler);
+}
+
 #pragma mark - WebState implementation
 
 WebStateDelegate* WebStateImpl::GetDelegate() {
