e8a18955fc1784846cdda9f793438d3d1487b9ba
Joey Arhar <jarhar@chromium.org>
2022-11-29 23:59:13 +0000

    Disallow showPopover on transitioning popovers
    hidePopover doesn't allow transitioning popovers, so we should do the
    same in showPopover. This inconsistency was pointed out here:
    https://github.com/whatwg/html/pull/8221#discussion_r1026699030
    
    Bug: 1307772
    Change-Id: I3410cd37043100004f02fa3f3be10ec177830324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061291
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077159}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1259,8 +1259,9 @@ const char* HTMLElement::IsPopoverNotReady(
   if (!isConnected()) {
     return "Invalid on disconnected popover elements";
   }
-  if (action == PopoverTriggerAction::kShow && popoverOpen()) {
-    return "Invalid on already-showing popover elements";
+  if (action == PopoverTriggerAction::kShow &&
+      GetPopoverData()->visibilityState() != PopoverVisibilityState::kHidden) {
+    return "Invalid on popover elements which aren't hidden";
   }
   if (action == PopoverTriggerAction::kHide &&
       GetPopoverData()->visibilityState() != PopoverVisibilityState::kShowing) {
@@ -1277,8 +1278,7 @@ const char* HTMLElement::IsPopoverNotReady(
   if (action == PopoverTriggerAction::kShow &&
       Fullscreen::IsFullscreenElement(*this)) {
     return "This element is already in fullscreen mode, and therefore cannot "
-           "be "
-           "opened as a popover.";
+           "be opened as a popover.";
   }
   return nullptr;
 }
