97f813921072d9e6efead4e5f062e8529489e724
Ari Chivukula <arichiv@chromium.org>
2022-11-29 22:00:56 +0000

    [UnsafeDanglingUntriaged] components/download/internal/common/resource_downloader.cc
    This CL is part of a larger cleanup effort to triage dangling pointers
    and ensure they aren't the cause of memory issues in Chrome. See:
    https://docs.google.com/document/d/164SnySRX6MZgFkX8qLLLClTnZ0S8s5wLiNFWNyQ8f4E/edit
    
    Bug: 1381423
    Change-Id: I61e8c3e8debdd98ca87cac73334f978c8f661d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049863
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077098}

--- components/download/internal/common/in_progress_download_manager.cc
+++ components/download/internal/common/in_progress_download_manager.cc
@@ -244,10 +244,10 @@ void InProgressDownloadManager::OnUrlDownloadStarted(
 }
 
 void InProgressDownloadManager::OnUrlDownloadStopped(
-    UrlDownloadHandler* downloader) {
+    UrlDownloadHandlerID downloader) {
   for (auto ptr = url_download_handlers_.begin();
        ptr != url_download_handlers_.end(); ++ptr) {
-    if (ptr->get() == downloader) {
+    if (reinterpret_cast<UrlDownloadHandlerID>(ptr->get()) == downloader) {
       url_download_handlers_.erase(ptr);
       return;
     }
@@ -699,7 +699,7 @@ void InProgressDownloadManager::AddInProgressDownloadForTest(
 void InProgressDownloadManager::CancelUrlDownload(
     UrlDownloadHandler* downloader,
     bool user_cancel) {
-  OnUrlDownloadStopped(downloader);
+  OnUrlDownloadStopped(reinterpret_cast<UrlDownloadHandlerID>(downloader));
 }
 
 }  // namespace download
