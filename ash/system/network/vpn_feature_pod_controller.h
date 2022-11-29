fddb854e11067c9e01114a94f4000d35c6b54692
James Cook <jamescook@chromium.org>
2022-11-29 22:47:31 +0000

    qs: Add quick settings feature tile for VPN for QsRevamp
    Add a tile for VPN settings. The hide/show logic is the same as the
    legacy feature pod button.
    
    https://screenshot.googleplex.com/32qtB5WXasCTtM2
    
    Bug: b:260620957
    Test: added to ash_unittests
    Change-Id: Id9f542345a6bde172bf540a3d2f1f1386400324b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062523
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077124}

--- ash/system/network/vpn_feature_pod_controller.h
+++ ash/system/network/vpn_feature_pod_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_NETWORK_VPN_FEATURE_POD_CONTROLLER_H_
 #define ASH_SYSTEM_NETWORK_VPN_FEATURE_POD_CONTROLLER_H_
 
+#include "ash/ash_export.h"
 #include "ash/constants/quick_settings_catalogs.h"
 #include "ash/system/network/tray_network_state_observer.h"
 #include "ash/system/unified/feature_pod_controller_base.h"
@@ -15,8 +16,8 @@ namespace ash {
 class UnifiedSystemTrayController;
 
 // Controller of vpn feature pod button.
-class VPNFeaturePodController : public FeaturePodControllerBase,
-                                public TrayNetworkStateObserver {
+class ASH_EXPORT VPNFeaturePodController : public FeaturePodControllerBase,
+                                           public TrayNetworkStateObserver {
  public:
   explicit VPNFeaturePodController(
       UnifiedSystemTrayController* tray_controller);
@@ -28,6 +29,7 @@ class VPNFeaturePodController : public FeaturePodControllerBase,
 
   // FeaturePodControllerBase:
   FeaturePodButton* CreateButton() override;
+  std::unique_ptr<FeatureTile> CreateTile() override;
   QsFeatureCatalogName GetCatalogName() override;
   void OnIconPressed() override;
 
@@ -37,9 +39,11 @@ class VPNFeaturePodController : public FeaturePodControllerBase,
  private:
   void Update();
 
-  // Unowned.
   UnifiedSystemTrayController* const tray_controller_;
+
+  // Owned by views hierarchy.
   FeaturePodButton* button_ = nullptr;
+  FeatureTile* tile_ = nullptr;
 };
 
 }  // namespace ash
