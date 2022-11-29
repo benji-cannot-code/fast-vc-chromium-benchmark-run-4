e96adafff3f01302f4174232f7ba49337bf6622e
Rune Lillesveen <futhark@chromium.org>
2022-11-29 10:07:38 +0000

    [@container] Make container-name a tree-scoped name
    No behavioral change. The tree-scopes are not matched as part of
    selecting a container.
    
    TEST=external/wpt/css/css-contain/container-queries/
    
    Bug: 1382790
    Change-Id: I6f8c49590fc08e988ddc3effae0c2e224d540b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062027
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076746}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -321,8 +321,8 @@ class StyleBuilderConverter {
   static ScrollbarGutter ConvertScrollbarGutter(StyleResolverState& state,
                                                 const CSSValue& value);
 
-  static Vector<AtomicString> ConvertContainerName(StyleResolverState&,
-                                                   const CSSValue&);
+  static ScopedCSSNameList* ConvertContainerName(StyleResolverState&,
+                                                 const ScopedCSSValue&);
 
   static absl::optional<StyleIntrinsicLength> ConvertIntrinsicDimension(
       const StyleResolverState&,
