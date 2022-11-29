d42904904e7f501bc0d7d60a0ebc96f30bde9e93
Alex Turner <alexmt@chromium.org>
2022-11-29 15:59:14 +0000

    Increase data clearing task priority in Attribution Reporting
    Currently, the task runner uses BEST_EFFORT priority. This updates that
    to USER_VISIBLE whenever there's a queued or running data clearing task.
    
    This cl also makes the task runner per-service (i.e. per-storage
    partition) instead of global. If a storage partition is destroyed and
    then immediately re-created, this may add a slight risk of concurrent
    file access. However, this should be rare (if even possible) and aligns
    with the behavior of other API storage. To accommodate this change, the
    testing constructor of AttributionManagerImpl was modified to avoid
    test errors. The same issue has already been fixed for the aggregation
    service and private aggregation components: crrev.com/c/4023027 and
    crrev.com/c/4026466, respectively.
    
    Bug: 1383515
    Change-Id: I3216a1db84ebdf4e868106aca88407313bdfcf8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048145
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076875}

--- content/browser/attribution_reporting/attribution_manager_impl.h
+++ content/browser/attribution_reporting/attribution_manager_impl.h
@@ -39,6 +39,7 @@ class SuitableOrigin;
 namespace base {
 class FilePath;
 class TimeDelta;
+class UpdateableSequencedTaskRunner;
 }  // namespace base
 
 namespace storage {
@@ -112,7 +113,8 @@ class CONTENT_EXPORT AttributionManagerImpl : public AttributionManager {
       std::unique_ptr<AttributionStorageDelegate> storage_delegate,
       std::unique_ptr<AttributionCookieChecker> cookie_checker,
       std::unique_ptr<AttributionReportSender> report_sender,
-      StoragePartitionImpl* storage_partition);
+      StoragePartitionImpl* storage_partition,
+      scoped_refptr<base::UpdateableSequencedTaskRunner> storage_task_runner);
 
   static std::unique_ptr<AttributionManagerImpl> CreateWithNewDbForTesting(
       StoragePartitionImpl* storage_partition,
@@ -182,7 +184,8 @@ class CONTENT_EXPORT AttributionManagerImpl : public AttributionManager {
       std::unique_ptr<AttributionStorageDelegate> storage_delegate,
       std::unique_ptr<AttributionCookieChecker> cookie_checker,
       std::unique_ptr<AttributionReportSender> report_sender,
-      std::unique_ptr<AttributionDataHostManager> data_host_manager);
+      std::unique_ptr<AttributionDataHostManager> data_host_manager,
+      scoped_refptr<base::UpdateableSequencedTaskRunner> storage_task_runner);
 
   void MaybeEnqueueEvent(SourceOrTrigger event);
   void ProcessEvents();
@@ -246,6 +249,8 @@ class CONTENT_EXPORT AttributionManagerImpl : public AttributionManager {
                                    bool is_debug_cookie_set,
                                    const CreateReportResult& result);
 
+  void OnClearDataComplete();
+
   // Never null.
   const raw_ptr<StoragePartitionImpl> storage_partition_;
 
@@ -261,6 +266,16 @@ class CONTENT_EXPORT AttributionManagerImpl : public AttributionManager {
   // Controls the maximum size of `pending_events_` to avoid unbounded memory
   // growth with adversarial input.
   size_t max_pending_events_;
+
+  // The task runner for all attribution reporting storage operations.
+  // Updateable to allow for priority to be temporarily increased to
+  // `USER_VISIBLE` when a clear data task is queued or running. Otherwise
+  // `BEST_EFFORT` is used.
+  scoped_refptr<base::UpdateableSequencedTaskRunner> storage_task_runner_;
+
+  // How many clear data storage tasks are queued or running currently, i.e.
+  // have been posted but the reply has not been run.
+  int num_pending_clear_data_tasks_ = 0;
 
   base::SequenceBound<AttributionStorage> attribution_storage_;
 
--- content/browser/attribution_reporting/attribution_manager_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_manager_impl_unittest.cc
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
 #include "base/scoped_observation.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "base/task/updateable_sequenced_task_runner.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -256,6 +259,12 @@ class AttributionManagerImplTest : public testing::Test {
     content::SetNetworkConnectionTrackerForTesting(
         network::TestNetworkConnectionTracker::GetInstance());
 
+    storage_task_runner_ =
+        base::ThreadPool::CreateUpdateableSequencedTaskRunner(
+            {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
+             base::TaskShutdownBehavior::BLOCK_SHUTDOWN,
+             base::ThreadPolicy::MUST_USE_FOREGROUND});
+
     CreateManager();
     CreateAggregationService();
   }
@@ -286,7 +295,8 @@ class AttributionManagerImplTest : public testing::Test {
         std::move(storage_delegate), std::move(cookie_checker),
         std::move(report_sender),
         static_cast<StoragePartitionImpl*>(
-            browser_context_->GetDefaultStoragePartition()));
+            browser_context_->GetDefaultStoragePartition()),
+        storage_task_runner_);
   }
 
   void ShutdownManager() {
@@ -350,6 +360,7 @@ class AttributionManagerImplTest : public testing::Test {
   raw_ptr<MockCookieChecker> cookie_checker_;
   raw_ptr<MockReportSender> report_sender_;
   raw_ptr<MockAggregationService> aggregation_service_;
+  scoped_refptr<base::UpdateableSequencedTaskRunner> storage_task_runner_;
 
   std::unique_ptr<AttributionManagerImpl> attribution_manager_;
 };
