48d9f32d3bd24c3cd9e0dd04b992b6155d6cf04c
Andrew Grieve <agrieve@chromium.org>
2022-11-30 01:54:09 +0000

    Revert "vt: Add RenderTests for interaction with Android UI"
    This reverts commit 3485e7ab5b4c4c8b207328fa6d8e0048b9bf49d0.
    
    Reason for revert: Broke the tree:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20(dbg)/4606/overview
    
    Note: CQ had a window of lack of coverage for errorprone, which
    is now fixed (CQ should now catch this) (crbug.com/1394212)
    
    Original change's description:
    > vt: Add RenderTests for interaction with Android UI
    >
    > Split out from https://crrev.com/c/4052241.
    >
    > A ViewTransition places snapshots within a "snapshot viewport"[1] that
    > is independent of any browser UI since such UI widgets may have
    > different visibility at the transition start and end.
    >
    > We want to test that the snapshots are correctly taken and
    > sized/positioned in scenarios like keyboard-shown --> keyboard-hidden,
    > URL-bar-shown --> URL-bar-hidden, etc. Most view transition tests are
    > Blink web tests but these UI widgets are implemented in chrome/browser
    > so they necessitate browser tests.
    >
    > It's also difficult to query the sizes/positions of snapshots since the
    > snapshots themselves live in the Viz process and they're displayed via
    > pseudo-elements which don't readily expose their location. To test this
    > robustly, use RenderTests to pixel test exactly what a user would be
    > seeing in these cases.
    >
    > [1] https://www.w3.org/TR/css-view-transitions-1/#snapshot-viewport-concept
    >
    > Bug: 1350910
    > Change-Id: I9670b8b51bbf4601fb787f5a6bc56c617e57048c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051991
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1077197}
    
    Bug: 1350910
    Change-Id: Ief63f794115d143241c7e9a2e18894a882a650e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062280
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077211}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
@@ -190,9 +190,4 @@ class ManualFillingCoordinator implements ManualFillingComponent {
     public int getKeyboardExtensionHeight() {
         return mMediator != null ? mMediator.getKeyboardExtensionHeight() : 0;
     }
-
-    @Override
-    public void forceShowForTesting() {
-        mMediator.showWhenKeyboardIsVisible();
-    }
 }
