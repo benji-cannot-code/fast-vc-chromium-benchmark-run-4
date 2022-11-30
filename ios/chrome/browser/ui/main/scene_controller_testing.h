6fbe1e3332305c6af48d520aad468516322b1024
Stepan Khapugin <stkhapugin@chromium.org>
2022-11-30 17:45:50 +0000

    [iOS] Don't declare a category with internals in tab opener test.
    Instead, use the existing header with internals exposed for tests; that
    is at least compile-time checked.
    
    Fixed: 1379045
    Change-Id: I0b60a3c046dec31f7191339fb414fb181cf10572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064404
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077562}

--- ios/chrome/browser/ui/main/scene_controller_testing.h
+++ ios/chrome/browser/ui/main/scene_controller_testing.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 struct UrlLoadParams;
 @class TabGridCoordinator;
+@protocol BrowserInterface;
 
 // Methods exposed for testing. This is terrible and should be rewritten.
 @interface SceneController ()
@@ -24,6 +25,8 @@ struct UrlLoadParams;
 // NO, then call `completion`.
 - (void)dismissModalDialogsWithCompletion:(ProceduralBlock)completion
                            dismissOmnibox:(BOOL)dismissOmnibox;
+
+- (id<BrowserInterface>)currentInterface;
 
 @end
 
