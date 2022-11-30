f6b6c6232d96ebaa02adf7962744568658727786
Wenbo Jie <wenbojie@chromium.org>
2022-11-30 06:34:54 +0000

    [FilesLabs] Add tree/tree-item demo to labs
    Demo: http://go/scrcast/NTA5MzEyOTc0NjEyMDcwNHw3Yjk4Y2Y4MC1mNg
    
    Bug: b:242938200
    Change-Id: I95ef6115b68f3b1decead7c255001f5f6336bec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053454
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077299}

--- ash/webui/file_manager/resources/labs/main.ts
+++ ash/webui/file_manager/resources/labs/main.ts
@@ -4,5 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://file-manager/foreground/elements/xf_button.js';
+import 'chrome://file-manager/widgets/xf_breadcrumb.js';
+import 'chrome://file-manager/widgets/xf_tree.js';
+import 'chrome://file-manager/widgets/xf_tree_item.js';
 
 console.log('main ts is here.');
