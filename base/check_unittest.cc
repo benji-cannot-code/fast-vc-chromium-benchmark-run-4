a612c9e2a98adb4d2657c9ba41415d90066637ff
Jeroen Dhollander <jeroendh@google.com>
2022-11-30 10:12:32 +0000

    Implement CHECK_DEREF
    This macro allows the caller to safely dereference a given pointer, by
    CHECKing the pointer before dereferencing.
    
    This is based on ABSL_DIE_IF_NULL in Google3, with the main difference
    that CHECK_DEREF returns a reference while ABSL_DIE_IF_NULL returns the
    original pointer.
    
    Example failure:
    
      FATAL chrome[2321503:2321503]: [remote_commands_service.cc(211)] Check failed: client != nullptr.
    
    Rational of implementing CHECK_DEREF and not supporting ABSL_DIE_IF_NULL:
       * ABSL_DIE_IF_NULL just returns the pointer rather than deferring it,
         forcing the caller to add a dereference.
       * CHECK_DEREF expresses the intent better (and forces people to
         continue with the reference and not the pointer,
         which is the goal).
       * ABSL_DIE_IF_NULL works on any type that compares with null,
         which could lead to confusion and even dangerous use-after-free
         scenarios, think
         `auto& my_ref = *ABSL_DIE_IF_NULL(SomethingReturningAUniquePtr());`
       * CHECK_DEREF on the other hand explicitly only supports pointers
    
    Change-Id: I17c8689feaab82e808eb99f49f6ad86bd14b0e16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3985828
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077368}

--- base/check_unittest.cc
+++ base/check_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/check_deref.h"
 #include "base/logging.h"
 #include "base/strings/string_piece.h"
 #include "base/test/gtest_util.h"
@@ -58,6 +59,8 @@ class ScopedCheckExpectation {
 // Macro which expects a CHECK to fire with a certain message. If msg starts
 // with "=~", it's interpreted as a regular expression.
 // Example: EXPECT_CHECK("Check failed: false.", CHECK(false));
+//
+// Note: Please use the `CheckDeathTest` fixture when using this check.
 #if !CHECK_WILL_STREAM()
 #define EXPECT_CHECK(msg, check_expr) \
   do {                                \
@@ -72,6 +75,8 @@ class ScopedCheckExpectation {
 #endif  // !CHECK_WILL_STREAM()
 
 // Macro which expects a DCHECK to fire if DCHECKs are enabled.
+//
+// Note: Please use the `CheckDeathTest` fixture when using this check.
 #define EXPECT_DCHECK(msg, check_expr)                                         \
   do {                                                                         \
     if (DCHECK_IS_ON() && logging::LOGGING_DCHECK == logging::LOGGING_FATAL) { \
@@ -82,9 +87,7 @@ class ScopedCheckExpectation {
     }                                                                          \
   } while (0)
 
-class CheckTest : public testing::Test {};
-
-TEST_F(CheckTest, Basics) {
+TEST(CheckDeathTest, Basics) {
   EXPECT_CHECK("Check failed: false. ", CHECK(false));
 
   EXPECT_CHECK("Check failed: false. foo", CHECK(false) << "foo");
@@ -92,11 +95,11 @@ TEST_F(CheckTest, Basics) {
   double a = 2, b = 1;
   EXPECT_CHECK("Check failed: a < b (2.000000 vs. 1.000000)", CHECK_LT(a, b));
 
-  EXPECT_CHECK("Check failed: a < b (2.000000 vs. 1.000000)foo",
-               CHECK_LT(a, b) << "foo");
+  EXPECT_CHECK("Check failed: a < b (2.000000 vs. 1.000000)custom message",
+               CHECK_LT(a, b) << "custom message");
 }
 
-TEST_F(CheckTest, PCheck) {
+TEST(CheckDeathTest, PCheck) {
   const char file[] = "/nonexistentfile123";
   std::ignore = fopen(file, "r");
   std::string err =
@@ -127,7 +130,7 @@ TEST_F(CheckTest, PCheck) {
       DPCHECK(fopen(file, "r") != nullptr) << "foo");
 }
 
-TEST_F(CheckTest, CheckOp) {
+TEST(CheckDeathTest, CheckOp) {
   int a = 1, b = 2;
   // clang-format off
   EXPECT_CHECK("Check failed: a == b (1 vs. 2)", CHECK_EQ(a, b));
@@ -146,7 +149,7 @@ TEST_F(CheckTest, CheckOp) {
   // clang-format on
 }
 
-TEST_F(CheckTest, CheckStreamsAreLazy) {
+TEST(CheckTest, CheckStreamsAreLazy) {
   int called_count = 0;
   int not_called_count = 0;
 
@@ -188,7 +191,7 @@ void DcheckEmptyFunction2() {}
 #if BUILDFLAG(DCHECK_IS_CONFIGURABLE)
 class ScopedDcheckSeverity {
  public:
-  ScopedDcheckSeverity(logging::LogSeverity new_severity)
+  explicit ScopedDcheckSeverity(logging::LogSeverity new_severity)
       : old_severity_(logging::LOGGING_DCHECK) {
     logging::LOGGING_DCHECK = new_severity;
   }
@@ -206,7 +209,7 @@ class ScopedDcheckSeverity {
 #else
 #define MAYBE_Dcheck Dcheck
 #endif
-TEST_F(CheckTest, MAYBE_Dcheck) {
+TEST(CheckDeathTest, MAYBE_Dcheck) {
 #if BUILDFLAG(DCHECK_IS_CONFIGURABLE)
   // DCHECKs are enabled, and LOGGING_DCHECK is mutable, but defaults to
   // non-fatal. Set it to LOGGING_FATAL to get the expected behavior from the
@@ -271,7 +274,7 @@ TEST_F(CheckTest, MAYBE_Dcheck) {
       DCHECK_EQ(mp2, &MemberFunctions::MemberFunction1));
 }
 
-TEST_F(CheckTest, DcheckReleaseBehavior) {
+TEST(CheckTest, DcheckReleaseBehavior) {
   int var1 = 1;
   int var2 = 2;
   int var3 = 3;
@@ -284,7 +287,7 @@ TEST_F(CheckTest, DcheckReleaseBehavior) {
   DCHECK_EQ(var1, 1) << var4;
 }
 
-TEST_F(CheckTest, DCheckEqStatements) {
+TEST(CheckTest, DCheckEqStatements) {
   bool reached = false;
   if (false)
     DCHECK_EQ(false, true);  // Unreached.
@@ -296,7 +299,7 @@ TEST_F(CheckTest, DCheckEqStatements) {
     DCHECK_EQ(false, true);  // Unreached.
 }
 
-TEST_F(CheckTest, CheckEqStatements) {
+TEST(CheckTest, CheckEqStatements) {
   bool reached = false;
   if (false)
     CHECK_EQ(false, true);  // Unreached.
@@ -309,7 +312,7 @@ TEST_F(CheckTest, CheckEqStatements) {
 }
 
 #if BUILDFLAG(DCHECK_IS_CONFIGURABLE)
-TEST_F(CheckTest, ConfigurableDCheck) {
+TEST(CheckDeathTest, ConfigurableDCheck) {
   // Verify that DCHECKs default to non-fatal in configurable-DCHECK builds.
   // Note that we require only that DCHECK is non-fatal by default, rather
   // than requiring that it be exactly INFO, ERROR, etc level.
@@ -327,7 +330,7 @@ TEST_F(CheckTest, ConfigurableDCheck) {
   EXPECT_CHECK("Check failed: 1 == 2 (1 vs. 2)", DCHECK_EQ(1, 2));
 }
 
-TEST_F(CheckTest, ConfigurableDCheckFeature) {
+TEST(CheckTest, ConfigurableDCheckFeature) {
   // Initialize FeatureList with and without DcheckIsFatal, and verify the
   // value of LOGGING_DCHECK. Note that we don't require that DCHECK take a
   // specific value when the feature is off, only that it is non-fatal.
@@ -395,7 +398,7 @@ std::ostream& operator<<(std::ostream& out,
 }
 #endif  // CHECK_WILL_STREAM()
 
-TEST_F(CheckTest, OstreamVsToString) {
+TEST(CheckDeathTest, OstreamVsToString) {
   StructWithOstream a, b;
   EXPECT_CHECK("Check failed: a == b (ostream vs. ostream)", CHECK_EQ(a, b));
 
@@ -443,7 +446,7 @@ TEST_F(CheckTest, OstreamVsToString) {
     logging::SetLogMessageHandler(nullptr);                                    \
   } while (0)
 
-TEST_F(CheckTest, NotReached) {
+TEST(CheckDeathTest, NotReached) {
 #if BUILDFLAG(ENABLE_LOG_ERROR_NOT_REACHED) && !DCHECK_IS_ON()
   // Expect LOG(ERROR) that looks like CHECK(false) with streamed params intact.
   EXPECT_LOG_ERROR(__LINE__, NOTREACHED() << "foo",
@@ -454,7 +457,7 @@ TEST_F(CheckTest, NotReached) {
 #endif
 }
 
-TEST_F(CheckTest, NotImplemented) {
+TEST(CheckTest, NotImplemented) {
   static const std::string expected_msg =
       std::string("Not implemented reached in ") + __PRETTY_FUNCTION__;
 
@@ -472,7 +475,7 @@ void NiLogOnce() {
   NOTIMPLEMENTED_LOG_ONCE() << "foo";
 }
 
-TEST_F(CheckTest, NotImplementedLogOnce) {
+TEST(CheckTest, NotImplementedLogOnce) {
   static const std::string expected_msg =
       "Not implemented reached in void (anonymous namespace)::NiLogOnce()\n";
 
@@ -483,6 +486,42 @@ TEST_F(CheckTest, NotImplementedLogOnce) {
   EXPECT_NO_LOG(NiLogOnce());
   EXPECT_NO_LOG(NiLogOnce());
 #endif
+}
+
+// Test CHECK_DEREF of `T*`
+TEST(CheckTest, CheckDerefOfPointer) {
+  std::string pointee = "not-null";
+  std::string* value_pointer = &pointee;
+
+  auto& deref_result = CHECK_DEREF(value_pointer);
+  static_assert(std::is_lvalue_reference_v<decltype(deref_result)>);
+  // Compare the pointers to ensure they are the same object (and not a copy)
+  EXPECT_EQ(&deref_result, &pointee);
+  static_assert(std::is_same_v<decltype(deref_result), std::string&>);
+}
+
+TEST(CheckDeathTest, CheckDerefOfNullPointer) {
+  std::string* null_pointer = nullptr;
+  EXPECT_CHECK("Check failed: null_pointer != nullptr. ",
+               CHECK_DEREF(null_pointer));
+}
+
+// Test CHECK_DEREF of `const T*`
+TEST(CheckTest, CheckDerefOfConstPointer) {
+  std::string pointee = "not-null";
+  const std::string* const_value_pointer = &pointee;
+
+  auto& deref_result = CHECK_DEREF(const_value_pointer);
+  static_assert(std::is_lvalue_reference_v<decltype(deref_result)>);
+  // Compare the pointers to ensure they are the same object (and not a copy)
+  EXPECT_EQ(&deref_result, &pointee);
+  static_assert(std::is_same_v<decltype(deref_result), const std::string&>);
+}
+
+TEST(CheckDeathTest, CheckDerefOfConstNullPointer) {
+  std::string* const_null_pointer = nullptr;
+  EXPECT_CHECK("Check failed: const_null_pointer != nullptr. ",
+               CHECK_DEREF(const_null_pointer));
 }
 
 }  // namespace
--- chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.cc
+++ chrome/browser/ntp_tiles/chrome_most_visited_sites_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/check_deref.h"
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
@@ -145,7 +146,7 @@ ChromeMostVisitedSitesFactory::NewForProfile(Profile* profile) {
       nullptr,
 #endif
 #if !BUILDFLAG(IS_ANDROID)
-      web_app::IsAnyChromeAppToWebAppMigrationEnabled(*profile)
+      web_app::IsAnyChromeAppToWebAppMigrationEnabled(CHECK_DEREF(profile))
 #else
       false
 #endif
