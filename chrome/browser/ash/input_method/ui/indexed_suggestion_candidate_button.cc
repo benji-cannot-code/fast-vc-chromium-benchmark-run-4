1978e583fe2a9ffb8a23ddc4370d9c5012fe2e57
jhtin <jhtin@chromium.org>
2022-11-30 01:26:45 +0000

    longpress diacritics: Match changed UX specs for suggestion button labels
    __SIMPLE_REVIEW__
    
    go/screenshots/8NcLtZZaaPJHei9 (Dark)
    go/screenshots/BieYfmpYuWNLhBU (Light)
    
    Bug: b/259147634
    Change-Id: Ibc2813830ef539d93fae75fde96a8cb7c57622da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062794
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077203}

--- chrome/browser/ash/input_method/ui/indexed_suggestion_candidate_button.cc
+++ chrome/browser/ash/input_method/ui/indexed_suggestion_candidate_button.cc
@@ -21,12 +21,12 @@ const int kCandidateSquareSide = 20;
 const views::Label::CustomFont kCandidateTextFont = {
     .font_list = gfx::FontList(gfx::FontList({"Roboto"},
                                              gfx::Font::NORMAL,
-                                             13,
-                                             gfx::Font::Weight::NORMAL))};
+                                             14,
+                                             gfx::Font::Weight::MEDIUM))};
 const views::Label::CustomFont kIndexFont = {
     .font_list = gfx::FontList(gfx::FontList({"Roboto"},
                                              gfx::Font::NORMAL,
-                                             11,
+                                             10,
                                              gfx::Font::Weight::NORMAL))};
 
 IndexedSuggestionCandidateButton::IndexedSuggestionCandidateButton(
@@ -108,7 +108,10 @@ void IndexedSuggestionCandidateButton::BuildCandidate(
   //   |   |
   //   | 1 | <-- label being created
   //   +---+
-  AddChildView(std::make_unique<views::Label>(index_text, kIndexFont));
+  auto* candidate_text_label =
+      AddChildView(std::make_unique<views::Label>(index_text, kIndexFont));
+  candidate_text_label->SetEnabledColor(
+      ResolveSemanticColor(cros_styles::ColorName::kTextColorSecondary));
 }
 
 IndexedSuggestionCandidateButton::~IndexedSuggestionCandidateButton() = default;
