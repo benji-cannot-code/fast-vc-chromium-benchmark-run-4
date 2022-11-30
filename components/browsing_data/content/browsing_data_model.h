84b9f63385a17cd5b7736b2b0272c3570bfc6dd5
Mariam Ali <alimariam@google.com>
2022-11-30 15:36:49 +0000

    [Browsing Data Model] Integrating FLEDGE API
    In this CL:
    - Creating a new data key for Interest Groups by creating a structure of
    owner origin and joining origin.
    - Exposing Get and Remove methods to the public interface of the
    interest group manager.
    - Utilizing these methods to fetch interest group data keys and add them
    to the browsing data model entries.
    
    
    Bug: 1271155, 1378703, b/254414153
    Change-Id: If8ceb8b62353c86e4b64bcf0f364c2307eec0c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4026870
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077490}

--- components/browsing_data/content/browsing_data_model.h
+++ components/browsing_data/content/browsing_data_model.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/containers/enum_set.h"
 #include "base/memory/raw_ref.h"
+#include "content/public/browser/interest_group_manager.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/origin.h"
@@ -34,6 +35,7 @@ class BrowsingDataModel {
   enum class StorageType {
     kTrustTokens,  // Only issuance information considered.
     kSharedStorage,
+    kInterestGroup,
     kPartitionedQuotaStorage,    // Not fetched from disk or deleted.
     kUnpartitionedQuotaStorage,  // Not fetched from disk or deleted.
 
@@ -46,8 +48,9 @@ class BrowsingDataModel {
   // The information which uniquely identifies this browsing data. The set of
   // data an entry represents can be pulled from the relevant storage backends
   // using this information.
-  typedef absl::variant<url::Origin,       // Single origin, e.g. Trust Tokens
-                        blink::StorageKey  // Partitioned JS storage
+  typedef absl::variant<url::Origin,        // Single origin, e.g. Trust Tokens
+                        blink::StorageKey,  // Partitioned JS storage
+                        content::InterestGroupManager::InterestGroupDataKey
                         // TODO(crbug.com/1271155): Additional backend keys.
                         >
       DataKey;
