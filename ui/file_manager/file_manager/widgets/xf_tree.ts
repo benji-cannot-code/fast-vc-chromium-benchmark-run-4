13bb43d9e9373386fc8205167dafff059306c476
Rebekah Potter <rbpotter@chromium.org>
2022-11-30 22:17:29 +0000

    File Manager: fix newly added bad import
    No-Try: true
    No-Tree-Checks: true
    Change-Id: I9a1a3f441c5319392283855b2d968266a021ed6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4068382
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@google.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Roman Arora <romanarora@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077652}

--- ui/file_manager/file_manager/widgets/xf_tree.ts
+++ ui/file_manager/file_manager/widgets/xf_tree.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {isRTL} from 'chrome://resources/js/util.js';
+import {isRTL} from 'chrome://resources/ash/common/util.js';
 
 import {css, customElement, html, query, state, XfBase} from './xf_base.js';
 import {TreeItemCollapsedEvent, TreeItemExpandedEvent, XfTreeItem} from './xf_tree_item.js';
