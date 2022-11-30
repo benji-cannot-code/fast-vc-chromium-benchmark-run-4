ec4d087cfe07076cf0759dfec2f0477879579767
Jenny Zhang <jennyz@chromium.org>
2022-11-30 23:56:21 +0000

    Add test for speaking lacros utterance with lacros voice.
    This cl implements the test coverage of crrev.com/c/4032062:
    1. Add GetTtsUtteranceQueueSize() in TestController to allow
    Lacros retrieve the utterance queue size of the TtsController
    from Ash.
    2. Add a lacros browser test to test tts.speak API to speak a
    Lacros utterance with a voice provided by a Lacros speech
    engine extension.
    
    Bug: 1227543
    Change-Id: I854d83915e5d9233bd0a555b02b52418e465911f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062648
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077686}

--- chrome/browser/ash/crosapi/test_controller_ash.cc
+++ chrome/browser/ash/crosapi/test_controller_ash.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sharesheet/sharesheet_service.h"
+#include "chrome/browser/speech/tts_crosapi_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -617,6 +618,12 @@ void TestControllerAsh::BindInputMethodTestInterface(
       std::make_unique<crosapi::InputMethodTestInterfaceAsh>(),
       std::move(receiver));
   std::move(callback).Run();
+}
+
+void TestControllerAsh::GetTtsUtteranceQueueSize(
+    GetTtsUtteranceQueueSizeCallback callback) {
+  std::move(callback).Run(
+      tts_crosapi_util::GetTtsUtteranceQueueSizeForTesting());
 }
 
 // This class waits for overview mode to either enter or exit and fires a
