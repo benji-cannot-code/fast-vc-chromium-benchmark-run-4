932468d7d00ce48292e44c74e079929820fdc27e
Tina Wang <tinazwang@google.com>
2022-11-30 22:56:58 +0000

    [P13n] Add skeleton of sign in promo half sheet
    Add coordinator, view controller and showcase sample to check the ui.
    
    Screenshot: https://screenshot.googleplex.com/4RDbz4bwFzGAEfu
    
    Bug: 1382615
    Change-Id: I4e7b17df84499e46143869ac9005de6418820da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060174
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077666}

---
+++ ios/showcase/feed_sign_in_promo/sc_feed_sign_in_promo_coordinator.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_SHOWCASE_FEED_SIGN_IN_PROMO_SC_FEED_SIGN_IN_PROMO_COORDINATOR_H_
+#define IOS_SHOWCASE_FEED_SIGN_IN_PROMO_SC_FEED_SIGN_IN_PROMO_COORDINATOR_H_
+
+#import "ios/showcase/common/navigation_coordinator.h"
+
+@interface SCFeedSignInPromoCoordinator : NSObject <NavigationCoordinator>
+@end
+
+#endif  // IOS_SHOWCASE_FEED_SIGN_IN_PROMO_SC_FEED_SIGN_IN_PROMO_COORDINATOR_H_
