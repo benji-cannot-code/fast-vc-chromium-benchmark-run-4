ad4ac2a1cfb534ac3d19e8c0d263a805edf678f5
Yoav Weiss <yoavweiss@chromium.org>
2022-11-30 14:29:13 +0000

    Avoid firing LCP triggered after mouseover over LCP element.
    This CL adds a flag that turns off LCP entries that are loaded right
    after a mouseover event on a previous LCP element.
    The flag is currently disabled by default.
    
    Bug: 1288027
    Change-Id: Idd8c1d47ad4a3bb91cc583b89ab4d68f08213ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061623
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077459}

--- chrome/browser/page_load_metrics/integration_tests/metric_integration_test.h
+++ chrome/browser/page_load_metrics/integration_tests/metric_integration_test.h
@@ -94,6 +94,10 @@ class MetricIntegrationTest : public InProcessBrowserTest {
   // metric name and value.
   void ExpectUKMPageLoadMetric(base::StringPiece metric_name,
                                int64_t expected_value);
+  void ExpectUKMPageLoadMetricGreaterThan(base::StringPiece metric_name,
+                                          int64_t expected_value);
+  void ExpectUKMPageLoadMetricLowerThan(base::StringPiece metric_name,
+                                        int64_t expected_value);
 
   int64_t GetUKMPageLoadMetricFlagSet(base::StringPiece metric_name);
 
@@ -149,6 +153,8 @@ class MetricIntegrationTest : public InProcessBrowserTest {
       const std::string& content,
       base::TimeDelta delay,
       const net::test_server::HttpRequest& request);
+
+  const ukm::mojom::UkmEntryPtr GetEntry();
 
   absl::optional<ukm::TestAutoSetUkmRecorder> ukm_recorder_;
   absl::optional<base::HistogramTester> histogram_tester_;
