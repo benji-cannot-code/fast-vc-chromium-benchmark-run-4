2b63ea3c44d18cd9054a98a85addec21e157157d
YuhanYang <yyhyyh@google.com>
2022-11-30 17:32:32 +0000

    Diagnostics: Update style for esc and alt in description label
    screenshot:
    - light mode: https://screenshot.googleplex.com/98mJnshwKbk4XRU
    - dark mode: https://screenshot.googleplex.com/AJczt3JYEfHWCxG
    
    Tested visual effect and ChromeVox on DUT
    
    Bug: b:207089601
    Change-Id: I3af51dda3aa4dd99497ce6c7418701e273c0730b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039101
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077539}

--- ash/webui/diagnostics_ui/resources/keyboard_tester.ts
+++ ash/webui/diagnostics_ui/resources/keyboard_tester.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './strings.m.js';
@@ -287,6 +288,10 @@ export class KeyboardTesterElement extends KeyboardTesterElementBase {
       return [];
     }
     return keyboard.topRowKeys.map((keyId: TopRowKey) => topRowKeyMap[keyId]);
+  }
+
+  protected getDescriptionLabel_(): TrustedHTML {
+    return this.i18nAdvanced('keyboardTesterInstruction', {attrs: ['id']});
   }
 
   private addEventListeners(): void {
