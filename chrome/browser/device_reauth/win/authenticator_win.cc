5b83b0e820cb898c5db6a4ec57f64d6ea5356ec7
Daniel Cheng <dcheng@chromium.org>
2022-11-30 20:29:16 +0000

    Require types used with base::Unretained() to be completely defined on Windows.
    LIke Linux, this initially only applies to non-test code since that
    provides the highest value for the amount of work required.
    
    Bug: 1392872
    Change-Id: I965d96b786cec93be71d664a7570d8ae506288e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067339
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077634}

--- chrome/browser/device_reauth/win/authenticator_win.cc
+++ chrome/browser/device_reauth/win/authenticator_win.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "ui/aura/window.h"
 
 namespace {
 
