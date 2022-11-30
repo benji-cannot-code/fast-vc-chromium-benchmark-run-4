76dbdd277f265193bce1efeae96bc893b38e8ce9
Ari Chivukula <arichiv@chromium.org>
2022-11-30 02:40:11 +0000

    [Expired Histograms] Extensions.ManifestIconSetIconWasVisibleForUnpacked*
    This CL is part of a code health project to remove expired histogram
    logging from the codebase. This data has not been recorded since 2019.
    
    Bug: 1384835
    Change-Id: If569cbdba1e99822fa28104a4e053bb7d6dc7885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051421
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077225}

--- extensions/common/file_util.cc
+++ extensions/common/file_util.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_file_value_serializer.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/escape.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
@@ -481,7 +480,6 @@ void SetReportErrorForInvisibleIconForTesting(bool value) {
 bool ValidateExtensionIconSet(const ExtensionIconSet& icon_set,
                               const Extension* extension,
                               const char* manifest_key,
-                              SkColor background_color,
                               std::string* error) {
   for (const auto& entry : icon_set.map()) {
     const base::FilePath path =
@@ -497,15 +495,6 @@ bool ValidateExtensionIconSet(const ExtensionIconSet& icon_set,
     if (extension->location() == ManifestLocation::kUnpacked) {
       const bool is_sufficiently_visible =
           image_util::IsIconAtPathSufficientlyVisible(path);
-      const bool is_sufficiently_visible_rendered =
-          image_util::IsRenderedIconAtPathSufficientlyVisible(path,
-                                                              background_color);
-      UMA_HISTOGRAM_BOOLEAN(
-          "Extensions.ManifestIconSetIconWasVisibleForUnpacked",
-          is_sufficiently_visible);
-      UMA_HISTOGRAM_BOOLEAN(
-          "Extensions.ManifestIconSetIconWasVisibleForUnpackedRendered",
-          is_sufficiently_visible_rendered);
       if (!is_sufficiently_visible && g_report_error_for_invisible_icon) {
         constexpr char kIconNotSufficientlyVisibleError[] =
             "Icon '%s' specified in '%s' is not sufficiently visible.";
