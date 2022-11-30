9f7107f8536649dc59f2a241594db41225d88564
David Munro <davidmunro@google.com>
2022-11-30 05:53:25 +0000

    crostini: Destroy upgrade notification upon uninstallation
    If Crostini's uninstalled then we shouldn't keep the upgrade
    notification around. Close + destroy it.
    
    Bug: b:232174138
    Test: Pop notification, uninstall Crostini
    Change-Id: I2d2170cb27492a386a1feee48a6dbabaa6c1a267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060386
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077282}

--- chrome/browser/ash/crostini/crostini_manager.cc
+++ chrome/browser/ash/crostini/crostini_manager.cc
@@ -4071,6 +4071,13 @@ void CrostiniManager::UnregisterContainer(
 
   guest_os::GuestOsSharePath::GetForProfile(profile_)->UnregisterGuest(
       container_id);
+
+  if (container_id == DefaultContainerId()) {
+    // For now the upgrade notification only supports the default container. If
+    // we're removing that container then destroy any notification we might have
+    // for it.
+    upgrade_available_notification_.reset();
+  }
 }
 
 void CrostiniManager::UnregisterAllContainers() {
@@ -4096,6 +4103,8 @@ void CrostiniManager::UnregisterAllContainers() {
       share_service->UnregisterGuest(guest);
     }
   }
+
+  upgrade_available_notification_.reset();
 }
 
 bool CrostiniManager::RegisterCreateOptions(
