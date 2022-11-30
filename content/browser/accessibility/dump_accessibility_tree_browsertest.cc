b0a630858530c5d61f09eceed35fcc570f46e257
Oriol Brufau <obrufau@igalia.com>
2022-11-30 12:30:22 +0000

    [a11y] Fix WaitForAllFramesLoaded and re-enable tests
    DumpAccessibilityTestBase uses WaitForAllFramesLoaded() in order to be
    able to dump the contents of the frames. This method had a limitation:
    it didn't support multiple frames with the same URL. When one frame
    loaded, others with the same URL wouldn't be awaited, causing flakiness.
    
    For example, a test with both <iframe srcdoc="foo"></iframe> and
    <iframe srcdoc="bar"></iframe> was flaky since both frames have the
    same URL, 'about:srcdoc'. To avoid this, they would need to be changed
    to unique data URLs like <iframe src="data:text/html,a"></iframe> and
    <iframe src="data:text/html,b"></iframe>. For the same reason,
    <embed src="foo.svg"></embed><embed src="foo.svg"></embed> had to be
    <embed src="foo.svg?1"></embed><embed src="foo.svg?2"></embed>.
    
    Additionally, WaitForAllFramesLoaded() had a bug: it wouldn't detect
    the load of ignored frames, so it would keep awaiting. That was because
    AccessibilityTreeContainsLoadedDocWithUrl() skipped ignored children.
    
    modal-dialog-and-iframes.html and inert-attribute.html are two tests
    that use srcdoc frames, which were flaky, but switching to src data URIs
    would trigger the bug since some of them are inert (and thus ignored).
    
    Then, this patch
    - Fixes WaitForAllFramesLoaded() to not miss ignored frames.
    - Makes WaitForAllFramesLoaded() support multiple frames with same URL.
    - Re-enables previously affected tests.
    
    Bug: 1367886
    TEST=All/DumpAccessibilityTreeTest.AccessibilitySvgAsEmbedSource/*
    TEST=All/DumpAccessibilityTreeTest.AccessibilitySvgAsObjectSource/*
    TEST=All/DumpAccessibilityTreeTestWithIgnoredNodes.AccessibilityModalDialogAndIframes/*
    TEST=All/DumpAccessibilityTreeTestWithIgnoredNodes.InertAttribute/*
    
    AX-Relnotes: n/a
    Change-Id: Ida5d19c0e8e7e0908474f072e7e511ed148ea0bf
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051127
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077410}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2233,9 +2233,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityInPageLinks) {
   RunHtmlTest(FILE_PATH_LITERAL("in-page-links.html"));
 }
 
-// TODO(crbug.com/1367886): This test is flaky.
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
-                       DISABLED_InertAttribute) {
+                       InertAttribute) {
   base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
       switches::kEnableBlinkFeatures, "InertAttribute");
   RunHtmlTest(FILE_PATH_LITERAL("inert-attribute.html"));
@@ -2695,9 +2694,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 */
 
-// TODO(crbug.com/1367886): This test is flaky.
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
-                       DISABLED_AccessibilityModalDialogAndIframes) {
+                       AccessibilityModalDialogAndIframes) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-and-iframes.html"));
 }
 
@@ -3006,29 +3004,13 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilitySvg) {
   RunHtmlTest(FILE_PATH_LITERAL("svg.html"));
 }
 
-// TODO: fails on android, the test hangs in WaitForAllFramesLoaded while
-// waiting for AccessibilityNotificationWaiter::WaitForNotification.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_AccessibilitySvgAsEmbedSource \
-  DISABLED_AccessibilitySvgAsEmbedSource
-#else
-#define MAYBE_AccessibilitySvgAsEmbedSource AccessibilitySvgAsEmbedSource
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       MAYBE_AccessibilitySvgAsEmbedSource) {
+                       AccessibilitySvgAsEmbedSource) {
   RunHtmlTest(FILE_PATH_LITERAL("svg-as-embed-source.html"));
 }
 
-// TODO: fails on android, the test hangs in WaitForAllFramesLoaded while
-// waiting for AccessibilityNotificationWaiter::WaitForNotification.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_AccessibilitySvgAsObjectSource \
-  DISABLED_AccessibilitySvgAsObjectSource
-#else
-#define MAYBE_AccessibilitySvgAsObjectSource AccessibilitySvgAsObjectSource
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       MAYBE_AccessibilitySvgAsObjectSource) {
+                       AccessibilitySvgAsObjectSource) {
   RunHtmlTest(FILE_PATH_LITERAL("svg-as-object-source.html"));
 }
 
