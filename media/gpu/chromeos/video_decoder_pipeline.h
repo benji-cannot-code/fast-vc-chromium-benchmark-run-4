cc983dd65c91410fe56dbe3d813fe73f46ee8e0f
Miguel Casas <mcasas@chromium.org>
2022-11-30 17:49:18 +0000

    RELAND: media/gpu/(v4l2,vaapi): Homogenize (video) frame allocations
    This CL relands https://crrev.com/c/3981035 which was reverted due
    to breaking a CTS test. PS0 contains the reverted CL verbatim.
    
    Original CL description ---------------------------------------------
    
    VideoDecoderPipeline must allocate enough VideoFrames in the pool to
    feed the driver (which needs them for reference frames) and the video
    pipeline in the Renderer (which might be long if the playback smoothing
    queue VideoRendererAlgorithm is used, i.e. for non-WebRTC cases). There
    might be an ImageProcessor involved, but the concept is still the
    same.
    
    VideoDecoderPipeline gets that info via PickDecoderOutputFormat(),
    concretely its |num_of_pictures| parameter. This number comes from:
    - The codec-specific decoder's GetRequiredNumOfPictures(), for the
    VAAPI and V4L2 stateless backend. This method calculates this as the
    per-codec number of reference frames, plus kMaxVideoFrames.
    - Querying the driver and then adding kMaxVideoFrames for the V4L2
    stateful backend.
    
    kMaxVideoFrames is 4, which is not enough to feed a Renderer pipeline
    when the mentioned VideoRendererAlgorithm is in place, i.e. for non
    WebRTC cases.
    
    This CL changes PickDecoderOutputFormat() to only need the amount of
    per-codec reference frames, and adds a member to VideoDecoderPipeline
    where to hold the |estimated_num_buffers_for_renderer_|, which is
    calculated upon Initialize(), looking at the |low_delay| parameter.
    
    Allocating more buffers reduces the contention, which in turn reduces
    the decode latency, albeit at the cost of having more allocated buffers.
    
    Some numbers from playing a 1080p H264 video via Tast PlaybackPerf:
    - Trogdor QC7180 (stateful v4l2), Renderer decode latency goes from
    ~220ms to ~172 ms. Platform Decoding stays similar (~165ms).
    - Jacuzzi MT8183 (stateless v4l2) Render decode latency goes from
    ~425ms to ~80ms. Platform Decoding stays similar (~65ms).
    - Volteer (Intel VA-API): Renderer latency goes from 400ms to ~65ms.
    Platform level decode latency stays at around 0.5ms before/after.
    
    Bug: b:250187354
    Change-Id: I436513fd6ebf2f68b7be4a7989b20de50b633860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981035
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1066566}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027031
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077568}

--- media/gpu/chromeos/video_decoder_pipeline.h
+++ media/gpu/chromeos/video_decoder_pipeline.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "media/base/cdm_context.h"
+#include "media/base/limits.h"
 #include "media/base/supported_video_decoder_config.h"
 #include "media/base/video_decoder.h"
 #include "media/base/video_decoder_config.h"
@@ -79,6 +80,10 @@ class MEDIA_GPU_EXPORT VideoDecoderMixin : public VideoDecoder {
     // if |allocator| is not absl::nullopt, the frame pool will be set
     // to use the allocator provided for allocating video frames.
     //
+    // The client also provides the |num_codec_reference_frames|, which is
+    // sometimes fixed (e.g. kVp9NumRefFrames) and sometimes variable (e.g.
+    // H.264, HEVC).
+    //
     // Note: after a call to this method, callers should assume that a pointer
     // returned by a prior call to GetVideoFramePool() is no longer valid.
     virtual CroStatus::Or<ImageProcessor::PixelLayoutCandidate>
@@ -87,7 +92,7 @@ class MEDIA_GPU_EXPORT VideoDecoderMixin : public VideoDecoder {
         const gfx::Rect& decoder_visible_rect,
         const gfx::Size& decoder_natural_size,
         absl::optional<gfx::Size> output_size,
-        size_t num_of_pictures,
+        size_t num_codec_reference_frames,
         bool use_protected,
         bool need_aux_frame_pool,
         absl::optional<DmabufVideoFramePool::CreateFrameCB> allocator) = 0;
@@ -177,7 +182,7 @@ class MEDIA_GPU_EXPORT VideoDecoderPipeline : public VideoDecoder,
       const gfx::Rect& decoder_visible_rect,
       const gfx::Size& decoder_natural_size,
       absl::optional<gfx::Size> output_size,
-      size_t num_of_pictures,
+      size_t num_codec_reference_frames,
       bool use_protected,
       bool need_aux_frame_pool,
       absl::optional<DmabufVideoFramePool::CreateFrameCB> allocator) override;
@@ -198,6 +203,7 @@ class MEDIA_GPU_EXPORT VideoDecoderPipeline : public VideoDecoder,
       CreateDecoderFunctionCB create_decoder_function_cb);
 
   void InitializeTask(const VideoDecoderConfig& config,
+                      bool low_delay,
                       CdmContext* cdm_context,
                       InitCB init_cb,
                       const OutputCB& output_cb,
@@ -327,6 +333,11 @@ class MEDIA_GPU_EXPORT VideoDecoderPipeline : public VideoDecoder,
   // needed for protected content on Intel platforms.
   bool need_frame_pool_rebuild_ GUARDED_BY_CONTEXT(decoder_sequence_checker_) =
       false;
+
+  // Calculated upon Initialize(), to determine how many buffers to allocate for
+  // the Renderer pipeline.
+  size_t estimated_num_buffers_for_renderer_ GUARDED_BY_CONTEXT(
+      decoder_sequence_checker_) = limits::kMaxVideoFrames + 1;
 
   // Set to true to bypass checks for encrypted content support for testing.
   bool allow_encrypted_content_for_testing_ = false;
