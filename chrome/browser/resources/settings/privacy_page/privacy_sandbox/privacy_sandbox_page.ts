061ef8cb6357f8a927101739fb6f2b0a5e266d62
Gabriel Oliveira <gabolvr@google.com>
2022-11-30 09:29:44 +0000

    [Privacy Sandbox] Add prefs and string identifiers to privacy sandbox component
    This CL addresses further comments in https://crrev.com/c/4047861 and
    computes the sublabels for privacy sandbox settings page link rows
    based on prefs.
    
    Bug: 1378703
    Change-Id: Id6f5689edfcb86e262d0f36c9f6203ed31d3adbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054620
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077347}

--- chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_page.ts
@@ -4,10 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import '../../prefs/prefs.js';
 import '../../settings_shared.css.js';
 
+import {CrLinkRowElement} from 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {PrefsMixin} from '../../prefs/prefs_mixin.js';
 import {routes} from '../../route.js';
 import {Router} from '../../router.js';
 
@@ -15,13 +19,17 @@ import {getTemplate} from './privacy_sandbox_page.html.js';
 
 export interface SettingsPrivacySandboxPageElement {
   $: {
-    privacySandboxAdMeasurementLinkRow: HTMLElement,
-    privacySandboxFledgeLinkRow: HTMLElement,
-    privacySandboxTopicsLinkRow: HTMLElement,
+    privacySandboxAdMeasurementLinkRow: CrLinkRowElement,
+    privacySandboxFledgeLinkRow: CrLinkRowElement,
+    privacySandboxTopicsLinkRow: CrLinkRowElement,
   };
 }
 
-export class SettingsPrivacySandboxPageElement extends PolymerElement {
+const SettingsPrivacySandboxPageElementBase =
+    I18nMixin(PrefsMixin(PolymerElement));
+
+export class SettingsPrivacySandboxPageElement extends
+    SettingsPrivacySandboxPageElementBase {
   static get is() {
     return 'settings-privacy-sandbox-page';
   }
@@ -30,22 +38,38 @@ export class SettingsPrivacySandboxPageElement extends PolymerElement {
     return getTemplate();
   }
 
+  static get properties() {
+    return {
+      /**
+       * Preferences state.
+       */
+      prefs: {
+        type: Object,
+        notify: true,
+      },
+    };
+  }
+
   private computePrivacySandboxTopicsSublabel_(): string {
-    // TODO(b/254412639): Change sublabel based on the respective toggle being
-    // enabled.
-    return 'Enabled Nulla eros tortor, placerat blandit dictum a, interdum id metus';
+    const enabled = this.getPref('privacy_sandbox.m1.topics_enabled').value;
+    return this.i18n(
+        enabled ? 'adPrivacyPageTopicsLinkRowSubLabelEnabled' :
+                  'adPrivacyPageTopicsLinkRowSubLabelDisabled');
   }
 
   private computePrivacySandboxFledgeSublabel_(): string {
-    // TODO(b/254410792): Change sublabel based on the respective toggle being
-    // enabled.
-    return 'Enabled Duis scelerisque a mi eget ultricies';
+    const enabled = this.getPref('privacy_sandbox.m1.fledge_enabled').value;
+    return this.i18n(
+        enabled ? 'adPrivacyPageFledgeLinkRowSubLabelEnabled' :
+                  'adPrivacyPageFledgeLinkRowSubLabelDisabled');
   }
 
   private computePrivacySandboxAdMeasurementSublabel_(): string {
-    // TODO(b/254412652): Change sublabel based on the respective toggle being
-    // enabled.
-    return 'Enabled Vivamus id lacus et lacus porttitor vulputate. Sed semper egestas orci vel maximus.';
+    const enabled =
+        this.getPref('privacy_sandbox.m1.ad_measurement_enabled').value;
+    return this.i18n(
+        enabled ? 'adPrivacyPageAdMeasurementLinkRowSubLabelEnabled' :
+                  'adPrivacyPageAdMeasurementLinkRowSubLabelDisabled');
   }
 
   private onPrivacySandboxTopicsClick_() {
