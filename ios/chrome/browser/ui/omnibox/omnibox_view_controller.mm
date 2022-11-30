7f9cbba41a84340f61c5f49792480a7e3fb5f54b
Hira Mahmood <hiramahmood@google.com>
2022-11-30 19:00:19 +0000

    [iOS] Fix omnibox button traversal order when VoiceOver enabled.
    Currently, when VO is enabled and the user swipes right on the omnibox
    text field, the cancel button is highlighted before the clear text
    button:
    [Textfield] -> [Cancel, button] -> [Clear text, button]
    
    This change fixes the order of traversal to the expected behavior:
    [Textfield] -> [Clear text, button] -> [Cancel, button]
    
    Bug: 1383120
    Change-Id: If80e30534238d7f0401ee9ca40d852be9372f422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067739
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077601}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -119,6 +119,8 @@ const CGFloat kClearButtonSize = 28.0f;
                                                  iconTint:iconTintColor];
   self.view.incognito = self.incognito;
 
+  self.view.shouldGroupAccessibilityChildren = YES;
+
   self.textField.delegate = self;
   self.textField.omniboxKeyboardDelegate = self;
 
