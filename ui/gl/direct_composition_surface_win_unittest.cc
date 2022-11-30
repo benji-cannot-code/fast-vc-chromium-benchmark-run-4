b7729fedcf3a99658e03c629b16f67aa59abfc17
Michael Tang <tangm@microsoft.com>
2022-11-30 01:03:08 +0000

    gpu: win: Add DCompImageBackingFactory tests
    - Also fix various issues with the backings
    - Respect the DXGISwapChainPresentInterval0 feature for SI
    - For DXGISwapChainImageBacking: support multiple Skia write accesses
      per overlay read access (i.e. support multiple pending swap rects)
    
    Bug: 1378741
    Change-Id: I8d54a61f70961720ecfddc9c957a2833eba71dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032574
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1077192}

--- ui/gl/direct_composition_surface_win_unittest.cc
+++ ui/gl/direct_composition_surface_win_unittest.cc
@@ -17,13 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/power_monitor_test.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "base/win/scoped_gdi_object.h"
-#include "base/win/scoped_hdc.h"
-#include "base/win/scoped_select_object.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/win/hidden_window.h"
 #include "ui/gfx/buffer_format_util.h"
-#include "ui/gfx/gdi_util.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/transform.h"
 #include "ui/gl/dc_renderer_layer_params.h"
@@ -38,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_switches.h"
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/init/gl_factory.h"
+#include "ui/gl/test/gl_test_helper.h"
 #include "ui/platform_window/platform_window_delegate.h"
 #include "ui/platform_window/win/win_window.h"
 
@@ -635,44 +632,6 @@ TEST_F(DirectCompositionSurfaceTest, ProtectedVideos) {
   // video support is enabled by defaut in the Intel driver and Chrome
 }
 
-std::vector<SkColor> ReadBackWindow(HWND window, const gfx::Size& size) {
-  base::win::ScopedCreateDC mem_hdc(::CreateCompatibleDC(nullptr));
-  DCHECK(mem_hdc.IsValid());
-
-  BITMAPV4HEADER hdr;
-  gfx::CreateBitmapV4HeaderForARGB888(size.width(), size.height(), &hdr);
-
-  void* bits = nullptr;
-  base::win::ScopedBitmap bitmap(
-      ::CreateDIBSection(mem_hdc.Get(), reinterpret_cast<BITMAPINFO*>(&hdr),
-                         DIB_RGB_COLORS, &bits, nullptr, 0));
-  DCHECK(bitmap.is_valid());
-
-  base::win::ScopedSelectObject select_object(mem_hdc.Get(), bitmap.get());
-
-  // Grab a copy of the window. Use PrintWindow because it works even when the
-  // window's partially occluded. The PW_RENDERFULLCONTENT flag is undocumented,
-  // but works starting in Windows 8.1. It allows for capturing the contents of
-  // the window that are drawn using DirectComposition.
-  UINT flags = PW_CLIENTONLY | PW_RENDERFULLCONTENT;
-
-  BOOL result = PrintWindow(window, mem_hdc.Get(), flags);
-  if (!result)
-    PLOG(ERROR) << "Failed to print window";
-
-  GdiFlush();
-
-  std::vector<SkColor> pixels(size.width() * size.height());
-  memcpy(pixels.data(), bits, pixels.size() * sizeof(SkColor));
-  return pixels;
-}
-
-SkColor ReadBackWindowPixel(HWND window, const gfx::Point& point) {
-  gfx::Size size(point.x() + 1, point.y() + 1);
-  auto pixels = ReadBackWindow(window, size);
-  return pixels[size.width() * point.y() + point.x()];
-}
-
 class DirectCompositionPixelTest : public DirectCompositionSurfaceTest {
  public:
   DirectCompositionPixelTest()
@@ -756,7 +715,7 @@ class DirectCompositionPixelTest : public DirectCompositionSurfaceTest {
 
     SkColor expected_color = SK_ColorRED;
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
     EXPECT_EQ(expected_color, actual_color)
         << std::hex << "Expected " << expected_color << " Actual "
         << actual_color;
@@ -838,7 +797,7 @@ class DirectCompositionVideoPixelTest : public DirectCompositionPixelTest {
 
     if (check_color) {
       SkColor actual_color =
-          ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+          GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
       EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
           << std::hex << "Expected " << expected_color << " Actual "
           << actual_color;
@@ -911,7 +870,7 @@ TEST_F(DirectCompositionPixelTest, SoftwareVideoSwapchain) {
 
   SkColor expected_color = SkColorSetRGB(0xff, 0xb7, 0xff);
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -929,7 +888,7 @@ TEST_F(DirectCompositionPixelTest, VideoHandleSwapchain) {
 
   SkColor expected_color = SkColorSetRGB(0xe1, 0x90, 0xeb);
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -949,7 +908,7 @@ TEST_F(DirectCompositionPixelTest, SkipVideoLayerEmptyBoundsRect) {
   // content.
   SkColor expected_color = SK_ColorBLACK;
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -1001,7 +960,7 @@ TEST_F(DirectCompositionPixelTest, SkipVideoLayerEmptyContentsRect) {
   // content.
   SkColor expected_color = SK_ColorBLACK;
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -1035,7 +994,7 @@ TEST_F(DirectCompositionPixelTest, NV12SwapChain) {
 
   SkColor expected_color = SkColorSetRGB(0xe1, 0x90, 0xeb);
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -1078,7 +1037,7 @@ TEST_F(DirectCompositionPixelTest, YUY2SwapChain) {
 
   SkColor expected_color = SkColorSetRGB(0xe1, 0x90, 0xeb);
   SkColor actual_color =
-      ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+      GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
   EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
       << std::hex << "Expected " << expected_color << " Actual "
       << actual_color;
@@ -1110,7 +1069,7 @@ TEST_F(DirectCompositionPixelTest, NonZeroBoundsOffset) {
       {{74, 74}, video_color},
   };
 
-  auto pixels = ReadBackWindow(window_.hwnd(), window_size);
+  auto pixels = GLTestHelper::ReadBackWindow(window_.hwnd(), window_size);
 
   for (const auto& test_case : test_cases) {
     const auto& point = test_case.point;
@@ -1359,7 +1318,7 @@ TEST_F(DirectCompositionPixelTest, SwapChainImage) {
 
     SkColor expected_color = SK_ColorRED;
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
     EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
         << std::hex << "Expected " << expected_color << " Actual "
         << actual_color;
@@ -1384,7 +1343,7 @@ TEST_F(DirectCompositionPixelTest, SwapChainImage) {
 
     SkColor expected_color = SK_ColorGREEN;
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
     EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
         << std::hex << "Expected " << expected_color << " Actual "
         << actual_color;
@@ -1407,7 +1366,7 @@ TEST_F(DirectCompositionPixelTest, SwapChainImage) {
 
     SkColor expected_color = SK_ColorRED;
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
     EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
         << std::hex << "Expected " << expected_color << " Actual "
         << actual_color;
@@ -1430,7 +1389,7 @@ TEST_F(DirectCompositionPixelTest, SwapChainImage) {
 
     SkColor expected_color = SK_ColorRED;
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), gfx::Point(75, 75));
     EXPECT_TRUE(AreColorsSimilar(expected_color, actual_color))
         << std::hex << "Expected " << expected_color << " Actual "
         << actual_color;
@@ -1523,7 +1482,7 @@ TEST_F(DirectCompositionPixelTest, RootSurfaceDrawOffset) {
 
   for (const auto& test_case : test_cases) {
     SkColor actual_color =
-        ReadBackWindowPixel(window_.hwnd(), test_case.position);
+        GLTestHelper::ReadBackWindowPixel(window_.hwnd(), test_case.position);
     EXPECT_TRUE(AreColorsSimilar(test_case.expected_color, actual_color))
         << std::hex << "Expected " << test_case.expected_color << " Actual "
         << actual_color;
