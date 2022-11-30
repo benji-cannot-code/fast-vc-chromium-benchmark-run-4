90d9093bf2d9f74da6dc7597eb098ccc20dfbe5d
Jan Keitel <jkeitel@google.com>
2022-11-30 12:57:57 +0000

    [CodeHealth] Use base::Value v2 API in HaTS view.
    Bug: 1303949
    Change-Id: I15cae31b1a8d7831113e538925023cc38fdb6fc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061129
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077422}

--- chrome/browser/ui/views/hats/hats_next_web_dialog.cc
+++ chrome/browser/ui/views/hats/hats_next_web_dialog.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64url.h"
 #include "base/json/json_writer.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_destroyer.h"
@@ -227,11 +228,11 @@ GURL HatsNextWebDialog::GetParameterizedHatsURL() const {
 
   // Append any Product Specific Data to the query. This will be interpreted
   // by the wrapper website and provided to the HaTS backend service.
-  base::DictionaryValue dict;
+  base::Value::Dict dict;
   for (const auto& field_value : product_specific_bits_data_)
-    dict.SetStringKey(field_value.first, field_value.second ? "true" : "false");
+    dict.Set(field_value.first, field_value.second ? "true" : "false");
   for (const auto& field_value : product_specific_string_data_)
-    dict.SetStringKey(field_value.first, field_value.second);
+    dict.Set(field_value.first, field_value.second);
 
   std::string product_specific_data_json;
   base::JSONWriter::Write(dict, &product_specific_data_json);
