203047e6bc743ae35b8e32a9e0eeb25ec7093839
Ben Wagner <bungeman@chromium.org>
2022-11-30 18:45:35 +0000

    Initialize discardable memory for pdf fuzzers
    Chromium builds Skia to use discarable memory by building in
    `skia/ext/SkDiscardableMemory_chrome.cc`. For this to work the process
    must initialize the discardable memory allocator by calling
    `DiscardableMemoryAllocator::SetInstance`. Implement a
    `LLVMFuzzerInitialize` to properly initialize the pdf fuzzer process.
    
    Bug: chromium:1393708
    Change-Id: I19e773711f351acc76514291ab1e96df0d081e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067799
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077592}

---
+++ pdf/pdfium/fuzzers/initialize_process.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/process/memory.h"
+#include "base/test/test_discardable_memory_allocator.h"
+
+namespace {
+base::TestDiscardableMemoryAllocator g_discardable_memory_allocator;
+}
+
+extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  base::EnableTerminationOnOutOfMemory();
+  base::DiscardableMemoryAllocator::SetInstance(
+      &g_discardable_memory_allocator);
+  return 0;
+}
