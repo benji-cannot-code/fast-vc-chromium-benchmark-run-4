4ab13931cdc3f59ba72d63887793f4a5d6f0831f
Ashley Prasad <ashleydp@google.com>
2022-12-01 02:05:21 +0000

    audio-settings: enable using fake in ui
    Enable using a fake version of the mojo interface in the UI while the
    "Audio Settings" feature is under development.
    
    - Move fake into device_page directory and change to TS file.
    - Export fakeCrosAudioConfig from os_settings.js to ensure it is
      available on optimized builds.
    - Update BUILD files for new location.
    - Ensure tests work.
    
    Bug: b:260277007
    Test: browser_tests --gtest_filter=*OSSettings*
    Change-Id: I61d169fada236e8307a536fd76930f64715e57bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044624
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077763}

--- chrome/test/data/webui/settings/chromeos/device_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/device_page_tests.js
@@ -5,18 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 
-import {crosAudioConfigMojomWebui, DevicePageBrowserProxyImpl, IdleBehavior, LidClosedBehavior, NoteAppLockScreenSupport, Router, routes, setCrosAudioConfigForTesting, setDisplayApiForTesting, StorageSpaceState} from 'chrome://os-settings/chromeos/os_settings.js';
+import {crosAudioConfigMojomWebui, DevicePageBrowserProxyImpl, fakeCrosAudioConfig, IdleBehavior, LidClosedBehavior, NoteAppLockScreenSupport, Router, routes, setCrosAudioConfigForTesting, setDisplayApiForTesting, StorageSpaceState} from 'chrome://os-settings/chromeos/os_settings.js';
+import {getDeepActiveElement} from 'chrome://resources/ash/common/util.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {getDeepActiveElement} from 'chrome://resources/ash/common/util.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks, waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-
-import {crosAudioConfigActiveFakeSpeaker, crosAudioConfigDefaultFakeMicJack, crosAudioConfigDefaultFakeSpeaker, crosAudioConfigInactiveFakeMicJack, defaultFakeAudioSystemProperties, FakeCrosAudioConfig} from './fake_cros_audio_config.js';
 import {FakeSystemDisplay} from './fake_system_display.js';
 import {TestDevicePageBrowserProxy} from './test_device_page_browser_proxy.js';
 
@@ -533,8 +531,8 @@ suite('SettingsDevicePage', function() {
 
       /** @type {!Array<!AudioDevice>} */
       outputDevices: [
-        crosAudioConfigDefaultFakeSpeaker,
-        crosAudioConfigDefaultFakeMicJack,
+        fakeCrosAudioConfig.defaultFakeSpeaker,
+        fakeCrosAudioConfig.defaultFakeMicJack,
       ],
     };
 
@@ -547,8 +545,8 @@ suite('SettingsDevicePage', function() {
 
       /** @type {!Array<!AudioDevice>} */
       outputDevices: [
-        crosAudioConfigDefaultFakeSpeaker,
-        crosAudioConfigDefaultFakeMicJack,
+        fakeCrosAudioConfig.defaultFakeSpeaker,
+        fakeCrosAudioConfig.defaultFakeMicJack,
       ],
     };
 
@@ -561,8 +559,8 @@ suite('SettingsDevicePage', function() {
 
       /** @type {!Array<!AudioDevice>} */
       outputDevices: [
-        crosAudioConfigDefaultFakeSpeaker,
-        crosAudioConfigDefaultFakeMicJack,
+        fakeCrosAudioConfig.defaultFakeSpeaker,
+        fakeCrosAudioConfig.defaultFakeMicJack,
       ],
     };
 
@@ -575,8 +573,8 @@ suite('SettingsDevicePage', function() {
 
       /** @type {!Array<!AudioDevice>} */
       outputDevices: [
-        crosAudioConfigDefaultFakeSpeaker,
-        crosAudioConfigDefaultFakeMicJack,
+        fakeCrosAudioConfig.defaultFakeSpeaker,
+        fakeCrosAudioConfig.defaultFakeMicJack,
       ],
     };
 
@@ -600,8 +598,8 @@ suite('SettingsDevicePage', function() {
 
       /** @type {!Array<!AudioDevice>} */
       outputDevices: [
-        crosAudioConfigActiveFakeSpeaker,
-        crosAudioConfigInactiveFakeMicJack,
+        fakeCrosAudioConfig.fakeSpeakerActive,
+        fakeCrosAudioConfig.fakeMicJackInactive,
       ],
     };
 
@@ -612,7 +610,7 @@ suite('SettingsDevicePage', function() {
       await init();
 
       // FakeAudioConfig must be set before audio subpage is loaded.
-      crosAudioConfig = new FakeCrosAudioConfig();
+      crosAudioConfig = new fakeCrosAudioConfig.FakeCrosAudioConfig();
       setCrosAudioConfigForTesting(crosAudioConfig);
       return showAndGetDeviceSubpage('audio', routes.AUDIO)
           .then(function(page) {
@@ -627,7 +625,8 @@ suite('SettingsDevicePage', function() {
 
       // Test default properties.
       assertEquals(
-          defaultFakeAudioSystemProperties.outputVolumePercent,
+          fakeCrosAudioConfig.defaultFakeAudioSystemProperties
+              .outputVolumePercent,
           outputVolumeSlider.value);
 
       // Test min volume case.
@@ -714,10 +713,11 @@ suite('SettingsDevicePage', function() {
 
       // Test default properties.
       assertEquals(
-          crosAudioConfigDefaultFakeMicJack.id,
+          fakeCrosAudioConfig.defaultFakeMicJack.id,
           BigInt(outputDeviceDropdown.value));
       assertEquals(
-          defaultFakeAudioSystemProperties.outputDevices.length,
+          fakeCrosAudioConfig.defaultFakeAudioSystemProperties.outputDevices
+              .length,
           outputDeviceDropdown.length);
 
       // Test empty output devices case.
@@ -734,7 +734,7 @@ suite('SettingsDevicePage', function() {
           activeSpeakerFakeAudioSystemProperties);
       await flushTasks();
       assertEquals(
-          crosAudioConfigActiveFakeSpeaker.id,
+          fakeCrosAudioConfig.fakeSpeakerActive.id,
           BigInt(outputDeviceDropdown.value));
       assertEquals(
           activeSpeakerFakeAudioSystemProperties.outputDevices.length,
