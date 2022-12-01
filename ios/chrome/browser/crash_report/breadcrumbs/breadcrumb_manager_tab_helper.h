ad332466f037e0c6c5b8d2ed536a94cd43286c64
Justin <justincohen@google.com>
2022-12-01 21:52:46 +0000

    ios: Fix breadcrumb scroll events and realized webstates.
    Fixed: 1394892
    Change-Id: I269092c6bf89c83f5c8c300678f358580d4b9589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066709
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078299}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_CRASH_REPORT_BREADCRUMBS_BREADCRUMB_MANAGER_TAB_HELPER_H_
 #define IOS_CHROME_BROWSER_CRASH_REPORT_BREADCRUMBS_BREADCRUMB_MANAGER_TAB_HELPER_H_
 
+#include "base/sequence_checker.h"
 #include "components/breadcrumbs/core/breadcrumb_manager_tab_helper.h"
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
@@ -28,6 +29,7 @@ class BreadcrumbManagerTabHelper
       delete;
 
  private:
+  SEQUENCE_CHECKER(sequence_checker_);
   friend class web::WebStateUserData<BreadcrumbManagerTabHelper>;
 
   explicit BreadcrumbManagerTabHelper(web::WebState* web_state);
@@ -46,6 +48,11 @@ class BreadcrumbManagerTabHelper
   void DidChangeVisibleSecurityState(web::WebState* web_state) override;
   void RenderProcessGone(web::WebState* web_state) override;
   void WebStateDestroyed(web::WebState* web_state) override;
+  void WebStateRealized(web::WebState* web_state) override;
+
+  // Helpers used to create and respond to the webState scrollViewProxy.
+  void CreateBreadcrumbScrollingObserver();
+  void OnScrollEvent(const std::string& event);
 
   // The webstate associated with this tab helper.
   web::WebState* web_state_ = nullptr;
@@ -58,6 +65,8 @@ class BreadcrumbManagerTabHelper
   __strong id<CRWWebViewScrollViewProxyObserver> scroll_observer_;
 
   WEB_STATE_USER_DATA_KEY_DECL();
+
+  base::WeakPtrFactory<BreadcrumbManagerTabHelper> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_CRASH_REPORT_BREADCRUMBS_BREADCRUMB_MANAGER_TAB_HELPER_H_
