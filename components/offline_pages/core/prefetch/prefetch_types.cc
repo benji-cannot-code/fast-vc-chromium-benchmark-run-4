ff503987113fc3b292e18a0ef843309e8bed5dca
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-12-01 21:53:10 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=harringtond@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: Id5bb0557085013f1e787061cab6c65c00f8af94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057388
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078300}

--- components/offline_pages/core/prefetch/prefetch_types.cc
+++ components/offline_pages/core/prefetch/prefetch_types.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/offline_pages/core/prefetch/prefetch_types.h"
 
+#include <ostream>
+
 namespace offline_pages {
 
 namespace {
