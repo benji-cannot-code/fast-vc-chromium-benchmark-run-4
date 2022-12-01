00a8761aa582fc1a2abd97832bed0a083ff78741
jhtin <jhtin@chromium.org>
2022-12-01 01:18:30 +0000

    longpress diacritics: Modifications to fit new ux spec better
    go/screenshot/7zvVxH6XTzP5LzU
    
    __SIMPLE_REVIEW__
    
    Bug: b/259147634
    Change-Id: Id461a1b1ed7269b273e2ebfd9c40d22d51a8857c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065063
    Auto-Submit: jhtin <jhtin@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077718}

--- chrome/browser/ash/input_method/ui/indexed_suggestion_candidate_button.cc
+++ chrome/browser/ash/input_method/ui/indexed_suggestion_candidate_button.cc
@@ -14,20 +14,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/flex_layout.h"
 
 namespace ui::ime {
-const int kPadding = 4;
-const int kBetweenSpacing = 8;
+const int kTopBottomPadding = 4;
+const int kLeftRightPadding = 2;
+const int kBetweenSpacing = 4;
 const int kBorderRadius = 2;
-const int kCandidateSquareSide = 20;
+const int kCandidateSquareSide = 24;
 const views::Label::CustomFont kCandidateTextFont = {
     .font_list = gfx::FontList(gfx::FontList({"Roboto"},
                                              gfx::Font::NORMAL,
-                                             14,
+                                             16,
                                              gfx::Font::Weight::MEDIUM))};
 const views::Label::CustomFont kIndexFont = {
     .font_list = gfx::FontList(gfx::FontList({"Roboto"},
                                              gfx::Font::NORMAL,
                                              10,
-                                             gfx::Font::Weight::NORMAL))};
+                                             gfx::Font::Weight::MEDIUM))};
 
 IndexedSuggestionCandidateButton::IndexedSuggestionCandidateButton(
     PressedCallback callback,
@@ -37,7 +38,10 @@ IndexedSuggestionCandidateButton::IndexedSuggestionCandidateButton(
     : views::Button(std::move(callback)) {
   SetAccessibleName(candidate_text);
   SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical, gfx::Insets(kPadding),
+      views::BoxLayout::Orientation::kVertical,
+      gfx::Insets()
+          .set_left_right(kLeftRightPadding, kLeftRightPadding)
+          .set_top_bottom(kTopBottomPadding, kTopBottomPadding),
       /* between_child_spacing=*/kBetweenSpacing));
 
   if (create_legacy_candidate) {
