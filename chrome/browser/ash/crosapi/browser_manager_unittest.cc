94b45a32e7149372ebcbb569da3bb3d51d9654d5
Henrique Ferreiro <hferreiro@igalia.com>
2022-12-01 11:41:18 +0000

    Add some new rules to disallow mixing Ash/Lacros code
    Ash and Lacros code shouldn't depend on each other, as they are part of
    different binaries in ChromeOS. Unfortunately, many code from those
    projects have a common directory ancestor, which allows for them to
    include each other, unless explicitly disallowed. This CL makes adds
    additional DEPS rules that disallow those includes.
    
    Bug: 1164001
    Change-Id: Ic15724dc23374a5f5d966c46ad97db6211c7e151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065966
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1077979}

--- chrome/browser/ash/crosapi/browser_manager_unittest.cc
+++ chrome/browser/ash/crosapi/browser_manager_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/component_updater/fake_cros_component_manager.h"
-#include "chrome/browser/lacros/browser_service_lacros.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
