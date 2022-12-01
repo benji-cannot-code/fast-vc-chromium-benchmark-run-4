2e70e37d42c59634de9dcc56b30f507ef666c9f1
Andrew Paseltiner <apaseltiner@chromium.org>
2022-12-01 22:00:30 +0000

    Encapsulate FrozenContext fields and rename it to ReceiverContext
    "Frozen" was a misleading name, as some of the fields are mutated
    during normal operation of the API.
    
    Encapsulation makes it clear which fields are logically const and for
    the ones that are mutable, the ways in which they are allowed to be
    mutated.
    
    Change-Id: I3b77b202ff5d285cdf2f4dfceb192481418b1258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4070347
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078304}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.h
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.h
@@ -37,10 +37,10 @@ namespace content {
 class AttributionManager;
 
 // Manages a receiver set of all ongoing `AttributionDataHost`s and forwards
-// events to the AttributionManager which owns `this`. Because attributionsrc
+// events to the `AttributionManager` that owns `this`. Because attributionsrc
 // requests may continue until after we have detached a frame, all browser
-// process data needed to validate sources/triggers is frozen and stored
-// alongside each receiver.
+// process data needed to validate sources/triggers is stored alongside each
+// receiver.
 class CONTENT_EXPORT AttributionDataHostManagerImpl
     : public AttributionDataHostManager,
       public blink::mojom::AttributionDataHost {
@@ -80,9 +80,8 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl
       const blink::AttributionSrcToken& attribution_src_token) override;
 
  private:
-  // Represents frozen data from the browser process associated with each
-  // receiver.
-  struct FrozenContext;
+  class ReceiverContext;
+
   struct DelayedTrigger;
   struct NavigationDataHost;
 
@@ -111,7 +110,7 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl
   // Owns `this`.
   raw_ptr<AttributionManager> attribution_manager_;
 
-  mojo::ReceiverSet<blink::mojom::AttributionDataHost, FrozenContext>
+  mojo::ReceiverSet<blink::mojom::AttributionDataHost, ReceiverContext>
       receivers_;
 
   // Map which stores pending receivers for data hosts which are going to
