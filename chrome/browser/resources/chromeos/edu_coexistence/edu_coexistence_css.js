e37a00500516121223a186767013d9aadb115fcc
dpapad <dpapad@chromium.org>
2022-12-01 19:47:52 +0000

    Enable TrustedTypes checks on chrome://chrome-signin
    Bug: 1098690
    Change-Id: Ic51a03323ab34cbccc56c97086cbf477d0c56373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062462
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078223}

--- chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_css.js
+++ chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_css.js
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
-const template = document.createElement('template');
-template.innerHTML = `
-<dom-module id="edu-coexistence-css">{__html_template__}</dom-module>
-`;
-document.body.appendChild(template.content.cloneNode(true));
+
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+const styleMod = document.createElement('dom-module');
+styleMod.appendChild(html`{__html_template__}`.content);
+styleMod.register('edu-coexistence-css');
