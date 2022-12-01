2152668e66a59d1bddf01a25c31fef594cfb2713
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-12-01 17:30:43 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    This CL was uploaded by git cl split.
    
    R=drubery@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I08eba982386106de538b7064faf58f6d3c2dbcc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057390
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078133}

--- components/security_interstitials/content/insecure_form_blocking_page.cc
+++ components/security_interstitials/content/insecure_form_blocking_page.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/security_interstitials/content/insecure_form_blocking_page.h"
 
+#include <ostream>
+
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
