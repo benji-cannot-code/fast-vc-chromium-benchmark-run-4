7bd111a558e11fdb612a0f449fb032f66fc1b1f0
James Cook <jamescook@chromium.org>
2022-12-01 01:10:45 +0000

    Add rounded containers to quick settings VPN detail page
    Add a rounded container for each "provider" (VPN app), followed by
    another rounded container for the list of networks under each provider.
    
    https://screenshot.googleplex.com/8XKk2FBhpGaEJR7
    
    Style/spacing updates will come in a later CL.
    
    Bug: b:260620957
    Change-Id: I68e08154fe598384c211e1b4f74719588211a531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064632
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077713}

--- ash/system/network/vpn_list_view.h
+++ ash/system/network/vpn_list_view.h
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "ash/ash_export.h"
 #include "ash/system/network/network_state_list_detailed_view.h"
 #include "ash/system/network/vpn_list.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 
 class PrefRegistrySimple;
 
@@ -35,9 +37,11 @@ namespace ash {
 // attempt. Clicking on the currently connected or connecting network shows its
 // configuration dialog. Clicking on a provider shows the provider's "add
 // network" dialog.
-class VPNListView : public NetworkStateListDetailedView,
-                    public VpnList::Observer {
+class ASH_EXPORT VPNListView : public NetworkStateListDetailedView,
+                               public VpnList::Observer {
  public:
+  METADATA_HEADER(VPNListView);
+
   using VpnProviderPtr = chromeos::network_config::mojom::VpnProviderPtr;
 
   VPNListView(DetailedViewDelegate* delegate, LoginStatus login);
@@ -57,16 +61,21 @@ class VPNListView : public NetworkStateListDetailedView,
   // See Shell::RegisterProfilePrefs().
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
-  // views::View:
-  const char* GetClassName() const override;
-
  private:
+  friend class VPNListViewTest;
+
   using NetworkStateList =
       std::vector<chromeos::network_config::mojom::NetworkStatePropertiesPtr>;
   void OnGetNetworkStateList(NetworkStateList networks);
 
-  // Adds a network to the list.
+  // Adds a `network` to the list in `container`.
   void AddNetwork(
+      const chromeos::network_config::mojom::NetworkStateProperties* network,
+      views::View* container);
+
+  // Adds a `network` to the list in its own unnested container. Some ARC VPNs
+  // do not have an associated VPN provider entry and are listed this way.
+  void AddUnnestedNetwork(
       const chromeos::network_config::mojom::NetworkStateProperties* network);
 
   // Adds the VPN provider identified by |vpn_provider| to the list, along with
