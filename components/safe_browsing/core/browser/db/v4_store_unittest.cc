e605687342a11fbd36bd40f798f2b14f796bc28f
Clark DuVall <cduvall@chromium.org>
2022-12-01 17:50:29 +0000

    Fix V4Store file size calculation after migration
    The hash list would be double counted in the file size after a migration
    to the mmap mode because the hashes would be counted both in the proto
    and the separate file.
    
    Bug: 1380507
    Change-Id: I20ff60c76a73b5735c197dd7bd81785b4215f595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4052081
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078146}

--- components/safe_browsing/core/browser/db/v4_store_unittest.cc
+++ components/safe_browsing/core/browser/db/v4_store_unittest.cc
@@ -1008,6 +1008,8 @@ TEST_F(V4StoreTest, MigrateToMmap) {
                                  file_format.hash_files(0).extension()),
       &contents));
   EXPECT_EQ(contents, kHash);
+  EXPECT_EQ(mmap_store.file_size(),
+            static_cast<int64_t>(proto_contents.size() + kHash.size()));
 
   // Reading again should not migrate.
   base::Time last_modified = GetLastModifiedTime(store_path_);
@@ -1051,6 +1053,10 @@ TEST_F(V4StoreTest, MigrateToInMemory) {
   EXPECT_EQ(in_memory_store.GetMatchingHashPrefix(kFullHash), kHash);
 
   EXPECT_FALSE(base::PathExists(hashes_path));
+
+  int64_t file_size;
+  EXPECT_TRUE(base::GetFileSize(store_path_, &file_size));
+  EXPECT_EQ(in_memory_store.file_size(), file_size);
 
   // Reading again should not migrate.
   base::Time last_modified = GetLastModifiedTime(store_path_);
