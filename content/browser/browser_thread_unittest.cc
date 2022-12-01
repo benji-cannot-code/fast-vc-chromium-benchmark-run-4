14527e8a1cfe005cc2f73ebb2a5f821677e48f20
Gabriel Charette <gab@chromium.org>
2022-12-01 17:53:29 +0000

    [base][codehealth] Disallow direct creation of SequenceManagerImpl
    This was initially going to be a prereq for crrev.com/c/4060840
    as I wanted to capture unbound creation. Turns out this won't be
    sufficient but this is still good code health.
    
    The only true use case for owning a SequenceManagerImpl* is in
    thread.cc. Special cased that one with a PassKey.
    
    Bug: 1344573
    Change-Id: I1fbb4d97320c6048aff29fdab0a189e7f2071d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061780
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078149}

--- content/browser/browser_thread_unittest.cc
+++ content/browser/browser_thread_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/task/current_thread.h"
-#include "base/task/sequence_manager/sequence_manager_impl.h"
+#include "base/task/sequence_manager/sequence_manager.h"
 #include "base/task/sequenced_task_runner_helpers.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/mock_callback.h"
@@ -42,8 +42,7 @@ class SequenceManagerThreadDelegate : public base::Thread::Delegate {
  public:
   SequenceManagerThreadDelegate() {
     ui_sequence_manager_ =
-        base::sequence_manager::internal::SequenceManagerImpl::CreateUnbound(
-            base::sequence_manager::SequenceManager::Settings());
+        base::sequence_manager::CreateUnboundSequenceManager();
     auto browser_ui_thread_scheduler =
         BrowserUIThreadScheduler::CreateForTesting(ui_sequence_manager_.get());
 
