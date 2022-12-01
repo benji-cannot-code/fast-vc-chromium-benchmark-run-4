f74ba6328c7fdb27f5d5072fbd7144e579fe4781
Viktor Semeniuk <vsemeniuk@google.com>
2022-12-01 11:42:26 +0000

    Replacing PasswordForm with CredentialUIEntry inside OnEdited
    Whenever a password is updated through SavedPasswordsPresenter,
    OnEdited is invoked. It is used to check for leaks/weak of edited
    credential.
    
    Before this change: it was invoked for all the duplicates in all cases.
    After this change: it is invoked only once and only if username or
    password values were changed.
    
    Bug: 1381203
    Change-Id: I8952fe074a07e5ce9325dc4bc444164a9000cbb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066642
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077981}

--- components/password_manager/core/browser/ui/bulk_leak_check_service_adapter.h
+++ components/password_manager/core/browser/ui/bulk_leak_check_service_adapter.h
@@ -15,8 +15,6 @@ class PrefService;
 
 namespace password_manager {
 
-struct PasswordForm;
-
 // This class serves as an apdater for the BulkLeakCheckService and exposes an
 // API that is intended to be consumed from the settings page.
 class BulkLeakCheckServiceAdapter : public SavedPasswordsPresenter::Observer {
@@ -46,7 +44,7 @@ class BulkLeakCheckServiceAdapter : public SavedPasswordsPresenter::Observer {
 
  private:
   // SavedPasswordsPresenter::Observer:
-  void OnEdited(const PasswordForm& form) override;
+  void OnEdited(const CredentialUIEntry& form) override;
 
   // Weak handles to a presenter and service, respectively. These must be not
   // null and must outlive the adapter.
--- components/password_manager/core/browser/ui/saved_passwords_presenter.cc
+++ components/password_manager/core/browser/ui/saved_passwords_presenter.cc
@@ -420,7 +420,11 @@ SavedPasswordsPresenter::EditSavedCredentials(
     } else {
       store.UpdateLogin(new_form);
     }
-    NotifyEdited(new_form);
+  }
+
+  // Only change in username or password is interesting for OnEdited listeners.
+  if (username_changed || password_changed) {
+    NotifyEdited(updated_credential);
   }
 
   password_manager::metrics_util::LogPasswordEditResult(username_changed,
@@ -494,9 +498,10 @@ void SavedPasswordsPresenter::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
-void SavedPasswordsPresenter::NotifyEdited(const PasswordForm& password) {
+void SavedPasswordsPresenter::NotifyEdited(
+    const CredentialUIEntry& credential) {
   for (auto& observer : observers_)
-    observer.OnEdited(password);
+    observer.OnEdited(credential);
 }
 
 void SavedPasswordsPresenter::NotifySavedPasswordsChanged() {
