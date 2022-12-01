2fe0843d20d2008c1538d2190b63bdd239d9f8a1
Taylor Bergquist <tbergquist@chromium.org>
2022-12-01 02:51:25 +0000

    Fix TabContainer not updating when its overall bounds view animates.
    Bug: 1383933
    Change-Id: If65eb0cd6f2d9c4dfd9fd6ba8303a477236fa1cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067892
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077819}

--- chrome/browser/ui/views/tabs/tab_container_impl.h
+++ chrome/browser/ui/views/tabs/tab_container_impl.h
@@ -153,7 +153,7 @@ class TabContainerImpl : public TabContainer,
   void MouseMovedOutOfHost() override;
 
   // views::BoundsAnimatorObserver:
-  void OnBoundsAnimatorProgressed(views::BoundsAnimator* animator) override {}
+  void OnBoundsAnimatorProgressed(views::BoundsAnimator* animator) override;
   void OnBoundsAnimatorDone(views::BoundsAnimator* animator) override;
 
  private:
