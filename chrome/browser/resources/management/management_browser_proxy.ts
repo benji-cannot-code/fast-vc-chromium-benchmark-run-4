af44427c2c913d090f4f20dc875315ab8dab1c04
Andreea Costinas <acostinas@google.com>
2022-12-01 10:33:24 +0000

    Show privacy message in chrome://management for DoH with identifiers
    If the admin has configured a proxy via policy, a privacy disclosure
    message is added to the transparency panel to inform users that the
    network traffic may be monitored.
    This CL shows the same message when secure DNS templates with
    identifiers are configured.
    
    http://go/cros-dohidentifiers-ux
    
    Bug: b/243904884
    Change-Id: I56027dc1a0b852a125fef4c8df56c58f68549552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061668
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077954}

--- chrome/browser/resources/management/management_browser_proxy.ts
+++ chrome/browser/resources/management/management_browser_proxy.ts
@@ -35,7 +35,7 @@ interface ManagedDataResponse {
   showUpdateRequiredEol: boolean;
   eolMessage: string;
   eolAdminMessage: string;
-  showProxyServerPrivacyDisclosure: boolean;
+  showMonitoredNetworkPrivacyDisclosure: boolean;
 }
 
 interface ThreatProtectionPermission {
--- chrome/browser/ui/webui/management/management_ui_handler.cc
+++ chrome/browser/ui/webui/management/management_ui_handler.cc
@@ -66,6 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/cros_settings.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
+#include "chrome/browser/net/stub_resolver_config_reader.h"
+#include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/ui/webui/management/management_ui_handler_chromeos.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
@@ -743,9 +745,22 @@ void ManagementUIHandler::AddUpdateRequiredEolInfo(
   response->Set("eolAdminMessage", eol_admin_message);
 }
 
-void ManagementUIHandler::AddProxyServerPrivacyDisclosure(
+void ManagementUIHandler::AddMonitoredNetworkPrivacyDisclosure(
     base::Value::Dict* response) const {
-  bool showProxyDisclosure = false;
+  bool showMonitoredNetworkDisclosure = false;
+
+  // Check for secure DNS templates with identifiers.
+  showMonitoredNetworkDisclosure =
+      SystemNetworkContextManager::GetStubResolverConfigReader()
+          ->GetDohWithIdentifiersDisplayServers()
+          .has_value();
+  if (showMonitoredNetworkDisclosure) {
+    response->Set("showMonitoredNetworkPrivacyDisclosure",
+                  showMonitoredNetworkDisclosure);
+    return;
+  }
+
+  // Check for proxy config.
   ash::NetworkHandler* network_handler = ash::NetworkHandler::Get();
   base::Value::Dict proxy_settings;
   // |ui_proxy_config_service| may be missing in tests. If the device is offline
@@ -766,10 +781,12 @@ void ManagementUIHandler::AddProxyServerPrivacyDisclosure(
         proxy_settings.FindStringByDottedPath(base::JoinString(
             {::onc::network_config::kType, ::onc::kAugmentationActiveSetting},
             "."));
-    showProxyDisclosure = proxy_specification_mode &&
-                          *proxy_specification_mode != ::onc::proxy::kDirect;
+    showMonitoredNetworkDisclosure =
+        proxy_specification_mode &&
+        *proxy_specification_mode != ::onc::proxy::kDirect;
   }
-  response->Set("showProxyServerPrivacyDisclosure", showProxyDisclosure);
+  response->Set("showMonitoredNetworkPrivacyDisclosure",
+                showMonitoredNetworkDisclosure);
 }
 
 // static
@@ -816,7 +833,7 @@ base::Value::Dict ManagementUIHandler::GetContextualManagedData(
   if (enterprise_manager.empty())
     enterprise_manager = GetAccountManager(profile);
   AddUpdateRequiredEolInfo(&response);
-  AddProxyServerPrivacyDisclosure(&response);
+  AddMonitoredNetworkPrivacyDisclosure(&response);
 #else
   std::string enterprise_manager = GetAccountManager(profile);
 
