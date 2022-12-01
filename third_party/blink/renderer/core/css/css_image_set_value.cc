4ab790b6a27fd0796b78e405784c816010cf4f72
Traian Captan <tcaptan@chromium.org>
2022-12-01 02:12:16 +0000

    Unprefix -webkit-image-set
    Currently Chrome only supports '-webkit-' prefixed image-set.
    With this change, the current image-set functionality is exposed
    without needing the '-webkit-' prefix.
    
    If both prefixed and standard versions are defined in the right order,
    and the standard version fails parsing,
    Chrome will fallback to the prefixed version.
    The `image-set-fallback` test has been added to verify this behavior.
    
    Tests related to image-set, and their corresponding expectations,
    have been updated to test both the old prefixed functionality
    and the new unprefixed one.
    
    Some tests have been renamed for better naming consistency.
    
    R=masonf, pdr
    
    Fixed: 630597
    Change-Id: I990bdee8aad5b2f8876ba183255f9d90497cd1ce
    Low-Coverage-Reason: NOTREACHED default case of switch statement
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063134
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077774}

--- third_party/blink/renderer/core/css/css_image_set_value.cc
+++ third_party/blink/renderer/core/css/css_image_set_value.cc
@@ -121,7 +121,11 @@ StyleImage* CSSImageSetValue::CacheImage(
 
 String CSSImageSetValue::CustomCSSText() const {
   StringBuilder result;
-  result.Append("-webkit-image-set(");
+
+  if (is_webkit_prefixed_)
+    result.Append("-webkit-");
+
+  result.Append("image-set(");
 
   wtf_size_t length = this->length();
   wtf_size_t i = 0;
@@ -169,6 +173,10 @@ CSSImageSetValue* CSSImageSetValue::ValueWithURLsMadeAbsolute() {
     image_value ? value->Append(*image_value->ValueWithURLMadeAbsolute())
                 : value->Append(*item);
   }
+
+  if (is_webkit_prefixed_)
+    value->MarkWebkitPrefixed();
+
   return value;
 }
 
--- third_party/blink/renderer/core/css/parser/css_property_parser_test.cc
+++ third_party/blink/renderer/core/css/parser/css_property_parser_test.cc
@@ -663,6 +663,16 @@ TEST_F(CSSPropertyUseCounterTest, CSSPropertyBackgroundImageWebkitImageSet) {
   EXPECT_TRUE(IsCounted(feature));
 }
 
+TEST_F(CSSPropertyUseCounterTest, CSSPropertyBackgroundImageImageSet) {
+  WebFeature feature = WebFeature::kImageSet;
+
+  ParseProperty(CSSPropertyID::kBackgroundImage, "none");
+  EXPECT_FALSE(IsCounted(feature));
+
+  ParseProperty(CSSPropertyID::kBackgroundImage, "image-set(url(foo) 2x)");
+  EXPECT_TRUE(IsCounted(feature));
+}
+
 TEST(CSSPropertyParserTest, InternalLightDarkAuthor) {
   auto* context = MakeGarbageCollected<CSSParserContext>(
       kHTMLStandardMode, SecureContextMode::kInsecureContext);
@@ -728,6 +738,7 @@ TEST(CSSPropertyParserTest, UAInternalLightDarkBackgroundImage) {
       {"-internal-light-dark(url(light.png), url(dark.png))", true},
       {"-internal-light-dark(url(light.png), none)", true},
       {"-internal-light-dark(none, -webkit-image-set(url(dark.png) 1x))", true},
+      {"-internal-light-dark(none, image-set(url(dark.png) 1x))", true},
       {"-internal-light-dark(  none  ,  none   )", true},
       {"-internal-light-dark(  url(light.png)  ,  url(dark.png)   )", true},
   };
@@ -771,6 +782,7 @@ TEST(CSSPropertyParserTest, UAInternalLightDarkBackgroundShorthand) {
       {"-internal-light-dark(url(light.png), url(dark.png))", true},
       {"-internal-light-dark(url(light.png), none)", true},
       {"-internal-light-dark(none, -webkit-image-set(url(dark.png) 1x))", true},
+      {"-internal-light-dark(none, image-set(url(dark.png) 1x))", true},
       {"-internal-light-dark(  none  ,  none   )", true},
       {"-internal-light-dark(  url(light.png)  ,  url(dark.png)   )", true},
   };
