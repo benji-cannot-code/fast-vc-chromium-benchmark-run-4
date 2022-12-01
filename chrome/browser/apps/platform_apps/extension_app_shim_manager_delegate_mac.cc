dd8aacea0d116a0e4c458b7fbc15a697c7c8c1bb
Marijn Kruisselbrink <mek@chromium.org>
2022-12-01 02:26:33 +0000

    Remove --app-shim-error command line switch.
    This switch is used by an app-shim to launch chrome when it fails to
    dlopen chrome, if the shim was launched by chrome, to tell chrome
    to rebuild the app shim and try launching it again.
    
    However, when Chrome is what launches the app shim, it already
    detects early termination of the app shim, triggering a rebuild and
    relaunch that way. Furthermore this switch never worked right for
    PWAs.
    
    So rather than trying to fix it for PWAs, this just removes the
    switch entirely.
    
    Bug: 1385016
    Include-Ci-Only-Tests: true
    Change-Id: I6bd7d55aff2730c4da7e3fe439797b0b731b15b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4035010
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077795}

--- chrome/browser/apps/platform_apps/extension_app_shim_manager_delegate_mac.cc
+++ chrome/browser/apps/platform_apps/extension_app_shim_manager_delegate_mac.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/extensions/extension_enable_flow.h"
 #include "chrome/browser/ui/extensions/extension_enable_flow_delegate.h"
 #include "chrome/browser/web_applications/extensions/web_app_extension_shortcut.h"
-#include "chrome/browser/web_applications/extensions/web_app_extension_shortcut_mac.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut_mac.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_metrics.h"
--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -119,7 +119,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "chrome/browser/web_applications/app_shim_registry_mac.h"
-#include "chrome/browser/web_applications/extensions/web_app_extension_shortcut_mac.h"
 #endif
 
 #if BUILDFLAG(IS_WIN)
@@ -991,11 +990,6 @@ bool StartupBrowserCreator::ProcessCmdLineImpl(
     silent_launch = true;
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if BUILDFLAG(IS_MAC)
-  if (web_app::MaybeRebuildShortcut(command_line))
-    return true;
-#endif
 
   if (process_startup == chrome::startup::IsProcessStartup::kNo &&
       command_line.HasSwitch(switches::kDumpBrowserHistograms)) {
