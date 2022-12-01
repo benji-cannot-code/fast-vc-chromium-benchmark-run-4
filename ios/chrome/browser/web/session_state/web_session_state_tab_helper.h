e0a9ddc193eb8e80331395199073a848435f508a
Justin <justincohen@google.com>
2022-12-01 22:24:17 +0000

    ios: Persist tab session restore data when scroll position changes.
    Fixed: 586888
    Change-Id: Idc7953362abb9ba59ace886940cdb5f8fdcb96d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067742
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078317}

--- ios/chrome/browser/web/session_state/web_session_state_tab_helper.h
+++ ios/chrome/browser/web/session_state/web_session_state_tab_helper.h
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "base/sequence_checker.h"
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
+
+@protocol CRWWebViewScrollViewProxyObserver;
 
 class ChromeBrowserState;
 
@@ -43,6 +46,7 @@ class WebSessionStateTabHelper
   void SaveSessionState();
 
  private:
+  SEQUENCE_CHECKER(sequence_checker_);
   friend class web::WebStateUserData<WebSessionStateTabHelper>;
 
   explicit WebSessionStateTabHelper(web::WebState* web_state);
@@ -53,6 +57,11 @@ class WebSessionStateTabHelper
                            web::NavigationContext* navigation_context) override;
   void WebFrameDidBecomeAvailable(web::WebState* web_state,
                                   web::WebFrame* web_frame) override;
+  void WebStateRealized(web::WebState* web_state) override;
+
+  // Helpers used to create and respond to the webState scrollViewProxy.
+  void CreateScrollingObserver();
+  void OnScrollEvent();
 
   ChromeBrowserState* GetBrowserState();
 
@@ -72,7 +81,12 @@ class WebSessionStateTabHelper
   // The WebState with which this object is associated.
   web::WebState* web_state_ = nullptr;
 
+  // Allows observing Objective-C object for Scroll and Zoom events.
+  __strong id<CRWWebViewScrollViewProxyObserver> scroll_observer_;
+
   WEB_STATE_USER_DATA_KEY_DECL();
+
+  base::WeakPtrFactory<WebSessionStateTabHelper> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_WEB_SESSION_STATE_WEB_SESSION_STATE_TAB_HELPER_H_
