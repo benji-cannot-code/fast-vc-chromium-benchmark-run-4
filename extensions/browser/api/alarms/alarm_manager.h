e3e869551ff626d2b054c9534479d45cd5f7df10
David Bertoni <dbertoni@chromium.org>
2022-12-01 04:42:51 +0000

    [Extensions] Modernize the Alarms API and supporting classes.
    This CL makes the Alarm struct moveable and removes uses of std::unique_ptr in the API. Instead, instances of these structs are moved.
    
    Bug: 
    Change-Id: I213e53b6b503bf5481d43503c856543bd6dd0146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025771
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077865}

--- extensions/browser/api/alarms/alarm_manager.h
+++ extensions/browser/api/alarms/alarm_manager.h
@@ -42,6 +42,9 @@ struct Alarm {
         base::TimeDelta min_granularity,
         base::Time now);
 
+  Alarm(Alarm&&) noexcept;
+  Alarm& operator=(Alarm&&) noexcept;
+
   Alarm(const Alarm&) = delete;
   Alarm& operator=(const Alarm&) = delete;
 
@@ -64,7 +67,7 @@ struct Alarm {
 class AlarmManager : public BrowserContextKeyedAPI,
                      public ExtensionRegistryObserver {
  public:
-  using AlarmList = std::vector<std::unique_ptr<Alarm>>;
+  using AlarmList = std::vector<Alarm>;
 
   class Delegate {
    public:
@@ -88,7 +91,7 @@ class AlarmManager : public BrowserContextKeyedAPI,
   // Adds |alarm| for the given extension, and starts the timer. Invokes
   // |callback| when done.
   void AddAlarm(const std::string& extension_id,
-                std::unique_ptr<Alarm> alarm,
+                Alarm alarm,
                 AddAlarmCallback callback);
 
   using GetAlarmCallback = base::OnceCallback<void(Alarm*)>;
@@ -153,7 +156,7 @@ class AlarmManager : public BrowserContextKeyedAPI,
   typedef std::pair<AlarmMap::iterator, AlarmList::iterator> AlarmIterator;
 
   // Part of AddAlarm that is executed after alarms are loaded.
-  void AddAlarmWhenReady(std::unique_ptr<Alarm> alarm,
+  void AddAlarmWhenReady(Alarm alarm,
                          AddAlarmCallback callback,
                          const std::string& extension_id);
 
@@ -189,8 +192,7 @@ class AlarmManager : public BrowserContextKeyedAPI,
   void OnAlarm(AlarmIterator iter);
 
   // Internal helper to add an alarm and start the timer with the given delay.
-  void AddAlarmImpl(const std::string& extension_id,
-                    std::unique_ptr<Alarm> alarm);
+  void AddAlarmImpl(const std::string& extension_id, Alarm alarm);
 
   // Syncs our alarm data for the given extension to/from the state storage.
   void WriteToStorage(const std::string& extension_id);
