352abbd750cbc7d0dfb3af67a53db01ef8282144
Andrey Kosyakov <caseq@chromium.org>
2022-12-01 17:18:56 +0000

    Support directory upload in headless
    Bug: chromedriver:2521
    Change-Id: I39cab04d867a0d1545b49aa9737835163409df58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066986
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078127}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/origin_util.h"
+#include "headless/lib/browser/directory_enumerator.h"
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
@@ -188,6 +189,12 @@ class HeadlessWebContentsImpl::Delegate : public content::WebContentsDelegate {
     return headless_web_contents_->browser_context()
         ->options()
         ->block_new_web_contents();
+  }
+
+  void EnumerateDirectory(content::WebContents* web_contents,
+                          scoped_refptr<content::FileSelectListener> listener,
+                          const base::FilePath& path) override {
+    DirectoryEnumerator::Start(path, std::move(listener));
   }
 
   void RequestToLockMouse(content::WebContents* web_contents,
