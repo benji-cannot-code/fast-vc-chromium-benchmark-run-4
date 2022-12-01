deb4bdb8979c266b545c3e8a8f632acd32abfec2
Noémie St-Onge <noemies@google.com>
2022-12-01 14:35:07 +0000

    Fixed and enabled testPasswordsStateAfterPresentingManagePasswords
    This test was using some wrong element matchers, so those were fixed. The test was also sometimes failing when the entire
    PasswordViewControllerTestCase suite was run. The failure was induced by the `testPasswordGenerationOnManualFallback` test, after which there is sometimes a frozen loading screen appearing. The latter was thus disabled. A bug was filed to reflect this other issue (crbug.com/1394448).
    
    Fixed: 1234759
    Change-Id: I7738730d5fd4163180d823349d54b00f68d806a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051967
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078046}

--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller_constants.h"
 #import "ios/chrome/browser/ui/settings/google_services/google_services_settings_constants.h"
 #import "ios/chrome/browser/ui/settings/import_data_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/password/password_settings/password_settings_constants.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h"
 #import "ios/chrome/browser/ui/settings/price_notifications/price_notifications_constants.h"
 #import "ios/chrome/browser/ui/settings/price_notifications/tracking_price/tracking_price_constants.h"
@@ -985,7 +986,7 @@ UIWindow* WindowWithAccessibilityIdentifier(NSString* accessibility_id) {
 }
 
 + (id<GREYMatcher>)settingsPasswordMatcher {
-  return grey_accessibilityID(kPasswordsTableViewId);
+  return grey_accessibilityID(kPasswordsSettingsTableViewId);
 }
 
 + (id<GREYMatcher>)settingsPasswordSearchMatcher {
