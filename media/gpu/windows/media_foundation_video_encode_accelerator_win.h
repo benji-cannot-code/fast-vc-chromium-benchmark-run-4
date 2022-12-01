7214313e3dd44617f33bc40f0911f38b5fef4838
yingyingma <yingying.ma@intel.com>
2022-12-01 01:13:38 +0000

    Video: enable VP9 CQP hardware encoding with libvpx BRC
    This is the part 3 of a patch series, split by "AV1/VP9 HW encoding
    with libaom/libvpx BRC".
    
    This CL switches VP9 hardware encoding from CBR mode to per-
    frame-QP mode by using the libvpx real-time rate controller.
    
    Design doc: https://docs.google.com/document/d/1VALjRS_RFWEGIKASKLMbxh3eRKNcEXlR9T9c1iysmZs/edit?resourcekey=0-fKGE5wHHgssv-cK23qMZ2Q#
    
    Bug: 1358750
    Change-Id: I64c897e98815a0a856ba45c1ff98d3ceaea2de4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965370
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Yingying Ma <yingying.ma@intel.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077715}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.h
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+class VideoRateControlWrapper;
+
 // Media Foundation implementation of the VideoEncodeAccelerator interface for
 // Windows.
 // This class saves the task runner on which it is constructed and runs client
@@ -63,6 +65,9 @@ class MEDIA_GPU_EXPORT MediaFoundationVideoEncodeAccelerator
   void UseOutputBitstreamBuffer(BitstreamBuffer buffer) override;
   void RequestEncodingParametersChange(const Bitrate& bitrate,
                                        uint32_t framerate) override;
+  void RequestEncodingParametersChange(
+      const VideoBitrateAllocation& bitrate_allocation,
+      uint32_t framerate) override;
   void Destroy() override;
   bool IsGpuFrameResizeSupported() override;
 
@@ -140,8 +145,9 @@ class MEDIA_GPU_EXPORT MediaFoundationVideoEncodeAccelerator
       std::unique_ptr<BitstreamBufferRef> buffer_ref);
 
   // Changes encode parameters on |encoder_thread_task_runner_|.
-  void RequestEncodingParametersChangeTask(const Bitrate& bitrate,
-                                           uint32_t framerate);
+  void RequestEncodingParametersChangeTask(
+      const VideoBitrateAllocation& bitrate_allocation,
+      uint32_t framerate);
 
   // Destroys encode session on |encoder_thread_task_runner_|.
   void DestroyTask();
@@ -184,7 +190,8 @@ class MEDIA_GPU_EXPORT MediaFoundationVideoEncodeAccelerator
   // For recording configured frame rate as we don't dynamically change it.
   // The default value here will be overridden during initialization.
   uint32_t configured_frame_rate_ = 30;
-  Bitrate bitrate_;
+  // Bitrate allocation in bps.
+  VideoBitrateAllocation bitrate_allocation_;
   bool low_latency_mode_;
   int num_temporal_layers_ = 1;
 
@@ -251,6 +258,9 @@ class MEDIA_GPU_EXPORT MediaFoundationVideoEncodeAccelerator
 
   // A buffer used as a scratch space for I420 to NV12 conversion
   std::vector<uint8_t> resize_buffer_;
+
+  // Bitrate controller for CBR encoding.
+  std::unique_ptr<VideoRateControlWrapper> rate_ctrl_;
 
   // Declared last to ensure that all weak pointers are invalidated before
   // other destructors run.
