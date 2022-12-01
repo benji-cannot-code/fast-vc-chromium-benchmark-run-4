a75acaa6aeb1505bc27abcfe2d8199550f686d66
Aaron Leventhal <aleventhal@google.com>
2022-12-01 21:48:20 +0000

    Update a11y event de-duping to avoid O(n^2)
    The current event de-duping mechanism walks through the vector of events each time an event is added.
    
    Another benefit is that this fixes some broken tests for the upcoming CL:4062111, which implements stable ids for AXObjects. In that CL, events were deduped when they shouldn't be, after an object was destroyed and recreated but had the same id, causing unintended code paths.
    
    Specifics of this change:
    1. Keeps de-duping for dirty object serialization, via the already_serialized_ids_ mechanism. Note that over time we are removing a11y events from Blink and just marking objects dirty instead.
    2. Does no processing on events where de-duping is unnecessary, e.g. Event::kExpanded or Event::kChecked.
    3. Adds specialized event deduping mechanisms for the events that make sense: kLocationChanged, kLayoutComplete, kValueChanged.
    4. Allows duplicate events where it is deemed harmless: kActiveDescendantChanged, kAriaAttributeChanged, kCheckedStateChanged, kClicked, kDocumentSelectionChanged, kDocumentTitleChanged, kBlur, kFocus, kHide, kShow, kHitTestResult, kLoadComplete, kLoadStart, kMenuListValueChanged, kRowCountChanged, kScrolledToAnchor, kSelectedChildrenChanged. These plus the events in bullet #3 were the only events where duplicates were detected when testing with NOTREACHED.
    
    Bug: none
    Change-Id: I726da94892e7af557fad45e6ef052b3b6ea587e5
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060563
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078296}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -195,7 +195,6 @@ class MODULES_EXPORT AXObjectCacheImpl
   void HandleInitialFocus() override;
   void HandleTextFormControlChanged(Node*) override;
   void HandleEditableTextContentChanged(Node*) override;
-  void HandleScaleAndLocationChanged(Document*) override;
   void HandleTextMarkerDataAdded(Node* start, Node* end) override;
   void HandleValueChanged(Node*) override;
   void HandleUpdateActiveMenuOption(Node*) override;
@@ -351,6 +350,7 @@ class MODULES_EXPORT AXObjectCacheImpl
   void PostNotification(const LayoutObject*, ax::mojom::blink::Event);
   // Creates object if necessary.
   void EnsurePostNotification(Node*, ax::mojom::blink::Event);
+  void EnsureMarkDirtyWithCleanLayout(Node*);
   // Does not create object.
   // TODO(accessibility) Find out if we can merge with EnsurePostNotification().
   void PostNotification(Node*, ax::mojom::blink::Event);
@@ -849,10 +849,17 @@ class MODULES_EXPORT AXObjectCacheImpl
   TreeUpdateCallbackQueue tree_update_callback_queue_main_;
   TreeUpdateCallbackQueue tree_update_callback_queue_popup_;
 
+  // Help de-dupe processing of repetitive events.
   HeapHashSet<WeakMember<Node>> nodes_with_pending_children_changed_;
+  HashSet<AXID> nodes_with_pending_location_changed_;
 
   // Nodes with document markers that have received accessibility updates.
   HeapHashSet<WeakMember<Node>> nodes_with_spelling_or_grammar_markers_;
+
+  // True when layout has changed, and changed locations must be serialized.
+  bool need_to_send_location_changes_ = false;
+
+  AXID last_value_change_node_ = ui::AXNodeData::kInvalidAXID;
 
   // If tree_update_callback_queue_ gets improbably large, stop
   // enqueueing updates and fire a single ChildrenChanged event on the
