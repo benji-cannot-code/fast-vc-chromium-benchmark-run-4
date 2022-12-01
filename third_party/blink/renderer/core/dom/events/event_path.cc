f86cca27d8a988e5c5963d21cdb52472c14bf8a4
Di Zhang <dizhangg@chromium.org>
2022-12-01 17:26:19 +0000

    Add UMA histogram for the time spent calculating EventPath
    This patch adds a UMA metric for calculating a path (list of nodes) when
    initializing an EventPath: Blink.EventPath.CalculateTime.
    
    Change-Id: Ic8d793208bc98dd769b20a6fae6c29d0d5b87e3f
    Bug: 1394555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062301
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078131}

--- third_party/blink/renderer/core/dom/events/event_path.cc
+++ third_party/blink/renderer/core/dom/events/event_path.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_slot_element.h"
 #include "third_party/blink/renderer/core/input/touch.h"
 #include "third_party/blink/renderer/core/input/touch_list.h"
+#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 
 namespace blink {
 
@@ -121,6 +122,10 @@ static bool EventNodePathCachingEnabled() {
 }
 
 void EventPath::CalculatePath() {
+  // TODO(crbug.com/1394555): This histogram should be removed once the UMA
+  // study DocumentEventNodePathCaching is complete.
+  SCOPED_BLINK_UMA_HISTOGRAM_TIMER_HIGHRES("Blink.EventPath.CalculateTime");
+
   DCHECK(node_);
   DCHECK(node_event_contexts_.empty());
 
