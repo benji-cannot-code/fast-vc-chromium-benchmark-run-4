8d3fb690c383e79af881a1df492968881c2c8e06
Federica Germinario <fedegermi@google.com>
2022-12-01 09:25:41 +0000

    [iOS] Removed exposed internal of the class
    Removed exposed internal in block_popups_table_view_controller_unittest.mm because editButtonPressed is not used inside of it, so that code is unnecessary.
    
    Fixed: 1379027
    Change-Id: I4d9042398135921dfeda47898d76663fe5527d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065999
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077933}

--- ios/chrome/browser/ui/settings/content_settings/block_popups_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/content_settings/block_popups_table_view_controller_unittest.mm
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface SettingsRootTableViewController (ExposedForTesting)
-- (void)editButtonPressed;
-@end
-
 namespace {
 
 const char* kAllowedPattern = "[*.]example.com";
