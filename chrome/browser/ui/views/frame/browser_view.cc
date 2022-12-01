e07de43982190ae42a0a86b46dc55ee3a87a32fe
Dana Fried <dfried@chromium.org>
2022-12-01 01:29:40 +0000

    [Test Automation] Assign ID to BrowserView, add WithView() verb
    This closes an existing hole in the API and removes a bunch of
    frustrating BrowserView::GetBrowserViewForBrowser(browser())
    boilerplate.
    
    A number of tests have been clarified/simplified as a result.
    
    Bug: 1374319
    Change-Id: I6c9f4afc94edd88dcfcd28d0c0d09550c3d89a66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4068041
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077725}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_dialogs.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator.h"
@@ -859,6 +860,8 @@ BrowserView::BrowserView(std::unique_ptr<Browser> browser)
     SetCanMaximize(browser_->create_params().can_maximize);
     SetCanMinimize(true);
   }
+
+  SetProperty(views::kElementIdentifierKey, kBrowserViewElementId);
 
   // Create user education resources.
   UserEducationService* const user_education_service =
