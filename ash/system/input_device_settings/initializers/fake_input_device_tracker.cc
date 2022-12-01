39d688b3a622d739a1af36d5359c998abbb0a71a
David Padlipsky <dpad@google.com>
2022-12-01 21:46:37 +0000

    Settings Split: Initial commit for input device tracker
    - InputDeviceTracker is used to observe connected devices and note them
      in prefs to initialize correctly when per-device settings launches.
    
    Bug: b/241965700
    Test: ash_unittests
    Change-Id: I35e49f64a8b2dd4135115c9c6d31135d4f32ec56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062671
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078293}

---
+++ ash/system/input_device_settings/initializers/fake_input_device_tracker.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/initializers/fake_input_device_tracker.h"
+
+#include "base/containers/contains.h"
+
+namespace ash {
+
+FakeInputDeviceTracker::FakeInputDeviceTracker() = default;
+FakeInputDeviceTracker::~FakeInputDeviceTracker() = default;
+
+void FakeInputDeviceTracker::RecordDeviceConnected(
+    InputDeviceCategory category,
+    const base::StringPiece& device_key) {
+  tracker_data_.emplace_back(category, device_key);
+}
+
+bool FakeInputDeviceTracker::WasDeviceRecorded(
+    InputDeviceCategory category,
+    const base::StringPiece& device_key) {
+  return base::Contains(tracker_data_,
+                        InputDeviceTrackerData(category, device_key));
+}
+
+}  // namespace ash
---
+++ ash/system/input_device_settings/initializers/input_device_tracker_impl.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/input_device_settings/initializers/input_device_tracker_impl.h"
+
+#include "ash/system/input_device_settings/input_device_settings_controller.h"
+#include "base/notreached.h"
+#include "components/prefs/pref_service.h"
+
+namespace ash {
+
+InputDeviceTrackerImpl::InputDeviceTrackerImpl(PrefService* pref_service)
+    : pref_service_(pref_service) {}
+InputDeviceTrackerImpl::~InputDeviceTrackerImpl() = default;
+
+// TODO(dpad@): Implement storing of observed device pref.
+void InputDeviceTrackerImpl::RecordDeviceConnected(
+    InputDeviceCategory category,
+    const base::StringPiece& device_key) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace ash
