9d24a03e1723eda697108ae9eccc3933af6343b1
Adithya Srinivasan <adithyas@chromium.org>
2022-12-01 01:09:39 +0000

    DocumentRules: Check if initialized before calling InvalidateAllLinks
    Follow up to https://crrev.com/c/4056158.
    
    Bug: 1371522
    Change-Id: I507aa6a5f37d46c9f9e6d16a83da980d28bf6b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4067700
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077711}

--- third_party/blink/renderer/core/speculation_rules/document_speculation_rules.cc
+++ third_party/blink/renderer/core/speculation_rules/document_speculation_rules.cc
@@ -262,7 +262,8 @@ void DocumentSpeculationRules::DocumentBaseURLChanged() {
     // new errors.
     DCHECK(parse_error.empty());
   }
-  InvalidateAllLinks();
+  if (initialized_)
+    InvalidateAllLinks();
   QueueUpdateSpeculationCandidates();
 }
 
