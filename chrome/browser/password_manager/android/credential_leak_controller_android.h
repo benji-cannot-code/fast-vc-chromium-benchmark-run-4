7e1f5574d90cdec8f743c0e37044d319e89cee71
Bruno Braga <brunobraga@google.com>
2022-12-02 13:22:51 +0000

    [APC Sunset] Remove password_change_success_tracker from the credential leak dialog.
    As part of left overs TODO from APC sunset, we are removing the successful password change tracker as one can no longer start a password change from the dialog.
    
    Bug: 1386065
    Change-Id: I4c8a09516e9d30708170802930a1e5d530413015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4069747
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078546}

--- chrome/browser/password_manager/android/credential_leak_controller_android.h
+++ chrome/browser/password_manager/android/credential_leak_controller_android.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
-#include "components/password_manager/core/browser/password_change_success_tracker.h"
 #include "url/gurl.h"
 
 namespace ui {
@@ -24,14 +23,10 @@ class CredentialLeakDialogViewAndroid;
 // detected. It is self-owned and it owns the dialog view.
 class CredentialLeakControllerAndroid {
  public:
-  // TODO(crbug.com/1386065): Remove |password_change_success_tracker| as part
-  // of APC clean up.
   CredentialLeakControllerAndroid(
       password_manager::CredentialLeakType leak_type,
       const GURL& origin,
       const std::u16string& username,
-      password_manager::PasswordChangeSuccessTracker*
-          password_change_success_tracker,
       ui::WindowAndroid* window_android,
       // Metrics recorder for leak dialog related UMA and UKM logging.
       std::unique_ptr<password_manager::metrics_util::LeakDialogMetricsRecorder>
@@ -82,9 +77,6 @@ class CredentialLeakControllerAndroid {
   const GURL origin_;
 
   const std::u16string username_;
-
-  const raw_ptr<password_manager::PasswordChangeSuccessTracker>
-      password_change_success_tracker_;
 
   const raw_ptr<ui::WindowAndroid> window_android_;
 
--- chrome/browser/password_manager/android/credential_leak_controller_android_unittest.cc
+++ chrome/browser/password_manager/android/credential_leak_controller_android_unittest.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
-#include "components/password_manager/core/browser/mock_password_change_success_tracker.h"
-#include "components/password_manager/core/browser/password_change_success_tracker.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/ukm/test_ukm_recorder.h"
@@ -29,8 +27,6 @@ using password_manager::HasChangeScript;
 using password_manager::IsReused;
 using password_manager::IsSaved;
 using password_manager::IsSyncing;
-using password_manager::MockPasswordChangeSuccessTracker;
-using password_manager::PasswordChangeSuccessTracker;
 using password_manager::metrics_util::LeakDialogDismissalReason;
 using password_manager::metrics_util::LeakDialogMetricsRecorder;
 using password_manager::metrics_util::LeakDialogType;
@@ -47,8 +43,7 @@ CredentialLeakControllerAndroid* MakeController(
     IsSaved is_saved,
     IsReused is_reused,
     IsSyncing is_syncing,
-    HasChangeScript has_change_script,
-    PasswordChangeSuccessTracker* password_change_success_tracker = nullptr) {
+    HasChangeScript has_change_script) {
   password_manager::CredentialLeakType leak_type =
       CreateLeakType(is_saved, is_reused, is_syncing, has_change_script);
   auto recorder = std::make_unique<LeakDialogMetricsRecorder>(
@@ -56,7 +51,7 @@ CredentialLeakControllerAndroid* MakeController(
   // Set sampling rate to 100% to avoid flakiness.
   recorder->SetSamplingRateForTesting(1.0);
   return new CredentialLeakControllerAndroid(
-      leak_type, GURL(kOrigin), kUsername, password_change_success_tracker,
+      leak_type, GURL(kOrigin), kUsername,
       /*window_android=*/nullptr, std::move(recorder));
 }
 
