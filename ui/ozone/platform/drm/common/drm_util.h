e5c5f871e17dd95498eaf41e138559cdc1cdf4a3
Gil Dekel <gildekel@chromium.org>
2022-12-02 03:59:48 +0000

    ui/ozone: Take ownership of connector in DrmDisplay
    This CL modifies DrmDisplay to take ownership of connectors as they are
    being updated during display reconfiguration events (i.e. incoming udev
    events) from the HardwareDisplayControllerInfo objects that were created
    especially for that.
    
    Note that it is safe to assume the connector objects are populated (as
    against to being nullptr) since the function that created them, namely
    GetDisplayInfosAndInvalidCrtcs(), only returns valid connectors via the
    HardwareDisplayControllerInfo objects.
    
    Bug: b:172210990, b:193019614, b:193060019
    Test: display_unittests && ozone_unittests
    Change-Id: Ic8a39650c528fa82b6d95b26fa395a6ad042daef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4071915
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078437}

--- ui/ozone/platform/drm/common/drm_util.h
+++ ui/ozone/platform/drm/common/drm_util.h
@@ -99,6 +99,8 @@ class HardwareDisplayControllerInfo {
   drmModeCrtc* crtc() const { return crtc_.get(); }
   uint8_t index() const { return index_; }
 
+  ScopedDrmConnectorPtr ReleaseConnector() { return std::move(connector_); }
+
  private:
   ScopedDrmConnectorPtr connector_;
   ScopedDrmCrtcPtr crtc_;
