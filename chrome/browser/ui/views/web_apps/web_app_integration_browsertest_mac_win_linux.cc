1a020178b87d087bf1a5581aac7c46b875c95e59
Clifford Cheng <cliffordcheng@chromium.org>
2022-12-02 22:20:15 +0000

    Reland "[dpwa] Adds a callback when Mac shim has finished startup or has been cancelled."
    This is a reland of commit 3e4e964229c354582ef2c5eb69b6dc6264c5fa83
    
    Original change's description:
    > [dpwa] Adds a callback when Mac shim has finished startup or has been cancelled.
    >
    > The action for LaunchFile is also updated to make use of this new
    > callback in test.
    >
    > Bug: 1349953
    > Change-Id: Icc733107066c5c6e85e3c24aed19096988eec3e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064597
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1078188}
    
    Bug: 1349953
    Change-Id: Ib2f954163b61c20f17f256e5e6f591bec77a6bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076790
    Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078792}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
@@ -67,7 +67,6 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckSiteNotHandlesFile) {
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckLaunchFileExpectDialog) {
   helper_.InstallMenuOption(InstallableSite::kFileHandler);
-  helper_.ClosePwa();
   helper_.LaunchFileExpectDialog(Site::kFileHandler, FilesOptions::kOneFooFile,
                                  AllowDenyOptions::kAllow,
                                  AskAgainOptions::kAskAgain);
@@ -76,12 +75,11 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckLaunchFileExpectDialog) {
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckLaunchFileExpectNoDialog_Allow) {
   helper_.InstallOmniboxIcon(InstallableSite::kFileHandler);
-  helper_.ClosePwa();
   // Open the file and set AskAgainOption to kRemember.
   helper_.LaunchFileExpectDialog(Site::kFileHandler, FilesOptions::kOneFooFile,
                                  AllowDenyOptions::kAllow,
                                  AskAgainOptions::kRemember);
-  helper_.ClosePwa();
+  helper_.CheckWindowCreated();
   // Open the file again.
   helper_.LaunchFileExpectNoDialog(Site::kFileHandler,
                                    FilesOptions::kOneFooFile);
@@ -90,12 +88,12 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckLaunchFileExpectNoDialog_Allow) {
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegration, CheckLaunchFileExpectNoDialog_Deny) {
   helper_.InstallOmniboxIcon(InstallableSite::kFileHandler);
-  helper_.ClosePwa();
   // Open the file and set AskAgainOption to kRemember.
   helper_.LaunchFileExpectDialog(Site::kFileHandler, FilesOptions::kOneFooFile,
                                  AllowDenyOptions::kDeny,
                                  AskAgainOptions::kRemember);
-
+  // No new window is created when denied.
+  helper_.CheckWindowNotCreated();
   // Open the file again.
   helper_.LaunchFileExpectNoDialog(Site::kFileHandler,
                                    FilesOptions::kOneFooFile);
