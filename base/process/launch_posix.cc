cf407eeabe38efa68543e3c3e9cc82f6aff7faa2
Bartek Nowierski <bartekn@chromium.org>
2022-12-02 03:28:19 +0000

    Unrewrite raw_ptr<>s in kernel_sigaction
    The struct is passed to kernel (by address), which thinks it has true
    raw pointers. Essentially, our view of kernel_sigaction must mimic the
    kernel's view of kernel_sigaction. (Adding the exclusion annotation is
    fine, as it doesn't affect the behavior.)
    
    Found by tasak@, thank you!
    
    Bug: 1395075
    Change-Id: Ic486beb4573e00f773bc7813164667272bd70c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074984
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078429}

--- base/process/launch_posix.cc
+++ base/process/launch_posix.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/process/environment_internal.h"
@@ -128,11 +128,12 @@ typedef uint64_t kernel_sigset_t;
 
 // This is what struct sigaction looks like to the kernel at least on X86 and
 // ARM. MIPS, for instance, is very different.
+// For that reason `k_sa_handler` and `k_sa_restorer` can't be raw_ptr<>.
 struct kernel_sigaction {
-  raw_ptr<void>
+  RAW_PTR_EXCLUSION void*
       k_sa_handler;  // For this usage it only needs to be a generic pointer.
   unsigned long k_sa_flags;
-  raw_ptr<void>
+  RAW_PTR_EXCLUSION void*
       k_sa_restorer;  // For this usage it only needs to be a generic pointer.
   kernel_sigset_t k_sa_mask;
 };
