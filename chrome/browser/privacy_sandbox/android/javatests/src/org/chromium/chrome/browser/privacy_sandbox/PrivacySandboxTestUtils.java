084b918412ded8c559d758e5eda3be0e7733e868
Rubin Deliallisi <rubindl@chromium.org>
2022-12-02 14:30:38 +0000

    Privacy Sandbox Settings: Populate current Topics list
    - Dynamically populate the current Topics list.
    - Test that the UI elements are displayed correctly.
    - Create a utilities test class.
    
    Screenshots: https://crbug.com/1378703#112
    Bug: 1378703, b/254413445, b/254412647
    Change-Id: Ib7f98eeb58af9c983420859dcd122b424c381581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075985
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078569}

---
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxTestUtils.java
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_sandbox;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
+import static androidx.test.espresso.matcher.ViewMatchers.withChild;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.allOf;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+/**
+ * Test utilities for various privacy_sandbox tests.
+ */
+public final class PrivacySandboxTestUtils {
+    /**
+     * Click an ImageButton located next to a View that contains the given text.
+     *
+     * @param text The text contained in the View adjacent to the ImageButton.
+     */
+    public static void clickImageButtonNextToText(String text) {
+        onView(allOf(withId(R.id.image_button), withParent(hasSibling(withChild(withText(text))))))
+                .perform(click());
+    }
+
+    /**
+     * Matcher for a {@link Topic} based only on the name.
+     *
+     * @param name The name of the {@link Topic}.
+     * @return The Matcher for the {@link Topic}.
+     */
+    public static Matcher<Topic> withTopic(String name) {
+        return new BaseMatcher<>() {
+            @Override
+            public boolean matches(Object o) {
+                return ((Topic) o).getName().equals(name);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Should contain " + name);
+            }
+        };
+    }
+}
