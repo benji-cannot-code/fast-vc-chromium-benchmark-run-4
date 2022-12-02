80691acb937733eefb442cf9680c836972f31e1b
Gang Wu <gangwu@chromium.org>
2022-12-02 22:45:18 +0000

    [Omnibox] Add holding space for the status icon
    Before the status icon background is added, the icon layout is
      [4dp space] [icon] [4dp space].
    So when the icon is GONE, there still 2 [4dp space] there.
    
    After the status icon background is added, the icon layout is
      [background [icon] background].(background is 32dp, icon is 24dp).
    And when the icon is GONE, the background is GONE as well, otherwise
    there will be 32dp space left.
    
    This CL add a custom status icon, when the status icon and its
    background are GONE, a 8dp space we add to the view, so the behavior
    will be same as before.
    
    Bug: 1383114
    Change-Id: I380f418c6b0a084eec316ee96f975066756461bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031711
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078803}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusView.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusView.java
@@ -61,7 +61,7 @@ public class StatusView extends LinearLayout {
 
     private ImageView mIconView;
     private View mIconBackground;
-    private View mIconViewFrame;
+    private StatusIconView mStatusIconView;
     private TextView mVerboseStatusTextView;
     private View mSeparatorView;
     private View mStatusExtraSpace;
@@ -96,7 +96,7 @@ public class StatusView extends LinearLayout {
 
         mIconView = findViewById(R.id.location_bar_status_icon);
         mIconBackground = findViewById(R.id.location_bar_status_icon_bg);
-        mIconViewFrame = findViewById(R.id.location_bar_status_icon_frame);
+        mStatusIconView = findViewById(R.id.location_bar_status_icon_view);
         mVerboseStatusTextView = findViewById(R.id.location_bar_verbose_status);
         mSeparatorView = findViewById(R.id.location_bar_verbose_status_separator);
         mStatusExtraSpace = findViewById(R.id.location_bar_verbose_status_extra_space);
@@ -108,7 +108,7 @@ public class StatusView extends LinearLayout {
      * Return whether search engine status icon is visible.
      */
     public boolean isSearchEngineStatusIconVisible() {
-        return mIconViewFrame.getVisibility() == VISIBLE;
+        return mStatusIconView.getIconVisibility() == VISIBLE;
     }
 
     /**
@@ -153,15 +153,14 @@ public class StatusView extends LinearLayout {
         // directly. All other options (5, 7) are no-op.
         //
         // Note: this will be compacted once we start using LayoutTransition with StatusView.
-
-        boolean isIconHidden = mIconViewFrame.getVisibility() == View.GONE;
+        boolean isIconHidden = mStatusIconView.getIconVisibility() == View.GONE;
         if (!wantIconHidden && (isIconHidden || mAnimatingStatusIconHide)) {
             // Action 1: animate showing, if icon was either hidden or hiding.
             if (mAnimatingStatusIconHide) mIconView.animate().cancel();
             mAnimatingStatusIconHide = false;
             mAnimatingStatusIconShow = true;
             keepControlsShownForAnimation();
-            mIconViewFrame.setVisibility(View.VISIBLE);
+            mStatusIconView.setVisibility(View.VISIBLE);
             mIconView.animate()
                     .alpha(1.0f)
                     .setDuration(getIconAnimationDuration())
@@ -187,7 +186,7 @@ public class StatusView extends LinearLayout {
                     .setDuration(mAnimationsEnabled ? getIconAnimationDuration() : 0)
                     .alpha(0.0f)
                     .withEndAction(() -> {
-                        mIconViewFrame.setVisibility(View.GONE);
+                        mStatusIconView.setVisibility(View.GONE);
                         mIconView.setAlpha(1f);
                         mAnimatingStatusIconHide = false;
                         allowBrowserControlsHide();
@@ -334,11 +333,11 @@ public class StatusView extends LinearLayout {
 
     /** Specify the status icon visibility. */
     void setStatusIconShown(boolean showIcon) {
-        if (mIconViewFrame == null) return;
+        if (mStatusIconView == null) return;
         // Check if layout was requested before changing our child view.
         boolean wasLayoutPreviouslyRequested = isLayoutRequested();
 
-        mIconViewFrame.setVisibility(showIcon ? VISIBLE : GONE);
+        mStatusIconView.setVisibility(showIcon ? VISIBLE : GONE);
         updateTouchDelegate();
         if (mIsAnimatingStatusIconChange && !showIcon) {
             // If the icon view is hidden before it gets a chance to draw, our animation status will
@@ -537,7 +536,7 @@ public class StatusView extends LinearLayout {
 
     /** @return True if the status icon is currently visible. */
     private boolean isIconVisible() {
-        return mStatusIconDrawable != null && mIconViewFrame.getVisibility() != GONE
+        return mStatusIconDrawable != null && mStatusIconView.getIconVisibility() != GONE
                 && mIconView.getAlpha() != 0;
     }
 
