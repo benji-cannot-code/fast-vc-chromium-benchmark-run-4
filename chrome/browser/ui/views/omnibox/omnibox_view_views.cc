2acde8bea680cded2d28d8e5db99726bef948ef4
manukh <manukh@chromium.org>
2022-12-02 17:55:47 +0000

    [omnibox][escape] Clean up `kBlurWithEscape`
    The feature launched through chromium roll out with m105
    crrev.com/c/3781111 .
    
    Stable is now at m108. So it seems safe to clean up the feature code
    (and chrome://flag).
    
    The related feature `kClosePopupWithEscape` is still being experimented
    with.
    
    Since the m105 launch, the histogram `Omnibox.Escape` no longer logs
    bucket 0, kNone, since the escape key now always does something when the
    omnibox is focused. The histogram is renamed to `Omnibox.Escape2` as its
    buckets haven't changed meaning, but rather the underlying event that
    bucket was measuring no longer occurs.
    
    Bug: 776080
    Change-Id: Ifa48181ee8d0db961999d4421f1e0d91c79a7fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4071584
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078665}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -1233,7 +1233,7 @@ bool OmniboxViewViews::SkipDefaultKeyEventProcessing(
     return true;
   }
   if (event.key_code() == ui::VKEY_ESCAPE)
-    return model()->WillHandleEscapeKey();
+    return true;
   return Textfield::SkipDefaultKeyEventProcessing(event);
 }
 
