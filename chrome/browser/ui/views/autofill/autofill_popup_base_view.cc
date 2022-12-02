04b5b9c0bb0f0a23db8a108fb4609c151de1f5a0
Dmitry Vykochko <vykochko@google.com>
2022-12-02 14:11:49 +0000

    Add more prominent style to autofill popup under a flag.
    Feature disabled(default): https://screenshot.googleplex.com/9oHqiSEzheRcvhP.png
    Feature enabled: https://screenshot.googleplex.com/Q9KNdXRkjWDMGVJ.png
    
    Bug: 1354136
    Change-Id: Ie8d234f11bdc441d81df72d28b1f50efaf8afbaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051784
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078562}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -48,6 +48,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+namespace {
+
+views::Emphasis GetPopupEmphasis() {
+  return base::FeatureList::IsEnabled(features::kAutofillMoreProminentPopup)
+             ? views::Emphasis::kMaximum
+             : views::Emphasis::kMedium;
+}
+
+}  // namespace
+
 // The widget that the AutofillPopupBaseView will be attached to.
 class AutofillPopupBaseView::Widget : public views::Widget {
  public:
@@ -96,8 +106,7 @@ class AutofillPopupBaseView::Widget : public views::Widget {
 
 // static
 int AutofillPopupBaseView::GetCornerRadius() {
-  return ChromeLayoutProvider::Get()->GetCornerRadiusMetric(
-      views::Emphasis::kMedium);
+  return ChromeLayoutProvider::Get()->GetCornerRadiusMetric(GetPopupEmphasis());
 }
 
 // static
@@ -340,6 +349,11 @@ gfx::Rect AutofillPopupBaseView::GetOptionalPositionAndPlaceArrowOnPopup(
 
   gfx::Rect popup_bounds;
 
+  int maximum_pixel_offset_to_center =
+      base::FeatureList::IsEnabled(features::kAutofillMoreProminentPopup)
+          ? features::kAutofillMoreProminentPopupMaxOffsetToCenterParam.Get()
+          : kMaximumPixelsToMoveSuggstionToCenter;
+
   // Deduce the arrow and the position.
   views::BubbleBorder::Arrow arrow = GetOptimalPopupPlacement(
       /*content_area_bounds=*/max_bounds_for_popup,
@@ -348,7 +362,7 @@ gfx::Rect AutofillPopupBaseView::GetOptionalPositionAndPlaceArrowOnPopup(
       /*right_to_left=*/delegate_->IsRTL(),
       /*scrollbar_width=*/gfx::scrollbar_size(),
       /*maximum_pixel_offset_to_center=*/
-      kMaximumPixelsToMoveSuggstionToCenter,
+      maximum_pixel_offset_to_center,
       /*maximum_width_percentage_to_center=*/
       kMaximumWidthPercentageToMoveTheSuggestionToCenter,
       /*popup_bounds=*/popup_bounds);
@@ -426,7 +440,7 @@ std::unique_ptr<views::Border> AutofillPopupBaseView::CreateBorder() {
   border->SetCornerRadius(GetCornerRadius());
   border->set_md_shadow_elevation(
       ChromeLayoutProvider::Get()->GetShadowElevationMetric(
-          views::Emphasis::kMedium));
+          GetPopupEmphasis()));
   return border;
 }
 
