88335136a0a321636f0e775e1a952425bad4a712
Mike Wittman <wittman@chromium.org>
2022-12-02 20:28:48 +0000

    [sampling profiler] Remove unused ArmCFITable
    This class is no longer used since the associated unwinder was removed
    in crrev.com/1076844.
    
    Bug: 1393052
    Change-Id: Id5448b1ef7188460a28c2a969ef5ed978c12e559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072161
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078758}

--- chrome/common/profiler/unwind_util.cc
+++ chrome/common/profiler/unwind_util.cc
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ANDROID_ARM32_UNWINDING_SUPPORTED
 #include "base/android/apk_assets.h"
 #include "base/files/memory_mapped_file.h"
-#include "base/profiler/arm_cfi_table.h"
 #include "base/profiler/chrome_unwinder_android.h"
 #endif  // ANDROID_ARM32_UNWINDING_SUPPORTED
 
