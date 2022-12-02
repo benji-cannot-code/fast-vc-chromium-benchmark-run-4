8babdb337018a3e7f84c56833c39a440f2971924
sbingler <bingler@chromium.org>
2022-12-02 22:19:09 +0000

    Remove DCHECK from RuntimeFeatureStateContext const getter
    This DCHECK is not required because there is no harm in reading this
    object.
    
    Bug: 1377000
    Change-Id: Ic31ec88e5313bd190f340f1eca65a3b76e28f877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076986
    Auto-Submit: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078790}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -8366,7 +8366,6 @@ NavigationRequest::GetMutableRuntimeFeatureStateContext() {
 
 const blink::RuntimeFeatureStateContext&
 NavigationRequest::GetRuntimeFeatureStateContext() {
-  DCHECK_LE(state_, NavigationState::READY_TO_COMMIT);
   return runtime_feature_state_context_;
 }
 
