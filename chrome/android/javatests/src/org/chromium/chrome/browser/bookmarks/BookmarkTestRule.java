51297ff63663c6e0c53fae2860d4953422513e08
Brandon Wylie <wylieb@chromium.org>
2022-12-02 00:46:10 +0000

    [bookmarks] Move testing code to bookmarks directory
    Change-Id: Iafc8cf64de55a3385d73611e73c20362622a7aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4071045
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078384}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTestRule.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.test.util;
+package org.chromium.chrome.browser.bookmarks;
 
 import android.support.test.InstrumentationRegistry;
 
@@ -16,7 +16,7 @@ import org.junit.runners.model.Statement;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.app.bookmarks.BookmarkActivity;
-import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
+import org.chromium.chrome.test.util.ActivityTestUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
 /**
--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTestUtil.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTestUtil.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.test.util;
+package org.chromium.chrome.browser.bookmarks;
 
 import android.support.test.InstrumentationRegistry;
 
@@ -14,7 +14,6 @@ import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.app.bookmarks.BookmarkEditActivity;
-import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.offlinepages.OfflinePageItem;
 import org.chromium.chrome.browser.offlinepages.OfflineTestUtil;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksShim;
@@ -29,7 +28,6 @@ import java.util.List;
  * Utility functions for dealing with bookmarks in tests.
  */
 public class BookmarkTestUtil {
-
     /**
      * Waits until the bookmark model is loaded, i.e. until
      * {@link BookmarkModel#isBookmarkModelLoaded()} is true.
