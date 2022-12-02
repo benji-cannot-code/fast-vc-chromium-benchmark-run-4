b3573d29ae1b614ba3191b4b40d7ef5b3bcc6efa
Elie Maamari <emaamari@google.com>
2022-12-02 14:26:33 +0000

    Allow access to InSessionAuthDialogController from ash/public/cpp
    In order to allow code under `chrome/browser/password_manager` to
    trigger the authentication flow on ash, we need to expose
    `InSessionAuthDialogController` to Ash's public interface.
    
    Bug: b:259526857
    Change-Id: I3b59df0f80899fb86442789d0cd8d9a6a76fb796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064401
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078567}

---
+++ ash/public/cpp/in_session_auth_dialog_controller.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/in_session_auth_dialog_controller.h"
+
+namespace ash {
+
+namespace {
+
+// non owning pointer, the singleton's lifetime is instead managed by ash::Shell
+InSessionAuthDialogController* g_instance = nullptr;
+
+}  // namespace
+
+// static
+InSessionAuthDialogController* InSessionAuthDialogController::Get() {
+  return g_instance;
+}
+
+InSessionAuthDialogController::InSessionAuthDialogController() {
+  DCHECK(!g_instance);
+  g_instance = this;
+}
+
+InSessionAuthDialogController::~InSessionAuthDialogController() {
+  DCHECK(g_instance);
+  g_instance = nullptr;
+}
+
+}  // namespace ash
--- ash/public/cpp/in_session_auth_dialog_controller.h
+++ ash/public/cpp/in_session_auth_dialog_controller.h
@@ -23,6 +23,9 @@ class ASH_PUBLIC_EXPORT InSessionAuthDialogController {
     kModifyAuthFactorsMultidevice
   };
 
+  // Returns the singleton instance.
+  static InSessionAuthDialogController* Get();
+
   // Callback passed from clients of the dialog
   // `success`: Whether or not the authentication was successful.
   // `token`: If the authentication was successful, a token is returned from
@@ -34,9 +37,6 @@ class ASH_PUBLIC_EXPORT InSessionAuthDialogController {
       base::OnceCallback<void(bool success,
                               const base::UnguessableToken& token,
                               base::TimeDelta timeout)>;
-
-  InSessionAuthDialogController() = default;
-  virtual ~InSessionAuthDialogController() = default;
 
   // Summons a native UI dialog that authenticates the user, providing a
   // token, timeout and status in return.
@@ -50,6 +50,10 @@ class ASH_PUBLIC_EXPORT InSessionAuthDialogController {
   // for generating an `AuthToken` after successful authentication.
   virtual void SetTokenProvider(
       InSessionAuthTokenProvider* auth_token_provider) = 0;
+
+ protected:
+  InSessionAuthDialogController();
+  virtual ~InSessionAuthDialogController();
 };
 
 }  // namespace ash
