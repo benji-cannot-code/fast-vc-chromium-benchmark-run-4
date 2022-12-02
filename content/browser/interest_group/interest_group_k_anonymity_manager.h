ce63c8d2735041adcf5049201b4005af0c6b5906
Maks Orlovich <morlovich@chromium.org>
2022-12-02 14:03:38 +0000

    FLEDGE: Run a parallel auction if needed based on k-anon mode.
    If we're configured to enforce k-anonymity, the alternate auction will keep track of the non-k-anon winner.
    
    If we're configured to simulate operation of k-anonymity, the alternate auction will keep track of what the winner would be if enforcement were on.
    
    The previous work in this are on BidderWorklet is changed some by this CL since it sometimes wasn't distinguishing between main and alternate bids being identical vs. alternate bid being a failure.
    
    (This is up to the level of AuctionRunner, the service simply ignores it; it may make sense to pull the flag reading into the service).
    
    Bug: 1234419
    Change-Id: I67766d04f70fac926853800b1dbaf23c245fba09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941227
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078556}

--- content/browser/interest_group/interest_group_k_anonymity_manager.h
+++ content/browser/interest_group/interest_group_k_anonymity_manager.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/k_anonymity_service_delegate.h"
 #include "third_party/blink/public/common/interest_group/interest_group.h"
 
+class GURL;
+
 namespace content {
 class InterestGroupManagerImpl;
 
@@ -23,6 +25,11 @@ std::string CONTENT_EXPORT KAnonKeyFor(const url::Origin& owner,
 // Calculates the k-anonymity key for an Ad that is used for bidding.
 std::string CONTENT_EXPORT KAnonKeyForAdBid(const blink::InterestGroup& group,
                                             const blink::InterestGroup::Ad& ad);
+
+// Given a key computed by KAnonKeyForAdBid, returns the `render_url` of the
+// ad that was used to produce it.
+GURL CONTENT_EXPORT RenderUrlFromKAnonKeyForAdBid(const std::string& key);
+
 // Calculates the k-anonymity key for an Ad that is used for reporting the
 // interest group name.
 std::string CONTENT_EXPORT
