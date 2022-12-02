83c4c9aa87b3afb6db18c45e5f0d2bc47cd60b7d
Tibor Goldschwendt <tiborg@chromium.org>
2022-12-02 20:18:46 +0000

    [cc-side-panel] Add basic color UI
    The UI simply renders the chrome colors used in the side panel. No
    interactivity yet.
    
    https://screenshot.googleplex.com/3XnuvvKSf3RZk6y
    
    Bug: 1384229
    Change-Id: Ifec637bbe83f46529a2bd8ac2e4d1c1d4f07a2d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064627
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078752}

--- chrome/test/data/webui/side_panel/customize_chrome/test_support.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/test_support.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 type Constructor<T> = new (...args: any[]) => T;
@@ -15,4 +16,9 @@ export function installMock<T extends object>(
   const mock = TestBrowserProxy.fromClass(clazz);
   installer!(mock);
   return mock;
+}
+
+export function assertStyle(element: Element, name: string, expected: string) {
+  const actual = window.getComputedStyle(element).getPropertyValue(name).trim();
+  assertEquals(expected, actual);
 }
