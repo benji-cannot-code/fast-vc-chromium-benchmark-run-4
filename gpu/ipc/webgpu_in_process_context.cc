0de02647bf33ce9310d9ea099a9f45597b7afade
Chris Bookholt <bookholt@chromium.org>
2022-12-02 01:17:41 +0000

    Add getter method to WebGPUInProcessContext to retrieve its CommandBufferHelper.
    I'm in the process of slowly writing a fuzzer (see CL chain if curious) and would find it very handy to get ahold of the CommandBufferHelper from the WebGPUInProcessContext object.
    
    I think this addition seems reasonable because aside from being handy, it conforms to similar class features. WebGPUInProcessContext is a testonly=true feature, so this change does not impact shipping code.
    
    Change-Id: I47729115f4562420d3b3fa9c69523a9191b0be97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064218
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078396}

--- gpu/ipc/webgpu_in_process_context.cc
+++ gpu/ipc/webgpu_in_process_context.cc
@@ -108,4 +108,9 @@ InProcessCommandBuffer* WebGPUInProcessContext::GetCommandBufferForTest()
   return command_buffer_.get();
 }
 
+CommandBufferHelper* WebGPUInProcessContext::GetCommandBufferHelperForTest()
+    const {
+  return helper_.get();
+}
+
 }  // namespace gpu
