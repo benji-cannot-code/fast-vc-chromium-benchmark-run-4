42151f0a0462c30a0c303096a210dfb82561d793
Mohamed Omar <mohamedaomar@google.com>
2022-12-02 23:03:38 +0000

    Add an API for refreshing remote commands
    This API can be called only when the remote command service is started.
    For Tast tests it requires enrollment fixture or unicorn fixture.
    
    
    Bug: b/255734389
    Change-Id: Ide946b2f8f57bf85c89fce213e62b6be1f84923c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981915
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Mohamed Omar <mohamedaomar@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Quick-Run: Mohamed Omar <mohamedaomar@google.com>
    Auto-Submit: Mohamed Omar <mohamedaomar@google.com>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078812}

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -1288,6 +1288,16 @@ var policyTests = [
 
 ];
 
+var remoteCommandsTests = [
+  function refreshRemoteCommands() {
+    chrome.autotestPrivate.refreshRemoteCommands(
+      chrome.test.callbackPass(function () {
+        chrome.test.succeed();
+      })
+    );
+  },
+];
+
 var arcPerformanceTracingTests = [
   function arcAppTracingNormal() {
     chrome.autotestPrivate.arcAppTracingStart(async function() {
@@ -1575,6 +1585,7 @@ var systemWebAppsTests = [
       'arcEnabled': arcEnabledTests,
       'arcProcess': arcProcessTests,
       'enterprisePolicies': policyTests,
+      'remoteCommands': remoteCommandsTests,
       'arcPerformanceTracing': arcPerformanceTracingTests,
       'overviewDefault': overviewTests,
       'overviewDrag': overviewDragTests,
