6b8ba9ecb54ecdca5db256a361bcf8d6677770ca
Colin Blundell <blundell@chromium.org>
2022-12-02 09:33:42 +0000

    [GLImagePbufferBacking] Eliminate it knowing about GLImage altogether
    After previous refactorings and simplifications, GLImagePbufferBacking
    needs to know about GLImagePbuffer only to scope a reference to its own
    lifetime. This CL changes that need to be served by having
    GLImagePbufferBacking take in a OnceClosure that it invokes on its
    destruction, with DXVAVideoDecodeAccelerator passing in a closure that
    is bound to its GLImagePbuffer reference and drops that reference when
    run.
    
    The benefit is that now GLImagePbufferBacking no longer needs to know
    about GLImage at all. A followup CL will rename it appropriately and
    move GLImagePbuffer (back) into //media.
    
    Elimination of either (1) DXVAVideoDecodeAccelerator's knowledge of
    GLImage or (2) DXVAVideoDecodeAccelerator altogether is also being
    tackled, but is a longer-term project (see crbug.com/1343723 and its
    blocking bugs).
    
    Bug: 1484438
    Change-Id: I53371fb80b5aa44a701d7c7b8ad679bf9a419dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065951
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078491}

--- gpu/command_buffer/service/shared_image/gl_image_pbuffer_backing.h
+++ gpu/command_buffer/service/shared_image/gl_image_pbuffer_backing.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_GL_IMAGE_PBUFFER_BACKING_H_
 #define GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_GL_IMAGE_PBUFFER_BACKING_H_
 
-#include "gpu/command_buffer/service/shared_image/gl_image_pbuffer.h"
 #include "gpu/command_buffer/service/shared_image/gl_texture_common_representations.h"
 #include "gpu/command_buffer/service/shared_image/gl_texture_image_backing_helper.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_backing.h"
@@ -15,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 // Implementation of SharedImageBacking that takes in a caller-created GL
-// Texture and GLImagePbuffer, scopes their lifetime, and exposes the texture
-// via SharedImageRepresentations. Used with the legacy mailbox implementation
-// in //media's DXVA video decoder. DO NOT USE FOR ANY OTHER PURPOSE.
+// Texture, scopes its lifetime, and exposes it via SharedImageRepresentations.
+// Used with the legacy mailbox implementation in //media's DXVA video decoder.
+// DO NOT USE FOR ANY OTHER PURPOSE.
 // TODO(crbug.com/1384438): Remove this class.
 class GPU_GLES2_EXPORT GLImagePbufferBacking
     : public ClearTrackingSharedImageBacking,
@@ -26,8 +25,9 @@ class GPU_GLES2_EXPORT GLImagePbufferBacking
   // Used when GLImagePbufferBacking is serving as a temporary SharedImage
   // wrapper to an already-allocated texture. The returned backing will not
   // create any new textures.
+  // |on_destruction_closure| is invoked on destruction of this object.
   static std::unique_ptr<GLImagePbufferBacking> CreateFromGLTexture(
-      scoped_refptr<GLImagePbuffer> image,
+      base::OnceClosure on_destruction_closure,
       const Mailbox& mailbox,
       viz::ResourceFormat format,
       const gfx::Size& size,
@@ -46,7 +46,7 @@ class GPU_GLES2_EXPORT GLImagePbufferBacking
 
  private:
   GLImagePbufferBacking(
-      scoped_refptr<GLImagePbuffer> image,
+      base::OnceClosure on_destruction_closure,
       const Mailbox& mailbox,
       viz::SharedImageFormat format,
       const gfx::Size& size,
@@ -87,7 +87,7 @@ class GPU_GLES2_EXPORT GLImagePbufferBacking
   void GLTextureImageRepresentationEndAccess(bool readonly) override;
   void GLTextureImageRepresentationRelease(bool have_context) override;
 
-  scoped_refptr<GLImagePbuffer> image_;
+  base::ScopedClosureRunner on_destruction_closure_runner_;
 
   scoped_refptr<gles2::TexturePassthrough> passthrough_texture_;
 
