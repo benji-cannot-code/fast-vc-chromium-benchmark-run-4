a89c75381da4baf6b9c0e8bc3727bcc512b6cdbb
Curtis McMullan <curtismcmullan@chromium.org>
2022-12-02 00:26:38 +0000

    autocorrect: add input options observer
    This CL adds the required input method options observer to implement the
    metrics described in the linked bug. We want to record any transitions a
    user has between autocorrect settings, and thus we will need to observe
    any changes to the autocorrect settings via an observer.
    
    This CL adds the required observer, and a future CL will record the
    autocorrect settings transition metric.
    
    BUG=b:257146732
    
    Change-Id: I02d3654245ace25e1c83d786140f4dfe47bb6f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025733
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078371}

---
+++ chrome/browser/ash/input_method/input_method_options_observer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/input_method/input_method_options_observer.h"
+
+#include "chrome/common/pref_names.h"
+
+namespace ash::input_method {
+
+InputMethodOptionsObserver::InputMethodOptionsObserver(PrefService* prefs) {
+  pref_change_registrar_.Init(prefs);
+}
+
+void InputMethodOptionsObserver::Observe(
+    InputMethodOptionsObserver::OnInputMethodOptionsChanged callback) {
+  pref_change_registrar_.Add(prefs::kLanguageInputMethodSpecificSettings,
+                             std::move(callback));
+}
+
+}  // namespace ash::input_method
