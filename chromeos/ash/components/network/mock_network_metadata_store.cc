c7e1956d83217037b3f14b2b3e38b3389de75b18
Emmanuel Arias Soto <eariassoto@google.com>
2022-12-02 11:54:19 +0000

    [CrOS Cellular] Create MockNetworkMetadataStore
    This change adds a mock class for NetworkMetadataStore. This will
    allow to unit test the future ApnMigrator service.
    
    Bug: b:162365553
    Change-Id: I6babbcf66693860c9cca9b1b890bbac3d7457d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047631
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078530}

---
+++ chromeos/ash/components/network/mock_network_metadata_store.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/network/mock_network_metadata_store.h"
+
+namespace ash {
+
+MockNetworkMetadataStore::MockNetworkMetadataStore()
+    : NetworkMetadataStore(
+          /*network_configuration_handler=*/nullptr,
+          /*network_connection_handler=*/nullptr,
+          /*network_state_handler=*/nullptr,
+          /*profile_pref_service=*/nullptr,
+          /*device_pref_service=*/nullptr,
+          /*is_enterprise_managed=*/false) {}
+
+MockNetworkMetadataStore::~MockNetworkMetadataStore() = default;
+
+}  // namespace ash
