9fc7e6db0bc79b5f513569175555c22a376722b3
Sameer Vijaykar <samvi@google.com>
2022-12-02 10:04:18 +0000

    Add an observer interface for ICE controller events. (#2/n)
    The new interface conveys ICE controller events with blink data types that mirror the relevant webrtc native data structures, but appropriate to expose to JS and compatible with thread hops.
    
    Bug: 1369096, webrtc:14131
    Change-Id: Ibf2171e28c9d5766228481a02c429fc6101c939f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065681
    Commit-Queue: Sameer Vijaykar <samvi@google.com>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078499}

---
+++ third_party/webrtc_overrides/p2p/base/ice_ping_proposal.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include "third_party/webrtc_overrides/p2p/base/ice_ping_proposal.h"
+
+#include "third_party/webrtc/p2p/base/ice_controller_interface.h"
+#include "third_party/webrtc_overrides/p2p/base/ice_proposal.h"
+
+namespace blink {
+
+IcePingProposal::IcePingProposal(
+    const cricket::IceControllerInterface::PingResult& ping_result,
+    bool reply_expected)
+    : IceProposal(reply_expected),
+      connection_(ping_result.connection),
+      recheck_delay_ms_(ping_result.recheck_delay_ms) {}
+
+}  // namespace blink
