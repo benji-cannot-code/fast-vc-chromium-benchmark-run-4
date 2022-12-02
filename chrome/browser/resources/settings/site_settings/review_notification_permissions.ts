c40f4e553e58812f9566a2d1afe495d13d8b68f1
Tom Van Goethem <tov@chromium.org>
2022-12-02 11:36:07 +0000

    [SafetyCheck] Fix intercepting keys
    The functionality for ctrl+z introduced by CL 4037744 captured keys, and
    ran preventDefault() on the associated event, intercepting and blocking
    any key presses on the page. This CL removes those calls and fixes this
    issue.
    
    Bug: 1345920, 1392664
    Change-Id: I4285097bdce0cc1b7746a0014b684b6d3425421f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4071806
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078523}

--- chrome/browser/resources/settings/site_settings/review_notification_permissions.ts
+++ chrome/browser/resources/settings/site_settings/review_notification_permissions.ts
@@ -353,7 +353,6 @@ export class SettingsReviewNotificationPermissionsElement extends
   private onKeyDown_(e: KeyboardEvent) {
     // Only allow undoing via ctrl+z when the undo toast is opened.
     if (!this.$.undoToast.open) {
-      e.preventDefault();
       return;
     }
 
@@ -366,7 +365,6 @@ export class SettingsReviewNotificationPermissionsElement extends
      * and ctrl on mac).
      */
     if (e.key !== 'z') {
-      e.preventDefault();
       return;
     }
     const excludedModifiers = [e.altKey, e.shiftKey];
@@ -380,7 +378,6 @@ export class SettingsReviewNotificationPermissionsElement extends
     // </if>
     if (!excludedModifiers.some(Boolean) && targetModifier) {
       this.undoLastAction();
-      e.preventDefault();
     }
   }
 
