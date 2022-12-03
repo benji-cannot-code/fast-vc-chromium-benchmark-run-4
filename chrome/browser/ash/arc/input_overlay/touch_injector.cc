99e05df9fd236b0233e003f7792f0a27c96f25c3
Cici Ruan <cuicuiruan@google.com>
2022-12-03 01:29:30 +0000

    arc/gio: resume event target on app window layer from overlay layer for menu reposition
    Follow up to https://crrev.com/c/3943127. When menu entry finishes
    the drag moving, it needs to set the event target back to the app window
    layer from the overlay layer. Otherwise, the app window layer can't
    receive any input events after menu entry dragging.
    
          events after menu entry reposition
    
    Bug: b/250900717
    Test: manual test to check if app window layer can receive the input
    Change-Id: I0f17a1f23fa9d22b283e5722b3a9bd3ef4b97dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4053478
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078871}

--- chrome/browser/ash/arc/input_overlay/touch_injector.cc
+++ chrome/browser/ash/arc/input_overlay/touch_injector.cc
@@ -607,7 +607,7 @@ ui::EventDispatchDetails TouchInjector::RewriteEvent(
     // Release all active touches when the display mode is changed from |kView|
     // to |kMenu|.
     CleanupTouchEvents();
-    display_overlay_controller_->SetDisplayMode(DisplayMode::kMenu);
+    display_overlay_controller_->SetDisplayMode(DisplayMode::kPreMenu);
     return SendEvent(continuation, &event);
   }
 
