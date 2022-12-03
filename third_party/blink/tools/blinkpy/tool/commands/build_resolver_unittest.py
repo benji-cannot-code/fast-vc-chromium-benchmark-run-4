f61862477f636c23891fedb4b7be19bf6bfc41a0
Jonathan Lee <jonathanjlee@google.com>
2022-12-03 03:35:16 +0000

    [blinkpy] 15/N: Update from CI results
    The build resolver for `update-metadata` and `rebaseline-cl` relied on
    the `builders.json` config to tell if a builder was 'try' or 'ci'. This
    is problematic when a builder has the same name in both the 'try' and
    'ci' buckets because `builders.json` is keyed on the builder name.
    
    Instead, allow `update-metadata` users to specify the bucket explicitly
    with an optional `{ci,try}/` prefix in the `--build` parameter
    (defaulting to 'try'). This syntax resembles that of other tools like
    lucicfg and buildbucket.
    
    This is a no-op change for `rebaseline-cl`, which continues to assume
    only try builders.
    
    Also, add a build range syntax "<builder>:<start>-<end>" so that it is
    easy to update expectations over a longer time horizon (which gives a
    stronger signal of test flakiness).
    
    Bug: 1299650
    Test: git cl issue 3601551
    Test: ./blink_tool.py rebaseline-cl --verbose --dry-run --patchset=15
    Change-Id: I2ae30e92e7ed0b3c3b00eff43b6fc256d1469603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076036
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078899}

--- third_party/blink/tools/blinkpy/tool/commands/build_resolver_unittest.py
+++ third_party/blink/tools/blinkpy/tool/commands/build_resolver_unittest.py
@@ -25,7 +25,7 @@ class BuildResolverTest(LoggingTestCase):
         # A CL should only be required for try builders without explicit build
         # numbers.
         self.git_cl = MockGitCL(self.host, issue_number='None')
-        self.resolver = BuildResolver(self.host.builders, self.git_cl)
+        self.resolver = BuildResolver(self.git_cl)
 
     def test_resolve_last_failing_ci_build(self):
         self.host.web.append_prpc_response({
@@ -35,6 +35,7 @@ class BuildResolverTest(LoggingTestCase):
                         'id': '123',
                         'builder': {
                             'builder': 'Fake Test Linux',
+                            'bucket': 'ci',
                         },
                         'number': 123,
                         'status': 'FAILURE',
@@ -43,10 +44,10 @@ class BuildResolverTest(LoggingTestCase):
             }],
         })
         build_statuses = self.resolver.resolve_builds(
-            [Build('Fake Test Linux')])
+            [Build('Fake Test Linux', bucket='ci')])
         self.assertEqual(
             build_statuses, {
-                Build('Fake Test Linux', 123, '123'):
+                Build('Fake Test Linux', 123, '123', 'ci'):
                 TryJobStatus('COMPLETED', 'FAILURE'),
             })
         (_, body), = self.host.web.requests
@@ -63,7 +64,8 @@ class BuildResolverTest(LoggingTestCase):
                             'status': 'FAILURE',
                         },
                         'fields': ('builds.*.id,builds.*.number,'
-                                   'builds.*.builder.builder,builds.*.status'),
+                                   'builds.*.builder.builder,'
+                                   'builds.*.builder.bucket,builds.*.status'),
                     },
                 }],
             })
@@ -75,6 +77,7 @@ class BuildResolverTest(LoggingTestCase):
                     'id': '123',
                     'builder': {
                         'builder': 'Fake Test Linux',
+                        'bucket': 'ci',
                     },
                     'number': 123,
                     'status': 'FAILURE',
@@ -84,6 +87,7 @@ class BuildResolverTest(LoggingTestCase):
                     'id': '456',
                     'builder': {
                         'builder': 'linux-rel',
+                        'bucket': 'try',
                     },
                     'number': 456,
                     'status': 'SCHEDULED',
@@ -91,12 +95,12 @@ class BuildResolverTest(LoggingTestCase):
             }],
         })
         build_statuses = self.resolver.resolve_builds([
-            Build('Fake Test Linux', 123),
+            Build('Fake Test Linux', 123, bucket='ci'),
             Build('linux-rel', 456),
         ])
         self.assertEqual(
             build_statuses, {
-                Build('Fake Test Linux', 123, '123'):
+                Build('Fake Test Linux', 123, '123', 'ci'):
                 TryJobStatus('COMPLETED', 'FAILURE'),
                 Build('linux-rel', 456, '456'):
                 TryJobStatus('SCHEDULED', None),
@@ -111,8 +115,10 @@ class BuildResolverTest(LoggingTestCase):
                             'bucket': 'ci',
                             'builder': 'Fake Test Linux',
                         },
-                        'buildNumber': 123,
-                        'fields': 'id,number,builder.builder,status',
+                        'buildNumber':
+                        123,
+                        'fields': ('id,number,builder.builder,builder.bucket,'
+                                   'status'),
                     },
                 }, {
                     'getBuild': {
@@ -121,8 +127,10 @@ class BuildResolverTest(LoggingTestCase):
                             'bucket': 'try',
                             'builder': 'linux-rel',
                         },
-                        'buildNumber': 456,
-                        'fields': 'id,number,builder.builder,status',
+                        'buildNumber':
+                        456,
+                        'fields': ('id,number,builder.builder,builder.bucket,'
+                                   'status'),
                     },
                 }],
             })
