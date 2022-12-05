2b0b737a5a444751d5458b448beb72a0950eb3b0
Gabriel Charette <gab@chromium.org>
2022-12-05 16:02:22 +0000

    [base][codehealth] Use PlatformThreadRef in AssociatedThreadId
    It's faster than PlatformThreadId for checks and GetBoundThreadId() is
    no longer used.
    
    This will result in better alignment when `original_thread_ref_` is
    added in crrev.com/c/4060840
    
    Bug: 1344573
    Change-Id: Ifa1e54f7fb03fe13ffd5a050433987dc527492da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061800
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079251}

--- base/task/sequence_manager/associated_thread_id.cc
+++ base/task/sequence_manager/associated_thread_id.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/sequence_manager/associated_thread_id.h"
 
+#include "base/check.h"
+#include "base/dcheck_is_on.h"
+
 namespace base {
 namespace sequence_manager {
 namespace internal {
@@ -13,13 +16,12 @@ AssociatedThreadId::AssociatedThreadId() = default;
 AssociatedThreadId::~AssociatedThreadId() = default;
 
 void AssociatedThreadId::BindToCurrentThread() {
-  // TODO(altimin): Remove this after MessageLoopImpl is gone and
-  // initialisation is simplified.
-  auto current_thread_id = PlatformThread::CurrentId();
-  [[maybe_unused]] auto prev_thread_id =
-      thread_id_.exchange(current_thread_id, std::memory_order_release);
-  DCHECK(prev_thread_id == current_thread_id ||
-         prev_thread_id == kInvalidThreadId);
+#if DCHECK_IS_ON()
+  const auto prev_thread_ref = thread_ref_.load(std::memory_order_relaxed);
+  DCHECK(prev_thread_ref.is_null() ||
+         prev_thread_ref == PlatformThread::CurrentRef());
+#endif
+  thread_ref_.store(PlatformThread::CurrentRef(), std::memory_order_release);
 
   // Rebind the thread and sequence checkers to the current thread/sequence.
   DETACH_FROM_THREAD(thread_checker);
--- base/task/sequence_manager/associated_thread_id.h
+++ base/task/sequence_manager/associated_thread_id.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "base/threading/platform_thread.h"
+#include "base/threading/platform_thread_ref.h"
 #include "base/threading/thread_checker.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -51,24 +52,6 @@ class BASE_EXPORT AssociatedThreadId
   // Can only be called once.
   void BindToCurrentThread();
 
-  // Returns the id of the thread bound to this object via a previous call to
-  // BindToCurrentThread(), nullopt if no thread was bound yet.
-  //
-  // This method guarantees a happens-before ordering with
-  // BindToCurrentThread(), that is all memory writes that happened-before the
-  // call to BindToCurrentThread() will become visible side-effects in the
-  // current thread.
-  //
-  // Attention: The result might be stale by the time this method returns.
-  absl::optional<PlatformThreadId> GetBoundThreadId() const {
-    auto thread_id = thread_id_.load(std::memory_order_acquire);
-    if (thread_id == kInvalidThreadId) {
-      return absl::nullopt;
-    } else {
-      return thread_id;
-    }
-  }
-
   // Checks whether this object has already been bound to a thread.
   //
   // This method guarantees a happens-before ordering with
@@ -78,7 +61,7 @@ class BASE_EXPORT AssociatedThreadId
   //
   // Attention: The result might be stale by the time this method returns.
   bool IsBound() const {
-    return thread_id_.load(std::memory_order_acquire) != kInvalidThreadId;
+    return !thread_ref_.load(std::memory_order_acquire).is_null();
   }
 
   // Checks whether this object is bound to the current thread. Returns false if
@@ -91,20 +74,15 @@ class BASE_EXPORT AssociatedThreadId
   //
   // Attention:: The result might be stale by the time this method returns.
   bool IsBoundToCurrentThread() const {
-    return thread_id_.load(std::memory_order_relaxed) ==
-           PlatformThread::CurrentId();
+    return thread_ref_.load(std::memory_order_relaxed) ==
+           PlatformThread::CurrentRef();
   }
-
-  // TODO(eseckler): Add a method that checks that we are either bound already
-  // or on the thread which created us and use it in any_thread() accessors.
 
  private:
   friend class base::RefCountedThreadSafe<AssociatedThreadId>;
   ~AssociatedThreadId();
 
-  // All access to this member can be std::memory_order_relaxed as this class
-  // provides no ordering guarantees.
-  std::atomic<PlatformThreadId> thread_id_{kInvalidThreadId};
+  std::atomic<PlatformThreadRef> thread_ref_{};
 };
 
 }  // namespace internal
