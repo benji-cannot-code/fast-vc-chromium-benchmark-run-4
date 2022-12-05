0a34f02d8bc873e878d500bfb9d650053bbb4ffe
Ramin Halavati <rhalavati@chromium.org>
2022-12-05 09:12:16 +0000

    Add ScreenAI DLC installer for ChromeOS.
    An installer is added to download and install ScreenAI DLC if needed.
    See go/chrome-screen-ai for more context.
    Also:
     - The heuristics to install/uninstall the component/DLC is moved from
       ScreenAIComponentInstaller to ScreenAIInstallState to be shared
       between all platforms.
     - ScreenAIInstallState is expanded to keep more states, including
       not downloaded, downloading, failed, and ready.
    
    Bug: 1278249
    Change-Id: I28cd75d049b7d0be83cddd5920f1e58ecb3878f4
    Ax-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013302
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079125}

--- chrome/browser/component_updater/screen_ai_component_installer.h
+++ chrome/browser/component_updater/screen_ai_component_installer.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "components/component_updater/component_installer.h"
-#include "components/prefs/pref_service.h"
 #include "components/update_client/update_client.h"
 
 namespace component_updater {
@@ -23,8 +22,7 @@ class ScreenAIComponentInstallerPolicy : public ComponentInstallerPolicy {
       const ScreenAIComponentInstallerPolicy&) = delete;
   ~ScreenAIComponentInstallerPolicy() override;
 
-  static void DeleteComponentOrScheduleDeletionIfNeeded(
-      PrefService* global_prefs);
+  static void DeleteComponent();
 
  private:
   // ComponentInstallerPolicy::
@@ -48,7 +46,7 @@ class ScreenAIComponentInstallerPolicy : public ComponentInstallerPolicy {
 // Call once during startup to make the component update service aware of
 // the ScreenAI component.
 void RegisterScreenAIComponent(ComponentUpdateService* cus,
-                               PrefService* global_prefs);
+                               PrefService* local_state);
 
 }  // namespace component_updater
 
---
+++ components/services/screen_ai/public/cpp/screen_ai_chromeos_installer.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SERVICES_SCREEN_AI_PUBLIC_CPP_SCREEN_AI_CHROMEOS_INSTALLER_H_
+#define COMPONENTS_SERVICES_SCREEN_AI_PUBLIC_CPP_SCREEN_AI_CHROMEOS_INSTALLER_H_
+
+#include "chromeos/ash/components/dbus/dlcservice/dlcservice_client.h"
+
+class PrefService;
+
+namespace screen_ai::chrome_os_installer {
+
+// If ScreenAI library is needed, registers for installation of its DLC. If not,
+// and after some delay, uninstalls the DLC.
+void ManageInstallation(PrefService* local_state);
+
+}  // namespace screen_ai::chrome_os_installer
+
+#endif  // COMPONENTS_SERVICES_SCREEN_AI_PUBLIC_CPP_SCREEN_AI_CHROMEOS_INSTALLER_H_
