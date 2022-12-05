ea6fb35c389b89dac63af2e61c8166d18d66464e
Tim Sergeant <tsergeant@chromium.org>
2022-12-05 03:56:26 +0000

    [APS] Restrict AppPreloadService to unmanaged regular accounts
    For the initial launch of APS, we will only be preloading OEM apps on
    regular, unmanaged/unsupervised accounts. This restricts the service so
    it does not start for other account types, preventing it from making
    network calls to the APS server when it is not necessary.
    
    Bug: b/259494622
    Change-Id: Id4ab62eed2224c0edbccfdbc1163cf65cc31e3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4073604
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079071}

--- chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_preload_service/app_preload_service.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
@@ -17,11 +18,12 @@ namespace apps {
 AppPreloadServiceFactory::AppPreloadServiceFactory()
     : ProfileKeyedServiceFactory(
           "AppPreloadService",
-          // Service is available in Kiosk, Chrome OS Guest mode (OTR), and
-          // Regular but not in incognito profiles.
+          // Service is currently only available in non-OTR regular profiles.
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOriginalOnly)
-              .WithGuest(ProfileSelection::kOffTheRecordOnly)
+              .WithGuest(ProfileSelection::kNone)
+              .WithSystem(ProfileSelection::kNone)
+              .WithAshInternals(ProfileSelection::kNone)
               .Build()) {
   DependsOn(web_app::WebAppProviderFactory::GetInstance());
   DependsOn(apps::AppServiceProxyFactory::GetInstance());
@@ -52,6 +54,13 @@ bool AppPreloadServiceFactory::IsAvailable(Profile* profile) {
   }
 
   if (!web_app::AreWebAppsEnabled(profile)) {
+    return false;
+  }
+
+  // App Preload Service is currently only available for unmanaged, unsupervised
+  // accounts.
+  if (profile->IsGuestSession() || profile->IsChild() ||
+      profile->GetProfilePolicyConnector()->IsManaged()) {
     return false;
   }
 
