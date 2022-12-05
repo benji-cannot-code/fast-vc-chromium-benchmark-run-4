00039e67a2098e41039fe2b164231bafcd740030
Gavin Williams <gavinwill@chromium.org>
2022-12-05 19:44:32 +0000

    Shimless: Add QR code service to Chrome delegate
    The new function `GenerateQrCode()` will be invoked by the Shimless
    service in a follow up CL to create better quality QR codes.
    
    Bug: b:260950299
    Change-Id: Icb79a19feaaf654a87405112dc92712ab1a4f541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076552
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079344}

--- ash/webui/shimless_rma/backend/shimless_rma_delegate.h
+++ ash/webui/shimless_rma/backend/shimless_rma_delegate.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WEBUI_SHIMLESS_RMA_BACKEND_SHIMLESS_RMA_DELEGATE_H_
 #define ASH_WEBUI_SHIMLESS_RMA_BACKEND_SHIMLESS_RMA_DELEGATE_H_
 
-namespace ash {
-namespace shimless_rma {
+#include <string>
+
+#include "base/functional/callback.h"
+
+namespace ash::shimless_rma {
 
 // A delegate which exposes browser functionality from //chrome to the Shimless
 // RMA UI.
@@ -24,9 +27,13 @@ class ShimlessRmaDelegate {
   // Sets the AccessibilityManager profile to the active profile to enable
   // accessibility features.
   virtual void RefreshAccessibilityManagerProfile() = 0;
+
+  // Returns a string encoded QR code image of `url`.
+  virtual void GenerateQrCode(
+      const std::string& url,
+      base::OnceCallback<void(const std::string& qr_code_image)> callback) = 0;
 };
 
-}  // namespace shimless_rma
-}  // namespace ash
+}  // namespace ash::shimless_rma
 
 #endif  // ASH_WEBUI_SHIMLESS_RMA_BACKEND_SHIMLESS_RMA_DELEGATE_H_
--- chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.h
+++ chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.h
@@ -8,8 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/shimless_rma/backend/shimless_rma_delegate.h"
 
-namespace ash {
-namespace shimless_rma {
+#include <string>
+
+#include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "chrome/services/qrcode_generator/public/mojom/qrcode_generator.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace ash::shimless_rma {
 
 class ChromeShimlessRmaDelegate : public ShimlessRmaDelegate {
  public:
@@ -25,9 +31,25 @@ class ChromeShimlessRmaDelegate : public ShimlessRmaDelegate {
   void ExitRmaThenRestartChrome() override;
   void ShowDiagnosticsDialog() override;
   void RefreshAccessibilityManagerProfile() override;
+  void GenerateQrCode(const std::string& url,
+                      base::OnceCallback<void(const std::string& qr_code_image)>
+                          callback) override;
+
+  void SetQRCodeServiceForTesting(
+      mojo::Remote<qrcode_generator::mojom::QRCodeGeneratorService>&& remote);
+
+ private:
+  void OnQrCodeGenerated(
+      base::OnceCallback<void(const std::string& qr_code_image)> callback,
+      const qrcode_generator::mojom::GenerateQRCodeResponsePtr response);
+
+  // The remote for invoking the QRCodeGenerator service.
+  mojo::Remote<qrcode_generator::mojom::QRCodeGeneratorService>
+      qrcode_service_remote_;
+
+  base::WeakPtrFactory<ChromeShimlessRmaDelegate> weak_ptr_factory_{this};
 };
 
-}  // namespace shimless_rma
-}  // namespace ash
+}  // namespace ash::shimless_rma
 
 #endif  // CHROME_BROWSER_ASH_SHIMLESS_RMA_CHROME_SHIMLESS_RMA_DELEGATE_H_
