14f2598bb948a66033fce91ffac45d1afbb3daf0
Xiaocheng Hu <xiaochengh@chromium.org>
2022-12-05 21:56:24 +0000

    [anchor-position] Implement scroll-aware position fallback
    This patch brings scroll offset into position fallback algorithm to
    decide which fallback position to use, and also calculates info to
    help decide when to invalidate the current fallback position.
    
    Bug: 1371217
    Change-Id: I5256b1422e6f48cfbb1c70e6f47c7e65362d3496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995704
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079447}

---
+++ third_party/blink/web_tests/external/wpt/css/css-anchor-position/support/test-common.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Asserts that the anchored element is at the top/bottom/left/right of the
+// anchor.
+function assert_fallback_position(anchored, anchor, direction) {
+  let anchoredRect = anchored.getBoundingClientRect();
+  let anchorRect = anchor.getBoundingClientRect();
+  let message = `Anchored element should be at the ${direction} of anchor`;
+  switch (direction) {
+    case 'top':
+      assert_equals(anchoredRect.bottom, anchorRect.top, message);
+      return;
+    case 'bottom':
+      assert_equals(anchoredRect.top, anchorRect.bottom, message);
+      return;
+    case 'left':
+      assert_equals(anchoredRect.right, anchorRect.left, message);
+      return;
+    case 'right':
+      assert_equals(anchoredRect.left, anchorRect.right, message);
+      return;
+    default:
+      assert_unreached('unsupported direction');
+  }
+}
+
+async function waitUntilNextAnimationFrame() {
+  return new Promise(resolve => requestAnimationFrame(resolve));
+}
