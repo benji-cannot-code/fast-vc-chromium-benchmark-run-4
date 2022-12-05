62d69289756de4babad93985039202e54b18d645
Christian Dullweber <dullweber@chromium.org>
2022-12-05 09:58:40 +0000

    AboutThisSite: Enable placeholder description by default
    Bug: 1351714
    Change-Id: Ia574c9580626f755260886fe48c1d2d9848f6783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066129
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079137}

--- components/page_info/core/features.cc
+++ components/page_info/core/features.cc
@@ -46,7 +46,7 @@ BASE_FEATURE(kPageInfoAboutThisSiteMoreInfo,
 
 BASE_FEATURE(kPageInfoAboutThisSiteDescriptionPlaceholder,
              "PageInfoAboutThisSiteDescriptionPlaceholder",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if !BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kPageInfoHistoryDesktop,
