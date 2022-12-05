cda3a4c2a67915979bdb2f4ed743ed37a5c9abf0
Steinar H. Gunderson <sesse@chromium.org>
2022-12-05 10:49:22 +0000

    Combine the two CSSBitset-generating .py files into one.
    They are nearly identical, and we'll soon be adding a third one.
    
    Change-Id: I96fe22af56fa08933c975d21b0ac1765f48a4cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076248
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079151}

--- third_party/blink/renderer/core/css/css_property_value_set.cc
+++ third_party/blink/renderer/core/css/css_property_value_set.cc
@@ -26,10 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_identifier_value.h"
-#include "third_party/blink/renderer/core/css/logical_group_properties.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_context.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/core/css/property_bitsets.h"
 #include "third_party/blink/renderer/core/css/style_property_serializer.h"
 #include "third_party/blink/renderer/core/css/style_sheet_contents.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
