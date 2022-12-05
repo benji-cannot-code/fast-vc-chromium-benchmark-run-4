995572d1d49c01c3133906b359e744d9257127a5
Anunoy Ghosh <anunoy@chromium.org>
2022-12-05 17:59:39 +0000

    Rename ExtensionPrefs method GetInstallTime to GetLastUpdateTime.
    The ExtensionPrefs::GetInstallTime retrieved the last_update_time pref
    which records the last time an extension was updated. This CL renames
    the method to GetLastUpdateTime to better match the pref retrieved. All call sites are updated to use the renamed method as well.
    
    This CL does not modify existing functionality.
    
    Bug: 1381132
    Change-Id: Ibea40753a9e6bec05ae4cae781655c447def578d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060893
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079289}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_unittest.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_unittest.cc
@@ -200,7 +200,7 @@ TEST_F(ExtensionTelemetryServiceTest, ProcessesSignal) {
   EXPECT_EQ(info->name(), kExtensionName[0]);
   EXPECT_EQ(info->version(), kExtensionVersion);
   EXPECT_EQ(info->install_timestamp_msec(),
-            extension_prefs_->GetInstallTime(kExtensionId[0]).ToJavaTime());
+            extension_prefs_->GetLastUpdateTime(kExtensionId[0]).ToJavaTime());
 }
 
 TEST_F(ExtensionTelemetryServiceTest, DiscardsInvalidSignal) {
@@ -278,7 +278,7 @@ TEST_F(ExtensionTelemetryServiceTest, GeneratesTelemetryReportWithNoSignals) {
               kExtensionVersion);
     EXPECT_EQ(
         telemetry_report_pb->reports(i).extension().install_timestamp_msec(),
-        extension_prefs_->GetInstallTime(kExtensionId[i]).ToJavaTime());
+        extension_prefs_->GetLastUpdateTime(kExtensionId[i]).ToJavaTime());
     // Verify that there is no signal data associated with the extension.
     EXPECT_EQ(telemetry_report_pb->reports(i).signals().size(), 0);
   }
@@ -304,7 +304,7 @@ TEST_F(ExtensionTelemetryServiceTest, GeneratesTelemetryReportWithSignal) {
               kExtensionVersion);
     EXPECT_EQ(
         telemetry_report_pb->reports(i).extension().install_timestamp_msec(),
-        extension_prefs_->GetInstallTime(kExtensionId[i]).ToJavaTime());
+        extension_prefs_->GetLastUpdateTime(kExtensionId[i]).ToJavaTime());
   }
 
   // Verify that first extension's report has signal data.
--- extensions/browser/api/declarative_webrequest/webrequest_rules_registry.cc
+++ extensions/browser/api/declarative_webrequest/webrequest_rules_registry.cc
@@ -288,7 +288,7 @@ WebRequestRulesRegistry::~WebRequestRulesRegistry() {}
 
 base::Time WebRequestRulesRegistry::GetExtensionInstallationTime(
     const std::string& extension_id) const {
-  return ExtensionPrefs::Get(browser_context_)->GetInstallTime(extension_id);
+  return ExtensionPrefs::Get(browser_context_)->GetLastUpdateTime(extension_id);
 }
 
 void WebRequestRulesRegistry::ClearCacheOnNavigation() {
