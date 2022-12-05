d91f39c67bfad7ed1a7c7861a5fe780fb8d59ce5
Sean Maher <spvw@chromium.org>
2022-12-05 18:54:43 +0000

    task posting v3: refactor PostTaskAndReplyWithResult with static init
    Clang codegen had created a static initializer in this file, so it
    needed to be handled separately, but it is fixed.
    
    Bug: 1026641
    Change-Id: Ia73b5e29aaebd4f337310a09bb35a517cb618a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4070825
    Auto-Submit: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079324}

--- chrome/browser/media/router/discovery/discovery_network_monitor.cc
+++ chrome/browser/media/router/discovery/discovery_network_monitor.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-#include "base/task/task_runner_util.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/default_tick_clock.h"
@@ -46,7 +45,8 @@ std::string ComputeNetworkId(
   return base::ToLowerASCII(base::HexEncode(hash.data(), hash.length()));
 }
 
-base::LazyInstance<DiscoveryNetworkMonitor>::Leaky g_discovery_monitor;
+base::LazyInstance<DiscoveryNetworkMonitor>::Leaky g_discovery_monitor =
+    LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -75,16 +75,16 @@ void DiscoveryNetworkMonitor::RemoveObserver(Observer* const observer) {
 }
 
 void DiscoveryNetworkMonitor::Refresh(NetworkIdCallback callback) {
-  base::PostTaskAndReplyWithResult(
-      task_runner_.get(), FROM_HERE,
+  task_runner_->PostTaskAndReplyWithResult(
+      FROM_HERE,
       base::BindOnce(&DiscoveryNetworkMonitor::UpdateNetworkInfo,
                      base::Unretained(this)),
       std::move(callback));
 }
 
 void DiscoveryNetworkMonitor::GetNetworkId(NetworkIdCallback callback) {
-  base::PostTaskAndReplyWithResult(
-      task_runner_.get(), FROM_HERE,
+  task_runner_->PostTaskAndReplyWithResult(
+      FROM_HERE,
       base::BindOnce(&DiscoveryNetworkMonitor::GetNetworkIdOnSequence,
                      base::Unretained(this)),
       std::move(callback));
