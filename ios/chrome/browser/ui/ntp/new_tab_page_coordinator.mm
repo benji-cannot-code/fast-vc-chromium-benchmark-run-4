b94269af125c2ab5fc8a0370933c6b5b37fe399f
Mike Pinkerton <pinkerton@google.com>
2022-12-05 19:28:48 +0000

    [iOS] Remove all VoiceSearch observation machinery for VoiceOver.
    Since VoiceSearch now works correctly with VoiceOver, there is no
    longer a need for all the observer infrastructure to watch for state
    changes and update the UI. Removing all of that then leaves several
    UIKit subclasses that now do nothing but enable themselves based on the
    state of the provider. It's better for the controller code to do this
    rather than have a view class do it, so these classes can be removed
    entirely.
    
    Fixed: 1382791
    Change-Id: I03a67bb3dd4fdbf7e555e8610b7704e1e6bf1f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080309
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079337}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -82,7 +82,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/named_guide.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
-#import "ios/chrome/browser/voice/voice_search_availability.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/ui_utils/ui_utils_api.h"
@@ -120,9 +119,6 @@ BASE_FEATURE(kEnableCheckForNewFollowContent,
                                      OverscrollActionsControllerDelegate,
                                      PrefObserverDelegate,
                                      SceneStateObserver> {
-  // Helper object managing the availability of the voice search feature.
-  VoiceSearchAvailability _voiceSearchAvailability;
-
   // Pref observer to track changes to prefs.
   std::unique_ptr<PrefObserverBridge> _prefObserverBridge;
   // Registrar for pref changes notifications.
@@ -550,18 +546,17 @@ BASE_FEATURE(kEnableCheckForNewFollowContent,
 - (void)initializeNTPComponents {
   self.ntpViewController = [[NewTabPageViewController alloc] init];
   self.ntpMediator = [[NTPHomeMediator alloc]
-             initWithWebState:self.webState
-           templateURLService:self.templateURLService
-                    URLLoader:UrlLoadingBrowserAgent::FromBrowser(self.browser)
-                  authService:self.authService
-              identityManager:IdentityManagerFactory::GetForBrowserState(
-                                  self.browser->GetBrowserState())
-        accountManagerService:ChromeAccountManagerServiceFactory::
-                                  GetForBrowserState(
-                                      self.browser->GetBrowserState())
-                   logoVendor:ios::provider::CreateLogoVendor(self.browser,
-                                                              self.webState)
-      voiceSearchAvailability:&_voiceSearchAvailability];
+           initWithWebState:self.webState
+         templateURLService:self.templateURLService
+                  URLLoader:UrlLoadingBrowserAgent::FromBrowser(self.browser)
+                authService:self.authService
+            identityManager:IdentityManagerFactory::GetForBrowserState(
+                                self.browser->GetBrowserState())
+      accountManagerService:ChromeAccountManagerServiceFactory::
+                                GetForBrowserState(
+                                    self.browser->GetBrowserState())
+                 logoVendor:ios::provider::CreateLogoVendor(self.browser,
+                                                            self.webState)];
   self.headerController = [[ContentSuggestionsHeaderViewController alloc] init];
   self.headerSynchronizer = [[ContentSuggestionsHeaderSynchronizer alloc]
       initWithCollectionController:self.ntpViewController
