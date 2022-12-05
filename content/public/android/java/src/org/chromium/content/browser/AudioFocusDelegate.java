ea2362974932688212530b5527039b1d6236cf18
Tommy Steimel <steimel@chromium.org>
2022-12-05 21:47:39 +0000

    Android Audio Focus: Update to new API for Oreo+
    This CL updates the Android AudioFocusDelegate to use the new API for
    requesting audio focus. This allows us to set which thread the
    `onAudioFocusChange()` callback is called on. This should fix threading
    issues we've seen coming from audio focus changes on Android.
    
    Bug: 1394490, 1276191
    Change-Id: I4efdef90982da6b6028f69edd917a4f651c9af51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076043
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079433}

--- content/public/android/java/src/org/chromium/content/browser/AudioFocusDelegate.java
+++ content/public/android/java/src/org/chromium/content/browser/AudioFocusDelegate.java
@@ -6,7 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser;
 
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
 import android.media.AudioManager;
+import android.os.Build;
+import android.os.Handler;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -32,13 +36,18 @@ public class AudioFocusDelegate implements AudioManager.OnAudioFocusChangeListen
 
     private int mFocusType;
     private boolean mIsDucking;
+    private AudioFocusRequest mFocusRequest;
 
     // Native pointer to C++ content::AudioFocusDelegateAndroid.
     // It will be set to 0 when the native AudioFocusDelegateAndroid object is destroyed.
     private long mNativeAudioFocusDelegateAndroid;
 
+    // Handle to the UI thread to ensure callbacks are on the correct thread.
+    private final Handler mHandler;
+
     private AudioFocusDelegate(long nativeAudioFocusDelegateAndroid) {
         mNativeAudioFocusDelegateAndroid = nativeAudioFocusDelegateAndroid;
+        mHandler = new Handler(ThreadUtils.getUiThreadLooper());
     }
 
     @CalledByNative
@@ -66,7 +75,14 @@ public class AudioFocusDelegate implements AudioManager.OnAudioFocusChangeListen
         assert ThreadUtils.runningOnUiThread();
         AudioManager am = (AudioManager) ContextUtils.getApplicationContext().getSystemService(
                 Context.AUDIO_SERVICE);
-        am.abandonAudioFocus(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (mFocusRequest != null) {
+                am.abandonAudioFocusRequest(mFocusRequest);
+                mFocusRequest = null;
+            }
+        } else {
+            am.abandonAudioFocus(this);
+        }
     }
 
     @CalledByNative
@@ -78,7 +94,24 @@ public class AudioFocusDelegate implements AudioManager.OnAudioFocusChangeListen
         AudioManager am = (AudioManager) ContextUtils.getApplicationContext().getSystemService(
                 Context.AUDIO_SERVICE);
 
-        int result = am.requestAudioFocus(this, AudioManager.STREAM_MUSIC, mFocusType);
+        int result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            AudioAttributes playbackAttributes =
+                    new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+                            .build();
+            mFocusRequest = new AudioFocusRequest.Builder(mFocusType)
+                                    .setAudioAttributes(playbackAttributes)
+                                    .setAcceptsDelayedFocusGain(false)
+                                    .setWillPauseWhenDucked(false)
+                                    .setOnAudioFocusChangeListener(this, mHandler)
+                                    .build();
+            result = am.requestAudioFocus(mFocusRequest);
+        } else {
+            result = am.requestAudioFocus(this, AudioManager.STREAM_MUSIC, mFocusType);
+        }
+
         return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
     }
 
