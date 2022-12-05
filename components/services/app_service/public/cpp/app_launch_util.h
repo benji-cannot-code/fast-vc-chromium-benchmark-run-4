be5e7e4b3527f6f54c2615323bb2f3cd82f6e59d
Nancy Wang <nancylingwang@chromium.org>
2022-12-05 01:37:30 +0000

    Remove types.mojom.h from the include file.
    BUG=1253250
    
    Change-Id: Ic1cac603b5b723cfb94ca9f7ee1437d3f9f1ec75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074454
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079048}

--- components/services/app_service/public/cpp/app_launch_util.h
+++ components/services/app_service/public/cpp/app_launch_util.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_APP_LAUNCH_UTIL_H_
 
 #include "base/component_export.h"
-#include "components/services/app_service/public/mojom/types.mojom.h"
 #include "components/services/app_service/public/protos/app_types.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/rect.h"
