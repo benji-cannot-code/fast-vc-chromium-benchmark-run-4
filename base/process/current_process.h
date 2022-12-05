f94dddd029fcf987d18dbc5efbeb9f66add39edc
Byoungchan Lee <daniel.l@hpcnt.com>
2022-12-05 08:22:36 +0000

    [base] Fix CurrentProcess::GetShortType to work when tracing is disabled
    Similar to https://crrev.com/c/4016554, use ENABLE_BASE_TRACING
    to protect the value of CurrentProcessType to prevent compilation
    failure when `enable_base_tracing = false`.
    
    Also add base/synchronization/lock.h import. Otherwise base::Lock
    will not be found when compiling base/process/current_process.cc.
    
    Bug: None
    Change-Id: Ib15148ff8a021202bbf0169a90b42d7e836188ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074112
    Commit-Queue: Daniel.L (Byoungchan) Lee <daniel.l@hpcnt.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079114}

--- base/process/current_process.h
+++ base/process/current_process.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/no_destructor.h"
 #include "base/process/process_handle.h"
+#include "base/synchronization/lock.h"
 #include "base/trace_event/base_tracing.h"
 #include "build/buildflag.h"
 
