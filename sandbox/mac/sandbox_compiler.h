1b36fc68b3a560d15600f8c72c9b959238ec2e9d
Robert Sesek <rsesek@chromium.org>
2022-12-05 21:24:49 +0000

    mac: Add support for compiling sandbox profiles to the binary format
    In the future, this will let profiles be pre-compiled and reused across
    multiple process launches. This adds the support to the SandboxCompiler
    and SeatbeltExec classes.
    
    Bug: 1315988
    Change-Id: I32a4f0b9adbc9c7c96f94afae7a643add0457452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079319
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079413}

--- sandbox/mac/sandbox_compiler.h
+++ sandbox/mac/sandbox_compiler.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "sandbox/mac/seatbelt.h"
 #include "sandbox/mac/seatbelt.pb.h"
 #include "sandbox/mac/seatbelt_export.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"  // nogncheck
 
 namespace sandbox {
 
@@ -17,7 +19,28 @@ namespace sandbox {
 // initialization and cleanup.
 class SEATBELT_EXPORT SandboxCompiler {
  public:
+  enum class Target {
+    // The result of compilation is a SandboxPolicy proto containing the policy
+    // string source and a map of key/value pairs.
+    kSource,
+
+    // The result of compilation is a SandboxPolicy proto containing a sealed,
+    // compiled, binary sandbox policy that can be applied immediately.
+    kCompiled,
+
+    // The result of compilation is to be immediately applied to the current
+    // process using CompileAndApplyProfile().
+    kImmediate = kCompiled,
+  };
+
+  // Creates a compiler in the default mode, `Target::kSource`.
   SandboxCompiler();
+
+  // Creates a compiler with the specified target mode.
+  explicit SandboxCompiler(Target mode);
+
+  // Creates a compiler with the specified policy `profile_str` in
+  // `Target::kImmediate` mode.
   explicit SandboxCompiler(const std::string& profile_str);
 
   ~SandboxCompiler();
@@ -41,11 +64,16 @@ class SEATBELT_EXPORT SandboxCompiler {
   // Compiles and applies the profile; returns true on success.
   bool CompileAndApplyProfile(std::string* error);
 
-  // Compiles the policy into a sandbox policy proto.
-  const mac::SandboxPolicy& CompilePolicyToProto();
+  // Compiles the policy into a sandbox policy proto. Returns `absl::nullopt` on
+  // error, with an error message set.
+  absl::optional<mac::SandboxPolicy> CompilePolicyToProto(std::string* error);
 
  private:
-  mac::SandboxPolicy policy_;
+  const Target mode_;
+
+  mac::SourcePolicy policy_;
+
+  Seatbelt::Parameters params_;
 };
 
 }  // namespace sandbox
