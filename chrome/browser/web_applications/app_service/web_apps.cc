9026e2e6e0a424a72cd1c54560b917c56f55b749
Nancy Wang <nancylingwang@chromium.org>
2022-12-05 02:31:47 +0000

    Modify GetWebAppCompressedIconData to use the uncompressed icon process.
    This is the follow up for CL:4055064, CL:4040524 to remove the icon
    effects parameter in the get app compressed icon data interface.
    
    Modify and simplify the current getting compressed icon data process to
    alway reuse the uncompressed icon loading process to generate the
    uncompressed icon, and convert to the compressed icon data.
    
    BUG=1380608
    
    Change-Id: I48f1019d14f15c28ee559c1d8a4f5eaa7c233953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075431
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079056}

--- chrome/browser/web_applications/app_service/web_apps.cc
+++ chrome/browser/web_applications/app_service/web_apps.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
+#include "chrome/browser/apps/app_service/app_icon/app_icon_util.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/profiles/profile.h"
@@ -103,14 +104,12 @@ void WebApps::LoadIcon(const std::string& app_id,
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 void WebApps::GetCompressedIconData(const std::string& app_id,
-                                    IconEffects icon_effects,
                                     apps::IconType icon_type,
                                     int32_t size_in_dip,
                                     ui::ResourceScaleFactor scale_factor,
                                     apps::LoadIconCallback callback) {
-  publisher_helper().GetCompressedIconData(app_id, icon_effects, icon_type,
-                                           size_in_dip, scale_factor,
-                                           std::move(callback));
+  publisher_helper().GetCompressedIconData(app_id, icon_type, size_in_dip,
+                                           scale_factor, std::move(callback));
 }
 #endif
 
