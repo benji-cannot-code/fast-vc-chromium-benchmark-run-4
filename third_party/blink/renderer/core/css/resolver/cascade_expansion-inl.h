94fe800827d87ec410e86fe27a7df8bc664b03a4
Steinar H. Gunderson <sesse@chromium.org>
2022-12-05 12:57:17 +0000

    Fewer calls to CSSProperty::GetVisitedProperty().
    Speed it up with a bit set, which is cheaper than calling
    virtual functions over and over again.
    
    +2.4% on MotionMark paths (95% CI: [+0.4%, +4.5%]).
    
    Style perftest (Zen 3, LTO but no PGO):
    
    Initial style (µs)     Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                7594      7492   +1.4%  [ +0.4%,  +2.2%]
    Encyclopedia            73408     73455   -0.1%  [ -0.8%,  +0.5%]
    Extension               94169     93383   +0.8%  [ +0.2%,  +1.5%]
    News                    31866     31602   +0.8%  [ +0.1%,  +1.5%]
    Search                   2056      2060   -0.2%  [ -1.1%,  +0.7%]
    Social1                 17914     17871   +0.2%  [ -0.4%,  +0.9%]
    Social2                   797       797   -0.1%  [ -1.4%,  +1.0%]
    Sports                  25518     25520   -0.0%  [ -1.3%,  +0.8%]
    Video                   31558     31333   +0.7%  [ +0.1%,  +1.3%]
    Geometric mean                            +0.4%  [ -0.0%,  +0.8%]
    
    Recalc style (µs)      Before     After    Perf      95% CI (BCa)
    =================== ========= ========= ======= =================
    ECommerce                9164      8813   +4.0%  [ +3.0%,  +5.0%]
    Encyclopedia            65688     65372   +0.5%  [ -0.1%,  +1.1%]
    Extension               91314     89900   +1.6%  [ +1.0%,  +2.2%]
    News                    24372     24137   +1.0%  [ +0.3%,  +1.6%]
    Search                    190       186   +2.2%  [ -0.4%,  +6.4%]
    Social1                 13089     12994   +0.7%  [ +0.1%,  +1.3%]
    Social2                   397       396   +0.2%  [ -1.9%,  +2.2%]
    Sports                  20534     20338   +1.0%  [ +0.2%,  +1.9%]
    Video                   20438     20173   +1.3%  [ +0.7%,  +2.0%]
    Geometric mean                            +1.4%  [ +0.8%,  +1.9%]
    
    Change-Id: Ifaf473d8bc9312bd0d0e5bc77c3a1d1b792fb8c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076249
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079179}

--- third_party/blink/renderer/core/css/resolver/cascade_expansion-inl.h
+++ third_party/blink/renderer/core/css/resolver/cascade_expansion-inl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/cascade_expansion.h"
 
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/core/css/property_bitsets.h"
 #include "third_party/blink/renderer/core/css/resolver/match_result.h"
 
 namespace blink {
@@ -71,7 +72,7 @@ void ExpandCascade(const MatchedProperties& matched_properties,
         callback(priority, property, CSSPropertyName(id), reference.Value(),
                  matched_properties.types_.tree_order);
       }
-      if (expand_visited) {
+      if (expand_visited && kPropertiesWithVisited.Has(id)) {
         const CSSProperty* visited_property = property.GetVisitedProperty();
         if (visited_property && !filter.Rejects(*visited_property)) {
           callback(priority, *visited_property,
