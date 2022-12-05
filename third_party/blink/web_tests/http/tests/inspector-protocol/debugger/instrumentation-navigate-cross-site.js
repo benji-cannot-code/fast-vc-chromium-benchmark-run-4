c234029ad7a39457d676ebb2b84e973948f53150
Jaroslav Sevcik <jarin@chromium.org>
2022-12-05 08:38:01 +0000

    [inspector] Introduce a restricted instrumentation pause
    We introduce a new instrumentation pause state in the renderer.
    The instrumentation pause is entered on script creation
    if the instrumentation breakpoints are enabled with
    Debugger.setInstrumentationBreakpoint. The purpose of the pause
    is to give the debugger a chance to insert its breakpoints
    correctly before the script runs.
    
    In this patch, we restrict the messages that are handled during
    the instrumentation pause to the ones that are handled by the
    devtools IO session (those are mostly breakpoint management
    messages). Handling of other messages are postponed until the
    instrumentation pause resumes.
    
    Without this patch, the instrumentation pause behaves the same
    as the normal pause, causing some undesirable interactions
    with layouting, navigation, etc. (e.g., see crbug.com/1304095).
    
    Note that the instrumentation breakpoints/pauses are
    an experiment in DevTools,  currently off by default.
    
    Bug: crbug.com/1354043
    Change-Id: I44e9e8473fcb10a2868974aa8d20a07e9b00130d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891025
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079116}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/debugger/instrumentation-navigate-cross-site.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests cross site navigation during instrumentation pause.`);
+
+  await dp.Target.setDiscoverTargets({discover: true});
+  await dp.Page.enable();
+  await dp.Debugger.enable();
+  await dp.Debugger.setInstrumentationBreakpoint(
+      {instrumentation: 'beforeScriptExecution'});
+
+  const result = session.evaluate('42');
+
+  const expressionPause = await dp.Debugger.oncePaused();
+  testRunner.log(`paused reason: ${expressionPause.params.reason}`);
+
+  const navigated = dp.Page.onceFrameNavigated();
+  session.navigate('http://devtools.oopif.test:8000/inspector-protocol/');
+
+  dp.Debugger.resume({terminateOnresume: false});
+  await dp.Debugger.onceResumed();
+  testRunner.log(`resumed`);
+
+  testRunner.log(`navigated ${(await navigated).params.frame.url}`);
+
+  testRunner.completeTest();
+})
