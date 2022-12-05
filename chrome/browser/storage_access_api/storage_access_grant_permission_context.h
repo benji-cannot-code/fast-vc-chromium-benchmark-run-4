7104833662187a7828312ecafac1d67fed84d9e3
Johann Hofmann <johannhof@chromium.org>
2022-12-05 22:09:41 +0000

    Autodeny rSA calls for FPS service sites
    One restriction for service sites is that they should never be the top
    level site in an SAA grant, which means we should also deny rSA calls
    with service sites as top-level.
    
    R=cfredric@chromium.org
    
    Bug: 1395469
    Change-Id: Ibe904f48844798bea3091eb98dd988092bff1fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079090
    Auto-Submit: Johann Hofmann <johannhof@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079464}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context.h
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context.h
@@ -46,11 +46,6 @@ enum class RequestOutcome {
   kMaxValue = kReusedPreviousDecision,
 };
 
-enum class StorageAccessRequestType {
-  kRequestStorageAccess,
-  kRequestStorageAccessForOrigin
-};
-
 class StorageAccessGrantPermissionContext
     : public permissions::PermissionContextBase {
  public:
@@ -117,7 +112,6 @@ class StorageAccessGrantPermissionContext
       const GURL& requesting_origin,
       const GURL& embedding_origin,
       bool user_gesture,
-      const StorageAccessRequestType request_type,
       permissions::BrowserPermissionCallback callback,
       net::FirstPartySetMetadata metadata);
 
