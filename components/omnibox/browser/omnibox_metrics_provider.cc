044ffd57d4b754b9958544743cf294a47e5a4703
Angela Yoeurng <yoangela@chromium.org>
2022-12-05 23:06:50 +0000

    [omnibox][ml] Log scoring signals to OmniboxEventProto on URL opened.
    This CL sets the scoring_signals field in OmniboxEventProto when a
    URL suggestion is opened.  This records the scoring signals for ALL
    suggestions presented.
    
    Bug: b/259444079
    Change-Id: Ib4feecdb9a84140ad4b3bf0765d3ac63b49b64d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4068145
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079503}

--- components/omnibox/browser/omnibox_metrics_provider.cc
+++ components/omnibox/browser/omnibox_metrics_provider.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_result.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_log.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "third_party/metrics_proto/omnibox_input_type.pb.h"
@@ -202,6 +203,11 @@ void OmniboxMetricsProvider::RecordOmniboxOpenedURL(const OmniboxLog& log) {
 
     suggestion->set_has_tab_match(i->has_tab_match.value_or(false));
     suggestion->set_is_keyword_suggestion(i->from_keyword);
+
+    if (OmniboxFieldTrial::IsLogUrlScoringSignalsEnabled() &&
+        !AutocompleteMatch::IsSearchType(i->type)) {
+      suggestion->mutable_scoring_signals()->CopyFrom(i->scoring_signals);
+    }
   }
   for (auto i(log.providers_info.begin()); i != log.providers_info.end(); ++i) {
     OmniboxEventProto::ProviderInfo* provider_info =
