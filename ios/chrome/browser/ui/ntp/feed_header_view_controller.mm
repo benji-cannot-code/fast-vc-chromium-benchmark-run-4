bbde089ee3c9ed64c5c73069779f7ed8050ea3be
Scott Yoder <scottyoder@google.com>
2022-12-05 16:53:43 +0000

    [ios] Fix width constraints related to discover feed on NTP
    Several problems have been reported on the NTP related to the width of
    the Discover Feed and related views:
     * The feed header settings button can become unclickable if the width
       of the Discover Feed becomes too narrow.
     * The Sign-In Promo can be wider than the view if it is contained in
       a really narrow window on an iPad (multiwindow).
     * The Discover Feed width shrinks if you open a new window on iPad
       and then close it.
    
    This fixes that by changing the way we constrain the widths of various
    views. Previously we were constraining some of them by the minimum of
    some constant value and the current width of a parent view. This was
    problematic because the parent view can change widths which may
    invalidate that calculation. Instead, we can set two separate
    constraints which together achieve the same thing, but without having
    to hardcode the width of the parent view.
    
    There also appears to be a fair amount of extremely fragile magic in
    these constraints, necessitated by our integration of an external
    component for the Discover Feed. The NTP doesn't have much control of
    its internal constraints, so instead the NTP places constraints on
    the views as best as it can to influence its presentation. If the NTP
    had more direct control, the constraints could be coordinated better.
    
    Fixed: 1298468, 1298458
    Change-Id: I774454157f5790f283d86af3fd01c3be5a29a4de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076590
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079263}

--- ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
@@ -526,6 +526,10 @@ NSInteger kFeedSymbolPointSize = 17;
 
   self.feedHeaderConstraints = [[NSMutableArray alloc] init];
 
+  NSLayoutConstraint* containerPreferredwidth = [self.container.widthAnchor
+      constraintEqualToConstant:kDiscoverFeedContentWidth];
+  containerPreferredwidth.priority = UILayoutPriorityDefaultHigh;
+
   [self.feedHeaderConstraints addObjectsFromArray:@[
     // Anchor container and menu button.
     [self.view.heightAnchor
@@ -538,8 +542,8 @@ NSInteger kFeedSymbolPointSize = 17;
     [self.container.centerXAnchor
         constraintEqualToAnchor:self.view.centerXAnchor],
     [self.container.widthAnchor
-        constraintEqualToConstant:MIN(kDiscoverFeedContentWidth,
-                                      self.view.frame.size.width)],
+        constraintLessThanOrEqualToAnchor:self.view.widthAnchor],
+    containerPreferredwidth,
     [self.menuButton.trailingAnchor
         constraintEqualToAnchor:self.container.trailingAnchor
                        constant:-kButtonHorizontalMargin],
