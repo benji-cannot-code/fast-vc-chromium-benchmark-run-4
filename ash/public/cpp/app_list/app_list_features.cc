501c5966434aff271ae32cbdc9ddda53661e9396
Ana Salazar <anasalazar@chromium.org>
2022-12-05 18:58:00 +0000

    Remove compact bubble launcher flag
    Bug: None
    Change-Id: I3d0e6e354f23853a296369a117bd37d2c4df92af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076041
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079328}

--- ash/public/cpp/app_list/app_list_features.cc
+++ ash/public/cpp/app_list/app_list_features.cc
@@ -33,9 +33,6 @@ BASE_FEATURE(kQuickActionShowBubbleLauncher,
 BASE_FEATURE(kDynamicSearchUpdateAnimation,
              "DynamicSearchUpdateAnimation",
              base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kCompactBubbleLauncher,
-             "CompactBubbleLauncher",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kLauncherPlayStoreSearch,
              "LauncherPlayStoreSearch",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -77,10 +74,6 @@ base::TimeDelta DynamicSearchUpdateAnimationDuration() {
 
 bool IsForceShowContinueSectionEnabled() {
   return base::FeatureList::IsEnabled(kForceShowContinueSection);
-}
-
-bool IsCompactBubbleLauncherEnabled() {
-  return base::FeatureList::IsEnabled(kCompactBubbleLauncher);
 }
 
 bool IsLauncherPlayStoreSearchEnabled() {
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2889,9 +2889,6 @@ extern const char kCameraAppLowStorageWarningDescription[];
 extern const char kCameraAppMultiPageDocScanName[];
 extern const char kCameraAppMultiPageDocScanDescription[];
 
-extern const char kCompactBubbleLauncherName[];
-extern const char kCompactBubbleLauncherDescription[];
-
 extern const char kCryptAuthV2DedupDeviceLastActivityTimeName[];
 extern const char kCryptAuthV2DedupDeviceLastActivityTimeDescription[];
 
