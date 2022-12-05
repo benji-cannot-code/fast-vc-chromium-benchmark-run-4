9ac930da93572fb05625c3f9778bb646657fd54a
Jie Sheng <jiesheng@google.com>
2022-12-05 23:55:23 +0000

    Add tag for web tests image diff stats
    This cl will upload the image diff stats information to
    web test tag in resultDB. This will help us query
    the result of a image comparison test.
    
    Bug: 1396136,1345700
    Change-Id: Id2c98707a146ee2aa01959a4c73e681c35f9b0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081320
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079534}

--- third_party/blink/tools/blinkpy/web_tests/models/test_results.py
+++ third_party/blink/tools/blinkpy/web_tests/models/test_results.py
@@ -41,6 +41,7 @@ def build_test_result(driver_output, test_name, failures=None, **kwargs):
     if not failures and driver_output.error:
         failures.append(test_failures.PassWithStderr(driver_output))
     kwargs.setdefault('command', driver_output.command)
+    kwargs.setdefault('image_diff_stats', driver_output.image_diff_stats)
     return TestResult(test_name, failures=failures, **kwargs)
 
 
@@ -60,7 +61,8 @@ class TestResult(object):
                  device_failed=False,
                  crash_site=None,
                  command=None,
-                 typ_host=None):
+                 typ_host=None,
+                 image_diff_stats=None):
         self.test_name = test_name
         self.failures = failures or []
         self.test_run_time = test_run_time or 0  # The time taken to execute the test itself.
@@ -74,6 +76,7 @@ class TestResult(object):
         self.crash_site = crash_site
         self.retry_attempt = retry_attempt
         self.command = command
+        self.image_diff_stats = image_diff_stats
 
         results = set([f.result for f in self.failures] or [ResultType.Pass])
         assert len(results) <= 2, (
