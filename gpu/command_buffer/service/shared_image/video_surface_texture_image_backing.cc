624160ec499e41f9ca0c1b7fc6f4e173423243db
Colin Blundell <blundell@chromium.org>
2022-12-05 13:53:36 +0000

    [GPU] Separate set_is_bind_pending() into {set, clear}_bind_pending()
    AbstractTexturePassthrough::set_is_bind_pending() takes a bool
    indicating whether to set or clear the texture as needing binding. To
    simplify readability and analysis of the code, this CL separates the
    method into two no-parameter methods: one that sets the texture as
    needing binding, and one that clears it. This makes it much simpler to
    see and understand the flows that actually set the texture as needing
    binding.
    
    Bug: 1323341
    Change-Id: I21f1804f132566fd4cb159f12cc05ccf4d414f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4078926
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079208}

--- gpu/command_buffer/service/shared_image/video_surface_texture_image_backing.cc
+++ gpu/command_buffer/service/shared_image/video_surface_texture_image_backing.cc
@@ -194,7 +194,7 @@ class VideoSurfaceTextureImageBacking::
 
     // If we passed a GLImage to BindStreamTextureImage(), mark it as bound.
     if (!base::FeatureList::IsEnabled(kPassNullForGLImageWhenBindingTexture)) {
-      passthrough_texture_->set_is_bind_pending(false);
+      passthrough_texture_->clear_bind_pending();
     }
 
     return true;
@@ -203,7 +203,7 @@ class VideoSurfaceTextureImageBacking::
   void EndAccess() override {
     // NOTE: It is not necessary to mark |texture_passthrough_| as needing
     // binding here: if there is a subsequent flow that requires that the
-    // texture be bound, that flow will itself invoke set_is_bind_pending() on
+    // texture be bound, that flow will itself invoke set_bind_pending() on
     // the texture.
   }
 
