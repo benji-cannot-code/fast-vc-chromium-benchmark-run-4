3aea8b3a7581288d3246cb163adced536a4e9cee
Kwanghee Lee <ekwange@gmail.com>
2022-12-05 20:57:05 +0000

    media/gpu/vdatest: Fix small typo and indentation
    Fix typo NofityFlushDone() => NotifyFlushDone()
    and changed a few spaces to match the rules
    
    Change-Id: Ibc2a5a245d35daa4006ef49253c3a29aa04d73c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4073866
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079389}

--- media/gpu/v4l2/v4l2_video_encode_accelerator.cc
+++ media/gpu/v4l2/v4l2_video_encode_accelerator.cc
@@ -1077,7 +1077,7 @@ void V4L2VideoEncodeAccelerator::Enqueue() {
   DCHECK(input_queue_ && output_queue_);
   TRACE_EVENT0("media,gpu", "V4L2VEA::Enqueue");
   DVLOGF(4) << "free_input_buffers: " << input_queue_->FreeBuffersCount()
-            << "input_queue: " << encoder_input_queue_.size();
+            << ", input_queue: " << encoder_input_queue_.size();
 
   bool do_streamon = false;
   // Enqueue all the inputs we can.
