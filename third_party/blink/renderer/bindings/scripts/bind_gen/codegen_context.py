d72a7d58e43530c1348976929a8d4c10696a7950
Yuki Shiino <yukishiino@chromium.org>
2022-12-05 09:08:34 +0000

    bindings: bind_gen.generate_sync_iterators
    Implements a bindings generator for sync iterators to produce
    SyncIterator<InterfaceType> and V8SyncIteratorInterfaceType.
    
    The file names of generated bindings will be like:
    v8_sync_iterator_INTERFACE_TYPE.{cc,h}
    
    Sample output is at the last section of:
    https://docs.google.com/document/d/1FwbRbF6TZItHTUJp8LcfklkCBqTULAV1vjKSJpq0Cus/edit?usp=sharing
    
    Bug: 1354546
    Change-Id: Iaa6ee8035858f45b986db6bebc46e9fc0164a11a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3679719
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079124}

--- third_party/blink/renderer/bindings/scripts/bind_gen/codegen_context.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/codegen_context.py
@@ -86,6 +86,7 @@ class CodeGenContext(object):
             "interface": None,
             "namespace": None,
             "observable_array": None,
+            "sync_iterator": None,
             "typedef": None,
             "union": None,
 
@@ -231,7 +232,7 @@ class CodeGenContext(object):
     @property
     def class_like(self):
         return (self.callback_interface or self.dictionary or self.interface
-                or self.namespace)
+                or self.namespace or self.sync_iterator)
 
     @property
     def does_override_idl_return_type(self):
