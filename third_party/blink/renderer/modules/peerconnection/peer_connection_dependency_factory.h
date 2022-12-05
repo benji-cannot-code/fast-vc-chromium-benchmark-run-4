511a5ba8aa67591e94a5d600ab469a46e66e3ac4
Johannes Kron <kron@chromium.org>
2022-12-05 14:28:22 +0000

    Remove expired histograms WebRTC.Video.HwCapabilities.*
    This CL revert the changes that were added in
    https://chromium-review.googlesource.com/c/chromium/src/+/3257688
    
    Fixed: chromium:1365486
    Change-Id: Ifcb08a89019ed66f7588aee27f34f5b4f8678ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4069667
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079225}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.h
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.h
@@ -201,8 +201,6 @@ class MODULES_EXPORT PeerConnectionDependencyFactory
 
   WebrtcVideoPerfReporter webrtc_video_perf_reporter_;
 
-  bool encode_decode_capabilities_reported_ = false;
-
   THREAD_CHECKER(thread_checker_);
 };
 
