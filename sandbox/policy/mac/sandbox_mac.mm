f6ac9a2eac9c076e784979d99e9473d9ccb61be9
Robert Sesek <rsesek@chromium.org>
2022-12-06 16:46:19 +0000

    mac: Add the CacheMacSandboxProfiles feature and sandbox policy cache
    When the feature is enabled, the sandbox policy for renderers, service,
    and utility processes will be precompiled in the browser process. The
    compiled policy can then be used across all future process launches of
    processes with those sandbox types.
    
    Bug: 1315988
    Change-Id: I3dbe6d477ae5b5ea1a40abc6d551ebc954eeba7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080527
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079813}

--- sandbox/policy/mac/sandbox_mac.mm
+++ sandbox/policy/mac/sandbox_mac.mm
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/feature_list.h"
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "components/services/screen_ai/buildflags/buildflags.h"
 #include "ppapi/buildflags/buildflags.h"
 #include "printing/buildflags/buildflags.h"
+#include "sandbox/policy/features.h"
 #include "sandbox/policy/mac/audio.sb.h"
 #include "sandbox/policy/mac/cdm.sb.h"
 #include "sandbox/policy/mac/common.sb.h"
@@ -110,6 +112,23 @@ std::string GetSandboxProfile(sandbox::mojom::Sandbox sandbox_type) {
       break;
   }
   return profile;
+}
+
+bool CanCacheSandboxPolicy(sandbox::mojom::Sandbox sandbox_type) {
+  static const bool feature_enabled =
+      base::FeatureList::IsEnabled(features::kCacheMacSandboxProfiles);
+  if (!feature_enabled)
+    return false;
+
+  switch (sandbox_type) {
+    case sandbox::mojom::Sandbox::kRenderer:
+    case sandbox::mojom::Sandbox::kService:
+    case sandbox::mojom::Sandbox::kServiceWithJit:
+    case sandbox::mojom::Sandbox::kUtility:
+      return true;
+    default:
+      return false;
+  }
 }
 
 }  // namespace policy
