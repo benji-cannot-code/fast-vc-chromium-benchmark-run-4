2ef218e27fbcecbe6919f7b1639f1eb1ce59fddc
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-12-06 18:40:30 +0000

    Add missing <memory> include.
    This header had a transitive dependency on <memory> via
    an include of <ostream> in base/numerics/safe_conversions.h. The
    ostream include in safe_conversions.h is however unused and should
    be removed.
    
    This CL was uploaded by git cl split.
    
    R=mark@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I3ef29c4e7240e56bdac80c5a0db34e8129b8ecc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057386
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079874}

--- chrome/browser/ui/cocoa/screentime/history_deleter_impl.h
+++ chrome/browser/ui/cocoa/screentime/history_deleter_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_COCOA_SCREENTIME_HISTORY_DELETER_IMPL_H_
 #define CHROME_BROWSER_UI_COCOA_SCREENTIME_HISTORY_DELETER_IMPL_H_
 
+#include <memory>
+
 #include "base/mac/scoped_nsobject.h"
 #include "chrome/browser/ui/cocoa/screentime/history_deleter.h"
 
