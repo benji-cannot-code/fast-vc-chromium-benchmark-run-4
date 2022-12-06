1376f6b6645b190be90da7b87a0062662e0dd1e8
Jean-Philippe Gravel <jpgravel@chromium.org>
2022-12-06 18:42:31 +0000

    Add missing ostream includes.
    These compilation units had a transitive dependency on ostream via
    base/numerics/safe_conversions.h. The ostream include in
    safe_conversions.h is however unused and should be removed.
    
    ostream is needed for `operator<<` used on PCHECK in this file. base/check.h only forward declares <ostream> by including <iosfwd>. The documentation mentions:
    
    // Additional information can be streamed to these macros and will be included
    // in the log output if the condition doesn't hold (you may need to include
    // <ostream>):
    //
    //   CHECK(condition) << "Additional info.";
    
    This CL was uploaded by git cl split.
    
    R=vitalybuka@chromium.org
    
    Bug: 1270812, 1372522
    Change-Id: I984e8a852b849e46d70b05abbf8ac5d4222d3482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057311
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Auto-Submit: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079876}

--- components/gwp_asan/client/guarded_page_allocator_posix.cc
+++ components/gwp_asan/client/guarded_page_allocator_posix.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gwp_asan/client/guarded_page_allocator.h"
 
 #include <sys/mman.h>
+#include <ostream>
 
 #include "base/check.h"
 #include "base/posix/eintr_wrapper.h"
