ce354f5b6a1ae7cdfdfcff7ddd4822d02ddfb77a
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-12-06 18:16:12 +0000

    Fix NavigationDownloadPolicy from the browser process
    The NavigationDownloadPolicy is currently computed by the renderer
    process. The problem: not every navigation is initiated from the
    renderer. This is a problem.
    
    Most fields from the bitfield can also be computed from the browser
    process. This patch computes the one related to the 'allow-download'
    sandbox flags from the navigation request. In the future, I believe we
    want to do something similar for the other properties.
    
    Bug: 1357366
    Change-Id: I0f18d2ff302271745d030494004007aecef1d738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061566
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079858}

--- components/error_page/content/browser/net_error_auto_reloader_browsertest.cc
+++ components/error_page/content/browser/net_error_auto_reloader_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
@@ -680,8 +681,13 @@ IN_PROC_BROWSER_TEST_F(NetErrorAutoReloaderBrowserTest,
     SimulateNetworkGoingOnline(popup);
     ForceScheduledAutoReloadNow(popup);
     navigation_observer.WaitForNavigationFinished();
-    // TODO(https://crbug.com/1357366): This must be false instead.
-    EXPECT_TRUE(handle_observer.is_download());
+    if (base::FeatureList::IsEnabled(
+            features::kBrowserSideDownloadPolicySandbox)) {
+      EXPECT_FALSE(handle_observer.is_download());
+    } else {
+      // TODO(https://crbug.com/1357366): This must be false instead.
+      EXPECT_TRUE(handle_observer.is_download());
+    }
   }
 }
 
