c88cbbfa7af7b5e6a42f9587f149aa53fdcf7dbf
Josiah Krutz <josiahk@google.com>
2022-12-06 22:59:37 +0000

    Add test ChangingCursorColorPrefChangesCursorColor
    Add test to accessibility_controller_unittest to verify changing the
    pref for cursor color pref actually changes the cursor color in
    cursor_window_controller.
    
    There's an exisiting test verifing setting cursor_window_controller
    color sets the actual mouse cursor color [1].
    
    [1] crsrc.org/c/ash/display/cursor_window_controller_unittest.cc;l=215
    
    Bug: b/261462562
    Change-Id: Idc9e6a373580390daf5fe9691db121779528629a
    AX-Relnotes: N/A.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081293
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080037}

--- ash/display/cursor_window_controller.h
+++ ash/display/cursor_window_controller.h
@@ -90,6 +90,7 @@ class ASH_EXPORT CursorWindowController {
 
   // Gets the cursor container for testing purposes.
   const aura::Window* GetContainerForTest() const;
+  SkColor GetCursorColorForTest() const;
 
  private:
   friend class CursorWindowControllerTest;
