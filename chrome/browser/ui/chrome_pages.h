5085c622448f036aeaa824680859be0fd027dad2
Hans Wennborg <hans@chromium.org>
2022-12-06 18:04:10 +0000

    Don't include app_launch_util.h in chrome_pages.h
    It's not needed, and include analysis says it costs 150 MB of compiler
    input for the 'chrome' build target [1].
    
    [1] https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2022-12-05_115924.html#view=edges&includer=chrome_pages%5C.h&included=app_launch_util%5C.h
    
    Bug: 242216
    Change-Id: I02dcb453181c5a0a15a0ca9b055f3895bfb62495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079254
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079853}

--- chrome/browser/ui/chrome_pages.h
+++ chrome/browser/ui/chrome_pages.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/content_settings/core/common/content_settings_types.h"
-#include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "url/gurl.h"
 
@@ -25,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ui/webui/settings/ash/app_management/app_management_uma.h"
 #endif
+
+namespace apps {
+enum class LaunchSource;
+}
 
 namespace signin {
 enum class ConsentLevel;
