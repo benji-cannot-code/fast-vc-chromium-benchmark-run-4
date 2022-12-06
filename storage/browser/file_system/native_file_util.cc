ded408a8f25f6cc9c0942a860a1baa81691b40e5
Sergey Poromov <poromov@chromium.org>
2022-12-06 07:29:41 +0000

    Add chronos-access group read access to created directories on ChromeOS.
    The system-created directories in ChromeOS cryptohome
    (.../MyFiles/ and .../MyFiles/Downloads/) have read access
    for chronos-access group members, unlike the directories created
    by Chrome, e.g.by users in Files app.
    This way members of chronos-access group can traverse only
    system-created, not user-created directories.
    
    This CL changes access mode from 711 to 751 and changes group
    ownership from chronos to chronos-access.
    
    Bug: 1378598
    Test: Create a folder in Files app, check access permissions.
    Change-Id: I674677759ba3b0c381a6c0e52967c6fba8d8fdcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016838
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079648}

--- storage/browser/file_system/native_file_util.cc
+++ storage/browser/file_system/native_file_util.cc
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "windows.h"
 #endif  // BUILDFLAG(IS_WIN)
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include <grp.h>
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
 namespace storage {
 
 namespace {
@@ -32,12 +36,18 @@ namespace {
 // TODO(benchan): Find a better place outside webkit to host this function.
 bool SetPlatformSpecificDirectoryPermissions(const base::FilePath& dir_path) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  // System daemons on Chrome OS may run as a user different than the Chrome
+  // System daemons on ChromeOS may run as a user different than the Chrome
   // process but need to access files under the directories created here.
   // Because of that, grant the execute permission on the created directory
-  // to group and other users.
-  if (HANDLE_EINTR(
-          chmod(dir_path.value().c_str(), S_IRWXU | S_IXGRP | S_IXOTH)) != 0) {
+  // to group and other users. Also chronos-access group should have read
+  // access to the directory.
+  if (HANDLE_EINTR(chmod(dir_path.value().c_str(),
+                         S_IRWXU | S_IRGRP | S_IXGRP | S_IXOTH)) != 0) {
+    return false;
+  }
+  auto* group = getgrnam("chronos-access");
+  if (!group ||
+      HANDLE_EINTR(chown(dir_path.value().c_str(), -1, group->gr_gid)) != 0) {
     return false;
   }
 #endif
