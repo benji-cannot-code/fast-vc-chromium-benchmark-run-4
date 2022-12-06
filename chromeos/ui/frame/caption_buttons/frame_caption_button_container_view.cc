56f983aefdb9d2f603953f816dcefe30800f629d
Sammie Quon <sammiequon@chromium.org>
2022-12-06 22:14:22 +0000

    float: Update strings for multitask menu
    Fullscreen and float have "Exit" when they are in fullscreen and float
    respectively, since they are toggles. Also, moves the ui resources to
    chromeos/ folder since its chromeos only.
    
    Test: manual
    Bug: b/257527067
    Change-Id: Id860843db5e28d21f8de6fd32a7dbf87197e65ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065023
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Judy Wang <shidi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080006}

--- chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.cc
+++ chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "chromeos/ui/base/display_util.h"
 #include "chromeos/ui/base/tablet_state.h"
 #include "chromeos/ui/base/window_properties.h"
@@ -229,7 +230,7 @@ FrameCaptionButtonContainerView::FrameCaptionButtonContainerView(
             base::Unretained(this)),
         views::CAPTION_BUTTON_ICON_FLOAT, HTMENU));
     float_button_->SetTooltipText(
-        l10n_util::GetStringUTF16(IDS_APP_ACCNAME_FLOAT));
+        l10n_util::GetStringUTF16(IDS_MULTITASK_MENU_FLOAT_BUTTON_NAME));
   }
 
   size_button_ = new FrameSizeButton(
@@ -563,12 +564,9 @@ void FrameCaptionButtonContainerView::UpdateFloatButton() {
   SetButtonImage(
       views::CAPTION_BUTTON_ICON_FLOAT,
       floated ? chromeos::kUnfloatButtonIcon : chromeos::kFloatButtonIcon);
-  float_button_->SetTooltipText(
-      floated
-          // TODO(sammiequon|shidi): Update this to the correct string once UX
-          // writing has a decision.
-          ? l10n_util::GetStringUTF16(IDS_APP_ACCNAME_RESTORE)
-          : l10n_util::GetStringUTF16(IDS_APP_ACCNAME_FLOAT));
+  float_button_->SetTooltipText(l10n_util::GetStringUTF16(
+      floated ? IDS_MULTITASK_MENU_EXIT_FLOAT_BUTTON_NAME
+              : IDS_MULTITASK_MENU_FLOAT_BUTTON_NAME));
 
   // Float button also needs to update its visibility when float state changes.
   float_button_->SetEnabled(
