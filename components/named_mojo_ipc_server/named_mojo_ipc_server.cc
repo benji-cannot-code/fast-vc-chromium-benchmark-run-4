d419322713ab40ac735ce83387b8e69d002178bf
Joshua Pawlicki <waffles@chromium.org>
2022-12-06 23:31:48 +0000

    NamedMojoIpcServer: Allow per-connection implementations.
    Each connection may have a different implementation to bind to,
    and the implementation may depend upon the process ID of the
    remote (although none of the current callers do).
    
    Future callers will depends on the UID of the remote.
    
    Bug: 1339108
    Change-Id: Iaa4309c58c6831325494a589fb5713fdad881763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4071784
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080053}

--- components/named_mojo_ipc_server/named_mojo_ipc_server.cc
+++ components/named_mojo_ipc_server/named_mojo_ipc_server.cc
@@ -61,10 +61,10 @@ void NamedMojoIpcServerBase::DelegateProxy::OnServerEndpointCreated() {
 NamedMojoIpcServerBase::NamedMojoIpcServerBase(
     const mojo::NamedPlatformChannel::ServerName& server_name,
     absl::optional<uint64_t> message_pipe_id,
-    IsTrustedMojoEndpointCallback is_trusted_endpoint_callback)
+    base::RepeatingCallback<void*(base::ProcessId)> impl_provider)
     : server_name_(server_name),
       message_pipe_id_(message_pipe_id),
-      is_trusted_endpoint_callback_(std::move(is_trusted_endpoint_callback)) {
+      impl_provider_(impl_provider) {
   io_sequence_ =
       base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
 }
@@ -119,7 +119,8 @@ void NamedMojoIpcServerBase::OnIpcDisconnected() {
 void NamedMojoIpcServerBase::OnClientConnected(
     mojo::PlatformChannelEndpoint endpoint,
     base::ProcessId peer_pid) {
-  if (!is_trusted_endpoint_callback_.Run(peer_pid)) {
+  void* impl = impl_provider_.Run(peer_pid);
+  if (!impl) {
     LOG(ERROR) << "Process " << peer_pid
                << " is not a trusted mojo endpoint. Connection refused.";
     return;
@@ -131,7 +132,7 @@ void NamedMojoIpcServerBase::OnClientConnected(
     mojo::ScopedMessagePipeHandle message_pipe =
         connection->Connect(std::move(endpoint));
     mojo::ReceiverId receiver_id =
-        TrackMessagePipe(std::move(message_pipe), peer_pid);
+        TrackMessagePipe(std::move(message_pipe), impl, peer_pid);
     active_connections_[receiver_id] = std::move(connection);
     return;
   }
@@ -159,7 +160,7 @@ void NamedMojoIpcServerBase::OnClientConnected(
 #undef INVALID_PROCESS_LOG
   mojo::OutgoingInvitation::Send(std::move(invitation), peer_process.Handle(),
                                  std::move(endpoint));
-  TrackMessagePipe(std::move(message_pipe), peer_pid);
+  TrackMessagePipe(std::move(message_pipe), impl, peer_pid);
 }
 
 void NamedMojoIpcServerBase::OnServerEndpointCreated() {
