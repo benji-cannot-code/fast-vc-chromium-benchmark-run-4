b5850e463017b735a3644ec02806f5f5b38e1550
Bryan Nguyen <nguyenbryan@google.com>
2022-12-06 01:29:35 +0000

    Add mouse capture on Windows.
    On Windows, a mouse capture should be set when initiating a drag in
    order to track mouse events when the user drags outside of the window.
    Previously, region selection did not behave properly on Windows when the
    user dragged outside of the window.
    
    This affects both the Desktop Screenshots feature and Lens Region
    Search.
    
    Bug: 1395184, b/234047736
    Change-Id: I58679635d03d8ffa09441ca7ff7c2cc7d6b3933b
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome,mac-chrome,linux-chrome,win-chrome,win64-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072156
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079581}

--- chrome/browser/image_editor/screenshot_flow.cc
+++ chrome/browser/image_editor/screenshot_flow.cc
@@ -126,9 +126,16 @@ void ScreenshotFlow::RemoveUIOverlay() {
   event_capture_mac_.reset();
 #else
   const gfx::NativeWindow& native_window = web_contents_->GetNativeView();
+#if BUILDFLAG(IS_WIN)
+  // This handles cases where the overlay is removed while a drag is still in
+  // progress, including if ScreenshotFlow is destroyed. It is safe to call
+  // ReleaseCapture() even if the capture has not been set or has already been
+  // released.
+  native_window->ReleaseCapture();
+#endif  // BUILDFLAG(IS_WIN)
   event_capture_.Reset();
   ui::Layer* content_layer = native_window->layer();
-#endif
+#endif  // else
 
   content_layer->Remove(screen_capture_layer_.get());
 
@@ -233,7 +240,7 @@ void ScreenshotFlow::OnMouseEvent(ui::MouseEvent* event) {
             location.y() > web_contents_bounds.height()) {
           return;
         }
-        is_dragging_ = true;
+        SetIsDragging(true);
         drag_start_ = location;
         drag_end_ = location;
         event->SetHandled();
@@ -250,7 +257,7 @@ void ScreenshotFlow::OnMouseEvent(ui::MouseEvent* event) {
       if ((capture_mode_ == CaptureMode::SELECTION_RECTANGLE ||
            capture_mode_ == CaptureMode::SELECTION_ELEMENT) &&
           is_dragging_) {
-        is_dragging_ = false;
+        SetIsDragging(false);
         AttemptRegionCapture(web_contents_bounds);
         event->SetHandled();
       }
@@ -260,7 +267,7 @@ void ScreenshotFlow::OnMouseEvent(ui::MouseEvent* event) {
       if ((capture_mode_ == CaptureMode::SELECTION_RECTANGLE ||
            capture_mode_ == CaptureMode::SELECTION_ELEMENT) &&
           event->AsMouseWheelEvent()->y_offset() > 0 && is_dragging_) {
-        is_dragging_ = false;
+        SetIsDragging(false);
         AttemptRegionCapture(web_contents_bounds);
         event->SetHandled();
       }
@@ -281,7 +288,7 @@ void ScreenshotFlow::OnScrollEvent(ui::ScrollEvent* event) {
   if ((capture_mode_ == CaptureMode::SELECTION_RECTANGLE ||
        capture_mode_ == CaptureMode::SELECTION_ELEMENT) &&
       event->y_offset() > 0 && is_dragging_) {
-    is_dragging_ = false;
+    SetIsDragging(false);
     AttemptRegionCapture(web_contents_bounds);
     event->SetHandled();
   }
@@ -395,6 +402,18 @@ void ScreenshotFlow::SetCursor(ui::mojom::CursorType cursor_type) {
     ui::Cursor cursor(cursor_type);
     host->SetCursor(cursor);
   }
+}
+
+void ScreenshotFlow::SetIsDragging(bool value) {
+  is_dragging_ = value;
+#if BUILDFLAG(IS_WIN)
+  const gfx::NativeWindow& native_window = web_contents_->GetNativeView();
+  if (value) {
+    native_window->SetCapture();
+  } else {
+    native_window->ReleaseCapture();
+  }
+#endif
 }
 
 bool ScreenshotFlow::IsCaptureModeActive() {
