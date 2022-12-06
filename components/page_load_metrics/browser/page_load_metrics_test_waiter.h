9bd9ad403eca4737be1eb005af3020c711c66330
Lan Wei <lanwei@chromium.org>
2022-12-06 22:36:09 +0000

    Add num interaction expectation to pageload metrics test waiter
    In order to wait the interaction metrics value to be set in UKM and
    UMA, we need to add an expectation for the interaction metrics value
    in PageLoadMetricsTestWaiter::OnPageInputTimingUpdated to wait for the
    mojom::InputTiming.num_interactions to be sent from renderer to
    browser.
    
    Bug: 1396428
    Change-Id: I7d36040dd509abfbd6aa8f64dc350d2089d587fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082227
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080019}

--- components/page_load_metrics/browser/page_load_metrics_test_waiter.h
+++ components/page_load_metrics/browser/page_load_metrics_test_waiter.h
@@ -143,6 +143,11 @@ class PageLoadMetricsTestWaiter : public MetricsLifecycleObserver {
     expected_num_input_events_ = expected_num_input_events;
   }
 
+  // Add the number of interactions count expectation.
+  void AddNumInteractionsExpectation(uint64_t expected_num_interactions) {
+    expected_num_interactions_ = expected_num_interactions;
+  }
+
  protected:
   virtual bool ExpectationsSatisfied() const;
   void AssertExpectationsSatisfied() const;
@@ -224,7 +229,8 @@ class PageLoadMetricsTestWaiter : public MetricsLifecycleObserver {
   // Updates observed page fields when a input timing update is received by the
   // MetricsWebContentsObserver. Stops waiting if expectations are satsfied
   // after update.
-  void OnPageInputTimingUpdated(uint64_t num_input_events);
+  void OnPageInputTimingUpdated(uint64_t num_interactions,
+                                uint64_t num_input_events);
 
   // Updates observed page fields when a timing update is received by the
   // MetricsWebContentsObserver. Stops waiting if expectations are satsfied
@@ -296,6 +302,7 @@ class PageLoadMetricsTestWaiter : public MetricsLifecycleObserver {
   bool MemoryUpdateExpectationsSatisfied() const;
   bool TotalInputDelayExpectationsSatisfied() const;
   bool LayoutShiftExpectationsSatisfied() const;
+  bool NumInteractionsExpectationsSatisfied() const;
 
   void AddObserver(page_load_metrics::PageLoadTracker* tracker);
 
@@ -346,6 +353,9 @@ class PageLoadMetricsTestWaiter : public MetricsLifecycleObserver {
 
   uint64_t current_num_input_events_ = 0;
   uint64_t expected_num_input_events_ = 0;
+
+  uint64_t current_num_interactions_ = 0;
+  uint64_t expected_num_interactions_ = 0;
 
   ShiftFrame shift_frame_ = ShiftFrame::NoLayoutShift;
 
