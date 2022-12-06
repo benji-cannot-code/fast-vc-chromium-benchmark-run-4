b4fbfea4cd9f568ae246002940b55f85cfdc52ca
Andy Phan <andyphan@chromium.org>
2022-12-06 01:20:28 +0000

    [PDF Viewer] Announce rotation state when PDF is rotated
    Have screen readers read aloud the rotation state, in degrees, each time
    the user rotates the PDF document.
    
    Also change the zoom level announcer to use loadTimeData.getString()
    instead of $i18n.
    
    Fixed: 1313277
    Change-Id: Ib3e7e7c5845dde49a285cfd41c1d78c3d5b9c69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018225
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079577}

--- chrome/browser/resources/pdf/viewport.ts
+++ chrome/browser/resources/pdf/viewport.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {getInstance as getAnnouncerInstance} from 'chrome://resources/cr_elements/cr_a11y_announcer/cr_a11y_announcer.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert_ts.js';
 import {EventTracker} from 'chrome://resources/js/event_tracker.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {hasKeyModifiers, isRTL} from 'chrome://resources/js/util_ts.js';
 
 import {ExtendedKeyEvent, FittingType, Point} from './constants.js';
@@ -1057,8 +1058,9 @@ export class Viewport implements ViewportInterface {
   /** Announce zoom level for screen readers. */
   private announceZoom_(): void {
     const announcer = getAnnouncerInstance();
-    announcer.announce(
-        `$i18n{zoomTextInputAriaLabel}: ${Math.round(100 * this.getZoom())}%`);
+    const ariaLabel = loadTimeData.getString('zoomTextInputAriaLabel');
+    const zoom = Math.round(100 * this.getZoom());
+    announcer.announce(`${ariaLabel}: ${zoom}%`);
   }
 
   private pageUpDownSpaceHandler_(e: KeyboardEvent, formFieldFocused: boolean) {
@@ -1237,6 +1239,7 @@ export class Viewport implements ViewportInterface {
   setDocumentDimensions(documentDimensions: DocumentDimensions) {
     this.mightZoom_(() => {
       const initialDimensions = !this.documentDimensions_;
+      const initialRotations = this.getClockwiseRotations();
       this.documentDimensions_ = documentDimensions;
 
       // Override layout direction based on isRTL().
@@ -1259,7 +1262,21 @@ export class Viewport implements ViewportInterface {
       }
       this.contentSizeChanged_();
       this.resize_();
+
+      if (initialRotations !== this.getClockwiseRotations()) {
+        this.announceRotation_();
+      }
     });
+  }
+
+  /** Announce state of rotation, clockwise, for screen readers. */
+  private announceRotation_() {
+    const announcer = getAnnouncerInstance();
+
+    const clockwiseRotationsDegrees = this.getClockwiseRotations() * 90;
+    const rotationStateLabel = loadTimeData.getString(
+        `rotationStateLabel${clockwiseRotationsDegrees}`);
+    announcer.announce(rotationStateLabel);
   }
 
   /** @return The bounds for page `page` minus the shadows. */
