d37d3ee9db7281f98242bce72de2633d90e321a0
Jeffrey Young <cowmoo@google.com>
2022-12-06 22:05:26 +0000

    personalization: more wallpaper-grid-item
    Consolidate more of the "image tiles" to use wallpaper-grid-item as much
    as possible. Replace google photos collection tile with
    wallpaper-grid-item.
    
    BUG=b:244241759
    TEST=browser_tests --gtest_filter="*WallpaperCollections*"
    
    Change-Id: I54d71f7d8bbf5212fc9670415662389476878bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081336
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079999}

--- ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
@@ -52,6 +52,7 @@ interface GooglePhotosTile {
   id: typeof kGooglePhotosCollectionId;
   name: string;
   type: TileType.IMAGE_GOOGLE_PHOTOS;
+  preview: [Url];
 }
 
 interface LocalTile {
@@ -110,6 +111,7 @@ function getGooglePhotosTile(enablementState: GooglePhotosEnablementState):
     id: kGooglePhotosCollectionId,
     name: loadTimeData.getString('googlePhotosLabel'),
     type: TileType.IMAGE_GOOGLE_PHOTOS,
+    preview: [{url: 'chrome://personalization/images/google_photos.svg'}],
   };
 }
 
