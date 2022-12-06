5d7bfcf4e8b698c430c7cd464545e8024f9dfccf
Keishi Hattori <keishi@chromium.org>
2022-12-06 00:22:59 +0000

    Fix bug with AffectedRawPtrFieldDecl param order
    AffectedRawPtrFieldDecl param order was wrong in FindBadRawPtrPatterns.cpp
    
    This CL adds tests raw_ptr_exclude_paths.cpp and
    raw_ptr_exclude_fields.txt that checks that params
    exclude-paths and exclude-fields are applied.
    
    Change-Id: I87617e20ce6424066cf5131005f4d03e14f852d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4078843
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079545}

---
+++ tools/clang/plugins/tests/raw_ptr_exclude_paths.cpp
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class SomeClass;
+
+class MyClass {
+  // No error expected because of exclude-paths file,
+  // raw_ptr_exclude_paths.exclude.
+  SomeClass* raw_ptr_field1;
+};
