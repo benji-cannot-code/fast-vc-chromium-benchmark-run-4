61c65e9284e4231c82b601a9a5a8a2217bad8740
zauri <zauri@google.com>
2022-12-06 22:25:12 +0000

    [Privacy Hub] Location - add stub controller
    Add Geolocation controller under Privacy Hub.
    When Location is toggled off from Settings>"Security and Privacy">
    "Privacy controls" SimpleGeolocationProvider class should stop
    requesting precise location.
    
    Bug: b/240921313
    Change-Id: Ib3f691a3ff0c743e7cf33b78c492e92478461d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008388
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Zauri Meshveliani‎ <zauri@chromium.org>
    Commit-Queue: Zauri Meshveliani‎ <zauri@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080011}

---
+++ ash/system/privacy_hub/geolocation_privacy_switch_controller.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_PRIVACY_HUB_GEOLOCATION_PRIVACY_SWITCH_CONTROLLER_H_
+#define ASH_SYSTEM_PRIVACY_HUB_GEOLOCATION_PRIVACY_SWITCH_CONTROLLER_H_
+
+#include <memory>
+
+#include "ash/ash_export.h"
+#include "ash/public/cpp/session/session_observer.h"
+
+class PrefChangeRegistrar;
+
+namespace ash {
+
+class ASH_EXPORT GeolocationPrivacySwitchController : public SessionObserver {
+ public:
+  GeolocationPrivacySwitchController();
+  ~GeolocationPrivacySwitchController() override;
+
+  GeolocationPrivacySwitchController(
+      const GeolocationPrivacySwitchController&) = delete;
+  GeolocationPrivacySwitchController& operator=(
+      const GeolocationPrivacySwitchController&) = delete;
+
+  void OnActiveUserPrefServiceChanged(PrefService* pref_service) override;
+
+ private:
+  void OnPreferenceChanged();
+
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_PRIVACY_HUB_GEOLOCATION_PRIVACY_SWITCH_CONTROLLER_H_
