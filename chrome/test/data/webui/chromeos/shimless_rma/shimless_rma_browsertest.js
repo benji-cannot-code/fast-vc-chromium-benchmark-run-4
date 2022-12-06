bb79816ff3dc44d0b03e2f20f66581a8ad75b68b
Gavin Williams <gavinwill@chromium.org>
2022-12-06 17:38:53 +0000

    Shimless: Disable flaky component browser tests
    There is a bug in the tests for selecting the individual components on
    the Select Components and Calibration Failed pages.
    
    Bug: 1296829
    Change-Id: Id77a9525f8b2d6d77e3104100e8b97eaa8cd02c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081312
    Auto-Submit: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079838}

--- chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
+++ chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
@@ -55,7 +55,7 @@ const tests = [
   ['OnboardingNetworkPageTest', 'onboarding_network_page_test.js'],
   [
     'OnboardingSelectComponentsPageTest',
-    'onboarding_select_components_page_test.js'
+    'onboarding_select_components_page_test.js', 'DISABLED_All'
   ],
   ['OnboardingUpdatePageTest', 'onboarding_update_page_test.js'],
   [
@@ -69,7 +69,7 @@ const tests = [
   ['RebootPageTest', 'reboot_page_test.js'],
   [
     'ReimagingCalibrationFailedPageTest',
-    'reimaging_calibration_failed_page_test.js'
+    'reimaging_calibration_failed_page_test.js', 'DISABLED_All'
   ],
   ['ReimagingCalibrationRunPageTest', 'reimaging_calibration_run_page_test.js'],
   [
