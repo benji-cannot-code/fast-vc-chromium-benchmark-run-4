42e4c05eb1166ce294fc0adefb13d31a556862a1
Rachel Wong <wrong@chromium.org>
2022-12-06 23:36:13 +0000

    Consolidate duplicated util function.
    There are currently two util functions that check if a result type is an
    app, mostly for metrics purposes.
    
    Note that app_list::IsResultTypeApp (the one being deleted) considers
    fewer result types to be apps than ash::IsAppListSearchResultAnApp.
    
    Change-Id: I57fb138803e540ca4255147b802171b424f1f252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082647
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080057}

--- chrome/browser/ui/app_list/app_list_client_impl.cc
+++ chrome/browser/ui/app_list/app_list_client_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/app_list_controller.h"
+#include "ash/public/cpp/app_list/app_list_types.h"
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/tablet_mode.h"
 #include "base/bind.h"
@@ -29,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_notifier_impl.h"
 #include "chrome/browser/ui/app_list/app_list_syncable_service.h"
 #include "chrome/browser/ui/app_list/app_list_syncable_service_factory.h"
-#include "chrome/browser/ui/app_list/app_list_util.h"
 #include "chrome/browser/ui/app_list/app_sync_ui_state_watcher.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder.h"
@@ -220,7 +220,7 @@ void AppListClientImpl::OpenSearchResult(int profile_id,
   const size_t last_query_length = search_controller_->get_query().size();
   if (launch_type == ash::AppListLaunchType::kAppSearchResult &&
       launched_from == ash::AppListLaunchedFrom::kLaunchedFromSearchBox &&
-      app_list::IsResultTypeApp(launch_data.result_type) &&
+      ash::IsAppListSearchResultAnApp(launch_data.result_type) &&
       last_query_length != 0) {
     ash::RecordSuccessfulAppLaunchUsingSearch(launched_from, last_query_length);
   }
--- chrome/browser/ui/app_list/search/search_controller_impl.cc
+++ chrome/browser/ui/app_list/search/search_controller_impl.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/app_list_model_updater.h"
-#include "chrome/browser/ui/app_list/app_list_util.h"
 #include "chrome/browser/ui/app_list/search/app_search_data_source.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/common/string_util.h"
@@ -403,7 +402,7 @@ void SearchControllerImpl::Train(LaunchData&& launch_data) {
 
     // Only record the last launched app if the hashed logging feature flag is
     // enabled, because it is only used by hashed logging.
-    if (IsResultTypeApp(launch_data.result_type)) {
+    if (IsAppListSearchResultAnApp(launch_data.result_type)) {
       last_launched_app_id_ = NormalizeId(launch_data.id);
     } else if (launch_data.result_type ==
                ash::AppListSearchResultType::kArcAppShortcut) {
