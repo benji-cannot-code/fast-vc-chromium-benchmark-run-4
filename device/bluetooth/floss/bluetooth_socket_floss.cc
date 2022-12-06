86843632e784c6e9bb7480772bb2e6e24629b22d
Abhishek Pandit-Subedi <abhishekpandit@google.com>
2022-12-06 17:42:59 +0000

    [floss] Sockets should use kL2cap
    Prefer to use kL2cap instead of kL2capLe for l2cap sockets. The apis are
    not meant to be for L2CAP LE sockets.
    
    BUG=b:243420879
    TEST=device_unittests --gtest_filter=BluetoothSocketFloss\*
    
    Change-Id: I4bcb84febe99e926fff60f02899b77967b1e834a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4077049
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079844}

--- device/bluetooth/floss/bluetooth_socket_floss.cc
+++ device/bluetooth/floss/bluetooth_socket_floss.cc
@@ -91,7 +91,7 @@ void BluetoothSocketFloss::Listen(
     return;
   }
 
-  if (socket_type != FlossSocketManager::SocketType::kL2capLe &&
+  if (socket_type != FlossSocketManager::SocketType::kL2cap &&
       socket_type != FlossSocketManager::SocketType::kRfcomm) {
     std::move(error_callback).Run(kInvalidSocketType);
     return;
@@ -111,7 +111,7 @@ void BluetoothSocketFloss::Listen(
       base::BindRepeating(&BluetoothSocketFloss::DoConnectionAccepted,
                           weak_ptr_factory_.GetWeakPtr());
 
-  if (socket_type == FlossSocketManager::SocketType::kL2capLe) {
+  if (socket_type == FlossSocketManager::SocketType::kL2cap) {
     FlossDBusManager::Get()->GetSocketManager()->ListenUsingL2cap(
         security, std::move(callback), state_change, accepted);
   } else if (socket_type == FlossSocketManager::SocketType::kRfcomm) {
