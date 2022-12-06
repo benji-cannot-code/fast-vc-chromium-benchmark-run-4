4ade0e6d1dcf73ed22c9bdeab605331b0da893e4
Bob Beck <bbe@google.com>
2022-12-06 23:08:45 +0000

    Remove remaining use of base/containers/span.h from net/cert/pki
    Convert the two remaining to std::string_view or bssl::Span
    
    Bug: 1322914
    Change-Id: I382914cc51637e6ac3d26b74f335319b113828e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4057549
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080043}

--- net/cert/pki/path_builder_unittest.cc
+++ net/cert/pki/path_builder_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_paths.h"
 #include "base/callback_forward.h"
-#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/test/bind.h"
