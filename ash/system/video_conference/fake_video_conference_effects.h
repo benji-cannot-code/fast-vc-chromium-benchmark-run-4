7c45268c5e08038b032c5a5fe6b4b3a2cb355d3e
Roger Tinkoff <rtinkoff@google.com>
2022-12-06 21:24:46 +0000

    ash: Small cleanups for the "fake" effects
    Remove some arguments that are identical for all simple fake
    effects. This addresses some review comments that were added to the
    following after it was submitted:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/4067479
    
    Bug: b/259838099
    Change-Id: I2d0065d3f4d7139f8e47e41ef539462e90105892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081209
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079978}

--- ash/system/video_conference/fake_video_conference_effects.h
+++ ash/system/video_conference/fake_video_conference_effects.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_VIDEO_CONFERENCE_FAKE_VIDEO_CONFERENCE_EFFECTS_H_
 
 #include "ash/ash_export.h"
-#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/system/video_conference/video_conference_tray_effects_delegate.h"
 #include "ash/system/video_conference/video_conference_tray_effects_manager_types.h"
 #include "ui/views/controls/button/button.h"
@@ -18,9 +17,7 @@ namespace ash::fake_video_conference {
 // type of effect there is i.e. a toggle with only one state.
 class SimpleToggleEffect : public VcEffectsDelegate {
  public:
-  SimpleToggleEffect(const gfx::VectorIcon* icon,
-                     const std::u16string& label_text,
-                     int accessible_name_id,
+  SimpleToggleEffect(const std::u16string& label_text,
                      views::Button::PressedCallback button_callback);
 
   SimpleToggleEffect(const SimpleToggleEffect&) = delete;
