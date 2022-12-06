58110a4d67542b9f29cbc7d6b1d102a0f8c86968
Peter Kotwicz <pkotwicz@chromium.org>
2022-12-06 01:29:13 +0000

    [FedCM] Send Origin header instead of Referrer header
    BUG=1381227
    
    Change-Id: Ifa2c3a74a05568d9f01c56c9e08703235891a02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018494
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079580}

--- content/browser/webid/idp_network_request_manager_unittest.cc
+++ content/browser/webid/idp_network_request_manager_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/public/browser/identity_request_dialog_controller.h"
 #include "content/public/browser/manifest_icon_downloader.h"
+#include "net/http/http_request_headers.h"
 #include "net/http/http_status_code.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -88,6 +89,12 @@ std::string ReplaceFirstLineWithKeyFromJson(const std::string& key,
 std::string RemoveAllLinesWithKeyFromJson(const std::string& key,
                                           const std::string& input) {
   return ReplaceFirstLineWithKeyFromJson(key, "", input, /*replace_all=*/true);
+}
+
+url::Origin GetOriginHeader(const network::ResourceRequest& request) {
+  std::string origin;
+  request.headers.GetHeader(net::HttpRequestHeaders::kOrigin, &origin);
+  return url::Origin::Create(GURL(origin));
 }
 
 class IdpNetworkRequestManagerTest : public ::testing::Test {
@@ -750,15 +757,17 @@ TEST_F(IdpNetworkRequestManagerTest, ParseManifestBrandingMinSize) {
   }
 }
 
-// Tests that we send the correct referrer for account requests.
-TEST_F(IdpNetworkRequestManagerTest, AccountRequestReferrer) {
+// Tests that we send the correct origin for account requests.
+TEST_F(IdpNetworkRequestManagerTest, AccountRequestOrigin) {
   bool called = false;
   auto interceptor =
       base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
         called = true;
         EXPECT_EQ(GURL(kTestAccountsEndpoint), request.url);
         EXPECT_EQ(request.request_body, nullptr);
-        EXPECT_EQ(false, request.referrer.is_valid());
+        EXPECT_FALSE(request.referrer.is_valid());
+        EXPECT_FALSE(
+            request.headers.HasHeader(net::HttpRequestHeaders::kOrigin));
       });
   test_url_loader_factory().SetInterceptor(interceptor);
 
@@ -791,6 +800,8 @@ TEST_F(IdpNetworkRequestManagerTest, AccountSignedInStatus) {
         EXPECT_EQ(GURL(kTestAccountsEndpoint), request.url);
         EXPECT_EQ(request.request_body, nullptr);
         EXPECT_FALSE(request.referrer.is_valid());
+        EXPECT_FALSE(
+            request.headers.HasHeader(net::HttpRequestHeaders::kOrigin));
       });
   test_url_loader_factory().SetInterceptor(interceptor);
 
@@ -855,7 +866,9 @@ TEST_F(IdpNetworkRequestManagerTest, TokenRequest) {
       base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
         called = true;
         EXPECT_EQ(GURL(kTestTokenEndpoint), request.url);
-        EXPECT_EQ(GURL(kTestRpUrl), request.referrer);
+        EXPECT_FALSE(request.referrer.is_valid());
+        EXPECT_EQ(url::Origin::Create(GURL(kTestRpUrl)),
+                  GetOriginHeader(request));
 
         // Check that the request body is correct (should be "request")
         ASSERT_NE(request.request_body, nullptr);
@@ -888,7 +901,9 @@ TEST_F(IdpNetworkRequestManagerTest, ClientMetadata) {
             std::string(kTestClientMetadataEndpoint) + "?client_id=xxx";
         EXPECT_EQ(GURL(url_string), request.url);
         EXPECT_EQ(request.request_body, nullptr);
-        EXPECT_EQ(GURL(kTestRpUrl), request.referrer);
+        EXPECT_FALSE(request.referrer.is_valid());
+        EXPECT_EQ(url::Origin::Create(GURL(kTestRpUrl)),
+                  GetOriginHeader(request));
       });
   test_url_loader_factory().SetInterceptor(interceptor);
   IdpClientMetadata data = SendClientMetadataRequestAndWaitForResponse("xxx");
@@ -907,6 +922,8 @@ TEST_F(IdpNetworkRequestManagerTest, RecordApprovedClientsMetrics) {
         EXPECT_EQ(GURL(kTestAccountsEndpoint), request.url);
         EXPECT_EQ(request.request_body, nullptr);
         EXPECT_FALSE(request.referrer.is_valid());
+        EXPECT_FALSE(
+            request.headers.HasHeader(net::HttpRequestHeaders::kOrigin));
       });
   test_url_loader_factory().SetInterceptor(interceptor);
 
