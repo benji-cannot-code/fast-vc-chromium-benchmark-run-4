a84896f5e9d435c89aebad5851f8f02c72714c6f
Jiahe Zhang <jiahe.zhang@intel.com>
2022-12-07 02:12:36 +0000

    Add FeatureParam for kQuickIntensiveWakeUpThrottlingAfterLoading
    Make the grace period of loaded pages configurable and change the
    default value to 60s based on suggestions from blink-dev.
    
    Bug: 1324656
    Change-Id: I3b856ba878cfe8756276a9e9be387e40cf098f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082054
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Jiahe Zhang <jiahe.zhang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1080134}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl_unittest.cc
@@ -3122,7 +3122,8 @@ TEST_F(FrameSchedulerImplTestQuickIntensiveWakeUpThrottlingEnabled,
 
 TEST_F(FrameSchedulerImplTestQuickIntensiveWakeUpThrottlingEnabled,
        LoadedPageGracePeriod) {
-  EXPECT_EQ(base::Seconds(kIntensiveWakeUpThrottling_GracePeriodSeconds_Loaded),
+  EXPECT_EQ(base::Seconds(
+                kIntensiveWakeUpThrottling_GracePeriodSecondsLoaded_Default),
             GetIntensiveWakeUpThrottlingGracePeriod(false));
 }
 
