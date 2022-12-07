f50fda4c0b53ec94c8d0c616bc1f6ebbeea265d0
Wenbo Jie <wenbojie@chromium.org>
2022-12-07 06:14:25 +0000

    [FilesRefresh] Move the css prefix util function to dom_utils
    Bug: b:242938200
    Change-Id: Ic0d1bfebd4384d842dc3e80f15b7301aac33e3ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083031
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080193}

--- ui/file_manager/file_manager/common/js/dom_utils.ts
+++ ui/file_manager/file_manager/common/js/dom_utils.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assertInstanceof} from 'chrome://resources/js/assert_ts.js';
 import {decorate} from './ui.js';
+import type {CSSResult} from '../../widgets/xf_base.js';
 
 /**
  * Function to be used as event listener for `mouseenter`, it sets the `title`
@@ -158,4 +159,34 @@ class UserDomError extends DOMError {
   override get message(): string {
     return this.message_;
   }
+}
+
+/**
+ * Add prefix selector for the CSS literal.
+ * To support both Legacy and Refresh23 styles in the same component, we
+ * have 2 style groups defined in each component, for all legacy/refresh23
+ * specific styles, we need to prefix all rules to have
+ * `[theme=legacy]` and `[theme=refresh23]` so they won't conflict
+ * with each other.
+ *
+ * For example:
+ * original style -> p { color: red; }
+ * prefix with Legacy -> :host-context([theme="legacy"]) p { color: red }
+ * prefix with Refresh23 -> :host-context([theme="refresh23"]) p { color: red }
+ */
+export function addCSSPrefixSelector(
+    css: CSSResult, prefixSelector: string): CSSStyleSheet {
+  const prefixedCSS = new CSSStyleSheet();
+  const cssRules = css.styleSheet?.cssRules || [];
+  for (let i = 0; i < cssRules.length; i++) {
+    const cssText = cssRules[i]?.cssText;
+    if (cssText) {
+      // If the existing selector is `:host` or `:host-context`, there should
+      // be no space after the newly added `:host-context`.
+      const noSpace = cssText.startsWith(':host');
+      prefixedCSS.insertRule(
+          `:host-context(${prefixSelector})${noSpace ? '' : ' '}${cssText}`, i);
+    }
+  }
+  return prefixedCSS;
 }
--- ui/file_manager/file_manager/widgets/xf_tree_item.ts
+++ ui/file_manager/file_manager/widgets/xf_tree_item.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/polymer/v3_0/paper-ripple/paper-ripple.js';
 
-import {util} from '../common/js/util.js';
+import {addCSSPrefixSelector} from '../common/js/dom_utils.js';
 
 import {css, customElement, html, ifDefined, property, PropertyValues, query, state, styleMap, XfBase} from './xf_base.js';
 import type {XfTree} from './xf_tree.js';
@@ -518,8 +518,8 @@ function getCSS() {
 
   return [
     commonCSS,
-    util.addCSSPrefixSelector(legacyStyle, '[theme="legacy"]'),
-    util.addCSSPrefixSelector(refresh23Style, '[theme="refresh23"]'),
+    addCSSPrefixSelector(legacyStyle, '[theme="legacy"]'),
+    addCSSPrefixSelector(refresh23Style, '[theme="refresh23"]'),
   ];
 }
 
