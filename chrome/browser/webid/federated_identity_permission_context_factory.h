56b8d50d7a94cccc57dc2bce66f77f56d8b35380
Nicolás Peña Moreno <npm@chromium.org>
2022-12-07 20:08:27 +0000

    [FedCM] Check that the FederatedIdentityPermissionContext exists
    This CL avoids creating a new FederatedIdentityPermissionContext if one
    does not already exist for the profile in FederatedIdentityPermissionContextFactory::BrowserContextShutdown()
    
    The crash was occurring because ProfileKeyedServiceFactory::GetServiceForBrowserContext() is not
    guaranteed to return a non-null context even when create==true when
    the SystemProfileSelectionDefaultNone feature is enabled.
    
    Bug: 1396251
    Change-Id: I931ade57495704622cbe67cb9f8a97faad2d2370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4084042
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080510}

--- chrome/browser/webid/federated_identity_permission_context_factory.h
+++ chrome/browser/webid/federated_identity_permission_context_factory.h
@@ -18,6 +18,8 @@ class FederatedIdentityPermissionContextFactory
  public:
   static FederatedIdentityPermissionContext* GetForProfile(
       content::BrowserContext* profile);
+  static FederatedIdentityPermissionContext* GetForProfileIfExists(
+      content::BrowserContext* profile);
   static FederatedIdentityPermissionContextFactory* GetInstance();
 
  private:
