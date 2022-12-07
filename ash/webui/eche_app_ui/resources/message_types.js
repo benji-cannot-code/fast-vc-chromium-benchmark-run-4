8922148b61bceb04b9c51826de3e6811cf7a3689
Denalex Orakwue <corakwue@google.com>
2022-12-07 08:56:08 +0000

    [Eche] Add feature flag: kEcheSWACheckAndroidNetworkInfo
    This flag allows CrOS to analyze Android network information to provide more context on connection errors.
    
    Bug: b/243831955
    Change-Id: Ib96e39f9a6422e62a30b37763fd024d45fba512a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080869
    Reviewed-by: Abbas Nayebi <nayebi@google.com>
    Commit-Queue: Denalex Orakwue <corakwue@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080228}

--- ash/webui/eche_app_ui/resources/message_types.js
+++ ash/webui/eche_app_ui/resources/message_types.js
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    MeasureLatency: boolean,
  *    sendStartSignaling: boolean,
  *    disable_stun_server: boolean,
+ *    check_android_network_info: boolean,
  * }}
  */
 /* #export */ let SystemInfo;
