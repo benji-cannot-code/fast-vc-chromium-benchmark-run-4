b4781273b3f583f16befd6d715dcea7c09df9d30
Emmanuel Arias Soto <eariassoto@google.com>
2022-12-07 06:38:22 +0000

    [CrOS Cellular] Pass Value::List as pointer to UserApnListToOnc
    To support the APN Revamp feature, the migrator service in the browser
    may have to request Shill to reset the user_apn_list ONC field for an
    specific network.
    This changes enables UserApnListToOnc to set Type::NONE (JSON null)
    for the user_apn_list field when the parameter is nullptr.
    
    Bug: b:162365553
    Test: chromeos_unittests
    Change-Id: If79780135666b6f015b9650232b78bf5cea6ec44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075951
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080203}

--- chromeos/services/network_config/cros_network_config.cc
+++ chromeos/services/network_config/cros_network_config.cc
@@ -3648,8 +3648,7 @@ void CrosNetworkConfig::CreateCustomApn(const std::string& network_guid,
   network_metadata_store->SetCustomApnList(network_guid, new_apns.Clone());
 
   SetPropertiesInternal(
-      network_guid, *network,
-      UserApnListToOnc(network_guid, std::move(new_apns)),
+      network_guid, *network, UserApnListToOnc(network_guid, &new_apns),
       base::BindOnce(
           [](const std::string& guid, mojom::ApnPropertiesPtr apn, bool success,
              const std::string& message) {
@@ -3722,8 +3721,7 @@ void CrosNetworkConfig::RemoveCustomApn(const std::string& network_guid,
 
   network_metadata_store->SetCustomApnList(network_guid, new_apns.Clone());
   SetPropertiesInternal(
-      network_guid, *network,
-      UserApnListToOnc(network_guid, std::move(new_apns)),
+      network_guid, *network, UserApnListToOnc(network_guid, &new_apns),
       base::BindOnce(
           [](const std::string& guid, std::vector<mojom::ApnType> apn_types,
              bool success, const std::string& message) {
@@ -3801,8 +3799,7 @@ void CrosNetworkConfig::ModifyCustomApn(const std::string& network_guid,
   network_metadata_store->SetCustomApnList(network_guid,
                                            new_custom_apns.Clone());
   SetPropertiesInternal(
-      network_guid, *network,
-      UserApnListToOnc(network_guid, std::move(new_custom_apns)),
+      network_guid, *network, UserApnListToOnc(network_guid, &new_custom_apns),
       base::BindOnce(
           [](const std::string& guid, bool success,
              const std::string& message) {
