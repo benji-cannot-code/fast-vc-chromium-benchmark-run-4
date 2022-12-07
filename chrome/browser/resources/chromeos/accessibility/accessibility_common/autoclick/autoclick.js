2854f6065f543dfcc57320c9e6ee4f37ec44c0f0
Katie Dektar <katie@chromium.org>
2022-12-07 20:01:30 +0000

    [Autoclick] Add Autoclick integration tests
    Adds tests which use mouse hovers to change Autoclick
    event types and check that the different click types
    perform as expected on HTML elements.
    
    AX-Relnotes: n/a
    Test: New
    Bug: b:261462562
    Change-Id: I2fc6d12a517d7070d66170ce7a6786174d6c4b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083729
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080500}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/autoclick/autoclick.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/autoclick/autoclick.js
@@ -48,6 +48,9 @@ export class Autoclick {
         [], chrome.automation.EventType.MOUSE_PRESSED,
         event => this.onAutomationHitTestResult_(event), {capture: true});
 
+    /** @private {?function()} */
+    this.onLoadDesktopCallbackForTest_ = null;
+
     this.init_();
   }
 
@@ -83,6 +86,11 @@ export class Autoclick {
       // at that point, in order to find the scrollable area.
       this.hitTestHandler_.setNodes(this.desktop_);
       this.hitTestHandler_.start();
+
+      if (this.onLoadDesktopCallbackForTest_) {
+        this.onLoadDesktopCallbackForTest_();
+        this.onLoadDesktopCallbackForTest_ = null;
+      }
     });
 
     chrome.accessibilityPrivate.onScrollableBoundsForPointRequested.addListener(
@@ -177,5 +185,19 @@ export class Autoclick {
     // The hit test will come back through onAutmoationHitTestResult_,
     // which will do the logic for finding the scrolling container.
     this.desktop_.hitTest(x, y, chrome.automation.EventType.MOUSE_PRESSED);
+  }
+
+  /**
+   * Used by C++ tests to ensure Autoclick JS load is completed.
+   * @param {!function()} callback Callback for when desktop is loaded from
+   * automation.
+   */
+  setOnLoadDesktopCallbackForTest(callback) {
+    if (!this.desktop_) {
+      this.onLoadDesktopCallbackForTest_ = callback;
+      return;
+    }
+    // Desktop already loaded.
+    callback();
   }
 }
