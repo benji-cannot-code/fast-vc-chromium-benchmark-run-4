a1b2e547917ded5f8bb509d090db4a1abfb67f3f
Mason Freed <masonf@chromium.org>
2022-12-07 21:52:47 +0000

    Add the shadowrootmode attribute, and trigger streaming only if present
    This CL adds the new "shadowrootmode" attribute for use with
    declarative Shadow DOM. See the discussion at [1] for more details,
    but this will be the new standardized attribute to enable DSD.
    This CL triggers the other new behavior, streaming, based on the
    presence of this new attribute. In this way, the old behavior and
    the new behavior can both co-exist while developers migrate from
    the old attribute to the new one, and behavior will not change
    until they opt in.
    
    The virtual/streaming-declarative-shadow-dom-disabled test suite
    was enhanced with a copy of all of the "old" tests using the
    "shadowroot" attribute, as they were before [2] landed. In this way
    the virtual suite maintains the functionality of the old syntax.
    This virtual suite also runs the "new" tests, to ensure no inadvertent
    changes happen for the flag-disabled case.
    
    [1] https://github.com/whatwg/html/pull/5465#discussion_r1026760650
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/3967571
    
    Bug: 1042130,1379513,1396384
    Change-Id: I5a888287b7f313d13dd28c8b4b0e36ae4feea512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4039504
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080576}

--- third_party/blink/perf_tests/shadow_dom/resources/declarative.js
+++ third_party/blink/perf_tests/shadow_dom/resources/declarative.js
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     measureParseAndAppend(test_div, html);
     return test_div;
   }
-  PerfTestRunner.assert_true(HTMLTemplateElement.prototype.hasOwnProperty("shadowRoot"),'Declarative Shadow DOM not enabled/supported');
+  PerfTestRunner.assert_true(HTMLTemplateElement.prototype.hasOwnProperty("shadowRootMode"),'Declarative Shadow DOM not enabled/supported');
   PerfTestRunner.assert_true(testParse(getShadowMarkup(true, 1, 1)).firstChild.shadowRoot,'Declarative Shadow DOM not detected');
   PerfTestRunner.assert_true(getShadowMarkup(true, 5, 6).length === getShadowMarkup(false, 5, 6).length,'Shadow and light DOM content should have identical length');
   const light1 = testParse(getShadowMarkup(false, 5, 6, /*lightDomDuplicates=*/1)).querySelectorAll(lightDomElementName).length;
--- third_party/blink/web_tests/virtual/streaming-declarative-shadow-dom-disabled/README.md
+++ third_party/blink/web_tests/virtual/streaming-declarative-shadow-dom-disabled/README.md
@@ -1,2 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # This suite runs tests with --disable-features=StreamingDeclarativeShadowDOM.
+
+The non-streaming-tests folder contains a copy of the older tests, which examine the behavior of the 'shadowroot' attribute.
