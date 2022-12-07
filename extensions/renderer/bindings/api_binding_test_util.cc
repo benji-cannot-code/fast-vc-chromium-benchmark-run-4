e23192772465c84ae6fb0f4fb1514bd7f889acb0
Minoru Chikamune <chikamune@chromium.org>
2022-12-07 04:14:16 +0000

    Code Health Rotation: Remove DeprecatedListValueFromString
    Remove unused function: DeprecatedListValueFromString
    
    Bug: 1187062
    Change-Id: I1e02e4319b0598f9b05424f86b0f04f230afc37c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081949
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080169}

--- extensions/renderer/bindings/api_binding_test_util.cc
+++ extensions/renderer/bindings/api_binding_test_util.cc
@@ -99,11 +99,6 @@ std::unique_ptr<base::Value> DeprecatedValueFromString(base::StringPiece str) {
   return value;
 }
 
-std::unique_ptr<base::ListValue> DeprecatedListValueFromString(
-    base::StringPiece str) {
-  return base::ListValue::From(DeprecatedValueFromString(str));
-}
-
 std::unique_ptr<base::DictionaryValue> DeprecatedDictionaryValueFromString(
     base::StringPiece str) {
   return base::DictionaryValue::From(DeprecatedValueFromString(str));
