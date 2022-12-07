5ef66760cc32d125611741e5a37ac0a605352616
Peter Kotwicz <pkotwicz@chromium.org>
2022-12-07 02:18:36 +0000

    [FedCM] Enable FedCM if site is whitelisted for third-party cookies
    This CL enables FedCM in the scenario that:
    - third-party cookies are disabled by default
    AND
    - the site is whitelisted for third-party cookies
    
    BUG=1395388
    TEST=FederatedIdentityApiPermissionContextTest.WhitelistedSiteForThirdPartyCookies
    
    Change-Id: I1ed8b3fd80fca06e4bda649f6e163f8749505135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081346
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080139}

--- chrome/browser/webid/federated_identity_api_permission_context_unittest.cc
+++ chrome/browser/webid/federated_identity_api_permission_context_unittest.cc
@@ -6,16 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webid/federated_identity_api_permission_context.h"
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/webid/federated_identity_api_permission_context_factory.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/common/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 #include "url/origin.h"
+
+using PermissionStatus =
+    content::FederatedIdentityApiPermissionContextDelegate::PermissionStatus;
 
 class FederatedIdentityApiPermissionContextTest : public testing::Test {
  public:
@@ -32,6 +39,8 @@ class FederatedIdentityApiPermissionContextTest : public testing::Test {
     host_content_settings_map_ =
         HostContentSettingsMapFactory::GetForProfile(&profile_);
   }
+
+  Profile* profile() { return &profile_; }
 
  protected:
   base::raw_ptr<FederatedIdentityApiPermissionContext> context_;
@@ -61,4 +70,20 @@ TEST_F(FederatedIdentityApiPermissionContextTest, EnabledEmbargo) {
   // Embargoing `rp_url` should return the default content setting for `rp_url`.
   context_->RecordDismissAndEmbargo(url::Origin::Create(rp_url));
   EXPECT_EQ(CONTENT_SETTING_BLOCK, GetContentSetting(rp_url));
+}
+
+// Test that FedCM is not disabled if the user disabled all third-party cookies
+// but whitelisted the relying party.
+TEST_F(FederatedIdentityApiPermissionContextTest,
+       WhitelistedSiteForThirdPartyCookies) {
+  const GURL kRpUrl("https://rp.com");
+
+  profile()->GetPrefs()->SetInteger(
+      prefs::kCookieControlsMode,
+      static_cast<int>(content_settings::CookieControlsMode::kBlockThirdParty));
+  CookieSettingsFactory::GetForProfile(profile())->SetThirdPartyCookieSetting(
+      kRpUrl, ContentSetting::CONTENT_SETTING_ALLOW);
+
+  EXPECT_EQ(PermissionStatus::GRANTED,
+            context_->GetApiPermissionStatus(url::Origin::Create(kRpUrl)));
 }
