2d44a37773e53ead46b413e1c98e803f3ba1d980
Cici Ruan <cuicuiruan@google.com>
2022-12-07 01:19:58 +0000

    arc/gio: Apply AlphaV2 flag control
    Split Action reposition and menu reposition feature from Beta version
    and put it into AlphaV2 version. Beta version includes the feature in
    AlphaV2.
    
    Also renamed AlphaTwo to AlphaV2.
    
          TouchInjectorTest.
          feature correctly.
    
    Bug: b/261064306
    Test: passed existing unit tests - ArcInputOverlayManagerTest,
    Test: manual test to turn on/off flags to see if the flags control the
    Change-Id: I756d28fc2a0876a4b102e920d6e56823fa7e49f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074249
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080108}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -153,9 +153,9 @@ BASE_FEATURE(kArcInputOverlayBeta,
              "ArcInputOverlayBeta",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Controls whether to enable support for ARC Input Overlay Alpha Two.
-BASE_FEATURE(kArcInputOverlayAlphaTwo,
-             "ArcInputOverlayAlphaTwo",
+// Controls whether to enable support for ARC Input Overlay Alpha v2.
+BASE_FEATURE(kArcInputOverlayAlphaV2,
+             "ArcInputOverlayAlphaV2",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Controls whether to enable support for ARC ADB sideloading for managed
@@ -2293,8 +2293,8 @@ bool IsArcInputOverlayBetaEnabled() {
   return base::FeatureList::IsEnabled(kArcInputOverlayBeta);
 }
 
-bool IsArcInputOverlayAlphaTwoEnabled() {
-  return base::FeatureList::IsEnabled(kArcInputOverlayAlphaTwo);
+bool IsArcInputOverlayAlphaV2Enabled() {
+  return base::FeatureList::IsEnabled(kArcInputOverlayAlphaV2);
 }
 
 bool IsArcNetworkDiagnosticsButtonEnabled() {
--- chrome/browser/ash/arc/input_overlay/touch_injector_unittest.cc
+++ chrome/browser/ash/arc/input_overlay/touch_injector_unittest.cc
@@ -330,7 +330,6 @@ class TouchInjectorTest : public views::ViewsTestBase {
         *widget_->GetNativeWindow()->GetProperty(ash::kArcPackageNameKey),
         base::BindLambdaForTesting(
             [&](std::unique_ptr<AppDataProto>, std::string) {}));
-    injector_->set_beta(true);
   }
 
   void TearDown() override {
@@ -869,6 +868,7 @@ TEST_F(TouchInjectorTest, TestProtoConversion) {
   // Check whether AppDataProto is serialized correctly.
   auto json_value =
       base::JSONReader::ReadAndReturnValueWithError(kValidJsonActionTapKey);
+  injector_->set_allow_reposition(true);
   injector_->ParseActions(*json_value);
   // Simulate a menu entry position change.
   auto menu_entry_location_point = gfx::Point(5, 5);
@@ -912,7 +912,7 @@ TEST_F(TouchInjectorTest, TestProtoConversion) {
       *widget_->GetNativeWindow()->GetProperty(ash::kArcPackageNameKey),
       base::BindLambdaForTesting(
           [&](std::unique_ptr<AppDataProto>, std::string) {}));
-  injector->set_beta(true);
+  injector->set_allow_reposition(true);
   injector->ParseActions(*json_value);
   injector->OnProtoDataAvailable(*proto);
   EXPECT_EQ(injector_->actions().size(), injector->actions().size());
@@ -930,6 +930,7 @@ TEST_F(TouchInjectorTest, TestProtoConversion) {
 TEST_F(TouchInjectorTest, TestAddAction) {
   auto json_value =
       base::JSONReader::ReadAndReturnValueWithError(kValidJsonActionTapKey);
+  injector_->set_beta(true);
   injector_->ParseActions(*json_value);
   EXPECT_EQ(2u, injector_->actions().size());
 
@@ -1017,6 +1018,7 @@ TEST_F(TouchInjectorTest, TestAddAction) {
 TEST_F(TouchInjectorTest, TestDeleteAction) {
   auto json_value =
       base::JSONReader::ReadAndReturnValueWithError(kValidJsonActionTapKey);
+  injector_->set_beta(true);
   injector_->ParseActions(*json_value);
   TouchInjectorResetAndAddTwoActions(injector_.get());
   ExpectActionSizes(/*size_pending_add_user_actions=*/0u,
