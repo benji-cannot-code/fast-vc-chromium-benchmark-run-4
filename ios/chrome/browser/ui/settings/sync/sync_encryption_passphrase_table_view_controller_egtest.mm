3cf606064377b88363b41a4c51da3c691e1fb8a7
Benjamin Williams <bwwilliams@google.com>
2022-12-07 15:25:45 +0000

    [ios] Introduce setUp/tearDown in SyncEncryptionPassphraseTestCase tests
    This change introduces setUp/tearDown methods to
    SyncEncryptionPassphraseTestCase to address flake.
    
    Each test needs to clear data from the sync server, as well as wait for
    the sync engine to initialize, before running.
    
    Bug: 1243139
    Change-Id: I16b2a8f236b1d177767379b0b6c1615014daabea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083666
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080342}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/metrics/metrics_app_interface.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/settings_table_view_controller_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
@@ -42,6 +44,24 @@ NSString* const kPassphrase = @"hello";
 @end
 
 @implementation SyncEncryptionPassphraseTestCase
+
+- (void)tearDown {
+  [SigninEarlGrey signOut];
+  [ChromeEarlGrey
+      waitForSyncEngineInitialized:NO
+                       syncTimeout:syncher::kSyncUKMOperationsTimeout];
+  [ChromeEarlGrey clearSyncServerData];
+
+  [super tearDown];
+}
+
+- (void)setUp {
+  [super setUp];
+
+  [ChromeEarlGrey
+      waitForSyncEngineInitialized:NO
+                       syncTimeout:syncher::kSyncUKMOperationsTimeout];
+}
 
 // Tests to open the sync passphrase view, and to close it.
 - (void)testShowSyncPassphraseAndDismiss {
