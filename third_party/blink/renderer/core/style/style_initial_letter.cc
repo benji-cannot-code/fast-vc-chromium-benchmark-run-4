0ef3790707c997fcecffb238bad74d99fe40abdd
Kent Tamura <tkent@chromium.org>
2022-12-07 08:49:15 +0000

    initial-letter: Fix a negative sink value by a large size value
    Cast from a positive float to the int type can be negative.
    
    Fixed: 1395673
    Change-Id: Ic09c61bab440d0343a9e50e42152ae5e8fe1007f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082356
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080227}

--- third_party/blink/renderer/core/style/style_initial_letter.cc
+++ third_party/blink/renderer/core/style/style_initial_letter.cc
@@ -6,13 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/style_initial_letter.h"
 
 #include "base/check_op.h"
+#include "base/numerics/safe_conversions.h"
 
 namespace blink {
 
 StyleInitialLetter::StyleInitialLetter() = default;
 
 StyleInitialLetter::StyleInitialLetter(float size)
-    : size_(size), sink_(std::floor(size)), sink_type_(kOmitted) {
+    : size_(size),
+      sink_(base::saturated_cast<int>(std::floor(size))),
+      sink_type_(kOmitted) {
   DCHECK_GE(size_, 1);
   DCHECK_GE(sink_, 1);
 }
@@ -25,7 +28,8 @@ StyleInitialLetter::StyleInitialLetter(float size, int sink)
 
 StyleInitialLetter::StyleInitialLetter(float size, SinkType sink_type)
     : size_(size),
-      sink_(sink_type == kDrop ? std::floor(size) : 1),
+      sink_(sink_type == kDrop ? base::saturated_cast<int>(std::floor(size))
+                               : 1),
       sink_type_(sink_type) {
   DCHECK_GE(size_, 1);
   DCHECK_GE(sink_, 1);
