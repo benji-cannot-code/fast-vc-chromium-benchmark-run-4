8f7ae33bc91af4ec254f1777813c3e5a55e679bd
Yuichiro Hanada <yhanada@google.com>
2022-12-07 10:18:02 +0000

    Run component_extension_manager_unittest
    It's not included in ui_base_unittests for some reason.
    This CL cleans it up and re-adds it to ui_base_unittests
    
    Bug: None
    Test: ui_base_unittests
    Change-Id: Ia6c1bc5dc7fdc20cdd961ea4c4e2a71b745e4ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083034
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080258}

--- ui/base/ime/ash/mock_component_extension_ime_manager_delegate.cc
+++ ui/base/ime/ash/mock_component_extension_ime_manager_delegate.cc
@@ -11,7 +11,6 @@ namespace input_method {
 
 MockComponentExtensionIMEManagerDelegate::
     MockComponentExtensionIMEManagerDelegate() = default;
-
 MockComponentExtensionIMEManagerDelegate::
     ~MockComponentExtensionIMEManagerDelegate() = default;
 
@@ -24,7 +23,10 @@ void MockComponentExtensionIMEManagerDelegate::Load(
     Profile* profile,
     const std::string& extension_id,
     const std::string& manifest,
-    const base::FilePath& path) {}
+    const base::FilePath& path) {
+  load_call_count_++;
+  last_loaded_extension_id_ = extension_id;
+}
 
 bool MockComponentExtensionIMEManagerDelegate::IsInLoginLayoutAllowlist(
     const std::string& layout) {
