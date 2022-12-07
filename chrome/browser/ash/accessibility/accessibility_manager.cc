b5547636b0697ffcfa65770d31ecb1bce133b8b9
Anastasia Helfinstein <anastasi@google.com>
2022-12-07 19:15:43 +0000

    [Accessibility] Check profile_ before using in AccessibilityManager
    This changes fixes a crash condition by checking that profile_ is not
    null before using it in the AccessibilityManager.
    
    R=jamescook
    
    AX-Relnotes: n/a.
    Bug: 1396444
    Test: cq
    Change-Id: I03d1ba02618492871a3fe82aacde225a54762d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087063
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080476}

--- chrome/browser/ash/accessibility/accessibility_manager.cc
+++ chrome/browser/ash/accessibility/accessibility_manager.cc
@@ -783,6 +783,8 @@ bool AccessibilityManager::PlaySpokenFeedbackToggleCountdown(int tick_count) {
 void AccessibilityManager::HandleAccessibilityGesture(
     ax::mojom::Gesture gesture,
     gfx::PointF location) {
+  if (!profile_)
+    return;
   extensions::EventRouter* event_router =
       extensions::EventRouter::Get(profile_);
 
