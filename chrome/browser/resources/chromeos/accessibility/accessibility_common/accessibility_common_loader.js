ffd13c8dab462336dd8429e147cede1ad3e42d46
Katie Dektar <katie@chromium.org>
2022-12-07 23:19:17 +0000

    Reland "[Autoclick] Add Autoclick integration tests"
    This reverts commit 8b37ce9684ac2c2b023c09f6c3dcadce72f39826.
    
    Reason for reland: Disabling flaky test and will try to
    fix it separately. Speculatively I can't use BindLambdaForTesting
    because the bound object goes out of scope.
    
    Original change's description:
    > [Autoclick] Add Autoclick integration tests
    >
    > Adds tests which use mouse hovers to change Autoclick
    > event types and check that the different click types
    > perform as expected on HTML elements.
    >
    > AX-Relnotes: n/a
    > Test: New
    > Bug: b:261462562
    > Change-Id: I2fc6d12a517d7070d66170ce7a6786174d6c4b8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083729
    > Commit-Queue: Katie Dektar <katie@chromium.org>
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1080500}
    
    Bug: b:261462562
    Change-Id: Ia85dc61b4b7e626e3b661b416cd654feda6b2ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087264
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080635}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/accessibility_common_loader.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/accessibility_common_loader.js
@@ -22,6 +22,10 @@ export class AccessibilityCommon {
     /** @private {Dictation} */
     this.dictation_ = null;
 
+    // For tests.
+    /** @private {?function()} */
+    this.autoclickLoadCallbackForTest_ = null;
+
     this.init_();
   }
 
@@ -83,6 +87,11 @@ export class AccessibilityCommon {
     if (details.value && !this.autoclick_) {
       // Initialize the Autoclick extension.
       this.autoclick_ = new Autoclick();
+      if (this.autoclickLoadCallbackForTest_) {
+        this.autoclick_.setOnLoadDesktopCallbackForTest(
+            this.autoclickLoadCallbackForTest_);
+        this.autoclickLoadCallbackForTest_ = null;
+      }
     } else if (!details.value && this.autoclick_) {
       // TODO(crbug.com/1096759): Consider using XHR to load/unload autoclick
       // rather than relying on a destructor to clean up state.
@@ -118,6 +127,20 @@ export class AccessibilityCommon {
       this.dictation_.onDictationDisabled();
       this.dictation_ = null;
     }
+  }
+
+  /**
+   * Used by C++ tests to ensure Autoclick load is completed.
+   * Set on AccessibilityCommon in case Autoclick has not started up yet.
+   * @param {!function()} callback Callback for Autoclick JS load complete.
+   */
+  setAutoclickLoadCallbackForTest(callback) {
+    if (!this.autoclick_) {
+      this.autoclickLoadCallbackForTest_ = callback;
+      return;
+    }
+    // Autoclick already loaded.
+    this.autoclick_.setOnLoadDesktopCallbackForTest(callback);
   }
 }
 
