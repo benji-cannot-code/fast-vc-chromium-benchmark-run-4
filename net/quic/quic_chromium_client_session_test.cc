e352940c17dc1f6a3c90badb92c962c7d1589400
Yoichi Osato <yoichio@chromium.org>
2022-12-07 02:05:26 +0000

    Roll src/net/third_party/quiche/src/ 9b4551033..2baa83fd4 (6 commits)
    https://quiche.googlesource.com/quiche.git/+log/9b4551033df6..2baa83fd486a
    
    $ git log 9b4551033..2baa83fd4 --date=short --no-merges --format='%ad %ae %s'
    2022-12-05 fayang Internal change
    2022-12-05 vasilvv Make QUICHE compile for MacOS
    2022-12-02 vasilvv Fix Balsa tests to pass on MacOS.
    2022-12-02 achernya Produce correct ICMP6 Echo Reply when pinging magic link-local address
    2022-12-01 ianswett Adds a BB2U copt causes QUIC BBR2 to wait two rounds with out draining the queue before exiting PROBE_UP and BB2S has the same effect in STARTUP.
    2022-12-01 renjietang [QUIC] Do not send multi-port probing when there's no active stream. Limit the max number of multi-port path creations to 5.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: Ifec14d413840d5d3aca5497fc1c634ce3b67d852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080171
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080131}

--- net/quic/quic_chromium_client_session_test.cc
+++ net/quic/quic_chromium_client_session_test.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quiche/src/quiche/quic/core/http/quic_client_promised_info.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_connection_id.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_packet_writer.h"
+#include "net/third_party/quiche/src/quiche/quic/core/quic_tag.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_utils.h"
 #include "net/third_party/quiche/src/quiche/quic/platform/api/quic_flags.h"
 #include "net/third_party/quiche/src/quiche/quic/platform/api/quic_test.h"
@@ -2470,11 +2471,11 @@ TEST_P(QuicChromiumClientSessionTest, DegradingWithMultiPortEnabled) {
   allow_port_migration_ = true;
   SetIetfConnectionMigrationFlagsAndConnectionOptions();
   auto options = config_.SendConnectionOptions();
-  options.push_back(quic::kMPQC);
+  config_.SetClientConnectionOptions(quic::QuicTagVector{quic::kMPQC});
   config_.SetConnectionOptionsToSend(options);
 
   Initialize();
-  EXPECT_TRUE(session_->connection()->multi_port_enabled());
+  EXPECT_TRUE(session_->connection()->multi_port_stats());
 
   session_->ReallyOnPathDegrading();
   EXPECT_EQ(1u, connectivity_monitor_->GetNumDegradingSessions());
