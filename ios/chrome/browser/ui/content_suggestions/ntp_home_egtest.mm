84a65d4309228d9f043be806e8e07ca19b733021
Chris Lu <thegreenfrog@chromium.org>
2022-12-07 13:26:43 +0000

    [ios] Re-enable testTrendingQueries
    Fixed: 1379289, 1364725
    Change-Id: I2ba36bbad0e448b5d322a60a3270bed8260ebac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081611
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080299}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -134,12 +134,6 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
   [super tearDown];
 }
 
-// TODO(crbug.com/1379289): Test fails on simulator.
-#if TARGET_IPHONE_SIMULATOR
-#define MAYBE_testTrendingQueries DISABLED_testTrendingQueries
-#else
-#define MAYBE_testTrendingQueries testTrendingQueries
-#endif
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   // Use commandline args to enable the Discover feed for this test case.
   // Disabled elsewhere to account for possible flakiness.
@@ -149,11 +143,6 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
   config.features_disabled.push_back(kEnableFeedAblation);
 
   config.features_enabled.push_back(kContentSuggestionsUIModuleRefresh);
-  if ([self isRunningTest:@selector(MAYBE_testTrendingQueries)]) {
-    // Enable arm that does not hide shortcuts.
-    config.features_enabled.push_back(kTrendingQueriesModule);
-    config.variations_enabled = {3350760};
-  }
   return config;
 }
 
@@ -510,8 +499,12 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 
 // Tests that the trending queries module header is visible and all four
 // trending queries are interactable.
-// TODO(crbug.com/1379289): Test fails on simulator.
-- (void)MAYBE_testTrendingQueries {
+- (void)testTrendingQueries {
+  AppLaunchConfiguration config = self.appConfigurationForTestCase;
+  config.features_enabled.push_back(kTrendingQueriesModule);
+  config.variations_enabled = {3350760};
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
   [[EarlGrey
       selectElementWithMatcher:
           grey_accessibilityID([NSString
@@ -520,7 +513,6 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
                   l10n_util::GetNSString(
                       IDS_IOS_CONTENT_SUGGESTIONS_TRENDING_QUERIES_MODULE_TITLE)])]
       assertWithMatcher:grey_sufficientlyVisible()];
-
   for (int index = 0; index < 4; index++) {
     [[EarlGrey
         selectElementWithMatcher:
