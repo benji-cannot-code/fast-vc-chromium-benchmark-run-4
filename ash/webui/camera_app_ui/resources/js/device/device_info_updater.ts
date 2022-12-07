4c8fccfe6a3e140d7c15ea7d03327630e6574bfe
Pi-Hsun Shih <pihsun@chromium.org>
2022-12-07 05:24:43 +0000

    CCA: Use CameraInfo in DeviceInfoUpdater listener callback
    Currently the DeviceInfoUpdater pass itself into the listener callback
    and the only client constructs a CameraInfo out of it immediately.
    Streamline the whole thing and make the DeviceInfoUpdater directly
    construct a CameraInfo directly and pass to in the listener callback, so
    we don't need to save and expose all the device info in the
    DeviceInfoUpdater.
    
    Bug: None
    Test: cca.py tsc brya
    Test: tast run <ip> camera.CCAUISmoke* camera.CCAUIStress*
    Change-Id: Ibf1f05d1c17f6ec41c1745e2f8ac2ddb46fd20ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063453
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080184}

--- ash/webui/camera_app_ui/resources/js/device/device_info_updater.ts
+++ ash/webui/camera_app_ui/resources/js/device/device_info_updater.ts
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assertExists} from '../assert.js';
 import {DeviceOperator} from '../mojo/device_operator.js';
-import {Camera3DeviceInfo} from './camera3_device_info.js';
+
 import {
   DeviceInfo,
   StreamManager,
 } from './stream_manager.js';
+import {CameraInfo} from './type.js';
+
+type DeviceChangeListener = (cameraInfo: CameraInfo) => void;
 
 /**
  * Contains information of all cameras on the device and will updates its value
@@ -19,19 +22,7 @@ export class DeviceInfoUpdater {
   /**
    * Listeners to be called after new camera information is available.
    */
-  private readonly deviceChangeListeners:
-      Array<(updater: DeviceInfoUpdater) => void> = [];
-
-  /**
-   * MediaDeviceInfo of all available video devices.
-   */
-  private devicesInfo: MediaDeviceInfo[] = [];
-
-  /**
-   * Camera3DeviceInfo of all available video devices. Is null on fake cameras
-   * which do not have private mojo API support.
-   */
-  private camera3DevicesInfo: Camera3DeviceInfo[]|null = null;
+  private readonly deviceChangeListeners: DeviceChangeListener[] = [];
 
   constructor() {
     StreamManager.getInstance().addRealDeviceChangeListener((devicesInfo) => {
@@ -45,36 +36,19 @@ export class DeviceInfoUpdater {
    * @param devicesInfo Updated devices info.
    */
   private update(devicesInfo: DeviceInfo[]) {
-    this.devicesInfo = devicesInfo.map((d) => d.v1Info);
-    if (DeviceOperator.isSupported()) {
-      this.camera3DevicesInfo = devicesInfo.map((d) => assertExists(d.v3Info));
-    } else {
-      this.camera3DevicesInfo = null;
-    }
+    const v1DevicesInfo = devicesInfo.map((d) => d.v1Info);
+    const camera3DevicesInfo = (DeviceOperator.isSupported()) ?
+        devicesInfo.map((d) => assertExists(d.v3Info)) :
+        null;
     for (const listener of this.deviceChangeListeners) {
-      listener(this);
+      listener(new CameraInfo(v1DevicesInfo, camera3DevicesInfo));
     }
   }
 
   /**
    * Registers listener to be called when state of available devices changes.
    */
-  addDeviceChangeListener(listener: (updater: DeviceInfoUpdater) => void):
-      void {
+  addDeviceChangeListener(listener: DeviceChangeListener): void {
     this.deviceChangeListeners.push(listener);
-  }
-
-  /**
-   * Gets MediaDeviceInfo for all available video devices.
-   */
-  getDevicesInfo(): MediaDeviceInfo[] {
-    return this.devicesInfo;
-  }
-
-  /**
-   * Gets Camera3DeviceInfo for all available video devices.
-   */
-  getCamera3DevicesInfo(): Camera3DeviceInfo[]|null {
-    return this.camera3DevicesInfo;
   }
 }
