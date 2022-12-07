f7cfcc455b82b1706f5338e7ade6802b586752e7
Olga Sharonova <olka@chromium.org>
2022-12-07 12:47:37 +0000

    Deleted Audio service connection duration UMA logging
    Metrics have not been monitored for a while.
    
    Bug: 1384869
    Change-Id: I73cde3bb7741c0389fb1dba9199c7bde2410ffb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085422
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Fredrik Hernqvist <fhernqvist@google.com>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080291}

--- services/audio/service.cc
+++ services/audio/service.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/audio/debug_recording.h"
 #include "services/audio/device_notifier.h"
 #include "services/audio/log_factory_manager.h"
-#include "services/audio/service_metrics.h"
 #include "services/audio/system_info.h"
 
 #if BUILDFLAG(IS_APPLE)
@@ -65,16 +64,11 @@ Service::Service(std::unique_ptr<AudioManagerAccessor> audio_manager_accessor,
   audio_manager_accessor_->GetAudioManager()->SetAecDumpRecordingManager(
       aecdump_recording_manager_->AsWeakPtr());
 #endif
-
-  metrics_ =
-      std::make_unique<ServiceMetrics>(base::DefaultTickClock::GetInstance());
 }
 
 Service::~Service() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   TRACE_EVENT0("audio", "audio::Service::~Service");
-
-  metrics_.reset();
 
   // Stop all streams cleanly before shutting down the audio manager.
   stream_factory_.reset();
--- services/audio/service.h
+++ services/audio/service.h
@@ -39,7 +39,6 @@ namespace audio {
 class DebugRecording;
 class DeviceNotifier;
 class LogFactoryManager;
-class ServiceMetrics;
 class SystemInfo;
 
 class Service final : public mojom::AudioService {
@@ -133,7 +132,6 @@ class Service final : public mojom::AudioService {
   absl::optional<StreamFactory> stream_factory_;
   std::unique_ptr<DeviceNotifier> device_notifier_;
   std::unique_ptr<LogFactoryManager> log_factory_manager_;
-  std::unique_ptr<ServiceMetrics> metrics_;
 };
 
 }  // namespace audio
