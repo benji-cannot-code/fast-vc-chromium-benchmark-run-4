730cc326d06ca45baca919018bf8e67ac496fa9f
Anna Tsvirchkova <atsvirchkova@google.com>
2022-12-07 09:59:55 +0000

    [TTFCC] Add a card icon and background for credit card items list
    This CL adds some styling for the credit card item - the icon and the background.
    See the screenshot: https://bugs.chromium.org/p/chromium/issues/detail?id=1247698#c33
    
    Bug: 1247698
    Change-Id: Ia27caf7fae37a805f6f21ef2694cd2378cc9d190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066648
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080256}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewBinder.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardViewBinder.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.touch_to_fill.payments;
 
 import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardProperties.CreditCardProperties.CARD_EXPIRATION;
+import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardProperties.CreditCardProperties.CARD_ICON_ID;
 import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardProperties.CreditCardProperties.CARD_NAME;
 import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardProperties.CreditCardProperties.CARD_NUMBER;
 import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCreditCardProperties.CreditCardProperties.ON_CLICK_ACTION;
@@ -19,7 +20,10 @@ import static org.chromium.chrome.browser.touch_to_fill.payments.TouchToFillCred
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.ui.modelutil.PropertyKey;
@@ -72,6 +76,13 @@ class TouchToFillCreditCardViewBinder {
 
     /** Binds the item view to the model properties. */
     static void bindCardItemView(PropertyModel model, View view, PropertyKey propertyKey) {
+        ImageView icon = view.findViewById(R.id.favicon);
+        int iconId = model.get(CARD_ICON_ID);
+        // Generally the resource id for the icon can only be zero in the tests.
+        // For production code a general card icon id is set by default in the CreditCard
+        // constructor if the card issuer is unknown.
+        icon.setImageDrawable(
+                iconId != 0 ? AppCompatResources.getDrawable(view.getContext(), iconId) : null);
         TextView cardName = view.findViewById(R.id.card_name);
         cardName.setText(model.get(CARD_NAME));
         TextView cardNumber = view.findViewById(R.id.card_number);
