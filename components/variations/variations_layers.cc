dc8f34a85f4ed800d646cf73e21b60a5a3e42fa7
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2022-12-07 20:14:23 +0000

    Revert "Add extra validation for layers."
    This reverts commit ed9541348cf31dd13590ee79490b6367af64ea4f.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8795502061372247937
    
    Sample failed build: https://ci.chromium.org/b/8795502061372247937
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8795502061372247937&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4081393
    
    Change-Id: Iefd00bf47a14399098b4b39357cc55dbb2641172
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4084142
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080517}

--- components/variations/variations_layers.cc
+++ components/variations/variations_layers.cc
@@ -25,8 +25,7 @@ enum class InvalidLayerReason {
   kInvalidEntropyMode = 3,
   kSlotsDoNotDivideLowEntropyDomain = 4,
   kInvalidSlotBounds = 5,
-  kUnknownFields = 6,
-  kMaxValue = kUnknownFields,
+  kMaxValue = kInvalidSlotBounds,
 };
 
 void LogInvalidLayerReason(InvalidLayerReason reason) {
@@ -137,17 +136,8 @@ NormalizedMurmurHashEntropyProvider ComputeRemainderEntropy(
 }
 
 bool ValidSlotBounds(const Layer& layer_proto) {
-  uint32_t next_slot_after_processed_ranges = 0;
-  // Since num_slots divides LES range, we know it is small enough that
-  // num_slots + 1 does not overflow.
-  DCHECK_LE(layer_proto.num_slots(), layer_proto.num_slots() + 1);
   for (const auto& member : layer_proto.members()) {
     for (const auto& range : member.slots()) {
-      // Ranges should be non-overlapping. We also require them to be in
-      // increasing order so that we can validate that easily.
-      if (range.start() < next_slot_after_processed_ranges) {
-        return false;
-      }
       // start and end are both unsigned (uint32_t) so no need to check that
       // they are non-negative.
       if (range.end() >= layer_proto.num_slots())
@@ -155,7 +145,6 @@ bool ValidSlotBounds(const Layer& layer_proto) {
       if (range.start() > range.end()) {
         return false;
       }
-      next_slot_after_processed_ranges = range.end() + 1;
     }
   }
   return true;
@@ -182,10 +171,6 @@ VariationsLayers::~VariationsLayers() = default;
 
 void VariationsLayers::ConstructLayer(const EntropyProviders& entropy_providers,
                                       const Layer& layer_proto) {
-  if (!layer_proto.unknown_fields().empty()) {
-    LogInvalidLayerReason(InvalidLayerReason::kUnknownFields);
-    return;
-  }
   if (layer_proto.id() == 0) {
     LogInvalidLayerReason(InvalidLayerReason::kInvalidId);
     return;
@@ -205,6 +190,11 @@ void VariationsLayers::ConstructLayer(const EntropyProviders& entropy_providers,
     return;
   }
 
+  if (!ValidSlotBounds(layer_proto)) {
+    LogInvalidLayerReason(InvalidLayerReason::kInvalidSlotBounds);
+    return;
+  }
+
   // Using the size of the domain as the output range maximizes the number of
   // possible pseudorandom outputs when using the low entropy source.
   size_t range = entropy_providers.low_entropy_domain();
@@ -215,18 +205,11 @@ void VariationsLayers::ConstructLayer(const EntropyProviders& entropy_providers,
         InvalidLayerReason::kSlotsDoNotDivideLowEntropyDomain);
     return;
   }
-
-  if (!ValidSlotBounds(layer_proto)) {
-    LogInvalidLayerReason(InvalidLayerReason::kInvalidSlotBounds);
-    return;
-  }
-
   const auto& entropy_provider = (layer_proto.entropy_mode() != Layer::LOW)
                                      ? entropy_providers.default_entropy()
                                      : entropy_providers.low_entropy();
-  uint32_t salt = layer_proto.salt() || layer_proto.id();
   ValueInRange pseudorandom = {
-      .value = entropy_provider.GetPseudorandomValue(salt, range),
+      .value = entropy_provider.GetPseudorandomValue(layer_proto.salt(), range),
       .range = static_cast<uint32_t>(range),
   };
   SlotSelection selection = SelectSlot(pseudorandom, layer_proto.num_slots());
