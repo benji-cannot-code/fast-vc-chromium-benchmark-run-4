70cb31aa226c233575fd9e1f5671ccd6fa292529
liqining.keiling <liqining.keeling@bytedance.com>
2022-12-08 03:03:01 +0000

    AppHome: Implements basic action menu in app home page.
    This CL implements basic action menu which shows app management items.
    Currently, it only has UI that shows the management items and doesn't
    have handles for each menu item.
    
    For screenshot of action menu, see:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1350406#c16.
    
    For background & design document, see following document:
     https://docs.google.com/document/d/1I_Ot2Wuyyztt1YIE61rpkeXewaMnPzfMNF-6qCbdtVw/edit#heading=h.q3ma6p1wdg55
    
    Bug: 1350406
    Change-Id: I3915eb917d7fbb4261afab9b694753b45286a0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054767
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1080752}

--- chrome/test/data/webui/app_home/app_list_test.ts
+++ chrome/test/data/webui/app_home/app_list_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://apps/app_list.js';
+import 'chrome://apps/app_item.js';
 
 import {AppInfo, PageRemote} from 'chrome://apps/app_home.mojom-webui.js';
 import {AppListElement} from 'chrome://apps/app_list.js';
@@ -80,13 +81,8 @@ suite('AppListTest', () => {
   test('app list present', () => {
     assertTrue(!!appListElement);
 
-    assertTrue(!!appListElement.shadowRoot!.querySelector('.icon-image'));
-
-    assertTrue(!!appListElement.shadowRoot!.querySelector('.text-container'));
-
     const elementLength =
-        appListElement.shadowRoot!.querySelectorAll('.element-container')
-            .length;
+        appListElement.shadowRoot!.querySelectorAll('app-item').length;
     assertEquals(apps.appList.length, elementLength);
   });
 
@@ -95,18 +91,22 @@ suite('AppListTest', () => {
     callbackRouterRemote.addApp(testAppInfo);
     await callbackRouterRemote.$.flushForTesting();
     flush();
-    let appNameList = Array.from(
-        appListElement.shadowRoot!.querySelectorAll('.text-container'));
-    assertTrue(!!appNameList.find(
-        appName => appName.textContent === testAppInfo.name));
+    let appItemList =
+        Array.from(appListElement.shadowRoot!.querySelectorAll('app-item'));
+    assertTrue(!!appItemList.find(
+        appItem => appItem.shadowRoot!.querySelector(
+                                          '.text-container')!.textContent ===
+            testAppInfo.name));
 
     // Test removing an app
     callbackRouterRemote.removeApp(testAppInfo);
     await callbackRouterRemote.$.flushForTesting();
     flush();
-    appNameList = Array.from(
-        appListElement.shadowRoot!.querySelectorAll('.text-container'));
-    assertFalse(!!appNameList.find(
-        appName => appName.textContent === testAppInfo.name));
+    appItemList =
+        Array.from(appListElement.shadowRoot!.querySelectorAll('app-item'));
+    assertFalse(!!appItemList.find(
+        appItem => appItem.shadowRoot!.querySelector(
+                                          '.text-container')!.textContent ===
+            testAppInfo.name));
   });
 });
