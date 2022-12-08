6c7c47833f162c25dc8172145e32e0e65b564005
Qi Tiezheng <qitiezheng@360.cn>
2022-12-08 20:46:27 +0000

    [chrome] Fix some cpplint errors in chrome part8
    chrome/browser/extensions/component_loader.cc:56:
      "ash/constants/ash_pref_names.h" already included at chrome/browser/extensions/component_loader.cc:10  [build/include] [4]
    chrome/browser/extensions/external_provider_impl.cc:73:
      "extensions/common/constants.h" already included at chrome/browser/extensions/external_provider_impl.cc:51  [build/include] [4]
    chrome/browser/extensions/menu_manager_unittest.cc:20:
      "chrome/browser/extensions/menu_manager.h" already included at chrome/browser/extensions/menu_manager_unittest.cc:5  [build/include] [4]
    
    Bug: None
    Change-Id: I76f8742f19b3cf1dd0708f5d4ae629d03db4b804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3826500
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Qi Tiezheng <qitiezheng@360.cn>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081094}

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "ash/constants/ash_pref_names.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/callback_helpers.h"
--- chrome/browser/extensions/menu_manager_unittest.cc
+++ chrome/browser/extensions/menu_manager_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
-#include "chrome/browser/extensions/menu_manager.h"
 #include "chrome/browser/extensions/menu_manager_test_observer.h"
 #include "chrome/browser/extensions/test_extension_prefs.h"
 #include "chrome/browser/extensions/test_extension_system.h"
