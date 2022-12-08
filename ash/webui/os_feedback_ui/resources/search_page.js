5212f3d7630ac68dcdbcd1ddd99927bab341979c
xiangdong kong <xiangdongkong@google.com>
2022-12-08 19:49:37 +0000

    feedback: show popular help content when none help content returned
    Help content in different languages will be filtered out. We fetch 10
    extra items hoping to still return some items in the same language. We
    choose 10 because the search api biases on the language specified.
    
    In certain cases when words entered exist in different languages with
    the same spelling, there will be zero items returned after filtering,
    although there were matched contents. In this case, we shall display
    the popular help content [2], not leave the section blank [1].
    
    Screenshots:
    
    1. before: http://screen/ARbTXkwmUnLLMNU.png.
    2. after: http://screen/3iTyzEy92NqxHmL.png.
    
    Bug: b:185624798, b:261625349
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.*
    Change-Id: I5cbf6c958031fc97d99ceaf62f07e6286cb3f5bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087070
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081052}

--- ash/webui/os_feedback_ui/resources/search_page.js
+++ ash/webui/os_feedback_ui/resources/search_page.js
@@ -286,7 +286,7 @@ export class SearchPageElement extends SearchPageElementBase {
       isPopularContent = true;
     } else {
       response = await this.helpContentProvider_.getHelpContents(request);
-      isPopularContent = (response.response.totalResults === 0);
+      isPopularContent = (response.response.results.length === 0);
       this.helpContentSearchResultCount = response.response.results.length;
     }
 
