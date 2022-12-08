f250ceea8037d243ebe889e8c0075bb8cc8f1d88
Joseph Koshy <jkoshy@chromium.org>
2022-12-08 14:44:01 +0000

    Minor: format JSON strings to be human-readable.
    Tested: locally tested.
    Bug: 1395098
    Change-Id: I7eb050e8116885303ec7fa8d2875f0965ba3cf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4070965
    Reviewed-by: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Joseph Koshy <jkoshy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080911}

--- tools/perf/contrib/power/perf_profile.py
+++ tools/perf/contrib/power/perf_profile.py
@@ -59,7 +59,10 @@ class ContribPowerPerfProfile(_PowerPerfBenchmarkBase):
           config {{
               name: "org.chromium.trace_metadata"
               chrome_config {{
-                  trace_config: "{{\\"record_mode\\":\\"record-until-full\\", \\"excluded_categories\\":[\\"*\\"]}}"
+                  trace_config: "{{
+                    \\"record_mode\\": \\"record-until-full\\",
+                    \\"excluded_categories\\": [ \\"*\\" ]
+                  }}"
                   client_priority: USER_INITIATED
               }}
           }}
@@ -68,7 +71,10 @@ class ContribPowerPerfProfile(_PowerPerfBenchmarkBase):
           config {{
               name: "org.chromium.trace_event"
               chrome_config {{
-                  trace_config: "{{\\"record_mode\\":\\"record-until-full\\", \\"excluded_categories\\":[\\"*\\"]}}"
+                  trace_config: "{{
+                    \\"record_mode\\": \\"record-until-full\\",
+                    \\"excluded_categories\\": [ \\"*\\" ]
+                  }}"
                   client_priority: USER_INITIATED
               }}
           }}
