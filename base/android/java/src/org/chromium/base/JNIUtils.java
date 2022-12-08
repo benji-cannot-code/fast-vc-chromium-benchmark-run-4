fcffdf053f797e5b53a057ac41713131053817ef
Andrew Grieve <agrieve@chromium.org>
2022-12-08 22:07:32 +0000

    Android: Try re-fetching ApplicationInfo when splits are not installed
    I can no longer repro a crash on my Pixel 6, but the assert I've added
    is still being hit in the wild. This is another random attempt at
    addressing it.
    
    Bug: 1395191
    Change-Id: Ia774105270630e4ead545a80764737ed6c623e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4089506
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081140}

--- base/android/java/src/org/chromium/base/JNIUtils.java
+++ base/android/java/src/org/chromium/base/JNIUtils.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
+import android.content.pm.ApplicationInfo;
 import android.os.Build;
 
 import org.chromium.base.annotations.CalledByNative;
@@ -31,14 +32,30 @@ public class JNIUtils {
         if (!splitName.isEmpty()) {
             boolean isInstalled = BundleUtils.isIsolatedSplitInstalled(splitName);
             Log.i(TAG, "Init JNI Classloader for %s. isInstalled=%b", splitName, isInstalled);
-            if (isInstalled) {
-                return BundleUtils.getOrCreateSplitClassLoader(splitName);
-            } else {
-                assert (!BundleUtils.isBundle()
-                        || Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+
+            if (!isInstalled && BundleUtils.isBundle()
+                    && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                // Address race condition on global ApplicationInfo being updated.
+                // https://crbug.com/1395191
+                ApplicationInfo globalAppInfo =
+                        ContextUtils.getApplicationContext().getApplicationInfo();
+                ApplicationInfo freshAppInfo =
+                        PackageUtils.getApplicationPackageInfo(0).applicationInfo;
+                globalAppInfo.splitNames = freshAppInfo.splitNames;
+                globalAppInfo.splitSourceDirs = freshAppInfo.splitSourceDirs;
+                globalAppInfo.splitPublicSourceDirs = freshAppInfo.splitPublicSourceDirs;
+
+                isInstalled = BundleUtils.isIsolatedSplitInstalled(splitName);
+                Log.i(TAG, "Init JNI Classloader for %s. isInstalled=%b", splitName, isInstalled);
+                assert isInstalled
                     : "Should not hit splitcompat mode for Android T+. You might have a "
                       + "generate_jni() target that declares split_name, but includes "
                       + "a .java file from the base split (https://crbug.com/1394148).";
+            }
+
+            if (isInstalled) {
+                return BundleUtils.getOrCreateSplitClassLoader(splitName);
+            } else {
                 // Split was installed by PlayCore in "compat" mode, meaning that our base module's
                 // ClassLoader was patched to add the splits' dex file to it.
             }
