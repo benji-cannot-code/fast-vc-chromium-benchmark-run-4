1303ffc46db577d06b74e159b21c703b4605a196
Kalvin Lee <kdlee@chromium.org>
2022-12-08 06:27:36 +0000

    [PA] Legitimize facade headers
    Following feedback, this CL marks the `raw_ptr` and `raw_ref` headers in
    `//base/memory` as semipermanent fixtures. Developers can continue to
    refer to the shorter path for convenience.
    
    Bug: 1371729
    Change-Id: I3a07a31e66d56c6639ff7775ebbed355ae3dc883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087804
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080799}

--- base/memory/raw_ref.h
+++ base/memory/raw_ref.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MEMORY_RAW_REF_H_
 #define BASE_MEMORY_RAW_REF_H_
 
-// Facade header to allow us time to slowly rewrite `raw_ref` users
-// to refer to the new location.
-#include "base/allocator/partition_allocator/pointers/raw_ref.h"
+// Although `raw_ref` is part of the standalone PA distribution, it is
+// easier to use the shorter path in `//base/memory`. We retain this
+// facade header for ease of typing.
+#include "base/allocator/partition_allocator/pointers/raw_ref.h"  // IWYU pragma: export
 
 #endif  // BASE_MEMORY_RAW_REF_H_
