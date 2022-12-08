1c907925c9b745fd910da991e1eb1813d4f3f4c0
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-12-08 23:32:33 +0000

    [android] Enable feed personalization for signed-in users by default
    Enable PersonalizeFeedNonSyncUsers and
    SyncAndroidPromosWithAlternativeTitle.
    
    Fixed: 1297981, 1205923
    Change-Id: Icb90e73c7e5a14ab5e74b4256398a896368f85fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4088525
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081202}

--- components/feed/feed_feature_list.cc
+++ components/feed/feed_feature_list.cc
@@ -134,7 +134,7 @@ BASE_FEATURE(kPersonalizeFeedUnsignedUsers,
 
 BASE_FEATURE(kPersonalizeFeedNonSyncUsers,
              "PersonalizeFeedNonSyncUsers",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 signin::ConsentLevel GetConsentLevelNeededForPersonalizedFeed() {
   if (!base::FeatureList::IsEnabled(kPersonalizeFeedNonSyncUsers))
