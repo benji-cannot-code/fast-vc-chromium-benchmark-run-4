aa1df8cb476eb6fec8587f98c0e3d1d915940d43
Sam Cackett <samcackett@google.com>
2022-12-08 19:45:55 +0000

    Expose calendar jelly feature flags
    Bug: b:260721777
    Change-Id: I564ef69136751f276ff04c9463a3871a5cc47c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086148
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081047}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2808,6 +2808,9 @@ extern const char kBluetoothUseFlossDescription[];
 extern const char kBluetoothUseLLPrivacyName[];
 extern const char kBluetoothUseLLPrivacyDescription[];
 
+extern const char kCalendarJellyName[];
+extern const char kCalendarJellyDescription[];
+
 extern const char kCalendarViewName[];
 extern const char kCalendarViewDescription[];
 
