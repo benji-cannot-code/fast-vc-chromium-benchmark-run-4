49f350bc49c19a5473fc50b7e993a37125d546d9
Jordan Bayles <jophba@chromium.org>
2022-12-08 22:14:40 +0000

    [Blink Audio] Cleanup AudioRendererSinkCache
    This class is not used in practice like originally written, and is
    instead a temporary cache of output device info. This patch updates this
    class' implementation to match usage, to simplify upcoming refactors.
    
    Bug: 151051, 1224504
    Change-Id: I84b726f01d5c1071668133eb6bc7289cc294fd8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4077311
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081149}

--- third_party/blink/renderer/modules/media/audio/audio_renderer_sink_cache.h
+++ third_party/blink/renderer/modules/media/audio/audio_renderer_sink_cache.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/synchronization/lock.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/thread_annotations.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "media/audio/audio_sink_parameters.h"
@@ -29,9 +30,9 @@ namespace blink {
 
 class LocalDOMWindow;
 
-// Caches AudioRendererSink instances, provides them to the clients for usage,
-// tracks their used/unused state, reuses them to obtain output device
-// information, garbage-collects unused sinks.
+// Creates temporary audio sinks in order to acquire OutputDeviceInfo from them.
+// These sinks live for a total time of |delete_timeout| to allow for multiple
+// queries without reconstructing the temporary sink, and are then deleted.
 // Must live on the main render thread. Thread safe.
 class MODULES_EXPORT AudioRendererSinkCache {
  public:
@@ -47,7 +48,7 @@ class MODULES_EXPORT AudioRendererSinkCache {
   // navigation.
   static void InstallWindowObserver(LocalDOMWindow&);
 
-  // |cleanup_task_runner| will be used to delete sinks when they are unused,
+  // |cleanup_task_runner| will be used to delete sinks.
   // AudioRendererSinkCache must outlive any tasks posted to it. Since
   // the sink cache is normally a process-wide singleton, this isn't a problem.
   AudioRendererSinkCache(
@@ -60,14 +61,9 @@ class MODULES_EXPORT AudioRendererSinkCache {
 
   ~AudioRendererSinkCache();
 
-  // AudioRendererSinkCache implementation:
   media::OutputDeviceInfo GetSinkInfo(const LocalFrameToken& source_frame_token,
                                       const base::UnguessableToken& session_id,
                                       const std::string& device_id);
-  scoped_refptr<media::AudioRendererSink> GetSink(
-      const LocalFrameToken& source_frame_token,
-      const std::string& device_id);
-  void ReleaseSink(const media::AudioRendererSink* sink_ptr);
 
  private:
   friend class AudioRendererSinkCacheTest;
@@ -79,22 +75,18 @@ class MODULES_EXPORT AudioRendererSinkCache {
 
   // Schedules a sink for deletion. Deletion will be performed on the same
   // thread the cache is created on.
-  void DeleteLaterIfUnused(scoped_refptr<media::AudioRendererSink> sink);
+  void DeleteLater(scoped_refptr<media::AudioRendererSink> sink);
 
-  // Deletes a sink from the cache. If |force_delete_used| is set, a sink being
-  // deleted can (and should) be in use at the moment of deletion; otherwise the
-  // sink is deleted only if unused.
-  void DeleteSink(const media::AudioRendererSink* sink_ptr,
-                  bool force_delete_used);
+  // Deletes a sink from the cache.
+  void DeleteSink(const media::AudioRendererSink* sink_ptr);
 
   CacheContainer::iterator FindCacheEntry_Locked(
       const LocalFrameToken& source_frame_token,
-      const std::string& device_id,
-      bool unused_only);
+      const std::string& device_id);
 
-  void CacheOrStopUnusedSink(const LocalFrameToken& source_frame_token,
-                             const std::string& device_id,
-                             scoped_refptr<media::AudioRendererSink> sink);
+  void MaybeCacheSink(const LocalFrameToken& source_frame_token,
+                      const std::string& device_id,
+                      scoped_refptr<media::AudioRendererSink> sink);
 
   void DropSinksForFrame(const LocalFrameToken& source_frame_token);
 
@@ -111,16 +103,11 @@ class MODULES_EXPORT AudioRendererSinkCache {
   const CreateSinkCallback create_sink_cb_;
 
   // Cached sink deletion timeout.
-  // For example: (1) sink was created and cached in GetSinkInfo(), and then (2)
-  // the same sink is requested in GetSink(), if time interval between (1) and
-  // (2) is less than |kDeleteTimeoutMs|, then sink cached in (1) is reused in
-  // (2). On the other hand, if after (1) nobody is interested in the sink
-  // within |kDeleteTimeoutMs|, it is garbage-collected.
   const base::TimeDelta delete_timeout_;
 
   // Cached sinks, protected by lock.
   base::Lock cache_lock_;
-  CacheContainer cache_;
+  CacheContainer cache_ GUARDED_BY(cache_lock_);
 };
 
 }  // namespace blink
