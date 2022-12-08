4d8a6151e29cb321e57d8e07e15e8181cbef73ab
Filipa Senra <fsenra@google.com>
2022-12-08 14:04:48 +0000

    [Android][Privacy Guide] Add metrics for MSBB's next button of PG
    This CL adds two metrics "Settings.PrivacyGuide.SettingsStates" and
    "Settings.PrivacyGuide.NextClickMSBB" to record the click on the next
    button in the MSBB card of Privacy Guide.
    We create a new class, PrivacyGuideMetricsDelegate, that is responsible for emitting the metrics on Privacy Guide.
    We also create another new class, PrivacyGuideUtils, to deal with the fetching the states of the cards of Privacy Guide.
    We also move the FragmentType enum from PrivacyGuidePagerAdapter to PrivacyGuideFragment: the fragmentType is used in both the PagerAdapter and the MetricsDelegate.
    
    Bug: 1238896
    Change-Id: I74734a92fd54ea4aa5af7cee48e775783c711f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4042183
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080901}

---
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PrivacyGuideUtils.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_guide;
+
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.signin.services.UnifiedConsentServiceBridge;
+
+/**
+ * A utility class for Privacy Guide that fetches the current state of {@link
+ * PrivacyGuideFragment.FragmentType}s.
+ *
+ * TODO(crbug.com/1393960): Utilize the methods on this class on the PG fragments
+ */
+class PrivacyGuideUtils {
+    public static boolean isMsbbEnabled() {
+        return UnifiedConsentServiceBridge.isUrlKeyedAnonymizedDataCollectionEnabled(
+                Profile.getLastUsedRegularProfile());
+    }
+}
