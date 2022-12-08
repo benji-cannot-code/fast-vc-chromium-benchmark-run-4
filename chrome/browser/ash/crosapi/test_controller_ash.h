f71bc4b4ac6ff110a2470e4e1a63ada2ee6230cc
Jenny Zhang <jennyz@chromium.org>
2022-12-08 02:09:08 +0000

    Implement tts.speak for speaking ash utterance with lacros voice.
    This cl includes the following change:
    
    1. A minor refactor of tts_crosapi_util::CreateUtteranceFromMojo()
    so that it can be used to create TtsUtterance for both Lacros
    and Ash utterances.
    
    2. To speak an Ash Utterance with a Lacros speech engine, TtsAsh::SpeakOrEnqueue calls crosapi to pass speech request
    to the remote speech engine in Lacros.
    
    3. TtsClientLacros::SpeakWithLacrosVoice will create a TtsUtterance
    for the Ash utterance in Lacros, sent to the specified speech engine
    , and pass the TtsEvent generated by the speech engine back to Ash.
    
    4. When the Ash utterance becomes invalid in Ash (either being stopped
    by tts.Stop or deleted when its associated WebContents is destroyed),
    Lacros will be notified to request the speech engine stop speaking
    and clean its pending instance in TtsClientLacros.
    
    5. Add a lacros browser test to test speaking an Ash utterance
    with a Lacros voice. The test loads a speech engine in Lacros, and
    calls TestController crosapi to request Ash TtsController to issue
    to speak an Ash utterance with a voice provided by Lacros testing
    speech engine.
    
    Bug: 1227543
    Change-Id: I226e8e6ac09d3090b2149a1518673214a31176f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081607
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080718}

--- chrome/browser/ash/crosapi/test_controller_ash.h
+++ chrome/browser/ash/crosapi/test_controller_ash.h
@@ -110,6 +110,12 @@ class TestControllerAsh : public mojom::TestController,
   void GetTtsUtteranceQueueSize(
       GetTtsUtteranceQueueSizeCallback callback) override;
 
+  void GetTtsVoices(GetTtsVoicesCallback callback) override;
+
+  void TtsSpeak(crosapi::mojom::TtsUtterancePtr mojo_utterance,
+                mojo::PendingRemote<crosapi::mojom::TtsUtteranceClient>
+                    utterance_client) override;
+
   mojo::Remote<mojom::StandaloneBrowserTestController>&
   GetStandaloneBrowserTestController() {
     DCHECK(standalone_browser_test_controller_.is_bound());
@@ -124,6 +130,10 @@ class TestControllerAsh : public mojom::TestController,
 
  private:
   class OverviewWaiter;
+  class AshUtteranceEventDelegate;
+
+  // Called when a Tts utterance is finished.
+  void OnAshUtteranceFinished(int utterance_id);
 
   // Called when a waiter has finished waiting for its event.
   void WaiterFinished(OverviewWaiter* waiter);
@@ -158,6 +168,10 @@ class TestControllerAsh : public mojom::TestController,
       standalone_browser_test_controller_;
 
   base::OneShotEvent on_standalone_browser_test_controller_bound_;
+
+  // Ash utterance event delegates by utterance id.
+  std::map<int, std::unique_ptr<AshUtteranceEventDelegate>>
+      ash_utterance_event_delegates_;
 };
 
 class TestShillControllerAsh : public crosapi::mojom::TestShillController {
