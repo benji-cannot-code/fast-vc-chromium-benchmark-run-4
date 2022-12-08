211c7e1e9f7051b9d5442e15a0ec6aac70833da7
Yohanes Shimelis <yohanes@google.com>
2022-12-08 20:09:20 +0000

    [omnibox][ui-simplification] Reduce rich suggestion images to 28px.
    * When the uniform row height feature is enabled, entity suggestion
      images are now 28px (rather than 32px) tall, and all suggestions are
      uniform in height (36px or 40px, depending on which feature param
      is used.)
    * Row height is now specified in field trial params and is used to
      determine heights directly when the uniform row height feature is
      enabled.
    * When displaying an answer or rich entity suggestion, the margins are
      reduced to prevent clipping.
    * Suggestion button row no longer has its bottom margin changed when the
      uniform row height feature is enabled.
    
    Bug: 1368852
    Change-Id: Ia350543520df7f827fc712849a6134447f0305b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032718
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Yohanes Shimelis <yohanes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081065}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/omnibox.mojom-shared.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_popup_selection.h"
 #include "components/omnibox/browser/vector_icons.h"
 #include "components/strings/grit/components_strings.h"
@@ -265,7 +266,8 @@ void OmniboxResultView::SetMatch(const AutocompleteMatch& match) {
     // calculator answers are 2-line but not deemphasized.
     const bool deemphasize =
         match_.type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY &&
-        OmniboxMatchCellView::ShouldDisplayImage(match_);
+        OmniboxMatchCellView::ShouldDisplayImage(match_) &&
+        !OmniboxFieldTrial::IsUniformRowHeightEnabled();
     suggestion_view_->description()->SetTextWithStyling(
         match_.description, match_.description_class, deemphasize);
   }
