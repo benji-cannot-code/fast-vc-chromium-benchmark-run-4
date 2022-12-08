4a548b11ca6a186e48a9c8a12e7096e39d29e837
David Padlipsky <dpad@google.com>
2022-12-08 20:05:03 +0000

    Diagnostics: Add 'tr.f' region for Turkish keyboards with F-Type layout
    Turkish keyboards come in 2 forms, Q-Type and F-Type. Both types are
    considered a part of the 'tr' region so there is no way to definitively
    know which of these two layouts a user is using. This implements a best
    effort guess. If the region is 'tr' and the user is using the F-Type
    xkb layout, we will assume their keyboard is F-Type and tell the
    diagnostics app to render an F-Type layout keyboard.
    
    F-Type Screenshot: https://screenshot.googleplex.com/Pge4os5xyAmtb5u
    Q-Type Screenshot: https://screenshot.googleplex.com/A6uaiExhCAvDpLP
    
    F-Type Spec: go/ikbd-fkey-spec
    Q-Type Spec: go/ikbd-qkey-spec
    
    Bug: b/221920512
    Test: See screenshots + ash_webui_unittests
    Change-Id: I1dfccb494b3ace0c4ad52629ea7463bc324e22d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086791
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081063}

--- ash/webui/diagnostics_ui/backend/input/input_data_provider_keyboard_unittest.cc
+++ ash/webui/diagnostics_ui/backend/input/input_data_provider_keyboard_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/constants/ash_switches.h"
+#include "ash/ime/ime_controller_impl.h"
+#include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/webui/diagnostics_ui/backend/input/input_data_provider.h"
 #include "ash/webui/diagnostics_ui/backend/input/input_data_provider_keyboard.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/diagnostics_ui/mojom/input_data_provider.mojom-shared.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/strings/string_piece_forward.h"
 #include "chromeos/ash/components/test/ash_test_suite.h"
 #include "chromeos/system/fake_statistics_provider.h"
 #include "content/public/test/browser_task_environment.h"
@@ -61,6 +64,12 @@ enum VivaldiTopRowScanCode {
   kRefresh = 0xE7,
 };
 
+// xkb layout ids for both turkish Q-Type and F-Type layouts.
+constexpr base::StringPiece kTurkishKeyboardLayoutId = "xkb:tr::tur";
+constexpr base::StringPiece kTurkishKeyboardFLayoutId = "xkb:tr:f:tur";
+constexpr base::StringPiece kTurkeyRegionCode = "tr";
+constexpr base::StringPiece kTurkeyFLayoutRegionCode = "tr.f";
+
 ui::InputDevice InputDeviceFromCapabilities(
     int device_id,
     const ui::DeviceCapabilities& capabilities) {
@@ -89,6 +98,17 @@ ui::EventRewriterChromeOS::MutableKeyState MakeMutableKeyStateForFKey(
       ui::DomKey(ui::DomKey::F1 + diff), key_code);
 }
 
+void SetCurrentImeId(const std::string& current_ime_id) {
+  // Only data relavent is the ime id.
+  ImeInfo ime_info;
+  ime_info.id = current_ime_id;
+  ime_info.name = u"English";
+  ime_info.short_name = u"US";
+  ime_info.third_party = false;
+
+  Shell::Get()->ime_controller()->RefreshIme(
+      current_ime_id, /*available_imes=*/{ime_info}, /*menu_items=*/{});
+}
 }  // namespace
 
 class InputDataProviderKeyboardTest : public ash::AshTestBase {
@@ -289,6 +309,28 @@ TEST_F(VivaldiKeyboardTestBase, ScanCodeIndexesWithZeroScanCodes) {
   EXPECT_EQ(2u, aux_data_.top_row_key_scancode_indexes[kFullscreen]);
 
   EXPECT_EQ(top_row_keys_, keyboard_info_->top_row_keys);
+}
+
+TEST_F(VivaldiKeyboardTestBase, TurkishNoFLayout) {
+  SetCurrentImeId(std::string(kTurkishKeyboardLayoutId));
+  statistics_provider_.SetMachineStatistic(chromeos::system::kRegionKey,
+                                           std::string(kTurkeyRegionCode));
+
+  keyboard_info_ = input_data_provider_keyboard_->ConstructKeyboard(
+      &device_information, &aux_data_);
+
+  EXPECT_EQ(kTurkeyRegionCode, keyboard_info_->region_code);
+}
+
+TEST_F(VivaldiKeyboardTestBase, TurkishFLayout) {
+  SetCurrentImeId(std::string(kTurkishKeyboardFLayoutId));
+  statistics_provider_.SetMachineStatistic(chromeos::system::kRegionKey,
+                                           std::string(kTurkeyRegionCode));
+
+  keyboard_info_ = input_data_provider_keyboard_->ConstructKeyboard(
+      &device_information, &aux_data_);
+
+  EXPECT_EQ(kTurkeyFLayoutRegionCode, keyboard_info_->region_code);
 }
 
 class MechanicalLayoutTest
