1fdd69fd5b6a8221532ca1c47a1faf940dfa2b81
Jiaming Cheng <jiamingc@chromium.org>
2022-12-08 02:16:40 +0000

    Use view ids to find views in status_area_widget_delegate
    This cl assigned view ids to the notification tray and the date tray.
    Refactored the code in the StatusAreaWidgetDelegate to use view ids
    to find the views instead of using the class name.
    
    Bug: b/251739510
    Change-Id: I04f5146b490ee144676d274a35cc2d70f7272fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083738
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080723}

--- ash/system/notification_center/notification_center_tray.cc
+++ ash/system/notification_center/notification_center_tray.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/constants/tray_background_view_catalog.h"
+#include "ash/public/cpp/ash_view_ids.h"
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/shelf/shelf.h"
 #include "ash/system/notification_center/notification_center_bubble.h"
@@ -28,6 +29,7 @@ NotificationCenterTray::NotificationCenterTray(Shelf* shelf)
                          RoundedCornerBehavior::kStartRounded),
       notification_icons_controller_(
           std::make_unique<NotificationIconsController>(shelf)) {
+  SetID(VIEW_ID_SA_NOTIFICATION_TRAY);
   SetLayoutManager(std::make_unique<views::FlexLayout>());
   set_use_bounce_in_animation(false);
 
--- ash/system/unified/date_tray.cc
+++ ash/system/unified/date_tray.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/date_tray.h"
 
 #include "ash/constants/tray_background_view_catalog.h"
+#include "ash/public/cpp/ash_view_ids.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/clock_model.h"
@@ -29,6 +30,7 @@ DateTray::DateTray(Shelf* shelf, UnifiedSystemTray* tray)
       time_view_(tray_container()->AddChildView(
           std::make_unique<TimeTrayItemView>(shelf, TimeView::Type::kDate))),
       unified_system_tray_(tray) {
+  SetID(VIEW_ID_SA_DATE_TRAY);
   SetPressedCallback(
       base::BindRepeating(&DateTray::OnButtonPressed, base::Unretained(this)));
 
