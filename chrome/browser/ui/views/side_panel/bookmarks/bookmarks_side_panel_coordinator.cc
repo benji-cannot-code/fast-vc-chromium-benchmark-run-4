33a33c371466da1efcaf3532f5281b974afb7c7e
Kunihiko Sakamoto <ksakamoto@chromium.org>
2022-12-08 04:25:07 +0000

    Revert "Adding interactive browser tests for Unified SidePanel"
    This reverts commit 1c9bb4cc73781b5f1e360b8738040efe577431b4.
    
    Reason for revert: SidePanelInteractiveTest is consistently failing
    on Mac10.13 Tests bot. https://crbug.com/1399386
    
    
    Original change's description:
    > Adding interactive browser tests for Unified SidePanel
    >
    > Bug: None
    > Change-Id: I6ddc7624f7a9f9f0fe782e001ee540d767682dda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064829
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1080656}
    
    Bug: 1399386
    Change-Id: I9187b5c1aba7fea40f9fd2fa49c2b9d1248980de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085171
    Owners-Override: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1080771}

--- chrome/browser/ui/views/side_panel/bookmarks/bookmarks_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/bookmarks/bookmarks_side_panel_coordinator.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/views/vector_icons.h"
-#include "ui/views/view_class_properties.h"
 
 BookmarksSidePanelCoordinator::BookmarksSidePanelCoordinator(Browser* browser)
     : BrowserUserData<BookmarksSidePanelCoordinator>(*browser) {}
@@ -49,8 +47,6 @@ BookmarksSidePanelCoordinator::CreateBookmarksWebView() {
               GetBrowser().profile(), IDS_BOOKMARK_MANAGER_TITLE,
               /*webui_resizes_host=*/false,
               /*esc_closes_ui=*/false));
-  bookmarks_web_view->SetProperty(views::kElementIdentifierKey,
-                                  kBookmarkSidePanelWebViewElementId);
   extensions::BookmarkManagerPrivateDragEventRouter::CreateForWebContents(
       bookmarks_web_view.get()->contents_wrapper()->web_contents());
   return bookmarks_web_view;
--- chrome/browser/ui/views/user_education/browser_user_education_service.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service.cc
@@ -220,7 +220,7 @@ void MaybeRegisterChromeFeaturePromos(
   // kIPHReadingListDiscoveryFeature:
   registry.RegisterFeature(FeaturePromoSpecification::CreateForLegacyPromo(
       &feature_engagement::kIPHReadingListDiscoveryFeature,
-      kSidePanelButtonElementId, IDS_READING_LIST_DISCOVERY_PROMO));
+      kReadLaterButtonElementId, IDS_READING_LIST_DISCOVERY_PROMO));
 
   // kIPHReadingListEntryPointFeature:
   registry.RegisterFeature(FeaturePromoSpecification::CreateForSnoozePromo(
@@ -240,7 +240,7 @@ void MaybeRegisterChromeFeaturePromos(
   // kIPHReadingListInSidePanelFeature:
   registry.RegisterFeature(FeaturePromoSpecification::CreateForLegacyPromo(
       &feature_engagement::kIPHReadingListInSidePanelFeature,
-      kSidePanelButtonElementId, IDS_READING_LIST_IN_SIDE_PANEL_PROMO));
+      kReadLaterButtonElementId, IDS_READING_LIST_IN_SIDE_PANEL_PROMO));
 
   // kIPHReopenTabFeature:
   registry.RegisterFeature(FeaturePromoSpecification::CreateForToastPromo(
@@ -342,7 +342,7 @@ void MaybeRegisterChromeFeaturePromos(
   // kIPHPriceTrackingInSidePanelFeature;
   registry.RegisterFeature(FeaturePromoSpecification::CreateForLegacyPromo(
       &feature_engagement::kIPHPriceTrackingInSidePanelFeature,
-      kSidePanelButtonElementId, IDS_PRICE_TRACKING_SIDE_PANEL_IPH));
+      kReadLaterButtonElementId, IDS_PRICE_TRACKING_SIDE_PANEL_IPH));
 }
 
 void MaybeRegisterChromeTutorials(
@@ -472,7 +472,7 @@ void MaybeRegisterChromeTutorials(
     // Open side panel
     TutorialDescription::Step open_side_panel_step(
         0, IDS_TUTORIAL_SIDE_PANEL_READING_LIST_OPEN_SIDE_PANEL,
-        ui::InteractionSequence::StepType::kShown, kSidePanelButtonElementId,
+        ui::InteractionSequence::StepType::kShown, kReadLaterButtonElementId,
         std::string(), HelpBubbleArrow::kTopRight);
     side_panel_description.steps.emplace_back(open_side_panel_step);
 
