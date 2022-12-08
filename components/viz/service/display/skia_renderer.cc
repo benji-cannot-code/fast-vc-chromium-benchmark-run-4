16d95a846c625a2712e05d21845ba42330bb3b5c
Femi Adegunloye <mrfemi@google.com>
2022-12-08 00:37:22 +0000

    lacros/hdr: pass down color space through aggregate quad
    Propagate the color space of an aggregate render passes through to
    overlay candidates.
    
    BUG=b:253053317
    
    Change-Id: I94b8e0f1abadecb6b89d3eb0459029c76df4c421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018014
    Commit-Queue: Femi Adegunloye <mrfemi@google.com>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Jim Shargo <jshargo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080683}

--- components/viz/service/display/skia_renderer.cc
+++ components/viz/service/display/skia_renderer.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "third_party/skia/modules/skcms/skcms.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/color_transform.h"
 #include "ui/gfx/geometry/angle_conversions.h"
 #include "ui/gfx/geometry/axis_transform2d.h"
@@ -3599,6 +3600,9 @@ void SkiaRenderer::PrepareRenderPassOverlay(
   }
   // Assume full damage every time the pass is rendered.
   overlay->damage_rect = gfx::RectF(filter_bounds);
+  // Fill in |format| and |color_space| information based on selected backing.
+  overlay->color_space = color_space;
+  overlay->format = BufferFormat(buffer_format);
 #endif  // BUILDFLAG(IS_APPLE)
 }
 #endif  // BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_OZONE)
