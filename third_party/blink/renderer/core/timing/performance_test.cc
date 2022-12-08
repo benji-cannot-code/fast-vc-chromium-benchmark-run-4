5ad8134427bc483c331bea3b2ee77b20c50acbe3
Jaspreet Sandhu <jaspreetsandhu@google.com>
2022-12-08 20:38:31 +0000

    Add getEntriesWithChildFrames() method to Performance
    This change introduces a method to include the Performance Entries of nested, same-origin iframes when fetching Performance Timeline data. It uses a Breadth-First Search algorithm on the Frame Tree whose root node is the current frame.
    
    This functionality is available via the `includeFrames` boolean flag in each of `getEntries()`, `getEntriesByType()`, and `getEntriesByName()`.
    
    Change-Id: I725d29c8295851d957c8b6580bd0d065f3bb197f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051366
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jaspreet Sandhu <jaspreetsandhu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081090}

--- third_party/blink/renderer/core/timing/performance_test.cc
+++ third_party/blink/renderer/core/timing/performance_test.cc
@@ -217,10 +217,11 @@ TEST_F(PerformanceTest, BackForwardCacheRestoration) {
   auto entries = PerformanceEntriesInObserver();
   CheckBackForwardCacheRestoration(entries);
 
-  entries = base_->getEntries();
+  entries = base_->getEntries(scope.GetScriptState());
   CheckBackForwardCacheRestoration(entries);
 
-  entries = base_->getEntriesByType("back-forward-cache-restoration");
+  entries = base_->getEntriesByType(scope.GetScriptState(),
+                                    "back-forward-cache-restoration");
   CheckBackForwardCacheRestoration(entries);
 }
 
@@ -234,7 +235,8 @@ TEST_F(PerformanceTest, InsertEntryOnEmptyBuffer) {
   PerformanceEventTiming* test_entry =
       PerformanceEventTiming::Create("event", 0.0, 0.0, 0.0, false, nullptr, 0);
 
-  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry);
+  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry,
+                                     Performance::kDoNotRecordSwaps);
 
   PerformanceEntryVector sorted_buffer_;
   sorted_buffer_.push_back(*test_entry);
@@ -263,7 +265,8 @@ TEST_F(PerformanceTest, InsertEntryOnExistingBuffer) {
   // Create copy of the test_buffer_.
   PerformanceEntryVector sorted_buffer_ = test_buffer_;
 
-  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry);
+  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry,
+                                     Performance::kDoNotRecordSwaps);
 
   sorted_buffer_.push_back(*test_entry);
   std::sort(sorted_buffer_.begin(), sorted_buffer_.end(),
@@ -293,7 +296,8 @@ TEST_F(PerformanceTest, InsertEntryToFrontOfBuffer) {
   // Create copy of the test_buffer_.
   PerformanceEntryVector sorted_buffer_ = test_buffer_;
 
-  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry);
+  base_->InsertEntryIntoSortedBuffer(test_buffer_, *test_entry,
+                                     Performance::kDoNotRecordSwaps);
 
   sorted_buffer_.push_back(*test_entry);
   std::sort(sorted_buffer_.begin(), sorted_buffer_.end(),
@@ -302,11 +306,9 @@ TEST_F(PerformanceTest, InsertEntryToFrontOfBuffer) {
   EXPECT_EQ(test_buffer_, sorted_buffer_);
 }
 
-TEST_F(PerformanceTest, MergePerformanceEntryVectorIntoListTest) {
+TEST_F(PerformanceTest, MergePerformanceEntryVectorsTest) {
   PerformanceEntryVector first_vector;
   PerformanceEntryVector second_vector;
-
-  std::list<Member<PerformanceEntry>> all_entries;
 
   PerformanceEntryVector test_vector;
 
@@ -318,8 +320,6 @@ TEST_F(PerformanceTest, MergePerformanceEntryVectorIntoListTest) {
     test_vector.push_back(*entry);
   }
 
-  MergePerformanceEntryVectorIntoList(all_entries, first_vector);
-
   for (int i = 1; i < 6; i += 2) {
     double tmp = 1.0;
     PerformanceEventTiming* entry = PerformanceEventTiming::Create(
@@ -328,17 +328,14 @@ TEST_F(PerformanceTest, MergePerformanceEntryVectorIntoListTest) {
     test_vector.push_back(*entry);
   }
 
-  MergePerformanceEntryVectorIntoList(all_entries, second_vector);
-
-  PerformanceEntryVector entries;
-  for (auto& entry : all_entries) {
-    entries.push_back(entry);
-  }
+  PerformanceEntryVector all_entries;
+  all_entries = MergePerformanceEntryVectors(all_entries, first_vector);
+  all_entries = MergePerformanceEntryVectors(all_entries, second_vector);
 
   std::sort(test_vector.begin(), test_vector.end(),
             PerformanceEntry::StartTimeCompareLessThan);
 
-  EXPECT_EQ(entries, test_vector);
+  EXPECT_EQ(all_entries, test_vector);
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/timing/performance_user_timing.cc
+++ third_party/blink/renderer/core/timing/performance_user_timing.cc
@@ -259,7 +259,8 @@ void UserTiming::InsertPerformanceEntry(
     PerformanceEntryMap& performance_entry_map,
     PerformanceEntryVector& performance_entry_buffer,
     PerformanceEntry& entry) {
-  performance_->InsertEntryIntoSortedBuffer(performance_entry_buffer, entry);
+  performance_->InsertEntryIntoSortedBuffer(performance_entry_buffer, entry,
+                                            Performance::kDoNotRecordSwaps);
 
   auto it = performance_entry_map.find(entry.name());
   if (it == performance_entry_map.end()) {
@@ -271,7 +272,8 @@ void UserTiming::InsertPerformanceEntry(
   }
 
   DCHECK(it->value);
-  performance_->InsertEntryIntoSortedBuffer(*it->value.Get(), entry);
+  performance_->InsertEntryIntoSortedBuffer(*it->value.Get(), entry,
+                                            Performance::kDoNotRecordSwaps);
 }
 
 void UserTiming::ClearPerformanceEntries(
