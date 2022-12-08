ea7eb636d9fba8291e04b0a9a60934d8c26c6ba4
Dibyajyoti Pal <dibyapal@chromium.org>
2022-12-08 00:01:23 +0000

    [dPWA] Make IntentPickerBubbleView be auto accepted for testing.
    This CL introduces functionality to auto accept an intent picker bubble
    view for testing and connects it with the WebAppIntegrationTestDriver
    to reduce flakiness.
    
    Bug: b/260743284, crbug.com/1393890
    Change-Id: I745cfaa8538e7cbf40bdfdcb311dc228ef6629ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080621
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080660}

--- chrome/browser/ui/views/intent_picker_bubble_view.h
+++ chrome/browser/ui/views/intent_picker_bubble_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/apps/intent_helper/apps_navigation_types.h"
@@ -97,6 +98,9 @@ class IntentPickerBubbleView : public LocationBarBubbleDelegateView {
   static IntentPickerBubbleView* intent_picker_bubble() {
     return intent_picker_bubble_;
   }
+
+  static base::AutoReset<bool> SetAutoAcceptIntentPickerBubbleForTesting();
+
   static void CloseCurrentBubble();
 
   // LocationBarBubbleDelegateView overrides:
