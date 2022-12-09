e2ffadd65c6fba854b3b4ec66c392b4b3522e369
Colin Blundell <blundell@chromium.org>
2022-12-09 15:04:53 +0000

    [GPU] Change EGLImageBacking to use ScopedEGLImage
    EGLImageBacking currently uses NativeImageBuffer to indirectly create
    and manipulate its EGLImage. This CL changes that usage to
    ScopedEGLImage, which is directly suited for this use case.
    Specifically:
    
    - A ScopedEGLImage ivar now creates and holds the EGLImage, with its
      creation params being specified to be identical to what
      NativeImageBufferEGL::Create() previously did
    - The code of NativeImageBufferEGL::BindToTexture() is now inlined
      into the callsite
    - ScopedEGLImage destroys the EGLImage in its own destructor, as
      NativeImageBufferEGL does. One difference is that
      NativeImageBufferEGL caches the EGLDisplay that it was passed on
      construction (which was
      gl::GLSurfaceEGL::GetGLDisplayEGL()->GetDisplay())
      and uses that in the destructor, while ScopedEGLImage
      calls gl::GLSurfaceEGL::GetGLDisplayEGL()->GetDisplay() again.
      eglDestroyImageKHR() will fail if the EGLDisplay passed to it is
      not the same one with which the image was created. Fortunately,
      gl::GLSurfaceEGL::GetGLDisplayEGL() is set on the first call and
      never subsequently changed, and GLDisplayEGL::GetDisplay() returns
      a value that is set in GLDisplayEGL::InitializeDisplay() and never
      subsequently changed.
    
    This CL also deletes native_image_buffer.*, which now has no usage.
    
    Bug: 1332554
    Change-Id: I9638838444b1538403726443303828ab2c797da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090605
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081459}

--- gpu/command_buffer/service/shared_image/egl_image_backing.h
+++ gpu/command_buffer/service/shared_image/egl_image_backing.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_image/shared_image_backing.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gl/gl_bindings.h"
+#include "ui/gl/scoped_egl_image.h"
 
 namespace gl {
 class GLFenceEGL;
@@ -24,10 +25,6 @@ class GpuDriverBugWorkarounds;
 class GLTextureImageRepresentation;
 class SkiaImageRepresentation;
 struct Mailbox;
-
-namespace gles2 {
-class NativeImageBuffer;
-}  // namespace gles2
 
 // Implementation of SharedImageBacking that is used to create EGLImage targets
 // from the same EGLImage object. Hence all the representations created from
@@ -100,8 +97,7 @@ class EGLImageBacking : public ClearTrackingSharedImageBacking {
   scoped_refptr<TextureHolder> source_texture_holder_;
   raw_ptr<gl::GLApi> created_on_context_;
 
-  // This class encapsulates the EGLImage object for android.
-  scoped_refptr<gles2::NativeImageBuffer> egl_image_buffer_ GUARDED_BY(lock_);
+  ui::ScopedEGLImage egl_image_ GUARDED_BY(lock_);
 
   // All reads and writes must wait for exiting writes to complete.
   // TODO(vikassoni): Use SharedGLFenceEGL here instead of GLFenceEGL here in
