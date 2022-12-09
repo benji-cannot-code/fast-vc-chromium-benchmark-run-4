8a859b37a96805b35c1605f3091df674b1c9779d
Cassy Chun-Crogan <cassycc@google.com>
2022-12-09 05:06:55 +0000

    [Office upload] Trigger setup flow for files already in DriveFS/ODFS
    The setup flow should always be triggered if it hasn't run before when
    opening an office file with microsoft 365 or Google Docs, including
    when the file is already in the correct location (OneDrive or Drive
    respectively). Added fix so that when the file is already in the
    correct location it still runs and the user isn't prompted to move the
    file (to the location it already exists in).
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: b:260663553
    Change-Id: I4acddf79aaefffe5b349f6749b08eacaaf71b698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075390
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081321}

--- chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/extensions/file_manager/event_router_factory.h"
 #include "chrome/browser/ash/file_manager/app_id.h"
 #include "chrome/browser/ash/file_manager/file_manager_test_util.h"
+#include "chrome/browser/ash/file_manager/file_tasks.h"
 #include "chrome/browser/ash/file_manager/file_tasks_notifier.h"
 #include "chrome/browser/ash/file_manager/file_tasks_observer.h"
 #include "chrome/browser/ash/file_manager/mount_test_util.h"
@@ -2839,6 +2840,13 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
   if (name == "mountHidden") {
     DCHECK(hidden_volume_);
     ASSERT_TRUE(hidden_volume_->Mount(profile()));
+    return;
+  }
+
+  if (name == "setOfficeSetupComplete") {
+    absl::optional<bool> complete = value.FindBool("complete");
+    ASSERT_TRUE(complete.has_value());
+    file_tasks::SetOfficeSetupComplete(profile(), complete.value());
     return;
   }
 
