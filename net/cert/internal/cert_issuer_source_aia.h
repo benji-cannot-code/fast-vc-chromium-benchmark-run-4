2adefc68c7f7e45c2d1da2b1af6d99e04fda49f9
Bob Beck <bbe@google.com>
2022-12-09 20:48:23 +0000

    Remove unused base/memory/ref_counted.h from net/cert/pki
    parsed_certificate.h does not need this, however this means
    that internal/cert_issuer_source_aia.h needs to come by
    base::scoped_refptr honestly.
    
    Bug: 1322914
    Change-Id: I18d76505693e840b5b3f4d7c5c274abf48c28cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090699
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Bob Beck <bbe@google.com>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081627}

--- net/cert/internal/cert_issuer_source_aia.h
+++ net/cert/internal/cert_issuer_source_aia.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_CERT_INTERNAL_CERT_ISSUER_SOURCE_AIA_H_
 #define NET_CERT_INTERNAL_CERT_ISSUER_SOURCE_AIA_H_
 
+#include "base/memory/scoped_refptr.h"
 #include "base/strings/string_piece.h"
 #include "net/base/net_export.h"
 #include "net/cert/pki/cert_issuer_source.h"
