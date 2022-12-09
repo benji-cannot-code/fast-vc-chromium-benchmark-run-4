90a7d7e31bc1bb122d1535aef2b0ce0894d8e04e
Tibor Goldschwendt <tiborg@chromium.org>
2022-12-09 01:37:55 +0000

    [cc-side-panel] Set chrome theme when clicking on color
    Bug: 1384229
    Change-Id: I423ae847a5cca3fd461839e541a3e9fc4609e9b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090044
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081277}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/native_theme/native_theme_observer.h"
 
+namespace chrome_colors {
+class ChromeColorsService;
+}  // namespace chrome_colors
+
 namespace content {
 class WebContents;
 }  // namespace content
@@ -52,6 +56,8 @@ class CustomizeChromePageHandler
   void GetBackgroundCollections(
       GetBackgroundCollectionsCallback callback) override;
   void UpdateTheme() override;
+  void SetDefaultColor() override;
+  void SetForegroundColor(SkColor foreground_color) override;
 
  private:
   // ui::NativeThemeObserver:
@@ -76,6 +82,7 @@ class CustomizeChromePageHandler
   GetBackgroundCollectionsCallback background_collections_callback_;
   base::TimeTicks background_collections_request_start_time_;
   raw_ptr<ThemeService> theme_service_;
+  raw_ptr<chrome_colors::ChromeColorsService> chrome_colors_service_;
 
   base::ScopedObservation<ui::NativeTheme, ui::NativeThemeObserver>
       native_theme_observation_{this};
