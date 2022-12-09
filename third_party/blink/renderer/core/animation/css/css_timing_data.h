d74bb3bfbd30b34517100ed785e584aa342c943a
Anders Hartvoll Ruud <andruud@chromium.org>
2022-12-09 14:14:17 +0000

    [scroll-animations] Support animation-duration:auto
    This CL supports using 'auto' (explicitly) as an animation duration.
    
    Whether or not 'auto' should become the new initial value of
    animation-duration will be handled separately.
    
    Fixed: 1382873
    Change-Id: I50a669fdf53323f32299948081d316997c7820c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079648
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081438}

--- third_party/blink/renderer/core/animation/css/css_timing_data.h
+++ third_party/blink/renderer/core/animation/css/css_timing_data.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_CSS_CSS_TIMING_DATA_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_CSS_CSS_TIMING_DATA_H_
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/animation/timing.h"
 #include "third_party/blink/renderer/platform/animation/timing_function.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -25,21 +26,23 @@ class CSSTimingData {
     return delay_start_list_;
   }
   const Vector<Timing::Delay>& DelayEndList() const { return delay_end_list_; }
-  const Vector<double>& DurationList() const { return duration_list_; }
+  const Vector<absl::optional<double>>& DurationList() const {
+    return duration_list_;
+  }
   const Vector<scoped_refptr<TimingFunction>>& TimingFunctionList() const {
     return timing_function_list_;
   }
 
   Vector<Timing::Delay>& DelayStartList() { return delay_start_list_; }
   Vector<Timing::Delay>& DelayEndList() { return delay_end_list_; }
-  Vector<double>& DurationList() { return duration_list_; }
+  Vector<absl::optional<double>>& DurationList() { return duration_list_; }
   Vector<scoped_refptr<TimingFunction>>& TimingFunctionList() {
     return timing_function_list_;
   }
 
   static Timing::Delay InitialDelayStart() { return Timing::Delay(); }
   static Timing::Delay InitialDelayEnd() { return Timing::Delay(); }
-  static double InitialDuration() { return 0; }
+  static absl::optional<double> InitialDuration() { return 0; }
   static scoped_refptr<TimingFunction> InitialTimingFunction() {
     return CubicBezierTimingFunction::Preset(
         CubicBezierTimingFunction::EaseType::EASE);
@@ -60,7 +63,7 @@ class CSSTimingData {
  private:
   Vector<Timing::Delay> delay_start_list_;
   Vector<Timing::Delay> delay_end_list_;
-  Vector<double> duration_list_;
+  Vector<absl::optional<double>> duration_list_;
   Vector<scoped_refptr<TimingFunction>> timing_function_list_;
 };
 
