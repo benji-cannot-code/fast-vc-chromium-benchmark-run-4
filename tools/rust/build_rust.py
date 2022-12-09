b164bf06a1a1eb25e3d7ea13d403206344aa665e
danakj <danakj@chromium.org>
2022-12-09 15:06:39 +0000

    Set the path to MSVC when running x.py to build Rust.
    This allows linking to work, now we fail because x.py is passing
    invalid arguments to the LLVM archiver.
    
    ```
    error: failed to run custom build command for `lzma-sys v0.1.17`
    ```
    
    We set the archiver in the environment:
    
    AR = Some("C:\\b\\s\\w\\ir\\cache\\builder\\src\\third_party\\llvm-build\\Release+Asserts\\bin\\llvm-ar")
    
    The command that fails is:
    
    ```
    "C:\\b\\s\\w\\ir\\cache\\builder\\src\\third_party\\llvm-build\\Release+Asserts\\bin\\llvm-ar" "-out:C:\\b\\s\\w\\ir\\cache\\builder\\src\\third_party\\rust_src\\src\\build\\bootstrap\\debug\\build\\lzma-sys-b4fbaf154720cb7c\\out\\liblzma.a" "-nologo" ...lots of .o files...
    ```
    
    It seems that the -nologo argument is the problem.
    
    Bug: 1271215
    Change-Id: I03636eee44a22b92c63ee4beb43711ffb766c95b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087043
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081462}

--- tools/rust/build_rust.py
+++ tools/rust/build_rust.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 '''Assembles a Rust toolchain in-tree linked against in-tree LLVM.
 
-!!! DO NOT USE IN PRODUCTION
 Builds a Rust toolchain bootstrapped from an untrusted rustc build.
 
 Rust has an official boostrapping build. At a high level:
@@ -51,6 +50,7 @@ sys.path.append(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'clang',
                  'scripts'))
 
+from build import RunCommand
 from update import (CLANG_REVISION, CLANG_SUB_REVISION, LLVM_BUILD_DIR,
                     GetDefaultHostOs, RmTree, UpdatePackage)
 import build
@@ -100,17 +100,6 @@ EXCLUDED_TESTS = [
     'src/test/codegen/sanitizer-cfi-emit-type-checks.rs',
     'src/test/codegen/sanitizer-cfi-emit-type-metadata-itanium-cxx-abi.rs',
 ]
-
-
-def RunCommand(command, env=None, fail_hard=True):
-    print('Running', command)
-    if subprocess.run(command, env=env,
-                      shell=sys.platform == 'win32').returncode == 0:
-        return True
-    print('Failed.')
-    if fail_hard:
-        sys.exit(1)
-    return False
 
 
 def VerifyStage0JsonHash():
@@ -182,7 +171,7 @@ def RunXPy(sub, args, gcc_toolchain_path, verbose):
     cmd = [sys.executable, 'x.py', sub]
     if verbose and verbose > 0:
         cmd.append('-' + verbose * 'v')
-    RunCommand(cmd + args, env=RUSTENV)
+    RunCommand(cmd + args, msvc_arch='x64', env=RUSTENV)
 
 
 # Get arguments to run desired test suites, minus disabled tests.
