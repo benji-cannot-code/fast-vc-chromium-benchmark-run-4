ede63b5b3a95a0369dcb0200bccd244899d60e08
Pavol Marko <pmarko@google.com>
2022-12-09 23:22:59 +0000

    Remove shill::kStateCarrier usage from chromium
    The state is never emitted by shill.
    
    Backend device management code never uses
    NetworkState::CARRIER either.
    
    Bug: b/260792466
    Change-Id: I8310ea192fc022000c990d18073bd7846441d068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4075958
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081710}

--- chrome/browser/ash/policy/status_collector/device_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector.cc
@@ -2385,7 +2385,6 @@ bool DeviceStatusCollector::GetNetworkStatus(
     em::NetworkState::ConnectionState state_constant;
   } kConnectionStateMap[] = {
       {shill::kStateIdle, em::NetworkState::IDLE},
-      {shill::kStateCarrier, em::NetworkState::CARRIER},
       {shill::kStateAssociation, em::NetworkState::ASSOCIATION},
       {shill::kStateConfiguration, em::NetworkState::CONFIGURATION},
       {shill::kStateReady, em::NetworkState::READY},
