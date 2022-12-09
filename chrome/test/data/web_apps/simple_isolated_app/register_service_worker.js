5896cd5b2b3eae0a6cfe22da73c60edb7d82d2fd
Dmitrii Kuragin <kuragin@chromium.org>
2022-12-09 23:16:23 +0000

    iwa: Migrate IsolatedWebAppBrowserTest to new install flow and remove kIsolatedAppOrigins flag.
    The tests use production flow for the installation (See
    `InstallDevModeProxyIsolatedWebApp`) instead of previous
    (`InstallIsolatedWebApp`) which inserts the data directly into the
    database and bypass some actions which are important (for example, creation of storage partition). If we use the only prod workflow, then
    we don't need to use the test-only shortcut way what improves tests
    stability.
    
    Notes:
     - The test use smallest possible set of JS code now.
     - Add verification of permissions in `SendMessageAndWaitUntilHandled`
       to make sure the permissions are granted when the function waits
       for push notifications.
     - `CanInstallAppWithManifestFieldAndIsolatedAppOriginsFlag` is
       intentonally checks for the old flow (kIsolatedAppOrigins flag) to
       make sure the flow still works. But it is being removed.
    
    Bug: 1333966
    Change-Id: I038c41c99ef16b18e2fba30bdba44fc9ad706a75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072811
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081706}

---
+++ chrome/test/data/web_apps/simple_isolated_app/register_service_worker.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  const policy = trustedTypes.createPolicy('default', {
+    createScriptURL: url => url,
+  });
+
+  navigator.serviceWorker.register(
+    policy.createScriptURL('/service_worker.js'),
+    // Match the scope defined in manifest_isolated.json. To be able to register
+    // the worker for this scope, the 'Service-Worker-Allowed' header must be
+    // set to '/' on the service worker script.
+    { scope: '/' }
+  );
+})();
---
+++ chrome/test/data/web_apps/simple_isolated_app/service_worker.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.addEventListener('fetch', e => {
+  e.respondWith((async () => {
+    try {
+      return await fetch(e.request);
+    } catch (error) {
+      return new Response('Hello offline page');
+    }
+  })());
+});
+
+self.addEventListener('push', event => {
+  self.registration.showNotification('Hello world!', {
+    body: 'Test Notification',
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow(
+    self.location.origin + '/register_service_worker.html'
+  ));
+});
