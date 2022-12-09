0c833735b454ce15286fa56ce9231770e646b5a2
Francois Doray <fdoray@chromium.org>
2022-12-09 18:18:57 +0000

    Pump remaining messages synchronously in InputDispatcher.
    InputDispatcher wants to invoke its callback only when there are no
    more messages of the type of interest in the message queue.
    
    Previously, when InputDispatcher::MatchingMessageProcessed() observed
    messages of the type of interest in the message queue, it would post
    another InputDispatcher::MatchingMessageProcessed() task, with the
    assumption that at least one message has been removed from the queue
    when that other task runs.
    
    When RunTasksByBatches is enabled, that assumption is incorrect,
    because multiple posted tasks can run in "batches" before system
    messages are processed. To address this problem, this CL makes
    InputDispatcher::MatchingMessageProcessed() process system messages
    synchronously until there are no more messages of the type of
    interested in the queue.
    
    Bug: 1299950
    Change-Id: Icd09ce153c5e12f20f8137a7c13d416b519abead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4052108
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081554}

--- ui/base/test/ui_controls_internal_win.cc
+++ ui/base/test/ui_controls_internal_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_checker.h"
@@ -129,6 +130,10 @@ class InputDispatcher {
 
   // The desired mouse position for a mouse move event.
   const gfx::Point expected_mouse_location_;
+
+  // Whether all desired messages were observed, but MatchingMessageProcessed()
+  // is flushing remaining messages of type `system_queue_flag_`.
+  bool flushing_messages_ = false;
 
   base::WeakPtrFactory<InputDispatcher> weak_factory_{this};
 };
@@ -310,7 +315,11 @@ void InputDispatcher::DispatchedMessage(
 void InputDispatcher::MatchingMessageProcessed(bool definitively_done) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-  if (message_waiting_for_ == WM_KEYUP && --num_keyups_awaited_ > 0)
+  // Guard against re-entrancy.
+  if (flushing_messages_)
+    return;
+
+  if (message_waiting_for_ == WM_KEYUP && --num_keyups_awaited_ != 0)
     return;
 
   // Unless specified otherwise by |definitively_done| : resume on the last
@@ -318,21 +327,20 @@ void InputDispatcher::MatchingMessageProcessed(bool definitively_done) {
   // InputDispatcher is created while there are preexisting matching events
   // remaining in the queue. Emit a warning to help diagnose flakes should the
   // queue somehow never become empty of such events.
-  if (HIWORD(::GetQueueStatus(system_queue_flag_)) && !definitively_done) {
-    LOG(WARNING)
-        << "Skipping message notification per remaining events in the queue "
-           "(will try again shortly) : "
-        << system_queue_flag_;
+  if (!definitively_done) {
+    while (HIWORD(::GetQueueStatus(system_queue_flag_))) {
+      LOG(WARNING) << "Got all expected messages, but the queue still contains "
+                      "messages of type "
+                   << system_queue_flag_
+                   << ". Pumping messages until it's no longer the case.";
 
-    // Check back on the next loop instead of relying on the remaining event
-    // being caught by our hooks (all events don't seem to reliably make it
-    // there).
-    if (message_waiting_for_ == WM_KEYUP)
-      ++num_keyups_awaited_;
-    base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-        FROM_HERE, base::BindOnce(&InputDispatcher::MatchingMessageProcessed,
-                                  weak_factory_.GetWeakPtr(), false));
-    return;
+      // RunLoop::Run() calls MessagePumpForUI::ProcessNextWindowsMessage(),
+      // which should remove at least one message from the queue.
+      flushing_messages_ = true;
+      auto weak_ptr = weak_factory_.GetWeakPtr();
+      base::RunLoop().RunUntilIdle();
+      DCHECK(weak_ptr);
+    }
   }
 
   // Delete |this| before running the callback to allow callers to chain input
