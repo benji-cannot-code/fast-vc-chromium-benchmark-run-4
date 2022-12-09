9f63754cf110e20dbc18685216e68cb5798a71f2
Juan Mojica <juanmojica@google.com>
2022-12-09 00:24:27 +0000

    Clean up instruction chip improvements feature after launch.
    Removes chrome://flags and feature code.
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome,mac-chrome,linux-chrome,win-chrome,win64-chrome
    Bug: b:248022565
    Change-Id: I7dd51a44c901e8d716993dc50c86e99992425fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085816
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081241}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -676,9 +676,6 @@ extern const char kEnableLensStandaloneFlagId[];
 extern const char kEnableLensStandaloneName[];
 extern const char kEnableLensStandaloneDescription[];
 
-extern const char kEnableLensInstructionChipImprovementsName[];
-extern const char kEnableLensInstructionChipImprovementsDescription[];
-
 extern const char kEnableRegionSearchOnPdfViewerName[];
 extern const char kEnableRegionSearchOnPdfViewerDescription[];
 
