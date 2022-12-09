a56d8ce6dec38f6216dd92e325a1e661ef6242a0
Ashley Prasad <ashleydp@google.com>
2022-12-09 01:35:12 +0000

    audio-settings: add setInputMuted to fake
    - Add `inputMuteState` to AudioSystemProperties in fake.
    - Add `setInputMuted` to fake to update inputMuteState.
    - Test `inputMuteState` updates as expected when mute function called.
    
    Bug: b:260277007
    Test: browser_test --gtest_filter=*OSSettings*FakeCros*
    Change-Id: I6b70a94d0fd37e35500a37e772f44a20ebbb549d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081189
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081276}

--- chrome/browser/resources/settings/chromeos/device_page/fake_cros_audio_config.ts
+++ chrome/browser/resources/settings/chromeos/device_page/fake_cros_audio_config.ts
@@ -58,6 +58,7 @@ export const fakeBluetoothMic: AudioDevice = {
 // handle audio input.
 export interface AudioSystemProperties extends AudioSystemPropertiesMojom {
   inputDevices: AudioDevice[];
+  inputMuteState: MuteState;
 }
 
 export interface FakePropertiesObserverInterface {
@@ -69,6 +70,7 @@ export const defaultFakeAudioSystemProperties: AudioSystemProperties = {
   outputVolumePercent: 75,
   outputMuteState: MuteState.kNotMuted,
   inputDevices: [fakeInternalFrontMic, fakeBluetoothMic],
+  inputMuteState: MuteState.kNotMuted,
 };
 
 /** Creates an audio device based on provided device and isActive override. */
@@ -81,6 +83,7 @@ export function createAudioDevice(
 export interface FakeCrosAudioConfigInterface extends CrosAudioConfigInterface {
   setActiveDevice(outputDevice: AudioDevice): void;
   setOutputMuted(muted: boolean): void;
+  setInputMuted(muted: boolean): void;
 }
 
 export class FakeCrosAudioConfig implements FakeCrosAudioConfigInterface {
@@ -134,6 +137,16 @@ export class FakeCrosAudioConfig implements FakeCrosAudioConfigInterface {
   setOutputMuted(muted: boolean): void {
     this.audioSystemProperties.outputMuteState =
         muted ? MuteState.kMutedByUser : MuteState.kNotMuted;
+    this.notifyAudioSystemPropertiesUpdated();
+  }
+
+  /**
+   * Sets the input device mute state to `kMutedByUser` when true and
+   * `kNotMuted` when false.
+   */
+  setInputMuted(muted: boolean): void {
+    const muteState = muted ? MuteState.kMutedByUser : MuteState.kNotMuted;
+    this.audioSystemProperties.inputMuteState = muteState;
     this.notifyAudioSystemPropertiesUpdated();
   }
 
