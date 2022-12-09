53ca65cc3a54ed2b08dc224b6f3f739fe572da98
Christian Biesinger <cbiesinger@chromium.org>
2022-12-09 17:51:24 +0000

    [FedCM] Also rename mojom::IdentityProvider to *Config
    Followup to https://crrev.com/c/4068059 which renamed the WebIDL
    dictionary.
    
    This will make things less confusing once we add a WebIDL interface
    named IdentityProvider.
    
    R=goto@chromium.org, yigu@chromium.org
    
    Bug: 1394805
    Change-Id: I54a22a6e8323a48a76727f918341d5f905747bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086244
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081539}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -39,9 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 
 using blink::mojom::FederatedAuthRequestResult;
-using blink::mojom::IdentityProvider;
+using blink::mojom::IdentityProviderConfig;
+using blink::mojom::IdentityProviderConfigPtr;
 using blink::mojom::IdentityProviderGetParametersPtr;
-using blink::mojom::IdentityProviderPtr;
 using blink::mojom::LogoutRpsStatus;
 using blink::mojom::RequestTokenStatus;
 using FederatedApiPermissionStatus =
@@ -322,7 +322,7 @@ FederatedAuthRequestPageData* GetPageData(RenderFrameHost* render_frame_host) {
 }  // namespace
 
 FederatedAuthRequestImpl::IdentityProviderGetInfo::IdentityProviderGetInfo(
-    blink::mojom::IdentityProvider provider,
+    blink::mojom::IdentityProviderConfig provider,
     bool prefer_auto_signin)
     : provider(std::move(provider)), prefer_auto_signin(prefer_auto_signin) {}
 
@@ -332,7 +332,7 @@ FederatedAuthRequestImpl::IdentityProviderGetInfo::IdentityProviderGetInfo(
     const IdentityProviderGetInfo&) = default;
 
 FederatedAuthRequestImpl::IdentityProviderInfo::IdentityProviderInfo(
-    blink::mojom::IdentityProvider provider,
+    blink::mojom::IdentityProviderConfig provider,
     IdpNetworkRequestManager::Endpoints endpoints,
     IdentityProviderMetadata metadata,
     bool prefer_auto_signin)
@@ -951,7 +951,7 @@ void FederatedAuthRequestImpl::OnAccountsResponseReceived(
 }
 
 void FederatedAuthRequestImpl::ComputeLoginStateAndReorderAccounts(
-    const IdentityProvider& idp,
+    const IdentityProviderConfig& idp,
     IdpNetworkRequestManager::AccountList& accounts) {
   // Populate the accounts login state.
   for (auto& account : accounts) {
@@ -1079,7 +1079,7 @@ void FederatedAuthRequestImpl::OnDialogDismissed(
 }
 
 void FederatedAuthRequestImpl::OnTokenResponseReceived(
-    const IdentityProvider& idp,
+    const IdentityProviderConfig& idp,
     IdpNetworkRequestManager::FetchStatus status,
     const std::string& id_token) {
   if (!auth_request_callback_)
@@ -1105,7 +1105,7 @@ void FederatedAuthRequestImpl::OnTokenResponseReceived(
 }
 
 void FederatedAuthRequestImpl::CompleteTokenRequest(
-    const IdentityProvider& idp,
+    const IdentityProviderConfig& idp,
     IdpNetworkRequestManager::FetchStatus status,
     const std::string& token) {
   DCHECK(!start_time_.is_null());
