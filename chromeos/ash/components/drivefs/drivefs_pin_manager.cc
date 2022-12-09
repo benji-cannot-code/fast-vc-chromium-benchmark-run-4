b0fc4bd8e2fd06268d3a16e7cde00d5e446b575e
Ben Reich <benreich@chromium.org>
2022-12-09 05:35:07 +0000

    [FilesTiering] Periodically clean the in progress syncing items
    There are a few types of items that once pinned has been enabled do not
    emit a OnSyncingStatusUpdate, currently these are hosted docs and
    possibly smaller documents as well. To avoid hardcoding all these
    possibilities, run a periodic task that verifies that the
    `available_offline` bit has been set and if so remove it from the in
    progress map.
    
    Fixed: b:261530666
    Test: chromeos_unittests --gtest_filter=*DriveFsPinManager*
    Change-Id: I2cba4120b70b3c6d11f5970acd60a467da007707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086083
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081330}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -55,6 +55,11 @@ class FreeDiskSpaceImpl : public FreeDiskSpaceDelegate {
 
 }  // namespace
 
+// TODO(b/261530666): This was chosen arbitrarily, this should be experimented
+// with and potentially made dynamic depending on feedback of the in progress
+// queue.
+constexpr base::TimeDelta kPeriodicRemovalInterval = base::Seconds(10);
+
 constexpr char kGCacheFolderName[] = "GCache";
 
 DriveFsPinManager::InProgressSyncingItems::InProgressSyncingItems() = default;
@@ -93,6 +98,19 @@ size_t DriveFsPinManager::InProgressSyncingItems::GetItemCount() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   VLOG(1) << "Remaining syncing items: " << in_progress_items_.size();
   return in_progress_items_.size();
+}
+
+std::vector<std::string>
+DriveFsPinManager::InProgressSyncingItems::GetUnstartedItems() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  std::vector<std::string> unstarted_items;
+  for (const auto& item : in_progress_items_) {
+    if (item.second.second > 0) {
+      continue;
+    }
+    unstarted_items.emplace_back(item.first);
+  }
+  return unstarted_items;
 }
 
 DriveFsPinManager::DriveFsPinManager(bool enabled,
@@ -175,7 +193,7 @@ void DriveFsPinManager::OnSearchResultForSizeCalculation(
   }
 
   if (items.value().size() == 0) {
-    VLOG(2) << "Iterated all files and calculated " << size_required_
+    VLOG(1) << "Iterated all files and calculated " << size_required_
             << " bytes required with " << free_space_ << " bytes available in "
             << timer_.Elapsed().InMilliseconds() << "ms";
     StartBatchPinning();
@@ -186,7 +204,7 @@ void DriveFsPinManager::OnSearchResultForSizeCalculation(
           << " for space calculation";
   for (const auto& item : items.value()) {
     if (item->metadata->pinned) {
-      VLOG(1) << "Item is already pinned, ignoring in space calculation";
+      VLOG(2) << "Item is already pinned, ignoring in space calculation";
       continue;
     }
     size_required_ += item->metadata->size;
@@ -226,6 +244,14 @@ void DriveFsPinManager::StartBatchPinning() {
   search_query_->GetNextPage(
       base::BindOnce(&DriveFsPinManager::OnSearchResultsForPinning,
                      weak_ptr_factory_.GetWeakPtr()));
+
+  // Start a periodic task that removes any files that are already available
+  // offline from the `in_progress_items_` map.
+  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&DriveFsPinManager::PeriodicallyRemovePinnedItems,
+                     weak_ptr_factory_.GetWeakPtr()),
+      kPeriodicRemovalInterval);
 }
 
 void DriveFsPinManager::OnSearchResultsForPinning(
@@ -270,7 +296,7 @@ void DriveFsPinManager::OnSearchResultsForPinning(
 
   for (const auto& item : items.value()) {
     if (item->metadata->pinned) {
-      VLOG(1) << "Item is already pinned, ignoring when batch pinning";
+      VLOG(2) << "Item is already pinned, ignoring when batch pinning";
       continue;
     }
     base::FilePath path(item->path);
@@ -285,7 +311,7 @@ void DriveFsPinManager::OnFilePinned(const std::string& path,
                                      drive::FileError status) {
   if (status != drive::FILE_ERROR_OK) {
     LOG(ERROR) << "Failed pinning an item: " << status;
-    VLOG(2) << "Path that failed to pin: " << path << " with error "
+    VLOG(1) << "Path that failed to pin: " << path << " with error "
             << drive::FileErrorToString(status);
     Complete(PinError::kErrorFailedToPinItem);
     return;
@@ -322,6 +348,52 @@ void DriveFsPinManager::MaybeStartSearch(size_t remaining_items) {
     search_query_->GetNextPage(
         base::BindOnce(&DriveFsPinManager::OnSearchResultsForPinning,
                        weak_ptr_factory_.GetWeakPtr()));
+  }
+}
+
+void DriveFsPinManager::PeriodicallyRemovePinnedItems() {
+  VLOG(1) << "Periodically removing pinned items";
+
+  syncing_items_.AsyncCall(&InProgressSyncingItems::GetUnstartedItems)
+      .Then(base::BindOnce(&DriveFsPinManager::GetMetadata,
+                           weak_ptr_factory_.GetWeakPtr()));
+
+  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&DriveFsPinManager::PeriodicallyRemovePinnedItems,
+                     weak_ptr_factory_.GetWeakPtr()),
+      kPeriodicRemovalInterval);
+}
+
+void DriveFsPinManager::GetMetadata(
+    const std::vector<std::string> unstarted_paths) {
+  for (const auto& path : unstarted_paths) {
+    base::FilePath file_path(path);
+    drivefs_interface_->GetMetadata(
+        file_path,
+        base::BindOnce(&DriveFsPinManager::OnMetadataRetrieved,
+                       weak_ptr_factory_.GetWeakPtr(), file_path.value()));
+  }
+
+  syncing_items_.AsyncCall(&InProgressSyncingItems::GetItemCount)
+      .Then(base::BindOnce(&DriveFsPinManager::MaybeStartSearch,
+                           weak_ptr_factory_.GetWeakPtr()));
+}
+
+void DriveFsPinManager::OnMetadataRetrieved(const std::string path,
+                                            drive::FileError error,
+                                            mojom::FileMetadataPtr metadata) {
+  if (error != drive::FILE_ERROR_OK) {
+    LOG(ERROR) << "Failed to retrieve metadata: " << error;
+    return;
+  }
+
+  if (metadata->available_offline || metadata->size == 0) {
+    VLOG(2) << "File " << path
+            << " has already been pinned or is a 0 byte file, removing from in "
+               "progress items";
+    syncing_items_.AsyncCall(&InProgressSyncingItems::RemoveItem)
+        .WithArgs(std::move(path));
   }
 }
 
