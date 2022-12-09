55d00269c9a129b66231ef7bd3328aa04d22cfa7
Ilkin Safarli <safarli@google.com>
2022-12-09 00:03:21 +0000

    [ambient] Adds feature flag for kScreenSaverPreview
    Bug: b:261900999
    Change-Id: Ibcbe2a4cb48bf5b0e624c252d6500f660a774f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090685
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081225}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3933,6 +3933,9 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAmbientSubpageUIChangeName,
      flag_descriptions::kAmbientSubpageUIChangeDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kAmbientSubpageUIChange)},
+    {"screen-saver-preview", flag_descriptions::kScreenSaverPreviewName,
+     flag_descriptions::kScreenSaverPreviewDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(ash::features::kScreenSaverPreview)},
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS)
