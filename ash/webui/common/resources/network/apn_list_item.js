9ee6c0baabfe2ab86a059daa5ded73615303a5eb
Antoni Dikov <adikov@google.com>
2022-12-09 13:57:25 +0000

    [CrOS Cellular] APN enable/disable nit fixes.
    * Use dom-ifs instead hidden attribute for three dot menu
    
    Related to nits from - https://chromium-review.googlesource.com/c/chromium/src/+/4056222
    
    Test: --gtest_filter=*ApnListItem*
    Change-Id: I7e8297443f189bc167cf68d7408e87363aa6b39c
    Bug: b:162365553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079454
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Antoni Dikov <adikov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081431}

--- ash/webui/common/resources/network/apn_list_item.js
+++ ash/webui/common/resources/network/apn_list_item.js
@@ -107,7 +107,7 @@ class ApnListItem extends ApnListItemBase {
     assert(this.guid);
     assert(this.apn);
     if (!this.apn.id) {
-      console.error('Only custom APNs can be modified.');
+      console.error('Only custom APNs can be disabled.');
       return;
     }
 
@@ -130,12 +130,12 @@ class ApnListItem extends ApnListItemBase {
     assert(this.guid);
     assert(this.apn);
     if (!this.apn.id) {
-      console.error('Only custom APNs can be modified.');
+      console.error('Only custom APNs can be enabled.');
       return;
     }
 
     if (this.apn.state !== ApnState.kDisabled) {
-      console.error('Only an APN that is diabled can be enabled.');
+      console.error('Only an APN that is disabled can be enabled.');
       return;
     }
 
@@ -160,16 +160,6 @@ class ApnListItem extends ApnListItemBase {
     /** @type {!CrActionMenuElement} */ (this.$.dotsMenu).close();
     this.networkConfig_.removeCustomApn(
         this.guid, /** @type {string} */ (this.apn.id));
-  }
-
-  /**
-   * Returns true if detail menu button should be shown.
-   * TODO(b/162365553): Implement.
-   * @return {boolean}
-   * @private
-   */
-  shouldShowDetailsMenuItem_() {
-    return true;
   }
 
   /**
