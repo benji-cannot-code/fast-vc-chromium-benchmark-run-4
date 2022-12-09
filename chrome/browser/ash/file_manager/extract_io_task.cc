c9337a9ad347a27b3e38ef6aebc0805647fd1c1e
Sergey Poromov <poromov@chromium.org>
2022-12-09 15:39:53 +0000

    Add chronos-access group read access to unzipped directories on ChromeOS.
    The in ChromeOS cryptohome (.../MyFiles/ and .../MyFiles/Downloads/)
    now have read access for chronos-access group members,
    unlike the directories created by unzipping an archive (with possible
    subfolders too).
    
    This CL changes access mode from 711 to 751 and changes group
    ownership from chronos to chronos-access.
    
    Bug: b:261695701
    Test: Unzip an archive with subdirectories, change permissions.
    Change-Id: Ifce8c101277c8f4426d45496b95cdc1d8eabbbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086142
    Reviewed-by: François Degros <fdegros@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081475}

--- chrome/browser/ash/file_manager/extract_io_task.cc
+++ chrome/browser/ash/file_manager/extract_io_task.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/file_manager/extract_io_task.h"
 
+#include <grp.h>
 #include <utility>
 
 #include "base/check_op.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/unzip/content/unzip_service.h"
 #include "components/services/unzip/public/mojom/unzipper.mojom.h"
 #include "content/public/browser/browser_thread.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/cros_system_api/constants/cryptohome.h"
 #include "third_party/zlib/google/redact.h"
 
@@ -106,19 +108,35 @@ void ExtractIOTask::FinishedExtraction(base::FilePath directory, bool success) {
   }
 }
 
+absl::optional<gid_t> GetDirectoriesOwnerGid() {
+  struct group grp, *result = nullptr;
+  std::vector<char> buffer(16384);
+  getgrnam_r("chronos-access", &grp, buffer.data(), buffer.size(), &result);
+  if (!result) {
+    return absl::nullopt;
+  }
+  return grp.gr_gid;
+}
+
 // Recursively walk directory and set 'u+rwx,g+x,o+x'.
 bool SetDirectoryPermissions(base::FilePath directory, bool success) {
   // Always set permissions in case of error mid-extract.
   base::FileEnumerator traversal(directory, true,
                                  base::FileEnumerator::DIRECTORIES);
+  const absl::optional<gid_t> owner_gid = GetDirectoriesOwnerGid();
   for (base::FilePath current = traversal.Next(); !current.empty();
        current = traversal.Next()) {
     base::SetPosixFilePermissions(current,
                                   base::FILE_PERMISSION_READ_BY_USER |
                                       base::FILE_PERMISSION_WRITE_BY_USER |
                                       base::FILE_PERMISSION_EXECUTE_BY_USER |
+                                      base::FILE_PERMISSION_READ_BY_GROUP |
                                       base::FILE_PERMISSION_EXECUTE_BY_GROUP |
                                       base::FILE_PERMISSION_EXECUTE_BY_OTHERS);
+    // Might not exist in tests.
+    if (owner_gid.has_value()) {
+      HANDLE_EINTR(chown(current.value().c_str(), -1, owner_gid.value()));
+    }
   }
   return success;
 }
@@ -164,8 +182,15 @@ bool CreateExtractionDirectory(const base::FilePath& destination_directory) {
         destination_directory, base::FILE_PERMISSION_READ_BY_USER |
                                    base::FILE_PERMISSION_WRITE_BY_USER |
                                    base::FILE_PERMISSION_EXECUTE_BY_USER |
+                                   base::FILE_PERMISSION_READ_BY_GROUP |
                                    base::FILE_PERMISSION_EXECUTE_BY_GROUP |
                                    base::FILE_PERMISSION_EXECUTE_BY_OTHERS);
+    // Might not exist in tests.
+    const absl::optional<gid_t> owner_gid = GetDirectoriesOwnerGid();
+    if (created_ok && owner_gid.has_value()) {
+      created_ok = HANDLE_EINTR(
+          chown(destination_directory.value().c_str(), -1, owner_gid.value()));
+    }
   }
   return created_ok;
 }
