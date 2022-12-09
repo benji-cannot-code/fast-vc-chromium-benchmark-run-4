c920b473bb9c87abcb108504a6470362fbce6d50
Keishi Hattori <keishi@chromium.org>
2022-12-09 06:50:06 +0000

    [BRP] Fix raw_ptr clang plugin to exclude some cases
    The rewriter only ran on files that were part of the git repo.
    https://source.chromium.org/chromium/chromium/src/+/main:tools/clang/scripts/run_tool.py;l=110?q=_GetFilesFromGit
    But the clang plugin runs on more files, so it requires additional filters.
    
    This CL fixes several issues:
    - GetFilename() for histograms_ field in metablock_inc.h returns
      "<scratch space>" so filters like isInThirdPartyLocation()
      doesn't work. Filter them out with isInScratchSpace() for now.
      Adding test raw_ptr_fields_scratch_space.cpp
    - GetFilename() for fields in files in out/Default/gen/ start with
      "gen/" so the isInGeneratedLocation() filter didn't work.
    - exclude code in build/linux for example
      "../../build/linux/debian_bullseye_amd64-sysroot/usr/include/x86_64-linux-gnu/sys/queue.h"
    - glslang_tab.cpp.h uses #line directive and modifies the file
      path to"MachineIndependent/glslang.y" so the
      isInThirdPartyLocation() filter cannot catch it even though
      glslang_tab.cpp.h is in third_party/
    
    Change-Id: I8dd45d688e9e9d2bdc568ed52ed066a16097d3f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081426
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081350}

---
+++ tools/clang/plugins/tests/raw_ptr_fields_scratch_space.cpp
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// // Based on third_party/brotli/enc/metablock_inc.h
+
+class HistogramLiteral;
+
+#define FN(X) X##Literal
+#include "raw_ptr_fields_scratch_space_inc.h" /* NOLINT(build/include) */
+#undef FN
