3b1ae2260ed009ad44c3089a6798f6a10878eb82
Victor Vianna <victorvianna@google.com>
2022-12-09 14:22:39 +0000

    Disable ClientHintsTest#testAllClientHints
    Change-Id: I364d1257b0ee270cac1951a1548f0f67f10f7d64
    Bug: 1399817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092536
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081441}

--- android_webview/javatests/src/org/chromium/android_webview/test/ClientHintsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientHintsTest.java
@@ -23,6 +23,7 @@ import org.chromium.android_webview.common.AwFeatures;
 import org.chromium.android_webview.test.util.CookieUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.common.ContentSwitches;
@@ -72,6 +73,7 @@ public class ClientHintsTest {
     @SmallTest
     @Feature({"AndroidWebView"})
     @CommandLineFlags.Add({ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
+    @DisabledTest(message = "crbug.com/1399817")
     public void testAllClientHints() throws Throwable {
         // Initial test setup.
         final TestAwContentsClient contentsClient = new TestAwContentsClient();
