45efa3ecc1ce63c9a2a4f8e6704b5147019adbbe
Nan Lin <linnan@chromium.org>
2022-12-09 17:13:58 +0000

    [Code Health] Replace base::DictionaryValue for HashStoreContents
    Bug: 1187023
    Change-Id: Icf4e59b7ec21761a5a072b79ac97319ec08310a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085209
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081498}

--- services/preferences/tracked/dictionary_hash_store_contents.h
+++ services/preferences/tracked/dictionary_hash_store_contents.h
@@ -7,12 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_PREFERENCES_TRACKED_DICTIONARY_HASH_STORE_CONTENTS_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/values.h"
 #include "services/preferences/tracked/hash_store_contents.h"
-
-namespace base {
-class DictionaryValue;
-class Value;
-}  // namespace base
 
 namespace user_prefs {
 class PrefRegistrySyncable;
@@ -26,7 +22,7 @@ class DictionaryHashStoreContents : public HashStoreContents {
  public:
   // Constructs a DictionaryHashStoreContents that reads from and writes to
   // |storage|.
-  explicit DictionaryHashStoreContents(base::DictionaryValue* storage);
+  explicit DictionaryHashStoreContents(base::Value::Dict* storage);
 
   DictionaryHashStoreContents(const DictionaryHashStoreContents&) = delete;
   DictionaryHashStoreContents& operator=(const DictionaryHashStoreContents&) =
@@ -50,16 +46,16 @@ class DictionaryHashStoreContents : public HashStoreContents {
   void ImportEntry(const std::string& path,
                    const base::Value* in_value) override;
   bool RemoveEntry(const std::string& path) override;
-  const base::DictionaryValue* GetContents() const override;
+  const base::Value::Dict* GetContents() const override;
   std::string GetSuperMac() const override;
   void SetSuperMac(const std::string& super_mac) override;
 
  private:
-  raw_ptr<base::DictionaryValue> storage_;
+  raw_ptr<base::Value::Dict> storage_;
 
   // Helper function to get a mutable version of the macs from |storage_|,
   // creating it if needed and |create_if_null| is true.
-  base::DictionaryValue* GetMutableContents(bool create_if_null);
+  base::Value::Dict* GetMutableContents(bool create_if_null);
 };
 
 #endif  // SERVICES_PREFERENCES_TRACKED_DICTIONARY_HASH_STORE_CONTENTS_H_
