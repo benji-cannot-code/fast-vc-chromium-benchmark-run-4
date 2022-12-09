9c54940e85ef4934ab69766ac3c7d620d3814aa4
Sinan Sahin <sinansahin@google.com>
2022-12-09 02:48:02 +0000

    [GMNext] Add histograms for downloadable font availability at onDraw
    Bug: 1378400
    Change-Id: Id0bbdbb12805a5ac34c443033629c20c0d30f07a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4077055
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081292}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -150,6 +150,13 @@ public class CustomTabActivity extends BaseCustomTabActivity {
     }
 
     @Override
+    protected void onFirstDrawComplete() {
+        super.onFirstDrawComplete();
+
+        FontPreloader.getInstance().onFirstDrawCustomTabActivity();
+    }
+
+    @Override
     public void finishNativeInitialization() {
         if (!mIntentDataProvider.isInfoPage()) {
             FirstRunSignInProcessor.openSyncSettingsIfScheduled(this);
