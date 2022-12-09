94436ebba628be45b2b9ee7eb9da30b6c2329347
Sergey Ulanov <sergeyu@chromium.org>
2022-12-09 21:44:14 +0000

    Reland "[Fuchsia] Enable YUV_420_BIPLANAR usage on Fuchsia"
    This is a reland of commit 49d2b7552d5360ae5f9d191e26ab0c267a75b51c
    
    Original change's description:
    > [Fuchsia] Enable YUV_420_BIPLANAR usage on Fuchsia
    >
    > Previously YUV_420_BIPLANAR were used on Fuchsia only for
    > FuchsiaVideoDecoder. GpuMemoryBufferVideoFramePool was using a pair of
    > R8 and RG88 textures for YUV frames. In this change:
    >  1. Implemented GetSupportedFormatsForTexturing() in Scenic and Flatland
    >     ozone platforms
    >  2. Updated GetPlatformSpecificTextureTarget() to return 0 on Fuchsia,
    >     to satisfy DCHECKs in the existing code.
    >  3. Updated gpu::FeatureInfo to detect YUV format support on Fuchsia.
    >  4. GpuMemoryBufferVideoFramePool now passes ycbcr_info from the source
    >     frame to the frames on the output.
    >  5. VideoResourceUpdater now sets ycbcr_info for video frames where it's
    >     not set by the decoder.
    >
    > Bug: 1395848
    > Change-Id: I26dba2aafaec6c1299f14c6df3befee613dfcc88
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086863
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1081115}
    
    Bug: 1395848
    Change-Id: I56f3fb0af3b30925620a47a71d09ef1130c7ff81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093100
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081658}

--- ui/ozone/platform/flatland/flatland_surface_factory.h
+++ ui/ozone/platform/flatland/flatland_surface_factory.h
@@ -68,6 +68,8 @@ class FlatlandSurfaceFactory : public SurfaceFactoryOzone {
   std::unique_ptr<gpu::VulkanImplementation> CreateVulkanImplementation(
       bool use_swiftshader,
       bool allow_protected_memory) override;
+  std::vector<gfx::BufferFormat> GetSupportedFormatsForTexturing()
+      const override;
 
   // Registers a surface for a |widget|.
   //
