dfdeb8ca1207e9af7cb5cc2d196d463732718cbc
ginnnnnnny <ginnyhuang@chromium.org>
2022-12-09 14:59:41 +0000

    [iOS] Delay feed update message for voiceover users on feed management
    Previously, when a voice over user follows and unfollows a site by going
    to the feed management menu, voice over would read aloud the alert that
    pops up but would be immediately interrupted and start reading the list
    of followed sites from index 0. This is because that the addition and/or
    removal of a cell in the feed management table view is asynchronous, and
    when the UI update is done, it would post the layout change notification
    that cuts off current announcement and start the new one.
    
    I tested on my own device and concluded that 0.6s delay would work for
    me, whereas 0.5s would not. To accommodate slower devices, I hardcoded
    the time to 0.8s. Here's a video of how the feed management table works
    now with voice over:
    
    https://drive.google.com/file/d/1Cdg6I7mc-w2_ZKdIhmQdeMTSfKHmznps/view?usp=share_link&resourcekey=0-jvxDYRVNnmgg2e-YwPRR8g
    
    Fixed: 1361947
    Change-Id: Ib60aa9b87df397190a9c99255b6b35f0b856e92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072726
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081456}

--- ios/chrome/browser/follow/follow_browser_agent.mm
+++ ios/chrome/browser/follow/follow_browser_agent.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/strings/sys_string_conversions.h"
+#import "base/threading/sequenced_task_runner_handle.h"
+#import "base/time/time.h"
 #import "components/feed/core/shared_prefs/pref_names.h"
 #import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -43,6 +45,10 @@ constexpr int kFirstFollowModalShownMaxCount = 3;
 // migration code in `ShouldShowFirstFollowUI()` is removed.
 NSString* const kDisplayedFirstFollowModalCountKey =
     @"DisplayedFirstFollowModalCount";
+
+// Time delay in showing and announcing the notification after a site is
+// followed/unfollowed from follow feed management.
+const base::TimeDelta kSnackbarMessageVoiceOverDelay = base::Seconds(0.8);
 
 // Returns whether the First Follow UI must be displayed.
 bool ShouldShowFirstFollowUI(PrefService* pref_service) {
@@ -163,6 +169,44 @@ base::WeakPtr<FollowBrowserAgent> FollowBrowserAgent::AsWeakPtr() {
 
 FollowBrowserAgent::FollowBrowserAgent(Browser* browser) : browser_(browser) {}
 
+void FollowBrowserAgent::ShowOverlayMessage(FollowSource source,
+                                            NSString* message,
+                                            NSString* button_text,
+                                            MessageBlock message_action,
+                                            CompletionBlock completion_action) {
+  base::WeakPtr<FollowBrowserAgent> weak_ptr = AsWeakPtr();
+  base::OnceClosure closure =
+      base::BindOnce(&FollowBrowserAgent::ShowOverlayMessageHelper, weak_ptr,
+                     message, button_text, message_action, completion_action);
+
+  // Delay showing the snackbar message when voice over is on and the user has
+  // followed/unfollowed the site through feed management. This is to avoid the
+  // announcement being cut off by the addition of a new row to the feed
+  // management table.
+  // TODO(crbug.com/1398955): Temporary solution. A permanent solution should be
+  // in place to make sure that the agent verifies that the feed management UI
+  // is updated before showing the snackbar message.
+  if (UIAccessibilityIsVoiceOverRunning() &&
+      source == FollowSource::Management) {
+    base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, std::move(closure), kSnackbarMessageVoiceOverDelay);
+    return;
+  }
+
+  std::move(closure).Run();
+}
+
+void FollowBrowserAgent::ShowOverlayMessageHelper(
+    NSString* message,
+    NSString* button_text,
+    MessageBlock message_action,
+    CompletionBlock completion_action) {
+  [snack_bar_commands_ showSnackbarWithMessage:message
+                                    buttonText:button_text
+                                 messageAction:message_action
+                              completionAction:completion_action];
+}
+
 void FollowBrowserAgent::OnFollowResponse(WebPageURLs* web_page_urls,
                                           FollowSource source,
                                           FollowResult result,
@@ -250,11 +294,8 @@ void FollowBrowserAgent::OnFollowSuccess(WebPageURLs* web_page_urls,
               FollowConfirmationType::kFollowSucceedSnackbarShown];
     }
   };
-
-  [snack_bar_commands_ showSnackbarWithMessage:message
-                                    buttonText:button_text
-                                 messageAction:message_action
-                              completionAction:completion_action];
+  ShowOverlayMessage(source, message, button_text, message_action,
+                     completion_action);
 }
 
 void FollowBrowserAgent::OnFollowFailure(WebPageURLs* web_page_urls,
@@ -284,11 +325,8 @@ void FollowBrowserAgent::OnFollowFailure(WebPageURLs* web_page_urls,
                             FollowConfirmationType::kFollowErrorSnackbarShown];
     }
   };
-
-  [snack_bar_commands_ showSnackbarWithMessage:message
-                                    buttonText:button_text
-                                 messageAction:message_action
-                              completionAction:completion_action];
+  ShowOverlayMessage(source, message, button_text, message_action,
+                     completion_action);
 }
 
 void FollowBrowserAgent::OnUnfollowSuccess(WebPageURLs* web_page_urls,
@@ -326,11 +364,8 @@ void FollowBrowserAgent::OnUnfollowSuccess(WebPageURLs* web_page_urls,
               FollowConfirmationType::kUnfollowSucceedSnackbarShown];
     }
   };
-
-  [snack_bar_commands_ showSnackbarWithMessage:message
-                                    buttonText:button_text
-                                 messageAction:message_action
-                              completionAction:completion_action];
+  ShowOverlayMessage(source, message, button_text, message_action,
+                     completion_action);
 }
 
 void FollowBrowserAgent::OnUnfollowFailure(WebPageURLs* web_page_urls,
@@ -362,11 +397,8 @@ void FollowBrowserAgent::OnUnfollowFailure(WebPageURLs* web_page_urls,
               FollowConfirmationType::kUnfollowErrorSnackbarShown];
     }
   };
-
-  [snack_bar_commands_ showSnackbarWithMessage:message
-                                    buttonText:button_text
-                                 messageAction:message_action
-                              completionAction:completion_action];
+  ShowOverlayMessage(source, message, button_text, message_action,
+                     completion_action);
 }
 
 raw_ptr<FollowService> FollowBrowserAgent::GetFollowService() {
