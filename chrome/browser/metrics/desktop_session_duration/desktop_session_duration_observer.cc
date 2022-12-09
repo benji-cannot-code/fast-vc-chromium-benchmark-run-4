f593098fe400d8ca3b34e793263bb8b4c196be5c
Nasko Oskov <nasko@chromium.org>
2022-12-09 20:30:21 +0000

    Remove RenderViewHostChanged from DesktopSessionDurationObserver
    RenderViewHostChanged is an old API which is deprecated and we would
    like to remove. This CL moves the DesktopSessionDurationObserver code to
    rely on the newer version which operates on frames. To minimize
    difference in behavior, the new implementation does only considers the
    primary main frame for the WebContents.
    
    Bug: 1168562, 1394513
    Change-Id: Ic02e6ad3fa6e2bd4ffa9d22fcc707d6e33839ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090209
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081596}

--- chrome/browser/metrics/desktop_session_duration/desktop_session_duration_observer.cc
+++ chrome/browser/metrics/desktop_session_duration/desktop_session_duration_observer.cc
@@ -18,8 +18,7 @@ DesktopSessionDurationObserver::DesktopSessionDurationObserver(
       content::WebContentsUserData<DesktopSessionDurationObserver>(
           *web_contents),
       service_(service) {
-  RegisterInputEventObserver(
-      web_contents->GetPrimaryMainFrame()->GetRenderViewHost());
+  RegisterInputEventObserver(web_contents->GetPrimaryMainFrame());
 }
 
 DesktopSessionDurationObserver::~DesktopSessionDurationObserver() {}
@@ -43,15 +42,15 @@ DesktopSessionDurationObserver::CreateForWebContents(
 }
 
 void DesktopSessionDurationObserver::RegisterInputEventObserver(
-    content::RenderViewHost* host) {
+    content::RenderFrameHost* host) {
   if (host != nullptr)
-    host->GetWidget()->AddInputEventObserver(this);
+    host->GetRenderWidgetHost()->AddInputEventObserver(this);
 }
 
 void DesktopSessionDurationObserver::UnregisterInputEventObserver(
-    content::RenderViewHost* host) {
+    content::RenderFrameHost* host) {
   if (host != nullptr)
-    host->GetWidget()->RemoveInputEventObserver(this);
+    host->GetRenderWidgetHost()->RemoveInputEventObserver(this);
 }
 
 void DesktopSessionDurationObserver::OnInputEvent(
@@ -59,9 +58,12 @@ void DesktopSessionDurationObserver::OnInputEvent(
   service_->OnUserEvent();
 }
 
-void DesktopSessionDurationObserver::RenderViewHostChanged(
-    content::RenderViewHost* old_host,
-    content::RenderViewHost* new_host) {
+void DesktopSessionDurationObserver::RenderFrameHostChanged(
+    content::RenderFrameHost* old_host,
+    content::RenderFrameHost* new_host) {
+  if (!new_host->IsInPrimaryMainFrame())
+    return;
+
   UnregisterInputEventObserver(old_host);
   RegisterInputEventObserver(new_host);
 }
