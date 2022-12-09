24e58f48cc47108802b2ac3409505627b6e0cddb
Philip Rogers <pdr@chromium.org>
2022-12-09 16:23:26 +0000

    Remove GPUMemoryForTilingsInKb metrics
    This has been superseded by Memory.Total.TileMemory.
    
    Bug: 1368160
    Change-Id: I6cba6e26afc34fb3748009d074b42f37d0427a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081207
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081491}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/input/browser_controls_offset_manager.h"
 #include "cc/input/page_scale_animation.h"
 #include "cc/input/scrollbar_animation_controller.h"
-#include "cc/input/scroller_size_metrics.h"
 #include "cc/layers/append_quads_data.h"
 #include "cc/layers/effect_tree_layer_list_iterator.h"
 #include "cc/layers/heads_up_display_layer_impl.h"
@@ -1613,10 +1612,6 @@ DrawResult LayerTreeHostImpl::PrepareToDraw(FrameData* frame) {
     // of the process, so the histogram names are runtime constant.
     const char* client_name = GetClientNameForMetrics();
     if (client_name) {
-      size_t total_gpu_memory_for_tilings_in_bytes = 0;
-      for (const PictureLayerImpl* layer : active_tree()->picture_layers())
-        total_gpu_memory_for_tilings_in_bytes += layer->GPUMemoryUsageInBytes();
-
       UMA_HISTOGRAM_CUSTOM_COUNTS(
           base::StringPrintf("Compositing.%s.NumActiveLayers", client_name),
           base::saturated_cast<int>(active_tree_->NumLayers()), 1, 1000, 20);
@@ -1626,19 +1621,6 @@ DrawResult LayerTreeHostImpl::PrepareToDraw(FrameData* frame) {
                              client_name),
           base::saturated_cast<int>(active_tree_->picture_layers().size()), 1,
           1000, 20);
-
-      // TODO(pdr): Instead of skipping empty picture layers, maybe we should
-      // accumulate layer->GetRasterSource()->GetMemoryUsage() above and skip
-      // recording when the accumulated memory usage is 0.
-      if (!active_tree()->picture_layers().empty()) {
-        UMA_HISTOGRAM_CUSTOM_COUNTS(
-            base::StringPrintf("Compositing.%s.GPUMemoryForTilingsInKb",
-                               client_name),
-            base::saturated_cast<int>(total_gpu_memory_for_tilings_in_bytes /
-                                      1024),
-            1, kGPUMemoryForTilingsLargestBucketKb,
-            kGPUMemoryForTilingsBucketCount);
-      }
     }
   }
 
