c4aa112d49daac190c4c885d7f59fe6dc25156c2
Wenbo Jie <wenbojie@chromium.org>
2022-12-09 04:13:37 +0000

    [FilesRefresh] Add a icon widget to render custom SVG icons
    Demo: http://screen/4MYDKA4xcMMdbT3.png
    
    
    Bug: b:261672864
    Test: browser_tests --gtest_filter="*XfIcon"
    Change-Id: I6538066481e4ad489ecbbbe391fcd47bccb27b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085685
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081311}

--- ash/webui/file_manager/resources/labs/main.ts
+++ ash/webui/file_manager/resources/labs/main.ts
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://file-manager/foreground/elements/xf_button.js';
 import 'chrome://file-manager/widgets/xf_breadcrumb.js';
+import 'chrome://file-manager/widgets/xf_icon.js';
 import 'chrome://file-manager/widgets/xf_tree.js';
 import 'chrome://file-manager/widgets/xf_tree_item.js';
 
