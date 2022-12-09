6d7a9584aa9175d25f67ca4429bec05145e97580
Nancy Wang <nancylingwang@chromium.org>
2022-12-09 00:30:08 +0000

    Remove the mojom CapabilityAccess struct.
    BUG=1253250
    
    Change-Id: I3b83a902464cb4df8cb8d8456677d05abf56b924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4087683
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081244}

--- components/services/app_service/public/cpp/app_capability_access_cache.h
+++ components/services/app_service/public/cpp/app_capability_access_cache.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/account_id/account_id.h"
 #include "components/services/app_service/public/cpp/capability_access.h"
 #include "components/services/app_service/public/cpp/capability_access_update.h"
-#include "components/services/app_service/public/cpp/features.h"
 
 namespace apps {
 
@@ -111,8 +110,6 @@ class COMPONENT_EXPORT(APP_UPDATE) AppCapabilityAccessCache {
   // The callee will consume the deltas. An apps::mojom::CapabilityAccessPtr has
   // the ownership semantics of a unique_ptr, and will be deleted when out of
   // scope. The caller presumably calls OnCapabilityAccesses(std::move(deltas)).
-  void OnCapabilityAccesses(
-      std::vector<apps::mojom::CapabilityAccessPtr> deltas);
   void OnCapabilityAccesses(std::vector<CapabilityAccessPtr> deltas);
 
   // Calls f, a void-returning function whose arguments are (const
@@ -183,14 +180,11 @@ class COMPONENT_EXPORT(APP_UPDATE) AppCapabilityAccessCache {
   }
 
  private:
-  void DoOnCapabilityAccesses(
-      std::vector<apps::mojom::CapabilityAccessPtr> deltas);
   void DoOnCapabilityAccesses(std::vector<CapabilityAccessPtr> deltas);
 
   base::ObserverList<Observer> observers_;
 
   // Maps from app_id to the latest state: the "sum" of all previous deltas.
-  std::map<std::string, apps::mojom::CapabilityAccessPtr> mojom_states_;
   std::map<std::string, CapabilityAccessPtr> states_;
 
   // Track the deltas being processed or are about to be processed by
@@ -211,9 +205,6 @@ class COMPONENT_EXPORT(APP_UPDATE) AppCapabilityAccessCache {
   // Nested OnCapabilityAccesses calls are expected to be rare (but still dealt
   // with sensibly). In the typical case, OnCapabilityAccesses should call
   // DoOnCapabilityAccesses exactly once, and deltas_pending_ will stay empty.
-  std::map<std::string, apps::mojom::CapabilityAccess*>
-      mojom_deltas_in_progress_;
-  std::vector<apps::mojom::CapabilityAccessPtr> mojom_deltas_pending_;
   std::map<std::string, CapabilityAccess*> deltas_in_progress_;
   std::vector<CapabilityAccessPtr> deltas_pending_;
 
