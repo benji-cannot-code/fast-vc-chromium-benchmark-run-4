1a0a9ec6c519dc7d6976f82098b34a0203958a7f
Anders Hartvoll Ruud <andruud@chromium.org>
2022-12-12 17:04:04 +0000

    Use ComputedStyle to determine CQ containers in ancestors
    StyleRecalcContext:From[Inclusive]Ancestors currently look
    at an ancestor's ContainerQueryEvaluator to determine whether
    or not that ancestor is a CQ container. This is correct for
    normal circumstances where style recalc has happened for the
    ancestor, but not if the ancestor is in display:none.
    In that case, the ancestor is still a container, but there
    will be no evaluator.
    
    This CL fixes this by instead checking the ComputedStyle
    of the ancestor to determine containership.
    
    Fixed: 1399755
    Change-Id: Ia4096d158737c2e98a9215a3d8e7b2273d77320f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4091926
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081833}

--- third_party/blink/renderer/core/css/style_recalc_context.cc
+++ third_party/blink/renderer/core/css/style_recalc_context.cc
@@ -17,7 +17,7 @@ Element* ClosestInclusiveAncestorContainer(Element& element,
                                            Element* stay_within = nullptr) {
   for (auto* container = &element; container && container != stay_within;
        container = container->ParentOrShadowHostElement()) {
-    if (container->GetContainerQueryEvaluator())
+    if (container->ComputedStyleRef().IsContainerForSizeContainerQueries())
       return container;
   }
   return nullptr;
@@ -27,6 +27,7 @@ Element* ClosestInclusiveAncestorContainer(Element& element,
 
 StyleRecalcContext StyleRecalcContext::FromInclusiveAncestors(
     Element& element) {
+  DCHECK(element.GetComputedStyle());
   return StyleRecalcContext{ClosestInclusiveAncestorContainer(element)};
 }
 
