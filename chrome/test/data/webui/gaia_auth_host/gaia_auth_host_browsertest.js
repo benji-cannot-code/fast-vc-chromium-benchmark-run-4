6f81fcc10fd943abfeef22a2c80e7f45b1bfb855
dpapad <dpapad@chromium.org>
2022-12-12 20:17:25 +0000

    Disable TrustedTypes CSP checks on chrome://chrome-signin.
    There is a TT violation (DOMParser.parseFromString) that was previously
    not caught by the corresponding tests at [1]. This CL, besides disabling
    TT checks, also fixes the tests so that if/when TT is re-enabled on
    chrome://chrome-signin/ the tests will fail, if the violation has not
    been fixed.
    
    [1] GaiaAuthHostSamlPasswordAttributesTest.All
    
    Bug: 1399912
    Change-Id: Ie071282fd4bba52466c2358f95680720d71fbb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4091719
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081989}

--- chrome/test/data/webui/gaia_auth_host/gaia_auth_host_browsertest.js
+++ chrome/test/data/webui/gaia_auth_host/gaia_auth_host_browsertest.js
@@ -19,11 +19,6 @@ var GaiaAuthHostBrowserTest = class extends testing.Test {
   get isAsync() {
     return true;
   }
-
-  /** @override */
-  get webuiHost() {
-    return 'chrome-signin';
-  }
 };
 
 [['PasswordChangeAuthenticator', 'password_change_authenticator_test.js'],
@@ -38,7 +33,7 @@ function registerTest(testName, module, caseName) {
   this[className] = class extends GaiaAuthHostBrowserTest {
     /** @override */
     get browsePreload() {
-      return `chrome://webui-test/test_loader.html?module=gaia_auth_host/${
+      return `chrome://chrome-signin/test_loader.html?module=gaia_auth_host/${
           module}`;
     }
   };
