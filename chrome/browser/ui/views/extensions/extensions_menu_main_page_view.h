8efb37f3c337117519c981ed6815184626e0f3be
Emilia Paz <emiliapaz@chromium.org>
2022-12-12 23:09:54 +0000

    [Extensions c2s] Add extensions menu base and main page view
    Sets the extensions menu contents view to `ExtensionsMenuBaseView`. This uses a page switcher as a container for the different pages, which for now just has `ExtensionsMenuMainPageView`.
    
    Screenshot: https://drive.google.com/file/d/1fSugvlz3tMnDygs35wMLnJgAyr0J1YWR/view?usp=sharing
    
    Bug: 1390952
    Change-Id: Ib6f81ccb11c41ceeb0c1b37fea4a50434b0001b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062053
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082119}

---
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_MAIN_PAGE_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_MAIN_PAGE_VIEW_H_
+
+#include "ui/views/view.h"
+
+class ExtensionsMenuNavigationHandler;
+
+// The main view of the extensions menu.
+class ExtensionsMenuMainPageView : public views::View {
+ public:
+  explicit ExtensionsMenuMainPageView(
+      ExtensionsMenuNavigationHandler* navigation_handler);
+  ~ExtensionsMenuMainPageView() override = default;
+  ExtensionsMenuMainPageView(const ExtensionsMenuMainPageView&) = delete;
+  const ExtensionsMenuMainPageView& operator=(
+      const ExtensionsMenuMainPageView&) = delete;
+
+ private:
+  raw_ptr<ExtensionsMenuNavigationHandler> navigation_handler_;
+};
+
+BEGIN_VIEW_BUILDER(/* no export */, ExtensionsMenuMainPageView, views::View)
+END_VIEW_BUILDER
+
+DEFINE_VIEW_BUILDER(/* no export */, ExtensionsMenuMainPageView)
+
+#endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_MAIN_PAGE_VIEW_H_
---
+++ chrome/browser/ui/views/extensions/extensions_menu_navigation_handler.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_NAVIGATION_HANDLER_H_
+#define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_NAVIGATION_HANDLER_H_
+
+// An interface that provides methods to navigate between pages of the
+// extensions menu.
+class ExtensionsMenuNavigationHandler {
+ public:
+  virtual void CloseBubble() = 0;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_NAVIGATION_HANDLER_H_
