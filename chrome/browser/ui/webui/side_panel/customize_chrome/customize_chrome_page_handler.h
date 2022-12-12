dcb8d41b73fa65dfb5dfe0bef0b81ad422a21fae
Riley Tatum <rtatum@google.com>
2022-12-12 22:55:47 +0000

    [NTP][Panorama] Add Setting Theme to Classic Chrome to Page Handler
    Bug: 1384245
    Change-Id: I4e352c3713adb7f13833beed1a0768e57c547a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4091365
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082100}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
@@ -58,6 +58,7 @@ class CustomizeChromePageHandler
   void UpdateTheme() override;
   void SetDefaultColor() override;
   void SetForegroundColor(SkColor foreground_color) override;
+  void SetClassicChromeDefaultTheme() override;
 
  private:
   // ui::NativeThemeObserver:
