588b2d04151e60d5c2d0d1f9996ae97cb6313eea
Andrey Zaytsev <andzaytsev@google.com>
2022-12-12 16:07:57 +0000

    Site Data Improvements: Add new Clear & Reset dialog
    Screenshot: https://screenshot.googleplex.com/5NxWMibXxxDkvgw.png
    
    Bug: 1342991
    Change-Id: I509139e7b9ddc1b151b3da865868ec79f50788dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086542
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1081744}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/GroupedWebsitesSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/GroupedWebsitesSettings.java
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.browser_ui.site_settings;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 
@@ -16,7 +20,8 @@ import org.chromium.components.browser_ui.settings.TextMessagePreference;
 /**
  * Shows the permissions and other settings for a group of websites.
  */
-public class GroupedWebsitesSettings extends SiteSettingsPreferenceFragment {
+public class GroupedWebsitesSettings
+        extends SiteSettingsPreferenceFragment implements Preference.OnPreferenceClickListener {
     public static final String EXTRA_GROUP = "org.chromium.chrome.preferences.site_group";
 
     // Preference keys, see grouped_websites_preferences.xml.
@@ -25,8 +30,11 @@ public class GroupedWebsitesSettings extends SiteSettingsPreferenceFragment {
     public static final String PREF_RELATED_SITES_HEADER = "related_sites_header";
     public static final String PREF_RELATED_SITES = "related_sites";
     public static final String PREF_SITES_IN_GROUP = "sites_in_group";
+    public static final String PREF_RESET_GROUP = "reset_group_button";
 
     private WebsiteGroup mSiteGroup;
+
+    private Dialog mConfirmationDialog;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -65,6 +73,7 @@ public class GroupedWebsitesSettings extends SiteSettingsPreferenceFragment {
                         getContext().getString(R.string.domain_settings_sites_in_group,
                                 mSiteGroup.getDomainAndRegistry())));
         setUpClearDataPreference();
+        setUpResetGroupPreference();
         setUpRelatedSitesPreferences();
         updateSitesInGroup();
     }
@@ -72,10 +81,37 @@ public class GroupedWebsitesSettings extends SiteSettingsPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference instanceof WebsiteRowPreference) {
+            // Handle a click on one of the sites in this group.
             ((WebsiteRowPreference) preference).handleClick(getArguments());
         }
-
         return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        // Handle a click on the Clear & Reset button.
+        View dialogView =
+                getActivity().getLayoutInflater().inflate(R.layout.clear_reset_dialog, null);
+        TextView mainMessage = dialogView.findViewById(R.id.main_message);
+        mainMessage.setText(R.string.website_reset_confirmation);
+        TextView signedOutText = dialogView.findViewById(R.id.signed_out_text);
+        signedOutText.setText(R.string.webstorage_clear_data_dialog_sign_out_message);
+        TextView offlineText = dialogView.findViewById(R.id.offline_text);
+        offlineText.setText(R.string.webstorage_clear_data_dialog_offline_message);
+        mConfirmationDialog =
+                new AlertDialog.Builder(getContext(), R.style.ThemeOverlay_BrowserUI_AlertDialog)
+                        .setView(dialogView)
+                        .setTitle(R.string.website_reset_confirmation_title)
+                        .setPositiveButton(
+                                R.string.website_reset, (dialog, which) -> { resetGroup(); })
+                        .setNegativeButton(
+                                R.string.cancel, (dialog, which) -> mConfirmationDialog = null)
+                        .show();
+        return true;
+    }
+
+    private void resetGroup() {
+        // TODO(crbug.com/1342991): Implement the deletion and UI logic for handling it.
     }
 
     private void setUpClearDataPreference() {
@@ -95,6 +131,16 @@ public class GroupedWebsitesSettings extends SiteSettingsPreferenceFragment {
             getPreferenceScreen().removePreference(preference);
         }
     }
+
+    private void setUpResetGroupPreference() {
+        Preference preference = findPreference(PREF_RESET_GROUP);
+        if (mSiteGroup.isCookieDeletionDisabled(
+                    getSiteSettingsDelegate().getBrowserContextHandle())) {
+            preference.setEnabled(false);
+        }
+        preference.setOnPreferenceClickListener(this);
+    }
+
     private void setUpRelatedSitesPreferences() {
         var relatedSitesHeader = findPreference(PREF_RELATED_SITES_HEADER);
         TextMessagePreference relatedSitesText = findPreference(PREF_RELATED_SITES);
