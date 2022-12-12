53d8c09d27dc1264564e023caf5ea74d0f0c8d2d
Steven Luong <stluong@chromium.org>
2022-12-12 16:43:47 +0000

    CBUI Views: Code Cleanups
    Cleaned up code that was mistakenly left in for previous CLs.
    Refactoring includes:
      - Replacing hardcoded values with class constants instead
      - Removing unused lines of code and #includes
      - Change return type of custom step functions to auto
      - Moving and renaming browser_app_menu_browsertest.cc to be more
    accurately named since this test was misnamed when written. This test
    focuses on testing the high efficiency IPH and not the browser app menu
    itself.
    
    Change-Id: Icc9996e5a119b7c31a92d7013273bbf185e05756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090157
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081797}

--- chrome/browser/ui/views/performance_controls/high_efficiency_chip_view_interactive_ui_test.cc
+++ chrome/browser/ui/views/performance_controls/high_efficiency_chip_view_interactive_ui_test.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/utils.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_controller.h"
 #include "chrome/browser/ui/views/performance_controls/high_efficiency_bubble_view.h"
@@ -31,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/interaction/element_tracker_views.h"
-#include "ui/views/view.h"
 #include "url/gurl.h"
 
 namespace {
@@ -98,7 +96,7 @@ class HighEfficiencyChipInteractiveTest : public InteractiveBrowserTest {
         ->IsDiscarded();
   }
 
-  StepBuilder DiscardTab(int discard_tab_index) {
+  auto DiscardTab(int discard_tab_index) {
     return Do(base::BindLambdaForTesting([=]() {
       EXPECT_NE(discard_tab_index, tab_strip_model_->active_index());
       EXPECT_FALSE(IsTabDiscarded(discard_tab_index));
@@ -111,8 +109,8 @@ class HighEfficiencyChipInteractiveTest : public InteractiveBrowserTest {
 
   // Discard the tab at discard_tab_index and navigates to that tab and waits
   // for it to reload
-  MultiStep DiscardAndSelectTab(int discard_tab_index,
-                                const ui::ElementIdentifier& contents_id) {
+  auto DiscardAndSelectTab(int discard_tab_index,
+                           const ui::ElementIdentifier& contents_id) {
     return Steps(FlushEvents(),
                  // This has to be done on a fresh message loop to prevent
                  // a tab being discarded while it is notifying its observers
@@ -121,10 +119,15 @@ class HighEfficiencyChipInteractiveTest : public InteractiveBrowserTest {
                  WaitForShow(contents_id));
   }
 
+  auto CheckChipIsExpandedState() {
+    return CheckViewProperty(kHighEfficiencyChipElementId,
+                             &PageActionIconView::ShouldShowLabel, true);
+  }
+
   // Discard and reload the tab at discard_tab_index the number of times the
   // high efficiency page action chip can expand so subsequent discards
   // will result in the chip staying in its collapsed state
-  MultiStep DiscardTabUntilChipStopsExpanding(
+  auto DiscardTabUntilChipStopsExpanding(
       size_t discard_tab_index,
       size_t non_discard_tab_index,
       const ui::ElementIdentifier& contents_id) {
@@ -141,23 +144,18 @@ class HighEfficiencyChipInteractiveTest : public InteractiveBrowserTest {
   }
 
   // Navigates the current active tab to the given URL and waits for it to load
-  MultiStep NavigateTab(GURL url, const ui::ElementIdentifier& contents_id) {
+  auto NavigateTab(GURL url, const ui::ElementIdentifier& contents_id) {
     return Steps(
         Do(base::BindLambdaForTesting([=]() { util_->LoadPage(url); })),
         WaitForWebContentsNavigation(contents_id, url));
   }
 
-  StepBuilder CheckChipIsExpandedState() {
-    return CheckViewProperty(kHighEfficiencyChipElementId,
-                             &PageActionIconView::ShouldShowLabel, true);
-  }
-
-  StepBuilder CheckChipIsCollapsedState() {
+  auto CheckChipIsCollapsedState() {
     return CheckViewProperty(kHighEfficiencyChipElementId,
                              &PageActionIconView::ShouldShowLabel, false);
   }
 
-  StepBuilder NameTab(size_t index, std::string name) {
+  auto NameTab(size_t index, std::string name) {
     return NameViewRelative(
         kTabStripElementId, name,
         base::BindOnce([](size_t index, TabStrip* tab_strip)
@@ -353,13 +351,6 @@ class HighEfficiencyInfoIPHInteractiveTest
     auto* promo_controller = static_cast<BrowserFeaturePromoController*>(
         browser()->window()->GetFeaturePromoController());
     return promo_controller;
-  }
-
-  StepBuilder WaitForFeatureEngagementReady() {
-    return Do(base::BindLambdaForTesting([&]() {
-      EXPECT_TRUE(user_education::test::WaitForFeatureEngagementReady(
-          GetFeaturePromoController()));
-    }));
   }
 };
 
--- chrome/browser/ui/views/performance_controls/high_efficiency_iph_browsertest.cc
+++ chrome/browser/ui/views/performance_controls/high_efficiency_iph_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -58,8 +58,9 @@ class HighEfficiencyHelpPromoTest : public InProcessBrowserTest {
     auto lock =
         BrowserFeaturePromoController::BlockActiveWindowCheckForTesting();
 
-    views::NamedWidgetShownWaiter waiter(views::test::AnyWidgetTestPasskey{},
-                                         "HelpBubbleView");
+    views::NamedWidgetShownWaiter waiter(
+        views::test::AnyWidgetTestPasskey{},
+        user_education::HelpBubbleView::kViewClassName);
 
     int tab_count_threshold =
         performance_manager::features::kHighEfficiencyModePromoTabCountThreshold
