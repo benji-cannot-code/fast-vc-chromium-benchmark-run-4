03aa67d919134ef6773481e6652036d093ce4f29
Xianzhu Wang <wangxianzhu@chromium.org>
2022-12-12 19:11:45 +0000

    Fix performance regression caused by crrev.com/1080566
    Let MemoryManagedPaintRecorder reuse the canvas (which now manages
    PaintOpBuffer) across recordings. Before crrev.com/1080566, the
    DisplayItemList in PaintRecorder was reused across recordings.
    
    PaintRecorder and InspectablePaintRecorder already reuse the canvas
    across recordings.
    
    This fixes 5% regression of motionmark lines caused by
    crrev.com/1080566.
    
    Bug: 1395099
    Change-Id: I60bce72f91bf17933dfd084940104198aafafe31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093178
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081951}

--- third_party/blink/renderer/platform/graphics/memory_managed_paint_canvas.cc
+++ third_party/blink/renderer/platform/graphics/memory_managed_paint_canvas.cc
@@ -15,6 +15,11 @@ MemoryManagedPaintCanvas::MemoryManagedPaintCanvas(const gfx::Size& size,
 
 MemoryManagedPaintCanvas::~MemoryManagedPaintCanvas() = default;
 
+sk_sp<cc::PaintRecord> MemoryManagedPaintCanvas::ReleaseAsRecord() {
+  cached_image_ids_.clear();
+  return cc::InspectableRecordPaintCanvas::ReleaseAsRecord();
+}
+
 void MemoryManagedPaintCanvas::drawImage(const cc::PaintImage& image,
                                          SkScalar left,
                                          SkScalar top,
